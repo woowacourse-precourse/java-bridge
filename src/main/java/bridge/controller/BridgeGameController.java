e9412ae569fe34266dc4433a035cdb1960709bb0
송민진 <m_6595@naver.com>
2022-11-17 03:08:35 +0900

    feat(bridge): start bridge game with a start message
    구현 : 게임 시작 문구 출력하기

---
+++ src/main/java/bridge/controller/BridgeGameController.java
@@ -0,0 +1,16 @@
+package bridge.controller;
+
+import bridge.model.BridgeGame;
+import bridge.view.InputView;
+import bridge.view.OutputView;
+
+public class BridgeGameController {
+    private final InputView inputView = new InputView();
+    private final OutputView outputView = new OutputView();
+
+    public void run() {
+        outputView.printStartMessage();
+        BridgeGame bridgeGame = new BridgeGame();
+
+    }
+}
