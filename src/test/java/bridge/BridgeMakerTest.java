8a666f3b7072d256eaca6d1d1e3810e0f1ae675c
seung-won-kim <hesjs5@skuniv.ac.kr>
2022-11-16 18:37:58 +0900

    test(BridgeMakerTest): 다리를 생성하는 기능 테스트

---
+++ src/test/java/bridge/BridgeMakerTest.java
@@ -0,0 +1,36 @@
+package bridge;
+
+import static bridge.constant.BridgeMove.*;
+import static org.assertj.core.api.Assertions.*;
+
+import bridge.constant.BridgeMove;
+import java.util.List;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+class BridgeMakerTest {
+
+    BridgeRandomNumberGenerator bridgeRandomNumberGenerator;
+    BridgeMaker bridgeMaker;
+
+    @BeforeEach
+    void init() {
+        bridgeRandomNumberGenerator = new BridgeRandomNumberGenerator();
+        bridgeMaker = new BridgeMaker(bridgeRandomNumberGenerator);
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {3})
+    void makeBridge(int size) {
+        // when
+        List<String> result = bridgeMaker.makeBridge(size);
+
+        // then
+        System.out.println("result = " + result);
+        assertThat(result.size()).isEqualTo(size);
+        assertThat(result).containsOnly(DOWN.getFirstLetter(), UP.getFirstLetter());
+        assertThat(result).allMatch(s -> s.equals(DOWN.getFirstLetter()) || s.equals(UP.getFirstLetter()));
+    }
+}
