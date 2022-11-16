9489e264e46c3d1ef2033f40fdb0e2638c81a110
hongil <sonhl0723@naver.com>
2022-11-16 23:28:30 +0900

    test (InputViewTest): 이동 방향 입력 기능 테스트

--- src/test/java/bridge/InputViewTest.java
+++ src/test/java/bridge/InputViewTest.java
@@ -21,13 +21,33 @@ public class InputViewTest {
         }
     }
 
-    @DisplayName("다리 길이가 숫자 타입이 아닐 경우 오류가 발생한다.")
+    @DisplayName("다리 길이가 3~20 사이의 숫자인 경우 오류가 발생하지 않는다.")
     @Test
     void correctBridgeSize() {
         List<String> testInput = List.of("3", "15", "20");
 
         for (String input : testInput) {
             assertThat(Integer.toString(inputView.readBridgeSize(input))).isEqualTo(input);
+        }
+    }
+
+    @DisplayName("이동 방향이 U 혹은 D이 아닌 경우 오류가 발생한다.")
+    @Test
+    void wrongDirection() {
+        List<String> testInput = List.of("1", "Q", "d", "-");
+
+        for (String input : testInput) {
+            assertThat(inputView.readMoving(input)).isNotEqualTo(input);
+        }
+    }
+
+    @DisplayName("이동 방향이 U 혹은 D인 경우 정상적으로 작동한다.")
+    @Test
+    void correctDirection() {
+        List<String> testInput = List.of("U", "D");
+
+        for (String input : testInput) {
+            assertThat(inputView.readMoving(input)).isEqualTo(input);
         }
     }
 }
