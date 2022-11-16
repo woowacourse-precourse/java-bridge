0858a378eacf8b30075317a22f1498f3392eca58
Dan-HeeYoung9182 <ldan9182@gmail.com>
2022-11-16 21:19:26 +0900

    유저로부터 다리 길이를 받고 잘못된 인풋에 대한 에러 체크

--- src/main/java/bridge/InputView.java
+++ src/main/java/bridge/InputView.java
@@ -1,15 +1,24 @@
 package bridge;
 
+import camp.nextstep.edu.missionutils.Console;
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
  */
 public class InputView {
 
+    static final String OPEN_STATEMENT = "다리 건너기 게임을 시작합니다.";
+    static final String ENTER_BRIDGE_LENGTH = "다리의 길이를 입력해주세요.";
+    static final String ERROR_NUMBER = "[ERROR] 다리 길이는 3부터 20 사이의 숫자여야 합니다.";
+    static final String ERROR_STRING = "[ERROR] 숫자를 입력하여 주세요.";
+    static final int UPPER_BOUND = 20;
+    static final int LOWER_BOUND = 3;
     /**
      * 다리의 길이를 입력받는다.
      */
     public int readBridgeSize() {
-        return 0;
+        System.out.println(OPEN_STATEMENT);
+        System.out.println(ENTER_BRIDGE_LENGTH);
+        return catchException();
     }
 
     /**
@@ -25,4 +34,22 @@ public class InputView {
     public String readGameCommand() {
         return null;
     }
+
+    public static int catchException(){
+        int input = 0;
+        try{
+            input = Integer.parseInt(Console.readLine());
+            rangeCheck(input);
+        } catch(NumberFormatException numException){
+            System.out.println(ERROR_STRING);
+        } catch(IllegalArgumentException argException){
+            System.out.println(ERROR_NUMBER);
+        }
+        return input;
+    }
+
+    public static void rangeCheck(int number){
+        if(!(number >= LOWER_BOUND && number <= UPPER_BOUND)) throw new IllegalArgumentException();
+    }
+
 }
---
+++ src/test/java/bridge/InputViewTest.java
@@ -0,0 +1,23 @@
+package bridge;
+
+import org.junit.jupiter.api.Test;
+import camp.nextstep.edu.missionutils.test.NsTest;
+
+import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class InputViewTest extends NsTest {
+
+//    static final String ERROR_MESSAGE = "[ERROR]";
+//
+//    @Test
+//    void 예외_테스트() {
+//        assertSimpleTest(() -> {
+//            runException("a");
+//            assertThat(output()).contains(ERROR_MESSAGE);
+//        });
+//    }
+
+    @Override
+    protected void runMain() {}
+}
