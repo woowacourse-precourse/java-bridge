f6e064b1d06edc8e867cd37146547e0b8e8f2634
JunH <tjdtls690@gmail.com>
2022-11-17 01:29:36 +0900

    :art: refactor(Domain : BridgeGame) : 다리를 이동하는 메서드 기능 분리

--- src/main/java/bridge/domain/BridgeGame.java
+++ src/main/java/bridge/domain/BridgeGame.java
@@ -15,14 +15,14 @@ import java.util.List;
  */
 public class BridgeGame {
     private final List<String> bridge;
-    private final LinkedList<State> states;
     private NumberOfTry numberOfTry;
+    private final LinkedList<State> states;
     
     public BridgeGame(final BridgeNumberGenerator bridgeNumberGenerator, final BridgeSizeDTO bridgeSizeDTO) {
         final int bridgeSize = bridgeSizeDTO.getBridgeSize();
         bridge = new BridgeMaker(bridgeNumberGenerator).makeBridge(bridgeSize);
-        states = new LinkedList<>();
         numberOfTry = new NumberOfTry();
+        states = new LinkedList<>();
         System.out.println(bridge);
     }
     
@@ -32,11 +32,17 @@ public class BridgeGame {
      * 이동을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
     public void move(final MovingDTO movingDTO) {
-        final Ready ready = new Ready(bridge);
-        final State nextState = ready.move(states.size(), movingDTO.getMoving());
-        states.add(nextState);
+        states.add(nextState(movingDTO));
     }
-
+    
+    private State nextState(final MovingDTO movingDTO) {
+        return initReady().move(states.size(), movingDTO.getMoving());
+    }
+    
+    private Ready initReady() {
+        return new Ready(bridge);
+    }
+    
     /**
      * 사용자가 게임을 다시 시도할 때 사용하는 메서드
      * <p>
@@ -51,11 +57,11 @@ public class BridgeGame {
         return !states.isEmpty() && lastState().isGameFinished(states.size());
     }
     
-    private State lastState() {
-        return states.getLast();
-    }
-    
     public boolean isMoveFail() {
         return lastState().isMoveFail();
+    }
+    
+    private State lastState() {
+        return states.getLast();
     }
 }
