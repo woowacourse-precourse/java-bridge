8f43c6eb7a7496f5a0344c3ab979337df57ee290
Hyun-Seo Oh <ohseo8813@gmail.com>
2022-11-16 18:07:21 +0900

    <feat>(BridgeMaker.java): 다리를 생성한다

--- src/main/java/bridge/BridgeMaker.java
+++ src/main/java/bridge/BridgeMaker.java
@@ -1,6 +1,12 @@
 package bridge;
 
+import static bridge.constant.BridgeConstant.LOWER_BLOCK;
+import static bridge.constant.BridgeConstant.UPPER_BLOCK;
+
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 /**
  * 다리의 길이를 입력 받아서 다리를 생성해주는 역할을 한다.
@@ -12,12 +18,17 @@ public class BridgeMaker {
     public BridgeMaker(BridgeNumberGenerator bridgeNumberGenerator) {
         this.bridgeNumberGenerator = bridgeNumberGenerator;
     }
-
-    /**
-     * @param size 다리의 길이
-     * @return 입력받은 길이에 해당하는 다리 모양. 위 칸이면 "U", 아래 칸이면 "D"로 표현해야 한다.
-     */
+    
     public List<String> makeBridge(int size) {
-        return null;
+        return IntStream.range(0, size)
+                .mapToObj(idx -> getBridgeBlock())
+                .collect(Collectors.toList());
+    }
+
+    private String getBridgeBlock() {
+        if (bridgeNumberGenerator.generate() == 1) {
+            return UPPER_BLOCK;
+        }
+        return LOWER_BLOCK;
     }
 }
--- src/main/java/bridge/constant/BridgeConstant.java
+++ src/main/java/bridge/constant/BridgeConstant.java
@@ -4,4 +4,7 @@ public class BridgeConstant {
 
     public static final int MAX_SIZE = 20;
     public static final int MIN_SIZE = 3;
+
+    public static final String UPPER_BLOCK = "U";
+    public static final String LOWER_BLOCK = "D";
 }
