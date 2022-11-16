e9396a2195b3133865713fcc94ccecd214ec0978
June <68291395+june-777@users.noreply.github.com>
2022-11-17 01:32:21 +0900

    feat(InputView): 플레이어 입력 상황별 안내문구 출력 기능 추가

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -12,6 +12,7 @@ public class InputView {
 	 * 다리의 길이를 입력받는다.
 	 */
 	public int readBridgeSize() {
+		System.out.println(MESSAGE_READ_BRIDGE_SIZE);
 		String bridgeSize = readLine();
 		validateBridgeSizeNumeric(bridgeSize);
 		validateBridgeSizeRange(bridgeSize);
@@ -36,6 +37,7 @@ public class InputView {
 	 * 사용자가 이동할 칸을 입력받는다.
 	 */
 	public String readMoving() {
+		System.out.println(MESSAGE_READ_MOVING);
 		String moving = readLine();
 		validateMoving(moving);
 		return moving;
@@ -52,6 +54,7 @@ public class InputView {
 	 * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
 	 */
 	public String readGameCommand() {
+		System.out.println(MESSAGE_READ_GAME_COMMAND);
 		String gameCommand = readLine();
 		validateGameCommand(gameCommand);
 		return gameCommand;
