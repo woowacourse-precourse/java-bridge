022d5494517b5b32ef2c169ccae8e6e10b9322b6
jinny-l <jinny.l.dev@gmail.com>
2022-11-16 17:18:06 +0900

    feat(BridgeMaker): 다리 길이 숫자만큼 다리를 생성하는 기능 구현

--- src/main/java/bridge/BridgeMaker.java
+++ src/main/java/bridge/BridgeMaker.java
@@ -1,5 +1,6 @@
 package bridge;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -7,10 +8,23 @@ import java.util.List;
  */
 public class BridgeMaker {
 
+    private static final String UP_BRIDGE_CHARACTER = "U";
+    private static final int UP_BRIDGE_NUMBER = 1;
+    private static final String DOWN_BRIDGE_CHARACTER = "D";
+    private static final int DOWN_BRIDGE_NUMBER = 0;
+
     private final BridgeNumberGenerator bridgeNumberGenerator;
 
     public BridgeMaker(BridgeNumberGenerator bridgeNumberGenerator) {
         this.bridgeNumberGenerator = bridgeNumberGenerator;
+    }
+
+    public static String getUpBridgeCharacter() {
+        return UP_BRIDGE_CHARACTER;
+    }
+
+    public static String getDownBridgeCharacter() {
+        return DOWN_BRIDGE_CHARACTER;
     }
 
     /**
@@ -18,6 +32,22 @@ public class BridgeMaker {
      * @return 입력받은 길이에 해당하는 다리 모양. 위 칸이면 "U", 아래 칸이면 "D"로 표현해야 한다.
      */
     public List<String> makeBridge(int size) {
-        return null;
+        List<Integer> bridgeNumbers = new ArrayList<>();
+        List<String> bridgeCharacters = new ArrayList<>();
+
+        for (int i = 0; i < size; i++) {
+            int bridgeNumber = bridgeNumberGenerator.generate();
+            bridgeNumbers.add(bridgeNumber);
+        }
+
+        for (Integer bridgeNumber : bridgeNumbers) {
+            if (bridgeNumber == UP_BRIDGE_NUMBER) {
+                bridgeCharacters.add(UP_BRIDGE_CHARACTER);
+            }
+            if (bridgeNumber == DOWN_BRIDGE_NUMBER) {
+                bridgeCharacters.add(DOWN_BRIDGE_CHARACTER);
+            }
+        }
+        return bridgeCharacters;
     }
 }
