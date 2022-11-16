dec5dc302ec66110142478e3d87660b672128da8
WhalesBob <ndw8200@naver.com>
2022-11-16 20:47:00 +0900

    feat(inputValidation) : add features about validating bridgeLength

--- src/main/java/bridge/UI/InputValidation.java
+++ src/main/java/bridge/UI/InputValidation.java
@@ -4,6 +4,22 @@ public class InputValidation {
 
     OutputView outputView = new OutputView();
 
+    public void validateBridgeLength(int bridgeLength){
+        if(!(3 <= bridgeLength && bridgeLength <= 20)){
+            String errorMessage = "[ERROR] 다리의 길이는 3 이상 20 이하여야 합니다.";
+            errorProcess(errorMessage);
+        }
+    }
+
+    public void validateBridgeLengthInputIsNumber(String input){
+        try{
+            Integer.parseInt(input);
+        }catch(NumberFormatException ex){
+            String errorMessage = "[ERROR] 다리 길이에 대한 입력은 숫자여야 합니다.";
+            errorProcess(errorMessage);
+        }
+    }
+
     private void errorProcess(String errorMessage){
         outputView.printStatement(errorMessage);
         throw new IllegalArgumentException();
