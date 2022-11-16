37d36cbe0b4a6aed6d091f135406bf5e6922c9ea
K-jun98 <alap_u@naver.com>
2022-11-17 00:51:07 +0900

    feat - 다리의 길이를 입력받는다.

--- src/main/java/bridge/BridgeMaker.java
+++ src/main/java/bridge/BridgeMaker.java
@@ -7,6 +7,8 @@ import java.util.List;
  */
 public class BridgeMaker {
 
+    private static final int MIN_BRIDGE_LENGTH = 3;
+    private static final int MAX_BRIDGE_LENGTH = 20;
     private final BridgeNumberGenerator bridgeNumberGenerator;
 
     public BridgeMaker(BridgeNumberGenerator bridgeNumberGenerator) {
@@ -18,6 +20,13 @@ public class BridgeMaker {
      * @return 입력받은 길이에 해당하는 다리 모양. 위 칸이면 "U", 아래 칸이면 "D"로 표현해야 한다.
      */
     public List<String> makeBridge(int size) {
+        validateLength(size);
         return null;
+    }
+
+    private void validateLength(int length) {
+        if (length < MIN_BRIDGE_LENGTH || MAX_BRIDGE_LENGTH < length) {
+            throw new IllegalArgumentException("다리의 길이는 3 이상 20 이하의 값만 입력 가능합니다.");
+        }
     }
 }
