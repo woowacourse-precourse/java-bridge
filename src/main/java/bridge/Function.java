a8c7d78c12b4683ba602de764c67d3b193cbf1b5
HyunJoong Kim <80106238+hjk0761@users.noreply.github.com>
2022-11-17 02:27:09 +0900

    feat: add methods for validationAdd methods for checking user input is valid

---
+++ src/main/java/bridge/Function.java
@@ -0,0 +1,48 @@
+package bridge;
+
+import bridge.view.OutputView;
+
+public class Function {
+
+    private static final int MIN_LENGTH = 3;
+    private static final int MAX_LENGTH = 20;
+
+    public static boolean validBridgeSize(String BridgeSizes) {
+        try {
+            int BridgeSize = Integer.parseInt(BridgeSizes);
+            if (BridgeSize < MIN_LENGTH || BridgeSize > MAX_LENGTH) {
+                throw new IllegalArgumentException("올바른 다리 길이를 입력해주세요.");
+            }
+            return true;
+        } catch (NumberFormatException e) {
+            OutputView.printError("다리 길이로 숫자를 입력해주세요.");
+        } catch (IllegalArgumentException e) {
+            OutputView.printError(e.getMessage());
+        }
+        return false;
+    }
+
+    public static boolean validMoving(String Moving) {
+        try {
+            if (!Moving.equals("U") && !Moving.equals("D")) {
+                throw new IllegalArgumentException("이동은 U 혹은 D로 입력해주세요.");
+            }
+            return true;
+        } catch (IllegalArgumentException e) {
+            OutputView.printError(e.getMessage());
+        }
+        return false;
+    }
+
+    public static boolean validGameCommand(String GameCommand) {
+        try {
+            if (!GameCommand.equals("R") && !GameCommand.equals("Q")) {
+                throw new IllegalArgumentException("재시도 여부는 R 또는 Q로 입력해주세요.");
+            }
+            return true;
+        } catch (IllegalArgumentException e) {
+            OutputView.printError(e.getMessage());
+        }
+        return false;
+    }
+}
