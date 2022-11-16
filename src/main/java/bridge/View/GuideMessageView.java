382d9e4b4389c34733d14496e67378c50e7eb9e5
eunbii0213 <eunbii0213@naver.com>
2022-11-17 01:07:43 +0900

    feat: 게임 실패 안내 메시지 설정

--- src/main/java/bridge/View/GuideMessageView.java
+++ src/main/java/bridge/View/GuideMessageView.java
@@ -6,7 +6,8 @@ public enum GuideMessageView {
 	SELECT_MOVE_GUIDE_MESSAGE("이동할 칸을 선택해주세요. (위: U, 아래: D)"),
 	RE_OR_END_GUIDE_MESSAGE("게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)"),
 	GAME_RESULT_GUIDE_MESSAGE("최종 게임 결과"),
-	GAME_SUCCESS_GUIDE_MESSAGE("게임 성공 여부: 성공");
+	GAME_SUCCESS_GUIDE_MESSAGE("게임 성공 여부: 성공"),
+	GAME_FAIL_GUIDE_MESSAGE("게임 성공 여부: 실패");
 
 	private String message;
 	GuideMessageView(String message){
