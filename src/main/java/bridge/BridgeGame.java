17bb1cad04075a6b5ba9434b0e7750c0857440e9
jinny-l <jinny.l.dev@gmail.com>
2022-11-16 23:09:40 +0900

    feat(BridgeGame): 재시작 여부를 확인하는 기능 구현

--- src/main/java/bridge/BridgeGame.java
+++ src/main/java/bridge/BridgeGame.java
@@ -1,9 +1,32 @@
 package bridge;
 
+import java.util.List;
+
 /**
  * 다리 건너기 게임을 관리하는 클래스
  */
 public class BridgeGame {
+
+    private static final String UP_BRIDGE_CHARACTER = "U";
+    private static final String DOWN_BRIDGE_CHARACTER = "D";
+    private static final String RETRY_CHARACTER = "R";
+    private static final String QUIT_CHARACTER = "Q";
+    private static final String WIN = "성공";
+    private static final String LOSE = "실패";
+
+    private final List<String> bridge;
+
+    public BridgeGame(List<String> bridge) {
+        this.bridge = bridge;
+    }
+
+    public static String getRetryCharacter() {
+        return RETRY_CHARACTER;
+    }
+
+    public static String getQuitCharacter() {
+        return QUIT_CHARACTER;
+    }
 
     /**
      * 사용자가 칸을 이동할 때 사용하는 메서드
@@ -18,6 +41,15 @@ public class BridgeGame {
      * <p>
      * 재시작을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void retry() {
+    public boolean retry(String input) {
+        validateRetryOrQuit(input);
+        return input.equals(RETRY_CHARACTER);
     }
+
+    private void validateRetryOrQuit(String input) {
+        if (!input.equals(RETRY_CHARACTER) && !input.equals(QUIT_CHARACTER)) {
+            throw new IllegalArgumentException();
+        }
+    }
+
 }
