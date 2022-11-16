5b2ee7e640839819fed74a73af1e33bc23b9ad11
이한솔 <hansol8701@naver.com>
2022-11-17 02:16:41 +0900

    docs: Comparator 기능 추가

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -1,9 +1,18 @@
 package bridge;
 
+import java.util.List;
+
 public class Application {
 
     public static void main(String[] args) {
         // TODO: 프로그램 구현
         System.out.println("다리 건너기 게임을 시작합니다.");
+        InputView inputView = new InputView();
+        OutputView outputView = new OutputView();
+        BridgeNumberGenerator bridgeNumberGenerator = new BridgeRandomNumberGenerator();
+        BridgeMaker bridgeMaker = new BridgeMaker(bridgeNumberGenerator);
+        int bridgeSize = inputView.readBridgeSize();
+        List<String> bridge = bridgeMaker.makeBridge(bridgeSize);
+        inputView.readMoving();
     }
 }
---
+++ src/main/java/bridge/Comparator.java
@@ -0,0 +1,9 @@
+package bridge;
+
+import java.util.List;
+
+public class Comparator {
+    public boolean compare(List<String> bridge, int round, String userInput) {
+        return false;
+    }
+}
