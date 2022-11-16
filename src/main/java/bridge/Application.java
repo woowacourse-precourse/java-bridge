35525117b77019f01d8ae80d9dbab56702a52e03
ymkim97 <rladudaud97@naver.com>
2022-11-16 23:25:08 +0900

    feat: generate random bridge

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -5,6 +5,7 @@ public class Application {
     public static void main(String[] args) {
         try {
             MainGame mainGame = new MainGame();
+            mainGame.playRound();
         } catch (IllegalArgumentException e) {
             System.out.println(e.getMessage());
         }
--- src/main/java/bridge/MainGame.java
+++ src/main/java/bridge/MainGame.java
@@ -1,17 +1,23 @@
 package bridge;
 
+import java.util.List;
+
 public class MainGame {
     private final OutputView outputView = new OutputView();
     private final InputView inputView = new InputView();
     private final BridgeGame bridgeGame = new BridgeGame();
+    private final List<String> bridge;
+    private final BridgeRandomNumberGenerator bridgeRandomNumberGenerator;
     private boolean endGame;
 
     public MainGame() {
+        bridgeRandomNumberGenerator = new BridgeRandomNumberGenerator();
+        BridgeMaker bridgeMaker = new BridgeMaker(bridgeRandomNumberGenerator);
+
         endGame = false;
         outputView.printStart();
         outputView.printLengthInput();
-        inputView.readBridgeSize();
-        playRound();
+        bridge = bridgeMaker.makeBridge(inputView.readBridgeSize());
     }
 
     public void playRound() {
