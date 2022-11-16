ac268aa8d9dbc3d8234dade0d7e89a6ae5984c11
hongil <sonhl0723@naver.com>
2022-11-17 01:11:45 +0900

    refactor (GameData): 게임 데이터 bridge 패키지로 리팩토링

---
+++ src/main/java/bridge/GameData.java
@@ -0,0 +1,17 @@
+package bridge;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class GameData {
+    private int totalTry = 0;
+    private String gameResult;
+    private List<String> topStatus;
+    private List<String> bottomStatus;
+
+    public void clearStatus() {
+        this.topStatus = Arrays.asList("[", "]");
+        this.bottomStatus = Arrays.asList("[", "]");
+    }
+
+}
--- src/main/java/data/GameData.java
+++
@@ -1,8 +0,0 @@
-package data;
-
-public class GameData {
-    private int totalTry = 0;
-    private String gameResult;
-    private String topStatus;
-    private String bottomStatus;
-}
