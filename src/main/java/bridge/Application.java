098a98fc50ba60a29aa524e5c7f633a30f665e88
jh868 <2weeks9947@naver.com>
2022-11-16 23:55:21 +0900

    feat: 다리 생성 구현

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -2,9 +2,9 @@ package bridge;
 
 public class Application {
     static InputView inputView = new InputView();
-
+    static BridgeMaker bridgeMaker = new BridgeMaker(new BridgeRandomNumberGenerator());
     public static void main(String[] args) {
         // TODO: 프로그램 구현
-        inputView.readBridgeSize();
+        bridgeMaker.makeBridge(inputView.readBridgeSize());
     }
 }
