4ab723315461053d48fa6eb0754c464b1446cf9c
amaran-th <songsy405@gmail.com>
2022-11-17 02:08:49 +0900

    refactor : OutputView의 코드를 InputView로 이동

--- src/main/java/bridge/InputView.java
+++ src/main/java/bridge/InputView.java
@@ -5,24 +5,28 @@ package bridge;
  */
 public class InputView {
 
-    /**
-     * 다리의 길이를 입력받는다.
-     */
-    public int readBridgeSize() {
-        return 0;
-    }
+  /**
+   * 다리의 길이를 입력받는다.
+   */
+  public int readBridgeSize() {
+    System.out.println("다리의 길이를 입력해주세요.");
+    return 0;
+  }
 
-    /**
-     * 사용자가 이동할 칸을 입력받는다.
-     */
-    public String readMoving() {
-        return null;
-    }
+  /**
+   * 사용자가 이동할 칸을 입력받는다.
+   */
+  public String readMoving() {
+    System.out.println("이동할 칸을 선택해주세요. (위: U, 아래: D)");
+    return null;
+  }
 
-    /**
-     * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
-     */
-    public String readGameCommand() {
-        return null;
-    }
+  /**
+   * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
+   */
+  public String readGameCommand() {
+    System.out.println("게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)");
+
+    return null;
+  }
 }
--- src/main/java/bridge/OutputView.java
+++ src/main/java/bridge/OutputView.java
@@ -7,12 +7,8 @@ public class OutputView {
 
   public void printGameStart() {
     System.out.println("다리 건너기 게임을 시작합니다.");
-    System.out.println("다리의 길이를 입력해주세요.");
   }
 
-  public void printWhereMove() {
-    System.out.println("이동할 칸을 선택해주세요. (위: U, 아래: D)");
-  }
 
   /**
    * 현재까지 이동한 다리의 상태를 정해진 형식에 맞춰 출력한다.
@@ -20,10 +16,6 @@ public class OutputView {
    * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
    */
   public void printMap() {
-  }
-
-  public void printWhetherRetry() {
-    System.out.println("게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)");
   }
 
   /**
