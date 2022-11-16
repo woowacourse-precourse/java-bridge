c63c8e56ed920d1cc113a5635349ee5a7a2b294c
HyeonjungYun <guswnd9971@gmail.com>
2022-11-16 19:36:28 +0900

    "feat(Application): 재시작을 묻는 메서드 retry() 추가"

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -5,4 +5,12 @@ public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
     }
+
+    public boolean retry () {
+        InputView input = new InputView();
+        if (input.readGameCommand().equals("R")) {
+            return true;
+        }
+        return false;
+    }
 }
