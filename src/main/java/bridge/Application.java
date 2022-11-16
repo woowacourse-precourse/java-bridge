e2d24c496afdc058a5402b98a896898de36b29f6
seokjin8678 <rnrn8678@naver.com>
2022-11-16 15:41:50 +0900

    feat: BridgeGame 내부 필드로 view, service 추가 및 의존관계 설정

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -1,8 +1,22 @@
 package bridge;
 
+import bridge.controller.BridgeGame;
+import bridge.service.BridgeMaker;
+import bridge.util.BridgeRandomNumberGenerator;
+import bridge.view.InputView;
+import bridge.view.OutputView;
+
 public class Application {
 
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        BridgeRandomNumberGenerator bridgeRandomNumberGenerator = new BridgeRandomNumberGenerator();
+        BridgeGame bridgeGame = BridgeGame.create(new InputView(), new OutputView(),
+                new BridgeMaker(bridgeRandomNumberGenerator));
+        try {
+            bridgeGame.start();
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+        }
     }
 }
