6753608dacc51788bc529db13afd29df439beb20
SEONMl <sm000117@naver.com>
2022-11-16 23:22:49 +0900

    fix: 요구 사항에 맞도록 타입 수정(char -> String)

--- src/main/java/bridge/domain/Command.java
+++ src/main/java/bridge/domain/Command.java
@@ -1,24 +1,33 @@
 package bridge.domain;
 
 public enum Command {
-    UP('U', "위"),
-    DOWN('D', "아래"),
-    RETRY('R', "재시도"),
-    QUIT('Q', "종료");
+    UP("U", "위", 1),
+    DOWN("D", "아래", 0),
+    RETRY("R", "재시도", -1),
+    QUIT("Q", "종료", -1);
 
-    private char abbreviation;
+    private String abbreviation;
     private String korExplanation;
+    private int numberCode;
 
-    Command(char abbreviation, String korExplanation) {
+    Command(String abbreviation, String korExplanation, int numberCode) {
         this.abbreviation = abbreviation;
         this.korExplanation = korExplanation;
+        this.numberCode = numberCode;
+    }
+
+    public static String getByCode(int code) {
+        if (code == 1) {
+            return Command.UP.getAbbreviation();
+        }
+        return Command.DOWN.getAbbreviation();
     }
 
     public String getKor() {
         return this.korExplanation;
     }
 
-    public char getAbbreviation() {
+    public String getAbbreviation() {
         return this.abbreviation;
     }
 }
