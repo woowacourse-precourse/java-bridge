2e15e949e6abc322cc8903970fafc149c085c606
ywj9811 <ywj9811@naver.com>
2022-11-17 05:11:45 +0900

    fix: OutputView 클래스
    get을 통한 출력으로 수정 및 다리 업데이트 오류 수정

--- src/main/java/bridge/view/OutputView.java
+++ src/main/java/bridge/view/OutputView.java
@@ -7,48 +7,52 @@ public class OutputView {
     private static String downBridge = "[]";
 
     public void start() {
-        System.out.println(OutputString.START);
-        System.out.println(OutputString.INPUT);
+        System.out.println(OutputString.START.getPrint());
+        System.out.println(OutputString.INPUT.getPrint());
     }
 
     public void choice() {
-        System.out.println(OutputString.MOVE);
+        System.out.println(OutputString.MOVE.getPrint());
     }
 
     public void printMap(String input, boolean tf, int idx) {
         if (tf == true) {
             printTrue(input);
-            if (idx == 0)
-                input.replace("|", "");
+            if (idx == 0) {
+                upBridge = upBridge.replace("|", "");
+                downBridge = downBridge.replace("|", "");
+            }
             return;
         }
         printFalse(input);
-        if (idx == 0)
-            input.replace("|", "");
+        if (idx == 0) {
+            upBridge = upBridge.replace("|", "");
+            downBridge = downBridge.replace("|", "");
+        }
     }
 
     public void printTrue(String input) {
         if (input.equals("U")) {
-            upBridge.replace("]", "| O ]");
-            downBridge.replace("]", "|   ]");
+            upBridge = upBridge.replace("]", "| O ]");
+            downBridge = downBridge.replace("]", "|   ]");
             return;
         }
         if (input.equals("D")) {
-            upBridge.replace("]", "|   ]");
-            downBridge.replace("]", "| O ]");
+            upBridge = upBridge.replace("]", "|   ]");
+            downBridge = downBridge.replace("]", "| O ]");
             return;
         }
     }
 
     public void printFalse(String input) {
         if (input.equals("U")) {
-            upBridge.replace("]", "| O ]");
-            downBridge.replace("]", "|   ]");
+            upBridge = upBridge.replace("]", "| O ]");
+            downBridge = downBridge.replace("]", "|   ]");
             return;
         }
         if (input.equals("D")) {
-            upBridge.replace("]", "|   ]");
-            downBridge.replace("]", "| X ]");
+            upBridge = upBridge.replace("]", "|   ]");
+            downBridge = downBridge.replace("]", "| X ]");
             return;
         }
     }
@@ -58,21 +62,30 @@ public class OutputView {
         System.out.println(downBridge);
     }
 
+    public void lastPrint() {
+        System.out.println();
+        System.out.println(OutputString.LAST.getPrint());
+        System.out.println(upBridge);
+        System.out.println(downBridge);
+        System.out.println();
+    }
+
     public void printResult(boolean tf, int count) {
+        lastPrint();
         if (!tf) {
-            System.out.print(OutputString.SUCCESS_OR_NOT);
-            System.out.println(OutputString.FAIL);
-            System.out.print(OutputString.TRY );
+            System.out.print(OutputString.RESULT.getPrint());
+            System.out.println(OutputString.FAIL.getPrint());
+            System.out.print(OutputString.TRY.getPrint());
             System.out.println(count);
             return;
         }
-        System.out.print(OutputString.SUCCESS_OR_NOT);
-        System.out.println(OutputString.SUCCESS);
-        System.out.print(OutputString.TRY );
+        System.out.print(OutputString.RESULT.getPrint());
+        System.out.println(OutputString.SUCCESS.getPrint());
+        System.out.print(OutputString.TRY.getPrint());
         System.out.println(count);
     }
 
     public void retry() {
-        System.out.println(OutputString.RETRY);
+        System.out.println(OutputString.RETRY.getPrint());
     }
 }
