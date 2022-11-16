b5915fa5083bf65cbbc229a100ab7185201f7ad3
junpakPark <112045553+junpakPark@users.noreply.github.com>
2022-11-16 22:49:01 +0900

    test(BridgeSizeTest): BridgeSize 객체 테스트 코드 작성
    BridgeSizeTest 목록
    - 3에서 20 사이 자연수 입력 시 해당 숫자를 getter 메서드로 반환 여부 확인
    - 정수 이외의 값 입력 시 에러 메시지 출력 여부 확인
    - 3에서 20 이외의 정수 입력시 에러 메시지 출력 여부 확인

---
+++ src/test/java/bridge/BridgeSizeTest.java
@@ -0,0 +1,99 @@
+package bridge;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+
+class BridgeSizeTest {
+	@DisplayName("3 에서 20 사이 자연수를 입력하면 해당 숫자를 반환한다")
+	@Test
+	void case1() {
+		for (int i = 3; i <= 20; i++) {
+			String value = Integer.toString(i);
+			assertEquals(new BridgeSize(value).getSize(), i);
+		}
+	}
+
+	@DisplayName("정수 외의 것을 입력하면 에러 메시지를 출력한다")
+	@Nested
+	class FormatTest {
+		@Test
+		void case1() {
+			Throwable exception = assertThrows(IllegalArgumentException.class, () -> {
+				BridgeSize bridgeSize = new BridgeSize("a");
+			});
+			assertEquals(Error.FORMAT.getMessage(), exception.getMessage());
+		}
+
+		@Test
+		void case2() {
+			Throwable exception = assertThrows(IllegalArgumentException.class, () -> {
+				BridgeSize bridgeSize = new BridgeSize("Z");
+			});
+			assertEquals(Error.FORMAT.getMessage(), exception.getMessage());
+		}
+
+		@Test
+		void case3() {
+			Throwable exception = assertThrows(IllegalArgumentException.class, () -> {
+				BridgeSize bridgeSize = new BridgeSize("");
+			});
+			assertEquals(Error.FORMAT.getMessage(), exception.getMessage());
+		}
+
+		@Test
+		void case4() {
+			Throwable exception = assertThrows(IllegalArgumentException.class, () -> {
+				BridgeSize bridgeSize = new BridgeSize(" ");
+			});
+			assertEquals(Error.FORMAT.getMessage(), exception.getMessage());
+		}
+
+		@Test
+		void case5() {
+			Throwable exception = assertThrows(IllegalArgumentException.class, () -> {
+				BridgeSize bridgeSize = new BridgeSize("1.1");
+			});
+			assertEquals(Error.FORMAT.getMessage(), exception.getMessage());
+		}
+	}
+
+	@DisplayName("3 에서 20 사이의 자연수 외에 정수를 입력하면 에러 메시지를 출력한다")
+	@Nested
+	class RangeTest {
+		@Test
+		void case1() {
+			Throwable exception = assertThrows(IllegalArgumentException.class, () -> {
+				BridgeSize bridgeSize = new BridgeSize("1");
+			});
+			assertEquals(Error.RANGE.getMessage(), exception.getMessage());
+		}
+
+		@Test
+		void case2() {
+			Throwable exception = assertThrows(IllegalArgumentException.class, () -> {
+				BridgeSize bridgeSize = new BridgeSize("2");
+			});
+			assertEquals(Error.RANGE.getMessage(), exception.getMessage());
+		}
+
+		@Test
+		void case3() {
+			Throwable exception = assertThrows(IllegalArgumentException.class, () -> {
+				BridgeSize bridgeSize = new BridgeSize("21");
+			});
+			assertEquals(Error.RANGE.getMessage(), exception.getMessage());
+		}
+
+		@Test
+		void case4() {
+			Throwable exception = assertThrows(IllegalArgumentException.class, () -> {
+				BridgeSize bridgeSize = new BridgeSize("10000");
+			});
+			assertEquals(Error.RANGE.getMessage(), exception.getMessage());
+		}
+	}
+
+}
