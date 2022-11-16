7465bcc3e4ca8fcc2451dcb45cfb49ec23205411
JunDonghyuk <ehdgur4814@naver.com>
2022-11-16 20:03:47 +0900

    feat(ErrorMessage): 에러 메시지 포맷 구현

--- src/main/java/bridge/ErrorMessage.java
+++ src/main/java/bridge/ErrorMessage.java
@@ -1,9 +1,14 @@
 package bridge;
 
+import bridge.Input.InputValidation;
+
 public class ErrorMessage {
     public static final String BASE_ERROR_MESSAGE = "[ERROR] ";
-    public static final String BRIDGE_SIZE_ERROR_MESSAGE = BASE_ERROR_MESSAGE + "다리 길이는 3부터 20 사이의 숫자여야 합니다.";
-    public static final String MOVE_COMMAND_ERROR_MESSAGE = BASE_ERROR_MESSAGE + "위: U, 아래: D를 입력해야 합니다.";
-    public static final String RETRY_COMMAND_ERROR_MESSAGE = BASE_ERROR_MESSAGE + "재시도: R, 종료: Q를 입력해야 합니다.";
-
+    public static final String BRIDGE_SIZE_ERROR_MESSAGE = BASE_ERROR_MESSAGE
+            + "다리 길이는 " + InputValidation.MIN_BRIDGE_SIZE + "부터 "
+            + InputValidation.MAX_BRIDGE_SIZE + " 사이의 숫자여야 합니다.";
+    public static final String MOVE_COMMAND_ERROR_MESSAGE = BASE_ERROR_MESSAGE
+            + "위: " + CommandConstant.UP_COMMAND + ", 아래: " + CommandConstant.DOWN_COMMAND + "를 입력해야 합니다.";
+    public static final String RETRY_COMMAND_ERROR_MESSAGE = BASE_ERROR_MESSAGE
+            + "재시도: " + CommandConstant.RETRY_COMMAND + ", 종료: " + CommandConstant.QUIT_COMMAND + "를 입력해야 합니다.";
 }
