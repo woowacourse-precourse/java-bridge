a39bff9600a5b7c65ba16de5a4da7c0d2282c2f7
youhobin <ghdb132@naver.com>
2022-11-16 19:13:19 +0900

    refactor : 다리 생성 기능 리팩토링

--- src/main/java/bridge/BridgeMaker.java
+++ src/main/java/bridge/BridgeMaker.java
@@ -2,12 +2,19 @@ package bridge;
 
 import java.util.ArrayList;
 import java.util.List;
-/** 인스턴스 변수 변경 안됨. 메서드의 인자와 이름, 반환타입 변경 안됨.
+
 /**
+ * 인스턴스 변수 변경 안됨. 메서드의 인자와 이름, 반환타입 변경 안됨.
+ * /**
  * 다리의 길이를 입력 받아서 다리를 생성해주는 역할을 한다.
  */
 public class BridgeMaker {
 
+    public static final int BRIDGE_DOWN_RANDOM_NUMBER = 0;
+    public static final int BRIDGE_UP_RANDOM_NUMBER = 1;
+    public static final String BRIDGE_DOWN_MARK = "D";
+    public static final String BRIDGE_UP_MARK = "U";
+    public static final int INIT_SIZE_INDEX = 0;
     private final BridgeNumberGenerator bridgeNumberGenerator;
 
     public BridgeMaker(BridgeNumberGenerator bridgeNumberGenerator) {
@@ -20,15 +27,19 @@ public class BridgeMaker {
      */
     public List<String> makeBridge(int size) {
         List<String> bridge = new ArrayList<>();
-        for (int i = 0; i < size; i++) {
+        for (int sizeIndex = INIT_SIZE_INDEX; sizeIndex < size; sizeIndex++) {
             int number = bridgeNumberGenerator.generate();
-            if (number == 0) {
-                bridge.add("D");
-            }
-            if (number == 1) {
-                bridge.add("U");
-            }
+            chageNumberToMark(bridge, number);
         }
         return bridge;
+    }
+
+    private static void chageNumberToMark(List<String> bridge, int number) {
+        if (number == BRIDGE_DOWN_RANDOM_NUMBER) {
+            bridge.add(BRIDGE_DOWN_MARK);
+        }
+        if (number == BRIDGE_UP_RANDOM_NUMBER) {
+            bridge.add(BRIDGE_UP_MARK);
+        }
     }
 }
