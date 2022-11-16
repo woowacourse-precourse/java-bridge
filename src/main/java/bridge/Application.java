9b8946c9c63e534f99c82e89383c91d4585477ee
CYC-ChoiYoungChang <choiyc1446@gmail.com>
2022-11-16 17:52:51 +0900

    feat: 에러 출력을 위한 try catch 기능

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -1,8 +1,18 @@
 package bridge;
 
+import bridge.Controller.BridgeGame;
+import bridge.view.OutputView;
+
 public class Application {
 
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        try {
+            BridgeGame bridgeGame = new BridgeGame();
+            bridgeGame.run();
+        } catch (IllegalArgumentException exception) {
+            OutputView outputView = new OutputView();
+            outputView.printError(exception.getMessage());
+        }
     }
 }
--- src/main/java/bridge/Controller/BridgeGame.java
+++ src/main/java/bridge/Controller/BridgeGame.java
@@ -1,9 +1,13 @@
-package bridge;
+package bridge.Controller;
 
 /**
  * 다리 건너기 게임을 관리하는 클래스
  */
 public class BridgeGame {
+
+    public void run(){
+
+    }
 
     /**
      * 사용자가 칸을 이동할 때 사용하는 메서드
