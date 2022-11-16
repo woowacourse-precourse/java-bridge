1d6d436d065cd3d69857be3ce1228279a83c26b0
junpakPark <112045553+junpakPark@users.noreply.github.com>
2022-11-16 23:18:02 +0900

    feat(InputView): InputView 객체 생성
    InputView 객체 주요 기능:
    - 입력 메시지 출력 후 사용자로 부터 입력 받는다

--- src/main/java/bridge/view/InputView.java
+++ src/main/java/bridge/view/InputView.java
@@ -1,28 +1,37 @@
 package bridge.view;
 
+import bridge.validator.BridgeSize;
+import bridge.validator.Command;
+import bridge.validator.Location;
+import camp.nextstep.edu.missionutils.Console;
+
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
  */
 public class InputView {
 
-    /**
-     * 다리의 길이를 입력받는다.
-     */
-    public int readBridgeSize() {
-        return 0;
-    }
+	/**
+	 * 다리의 길이를 입력받는다.
+	 */
+	public int readBridgeSize() {
+		System.out.println("다리의 길이를 입력해주세요.");
+		return new BridgeSize(Console.readLine()).getSize();
+	}
 
-    /**
-     * 사용자가 이동할 칸을 입력받는다.
-     */
-    public String readMoving() {
-        return null;
-    }
+	/**
+	 * 사용자가 이동할 칸을 입력받는다.
+	 */
+	public int readMoving() {
+		System.out.println("이동할 칸을 선택해주세요. (위: U, 아래: D)");
+		return Location.convertToInt(Console.readLine());
+	}
 
-    /**
-     * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
-     */
-    public String readGameCommand() {
-        return null;
-    }
+	/**
+	 * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
+	 */
+	public Boolean readGameCommand() {
+		System.out.println("게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)");
+		return Command.isReplay(Console.readLine());
+	}
+
 }
