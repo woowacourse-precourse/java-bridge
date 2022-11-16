7264c36003de58795edcc66dc4333c54e3bbf99a
송민진 <m_6595@naver.com>
2022-11-17 07:22:48 +0900

    feat(bridge): validate type of the input meaning bridge size
    구현 : 입력받은 다리 길이가 숫자형이 아니면 오류 발생 후 재입력 진행
    	- 에러 메시지 : [ERROR] 다리 길이는 숫자로만 입력하여 주십시오.

--- src/main/java/bridge/util/ErrorCode.java
+++ src/main/java/bridge/util/ErrorCode.java
@@ -2,7 +2,8 @@ package bridge.util;
 
 public enum ErrorCode {
     // 다리 생성 관련 오류
-    INVALID_BRIDGE_SIZE_RANGE("[ERROR] 다리 길이는 3부터 20 사이의 숫자여야 합니다.");
+    INVALID_BRIDGE_SIZE_RANGE("[ERROR] 다리 길이는 3부터 20 사이의 숫자여야 합니다."),
+    INVALID_BRIDGE_SIZE_TYPE("[ERROR] 다리 길이는 숫자로만 입력하여 주십시오.");
 
     private final String message;
 
