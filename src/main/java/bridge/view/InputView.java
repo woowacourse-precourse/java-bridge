6f8bbebe2a0490a49239b0099fc46e77ad39aafa
Hyun-Seo Oh <ohseo8813@gmail.com>
2022-11-16 18:23:49 +0900

    <feat>(InputView.java): 이동할 칸을 입력하는 기능

--- src/main/java/bridge/view/InputView.java
+++ src/main/java/bridge/view/InputView.java
@@ -6,13 +6,20 @@ import camp.nextstep.edu.missionutils.Console;
  * 사용자로부터 입력을 받는 역할을 한다.
  */
 public class InputView {
-    /**
-     * 다리의 길이를 입력받는다.
-     */
+
     public int readBridgeSize() {
         System.out.println("다리 건너기 게임을 시작합니다.");
         System.out.println("다리의 길이를 입력해주세요.");
         return convertInputToInt(Console.readLine());
+    }
+
+    public String readMoving() {
+        System.out.println("이동할 칸을 선택해주세요. (위: U, 아래: D)");
+        return Console.readLine();
+    }
+
+    public String readGameCommand() {
+        return null;
     }
 
     private int convertInputToInt(String input) {
@@ -24,19 +31,5 @@ public class InputView {
 
     private boolean isNotNumber(String input) {
         return !input.chars().allMatch(Character::isDigit);
-    }
-
-    /**
-     * 사용자가 이동할 칸을 입력받는다.
-     */
-    public String readMoving() {
-        return null;
-    }
-
-    /**
-     * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
-     */
-    public String readGameCommand() {
-        return null;
     }
 }
