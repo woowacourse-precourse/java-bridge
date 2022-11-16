96382901c1d6f25073f6ff74fcbb4e971bd3e55e
SeolYoungKim <nasur4da@gmail.com>
2022-11-16 19:21:12 +0900

    feat(OutputView): 출력 로직 일부 구현
    출력 로직의 일부를 구현하였습니다. 추가 로직은 비즈니스 로직 구현 후 구현 예정입니다.

--- src/main/java/bridge/view/OutputView.java
+++ src/main/java/bridge/view/OutputView.java
@@ -8,12 +8,26 @@ package bridge.view;
  */
 public class OutputView {
 
+    public void printStartMessage() {
+        System.out.println("다리 건너기 게임을 시작합니다.");
+    }
+
+    public void printMovingMessage() {
+        System.out.println("이동할 칸을 선택해주세요. (위: U, 아래: D)");
+    }
+
     /**
      * 현재까지 이동한 다리의 상태를 정해진 형식에 맞춰 출력한다.
      * <p>
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
     public void printMap() {
+        // TODO 현재까지 유저가 건넌 다리의 상태를 출력해야 한다.
+        // TODO 이는 Bridge 관련 로직 구현 후에 구현하자.
+    }
+
+    public void printGameCommandMessage() {
+        System.out.println("게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)");
     }
 
     /**
@@ -22,5 +36,10 @@ public class OutputView {
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
     public void printResult() {
+        System.out.println("최종 게임 결과");
+        // TODO 최종 게임 결과를 출력해야 한다.
+
+        System.out.println("게임 성공 여부: ");
+        System.out.println("총 시도한 횟수: ");
     }
 }
