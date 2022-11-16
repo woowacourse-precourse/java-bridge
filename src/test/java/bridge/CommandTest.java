1d61f3b15940a05ef870102a9a580ec58f008d42
junpakPark <112045553+junpakPark@users.noreply.github.com>
2022-11-16 21:43:18 +0900

    feat(CommandTest): Command Enum 테스트 코드 작성
    CommadTest 목록:
    - 대소문자 관계 없이 R 혹은 Q를 입력하면 Boolean 값을 리턴한다
    - R 혹은 Q를 제외한 값을 입력하면 에러메시지를 출력한다.

---
+++ src/test/java/bridge/CommandTest.java
@@ -0,0 +1,110 @@
+package bridge;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+
+class CommandTest {
+	@DisplayName("R 혹은 Q를 입력하면 Boolean을 리턴한다.")
+	@Test
+	void case1() {
+		assertTrue(Command.isReplay("R"));
+		assertTrue(Command.isReplay("r"));
+		assertFalse(Command.isReplay("Q"));
+		assertFalse(Command.isReplay("q"));
+	}
+
+	@DisplayName("R 혹은 Q 외의 것을 입력하면 에러 메시지를 출력한다")
+	@Nested
+	class IsReplayTest {
+		@Test
+		void case1() {
+			Throwable exception = assertThrows(IllegalArgumentException.class, () -> {
+				Command.isReplay("a");
+			});
+			assertEquals(Error.COMMAND.getMessage(), exception.getMessage());
+		}
+
+		@Test
+		void case2() {
+			Throwable exception = assertThrows(IllegalArgumentException.class, () -> {
+				Command.isReplay("Z");
+			});
+			assertEquals(Error.COMMAND.getMessage(), exception.getMessage());
+		}
+
+		@Test
+		void case3() {
+			Throwable exception = assertThrows(IllegalArgumentException.class, () -> {
+				Command.isReplay("0");
+			});
+			assertEquals(Error.COMMAND.getMessage(), exception.getMessage());
+		}
+
+		@Test
+		void case4() {
+			Throwable exception = assertThrows(IllegalArgumentException.class, () -> {
+				Command.isReplay("-1");
+			});
+			assertEquals(Error.COMMAND.getMessage(), exception.getMessage());
+		}
+
+		@Test
+		void case5() {
+			Throwable exception = assertThrows(IllegalArgumentException.class, () -> {
+				Command.isReplay("100000");
+			});
+			assertEquals(Error.COMMAND.getMessage(), exception.getMessage());
+		}
+
+		@Test
+		void case6() {
+			Throwable exception = assertThrows(IllegalArgumentException.class, () -> {
+				Command.isReplay("");
+			});
+			assertEquals(Error.COMMAND.getMessage(), exception.getMessage());
+		}
+
+		@Test
+		void case7() {
+			Throwable exception = assertThrows(IllegalArgumentException.class, () -> {
+				Command.isReplay(" ");
+			});
+			assertEquals(Error.COMMAND.getMessage(), exception.getMessage());
+		}
+
+		@Test
+		void case8() {
+			Throwable exception = assertThrows(IllegalArgumentException.class, () -> {
+				Command.isReplay("U");
+			});
+			assertEquals(Error.COMMAND.getMessage(), exception.getMessage());
+		}
+
+		@Test
+		void case9() {
+			Throwable exception = assertThrows(IllegalArgumentException.class, () -> {
+				Command.isReplay("u");
+			});
+			assertEquals(Error.COMMAND.getMessage(), exception.getMessage());
+		}
+
+		@Test
+		void case10() {
+			Throwable exception = assertThrows(IllegalArgumentException.class, () -> {
+				Command.isReplay("D");
+			});
+			assertEquals(Error.COMMAND.getMessage(), exception.getMessage());
+		}
+
+		@Test
+		void case11() {
+			Throwable exception = assertThrows(IllegalArgumentException.class, () -> {
+				Command.isReplay("d");
+			});
+			assertEquals(Error.COMMAND.getMessage(), exception.getMessage());
+		}
+	}
+}
