387777f88a0e440c7c909ff7c8e8e43e4f4e5ccd
jiwon1217 <koagjw@gmail.com>
2022-11-17 00:54:04 +0900

    feat: 생성할 다리의 길이로는 숫자만 입력 가능하도록 예외처리

--- src/main/java/bridge/exception/InputException.java
+++ src/main/java/bridge/exception/InputException.java
@@ -1,4 +1,14 @@
 package bridge.exception;
 
 public class InputException {
+
+    private static final String INVALID_BRIDGE_SIZE_TYPE = "[ERROR] 다리의 길이는 숫자여야 합니다.";
+
+    public static void isValidBridgeSizeType(String bridgeSize) {
+        try {
+            Integer.parseInt(bridgeSize);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException(INVALID_BRIDGE_SIZE_TYPE);
+        }
+    }
 }
--- src/main/java/bridge/view/InputView.java
+++ src/main/java/bridge/view/InputView.java
@@ -2,6 +2,8 @@ package bridge.view;
 
 import camp.nextstep.edu.missionutils.Console;
 
+import static bridge.exception.InputException.*;
+
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
  */
@@ -11,6 +13,8 @@ public class InputView {
      * 다리의 길이를 입력받는다.
      */
     public int readBridgeSize() {
+        String bridgeSize = Console.readLine();
+        isValidBridgeSizeType(bridgeSize);
         return Integer.parseInt(Console.readLine());
     }
 
