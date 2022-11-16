02ece337ab122c847097e57c1be8f387705d94ac
yeonhee <ydol2@naver.com>
2022-11-16 17:53:03 +0900

    feat(OutputView) : 최종결과 출력 관련 구현

--- src/main/java/bridge/view/OutputView.java
+++ src/main/java/bridge/view/OutputView.java
@@ -5,7 +5,11 @@ package bridge.view;
  */
 public class OutputView {
 
+    private static final String LINE = System.lineSeparator();
     private static final String OUTPUT_START_MESSAGE = "다리 건너기 게임을 시작합니다.";
+    private static final String OUTPUT_RESULT_MESSAGE = LINE + "최종 게임 결과" ;
+    private static final String OUTPUT_RESULT_IS_SUCCESS = LINE + "게임 성공 여부: " ;
+    private static final String OUTPUT_RESULT_TRY_NUMBER = "총 시도한 횟수: " ;
 
     public void printStart() {
         System.out.println(OUTPUT_START_MESSAGE);
@@ -25,5 +29,8 @@ public class OutputView {
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
     public void printResult() {
+        System.out.println(OUTPUT_RESULT_MESSAGE);
+        System.out.println(OUTPUT_RESULT_IS_SUCCESS);
+        System.out.println(OUTPUT_RESULT_TRY_NUMBER);
     }
 }
