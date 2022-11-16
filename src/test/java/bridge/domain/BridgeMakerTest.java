664da48e7d3f5e0e425592dc828b047082729170
Damgom <wisewish1@gmail.com>
2022-11-17 01:40:34 +0900

    ADD : make bridge test code

---
+++ src/test/java/bridge/domain/BridgeMakerTest.java
@@ -0,0 +1,48 @@
+package bridge.domain;
+
+import bridge.util.BridgeNumberGenerator;
+import bridge.util.BridgeRandomNumberGenerator;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.assertj.core.util.Lists.newArrayList;
+import static org.junit.jupiter.api.Assertions.*;
+
+class BridgeMakerTest {
+
+    @DisplayName("입력된 숫자와 다리의 길이가 같아야 합니다.")
+    @Test
+    void makeBridge() {
+        BridgeNumberGenerator bridgeNumberGenerator = new BridgeRandomNumberGenerator();
+        BridgeMaker bridgeMaker = new BridgeMaker(bridgeNumberGenerator);
+        List<String> bridge = bridgeMaker.makeBridge(3);
+        assertThat(bridge).size().isEqualTo(3);
+    }
+
+    @DisplayName("1은 U로, 0은 D로 변환")
+    @Test
+    void makeBridgesMapping() {
+        BridgeNumberGenerator numberGenerator = new TestNumberGenerator(newArrayList(1, 0, 0, 1, 1));
+        BridgeMaker bridgeMaker = new BridgeMaker(numberGenerator);
+        List<String> bridge = bridgeMaker.makeBridge(5);
+        assertThat(bridge).containsExactly("U", "D", "D", "U", "U");
+    }
+
+    static class TestNumberGenerator implements BridgeNumberGenerator {
+
+        private final List<Integer> numbers;
+
+        TestNumberGenerator(List<Integer> numbers) {
+            this.numbers = numbers;
+        }
+
+        @Override
+        public int generate() {
+            return numbers.remove(0);
+        }
+    }
+}
