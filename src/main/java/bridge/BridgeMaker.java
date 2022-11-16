6eef2f946f5862d5e26b5a7f0226f0dc9d21b898
ghchoi <ghchoi0427@gmail.com>
2022-11-16 16:08:48 +0900

    feat: makeBridge 구현

--- src/main/java/bridge/BridgeMaker.java
+++ src/main/java/bridge/BridgeMaker.java
@@ -1,5 +1,6 @@
 package bridge;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -18,6 +19,21 @@ public class BridgeMaker {
      * @return 입력받은 길이에 해당하는 다리 모양. 위 칸이면 "U", 아래 칸이면 "D"로 표현해야 한다.
      */
     public List<String> makeBridge(int size) {
-        return null;
+        BridgeRandomNumberGenerator generator = new BridgeRandomNumberGenerator();
+        List<String> result = new ArrayList<>();
+        for (int i = 0; i < size; i++) {
+            mapBinaryIntToBridge(generator, result);
+        }
+        return result;
+    }
+
+    private void mapBinaryIntToBridge(BridgeRandomNumberGenerator generator, List<String> result) {
+        int generated = generator.generate();
+        if (generated == 0) {
+            result.add("D");
+        }
+        if (generated == 1) {
+            result.add("U");
+        }
     }
 }
