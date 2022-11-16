bd33f4a5a60acb225c3e2469c58ae0bd3da6b389
HyeonjungYun <guswnd9971@gmail.com>
2022-11-16 19:35:17 +0900

    "feat(BridgeGame): 메서드 move() 구현"

--- src/main/java/bridge/BridgeGame.java
+++ src/main/java/bridge/BridgeGame.java
@@ -5,12 +5,35 @@ package bridge;
  */
 public class BridgeGame {
 
+    private final List<String> randomBridge;
+    public Stack<String> passHistory;
+
     /**
      * 사용자가 칸을 이동할 때 사용하는 메서드
      * <p>
      * 이동을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void move() {
+    public BridgeGame (List<String> randomBridge) {
+        this.randomBridge = randomBridge;
+    }
+    public boolean move(String choiceUpAndDown) {
+
+        if (!canPass(choiceUpAndDown)) {
+            this.passHistory.pop();
+            return retry();
+        }
+
+        if (passHistory.size() == randomBridge.size()) {
+            return false;
+        }
+
+        return true;
+    }
+
+    public boolean canPass (String choiceUpAndDown) {
+        this.passHistory.push(choiceUpAndDown);
+
+        return randomBridge.get(passHistory.size()-1).equals(choiceUpAndDown);
     }
 
     /**
