ac2925ad16b56fb91196a5d1c0ebbd3823f6b72f
yeonhee <ydol2@naver.com>
2022-11-17 04:23:49 +0900

    chore(Application) : application에서 시작 실행하도록

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -7,7 +7,7 @@ public class Application {
     public static void main(String[] args) {
         BridgeGameController bridgeGameController = BridgeGameController.create();
         try{
-            bridgeGameController.run();
+            bridgeGameController.start();
         } catch (Exception e){
             System.out.println(ERROR + e.getMessage());
         }
