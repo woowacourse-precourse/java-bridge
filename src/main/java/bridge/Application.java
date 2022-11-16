03f983c2101234cd0bc27e0ad63583ae5b5c7c6d
JHS <wnghdtjr129@naver.com>
2022-11-16 16:00:24 +0900

    feat(Application, InputView, OutputView): 다리 길이 입력 기능 구현

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -1,8 +1,18 @@
 package bridge;
 
+import bridge.view.InputView;
+import bridge.view.OutputView;
+
 public class Application {
 
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        OutputView outputView = new OutputView();
+        InputView inputView = new InputView();
+
+        outputView.startGame();
+        int bridgeSize = inputView.readBridgeSize();
+        if (bridgeSize < 3 || bridgeSize > 20) {
+            throw new IllegalArgumentException("[ERROR] 다리 길이는 3부터 20 사이의 숫자여야 합니다.");
+        }
     }
 }
