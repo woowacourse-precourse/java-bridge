5c9aac7d6ca492e4074ec6b09eb9e308cd1d5121
JaeWon Choi <87649850+codesver@users.noreply.github.com>
2022-11-16 21:09:45 +0900

    feat(ErrorMessage): add error constant

--- src/main/java/bridge/constant/ErrorMessage.java
+++ src/main/java/bridge/constant/ErrorMessage.java
@@ -2,7 +2,8 @@ package bridge.constant;
 
 public enum ErrorMessage {
 
-    IS_NOT_POSITIVE_NUMBER("[ERROR] 다리 길이 입력값이 양의 정수가 아닙니다.");
+    IS_NOT_POSITIVE_NUMBER("[ERROR] 다리 길이 입력값이 양의 정수가 아닙니다."),
+    IS_OUT_OF_RANGE("[ERROR] 다리 길이 입력값이 범위 3 ~ 20을 벗어났습니다.");
 
     private final String message;
 
