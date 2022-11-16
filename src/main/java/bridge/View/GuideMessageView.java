aec3950d8dcc5b2a1617b747a6745ad670855531
eunbii0213 <eunbii0213@naver.com>
2022-11-17 01:01:38 +0900

    feat: 다리 길이 입력 안내 메시지 설정

--- src/main/java/bridge/View/GuideMessageView.java
+++ src/main/java/bridge/View/GuideMessageView.java
@@ -1,7 +1,8 @@
 package bridge.View;
 
 public enum GuideMessageView {
-	START_BRIDGE_GAME_GUIDE_MESSAGE("다리 건너기 게임을 시작합니다.");
+	START_BRIDGE_GAME_GUIDE_MESSAGE("다리 건너기 게임을 시작합니다."),
+	BRIDGE_LENGTH_GUIDE_MESSAGE("다리의 길이를 입력해주세요.");
 
 	private String message;
 	GuideMessageView(String message){
