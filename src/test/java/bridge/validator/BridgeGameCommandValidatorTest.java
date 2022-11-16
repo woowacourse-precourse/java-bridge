968995732ccaf2173d7e87b5572aaa13b26631cd
Taewoon Kim <xodns98@gmail.com>
2022-11-17 04:01:45 +0900

    test(validator): BridgeGameCommandValidatorTest 구현

---
+++ src/test/java/bridge/validator/BridgeGameCommandValidatorTest.java
@@ -0,0 +1,86 @@
+package bridge.validator;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+class BridgeGameCommandValidatorTest {
+
+    @DisplayName("다리 건너기 테스트")
+    @Nested
+    class movingTest {
+
+        @DisplayName("커맨드가 올바를 때 인자 값을 그대로 반환하는지 확인")
+        @ParameterizedTest(name = "[{index}] : {0}")
+        @ValueSource(strings = {"U", "D"})
+        void movingCommandWithValidCommand(String command) {
+            assertThat(BridgeGameCommandValidator.movingCommandValidation(command)).isEqualTo(command);
+        }
+
+        @DisplayName("커맨드 문자열이 1자리가 아닐때 Exception 을 던지는지 확인")
+        @ParameterizedTest(name = "[{index}] : {0}")
+        @ValueSource(strings = {"", "12", "123", "1234", "12345"})
+        void movingCommandWithInvalidCommandSize(String command) {
+            assertThatThrownBy(() -> BridgeGameCommandValidator.movingCommandValidation(command))
+                    .isInstanceOf(IllegalArgumentException.class);
+        }
+
+        @DisplayName("커맨드 문자열이 소문자일 때 Exception 을 던지는지 확인")
+        @ParameterizedTest(name = "[{index}] : {0}")
+        @ValueSource(strings = {"u", "d"})
+        void movingCommandWithLowerCase(String command) {
+            assertThatThrownBy(() -> BridgeGameCommandValidator.movingCommandValidation(command))
+                    .isInstanceOf(IllegalArgumentException.class);
+        }
+
+        @DisplayName("커맨드 문자열이 'U', 'D'가 아닐 때 Exception 을 던지는지 확인")
+        @ParameterizedTest(name = "[{index}] : {0}")
+        @ValueSource(strings = {"A", "B", "C", "E", "F", "G",
+                "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "V", "W", "X", "Y", "Z"})
+        void movingCommandWithInvalidCharacter(String command) {
+            assertThatThrownBy(() -> BridgeGameCommandValidator.movingCommandValidation(command))
+                    .isInstanceOf(IllegalArgumentException.class);
+        }
+    }
+
+    @DisplayName("다리 건너기 게임 재시작 테스트")
+    @Nested
+    class retryTest {
+
+        @DisplayName("커맨드가 올바를 때 인자 값을 그대로 반환하는지 확인")
+        @ParameterizedTest(name = "[{index}] : {0}")
+        @ValueSource(strings = {"R", "Q"})
+        void retryCommandWithValidCommand(String command) {
+            assertThat(BridgeGameCommandValidator.retryCommandValidation(command)).isEqualTo(command);
+        }
+
+        @DisplayName("커맨드 문자열이 1자리가 아닐때 Exception 을 던지는지 확인")
+        @ParameterizedTest(name = "[{index}] : {0}")
+        @ValueSource(strings = {"", "12", "123", "1234", "12345"})
+        void retryCommandWithInvalidCommandSize(String command) {
+            assertThatThrownBy(() -> BridgeGameCommandValidator.retryCommandValidation(command))
+                    .isInstanceOf(IllegalArgumentException.class);
+        }
+
+        @DisplayName("커맨드 문자열이 소문자일 때 Exception 을 던지는지 확인")
+        @ParameterizedTest(name = "[{index}] : {0}")
+        @ValueSource(strings = {"r", "q"})
+        void retryCommandWithLowerCase(String command) {
+            assertThatThrownBy(() -> BridgeGameCommandValidator.retryCommandValidation(command))
+                    .isInstanceOf(IllegalArgumentException.class);
+        }
+
+        @DisplayName("커맨드 문자열이 'R', 'Q'가 아닐 때 Exception 을 던지는지 확인")
+        @ParameterizedTest(name = "[{index}] : {0}")
+        @ValueSource(strings = {"A", "B", "C", "D", "E", "F", "G",
+                "H", "I", "J", "K", "L", "M", "N", "O", "P", "S", "T", "U", "V", "W", "X", "Y", "Z"})
+        void retryCommandWithInvalidCharacter(String command) {
+            assertThatThrownBy(() -> BridgeGameCommandValidator.retryCommandValidation(command))
+                    .isInstanceOf(IllegalArgumentException.class);
+        }
+    }
+}
