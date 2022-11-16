f6c451bafc862c58fa6ffe6007fbf9af959e8688
youhobin <ghdb132@naver.com>
2022-11-16 20:33:58 +0900

    feat(Bridge) : 생성된 다리와 이동을 위해 입력한 값을 1개씩 비교하는 기능 구현

---
+++ src/main/java/bridge/Bridge.java
@@ -0,0 +1,17 @@
+package bridge;
+
+public class Bridge {
+    private final String answerPosition;
+    private final String myMovingPostion;
+    private final boolean correctBridge;
+
+    public Bridge(String answerPosition, String myMovingPostion) {
+        this.answerPosition = answerPosition;
+        this.myMovingPostion = myMovingPostion;
+        this.correctBridge = answerPosition.equals(myMovingPostion);
+    }
+
+    public boolean isCorrectBridge() {
+        return correctBridge;
+    }
+}
