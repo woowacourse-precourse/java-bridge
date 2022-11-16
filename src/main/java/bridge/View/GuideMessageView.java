81fe5425995375cda990bfd2013b6787ffbc4fa3
eunbii0213 <eunbii0213@naver.com>
2022-11-17 01:02:30 +0900

    feat: 이동할 칸 입력 안내 메시지 설정

--- src/main/java/bridge/View/GuideMessageView.java
+++ src/main/java/bridge/View/GuideMessageView.java
@@ -2,8 +2,8 @@ package bridge.View;
 
 public enum GuideMessageView {
 	START_BRIDGE_GAME_GUIDE_MESSAGE("다리 건너기 게임을 시작합니다."),
-	BRIDGE_LENGTH_GUIDE_MESSAGE("다리의 길이를 입력해주세요.");
-
+	BRIDGE_LENGTH_GUIDE_MESSAGE("다리의 길이를 입력해주세요."),
+	SELECT_MOVE_GUIDE_MESSAGE("이동할 칸을 선택해주세요. (위: U, 아래: D)");
 	private String message;
 	GuideMessageView(String message){
 		this.message = message;
