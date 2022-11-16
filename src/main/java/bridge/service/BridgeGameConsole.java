0d41abbd63c5512a691cdf10f2a542dc798876df
JaeWon Choi <87649850+codesver@users.noreply.github.com>
2022-11-16 20:30:22 +0900

    feat(BridgeGameConsole): build skeleton code

---
+++ src/main/java/bridge/service/BridgeGameConsole.java
@@ -0,0 +1,34 @@
+package bridge.service;
+
+public class BridgeGameConsole {
+
+    public void startGame() {
+        // 게임 시작 안내문을 출력한다.
+        // 다리 길이를 입력 받는다.
+        // 다리 생성
+        // 다리 건너기
+        // 게임 결과 출력
+    }
+
+    private int createBridgeLength() {
+        // 다리 길이 입력 안내
+        // 다리 길이 입력
+        return 0;
+    }
+
+    private void crossBridge() {
+        while (true) {
+            // 이동 방향 입력 안내
+            // 이동 방향 입력
+            // 이동 방향 입력값 처리
+            // 이동 결과 출력
+            // 이동 결과 처리
+        }
+    }
+
+    private void showGameResult() {
+        // 최종 다리 이동 결과 출력하기
+        // 게임 성공 여부 출력하기
+        // 시도 횟수 출력하기
+    }
+}
