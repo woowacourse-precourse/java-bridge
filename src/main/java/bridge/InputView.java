957e8b0de90c128772d66f7509f1653b54e2e6fc
ywj9811 <ywj9811@naver.com>
2022-11-16 23:30:17 +0900

    feat: InputView 클래스
    내부 로직 초안 작성

--- src/main/java/bridge/InputView.java
+++ src/main/java/bridge/InputView.java
@@ -1,5 +1,7 @@
 package bridge;
 
+import static camp.nextstep.edu.missionutils.Console.*;
+
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
  */
@@ -9,20 +11,29 @@ public class InputView {
      * 다리의 길이를 입력받는다.
      */
     public int readBridgeSize() {
-        return 0;
+        String inputSize = readLine();
+
+        return Integer.parseInt(inputSize);
     }
 
     /**
      * 사용자가 이동할 칸을 입력받는다.
      */
-    public String readMoving() {
-        return null;
+    public int readMoving() {
+        String move = readLine();
+        if (move.equals("U"))
+            return 1;
+        if (move.equals("D"))
+            return 0;
+        throw new IllegalArgumentException();
     }
 
     /**
      * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
      */
     public String readGameCommand() {
-        return null;
+        String reply = readLine();
+
+        return reply;
     }
 }
