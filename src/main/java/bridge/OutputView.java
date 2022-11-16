f8aa5094e7c78126c0a165c70903e6f34c8ab7de
2021110806 <2021110806@dgu.ac.kr>
2022-11-17 00:32:50 +0900

    feat(OutputView):저장된 메시지를 출력하는 outputView class 구현

--- src/main/java/bridge/OutputView.java
+++ src/main/java/bridge/OutputView.java
@@ -1,23 +1,26 @@
 package bridge;
 
-/**
- * 사용자에게 게임 진행 상황과 결과를 출력하는 역할을 한다.
- */
 public class OutputView {
 
-    /**
-     * 현재까지 이동한 다리의 상태를 정해진 형식에 맞춰 출력한다.
-     * <p>
-     * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
-     */
-    public void printMap() {
+    public void printGameStart() {
+        System.out.println(Output.gameStart.getOutput());
     }
 
-    /**
-     * 게임의 최종 결과를 정해진 형식에 맞춰 출력한다.
-     * <p>
-     * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
-     */
-    public void printResult() {
+    public void printBridgeLengthInputRequest() {
+        System.out.println(Output.bridgeLengthInputRequest.getOutput());
     }
+
+    public void printWhereToGoInputRequest() {
+        System.out.println(Output.whereToGoInputRequest);
+    }
+
+    public void printMap(String map) {
+        System.out.println(map);
+    }
+
+    public void printResult(String winOrLose, int numberOfAttempt) {
+        System.out.println(Output.winOrLoseResult.getOutput()+winOrLose);
+        System.out.println(Output.howManyAttempts.getOutput()+numberOfAttempt);
+    }
+
 }
