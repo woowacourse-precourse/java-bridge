4198d88040bd4cf72a90cace966658f265553af7
JaeWon Choi <87649850+codesver@users.noreply.github.com>
2022-11-16 20:39:37 +0900

    feat(Emcee): build skeleton code

---
+++ src/main/java/bridge/service/Emcee.java
@@ -0,0 +1,28 @@
+package bridge.service;
+
+public class Emcee {
+
+    public void guideGame() {
+
+    }
+
+    public void guideEnteringBridgeLength() {
+
+    }
+
+    public void guideEnteringMovement() {
+
+    }
+
+    public void guideEnteringRetryStatus() {
+
+    }
+
+    public void showBridgeMovementStatus() {
+
+    }
+
+    public void showGameResult() {
+
+    }
+}
