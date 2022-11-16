25a47b288800e3671be73e480ed64d4aee1f158e
H4R1B0 <qkrguswns25@naver.com>
2022-11-16 17:13:56 +0900

    feat: validate that input is between 3 and 20

--- src/main/java/bridge/Validation.java
+++ src/main/java/bridge/Validation.java
@@ -6,4 +6,9 @@ public class Validation {
         if (result.length() != 0)
             throw new IllegalArgumentException(ErrorMessage.ERROR_MESSAGE_INPUT_INCLUDE_CHARACTER.getMessage());
     }
+
+    public static void isInputBetween3And20(int input) {
+        if (input < 3 || input > 20)
+            throw new IllegalArgumentException(ErrorMessage.ERROR_MESSAGE_INPUT_NOT_BETWEEN_3_AND_20.getMessage());
+    }
 }
