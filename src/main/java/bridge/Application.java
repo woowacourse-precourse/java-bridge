bf3410288530b5afd9a412b939771d9dd107b640
Hyun-Seo Oh <ohseo8813@gmail.com>
2022-11-16 22:38:00 +0900

    <feat>(Application.java): 현재까지 완성된 로직 실행

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -1,8 +1,25 @@
 package bridge;
 
+import bridge.view.InputView;
+import bridge.view.OutputView;
+import java.util.List;
+
 public class Application {
 
+    private static final InputView inputView = new InputView();
+    private static final OutputView outputView = new OutputView();
+    private static final BridgeGame bridgeGame = new BridgeGame();
+
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        List<String> bridge = bridgeGame.getBridge(inputView.readBridgeSize());
+        if (moveResult(bridge)) {
+            moveResult(bridge);
+        }
+    }
+
+    private static boolean moveResult(List<String> bridge) {
+        boolean isCorrect = bridgeGame.move(bridge, inputView.readMoving());
+        outputView.printMap(bridge, isCorrect);
+        return isCorrect;
     }
 }
