d52d05282c7d8ca402677f1e8df37bb2a24f27dd
ywj9811 <ywj9811@naver.com>
2022-11-17 02:58:03 +0900

    feat: OutputView 클래스
    결과 출력 기능 추가

--- src/main/java/bridge/view/OutputView.java
+++ src/main/java/bridge/view/OutputView.java
@@ -2,29 +2,10 @@ package bridge.view;
 
 import java.util.List;
 
-/**
- * 사용자에게 게임 진행 상황과 결과를 출력하는 역할을 한다.
- */
 public class OutputView {
     private static String upBridge = "[]";
     private static String downBridge = "[]";
-    private static OutputString outputString;
 
-    /**
-     * 현재까지 이동한 다리의 상태를 정해진 형식에 맞춰 출력한다.
-     * <p>
-     * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
-     */
-
-    /**
-     * tf가 true이면 한칸 더 앞으로 가야함
-     * [ O ]
-     * [   ]
-     * [ O |   ]
-     * [   | X ]
-     * [ O |   |   ]
-     * [   | O | O ]
-     */
     public void start() {
         System.out.println(OutputString.START);
         System.out.println(OutputString.INPUT);
@@ -69,13 +50,24 @@ public class OutputView {
         }
     }
 
-    /**
-     * 게임의 최종 결과를 정해진 형식에 맞춰 출력한다.
-     * <p>
-     * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
-     */
-    public void printResult() {
+    public void printMove() {
         System.out.println(upBridge);
         System.out.println(downBridge);
+    }
+
+    public void printResult(boolean tf, int count, String re) {
+        if (!tf && re.equals("Q")) {
+            System.out.print(OutputString.SUCCESS_OR_NOT);
+            System.out.println(OutputString.FAIL);
+            System.out.println(OutputString.TRY + "count");
+            return;
+        }
+        System.out.print(OutputString.SUCCESS_OR_NOT);
+        System.out.println(OutputString.SUCCESS);
+        System.out.println(OutputString.TRY + "count");
+    }
+
+    public void retry() {
+        System.out.println(OutputString.RETRY);
     }
 }
