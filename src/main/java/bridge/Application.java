c7b6e576a2273298e2ec3124edbc1569f86f560b
Dan-HeeYoung9182 <ldan9182@gmail.com>
2022-11-16 21:33:09 +0900

    입력된 길이 만큼의 U 혹은 D를 생성해서 List<String>에 저장

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -1,9 +1,10 @@
 package bridge;
 
+import java.util.List;
+
 public class Application {
 
     public static void main(String[] args) {
-        InputView inputview = new InputView();
-        inputview.readBridgeSize();
+
     }
 }
