cdfaf6c6bd96c79cf8f461838f2e189f5865361d
sjk <80821744+ShinjaeKwon@users.noreply.github.com>
2022-11-16 19:25:05 +0900

    feat : 게임 종료 여부 입력 기능 추가 및 인라인화 리팩터링

--- src/main/java/bridge/view/InputView.java
+++ src/main/java/bridge/view/InputView.java
@@ -12,24 +12,24 @@ public class InputView {
 	 * 다리의 길이를 입력받는다.
 	 */
 	public int readBridgeSize() {
-		String input = Console.readLine();
-		CheckHandler.isBridgeLengthInput(input); // TODO : 다리 길이 입력시 정상 입력되었는지 확인
-		return Integer.parseInt(input);
+		// TODO : 다리 길이 입력시 정상 입력되었는지 확인
+		return Integer.parseInt(CheckHandler.isBridgeLengthInput(Console.readLine()));
 	}
 
 	/**
 	 * 사용자가 이동할 칸을 입력받는다.
 	 */
 	public String readMoving() {
-		String input = Console.readLine();
-		CheckHandler.isMoveInput(input);
-		return input;
+		// TODO : 이동 위치 입력시 정상 입력되었는지 확인
+		return CheckHandler.isMoveInput(Console.readLine());
 	}
 
 	/**
 	 * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
 	 */
 	public String readGameCommand() {
-		return null;
+		// TODO : 게임 종료 여부 입력시 정상 입력되었는지 확인
+		return CheckHandler.isGameCommandInput(Console.readLine());
 	}
+
 }
