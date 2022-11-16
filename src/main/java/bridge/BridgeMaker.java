b2cdd16d3f15dda129e33503210adbc3698bab19
ywj9811 <ywj9811@naver.com>
2022-11-16 23:31:06 +0900

    feat: BridgeGame 클래스
    다리를 숫자로 받아서 U, D로 변환하여 반환해준다.

--- src/main/java/bridge/BridgeMaker.java
+++ src/main/java/bridge/BridgeMaker.java
@@ -1,5 +1,6 @@
 package bridge;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -18,6 +19,23 @@ public class BridgeMaker {
      * @return 입력받은 길이에 해당하는 다리 모양. 위 칸이면 "U", 아래 칸이면 "D"로 표현해야 한다.
      */
     public List<String> makeBridge(int size) {
-        return null;
+        List<Integer> temporaryBridge = new ArrayList<>();
+        for (int length = 0; length < size; length++) {
+            temporaryBridge.add(bridgeNumberGenerator.generate());
+        }
+        List<String> bridge = compareBridge(temporaryBridge);
+        return bridge;
+    }
+
+    public List<String> compareBridge(List<Integer> temporaryBridge) {
+        List<String> bridge = new ArrayList<>();
+        for (int idx = 0; idx < temporaryBridge.size(); idx++) {
+            if (temporaryBridge.get(idx) == 0) {
+                bridge.add("D");
+                continue;
+            }
+            bridge.add("U");
+        }
+        return bridge;
     }
 }
