c1faeba83170a0fe1676ec56e0f03ca24204b0c4
Taewoon Kim <xodns98@gmail.com>
2022-11-17 04:13:04 +0900

    test(validator): BridgeValidatorTest 구현

--- src/test/java/bridge/validator/BridgeGameCommandValidatorTest.java
+++ src/test/java/bridge/validator/BridgeGameCommandValidatorTest.java
@@ -14,15 +14,15 @@ class BridgeGameCommandValidatorTest {
     @Nested
     class movingTest {
 
-        @DisplayName("커맨드가 올바를 때 인자 값을 그대로 반환하는지 확인")
-        @ParameterizedTest(name = "[{index}] : {0}")
+        @DisplayName("커맨드가 올바를 때 커맨드를 그대로 반환하는지 확인")
+        @ParameterizedTest(name = "[{index}]번째 테스트 : {0}")
         @ValueSource(strings = {"U", "D"})
         void movingCommandWithValidCommand(String command) {
             assertThat(BridgeGameCommandValidator.movingCommandValidation(command)).isEqualTo(command);
         }
 
         @DisplayName("커맨드 문자열이 1자리가 아닐때 Exception 을 던지는지 확인")
-        @ParameterizedTest(name = "[{index}] : {0}")
+        @ParameterizedTest(name = "[{index}]번째 테스트 : {0}")
         @ValueSource(strings = {"", "12", "123", "1234", "12345"})
         void movingCommandWithInvalidCommandSize(String command) {
             assertThatThrownBy(() -> BridgeGameCommandValidator.movingCommandValidation(command))
@@ -30,7 +30,7 @@ class BridgeGameCommandValidatorTest {
         }
 
         @DisplayName("커맨드 문자열이 소문자일 때 Exception 을 던지는지 확인")
-        @ParameterizedTest(name = "[{index}] : {0}")
+        @ParameterizedTest(name = "[{index}]번째 테스트 : {0}")
         @ValueSource(strings = {"u", "d"})
         void movingCommandWithLowerCase(String command) {
             assertThatThrownBy(() -> BridgeGameCommandValidator.movingCommandValidation(command))
@@ -38,7 +38,7 @@ class BridgeGameCommandValidatorTest {
         }
 
         @DisplayName("커맨드 문자열이 'U', 'D'가 아닐 때 Exception 을 던지는지 확인")
-        @ParameterizedTest(name = "[{index}] : {0}")
+        @ParameterizedTest(name = "[{index}]번째 테스트 : {0}")
         @ValueSource(strings = {"A", "B", "C", "E", "F", "G",
                 "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "V", "W", "X", "Y", "Z"})
         void movingCommandWithInvalidCharacter(String command) {
@@ -51,15 +51,15 @@ class BridgeGameCommandValidatorTest {
     @Nested
     class retryTest {
 
-        @DisplayName("커맨드가 올바를 때 인자 값을 그대로 반환하는지 확인")
-        @ParameterizedTest(name = "[{index}] : {0}")
+        @DisplayName("커맨드가 올바를 때 커맨드를 그대로 반환하는지 확인")
+        @ParameterizedTest(name = "[{index}]번째 테스트 : {0}")
         @ValueSource(strings = {"R", "Q"})
         void retryCommandWithValidCommand(String command) {
             assertThat(BridgeGameCommandValidator.retryCommandValidation(command)).isEqualTo(command);
         }
 
         @DisplayName("커맨드 문자열이 1자리가 아닐때 Exception 을 던지는지 확인")
-        @ParameterizedTest(name = "[{index}] : {0}")
+        @ParameterizedTest(name = "[{index}]번째 테스트 : {0}")
         @ValueSource(strings = {"", "12", "123", "1234", "12345"})
         void retryCommandWithInvalidCommandSize(String command) {
             assertThatThrownBy(() -> BridgeGameCommandValidator.retryCommandValidation(command))
@@ -67,7 +67,7 @@ class BridgeGameCommandValidatorTest {
         }
 
         @DisplayName("커맨드 문자열이 소문자일 때 Exception 을 던지는지 확인")
-        @ParameterizedTest(name = "[{index}] : {0}")
+        @ParameterizedTest(name = "[{index}]번째 테스트 : {0}")
         @ValueSource(strings = {"r", "q"})
         void retryCommandWithLowerCase(String command) {
             assertThatThrownBy(() -> BridgeGameCommandValidator.retryCommandValidation(command))
@@ -75,7 +75,7 @@ class BridgeGameCommandValidatorTest {
         }
 
         @DisplayName("커맨드 문자열이 'R', 'Q'가 아닐 때 Exception 을 던지는지 확인")
-        @ParameterizedTest(name = "[{index}] : {0}")
+        @ParameterizedTest(name = "[{index}]번째 테스트 : {0}")
         @ValueSource(strings = {"A", "B", "C", "D", "E", "F", "G",
                 "H", "I", "J", "K", "L", "M", "N", "O", "P", "S", "T", "U", "V", "W", "X", "Y", "Z"})
         void retryCommandWithInvalidCharacter(String command) {
