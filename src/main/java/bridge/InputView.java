13c96ee12817cc5816938ca78e9b8daa8aaab4f7
WhoIsWoony <WhoIsWoony@gmail.com>
2022-11-16 15:42:22 +0900

    feat: add readGameCommand

--- src/main/java/bridge/InputView.java
+++ src/main/java/bridge/InputView.java
@@ -37,6 +37,12 @@ public class InputView {
      * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
      */
     public String readGameCommand() {
-        return null;
+        System.out.println("게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)");
+
+        Scanner sc = new Scanner(System.in);
+        String gameCommand = sc.next();
+        sc.close();
+
+        return gameCommand;
     }
 }
