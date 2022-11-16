41335540a9cb976db354cd12bd390e80098cc200
seung-won-kim <hesjs5@skuniv.ac.kr>
2022-11-16 17:12:34 +0900

    test(ValidatorTest): 숫자가 3부터 20 사이의 수인지 확인하는 기능 테스트

---
+++ src/test/java/bridge/util/ValidatorTest.java
@@ -0,0 +1,24 @@
+package bridge.util;
+
+import static bridge.util.Validator.validateNumberInBridgeRange;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+class ValidatorTest {
+
+    @ParameterizedTest
+    @ValueSource(ints = {1, 2, 21, 22, 23})
+    void validateNumberInBridgeRangeByOutOfRange(int number) {
+        assertThatThrownBy(() -> validateNumberInBridgeRange(number))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20})
+    void validateNumberInBridgeRangeByInRange(int number) {
+        assertDoesNotThrow(() -> validateNumberInBridgeRange(number));
+    }
+}
