3132aacd2a6f358450baf021f33f03f2d7bbb6e3
eunji53 <duqtj00@naver.com>
2022-11-16 22:45:15 +0900

    feat : 다리 길이 숫자 이외의 것 입력 시 오류 발생

--- src/main/java/bridge/InputView.java
+++ src/main/java/bridge/InputView.java
@@ -6,15 +6,18 @@ import static camp.nextstep.edu.missionutils.Console.readLine;
  * 사용자로부터 입력을 받는 역할을 한다.
  */
 public class InputView {
-
-    /**
-     * 다리의 길이를 입력받는다.
-     */
     public int readBridgeSize() {
         String bridgesSize = readLine();
+        notNumber(bridgesSize);
         return Integer.valueOf(bridgesSize);
     }
-
+    private void notNumber(String bridgesSize){
+        try {
+            Double.parseDouble(bridgesSize);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("[ERROR] 숫자만 입력해 주세요.");
+        }
+    }
     /**
      * 사용자가 이동할 칸을 입력받는다.
      */
