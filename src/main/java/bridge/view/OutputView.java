8338ee3ab77fff6e2068afe2e535b6b33b3439eb
Cheon-DM <76736548+Cheon-DM@users.noreply.github.com>
2022-11-16 23:52:06 +0900

    feat(outputView): 출력값 관련 상수 선언

--- src/main/java/bridge/view/OutputView.java
+++ src/main/java/bridge/view/OutputView.java
@@ -5,6 +5,14 @@ package bridge.view;
  */
 public class OutputView {
 
+    private static final String START_BRIDGE = "[";
+    private static final String END_BRIDGE = "]";
+    private static final String DIVISION_BRIDGE = "|";
+    private static final String FINAL_GAME_RESULT = "최종 게임 결과";
+    private static final String GAME_SUCCESS_OR_FAIL = "게임 성공 여부: ";
+    private static final String GAME_TRY = "총 시도한 횟수: ";
+
+
     /**
      * 현재까지 이동한 다리의 상태를 정해진 형식에 맞춰 출력한다.
      * <p>
