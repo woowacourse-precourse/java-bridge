6def86104b62ed1e2cc787c49300f443a5069396
SH <gram753@naver.com>
2022-11-16 22:16:16 +0900

    refactor: 문자열, 숫자 상수화

--- src/main/java/bridge/view/Validator.java
+++ src/main/java/bridge/view/Validator.java
@@ -3,21 +3,27 @@ package bridge.view;
 public class Validator {
 	private static final String SIZE_ERROR = "[ERROR] 다리 길이는 3부터 20 사이의 숫자여야 합니다.";
 	public static final String INVALID_INPUT = "[ERROR] 잘못된 입력입니다.";
+	public static final String UPSIDE = "U";
+	public static final String DOWNSIDE = "D";
+	public static final String RESTART_GAME = "R";
+	public static final String QUIT_GAME = "Q";
+	static final int MIN = 3;
+	static final int MAX = 20;
 
 	static void isSizeValidate(int size) {
-		if (size < 3 || size > 20) {
+		if (size < MIN || size > MAX) {
 			throw new IllegalArgumentException(SIZE_ERROR);
 		}
 	}
 
 	static void isMoveValidate(String input) {
-		if (!input.equals("U") && !input.equals("D")) {
+		if (!input.equals(UPSIDE) && !input.equals(DOWNSIDE)) {
 			throw new IllegalArgumentException(INVALID_INPUT);
 		}
 	}
 
 	static void isCommandValidate(String command) {
-		if (!command.equals("R") && !command.equals("Q")) {
+		if (!command.equals(RESTART_GAME) && !command.equals(QUIT_GAME)) {
 			throw new IllegalArgumentException(INVALID_INPUT);
 		}
 	}
