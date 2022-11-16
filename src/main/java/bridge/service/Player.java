f101346cc5446dfcbab4ef9e271678180c8cd168
JaeWon Choi <87649850+codesver@users.noreply.github.com>
2022-11-16 20:36:03 +0900

    feat(Player): build skeleton code

---
+++ src/main/java/bridge/service/Player.java
@@ -0,0 +1,22 @@
+package bridge.service;
+
+public class Player {
+
+    public int enterBridgeLength() {
+        // 다리의 길이 입력 3 ~ 20
+        // 다리 길이 입력값 검증
+        return 0;
+    }
+
+    public char enterMovement() {
+        // 이동 방향 입력 U or D
+        // 이동 방향 입력값 검증
+        return '0';
+    }
+
+    public char enterRetryStatus() {
+        // 재시도 여부 입력 R or Q
+        // 재시도 여부 입력값 검증
+        return '0';
+    }
+}
