9f77f82ec59bd087a5ab0490dae3be5abac9bf5f
이한솔 <hansol8701@naver.com>
2022-11-16 22:04:01 +0900

    test(InputView): 다리의 길이가 범위를 벗어날 경우 예외 발생을 확인하는 테스트 코드 추가

--- src/test/java/bridge/view/ViewTest.java
+++ src/test/java/bridge/view/ViewTest.java
@@ -11,24 +11,41 @@ import java.io.InputStream;
 import java.nio.charset.StandardCharsets;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class ViewTest {
 
     public static InputStream generateUserInput(String input){
         return new ByteArrayInputStream(input.getBytes());
     }
+
     @Test
     @DisplayName("다리의 길이를 입력받는다.")
     void 다리의_길이를_입력받는다(){
         //given
         InputView inputView = new InputView();
-        InputStream in = generateUserInput("3");
-        System.setIn(in);
 
         //when
+        InputStream in = generateUserInput("3");
+        System.setIn(in);
         int bridgeLength = inputView.readBridgeSize();
 
         //then
         assertThat(bridgeLength).isEqualTo(3);
+    }
+
+    @Test
+    @DisplayName("다리의 길이가 범위를 벗어날 경우 예외가 발생한다.")
+    void 다리의_길이가_범위를_벗어날_경우_예외가_발생한다() {
+        // given
+        InputView inputView = new InputView();
+
+        // when
+        InputStream in = generateUserInput("22");
+        System.setIn(in);
+
+        // then
+        assertThatThrownBy(inputView::readBridgeSize)
+                .isInstanceOf(IllegalArgumentException.class);
     }
 }
