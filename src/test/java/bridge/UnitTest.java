f4ae48f92ef17a6386b58d702801d0a5cf4975c3
jhon3242 <rldotnr2@gmail.com>
2022-11-16 19:01:54 +0900

    feat(UnitTest) : 다리 정상 생성 테스트 구현

---
+++ src/test/java/bridge/UnitTest.java
@@ -0,0 +1,30 @@
+package bridge;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.provider.ValueSource;
+
+public class UnitTest {
+
+	@DisplayName("다리 생성 정상 테스트")
+	@ValueSource(ints = {3, 4, 5 , 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20})
+	@Test
+	void BridgeSizeTest(Integer input) {
+		Assertions.assertThatCode(() -> {
+			BridgeMaker bridgeMaker = new BridgeMaker(new BridgeRandomNumberGenerator());
+			bridgeMaker.makeBridge(input);
+		}).doesNotThrowAnyException();
+	}
+
+	@DisplayName("다리 생성 예외 테스트")
+	@ValueSource(ints = {-1, 0, 1, 2, 21, 50, 100})
+	@Test
+	void BridgeSizeExceptionTest(Integer input) {
+		Assertions.assertThatThrownBy(() -> {
+			BridgeMaker bridgeMaker = new BridgeMaker(new BridgeRandomNumberGenerator());
+			bridgeMaker.makeBridge(input);
+		}).isInstanceOf(IllegalArgumentException.class);
+	}
+
+}
--- src/test/java/bridge/ViewTest.java
+++ src/test/java/bridge/ViewTest.java
@@ -41,23 +41,4 @@ public class ViewTest {
 
 
 
-//	@DisplayName("다리 생성 정상 테스트")
-//	@ValueSource(ints = {3, 4, 5 , 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20})
-//	@Test
-//	void BridgeSizeTest(Integer input) {
-//		Assertions.assertThatCode(() -> {
-//			BridgeMaker bridgeMaker = new BridgeMaker(new BridgeRandomNumberGenerator());
-//			bridgeMaker.makeBridge(input);
-//		}).doesNotThrowAnyException();
-//	}
-//
-//	@DisplayName("다리 생성 예외 테스트")
-//	@ValueSource(ints = {-1, 0, 1, 2, 21, 50, 100})
-//	@Test
-//	void BridgeSizeExceptionTest(Integer input) {
-//		Assertions.assertThatThrownBy(() -> {
-//			BridgeMaker bridgeMaker = new BridgeMaker(new BridgeRandomNumberGenerator());
-//			bridgeMaker.makeBridge(input);
-//		}).isInstanceOf(IllegalArgumentException.class);
-//	}
 }
