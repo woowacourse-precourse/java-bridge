fb6b6e0a47ba794d22dcd2b9a1e5d34b0c4a9353
ghchoi <ghchoi0427@gmail.com>
2022-11-16 16:06:40 +0900

    feat: Validation 클래스 생성

---
+++ src/main/java/bridge/Validation.java
@@ -0,0 +1,33 @@
+package bridge;
+
+import java.util.regex.Pattern;
+
+public class Validation {
+
+    public void validateBridgeSizeInput(String input) {
+    }
+
+    public void validateMoving(String input) {
+    }
+
+    public void validateGameCommand(String input) {
+    }
+
+    private void checkEmpty(String input) {
+    }
+
+    private void isNumeric(String str) {
+    }
+
+    private void isAlphabetic(String str) {
+    }
+
+    private void isSizeInBound(int size, int min, int max) {
+    }
+
+    private void isMovingCommand(String input) {
+    }
+
+    private void isGameCommand(String input) {
+    }
+}
