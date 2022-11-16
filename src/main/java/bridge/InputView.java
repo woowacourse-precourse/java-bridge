3c4991e327d4a337f4c3d5e438e20f8ed26b803b
chlwodyd9829 <chlwodyd9829@naver.com>
2022-11-16 16:45:25 +0900

    feat: 이동할 칸 입력 및 값 검증 기능

--- src/main/java/bridge/InputView.java
+++ src/main/java/bridge/InputView.java
@@ -8,11 +8,11 @@ import camp.nextstep.edu.missionutils.Console;
 public class InputView {
 
     private void sizeValidate(String size) {
-        if (!isNumber(size)){
+        if (!isNumber(size)) {
             throw new IllegalArgumentException();
         }
-        if (!isRange(size)){
-            throw  new IllegalArgumentException();
+        if (!isRange(size)) {
+            throw new IllegalArgumentException();
         }
     }
 
@@ -22,13 +22,14 @@ public class InputView {
         return true;
     }
 
-    private boolean isNumber(String size){
-        for (int i = 0; i < size.length(); i++){
+    private boolean isNumber(String size) {
+        for (int i = 0; i < size.length(); i++) {
             char c = size.charAt(i);
             if (!Character.isDigit(c)) return false;
         }
         return true;
     }
+
     /**
      * 다리의 길이를 입력받는다.
      */
@@ -38,12 +39,33 @@ public class InputView {
         return Integer.parseInt(size);
     }
 
+    private void moveValidate(String move) {
+        if (!isOne(move)) {
+            throw new IllegalArgumentException();
+        }
+        if (!isUpDown(move)) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private boolean isOne(String move) {
+        if (move.length() > 1) return false;
+        return true;
+    }
+
+    private boolean isUpDown(String move) {
+        if (!move.equals("U") && !move.equals("D")) return false;
+        return true;
+    }
 
     /**
      * 사용자가 이동할 칸을 입력받는다.
      */
     public String readMoving() {
-        return null;
+        String move = Console.readLine();
+        move = move.toUpperCase();
+        moveValidate(move);
+        return move;
     }
 
     /**
