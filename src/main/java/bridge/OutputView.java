8db247dc8f6f2d27c1d60b6ff7393d71d73dcfa9
WonJeongYeon <wonjungyoun@naver.com>
2022-11-16 18:22:58 +0900

    feat: 게임이 진행되면서 다리를 출력하는 기능 추가

--- src/main/java/bridge/OutputView.java
+++ src/main/java/bridge/OutputView.java
@@ -1,5 +1,9 @@
 package bridge;
 
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+
 /**
  * 사용자에게 게임 진행 상황과 결과를 출력하는 역할을 한다.
  */
@@ -10,7 +14,27 @@ public class OutputView {
      * <p>
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void printMap() {
+    public void printMap(List<List<String>> userPlaying) {
+        System.out.println(printLoop(userPlaying, "U"));
+        System.out.println(printLoop(userPlaying, "D"));
+
+    }
+
+    public String printLoop(List<List<String>> userPlaying, String bridgeLocate) {
+        String upPrint = "[";
+        for (int i = 0; i< userPlaying.size(); i++) {
+            if (i != 0)
+                upPrint += "|";
+            upPrint += printContent(userPlaying.get(i), bridgeLocate);
+        }
+        upPrint += "]";
+        return upPrint;
+    }
+
+    public String printContent(List<String> currentPlaying, String bridgeLocate) {
+        if (Objects.equals(currentPlaying.get(0), bridgeLocate))
+            return " " + currentPlaying.get(1) + " ";
+        return "   ";
     }
 
     /**
