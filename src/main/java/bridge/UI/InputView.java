efdc175d85a49ec3c4b10e0c434b7c640095c105
WhalesBob <ndw8200@naver.com>
2022-11-16 21:27:05 +0900

    feat(inputView) : make readGameCommand features in User's Input(UI)

--- src/main/java/bridge/UI/InputView.java
+++ src/main/java/bridge/UI/InputView.java
@@ -40,6 +40,12 @@ public class InputView {
      * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
      */
     public String readGameCommand() {
-        return null;
+        String gameCommandInput;
+        do{
+            gameCommandInput = Console.readLine();
+            isRightInput = checkValidation.validateMovingInput(gameCommandInput);
+        }while(!isRightInput);
+
+        return gameCommandInput;
     }
 }
