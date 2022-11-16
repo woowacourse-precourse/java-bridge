6468b507c944251d9de9fa9bb5747b29629fa990
kariskan <zkfzpf56@naver.com>
2022-11-16 18:41:44 +0900

    feat: 입력 역할을 하는 InputView 작성
    1. 다리 길이 입력받는 readBridgeSize()
    
    2. 이동할 칸을 입력받는 readMoving()
    
    3. 게임 재시작 여부를 입력받는 readGameCommand()

--- src/main/java/bridge/InputView.java
+++ src/main/java/bridge/InputView.java
@@ -1,5 +1,8 @@
 package bridge;
 
+import bridge.utils.validator.InputValidator;
+import camp.nextstep.edu.missionutils.Console;
+
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
  */
@@ -9,20 +12,41 @@ public class InputView {
      * 다리의 길이를 입력받는다.
      */
     public int readBridgeSize() {
-        return 0;
+        String input = Console.readLine();
+        try {
+            InputValidator.bridgeSizeValidate(input);
+            return Integer.parseInt(input);
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+            return readBridgeSize();
+        }
     }
 
     /**
      * 사용자가 이동할 칸을 입력받는다.
      */
     public String readMoving() {
-        return null;
+        try {
+            String move = Console.readLine();
+            InputValidator.movingValidate(move);
+            return move;
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+            return readMoving();
+        }
     }
 
     /**
      * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
      */
     public String readGameCommand() {
-        return null;
+        try {
+            String command = Console.readLine();
+            InputValidator.gameCommandValidate(command);
+            return command;
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+            return readGameCommand();
+        }
     }
 }
