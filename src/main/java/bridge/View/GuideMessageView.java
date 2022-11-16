6da71b7d98037af9003d758f060fe08909680935
eunbii0213 <eunbii0213@naver.com>
2022-11-17 01:03:48 +0900

    feat: 재시작 혹은 종료 안내 메시지 설정

--- src/main/java/bridge/View/GuideMessageView.java
+++ src/main/java/bridge/View/GuideMessageView.java
@@ -3,7 +3,8 @@ package bridge.View;
 public enum GuideMessageView {
 	START_BRIDGE_GAME_GUIDE_MESSAGE("다리 건너기 게임을 시작합니다."),
 	BRIDGE_LENGTH_GUIDE_MESSAGE("다리의 길이를 입력해주세요."),
-	SELECT_MOVE_GUIDE_MESSAGE("이동할 칸을 선택해주세요. (위: U, 아래: D)");
+	SELECT_MOVE_GUIDE_MESSAGE("이동할 칸을 선택해주세요. (위: U, 아래: D)"),
+	RE_OR_END_GUIDE_MESSAGE("게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)");
 	private String message;
 	GuideMessageView(String message){
 		this.message = message;
