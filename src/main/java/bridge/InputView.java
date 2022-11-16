e61b99d9ff2d344f3f4df05e4dfda53dadadea98
H4R1B0 <qkrguswns25@naver.com>
2022-11-16 17:27:42 +0900

    feat(InputView): input bridge size

--- src/main/java/bridge/InputView.java
+++ src/main/java/bridge/InputView.java
@@ -1,15 +1,35 @@
 package bridge;
 
+import camp.nextstep.edu.missionutils.Console;
+
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
  */
 public class InputView {
+    private static final String INPUT_BRIDGE_SIZE = "다리의 길이를 입력해주세요.";
 
     /**
      * 다리의 길이를 입력받는다.
      */
     public int readBridgeSize() {
-        return 0;
+        while (true) {
+            System.out.println(INPUT_BRIDGE_SIZE);
+            String bridgeSize = Console.readLine();
+            if (!validateReadBridgeSize(bridgeSize))
+                continue;
+            return Integer.parseInt(bridgeSize);
+        }
+    }
+
+    private boolean validateReadBridgeSize(String bridgeSize) {
+        try {
+            Validation.isInputIncludeCharacter(bridgeSize);
+            Validation.isInputBetween3And20(Integer.parseInt(bridgeSize));
+        } catch (IllegalArgumentException e) {
+            System.out.println(e);
+            return false;
+        }
+        return true;
     }
 
     /**
