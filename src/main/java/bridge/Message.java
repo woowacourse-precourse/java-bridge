866ce7632859258d21e185866fa9965a171e7452
abbd2 <ajkety00@gmail.com>
2022-11-16 23:50:26 +0900

    Style(Message): char 상수의 이름을 변경

--- src/main/java/bridge/Message.java
+++ src/main/java/bridge/Message.java
@@ -7,12 +7,12 @@ public enum Message {
     INPUT_MOVE_BRIDGE_MESSAGE("이동할 칸을 선택해주세요."),
     INPUT_SELECT_GAME_RETRY("게임을 다시 시도할지 여부를 입력해주세요.");
 
-    private static final char UP_BRIDGE = 'U';
-    private static final char DOWN_BRIDGE = 'D';
-    private static final String BRIDGE_OPTIONS = "(위: " + UP_BRIDGE + ", 아래: " + DOWN_BRIDGE + ")";
-    private static final char RETRY_GAME = 'R';
-    private static final char QUIT_GAME = 'Q';
-    private static final String GAME_POTIONS = "(재시도: " + RETRY_GAME + ", 종료:" + QUIT_GAME + ")";
+    private static final char UP_BRIDGE_KEY = 'U';
+    private static final char DOWN_BRIDGE_KEY = 'D';
+    private static final String BRIDGE_OPTIONS = "(위: " + UP_BRIDGE_KEY + ", 아래: " + DOWN_BRIDGE_KEY + ")";
+    private static final char RETRY_GAME_KEY = 'R';
+    private static final char QUIT_GAME_KEY = 'Q';
+    private static final String GAME_POTIONS = "(재시도: " + RETRY_GAME_KEY + ", 종료:" + QUIT_GAME_KEY + ")";
     private String script;
     Message(String script) {
         this.script = script;
