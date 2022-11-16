d59747f20257d4b179004b5271e66a43fb22557b
Hyun-Seo Oh <ohseo8813@gmail.com>
2022-11-16 23:50:25 +0900

    <test>(ValidatorTest.java): 게임 재시작 여부 메시지 검증

--- src/test/java/bridge/ValidatorTest.java
+++ src/test/java/bridge/ValidatorTest.java
@@ -1,8 +1,10 @@
 package bridge;
 
+import static bridge.constant.BridgeConstant.CLOSE_GAME;
 import static bridge.constant.BridgeConstant.LOWER_BLOCK;
 import static bridge.constant.BridgeConstant.MAX_SIZE;
 import static bridge.constant.BridgeConstant.MIN_SIZE;
+import static bridge.constant.BridgeConstant.RESTART_GAME;
 import static bridge.constant.BridgeConstant.UPPER_BLOCK;
 import static org.assertj.core.api.Assertions.assertThatNoException;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
@@ -49,24 +51,50 @@ class ValidatorTest {
     @DisplayName("이동 명령 메시지가")
     class MoveMessageTest {
 
-        @DisplayName(UPPER_BLOCK + "일시 정상 통과한다")
+        @DisplayName(UPPER_BLOCK + " 일시 정상 통과한다")
         @Test
         void test1() {
             assertThatNoException()
                     .isThrownBy(() -> validator.validateMoveMessage(UPPER_BLOCK));
         }
 
-        @DisplayName(LOWER_BLOCK + "일시 정상 통과한다")
+        @DisplayName(LOWER_BLOCK + " 일시 정상 통과한다")
         @Test
         void test2() {
             assertThatNoException()
                     .isThrownBy(() -> validator.validateMoveMessage(LOWER_BLOCK));
         }
 
-        @DisplayName(UPPER_BLOCK + "이나" + LOWER_BLOCK + "이 아니라면 예외를 반환한다.")
+        @DisplayName(UPPER_BLOCK + "," + LOWER_BLOCK + "가 아니라면 예외를 반환한다.")
         @Test
         void test3() {
             assertThatThrownBy(() -> validator.validateMoveMessage(UPPER_BLOCK + LOWER_BLOCK))
+                    .isInstanceOf(IllegalArgumentException.class);
+        }
+    }
+
+    @Nested
+    @DisplayName("게임 재시작 여부 메시지가")
+    class GameRestartMessageTest {
+
+        @DisplayName(RESTART_GAME + " 일시 정상 통과한다")
+        @Test
+        void test1() {
+            assertThatNoException()
+                    .isThrownBy(() -> validator.validateGameRestartMessage(RESTART_GAME));
+        }
+
+        @DisplayName(CLOSE_GAME + " 일시 정상 통과한다")
+        @Test
+        void test2() {
+            assertThatNoException()
+                    .isThrownBy(() -> validator.validateGameRestartMessage(CLOSE_GAME));
+        }
+
+        @DisplayName(RESTART_GAME + "," + CLOSE_GAME + "가 아니라면 예외를 반환한다.")
+        @Test
+        void test3() {
+            assertThatThrownBy(() -> validator.validateGameRestartMessage(RESTART_GAME + CLOSE_GAME))
                     .isInstanceOf(IllegalArgumentException.class);
         }
     }
