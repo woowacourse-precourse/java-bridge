d4e7e4ab30839fad9ce479fb3683869c31338c35
junpakPark <112045553+junpakPark@users.noreply.github.com>
2022-11-16 21:47:30 +0900

    feat(Command): Command Enum 생성
    Command Enum 주요 기능:
    - R 혹은 Q인지 유효성 검사
    - 입력된 문자열을 바탕으로 boolean 값 리턴

---
+++ src/main/java/bridge/Command.java
@@ -0,0 +1,29 @@
+package bridge;
+
+import java.util.List;
+
+public enum Command {
+	REPLAY("R"),
+	QUIT("Q"),
+	;
+
+	private final String value;
+
+	Command(String value) {
+		this.value = value;
+	}
+
+	public static boolean isReplay(String inputValue) {
+		String upperCase = inputValue.toUpperCase();
+		validate(upperCase);
+		return upperCase.equals(REPLAY.value);
+	}
+
+	private static void validate(String inputValue) {
+		List<String> commandStrings = List.of(REPLAY.value, QUIT.value);
+
+		if (!commandStrings.contains(inputValue)) {
+			throw new IllegalArgumentException(Error.COMMAND.getMessage());
+		}
+	}
+}
