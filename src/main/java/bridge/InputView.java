8046ca62b4028b1882a1544e8cfa37e2f15538ae
Cheon-DM <76736548+Cheon-DM@users.noreply.github.com>
2022-11-16 23:19:10 +0900

    feat(inputView): 입력 뷰 관련 기능 추가
    입력 뷰 관련 기능
    - 다리 길이 입력
    - 사용자 이동 칸 입력
    - 사용자 재시작 입력
    - String -> int 변환 오류

--- src/main/java/bridge/InputView.java
+++ src/main/java/bridge/InputView.java
@@ -1,28 +1,48 @@
 package bridge;
 
+import camp.nextstep.edu.missionutils.Console;
+
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
  */
 public class InputView {
 
+    private static final String BRIDGE_GAME_START_MSG = "다리 건너기 게임을 시작합니다.";
+    private static final String INPUT_BRIDGE_LENGTH = "다리의 길이를 입력해주세요.";
+    private static final String INPUT_MOVE_UP_OR_DOWN = "이동할 칸을 선택해주세요. (위: U, 아래: D)";
+    private static final String INPUT_RESTART_OR_QUIT = "게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)";
+    private static final String ERROR_MSG = "[ERROR]";
+
     /**
      * 다리의 길이를 입력받는다.
      */
     public int readBridgeSize() {
-        return 0;
+        System.out.println(INPUT_BRIDGE_LENGTH);
+        String bridgeLength = Console.readLine();
+        return parseIntOrThrowException(bridgeLength);
     }
 
     /**
      * 사용자가 이동할 칸을 입력받는다.
      */
     public String readMoving() {
-        return null;
+        System.out.println(INPUT_MOVE_UP_OR_DOWN);
+        return Console.readLine();
     }
 
     /**
      * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
      */
     public String readGameCommand() {
-        return null;
+        System.out.println(INPUT_RESTART_OR_QUIT);
+        return Console.readLine();
+    }
+
+    private int parseIntOrThrowException(String input){
+        try {
+            return Integer.parseInt(input);
+        } catch (IllegalArgumentException e){
+            throw new IllegalArgumentException(ERROR_MSG + "숫자가 아닌 값은 입력받을 수 없습니다.");
+        }
     }
 }
