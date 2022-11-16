87111eb696d077f269ea3ca4f466e6518e05c5a4
chlwodyd9829 <chlwodyd9829@naver.com>
2022-11-16 16:18:45 +0900

    feat: 다리사이즈 입력 및 값 검증 기능

--- src/main/java/bridge/InputView.java
+++ src/main/java/bridge/InputView.java
@@ -1,15 +1,42 @@
 package bridge;
 
+import camp.nextstep.edu.missionutils.Console;
+
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
  */
 public class InputView {
 
+    private boolean sizeValidate(String size) {
+        if (!isNumber(size)){
+            throw new IllegalArgumentException();
+        }
+        if (!isRange(size)){
+            throw  new IllegalArgumentException();
+        }
+        return true;
+    }
+
+    private boolean isRange(String size) {
+        int size_I = Integer.parseInt(size);
+        if (size_I < 3 || size_I > 20) return false;
+        return true;
+    }
+
+    private boolean isNumber(String size){
+        for (int i = 0; i < size.length(); i++){
+            char c = size.charAt(i);
+            if (!Character.isDigit(c)) return false;
+        }
+        return true;
+    }
     /**
      * 다리의 길이를 입력받는다.
      */
     public int readBridgeSize() {
-        return 0;
+        String size = Console.readLine();
+        sizeValidate(size);
+        return Integer.parseInt(size);
     }
 
     /**
