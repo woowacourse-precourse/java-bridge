484e393571e77a9f69028829a1ba02e640a503cc
June <68291395+june-777@users.noreply.github.com>
2022-11-16 22:21:26 +0900

    test: 다리 길이 입력 기능 예외상황 테스트

--- src/test/java/view/InputViewTest.java
+++ src/test/java/view/InputViewTest.java
@@ -1,10 +1,13 @@
 package view;
 
+import static org.assertj.core.api.Assertions.*;
+
 import java.io.ByteArrayInputStream;
 import java.io.InputStream;
 import java.util.List;
 
 import org.assertj.core.api.Assertions;
+
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
@@ -25,14 +28,13 @@ public class InputViewTest {
 					"16", "17", "18", "19", "20");
 
 			for (String bridgeSize : bridgeSizes) {
-
 				// when
 				int expectedBridgeSize = Integer.parseInt(bridgeSize);
 				readLine(bridgeSize);
 				int resultBridgeSize = inputView.readBridgeSize();
 
 				// then
-				Assertions.assertThat(resultBridgeSize).isEqualTo(expectedBridgeSize);
+				assertThat(resultBridgeSize).isEqualTo(expectedBridgeSize);
 			}
 		}
 	}
@@ -41,6 +43,70 @@ public class InputViewTest {
 	@DisplayName("예외상황 테스트 클래스")
 	class FailTest {
 
+		@Test
+		@DisplayName("다리 길이 영어를 입력하는 경우 테스트")
+		void inputBridgeSizeEnglish() {
+			// given
+			List<String> bridgeSizes = List.of("3a", "2z0", "9e", "s10", "ra");
+
+			for (String bridgeSize : bridgeSizes) {
+				// when, then
+				readLine(bridgeSize);
+				assertThatThrownBy(() -> inputView.readBridgeSize()).isInstanceOf(IllegalArgumentException.class);
+			}
+		}
+
+		@Test
+		@DisplayName("다리 길이 한글을 입력하는 경우 테스트")
+		void inputBridgeSizeKorean() {
+			// given
+			List<String> bridgeSizes = List.of("ㄱ", "1ㅂ", "2ㅃ", "1ㄲ", "ㅏ2", "ㅎ3", "퉯1", "개", "댤ㅇㅣ얽2", "다리길이");
+
+			for (String bridgeSize : bridgeSizes) {
+				// when, then
+				readLine(bridgeSize);
+				assertThatThrownBy(() -> inputView.readBridgeSize()).isInstanceOf(IllegalArgumentException.class);
+			}
+		}
+
+		@Test
+		@DisplayName("다리 길이 특수문자를 입력하는 경우 테스트")
+		void inputBridgeSizeSpecialCharacter() {
+			// given
+			List<String> bridgeSizes = List.of("!", "1%", "2/", "/3", ")%", "2");
+
+			for (String bridgeSize : bridgeSizes) {
+				// when, then
+				readLine(bridgeSize);
+				assertThatThrownBy(() -> inputView.readBridgeSize()).isInstanceOf(IllegalArgumentException.class);
+			}
+		}
+
+		@Test
+		@DisplayName("다리 길이 공백을 입력하는 경우 테스트")
+		void inputBridgeSizeSpaceBar() {
+			// given
+			List<String> bridgeSizes = List.of("1 ", " 9", "2 0", "1 2", " ", "  ");
+
+			for (String bridgeSize : bridgeSizes) {
+				// when, then
+				readLine(bridgeSize);
+				assertThatThrownBy(() -> inputView.readBridgeSize()).isInstanceOf(IllegalArgumentException.class);
+			}
+		}
+
+		@Test
+		@DisplayName("다리 길이 3~20을 벗어나는 입력 경우 테스트")
+		void inputBridgeSizeOutOfRange() {
+			// given
+			List<String> bridgeSizes = List.of("2", "21", "1", "111", "22", "23");
+
+			for (String bridgeSize : bridgeSizes) {
+				// when, then
+				readLine(bridgeSize);
+				assertThatThrownBy(() -> inputView.readBridgeSize()).isInstanceOf(IllegalArgumentException.class);
+			}
+		}
 	}
 
 	private void readLine(String bridgeSize) {
