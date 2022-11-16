3c35559b808b646d878a72a7faa6e1bc5b06a1e8
hyun-seung <ardimento22@naver.com>
2022-11-17 00:54:58 +0900

    [FEAT] 다리 지도를 출력한다.

--- src/main/java/bridge/View/OutputView.java
+++ src/main/java/bridge/View/OutputView.java
@@ -1,11 +1,17 @@
 package bridge.View;
 
+import java.util.List;
+
 /**
  * 사용자에게 게임 진행 상황과 결과를 출력하는 역할을 한다.
  */
 public class OutputView {
 
     private static final String GAME_START = "다리 건너기 게임을 시작합니다.";
+    private static final String MAP_START_PART = "[ ";
+    private static final String MAP_MIDDLE_PART = " | ";
+    private static final String MAP_END_PART = " ]";
+    private static final String RESULT = "최종 게임 결과";
 
     public void printStart() {
         System.out.println(GAME_START);
@@ -16,7 +22,14 @@ public class OutputView {
      * <p>
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void printMap() {
+    public void printMap(List<String> map) {
+        System.out.print(MAP_START_PART);
+        for (int i = 0; i < map.size() - 1; i++) {
+            System.out.print(map.get(i));
+            System.out.print(MAP_MIDDLE_PART);
+        }
+        System.out.print(map.get(map.size() - 1));
+        System.out.println(MAP_END_PART);
     }
 
     /**
