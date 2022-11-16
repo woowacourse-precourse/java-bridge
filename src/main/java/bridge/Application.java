3c50d781e39cb73dd0c89eef9f68998b24833a69
nswon <swnam0729@gmail.com>
2022-11-17 07:50:28 +0900

    feat: 컨트롤러를 생성할 때 외부에서 서비스를 의존받는 형식으로 구현

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -1,8 +1,17 @@
 package bridge;
 
+import bridge.controller.BridgeGameController;
+import bridge.service.BridgeGame;
+
 public class Application {
 
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        BridgeGameController controller = new BridgeGameController(bridgeGame());
+        controller.play();
+    }
+
+    private static BridgeGame bridgeGame() {
+        return new BridgeGame();
     }
 }
---
+++ src/main/java/bridge/controller/BridgeGameController.java
@@ -0,0 +1,15 @@
+package bridge.controller;
+
+import bridge.service.BridgeGame;
+
+public class BridgeGameController {
+    private final BridgeGame bridgeGame;
+
+    public BridgeGameController(BridgeGame bridgeGame) {
+        this.bridgeGame = bridgeGame;
+    }
+
+    public void play() {
+
+    }
+}
