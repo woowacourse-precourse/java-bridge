fe7e5ff12c2539e259ccccf171de730898e274fc
panorama713 <yaiba713@gmail.com>
2022-11-16 19:03:07 +0900

    refactor(bridge): redBridgeSize

--- src/main/java/bridge/InputView.java
+++ src/main/java/bridge/InputView.java
@@ -6,12 +6,18 @@ import camp.nextstep.edu.missionutils.Console;
  */
 public class InputView {
 
+    private static String redBridgeSize = "다리의 길이를 입력해주세요.";
+    private static String redBridgeSizeError = "[ERROR] 다리 길이는 3부터 20 사이의 숫자여야 합니다.";
     /**
      * 다리의 길이를 입력받는다.
      */
     public int readBridgeSize() {
-        System.out.println("다리의 길이를 입력해주세요.");
+        System.out.println(redBridgeSize);
         int result = Integer.parseInt(Console.readLine());
+        if(result < 3 || result > 20){
+            System.out.println(redBridgeSizeError);
+            throw new IllegalArgumentException();
+        }
         return result;
     }
 
