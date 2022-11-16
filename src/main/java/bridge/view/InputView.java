58620fb4c6e7a6b09f9d61b3b03269985ae4f390
pilyang <30036534+pilyang@users.noreply.github.com>
2022-11-17 00:44:11 +0900

    feat(InputView): 다리 길이 - 숫자 입력 구현

--- src/main/java/bridge/view/InputView.java
+++ src/main/java/bridge/view/InputView.java
@@ -1,15 +1,24 @@
 package bridge.view;
 
+import camp.nextstep.edu.missionutils.Console;
+
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
  */
 public class InputView {
 
+    private static final String NUMBER_INPUT_ERROR_MESSAGE = "[ERROR] 숫자를 입력하여야 합니다.";
+
+    private static final String NUMBER_REGEX = "^\\d+$";
+
     /**
      * 다리의 길이를 입력받는다.
      */
     public int readBridgeSize() {
-        return 0;
+        String bridgeSizeInput = Console.readLine();
+        validateIsNumber(bridgeSizeInput);
+        int bridgeSize = Integer.parseInt(bridgeSizeInput);
+        return bridgeSize;
     }
 
     /**
@@ -24,5 +33,12 @@ public class InputView {
      */
     public String readGameCommand() {
         return null;
+    }
+
+    private void validateIsNumber(String input){
+        if(input.matches(NUMBER_REGEX)){
+            return;
+        }
+        throw new IllegalArgumentException(NUMBER_INPUT_ERROR_MESSAGE);
     }
 }
