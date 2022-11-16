27073d34904e4226155b80807ec9d45a07e7cbb3
Hyun-Seo Oh <ohseo8813@gmail.com>
2022-11-16 22:59:53 +0900

    <feat>(InputView.java): 게임 재시작 여부 입력 기능

--- src/main/java/bridge/view/InputView.java
+++ src/main/java/bridge/view/InputView.java
@@ -19,7 +19,8 @@ public class InputView {
     }
 
     public String readGameCommand() {
-        return null;
+        System.out.println("게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)");
+        return Console.readLine();
     }
 
     private int convertInputToInt(String input) {
