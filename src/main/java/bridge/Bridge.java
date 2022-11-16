28b52d510507cd9810ee77083d10801bd2d05b70
yj2980 <gum0945@naver.com>
2022-11-16 23:49:47 +0900

    rename : 변수명 변경

--- src/main/java/bridge/Bridge.java
+++ src/main/java/bridge/Bridge.java
@@ -3,28 +3,42 @@ package bridge;
 import java.util.Arrays;
 
 public enum Bridge {
-	UP(1, "U"),
-	DOWN(0, "D"),
-	UNKNOWN(-1, "False");
+	UP(1, "U", "O"),
+	DOWN(0, "D", " "),
+	WRONG(-1, "W", "X"),
+	UNKNOWN(2, "F", " ");
 
-	private final int index;
+	private final int first;
 	private final String order;
+	private final String result;
 
-	Bridge(int index, String order) {
-		this.index = index;
+	Bridge(int index, String order, String result) {
+		this.first = index;
 		this.order = order;
+		this.result = result;
 	}
 
-	public int getIndex() {
-		return index;
+	public int getFirst() {
+		return first;
 	}
 
 	public String getOrder() {
 		return order;
 	}
+
+	public String getResult() {
+		return result;
+	}
 	public static Bridge findOrder(int number) {
 		return Arrays.stream(values())
-				.filter(v -> v.index == number)
+				.filter(v -> v.first == number)
+				.findFirst()
+				.orElse(UNKNOWN);
+	}
+
+	public static Bridge findIndex(String shape) {
+		return Arrays.stream(values())
+				.filter(v -> v.getOrder().equals(shape))
 				.findFirst()
 				.orElse(UNKNOWN);
 	}
