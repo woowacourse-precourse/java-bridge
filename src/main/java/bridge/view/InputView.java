3117eb454177e29a7a22fc42ac57f06f88f1cfd4
Hyun-Seo Oh <ohseo8813@gmail.com>
2022-11-16 17:40:13 +0900

    feat<InputView.java>: 입력한 다리 길이가 숫자가 아니라면 예외를 반환한다

--- src/main/java/bridge/view/InputView.java
+++ src/main/java/bridge/view/InputView.java
@@ -1,21 +1,29 @@
 package bridge.view;
 
-import bridge.domain.BridgeSize;
 import camp.nextstep.edu.missionutils.Console;
 
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
  */
 public class InputView {
-
     /**
      * 다리의 길이를 입력받는다.
      */
     public int readBridgeSize() {
         System.out.println("다리 건너기 게임을 시작합니다.");
         System.out.println("다리의 길이를 입력해주세요.");
-        String size = Console.readLine();
-        return Integer.parseInt(size);
+        return convertInputToInt(Console.readLine());
+    }
+
+    private int convertInputToInt(String input) {
+        if (isNotNumber(input)) {
+            throw new IllegalArgumentException("숫자를 입력해주세요. 입력값 : " + input);
+        }
+        return Integer.parseInt(input);
+    }
+
+    private boolean isNotNumber(String input) {
+        return !input.chars().allMatch(Character::isDigit);
     }
 
     /**
