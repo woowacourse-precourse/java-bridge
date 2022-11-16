98a02f54cc3445287c01c5e93c007bf0bcbab28a
chlwodyd9829 <chlwodyd9829@naver.com>
2022-11-16 16:48:33 +0900

    test: 이동 입력 테스트

--- src/test/java/bridge/InputViewTest.java
+++ src/test/java/bridge/InputViewTest.java
@@ -1,6 +1,5 @@
 package bridge;
 
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -10,21 +9,28 @@ import org.junit.jupiter.params.provider.ValueSource;
 import java.io.ByteArrayInputStream;
 import java.io.InputStream;
 
-import static org.assertj.core.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class InputViewTest {
     InputView inputView;
     InputStream in;
+
     @BeforeEach
-    void init(){
+    void init() {
         inputView = new InputView();
+    }
+
+    void setIn(String input) {
+        in = new ByteArrayInputStream(input.getBytes());
+        System.setIn(in);
     }
 
     @DisplayName("다리사이즈 올바른 입력 테스트")
     @Test
-    void sizeTest(){
-        String[] input = {"3","10","20"};
-        int[] result = {3,10,20};
+    void sizeTest() {
+        String[] input = {"3", "10", "20"};
+        int[] result = {3, 10, 20};
         for (int i = 0; i < 3; i++) {
             in = new ByteArrayInputStream(input[i].getBytes());
             System.setIn(in);
@@ -32,14 +38,34 @@ class InputViewTest {
             assertThat(size).isEqualTo(result[i]);
         }
     }
+
     @DisplayName("다리사이즈 에러 테스트")
     @ParameterizedTest
-    @ValueSource(strings = {"40","0","2","qwe","oo"})
-    void sizeErrorTest(String input){
-        in = new ByteArrayInputStream(input.getBytes());
-        System.setIn(in);
-        assertThatThrownBy(()->{
+    @ValueSource(strings = {"40", "0", "2", "qwe", "oo"})
+    void sizeErrorTest(String input) {
+        setIn(input);
+        assertThatThrownBy(() -> {
             inputView.readBridgeSize();
-        });
+        }).isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("이동 입력 테스트")
+    @ParameterizedTest
+    @ValueSource(strings = {"U", "D", "u", "d"})
+    void moveTest(String input) {
+        String[] result = {"U", "D"};
+        setIn(input);
+        String move = inputView.readMoving();
+        assertThat(result).contains(move);
+    }
+
+    @DisplayName("이동 입력 에러 테스트")
+    @ParameterizedTest
+    @ValueSource(strings = {"a", "x", "Q", "l", "UU", "DD"})
+    void moveErrorTest(String input) {
+        setIn(input);
+        assertThatThrownBy(() -> {
+            inputView.readMoving();
+        }).isInstanceOf(IllegalArgumentException.class);
     }
 }
