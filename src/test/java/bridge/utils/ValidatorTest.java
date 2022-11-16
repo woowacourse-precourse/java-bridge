66a7089eafa03db7d020fa27c92c5c6c0a8427bb
HwangSeokJin <ghkd3703@gmail.com>
2022-11-16 22:56:29 +0900

    test(Validator): 다리 길이 입력 시 3 이상 20 이하의 숫자를 입력하지 않을 때 예외 처리 테스트 작성

--- src/test/java/bridge/utils/ValidatorTest.java
+++ src/test/java/bridge/utils/ValidatorTest.java
@@ -1,4 +1,22 @@
-import static org.junit.jupiter.api.Assertions.*;
+package bridge.utils;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.DisplayName;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
 class ValidatorTest {
-  
+    @DisplayName("다리 길이 입력 시 숫자를 입력하지 않으면 예외 처리한다.")
+    @Test
+    void inputNonNumeric() {
+        assertThatThrownBy(() -> Validator.validateBridgeSize("q"))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("3 이상 20 이하의 숫자를 입력하지 않으면 예외 처리한다.")
+    @Test
+    void inputInvalidRange() {
+        assertThatThrownBy(() -> Validator.validateBridgeSize("21"))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
 }
