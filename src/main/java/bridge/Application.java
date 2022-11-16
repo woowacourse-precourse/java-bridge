78144669a6647b0aa4cd2177ba6319010fb78c58
JHS <wnghdtjr129@naver.com>
2022-11-16 16:08:26 +0900

    refactor(Application, GameController): 게임 실행 기능 분리

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -1,18 +1,16 @@
 package bridge;
 
+import bridge.controller.GameController;
 import bridge.view.InputView;
 import bridge.view.OutputView;
 
 public class Application {
 
     public static void main(String[] args) {
-        OutputView outputView = new OutputView();
         InputView inputView = new InputView();
+        OutputView outputView = new OutputView();
 
-        outputView.startGame();
-        int bridgeSize = inputView.readBridgeSize();
-        if (bridgeSize < 3 || bridgeSize > 20) {
-            throw new IllegalArgumentException("[ERROR] 다리 길이는 3부터 20 사이의 숫자여야 합니다.");
-        }
+        GameController gameController = new GameController(inputView, outputView);
+        gameController.start();
     }
 }
---
+++ src/main/java/bridge/controller/GameController.java
@@ -0,0 +1,22 @@
+package bridge.controller;
+
+import bridge.view.InputView;
+import bridge.view.OutputView;
+
+public class GameController {
+    private final InputView inputView;
+    private final OutputView outputView;
+
+    public GameController(InputView inputView, OutputView outputView) {
+        this.inputView = inputView;
+        this.outputView = outputView;
+    }
+
+    public void start() {
+        outputView.startGame();
+        int bridgeSize = inputView.readBridgeSize();
+        if (bridgeSize < 3 || bridgeSize > 20) {
+            throw new IllegalArgumentException("[ERROR] 다리 길이는 3부터 20 사이의 숫자여야 합니다.");
+        }
+    }
+}
