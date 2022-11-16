63530bd90b5b129841bd237f3d36627c7a64d95f
SH <gram753@naver.com>
2022-11-16 19:41:43 +0900

    feat(view): 이동할 칸을 입력하는 기능을 검증 기능 구현

--- src/main/java/bridge/view/Validator.java
+++ src/main/java/bridge/view/Validator.java
@@ -3,9 +3,16 @@ package bridge.view;
 public class Validator {
 	private static final String SIZE_ERROR = "[ERROR] 다리 길이는 3부터 20 사이의 숫자여야 합니다.";
 	public static final String INVALID_INPUT = "[ERROR] 잘못된 입력입니다.";
+
 	static void isSizeValidate(int size){
 		if (size < 3 || size > 20) {
 			throw new IllegalArgumentException(SIZE_ERROR);
+		}
+	}
+
+	static void isMoveValidate(String input){
+		if(!input.equals("U") || !input.equals("D")){
+			throw new IllegalArgumentException(INVALID_INPUT);
 		}
 	}
 }
