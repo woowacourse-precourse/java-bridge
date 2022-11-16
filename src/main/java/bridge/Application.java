bd69ff5e8de57ea61a198b1d40c5bf2e4fc1a1a8
JuHyeokParkk <hyeok5596@naver.com>
2022-11-16 20:43:45 +0900

    feat: 건널 수 있는 칸들의 기록을 이용해 다리를 생성하는 기능 구현

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -6,7 +6,43 @@ public class Application {
 
     public static void main(String[] args) {
         ArrayList<Integer> crossablePositions = makeCrossablePositions("3");
+
+
     }
+
+    static ArrayList<StringBuilder> makeBridge(char input, ArrayList<StringBuilder> sb, ArrayList<Integer> crossablePositions) {
+
+        int curIndex = sb.size() / 5;
+
+        StringBuilder upBridge = sb.get(0);
+        StringBuilder downBridge = sb.get(1);
+
+        upBridge.replace(upBridge.length() - 1, upBridge.length(), "| ");
+        downBridge.replace(downBridge.length() - 1, downBridge.length(), "| ");
+
+        if (input == 'U') {
+            downBridge.append("   ]");
+            if (crossablePositions.get(curIndex) == 0) {
+                upBridge.append(" o ]");
+
+            } else {
+                upBridge.append(" x ]");
+
+            }
+        } else {
+            upBridge.append("   ]");
+            if (crossablePositions.get(curIndex) == 1) {
+                downBridge.append(" o ]");
+            } else {
+                downBridge.append(" x ]");
+            }
+        }
+
+        return sb;
+
+
+    }
+
 
     static ArrayList<Integer> makeCrossablePositions(String input) {
 
