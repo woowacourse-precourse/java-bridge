05a220a6a68bf45bec3e730a23319aec1573f25b
seokjin8678 <rnrn8678@naver.com>
2022-11-16 16:15:30 +0900

    feat: 다리 길이 입력 검증 로직 추가

--- src/main/java/bridge/view/InputView.java
+++ src/main/java/bridge/view/InputView.java
@@ -1,18 +1,35 @@
 package bridge.view;
 
 import camp.nextstep.edu.missionutils.Console;
+import java.util.regex.Pattern;
 
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
  */
 public class InputView {
+    private final Pattern BRIDGE_SIZE_PATTERN = Pattern.compile("\\d*");
 
     /**
      * 다리의 길이를 입력받는다.
      */
     public int readBridgeSize() {
         String input = receiveInput();
-        return Integer.parseInt(input);
+        validateNumber(input);
+        int length = Integer.parseInt(input);
+        validateLength(length);
+        return length;
+    }
+
+    private void validateLength(int length) {
+        if (length < 3 || length > 20) {
+            throw new IllegalArgumentException("[ERROR] 다리 길이는 3부터 20 사이의 숫자여야 합니다.");
+        }
+    }
+
+    private void validateNumber(String input) {
+        if (!BRIDGE_SIZE_PATTERN.matcher(input).matches()) {
+            throw new IllegalArgumentException("[ERROR] 숫자만 입력해주세요.");
+        }
     }
 
     /**
