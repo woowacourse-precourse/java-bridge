4bf03992d9869a17a7f208b1d25be569a24725a1
jh868 <2weeks9947@naver.com>
2022-11-16 23:48:45 +0900

    feat: 다리 길이 입력 구현

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -1,8 +1,10 @@
 package bridge;
 
 public class Application {
+    static InputView inputView = new InputView();
 
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        inputView.readBridgeSize();
     }
 }
--- src/main/java/bridge/InputView.java
+++ src/main/java/bridge/InputView.java
@@ -1,15 +1,19 @@
 package bridge;
 
+import camp.nextstep.edu.missionutils.Console;
+
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
  */
 public class InputView {
+    private final String start_input_BridgeSize = "다리 건너기 게임을 시작합니다.\n\n다리의 길이를 입력해주세요.\n";
 
     /**
      * 다리의 길이를 입력받는다.
      */
     public int readBridgeSize() {
-        return 0;
+        System.out.printf(start_input_BridgeSize);
+        return convertToInt_Validate(Console.readLine());
     }
 
     /**
@@ -24,5 +28,15 @@ public class InputView {
      */
     public String readGameCommand() {
         return null;
+    }
+
+    private int convertToInt_Validate(String input){
+        if (!input.matches("^[0-9]*$")){
+            throw new IllegalArgumentException();
+        }
+        if (Integer.parseInt(input) < 3 || Integer.parseInt(input) > 20){
+            throw new IllegalArgumentException();
+        }
+        return Integer.parseInt(input);
     }
 }
