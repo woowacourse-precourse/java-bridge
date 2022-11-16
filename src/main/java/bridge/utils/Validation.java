c2e298cd0acf920807f6b047f87bab743407ef3a
youhobin <ghdb132@naver.com>
2022-11-16 18:36:56 +0900

    feat(Validation) : 입력 받은 다리 길이의 유효성 검사

--- src/main/java/bridge/utils/Validation.java
+++ src/main/java/bridge/utils/Validation.java
@@ -2,23 +2,29 @@ package bridge.utils;
 
 public class Validation {
     public static int validate(String bridgeSize) {
-        int bridge = validateNumber(bridgeSize);
-        return validateSize(bridge);
+        int bridgeSizeNumber = validateNumber(bridgeSize);
+        return validateSize(bridgeSizeNumber);
     }
 
-    private static int validateSize(int bridgeSize) {
+    private static int validateSize(int bridgeSizeNumber) {
         try {
-            if (bridgeSize < 3 || bridgeSize > 20) {
+            if (bridgeSizeNumber < 3 || bridgeSizeNumber > 20) {
                 throw new IllegalArgumentException("[ERROR] 다리의 길이는 3이상 20이하여야 합니다.");
             }
         } catch (IllegalArgumentException error) {
             System.out.println(error.getMessage());
         }
 
-        return bridgeSize;
+        return bridgeSizeNumber;
     }
 
     private static int validateNumber(String bridgeSize) {
-        return Integer.parseInt(bridgeSize);
+        int bridgeSizeNumber = 0;
+        try {
+            bridgeSizeNumber = Integer.parseInt(bridgeSize);
+        } catch (IllegalArgumentException error) {
+            System.out.println(error.getMessage());
+        }
+        return bridgeSizeNumber;
     }
 }
