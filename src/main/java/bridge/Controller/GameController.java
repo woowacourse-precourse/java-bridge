74fb9dbef057b65f2bb4fda2ef0b96535d5636cb
antaeho <an981022@naver.com>
2022-11-16 23:03:50 +0900

    Feat: BridgeGame 의 move 구현

--- src/main/java/bridge/Controller/GameController.java
+++ src/main/java/bridge/Controller/GameController.java
@@ -10,6 +10,8 @@ import java.util.List;
 
 public class GameController {
 
+    private boolean KEEP_GOING = true;
+
     InputView inputView;
     OutputView outputView;
     BridgeGame bridgeGame;
@@ -24,9 +26,14 @@ public class GameController {
     }
 
     public void run() {
+        int i = 0;
         int bridgeSize = inputView.readBridgeSize();
         List<String> bridge = bridgeMaker.makeBridge(bridgeSize);
-        String nextMove = inputView.readMoving();
-        System.out.println("nextMove = " + nextMove);
+        while(KEEP_GOING && i < bridgeSize){
+            String nextMove = inputView.readMoving();
+            String moveResult = bridgeGame.move(bridge.get(i), nextMove);
+            System.out.println("moveResult = " + moveResult);
+            i++;
+        }
     }
 }
--- src/main/java/bridge/Model/BridgeGame.java
+++ src/main/java/bridge/Model/BridgeGame.java
@@ -1,16 +1,29 @@
 package bridge.Model;
 
+import bridge.View.OutputView;
+
 /**
  * 다리 건너기 게임을 관리하는 클래스
  */
 public class BridgeGame {
 
+    OutputView outputView;
+
+    public BridgeGame() {
+        this.outputView = new OutputView();
+    }
+
     /**
      * 사용자가 칸을 이동할 때 사용하는 메서드
      * <p>
      * 이동을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
+     *
+     * @return 해당 움직임의 성공/ 실패를 전달한다.
      */
-    public void move() {
+    public String move(String bridgeShape, String nextMove) {
+        if(bridgeShape.matches(nextMove))
+            return "O";
+        return "X";
     }
 
     /**
