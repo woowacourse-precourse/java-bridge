d972b913c68665746c4a90f90cf12d088c0c0230
H4R1B0 <qkrguswns25@naver.com>
2022-11-16 17:59:16 +0900

    feat: input to move

--- src/main/java/bridge/Validation.java
+++ src/main/java/bridge/Validation.java
@@ -11,4 +11,9 @@ public class Validation {
         if (input < 3 || input > 20)
             throw new IllegalArgumentException(ErrorMessage.ERROR_MESSAGE_INPUT_NOT_BETWEEN_3_AND_20.getMessage());
     }
+
+    public static void isInputUpOrDown(String input) {
+        if (!input.equals("U") && !input.equals("D"))
+            throw new IllegalArgumentException(ErrorMessage.ERROR_MESSAGE_INPUT_UP_OR_DOWN.getMessage());
+    }
 }
