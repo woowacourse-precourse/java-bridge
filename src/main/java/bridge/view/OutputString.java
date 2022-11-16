7eb0b27cc59b50509a333bd681fc534028796663
ywj9811 <ywj9811@naver.com>
2022-11-17 05:11:13 +0900

    feat: OutputString Enum클래스
    출력할 수 있도록 getter추가

--- src/main/java/bridge/view/OutputString.java
+++ src/main/java/bridge/view/OutputString.java
@@ -4,12 +4,12 @@ public enum OutputString {
     START("다리 건너기 게임을 시작합니다."),
     INPUT("다리의 길이를 입력해주세요."),
     MOVE("이동할 칸을 선택해주세요. (위 : U, 아래 : D"),
-    RESULT("게임 성공 여부 : "),
-    TRY("총 시도한 횟수 : "),
+    RESULT("게임 성공 여부: "),
+    TRY("총 시도한 횟수: "),
     SUCCESS("성공"),
     FAIL("실패"),
     RETRY("게임을 다시 시도할지 여부를 입력해주세요. (재시도 : R, 종료 : Q)"),
-    SUCCESS_OR_NOT("게임 성공 여부");
+    LAST("최종 게임 결과");
 
 
     private String print;
@@ -18,4 +18,7 @@ public enum OutputString {
         this.print = print;
     }
 
+    public String getPrint() {
+        return print;
+    }
 }
