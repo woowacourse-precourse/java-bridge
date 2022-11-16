a635d99fa4ef9968401317a65bfee2d902fbe236
JunJI97 <bigwkddo@gmail.com>
2022-11-17 05:50:34 +0900

    [기능구현] main문 작성 및 게임 입출력 기능 구현

--- src/main/java/bridge/BridgeGame.java
+++ src/main/java/bridge/BridgeGame.java
@@ -4,7 +4,16 @@ package bridge;
  * 다리 건너기 게임을 관리하는 클래스
  */
 public class BridgeGame {
+    BridgeNumberGenerator bridgeNumberGenerator = new BridgeRandomNumberGenerator();
+    BridgeMaker bridge = new BridgeMaker(bridgeNumberGenerator);
+    InputView input = new InputView();
 
+    public void run() {
+        int lengthBridge = input.readBridgeSize();
+        // make Bridge
+//        bridge.makeBridge(3);
+
+    }
     /**
      * 사용자가 칸을 이동할 때 사용하는 메서드
      * <p>
@@ -18,6 +27,7 @@ public class BridgeGame {
      * <p>
      * 재시작을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void retry() {
+    public boolean retry() {
+        return true;
     }
 }
