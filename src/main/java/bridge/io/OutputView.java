e033cfe4070772eee52fa674b93ec32516d7004b
JaeWon Choi <87649850+codesver@users.noreply.github.com>
2022-11-16 20:42:50 +0900

    feat(OutputView): implement method printGuide

--- src/main/java/bridge/io/OutputView.java
+++ src/main/java/bridge/io/OutputView.java
@@ -9,6 +9,10 @@ package bridge.io;
  */
 public class OutputView {
 
+    public void printGuide(String guide) {
+        System.out.println(guide);
+    }
+
     /**
      * 현재까지 이동한 다리의 상태를 정해진 형식에 맞춰 출력한다.
      * <p>
