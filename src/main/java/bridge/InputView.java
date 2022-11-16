56ab277df52387efab6dc1ffda2de5f5d4edf0e0
suheonjoo <suheonj95@naver.com>
2022-11-16 17:28:29 +0900

    build: 자바 컨벤션 환경 설정 완료

--- src/main/java/bridge/InputView.java
+++ src/main/java/bridge/InputView.java
@@ -5,24 +5,24 @@ package bridge;
  */
 public class InputView {
 
-    /**
-     * 다리의 길이를 입력받는다.
-     */
-    public int readBridgeSize() {
-        return 0;
-    }
+	/**
+	 * 다리의 길이를 입력받는다.
+	 */
+	public int readBridgeSize() {
+		return 0;
+	}
 
-    /**
-     * 사용자가 이동할 칸을 입력받는다.
-     */
-    public String readMoving() {
-        return null;
-    }
+	/**
+	 * 사용자가 이동할 칸을 입력받는다.
+	 */
+	public String readMoving() {
+		return null;
+	}
 
-    /**
-     * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
-     */
-    public String readGameCommand() {
-        return null;
-    }
+	/**
+	 * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
+	 */
+	public String readGameCommand() {
+		return null;
+	}
 }
