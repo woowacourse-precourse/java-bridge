49f793b13482c43900f9aa06316ac636e156e983
ywj9811 <ywj9811@naver.com>
2022-11-17 01:13:56 +0900

    feat: OutputView 클래스
    다리를 출력할 수 있도록 메소드 준비
    처음 혹은 마지막 과 중간의 출력문 작성

--- src/main/java/bridge/OutputView.java
+++ src/main/java/bridge/OutputView.java
@@ -1,16 +1,74 @@
 package bridge;
 
+import java.util.List;
+
 /**
  * 사용자에게 게임 진행 상황과 결과를 출력하는 역할을 한다.
  */
 public class OutputView {
+    private static String upBridge = "[]";
+    private static String downBridge = "[]";
 
     /**
      * 현재까지 이동한 다리의 상태를 정해진 형식에 맞춰 출력한다.
      * <p>
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void printMap() {
+
+    /**
+     * tf가 true이면 한칸 더 앞으로 가야함
+     * [ O ]
+     * [   ]
+     * [ O |   ]
+     * [   | X ]
+     * [ O |   |   ]
+     * [   | O | O ]
+     */
+    public void printMap(String input, boolean tf) {
+        if (tf == true) {
+            printTrue(input);
+            return;
+        }
+        printFalse(input);
+    }
+
+    public void printTrue(String input) {
+        if (input.equals("U")) {
+            upBridge.replace("]", "| O ]");
+            downBridge.replace("]", "|   ]");
+            return;
+        }
+        if (input.equals("D")) {
+            upBridge.replace("]", "|   ]");
+            downBridge.replace("]", "| O ]");
+            return;
+        }
+    }
+
+    public void printFalse(String input) {
+        if (input.equals("U")) {
+            upBridge.replace("]", "| O ]");
+            downBridge.replace("]", "|   ]");
+            return;
+        }
+        if (input.equals("D")) {
+            upBridge.replace("]", "|   ]");
+            downBridge.replace("]", "| X ]");
+            return;
+        }
+    }
+
+    public void printFirstOrLast(String input) {
+        if (input.equals("U")) {
+            upBridge.replace("]", " O ]");
+            downBridge.replace("]", "   ]");
+            return;
+        }
+        if (input.equals("D")) {
+            upBridge.replace("]", "   ]");
+            downBridge.replace("]", " O ]");
+            return;
+        }
     }
 
     /**
