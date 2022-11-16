8e6d2b2955202d52ab26862cb9dfe120b6ea3386
yeonhee <ydol2@naver.com>
2022-11-17 04:10:31 +0900

    refactor(BridgeMarker) : 다리생성 for문 stream으로 변경

--- src/main/java/bridge/BridgeMaker.java
+++ src/main/java/bridge/BridgeMaker.java
@@ -2,6 +2,7 @@ package bridge;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.IntStream;
 
 /**
  * 다리의 길이를 입력 받아서 다리를 생성해주는 역할을 한다.
@@ -20,9 +21,8 @@ public class BridgeMaker {
      */
     public List<String> makeBridge(int size) {
         List<String> bridge = new ArrayList<>();
-        for(int i=0;i<size;i++){
-            bridge.add(upOrDown(bridgeNumberGenerator.generate()));
-        }
+        IntStream.range(0,size)
+                .forEach(index -> bridge.add(upOrDown(bridgeNumberGenerator.generate())));
         return bridge;
     }
 
