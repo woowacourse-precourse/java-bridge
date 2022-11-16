e54c58ed071ae5ed1f2b453ebd407611287669e0
eunbii0213 <eunbii0213@naver.com>
2022-11-17 01:08:41 +0900

    feat: 총 시도한 횟수 안내 메시지 설정 설정

--- src/main/java/bridge/View/GuideMessageView.java
+++ src/main/java/bridge/View/GuideMessageView.java
@@ -7,7 +7,8 @@ public enum GuideMessageView {
 	RE_OR_END_GUIDE_MESSAGE("게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)"),
 	GAME_RESULT_GUIDE_MESSAGE("최종 게임 결과"),
 	GAME_SUCCESS_GUIDE_MESSAGE("게임 성공 여부: 성공"),
-	GAME_FAIL_GUIDE_MESSAGE("게임 성공 여부: 실패");
+	GAME_FAIL_GUIDE_MESSAGE("게임 성공 여부: 실패"),
+	TRY_COUNT_GUIDE_MESSAGE("총 시도한 횟수: ");
 
 	private String message;
 	GuideMessageView(String message){
