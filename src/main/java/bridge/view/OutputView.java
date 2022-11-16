230947bd104852ef0f2ed7923928c8162019d686
sjk <80821744+ShinjaeKwon@users.noreply.github.com>
2022-11-16 20:16:34 +0900

    feat : 게임의 최종 결과 출력 기능 추가

--- src/main/java/bridge/view/OutputView.java
+++ src/main/java/bridge/view/OutputView.java
@@ -17,6 +17,15 @@ public class OutputView {
 		System.out.println(map);
 	}
 
+	/**
+	 * 게임의 최종 결과를 정해진 형식에 맞춰 출력한다.
+	 * <p>
+	 * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
+	 */
+	public void printResult(List<Integer> bridge, List<Integer> inputMoveCommands) {
+		printMap(bridge, inputMoveCommands);
+	}
+
 	private String mapDraw(List<Integer> bridge, List<Integer> inputMoveCommands) {
 		StringBuilder upSb = new StringBuilder();
 		StringBuilder downSb = new StringBuilder();
@@ -27,14 +36,6 @@ public class OutputView {
 		}
 		lastPositionDraw(upSb, downSb);
 		return upSb.append('\n').append(downSb).toString();
-	}
-
-	/**
-	 * 게임의 최종 결과를 정해진 형식에 맞춰 출력한다.
-	 * <p>
-	 * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
-	 */
-	public void printResult() {
 	}
 
 	private void lastPositionDraw(StringBuilder upSb, StringBuilder downSb) {
