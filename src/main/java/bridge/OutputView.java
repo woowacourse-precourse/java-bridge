a545686b4b0045354c741381b9c0610f637c2137
H4R1B0 <qkrguswns25@naver.com>
2022-11-17 00:15:47 +0900

    feat(OutputView): edit printResult

--- src/main/java/bridge/OutputView.java
+++ src/main/java/bridge/OutputView.java
@@ -10,7 +10,7 @@ import java.util.stream.Collectors;
  */
 public class OutputView {
     private static final String FINAL_GAME_RESULT = "최종 게임 결과";
-    private static final String GAME_WIN_RESULT = "게임 성공 여부: 성공\n총 시도한 횟수: %d";
+    private static final String GAME_WIN_RESULT = "게임 성공 여부: %s\n총 시도한 횟수: %d";
 
     /**
      * 현재까지 이동한 다리의 상태를 정해진 형식에 맞춰 출력한다.
@@ -45,9 +45,9 @@ public class OutputView {
      * <p>
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void printResult(List<Bridge> bridges, int count) {
+    public void printResult(List<Bridge> bridges, String gameResult, int count) {
         System.out.println(FINAL_GAME_RESULT);
         printMap(bridges);
-        System.out.println(String.format(GAME_WIN_RESULT, count));
+        System.out.printf(GAME_WIN_RESULT, gameResult, count);
     }
 }
