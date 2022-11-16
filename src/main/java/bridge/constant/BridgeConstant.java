49595bd805df98965c16f9f5fc6d207323c7a448
Hyun-Seo Oh <ohseo8813@gmail.com>
2022-11-16 23:49:47 +0900

    <feat>(Validator.java): 게임 재시작 여부 메시지 검증
    validateMoveMeesage와 중복되는 부분 존재해 추출해서 메소드 재사용

--- src/main/java/bridge/constant/BridgeConstant.java
+++ src/main/java/bridge/constant/BridgeConstant.java
@@ -5,6 +5,9 @@ public class BridgeConstant {
     public static final int MAX_SIZE = 20;
     public static final int MIN_SIZE = 3;
 
+    //enum 사용도 괜찮은듯!
     public static final String UPPER_BLOCK = "U";
     public static final String LOWER_BLOCK = "D";
+    public static final String RESTART_GAME = "R";
+    public static final String CLOSE_GAME = "Q";
 }
--- src/main/java/bridge/exception/Validator.java
+++ src/main/java/bridge/exception/Validator.java
@@ -1,9 +1,15 @@
 package bridge.exception;
 
+import static bridge.constant.BridgeConstant.CLOSE_GAME;
 import static bridge.constant.BridgeConstant.LOWER_BLOCK;
 import static bridge.constant.BridgeConstant.MAX_SIZE;
 import static bridge.constant.BridgeConstant.MIN_SIZE;
+import static bridge.constant.BridgeConstant.RESTART_GAME;
 import static bridge.constant.BridgeConstant.UPPER_BLOCK;
+import static java.text.MessageFormat.format;
+
+import java.util.Arrays;
+import java.util.function.Predicate;
 
 public class Validator {
 
@@ -12,15 +18,26 @@ public class Validator {
     }
 
     public void validateMoveMessage(String message) {
-        if (!message.equals(UPPER_BLOCK) && !message.equals(LOWER_BLOCK)) {
-            throw new IllegalArgumentException("입력 값은 " + UPPER_BLOCK + "혹은 " + LOWER_BLOCK + "이여야 합니다." +
-                    "입력값 : " + message);
+        validateInputIsCorrectValues(message, UPPER_BLOCK, LOWER_BLOCK);
+    }
+
+    public void validateGameRestartMessage(String message) {
+        validateInputIsCorrectValues(message, RESTART_GAME, CLOSE_GAME);
+    }
+
+    private void validateInputIsCorrectValues(String inputMessage,
+                                              String... correctValues) {
+        if (Arrays.stream(correctValues)
+                .noneMatch(Predicate.isEqual(inputMessage))) {
+            throw new IllegalArgumentException(format("{0} 이외의 값은 허용되지 않습니다. 입력값 : {1}",
+                    String.join(",", correctValues), inputMessage));
         }
     }
 
     private void validateBridgeSizeRange(int size) {
         if (size > MAX_SIZE || size < MIN_SIZE) {
-            throw new IllegalArgumentException("다리 길이는 3 이상, 20 이하여야합니다. 입력값 : " + size);
+            throw new IllegalArgumentException(format("다리 길이는 {0} 이상, {1} 이하여야 합니다. " + "입력값 : {2}",
+                    MIN_SIZE, MAX_SIZE, size));
         }
     }
 }
