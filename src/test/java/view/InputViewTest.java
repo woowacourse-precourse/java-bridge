2c0418b39f7dee3557c267d4f743a872d5a026ea
June <68291395+june-777@users.noreply.github.com>
2022-11-16 22:43:45 +0900

    refactor(InputViewTest): 기능별 테스트 계층화

--- src/test/java/view/InputViewTest.java
+++ src/test/java/view/InputViewTest.java
@@ -6,8 +6,6 @@ import java.io.ByteArrayInputStream;
 import java.io.InputStream;
 import java.util.List;
 
-import org.assertj.core.api.Assertions;
-
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
@@ -17,95 +15,117 @@ public class InputViewTest {
 	InputView inputView = new InputView();
 
 	@Nested
-	@DisplayName("성공 테스트 클래스")
-	class SuccessTest {
+	@DisplayName("다리 길이 입력 기능 테스트 클래스")
+	class ReadBridgeSizeTest {
 
-		@Test
-		@DisplayName("다리 길이 3에서 20을 입력하는 경우 테스트")
-		void inputBridgeSizeThreeToTwenty() {
-			// given
-			List<String> bridgeSizes = List.of("3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15",
-					"16", "17", "18", "19", "20");
+		@Nested
+		@DisplayName("성공 테스트 클래스")
+		class SuccessTest {
 
-			for (String bridgeSize : bridgeSizes) {
-				// when
-				int expectedBridgeSize = Integer.parseInt(bridgeSize);
-				readLine(bridgeSize);
-				int resultBridgeSize = inputView.readBridgeSize();
+			@Test
+			@DisplayName("다리 길이 3에서 20을 입력하는 경우 테스트")
+			void inputBridgeSizeThreeToTwenty() {
+				// given
+				List<String> bridgeSizes = List.of("3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14",
+						"15", "16", "17", "18", "19", "20");
 
-				// then
-				assertThat(resultBridgeSize).isEqualTo(expectedBridgeSize);
+				for (String bridgeSize : bridgeSizes) {
+					// when
+					int expectedBridgeSize = Integer.parseInt(bridgeSize);
+					readLine(bridgeSize);
+					int resultBridgeSize = inputView.readBridgeSize();
+
+					// then
+					assertThat(resultBridgeSize).isEqualTo(expectedBridgeSize);
+				}
+			}
+		}
+
+		@Nested
+		@DisplayName("예외상황 테스트 클래스")
+		class FailTest {
+
+			@Test
+			@DisplayName("다리 길이 영어를 입력하는 경우 테스트")
+			void inputBridgeSizeEnglish() {
+				// given
+				List<String> bridgeSizes = List.of("3a", "2z0", "9e", "s10", "ra");
+
+				for (String bridgeSize : bridgeSizes) {
+					// when, then
+					readLine(bridgeSize);
+					assertThatThrownBy(() -> inputView.readBridgeSize()).isInstanceOf(IllegalArgumentException.class);
+				}
+			}
+
+			@Test
+			@DisplayName("다리 길이 한글을 입력하는 경우 테스트")
+			void inputBridgeSizeKorean() {
+				// given
+				List<String> bridgeSizes = List.of("ㄱ", "1ㅂ", "2ㅃ", "1ㄲ", "ㅏ2", "ㅎ3", "퉯1", "개", "댤ㅇㅣ얽2", "다리길이");
+
+				for (String bridgeSize : bridgeSizes) {
+					// when, then
+					readLine(bridgeSize);
+					assertThatThrownBy(() -> inputView.readBridgeSize()).isInstanceOf(IllegalArgumentException.class);
+				}
+			}
+
+			@Test
+			@DisplayName("다리 길이 특수문자를 입력하는 경우 테스트")
+			void inputBridgeSizeSpecialCharacter() {
+				// given
+				List<String> bridgeSizes = List.of("!", "1%", "2/", "/3", ")%", "2");
+
+				for (String bridgeSize : bridgeSizes) {
+					// when, then
+					readLine(bridgeSize);
+					assertThatThrownBy(() -> inputView.readBridgeSize()).isInstanceOf(IllegalArgumentException.class);
+				}
+			}
+
+			@Test
+			@DisplayName("다리 길이 공백을 입력하는 경우 테스트")
+			void inputBridgeSizeSpaceBar() {
+				// given
+				List<String> bridgeSizes = List.of("1 ", " 9", "2 0", "1 2", " ", "  ");
+
+				for (String bridgeSize : bridgeSizes) {
+					// when, then
+					readLine(bridgeSize);
+					assertThatThrownBy(() -> inputView.readBridgeSize()).isInstanceOf(IllegalArgumentException.class);
+				}
+			}
+
+			@Test
+			@DisplayName("다리 길이 3~20을 벗어나는 입력 경우 테스트")
+			void inputBridgeSizeOutOfRange() {
+				// given
+				List<String> bridgeSizes = List.of("2", "21", "1", "111", "22", "23");
+
+				for (String bridgeSize : bridgeSizes) {
+					// when, then
+					readLine(bridgeSize);
+					assertThatThrownBy(() -> inputView.readBridgeSize()).isInstanceOf(IllegalArgumentException.class);
+				}
 			}
 		}
 	}
 
 	@Nested
-	@DisplayName("예외상황 테스트 클래스")
-	class FailTest {
+	@DisplayName("이동할 칸 입력 기능 테스트")
+	class ReadMovingTest {
 
-		@Test
-		@DisplayName("다리 길이 영어를 입력하는 경우 테스트")
-		void inputBridgeSizeEnglish() {
-			// given
-			List<String> bridgeSizes = List.of("3a", "2z0", "9e", "s10", "ra");
+		@Nested
+		@DisplayName("성공 테스트 클래스")
+		class SuccessTest {
 
-			for (String bridgeSize : bridgeSizes) {
-				// when, then
-				readLine(bridgeSize);
-				assertThatThrownBy(() -> inputView.readBridgeSize()).isInstanceOf(IllegalArgumentException.class);
-			}
 		}
 
-		@Test
-		@DisplayName("다리 길이 한글을 입력하는 경우 테스트")
-		void inputBridgeSizeKorean() {
-			// given
-			List<String> bridgeSizes = List.of("ㄱ", "1ㅂ", "2ㅃ", "1ㄲ", "ㅏ2", "ㅎ3", "퉯1", "개", "댤ㅇㅣ얽2", "다리길이");
+		@Nested
+		@DisplayName("에외상황 테스트 클래스")
+		class FailTest {
 
-			for (String bridgeSize : bridgeSizes) {
-				// when, then
-				readLine(bridgeSize);
-				assertThatThrownBy(() -> inputView.readBridgeSize()).isInstanceOf(IllegalArgumentException.class);
-			}
-		}
-
-		@Test
-		@DisplayName("다리 길이 특수문자를 입력하는 경우 테스트")
-		void inputBridgeSizeSpecialCharacter() {
-			// given
-			List<String> bridgeSizes = List.of("!", "1%", "2/", "/3", ")%", "2");
-
-			for (String bridgeSize : bridgeSizes) {
-				// when, then
-				readLine(bridgeSize);
-				assertThatThrownBy(() -> inputView.readBridgeSize()).isInstanceOf(IllegalArgumentException.class);
-			}
-		}
-
-		@Test
-		@DisplayName("다리 길이 공백을 입력하는 경우 테스트")
-		void inputBridgeSizeSpaceBar() {
-			// given
-			List<String> bridgeSizes = List.of("1 ", " 9", "2 0", "1 2", " ", "  ");
-
-			for (String bridgeSize : bridgeSizes) {
-				// when, then
-				readLine(bridgeSize);
-				assertThatThrownBy(() -> inputView.readBridgeSize()).isInstanceOf(IllegalArgumentException.class);
-			}
-		}
-
-		@Test
-		@DisplayName("다리 길이 3~20을 벗어나는 입력 경우 테스트")
-		void inputBridgeSizeOutOfRange() {
-			// given
-			List<String> bridgeSizes = List.of("2", "21", "1", "111", "22", "23");
-
-			for (String bridgeSize : bridgeSizes) {
-				// when, then
-				readLine(bridgeSize);
-				assertThatThrownBy(() -> inputView.readBridgeSize()).isInstanceOf(IllegalArgumentException.class);
-			}
 		}
 	}
 
