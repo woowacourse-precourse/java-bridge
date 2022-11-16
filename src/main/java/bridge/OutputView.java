f7531739a7a861f5857c7a4cc198631dd5d93c2b
jinny-l <jinny.l.dev@gmail.com>
2022-11-16 17:20:11 +0900

    feat(OutputView): 게임 시작 문구 출력 기능 구현

--- src/main/java/bridge/OutputView.java
+++ src/main/java/bridge/OutputView.java
@@ -5,6 +5,18 @@ package bridge;
  */
 public class OutputView {
 
+    private static final String GAME_START_MESSAGE = "다리 건너기 게임을 시작합니다.";
+    private static final String PRINT_MAP_MESSAGE = "최종 게임 결과";
+    private static final String GAME_RESULT_MESSAGE = "게임 성공 여부: ";
+    private static final String NUMBER_OF_TRIALS_MESSAGE = "총 시도한 횟수: ";
+    private static final String BRIDGE_START_DELIMITER = "[ ";
+    private static final String BRIDGE_END_DELIMITER = " ]";
+    private static final String BLOCK_DELIMITER = " |";
+
+    public void printStart() {
+        System.out.println(GAME_START_MESSAGE);
+    }
+
     /**
      * 현재까지 이동한 다리의 상태를 정해진 형식에 맞춰 출력한다.
      * <p>
