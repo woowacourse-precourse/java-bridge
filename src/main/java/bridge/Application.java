f8fbf503cecc90a2fdce68d37f6b2127908fc929
JuHyeokParkk <hyeok5596@naver.com>
2022-11-16 19:58:27 +0900

    feat: 건널 수 있는 다리의 칸을 입력 받은 길이만큼 생성해 저장하는 기능 구현

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -1,8 +1,29 @@
 package bridge;
 
+import java.util.ArrayList;
+
 public class Application {
 
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        ArrayList<Integer> crossablePositions = makeCrossablePositions("3");
+    }
+
+    static ArrayList<Integer> makeCrossablePositions(String input) {
+
+        int length;
+
+        try {
+            length = Integer.parseInt(input);
+        } catch (Exception e) {
+            throw new IllegalArgumentException("[ERROR] 3에서 20 사이의 숫자만 입력해주세요");
+        }
+
+        ArrayList<Integer> crossablePositions = new ArrayList<>();
+
+        for (int i = 0; i < length; i++) {
+            crossablePositions.add(new BridgeRandomNumberGenerator().generate());
+        }
+
+        return crossablePositions;
     }
 }
