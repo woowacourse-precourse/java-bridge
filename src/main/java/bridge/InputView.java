e5c99482cd6685e5db732e4d5a56a870804277c0
good9804 <good9804@naver.com>
2022-11-16 18:59:12 +0900

    feat: UI로직 생성및 다리길이를 입력받는기능및 입력받은 길이 예외를체크하는기능

--- src/main/java/bridge/InputView.java
+++ src/main/java/bridge/InputView.java
@@ -1,15 +1,37 @@
 package bridge;
 
+import camp.nextstep.edu.missionutils.Console;
+
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
  */
 public class InputView {
-
+    private static final String ERROR="[ERROR] 다리 길이는 3부터 20 사이의 숫자여야 합니다.";
     /**
      * 다리의 길이를 입력받는다.
      */
     public int readBridgeSize() {
-        return 0;
+        String Input=Console.readLine();
+        checkError(Input);
+        return Integer.parseInt(Input);
+    }
+    public void checkError(String Input){
+        isNumber(Input);
+        int size=Integer.parseInt(Input);
+        isRange(size);
+    }
+    public void isNumber(String Input){
+        try{
+            Double.parseDouble(Input);
+        } catch (NumberFormatException e){
+            throw new IllegalArgumentException(ERROR);
+        }
+    }
+
+    public void isRange(int Size){
+        if(!(Size>=3&&Size<=20)){
+            throw new IllegalArgumentException(ERROR);
+        }
     }
 
     /**
