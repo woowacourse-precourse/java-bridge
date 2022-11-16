8547a00393c2f7a8ae8cfd9ec8014787b3539804
kariskan <zkfzpf56@naver.com>
2022-11-16 18:47:14 +0900

    feat: 다리를 생성하는 makeBridge 메서드 작성
    1. bridgeNumberGenerator.generate()를 이용해 0 또는 1의 수를 받는다.
    
    2. getBridgePlace를 통해 1이면 "U"를 반환, 0이면 "D"를 반환한다.

--- src/main/java/bridge/BridgeMaker.java
+++ src/main/java/bridge/BridgeMaker.java
@@ -1,6 +1,9 @@
 package bridge;
 
+import bridge.utils.constants.Commons;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.IntStream;
 
 /**
  * 다리의 길이를 입력 받아서 다리를 생성해주는 역할을 한다.
@@ -18,6 +21,16 @@ public class BridgeMaker {
      * @return 입력받은 길이에 해당하는 다리 모양. 위 칸이면 "U", 아래 칸이면 "D"로 표현해야 한다.
      */
     public List<String> makeBridge(int size) {
-        return null;
+        List<String> bridge = new ArrayList<>();
+        IntStream.range(0, size)
+                .forEach(index -> bridge.add(getBridgePlace(bridgeNumberGenerator.generate())));
+        return bridge;
+    }
+
+    private String getBridgePlace(int randomNumber) {
+        if (randomNumber == 1) {
+            return Commons.UP.getCommand();
+        }
+        return Commons.DOWN.getCommand();
     }
 }
