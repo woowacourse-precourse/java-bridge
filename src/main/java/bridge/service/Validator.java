15164350180b1287f0a8d61d2e3b40d0f9f206bc
JaeWon Choi <87649850+codesver@users.noreply.github.com>
2022-11-16 20:51:11 +0900

    feat(Validator): build skeleton code
    Validator validates user input.

---
+++ src/main/java/bridge/service/Validator.java
@@ -0,0 +1,16 @@
+package bridge.service;
+
+public class Validator {
+
+    public void validateBridgeLength(String bridgeLength) {
+
+    }
+
+    public void validateMovement(String movement) {
+
+    }
+
+    public void validateRetryStatus(String retryStatus) {
+
+    }
+}
