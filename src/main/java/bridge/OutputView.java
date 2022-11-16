3311fb678f1db4bb62e1d81d33f4bdd17d1f2ef6
이한솔 <hansol8701@naver.com>
2022-11-17 00:55:48 +0900

    feat(OutputView): 현재까지 이동한 다리의 상태를 출력하는 기능 추가

--- src/main/java/bridge/OutputView.java
+++ src/main/java/bridge/OutputView.java
@@ -1,6 +1,7 @@
 package bridge;
 
 import java.util.List;
+import java.util.Map;
 
 /**
  * 사용자에게 게임 진행 상황과 결과를 출력하는 역할을 한다.
@@ -9,13 +10,20 @@ public class OutputView {
 
     /**
      * 현재까지 이동한 다리의 상태를 정해진 형식에 맞춰 출력한다.
-     * <p>
-     * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
+     * @param bridge 다리
+     * @param round 현재 회차
+     * @param isCorrect 현재 회차의 선택이 맞았는지 여부
      */
-    public void printMap() {
+    public void printMap(List<String> bridge, int round, boolean isCorrect) {
+        Map<String, List<String>> map = convertToMap(bridge, round, isCorrect);
+        printLine(map.get("upper"));
+        printLine(map.get("lower"));
     }
 
-    public List<Integer> convertToMap(List<String> bridge, int round, boolean isCorrect){
+    private void printLine(List<String> line) {
+    }
+
+    public Map<String, List<String>> convertToMap(List<String> bridge, int round, boolean isCorrect){
         return null;
     }
     /**
