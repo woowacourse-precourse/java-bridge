73671836d80411e4903378850abc0072435fe91f
jihyun <jihyun@jihyunui-MacBookAir.local>
2022-11-16 16:54:37 +0900

    feat(bridge): 다리길이 입력 기능, 다리이동 입력 기능, 재시작/종료 입력 기능 구현

--- src/main/java/bridge/view/InputView.java
+++ src/main/java/bridge/view/InputView.java
@@ -1,28 +1,46 @@
 package bridge.view;
 
+import camp.nextstep.edu.missionutils.Console;
+
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
  */
 public class InputView {
 
+    private static final String INPUT_BRIDGE_LENGTH = "다리의 길이를 입력해주세요.";
+    private static final String INPUT_BRIDGE_LOCATION = "이동할 칸을 선택해주세요. (위: U, 아래: D)";
+    private static final String INPUT_RESTART_QUIT = "게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)";
+    private static final String DATA_TYPE_ERR_MESSAGE = "[ERROR] 숫자가 아닙니다.";
+
     /**
      * 다리의 길이를 입력받는다.
      */
     public int readBridgeSize() {
-        return 0;
+        System.out.println(INPUT_BRIDGE_LENGTH);
+        return convertStringtoInt(Console.readLine());
     }
 
     /**
      * 사용자가 이동할 칸을 입력받는다.
      */
     public String readMoving() {
-        return null;
+        System.out.println(INPUT_BRIDGE_LOCATION);
+        return Console.readLine();
     }
 
     /**
      * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
      */
     public String readGameCommand() {
-        return null;
+        System.out.println(INPUT_RESTART_QUIT);
+        return Console.readLine();
+    }
+
+    private static int convertStringtoInt(String input) {
+        try {
+            return Integer.parseInt(input);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException(DATA_TYPE_ERR_MESSAGE);
+        }
     }
 }
