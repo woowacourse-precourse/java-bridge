81c593ce0d7108d3388e31b28bca1d00af3c35d6
SH <gram753@naver.com>
2022-11-16 19:31:54 +0900

    test: 사이즈 입력 기능 테스트

---
+++ src/test/java/bridge/view/InputViewTest.java
@@ -0,0 +1,14 @@
+package bridge.view;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class InputViewTest {
+
+	@Test
+	void 사이즈_입력_테스트(){
+		InputView input = new InputView();
+		Assertions.assertThrows(IllegalArgumentException.class,
+			() -> input.readBridgeSize());
+	}
+}
