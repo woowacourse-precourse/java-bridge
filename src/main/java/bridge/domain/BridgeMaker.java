3db702181cbc9946a81a886a66c06183d976a8a7
YSEO <1park4170@gmail.com>
2022-11-16 20:36:55 +0900

    feat<BridgeMaker> : makeBridge를 용도에 맞게 채우고, upperOrDown메서드 추가
    - makeBridge(int size) : 지역변수로 bridge란 이름의 리스트를 선언하고, for문을 통해서 size만큼 길이의 배열을 생성한다
     - upperOrDown(int randomNum) : 이때, 0또는 1의 랜덤 숫자를 만들어서 위와 아래 중 갈 수 있는 다리를 고르는데, 랜덤 숫자를 다리에 채워넣을 U 또는 D로 변경하는 역할의 메소드

--- src/main/java/bridge/domain/BridgeMaker.java
+++ src/main/java/bridge/domain/BridgeMaker.java
@@ -1,5 +1,6 @@
-package bridge;
+package bridge.domain;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -18,6 +19,22 @@ public class BridgeMaker {
      * @return 입력받은 길이에 해당하는 다리 모양. 위 칸이면 "U", 아래 칸이면 "D"로 표현해야 한다.
      */
     public List<String> makeBridge(int size) {
-        return null;
+        List<String> bridge = new ArrayList<>();
+
+        for (int i =0; i < size; i++) {
+            int generate = bridgeNumberGenerator.generate();
+            String directionIndicator = upperOrDown(generate);
+            bridge.add(directionIndicator);
+        }
+
+        return bridge;
     }
+
+    private String upperOrDown(int randomNum) { // 위인지 아래인지 고르는 메서드
+        if (randomNum == 1) {
+            return "U";
+        }
+        return "D";
+    }
+
 }
