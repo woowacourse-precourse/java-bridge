d49601dd4682c7bc24dd5c1935c3c0030a59e9e3
이한솔 <hansol8701@naver.com>
2022-11-17 02:58:01 +0900

    feat: Application main 작성

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -11,8 +11,31 @@ public class Application {
         OutputView outputView = new OutputView();
         BridgeNumberGenerator bridgeNumberGenerator = new BridgeRandomNumberGenerator();
         BridgeMaker bridgeMaker = new BridgeMaker(bridgeNumberGenerator);
+        Comparator comparator = new Comparator();
+        BridgeGame bridgeGame = new BridgeGame();
         int bridgeSize = inputView.readBridgeSize();
         List<String> bridge = bridgeMaker.makeBridge(bridgeSize);
-        inputView.readMoving();
+        int count = 0;
+        int round = 0;
+
+        boolean ongoing = true;
+
+
+        while (round <= bridgeSize && ongoing) {
+            String moving = inputView.readMoving();
+            boolean isCorrect = comparator.compare(bridge, round, moving);
+            outputView.printMap(bridge, round, isCorrect);
+            if (isCorrect){
+                round++;
+                continue;
+            }
+            String command = inputView.readGameCommand();
+            ongoing = bridgeGame.retry(command);
+            if (ongoing){
+                round = 0;
+                count++;
+            }
+        }
+        outputView.printResult(ongoing, count);
     }
 }
