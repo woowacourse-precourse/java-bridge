7c2e33abcb6a486fe4f875e3430962f33230bff5
yeonhee <ydol2@naver.com>
2022-11-17 04:22:57 +0900

    feat(BridgeGameController) : 게임 시작 관련 구현

--- src/main/java/bridge/controller/BridgeGameController.java
+++ src/main/java/bridge/controller/BridgeGameController.java
@@ -1,11 +1,25 @@
 package bridge.controller;
 
+import bridge.view.InputView;
+import bridge.view.OutputView;
+
 public class BridgeGameController {
+
+    private static final InputView inputView = new InputView();
+    private static final OutputView outputView = new OutputView();
+
+    private int bridgeSize;
+
     public static BridgeGameController create(){
         return new BridgeGameController();
     }
 
-    public void run(){
-        
+    public void start(){
+        startGame();
+    }
+
+    private void startGame(){
+        outputView.printStart();
+        bridgeSize = inputView.readBridgeSize();
     }
 }
