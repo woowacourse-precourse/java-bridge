9b9cf3fdd8c54216e850825d039e96f262d0d4ad
Hyun-Seo Oh <ohseo8813@gmail.com>
2022-11-16 22:06:51 +0900

    <feat>(OutputView.java): 다리 출력 기능

--- src/main/java/bridge/view/OutputView.java
+++ src/main/java/bridge/view/OutputView.java
@@ -1,16 +1,57 @@
 package bridge.view;
 
+import static bridge.constant.BridgeConstant.LOWER_BLOCK;
+import static bridge.constant.BridgeConstant.UPPER_BLOCK;
+
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * 사용자에게 게임 진행 상황과 결과를 출력하는 역할을 한다.
  */
 public class OutputView {
 
-    /**
-     * 현재까지 이동한 다리의 상태를 정해진 형식에 맞춰 출력한다.
-     * <p>
-     * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
-     */
-    public void printMap() {
+    private static final String BLANK = " ";
+
+    private final List<String> upperBlocks = new ArrayList<>();
+    private final List<String> lowerBlocks = new ArrayList<>();
+
+    public void printMap(List<String> bridge,
+                         boolean isCorrect) {
+        String answer = getAnswer(bridge);
+        String resultMark = getResultMark(isCorrect);
+        setBlocks(answer, resultMark);
+
+        printEachMessage(upperBlocks);
+        printEachMessage(lowerBlocks);
+    }
+
+    private String getAnswer(List<String> bridge) {
+        return bridge.get(upperBlocks.size());
+    }
+
+    private String getResultMark(boolean isCorrect) {
+        if (isCorrect) {
+            return "O";
+        }
+        return "X";
+    }
+
+    private void setBlocks(String answer, String resultMark) {
+        if (answer.equals(UPPER_BLOCK)) {
+            upperBlocks.add(resultMark);
+            lowerBlocks.add(BLANK);
+        }
+
+        if (answer.equals(LOWER_BLOCK)) {
+            upperBlocks.add(BLANK);
+            lowerBlocks.add(resultMark);
+        }
+    }
+
+    private void printEachMessage(List<String> blocks) {
+        String combinedMessage = String.join(" | ", blocks);
+        System.out.printf("[ %s ]\n", combinedMessage);
     }
 
     /**
