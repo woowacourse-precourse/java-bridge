c22aee972707d0068be6c329aef7adf885b9180b
junpakPark <112045553+junpakPark@users.noreply.github.com>
2022-11-16 22:55:36 +0900

    feat(BridgeSize): BridgeSize 객체 생성
    BridgeSize 객체 주요 기능:
    - 정수로 변환 이 가능할 시 정수 변환, 불가능할 시 에러 발생
    - 3에서 20 사이의 자연수 외의 정수 입력 시 에러 발생
    - getter 메서드

---
+++ src/main/java/bridge/BridgeSize.java
@@ -0,0 +1,33 @@
+package bridge;
+
+public class BridgeSize {
+	private final int bridgeSize;
+
+	public BridgeSize(String inputValue) {
+		int value = convertToInt(inputValue);
+		validateRange(value);
+		this.bridgeSize = value;
+	}
+
+	public int getSize() {
+		return bridgeSize;
+	}
+
+	private int convertToInt(String input) {
+		try {
+			return Integer.parseInt(input);
+		} catch (NumberFormatException e) {
+			throw new IllegalArgumentException(Error.FORMAT.getMessage());
+		}
+	}
+
+	private void validateRange(int value) {
+		if (!isInRange(value)) {
+			throw new IllegalArgumentException(Error.RANGE.getMessage());
+		}
+	}
+
+	private static boolean isInRange(int value) {
+		return 3 <= value && value <= 20;
+	}
+}
