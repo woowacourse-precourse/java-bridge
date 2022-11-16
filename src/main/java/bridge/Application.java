1b61c55f707da5bb91009b1cb00b75f11b3b8de4
HyeonjungYun <guswnd9971@gmail.com>
2022-11-16 21:18:24 +0900

    "feat(Application): main 동작 구현"

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -1,9 +1,31 @@
 package bridge;
 
+import bridge.message.PrintInput;
+
+import java.util.List;
+
 public class Application {
 
+    public static boolean outcome;
+    public static int attempt;
+
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        BridgeMaker randomBridge = new BridgeMaker(new BridgeRandomNumberGenerator());
+        InputView input = new InputView();
+        OutputView output = new OutputView();
+        PrintInput.START.print();
+        BridgeGame bridgeGame = new BridgeGame(randomBridge.makeBridge(input.readBridgeSize()));
+        attempt = 1;
+
+        boolean playngBridge = true;
+
+        while (playngBridge) {
+            playngBridge = bridgeGame.move(input.readMoving());
+            if (!bridgeGame.passHistory.contains("UX") && !bridgeGame.passHistory.contains("DX")) {
+                output.printMap(bridgeGame.passHistory);
+            }
+        }
+        output.printResult(attempt, outcome);
     }
 
     public boolean retry (List<String> passHistory) {
