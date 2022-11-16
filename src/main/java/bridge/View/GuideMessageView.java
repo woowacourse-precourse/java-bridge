3b9642be65e649faafe21d6522e85185b99172ab
eunbii0213 <eunbii0213@naver.com>
2022-11-17 01:01:12 +0900

    feat: 다리 건너기 게임 시작 안내 메시지 설정

--- src/main/java/bridge/View/GuideMessageView.java
+++ src/main/java/bridge/View/GuideMessageView.java
@@ -1,8 +1,7 @@
 package bridge.View;
 
 public enum GuideMessageView {
-
-	StartBridgeGameGuideMessage("다리 건너기 게임을 시작합니다.");
+	START_BRIDGE_GAME_GUIDE_MESSAGE("다리 건너기 게임을 시작합니다.");
 
 	private String message;
 	GuideMessageView(String message){
