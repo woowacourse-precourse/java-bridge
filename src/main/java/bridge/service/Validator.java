e5c704e51d59afeac4c142702dd362c650a7ec7e
JaeWon Choi <87649850+codesver@users.noreply.github.com>
2022-11-16 21:05:50 +0900

    feat(validatePositiveNumber): use enum message

--- src/main/java/bridge/service/Validator.java
+++ src/main/java/bridge/service/Validator.java
@@ -1,5 +1,7 @@
 package bridge.service;
 
+import bridge.constant.ErrorMessage;
+
 public class Validator {
 
     public void validateBridgeLength(String bridgeLength) {
@@ -8,7 +10,7 @@ public class Validator {
 
     private void validatePositiveNumber(String bridgeLength) {
         if (!bridgeLength.matches("[1-9][0-9]*")) {
-            throw new IllegalArgumentException("[ERROR] 다리 길이 입력값이 양의 정수가 아닙니다.");
+            throw new IllegalArgumentException(ErrorMessage.IS_NOT_POSITIVE_NUMBER.message());
         }
     }
 
