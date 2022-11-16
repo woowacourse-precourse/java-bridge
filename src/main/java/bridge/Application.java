091c33729abe586d1031d2570a3d7840c5e8cfb8
소대훈 <theogns0681@naver.com>
2022-11-16 17:57:19 +0900

    [feat] 게임 실행

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -1,8 +1,16 @@
 package bridge;
 
+import java.util.List;
+
 public class Application {
+    static InputView inputView = new InputView();
+    static BridgeMaker bridgeMaker = new BridgeMaker(new BridgeRandomNumberGenerator());
+    static BridgeGame bridgeGame = new BridgeGame();
 
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        int bridgeSize = inputView.readBridgeSize();
+        List<String> bridgeList = bridgeMaker.makeBridge(bridgeSize);
+        bridgeGame.move(0, bridgeList, 0, "");
     }
 }
