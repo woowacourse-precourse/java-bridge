a0e2306443a335706d093032b5c866013ffa0bfa
JaeWon Choi <87649850+codesver@users.noreply.github.com>
2022-11-16 21:03:08 +0900

    feat(validatePositiveNumber): implement method

--- src/main/java/bridge/service/Validator.java
+++ src/main/java/bridge/service/Validator.java
@@ -6,6 +6,12 @@ public class Validator {
 
     }
 
+    private void validatePositiveNumber(String bridgeLength) {
+        if (!bridgeLength.matches("[1-9][0-9]*")) {
+            throw new IllegalArgumentException("[ERROR] 다리 길이 입력값이 양의 정수가 아닙니다.");
+        }
+    }
+
     public void validateMovement(String movement) {
 
     }
