fda471e81dbd2968ad685490e0c47000eea90f28
소대훈 <theogns0681@naver.com>
2022-11-16 15:52:07 +0900

    [feat] (InputView) readMoving & readGameCommand 구현

--- src/main/java/bridge/InputView.java
+++ src/main/java/bridge/InputView.java
@@ -2,6 +2,8 @@ package bridge;
 
 import camp.nextstep.edu.missionutils.Console;
 
+import java.util.Objects;
+
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
  */
@@ -42,13 +44,31 @@ public class InputView {
      * 사용자가 이동할 칸을 입력받는다.
      */
     public String readMoving() {
-        return null;
+        String input;
+        System.out.println(readMovingMs);
+        try {
+            input = Console.readLine().toUpperCase();
+            if (!Objects.equals(input, "U") && !Objects.equals(input, "D")) throw new IllegalArgumentException();
+        } catch (IllegalArgumentException e) {
+            System.out.println("[ERROR] U 혹은 D를 입력해주세요.");
+            input = readMoving();
+        }
+        return input;
     }
 
     /**
      * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
      */
     public String readGameCommand() {
-        return null;
+        String input;
+        System.out.println(readGameCommandMs);
+        try {
+            input = Console.readLine().toUpperCase();
+            if (!Objects.equals(input, "R") && !Objects.equals(input, "Q")) throw new IllegalArgumentException();
+        } catch (IllegalArgumentException e) {
+            System.out.println("[ERROR] R 혹은 Q를 입력해주세요.");
+            input = readGameCommand();
+        }
+        return input;
     }
 }
