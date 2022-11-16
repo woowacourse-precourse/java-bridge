c691f1ac2facddf4cfe9459a9541351eb496a1a3
WonJeongYeon <wonjungyoun@naver.com>
2022-11-16 16:40:04 +0900

    feat: 다리 길이 입력 받는 기능 추가

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -3,6 +3,22 @@ package bridge;
 public class Application {
 
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        System.out.println("다리 건너기 게임을 시작합니다.");
+        InputView inputView = new InputView();
+        String readValue = inputView.readBridgeSize();
+        int size = checkSize(readValue);
+    }
+
+    public static int checkSize(String str) {
+        int size = 0;
+        try {
+            size = Integer.parseInt(str);
+        } catch (NumberFormatException e) {
+            //throw new IllegalArgumentException("[ERROR] 다시 시도해 주세요.");
+            InputView inputView = new InputView();
+            String s = inputView.readBridgeSize();
+            checkSize(s);
+        }
+        return size;
     }
 }
