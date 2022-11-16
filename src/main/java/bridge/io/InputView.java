49de4a047c52621f1157a3554f863a167ef6e1ca
JaeWon Choi <87649850+codesver@users.noreply.github.com>
2022-11-16 20:55:12 +0900

    feat(readBridgeLength): implement method
    Read input value from console.

--- src/main/java/bridge/io/InputView.java
+++ src/main/java/bridge/io/InputView.java
@@ -1,5 +1,7 @@
 package bridge.io;
 
+import camp.nextstep.edu.missionutils.Console;
+
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
  * - Package 변경 가능
@@ -8,11 +10,8 @@ package bridge.io;
  */
 public class InputView {
 
-    /**
-     * 다리의 길이를 입력받는다.
-     */
-    public int readBridgeSize() {
-        return 0;
+    public String readBridgeLength() {
+        return Console.readLine();
     }
 
     /**
