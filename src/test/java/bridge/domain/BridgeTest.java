9541e1bb8fa9f1998748ec93a39b10009920955d
kimtaehoonDev <taehoon_dev@naver.com>
2022-11-16 16:42:45 +0900

    test(Bridge): 테스트케이스 작성

---
+++ src/test/java/bridge/domain/BridgeTest.java
@@ -0,0 +1,48 @@
+package bridge.domain;
+
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+
+@Nested
+class BridgeTest {
+    @Test
+    void 정상_입력값이_범위안() {
+
+    }
+
+    @Test
+    void 예외_입력값_3보다_작다() {
+
+    }
+
+    @Test
+    void 예외_입력값_20보다_크다() {
+
+    }
+
+    @Test
+    void 예외_입력값에_U_D_이외_값() {
+
+    }
+
+    @Test
+    void 정상_다리가_존재하는_인덱스_물어봄() {
+
+    }
+
+    @Test
+    void 예외_다리_범위_바깥_인덱스_물어봄() {
+
+    }
+
+    @Test
+    void 예외_0보다_작은_인덱스로_물어봄() {
+
+    }
+
+    @Test
+    void 예외_입력값이_FootrestLocation_아님() {
+
+    }
+
+}
