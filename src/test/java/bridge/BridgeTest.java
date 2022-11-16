2a70f92380a23fd0ffcff726e4c7c8784bc32bef
CYC-ChoiYoungChang <choiyc1446@gmail.com>
2022-11-16 18:00:34 +0900

    test: 다리 길이 검증 테스트 추가

---
+++ src/test/java/bridge/BridgeTest.java
@@ -0,0 +1,33 @@
+package bridge;
+
+import bridge.util.BridgeSizeValidator;
+import camp.nextstep.edu.missionutils.Console;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+
+import static org.assertj.core.api.Assertions.*;
+
+public class BridgeTest {
+
+    @DisplayName("범위 벗어나는 숫자 입력시 예외 발생")
+    @Test
+    void createBridgeByOverRange(){
+        InputStream in = new ByteArrayInputStream("50".getBytes());
+        System.setIn(in);
+        assertThatThrownBy(() -> new BridgeSizeValidator().run(Console.readLine()))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("잘못된 값 입력시 예외 발생")
+    @Test
+    void createBridgeByWrongInput(){
+        InputStream in = new ByteArrayInputStream("10J".getBytes());
+        System.setIn(in);
+        assertThatThrownBy(() -> new BridgeSizeValidator().run(Console.readLine()))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+}
