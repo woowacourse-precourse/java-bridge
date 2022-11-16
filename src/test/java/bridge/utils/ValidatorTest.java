6ed7cf0d653ebe0f6f262f0f083a8fd145513131
HwangSeokJin <ghkd3703@gmail.com>
2022-11-17 00:51:04 +0900

    style(test): 테스트 메소드 이름 변경

--- src/test/java/bridge/utils/ValidatorTest.java
+++ src/test/java/bridge/utils/ValidatorTest.java
@@ -8,14 +8,14 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 class ValidatorTest {
     @DisplayName("다리 길이 입력 시 숫자를 입력하지 않으면 예외 처리한다.")
     @Test
-    void inputNonNumeric() {
+    void setBridgeSizeToNonNumeric() {
         assertThatThrownBy(() -> Validator.validateBridgeSize("q"))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
-    @DisplayName("3 이상 20 이하의 숫자를 입력하지 않으면 예외 처리한다.")
+    @DisplayName("다리 길이 입력 시 3 이상 20 이하의 숫자를 입력하지 않으면 예외 처리한다.")
     @Test
-    void inputInvalidRange() {
+    void setBridgeSizeToInvalidRange() {
         assertThatThrownBy(() -> Validator.validateBridgeSize("21"))
                 .isInstanceOf(IllegalArgumentException.class);
     }
