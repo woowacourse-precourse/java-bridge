ef945f8c8b76283216f0bbd8970dcfc50e6dad69
mckim123 <minchan06@gmail.com>
2022-11-17 20:35:42 +0900

    feat: implement Application.java
    Create startGame() method to control restarting game
    Add fields : InputView, OutputView, IsSuccess, BridgeGame

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -1,8 +1,34 @@
 package bridge;
 
+import bridge.domain.BridgeGame;
+import bridge.view.InputView;
+import bridge.view.OutputView;
+
 public class Application {
+    static InputView inputView = new InputView();
+    static OutputView outputView = new OutputView();
+    static boolean IsSuccess;
+    static BridgeGame bridgeGame;
 
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        int bridgeSize = inputView.readBridgeSize();
+        bridgeGame = new BridgeGame(bridgeSize);
+        startGame();
+    }
+
+    public static void startGame() {
+        bridgeGame.start();
+        while (bridgeGame.isPlaying()) {
+            IsSuccess = bridgeGame.move(inputView.readMoving());
+            outputView.printMap(bridgeGame.getMap());
+            if (IsSuccess == false) {
+                if (inputView.readGameCommand() == "R") {
+                    startGame();
+                    return;
+                }
+                break;
+            }
+        }
+        outputView.printResult(IsSuccess, bridgeGame.getTrials(), bridgeGame.getMap());
     }
 }
