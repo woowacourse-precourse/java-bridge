e4800c2ae4c520f8aac33ee7f0fd8d1bf4b9d015
Guga <103228463+BGuga@users.noreply.github.com>
2022-11-17 16:15:38 +0900

    feat: 다리의 길이는 3~20 사이 값이다

--- src/main/java/bridge/domain/Bridge.java
+++ src/main/java/bridge/domain/Bridge.java
@@ -4,11 +4,27 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Bridge {
+    private static final int MIN_BRIDGE_LENGTH = 3;
+    private static final int MAX_BRIDGE_LENGTH = 20;
+    private static final String MIN_VALIDATION_ERROR_MESSAGE = "[ERROR] 다리의 최소 길이는 %d이상 입니다.";
+    private static final String MAX_VALIDATION_ERROR_MESSAGE = "[ERROR] 다리의 최대 길이는 %d 입니다.";
+
     private List<Direction> directions;
 
     public Bridge(List<String> directionData) {
+        validateLength(directionData);
         this.directions = makeDirectionsByStrings(directionData);
     }
+
+    private void validateLength(List<String> directionData) {
+        if (directionData.size() < MIN_BRIDGE_LENGTH) {
+            throw new IllegalArgumentException(String.format(MIN_VALIDATION_ERROR_MESSAGE, MIN_BRIDGE_LENGTH));
+        }
+        if (directionData.size() > MAX_BRIDGE_LENGTH) {
+            throw new IllegalArgumentException(String.format(MAX_VALIDATION_ERROR_MESSAGE, MAX_BRIDGE_LENGTH));
+        }
+    }
+
 
     private List<Direction> makeDirectionsByStrings(List<String> directionData) {
         List<Direction> result = new ArrayList<>();
@@ -17,5 +33,6 @@ public class Bridge {
         }
         return result;
     }
+
 
 }
