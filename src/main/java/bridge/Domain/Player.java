ed93bc52627932d6bbe410893bdf6e47ab2f2108
swandevson <shai4956@gmail.com>
2022-11-17 21:38:34 +0900

    Add(Player): 다리 건너기 게임을 플레이하는 플레이어
    - 다음에 갈 위치, 마지막 선택, 생존 여부를 멤버로 가짐
    - getter와 setter 작성

--- src/main/java/bridge/Domain/Player.java
+++ src/main/java/bridge/Domain/Player.java
@@ -1,4 +1,32 @@
 package bridge.Domain;
 
 public class Player {
+    private int nextLocation;
+    private String lastSelection;
+    private boolean alive;
+
+
+    public Player() {
+        this.nextLocation = 0;
+        this.lastSelection = null;
+        this.alive = true;
+    }
+
+
+    public void setLastSelection(String lastSelection) {
+        this.lastSelection = lastSelection;
+    }
+
+
+    public boolean isAlive() {
+        return this.alive;
+    }
+
+    public int getNextLocation() {
+        return this.nextLocation;
+    }
+
+    public String getLastSelection() {
+        return this.lastSelection;
+    }
 }
