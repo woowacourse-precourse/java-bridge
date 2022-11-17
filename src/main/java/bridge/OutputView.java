103140de5e1b8c0dcddfab9a8a7731ec5228ee71
연주 <50358956+Yeon-jooKim@users.noreply.github.com>
2022-11-17 20:59:18 +0900

    feat: 이동 로그 출력

--- src/main/java/bridge/OutputView.java
+++ src/main/java/bridge/OutputView.java
@@ -5,12 +5,38 @@ package bridge;
  */
 public class OutputView {
 
+    public void printStartMessage() {
+        System.out.println("다리 건너기 게임을 시작합니다.");
+    }
+
+    public void printBridgeSizeRequest() {
+        System.out.println("다리의 길이를 입력해주세요.");
+    }
+
+    public void printMovingRequest(){
+        System.out.println("이동할 칸을 선택해주세요. (위: U, 아래: D)");
+    }
+
+    public void printGameStatusRequest() {
+        System.out.println("게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)");
+    }
+
+
     /**
      * 현재까지 이동한 다리의 상태를 정해진 형식에 맞춰 출력한다.
      * <p>
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void printMap() {
+    public void printMap(char[][] movingLog) {
+        StringBuilder stringBuilder = new StringBuilder();
+        for (int i = 0; i <movingLog.length ; i++) {
+            stringBuilder.append("[ ");
+            for (int j = 0; j <movingLog[i].length ; j++) {
+                stringBuilder.append(movingLog[i][j] + " ");
+            }
+            stringBuilder.append("\n");
+        }
+        System.out.println(stringBuilder.toString());
     }
 
     /**
