3b622c29e90311692951d41393d8466b82a80a79
tenta <eunoia3802@gmail.com>
2022-11-17 17:08:44 +0900

    feat: 게임 시작 문구 출력 기능 구현

--- src/main/java/view/OutputView.java
+++ src/main/java/view/OutputView.java
@@ -4,6 +4,10 @@ package view;
  * 사용자에게 게임 진행 상황과 결과를 출력하는 역할을 한다.
  */
 public class OutputView {
+    private final String START_GAME = "다리 건너기 게임을 시작합니다.";
+    public void printStart() {
+        System.out.println(START_GAME);
+    }
 
     /**
      * 현재까지 이동한 다리의 상태를 정해진 형식에 맞춰 출력한다.
