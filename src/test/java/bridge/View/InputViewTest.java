916e135541819b6436f60c78c473548c881ecd96
skck0226 <skck0226@ajou.ac.kr>
2022-11-17 20:27:19 +0900

    [refactor] (InputViewTest) @parameterizedTest 적용, 성공 실패 케이스 분리

--- src/test/java/bridge/View/InputViewTest.java
+++ src/test/java/bridge/View/InputViewTest.java
@@ -1,6 +1,9 @@
 package bridge.View;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static bridge.AssertInput.assertInputByMockingTest;
 import static org.junit.jupiter.api.Assertions.*;
@@ -9,33 +12,21 @@ class InputViewTest {
 
     InputView inputView = new InputView();
 
-    @Test
-    void 다리길이입력_테스트() {
-
-        // 파라미터 테스트 적용 예정
+    @DisplayName("다리길이 입력 실패 케이스 테스트")
+    @ParameterizedTest(name = "{index} {displayName} message={0}")
+    @ValueSource(strings = {")", "ㅁ", "a", "0", "2", "21"})
+    void 다리길이_입력_실패케이스_테스트(String params) {
         assertInputByMockingTest(()->{
                 assertThrows(IllegalArgumentException.class,()->inputView.readBridgeSize());
-        },")");
+        },params);
+    }
 
-        assertInputByMockingTest(()->{
-            assertThrows(IllegalArgumentException.class,()->inputView.readBridgeSize());
-        },"ㅁ");
-
-        assertInputByMockingTest(()->{
-            assertThrows(IllegalArgumentException.class,()->inputView.readBridgeSize());
-        },"a");
-
-        assertInputByMockingTest(()->{
-            assertThrows(IllegalArgumentException.class,()->inputView.readBridgeSize());
-        },"2");
-
-        assertInputByMockingTest(()->{
-            assertThrows(IllegalArgumentException.class,()->inputView.readBridgeSize());
-        },"21");
-
+    @DisplayName("다리길이 입력 성공 케이스 테스트")
+    @ParameterizedTest(name = "{index} {displayName} message={0}")
+    @ValueSource(strings = {"3","5","20"})
+    void 다리길이_입력_성공케이스_테스트(String params) {
         assertInputByMockingTest(()->{
             assertDoesNotThrow(()->inputView.readBridgeSize());
-        },"4");
-
+        },params);
     }
 }
