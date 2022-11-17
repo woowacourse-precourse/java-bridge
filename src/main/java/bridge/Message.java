2ec31b389b4ec73e10afdaae54b01b7b519656de
abbd2 <ajkety00@gmail.com>
2022-11-17 23:51:28 +0900

    Feat(Message): ui 출력 메서드 구현

--- src/main/java/bridge/Message.java
+++ src/main/java/bridge/Message.java
@@ -4,7 +4,7 @@ public enum Message {
 
     BRIDGE_OPTIONS("(위: U, 아래: D)"),
     GAME_OPTIONS("(재시도: R, 종료: Q)"),
-    GAME_START_MESSAGE("다리 건너기 게임을 시작합니다."),
+    START_GAME_MESSAGE("다리 건너기 게임을 시작합니다."),
     INPUT_RANGE_BRIDGE_MESSAGE("다리의 길이를 입력해주세요."),
     INPUT_MOVE_BRIDGE_MESSAGE("이동할 칸을 선택해주세요."),
     INPUT_SELECT_GAME_RETRY("게임을 다시 시도할지 여부를 입력해주세요.");
@@ -15,20 +15,20 @@ public enum Message {
         this.script = script;
     }
 
-    public void gameStartMessage() {
-        System.out.println(GAME_START_MESSAGE);
+    public static void startGameMessage() {
+        System.out.println(START_GAME_MESSAGE.script);
     }
 
-    public void inputRangeBridgeMessage() {
-        System.out.println(INPUT_RANGE_BRIDGE_MESSAGE);
+    public static void inputRangeBridgeMessage() {
+        System.out.println(INPUT_RANGE_BRIDGE_MESSAGE.script);
     }
 
-    public void inputMoveBridgeMessage() {
-        System.out.println(INPUT_MOVE_BRIDGE_MESSAGE + " " + BRIDGE_OPTIONS);
+    public static void inputMoveBridgeMessage() {
+        System.out.println(INPUT_MOVE_BRIDGE_MESSAGE.script + " " + BRIDGE_OPTIONS.script);
     }
 
-    public void inputSelectGameRetry() {
-        System.out.println(INPUT_SELECT_GAME_RETRY + " " + GAME_OPTIONS);
+    public static void inputSelectGameRetry() {
+        System.out.println(INPUT_SELECT_GAME_RETRY.script + " " + GAME_OPTIONS.script);
     }
 
 }
