b1c8d4070c95d2931aaa88c145dccbfb2e4433be
jjmin9797 <jjmin9797@naver.com>
2022-11-17 18:54:54 +0900

    feat(InputView): 다리 길이 입력 기능 구현

--- src/main/java/bridge/view/InputView.java
+++ src/main/java/bridge/view/InputView.java
@@ -1,4 +1,5 @@
 package bridge.view;
+import camp.nextstep.edu.missionutils.Console;
 
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
@@ -6,7 +7,11 @@ package bridge.view;
 public class InputView {
 
     private static final String GAME_START_MESSAGE = "다리 건너기 게임을 시작합니다.";
+    private static final String READ_BRIDGE_SIZE_MESSAGE = "다리의 길이를 입력해주세요.";
 
+    /**
+     *  게임 시작 메시지를 출력한다.
+     */
     public void showGameStartMessage() {
         System.out.println(GAME_START_MESSAGE);
     }
@@ -14,8 +19,10 @@ public class InputView {
     /**
      * 다리의 길이를 입력받는다.
      */
-    public int readBridgeSize() {
-        return 0;
+    public String readBridgeSize() {
+        System.out.println(READ_BRIDGE_SIZE_MESSAGE);
+        String size = Console.readLine();
+        return size;
     }
 
     /**
