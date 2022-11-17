3d7b43ea8e9ae1ea89f67a89c91c7263285a85b6
Damgom <wisewish1@gmail.com>
2022-11-17 18:16:07 +0900

    FEAT: output message

--- src/main/java/bridge/view/OutputView.java
+++ src/main/java/bridge/view/OutputView.java
@@ -5,6 +5,11 @@ package bridge.view;
  */
 public class OutputView {
 
+    private static final String START_GAME = "다리 건너기 게임을 시작합니다.";
+    private static final String INPUT_BRIDGE_LENGTH = "다리의 길이를 입력해주세요.";
+    private static final String MOVE_UP_OR_DOWN = "이동할 칸을 선택해주세요. (위: U, 아래: D)";
+    private static final String RETRY_OR_QUIT = "게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)";
+    private static final String GAME_RESULT = "최종 게임 결과";
     private static final String SUCCESS_OR_FAIL = "게임 성공 여부: ";
     private static final String TRY_COUNT = "총 시도한 횟수: ";
     private static final String SUCCESS = "성공";
@@ -30,5 +35,25 @@ public class OutputView {
             System.out.println(SUCCESS_OR_FAIL + FAIL);
         }
         System.out.println(TRY_COUNT + tryCount);
+    }
+
+    public void printStartGame() {
+        System.out.println(START_GAME);
+    }
+
+    public void printInputBridgeLength() {
+        System.out.println(INPUT_BRIDGE_LENGTH);
+    }
+
+    public void printSelectMove() {
+        System.out.println(MOVE_UP_OR_DOWN);
+    }
+
+    public void printSelectRetry() {
+        System.out.println(RETRY_OR_QUIT);
+    }
+
+    public void printResultMessage() {
+        System.out.println(GAME_RESULT);
     }
 }
