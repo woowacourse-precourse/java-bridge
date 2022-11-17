649f3691e6f85fbda8b95250f8733f32740339dd
MinjeongKong <kong1301@ajou.ac.kr>
2022-11-18 03:55:53 +0900

    feat: 현재까지 이동한 다리의 상태 출력

--- src/main/java/bridge/OutputView.java
+++ src/main/java/bridge/OutputView.java
@@ -1,16 +1,78 @@
 package bridge;
 
+import static bridge.Controller.GAME_FAIL;
+import static bridge.Controller.GAME_FINISH;
+
 /**
  * 사용자에게 게임 진행 상황과 결과를 출력하는 역할을 한다.
  */
 public class OutputView {
+    private StringBuilder up;
+    private StringBuilder down;
+
+    public OutputView() {
+    }
+
+    public void printStart() {
+        System.out.println("다리 건너기 게임을 시작합니다.\n");
+    }
+
+    private void initMap() {
+        up = new StringBuilder();
+        down = new StringBuilder();
+    }
 
     /**
      * 현재까지 이동한 다리의 상태를 정해진 형식에 맞춰 출력한다.
      * <p>
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void printMap() {
+    public void printMap(String pos, int bridgeIndex, int moveState) {
+        setMap(bridgeIndex);
+        saveMap(pos, moveState);
+        printSaveAll();
+    }
+
+    private void setMap(int bridgeIndex) {
+        if (bridgeIndex == 0) {
+            initMap();
+            return;
+        }
+        up.append(" | ");
+        down.append(" | ");
+    }
+
+    private void saveMap(String pos, int moveState) {
+        if (moveState == GAME_FAIL) {
+            saveFail(pos);
+            return;
+        }
+        saveSuccess(pos);
+    }
+
+    private void saveFail(String pos) {
+        if (pos.equals("U")) {
+            up.append("X");
+            down.append(" ");
+            return;
+        }
+        up.append(" ");
+        down.append("X");
+    }
+
+    private void saveSuccess(String pos) {
+        if (pos.equals("U")) {
+            up.append("O");
+            down.append(" ");
+            return;
+        }
+        up.append(" ");
+        down.append("O");
+    }
+
+    private void printSaveAll() {
+        System.out.println("[ " + up + " ]");
+        System.out.println("[ " + down + " ]\n");
     }
 
     /**
