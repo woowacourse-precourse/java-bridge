b1bb7e6bf7217102648fffe741ca88d73f6d787a
SeolYoungKim <nasur4da@gmail.com>
2022-11-17 20:24:21 +0900

    test(GameResults): 본 클래스 이름 변경에 따라, test 클래스도 이름을 변경하였습니다

--- src/test/java/bridge/domain/MoveResultsTest.java
+++ src/test/java/bridge/domain/MoveResultsTest.java
@@ -8,37 +8,37 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
-class GameResultsTest {
+class MoveResultsTest {
 
-    private final GameResults gameResults = new GameResults();
+    private final MoveResults moveResults = new MoveResults();
 
     @BeforeEach
     void init() {
-        gameResults.reset();
+        moveResults.reset();
     }
 
     @ParameterizedTest(name = "{0}, {1}을 입력 받으면 upResult에는 {1}가, downResults에는 공백이 저장된다.")
     @CsvSource({"U,O", "U,X"})
     void addResultsUp(String moveCommand, String resultSign) {
-        gameResults.addResults(moveCommand, resultSign);
-        assertThat(gameResults.upResults()).contains(resultSign);
-        assertThat(gameResults.downResults()).contains(" ");
+        moveResults.addResults(moveCommand, resultSign);
+        assertThat(moveResults.upResults()).contains(resultSign);
+        assertThat(moveResults.downResults()).contains(" ");
     }
 
     @ParameterizedTest(name = "{0}, {1}을 입력 받으면 upResult에는 공백이, downResults에는 {1}가 저장된다.")
     @CsvSource({"D,O", "D,X"})
     void addResultsDown(String moveCommand, String resultSign) {
-        gameResults.addResults(moveCommand, resultSign);
-        assertThat(gameResults.upResults()).contains(" ");
-        assertThat(gameResults.downResults()).contains(resultSign);
+        moveResults.addResults(moveCommand, resultSign);
+        assertThat(moveResults.upResults()).contains(" ");
+        assertThat(moveResults.downResults()).contains(resultSign);
     }
 
     @DisplayName("reset 메서드는 upResult와 downResult를 전부 비운다.")
     @Test
     void reset() {
-        gameResults.addResults("U", "O");
-        gameResults.reset();
-        assertThat(gameResults.upResults()).isEmpty();
-        assertThat(gameResults.downResults()).isEmpty();
+        moveResults.addResults("U", "O");
+        moveResults.reset();
+        assertThat(moveResults.upResults()).isEmpty();
+        assertThat(moveResults.downResults()).isEmpty();
     }
 }
