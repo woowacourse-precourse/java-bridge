d6d544add449095cc2e2c4821d3f08d5d8ec3937
kevstevie <jianna442@gmail.com>
2022-11-17 20:21:55 +0900

    feat: printMap 메서드 추가

--- src/main/java/bridge/view/OutputView.java
+++ src/main/java/bridge/view/OutputView.java
@@ -1,22 +1,49 @@
 package bridge.view;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * 사용자에게 게임 진행 상황과 결과를 출력하는 역할을 한다.
  */
 public class OutputView {
 
-    private static final String BRIDGE_START = "[ ";
-    private static final String BRIDGE_END = " ]";
-    private static final String BRIDGE_BOUNDARY = " | ";
-    private static final String BLANK = " ";
-    private static final String CORRECT = "O";
-    private static final String WRONG = "X";
+    private static final String BRIDGE_START = "[";
+    private static final String BRIDGE_END = "]";
+    private static final String BRIDGE_BOUNDARY = "|";
+    private static final String BLANK = "   ";
+    private static final String CORRECT = " O ";
+    private static final String WRONG = " X ";
+    private static final String UP = "U";
+    private static final String DOWN = "D";
+
+    StringBuilder upsideBridge = new StringBuilder(BRIDGE_START);
+    StringBuilder downsideBridge = new StringBuilder(BRIDGE_START);
     /**
      * 현재까지 이동한 다리의 상태를 정해진 형식에 맞춰 출력한다.
      * <p>
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void printMap() {
+    public void printMap(String selection, boolean isCorrect) {
+        if (selection.equals(UP)) {
+            addMap(isCorrect, upsideBridge, downsideBridge);
+        }
+        if (selection.equals(DOWN)) {
+            addMap(isCorrect, downsideBridge, upsideBridge);
+        }
+        System.out.println(upsideBridge + BRIDGE_END);
+        System.out.println(downsideBridge + BRIDGE_END);
+    }
+
+    private void addMap(boolean isCorrect, StringBuilder sb, StringBuilder opposite) {
+        if (isCorrect) {
+            sb.append(CORRECT);
+            opposite.append(BLANK);
+        }
+        if (!isCorrect) {
+            sb.append(WRONG);
+            opposite.append(BLANK);
+        }
     }
 
     /**
@@ -27,7 +54,7 @@ public class OutputView {
     public void printResult() {
     }
 
-    public void gameStart() {
+    public void printGameStart() {
         System.out.println(ViewConstants.GAME_START);
         System.out.println();
     }
