07069bea9ae06037be9f29a12323eed176ff1f75
Bumsoo <rhqjatn2398@naver.com>
2022-11-17 19:50:52 +0900

    feat(bridge): 다리를 생성한다
    다리의 길이가 3이상 20이하의 숫자가 아닌 경우 예외 처리한다.
    각 가로칸에 대해서 위 칸을 건널 수 있으면 U, 아래 칸을 건널 수 있으면 D 값으로 다리를 나타낸다.

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -1,17 +1,23 @@
 package bridge;
 
+import java.util.List;
+
 public class Application {
-    private static InputView inputView = InputView.getInstance();
+    private static final InputView inputView = InputView.getInstance();
+    private static BridgeMaker bridgeMaker;
+    private static final BridgeGame bridgeGame = BridgeGame.getInstance();
 
     public static void main(String[] args) {
         start();
     }
 
     private static void start() {
-        constructBridge();
+        int bridgeSize = inputView.readBridgeSize();
+        constructBridge(bridgeSize);
     }
 
-    private static void constructBridge() {
-        int bridgeSize = inputView.readBridgeSize();
+    private static List<String> constructBridge(int bridgeSize) {
+        bridgeMaker = new BridgeMaker(new BridgeRandomNumberGenerator());
+        return bridgeMaker.makeBridge(bridgeSize);
     }
 }
--- src/main/java/bridge/BridgeMaker.java
+++ src/main/java/bridge/BridgeMaker.java
@@ -1,6 +1,9 @@
 package bridge;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 /**
  * 다리의 길이를 입력 받아서 다리를 생성해주는 역할을 한다.
@@ -8,6 +11,7 @@ import java.util.List;
 public class BridgeMaker {
 
     private final BridgeNumberGenerator bridgeNumberGenerator;
+    private final Map<Integer, String> numToStringMapping = Map.of(0, "D", 1, "U");
 
     public BridgeMaker(BridgeNumberGenerator bridgeNumberGenerator) {
         this.bridgeNumberGenerator = bridgeNumberGenerator;
@@ -18,6 +22,31 @@ public class BridgeMaker {
      * @return 입력받은 길이에 해당하는 다리 모양. 위 칸이면 "U", 아래 칸이면 "D"로 표현해야 한다.
      */
     public List<String> makeBridge(int size) {
-        return null;
+        validateBridgeSize(size);
+
+        return getNumbers(size).stream()
+                .map(this::convertNumToString)
+                .collect(Collectors.toList());
     }
+
+    private void validateBridgeSize(int size) {
+        if (size < BridgeSizeBound.LOWER.getBound() || BridgeSizeBound.UPPER.getBound() < size) {
+            throw new IllegalArgumentException(ErrorMessage.BRIDGE_SIZE_EXCEPTION.getMessage());
+        }
+    }
+
+    private List<Integer> getNumbers(int size) {
+        List<Integer> numbers = new ArrayList<>();
+        for (int i = 0; i < size; i++) {
+            int randNum = bridgeNumberGenerator.generate();
+            numbers.add(randNum);
+        }
+
+        return numbers;
+    }
+
+    private String convertNumToString(int num) {
+        return numToStringMapping.get(num);
+    }
+
 }
