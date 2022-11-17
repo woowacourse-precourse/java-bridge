6b119b008ae03e809b659e1c793b30d87856cd7c
Mr.Kim <joajy98@gmail.com>
2022-11-18 00:53:41 +0900

    feat: construct base inputview's methods

--- src/main/java/bridge/InputView.java
+++ src/main/java/bridge/InputView.java
@@ -3,26 +3,36 @@ package bridge;
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
  */
-public class InputView {
+import camp.nextstep.edu.missionutils.Console;
 
+public class InputView {
     /**
      * 다리의 길이를 입력받는다.
      */
     public int readBridgeSize() {
-        return 0;
+        int bridgeSize =  Integer.parseInt(Console.readLine());
+        if(bridgeSize > 3 || bridgeSize < 20)
+            throw new IllegalArgumentException();
+        return bridgeSize;
     }
 
     /**
      * 사용자가 이동할 칸을 입력받는다.
      */
     public String readMoving() {
-        return null;
+        String move = Console.readLine();
+        if(!(move == "U" || move == "D"))
+            throw new IllegalArgumentException();
+        return move;
     }
 
     /**
      * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
      */
     public String readGameCommand() {
-        return null;
+        String Replay = Console.readLine();
+        if(!(Replay == "R" || Replay == "Q"))  
+            throw new IllegalArgumentException();
+        return Replay;
     }
 }
