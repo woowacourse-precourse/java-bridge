00ef4671ce18ff6e11983ff7f5d8cba19846d202
jinny-codes <77732679+jinny-codes@users.noreply.github.com>
2022-11-18 02:51:22 +0900

    feat(OutputView): printGetLength 구현

--- src/main/java/bridge/OutputView.java
+++ src/main/java/bridge/OutputView.java
@@ -33,6 +33,9 @@ public class OutputView {
     public void printStart() {
         System.out.println("다리 건너기 게임을 시작합니다.");
     }
+    public void printGetLength() {
+        System.out.println("다리의 길이를 입력해주세요.");
+    }
 
 
 }
