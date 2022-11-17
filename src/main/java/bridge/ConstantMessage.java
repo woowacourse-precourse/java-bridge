31368bb0af61ad50a3ad60a34ab2748950d91586
박시균\psk98 <psg980331@naver.com>
2022-11-17 13:08:27 +0900

    Feat: 게임에 사용할 메시지 상수화

---
+++ src/main/java/bridge/ConstantMessage.java
@@ -0,0 +1,11 @@
+package bridge;
+
+public class ConstantMessage {
+    static final String BRIDGE_GAME_START_MESSAGE = "다리 건너기 게임을 시작합니다.";
+    static final String REQUEST_BRIDGE_LENGTH_MESSAGE = "다리의 길이를 입력해주세요.";
+    static final String REQUEST_MOVE_DIRECTION_MESSAGE = "이동할 칸을 선택해주세요. (위: U, 아래: D)";
+    static final String REQUEST_GAME_RETRY_ANSWER_MESSAGE = "게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)";
+    static final String GAME_RESULT_MAP_MESSAGE = "최종 게임 결과";
+    static final String GAME_RESULT_SUCCESS_OR_FAIL_MESSAGE = "게임 성공 여부: ";
+    static final String GAME_RETRY_COUNT_MESSAGE = "총 시도한 횟수: ";
+}
