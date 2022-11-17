a40a16dddad68c2090399582e330448ee570edf0
WhoIsWoony <WhoIsWoony@gmail.com>
2022-11-17 23:37:06 +0900

    feat: add validator for all parameter of BridgeGame

--- src/main/java/bridge/BridgeGame.java
+++ src/main/java/bridge/BridgeGame.java
@@ -13,11 +13,16 @@ public class BridgeGame {
     public boolean result;
 
     public BridgeGame(int boardSize){
+        validateSize(boardSize);
         BridgeRandomNumberGenerator randomGenerator = new BridgeRandomNumberGenerator();
         board = new BridgeMaker(randomGenerator).makeBridge(boardSize);
         step = 0;
         retryNum = 0;
         result = true;
+    }
+    private void validateSize(int boardSize){
+        if(boardSize < 3 || boardSize > 20)
+            throw new IllegalArgumentException();
     }
     /**
      * 사용자가 칸을 이동할 때 사용하는 메서드
@@ -25,8 +30,14 @@ public class BridgeGame {
      * 이동을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
     public void move(String move) {
+        validateMove(move);
         result = board.get(step).equals(move);
         step += 1;
+    }
+
+    private void validateMove(String move){
+        if(!(move.equals("U") || move.equals("D")))
+            throw new IllegalArgumentException();
     }
 
     /**
@@ -35,6 +46,7 @@ public class BridgeGame {
      * 재시작을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
     public boolean retry(String command) {
+        validateRetry(command);
         if(command.equals("R")){
             step = 0;
             retryNum += 1;
@@ -42,6 +54,11 @@ public class BridgeGame {
             return true;
         }
         return false;
+    }
+
+    private void validateRetry(String command){
+        if(!(command.equals("R") || command.equals("Q")))
+            throw new IllegalArgumentException();
     }
 
     public boolean clear(){
