99257c122c8df3ecdf8ba7d195e850c3c136095d
KYR <freekyr7529@gmail.com>
2022-11-18 06:38:39 +0900

    feature : OutputView 클래스에 printMsg 메소드 생성

--- src/main/java/bridge/OutputView.java
+++ src/main/java/bridge/OutputView.java
@@ -4,7 +4,9 @@ package bridge;
  * 사용자에게 게임 진행 상황과 결과를 출력하는 역할을 한다.
  */
 public class OutputView {
-
+    public void printMsg(String message) {
+        System.out.println(message);
+    }
     /**
      * 현재까지 이동한 다리의 상태를 정해진 형식에 맞춰 출력한다.
      * <p>
