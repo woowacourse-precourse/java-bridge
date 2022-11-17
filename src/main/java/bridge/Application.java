cf85a419d8cf8f4d1396e80f7a733efe0a28f0ef
K-Diger <kitten.diger@gmail.com>
2022-11-18 02:54:09 +0900

    chore : 클라이언트 코드 추가

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -1,8 +1,21 @@
 package bridge;
 
 public class Application {
-
+    private final static ContextFactory contextFactory = new ContextFactory();
+    private final static BridgeGameStarter bridgeGameStarter = contextFactory.getBridgeGameStarter();
+    private static int tryCount = 1;
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        try {
+            bridgeGameStarter.initialize();
+            boolean retryFlag = bridgeGameStarter.run();
+            while (retryFlag) {
+                bridgeGameStarter.reInitialize();
+                retryFlag = bridgeGameStarter.run();
+                tryCount ++;
+            }
+            bridgeGameStarter.closeGame(tryCount);
+        } catch (IllegalArgumentException exception) {
+            System.out.println(exception.getMessage());
+        }
     }
 }
