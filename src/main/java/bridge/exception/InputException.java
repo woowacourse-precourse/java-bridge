4001cec29993b17d0ce508f8d15f762268013794
nswon <swnam0729@gmail.com>
2022-11-17 12:01:18 +0900

    feat: 칸 입력 검증

--- src/main/java/bridge/exception/InputException.java
+++ src/main/java/bridge/exception/InputException.java
@@ -4,6 +4,8 @@ public class InputException {
     private static final String NOT_ONLY_NUMBER = "[ERROR] 숫자만 입력이 가능합니다.";
     private static final String NOT_MIN_SIZE = "[ERROR] 다리의 길이는 최소 3이상이어야 합니다.";
     private static final String NOT_MAX_SIZE = "[ERROR] 다리의 길이는 최대 20이하여야 합니다.";
+    private static final String NOT_SPACE_FORMAT = "[ERROR] 칸은 대문자 U 또는 D로 나타내야 합니다.";
+    private static final String NOT_ONLY_ONE_LENGTH = "[ERROR] 칸은 한 글자로 입력해야 합니다.";
 
     public static void printNotOnlyNumberException() {
         throw new IllegalArgumentException(NOT_ONLY_NUMBER);
@@ -15,5 +17,13 @@ public class InputException {
 
     public static void printNotMaxSizeException() {
         throw new IllegalArgumentException(NOT_MAX_SIZE);
+    }
+
+    public static void printNotOnlyOneLengthException() {
+        throw new IllegalArgumentException(NOT_ONLY_ONE_LENGTH);
+    }
+
+    public static void printNotSpaceFormatException() {
+        throw new IllegalArgumentException(NOT_SPACE_FORMAT);
     }
 }
--- src/main/java/bridge/validator/InputValidator.java
+++ src/main/java/bridge/validator/InputValidator.java
@@ -8,6 +8,9 @@ public class InputValidator {
     private static final String ONLY_NUMBER_REGEX = "^[0-9]*$";
     private static final int MIN_SIZE = 3;
     private static final int MAX_SIZE = 20;
+    private static final String SPACE_FORMAT = "^(U)|(D)*$";
+    private static final int SPACE_LENGTH = 1;
+
 
     private InputValidator() {
     }
@@ -17,6 +20,12 @@ public class InputValidator {
         validateMinSize(Integer.parseInt(size));
         validateMaxSize(Integer.parseInt(size));
         return Integer.parseInt(size);
+    }
+
+    public static String checkMovingSpace(String space) {
+        validateOneLength(space);
+        validateSpaceFormat(space);
+        return space;
     }
 
     private static void validateOnlyNumber(String input) {
@@ -34,6 +43,18 @@ public class InputValidator {
     private static void validateMaxSize(int size) {
         if(size > MAX_SIZE) {
             printNotMaxSizeException();
+        }
+    }
+
+    private static void validateOneLength(String space) {
+        if(space.length() != SPACE_LENGTH) {
+            printNotOnlyOneLengthException();
+        }
+    }
+
+    private static void validateSpaceFormat(String space) {
+        if(!Pattern.matches(SPACE_FORMAT, space)) {
+            printNotSpaceFormatException();
         }
     }
 }
