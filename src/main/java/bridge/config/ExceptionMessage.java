82c9f5427481baa114165e66a6dd67e9f0c2e961
kimminsu <xjfcnfw3@naver.com>
2022-11-17 16:20:23 +0900

    feat: 다리의 길이가 제한된 범위에 있는지 검증

--- src/main/java/bridge/config/ExceptionMessage.java
+++ src/main/java/bridge/config/ExceptionMessage.java
@@ -2,7 +2,8 @@ package bridge.config;
 
 public enum ExceptionMessage {
     WRONG_ELEMENT("다리의 요소는 반드시 U, D이어야 합니다."),
-    NOT_NUMBER_SIZE("다리의 길이는 오직 숫자만 입력받아야 합니다.");
+    NOT_NUMBER_SIZE("다리의 길이는 오직 숫자만 입력받아야 합니다."),
+    NUMBER_OUT_OF_RANGE("다리의 길이는 3 ~ 20이어야 합니다.");
 
     private final String message;
 
--- src/main/java/bridge/size/BridgeSize.java
+++ src/main/java/bridge/size/BridgeSize.java
@@ -1,16 +1,29 @@
 package bridge.size;
 
 import static bridge.config.ExceptionMessage.NOT_NUMBER_SIZE;
+import static bridge.config.ExceptionMessage.NUMBER_OUT_OF_RANGE;
+
+import bridge.config.ExceptionMessage;
 
 public class BridgeSize {
+    private static final int MIN_SIZE = 3;
+    private static final int MAX_SIZE = 20;
+
     private final int size;
 
     public BridgeSize(String size) {
         this.size = validateOnlyNumber(size);
+        validate(this.size);
     }
 
     public void validate(int size) {
+        validateSizeRange(size);
+    }
 
+    private void validateSizeRange(int size) {
+        if (size < MIN_SIZE || size > MAX_SIZE) {
+            throw new IllegalArgumentException(NUMBER_OUT_OF_RANGE.toString());
+        }
     }
 
     private int validateOnlyNumber(String size) {
