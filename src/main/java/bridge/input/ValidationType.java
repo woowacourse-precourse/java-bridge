6d0be276517d4371492bb8da0a7dca0e8860d974
Kong-hana01 <gksqlsl11@khu.ac.kr>
2022-11-17 21:34:53 +0900

    feat(ValidationType): 사용자 입력 검증을 위한 ValidationType enum 클래스 생성

--- src/main/java/bridge/input/ValidationType.java
+++ src/main/java/bridge/input/ValidationType.java
@@ -1,4 +1,20 @@
 package bridge.input;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
 public enum ValidationType {
+    CHECK_SIZE(Arrays.stream(IntStream.rangeClosed(3, 20).toArray())
+            .boxed().map(String::valueOf)
+            .collect(Collectors.toList())),
+    CHECK_MOVING(List.of("U", "D")),
+    CHECK_GAME_COMMAND(List.of("R", "Q"));
+
+    private List<String> validationRange;
+
+    ValidationType(List<String> validationRange) {
+        this.validationRange = validationRange;
+    }
 }
