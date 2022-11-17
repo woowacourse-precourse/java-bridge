4fefac157bf4d65ff40edab6a9a167e6058c2436
kokodak <kokodakadokok@gmail.com>
2022-11-17 21:44:04 +0900

    feat(bridge): 다리와 사용자가 이동한 칸의 매칭 상황을 구하는 기능 구현

--- src/main/java/bridge/domain/Bridge.java
+++ src/main/java/bridge/domain/Bridge.java
@@ -1,5 +1,6 @@
 package bridge.domain;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Bridge {
@@ -12,5 +13,17 @@ public class Bridge {
 
     public boolean canMove(List<String> userMoved, int idx) {
         return bridge.get(idx) == userMoved.get(idx);
+    }
+
+    public List<String> getMatchingStatus(List<String> userMoved) {
+        List<String> matchingStatus = new ArrayList<>();
+        for (int i = 0; i < userMoved.size(); i++) {
+            if (canMove(userMoved, i)) {
+                matchingStatus.add("O");
+                continue;
+            }
+            matchingStatus.add("X");
+        }
+        return matchingStatus;
     }
 }
