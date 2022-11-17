67bfec47c7de0fd4abaca4d0f666bd9dc348eaa5
Guga <103228463+BGuga@users.noreply.github.com>
2022-11-18 01:41:06 +0900

    feat: R은 재시작을 의미하고 Q는 게임을 그만둔다는 의사 표현이다

---
+++ src/main/java/bridge/domain/Command.java
@@ -0,0 +1,25 @@
+package bridge.domain;
+
+import java.util.Arrays;
+import java.util.Optional;
+
+public enum Command {
+    Restart("R"),
+    Quit("Q");
+
+    private static String INVALID_STRING_ERROR_MESSAGE = "[ERROR] 유효하지 않은 게임 진행 Command입니다.";
+
+    private String command;
+
+    Command(String command) {
+        this.command = command;
+    }
+
+    public static Command getCommandByString(String commandData) {
+        Optional<Command> returnCommand = Arrays.stream(Command.values())
+                .filter((ch) -> ch.command.equals(commandData))
+                .findAny();
+        returnCommand.orElseThrow(() -> new IllegalArgumentException(INVALID_STRING_ERROR_MESSAGE));
+        return returnCommand.get();
+    }
+}
