06725569ccee7cfd66b47434445695b99aadd4c2
great-park <cksgh1735@naver.com>
2022-11-17 19:08:55 +0900

    feat: 게임 시작 시 한 번만 다리의 길이를 입력받는다.

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -1,11 +1,13 @@
 package bridge;
 
 import bridge.controller.BridgeController;
+import bridge.view.InputView;
+import bridge.view.OutputView;
 
 public class Application {
 
     public static void main(String[] args) {
-        BridgeController bridgeController = new BridgeController();
+        BridgeController bridgeController = new BridgeController(new InputView(), new OutputView());
         bridgeController.start();
     }
 }
--- src/main/java/bridge/controller/BridgeController.java
+++ src/main/java/bridge/controller/BridgeController.java
@@ -1,6 +1,19 @@
 package bridge.controller;
 
+import bridge.domain.vo.BridgeSize;
+import bridge.view.InputView;
+import bridge.view.OutputView;
+
 public class BridgeController {
+    private final InputView inputView;
+    private final OutputView outputView;
+
+    public BridgeController(InputView inputView, OutputView outputView){
+        this.inputView = inputView;
+        this.outputView = outputView;
+    }
+
     public void start() {
+        BridgeSize bridgeSize = inputView.readBridgeSize();
     }
 }
