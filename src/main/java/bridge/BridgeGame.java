ee392eb93d76a8e7ee3e78ce87d029591243b7b7
jipyo0106 <68218081+jipyo0106@users.noreply.github.com>
2022-11-17 23:40:23 +0900

    feat: 게임 시작 시 초기 설정
    BridgeGame 객체 생성(게임 시작 준비)을 하면서 BridgeMaker 생성 시 댜양성을 위해 2개의 생성자 구현
    bridgeSize만 받는 경우 자동으로 RandomGenerator을 주입받고, CostumNumberGenerator을 주입할 수도 있다.

--- src/main/java/bridge/BridgeGame.java
+++ src/main/java/bridge/BridgeGame.java
@@ -1,9 +1,25 @@
 package bridge;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * 다리 건너기 게임을 관리하는 클래스
  */
 public class BridgeGame {
+    private final BridgeMaker bridgeMaker;
+    private final List<String> bridgeLine;
+    private final List<String> userCrossBridge;
+
+    public BridgeGame(int bridgeSize) {
+        this(BridgeMaker.getInstanceByRandomGenerator(), bridgeSize);
+    }
+
+    public BridgeGame(BridgeMaker bridgeMaker, int bridgeSize) {
+        this.bridgeMaker = bridgeMaker;
+        this.bridgeLine = bridgeMaker.makeBridge(bridgeSize);
+        this.userCrossBridge = new ArrayList<>(bridgeSize);
+    }
 
     /**
      * 사용자가 칸을 이동할 때 사용하는 메서드
