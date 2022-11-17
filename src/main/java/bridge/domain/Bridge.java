63fff6e6f9f47d071c04c4addbe02cb92a89e0d6
twoosky <lhjae3@naver.com>
2022-11-18 01:55:51 +0900

    feat(Bridge): 이동할 수 있는 칸인지 검사하는 메소드 구현

--- src/main/java/bridge/domain/Bridge.java
+++ src/main/java/bridge/domain/Bridge.java
@@ -1,11 +1,20 @@
 package bridge.domain;
 
 import java.util.List;
+import java.util.Objects;
 
 public class Bridge {
     private final List<Integer> bridge;
 
     public Bridge(List<Integer> bridge) {
         this.bridge = bridge;
+    }
+
+    public boolean isPossibleMove (int index, String moving) {
+        int movingSpace = bridge.get(index);
+        if (moving.equals("U")) {
+            return movingSpace == 1;
+        }
+        return movingSpace == 0;
     }
 }
