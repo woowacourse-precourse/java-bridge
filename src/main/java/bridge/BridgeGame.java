a1c620c4156f9c1578b09e63db505a79e3c33c13
JunDonghyuk <ehdgur4814@naver.com>
2022-11-17 22:29:15 +0900

    fear(BridgeGame): 이동과 재도전 기능 구현

--- src/main/java/bridge/BridgeGame.java
+++ src/main/java/bridge/BridgeGame.java
@@ -1,16 +1,36 @@
 package bridge;
 
+import java.util.List;
+
 /**
  * 다리 건너기 게임을 관리하는 클래스
  */
 public class BridgeGame {
+    private List<String> bridge;
+    private int location;
+    private boolean moveSuccess;
+
+    public BridgeGame(List<String> bridge) {
+        this.bridge = bridge;
+        location = 0;
+        moveSuccess = true;
+    }
+
+    public int getLocation() {
+        return location;
+    }
 
     /**
      * 사용자가 칸을 이동할 때 사용하는 메서드
      * <p>
      * 이동을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void move() {
+    public void move(String moveCommand) {
+        if (bridge.get(location).equals(moveCommand)) {
+            location++;
+            return;
+        }
+        moveSuccess = false;
     }
 
     /**
@@ -18,6 +38,21 @@ public class BridgeGame {
      * <p>
      * 재시작을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void retry() {
+    public boolean retry(String gameCommand) {
+        if (gameCommand.equals(CommandConstant.RETRY_COMMAND)) {
+            return true;
+        }
+        return false;
+    }
+
+    public boolean isGameEnd() {
+        if (bridge.size() == location) {
+            return true;
+        }
+        return false;
+    }
+
+    public boolean succeedMove() {
+        return moveSuccess;
     }
 }
