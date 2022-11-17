1f9d0125de8a199d1485146f46fb983f6f96bb0b
HaODay <ycis5013@naver.com>
2022-11-18 01:40:39 +0900

    feat(bridge): 입력값(재시작/종료 여부)을 검증하는 기능 구현
    - 대문자 R 또는 대문자 Q가 아닌 경우 예외 발생

--- src/main/java/bridge/ValidateInput.java
+++ src/main/java/bridge/ValidateInput.java
@@ -2,22 +2,28 @@ package bridge;
 
 public class ValidateInput {
 
-    public static final String ERROR_MESSAGE="[ERROR] ";
-    public static int validateBridgeSize(String bridgeSize) throws IllegalArgumentException{
-        try{
-            if(!(3<=Integer.parseInt(bridgeSize)&&Integer.parseInt(bridgeSize)<=20)){
+    public static int validateBridgeSize(String bridgeSize) throws IllegalArgumentException {
+        try {
+            if (!(3 <= Integer.parseInt(bridgeSize) && Integer.parseInt(bridgeSize) <= 20)) {
                 throw new IllegalArgumentException();
             }
             return Integer.parseInt(bridgeSize);
-        }catch(NumberFormatException e){
+        } catch (NumberFormatException e) {
             throw new IllegalArgumentException();
         }
     }
 
-    public static String validateMoving(String moving) throws IllegalArgumentException{
-        if(!(moving.equals("U")||moving.equals("D"))){
+    public static String validateMoving(String moving) throws IllegalArgumentException {
+        if (!(moving.equals("U") || moving.equals("D"))) {
             throw new IllegalArgumentException();
         }
         return moving;
+    }
+
+    public static String validateGameCommand(String gameCommand) throws IllegalArgumentException {
+        if (!(gameCommand.equals("R") || gameCommand.equals("Q"))) {
+            throw new IllegalArgumentException();
+        }
+        return gameCommand;
     }
 }
