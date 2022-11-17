cb545d0e7d6b36571c0048e2f05022b3b9165328
SEONGJOON KIM <jobcho6320@naver.com>
2022-11-18 05:25:51 +0900

    refactor: Bridge 클래스 메소드 bridgeGame 클래스로 수정
    추가 Bridge 클래스에 작성한 메소드들이 고정되있는 Bridge 클래스와 도메인의 범위가 일치한다고 생각해 수정

---
+++ src/main/java/bridge/domain/BridgeGame.java
@@ -0,0 +1,50 @@
+package bridge.domain;
+
+import bridge.view.OutputView;
+
+import java.util.List;
+
+import static bridge.view.OutputView.printRetryMessage;
+
+/**
+ * 다리 건너기 게임을 관리하는 클래스
+ */
+public class BridgeGame {
+
+    private List<String> bridgGame;
+
+    public BridgeGame(List<String> bridgGame) {
+        this.bridgGame = bridgGame;
+    }
+    public boolean matchMoving(int index, String move) {
+        return bridgGame.get(index).equals(move);
+    }
+
+    public int size() {
+        return bridgGame.size();
+    }
+
+    public String get(int index) {
+        return bridgGame.get(index);
+    }
+
+    public String toString(List<String> result) {
+        return result.toString();
+    }
+    /**
+     * 사용자가 칸을 이동할 때 사용하는 메서드
+     * <p>
+     * 이동을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
+     */
+    public void move() {
+    }
+
+    /**
+     * 사용자가 게임을 다시 시도할 때 사용하는 메서드
+     * <p>
+     * 재시작을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
+     */
+    public void retry() {
+        OutputView.printRetryMessage();
+    }
+}
