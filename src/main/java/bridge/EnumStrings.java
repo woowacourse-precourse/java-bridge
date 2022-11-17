31935cef4390defbeb6dd3b044d3c67b2fec1c17
jkroh1995 <85445649+jkroh1995@users.noreply.github.com>
2022-11-18 03:55:37 +0900

    refactor : 코드의 일관성을 위한 변수명 변경 / feat : 게임 시작 메세지 추가

--- src/main/java/bridge/EnumStrings.java
+++ src/main/java/bridge/EnumStrings.java
@@ -2,7 +2,8 @@ package bridge;
 
 public enum EnumStrings {
     PATTERN("^[0-9]*$"),
-    PRINT_LENGTH ("다리의 길이를 입력해주세요."),
+    PRINT_START("다리 건너기 게임을 시작합니다."),
+    PRINT_SIZE ("다리의 길이를 입력해주세요."),
     PRINT_MOVE ("이동할 칸을 선택해주세요. (위: U, 아래: D)"),
     PRINT_RETRY ("게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)"),
     SUCCESS("성공"),
--- src/main/java/bridge/InputView.java
+++ src/main/java/bridge/InputView.java
@@ -15,7 +15,8 @@ public class InputView {
      * 다리의 길이를 입력받는다.
      */
     public String readBridgeSize() {
-        System.out.println(EnumStrings.PRINT_LENGTH.getValue());
+        System.out.println();
+        System.out.println(EnumStrings.PRINT_SIZE.getValue());
         return Console.readLine();
     }
 
@@ -23,6 +24,7 @@ public class InputView {
      * 사용자가 이동할 칸을 입력받는다.
      */
     public String readMoving() {
+        System.out.println();
         System.out.println(EnumStrings.PRINT_MOVE.getValue());
         return Console.readLine();
     }
@@ -31,6 +33,7 @@ public class InputView {
      * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
      */
     public String readGameCommand() {
+        System.out.println();
         System.out.println(EnumStrings.PRINT_RETRY.getValue());
         return Console.readLine();
     }
