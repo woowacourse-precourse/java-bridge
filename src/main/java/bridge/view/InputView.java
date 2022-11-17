1a62f026d757f9d6a5035290624f7f5a7cdf90de
HyeonJun0530 <nexus2697@pukyong.ac.kr>
2022-11-17 23:53:38 +0900

    feat(InputView): Input UI 기능 구현및 예외처리

--- src/main/java/bridge/view/InputView.java
+++ src/main/java/bridge/view/InputView.java
@@ -1,28 +1,95 @@
 package bridge.view;
 
+import bridge.msg.ErrorMsg;
+import bridge.msg.InputMsg;
+import camp.nextstep.edu.missionutils.Console;
+
+import static bridge.msg.ErrorMsg.*;
+import static bridge.msg.InputMsg.*;
+
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
  */
 public class InputView {
 
+    private final static String REGEX = "[0-9]+";
+
     /**
      * 다리의 길이를 입력받는다.
      */
     public int readBridgeSize() {
-        return 0;
+        System.out.println("다리의 길이를 입력해주세요.");
+        String inputSize = Console.readLine();
+
+        sizeValidateHandler(inputSize);
+
+        return Integer.valueOf(inputSize);
     }
 
     /**
      * 사용자가 이동할 칸을 입력받는다.
      */
     public String readMoving() {
-        return null;
+        String inputMove = Console.readLine();
+
+        moveValidateHandler(inputMove);
+
+        return inputMove;
     }
+
 
     /**
      * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
      */
     public String readGameCommand() {
-        return null;
+        String restartInput = Console.readLine();
+
+        commendValidateHandler(restartInput);
+
+        return restartInput;
+    }
+
+    private void sizeValidateHandler(String inputSize) {
+        try {
+            validateSize(inputSize);
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+            readBridgeSize();
+        }
+    }
+
+    private void moveValidateHandler(String inputMove) {
+        try {
+            validateMove(inputMove);
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+            readMoving();
+        }
+    }
+
+    private void commendValidateHandler(String restartInput) {
+        try {
+            validateCommand(restartInput);
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+            readGameCommand();
+        }
+    }
+
+    public void validateSize(String inputSize) {
+        if (!inputSize.matches(REGEX))
+            throw new IllegalArgumentException(BRIDGE_LENGTH_ERR.getMsg());
+        if (Integer.valueOf(inputSize) < 3 || Integer.valueOf(inputSize) > 20)
+            throw new IllegalArgumentException(BRIDGE_LENGTH_ERR.getMsg());
+    }
+
+    public void validateMove(String inputMove) {
+        if (!inputMove.equals(UP_MSG.getMsg()) || !inputMove.equals(DOWN_MSG.getMsg()))
+            throw new IllegalArgumentException(MOVE_ERR.getMsg());
+    }
+
+    public void validateCommand(String restartInput) {
+        if (!restartInput.equals(RESTART_MSG.getMsg()) || !restartInput.equals(QUIT_MSG.getMsg()))
+            throw new IllegalArgumentException(RESTART_ERR.getMsg());
     }
 }
