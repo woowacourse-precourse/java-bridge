f5d1c64280516298fadd7806076eed83a53e2a72
Eungyu-code <Agyu625@gmail.com>
2022-11-17 23:17:26 +0900

    feat(Application class): bridgeShape, bridge 변수 추가

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -1,5 +1,7 @@
 package bridge;
 
+import java.util.List;
+
 public class Application {
 
     private static int size;
@@ -9,6 +11,10 @@ public class Application {
     private static String retry;
 
     private static Error error;
+
+    private static List<String> bridgeShape;
+
+    private static List<String> bridge;
 
     public static void main(String[] args) {
         // TODO: 프로그램 구현
