5992825cab35588999b3659795888e5dae459485
Park shinyeong <syngh503@gmail.com>
2022-11-17 20:08:31 +0900

    Feat(InputUtils): 입력값이 정수인지 확인하는 기능 구현
    입력값이 정수인지 확인하는 isDigit 함수를 구현하였습니다.
    정수이면 true를, 이외의 것이면 false를 리턴합니다 .

--- src/main/java/Utils/InputUtils.java
+++ src/main/java/Utils/InputUtils.java
@@ -1,4 +1,13 @@
 package Utils;
 
 public class InputUtils {
+
+    public boolean isDigit(String input) {
+        try{
+            Integer.parseInt(input);
+        } catch (NumberFormatException exception) {
+            return false;
+        }
+        return true;
+    }
 }
