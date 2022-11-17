e2dfe8b01d455d34e81dcf17c5e0b3c5747c8255
eunbii0213 <eunbii0213@naver.com>
2022-11-17 13:57:28 +0900

    fix: 테스트 코드 분리

--- src/test/java/bridge/ValidationTest.java
+++ src/test/java/bridge/ValidationTest.java
@@ -10,32 +10,26 @@ import bridge.Validation.Validation;
 
 public class ValidationTest{
 
-	@DisplayName("올바르지 않은 BridgeSize입력 예외처리 테스트")
-	@ParameterizedTest
-	@ValueSource(strings = {"22", "ㅁ"})
-	void isBridgeSizeException_메서드_테스트(String input) {
-
-		Assertions.assertAll(
-			() -> validateNumberOnly_메서드_테스트(input),
-			() -> validateLength_메서드_테스트(input),
-			() -> validateRange_메서드_테스트(input)
-		);
-	}
-
 	@DisplayName("input이 숫자만으로 이루어져있는지 테스트")
-	private void validateRange_메서드_테스트(String input) {
+	@Test
+	private void validateRange_메서드_테스트() {
+		String input = "22";
 		Assertions.assertThrows(IllegalArgumentException.class,
 			() -> Validation.validateRange(input));
 	}
 
 	@DisplayName("input이 숫자만으로 이루어져있는지 테스트")
-	void validateNumberOnly_메서드_테스트(String input) {
+	@Test
+	void validateNumberOnly_메서드_테스트() {
+		String input = "a";
 		Assertions.assertThrows(IllegalArgumentException.class,
 			() -> Validation.validateNumberOnly(input));
 	}
 
 	@DisplayName("input이 올바른 길이인지 테스트")
-	void validateLength_메서드_테스트(String input) {
+	@Test
+	void validateLength_메서드_테스트() {
+		String input = "22";
 		Assertions.assertThrows(IllegalArgumentException.class,
 			() -> Validation.validateLength(input, 1));
 	}
