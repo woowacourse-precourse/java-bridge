758e48756e9aea348d9167de42d74e277b69940a
ksh-beginner <ohk9134@naver.com>
2022-11-17 12:12:01 +0900

    feat(InputView/OutputView) : 이동할 칸 입력 기능 구현

--- src/main/java/bridge/view/InputView.java
+++ src/main/java/bridge/view/InputView.java
@@ -17,8 +17,8 @@ public class InputView {
     /**
      * 사용자가 이동할 칸을 입력받는다.
      */
-    public String readMoving() {
-        return null;
+    public String inputMovingStep() {
+        return Console.readLine();
     }
 
     /**
