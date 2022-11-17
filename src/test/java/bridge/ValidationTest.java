ae49f2a1615e075822f20f25cbe0ad8465d188b5
youhobin <ghdb132@naver.com>
2022-11-17 19:07:44 +0900

    refactor : error메세지를 상수로 선언

--- src/test/java/bridge/ValidationTest.java
+++ src/test/java/bridge/ValidationTest.java
@@ -7,25 +7,27 @@ import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class ValidationTest {
+
+    private static final String ERROR_MESSAGE = "[ERROR]";
     @Test
     void 유효성검사_다리길이_문자() {
         assertThatThrownBy(() -> Validation.validateBridgeSize("a"))
             .isInstanceOf(IllegalArgumentException.class)
-            .hasMessageContaining("[ERROR]");
+            .hasMessageContaining(ERROR_MESSAGE);
     }
 
     @Test
     void 유효성검사_다리길이_최소값미만() {
         assertThatThrownBy(() -> Validation.validateBridgeSize("2"))
             .isInstanceOf(IllegalArgumentException.class)
-            .hasMessageContaining("[ERROR]");
+            .hasMessageContaining(ERROR_MESSAGE);
     }
 
     @Test
     void 유효성검사_다리길이_최대값초과() {
         assertThatThrownBy(() -> Validation.validateBridgeSize("21"))
             .isInstanceOf(IllegalArgumentException.class)
-            .hasMessageContaining("[ERROR]");
+            .hasMessageContaining(ERROR_MESSAGE);
     }
 
     @DisplayName("U와 D 이외의 문자이면 예외가 발생한다.")
@@ -33,7 +35,7 @@ public class ValidationTest {
     void 유효성검사_칸선택() {
         assertThatThrownBy(() -> Validation.validateMoving("R"))
             .isInstanceOf(IllegalArgumentException.class)
-            .hasMessageContaining("[ERROR]");
+            .hasMessageContaining(ERROR_MESSAGE);
     }
 
     @DisplayName("R과 Q 이외의 문자이면 예외가 발생한다.")
@@ -41,6 +43,6 @@ public class ValidationTest {
     void 유효성검사_재시작선택() {
         assertThatThrownBy(() -> Validation.validateRestartInput("U"))
             .isInstanceOf(IllegalArgumentException.class)
-            .hasMessageContaining("[ERROR]");
+            .hasMessageContaining(ERROR_MESSAGE);
     }
 }
