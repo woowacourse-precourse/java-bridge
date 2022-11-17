ae327bfcd5c81b8730c8151fd7d25ed7c3e94037
KYR <freekyr7529@gmail.com>
2022-11-18 06:41:24 +0900

    feature : OutputView 클래스에 로그를 출력하는 printMsg 메소드 생성

--- src/main/java/bridge/OutputView.java
+++ src/main/java/bridge/OutputView.java
@@ -4,7 +4,7 @@ package bridge;
  * 사용자에게 게임 진행 상황과 결과를 출력하는 역할을 한다.
  */
 public class OutputView {
-    public void printMsg(String message) {
+    static public void printMsg(String message) {
         System.out.println(message);
     }
     /**
