968196c6dcd45943750e3a83737270375dc3175a
Park shinyeong <syngh503@gmail.com>
2022-11-17 23:53:06 +0900

    Feat(Player): 플레이어 위치 저장 및 플레이어 이동
    플레이어의 위치를 저장하는 Player 클래스를 생성하였습니다.
    position에 플레이어 위치를 저장합니다. (0부터 시작합니다.)
    플레이어의 위치를 한 칸씩 이동하는 movePlayer 메소드를 구현하였습니다.

--- src/main/java/bridge/Domain/Player.java
+++ src/main/java/bridge/Domain/Player.java
@@ -1,4 +1,14 @@
 package bridge.Domain;
 
 public class Player {
+    private int position;
+
+    public Player() {
+        this.position = 0;
+    }
+
+    public void movePlayer() {
+        this.position++;
+    }
+
 }
