e329c2459dc82e471fb6a6a97d07a958cd99c649
kimminsu <xjfcnfw3@naver.com>
2022-11-17 14:30:04 +0900

    test: 다리에 U와 D만 있는지 테스트

--- src/test/java/bridge/domain/BridgeTest.java
+++ src/test/java/bridge/domain/BridgeTest.java
@@ -1,27 +1,39 @@
 package bridge.domain;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
+import bridge.config.ExceptionMessage;
 import java.util.List;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class BridgeTest {
+
     @DisplayName("정답 다리와 유저 입력 다리 테스트")
     @Test
-    void compareCorrectBridgesTest(){
+    void compareCorrectBridgesTest() {
         Bridge answerBridge = new Bridge(List.of("U", "D", "U", "U", "N"));
         Bridge userBridge = new Bridge(List.of("U", "D", "U"));
         boolean answer = true;
-        assertThat(answer).isEqualTo(answerBridge.checkBridge(userBridge));
+        assertThat(answer).isEqualTo(answerBridge.isPassableBridge(userBridge));
     }
 
     @DisplayName("틀린 다리와 유저 입력 다리 테스트")
     @Test
-    void compareWrongBridgesTest(){
+    void compareWrongBridgesTest() {
         Bridge answerBridge = new Bridge(List.of("U", "D", "U", "U", "N"));
         Bridge userBridge = new Bridge(List.of("U", "D", "D"));
         boolean answer = false;
-        assertThat(answer).isEqualTo(answerBridge.checkBridge(userBridge));
+        assertThat(answer).isEqualTo(answerBridge.isPassableBridge(userBridge));
     }
+
+    @DisplayName("다리의 요소중에 U와 D가 아니면 예외가 발생한다.")
+    @Test
+    void insertSquaresWithWrongElement() {
+        assertThatThrownBy(() -> new Bridge(List.of("U", "D", "A", "D")))
+            .isInstanceOf(IllegalArgumentException.class)
+            .hasMessageContaining(ExceptionMessage.ERROR);
+    }
+
 }
