c07d5277b8b73eb43122ae6440e9877243ab3ee9
swandevson <shai4956@gmail.com>
2022-11-17 13:56:01 +0900

    Feat(InputView): 게임 시작 메세지 출력, 다리 길이 입력 후 반환

--- src/main/java/bridge/View/InputView.java
+++ src/main/java/bridge/View/InputView.java
@@ -1,15 +1,37 @@
 package bridge.View;
 
+import bridge.Domain.InputConverter;
+
+import static camp.nextstep.edu.missionutils.Console.readLine;
+
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
  */
 public class InputView {
+    private final String START_GAME = "다리 건너기 게임을 시작합니다.";
+    private final String INPUT_BRIDGE_SIZE = "다리의 길이를 입력해주세요.";
 
     /**
      * 다리의 길이를 입력받는다.
      */
-    public int readBridgeSize() {
-        return 0;
+    public void printStartMessage() {
+        System.out.println(START_GAME);
+    }
+
+    public int readBridgeSize() throws IllegalArgumentException {
+        System.out.println(INPUT_BRIDGE_SIZE);
+
+        String input = readLine();
+
+        try {
+            int bridgeSize = InputConverter.convertToInt(input);
+
+            return bridgeSize;
+        } catch (IllegalArgumentException illegalArgumentException) {
+            throw illegalArgumentException;
+        }
+
+
     }
 
     /**
