25ce2c82627ce8afa5026aa0b24883668ba874ed
rt3310 <btac3310@gmail.com>
2022-11-17 12:10:55 +0900

    style: 메서드 명 변경 printBridgeLengthInputMessage -> printBridgeSizeInputMessage

--- src/main/java/bridge/OutputView.java
+++ src/main/java/bridge/OutputView.java
@@ -25,7 +25,7 @@ public class OutputView {
         System.out.println(InformationMessage.START.getMessage());
     }
 
-    public void printBridgeLengthInputMessage() {
+    public void printBridgeSizeInputMessage() {
         System.out.println(InformationMessage.BRIDGE_LENGTH.getMessage());
     }
 }
