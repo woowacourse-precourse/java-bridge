74278e90182b18a22f57f39999bd7e8e285a65f5
Voyager003 <85725033+Voyager003@users.noreply.github.com>
2022-11-17 21:49:29 +0900

    Test: 추가 - ExceptionTest 입력한 다리의 길이가 범위에 맞지 않다면 예외 발생 및 미발생 테스트코드 작성

--- src/test/java/bridge/ExceptionTest.java
+++ src/test/java/bridge/ExceptionTest.java
@@ -1,19 +1,25 @@
 package bridge;
 
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.Assertions.assertThatCode;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
-public class InputViewTest {
+public class ExceptionTest {
 
     @DisplayName("다리의 길이 입력이 3부터 20사이의 수가 아니라면 예외 발생")
     @Test
-    void checkRange() {
-        int input = 2;
+    void checkRangefalse() {
         assertThatThrownBy(() -> Exception.lengthrangeException(2))
                 .hasMessageContaining("[ERROR] 다리 길이는 3부터 20 사이의 숫자여야 합니다.")
                 .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("3부터 20사이의 수라면 예외가 발생하지 않음")
+    @Test
+    void checkRangetrue() {
+        assertThatCode(() -> Exception.lengthrangeException(15))
+                .doesNotThrowAnyException();
     }
 }
