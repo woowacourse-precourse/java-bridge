42a1f713dd92bf2c508a6ab04e0c84ec244dafbc
Rovna99 <80767387+Rovna99@users.noreply.github.com>
2022-11-17 17:57:44 +0900

    feat: 다리 생성 값 세팅 기능 구현

--- src/main/java/bridge/view/InputView.java
+++ src/main/java/bridge/view/InputView.java
@@ -1,23 +1,30 @@
 package bridge.view;
 
-/**
- * 사용자로부터 입력을 받는 역할을 한다.
- */
+import camp.nextstep.edu.missionutils.Console;
+
 public class InputView {
 
-    /**
-     * 다리의 길이를 입력받는다.
-     */
     public int readBridgeSize() {
-        return 0;
+        String input = Console.readLine();
+        try {
+            validSize(input);
+        } catch (IllegalArgumentException e) {
+            System.out.println("[ERROR] 다리 길이는 숫자여야 합니다.");
+        }
+        return Integer.parseInt(input);
+    }
+
+    private void validSize(String input) {
+        int size = Integer.parseInt(input);
+        if (size < 0) {
+            throw new IllegalArgumentException();
+        }
     }
 
     /**
      * 사용자가 이동할 칸을 입력받는다.
      */
-    public String readMoving() {
-        return null;
-    }
+    public String readMoving() { return null;}
 
     /**
      * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
