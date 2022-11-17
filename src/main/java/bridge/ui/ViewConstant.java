c7f1632feacabc51a164a5f806de5af0ec220380
whdcks420 <whdcks420@gmail.com>
2022-11-17 19:08:03 +0900

    feat: view constant 메시지 상수

--- src/main/java/bridge/ui/ViewConstant.java
+++ src/main/java/bridge/ui/ViewConstant.java
@@ -1,2 +1,13 @@
-package bridge.ui;public class ViewConstant {
+package bridge.ui;
+
+public class ViewConstant {
+    public static final String LINE_FEED = "\n";
+
+    // InputView
+    public static final String INPUT_BRIDGE_SIZE = "다리의 길이를 입력해주세요.";
+    public static final String INPUT_DIRECTION = LINE_FEED + "이동할 칸을 선택해주세요. (위: U, 아래: D)";
+    public static final String INPUT_RESTART_WHETHER = "게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)";
+
+    // OutView
+    public static final String GAME_INIT_MESSAGE = "다리 건너기 게임을 시작합니다." + LINE_FEED;
 }
