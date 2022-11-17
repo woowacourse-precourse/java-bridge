b7f4eaa248d362f54074f4a970310348ec76d948
twoosky <lhjae3@naver.com>
2022-11-18 01:57:33 +0900

    feat(utils): 에러 메시지 상수 선언

--- src/main/java/bridge/utils/ErrorMessages.java
+++ src/main/java/bridge/utils/ErrorMessages.java
@@ -2,4 +2,7 @@ package bridge.utils;
 
 public class ErrorMessages {
     public static final String ERROR_NOT_INTEGER = "[ERROR] 다리 길이는 숫자여야 합니다.";
+    public static final String ERROR_OUT_OF_RANGE = "[ERROR] 다리 길이는 3부터 20 사이의 숫자여야 합니다.";
+    public static final String ERROR_INVALID_MOVING = "[ERROR] 이동할 칸은 U 또는 D만 입력 가능합니다.";
+    public static final String ERROR_INVALID_GAME_COMMAND = "[ERROR] 재시작 여부는 R 또는 Q만 입력 가능합니다.";
 }
