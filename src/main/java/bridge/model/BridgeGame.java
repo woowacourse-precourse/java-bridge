bbd1f982d5cebc409d007e733f9c07b77d59ef99
imsongj <imsongj@gmail.com>
2022-11-18 00:52:55 +0900

    feat(bridge) : 플레이어 이동 기능 구현

--- src/main/java/bridge/model/BridgeGame.java
+++ src/main/java/bridge/model/BridgeGame.java
@@ -1,16 +1,38 @@
-package bridge;
+package bridge.model;
+
+import java.util.List;
 
 /**
  * 다리 건너기 게임을 관리하는 클래스
  */
 public class BridgeGame {
+    Player player;
+    List<String> bridge;
+    int numberOfAttempts;
+
+    public BridgeGame() {
+        numberOfAttempts = 0;
+    }
+
+    private void increaseNumberOfAttempts() {
+        this.numberOfAttempts++;
+    }
+
+    public void initializeBridge(List<String> bridge) {
+        this.bridge = bridge;
+    }
+
+    public void createNewPlayer() {
+        this.player = new Player();
+    }
 
     /**
      * 사용자가 칸을 이동할 때 사용하는 메서드
      * <p>
      * 이동을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void move() {
+    public void move(GameMove move) {
+        this.player.move(move);
     }
 
     /**
