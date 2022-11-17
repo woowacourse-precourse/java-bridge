97177bc8dc72f1a4880b18aa61c260a6bbd23ede
twoosky <lhjae3@naver.com>
2022-11-18 00:27:04 +0900

    feat(utils): 출력 메시지 상수 선언

--- src/main/java/bridge/utils/ViewMessages.java
+++ src/main/java/bridge/utils/ViewMessages.java
@@ -5,4 +5,10 @@ public class ViewMessages {
     public static final String SELECT_MOVE_SPACE = "이동할 칸을 선택해주세요. (위: U, 아래: D)";
     public static final String RETRY_FLAG = "게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)";
 
+    public static final String GAME_RESULT = "최종 게임 결과";
+    public static final String BRIDGE_START = "[ ";
+    public static final String BRIDGE_SPACE_DIVISION = " | ";
+    public static final String BRIDGE_END = " ]";
+    public static final String SUCCESS_OR_FAIL = "게임 성공 여부: ";
+    public static final String GAME_COUNT = "총 시도한 횟수: ";
 }
