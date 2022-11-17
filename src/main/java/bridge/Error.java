645d487fa3c479e9bc5678a6f3068a9dcc4f3041
junpakPark <112045553+junpakPark@users.noreply.github.com>
2022-11-17 15:29:15 +0900

    style(Error): Enum 명 변경

--- src/main/java/bridge/Error.java
+++ src/main/java/bridge/Error.java
@@ -1,13 +1,14 @@
 package bridge;
 
-import bridge.validator.Command;
+import bridge.validator.ExitOption;
 import bridge.validator.Location;
 
 public enum Error {
 	FORMAT("자연수만 입력해주세요."),
 	RANGE(String.format("%d 에서 %d 사이의 자연수만 입력해 주세요", 3, 20)),
-	LOCATION(String.format("%s 혹은 %s만 입력해주세요.", Location.UP.getInputValue(), Location.DOWN.getInputValue())),
-	COMMAND(String.format("%s 혹은 %s만 입력해주세요.", Command.REPLAY.getValue(), Command.QUIT.getValue())),
+	LOCATION_COMMAND(String.format("%s 혹은 %s만 입력해주세요.", Location.UP.getCommand(), Location.DOWN.getCommand())),
+	LOCATION_NUMBER(String.format("%d 혹은 %d만 입력해주세요.", Location.UP.getNumber(), Location.DOWN.getNumber())),
+	EXIT_OPTION(String.format("%s 혹은 %s만 입력해주세요.", ExitOption.REPLAY.getCommand(), ExitOption.QUIT.getCommand())),
 	;
 
 	private static final String ERROR = "[ERROR] ";
