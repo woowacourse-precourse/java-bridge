54c7dcf9f960395b837ab43e9eff51308b0a7e50
JaeWon Choi <87649850+codesver@users.noreply.github.com>
2022-11-17 15:08:07 +0900

    feat(Movement): add values to enum

--- src/main/java/bridge/constant/Movement.java
+++ src/main/java/bridge/constant/Movement.java
@@ -1,5 +1,24 @@
 package bridge.constant;
 
 public enum Movement {
-    UP_AND_O, UP_AND_X, DOWN_AND_O, DOWN_AND_X
+    UP_AND_O(true, true),
+    UP_AND_X(true, false),
+    DOWN_AND_O(false, true),
+    DOWN_AND_X(false, false);
+
+    private boolean isUpper;
+    private boolean availability;
+
+    Movement(boolean isUpper, boolean availability) {
+        this.isUpper = isUpper;
+        this.availability = availability;
+    }
+
+    public boolean isUpper() {
+        return isUpper;
+    }
+
+    public boolean isAvailable() {
+        return availability;
+    }
 }
