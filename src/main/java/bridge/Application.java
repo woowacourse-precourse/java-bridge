bf5e6a86b4b30ad7fe3e3cca38de9b55f7478a68
Eungyu-code <Agyu625@gmail.com>
2022-11-17 22:42:42 +0900

    feat(Application class): startBridgeGame 메소드 추가
    다리 게임을 시작하는 메소드

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -4,6 +4,10 @@ public class Application {
 
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        startBridgeGame();
+    }
+
+    public static void startBridgeGame() {
         playBridgeGame();
     }
 
