9de7c96e9b25e372d66c3dc5163db4d8cf4ff052
K-IMjihun <62210749+K-IMjihun@users.noreply.github.com>
2022-11-17 12:06:40 +0900

    Application // 길이 입력을 위해 InputView의 readBridgeSize 호출

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -1,8 +1,16 @@
 package bridge;
 
-public class Application {
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Scanner;
 
+public class Application {
+    static InputView inputView = new InputView();
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        System.out.println("다리 건너기 게임을 시작합니다.");
+        System.out.println("\n" + "다리의 길이를 입력해주세요.");
+        inputView.readBridgeSize();
     }
 }
