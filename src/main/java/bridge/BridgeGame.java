e24fab32888ab29f253577ecf3fd665ab88f1563
kimdahee0815 <diana815@naver.com>
2022-11-17 23:55:41 +0900

    feat(retry) : retry의 인자(비교한 값)가 false이면 다리건너기를 실패한 것이므로 다시 시도할 수 있도록 기능 구현

--- src/main/java/bridge/BridgeGame.java
+++ src/main/java/bridge/BridgeGame.java
@@ -7,7 +7,7 @@ import java.util.List;
  * 다리 건너기 게임을 관리하는 클래스
  */
 public class BridgeGame {
-
+	
 	OutputView outputView= new OutputView();
 	/**
 	 * 사용자가 칸을 이동할 때 사용하는 메서드
@@ -15,11 +15,13 @@ public class BridgeGame {
 	 * 이동을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
 	 */
 	public void move(String inputMoving, List<String> madeBridge, int bridgeSize) {
-		if(compareMoving(inputMoving, madeBridge, order)) {
+		boolean compareResult = compareMoving(inputMoving, madeBridge, order);
+		if(compareResult) {
 			
 				
 		}
 		outputView.printMap();
+		retry(compareResult);
 	}
 	
 	public boolean compareMoving(String inputMoving, List<String> madeBridge, int order) {
@@ -34,7 +36,9 @@ public class BridgeGame {
 	 * <p>
 	 * 재시작을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
 	 */
-	public void retry() {
-		
+	public void retry(boolean compareResult) {
+		if(compareResult == false) {
+			System.out.println("게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q");
+		}
 	}
 }
