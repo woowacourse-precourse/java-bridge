220e6a1e9d65952d25f89f4dc973f5540aa3689c
junhei Lee <jun2ee22@naver.com>
2022-11-18 08:25:19 +0900

    test: 기능 테스트 경우의 수 추가

---
+++ src/test/java/bridge/MyTest.java
@@ -0,0 +1,110 @@
+package bridge;
+
+import bridge.Application;
+import bridge.BridgeNumberGenerator;
+import camp.nextstep.edu.missionutils.test.NsTest;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+
+public class MyTest extends NsTest {
+
+    @Test
+    void 기능_테스트() {
+        assertRandomNumberInRangeTest(() -> {
+            run("5", "U", "D", "U", "U", "U");
+            assertThat(output()).contains(
+                    "최종 게임 결과",
+                    "[ O |   | O | O | O ]",
+                    "[   | O |   |   |   ]",
+                    "게임 성공 여부: 성공",
+                    "총 시도한 횟수: 1"
+            );
+
+            int upSideIndex = output().indexOf("[ O |   | O | O | O ]");
+            int downSideIndex = output().indexOf("[   | O |   |   |   ]");
+            assertThat(upSideIndex).isLessThan(downSideIndex);
+        }, 1, 0, 1, 1, 1);
+    }
+
+    @Test
+    void 기능_테스트2() {
+        assertRandomNumberInRangeTest(() -> {
+            run("3", "U", "U", "R", "U", "D", "D");
+            assertThat(output()).contains(
+                    "[ O | X ]",
+                    "[   |   ]",
+                    "게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)",
+                    "최종 게임 결과",
+                    "[ O |   |   ]",
+                    "[   | O | O ]",
+                    "게임 성공 여부: 성공",
+                    "총 시도한 횟수: 2"
+            );
+
+            int upSideIndex = output().indexOf("[ O |   |   ]");
+            int downSideIndex = output().indexOf("[   | O | O ]");
+            assertThat(upSideIndex).isLessThan(downSideIndex);
+        }, 1, 0, 0);
+    }
+
+    @Test
+    void 기능_테스트3() {
+        assertRandomNumberInRangeTest(() -> {
+            run("3", "U", "U", "Q");
+            assertThat(output()).contains(
+                    "최종 게임 결과",
+                    "[ O | X ]",
+                    "[   |   ]",
+                    "게임 성공 여부: 실패",
+                    "총 시도한 횟수: 1"
+            );
+
+            int upSideIndex = output().indexOf("[ O | X ]");
+            int downSideIndex = output().indexOf("[   |   ]");
+            assertThat(upSideIndex).isLessThan(downSideIndex);
+        }, 1, 0, 1);
+    }
+
+    @Test
+    void 기능_테스트4() {
+        assertRandomNumberInRangeTest(() -> {
+            run("4","D","R", "U", "U", "R", "U", "D", "D", "R", "U", "D", "U", "U", "R", "U", "D", "U", "D");
+            assertThat(output()).contains(
+                    "최종 게임 결과",
+                    "[ O | X ]",
+                    "[   |   ]",
+                    "게임 성공 여부: 성공",
+                    "총 시도한 횟수: 5"
+            );
+
+            int upSideIndex = output().indexOf("[ O | X ]");
+            int downSideIndex = output().indexOf("[   |   ]");
+            assertThat(upSideIndex).isLessThan(downSideIndex);
+        }, 1, 0, 1, 0);
+    }
+
+    @Override
+    protected void runMain() {
+        Application.main(new String[]{});
+    }
+
+    static class TestNumberGenerator implements BridgeNumberGenerator {
+
+        private final List<Integer> numbers;
+
+        TestNumberGenerator(List<Integer> numbers) {
+            this.numbers = numbers;
+        }
+
+        @Override
+        public int generate() {
+            return numbers.remove(0);
+        }
+    }
+}
