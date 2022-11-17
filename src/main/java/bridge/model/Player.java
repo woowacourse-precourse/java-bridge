962e77f2f0338962fd2d08f877e5d78906cf04d7
woo-chang <clllickme@naver.com>
2022-11-17 20:20:51 +0900

    feat(Player): 이동 방향 목록 반환, 이동 방향 목록 추가하는 기능 구현

--- src/main/java/bridge/model/Player.java
+++ src/main/java/bridge/model/Player.java
@@ -1,15 +1,27 @@
 package bridge.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Player {
 
+    private final List<Direction> directions = new ArrayList<>();
     private int attempt = 1;
 
     public void attempt() {
         attempt += 1;
     }
 
+    public void move(Direction direction) {
+        directions.add(direction);
+    }
+
     public int getAttempt() {
         return attempt;
+    }
+
+    public List<Direction> getDirections() {
+        return new ArrayList<>(directions);
     }
 
 }
