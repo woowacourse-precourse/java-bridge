6eca3470c2224fe8eb9ee144e7a29f1354bcc8fb
EUNJEONGMUN <munej26@gmail.com>
2022-11-18 02:01:25 +0900

    feat: 재시작 여부를 확인하고 재시작 및 종료하는 retry메서드 구현

--- src/main/java/bridge/BridgeGame.java
+++ src/main/java/bridge/BridgeGame.java
@@ -39,11 +39,13 @@ public class BridgeGame {
     private boolean move(Moving moving, Movement movement) {
         return movement.canMove(moving);
     }
-    /**
-     * 사용자가 게임을 다시 시도할 때 사용하는 메서드
-     * <p>
-     * 재시작을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
-     */
+
     public void retry() {
+        Command command = inputView.readGameCommand();
+        if (command.isRetry()) {
+            movement.clearMoving();
+            startGame();
+        }
+        outputView.printResult();
     }
 }
