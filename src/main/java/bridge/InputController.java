ce84306e8c51437b4600ce4b5d364fbcda0cea9c
jkroh1995 <85445649+jkroh1995@users.noreply.github.com>
2022-11-18 03:49:03 +0900

    feat : 사용자가 입력한 이동 값을 검증하고 반환하는 기능 구현
    getMoving()함수 구현

--- src/main/java/bridge/InputController.java
+++ src/main/java/bridge/InputController.java
@@ -2,14 +2,27 @@ package bridge;
 
 public class InputController {
 
-    public static int getBridgeSize() {
+    InputView inputView = new InputView();
+
+    public int getBridgeSize() {
         try {
-            String size = InputView.readBridgeSize();
+            String size = inputView.readBridgeSize();
             new BridgeSizeValidator(size);
             return new Converter().toInt(size);
         } catch (IllegalArgumentException e) {
             OutputView.printError(e.getMessage());
             return getBridgeSize();
+        }
+    }
+
+    public String getMoving(){
+        try{
+            String moving = inputView.readMoving();
+            new MovingValidator(moving);
+            return moving;
+        } catch (IllegalArgumentException e){
+            OutputView.printError(e.getMessage());
+            return getMoving();
         }
     }
 
