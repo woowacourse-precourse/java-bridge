2c3b46a6f49cfa49005facd0a44b0018d60223cd
yeonhee <ydol2@naver.com>
2022-11-17 20:30:36 +0900

    feat(BridgeGameValidator) : 다리길이에 숫자가 아닌 경우 예외처리 추가

--- src/main/java/bridge/exception/BridgeGameValidator.java
+++ src/main/java/bridge/exception/BridgeGameValidator.java
@@ -2,9 +2,20 @@ package bridge.exception;
 
 public class BridgeGameValidator {
 
+    private static final String ERROR_MESSAGE_BRIDGE_SIZE = "[ERROR] 다리 길이는 3부터 20 사이의 숫자여야 합니다.";
+
     public static void isValidGameNumber(int number) {
         if (!(number >= 3 && number <= 20)) {
-            throw new IllegalArgumentException("[ERROR] 다리 길이는 3부터 20 사이의 숫자여야 합니다.");
+            throw new IllegalArgumentException(ERROR_MESSAGE_BRIDGE_SIZE);
+        }
+    }
+
+    public static int validateInt(String str){
+        try {
+            int stringToInt = Integer.parseInt(str);
+            return stringToInt;
+        }catch (Exception e){
+            throw new IllegalArgumentException(ERROR_MESSAGE_BRIDGE_SIZE);
         }
     }
 }
