5960a0a104c36fa4b9d95d27ee25dcbc59c847b3
jkroh1995 <85445649+jkroh1995@users.noreply.github.com>
2022-11-18 03:41:45 +0900

    feat : 사용자에게 다리 길이 입력을 요구하는 기능 구현
    readBridgeSize()구현

--- src/main/java/bridge/InputView.java
+++ src/main/java/bridge/InputView.java
@@ -1,15 +1,21 @@
 package bridge;
 
+import camp.nextstep.edu.missionutils.Console;
+
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
  */
 public class InputView {
+    public static final String PRINT_LENGTH = "다리의 길이를 입력해주세요.";
+    public static final String PRINT_MOVE = "이동할 칸을 선택해주세요. (위: U, 아래: D)";
+    public static final String PRINT_RETRY = "게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)";
 
     /**
      * 다리의 길이를 입력받는다.
      */
-    public int readBridgeSize() {
-        return 0;
+    public static String readBridgeSize() {
+        System.out.println(PRINT_LENGTH);
+        return Console.readLine();
     }
 
     /**
