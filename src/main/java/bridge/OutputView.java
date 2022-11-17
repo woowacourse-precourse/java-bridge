7bcda666a1053993c8e400f014a309a75bbf6a59
WhoIsWoony <WhoIsWoony@gmail.com>
2022-11-17 22:07:36 +0900

    feat: add print map

--- src/main/java/bridge/OutputView.java
+++ src/main/java/bridge/OutputView.java
@@ -34,7 +34,16 @@ public class OutputView {
      * <p>
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void printMap() {
+    public void printMap(List<String> board, int step, boolean isSuccess) {
+        if(!isSuccess) step -= 1;
+        List<String> resultUp = convertToMap("U", board, step);
+        List<String> resultDown = convertToMap("D", board, step);
+        if(!isSuccess){
+            addFailResult("U", board.get(step), resultUp);
+            addFailResult("D", board.get(step), resultDown);
+        }
+        System.out.println("[ "+String.join(" | ", resultUp)+" ]");
+        System.out.println("[ "+String.join(" | ", resultDown)+" ]");
     }
 
     /**
