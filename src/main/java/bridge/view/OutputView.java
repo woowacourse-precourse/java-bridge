3774b29f522e5ba6b83f90b8e0084b5cc48b51ed
K-jun98 <alap_u@naver.com>
2022-11-17 23:43:40 +0900

    feat - 플레이어가 선택한 칸이 건널 수 있다면 O, 건널 수 없다면 X로 표시한다.

--- src/main/java/bridge/view/OutputView.java
+++ src/main/java/bridge/view/OutputView.java
@@ -1,20 +1,32 @@
 package bridge.view;
 
-import camp.nextstep.edu.missionutils.Randoms;
+import bridge.domain.BridgeToken;
+import bridge.domain.Direction;
+import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * 사용자에게 게임 진행 상황과 결과를 출력하는 역할을 한다.
  */
 public class OutputView {
 
-    private static final String BRIDGE_GAME_START_MESSAGE = "다리 건너기 게임을 시작합니다.";
+    private static final String BRIDGE_GAME_START_MESSAGE = "다리 건너기 게임을 시작합니다.\n";
 
     /**
      * 현재까지 이동한 다리의 상태를 정해진 형식에 맞춰 출력한다.
      * <p>
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void printMap() {
+    public void printMap(List<BridgeToken> bridgeTokens) {
+        StringBuilder stringBuilder = new StringBuilder();
+        for (Direction direction : Direction.values()) {
+            stringBuilder.append("[ ");
+            stringBuilder.append(bridgeTokens.stream()
+                    .map(bridgeToken -> toSign(direction, bridgeToken))
+                    .collect(Collectors.joining(" | ")));
+            stringBuilder.append(" ]\n");
+        }
+        System.out.println(stringBuilder);
     }
 
     /**
@@ -22,10 +34,21 @@ public class OutputView {
      * <p>
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void printResult() {
+    public void printResult(List<BridgeToken> bridgeTokens) {
+        System.out.println("최종 게임 결과");
+        printMap(bridgeTokens);
     }
 
     public void printGameStartMessage() {
         System.out.println(BRIDGE_GAME_START_MESSAGE);
+    }
+
+    private String toSign(Direction direction, BridgeToken bridgeToken) {
+        if (direction.equals(bridgeToken.getDirection()) && bridgeToken.isCorrect()) {
+            return "O";
+        } else if (direction.equals(bridgeToken.getDirection()) && !bridgeToken.isCorrect()) {
+            return "X";
+        }
+        return " ";
     }
 }
