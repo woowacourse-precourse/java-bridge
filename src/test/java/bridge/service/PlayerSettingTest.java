84d79848aebf61162712ebbdbdeb6e906cd50ce9
KIMSEI1124 <workju1124@gmail.com>
2022-11-17 16:37:32 +0900

    test: 다리의 길이를 입력하는 테스트 기능 구현

---
+++ src/test/java/bridge/service/PlayerSettingTest.java
@@ -0,0 +1,34 @@
+package bridge.service;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+public class PlayerSettingTest extends PlayerSetting {
+    @Nested
+    @TestInstance(TestInstance.Lifecycle.PER_CLASS)
+    @DisplayName("플레이어의 다리 크기 입력받은 뒤 저장")
+    class setReadBridgeSizeToPlayerTest {
+        @ParameterizedTest
+        @MethodSource("data")
+        void test(String readBridgeSize, int validate) {
+            setBridgeSizeToPlayer(0);   // Param 변경 필요 Integer -> String
+            int playerBridgeSize = player.getBridgeSize();  // player 가 private 이여서 다른 방법 필요
+            Assertions.assertThat(playerBridgeSize).isEqualTo(validate);
+        }
+
+        Stream<Arguments> data() {
+            return Stream.of(
+                    Arguments.of("3", 3),
+                    Arguments.of("10", 10),
+                    Arguments.of("20", 20)
+            );
+        }
+    }
+}
