bfc340a72c49ddd9fb96a18e6b4ec793af3e94c5
making-a-scene <4936joo@naver.com>
2022-11-17 16:00:26 +0900

    feat: 사용자가 매 라운드마다 건넌 다리 현황을 저장할 UsersBridgeCrossStatus 클래스 생성

---
+++ src/main/java/bridge/UsersBridgeCrossStatus.java
@@ -0,0 +1,11 @@
+package bridge;
+
+import java.util.List;
+
+public class UsersBridgeCrossStatus {
+    private final List<List<String>> currentBridge;
+
+    public UsersBridgeCrossStatus(List<List<String>> currentBridge) {
+        this.currentBridge = currentBridge;
+    }
+}
