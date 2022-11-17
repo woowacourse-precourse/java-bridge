88bc3f987d0136890d1e088bd805301cb5dede24
ywj9811 <ywj9811@naver.com>
2022-11-18 00:37:45 +0900

    feat: Application 클래스 최종 예외 처리 추가

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -5,6 +5,11 @@ public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
         GameController gameController = new GameController();
-        gameController.gameTotalProgress();
+        try {
+            gameController.gameTotalProgress();
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+            return;
+        }
     }
 }
