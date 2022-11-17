9bc103aa900bafc6510d2754b4e12b52ea1af628
twoosky <lhjae3@naver.com>
2022-11-18 00:20:14 +0900

    feat(utils): 입력 요청 메시지 상수 선언

---
+++ src/main/java/bridge/utils/ViewMessages.java
@@ -0,0 +1,8 @@
+package bridge.utils;
+
+public class ViewMessages {
+    public static final String BRIDGE_LENGTH = "다리의 길이를 입력해주세요.";
+    public static final String SELECT_MOVE_SPACE = "이동할 칸을 선택해주세요. (위: U, 아래: D)";
+    public static final String RETRY_FLAG = "게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)";
+
+}
