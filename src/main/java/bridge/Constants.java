28a9244217387a67cf56ed2b8e2bedb6b4226a1f
jkroh1995 <85445649+jkroh1995@users.noreply.github.com>
2022-11-18 03:43:08 +0900

    feat : 상수들을 담을 클래스 생성
    Constants 클래스 생성

---
+++ src/main/java/bridge/Constants.java
@@ -0,0 +1,8 @@
+package bridge;
+
+public class Constants {
+
+    public final static String PATTERN = "^[0-9]*$";
+    public final static int MIN_LENGTH = 3;
+    public final static int MAX_LENGTH = 20;
+}
