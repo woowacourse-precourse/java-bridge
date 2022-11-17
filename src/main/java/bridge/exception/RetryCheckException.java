720835ad39b21a88f07375bce62816bd0c3d3996
YuHayeon <youha0930@gmail.com>
2022-11-18 00:46:37 +0900

    feat(RetryCheckException): 재시도 여부 입력이 R이나 Q가 아닐 경우 예외처리

---
+++ src/main/java/bridge/exception/RetryCheckException.java
@@ -0,0 +1,14 @@
+package bridge.exception;
+
+public class RetryCheckException {
+
+    private static final String RETRY = "R";
+    private static final String QUIT = "Q";
+    private static final String WRONG_RETRY_ERROR_MESSAGE = "[ERROR] 재시도는 R, 종료는 Q로 입력해야 합니다.";
+
+    public static void validate(String input) {
+        if (!input.equals(RETRY) && !input.equals(QUIT)) {
+            throw new IllegalArgumentException(WRONG_RETRY_ERROR_MESSAGE);
+        }
+    }
+}
