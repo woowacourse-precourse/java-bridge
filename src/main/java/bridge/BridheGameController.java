f0c43488b6f927583437dc70110a420c52bb3b0c
sinsu <sinsu19012@gmail.com>
2022-11-17 19:25:59 +0900

    ✨feat: "다리 건너기 게임을 시작합니다" 메세지 출력 기능 구현

---
+++ src/main/java/bridge/BridheGameController.java
@@ -0,0 +1,14 @@
+package bridge;
+
+public class BridheGameController {
+
+    public BridheGameController() {
+        gameStart();
+    }
+
+    private void gameStart() {
+        OutputView outputView = new OutputView();
+        outputView.gameStart();
+    }
+
+}
