f5c0c5227c76b8127a481b17ac4138e466674a99
SangWoo Yun <64531982+Awarduuu@users.noreply.github.com>
2022-11-18 00:09:53 +0900

    feat(InputController): 다리 길이 입력 뷰와 다리게임 연결

--- src/main/java/bridge/controller/InputController.java
+++ src/main/java/bridge/controller/InputController.java
@@ -1,5 +1,17 @@
 package bridge.controller;
 
+import bridge.BridgeNumberGenerator;
+import bridge.BridgeRandomNumberGenerator;
+import bridge.domain.BridgeGame;
+import bridge.domain.BridgeMaker;
+import bridge.view.InputView;
+
 public class InputController {
+
+    public static BridgeGame inputBridgeSize(){
+        BridgeNumberGenerator numberGenerator = new BridgeRandomNumberGenerator();
+        BridgeMaker bridgeMaker = new BridgeMaker(numberGenerator);
+        return new BridgeGame(bridgeMaker.makeBridge(InputView.readBridgeSize()));
+    }
 
 }
