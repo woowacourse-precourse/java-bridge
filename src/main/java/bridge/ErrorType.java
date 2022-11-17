0e67c8874923a6561eb2162c54078c4a9ed36bed
cpot5620 <=cpot5620@naver.com>
2022-11-17 17:53:00 +0900

    style(ErrorType): modify variable name to upper case

--- src/main/java/bridge/ErrorType.java
+++ src/main/java/bridge/ErrorType.java
@@ -4,7 +4,7 @@ public enum ErrorType {
     NUMERIC("3 ~ 20 사이의 숫자를 입력해주세요."),
     RANGE("다리 길이는 3 ~ 20 사이입니다."),
     MOVE("U(위 칸), D(아래 칸)만 입력해주세요."),
-    Command("R(재시작), Q(종료)만 입력해주세요.");
+    COMMAND("R(재시작), Q(종료)만 입력해주세요.");
 
     private final String message;
 
