20131419f7c44b04874716a0aadee6803ec3fd14
cocomozila <joseyun94@naver.com>
2022-11-17 23:07:38 +0900

    feat(InputView): 숫자가 아닐경우 예외처리

--- src/main/java/bridge/view/InputView.java
+++ src/main/java/bridge/view/InputView.java
@@ -1,15 +1,26 @@
 package bridge.view;
 
+import camp.nextstep.edu.missionutils.Console;
+
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
  */
 public class InputView {
 
-    /**
-     * 다리의 길이를 입력받는다.
-     */
+    // 다리 길이 입력받는 기능
     public int readBridgeSize() {
-        return 0;
+        String input = Console.readLine();
+        checkIsNumber(input);
+        return Integer.parseInt(input);
+    }
+
+    // 숫자가 아닐경우 예외처리
+    private void checkIsNumber(String input) {
+        for (int i = 0; i < input.length(); i++) {
+            if (!Character.isDigit(input.charAt(i))) {
+                throw new IllegalArgumentException("[ERROR] 3이상 20이하의 숫자를 입력해야 합니다.");
+            }
+        }
     }
 
     /**
