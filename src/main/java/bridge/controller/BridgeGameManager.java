f9e99c9406822de105a009a573b364a7de7b986a
JJongmen <pjy90123@gmail.com>
2022-11-17 15:08:54 +0900

    feat: 게임을 시작하는 기능 뼈대 코드 작성 및 패키지 구조 변경

---
+++ src/main/java/bridge/controller/BridgeGameManager.java
@@ -0,0 +1,22 @@
+package bridge.controller;
+
+public class BridgeGameManager {
+
+    public void play() {
+        makeBridge();
+        crossBridge();
+        printGameResult();
+    }
+
+    private void makeBridge() {
+
+    }
+
+    private void crossBridge() {
+
+    }
+
+    private void printGameResult() {
+
+    }
+}
