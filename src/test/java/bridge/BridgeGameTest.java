4e58838a6c0fbd84b3da9e7340c5a3939a69446c
yumyeonghan <mgtmh991013@gmail.com>
2022-11-17 16:48:31 +0900

    test: add BridgeGameTest
    Test the method to move a position

---
+++ src/test/java/bridge/BridgeGameTest.java
@@ -0,0 +1,27 @@
+package bridge;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+
+class BridgeGameTest {
+
+    private BridgeGame bridgeGame;
+
+    @BeforeEach
+    void 객체_생성() {
+        bridgeGame = new BridgeGame(new Player(), new BridgeMaker(new BridgeRandomNumberGenerator()));
+    }
+
+    @Test
+    void 칸을_이동하는_메서드_테스트 () {
+
+        Assertions.assertThat(bridgeGame.move(4, "U", Arrays.asList("U", "D", "U")))
+                .isFalse();
+
+        Assertions.assertThat(bridgeGame.move(2, "U", Arrays.asList("U", "D", "U")))
+                .isTrue();
+    }
+}
