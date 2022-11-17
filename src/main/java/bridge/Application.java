6251669e852de2c77659bd086fefd1cdd78649b0
KimTaeKang57 <zizoen123@gmail.com>
2022-11-17 14:39:59 +0900

    feat: 다리의 길이를 입력하는 기능 추가

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -1,8 +1,18 @@
 package bridge;
 
+import java.util.List;
+
 public class Application {
 
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        // 다리 건너기 게임 시작
+        System.out.println("다리 건너기 게임을 시작합니다.");
+
+        // 다리의 길이를 입력
+        System.out.println("다리의 길이를 입력해주세요.");
+        InputView inputView = new InputView();
+        int size = inputView.readBridgeSize();
+
     }
 }
