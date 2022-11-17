ae72501107c4b28d9bf8a3f1b77b00b195239c55
Jeangyu <gjwlsrb1700@naver.com>
2022-11-17 15:25:12 +0900

    [FEAT: bridge] 이동할 칸 입력이 U, D인지 검증하는 메서드 구현

--- src/main/java/bridge/InputView.java
+++ src/main/java/bridge/InputView.java
@@ -27,6 +27,8 @@ public class InputView {
         System.out.println("이동할 칸을 선택해주세요. (위: U, 아래: D)");
         String userInput = readLine();
 
+        Validator.isMoveAlpha(userInput);
+
         return userInput;
     }
 
--- src/main/java/bridge/Validator.java
+++ src/main/java/bridge/Validator.java
@@ -6,4 +6,10 @@ public class Validator {
             throw new IllegalArgumentException("[ERROR] 입력이 숫자가 아닙니다.");
         }
     }
+
+    public static void isMoveAlpha(String alpha){
+        if (!(alpha == "D" || alpha == "U")){
+            throw new IllegalArgumentException("[ERROR] 이동할 칸은 U, D의 입력만 가능합니다.");
+        }
+    }
 }
