96d63248f9025668ef9c660974968196bf61d3ac
juhwang <juhong4930@naver.com>
2022-11-17 15:08:56 +0900

    test(BridgeGameTest): fail input retry command

--- src/test/java/bridge/BridgeGameTest.java
+++ src/test/java/bridge/BridgeGameTest.java
@@ -6,8 +6,7 @@ import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
-import static org.assertj.core.api.Assertions.*;
-import static org.assertj.core.util.Lists.newArrayList;
+import static org.assertj.core.api.Assertions.assertThat;
 
 class BridgeGameTest {
 
@@ -15,7 +14,7 @@ class BridgeGameTest {
 
     @BeforeEach
     void createBridgeGame() {
-        bridgeGame = new BridgeGame(List.of("U","D","D"));
+        bridgeGame = new BridgeGame(List.of("U", "D", "D"));
     }
 
     @DisplayName("올바른 다리를 건넌 경우 true 반환")
@@ -28,6 +27,12 @@ class BridgeGameTest {
     @Test
     void moveIncorrectBridge() {
         assertThat(bridgeGame.move("D")).isFalse();
+    }
+
+    @DisplayName("재시작을 한 경우 true 반환")
+    @Test
+    void retry() {
+        assertThat(bridgeGame.retry("R")).isTrue();
     }
 
 
