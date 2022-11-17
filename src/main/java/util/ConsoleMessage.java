6df7fd8dd86bbef861380ff764e5bd595a185f0c
gyuturn <rbtjrrbals@gmail.com>
2022-11-17 22:23:34 +0900

    feat: 게임시작 안내문구 출력
    A, A-1

---
+++ src/main/java/util/ConsoleMessage.java
@@ -0,0 +1,5 @@
+package util;
+
+public class ConsoleMessage {
+    public static final String START_BRIDGE_GAME="다리 건너기 게임을 시작합니다.";
+}
--- src/main/java/view/OutputView.java
+++ src/main/java/view/OutputView.java
@@ -1,4 +1,8 @@
-package bridge;
+package view;
+
+import static util.ConsoleMessage.*;
+
+import util.ConsoleMessage;
 
 /**
  * 사용자에게 게임 진행 상황과 결과를 출력하는 역할을 한다.
@@ -19,5 +23,9 @@ public class OutputView {
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
     public void printResult() {
+    }
+
+    public static void printStartGame(){
+        System.out.println(START_BRIDGE_GAME);
     }
 }
