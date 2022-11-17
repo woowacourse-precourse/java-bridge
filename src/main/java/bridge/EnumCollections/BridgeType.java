47d17ac316ad235c4cc391297a4d17f09d0b477c
Park shinyeong <syngh503@gmail.com>
2022-11-17 21:41:32 +0900

    Feat(BridgeType): 다리 위치에 대한 enum 생성
    다리 위치 U, D에 대한 enum을 생성하였습니다.

--- src/main/java/bridge/EnumCollections/BridgeType.java
+++ src/main/java/bridge/EnumCollections/BridgeType.java
@@ -1,4 +1,26 @@
 package bridge.EnumCollections;
 
-public class BridgeType {
+public enum BridgeType {
+    UP('U', 1),
+    DOWN('D', 0);
+
+    private char position;
+    private int randomValue;
+
+    BridgeType(char position, int randomValue) {
+        this.position = position;
+        this.randomValue = randomValue;
+    }
+
+    public char getPosition() {
+        return position;
+    }
+
+    public int getRandomValue() {
+        return randomValue;
+    }
+
+    public static boolean isPosition(char input) {
+        return input == UP.getPosition() || input == DOWN.getPosition();
+    }
 }
