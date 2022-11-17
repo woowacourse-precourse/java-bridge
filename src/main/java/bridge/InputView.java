594546f9ec1860e2f46a1b5b3f308ea8b1d320d4
hoonn9 <hun9935@gmail.com>
2022-11-17 22:40:14 +0900

    feat(InputView): readBridgeSize 메소드, 검증 구현

--- src/main/java/bridge/InputView.java
+++ src/main/java/bridge/InputView.java
@@ -1,5 +1,7 @@
 package bridge;
 
+import camp.nextstep.edu.missionutils.Console;
+
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
  */
@@ -9,7 +11,15 @@ public class InputView {
      * 다리의 길이를 입력받는다.
      */
     public int readBridgeSize() {
-        return 0;
+        int numberInput = getNumber();
+        validateBridgeSize(numberInput);
+        return numberInput;
+    }
+    
+    private void validateBridgeSize(int input) {
+        if (input < 1) {
+            throw new IllegalArgumentException("다리 길이는 1 이상이여야 합니다.");
+        }
     }
 
     /**
@@ -24,5 +34,19 @@ public class InputView {
      */
     public String readGameCommand() {
         return null;
+    }
+    
+    public int getNumber() throws IllegalArgumentException {
+        String line = Console.readLine();
+        validateNumberString(line);
+        return Integer.parseInt(line);
+    }
+    
+    private void validateNumberString(String line) {
+        for (char c : line.toCharArray()) {
+            if (!Character.isDigit(c)) {
+                throw new IllegalArgumentException("잘못된 입력입니다. 다시 시도하세요.");
+            }
+        }
     }
 }
