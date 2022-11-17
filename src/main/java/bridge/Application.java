4d37cfa49170ec8402a88f44c843eb627bbc1924
devmizz <garamminchan@gmail.com>
2022-11-18 02:40:47 +0900

    fix: 메인 어플리케이션 변경
    다리 초기화 부분을 메서드 분리
    예외처리, 메서드 길이 축소를 위함

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -1,8 +1,22 @@
 package bridge;
 
+import java.util.List;
+
 public class Application {
+
+    private static InputView inputView = new InputView();
+    private static OutputView outputView = new OutputView();
 
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        Application app = new Application();
+
+        List<String> bridge = app.initializeBridge();
+    }
+
+    private List<String> initializeBridge() {
+        BridgeMaker bridgeMaker = new BridgeMaker(new BridgeRandomNumberGenerator());
+
+        return bridgeMaker.makeBridge(inputView.readBridgeSize());
     }
 }
