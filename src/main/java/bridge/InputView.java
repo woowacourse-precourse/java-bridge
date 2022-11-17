5aa5734886c79293ff1e0aa0e2547a68c30ccdb4
dohyun <ldh19738@gmail.com>
2022-11-17 20:20:42 +0900

    feat(InputView): 입력에 대한 메시지 추가

--- src/main/java/bridge/InputView.java
+++ src/main/java/bridge/InputView.java
@@ -6,18 +6,24 @@ import camp.nextstep.edu.missionutils.Console;
  * 사용자로부터 입력을 받는 역할을 한다.
  */
 public class InputView {
+    private static final String INPUT_BRIDGE_SIZE_MESSAGE = "다리의 길이를 입력해주세요.";
+    private static final String CHOOSE_MOVING_BRIDGE_MESSAGE = "이동할 칸을 선택해주세요. (위: U, 아래: D)";
+    private static final String RESTART_MESSAGE = "게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)";
 
     /**
      * 다리의 길이를 입력받는다.
      */
     public int readBridgeSize() {
-        return Integer.parseInt(Console.readLine());
+        System.out.println(INPUT_BRIDGE_SIZE_MESSAGE);
+        String bridgeSize = Console.readLine();
+        return Integer.parseInt(bridgeSize);
     }
 
     /**
      * 사용자가 이동할 칸을 입력받는다.
      */
     public String readMoving() {
+        System.out.println(CHOOSE_MOVING_BRIDGE_MESSAGE);
         return Console.readLine();
     }
 
@@ -25,6 +31,7 @@ public class InputView {
      * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
      */
     public String readGameCommand() {
+        System.out.println(RESTART_MESSAGE);
         return Console.readLine();
     }
 }
