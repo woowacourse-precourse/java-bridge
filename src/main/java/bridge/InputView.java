848db8212613d223c28cb25d403caf5998983821
chanwonlee <clw8679@naver.com>
2022-11-17 14:12:11 +0900

    feat: InputView 기능 구현

--- src/main/java/bridge/InputView.java
+++ src/main/java/bridge/InputView.java
@@ -1,5 +1,9 @@
 package bridge;
 
+import camp.nextstep.edu.missionutils.Console;
+
+import java.util.Objects;
+
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
  */
@@ -9,20 +13,29 @@ public class InputView {
      * 다리의 길이를 입력받는다.
      */
     public int readBridgeSize() {
-        return 0;
+        System.out.println("다리의 길이를 입력해주세요.");
+        String input = Console.readLine();
+        validationInteger(input);
+        int bridgeSize = Integer.parseInt(input);
+        validationBridgeSize(bridgeSize);
+        return bridgeSize;
     }
 
     /**
      * 사용자가 이동할 칸을 입력받는다.
      */
     public String readMoving() {
-        return null;
+        String moveCommand = Console.readLine();
+        validationMoveCommand(moveCommand);
+        return moveCommand;
     }
 
     /**
      * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
      */
     public String readGameCommand() {
-        return null;
+        String gameCommand = Console.readLine();
+        validationGameCommand(gameCommand);
+        return gameCommand;
     }
 }
