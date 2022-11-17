83c1c68783ec5caa642d7a22152fc60be5cd672e
chanwonlee <clw8679@naver.com>
2022-11-17 22:03:31 +0900

    feat: 사용자가 다리를 이동하는 기능 추가 [BridgeGameController]

--- src/main/java/bridge/BridgeGameController.java
+++ src/main/java/bridge/BridgeGameController.java
@@ -6,6 +6,22 @@ public class BridgeGameController {
     public static void run() {
         int bridgeSize = inputBridgeSize();
         List<String> bridge = createBridge(bridgeSize);
+        BridgeGame bridgeGame = new BridgeGame(bridge);
+        boolean successGame = isSuccessGame(bridgeGame, bridgeSize);
+        OutputView.printResult(bridgeGame, successGame);
+    }
+
+    private static boolean isSuccessGame(BridgeGame bridgeGame, int bridgeSize) {
+        boolean continueGame = true;
+        while (continueGame) {
+            State state = inputMoveCommand();
+            continueGame = bridgeGame.move(state);
+            OutputView.printMap(bridgeGame, continueGame);
+            if (bridgeGame.getCurrentBridgeIndex() == bridgeSize - 1) {
+                return true;
+            }
+        }
+        return false;
     }
 
     private static int inputBridgeSize() {
@@ -14,6 +30,24 @@ public class BridgeGameController {
         } catch (IllegalArgumentException e) {
             System.out.println(e.getMessage());
             return inputBridgeSize();
+        }
+    }
+
+    private static State inputMoveCommand() {
+        try {
+            return InputView.readMoving();
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+            return inputMoveCommand();
+        }
+    }
+
+    private static String inputGameCommand() {
+        try {
+            return InputView.readGameCommand();
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+            return inputGameCommand();
         }
     }
 
