fa426532c7e9f90aa81f9dbedc642adc44dec3a5
saewoo1 <wooozooo1123@gmail.com>
2022-11-17 20:17:51 +0900

    feat(BridgeGame): move 메소드 구현방식 변경
    자체에서 for문을 돌면서 처음부터 검사하는 방식 대신
    외부에서 for문을 돌리고, move 메소드는 오직 정답인지 아닌지만 판별하도록 수정

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -1,12 +1,27 @@
 package bridge;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Application {
 
     public static void main(String[] args) {
         // TODO: 프로그램 구현
         BridgeMaker bridgeMaker = new BridgeMaker(new BridgeRandomNumberGenerator());
+        BridgeGame bridgeGame = new BridgeGame();
+        List<String> test1 = new ArrayList<>();
+        test1.add("U");
+        test1.add("D");
+        test1.add("U");
+        List<String> test2 = new ArrayList<>();
+        test2.add("U");
+        test2.add("D");
+        System.out.println(test1.getClass().getName());
+        System.out.println(test2.getClass().getName());
+        String test3 = "U";
 
-        bridgeMaker.makeBridge(4);
+//        bridgeMaker.makeBridge(4);
+        bridgeGame.move(test1, test2, test3);
 
     }
 }
--- src/main/java/bridge/BridgeGame.java
+++ src/main/java/bridge/BridgeGame.java
@@ -7,18 +7,19 @@ import java.util.List;
  * 다리 건너기 게임을 관리하는 클래스
  */
 public class BridgeGame {
-
+    OutputView outputView = new OutputView();
     /**
      * 사용자가 칸을 이동할 때 사용하는 메서드
      * <p>
      * 이동을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
+
+    // 외부에 새 currentBridge를 선언하고, 하나씩 추가하는 방식은 어떨까ㅏㅏ..
     public void move(List<String> bridgeMap, List<String> currentBridge, String userMovingValue) {
-        for (int i = 0; i < bridgeMap.size(); i++) {
-            if (bridgeMap.get(i).equals(userMovingValue)) {
-                currentBridge.add("O");
-            }
+        if (bridgeMap.get(currentBridge.size()).equals(userMovingValue)) {
+            currentBridge.add("O");
         }
+        retry();
     }
 
     /**
