eae73a73f842ef548fb48fffc6510515e42ec115
TaeyeonRoyce <skdlxmtpqms@gmail.com>
2022-11-17 10:46:06 +0900

    feat(BridgeGameController): BridgeGameController의 의존관계를 추가합니다

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -1,8 +1,17 @@
 package bridge;
 
+import bridge.domain.bridge.application.BridgeGameController;
+import bridge.ui.input.InputView;
+import bridge.ui.output.OutputView;
+
 public class Application {
 
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        InputView inputView = new InputView();
+        OutputView outputView = new OutputView();
+        BridgeGameController bridgeGameController = new BridgeGameController(inputView, outputView);
+
+        bridgeGameController.init();
     }
 }
--- src/main/java/bridge/domain/bridge/application/BridgeGameController.java
+++ src/main/java/bridge/domain/bridge/application/BridgeGameController.java
@@ -1,4 +1,14 @@
 package bridge.domain.bridge.application;
 
+import bridge.ui.input.InputView;
+import bridge.ui.output.OutputView;
+
 public class BridgeGameController {
+    private final InputView inputView;
+    private final OutputView outputView;
+
+    public BridgeGameController(InputView inputView, OutputView outputView) {
+        this.inputView = inputView;
+        this.outputView = outputView;
+    }
 }
