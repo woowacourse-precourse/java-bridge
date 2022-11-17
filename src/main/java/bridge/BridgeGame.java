35af04484c606b23c60d0b6a09327b1d32071fa9
minaver <psmjoshua@gmail.com>
2022-11-17 22:03:25 +0900

    feat(BridgeGame): 다음 내딪을 칸을 잘 내딪었는지 확인하는 메서드 추가

--- src/main/java/bridge/BridgeGame.java
+++ src/main/java/bridge/BridgeGame.java
@@ -18,10 +18,11 @@ public class BridgeGame {
 
     /**
      * 사용자가 칸을 이동할 때 사용하는 메서드
-     * <p>
-     * 이동을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
+     * @param nextStep 다음 내딪을 위치. 위 칸이면 "U", 아래 칸이면 "D"
+     * @return 현재까지의 점수판(gradingBoard)과 내딪은 위치가 정답인지 여부(isCorrect)를 표현한다.
      */
-    public void move() {
+    public BridgeGameResultDto move(String nextStep) {
+
     }
 
     /**
@@ -31,4 +32,18 @@ public class BridgeGame {
      */
     public void retry() {
     }
+
+    /**
+     * 다음 내딪을 칸을 잘 내딪었는지 확인하는 메서드
+     * @param nextStep 다음 내딪을 위치. 위 칸이면 "U", 아래 칸이면 "D"
+     * @return nextStep이 정답이면 true, 정답이 아니면 false
+     */
+    private boolean compareNextStep(String nextStep) {
+        int nextPosition = gradingBoard.get(0).size();
+        if (answerBridge.get(nextPosition).equals(nextStep)) {
+            return true;
+        }
+        return false;
+    }
+
 }
