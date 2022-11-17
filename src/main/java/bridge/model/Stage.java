2db79ea0dd6110c7dbb1f9d7f7ff0653b4a5f04b
junhei Lee <jun2ee22@naver.com>
2022-11-17 20:48:14 +0900

    feat(domain): 입력 받은 칸을 판별해 진행 상황을 저장하는 기능

---
+++ src/main/java/bridge/model/Stage.java
@@ -0,0 +1,27 @@
+package bridge.model;
+
+public enum Stage {
+    CROSS_UP_STAIR("U", "U", " -O"),
+    CROSS_DOWN_STAIR("D", "D", "O- "),
+    CAN_NOT_CROSS_UP_STAIR("U", "D", " -X"),
+    CON_NOT_CROSS_DOWN_STAIR("D", "U", "X- ");
+
+    private final String bridgeStage;
+    private final String userMoving;
+    private final String mapElement;  //"아래 칸-위 칸"
+
+    private Stage(String bridgeStage, String userMoving, String mapElement) {
+        this.bridgeStage = bridgeStage;
+        this.userMoving = userMoving;
+        this.mapElement = mapElement;
+    }
+
+    public static String judge(String bridgeStage, String userMoving) {
+        for (Stage stage : Stage.values()) {
+            if (bridgeStage == stage.bridgeStage && userMoving == stage.userMoving) {
+                return stage.mapElement;
+            }
+        }
+        throw new IllegalArgumentException();  //상황에 맞는 예외 찾아보기
+    }
+}
---
+++ src/test/java/bridge/model/BridgeGameTest.java
@@ -0,0 +1,35 @@
+package bridge.model;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class BridgeGameTest {
+    BridgeGame bridgeGame = new BridgeGame();
+
+
+    @DisplayName("다리가 위 칸일 때, 사용자가 U과 D을 입력한 경우 테스트")
+    @Test
+    void saveStageWhenBridgeStageIsUp() {
+        List<List<String>> currentMap = new ArrayList<>();
+        bridgeGame.move("U", "U");
+        currentMap.addAll(bridgeGame.move("U", "D"));
+
+        Assertions.assertThat(currentMap).isEqualTo(List.of(List.of(" ", "O"), List.of(" ", "X")));
+    }
+
+    @DisplayName("다리가 아래 칸일 때, 사용자가 U과 D을 입력한 경우 테스트")
+    @Test
+    void saveStageWhenBridgeStageIsDown() {
+        List<List<String>> currentMap = new ArrayList<>();
+        bridgeGame.move("D", "U");
+        currentMap.addAll(bridgeGame.move("D", "D"));
+
+        Assertions.assertThat(currentMap).isEqualTo(List.of(List.of("X", " "), List.of("O", " ")));
+    }
+
+
+}
