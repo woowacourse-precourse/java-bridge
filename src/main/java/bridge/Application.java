c6c900e77f8d7ee965858e00f3672159f0bb5b7d
YoosunMoon <muzikue@naver.com>
2022-11-18 01:57:20 +0900

    feat: 건널 수 있는 칸인지 판단하고 진행 상황 출력

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -3,18 +3,29 @@ package bridge;
 import java.util.List;
 
 public class Application {
-
     public static void main(String[] args) {
         InputView inputView = InputView.getInstance();
         OutputView outputView = OutputView.getInstance();
 
         outputView.printStartMessage();
+
         int bridgeSize = inputView.readBridgeSize();
-
         BridgeMaker bridgeMaker = new BridgeMaker(new BridgeRandomNumberGenerator());
-
         List<String> bridge = bridgeMaker.makeBridge(bridgeSize);
+        int bridgeIdx = 0;
+        BridgeGame bridgeGame = new BridgeGame();
 
-        String moving = inputView.readMoving();
+        while (bridgeIdx < bridgeSize) {
+            boolean isMoved =
+                    bridgeGame.move(inputView.readMoving(), bridge.get(bridgeIdx));
+
+            outputView.printMap(bridgeGame.bridgeMapToString());
+
+            if (!isMoved) {
+                break;
+            }
+
+            bridgeIdx++;
+        }
     }
 }
--- src/main/java/bridge/BridgeGame.java
+++ src/main/java/bridge/BridgeGame.java
@@ -4,13 +4,32 @@ package bridge;
  * 다리 건너기 게임을 관리하는 클래스
  */
 public class BridgeGame {
+    // 이 클래스에 필드(인스턴스 변수)를 추가할 수 있다.
+    // 이 클래스의 패키지는 변경할 수 있다.
+    // 이 클래스의 메서드 이름은 변경할 수 없고!! 인자와 반환 타입은 필요에 따라 추가하거나 변경할 수 있다.
+    // 게임 진행을 위해 필요한 메서드를 추가하거나 변경할 수 있다.
+    // 이 클래스에서 InputView, OutputView 사용하지 않는다!
+
+    private static final String UP = "U";
+
+    private BridgeMap bridgeMap;
+
+    public BridgeGame() {
+        this.bridgeMap = new BridgeMap();
+    }
 
     /**
      * 사용자가 칸을 이동할 때 사용하는 메서드
      * <p>
      * 이동을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void move() {
+    public boolean move(String moving, String bridgeBlock) {
+        boolean isUpSide = moving.equals(UP);
+        boolean isRight = moving.equals(bridgeBlock);
+
+        this.bridgeMap.add(isUpSide, isRight);
+
+        return isRight;
     }
 
     /**
@@ -19,5 +38,9 @@ public class BridgeGame {
      * 재시작을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
     public void retry() {
+    }
+
+    public String bridgeMapToString() {
+        return bridgeMap.toString();
     }
 }
