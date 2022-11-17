9afdf7b0c75946fddf5e9e794f9d2030a7ee7137
mckim123 <minchan06@gmail.com>
2022-11-17 22:30:28 +0900

    feat: change BridgeGame class to implement getMap in OutputView
    Make full map when construct instance of BridgeGame
    When getMap() is called, get sublists of upperMap, lowerMap
    - if last result is false, change last symbol of each sublist to X or blank
    Locate BridgeMaker, BridgeRandomNumberGenerator inside constructor

--- src/main/java/bridge/domain/BridgeGame.java
+++ src/main/java/bridge/domain/BridgeGame.java
@@ -3,6 +3,9 @@ package bridge.domain;
 import bridge.BridgeMaker;
 import bridge.BridgeRandomNumberGenerator;
 import bridge.constant.GameCommand;
+import bridge.constant.Moving;
+import bridge.constant.PrintMessage;
+import java.util.ArrayList;
 import java.util.List;
 
 public class BridgeGame {
@@ -10,18 +13,37 @@ public class BridgeGame {
     private int trials;
     private int currentPosition;
     private int bridgeSize;
-    private BridgeRandomNumberGenerator bridgeRandomNumberGenerator = new BridgeRandomNumberGenerator();
-    private BridgeMaker bridgeMaker = new BridgeMaker(bridgeRandomNumberGenerator);
+    private boolean isCorrect;
+    private List<String> fullUpperMap;
+    private List<String> fullLowerMap;
 
     public BridgeGame(int bridgeSize) {
+        BridgeRandomNumberGenerator bridgeRandomNumberGenerator = new BridgeRandomNumberGenerator();
+        BridgeMaker bridgeMaker = new BridgeMaker(bridgeRandomNumberGenerator);
         this.bridge = bridgeMaker.makeBridge(bridgeSize);
         this.trials = 1;
         this.bridgeSize = bridgeSize;
         this.currentPosition = 0;
+        fullUpperMap = new ArrayList<>();
+        fullLowerMap = new ArrayList<>();
+        makeFullMap();
+    }
+
+    private void makeFullMap() {
+        for (String answer : bridge) {
+            if (answer.equals(Moving.U.toString())) {
+                fullUpperMap.add(PrintMessage.MAP_RIGHT.getString());
+                fullLowerMap.add(PrintMessage.MAP_BLANK.getString());
+                continue;
+            }
+            fullUpperMap.add(PrintMessage.MAP_BLANK.getString());
+            fullLowerMap.add(PrintMessage.MAP_RIGHT.getString());
+        }
     }
 
     public boolean move(String moving) {
-        return bridge.get(currentPosition++).equals(moving);
+        isCorrect = bridge.get(currentPosition++).equals(moving);
+        return isCorrect;
     }
 
     public boolean retry(String gameCommand) {
@@ -37,8 +59,21 @@ public class BridgeGame {
         return (currentPosition < bridgeSize);
     }
 
-    public String getMap() {
-        return null;
+    public List<String> getMap() {
+        List<String> totalMap = new ArrayList<>(fullUpperMap.subList(0, currentPosition));
+        totalMap.addAll(fullLowerMap.subList(0, currentPosition));
+        if (!isCorrect) {
+            totalMap.set(currentPosition - 1, changeSymbol(totalMap.get(currentPosition - 1)));
+            totalMap.set(2 * currentPosition - 1, changeSymbol(totalMap.get(2 * currentPosition - 1)));
+        }
+        return totalMap;
+    }
+
+    private String changeSymbol(String s) {
+        if (s.equals(PrintMessage.MAP_RIGHT.getString())) {
+            return PrintMessage.MAP_BLANK.getString();
+        }
+        return PrintMessage.MAP_WRONG.getString();
     }
 
     public int getTrials() {
