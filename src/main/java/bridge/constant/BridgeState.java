5b1348444ff089efa19cd238a8735dccf996d11b
gomljo <gomljo0307@naver.com>
2022-11-17 18:28:26 +0900

    feat(BridgeState): 정의된 관계를 이용하여 숫자로 생성된 다리 상태를 확인하는 기능 구현

--- src/main/java/bridge/constant/BridgeState.java
+++ src/main/java/bridge/constant/BridgeState.java
@@ -1,5 +1,8 @@
 package bridge.constant;
 
+import java.util.Arrays;
+import java.util.NoSuchElementException;
+
 public enum BridgeState {
     UP(1,"U"),
     DOWN(0, "D");
@@ -12,8 +15,13 @@ public enum BridgeState {
         this.bridgeState=bridgeState;
     }
 
+    private static boolean isSameBridgeState(BridgeState bridgeState, int numberBridgeState){
+        return bridgeState.numberBridgeState==numberBridgeState;
+    }
 
-
-
+    private static boolean contains(int numberBridgeState){
+        return Arrays.stream(BridgeState.values())
+                .anyMatch(bridgeState -> isSameBridgeState(bridgeState, numberBridgeState));
+    }
 
 }
