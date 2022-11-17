cf94147bd15c36892a11ffaabe340fc3047bcc77
jjmin9797 <jjmin9797@naver.com>
2022-11-17 19:43:38 +0900

    feat(User): User 클래스 생성자 구현

--- src/main/java/bridge/domain/User.java
+++ src/main/java/bridge/domain/User.java
@@ -7,4 +7,8 @@ public class User {
 
     private List<String> bridge;
     private ArrayList<String> userBridge;
+
+    public void User(List<String> bridge) {
+        this.bridge = bridge;
+    }
 }
