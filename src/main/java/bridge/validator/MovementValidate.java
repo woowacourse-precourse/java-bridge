4fe7741ddc7fc3341b42cd04c4cd2d341858352e
KyungKyu Park <rudrb0505@gmail.com>
2022-11-17 17:31:14 +0900

    feat(MovementValidate): 이동할 칸을 잘못 입력한 경우 예외 처리

--- src/main/java/bridge/validator/MovementValidate.java
+++ src/main/java/bridge/validator/MovementValidate.java
@@ -3,18 +3,26 @@ package bridge.validator;
 public class MovementValidate {
 
 	public static void validateCharacter(String input) {
-		throw new IllegalArgumentException("여러 개의 문자를 입력할 수 없습니다.");
+		if (input.length() != 1) {
+			throw new IllegalArgumentException("여러 개의 문자를 입력할 수 없습니다.");
+		}
 	}
 
 	public static void validateEnglish(String input) {
-		throw new IllegalArgumentException("영어만 입력할 수 있습니다.");
+		if (!input.matches("^[a-zA-Z]+$")) {
+			throw new IllegalArgumentException("영어만 입력할 수 있습니다.");
+		}
 	}
 
 	public static void validateUpperCase(String input) {
-		throw new IllegalArgumentException("대문자만 입력할 수 있습니다.");
+		if (!input.matches("^[A-Z]+$")) {
+			throw new IllegalArgumentException("대문자만 입력할 수 있습니다.");
+		}
 	}
 
 	public static void validateExactCharacter(String input) {
-		throw new IllegalArgumentException("U와 D만 입력할 수 있습니다.");
+		if (!input.matches("^[UD]+$")) {
+			throw new IllegalArgumentException("U와 D만 입력할 수 있습니다.");
+		}
 	}
 }
