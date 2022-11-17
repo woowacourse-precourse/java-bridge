f9ac0e84447fde11be948aa0d166d5abe0ba61bb
pilyang <30036534+pilyang@users.noreply.github.com>
2022-11-17 15:33:58 +0900

    feat(Bridge): 다리 생성 구현
    - Bridge class 생성
    - private 생성자 구현
    - List<String> 을 통한 생성 메서드 구현
    - 다리 사이즈 validation 구현

---
+++ src/main/java/bridge/domain/Bridge.java
@@ -0,0 +1,36 @@
+package bridge.domain;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Bridge {
+
+    public static final int MIN_SIZE = 3;
+    public static final int MAX_SIZE = 20;
+
+    public static final String BRIDGE_SIZE_EXCEPTION_MESSAGE = "[ERROR] 다리의 길이는 %d부터 %d 사이의 숫자여야 합니다.";
+
+    private final List<BridgeTile> bridgeTiles;
+
+    private Bridge(List<BridgeTile> bridgeTiles) {
+        this.bridgeTiles = List.copyOf(bridgeTiles);
+    }
+
+    public static Bridge from(List<String> bridgePositions) {
+        validateBridgePositions(bridgePositions);
+
+        List<BridgeTile> tiles = new ArrayList<>();
+        for (String position : bridgePositions) {
+            tiles.add(BridgeTile.findByTilePosition(position));
+        }
+
+        return new Bridge(tiles);
+    }
+
+
+    private static void validateBridgePositions(List<String> bridgePositions) {
+        if (bridgePositions.size() < MIN_SIZE || bridgePositions.size() > MAX_SIZE) {
+            throw new IllegalArgumentException(String.format(BRIDGE_SIZE_EXCEPTION_MESSAGE, MIN_SIZE, MAX_SIZE));
+        }
+    }
+}
