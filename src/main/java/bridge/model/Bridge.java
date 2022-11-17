22db49f73275e7dd8b41d59a1846d6d1ceefa5f6
dooboocookie <bkh3030@gmail.com>
2022-11-17 16:00:08 +0900

    feat: 다리길이를 검증하고 예외 발생시키는 내용 추가

--- src/main/java/bridge/model/Bridge.java
+++ src/main/java/bridge/model/Bridge.java
@@ -2,6 +2,8 @@ package bridge.model;
 
 import bridge.BridgeMaker;
 import bridge.BridgeRandomNumberGenerator;
+import bridge.util.Errors;
+import bridge.util.Rules;
 
 import java.util.List;
 
@@ -9,10 +11,22 @@ public class Bridge {
     private final List<String> bridge;
 
     public Bridge(int bridgeLength) {
+        validateLength(bridgeLength);
         BridgeRandomNumberGenerator bridgeRandomNumberGenerator = new BridgeRandomNumberGenerator();
         BridgeMaker bridgeMaker = new BridgeMaker(bridgeRandomNumberGenerator);
         List<String> makeBridge = bridgeMaker.makeBridge(bridgeLength);
         this.bridge = makeBridge;
+    }
+
+    private void validateLength(int bridgeLength) {
+        if(isOutOfBoundsLength(bridgeLength)) {
+            throw new IllegalArgumentException(Errors.ERROR_BRIDGE_LENGTH.getMessage());
+        }
+    }
+
+    private boolean isOutOfBoundsLength(int bridgeLength) {
+        return (bridgeLength < Rules.BRIDGE_MIN_LENGTH)
+                || (bridgeLength > Rules.BRIDGE_MAX_LENGTH);
     }
 
     public boolean possibleNextStep() {
--- src/main/java/bridge/util/Errors.java
+++ src/main/java/bridge/util/Errors.java
@@ -1,2 +1,15 @@
-package bridge.util;public enum Errors {
+package bridge.util;
+
+public enum Errors {
+    ERROR_BRIDGE_LENGTH("[ERROR] 다리 길이가 범위(3~20)을 벗어났습니다.");
+
+    private String message;
+
+    Errors(String message){
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return this.message;
+    }
 }
