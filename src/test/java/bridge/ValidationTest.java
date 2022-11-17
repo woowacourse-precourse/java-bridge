aba77dd940d91098d52b3a47430a6a56f2550aac
HubCreator <chocomilk8604@gmail.com>
2022-11-17 09:35:04 +0900

    refactor(ValidationTest) : @ParameterizedTest 사용

--- src/test/java/bridge/ValidationTest.java
+++ src/test/java/bridge/ValidationTest.java
@@ -2,69 +2,67 @@ package bridge;
 
 import bridge.enums.ErrorMessage;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.Assertions.*;
 
 public class ValidationTest {
 
     @DisplayName("다리의 길이는 3이상 20 이하이다.")
-    @Test
-    void bridgeLength() {
-        ValidationUtil.isValidBridgeLength(3);
-        ValidationUtil.isValidBridgeLength(20);
+    @Nested
+    class BridgeLengthTest {
+        @Test
+        void valid() {
+            ValidationUtil.isValidBridgeLength(3);
+            ValidationUtil.isValidBridgeLength(20);
+        }
 
-        assertThatThrownBy(() -> ValidationUtil.isValidBridgeLength(2))
-                .isInstanceOf(IllegalArgumentException.class)
-                .hasMessage(ErrorMessage.INVALID_LENGTH.getValue());
-        assertThatThrownBy(() -> ValidationUtil.isValidBridgeLength(21))
-                .isInstanceOf(IllegalArgumentException.class)
-                .hasMessage(ErrorMessage.INVALID_LENGTH.getValue());
+        @ValueSource(strings = {"2", "21"})
+        @ParameterizedTest
+        void inValid(Integer input) {
+            assertThatThrownBy(() -> ValidationUtil.isValidBridgeLength(input))
+                    .isInstanceOf(IllegalArgumentException.class)
+                    .hasMessage(ErrorMessage.INVALID_LENGTH.getValue());
+        }
     }
 
     @DisplayName("U 혹은 D인지 확인한다.")
-    @Test
-    void isUpOrDown() {
-        ValidationUtil.isUpOrDown("U");
-        ValidationUtil.isUpOrDown("D");
+    @Nested
+    class isUpOrDownTest {
+        @Test
+        void valid() {
+            ValidationUtil.isUpOrDown("U");
+            ValidationUtil.isUpOrDown("D");
+        }
 
-        assertThatThrownBy(() -> ValidationUtil.isUpOrDown("R"))
-                .isInstanceOf(IllegalArgumentException.class)
-                .hasMessage(ErrorMessage.INVALID_INPUT.getValue());
-        assertThatThrownBy(() -> ValidationUtil.isUpOrDown("Q"))
-                .isInstanceOf(IllegalArgumentException.class)
-                .hasMessage(ErrorMessage.INVALID_INPUT.getValue());
-        assertThatThrownBy(() -> ValidationUtil.isUpOrDown("A"))
-                .isInstanceOf(IllegalArgumentException.class)
-                .hasMessage(ErrorMessage.INVALID_INPUT.getValue());
-        assertThatThrownBy(() -> ValidationUtil.isUpOrDown("1"))
-                .isInstanceOf(IllegalArgumentException.class)
-                .hasMessage(ErrorMessage.INVALID_INPUT.getValue());
-        assertThatThrownBy(() -> ValidationUtil.isUpOrDown("*"))
-                .isInstanceOf(IllegalArgumentException.class)
-                .hasMessage(ErrorMessage.INVALID_INPUT.getValue());
+        @ValueSource(strings = {"R", "Q", "A", "1", "*", "!"})
+        @ParameterizedTest
+        void inValid(String input) {
+            assertThatThrownBy(() -> ValidationUtil.isUpOrDown(input))
+                    .isInstanceOf(IllegalArgumentException.class)
+                    .hasMessage(ErrorMessage.INVALID_INPUT.getValue());
+        }
     }
 
-    @DisplayName("R 혹은 Q인지 확인한다.")
-    @Test
-    void isRetryOrQuit() {
-        ValidationUtil.isRetryOrQuit("R");
-        ValidationUtil.isRetryOrQuit("Q");
 
-        assertThatThrownBy(() -> ValidationUtil.isRetryOrQuit("U"))
-                .isInstanceOf(IllegalArgumentException.class)
-                .hasMessage(ErrorMessage.INVALID_INPUT.getValue());
-        assertThatThrownBy(() -> ValidationUtil.isRetryOrQuit("D"))
-                .isInstanceOf(IllegalArgumentException.class)
-                .hasMessage(ErrorMessage.INVALID_INPUT.getValue());
-        assertThatThrownBy(() -> ValidationUtil.isRetryOrQuit("A"))
-                .isInstanceOf(IllegalArgumentException.class)
-                .hasMessage(ErrorMessage.INVALID_INPUT.getValue());
-        assertThatThrownBy(() -> ValidationUtil.isRetryOrQuit("1"))
-                .isInstanceOf(IllegalArgumentException.class)
-                .hasMessage(ErrorMessage.INVALID_INPUT.getValue());
-        assertThatThrownBy(() -> ValidationUtil.isRetryOrQuit("*"))
-                .isInstanceOf(IllegalArgumentException.class)
-                .hasMessage(ErrorMessage.INVALID_INPUT.getValue());
+    @DisplayName("R 혹은 Q인지 확인한다.")
+    @Nested
+    class isRetryOrQuitTest {
+        @Test
+        void valid() {
+            ValidationUtil.isRetryOrQuit("R");
+            ValidationUtil.isRetryOrQuit("Q");
+        }
+
+        @ValueSource(strings = {"U", "D", "A", "1", "*", "!"})
+        @ParameterizedTest
+        void inValid(String input) {
+            assertThatThrownBy(() -> ValidationUtil.isRetryOrQuit(input))
+                    .isInstanceOf(IllegalArgumentException.class)
+                    .hasMessage(ErrorMessage.INVALID_INPUT.getValue());
+        }
     }
 }
