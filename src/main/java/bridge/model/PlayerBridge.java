d3e73bfd082fd00f6876b5033dfef615de14163e
ksh-beginner <ohk9134@naver.com>
2022-11-17 17:49:51 +0900

    feat(PlayerBridge) : 이동할 칸 이동 기능 구현

--- src/main/java/bridge/model/PlayerBridge.java
+++ src/main/java/bridge/model/PlayerBridge.java
@@ -1,5 +1,47 @@
 package bridge.model;
 
+import bridge.Step;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class PlayerBridge {
 
+    private static final String UP_STEP = "U";
+    private static final String DOWN_STEP = "D";
+    private static final String CAN_MOVE_SHAPE = " O ";
+    private static final String CANNOT_MOVE_SHAPE = " X ";
+    private static final String BLANK_SHAPE = "  ";
+
+    private final List<String> upShape = new ArrayList<>();
+    private final List<String> downShape = new ArrayList<>();
+
+    public void moveNextStep(String inputNextStep, boolean canMove) {
+        if (inputNextStep.equals(UP_STEP)) {
+            moveUpStep(canMove);
+        }
+        if (inputNextStep.equals(DOWN_STEP)) {
+            moveDownStep(canMove);
+        }
+    }
+
+    private void moveUpStep(boolean canMove) {
+        if (canMove) {
+            upShape.add(CAN_MOVE_SHAPE);
+        }
+        if (!canMove) {
+            upShape.add(CANNOT_MOVE_SHAPE);
+        }
+        downShape.add(BLANK_SHAPE);
+    }
+
+    private void moveDownStep(boolean canMove) {
+        if (canMove) {
+            downShape.add(CAN_MOVE_SHAPE);
+        }
+        if (!canMove) {
+            downShape.add(CANNOT_MOVE_SHAPE);
+        }
+        upShape.add(BLANK_SHAPE);
+    }
 }
