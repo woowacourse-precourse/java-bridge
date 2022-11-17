6706a54f185fd06f4bfb17285a18eb40b4750615
seung-won-kim <hesjs5@skuniv.ac.kr>
2022-11-17 13:49:58 +0900

    test(ValidatorTest): 위 또는 아래를 입력했는지 확인하는 기능 테스트

--- src/test/java/bridge/util/ValidatorTest.java
+++ src/test/java/bridge/util/ValidatorTest.java
@@ -1,5 +1,6 @@
 package bridge.util;
 
+import static bridge.util.Validator.*;
 import static bridge.util.Validator.validateNumberInBridgeRange;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
@@ -20,5 +21,18 @@ class ValidatorTest {
     @ValueSource(ints = {3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20})
     void validateNumberInBridgeRangeByInRange(int number) {
         assertDoesNotThrow(() -> validateNumberInBridgeRange(number));
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"d", "u", "a", "ua", "da", "", " "})
+    void validateMovableInputByNotMovableInput(String input) {
+        assertThatThrownBy(() -> validateMovableInput(input))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"D", "U"})
+    void validateMovableInputByMovableInput(String input) {
+        assertDoesNotThrow(() -> validateMovableInput(input));
     }
 }
