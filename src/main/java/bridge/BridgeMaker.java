809d67c6e46799ae7bf799a70e774ad06fe8bfb1
gyumin159 <gyumin159@naver.com>
2022-11-17 16:02:35 +0900

    feat : 주어진 사이즈에 맞게 다리를 생성하는 메소드

--- src/main/java/bridge/BridgeMaker.java
+++ src/main/java/bridge/BridgeMaker.java
@@ -1,6 +1,8 @@
 package bridge;
 
+import java.util.ArrayList;
 import java.util.List;
+
 
 /**
  * 다리의 길이를 입력 받아서 다리를 생성해주는 역할을 한다.
@@ -18,6 +20,14 @@ public class BridgeMaker {
      * @return 입력받은 길이에 해당하는 다리 모양. 위 칸이면 "U", 아래 칸이면 "D"로 표현해야 한다.
      */
     public List<String> makeBridge(int size) {
-        return null;
+        List<String> bridge = new ArrayList<>(size);
+        for (int cnt = 0; cnt < size; cnt++) {
+            if (this.bridgeNumberGenerator.generate() == 1) {
+                bridge.add("U");
+                continue;
+            }
+            bridge.add("D");
+        }
+        return bridge;
     }
 }
