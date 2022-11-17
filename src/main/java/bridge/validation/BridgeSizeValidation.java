cbd34c1eac5c7883880736b1e80076026c88a0bd
ykmxxi <ykmxxi97@gmail.com>
2022-11-17 16:44:21 +0900

    feat: 다리 길이 입력 예외 사항 skeleton code 구현

---
+++ src/main/java/bridge/validation/BridgeSizeValidation.java
@@ -0,0 +1,15 @@
+package bridge.validation;
+
+public class BridgeSizeValidation {
+    public BridgeSizeValidation() {
+    }
+
+    public void isEmptyAndBlank() {
+    }
+
+    public void isNumber() {
+    }
+
+    public void isCorrectRange() {
+    }
+}
