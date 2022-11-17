1fc83a870a1870e959ab8be3c5c9588cdce779a0
tseop <77163842+tseop@users.noreply.github.com>
2022-11-17 15:27:32 +0900

    refactor: ErrorMessage 상수 접근제어자 수정

--- src/main/java/bridge/message/ErrorMessage.java
+++ src/main/java/bridge/message/ErrorMessage.java
@@ -1,8 +1,9 @@
 package bridge.message;
 
 public final class ErrorMessage {
-    public static final String ERROR = "[ERROR] ";
-    public static final String BRIDGE_SIZE_ERROR = ERROR + "다리의 크기는 3 ~ 20 사이의 크기로 생성해야 합니다.";
+    private static final String ERROR = "[ERROR] ";
+    public static final String INPUT_NO_NUMBER_ERROR = ERROR + "숫자를 입력해주세요.";
+    public static final String BRIDGE_SIZE_ERROR = ERROR + "다리 길이는 3 ~ 20 사이 값만 입력이 가능합니다.";
     public static final String INPUT_MOVE_ERROR = ERROR + "위로 가려면 U, 밑으로 가려면 D를 입력해주세요.";
     public static final String INPUT_RETRY_ERROR = ERROR + "재시도는 R, 게임 종료는 Q를 입력해주세요.";
 }
