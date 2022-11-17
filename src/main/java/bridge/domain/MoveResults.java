16d0229958cef425b966cdb45c58d3c25574dcd5
SeolYoungKim <nasur4da@gmail.com>
2022-11-17 20:23:12 +0900

    refactor(GameResults): MoveResults로 이름 변경
    GameResults는 게임의 최종 결과 느낌이 강해, 이동 관련 결과 객체라는 의미를 담기 위해 MoveResults로 변경하였습니다.

--- src/main/java/bridge/domain/MoveResults.java
+++ src/main/java/bridge/domain/MoveResults.java
@@ -6,7 +6,7 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
-public class GameResults {
+public class MoveResults {
 
     private static final String BLANK = " ";
 
