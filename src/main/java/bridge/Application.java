a7c5235211d2747a7c3fc2e5c745735838e02fba
kimdahee0815 <diana815@naver.com>
2022-11-17 23:48:28 +0900

    feat(main) : 다리건너기 게임을 main에서 실행하게 되므로, 필요한 각 클래스들을 생성하고 메소드 호출함

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -1,9 +1,26 @@
 package bridge;
 
-public class Application {
+import java.util.ArrayList;
+import java.util.List;
 
+public class Application {
+	static List<String> madeBridge = new ArrayList<>();
+	static int bridgeSize = 0;
+	static String inputMoving = "";
+	
+	InputView inputView = new InputView();
+    BridgeGame bridgeGame = new BridgeGame();
+    
     public static void main(String[] args) {
         System.out.println("다리 건너기 게임을 시작합니다.");
         
+        while(true) {
+        	bridgeSize = inputView.readBridgeSize();
+        	BridgeNumberGenerator bridgeNumberGenerator = new BridgeRandomNumberGenerator();
+        	BridgeMaker bridgeMaker = new BridgeMaker(bridgeNumberGenerator);
+        	madeBridge = bridgeMaker.makeBridge(bridgeSize);
+        	inputMoving = inputView.readMoving();
+        	
+        }
     }
 }
--- src/main/java/bridge/BridgeGame.java
+++ src/main/java/bridge/BridgeGame.java
@@ -13,12 +13,16 @@ public class BridgeGame {
 	 * <p>
 	 * 이동을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
 	 */
-	public void move(String inputMoving) {
-		
+	public void move(String inputMoving, List<String> madeBridge, int bridgeSize) {
+		for(int order = 0; order < bridgeSize; order++) {
+			if(compareMoving(inputMoving, madeBridge, order)) {
+				
+			}	
+		}
 	}
 	
-	public boolean compareMoving(String inputMoving, List<String> MadeBridge, int order) {
-		if(inputMoving.equals(MadeBridge.get(order))) {
+	public boolean compareMoving(String inputMoving, List<String> madeBridge, int order) {
+		if(inputMoving.equals(madeBridge.get(order))) {
 			return true;
 		}
 		return false;
