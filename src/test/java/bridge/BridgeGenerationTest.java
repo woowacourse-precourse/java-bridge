c610a7b1a8b71e9db4276b7300c629dafd33689c
making-a-scene <4936joo@naver.com>
2022-11-17 21:53:54 +0900

    refactor: 다리 생성 로직을 Uils에서 BridgeMakerImpl 클래스로 분리

--- src/test/java/bridge/BridgeGenerationTest.java
+++ src/test/java/bridge/BridgeGenerationTest.java
@@ -4,17 +4,16 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.*;
-import java.util.function.Consumer;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-public class ObjectGenerationTest {
-    private final Utils utils = new Utils();
+public class BridgeGenerationTest {
+    private final BridgeMakerImpl bridgeMakerImpl = new BridgeMakerImpl();
 
     @DisplayName("새로운 UsersBridgeCrossStatus 객체를 생성한다.")
     @Test
     void generateUsersBridgeCrossStatusTest() {
-        UsersBridgeCrossStatus initialBridge = utils.makeInitialBridge();
+        UsersBridgeCrossStatus initialBridge = bridgeMakerImpl.makeInitialBridge();
 
         List<String> topSpace = initialBridge.getCurrentBridge().get(1);
         assertThat(topSpace).isEqualTo(List.of("[", " ", " ", "]"));
@@ -25,7 +24,7 @@ public class ObjectGenerationTest {
     @DisplayName("길이가 10인 새로운 answerBridge를 생성한다.")
     @Test
     void generateAnswerBridgeTest() {
-        List<String> answerBridge = utils.makeAnswerBridge(10);
+        List<String> answerBridge = bridgeMakerImpl.makeAnswerBridge(10);
         assertThat(answerBridge.size()).isEqualTo(10);
         assertThat(answerBridge).satisfiesAnyOf(
                 value -> {assertThat(answerBridge).containsOnly("U", "D");},
