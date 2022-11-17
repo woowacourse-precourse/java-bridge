a98c5fd4a10828a9efadbb959fc610b11181cd0e
June <68291395+june-777@users.noreply.github.com>
2022-11-17 19:06:15 +0900

    refactor(BridgeMakerTest): 불필요한 Nested 클래스 계층화 제거

--- src/test/java/bridge/BridgeMakerTest.java
+++ src/test/java/bridge/BridgeMakerTest.java
@@ -9,72 +9,68 @@ import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.util.Lists.newArrayList;
 
-public class BridgeMakerTest {
+@Nested
+@DisplayName("다리 만들기 기능 테스트 클래스")
+class MakeBridgeTest {
 
 	@Nested
-	@DisplayName("다리 만들기 기능 테스트 클래스")
-	class MakeBridgeTest {
+	@DisplayName("성공 테스트 클래스")
+	class SuccessTest {
 
-		@Nested
-		@DisplayName("성공 테스트 클래스")
-		class SuccessTest {
+		@Test
+		@DisplayName("다리의 상태를 확인하는 테스트. (0은 아래칸(D), 1은 윗칸(U))")
+		void correctBridgeStateTest() {
 
-			@Test
-			@DisplayName("다리의 상태를 확인하는 테스트. (0은 아래칸(D), 1은 윗칸(U))")
-			void correctBridgeStateTest() {
+			// given
+			BridgeNumberGenerator bridgeFixNumberGenerator = new BridgeFixNumberGenerator(
+					newArrayList(1, 1, 0, 0, 1, 0, 1));
+			BridgeMaker bridgeMaker = new BridgeMaker(bridgeFixNumberGenerator);
 
-				// given
-				BridgeNumberGenerator bridgeFixNumberGenerator = new BridgeFixNumberGenerator(
-						newArrayList(1, 1, 0, 0, 1, 0, 1));
-				BridgeMaker bridgeMaker = new BridgeMaker(bridgeFixNumberGenerator);
+			// when
+			List<String> bridge = bridgeMaker.makeBridge(7);
 
-				// when
-				List<String> bridge = bridgeMaker.makeBridge(7);
+			// then
+			assertThat(bridge).containsExactly("U", "U", "D", "D", "U", "D", "U");
+		}
+	}
 
-				// then
-				assertThat(bridge).containsExactly("U", "U", "D", "D", "U", "D", "U");
-			}
+	@Nested
+	@DisplayName("예외상황 테스트 클래스")
+	class FailTest {
+
+		@Test
+		@DisplayName("다리의 상태를 확인하는 테스트. (0은 아래칸(D), 1은 윗칸(U))")
+		void wrongBridgeStateTest() {
+
+			// given
+			BridgeNumberGenerator bridgeFixNumberGenerator = new BridgeFixNumberGenerator(
+					newArrayList(1, 0, 1, 1, 1, 0, 0));
+			BridgeMaker bridgeMaker = new BridgeMaker(bridgeFixNumberGenerator);
+
+			// when
+			List<String> bridge = bridgeMaker.makeBridge(7);
+			String actuallyDButU = "U";
+			String wrongState = "A";
+
+			// then
+			assertThat(bridge).doesNotContainSequence("U", wrongState, "U", "U", "U", actuallyDButU, "D");
 		}
 
-		@Nested
-		@DisplayName("예외상황 테스트 클래스")
-		class FailTest {
+		@Test
+		@DisplayName("다리 생성 시 랜덤값 유효성 검사 기능을 확인하는 테스트")
+		void validateRandomNumberTest() {
 
-			@Test
-			@DisplayName("다리의 상태를 확인하는 테스트. (0은 아래칸(D), 1은 윗칸(U))")
-			void wrongBridgeStateTest() {
+			// given
+			BridgeNumberGenerator bridgeFixNumberGenerator = new BridgeFixNumberGenerator(
+					newArrayList(1, 1, 1, 1, -1, 2, 0));
+			BridgeMaker bridgeMaker = new BridgeMaker(bridgeFixNumberGenerator);
 
-				// given
-				BridgeNumberGenerator bridgeFixNumberGenerator = new BridgeFixNumberGenerator(
-						newArrayList(1, 0, 1, 1, 1, 0, 0));
-				BridgeMaker bridgeMaker = new BridgeMaker(bridgeFixNumberGenerator);
+			// when
+			String expectedExceptionMessage = BridgeState.DOWN.getBridgeStateErrorMessage();
 
-				// when
-				List<String> bridge = bridgeMaker.makeBridge(7);
-				String actuallyDButU = "U";
-				String wrongState = "A";
-
-				// then
-				assertThat(bridge).doesNotContainSequence("U", wrongState, "U", "U", "U", actuallyDButU, "D");
-			}
-
-			@Test
-			@DisplayName("다리 생성 시 랜덤값 유효성 검사 기능을 확인하는 테스트")
-			void validateRandomNumberTest() {
-
-				// given
-				BridgeNumberGenerator bridgeFixNumberGenerator = new BridgeFixNumberGenerator(
-						newArrayList(1, 1, 1, 1, -1, 2, 0));
-				BridgeMaker bridgeMaker = new BridgeMaker(bridgeFixNumberGenerator);
-
-				// when
-				String expectedExceptionMessage = BridgeState.DOWN.getBridgeStateErrorMessage();
-
-				// then
-				assertThatThrownBy(() -> bridgeMaker.makeBridge(7)).hasMessageContaining(expectedExceptionMessage);
-			}
+			// then
+			assertThatThrownBy(() -> bridgeMaker.makeBridge(7)).hasMessageContaining(expectedExceptionMessage);
 		}
-
 	}
 
 	static class BridgeFixNumberGenerator implements BridgeNumberGenerator {
