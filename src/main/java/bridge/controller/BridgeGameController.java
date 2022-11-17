f452e21073fdb24a81e60d5c011712286765564c
eora21 <eora21@naver.com>
2022-11-17 10:31:44 +0900

    feat: 🎸 변경 사이즈 대신 다리를 만들어 받을 수 있도록 수정
    사이즈 입력 -> 다리 생성이 같은 로직 안에서 구성되어야 정수 변환이 가능한지, 3 이상 20 이하인지 동시에 확인하고 아니라면
    값을 다시 요청할 수 있다.

--- src/main/java/bridge/controller/BridgeGameController.java
+++ src/main/java/bridge/controller/BridgeGameController.java
@@ -1,7 +1,11 @@
 package bridge.controller;
 
+import bridge.BridgeMaker;
+import bridge.BridgeNumberGenerator;
+import bridge.BridgeRandomNumberGenerator;
 import bridge.view.InputView;
 import bridge.view.OutputView;
+import java.util.List;
 
 public class BridgeGameController {
 
@@ -11,15 +15,20 @@ public class BridgeGameController {
 
     private final ExceptionHandle exceptionHandle = new ExceptionHandle(outputView);
 
+    private final BridgeNumberGenerator bridgeNumberGenerator = new BridgeRandomNumberGenerator();
+
+    private final BridgeMaker bridgeMaker = new BridgeMaker(bridgeNumberGenerator);
+
     public void startGame() {
         outputView.printGameStart();
-        int bridgeSize = getBridgeSize();
+        List<String> bridge = createBridge();
     }
 
-    private int getBridgeSize() {
+    private List<String> createBridge() {
         return exceptionHandle.getCorrectInput(() -> {
             outputView.printRequestBridgeLength();
-            return inputView.readBridgeSize();
+            int bridgeSize = inputView.readBridgeSize();
+            return bridgeMaker.makeBridge(bridgeSize);
         });
     }
 
