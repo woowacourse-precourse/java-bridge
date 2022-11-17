0a7aa594e154be410d3b52a2c0ce6b30d8bf5e5a
kevstevie <jianna442@gmail.com>
2022-11-17 16:35:40 +0900

    feat: 재시작 입력 유효성 검사 추가

---
+++ src/test/java/bridge/RestartValidatorTest.java
@@ -0,0 +1,20 @@
+package bridge;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+class RestartValidatorTest {
+
+    @DisplayName("잘못된 입력")
+    @Test
+    void wrongCase() {
+        Validator validator = new RestartValidator();
+        String str = "d";
+        String error = "Q 혹은 R";
+        assertThatThrownBy(() -> validator.validate(str))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(error);
+    }
+}
