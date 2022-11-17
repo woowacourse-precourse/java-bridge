bbed11f98f5dbdf04da0c4e0c74c991b6214c9f8
WhalesBob <ndw8200@naver.com>
2022-11-17 10:17:50 +0900

    feat(BridgeGame) : add codes about retry features to decide gameWinningClosed and make gameWinningClosed default true

--- src/main/java/bridge/businesslogic/BridgeGame.java
+++ src/main/java/bridge/businesslogic/BridgeGame.java
@@ -13,7 +13,7 @@ public class BridgeGame {
     private final InputView inputView = new InputView();
     private final OutputView outputView = new OutputView();
     private Bridge bridge;
-    private boolean isGameWin;
+    private boolean isGameWinningClosed = true;
     private int gameCount;
 
     private void initBridgeGame(){
@@ -66,8 +66,10 @@ public class BridgeGame {
      * 재시작을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
     public boolean retry() {
+        bridge.dead();
         gameCount++;
-        return askUserToRetry();
+        isGameWinningClosed = askUserToRetry();
+        return isGameWinningClosed;
     }
 
     public boolean askUserToRetry(){
