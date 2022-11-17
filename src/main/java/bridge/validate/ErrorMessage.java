ab1f9a6a151608e6ede48c52a970b518c5a5b092
splitube <voicesplit@daum.net>
2022-11-17 16:50:07 +0900

    feat(ErrorMessage): 다리길이 입력값 에러 메시지 작성

---
+++ src/main/java/bridge/validate/ErrorMessage.java
@@ -0,0 +1,19 @@
+package bridge.validate;
+
+public enum ErrorMessage {
+    ERROR("[ERROR] "),
+    INPUT_NOT_A_NUMBER("입력값이 숫자가 아닙니다."),
+    NUMBER_OUT_OF_RANGE("올바르지 않은 범위의 숫자입니다."),
+    PROPER_BRIDGE_LENGTH("( 다리 길이는 3이상 20이하 )");
+
+    public final String message;
+
+    ErrorMessage(String message) {
+        this.message = message;
+    }
+
+    @Override
+    public String toString() {
+        return message;
+    }
+}
