a3cec265e0533f71d8b4084eb992fde62bf19562
Be-student <presentsong@naver.com>
2022-11-18 01:00:52 +0900

    test(Position): from 메서드 테스트 추가

---
+++ src/test/java/bridge/domain/PositionTest.java
@@ -0,0 +1,34 @@
+package bridge.domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.DisplayNameGeneration;
+import org.junit.jupiter.api.DisplayNameGenerator;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+@DisplayName("Position 클래스")
+@DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
+public class PositionTest {
+    @Test
+    void from_메서드는_U_D_를_받으면_해당하는_enum_을_반환한다() {
+        assertThat(Position.from("U")).isEqualTo(Position.UP);
+        assertThat(Position.from("D")).isEqualTo(Position.DOWN);
+    }
+
+    @ParameterizedTest
+    @NullAndEmptySource
+    @ValueSource(strings = {"A", " U", "D ", "한글"})
+    void from_메서드는_U_D_를_제외한_입력을_받으면_IEA_예외가_발생한다(String input) {
+        try {
+            assertThrows(IllegalArgumentException.class, () -> Position.from(input));
+            Position.from(input);
+        } catch (IllegalArgumentException expected) {
+            assertThat(expected.getMessage()).isEqualTo("U,D 만 입력 가능합니다");
+        }
+    }
+}
