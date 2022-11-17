623e4e8ec2202fad345f2095b293dff03e2c26f9
cpot5620 <=cpot5620@naver.com>
2022-11-18 02:27:27 +0900

    feat(BridgeGame): create method to update path

--- src/main/java/bridge/BridgeGame.java
+++ src/main/java/bridge/BridgeGame.java
@@ -6,30 +6,41 @@ import java.util.List;
 public class BridgeGame {
     private final Bridge bridge;
     private List<String> pathOfUser;
+    private Path path;
 
     public BridgeGame(Bridge bridge) {
         this.bridge = bridge;
         pathOfUser = new ArrayList<>();
+        path = new Path();
     }
 
     public boolean move(String command) {
         boolean isMovable = isMovableBlock(command);
         pathOfUser.add(command);
-
+        updatePath(command, isMovable);
         return isMovable;
     }
 
     public void retry() {
         pathOfUser.clear();
+        path.clear();
     }
 
     public boolean isGameClear() {
         return bridge.equals(pathOfUser);
     }
 
+    public String getPath() {
+        return path.getPath();
+    }
+
     private boolean isMovableBlock(String command) {
         int indexOfNextBlock = pathOfUser.size();
 
         return bridge.isMovable(command, indexOfNextBlock);
+    }
+
+    private void updatePath(String command, boolean isMovable) {
+        path.updatePath(command, isMovable);
     }
 }
