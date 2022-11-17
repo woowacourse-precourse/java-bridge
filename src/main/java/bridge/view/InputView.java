6d8c56aa34fcf11daac3465f3404b117f3a70c1d
jjmin9797 <jjmin9797@naver.com>
2022-11-17 19:14:00 +0900

    feat(InputView): 게임 재시작 여부 입력받는 기능 구현

--- src/main/java/bridge/view/InputView.java
+++ src/main/java/bridge/view/InputView.java
@@ -9,6 +9,7 @@ public class InputView {
     private static final String GAME_START_MESSAGE = "다리 건너기 게임을 시작합니다.";
     private static final String READ_BRIDGE_SIZE_MESSAGE = "다리의 길이를 입력해주세요.";
     private static final String READ_MOVE_MESSAGE = "이동할 칸을 선택해주세요. (위: U, 아래: D)";
+    private static final String READ_GAME_COMMAND_MESSAGE = "게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)";
 
     /**
      *  게임 시작 메시지를 출력한다.
@@ -39,6 +40,8 @@ public class InputView {
      * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
      */
     public String readGameCommand() {
-        return null;
+        System.out.println(READ_GAME_COMMAND_MESSAGE);
+        String gameCommand = Console.readLine();
+        return gameCommand;
     }
 }
