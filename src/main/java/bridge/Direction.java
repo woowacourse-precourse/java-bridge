c784e32accd58d67028abd5a79843d49244e590f
shpark30 <seohyeonpark30.gmail.com>
2022-11-17 23:20:17 +0900

    docs: 기능 목록 작성

---
+++ src/main/java/bridge/Direction.java
@@ -0,0 +1,17 @@
+package bridge;
+
+public enum Direction {
+    UP("U", 1),
+    DOWN("D", 0);
+    private String direction;
+    private int binaryDirection;
+
+    Direction(String direction, int binaryDirection) {
+        this.direction = direction;
+        this.binaryDirection = binaryDirection;
+    }
+
+    public String toString() {
+        return direction;
+    }
+}
