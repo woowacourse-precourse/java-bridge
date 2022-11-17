68ad15c46a2214f195d9343f7193a30a03abd86c
JJ <jeongjae1996@naver.com>
2022-11-18 03:58:52 +0900

    feat: 다리 길이의 값에 대한 예외 처리가 제대로 되는지 확인하기 위한 단위 테스트 코드 작성

---
+++ src/test/java/bridge/ValidateTest.java
@@ -0,0 +1,17 @@
+package bridge;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+public class ValidateTest {
+    @DisplayName("다리의 길이가 올바른 값이 아니면 예외가 발생한다.")
+    @ValueSource(strings = {"999", "0", "-123", "21", "-1", "10j", " 1", "20 "})
+    @ParameterizedTest
+    void testBridgeSize(String input) {
+        assertThatThrownBy(() -> Validate.bridgeSize(input))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+}
