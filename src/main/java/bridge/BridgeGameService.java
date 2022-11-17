50ae099095e1b9d1d92378efca55f3e133c18c1b
Hyun-Seo Oh <ohseo8813@gmail.com>
2022-11-17 16:25:41 +0900

    <feat>(BridgeGame.java): 게임 재시작 기능

--- src/main/java/bridge/BridgeGameService.java
+++ src/main/java/bridge/BridgeGameService.java
@@ -1,10 +1,13 @@
 package bridge;
 
+import static bridge.constant.BridgeConstant.RESTART_GAME;
+
 import java.util.List;
 
 public class BridgeGameService {
 
     private int round = 0;
+    private int tryCount = 1;
 
     public List<String> createBridge(int size) {
         return new BridgeMaker(new BridgeRandomNumberGenerator()).makeBridge(size);
@@ -12,5 +15,12 @@ public class BridgeGameService {
 
     public boolean isCorrectMove(List<String> bridge, String moveMessage) {
         return bridge.get(round++).equals(moveMessage);
+    }
+    public boolean isRetry(String retryMessage) {
+        if (retryMessage.equals(RESTART_GAME)) {
+            tryCount++;
+            return true;
+        }
+        return false;
     }
 }
