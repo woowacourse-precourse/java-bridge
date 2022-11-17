036065da053909d8fe6d56bc0c15e0f14dc0f779
Voyager003 <85725033+Voyager003@users.noreply.github.com>
2022-11-18 00:36:00 +0900

    Feat: 추가 - validateMoving 이동할 칸 입력 시 U, D이외의 문자가 입력되면 IllegalArgumentException 발생

--- src/main/java/bridge/Exception.java
+++ src/main/java/bridge/Exception.java
@@ -1,10 +1,19 @@
 package bridge;
 
+import bridge.constant.Constant;
+
 public class Exception {
 
     public static void lengthrangeException(int input) {
         if (input > 20 || input < 3) {
             throw new IllegalArgumentException("[ERROR] 다리 길이는 3부터 20 사이의 숫자여야 합니다.");
         }
+    }
+
+    public static String validateMoving(String input) {
+        if (input.matches("[^UD]")) {
+            throw new IllegalArgumentException("[ERROR] 잘못된 입력입니다. U와 D중 하나를 입력해주세요.");
+        }
+        return input;
     }
 }
