a99c298d22337eab5fffefa85a55dd60832f6105
EUNJEONGMUN <munej26@gmail.com>
2022-11-17 16:50:17 +0900

    feat: 위와 아래를 나타내는 상수 추가

--- src/main/java/bridge/constant/Constant.java
+++ src/main/java/bridge/constant/Constant.java
@@ -4,4 +4,8 @@ public class Constant {
     public static final int BRIDGE_SIZE_MIN = 3;
     public static final int BRIDGE_SIZE_MAX = 20;
     public static final int INITIAL_NUMBER = 0;
+
+    public static final String UP = "U";
+    public static final String DOWN = "D";
+
 }
