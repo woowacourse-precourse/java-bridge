ac8cf32ba346aa76c1f37402defc022c3f9b949f
popomarin28@gmail.com <popomarin28@gmail.com>
2022-11-17 15:45:22 +0900

    feat(InputView) : gameCommandButton 구현

--- src/main/java/bridge/InputView.java
+++ src/main/java/bridge/InputView.java
@@ -24,7 +24,7 @@ public class InputView {
      * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
      */
     public String readGameCommand(String gameCommandButton) {
-
-        return null;
+        validateUserInput.checkGameCommand(gameCommandButton);
+        return gameCommandButton;
     }
 }
