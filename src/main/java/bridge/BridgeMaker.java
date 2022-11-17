1dde3e7a22fc342497c964a128072bf4e5f7f0a0
shpark30 <seohyeonpark30.gmail.com>
2022-11-17 23:42:57 +0900

    fix(BridgeMaker): 에러 메세지 수정

--- src/main/java/bridge/BridgeMaker.java
+++ src/main/java/bridge/BridgeMaker.java
@@ -31,7 +31,7 @@ public class BridgeMaker {
 
     private void validateLengthOfBridge(int size) {
         if (size < 3 || size >20) {
-            throw new IllegalArgumentException("[ERROR] 다리 길이는 3이상, 20이하여야 합니다.");
+            throw new IllegalArgumentException("[ERROR] 다리 길이는 3부터 20 사이의 숫자여야 합니다.");
         }
     }
 }
