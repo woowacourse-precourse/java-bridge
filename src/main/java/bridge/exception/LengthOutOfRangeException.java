fc128cedd79de400aaf83bd6cc5be32822838c99
JeongHunHui <deca3248@gmail.com>
2022-11-17 14:47:56 +0900

    feat: 범위를 벗어난 다리 길이를 입력 시 발생할 예외클래스 생성

---
+++ src/main/java/bridge/exception/LengthOutOfRangeException.java
@@ -0,0 +1,10 @@
+package bridge.exception;
+
+public class LengthOutOfRangeException extends IllegalArgumentException {
+  private static final String LENGTH_OUT_OF_RANGE_EXCEPTION_MESSAGE =
+      "[ERROR] 다리 길이는 3부터 20 사이의 숫자여야 합니다.";
+
+  public LengthOutOfRangeException() {
+    super(LENGTH_OUT_OF_RANGE_EXCEPTION_MESSAGE);
+  }
+}
