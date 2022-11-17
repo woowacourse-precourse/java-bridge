80aea3e322ca3efa3aebc7c5e55144cf3dbab02c
정현수 <96504592+hyunsb@users.noreply.github.com>
2022-11-17 20:14:28 +0900

    feat: 사용자가 생성할 다리의 길이를 입력받고 반환하는 기능 추가

--- src/main/java/bridge/InputView.java
+++ src/main/java/bridge/InputView.java
@@ -1,15 +1,14 @@
 package bridge;
+import camp.nextstep.edu.missionutils.Console;
 
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
  */
 public class InputView {
 
-    /**
-     * 다리의 길이를 입력받는다.
-     */
+    // TODO: 사용자가 생성할 다리의 길이를 입력받는다.
     public int readBridgeSize() {
-        return 0;
+        return Validate.bridgeSize(Console.readLine());
     }
 
     /**
