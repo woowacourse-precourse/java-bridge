2cd3eded68673b6e81a86f677df9a1762f35310a
JHS <wnghdtjr129@naver.com>
2022-11-17 18:46:38 +0900

    feat(InputView, OutputView): 게임 과정 및 종료 입출력 구현

--- src/main/java/bridge/view/OutputView.java
+++ src/main/java/bridge/view/OutputView.java
@@ -1,18 +1,12 @@
 package bridge.view;
 
-import bridge.model.ResultManager;
-
 public class OutputView {
 
-    /**
-     * 현재까지 이동한 다리의 상태를 정해진 형식에 맞춰 출력한다.
-     * <p>
-     * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
-     */
-    public void printMap() {
+    public void printMap(String bridgeState) {
+        System.out.println(bridgeState);
     }
 
-    public void printResult(ResultManager result) {
+    public void printResult(String result) {
         System.out.println(result);
     }
 
@@ -23,5 +17,9 @@ public class OutputView {
 
     public void askMoveSpace() {
         System.out.println("이동할 칸을 선택해주세요. (위: U, 아래: D)");
+    }
+
+    public void askRetry() {
+        System.out.println("게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)");
     }
 }
