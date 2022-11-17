923fa3e10a04092a784a6ace356d394490b4857f
cocomozila <joseyun94@naver.com>
2022-11-17 22:59:33 +0900

    refactor: 파일 및 패키지 정리

---
+++ src/main/java/bridge/controller/Controller.java
@@ -0,0 +1,16 @@
+package bridge.controller;
+
+import bridge.Service.Service;
+
+public class Controller {
+
+    private Service service;
+
+    public Controller() {
+        this.service = new Service();
+    }
+
+    public void startBridgeGame() {
+
+    }
+}
