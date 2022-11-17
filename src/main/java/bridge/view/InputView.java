96d686e508f5694d47674e4d88bc34bc9f2149e3
박승민 <tmddudals369@naver.com>
2022-11-18 03:14:49 +0900

    feat: 입력 기능 구현

--- src/main/java/bridge/view/InputView.java
+++ src/main/java/bridge/view/InputView.java
@@ -1,5 +1,10 @@
 package bridge.view;
 
+import static bridge.utils.constants.BridgeConstants.*;
+
+import bridge.utils.constants.GameConstants;
+import camp.nextstep.edu.missionutils.Console;
+
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
  */
@@ -8,21 +13,27 @@ public class InputView {
     /**
      * 다리의 길이를 입력받는다.
      */
-    public int readBridgeSize() {
-        return 0;
+    public static int readBridgeSize() {
+        System.out.println(INPUT_BRIDGE_LENGTH);
+        String bridgeSize = Console.readLine();
+        return Integer.parseInt(bridgeSize);
     }
 
     /**
      * 사용자가 이동할 칸을 입력받는다.
      */
-    public String readMoving() {
-        return null;
+    public static String readMoving() {
+        System.out.println(INPUT_BRIDGE_MOVE_OPTION);
+        String moving = Console.readLine();
+        return moving;
     }
 
     /**
      * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
      */
-    public String readGameCommand() {
-        return null;
+    public static String readGameCommand() {
+        System.out.println(GameConstants.RESTART_GAME_OPTION);
+        String gameCommand = Console.readLine();
+        return gameCommand;
     }
 }
