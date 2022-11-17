43439504e6bb84fc94b11e6c1d2a57521dbccca8
kokodak <kokodakadokok@gmail.com>
2022-11-17 17:57:12 +0900

    feat(bridge): 사용자가 이동할 칸에 다리가 있는지 판단하는 기능 구현

--- src/main/java/bridge/domain/Bridge.java
+++ src/main/java/bridge/domain/Bridge.java
@@ -9,4 +9,9 @@ public class Bridge {
     public Bridge(List<String> bridge) {
         this.bridge = bridge;
     }
+
+    public boolean canMove(List<String> userMoved) {
+        int userMovedSize = userMoved.size();
+        return bridge.get(userMovedSize - 1) == userMoved.get(userMovedSize - 1);
+    }
 }
