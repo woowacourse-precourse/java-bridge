d92153daaf49feabacdd73914341a7547fa71f4f
K-Diger <kitten.diger@gmail.com>
2022-11-18 02:37:34 +0900

    refactor(output) : output 패키지로 분할했던 OutputView 클래스를 bridge 클래스로 이동하였다.

---
+++ src/main/java/bridge/OutputView.java
@@ -0,0 +1,20 @@
+package bridge;
+
+public class OutputView {
+
+    public void printGameStartContext() {
+        System.out.println("다리 건너기 게임을 시작합니다.");
+    }
+
+    public void printInputBridgeSize() {
+        System.out.println("\n다리의 길이를 입력해주세요");
+    }
+
+    public void printInputMoving() {
+        System.out.println("\n이동할 칸을 선택해주세요. (위: U, 아래: D)");
+    }
+
+    public void printInputGameCommand() {
+        System.out.println("\n게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)");
+    }
+}
