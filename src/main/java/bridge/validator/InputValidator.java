e310e855257293828db1959c3f20545cd67904df
java-saeng <gusdn85554@naver.com>
2022-11-17 17:11:10 +0900

    fix : 에러 메시지에 [ERROR] 추가

--- src/main/java/bridge/validator/InputValidator.java
+++ src/main/java/bridge/validator/InputValidator.java
@@ -5,7 +5,7 @@ public class InputValidator {
     public void isDigit(String input) {
         for (int i = 0; i < input.length(); i++) {
             if (!Character.isDigit(input.charAt(i))) {
-                throw new IllegalArgumentException("숫자만 입력 가능합니다.");
+                throw new IllegalArgumentException("[ERROR] 숫자만 입력 가능합니다.");
             }
         }
     }
@@ -14,7 +14,7 @@ public class InputValidator {
         int length = Integer.parseInt(input);
 
         if (length < 3 || length > 20) {
-            throw new IllegalArgumentException("3이상 20 이하의 숫자만 가능합니다.");
+            throw new IllegalArgumentException("[ERROR] 3이상 20 이하의 숫자만 가능합니다.");
         }
     }
 }
