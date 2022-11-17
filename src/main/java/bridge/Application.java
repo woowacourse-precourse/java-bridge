98b725d24c189303b2ccabc73d336c4b70104f4b
KimTaeKang57 <zizoen123@gmail.com>
2022-11-17 14:57:02 +0900

    feat: main 함수 try-catch 적용

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -1,25 +1,14 @@
 package bridge;
 
-import java.util.List;
-
 public class Application {
 
     public static void main(String[] args) {
         // TODO: 프로그램 구현
-        // 다리 건너기 게임 시작
-        System.out.println("다리 건너기 게임을 시작합니다.");
-
-        // 다리의 길이를 입력
-        System.out.println("다리의 길이를 입력해주세요.");
-        InputView inputView = new InputView();
-        int size = inputView.readBridgeSize();
-
-        // 다리를 만듦
-        BridgeRandomNumberGenerator bridgeRandomNumberGenerator = new BridgeRandomNumberGenerator();
-        BridgeMaker bridgeMaker = new BridgeMaker(bridgeRandomNumberGenerator);
-        List<String> bridge = bridgeMaker.makeBridge(size);
-        System.out.println(bridge.toString());
-
-
+        try {
+            BridgeApplication bridgeApplication = new BridgeApplication();
+            bridgeApplication.run();
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+        }
     }
 }
