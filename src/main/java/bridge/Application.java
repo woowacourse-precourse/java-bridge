e37cb519eff24bbc0ad205c5cc08b81f69f881ac
Voyager003 <85725033+Voyager003@users.noreply.github.com>
2022-11-17 21:50:07 +0900

    Feat: 추가 Application 게임 시작 시, 시작 메시지 출력

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -1,10 +1,12 @@
 package bridge;
 
+import bridge.constant.Message;
 import bridge.controller.Game;
 
 public class Application {
 
     public static void main(String[] args) {
+        System.out.println(Message.GAME_START.getMessage());
         Game game = new Game();
         game.play();
     }
