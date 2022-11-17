ebb87fd0cdc0b2fc2d97bd3adbc8cd8cd24228a1
Heesom <heesom.hs@gmail.com>
2022-11-17 21:06:36 +0900

    feat(InputView) : 게임 재시작/종료를 입력받는 기능 추가

--- src/main/java/bridge/UI/Input/InputView.java
+++ src/main/java/bridge/UI/Input/InputView.java
@@ -3,12 +3,10 @@ package bridge.UI.Input;
 
 import camp.nextstep.edu.missionutils.Console;
 
-/**
- * 사용자로부터 입력을 받는 역할을 한다.
- */
+
 public class InputView {
 
-    public int readBridgeSize() {
+    public int readBridgeSize() throws IllegalArgumentException {
         int bridgeSize;
         try {
             bridgeSize = Integer.parseInt(Console.readLine());
@@ -20,27 +18,21 @@ public class InputView {
         }
         return bridgeSize;
     }
-    /**
-     * 사용자가 이동할 칸을 입력받는다.
-     *
-     * 메서드의 인자와 이름 그리고 반환 타입을 변경할 수 있다.
-     */
+
     public String readMoving() {
         String userInput = Console.readLine();
         if(!(userInput.equals("U") || userInput.equals("D"))) {
-            throw new IllegalArgumentException("[ERROR] U 혹은 D를 입력해주세요");
+            throw new IllegalArgumentException("[ERROR] U(위칸) 혹은 D(아래칸)을 입력해주세요");
         }
         return userInput;
     }
 
-    /**
-     * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
-     *
-     * 메서드의 인자와 이름 그리고 반환 타입을 변경할 수 있다.
-     */
     public String readGameCommand() {
-        //TODO : R,Q가 아니면 예외
-        return null;
+        String userInput = Console.readLine();
+        if(!(userInput.equals("R") || userInput.equals("Q"))) {
+            throw new IllegalArgumentException("[ERROR] R(재시작) 혹은 Q(종료)를 입력해주세요");
+        }
+        return userInput;
     }
 }
 
