c5d445d3f445177316c14639fe702e8d5c171e37
소대훈 <theogns0681@naver.com>
2022-11-17 09:56:06 +0900

    [refactor] 문자열 변수로 분리

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -6,10 +6,10 @@ public class Application {
     static InputView inputView = new InputView();
     static BridgeMaker bridgeMaker = new BridgeMaker(new BridgeRandomNumberGenerator());
     static BridgeGame bridgeGame = new BridgeGame();
-
+    static final String startGame = "다리 건너기 게임을 시작합니다.\n";
     public static void main(String[] args) {
         // TODO: 프로그램 구현
-        System.out.println("다리 건너기 게임을 시작합니다.\n");
+        System.out.println(startGame);
         int bridgeSize = inputView.readBridgeSize();
         List<String> bridgeList = bridgeMaker.makeBridge(bridgeSize);
         System.out.println();
