709b416d9a8f89cd45a2782ac076728be298178c
장기환 <jang-gihwan@jang-gihwan-ui-MacBookPro.local>
2022-11-17 20:33:10 +0900

    feat : (InputView) 재시도 여부 입력 기능

--- src/main/java/bridge/Exception.java
+++ src/main/java/bridge/Exception.java
@@ -7,6 +7,7 @@ public interface Exception {
     public final String INPUT_IS_NOT_NUM_EXCEPTION_MESSAGE = "[ERROR] 다리 길이는 숫자여야 합니다.";
     public final String BRIDGE_SIZE_OUT_OF_RANGE_EXCEPTION_MESSAGE = "[ERROR] 다리 길이는 3부터 20 사이의 숫자여야 합니다.";
     public final String MOVING_STRING_EXCEPTION_MESSAGE = "[ERROR] 이동 시 위 칸은 대문자 U, 아래 칸은 대문자 D여야 합니다.";
+    public final String GAME_COMMAND_STRING_EXCEPTION_MESSAGE = "[ERROR] 재시작은 대문자 R, 종료는 대문자 Q여야 합니다.";
     
     public static void checkInputIsNum(String input){
         for(int index = 0; index < input.length(); ++index)
@@ -22,5 +23,10 @@ public interface Exception {
     public static void checkMovingString(String moving){
         if(!moving.equals("U") && !moving.equals("D"))
             throw new IllegalArgumentException(MOVING_STRING_EXCEPTION_MESSAGE);
+    }
+
+    public static void checkGameCommandString(String gameCommand){
+        if(!gameCommand.equals("R") && !gameCommand.equals("Q"))
+            throw new IllegalArgumentException(GAME_COMMAND_STRING_EXCEPTION_MESSAGE);
     }
 }
--- src/main/java/bridge/InputView.java
+++ src/main/java/bridge/InputView.java
@@ -35,13 +35,20 @@ public class InputView {
             System.out.println(e.getMessage());
             return readMoving();
         }
-        return null;
+        return moving;
     }
 
     /**
      * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
      */
     public String readGameCommand() {
-        return null;
+        String gameCommand = input();
+        try{
+            Exception.checkGameCommandString(gameCommand);
+        }catch(IllegalArgumentException e){
+            System.out.println(e.getMessage());
+            return readGameCommand();
+        }
+        return gameCommand;
     }
 }
