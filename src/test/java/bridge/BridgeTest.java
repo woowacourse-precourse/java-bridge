d280cc7cfc5079af8f90d5dfa92e2cb369a2f951
TaeHeyeon_Kim <thk98k@gmail.com>
2022-11-17 23:12:13 +0900

    test: BridgeMaker 테스트케이스 작성
    입력받은 size에 맞게 다리를 생성합니다.
    random 값으로 다리를 생성하기 때문에 리스트 안의 요소들을 검증하는 테스트케이스는 작성하지 못했습니다

--- src/test/java/bridge/BridgeTest.java
+++ src/test/java/bridge/BridgeTest.java
@@ -1,2 +1,18 @@
 package bridge;
 
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+
+public class BridgeTest {
+    BridgeNumberGenerator bridgeNumberGenerator = new BridgeRandomNumberGenerator();
+    BridgeMaker bridgeMaker = new BridgeMaker(bridgeNumberGenerator);
+    @DisplayName("size에 맞게 List를 생성한다.")
+    @Test
+    public void bridgeMakerTest1() {
+        int num = 5;
+
+        Assertions.assertThat(bridgeMaker.makeBridge(num).size()).isEqualTo(num);
+    }
+}
