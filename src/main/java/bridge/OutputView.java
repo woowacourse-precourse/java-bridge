99f5c3ed27b7ce1af62802798bfd7051f28017fa
cpot5620 <=cpot5620@naver.com>
2022-11-18 02:15:57 +0900

    feat(OutputView): create methods for printing

--- src/main/java/bridge/OutputView.java
+++ src/main/java/bridge/OutputView.java
@@ -1,23 +1,40 @@
 package bridge;
 
-/**
- * 사용자에게 게임 진행 상황과 결과를 출력하는 역할을 한다.
- */
 public class OutputView {
 
-    /**
-     * 현재까지 이동한 다리의 상태를 정해진 형식에 맞춰 출력한다.
-     * <p>
-     * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
-     */
-    public void printMap() {
+    public static void askBridgeLength() {
+        printGameStart();
+        printMessage("다리의 길이를 입력해주세요.");
     }
 
-    /**
-     * 게임의 최종 결과를 정해진 형식에 맞춰 출력한다.
-     * <p>
-     * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
-     */
-    public void printResult() {
+    public static void askMovingCommand() {
+        printMessage("이동할 칸을 선택해주세요. (위: U, 아래: D)");
+    }
+
+    public static void askRestartCommand() {
+        printMessage("게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)");
+    }
+
+    public static void printMap(String message) {
+        printMessage(message);
+    }
+
+    public static void printResult(String path, int countOfAttempt, boolean isGameClear) {
+        String result = "성공";
+        if (!isGameClear)
+            result = "실패";
+
+        printMessage("최종 게임 결과");
+        printMap(path);
+        printMessage(String.format("게임 성공 여부: %s", result));
+        printMessage(String.format("총 시도한 횟수: %d", countOfAttempt));
+    }
+
+    private static void printGameStart() {
+        printMessage("다리 건너기 게임을 시작합니다.");
+    }
+
+    private static void printMessage(String message) {
+        System.out.println(message);
     }
 }
