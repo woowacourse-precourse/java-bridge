7544c353989540d37221d1fc0253397529bdd6f7
Gaby <gaby@Gabyui-MacBookPro.local>
2022-11-17 14:37:17 +0900

    fix: add required message before user input

--- src/main/java/bridge/InputView.java
+++ src/main/java/bridge/InputView.java
@@ -15,6 +15,7 @@ public class InputView {
      * 다리의 길이를 입력받는다.
      */
     public int readBridgeSize() throws IllegalArgumentException {
+        System.out.println("다리의 길이를 입력해주세요.");
         String input = Console.readLine();
         if (!input.matches("\\d+") ||
                 Integer.parseInt(input) < 3 ||
@@ -27,6 +28,7 @@ public class InputView {
      * 사용자가 이동할 칸을 입력받는다.
      */
     private String readMoving() throws IllegalArgumentException {
+        System.out.println("이동할 칸을 선택해주세요. (위: U, 아래: D)");
         String input = Console.readLine();
         if (!input.equals("U") && !input.equals("D"))
             Err.WRONG_MOVE.invalid();
@@ -37,6 +39,7 @@ public class InputView {
      * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
      */
     private String readGameCommand() throws IllegalArgumentException {
+        System.out.println("게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)");
         String input = Console.readLine();
         if (!input.equals("R") && !input.equals("Q"))
             Err.WRONG_RETRY_OR_TERMINATE.invalid();
