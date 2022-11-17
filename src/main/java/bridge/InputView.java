69eeb44cac3ef2d51bfbcfe03ad41f98a89dace1
KYR <freekyr7529@gmail.com>
2022-11-18 06:42:43 +0900

    feature : InputView 클래스에 입력을 담당하는 readValue 메소드와 입력을 정수로 변환하는 convertToInteger 메소드를 생성

--- src/main/java/bridge/InputView.java
+++ src/main/java/bridge/InputView.java
@@ -1,10 +1,22 @@
 package bridge;
 
+import camp.nextstep.edu.missionutils.Console;
+
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
  */
 public class InputView {
 
+    public String readValue() {
+        return Console.readLine();
+    }
+    private Integer convertToInteger(String value) {
+        try {
+            return Integer.parseInt(value);
+        } catch (NumberFormatException exception) {
+            throw new IllegalArgumentException("[ERROR] 입력하신 값은 정수가 아닙니다.");
+        }
+    }
     /**
      * 다리의 길이를 입력받는다.
      */
