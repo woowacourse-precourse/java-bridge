4275a74b5daa52289c563dcf70bc36fc0b249291
hoonn9 <hun9935@gmail.com>
2022-11-17 18:57:44 +0900

    feat(BridgeMaker): 다리 생성 및 최소 길이 검증 기능 추가

--- src/main/java/bridge/BridgeMaker.java
+++ src/main/java/bridge/BridgeMaker.java
@@ -1,23 +1,37 @@
 package bridge;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
  * 다리의 길이를 입력 받아서 다리를 생성해주는 역할을 한다.
  */
 public class BridgeMaker {
-
+    public static final Integer MIN_SIZE = 1;
     private final BridgeNumberGenerator bridgeNumberGenerator;
-
+    
     public BridgeMaker(BridgeNumberGenerator bridgeNumberGenerator) {
         this.bridgeNumberGenerator = bridgeNumberGenerator;
     }
-
+    
     /**
      * @param size 다리의 길이
      * @return 입력받은 길이에 해당하는 다리 모양. 위 칸이면 "U", 아래 칸이면 "D"로 표현해야 한다.
      */
     public List<String> makeBridge(int size) {
-        return null;
+        validateSize(size);
+        List<String> result = new ArrayList<>(size);
+        
+        for (int i = 0; i < size; i++) {
+            result.add(BridgeDirection.generateByNumber(bridgeNumberGenerator.generate()).getSelect());
+        }
+        
+        return result;
+    }
+    
+    private void validateSize(int size) {
+        if (size < MIN_SIZE) {
+            throw new IllegalArgumentException("다리 길이는 최소 1이상으로 입력해주세요.");
+        }
     }
 }
