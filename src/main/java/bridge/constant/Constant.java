bfa5a1ac219a9412bee99996439b62ecb5daa5a3
EUNJEONGMUN <munej26@gmail.com>
2022-11-18 01:55:10 +0900

    feat: 재시작과 종료 상수 추가

--- src/main/java/bridge/constant/Constant.java
+++ src/main/java/bridge/constant/Constant.java
@@ -7,5 +7,7 @@ public class Constant {
 
     public static final String UP = "U";
     public static final String DOWN = "D";
+    public static final String RETRY = "R";
+    public static final String QUIT = "Q";
 
 }
