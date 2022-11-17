9fbe8ea2d57e2f8cdc964d2679726b8aa45c447e
kki4504 <rhkddlf9876@gmail.com>
2022-11-18 08:47:27 +0900

    feat(InputView) : 입력값 유효성 검사 기능 추가

--- src/main/java/bridge/view/InputView.java
+++ src/main/java/bridge/view/InputView.java
@@ -11,12 +11,15 @@ package bridge.view;
  * 사용자 값 입력을 위해 필요한 메서드를 추가할 수 있다.
  */
 public class InputView {
+    private static final String ERROR_INPUT_LENGTH_ZERO = "[ERROR] 아무것도 입력하지 않았습니다.";
+    private static final String ERROR_NOT_INTEGER = "[ERROR] 숫자를 입력해야 합니다.";
+    private static final int EMPTY_LENGTH_VALUE = 0;
 
     /**
      * 다리의 길이를 입력받는다.
      */
-    public int readBridgeSize() {
-        return 0;
+    public int readBridgeSize(String readLine) {
+        return validateStringToInt(validateNotEmpty(readLine));
     }
 
     /**
@@ -31,5 +34,20 @@ public class InputView {
      */
     public String readGameCommand() {
         return null;
+    }
+
+    private String validateNotEmpty(String readLine) {
+        if (readLine.length() > EMPTY_LENGTH_VALUE) {
+            return readLine;
+        }
+        throw new IllegalArgumentException(ERROR_INPUT_LENGTH_ZERO);
+    }
+
+    private int validateStringToInt(String readLine) {
+        try {
+            return Integer.parseInt(validateNotEmpty(readLine));
+        } catch (NumberFormatException numberFormatException) {
+            throw new IllegalArgumentException(ERROR_NOT_INTEGER);
+        }
     }
 }
