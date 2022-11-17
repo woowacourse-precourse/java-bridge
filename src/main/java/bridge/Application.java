abf3001f9df12418f24b9fb9c06fc1b6144faaa9
WhoIsWoony <WhoIsWoony@gmail.com>
2022-11-17 22:08:13 +0900

    feat: add main work

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -1,8 +1,17 @@
 package bridge;
 
+
 public class Application {
 
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        BridgeGame bridgeGame = new BridgeGame();
+        while(bridgeGame.clear()) //게임클리어여부
+        {
+            bridgeGame.move(); //이동
+            if(!bridgeGame.retry()) //재시작여부
+                break;
+        }
+        bridgeGame.result(); //결과출력
     }
 }
