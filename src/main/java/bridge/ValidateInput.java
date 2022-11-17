728c1163b8f889ac72e556462382062e695ca032
gyumin159 <gyumin159@naver.com>
2022-11-17 11:27:14 +0900

    feat : 플레이어의 이동 입력이 올바른지(U 또는 D) 확인하는 메소드

--- src/main/java/bridge/ValidateInput.java
+++ src/main/java/bridge/ValidateInput.java
@@ -4,7 +4,8 @@ import java.util.regex.Pattern;
 
 public class ValidateInput {
     private enum regexPatterns{
-        ONLY_DIGIT("^[0-9]*$");
+        ONLY_DIGIT("^[0-9]+$"),
+        U_OR_D("^[UD]$");
 
         final String regex;
 
@@ -20,9 +21,16 @@ public class ValidateInput {
         }
     }
 
-    public static void isInRange(String rawInput) {
+    public static void isInRange(String rawInput) { // "0004" 이런 입력은 올바른 입력으로 통과됨.
         int target = Integer.parseInt(rawInput);
         if (target < 3 || target > 20) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    public static void isUorD(String rawInput){
+        boolean isUorD = Pattern.matches(regexPatterns.U_OR_D.regex, rawInput);
+        if (!isUorD){
             throw new IllegalArgumentException();
         }
     }
