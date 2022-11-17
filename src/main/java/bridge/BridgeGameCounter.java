230fe90f4a65860e2cd0ed0504e62c37f3b8b73a
woosung1223 <dntjd991223@naver.com>
2022-11-17 17:04:14 +0900

    feat(BridgeGameCounter): 클래스 및 생성자 생성

---
+++ src/main/java/bridge/BridgeGameCounter.java
@@ -0,0 +1,10 @@
+package bridge;
+
+public class BridgeGameCounter {
+    private final int countLimit;
+    private int count = 0;
+
+    public BridgeGameCounter(int limit) {
+        countLimit = limit;
+    }
+}
