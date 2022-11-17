62cbbdc188114a6355023a84de3490e4ca41dd92
JaeUpSu <hyeongs2323@gmail.com>
2022-11-17 16:30:14 +0900

    GameMessage | Exception enum 만들기

--- src/main/java/bridge/GameMessage.java
+++ src/main/java/bridge/GameMessage.java
@@ -28,6 +28,7 @@ public class GameMessage {
         SUCCESS("성공"),
         FAIL("실패"),
         TOTAL_TRY("총 시도한 횟수: %d");
+
         private final String contents;
 
         Result(String contents) {
@@ -39,4 +40,20 @@ public class GameMessage {
         }
     }
 
+    public enum Exception {
+        IS_NUMBER("다리 길이는 숫자여야 합니다."),
+        OUT_OF_RANGE_BRIDGE("다리 길이가 3부터 20 사이의 숫자에서 벗어났습니다."),
+        DISALLOWED_INPUT_MOVE("이동할 칸을 선택하는 값은 U, D 만을 허용합니다."),
+        DISALLOWED_INPUT_RE_GAME("게임을 다시 시도하는 여부의 값은 R, Q 만을 허용합니다.");
+
+        private final String contents;
+
+        Exception(String contents) {
+            this.contents = contents;
+        }
+
+        public String getContents() {
+            return contents;
+        }
+    }
 }
