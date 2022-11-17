dd781711523538cc77c232f888ae34dba5f11b8e
minaver <psmjoshua@gmail.com>
2022-11-17 21:55:16 +0900

    feat(BridgeGameResultDto): BridgeGameResultDto 필드, 생성자, setter 추가

--- src/main/java/bridge/BridgeGameResultDto.java
+++ src/main/java/bridge/BridgeGameResultDto.java
@@ -1,2 +1,16 @@
-package bridge;public class BridgeGameResultDto {
+package bridge;
+
+import java.util.List;
+
+public class BridgeGameResultDto {
+    private List<List<String>> gradingBoard;
+    private boolean isCorrect;
+
+    public void setGradingBoard(List<List<String>> gradingBoard) {
+        this.gradingBoard = gradingBoard;
+    }
+
+    public void setCorrect(boolean correct) {
+        isCorrect = correct;
+    }
 }
