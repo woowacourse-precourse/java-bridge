ec304b28ce0696cb3540cc0ec6cbbe07b4cdd0f4
whdcks420 <whdcks420@gmail.com>
2022-11-17 19:34:06 +0900

    test: 재시작 여부 입력 테스트

---
+++ src/test/java/bridge/validator/ReStartValidatorTest.java
@@ -0,0 +1,24 @@
+package bridge.validator;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ReStartValidatorTest {
+
+    @DisplayName("재시작 여부 입력 테스트 - 실패")
+    @ParameterizedTest
+    @ValueSource(strings = {"woowa", "tech", "course"})
+    void inputRestartFailTest(String input) {
+        assertThrows(IllegalArgumentException.class, () -> ReStartValidator.validate(input));
+    }
+
+    @DisplayName("재시작 여부 입력 테스트 - 실패")
+    @ParameterizedTest
+    @ValueSource(strings = {"Q", "R"})
+    void inputRestartSuccessTest(String input) {
+        assertDoesNotThrow(() -> ReStartValidator.validate(input));
+    }
+}
