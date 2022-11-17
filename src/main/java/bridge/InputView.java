7022579295ce75f03c3eeb1beaa4f907915400c0
ouohoon <begreat@kakao.com>
2022-11-18 01:18:52 +0900

    feat(InputView): 재시작/종료 여부를 입력받고, 검증하는 기능 구현

--- src/main/java/bridge/InputView.java
+++ src/main/java/bridge/InputView.java
@@ -52,6 +52,14 @@ public class InputView {
      * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
      */
     public String readGameCommand() {
-        return null;
+        String input = readLine();
+        validateCommand(input);
+        return input;
+    }
+
+    private void validateCommand(String command) {
+        if (!("R".equals(command) || "Q".equals(command))) {
+            throw new IllegalArgumentException();
+        }
     }
 }
