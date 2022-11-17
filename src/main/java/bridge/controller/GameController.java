87359a32c67ab04c3c7d30b7624fa7587a9f2d2a
pilyang <30036534+pilyang@users.noreply.github.com>
2022-11-17 14:53:44 +0900

    feat: GameController 추가
    - controller package 생성
    - GameControlelr class 생성

---
+++ src/main/java/bridge/controller/GameController.java
@@ -0,0 +1,12 @@
+package bridge.controller;
+
+import bridge.domain.BridgeGame;
+
+public class GameController {
+
+    private BridgeGame bridgeGame;
+
+    public GameController() {
+        bridgeGame = new BridgeGame();
+    }
+}
