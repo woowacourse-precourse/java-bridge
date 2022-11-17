47f5dccec8d3fc7800b2238aa4e05245ae61cb66
great-park <cksgh1735@naver.com>
2022-11-18 01:34:50 +0900

    feat: 시작 메시지 출력

--- src/main/java/bridge/view/OutputView.java
+++ src/main/java/bridge/view/OutputView.java
@@ -1,5 +1,9 @@
 package bridge.view;
 
+import bridge.domain.BridgeGameResult;
+
+import java.util.List;
+
 /**
  * 사용자에게 게임 진행 상황과 결과를 출력하는 역할을 한다.
  */
@@ -7,13 +11,13 @@ public class OutputView {
     private static final String BRIDGE_SIZE_REQUEST_MESSAGE = "다리의 길이를 입력해주세요.";
     private static final String MOVING_REQUEST_MESSAGE = "이동할 칸을 선택해주세요. (위: U, 아래: D)";
     private static final String GAME_COMMAND_REQUEST_MESSAGE = "게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)";
-
+    private static final String START_MESSAGE = "다리 건너기 게임을 시작합니다.";
     /**
      * 현재까지 이동한 다리의 상태를 정해진 형식에 맞춰 출력한다.
      * <p>
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void printMap() {
+    public static void printMap(List<BridgeGameResult> bridgeGameResults) {
     }
 
     /**
@@ -21,7 +25,12 @@ public class OutputView {
      * <p>
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void printResult() {
+    public static void printResult(List<BridgeGameResult> bridgeGameResults) {
+    }
+
+    public static void printStartMessage() {
+        System.out.println(START_MESSAGE);
+        printEnter();
     }
 
     public static void printEnter() {
