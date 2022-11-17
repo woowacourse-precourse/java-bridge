0c769d72759f363665206911d7c63f1a4e757acb
sunwng <sunwgna@gmail.com>
2022-11-17 16:30:39 +0900

    feat: feature_number_6 / add getting bridge size input method and related exception check method

--- docs/README.md
+++ docs/README.md
@@ -4,10 +4,11 @@
 - [X] 입력된 이동에 따른 진행 상황 계산 기능 (3)
 - [X] 사용자의 직전 경로 초기화 기능 (4)
 - [X] 게임 관리 기능 (재시도 or 종료 / 시도 횟수) (5)
-- [ ] 다리 길이 입력 인터페이스 (InputView - BridgeGame) (6)
+- [X] 다리 길이 입력 인터페이스 (InputView - BridgeGame) (6)
 - [ ] 이동 입력 인터페이스 (InputView - BirdgeGame) (7)
 - [ ] 진행 상황 출력 인터페이스 (OutputView - BridgeGame) (8)
 - [ ] 게임 결과 출력 인터페이스 (OutputView - BirdgeGame) (9)
+- [ ] Application 에 기능 통합 (10)
 
 **예외 사항 목록**  
 &rarr; IllegalArgumentException 발생 후 발생 시점 전으로 회귀해야 함
--- src/main/java/bridge/InputView.java
+++ src/main/java/bridge/InputView.java
@@ -1,22 +1,23 @@
 package bridge;
 
+import camp.nextstep.edu.missionutils.Console;
+
+import java.util.Scanner;
+
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
  */
 public class InputView {
-
-    /**
-     * 다리의 길이를 입력받는다.
-     */
-    public int readBridgeSize() {
-        return 0;
-    }
-
-    /**
-     * 사용자가 이동할 칸을 입력받는다.
-     */
-    public String readMoving() {
-        return null;
+    public static int readBridgeSize() {
+        try {
+            System.out.println("다리의 길이를 입력해주세요.");
+            String userInputSize = Console.readLine();
+            UserInputException.numberCheck(userInputSize);
+            return Integer.parseInt(userInputSize);
+        } catch (IllegalArgumentException e) {
+            System.out.println("[ERROR]");
+            return readBridgeSize();
+        }
     }
 
     /**
