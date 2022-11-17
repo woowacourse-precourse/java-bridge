ed526056a5f1508a08e8c13b88f1df575668d937
JeongHunHui <deca3248@gmail.com>
2022-11-17 13:11:45 +0900

    feat: 게임 시작 메세지를 출력하는 기능 구현

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -2,7 +2,8 @@ package bridge;
 
 public class Application {
 
-    public static void main(String[] args) {
-        // TODO: 프로그램 구현
-    }
+  public static void main(String[] args) {
+    BridgeGame bridgeGame = new BridgeGame();
+    bridgeGame.run();
+  }
 }
