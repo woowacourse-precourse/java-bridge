da0db6a6174e3ccadb1f96a2c338a99fd8190ba1
making-a-scene <4936joo@naver.com>
2022-11-17 20:31:11 +0900

    test: 랜덤으로 생성된 갈 수 있는 길 리스트 생성 검증

--- src/test/java/bridge/ObjectGenerationTest.java
+++ src/test/java/bridge/ObjectGenerationTest.java
@@ -1,22 +1,37 @@
 package bridge;
 
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import java.util.List;
+import java.util.*;
+import java.util.function.Consumer;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class ObjectGenerationTest {
     private final Utils utils = new Utils();
 
-    @Test
     @DisplayName("새로운 UsersBridgeCrossStatus 객체를 생성한다.")
+    @Test
     void generateUsersBridgeCrossStatusTest() {
         UsersBridgeCrossStatus initialBridge = utils.makeInitialBridge();
 
         List<String> topSpace = initialBridge.getCurrentBridge().get(1);
-        Assertions.assertThat(topSpace).isEqualTo(List.of("[", " ", " ", "]"));
+        assertThat(topSpace).isEqualTo(List.of("[", " ", " ", "]"));
         List<String> underSpace = initialBridge.getCurrentBridge().get(0);
-        Assertions.assertThat(underSpace).isEqualTo(List.of("[", " ", " ", "]"));
+        assertThat(underSpace).isEqualTo(List.of("[", " ", " ", "]"));
     }
+
+    @DisplayName("길이가 10인 새로운 answerBridge를 생성한다.")
+    @Test
+    void generateAnswerBridgeTest() {
+        List<String> answerBridge = utils.makeAnswerBridge(10);
+        assertThat(answerBridge.size()).isEqualTo(10);
+        assertThat(answerBridge).satisfiesAnyOf(
+                value -> {assertThat(answerBridge).containsOnly("U", "D");},
+                value -> {assertThat(answerBridge).containsOnly("D");},
+                value -> {assertThat(answerBridge).containsOnly("U");}
+        );
+    }
+
 }
