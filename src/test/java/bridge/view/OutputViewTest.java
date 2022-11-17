52fbbe3d865590aaa944e27fb7c7cf20661c8b55
newh08 <newh08@gmail.com>
2022-11-17 23:53:25 +0900

    test: implement test for function to print final result message

--- src/test/java/bridge/view/OutputViewTest.java
+++ src/test/java/bridge/view/OutputViewTest.java
@@ -57,4 +57,75 @@ class OutputViewTest {
                 outputStreamCaptor.toString()
                         .trim());
     }
+
+    @DisplayName("게임을 첫 번째에 성공시 printResult() 로 정확한 내용을 출력한다.")
+    @Test
+    void printResultTest1() {
+        List<String> bridge = new ArrayList<>(Arrays.asList("U", "D", "U", "D"));
+        BridgeGame bridgeGame = new BridgeGame(bridge);
+        bridgeGame.move("U");
+        bridgeGame.move("D");
+        bridgeGame.move("U");
+        bridgeGame.move("D");
+        OutputView outputView = new OutputView(bridgeGame);
+        outputView.printResult();
+        assertEquals("최종 게임 결과\n[ O |   | O |   ]\n[   | O |   | O ]\n\n게임 성공 여부: 성공\n총 시도한 횟수: 1",
+                outputStreamCaptor.toString()
+                        .trim());
+    }
+
+    @DisplayName("게임 첫 번째 실패시 printResult() 로 정확한 내용을 출력한다.")
+    @Test
+    void printResultTest2() {
+        List<String> bridge = new ArrayList<>(Arrays.asList("U", "D", "U", "D"));
+        BridgeGame bridgeGame = new BridgeGame(bridge);
+        bridgeGame.move("U");
+        bridgeGame.move("D");
+        bridgeGame.move("D");
+        OutputView outputView = new OutputView(bridgeGame);
+        outputView.printResult();
+        assertEquals("최종 게임 결과\n[ O |   |   ]\n[   | O | X ]\n\n게임 성공 여부: 실패\n총 시도한 횟수: 1",
+                outputStreamCaptor.toString()
+                        .trim());
+    }
+
+    @DisplayName("게임을 두 번째에 성공시 printResult() 로 정확한 내용을 출력한다.")
+    @Test
+    void printResultTest3() {
+        List<String> bridge = new ArrayList<>(Arrays.asList("U", "D", "U", "D"));
+        BridgeGame bridgeGame = new BridgeGame(bridge);
+        OutputView outputView = new OutputView(bridgeGame);
+        bridgeGame.move("U");
+        bridgeGame.move("D");
+        bridgeGame.move("D");
+        bridgeGame.retry();
+        bridgeGame.move("U");
+        bridgeGame.move("D");
+        bridgeGame.move("U");
+        bridgeGame.move("D");
+        outputView.printResult();
+        assertEquals("최종 게임 결과\n[ O |   | O |   ]\n[   | O |   | O ]\n\n게임 성공 여부: 성공\n총 시도한 횟수: 2",
+                outputStreamCaptor.toString()
+                        .trim());
+    }
+
+    @DisplayName("게임 두 번째 실패시 정확한 printResult() 로 내용을 출력한다.")
+    @Test
+    void printResultTest4() {
+        List<String> bridge = new ArrayList<>(Arrays.asList("U", "D", "U", "D"));
+        BridgeGame bridgeGame = new BridgeGame(bridge);
+        OutputView outputView = new OutputView(bridgeGame);
+        bridgeGame.move("U");
+        bridgeGame.move("D");
+        bridgeGame.move("D");
+        bridgeGame.retry();
+        bridgeGame.move("U");
+        bridgeGame.move("D");
+        bridgeGame.move("U");
+        bridgeGame.move("U");
+        outputView.printResult();
+        assertEquals("최종 게임 결과\n[ O |   | O | X ]\n[   | O |   |   ]\n\n게임 성공 여부: 실패\n총 시도한 횟수: 2",
+                outputStreamCaptor.toString()
+                        .trim());
+    }
 }
