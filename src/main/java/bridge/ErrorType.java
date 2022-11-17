a81860e949a06579c34bc52d3241a8182445fe92
cpot5620 <=cpot5620@naver.com>
2022-11-17 18:31:57 +0900

    refactor(ErrorType): modify variable names

--- src/main/java/bridge/ErrorType.java
+++ src/main/java/bridge/ErrorType.java
@@ -3,8 +3,8 @@ package bridge;
 public enum ErrorType {
     NUMERIC("3 ~ 20 사이의 숫자를 입력해주세요."),
     RANGE("다리 길이는 3 ~ 20 사이입니다."),
-    MOVE("U(위 칸), D(아래 칸)만 입력해주세요."),
-    COMMAND("R(재시작), Q(종료)만 입력해주세요.");
+    MOVING("U(위 칸), D(아래 칸)만 입력해주세요."),
+    CONTINUING("R(재시작), Q(종료)만 입력해주세요.");
 
     private final String message;
 
