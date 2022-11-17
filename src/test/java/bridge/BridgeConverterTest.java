ad490c339059aaf70bcbaa126b22dcaf3b0b0329
woosung1223 <dntjd991223@naver.com>
2022-11-17 20:08:08 +0900

    test(BridgeConveter): 테스트 코드 생성

---
+++ src/test/java/bridge/BridgeConverterTest.java
@@ -0,0 +1,24 @@
+package bridge;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BridgeConverterTest {
+
+    @Nested
+    @DisplayName("getMessageFromNumber 메소드 테스트")
+    class GetMessageFromNumber {
+        @ParameterizedTest
+        @CsvSource(value = {"0, D", "1, U"})
+        void 숫자를_입력받으면_매핑된_메세지를_잘_반환하는지_테스트(int number, String expected) {
+            assertThat(BridgeConverter.getMessageFromNumber(number)).isEqualTo(expected);
+        }
+    }
+}
