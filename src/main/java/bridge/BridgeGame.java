0acbffb96fd421f4354595bdac2554d06cd3a04c
소대훈 <theogns0681@naver.com>
2022-11-17 10:32:57 +0900

    [refactor] 매개변수 감소

--- src/main/java/bridge/BridgeGame.java
+++ src/main/java/bridge/BridgeGame.java
@@ -25,7 +25,7 @@ public class BridgeGame {
         input = inputView.readMoving();
         outputView.printMap(currentIndex, bridgeList, input);
         if (!Objects.equals(bridgeList.get(currentIndex), input)) {
-            retry(bridgeList, currentIndex, false);
+            retry(bridgeList, currentIndex);
             return;
         }
         move(currentIndex + 1, bridgeList);
@@ -36,13 +36,13 @@ public class BridgeGame {
      * <p>
      * 재시작을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void retry(List<String> bridgeList, int currentIndex, boolean winning) {
+    public void retry(List<String> bridgeList, int currentIndex) {
         String regame = inputView.readGameCommand();
         if (Objects.equals(regame, "R")) {
             tryCount++;
             move(0, bridgeList);
-        }else if (Objects.equals(regame, "Q")) {
-            outputView.printResult(currentIndex, bridgeList, input, tryCount, winning);
+        } else if (Objects.equals(regame, "Q")) {
+            outputView.printResult(currentIndex, bridgeList, input, tryCount, false);
         }
     }
 }
