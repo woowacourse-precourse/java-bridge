19e9cb1226a5bb6c9bb42e4279423e16f4c89fa3
Joonsoo Kim <kjsoo1901@gmail.com>
2022-11-17 13:40:52 +0900

    feat: BridgeSize 관련 Validation 로직 추가
    StringToIntegerConverter 추가
    
    Validation 추가
    validateInteger, validateBridgeSizeInRange3to20 추가

--- src/main/java/bridge/utils/Constants.java
+++ src/main/java/bridge/utils/Constants.java
@@ -5,4 +5,9 @@ public class Constants {
     public static final int UP_NUMBER = 1;
     public static final String DOWN_BLOCK = "D";
     public static final String UP_BLOCK = "U";
+    public static final int MIN_BRIDGE_SIZE = 3;
+    public static final int MAX_BRIDGE_SIZE = 20;
+
+    public static final String ERROR_BRIDGE_SIZE_NOT_INTEGER = "[ERROR] 다리 길이는 숫자여야 합니다";
+    public static final String ERROR_BRIDGE_SIZE_NOT_IN_RANGE = "[ERROR] 다리 길이는 3부터 20 사이의 숫자여야 합니다.";
 }
---
+++ src/main/java/bridge/utils/Validation.java
@@ -0,0 +1,27 @@
+package bridge.utils;
+
+
+import static bridge.utils.Constants.*;
+
+public class Validation {
+    public void validateInteger(String stringValueIsInteger){
+        if(stringValueIsInteger.charAt(0) < '0' || stringValueIsInteger.charAt(0) > '9')
+            throw new IllegalArgumentException(ERROR_BRIDGE_SIZE_NOT_INTEGER);
+    }
+
+    public void validateBridgeSizeInRange3to20(int bridgeSize){
+        if(bridgeSize < MIN_BRIDGE_SIZE || bridgeSize > MAX_BRIDGE_SIZE){
+            throw new IllegalArgumentException(ERROR_BRIDGE_SIZE_NOT_IN_RANGE);
+        }
+    }
+
+
+
+    public void validatePlayerMove(){
+
+    }
+
+    public void validateRestart(){
+
+    }
+}
