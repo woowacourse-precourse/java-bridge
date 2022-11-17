efdc7dae45fff54b4ed13bb874925f898adbcba5
dongjun0128 <70315475+dongjun0128@users.noreply.github.com>
2022-11-17 14:08:21 +0900

    feat(Application) : 게임 시작 기능 추가

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -4,5 +4,12 @@ public class Application {
 
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        try{
+            BridgeProcess bridgeProcess = new BridgeProcess();
+            bridgeProcess.startGame();
+        } catch (IllegalArgumentException e){
+            System.out.println(e);
+        }
+
     }
 }
