41fa31412d8c9709b9da0281698da2d01660e7e5
nylonmusk <106235834+nylonmusk@users.noreply.github.com>
2022-11-18 01:55:45 +0900

    style(ErrorMessage): 공백줄 제거

--- src/main/java/constant/ErrorMessage.java
+++ src/main/java/constant/ErrorMessage.java
@@ -1,7 +1,6 @@
-package bridge;
+package constant;
 
 public enum ErrorMessage {
-
     RETRY_OR_QUIT_INVALID("재시도는 R, 종료는 Q를 입력하세요."),
     MOVE_INVALID("이동할 칸은 위(U) 또는 아래(D)를 입력하세요."),
     BRIDGE_SIZE_NOT_NUMBER("다리 길이는 숫자만 입력 가능합니다."),
