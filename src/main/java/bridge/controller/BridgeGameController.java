d4344e85fb12d784e3037e11d9d2bee3be96d022
dooboocookie <bkh3030@gmail.com>
2022-11-17 20:53:50 +0900

    feat: 다리 길이를 입력받는 뷰와 새로운 다리를 만드는 모델 이어주는 컨트롤러 메소드 구현

--- src/main/java/bridge/controller/BridgeGameController.java
+++ src/main/java/bridge/controller/BridgeGameController.java
@@ -1,16 +1,24 @@
 package bridge.controller;
 
 import bridge.model.BridgeGame;
+import bridge.view.InputView;
+import bridge.view.OutputView;
 
 public class BridgeGameController {
+    InputView inputView;
+    OutputView outputView;
     BridgeGame bridgeGame;
 
-    public BridgeGameController() {
-
+    public BridgeGameController(InputView inputView, OutputView outputView, BridgeGame bridgeGame) {
+        this.inputView = inputView;
+        this.outputView = outputView;
+        this.bridgeGame = bridgeGame;
     }
 
     public void createNewGame() {
+        int bridgeLength = inputView.readBridgeSize();
 
+        bridgeGame.newBridge(bridgeLength);
     }
 
     public void playTurn() {
