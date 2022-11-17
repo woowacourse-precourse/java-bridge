92fc1c080c570ca41faeccf8cf3a18f8a4003a4a
popomarin28@gmail.com <popomarin28@gmail.com>
2022-11-17 15:13:21 +0900

    test(ValidateUserInput) : 다리 길이 예외 검사실행

---
+++ src/test/java/bridge/ValidateUserInputTest.java
@@ -0,0 +1,24 @@
+package bridge;
+
+import static org.assertj.core.api.Assertions.*;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ValidateUserInputTest {
+    ValidateUserInput validateUserInput = new ValidateUserInput();
+
+    @DisplayName("다리 길이가 유효한지에 대한 검사")
+    @Test
+    void checkBridgeLength() {
+        String bridgeLength = "2,1";
+        assertThatThrownBy(() -> validateUserInput.checkBridgeLength(bridgeLength))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("[ERROR]");
+    }
+
+    @Test
+    void checkReadMoving() {
+    }
+}
