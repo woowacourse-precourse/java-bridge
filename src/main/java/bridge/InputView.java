cebb8f25d8a3d4bf9eae1ca7412de5b88baf342e
KyungKyu Park <rudrb0505@gmail.com>
2022-11-17 14:05:43 +0900

    feat(InputView): 생성할 다리 길이를 입력하는 기능 구현

--- src/main/java/bridge/InputView.java
+++ src/main/java/bridge/InputView.java
@@ -1,28 +1,40 @@
 package bridge;
 
+
+import camp.nextstep.edu.missionutils.Console;
+
+/**
+ * 제공된 InputView 클래스를 활용해 구현해야 한다.
+ * InputView의 패키지는 변경할 수 있다.
+ * InputView의 메서드의 시그니처(인자, 이름)와 반환 타입은 변경할 수 있다.
+ * 사용자 값 입력을 위해 필요한 메서드를 추가할 수 있다.
+ */
+
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
  */
 public class InputView {
 
-    /**
-     * 다리의 길이를 입력받는다.
-     */
-    public int readBridgeSize() {
-        return 0;
-    }
+	/**
+	 * 다리의 길이를 입력받는다.
+	 */
+	public int readBridgeSize() {
+		System.out.println();
+		String userInput = Console.readLine();
+		return Integer.parseInt(userInput);
+	}
 
-    /**
-     * 사용자가 이동할 칸을 입력받는다.
-     */
-    public String readMoving() {
-        return null;
-    }
+	/**
+	 * 사용자가 이동할 칸을 입력받는다.
+	 */
+	public String readMoving() {
+		return null;
+	}
 
-    /**
-     * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
-     */
-    public String readGameCommand() {
-        return null;
-    }
+	/**
+	 * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
+	 */
+	public String readGameCommand() {
+		return null;
+	}
 }
