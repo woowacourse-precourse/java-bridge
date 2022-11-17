0f238f7fb76492645ea055955ee6fb849c14d0bc
junpakPark <112045553+junpakPark@users.noreply.github.com>
2022-11-17 15:26:21 +0900

    feat(ExitOption): 클래스명 변경 및 of 메서드 추가

--- src/main/java/bridge/validator/ExitOption.java
+++ src/main/java/bridge/validator/ExitOption.java
@@ -4,32 +4,39 @@ import java.util.List;
 
 import bridge.Error;
 
-public enum Option {
+public enum ExitOption {
 	REPLAY("R"),
 	QUIT("Q"),
 	;
 
 	private final String command;
 
-	Option(String command) {
+	ExitOption(String command) {
 		this.command = command;
 	}
 
-	public static boolean isReplay(String inputValue) {
+	public static ExitOption of(String inputValue) {
 		String upperCase = inputValue.toUpperCase();
 		validate(upperCase);
-		return upperCase.equals(REPLAY.command);
+		if (upperCase.equals(REPLAY.command)) {
+			return REPLAY;
+		}
+		return QUIT;
 	}
 
 	public String getCommand() {
 		return command;
 	}
 
+	public boolean isReplay() {
+		return this == REPLAY;
+	}
+
 	private static void validate(String inputValue) {
 		List<String> commandStrings = List.of(REPLAY.command, QUIT.command);
 
 		if (!commandStrings.contains(inputValue)) {
-			throw new IllegalArgumentException(Error.COMMAND.getMessage());
+			throw new IllegalArgumentException(Error.EXIT_OPTION.getMessage());
 		}
 	}
 }
