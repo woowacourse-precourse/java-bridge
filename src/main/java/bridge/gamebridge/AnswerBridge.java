c36beff2bb13cb74c247f652674add88f12faac5
kimminsu <xjfcnfw3@naver.com>
2022-11-17 21:49:44 +0900

    feat: 자동으로 생성된 다리를 저장하는 객체 구현

---
+++ src/main/java/bridge/gamebridge/AnswerBridge.java
@@ -0,0 +1,12 @@
+package bridge.gamebridge;
+
+import bridge.domain.Bridge;
+
+public class AnswerBridge {
+
+    private final Bridge bridge;
+
+    public AnswerBridge(Bridge bridge) {
+        this.bridge = bridge;
+    }
+}
