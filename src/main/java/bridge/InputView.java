3a24b552f1c21b53142fb079789e169b218a5551
realslimtaek <89964264+realslimtaek@users.noreply.github.com>
2022-11-17 18:28:10 +0900

    fix : 유저 입력을 리스트로 추가하기 위한 수정

--- src/main/java/bridge/InputView.java
+++ src/main/java/bridge/InputView.java
@@ -2,57 +2,55 @@ package bridge;
 
 import camp.nextstep.edu.missionutils.Console;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
  */
 public class InputView {
 
-    static int size = 0;
+    static int size = 1;
+    static List<String> move = new ArrayList<>();
 
-    public int readBridgeSize() {
+    public void readBridgeSize() {
         System.out.println("다리의 길이를 입력해주세요.");
-        int bridge_size = 0;
-        try{
-            bridge_size = write_bridge_size();
-        } catch(IllegalArgumentException e){
+        try {
+            size = write_bridge_size();
+        } catch (IllegalArgumentException e) {
             System.out.println(e.getMessage());
             readBridgeSize();
         }
-        size = bridge_size;
-        return bridge_size;
     }
-    public int write_bridge_size(){
+
+    public int write_bridge_size() {
         int answer = 0;
         try {
             answer = Integer.parseInt(Console.readLine());
-        }
-        catch(NumberFormatException e){
+        } catch (NumberFormatException e) {
             throw new IllegalArgumentException("[ERROR] 잘못된 입력을 하였습니다.");
         }
         return answer;
     }
+
     /**
      * 사용자가 이동할 칸을 입력받는다.
      */
-    public String readMoving() {
+    public void readMoving() {
         System.out.println("이동할 칸을 선택해주세요. (위: U, 아래: D)");
-        String answer = "";
         try {
-            answer = checkMoving();
-        }
-        catch(IllegalArgumentException e){
+            move.add(checkMoving());
+        } catch (IllegalArgumentException e) {
             System.err.println(e.getMessage());
             readMoving();
         }
-        return answer;
     }
 
     public String checkMoving() {
         String answer = "";
         try {
             answer = Console.readLine();
-        }
-        catch(StringIndexOutOfBoundsException e){
+        } catch (StringIndexOutOfBoundsException e) {
             throw new IllegalArgumentException("[ERROR] 잘못된 입력을 하였습니다.");
         }
         return answer;
@@ -65,7 +63,7 @@ public class InputView {
         return null;
     }
 
-    public int getSize(){
+    public int getSize() {
         return size;
     }
 }
