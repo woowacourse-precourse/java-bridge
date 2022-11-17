9e015d8df48e9869e86304af71603600e7805ee5
JHS <wnghdtjr129@naver.com>
2022-11-17 16:51:36 +0900

    feat(InputView, OutPutView): 이동 칸 입력

--- src/main/java/bridge/view/InputView.java
+++ src/main/java/bridge/view/InputView.java
@@ -2,24 +2,15 @@ package bridge.view;
 
 import camp.nextstep.edu.missionutils.Console;
 
-/**
- * 사용자로부터 입력을 받는 역할을 한다.
- */
 public class InputView {
 
-    /**
-     * 다리의 길이를 입력받는다.
-     */
     public int readBridgeSize() {
         String bridgeSize = Console.readLine();
         return Integer.parseInt(bridgeSize);
     }
 
-    /**
-     * 사용자가 이동할 칸을 입력받는다.
-     */
     public String readMoving() {
-        return null;
+        return Console.readLine();
     }
 
     /**
