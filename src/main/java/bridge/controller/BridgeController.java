c3c8784c878e66156c8a55c6057538bdfd6254de
great-park <cksgh1735@naver.com>
2022-11-18 01:35:56 +0900

    feat: 다리의 길이만큼 연속으로 정답을 맞추면 승리한다.

--- src/main/java/bridge/controller/BridgeController.java
+++ src/main/java/bridge/controller/BridgeController.java
@@ -1,55 +1,81 @@
 package bridge.controller;
 
-import bridge.domain.BridgeGameMachine;
-import bridge.domain.BridgeGameResult;
-import bridge.domain.BridgeGameTotalResult;
-import bridge.domain.BridgeMaker;
+import bridge.domain.*;
 import bridge.domain.generator.BridgeRandomNumberGenerator;
+import bridge.domain.vo.GameCommand;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 import static bridge.view.InputView.*;
 import static bridge.view.OutputView.*;
 
 public class BridgeController {
     private final BridgeMaker bridgeMaker;
-    private final BridgeGameMachine bridgeGameMachine;
+    private final BridgeGame bridgeGame;
+
+    private final String QUIT_CONDITION = "Q";
 
     public BridgeController(BridgeRandomNumberGenerator bridgeRandomNumberGenerator) {
         this.bridgeMaker = new BridgeMaker(bridgeRandomNumberGenerator);
-        this.bridgeGameMachine = new BridgeGameMachine();
+        this.bridgeGame = new BridgeGame();
     }
 
     public void start() {
         try {
             printStartMessage();
-            BridgeGameTotalResult bridgeGameTotalResult = play(createBridge());
-            printResult(bridgeGameTotalResult);
+            List<BridgeGameResult> bridgeGameResults = play(createBridge());
+            printResult(bridgeGameResults);
         } catch (IllegalArgumentException error) {
             printErrorMessage(error.getMessage());
         }
     }
 
-    private BridgeGameTotalResult play(List<String> bridge) {
-        BridgeGameTotalResult bridgeGameTotalResult = new BridgeGameTotalResult();
-        createGameResult(bridgeGameTotalResult, bridge);
-        return bridgeGameTotalResult;
+    private List<BridgeGameResult> play(List<String> bridge) {
+        List<BridgeGameResult> bridgeGameResults = tryOnce(bridge);
+        if (!isSuccess(bridgeGameResults, bridge.size())) {
+            bridgeGame.retry();
+        }
+        return bridgeGameResults;
     }
 
-    private void createGameResult(BridgeGameTotalResult bridgeGameTotalResult, List<String> bridge) {
+    private List<BridgeGameResult> tryOnce(List<String> bridge) {
+        List<BridgeGameResult> bridgeGameResults = new ArrayList<>();
+        do {
+            bridgeGameResults.clear();
+            createGameResult(bridgeGameResults, bridge);
+        } while (isRetry());
+        return bridgeGameResults;
+    }
+
+    private void createGameResult(List<BridgeGameResult> bridgeGameResults, List<String> bridge) {
         BridgeGameResult bridgeGameResult;
         int bridgeIndex = 0;
         do {
-            bridgeGameResult = bridgeGameMachine.computeGameResult(readMoving(), bridge.get(bridgeIndex));
+            bridgeGameResult = bridgeGame.move(bridge, bridgeIndex);
             bridgeIndex += 1;
-        } while (isContinue(bridgeGameResult));
+            addGameResult(bridgeGameResult, bridgeGameResults);
+            printMap(bridgeGameResults);
+        } while (isContinue(bridgeGameResult, bridgeIndex, bridge.size()));
     }
 
-    private void addGameResult(BridgeGameResult bridgeGameResult, BridgeGameTotalResult bridgeGameTotalResult) {
+    private void addGameResult(BridgeGameResult bridgeGameResult, List<BridgeGameResult> bridgeGameResults) {
+        bridgeGameResults.add(bridgeGameResult);
     }
 
-    private boolean isContinue(BridgeGameResult bridgeGameResult) {
+    private boolean isContinue(BridgeGameResult bridgeGameResult, int bridgeIndex, int bridgeSize) {
+        if (bridgeIndex == bridgeSize){
+            return false;
+        }
         return bridgeGameResult.getIsMatched();
+    }
+
+    private boolean isSuccess(List<BridgeGameResult> bridgeGameResults, int bridgeSize) {
+        if (bridgeGameResults.size() == bridgeSize) {
+            return true;
+        }
+        return false;
     }
 
     private List<String> createBridge() {
--- src/main/java/bridge/domain/BridgeGame.java
+++ src/main/java/bridge/domain/BridgeGame.java
@@ -1,5 +1,12 @@
 package bridge.domain;
 
+import bridge.domain.vo.Moving;
+
+import java.util.List;
+import java.util.Objects;
+
+import static bridge.view.InputView.readMoving;
+
 /**
  * 다리 건너기 게임을 관리하는 클래스
  */
@@ -10,7 +17,8 @@ public class BridgeGame {
      * <p>
      * 이동을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void move() {
+    public BridgeGameResult move(List<String> bridge, int bridgeIndex) {
+        return computeGameResult(readMoving(), bridge.get(bridgeIndex));
     }
 
     /**
@@ -19,5 +27,14 @@ public class BridgeGame {
      * 재시작을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
     public void retry() {
+    }
+
+    private BridgeGameResult computeGameResult(Moving moving, String bridgeRoom) {
+        boolean isMatched = compare(moving.getMoving(), bridgeRoom);
+        return new BridgeGameResult(isMatched, moving.getMoving());
+    }
+
+    private boolean compare(String moving, String bridgeRoom) {
+        return Objects.equals(moving, bridgeRoom);
     }
 }
