2e201808a14b3b0116a5a4ec70e57c137ad644b2
renechoi90@gmail.com <renechoi90@gmail.com>
2022-11-17 11:30:52 +0900

    feat : 이동 칸 입력 및 예외 처리 기능 구현

--- src/main/java/bridge/InputException.java
+++ src/main/java/bridge/InputException.java
@@ -6,6 +6,8 @@ public class InputException extends IllegalArgumentException {
 	public static final String NOT_A_NUMBER = "[ERROR] 자연수만 입력해주세요.";
 	public static final String NOT_IN_BETWEEN_PROPER_RANGE = "[ERROR] 숫자는 %d 이상 %d 이하의 자연수 값이어야 합니다.";
 
+	public static final String MOVEMENT_NOT_ASSIGNED_COMMAND = "[ERROR] 이동 선택은 U 혹은 D 둘 중에 하나여야 합니다.";
+
 	public InputException(String message) {
 		super(message);
 		OutputView.printException(message);
---
+++ src/main/java/bridge/Movement.java
@@ -0,0 +1,32 @@
+package bridge;
+
+public class Movement implements InputValidator{
+	private final String movement;
+	private static final String UPPER_SIDE_MOVEMENT = "U";
+	private static final String DOWN_SIDE_MOVEMENT = "D";
+
+	public Movement(String movement){
+		validate(movement);
+		this.movement = movement;
+	}
+
+	public static Movement valueOf(String movement){		//TODO : VALIDATION과 "U" or "D" 리턴을 getter 없이 할 수 있는 방법에 대해 고민
+		return new Movement(movement);
+	}
+
+	@Override
+	public void validate(Object inputMovement) {
+		isAssignedCommand((String) inputMovement);
+	}
+
+	private void isAssignedCommand(String movement){
+		System.out.println(movement);
+		if (!movement.equals(UPPER_SIDE_MOVEMENT) && !movement.equals(DOWN_SIDE_MOVEMENT)){
+			throw new InputException(InputException.MOVEMENT_NOT_ASSIGNED_COMMAND);
+		}
+	}
+
+	public String getMovement() {
+		return movement;
+	}
+}
