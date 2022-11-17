75e9ec47af2d65256b06fc62bbc3bac864b36dbf
KangSungBum <peter9706@naver.com>
2022-11-17 17:19:37 +0900

    feat(ValidationUtil.java): add user input validation methods
    사용자 입력 validation 함수 추가
    - validateUserMovement: 사용자 입력 움직임 검증
    - validateExitCode: 사용자 입력 종료코드 검증

--- src/main/java/util/ValidationUtil.java
+++ src/main/java/util/ValidationUtil.java
@@ -5,9 +5,21 @@ import response.ErrorResponse;
 
 public class ValidationUtil {
 
-    public void validateBridgeSizeInput(int sizeInput) {
+    public static void validateBridgeSizeInput(int sizeInput) {
         if (sizeInput < 3 || sizeInput > 20) {
             throw new UserInputException(ErrorResponse.INPUT_BRIDE_SIZE_RANGE_ERROR);
+        }
+    }
+
+    public static void validateUserMovement(String movement) {
+        if (!movement.equals("U") || !movement.equals("D")) {
+            throw new UserInputException(ErrorResponse.INPUT_MOVEMENT_ERROR);
+        }
+    }
+
+    public static void validateExitCode(String exitCode) {
+        if (!exitCode.equals("R") || !exitCode.equals("Q")) {
+            throw new UserInputException(ErrorResponse.INPUT_EXITCODE_ERROR);
         }
     }
 }
