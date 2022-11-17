6ed051a6ea8afd8141cc0800009c7b3cd7852307
kdkdhoho <hkim4410@naver.com>
2022-11-17 16:44:47 +0900

    test(BridgeMakerTest): 다리 만드는 함수 테스트 코드 작성

---
+++ src/test/java/bridge/BridgeMakerTest.java
@@ -0,0 +1,28 @@
+package bridge;
+
+import bridge.domain.BridgeMaker;
+import bridge.domain.BridgeRandomNumberGenerator;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
+
+public class BridgeMakerTest {
+
+    BridgeMaker bridgeMaker = new BridgeMaker(new BridgeRandomNumberGenerator());
+
+    @DisplayName("다리 만드는 함수 테스트")
+    @Test
+    void makeBridgeTest() {
+        int size = 3;
+        List<String> bridge = bridgeMaker.makeBridge(size);
+        assertThat(bridge).contains("U", "D");
+        assertAll(
+            () -> assertThat(bridge).containsOnly("U", "D"),
+            () -> assertThat(bridge.size()).isEqualTo(size)
+        );
+
+    }
+}
