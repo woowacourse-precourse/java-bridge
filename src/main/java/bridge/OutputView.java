e06d24118337532bb2789db76c7d40aa6b04e9e6
seoyoung <sy9723@korea.ac.kr>
2022-11-18 08:38:50 +0900

    refactor: printMessageForLength 메서드 이름 변경

--- src/main/java/bridge/OutputView.java
+++ src/main/java/bridge/OutputView.java
@@ -9,7 +9,7 @@ public class OutputView {
         System.out.println("다리 건너기 게임을 시작합니다.");
     }
 
-    public void printMessageForLength() {
+    public void printMessageForBridgeSize() {
         System.out.println("다리의 길이를 입력해주세요.");
     }
 
