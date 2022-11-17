dca40a9b32562cbf1fd9cf0283c814f772f24b5c
nylonmusk <106235834+nylonmusk@users.noreply.github.com>
2022-11-18 01:48:48 +0900

    feat(StringConstant): 문자와 관련된 상수 선언

---
+++ src/main/java/constant/StringConstant.java
@@ -0,0 +1,22 @@
+package constant;
+
+public enum StringConstant {
+    CORRECT_PATH("O"),
+    WRONG_PATH("X"),
+    UP_SIDE("U"),
+    DOWN_SIDE("D"),
+    QUIT("Q"),
+    RETRY("R"),
+    BRIDGE_SIZE_REGEX("^[0-9]*$"),
+    BLANK(" ");
+
+    public final String stringConstant;
+
+    StringConstant(final String stringConstant) {
+        this.stringConstant = stringConstant;
+    }
+
+    public String getConstant() {
+        return stringConstant;
+    }
+}
