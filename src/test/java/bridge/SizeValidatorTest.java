caa89fadc7d448f7653b9d6dfd399c85c8ef20f7
kevstevie <jianna442@gmail.com>
2022-11-17 16:24:50 +0900

    feat: 다리 크기 입력 유효성 검사 클래스 추가

---
+++ src/test/java/bridge/SizeValidatorTest.java
@@ -0,0 +1,32 @@
+package bridge;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+
+public class SizeValidatorTest {
+
+    @DisplayName("범위를 벗어난 입력")
+    @Test
+    void outOfRange() {
+        Validator validator = new SizeValidator();
+        String str = "333";
+        String error = "범위";
+        assertThatThrownBy(() -> validator.validate(str))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(error);
+    }
+
+    @DisplayName("숫자가 아닌 것을 입력")
+    @Test
+    void notNumber() {
+        Validator validator = new SizeValidator();
+        String str = "가나다";
+        String error = "숫자를";
+        assertThatThrownBy(() -> validator.validate(str))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(error);
+    }
+}
