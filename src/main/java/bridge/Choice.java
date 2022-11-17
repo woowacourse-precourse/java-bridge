5a2e3aca536dd1ca333056b59e568677b1a23782
Jeangyu <gjwlsrb1700@naver.com>
2022-11-17 17:43:30 +0900

    [FEAT: bridge] 유저의 선택과 맞는지 여부를 저장하는 클래스 구현

---
+++ src/main/java/bridge/Choice.java
@@ -0,0 +1,22 @@
+package bridge;
+
+public class Choice {
+    private String input;
+    private String isRight;
+
+    public String getInput() {
+        return input;
+    }
+
+    public void setInput(String input) {
+        this.input = input;
+    }
+
+    public String getIsRight() {
+        return isRight;
+    }
+
+    public void setIsRight(String isRight) {
+        this.isRight = isRight;
+    }
+}
