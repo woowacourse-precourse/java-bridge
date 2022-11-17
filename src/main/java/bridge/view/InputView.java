d626c03a803c2c719930059794aa4ad49cb89248
mckim123 <minchan06@gmail.com>
2022-11-17 19:29:37 +0900

    feat: implement input validation method for Moving, GameCommand
    Implement validateMoving method, validateGameCommand method
    - put try-catch to catch IllegalArgumentException
    - implement restarting logic recursively

--- src/main/java/bridge/view/InputView.java
+++ src/main/java/bridge/view/InputView.java
@@ -2,6 +2,8 @@ package bridge.view;
 
 import bridge.constant.Constant;
 import bridge.constant.ExceptionMessage;
+import bridge.constant.GameCommand;
+import bridge.constant.Moving;
 import bridge.constant.PrintMessage;
 import camp.nextstep.edu.missionutils.Console;
 
@@ -22,7 +24,10 @@ public class InputView {
     public String readMoving() {
         System.out.print(PrintMessage.INPUT_MOVING.getString());
         String inputMoving = Console.readLine();
-        if (validateMoving(inputMoving) == false) {
+        try {
+            validateMoving(inputMoving);
+        } catch (IllegalArgumentException e) {
+            e.printStackTrace();
             return readMoving();
         }
         return inputMoving;
@@ -31,7 +36,10 @@ public class InputView {
     public String readGameCommand() {
         System.out.print(PrintMessage.INPUT_GAME_COMMAND.getString());
         String inputGameCommand = Console.readLine();
-        if (validateMoving(inputGameCommand) == false) {
+        try {
+            validateGameCommand(inputGameCommand);
+        } catch (IllegalArgumentException e) {
+            e.printStackTrace();
             return readGameCommand();
         }
         return inputGameCommand;
@@ -48,11 +56,19 @@ public class InputView {
         }
     }
 
-    private boolean validateMoving(String Moving) {
-        return true;
+    private void validateMoving(String moving) throws IllegalArgumentException {
+        try {
+            Moving.valueOf(moving);
+        } catch (IllegalArgumentException e) {
+            throw new IllegalArgumentException(ExceptionMessage.INVALID_MOVING.getString());
+        }
     }
 
-    private boolean validateGameCommand(String gameCommand) {
-        return true;
+    private void validateGameCommand(String gameCommand) throws IllegalArgumentException {
+        try {
+            GameCommand.valueOf(gameCommand);
+        } catch (IllegalArgumentException e) {
+            throw new IllegalArgumentException(ExceptionMessage.INVALID_GAME_COMMAND.getString());
+        }
     }
 }
