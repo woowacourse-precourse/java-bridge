6004c3925162665df6221a7e6a4bebbf405ef737
김태용 <amico741@naver.com>
2022-11-18 04:28:06 +0900

    test: 다리 길이만큼 생성하는지 테스트 추가

--- src/test/java/bridge/BridgeMakerTest.java
+++ src/test/java/bridge/BridgeMakerTest.java
@@ -21,4 +21,12 @@ public class BridgeMakerTest {
     void 랜덤숫자생성기는_오직_0_OR_1_생성한다() {
         assertThat(new BridgeRandomNumberGenerator().generate()).isIn(0, 1);
     }
+
+    @ParameterizedTest
+    @ValueSource(ints = {3, 5, 7, 11, 20})
+    @DisplayName("다리 길이가 주어졌을 때 길이만큼 건널 수 있는 방향을 생성한다.")
+    void 다리_길이가_주어졌을때_방향의개수가_일치한다(int expected) {
+        assertThat(bridgeMaker.makeBridge(expected).size())
+                .isEqualTo(expected);
+    }
 }
