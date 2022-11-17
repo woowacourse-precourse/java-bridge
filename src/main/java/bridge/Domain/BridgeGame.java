bf6cb22122e4bd78a4ed17472c96c83c18aa203c
swandevson <shai4956@gmail.com>
2022-11-17 21:36:11 +0900

    Feat(BridgeGame): 필드 및 생성자 작성
    - 다리와 플레이어를 필드로 설정
    - 사이즈를 인자로 받는 생성자

--- src/main/java/bridge/Domain/BridgeGame.java
+++ src/main/java/bridge/Domain/BridgeGame.java
@@ -4,6 +4,18 @@ package bridge.Domain;
  * 다리 건너기 게임을 관리하는 클래스
  */
 public class BridgeGame {
+    Bridge bridge;
+    Player player;
+
+
+    public BridgeGame(int size) throws IllegalArgumentException {
+        try {
+            this.bridge = new Bridge(size);
+            this.player = new Player();
+        } catch (IllegalArgumentException illegalArgumentException) {
+            throw illegalArgumentException;
+        }
+    }
 
     /**
      * 사용자가 칸을 이동할 때 사용하는 메서드
