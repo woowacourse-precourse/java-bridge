bc4637561f55f39f11318dbaa9fd9d79669875c4
EUNJEONGMUN <munej26@gmail.com>
2022-11-17 16:37:31 +0900

    feat: 초기화 상수 추가

--- src/main/java/bridge/constant/Constant.java
+++ src/main/java/bridge/constant/Constant.java
@@ -3,4 +3,5 @@ package bridge.constant;
 public class Constant {
     public static final int BRIDGE_SIZE_MIN = 3;
     public static final int BRIDGE_SIZE_MAX = 20;
+    public static final int INITIAL_NUMBER = 0;
 }
