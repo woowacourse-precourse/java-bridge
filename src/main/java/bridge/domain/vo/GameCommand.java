d36f30e533f9927c9dac53da9c3d17508b934fce
great-park <cksgh1735@naver.com>
2022-11-18 03:13:15 +0900

    refactor: enum으로 정리

--- src/main/java/bridge/domain/vo/GameCommand.java
+++ src/main/java/bridge/domain/vo/GameCommand.java
@@ -1,10 +1,10 @@
 package bridge.domain.vo;
 
+import static bridge.domain.enumeration.GameCommandType.*;
+
 public class GameCommand {
     private static final String GAME_COMMAND_LENGTH_IS_NOT_ONE = "[ERROR] 게임 재시작 여부는 문자 1개를 입력해주세요.";
     private static final String GAME_COMMAND_IS_NOT_R_AND_Q = "[ERROR] 게임 재시작 여부는 R 또는 Q로 입력해주세요.";
-    private static final String RETRY = "R";
-    private static final String END = "Q";
 
     private final String gameCommand;
 
@@ -25,7 +25,7 @@ public class GameCommand {
         if (gameCommand.length() != 1) {
             throw new IllegalArgumentException(GAME_COMMAND_LENGTH_IS_NOT_ONE);
         }
-        if (!gameCommand.equals(RETRY) && !gameCommand.equals(END)) {
+        if (!gameCommand.equals(RETRY.getGameCommandType()) && !gameCommand.equals(QUIT.getGameCommandType())) {
             throw new IllegalArgumentException(GAME_COMMAND_IS_NOT_R_AND_Q);
         }
     }
--- src/main/java/bridge/domain/vo/Moving.java
+++ src/main/java/bridge/domain/vo/Moving.java
@@ -1,10 +1,12 @@
 package bridge.domain.vo;
 
+import bridge.domain.enumeration.MovingType;
+
+import static bridge.domain.enumeration.MovingType.*;
+
 public class Moving {
     private static final String MOVING_LENGTH_IS_NOT_ONE = "[ERROR] 이동할 칸은 문자 1개를 입력해주세요.";
     private static final String MOVING_IS_NOT_U_AND_D = "[ERROR] 이동할 칸은 U 또는 D로 입력해주세요.";
-    private static final String MOVING_UP = "U";
-    private static final String MOVING_DOWN = "D";
 
     private final String moving;
 
@@ -25,7 +27,7 @@ public class Moving {
         if (moving.length() != 1) {
             throw new IllegalArgumentException(MOVING_LENGTH_IS_NOT_ONE);
         }
-        if (!moving.equals(MOVING_UP) && !moving.equals(MOVING_DOWN)) {
+        if (!moving.equals(UP.getMovingType()) && !moving.equals(DOWN.getMovingType())) {
             throw new IllegalArgumentException(MOVING_IS_NOT_U_AND_D);
         }
     }
