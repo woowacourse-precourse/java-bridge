b847a2f37ab4709d930297f9abb7cda90d7827e4
win9 <krrr8@naver.com>
2022-11-17 23:42:59 +0900

    Feat(Bridge): 다리를 관리하는 다리개개체 생성
    역할을 분리시키기 위하여 다리만을 관리하는 다리 객체를 생성한다.
    setBridge시 해당 다리가 만들어진다.

---
+++ src/main/java/bridge/model/Bridge.java
@@ -0,0 +1,16 @@
+package bridge.model;
+
+import java.util.List;
+
+public class Bridge {
+    private List<String> bridge;
+    private BridgeMaker bridgeMaker = new BridgeMaker(new BridgeRandomNumberGenerator());
+
+    public List<String> getBridge() {
+        return bridge;
+    }
+
+    public void setBridge(int size) {
+        bridge = bridgeMaker.makeBridge(size);
+    }
+}
