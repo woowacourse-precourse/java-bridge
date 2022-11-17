babee426281064cbaf73f92067c42bd40286714a
making-a-scene <4936joo@naver.com>
2022-11-17 17:04:49 +0900

    refactor: 다리에 사용되는 문자들을 final로 선언해 사용

--- src/main/java/bridge/Utils.java
+++ src/main/java/bridge/Utils.java
@@ -5,15 +5,20 @@ import java.util.Arrays;
 import java.util.List;
 
 public class Utils {
-
     private static final int TOP_SPACE = 1;
     private static final int UNDER_SPACE = 0;
+    private static final String HEAD_OF_BRIDGE = "[";
+    private static final String TAIL_OF_BRIDGE = "]";
+    private static final String BLANK = " ";
 
     // 게임 시작 전 초기 다리 생성
     public UsersBridgeCrossStatus makeInitialBridge() {
         List<List<String>> newBridge = new ArrayList<>();
-        newBridge.add(TOP_SPACE, Arrays.asList("[", " ", " ", "]"));
-        newBridge.add(UNDER_SPACE, Arrays.asList("[", " ", " ", "]"));
+
+        List<String> initialBridgeShape = Arrays.asList(HEAD_OF_BRIDGE, BLANK, BLANK, TAIL_OF_BRIDGE);
+        newBridge.add(TOP_SPACE, initialBridgeShape);
+        newBridge.add(UNDER_SPACE, initialBridgeShape);
+
         return new UsersBridgeCrossStatus(newBridge);
     }
 
