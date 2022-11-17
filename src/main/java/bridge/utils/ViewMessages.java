12f1145ff21f878303595e22abdf3ff6c07fdf1e
twoosky <lhjae3@naver.com>
2022-11-18 02:23:11 +0900

    feat(view): 게임 시작 문구 출력 메소드 구현

--- src/main/java/bridge/utils/ViewMessages.java
+++ src/main/java/bridge/utils/ViewMessages.java
@@ -5,6 +5,7 @@ public class ViewMessages {
     public static final String MOVING = "이동할 칸을 선택해주세요. (위: U, 아래: D)";
     public static final String GAME_COMMAND = "게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)";
 
+    public static final String GAME_START = "다리 건너기 게임을 시작합니다.";
     public static final String GAME_RESULT = "최종 게임 결과";
     public static final String BRIDGE_START = "[ ";
     public static final String BRIDGE_SPACE_DIVISION = " | ";
