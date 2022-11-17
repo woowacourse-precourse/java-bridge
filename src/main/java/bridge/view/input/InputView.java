d256df29f07ead1bcb7949255772f310a375e099
K-Diger <kitten.diger@gmail.com>
2022-11-17 18:59:57 +0900

    refactor : 기존 InputHandler에 존재하던 입력 구문들을 InputView 로 이관하였다.

--- src/main/java/bridge/view/input/InputView.java
+++ src/main/java/bridge/view/input/InputView.java
@@ -1,28 +1,46 @@
 package bridge.view.input;
 
+import bridge.model.inputvalidator.InputValidator;
+import camp.nextstep.edu.missionutils.Console;
+
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
  */
 public class InputView {
 
+    private final InputValidator inputValidator;
+
+    public InputView(InputValidator inputValidator) {
+        this.inputValidator = inputValidator;
+    }
+
     /**
      * 다리의 길이를 입력받는다.
      */
     public int readBridgeSize() {
-        return 0;
+        System.out.println("\n다리의 길이를 입력해주세요");
+        String input = Console.readLine();
+        inputValidator.bridgeSize(input);
+        return Integer.parseInt(input);
     }
 
     /**
      * 사용자가 이동할 칸을 입력받는다.
      */
     public String readMoving() {
-        return null;
+        System.out.println("\n이동할 칸을 선택해주세요. (위: U, 아래: D)");
+        String input = Console.readLine();
+        inputValidator.moving(input);
+        return input;
     }
 
     /**
      * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
      */
     public String readGameCommand() {
-        return null;
+        System.out.println("\n게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)");
+        String input = Console.readLine();
+        inputValidator.gameCommand(input);
+        return input;
     }
 }
