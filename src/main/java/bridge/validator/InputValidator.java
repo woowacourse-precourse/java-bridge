a104fccf8d90465b653b6e8e85a74bf8a0fc9e5c
kokodak <kokodakadokok@gmail.com>
2022-11-17 16:39:30 +0900

    feat(inputValidator): 게임 재시작/종료 여부에 대한 값을 검증하는 기능 구현

--- src/main/java/bridge/validator/InputValidator.java
+++ src/main/java/bridge/validator/InputValidator.java
@@ -4,6 +4,7 @@ public class InputValidator {
 
     private static final String BRIDGE_SIZE_ERROR = "[ERROR] 다리 길이는 3부터 20 사이의 숫자여야 합니다.";
     private static final String MOVING_ERROR = "[ERROR] 이동할 칸은 U 혹은 D 중 하나여야 합니다.";
+    private static final String GAME_COMMAND_ERROR = "[ERROR] 게임 재시작/종료 여부는 R 혹은 Q 중 하나여야 합니다.";
 
     public static int validateBridgeSize(String input) {
         if (!isNumber(input)) {
@@ -32,6 +33,13 @@ public class InputValidator {
     public static String validateMoving(String input) {
         if (!input.equals("U") && !input.equals("D")) {
             throw new IllegalArgumentException(MOVING_ERROR);
+        }
+        return input;
+    }
+
+    public static String validateGameCommand(String input) {
+        if (!input.equals("R") && !input.equals("Q")) {
+            throw new IllegalArgumentException(GAME_COMMAND_ERROR);
         }
         return input;
     }
