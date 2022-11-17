8917da8ff75a2b140f83916b338adbbb1291eeb5
jiisuniui <dudndi9752@gmail.com>
2022-11-18 03:53:18 +0900

    refactor(domain): 제공된 class들 controller, view에 각각 알맞은 곳으로 이동

--- src/main/java/bridge/domain/controller/BridgeMaker.java
+++ src/main/java/bridge/domain/controller/BridgeMaker.java
@@ -1,4 +1,4 @@
-package bridge;
+package bridge.domain.controller;
 
 import java.util.List;
 
--- src/main/java/bridge/domain/controller/BridgeRandomNumberGenerator.java
+++ src/main/java/bridge/domain/controller/BridgeRandomNumberGenerator.java
@@ -1,4 +1,4 @@
-package bridge;
+package bridge.domain.controller;
 
 import camp.nextstep.edu.missionutils.Randoms;
 
