7fe9cec511d12f4e1675c74acf4f05977730e5f6
eora21 <eora21@naver.com>
2022-11-17 10:24:58 +0900

    feat: 🎸 (BridgeMaker) 추가 다리 생성 메서드
    다리 생성 시 유효값 확인 후 랜덤값을 받고 규칙에 맞는 전환 후 생성하여 반환해주도록 했다.

--- src/main/java/bridge/BridgeMaker.java
+++ src/main/java/bridge/BridgeMaker.java
@@ -1,6 +1,8 @@
 package bridge;
 
+import bridge.model.BridgeSelectRule;
 import bridge.model.BridgeSizeRule;
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -19,7 +21,12 @@ public class BridgeMaker {
      * @return 입력받은 길이에 해당하는 다리 모양. 위 칸이면 "U", 아래 칸이면 "D"로 표현해야 한다.
      */
     public List<String> makeBridge(int size) {
-        return null;
+        validate(size);
+        return new ArrayList<>() {{
+            for (int idx = 0; idx < size; idx++) {
+                add(BridgeSelectRule.convertRandomNumberToText(bridgeNumberGenerator.generate()));
+            }
+        }};
     }
 
     private void validate(int size) {
