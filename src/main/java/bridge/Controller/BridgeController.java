774ff7c4a875b5ad11c458c98bfd27bf40da8d7f
CYC-ChoiYoungChang <choiyc1446@gmail.com>
2022-11-17 19:02:07 +0900

    refactor: 컨트롤러 로직 변경

--- src/main/java/bridge/Controller/BridgeController.java
+++ src/main/java/bridge/Controller/BridgeController.java
@@ -1,16 +1,31 @@
 package bridge.Controller;
 
-import bridge.domain.Bridge;
+import bridge.BridgeMaker;
+import bridge.BridgeRandomNumberGenerator;
+import bridge.service.BridgeGame;
+import bridge.view.InputView;
+import bridge.view.OutputView;
+
+import java.util.List;
 
 public class BridgeController {
 
+    private Integer bridgeSize;
+    private List<String> bridge;
+
+    private BridgeGame bridgeGame = new BridgeGame();
+    private InputView inputView = new InputView();
+    private OutputView outputView = new OutputView();
+    private BridgeMaker bridgeMaker = new BridgeMaker(new BridgeRandomNumberGenerator());
+
+    public BridgeController() {
+        outputView.printStartMessage();
+        init();
+    }
+    public void init(){
+        bridgeSize = inputView.readBridgeSize();
+        bridge = bridgeMaker.makeBridge(bridgeSize);
+    }
     public void run(){
-        System.out.println("다리 건너기 게임을 시작합니다.");
-        //다리 생성
-        Bridge bridge = new Bridge();
-
-        for (int bridgePosition = 0; bridgePosition < bridge.getBridgeSize(); bridgePosition++) {
-
-        }
     }
 }
