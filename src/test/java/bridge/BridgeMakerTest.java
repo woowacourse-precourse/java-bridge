64c21f1491abdf8003320744310248bba311cb97
devyourown <hyojoon56@gmail.com>
2022-11-17 17:46:23 +0900

    feat : make bridgemaker and test the test

---
+++ src/test/java/bridge/BridgeMakerTest.java
@@ -0,0 +1,31 @@
+package bridge;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BridgeMakerTest {
+    private BridgeMaker bridgeMaker;
+
+    @BeforeEach
+    void setup() {
+        bridgeMaker = new BridgeMaker(new BridgeRandomNumberGenerator());
+    }
+
+    @Test
+    void testBridgeSize() {
+        assertEquals( 3, bridgeMaker.makeBridge(3).size());
+    }
+
+    @Test
+    void testBridgeString() {
+        List<String> bridge = bridgeMaker.makeBridge(3);
+        for (int i=0; i<3; i++) {
+            assertTrue(bridge.get(i) == "U"
+                    || bridge.get(i) == "D");
+        }
+    }
+}
