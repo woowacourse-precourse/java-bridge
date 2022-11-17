88414563b72ec69817497984eea12c593968427f
JUN <chltmdwns96@naver.com>
2022-11-17 19:40:52 +0900

    feat : 상수 클래스 구현

---
+++ src/main/java/bridge/constant/Constants.java
@@ -0,0 +1,19 @@
+package bridge.constant;
+
+public class Constants {
+    public static class BridgeUtils{
+
+        public static final String BRIDGE_SEPARATOR = " | ";
+        public static final String BRIDGE_START = "[ ";
+        public static final String BRIDGE_FINISH = " ]";
+    }
+
+    public static class BridgeSign{
+        public static final String PASS = "O";
+        public static final String FAIL = "X";
+        public static final String RETRY = "R";
+        public static final String QUIT = "Q";
+        public static final String UP = "U";
+        public static final String DOWN = "D";
+    }
+}
