5b2be62b2d605a3e45db4408a7a686f2555a428b
ddubidubap <park62000@gmail.com>
2022-11-18 01:24:08 +0900

    feat: 이동할 칸 입력하라는 문구 출력하기

--- src/main/java/bridge/view/OutputView.java
+++ src/main/java/bridge/view/OutputView.java
@@ -3,6 +3,7 @@ package bridge.view;
 public class OutputView {
     static final String START_GAME = "다리 건너기 게임을 시작합니다.";
     static final String INPUT_BRIDGE_SIZE = "다리의 길이를 입력해주세요.";
+    static final String INPUT_MOVING = "이동할 칸을 선택해주세요. (위: U, 아래: D)";
 
     public void printStart() {
         System.out.println(START_GAME);
@@ -10,6 +11,10 @@ public class OutputView {
 
     public void printInputBridgeSize() {
         System.out.println(INPUT_BRIDGE_SIZE);
+    }
+
+    public void printInputMoving() {
+        System.out.println(INPUT_MOVING);
     }
 
     public void printMap() {
