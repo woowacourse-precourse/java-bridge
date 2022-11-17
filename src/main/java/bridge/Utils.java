9921e0f75333b73e765746e3fa01a63ec59bd25b
making-a-scene <4936joo@naver.com>
2022-11-17 21:59:06 +0900

    feat: 사용자가 택한 칸이 이동 가능한 칸인지 확인하는 기능 추가

--- src/main/java/bridge/Utils.java
+++ src/main/java/bridge/Utils.java
@@ -1,6 +1,14 @@
 package bridge;
 
 public class Utils {
+    private static final String CROSS_SUCCEEDED = "O";
+    private static final String CROSS_FAILED = "X";
 
+    public String checkCrossability(String answerBridge, String usersSelection) {
+        if (answerBridge.equals(usersSelection)) {
+            return CROSS_SUCCEEDED;
+        }
+        return CROSS_FAILED;
+    }
 
 }
