f5a5387886f9fb0246b215c29a3f4131536906ba
jiisuniui <dudndi9752@gmail.com>
2022-11-18 04:53:14 +0900

    refactor(GameGuide): 상수명 리팩토링

--- src/main/java/bridge/constants/GuideMessage.java
+++ src/main/java/bridge/constants/GuideMessage.java
@@ -2,8 +2,8 @@ package bridge.constants;
 
 public enum GuideMessage {
     START_MESSAGE("다리 건너기 게임을 시작합니다."),
-    LENGTH_MESSAGE("다리의 길이를 입력해주세요."),
-    MOVE_MESSAGE("이동할 칸을 선택해주세요. (위: U, 아래: D)"),
+    INPUT_LENGTH_MESSAGE("다리의 길이를 입력해주세요."),
+    INPUT_MOVE_MESSAGE("이동할 칸을 선택해주세요. (위: U, 아래: D)"),
     RESTART_MESSAGE("게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)"),
     FINAL_RESULT_MESSAGE("최종 게임 결과"),
     SUCCESS_OR_FAILURE_MESSAGE("게임 성공 여부: "),
