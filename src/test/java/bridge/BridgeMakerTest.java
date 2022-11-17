38b8ca37313b86a52ce9497bf2f5f08f343b1dfe
Isu Shin <isu010614@naver.com>
2022-11-17 23:12:46 +0900

    fix: 입력받은 길이만큼 이동 정보가 생성되는지 확인하는 테스트 코드로 수정

--- src/test/java/bridge/BridgeMakerTest.java
+++ src/test/java/bridge/BridgeMakerTest.java
@@ -3,7 +3,6 @@ package bridge;
 import bridge.domain.BridgeMaker;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.List;
@@ -12,14 +11,13 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.util.Lists.newArrayList;
 
 class BridgeMakerTest {
+    @Test
     @DisplayName("다리 생성 테스트")
-    @ValueSource(strings = {"999", "0", "-123"})
-    @ParameterizedTest
-    void validateBridgeMaker() {
+    void validateBridgeMakerLength() {
         BridgeNumberGenerator numberGenerator
-                = new ApplicationTest.TestNumberGenerator(newArrayList(1, 0, 0));
+                = new ApplicationTest.TestNumberGenerator(newArrayList(1, 0, 0, 1));
         BridgeMaker bridgeMaker = new BridgeMaker(numberGenerator);
-        List<String> bridge = bridgeMaker.makeBridge(3);
-        assertThat(bridge).containsExactly("U", "D", "D");
+        List<String> bridge = bridgeMaker.makeBridge(4);
+        assertThat(bridge).containsExactly("U", "D", "D", "U");
     }
 }
