cca9c5923b67a11c7b55c1458e579c98bbb5c322
K-Diger <kitten.diger@gmail.com>
2022-11-18 02:49:41 +0900

    refactor(model[InputView]) : util 패키지로 이동하였다.

--- src/main/java/bridge/InputView.java
+++ src/main/java/bridge/InputView.java
@@ -1,6 +1,6 @@
-package bridge.view.input;
+package bridge;
 
-import bridge.model.inputvalidator.InputValidator;
+import bridge.util.inputvalidator.InputValidator;
 import camp.nextstep.edu.missionutils.Console;
 
 /**
@@ -18,7 +18,6 @@ public class InputView {
      * 다리의 길이를 입력받는다.
      */
     public int readBridgeSize() {
-        System.out.println("\n다리의 길이를 입력해주세요");
         String input = Console.readLine();
         inputValidator.bridgeSize(input);
         return Integer.parseInt(input);
@@ -28,7 +27,6 @@ public class InputView {
      * 사용자가 이동할 칸을 입력받는다.
      */
     public String readMoving() {
-        System.out.println("\n이동할 칸을 선택해주세요. (위: U, 아래: D)");
         String input = Console.readLine();
         inputValidator.moving(input);
         return input;
@@ -38,7 +36,6 @@ public class InputView {
      * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
      */
     public String readGameCommand() {
-        System.out.println("\n게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)");
         String input = Console.readLine();
         inputValidator.gameCommand(input);
         return input;
