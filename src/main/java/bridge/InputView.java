76caf383b683b726f700256a03497a278c0a7950
JaeUpSu <hyeongs2323@gmail.com>
2022-11-17 17:42:47 +0900

    readBridgeSize | size 예외 처리 후 길이 반환

--- src/main/java/bridge/InputView.java
+++ src/main/java/bridge/InputView.java
@@ -1,5 +1,7 @@
 package bridge;
 
+import camp.nextstep.edu.missionutils.Console;
+
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
  */
@@ -9,7 +11,13 @@ public class InputView {
      * 다리의 길이를 입력받는다.
      */
     public int readBridgeSize() {
-        return 0;
+        String lengthInput = Console.readLine();
+        checkIsNumber(lengthInput);
+
+        int length = Integer.valueOf(lengthInput);
+        checkRange(length);
+
+        return length;
     }
 
     /**
@@ -24,5 +32,58 @@ public class InputView {
      */
     public String readGameCommand() {
         return null;
+    }
+
+    private static void checkIsNumber(String lengthInput) {
+        try {
+            Integer.valueOf(lengthInput);
+        } catch (IllegalArgumentException e) {
+            String message = GameMessage.IS_ERROR;
+            String contents = GameMessage.Exception.IS_NUMBER.getContents();
+            OutputView.printMessage(message + contents);
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private static void checkRange(int length) {
+        boolean rangeError = (checkStartRange(length) || checkEndRange(length));
+
+        if (rangeError) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private static boolean checkStartRange(int length) {
+        String startValue = BridgeValue
+                .Information
+                .MINIMUM_LENGTH
+                .getValue();
+
+        int start = BridgeValue.stringToInt(startValue);
+        if (length < start) {
+            return true;
+        }
+        return false;
+    }
+
+    private static boolean checkEndRange(int length) {
+        String endValue = BridgeValue
+                .Information
+                .MINIMUM_LENGTH
+                .getValue();
+
+        int end = BridgeValue.stringToInt(endValue);
+        if (length > end) {
+            return true;
+        }
+        return false;
+    }
+
+    private static void checkMoving(String move) {
+
+    }
+
+    private static void checkCommand(String command) {
+
     }
 }
