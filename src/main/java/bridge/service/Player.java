6e61cbdb33f5f7095eff7f4d41d8fdcac23d8373
JaeWon Choi <87649850+codesver@users.noreply.github.com>
2022-11-17 09:56:16 +0900

    feat(enterBridgeLength): implement method
    Implement method which returns bridge length from player.

--- src/main/java/bridge/service/Player.java
+++ src/main/java/bridge/service/Player.java
@@ -1,11 +1,24 @@
 package bridge.service;
 
+import bridge.io.InputView;
+import bridge.io.OutputView;
+
 public class Player {
 
+    private final Validator validator = new Validator();
+    private final InputView input = new InputView();
+    private final OutputView output = new OutputView();
+
     public int enterBridgeLength() {
-        // 다리의 길이 입력 3 ~ 20
-        // 다리 길이 입력값 검증
-        return 0;
+        while (true) {
+            try {
+                String bridgeLength = input.readBridgeLength();
+                validator.validateBridgeLength(bridgeLength);
+                return Integer.parseInt(bridgeLength);
+            } catch (IllegalArgumentException exception) {
+                output.printError(exception);
+            }
+        }
     }
 
     public char enterMovement() {
