641343ac4faabdae21247553597568a8c27eea80
renechoi90@gmail.com <renechoi90@gmail.com>
2022-11-17 11:29:35 +0900

    test : 이동 칸 입력시 객체 생성 및 예외 처리 기능에 대한 테스트 구현

---
+++ src/test/java/bridge/MovementTest.java
@@ -0,0 +1,29 @@
+package bridge;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.assertj.core.api.Assertions.*;
+
+public class MovementTest {
+
+	private static final String UPPER_SIDE_MOVEMENT = "U";
+	private static final String DOWN_SIDE_MOVEMENT = "D";
+
+	@DisplayName("이동할 칸 정상값 입력시 인스턴스 생성을 검증한다")
+	@ParameterizedTest
+	@ValueSource(strings = {UPPER_SIDE_MOVEMENT, DOWN_SIDE_MOVEMENT})
+	void verify_Movement_CreateInstance(String value) {
+		assertThat(new Movement(value)).isInstanceOf(Movement.class);
+	}
+
+	@DisplayName("이동할 칸 입력시 정상값을 제외한 다른 입력에 대한 예외 처리를 검증한다")
+	@ParameterizedTest
+	@ValueSource(strings = {"a", "@", "UP", "1"})
+	void verify_Movement_OnlyAsAssigned(String value) {
+		assertThatThrownBy(() -> Movement.valueOf(value))
+				.isInstanceOf(IllegalArgumentException.class)
+				.hasMessage(InputException.MOVEMENT_NOT_ASSIGNED_COMMAND);
+	}
+}
