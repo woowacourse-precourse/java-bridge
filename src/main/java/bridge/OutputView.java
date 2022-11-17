aebc9fa9aa1869b4976a76973e5c6ad5b16593dc
박시균\psk98 <psg980331@naver.com>
2022-11-17 14:38:15 +0900

    Feat: 사용자의 잘못된 다리 이동입력값에 대한 에러메시지 출력

--- src/main/java/bridge/OutputView.java
+++ src/main/java/bridge/OutputView.java
@@ -26,6 +26,10 @@ public class OutputView {
     public void printRequestMoveDirectionMessage(){
         System.out.println(REQUEST_MOVE_DIRECTION_MESSAGE);
     }
+    
+    public void printInValidMoveDirectionErrorMessage(){
+        System.out.println(INVALID_DIRECTION_MESSAGE);
+    }
 
     /**
      * 현재까지 이동한 다리의 상태를 정해진 형식에 맞춰 출력한다.
