68734ecf883d7bf708dd0eccd29dc209407a96b9
HyeonjungYun <guswnd9971@gmail.com>
2022-11-17 21:13:43 +0900

    "refactor(PrintInput): 저장된 문구 일부 변경"

--- src/main/java/bridge/message/PrintCommon.java
+++ src/main/java/bridge/message/PrintCommon.java
@@ -4,7 +4,7 @@ public enum PrintInput {
 
     START("다리 건너기 게임을 시작합니다.\n"),
     INPUT_SIZE("다리의 길이를 입력해주세요."),
-    INPUT_MOVING("이동할 칸을 선택해주세요. (위: U, 아래: D)"),
+    INPUT_MOVING("\n이동할 칸을 선택해주세요. (위: U, 아래: D)"),
     INPUT_RETRY("게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)"),
     RESULT("게임 성공 여부: "),
     RESULT_NUMBER("총 시도한 횟수: ");
