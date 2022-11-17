e35db1c8036680933ae53c9836a7279f353bfb7a
greeng00se <thegreengoose7@gmail.com>
2022-11-17 23:01:10 +0900

    feat(BridgeGameController): 요청을 처리하는 BridgeGameController 클래스 추가

---
+++ src/main/java/bridge/controller/BridgeGameController.java
@@ -0,0 +1,15 @@
+package bridge.controller;
+
+import bridge.view.InputView;
+import bridge.view.OutputView;
+
+public class BridgeGameController {
+    private final InputView inputView = new InputView();
+    private final OutputView outputView = new OutputView();
+
+    public BridgeGameController() {
+    }
+    
+    public void run() {
+    }
+}
