18a50a2c37b8234b9b5cd0235f6d9064ce9df349
WhalesBob <ndw8200@naver.com>
2022-11-17 09:24:35 +0900

    feat(Bridge) : add main BridgeGame Playing features

--- src/main/java/bridge/businesslogic/BridgeGame.java
+++ src/main/java/bridge/businesslogic/BridgeGame.java
@@ -22,12 +22,26 @@ public class BridgeGame {
         bridge = new Bridge(bridgeMaker, bridgeSize);
     }
 
+    public void play(){
+        initBridgeGame();
+
+        while(!bridge.isBridgeAllCrossed()){
+            String usersPick = inputView.readMoving();
+            boolean continueGame = move(usersPick);
+            if(!continueGame){
+                break;
+            }
+        }
+    }
+
+
     /**
      * 사용자가 칸을 이동할 때 사용하는 메서드
      * <p>
      * 이동을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void move() {
+    public boolean move(String usersPick) {
+        return true;
     }
 
     /**
@@ -35,6 +49,7 @@ public class BridgeGame {
      * <p>
      * 재시작을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void retry() {
+    public boolean retry() {
+        return false;
     }
 }
