58581293e13ab430ca37c6328acd801edea0857e
dohyun <ldh19738@gmail.com>
2022-11-18 01:00:32 +0900

    refactor(InputView, OutputView): view 패키지로 통합
    InputView의 메서드들을 정적으로 바꾸고 readBridgeSize의 타입 변경

--- src/main/java/bridge/view/InputView.java
+++ src/main/java/bridge/view/InputView.java
@@ -1,4 +1,4 @@
-package bridge;
+package bridge.view;
 
 import camp.nextstep.edu.missionutils.Console;
 
@@ -13,16 +13,15 @@ public class InputView {
     /**
      * 다리의 길이를 입력받는다.
      */
-    public int readBridgeSize() {
+    public static String readBridgeSize() {
         System.out.println(INPUT_BRIDGE_SIZE_MESSAGE);
-        String bridgeSize = Console.readLine();
-        return Integer.parseInt(bridgeSize);
+        return Console.readLine();
     }
 
     /**
      * 사용자가 이동할 칸을 입력받는다.
      */
-    public String readMoving() {
+    public static String readMoving() {
         System.out.println(CHOOSE_MOVING_BRIDGE_MESSAGE);
         return Console.readLine();
     }
@@ -30,7 +29,7 @@ public class InputView {
     /**
      * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
      */
-    public String readGameCommand() {
+    public static String readGameCommand() {
         System.out.println(RESTART_MESSAGE);
         return Console.readLine();
     }
