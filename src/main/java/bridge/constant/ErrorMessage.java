ad82c73577172dd6be1e85ec2e28f736874d2b9d
EUNJEONGMUN <munej26@gmail.com>
2022-11-18 02:02:52 +0900

    feat: 재시작 여부 입력 시 사용되는 에러 메시지 상수 추가

--- src/main/java/bridge/constant/ErrorMessage.java
+++ src/main/java/bridge/constant/ErrorMessage.java
@@ -3,7 +3,8 @@ package bridge.constant;
 public enum ErrorMessage {
     NOT_NUMBER("숫자가 아닙니다."),
     NOT_BRIDGE_SIZE_RANGE("다리의 길이는 3이상 20이하입니다."),
-    NOT_MOVING_VALUE("U(위) 또는 D(아래)만 입력할 수 있습니다.");
+    NOT_MOVING_VALUE("U(위) 또는 D(아래)만 입력할 수 있습니다."),
+    NOT_COMMAND_VALUE("R(재시작) 또는 Q(종료)만 입력할 수 있습니다.");
 
     private final String message;
     private String errorMark = "[ERROR] ";
