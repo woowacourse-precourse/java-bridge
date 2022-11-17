d5297368402c16a00f970d69091254cd190eab2c
SangWoo Yun <64531982+Awarduuu@users.noreply.github.com>
2022-11-17 16:26:01 +0900

    feat(InputView): 게임시작 입력 뷰 생성

--- src/main/java/bridge/utils/Util.java
+++ src/main/java/bridge/utils/Util.java
@@ -1,5 +1,14 @@
 package bridge.utils;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Util {
+    public static String getInput(){
+        return Console.readLine();
+    }
+
+    public static int getInt(String s) {
+        return Integer.parseInt(s);
+    }
 
 }
--- src/main/java/bridge/view/InputView.java
+++ src/main/java/bridge/view/InputView.java
@@ -1,5 +1,7 @@
 package bridge.view;
 
+import static bridge.utils.Util.*;
+
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
  */
@@ -8,8 +10,13 @@ public class InputView {
     /**
      * 다리의 길이를 입력받는다.
      */
+    private static final String PRINT_ASK_START = "다리 건너기 게임을 시작합니다.";
+    private static final String PRINT_ASK_SIZE = "다리의 길이를 입력해주세요.";
     public int readBridgeSize() {
-        return 0;
+        printSentence(PRINT_ASK_START);
+        printBlankLine();
+        printSentence(PRINT_ASK_SIZE);
+        return getInt(getInput());
     }
 
     /**
@@ -24,5 +31,13 @@ public class InputView {
      */
     public String readGameCommand() {
         return null;
+    }
+
+    public void printSentence(String s){
+        System.out.println(s);
+    }
+
+    public void printBlankLine(){
+        System.out.println("");
     }
 }
