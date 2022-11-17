128b5287664bc545ddce33dcabd41e17d78cd63b
jeongjihun <wlgns2234@naver.com>
2022-11-18 01:37:50 +0900

    feat(constant): 게임 안내메세지 상수 구현

---
+++ src/main/java/bridge/constant/BridgeGameInfo.java
@@ -0,0 +1,18 @@
+package bridge.constant;
+
+import bridge.model.BridgeGame;
+
+public enum BridgeGameInfo {
+    START("다리 건너기 게임을 시작합니다."),
+    INPUT_BRIDGE_SIZE("다리의 길이를 입력해주세요."),
+    SELECT_UP_DOWN("이동할 칸을 선택해주세요. (위: U, 아래: D)"),
+    RESTART("게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)"),
+    RESULT("최종 게임 결과: "),
+    TOTAL_TRY("총 시도한 횟수: ");
+
+    public final String message;
+    BridgeGameInfo(String message) {
+        this.message = message;
+    }
+}
+
