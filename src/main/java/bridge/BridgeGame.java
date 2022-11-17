b8c93e1ba3279c22adf995da5f74fb02d70b281f
cpot5620 <=cpot5620@naver.com>
2022-11-17 22:01:13 +0900

    feat(BridgeGame): create method to check game clear

--- src/main/java/bridge/BridgeGame.java
+++ src/main/java/bridge/BridgeGame.java
@@ -3,9 +3,6 @@ package bridge;
 import java.util.ArrayList;
 import java.util.List;
 
-/**
- * 다리 건너기 게임을 관리하는 클래스
- */
 public class BridgeGame {
     private final Bridge bridge;
     private List<String> pathOfUser;
@@ -24,6 +21,10 @@ public class BridgeGame {
 
     public void retry() {
         pathOfUser.clear();
+    }
+
+    public boolean isGameClear() {
+        return bridge.equals(pathOfUser);
     }
 
     private boolean isMovableBlock(String command) {
