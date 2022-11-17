66664fae7ccd8301362d1faaf14262f03dd4c9dc
popomarin28@gmail.com <popomarin28@gmail.com>
2022-11-17 20:48:35 +0900

    feat(BridgeGame) : play 구현 (대략적인 것만) feat(Outputview) : 각각의 업 다운 다리를 출력 하는 기능 구현

--- src/main/java/bridge/BridgeGame.java
+++ src/main/java/bridge/BridgeGame.java
@@ -10,25 +10,58 @@ import java.util.List;
  */
 public class BridgeGame {
     OutputView printMessage = new OutputView();
+
+    public static void main(String[] args) {
+        BridgeGame test = new BridgeGame();
+        test.play();
+    }
     public void play(){
         InputView inputView = new InputView();
         BridgeMaker bridgeMaker = new BridgeMaker(new BridgeRandomNumberGenerator());
-        printMessage.printAskGameRestart();
+        printMessage.printStart();
         printMessage.printAskBridgeLength();
         List<String> bridge = bridgeMaker.makeBridge(inputView.readBridgeSize(Console.readLine()));
-
+        System.out.println(bridge);
+        int [] upCase = new int[bridge.size()];
+        int [] downCase = new int[bridge.size()];
+        printMessage.printAskMovingButton();
+        int length = 1;
+        int gameCount = 1;
+        for (int i = 0; i < bridge.size(); i++) {
+            String input = Console.readLine();
+            if(!move(bridge.get(i), input)){
+                if(input.equals("U"))
+                    upCase[i] += 2;
+                if(input.equals("D"))
+                    downCase[i] += 2;
+                printMessage.printMap(upCase, downCase, length);
+                gameCount++;
+                return;
+            }
+            if(bridge.get(i).equals("U")){
+                upCase[i]++;
+            }
+            if(bridge.get(i).equals("D")){
+                downCase[i]++;
+            }
+            printMessage.printMap(upCase, downCase, length);
+            length++;
+        }
+        printMessage.printMapResult();
+        printMessage.printMap(upCase, downCase, length - 1);
+        printMessage.printGameResult();
+        printMessage.printGameTries(gameCount);
     }
     /**
      * 사용자가 칸을 이동할 때 사용하는 메서드
      * <p>
      * 이동을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void move(List<String> bridge, String moveCommand) {
-        for (String answer : bridge) {
-            if(answer.equals(moveCommand)){
-
-            }
+    public boolean move(String answerBridge, String moveCommand) {
+        if(answerBridge.equals(moveCommand)){
+            return true;
         }
+        return false;
     }
 
     /**
--- src/main/java/bridge/OutputView.java
+++ src/main/java/bridge/OutputView.java
@@ -1,5 +1,7 @@
 package bridge;
 
+import java.util.List;
+
 /**
  * 사용자에게 게임 진행 상황과 결과를 출력하는 역할을 한다.
  */
@@ -10,15 +12,43 @@ public class OutputView {
      * <p>
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void printMap() {
+    public void printMap(int [] upCase, int [] downCase, int count){
+        System.out.printf("%s","[ ");
+        printBridge(upCase, count);
+        System.out.printf("%s", "]\n");
+        System.out.printf("%s", "[ ");
+        printBridge(downCase, count);
+        System.out.printf("%s", "]\n");
     }
-
+    public void printBridge(int [] bridgeCase, int count){
+        for(int i = 0; i < count; i++){
+            if(bridgeCase[i] == 1){
+                System.out.printf("%s", "O ");
+            }
+            if(bridgeCase[i] == 0){
+                System.out.printf("%s", "  ");
+            }
+            if(i < count - 1) {
+                System.out.printf("%s", "| ");
+            }
+            if(bridgeCase[i] == 2){
+                System.out.printf("%s", "X ");
+            }
+        }
+    }
     /**
      * 게임의 최종 결과를 정해진 형식에 맞춰 출력한다.
      * <p>
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void printResult() {
+    public void printMapResult() {
+        System.out.println("최종 게임 결과");
+    }
+    public void printGameResult(){
+        System.out.println("게임 성공 여부: 성공");
+    }
+    public void printGameTries(int gameCount){
+        System.out.println("총 시도한 횟수: " + gameCount);
     }
     public void printStart(){
         System.out.println("다리 건너기 게임을 시작합니다.");
