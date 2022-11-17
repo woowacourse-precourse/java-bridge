477a75f9e13e3418c799662de17c99d94306c694
woo-chang <clllickme@naver.com>
2022-11-17 21:03:37 +0900

    test(Direction): 방향과 관련된 생성 코드 테스트

--- src/test/java/bridge/model/DirectionTest.java
+++ src/test/java/bridge/model/DirectionTest.java
@@ -27,10 +27,36 @@ class DirectionTest {
         String direction = "D";
 
         //when
-        Direction up = Direction.DOWN;
+        Direction down = Direction.DOWN;
 
         //then
-        assertThat(up.getDirection()).isEqualTo(direction);
+        assertThat(down.getDirection()).isEqualTo(direction);
+    }
+
+    @DisplayName("0은 아래 방향을 생성하는 코드이다.")
+    @Test
+    void generateCodeDown() {
+        //given
+        int code = 0;
+
+        //when
+        Direction down = Direction.DOWN;
+
+        //then
+        assertThat(down.isMatchGenerateCode(code)).isTrue();
+    }
+
+    @DisplayName("1은 위 방향을 생성하는 코드이다.")
+    @Test
+    void generateCodeUp() {
+        //given
+        int code = 1;
+
+        //when
+        Direction up = Direction.UP;
+
+        //then
+        assertThat(up.isMatchGenerateCode(code)).isTrue();
     }
 
 }
