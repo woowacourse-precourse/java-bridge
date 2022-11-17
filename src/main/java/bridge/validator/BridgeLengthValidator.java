3a36bb5fadbc2f4711912b65bef1bb8ef59c055f
KyungKyu Park <rudrb0505@gmail.com>
2022-11-17 14:34:52 +0900

    test(BridgeLengthValidatorTest): 다리 길이가 유효한 값인지 확인하는 테스트

---
+++ src/main/java/bridge/validator/BridgeLengthValidator.java
@@ -0,0 +1,11 @@
+package bridge.validator;
+
+public class BridgeLengthValidator {
+	public static void validateNumber(String input) {
+		throw new IllegalArgumentException();
+	}
+
+	public static void validateRange(String input) {
+		throw new IllegalArgumentException();
+	}
+}
