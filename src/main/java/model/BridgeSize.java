4a53cd1ee844d33e105660f27a1c4b2a8d96e937
dclxxi <dclxxi@kakao.com>
2022-11-17 17:06:53 +0900

    feat: 다리의 길이가 3~20 범위가 아닌 경우 예외 처리 추가

---
+++ src/main/java/model/BridgeSize.java
@@ -0,0 +1,21 @@
+package model;
+
+import static constant.Config.ERROR;
+
+public class BridgeSize {
+    private static final int MIN_RANGE = 3;
+    private static final int MAX_RANGE = 20;
+    private static final String RANGE_ERROR = ERROR + String.format("다리 길이는 %d부터 %d 사이여야 합니다.", MIN_RANGE, MAX_RANGE);
+    private final int size;
+
+    public BridgeSize(int size) {
+        validateRange(size);
+        this.size = size;
+    }
+
+    private void validateRange(int size) {
+        if (size < MIN_RANGE || size > MAX_RANGE) {
+            throw new IllegalArgumentException(RANGE_ERROR);
+        }
+    }
+}
