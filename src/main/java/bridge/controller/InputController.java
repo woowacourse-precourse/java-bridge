8678964b53914ed976c07592ad775c44c18b7787
SangWoo Yun <64531982+Awarduuu@users.noreply.github.com>
2022-11-18 01:33:01 +0900

    feat(InputController): 이동 입력과 이동 모델 연결

--- src/main/java/bridge/controller/InputController.java
+++ src/main/java/bridge/controller/InputController.java
@@ -5,12 +5,17 @@ import bridge.BridgeRandomNumberGenerator;
 import bridge.domain.Bridge;
 import bridge.domain.BridgeGame;
 import bridge.domain.BridgeMaker;
+import bridge.domain.Move;
 import bridge.view.InputView;
 
 public class InputController {
 
     public static Bridge inputBridgeSize(){
         return new Bridge(InputView.readBridgeSize());
+    }
+
+    public static Move inputMoving(){
+        return new Move(InputView.readMoving());
     }
 
 }
