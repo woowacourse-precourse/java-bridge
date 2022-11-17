f55430160a3d3d477d09d52eee4eff9bfd297785
minju412 <ln8847@ajou.ac.kr>
2022-11-17 10:09:58 +0900

    test(BridgeMakerTest): add test
    - 다리의 길이는 입력받은 값으로 설정합니다.
    
    - 다리는 U 와 D 로만 구성되어 있습니다.

--- src/test/java/bridge/BridgeMakerTest.java
+++ src/test/java/bridge/BridgeMakerTest.java
@@ -1,18 +1,35 @@
-package bridge.domain;
+package bridge;
 
-import bridge.BridgeMaker;
-import bridge.BridgeNumberGenerator;
-import bridge.BridgeRandomNumberGenerator;
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class BridgeMakerTest {
 
-    @DisplayName("랜덤으로 다리를 생성한다.")
-    @Test
-    void makeBridge() {
+    private static final String UP = "U";
+    private static final String DOWN = "D";
+    private BridgeMaker bridgeMaker;
+
+    @BeforeEach
+    void setUp() {
         BridgeNumberGenerator bridgeNumberGenerator = new BridgeRandomNumberGenerator();
-        BridgeMaker bridgeMaker = new BridgeMaker(bridgeNumberGenerator);
-        System.out.println(bridgeMaker.makeBridge(5).toString());
+        bridgeMaker = new BridgeMaker(bridgeNumberGenerator);
+    }
+
+    @DisplayName("다리의 길이는 입력받은 값으로 설정합니다.")
+    @Test
+    void makeBridge_size() {
+        assertThat(bridgeMaker.makeBridge(3).size())
+                .isEqualTo(3);
+    }
+
+    @DisplayName("다리는 U 와 D 로만 구성되어 있습니다.")
+    @Test
+    void makeBridge_component() {
+        assertThat(bridgeMaker.makeBridge(3).stream()
+                .allMatch(element -> element.equals(UP) || element.equals(DOWN)))
+                .isTrue();
     }
 }
