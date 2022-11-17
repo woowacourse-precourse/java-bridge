eeb2833a51c2918623272828c62c1a8a3ac9a0d1
K-Diger <kitten.diger@gmail.com>
2022-11-18 02:48:35 +0900

    feat : 널 수 있는 다리를 나타낼 번호를 무작위로 생성한다. 단, 다리의 길이는 사용자에게 입력받은 값을 활용한다.

---
+++ src/main/java/bridge/BridgeMaker.java
@@ -0,0 +1,26 @@
+package bridge;
+
+import bridge.util.numbergenerator.BridgeNumberGenerator;
+import bridge.util.numberhandler.RandomNumberHandler;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class BridgeMaker {
+
+    private final BridgeNumberGenerator bridgeNumberGenerator;
+    private final RandomNumberHandler randomNumberHandler;
+
+    public BridgeMaker(BridgeNumberGenerator bridgeNumberGenerator, RandomNumberHandler randomNumberHandler) {
+        this.bridgeNumberGenerator = bridgeNumberGenerator;
+        this.randomNumberHandler = randomNumberHandler;
+    }
+
+    public List<String> makeBridge(int size) {
+        List<String> bridge = new ArrayList<>();
+        for (int i = 0; i < size; i++) {
+            bridge.add(randomNumberHandler.convertRandomNumber(bridgeNumberGenerator.generate()));
+        }
+        return bridge;
+    }
+}
