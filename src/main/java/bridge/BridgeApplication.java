908acdf4bde0f61fd16cef1f22b2c5b455948b0b
KimTaeKang57 <zizoen123@gmail.com>
2022-11-17 22:06:15 +0900

    feat: BridgeApplication - 다리 건너기 게임 시작 및 최종 게임 결과 출력

--- src/main/java/bridge/BridgeApplication.java
+++ src/main/java/bridge/BridgeApplication.java
@@ -3,7 +3,7 @@ package bridge;
 import java.util.List;
 
 public class BridgeApplication {
-    void run(){
+    void run() {
         // 다리 건너기 게임 시작
         System.out.println("다리 건너기 게임을 시작합니다.");
 
@@ -19,26 +19,28 @@ public class BridgeApplication {
         System.out.println(bridge.toString());
 
         OutputView outputView = new OutputView();
-        BridgeGame bridgeGame = new BridgeGame();
+        BridgeGame bridgeGame = new BridgeGame(bridge, 1);
 
-        while (true) {
-            for (int idx = 0; idx < size; idx++) {
-                System.out.println("이동할 칸을 선택해주세요. (위: U, 아래: D)");
-                String moving = inputView.readMoving();
-
-                if (bridgeGame.move(bridge, moving, idx)) {
-                    outputView.printMap();
-                    System.out.println("성공 " + idx);
-                } else {
-                    outputView.printMap();
-                    System.out.println("실패");
-                    if (bridgeGame.retry(inputView)) {
-                        return;
-                    } else {
-                        break;
-                    }
-                }
+        // 다리건너기 시작
+        for (int idx = 0; idx < size; idx++) {
+            System.out.println("이동할 칸을 선택해주세요. (위: U, 아래: D)");
+            String moving = inputView.readMoving();
+            bridgeGame.move(moving, idx);
+            if (bridgeGame.isMove()) {
+                outputView.printMap(bridgeGame);
+                continue;
             }
+            outputView.printMap(bridgeGame);
+            if (!(bridgeGame.retry(inputView))) {
+                break;
+            }
+            idx = -1;
+            bridgeGame.plusCnt();
         }
+
+        System.out.println("최종 게임 결과");
+        outputView.printMap(bridgeGame);
+
+        outputView.printResult(bridgeGame);
     }
 }
