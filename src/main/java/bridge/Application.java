938b3fb3d3446ae192631cba45a8f95fc40cb0b4
EUNJEONGMUN <munej26@gmail.com>
2022-11-18 03:08:07 +0900

    feat: 게임 시작 문구 출력 및 게임 시작 코드 구현

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -1,8 +1,23 @@
 package bridge;
 
+import bridge.constant.Messages;
+import bridge.service.BridgeMaker;
+import bridge.service.BridgeNumberGenerator;
+import bridge.service.BridgeRandomNumberGenerator;
+import bridge.view.InputView;
+import bridge.view.OutputView;
+
 public class Application {
 
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        try {
+            System.out.println(Messages.START_MESSAGE);
+            BridgeNumberGenerator bridgeNumberGenerator = new BridgeRandomNumberGenerator();
+            BridgeGame bridgeGame = new BridgeGame(
+                    new BridgeMaker(bridgeNumberGenerator), new InputView(), new OutputView());
+            bridgeGame.startGame();
+        } catch (RuntimeException e) {
+            System.out.println(e.getMessage());
+        }
     }
 }
