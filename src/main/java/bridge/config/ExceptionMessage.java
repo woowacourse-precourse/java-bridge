b5cc9e33cee3a23870a298a32d62096d0a9fa991
kimminsu <xjfcnfw3@naver.com>
2022-11-17 21:29:27 +0900

    feat: 플레이어 이동을 U과 D만 받도록 검증

--- src/main/java/bridge/config/ExceptionMessage.java
+++ src/main/java/bridge/config/ExceptionMessage.java
@@ -6,7 +6,8 @@ public enum ExceptionMessage {
     NUMBER_OUT_OF_RANGE("다리의 길이는 3 ~ 20이어야 합니다."),
     NOT_ALL_UPPERCASE("오직 대문자만 입력 받을 수 있습니다."),
     WRONG_OPTION_LENGTH("1글자만 입력해야 합니다."),
-    WRONG_GAME_OPTION("R(게임 재시작)과 Q(게임 종료)만 입력할 수 있습니다.");
+    WRONG_GAME_OPTION("R(게임 재시작)과 Q(게임 종료)만 입력할 수 있습니다."),
+    WRONG_MOVE_OPTION("오직 U(위 칸)과 D(아래 칸)을 입력할 수 있습니다.");
 
     private final String message;
 
--- src/main/java/bridge/option/Move.java
+++ src/main/java/bridge/option/Move.java
@@ -1,13 +1,26 @@
 package bridge.option;
 
-import static bridge.option.Option.validate;
+import static bridge.config.ExceptionMessage.WRONG_MOVE_OPTION;
+
+import bridge.player.PlayerMove;
 
 public class Move {
 
     private final String move;
 
     public Move(String move) {
+        Option.validate(move);
         validate(move);
         this.move = move;
+    }
+
+    public static void validate(String move) {
+        validateMoveAvailable(move);
+    }
+
+    public static void validateMoveAvailable(String move) {
+        if (!PlayerMove.isInPlayerMove(move)) {
+            throw new IllegalArgumentException(WRONG_MOVE_OPTION.toString());
+        }
     }
 }
