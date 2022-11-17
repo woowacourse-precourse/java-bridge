0738780a3e7ccd62f6484c9aee960412d08ae4a0
Soseung Soo <sss9073@naver.com>
2022-11-17 13:35:41 +0900

    feat(UpAndDown): (0,1)을 ("U","D")로 반환하는 기능 구현

--- src/main/java/bridge/UpAndDown.java
+++ src/main/java/bridge/UpAndDown.java
@@ -5,10 +5,17 @@ public enum UpAndDown {
     DOWN("D", 0);
 
     private String direction;
-    private int InputDirectionNumber;
+    private int inputDirectionNumber;
 
     UpAndDown(String direction, int inputDirectionNumber) {
         this.direction = direction;
-        this.InputDirectionNumber = inputDirectionNumber;
+        this.inputDirectionNumber = inputDirectionNumber;
+    }
+
+    public String convertNumberToUpAndDown(int number){
+        if(number == this.inputDirectionNumber){
+            return this.direction;
+        }
+        return null;
     }
 }
