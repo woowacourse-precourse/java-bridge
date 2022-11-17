c3bec481ab42004da4a001a02034a77569fff5ac
ddingmin <ddingminyt@gmail.com>
2022-11-17 17:36:38 +0900

    refactor(java-bridge): 도메인과 ui 분리

--- src/main/java/bridge/ui/InputView.java
+++ src/main/java/bridge/ui/InputView.java
@@ -1,13 +1,9 @@
-package bridge;
+package bridge.ui;
 
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
  */
 public class InputView {
-
-    /**
-     * 다리의 길이를 입력받는다.
-     */
     public int readBridgeSize() {
         return 0;
     }
