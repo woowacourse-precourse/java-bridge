1e91b9f3a7d3ed806817e8c9d4fb70ce75ff5d83
김경훈 <54941130+ouvir@users.noreply.github.com>
2022-11-18 00:21:33 +0900

    test: add operation test of make bridge size
    test make bridge size

--- src/test/java/bridge/MakeBridgeTest.java
+++ src/test/java/bridge/MakeBridgeTest.java
@@ -22,9 +22,18 @@ public class MakeBridgeTest extends NsTest {
         List<String> bridge = bridgeMaker.makeBridge(3);
         assertThat(bridge).containsExactly("U", "D", "D");
     }
+
+    @Test
+    @DisplayName("입력에 따른 다리 크기 테스트")
+    void makeBridgeSizeTest() {
+        BridgeMaker newBridge = new BridgeMaker(new BridgeRandomNumberGenerator());
+        List<String> bridge = newBridge.makeBridge(15);
+        assertThat(bridge.size()).isEqualTo(15);
+    }
+
     @DisplayName("다리 크기 입력에 대한 예외 처리 테스트")
     @Nested
-    class makeBridgeExceptionTest{
+    class makeBridgeExceptionTest {
         @Test
         @DisplayName("입력이 숫자가 아닌 경우에 대한 예외 처리")
         void checkNotNumberTest() {
@@ -33,9 +42,10 @@ public class MakeBridgeTest extends NsTest {
                 assertThat(output()).contains(ERROR_MESSAGE, "다리 길이는 숫자를 입력해야 합니다.");
             });
         }
+
         @Test
         @DisplayName("입력이 다리 크기 범위를 벗어났을 때 예외 처리")
-        void checkOutOfRangeTest(){
+        void checkOutOfRangeTest() {
             assertSimpleTest(() -> {
                 runException("2");
                 assertThat(output()).contains(ERROR_MESSAGE, "다리 길이는 3부터 20 사이의 숫자여야 합니다.");
