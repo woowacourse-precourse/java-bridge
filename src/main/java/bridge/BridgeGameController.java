6d277e416add377bbdb437d81df030b2cda3669a
cpot5620 <=cpot5620@naver.com>
2022-11-18 02:19:09 +0900

    feat(Controller): create method to make bridge about input

--- src/main/java/bridge/BridgeGameController.java
+++ src/main/java/bridge/BridgeGameController.java
@@ -1,2 +1,20 @@
-package bridge;public class BridgeGameController {
+package bridge;
+
+public class BridgeGameController {
+    private BridgeGame bridgeGame;
+    private boolean isContinue;
+    private int countOfAttempt;
+
+    public BridgeGameController() {
+        isContinue = true;
+        countOfAttempt = 0;
+    }
+
+    private void makeBridge(int size) {
+        BridgeNumberGenerator generator = new BridgeRandomNumberGenerator();
+        BridgeMaker bridgeMaker = new BridgeMaker(generator);
+        Bridge bridge = new Bridge(bridgeMaker.makeBridge(size));
+
+        this.bridgeGame = new BridgeGame(bridge);
+    }
 }
