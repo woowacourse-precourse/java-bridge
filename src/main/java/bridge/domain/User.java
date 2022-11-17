09cc7959096fff3a64f877d5b167e86c0e72f22b
jjmin9797 <jjmin9797@naver.com>
2022-11-17 19:46:14 +0900

    feat(User): User 클래스 Getter/Setter 생성

--- src/main/java/bridge/domain/User.java
+++ src/main/java/bridge/domain/User.java
@@ -11,4 +11,20 @@ public class User {
     public void User(List<String> bridge) {
         this.bridge = bridge;
     }
+
+    public List<String> getBridge() {
+        return bridge;
+    }
+
+    public void setBridge(List<String> bridge) {
+        this.bridge = bridge;
+    }
+
+    public ArrayList<String> getUserBridge() {
+        return userBridge;
+    }
+
+    public void setUserBridge(ArrayList<String> userBridge) {
+        this.userBridge = userBridge;
+    }
 }
