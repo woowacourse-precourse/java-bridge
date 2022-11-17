924a83343b4c28cad46a3eb0205c916990b6abd3
KangSungBum <peter9706@naver.com>
2022-11-17 17:47:29 +0900

    feat(OutputView.java): add new print methods
    게임 기능 요구사항에 맞는 출력 함수 추가
    - printGameStartMessage: 게임 시작 문구 출력
    - printBridgeSizeRequestMessage: 다리 길이 사용자 입력 요청 문구 출력
    - printMovementRequestMessage: 위아래 움직임 사용자 입력 요청 문구 출력
    - printExitCodeRequestMessage: 게임 재시도 또는 종료 여부 사용자 입력 요청 문구 출력

--- src/main/java/view/OutputView.java
+++ src/main/java/view/OutputView.java
@@ -20,4 +20,21 @@ public class OutputView {
      */
     public void printResult() {
     }
+
+    public void printGameStartMessage() {
+        System.out.println("다리 건너기 게임을 시작합니다.");
+    }
+
+    public void printBridgeSizeRequestMessage() {
+        System.out.println("다리의 길이를 입력해주세요.");
+    }
+
+    public void printMovementRequestMessage() {
+        System.out.println("이동할 칸을 선택해주세요. (위: U, 아래: D)");
+    }
+
+    public void printExitCodeRequestMessage() {
+        System.out.println("게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)");
+    }
+
 }
