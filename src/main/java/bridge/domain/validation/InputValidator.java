8fb2a611a8b6a729bf5267e1822c9219073d3378
SeolYoungKim <nasur4da@gmail.com>
2022-11-17 19:30:07 +0900

    refactor(InputValidator): InputValidator의 단위 테스트 작성
    InputValidator의 예외 상황에 대한 단위 테스트를 작성하였습니다.

--- src/main/java/bridge/domain/validation/InputValidator.java
+++ src/main/java/bridge/domain/validation/InputValidator.java
@@ -7,13 +7,18 @@ import static bridge.domain.constants.MoveCommands.MOVE_UP_COMMAND;
 
 public class InputValidator {
 
+    static final String INPUT_ONLY_NUMBERS = "숫자만 입력해 주세요.";
+    static final String NOT_VALID_RANGE_OF_BRIDGE_SIZE = "다리의 길이는 3 이상 20 이하여야 합니다.";
+    static final String NOT_VALID_MOVE_COMMANDS = "U 또는 D만 입력해 주세요. (U:위, D:아래)";
+    static final String NOT_VALID_GAME_COMMANDS = "R 또는 Q만 입력해 주세요. (R:재시작, Q:종료)";
+
     private InputValidator() {
     }
 
     public static void validateThisIsNumber(String inputValue) {
         final String REGEX_FOR_NUMBER = "^\\d+$";
         if (!inputValue.matches(REGEX_FOR_NUMBER)) {
-            throw new IllegalArgumentException("숫자만 입력해 주세요.");
+            throw new IllegalArgumentException(INPUT_ONLY_NUMBERS);
         }
     }
 
@@ -21,19 +26,19 @@ public class InputValidator {
         final int MIN_SIZE = 3;
         final int MAX_SIZE = 20;
         if (size < MIN_SIZE || MAX_SIZE < size) {
-            throw new IllegalArgumentException("다리의 길이는 3 이상 20 이하여야 합니다.");
+            throw new IllegalArgumentException(NOT_VALID_RANGE_OF_BRIDGE_SIZE);
         }
     }
 
     public static void validateCorrectValueForMoving(String inputValue) {
         if (!inputValue.equals(MOVE_UP_COMMAND) && !inputValue.equals(MOVE_DOWN_COMMAND)) {
-            throw new IllegalArgumentException("U 또는 D만 입력해 주세요. (U:위, D:아래)");
+            throw new IllegalArgumentException(NOT_VALID_MOVE_COMMANDS);
         }
     }
 
     public static void validateCorrectValueForGameCommand(String inputValue) {
         if (!inputValue.equals(RESTART_GAME_COMMAND) && !inputValue.equals(QUIT_GAME_COMMAND)) {
-            throw new IllegalArgumentException("R 또는 Q만 입력해 주세요. (R:재시작, Q:종료)");
+            throw new IllegalArgumentException(NOT_VALID_GAME_COMMANDS);
         }
     }
 }
---
+++ src/test/java/bridge/domain/validation/InputValidatorTest.java
@@ -0,0 +1,45 @@
+package bridge.domain.validation;
+
+import static bridge.domain.validation.InputValidator.INPUT_ONLY_NUMBERS;
+import static bridge.domain.validation.InputValidator.NOT_VALID_GAME_COMMANDS;
+import static bridge.domain.validation.InputValidator.NOT_VALID_MOVE_COMMANDS;
+import static bridge.domain.validation.InputValidator.NOT_VALID_RANGE_OF_BRIDGE_SIZE;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+class InputValidatorTest {
+
+    @ParameterizedTest(name = "숫자가 아닌 값을 입력하면 예외가 발생한다. 입력: {0}")
+    @ValueSource(strings = {"하하", "hoho", " ", "1234a"})
+    void validateThisIsNumber(String inputValue) {
+        assertThatThrownBy(() -> InputValidator.validateThisIsNumber(inputValue))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(INPUT_ONLY_NUMBERS);
+    }
+
+    @ParameterizedTest(name = "다리 길이 범위를 벗어나는 값을 입력하면 예외가 발생한다. 입력: {0}")
+    @ValueSource(ints = {0, 2, 25, 30})
+    void validateBridgeSize(int inputValue) {
+        assertThatThrownBy(() -> InputValidator.validateBridgeSize(inputValue))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(NOT_VALID_RANGE_OF_BRIDGE_SIZE);
+    }
+
+    @ParameterizedTest(name = "U, D가 아닌 값을 입력하면 예외가 발생한다. 입력: {0}")
+    @ValueSource(strings = {"R", "Q", "ddd", "1"})
+    void validateCorrectValueForMoving(String inputValue) {
+        assertThatThrownBy(() -> InputValidator.validateCorrectValueForMoving(inputValue))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(NOT_VALID_MOVE_COMMANDS);
+    }
+
+    @ParameterizedTest(name = "R, Q가 아닌 값을 입력하면 예외가 발생한다. 입력: {0}")
+    @ValueSource(strings = {"U", "D", "ddd", "1"})
+    void validateCorrectValueForGameCommand(String inputValue) {
+        assertThatThrownBy(() -> InputValidator.validateCorrectValueForGameCommand(inputValue))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(NOT_VALID_GAME_COMMANDS);
+    }
+}
