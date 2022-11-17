c2465feb387e6db6fccaec7a1258df31f7adcca3
Jeangyu <gjwlsrb1700@naver.com>
2022-11-17 18:13:26 +0900

    [FEAT: bridge] 다리 건너는 상태를 출력하는 메서드 구현

--- src/main/java/bridge/OutputView.java
+++ src/main/java/bridge/OutputView.java
@@ -1,5 +1,8 @@
 package bridge;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * 사용자에게 게임 진행 상황과 결과를 출력하는 역할을 한다.
  */
@@ -10,7 +13,22 @@ public class OutputView {
      * <p>
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void printMap() {
+    public void printMap(List<Choice> inputs) {
+        printBridge(inputs, "U");
+        printBridge(inputs, "D");
+    }
+
+    public void printBridge(List<Choice> inputs, String upDown){
+        String result = "[";
+
+        for (Choice input: inputs){
+            String tmp = "   |";
+            if (input.getInput() == upDown){
+                tmp = " " + input.getInput() + " |";
+            }
+            result+=tmp;
+        }
+        System.out.println(result.subSequence(0, result.length()-2)+"]");
     }
 
     /**
@@ -19,5 +37,6 @@ public class OutputView {
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
     public void printResult() {
+
     }
 }
