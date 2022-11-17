1ab8360463bb12093334d190bd704eac9b874413
dongjun0128 <70315475+dongjun0128@users.noreply.github.com>
2022-11-17 17:14:36 +0900

    feat : bridge를 생성하는 기능 추가

--- src/main/java/bridge/BridgeMaker.java
+++ src/main/java/bridge/BridgeMaker.java
@@ -1,5 +1,6 @@
 package bridge;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /*
@@ -14,6 +15,8 @@ import java.util.List;
 public class BridgeMaker {
 
     private final BridgeNumberGenerator bridgeNumberGenerator;
+    private final String UPPER_BRIDGE = "U";
+    private final String LOWER_BRIDGE = "D";
 
     public BridgeMaker(BridgeNumberGenerator bridgeNumberGenerator) {
         this.bridgeNumberGenerator = bridgeNumberGenerator;
@@ -24,6 +27,17 @@ public class BridgeMaker {
      * @return 입력받은 길이에 해당하는 다리 모양. 위 칸이면 "U", 아래 칸이면 "D"로 표현해야 한다.
      */
     public List<String> makeBridge(int size) {
-        return null;
+        List<String> bridge = new ArrayList<>();
+
+        for(int numberOfSize = 0 ; numberOfSize < size ; numberOfSize++){
+            bridge.add(convertToBridge(bridgeNumberGenerator.generate()));
+        }
+
+        return bridge;
+    }
+
+    public String convertToBridge(int randomNumber){
+        if(randomNumber == 1) return UPPER_BRIDGE;
+        return LOWER_BRIDGE;
     }
 }
