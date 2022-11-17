03164e4dcbc21f21882767ae73c3f5c3e39d6e29
nylonmusk <106235834+nylonmusk@users.noreply.github.com>
2022-11-17 23:25:18 +0900

    feat(BridgeMaker): 랜덤으로 생성된 값을 bridge에 "U" 또는 "D"로 저장 구현

--- src/main/java/bridge/BridgeMaker.java
+++ src/main/java/bridge/BridgeMaker.java
@@ -1,12 +1,13 @@
 package bridge;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 /**
  * 다리의 길이를 입력 받아서 다리를 생성해주는 역할을 한다.
  */
 public class BridgeMaker {
-
     private final BridgeNumberGenerator bridgeNumberGenerator;
 
     public BridgeMaker(BridgeNumberGenerator bridgeNumberGenerator) {
@@ -18,6 +19,22 @@ public class BridgeMaker {
      * @return 입력받은 길이에 해당하는 다리 모양. 위 칸이면 "U", 아래 칸이면 "D"로 표현해야 한다.
      */
     public List<String> makeBridge(int size) {
-        return null;
+
+        List<Integer> bridgeWithNumber = new ArrayList<>();
+        List<String> bridgeWithAlphabet = new ArrayList<>();
+
+        for (int i = 0; i < size; i++) {
+            bridgeWithNumber.add(bridgeNumberGenerator.generate());
+        }
+
+        for (int i = 0; i < size; i++) {
+            if (bridgeWithNumber.get(i) == 0) {
+                bridgeWithAlphabet.add("D");
+            }
+            if (bridgeWithNumber.get(i) == 1) {
+                bridgeWithAlphabet.add("U");
+            }
+        }
+        return Collections.unmodifiableList(bridgeWithAlphabet);
     }
 }
