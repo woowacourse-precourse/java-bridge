48c944498e67460585ae4219424f924375edf208
seokjin8678 <rnrn8678@naver.com>
2022-11-17 19:41:06 +0900

    refactor: User 클래스 movedPosition 자료형 List로 변경, 불필요한 메서드 제거

--- src/main/java/bridge/domain/User.java
+++ src/main/java/bridge/domain/User.java
@@ -1,13 +1,13 @@
 package bridge.domain;
 
+import java.util.ArrayList;
 import java.util.List;
-import java.util.Stack;
 
 public class User {
-    private final Stack<UpDownFlag> movedPosition;
+    private final List<UpDownFlag> movedPosition;
 
     public User() {
-        movedPosition = new Stack<>();
+        movedPosition = new ArrayList<>();
     }
 
     public void move(UpDownFlag flag) {
@@ -18,23 +18,7 @@ public class User {
         return movedPosition.size();
     }
 
-    public int getCurrentIndex() {
-        validateEmpty();
-        return movedPosition.size() - 1;
-    }
-
-    private void validateEmpty() {
-        if (movedPosition.isEmpty()) {
-            throw new IllegalStateException("[ERROR] 사용자가 이동한 곳이 없습니다.");
-        }
-    }
-
     public List<UpDownFlag> getMovedPosition() {
         return List.copyOf(movedPosition);
-    }
-
-    public UpDownFlag getCurrentUpDown() {
-        validateEmpty();
-        return movedPosition.peek();
     }
 }
