00d2a0a705bc05e8f521867e30eb81e1866d59d0
gomljo <gomljo0307@naver.com>
2022-11-17 19:17:21 +0900

    refactor(BridgeMaker): 정의된 다리 상태 관계를 활용해 다리를 생성하는 코드 수정

--- src/main/java/bridge/BridgeMaker.java
+++ src/main/java/bridge/BridgeMaker.java
@@ -1,7 +1,12 @@
 package bridge;
 
+import bridge.constant.BridgeState;
+
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 /**
  * 다리의 길이를 입력 받아서 다리를 생성해주는 역할을 한다.
@@ -15,24 +20,16 @@ public class BridgeMaker {
     }
 
     /**
-     * @param size 다리의 길이
+     * @param bridgeLength 다리의 길이
      * @return 입력받은 길이에 해당하는 다리 모양. 위 칸이면 "U", 아래 칸이면 "D"로 표현해야 한다.
      */
 
-    public List<String> makeBridge(int size) {
-        int bridgeLength=0;
-        int bridgeStateNumber;
-        String bridgeState;
-        List<String> bridge=new ArrayList<>();
-        while(bridgeLength<size) {
-            bridgeStateNumber=bridgeNumberGenerator.generate();
-            bridgeState="D";
-            if(bridgeStateNumber==1){
-                bridgeState="U";
-            }
-            bridge.add(bridgeState);
-            bridgeLength+=1;
-        }
-        return bridge;
+    public List<String> makeBridge(int bridgeLength) {
+        Stream<String> bridge = Stream.generate(bridgeNumberGenerator::generate)
+                                        .limit(bridgeLength)
+                                        .filter(BridgeState::validateBridgeState)
+                                        .map(BridgeState::transform);
+
+        return bridge.collect(Collectors.toList());
     }
 }
