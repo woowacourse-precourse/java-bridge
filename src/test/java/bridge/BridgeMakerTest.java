14aaf2a1686d654dcfe6991721e1ea8f4269bc16
heesang <dlagmltkd12@naver.com>
2022-11-17 23:55:38 +0900

    feat: 다리의 크기가 주어지고, 0만 주어질 때 D로만 이루어진 다리를 반환하는 기능

--- src/test/java/bridge/BridgeMakerTest.java
+++ src/test/java/bridge/BridgeMakerTest.java
@@ -16,4 +16,12 @@ class BridgeMakerTest {
         List<String> bridge = bridgeMaker.makeBridge(6);
         assertThat(bridge).containsExactly("U", "U", "U", "U", "U", "U");
     }
+
+    @DisplayName("다리의 크기가 주어지고, 0만 주어질 때 D로만 이루어진 다리를 반환")
+    @Test
+    void makeBridgeGiveZero() {
+        BridgeMaker bridgeMaker = new BridgeMaker(() -> 0);
+        List<String> bridge = bridgeMaker.makeBridge(6);
+        assertThat(bridge).containsExactly("D", "D", "D", "D", "D", "D");
+    }
 }
