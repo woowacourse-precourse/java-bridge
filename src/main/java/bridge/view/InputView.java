dd585794eb66c9918b8fdbfe9aa128c70b8703ea
ddubidubap <park62000@gmail.com>
2022-11-18 00:59:56 +0900

    feat: 입력값을 숫자로 변형하기

--- src/main/java/bridge/view/InputView.java
+++ src/main/java/bridge/view/InputView.java
@@ -3,6 +3,8 @@ package bridge.view;
 import static camp.nextstep.edu.missionutils.Console.readLine;
 
 public class InputView {
+    static final String IS_NOT_NUMBER = "다리 길이는 숫자여야 합니다";
+
     public int readBridgeSize() {
         String bridgeSize = readLine();
         return 0;
@@ -14,5 +16,13 @@ public class InputView {
 
     public String readGameCommand() {
         return null;
+    }
+
+    private int strToInt(String input) {
+        try {
+            return Integer.parseInt(input);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException(IS_NOT_NUMBER);
+        }
     }
 }
