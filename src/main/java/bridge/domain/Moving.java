24cc3a59963b6839666d5e01c4448c86c77b08cd
EUNJEONGMUN <munej26@gmail.com>
2022-11-17 20:37:44 +0900

    feat: 이동할 칸을 가진 Moving 클래스를 생성하고 내부에서 값 예외처리

---
+++ src/main/java/bridge/domain/Moving.java
@@ -0,0 +1,19 @@
+package bridge.domain;
+
+import bridge.constant.Constant;
+import bridge.constant.ErrorMessage;
+
+public class Moving {
+    private final String moving;
+
+    public Moving(String moving) {
+        validMoving(moving);
+        this.moving = moving;
+    }
+
+    private void validMoving(String moving) {
+        if (!moving.equals(Constant.UP) && !moving.equals(Constant.DOWN)) {
+            throw new IllegalArgumentException(ErrorMessage.NOT_MOVING_VALUE.getMessage());
+        }
+    }
+}
