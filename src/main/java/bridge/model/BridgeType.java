9d34f589c7c507cb2428e9286c35e0f13b23bcb7
eora21 <eora21@naver.com>
2022-11-17 20:33:22 +0900

    feat: 🎸 추가 입력한 글자로 다리를 선택할 수 있는 메서드
    U나 D에 따라 어떤 다리에 있는 칸을 건널지 찾을 수 있게끔 메서드를 구성했다. 또한 지정된 글자가 아니라면 예외를 발생시키게끔
    했다.

--- src/main/java/bridge/model/BridgeType.java
+++ src/main/java/bridge/model/BridgeType.java
@@ -3,14 +3,20 @@ package bridge.model;
 import java.util.HashMap;
 import java.util.Map;
 
-public enum TypeOfBridge {
+public enum BridgeType {
     UP_BRIDGE("U", "위", 1),
     DOWN_BRIDGE("D", "아래",  0),
     ;
 
     private static final Map<Integer, String> bridgeNumberText = new HashMap<>() {{
-        for (TypeOfBridge rule : TypeOfBridge.values()) {
-            put(rule.bridgeNumber, rule.text);
+        for (BridgeType type : BridgeType.values()) {
+            put(type.bridgeNumber, type.text);
+        }
+    }};
+
+    private static final Map<String, BridgeType> textBridge = new HashMap<>() {{
+        for (BridgeType type : BridgeType.values()) {
+            put(type.text, type);
         }
     }};
 
@@ -20,7 +26,7 @@ public enum TypeOfBridge {
 
     private final int bridgeNumber;
 
-    TypeOfBridge(String text, String position, int bridgeNumber) {
+    BridgeType(String text, String position, int bridgeNumber) {
         this.text = text;
         this.position = position;
         this.bridgeNumber = bridgeNumber;
@@ -34,6 +40,16 @@ public enum TypeOfBridge {
         }
 
         return bridgeText;
+    }
+
+    public static BridgeType searchBridgeToText(String text) {
+        BridgeType bridge = textBridge.get(text);
+
+        if (bridge == null) {
+            throw new IllegalArgumentException("허용되지 않는 글자입니다. 일치하는 다리를 찾을 수 없습니다.");
+        }
+
+        return bridge;
     }
 
     public String getText() {
