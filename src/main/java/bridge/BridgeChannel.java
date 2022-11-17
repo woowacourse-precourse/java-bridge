bfd952451409cf75e78e55d0cefa0457a8b5e6e7
renechoi90@gmail.com <renechoi90@gmail.com>
2022-11-17 15:57:49 +0900

    feat : 다리 생성 기능 구현

---
+++ src/main/java/bridge/BridgeChannel.java
@@ -0,0 +1,37 @@
+package bridge;
+
+import java.util.Arrays;
+
+public enum BridgeChannel {
+
+	DOWN(0,"D"),
+	UP(1,"U");
+
+	private final int channelInNumber;
+	private final String channelInString;
+
+	BridgeChannel(int channelInNumber, String channelInString){
+		this.channelInNumber = channelInNumber;
+		this.channelInString = channelInString;
+	}
+
+	public int getChannelInNumber(){
+		return channelInNumber;
+	}
+
+	public String getChannelInString() {
+		return channelInString;
+	}
+
+	public static BridgeChannel of(int channelInNumber){
+		return Arrays.stream(BridgeChannel.values())
+				.filter(channelShape-> channelShape.channelInNumber == channelInNumber)	//TODO : ==처리를 다른 것으로 바꿀 수 있을지 고민
+				.findFirst()
+				.orElseThrow(IllegalArgumentException::new);
+	}
+
+	@Override
+	public String toString() {
+		return channelInString;
+	}
+}
