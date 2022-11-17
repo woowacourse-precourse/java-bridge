f6fc0bfbcfec6288260651f2cda8edeea30f7c3d
Voyager003 <85725033+Voyager003@users.noreply.github.com>
2022-11-18 00:33:06 +0900

    Feat: 추가 - readMoving 메서드 사용자가 이동할 칸(U, D)를 입력받는 기능

--- src/main/java/bridge/view/InputView.java
+++ src/main/java/bridge/view/InputView.java
@@ -24,7 +24,9 @@ public class InputView {
      * 사용자가 이동할 칸을 입력받는다.
      */
     public String readMoving() {
-        return null;
+        System.out.println(Message.CHOOSE_STEP.getMessage());
+        String step = Console.readLine();
+        return Exception.validateMoving(step);
     }
 
     /**
