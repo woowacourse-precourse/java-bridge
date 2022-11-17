d654537ea33f0401622347b1df5f52f79a74eef6
Eungyu-code <Agyu625@gmail.com>
2022-11-17 22:44:48 +0900

    feat(Application class): size, move, retry 변수 추가
    입력받는 값들을 저장하기 위한 변수

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -2,6 +2,12 @@ package bridge;
 
 public class Application {
 
+    private int size;
+
+    private String move;
+
+    private String retry;
+
     public static void main(String[] args) {
         // TODO: 프로그램 구현
         startBridgeGame();
