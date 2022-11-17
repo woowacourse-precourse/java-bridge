b0e4e59c484b72c5b0dbe7e2421a340450126748
JaeWon Choi <87649850+codesver@users.noreply.github.com>
2022-11-17 14:12:27 +0900

    feat(ErrorMessage): add error constant

--- src/main/java/bridge/constant/ErrorMessage.java
+++ src/main/java/bridge/constant/ErrorMessage.java
@@ -2,6 +2,7 @@ package bridge.constant;
 
 public enum ErrorMessage {
 
+    SHOULD_NOT_REACH_HERE("[ERROR] Should not reach here."),
     IS_NOT_POSITIVE_NUMBER("[ERROR] 다리 길이 입력값이 양의 정수가 아닙니다."),
     IS_OUT_OF_RANGE("[ERROR] 다리 길이 입력값이 범위 3 ~ 20을 벗어났습니다."),
     IS_WRONG_MOVEMENT("[ERROR] 움직임 입력이 U 혹은 D 가 아닙니다.");
