557faedee398ecff8b3ec718b82483d722a00f0d
cocomozila <joseyun94@naver.com>
2022-11-17 23:19:08 +0900

    feat(Setting): 게임 설정 클래스 추가

---
+++ src/main/java/bridge/setting/Setting.java
@@ -0,0 +1,11 @@
+package bridge.setting;
+
+public class Setting {
+
+    public static final String MOVE_UP_KEY = "U";
+    public static final String MOVE_DOWN_KEY = "K";
+    public static final String RETRY_BUTTON = "R";
+    public static final String EXIT_BUTTON = "Q";
+    public static final int MAX_BRIDGE_SIZE = 20;
+    public static final int MIN_BRIDGE_SIZE = 3;
+}
