1a69b815fe5f74faf8d0a0bdf43973fc31655030
goldcollar95 <yjs7298@naver.com>
2022-11-17 18:18:34 +0900

    생성할 다리 길이 구현

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -4,5 +4,10 @@ public class Application {
 
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+
+        InputView inputView = new InputView();
+        int input = inputView.readBridgeSize();
+                System.out.println(input);
+
     }
 }
--- src/main/java/bridge/InputView.java
+++ src/main/java/bridge/InputView.java
@@ -1,5 +1,10 @@
 package bridge;
 
+import java.util.List;
+
+
+import static camp.nextstep.edu.missionutils.Console.readLine;
+
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
  */
@@ -8,7 +13,18 @@ public class InputView {
     /**
      * 다리의 길이를 입력받는다.
      */
-    public int readBridgeSize() {
+
+    private static final int[] Min_Value_MaxVlaue = {};
+//    private static final int Max_Value = 20;
+
+
+    public static int readBridgeSize(){
+        String input = readLine();
+        int inputToint = Integer.parseInt(input);
+        if(inputToint > 2 && inputToint < 21){
+
+            return inputToint;
+        }
         return 0;
     }
 
