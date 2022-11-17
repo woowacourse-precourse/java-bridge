ab703434b450bbdef54ae9d18093238c9459ad26
jjongwa <troas96@naver.com>
2022-11-18 01:42:23 +0900

    feat: 게임 진행 후 성공 실패 여부 반환하는 기능

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -21,6 +21,23 @@ public class Application {
         return bridgeMaker.makeBridge(bridgeSize);
     }
 
+    static boolean gameProgress(InputView input, BridgeGame bridgeGame, int bridgeSize) {
+        for (int step = 0; step < bridgeSize; step++) {
+            String nextStep = readMoving(input);
+            boolean rightStep = bridgeGame.move(nextStep, step);
+            // 스텝 후 현재 상황 출력
+            if (rightStep && step == bridgeSize - 1) return true;    // 게임 종료로 분기
+            if (!rightStep) return false;   // 게임 재시도 여부로 분기
+            bridgeGame.nextStepRecord();
+        }
+        throw new IllegalArgumentException();
+    }
+
+    static String readMoving(InputView input) {
+        System.out.println("이동할 칸을 선택해주세요. (위: U, 아래: D)");
+        return input.readMoving();
+    }
+
     public static void main(String[] args) {
         // TODO: 프로그램 구현
         try {
@@ -28,6 +45,8 @@ public class Application {
             InputView input = new InputView();
             int bridgeSize = readBridgeSize(input);
             List<String> bridge = makeBridge(bridgeSize);
+            BridgeGame bridgeGame = new BridgeGame(bridge);
+            boolean gameResult = gameProgress(input, bridgeGame, bridgeSize);
         } catch (IllegalArgumentException exception) {
             System.out.print("[ERROR] ");
             System.out.println(exception.getMessage());
