8219bcde7e0b4b6b5704751a1af2f6dc9c655123
Eunkeee <eunkeeeee@gmail.com>
2022-11-17 15:10:04 +0900

    refactor: MVC 패턴 적용해 패키지 변경

--- src/main/java/bridge/view/InputView.java
+++ src/main/java/bridge/view/InputView.java
@@ -1,4 +1,6 @@
-package bridge;
+package bridge.view;
+
+import camp.nextstep.edu.missionutils.Console;
 
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
@@ -9,7 +11,11 @@ public class InputView {
      * 다리의 길이를 입력받는다.
      */
     public int readBridgeSize() {
-        return 0;
+        String input = Console.readLine();
+        // validation
+        
+        int bridgeSize = Integer.parseInt(input);
+        return bridgeSize;
     }
 
     /**
