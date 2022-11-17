e24e38fd0bf9d48b65c4ec917b7139f84075863d
whipbaek <jibaek0513@gmail.com>
2022-11-17 21:06:18 +0900

    feat(BridgeGame): 유저가 이동하면 일어나는 기능 추가 - move시, 유저 입력값을 기준으로 유저의 다리를 판단해서 만들어준다.

--- src/main/java/bridge/BridgeGame.java
+++ src/main/java/bridge/BridgeGame.java
@@ -1,16 +1,32 @@
 package bridge;
 
+import java.util.List;
+
 /**
  * 다리 건너기 게임을 관리하는 클래스
  */
 public class BridgeGame {
+
+    private List<String> answerBridge;
+    private User user;
+    private int bridgeLength;
+    private int gameTryNumber;
 
     /**
      * 사용자가 칸을 이동할 때 사용하는 메서드
      * <p>
      * 이동을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void move() {
+    public void move(String userInputValue) {
+        user.getUserBridge().add(makeField(userInputValue));
+    }
+
+    private Field makeField(String userInputValue){
+
+         if (answerBridge.get(user.getGameCount()) == userInputValue){
+             return new Field(userInputValue, true);
+        }
+         return new Field(userInputValue, false);
     }
 
     /**
