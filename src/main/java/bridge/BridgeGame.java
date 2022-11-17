5c6252315573ddd9a34c00cd5e8bd689584959f9
소대훈 <theogns0681@naver.com>
2022-11-17 10:59:06 +0900

    [refactor] 매개변수 순서 변경

--- src/main/java/bridge/BridgeGame.java
+++ src/main/java/bridge/BridgeGame.java
@@ -25,7 +25,7 @@ public class BridgeGame {
         input = inputView.readMoving();
         if (!Objects.equals(bridgeList.get(currentIndex), input)) {
             outputView.printMap(currentIndex, bridgeList, false, input);
-            retry(bridgeList, currentIndex);
+            retry(currentIndex, bridgeList);
             return;
         }
         outputView.printMap(currentIndex, bridgeList, true, input);
@@ -37,7 +37,7 @@ public class BridgeGame {
      * <p>
      * 재시작을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void retry(List<String> bridgeList, int currentIndex) {
+    public void retry(int currentIndex, List<String> bridgeList) {
         String regame = inputView.readGameCommand();
         if (Objects.equals(regame, "R")) {
             tryCount++;
