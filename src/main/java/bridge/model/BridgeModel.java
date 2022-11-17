dbb71f5b1c2975417e3393c8a7467cd8b09e8630
junhei Lee <jun2ee22@naver.com>
2022-11-17 19:02:34 +0900

    docs: 기능 목록 README.md 업데이트
    첫번째 업데이트, 패키지 변경

---
+++ src/main/java/bridge/model/BridgeModel.java
@@ -0,0 +1,11 @@
+package bridge.model;
+
+import java.util.List;
+
+public class BridgeModel {
+
+    private List<List<String>> currentMap;
+    private int numberOfAttempts = 1;
+    private String outcome;
+
+}
