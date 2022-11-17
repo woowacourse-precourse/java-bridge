5ed8f9465b141018e984e441b2220fc3141206e4
HaODay <ycis5013@naver.com>
2022-11-18 01:42:47 +0900

    feat(bridge): 게임 재시작하는 기능 구현

--- src/main/java/bridge/BridgeGame.java
+++ src/main/java/bridge/BridgeGame.java
@@ -57,6 +57,11 @@ public class BridgeGame {
      * 재시작을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
     public void retry() {
-
+        count++;
+        position = 0;
+        flag = true;
+        map[0] = new StringBuilder();
+        map[1]  = new StringBuilder();
+        Application.playGame(this);
     }
 }
