9cae77b0fd34f980210b413e2c7052803f1e7769
whdcks420 <whdcks420@gmail.com>
2022-11-17 19:40:55 +0900

    docs: 완료 체크

---
+++ src/main/java/bridge/utils/Convert.java
@@ -0,0 +1,18 @@
+package bridge.utils;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Convert {
+    public static final String ZERO = "0";
+    public static final String ONE = "1";
+    public static final String UP = "U";
+    public static final String DOWN = "D";
+
+    public static List<String> toUpAndDownList(List<String> bridge) {
+        return bridge.stream()
+                .map(value -> value.replace(ONE, UP))
+                .map(value -> value.replace(ZERO, DOWN))
+                .collect(Collectors.toList());
+    }
+}
