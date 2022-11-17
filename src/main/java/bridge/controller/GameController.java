9381a388d149bbec2a145483528b187cfb99fd5a
kevstevie <jianna442@gmail.com>
2022-11-17 18:43:30 +0900

    refactor: gameInit 메서드 수정

--- src/main/java/bridge/controller/GameController.java
+++ src/main/java/bridge/controller/GameController.java
@@ -1,13 +1,26 @@
 package bridge.controller;
 
+import bridge.model.BridgeGame;
+import bridge.model.BridgeMaker;
+import bridge.model.BridgeRandomNumberGenerator;
 import bridge.view.InputView;
 import bridge.view.OutputView;
 
+import java.util.List;
+
 public class GameController {
 
+    BridgeGame bridgeGame;
     InputView inputView = new InputView();
     OutputView outputView = new OutputView();
-    void gameInit() {
+    public void gameInit() {
         outputView.gameStart();
+        int size = inputView.readBridgeSize();
+        BridgeMaker bridgeMaker = new BridgeMaker(new BridgeRandomNumberGenerator());
+        List<String> bridge = bridgeMaker.makeBridge(size);
+        bridgeGame = new BridgeGame(bridge);
+    }
+
+    void movePlayer() {
     }
 }
