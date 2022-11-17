edd64a75be9ce52c719353e03681d7750859e09d
은지 <midzy0228@gmail.com>
2022-11-18 00:21:22 +0900

    test: 재시작/종료 여부 테스트 케이스 추가

---
+++ src/test/java/bridge/GameCommandTest.java
@@ -0,0 +1,15 @@
+package bridge;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+public class GameCommandTest {
+    @DisplayName("게임 재시작(R)/종료(Q) 이외의 입력시 예외가 발생한다.")
+    @Test
+    void createCommandByUnValid() {
+        assertThatThrownBy(() -> new GameCommand("A"))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+}
