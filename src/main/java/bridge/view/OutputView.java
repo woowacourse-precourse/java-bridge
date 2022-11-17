b5e592f79af280617c106cf374a54421e7d9650b
SH <gram753@naver.com>
2022-11-17 20:40:33 +0900

    feat(view): 결과를 출력하는 기능 구현

--- src/main/java/bridge/view/OutputView.java
+++ src/main/java/bridge/view/OutputView.java
@@ -11,21 +11,17 @@ public class OutputView {
 	private static final String WIN = "게임 성공 여부: 성공";
 	private static final String LOSE = "게임 성공 여부: 실패";
 	private static final String TOTAL_TRY = "총 시도한 횟수: ";
-	private int tries = 0;
 
-	public static void printMap(String upsideBridge) {
-		List<String> upsideCase = new ArrayList<>(List.of(upsideBridge.split("")));
-		int i = 0;
-		while (i < 3) {
-			upsideCase.remove(0);
-			i++;
-		}
-		System.out.println(OPEN_BRACKET + toString(upsideCase) + CLOSE_BRACKET);
+	public static void printMap(List<String> state) {
+		List<String> upsideState = new ArrayList<>(List.of(state.get(0).split("")));
+		List<String> downsideState = new ArrayList<>(List.of(state.get(1).split("")));
+		System.out.println(OPEN_BRACKET + toString(upsideState) + CLOSE_BRACKET);
+		System.out.println(OPEN_BRACKET + toString(downsideState) + CLOSE_BRACKET);
 	}
 
 	public static String toString(List<String> list) {
 		StringBuffer line = new StringBuffer();
-		for (int i = 0; i < list.size(); i++) {
+		for (int i = 3; i < list.size(); i++) {
 			line.append(list.get(i));
 		}
 		return line.toString();
@@ -36,15 +32,21 @@ public class OutputView {
 	 * <p>
 	 * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
 	 */
-	public static void printResult() {
+	public static void printResult(List<String> state, boolean win, int tryCount) {
 		System.out.println(RESULT);
+		printMap(state);
+		System.out.println();
+		if (win) {
+			System.out.println(WIN);
+			System.out.println(TOTAL_TRY + tryCount);
+		}
+		if (!win){
+			System.out.println(LOSE);
+			System.out.println(TOTAL_TRY + tryCount);
+		}
 	}
 
 	public static void printStartMessage() {
 		System.out.println(START_MESSAGE);
-	}
-
-	public void resetTries() {
-		this.tries = 0;
 	}
 }
