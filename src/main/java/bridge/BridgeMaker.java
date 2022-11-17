3a4faa664ddd9edaca1d14585f291e26fa547280
neppiness <0414kjh@naver.com>
2022-11-17 21:00:44 +0900

    feat: BridgeMaker 클래스 내 메소드 구현 (src/main/java/bridge)
    생성자
    - InputView의 readBridgeSize로 bridgeSize를 입력받은 후 makeBridge 메소드 호출
    makeBridge 메소드
    - 주어진 bridgeSize만큼 숫자를 임의로 생성함
    - bridge 배열을 구성한 후 반환

--- src/main/java/bridge/BridgeMaker.java
+++ src/main/java/bridge/BridgeMaker.java
@@ -1,5 +1,6 @@
 package bridge;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -11,6 +12,8 @@ public class BridgeMaker {
 
     public BridgeMaker(BridgeNumberGenerator bridgeNumberGenerator) {
         this.bridgeNumberGenerator = bridgeNumberGenerator;
+        int bridgeSize = new InputView().readBridgeSize();
+        makeBridge(bridgeSize);
     }
 
     /**
@@ -18,6 +21,12 @@ public class BridgeMaker {
      * @return 입력받은 길이에 해당하는 다리 모양. 위 칸이면 "U", 아래 칸이면 "D"로 표현해야 한다.
      */
     public List<String> makeBridge(int size) {
-        return null;
+        List<String> bridge = new ArrayList<>();
+        String[] partition = {"D", "U"};
+        for (int index = 0; index < size; index++) {
+            int generatedNumber = this.bridgeNumberGenerator.generate();
+            bridge.add(partition[generatedNumber]);
+        }
+        return bridge;
     }
 }
