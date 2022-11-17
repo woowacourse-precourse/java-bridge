3ae3585744781b799093ad1d89006f48385c3595
kissshot1104 <kissshot.heartunderblade1104@gmail.com>
2022-11-18 07:43:08 +0900

    Test(UtilTest) : 1, 0입력받고 U, D로 변환 기능 테스트 코드 추가
    public static String convertBridgeNumberGenerator(int number)
    메소드의 테스트 코드를 추가했습니다.

--- src/test/java/bridge/UtilTest.java
+++ src/test/java/bridge/UtilTest.java
@@ -8,7 +8,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 class UtilTest {
 
     @Test
-    @DisplayName("숫자인지 아닌지 검사합니다")
+    @DisplayName("숫자인지 아닌지 검사하는 테스트 코드")
     void isNumberTest(){
         boolean isTrue = Util.isNumber("1");
         assertThat(isTrue)
@@ -20,7 +20,7 @@ class UtilTest {
     }
 
     @Test
-    @DisplayName("문자인지 아닌지 검사합니다")
+    @DisplayName("문자인지 아닌지 검사하는 테스트 코드")
     void isLetterTest(){
         boolean isTrue = Util.isLetter("a");
         assertThat(isTrue)
@@ -32,7 +32,7 @@ class UtilTest {
     }
 
     @Test
-    @DisplayName("한 글자인지 아닌지 검사합니다")
+    @DisplayName("한 글자인지 아닌지 검사하는 테스트 코드")
     void isOneLetterTest(){
         boolean isTrue = Util.isOneLetter("a");
         assertThat(isTrue)
@@ -45,7 +45,7 @@ class UtilTest {
 
 
     @Test
-    @DisplayName("여러 글자인지 아닌지 검사합니다")
+    @DisplayName("여러 글자인지 아닌지 검사하는 테스트 코드")
     void isStringTest(){
         boolean isTrue = Util.isString("aa");
         assertThat(isTrue)
@@ -56,4 +56,14 @@ class UtilTest {
                 .isFalse();
     }
 
+    @Test
+    @DisplayName("1입력시 U, 0입력시 D 리턴 테스트 코드")
+    void convertBridgeNumberGeneratorTest(){
+        assertThat(Util.convertBridgeNumberGenerator(1))
+                .isEqualTo("U");
+
+        assertThat(Util.convertBridgeNumberGenerator(0))
+                .isEqualTo("D");
+    }
+    
 }
