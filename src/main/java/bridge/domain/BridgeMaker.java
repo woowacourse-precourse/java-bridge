4905bcadc0b06c542051199efa5e0575a0e96767
kdkdhoho <hkim4410@naver.com>
2022-11-17 16:03:59 +0900

    refactor: 파라미터에 final 키워드 추가

--- src/main/java/bridge/domain/BridgeMaker.java
+++ src/main/java/bridge/domain/BridgeMaker.java
@@ -20,8 +20,8 @@ public class BridgeMaker {
      * @param size 다리의 길이
      * @return 입력받은 길이에 해당하는 다리 모양. 위 칸이면 "U", 아래 칸이면 "D"로 표현해야 한다.
      */
-    public List<String> makeBridge(int size) {
-        List<String> bridge = new ArrayList<>(size);
+    public List<String> makeBridge(final int size) {
+        List<String> bridge = new ArrayList<>();
 
         for (int repeat = 0; repeat < size; repeat++) {
             int generatedNumber = bridgeNumberGenerator.generate();
--- src/main/java/bridge/util/ExceptionHandler.java
+++ src/main/java/bridge/util/ExceptionHandler.java
@@ -5,14 +5,14 @@ public class ExceptionHandler {
     static final int BRIDGE_SIZE_MIN = 3;
     static final int BRIDGE_SIZE_MAX = 20;
 
-    public static void checkBridgeSize(String input) throws IllegalArgumentException {
+    public static void checkBridgeSize(final String input) throws IllegalArgumentException {
         int size = Integer.parseInt(input);
         if (!isValidRange(size)) {
             throw new IllegalArgumentException();
         }
     }
 
-    private static boolean isValidRange(int size) {
+    private static boolean isValidRange(final int size) {
         return (BRIDGE_SIZE_MIN <= size && size <= BRIDGE_SIZE_MAX);
     }
 }
