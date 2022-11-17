c2cde108dc753f8f6be190a63b058eb1ede75da7
박준용 <jo101044@naver.com>
2022-11-17 17:13:47 +0900

    feat: 다리 생성 기능 추가

--- src/main/java/bridge/ExceptionMessage.java
+++ src/main/java/bridge/ExceptionMessage.java
@@ -1,7 +1,9 @@
 package bridge;
 
 public enum ExceptionMessage {
-    incorrectSize("[ERROR] 올바른 값을 입력해주세요");
+    incorrectSize("[ERROR] 올바른 값을 입력해주세요"),
+    bridgeNotEqualsSize("[ERROR] 다리의 길이가 입력값과 다릅니다."),
+    bridgeNotUorD("[ERROR] 다리가 잘못 생성되었습니다.");
 
     private final String message;
 
--- src/main/java/bridge/Validator.java
+++ src/main/java/bridge/Validator.java
@@ -1,5 +1,7 @@
 package bridge;
 
+import java.util.List;
+
 public class Validator {
 
     public void checkSize(String sizeName) {
@@ -12,4 +14,23 @@ public class Validator {
                     ExceptionMessage.incorrectSize.getMessage());
         }
     }
+
+    public void checkBridgeSize(int size, List<String> bridge) {
+        if(size != bridge.size()) {
+            throw new IllegalArgumentException(
+                    ExceptionMessage.bridgeNotEqualsSize.getMessage());
+        }
+    }
+
+    public void checkBridgeWord(int size, List<String> bridge) {
+        long count = bridge.stream()
+                .filter(s -> s.equals("U")||s.equals("D")).count();
+
+        if(count != size) {
+            throw new IllegalArgumentException(
+                    ExceptionMessage.bridgeNotUorD.getMessage());
+        }
+    }
+
+
 }
