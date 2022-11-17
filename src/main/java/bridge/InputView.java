80130adaab8be229d180155353f492ff66bba45c
jkroh1995 <85445649+jkroh1995@users.noreply.github.com>
2022-11-18 03:46:35 +0900

    feat : 사용자에게 이동할 칸 / 재시작 및 종료 여부의 입력을 요구하는 기능 구현
    readMoving() 함수 구현
    readGameCommand() 함수 구현

--- src/main/java/bridge/InputView.java
+++ src/main/java/bridge/InputView.java
@@ -6,15 +6,12 @@ import camp.nextstep.edu.missionutils.Console;
  * 사용자로부터 입력을 받는 역할을 한다.
  */
 public class InputView {
-    public static final String PRINT_LENGTH = "다리의 길이를 입력해주세요.";
-    public static final String PRINT_MOVE = "이동할 칸을 선택해주세요. (위: U, 아래: D)";
-    public static final String PRINT_RETRY = "게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)";
 
     /**
      * 다리의 길이를 입력받는다.
      */
     public static String readBridgeSize() {
-        System.out.println(PRINT_LENGTH);
+        System.out.println(Constants.PRINT_LENGTH);
         return Console.readLine();
     }
 
@@ -22,13 +19,15 @@ public class InputView {
      * 사용자가 이동할 칸을 입력받는다.
      */
     public String readMoving() {
-        return null;
+        System.out.println(Constants.PRINT_MOVE);
+        return Console.readLine();
     }
 
     /**
      * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
      */
     public String readGameCommand() {
-        return null;
+        System.out.println(Constants.PRINT_RETRY);
+        return Console.readLine();
     }
 }
