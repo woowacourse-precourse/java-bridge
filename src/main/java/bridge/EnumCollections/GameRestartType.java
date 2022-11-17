6e54f7b1d36e14aa9391dfb2048b5c82e1df4278
Park shinyeong <syngh503@gmail.com>
2022-11-17 21:53:56 +0900

    Feat(GameRestartType): 게임 재시작 타입 생성
    게임 재시작과 관련된 GameRestartType enum을 생성하였습니다.

--- src/main/java/bridge/EnumCollections/GameRestartType.java
+++ src/main/java/bridge/EnumCollections/GameRestartType.java
@@ -1,4 +1,20 @@
 package bridge.EnumCollections;
 
-public class GameRestartType {
+public enum GameRestartType {
+    RESTART('R'),
+    QUIT('Q');
+
+    private char restartStatus;
+
+    GameRestartType(char restartStatus) {
+        this.restartStatus = restartStatus;
+    }
+
+    public char getRestartStatus() {
+        return restartStatus;
+    }
+
+    public static boolean isRestartStatus(char input) {
+        return input == RESTART.getRestartStatus() || input == QUIT.getRestartStatus();
+    }
 }
