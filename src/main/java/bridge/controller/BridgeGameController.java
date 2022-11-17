fd3acdf551df1b75f860a15c6f4237409a2d5fb4
Cho SangHyun <98shcho@naver.com>
2022-11-18 01:47:30 +0900

    feat: 게임 시작 안내문구 출력 기능 추가

--- src/main/java/bridge/controller/BridgeGameController.java
+++ src/main/java/bridge/controller/BridgeGameController.java
@@ -1,4 +1,18 @@
 package bridge.controller;
 
+import bridge.view.InputView;
+import bridge.view.OutputView;
+
 public class BridgeGameController {
+    private final InputView inputView;
+    private final OutputView outputView;
+
+    public BridgeGameController() {
+        inputView = new InputView();
+        outputView = new OutputView();
+    }
+
+    public void startGame() {
+        outputView.printGameStart();
+    }
 }
