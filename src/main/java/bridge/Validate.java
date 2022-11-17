2a631e383bcae19f54bcb7bb2435bdac3f0cac7a
JJ <jeongjae1996@naver.com>
2022-11-18 03:30:00 +0900

    feat: 다리의 길이 입력 값을 검증하는 기능 구현
    - 3-20 사이의 값이 아니라면 IllegalArgumentException이 발생됩니다.

---
+++ src/main/java/bridge/Validate.java
@@ -0,0 +1,16 @@
+package bridge;
+
+import java.util.regex.Pattern;
+
+public class Validate {
+    public void bridgeSize(String input) {
+        final String INPUT_PATTERN = "^[3-9]$|^1[0-9]$|^20$";
+        final String EXCEPTION_MESSAGE = "[ERROR] 다리의 길이는 3 이상 20 이하의 숫자만 입력 가능합니다.";
+
+        boolean isMatch = Pattern.matches(INPUT_PATTERN, input);
+
+        if (!isMatch) {
+            throw new IllegalArgumentException(EXCEPTION_MESSAGE);
+        }
+    }
+}
