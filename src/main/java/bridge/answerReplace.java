daff198eeba3961176f5cb50d0ccdc2403476c01
saewoo1 <wooozooo1123@gmail.com>
2022-11-17 15:37:51 +0900

    refactor(answerReplace): 정답을 U, D로 변환하는 메소드 기능 분리

--- src/main/java/bridge/answerReplace.java
+++ src/main/java/bridge/answerReplace.java
@@ -4,16 +4,26 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class answerReplace {
-  public List<String> replace(int size) {
-    BridgeRandomNumberGenerator bridgeRandomNumberGenerator =  new BridgeRandomNumberGenerator();
-    List<String> replacedAnswer = new ArrayList<>();
+
+  // replaceAnswers(generateAnswers(InputView.readsize())); 이런 식으루.. 지저분한가
+  public List<Integer> generateAnswers(int size) {
+    BridgeRandomNumberGenerator bridgeRandomNumberGenerator = new BridgeRandomNumberGenerator();
+    List<Integer> answers = new ArrayList<>();
 
     for (int i = 0; i < size; i++) {
       int num = bridgeRandomNumberGenerator.generate();
-      if (num == 1) {
+      answers.add(num);
+    }
+    return answers;
+  }
+
+  public List<String> replaceAnswers(List<Integer> answers) {
+    ArrayList<String> replacedAnswer = new ArrayList<>();
+    for (Integer answer : answers) {
+      if (answer == 1) {
         replacedAnswer.add("U");
       }
-      if (num == 0) {
+      if (answer == 0) {
         replacedAnswer.add("D");
       }
     }
