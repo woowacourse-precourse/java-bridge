202d4b3cb2056e6fad28054677c768c2c0fbad12
MuSeung <peter0757@nvaer.com>
2022-11-17 17:44:56 +0900

    test: BridgeMaker의 사이즈 테스트의 케이스 추가
    makeBridgeBySizes
        - 테스트 케이스를 추가하였다.

--- src/test/java/bridge/BridgeMakerTest.java
+++ src/test/java/bridge/BridgeMakerTest.java
@@ -7,17 +7,19 @@ import bridge.ApplicationTest.TestNumberGenerator;
 import java.util.List;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 public class BridgeMakerTest {
 
-    @DisplayName("size로 6이 주어지면 반환되는 list의 사이즈는 6이다.")
-    @Test
-    void makeBridgeBySizeSix() {
+    @DisplayName("size가 주어지면 반환되는 list의 사이즈는 size와 같다.")
+    @ParameterizedTest
+    @ValueSource(ints = {3, 6, 8, 9, 18, 19, 20})
+    void makeBridgeBySizes(int size) {
         TestNumberGenerator numberGenerator = new TestNumberGenerator(
                 newArrayList(1, 0, 1, 0, 1, 0, 1, 0, 1)
         );
         BridgeMaker bridgeMaker = new BridgeMaker(numberGenerator);
-        int size = 6;
         List<String> result = bridgeMaker.makeBridge(size);
         assertThat(result.size()).isEqualTo(size);
     }
