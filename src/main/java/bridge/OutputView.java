81cfa3614f92daad254bdcbb117bcc1f90f8f0f5
jinny-codes <77732679+jinny-codes@users.noreply.github.com>
2022-11-18 02:50:53 +0900

    feat(OutputView): printStart 구현

--- src/main/java/bridge/OutputView.java
+++ src/main/java/bridge/OutputView.java
@@ -30,6 +30,9 @@ public class OutputView {
         }
         System.out.println("총 시도한 횟수" +numTrial);
     }
+    public void printStart() {
+        System.out.println("다리 건너기 게임을 시작합니다.");
+    }
 
 
 }
