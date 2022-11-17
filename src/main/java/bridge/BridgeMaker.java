22e4caaaaaaa8ccdc18a9f60e5d4f50bf2487576
Jeongthingamajig <netpetet@naver.com>
2022-11-17 23:51:22 +0900

    Feat : BridgeMaker 클래스 구현
    BridgeRandomNumberGenerator 로 생성자에서 업캐스팅해 0과 1을 구하는 메소드를 사용하도록 함. 이 메소드를 사용해서 0과 1의 리스트를 구하고 그것을 U와 D로 이루어진 리스트를 구하는데 사용함

--- src/main/java/bridge/BridgeMaker.java
+++ src/main/java/bridge/BridgeMaker.java
@@ -1,5 +1,6 @@
 package bridge;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -9,8 +10,8 @@ public class BridgeMaker {
 
     private final BridgeNumberGenerator bridgeNumberGenerator;
 
-    public BridgeMaker(BridgeNumberGenerator bridgeNumberGenerator) {
-        this.bridgeNumberGenerator = bridgeNumberGenerator;
+    public BridgeMaker(BridgeRandomNumberGenerator bridgeRandomNumberGenerator) {
+        this.bridgeNumberGenerator = bridgeRandomNumberGenerator;
     }
 
     /**
@@ -18,6 +19,33 @@ public class BridgeMaker {
      * @return 입력받은 길이에 해당하는 다리 모양. 위 칸이면 "U", 아래 칸이면 "D"로 표현해야 한다.
      */
     public List<String> makeBridge(int size) {
-        return null;
+        List<Integer> bridgeNumber = new ArrayList<>(size);
+
+        makeBridgeNumber(bridgeNumberGenerator, bridgeNumber, size);
+
+        return bridgeWinning(bridgeNumber);
+    }
+
+    private void makeBridgeNumber(BridgeNumberGenerator bridgeNumberGenerator, List<Integer> numberList, int size){
+        while (numberList.size() == size){
+            numberList.add(bridgeNumberGenerator.generate());
+        }
+    }
+
+    private List<String> bridgeWinning(List<Integer> bridgeNumberList){
+        List<String> bridgeWinningList = new ArrayList<>(bridgeNumberList.size());
+        for(int number : bridgeNumberList){
+            bridgeWinningWord(bridgeWinningList,number);
+        }
+        return bridgeWinningList;
+    }
+
+    private void bridgeWinningWord(List<String> bridgeNumberList, int number){
+        if(number == 1){
+            bridgeNumberList.add("U");
+        }
+        if(number == 0){
+            bridgeNumberList.add("D");
+        }
     }
 }
