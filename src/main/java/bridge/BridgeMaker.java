55afbe464e7a120be23a24e772aadda5820cd268
JunDonghyuk <ehdgur4814@naver.com>
2022-11-17 17:34:32 +0900

    feat(BridgeMaker): 다리 길이를 받아 다리 생성 기능 구현

--- src/main/java/bridge/BridgeMaker.java
+++ src/main/java/bridge/BridgeMaker.java
@@ -1,5 +1,6 @@
 package bridge;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -18,6 +19,24 @@ public class BridgeMaker {
      * @return 입력받은 길이에 해당하는 다리 모양. 위 칸이면 "U", 아래 칸이면 "D"로 표현해야 한다.
      */
     public List<String> makeBridge(int size) {
+        List<String> bridge = new ArrayList<>();
+        for (int length = 0; length < size; length++) {
+            String bridgeCommand = changeBridgeNumberToString(bridgeNumberGenerator.generate());
+            bridge.add(bridgeCommand);
+        }
+
+        return bridge;
+    }
+
+    private String changeBridgeNumberToString(int bridgeNumber) {
+        if (bridgeNumber == 0) {
+            return CommandConstant.DOWN_COMMAND;
+        }
+
+        if (bridgeNumber == 1) {
+            return CommandConstant.UP_COMMAND;
+        }
+
         return null;
     }
 }
