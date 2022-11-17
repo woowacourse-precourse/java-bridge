421082c6326acd9b32375896edb2fef70936e7d2
jeomxon <jeomxon@gmail.com>
2022-11-17 19:21:46 +0900

    feat: 다리 건너기 게임을 재시작하기 위한 기능 구현

--- src/main/java/bridge/BridgeGame.java
+++ src/main/java/bridge/BridgeGame.java
@@ -4,6 +4,8 @@ package bridge;
  * 다리 건너기 게임을 관리하는 클래스
  */
 public class BridgeGame {
+    private final InputView inputView = new InputView();
+    private final OutputView outputView = new OutputView();
 
     /**
      * 사용자가 칸을 이동할 때 사용하는 메서드
@@ -18,6 +20,21 @@ public class BridgeGame {
      * <p>
      * 재시작을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void retry() {
+    private boolean retry(String command) {
+        if (command.equals("U")) {
+            return true;
+        }
+        return false;
+    }
+
+    public void start() {
+        do {
+            outputView.startBridgeGame();
+            int bridgeSize = inputView.readBridgeSize();
+            BridgeRandomNumberGenerator bridgeRandomNumberGenerator = new BridgeRandomNumberGenerator();
+            BridgeMaker bridgeMaker = new BridgeMaker(bridgeRandomNumberGenerator);
+            bridgeMaker.makeBridge(bridgeSize);
+//            move(bridgeSize);
+        } while (retry(inputView.readGameCommand()));
     }
 }
