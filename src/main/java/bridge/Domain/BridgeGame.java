16f007fdc64aa45c148c16de42b62d18b0c40a09
swandevson <shai4956@gmail.com>
2022-11-17 22:47:24 +0900

    Implement(OutputView): 입력 결과, 최종 결과 출력

--- src/main/java/bridge/Domain/BridgeGame.java
+++ src/main/java/bridge/Domain/BridgeGame.java
@@ -41,6 +41,7 @@ public class BridgeGame {
      * 재시작을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
     public void retry() {
+        this.retryCount++;
     }
 
 
@@ -71,5 +72,13 @@ public class BridgeGame {
         if (player.isAlive()) {
             player.success();
         }
+    }
+
+    public Player getPlayer() {
+        return this.player;
+    }
+
+    public int getRetryCount() {
+        return this.retryCount;
     }
 }
--- src/main/java/bridge/View/OutputView.java
+++ src/main/java/bridge/View/OutputView.java
@@ -4,13 +4,50 @@ package bridge.View;
  * 사용자에게 게임 진행 상황과 결과를 출력하는 역할을 한다.
  */
 public class OutputView {
+    private String printLastStair(Player player, String stair) {
+        if (!stair.equals(player.getLastSelection())) {
+            return "   ";
+        }
 
-    /**
-     * 현재까지 이동한 다리의 상태를 정해진 형식에 맞춰 출력한다.
-     * <p>
-     * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
-     */
-    public void printMap() {
+        if (!player.isAlive()) {
+            return " X ";
+        }
+
+        return " O ";
+    }
+
+    private String printPassedStair(List<String> bridgeStates, Player player, String stair) {
+        String result = "";
+
+        for (int bridgeLocation = 0; bridgeLocation < player.getNextLocation() - 1; bridgeLocation++) {
+            String state = "   ";
+            if (stair.equals(bridgeStates.get(bridgeLocation))) {
+                state = " O ";
+            }
+            state += "|";
+            result += state;
+        }
+
+        return result;
+    }
+
+    private void printStair(List<String> bridgeStates, Player player, String stair) {
+        String result = "[";
+        result += printPassedStair(bridgeStates, player, stair);
+        result += printLastStair(player, stair);
+        result += "]";
+
+        System.out.println(result);
+    }
+
+    public void printMap(BridgeGame bridgeGame) {
+        List<String> bridgeState = bridgeGame.getBridgeStates();
+        Player player = bridgeGame.getPlayer();
+
+        System.out.println(player.getNextLocation());
+        printStair(bridgeState, player, "U");
+        printStair(bridgeState, player, "D");
+        System.out.println();
     }
 
     /**
@@ -18,6 +55,19 @@ public class OutputView {
      * <p>
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void printResult() {
+
+    private String printIsSuccess(BridgeGame bridgeGame) {
+        if (bridgeGame.isPlayerDead()) {
+            return "실패";
+        }
+
+        return "성공";
+    }
+    public void printResult(BridgeGame bridgeGame) {
+        System.out.println("최종 게임 결과");
+        printMap(bridgeGame);
+
+        System.out.println("게임 성공 여부: " + printIsSuccess(bridgeGame));
+        System.out.println("총 시도한 횟수: " + bridgeGame.getRetryCount());
     }
 }
