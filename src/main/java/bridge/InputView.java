67d8a6be975071b7faa1fc3bb71dc41b1def64c3
neppiness <0414kjh@naver.com>
2022-11-17 11:26:17 +0900

    feat: 다리 길이 입력 및 예외 처리 구현 (src/main/java/bridge)
    readBridgeSize를 통해 다리 길이 입력
    validate를 통해 예외 처리
    - checkLengthLimit: 문자열 길이가 3 이상인 입력
    - isInputNumber: 숫자 외 다른 문자가 사용된 입력
    - isTenthsPlaceZero: 십의 자리 숫자가 0인 숫자 입력
    - isInValidRange: 3 이상 20 이하의 숫자가 아닌 입력

--- src/main/java/bridge/InputView.java
+++ src/main/java/bridge/InputView.java
@@ -1,17 +1,58 @@
 package bridge;
 
+import camp.nextstep.edu.missionutils.Console;
+
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
  */
 public class InputView {
+    private String lineInput;
 
+    private void checkLengthLimit(String lineInput, int limit) {
+        if (lineInput.length() > limit)
+            throw new IllegalArgumentException("[ERROR] 한 글자 또는 두 글자만 입력해주세요.");
+    }
+
+    private void isInputNumber(String lineInput) {
+        for (int index = 0; index < lineInput.length(); index++) {
+            if (lineInput.charAt(index) < '0' || lineInput.charAt(index) > '9')
+                throw new IllegalArgumentException("[ERROR] 숫자 외 다른 문자는 입력할 수 없습니다.");
+        }
+    }
+
+    private void isTenthsPlaceZero(String lineInput) {
+        if (lineInput.charAt(0) == '0')
+            throw new IllegalArgumentException("[ERROR] 십의 자리가 0일 수 없습니다.");
+    }
+
+    private void isInValidRange(String lineInput, int lowerBound, int upperBound) {
+        int number = Integer.parseInt(lineInput);
+        if (number < lowerBound || number > upperBound)
+            throw new IllegalArgumentException("[ERROR] 3 이상 20 이하의 자연수를 입력해주세요.");
+    }
+
+    private boolean validate(String lineInput) {
+        try {
+            checkLengthLimit(lineInput, 2);
+            isInputNumber(lineInput);
+            isTenthsPlaceZero(lineInput);
+            isInValidRange(lineInput, 3, 20);
+        } catch(IllegalArgumentException e) {
+            // TODO: pass message to OutputView
+            return false;
+        }
+    }
+    
     /**
      * 다리의 길이를 입력받는다.
      */
     public int readBridgeSize() {
-        return 0;
+        while(true) {
+            String lineInput = Console.readLine();
+            if(validate(lineInput)) break;
+        }
+        return Integer.parseInt(lineInput);
     }
-
     /**
      * 사용자가 이동할 칸을 입력받는다.
      */
