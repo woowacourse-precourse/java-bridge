2154b372a9a0c708b37084fc301211417b620b3a
woosung1223 <dntjd991223@naver.com>
2022-11-17 20:04:18 +0900

    feat(BridgeConverter): 해당 숫자에 맞는 메세지를 반환하는 메소드 생성

--- src/main/java/bridge/BridgeConverter.java
+++ src/main/java/bridge/BridgeConverter.java
@@ -1,5 +1,7 @@
 package bridge;
 
+import java.util.Arrays;
+
 public enum BridgeConverter {
     DOWN(0, "D"),
     UP(1, "U");
@@ -10,5 +12,13 @@ public enum BridgeConverter {
     BridgeConverter(int number, String message) {
         this.number = number;
         this.message = message;
+    }
+
+    public static String getMessageFromNumber(int number) {
+        return Arrays.stream(values())
+                .filter(value -> value.number == number)
+                .findFirst()
+                .get()
+                .message;
     }
 }
