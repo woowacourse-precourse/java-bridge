1ad85adf8de5fcd559f969c35bc95aa28e45b4f2
junhei Lee <jun2ee22@naver.com>
2022-11-17 21:40:14 +0900

    feat(ui): 사용자로부터 입력 받는 기능
    자동으로 생성할 다리 길이, 이동할 칸, 게임 재시작/종료 여부

--- src/main/java/bridge/view/InputView.java
+++ src/main/java/bridge/view/InputView.java
@@ -1,28 +1,42 @@
 package bridge.view;
 
+import camp.nextstep.edu.missionutils.Console;
+
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
  */
 public class InputView {
+    private static final String INPUT_ONLY_NUMBER = "다리길이는 숫자여야 합니다.";
+    private static final String NOT_MOVING_PATTERN = "[^UD]";
+    private static final String INPUT_U_OR_D = "위 칸은 U, 아래 칸은 D로만 입력 가능합니다.";
+    private static final String NOT_GAME_COMMAND_PATTERN = "[^RQ]";
+    private static final String INPUT_R_OR_Q = "재시작은 R, 종료는 Q로만 입력 가능합니다.";
 
-    /**
-     * 다리의 길이를 입력받는다.
-     */
     public int readBridgeSize() {
-        return 0;
+        int bridgeSize;
+        try {
+            bridgeSize = Integer.parseInt(Console.readLine());
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException(INPUT_ONLY_NUMBER);
+        }
+        return bridgeSize;
     }
 
-    /**
-     * 사용자가 이동할 칸을 입력받는다.
-     */
     public String readMoving() {
-        return null;
+        String moving = Console.readLine();
+        validate(moving, NOT_MOVING_PATTERN, INPUT_U_OR_D);
+        return moving;
     }
 
-    /**
-     * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
-     */
     public String readGameCommand() {
-        return null;
+        String gameCommand = Console.readLine();
+        validate(gameCommand, NOT_GAME_COMMAND_PATTERN, INPUT_R_OR_Q);
+        return gameCommand;
+    }
+
+    private void validate(String input, String pattern, String errorMessage) {
+        if (input.matches(pattern)) {
+            throw new IllegalArgumentException(errorMessage);
+        }
     }
 }
