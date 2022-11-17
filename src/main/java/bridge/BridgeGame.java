268720cce4d6d67f0c0716a3ed6ef8ee93e8848e
kevstevie <jianna442@gmail.com>
2022-11-17 15:01:07 +0900

    feat: BridgeGame 인스턴스 생성자 추가
    BridgeMaker 에서 BridgeGame 을 생성하도록 설계

--- src/main/java/bridge/BridgeGame.java
+++ src/main/java/bridge/BridgeGame.java
@@ -1,9 +1,21 @@
 package bridge;
 
+import java.util.List;
+
 /**
  * 다리 건너기 게임을 관리하는 클래스
  */
 public class BridgeGame {
+
+    private int position;
+    private long playTime;
+    List<String> gameMap;
+
+    public BridgeGame(List<String> gameMap) {
+        this.gameMap = gameMap;
+        position = 0;
+        playTime = 1;
+    }
 
     /**
      * 사용자가 칸을 이동할 때 사용하는 메서드
