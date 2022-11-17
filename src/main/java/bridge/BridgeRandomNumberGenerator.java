7208f3a1dfd7e488d8668f905133d4eed9ea5e69
nylonmusk <106235834+nylonmusk@users.noreply.github.com>
2022-11-18 02:09:54 +0900

    refactor(BridgeRandomNumberGenerator): 매직넘버 제거

--- src/main/java/bridge/BridgeRandomNumberGenerator.java
+++ src/main/java/bridge/BridgeRandomNumberGenerator.java
@@ -1,14 +1,12 @@
 package bridge;
 
 import camp.nextstep.edu.missionutils.Randoms;
+import constant.GameConstant;
 
 public class BridgeRandomNumberGenerator implements BridgeNumberGenerator {
 
-    private static final int RANDOM_LOWER_INCLUSIVE = 0;
-    private static final int RANDOM_UPPER_INCLUSIVE = 1;
-
     @Override
     public int generate() {
-        return Randoms.pickNumberInRange(RANDOM_LOWER_INCLUSIVE, RANDOM_UPPER_INCLUSIVE);
+        return Randoms.pickNumberInRange(GameConstant.RANDOM_LOWER_INCLUSIVE.getConstant(), GameConstant.RANDOM_UPPER_INCLUSIVE.getConstant());
     }
 }
