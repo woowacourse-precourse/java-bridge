fd50df76591f08a90ed46ccdde48e934f8ac9b91
JunH <tjdtls690@gmail.com>
2022-11-17 19:40:45 +0900

    :white_check_mark: test(Validator : InputSelectCharValidatorTest) : 재시도 여부 입력 시, 특수 문자 입력 시 예외 처리 기능 테스트 성공

--- src/test/java/bridge/validator/InputSelectCharValidatorTest.java
+++ src/test/java/bridge/validator/InputSelectCharValidatorTest.java
@@ -118,4 +118,13 @@ class InputSelectCharValidatorTest {
                 .isThrownBy(() -> InputSelectCharValidator.validate(input, RETRY_SELECT_CHAR_VALIDATE_NUMBER))
                 .withMessageStartingWith(ErrorMessageConstant.ERROR_MESSAGE);
     }
+    
+    @DisplayName("재시도 여부 입력 예외 처리 : 특수 문자 입력 시")
+    @ParameterizedTest(name = "{displayName} : input => {0}")
+    @ValueSource(strings = {"@", "$", "%", ",", "."})
+    void specialCharactersRetryException(final String input) {
+        assertThatIllegalArgumentException()
+                .isThrownBy(() -> InputSelectCharValidator.validate(input, RETRY_SELECT_CHAR_VALIDATE_NUMBER))
+                .withMessageStartingWith(ErrorMessageConstant.ERROR_MESSAGE);
+    }
 }
