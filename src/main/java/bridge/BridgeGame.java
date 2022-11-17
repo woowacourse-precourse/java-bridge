8552084cd9ad5777a6cc562ba1ff45042bd22d1a
JunJI97 <bigwkddo@gmail.com>
2022-11-18 02:30:15 +0900

    [기능구현] 다리 생성 기능 구현

--- src/main/java/bridge/BridgeGame.java
+++ src/main/java/bridge/BridgeGame.java
@@ -1,19 +1,21 @@
 package bridge;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * 다리 건너기 게임을 관리하는 클래스
  */
 public class BridgeGame {
+    private static List<String> bridge = new ArrayList<>();
     BridgeNumberGenerator bridgeNumberGenerator = new BridgeRandomNumberGenerator();
-    BridgeMaker bridge = new BridgeMaker(bridgeNumberGenerator);
+    BridgeMaker bridgemaker = new BridgeMaker(bridgeNumberGenerator);
     InputView input = new InputView();
 
     public void run() {
         int lengthBridge = input.readBridgeSize();
-        System.out.println(lengthBridge);
-        // make Bridge
-//        bridge.makeBridge(3);
-
+        bridge = bridgemaker.makeBridge(lengthBridge);
+        System.out.println(bridge);
     }
     /**
      * 사용자가 칸을 이동할 때 사용하는 메서드
