a750d3bd3af76c2b913f8c8be25a4e0330823c00
Jenn Kim <grdd0419@gmail.com>
2022-11-18 01:42:32 +0900

    feat: 다리 길이를 입력받는 기능 추가

--- src/main/java/bridge/InputView.java
+++ src/main/java/bridge/InputView.java
@@ -1,5 +1,7 @@
 package bridge;
 
+import camp.nextstep.edu.missionutils.Console;
+
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
  */
@@ -9,7 +11,18 @@ public class InputView {
      * 다리의 길이를 입력받는다.
      */
     public int readBridgeSize() {
-        return 0;
+
+        int bridgeSize;
+        try {
+            bridgeSize = Integer.parseInt(Console.readLine());
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("[ERROR] 다리 길이는 숫자여야 합니다.");
+        }
+
+        if (bridgeSize < 3 || bridgeSize > 20)
+            throw new IllegalArgumentException("[ERROR] 다리 길이는 3부터 20 사이의 숫자여야 합니다.");
+
+        return bridgeSize;
     }
 
     /**
