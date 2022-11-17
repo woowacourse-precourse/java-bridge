97c227ad20595c2351ab9e97af1e9ef4ed43fc9d
kissshot1104 <kissshot.heartunderblade1104@gmail.com>
2022-11-17 18:59:28 +0900

    Feat(ExceptionBasket) : 잘못된 키 입력시 예외처리 테스트 코드를 추가
    public static void invalidGameKey(String input) 메소드를 추가했습니다.
    이 메소드는 input이 'U'나 'D'가 아니라면 illegalArgumentException예외 처리 합니다.

---
+++ src/main/java/bridge/ExceptionBasket.java
@@ -0,0 +1,46 @@
+package bridge;
+
+import java.math.BigInteger;
+
+public class ExceptionBasket {
+
+    public static final int MAX = 20;
+    public static final int MIN = 1;
+    public static final String ERROR_MESSAGE = "[ERROR]";
+
+    //1 ~ 20이외의 정수나 맨 앞에 0과 +가 나온다면 예외 처리
+    public static void inputIsNotRangeException(String input) throws IllegalArgumentException {
+        BigInteger tmpInput;
+        try {
+            tmpInput = new BigInteger(input);
+        }catch (IllegalArgumentException iae) {
+            throw new IllegalArgumentException(ERROR_MESSAGE + " 1~20사이의 정수가 아닙니다.");
+        }
+        if (tmpInput.compareTo(BigInteger.valueOf(MIN)) == -1 || tmpInput.compareTo(BigInteger.valueOf(MAX)) == 1
+                || input.indexOf('0') == 0 || input.indexOf('+') == 0){
+            throw new IllegalArgumentException(ERROR_MESSAGE + " 1~20사이의 정수가 아닙니다.");
+        }
+    }
+
+    //숫자가 아닐 경우 예외 처리
+    public static void inputIsNotNumberException(String input) throws IllegalArgumentException {
+        if (Util.isNumber(input)){
+            throw new IllegalArgumentException(ERROR_MESSAGE + " 숫자가 아닙니다!");
+        }
+    }
+
+    public static void invalidGameKey(String input) throws IllegalArgumentException {
+        if (!input.equals("U") || !input.equals("D")){
+            throw new IllegalArgumentException(ERROR_MESSAGE + " U나 D를 누르세요!");
+        }
+    }
+
+    /*public static void invalidNumberic(String input) throws IllegalArgumentException {
+        if (input.indexOf('+') == 0
+                || input.indexOf('0') == 0
+                || input.indexOf('-') == 0){
+            throw new IllegalArgumentException(ERROR_MESSAGE + " 잘못된 숫자열입니다.");
+        }
+    }*/
+
+}
