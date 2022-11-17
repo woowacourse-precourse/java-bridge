0e100eb4d69ba3bd6f45c5994fc72c3070984732
jjongwa <troas96@naver.com>
2022-11-17 22:55:17 +0900

    feat: 게임 시작, 다리 길이 입력 UI

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -1,8 +1,27 @@
 package bridge;
 
 public class Application {
+    static void startMessage() {
+        System.out.println("다리 건너기 게임을 시작합니다.");
+        System.out.println();
+    }
+
+    static int readBridgeSize(InputView input) {
+        System.out.println("다리의 길이를 입력해주세요.");
+        int readInput = input.readBridgeSize();
+        System.out.println();
+        return readInput;
+    }
 
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        try {
+            startMessage();
+            InputView input = new InputView();
+            int bridgeSize = readBridgeSize(input);
+        } catch (IllegalArgumentException exception) {
+            System.out.print("[ERROR] ");
+            System.out.println(exception.getMessage());
+        }
     }
 }
