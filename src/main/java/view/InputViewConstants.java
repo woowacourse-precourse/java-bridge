964cdd91816907e4acd4cb0d7877a04ca9469f8a
June <68291395+june-777@users.noreply.github.com>
2022-11-17 14:26:28 +0900

    refactor(InputViewConstants): 사용하지 않는 상수 제거

--- src/main/java/view/InputViewConstants.java
+++ src/main/java/view/InputViewConstants.java
@@ -1,8 +1,6 @@
 package view;
 
 public class InputViewConstants {
-	public static final String REGEX_ALL_NUMERIC = "[+-]?\\d*(\\.\\d+)?";
-
 	public static final String MESSAGE_READ_BRIDGE_SIZE = "다리의 길이를 입력해주세요.";
 	public static final String MESSAGE_READ_MOVING = "이동할 칸을 선택해주세요. (위: U, 아래: D)";
 	public static final String MESSAGE_READ_GAME_COMMAND = "게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)";
