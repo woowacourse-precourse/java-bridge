6871aff3418c1039d67d0faa0a99329e73f00759
realslimtaek <89964264+realslimtaek@users.noreply.github.com>
2022-11-17 18:21:41 +0900

    feat : 사용자의 size 입력에 따른 정답 리스트 생성

--- src/main/java/bridge/BridgeMaker.java
+++ src/main/java/bridge/BridgeMaker.java
@@ -1,5 +1,6 @@
 package bridge;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -7,8 +8,10 @@ import java.util.List;
  */
 public class BridgeMaker {
 
-    private final BridgeNumberGenerator bridgeNumberGenerator;
 
+    private final BridgeNumberGenerator bridgeNumberGenerator;
+    static int now_size = 1;
+    static List<String> bridge_answer = new ArrayList<>();
     public BridgeMaker(BridgeNumberGenerator bridgeNumberGenerator) {
         this.bridgeNumberGenerator = bridgeNumberGenerator;
     }
@@ -18,6 +21,21 @@ public class BridgeMaker {
      * @return 입력받은 길이에 해당하는 다리 모양. 위 칸이면 "U", 아래 칸이면 "D"로 표현해야 한다.
      */
     public List<String> makeBridge(int size) {
+        BridgeRandomNumberGenerator bridgeRandomNumberGenerator = new BridgeRandomNumberGenerator();
+        for (int i = 0; i < size; i++) {
+            int a = bridgeRandomNumberGenerator.generate();
+            if (a == 0)
+                bridge_answer.add("U");
+            if (a == 1)
+                bridge_answer.add("D");
+        }
+        return bridge_answer;
+    }
+
+    public String resultBridge(int size){
+
         return null;
     }
+
+
 }
