71c25dba9a9e5ff9e413af9a356aa4b0c8e71316
ysng_ysng <yeasung67@gmail.com>
2022-11-18 00:51:18 +0900

    feat(BridgeMaker): Implement the new methods to make bridge and bridge unit
    BridgeMaker클래스에서 다리를 만들어 List<String>으로 반환하는 makeBrige 메서드와
    bridge에
    이동가능
    위치를 랜덤으로 지정하는 makeRandomMovableLocation 메서드를 구현했습니다.

--- src/main/java/bridge/model/BridgeMaker.java
+++ src/main/java/bridge/model/BridgeMaker.java
@@ -2,6 +2,7 @@ package bridge.model;
 
 import bridge.BridgeNumberGenerator;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -9,17 +10,27 @@ import java.util.List;
  */
 public class BridgeMaker {
 
-    private final BridgeNumberGenerator bridgeNumberGenerator;
+	private final BridgeNumberGenerator bridgeNumberGenerator;
 
-    public BridgeMaker(BridgeNumberGenerator bridgeNumberGenerator) {
-        this.bridgeNumberGenerator = bridgeNumberGenerator;
-    }
+	public BridgeMaker(BridgeNumberGenerator bridgeNumberGenerator) {
+		this.bridgeNumberGenerator = bridgeNumberGenerator;
+	}
 
-    /**
-     * @param size 다리의 길이
-     * @return 입력받은 길이에 해당하는 다리 모양. 위 칸이면 "U", 아래 칸이면 "D"로 표현해야 한다.
-     */
-    public List<String> makeBridge(int size) {
-        return null;
-    }
+	/**
+	 * @param size 다리의 길이
+	 * @return 입력받은 길이에 해당하는 다리 모양. 위 칸이면 "U", 아래 칸이면 "D"로 표현해야 한다.
+	 */
+	public List<String> makeBridge(int size) {
+		List<String> bridge = new ArrayList<>();
+
+		for (int length = 1; length <= size; length++) {
+			bridge.add(makeRandomMovableLocation());
+		}
+
+		return bridge;
+	}
+
+	private String makeRandomMovableLocation() {
+		return BridgeUnitEnum.getMovableSymbolFromMovable(bridgeNumberGenerator.generate());
+	}
 }
