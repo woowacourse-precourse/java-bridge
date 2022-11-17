d5174fd3e9b785aba60f10f7a60f303127f34fa5
seung-won-kim <hesjs5@skuniv.ac.kr>
2022-11-17 18:00:14 +0900

    refactor(Validator): 3 부터 20 사이의 숫자인지 확인하는 기능을 분리

--- src/main/java/bridge/util/Validator.java
+++ src/main/java/bridge/util/Validator.java
@@ -7,12 +7,16 @@ import bridge.constant.GameCommand;
 public class Validator {
 
     public static void validateNumberInBridgeRange(int number) {
+        if (isNumberNotInBridgeRange(number)) {
+            throw new IllegalArgumentException("[ERROR] 다리 길이는 3부터 20 사이의 숫자여야 합니다.");
+        }
+    }
+
+    public static boolean isNumberNotInBridgeRange(int number) {
         int minRange = BridgeRange.MIN.getValue();
         int maxRange = BridgeRange.MAX.getValue();
 
-        if (!(minRange <= number && number <= maxRange)) {
-            throw new IllegalArgumentException("[ERROR] 다리 길이는 3부터 20 사이의 숫자여야 합니다.");
-        }
+        return !(minRange <= number && number <= maxRange);
     }
 
     public static void validateMovableInput(String input) {
