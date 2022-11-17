c21fbe0621d84f9f02ece0b9a18bd240111d98c9
JeongHunHui <deca3248@gmail.com>
2022-11-17 16:21:44 +0900

    refactor: BridgeLengthValidatorTest를 BridgeValidatorTest로 통합

--- src/test/java/bridge/BridgeValidatorTest.java
+++ src/test/java/bridge/BridgeValidatorTest.java
@@ -2,13 +2,14 @@ package bridge;
 
 import bridge.exception.LengthOutOfRangeException;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
-public class BridgeLengthValidatorTest {
+public class BridgeValidatorTest {
     @ParameterizedTest(name = "{index}: {displayName} length:{0}")
     @DisplayName("옳바른 다리 길이 입력 시")
     @ValueSource(ints = {3, 5, 20})
