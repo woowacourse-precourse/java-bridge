ae0f3affcc997c2d13ba727a822ff6f8f9d737a9
popomarin28@gmail.com <popomarin28@gmail.com>
2022-11-17 16:56:55 +0900

    feat(OutputView) : 게임의 필요한 메세지 출력 기능

--- src/main/java/bridge/OutputView.java
+++ src/main/java/bridge/OutputView.java
@@ -20,4 +20,16 @@ public class OutputView {
      */
     public void printResult() {
     }
+    public void printStart(){
+        System.out.println("다리 건너기 게임을 시작합니다.");
+    }
+    public void printAskBridgeLength() {
+        System.out.println("다리의 길이를 입력해주세요.");
+    }
+    public void printAskMovingButton(){
+        System.out.println("이동할 칸을 선택해주세요. (위: U, 아래: D)");
+    }
+    public void printAskGameRestart(){
+        System.out.println("게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)");
+    }
 }
