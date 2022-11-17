3d5b5403de4b2b9dbf353d5ed191f05bc9944815
renechoi90@gmail.com <renechoi90@gmail.com>
2022-11-17 11:47:46 +0900

    test : 이동 선택 입력 테스트 인자 추가

--- src/test/java/bridge/MovementTest.java
+++ src/test/java/bridge/MovementTest.java
@@ -20,7 +20,7 @@ public class MovementTest {
 
 	@DisplayName("이동할 칸 입력시 정상값을 제외한 다른 입력에 대한 예외 처리를 검증한다")
 	@ParameterizedTest
-	@ValueSource(strings = {"a", "@", "UP", "1"})
+	@ValueSource(strings = {"a", "@", "UP", "1", "u"})
 	void verify_Movement_OnlyAsAssigned(String value) {
 		assertThatThrownBy(() -> Movement.valueOf(value))
 				.isInstanceOf(IllegalArgumentException.class)
