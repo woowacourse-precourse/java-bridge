260ebbe7ad01bde6c17393fd7d775205a8a41e94
JUN <chltmdwns96@naver.com>
2022-11-17 20:31:14 +0900

    comment : 클래스 구현 사항 주석 추가

--- src/main/java/bridge/InputView.java
+++ src/main/java/bridge/InputView.java
@@ -7,6 +7,8 @@ package bridge;
  * InputView의 패키지는 변경할 수 있다.
  * InputView의 메서드의 시그니처(인자, 이름)와 반환 타입은 변경할 수 있다.
  * 사용자 값 입력을 위해 필요한 메서드를 추가할 수 있다.
+ *
+ * ★ InputView 클래스에서만 camp.nextstep.edu.missionutils.Console 의 readLine() 메서드를 이용해 사용자의 입력을 받을 수 있다.
  */
 public class InputView {
 
