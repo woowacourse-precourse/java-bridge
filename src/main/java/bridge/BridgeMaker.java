db8bf1fbceb94b048aef224bc441658ada9c0651
정승구 <jeongseung-gu@jeongseung-guui-MacBookAir.local>
2022-11-17 10:46:00 +0900

    refactor: 패키지 변경

--- src/main/java/bridge/BridgeMaker.java
+++ src/main/java/bridge/BridgeMaker.java
@@ -1,4 +1,4 @@
-package bridge.controller;
+package bridge;
 
 import bridge.BridgeNumberGenerator;
 
--- src/main/java/bridge/model/BridgeGame.java
+++ src/main/java/bridge/model/BridgeGame.java
@@ -1,4 +1,4 @@
-package bridge.controller;
+package bridge.model;
 
 /**
  * 다리 건너기 게임을 관리하는 클래스
