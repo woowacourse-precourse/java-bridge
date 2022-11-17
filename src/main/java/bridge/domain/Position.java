c8b06a8f8bcb96e40669d2125ec9f6f61ae49182
minju412 <ln8847@ajou.ac.kr>
2022-11-17 12:33:32 +0900

    feat: add Position
    - 사용자가 몇 번째 칸에 위치하는지와 UP/DOWN 상태를 가지고 있는 클래스

---
+++ src/main/java/bridge/domain/Position.java
@@ -0,0 +1,40 @@
+package bridge.domain;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Position {
+
+    private static final int INDEX_INDEX = 0;
+    private static final int ELEMENT_INDEX = 1; // up or down
+    private static final int UP = 1;
+    private static final int DOWN = 0;
+
+    private final List<Integer> position = new ArrayList<>();
+
+    public Position() {
+        resetPosition();
+    }
+
+    public void updatePosition(String input) {
+        position.add(INDEX_INDEX, position.get(INDEX_INDEX) + 1);
+        if (input.equals("U")) {
+            position.add(ELEMENT_INDEX, UP);
+        }
+        if (input.equals("D")) {
+            position.add(ELEMENT_INDEX, DOWN);
+        }
+    }
+
+    public void resetPosition() {
+        position.add(INDEX_INDEX, -1);
+    }
+
+    public int getElementIndex() { // TODO: getter 사용 자제... 메시지를 보내기
+        return position.get(ELEMENT_INDEX);
+    }
+
+    public int getIndex() { // TODO: getter 사용 자제... 메시지를 보내기
+        return position.get(INDEX_INDEX);
+    }
+}
