a1be4c49b8f124553b3afb25d97373234742ced8
cpot5620 <=cpot5620@naver.com>
2022-11-17 21:06:22 +0900

    feat(BridgeGame): create move method

---
+++ src/test/java/bridge/BridgeGameTest.java
@@ -0,0 +1,33 @@
+package bridge;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BridgeGameTest {
+    private final BridgeGame bridgeGame = new BridgeGame(
+            new Bridge(Arrays.asList("U", "U", "U", "D")));
+
+    @DisplayName("올바른 칸으로 이동하면 true를 반환한다.")
+    @Test
+    void moveToValidPath() {
+        for (int i = 0; i < 3; i++) {
+            Assertions.assertThat(bridgeGame.move("U"))
+                    .isEqualTo(true);
+        }
+        Assertions.assertThat(bridgeGame.move("D"))
+                .isEqualTo(true);
+    }
+
+    @DisplayName("이동할 수 없는 칸으로 이동하면 false를 반환한다.")
+    @Test
+    void moveToInvalidPath() {
+        Assertions.assertThat(bridgeGame.move("D"))
+                .isEqualTo(false);
+    }
+
+}
