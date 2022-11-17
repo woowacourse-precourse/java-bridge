a7228868a5c2a7f07f81211545bf0e8ba7898b6d
Be-student <presentsong@naver.com>
2022-11-18 00:33:27 +0900

    feat(ControllerCommand): 컨트롤러에서 사용되는 커맨드 추가

---
+++ src/main/java/bridge/domain/ControllerCommand.java
@@ -0,0 +1,27 @@
+package bridge.domain;
+
+import java.util.Arrays;
+import java.util.Objects;
+
+public enum ControllerCommand {
+    RETRY("R"),
+    QUIT("Q");
+
+    private final String key;
+
+    ControllerCommand(String key) {
+        this.key = key;
+    }
+
+    public static ControllerCommand from(String key) {
+        return Arrays.stream(ControllerCommand.values())
+                .filter(it -> Objects.equals(it.key, key))
+                .findFirst()
+                .orElseThrow(() -> new IllegalArgumentException("R,Q 만 입력 가능합니다"));
+    }
+
+    @Override
+    public String toString() {
+        return "ControllerCommand : " + key;
+    }
+}
