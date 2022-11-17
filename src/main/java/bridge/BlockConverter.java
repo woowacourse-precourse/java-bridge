51accab6945b3786510635915aada0777080fe57
woosung1223 <dntjd991223@naver.com>
2022-11-17 20:38:30 +0900

    refactor(BlockConveter): 인스턴스 변수 final 상수화

--- src/main/java/bridge/BlockConverter.java
+++ src/main/java/bridge/BlockConverter.java
@@ -6,8 +6,8 @@ public enum BlockConverter {
     DOWN(0, "D"),
     UP(1, "U");
 
-    private int number;
-    private String message;
+    private final int number;
+    private final String message;
 
     BlockConverter(int number, String message) {
         this.number = number;
