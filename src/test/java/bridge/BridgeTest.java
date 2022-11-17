6265cacf94c84b8a8957de1550528f63a3e22331
dooboocookie <bkh3030@gmail.com>
2022-11-17 20:40:14 +0900

    test: Bridge 객체 다리 길이를 통하여 생성 테스트 및 예외테스트

---
+++ src/test/java/bridge/BridgeTest.java
@@ -0,0 +1,34 @@
+package bridge;
+
+import bridge.model.Bridge;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+public class BridgeTest {
+
+    @DisplayName("Bridge 객체 생성 테스트")
+    @Test
+    void newBridgeTest() {
+        assertThat(new Bridge(3))
+                .isInstanceOf(Bridge.class);
+        assertThat(new Bridge(10))
+                .isInstanceOf(Bridge.class);
+        assertThat(new Bridge(20))
+                .isInstanceOf(Bridge.class);
+    }
+
+    @DisplayName("Bridge 객체 생성 예외 테스트")
+    @Test
+    void newBridgeExceptionTest() {
+        assertThatThrownBy(() -> new Bridge(1))
+                .isInstanceOf(IllegalArgumentException.class);
+        assertThatThrownBy(() -> new Bridge(-1))
+                .isInstanceOf(IllegalArgumentException.class);
+        assertThatThrownBy(() -> new Bridge(21))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+}
