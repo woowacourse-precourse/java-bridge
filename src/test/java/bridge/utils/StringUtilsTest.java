e1b47219260c9a66c017dc8aa473b75a86746d56
JunH <tjdtls690@gmail.com>
2022-11-17 20:09:23 +0900

    :white_check_mark: test(Domain : StringUtilsTest) : Null or Empty 인지 확인하는 단위 테스트 추가

---
+++ src/test/java/bridge/utils/StringUtilsTest.java
@@ -0,0 +1,18 @@
+package bridge.utils;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+class StringUtilsTest {
+    @DisplayName("Null or Empty 인지 확인")
+    @ParameterizedTest(name = "{displayName} : input => {0}")
+    @NullAndEmptySource
+    void isBlank(final String input) {
+        assertThat(StringUtils.isBlank(input)).isTrue();
+    }
+}
