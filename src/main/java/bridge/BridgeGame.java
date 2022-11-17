322e6252feadc4f887a85ee1b8b64466b90f80f5
Cheon-DM <76736548+Cheon-DM@users.noreply.github.com>
2022-11-17 23:50:27 +0900

    feat(bridgeGame): 사용자가 칸을 이동하는 메서드
    사용자가 칸을 이동하는 메서드
    - 이동하는 칸 예외 처리

--- src/main/java/bridge/BridgeGame.java
+++ src/main/java/bridge/BridgeGame.java
@@ -5,12 +5,19 @@ package bridge;
  */
 public class BridgeGame {
 
+    private static final String UP = "U";
+    private static final String DOWN = "D";
+    private static final String RETRY = "R";
+    private static final String QUIT = "Q";
+    private static final String ERROR_MSG = "[ERROR]";
+
     /**
      * 사용자가 칸을 이동할 때 사용하는 메서드
      * <p>
      * 이동을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void move() {
+    public void move(String moving) {
+        checkUpDownOrThrowException(moving);
     }
 
     /**
@@ -19,5 +26,11 @@ public class BridgeGame {
      * 재시작을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
     public void retry() {
+    }
+
+    private void checkUpDownOrThrowException(String input){
+        if (!input.equals(UP) && !input.equals(DOWN)){
+            throw new IllegalArgumentException(ERROR_MSG + "U, D 이외의 문자는 입력할 수 없습니다.");
+        }
     }
 }
