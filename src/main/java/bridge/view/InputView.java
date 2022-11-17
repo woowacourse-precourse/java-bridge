0c37c9f706a6fc77c603d71656c795ae31596fcc
mckim123 <minchan06@gmail.com>
2022-11-17 13:31:25 +0900

    feat: implement user input reading methods
    Implement user input reading methods
    - readMoving(), readGameCommand(), readValidateBridgeSize()
    - need to implement validation methods

--- src/main/java/bridge/view/InputView.java
+++ src/main/java/bridge/view/InputView.java
@@ -1,17 +1,35 @@
 package bridge.view;
 
+import bridge.constant.PrintMessage;
+import camp.nextstep.edu.missionutils.Console;
+
 public class InputView {
 
     public int readBridgeSize() {
-        return 0;
+        System.out.print(PrintMessage.INPUT_BRIDGE_SIZE.getString());
+        String inputBridgeSize = Console.readLine();
+        if (validateBridgeSize(inputBridgeSize) == false) {
+            return readBridgeSize();
+        }
+        return Integer.parseInt(inputBridgeSize);
     }
 
     public String readMoving() {
-        return null;
+        System.out.print(PrintMessage.INPUT_MOVING.getString());
+        String inputMoving = Console.readLine();
+        if (validateMoving(inputMoving) == false) {
+            return readMoving();
+        }
+        return inputMoving;
     }
 
     public String readGameCommand() {
-        return null;
+        System.out.print(PrintMessage.INPUT_GAME_COMMAND.getString());
+        String inputGameCommand = Console.readLine();
+        if (validateMoving(inputGameCommand) == false) {
+            return readGameCommand();
+        }
+        return inputGameCommand;
     }
 
     private boolean validateBridgeSize(String inputBridgeSize) {
