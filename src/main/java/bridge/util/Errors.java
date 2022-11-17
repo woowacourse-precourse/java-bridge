607b7291e0d7c0d05797e954fd5c791c79807974
dooboocookie <bkh3030@gmail.com>
2022-11-17 20:47:25 +0900

    feat: 다리 생성 시 다리길이 입력받는 기능 구현 및 검증

--- src/main/java/bridge/util/Errors.java
+++ src/main/java/bridge/util/Errors.java
@@ -1,7 +1,8 @@
 package bridge.util;
 
 public enum Errors {
-    ERROR_BRIDGE_LENGTH("[ERROR] 다리 길이가 범위(3~20)을 벗어났습니다.");
+    ERROR_BRIDGE_LENGTH("[ERROR] 다리 길이가 범위(3~20)을 벗어났습니다."),
+    ERROR_INPUT_NOT_NUMBER_TYPE("[ERROR] 숫자 타입을 입력해주세요.");
 
     private String message;
 
--- src/main/java/bridge/view/InputView.java
+++ src/main/java/bridge/view/InputView.java
@@ -1,15 +1,28 @@
 package bridge.view;
 
+import bridge.util.Errors;
+import camp.nextstep.edu.missionutils.Console;
+
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
  */
 public class InputView {
+    private final String ASK_BRIDGE_SIZE = "다리의 길이를 입력해주세요.";
 
     /**
      * 다리의 길이를 입력받는다.
      */
     public int readBridgeSize() {
-        return 0;
+        System.out.println(ASK_BRIDGE_SIZE);
+        String input = readString();
+        validateNumberType(input);
+        return Integer.parseInt(input);
+    }
+
+    private void validateNumberType(String input) {
+        if (!input.matches("[0-9]+")) {
+            throw new IllegalArgumentException(Errors.ERROR_INPUT_NOT_NUMBER_TYPE.getMessage());
+        }
     }
 
     /**
@@ -24,5 +37,9 @@ public class InputView {
      */
     public String readGameCommand() {
         return null;
+    }
+
+    private String readString() {
+        return Console.readLine().trim();
     }
 }
