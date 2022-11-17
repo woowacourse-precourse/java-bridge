55cd75593965410ada4b8564e2756af92aa3d496
swandevson <shai4956@gmail.com>
2022-11-18 02:22:43 +0900

    Test(ApplicationTest): 재입력 동작에 대한 테스트

--- src/test/java/bridge/ApplicationTest.java
+++ src/test/java/bridge/ApplicationTest.java
@@ -40,6 +40,42 @@ class ApplicationTest extends NsTest {
     }
 
     @Test
+    void 기능_테스트_실패_후_성공() {
+        assertRandomNumberInRangeTest(() -> {
+            run("3", "U", "U", "R", "D", "R", "U", "D", "U");
+            assertThat(output()).contains(
+                    "[ O | X ]",
+                    "[   |   ]",
+                    "[   ]",
+                    "[ X ]",
+                    "최종 게임 결과",
+                    "[ O |   | O ]",
+                    "[   | O |   ]",
+                    "게임 성공 여부: 성공",
+                    "총 시도한 횟수: 3"
+            );
+        }, 1, 0, 1);
+    }
+
+    @Test
+    void 기능_테스트_실패_후_종료() {
+        assertRandomNumberInRangeTest(() -> {
+            run("3", "U", "U", "R", "D", "Q");
+            assertThat(output()).contains(
+                    "[ O | X ]",
+                    "[   |   ]",
+                    "[   ]",
+                    "[ X ]",
+                    "최종 게임 결과",
+                    "[ O | X ]",
+                    "[   |   ]",
+                    "게임 성공 여부: 실패",
+                    "총 시도한 횟수: 2"
+            );
+        }, 1, 0, 1);
+    }
+
+    @Test
     void 예외_테스트() {
         assertSimpleTest(() -> {
             runException("a");
