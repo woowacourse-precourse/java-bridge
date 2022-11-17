c3a26fbbd33b1f9b1ecc1a303d40188ca48bd4d0
KyungKyu Park <rudrb0505@gmail.com>
2022-11-17 14:56:12 +0900

    feat(BridgeLengthValidator): 입력받은 문자가 유효하지 않은 경우 예외 처리

--- src/main/java/bridge/validator/BridgeLengthValidator.java
+++ src/main/java/bridge/validator/BridgeLengthValidator.java
@@ -2,10 +2,15 @@ package bridge.validator;
 
 public class BridgeLengthValidator {
 	public static void validateNumber(String input) {
-		throw new IllegalArgumentException();
+		if (!input.matches("^[0-9]+$")) {
+			throw new IllegalArgumentException("숫자만 입력해주세요.");
+		}
 	}
 
 	public static void validateRange(String input) {
-		throw new IllegalArgumentException();
+		int number = Integer.parseInt(input);
+		if (number < 2 || number > 20) {
+			throw new IllegalArgumentException("2 이상 20 이하의 숫자를 입력해주세요.");
+		}
 	}
 }
