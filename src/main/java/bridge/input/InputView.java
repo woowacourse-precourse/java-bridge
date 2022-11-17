54a76e5b74968c0b85a31fe9dcbf141c65854806
alstjr706 <alstjr706@naver.com>
2022-11-17 20:15:16 +0900

    feat : (다리 건너기) 입력 받기 구현 & README 수정

--- src/main/java/bridge/input/InputView.java
+++ src/main/java/bridge/input/InputView.java
@@ -1,4 +1,6 @@
-package bridge;
+package bridge.input;
+
+import camp.nextstep.edu.missionutils.Console;
 
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
@@ -9,20 +11,24 @@ public class InputView {
      * 다리의 길이를 입력받는다.
      */
     public int readBridgeSize() {
-        return 0;
+        String bridgeInput = Console.readLine();
+        int legLength = Integer.parseInt(bridgeInput);
+        return legLength;
     }
 
     /**
      * 사용자가 이동할 칸을 입력받는다.
      */
     public String readMoving() {
-        return null;
+        String playerMoving = Console.readLine();
+        return playerMoving;
     }
 
     /**
      * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
      */
     public String readGameCommand() {
-        return null;
+        String restartInput = Console.readLine();
+        return restartInput;
     }
 }
