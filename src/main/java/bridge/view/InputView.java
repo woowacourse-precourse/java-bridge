93bde8a97d6b4726dadb9a89114ca12951389195
BioBeBe <76714219+JaegeonYu@users.noreply.github.com>
2022-11-17 15:56:58 +0900

    feat: readBridgeSize 메소드 작성

--- src/main/java/bridge/view/InputView.java
+++ src/main/java/bridge/view/InputView.java
@@ -1,5 +1,7 @@
 package bridge.view;
 
+import camp.nextstep.edu.missionutils.Console;
+
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
  */
@@ -9,8 +11,11 @@ public class InputView {
      * 다리의 길이를 입력받는다.
      */
     public int readBridgeSize() {
-        return 0;
+        String bridgeSizeInput = Console.readLine();
+        return Integer.parseInt(bridgeSizeInput);
     }
+
+
 
     /**
      * 사용자가 이동할 칸을 입력받는다.
