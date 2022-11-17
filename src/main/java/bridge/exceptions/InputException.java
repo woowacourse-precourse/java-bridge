49463475b37a9b173bae4e195f7d5b6382e2c9f6
hojunking96 <junsong96@naver.com>
2022-11-17 14:26:23 +0900

    feats(inputException.java) : 이동할 칸 입력 예외 처리 기능 구현

--- src/main/java/bridge/exceptions/InputException.java
+++ src/main/java/bridge/exceptions/InputException.java
@@ -1,14 +1,19 @@
 package bridge.exceptions;
 
 public class InputException {
-    public static void  bridgeSizeNotNumber(String bridgeSizeInput) {
+    public static void bridgeSizeNumberCheck(String bridgeSizeInput) {
         for (int i = 0; i < bridgeSizeInput.length(); i++)
             if (!Character.isDigit(bridgeSizeInput.charAt(i)))
                 throw new IllegalArgumentException();
     }
 
-    public static void bridgeSizeOutOfRange(int bridgeSize) {
+    public static void bridgeSizeRangeCheck(int bridgeSize) {
         if (bridgeSize < 3 || bridgeSize > 20)
+            throw new IllegalArgumentException();
+    }
+
+    public static void movingInputErrorCheck(String movingInput) {
+        if (!movingInput.equals("U") && !movingInput.equals("D"))
             throw new IllegalArgumentException();
     }
 }
