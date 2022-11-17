f2376730bc157d891e4729351181a821b087a1e3
jiisuniui <dudndi9752@gmail.com>
2022-11-18 08:52:05 +0900

    feat(Way): 방향 정보를 갖고 있는 클래스 생성

---
+++ src/main/java/bridge/domain/model/Way.java
@@ -0,0 +1,12 @@
+package bridge.domain.model;
+
+public class Way {
+
+    String up;
+    String down;
+
+    public Way(String up, String down) {
+        this.up = up;
+        this.down = down;
+    }
+}
