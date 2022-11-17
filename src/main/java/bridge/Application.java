3bc9e4505a5fddfbd5709016c365627a52f427d3
jkroh1995 <85445649+jkroh1995@users.noreply.github.com>
2022-11-18 03:56:05 +0900

    feat : 게임을 시작하고 재시작 하는 기능 구현
    start()구현
    play() 구현
    retry()구현
    initializeVariables()구현
    checkResult()구현

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -1,8 +1,10 @@
 package bridge;
 
-public class Application {
 
+public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        System.out.println(EnumStrings.PRINT_START.getValue());
+        new BridgeGame().start();
     }
 }
--- src/main/java/bridge/BridgeGame.java
+++ src/main/java/bridge/BridgeGame.java
@@ -7,16 +7,27 @@ import java.util.List;
  */
 public class BridgeGame {
 
-    public BridgeGame(){
+    RealTimeBridge realTimeBridge = new RealTimeBridge();
+    OutputView outputView = new OutputView();
+    InputController inputController = new InputController();
+
+    int count = 1;
+    int size;
+    List<String> bridge;
+    boolean flag = true;
+    public String[][] realTimeMap = realTimeBridge.getMap();
+
+    public BridgeGame() {
 
     }
+
     /**
      * 사용자가 칸을 이동할 때 사용하는 메서드
      * <p>
      * 이동을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
     public String move(String bridgeText, String userMove) {
-        if(!bridgeText.equals(userMove)){
+        if (!bridgeText.equals(userMove)) {
             return " X ";
         }
         return " O ";
@@ -27,6 +38,44 @@ public class BridgeGame {
      * <p>
      * 재시작을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void retry() {
+
+    private void retry() {
+        String gameCommand = inputController.getGameCommand();
+        if (gameCommand.equals("Q")) {
+            outputView.printResult(flag, count, realTimeMap);
+            return;
+        }
+        initializeVariables();
+        play(size, bridge);
+    }
+
+    private void initializeVariables() {
+        flag = true;
+        count++;
+        realTimeBridge.initialize();
+    }
+
+    public void start() {
+        this.size = inputController.getBridgeSize();
+        BridgeNumberGenerator bridgeNumberGenerator = new BridgeRandomNumberGenerator();
+        BridgeMaker bridgeMaker = new BridgeMaker(bridgeNumberGenerator);
+        this.bridge = bridgeMaker.makeBridge(size);
+        play(size, bridge);
+    }
+
+    private void play(int size, List<String> bridge) {
+        for (int i = 0; i < size; i++) {
+            if (compareInputActual(bridge, i)) {
+                break;
+            }
+        }
+        checkResult();
+        outputView.printResult(flag, count, realTimeMap);
+    }
+
+    private void checkResult() {
+        if (!flag) {
+            retry();
+        }
     }
 }
