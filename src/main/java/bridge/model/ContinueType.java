e1f86d9d6699ac24e5ae5be930e867e0e95788a5
eora21 <eora21@naver.com>
2022-11-17 23:09:39 +0900

    feat: 🎸 추가 유저의 재시도 여부 선택에 대한 Enum 타입 획득
    재시도 입력값에 따른 타입을 얻어올 수 있게 했다.

--- src/main/java/bridge/model/ContinueType.java
+++ src/main/java/bridge/model/ContinueType.java
@@ -1,5 +1,8 @@
 package bridge.model;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public enum ContinueType {
     RETRY("R", "재시도"),
     QUIT("Q", "종료")
@@ -9,9 +12,25 @@ public enum ContinueType {
 
     private final String description;
 
+    private static final Map<String, ContinueType> textContinue = new HashMap<>() {{
+        for (ContinueType type : ContinueType.values()) {
+            put(type.text, type);
+        }
+    }};
+
     ContinueType(String text, String description) {
         this.text = text;
         this.description = description;
+    }
+
+    public static ContinueType searchContinueTypeToText(String text) {
+        ContinueType continueType = textContinue.get(text);
+
+        if (continueType == null) {
+            throw new IllegalArgumentException("허용되지 않는 글자입니다. 재시도 여부를 선택할 수 없습니다.");
+        }
+
+        return continueType;
     }
 
     public String getText() {
