fad705014d6dc9c6d62235edd1ee27ccbe0fcff6
chaos0103 <lyt1228@naver.com>
2022-11-17 11:11:14 +0900

    test(Bridge): 다리의 길이를 입력받은 수 만큼 생성하는 기능 테스트

---
+++ src/test/java/bridge/BridgeMakerTest.java
@@ -0,0 +1,36 @@
+package bridge;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.*;
+
+class BridgeMakerTest {
+
+    private final BridgeNumberGenerator bridgeNumberGenerator = new BridgeRandomNumberGenerator();
+    private final BridgeMaker bridgeMaker = new BridgeMaker(bridgeNumberGenerator);
+
+    private static final int SIZE = 10;
+    private static List<String> bridge;
+
+    @BeforeEach
+    void setUp() {
+        bridge = bridgeMaker.makeBridge(SIZE);
+    }
+
+    @DisplayName("다리의 길이를 입력된 수 만큼 생성")
+    @Test
+    void makeBridgeSize() {
+        assertThat(bridge.size()).isEqualTo(SIZE);
+    }
+
+    @DisplayName("생성된 다리는 U, D로 구성")
+    @Test
+    void makeBridgeString() {
+        bridge.forEach(target ->
+                assertThat(target).isIn("U", "D"));
+    }
+}
