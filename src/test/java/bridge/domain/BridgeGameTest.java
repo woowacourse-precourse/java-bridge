12d365eea6d32aee96bf59a0c0bd9a5dca7938ac
pilyang <30036534+pilyang@users.noreply.github.com>
2022-11-17 23:08:36 +0900

    test: BridgeGameTest 추가
    - 이동 성공, 실패 모두 테스트

--- src/test/java/bridge/domain/BridgeGameTest.java
+++ src/test/java/bridge/domain/BridgeGameTest.java
@@ -1,32 +1,35 @@
 package bridge.domain;
 
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
 
 class BridgeGameTest {
 
     private BridgeGame bridgeGame;
+    private Bridge basicBridge;
 
     @BeforeEach
     private void setUp() {
         bridgeGame = new BridgeGame();
+        basicBridge = Bridge.from(List.of("U", "D", "D", "U", "D", "D", "U", "D"));
     }
 
-    @Test
-    void moveTest() {
-        Bridge bridge = Bridge.from(List.of("U", "D", "D", "U", "D"));
+    @ParameterizedTest(name = "이동 결과 테스트 [{index}] : {0}이동 - 성공 여부 {1}")
+    @CsvSource(value = {"UDDUD,true", "UDDUU,false"})
+    void moveTest(String movingPath, boolean expectedResult) {
+
         Player player = new Player();
+        boolean actualResult = false;
 
-        bridgeGame.move(bridge, player, BridgeTile.UP);
-        bridgeGame.move(bridge, player, BridgeTile.DOWN);
-        boolean actualResult = bridgeGame.move(bridge, player, BridgeTile.DOWN);
-
-        boolean expectedResult = true;
+        for (String movingDirection : movingPath.split("")) {
+            BridgeTile targetTile = BridgeTile.findByTilePosition(movingDirection);
+            actualResult = bridgeGame.move(basicBridge, player, targetTile);
+        }
 
         assertThat(actualResult).isEqualTo(expectedResult);
     }
