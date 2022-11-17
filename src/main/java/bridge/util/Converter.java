e7608db3523ab8ed8bea789edaa7685f95255aa7
송민진 <m_6595@naver.com>
2022-11-17 09:48:48 +0900

    feat(bridge): make a bridge using bridge size input
    구현 : 사용자로부터 입력받은 다리 길이 입력값으로 다리 생성
    	- 입력값이 잘못되었다면 계속 시도하도록 구현
    	- 코드라인 10줄 이내로 작성

--- src/main/java/bridge/util/Converter.java
+++ src/main/java/bridge/util/Converter.java
@@ -1,7 +1,9 @@
 package bridge.util;
 
 public class Converter {
+
     public int convertToInt(String s) {
         return Integer.parseInt(s);
     }
+
 }
--- src/main/java/bridge/util/Validator.java
+++ src/main/java/bridge/util/Validator.java
@@ -5,4 +5,5 @@ public class Validator {
     public boolean isValidRange(int size) {
         return size >= 3 && size <= 20;
     }
+
 }
