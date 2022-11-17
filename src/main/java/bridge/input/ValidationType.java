230a53bc0746d795f6bac23bd7de7ae708e67308
Kong-hana01 <gksqlsl11@khu.ac.kr>
2022-11-17 21:43:20 +0900

    feat(ValidationType): getErrorMessage 메서드 작성
    - 각 인스턴스별 에러메세지 작성

--- src/main/java/bridge/input/ValidationType.java
+++ src/main/java/bridge/input/ValidationType.java
@@ -8,14 +8,16 @@ import java.util.stream.IntStream;
 public enum ValidationType {
     CHECK_SIZE(Arrays.stream(IntStream.rangeClosed(3, 20).toArray())
             .boxed().map(String::valueOf)
-            .collect(Collectors.toList())),
-    CHECK_MOVING(List.of("U", "D")),
-    CHECK_GAME_COMMAND(List.of("R", "Q"));
+            .collect(Collectors.toList()), "[ERROR] 다리 길이는 3부터 20 사이의 숫자여야 합니다."),
+    CHECK_MOVING(List.of("U", "D"), "[ERROR] U나 D를 입력해주시기 바랍니다."),
+    CHECK_GAME_COMMAND(List.of("R", "Q"), "[ERROR] R이나 Q를 입력해주시기 바랍니다.");
 
     private List<String> validationRange;
+    private String errorMessage;
 
-    ValidationType(List<String> validationRange) {
+    ValidationType(List<String> validationRange, String errorMessage) {
         this.validationRange = validationRange;
+        this.errorMessage = errorMessage;
     }
 
     private List<String> getValidationRange() {
@@ -24,5 +26,9 @@ public enum ValidationType {
 
     public static boolean validate(ValidationType validationType, String input) {
         return validationType.getValidationRange().contains(input);
+    }
+
+    public String getErrorMessage() {
+        return errorMessage;
     }
 }
