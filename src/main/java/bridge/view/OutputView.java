14d9aaa2c8497f20b841062cfdec075d781e70e1
inhwanK <inhan1009@naver.com>
2022-11-17 21:25:47 +0900

    feat(OutputView): 게임 진행 상황 출력 메서드 추가
    시작 문구 추가
    다리 길이 입력 문구 추가
    이동할 칸 선택 문구 추가
    시도 횟수 문구 추가

--- src/main/java/bridge/view/OutputView.java
+++ src/main/java/bridge/view/OutputView.java
@@ -24,4 +24,20 @@ public class OutputView {
     public void printStartMessage() {
         System.out.println("다리 건너기 게임을 시작합니다.");
     }
+
+    public void printBridgeLengthInputRequestMessage() {
+        System.out.println("다리의 길이를 입력해주세요.");
+    }
+
+    public void printMoveBlockInputRequestMessage() {
+        System.out.println("이동할 칸을 선택해주세요. (위: U, 아래:D)");
+    }
+
+    public void printChoseRetryInputRequestMessage() {
+        System.out.println("게임을 다시 시도할지 여부를 입력해주세요.(재시도: R, 종료:Q)");
+    }
+
+    public void printNumberOfTries() {
+        System.out.println("총 시도한 횟수: 2");
+    }
 }
