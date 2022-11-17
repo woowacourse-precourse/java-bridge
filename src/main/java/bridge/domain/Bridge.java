9bfccb2c45f7184271fb6e329f42d5ed544fbad6
drunkenhw <drunkenhw@gmail.com>
2022-11-17 19:20:51 +0900

    feat(Bridge): 다리 생성 시 길이에 따른 예외 발생 추가

---
+++ src/main/java/bridge/domain/Bridge.java
@@ -0,0 +1,25 @@
+package bridge.domain;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Bridge {
+    private List<BridgeDirection> directions;
+
+    public Bridge(List<String> directionWords) {
+        validateSize(directionWords);
+        this.directions = getBridgeDirections(directionWords);
+    }
+
+    private void validateSize(List<String> location) {
+        if (location.size() < 3 || location.size() > 20) {
+            throw new IllegalArgumentException("다리의 길이는 3 ~ 20 사이의 숫자만 가능합니다.");
+        }
+    }
+
+    private List<BridgeDirection> getBridgeDirections(List<String> directionWords) {
+        return directionWords.stream()
+                .map(BridgeDirection::wordToDirection)
+                .collect(Collectors.toList());
+    }
+}
