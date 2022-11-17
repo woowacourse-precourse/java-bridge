cdd52984f03760445dc997f701af7ed92ba60668
JeongHunHui <deca3248@gmail.com>
2022-11-17 17:40:39 +0900

    feat: 사용자에게 다리 길이를 입력받고, 잘못된 값을 입력 했을때 예외메세지를 출력하는 기능 구현

--- src/main/java/bridge/BridgeGame.java
+++ src/main/java/bridge/BridgeGame.java
@@ -1,14 +1,27 @@
 package bridge;
 
+import java.util.List;
+
+import static bridge.InputView.readBridgeSize;
 import static bridge.OutputView.*;
 
 /** 다리 건너기 게임을 관리하는 클래스 */
 public class BridgeGame {
+  private final BridgeMaker bridgeMaker;
+
+  public BridgeGame() {
+    this.bridgeMaker = new BridgeMaker(new BridgeRandomNumberGenerator());
+  }
 
   public void run() {
     printGameStartMessage();
-    printLengthInputMessage();
+    try{
+      List<String> bridge = makeBridgeByLengthInput();
+    }catch (IllegalArgumentException e) {
+      printErrorMessage(e);
+    }
   }
+
 
   /**
    * 사용자가 칸을 이동할 때 사용하는 메서드
@@ -23,4 +36,10 @@ public class BridgeGame {
    * <p>재시작을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
    */
   public void retry() {}
+
+  private List<String> makeBridgeByLengthInput(){
+    printBlankLine();
+    printLengthInputMessage();
+    return bridgeMaker.makeBridge(readBridgeSize());
+  }
 }
