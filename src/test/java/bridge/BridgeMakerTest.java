5241e810cef36cf6032e2f76bb2d8f784155bde7
woosung1223 <dntjd991223@naver.com>
2022-11-17 20:32:21 +0900

    refactor(test): 사용하지 않는 import문 제거

--- src/test/java/bridge/BridgeMakerTest.java
+++ src/test/java/bridge/BridgeMakerTest.java
@@ -1,21 +1,17 @@
 package bridge;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.util.Lists.newArrayList;
+
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
-import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.MethodSource;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Stream;
-
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.util.Lists.newArrayList;
-import static org.junit.jupiter.api.Assertions.*;
 
 class BridgeMakerTest {
 
