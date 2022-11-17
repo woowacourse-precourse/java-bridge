7ea056b3c5356c385f1b180d95e57403c7d3db78
HyeonJun0530 <nexus2697@pukyong.ac.kr>
2022-11-17 23:56:31 +0900

    fix(InputView): 출력 메시지 추가

--- src/main/java/bridge/view/InputView.java
+++ src/main/java/bridge/view/InputView.java
@@ -30,6 +30,7 @@ public class InputView {
      * 사용자가 이동할 칸을 입력받는다.
      */
     public String readMoving() {
+        System.out.println("이동할 칸을 선택해주세요. (위: U, 아래: D)");
         String inputMove = Console.readLine();
 
         moveValidateHandler(inputMove);
@@ -42,6 +43,7 @@ public class InputView {
      * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
      */
     public String readGameCommand() {
+        System.out.println("게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)");
         String restartInput = Console.readLine();
 
         commendValidateHandler(restartInput);
