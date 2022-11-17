17e3d6c6beb8a2d7a5721a902d9ba7c3a0bec62e
ksh-beginner <ohk9134@naver.com>
2022-11-17 17:41:29 +0900

    feat(GeneratedBridge) : 생성된 다리 관련 로직을 가지는 Model 객체 생성
    생성될 때 생성된 다리를 파라미터로 받아서 저장

---
+++ src/main/java/bridge/model/GeneratedBridge.java
@@ -0,0 +1,12 @@
+package bridge.model;
+
+import java.util.List;
+
+public class GeneratedBridge {
+
+    private final List<String> shape;
+
+    public GeneratedBridge(List<String> bridgeShape) {
+        this.shape = bridgeShape;
+    }
+}
