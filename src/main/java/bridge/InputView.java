9fb21c0f60d7f62f9a99a4304fc82853e29f48cc
jjongwa <troas96@naver.com>
2022-11-17 14:23:10 +0900

    feat: 다리의 길이 입력받는 기능
    입력값에 대한 예외처리 함수 추가
    입력값이 숫자인지 - checkSizeIsNumber()
    입력값이 3에서 20 사이인지 - checkSizeIsInRange()
    입력값이 0으로 시작하는 숫자 예외리 - checkFirstNumberIsZero()

--- src/main/java/bridge/InputView.java
+++ src/main/java/bridge/InputView.java
@@ -1,5 +1,7 @@
 package bridge;
 
+import camp.nextstep.edu.missionutils.Console;
+
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
  */
@@ -9,8 +11,32 @@ public class InputView {
      * 다리의 길이를 입력받는다.
      */
     public int readBridgeSize() {
-        return 0;
+        String input = Console.readLine();
+        int inputNum = checkSizeIsNumber(input);
+        checkSizeIsInRange(inputNum);
+        checkFirstNumberIsZero(input);
+        return inputNum;
     }
+
+    private int checkSizeIsNumber(String input) {
+        try {
+            return Integer.parseInt(input);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("다리의 길이는 숫자로 입력해 주어야 합니다.");
+        }
+    }
+
+    private void checkSizeIsInRange(int inputNum) {
+        if (inputNum < 3 || inputNum > 20) {
+            throw new IllegalArgumentException("다리의 길이는 3에서 20 사이의 숫자입니다.");
+        }
+    }
+    private void checkFirstNumberIsZero(String input) {
+        if (input.charAt(0) == '0') {
+            throw new IllegalArgumentException("0으로 시작하는 숫자는 옳은 입력방식이 아닙니다.");
+        }
+    }
+
 
     /**
      * 사용자가 이동할 칸을 입력받는다.
