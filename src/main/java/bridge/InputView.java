76f586794a2e01fac2e674a72ef183586abbaf7b
YuHayeon <youha0930@gmail.com>
2022-11-18 01:35:06 +0900

    feat(InputView): 사용자의 게임 재시도 여부 입력
    사용자가 게임을 다시 시작하면 R, 종료하면 Q를 입력받는다.

--- src/main/java/bridge/InputView.java
+++ src/main/java/bridge/InputView.java
@@ -9,6 +9,7 @@ public class InputView {
 
     private static final String BRIDGE_SIZE_PRINT = "다리의 길이를 입력해주세요.";
     private static final String MOVING_PRINT = "이동할 칸을 선택해주세요. (위: U, 아래: D)";
+    private static final String GAME_COMMAND_PRINT = "게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)";
 
     /**
      * 다리의 길이를 입력받는다.
@@ -30,6 +31,7 @@ public class InputView {
      * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
      */
     public String readGameCommand() {
-        return null;
+        System.out.println(GAME_COMMAND_PRINT);
+        return Console.readLine();
     }
 }
