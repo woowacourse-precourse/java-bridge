a46d3c52ab0419904cb58a7ec1c585acd32d8d28
hojunking96 <junsong96@naver.com>
2022-11-17 13:12:20 +0900

    feats(inputView.java) : 게임 재시도 여부 입력 기능 구현

--- src/main/java/bridge/InputView.java
+++ src/main/java/bridge/InputView.java
@@ -28,7 +28,7 @@ public class InputView {
      */
     public String readMoving() {
         /*
-        "이동할 칸을 선택해주세요."
+        "이동할 칸을 선택해주세요. (위: U, 아래: D)"
          */
         String movingInput = Console.readLine();
         /*
@@ -41,6 +41,13 @@ public class InputView {
      * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
      */
     public String readGameCommand() {
-        return null;
+        /*
+        "게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)"
+         */
+        String gameCommandInput = Console.readLine();
+        /*
+        입력이 유효한지 확인
+         */
+        return gameCommandInput;
     }
 }
