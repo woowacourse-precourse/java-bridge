74efb0b88b53e601402d237f016647904410c053
swandevson <shai4956@gmail.com>
2022-11-17 14:44:49 +0900

    Add(BridgeBlcok): UP, DOWN 상수에 대한 enum 클래스 생성
    - 문자열, 숫자 값으로 구성

---
+++ src/test/java/bridge/Domain/BridgeBlockTest.java
@@ -0,0 +1,26 @@
+package bridge.Domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.EnumSource;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+class BridgeBlockTest {
+
+    @DisplayName("생성 번호에 따른 올바른 문자 반환하는지 테스트")
+    @Nested
+    class StringReturnTest {
+        @Test
+        void 반환_올바른_문자_UP() {
+            assertThat(BridgeBlock.getBlockIdentifier(0)).isEqualTo("U");
+        }
+        @Test
+        void 반환_올바른_문자_DOWN() {
+            assertThat(BridgeBlock.getBlockIdentifier(1)).isEqualTo("D");
+        }
+    }
+}
