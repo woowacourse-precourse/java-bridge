3a94fb6294b44a6072d92e7962919e354d35e048
twoosky <lhjae3@naver.com>
2022-11-18 01:57:06 +0900

    feat(Result): 결과 객체 생성

---
+++ src/main/java/bridge/domain/Result.java
@@ -0,0 +1,11 @@
+package bridge.domain;
+
+import java.util.Map;
+
+public class Result {
+    private final Map<String, String> result;
+
+    public Result(Map<String, String> result) {
+        this.result = result;
+    }
+}
