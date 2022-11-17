21182dbf49a5085a20a9d08700a998b33e583fae
abbd2 <ajkety00@gmail.com>
2022-11-17 23:45:17 +0900

    Feat(InputView): 다리의 길이를 조건에 맞게 입력 받고, 조건에 맞지 않으면 오류 메세지를 출력하고 다시 입력받는 readBridgeSize 메서드 구현

--- src/main/java/bridge/InputView.java
+++ src/main/java/bridge/InputView.java
@@ -2,6 +2,8 @@ package bridge;
 
 import camp.nextstep.edu.missionutils.Console;
 
+import java.io.PrintStream;
+
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
  */
@@ -11,7 +13,18 @@ public class InputView {
      * 다리의 길이를 입력받는다.
      */
     public int readBridgeSize() {
-        return 0;
+        int bridgeSize = 0;
+        while (true) {
+            try {
+                bridgeSize = Integer.parseInt(readValue());
+                if(isBridgeNumber(bridgeSize)){
+                    break;
+                }
+            } catch (NumberFormatException e) {
+                System.out.println("[ERROR] 3 이상 20 이하의 숫자를 입력해주세요.");
+            }
+        }
+        return bridgeSize;
     }
 
     /**
@@ -28,8 +41,15 @@ public class InputView {
         return null;
     }
 
-    public String readValue(){
+    public String readValue() {
         String value = Console.readLine();
         return value;
+    }
+
+    public boolean isBridgeNumber(int bridgeSize){
+        if(bridgeSize >= 3 && bridgeSize <= 20)
+            return true;
+        System.out.println("[ERROR] 3 이상 20 이하의 숫자를 입력해주세요.");
+        return false;
     }
 }
