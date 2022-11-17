e7df79c5bdd780b067656d3c4687dddcac3ebb3a
Park shinyeong <syngh503@gmail.com>
2022-11-17 23:36:47 +0900

    Feat(Bridge): 다리 관련 클래스 생성
    다리를 저장하는 클래스를 생성하였습니다.

--- src/main/java/bridge/Domain/Bridge.java
+++ src/main/java/bridge/Domain/Bridge.java
@@ -1,4 +1,11 @@
 package bridge.Domain;
 
+import java.util.List;
+
 public class Bridge {
+    private final List<String> bridge;
+
+    public Bridge(List<String> bridge) {
+        this.bridge = bridge;
+    }
 }
