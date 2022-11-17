a993a81ed62433f43e21c9c915d9f5f781ffef52
JJ <jeongjae1996@naver.com>
2022-11-18 03:43:51 +0900

    feat: 이동할 칸 입력 후, 올바른 값이 입력되었는지 확인하기 위한 기능 구현
    - U 또는 D를 제외한 값이 들어오면 IllegalArgumentException이 발생합니다.

--- src/main/java/bridge/Validate.java
+++ src/main/java/bridge/Validate.java
@@ -13,4 +13,15 @@ public class Validate {
             throw new IllegalArgumentException(EXCEPTION_MESSAGE);
         }
     }
+
+    public static void moving(String input) {
+        final String INPUT_PATTERN = "^[UD]$";
+        final String EXCEPTION_MESSAGE = "[ERROR] U 또는 D를 제외한 문자가 입력되었습니다.";
+
+        boolean isMatch = Pattern.matches(INPUT_PATTERN, input);
+
+        if (!isMatch) {
+            throw new IllegalArgumentException(EXCEPTION_MESSAGE);
+        }
+    }
 }
