877711b78bae159ada9458d857c95413e33c965d
yeonhee <ydol2@naver.com>
2022-11-17 20:34:59 +0900

    refactor(Application) : application에서의 예외 처리 제거

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -3,13 +3,8 @@ package bridge;
 import bridge.controller.BridgeGameController;
 
 public class Application {
-    private static final String ERROR = "[ERROR]";
     public static void main(String[] args) {
         BridgeGameController bridgeGameController = BridgeGameController.create();
-        try{
-            bridgeGameController.start();
-        } catch (IllegalArgumentException e){
-            System.out.println(ERROR + e.getMessage());
-        }
+        bridgeGameController.start();
     }
 }
