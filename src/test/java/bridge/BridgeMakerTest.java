9684c12f0dfa192eef50b9ea81ffa01cf6f1bd1f
swonny <wonny921@gmail.com>
2022-11-17 23:24:25 +0900

    test(BridgeMakerTest): 생성한 다리 길이 테스트, 칸 포함 테스트 추가

--- src/test/java/bridge/BridgeMakerTest.java
+++ src/test/java/bridge/BridgeMakerTest.java
@@ -1,2 +1,24 @@
-package bridge;public class BridgeMakerTest {
+package bridge;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+public class BridgeMakerTest {
+    @DisplayName("다리 생성 테스트")
+    @Test
+    void 생성한_다리_길이_테스트() {
+        BridgeMaker bridgeMaker = new BridgeMaker(new BridgeRandomNumberGenerator());
+        assertThat(bridgeMaker.makeBridge(3).size())
+                .isEqualTo(3);
+    }
+
+    @DisplayName("다리 이용 가능한 칸 포함 테스트")
+    @Test
+    void 다리_위치_포함_테스트() {
+        BridgeMaker bridgeMaker = new BridgeMaker(new BridgeRandomNumberGenerator());
+        assertThat(bridgeMaker.makeBridge(3).toString())
+                .contains("D", "U");
+    }
 }
