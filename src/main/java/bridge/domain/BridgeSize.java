7e6c93544363c4892c4dbcdc79c37082cf8f36c2
EUNJEONGMUN <munej26@gmail.com>
2022-11-17 15:28:03 +0900

    feat: 다리 길이 입력 에러 처리 구현

--- src/main/java/bridge/domain/BridgeSize.java
+++ src/main/java/bridge/domain/BridgeSize.java
@@ -1,9 +1,28 @@
 package bridge.domain;
 
+import bridge.constant.Constant;
+import bridge.constant.ErrorMessage;
+
 public class BridgeSize {
     private final int bridgeSize;
 
     public BridgeSize(String bridgeSize) {
+        validBridgeSize(bridgeSize);
         this.bridgeSize = Integer.parseInt(bridgeSize);
+    }
+
+    private void validBridgeSize(String bridgeSize) {
+        validIsDigit(bridgeSize);
+        validBridgeSizeRange(Integer.parseInt(bridgeSize));
+    }
+    private void validIsDigit(String bridgeSize) {
+        if (!bridgeSize.chars().allMatch(Character::isDigit)) {
+            throw new IllegalArgumentException(ErrorMessage.NOT_NUMBER.getMessage());
+        }
+    }
+    private void validBridgeSizeRange(int bridgeSize) {
+        if (bridgeSize< Constant.BRIDGE_SIZE_MIN || bridgeSize>Constant.BRIDGE_SIZE_MAX) {
+            throw new IllegalArgumentException(ErrorMessage.NOT_BRIDGE_SIZE_RANGE.getMessage());
+        }
     }
 }
