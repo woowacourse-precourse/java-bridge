61b78ec11512b8a54d7c4e3daa8bb9c3c7732da6
jeongjihun <wlgns2234@naver.com>
2022-11-18 01:44:04 +0900

    feat(constant): 출력메세지 구현
    현재까지 이동한 다리상태 출력(미완성)

--- src/main/java/bridge/view/OutputView.java
+++ src/main/java/bridge/view/OutputView.java
@@ -1,5 +1,7 @@
 package bridge.view;
 
+import bridge.constant.BridgeGameInfo;
+
 /**
  * 사용자에게 게임 진행 상황과 결과를 출력하는 역할을 한다.
  */
@@ -11,6 +13,7 @@ public class OutputView {
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
     public void printMap() {
+        System.out.println("현재 이동한 다리 상태 출력(미완성)");
     }
 
     /**
@@ -18,17 +21,22 @@ public class OutputView {
      * <p>
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void printStart() {
+    public void printResult() {
+        System.out.println(BridgeGameInfo.RESULT);
     }
 
-    public void printResult() {
+    // 게임시작 메세지 출력
+    public static void printStart() {
+        System.out.println(BridgeGameInfo.START.message);
     }
 
     // 시도 횟수 출력
     public void printTryNumber() {
+        System.out.println(BridgeGameInfo.TOTAL_TRY.message);
     }
 
     // 성공 여부 출력
     public void printSuccessFailure() {
+        System.out.println(BridgeGameInfo.SUCSSES_FAILURE);
     }
 }
