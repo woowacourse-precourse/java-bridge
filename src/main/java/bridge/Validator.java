07ad47765c8e6ae682be81749e97e651cf39ad99
SuhHyeonjun <dmsgktn0208@gmail.com>
2022-11-17 13:50:37 +0900

    feat : 다리 길이를 입력하지 않을 시 예외 발생

--- src/main/java/bridge/Validator.java
+++ src/main/java/bridge/Validator.java
@@ -1,9 +1,40 @@
 package bridge;
 
+import static bridge.InputView.inputBridgeSize;
+
 public class Validator {
+
+    /**
+     * 다리 길이의 유효성 검사
+     */
     public static void validateInputSize(int bridgeSize) {
-        if (bridgeSize < 3 || bridgeSize > 20) {
-            throw new IllegalArgumentException(ErrorMessage.BRIDGE_SIZE_INPUT_RANGE.getErrorMessage());
+        try {
+            if (bridgeSize < 3 || bridgeSize > 20) {
+                throw new IllegalArgumentException();
+            }
+        } catch (IllegalArgumentException e) {
+            System.out.println(ErrorMessage.BRIDGE_SIZE_INPUT_RANGE.getErrorMessage());
+            inputBridgeSize();
+        }
+    }
+
+    public static void validateInputType(String inputSize) {
+        try {
+            Integer.parseInt(inputSize);
+        } catch (NumberFormatException e) {
+            System.out.println(ErrorMessage.BRIDGE_SIZE_INPUT_TYPE.getErrorMessage());
+            inputBridgeSize();
+        }
+    }
+
+    public static void validateInputNull(String inputSize) {
+        try {
+            if (inputSize.length() == 0) {
+                throw new NullPointerException();
+            }
+        } catch (NullPointerException e) {
+            System.out.println(ErrorMessage.INPUT_NULL.getErrorMessage());
+            inputBridgeSize();
         }
     }
 }
