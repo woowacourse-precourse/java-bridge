de8b207a953dbd4003bf185215a0fc7bdd2fbcd3
HyeonjungYun <guswnd9971@gmail.com>
2022-11-17 21:06:11 +0900

    "test(BridgeGameTest): 테스트 클래스 추가"

---
+++ src/test/java/bridge/BridgeGameTest.java
@@ -0,0 +1,55 @@
+package bridge;
+
+import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class BridgeGameTest{
+
+    @Test
+    void 칸_이동_확인 () {
+        List<String> randomBridge = List.of("U", "D", "U", "U");
+        BridgeGame bridgeGame = new BridgeGame(randomBridge);
+
+        assertThat(bridgeGame.move("U")).isEqualTo(true);
+    }
+
+    @Test
+    void 칸_이동_확인2 () {
+        List<String> randomBridge = List.of("U", "D", "U", "U");
+        BridgeGame bridgeGame = new BridgeGame(randomBridge);
+        bridgeGame.passHistory = new ArrayList<>(List.of("U", "D", "U"));
+
+        assertThat(bridgeGame.move("U")).isEqualTo(false);
+    }
+
+    @Test
+    void 칸_이동_실패_후_종료 () {
+        List<String> randomBridge = List.of("U", "D", "U", "U");
+        BridgeGame bridgeGame = new BridgeGame(randomBridge);
+        bridgeGame.passHistory = new ArrayList<>(List.of("U", "D", "U"));
+
+        String command = "Q";
+        InputStream in = new ByteArrayInputStream(command.getBytes());
+        System.setIn(in);
+
+        assertThat(bridgeGame.move("D")).isEqualTo(false);
+    }
+
+    @Test
+    void 칸_이동_실패_후_재시작() {
+        List<String> randomBridge = List.of("U", "D", "U", "U");
+        BridgeGame bridgeGame = new BridgeGame(randomBridge);
+        bridgeGame.passHistory = new ArrayList<>(List.of("U", "D", "U"));
+
+        String command = "R";
+        InputStream in = new ByteArrayInputStream(command.getBytes());
+        System.setIn(in);
+
+        assertThat(bridgeGame.move("D")).isEqualTo(true);
+    }
+}
