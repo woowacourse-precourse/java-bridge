1df822785a2af04ffebc095bd14811227d5e1e94
K-jun98 <alap_u@naver.com>
2022-11-18 01:56:42 +0900

    refactor - 다리의 길이를 입력받고 예외가 발생시 재입력 받는다.

--- src/main/java/bridge/view/InputView.java
+++ src/main/java/bridge/view/InputView.java
@@ -1,6 +1,5 @@
 package bridge.view;
 
-import bridge.Controller;
 import bridge.domain.Direction;
 import camp.nextstep.edu.missionutils.Console;
 
@@ -9,14 +8,24 @@ import camp.nextstep.edu.missionutils.Console;
  */
 public class InputView {
 
+
     private static final String REQUEST_INPUT_BRIDGE_LENGTH = "다리의 길이를 입력해주세요.";
+    private static final int MIN_BRIDGE_LENGTH = 3;
+    private static final int MAX_BRIDGE_LENGTH = 20;
 
     /**
      * 다리의 길이를 입력받는다.
      */
     public int readBridgeSize() {
         System.out.println(REQUEST_INPUT_BRIDGE_LENGTH);
-        return Integer.parseInt(Console.readLine());
+        try {
+            int size = Integer.parseInt(Console.readLine());
+            validateLength(size);
+            return size;
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+            return readBridgeSize();
+        }
     }
 
     /**
@@ -56,5 +65,11 @@ public class InputView {
             return moving;
         }
         throw new IllegalArgumentException("이동할 칸은 위: U, 아래: D로 입력해야 합니다.");
+    }
+
+    private void validateLength(int length) {
+        if (length < MIN_BRIDGE_LENGTH || MAX_BRIDGE_LENGTH < length) {
+            throw new IllegalArgumentException("다리의 길이는 3 이상 20 이하의 값만 입력 가능합니다.");
+        }
     }
 }
