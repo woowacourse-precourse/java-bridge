9099d1465ae5f26a06b59dfd151b37510ed73727
박시균\psk98 <psg980331@naver.com>
2022-11-17 13:48:38 +0900

    Feat: 다리 길이 입력값이 3~20 사이가 아닌경우 에러메시지 출력

--- src/main/java/bridge/OutputView.java
+++ src/main/java/bridge/OutputView.java
@@ -19,6 +19,10 @@ public class OutputView {
         System.out.println(NOT_NUMBER_BRIDGE_LENGTH_MESSAGE);
     }
 
+    public void printInValidRangeBridgeLengthErrorMessage(){
+        System.out.println(INVALID_RANGE_BRIDGE_LENGTH_MESSAGE);
+    }
+
     /**
      * 현재까지 이동한 다리의 상태를 정해진 형식에 맞춰 출력한다.
      * <p>
