75d7ab042b16c99f0918642484b2bdad17efa2c2
kokodak <kokodakadokok@gmail.com>
2022-11-17 18:15:38 +0900

    feat(inputView): 잘못된 입력을 받으면 다시 입력 받는 기능 구현

--- src/main/java/bridge/view/InputView.java
+++ src/main/java/bridge/view/InputView.java
@@ -8,27 +8,49 @@ import camp.nextstep.edu.missionutils.Console;
  */
 public class InputView {
 
+    private static final String READ_BRIDGE_SIZE = "다리의 길이를 입력해주세요.";
+    private static final String READ_MOVING = "이동할 칸을 선택해주세요. (위: U, 아래: D)";
+    private static final String READ_GAME_COMMAND = "게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)";
+
     /**
      * 다리의 길이를 입력받는다.
      */
-    public int readBridgeSize() throws IllegalArgumentException {
+    public int readBridgeSize() {
+        System.out.println(READ_BRIDGE_SIZE);
         String input = Console.readLine();
-        return InputValidator.validateBridgeSize(input);
+        try {
+            return InputValidator.validateBridgeSize(input);
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+            return readBridgeSize();
+        }
     }
 
     /**
      * 사용자가 이동할 칸을 입력받는다.
      */
-    public String readMoving() throws IllegalArgumentException {
+    public String readMoving() {
+        System.out.println(READ_MOVING);
         String input = Console.readLine();
-        return InputValidator.validateMoving(input);
+        try {
+            return InputValidator.validateMoving(input);
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+            return readMoving();
+        }
     }
 
     /**
      * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
      */
-    public String readGameCommand() throws IllegalArgumentException {
+    public String readGameCommand() {
+        System.out.println(READ_GAME_COMMAND);
         String input = Console.readLine();
-        return InputValidator.validateGameCommand(input);
+        try {
+            return InputValidator.validateGameCommand(input);
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+            return readGameCommand();
+        }
     }
 }
