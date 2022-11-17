74eb4dcf58acc2a9e0a22e52a9f6091874ab0f58
JeongHunHui <deca3248@gmail.com>
2022-11-17 14:49:48 +0900

    feat: 다리 길이의 유효성을 검증하는 클래스 생성

---
+++ src/main/java/bridge/BridgeLengthValidator.java
@@ -0,0 +1,22 @@
+package bridge;
+
+import bridge.exception.LengthOutOfRangeException;
+
+public class BridgeLengthValidator {
+  private static final int MIN_LENGTH = 3;
+  private static final int MAX_LENGTH = 20;
+
+  public static void validate(final int length) {
+    validateLengthInRange(length);
+  }
+
+  private static void validateLengthInRange(final int length) {
+    if (!isLengthInRange(length)) {
+      throw new LengthOutOfRangeException();
+    }
+  }
+
+  private static boolean isLengthInRange(final int length) {
+    return length < MIN_LENGTH || length > MAX_LENGTH;
+  }
+}
