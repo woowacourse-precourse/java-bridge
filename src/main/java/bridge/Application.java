4ad9fd586759d4637a105b561f839dc0d7a2489d
jh868 <2weeks9947@naver.com>
2022-11-17 23:16:01 +0900

    feat: 다리 건너기 입력 구현, 메세지 상수화

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -4,7 +4,7 @@ public class Application {
     static InputView inputView = new InputView();
     static BridgeMaker bridgeMaker = new BridgeMaker(new BridgeRandomNumberGenerator());
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
         bridgeMaker.makeBridge(inputView.readBridgeSize());
+        inputView.readMoving();
     }
 }
--- src/main/java/bridge/InputView.java
+++ src/main/java/bridge/InputView.java
@@ -6,21 +6,37 @@ import camp.nextstep.edu.missionutils.Console;
  * 사용자로부터 입력을 받는 역할을 한다.
  */
 public class InputView {
-    private final String start_input_BridgeSize = "다리 건너기 게임을 시작합니다.\n\n다리의 길이를 입력해주세요.\n";
+    private static final String INPUT_BRIDGE_SIZE = "다리 건너기 게임을 시작합니다.\n\n다리의 길이를 입력해주세요.\n";
+    private static final String READ_MOVE = "\n이동할 칸을 선택해주세요. (위: U, 아래: D)\n";
+    private static final String ERROR_BRIDGE_SIZE = "[ERROR] 다리 길이는 3부터 20 사이의 숫자여야 합니다.";
+    private static final String ERROR_READ_MOVE = "[ERROR] 이동은 U 혹은 D로 입력해주세요.";
 
     /**
      * 다리의 길이를 입력받는다.
      */
     public int readBridgeSize() {
-        System.out.printf(start_input_BridgeSize);
-        return convertToInt_Validate(Console.readLine());
+        System.out.printf(INPUT_BRIDGE_SIZE);
+        try{
+            return convertToInt_Validate(Console.readLine());
+        }
+        catch (IllegalArgumentException e){
+            System.out.println(e.getMessage());
+            return readBridgeSize();
+        }
     }
 
     /**
      * 사용자가 이동할 칸을 입력받는다.
      */
     public String readMoving() {
-        return null;
+        System.out.printf(READ_MOVE);
+        try{
+            return checkUserMove(Console.readLine());
+        }
+        catch (IllegalArgumentException e){
+            System.out.println(e.getMessage());
+            return readMoving();
+        }
     }
 
     /**
@@ -32,11 +48,20 @@ public class InputView {
 
     private int convertToInt_Validate(String input){
         if (!input.matches("^[0-9]*$")){
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException(ERROR_BRIDGE_SIZE);
         }
         if (Integer.parseInt(input) < 3 || Integer.parseInt(input) > 20){
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException(ERROR_BRIDGE_SIZE);
         }
         return Integer.parseInt(input);
+    }
+    private String checkUserMove(String input){
+        if (input.equals("U")){
+            return input;
+        }
+        if (input.equals("D")){
+            return input;
+        }
+        throw new IllegalArgumentException(ERROR_READ_MOVE);
     }
 }
