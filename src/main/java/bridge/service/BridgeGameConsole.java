23cd123bc75fac2097454151506801a5a992db71
JaeWon Choi <87649850+codesver@users.noreply.github.com>
2022-11-18 08:51:43 +0900

    feat(run): implement method
    Run the total game.

--- src/main/java/bridge/service/BridgeGameConsole.java
+++ src/main/java/bridge/service/BridgeGameConsole.java
@@ -1,16 +1,25 @@
 package bridge.service;
 
+import bridge.BridgeMaker;
+import bridge.BridgeNumberGenerator;
+import bridge.BridgeRandomNumberGenerator;
+
+import java.util.List;
+
 public class BridgeGameConsole {
 
     private final Emcee emcee = new Emcee();
     private final Player player = new Player();
+    private final BridgeNumberGenerator generator = new BridgeRandomNumberGenerator();
+    private final BridgeMaker maker = new BridgeMaker(generator);
 
     public void run() {
-        // 게임 시작 안내문을 출력한다.
-        // 다리 길이를 입력 받는다.
-        // 다리 생성
-        // 다리 건너기
-        // 게임 결과 출력
+        emcee.guideGame();
+        int bridgeLength = player.enterBridgeLength();
+        List<String> bridge = maker.makeBridge(bridgeLength);
+        BridgeGame bridgeGame = new BridgeGame(bridge);
+        int attemptCount = playGame(bridgeGame);
+        emcee.showGameResult(bridgeGame.createMovementStatus(), bridgeGame.crossedBridge(), attemptCount);
     }
 
     private int createBridgeLength() {
