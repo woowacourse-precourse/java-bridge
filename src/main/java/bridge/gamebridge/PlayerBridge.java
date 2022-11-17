13df856de6a2c0d9cd9da55d60b7dbde1c6c1072
kimminsu <xjfcnfw3@naver.com>
2022-11-17 21:48:10 +0900

    feat: player가 지나간 위치를 저장하는 객체 생성

---
+++ src/main/java/bridge/gamebridge/PlayerBridge.java
@@ -0,0 +1,15 @@
+package bridge.gamebridge;
+
+import bridge.domain.Bridge;
+
+public class PlayerBridge {
+    private Bridge bridge;
+
+    public Bridge getBridge() {
+        return bridge;
+    }
+
+    public void setBridge(Bridge bridge) {
+        this.bridge = bridge;
+    }
+}
