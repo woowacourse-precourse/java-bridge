34ecfacf9457c0e9eca95d4ac35300ca78994438
making-a-scene <4936joo@naver.com>
2022-11-17 16:56:40 +0900

    refactor: 위 칸과 아래 칸을 나타내는 1과 0을 final로 선언해 사용

--- src/main/java/bridge/Utils.java
+++ src/main/java/bridge/Utils.java
@@ -6,11 +6,14 @@ import java.util.List;
 
 public class Utils {
 
+    private static final int TOP_SPACE = 1;
+    private static final int UNDER_SPACE = 0;
+
     // 게임 시작 전 초기 다리 생성
     public UsersBridgeCrossStatus makeInitialBridge() {
         List<List<String>> newBridge = new ArrayList<>();
-        newBridge.add(0, Arrays.asList("[", " ", " ", "]"));
-        newBridge.add(1, Arrays.asList("[", " ", " ", "]"));
+        newBridge.add(TOP_SPACE, Arrays.asList("[", " ", " ", "]"));
+        newBridge.add(UNDER_SPACE, Arrays.asList("[", " ", " ", "]"));
         return new UsersBridgeCrossStatus(newBridge);
     }
 
