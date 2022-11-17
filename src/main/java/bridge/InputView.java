b90278bf639ec04b8e4e1cb79813f5de13fa333a
소대훈 <theogns0681@naver.com>
2022-11-17 09:51:52 +0900

    [refactor] 문자열 변수로 분리

--- src/main/java/bridge/InputView.java
+++ src/main/java/bridge/InputView.java
@@ -11,6 +11,9 @@ public class InputView {
     final String readBridgeSizeMs = "다리의 길이를 입력해주세요.";
     final String readMovingMs = "이동할 칸을 선택해주세요. (위: U, 아래: D)";
     final String readGameCommandMs = "게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)";
+    final String exceptNumber = "[ERROR] 3이상 20이하의 숫자를 입력해주세요.";
+    final String exceptMoving = "[ERROR] U 혹은 D를 입력해주세요.";
+    final String exceptRetry = "[ERROR] R 혹은 Q를 입력해주세요.";
 
     /**
      * 다리의 길이를 입력받는다.
@@ -25,7 +28,7 @@ public class InputView {
             size = Integer.parseInt(input);
             if (size < 3 || size > 20) throw new IllegalArgumentException();
         } catch (IllegalArgumentException e) {
-            System.out.println("[ERROR] 3이상 20이하의 숫자를 입력해주세요.");
+            System.out.println(exceptNumber);
             size = readBridgeSize();
         }
         return size;
@@ -50,7 +53,7 @@ public class InputView {
             input = Console.readLine().toUpperCase();
             if (!Objects.equals(input, "U") && !Objects.equals(input, "D")) throw new IllegalArgumentException();
         } catch (IllegalArgumentException e) {
-            System.out.println("[ERROR] U 혹은 D를 입력해주세요.");
+            System.out.println(exceptMoving);
             input = readMoving();
         }
         return input;
@@ -66,7 +69,7 @@ public class InputView {
             input = Console.readLine();
             if (!Objects.equals(input, "R") && !Objects.equals(input, "Q")) throw new IllegalArgumentException();
         } catch (IllegalArgumentException e) {
-            System.out.println("[ERROR] R 혹은 Q를 입력해주세요.");
+            System.out.println(exceptRetry);
             input = readGameCommand();
         }
         return input;
