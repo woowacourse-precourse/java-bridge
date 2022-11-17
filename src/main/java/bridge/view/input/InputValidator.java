f08fe7f3395268996448834b26aa5057ef57b4bd
great-park <cksgh1735@naver.com>
2022-11-17 18:18:47 +0900

    feat: 게임 재시작 여부를 입력 받는다.(재시도: R, 종료: Q)

--- src/main/java/bridge/view/input/InputValidator.java
+++ src/main/java/bridge/view/input/InputValidator.java
@@ -9,6 +9,10 @@ public class InputValidator {
     private static final String MOVING_IS_NOT_U_AND_D = "이동할 칸은 U 또는 D로 입력해주세요.";
     private static final String MOVING_UP = "U";
     private static final String MOVING_DOWN = "D";
+    private static final String GAME_COMMAND_LENGTH_IS_NOT_ONE = "게임 재시작 여부는 문자 1개를 입력해주세요.";
+    private static final String RETRY = "R";
+    private static final String END = "Q";
+    private static final String GAME_COMMAND_IS_NOT_R_AND_Q = "게임 재시작 여부는 R 또는 Q로 입력해주세요.";
 
     public void validateBridegeSize(String bridgeSize) {
         if (bridgeSize.chars().allMatch(Character::isDigit)) {
@@ -25,6 +29,15 @@ public class InputValidator {
         }
         if (!moving.equals(MOVING_UP) && !moving.equals(MOVING_DOWN)){
             throw new IllegalArgumentException(MOVING_IS_NOT_U_AND_D);
+        }
+    }
+
+    public void validateGameCommand(String gameCommand) {
+        if (gameCommand.length() != 1){
+            throw new IllegalArgumentException(GAME_COMMAND_LENGTH_IS_NOT_ONE);
+        }
+        if (!gameCommand.equals(RETRY) && !gameCommand.equals(END)){
+            throw new IllegalArgumentException(GAME_COMMAND_IS_NOT_R_AND_Q);
         }
     }
 }
--- src/main/java/bridge/view/input/InputView.java
+++ src/main/java/bridge/view/input/InputView.java
@@ -43,6 +43,9 @@ public class InputView {
      * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
      */
     public String readGameCommand() {
-        return null;
+        outputView.printGameCommandRequestMessage();
+        String gameCommand = Console.readLine();
+        inputValidator.validateGameCommand(gameCommand);
+        return gameCommand;
     }
 }
