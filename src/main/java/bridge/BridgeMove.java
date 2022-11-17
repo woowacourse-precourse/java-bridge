a0608510d9e768080179497fbf27d5fdf112c25d
hoonn9 <hun9935@gmail.com>
2022-11-18 00:55:07 +0900

    refactor(BridgeMove): BridgeDirection을 BridgeMove로 수정

---
+++ src/main/java/bridge/BridgeMove.java
@@ -0,0 +1,49 @@
+package bridge;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+public enum BridgeMove {
+    DOWN("D", 0),
+    UP("U", 1);
+    
+    private String direction;
+    private Integer generateNumber;
+    
+    BridgeMove(String direction, Integer generateNumber) {
+        this.direction = direction;
+        this.generateNumber = generateNumber;
+    }
+    
+    public String getDirection() {
+        return direction;
+    }
+    
+    public static List<String> getDirectionList() {
+        return Arrays.stream(BridgeMove.values()).map((bridgeDirection -> bridgeDirection.getDirection())).collect(
+                Collectors.toList());
+    }
+    
+    /**
+     * @param generateNumber 0 또는 1, 아니면 AssertionError
+     * @return generateNumber에 매치되는 값
+     */
+    public static BridgeMove generateByNumber(Integer generateNumber) {
+        Optional<BridgeMove> bridgeMove = findByGenerateNumber(generateNumber);
+        return bridgeMove.orElseThrow(() -> new AssertionError("다리 생성 무작위 값이 잘못되었씁니다."));
+    }
+    
+    public static Optional<BridgeMove> findByDirection(String direction) {
+        return Arrays.stream(BridgeMove.values())
+                .filter(bridgeDirection -> bridgeDirection.direction.equals(direction))
+                .findAny();
+    }
+    
+    private static Optional<BridgeMove> findByGenerateNumber(Integer generateNumber) {
+        return Arrays.stream(BridgeMove.values())
+                .filter(bridgeDirection -> bridgeDirection.generateNumber.equals(generateNumber))
+                .findAny();
+    }
+}
--- src/test/java/bridge/BridgeMoveTest.java
+++ src/test/java/bridge/BridgeMoveTest.java
@@ -5,26 +5,26 @@ import static org.assertj.core.api.Assertions.*;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-public class BridgeDirectionTest {
+public class BridgeMoveTest {
     
     @DisplayName("gnearateNumber가 0이면 DOWN을 반환한다.")
     @Test
     void generateByNumberForDown() {
-        assertThat(BridgeDirection.generateByNumber(0))
-                .isEqualTo(BridgeDirection.DOWN);
+        assertThat(BridgeMove.generateByNumber(0))
+                .isEqualTo(BridgeMove.DOWN);
     }
     
     @DisplayName("gnearateNumber가 1이면 UP을 반환한다.")
     @Test
     void generateByNumberForUp() {
-        assertThat(BridgeDirection.generateByNumber(1))
-                .isEqualTo(BridgeDirection.UP);
+        assertThat(BridgeMove.generateByNumber(1))
+                .isEqualTo(BridgeMove.UP);
     }
     
     @DisplayName("잘못된 생성 Number를 인수로 실행하면 AssertionError를 발생시킨다.")
     @Test
     void generateByNumberByInvalidGenerateNumber() {
-        assertThatThrownBy(() -> BridgeDirection.generateByNumber(5))
+        assertThatThrownBy(() -> BridgeMove.generateByNumber(5))
                 .isInstanceOf(AssertionError.class);
     }
 }
