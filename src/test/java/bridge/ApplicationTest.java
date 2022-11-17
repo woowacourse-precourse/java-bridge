28296adbf57438d1c4589c700cf0d724c171f61f
Gaby <gaby@Gabyui-MacBookPro.local>
2022-11-17 16:34:05 +0900

    test: more complicated functionality test

--- src/test/java/bridge/ApplicationTest.java
+++ src/test/java/bridge/ApplicationTest.java
@@ -3,15 +3,21 @@ package bridge;
 import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeTest;
 import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.in;
 import static org.assertj.core.util.Lists.newArrayList;
 
 import camp.nextstep.edu.missionutils.test.NsTest;
 import java.util.List;
+import java.util.stream.Stream;
 
+import org.assertj.core.util.Arrays;
+import org.assertj.core.util.Streams;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 class ApplicationTest extends NsTest {
@@ -31,16 +37,29 @@ class ApplicationTest extends NsTest {
         assertRandomNumberInRangeTest(() -> {
             run("3", "U", "D", "U");
             assertThat(output()).contains(
-                "최종 게임 결과",
-                "[ O |   | O ]",
-                "[   | O |   ]",
-                "게임 성공 여부: 성공",
-                "총 시도한 횟수: 1"
+                    "최종 게임 결과",
+                    "[ O |   | O ]",
+                    "[   | O |   ]",
+                    "게임 성공 여부: 성공",
+                    "총 시도한 횟수: 1"
             );
 
             int upSideIndex = output().indexOf("[ O |   | O ]");
             int downSideIndex = output().indexOf("[   | O |   ]");
             assertThat(upSideIndex).isLessThan(downSideIndex);
+        }, 1, 0, 1);
+    }
+
+    @Test
+    void 틀린_움직임_테스트() {
+        assertRandomNumberInRangeTest(() -> {
+            run("3","D", "R", "U", "U", "R", "D", "U");
+            assertThat(output()).contains(
+                    "총 시도한 횟수: 3",
+                    "[ X ]",
+                    "[ O | X ]",
+                    "[   |   ]"
+            );
         }, 1, 0, 1);
     }
 
@@ -55,19 +74,21 @@ class ApplicationTest extends NsTest {
     }
 
     @DisplayName("움직임에 대한 예외처리")
-    @Test
-    void 움직임_예외_테스트() {
+    @ValueSource(strings = {"a", "0", "22"})
+    @ParameterizedTest
+    void 움직임_예외_테스트(String input) {
         assertRandomNumberInRangeTest(() -> {
-            run("3", "123", "U", "D", "U");
+            run("3", input, "U", "D", "U");
             assertThat(output()).contains(ERROR_MESSAGE);
         }, 1,0, 1);
     }
 
     @DisplayName("재시작 대한 예외처리")
-    @Test
-    void 재시작_예외_테스트() {
+    @ValueSource(strings = {"a", "0", "22"})
+    @ParameterizedTest
+    void 재시작_예외_테스트(String input) {
         assertRandomNumberInRangeTest(() -> {
-            run("3", "D", "123", "R", "U", "D", "U");
+            run("3", "D", input, "R", "U", "D", "U");
             assertThat(output()).contains(ERROR_MESSAGE);
         }, 1,0, 1);
     }
