0852aeeca96c5a94dcca147137d970f7b299f12a
HubCreator <chocomilk8604@gmail.com>
2022-11-17 15:03:56 +0900

    feat(Application) : 프로덕션 코드 작성

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -3,6 +3,7 @@ package bridge;
 import bridge.domain.BridgeGame;
 import bridge.domain.BridgeMaker;
 import bridge.view.InputView;
+import bridge.view.OutputView;
 
 import java.util.List;
 
@@ -14,15 +15,22 @@ public class Application {
         BridgeGame bridgeGame = new BridgeGame(bridge);
 
         while (true) {
-            boolean flag = true;
-            while (flag) {
-                bridgeGame.move(InputView.readMoving());
+            int positiion = 0;
+            while (positiion < bridge.size() - 1 && positiion != -1) {
+                positiion = bridgeGame.move(InputView.readMoving());
+                OutputView.printMap(bridgeGame);
             }
-            String command = InputView.readGameCommand();
-            if (command.equals("Q")) {
-                System.out.println(bridgeGame.printResult());
-                break;
+            if (positiion == -1) {
+                String command = InputView.readGameCommand();
+                if (command.equals("Q")) {
+                    OutputView.printResult(bridgeGame);
+                    break;
+                }
+                bridgeGame.retry();
+                continue;
             }
+            OutputView.printResult(bridgeGame);
+            break;
         }
     }
 }
