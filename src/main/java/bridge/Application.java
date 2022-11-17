0f51839968d3bc53c804cf7144a1e4f358a5395b
소대훈 <theogns0681@naver.com>
2022-11-17 09:42:31 +0900

    [refactor] 시작출력 추가

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -9,6 +9,7 @@ public class Application {
 
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        System.out.println("다리 건너기 게임을 시작합니다.\n");
         int bridgeSize = inputView.readBridgeSize();
         List<String> bridgeList = bridgeMaker.makeBridge(bridgeSize);
         bridgeGame.move(0, bridgeList, 0, "");
