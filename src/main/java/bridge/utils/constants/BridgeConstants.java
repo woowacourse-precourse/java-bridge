8a3872b3cdcd920b535870e882f045970985fc77
박승민 <tmddudals369@naver.com>
2022-11-18 02:48:00 +0900

    feat: 입출력에 필요한 상수 정의

---
+++ src/main/java/bridge/utils/constants/BridgeConstants.java
@@ -0,0 +1,12 @@
+package bridge.utils.constants;
+
+public class BridgeConstants {
+
+    public static final String BRIDGE_START_POINT = "[ ";
+    public static final String BRIDGE_DIVIDING_POINT = " | ";
+    public static final String BRIDGE_END_POINT = " ]";
+    public static final String CORRECT_ANSWER = "O";
+    public static final String INCORRECT_ANSWER = "X";
+    public static final String INPUT_BRIDGE_LENGTH = "\n다리의 길이를 입력해주세요.";
+    public static final String INPUT_BRIDGE_MOVE_OPTION = "\n이동할 칸을 선택해주세요. (위: U, 아래: D)";
+}
---
+++ src/main/java/bridge/utils/constants/GameConstants.java
@@ -0,0 +1,11 @@
+package bridge.utils.constants;
+
+public class GameConstants {
+
+    public static final String START_GAME = "다리 건너기 게임을 시작합니다.";
+    public static final String RESTART_GAME_OPTION = "\n게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)";
+    public static final String GAME_RESULT = "최종 게임 결과";
+    public static final String SUCCESS_GAME = "\n게임 성공 여부: 성공";
+    public static final String FAIL_GAME = "\n게임 성공 여부: 실패";
+    public static final String GAME_TRY_COUNT = "총 시도한 횟수: %d";
+}
