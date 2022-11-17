67b0175825cc3dba00a191afa317974b1c9278e5
yumyeonghan <mgtmh991013@gmail.com>
2022-11-17 15:49:44 +0900

    feat: Add bridgeLengthValidation function
    A function that returns false if the bridge length is not greater than 3 and less than 20

--- src/main/java/exception/Exception.java
+++ src/main/java/exception/Exception.java
@@ -1,4 +1,13 @@
 package exception;
 
+import java.util.List;
+
 public class Exception {
+
+    public static boolean bridgeLengthValidation(List<String> bridge) {
+        if (!(3 <= bridge.size() && bridge.size() <= 20)) {
+            return false;
+        }
+        return true;
+    }
 }
