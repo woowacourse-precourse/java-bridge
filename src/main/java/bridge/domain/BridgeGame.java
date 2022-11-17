6ba3f017a47dbd1af564a296aab3ac0ae41ed73c
HyeonJun0530 <nexus2697@pukyong.ac.kr>
2022-11-17 22:37:01 +0900

    feat(BridgeGame): 칸 이동, 재시작 로직 구현

--- src/main/java/bridge/domain/BridgeGame.java
+++ src/main/java/bridge/domain/BridgeGame.java
@@ -1,16 +1,27 @@
 package bridge.domain;
 
+
+
+import java.util.List;
+
+
 /**
  * 다리 건너기 게임을 관리하는 클래스
  */
 public class BridgeGame {
+
+    private final static String RESTART_MSG = "R";
 
     /**
      * 사용자가 칸을 이동할 때 사용하는 메서드
      * <p>
      * 이동을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void move() {
+    public boolean move(String moveInput, List<String> bridge, int index) {
+        if(moveInput.equals(bridge.get(index)))
+            return true;
+
+        return false;
     }
 
     /**
@@ -18,6 +29,10 @@ public class BridgeGame {
      * <p>
      * 재시작을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void retry() {
+    public boolean retry(String restartInput) {
+        if(restartInput.equals(RESTART_MSG))
+            return true;
+
+        return false;
     }
 }
