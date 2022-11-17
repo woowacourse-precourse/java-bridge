ad25ee150168190ff685cfcad2d21e0a1db358c4
yeonsu <jack0810@kookmin.ac.kr>
2022-11-17 15:48:40 +0900

    feat($BridgeMaker) 다리 생성함수 추가 (src/main/java/bridge/BridgeMaker.java)

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -4,12 +4,15 @@ public class Application {
 
     public static void main(String[] args) {
         // TODO: 프로그램 구현
-        InputView inputView = new InputView();
-        try{
-            inputView.readBridgeSize();
-        }catch (IllegalArgumentException e){
-            System.out.println("[ERROR] : " + e.getMessage());
-        }
+//        InputView inputView = new InputView();
+//        try{
+//            inputView.readBridgeSize();
+//        }catch (IllegalArgumentException e){
+//            System.out.println("[ERROR] : " + e.getMessage());
+//        }
 
+        BridgeNumberGenerator bridgeNumberGenerator = null;
+        BridgeMaker bridgeMaker = new BridgeMaker(null);
+        System.out.println(bridgeMaker.makeBridge(3));
     }
 }
--- src/main/java/bridge/InputView.java
+++ src/main/java/bridge/InputView.java
@@ -29,7 +29,7 @@ public class InputView {
     /**
      * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
      */
-    public String readGameCommand() {r
+    public String readGameCommand() {
         String inputCommand = Console.readLine();
         ValidationCheck.validateCommandType(inputCommand);
         return inputCommand;
