7d4a8f38c4ff25782558796f037a8275c332085e
zcae1020 <zcae1020@naver.com>
2022-11-17 19:04:24 +0900

    feat: gameSimulator의 field 추가

--- src/main/java/bridge/GameSimulator.java
+++ src/main/java/bridge/GameSimulator.java
@@ -2,6 +2,14 @@ package bridge;
 
 public class GameSimulator {
 
+    private InputView inputView = new InputView();
+    private OutputView outputView = new OutputView();
+    private BridgeGame bridgeGame = new BridgeGame();
+
+    private int trial = 1;
+    private int bridgeSize;
+    private String route = "";
+
     public void simulateGame() {
 
     }
