ed11857c006384b32a8c7936c1feccc982df1cbd
dooboocookie <bkh3030@gmail.com>
2022-11-17 15:36:28 +0900

    feat: 다리 길이가 주어졌을 때 랜덤한 다리를 생성하는 생성자 추가

--- src/main/java/bridge/model/Bridge.java
+++ src/main/java/bridge/model/Bridge.java
@@ -1,12 +1,18 @@
 package bridge.model;
 
+import bridge.BridgeMaker;
+import bridge.BridgeRandomNumberGenerator;
+
 import java.util.List;
 
 public class Bridge {
     private final List<String> bridge;
 
-    public Bridge(List<String> bridge) {
-        this.bridge = bridge;
+    public Bridge(int bridgeLength) {
+        BridgeRandomNumberGenerator bridgeRandomNumberGenerator = new BridgeRandomNumberGenerator();
+        BridgeMaker bridgeMaker = new BridgeMaker(bridgeRandomNumberGenerator);
+        List<String> makeBridge = bridgeMaker.makeBridge(bridgeLength);
+        this.bridge = makeBridge;
     }
 
     public boolean possibleNextStep() {
