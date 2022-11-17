434974bb004bab72ef65c2679d7d9d54f326d87d
ddubidubap <park62000@gmail.com>
2022-11-18 01:23:17 +0900

    feat: 다리 길이 입력하라는 문구 출력하기

--- src/main/java/bridge/view/OutputView.java
+++ src/main/java/bridge/view/OutputView.java
@@ -2,11 +2,16 @@ package bridge.view;
 
 public class OutputView {
     static final String START_GAME = "다리 건너기 게임을 시작합니다.";
+    static final String INPUT_BRIDGE_SIZE = "다리의 길이를 입력해주세요.";
 
     public void printStart() {
         System.out.println(START_GAME);
     }
-    
+
+    public void printInputBridgeSize() {
+        System.out.println(INPUT_BRIDGE_SIZE);
+    }
+
     public void printMap() {
     }
 
