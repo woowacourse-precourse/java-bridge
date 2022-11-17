6d4ded0708bce8adf13b48df4ad70abe2349f9e1
Hayoon <hy97@sju.ac.kr>
2022-11-18 01:07:24 +0900

    feat(ErrorView): 예외처리 Enum 종류 추가 생성

--- src/main/java/bridge/view/ErrorView.java
+++ src/main/java/bridge/view/ErrorView.java
@@ -1,9 +1,11 @@
 package bridge.view;
 
 public enum ErrorView {
-    INVALID_BRIDGE_LENGTH("다리 길이는 3부터 20 사이의 숫자여야 합니다.");
+    INVALID_BRIDGE_LENGTH("다리 길이는 3부터 20 사이의 숫자여야 합니다."),
+    INVALID_MOVING_DIRECTION("다리 길이는 3부터 20 사이의 숫자여야 합니다.");
 
     private final String message;
+    public static final String error = "[ERROR] ";
 
     ErrorView(String message) {
         this.message = message;
