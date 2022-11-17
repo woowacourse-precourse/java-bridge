9a8d6958ec5cb69c693d2c149f59f646f3d84ec3
HyeonjungYun <guswnd9971@gmail.com>
2022-11-17 21:01:43 +0900

    "style(InputView): 공백 출력 제거"

--- src/main/java/bridge/InputView.java
+++ src/main/java/bridge/InputView.java
@@ -35,7 +35,6 @@ public class InputView {
      * 사용자가 이동할 칸을 입력받는다.
      */
     public String readMoving() {
-        System.out.println();
         PrintInput.INPUT_MOVING.print();
 
         return inputMoving();
