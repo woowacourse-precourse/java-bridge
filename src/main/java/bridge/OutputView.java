2eacbe0fce2d517987552c9240e81f1e667c9af8
정현수 <96504592+hyunsb@users.noreply.github.com>
2022-11-17 20:17:31 +0900

    feat: 게임 진행 중 필요한 문구 출력 기능 추가

--- src/main/java/bridge/OutputView.java
+++ src/main/java/bridge/OutputView.java
@@ -1,23 +1,36 @@
 package bridge;
 
-/**
- * 사용자에게 게임 진행 상황과 결과를 출력하는 역할을 한다.
- */
 public class OutputView {
+    private static final String ENTER = "\n";
+    private static final String GAME_START_MESSAGE = "다리 건너기 게임을 시작합니다.";
+    private static final String INPUT_LENGTH_OF_BRIDGE = "다리의 길이를 입력해주세요.";
+    private static final String INPUT_DIRECTION_TO_MOVE = "이동할 칸을 선택해주세요. (위: U, 아래: D)";
+    private static final String INPUT_GAME_RESTART_STATUS = "게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)";
 
-    /**
-     * 현재까지 이동한 다리의 상태를 정해진 형식에 맞춰 출력한다.
-     * <p>
-     * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
-     */
+    public static void printStartMessage(){
+        System.out.println(GAME_START_MESSAGE);
+    }
+
+    public static void printInputLengthOfBridge(){
+        System.out.println(ENTER + INPUT_LENGTH_OF_BRIDGE);
+    }
+
+    public static void printInputDirectionToMove(){
+        System.out.println(INPUT_DIRECTION_TO_MOVE);
+    }
+
+    public static void printInputGameRestartStatus(){
+        System.out.println(ENTER + INPUT_GAME_RESTART_STATUS);
+    }
+
+    public static void printEnter() {
+        System.out.println(ENTER);
+    }
+
     public void printMap() {
     }
 
-    /**
-     * 게임의 최종 결과를 정해진 형식에 맞춰 출력한다.
-     * <p>
-     * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
-     */
+
     public void printResult() {
     }
 }
