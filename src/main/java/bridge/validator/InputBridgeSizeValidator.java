405a8eb0b9ecdabb38885e43cbb6be60f1b2d680
JunH <tjdtls690@gmail.com>
2022-11-17 17:29:56 +0900

    :sparkles: feat(Validator : InputBridgeSizeValidator) : 공백 입력 시 예외 처리 기능 구현

--- src/main/java/bridge/validator/InputBridgeSizeValidator.java
+++ src/main/java/bridge/validator/InputBridgeSizeValidator.java
@@ -1,12 +1,25 @@
 package bridge.validator;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class InputBridgeSizeValidator {
+    private static final String BRIDGE_SIZE_FORM_ERROR_MESSAGE = "[ERROR] '3~20 범위'의 '숫자'만 입력 가능합니다.";
+    private static final String BRIDGE_SIZE_INPUT_FORM = "[3-9]|1\\d|20";
     
     public static void validate(final String inputBridgeSize) {
-        validatorNullOrEmpty(inputBridgeSize);
+        validateNullOrEmpty(inputBridgeSize);
+        validateInputtedBridgeSizeFormMatch(inputBridgeSize);
     }
     
-    private static void validatorNullOrEmpty(final String inputBridgeSize) {
+    private static void validateNullOrEmpty(final String inputBridgeSize) {
         InputCommonValidator.validate(inputBridgeSize);
+    }
+    
+    private static void validateInputtedBridgeSizeFormMatch(final String inputBridgeSize) {
+        final Matcher matcher = Pattern.compile(BRIDGE_SIZE_INPUT_FORM).matcher(inputBridgeSize);
+        if (!matcher.matches()) {
+            throw new IllegalArgumentException(BRIDGE_SIZE_FORM_ERROR_MESSAGE);
+        }
     }
 }
--- src/test/java/bridge/validator/InputBridgeSizeValidatorTest.java
+++ src/test/java/bridge/validator/InputBridgeSizeValidatorTest.java
@@ -4,6 +4,7 @@ import bridge.constant.ErrorMessageConstant;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.NullAndEmptySource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
 
@@ -12,6 +13,15 @@ class InputBridgeSizeValidatorTest {
     @ParameterizedTest(name = "{displayName} : input => {0}")
     @NullAndEmptySource
     void nullOrEmptyException(final String input) {
+        assertThatIllegalArgumentException()
+                .isThrownBy(() -> InputBridgeSizeValidator.validate(input))
+                .withMessageStartingWith(ErrorMessageConstant.ERROR_MESSAGE);
+    }
+    
+    @DisplayName("예외 처리 : 공백 입력 시")
+    @ParameterizedTest(name = "{displayName} : input => {0}")
+    @ValueSource(strings = {" ", "  ", "3 ", " 3"})
+    void spaceException(final String input) {
         assertThatIllegalArgumentException()
                 .isThrownBy(() -> InputBridgeSizeValidator.validate(input))
                 .withMessageStartingWith(ErrorMessageConstant.ERROR_MESSAGE);
