c0531fda03a4044bc1ae57adb27e91e55c80f50b
twoosky <lhjae3@naver.com>
2022-11-18 03:59:33 +0900

    refactor(utils): 메시지 포맷 객체로 분리

---
+++ src/main/java/bridge/utils/MessageFormatUtil.java
@@ -0,0 +1,7 @@
+package bridge.utils;
+
+public class MessageFormatUtil {
+    public static final String BRIDGE_START = "[ ";
+    public static final String BRIDGE_SPACE_DIVISION = " | ";
+    public static final String BRIDGE_END = " ]";
+}
--- src/main/java/bridge/utils/ViewMessages.java
+++ src/main/java/bridge/utils/ViewMessages.java
@@ -7,9 +7,6 @@ public class ViewMessages {
 
     public static final String GAME_START = "다리 건너기 게임을 시작합니다.";
     public static final String GAME_RESULT = "최종 게임 결과";
-    public static final String BRIDGE_START = "[ ";
-    public static final String BRIDGE_SPACE_DIVISION = " | ";
-    public static final String BRIDGE_END = " ]";
     public static final String SUCCESS_OR_FAIL = "게임 성공 여부: ";
     public static final String GAME_COUNT = "총 시도한 횟수: ";
 }
