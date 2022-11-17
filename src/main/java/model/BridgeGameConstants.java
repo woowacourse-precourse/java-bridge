58379c9a857c65f994fa3aad71cea03c0be58602
June <68291395+june-777@users.noreply.github.com>
2022-11-17 21:06:48 +0900

    feat(BridgeGameConstants): 다리 게임과 연관있는 상수는 클래스로 관리

---
+++ src/main/java/model/BridgeGameConstants.java
@@ -0,0 +1,19 @@
+package model;
+
+public class BridgeGameConstants {
+	public static final int MOVING_FAIL_ALREADY_END = 1;
+	public static final int MOVING_FAIL_WRONG_MOVING = 2;
+	public static final int MOVING_SUCCESS = 3;
+	
+	public static final String MOVING_UP = "U";
+	public static final String MOVING_DOWN = "D";
+	
+	public static final int ONE_STEP_FORWARD = 1;
+	
+	public static final int USELESS_SPACE_OF_BRIDGE = 1;
+	
+	public static final int START_POSITION = -1;
+
+	private BridgeGameConstants() {
+	}
+}
