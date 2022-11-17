4fadca067f74c29671a793d9c5c1c07e44f1657d
Soseung Soo <sss9073@naver.com>
2022-11-17 14:49:54 +0900

    feat(Converter): 사용자로부터 입력받은 다리를 2차원 리스트로 변환

--- src/main/java/bridge/Converter.java
+++ src/main/java/bridge/Converter.java
@@ -1,5 +1,8 @@
 package bridge;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Converter {
 
     public String convertNumberToUpAndDown(int number){
@@ -9,5 +12,30 @@ public class Converter {
             }
         }
         return null;
+    }
+
+    public List<List<String>> convertBridgeToBridges(List<String> bridge){
+        List<List<String>> bridges = createBridges(bridge.size());
+        for(int i = 0; i < bridge.size(); i++){
+            String block = bridge.get(i);
+            String emptyBlock = " ";
+            if(UpAndDown.UP.isEqualDirection(block)){
+                bridges.get(UpAndDown.UP.getInputDirectionNumber()).add(block);
+                bridges.get(UpAndDown.DOWN.getInputDirectionNumber()).add(emptyBlock);
+            }
+            if(UpAndDown.DOWN.isEqualDirection(block)){
+                bridges.get(UpAndDown.DOWN.getInputDirectionNumber()).add(block);
+                bridges.get(UpAndDown.UP.getInputDirectionNumber()).add(emptyBlock);
+            }
+        }
+        return bridges;
+    }
+
+    public List<List<String>> createBridges(int size){
+        List<List<String>> bridges = new ArrayList<>();
+        for(int i = 0; i < size; i++){
+            bridges.add(new ArrayList<>());
+        }
+        return bridges;
     }
 }
