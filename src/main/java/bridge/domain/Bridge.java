0f4ea6195eb689b4239ac7dd3015bc597698b9ae
twoosky <lhjae3@naver.com>
2022-11-18 00:57:34 +0900

    feat(Bridge): bridge 객체 생성

--- src/main/java/bridge/domain/Bridge.java
+++ src/main/java/bridge/domain/Bridge.java
@@ -2,18 +2,10 @@ package bridge.domain;
 
 import java.util.List;
 
-import static bridge.utils.ErrorMessages.ERROR_OUT_OF_RANGE;
-
 public class Bridge {
     private final List<Integer> bridge;
 
-    public Bridge(int bridgeSize) {
-        validateSize(bridgeSize);
-    }
-
-    private void validateSize(int bridgeSize) {
-        if (bridgeSize < 3 || bridgeSize > 20) {
-            throw new IllegalArgumentException(ERROR_OUT_OF_RANGE);
-        }
+    public Bridge(List<Integer> bridge) {
+        this.bridge = bridge;
     }
 }
