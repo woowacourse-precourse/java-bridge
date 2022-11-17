feb7be59b4438156c3fe59df2096377461e09376
Soseung Soo <sss9073@naver.com>
2022-11-17 13:55:37 +0900

    feat(OutputView): 게임 다시 시도 여부 명령어 입력 문구 출력

--- src/main/java/View/OutputView.java
+++ src/main/java/View/OutputView.java
@@ -35,4 +35,9 @@ public class OutputView {
         final String readMovingMessage = "이동할 칸을 선택해주세요. (위: U, 아래: D)";
         System.out.println(readMovingMessage);
     }
+
+    public void printReadGameCommand(){
+        final String readGameCommandMessage = "게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)";
+        System.out.println(readGameCommandMessage);
+    }
 }
