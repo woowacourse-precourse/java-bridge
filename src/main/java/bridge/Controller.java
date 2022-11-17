9625aadc775d74838a341e8aa5d9d30e1a81d3e6
dev_kong <ahdjd5@gmail.com>
2022-11-18 04:23:30 +0900

    feat(Controller): add controller setting

--- src/main/java/bridge/Controller.java
+++ src/main/java/bridge/Controller.java
@@ -1,4 +1,15 @@
 package bridge;
 
+import bridge.service.Service;
+import bridge.view.InputView;
+import bridge.view.OutputView;
+
 public class Controller {
+    Service service = new Service();
+    InputView inputView = new InputView();
+    OutputView outputView = new OutputView();
+
+    public void run() {
+
+    }
 }
