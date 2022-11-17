36f1d4756e07564a9e9c50bfa423ff6a3a444b73
HamSeungChan <head0618@naver.com>
2022-11-17 23:33:21 +0900

    feat(test) : 다리 생성 U칸과 D칸 확인 테스트 구현

--- src/test/java/bridge/BridgeMakerTest.java
+++ src/test/java/bridge/BridgeMakerTest.java
@@ -3,21 +3,34 @@ package bridge;
 import bridge.domain.BridgeMaker;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
-
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class BridgeMakerTest {
 
+    private final BridgeNumberGenerator bridgeNumberGenerator = new BridgeRandomNumberGenerator();
     @ParameterizedTest
     @DisplayName("다리 생성 TEST(다리 크기)")
     @ValueSource(ints = {3, 4, 5})
     void checkBridgeSize(int size) {
-        BridgeNumberGenerator bridgeNumberGenerator = new BridgeRandomNumberGenerator();
         BridgeMaker bridgeMaker =new BridgeMaker(bridgeNumberGenerator);
         List<String> bridge = bridgeMaker.makeBridge(size);
         Assertions.assertEquals(bridge.size(),size);
+    }
+
+    @ParameterizedTest
+    @DisplayName("다리 생성 TEST(U칸과 D칸 생성)")
+    @ValueSource(ints = 3)
+    void checkBridge(int size) {
+        List<String> bridgeList = List.of("U","D");
+        BridgeMaker bridgeMaker =new BridgeMaker(bridgeNumberGenerator);
+        List<String> bridge = bridgeMaker.makeBridge(size);
+    Assertions.assertTrue(bridgeList.containsAll(bridge.stream()
+            .distinct()
+            .collect(Collectors.toList())));
+
     }
 }
