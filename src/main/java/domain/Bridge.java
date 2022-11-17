f9c6ac0cb31dad7c68f40ad15987a0e1387dbb81
egoing <dudqk9696@naver.com>
2022-11-17 22:25:54 +0900

    feat : 다리 길이 3~20 사이 정수 아닐시 예외 처리

---
+++ src/main/java/domain/Bridge.java
@@ -0,0 +1,16 @@
+package domain;
+
+public class Bridge {
+
+    private void checkBridgeLength(String bridgeSize) {
+        try {
+            int size = Integer.parseInt(bridgeSize);
+            if(size > 20 || size < 3) {
+                throw new IllegalArgumentException("[ERROR] 3~20 사이의 양의 정수를 입력해주세요");
+            }
+        } catch(NumberFormatException e) {
+            throw new IllegalArgumentException("[ERROR] 3~20 사이의 양의 정수를 입력해주세요");
+        }
+    }
+
+}
