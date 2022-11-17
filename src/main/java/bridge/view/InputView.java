e55c34f281e028422bb0ad83d4822160bad88b35
minju412 <ln8847@ajou.ac.kr>
2022-11-17 10:27:04 +0900

    feat(InputView): add methods to receive input

--- src/main/java/bridge/view/InputView.java
+++ src/main/java/bridge/view/InputView.java
@@ -1,28 +1,41 @@
 package bridge.view;
 
+import camp.nextstep.edu.missionutils.Console;
+
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
  */
 public class InputView {
 
+    private static final String ENTER_BRIDGE_SIZE = "다리의 길이를 입력해주세요.";
+    private static final String ENTER_SPACE_WANT_TO_MOVE = "이동할 칸을 선택해주세요. (위: U, 아래: D)";
+    private static final String ENTER_RETRY_OR_QUIT = "게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)";
+
+    private static String input() {
+        return Console.readLine();
+    }
+
     /**
      * 다리의 길이를 입력받는다.
      */
-    public int readBridgeSize() {
-        return 0;
+    public String readBridgeSize() {
+        System.out.println(ENTER_BRIDGE_SIZE);
+        return input();
     }
 
     /**
      * 사용자가 이동할 칸을 입력받는다.
      */
     public String readMoving() {
-        return null;
+        System.out.println(ENTER_SPACE_WANT_TO_MOVE);
+        return input();
     }
 
     /**
      * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
      */
     public String readGameCommand() {
-        return null;
+        System.out.println(ENTER_RETRY_OR_QUIT);
+        return input();
     }
 }
