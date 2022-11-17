a3d2b3970b080461c7ae1c3fbd68525494c2dc75
splitube <voicesplit@daum.net>
2022-11-17 22:18:33 +0900

    feat(ErrorMessage): 사용자 이동 입력 관련 에러 메세지 추가

--- src/main/java/bridge/validate/ErrorMessage.java
+++ src/main/java/bridge/validate/ErrorMessage.java
@@ -4,7 +4,8 @@ public enum ErrorMessage {
     ERROR("[ERROR] "),
     INPUT_NOT_A_NUMBER("입력값이 숫자가 아닙니다."),
     NUMBER_OUT_OF_RANGE("올바르지 않은 범위의 숫자입니다."),
-    PROPER_BRIDGE_LENGTH("( 다리 길이는 3이상 20이하 )");
+    PROPER_BRIDGE_LENGTH("( 다리 길이는 3이상 20이하 )"),
+    NOT_PROPER_MOVE("'U', 'D' 중에 입력해주세요.");
 
     public final String message;
 
