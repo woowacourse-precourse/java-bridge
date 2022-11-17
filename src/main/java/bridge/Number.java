da5b618c5c1ef61c664c53ac1589b8f62314c438
JunTaeINC <juntea3523@naver.com>
2022-11-17 22:04:27 +0900

    feat : (Number) enum 생성

---
+++ src/main/java/bridge/Number.java
@@ -0,0 +1,20 @@
+package bridge;
+
+public enum Number {
+    RANGE_MIN(3),
+    RANGE_MAX(20),
+    D_NUMBER(0),
+    U_NUMBER(1),
+    NUMBER_OF_ATTEMPTS(0);
+
+
+    private final int value;
+
+    Number(int value) {
+        this.value = value;
+    }
+
+    public int getValue() {
+        return value;
+    }
+}
