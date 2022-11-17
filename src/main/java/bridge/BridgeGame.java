619205c31911412d4c2b06ae45bf63c1d194e6da
chanwonlee <clw8679@naver.com>
2022-11-17 21:56:33 +0900

    feat: BridgeGame 기능 완성

--- src/main/java/bridge/BridgeGame.java
+++ src/main/java/bridge/BridgeGame.java
@@ -1,16 +1,37 @@
 package bridge;
 
+import java.util.List;
+import java.util.Objects;
+
 /**
  * 다리 건너기 게임을 관리하는 클래스
  */
 public class BridgeGame {
+
+    private final List<String> bridge;
+    private int currentBridgeIndex;
+
+    public BridgeGame(List<String> bridge) {
+        this.bridge = bridge;
+        currentBridgeIndex = 0;
+    }
+
+    public int getCurrentBridgeIndex() {
+        return currentBridgeIndex;
+    }
 
     /**
      * 사용자가 칸을 이동할 때 사용하는 메서드
      * <p>
      * 이동을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void move() {
+    public boolean move(State state) {
+        validateBridgeIndex();
+        if (Objects.equals(bridge.get(currentBridgeIndex), state.getCode())) {
+            currentBridgeIndex++;
+            return true;
+        }
+        return false;
     }
 
     /**
@@ -19,5 +40,13 @@ public class BridgeGame {
      * 재시작을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
     public void retry() {
+        currentBridgeIndex = 0;
     }
+
+    private void validateBridgeIndex() {
+        if (currentBridgeIndex >= bridge.size()) {
+            throw new IndexOutOfBoundsException();
+        }
+    }
+
 }
