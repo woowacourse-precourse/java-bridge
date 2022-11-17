deeeb73a1eb7e9b9be6a8d522a64846f44bef88a
nylonmusk <106235834+nylonmusk@users.noreply.github.com>
2022-11-17 23:26:37 +0900

    feat(InputView): 사용자로부터 입력을 받는 기능 및 잘못된 입력값일때 에러 발생 구현

--- src/main/java/bridge/InputView.java
+++ src/main/java/bridge/InputView.java
@@ -6,25 +6,68 @@ import camp.nextstep.edu.missionutils.Console;
  * 사용자로부터 입력을 받는 역할을 한다.
  */
 public class InputView {
+    private static final InputView inputView = new InputView();
+
+    private InputView() {
+    }
+
+    public static InputView getInstance() {
+        return inputView;
+    }
 
     /**
      * 다리의 길이를 입력받는다.
      */
-    public int readBridgeSize() {
-        return Integer.parseInt(Console.readLine());
+    public int readBridgeSize() throws IllegalArgumentException {
+        String size = Console.readLine();
+        isValidBridgeSize(size);
+        return Integer.parseInt(size);
     }
 
     /**
      * 사용자가 이동할 칸을 입력받는다.
      */
-    public String readMoving() {
-        return Console.readLine();
+    public String readMoving() throws IllegalArgumentException {
+        String moving = Console.readLine();
+        isValidMove(moving);
+        return moving;
     }
 
     /**
      * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
      */
-    public String readGameCommand() {
-        return Console.readLine();
+    public String readGameCommand() throws IllegalArgumentException {
+        String gameCommand = Console.readLine();
+        isValidRetryOrQuit(gameCommand);
+        return gameCommand;
+    }
+
+    public void isValidBridgeSize(String size) throws IllegalArgumentException {
+        isBridgeSizeNumber(size);
+        isBridgeSizeValidRange(size);
+    }
+
+    public void isBridgeSizeNumber(String size) {
+        if (!size.matches("^[0-9]*$")) {
+            throw new IllegalArgumentException(InputException.BRIDGE_SIZE_NOT_NUMBER.message());
+        }
+    }
+
+    public void isBridgeSizeValidRange(String size) {
+        if (3 > Integer.parseInt(size) || Integer.parseInt(size) > 20) {
+            throw new IllegalArgumentException(InputException.BRIDGE_SIZE_INVALID_RANGE.message());
+        }
+    }
+
+    public void isValidMove(String moving) {
+        if (!moving.equals("U") && !moving.equals("D")) {
+            throw new IllegalArgumentException(InputException.MOVE_INVALID.message());
+        }
+    }
+
+    public void isValidRetryOrQuit(String gameCommand) {
+        if (!gameCommand.equals("Q") && !gameCommand.equals("R")) {
+            throw new IllegalArgumentException(InputException.RETRY_OR_QUIT_INVALID.message());
+        }
     }
 }
