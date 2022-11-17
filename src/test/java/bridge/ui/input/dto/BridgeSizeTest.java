e40b34145ba0ae033c2b4724a9a13e63f27d8530
TaeyeonRoyce <skdlxmtpqms@gmail.com>
2022-11-17 12:40:49 +0900

    test(BridgeSizeTest): 예외 메세지에 대한 테스트를 추가합니다

--- src/test/java/bridge/ui/input/dto/BridgeSizeTest.java
+++ src/test/java/bridge/ui/input/dto/BridgeSizeTest.java
@@ -1,5 +1,6 @@
 package bridge.ui.input.dto;
 
+import bridge.ui.input.InputErrorText;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
@@ -9,9 +10,12 @@ class BridgeSizeTest {
     @ParameterizedTest
     @ValueSource(strings = {"0", "21", "1a", "", " ", "1.0", "12.4"})
     public void 다리_길이는_3에서_20사이의_자연수만_가능하다(String input) {
+        String expectedErrorText = InputErrorText.ERROR_BRIDGE_SIZE.errorText();
+
         Assertions.assertThatThrownBy(() ->
                         BridgeSize.from(input))
-                .isInstanceOf(IllegalArgumentException.class);
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(expectedErrorText);
     }
 
     @ParameterizedTest
