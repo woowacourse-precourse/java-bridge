c22c59dbae9f971e40d1b03e2773dc8e384619c5
junhei Lee <jun2ee22@naver.com>
2022-11-17 20:49:23 +0900

    feat(ui): 진행 상황을 출력하는 기능

--- src/main/java/bridge/view/OutputView.java
+++ src/main/java/bridge/view/OutputView.java
@@ -1,16 +1,31 @@
 package bridge.view;
 
+import java.util.List;
+import java.util.StringJoiner;
+
 /**
  * 사용자에게 게임 진행 상황과 결과를 출력하는 역할을 한다.
  */
 public class OutputView {
+    private static final String MAP_START = "[ ";
+    private static final String MAP_SEPARATOR = " | ";
+    private static final String MAP_END = " ]";
 
     /**
      * 현재까지 이동한 다리의 상태를 정해진 형식에 맞춰 출력한다.
      * <p>
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void printMap() {
+    //더 좋은 방법 있는지 확인
+    public void printMap(List<List<String>> currentMap) {
+        StringJoiner upStairMap = new StringJoiner(MAP_SEPARATOR, MAP_START, MAP_END);
+        StringJoiner downStairMap = new StringJoiner(MAP_SEPARATOR, MAP_START, MAP_END);
+        for (List<String> map : currentMap) {
+            upStairMap.add(map.get(1));
+            downStairMap.add(map.get(0));
+        }
+        System.out.println(upStairMap);
+        System.out.println(downStairMap);
     }
 
     /**
