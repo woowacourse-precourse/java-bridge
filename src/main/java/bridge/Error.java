03435fbe543bcf6a38d8667034ca62ff7c451b6d
skck0226 <skck0226@ajou.ac.kr>
2022-11-17 15:32:24 +0900

    [feat] (Error) 에러메시지 enum으로 관리

---
+++ src/main/java/bridge/Error.java
@@ -0,0 +1,18 @@
+package bridge;
+
+public enum Error {
+    BRIDGE_SIZE_FORMAT_ERROR("[ERROR] bridge 사이즈 입력형식이 잘못되었습니다 (숫자만 입력)"),
+    BRIDGE_SIZE_RANGE_ERROR("[ERROR] bridge 사이즈 입력 범위가 잘못되었습니다 (3부터 20사이의 숫자)"),
+
+    ;
+
+    private String message;
+
+    Error(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+}
