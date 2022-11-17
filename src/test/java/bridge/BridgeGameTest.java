c47ab4f79847e5173fbd7e60c9f26b6018cfc612
연주 <50358956+Yeon-jooKim@users.noreply.github.com>
2022-11-17 16:05:12 +0900

    test: 이동에 따른 생존여부 변경 테스트

--- src/test/java/bridge/BridgeGameTest.java
+++ src/test/java/bridge/BridgeGameTest.java
@@ -2,6 +2,9 @@ package bridge;
 
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.*;
@@ -18,6 +21,13 @@ class BridgeGameTest {
         bridgeGame.move("U", "D");
         char[][] answer = {{'O', 'O', 'O', ' '}, {' ', ' ', ' ', 'X'}};
         assertThat(bridgeGame.log).isEqualTo(answer);
+    }
 
+    @ParameterizedTest
+    @CsvSource(value = {"U:U:true","U:D:false","D:D:true","D:U:false"}, delimiter = ':')
+    void 이동에_따른_생존여부_변경_테스트(String answer, String submit, boolean result) {
+        BridgeGame bridgeGame = new BridgeGame();
+        bridgeGame.move(answer,submit);
+        assertThat(bridgeGame.survival).isEqualTo(result);
     }
 }
