be4e32f0d71a5e469e864fa03be9a15c4c1d75b6
ddubidubap <park62000@gmail.com>
2022-11-18 01:25:45 +0900

    feat: 게임 재시작 여부 입력하라는 문구 출력하기

--- src/main/java/bridge/view/OutputView.java
+++ src/main/java/bridge/view/OutputView.java
@@ -4,6 +4,7 @@ public class OutputView {
     static final String START_GAME = "다리 건너기 게임을 시작합니다.";
     static final String INPUT_BRIDGE_SIZE = "다리의 길이를 입력해주세요.";
     static final String INPUT_MOVING = "이동할 칸을 선택해주세요. (위: U, 아래: D)";
+    static final String INPUT_COMMAND = "게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)";
 
     public void printStart() {
         System.out.println(START_GAME);
@@ -15,6 +16,10 @@ public class OutputView {
 
     public void printInputMoving() {
         System.out.println(INPUT_MOVING);
+    }
+
+    public void printInputCommand() {
+        System.out.println(INPUT_COMMAND);
     }
 
     public void printMap() {
