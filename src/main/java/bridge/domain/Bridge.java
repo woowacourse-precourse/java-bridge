2c0b8f20944499b9a0ead63b06be0bf0d0e2d28c
minju412 <ln8847@ajou.ac.kr>
2022-11-17 12:36:46 +0900

    feat: compare whether user can move
    - 랜덤으로 생성된 bridge 와 사용자의 position 을 비교해
      O 또는 X 를 반환한다

--- src/main/java/bridge/domain/Bridge.java
+++ src/main/java/bridge/domain/Bridge.java
@@ -4,26 +4,38 @@ import java.util.List;
 
 public class Bridge {
 
-    private static final String UP = "U";
-    private static final String DOWN = "D";
+    private static final int UP = 1;
+    private static final int DOWN = 0;
     private static final String BRIDGE_ELEMENT_INVALID_ERROR = "다리의 구성 요소가 올바르지 않습니다.";
-    private final List<String> elements;
+    private final List<Integer> elements;
 
-    public Bridge(List<String> elements) {
+    public Bridge(List<Integer> elements) {
         validate(elements);
         this.elements = elements;
     }
 
-    private void validate(List<String> elements) {
+    private void validate(List<Integer> elements) {
         checkComponent(elements);
     }
 
-    private void checkComponent(List<String> elements) {
+    // Bridge (정답) 과 input 을 비교해 O 또는 X 를 반환
+    public String compare(Position position) {
+        if (elements.get(position.getIndex()) == position.getElementIndex()) {
+            return "O";
+        }
+        return "X";
+    }
+
+    private void checkComponent(List<Integer> elements) {
         boolean isValid = elements.stream()
-                .filter(element -> element.equals(UP) || element.equals(DOWN))
+                .filter(element -> element == UP || element == DOWN)
                 .count() == elements.size();
         if (!isValid) {
             throw new IllegalArgumentException(BRIDGE_ELEMENT_INVALID_ERROR); // TODO: exception 타입
         }
+    }
+
+    public List<Integer> getElements() {
+        return elements;
     }
 }
