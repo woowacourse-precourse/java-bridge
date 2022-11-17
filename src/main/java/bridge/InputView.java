531fb189cea27bcff84245e799ce25155928a8f4
WhoIsWoony <WhoIsWoony@gmail.com>
2022-11-17 22:05:54 +0900

    fix: fix scanner bug for input

--- src/main/java/bridge/InputView.java
+++ src/main/java/bridge/InputView.java
@@ -6,18 +6,14 @@ import java.util.Scanner;
  * 사용자로부터 입력을 받는 역할을 한다.
  */
 public class InputView {
+    Scanner sc = new Scanner(System.in);
 
     /**
      * 다리의 길이를 입력받는다.
      */
     public int readBridgeSize() {
         System.out.println("다리의 길이를 입력해주세요.");
-
-        Scanner sc = new Scanner(System.in);
-        int bridgeSize = sc.nextInt();
-        sc.close();
-
-        return bridgeSize;
+        return sc.nextInt();
     }
 
     /**
@@ -25,12 +21,7 @@ public class InputView {
      */
     public String readMoving() {
         System.out.println("이동할 칸을 선택해주세요. (위: U, 아래: D)");
-
-        Scanner sc = new Scanner(System.in);
-        String move = sc.next();
-        sc.close();
-
-        return move;
+        return sc.next();
     }
 
     /**
@@ -38,11 +29,6 @@ public class InputView {
      */
     public String readGameCommand() {
         System.out.println("게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)");
-
-        Scanner sc = new Scanner(System.in);
-        String gameCommand = sc.next();
-        sc.close();
-
-        return gameCommand;
+        return sc.next();
     }
 }
