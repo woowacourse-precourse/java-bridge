890124c1044f69132ce1a49c6efcbf80b560b3cb
woo-chang <clllickme@naver.com>
2022-11-17 20:21:09 +0900

    test(Player): 이동 방향 목록 반환, 이동 방향 목록 추가하는 기능 테스트

--- src/test/java/bridge/model/PlayerTest.java
+++ src/test/java/bridge/model/PlayerTest.java
@@ -2,6 +2,7 @@ package bridge.model;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import java.util.List;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -32,6 +33,36 @@ class PlayerTest {
 
         //then
         assertThat(attempt).isEqualTo(2);
+    }
+
+    @DisplayName("처음 생성된 플레이어는 이동 방향을 가지고 있지 않다.")
+    @Test
+    void initDirections() {
+        //given
+        Player player = new Player();
+
+        //when
+        List<Direction> directions = player.getDirections();
+
+        //then
+        assertThat(directions).isEmpty();
+    }
+
+    @DisplayName("플레이어가 이동한다.")
+    @Test
+    void move() {
+        //given
+        Player player = new Player();
+        Direction direction = Direction.UP;
+
+        //when
+        player.move(direction);
+        List<Direction> directions = player.getDirections();
+
+        //then
+        assertThat(directions).isNotEmpty();
+        assertThat(directions.size()).isEqualTo(1);
+        assertThat(directions).containsExactly(direction);
     }
 
 }
