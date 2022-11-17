60e8fe8c5098006f37d8518ef5558220b2ea7a45
jjmin9797 <jjmin9797@naver.com>
2022-11-17 19:42:03 +0900

    feat(User): User 클래스 생성 및 필드 생성

---
+++ src/main/java/bridge/domain/User.java
@@ -0,0 +1,10 @@
+package bridge.domain;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class User {
+
+    private List<String> bridge;
+    private ArrayList<String> userBridge;
+}
