8bd9df3b6b3e9da2c1f2586fbab32b583a84c245
SuhHyeonjun <dmsgktn0208@gmail.com>
2022-11-17 21:39:32 +0900

    feat : InputView 클래스 규칙에 맞게 수정

--- src/main/java/bridge/InputView.java
+++ src/main/java/bridge/InputView.java
@@ -6,32 +6,33 @@ import static camp.nextstep.edu.missionutils.Console.readLine;
  * 사용자로부터 입력을 받는 역할을 한다.
  */
 public class InputView {
-    public static int bridgeSize;
-    public static String inputDirection;
-    public static String inputRestart;
 
     /**
      * 다리의 길이를 입력받는다.
      */
-    public static void inputBridgeSize() {
+    public int inputBridgeSize() {
+        System.out.println(Message.INPUT_SIZE.getMessage());
         String inputSize = readLine();
         System.out.println();
-        bridgeSize = Integer.parseInt(inputSize);
+        int bridgeSize = Integer.parseInt(inputSize);
+        return bridgeSize;
     }
 
     /**
      * 사용자가 이동할 칸을 입력받는다.
      */
-    public static void inputMovingDirection() {
+    public String inputMovingDirection() {
         System.out.println(Message.SELECT_DIRECTION.getMessage());
-        inputDirection = readLine();
+        String inputDirection = readLine();
+        return inputDirection;
     }
 
     /**
      * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
      */
-    public static void inputGameRestart() {
+    public String inputGameRestart() {
         System.out.println(Message.ASK_RESTART.getMessage());
-        inputRestart = readLine();
+        String inputRestart = readLine();
+        return inputRestart;
     }
 }
