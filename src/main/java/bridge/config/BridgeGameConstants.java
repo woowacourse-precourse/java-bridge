91b192cc5247e4dcbac917bc3201583b5874a86c
Be-student <presentsong@naver.com>
2022-11-18 00:50:37 +0900

    feat(constant): 전체에서 사용되는 상수 추가

---
+++ src/main/java/bridge/config/BridgeGameConstants.java
@@ -0,0 +1,9 @@
+package bridge.config;
+
+public final class BridgeGameConstants {
+    public static final int BRIDGE_MIN_LENGTH = 3;
+    public static final int BRIDGE_MAX_LENGTH = 20;
+
+    private BridgeGameConstants() {
+    }
+}
