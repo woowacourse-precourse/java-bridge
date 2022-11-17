761b7a0ff70950b9365b5a35cf3bfe7084496a8d
hojunking96 <junsong96@naver.com>
2022-11-17 13:00:47 +0900

    feats(inputView.java) : 다리의 길이 입력 기능 구현

--- src/main/java/bridge/InputView.java
+++ src/main/java/bridge/InputView.java
@@ -1,15 +1,23 @@
 package bridge;
 
+import camp.nextstep.edu.missionutils.Console;
+
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
  */
 public class InputView {
 
+
     /**
      * 다리의 길이를 입력받는다.
      */
     public int readBridgeSize() {
-        return 0;
+        String bridgeSizeInput = Console.readLine();
+        /*
+         * 입력이 유효한지 확인
+         */
+        int bridgeSize = (Integer.parseInt(bridgeSizeInput));
+        return bridgeSize;
     }
 
     /**
