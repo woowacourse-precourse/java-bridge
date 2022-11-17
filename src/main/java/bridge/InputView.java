5b8ecae3d20280a36f90a32624b342327aedccce
chaerim <qkrdbsk28@naver.com>
2022-11-18 06:02:50 +0900

    feat:  재시도 여부 입력 기능 구현

--- src/main/java/bridge/InputView.java
+++ src/main/java/bridge/InputView.java
@@ -8,6 +8,7 @@ import camp.nextstep.edu.missionutils.Console;
 public class InputView {
     private final static String INPUT_BRIDGE_SIZE_MESSAGE = "다리의 길이를 입력해주세요.";
     private final static String INPUT_MOVING_MESSAGE = "이동할 칸을 선택해주세요. (위: " + BridgeValue.UP.getValue() + ", 아래: " + BridgeValue.DOWN.getValue() + " )";
+    private final static String INPUT_GAME_COMMAND_MESSAGE = "게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)";
 
     /**
      * 다리의 길이를 입력받는다.
@@ -33,6 +34,8 @@ public class InputView {
      * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
      */
     public String readGameCommand() {
-        return null;
+        System.out.println(INPUT_GAME_COMMAND_MESSAGE);
+        String inputCommand = Console.readLine();
+        return inputCommand;
     }
 }
