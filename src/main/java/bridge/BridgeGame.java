bb11af22a412c37298a7e45d58ab00f5640f6396
yumyeonghan <mgtmh991013@gmail.com>
2022-11-17 16:38:43 +0900

    feat: implement the move function
    Return true value if the position to move is smaller than the size of the bridge and the information entered by the user is the same as the position information

--- src/main/java/bridge/BridgeGame.java
+++ src/main/java/bridge/BridgeGame.java
@@ -1,5 +1,7 @@
 package bridge;
 
+import java.util.List;
+
 /**
  * 다리 건너기 게임을 관리하는 클래스
  */
@@ -14,11 +16,16 @@ public class BridgeGame {
     }
 
     /**
-     * 사용자가 칸을 이동할 때 사용하는 메서드
+     * 사용자가 칸을 이동할 때 사용하는 메서드1
      * <p>
      * 이동을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void move() {
+    public boolean move(int movedPosition, String inputInformation, List<String> bridge) {
+
+        if (movedPosition < bridge.size() && inputInformation.equals(bridge.get(movedPosition))) {
+            return true;
+        }
+        return false;
     }
 
     /**
