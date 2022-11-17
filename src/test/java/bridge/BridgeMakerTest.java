8352563aefa18de9d3c477dfd425cf70a40a0acb
comet7001 <comet7001@gmail.com>
2022-11-17 21:26:37 +0900

    chore(BridgeMakerTest): validate_is_Number_fail()에 @ValueSource 추가

--- src/test/java/bridge/BridgeMakerTest.java
+++ src/test/java/bridge/BridgeMakerTest.java
@@ -2,15 +2,17 @@ package bridge;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class BridgeMakerTest {
 
     @DisplayName("다리의 길이로 입력받은 값이 정수형인지 확인한다.")
+    @ValueSource(strings = {"문자","🍖"," ",""})
     @Test
-    void validate_is_Number_fail() {
-        assertThatThrownBy(()-> new BridgeMaker("문자"))
+    void validate_is_Number_fail(BridgeNumberGenerator input) {
+        assertThatThrownBy(()-> new BridgeMaker(input))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageStartingWith("[ERROR]");
     }
