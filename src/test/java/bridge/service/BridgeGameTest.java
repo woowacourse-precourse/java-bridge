e4622c1da205174cc28573fc02089d28ed0579d3
JaeWon Choi <87649850+codesver@users.noreply.github.com>
2022-11-17 16:05:02 +0900

    refactor(BridgeGameTest): move duplicate code

--- src/test/java/bridge/service/BridgeGameTest.java
+++ src/test/java/bridge/service/BridgeGameTest.java
@@ -1,6 +1,7 @@
 package bridge.service;
 
 import bridge.constant.Movement;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -11,25 +12,28 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 class BridgeGameTest {
 
+    private BridgeGame bridgeGame;
+
+    @BeforeEach
+    void init() {
+        List<String> bridge = Arrays.asList("U", "D", "D", "U");
+        bridgeGame = new BridgeGame(bridge);
+    }
+
     @Test
     @DisplayName("사용자의 이동 방향과 다리의 이동 가능 방향에 따라 움직인다.")
     void moving() {
-        List<String> bridge = Arrays.asList("U", "D", "D", "U");
-        BridgeGame game = new BridgeGame(bridge);
-
-        assertThat(game.move("U")).isTrue();
-        assertThat(game.move("U")).isFalse();
+        assertThat(bridgeGame.move("U")).isTrue();
+        assertThat(bridgeGame.move("U")).isFalse();
     }
 
     @Test
     @DisplayName("움직임에 대한 현재 상태를 생성한다.")
     void creatingMovementStatus() {
-        List<String> bridge = Arrays.asList("U", "D", "D", "U");
-        BridgeGame game = new BridgeGame(bridge);
-        game.move("U");
-        game.move("D");
-        game.move("U");
-        String movementStatus = game.createMovementStatus();
+        bridgeGame.move("U");
+        bridgeGame.move("D");
+        bridgeGame.move("U");
+        String movementStatus = bridgeGame.createMovementStatus();
         assertThat(movementStatus).isEqualTo("[ O |   | X ]\n[   | O |   ]\n");
     }
 }
