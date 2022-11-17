681a1c66014f154ec26a77d95c710c23458d4034
kokodak <kokodakadokok@gmail.com>
2022-11-17 15:58:57 +0900

    feat(inputValidator): 다리의 길이가 숫자인지 검증하는 기능 구현

---
+++ src/main/java/bridge/validator/InputValidator.java
@@ -0,0 +1,22 @@
+package bridge.validator;
+
+public class InputValidator {
+
+    private static final String BRIDGE_SIZE_ERROR = "[ERROR] 다리 길이는 3부터 20 사이의 숫자여야 합니다.";
+
+    public static int validateBridgeSize(String input) {
+        if (!isNumber(input)) {
+            throw new IllegalArgumentException(BRIDGE_SIZE_ERROR);
+        }
+        return Integer.parseInt(input);
+    }
+
+    private static boolean isNumber(String input) {
+        for (int i = 0; i < input.length(); i++) {
+            if (input.charAt(i) < '0' || input.charAt(i) > '9') {
+                return false;
+            }
+        }
+        return true;
+    }
+}
