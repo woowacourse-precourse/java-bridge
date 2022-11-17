c9483b9c5d1f6f61bc604473cd50be197d67af98
WhalesBob <ndw8200@naver.com>
2022-11-17 09:28:28 +0900

    feat(BridgeGame) : add features of move bridge(initial phase)

--- src/main/java/bridge/businesslogic/BridgeGame.java
+++ src/main/java/bridge/businesslogic/BridgeGame.java
@@ -41,8 +41,20 @@ public class BridgeGame {
      * 이동을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
     public boolean move(String usersPick) {
+        boolean isUsersPickRight = bridge.isSelectedBridgesRight(usersPick);
+        String userPickResult = convertUsersPick(isUsersPickRight);
+        bridge.crossBridge();
+
         return true;
     }
+
+    private String convertUsersPick(boolean isUsersPickRight){
+        if(isUsersPickRight){
+            return "Y";
+        }
+        return "N";
+    }
+
 
     /**
      * 사용자가 게임을 다시 시도할 때 사용하는 메서드
