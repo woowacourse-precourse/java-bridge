49e7ee00bb6d8eb7c8b0776f92e4cc23ca907490
heesang <dlagmltkd12@naver.com>
2022-11-17 23:54:49 +0900

    feat: 다리의 크기가 주어지고, 1만 주어질 때 U로만 이루어진 다리를 반환하는 기능

--- src/main/java/bridge/BridgeMaker.java
+++ src/main/java/bridge/BridgeMaker.java
@@ -1,6 +1,8 @@
 package bridge;
 
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 /**
  * 다리의 길이를 입력 받아서 다리를 생성해주는 역할을 한다.
@@ -10,6 +12,13 @@ import java.util.List;
  * BridgeMaker의 메서드의 시그니처(인자, 이름)와 반환 타입은 변경할 수 없다.
  */
 public class BridgeMaker {
+
+    public static final int START_INCLUSIVE_ZERO = 0;
+    public static final int UP_VALUE = 1;
+    public static final int DOWN_VALUE = 0;
+    public static final String UP_MARK = "U";
+    public static final String DOWN_MARK = "D";
+    public static final String ERROR_INVALID_BRIDGE_NUMBER = "[ERROR] 유효하지 않는 다리 숫자 값 입니다.";
 
     private final BridgeNumberGenerator bridgeNumberGenerator;
 
@@ -22,6 +31,26 @@ public class BridgeMaker {
      * @return 입력받은 길이에 해당하는 다리 모양. 위 칸이면 "U", 아래 칸이면 "D"로 표현해야 한다.
      */
     public List<String> makeBridge(int size) {
-        return null;
+        return IntStream.range(START_INCLUSIVE_ZERO, size)
+                .mapToObj(i -> mapToString(bridgeNumberGenerator.generate()))
+                .collect(Collectors.toList());
+    }
+
+    private String mapToString(int mark) {
+        if (isUp(mark)) {
+            return UP_MARK;
+        }
+        if (isDown(mark)) {
+            return DOWN_MARK;
+        }
+        throw new IllegalArgumentException(ERROR_INVALID_BRIDGE_NUMBER);
+    }
+
+    private boolean isUp(int mark) {
+        return mark == UP_VALUE;
+    }
+
+    private boolean isDown(int mark) {
+        return mark == DOWN_VALUE;
     }
 }
---
+++ src/test/java/bridge/BridgeMakerTest.java
@@ -0,0 +1,19 @@
+package bridge;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class BridgeMakerTest {
+
+    @DisplayName("다리의 크기가 주어지고, 1만 주어질 때 U로만 이루어진 다리를 반환")
+    @Test
+    void makeBridgeGiveOne() {
+        BridgeMaker bridgeMaker = new BridgeMaker(() -> 1);
+        List<String> bridge = bridgeMaker.makeBridge(6);
+        assertThat(bridge).containsExactly("U", "U", "U", "U", "U", "U");
+    }
+}
