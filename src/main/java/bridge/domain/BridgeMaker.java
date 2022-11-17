6a27d0cc4f58f3bc1450c8004716b966042a97ef
kdkdhoho <hkim4410@naver.com>
2022-11-17 16:12:19 +0900

    refactor(BridgeMaker): 코드 간결화 리팩토링 및 상수 추가, 주석제거

--- src/main/java/bridge/domain/BridgeMaker.java
+++ src/main/java/bridge/domain/BridgeMaker.java
@@ -10,28 +10,24 @@ import java.util.List;
  */
 public class BridgeMaker {
 
+    private final int DOWN = 0;
+
     private final BridgeNumberGenerator bridgeNumberGenerator;
 
     public BridgeMaker(BridgeNumberGenerator bridgeNumberGenerator) {
         this.bridgeNumberGenerator = bridgeNumberGenerator;
     }
 
-    /**
-     * @param size 다리의 길이
-     * @return 입력받은 길이에 해당하는 다리 모양. 위 칸이면 "U", 아래 칸이면 "D"로 표현해야 한다.
-     */
     public List<String> makeBridge(final int size) {
         List<String> bridge = new ArrayList<>();
-
         for (int repeat = 0; repeat < size; repeat++) {
-            int generatedNumber = bridgeNumberGenerator.generate();
-            bridge.add(generateAnswer(generatedNumber));
+            bridge.add(generateAnswer());
         }
         return bridge;
     }
 
-    private String generateAnswer(int generatedNumber) {
-        if (generatedNumber == 0) {
+    private String generateAnswer() {
+        if (bridgeNumberGenerator.generate() == DOWN) {
             return "D";
         }
         return "U";
