87453aa9c2f0d7cbec292ae72145c69c8bd616b2
seokjin8678 <rnrn8678@naver.com>
2022-11-17 19:27:58 +0900

    feat: 게임의 재시작 유무를 출력하는 기능 추가

--- src/main/java/bridge/view/OutputView.java
+++ src/main/java/bridge/view/OutputView.java
@@ -71,4 +71,8 @@ public class OutputView {
         System.out.println();
         System.out.println("이동할 칸을 선택해주세요. (위: U, 아래: D)");
     }
+
+    public void printRestartOrQuitMessage() {
+        System.out.println("게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)");
+    }
 }
