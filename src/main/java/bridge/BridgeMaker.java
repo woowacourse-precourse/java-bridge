266b5665cd6f847c7ee2ca769d214100d00f49f7
sugamypapa <sk000801@naver.com>
2022-11-17 22:45:41 +0900

    feat: 다리 게임 정답 반환 기능 구현

--- src/main/java/bridge/BridgeMaker.java
+++ src/main/java/bridge/BridgeMaker.java
@@ -1,5 +1,6 @@
 package bridge;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -13,11 +14,27 @@ public class BridgeMaker {
         this.bridgeNumberGenerator = bridgeNumberGenerator;
     }
 
+    // TODO: 더 효율적으로 코드 작성할 수 있는 방법(ex.enum)생각해보기
+    public String returnMove(int number) {
+        String direction = "";
+        if (number == 0) {
+            direction = "D";
+        }
+        if (number == 1) {
+            direction = "U";
+        }
+        return direction;
+    }
+
     /**
      * @param size 다리의 길이
      * @return 입력받은 길이에 해당하는 다리 모양. 위 칸이면 "U", 아래 칸이면 "D"로 표현해야 한다.
      */
     public List<String> makeBridge(int size) {
-        return null;
+        List<String> answerBridge = new ArrayList<>();
+        for (int i = 0; i < size; i++) {
+            answerBridge.add(returnMove(bridgeNumberGenerator.generate()));
+        }
+        return answerBridge;
     }
 }
