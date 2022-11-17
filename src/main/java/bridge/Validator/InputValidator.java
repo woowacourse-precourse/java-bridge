1adea9a2fc22ff664068cfe0d6aee07a797d96fb
hongsikcho <jjdd9758@naver.com>
2022-11-17 22:58:23 +0900

    feat : 다리 길이 입력 및 유효성 검사

---
+++ src/main/java/bridge/Validator/InputValidator.java
@@ -0,0 +1,30 @@
+package bridge.Validator;
+
+public class InputValidator {
+
+    public static void checkBridgeSize(String size) {
+        if(!(checkOnlyNumber(size))){
+            throw new IllegalArgumentException("숫자가 아닙니다.");
+        }
+        if(!(checkBoundaryNumber(size))){
+            throw new IllegalArgumentException("범위가 아닙니다.");
+        }
+    }
+
+    private static boolean checkBoundaryNumber(String size) {
+        Integer bridgeSize = Integer.parseInt(size);
+        if(bridgeSize < 3 || bridgeSize > 20){
+            return false;
+        }
+        return true;
+    }
+
+    private static boolean checkOnlyNumber(String size) {
+        try{
+            Integer.parseInt(size);
+        }catch(NumberFormatException e){
+            return false;
+        }
+        return true;
+    }
+}
--- src/main/java/bridge/controller/BridgeGame.java
+++ src/main/java/bridge/controller/BridgeGame.java
@@ -1,9 +1,19 @@
-package bridge;
+package bridge.controller;
+
+import bridge.view.InputView;
 
 /**
  * 다리 건너기 게임을 관리하는 클래스
  */
 public class BridgeGame {
+
+    public void startGame(){
+        initBridge();
+    }
+
+    private void initBridge() {
+        Integer bridgeSize = InputView.readBridgeSize();
+    }
 
     /**
      * 사용자가 칸을 이동할 때 사용하는 메서드
