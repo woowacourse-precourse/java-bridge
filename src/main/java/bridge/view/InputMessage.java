0ba33f6a285d530c08076fdd1e0b8b642f975023
Isu Shin <isu010614@naver.com>
2022-11-18 00:52:01 +0900

    feat: 사용자에게 위/아래 칸을 입력받을 때의 예외처리 기능 구현
    - U 혹은 D 이외의 문자가 입력된 경우 에러메시지 출력

--- src/main/java/bridge/view/InputMessage.java
+++ src/main/java/bridge/view/InputMessage.java
@@ -2,6 +2,7 @@ package bridge.view;
 
 public class InputMessage {
     public static final String INPUT_BRIDGE_LENGTH = "다리의 길이를 입력해주세요.";
+    public static final String INPUT_MOVING = "이동할 칸을 선택해주세요. (위: U, 아래: D)";
 
     private InputMessage() {
     }
--- src/main/java/bridge/view/InputValidator.java
+++ src/main/java/bridge/view/InputValidator.java
@@ -1,6 +1,7 @@
 package bridge.view;
 
 import bridge.domain.ExceptionMessage;
+import bridge.domain.MovingStatus;
 
 public class InputValidator {
    static final int START_LENGTH_RANGE = 3;
@@ -22,5 +23,16 @@ public class InputValidator {
     private boolean isInRange(String inputLength){
         int bridgeLength = Integer.parseInt(inputLength);
         return bridgeLength >= START_LENGTH_RANGE && bridgeLength <= END_LENGTH_RANGE;
+    }
+
+    public void validateMoving(String moving){
+        if(!isUpOrDown(moving)){
+            throw new IllegalArgumentException(ExceptionMessage.NOT_UP_OR_DOWN);
+        }
+    }
+
+    private boolean isUpOrDown(String moving){
+        return moving.equals(MovingStatus.UP.getMovingKey())
+                || moving.equals(MovingStatus.DOWN.getMovingKey());
     }
 }
