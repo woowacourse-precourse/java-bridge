0621f7daf9b43c46ea351e85e7203377f09e316c
Park shinyeong <syngh503@gmail.com>
2022-11-17 19:31:18 +0900

    Feat(InputView): 게임 재시작 여부 입력
    사용자에게 게임 재시작 여부를 입력 받아, 입력 값을 문자열로 리턴하는 메소드를 구현하였습니다.

--- src/main/java/bridge/View/InputView.java
+++ src/main/java/bridge/View/InputView.java
@@ -28,7 +28,8 @@ public class InputView {
      * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
      */
     public String readGameCommand() {
-        return null;
+        printSystemMessage(InputSystemMessage.GAME_RESTART_INPUT);
+        return Console.readLine();
     }
 
     public void printSystemMessage(InputSystemMessage situation) {
