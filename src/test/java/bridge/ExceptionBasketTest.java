f4044a16cd1c1a800cfe8cd1ae0f70de233a829e
kissshot1104 <kissshot.heartunderblade1104@gmail.com>
2022-11-17 19:00:32 +0900

    Test(ExceptionBasketTest) : 잘못된 키 입력시 예외처리 테스트 코드를 추가
    ExceptionBasket클래스의 public static void invalidGameKey(String input) 메소드의 테스트 코드를 추가했습니다.

--- src/test/java/bridge/ExceptionBasketTest.java
+++ src/test/java/bridge/ExceptionBasketTest.java
@@ -14,9 +14,87 @@ class ExceptionBasketTest {
     @DisplayName("1~20이외의 숫자일 경우 예외 처리")
     void inputIsNotRangeExceptionTest(){
         assertThatThrownBy(() ->
+                ExceptionBasket.inputIsNotRangeException("5.0"))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(ERROR_MESSAGE);
+        assertThatThrownBy(() ->
+                ExceptionBasket.inputIsNotRangeException("5.3"))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(ERROR_MESSAGE);
+        assertThatThrownBy(() ->
+                ExceptionBasket.inputIsNotRangeException("+5"))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(ERROR_MESSAGE);
+        assertThatThrownBy(() ->
+                ExceptionBasket.inputIsNotRangeException("-5"))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(ERROR_MESSAGE);
+        assertThatThrownBy(() ->
                 ExceptionBasket.inputIsNotRangeException("05"))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining(ERROR_MESSAGE);
     }
+
+    @Test
+    @DisplayName("허용된 숫자열이 아닐 시 예외 처리")
+    void inputIsNotNumberExceptionTest(){
+        assertThatThrownBy(() ->
+                ExceptionBasket.inputIsNotNumberException("a"))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(ERROR_MESSAGE);
+
+        assertThatThrownBy(() ->
+                ExceptionBasket.inputIsNotNumberException("5.3"))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(ERROR_MESSAGE);
+
+        assertThatThrownBy(() ->
+                ExceptionBasket.inputIsNotNumberException("5.3"))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(ERROR_MESSAGE);
+
+        assertThatThrownBy(() ->
+                ExceptionBasket.inputIsNotNumberException("5.3"))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(ERROR_MESSAGE);
+    }
+
+    @Test
+    @DisplayName("게임 플레이중 잘못된 키를 누를 경우 예외 처리")
+    void invalidGameKeyTest(){
+        assertThatThrownBy(() ->
+                ExceptionBasket.invalidGameKey("A"))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(ERROR_MESSAGE);
+
+        assertThatThrownBy(() ->
+                ExceptionBasket.invalidGameKey("UU"))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(ERROR_MESSAGE);
+
+        assertThatThrownBy(() ->
+                ExceptionBasket.invalidGameKey("DD"))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(ERROR_MESSAGE);
+    }
+
+    /*@Test
+    @DisplayName("허용되지 않은 숫자열 일 시 예외 처리")
+    void invalidNumbericTest() {
+        assertThatThrownBy(() ->
+                ExceptionBasket.invalidNumberic("+5"))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(ERROR_MESSAGE);
+
+        assertThatThrownBy(() ->
+                ExceptionBasket.invalidNumberic("-5"))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(ERROR_MESSAGE);
+
+        assertThatThrownBy(() ->
+                ExceptionBasket.invalidNumberic("05"))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(ERROR_MESSAGE);
+    }*/
 
 }
