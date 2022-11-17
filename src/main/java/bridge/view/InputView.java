c4e5e5508f770e892afe0cdecf2e8256be594ba5
leewoojin <zmfpdl64@naver.com>
2022-11-17 18:46:44 +0900

    Style: 클래스를 나누면서 패키지를 이동시켰다.

--- src/main/java/bridge/view/InputView.java
+++ src/main/java/bridge/view/InputView.java
@@ -1,4 +1,6 @@
-package bridge;
+package bridge.view;
+
+import camp.nextstep.edu.missionutils.Console;
 
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
@@ -9,14 +11,14 @@ public class InputView {
      * 다리의 길이를 입력받는다.
      */
     public int readBridgeSize() {
-        return 0;
+        return Integer.parseInt(input());
     }
 
     /**
      * 사용자가 이동할 칸을 입력받는다.
      */
     public String readMoving() {
-        return null;
+        return input();
     }
 
     /**
@@ -24,5 +26,9 @@ public class InputView {
      */
     public String readGameCommand() {
         return null;
+    }
+
+    public String input() {
+        return Console.readLine();
     }
 }
