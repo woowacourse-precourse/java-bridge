dd6cc19d447c3a6c1f9e3c2f676c1dc1fdec6188
Eungyu-code <Agyu625@gmail.com>
2022-11-17 13:38:08 +0900

    feat(Application class): playBridgeGame 메소드 추가
    다리 게임이 진행되는 함수

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -4,5 +4,10 @@ public class Application {
 
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        playBridgeGame();
+    }
+
+    public static void playBridgeGame() {
+
     }
 }
