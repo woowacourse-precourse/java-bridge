9d7344d53eb27d1412f419cf4af8030bd62e034b
KangSungBum <peter9706@naver.com>
2022-11-17 17:22:47 +0900

    feat(ErrorResponse.java): add new error response enum value
    새로운 에러 이넘 값 추가
    - INPUT_MOVEMENT_ERROR: 사용자 이동 입력 예외 코드
    - INPUT_EXITCODE_ERROR: 사용자 종료 입력 예외 코드

--- src/main/java/response/ErrorResponse.java
+++ src/main/java/response/ErrorResponse.java
@@ -2,6 +2,8 @@ package response;
 
 public enum ErrorResponse {
     INPUT_BRIDE_SIZE_RANGE_ERROR("[ERROR] 다리 길이는 3부터 20 사이의 숫자여야 합니다."),
+    INPUT_MOVEMENT_ERROR("[ERROR] 이동 입력은 대문자 U 또는 대문자 D만 허용됩니다."),
+    INPUT_EXITCODE_ERROR("[ERROR] 종료 선택 입력은 대문자 R 또는 대문자 Q만 허용됩니다."),
     ;
 
     private String errorMessage;
