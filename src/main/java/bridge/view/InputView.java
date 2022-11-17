e236c55da43e4dd36873317662a05ca96b9c21e2
ddubidubap <park62000@gmail.com>
2022-11-18 01:03:42 +0900

    feat: 다리 길이를 올바른 범위 내에 있는 숫자로 설정했는지 확인하기

--- src/main/java/bridge/view/InputView.java
+++ src/main/java/bridge/view/InputView.java
@@ -3,7 +3,10 @@ package bridge.view;
 import static camp.nextstep.edu.missionutils.Console.readLine;
 
 public class InputView {
+    static final int MIN_BRIDGE_SIZE = 3;
+    static final int MAX_BRIDGE_SIZE = 20;
     static final String IS_NOT_NUMBER = "다리 길이는 숫자여야 합니다";
+    static final String IS_NOT_IN_RANGE = "다리 길이는 3 이상, 20 이하의 숫자여야 합니다";
 
     public int readBridgeSize() {
         String bridgeSize = readLine();
@@ -23,6 +26,12 @@ public class InputView {
             return Integer.parseInt(input);
         } catch (NumberFormatException e) {
             throw new IllegalArgumentException(IS_NOT_NUMBER);
+        }
+    }
+
+    private void isBetweenRange(int input) {
+        if (input < MIN_BRIDGE_SIZE || input > MAX_BRIDGE_SIZE) {
+            throw new IllegalArgumentException(IS_NOT_IN_RANGE);
         }
     }
 }
