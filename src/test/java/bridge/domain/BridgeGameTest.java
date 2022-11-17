4c50687d10b89ad5a264e1aec6891e070c316187
HubCreator <chocomilk8604@gmail.com>
2022-11-17 11:41:12 +0900

    test(BridgeGameTest) : 도메인 로직 테스트 케이스 추가

--- src/test/java/bridge/domain/BridgeGameTest.java
+++ src/test/java/bridge/domain/BridgeGameTest.java
@@ -1,5 +1,7 @@
 package bridge.domain;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 import java.util.Arrays;
@@ -8,13 +10,60 @@ import static org.assertj.core.api.Assertions.*;
 
 class BridgeGameTest {
 
-    @Test
-    void test() {
-        BridgeGame bridgeGame = new BridgeGame(Arrays.asList("U", "U", "D"));
-        bridgeGame.move("U");
-        System.out.println(bridgeGame);
-        assertThat(bridgeGame.getPosition()).isEqualTo(0);
-        assertThat(bridgeGame.getUpBridgeStatus()).containsExactly('O', ' ', ' ');
+    BridgeGame bridgeGame;
+
+    @BeforeEach
+    void init() {
+        bridgeGame = new BridgeGame(Arrays.asList("U", "U", "D"));
+    }
+
+    @Nested
+    class MoveTest {
+        @Test
+        void test1() {
+            bridgeGame.move("U");
+            assertThat(bridgeGame.getPosition()).isEqualTo(0);
+            assertThat(bridgeGame.getUpBridgeStatus()).containsExactly('O', ' ', ' ');
+            assertThat(bridgeGame.getDownBridgeStatus()).containsExactly(' ', ' ', ' ');
+        }
+
+        @Test
+        void test2() {
+            bridgeGame.move("U");
+            bridgeGame.move("D");
+            assertThat(bridgeGame.getPosition()).isEqualTo(1);
+            assertThat(bridgeGame.getUpBridgeStatus()).containsExactly('O', ' ', ' ');
+            assertThat(bridgeGame.getDownBridgeStatus()).containsExactly(' ', 'X', ' ');
+        }
+
+        @Test
+        void test3() {
+            bridgeGame.move("U");
+            bridgeGame.move("U");
+            assertThat(bridgeGame.getPosition()).isEqualTo(1);
+            assertThat(bridgeGame.getUpBridgeStatus()).containsExactly('O', 'O', ' ');
+            assertThat(bridgeGame.getDownBridgeStatus()).containsExactly(' ', ' ', ' ');
+        }
+
+        @Test
+        void test4() {
+            bridgeGame.move("U");
+            bridgeGame.move("U");
+            bridgeGame.move("U");
+            assertThat(bridgeGame.getPosition()).isEqualTo(2);
+            assertThat(bridgeGame.getUpBridgeStatus()).containsExactly('O', 'O', 'X');
+            assertThat(bridgeGame.getDownBridgeStatus()).containsExactly(' ', ' ', ' ');
+        }
+
+        @Test
+        void test5() {
+            bridgeGame.move("U");
+            bridgeGame.move("U");
+            bridgeGame.move("D");
+            assertThat(bridgeGame.getPosition()).isEqualTo(2);
+            assertThat(bridgeGame.getUpBridgeStatus()).containsExactly('O', 'O', ' ');
+            assertThat(bridgeGame.getDownBridgeStatus()).containsExactly(' ', ' ', 'O');
+        }
     }
 
 }
