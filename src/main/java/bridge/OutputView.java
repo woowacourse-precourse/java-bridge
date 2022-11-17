725f0d420b22e2ce0cfbc0f5944de1761e66e6d3
소대훈 <theogns0681@naver.com>
2022-11-17 09:55:06 +0900

    [refactor] 문자열 변수로 분리

--- src/main/java/bridge/OutputView.java
+++ src/main/java/bridge/OutputView.java
@@ -7,6 +7,10 @@ import java.util.Objects;
  * 사용자에게 게임 진행 상황과 결과를 출력하는 역할을 한다.
  */
 public class OutputView {
+    final String gameResult = "최종 게임 결과";
+    final String success = "게임 성공 여부: 성공";
+    final String fail = "게임 성공 여부: 실패";
+    final String countingGame = "총 시도한 횟수: ";
 
     /**
      * 현재까지 이동한 다리의 상태를 정해진 형식에 맞춰 출력한다.
@@ -52,10 +56,10 @@ public class OutputView {
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
     public void printResult(int movingIdx, List<String> bridgeList, String input, int count, boolean winning) {
-        System.out.println("최종 게임 결과");
+        System.out.println(gameResult);
         printMap(movingIdx, bridgeList, input);
-        if (winning) System.out.println("게임 성공 여부: 성공");
-        if (!winning) System.out.println("게임 성공 여부: 실패");
-        System.out.print("총 시도한 횟수: " + count);
+        if (winning) System.out.println(success);
+        if (!winning) System.out.println(fail);
+        System.out.print(countingGame + count);
     }
 }
