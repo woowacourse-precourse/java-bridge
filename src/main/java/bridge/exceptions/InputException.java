473646656dcb8e3ee699d06345fc55f4524b7d15
hojunking96 <junsong96@naver.com>
2022-11-17 14:18:00 +0900

    fix(inputException.java) : 메소드 static으로 변경

--- src/main/java/bridge/exceptions/InputException.java
+++ src/main/java/bridge/exceptions/InputException.java
@@ -1,13 +1,13 @@
 package bridge.exceptions;
 
 public class InputException {
-    public void bridgeSizeNotNumber(String bridgeSizeInput) {
+    public static void  bridgeSizeNotNumber(String bridgeSizeInput) {
         for (int i = 0; i < bridgeSizeInput.length(); i++)
             if (!Character.isDigit(bridgeSizeInput.charAt(i)))
                 throw new IllegalArgumentException();
     }
 
-    public void bridgeSizeOutOfRange(int bridgeSize) {
+    public static void bridgeSizeOutOfRange(int bridgeSize) {
         if (bridgeSize < 3 || bridgeSize > 20)
             throw new IllegalArgumentException();
     }
