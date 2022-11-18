4d14966218ba14dc2bb081b17980b484a9c1d7a5
TaeyeonRoyce <skdlxmtpqms@gmail.com>
2022-11-18 16:36:33 +0900

    test(BridgeMapComponentTest): 시그니처로 다리 구성 요소 생성에 대한 테스트를 추가합니다

--- src/test/java/bridge/domain/bridge/model/BridgeMapComponentTest.java
+++ src/test/java/bridge/domain/bridge/model/BridgeMapComponentTest.java
@@ -1,8 +1,11 @@
 package bridge.domain.bridge.model;
 
+import java.util.NoSuchElementException;
 import org.assertj.core.api.Assertions;
 import org.assertj.core.api.SoftAssertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 class BridgeMapComponentTest {
     @Test
@@ -16,5 +19,31 @@ class BridgeMapComponentTest {
         );
     }
 
+    @ParameterizedTest
+    @ValueSource(strings = {"-1", "3"})
+    public void 잘못된_숫자로_생성시_예외_발생(Integer input) {
+        Assertions.assertThatThrownBy(() ->
+                        BridgeMapComponent.fromCode(input))
+                .isInstanceOf(NoSuchElementException.class);
+    }
+
+    @Test
+    public void 시그니처로_생성_하는_경우_테스트() {
+        String up = "U";
+        String down = "D";
+        SoftAssertions.assertSoftly(softAssertions -> {
+                    softAssertions.assertThat(BridgeMapComponent.fromSignature(up)).isEqualTo(BridgeMapComponent.UP);
+                    softAssertions.assertThat(BridgeMapComponent.fromSignature(down)).isEqualTo(BridgeMapComponent.DOWN);
+                }
+        );
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"A", "C", "UP"})
+    public void 잘못된_시그니처로_생성시_경우_예외_발생(String input) {
+        Assertions.assertThatThrownBy(() ->
+                        BridgeMapComponent.fromSignature(input))
+                .isInstanceOf(NoSuchElementException.class);
+    }
 
 }
