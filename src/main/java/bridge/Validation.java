8acb195dfb35aba0aba8fbb4f6ea63822e198835
Seiyon Park <seiyonpdev@gmail.com>
2022-11-19 05:26:41 +0900

    feat(Validation): 메소드 validateMovingChoice 구현
    입력받은 이동 위치의 예외를 처리하는 메소드를 구현했다

--- src/main/java/bridge/Validation.java
+++ src/main/java/bridge/Validation.java
@@ -4,13 +4,13 @@ public class Validation {
     static final int BRIDGE_SIZE_MINIMUM_INCLUSIVE = 3;
     static final int BRIDGE_SIZE_MAXIMUM_INCLUSIVE = 20;
 
-    static void validateBridgeSize(String userInput) {
+    public static void validateBridgeSize(String userInput) {
         validateBridgeSizeIsNumber(userInput);
         validateBridgeSizeOutOfRange(userInput);
     }
 
     private static void validateBridgeSizeIsNumber(String userInput) {
-        if (userInput.matches("\\D+")) {
+        if (!userInput.matches("\\d+")) {
             System.out.println("[ERROR] 다리 길이는 숫자여야 합니다.");
             throw new IllegalArgumentException();
         }
@@ -21,6 +21,13 @@ public class Validation {
 
         if (bridgeSize < BRIDGE_SIZE_MINIMUM_INCLUSIVE || bridgeSize > BRIDGE_SIZE_MAXIMUM_INCLUSIVE) {
             System.out.println("[ERROR] 다리 길이는 3부터 20 사이의 숫자여야 합니다.");
+            throw new IllegalArgumentException();
+        }
+    }
+
+    public static void validateMovingChoice(String userInput) {
+        if (!userInput.matches("[UD]")) {
+            System.out.println("[ERROR] U 또는 D만 입력할 수 있습니다.");
             throw new IllegalArgumentException();
         }
     }
