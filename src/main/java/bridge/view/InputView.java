2082d45a6cb6c02270ad1dc38c57f98adbe8e806
lee <higgs95@naver.com>
2022-11-19 05:26:51 +0900

    feat(view): 이동 관련 입출력 기능 추가

--- src/main/java/bridge/view/InputView.java
+++ src/main/java/bridge/view/InputView.java
@@ -11,6 +11,7 @@ public class InputView {
      * 다리의 길이를 입력받는다.
      */
     public static int readBridgeSize() {
+        System.out.println("다리의 길이를 입력해주세요.");
         String size = readLine();
         try {
             validateDigit(size);
@@ -25,7 +26,8 @@ public class InputView {
     /**
      * 사용자가 이동할 칸을 입력받는다.
      */
-    public String readMoving() {
+    public static String readMoving() {
+        System.out.println("이동할 칸을 선택해주세요. (위: U, 아래: D)");
         String moving = readLine();
         try{
             validateMoving(moving);
@@ -40,6 +42,7 @@ public class InputView {
      * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
      */
     public String readGameCommand() {
+        System.out.println("게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)");
         String command = readLine();
         try{
             validateRetryOrExit(command);
--- src/main/java/bridge/view/OutputView.java
+++ src/main/java/bridge/view/OutputView.java
@@ -1,16 +1,25 @@
 package bridge.view;
 
+import java.util.List;
+import java.util.StringJoiner;
+
 /**
  * 사용자에게 게임 진행 상황과 결과를 출력하는 역할을 한다.
  */
 public class OutputView {
-
+        private static final String mapStarter = "[ ";
+        private static final String mapFinisher = " ]";
+        private static final String mapSeparator = " | ";
     /**
      * 현재까지 이동한 다리의 상태를 정해진 형식에 맞춰 출력한다.
      * <p>
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void printMap() {
+    public static void printMap(List<List<String>> playingMap) {
+        for(List<String> playingBridge: playingMap) {
+            String bridgeMap = toStringOf(playingBridge);
+            System.out.println(bridgeMap);
+        }
     }
 
     /**
@@ -18,6 +27,19 @@ public class OutputView {
      * <p>
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void printResult() {
+//    public void printResult(PlayRecord playRecord) {
+//        System.out.println("최종 게임 결과");
+//        printMap(playRecord);
+//        System.out.println();
+//        System.out.println("게임 성공 여부: ");
+//        System.out.println("총 시도한 횟수: ");
+//    }
+
+    private static String toStringOf(List<String> playingBridge) {
+        StringJoiner stringJoiner = new StringJoiner(mapSeparator,mapStarter,mapFinisher);
+        for(String result : playingBridge) {
+            stringJoiner.add(result);
+        }
+        return stringJoiner.toString();
     }
 }
