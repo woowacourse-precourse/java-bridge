2b39dc97a214766f2e5d0ff2e49112931a240df2
1eejisoo <jisu991012@gmail.com>
2022-11-19 02:35:57 +0900

    feat: 최종 게임 결과 출력 기능 추가

--- src/main/java/view/OutputView.java
+++ src/main/java/view/OutputView.java
@@ -13,6 +13,9 @@ import java.util.Arrays;
  */
 public class OutputView {
     private static final String START_MESSAGE = "다리 건너기 게임을 시작합니다.";
+    private static final String END_MESSAGE = "최종 게임 결과";
+    private static final String SUCCESS_OR_FAIL = "게임 성공 여부: ";
+    private static final String TOTAL_NUMBER = "총 시도한 횟수: ";
     private static StringBuilder upSideBridge;
     private static StringBuilder downSideBridge;
 
@@ -44,5 +47,8 @@ public class OutputView {
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
     public void printResult(int recursiveCount, String result) {
+        System.out.println(END_MESSAGE);
+        System.out.println(upSideBridge + "\n" + downSideBridge + "\n");
+        System.out.println(SUCCESS_OR_FAIL + result + "\n" + TOTAL_NUMBER + recursiveCount);
     }
 }
