ee9871916dc056506460843822f1159347fa9373
Seokhyun Lee <lseokhyun99@gmail.com>
2022-11-18 14:41:04 +0900

    feat: 다리 길이 오류 체크 메소드 구현
    다리 길이 입력 시 오류를 체크하는 메소드
    readBridgeSizeErrorCheck()를 구현하였다.
    
    입력된 다리의 길이가
    (1) 숫자의 형식인지
    (2) 3~20 사이의 숫자인지 확인한다.

--- src/main/java/bridge/BridgeInputErrorCheck.java
+++ src/main/java/bridge/BridgeInputErrorCheck.java
@@ -1,5 +1,23 @@
 package bridge;
 
-public class BridgeInputErrorCheck {
+import java.util.regex.Pattern;
 
+public class BridgeInputErrorCheck {
+    private static final String ERROR_MESSAGE = "[ERROR]";
+    private static final String NOT_NUMBER_ERROR = ERROR_MESSAGE+" 숫자를 입력해야 합니다.";
+    private static final String NUMBER_SIZE_ERROR = ERROR_MESSAGE+" 다리 길이는 3부터 20 사이의 숫자여야 합니다.";
+    private static final String NUMBER_PATTERN = "^[0-9]{1,}$";
+    private static final int BRIDGE_SIZE_MIN = 3;
+    private static final int BRIDGE_SIZE_MAX = 20;
+
+
+    public void readBridgeSizeErrorCheck(String bridgeSize){
+
+        if(!Pattern.matches(NUMBER_PATTERN, bridgeSize))
+            throw new IllegalArgumentException(NOT_NUMBER_ERROR);
+
+        int size = Integer.parseInt(bridgeSize);
+        if( size < BRIDGE_SIZE_MIN || size > BRIDGE_SIZE_MAX)
+            throw new IllegalArgumentException(NUMBER_SIZE_ERROR);
+    }
 }
