1800f3e5cc450ec3877d9ab6434db906b0cfdcd4
yangjiyoung <0112jyoung@naver.com>
2022-11-18 13:31:34 +0900

    refactor: 오타 수정

--- src/main/java/bridge/OutputView.java
+++ src/main/java/bridge/OutputView.java
@@ -2,7 +2,7 @@ package bridge;
 
 public class OutputView {
     public void printStartGame(){
-        System.out.println("다리 건너기 게이믕ㄹ 시작합니다.");
+        System.out.println("다리 건너기 게임을 시작합니다.");
     }
 
     public void printMap() {
