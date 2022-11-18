7d33527726711ae934ad0abdb0ef2af64663064f
Ilchul Kim <huj0159@gmail.com>
2022-11-18 21:38:59 +0900

    feat: check bridge size is valid

--- src/main/java/bridge/InputView.java
+++ src/main/java/bridge/InputView.java
@@ -1,5 +1,7 @@
 package bridge;
 
+import java.util.regex.Pattern;
+
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
  */
@@ -8,8 +10,25 @@ public class InputView {
     /**
      * 다리의 길이를 입력받는다.
      */
-    public int readBridgeSize() {
-        return 0;
+    public int readBridgeSize(String userInput){
+        isDigit(userInput);
+
+        int bridgeSize = Integer.parseInt(userInput);
+        isValidBridgeSize(bridgeSize);
+
+        return bridgeSize;
+    }
+
+    private void isDigit(String userInput) {
+        if (!Pattern.matches("^[0-9]+$", userInput)) {
+            throw new IllegalArgumentException("[ERROR] 숫자만 입력해야 합니다");
+        }
+    }
+
+    private void isValidBridgeSize(int bridgeSize) {
+        if (bridgeSize < 3 || bridgeSize > 20) {
+            throw new IllegalArgumentException("[ERROR] 3이상 20이하의 수만 입력해야 합니다");
+        }
     }
 
     /**
@@ -18,7 +37,6 @@ public class InputView {
     public String readMoving() {
         return null;
     }
-
     /**
      * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
      */
