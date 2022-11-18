7d060e597b29092639794826f64e2a6ccff2e12a
jipyo0106 <68218081+jipyo0106@users.noreply.github.com>
2022-11-18 22:24:48 +0900

    feat: 게임 실행 기능

---
+++ src/main/java/bridge/GamePlayer.java
@@ -0,0 +1,44 @@
+package bridge;
+
+public class GamePlayer {
+    private final InputView inputView;
+    private final OutputView outputView;
+    private final BridgeGame bridgeGame;
+    private int retryCount;
+
+    public GamePlayer() {
+        this.inputView = new InputView();
+        this.outputView = new OutputView();
+        this.bridgeGame = new BridgeGame(readBridgeSize());
+        retryCount = 0;
+    }
+
+    public void run() {
+        String restartOrQuit = "";
+        String upOrDown;
+        boolean isNotMovalbe = false;
+
+        do {
+            upOrDown = inputView.readMoving();
+            bridgeGame.move(upOrDown);
+
+            if (bridgeGame.isEnd()) {
+                break;
+            }
+
+            if (!bridgeGame.isMovable()) {
+                restartOrQuit = inputView.readGameCommand();
+
+                if (restartOrQuit.equals(GameKeySet.RESTART.getKeySet())) {
+                    bridgeGame.retry();
+                    retryCount++;
+                }
+            }
+
+        } while (restartOrQuit != GameKeySet.QUIT.getKeySet());
+    }
+
+    private int readBridgeSize() {
+        return inputView.readBridgeSize();
+    }
+}
