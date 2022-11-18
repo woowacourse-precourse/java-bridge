63649047159acabbe095903ac6850efcca4d9f60
sugamypapa <sk000801@naver.com>
2022-11-18 23:09:49 +0900

    fix: input 클래스 메서드 문제 조건에 맞게 수정

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -1,6 +1,7 @@
 package view;
 
 import camp.nextstep.edu.missionutils.Console;
+import game.GameAnnounce;
 
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
@@ -10,22 +11,28 @@ public class InputView {
     /**
      * 다리의 길이를 입력받는다.
      */
-    public int readBridgeSize(String enterLength) {
-        Valid.isLengthValid(enterLength);
-        return Integer.parseInt(enterLength);
+    public String readBridgeSize() {
+        System.out.println(GameAnnounce.start.getAnnouncement());
+        return Console.readLine();
     }
 
     /**
      * 사용자가 이동할 칸을 입력받는다.
      */
     public String readMoving() {
-        return null;
+        System.out.println(GameAnnounce.userMove.getAnnouncement());
+        String moving = Console.readLine();
+        Valid.isEnterValid(moving);
+        return moving;
     }
 
     /**
      * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
      */
     public String readGameCommand() {
-        return null;
+        System.out.println(GameAnnounce.userRestart.getAnnouncement());
+        String finish = Console.readLine();
+        Valid.isEnterFinishValid(finish);
+        return finish;
     }
 }
