53be5d7ff997fb08be387d67e3b1b981382de757
joohyeongKim <wnguddl96@knou.ac.kr>
2022-11-19 03:23:53 +0900

    feat : 출력 기능 생성

--- src/main/java/bridge/OutputView.java
+++ src/main/java/bridge/OutputView.java
@@ -1,16 +1,29 @@
 package bridge;
 
+import static bridge.UserInterface.INSERT_SIZE;
+import static bridge.UserInterface.RETRY;
+import static bridge.UserInterface.SELECT_ROW;
+import static bridge.UserInterface.START;
+import static bridge.UserInterface.SUCCESSFUL;
+import static bridge.UserInterface.TRY_COUNT;
+
+import java.util.List;
+
 /**
  * 사용자에게 게임 진행 상황과 결과를 출력하는 역할을 한다.
  */
 public class OutputView {
+
+    private String guideLine;
+    FrontController frontController;
 
     /**
      * 현재까지 이동한 다리의 상태를 정해진 형식에 맞춰 출력한다.
      * <p>
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void printMap() {
+    public List<String> printMap() {
+        return frontController.bridgeRepository();
     }
 
     /**
@@ -18,6 +31,18 @@ public class OutputView {
      * <p>
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void printResult() {
+    public String printResult(String userInterface) {
+
+        return userInterface;
+    }
+
+
+    public void setPrint() {
+        printResult(START.interact());
+        printResult(INSERT_SIZE.interact());
+        printResult(SELECT_ROW.interact());
+        printResult(RETRY.interact());
+        printResult(SUCCESSFUL.interact());
+        printResult(TRY_COUNT.interact());
     }
 }
