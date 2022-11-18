a475d09288c04e4d07499ff593aa6f394acbcd86
chaerim <qkrdbsk28@naver.com>
2022-11-18 23:51:11 +0900

    test: 생성된 다리 길이가 사용자 입력 값과 동일한지 테스트

---
+++ src/test/java/bridge/BridgeGameTest.java
@@ -0,0 +1,17 @@
+package bridge;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class BridgeGameTest {
+
+    @DisplayName("생성된 다리 길이가 입력값과 동일한지 테스트")
+    @Test()
+    public void createBridge() {
+        BridgeGame bridgeGame = new BridgeGame(5);
+        assertThat(bridgeGame.getBridgeSize())
+                .isEqualTo(5);
+    }
+}
