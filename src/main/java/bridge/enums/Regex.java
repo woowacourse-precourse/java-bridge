85973727fa63077dd92eb3d91383211a9504c60b
jeongwle <dldl4568@naver.com>
2022-11-18 18:39:37 +0900

    feat: Regex enum 초안 작성

---
+++ src/main/java/bridge/enums/Regex.java
@@ -0,0 +1,18 @@
+package bridge.enums;
+
+public enum Regex {
+    BRIDGE_SIZE_REGEX("^[0-9]+$"),
+    MOVING_DIRECTION_REGEX("^[U|D]{1}$"),
+    GAME_COMMAND_REGEX("^[Q|D]{1}$")
+    ;
+    private final String regex;
+
+    Regex(String regex) {
+        this.regex = regex;
+    }
+
+    @Override
+    public String toString() {
+        return regex;
+    }
+}
