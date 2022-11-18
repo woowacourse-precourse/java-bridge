2a37c7a8be5a4eb6c21cdb03396f82b055984a56
sosow0212 <sosow0212@naver.com>
2022-11-18 15:32:08 +0900

    refactor : (BridgeGame) 요구 사항에 맞게 View 제거

--- src/main/java/bridge/service/BridgeGame.java
+++ src/main/java/bridge/service/BridgeGame.java
@@ -4,19 +4,10 @@ package bridge.service;
  * 다리 건너기 게임을 관리하는 클래스
  */
 public class BridgeGame {
-    private final InputService inputService = new InputService();
 
     // 여기서 View를 쓰면 안됨!
     // 메서드 이름은 변경하면 안됨! / 인자와 반환은 추가 및 변경 가능
     // 메서드 추가 가능
-
-    public void gameStart() {
-        inputService.printGameStart();
-    }
-
-    public int getBridgeSize() {
-        return inputService.requestBridgeSize();
-    }
 
     /**
      * 사용자가 칸을 이동할 때 사용하는 메서드
