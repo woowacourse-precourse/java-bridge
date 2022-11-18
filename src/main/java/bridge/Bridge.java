24832beb70f48b2ed97f9afdd7440a2180f9bcf5
swonny <wonny921@gmail.com>
2022-11-18 15:26:28 +0900

    feat(Bridge): 사용자의 위치 업데이트 메소드 구현

--- src/main/java/bridge/Bridge.java
+++ src/main/java/bridge/Bridge.java
@@ -3,8 +3,8 @@ package bridge;
 import java.util.List;
 
 public class Bridge {
-    int playerPosition;
-    final List<String> availableBlocks;
+    private int playerPosition;
+    private final List<String> availableBlocks;
 
     public Bridge(List<String> availableBlocks) {
         this.availableBlocks = availableBlocks;
@@ -12,5 +12,9 @@ public class Bridge {
 
     public boolean isNextAvailable(Position position) {
         return availableBlocks.get(playerPosition + 1).equals(position);
+    }
+
+    public void movePlayer() {
+        playerPosition++;
     }
 }
