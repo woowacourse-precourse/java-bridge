f2473fe1737c407e5e58040220cd897dfeff8b90
taeyun-work <wmfajr1357@gmail.com>
2022-11-18 21:22:10 +0900

    feat: 입력받은 다리길이로 다리를 생성하는 기능 구현
    BridgeMaker 클래스에 입력 받은 다리 길이로 다리를 생성하는 메소드를 구현함

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -1,16 +1,24 @@
 package bridge;
 
+import java.util.List;
+
 public class Application {
 
     private static OutputView outputView = new OutputView();
     private static InputView inputView = new InputView();
-
+    private static BridgeRandomNumberGenerator bridgeRandomNumberGenerator = new BridgeRandomNumberGenerator();
+    private static BridgeMaker bridgeMaker = new BridgeMaker(bridgeRandomNumberGenerator);
     public static void main(String[] args) {
         // TODO: 프로그램 구현
 
+        // 게임을 시작하는 문구 출력
         outputView.printStartGame();
+        
+        // 다리 길이 입력 받는 안내 문구 출력
         outputView.printInputBridgeLength();
         int bridgeSize;
+        
+        // 다리길이 입력받기 + 예외 시 추가적으로 계속 입력
         while(true) {
             try {
                 bridgeSize = inputView.readBridgeSize();
@@ -19,6 +27,8 @@ public class Application {
                 System.out.println(e);
             }
         }
+
+        List<String> bridge = bridgeMaker.makeBridge(bridgeSize);
 
     }
 }
