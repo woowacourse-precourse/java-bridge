a4fed82b37e51a0f7fffb488432401513d0916ad
minju412 <ln8847@ajou.ac.kr>
2022-11-18 23:36:34 +0900

    test(DirectionTest): add tests
    - 방향이 공백이라면 예외가 발생한다
    - 방향이 U 또는 D 가 아니라면 예외가 발생한다

---
+++ src/test/java/bridge/domain/DirectionTest.java
@@ -0,0 +1,23 @@
+package bridge.domain;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class DirectionTest {
+
+    @DisplayName("방향이 공백이라면 예외가 발생한다.")
+    @Test
+    void createDirectionWithBlank() {
+        assertThatThrownBy(() -> new Direction(""))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("방향이 U 또는 D 가 아니라면 예외가 발생한다.")
+    @Test
+    void createDirectionWithInvalidCharacter() {
+        assertThatThrownBy(() -> new Direction("r"))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+}
