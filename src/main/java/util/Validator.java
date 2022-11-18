1c8bb2920b7c33f615531c527624f3ea03876e2b
Devtist <yt930310@snu.ac.kr>
2022-11-18 21:19:49 +0900

    feat: 다리 길이의 입력이 숫자인지 검증하는 기능 구현

--- src/main/java/util/Validator.java
+++ src/main/java/util/Validator.java
@@ -3,9 +3,17 @@ package util;
 import java.util.List;
 
 public class Validator {
+	private static final String NUMBER_REGEX = "\\d+";
+
 	public static void validateBridgeSize(List<String> bridge, int size) {
 		if (bridge.size() != size) {
 			throw new IllegalArgumentException("[ERROR] 다리의 길이가 입력받은 길이와 같지 않습니다.");
+		}
+	}
+
+	public static void validateSizeIsNumber(String number) {
+		if (!number.matches(NUMBER_REGEX)) {
+			throw new IllegalArgumentException("[ERROR] 다리의 길이는 숫자여야 합니다.");
 		}
 	}
 }
