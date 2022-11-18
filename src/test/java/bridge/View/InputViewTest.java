2521cb4653b8ce87d82a6132af01219b1984657f
skck0226 <skck0226@ajou.ac.kr>
2022-11-18 11:05:09 +0900

    [test] (InputViewTest) 이동방향 입력 테스트

--- src/test/java/bridge/View/InputViewTest.java
+++ src/test/java/bridge/View/InputViewTest.java
@@ -1,7 +1,6 @@
 package bridge.View;
 
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
@@ -28,5 +27,24 @@ class InputViewTest {
         assertInputByMockingTest(()->{
             assertEquals(inputView.readBridgeSize(),Integer.parseInt(bridge_size_input));
         },bridge_size_input);
+    }
+
+
+    @DisplayName("이동방향 입력 성공 케이스 테스트")
+    @ParameterizedTest(name = "{index} {displayName} message={0}")
+    @ValueSource(strings = {"U","D","U","D"})
+    void 이동방향_입력_성공케이스_테스트(String moving_direction) {
+        assertInputByMockingTest(()->{
+            assertEquals(inputView.readMoving(),moving_direction);
+        },moving_direction);
+    }
+
+    @DisplayName("이동방향 입력 실패 케이스 테스트")
+    @ParameterizedTest(name = "{index} {displayName} message={0}")
+    @ValueSource(strings = {"UD","DU","u","d","마","1","#"})
+    void 이동방향_입력_실패케이스_테스트(String moving_direction) {
+        assertInputByMockingTest(()->{
+            assertThrows(IllegalArgumentException.class, () -> inputView.readMoving());
+        },moving_direction);
     }
 }
