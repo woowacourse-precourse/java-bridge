f771e4d32e7d419d3d76180327cae4f7eb62dd9a
ksh-beginner <ohk9134@naver.com>
2022-11-18 09:23:46 +0900

    feat(PrintBridge) : 출력용 shape를 생성하는 기능 추가

--- src/main/java/bridge/model/PrintBridge.java
+++ src/main/java/bridge/model/PrintBridge.java
@@ -1,6 +1,27 @@
 package bridge.model;
 
+import java.util.ArrayList;
+import java.util.List;
 
 public class PrintBridge {
 
+    private static final int UP_SHAPE_INDEX = 0;
+    private static final int DOWN_SHAPE_INDEX = 1;
+
+    private final List<String> shape = new ArrayList<>();
+
+    public void generateShape(List<String> playerUpShape, List<String> playerDownShape) {
+        String replacedPlayerUpShape = playerUpShape.toString().replace(",", "|");
+        String replacedPlayerDownShape = playerDownShape.toString().replace(",", "|");
+        shape.add(replacedPlayerUpShape);
+        shape.add(replacedPlayerDownShape);
+    }
+
+    public String getUpShape() {
+        return shape.get(UP_SHAPE_INDEX);
+    }
+
+    public String getDownShape() {
+        return shape.get(DOWN_SHAPE_INDEX);
+    }
 }
