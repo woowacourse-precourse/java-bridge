d8575960e9a0f84d2b64c28e71c5b3a535b050ff
anneu <b.imnuj@gmail.com>
2022-11-18 14:56:55 +0900

    feat: 다리 건너기 결과 출력 기능 구현

--- src/main/java/bridge/view/OutputView.java
+++ src/main/java/bridge/view/OutputView.java
@@ -1,5 +1,6 @@
 package bridge.view;
 
+import bridge.BridgeGameResult;
 import java.util.List;
 
 /**
@@ -17,10 +18,13 @@ public class OutputView {
 
     /**
      * 현재까지 이동한 다리의 상태를 정해진 형식에 맞춰 출력한다.
-     * <p>
-     * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void printMap() {
+    public void printMap(BridgeGameResult bridgeGameResult) {
+        List<String> upStates = bridgeGameResult.getUpStates();
+        List<String> downStates = bridgeGameResult.getDownStates();
+
+        System.out.println(format(upStates));
+        System.out.println(format(downStates));
     }
 
     private String format(List<String> states) {
