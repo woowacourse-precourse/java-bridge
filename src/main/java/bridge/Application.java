a34618c6ae321191e2831d35542570b6fd08b906
taeyun-work <wmfajr1357@gmail.com>
2022-11-18 21:00:55 +0900

    feat: 다리길이 입력받는 기능 구현
    InputView클래스에서 다리 길이를 입력받는 메소드를 구현함

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -3,11 +3,12 @@ package bridge;
 public class Application {
 
     private static OutputView outputView = new OutputView();
+    private static InputView inputView = new InputView();
     public static void main(String[] args) {
         // TODO: 프로그램 구현
 
         outputView.printStartGame();
         outputView.printInputBridgeLength();
-
+        int bridgeSize = inputView.readBridgeSize();
     }
 }
