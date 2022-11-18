0ea0bdcc00d83c4f12ab937ba1829c6dd2b038cd
Guga <103228463+BGuga@users.noreply.github.com>
2022-11-18 14:44:27 +0900

    feat: 에러 메시지를 출력하는 기능 추가

--- src/main/java/bridge/view/OutputView.java
+++ src/main/java/bridge/view/OutputView.java
@@ -20,4 +20,8 @@ public class OutputView {
      */
     public void printResult() {
     }
+
+    public void printErrorMessage(String errorMessage) {
+        System.out.println(errorMessage);
+    }
 }
