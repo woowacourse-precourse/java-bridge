6e3b4eba2d6355a6e0a4e1f78f16cd00e5c16b23
KimTaeKang57 <zizoen123@gmail.com>
2022-11-18 14:43:29 +0900

    refac: OutputView : printMap() 메소드 분리 및 리팩토링

--- src/main/java/bridge/OutputView.java
+++ src/main/java/bridge/OutputView.java
@@ -13,58 +13,57 @@ public class OutputView {
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
     public void printMap(BridgeGame bridgeGame) {
-        System.out.println("[" + getUpPrint(bridgeGame.getBridge(), bridgeGame.getUsersMove()) + "]");
-        System.out.println("[" + getDownPrint(bridgeGame.getBridge(), bridgeGame.getUsersMove()) + "]");
+        System.out.print("[");
+        getUpPrint(bridgeGame.getBridge(), bridgeGame.getUsersMove());
+        System.out.println("]");
+        System.out.print("[");
+        getDownPrint(bridgeGame.getBridge(), bridgeGame.getUsersMove());
+        System.out.println("]");
     }
 
-    private String getDownPrint(List<String> bridge, List<String> usersMove) {
-        StringBuilder down = new StringBuilder();
+    private void getDownPrint(List<String> bridge, List<String> usersMove) {
         for (int idx = 0; idx < usersMove.size(); idx++) {
-            if (!(usersMove.get(idx).equals("D"))) {
-                if (idx != 0) {
-                    down.append("|");
-                }
-                down.append("   ");
-                continue;
-            }
-            if (bridge.get(idx).equals(usersMove.get(idx))) {
-                if (idx != 0) {
-                    down.append("|");
-                }
-                down.append(" O ");
-                continue;
-            }
-            if (idx != 0) {
-                down.append("|");
-            }
-            down.append(" X ");
+            if (printBlank(usersMove, idx, "D")) continue;
+            if (printO(bridge, usersMove, idx)) continue;
+            printX(idx);
         }
-        return down.toString();
     }
 
-    private String getUpPrint(List<String> bridge, List<String> usersMove) {
-        StringBuilder up = new StringBuilder();
+    private void getUpPrint(List<String> bridge, List<String> usersMove) {
         for (int idx = 0; idx < usersMove.size(); idx++) {
-            if (!(usersMove.get(idx).equals("U"))) {
-                if (idx != 0) {
-                    up.append("|");
-                }
-                up.append("   ");
-                continue;
-            }
-            if (bridge.get(idx).equals(usersMove.get(idx))) {
-                if (idx != 0) {
-                    up.append("|");
-                }
-                up.append(" O ");
-                continue;
-            }
-            if (idx != 0) {
-                up.append("|");
-            }
-            up.append(" X ");
+            if (printBlank(usersMove, idx, "U")) continue;
+            if (printO(bridge, usersMove, idx)) continue;
+            printX(idx);
         }
-        return up.toString();
+    }
+
+    private boolean printBlank(List<String> usersMove, int idx, String d) {
+        if (!(usersMove.get(idx).equals(d))) {
+            if (idx != 0) {
+                System.out.print("|");
+            }
+            System.out.print("   ");
+            return true;
+        }
+        return false;
+    }
+
+    private void printX(int idx) {
+        if (idx != 0) {
+            System.out.print("|");
+        }
+        System.out.print(" X ");
+    }
+
+    private boolean printO(List<String> bridge, List<String> usersMove, int idx) {
+        if (bridge.get(idx).equals(usersMove.get(idx))) {
+            if (idx != 0) {
+                System.out.print("|");
+            }
+            System.out.print(" O ");
+            return true;
+        }
+        return false;
     }
 
     /**
