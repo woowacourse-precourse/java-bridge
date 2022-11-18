c265c725ce57a0963dcc779a04d94dcefd2609b1
yumyeonghan <mgtmh991013@gmail.com>
2022-11-19 03:06:34 +0900

    feat: printResult method implementation
    Implement the result output method

--- src/main/java/view/OutputView.java
+++ src/main/java/view/OutputView.java
@@ -9,6 +9,9 @@ import java.util.stream.Collectors;
  * 사용자에게 게임 진행 상황과 결과를 출력하는 역할을 한다.
  */
 public class OutputView {
+    private static final String SUCCESS = "성공";
+    private static final String FAIL = "실패";
+
     private final BridgeGame bridgeGame;
     public final List<String> upOutputBoard;
     public final List<String> downOutputBoard;
@@ -188,6 +191,13 @@ public class OutputView {
      * <p>
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void printResult() {
+    public void printResult(OutputView outputView, boolean successOrNot, int count) {
+        System.out.println("최종 게임 결과");
+        System.out.println(upOutputBoard.stream().collect(Collectors.joining()));
+        System.out.println(downOutputBoard.stream().collect(Collectors.joining()));
+        System.out.println();
+        if (successOrNot) System.out.println("게임 성공 여부: " + SUCCESS);
+        if (!successOrNot) System.out.println("게임 성공 여부: " + FAIL );
+        System.out.println("총 시도한 횟수: " + count);
     }
 }
