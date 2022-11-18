2af4d5f0856edb0b32cf6312a483c99a60630fe2
Sieun Park <psieun0606@gmail.com>
2022-11-19 05:16:52 +0900

    feat: 자동으로 생성할 다리 길이를 입력 받는 기능 추가

--- src/main/java/bridge/View/InputView.java
+++ src/main/java/bridge/View/InputView.java
@@ -1,15 +1,20 @@
 package bridge.View;
 
+import camp.nextstep.edu.missionutils.Console;
+
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
  */
 public class InputView {
+    private static final String BRIDGE_SIZE_PHRASE = "다리의 길이를 입력해주세요.";
 
     /**
      * 다리의 길이를 입력받는다.
      */
-    public int readBridgeSize() {
-        return 0;
+    public static int readBridgeSize() {
+        System.out.println(BRIDGE_SIZE_PHRASE);
+        String userInputLengthOfBridge = Console.readLine();
+        return Integer.parseInt(userInputLengthOfBridge);
     }
 
     /**
