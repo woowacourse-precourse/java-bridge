9a94f5254be739408f531ee7b32ab1199e8a198f
anneu <b.imnuj@gmail.com>
2022-11-18 14:39:50 +0900

    feat: 다리 건너기 결과를 생성하는 기능 구현

--- src/main/java/bridge/BridgeGameResult.java
+++ src/main/java/bridge/BridgeGameResult.java
@@ -8,11 +8,31 @@ import java.util.List;
  */
 public class BridgeGameResult {
 
+    private static final String UP = "U";
+    private static final String DOWN = "D";
+    private static final String BLANK = " ";
+
     private final List<String> upStates;
     private final List<String> downStates;
 
     public BridgeGameResult() {
         this.upStates = new ArrayList<>();
         this.downStates = new ArrayList<>();
+    }
+
+    /**
+     * @param moving    이동할 칸
+     * @param moveState 이동 상태 (가능:O, 불가능:X)
+     */
+    public void createResult(String moving, String moveState) {
+        if (moving.equals(UP)) {
+            upStates.add(moveState);
+            downStates.add(BLANK);
+        }
+
+        if (moving.equals(DOWN)) {
+            downStates.add(moveState);
+            upStates.add(BLANK);
+        }
     }
 }
