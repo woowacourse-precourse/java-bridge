c60102a088d75f84c27e3b0dc1003a5c41120eca
jthugg <jthug@naver.com>
2022-11-19 03:27:37 +0900

    Feat<OutputView>: 상황별 메세지 출력 기능 추가

--- src/main/java/bridge/OutputView.java
+++ src/main/java/bridge/OutputView.java
@@ -20,4 +20,24 @@ public class OutputView {
      */
     public void printResult() {
     }
+
+    public void printStart() {
+        System.out.println("다리 건너기 게임을 시작합니다.");
+    }
+
+    public void printRequestBridgeSize() {
+        System.out.println("다리의 길이를 입력해주세요.");
+    }
+
+    public void printRequestUpDown() {
+        System.out.println("이동할 칸을 선택해주세요. (위: U, 아래: D)");
+    }
+
+    public void printRequestRetry() {
+        System.out.println("게임을 다시 시도할 지 여부를 입력해주세요. (재시도: R, 종료: Q)");
+    }
+
+    public void printErrorMessage(String errorMessage) {
+        System.out.println("[ERROR] " + errorMessage);
+    }
 }
