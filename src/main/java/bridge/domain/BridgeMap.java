47d105c8d8b5dd9096ec049db355afcf7149135a
jjmin9797 <jjmin9797@naver.com>
2022-11-18 19:03:49 +0900

    feat(formatMap): 다리 출력 형식 구현

--- src/main/java/bridge/domain/BridgeMap.java
+++ src/main/java/bridge/domain/BridgeMap.java
@@ -1,11 +1,12 @@
 package bridge.domain;
 
 public enum BridgeMap {
-    BRIDGE_START_MARK("["),
-    BRIDGE_END_MARK("]"),
+    BRIDGE_START_MARK("[ "),
+    BRIDGE_END_MARK(" ]"),
     BRIDGE_DIVISION_MARK(" | "),
     SUCCESS_MARK("O"),
-    FAIL_MARK("X");
+    FAIL_MARK("X"),
+    BLANK_MARK(" ");
 
     private final String mark;
 
--- src/main/java/bridge/service/MapMakerService.java
+++ src/main/java/bridge/service/MapMakerService.java
@@ -1,5 +1,8 @@
 package bridge.service;
 
+import bridge.domain.BridgeMap;
+
+import java.util.Arrays;
 import java.util.List;
 
 public class MapMakerService {
@@ -9,4 +12,21 @@ public class MapMakerService {
     public MapMakerService(List<String> bridge) {
         this.bridge = bridge;
     }
+
+    public List<String> makeMap(List<String> userBridge) {
+        String topLow = "";
+        String bottomLow = "";
+        for (int i = 0; i < userBridge.size(); i++) {
+
+        }
+        topLow = formatMap(topLow);
+        bottomLow = formatMap(bottomLow);
+        return Arrays.asList(topLow,bottomLow);
+    }
+
+    public String formatMap(String low) {
+        return BridgeMap.BRIDGE_START_MARK.getMark()+low+BridgeMap.BRIDGE_END_MARK;
+    }
+
+
 }
