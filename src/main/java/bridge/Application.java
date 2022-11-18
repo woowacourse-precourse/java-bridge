a7a034d24ed70a9b9df4319ebaf11c0d170ace01
junpakPark <112045553+junpakPark@users.noreply.github.com>
2022-11-18 21:09:46 +0900

    feat(Application): Application 객체 생성

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -1,8 +1,11 @@
 package bridge;
 
+import bridge.controller.GameController;
+
 public class Application {
 
-    public static void main(String[] args) {
-        // TODO: 프로그램 구현
-    }
+	public static void main(String[] args) {
+		GameController gameController = new GameController();
+		gameController.run();
+	}
 }
