3beb9989f3bb6d62b15a0d17040c5e0dd2685285
H4R1B0 <qkrguswns25@naver.com>
2022-11-18 15:54:31 +0900

    feat: realize full game progress

--- src/main/java/bridge/BridgeGame.java
+++ src/main/java/bridge/BridgeGame.java
@@ -1,12 +1,49 @@
 package bridge;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
  * 다리 건너기 게임을 관리하는 클래스
  */
 public class BridgeGame {
+    private static final String BRIDGE_GAME_START = "다리 건너기 게임을 시작합니다.";
+    private static final String GAME_WIN = "성공";
+    private static final String GAME_LOSE = "실패";
     private static final InputController inputController = new InputController();
+    private static final OutputController outputController = new OutputController();
+    private static final BridgeMaker bridgeMaker = new BridgeMaker(new BridgeRandomNumberGenerator());
+    private int tryCount = 1;
+    private int currentPosition = 0;
+    private List<Bridge> currentBridge = new ArrayList<>();
+
+    public void start() {
+        System.out.println(BRIDGE_GAME_START);
+        final int bridgeSize = inputController.inputBridgeSize();
+        final List<String> bridge = bridgeMaker.makeBridge(bridgeSize);
+        gameLoop(bridge, bridgeSize);
+    }
+
+    private void gameLoop(List<String> bridge, int bridgeSize) {
+        while (currentPosition < bridgeSize) {
+            Bridge moveResult = move(bridge, currentPosition);
+            currentBridge.add(moveResult);
+            outputController.printMap(currentBridge);
+            if (moveResult.getUpShape().equals("X") || moveResult.getDownShape().equals("X")) {
+                boolean retryResult = retry();
+                if (!retryResult) {
+                    outputController.printResult(currentBridge, GAME_LOSE, tryCount);
+                    return;
+                }
+                currentBridge = new ArrayList<>();
+                currentPosition = 0;
+                tryCount++;
+                continue;
+            }
+            currentPosition++;
+        }
+        outputController.printResult(currentBridge, GAME_WIN, tryCount);
+    }
 
     /**
      * 사용자가 칸을 이동할 때 사용하는 메서드
