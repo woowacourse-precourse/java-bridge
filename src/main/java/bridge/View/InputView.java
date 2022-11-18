091ed25727ff04f771496785b170519d84eaae32
Kwon-sang <kwonsg514@gmail.com>
2022-11-18 12:37:22 +0900

    [다리 길이 입력 구현] 다리길이 입력메서드 및 검증메서드 구현

--- src/main/java/bridge/View/InputView.java
+++ src/main/java/bridge/View/InputView.java
@@ -1,5 +1,7 @@
+
 package bridge.View;
 
+import camp.nextstep.edu.missionutils.*;
 /*
 * InputView 패키지는 변경할 수 있다.
 * InputView 메서드의 시그니처(인자, 이름)와 반환 타입은 변경할 수 있다.
@@ -15,7 +17,14 @@ public class InputView {
      * 다리의 길이를 입력받는다.
      */
     public int readBridgeSize() {
-        return 0;
+        System.out.println(Messeges.INPUT_BRIDGE_LENGTH.msg);
+        String bridgeSizeInput = readGameCommand();
+        try {
+            validateBridgeSize(bridgeSizeInput);
+        }catch (IllegalArgumentException e) {
+            readBridgeSize();
+        }
+        return Integer.parseInt(bridgeSizeInput);
     }
 
     /**
@@ -29,6 +38,13 @@ public class InputView {
      * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
      */
     public String readGameCommand() {
-        return null;
+        return Console.readLine();
+    }
+
+    private void validateBridgeSize(String bridgeSizeInput) {
+        String regex = "^[3-9]{1}$|^[1]{1}[0-9]{1}$|^[2]{1}[0]{1}$";
+        if (!bridgeSizeInput.matches(regex)) {
+            throw new IllegalArgumentException();
+        }
     }
 }
