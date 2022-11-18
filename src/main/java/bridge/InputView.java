117c8f7f55043e04206b7d7b9bc0cf9de4ae02f0
MuSeung <peter0757@nvaer.com>
2022-11-18 15:20:00 +0900

    feat: InputView의 모든 메서드에서 예외가 발생하도록 기능 구현

--- src/main/java/bridge/InputView.java
+++ src/main/java/bridge/InputView.java
@@ -9,6 +9,9 @@ public class InputView {
      * 다리의 길이를 입력받는다.
      */
     public int readBridgeSize() {
+        if (true) {
+            throw new IllegalArgumentException();
+        }
         return 0;
     }
 
@@ -16,6 +19,9 @@ public class InputView {
      * 사용자가 이동할 칸을 입력받는다.
      */
     public String readMoving() {
+        if (true) {
+            throw new IllegalArgumentException();
+        }
         return null;
     }
 
@@ -23,6 +29,9 @@ public class InputView {
      * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
      */
     public String readGameCommand() {
+        if (true) {
+            throw new IllegalArgumentException();
+        }
         return null;
     }
 }
