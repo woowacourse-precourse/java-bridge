23471c6c5cb253c557c2306c0ab92f49721d845a
abbd2 <ajkety00@gmail.com>
2022-11-18 21:14:23 +0900

    Style(Message): INPUT_RANGE_BRIDGE_MESSAGE의 이름을 INPUT_BRIDGE_SIZE_MESSAGE로 변경

--- src/main/java/bridge/Message.java
+++ src/main/java/bridge/Message.java
@@ -5,7 +5,7 @@ public enum Message {
     BRIDGE_OPTIONS("(위: U, 아래: D)"),
     GAME_OPTIONS("(재시도: R, 종료: Q)"),
     START_GAME_MESSAGE("다리 건너기 게임을 시작합니다."),
-    INPUT_RANGE_BRIDGE_MESSAGE("다리의 길이를 입력해주세요."),
+    INPUT_BRIDGE_SIZE_MESSAGE("다리의 길이를 입력해주세요."),
     INPUT_MOVE_BRIDGE_MESSAGE("이동할 칸을 선택해주세요."),
     INPUT_SELECT_GAME_RETRY("게임을 다시 시도할지 여부를 입력해주세요.");
 
@@ -19,9 +19,9 @@ public enum Message {
         System.out.println(START_GAME_MESSAGE.script);
     }
 
-    public static void inputRangeBridgeMessage() {
+    public static void inputBridgeSizeMessage() {
         System.out.println();
-        System.out.println(INPUT_RANGE_BRIDGE_MESSAGE.script);
+        System.out.println(INPUT_BRIDGE_SIZE_MESSAGE.script);
     }
 
     public static void inputMoveBridgeMessage() {
