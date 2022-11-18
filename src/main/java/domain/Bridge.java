0fcf9048b3f2fb394f42fc267b8a6c665a71414d
qkrdmswl <eunji303@naver.com>
2022-11-19 02:59:50 +0900

    feat(Bridge): make Bridge object

--- src/main/java/domain/Bridge.java
+++ src/main/java/domain/Bridge.java
@@ -1,2 +1,21 @@
-package domain;public class Bridge {
+package domain;
+
+
+import bridge.BridgeMaker;
+
+public class Bridge {
+    BridgeMaker bridgeMaker;
+
+    private static Bridge bridge = null;
+
+    private Bridge(int bridgeSize){
+        this.bridge = (Bridge) bridgeMaker.makeBridge(bridgeSize);
+    }
+
+    public static Bridge getInstance(int bridgeSize) {
+        if (bridge == null) {
+            bridge = new Bridge(bridgeSize);
+        }
+        return bridge;
+    }
 }
