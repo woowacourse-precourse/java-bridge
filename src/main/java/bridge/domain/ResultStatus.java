ed9b8ad23d48b08dcb4b81f393d1674609dadf28
kimjeonghan <[rlawjdgks318@naver.com]>
2022-11-18 16:29:33 +0900

    feat(ResultStatus): 게임 결과 상태를 만드는 기능 추가

---
+++ src/main/java/bridge/domain/ResultStatus.java
@@ -0,0 +1,20 @@
+package bridge.domain;
+
+public enum ResultStatus {
+	SUCCESS("성공"),
+	RETRY("재시도"),
+	FAILURE("실패");
+
+	private final String resultStatusMessage;
+
+	ResultStatus(String resultStatusMessage) {
+		this.resultStatusMessage = resultStatusMessage;
+	}
+
+	public static ResultStatus makeResultStatus(String bridgeCommand, String command) {
+		if (bridgeCommand.equals(command)) {
+			return SUCCESS;
+		}
+		return FAILURE;
+	}
+}
