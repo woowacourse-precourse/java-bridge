2eb638b656cd37a9c8117ef466e8f188674abf18
chanwonlee <clw8679@naver.com>
2022-11-19 01:30:18 +0900

    refactor: BridgeGameController 리팩토링

--- src/main/java/bridge/BridgeGameController.java
+++ src/main/java/bridge/BridgeGameController.java
@@ -4,35 +4,37 @@ import java.util.List;
 
 public class BridgeGameController {
     public static void run() {
-        System.out.println("다리 건너기 게임을 시작합니다.");
-        System.out.println();
-        int bridgeSize = inputBridgeSize();
-        BridgeGame bridgeGame = new BridgeGame(createBridge(bridgeSize));
-        boolean successGame = playGame(bridgeSize, bridgeGame);
-        OutputView.printResult(bridgeGame,successGame);
+        System.out.println("다리 건너기 게임을 시작합니다.\n");
+        List<String> bridge = createBridge(inputBridgeSize());
+
+        BridgeGame bridgeGame = new BridgeGame(bridge);
+        boolean isGameSuccess = playBridgeGame(bridgeGame);
+
+        OutputView.printResult(bridgeGame,isGameSuccess);
     }
 
-    private static boolean playGame(int bridgeSize, BridgeGame bridgeGame) {
-        boolean retry = true;
-        while(retry) {
-            boolean continueGame = isSuccessGame(bridgeGame, bridgeSize);
-            if(continueGame){
-                break;
+    private static boolean playBridgeGame(BridgeGame bridgeGame) {
+        boolean isRetry = true;
+
+        while(isRetry) {
+            boolean isCrossBridgeSuccess = crossBridge(bridgeGame);
+            if(isCrossBridgeSuccess) {
+                return true;
             }
-            GameCommand gameCommand = inputGameCommand();
-            retry = bridgeGame.retry(gameCommand);
+            isRetry = bridgeGame.retry(inputGameCommand());
         }
-        return retry;
+        return false;
     }
 
-    private static boolean isSuccessGame(BridgeGame bridgeGame, int bridgeSize) {
-        boolean crossBridge = true;
-        while (crossBridge && bridgeGame.getCurrentBridgeIndex() < bridgeSize) {
-            MoveCommand moveCommand = inputMoveCommand();
-            crossBridge = bridgeGame.move(moveCommand);
-            OutputView.printMap(bridgeGame, crossBridge);
+    private static boolean crossBridge(BridgeGame bridgeGame) {
+        boolean isCrossBridge = true;
+        int bridgeSize = bridgeGame.getBridge().size();
+
+        while (isCrossBridge && bridgeGame.getCurrentBridgeIndex() < bridgeSize) {
+            isCrossBridge = bridgeGame.move(inputMoveCommand());
+            OutputView.printMap(bridgeGame, isCrossBridge);
         }
-        return crossBridge;
+        return isCrossBridge;
     }
 
     private static int inputBridgeSize() {
@@ -65,6 +67,7 @@ public class BridgeGameController {
     private static List<String> createBridge(int bridgeSize) {
         BridgeNumberGenerator randomNumberGenerator = new BridgeRandomNumberGenerator();
         BridgeMaker bridgeMaker = new BridgeMaker(randomNumberGenerator);
+
         return bridgeMaker.makeBridge(bridgeSize);
     }
 }
