98139162bcc354302df444baa9aaae7fc7a79383
Hayoon <hy97@sju.ac.kr>
2022-11-18 21:42:39 +0900

    feat(Bridge): 사용자 이동에 따른 다리 수정 메서드 구현

--- src/main/java/bridge/model/Bridge.java
+++ src/main/java/bridge/model/Bridge.java
@@ -1,16 +1,76 @@
 package bridge.model;
 
-import bridge.BridgeRandomNumberGenerator;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Bridge {
-    private int number;
+    private ArrayList<String> upBridge;
+    private ArrayList<String> downBridge;
 
-    public int execute() {
-        BridgeRandomNumberGenerator numberGenerator = new BridgeRandomNumberGenerator();
-        return numberGenerator.generate();
+    public Bridge() {
+        upBridge = new ArrayList<>(){{
+            upBridge.add("[");
+            upBridge.add("]");
+        }};
+        downBridge = new ArrayList<>(){{
+            downBridge.add("[");
+            downBridge.add("]");
+        }};
     }
 
-    public void setBridgeValue(int number) {
-        this.number = number;
+    public ArrayList<String> getUpBridge() {
+        return upBridge;
+    }
+
+    public ArrayList<String> getDownBridge() {
+        return downBridge;
+    }
+
+    public void setUpBridge(String mark) {
+        if (Player.getCurrentLocation() == 0) {
+            if (mark.equals("O")) {
+                upBridge.add(1, "O");
+                downBridge.add(1, " ");
+            } else {
+                upBridge.add(1, "X");
+                downBridge.add(1, " ");
+            }
+        } else {
+            if (mark.equals("O")) {
+                upBridge.add(Player.getCurrentLocation() + 1, "|");
+                upBridge.add(Player.getCurrentLocation() + 2, "O");
+                downBridge.add(Player.getCurrentLocation() + 1, "|");
+                downBridge.add(Player.getCurrentLocation() + 2, " ");
+            } else {
+                upBridge.add(Player.getCurrentLocation() + 1, "|");
+                upBridge.add(Player.getCurrentLocation() + 2, "X");
+                downBridge.add(Player.getCurrentLocation() + 1, "|");
+                downBridge.add(Player.getCurrentLocation() + 2, " ");
+            }
+        }
+    }
+
+    public void setDownBridge(String mark) {
+        if (Player.getCurrentLocation() == 0) {
+            if (mark.equals("O")) {
+                downBridge.add(1, "O");
+                upBridge.add(1, " ");
+            } else {
+                downBridge.add(1, "X");
+                upBridge.add(1, " ");
+            }
+        } else {
+            if (mark.equals("O")) {
+                downBridge.add(Player.getCurrentLocation() + 1, "|");
+                downBridge.add(Player.getCurrentLocation() + 2, "O");
+                upBridge.add(Player.getCurrentLocation() + 1, "|");
+                upBridge.add(Player.getCurrentLocation() + 2, " ");
+            } else {
+                downBridge.add(Player.getCurrentLocation() + 1, "|");
+                downBridge.add(Player.getCurrentLocation() + 2, "X");
+                upBridge.add(Player.getCurrentLocation() + 1, "|");
+                upBridge.add(Player.getCurrentLocation() + 2, " ");
+            }
+        }
     }
 }
