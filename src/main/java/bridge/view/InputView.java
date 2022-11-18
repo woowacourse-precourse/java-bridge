01f1c139cdea36d0782173a96e3c8c01113ae1ec
dev_kong <ahdjd5@gmail.com>
2022-11-18 18:41:56 +0900

    feat(InputView): add readMoving method to get move input

--- src/main/java/bridge/view/InputView.java
+++ src/main/java/bridge/view/InputView.java
@@ -11,11 +11,9 @@ public class InputView {
         return Console.readLine();
     }
 
-    /**
-     * 사용자가 이동할 칸을 입력받는다.
-     */
     public String readMoving() {
-        return null;
+        System.out.println("\n이동할 칸을 선택해주세요. (위: U, 아래: D)");
+        return Console.readLine();
     }
 
     /**
