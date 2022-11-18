ee2d4ab83f8996b2f58819a645b55546b8d7d1a7
Choi-JY <Choi-JY1107@github.com>
2022-11-18 22:10:01 +0900

    refactor: 기존 파일 폴더에 맞게 이동

--- src/main/java/bridge/view/InputView.java
+++ src/main/java/bridge/view/InputView.java
@@ -1,12 +1,26 @@
 package bridge.view;
 
-public class InputView {
+import camp.nextstep.edu.missionutils.Console;
 
-    /**
-     * 다리의 길이를 입력받는다.
-     */
-    public int readBridgeSize() {
-        return 0;
+public class InputView {
+    public static int inputBridgeSize() {
+        String bridgeSize = Console.readLine();
+        validateSizeNumber(bridgeSize);
+        validateSizeRange(bridgeSize);
+        return Integer.parseInt(bridgeSize);
+    }
+
+    private static void validateSizeNumber(String bridgeSize) {
+        if (!bridgeSize.chars().allMatch(Character::isDigit)) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private static void validateSizeRange(String bridgeSize) {
+        if (Integer.parseInt(bridgeSize) < 3 ||
+            Integer.parseInt(bridgeSize) > 20) {
+            throw new IllegalArgumentException();
+        }
     }
 
     /**
