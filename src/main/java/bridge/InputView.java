6b36fb3fce1a7d564950194a0627d5cccb1f922a
watterricecake <fruturum@nate.com>
2022-11-18 21:17:11 +0900

    feat: 입력받는 함수 interface

--- src/main/java/bridge/InputView.java
+++ src/main/java/bridge/InputView.java
@@ -5,22 +5,25 @@ import camp.nextstep.edu.missionutils.Console;
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
  */
-public class InputView {
-
+public class InputView{
+    private final Input input;
     /**
      * 다리의 길이를 입력받는다.
      */
-    public static int readBridgeSize() {
+    public InputView(Input input){
+        this.input = input;
+    }
+    public int readBridgeSize() {
         System.out.println("다리의 길이를 입력해주세요.");
-        return Integer.parseInt(Console.readLine());
+        return input.inputInteger();
     }
 
     /**
      * 사용자가 이동할 칸을 입력받는다.
      */
-    public static String readMoving() {
+    public String readMoving() {
         System.out.println("이동할 칸을 선택해주세요. (위: U, 아래: D)");
-        return Console.readLine();
+        return input.inputString();
     }
 
     /**
