b6f4ae44ccc1594f29938d4d77f184c666b40417
swonny <wonny921@gmail.com>
2022-11-18 23:58:54 +0900

    feat(BridgeTest): 이동 가능한 칸 찾기 테스트, 칸 이동 테스트 추가

---
+++ src/test/java/bridge/BridgeTest.java
@@ -0,0 +1,31 @@
+package bridge;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+public class BridgeTest {
+    @DisplayName("다음 칸이 이동 가능한 칸인지 확인하는 테스트")
+    @Test
+    void 이동_가능한_칸_확인_테스트() {
+        Bridge bridge = new Bridge(List.of("U", "D", "D"));
+        bridge.initializeCurrentPosition();
+        assertThat(bridge.isNextAvailable(Position.DOWN))
+                .isTrue();
+    }
+
+    @DisplayName("칸 이동 확인 테스트")
+    @Test
+    void 칸_이동_확인_테스트() {
+        Bridge bridge = new Bridge(List.of("U", "D", "D"));
+        bridge.initializeCurrentPosition();
+        bridge.movePlayer();
+        bridge.movePlayer();
+        bridge.movePlayer();
+        assertThat(bridge.isLastBlock())
+                .isTrue();
+    }
+}
