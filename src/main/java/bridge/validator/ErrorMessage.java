1c90e2005e4fcb9308319da836412ea5a08da020
yoojin <yoojinjangjang@gmail.com>
2022-11-18 11:45:07 +0900

    [feat] 다리 길이 유효성 검사

---
+++ src/main/java/bridge/validator/ErrorMessage.java
@@ -0,0 +1,15 @@
+package bridge.validator;
+
+public enum ErrorMessage {
+    NOT_NUMBER("[ERROR] 숫자만 입력해야 합니다."),
+    NOT_IN_RANGE_3_TO_20("[ERROR] 3이상 20이하의 숫자가 아닙니다.");
+    private final String message;
+
+    ErrorMessage(String message) {
+        this.message = message;
+    }
+
+    public String getErrorMessage() {
+        return message;
+    }
+}
