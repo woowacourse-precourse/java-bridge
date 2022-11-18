068a0fd4874eb0fb489a4187edcf5ce7376f15e7
twoosky <lhjae3@naver.com>
2022-11-18 23:50:04 +0900

    refactor(utils): 패키지 구조 리팩토링

--- src/main/java/bridge/utils/MessageFormat.java
+++ src/main/java/bridge/utils/MessageFormat.java
@@ -1,6 +1,6 @@
 package bridge.utils;
 
-public class MessageFormatUtil {
+public class MessageFormat {
     public static final String BRIDGE_START = "[ ";
     public static final String BRIDGE_SPACE_DIVISION = " | ";
     public static final String BRIDGE_END = " ]";
