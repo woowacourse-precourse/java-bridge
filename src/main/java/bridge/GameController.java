185af1980629923c3c367912bf5c9825dae3cfc2
wugawuga <dnr3655@naver.com>
2022-11-18 14:24:51 +0900

    feat: 게임 시작, 다리 길이 입력 출력 기능

--- src/main/java/bridge/GameController.java
+++ src/main/java/bridge/GameController.java
@@ -1,6 +1,12 @@
 package bridge;
 
 public class GameController {
+    private final InputView input = new InputView();
+    private final OutputView output = new OutputView();
+
     public void start() {
+        output.printStart();
+        output.printInputBridgeSize();
+        int bridgeSize = input.readBridgeSize();
     }
 }
--- src/main/java/bridge/OutputView.java
+++ src/main/java/bridge/OutputView.java
@@ -20,4 +20,13 @@ public class OutputView {
      */
     public void printResult() {
     }
+
+    public void printStart() {
+        System.out.println("다리 건너기 게임을 시작합니다.");
+    }
+
+    public void printInputBridgeSize() {
+        System.out.println();
+        System.out.println("다리의 길이를 입력해 주세요.");
+    }
 }
