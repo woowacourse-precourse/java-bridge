86d8d50413cb3136df417841c6593202d595e53e
0chil <0@chll.it>
2022-11-19 02:33:06 +0900

    feat(Deck): 특정 방향이 이동 가능한지 알 수 있다

--- src/main/java/bridge/domain/Deck.java
+++ src/main/java/bridge/domain/Deck.java
@@ -4,10 +4,18 @@ import bridge.constant.Direction;
 
 public class Deck {
 
-    private final Direction direction;
+    private final Direction movableDirection;
 
     public Deck(String capitalLetter) {
-        this.direction = Direction.from(capitalLetter)
+        this.movableDirection = directionFrom(capitalLetter);
+    }
+
+    public boolean isMovable(String capitalLetter) {
+        return movableDirection.equals(directionFrom(capitalLetter));
+    }
+
+    private Direction directionFrom(String capitalLetter) {
+        return Direction.from(capitalLetter)
                 .orElseThrow(() -> new IllegalArgumentException("방향은 위, 아래만 가능합니다"));
     }
 }
--- src/test/java/bridge/domain/DeckTest.java
+++ src/test/java/bridge/domain/DeckTest.java
@@ -1,15 +1,27 @@
 package bridge.domain;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
 
 import org.junit.jupiter.api.Test;
 
 public class DeckTest {
 
+    private static final String CAPITAL_LETTER_UPPER = "U";
+    private static final String CAPITAL_LETTER_LOWER = "D";
+
     @Test
     void 방향이_위나_아래가_아니면_예외를_던진다() {
         String anomalyDirection = "ASDF";
 
         assertThatIllegalArgumentException().isThrownBy(() -> new Deck(anomalyDirection));
+    }
+
+    @Test
+    void 특정_방향이_이동가능한지_알_수_있다() {
+        Deck deck = new Deck(CAPITAL_LETTER_UPPER);
+
+        assertThat(deck.isMovable(CAPITAL_LETTER_UPPER)).isTrue();
+        assertThat(deck.isMovable(CAPITAL_LETTER_LOWER)).isFalse();
     }
 }
