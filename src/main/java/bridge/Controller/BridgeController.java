8aa7d42dddad961cb29fda6071ccd45058f9977f
Jenn Kim <grdd0419@gmail.com>
2022-11-19 01:07:02 +0900

    feat: 컨트롤러 추가

---
+++ src/main/java/bridge/Controller/BridgeController.java
@@ -0,0 +1,24 @@
+package bridge.Controller;
+
+public class BridgeController {
+
+    public void playBridgeGame() {
+
+        // 게임 시작 문구 출력
+
+        // 다리의 길이를 입력받는다
+
+        // 다리를 생성한다
+
+        while (true) {      // 사용자가 Q를 입력하기 전까지 게임이 계속된다
+            // 이동할 칸을 입력받는다
+
+            // 다리를 건넌다
+
+            // 이동할 수 없는 경우 재시작 여부를 입력받는다
+        }
+
+        // 최종 결과를 출력한다
+        // 게임 성공 여부와 시도한 횟수를 출력한다
+    }
+}
