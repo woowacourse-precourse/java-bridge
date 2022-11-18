46d090100960f27f9ead0f6a361889f517ca84e4
lee2963 <sodel2963@naver.com>
2022-11-18 18:51:48 +0900

    feat: 게임 실행 컨트롤러 기능 구현

---
+++ src/main/java/bridge/controller/BridgeController.java
@@ -0,0 +1,40 @@
+package bridge.controller;
+
+import bridge.BridgeGame;
+import bridge.BridgeMaker;
+import bridge.BridgeNumberGenerator;
+import bridge.BridgeRandomNumberGenerator;
+import bridge.view.InputView;
+import bridge.view.OutputView;
+
+public class BridgeController {
+    private static BridgeGame bridgeGame;
+    private static final BridgeNumberGenerator bridgeNumberGenerator = new BridgeRandomNumberGenerator();
+    private static final InputView inputView = new InputView();
+    private static final BridgeMaker bridgeMaker = new BridgeMaker(bridgeNumberGenerator);
+    private static final OutputView outputView = new OutputView();
+
+    public void play() {
+        outputView.printGameStart();
+        bridgeGame = new BridgeGame(bridgeMaker.makeBridge(inputView.readBridgeSize()));
+        boolean game = true;
+
+        while(game) {
+            if(bridgeGame.checkGameIsDone()) break;
+            game = playerMove();
+        }
+        outputView.printResult(bridgeGame);
+    }
+
+    public boolean playerMove() {
+        String move = inputView.readMoving();
+        boolean game = bridgeGame.move(move);
+        outputView.printMap(bridgeGame);
+
+        if (!game) {
+            game = bridgeGame.retry(inputView.readGameCommand());
+        }
+
+        return game;
+    }
+}
