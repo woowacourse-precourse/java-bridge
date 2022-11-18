a10ff65e797bd67eb2380d069bb864658d1d2107
chj <bluedog129@gmail.com>
2022-11-18 20:31:43 +0900

    feat(InputView) : 다리길이 입력받기 구현

--- src/main/java/bridge/Controller.java
+++ src/main/java/bridge/Controller.java
@@ -1,2 +1,13 @@
-package bridge;public class Controller {
+package bridge;
+
+public class Controller {
+    public Controller() {
+    }
+    private final Model model = new Model();
+    private final InputView inputView = new InputView();
+    private final OutputView outputView = new OutputView();
+
+    public void inputBridgeLength() {
+        Integer bridgeLength = inputView.readBridgeSize();
+    }
 }
