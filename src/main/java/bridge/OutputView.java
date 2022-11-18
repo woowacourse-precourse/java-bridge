d88d6c3ae85443af662ee88a93aaa49d70953c7d
neppiness <0414kjh@naver.com>
2022-11-18 12:55:55 +0900

    refactor: Guide enum 추가 및 guide 메소드 구현 (src/main/java/bridge)
    OutputView 클래스에 Guide enum 추가, 각 안내 문구를 enum의 message 변수로 할당
    안내 유형 마다 다른 메소드 호출하도록 함. 각 메소드별로 다른 enum을 guide 메소드에 전달하여 각각 다른 안내 문구 출력

--- src/main/java/bridge/OutputView.java
+++ src/main/java/bridge/OutputView.java
@@ -4,6 +4,15 @@ package bridge;
  * 사용자에게 게임 진행 상황과 결과를 출력하는 역할을 한다.
  */
 public class OutputView {
+    enum Guide {
+        START("다리 건너기 게임을 시작합니다."),
+        BRIDGESIZEINPUT("다리의 길이를 입력해주세요."),
+        MOVECOMMANDINPUT("이동할 칸을 선택해주세요. (위: U, 아래: D)"),
+        RETRY("게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)");
+
+        private final String message;
+        Guide(String message) { this.message = message; }
+    }
     /**
      * 현재까지 이동한 다리의 상태를 정해진 형식에 맞춰 출력한다.
      * <p>
@@ -20,5 +29,12 @@ public class OutputView {
     public void printResult() {
     }
 
-    public void printErrorMessage(String errorMessage) { System.out.println(errorMessage); }
+    public void printMessage(String message) { System.out.println(message); }
+
+    private void guide(Guide guide) { System.out.println(guide.message); }
+
+    public void guideStart() { guide(Guide.START); }
+    public void guideBridgeSizeInput() { guide(Guide.BRIDGESIZEINPUT); }
+    public void guideMovingCommandInput() { guide(Guide.MOVECOMMANDINPUT); }
+    public void guideRetry() { guide(Guide.RETRY); }
 }
