1be229b8f9faed96947820c65bfaf32a3c7a5696
alstjr706 <alstjr706@naver.com>
2022-11-18 13:45:31 +0900

    fix : (다리 건너기) 메인 메서드 코드 수정

--- src/main/java/bridge/domain/BridgeGameMachine.java
+++ src/main/java/bridge/domain/BridgeGameMachine.java
@@ -3,6 +3,9 @@ package bridge.domain;
 import bridge.BridgeMaker;
 import bridge.BridgeRandomNumberGenerator;
 import bridge.input.InputView;
+import bridge.output.OutputView;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class BridgeGameMachine {
@@ -13,8 +16,42 @@ public class BridgeGameMachine {
 
         BridgeRandomNumberGenerator randomGenerator = new BridgeRandomNumberGenerator();
         BridgeMaker bridgeMaker = new BridgeMaker(randomGenerator);
-        List<String> strings = bridgeMaker.makeBridge(bridgeLength);
+        List<String> designBridge = bridgeMaker.makeBridge(bridgeLength);
+        BridgeGame bridgeGame = new BridgeGame();
+        OutputView outputView = new OutputView();
 
+        List<String> uPrintBridge = new ArrayList<String>();
+        List<String> dPrintBridge = new ArrayList<String>();
+
+
+        for (String playerLocation : designBridge) {
+            String moveLocation = inputView.readMoving();
+
+            if (moveLocation.equals("U")) {
+                List<String> discriminant = bridgeGame.move(playerLocation, uPrintBridge);
+            }
+
+            if (moveLocation.equals("D")) {
+                List<String> discriminant = bridgeGame.move(playerLocation, uPrintBridge);
+            }
+
+
+
+            //printBridge.add(printBridge);
+
+        }
 
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
