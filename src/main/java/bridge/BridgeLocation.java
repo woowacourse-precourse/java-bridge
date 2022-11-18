17acc6efa3828d53d66fe3a58052a21abbbbfafd
juhwang <juhong4930@naver.com>
2022-11-19 00:38:00 +0900

    feat(BridgeLocation): create get correct location method

--- src/main/java/bridge/BridgeLocation.java
+++ src/main/java/bridge/BridgeLocation.java
@@ -9,6 +9,9 @@ public enum BridgeLocation {
     DOWN_TRUE(0, true, "D", "O");
 
     public static final String UN_CHOSEN_LOCATION = " ";
+    public static final String LOCATION_NUMBER_ERROR = "[ERROR] 존재하지 않는 정보입니다.";
+    public static final String LOCATION_ERROR = "[ERROR] U 또는 D를 입력해 주시기 바랍니다.";
+
     private final int locationNumber;
     private final boolean correctLocation;
     private final String location;
@@ -25,7 +28,7 @@ public enum BridgeLocation {
         return Arrays.stream(values())
                 .filter(locationValue -> locationValue.locationNumber == locationNumber)
                 .findFirst()
-                .orElseThrow(() -> new IllegalArgumentException("[ERROR] 존재하지 않는 정보입니다."));
+                .orElseThrow(() -> new IllegalArgumentException(LOCATION_NUMBER_ERROR));
     }
 
     public static BridgeLocation createLocation(String location, boolean correctLocation) {
@@ -33,7 +36,7 @@ public enum BridgeLocation {
                 .filter(locationValue -> isLocation(locationValue, location)
                         && isCorrectLocation(locationValue, correctLocation))
                 .findFirst()
-                .orElseThrow(() -> new IllegalArgumentException("[ERROR] U 또는 D를 입력해 주시기 바랍니다."));
+                .orElseThrow(() -> new IllegalArgumentException(LOCATION_ERROR));
     }
 
     private static boolean isCorrectLocation(BridgeLocation locationValue, boolean correctLocation) {
@@ -46,6 +49,10 @@ public enum BridgeLocation {
 
     public String getLocation() {
         return location;
+    }
+
+    public boolean getCorrectLocation() {
+        return correctLocation;
     }
 
     public String getStateLocation(String location) {
