363e541323ea33b29611e574349c8a9fe6628e68
YSEO <1park4170@gmail.com>
2022-11-18 21:56:46 +0900

    refactor<OutputView> : System.out.println()을 사용하던 코드를 StringBuffer를 사용하는 방식으로 리팩토링, 중간단계 및 최종단계 map 출력 기능 구현
    - printMap() : 각각 위, 아래의 버퍼를 채워서 이를 프린트하는 방식
     - fillEachBuffer() : 다리와 유저의 입력을 비교하여 올바른 문자를 채운다
    - clearBuffer() : 게임이 리셋될때마다 버퍼를 비운다
    - printResult() : 채워진 버퍼를 그대로 프린트하고, 게임 성공 여부와 총 시도 횟수를 출력한다

--- src/main/java/bridge/view/OutputView.java
+++ src/main/java/bridge/view/OutputView.java
@@ -8,72 +8,59 @@ import java.util.List;
  * 사용자에게 게임 진행 상황과 결과를 출력하는 역할을 한다.
  */
 public class OutputView {
+    private StringBuffer uppperBuffer = new StringBuffer();
+    private StringBuffer lowerBuffer = new StringBuffer();
+    private Bridge bridge;
+    private List<String> choices;
+
+    public OutputView(Bridge bridge) {
+        this.bridge = bridge;
+    }
+
+    /* 특정 위치의 O X 빈칸 중 하나를 출력한다 */
+    public String properOXSpace(String UpOrDown , String directionChoice, String whereToPrint) {
+        /* 실제로는 위냐 아래냐, 유저 선택은 뭐냐, 위쪽용 프린터냐 아래쪽용 프린터냐 */
+        /* String UpOrDown = bridge.returnCertainIndexUpOrDown(index); */
+        if (!(directionChoice.equalsIgnoreCase(whereToPrint))) {
+            return " ";
+        }
+        if (UpOrDown.equalsIgnoreCase(directionChoice)) {
+            return "O";
+        }
+
+        return "X";
+    }
+
     /**
      * 현재까지 이동한 다리의 상태를 정해진 형식에 맞춰 출력한다.
      * <p>
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void printMap(Bridge bridge, int ongoing) {
-        for (int i = 0; i< ongoing; i++) {
-            printUpper(bridge, i);
-        }
-        System.out.println(" ]");
-        for (int i = 0; i< ongoing; i++) {
-            printLower(bridge, i);
-        }
-        System.out.println(" ]");
+    public void printMap(final int ongoing, List<String> choices) {
+        this.choices = choices;
+
+        fillEachBuffer(uppperBuffer, ongoing, "U");
+        System.out.println(uppperBuffer);
+
+        fillEachBuffer(lowerBuffer, ongoing, "D");
+        System.out.println(lowerBuffer);
     }
 
-    public void printUpper(Bridge bridge, int index) {
-        if (index == 0) {
-            System.out.print("[ ");
-        }
-
-        String upOrDown = bridge.returnCertainIndexUpOrDown(index);
-        int bridgeLength = bridge.returnBridgeLength();
-
-        if (upOrDown == "U" ) {
-            if (index == bridgeLength-1) {
-                System.out.print("O ");
-                return;
-            }
-            System.out.print("O | ");
-        } else {
-            if (index == bridgeLength-1 ) {
-                System.out.print("  ");
-            } else {
-                System.out.print("  | ");
+    public void fillEachBuffer(StringBuffer buffer, int ongoing, String whereToPrint) {
+        buffer.append("[ ");
+        for (int i = 0; i<=ongoing; i++) {
+            String realUpOrDown = bridge.returnCertainIndexUpOrDown(i);
+            buffer.append(properOXSpace(realUpOrDown, choices.get(i), whereToPrint)); // 아!
+            if (i != ongoing) {
+                buffer.append(" | ");
             }
         }
-//        if (index == bridgeLength-1) {
-//            System.out.println("]");
-//        }
+        buffer.append(" ]");
     }
 
-    public void printLower(Bridge bridge, int index) {
-        if (index == 0) {
-            System.out.print("[ ");
-        }
-
-        String upOrDown = bridge.returnCertainIndexUpOrDown(index);
-        int bridgeLength = bridge.returnBridgeLength();
-
-        if (upOrDown == "D" ) {
-            if (index == bridgeLength-1) {
-                System.out.print("O ");
-                return;
-            }
-            System.out.print("O | ");
-        } else {
-            if (index == bridgeLength-1 ) {
-                System.out.print("  ");
-            } else {
-                System.out.print("  | ");
-            }
-        }
-//        if (index == bridgeLength-1) {
-//            System.out.println("]");
-//        }
+    public void clearBuffer() {
+        uppperBuffer.delete(0,uppperBuffer.length());
+        lowerBuffer.delete(0,lowerBuffer.length());
     }
 
     /**
@@ -81,29 +68,30 @@ public class OutputView {
      * <p>
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void printResultMap(Bridge bridge, int bridgeSize) {
-        System.out.println("최종 게임 결과");
-        for (int i = 0; i< bridgeSize; i++) {
-            printUpper(bridge, i);
-        }
-        System.out.println("]");
-        for (int i = 0; i< bridgeSize; i++) {
-            printLower(bridge, i);
-        }
-        System.out.println("]");
+
+    public void printResult(boolean pass, int tryNumber) {
+        printWinorLose(pass);
+        System.out.println(uppperBuffer.toString());
+        System.out.println(lowerBuffer.toString());
+        printTryNumbers(tryNumber);
     }
 
     public void printWinorLose(boolean pass) {
         String result = "";
         if (pass) {
-            result = "성공" ;
+//            result = "성공" ;
+            result = "success";
         } else {
-            result = "실패";
+//            result = "실패";
+            result = "fail";
         };
-        System.out.printf("게임 성공 여부: %s\n", result);
+//        System.out.printf("게임 성공 여부: %s\n", result);
+        System.out.printf("game success or not: %s\n", result);
     }
 
     public void printTryNumbers(int tryNumber) {
-        System.out.printf("총 시도한 횟수: %s", tryNumber);
+//        System.out.printf("총 시도한 횟수: %s", tryNumber);
+        System.out.printf("all tried number: %s", tryNumber);
     }
 }
+
