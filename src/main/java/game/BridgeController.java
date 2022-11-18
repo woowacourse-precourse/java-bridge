01200945704c3fb87557cc2e2fe63385b8db59e5
TaeHeyeon_Kim <thk98k@gmail.com>
2022-11-18 23:47:49 +0900

    style(game): 기능 구현 뼈대코드 작성

--- src/main/java/game/BridgeController.java
+++ src/main/java/game/BridgeController.java
@@ -10,11 +10,31 @@ public class BridgeController {
     private OutputView outputView = new OutputView();
     private BridgeMaker bridgeMaker = new BridgeMaker(new BridgeRandomNumberGenerator());
 
-    public void run() {
+    public void settingGame() {
         System.out.println("다리 건너기 게임을 시작합니다.");
         System.out.println("다리의 길이를 입력해주세요.");
         List<String> bridge = getBridge();
-        System.out.println("bridge = " + bridge);
+
+        run(bridge);
+    }
+
+    private void run(List<String> bridge) {
+        int count = 0;
+        while (true) {
+            count++;
+
+            playGame(bridge);
+
+            String retry = retryCommand();
+            if (retry.equals("Q")) break;
+            if (retry.equals("R")) playGame(bridge);
+        }
+        System.out.println("게임 성공 여부: " + "실패");
+        System.out.println("총 시도한 횟수: " + count);
+    }
+
+    private void playGame(List<String> bridge) {
+
     }
 
 
