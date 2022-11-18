3d5562e6d2411f00ef0787d095914b9b3deadba0
Kayoung <rkdkdudjd@gmail.com>
2022-11-18 17:34:24 +0900

    feat(Logger): 클래스 정의
    예외 메시지를 찍어주는 클래스를 정의했습니다.

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -1,8 +1,14 @@
 package bridge;
 
+import bridge.controller.GameController;
+
 public class Application {
 
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        try {
+            GameController.run();
+        } catch (Exception e) {
+            Logger.log(e);
+        }
     }
 }
