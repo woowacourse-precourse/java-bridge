9a560f472c5bdeccf94ef3aa06d90face4f521d4
eun-byeol <62530884+eun-byeol@users.noreply.github.com>
2022-11-18 14:28:42 +0900

    feat(ViewMessage): 사용자 입력 메시지 정의

---
+++ src/main/java/bridge/utils/ViewMessage.java
@@ -0,0 +1,13 @@
+package bridge.utils;
+
+public class ViewMessage {
+    public static final String GAME_START = "다리 건너기 게임을 시작합니다.";
+
+    public static final String INPUT_BRIDGE_SIZE = "다리의 길이를 입력해주세요.";
+    public static final String INPUT_DIRECTION = "이동할 칸을 선택해주세요. (위: U, 아래: D)";
+    public static final String INPUT_RESTART_OR_END = "게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)";
+
+    public static final String OUTPUT_FINAL_RESULT = "최종 게임 결과";
+    public static final String OUTPUT_GAME_SUCCESS_OR_NOT = "게임 성공 여부: %s%n";
+    public static final String OUTPUT_NUMBER_OF_ATTEMPTS = "총 시도한 횟수: %d";
+}
