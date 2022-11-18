2ac8c1dc5192baaeebce18fbb9ae703e1eec636b
simba <shim9597@gmail.com>
2022-11-18 19:50:13 +0900

    feat: 입력값에 맞는 길이의 다리 생성하는 기능 구현

--- src/main/java/bridge/BridgeMaker.java
+++ src/main/java/bridge/BridgeMaker.java
@@ -1,5 +1,6 @@
 package bridge;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -18,6 +19,27 @@ public class BridgeMaker {
      * @return 입력받은 길이에 해당하는 다리 모양. 위 칸이면 "U", 아래 칸이면 "D"로 표현해야 한다.
      */
     public List<String> makeBridge(int size) {
-        return null;
+        /**
+         * 여기서만 쓰일 것들.
+         * NumberConvertor
+         */
+        List<String> tmp = new ArrayList<>(size);
+
+
+        NumberConvertor numberConvertor = new NumberConvertor();
+        // 위에 이놈 어떡하지.. 어떻게 처리할까
+
+        for (int i = 0; i < size; i++) {
+            int number = bridgeNumberGenerator.generate();
+            // 랜덤으로 주어지는 0 또는 1 의 값은 쓰일 일이 여기 밖에 없음.
+            // U / D 로 여기서 바꿔버려 됨.
+            // 어차피 input 값과 비교도 U/D 이니까.
+            String upOrDown = numberConvertor.convert(number);
+            // 1,0 을 U,D로 바꾸는 기능 따로 빼기.
+            // U,D는 상수? 하는게 나을듯
+            tmp.add(i, upOrDown); // ex) (U,D,U,U,D,U)
+        }
+
+        return tmp;
     }
 }
