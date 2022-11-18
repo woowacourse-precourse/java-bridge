5ba2871a1a7ce5125e7f759bff18a6a866c65635
dlrjs2360 <lhg3670@naver.com>
2022-11-19 02:00:30 +0900

    :sparkles: feat: 다리 생성 기능 구현
    메서드
    - makeBridge()
    
    세부 사항
    - 입력된 사이즈에 따라 다리 생성
    - BridgeRandomNumberGenerator의 generate() 메서드로 반복하며 1과 0중 한 값을 받아옴.
    - 0이면 "U", 1이면 "D"를 bridge에 추가

--- src/main/java/bridge/Controller/GameController.java
+++ src/main/java/bridge/Controller/GameController.java
@@ -1,4 +1,15 @@
 package bridge.Controller;
 
+import bridge.BridgeNumberGenerator;
+import bridge.BridgeRandomNumberGenerator;
+import bridge.Domain.BridgeGame;
+import bridge.Domain.BridgeMaker;
+import bridge.View.InputView;
+
 public class GameController {
+    InputView inputView = new InputView();
+    BridgeGame bridgeGame = new BridgeGame();
+    BridgeMaker bridgeMaker = new BridgeMaker(new BridgeRandomNumberGenerator());
+
+
 }
--- src/main/java/bridge/Domain/BridgeGame.java
+++ src/main/java/bridge/Domain/BridgeGame.java
@@ -1,4 +1,4 @@
-package bridge.Service;
+package bridge.Domain;
 
 /**
  * 다리 건너기 게임을 관리하는 클래스
