d99122276a1830d66402c680502e4de6b9da78de
situodys <situodys@gmail.com>
2022-11-18 18:51:52 +0900

    feat(InputConverter): 정수 범위를 넘는 입력인 경우 검증 구현

---
+++ src/main/java/bridge/util/InputConverter.java
@@ -0,0 +1,14 @@
+package bridge.util;
+
+public class InputConverter {
+    private static final String OVERSIZED_INPUT_MSG = "정수 범위를 넘은 입력값입니다.";
+
+    public int toInt(String userInput) {
+        try {
+            return Integer.parseInt(userInput);
+        } catch (Exception e) {
+            throw new IllegalArgumentException(OVERSIZED_INPUT_MSG);
+        }
+    }
+
+}
