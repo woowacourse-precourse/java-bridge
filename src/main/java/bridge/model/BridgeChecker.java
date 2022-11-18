c666aa921b54db6fe634562d8052059e8d7a5a6d
ysng_ysng <yeasung67@gmail.com>
2022-11-18 14:33:09 +0900

    feat(BridgeChecker): Implement the methods to check movable bridge step
    이동 명령이 주어졌을 때 명령이 이동가능한 위치인지 아닌지 판단합니다.

---
+++ src/main/java/bridge/model/BridgeChecker.java
@@ -0,0 +1,19 @@
+package bridge.model;
+
+import java.util.List;
+
+// todo : 빠뜨린 조건이 있는지 다시 생각해볼것!
+public class BridgeChecker {
+	public static boolean isNotMovableStep(List<String> bridge, String moveCommand, int gameTurn) {
+		// 이동할 수 없다면 true, 이동할 수 있다면 false
+		return isFinish(bridge, gameTurn) || isNotMovable(bridge, moveCommand, gameTurn);
+	}
+
+	private static boolean isFinish(List<String> bridge, int gameTurn) {
+		return bridge.size() == gameTurn;
+	}
+
+	private static boolean isNotMovable(List<String> bridge, String moveCommand, int gameTurn) {
+		return !bridge.get(gameTurn).equals(moveCommand);
+	}
+}
