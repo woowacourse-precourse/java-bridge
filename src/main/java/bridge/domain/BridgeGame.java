ab9595cae48f772e6f6f9c172bc7804d297caf42
Yongwoo Choi <cyw7515@naver.com>
2022-11-18 21:01:30 +0900

    refactor (BridgeGame): else문 삭제를 위한 수정

--- src/main/java/bridge/domain/BridgeGame.java
+++ src/main/java/bridge/domain/BridgeGame.java
@@ -17,7 +17,7 @@ public class BridgeGame {
     private List<String> result;
 
     /**
-     * 게임 관리 컨트롤러에서 필요한 객체를 생성해 주입한다.
+     * 다리 생성기를 주입하고, 시도 횟수를 초기화하는 메서드
      */
     public BridgeGame(BridgeNumberGenerator bridgeNumberGenerator) {
         bridgeMaker = new BridgeMaker(bridgeNumberGenerator);
@@ -43,8 +43,6 @@ public class BridgeGame {
 
     /**
      * 사용자가 칸을 이동할 때 사용하는 메서드
-     * <p>
-     * 이동을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
     public MovingResultDto move(String commend) {
         String correctPosition = bridge.get(position);
@@ -60,24 +58,25 @@ public class BridgeGame {
                 completeness = true;
                 success = true;
             }
-        } else {
-            result.add("X");
 
-            completeness = true;
+            return new MovingResultDto(result, completeness, success);
         }
 
-        return new MovingResultDto(result, completeness, success);
+        result.add("X");
+
+        return new MovingResultDto(result, true, false);
     }
 
     /**
      * 사용자가 게임을 다시 시도할 때 사용하는 메서드
-     * <p>
-     * 재시작을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public boolean retry() {
-        initGame();
+    public boolean retry(String input) {
+        if (input.equals("R")) {
+            initGame();
+            return true;
+        }
 
-        return true;
+        return false;
     }
 
     public List<String> getBridge() {
