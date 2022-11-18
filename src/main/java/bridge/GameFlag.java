77d7382244c869c99c914c91f96baea05d6ba67c
woosung1223 <dntjd991223@naver.com>
2022-11-18 20:31:54 +0900

    feat(GameFlag): 게임 성공 여부를 나타내는 enum 클래스 생성

---
+++ src/main/java/bridge/GameFlag.java
@@ -0,0 +1,13 @@
+package bridge;
+
+public enum GameFlag {
+    PLAYING("게임 진행중"),
+    CLEAR("성공"),
+    FAIL("실패");
+
+    private final String message;
+
+    GameFlag(String message) {
+        this.message = message;
+    }
+}
--- src/main/java/bridge/controller/Controller.java
+++ src/main/java/bridge/controller/Controller.java
@@ -1,4 +1,38 @@
 package bridge.controller;
 
-public class Controller {
+import bridge.BridgeGame;
+import bridge.GameStatus;
+import bridge.view.InputView;
+import bridge.view.OutputView;
+
+public class GameController {
+    BridgeGame bridgeGame = new BridgeGame();
+    InputView inputView = new InputView();
+    OutputView outputView = new OutputView();
+
+
+    public void run() {
+        initialize();
+        GameStatus finalResult = getOneGameResult();
+        outputView.printMap(finalResult);
+    }
+
+    private GameStatus getOneGameResult() { // 정상적인 경우가 아니라면 루프 종료
+        GameStatus gameResult;
+        do {
+            String moving = inputView.readMoving();
+            gameResult = bridgeGame.getMovingResult(moving);
+            outputView.printMap(gameResult);
+        } while(gameResult.getFlag() == -1);
+        return gameResult;
+    }
+
+    private void initialize() {
+        int size = inputView.readBridgeSize();
+        bridgeGame.initialize(size);
+    }
+
+    private void retry() {
+        bridgeGame.retry();
+    }
 }
