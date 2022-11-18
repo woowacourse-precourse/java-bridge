735897ae320ead67c984b76bcbb184c9504c403a
nylonmusk <106235834+nylonmusk@users.noreply.github.com>
2022-11-18 21:18:33 +0900

    style(StringConstant): 라인 변경 추가

--- src/main/java/constant/StringConstant.java
+++ src/main/java/constant/StringConstant.java
@@ -3,16 +3,22 @@ package constant;
 public enum StringConstant {
     CORRECT_PATH("O"),
     WRONG_PATH("X"),
+
     UP_SIDE("U"),
     DOWN_SIDE("D"),
+
     QUIT("Q"),
     RETRY("R"),
-    BRIDGE_SIZE_REGEX("^[0-9]*$"),
+
     WIN("성공"),
     FAIL("실패"),
+
     START_SQUARE_BRACKET("[ "),
-    DELIMETER(" | "),
     END_SQUARE_BRACKET(" ]"),
+    DELIMETER(" | "),
+
+    BRIDGE_SIZE_REGEX("^[0-9]*$"),
+
     BLANK(" ");
 
     public final String stringConstant;
