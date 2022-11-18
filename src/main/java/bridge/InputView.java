531671edf03618f698e7e68378820d70fbbdb842
dener8 <8dener@gmail.com>
2022-11-18 20:02:48 +0900

    feat(InputView): 사용자 입력이 잘못됐을 경우 exception을 발생시키도록 클래스를 구성
    입력을 읽어오는 3개의 메서드와 그 입력의 오류를 확인하는 3개의 메서드 구현

--- src/main/java/bridge/InputView.java
+++ src/main/java/bridge/InputView.java
@@ -2,6 +2,8 @@ package bridge;
 
 import camp.nextstep.edu.missionutils.Console;
 
+import static view.ErrorMessage.*;
+
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
  */
@@ -11,7 +13,20 @@ public class InputView {
      * 다리의 길이를 입력받는다.
      */
     public int readBridgeSize() {
-        int bridgeSize = Integer.parseInt(Console.readLine());
+        int bridgeSize = 0;
+        try {
+            bridgeSize = checkBridgeSize(Integer.parseInt(Console.readLine()));
+        } catch(RuntimeException e) {
+            System.out.println(e.getMessage());
+            readBridgeSize();
+        }
+        return bridgeSize;
+    }
+
+    public int checkBridgeSize(int bridgeSize) { // 문자로 들어왔을 경우에도 예외처리 해줘야한다.
+        if (bridgeSize < 3 || bridgeSize > 20) {
+            throw new RuntimeException(OUT_OF_LENGTH_ERROR);
+        }
         return bridgeSize;
     }
 
@@ -19,7 +34,20 @@ public class InputView {
      * 사용자가 이동할 칸을 입력받는다.
      */
     public String readMoving() {
-        String upOrDown = Console.readLine();
+        String upOrDown = "";
+        try {
+            upOrDown = checkMoving(Console.readLine());
+        } catch (RuntimeException e) {
+            System.out.println(e.getMessage());
+            readMoving();
+        }
+        return upOrDown;
+    }
+
+    public String checkMoving(String upOrDown) { // 숫자일 경우도 예외지만, 숫자는 그냥 String으로 받아올 수 있으니까 괜찮지 않을까?
+        if (upOrDown != "U" || upOrDown != "D") {
+            throw new RuntimeException(MOVING_ERROR);
+        }
         return upOrDown;
     }
 
@@ -27,7 +55,20 @@ public class InputView {
      * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
      */
     public String readGameCommand() {
-        String retryOrNot = Console.readLine();
+        while (true) {
+            try {
+                String retryOrNot = checkGameCommand(Console.readLine());
+                return retryOrNot;
+            } catch (RuntimeException e) {
+                System.out.println(e.getMessage());
+            }
+        }
+    }
+
+    public String checkGameCommand(String retryOrNot) {
+        if (retryOrNot != "R" || retryOrNot != "Q") {
+            throw new RuntimeException(RETRY_INPUT_ERROR);
+        }
         return retryOrNot;
     }
 
