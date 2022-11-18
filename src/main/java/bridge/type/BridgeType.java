9a590866837476b68f542eb62be5010db0b1babd
Laeng <Laeng@users.noreply.github.com>
2022-11-18 19:36:07 +0900

    feat(BridgeType): 0, 1이 아닌 값이 들어왔을 때의 대응 처리 추가

--- src/main/java/bridge/type/BridgeType.java
+++ src/main/java/bridge/type/BridgeType.java
@@ -5,16 +5,27 @@ import java.util.Arrays;
 public enum BridgeType {
     DOWN(0, "D"),
     UP(1, "U"),
-    UNKNOWN(-1, "E");
+    UNKNOWN(-1, "N");
 
     private final int id;
     private final String typeName;
 
     public static BridgeType create(int id) {
         return Arrays.stream(BridgeType.values())
-                .filter(type -> type.id == id)
+                .filter(type -> type.id == purifyId(id))
                 .findFirst()
                 .orElse(BridgeType.UNKNOWN);
+    }
+
+    private static int purifyId(int id) {
+        boolean hasId = Arrays.stream(BridgeType.values())
+                .anyMatch(bridgeType -> bridgeType.id == id);
+
+        if (!hasId) {
+            return -1;
+        }
+
+        return id;
     }
 
     BridgeType(int id, String typeName) {
