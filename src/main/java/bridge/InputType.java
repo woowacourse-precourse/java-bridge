3a7e9f572e4a4d397da0451e796b35bc8824016c
Hyeonjun Park <phjppo0918@gmail.com>
2022-11-18 15:59:20 +0900

    feat(InputType) : value 입력시 그에 맞는 요소 반환

--- src/main/java/bridge/InputType.java
+++ src/main/java/bridge/InputType.java
@@ -1,5 +1,8 @@
 package bridge;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public enum InputType {
     UP('U'),
     DOWN('D'),
@@ -8,7 +11,20 @@ public enum InputType {
 
     private final Character value;
 
+    private static Map<Character, InputType> mapping = new HashMap<>();
+
+    static {
+        mapping.put(UP.value, UP);
+        mapping.put(DOWN.value, DOWN);
+        mapping.put(RETRY.value, RETRY);
+        mapping.put(QUIT.value, QUIT);
+    }
+
     InputType(Character value) {
         this.value = value;
+    }
+
+    public InputType getType(Character value) {
+        return mapping.get(value);
     }
 }
