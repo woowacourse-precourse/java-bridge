b07dd6a86fd8f2981386de34fdd964705b228f77
saint6839 <saint6839@gmail.com>
2022-11-18 16:34:36 +0900

    feat(domain) : 각각의 다리가 건널 수 있는지 없는지 판별할 수 있는 List<Boolean> 필드 추가

--- src/main/java/bridge/Bridge.java
+++ src/main/java/bridge/Bridge.java
@@ -1,12 +1,17 @@
 package bridge;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Bridge {
     private static final String INVALID_LENGTH_RANGE_MESSAGE = "[ERROR] 다리 길이는 3이상 20이하의 숫자를 입력해주세요.";
     private int length;
+    private List<Boolean> isBridgeChecks;
 
     public Bridge(int length) {
         validateLengthRange(length);
         this.length = length;
+        isBridgeChecks = new ArrayList<>();
     }
 
     private void validateLengthRange(int length) {
