479cc261bf4dc45a9918efa7ad77645457eb7a43
sosow0212 <sosow0212@naver.com>
2022-11-18 17:36:35 +0900

    feat : (BridgeController) 게임 시작을 위한 틀 작성

--- src/main/java/bridge/controller/BridgeController.java
+++ src/main/java/bridge/controller/BridgeController.java
@@ -2,13 +2,16 @@ package bridge.controller;
 
 import bridge.domain.Bridge;
 import bridge.domain.User;
+import bridge.service.BridgeGame;
 import bridge.service.BridgeService;
 
 public class BridgeController {
     private final BridgeService bridgeService = new BridgeService();
+    private final BridgeGame bridgeGame = new BridgeGame();
 
-    public void gameStart() {
+    public void start() {
         Bridge bridge = bridgeService.bridgeMaker();
         User user = new User();
+        bridgeGame.gameStart(bridge, user);
     }
 }
