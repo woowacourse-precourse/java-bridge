cf908d0987bc60e4255768ea0b014ad852b4b8c2
sosow0212 <sosow0212@naver.com>
2022-11-18 16:42:24 +0900

    test : (BridgeTest) nowIndex 필드 추가함에 따라 추가된 메소드 테스트

--- src/test/java/bridge/domain/BridgeTest.java
+++ src/test/java/bridge/domain/BridgeTest.java
@@ -20,4 +20,46 @@ public class BridgeTest {
         // then
         assertThat(bridge.getBridge()).isEqualTo(list);
     }
+
+    @DisplayName("nowIndex Getter, Setter 테스트")
+    @Test
+    public void nowIndexGetterAndSetterTest() {
+        // given
+        Bridge bridge = new Bridge(List.of("U", "D", "D"));
+        int index = 3;
+
+        // when
+        bridge.setNowIndex(index);
+
+        // then
+        assertThat(bridge.getNowIndex()).isEqualTo(index);
+    }
+
+    @DisplayName("resetGame 테스트")
+    @Test
+    public void resetGameTest() {
+        // given
+        Bridge bridge = new Bridge(List.of("U", "D", "D"));
+        bridge.setNowIndex(3);
+
+        // when
+        bridge.resetGame();
+
+        // then
+        assertThat(bridge.getNowIndex()).isEqualTo(1);
+    }
+
+    @DisplayName("nowIndexUpdate 테스트")
+    @Test
+    public void nowIndexUpdateTest() {
+        // given
+        Bridge bridge = new Bridge(List.of("U", "D", "D"));
+
+        // when
+        bridge.nowIndexUpdate();
+
+        // then
+        assertThat(bridge.getNowIndex()).isEqualTo(2);
+    }
+
 }
