28b3cda9e2fab2d737134b8af8ccbf26803c7bfa
nn98 <jkllhgb@gmail.com>
2022-11-18 15:29:11 +0900

    Feat: 출력 메소드 구현

--- src/main/java/bridge/OutputView.java
+++ src/main/java/bridge/OutputView.java
@@ -2,6 +2,8 @@ package bridge;
 
 import static constant.Values.Message.*;
 
+import constant.Values.BridgeCase;
+
 /**
  * 사용자에게 게임 진행 상황과 결과를 출력하는 역할을 한다.
  */
@@ -12,8 +14,13 @@ public class OutputView {
     }
 
     public void printInputSize() {
-        System.out.println(INPUT_SIZE.getMessage());
+        System.out.println("\n"+INPUT_SIZE.getMessage());
     }
+
+    public void printSelectMove() {
+        System.out.println("\n"+SELECT_MOVE.getMessage());
+    }
+
     /**
      * 현재까지 이동한 다리의 상태를 정해진 형식에 맞춰 출력한다.
      * <p>
@@ -22,11 +29,29 @@ public class OutputView {
     public void printMap() {
     }
 
+    public void printSelectRetry() {
+        System.out.println("\n"+SELECT_RETRY.getMessage());
+    }
+
     /**
      * 게임의 최종 결과를 정해진 형식에 맞춰 출력한다.
      * <p>
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
     public void printResult() {
+        printMap();
     }
+
+    public void printGameSuccess(BridgeCase bridgeCase) {
+        System.out.println("\n"+String.format(GAME_SUCCESS.getMessage(),bridgeCase.getMessage()));
+    }
+
+    public void printGameTry(int tryCount) {
+        System.out.println("\n"+String.format(GAME_TRY.getMessage(),tryCount));
+    }
+
+    public void printErrorMessage(IllegalArgumentException illegalArgumentException) {
+        System.out.println("\n"+String.format(ERROR_MESSAGE.getMessage(),illegalArgumentException.getMessage()));
+    }
+
 }
