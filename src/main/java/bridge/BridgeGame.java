5939d6ad5feafb578d094fafc071ce3d9cd97da7
jin-chang-ho <ckdgh8204@naver.com>
2022-11-18 15:57:27 +0900

    Refactor: 게임 시작 문구 출력 기능 구조 변경
    Ref: #2

--- src/main/java/bridge/BridgeGame.java
+++ src/main/java/bridge/BridgeGame.java
@@ -4,6 +4,14 @@ package bridge;
  * 다리 건너기 게임을 관리하는 클래스
  */
 public class BridgeGame {
+    /**
+     * 게임을 시작하는 메서드
+     */
+    public void start() {
+        OutputView outputview = new OutputView();
+
+        outputview.printStart();
+    }
 
     /**
      * 사용자가 칸을 이동할 때 사용하는 메서드
