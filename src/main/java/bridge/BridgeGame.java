c2425b22644f9d9895eb486822195a9138dc912e
neppiness <0414kjh@naver.com>
2022-11-18 15:51:49 +0900

    feat: BridgeGame 클래스 구현 (src/main/java/bridge)
    필드: 게임에서 생성된 bridge와 사용자가 진행 중인 path, 총 시도 횟수를 저장할 numberOfTry
    생성자: 클래스 내 필드 초기화
    run: 게임 흐름에 따라 진행
    move: 사용자 입력을 받고 path를 기록, 현재 상태를 printMap 메소드로 출력
    retry: 잘못된 경로로 이동한 경우 게임 진행 여부를 묻고 path를 수정
    end: 현재 상태에 따라 종료 문구 출력
    getter 메소드: getMovingCommand
    기타 로직 판단 메소드: isWrong, isQuitCommand, isEnd

--- src/main/java/bridge/BridgeGame.java
+++ src/main/java/bridge/BridgeGame.java
@@ -1,23 +1,90 @@
 package bridge;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * 다리 건너기 게임을 관리하는 클래스
  */
 public class BridgeGame {
+    private final static OutputView outputView = new OutputView();
+    private final static InputView inputView = new InputView();
+
+    private List<String> bridge, path;
+    private int numberOfTry;
 
     /**
-     * 사용자가 칸을 이동할 때 사용하는 메서드
-     * <p>
-     * 이동을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
+     * 게임을 생성하는 메소드
      */
-    public void move() {
+    public BridgeGame(List<String> bridge) {
+        this.bridge = bridge;
+        this.path = new ArrayList<>();
+        this.numberOfTry = 1;
+    }
+    
+    /**
+     * 게임을 실행하는 메소드
+     */
+    public void run() {
+        while (!isEnd()) {
+            move();
+            while (isWrong()) {
+                outputView.guideRetry();
+                String gameCommand = inputView.readGameCommand();
+                if (isQuitCommand(gameCommand)) break;
+                retry();
+            }
+        }
+        end();
     }
 
     /**
-     * 사용자가 게임을 다시 시도할 때 사용하는 메서드
+     * 사용자가 칸을 이동할 때 사용하는 메소드
      * <p>
-     * 재시작을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
+     * 이동을 위해 필요한 메소드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void retry() {
+    private void move() {
+        outputView.guideMovingCommandInput();
+        String movingCommand = getMovingCommand();
+        this.path.add(movingCommand);
+        outputView.printMap(this.path);
+    }
+
+    /**
+     * 게임을 끝내는 메소드
+     */
+    private void end() { outputView.printResult(isEnd(), this.path, this.numberOfTry); }
+
+    private String getMovingCommand() { return inputView.readMoving(); }
+
+    /**
+     * 플레이어가 다리 끝에 도달했는지 그 여부를 불린형으로 반환하는 메소드
+     */
+    private boolean isEnd() { return this.path.size() == this.bridge.size(); }
+
+    /**
+     * 종료 코드를 받았는지 확인하는 메소드
+     */
+    private boolean isQuitCommand(String command) { return command.equals("Q"); }
+
+    /**
+     * 사용자가 올바른 칸으로 이동했는지 여부를 불린으로 반환하는 메소드
+     */
+    private boolean isWrong() {
+        int currentPartition = this.path.size() - 1;
+        String currentMovingCommand = this.path.get(currentPartition);
+        String currentBridgePartition = this.bridge.get(currentPartition);
+        return !currentMovingCommand.equals(currentBridgePartition);
+    }
+
+    /**
+     * 사용자가 게임을 다시 시도할 때 사용하는 메소드
+     * <p>
+     * 재시작을 위해 필요한 메소드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
+     */
+    private void retry() {
+        this.numberOfTry++;
+        this.path.remove(this.path.size() - 1);
+        move();
     }
 }
