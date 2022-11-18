c000b541e4c0be002f696647e6e35a1cd53d0bd4
alswn <alswn9938@gmail.com>
2022-11-18 17:46:28 +0900

    feat(OutputView): add 가이드 문장 출력 함수

--- src/main/java/bridge/view/OutputView.java
+++ src/main/java/bridge/view/OutputView.java
@@ -20,4 +20,29 @@ public class OutputView {
      */
     public void printResult() {
     }
+
+    public void printEmptyLine(){
+        System.out.println();
+    }
+
+    public void printStartGuide(){
+        System.out.println("다리 건너기 게임을 시작합니다.");
+    }
+
+    public void printInputBridgeLengthGuide(){
+        System.out.println("다리의 길이를 입력해주세요.");
+    }
+
+    public void printInputMoveOptionGuide(){
+        System.out.println("이동할 칸을 선택해주세요. (위: U, 아래: D)");
+    }
+
+    public void printInputRestartOptionGuide(){
+        System.out.println("게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)");
+    }
+
+    public void printFinalResultGuide(){
+        System.out.println("최종 게임 결과");
+    }
+
 }
