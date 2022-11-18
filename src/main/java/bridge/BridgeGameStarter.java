d7f15f1c1aa117c9e09d89af3aa4fb2221cfe09e
K-Diger <kitten.diger@gmail.com>
2022-11-18 19:38:08 +0900

    feat(BridgeGameStarter) : 게임 성공 여부 및 재시작 여부를 셋팅하는 메서드 추가

--- src/main/java/bridge/BridgeGameStarter.java
+++ src/main/java/bridge/BridgeGameStarter.java
@@ -1,16 +1,21 @@
 package bridge;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class BridgeGameStarter {
 
-    private List<String> bridge = new ArrayList<>();
-    private List<String> result = new ArrayList<>();
+    private final Map<String, Boolean> gameStatusFlag = new HashMap<>();
     private final InputView inputView;
     private final OutputView outputView;
     private final BridgeMaker bridgeMaker;
     private final BridgeGame bridgeGame;
+
+    private List<String> bridge = new ArrayList<>();
+    private List<String> result = new ArrayList<>();
+    private int tryCount = 1;
 
     public BridgeGameStarter(InputView inputView, OutputView outputView, BridgeMaker bridgeMaker, BridgeGame bridgeGame) {
         this.inputView = inputView;
@@ -23,34 +28,72 @@ public class BridgeGameStarter {
         outputView.printGameStartContext();
         outputView.printInputBridgeSize();
         bridge = bridgeMaker.makeBridge(inputView.readBridgeSize());
-        System.out.println(bridge);
         outputView.printInputMoving();
         result = bridgeGame.move(inputView.readMoving(), bridge, 0);
         outputView.printMap(result);
-    }
-
-    public boolean run() {
-        int index = 1;
-        while (!bridgeGame.isGameDone(result) && index < bridge.size()) {
-            outputView.printInputMoving();
-            result = bridgeGame.move(inputView.readMoving(), bridge, index);
-            outputView.printMap(result);
-            index ++;
-        }
-        outputView.printInputGameCommand();
-        return bridgeGame.retry(inputView.readGameCommand());
     }
 
     public void reInitialize() {
         outputView.printInputBridgeSize();
         outputView.printInputMoving();
-        bridgeGame.initializeLeftRightResult();
+        bridgeGame.initializeLeftRightMovingLog();
         result = bridgeGame.move(inputView.readMoving(), bridge, 0);
         outputView.printMap(result);
     }
 
-    public void closeGame(int tryCount) {
-        outputView.printSuccessStatus(true);
-        outputView.printResult(tryCount);
+    public int additionalInput(int index) {
+        outputView.printInputMoving();
+        result = bridgeGame.move(inputView.readMoving(), bridge, index);
+        outputView.printMap(result);
+        index++;
+        return index;
+    }
+
+    public void notifySuccessGame() {
+        gameStatusFlag.put("successFlag", true);
+        gameStatusFlag.put("retryFlag", false);
+    }
+
+    public void notifyFailGameWithRetry() {
+        gameStatusFlag.put("successFlag", false);
+        gameStatusFlag.put("retryFlag", true);
+    }
+
+    public void notifyFailGameWithNotRetry() {
+        gameStatusFlag.put("successFlag", false);
+        gameStatusFlag.put("retryFlag", false);
+    }
+
+    public void operate() {
+        int index = 1;
+        while (bridgeGame.canMove(result)) {
+            if (bridgeGame.isGameEnded(bridge, index)) {
+                notifySuccessGame();
+                return;
+            }
+            index = additionalInput(index);
+        }
+        outputView.printInputGameCommand();
+        if (bridgeGame.retry(inputView.readGameCommand())) {
+            notifyFailGameWithRetry();
+            return;
+        }
+        notifyFailGameWithNotRetry();
+    }
+
+    public void closeGame() {
+        outputView.printFinalMap(result);
+        outputView.printResult(gameStatusFlag.get("successFlag"), tryCount);
+    }
+
+    public void run() {
+        initialize();
+        operate();
+        while (gameStatusFlag.get("retryFlag")) {
+            tryCount++;
+            reInitialize();
+            operate();
+        }
+        closeGame();
     }
 }
