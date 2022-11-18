d5c9dae72ec06815a6e27a9f686fc6c19f4d53c0
tenta <eunoia3802@gmail.com>
2022-11-18 14:23:11 +0900

    feat: 이동할 칸 입력 받기 기능 구현

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -7,6 +7,7 @@ import camp.nextstep.edu.missionutils.Console;
  */
 public class InputView {
     private final String BRIDGE_SIZE = "다리의 길이를 입력해주세요.";
+    private final String MOVE_BRIDGE = "이동할 칸을 선택해주세요. (위: U, 아래: D)";
 
     /**
      * 다리의 길이를 입력받는다.
@@ -21,7 +22,9 @@ public class InputView {
      * 사용자가 이동할 칸을 입력받는다.
      */
     public String readMoving() {
-        return null;
+        System.out.println(MOVE_BRIDGE);
+        String sideToMove = Console.readLine();
+        return sideToMove;
     }
 
     /**
