5c89bc47ffe6b4b47d055e1f912d3d629e1f0ea3
panorama713 <yaiba713@gmail.com>
2022-11-18 20:49:27 +0900

    Test: 종료를 눌렀을 때 출력되는 값 확인 테스트

--- src/test/java/bridge/BridgeGameTest.java
+++ src/test/java/bridge/BridgeGameTest.java
@@ -4,6 +4,11 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.io.ByteArrayOutputStream;
+import java.io.OutputStream;
+import java.io.PrintStream;
+import java.util.List;
+
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -31,6 +36,20 @@ class BridgeGameTest {
     }
 
     @Test
+    @DisplayName("종료를 눌렀을 때 출력되는 값 확인.")
     void retry() {
+        String input = "Q";
+        List<String> bridge = List.of("U", "D");
+        List<String> moveMapList = List.of("O","X");
+
+        OutputStream out = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(out));
+        bridgeGame.retry(input, bridge, moveMapList, 0);
+
+        assertThat("최종 게임 결과\n" +
+                "[ O | X ]\n" +
+                "[   |   ]\n" +
+                "게임 성공 여부: 성공\n" +
+                "총 시도한 횟수: 1\n").isEqualTo(out.toString());
     }
 }
