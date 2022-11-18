b5042700196acfc4e6c80707889a74240d5a8159
bjk1649 <qhwns1649@naver.com>
2022-11-19 00:57:14 +0900

    fix: 입력을 받을 떄 예외가 일어날 경우 다시 입력을 받도록 수정

--- src/main/java/bridge/view/InputView.java
+++ src/main/java/bridge/view/InputView.java
@@ -12,29 +12,47 @@ public class InputView {
      * 다리의 길이를 입력받는다.
      */
     public int readBridgeSize() {
-        System.out.println("다리의 길이를 입력해주세요.");
-        String input = Console.readLine();
-        InputValidator.validateBridgeSize(input);
-        return Integer.parseInt(input);
+        while (true) {
+            try {
+                System.out.println("다리의 길이를 입력해주세요.");
+                String input = Console.readLine();
+                InputValidator.validateBridgeSize(input);
+                return Integer.parseInt(input);
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            }
+        }
     }
 
     /**
      * 사용자가 이동할 칸을 입력받는다.
      */
     public String readMoving() {
-        System.out.println("이동할 칸을 선택해주세요. (위: U, 아래: D)");
-        String input = Console.readLine();
-        InputValidator.validateMoving(input);
-        return input;
+        while (true) {
+            try {
+                System.out.println("이동할 칸을 선택해주세요. (위: U, 아래: D)");
+                String input = Console.readLine();
+                InputValidator.validateMoving(input);
+                return input;
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            }
+        }
     }
 
     /**
      * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
      */
     public String readGameCommand() {
-    System.out.println("게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)");
-        String input = Console.readLine();
-        InputValidator.validateGameCommand(input);
-        return input;
+        while (true) {
+            try {
+                System.out.println("게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)");
+                String input = Console.readLine();
+                InputValidator.validateGameCommand(input);
+                return input;
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            }
+        }
     }
 }
