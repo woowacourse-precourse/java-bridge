a5c43e309069337785d01c7c6eeaa7bdd6401609
JaeWon Choi <87649850+codesver@users.noreply.github.com>
2022-11-18 10:42:39 +0900

    feat(ErrorMessage): add after error message

--- src/main/java/bridge/constant/ErrorMessage.java
+++ src/main/java/bridge/constant/ErrorMessage.java
@@ -5,7 +5,8 @@ public enum ErrorMessage {
     IS_NOT_POSITIVE_NUMBER("[ERROR] 다리 길이 입력값이 양의 정수가 아닙니다."),
     IS_OUT_OF_RANGE("[ERROR] 다리 길이 입력값이 범위 3 ~ 20을 벗어났습니다."),
     IS_WRONG_MOVEMENT("[ERROR] 움직임 입력이 U 혹은 D 가 아닙니다."),
-    IS_WRONG_RETRY_STATUS("[ERROR] 재시도 입력이 R 혹은 Q 가 아닙니다.");
+    IS_WRONG_RETRY_STATUS("[ERROR] 재시도 입력이 R 혹은 Q 가 아닙니다."),
+    INPUT_AGAIN("올바른 형식으로 재입력하시기 바랍니다.");
 
     private final String message;
 
