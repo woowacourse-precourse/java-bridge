ab88e24031700370662b7c1e36aeabaa4d290da2
woo-chang <clllickme@naver.com>
2022-11-18 17:29:35 +0900

    test(Bridge): 플레이어가 이동할 수 있는 상태인지 검증하는 기능 테스트

--- src/test/java/bridge/model/BridgeTest.java
+++ src/test/java/bridge/model/BridgeTest.java
@@ -1,6 +1,5 @@
 package bridge.model;
 
-import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import java.util.List;
@@ -9,59 +8,51 @@ import org.junit.jupiter.api.Test;
 
 class BridgeTest {
 
-    @DisplayName("플레이어가 이동할 수 있으면 true 반환한다.")
+    @DisplayName("플레이어의 방향 개수가 다리 길이와 같다면 예외가 발생한다.")
     @Test
-    void canMove() {
+    void validatePlayerStatus1() {
         //given
-        Player player = new Player();
         Bridge bridge = new Bridge(List.of("U", "D"));
-
-        //when
-        boolean result = bridge.canMove(player);
-
-        //then
-        assertThat(result).isTrue();
-    }
-
-    @DisplayName("플레이어가 이동할 수 없으면 false 반환한다.")
-    @Test
-    void cannotMove() {
-        //given
         Player player = new Player();
-        Bridge bridge = new Bridge(List.of("U", "D"));
 
         //when
         player.move(Direction.UP);
         player.move(Direction.DOWN);
-        boolean result = bridge.canMove(player);
 
         //then
-        assertThat(result).isFalse();
-    }
-
-    @DisplayName("해당 위치가 다리 길이를 벗어나면 예외가 발생한다.")
-    @Test
-    void outOfIndex() {
-        //given
-        Bridge bridge = new Bridge(List.of("U", "D"));
-        int index = 2;
-        String direction = "D";
-
-        //then
-        assertThatThrownBy(() -> bridge.validateDirection(index, direction))
+        assertThatThrownBy(() -> bridge.validatePlayerStatus(player))
                 .isInstanceOf(IllegalStateException.class);
     }
 
-    @DisplayName("해당 위치의 방향이 올바르지 않으면 예외가 발생한다.")
+    @DisplayName("플레이어의 방향 개수가 다리 길이보다 크다면 예외가 발생한다.")
     @Test
-    void validateDirection() {
+    void validatePlayerStatus2() {
         //given
         Bridge bridge = new Bridge(List.of("U", "D"));
-        int index = 0;
-        String direction = "D";
+        Player player = new Player();
+
+        //when
+        player.move(Direction.UP);
+        player.move(Direction.DOWN);
+        player.move(Direction.DOWN);
 
         //then
-        assertThatThrownBy(() -> bridge.validateDirection(index, direction))
+        assertThatThrownBy(() -> bridge.validatePlayerStatus(player))
+                .isInstanceOf(IllegalStateException.class);
+    }
+
+    @DisplayName("플레이어의 방향 중 다리와 일치하지 않는 것이 존재하면 예외가 발생한다.")
+    @Test
+    void validatePlayerStatus3() {
+        //given
+        Bridge bridge = new Bridge(List.of("U", "D"));
+        Player player = new Player();
+
+        //when
+        player.move(Direction.DOWN);
+
+        //then
+        assertThatThrownBy(() -> bridge.validatePlayerStatus(player))
                 .isInstanceOf(IllegalStateException.class);
     }
 
