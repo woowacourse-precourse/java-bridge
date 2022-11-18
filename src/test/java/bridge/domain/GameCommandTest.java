e83845fafa7bf4725078d407d1d42e33e19979be
minju412 <ln8847@ajou.ac.kr>
2022-11-18 23:38:44 +0900

    test(GameCommandTest): add tests
    - 재시작 혹은 종료를 위한 입력이 공백이라면 예외가 발생한다
    - 재시작 혹은 종료를 위한 입력이 R 또는 Q 가 아니라면 예외가 발생한다

---
+++ src/test/java/bridge/domain/GameCommandTest.java
@@ -0,0 +1,23 @@
+package bridge.domain;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class GameCommandTest {
+
+    @DisplayName("재시작 혹은 종료를 위한 입력이 공백이라면 예외가 발생한다.")
+    @Test
+    void createDirectionWithBlank() {
+        assertThatThrownBy(() -> new GameCommand(""))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("재시작 혹은 종료를 위한 입력이 R 또는 Q 가 아니라면 예외가 발생한다.")
+    @Test
+    void createDirectionWithInvalidCharacter() {
+        assertThatThrownBy(() -> new GameCommand("r"))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+}
