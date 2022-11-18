a4b57ab8148b66ad6de367089b4d20433b956956
5nam <ohnam90@gmail.com>
2022-11-18 18:03:18 +0900

    feat: 생성된 다리와 사용자 이동방향을 비교하여 True, False 를 리턴하는 함수 추가

--- src/main/java/bridge/Model/BridgeJudge.java
+++ src/main/java/bridge/Model/BridgeJudge.java
@@ -1,4 +1,9 @@
 package bridge.Model;
 
+import java.util.List;
+
 public class BridgeJudge {
+    public boolean judgeInput(String userInput, List<String> bridge, int index) {
+        return userInput.equals(bridge.get(index));
+    }
 }
