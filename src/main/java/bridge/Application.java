cfdfd98ec29e8c76244543d8ea8539589a22788e
asas6978 <asas6978@naver.com>
2022-11-19 02:48:52 +0900

    feat: 메인 실행 메서드 구현

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -3,6 +3,11 @@ package bridge;
 public class Application {
 
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        try {
+            BridgeGame bridgeGame = new BridgeGame(new BridgeRandomNumberGenerator());
+            bridgeGame.start();
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+        }
     }
 }
