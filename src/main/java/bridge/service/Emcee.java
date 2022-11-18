84803e6264b496a3f82bbac644ad8de54c2bbaae
JaeWon Choi <87649850+codesver@users.noreply.github.com>
2022-11-18 19:45:20 +0900

    refactor(Emcee): use enum instead String

--- src/main/java/bridge/service/Emcee.java
+++ src/main/java/bridge/service/Emcee.java
@@ -1,5 +1,6 @@
 package bridge.service;
 
+import bridge.constant.GuideMessage;
 import bridge.view.OutputView;
 
 public class Emcee {
@@ -7,25 +8,22 @@ public class Emcee {
     private final OutputView outputView = new OutputView();
 
     public void guideGame() {
-        String guide = "다리 건너기 게임을 시작합니다.\n" +
-                "다리는 위쪽과 아래쪽으로 구별되며 왼쪽에서 오른쪽으로 건너갑니다.\n" +
-                "위쪽과 아래쪽 중에서 하나의 칸만 이동이 가능합니다.\n";
+        String guide = GuideMessage.GAME_STARTING_GUIDE.message();
         outputView.printGuide(guide);
     }
 
     public void guideEnteringBridgeLength() {
-        String guide = "다리의 길이를 입력해주세요.\n" +
-                "다리의 길이는 3 이상 20 이하입니다.";
+        String guide = GuideMessage.BRIDGE_LENGTH_ENTERING_GUIDE.message();
         outputView.printGuide(guide);
     }
 
     public void guideEnteringMovement() {
-        String guide = "이동할 칸을 선택하세요. (위: U, 아래: D)";
+        String guide = GuideMessage.MOVEMENT_ENTERING_GUIDE.message();
         outputView.printGuide(guide);
     }
 
     public void guideEnteringRetryStatus() {
-        String guide = "게임을 다시 시도할지 여부를 입력하세요. (재시도: R, 종료: Q)";
+        String guide = GuideMessage.RETRY_ENTERING_GUIDE.message();
         outputView.printGuide(guide);
     }
 
