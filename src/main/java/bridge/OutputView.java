f3552470a0101e7687d4ea16868706669542411f
JIWEON-JEONG <dlektl6@naver.com>
2022-11-18 23:20:26 +0900

    docs : 현재까지 이동한 다리의 상태 출력

--- src/main/java/bridge/OutputView.java
+++ src/main/java/bridge/OutputView.java
@@ -5,6 +5,14 @@ package bridge;
  */
 public class OutputView {
 
+    private final StringBuilder upViewMap;
+    private final StringBuilder downViewMap;
+
+    public OutputView() {
+        this.upViewMap = new StringBuilder();
+        this.downViewMap = new StringBuilder();
+    }
+
     private void printStartMessage() {
         System.out.println("다리 건너기 게임을 시작합니다.");
         System.out.println();
@@ -14,11 +22,15 @@ public class OutputView {
         System.out.println("다리의 길이를 입력해주세요.");
     }
 
+    //application 에서 코드 줄이기 위해 병합.
     public void printGettingStart() {
         printStartMessage();
         askBridgeSize();
     }
 
+    public void askCommand() {
+        System.out.println("이동할 칸을 선택해주세요. (위: U, 아래: D)");
+    }
 
     /**
      * 현재까지 이동한 다리의 상태를 정해진 형식에 맞춰 출력한다.
@@ -26,6 +38,17 @@ public class OutputView {
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
     public void printMap() {
+
+    }
+
+    private void handleMap(StringBuilder builder, String result) {
+        builder.replace(builder.length() - 1, builder.length(), "|");
+
+    }
+    private void handleMapIfFirst(StringBuilder builder, String result) {
+        builder.append("[ ");
+        builder.append(result);
+        builder.append(" ]");
     }
 
     /**
--- src/main/java/bridge/Result.java
+++ src/main/java/bridge/Result.java
@@ -3,14 +3,14 @@ package bridge;
 public enum Result {
     SUCCESS("O"), FAIL("X"),;
 
-    private String printSymbol;
+    private String view;
 
-    Result(String printSymbol) {
-        this.printSymbol = printSymbol;
+    Result(String view) {
+        this.view = view;
     }
 
     //메서드 이름 리팩토링.
-    public String getPrintSymbol() {
-        return printSymbol;
+    public String getView() {
+        return view;
     }
 }
