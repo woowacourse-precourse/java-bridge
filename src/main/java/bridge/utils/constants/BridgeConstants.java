8d1cb27b7625514857a12110cf3205e1e58e35e7
박승민 <tmddudals369@naver.com>
2022-11-19 04:07:07 +0900

    feat: 다리 생성 공백 부분 상수 추가

--- src/main/java/bridge/utils/constants/BridgeConstants.java
+++ src/main/java/bridge/utils/constants/BridgeConstants.java
@@ -9,6 +9,7 @@ public class BridgeConstants {
     public static final String INCORRECT_ANSWER = "X";
     public static final String UP_CROSS = "U";
     public static final String DOWN_CROSS = "D";
+    public static final String SPACE = " ";
     public static final String INPUT_BRIDGE_LENGTH = "\n다리의 길이를 입력해주세요.";
     public static final String INPUT_BRIDGE_MOVE_OPTION = "\n이동할 칸을 선택해주세요. (위: U, 아래: D)";
 }
