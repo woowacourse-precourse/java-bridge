1481d0dcebbf4841ba9c783692abcd4f4d8856be
egoing <dudqk9696@naver.com>
2022-11-18 21:05:22 +0900

    feat : 게임 성공 여부 확인

--- src/main/java/bridge/BridgeGame.java
+++ src/main/java/bridge/BridgeGame.java
@@ -45,6 +45,12 @@ public class BridgeGame {
         return false;
     }
 
+    public boolean checkEnd(AllBridge allBridge) {
+        if(userPos == allBridge.getBridge().size()) {
+            return true;
+        }
+        return false;
+    }
 
     /**
      * 사용자가 게임을 다시 시도할 때 사용하는 메서드
