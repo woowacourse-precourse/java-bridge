ad8a47d35f64042ae7b0c6ef36da9323ae73fbf1
SuhHyeonjun <dmsgktn0208@gmail.com>
2022-11-18 14:27:48 +0900

    feat : 다리 건너기 게임 시작 메서드

--- src/main/java/bridge/controller/BridgeGameController.java
+++ src/main/java/bridge/controller/BridgeGameController.java
@@ -1,8 +1,36 @@
 package bridge.controller;
 
+import static bridge.UpDownBridge.downBridge;
+import static bridge.UpDownBridge.upBridge;
+
+import bridge.BridgeGame;
+import bridge.BridgeMaker;
+import bridge.BridgeRandomNumberGenerator;
+import bridge.InputView;
+import bridge.Message;
+import bridge.OutputView;
+import bridge.UpDownBridge;
+import java.util.List;
+
 public class BridgeGameController {
 
-    public static void  start() {
+    private static boolean status = true;
+    private static int bridgeLocation = 0;
+    private final InputView inputView = new InputView();
+    private final OutputView outputView = new OutputView();
+    private final BridgeGame bridgeGame = new BridgeGame();
+    private final BridgeMaker bridgeMaker = new BridgeMaker(new BridgeRandomNumberGenerator());
 
+    public void  start() {
+        int inputSize = inputView.inputBridgeSize();
+        List<String> bridges = bridgeMaker.makeBridge(inputSize);
+        System.out.println(bridges);
+        while (status && bridgeLocation < inputSize) {
+            String inputDirection = inputView.inputMovingDirection();
+            String moveResult = bridgeGame.move(bridges.get(bridgeLocation), inputDirection);
+            outputView.printMap(inputDirection, moveResult);
+            bridgeLocation++;
+        }
+        outputView.printResult(status);
     }
 }
