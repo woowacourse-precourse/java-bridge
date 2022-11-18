8aab41e7bcba813b91dcef80586227fb296ff36c
kdkdhoho <hkim4410@naver.com>
2022-11-18 15:20:47 +0900

    comment: 필요없는 주석 제거

--- src/main/java/bridge/model/AnswerBridge.java
+++ src/main/java/bridge/model/AnswerBridge.java
@@ -15,8 +15,6 @@ public class AnswerBridge {
         return answerBridge;
     }
 
-    // TODO: 이제 중간에 실패할 경우, 재시작 여부를 묻고, 그에 따라 게임을 재시작하거나 끝내는 기능 만들면 됨
-
     public List<Boolean> compareTo(User user) {
         List<String> userBridge = user.getChoices();
         List<Boolean> result = new ArrayList<>();
