f2b08aef303e44b88f8c703502eed7cc5b63c7c3
jungle <minuet1215@jbnu.ac.kr>
2022-11-18 15:23:09 +0900

    Feat(Position): Player의 좌표 값을 담는 Position 클래스 구현

--- src/main/java/bridge/model/Position.java
+++ src/main/java/bridge/model/Position.java
@@ -1,2 +1,10 @@
-package bridge.model;public class Position {
+package bridge.model;
+
+public class Position {
+    public int x, y;
+
+    public Position() {
+        this.x = 0;
+        this.y = 0;
+    }
 }
