479814c87a6e36cd9e43a32ab061eb64d466460c
hwikyungkim-1215 <buc06137@naver.com>
2022-11-19 05:19:10 +0900

    feat: [기능3] 다리 생성하는 기능 구현

--- src/main/java/bridge/controller/BridgeGameController.java
+++ src/main/java/bridge/controller/BridgeGameController.java
@@ -1,13 +1,21 @@
 package bridge.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import static bridge.model.BridgeMaker.makeBridge;
 import static bridge.view.InputView.*;
 
 public class BridgeGameController {
     public static void bridgeGame(){
         int bridgeLen = readBridgeSize();
         bridgeLenRangeCheck(bridgeLen);
+        List<String> bridges = new ArrayList<>();
+        bridges = makeBridge(bridgeLen);
+
         String move = readMoving();
         moveCheck(move);
+
 
     }
 }
--- src/main/java/bridge/model/BridgeMaker.java
+++ src/main/java/bridge/model/BridgeMaker.java
@@ -1,6 +1,11 @@
 package bridge.model;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
 import java.util.List;
+
+import static bridge.view.InputView.readBridgeSize;
 
 /**
  * 다리의 길이를 입력 받아서 다리를 생성해주는 역할을 한다.
@@ -17,7 +22,21 @@ public class BridgeMaker {
      * @param size 다리의 길이
      * @return 입력받은 길이에 해당하는 다리 모양. 위 칸이면 "U", 아래 칸이면 "D"로 표현해야 한다.
      */
-    public List<String> makeBridge(int size) {
-        return null;
+    public static List<String> makeBridge(int size) {
+        List<String> bridges = new ArrayList<>();
+        for(int num = 0; num < size; num++){
+            double ran = Math.random()*2;
+            int rand =  (int) ran;
+            System.out.println("rand: " + rand);
+            // 0이면 U, 1이면 D
+            if(rand == 0){
+                bridges.add("U");
+            }else if(rand == 1){
+                bridges.add("D");
+            }
+
+        }
+        System.out.println("bridge만들기: " + bridges.toString());
+        return bridges;
     }
 }
