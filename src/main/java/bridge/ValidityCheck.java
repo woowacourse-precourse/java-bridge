f7fd9e792a83e777fe57a85af29b8bb4deb08e1f
JunTaeINC <juntea3523@naver.com>
2022-11-18 09:19:44 +0900

    feat : (ValidityCheck) stringChecker , numberRangeChecker 리턴값 boolean 으로 번경

--- src/main/java/bridge/ValidityCheck.java
+++ src/main/java/bridge/ValidityCheck.java
@@ -4,27 +4,24 @@ import bridge.enums.Number;
 
 import java.util.regex.Pattern;
 
-import static bridge.enums.ErrorMessage.*;
-
 public class ValidityCheck {
 
-    public boolean stringChecker(String testObject) {
+    public static boolean stringChecker(String testObject) {
         Pattern zeroToNine = Pattern.compile("^[0-9]*$");
         if (!zeroToNine.matcher(testObject).matches()) {
-            throw new IllegalArgumentException(ER_BRIDGE_LENGTH_INPUT.getMessage());
+            return false;
         }
         return true;
     }
 
-    public boolean numberRangeChecker(String testObject) {
+    public static boolean numberRangeChecker(String testObject) {
         if (stringChecker(testObject)) {
             if (Number.RANGE_MIN.getValue() > Integer.parseInt(testObject) ||
                     Integer.parseInt(testObject) > Number.RANGE_MAX.getValue()) {
-                throw new IllegalArgumentException(ER_BRIDGE_LENGTH_INPUT.getMessage());
+                return false;
             }
         }
         return true;
     }
-
 }
 
--- src/test/java/bridge/ValidityCheckTest.java
+++ src/test/java/bridge/ValidityCheckTest.java
@@ -2,7 +2,6 @@ package bridge;
 
 import org.junit.jupiter.api.*;
 
-import static bridge.enums.ErrorMessage.*;
 import static org.junit.jupiter.api.Assertions.*;
 
 class ValidityCheckTest {
@@ -16,46 +15,46 @@ class ValidityCheckTest {
 
     @DisplayName("다리의 길이 입력시 문자를 입력하면 예외가 발생한다.")
     @Nested
-    class 문자_검사 {
-
+    class 문자_확인 {
         @Test
-        void case_1() {
-            Throwable illegalArgumentException = assertThrows(IllegalArgumentException.class, () -> {
-                validityCheck.stringChecker("십오");
-            });
-            assertEquals(ER_BRIDGE_LENGTH_INPUT.getMessage(), illegalArgumentException.getMessage());
+        void case_문자() {
+            String Test1 = "십오";
+            String Test2 = "15j";
+
+            assertFalse(validityCheck.stringChecker(Test1));
+            assertFalse(validityCheck.stringChecker(Test2));
         }
 
         @Test
-        void case_2() {
-            Throwable illegalArgumentException = assertThrows(IllegalArgumentException.class, () -> {
-                validityCheck.stringChecker("10s");
-            });
-            assertEquals(ER_BRIDGE_LENGTH_INPUT.getMessage(), illegalArgumentException.getMessage());
-        }
+        void case_숫자() {
+            String Test1 = "15";
+            String Test2 = "20";
 
+            assertTrue(validityCheck.stringChecker(Test1));
+            assertTrue(validityCheck.stringChecker(Test2));
+        }
     }
 
     @DisplayName("다리의 길이 범위(3~20)를 벗어난 숫자 입력시 예외가 발생한다.")
     @Nested
-    class 범위_검사 {
-
+    class 범위_확인 {
         @Test
-        void case_1() {
-            Throwable illegalArgumentException = assertThrows(IllegalArgumentException.class, () -> {
-                validityCheck.numberRangeChecker("21");
-            });
-            assertEquals(ER_BRIDGE_LENGTH_INPUT.getMessage(), illegalArgumentException.getMessage());
+        void case_범위_밖() {
+            String rangeOut1 = "21";
+            String rangeOut2 = "2";
+
+            assertFalse(validityCheck.numberRangeChecker(rangeOut1));
+            assertFalse(validityCheck.numberRangeChecker(rangeOut2));
         }
 
         @Test
-        void case_2() {
-            Throwable illegalArgumentException = assertThrows(IllegalArgumentException.class, () -> {
-                validityCheck.numberRangeChecker("0");
-            });
-            assertEquals(ER_BRIDGE_LENGTH_INPUT.getMessage(), illegalArgumentException.getMessage());
+        void case_범위_안() {
+            String rangeIn1 = "11";
+            String rangeIn2 = "16";
 
+            assertTrue(validityCheck.numberRangeChecker(rangeIn1));
+            assertTrue(validityCheck.numberRangeChecker(rangeIn2));
         }
-
     }
 }
+
