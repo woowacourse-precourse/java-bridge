208e0f6af57b916eb6fe3ba4a70381f87f6e0fcf
Guga <103228463+BGuga@users.noreply.github.com>
2022-11-18 17:39:04 +0900

    feat: controller 생성

--- src/main/java/bridge/controller/BridgeGameController.java
+++ src/main/java/bridge/controller/BridgeGameController.java
@@ -1,2 +1,50 @@
-package bridge.controller;public class BridgeGameController {
+package bridge.controller;
+
+import bridge.BridgeMaker;
+import bridge.BridgeRandomNumberGenerator;
+import bridge.domain.Bridge;
+import bridge.domain.BridgeGame;
+import bridge.domain.Command;
+import bridge.domain.Direction;
+import bridge.view.InputView;
+import bridge.view.OutputView;
+
+import java.util.List;
+
+public class BridgeGameController {
+    OutputView outputView = new OutputView();
+    InputView inputView = new InputView();
+
+    public void process() {
+        outputView.printGameStartingMessage();
+        BridgeGame bridgeGame = new BridgeGame(getRandomBridgeFromUserByBridge());
+        while (bridgeGame.isNeedToQuit()) {
+            playGameUntilEnd(bridgeGame);
+            askGameCommand(bridgeGame);
+        }
+        outputView.printResult(bridgeGame.getGameResult());
+    }
+
+    private Bridge getRandomBridgeFromUserByBridge() {
+        BridgeMaker bridgeMaker = new BridgeMaker(new BridgeRandomNumberGenerator());
+        int bridgeLength = inputView.readBridgeSize();
+        List<String> bridgeSourceData = bridgeMaker.makeBridge(bridgeLength);
+        return new Bridge(bridgeSourceData);
+    }
+
+    private void playGameUntilEnd(BridgeGame bridgeGame) {
+        while (bridgeGame.isEndGame()) {
+            Direction direction = inputView.readDirection();
+            bridgeGame.move(direction);
+            outputView.printMap(bridgeGame.getProgressUserState());
+        }
+    }
+
+    private void askGameCommand(BridgeGame bridgeGame) {
+        if (bridgeGame.isNeedToQuit()) {
+            return;
+        }
+        Command command = inputView.readGameCommand();
+        bridgeGame.retry(command);
+    }
 }
