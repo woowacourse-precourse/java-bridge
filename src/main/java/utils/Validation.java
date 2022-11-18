67054daedca5f2208c3fc6ec331da3d9f9439721
ho2n <jungho2n@gmail.com>
2022-11-18 15:32:27 +0900

    feat(Validate): 입력 받은 다리의 길이를 검증하는 기능을 추가

--- src/main/java/utils/Validation.java
+++ src/main/java/utils/Validation.java
@@ -1,7 +1,32 @@
 package utils;
 
+import static constant.ErrorMessage.*;
+import static constant.ErrorMessage.*;
+
 public class Validation {
+
+    public static final int MINIMUM_LENGTH = 3;
+    public static final int MAXIMUM_LENGTH = 20;
+
     public Validation() {
 
     }
+
+    public void validBridgeSize(String bridgeSize) throws IllegalArgumentException {
+        try {
+            validBridgeLengthRange(Long.parseLong(bridgeSize));
+        } catch (NullPointerException e) {
+            throw new IllegalArgumentException(String.format(ASK_INPUT_VALUE, ERROR_HEAD_MESSAGE));
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException(String.format(ASK_INPUT_NUMBER, ERROR_HEAD_MESSAGE));
+        }
+    }
+
+    private void validBridgeLengthRange(long bridgeSize) throws IllegalArgumentException {
+        if (bridgeSize < MINIMUM_LENGTH || bridgeSize > MAXIMUM_LENGTH) {
+            throw new IllegalArgumentException(String.format(WRONG_LENGTH, ERROR_HEAD_MESSAGE));
+        }
+    }
+
+
 }
--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -4,19 +4,27 @@ import static constant.Message.*;
 
 import camp.nextstep.edu.missionutils.Console;
 import constant.Message;
+import utils.Validation;
 
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
  * InputView의 메서드의 시그니처(인자, 이름)와 반환 타입은 변경할 수 있다.
  */
 public class InputView {
+    private final Validation validation;
+
+    public InputView(Validation validation) {
+        this.validation = validation;
+    }
 
     /**
      * 다리의 길이를 입력받는다.
      */
     public int readBridgeSize() {
         System.out.println(INPUT_BRIDGE_LENGTH);
-        Console.readLine();
+        String bridgeSize = Console.readLine();
+        validation.validBridgeSize(bridgeSize);
+        return Integer.parseInt(bridgeSize);
     }
 
     /**
