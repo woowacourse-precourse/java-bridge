83ace4c9b827cd1272ea3560d8922a0d7cb443db
Jin409 <sualng123@gmail.com>
2022-11-19 01:26:04 +0900

    feat: 다리 생성하는 기능 구현

--- src/main/java/bridge/constant/BridgeGameConstant.java
+++ src/main/java/bridge/constant/BridgeGameConstant.java
@@ -3,4 +3,6 @@ package bridge.constant;
 public class BridgeGameConstant {
     public static final int VALID_MIN_RANGE = 3;
     public static final int VALID_MAX_RANGE = 20;
+    public static final String UP_SIGN = "U";
+    public static final String DOWN_SIGN = "D";
 }
---
+++ src/main/java/bridge/model/Bridge.java
@@ -0,0 +1,13 @@
+package bridge.model;
+
+import java.util.List;
+
+public class Bridge {
+    List<String> path;
+
+    public Bridge(
+            List<String> path
+    ) {
+        this.path = path;
+    }
+}
