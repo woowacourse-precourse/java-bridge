d28407442e02a842f9514534e97f4d0e00cb6768
Be-student <presentsong@naver.com>
2022-11-18 14:55:06 +0900

    feat(PlayLogDto): 로그 검증 null 검증 기능 추가

--- src/main/java/bridge/dto/PlayLogDto.java
+++ src/main/java/bridge/dto/PlayLogDto.java
@@ -1,10 +1,18 @@
 package bridge.dto;
 
 public final class PlayLogDto {
+    private static final String NULL_MESSAGE = "로그에는 null 이 들어오면 안됩니다";
     private final String log;
 
     public PlayLogDto(String log) {
+        validateLog(log);
         this.log = log;
+    }
+
+    private void validateLog(String log) {
+        if (log == null) {
+            throw new IllegalArgumentException(NULL_MESSAGE);
+        }
     }
 
     public String getLog() {
