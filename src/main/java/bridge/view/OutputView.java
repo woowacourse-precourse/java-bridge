26f26e5c77c69d28e4ea9db7c7ac31535e2fbad1
anneu <b.imnuj@gmail.com>
2022-11-19 01:03:08 +0900

    feat: 게임 최종 결과 출력 기능 구현

--- src/main/java/bridge/view/OutputView.java
+++ src/main/java/bridge/view/OutputView.java
@@ -1,5 +1,6 @@
 package bridge.view;
 
+import bridge.BridgeGameResult;
 import bridge.BridgeMoveResult;
 import java.util.List;
 
@@ -16,6 +17,9 @@ public class OutputView {
     private static final String BRIDGE_SIZE = "다리의 길이를 입력해주세요.";
     private static final String MOVING = "이동할 칸을 선택해주세요. (위: U, 아래: D)";
     private static final String GAME_COMMAND = "게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)";
+    private static final String GAME_RESULT = "최종 게임 결과";
+    private static final String GAME_SUCCESS = "게임 성공 여부: ";
+    private static final String GAME_TRY_COUNT = "총 시도한 횟수: ";
 
     /**
      * 현재까지 이동한 다리의 상태를 정해진 형식에 맞춰 출력한다.
@@ -50,10 +54,12 @@ public class OutputView {
 
     /**
      * 게임의 최종 결과를 정해진 형식에 맞춰 출력한다.
-     * <p>
-     * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void printResult() {
+    public void printResult(BridgeGameResult gameResult) {
+        System.out.println(separateLine() + GAME_RESULT);
+        printMap(gameResult.getMoveResult());
+        System.out.println(separateLine() + GAME_SUCCESS + gameResult.getGameResult());
+        System.out.println(GAME_TRY_COUNT + gameResult.getTryCount());
     }
 
     public void printGameStartMsg() {
