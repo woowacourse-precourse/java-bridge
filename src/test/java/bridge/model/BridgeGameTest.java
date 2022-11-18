931a4f97f6ca7491bfe3d0ea94637c872742fe88
jeongjihun <wlgns2234@naver.com>
2022-11-18 19:27:06 +0900

    test(model): 현재 이동상황을 기록해주는 기능 테스트

---
+++ src/test/java/bridge/model/BridgeGameTest.java
@@ -0,0 +1,29 @@
+package bridge.model;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class BridgeGameTest {
+    private BridgeGame bridgeGame;
+
+    @BeforeEach
+    void setUp() {
+        bridgeGame = new BridgeGame();
+    }
+
+    @DisplayName("현재 상태가 잘 추가 되었나 확인")
+    @Test
+    void 현재_건넌_상태_추가() {
+        List<String> preStatus = new ArrayList<>();
+        bridgeGame.move(preStatus, "U");
+        assertThat(preStatus).isEqualTo(Arrays.asList("U"));
+    }
+
+}
