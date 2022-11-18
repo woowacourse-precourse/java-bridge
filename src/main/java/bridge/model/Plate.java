9e22235ad2dfa32c579af326d963f508ad302edc
dooboocookie <bkh3030@gmail.com>
2022-11-18 14:58:22 +0900

    feat: Plate enum U,D 혹은 0,1로 enum값 찾을 수 있도록 맵 필드와 메소드 구현

--- src/main/java/bridge/model/Plate.java
+++ src/main/java/bridge/model/Plate.java
@@ -3,10 +3,20 @@ package bridge.model;
 import bridge.util.Rules;
 
 import java.text.DecimalFormat;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public enum Plate {
     UP_PLATE("U", Rules.PLATE_UP_PLATE),
     DOWN_PLATE("D", Rules.PLATE_DOWN_PLATE);
+
+    private final static Map<String, Plate> BY_SYMBOL = Stream.of(values())
+            .collect(Collectors.toMap(Plate::getSymbol, Function.identity()));
+
+    private final static Map<Integer, Plate> BY_RANDOM_NUMBER = Stream.of(values())
+            .collect(Collectors.toMap(Plate::getRandomNumber, Function.identity()));
 
     private String symbol;
     private int randomNumber;
@@ -14,5 +24,21 @@ public enum Plate {
     Plate(String symbol, int randomNumber) {
         this.symbol = symbol;
         this.randomNumber = randomNumber;
+    }
+
+    public String getSymbol() {
+        return this.symbol;
+    }
+
+    public int getRandomNumber() {
+        return this.randomNumber;
+    }
+
+    public static Plate findBySymbol(String symbol) {
+        return BY_SYMBOL.get(symbol);
+    }
+
+    public static Plate findByRandomNumber(int randomNumber) {
+        return BY_RANDOM_NUMBER.get(randomNumber);
     }
 }
