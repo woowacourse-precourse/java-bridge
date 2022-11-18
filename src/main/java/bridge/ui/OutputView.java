aaad9a36a755538c1eba99aa0f82dda90f6fd2db
SHEOMM <esh0821@snu.ac.kr>
2022-11-18 22:31:42 +0900

    feat: 다리 그리는 함수 구현

--- src/main/java/bridge/ui/OutputView.java
+++ src/main/java/bridge/ui/OutputView.java
@@ -1,23 +1,50 @@
 package bridge.ui;
 
+import bridge.model.Answer;
+import bridge.model.Bridge;
+import bridge.model.Direction;
 import bridge.model.GameMessage;
+import java.util.List;
 
 public class OutputView {
 
-    public void printMap(String upperSide, String downSide) {
-        System.out.println(upperSide);
-        System.out.println(downSide);
+    public void printMap(List<Direction> bridge, int step) {
+        drawBridge(bridge, step, Direction.UP);
+        drawBridge(bridge, step, Direction.DOWN);
     }
-
 
     public void printResult(String isSuccess, String totalTry) {
         System.out.println(isSuccess);
         System.out.print(totalTry);
     }
 
-    public void printMapResult(String upperSide, String downSide){
-        System.out.println(GameMessage.GAME_RESULT.getOutput());
-        System.out.println(upperSide);
-        System.out.println(downSide);
+    private void drawBridge(List<Direction> bridge, int step, Direction direction){
+        System.out.println(Bridge.START.getView()
+                + getProgress(bridge, step, direction)
+                + getFinalProgress(bridge, step, direction));
+    }
+
+    private String getProgress(List<Direction> bridge, int step, Direction direction){
+        StringBuilder result = new StringBuilder();
+        for(int stepCount = 0; stepCount < step - 2; stepCount++){
+            if(bridge.get(stepCount) == direction){
+                result.append(Answer.CORRECT.getOutput());
+            }else if(bridge.get(stepCount) != direction){
+                result.append(Answer.NONE.getOutput());
+            }
+            result.append(Bridge.MID.getView());
+        }
+        return result.toString();
+    }
+
+    private String getFinalProgress(List<Direction> bridge, int step, Direction direction){
+        StringBuilder result = new StringBuilder();
+        if(bridge.get(step - 1) == direction){
+            result.append(Answer.CORRECT.getOutput());
+        }else if(bridge.get(step - 1) != direction){
+            result.append(Answer.INCORRECT.getOutput());
+        }
+        result.append(Bridge.END.getView());
+        return result.toString();
     }
 }
