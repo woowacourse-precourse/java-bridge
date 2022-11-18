293f37a196b11c11f80ba72b3c6ebdd069849588
asas6978 <asas6978@naver.com>
2022-11-19 02:05:04 +0900

    feat: 에러 메시지를 포함해 예외를 던지도록 추가

--- src/main/java/bridge/Validation.java
+++ src/main/java/bridge/Validation.java
@@ -1,32 +1,35 @@
 package bridge;
 
 import bridge.util.Constant;
+import bridge.util.ExceptionMessage;
 
 public class Validation {
-    public void checkOnlyNumber(String inputSize) throws IllegalArgumentException {
+    private static final String ERROR_MESSAGE = "[ERROR] ";
+
+    public static void checkOnlyNumber(String inputSize) throws IllegalArgumentException {
         try {
             Integer.parseInt(inputSize);
         } catch (NumberFormatException e) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException(ERROR_MESSAGE + ExceptionMessage.CANNOT_CONVERT_TO_NUMBER);
         }
     }
 
-    public void isValidBridgeSize(String inputSize) throws IllegalArgumentException {
+    public static void isValidBridgeSize(String inputSize) throws IllegalArgumentException {
         int size = Integer.parseInt(inputSize);
         if (size < 3 || size > 20) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException(ERROR_MESSAGE + ExceptionMessage.BRIDGE_SIZE_OUT_OF_RANGE);
         }
     }
 
-    public void isValidDirection(String inputDirection) throws IllegalArgumentException {
+    public static void isValidDirection(String inputDirection) throws IllegalArgumentException {
         if (!inputDirection.equals(Constant.UP) && !inputDirection.equals(Constant.DOWN)) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException(ERROR_MESSAGE + ExceptionMessage.INVALID_DIRECTION);
         }
     }
 
-    public void isValidGameCommand(String inputCommand) throws IllegalArgumentException {
+    public static void isValidGameCommand(String inputCommand) throws IllegalArgumentException {
         if (!inputCommand.equals(Constant.RETRY) && !inputCommand.equals(Constant.QUIT)) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException(ERROR_MESSAGE + ExceptionMessage.INVALID_GAME_COMMAND);
         }
     }
 }
