4c08f90b9b58b9d3c0e0e73c9747150cd5185dca
맹순영 <symaeng98@naver.com>
2022-11-18 21:27:29 +0900

    feat(OutputView): 현재 상태 출력, 결과 출력 등 출력 기능 구현

--- src/main/java/bridge/OutputView.java
+++ src/main/java/bridge/OutputView.java
@@ -1,17 +1,52 @@
 package bridge;
 
+import java.util.List;
+
 /**
  * 사용자에게 게임 진행 상황과 결과를 출력하는 역할을 한다.
  */
 public class OutputView {
     private final String START_SENTENCE = "다리 건너기 게임을 시작합니다.";
     private final String INPUT_BRIDGE_LENGTH_SENTENCE = "다리의 길이를 입력해주세요.";
+    private final String INPUT_RETRY_GAME_SENTENCE = "게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)";
+    private final String GAME_RESULT_SENTENCE = "최종 게임 결과";
+    private final String GAME_SUCCESS_OR_NOT_SENTENCE = "게임 성공 여부: ";
+    private final String GAME_TRY_NUMBER_SENTENCE = "총 시도한 횟수: ";
+
     /**
      * 현재까지 이동한 다리의 상태를 정해진 형식에 맞춰 출력한다.
      * <p>
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void printMap() {
+    public String printMap(List<String> upBoardCheckList, List<String> downBoardCheckList) {
+        String upMap = getUpMap(upBoardCheckList);
+        String downMap = getDownMap(downBoardCheckList);
+        System.out.println(upMap + downMap);
+        return upMap + downMap;
+    }
+
+    public String getUpMap(List<String> upBoardCheckList) {
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append("[");
+        for (int i = 0; i < upBoardCheckList.size() - 1; i++) {
+            stringBuilder.append(" ").append(upBoardCheckList.get(i)).append(" ");
+            stringBuilder.append("|");
+        }
+        stringBuilder.append(" ").append(upBoardCheckList.get(upBoardCheckList.size() - 1)).append(" ");
+        stringBuilder.append("]\n");
+        return stringBuilder.toString();
+    }
+
+    public String getDownMap(List<String> downBoardCheckList) {
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append("[");
+        for (int i = 0; i < downBoardCheckList.size() - 1; i++) {
+            stringBuilder.append(" ").append(downBoardCheckList.get(i)).append(" ");
+            stringBuilder.append("|");
+        }
+        stringBuilder.append(" ").append(downBoardCheckList.get(downBoardCheckList.size() - 1)).append(" ");
+        stringBuilder.append("]\n");
+        return stringBuilder.toString();
     }
 
     /**
@@ -19,7 +54,16 @@ public class OutputView {
      * <p>
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void printResult() {
+    public void printResult(boolean success, int tryCount, List<String> upBoardCheckList, List<String> downBoardCheckList) {
+        System.out.println(GAME_RESULT_SENTENCE);
+        String result = printMap(upBoardCheckList, downBoardCheckList);
+        System.out.println(result);
+
+        System.out.print(GAME_SUCCESS_OR_NOT_SENTENCE);
+        if (success) System.out.println("성공");
+        if (!success) System.out.println("실패");
+        System.out.print(GAME_TRY_NUMBER_SENTENCE);
+        System.out.println(tryCount);
     }
 
     /**
@@ -28,7 +72,18 @@ public class OutputView {
     public void printStart() {
         System.out.println(START_SENTENCE);
     }
+
+    /**
+     * 다리의 길이 입력 문구를 출력한다.
+     */
     public void printInputBridgeLength() {
         System.out.println(INPUT_BRIDGE_LENGTH_SENTENCE);
+    }
+
+    /**
+     * 게임 재시작 문구 출력
+     */
+    public void printInputRetry() {
+        System.out.println(INPUT_RETRY_GAME_SENTENCE);
     }
 }
