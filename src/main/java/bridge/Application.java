6082b164850d9471803da70aa41f35818ef28b4b
renechoi90@gmail.com <renechoi90@gmail.com>
2022-11-18 20:03:49 +0900

    refactor : 예기치 못한 오류에 대한 catch 기능을 전체 어플리케이션으로 확대

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -1,14 +1,22 @@
 package bridge;
 
 import bridge.controller.Controller;
+import bridge.view.OutputView;
 
 public class Application {
 
+    private static final String EXIT_MESSAGE = "어플리케이션을 종료합니다.";
+    private static final String ENTER = "\n";
+
     public static void main(String[] args) {
-        Controller controller = new Controller();
-        controller.run();
+        try {
+            Controller controller = new Controller();
+            controller.run();
+        } catch (IllegalArgumentException ignored) {
+        } finally {
+            OutputView.printGameInfo(ENTER + EXIT_MESSAGE);
+        }
     }
 }
-
 
 
--- src/main/java/bridge/controller/Controller.java
+++ src/main/java/bridge/controller/Controller.java
@@ -1,28 +1,20 @@
 package bridge.controller;
 
-import bridge.domain.game.BridgeGame;
 import bridge.domain.bridgeMaker.BridgeMaker;
 import bridge.domain.bridgeMaker.BridgeRandomNumberGenerator;
+import bridge.domain.game.BridgeGame;
 import bridge.domain.result.ResultRendering;
 import bridge.view.InputView;
 import bridge.view.OutputView;
 
 public class Controller {
 
-	private static final String EXIT_MESSAGE = "게임을 종료합니다.";
-	private static final String ENTER = "\n";
-
 	public void run() {
-		try {
-			InputView inputView = new InputView();
-			BridgeMaker bridgeMaker = new BridgeMaker(new BridgeRandomNumberGenerator());
-			BridgeGame bridgeGame = new BridgeGame();
+		InputView inputView = new InputView();
+		BridgeMaker bridgeMaker = new BridgeMaker(new BridgeRandomNumberGenerator());
+		BridgeGame bridgeGame = new BridgeGame();
 
-			bridgeGame.play(inputView, bridgeMaker);
-			OutputView.printGameInfo(ResultRendering.renderFinalResult(bridgeGame.getGameResult()));
-		} catch (IllegalArgumentException ignored) {
-		} finally {
-			OutputView.printGameInfo(ENTER + EXIT_MESSAGE);
-		}
+		bridgeGame.play(inputView, bridgeMaker);
+		OutputView.printGameInfo(ResultRendering.renderFinalResult(bridgeGame.getGameResult()));
 	}
 }
