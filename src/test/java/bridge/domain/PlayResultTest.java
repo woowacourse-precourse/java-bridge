9ffa7ad73011df4a9408cd4fb4353592553afd5d
rio <75190035+HyoJongPark@users.noreply.github.com>
2022-11-18 09:31:12 +0900

    test(PlayResult): 재시작 기능 테스트 추가

--- src/test/java/bridge/domain/PlayResultTest.java
+++ src/test/java/bridge/domain/PlayResultTest.java
@@ -3,6 +3,7 @@ package bridge.domain;
 import org.assertj.core.api.Assertions;
 import org.assertj.core.api.InstanceOfAssertFactories;
 import org.assertj.core.api.InstanceOfAssertFactory;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -10,10 +11,16 @@ import static org.assertj.core.api.Assertions.*;
 
 class PlayResultTest {
 
+    PlayResult playResult;
+
+    @BeforeEach
+    void setup() {
+        playResult = new PlayResult();
+    }
+
     @DisplayName("다리를 건널 수 있고, 위 방향이면 UP")
     @Test
     void 건널_수_있고_위_방향은_UP() {
-        PlayResult playResult = new PlayResult();
         playResult.updateResult("U", true);
 
         assertThat(playResult)
@@ -25,7 +32,6 @@ class PlayResultTest {
     @DisplayName("다리를 건널 수 있고, 아래 방향이면 DOWN")
     @Test
     void 건널_수_있고_아래_방향은_DOWN() {
-        PlayResult playResult = new PlayResult();
         playResult.updateResult("D", true);
 
         assertThat(playResult)
@@ -37,7 +43,6 @@ class PlayResultTest {
     @DisplayName("다리를 건널 수 없고, 위 방향은 UP_FAIL")
     @Test
     void 건널_수_없고_위_방향은_UP_FAIL() {
-        PlayResult playResult = new PlayResult();
         playResult.updateResult("U", false);
 
         assertThat(playResult)
@@ -49,7 +54,6 @@ class PlayResultTest {
     @DisplayName("다리를 건널 수 없고, 아래 방향은 DOWN_FAIL")
     @Test
     void 건널_수_없고_아래_방향은_DOWN_FAIL() {
-        PlayResult playResult = new PlayResult();
         playResult.updateResult("D", false);
 
         assertThat(playResult)
@@ -58,4 +62,23 @@ class PlayResultTest {
                 .isEqualTo(MoveStatus.DOWN_FAIL);
     }
 
+    @DisplayName("재시작 시 시도 횟수 증가")
+    @Test
+    void 재시작_시_시도_횟수_증가() {
+        playResult.updateResult("D", false);
+        int tryCountBeforeRetry = playResult.getTryCount();
+        playResult.retry();
+
+        assertThat(playResult.getTryCount()).isEqualTo(tryCountBeforeRetry + 1);
+    }
+
+    @DisplayName("재시작 시 실패 결과 삭제")
+    @Test
+    void 재시작_시_실패_결과_삭제() {
+        playResult.updateResult("D", false);
+        int playResultSizeBeforeRetry = playResult.getPlayResult().size();
+        playResult.retry();
+
+        assertThat(playResult.getPlayResult().size()).isEqualTo(playResultSizeBeforeRetry - 1);
+    }
 }
