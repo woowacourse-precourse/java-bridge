a18a5cb558f946bb4e637b7f05531b42507ab15b
BioBeBe <76714219+JaegeonYu@users.noreply.github.com>
2022-11-18 13:52:28 +0900

    feat: String Enum 필드 추가

--- src/main/java/bridge/enums/StringEnum.java
+++ src/main/java/bridge/enums/StringEnum.java
@@ -1,5 +1,15 @@
 package bridge.enums;
 
 public enum StringEnum {
-    
+    UP("U"),
+    DOWN("D"),
+    QUIT("Q"),
+    RESTART("R");
+    private final String key;
+    private StringEnum(String key){
+        this.key=key;
+    }
+    public String key(){
+        return this.key;
+    }
 }
