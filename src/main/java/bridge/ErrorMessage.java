b197de5257781f24b8cdab2f030963a4c9c00846
abbd2 <ajkety00@gmail.com>
2022-11-18 21:12:11 +0900

    Feat(ErrorMessage): "재시작은 'R', 종료는 'Q'를 입력해주세요."를 출력하는 inputRetryError 메서드 구현

--- src/main/java/bridge/ErrorMessage.java
+++ src/main/java/bridge/ErrorMessage.java
@@ -2,8 +2,9 @@ package bridge;
 
 public enum ErrorMessage {
     ERROR_MESSAGE("[ERROR] "),
-    INPUT_BRIDGE_NUMBER("3 이상 20 이하의 숫자를 입력해주세요."),
-    INPUT_MOVE_BRIDGE_ERROR("위 칸은 'U', 아래 칸은 'D'를 입력해주세요.");
+    INPUT_BRIDGE_NUMBER_ERROR("3 이상 20 이하의 숫자를 입력해주세요."),
+    INPUT_MOVE_BRIDGE_ERROR("위 칸은 'U', 아래 칸은 'D'를 입력해주세요."),
+    INPUT_RETRY_ERROR("재시작은 'R', 종료는 'Q'를 입력해주세요.");
 
 
     private String script;
@@ -13,10 +14,14 @@ public enum ErrorMessage {
     }
 
     public static void inputBridgeNumber() {
-        System.out.println(ERROR_MESSAGE.script + INPUT_BRIDGE_NUMBER.script);
+        System.out.println(ERROR_MESSAGE.script + INPUT_BRIDGE_NUMBER_ERROR.script);
     }
 
     public static void inputMoveBridgeError() {
         System.out.println(ERROR_MESSAGE.script + INPUT_MOVE_BRIDGE_ERROR.script);
+    }
+
+    public static void inputRetryError() {
+        System.out.println(ERROR_MESSAGE.script + INPUT_RETRY_ERROR.script);
     }
 }
