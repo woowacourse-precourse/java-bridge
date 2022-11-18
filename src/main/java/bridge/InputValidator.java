258eee824d2e8c199f4a14dff5d7b893dc5fc868
Sanmgmin <sangmins930@gmail.com>
2022-11-18 17:00:10 +0900

    feat(InputValidator): implement validateUpOrDown

--- src/main/java/bridge/InputValidator.java
+++ src/main/java/bridge/InputValidator.java
@@ -1,8 +1,10 @@
 package bridge;
 
 public class InputValidator {
-    private static String ERROR_FOR_LENGTH_OF_BRIDGE = "[ERROR] 다리의 길이는 3이상 20이하의 숫자만 가능합니다.";
-
+    private static final String ERROR_FOR_LENGTH_OF_BRIDGE = "[ERROR] 다리의 길이는 3이상 20이하의 숫자만 가능합니다.";
+    private static final String ERROR_FOR_UP_OR_DOWN = "[ERROR] 위: \"U\", 아래: \"D\"로만 가능합니다.";
+    private static final String UP = "U";
+    private static final String DOWN = "D";
     public static int validateNumeric(String input) {
         try {
             return Integer.parseInt(input);
@@ -14,6 +16,12 @@ public class InputValidator {
     public static void validateRange(int input) {
         if (input < 3 || input > 20) {
             throw new IllegalArgumentException(ERROR_FOR_LENGTH_OF_BRIDGE);
+        }
+    }
+
+    public static void validateUpOrDown(String input) {
+        if (!(input.equals(UP) || input.equals(DOWN))) {
+            throw new IllegalArgumentException(ERROR_FOR_UP_OR_DOWN);
         }
     }
 }
