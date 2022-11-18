1a51ea38d785442dcaf71a95afabaab5de72f8e1
Polite-Jehu <werber112@kyonggi.ac.kr>
2022-11-18 13:40:16 +0900

    feat(bridge): feat input bridge length function

--- src/main/java/bridge/InputView.java
+++ src/main/java/bridge/InputView.java
@@ -1,5 +1,9 @@
 package bridge;
 
+import camp.nextstep.edu.missionutils.Console;
+
+import java.util.List;
+
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
  */
@@ -9,7 +13,12 @@ public class InputView {
      * 다리의 길이를 입력받는다.
      */
     public int readBridgeSize() {
-        return 0;
+        String input = "";
+        while(!this.checkMessage(input,ErrorMessage.BRIDGELENGTHERROREXCEPTION).equals("PASS"))
+        {
+            input = Console.readLine();
+        }
+        return Integer.parseInt(input);
     }
 
     /**
@@ -26,4 +35,42 @@ public class InputView {
         return null;
     }
 
+    public String checkMessage(String value, ErrorMessage errorValue){
+        StringBuilder stringBuilder = new StringBuilder("[ERROR] ");
+        if(errorValue.equals(ErrorMessage.BRIDGELENGTHERROREXCEPTION))
+            errorValue = bridgeLengthChecker(value);
+        if(errorValue.equals(ErrorMessage.WRONGGAMEENDINPUTEXCEPTION))
+            errorValue = endGameChecker(value);
+        if(errorValue.equals(ErrorMessage.WRONGMOVEINPUTEXCEPTION))
+            errorValue = moveInputChecker(value);
+        if(!errorValue.equals(ErrorMessage.PASS))
+            return stringBuilder.append(errorValue.getErrorMessage()).toString();
+        return ErrorMessage.PASS.toString();
+    }
+
+    private ErrorMessage moveInputChecker(String value) {
+        if(value.equals("U")||value.equals("D"))
+            return ErrorMessage.PASS;
+        return ErrorMessage.WRONGMOVEINPUTEXCEPTION;
+    }
+
+    private ErrorMessage endGameChecker(String value) {
+        if(value.equals("R")||value.equals("Q"))
+            return ErrorMessage.PASS;
+        return ErrorMessage.WRONGGAMEENDINPUTEXCEPTION;
+    }
+
+    public ErrorMessage bridgeLengthChecker(String value)
+    {
+        try{
+            int length = Integer.parseInt(value);
+            if(length<3||length>20)
+                throw new NumberFormatException();
+        }
+        catch(NumberFormatException e)
+        {
+            return ErrorMessage.BRIDGELENGTHERROREXCEPTION;
+        }
+        return ErrorMessage.PASS;
+    }
 }
