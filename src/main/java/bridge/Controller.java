1b3dda8f20e78d590fcd83482f3304e8db7f78a0
renechoi90@gmail.com <renechoi90@gmail.com>
2022-11-18 11:50:39 +0900

    feat : controller 셋업 역할 위임 및 running 처리 기능 구현

--- src/main/java/bridge/Controller.java
+++ src/main/java/bridge/Controller.java
@@ -2,11 +2,19 @@ package bridge;
 
 public class Controller {
 
-	public void run() {
-		InputView inputView = new InputView();
+	private static final String EXIT_MESSAGE = "게임을 종료합니다.";
+	private static final String ENTER = "\n";
 
-		BridgeGame bridgeGame = new BridgeGame();
-		bridgeGame.play(inputView);
+	public void run() {
+		try {
+			InputView inputView = new InputView();
+			BridgeMaker bridgeMaker = new BridgeMaker(new BridgeRandomNumberGenerator());
+			BridgeGame bridgeGame = new BridgeGame();
+
+			bridgeGame.play(inputView, bridgeMaker);
+		} catch (IllegalArgumentException ignored) {
+		} finally {
+			OutputView.printGameInfo(ENTER + EXIT_MESSAGE);
+		}
 	}
 }
-
