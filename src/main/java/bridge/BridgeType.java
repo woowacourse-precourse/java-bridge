daf9671d454087ea82b4c54a549c2ca36f77b512
wormjoo <wormjoo@naver.com>
2022-11-19 02:39:11 +0900

    feat: 다리 enum 클래스 구현

--- src/main/java/bridge/BridgeType.java
+++ src/main/java/bridge/BridgeType.java
@@ -1,2 +1,33 @@
-package bridge;public enum BridgeType {
+package bridge;
+
+public enum BridgeType {
+    DOWN(0, "D"),
+    UP(1, "U");
+
+    private final int number;
+    private final String initial;
+
+    BridgeType(int number, String initial) {
+        this.number = number;
+        this.initial = initial;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public String getInitial() {
+        return initial;
+    }
+
+    public static BridgeType valueOf(int number) {
+        BridgeType bridge = null;
+        if (number == 0) {
+            bridge = DOWN;
+        }
+        if (number == 1) {
+            bridge = UP;
+        }
+        return bridge;
+    }
 }
