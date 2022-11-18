f7d91bfd1a00a5888bc384e491376fbc1b544fce
박승민 <tmddudals369@naver.com>
2022-11-19 00:55:13 +0900

    test: 다리 건너기 기능 테스트

---
+++ src/test/java/bridge/model/BridgeGameTest.java
@@ -0,0 +1,45 @@
+package bridge.model;
+
+import static org.assertj.core.api.Assertions.*;
+
+import java.util.List;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class BridgeGameTest {
+
+    private BridgeGame bridgeGame;
+    private List<String> bridge = List.of("U", "D", "D");
+
+    @BeforeEach
+    void beforeEach() {
+        bridgeGame = new BridgeGame();
+    }
+
+    @Test
+    void oneStepMoveCorrectBridge() {
+        boolean result = bridgeGame.move(bridge, "U");
+        assertThat(result).isTrue();
+    }
+
+    @Test
+    void MoveCorrectBridge() {
+        bridgeGame.move(bridge, "U");
+        bridgeGame.move(bridge, "D");
+        boolean result = bridgeGame.move(bridge, "D");
+        assertThat(result).isTrue();
+    }
+
+    @Test
+    void oneStepMoveInCorrectBridge() {
+        boolean result = bridgeGame.move(bridge, "D");
+        assertThat(result).isFalse();
+    }
+
+    @Test
+    void twoStepMoveInCorrectBridge() {
+        bridgeGame.move(bridge, "U");
+        boolean result = bridgeGame.move(bridge, "U");
+        assertThat(result).isFalse();
+    }
+}
