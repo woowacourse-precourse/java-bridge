7685c0116f23d6076942aa79b2ff237aaae21c41
박원종 <wonjong1030@naver.com>
2022-11-18 16:10:38 +0900

    test(BridgeGameTest): 입력받은 다리로 이동하는 기능 테스트

--- src/test/java/bridge/service/BridgeGameTest.java
+++ src/test/java/bridge/service/BridgeGameTest.java
@@ -1,4 +1,31 @@
+package bridge.service;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
+
 class BridgeGameTest {
-  
+    @Test
+    void moveTest_count3() {
+        BridgeGame bridgeGame = new BridgeGame(Arrays.asList("U","D","D"));
+        bridgeGame.move("U");
+        bridgeGame.move("D");
+        bridgeGame.move("D");
+        List<String>[] list = bridgeGame.getResult();
+        assertThat(list[0]).containsExactly("O", " ", " ");
+        assertThat(list[1]).containsExactly(" ", "O" ,"O");
+    }
+    @Test
+    void moveTest_count2() {
+        BridgeGame bridgeGame = new BridgeGame(Arrays.asList("U","D","D"));
+        bridgeGame.move("U");
+        bridgeGame.move("D");
+        List<String>[] list = bridgeGame.getResult();
+        assertThat(list[0]).containsExactly("O", " ");
+        assertThat(list[1]).containsExactly(" ", "O");
+    }
 }
