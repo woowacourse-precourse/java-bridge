846fb1ee5c65976d90586d34ad9ea107a71e94e3
Seiyon Park <seiyonpdev@gmail.com>
2022-11-19 00:50:48 +0900

    feat(Validation): 메소드 validateBridgeSizeOutOfRange 구현
    입력받은 다리 길이가 3~20 범위 밖인 경우 예외처리하는 메소드를 구현했다

--- src/main/java/bridge/Validation.java
+++ src/main/java/bridge/Validation.java
@@ -1,9 +1,21 @@
 package bridge;
 
 public class Validation {
+    static final int BRIDGE_SIZE_MINIMUM_INCLUSIVE = 3;
+    static final int BRIDGE_SIZE_MAXIMUM_INCLUSIVE = 20;
+
     private static void validateBridgeSizeIsNumber(String userInput) {
         if (userInput.matches("\\D+")) {
             System.out.println("[ERROR] 다리 길이는 숫자여야 합니다.");
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private static void validateBridgeSizeOutOfRange(String userInput) {
+        int bridgeSize = Integer.parseInt(userInput);
+
+        if (bridgeSize < BRIDGE_SIZE_MINIMUM_INCLUSIVE || bridgeSize > BRIDGE_SIZE_MAXIMUM_INCLUSIVE) {
+            System.out.println("[ERROR] 다리 길이는 3부터 20 사이의 숫자여야 합니다.");
             throw new IllegalArgumentException();
         }
     }
