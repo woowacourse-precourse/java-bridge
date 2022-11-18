6f9cd612da28b6baa52ec26f511b66848a94dc80
MJoon-Jung <gjgjajaj31@gmail.com>
2022-11-18 20:34:36 +0900

    test<Direction>: 0과 1이 아닐 경우 예외 반환 테스트 추가

--- src/test/java/bridge/model/DirectionTest.java
+++ src/test/java/bridge/model/DirectionTest.java
@@ -1,6 +1,7 @@
 package bridge.model;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import org.junit.jupiter.api.Test;
 
@@ -13,5 +14,13 @@ class DirectionTest {
     @Test
     void 랜덤한_숫자_0이_들어올_경우_D를_반환한다() {
         assertThat(Direction.findDirectionByRandomValue(0)).isEqualTo(Direction.D);
+    }
+
+    @Test
+    void 랜덤한_숫자_0과_1이_아닐경우_예외를_발생한다() {
+        assertThatThrownBy(() -> Direction.findDirectionByRandomValue(3))
+                .isInstanceOf(IllegalArgumentException.class);
+        assertThatThrownBy(() -> Direction.findDirectionByRandomValue(5))
+                .isInstanceOf(IllegalArgumentException.class);
     }
 }
