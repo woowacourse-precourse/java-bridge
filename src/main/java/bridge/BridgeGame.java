c07a9ad5d5f31487fd4edb30fa2e67aa405425a9
renechoi90@gmail.com <renechoi90@gmail.com>
2022-11-18 11:41:58 +0900

    fix : 건너기 기능 및 결과 처리 기능 구현

--- src/main/java/bridge/BridgeGame.java
+++ src/main/java/bridge/BridgeGame.java
@@ -12,11 +12,17 @@ public class BridgeGame {
 
 	private static final String GAME_START_INFO = "다리 건너기 게임을 시작합니다.";
 	private static final String REQUEST_BRIDGE_SIZE = "다리의 길이를 입력해주세요.";
+	private static final String REQUEST_MOVEMENT = "이동할 칸을 선택해주세요. (위: U, 아래: D)";
 	private static final String ENTER = "\n";
 
+
+	private List<String> bridgeNowCrossing;
+	private boolean passable;
 	public static List<String> upperBridge = new ArrayList<>();
 	public static List<String> downBridge = new ArrayList<>();
 	private ResultDescription resultDescription;
+
+
 
 	private void BridgeGame(List<String> bridge) {
 		this.resultDescription = resultDescription;
@@ -27,6 +33,46 @@ public class BridgeGame {
 
 		OutputView.printRequest(REQUEST_BRIDGE_SIZE);
 		Bridge bridge = new Bridge(inputView.readBridgeSize(), bridgeMaker);
+
+		crossingTrialStart(inputView, bridge);
 	}
 
+	private void crossingTrialStart(InputView inputView, Bridge bridge) {
+		bridgeNowCrossing = bridge.getBridgeToCross();
+		crossBridge(inputView);
+	}
+
+	private void crossBridge(InputView inputView) {
+		do {
+			OutputView.printRequest(REQUEST_MOVEMENT);
+			Movement movement = inputView.readMovement();
+			judgement(movement, bridgeNowCrossing);
+			passThrough();
+			resultDescription = ResultDescription.generatedBy(passable, movement);
+			OutputView.printGameInfo(resultDescription.getBridgeDescription() + ENTER);
+
+		} while (continueToPassThrough(resultDescription));
+	}
+
+	private void judgement(Movement movement, List<String> bridge) {
+		passable = false;
+		if (movement.side().equals(bridge.get(0))) {
+			passable = true;
+		}
+	}
+
+	private void passThrough() {
+		if (passable == true) {
+			bridgeNowCrossing.remove(0);
+		}
+	}
+
+	private boolean continueToPassThrough(ResultDescription resultDescription) {
+		return !(resultDescription.getBridgeDescription().contains("X") || completeCrossing());
+	}
+
+
+	private boolean completeCrossing() {
+		return (bridgeNowCrossing.isEmpty());
+	}
 }
