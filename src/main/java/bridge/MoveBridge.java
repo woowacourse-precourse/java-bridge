77a5979eb1f2caab2e5f0a146e5e9786486a3e65
dohyun <ldh19738@gmail.com>
2022-11-18 22:16:53 +0900

    feat(MoveBridge): 예외 메시지 추가

--- src/main/java/bridge/MoveBridge.java
+++ src/main/java/bridge/MoveBridge.java
@@ -4,11 +4,12 @@ import java.util.NoSuchElementException;
 import java.util.Objects;
 
 public class MoveBridge {
+    private static final String MOVE_BRIDGE_EXCEPTION_MESSAGE = "[ERROR] 위는 U, 아래는 D로 입력해야 합니다.";
     private final String moveBridgePhrase;
 
     public MoveBridge(String moveBridgePhrase) {
         if ((!Objects.equals(moveBridgePhrase, BridgeMaker.UP_BRIDGE_PHRASE)) && (!Objects.equals(moveBridgePhrase, BridgeMaker.DOWN_BRIDGE_PHRASE))) {
-            throw new NoSuchElementException();
+            throw new NoSuchElementException(MOVE_BRIDGE_EXCEPTION_MESSAGE);
         }
         this.moveBridgePhrase = moveBridgePhrase;
     }
