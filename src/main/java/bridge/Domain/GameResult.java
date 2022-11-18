c95194d1f3322d8fd2960b7c69eed1f7b67b1089
Park shinyeong <syngh503@gmail.com>
2022-11-18 15:23:03 +0900

    Feat(GameResult): 게임 결과를 저장하고 확인하는 GameResult 생성
    게임 결과를 저장하고 확인하는 GameResult 클래스를 생성하였습니다.
    사용자의 위치와 게임 성공 여부를 저장합니다.
    사용자의 위치가 U인지 확인하는 isPositionUpper() 메소드
    게임이 성공했는지 확인하는 isGameSuccess() 메소드를 생성하였습니다.

--- src/main/java/bridge/Domain/GameResult.java
+++ src/main/java/bridge/Domain/GameResult.java
@@ -1,4 +1,25 @@
 package bridge.Domain;
 
+import bridge.EnumCollections.BridgeResultType;
+import bridge.EnumCollections.BridgeType;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class GameResult {
+   private BridgeType position;
+   private BridgeResultType result;
+
+   public GameResult(BridgeType position, BridgeResultType result) {
+       this.position = position;
+       this.result = result;
+   }
+
+   public boolean isPositionUpper() {
+       return this.position == BridgeType.UP;
+   }
+
+   public boolean isGameSuccess() {
+       return this.result == BridgeResultType.POSSIBLE;
+   }
 }
