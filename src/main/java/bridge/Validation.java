c40dcd04f292499737ed0887bd382d858a78bc0f
wath1457 <wath1457@gmail.com>
2022-11-18 18:40:45 +0900

    feat(Validation): 이동 위치, 재시도 검증 로직 추가

--- src/main/java/bridge/Validation.java
+++ src/main/java/bridge/Validation.java
@@ -3,16 +3,28 @@ package bridge;
 import java.util.regex.Pattern;
 
 public class Validation {
-    public static void isPositiveInteger(String bridgeSize) {
+    private void isPositiveInteger(String bridgeSize) {
         String pattern = "^[^0]\\d*";
         if (!Pattern.matches(pattern, bridgeSize)) {
             throw new IllegalArgumentException("[ERROR} 다리 개수는 양의 정수이어야 합니다.");
         }
     }
 
-    public static void isInRange(int bridgeSize) {
+    private void isInRange(int bridgeSize) {
         if (!(3 <= bridgeSize && bridgeSize <= 20)) {
             throw new IllegalArgumentException("[ERROR] 다리 길이는 3부터 20 사이의 숫자여야 합니다.");
+        }
+    }
+
+    private void validateMoveTo(String position) {
+        if (!(position.equals("U") || position.equals("D"))) {
+            throw new IllegalArgumentException("[ERROR] : 이동할 칸은 위(U) 또는 아래(D) 이어야 합니다.");
+        }
+    }
+
+    private void validateContinue(String restartGame) {
+        if (!(restartGame.equals("Q") || restartGame.equals("R"))) {
+            throw new IllegalArgumentException("[ERROR] : 재시도 여부는 종료(Q) 또는 재시도(R) 이어야 합니다.");
         }
     }
 }
