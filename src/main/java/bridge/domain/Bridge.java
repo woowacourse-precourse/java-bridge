82a145a3510ea350395b79250771cc49ad806fcf
cocomozila <joseyun94@naver.com>
2022-11-19 02:40:32 +0900

    feat(Bridge): 게임 진행을 담당하는 Bridge객체 추가

---
+++ src/main/java/bridge/domain/Bridge.java
@@ -0,0 +1,55 @@
+package bridge.domain;
+
+import bridge.setting.Setting;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Bridge {
+
+    private List<String> bridge;
+    private List<String> userKeyList;
+
+    public Bridge(List<String> bridge) {
+        this.bridge = bridge;
+    }
+
+    // 유저가 입력할 키 리스트 초기화
+    public void initUserKeyList() {
+        userKeyList = new ArrayList<>();
+    }
+
+    // 움직인 키 입력받기
+    public void move(String key) {
+        userKeyList.add(key);
+    }
+
+    // Bridge 맵 반환
+    public String[] getBridgeMap() {
+        String[] bridgeMap = new String[userKeyList.size()];
+        for (int index = 0; index < userKeyList.size(); index++) {
+            String bridgeIndex = getBridge().get(index);
+            String keyIndex = getUserKeyList().get(index);
+            bridgeMap[index] = compareKey(bridgeIndex, keyIndex);
+        }
+        return bridgeMap;
+    }
+
+    // Bridge 와 key index 비교 후 O, X 반환
+    private String compareKey(String bridgeIndex, String keyIndex) {
+        if (bridgeIndex.equals(keyIndex)) {
+            return Setting.KEY_MATCH;
+        }
+        return Setting.KEY_NOT_MATCH;
+    }
+
+    // 만든 다리 반환
+    public List<String> getBridge() {
+        return this.bridge;
+    }
+
+    // 입력한 키 리스트 반환
+    public List<String> getUserKeyList() {
+        return this.userKeyList;
+    }
+}
