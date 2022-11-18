d426b1c53fe075cf02940b6761814df0bf5c4294
KimTaeKang57 <zizoen123@gmail.com>
2022-11-18 14:15:10 +0900

    refac: BridgeApplication 필드변수 설정 및 메소드 분리

--- src/main/java/bridge/BridgeApplication.java
+++ src/main/java/bridge/BridgeApplication.java
@@ -3,44 +3,83 @@ package bridge;
 import java.util.List;
 
 public class BridgeApplication {
+    private InputView inputView;
+    private BridgeRandomNumberGenerator bridgeRandomNumberGenerator;
+    private BridgeMaker bridgeMaker;
+    private OutputView outputView;
+    private BridgeGame bridgeGame;
+
+    private List<String> bridge;
+
     void run() {
-        // 다리 건너기 게임 시작
-        System.out.println("다리 건너기 게임을 시작합니다.");
+        System.out.println("다리 건너기 게임을 시작합니다."); // 다리 건너기 게임 시작
+        int size = inputBridgeSize(); // 다리의 길이를 입력
+        makeBridge(size); // 다리를 만듦
 
-        // 다리의 길이를 입력
-        System.out.println("다리의 길이를 입력해주세요.");
-        InputView inputView = new InputView();
-        int size = inputView.readBridgeSize();
+        outputView = new OutputView();
+        bridgeGame = new BridgeGame(bridge, 1);
 
-        // 다리를 만듦
-        BridgeRandomNumberGenerator bridgeRandomNumberGenerator = new BridgeRandomNumberGenerator();
-        BridgeMaker bridgeMaker = new BridgeMaker(bridgeRandomNumberGenerator);
-        List<String> bridge = bridgeMaker.makeBridge(size);
-        System.out.println(bridge.toString());
+        doBridgeGame(size); // 다리건너기 시작
+        printBridgeGameResult();
+    }
 
-        OutputView outputView = new OutputView();
-        BridgeGame bridgeGame = new BridgeGame(bridge, 1);
-
-        // 다리건너기 시작
-        for (int idx = 0; idx < size; idx++) {
-            System.out.println("이동할 칸을 선택해주세요. (위: U, 아래: D)");
-            String moving = inputView.readMoving();
-            bridgeGame.move(moving, idx);
-            if (bridgeGame.isMove()) {
-                outputView.printMap(bridgeGame);
-                continue;
-            }
-            outputView.printMap(bridgeGame);
-            if (!(bridgeGame.retry(inputView))) {
-                break;
-            }
-            idx = -1;
-            bridgeGame.plusCnt();
-        }
-
+    private void printBridgeGameResult() {
         System.out.println("최종 게임 결과");
-        outputView.printMap(bridgeGame);
-
+        printMap();
         outputView.printResult(bridgeGame);
+    }
+
+    private void doBridgeGame(int size) {
+        for (int idx = 0; idx < size; idx++) {
+            String moving = inputMoving();
+            bridgeGame.move(moving, idx);
+            if (isCorrectMove()) continue;
+            printMap();
+            if (isSelectRetry()) break;
+            idx = -1; // 반복문의 idx 를 -1로 돌려주어 다시 0부터 시작할 수 있게끔 함
+            plusGameCnt();
+        }
+    }
+
+    private void plusGameCnt() {
+        bridgeGame.plusCnt();
+    }
+
+    private boolean isSelectRetry() {
+        if (!(bridgeGame.retry(inputView))) {
+            return true;
+        }
+        return false;
+    }
+
+    private void printMap() {
+        outputView.printMap(bridgeGame);
+    }
+
+    private boolean isCorrectMove() {
+        if (bridgeGame.isMove()) {
+            printMap();
+            return true;
+        }
+        return false;
+    }
+
+    private String inputMoving() {
+        System.out.println("이동할 칸을 선택해주세요. (위: U, 아래: D)");
+        String moving = inputView.readMoving();
+        return moving;
+    }
+
+    private void makeBridge(int size) {
+        bridgeRandomNumberGenerator = new BridgeRandomNumberGenerator();
+        bridgeMaker = new BridgeMaker(bridgeRandomNumberGenerator);
+        bridge = bridgeMaker.makeBridge(size);
+    }
+
+    private int inputBridgeSize() {
+        System.out.println("다리의 길이를 입력해주세요.");
+        inputView = new InputView();
+        int size = inputView.readBridgeSize();
+        return size;
     }
 }
