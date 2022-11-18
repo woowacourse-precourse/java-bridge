ec58efffc2cffb2b2133b2e7bc0596adb5494fc1
김태용 <amico741@naver.com>
2022-11-18 19:27:50 +0900

    feat: Bridge 클래스 생성

--- src/main/java/bridge/domain/Bridge.java
+++ src/main/java/bridge/domain/Bridge.java
@@ -1,12 +1,13 @@
 package bridge.domain;
 
 
-import java.util.List;
+import java.util.LinkedList;
+import java.util.Queue;
 
 public class Bridge {
-    private final List<String> bridges;
+    private final Queue<String> directions;
 
-    public Bridge(List<String> bridges) {
-        this.bridges = bridges;
+    public Bridge(LinkedList<String> directions) {
+        this.directions = directions;
     }
 }
