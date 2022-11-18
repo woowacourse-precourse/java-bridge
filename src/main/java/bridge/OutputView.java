3d63593b1c9e9c4cfdbd3c69944f41fde1b7efca
K-IMjihun <62210749+K-IMjihun@users.noreply.github.com>
2022-11-18 20:58:36 +0900

    OutputView // 진행상황 및 결과 출력

--- src/main/java/bridge/OutputView.java
+++ src/main/java/bridge/OutputView.java
@@ -1,16 +1,47 @@
 package bridge;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * 사용자에게 게임 진행 상황과 결과를 출력하는 역할을 한다.
+ // * 제공된 OutputView 클래스를 활용해 구현해야 한다.
+ // * OutputView의 패키지는 변경할 수 있다.
+ // * OutputView의 메서드의 이름은 변경할 수 없고, 인자와 반환 타입은 필요에 따라 추가하거나 변경할 수 있다.
+ // * 값 출력을 위해 필요한 메서드를 추가할 수 있다.
  */
 public class OutputView {
+    static List<String> printMap1 = new ArrayList<>();
+    static List<String> printMap2 = new ArrayList<>();
 
     /**
      * 현재까지 이동한 다리의 상태를 정해진 형식에 맞춰 출력한다.
      * <p>
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void printMap() {
+    public void printMap(String bridge) {
+        if(bridge == "U"){
+             printMap1.add("O");
+             printMap2.add(" ");
+        }
+        if(bridge == "D"){
+            printMap1.add(" ");
+            printMap2.add("O");
+        }
+        System.out.println(printMap1);
+        System.out.println(printMap2);
+    }
+    public void wrongPrintMap(String bridge){
+        if(bridge == "U"){
+            printMap1.add(" ");
+            printMap2.add("X");
+        }
+        if(bridge == "D"){
+            printMap1.add("X");
+            printMap2.add(" ");
+        }
+        System.out.println(printMap1);
+        System.out.println(printMap2);
     }
 
     /**
@@ -18,6 +49,12 @@ public class OutputView {
      * <p>
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void printResult() {
+    public void printResult(String result, int count) {
+        System.out.println();
+        System.out.println("최종 게임 결과");
+        System.out.println(printMap1);
+        System.out.println(printMap2);
+        System.out.println("게임 성공 여부: " + result);
+        System.out.println("총 시도한 횟수: " + count);
     }
 }
