aa6a0ae3c3fc8fd9f55d4156a810d03cdd365a43
saint6839 <saint6839@gmail.com>
2022-11-18 22:48:30 +0900

    refactor(domain) : 사용자가 입력한 칸 이동 값을 검증하는 메서드 validator에서 MoveDirection 생성자로 이동

--- src/main/java/bridge/support/validator/InputValidator.java
+++ src/main/java/bridge/support/validator/InputValidator.java
@@ -2,9 +2,6 @@ package bridge.support.validator;
 
 public final class InputValidator {
     private static final String NOT_A_NUMBER_MESSAGE = "[ERROR] 숫자를 입력해주세요.";
-    private static final String INVALID_MOVING_SIGN_MESSAGE = "[ERROR] 이동할 칸은 U또는 D만 입력 가능합니다";
-    private static final String UP_SIGN = "U";
-    private static final String DOWN_SIGN = "D";
 
     private InputValidator() {}
 
@@ -14,12 +11,5 @@ public final class InputValidator {
         } catch (NumberFormatException e) {
             throw new IllegalArgumentException(NOT_A_NUMBER_MESSAGE);
         }
-    }
-
-    public static void validateMoving(String input) {
-        if (input.equals(UP_SIGN) || input.equals(DOWN_SIGN)) {
-            return;
-        }
-        throw new IllegalArgumentException(INVALID_MOVING_SIGN_MESSAGE);
     }
 }
