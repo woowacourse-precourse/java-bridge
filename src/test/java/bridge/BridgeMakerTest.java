87c01e4751bbacfb1ce633b4490c122221d60c9e
eun-byeol <62530884+eun-byeol@users.noreply.github.com>
2022-11-18 23:34:33 +0900

    test(BridgeMakerTest): 입력된 숫자만큼 다리 생성 테스트 코드 작성

--- src/test/java/bridge/BridgeMakerTest.java
+++ src/test/java/bridge/BridgeMakerTest.java
@@ -4,6 +4,7 @@ import bridge.domain.BridgeMaker;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -20,5 +21,14 @@ public class BridgeMakerTest {
     void makeBridgeMovingByNumber(int number, String expected) {
         bridgeMaker.addBridgeMoving(bridge, number);
         assertThat(bridge).contains(expected);
+    }
+
+    @ParameterizedTest
+    @DisplayName("다리 길이만큼 다리를 생성한다.")
+    @ValueSource(ints = {0, 1, 10, 50})
+    void makeBridgeAsIntegerSize(int size) {
+        assertThat(bridgeMaker.makeBridge(size))
+                .size()
+                .isEqualTo(size);
     }
 }
