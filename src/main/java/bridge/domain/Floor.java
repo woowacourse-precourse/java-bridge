a2b2d3af95cf65f6fcaedeedbba7dcd2942ad36d
0chil <0@chll.it>
2022-11-19 01:10:29 +0900

    feat(Floor): 이동 가능 여부를 알 수 있다

---
+++ src/main/java/bridge/domain/Floor.java
@@ -0,0 +1,14 @@
+package bridge.domain;
+
+public class Floor {
+
+    private final boolean isFirm;
+
+    public Floor(boolean isFirm) {
+        this.isFirm = isFirm;
+    }
+
+    public boolean isFirm() {
+        return isFirm;
+    }
+}
