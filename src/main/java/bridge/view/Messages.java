046f2dbbb40ed9d5c7bb961d804d0b9d5d99ffac
lee <whddlsk123@naver.com>
2022-11-18 20:17:58 +0900

    feat(messages) : 에러메시지에 [ERROR] 추가

--- src/main/java/bridge/view/Messages.java
+++ src/main/java/bridge/view/Messages.java
@@ -1,8 +1,8 @@
 package bridge.view;
 
 public enum Messages {
-    ERROR_INPUT_NUMBER("입력은 숫자만 가능합니다."),
-    ERROR_BRIDGE_SIZE("다리의 길이는 3 ~ 20까지만 입력 가능합니다.");
+    ERROR_INPUT_NUMBER("[ERROR] 입력은 숫자만 가능합니다."),
+    ERROR_BRIDGE_SIZE("[ERROR] 다리의 길이는 3 ~ 20까지만 입력 가능합니다.");
 
     private String message;
 
