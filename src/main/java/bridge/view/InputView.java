ac622fe0c7fd23a4ced808dbaaf417e121979a76
inhwanK <79566221+inhwanK@users.noreply.github.com>
2022-11-18 22:35:41 +0900

    feat(InputView): 생성할 다리 길이 입력 받기 구현
    validateBridgeSizeInputLength 메서드 추가
    validateSizeLimit 메서드 추가
    getBridgeSize 메서드 추가

--- src/main/java/bridge/view/InputView.java
+++ src/main/java/bridge/view/InputView.java
@@ -1,5 +1,7 @@
 package bridge.view;
 
+import camp.nextstep.edu.missionutils.Console;
+
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
  */
@@ -9,20 +11,47 @@ public class InputView {
      * 다리의 길이를 입력받는다.
      */
     public int readBridgeSize() {
-        return 0;
+        String input = Console.readLine();
+
+        validateBridgeSizeInputLength(input);
+        int bridgeSize = getBridgeSize(input);
+        validateSizeLimit(bridgeSize);
+        return bridgeSize;
+    }
+
+    private void validateBridgeSizeInputLength(String input) {
+        if(input.length() > 2) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private void validateSizeLimit(int bridgeSize) {
+        if(bridgeSize < 3 || bridgeSize > 20) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private int getBridgeSize(String input) {
+        int bridgeSize;
+        try {
+            bridgeSize = Integer.parseInt(input);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException();
+        }
+        return bridgeSize;
     }
 
     /**
      * 사용자가 이동할 칸을 입력받는다.
      */
     public String readMoving() {
-        return null;
+        return Console.readLine();
     }
 
     /**
      * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
      */
     public String readGameCommand() {
-        return null;
+        return Console.readLine();
     }
 }
