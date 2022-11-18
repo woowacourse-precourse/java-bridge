6696dc7cfd1ed8c9b21a7284e8d88bce96ec6ea5
asas6978 <asas6978@naver.com>
2022-11-19 02:17:06 +0900

    feat: 방향-숫자 간 맵핑 관계 생성

---
+++ src/main/java/bridge/util/Direction.java
@@ -0,0 +1,16 @@
+package bridge.util;
+
+public enum Direction {
+    DOWN(0),
+    UP(1);
+
+    private final int number;
+
+    Direction(int number) {
+        this.number = number;
+    }
+
+    public static boolean isEqual(int number, Direction direction) {
+        return number == direction.number;
+    }
+}
