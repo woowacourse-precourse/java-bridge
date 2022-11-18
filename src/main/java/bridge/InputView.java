aa6859f8a0354c895cff65bd497f5e174e7e7c85
JH-TT <leejongho9803@gmail.com>
2022-11-19 04:03:47 +0900

    feat(inputView) : 다리 길이를 입력받고 예외처리되면 다시 입력받도록 구현
    아직 에러 메시지는 상수화 안함. 후에 추가 예정.

--- src/main/java/bridge/InputView.java
+++ src/main/java/bridge/InputView.java
@@ -1,15 +1,33 @@
 package bridge;
 
+
+import camp.nextstep.edu.missionutils.Console;
+
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
  */
 public class InputView {
 
+//    private final Validate validate;
+//
+//    public InputView(Validate validate) {
+//        this.validate = validate;
+//    }
+
     /**
      * 다리의 길이를 입력받는다.
      */
     public int readBridgeSize() {
-        return 0;
+        while(true){
+            System.out.println("다리 길이를 입력해 주세요.");
+            String bridgeSize = Console.readLine();
+            try{
+                Validate.isInputBridgeSizeValid(bridgeSize);
+                return Integer.parseInt(bridgeSize);
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            }
+        }
     }
 
     /**
