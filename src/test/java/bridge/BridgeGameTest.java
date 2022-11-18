c9faac5f88ee874c8d2e28f095c404e1d3ca0e42
skck0226 <skck0226@ajou.ac.kr>
2022-11-18 17:10:08 +0900

    [test] (BridgeGameTest) 다리 move 한칸 테스트 수정

--- src/test/java/bridge/BridgeGameTest.java
+++ src/test/java/bridge/BridgeGameTest.java
@@ -1,9 +1,9 @@
 package bridge;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.MethodSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.List;
@@ -12,19 +12,32 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class BridgeGameTest {
 
-    BridgeGame bridgeGame = new BridgeGame(List.of("U", "D", "D", "D", "U", "D"));
+    BridgeGame bridgeGame;
+
+    @BeforeEach
+    void init() {
+        bridgeGame = new BridgeGame(List.of("U", "D", "D", "D", "U", "D"));
+    }
 
     @DisplayName("한칸 이동 성공 테스트")
-    @ParameterizedTest(name = "{index} {displayName} message={0}")
-    @ValueSource(strings = {"U","D","D","D","U","D"})
-    void 한칸이동_성공_테스트(String direction) {
-        assertTrue(bridgeGame.move(direction));
+    @Test
+    void 한칸이동_성공_테스트() {
+        assertTrue(bridgeGame.move("U"));
+        assertTrue(bridgeGame.move("D"));
+        assertTrue(bridgeGame.move("D"));
+        assertTrue(bridgeGame.move("D"));
+        assertTrue(bridgeGame.move("U"));
+        assertTrue(bridgeGame.move("D"));
     }
 
     @DisplayName("한칸 이동 실패 테스트")
-    @ParameterizedTest(name = "{index} {displayName} message={0}")
-    @ValueSource(strings = {"D","U","U","U","D","U"})
-    void 한칸이동_실패_테스트(String direction) {
-        assertFalse(bridgeGame.move(direction));
+    @Test
+    void 한칸이동_실패_테스트() {
+        assertFalse(bridgeGame.move("D"));
+        assertFalse(bridgeGame.move("U"));
+        assertFalse(bridgeGame.move("U"));
+        assertFalse(bridgeGame.move("U"));
+        assertFalse(bridgeGame.move("D"));
+        assertFalse(bridgeGame.move("U"));
     }
 }
