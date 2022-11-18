baed5366b676163771858cce90ca1da5ecf51274
nn98 <jkllhgb@gmail.com>
2022-11-18 14:38:22 +0900

    Feat: 유효성 검사 메소드 정의

---
+++ src/main/java/bridge/Checker.java
@@ -0,0 +1,16 @@
+package bridge;
+
+public class Checker {
+    public int checkValidate(String input) {
+        return 0;
+    }
+    public int checkNumeric(String input) {
+        return 0;
+    }
+    public int checkNegative(int input) {
+        return input;
+    }
+    public int checkBoundary(int input) {
+        return input;
+    }
+}
