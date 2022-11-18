4f9ff96391de53d919e6ef0413a0f6775790d9b1
rt3310 <btac3310@gmail.com>
2022-11-19 02:40:23 +0900

    feat: 이동할 칸을 입력받으면 이동 가능 여부를 판단하는 기능 구현

--- src/main/java/bridge/Bridge.java
+++ src/main/java/bridge/Bridge.java
@@ -1,2 +1,15 @@
-package bridge;public class Bridge {
+package bridge;
+
+import java.util.List;
+
+public class Bridge {
+    private final List<String> bridgeBlocks;
+
+    public Bridge(List<String> bridgeBlocks) {
+        this.bridgeBlocks = bridgeBlocks;
+    }
+
+    public boolean isPossibleToMove(String bridgePosition, int order) {
+        return bridgeBlocks.get(order).equals(bridgePosition);
+    }
 }
