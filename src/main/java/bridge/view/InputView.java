e439b30054d9ef6c837d39b3eaf3447d4c3d026f
JeonghakLee <wjdgkr0527@naver.com>
2022-11-18 13:31:39 +0900

    feat: 게임 재시작/종료 여부 입력 기능 구현

--- src/main/java/bridge/view/InputView.java
+++ src/main/java/bridge/view/InputView.java
@@ -3,17 +3,18 @@ package bridge.view;
 import static camp.nextstep.edu.missionutils.Console.readLine;
 
 import bridge.domain.BridgeCellType;
+import bridge.domain.GameCommand;
 
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
  */
 public class InputView {
+
     private static final String INPUT_BRIDGE_LENGTH_MESSAGE = "다리의 길이를 입력해주세요.";
     private static final String INPUT_MOVE_MESSAGE = "이동할 칸을 선택해주세요. (위: U, 아래: D)";
-
+    private static final String INPUT_COMMAND_MESSAGE = "게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)";
     private static final String NOT_INT_MESSAGE = "입력이 정수가 아닙니다. 정수를 입력해 주세요.";
 
-    private static final String INVALID_BRIDGE_CELL_TYPE_MESSAGE = "이동할 칸이 올바르지 않습니다. (U or D)";
 
     /**
      * 다리의 길이를 입력받는다.
@@ -35,23 +36,16 @@ public class InputView {
     /**
      * 사용자가 이동할 칸을 입력받는다.
      */
-    public String readMoving() {
+    public BridgeCellType readMoving() {
         System.out.println(INPUT_MOVE_MESSAGE);
-        String input = readLine();
-        validateMove(input);
-        return input;
-    }
-
-    private void validateMove(String input) {
-        if (BridgeCellType.isNotBridgeCellType(input)) {
-            throw new IllegalArgumentException(INVALID_BRIDGE_CELL_TYPE_MESSAGE);
-        }
+        return BridgeCellType.of(readLine());
     }
 
     /**
      * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
      */
-    public String readGameCommand() {
-        return null;
+    public GameCommand readGameCommand() {
+        System.out.println(INPUT_COMMAND_MESSAGE);
+        return GameCommand.of(readLine());
     }
 }
