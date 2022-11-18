624c8b725b3afa148f3aab6a096cbd1ef4c97d3a
ddubidubap <park62000@gmail.com>
2022-11-18 18:38:39 +0900

    feat: D/U를 0/1로 변환하기

--- src/main/java/bridge/BridgeGame.java
+++ src/main/java/bridge/BridgeGame.java
@@ -1,23 +1,27 @@
 package bridge;
 
-/**
- * 다리 건너기 게임을 관리하는 클래스
- */
-public class BridgeGame {
+import java.util.ArrayList;
+import java.util.List;
 
-    /**
-     * 사용자가 칸을 이동할 때 사용하는 메서드
-     * <p>
-     * 이동을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
-     */
-    public void move() {
+public class BridgeGame {
+    private final String CORRECT = "O";
+    private final String WRONG = "X";
+    private final String NOTHING = " ";
+    private final List<List<String>> bridgePattern = new ArrayList<>();
+    private final List<String> correctBridge;
+
+    public BridgeGame(List<String> correctBridge) {
+        this.correctBridge = correctBridge;
+        for (int i = 0; i < 2; i++)
+            this.bridgePattern.add(new ArrayList<>());
     }
 
-    /**
-     * 사용자가 게임을 다시 시도할 때 사용하는 메서드
-     * <p>
-     * 재시작을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
-     */
     public void retry() {
+    }
+
+    private int toMovingNumber(String moving) {
+        if (moving.equals("D"))
+            return 0;
+        return 1;
     }
 }
