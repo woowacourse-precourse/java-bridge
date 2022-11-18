562402cb69f80539e3d4f253ae3436f9396df719
hdsung98 <hdsung98@gmail.com>
2022-11-19 02:49:41 +0900

    test: BridgeGame, BridgeGameDto test 구현
    1. move, sendDto의 기능 테스트
    2. move의 예외 테스트(알파벳이 아니거나, 2개이상의 알파벳인 경우는 InputView에서 이미 검증했으므로 생략)

---
+++ src/test/java/bridge/BridgeGameTest.java
@@ -0,0 +1,35 @@
+package bridge;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.*;
+
+import dto.BridgeGameDto;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class BridgeGameTest {
+
+    public static final String VALID_DIRECTION = "유효한 방향은 U 또는 D 입니다.";
+
+    @Test
+    void move_기능_테스트() {
+        BridgeGame bridgeGame = new BridgeGame();
+        bridgeGame.move("U");
+        bridgeGame.move("D");
+        bridgeGame.move("D");
+        BridgeGameDto bridgeGameDto = bridgeGame.sendDto();
+        assertThat(bridgeGameDto.currentPosition).isEqualTo(2);
+        assertThat(bridgeGameDto.totalTrial).isEqualTo(3);
+    }
+
+    @Test
+    void move_예외_테스트() {
+        assertThatThrownBy(() -> {
+            BridgeGame bridgeGame = new BridgeGame();
+            bridgeGame.move("A");
+        }).isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(VALID_DIRECTION);
+    }
+
+}
