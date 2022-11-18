fd88cb74b487206f1b0c241e1a87521b0888e8b6
abbd2 <ajkety00@gmail.com>
2022-11-18 20:02:03 +0900

    Test(InputViewTest): isReadMoving(입력 값이 U 또는 D 인지 검사하는 메서드)의 테스트 추가

--- src/test/java/bridge/InputViewTest.java
+++ src/test/java/bridge/InputViewTest.java
@@ -8,11 +8,18 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class InputViewTest {
     
-    @DisplayName("다리의 길이로 지정이 가능한 숫자인지 확인하는 메서드")
+    @DisplayName("다리의 길이로 지정이 가능한 숫자인지 검사하는 메서드")
     @Test
     void isBridgeSizeNumber(){
         InputView inputView = new InputView();
         assertTrue(inputView.isBridgeSizeNumber("20"));
+    }
+    
+    @DisplayName("입력 값이 U 또는 D인지 검사하는 메서드")
+    @Test
+    void isReadMoving(){
+        InputView inputView = new InputView();
+        assertTrue(inputView.isReadMoving("U"));
     }
 
 }
