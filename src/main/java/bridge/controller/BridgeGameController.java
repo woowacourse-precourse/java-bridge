4d5d45012c392f9de78dcf1545e83fb76e6f743d
youngkyunIm <yee0230@naver.com>
2022-11-19 00:32:34 +0900

    feat(Controller): add make input Game moving, command

--- src/main/java/bridge/controller/BridgeGameController.java
+++ src/main/java/bridge/controller/BridgeGameController.java
@@ -5,6 +5,8 @@ import bridge.domain.BridgeMaker;
 import bridge.domain.BridgeRandomNumberGenerator;
 import bridge.domain.vo.BridgeMap;
 import bridge.domain.vo.BridgeSize;
+import bridge.domain.vo.GameCommand;
+import bridge.domain.vo.GameMoving;
 import bridge.view.InputView;
 import bridge.view.OutputView;
 import java.util.List;
@@ -23,22 +25,47 @@ public class BridgeGameController {
         inputView.printStartBridgeGame();
         BridgeSize bridgeSize = inputBridgeSize();
         BridgeMap bridgeMap = makeBridgeMap(bridgeSize);
+        GameCommand gameCommand = inputGameCommand();
     }
 
     private BridgeSize inputBridgeSize() {
+
         try {
-            return new BridgeSize(inputView.readBridgeSize());
+            BridgeSize bridgeSize = new BridgeSize(inputView.readBridgeSize());
+            return bridgeSize;
         } catch (IllegalArgumentException e) {
             System.out.println(e.getMessage());
-            inputBridgeSize();
         }
-        return null;
+        return inputBridgeSize();
     }
 
     private BridgeMap makeBridgeMap(BridgeSize bridgeSize) {
+        System.out.println(bridgeSize.getValue());
         BridgeRandomNumberGenerator bridgeRandomNumberGenerator = new BridgeRandomNumberGenerator();
         BridgeMaker bridgeMaker = new BridgeMaker(bridgeRandomNumberGenerator);
         List<String> test = bridgeMaker.makeBridge(bridgeSize.getValue());
+        System.out.println(test);
         return new BridgeMap(test);
+    }
+
+    private GameMoving inputMoving() {
+        try {
+            GameMoving gameMoving = new GameMoving(inputView.readMoving());
+            return gameMoving;
+        } catch (IllegalArgumentException illegalArgumentException) {
+            System.out.println(illegalArgumentException.getMessage());
+        }
+        return inputMoving();
+    }
+
+
+    private GameCommand inputGameCommand() {
+        try {
+            GameCommand gameCommand = new GameCommand(inputView.readGameCommand());
+            return gameCommand;
+        } catch (IllegalArgumentException illegalArgumentException) {
+            System.out.println(illegalArgumentException.getMessage());
+        }
+        return inputGameCommand();
     }
 }
