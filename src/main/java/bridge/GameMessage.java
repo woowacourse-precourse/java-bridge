92c9aa3f11305af45b6b4e45b3695de5f1b2c2ef
Amyhds <dasomhan524@gmail.com>
2022-11-18 13:31:26 +0900

    style(gamemessage): 상수 이름 변경

--- src/main/java/bridge/GameMessage.java
+++ src/main/java/bridge/GameMessage.java
@@ -2,7 +2,7 @@ package bridge;
 
 public enum GameMessage {
     START("다리 건너기 게임을 시작합니다."),
-    BRIDGE_LENGTH("다리의 길이를 입력해 주세요."),
+    BRIDGE_SIZE("다리의 길이를 입력해 주세요."),
     MOVING("이동할 칸을 선택해주세요. (위: U, 아래: D)"),
     REGAME_OR_END("게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)"),
     GAME_RESULT("최종 게임 결과"),
