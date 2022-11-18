860060916a2d3cd502901dbb1a304c85aa746dfe
newh08 <newh08@gmail.com>
2022-11-18 20:58:04 +0900

    test: update InputViewTest due to update InputView class
    Do-while loop was added on InputView class.
    extends NsTest to test the Do-while loop.

--- src/test/java/bridge/view/InputViewTest.java
+++ src/test/java/bridge/view/InputViewTest.java
@@ -1,16 +1,20 @@
 package bridge.view;
 
+import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
 
 import java.io.ByteArrayInputStream;
 import java.io.InputStream;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import camp.nextstep.edu.missionutils.test.NsTest;
 
-class InputViewTest {
-
+class InputViewTest extends NsTest {
+    private static final String ERROR_MESSAGE = "[ERROR]";
+    private static final int READ_SIZE_TEST = 0;
+    private static final int READ_MOVE_TEST = 1;
+    private static final int READ_COMMAND_TEST = 2;
     static InputView inputView;
 
     @BeforeAll
@@ -36,40 +40,45 @@ class InputViewTest {
         assertThat(inputView.readBridgeSize()).isEqualTo(20);
     }
 
-    @DisplayName("bridge 크기로 2를 (3 미만) 넣으면 IllegalArgumentException 가 발생한다.")
+    @DisplayName("bridge 크기로 2를 (3 미만) 넣으면 에러 메시지를 출력한다.")
     @Test
     void readBridgeSizeErrorTest1() {
-        String input = "2";
-        InputStream in = new ByteArrayInputStream(input.getBytes());
-        System.setIn(in);
-        assertThatIllegalArgumentException().isThrownBy(() -> inputView.readBridgeSize());
+        assertSimpleTest(() -> {
+            run(READ_SIZE_TEST, "2", "5");
+            String s = output();
+            assertThat(s).contains(ERROR_MESSAGE);
+        });
     }
 
-    @DisplayName("bridge 크기로 21을 (20 초과) 넣으면 IllegalArgumentException 가 발생한다.")
+    @DisplayName("bridge 크기로 21을 (20 초과) 넣으면 에러 메시지를 출력한다.")
     @Test
     void readBridgeSizeErrorTest2() {
-        String input = "21";
-        InputStream in = new ByteArrayInputStream(input.getBytes());
-        System.setIn(in);
-        assertThatIllegalArgumentException().isThrownBy(() -> inputView.readBridgeSize());
+        assertSimpleTest(() -> {
+            run(READ_SIZE_TEST, "21", "5");
+            String s = output();
+            assertThat(s).contains(ERROR_MESSAGE);
+        });
+
     }
 
-    @DisplayName("bridge 크기로 숫자 이외의 것을 넣으면 IllegalArgumentException 가 발생한다.")
+    @DisplayName("bridge 크기로 숫자 이외의 것을 넣으면 에러 메시지를 출력한다.")
     @Test
     void readBridgeSizeErrorTest3() {
-        String input = "s";
-        InputStream in = new ByteArrayInputStream(input.getBytes());
-        System.setIn(in);
-        assertThatIllegalArgumentException().isThrownBy(() -> inputView.readBridgeSize());
+        assertSimpleTest(() -> {
+            run(READ_SIZE_TEST, "s", "5");
+            String s = output();
+            assertThat(s).contains(ERROR_MESSAGE);
+        });
     }
 
-    @DisplayName("bridge 크기로 숫자 이외의 것을 넣으면 IllegalArgumentException 가 발생한다.")
+    @DisplayName("bridge 크기로 숫자 이외의 것을 넣으면 에러 메시지를 출력한다.")
     @Test
     void readBridgeSizeErrorTest4() {
-        String input = " ";
-        InputStream in = new ByteArrayInputStream(input.getBytes());
-        System.setIn(in);
-        assertThatIllegalArgumentException().isThrownBy(() -> inputView.readBridgeSize());
+        assertSimpleTest(() -> {
+            run(READ_SIZE_TEST, " ", "5");
+            String s = output();
+            assertThat(s).contains(ERROR_MESSAGE);
+        });
     }
 
     @DisplayName("이동 위치로 'U' 을 입력하면 readMoving 메서드는 'U'를 리턴한다.")
@@ -90,31 +99,34 @@ class InputViewTest {
         assertThat(inputView.readMoving()).isEqualTo(input);
     }
 
-    @DisplayName("이동 위치로 'U', 'D' 이외의 것을 넣으면 IllegalArgumentException 가 발생한다.")
+    @DisplayName("이동 위치로 'U', 'D' 이외의 것을 넣으면 에러 메시지를 출력한다.")
     @Test
     void readMovingErrorTest1() {
-        String input = "e";
-        InputStream in = new ByteArrayInputStream(input.getBytes());
-        System.setIn(in);
-        assertThatIllegalArgumentException().isThrownBy(() ->inputView.readMoving());
+        assertSimpleTest(() -> {
+            run(READ_MOVE_TEST, "e", "U");
+            String s = output();
+            assertThat(s).contains(ERROR_MESSAGE);
+        });
     }
 
-    @DisplayName("이동 위치로 'U', 'D' 이외의 것을 넣으면 IllegalArgumentException 가 발생한다.")
+    @DisplayName("이동 위치로 'U', 'D' 이외의 것을 넣으면 에러 메시지를 출력한다.")
     @Test
     void readMovingErrorTest2() {
-        String input = "UD";
-        InputStream in = new ByteArrayInputStream(input.getBytes());
-        System.setIn(in);
-        assertThatIllegalArgumentException().isThrownBy(() ->inputView.readMoving());
+        assertSimpleTest(() -> {
+            run(READ_MOVE_TEST, "UD", "U");
+            String s = output();
+            assertThat(s).contains(ERROR_MESSAGE);
+        });
     }
 
-    @DisplayName("이동 위치로 'U', 'D' 이외의 것을 넣으면 IllegalArgumentException 가 발생한다.")
+    @DisplayName("이동 위치로 'U', 'D' 이외의 것을 넣으면 에러 메시지를 출력한다.")
     @Test
     void readMovingErrorTest3() {
-        String input = " ";
-        InputStream in = new ByteArrayInputStream(input.getBytes());
-        System.setIn(in);
-        assertThatIllegalArgumentException().isThrownBy(() ->inputView.readMoving());
+        assertSimpleTest(() -> {
+            run(READ_MOVE_TEST, " ", "U");
+            String s = output();
+            assertThat(s).contains(ERROR_MESSAGE);
+        });
     }
 
     @DisplayName("게임 커멘드로 'R' 을 입력하면 readMoving 메서드는 'R'을 리턴한다.")
@@ -135,30 +147,59 @@ class InputViewTest {
         assertThat(inputView.readGameCommand()).isEqualTo(input);
     }
 
-    @DisplayName("게임 커멘드로 'R', 'Q' 이외의 것을 넣으면 IllegalArgumentException 가 발생한다.")
+    @DisplayName("게임 커멘드로 'R', 'Q' 이외의 것을 넣으면 에러 메시지를 출력한다.")
     @Test
     void readGameCommandErrorTest1() {
-        String input = "s";
-        InputStream in = new ByteArrayInputStream(input.getBytes());
-        System.setIn(in);
-        assertThatIllegalArgumentException().isThrownBy(() ->inputView.readGameCommand());
+        assertSimpleTest(() -> {
+            run(READ_COMMAND_TEST, "r", "R");
+            String s = output();
+            assertThat(s).contains(ERROR_MESSAGE);
+        });
     }
 
-    @DisplayName("게임 커멘드로 'R', 'Q' 이외의 것을 넣으면 IllegalArgumentException 가 발생한다.")
+    @DisplayName("게임 커멘드로 'R', 'Q' 이외의 것을 넣으면 에러 메시지를 출력한다.")
     @Test
     void readGameCommandErrorTest2() {
-        String input = "RQ";
-        InputStream in = new ByteArrayInputStream(input.getBytes());
-        System.setIn(in);
-        assertThatIllegalArgumentException().isThrownBy(() ->inputView.readGameCommand());
+        assertSimpleTest(() -> {
+            run(READ_COMMAND_TEST, "RQ", "R");
+            String s = output();
+            assertThat(s).contains(ERROR_MESSAGE);
+        });
     }
 
-    @DisplayName("게임 커멘드로 'R', 'Q' 이외의 것을 넣으면 IllegalArgumentException 가 발생한다.")
+    @DisplayName("게임 커멘드로 'R', 'Q' 이외의 것을 넣으면 에러 메시지를 출력한다.")
     @Test
     void readGameCommandErrorTest3() {
-        String input = "1";
-        InputStream in = new ByteArrayInputStream(input.getBytes());
-        System.setIn(in);
-        assertThatIllegalArgumentException().isThrownBy(() ->inputView.readGameCommand());
+        assertSimpleTest(() -> {
+            run(READ_COMMAND_TEST, "1", "R");
+            String s = output();
+            assertThat(s).contains(ERROR_MESSAGE);
+        });
+    }
+
+    @Override
+    protected void runMain() {
+    }
+
+    protected void runMain(int test) {
+        if (test == READ_SIZE_TEST) {
+            inputView.readBridgeSize();
+        }
+        if (test == READ_MOVE_TEST) {
+            inputView.readMoving();
+        }
+        if (test == READ_COMMAND_TEST) {
+            inputView.readGameCommand();
+        }
+    }
+
+    protected void run(int test, final String... args) {
+        command(args);
+        runMain(test);
+    }
+
+    private void command(final String... args) {
+        final byte[] buf = String.join("\n", args).getBytes();
+        System.setIn(new ByteArrayInputStream(buf));
     }
 }
