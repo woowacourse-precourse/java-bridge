2597cd3a36198b37dc9f6ed4b0abb00760654715
ouohoon <begreat@kakao.com>
2022-11-18 11:54:44 +0900

    test(InputView): 다리 길이 검증 테스트에서 정상값 테스트 추가

--- src/test/java/bridge/InputViewTest.java
+++ src/test/java/bridge/InputViewTest.java
@@ -1,5 +1,6 @@
 package bridge;
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
@@ -7,15 +8,25 @@ import org.junit.jupiter.params.provider.ValueSource;
 
 import java.io.ByteArrayInputStream;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
 
 
 class InputViewTest {
 
     @ParameterizedTest
+    @ValueSource(strings = {"3", "20"})
+    @DisplayName("다리 길이가 3 이상 20 이하 숫자라면 숫자를 리턴한다.")
+    void readBridgeSize_정상_입력(String input) {
+        System.setIn(new ByteArrayInputStream(input.getBytes()));
+        InputView inputView = new InputView();
+        assertThat(inputView.readBridgeSize())
+                .isEqualTo(Integer.parseInt(input));
+    }
+    @ParameterizedTest
     @ValueSource(strings = {"20a", " ", "a"})
     @DisplayName("다리 길이를 숫자로 입력받지 않으면 예외가 발생한다.")
-    void readBridgeSize_문자_입력(String input) {
+    void readBridgeSize_예외_문자_입력(String input) {
         System.setIn(new ByteArrayInputStream(input.getBytes()));
         InputView inputView = new InputView();
         assertThatIllegalArgumentException()
@@ -25,19 +36,20 @@ class InputViewTest {
     @ParameterizedTest
     @ValueSource(strings = {"21", "2"})
     @DisplayName("다리 길이가 3 이상 20 이하가 아니라면 예외가 발생한다.")
-    void readBridgeSize_숫자_범위_오류(String input) {
+    void readBridgeSize_예외_숫자_범위(String input) {
         System.setIn(new ByteArrayInputStream(input.getBytes()));
         InputView inputView = new InputView();
         assertThatIllegalArgumentException()
                 .isThrownBy(inputView::readBridgeSize);
     }
 
-    @Test
+    @ParameterizedTest
+    @ValueSource(strings = {})
     void readMoving() {
-
     }
 
-    @Test
+    @ParameterizedTest
+    @ValueSource(strings = {})
     void readGameCommand() {
     }
 }
