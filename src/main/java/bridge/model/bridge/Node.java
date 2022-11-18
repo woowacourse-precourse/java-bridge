0da48dfa6cb03dd57239fa2a4566521394918e51
Kayoung <rkdkdudjd@gmail.com>
2022-11-18 16:14:40 +0900

    feat(Node): 클래스 정의
    Node는 다리 한 칸을 의미합니다

--- src/main/java/bridge/model/bridge/Node.java
+++ src/main/java/bridge/model/bridge/Node.java
@@ -1,4 +1,28 @@
 package bridge.model.bridge;
 
 public enum Node {
+    UP("U"),
+    DOWN("D");
+
+    private static final int UP_POSITION = 1;
+    private static final int DOWN_POSITION = 0;
+
+    private static final String INVALID_POSITION_FOR_BRIDGE_NODE =
+            "다리의 위치는" + UP.position + " 또는 " + DOWN.position + "이어야 합니다.";
+
+    String position;
+
+    Node(String upOrDown) {
+        position = upOrDown;
+    }
+
+    public Node of(int position) {
+        if (position == UP_POSITION) {
+            return UP;
+        }
+        if (position == DOWN_POSITION) {
+            return DOWN;
+        }
+        throw new IllegalArgumentException(INVALID_POSITION_FOR_BRIDGE_NODE);
+    }
 }
