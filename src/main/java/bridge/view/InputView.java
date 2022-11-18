e1b061fd5bdd630ff1a73a24e421f59002335c5f
BioBeBe <76714219+JaegeonYu@users.noreply.github.com>
2022-11-18 13:47:36 +0900

    feat: readMoving 메소드 작성

--- src/main/java/bridge/view/InputView.java
+++ src/main/java/bridge/view/InputView.java
@@ -43,7 +43,8 @@ public class InputView {
      * 사용자가 이동할 칸을 입력받는다.
      */
     public String readMoving() {
-        return null;
+        String movingInput = Console.readLine();
+        return movingInput;
     }
 
     /**
