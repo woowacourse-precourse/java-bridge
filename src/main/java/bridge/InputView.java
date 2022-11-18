bcb49b3d1e9d86d8641472a6abb42423d82356aa
kissshot1104 <kissshot.heartunderblade1104@gmail.com>
2022-11-18 11:33:16 +0900

    Feat(InputView) : 움직임을 입력받는 기능 추가
    public String readMoveing() 메소드를 추가했습니다.
    이 메소드는 사용자로부터 다음의 움직임을 입력받습니다.

--- src/main/java/bridge/InputView.java
+++ src/main/java/bridge/InputView.java
@@ -1,5 +1,7 @@
 package bridge;
 
+import camp.nextstep.edu.missionutils.Console;
+
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
  */
@@ -9,14 +11,29 @@ public class InputView {
      * 다리의 길이를 입력받는다.
      */
     public int readBridgeSize() {
-        return 0;
+        int bridgeSize = 0;
+        try {
+            String input = Console.readLine();
+            bridgeSize = Util.stringToInteger(input);
+        } catch(IllegalArgumentException iae){
+            String errorMessage = iae.getMessage();
+            System.out.println(errorMessage);
+        }
+        return bridgeSize;
     }
 
     /**
      * 사용자가 이동할 칸을 입력받는다.
      */
     public String readMoving() {
-        return null;
+        String input = "";
+        try{
+            input = Console.readLine();
+            ExceptionBasket.invalidGameKey(input);
+        } catch (IllegalArgumentException iae){
+            System.out.println(iae);
+        }
+        return input;
     }
 
     /**
