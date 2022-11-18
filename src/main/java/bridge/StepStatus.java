5d2581eafa065ea67283ea503d3feb4be67520ca
minaver <psmjoshua@gmail.com>
2022-11-18 10:44:29 +0900

    feat(StepStatus): StepStatus VO 생성

---
+++ src/main/java/bridge/StepStatus.java
@@ -0,0 +1,19 @@
+package bridge;
+
+public class StepStatus {
+    private UpDown upDown;
+    private boolean isCorrect;
+
+    public StepStatus(UpDown upDown, boolean isCorrect) {
+        this.upDown = upDown;
+        this.isCorrect = isCorrect;
+    }
+
+    public UpDown getUpDown() {
+        return upDown;
+    }
+
+    public boolean isCorrect() {
+        return isCorrect;
+    }
+}
