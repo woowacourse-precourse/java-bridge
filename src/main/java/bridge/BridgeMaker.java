e2e751ed4a9f48f04051051e0bd38c4590f958be
Devtist <yt930310@snu.ac.kr>
2022-11-18 16:17:17 +0900

    feat: 다리를 생성하는 기능 구현

--- src/main/java/bridge/BridgeMaker.java
+++ src/main/java/bridge/BridgeMaker.java
@@ -1,23 +1,40 @@
 package bridge;
 
+import java.util.ArrayList;
 import java.util.List;
+
+import util.Validator;
 
 /**
  * 다리의 길이를 입력 받아서 다리를 생성해주는 역할을 한다.
  */
 public class BridgeMaker {
+	private final BridgeNumberGenerator bridgeNumberGenerator;
 
-    private final BridgeNumberGenerator bridgeNumberGenerator;
+	public BridgeMaker(BridgeNumberGenerator bridgeNumberGenerator) {
+		this.bridgeNumberGenerator = bridgeNumberGenerator;
+	}
 
-    public BridgeMaker(BridgeNumberGenerator bridgeNumberGenerator) {
-        this.bridgeNumberGenerator = bridgeNumberGenerator;
-    }
+	/**
+	 * @param size 다리의 길이
+	 * @return 입력받은 길이에 해당하는 다리 모양. 위 칸이면 "U", 아래 칸이면 "D"로 표현해야 한다.
+	 */
+	public List<String> makeBridge(int size) {
+		List<String> bridge = new ArrayList<>();
+		for (int index = 0; index < size; index++) {
+			addUpperOrLowerSpace(bridge);
+		}
+		Validator.validateBridgeSize(bridge, size);
+		return bridge;
+	}
 
-    /**
-     * @param size 다리의 길이
-     * @return 입력받은 길이에 해당하는 다리 모양. 위 칸이면 "U", 아래 칸이면 "D"로 표현해야 한다.
-     */
-    public List<String> makeBridge(int size) {
-        return null;
-    }
+	private void addUpperOrLowerSpace(List<String> bridgeSpaces) {
+		int bridgeNumber = bridgeNumberGenerator.generate();
+		if (bridgeNumber == 0) {
+			bridgeSpaces.add("D");
+		}
+		if (bridgeNumber == 1) {
+			bridgeSpaces.add("U");
+		}
+	}
 }
