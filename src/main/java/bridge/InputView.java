a6b45c272cb7a2f59004e25d379f0a24adc978c0
MIN-04 <kimji_0424@naver.com>
2022-11-18 14:17:20 +0900

    feat(InputView.java): add method logic readBridgeSize
    Added method logic readBridgeSize() :
    - 다리 길이 입력 받는 로직
    - 예외 처리
    - TODO

--- src/main/java/bridge/InputView.java
+++ src/main/java/bridge/InputView.java
@@ -1,5 +1,7 @@
 package bridge;
 
+import camp.nextstep.edu.missionutils.Console;
+
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
  */
@@ -9,7 +11,10 @@ public class InputView {
      * 다리의 길이를 입력받는다.
      */
     public int readBridgeSize() {
-        return 0;
+        System.out.println("다리의 길이를 입력해주세요.");
+        String bridgeSizeInput = Console.readLine();
+        // TODO : 예외 발생 시 다시 받기
+        return validateBridgeSize(bridgeSizeInput);
     }
 
     /**
@@ -24,5 +29,15 @@ public class InputView {
      */
     public String readGameCommand() {
         return null;
+    }
+
+    private int validateBridgeSize(String bridgeSizeInput) {
+        if (!bridgeSizeInput.matches(RegValue.ONLY_NUMBER.getReg())) {
+            throw new IllegalArgumentException("[ERROR] 숫자만 입력 가능합니다.");
+        }
+        if (Integer.parseInt(bridgeSizeInput) < 3 || Integer.parseInt(bridgeSizeInput) > 20) {
+            throw new IllegalArgumentException("[ERROR] 3 이상 20 이하의 숫자만 입력 가능합니다.");
+        }
+        return Integer.parseInt(bridgeSizeInput);
     }
 }
