fcf73b736605bfe3fb9a8a137a2c3c40cd4f2485
saint6839 <saint6839@gmail.com>
2022-11-18 13:50:01 +0900

    test(support) : 사용자가 숫자로 변환이 불가능한 문자열을 입력했을때 예외를 던지는 테스트 작성

---
+++ src/test/java/bridge/support/validator/InputValidatorTest.java
@@ -0,0 +1,23 @@
+package bridge.support.validator;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+
+class InputValidatorTest {
+
+    @DisplayName("사용자가 숫자로 변환이 불가능한 문자열을 입력하였을 때, 예외를 던지는지 테스트")
+    @Test
+    void validateParseStringToInteger() {
+        //given
+        String input = "삼";
+        //when
+        //then
+        Assertions.assertThatThrownBy(() -> {
+                      InputValidator.validateParseStringToInteger(input);
+                  })
+                  .isInstanceOf(IllegalArgumentException.class)
+                  .hasMessageContaining("[ERROR] 숫자를 입력해주세요.");
+    }
+}
