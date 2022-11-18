c5bc595968d7f42d4b9e49b09dce0512d54d9dc9
eugene1204 <s1061001@naver.com>
2022-11-18 18:55:38 +0900

    feat : InputView 클래스 작성

--- src/main/java/bridge/InputView.java
+++ src/main/java/bridge/InputView.java
@@ -1,5 +1,5 @@
 package bridge;
-
+import camp.nextstep.edu.missionutils.Console;
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
  */
@@ -9,20 +9,23 @@ public class InputView {
      * 다리의 길이를 입력받는다.
      */
     public int readBridgeSize() {
-        return 0;
+        String inputBridgeSize = Console.readLine();
+        return Integer.valueOf(inputBridgeSize);
     }
 
     /**
      * 사용자가 이동할 칸을 입력받는다.
      */
     public String readMoving() {
-        return null;
+        String inputMoving = Console.readLine();
+        return inputMoving;
     }
 
     /**
      * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
      */
     public String readGameCommand() {
-        return null;
+        String inputGameCommand = Console.readLine();
+        return inputGameCommand;
     }
 }
