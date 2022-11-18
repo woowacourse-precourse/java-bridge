245dc3a03cdd4751381664a7b3dfc0c567ee2fce
ghchoi <ghchoi0427@gmail.com>
2022-11-18 14:01:58 +0900

    test: BridgeMaker 테스트

---
+++ src/test/java/bridge/BridgeMakerTest.java
@@ -0,0 +1,21 @@
+package bridge;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+class BridgeMakerTest {
+
+    @Test
+    void testMakeBridge() {
+        BridgeMaker maker = new BridgeMaker(new BridgeRandomNumberGenerator());
+        int length = 8;
+        List<String> bridge = maker.makeBridge(length);
+
+        Assertions.assertThat(bridge).isNotNull();
+        Assertions.assertThat(bridge.size()).isEqualTo(length);
+        Assertions.assertThat(bridge).containsAnyOf("U", "D");
+    }
+
+}
