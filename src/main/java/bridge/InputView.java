baf91c550b5edce7b023ef6f50008aaafb7fc90d
nn98 <jkllhgb@gmail.com>
2022-11-18 14:45:08 +0900

    Feat: 다리 길이 입력 및 유효성 검사 구현

--- src/main/java/bridge/InputView.java
+++ src/main/java/bridge/InputView.java
@@ -1,5 +1,7 @@
 package bridge;
 
+import camp.nextstep.edu.missionutils.Console;
+
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
  */
@@ -11,7 +13,8 @@ public class InputView {
      * 다리의 길이를 입력받는다.
      */
     public int readBridgeSize() {
-        return checker.checkValidate("");
+        String bridgeSize= Console.readLine();
+        return checker.checkValidate(bridgeSize);
     }
 
     /**
