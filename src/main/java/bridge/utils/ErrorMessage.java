69e9f508f42e8819d91406597129064cf1cd2cbd
eun-byeol <62530884+eun-byeol@users.noreply.github.com>
2022-11-18 16:49:53 +0900

    feat(ErrorMessage): 예외 처리 메시지 정의

---
+++ src/main/java/bridge/utils/ErrorMessage.java
@@ -0,0 +1,12 @@
+package bridge.utils;
+
+public class ErrorMessage {
+    private static final String ERROR_TAG = "[ERROR] ";
+
+    public static final String BRIDGE_SIZE_NOT_INTEGER = ERROR_TAG + "다리 길이는 숫자만 가능합니다.";
+    public static final String BRIDGE_SIZE_OUT_OF_RANGE = ERROR_TAG + "다리 길이는 3부터 20 사이의 숫자여야 합니다.";
+
+    public static final String MOVING_NOT_OPTIONS = ERROR_TAG + "U(위 칸)와 D(아래 칸) 중 하나의 문자여야 합니다.";
+
+    public static final String GAME_COMMAND_NOT_OPTIONS = ERROR_TAG + "R(재시작)과 Q(종료) 중 하나의 문자여야 합니다.";
+}
