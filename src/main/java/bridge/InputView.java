79c5f43368f60cad24dd39a4d9ef97d104bbab8e
0112leesy <0112leesy@naver.com>
2022-11-18 16:41:13 +0900

    feat(InputView) : 입력 안내 메시지 추가
    안내 메시지 추후 상수로 선언 예정

--- src/main/java/bridge/InputView.java
+++ src/main/java/bridge/InputView.java
@@ -11,6 +11,7 @@ public class InputView {
      * 다리의 길이를 입력받는다.
      */
     public int readBridgeSize() throws IllegalArgumentException {
+        System.out.println("다리의 길이를 입력해주세요.");
         String input = readLine();
         if (!input.matches("^[0-9]*$")) {
             throw new IllegalArgumentException("[ERROR] <임시 에러 메시지>");
@@ -23,6 +24,7 @@ public class InputView {
      * 사용자가 이동할 칸을 입력받는다.
      */
     public String readMoving() throws IllegalArgumentException {
+        System.out.println("이동할 칸을 선택해주세요. (위: U, 아래: D)");
         String input = readLine();
         if (!input.equals("U") && !input.equals("D")) {
             throw new IllegalArgumentException("[ERROR] <임시 에러 메시지>");
@@ -34,6 +36,7 @@ public class InputView {
      * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
      */
     public String readGameCommand() throws IllegalArgumentException {
+        System.out.println("게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)");
         String input = readLine();
         if (!input.equals("R") && !input.equals("Q")) {
             throw new IllegalArgumentException("[ERROR] <임시 에러 메시지>");
