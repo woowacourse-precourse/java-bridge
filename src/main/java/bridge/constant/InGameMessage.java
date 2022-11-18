20cde402438c356550d65fe3d933afbd75e62aa6
yubin <dbqls2821@gmail.com>
2022-11-18 23:36:02 +0900

    feat(InGameMessage): 게임 내 메시지 enum 상수화

---
+++ src/main/java/bridge/constant/InGameMessage.java
@@ -0,0 +1,26 @@
+package bridge.constant;
+
+public enum InGameMessage {
+
+  START("다리 건너기 게임을 시작합니다."),
+  INPUT_BRIDGE_LENGTH("다리의 길이를 입력해주세요."),
+  INPUT_DIRECTION("이동할 칸을 선택해주세요. (" + ControlKey.UP.getPair() + ", " + ControlKey.DOWN.getPair() + ")"),
+  INPUT_RETRY_OR_QUIT("게임을 다시 시도할지 여부를 입력해주세요. (" + ControlKey.RETRY.getPair() + ", " + ControlKey.QUIT.getPair() + ")"),
+  RESULT("최종 게임 결과"),
+  SUCCESS_OR_FAILURE("게임 성공 여부: "),
+  TRY_COUNT("총 시도한 횟수: "),
+  SUCCESS("성공"),
+  FAILURE("실패");
+
+
+  private String message;
+
+  InGameMessage(String message) {
+    this.message = message;
+  }
+
+  public String getMessage() {
+    return message;
+  }
+
+}
