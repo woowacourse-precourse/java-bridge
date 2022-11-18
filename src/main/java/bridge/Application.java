6cbdb4a0c4382e67e1b00b83e2008ab56cb1b706
devjordy14 <devjordy14@gmail.com>
2022-11-18 19:13:37 +0900

    feat(BridgeMaker): add features for makeBridge function

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -1,10 +1,15 @@
 package bridge;
 
+import java.util.List;
+
 public class Application {
 
     public static void main(String[] args) {
         InputView inputView = new InputView();
         int bridgeSize = inputView.readBridgeSize();
 
+        BridgeMaker bridgeMaker = new BridgeMaker(new BridgeRandomNumberGenerator());
+        List<String> bridge = bridgeMaker.makeBridge(bridgeSize);
+        System.out.println(bridge);
     }
 }
