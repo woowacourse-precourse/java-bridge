6703643e9d9f42241912a0dd72db975a8a3dfd02
anneu <b.imnuj@gmail.com>
2022-11-18 13:42:36 +0900

    test: 다리를 이동하는 기능 테스트

---
+++ src/test/java/bridge/BridgeGameTest.java
@@ -0,0 +1,22 @@
+package bridge;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.List;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+class BridgeGameTest {
+
+    private final BridgeGame bridgeGame = new BridgeGame();
+
+    @DisplayName("이동이 가능하면 O, 불가능하면 X로 표시한다.")
+    @CsvSource(value = {"D, O", "U, X"})
+    @ParameterizedTest
+    void move(String moving, String expected) {
+        Bridge bridge = new Bridge(List.of("D", "U", "U"));
+        String actual = bridgeGame.move(bridge, 0, moving);
+        assertThat(actual).isEqualTo(expected);
+    }
+}
