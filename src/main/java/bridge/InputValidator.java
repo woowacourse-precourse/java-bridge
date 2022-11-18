9671cc9ae7137334590a507353579c9c9e4864e7
Sanmgmin <sangmins930@gmail.com>
2022-11-18 14:33:22 +0900

    feat(InputValidator): create InputValidator and implement validateNumeric

---
+++ src/main/java/bridge/InputValidator.java
@@ -0,0 +1,13 @@
+package bridge;
+
+public class InputValidator {
+    private static String ERROR_FOR_LENGTH_OF_BRIDGE = "[ERROR] 다리의 길이는 3이상 20이하의 숫자만 가능합니다.";
+
+    public static int validateNumeric(String input) {
+        try {
+            return Integer.parseInt(input);
+        } catch (NumberFormatException nfe) {
+            throw new IllegalArgumentException(ERROR_FOR_LENGTH_OF_BRIDGE);
+        }
+    }
+}
