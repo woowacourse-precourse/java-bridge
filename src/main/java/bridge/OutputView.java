e4986011fb2992b5ddb9e2438dc7f5f270fdfc94
asas6978 <asas6978@naver.com>
2022-11-19 01:39:44 +0900

    feat: 사용자 입력 시 안내 메시지 출력 메서드 구현

--- src/main/java/bridge/OutputView.java
+++ src/main/java/bridge/OutputView.java
@@ -9,6 +9,14 @@ import java.util.List;
  * 사용자에게 게임 진행 상황과 결과를 출력하는 역할을 한다.
  */
 public class OutputView {
+    public void printGameStart() {
+        System.out.println(GuideMessage.START_GAME);
+    }
+
+    public void printEnterSize() {
+        System.out.println(GuideMessage.ENTER_BRIDGE_SIZE);
+    }
+
     public void printFirstOfBridge() {
         System.out.print(GuideMessage.FIRST_OF_BRIDGE);
     }
