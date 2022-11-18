538f6fd4555590b07ebbebb8f4f103947153ed4f
SuhHyeonjun <dmsgktn0208@gmail.com>
2022-11-18 18:54:14 +0900

    feat : Up, down 다리의 값들을 초기화하는 메서드 추가

--- src/main/java/bridge/UpDownBridge.java
+++ src/main/java/bridge/UpDownBridge.java
@@ -13,24 +13,29 @@ public class UpDownBridge {
     }
 
     public static void addUpBridge(String inputDirection, String moveResult) {
-            if(inputDirection.equals(Message.UP.getMessage())) {
-                upBridge.add(moveResult);
-                downBridge.add(Message.BLANK.getMessage());
-            }
+        if (inputDirection.equals(Message.UP.getMessage())) {
+            upBridge.add(moveResult);
+            downBridge.add(Message.BLANK.getMessage());
+        }
     }
 
     public static void addDownBridge(String inputDirection, String moveResult) {
-            if (inputDirection.equals(Message.Down.getMessage())) {
-                upBridge.add(Message.BLANK.getMessage());
-                downBridge.add(moveResult);
-            }
+        if (inputDirection.equals(Message.Down.getMessage())) {
+            upBridge.add(Message.BLANK.getMessage());
+            downBridge.add(moveResult);
+        }
+    }
+
+    public static void initBridge() {
+        upBridge.clear();
+        downBridge.clear();
     }
 
     public static String addCenterBar(List<String> toAddBridge) {
         return String.join(Message.CENTER_BAR.getMessage(), toAddBridge);
     }
 
-    public static List<String> getUpbridge() {
+    public static List<String> getUpBridge() {
         return upBridge;
     }
 
