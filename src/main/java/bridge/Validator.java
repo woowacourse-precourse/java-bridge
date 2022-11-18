c68bb3a516b63440ec3d8794b1c6f29aace35bf4
Amyhds <dasomhan524@gmail.com>
2022-11-18 17:10:04 +0900

    refactor(validator): 예외 발생과 동시에 메시지 출력하도록 변경

--- src/main/java/bridge/Validator.java
+++ src/main/java/bridge/Validator.java
@@ -5,37 +5,32 @@ public class Validator {
     public static void validateNumber(String userInput) {
         for (int index = 0; index < userInput.length(); index++) {
             if (!Character.isDigit(userInput.charAt(index))) {
-                System.out.println(ErrorMessage.NUMBER_ERROR.getMessage());
-                throw new IllegalArgumentException();
+                throw new IllegalArgumentException(ErrorMessage.NUMBER_ERROR.getMessage());
             }
         }
     }
 
     public static void validateRange(int bridgeNumber) {
         if (bridgeNumber < 3 || bridgeNumber > 20) {
-            System.out.println(ErrorMessage.RANGE_ERROR.getMessage());
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException(ErrorMessage.RANGE_ERROR.getMessage());
         }
     }
 
     public static void validateLength(String userInput) {
         if (userInput.length() != 1) {
-            System.out.println(ErrorMessage.LENGTH_ERROR.getMessage());
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException(ErrorMessage.LENGTH_ERROR.getMessage());
         }
     }
 
     public static void validateMoving(char moving) {
         if (moving != 'U' && moving != 'D') {
-            System.out.println(ErrorMessage.MOVING_ERROR.getMessage());
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException(ErrorMessage.MOVING_ERROR.getMessage());
         }
     }
 
     public static void validateGameCommand(char gameCommand) {
         if (gameCommand != 'R' && gameCommand != 'Q') {
-            System.out.println(ErrorMessage.GAMECOMMAND_ERROR.getMessage());
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException(ErrorMessage.GAMECOMMAND_ERROR.getMessage());
         }
     }
 }
