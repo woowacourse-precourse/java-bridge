4cb6003a56eda7b5c9ae57f6103f36320249946d
saint6839 <saint6839@gmail.com>
2022-11-18 14:54:09 +0900

    test(domain) : 입력된 다리 길이가 3보다 작을 경우 다리 생성시 예외가 발생하는지 테스트 작성

---
+++ src/test/java/bridge/BridgeTest.java
@@ -0,0 +1,27 @@
+package bridge;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BridgeTest {
+    private static final String INVALID_LENGTH_RANGE_MESSAGE = "[ERROR] 다리 길이는 3이상 20이하의 숫자를 입력해주세요.";
+
+    @DisplayName("다리 길이가 3미만일 경우 예외가 발생하는지 테스트")
+    @Test
+    void validateLengthRangeByUnder3() {
+        Assertions.assertThatThrownBy(() -> {
+                      new Bridge(2);
+                  })
+                  .isInstanceOf(IllegalArgumentException.class)
+                  .hasMessageContaining(INVALID_LENGTH_RANGE_MESSAGE);
+    }
+
+    @DisplayName("다리 길이가 20초과일 경우 예외가 발생하는지 테스트")
+    @Test
+    void validateLengthRangeByOver20() {
+
+    }
+}
