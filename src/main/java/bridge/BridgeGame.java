2ed1f429f49084e4e643a3a6289ff114ade3e679
Dan-HeeYoung9182 <ldan9182@gmail.com>
2022-11-18 13:15:29 +0900

    사용자가 칸을 이동할 때 사용하는 메서드 생성

--- src/main/java/bridge/BridgeGame.java
+++ src/main/java/bridge/BridgeGame.java
@@ -20,7 +20,12 @@ public class BridgeGame {
      * <p>
      * 이동을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void move() {}
+    public String move() {
+        String userInput = iv.readMoving();
+        totalCount++;
+        attempt++;
+        return lastUserInput = userInput;
+    }
 
     /**
      * 사용자가 게임을 다시 시도할 때 사용하는 메서드
