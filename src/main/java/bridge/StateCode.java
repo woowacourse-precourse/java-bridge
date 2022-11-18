0355c677401654cd960c04832b54f29e25d7b5da
0112leesy <0112leesy@naver.com>
2022-11-18 17:48:50 +0900

    feat(StateCode) : 다리 상태와 사용자의 선택에 따른 상태 코드 선언
    4가지 상태가 선언됨
    - U일때 U를 선택한 경우) U_TRUE
    - U일때 D를 선택한 경우) U_FALSE
    - D일때 U를 선택한 경우) D_FALSE
    - D일때 D를 선택한 경우) D_TRUE

---
+++ src/main/java/bridge/StateCode.java
@@ -0,0 +1,25 @@
+package bridge;
+
+public enum StateCode {
+
+    U_TRUE("UU", 1),
+    U_FALSE("UD", 2),
+    D_FALSE("DU", 3),
+    D_TRUE("DD", 4);
+
+    private final String state;
+    private final int code;
+
+    StateCode(String state, int code) {
+        this.state = state;
+        this.code = code;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public int getCode() {
+        return code;
+    }
+}
