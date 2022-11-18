107658532e98a6c07d6a133614f52fe4acab7877
semi-cloud <sksmsrkdtpal@naver.com>
2022-11-18 17:53:22 +0900

    feat(Square): 이동할 칸 입력에 U와 D가 들어오지 않는 예외 처리 구현
    - 특수문자와 숫자가 들어오는 예외
    - U와 D를 제외한 문자가 들어오는 예외
    - U와 D의 소문자가 들어오는 예외

--- src/main/java/bridge/domain/Square.java
+++ src/main/java/bridge/domain/Square.java
@@ -2,12 +2,20 @@ package bridge.domain;
 
 public class Square {
 
-    private final int position;
-    private final String bridgeType;
+    private static final String REG_XP_MOVE_TYPE = "[UD]+";
 
-    public Square(int position, String bridgeType) {
+    private final int position;
+    private final String moveType;
+
+    public Square(int position, String moveType) {
+        isValidMoveType(moveType);
         this.position = position;
-        this.bridgeType = bridgeType;
+        this.moveType = moveType;
     }
 
+    private void isValidMoveType(String moveType) {
+        if (!moveType.matches(REG_XP_MOVE_TYPE)) {
+            throw new IllegalArgumentException("[ERROR] 이동할 칸은 U와 D 문자만 입력 가능합니다.");
+        }
+    }
 }
