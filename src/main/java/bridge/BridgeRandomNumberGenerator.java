c5fa3938eeef6ad4920fd139f2979ff8b10b6387
bando99 <bando991225@gmail.com>
2022-11-18 21:11:05 +0900

    0,1 을 랜덤으로 반환하는 기능 구현

--- src/main/java/bridge/BridgeRandomNumberGenerator.java
+++ src/main/java/bridge/BridgeRandomNumberGenerator.java
@@ -7,8 +7,12 @@ public class BridgeRandomNumberGenerator implements BridgeNumberGenerator {
     private static final int RANDOM_LOWER_INCLUSIVE = 0;
     private static final int RANDOM_UPPER_INCLUSIVE = 1;
 
+
+    // 0과 1 중 랜덤으로 하나를 출력하는 것
+    // 정답 다리를 정하는 거 같음.
     @Override
     public int generate() {
         return Randoms.pickNumberInRange(RANDOM_LOWER_INCLUSIVE, RANDOM_UPPER_INCLUSIVE);
     }
+
 }
