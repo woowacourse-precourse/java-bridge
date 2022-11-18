e74cd221afa806da04269eb126509ac050a146a8
jaeyeon kim <jakind@naver.com>
2022-11-18 20:42:56 +0900

    test(ValidatorTest): 이동 입력 유효성 검증 테스트 코드

--- src/test/java/bridge/ValidatorTest.java
+++ src/test/java/bridge/ValidatorTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
 import static bridge.ErrorCode.WRONG_BRIDGE_LENGTH_INPUT;
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class ValidatorTest {
@@ -31,5 +32,29 @@ public class ValidatorTest {
         assertThatThrownBy(() -> validator.bridgeLengthValidate(length))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining(WRONG_BRIDGE_LENGTH_INPUT);
+    }
+
+    @DisplayName("이동 입력이 유효한 경우")
+    @ParameterizedTest
+    @ValueSource(strings = {"U", "D"})
+    void moveCorrectValue(String move) {
+        // given
+        Validator validator = new Validator();
+
+        // then
+        validator.moveValidate(move);
+    }
+
+    @DisplayName("이동 입력이 유효하지 않은 경우")
+    @ParameterizedTest
+    @ValueSource(strings = {"a", " ", "d"})
+    void moveWrongValue(String move) {
+        // given
+        Validator validator = new Validator();
+
+        // then
+        assertThatThrownBy(() -> validator.moveValidate(move))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(WRONG_MOVE_INPUT);
     }
 }
