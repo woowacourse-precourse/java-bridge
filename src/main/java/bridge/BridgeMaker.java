bdeca0156baa6ff4b8a88a8fe69327da3217f946
JuHyeokParkk <hyeok5596@naver.com>
2022-11-18 22:48:34 +0900

    fix: 다리 생성 테스트를 통과하지 않던 오류 수정

--- src/main/java/bridge/BridgeMaker.java
+++ src/main/java/bridge/BridgeMaker.java
@@ -20,7 +20,7 @@ public class BridgeMaker {
      * @return 입력받은 길이에 해당하는 다리 모양. 위 칸이면 "U", 아래 칸이면 "D"로 표현해야 한다.
      */
     public List<String> makeBridge(int size) {
-        List<String> alphabetMappingIndex = List.of("U", "D");
+        List<String> alphabetMappingIndex = List.of("D", "U");
         List<String> bridge = new ArrayList<>();
 
         for (int currentBridgeSize = 0; currentBridgeSize < size; currentBridgeSize++) {
