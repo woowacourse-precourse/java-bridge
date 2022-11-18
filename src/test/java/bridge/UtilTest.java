a3300fceabd5e6640c508df83a49d63e8db30114
kissshot1104 <kissshot.heartunderblade1104@gmail.com>
2022-11-18 09:58:49 +0900

    Test(UtilTest) : 숫자열을 정수로 바꾸는 기능 테스트 코드 추가
    Util클래스의
    public static integer stringToInteger(String input) 메소드의
    테스트 코드를 추가했습니다.

--- src/test/java/bridge/UtilTest.java
+++ src/test/java/bridge/UtilTest.java
@@ -1,11 +1,15 @@
 package bridge;
 
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import static org.assertj.core.api.Assertions.assertThat;
-
 class UtilTest {
+
+    public static final String ERROR_MESSAGE = "[ERROR]";
 
     @Test
     @DisplayName("숫자인지 아닌지 검사하는 테스트 코드")
@@ -65,5 +69,37 @@ class UtilTest {
         assertThat(Util.convertBridgeNumberGenerator(0))
                 .isEqualTo("D");
     }
-    
+
+
+    @Test
+    @DisplayName("숫자열을 정수로 바꿔준다. 범위는 1~20이 최대다")
+    void stringToIntegerTest(){
+        assertThatThrownBy(() ->
+                Util.stringToInteger("0"))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(ERROR_MESSAGE);
+
+        assertThatThrownBy(() ->
+                Util.stringToInteger("-1"))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(ERROR_MESSAGE);
+
+        assertThatThrownBy(() ->
+                Util.stringToInteger("100000000000"))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(ERROR_MESSAGE);
+
+        assertThatThrownBy(() ->
+                Util.stringToInteger("010"))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(ERROR_MESSAGE);
+
+        assertThat(Util.stringToInteger("1"))
+                .isEqualTo(1);
+
+        assertThat(Util.stringToInteger("20"))
+                .isEqualTo(20);
+
+    }
+
 }
