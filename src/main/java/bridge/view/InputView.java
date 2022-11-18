6fb6333c9ce79ab5239660738ad2f1d5aecf9e32
jaehee329 <jaehee329@yonsei.ac.kr>
2022-11-18 20:47:08 +0900

    feat(InputView): 재시도 여부를 입력받는 기능 구현

--- src/main/java/bridge/view/InputView.java
+++ src/main/java/bridge/view/InputView.java
@@ -35,6 +35,9 @@ public class InputView {
      * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
      */
     public String readGameCommand() {
-        return null;
+        System.out.println(OutMsg.RETRY_REQUEST.getMessage());
+        String retry = Console.readLine();
+        inputValidator.validateRetry(retry);
+        return retry;
     }
 }
