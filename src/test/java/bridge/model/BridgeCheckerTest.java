3a2c76ac3628e01e264b69477e8d40d352665239
ysng_ysng <yeasung67@gmail.com>
2022-11-18 15:24:54 +0900

    test(BridgeCheckerTest): Implement the test methods to check the game finished
    다리를 모두 건넜는지 체크하는 메서드에 대한 테스트 메서드를 구현했습니다.

--- src/test/java/bridge/model/BridgeCheckerTest.java
+++ src/test/java/bridge/model/BridgeCheckerTest.java
@@ -9,24 +9,27 @@ import org.junit.jupiter.api.Test;
 import java.util.List;
 
 public class BridgeCheckerTest {
-	@DisplayName("정상적으로 이동하는 경우") @Test public void 이동가능테스트() {
+	@DisplayName("정상적으로 이동하는 경우") @Test public void 이동가능테스트2() {
 		int userInputLength = 3;
 		BridgeNumberGenerator bridgeNumberGenerator = new BridgeRandomNumberGenerator();
 		BridgeMaker bridgeMaker = new BridgeMaker(bridgeNumberGenerator);
 		List<String> bridge = bridgeMaker.makeBridge(userInputLength);
 
-		int gameTurn;
-		boolean movableFlag = false;
-		for (gameTurn = 0; gameTurn < userInputLength; gameTurn++) {
-			movableFlag = BridgeChecker.isNotMovableStep(bridge, "U", gameTurn);
-			if (movableFlag) {
+		int gameTurn = 0;
+		while(true) {
+			if (BridgeChecker.isEndGame(bridge, gameTurn)) {
+				break;
+			}
+
+			if (BridgeChecker.isNotMovableStep(bridge, "U", gameTurn)) {
 				System.out.println("게임이 끝난 턴 : " + (gameTurn + 1));
 				Assertions.assertThat(gameTurn).isNotEqualTo(bridge.size());
 				break;
 			}
+			gameTurn++;
 		}
 
-		if (!movableFlag) {
+		if (!BridgeChecker.isNotMovableStep(bridge, "U", gameTurn)) {
 			System.out.println("게임이 끝난 턴 : " + gameTurn);
 			Assertions.assertThat(gameTurn).isEqualTo(bridge.size());
 		}
