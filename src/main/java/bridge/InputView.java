2cf806255c0f9589c3d2ab1b0ffe56ed8b460727
jmj005 <jmj00005@naver.com>
2022-11-18 17:36:14 +0900

    feat(InputView): 다리 길이 유효성 확인하기 기능 구현

--- src/main/java/bridge/InputView.java
+++ src/main/java/bridge/InputView.java
@@ -13,7 +13,17 @@ public class InputView {
     public int readBridgeSize() {
         System.out.println("다리의 길이를 입력해주세요.");
         String bridgeSize = Console.readLine();
+        validateBridgeSize(bridgeSize);
         return Integer.parseInt(bridgeSize);
+    }
+
+    private void validateBridgeSize(String bridgeSize) {
+        if (!bridgeSize.matches("^[0-9]*$")) {
+            throw new IllegalArgumentException("[ERROR] 숫자만 입력 가능합니다.");
+        }
+        if (3 > Integer.parseInt(bridgeSize) || Integer.parseInt(bridgeSize) > 20) {
+            throw new IllegalArgumentException("[ERROR] 다리 길이는 3부터 20 사이의 숫자여야 합니다.");
+        }
     }
 
     /**
