268441bd0f0175dfa8bb7d998f0fbef2e50e5763
Donguk <95donguk.dev@gmail.com>
2022-11-18 22:44:30 +0900

    feat: 다리 길이 입력 기능 구현

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -5,6 +5,8 @@ import bridge.resource.message.FixedMessage;
 public class Application {
 
     public static void main(String[] args) {
+        InputView inputView = new InputView();
         System.out.println(FixedMessage.GAME_START.getMessage() + "\n");
+        inputView.readBridgeSize();
     }
 }
