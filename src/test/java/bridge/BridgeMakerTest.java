9f0342fc211e470515e6a616bba8bcfa096f704b
jsc1014 <wotlr5209@gmail.com>
2022-11-19 01:17:04 +0900

    test: 다리가 길이에 맞게 생성되는지 테스트

--- src/test/java/bridge/BridgeMakerTest.java
+++ src/test/java/bridge/BridgeMakerTest.java
@@ -1,4 +1,26 @@
+package bridge;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
+
 class BridgeMakerTest {
-  
+
+    @DisplayName("다리가 길이에 맞게 생성되는지 테스트")
+    @Test
+    void createRandomBridge(){
+        BridgeNumberGenerator bridgeNumberGenerator = new BridgeRandomNumberGenerator();
+        BridgeMaker bridgeMaker = new BridgeMaker(bridgeNumberGenerator);
+        List<String> firstBridge = new ArrayList<>(bridgeMaker.makeBridge(3));
+
+        System.out.println(firstBridge);
+        assertThat(firstBridge.size() == 3);
+    }
 }
