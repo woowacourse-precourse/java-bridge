8b6b5c782483dae1d8ac63a006b4e46fd8c97325
minhee020 <minhee020@naver.com>
2022-11-18 15:02:22 +0900

    feat : 게임 진행 설계 및 구현

--- src/main/java/bridge/GameController.java
+++ src/main/java/bridge/GameController.java
@@ -1,19 +1,35 @@
 package bridge;
 
+import java.util.List;
+
 public class GameController {
     public void start() {
         System.out.println("다리 건너기 게임을 시작합니다.\n");
-        getBridgeSize();
-        startCrossBridge();
+        int bridgeSize = getBridgeSize();
+        BridgeMaker bridgeMaker = new BridgeMaker(new BridgeRandomNumberGenerator());
+        startCrossBridge(bridgeMaker.makeBridge(bridgeSize));
         getGameResult();
     }
 
-    private void getBridgeSize() {
-
+    private int getBridgeSize() {
+        InputView inputView = new InputView();
+        return inputView.readBridgeSize();
     }
 
-    private void startCrossBridge() {
+    private void startCrossBridge(List<String> bridge) {
+        int count = 0;
+        BridgeGame bridgeGame = new BridgeGame(bridge);
+        InputView inputView = new InputView();
+        OutputView outputView = new OutputView();
 
+        while (true) {
+            String moving = inputView.readMoving();
+            if (!bridgeGame.move(moving, count++)) {
+                outputView.printMap(bridgeGame.getResult());
+                break;
+            }
+            outputView.printMap(bridgeGame.getResult());
+        }
     }
 
     private void getGameResult() {
