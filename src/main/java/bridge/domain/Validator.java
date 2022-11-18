93f19b9bcae60f06fa306460c0b22035f1248575
Yerim lee <103623292+lyrWinterCat@users.noreply.github.com>
2022-11-18 17:48:01 +0900

    feat (InputView, Validator) : 다리 길이 입력 구현

--- src/main/java/bridge/domain/Validator.java
+++ src/main/java/bridge/domain/Validator.java
@@ -1,4 +1,23 @@
 package bridge.domain;
 
 public class Validator {
+    static final private String BRIDGE_SIZE_NOT_NUMBER = "[ERROR] 다리 길이는 숫자로 입력 가능합니다.";
+    static final private String BRIDGE_SIZE_WRONG_RANGE = "[ERROR] 다리 길이는 3 이상 20 이하로 입력해야 합니다.";
+
+    public static void validateBridgeSize(String bridgeSize) {
+        validateNumber(bridgeSize);
+        validateRange(bridgeSize);
+    }
+
+    private static void validateNumber(String bridgeSize) {
+        if(!bridgeSize.matches("\\d+")){
+            throw new IllegalArgumentException(BRIDGE_SIZE_NOT_NUMBER);
+        }
+    }
+
+    private static void validateRange(String bridgeSize) {
+        if(!bridgeSize.matches("^[3-9]$|^1\\d$|^20$")){
+            throw new IllegalArgumentException(BRIDGE_SIZE_WRONG_RANGE);
+        }
+    }
 }
--- src/main/java/bridge/view/InputView.java
+++ src/main/java/bridge/view/InputView.java
@@ -1,5 +1,8 @@
 package bridge.view;
 
+import bridge.domain.Validator;
+import camp.nextstep.edu.missionutils.Console;
+
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
  */
@@ -9,7 +12,9 @@ public class InputView {
      * 다리의 길이를 입력받는다.
      */
     public int readBridgeSize() {
-        return 0;
+        String bridgeSize = Console.readLine();
+        Validator.validateBridgeSize(bridgeSize);
+        return Integer.parseInt(bridgeSize);
     }
 
     /**
