2d3ee345c9ecc8f130106bfbca91ff58a8e2965e
김태용 <amico741@naver.com>
2022-11-18 12:58:32 +0900

    feat: view의 다리 길이 입력 출력문 추가

--- src/main/java/bridge/OutputView.java
+++ src/main/java/bridge/OutputView.java
@@ -20,4 +20,12 @@ public class OutputView {
      */
     public void printResult() {
     }
+
+    public void printStartMessage() {
+        System.out.println("다리 건너기 게임을 시작합니다.");
+    }
+
+    public void printInputLengthMessage() {
+        System.out.println("다리의 길이를 입력해주세요.");
+    }
 }
