374d38653c49ed3e3128d364e4f8bfa384dc83e8
Jimin <thswlals0219@gmail.com>
2022-11-18 14:18:05 +0900

    test(Player): 사용자의 다리 건너기 기능 테스트 추가

---
+++ src/test/java/bridge/domain/PlayerTest.java
@@ -0,0 +1,54 @@
+package bridge.domain;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.params.provider.Arguments.arguments;
+
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.MethodSource;
+
+class PlayerTest {
+    private Player player;
+
+    @BeforeEach
+    void setUp() {
+        player = new Player();
+    }
+
+    @DisplayName("사용자의 움직임은 건널 수 있다면 O, 없다면 X 상태값을 가진다.")
+    @ParameterizedTest
+    @CsvSource({"true, O", "false, X"})
+    void 이동가능_여부에_따른_상태값_계산(Boolean isMoveble, String expected) {
+        assertThat(player.calculateStatusAfterMove(isMoveble)).isEqualTo(expected);
+    }
+
+    @DisplayName("사용자가 다리를 건넌 후의 상태를 테스트한다.")
+    @ParameterizedTest
+    @MethodSource("parametersProvider")
+    void 사용자가_다리를_건넌_후_움직임_상태(String moves, Boolean isMoveble, String expectedUp, String expectedDown) {
+        for (String move : moves.split("")) {
+            Movement userMovement = Movement.valueOf(move);
+            player.crossOverBridge(userMovement, isMoveble);
+        }
+        Map<Movement, List<String>> movementStatus = player.getMovementStatus();
+
+        assertThat(movementStatus.get(Movement.U).stream().collect(Collectors.joining()))
+                .isEqualTo(expectedUp);
+        assertThat(movementStatus.get(Movement.D).stream().collect(Collectors.joining()))
+                .isEqualTo(expectedDown);
+    }
+
+    static Stream<Arguments> parametersProvider() {
+        return Stream.of(
+                arguments("UDD", true, "O  ", " OO"),
+                arguments("D", false, " ", "X")
+        );
+    }
+}
