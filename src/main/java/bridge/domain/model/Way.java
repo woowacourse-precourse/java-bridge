a0d481d72e5fbcbdff6155b797d6645755319715
jiisuniui <dudndi9752@gmail.com>
2022-11-18 09:59:59 +0900

    feat(Way): 생성자 추가

--- src/main/java/bridge/domain/model/Way.java
+++ src/main/java/bridge/domain/model/Way.java
@@ -2,11 +2,27 @@ package bridge.domain.model;
 
 public class Way {
 
+    private static final String UP = "u";
+    private static final String DOWN = "d";
+
     String up;
     String down;
 
     public Way(String up, String down) {
         this.up = up;
         this.down = down;
+    }
+
+    public Way(String where, boolean isValid) {
+        if (isValid) {
+            if (where.equals(UP)) {
+                new Way("O", " ");
+            }
+            new Way(" ", "O");
+        }
+        if (where.equals(UP)) {
+            new Way("X", " ");
+        }
+        new Way(" ", "X");
     }
 }
