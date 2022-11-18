4b9e3d193580811659f9b301f8a396eeb65b841e
Life is a ninanino <tvxqinak@naver.com>
2022-11-19 03:57:38 +0900

    feat : 사용자로부터 입력을 받은 값을 검증으로 넘긴다

--- src/main/java/bridge/view/InputView.java
+++ src/main/java/bridge/view/InputView.java
@@ -1,28 +1,35 @@
 package bridge.view;
 
+import bridge.utils.Validator;
+import camp.nextstep.edu.missionutils.Console;
+
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
  */
 public class InputView {
 
+    public String input(){
+        return Console.readLine();
+    }
+
     /**
      * 다리의 길이를 입력받는다.
      */
     public int readBridgeSize() {
-        return 0;
+        return Validator.validateBridgeLength(input());
     }
 
     /**
      * 사용자가 이동할 칸을 입력받는다.
      */
     public String readMoving() {
-        return null;
+        return Validator.validateMove(input());
     }
 
     /**
      * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
      */
     public String readGameCommand() {
-        return null;
+        return Validator.validateRestartOrQuit(input());
     }
 }
