2b5fee9d34e817c04be1db886edcf3c68a0b2335
dev_kong <ahdjd5@gmail.com>
2022-11-18 18:39:27 +0900

    feat(Validator): add moveInput method to check moveInput validation

--- src/main/java/bridge/utils/Validator.java
+++ src/main/java/bridge/utils/Validator.java
@@ -6,4 +6,10 @@ public class Validator {
             throw new IllegalArgumentException("3 이상 20 이하의 숫자만 입력가능합니다");
         }
     }
+
+    public static void checkMoveInput(String moveInput) {
+        if(!moveInput.equals("U") && !moveInput.equals("D")) {
+            throw new IllegalArgumentException("보기에 있는 값만 입력 가능합니다.");
+        }
+    }
 }
