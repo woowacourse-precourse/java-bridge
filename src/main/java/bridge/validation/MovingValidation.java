bd1f433ff6b06390b83297811fa8d7ae2e8735d7
ykmxxi <ykmxxi97@gmail.com>
2022-11-18 15:09:53 +0900

    feat: 이동할 칸 입력 예외 처리기능[공백의 문자열 처리] 구현

--- src/main/java/bridge/validation/MovingValidation.java
+++ src/main/java/bridge/validation/MovingValidation.java
@@ -5,6 +5,13 @@ import bridge.validation.enums.ErrorMessage;
 public class MovingValidation {
     private ErrorMessage message;
 
+    public MovingValidation() {
+    }
+
+    public void throwError(String message) {
+        throw new IllegalArgumentException(message);
+    }
+
     public void isNull(String moving) {
         if (moving.isEmpty()) {
             message = ErrorMessage.valueOf("MOVING_NULL_EXCEPTION");
@@ -13,6 +20,10 @@ public class MovingValidation {
     }
 
     public void isBlank(String moving) {
+        if (moving.isBlank() && moving.length() != 0) {
+            message = ErrorMessage.valueOf("MOVING_BLANK_EXCEPTION");
+            throwError(message.getMessage());
+        }
     }
 
     public void isUpperCaseUD(String moving) {
