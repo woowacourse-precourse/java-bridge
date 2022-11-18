5f3989d65d03155c091e14ee239b87504ef0cdd0
Amyhds <dasomhan524@gmail.com>
2022-11-18 16:44:32 +0900

    refactor(validator): 정적 메서드로 변경

--- src/main/java/bridge/Validator.java
+++ src/main/java/bridge/Validator.java
@@ -1,7 +1,8 @@
 package bridge;
 
 public class Validator {
-    public void validateNumber(String bridgeNumber) {
+
+    public static void validateNumber(String bridgeNumber) {
         for (int index = 0; index < bridgeNumber.length(); index++) {
             if (!Character.isDigit(bridgeNumber.charAt(index))) {
                 System.out.println(ErrorMessage.NUMBER_ERROR.getMessage());
@@ -10,28 +11,28 @@ public class Validator {
         }
     }
 
-    public void validateRange(int bridgeNumber) {
+    public static void validateRange(int bridgeNumber) {
         if (bridgeNumber < 3 || bridgeNumber > 20) {
             System.out.println(ErrorMessage.RANGE_ERROR.getMessage());
             throw new IllegalArgumentException();
         }
     }
 
-    public void validateLength(String userInput) {
+    public static void validateLength(String userInput) {
         if (userInput.length() != 1) {
             System.out.println(ErrorMessage.LENGTH_ERROR.getMessage());
             throw new IllegalArgumentException();
         }
     }
 
-    public void validateMoving(char moving) {
+    public static void validateMoving(char moving) {
         if (moving != 'U' && moving != 'D') {
             System.out.println(ErrorMessage.MOVING_ERROR.getMessage());
             throw new IllegalArgumentException();
         }
     }
 
-    public void validateGameCommand(char gameCommand) {
+    public static void validateGameCommand(char gameCommand) {
         if (gameCommand != 'R' && gameCommand != 'Q') {
             System.out.println(ErrorMessage.GAMECOMMAND_ERROR.getMessage());
             throw new IllegalArgumentException();
