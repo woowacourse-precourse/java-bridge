a3f09074708b6936c04613bc5a125a7652e2f8bb
Voyager003 <85725033+Voyager003@users.noreply.github.com>
2022-11-18 15:44:12 +0900

    Test: 추가 - BridgeMakerTest 생성한 리스트에 문자 U와 D가 포함되는지 확인

--- src/test/java/bridge/BridgeMakerTest.java
+++ src/test/java/bridge/BridgeMakerTest.java
@@ -1,2 +1,23 @@
-package bridge;public class BridgeMakerTest {
+package bridge;
+
+import bridge.domain.BridgeMaker;
+import bridge.domain.BridgeRandomNumberGenerator;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class BridgeMakerTest {
+
+    @DisplayName("리스트에 U와 D가 포함되는지 검증")
+    @Test
+    void makeBridgeTest() {
+        BridgeMaker bridgeMaker;
+        bridgeMaker = new BridgeMaker(new BridgeRandomNumberGenerator());
+        List<String> bridge = bridgeMaker.makeBridge(5);
+        assertThat(bridge).containsOnly("U","D");
+    }
 }
