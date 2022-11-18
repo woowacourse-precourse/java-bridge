75ea4a0ab90ff8723ffc816d87bfd7dcdbce3355
sosow0212 <sosow0212@naver.com>
2022-11-18 15:10:13 +0900

    feat : (BridgeInputConverter) String타입으로 들어온 다리의 길이를 int타입으로 리턴해준다.

---
+++ src/main/java/bridge/util/BridgeInputConverter.java
@@ -0,0 +1,8 @@
+package bridge.util;
+
+public class BridgeInputConverter {
+    public int bridgeSizeConverter(String bridgeSize) {
+        int convertBridgeSize = Integer.parseInt(bridgeSize);
+        return convertBridgeSize;
+    }
+}
