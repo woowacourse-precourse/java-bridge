d6778fe19fa1cf30435e4bf55f5feb034177ebd4
tenta <eunoia3802@gmail.com>
2022-11-18 15:30:33 +0900

    feat: 다리 이동 기능(이동 후 결과 반환) 구현

--- src/main/java/bridge/BridgeGame.java
+++ src/main/java/bridge/BridgeGame.java
@@ -6,13 +6,36 @@ import java.util.List;
  * 다리 건너기 게임을 관리하는 클래스
  */
 public class BridgeGame {
+    private final String MOVABLE = "O ";
+    private final String UNMOVABLE = "X ";
+    private final String SPACE = "| ";
 
     /**
      * 사용자가 칸을 이동할 때 사용하는 메서드
      * <p>
      * 이동을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void move() {
+    public StringBuilder move(List<String> bridge, String sideToMove,
+                              StringBuilder initialBridge) {
+        int lastIndex = initialBridge.length() - 1;
+        if (isCrossBridge(bridge, sideToMove)) {
+            initialBridge.insert(lastIndex, MOVABLE);
+            initialBridge = isBridgeLengthMoreThan5(initialBridge);
+        }
+        if (!isCrossBridge(bridge, sideToMove)) {
+            initialBridge.insert(lastIndex, UNMOVABLE);
+            initialBridge = isBridgeLengthMoreThan5(initialBridge);
+        }
+        return initialBridge;
+    }
+
+    public StringBuilder isBridgeLengthMoreThan5(StringBuilder initialBridge) {
+        int bridgeLength = initialBridge.length();
+        if (bridgeLength > 5) {
+            int lastIndex = initialBridge.length() - 1;
+            initialBridge.insert(lastIndex, SPACE);
+        }
+        return initialBridge;
     }
 
     public boolean isCrossBridge(List<String> bridge, String sideToMove) {
@@ -25,11 +48,11 @@ public class BridgeGame {
         return false;
     }
 
-        /**
-         * 사용자가 게임을 다시 시도할 때 사용하는 메서드
-         * <p>
-         * 재시작을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
-         */
-        public void retry () {
-        }
+    /**
+     * 사용자가 게임을 다시 시도할 때 사용하는 메서드
+     * <p>
+     * 재시작을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
+     */
+    public void retry() {
     }
+}
