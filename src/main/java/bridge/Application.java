596c1cf2c95068ba1f8a88c71bed3f0130ba7a43
nylonmusk <106235834+nylonmusk@users.noreply.github.com>
2022-11-19 00:10:37 +0900

    refactor(Application): try catch문 적용

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -1,13 +1,19 @@
 package bridge;
 
+import model.BridgeGame;
+import view.OutputView;
 
 public class Application {
 
     private static final OutputView outputView = OutputView.getInstance();
 
     public static void main(String[] args) {
-        outputView.printStart();
-        BridgeGame bridgeGame = new BridgeGame();
-        bridgeGame.run();
+        try {
+            outputView.printStart();
+            BridgeGame bridgeGame = new BridgeGame();
+            bridgeGame.run();
+        } catch (IllegalArgumentException ex) {
+            System.out.println(ex.getMessage());
+        }
     }
 }
