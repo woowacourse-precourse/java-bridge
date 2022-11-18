6adbb9e9a9d9590751690e3ada3d9a08655fd71d
jkroh1995 <85445649+jkroh1995@users.noreply.github.com>
2022-11-18 15:50:53 +0900

    refactor : 기존 하드코딩 문자열을 enum 문자열로 치환, 변수명에 의미 부여

--- src/main/java/view/OutputView.java
+++ src/main/java/view/OutputView.java
@@ -1,6 +1,7 @@
 package view;
 
 import system.BridgeGame;
+import utils.EnumStrings;
 
 /**
  * 사용자에게 게임 진행 상황과 결과를 출력하는 역할을 한다.
@@ -13,9 +14,9 @@ public class OutputView {
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
     public void printMap(String[][] realTimeMap) {
-        for (int j = 0; j < realTimeMap.length; j++) {
-            for (int k = 0; k < realTimeMap[0].length; k++) {
-                System.out.print(realTimeMap[j][k]);
+        for (int row = 0; row < realTimeMap.length; row++) {
+            for (int column = 0; column < realTimeMap[0].length; column++) {
+                System.out.print(realTimeMap[row][column]);
             }
             System.out.println();
         }
@@ -29,11 +30,11 @@ public class OutputView {
     public void printResult(boolean flag, int count, String[][] realTimeMap) {
         String result = new BridgeGame().isSuccess(flag);
         System.out.println();
-        System.out.println("최종 게임 결과");
+        System.out.println(EnumStrings.PRINT_END.getValue());
         printMap(realTimeMap);
         System.out.println();
-        System.out.println("게임 성공 여부: " + result);
-        System.out.println("총 시도한 횟수: " + count);
+        System.out.println(EnumStrings.PRINT_RESULT.getValue() + result);
+        System.out.println(EnumStrings.PRINT_COUNT.getValue() + count);
     }
 
     public static void printError(String errorMessage) {
