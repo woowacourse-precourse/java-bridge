7b29c794314229f1eaf917e097ba0053cbcc118d
whdcks420 <whdcks420@gmail.com>
2022-11-18 18:07:25 +0900

    feat: 다리 건너기 게임 실행 기능

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -1,8 +1,13 @@
 package bridge;
 
+import bridge.domain.BridgeApplication;
+import bridge.ui.InputView;
+import bridge.ui.OutputView;
+
 public class Application {
 
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        BridgeApplication bridgeApplication = new BridgeApplication(new InputView(), new OutputView());
+        bridgeApplication.run();
     }
 }
