ff0f2570dbb0b32d51db0c8bffc1dc260bc9cea7
GwansuShin <happysun7080@naver.com>
2022-11-18 17:19:25 +0900

    refactor(bridge): 이동한 칸, 방향 열거형 이름 변경
    이동할 칸을 입력할 때 메소드명이 readMoving이므로 'Direction'을 'Moving'으로 변경
    이동한 칸의 결과인 Moving을 '한 칸'에 해당하는 용어인 'Step'으로 변경

--- src/main/java/bridge/bridge/Moving.java
+++ src/main/java/bridge/bridge/Moving.java
@@ -1,18 +1,34 @@
 package bridge.bridge;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public enum Moving {
-    MOVABLE("O"),
-    IMMOVABLE("X"),
-    EMPTY(" "),
+
+    UP(1, "U"),
+    DOWN(0, "D"),
     ;
 
-    private final String moving;
-
-    Moving(String moving) {
-        this.moving = moving;
+    private static final Map<Integer, String> MOVING_DIRECTION = new HashMap<>();
+    static {
+        for (Moving value : values()) {
+            MOVING_DIRECTION.put(value.number, value.direction);
+        }
     }
 
-    public String moving() {
-        return moving;
+    private final int number;
+    private final String direction;
+
+    Moving(int number, String direction) {
+        this.number = number;
+        this.direction = direction;
+    }
+
+    public boolean equals(String moving) {
+        return moving.equals(this.direction);
+    }
+
+    public static String convert(int number) {
+        return MOVING_DIRECTION.get(number);
     }
 }
