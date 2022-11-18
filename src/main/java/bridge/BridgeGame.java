e3a0952be15d163d7916c106c918376a3d4fceb1
KangSungBum <peter9706@naver.com>
2022-11-18 16:02:07 +0900

    feat(BridgeGame.java): add initBridge method
    게임에서 사용할 다리 생성하는 메소드 추가
    - List<String> initBridge(int size)
    - BridgeMaker.makeBridge 사용하여 다리 생성하고 반환

--- src/main/java/bridge/BridgeGame.java
+++ src/main/java/bridge/BridgeGame.java
@@ -1,5 +1,10 @@
 package bridge;
 
+import view.InputView;
+import view.OutputView;
+
+import java.util.List;
+
 /**
  * 다리 건너기 게임을 관리하는 클래스
  */
@@ -19,5 +24,38 @@ public class BridgeGame {
      * 재시작을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
     public void retry() {
+    }
+
+    public List<String> initBridge(int size) {
+        BridgeNumberGenerator numberGenerator = new BridgeRandomNumberGenerator();
+        BridgeMaker bridgeMaker = new BridgeMaker(numberGenerator);
+        List<String> gameBridge = bridgeMaker.makeBridge(size);
+
+        return gameBridge;
+    }
+
+    /**
+     * 아래 코드는 임시 흐름 코드
+     * skeleton code
+     */
+    public void start() {
+        InputView inputModule = new InputView();
+        OutputView outputModule = new OutputView();
+
+        outputModule.printGameStartMessage();
+
+        outputModule.printBridgeSizeRequestMessage();
+        inputModule.readBridgeSize();
+
+        while (true) {
+            move();
+            outputModule.printMap();
+
+            if (1 == 1) {
+                break;
+            }
+        }
+
+        outputModule.printResult();
     }
 }
