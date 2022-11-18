c8f54127b27b903ca79ef211926d339b06d6ec4c
SHEOMM <esh0821@snu.ac.kr>
2022-11-18 16:24:17 +0900

    feat: 방향 input이 유효한지 검증 함수 구현

--- src/main/java/bridge/domain/BridgeGame.java
+++ src/main/java/bridge/domain/BridgeGame.java
@@ -1,12 +1,14 @@
 package bridge.domain;
 
+import bridge.model.Direction;
+import bridge.model.ErrorMessage;
 import java.util.List;
 
 /**
  * 다리 건너기 게임을 관리하는 클래스
  */
 public class BridgeGame {
-    List<String> bridge;
+    private final List<String> bridge;
 
     BridgeGame(List<String> bridge){
         this.bridge = bridge;
@@ -17,7 +19,7 @@ public class BridgeGame {
      * <p>
      * 이동을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void move() {
+    public void move(String input) {
     }
 
     /**
@@ -26,5 +28,13 @@ public class BridgeGame {
      * 재시작을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
     public void retry() {
+    }
+
+    private void isValidDirection(String input){
+        if(input.equals(Direction.DOWN.getInput())
+                || input.equals(Direction.UP.getInput())){
+            return;
+        }
+        throw new IllegalArgumentException(ErrorMessage.INVALID_DIRECTION_VALUE.getOutput());
     }
 }
