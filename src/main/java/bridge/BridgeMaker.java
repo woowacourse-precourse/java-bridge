681cf0702f0e3551fb8f09343bc58d88945ab8d6
ddubidubap <park62000@gmail.com>
2022-11-18 12:45:08 +0900

    feat: 다리 생성하기

--- src/main/java/bridge/BridgeMaker.java
+++ src/main/java/bridge/BridgeMaker.java
@@ -11,6 +11,17 @@ public class BridgeMaker {
         this.bridgeNumberGenerator = bridgeNumberGenerator;
     }
 
+    public List<String> makeBridge(int size) {
+        List<String> bridge = new ArrayList<>();
+
+        for (int i = 0; i < size; i++) {
+            int number = bridgeNumberGenerator.generate();
+            String pattern = numToPattern(number);
+            bridge.add(pattern);
+        }
+        return bridge;
+    }
+
     private String numToPattern(int number) {
         if (number == 0)
             return "D";
