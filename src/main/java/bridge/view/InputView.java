809314fb2f1ceee635f66e202988b5356ae6dd11
Kayoung <rkdkdudjd@gmail.com>
2022-11-18 17:38:41 +0900

    feat(InputView): 옳은 입력이 들어올 때까지 반복하는 템플릿

--- src/main/java/bridge/view/InputView.java
+++ src/main/java/bridge/view/InputView.java
@@ -1,5 +1,7 @@
 package bridge.view;
 
+import bridge.Logger;
+
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
  */
@@ -9,20 +11,38 @@ public class InputView {
      * 다리의 길이를 입력받는다.
      */
     public int readBridgeSize() {
-        return 0;
+        while (true) {
+            try {
+                return 0;
+            } catch (IllegalArgumentException e) {
+                Logger.log(e);
+            }
+        }
     }
 
     /**
      * 사용자가 이동할 칸을 입력받는다.
      */
     public String readMoving() {
-        return null;
+        while (true) {
+            try {
+                return null;
+            } catch (IllegalArgumentException e) {
+                Logger.log(e);
+            }
+        }
     }
 
     /**
      * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
      */
     public String readGameCommand() {
-        return null;
+        while (true) {
+            try {
+                return null;
+            } catch (IllegalArgumentException e) {
+                Logger.log(e);
+            }
+        }
     }
 }
