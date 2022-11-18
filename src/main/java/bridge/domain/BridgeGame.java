6886e7cde3b8cf09608abec7a414c1dd1b830f99
alstjr706 <alstjr706@naver.com>
2022-11-19 04:47:46 +0900

    fix : (다리 건너기) 사용자가 칸을 이동할 시 코드 수정

--- src/main/java/bridge/domain/BridgeGame.java
+++ src/main/java/bridge/domain/BridgeGame.java
@@ -13,16 +13,31 @@ public class BridgeGame {
      * <p>
      * 이동을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
+    //TODO: 지금 사용자의 값중 뭐가 U 라인이고, D 라인인지 모른다.
+    //TODO: 최종 list<String> 형태로 넘어간다 [ O, O, X, O ] -> 뭐가 뭐인지 모르는 사오
     public String move(String readMoving, String randomNumber) {
         String printBridge = "";
 
-        if (readMoving.equals(randomNumber)) {
-            printBridge = "O";
+        if (readMoving.equals("U")) {
+            if (readMoving.equals(randomNumber)) {
+                printBridge = "O";
+            }
+
+            if (!(readMoving.equals(randomNumber))) {
+                printBridge = "X";
+            }
         }
 
-        if (!(readMoving.equals(randomNumber))) {
-            printBridge = "X";
+        if (readMoving.equals("D")) {
+            if (readMoving.equals(randomNumber)) {
+                printBridge = "O";
+            }
+
+            if (!(readMoving.equals(randomNumber))) {
+                printBridge = "X";
+            }
         }
+
         return printBridge;
     }
 
