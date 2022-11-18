62e8131dc151c9de80d67c69c30ccdcef8774b1a
Jzakka <mouse4786@gmail.com>
2022-11-18 21:56:31 +0900

    test: 이동 기능 테스트

---
+++ src/test/java/bridge/domain/BridgeGameTest.java
@@ -0,0 +1,20 @@
+package bridge.domain;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BridgeGameTest {
+
+    private BridgeGame bridgeGame = new BridgeGame(new Bridge(List.of("U", "D", "D")), new User());
+
+    @Test
+    void move() {
+        Assertions.assertThat(bridgeGame.move(0, "U")).isTrue();
+        Assertions.assertThat(bridgeGame.move(1, "D")).isTrue();
+        Assertions.assertThat(bridgeGame.move(2, "U")).isFalse();
+    }
+}
