5518506d465bcd3d22314dfe2e2f0944f9bf6cf9
Park shinyeong <syngh503@gmail.com>
2022-11-18 20:10:35 +0900

    Feat(EndGameService): endGameService 구현
    게임 종료 시 재시작 여부를 묻는 isGameRestart()와 게임을 종료하는 endGame 메소드를 구현하였습니다.

--- src/main/java/bridge/Controller/Service/EndGameService.java
+++ src/main/java/bridge/Controller/Service/EndGameService.java
@@ -1,4 +1,33 @@
 package bridge.Controller.Service;
 
+import Utils.ValidateUtils;
+import bridge.Domain.BridgeGame;
+import bridge.Domain.GameResult;
+import bridge.Domain.GameResultGenerator;
+import bridge.EnumCollections.GameRestartType;
+import bridge.View.InputView;
+import bridge.View.OutputView;
+
 public class EndGameService {
+    private BridgeGame bridgeGame;
+    private GameResultGenerator gameResultGenerator;
+
+    public EndGameService(BridgeGame bridgeGame, GameResultGenerator gameResultGenerator) {
+        this.bridgeGame = bridgeGame;
+        this.gameResultGenerator = gameResultGenerator;
+    }
+
+    public boolean isGameRestart() throws IllegalArgumentException {
+        String input = InputView.readGameCommand();
+        ValidateUtils.isValidateGameRestart(input);
+
+        return input.equals(GameRestartType.RESTART.getRestartStatus());
+    }
+
+    public void endGame(GameResult gameResult) {
+        String bridgeStatus = gameResultGenerator.getGameResult();
+        int gameGount = bridgeGame.getGameStartCount();
+        boolean gameSuccess = gameResult.isGameSuccess();
+        OutputView.printResult(bridgeStatus, gameSuccess, gameGount);
+    }
 }
