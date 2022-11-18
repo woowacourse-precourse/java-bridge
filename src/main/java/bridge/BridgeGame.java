782ef028b7478b86860712e630d307642e44dd75
bsh1603 <bsh1603@github.com>
2022-11-19 02:02:25 +0900

    Feat: 게임 시작 및 사용자에게 다리 길이 입력받기

--- src/main/java/bridge/BridgeGame.java
+++ src/main/java/bridge/BridgeGame.java
@@ -4,6 +4,25 @@ package bridge;
  * 다리 건너기 게임을 관리하는 클래스
  */
 public class BridgeGame {
+    InputView inputView = new InputView();
+
+    public void gameStart(){
+        System.out.println("다리 건너기 게임을 시작합니다.");
+        System.out.println("");
+    }
+
+    public int requestBridgeSize(){
+        System.out.println("다리 길이를 입력해주세요.");
+        while(true){
+            try{
+                inputView.readBridgeSize();
+                return inputView.readBridgeSize();
+            }catch (IllegalArgumentException e){
+                System.out.println("다리 길이를 입력해주세요");
+                inputView.readBridgeSize();
+            }
+        }
+    }
 
     /**
      * 사용자가 칸을 이동할 때 사용하는 메서드
