a1fdfcb8f20c7afe5b338faae134f09fb0c8875a
dev_kong <ahdjd5@gmail.com>
2022-11-18 17:21:17 +0900

    fix(Service): add user to prepareGame method

--- src/main/java/bridge/service/Service.java
+++ src/main/java/bridge/service/Service.java
@@ -3,6 +3,7 @@ package bridge.service;
 import bridge.domain.BridgeGame;
 import bridge.domain.BridgeMaker;
 import bridge.domain.BridgeRandomNumberGenerator;
+import bridge.domain.User;
 import bridge.utils.Converter;
 
 import java.util.List;
@@ -16,6 +17,11 @@ public class Service {
     public void prepareGame(String bridgeSizeInput) {
         int bridgeSize = converter.toIntFromString(bridgeSizeInput);
         List<String> answerBridge = bridgeMaker.makeBridge(bridgeSize);
-        game = new BridgeGame(answerBridge);
+        User user = new User();
+        game = new BridgeGame(answerBridge, user);
+    }
+
+    public void move(String moveInput) {
+
     }
 }
