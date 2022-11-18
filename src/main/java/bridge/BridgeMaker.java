78511be0eb5739ffd5fc1b66e7aeba30ea654497
김선동 <mok03127@gmail.com>
2022-11-18 19:09:25 +0900

    refactor: 입력받은 길이가 3부터 20사이의 숫자인지 확인하고 아니면 예외 발생하는 코드 InputView로 이동

--- src/main/java/bridge/BridgeMaker.java
+++ src/main/java/bridge/BridgeMaker.java
@@ -10,9 +10,6 @@ public class BridgeMaker {
     private static final int MOVE_DOWN_NUMBER = 0;
     private static final String MOVE_UP = "U";
     private static final String MOVE_DOWN = "D";
-    private static final int MIN_SIZE = 3;
-    private static final int MAX_SIZE = 20;
-    private static final String BRIDGE_SIZE_ERROR = "[ERROR] 다리 길이는 3부터 20 사이의 숫자여야 합니다.";
 
     private final BridgeNumberGenerator bridgeNumberGenerator;
 
@@ -25,7 +22,7 @@ public class BridgeMaker {
      * @return 입력받은 길이에 해당하는 다리 모양. 위 칸이면 "U", 아래 칸이면 "D"로 표현해야 한다.
      */
     public List<String> makeBridge(int size) {
-        validateBridgeSize(size);
+        //validateBridgeSize(size);
         List<String> bridge = new ArrayList<>();
         for (int i = 0; i < size; i++) {
             if (bridgeNumberGenerator.generate() == MOVE_DOWN_NUMBER) {
@@ -35,10 +32,5 @@ public class BridgeMaker {
             bridge.add(MOVE_UP);
         }
         return bridge;
-    }
-    private void validateBridgeSize(int size) {
-        if (size < MIN_SIZE || size > MAX_SIZE) {
-            throw new IllegalArgumentException(BRIDGE_SIZE_ERROR);
-        }
     }
 }
--- src/main/java/bridge/InputView.java
+++ src/main/java/bridge/InputView.java
@@ -6,6 +6,8 @@ import camp.nextstep.edu.missionutils.Console;
  * 사용자로부터 입력을 받는 역할을 한다.
  */
 public class InputView {
+    private static final int MIN_SIZE = 3;
+    private static final int MAX_SIZE = 20;
     private static final String CHECK_NUMBER_REGEX = "^[0-9]*$";
     private static final String RESTART = "R";
     private static final String QUIT = "Q";
@@ -24,6 +26,7 @@ public class InputView {
         String bridgeSize = Console.readLine();
         try {
             isDigit(bridgeSize);
+            validateBridgeSize(Integer.parseInt(bridgeSize));
             return Integer.parseInt(bridgeSize);
         } catch (IllegalArgumentException e) {
             System.out.println(e.getMessage());
@@ -51,6 +54,12 @@ public class InputView {
     private static void isDigit(String input) {
         if(!input.matches(CHECK_NUMBER_REGEX)) {
             throw new IllegalArgumentException(TYPE_ERROR);
+        }
+    }
+
+    private void validateBridgeSize(int size) {
+        if (size < MIN_SIZE || size > MAX_SIZE) {
+            throw new IllegalArgumentException(BRIDGE_SIZE_ERROR);
         }
     }
 
