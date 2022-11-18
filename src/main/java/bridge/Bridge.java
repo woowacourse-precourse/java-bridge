a0c688b1390419913acce7f09e7f24f64671ca98
jthugg <jthug@naver.com>
2022-11-19 07:42:04 +0900

    Feat<Bridge>: 생성된 다리의 정보를 저장하는 기능 추가

---
+++ src/main/java/bridge/Bridge.java
@@ -0,0 +1,35 @@
+package bridge;
+
+import java.util.List;
+
+public class Bridge {
+	List<String> bridge;
+	List<String> upperBridge;
+	List<String> lowerBridge;
+
+	Bridge(List<String> bridge) {
+		this.bridge = bridge;
+		buildUpperLower();
+	}
+
+	private void buildUpperLower() {
+		for(String shape : bridge) {
+			up(shape);
+			down(shape);
+		}
+	}
+
+	private void up(String shape) {
+		if(shape.equals("U")) {
+			upperBridge.add("O");
+			lowerBridge.add("X");
+		}
+	}
+
+	private void down(String shape) {
+		if(shape.equals("D")) {
+			upperBridge.add("X");
+			lowerBridge.add("O");
+		}
+	}
+}
