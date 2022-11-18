df5944cd86aff6b22e14b5d4422efb36e83462b6
Soomin Lim <augustth511@gmail.com>
2022-11-18 17:48:19 +0900

    refactor(BridgeGame): domain 패키지로 이동

--- src/main/java/bridge/domain/BridgeGame.java
+++ src/main/java/bridge/domain/BridgeGame.java
@@ -1,6 +1,8 @@
-package bridge;
+package bridge.domain;
 
-import bridge.domain.Bridge;
+import bridge.BridgeMaker;
+import bridge.BridgeNumberGenerator;
+import bridge.BridgeRandomNumberGenerator;
 
 /**
  * 다리 건너기 게임을 관리하는 클래스
