39d4c89c03e52f77f16487b8341006bef01324ca
nylonmusk <106235834+nylonmusk@users.noreply.github.com>
2022-11-19 00:09:52 +0900

    style(BridgeGame): 메소드 위치 변경

--- src/main/java/model/BridgeGame.java
+++ src/main/java/model/BridgeGame.java
@@ -26,6 +26,18 @@ public class BridgeGame {
         return Collections.unmodifiableList(bridgeByUser);
     }
 
+    public void retry(List<String> bridge, List<String> upSide, List<String> downSide) {
+        String retryOrQuit = BridgeController.getGameCommand();
+        BridgeController.printRetryOrQuit(retryOrQuit);
+        if (isUserInputRetry(retryOrQuit)) {
+            countTryNumber();
+            checkNextPath(bridge, upSide, downSide);
+        }
+        if (isUserInputQuit(retryOrQuit)) {
+            BridgeController.printResult(upSide, downSide, tryNumber);
+        }
+    }
+
     private void addPathInBridgeByUser(List<String> bridge, List<String> bridgeByUser, String path) {
         if (isUserPathEqualsBridge(bridge, path)) {
             addCorrectPath(bridgeByUser, path);
@@ -35,32 +47,24 @@ public class BridgeGame {
         }
     }
 
-    public void countBridgeNumber() {
-        bridgeNumber++;
-    }
-
-    public void countTryNumber() {
-        tryNumber++;
-    }
-
-    public void addCorrectPath(List<String> isCorrectBridge, String path) {
+    private void addCorrectPath(List<String> isCorrectBridge, String path) {
         isCorrectBridge.add(path);
         isCorrectBridge.add(StringConstant.CORRECT_PATH.getConstant());
     }
 
-    public void addWrongPath(List<String> isCorrectBridge, String path) {
+    private void addWrongPath(List<String> isCorrectBridge, String path) {
         isCorrectBridge.add(path);
         isCorrectBridge.add(StringConstant.WRONG_PATH.getConstant());
     }
 
-    public boolean isUserPathEqualsBridge(List<String> bridge, String path) {
+    private boolean isUserPathEqualsBridge(List<String> bridge, String path) {
         return bridge.get(bridgeNumber).equals(path);
     }
 
-    public void checkNextPath(List<String> bridge, List<String> upSide, List<String> downSide) {
+    private void checkNextPath(List<String> bridge, List<String> upSide, List<String> downSide) {
         addCorrectResult(bridge, upSide, downSide);
         addWrongResult(bridge, upSide, downSide);
-        if (UserInputDone(bridge, upSide, downSide)) {
+        if (isUserInputDone(bridge, upSide, downSide)) {
             return;
         }
         checkNextPath(bridge, upSide, downSide);
@@ -78,7 +82,7 @@ public class BridgeGame {
         }
     }
 
-    private boolean UserInputDone(List<String> bridge, List<String> upSide, List<String> downSide) {
+    private boolean isUserInputDone(List<String> bridge, List<String> upSide, List<String> downSide) {
         if (isUserInputDone(bridge, upSide)) {
             BridgeController.printResult(upSide, downSide, tryNumber);
             return true;
@@ -86,7 +90,7 @@ public class BridgeGame {
         return false;
     }
 
-    public void addCorrectSymbolUpSideDownSide(List<String> bridge, List<String> upSide, List<String> downSide) {
+    private void addCorrectSymbolUpSideDownSide(List<String> bridge, List<String> upSide, List<String> downSide) {
         List<String> userMove = getUserMove(bridge);
         if (userMoveEqualsU(userMove) && userMoveCorrect(userMove)) {
             upSide.add(StringConstant.CORRECT_PATH.getConstant());
@@ -99,7 +103,7 @@ public class BridgeGame {
         BridgeController.printMap(upSide, downSide);
     }
 
-    public void addWrongSymbolUpSideDownSide(List<String> bridge, List<String> upSide, List<String> downSide) {
+    private void addWrongSymbolUpSideDownSide(List<String> bridge, List<String> upSide, List<String> downSide) {
         List<String> userMove = getUserMove(bridge);
         if (userMoveEqualsU(userMove) && userMoveWrong(userMove)) {
             upSide.add(StringConstant.WRONG_PATH.getConstant());
@@ -113,47 +117,43 @@ public class BridgeGame {
         retry(bridge, upSide, downSide);
     }
 
-    public boolean isUserInputDone(List<String> bridge, List<String> upSide) {
+    private boolean isUserInputDone(List<String> bridge, List<String> upSide) {
         return bridge.size() == upSide.size();
     }
 
-    public boolean userMoveEqualsU(List<String> userMove) {
+    private boolean userMoveEqualsU(List<String> userMove) {
         return userMove.get(GameConstant.FIRST_INDEX.getConstant()).equals(StringConstant.UP_SIDE.getConstant());
     }
 
-    public boolean userMoveEqualsD(List<String> userMove) {
+    private boolean userMoveEqualsD(List<String> userMove) {
         return userMove.get(GameConstant.FIRST_INDEX.getConstant()).equals(StringConstant.DOWN_SIDE.getConstant());
     }
 
-    public boolean userMoveCorrect(List<String> userMove) {
+    private boolean userMoveCorrect(List<String> userMove) {
         return userMove.get(GameConstant.SECOND_INDEX.getConstant()).equals(StringConstant.CORRECT_PATH.getConstant());
     }
 
-    public boolean userMoveWrong(List<String> userMove) {
+    private boolean userMoveWrong(List<String> userMove) {
         return userMove.get(GameConstant.SECOND_INDEX.getConstant()).equals(StringConstant.WRONG_PATH.getConstant());
     }
 
-    public List<String> getUserMove(List<String> bridge) {
+    private List<String> getUserMove(List<String> bridge) {
         return move(bridge, BridgeController.getMoving());
     }
 
-    public void retry(List<String> bridge, List<String> upSide, List<String> downSide) {
-        String retryOrQuit = BridgeController.getGameCommand();
-        BridgeController.printRetryOrQuit(retryOrQuit);
-        if (isUserInputRetry(retryOrQuit)) {
-            countTryNumber();
-            checkNextPath(bridge, upSide, downSide);
-        }
-        if (isUserInputQuit(retryOrQuit)) {
-            BridgeController.printResult(upSide, downSide, tryNumber);
-        }
-    }
-
-    public boolean isUserInputRetry(String retryOrQuit) {
+    private boolean isUserInputRetry(String retryOrQuit) {
         return retryOrQuit.equals(StringConstant.RETRY.getConstant());
     }
 
-    public boolean isUserInputQuit(String retryOrQuit) {
+    private boolean isUserInputQuit(String retryOrQuit) {
         return retryOrQuit.equals(StringConstant.QUIT.getConstant());
+    }
+
+    private void countBridgeNumber() {
+        bridgeNumber++;
+    }
+
+    private void countTryNumber() {
+        tryNumber++;
     }
 }
