f0f40dffc8d28ff4564d3565e7708c9aa5235715
skck0226 <skck0226@ajou.ac.kr>
2022-11-18 10:45:23 +0900

    [test] 다리길이 입력 성공 테스트 assert 변경

--- src/test/java/bridge/View/InputViewTest.java
+++ src/test/java/bridge/View/InputViewTest.java
@@ -15,18 +15,18 @@ class InputViewTest {
     @DisplayName("다리길이 입력 실패 케이스 테스트")
     @ParameterizedTest(name = "{index} {displayName} message={0}")
     @ValueSource(strings = {")", "ㅁ", "a", "0", "2", "21"})
-    void 다리길이_입력_실패케이스_테스트(String params) {
+    void 다리길이_입력_실패케이스_테스트(String bridge_size_input) {
         assertInputByMockingTest(()->{
                 assertThrows(IllegalArgumentException.class,()->inputView.readBridgeSize());
-        },params);
+        },bridge_size_input);
     }
 
     @DisplayName("다리길이 입력 성공 케이스 테스트")
     @ParameterizedTest(name = "{index} {displayName} message={0}")
     @ValueSource(strings = {"3","5","20"})
-    void 다리길이_입력_성공케이스_테스트(String params) {
+    void 다리길이_입력_성공케이스_테스트(String bridge_size_input) {
         assertInputByMockingTest(()->{
-            assertDoesNotThrow(()->inputView.readBridgeSize());
-        },params);
+            assertEquals(inputView.readBridgeSize(),Integer.parseInt(bridge_size_input));
+        },bridge_size_input);
     }
 }
