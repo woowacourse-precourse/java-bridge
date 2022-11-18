2d97f9fc29c07a893039500236202159d248e785
whibaek <jibaek0513@gmail.com>
2022-11-19 02:16:30 +0900

    test(BridgeMakerTest): 다리 생성 테스트

---
+++ src/test/java/bridge/BridgeMakerTest.java
@@ -0,0 +1,18 @@
+package bridge;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class BridgeMakerTest {
+
+    @DisplayName("다리 생성 테스트")
+    @Test
+    void makeBridgeTest() {
+        BridgeRandomNumberGenerator bridgeRandomNumberGenerator = new BridgeRandomNumberGenerator();
+        for(int i=0; i<5; i++)
+            System.out.println(bridgeRandomNumberGenerator.generate());
+
+        BridgeMaker bridgeMaker = new BridgeMaker(bridgeRandomNumberGenerator);
+        System.out.println(bridgeMaker.makeBridge(5));
+    }
+}
