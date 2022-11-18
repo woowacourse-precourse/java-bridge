af024d060f388a475017cebb8453a7e44bc25979
김태용 <amico741@naver.com>
2022-11-18 15:41:25 +0900

    test: 이동할 칸 입력 테스트 추가

--- src/test/java/bridge/InputTest.java
+++ src/test/java/bridge/InputTest.java
@@ -5,6 +5,8 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
 
 
@@ -36,5 +38,19 @@ public class InputTest {
     void 입력값이_다리길이범위_벗어나면_예외발생(String input) {
         assertThatIllegalArgumentException().isThrownBy(() -> inputView.validateIsLengthRange(input))
                 .withMessage("[ERROR] 다리 길이는 3부터 20 사이의 숫자여야 합니다.");
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"", " ", "a", "b", "1", "2", "UD"})
+    @DisplayName("사용자가 U or D가 아닌 다른 값을 입력하면 예외가 발생한다.")
+    void 입력값이_U_OR_D가_아닐때_예외발생(String input) {
+        assertThatIllegalArgumentException().isThrownBy(() -> inputView.validateDirection(input));
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"U", "D"})
+    @DisplayName("사용자가 U or D를 입력하면 해당 값을 Return한다.")
+    void 입력값이_U_OR_D일때_정상_Return_한다(String input) {
+        assertThat(inputView.readMoving(input)).isEqualTo(input);
     }
 }
