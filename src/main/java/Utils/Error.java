4a4ea4d645d4003aa6ea05e84920766f80d68a37
canbeyour <wuzucy@gmail.com>
2022-11-18 11:10:36 +0900

    feat: add ability to throw error according to message

---
+++ src/main/java/Utils/Error.java
@@ -0,0 +1,14 @@
+package Utils;
+
+import java.util.NoSuchElementException;
+
+public class Error {
+    public static void generate(Message message) {
+        try {
+            throw new IllegalArgumentException(message.getMessage());
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+            throw new NoSuchElementException();
+        }
+    }
+}
