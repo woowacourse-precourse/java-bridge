f4940429058cb5944dcc8c5f14fef4e6edfc7957
suheonjoo <suheonj95@naver.com>
2022-11-18 16:02:35 +0900

    feat(OutputView): 사용자가 걸어온 길 및 최종결과 출력 기능 추가

---
+++ src/main/java/bridge/util/OutputViewConst.java
@@ -0,0 +1,13 @@
+package bridge.util;
+
+public class OutputViewConst {
+	public static String USER_FINAL_RESULT_PHRASE = "최종 게임 결과";
+	public static String USER_FINAL_RESULT = "게임 성공 여부: ";
+	public static String USER_TRY_COUNT_PHRASE = "총 시도한 횟수: ";
+	public static String RESULT_PREFIX = "[ ";
+	public static String USER_SPACE = "O";
+	public static String USER_WRONG_SPACE = "X";
+	public static String BLANK_SPACE = " ";
+	public static String RESULT_PARTITION = " | ";
+	public static String RESULT_SUFFIX = " ]";
+}
--- src/main/java/bridge/view/OutputView.java
+++ src/main/java/bridge/view/OutputView.java
@@ -1,6 +1,9 @@
 package bridge.view;
 
+import java.util.List;
+
 import bridge.util.InputViewConst;
+import bridge.util.OutputViewConst;
 
 /**
  * 사용자에게 게임 진행 상황과 결과를 출력하는 역할을 한다.
@@ -16,7 +19,30 @@ public class OutputView {
 	 * <p>
 	 * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
 	 */
-	public void printMap() {
+	public void printMap(List<String> userMap, Integer bridgeSize) {
+		System.out.println(OutputViewConst.USER_FINAL_RESULT_PHRASE);
+		printBridge(userMap, bridgeSize, InputViewConst.MOVING_UP);
+		printBridge(userMap, bridgeSize, InputViewConst.MOVING_DOWN);
+	}
+
+	private void printBridge(List<String> userMap, Integer bridgeSize, String moving) {
+		System.out.print(OutputViewConst.RESULT_PREFIX);
+		for (int i = 0; i < bridgeSize; i++) {
+			if (userMap.get(i).equals(moving)) {
+				System.out.print(OutputViewConst.USER_SPACE);
+			}
+
+			//Todo: X 처리도 해야한다 -> 일단 어느정도 완성후에 할 것
+
+			if (!userMap.get(i).equals(moving)) {
+				System.out.print(OutputViewConst.BLANK_SPACE);
+			}
+
+			if (i != bridgeSize - 1) {
+				System.out.print(OutputViewConst.RESULT_PARTITION);
+			}
+		}
+		System.out.println(OutputViewConst.RESULT_SUFFIX);
 	}
 
 	/**
@@ -24,7 +50,9 @@ public class OutputView {
 	 * <p>
 	 * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
 	 */
-	public void printResult() {
+	public void printResult(String result, Integer count) {
+		System.out.println(OutputViewConst.USER_FINAL_RESULT + result);
+		System.out.println(OutputViewConst.USER_TRY_COUNT_PHRASE + count); // 여기서 string.format 이용해서 출력받기
 	}
 
 	public void printError(String errorMessage) {
