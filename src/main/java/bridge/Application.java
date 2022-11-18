6a4748be747e34e5e9620f74fb7f369ce7c3e985
K-Diger <kitten.diger@gmail.com>
2022-11-18 21:03:30 +0900

    fix(InputView) : 사용자가 잘 못 입력했을 때 예외처리 후 다시 입력받을 수 없던 버그 수정

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -5,10 +5,6 @@ public class Application {
     public static void main(String[] args) {
         final ContextFactory contextFactory = new ContextFactory();
         final BridgeGameStarter bridgeGameStarter = contextFactory.getBridgeGameStarter();
-        try {
-            bridgeGameStarter.run();
-        } catch (IllegalArgumentException exception) {
-            System.out.println(exception.getMessage());
-        }
+        bridgeGameStarter.run();
     }
 }
--- src/main/java/bridge/InputView.java
+++ src/main/java/bridge/InputView.java
@@ -18,26 +18,44 @@ public class InputView {
      * 다리의 길이를 입력받는다.
      */
     public int readBridgeSize() {
-        String input = Console.readLine();
-        inputValidator.bridgeSize(input);
-        return Integer.parseInt(input);
+        String inputBridgeSize = "";
+        try {
+            inputBridgeSize = Console.readLine();
+            inputValidator.bridgeSize(inputBridgeSize);
+        } catch (IllegalArgumentException exception) {
+            System.out.println(exception.getMessage());
+            return readBridgeSize();
+        }
+        return Integer.parseInt(inputBridgeSize);
     }
 
     /**
      * 사용자가 이동할 칸을 입력받는다.
      */
     public String readMoving() {
-        String input = Console.readLine();
-        inputValidator.moving(input);
-        return input;
+        String inputMoving = "";
+        try {
+            inputMoving = Console.readLine();
+            inputValidator.moving(inputMoving);
+        } catch (IllegalArgumentException exception) {
+            System.out.println(exception.getMessage());
+            return readMoving();
+        }
+        return inputMoving;
     }
 
     /**
      * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
      */
     public String readGameCommand() {
-        String input = Console.readLine();
-        inputValidator.gameCommand(input);
-        return input;
+        String inputGameCommand = "";
+        try {
+            inputGameCommand = Console.readLine();
+            inputValidator.gameCommand(inputGameCommand);
+        } catch (IllegalArgumentException exception) {
+            System.out.println(exception.getMessage());
+            readGameCommand();
+        }
+        return inputGameCommand;
     }
 }
