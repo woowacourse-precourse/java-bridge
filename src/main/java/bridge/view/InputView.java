fe22f6a4884c575f41fa1ea51dcf5310e09bc01c
splitube <voicesplit@daum.net>
2022-11-18 15:10:37 +0900

    feat(InputView): 게임 재시도 여부 입력 기능 구현

--- src/main/java/bridge/view/InputView.java
+++ src/main/java/bridge/view/InputView.java
@@ -3,9 +3,6 @@ package bridge.view;
 import camp.nextstep.edu.missionutils.Console;
 import bridge.validate.Validator;
 
-/**
- * 사용자로부터 입력을 받는 역할을 한다.
- */
 public class InputView {
     public int readBridgeSize() {
         String bridgeSize = Console.readLine();
@@ -13,17 +10,16 @@ public class InputView {
         Validator.isProperBridgeSize(bridgeSize);
         return Integer.parseInt(bridgeSize);
     }
-    
+
     public String readMoving() {
         String move = Console.readLine();
         Validator.isMove(move);
         return move;
     }
 
-    /**
-     * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
-     */
     public String readGameCommand() {
-        return null;
+        String gameCommand = Console.readLine();
+        Validator.isProperGameCommand(gameCommand);
+        return gameCommand;
     }
 }
