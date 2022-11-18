6ccdd4441690b747864745cabd165f61ba9ae262
splitube <voicesplit@daum.net>
2022-11-18 15:12:23 +0900

    feat(ErrorMessage): 게임 재시도 여부 입력 에러 메시지 추가

--- src/main/java/bridge/validate/ErrorMessage.java
+++ src/main/java/bridge/validate/ErrorMessage.java
@@ -5,7 +5,8 @@ public enum ErrorMessage {
     INPUT_NOT_A_NUMBER("입력값이 숫자가 아닙니다."),
     NUMBER_OUT_OF_RANGE("올바르지 않은 범위의 숫자입니다."),
     PROPER_BRIDGE_LENGTH("( 다리 길이는 3이상 20이하 )"),
-    NOT_PROPER_MOVE("'U', 'D' 중에 입력해주세요.");
+    NOT_PROPER_MOVE("'U', 'D' 중에 입력해주세요."),
+    NOT_PROPER_GAME_COMMAND("'R', 'Q' 중에 입력해주세요.");
 
     public final String message;
 
