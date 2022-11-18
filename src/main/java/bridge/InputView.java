e8920956040ddefd7fd57791913910c605dc8ef9
OhJintaek <sunshot@hanyang.ac.kr>
2022-11-18 19:18:08 +0900

    type:feat 사용자가 이동할 칸을 입력받는 기능 추가

--- src/main/java/bridge/InputView.java
+++ src/main/java/bridge/InputView.java
@@ -13,6 +13,8 @@ public class InputView {
         String input = Console.readLine();
         int size = Integer.valueOf(input);
 
+        //에러 판독
+
         return size;
     }
 
@@ -20,7 +22,11 @@ public class InputView {
      * 사용자가 이동할 칸을 입력받는다.
      */
     public String readMoving() {
-        return null;
+        String input = Console.readLine();
+
+        //에러 판독
+
+        return input;
     }
 
     /**
