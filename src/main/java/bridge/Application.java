720dc94a52288bd1f2122feba946c0d539cc9a5c
K-Diger <kitten.diger@gmail.com>
2022-11-18 19:39:53 +0900

    chore : 클라이언트 코드 개선

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -1,19 +1,12 @@
 package bridge;
 
 public class Application {
-    private final static ContextFactory contextFactory = new ContextFactory();
-    private final static BridgeGameStarter bridgeGameStarter = contextFactory.getBridgeGameStarter();
-    private static int tryCount = 1;
+
     public static void main(String[] args) {
+        final ContextFactory contextFactory = new ContextFactory();
+        final BridgeGameStarter bridgeGameStarter = contextFactory.getBridgeGameStarter();
         try {
-            bridgeGameStarter.initialize();
-            boolean retryFlag = bridgeGameStarter.run();
-            while (retryFlag) {
-                bridgeGameStarter.reInitialize();
-                retryFlag = bridgeGameStarter.run();
-                tryCount ++;
-            }
-            bridgeGameStarter.closeGame(tryCount);
+            bridgeGameStarter.run();
         } catch (IllegalArgumentException exception) {
             System.out.println(exception.getMessage());
         }
