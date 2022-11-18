5d5994d566d926043a1f39c203c3d15a636e63d3
60jong <rudwhd515@gmail.com>
2022-11-18 17:15:39 +0900

    feat(BridgeMaker) : 다리 길이에 맞게 1 = U / 0 = D에 맞게 U / D 리스트를 생성하는 기능 구현

--- src/main/java/bridge/BridgeMaker.java
+++ src/main/java/bridge/BridgeMaker.java
@@ -1,6 +1,10 @@
 package bridge;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
+
+import static bridge.BlockNumber.*;
 
 /**
  * 다리의 길이를 입력 받아서 다리를 생성해주는 역할을 한다.
@@ -18,6 +22,15 @@ public class BridgeMaker {
      * @return 입력받은 길이에 해당하는 다리 모양. 위 칸이면 "U", 아래 칸이면 "D"로 표현해야 한다.
      */
     public List<String> makeBridge(int size) {
-        return null;
+        List<String> bridgeBlocks = new ArrayList<>();
+        while (bridgeBlocks.size() < size) {
+            bridgeBlocks.add(getBridgeBlock());
+        }
+        return bridgeBlocks;
+    }
+
+    public String getBridgeBlock() {
+        return BlockNumber.findByNumber(bridgeNumberGenerator.generate())
+                .getBlock();
     }
 }
