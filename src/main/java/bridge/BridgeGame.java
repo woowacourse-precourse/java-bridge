83850f3d68027640814859f0a945afb98a4d8a1e
dongjun0128 <70315475+dongjun0128@users.noreply.github.com>
2022-11-18 17:04:35 +0900

    feat : 유저의 입력에 따라 움직이는 기능 추가

--- src/main/java/bridge/BridgeGame.java
+++ src/main/java/bridge/BridgeGame.java
@@ -9,16 +9,33 @@ package bridge;
  * InputView, OutputView 를 사용하지 않는다.
  */
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * 다리 건너기 게임을 관리하는 클래스
  */
 public class BridgeGame {
+    private final List<String> bridge;
+    private List<String> userMoving;
+
+    public BridgeGame(List<String> bridge) {
+        this.bridge = bridge;
+        this.userMoving = new ArrayList<>();
+    }
+
     /**
      * 사용자가 칸을 이동할 때 사용하는 메서드
      * <p>
      * 이동을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void move() {
+    public boolean move(String moving) {
+        if (moving.equals(bridge.get(userMoving.size()))) {
+            userMoving.add(moving);
+            return true;
+        }
+
+        return false;
     }
 
     /**
@@ -27,5 +44,9 @@ public class BridgeGame {
      * 재시작을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
     public void retry() {
+    }
+
+    public List<String> getBridge() {
+        return bridge;
     }
 }
