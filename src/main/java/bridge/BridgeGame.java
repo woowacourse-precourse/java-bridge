f8b24042ec33f1f4a778bdd197e540fbcacd1f44
lee2963 <sodel2963@naver.com>
2022-11-18 18:43:04 +0900

    다리 이동 기능 구현

--- src/main/java/bridge/BridgeGame.java
+++ src/main/java/bridge/BridgeGame.java
@@ -1,17 +1,72 @@
 package bridge;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * 다리 건너기 게임을 관리하는 클래스
  */
 public class BridgeGame {
 
-    /**
-     * 사용자가 칸을 이동할 때 사용하는 메서드
-     * <p>
-     * 이동을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
-     */
-    public void move() {
+    private int gameCnt = 1;
+    private boolean gameSuccess = true;
+
+    public List<String> getBridgeRoad() {
+        return bridgeRoad;
     }
+
+    private final List<String> bridgeRoad;
+    private final List<String> upMove = new ArrayList<>();
+    private final List<String> downMove = new ArrayList<>();
+
+    public List<String> getUpMove() {
+        return upMove;
+    }
+
+    public List<String> getDownMove() {
+        return downMove;
+    }
+
+    public BridgeGame(List<String> bridgeRoad) {
+        this.bridgeRoad = bridgeRoad;
+    }
+
+    public boolean move(String move) {
+        boolean chk = true;
+
+        if (move.equals("U"))
+            chk = confirmUpMove();
+
+        if (move.equals("D"))
+            chk = confirmDownMove();
+
+        return chk;
+    }
+
+    public boolean confirmUpMove() {
+        int size = upMove.size();
+        downMove.add("   ");
+
+        if (bridgeRoad.get(size).equals("U")) {
+            upMove.add(" O ");
+            return true;
+        }
+        upMove.add(" X ");
+        return false;
+    }
+
+    public boolean confirmDownMove() {
+        int size = downMove.size();
+        upMove.add("   ");
+
+        if (bridgeRoad.get(size).equals("D")) {
+            downMove.add(" O ");
+            return true;
+        }
+        downMove.add(" X ");
+        return false;
+    }
+
 
     /**
      * 사용자가 게임을 다시 시도할 때 사용하는 메서드
--- src/main/java/bridge/OutputView.java
+++ src/main/java/bridge/OutputView.java
@@ -5,12 +5,41 @@ package bridge;
  */
 public class OutputView {
 
-    /**
-     * 현재까지 이동한 다리의 상태를 정해진 형식에 맞춰 출력한다.
-     * <p>
-     * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
-     */
-    public void printMap() {
+    public void printGameStart() {
+        System.out.println("다리 건너기 게임을 시작합니다.");
+    }
+
+    public void printBridgeSizeToInput() {
+        System.out.println("다리의 길이를 입력해주세요.");
+    }
+
+    public void printPlayerMove() {
+        System.out.println("이동할 칸을 선택해주세요. (위: U, 아래: D)");
+    }
+
+    public void printMap(BridgeGame bridgeGame) {
+        printUpMap(bridgeGame);
+        printDownMap(bridgeGame);
+    }
+
+    public void printUpMap(BridgeGame bridgeGame) {
+        System.out.print("[");
+        for (int i = 0; i < bridgeGame.getUpMove().size(); i++) {
+            System.out.print(bridgeGame.getUpMove().get(i));
+            if (i + 1 < bridgeGame.getUpMove().size())
+                System.out.print("|");
+        }
+        System.out.println("]");
+    }
+
+    public void printDownMap(BridgeGame bridgeGame) {
+        System.out.print("[");
+        for (int i = 0; i < bridgeGame.getDownMove().size(); i++) {
+            System.out.print(bridgeGame.getDownMove().get(i));
+            if (i + 1 < bridgeGame.getDownMove().size())
+                System.out.print("|");
+        }
+        System.out.println("]");
     }
 
     /**
