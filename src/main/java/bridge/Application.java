21a781167a8900bb16aa3776ce2e52230202b5a7
JUNG-jihoon <73165763+JUNG-jihoon@users.noreply.github.com>
2022-11-18 19:21:11 +0900

    Feat : "추가 다리 생성"
    BridgeMaker에서 인자들을 받아 1일때 U를 0일 때 D를 리스트에 저장하는 함수를 구현하였습니다. Application.java에 가져와 실행시켰습니다.

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -1,8 +1,22 @@
 package bridge;
 
+
+import static bridge.BridgeGame.Bridge_U;
+
 public class Application {
+    static InputView input = new InputView();
+    static BridgeRandomNumberGenerator bridgeRandomNumberGenerator = new BridgeRandomNumberGenerator();
+    static BridgeMaker maker = new BridgeMaker(bridgeRandomNumberGenerator);
 
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        int size = input.readBridgeSize();
+        Bridge_Make(size);
+    }
+
+    public static void Bridge_Make(int size) {
+        BridgeRandomNumberGenerator bridgeRandomNumberGenerator = new BridgeRandomNumberGenerator();
+        BridgeMaker maker = new BridgeMaker(bridgeRandomNumberGenerator);
+        Bridge_U = maker.makeBridge(size);
     }
 }
