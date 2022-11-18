62b63d417ea5e78b1e9b5fc59ebccb8bc8528423
BioBeBe <76714219+JaegeonYu@users.noreply.github.com>
2022-11-18 14:25:32 +0900

    feat: makeBridge 메소드 작성

--- src/main/java/bridge/enums/IntEnum.java
+++ src/main/java/bridge/enums/IntEnum.java
@@ -2,7 +2,9 @@ package bridge.enums;
 
 public enum IntEnum {
     BRIDGE_START(3),
-    BRIDGE_LAST(20);
+    BRIDGE_LAST(20),
+    UP_INT(1),
+    DOWN_INT(0);
 
     private final int num;
     private IntEnum(int num){
--- src/main/java/bridge/util/BridgeMaker.java
+++ src/main/java/bridge/util/BridgeMaker.java
@@ -1,6 +1,13 @@
 package bridge.util;
 
+import bridge.enums.IntEnum;
+import bridge.enums.StringEnum;
+
+import java.util.ArrayList;
 import java.util.List;
+
+import static bridge.enums.IntEnum.*;
+import static bridge.enums.StringEnum.*;
 
 /**
  * 다리의 길이를 입력 받아서 다리를 생성해주는 역할을 한다.
@@ -17,8 +24,14 @@ public class BridgeMaker {
      * @param size 다리의 길이
      * @return 입력받은 길이에 해당하는 다리 모양. 위 칸이면 "U", 아래 칸이면 "D"로 표현해야 한다.
      */
-    public List<String> makeBridge(int size) {
-        return null;
+    public List<String> makeBridge(int bridgeSize) {
+        List<String> bridge = new ArrayList<>();
+        for (int bridgeLength = 0; bridgeLength < bridgeSize; bridgeLength++) {
+            int randomNumber = bridgeNumberGenerator.generate();
+            if (randomNumber == UP_INT.num()) bridge.add(UP.key());
+            if (randomNumber == DOWN_INT.num()) bridge.add(DOWN.key());
+        }
+        return bridge;
     }
 
 }
