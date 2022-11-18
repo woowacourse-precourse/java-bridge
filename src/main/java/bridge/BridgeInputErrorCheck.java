9aa79562ab17feb3fa525c73d05e7c28a3e6c508
Seokhyun Lee <lseokhyun99@gmail.com>
2022-11-18 15:10:09 +0900

    feat: 이동할 칸 입력 오류 체크 메소드 추가
    이동할 칸 입력 시 오류를 체크하는 메소드
    readMovingErrorCheck()를 구현하였다.
    입력된 String이 "U", "D"이 아니면 에러처리한다.

--- src/main/java/bridge/BridgeInputErrorCheck.java
+++ src/main/java/bridge/BridgeInputErrorCheck.java
@@ -6,9 +6,12 @@ public class BridgeInputErrorCheck {
     private static final String ERROR_MESSAGE = "[ERROR]";
     private static final String NOT_NUMBER_ERROR = ERROR_MESSAGE+" 숫자를 입력해야 합니다.";
     private static final String NUMBER_SIZE_ERROR = ERROR_MESSAGE+" 다리 길이는 3부터 20 사이의 숫자여야 합니다.";
+    private static final String MOVING_SPACE_ERROR = ERROR_MESSAGE+" 이동할 칸은 U 혹은 D 중에서 선택해야 합니다.";
     private static final String NUMBER_PATTERN = "^[0-9]{1,}$";
     private static final int BRIDGE_SIZE_MIN = 3;
     private static final int BRIDGE_SIZE_MAX = 20;
+    private static final String MOVE_UP = "U";
+    private static final String MOVE_DOWN = "D";
 
 
     public static void readBridgeSizeErrorCheck(String bridgeSize){
@@ -19,5 +22,9 @@ public class BridgeInputErrorCheck {
         int size = Integer.parseInt(bridgeSize);
         if( size < BRIDGE_SIZE_MIN || size > BRIDGE_SIZE_MAX)
             throw new IllegalArgumentException(NUMBER_SIZE_ERROR);
+    }
+    public static void readMovingErrorCheck(String movingSpace){
+        if ( !movingSpace.equals(MOVE_UP) && !movingSpace.equals(MOVE_DOWN))
+            throw new IllegalArgumentException(MOVING_SPACE_ERROR);
     }
 }
