efc5be7c8dbc1e65064832636d6f72d2bc5f1e04
koo9b9h <90121024+koo9b9h@users.noreply.github.com>
2022-11-18 22:10:43 +0900

    feat : InputView클래스 메소드에 입력값과 반환값 추가

--- src/main/java/bridge/presentation/InputView.java
+++ src/main/java/bridge/presentation/InputView.java
@@ -1,28 +1,21 @@
 package bridge.presentation;
 
-/**
- * 사용자로부터 입력을 받는 역할을 한다.
- */
+import camp.nextstep.edu.missionutils.Console;
+
 public class InputView {
 
-    /**
-     * 다리의 길이를 입력받는다.
-     */
     public int readBridgeSize() {
-        return 0;
-    }
+        int bridgeSize = Integer.parseInt(Console.readLine());
+        return bridgeSize;
+    }//readBridgeSize
 
-    /**
-     * 사용자가 이동할 칸을 입력받는다.
-     */
     public String readMoving() {
-        return null;
-    }
+        String movement = Console.readLine();
+        return movement;
+    }//readMoving
 
-    /**
-     * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
-     */
     public String readGameCommand() {
-        return null;
-    }
-}
+        String command = Console.readLine();
+        return command;
+    }//readGameCommand
+}// end class
