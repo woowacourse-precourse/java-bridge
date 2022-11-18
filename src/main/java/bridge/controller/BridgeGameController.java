36344295bb7fe512663cdd9e7b3b674d95888e54
SuhHyeonjun <dmsgktn0208@gmail.com>
2022-11-18 21:34:17 +0900

    feat : 게임 재시작 메서드 추가

--- src/main/java/bridge/controller/BridgeGameController.java
+++ src/main/java/bridge/controller/BridgeGameController.java
@@ -1,5 +1,7 @@
 package bridge.controller;
 
+import static bridge.UpDownBridge.initBridge;
+
 import bridge.BridgeGame;
 import bridge.BridgeMaker;
 import bridge.BridgeRandomNumberGenerator;
@@ -10,37 +12,57 @@ import bridge.OutputView;
 import java.util.List;
 
 public class BridgeGameController {
+    public static List<String> bridges;
+    public static int inputSize;
+    private static final InputView inputView = new InputView();
+    private static final OutputView outputView = new OutputView();
+    private static final BridgeGame bridgeGame = new BridgeGame();
+    private static final BridgeMaker bridgeMaker = new BridgeMaker(new BridgeRandomNumberGenerator());
     private static boolean status = true;
     private static int bridgeLocation = 0;
-    private final InputView inputView = new InputView();
-    private final OutputView outputView = new OutputView();
-    private final BridgeGame bridgeGame = new BridgeGame();
-    private final BridgeMaker bridgeMaker = new BridgeMaker(new BridgeRandomNumberGenerator());
+    private static int count = 1;
 
     public void start() {
         System.out.println(Message.START_GAME.getMessage());
-        int inputSize = inputView.inputBridgeSize();
-        List<String> bridges = bridgeMaker.makeBridge(inputSize);
+        inputSize = inputView.inputBridgeSize();
+        bridges = bridgeMaker.makeBridge(inputSize);
         System.out.println(bridges);
-        while (status && bridgeLocation < inputSize) {
-            String inputDirection = inputView.inputMovingDirection();
-            String moveResult = bridgeGame.move(bridges.get(bridgeLocation), inputDirection);
-            getCurrentState(moveResult);
-            outputView.printMap(inputDirection, moveResult);
-            bridgeLocation++;
-        }
+        setGame();
         outputView.printResult();
     }
 
-    public static void getCurrentState(String moveResult) {
-        if (moveResult.contains(Message.MOVE_FAIL.getMessage())) {
+    public static void setGame() {
+        while (status && bridgeLocation < inputSize) {
+            String inputDirection = inputView.inputMovingDirection();
+            String moveResult = bridgeGame.move(bridges.get(bridgeLocation), inputDirection);
+            setCurrentState(moveResult);
+            outputView.printMap(inputDirection, moveResult);
+            bridgeGame.retry(moveResult);
+            bridgeLocation++;
+        }
+    }
+
+    public static void setCurrentState(String moveResult) {
+        if (moveResult.equals(Message.MOVE_FAIL.getMessage())) {
             status = false;
-        } else if (moveResult.contains(Message.MOVE_SUCCESS.getMessage())) {
+        } else if (moveResult.equals(Message.MOVE_SUCCESS.getMessage())) {
             status = true;
         }
     }
 
+    public static void restartGame() {
+        bridgeLocation = 0;
+        count++;
+        initBridge();
+        status = true;
+        setGame();
+    }
+
     public static boolean getStatus() {
         return status;
+    }
+
+    public static int getCount() {
+        return count;
     }
 }
