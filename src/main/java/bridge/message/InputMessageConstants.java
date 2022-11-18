6ecaa48542be18f94cabac996241ca9b48e64480
kimjeonghan <[rlawjdgks318@naver.com]>
2022-11-18 16:51:10 +0900

    feat(InputMessageConstans): 사용자 입력 메시지 상수 정의 추가

---
+++ src/main/java/bridge/message/InputMessageConstants.java
@@ -0,0 +1,8 @@
+package bridge.message;
+
+public interface InputMessageConstants {
+
+	String BRIDGE_SIZE_MESSAGE = "다리의 길이를 입력해주세요.\n";
+	String GAME_COMMAND_MESSAGE_FORMAT = "이동할 칸을 선택해주세요. (위: %s, 아래: %s)\n";
+	String RESTART_OR_EXIT_MESSAGE_FORMAT = "게임을 다시 시도할지 여부를 입력해주세요. (재시도: %s, 종료: %s)\n";
+}
