7691afb16b4b11bb88dfdefd667a710e9a71f6de
SeokHwan An <asok369258@naver.com>
2022-11-18 21:43:22 +0900

    feat : 다리의 길이 입력값이 3이상 20이하인지 검증하는 기능 추가

--- src/main/java/bridge/validation/Validation.java
+++ src/main/java/bridge/validation/Validation.java
@@ -10,4 +10,12 @@ public class Validation {
         }
     }
 
+    // 다리의 길이 입력값이 3이상 20이하인지 검증하는 기능
+    public static void checkBridgeLengthRange3To20(String bridgeLengthInput) {
+        int bridgeLength = Integer.parseInt(bridgeLengthInput);
+        if (!(BRIDGE_LENGTH_MIN <= bridgeLength && bridgeLength <= BRIDGE_LENGTH_MAX)) {
+            throw new IllegalArgumentException(ERROR_MESSAGE_BRIDGE_LENGTH_RANGE_3_TO_20);
+        }
+    }
+
 }
