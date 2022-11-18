7aad64df6aee7c72c67e06865044d27421dbac90
nswon <swnam0729@gmail.com>
2022-11-18 18:42:05 +0900

    fix: 다리 객체 삭제

--- src/main/java/bridge/domain/Bridges.java
+++ src/main/java/bridge/domain/Bridges.java
@@ -1,27 +1,11 @@
 package bridge.domain;
 
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
 
 public class Bridges {
-    private final List<Bridge> bridges;
+    private List<String> bridges;
 
-    public Bridges() {
-        this.bridges = new ArrayList<>();
-    }
-
-    public void generate(List<String> realBridges) {
-        realBridges.forEach(bridge -> bridges.add(Bridge.create(bridge)));
-    }
-
-    public boolean isGameClear() {
-        return bridges.stream().allMatch(Bridge::isCrossed);
-    }
-
-    public Optional<Bridge> findByPositionToMove(int position) {
-        return bridges.stream()
-                .filter(bridge -> bridges.indexOf(bridge) == position)
-                .findAny();
+    public void generate(List<String> bridges) {
+        this.bridges = bridges;
     }
 }
