c0ec68257a166a22a6974c23aa216a263d7c3f70
dongjun0128 <70315475+dongjun0128@users.noreply.github.com>
2022-11-18 17:13:32 +0900

    feat : 움직이는 기능 추가

--- src/main/java/bridge/controller/BridgeController.java
+++ src/main/java/bridge/controller/BridgeController.java
@@ -1,20 +1,29 @@
 package bridge.controller;
 
-import bridge.InputView;
-import bridge.OutputView;
+import bridge.*;
 
 public class BridgeController {
     private OutputView outputView = new OutputView();
     private InputView inputView = new InputView();
+    private BridgeNumberGenerator bridgeNumberGenerator = new BridgeRandomNumberGenerator();
+    private BridgeMaker bridgeMaker = new BridgeMaker(bridgeNumberGenerator);
+    private BridgeGame bridgeGame;
 
-    public void startGame(){
+    public void startGame() {
         outputView.printStartingPhrase();
-
+        bridgeGame = new BridgeGame(bridgeMaker.makeBridge(inputView.readBridgeSize()));
+        //System.out.println(bridgeGame.getBridge());
+        startMove();
     }
 
-    public void inputBridgeSize(){
-        inputView.readBridgeSize();
-
+    public void startMove() {
+        while (true) {
+            if (bridgeGame.move(inputView.readMoving()) == false) {
+                break;
+            }
+            if(bridgeGame.isEndOfBridge() == true){
+                break;
+            }
+        }
     }
-
 }
