3f546e6457dff8ef001e27e8226a761a8e934357
1eejisoo <jisu991012@gmail.com>
2022-11-19 02:28:18 +0900

    feat: 재시작 기능 추가

---
+++ src/main/java/bridge/RetryCode.java
@@ -0,0 +1,16 @@
+package bridge;
+
+public enum RetryCode {
+    RETRY("R"),
+    QUIT("Q");
+
+    private final String retryCode;
+
+    RetryCode(String retryCode) {
+        this.retryCode = retryCode;
+    }
+
+    public String getRetryCode() {
+        return retryCode;
+    }
+}
