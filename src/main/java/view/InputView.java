8531ad3872a374df3e64b25f863c20e795cd731e
jeongwle <dldl4568@naver.com>
2022-11-18 18:43:16 +0900

    feat: 다리의 길이를 입력받는 기능

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -1,5 +1,13 @@
 package view;
 
+import static bridge.enums.Regex.BRIDGE_SIZE_REGEX;
+import static bridge.enums.ErrorMessages.BRIDGE_SIZE_ERROR_MESSAGE;
+import static bridge.enums.Range.STARTING_POINT_INCLUSIVE;
+import static bridge.enums.Range.ENDING_POINT_INCLUSIVE;
+
+import camp.nextstep.edu.missionutils.Console;
+import java.util.regex.Pattern;
+
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
  */
@@ -9,7 +17,9 @@ public class InputView {
      * 다리의 길이를 입력받는다.
      */
     public int readBridgeSize() {
-        return 0;
+        String size = Console.readLine();
+        validateSize(size);
+        return toInt(size);
     }
 
     /**
@@ -24,5 +34,22 @@ public class InputView {
      */
     public String readGameCommand() {
         return null;
+    }
+
+    private void validateSize(String size) {
+        if (size.length() > 2 || !Pattern.matches(BRIDGE_SIZE_REGEX.toString(), size)
+                || !isValidateSize(size)) {
+            throw new IllegalArgumentException(BRIDGE_SIZE_ERROR_MESSAGE.toString());
+        }
+    }
+
+    private boolean isValidateSize(String size) {
+        int bridgeSize = toInt(size);
+        return (bridgeSize >= STARTING_POINT_INCLUSIVE.getPoint()
+                && bridgeSize <= ENDING_POINT_INCLUSIVE.getPoint());
+    }
+
+    private int toInt(String input) {
+        return Integer.parseInt(input);
     }
 }
