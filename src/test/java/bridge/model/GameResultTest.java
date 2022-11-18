25a0f13d1aa18d744c6493d8e87ae7913ceec4a3
junpakPark <112045553+junpakPark@users.noreply.github.com>
2022-11-18 14:05:46 +0900

    test(GameResultTest): GameResult 객체 테스트 코드 작성
    GameResultTest 주요 목록:
    - getNumberOfAttempts 메서드 호출 시  addNumberOfAttepts의 호출 횟수를 리턴하는지 확인한다.
    - getResult 메서드 호출 시 최종적으로 changeResult한 결과를 리턴하는 지 확인한다.

---
+++ src/test/java/bridge/model/GameResultTest.java
@@ -0,0 +1,126 @@
+package bridge.model;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+
+class GameResultTest {
+	@DisplayName("addNumberOfAttempts의 호출 수 만큼 numberOfAttempts가 변화한다.")
+	@Nested
+	class GetNumberOfAttemptsTest {
+		@Test
+		void case1() {
+			int count = 1;
+
+			GameResult gameResult = new GameResult();
+
+			for (int i = 0; i < count; i++) {
+				gameResult.addNumberOfAttempts();
+			}
+			assertEquals(gameResult.getNumberOfAttempts(), count);
+		}
+
+		@Test
+		void case2() {
+			int count = 100;
+
+			GameResult gameResult = new GameResult();
+
+			for (int i = 0; i < count; i++) {
+				gameResult.addNumberOfAttempts();
+			}
+			assertEquals(gameResult.getNumberOfAttempts(), count);
+		}
+
+		@Test
+		void case3() {
+			int count = 4;
+
+			GameResult gameResult = new GameResult();
+
+			for (int i = 0; i < count; i++) {
+				gameResult.addNumberOfAttempts();
+			}
+			assertEquals(gameResult.getNumberOfAttempts(), count);
+		}
+
+		@Test
+		void case4() {
+			int count = 7;
+
+			GameResult gameResult = new GameResult();
+
+			for (int i = 0; i < count; i++) {
+				gameResult.addNumberOfAttempts();
+			}
+			assertEquals(gameResult.getNumberOfAttempts(), count);
+		}
+	}
+
+	@DisplayName("최종적으로 changeResult한 결과를 리턴한다.")
+	@Nested
+	class ChangeResultTest {
+		@Test
+		void case1() {
+			GameResult gameResult = new GameResult();
+
+			gameResult.changeResultToSuccess();
+
+			assertEquals(gameResult.getResult(), "성공");
+		}
+
+		@Test
+		void case2() {
+			GameResult gameResult = new GameResult();
+
+			gameResult.changeResulToFail();
+
+			assertEquals(gameResult.getResult(), "실패");
+		}
+
+		@Test
+		void case3() {
+			GameResult gameResult = new GameResult();
+
+			gameResult.changeResultToSuccess();
+			gameResult.changeResultToSuccess();
+
+			assertEquals(gameResult.getResult(), "성공");
+		}
+
+		@Test
+		void case4() {
+			GameResult gameResult = new GameResult();
+
+			gameResult.changeResulToFail();
+			gameResult.changeResulToFail();
+
+			assertEquals(gameResult.getResult(), "실패");
+		}
+
+		@Test
+		void case5() {
+			GameResult gameResult = new GameResult();
+
+			gameResult.changeResultToSuccess();
+			gameResult.changeResultToSuccess();
+			gameResult.changeResulToFail();
+
+			assertEquals(gameResult.getResult(), "실패");
+		}
+
+		@Test
+		void case6() {
+			GameResult gameResult = new GameResult();
+
+			gameResult.changeResulToFail();
+			gameResult.changeResulToFail();
+			gameResult.changeResultToSuccess();
+
+			assertEquals(gameResult.getResult(), "성공");
+		}
+	}
+
+}
