b4d7529dc286563fbea60346c09df52f01cc513f
egoing <dudqk9696@naver.com>
2022-11-18 22:37:45 +0900

    test : 이동할 칸 입력 예외 처리

--- src/test/java/bridge/InputTest.java
+++ src/test/java/bridge/InputTest.java
@@ -5,38 +5,39 @@ import domain.AllBridge;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeTest;
 import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class InputTest extends NsTest {
+    private static final String ERROR_MESSAGE = "[ERROR]";
+
     @DisplayName("다리 길이로 문자가 입력되었을때")
     @Test
     void 다리길이_문자입력() {
         assertSimpleTest(() -> {
             runException("1a");
-            assertThat(output()).contains("[ERROR]");
+            assertThat(output()).contains(ERROR_MESSAGE);
         });
     }
 
-    @DisplayName("다리 길이로 2가 입력되었을 때")
+    @DisplayName("다리 길이로 3~20이 아닐때")
     @Test
     void 다리길이_2_입력() {
         assertSimpleTest(() -> {
             runException("2");
-            assertThat(output()).contains("[ERROR]");
+            assertThat(output()).contains(ERROR_MESSAGE);
         });
     }
 
-    @DisplayName("다리 길이로 21이 입력되었을 때")
     @Test
-    void 다리길이_21_입력() {
-        assertSimpleTest(() -> {
-            runException("21");
-            assertThat(output()).contains("[ERROR]");
-        });
+    void 잘못된_이동할칸_입력() {
+        assertRandomNumberInRangeTest(() -> {
+            run("3", "A", "D", "U");
+            assertThat(output()).contains(ERROR_MESSAGE);
+        }, 1, 0, 1);
     }
-
 
     @Override
     protected void runMain() {
