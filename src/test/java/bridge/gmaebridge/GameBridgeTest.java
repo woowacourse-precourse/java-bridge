babdfe12d96c550fdfbdd7fb9c9adfaa84b1236e
kimminsu <xjfcnfw3@naver.com>
2022-11-18 13:01:38 +0900

    test: GameBridge에 move 입력 테스트

--- src/test/java/bridge/gmaebridge/GameBridgeTest.java
+++ src/test/java/bridge/gmaebridge/GameBridgeTest.java
@@ -9,6 +9,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Stream;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
@@ -24,6 +25,24 @@ public class GameBridgeTest {
         Bridge answerBridge = new Bridge(inputMoves);
         Bridge bridge = gameBridge.getPlayerBridge().getBridge();
         assertThat(bridge.getSquares()).isEqualTo(answerBridge.getSquares());
+    }
+
+    @DisplayName("사용자 입력 결과 참인 테스트")
+    @Test
+    void checkInsertedMoveTest() {
+        GameBridge gameBridge = new GameBridge();
+        gameBridge.generateAnswerBridge(new Bridge(List.of("U", "D", "U", "U", "D")));
+        insertMovesForTest(List.of("U", "D"), gameBridge);
+        assertThat(gameBridge.insertMove(new Move("U"))).isEqualTo(true);
+    }
+
+    @DisplayName("사용자 입력 결과 거짓인 테스트")
+    @Test
+    void checkInsertedWrongMoveTest() {
+        GameBridge gameBridge = new GameBridge();
+        gameBridge.generateAnswerBridge(new Bridge(List.of("U", "D", "U", "U", "D")));
+        insertMovesForTest(List.of("U", "D"), gameBridge);
+        assertThat(gameBridge.insertMove(new Move("D"))).isEqualTo(false);
     }
 
     private static Stream<Arguments> generateTestMoveInput() {
