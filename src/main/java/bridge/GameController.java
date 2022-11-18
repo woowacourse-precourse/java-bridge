0f1c5008d0b3b5f23b753a92d00367b5178f7ca6
tdmdgh <tdmdgh@gmail.com>
2022-11-18 17:16:55 +0800

    style(bridge): GameController와 Message에 comment 추가

--- src/main/java/bridge/GameController.java
+++ src/main/java/bridge/GameController.java
@@ -1,5 +1,7 @@
 package bridge;
-
+/**
+ * 게임의 진행을 관리하는 역할을 한다.
+ */
 public class GameController {
     public static void begin(){
         OutputView.gameStartMessage();
--- src/main/java/bridge/Message.java
+++ src/main/java/bridge/Message.java
@@ -1,5 +1,7 @@
 package bridge;
-
+/**
+ * 출력할 메시지를 저장하는 역할을 한다.
+ */
 public enum Message {
     Game_Start("다리 건너기 게임을 시작합니다.");
     private String message;
