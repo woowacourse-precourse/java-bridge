619eca4fefa2f9dc4c136a7dcb8b2c6be9c9d292
bjk1649 <qhwns1649@naver.com>
2022-11-18 20:58:00 +0900

    fix: 예외가 발생했을 때 메시지를 예외 안에서 가져올 수 있도록 변경

--- src/main/java/bridge/validator/InputValidator.java
+++ src/main/java/bridge/validator/InputValidator.java
@@ -9,29 +9,25 @@ public class InputValidator {
     public static void validateBridgeSize(String input) throws IllegalArgumentException {
         String condition = "^[0-9]+$";
         if (!Pattern.matches(condition, input)) {
-            System.out.println("[ERROR] 다리의 길이는 3에서 20 사이의 정수입니다.");
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException("[ERROR] 다리의 길이는 3에서 20 사이의 정수입니다.");
         }
         int bridgeSize = Integer.parseInt(input);
         if ((bridgeSize < BRIDGE_SIZE_MIN) || (bridgeSize > BRIDGE_SIZE_MAX)) {
-            System.out.println("[ERROR] 다리의 길이는 3에서 20 사이의 정수입니다.");
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException("[ERROR] 다리의 길이는 3에서 20 사이의 정수입니다.");
         }
     }
 
     public static void validateMoving(String input) throws IllegalArgumentException {
         String condition = "^[UD]{1}$";
         if (!Pattern.matches(condition, input)) {
-            System.out.println("[ERROR] 이동할 칸은 공백없이 'U' 또는 'D' 이어야 합니다.");
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException("[ERROR] 이동할 칸은 공백없이 'U' 또는 'D' 이어야 합니다.");
         }
     }
 
     public static void validateGameCommand(String input) throws IllegalArgumentException {
         String condition = "^[RQ]{1}$";
         if (!Pattern.matches(condition, input)) {
-            System.out.println("[ERROR] 재시작 여부는 'R' 또는 'Q' 이어야 합니다.");
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException("[ERROR] 재시작 여부는 'R' 또는 'Q' 이어야 합니다.");
         }
     }
 }
