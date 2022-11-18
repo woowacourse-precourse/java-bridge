371c53cb25f2495b15abf949a2df1f56ead36e65
jungle <minuet1215@jbnu.ac.kr>
2022-11-18 19:15:07 +0900

    Feat(BridgeConstant): UP_NUMBER, DOWN_NUMBER 숫자 교체

--- src/main/java/bridge/util/BridgeConstant.java
+++ src/main/java/bridge/util/BridgeConstant.java
@@ -2,10 +2,10 @@ package bridge.util;
 
 public class BridgeConstant {
     public static final String ERROR_PREFIX = "[ERROR] ";
-    public static final int UP = 0;
-    public static final int DOWN = 1;
-    public static final String UP_KEY = "U";
+    public static final int DOWN = 0;
+    public static final int UP = 1;
     public static final String DOWN_KEY = "D";
+    public static final String UP_KEY = "U";
     public static final String FALL_POSITION = "X";
     public static final String GAME_RETRY_KEY = "R";
     public static final String GAME_QUIT_KEY = "Q";
