62423759cbd3ade179119ae3449c1d3c26d2557c
JJ <jeongjae1996@naver.com>
2022-11-19 03:05:00 +0900

    feat: 게임 시작을 담당하는 과정 구현
    안내 메시지를 출력하고, 다리를 생성하는 과정까지를 구현했다.

--- src/main/java/bridge/GamePlay.java
+++ src/main/java/bridge/GamePlay.java
@@ -1,5 +1,7 @@
 package bridge;
 
+import java.util.List;
+
 public class GamePlay {
     private final InputView inputView;
     private final BridgeMaker bridgeMaker;
@@ -7,5 +9,15 @@ public class GamePlay {
     public GamePlay() {
         this.inputView = new InputView();
         this.bridgeMaker = new BridgeMaker(new BridgeRandomNumberGenerator());
+    }
+
+    private List<String> start() {
+        final String EVENT_MESSAGE = "다리 건너기 게임을 시작합니다.\n\n다리의 길이를 입력해주세요.";
+
+        System.out.println(EVENT_MESSAGE);
+        int bridgeSize = this.inputView.readBridgeSize();
+        List<String> bridge = this.bridgeMaker.makeBridge(bridgeSize);
+
+        return bridge;
     }
 }
