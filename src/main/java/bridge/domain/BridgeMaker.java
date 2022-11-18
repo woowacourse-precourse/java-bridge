401fbd899b4809a0e1639db82b6ef7fac9c86e40
Choi-JY <Choi-JY1107@github.com>
2022-11-18 23:43:00 +0900

    feat: BridegeMaker 다리 생성 메소드 구현

--- src/main/java/bridge/domain/BridgeMaker.java
+++ src/main/java/bridge/domain/BridgeMaker.java
@@ -1,11 +1,15 @@
 package bridge.domain;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
  * 다리의 길이를 입력 받아서 다리를 생성해주는 역할을 한다.
  */
 public class BridgeMaker {
+    private static final String UPPER_CELL = "U";
+    private static final String DOWN_CELL = "D";
+    private static final Integer UPPER_CELL_TO_INT = 1;
 
     private final BridgeNumberGenerator bridgeNumberGenerator;
 
@@ -18,6 +22,18 @@ public class BridgeMaker {
      * @return 입력받은 길이에 해당하는 다리 모양. 위 칸이면 "U", 아래 칸이면 "D"로 표현해야 한다.
      */
     public List<String> makeBridge(int size) {
-        return null;
+        List<String> bridge = new ArrayList<>();
+        for (int brideIndex = 0; brideIndex < size; ++brideIndex) {
+            bridge.add(toStringBridge());
+        }
+
+        return bridge;
+    }
+
+    private String toStringBridge() {
+        if (bridgeNumberGenerator.generate() == UPPER_CELL_TO_INT) {
+            return UPPER_CELL;
+        }
+        return DOWN_CELL;
     }
 }
