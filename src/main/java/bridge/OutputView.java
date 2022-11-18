ce6789a7159a5cab1a4d970a97ff493e49e33c6c
Polite-Jehu <werber112@kyonggi.ac.kr>
2022-11-18 14:13:04 +0900

    refactor(bridge): remove inputView.readBridgeSize() call
    remove inputView.readBridgeSize() call
    cause inputView call only Application

--- src/main/java/bridge/OutputView.java
+++ src/main/java/bridge/OutputView.java
@@ -7,9 +7,8 @@ public class OutputView {
     public void printStart(){
         System.out.print("다리 건너기 게임을 시작합니다.\n");
     }
-    public void printInputBridgeLength(InputView inputView){
+    public void printInputBridgeLength(){
         System.out.print("다리의 길이를 입력해주세요.\n");
-        inputView.readBridgeSize();
     }
     /**
      * 현재까지 이동한 다리의 상태를 정해진 형식에 맞춰 출력한다.
