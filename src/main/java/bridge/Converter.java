718580e736d726388f794e8fa44cabada8f15fad
Amyhds <dasomhan524@gmail.com>
2022-11-18 17:04:37 +0900

    feat(converter): 문자열을 정수로 변환하는 메서드 구현

--- src/main/java/bridge/Converter.java
+++ src/main/java/bridge/Converter.java
@@ -1,4 +1,12 @@
 package bridge;
 
 public class Converter {
+
+    public static int convertToNumber(String userInput) {
+        return Integer.parseInt(userInput);
+    }
+
+    public static char convertToLetter(String userInput){
+
+    }
 }
