e2e6b72aaa0e5189fff8d350fd74d416a2729ce0
inhwanK <79566221+inhwanK@users.noreply.github.com>
2022-11-18 23:33:17 +0900

    feat(Application): 게임 시작 및 이동할 칸 입력 받기 구현

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -1,8 +1,28 @@
 package bridge;
 
+import bridge.view.InputView;
+import bridge.view.OutputView;
+
 public class Application {
+    private static InputView input = new InputView();
+    private static OutputView output = new OutputView();
+    private static int bridgeSize;
 
     public static void main(String[] args) {
+        try {
+            // 게임 시작
+            output.printStartMessage();
+            output.printBridgeLengthInputRequestMessage();
+            bridgeSize = input.readBridgeSize();
+
+            System.out.println(bridgeSize);
+
+            output.printMoveSpaceInputRequestMessage();
+            input.readMoving();
+        } catch (IllegalArgumentException e) {
+            System.out.printf("[ERROR] %s", e.getMessage());
+        }
+
         // TODO: 프로그램 구현
     }
 }
--- src/main/java/bridge/view/OutputView.java
+++ src/main/java/bridge/view/OutputView.java
@@ -22,14 +22,14 @@ public class OutputView {
     }
 
     public void printStartMessage() {
-        System.out.println("다리 건너기 게임을 시작합니다.");
+        System.out.println("다리 건너기 게임을 시작합니다.\n");
     }
 
     public void printBridgeLengthInputRequestMessage() {
         System.out.println("다리의 길이를 입력해주세요.");
     }
 
-    public void printMoveBlockInputRequestMessage() {
+    public void printMoveSpaceInputRequestMessage() {
         System.out.println("이동할 칸을 선택해주세요. (위: U, 아래:D)");
     }
 
