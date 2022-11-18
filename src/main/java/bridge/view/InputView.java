96d94f9a834cf6009a9f753c512f46a299a3e065
sosow0212 <sosow0212@naver.com>
2022-11-18 15:15:33 +0900

    feat : (InputView) 다리의 길이를 입력 받는 메소드 구현

--- src/main/java/bridge/view/InputView.java
+++ src/main/java/bridge/view/InputView.java
@@ -1,4 +1,7 @@
-package bridge;
+package bridge.view;
+
+import bridge.util.BridgeInputConverter;
+import camp.nextstep.edu.missionutils.Console;
 
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
@@ -9,7 +12,12 @@ public class InputView {
      * 다리의 길이를 입력받는다.
      */
     public int readBridgeSize() {
-        return 0;
+        try {
+            String bridgeSize = Console.readLine();
+            return BridgeInputConverter.bridgeSizeConverter(bridgeSize);
+        } catch (IllegalArgumentException exception) {
+            throw new IllegalArgumentException(exception.getMessage());
+        }
     }
 
     /**
