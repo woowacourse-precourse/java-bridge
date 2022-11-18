f4628aebc2bcb7f615322bdb9e9c7f9cdf77cfd5
Seiyon Park <seiyonpdev@gmail.com>
2022-11-19 00:58:59 +0900

    test(ValidationTest): 테스트 ValidationTest 작성
    다리 길이 입력시 예외 처리에 대한 테스트를 작성했다

---
+++ src/test/java/bridge/ValidationTest.java
@@ -0,0 +1,32 @@
+package bridge;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+class ValidationTest {
+    @DisplayName("다리 길이 입력에서 숫자가 아닌 것을 입력할 경우")
+    @ParameterizedTest
+    @CsvSource({"U","UD"})
+    void validate_notNum(String notNum) {
+        assertThatThrownBy(() -> Validation.validateBridgeSize(notNum))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("다리 길이 입력에서 3 미만 테스트")
+    @Test
+    void validate_under3() {
+        assertThatThrownBy(() -> Validation.validateBridgeSize("2"))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("다리 길이 입력에서 20 초과 테스트")
+    @Test
+    void validate_over20() {
+        assertThatThrownBy(() -> Validation.validateBridgeSize("21"))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+}
