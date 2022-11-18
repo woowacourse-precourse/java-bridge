707f8fcc6225d05f83ae1af04b3c831ce1ca0e57
qudtjs0753 <qudtjs0753@naver.com>
2022-11-18 18:14:01 +0900

    refactor(constant): 다리 길이와 관련된 상수명 변경
    INPUT_LENGTH_OF_BRIDGE에서 길이에 대한 변수명을 통일하기 위해 INPUT_SIZE_OF_BRIDGE로 이름을 변경한다.

--- src/main/java/bridge/constant/Directive.java
+++ src/main/java/bridge/constant/Directive.java
@@ -2,7 +2,7 @@ package bridge.constant;
 
 public enum Directive {
 
-    INPUT_LENGTH_OF_BRIDGE("다리의 길이를 입력해주세요.");
+    INPUT_SIZE_OF_BRIDGE("다리의 길이를 입력해주세요.");
 
     private final String message;
 
