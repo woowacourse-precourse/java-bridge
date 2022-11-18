be8387b5e9decb1c19bc59e5970d61234cc48a18
jjongwa <troas96@naver.com>
2022-11-18 22:48:45 +0900

    refactor: 에러 발생 후 재입력하도록 로직 변경

--- src/main/java/bridge/InputView.java
+++ src/main/java/bridge/InputView.java
@@ -13,7 +13,17 @@ public class InputView {
      * 다리의 길이를 입력받는다.
      */
     public int readBridgeSize() {
-        String input = Console.readLine();
+        while (true) {
+            try {
+                String input = Console.readLine();
+                return checkBridgeSize(input);
+            } catch (IllegalArgumentException exception) {
+                System.out.println("[ERROR] " + exception.getMessage());
+            }
+        }
+    }
+
+    private int checkBridgeSize(String input) {
         int inputNum = checkSizeIsNumber(input);
         checkSizeIsInRange(inputNum);
         checkFirstNumberIsZero(input);
@@ -45,9 +55,15 @@ public class InputView {
      * 사용자가 이동할 칸을 입력받는다.
      */
     public String readMoving() {
-        String input = Console.readLine();
-        checkMoveCommand(input);
-        return input;
+        while (true) {
+            try {
+                String input = Console.readLine();
+                checkMoveCommand(input);
+                return input;
+            } catch (IllegalArgumentException exception) {
+                System.out.println("[ERROR] " + exception.getMessage());
+            }
+        }
     }
 
     private void checkMoveCommand(String input) {
@@ -60,9 +76,15 @@ public class InputView {
      * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
      */
     public String readGameCommand() {
-        String input = Console.readLine();
-        checkGameCommand(input);
-        return input;
+        while (true) {
+            try {
+                String input = Console.readLine();
+                checkGameCommand(input);
+                return input;
+            } catch (IllegalArgumentException exception) {
+                System.out.println("[ERROR] " + exception.getMessage());
+            }
+        }
     }
 
     private void checkGameCommand(String input) {
