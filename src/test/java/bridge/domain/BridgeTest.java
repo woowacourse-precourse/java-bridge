011d15494ee8baaada7fbdedb6dab0e23df75387
pilyang <30036534+pilyang@users.noreply.github.com>
2022-11-19 01:09:28 +0900

    test(Bridge): 추가된 기능 테스트 추가
    - checkBridgTileAt test 추가
    - getBridgeSize test 추가

--- src/test/java/bridge/domain/BridgeTest.java
+++ src/test/java/bridge/domain/BridgeTest.java
@@ -5,10 +5,12 @@ import bridge.BridgeRandomNumberGenerator;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.List;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class BridgeTest {
@@ -33,5 +35,18 @@ class BridgeTest {
                 .hasMessageContaining(EXCEPTION_MESSAGE);
     }
 
+    @ParameterizedTest(name = "다리 타일 체크 테스트 [{index}] : 테스트 결과 반환 - {2}")
+    @CsvSource(value = {"3,DOWN,true", "1,UP,false"})
+    void checkBridgeTileAtTest(int positionIndex, Tile tile, boolean expectedResult) {
+        Bridge bridge = Bridge.from(List.of("U", "D", "U", "D"));
+        assertThat(bridge.checkBridgeTileAt(positionIndex, tile)).isEqualTo(expectedResult);
+    }
+
+    @DisplayName("다리 사이즈 반환 테스트")
+    @Test
+    void bridgeSizeTest() {
+        Bridge bridge = Bridge.from(List.of("U", "D", "U"));
+        assertThat(bridge.getBridgeSize()).isEqualTo(3);
+    }
 
 }
