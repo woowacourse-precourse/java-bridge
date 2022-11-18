275165b84a629fe5bb88d46fb87a78d99a617327
RO-OR <kyk3531@gmail.com>
2022-11-18 21:08:39 +0900

    feat($view) : OutputView클래스에 printMap메소드의 기능을 추가 하였습니다.
    파라미터로 받은 리스트를 사용하여 upPrintMap과 downPrintMap 메소드를 호출하도록 하였습니다.
    
    OutputView클래스에 printMap메소드의 기능을 추가하였습니다.

--- src/main/java/bridge/view/OutputView.java
+++ src/main/java/bridge/view/OutputView.java
@@ -15,8 +15,9 @@ public class OutputView {
      * <p>
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void printMap() {
-
+    public void printMap(List<String> bridgeView) {
+        upPrintMap(bridgeView);
+        downPrintMap(bridgeView);
     }
 
     public void upPrintMap(List<String> bridgeView) {
