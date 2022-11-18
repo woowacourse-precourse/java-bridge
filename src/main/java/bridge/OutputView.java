53c8dec4d4d11cb335a33477bed7646a572e7007
SuhHyeonjun <dmsgktn0208@gmail.com>
2022-11-18 14:48:13 +0900

    feat : 총 시도한 횟수 출력하는 메서드

--- src/main/java/bridge/OutputView.java
+++ src/main/java/bridge/OutputView.java
@@ -3,11 +3,13 @@ package bridge;
 import static bridge.UpDownBridge.downBridge;
 import static bridge.UpDownBridge.makeUpDownBridge;
 import static bridge.UpDownBridge.upBridge;
+import static bridge.controller.BridgeGameController.getStatus;
 
 /**
  * 사용자에게 게임 진행 상황과 결과를 출력하는 역할을 한다.
  */
 public class OutputView {
+    private static int count = 0;
 
     /**
      * 현재까지 이동한 다리의 상태를 정해진 형식에 맞춰 출력한다.
@@ -17,6 +19,7 @@ public class OutputView {
     public void printMap(String inputDirection, String moveResult) {
         makeUpDownBridge(inputDirection, moveResult);
         printProgress();
+        System.out.println();
     }
 
     public static void printProgress() {
@@ -29,10 +32,23 @@ public class OutputView {
      * <p>
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public static void printResult(boolean keepGoing) {
+    public void printResult() {
         System.out.println(Message.GAME_RESULT.getMessage());
+        printProgress();
+        System.out.println();
+        System.out.println(getSuccessOrFailure());
+        count++;
+        printTotalCount(count);
+    }
+
+    private static String getSuccessOrFailure() {
+        if (getStatus()) {
+            return Message.GAME_SUCCESS.getMessage();
+        }
+        return Message.GAME_FAILURE.getMessage();
     }
 
     public static void printTotalCount(int count) {
+        System.out.print(Message.TRY_ATTEMPTS.getMessage() + count);
     }
 }
