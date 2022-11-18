41f0e29682b0486b44dd4c6f02a8d10e03ac54a8
tseop <77163842+tseop@users.noreply.github.com>
2022-11-19 01:45:01 +0900

    feat: Application 기능 1차 구현
    - 기능 구현만 진행
    - 요구사항에 맞춰 리팩토링 진행 필요

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -1,8 +1,55 @@
 package bridge;
 
+import bridge.game.BridgeGame;
+import bridge.message.GameMessage;
+import bridge.validation.InputValidation;
+import bridge.view.InputView;
+import bridge.view.OutputView;
+import java.util.List;
+import java.util.StringJoiner;
+
 public class Application {
 
+    private static final InputValidation inputValidation = new InputValidation();
+    private static final InputView inputView = new InputView();
+    private static BridgeGame game;
+
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+
+        do {
+            System.out.println(GameMessage.START_MSG);
+            game = BridgeGame.getInstance();
+        } while (game.move());
     }
+
+    public static String sizeValidation() {
+        try {
+            String input = inputView.readBridgeSize();
+            inputValidation.bridgeSizeValidation(input);
+            return input;
+        } catch (IllegalArgumentException e) {
+            return sizeValidation();
+        }
+    }
+
+    public static String moveValidation() {
+        try {
+            String input = inputView.readMoving();
+            inputValidation.bridgeMoveValidation(input);
+            return input;
+        } catch (IllegalArgumentException e) {
+            return moveValidation();
+        }
+    }
+
+    public static String retryValidation() {
+        try {
+            String input = inputView.readGameCommand();
+            inputValidation.retryValidation(input);
+            return input;
+        } catch (IllegalArgumentException e) {
+            return retryValidation();
+        }
+    }
+
 }
