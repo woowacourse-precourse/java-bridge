23d165a7a4e4a5e381f4fc723eee52afe39a116e
semi-cloud <sksmsrkdtpal@naver.com>
2022-11-18 23:12:02 +0900

    test(Bridge): 다리 건널 수 있는지 판별하는 기능 테스트

---
+++ src/test/java/bridge/domain/BridgeTest.java
@@ -0,0 +1,40 @@
+package bridge.domain;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class BridgeTest {
+
+    private Bridge bridge;
+
+    @BeforeEach
+    void init() {
+        List<String> bridgeList = List.of("U", "U", "D", "U");
+        bridge = Bridge.valueOf(bridgeList);
+    }
+
+    @DisplayName("다리를 건널 수 있다면 true 를 반환한다.")
+    @Test
+    void moveSuccess() {
+        Square square = new Square("U");
+        int position = 0;
+
+        boolean move = bridge.canMoveForward(square, position);
+        assertThat(move).isTrue();
+    }
+
+    @DisplayName("다리를 건널 수 없다면 false 를 반환한다.")
+    @Test
+    void moveFail() {
+        Square square = new Square("D");
+        int position = 0;
+
+        boolean move = bridge.canMoveForward(square, position);
+        assertThat(move).isFalse();
+    }
+}
