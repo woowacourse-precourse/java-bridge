951695d543c19528e42dfb4337fd2577cae8ab6d
dev_kong <ahdjd5@gmail.com>
2022-11-18 17:18:08 +0900

    feat(User): create user class

---
+++ src/main/java/bridge/domain/User.java
@@ -0,0 +1,17 @@
+package bridge.domain;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class User {
+    List<String> userMoves = new ArrayList<>();
+
+    public void move(String moveInput) {
+
+    }
+
+    public int getMoveCount() {
+        return userMoves.size();
+    }
+
+}
