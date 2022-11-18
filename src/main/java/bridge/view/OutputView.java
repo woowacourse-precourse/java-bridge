a231ef5dde64e741c6c808eaae493a725ff0abf1
BioBeBe <76714219+JaegeonYu@users.noreply.github.com>
2022-11-18 17:05:00 +0900

    feat: lastIndexOutput 메소드 작성

--- src/main/java/bridge/view/OutputView.java
+++ src/main/java/bridge/view/OutputView.java
@@ -1,20 +1,68 @@
 package bridge.view;
 
-import bridge.enums.StringEnum;
+import java.util.List;
 
+import static bridge.enums.IntEnum.*;
 import static bridge.enums.StringEnum.*;
 
 /**
  * 사용자에게 게임 진행 상황과 결과를 출력하는 역할을 한다.
  */
 public class OutputView {
+    private final List<String> bridge;
+    private int stage;
+
+    public OutputView(List<String> bridge) {
+        this.bridge = bridge;
+        this.stage = RESET_STAGE.num();
+    }
 
     /**
      * 현재까지 이동한 다리의 상태를 정해진 형식에 맞춰 출력한다.
      * <p>
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void printMap() {
+    public String printMap(int result) {
+        StringBuilder upPrint = new StringBuilder("[");
+        StringBuilder downPrint = new StringBuilder("[");
+
+        upPrint.append("]");
+        downPrint.append("]");
+        stage++;
+        upPrint.append("\n").append(downPrint);
+        return upPrint.toString();
+    }
+
+
+    private void lastIndexOutput(int result, StringBuilder upPrint, StringBuilder downPrint) {
+        if (result == GAME_WIN.num() || result == GAME_CONTINUE.num()) {
+            continuePrint(stage, upPrint, downPrint);
+        }
+        if (result == GAME_LOSE.num()) {
+            failPrint(upPrint, downPrint);
+        }
+    }
+
+    private void failPrint(StringBuilder upPrint, StringBuilder downPrint) {
+        if (bridge.get(stage).equals(UP.key())) {
+            upPrint.append("  ");
+            downPrint.append(" x ");
+        }
+        if (bridge.get(stage).equals(DOWN.key())) {
+            upPrint.append(" x ");
+            downPrint.append("   ");
+        }
+    }
+
+    private void continuePrint(int duringIndex, StringBuilder upPrint, StringBuilder downPrint) {
+        if (bridge.get(duringIndex).equals(UP.key())) {
+            upPrint.append(" o ");
+            downPrint.append("   ");
+        }
+        if (bridge.get(duringIndex).equals(DOWN.key())) {
+            upPrint.append("   ");
+            downPrint.append(" o ");
+        }
     }
 
     /**
@@ -40,4 +88,5 @@ public class OutputView {
     public void printAskLength() {
         System.out.println(ASK_BRIDGE_LENGTH.key());
     }
+
 }
