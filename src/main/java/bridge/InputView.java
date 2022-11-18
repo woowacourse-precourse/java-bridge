3872e734ccf9305ce7960146a350e0119c876be1
minaver <psmjoshua@gmail.com>
2022-11-18 10:19:32 +0900

    feat(InputView): 다리 길이 입력 및 Validation 메서드 추가

--- src/main/java/bridge/InputView.java
+++ src/main/java/bridge/InputView.java
@@ -1,5 +1,11 @@
 package bridge;
 
+import camp.nextstep.edu.missionutils.Console;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.List;
+
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
  */
@@ -9,7 +15,11 @@ public class InputView {
      * 다리의 길이를 입력받는다.
      */
     public int readBridgeSize() {
-        return 0;
+        System.out.println("다리의 길이를 입력해주세요.");
+        String input = Console.readLine();
+        validateSizeType(input);
+        validateSizeRange(input);
+        return Integer.parseInt(input);
     }
 
     /**
@@ -25,4 +35,28 @@ public class InputView {
     public String readGameCommand() {
         return null;
     }
+
+    /**
+     * @param input 사용자에게 입력받는 다리의 길이
+     */
+    private void validateSizeType(String input) {
+        List<String> inputSplit = Arrays.asList(input.split(""));
+        boolean typeValid = inputSplit.stream().anyMatch(i -> Character.isDigit(i.charAt(0)));
+        if (typeValid == false) {
+            System.out.println("[ERROR] 입력한 size의 타입이 유효하지 않습니다.");
+            throw new IllegalArgumentException();
+        }
+    }
+
+    /**
+     * @param input 사용자에게 입력받는 다리의 길이
+     */
+    private void validateSizeRange(String input) {
+        int inputNumber = Integer.parseInt(input);
+        if (inputNumber < 3 || inputNumber > 20) {
+            System.out.println("[ERROR] 입력한 size의 범위가 유효하지 않습니다.");
+            throw new IllegalArgumentException();
+        }
+    }
+
 }
