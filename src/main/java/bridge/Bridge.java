feb8b4d7588b388344100a9e8a260f6bbfde0971
kariskan <zkfzpf56@naver.com>
2022-11-18 19:07:22 +0900

    feat: 다리 검증기 작성
    1. 올바른 다리 사이즈(3이상 20이하)인지
    
    2. "U" 또는 "D" 이외의 문자만 있는지

--- src/main/java/bridge/Bridge.java
+++ src/main/java/bridge/Bridge.java
@@ -1,5 +1,8 @@
 package bridge;
 
+import bridge.utils.constants.Comments;
+import bridge.utils.constants.Commons;
+import bridge.utils.constants.ErrorMessages;
 import java.util.List;
 
 public class Bridge {
@@ -7,6 +10,7 @@ public class Bridge {
     List<String> bridge;
 
     public Bridge(List<String> bridge) {
+        validate(bridge);
         this.bridge = bridge;
     }
 
@@ -17,4 +21,25 @@ public class Bridge {
     public int getBridgeSize() {
         return bridge.size();
     }
+
+    private void validate(List<String> bridge) {
+        if (isNotValidBridge(bridge)) {
+            throw new IllegalArgumentException(ErrorMessages.INVALID_BRIDGE.getMessage());
+        }
+        if (isNotValidBridgeSize(bridge)) {
+            throw new IllegalArgumentException(ErrorMessages.INVALID_BRIDGE_SIZE.getMessage());
+        }
+    }
+
+    private boolean isNotValidBridgeSize(List<String> bridge) {
+        return !(bridge.size() >= Commons.MIN_BRIDGE_RANGE.getNumber()
+                && bridge.size() <= Commons.MAX_BRIDGE_RANGE.getNumber());
+    }
+
+    private static boolean isNotValidBridge(List<String> bridge) {
+        return !(bridge.stream()
+                .allMatch((val) -> Comments.UP.getComment().equals(val) || Comments.DOWN.getComment().equals(val)));
+    }
+
+
 }
