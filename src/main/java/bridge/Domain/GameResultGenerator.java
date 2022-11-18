41502bf33850cfc0cf48d8f39d865a39c2d677f1
Park shinyeong <syngh503@gmail.com>
2022-11-18 15:25:03 +0900

    Feat(GameResultGenerator): 게임 결과 세팅 메소드 생성
    게임 결과에 따라 O, X,  를 추가하는 setUpperBridgeResult와 setLoserBridgeResult 를 생성하였습니다.

---
+++ src/main/java/bridge/Domain/GameResultGenerator.java
@@ -0,0 +1,40 @@
+package bridge.Domain;
+
+import bridge.EnumCollections.BridgeResultType;
+import bridge.EnumCollections.BridgeType;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class GameResultGenerator {
+    private List<String> upperBridge;
+    private List<String> lowerBridge;
+
+    public GameResultGenerator() {
+        this.upperBridge = new ArrayList<>();
+        this.lowerBridge = new ArrayList<>();
+    }
+
+    public void setUpperBridgeResult(GameResult gameResult) {
+        String upperResult = BridgeResultType.NONE.getResult();
+        if (gameResult.isGameSuccess() && gameResult.isPositionUpper()) {
+            upperResult = BridgeResultType.POSSIBLE.getResult();
+        } else if (!gameResult.isGameSuccess() && gameResult.isPositionUpper()) {
+            upperResult = BridgeResultType.IMPOSSIBLE.getResult();
+        }
+        this.upperBridge.add(upperResult);
+    }
+
+    public void setLowerBridgeResult(GameResult gameResult) {
+        String lowerResult = BridgeResultType.NONE.getResult();
+        if (gameResult.isGameSuccess() && !gameResult.isPositionUpper()) {
+            lowerResult = BridgeResultType.POSSIBLE.getResult();
+        } else if (!gameResult.isGameSuccess() && !gameResult.isPositionUpper()) {
+            lowerResult = BridgeResultType.IMPOSSIBLE.getResult();
+        }
+        this.lowerBridge.add(lowerResult);
+    }
+
+  
+
+}
