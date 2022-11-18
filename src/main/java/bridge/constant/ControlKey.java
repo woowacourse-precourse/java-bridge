f8dcc7c38867168991196780d9b459890430fe9c
yubin <dbqls2821@gmail.com>
2022-11-18 23:34:45 +0900

    feat(ControlKey): 컨트롤 키 (U, D, R, Q) enum 상수화

---
+++ src/main/java/bridge/constant/ControlKey.java
@@ -0,0 +1,43 @@
+package bridge.constant;
+
+public enum ControlKey {
+  UP("U", "위"){
+    public String getPair() {
+      return getKeyInKorean() + ": " + getKey();
+    }
+  },
+  DOWN("D", "아래"){
+    public String getPair() {
+      return getKeyInKorean() + ": " + getKey();
+    }
+  },
+  RETRY("R", "재시도"){
+    public String getPair() {
+      return getKeyInKorean() + ": " + getKey();
+    }
+  },
+  QUIT("Q", "종료"){
+    public String getPair() {
+      return getKeyInKorean() + ": " + getKey();
+    }
+  };
+
+  private String key;
+  private String keyInKorean;
+
+  ControlKey(String key, String keyInKorean) {
+    this.key = key;
+    this.keyInKorean = keyInKorean;
+  }
+
+  public String getKey() {
+    return key;
+  }
+
+  public String getKeyInKorean() {
+    return keyInKorean;
+  }
+
+  public abstract String getPair();
+
+}
