b47800da5099c930752506b0dda75225fad693eb
dev_kong <ahdjd5@gmail.com>
2022-11-18 16:28:26 +0900

    feat(Converter): add toIntFromString method to convert user input to int

---
+++ src/main/java/bridge/utils/Converter.java
@@ -0,0 +1,11 @@
+package bridge.utils;
+
+public class Converter {
+    public int toIntFromString(String input) {
+        try {
+            return Integer.parseInt(input);
+        } catch (IllegalArgumentException exception) {
+            throw new IllegalArgumentException("숫자만 입력 가능합니다.");
+        }
+    }
+}
