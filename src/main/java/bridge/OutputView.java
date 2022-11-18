ebd2ed7f2b0d38ff29a6178c3bfb8c186f26f02e
ouohoon <begreat@kakao.com>
2022-11-18 12:22:42 +0900

    feat(OutputView): 게임 시작 문구 출력 기능 구현

--- src/main/java/bridge/OutputView.java
+++ src/main/java/bridge/OutputView.java
@@ -14,6 +14,13 @@ public class OutputView {
     private final String totalTryText = "총 시도한 횟수: ";
 
     /**
+     *
+     */
+    public void printStart() {
+        System.out.println(startText);
+    }
+
+    /**
      * 현재까지 이동한 다리의 상태를 정해진 형식에 맞춰 출력한다.
      * <p>
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
