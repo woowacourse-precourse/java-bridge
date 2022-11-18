674f2d5db52754a10ec77ce6bc075b96f33d5a15
kimjeonghan <[rlawjdgks318@naver.com]>
2022-11-18 16:54:03 +0900

    feat(InputView): 재시작 또는 종료 커맨드를 사용자 입력으로 받는 기능 추가

--- src/main/java/bridge/view/InputView.java
+++ src/main/java/bridge/view/InputView.java
@@ -26,12 +26,13 @@ public class InputView {
 		return readUserInput();
 	}
 
-    /**
-     * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
-     */
-    public String readGameCommand() {
-        return null;
-    }
+	/**
+	 * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
+	 */
+	public String readGameCommand() {
+		System.out.printf(RESTART_OR_EXIT_MESSAGE_FORMAT, RESTART_COMMAND_MESSAGE, EXIT_COMMAND_MESSAGE);
+		return readUserInput();
+	}
 
 	public String readUserInput() {
 		return Console.readLine();
