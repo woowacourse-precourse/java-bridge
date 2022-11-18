50df698e4b54d1baafad0c0888504fd6b519d165
junpakPark <112045553+junpakPark@users.noreply.github.com>
2022-11-18 14:20:43 +0900

    feat(BridgeGame): BridgeGame 객체 메서드 완성 및 setUp 메서드 추가

--- src/main/java/bridge/service/BridgeGame.java
+++ src/main/java/bridge/service/BridgeGame.java
@@ -1,23 +1,41 @@
-package bridge;
+package bridge.service;
+
+import java.util.List;
+
+import bridge.BridgeMaker;
+import bridge.BridgeNumberGenerator;
+import bridge.BridgeRandomNumberGenerator;
+import bridge.model.BridgeSize;
+import bridge.model.ExitOption;
+import bridge.model.FootPrint;
 
 /**
  * 다리 건너기 게임을 관리하는 클래스
  */
 public class BridgeGame {
+	public List<String> setUp(int userInput) {
+		BridgeSize bridgeSize = new BridgeSize(userInput);
+		BridgeNumberGenerator bridgeNumberGenerator = new BridgeRandomNumberGenerator();
+		BridgeMaker bridgeMaker = new BridgeMaker(bridgeNumberGenerator);
+		return bridgeMaker.makeBridge(bridgeSize.getSize());
+	}
 
-    /**
-     * 사용자가 칸을 이동할 때 사용하는 메서드
-     * <p>
-     * 이동을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
-     */
-    public void move() {
-    }
+	/**
+	 * 사용자가 칸을 이동할 때 사용하는 메서드
+	 * <p>
+	 * 이동을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
+	 */
+	public List<String> move(int stairs, boolean isEqual) {
+		return FootPrint.makeFootPrints(stairs, isEqual);
+	}
 
-    /**
-     * 사용자가 게임을 다시 시도할 때 사용하는 메서드
-     * <p>
-     * 재시작을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
-     */
-    public void retry() {
-    }
+	/**
+	 * 사용자가 게임을 다시 시도할 때 사용하는 메서드
+	 * <p>
+	 * 재시작을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
+	 */
+	public boolean retry(ExitOption exitOption) {
+		return exitOption.isReplay();
+	}
+
 }
