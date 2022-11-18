b97237d34d2ff728c0ebade9a49629e5a5ad0b7a
Polite-Jehu <werber112@kyonggi.ac.kr>
2022-11-18 15:00:35 +0900

    test(bridge): test game over function

--- src/test/java/bridge/DomainTest.java
+++ src/test/java/bridge/DomainTest.java
@@ -9,6 +9,7 @@ import camp.nextstep.edu.missionutils.test.NsTest;
 
 import java.io.ByteArrayInputStream;
 import java.io.InputStream;
+import java.util.ArrayList;
 import java.util.List;
 
 import org.junit.jupiter.api.BeforeEach;
@@ -18,10 +19,20 @@ import org.junit.jupiter.api.Test;
 public class DomainTest {
     private OutputView outputView = null;
     private InputView inputView = null;
+    private BridgeGame bridgeGame = null;
+
+    ArrayList<String> bridgeSet(){
+        ArrayList<String> bridge = new ArrayList<>();
+        bridge.add("U");
+        bridge.add("U");
+        bridge.add("D");
+        return bridge;
+    }
     @BeforeEach
     void SetUp() {
         outputView= new OutputView();
         inputView = new InputView();
+        bridgeGame = new BridgeGame(bridgeSet());
     }
 
     @DisplayName("다리 길이 3 테스트")
@@ -60,6 +71,13 @@ public class DomainTest {
         assertThat(inputView.checkMessage("U",ErrorMessage.WRONGMOVEINPUTEXCEPTION)).contains("PASS");
         assertThat(inputView.checkMessage("D",ErrorMessage.WRONGMOVEINPUTEXCEPTION)).contains("PASS");
         assertThat(inputView.checkMessage("dkfjsdkf",ErrorMessage.WRONGMOVEINPUTEXCEPTION)).contains("ERROR");
+    }
+
+    @DisplayName("다리만큼 다왔으면 게임 오버")
+    @Test
+    void GameOverCheck() {
+        bridgeGame.gameOverChecker(3);
+        assertThat(bridgeGame.isGameOver()).isTrue();
     }
 
 }
