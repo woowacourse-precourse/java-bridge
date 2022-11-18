72278dd4a85574869d4b6e3de85c477322a04c9c
TaeHeyeon_Kim <thk98k@gmail.com>
2022-11-18 23:37:18 +0900

    feat(game): 게임을 다시 시도할지 여부를 입력받는 기능 구현

--- src/main/java/game/BridgeController.java
+++ src/main/java/game/BridgeController.java
@@ -1,4 +1,4 @@
-package Game;
+package game;
 
 import bridge.*;
 
@@ -26,6 +26,12 @@ public class BridgeController {
     private String moveCommand() {
         System.out.println("이동할 칸을 선택해주세요. (위: U, 아래: D)");
         String command = inputView.readMoving();
+        return command;
+    }
+
+    private String retryCommand() {
+        System.out.println("게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)");
+        String command = inputView.readGameCommand();
         return command;
     }
 }
