db710124447fa93139c1cbfa2c48c0a890b3795a
dclxxi <dclxxi@kakao.com>
2022-11-18 21:48:32 +0900

    feat: 게임의 재시작/종료를 입력받는 기능 추가

---
+++ src/main/java/model/CommandType.java
@@ -0,0 +1,18 @@
+package model;
+
+public enum CommandType {
+    RETRY(true, "R"),
+    QUIT(false, "Q");
+
+    private final boolean run;
+    private final String mark;
+
+    CommandType(boolean run, String mark) {
+        this.run = run;
+        this.mark = mark;
+    }
+
+    public String getMark() {
+        return mark;
+    }
+}
--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -5,6 +5,8 @@ import camp.nextstep.edu.missionutils.Console;
 import static constant.Config.ERROR;
 import static model.BridgeType.DOWN;
 import static model.BridgeType.UP;
+import static model.CommandType.QUIT;
+import static model.CommandType.RETRY;
 
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
@@ -13,6 +15,7 @@ public class InputView {
     private static final String BRIDGE_SIZE_MESSAGE = "다리의 길이를 입력해주세요.";
     private static final String BRIDGE_SIZE_ERROR = ERROR + "다리 길이는 숫자여야 합니다.";
     private static final String MOVING_MESSAGE = String.format("이동할 칸을 선택해주세요. (위: %s, 아래: %s)", UP.getMark(), DOWN.getMark());
+    private static final String GAME_COMMAND_MESSAGE = String.format("게임을 다시 시도할지 여부를 입력해주세요. (재시도: %s, 종료: %s)", RETRY.getMark(), QUIT.getMark());
 
     /**
      * 다리의 길이를 입력받는다.
@@ -42,6 +45,7 @@ public class InputView {
      * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
      */
     public String readGameCommand() {
-        return null;
+        System.out.println(GAME_COMMAND_MESSAGE);
+        return Console.readLine();
     }
 }
