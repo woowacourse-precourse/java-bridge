b42c7ada8eda9b507202b924281e0f32094ec835
1eejisoo <jisu991012@gmail.com>
2022-11-19 02:03:38 +0900

    refactor: 다리 길이, 이동할 칸의 유효성 검증 기능 수정

--- src/main/java/view/InputMessage.java
+++ src/main/java/view/InputMessage.java
@@ -3,21 +3,18 @@ package view;
 public enum InputMessage {
     INPUT_BRIDGE_SIZE("다리의 길이를 입력해주세요.") {
         @Override
-        public void eachValidation(String input) {
-            if (Integer.parseInt(input) < 3 || Integer.parseInt(input) > 20) {
-                throw new IllegalArgumentException("다리의 길이는 3 이상 20 이하여야 합니다.");
-            }
+        public void validation(String input) {
+            validation.validateBridgeSizeRange(input);
         }
     },
     INPUT_BRIDGE_MOVE("이동할 칸을 선택해주세요. (위: U, 아래: D)") {
         @Override
-        public void eachValidation(String input) {
-            if (!input.equals("U") && !input.equals("D")) {
-                throw new IllegalArgumentException("이동할 칸의 입력은 'U' 또는 'D'가 되어야 합니다.");
-            }
+        public void validation(String input) {
+            validation.validateMoving(input);
         }
     };
 
+    private static final Validation validation = new Validation();
     private final String message;
 
     InputMessage(String message) {
@@ -28,5 +25,5 @@ public enum InputMessage {
         return message;
     }
 
-    abstract public void eachValidation(String input);
+    abstract public void validation(String input);
 }
--- src/main/java/view/Validation.java
+++ src/main/java/view/Validation.java
@@ -8,7 +8,7 @@ public class Validation {
         validateContainBlank(input);
         Arrays.stream(InputMessage.values())
                 .filter(inputMessage -> message.equals(inputMessage.getMessage()))
-                .forEach(inputMessage -> inputMessage.eachValidation(input));
+                .forEach(inputMessage -> inputMessage.validation(input));
     }
 
     public void validateEmpty(String input) {
@@ -20,6 +20,18 @@ public class Validation {
     public void validateContainBlank(String input) {
         if (input.contains(" ")) {
             throw new IllegalArgumentException("입력 값에 공백이 포함되지 않아야 합니다.");
+        }
+    }
+
+    public void validateBridgeSizeRange(String input) {
+        if (Integer.parseInt(input) < 3 || Integer.parseInt(input) > 20) {
+            throw new IllegalArgumentException("다리의 길이는 3 이상 20 이하여야 합니다.");
+        }
+    }
+
+    public void validateMoving(String input){
+        if (!input.equals("U") && !input.equals("D")) {
+            throw new IllegalArgumentException("이동할 칸의 입력은 'U' 또는 'D'가 되어야 합니다.");
         }
     }
 }
