62b8c50d1b3e2a64223559b9d38ed7738764a500
yeonhee <ydol2@naver.com>
2022-11-19 00:05:13 +0900

    feat(BridgeGameValidator) : R,Q 입력인지 검증하는 로직 추가

--- src/main/java/bridge/exception/BridgeGameValidator.java
+++ src/main/java/bridge/exception/BridgeGameValidator.java
@@ -4,6 +4,7 @@ public class BridgeGameValidator {
 
     private static final String ERROR_MESSAGE_BRIDGE_SIZE = "[ERROR] 다리 길이는 3부터 20 사이의 숫자여야 합니다.";
     private static final String ERROR_MESSAGE_UP_AND_DOWN = "[ERROR] '위: U 아래: D'로 입력하셔야 합니다.";
+    private static final String ERROR_MESSAGE_RESTART_AND_QUIT = "[ERROR] '재시도: R 종료: Q'로 입력하셔야 합니다.";
 
 
     public static void isValidGameNumber(int number) {
@@ -24,6 +25,12 @@ public class BridgeGameValidator {
     public static void validateUpAndDown(String str){
         if(!(str=="U" || str=="D")){
             throw new IllegalArgumentException(ERROR_MESSAGE_UP_AND_DOWN);
+        }
+    }
+
+    public static void validateRestartAndQuit(String str){
+        if(!(str=="R" || str=="Q")){
+            throw new IllegalArgumentException(ERROR_MESSAGE_RESTART_AND_QUIT);
         }
     }
 }
