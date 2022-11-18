4f91d5563a8f3fbeb6c47386840e38a80f31c137
panorama713 <yaiba713@gmail.com>
2022-11-18 19:09:41 +0900

    refactor(bridge): main 수정

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -10,20 +10,25 @@ public class Application {
     static InputView inputView = new InputView();
     static BridgeGame bridgeGame = new BridgeGame();
     private static List<String> bridge = new ArrayList<>();
+    private static String retryInput = "";
 
     public static void main(String[] args) {
         // TODO: 프로그램 구현
         outputView.printStart();
         bridge = bridgeMaker.makeBridge(inputView.readBridgeSize());
+        validateMove();
+        outputView.printResult();
+    }
+    public static void validateMove() {
         for (int i = 0; i < bridge.size(); i++) {
-            System.out.println(bridge.get(i));
             if (bridgeGame.move(bridge.get(i), inputView.readMoving()).equals("X")) {
                 outputView.printMap(bridge, BridgeGame.moveMapList);
-                bridgeGame.retry(bridge, BridgeGame.moveMapList, i);
+                retryInput = inputView.readGameCommand();
+                bridgeGame.retry(retryInput, bridge, BridgeGame.moveMapList, i);
             }
+            if (retryInput.equals("Q")) return;
             if(i == bridge.size() - 1) System.out.println("최종 게임 결과");
             outputView.printMap(bridge, BridgeGame.moveMapList);
         }
-        outputView.printResult();
     }
 }
