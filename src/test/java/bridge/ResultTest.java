1d12fd43e075282447fdc7edeb2f71263853d69e
saint6839 <saint6839@gmail.com>
2022-11-18 22:36:06 +0900

    test(domain) : 정답 여부가 결과에 입력되었을 때, 정답 여부 리스트와 시도 횟수가 잘 저장되는지 테스트 작성

---
+++ src/test/java/bridge/ResultTest.java
@@ -0,0 +1,30 @@
+package bridge;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class ResultTest {
+
+    @DisplayName("정답 여부가 결과에 입력되었을 때, 정답 여부 리스트와 시도 횟수가 잘 저장되는지 테스트")
+    @Test
+    void updateIsAnswers() {
+        //given
+        Result result = new Result();
+
+        //when
+        result.updateIsAnswers(true);
+        result.updateIsAnswers(false);
+        result.updateIsAnswers(true);
+
+        //then
+        Assertions.assertThat(result.getTryCount())
+                  .isEqualTo(3);
+        Assertions.assertThat(result.getIsAnswers()
+                                    .get(0)).isTrue();
+        Assertions.assertThat(result.getIsAnswers()
+                                    .get(1)).isFalse();
+        Assertions.assertThat(result.getIsAnswers()
+                                    .get(2)).isTrue();
+    }
+}
