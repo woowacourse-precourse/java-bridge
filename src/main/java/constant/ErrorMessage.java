7535c6825580aeb86a752ad185f45a8b60e215df
ho2n <jungho2n@gmail.com>
2022-11-18 13:29:35 +0900

    feat(ErrorMessage): 예외상황시 출력할 문구를 ErrorMessage 클래스에 상수로 정리

---
+++ src/main/java/constant/ErrorMessage.java
@@ -0,0 +1,6 @@
+package constant;
+
+public class ErrorMessage {
+    public static final String ERROR_MESSAGE = "[ERROR]";
+    public static final String WRONG_LENGTH = "다리 길이는 3부터 20 사이의 숫자여야 합니다.";
+}
