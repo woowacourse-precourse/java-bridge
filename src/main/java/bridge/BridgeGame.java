787b951c969632a404624e1e742b87ff76735006
이요환 <91039622+bumstead-bumstead@users.noreply.github.com>
2022-11-18 20:34:41 +0900

    <feat>(BridgeGage.java):  generateBridge 메서드 생성
     - 다리 길이를 입력받아 다리 List를 반환하는 메서드 새엇ㅇ
     - 기능 목록에 추가

--- src/main/java/bridge/BridgeGame.java
+++ src/main/java/bridge/BridgeGame.java
@@ -1,6 +1,7 @@
 package bridge;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 
@@ -8,6 +9,7 @@ import java.util.List;
  * 다리 건너기 게임을 관리하는 클래스
  */
 public class BridgeGame {
+    private static List<String> bridgeIntegerMapper = Arrays.asList("D", "U"); //1일 때 위, 0일 때 밑
 
     private BridgeNumberGenerator numberGenerator = new BridgeRandomNumberGenerator();
     private List<String> realBridge;
@@ -17,6 +19,15 @@ public class BridgeGame {
         realBridge = generateBridge(bridgeLength);
         moveStatus.put("U", new ArrayList<Integer>());
         moveStatus.put("D", new ArrayList<Integer>());
+    }
+
+    private List<String> generateBridge(int bridgeLength) {
+        List<String> bridge = new ArrayList<>();
+        for (int i = 0; i < bridgeLength; i++) {
+            realBridge.add(bridgeIntegerMapper.get(numberGenerator.generate()));
+        }
+
+        return bridge;
     }
     public void move(String direction) {
     }
