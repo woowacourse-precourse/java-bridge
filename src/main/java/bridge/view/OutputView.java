c944613e0d9316e049b26aa4b3216b3e94249232
tseop <77163842+tseop@users.noreply.github.com>
2022-11-19 01:08:44 +0900

    feat: OutputView 기능 구현
    Game 시작 시 메세지 Pirnt
    현재 다리 통과 현황 구현
    게임 종료 시 결과 값 print 메세지 구현

---
+++ src/main/java/bridge/view/OutputView.java
@@ -0,0 +1,39 @@
+package bridge.view;
+
+import bridge.game.BridgeGame;
+import bridge.message.GameMessage;
+import java.util.List;
+
+/**
+ * 사용자에게 게임 진행 상황과 결과를 출력하는 역할을 한다.
+ */
+public class OutputView {
+    /**
+     * 함수 이름 변경 불가
+     */
+
+    public void start() {
+        System.out.println(GameMessage.START_MSG);
+    }
+
+    /**
+     * 현재까지 이동한 다리의 상태를 정해진 형식에 맞춰 출력한다.
+     * <p>
+     * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
+     */
+    public void printMap(String topMap, String bottomMap) {
+        System.out.println("[ " + topMap + " ]");
+        System.out.println("[ " + bottomMap + " ]");
+    }
+
+
+    /**
+     * 게임의 최종 결과를 정해진 형식에 맞춰 출력한다.
+     * <p>
+     * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
+     */
+    public void printResult(String clear, int count) {
+        System.out.printf(GameMessage.GAME_CLEAR_WHETHER, clear);
+        System.out.printf(GameMessage.TOTAL_TRY_COUNT, count);
+    }
+}
