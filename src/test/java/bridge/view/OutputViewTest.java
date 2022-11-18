a43eae9b0380932577c12c86eedc4ebce69b731c
newh08 <newh08@gmail.com>
2022-11-18 20:56:43 +0900

    test: refactor OutputViewTest  to clean code

--- src/test/java/bridge/view/OutputViewTest.java
+++ src/test/java/bridge/view/OutputViewTest.java
@@ -14,12 +14,21 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class OutputViewTest {
+    private BridgeGame bridgeGame;
+    private OutputView outputView;
     private final PrintStream standardOut = System.out;
     private final ByteArrayOutputStream outputStreamCaptor = new ByteArrayOutputStream();
 
     @BeforeEach
     public void setUp() {
         System.setOut(new PrintStream(outputStreamCaptor));
+    }
+
+    @BeforeEach
+    void initEach() {
+        final List<String> bridge = new ArrayList<>(Arrays.asList("U", "D", "U", "D"));
+        bridgeGame = new BridgeGame((bridge));
+        outputView = new OutputView(bridgeGame);
     }
 
     @AfterEach
@@ -30,13 +39,10 @@ class OutputViewTest {
     @DisplayName("입력 값에 따른 다리를 출력한다.")
     @Test
     void printMapTest1() {
-        List<String> bridge = new ArrayList<>(Arrays.asList("U", "D", "U", "D"));
-        BridgeGame bridgeGame = new BridgeGame(bridge);
-        bridgeGame.move("U");
-        bridgeGame.move("D");
-        bridgeGame.move("U");
-        bridgeGame.move("U");
-        OutputView outputView = new OutputView(bridgeGame);
+        String moveInput = "UDUU";
+        for (char input : moveInput.toCharArray()) {
+            bridgeGame.move(String.valueOf(input));
+        }
         outputView.printMap();
         assertEquals("[ O |   | O | X ]\n[   | O |   |   ]",
                 outputStreamCaptor.toString()
@@ -46,12 +52,10 @@ class OutputViewTest {
     @DisplayName("입력 값에 따른 다리를 출력한다.")
     @Test
     void printMapTest2() {
-        List<String> bridge = new ArrayList<>(Arrays.asList("U", "D", "U", "D"));
-        BridgeGame bridgeGame = new BridgeGame(bridge);
-        bridgeGame.move("U");
-        bridgeGame.move("D");
-        bridgeGame.move("D");
-        OutputView outputView = new OutputView(bridgeGame);
+        String moveInput = "UDD";
+        for (char input : moveInput.toCharArray()) {
+            bridgeGame.move(String.valueOf(input));
+        }
         outputView.printMap();
         assertEquals("[ O |   |   ]\n[   | O | X ]",
                 outputStreamCaptor.toString()
@@ -61,13 +65,10 @@ class OutputViewTest {
     @DisplayName("게임을 첫 번째에 성공시 printResult() 로 정확한 내용을 출력한다.")
     @Test
     void printResultTest1() {
-        List<String> bridge = new ArrayList<>(Arrays.asList("U", "D", "U", "D"));
-        BridgeGame bridgeGame = new BridgeGame(bridge);
-        bridgeGame.move("U");
-        bridgeGame.move("D");
-        bridgeGame.move("U");
-        bridgeGame.move("D");
-        OutputView outputView = new OutputView(bridgeGame);
+        String moveInput = "UDUD";
+        for (char input : moveInput.toCharArray()) {
+            bridgeGame.move(String.valueOf(input));
+        }
         outputView.printResult();
         assertEquals("최종 게임 결과\n[ O |   | O |   ]\n[   | O |   | O ]\n\n게임 성공 여부: 성공\n총 시도한 횟수: 1",
                 outputStreamCaptor.toString()
@@ -77,12 +78,10 @@ class OutputViewTest {
     @DisplayName("게임 첫 번째 실패시 printResult() 로 정확한 내용을 출력한다.")
     @Test
     void printResultTest2() {
-        List<String> bridge = new ArrayList<>(Arrays.asList("U", "D", "U", "D"));
-        BridgeGame bridgeGame = new BridgeGame(bridge);
-        bridgeGame.move("U");
-        bridgeGame.move("D");
-        bridgeGame.move("D");
-        OutputView outputView = new OutputView(bridgeGame);
+        String moveInput = "UDD";
+        for (char input : moveInput.toCharArray()) {
+            bridgeGame.move(String.valueOf(input));
+        }
         outputView.printResult();
         assertEquals("최종 게임 결과\n[ O |   |   ]\n[   | O | X ]\n\n게임 성공 여부: 실패\n총 시도한 횟수: 1",
                 outputStreamCaptor.toString()
@@ -92,17 +91,15 @@ class OutputViewTest {
     @DisplayName("게임을 두 번째에 성공시 printResult() 로 정확한 내용을 출력한다.")
     @Test
     void printResultTest3() {
-        List<String> bridge = new ArrayList<>(Arrays.asList("U", "D", "U", "D"));
-        BridgeGame bridgeGame = new BridgeGame(bridge);
-        OutputView outputView = new OutputView(bridgeGame);
-        bridgeGame.move("U");
-        bridgeGame.move("D");
-        bridgeGame.move("D");
+        String moveInput1 = "UDD";
+        for (char input : moveInput1.toCharArray()) {
+            bridgeGame.move(String.valueOf(input));
+        }
         bridgeGame.retry();
-        bridgeGame.move("U");
-        bridgeGame.move("D");
-        bridgeGame.move("U");
-        bridgeGame.move("D");
+        String moveInput2 = "UDUD";
+        for (char input : moveInput2.toCharArray()) {
+            bridgeGame.move(String.valueOf(input));
+        }
         outputView.printResult();
         assertEquals("최종 게임 결과\n[ O |   | O |   ]\n[   | O |   | O ]\n\n게임 성공 여부: 성공\n총 시도한 횟수: 2",
                 outputStreamCaptor.toString()
@@ -112,17 +109,15 @@ class OutputViewTest {
     @DisplayName("게임 두 번째 실패시 정확한 printResult() 로 내용을 출력한다.")
     @Test
     void printResultTest4() {
-        List<String> bridge = new ArrayList<>(Arrays.asList("U", "D", "U", "D"));
-        BridgeGame bridgeGame = new BridgeGame(bridge);
-        OutputView outputView = new OutputView(bridgeGame);
-        bridgeGame.move("U");
-        bridgeGame.move("D");
-        bridgeGame.move("D");
+        String moveInput1 = "UDD";
+        for (char input : moveInput1.toCharArray()) {
+            bridgeGame.move(String.valueOf(input));
+        }
         bridgeGame.retry();
-        bridgeGame.move("U");
-        bridgeGame.move("D");
-        bridgeGame.move("U");
-        bridgeGame.move("U");
+        String moveInput2 = "UDUU";
+        for (char input : moveInput2.toCharArray()) {
+            bridgeGame.move(String.valueOf(input));
+        }
         outputView.printResult();
         assertEquals("최종 게임 결과\n[ O |   | O | X ]\n[   | O |   |   ]\n\n게임 성공 여부: 실패\n총 시도한 횟수: 2",
                 outputStreamCaptor.toString()
