148bd72b38ba176d4449df7ddeab4c31262e713d
이요환 <91039622+bumstead-bumstead@users.noreply.github.com>
2022-11-18 21:32:48 +0900

    <feat>(InputView.java): validateGameCommand() 메서드 작성
     - 게임 재시작 여부 input의 유효성을 검증하는 메서드 작성
     - 해당 메서드의 에러 메세지를 저장하는 상수 gameCommandInputExceptionMessage 생성

--- src/main/java/bridge/InputView.java
+++ src/main/java/bridge/InputView.java
@@ -6,6 +6,7 @@ package bridge;
 public class InputView {
     private static final String bridgeSizeInputExceptionMessage = "다리 길이는 3부터 20 사이의 숫자여야 합니다.";
     private static final String movingInputExceptionMessage = "U와 D 중 하나로 입력해야 합니다.";
+    private static final String gameCommandInputExceptionMessage = "R과 Q 중 하나로 입력해야 합니다.";
 
     /**
      * 다리의 길이를 입력받는다.
@@ -41,6 +42,12 @@ public class InputView {
     private void validateMovingInput(String input) {
         if (!input.equals("U") && !input.equals("D")) {
             throw new IllegalArgumentException(movingInputExceptionMessage);
+        }
+    }
+
+    private void validateGameCommand(String input) {
+        if (!input.equals("R") && !input.equals("Q")) {
+            throw new IllegalArgumentException(gameCommandInputExceptionMessage);
         }
     }
 }
