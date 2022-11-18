8aea97b2978ee3d330523fb26c70c576812ffdf7
junpakPark <112045553+junpakPark@users.noreply.github.com>
2022-11-18 14:10:00 +0900

    feat(GameResult): GameResult 객체 생성
    GameResult 객체 주요 기능 목록:
    - addNumberOFAttempts 메서드 호출 시 numberOfAttempts의 값이 1씩 증가한다
    - changeResultToSuccess와 chageResultToFail 메서드 호출시 result가 해당 상태로 변화한다.
    - getter 메서드로 각 상태값들을 리턴한다

---
+++ src/main/java/bridge/model/GameResult.java
@@ -0,0 +1,36 @@
+package bridge.model;
+
+public class GameResult {
+
+	private String result;
+	private int numberOfAttempts;
+
+	public GameResult() {
+		this.result = "";
+		this.numberOfAttempts = 0;
+	}
+
+	public void addNumberOfAttempts() {
+		numberOfAttempts += 1;
+	}
+
+	public void changeResultToSuccess() {
+		if (!result.equals("성공")) {
+			result = "성공";
+		}
+	}
+
+	public void changeResulToFail() {
+		if (!result.equals("실패")) {
+			result = "실패";
+		}
+	}
+
+	public String getResult() {
+		return result;
+	}
+
+	public int getNumberOfAttempts() {
+		return numberOfAttempts;
+	}
+}
