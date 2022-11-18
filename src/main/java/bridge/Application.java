d026c9ee3db1f41c5a90af9cd76e9a04ed12950d
Jinuk <wlsdnr214@gmail.com>
2022-11-18 15:29:42 +0900

    feat(bridge): 다리길이, 이동할 칸, 게임 재시작여부 입력 구현

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -4,5 +4,10 @@ public class Application {
 
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+
+        System.out.println("다리 건너기 게임을 시작합니다.");
+
+        InputView inputView = new InputView();
+        inputView.readBridgeSize();
     }
 }
--- src/main/java/bridge/InputView.java
+++ src/main/java/bridge/InputView.java
@@ -1,5 +1,6 @@
 package bridge;
 
+import camp.nextstep.edu.missionutils.Console;
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
  */
@@ -9,20 +10,48 @@ public class InputView {
      * 다리의 길이를 입력받는다.
      */
     public int readBridgeSize() {
-        return 0;
+        System.out.println("다리의 길이를 입력해주세요.");
+        String size = Console.readLine();
+        int bridgeSize = Integer.parseInt(size);
+        checkBridgeSize(bridgeSize);
+        return bridgeSize;
     }
 
     /**
      * 사용자가 이동할 칸을 입력받는다.
      */
     public String readMoving() {
-        return null;
+        System.out.println("이동할 칸을 선택해주세요.");
+        String moving = Console.readLine();
+        checkMoving(moving);
+        return moving;
     }
 
     /**
      * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
      */
     public String readGameCommand() {
-        return null;
+        System.out.println("게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)");
+        String gameCommand = Console.readLine();
+        checkGameCommand(gameCommand);
+        return gameCommand;
+    }
+
+    private void checkBridgeSize(int bridgeSize) {
+        if(bridgeSize < 3 || bridgeSize > 20){
+            throw new IllegalArgumentException("[ERROR] 다리 길이는 3부터 20 사이의 숫자여야 합니다.");
+        }
+    }
+
+    private void checkMoving(String moving){
+        if(moving != "U" || moving != "D"){
+            throw new IllegalArgumentException("[ERROR] U 또는 D를 입력해주세요.");
+        }
+    }
+
+    private void checkGameCommand(String command) {
+        if(command != "R" || command != "Q"){
+            throw new IllegalArgumentException("[ERROR] R 또는 Q를 입력해주세요.");
+        }
     }
 }
