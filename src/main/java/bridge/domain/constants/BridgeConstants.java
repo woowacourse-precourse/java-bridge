5999139100271c4c358156373f41340a7032f179
youngkyunIm <yee0230@naver.com>
2022-11-18 19:22:24 +0900

    feat: add constants with error code

---
+++ src/main/java/bridge/domain/constants/BridgeConstants.java
@@ -0,0 +1,11 @@
+package bridge.domain.constants;
+
+public class BridgeConstants {
+
+    public static final int BRIDGE_SIZE_START = 3;
+    public static final int BRIDGE_SIZE_END = 20;
+    public static final String BRIDGE_GAME_COMMAND_R = "R";
+    public static final String BRIDGE_GAME_COMMAND_Q = "Q";
+    public static final String BRIDGE_GAME_MOVING_U = "U";
+    public static final String BRIDGE_GAME_MOVING_D = "D";
+}
---
+++ src/main/java/bridge/domain/constants/ErrorCode.java
@@ -0,0 +1,23 @@
+package bridge.domain.constants;
+
+public enum ErrorCode {
+    SIZE_NOT_NUMBER("다리의 개수는 숫자만 가능합니다.."),
+    SIZE_NOT_IN_RANGE("다리 개수의 범위는 " + BridgeConstants.BRIDGE_SIZE_START + "부터 "
+            + BridgeConstants.BRIDGE_SIZE_END + "가지 입니다."),
+    MOVING_NOT_U_D("이동할 때 위 칸은 대문자 " + BridgeConstants.BRIDGE_GAME_MOVING_U + ", 아래 칸은 대문자 "
+            + BridgeConstants.BRIDGE_GAME_MOVING_D + " 만 가능합니다."),
+    GAME_COMMAND_NOT_R_Q(
+            "재시도 여부 값은 재시도는 대문자 " + BridgeConstants.BRIDGE_GAME_COMMAND_R + ", 종료는 대문자 "
+                    + BridgeConstants.BRIDGE_GAME_COMMAND_Q + " 만 가능합니다.");
+
+    private static final String ERROR_BEGIN = "[ERROR] ";
+    private final String errorMessage;
+
+    ErrorCode(String message) {
+        errorMessage = ERROR_BEGIN + message;
+    }
+
+    public RuntimeException getException() {
+        return new IllegalArgumentException(errorMessage);
+    }
+}
