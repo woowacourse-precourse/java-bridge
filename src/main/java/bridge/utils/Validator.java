d356ea553f58f4e55c498265c48dc41c5ea3f17e
Life is a ninanino <tvxqinak@naver.com>
2022-11-19 03:44:40 +0900

    feat : 사용자의 입력 검증
    검증을 위한 validator 생성

--- src/main/java/bridge/utils/Validator.java
+++ src/main/java/bridge/utils/Validator.java
@@ -7,5 +7,12 @@ public class Validator {
     public static int validateBridgeLength(String bridgeLength){
         return Exception.checkBridgeLength(Exception.checkString(bridgeLength));
     }
-    
+
+    public static String validateMove(String userInput){
+        return Exception.checkMove(userInput);
+    }
+
+    public static String validateRestartOrQuit(String userInput){
+        return Exception.checkRestartOrQuit(userInput);
+    }
 }
