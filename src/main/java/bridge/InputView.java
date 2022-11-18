0b6115cc424bf34df9e03f3062f13b700d73fab8
Seiyon Park <seiyonpdev@gmail.com>
2022-11-19 00:41:57 +0900

    feat(InputView): 메소드 readBridgeSize 구현
    다리 길이를 입력받는 메소드를 구현했다

--- src/main/java/bridge/InputView.java
+++ src/main/java/bridge/InputView.java
@@ -1,5 +1,7 @@
 package bridge;
 
+import camp.nextstep.edu.missionutils.Console;
+
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
  */
@@ -9,7 +11,15 @@ public class InputView {
      * 다리의 길이를 입력받는다.
      */
     public int readBridgeSize() {
-        return 0;
+        String userInput = Console.readLine();
+
+        try {
+            Validation.validateBridgeSize(userInput);
+        } catch (IllegalArgumentException illegalArgumentException) {
+            readBridgeSize();
+        }
+
+        return Integer.parseInt(userInput);
     }
 
     /**
