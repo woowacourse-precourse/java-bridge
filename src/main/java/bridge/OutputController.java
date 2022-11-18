558967a12c37d4555bd4a9edc21c0a48ad17668b
asas6978 <asas6978@naver.com>
2022-11-19 01:42:21 +0900

    feat: OutputView를 연결하는 컨트롤러 구현

---
+++ src/main/java/bridge/OutputController.java
@@ -0,0 +1,30 @@
+package bridge;
+
+import bridge.util.Constant;
+import bridge.util.GuideMessage;
+
+import java.util.List;
+import java.util.Map;
+
+public class OutputController {
+    private final OutputView outputView;
+
+    public OutputController(OutputView outputView) {
+        this.outputView = outputView;
+    }
+
+    public void getChoiceResult(List<String> board, List<String> bridge) {
+        outputView.printMap(board, bridge);
+    }
+
+    public void getFinalResult(List<String> board, List<String> bridge, Map<String, Integer> resultBoard) {
+        String result = GuideMessage.FAIL;
+        if (resultBoard.get(Constant.SUCCESS_OR_FAIL) == 1) {
+            result = GuideMessage.SUCCESS;
+        }
+
+        outputView.endOfGame();
+        getChoiceResult(board, bridge);
+        outputView.printResult(result, resultBoard.get(Constant.NUMBER_OF_ATTEMPTS));
+    }
+}
