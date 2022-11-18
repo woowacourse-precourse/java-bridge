35de867d2e2fb4c281bde67533623f45fe7a7ef8
SHEOMM <esh0821@snu.ac.kr>
2022-11-18 16:04:41 +0900

    test: 범위에서 벗어난 경우 에러 테스트

--- src/test/java/bridge/domain/BridgeMakerTest.java
+++ src/test/java/bridge/domain/BridgeMakerTest.java
@@ -6,6 +6,8 @@ import bridge.BridgeNumberGenerator;
 import bridge.BridgeRandomNumberGenerator;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 public class BridgeMakerTest {
     BridgeNumberGenerator bridgeNumberGenerator = new BridgeRandomNumberGenerator();
@@ -16,6 +18,14 @@ public class BridgeMakerTest {
     void 숫자_아닌_경우_에러_테스트(){
         String input = "숫자아님";
         assertThatThrownBy(() -> bridgeMaker.getSizeInteger(input))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("범위에서 벗어난 경우 에러 테스트")
+    @ValueSource(strings = {"2", "-5", "31"})
+    @ParameterizedTest
+    void 범위_벗어난_경우_에러_테스트(Integer input){
+        assertThatThrownBy(() -> bridgeMaker.makeBridge(input))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 }
