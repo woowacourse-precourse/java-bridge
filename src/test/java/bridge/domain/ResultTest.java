2f94751ff847c53c6b93c6c87ebb4fa74f0fa744
Miseong Kim <miseongk@naver.com>
2022-11-19 05:20:14 +0900

    test(Result): update result after move

---
+++ src/test/java/bridge/domain/ResultTest.java
@@ -0,0 +1,27 @@
+package bridge.domain;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class ResultTest {
+
+    private Result result;
+
+    @BeforeEach
+    void init() {
+        result = new Result();
+    }
+
+    @DisplayName("이동 가능한 칸이면 O, 아니면 X를 추가")
+    @Test
+    void updateResult() {
+        result.update("O", "U");
+        result.update("X", "U");
+        assertThat(result.getUp().get(0)).isEqualTo("O");
+        assertThat(result.getUp().get(1)).isEqualTo("X");
+        assertThat(result.getDown().size()).isEqualTo(0);
+    }
+}
