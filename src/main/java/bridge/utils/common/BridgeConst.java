eb28f5bf0c091f31e58511bd6b2eee31bddd79d7
apptie <appti@naver.com>
2022-11-18 14:57:05 +0900

    refactor(BridgeConst): 다리 생성 시 위, 아래 값 상수로 정의

--- src/main/java/bridge/utils/common/BridgeConst.java
+++ src/main/java/bridge/utils/common/BridgeConst.java
@@ -5,6 +5,9 @@ public final class BridgeConst {
     public static final int MIN_BRIDGE_SIZE = 3;
     public static final int MAX_BRIDGE_SIZE = 20;
 
+    public static final int DOWN_TILE_VALUE = 0;
+    public static final int UP_TILE_VALUE = 1;
+
     private BridgeConst() {
     }
 }
