975d81baf339bbd47b1f4a443bd7ab4d64066f45
HyebinShin <hyeviin.dev@gmail.com>
2022-11-18 22:33:49 +0900

    Change: 총 시도한 횟수 문구 수정
    총 시도한 횟수 -> 총 시도한 횟수:

--- src/main/java/bridge/PrintWord.java
+++ src/main/java/bridge/PrintWord.java
@@ -7,5 +7,5 @@ public class PrintWord {
     public static final String SELECT_RETRY = "게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료:Q)";
     public static final String RESULT = "최종 게임 결과";
     public static final String IS_SUCCESS = "게임 성공 여부: ";
-    public static final String COUNT = "총 시도한 횟수";
+    public static final String COUNT = "총 시도한 횟수: ";
 }
