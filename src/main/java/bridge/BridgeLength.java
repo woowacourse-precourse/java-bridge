0bbba3def322ad0c526c4a78be634b1e6b280dcc
LHS-11 <ghktjq1119@naver.com>
2022-11-18 22:42:30 +0900

    [Feat] : 입력 받은 다리의 길이 validate 구현

---
+++ src/main/java/bridge/BridgeLength.java
@@ -0,0 +1,46 @@
+package bridge;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class BridgeLength {
+
+    private final int length;
+
+
+    public BridgeLength(String length) {
+        validateLength(length);
+        this.length = Integer.parseInt(length);
+    }
+
+    public void validateLength(String length){
+        validateLengthNumber(length);
+        validateLengthRange(length);
+    }
+
+    public void validateLengthNumber(String length){
+        if(isLengthNumber(length)){
+            return;
+        }
+        throw new IllegalArgumentException("[ERROR] 다리 길이는 숫자여야 합니다.");
+    }
+
+    public boolean isLengthNumber(String length){
+        String regex = "^[1-2]?[1-9]$";
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(length);
+        return matcher.matches();
+    }
+
+    public void validateLengthRange(String length){
+        if(isLengthRange(length)){
+            return;
+        }
+        throw new IllegalArgumentException("[ERROR] 다리 길이는 3부터 20 사이의 숫자여야 합니다.");
+    }
+
+    public boolean isLengthRange(String length){
+        int lengthNumber = Integer.parseInt(length);
+        return lengthNumber >= 3 && lengthNumber <= 20;
+    }
+}
