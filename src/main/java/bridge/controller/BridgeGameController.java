e9688a7d052e879432e2700ff49e7ed433460cc3
SuhHyeonjun <dmsgktn0208@gmail.com>
2022-11-18 16:32:49 +0900

    feat : 게임 상태 값 boolean을 얻는 getter 추가

--- src/main/java/bridge/controller/BridgeGameController.java
+++ src/main/java/bridge/controller/BridgeGameController.java
@@ -6,10 +6,10 @@ import bridge.BridgeRandomNumberGenerator;
 import bridge.InputView;
 import bridge.Message;
 import bridge.OutputView;
+
 import java.util.List;
 
 public class BridgeGameController {
-
     private static boolean status = true;
     private static int bridgeLocation = 0;
     private final InputView inputView = new InputView();
@@ -17,25 +17,30 @@ public class BridgeGameController {
     private final BridgeGame bridgeGame = new BridgeGame();
     private final BridgeMaker bridgeMaker = new BridgeMaker(new BridgeRandomNumberGenerator());
 
-    public void  start() {
+    public void start() {
+        System.out.println(Message.START_GAME.getMessage());
         int inputSize = inputView.inputBridgeSize();
         List<String> bridges = bridgeMaker.makeBridge(inputSize);
         System.out.println(bridges);
         while (status && bridgeLocation < inputSize) {
             String inputDirection = inputView.inputMovingDirection();
             String moveResult = bridgeGame.move(bridges.get(bridgeLocation), inputDirection);
-            getState(moveResult);
+            getCurrentState(moveResult);
             outputView.printMap(inputDirection, moveResult);
             bridgeLocation++;
         }
-        outputView.printResult(status);
+        outputView.printResult();
     }
 
-    public static void getState(String moveResult) {
+    public static void getCurrentState(String moveResult) {
         if (moveResult.contains(Message.MOVE_FAIL.getMessage())) {
             status = false;
         } else if (moveResult.contains(Message.MOVE_SUCCESS.getMessage())) {
             status = true;
         }
+    }
+
+    public static boolean getStatus() {
+        return status;
     }
 }
