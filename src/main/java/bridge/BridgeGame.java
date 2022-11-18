5e877859d9ef4b85b51dcd3726d057c0a5bf65cc
jmj005 <jmj00005@naver.com>
2022-11-18 18:42:59 +0900

    feat(OutputView): 현재 다리 상태 출력 기능 구현

--- src/main/java/bridge/BridgeGame.java
+++ src/main/java/bridge/BridgeGame.java
@@ -1,16 +1,39 @@
 package bridge;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * 다리 건너기 게임을 관리하는 클래스
  */
 public class BridgeGame {
+
+    InputView inputView = new InputView();
+    OutputView outputView = new OutputView();
+    BridgeNumberGenerator numberGenerator = new BridgeRandomNumberGenerator();
+    BridgeMaker bridgeMaker = new BridgeMaker(numberGenerator);
+
+    public void run() {
+        int size = inputView.readBridgeSize();
+        List<String> bridge = bridgeMaker.makeBridge(size);
+        move(bridge);
+    }
 
     /**
      * 사용자가 칸을 이동할 때 사용하는 메서드
      * <p>
      * 이동을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void move() {
+    public void move(List<String> bridge) {
+        List<String> upSideDownSideMove = new ArrayList<>(List.of("", ""));
+        for (String bridgeElement : bridge) {
+            CompareBridge playerMove = new CompareBridge(inputView.readMoving());
+            upSideDownSideMove.set(0, upSideDownSideMove.get(0)
+                    + playerMove.getUpSideMove(playerMove, bridgeElement).toString());
+            upSideDownSideMove.set(1, upSideDownSideMove.get(1)
+                    + playerMove.getDownSideMove(playerMove, bridgeElement).toString());
+            outputView.printMap(upSideDownSideMove);
+        }
     }
 
     /**
