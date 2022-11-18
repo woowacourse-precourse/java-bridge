d3094e83307771d36bf02dce23ea374e113fa8bb
PgmJun <84304802+PgmJun@users.noreply.github.com>
2022-11-18 11:47:30 +0900

    feat : 다리 생성 기능 구현

--- src/main/java/bridge/domain/BridgeMaker.java
+++ src/main/java/bridge/domain/BridgeMaker.java
@@ -2,11 +2,15 @@ package bridge.domain;
 
 import bridge.BridgeNumberGenerator;
 
+import java.util.ArrayList;
 import java.util.List;
+
+import static bridge.constant.Constants.BridgeSign.DOWN;
+import static bridge.constant.Constants.BridgeSign.UP;
 
 /**
  * 다리의 길이를 입력 받아서 다리를 생성해주는 역할을 한다.
- *
+ * <p>
  * 제공된 BridgeMaker 클래스를 활용해 구현해야 한다.
  * BridgeMaker의 필드(인스턴스 변수)를 변경할 수 없다.
  * BridgeMaker의 메서드의 시그니처(인자, 이름)와 반환 타입은 변경할 수 없다.
@@ -24,6 +28,27 @@ public class BridgeMaker {
      * @return 입력받은 길이에 해당하는 다리 모양. 위 칸이면 "U", 아래 칸이면 "D"로 표현해야 한다.
      */
     public List<String> makeBridge(int size) {
-        return null;
+        validateBridgeSize(size);
+        return generateBridgeList(size);
+    }
+
+    private List<String> generateBridgeList(int size) {
+        List<String> bridge = new ArrayList<>();
+        for (var i = 0; i < size; i++) {
+            int direction = bridgeNumberGenerator.generate();
+            if (direction == 1) {
+                bridge.add(UP);
+            } else if (direction == 0) {
+                bridge.add(DOWN);
+            }
+        }
+        return bridge;
+    }
+
+    private void validateBridgeSize(int size) {
+        if (size >= 3 && size <= 20) {
+            return;
+        }
+        throw new IllegalArgumentException("[ERROR] 다리 길이가 3이상 20이하 범위의 값이 아닙니다.");
     }
 }
--- src/main/java/bridge/view/InputView.java
+++ src/main/java/bridge/view/InputView.java
@@ -1,13 +1,15 @@
 package bridge.view;
 
+import camp.nextstep.edu.missionutils.Console;
+
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
- *
+ * <p>
  * 제공된 InputView 클래스를 활용해 구현해야 한다.
  * InputView의 패키지는 변경할 수 있다.
  * InputView의 메서드의 시그니처(인자, 이름)와 반환 타입은 변경할 수 있다.
  * 사용자 값 입력을 위해 필요한 메서드를 추가할 수 있다.
- *
+ * <p>
  * ★ InputView 클래스에서만 camp.nextstep.edu.missionutils.Console 의 readLine() 메서드를 이용해 사용자의 입력을 받을 수 있다.
  */
 public class InputView {
@@ -16,7 +18,8 @@ public class InputView {
      * 다리의 길이를 입력받는다.
      */
     public int readBridgeSize() {
-        return 0;
+        String size = Console.readLine();
+        return Integer.parseInt(size);
     }
 
     /**
