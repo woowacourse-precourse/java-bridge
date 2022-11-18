ba9631decc99f584cf98c1ec335482b88fb12999
lee <higgs95@naver.com>
2022-11-19 05:25:43 +0900

    feat(BridgeGame): 이동 기능 추가

--- src/main/java/bridge/service/BridgeGame.java
+++ src/main/java/bridge/service/BridgeGame.java
@@ -1,23 +1,64 @@
-package bridge;
+package bridge.service;
 
-/**
- * 다리 건너기 게임을 관리하는 클래스
- */
+import static bridge.view.InputView.readMoving;
+
+import bridge.type.MovingType;
+import bridge.view.OutputView;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+
 public class BridgeGame {
+    private final List<String> movingRecord;
+    private final List<String> bridgeInfo;
+    private int stageCount;
 
-    /**
-     * 사용자가 칸을 이동할 때 사용하는 메서드
-     * <p>
-     * 이동을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
-     */
-    public void move() {
+    public BridgeGame(List<String> bridgeInfo) {
+        this.movingRecord = new ArrayList<>();
+        this.bridgeInfo=  bridgeInfo;
+        this.stageCount = 0;
     }
 
-    /**
-     * 사용자가 게임을 다시 시도할 때 사용하는 메서드
-     * <p>
-     * 재시작을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
-     */
+    public void move() {
+        String moving = readMoving();
+        movingRecord.add(moving);
+        OutputView.printMap(printPlayingMap());
+        stageCount++;
+    }
+
+    private List<List<String>> printPlayingMap() {
+       List<String> upBridge = new ArrayList<>();
+       List<String> downBridge = new ArrayList<>();
+       List<List<String>> playingMap = new ArrayList<>(Arrays.asList(downBridge, upBridge));
+
+       for(int index=0; index<movingRecord.size(); index++) {
+           playingMap.get(checkUpAndDown(movingRecord.get(index)).getValue())
+                   .add(checkYesOrNo(index, movingRecord.get(index)));
+           playingMap.get(1-checkUpAndDown(movingRecord.get(index)).getValue())
+                   .add(" ");
+       }
+       return playingMap;
+    }
+    private String checkYesOrNo(int index, String moving) {
+        if(bridgeInfo.get(index).equals(moving)) {
+            return "O";
+        }
+        return "X";
+    }
+
+    private MovingType checkUpAndDown(String moving) {
+        return MovingType.createMoving(moving);
+    }
+
+    public int getStageCount() {
+        return stageCount;
+    }
     public void retry() {
+        // TODO: 재시작 출력
+    }
+
+    public void exit() {
+        // TODO: 게임 결과 출력
     }
 }
