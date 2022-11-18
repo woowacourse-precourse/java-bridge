947dc246baee5a4a9ff7890c8b5a33f6b7d92b0c
asas6978 <asas6978@naver.com>
2022-11-19 01:37:18 +0900

    feat: 다리 상태 출력 기능 구현

--- src/main/java/bridge/OutputView.java
+++ src/main/java/bridge/OutputView.java
@@ -1,16 +1,80 @@
 package bridge;
 
+import bridge.util.Constant;
+import bridge.util.GuideMessage;
+
+import java.util.List;
+
 /**
  * 사용자에게 게임 진행 상황과 결과를 출력하는 역할을 한다.
  */
 public class OutputView {
+    public void printFirstOfBridge() {
+        System.out.print(GuideMessage.FIRST_OF_BRIDGE);
+    }
 
+    public void printMiddleOfBridge() {
+        System.out.print(GuideMessage.MIDDLE_OF_BRIDGE);
+    }
+
+    public void printLastOfBridge() {
+        System.out.println(GuideMessage.LAST_OF_BRIDGE);
+    }
+
+    public void printChoiceResult(String content) {
+        System.out.print(content);
+    }
     /**
      * 현재까지 이동한 다리의 상태를 정해진 형식에 맞춰 출력한다.
      * <p>
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void printMap() {
+    public void printMap(List<String> board, List<String> bridge) {
+        printUpperLine(board, bridge);
+        printLowerLine(board, bridge);
+        breakLine();
+    }
+
+    private void printUpperLine(List<String> board, List<String> bridge) {
+        printFirstOfBridge();
+        for (int index = 0; index < board.size(); index++) {
+            printUpperBlock(board, index, bridge);
+        }
+        printLastOfBridge();
+    }
+
+    private void printUpperBlock(List<String> board, int index, List<String> bridge) {
+        String contentToPrint = compare(board.get(index), bridge.get(index), Constant.UP);
+        printChoiceResult(contentToPrint);
+        if (index < board.size() - 1) {
+            printMiddleOfBridge();
+        }
+    }
+
+    private String compare(String userResult, String bridgeBlock, String direction) {
+        if (bridgeBlock.equals(direction) && userResult.equals(direction)) {
+            return GuideMessage.CORRECT_CHOICE;
+        }
+        if (!bridgeBlock.equals(direction) && userResult.equals(direction)) {
+            return GuideMessage.INCORRECT_CHOICE;
+        }
+        return Constant.EMPTY;
+    }
+
+    private void printLowerLine(List<String> board, List<String> bridge) {
+        printFirstOfBridge();
+        for (int index = 0; index < board.size(); index++) {
+            printLowerBlock(board, index, bridge);
+        }
+        printLastOfBridge();
+    }
+
+    private void printLowerBlock(List<String> board, int index, List<String> bridge) {
+        String contentToPrint = compare(board.get(index), bridge.get(index), Constant.DOWN);
+        printChoiceResult(contentToPrint);
+        if (index < board.size() - 1) {
+            printMiddleOfBridge();
+        }
     }
 
     /**
@@ -19,5 +83,9 @@ public class OutputView {
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
     public void printResult() {
+    }
+
+    private void breakLine() {
+        System.out.println();
     }
 }
