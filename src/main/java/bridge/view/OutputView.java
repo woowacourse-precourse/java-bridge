8f26507f2da9194606ebd84ae40a25357632aaeb
JeonghakLee <wjdgkr0527@naver.com>
2022-11-18 13:39:39 +0900

    feat: 게임 결과 출력 기능 구현

--- src/main/java/bridge/view/OutputView.java
+++ src/main/java/bridge/view/OutputView.java
@@ -1,9 +1,6 @@
 package bridge.view;
 
-import bridge.domain.BridgeCellType;
 import bridge.domain.MoveResult;
-import java.util.List;
-import java.util.Map;
 
 /**
  * 사용자에게 게임 진행 상황과 결과를 출력하는 역할을 한다.
@@ -11,8 +8,10 @@ import java.util.Map;
 public class OutputView {
 
     public static String GAME_START_MESSAGE = "다리 건너기 게임을 시작합니다.";
-    public static String INPUT_BRIDGE_LENGTH_MESSAGE = "다리의 길이를 입력해주세요.";
-    public static String INPUT_MOVE_MESSAGE = "이동할 칸을 선택해주세요. (위: U, 아래: D)";
+
+    public void printGameStartMessage() {
+        System.out.println(GAME_START_MESSAGE);
+    }
 
     /**
      * 현재까지 이동한 다리의 상태를 정해진 형식에 맞춰 출력한다.
@@ -20,6 +19,7 @@ public class OutputView {
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
     public void printMap(MoveResult result) {
+        System.out.println(result);
     }
 
     /**
@@ -27,6 +27,18 @@ public class OutputView {
      * <p>
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void printResult() {
+    public void printResult(MoveResult moveResult) {
+        System.out.println("최종게임결과");
+        printMap(moveResult);
+        printGameResultMessage(moveResult.isSuccess());
+        System.out.println("총 시도한 횟수: " + moveResult.getTryCount());
+    }
+
+    private static void printGameResultMessage(boolean isSuccess) {
+        if (isSuccess) {
+            System.out.println("게임 성공 여부: 성공");
+            return;
+        }
+        System.out.println("게임 성공 여부: 실패");
     }
 }
