626b05ad6a9af1521ff49d99d350245224c83906
lee2963 <sodel2963@naver.com>
2022-11-18 18:55:07 +0900

    test: 입력된 다리 이동과 다리 길 일치 하지 않는 경우 X 출력 테스트

--- src/test/java/bridge/BridgeGameTest.java
+++ src/test/java/bridge/BridgeGameTest.java
@@ -15,4 +15,12 @@ public class BridgeGameTest {
         bridgeGame.move("U");
         Assertions.assertEquals(bridgeGame.getUpMove().get(0), " O ");
     }
+
+    @DisplayName("현재 입력과 다리 길 일치하지 않는 경우 X 출력")
+    @Test
+    void moveNotCorrect() {
+        BridgeGame bridgeGame = new BridgeGame(List.of("U","U","U"));
+        bridgeGame.move("D");
+        Assertions.assertEquals(bridgeGame.getDownMove().get(0), " X ");
+    }
 }
