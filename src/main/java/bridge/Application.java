cd767efd4231c75b01dab25011a116f0a788f412
SH <gram753@naver.com>
2022-11-18 11:36:19 +0900

    feat(controller): 게임의 전체적인 로직을 실행하는 기능 구현

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -1,8 +1,18 @@
 package bridge;
 
+import static bridge.view.Validator.*;
+
+import bridge.controller.Controller;
+
 public class Application {
 
-    public static void main(String[] args) {
-        // TODO: 프로그램 구현
-    }
+	public static void main(String[] args) {
+		Controller controller = new Controller();
+		try {
+			controller.init();
+			controller.play(controller.getGame());
+		} catch (IllegalArgumentException e) {
+			System.out.println(INVALID_INPUT);
+		}
+	}
 }
--- src/main/java/bridge/controller/Controller.java
+++ src/main/java/bridge/controller/Controller.java
@@ -1,2 +1,52 @@
-package bridge.controller;public class Controller {
+package bridge.controller;
+
+import static bridge.controller.Constant.*;
+
+import java.util.List;
+
+import bridge.view.InputView;
+import bridge.view.OutputView;
+
+public class Controller {
+	private BridgeGame game;
+	List<String> state;
+	private String command = "";
+	private int size;
+
+	public void init() {
+		OutputView.printStartMessage();
+		this.size = InputView.readBridgeSize();
+		this.game = new BridgeGame(size);
+	}
+
+	public void play(BridgeGame game) {
+		logic(game);
+		OutputView.printResultWin(state, game.getTryCount());
+	}
+
+	private void logic(BridgeGame game) {
+		for (int i = 0; i < size; i++) {
+			String location = InputView.readMoving();
+			state = game.move(location, i);
+			OutputView.printMap(state);
+			if (game.checkRetry()) {
+				command = InputView.readGameCommand();
+				retryLogic(game);
+			}
+		}
+	}
+
+	private void retryLogic(BridgeGame game) {
+		if (command.equals(RETRY_GAME)) {
+			game.retry();
+			play(this.game);
+		}
+		if (command.equals(QUIT_GAME)) {
+			OutputView.printResultLose(state, game.getTryCount());
+		}
+	}
+
+	public BridgeGame getGame() {
+		return game;
+	}
 }
