02b87a244441dd2efc58a1c2348a6dd32176d316
qdeu22 <qdeu22@gmail.com>
2022-11-18 15:39:17 +0900

    feat(bridge): 필요 없는 1 이상의 숫자가 아니면 예외처리기능 삭제

--- src/main/java/bridge/InputView.java
+++ src/main/java/bridge/InputView.java
@@ -18,42 +18,31 @@ public class InputView {
     //숫자 검증
     public int numberValidate(String size) {
         try {
-            return negativeValidate(Integer.parseInt(size));
+            return rangeValidate(Integer.parseInt(size));
         } catch (NumberFormatException e) {
             System.out.println("[ERROR] 숫자가 아닙니다.");
-            return readBridgeSize();
         }
+        return readBridgeSize();
     }
 
-    //1 이상 검증
-    public int negativeValidate(int size) {
-        try {
-            if (size <= 0) {
-                throw new IllegalArgumentException("[ERROR] 1 이상의 숫자를 입력해주세요.");
-            }
-        } catch (IllegalArgumentException e) {
-            System.out.println(e.getMessage());
-            return readBridgeSize();
-        }
-        return rangeValidate(size);
-    }
     //3에서 20사이 검증
     public int rangeValidate(int size) {
         try {
             if (!(size >= 3 && size <= 20)) {
                 throw new IllegalArgumentException("[ERROR] 3에서 20사이의 숫자를 입력해주세요.");
             }
+            return size;
         } catch (IllegalArgumentException e) {
             System.out.println(e.getMessage());
-            return readBridgeSize();
         }
-        return size;
+        return readBridgeSize();
     }
 
     /**
      * 사용자가 이동할 칸을 입력받는다.
      */
     public String readMoving() {
+        System.out.println("이동할 칸을 선택해주세요. (위: U, 아래: D)");
         return null;
     }
 
