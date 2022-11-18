21417316689415d4ceb3ac760a128dffd2dd8789
SHEOMM <esh0821@snu.ac.kr>
2022-11-18 14:07:30 +0900

    feat: Direction 모델 구현
    사용자의 다리 건너기 방향을 나타내는 모델 구현
    - UP('U', 1)
    - DOWN('D', 0)

---
+++ src/main/java/bridge/controller/Direction.java
@@ -0,0 +1,22 @@
+package bridge.controller;
+
+public enum Direction {
+    UP("U", 1),
+    DOWN("D", 0);
+
+    private final String input;
+    private final int value;
+
+    Direction(String input, int value){
+        this.input = input;
+        this.value = value;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public String getInput() {
+        return input;
+    }
+}
