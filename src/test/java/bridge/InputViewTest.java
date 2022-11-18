e09f6c94796f539e64b418d30d33fddbe0fd6535
jsc1014 <wotlr5209@gmail.com>
2022-11-19 00:49:59 +0900

    test: 다리길이 입력 예외 테스트 3~20을 벗어난 숫자 입력시 예외처리 숫자가 아닌 값 입력시 예외처리

--- src/test/java/bridge/InputViewTest.java
+++ src/test/java/bridge/InputViewTest.java
@@ -1,4 +1,31 @@
-import static org.junit.jupiter.api.Assertions.*;
+package bridge;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+
 class InputViewTest {
-  
+
+    @DisplayName("다리의 길이를 3~20에서 벗어난 숫자를 입력한 경우 예외처리")
+    @Test
+    void checkBridgeSizeOutRange(){
+        assertThatThrownBy(() -> InputView.errorBridgeSizeOverRange("21"))
+                .isInstanceOf(IllegalArgumentException.class);
+
+        assertThatThrownBy(() -> InputView.errorBridgeSizeOverRange("2"))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("다리의 길이를 숫자가 아닌 것을 입력시 예외처리")
+    @Test
+    void checkBridgInputNotNumber(){
+        assertThatThrownBy(() -> InputView.errorBridgeSizeOverRange("a"))
+                .isInstanceOf(IllegalArgumentException.class);
+
+        assertThatThrownBy(() -> InputView.errorBridgeSizeOverRange("!!"))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
 }
