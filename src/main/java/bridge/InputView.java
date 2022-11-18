a0c5d991dd20dfbb45c32344d1180e177d99274d
sunjinb <tjswls8062@skuniv.ac.kr>
2022-11-18 13:46:02 +0900

    feat(bridge) : 게임 재시도 여부 입력 기능 구현

--- src/main/java/bridge/InputView.java
+++ src/main/java/bridge/InputView.java
@@ -9,6 +9,7 @@ public class InputView {
     static Scanner sc = new Scanner(System.in);
     static int bridgeSize;
     static String movingPoint;
+    static String exitPoint;
     /**
      * 다리의 길이를 입력받는다.
      */
@@ -45,7 +46,16 @@ public class InputView {
     /**
      * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
      */
-    public String readGameCommand() {
-        return null;
+    public static String readGameCommand() {
+        try {
+            System.out.println("게임을 다시 시도할지 여부를 입력해주세요. (재시도 : R, 종료 : Q)");
+            exitPoint = sc.next();
+            if (!(exitPoint.equals("R") || exitPoint.equals("Q")))
+                throw new IllegalArgumentException();
+        } catch (IllegalArgumentException e) {
+            System.out.println("[ERROR] 잘못된 여부 선택");
+            readGameCommand();
+        }
+        return exitPoint;
     }
 }
