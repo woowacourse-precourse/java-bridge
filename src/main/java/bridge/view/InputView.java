5bc0361265a188c6d84273ab5ab281f75d5517d4
ddubidubap <park62000@gmail.com>
2022-11-18 14:17:53 +0900

    refactor: 입력 받는 클래스와 입력값 확인하는 클래스 분리

--- src/main/java/bridge/view/InputView.java
+++ src/main/java/bridge/view/InputView.java
@@ -3,60 +3,23 @@ package bridge.view;
 import static camp.nextstep.edu.missionutils.Console.readLine;
 
 public class InputView {
-    static final int MIN_BRIDGE_SIZE = 3;
-    static final int MAX_BRIDGE_SIZE = 20;
-    static final String ERROR = "[ERROR] ";
-    static final String IS_NOT_NUMBER = ERROR + "다리 길이는 숫자(정수)여야 합니다.";
-    static final String IS_NOT_IN_RANGE = ERROR + "다리 길이는 3 이상, 20 이하의 숫자여야 합니다.";
-    static final String IS_NOT_PROPER_MOVING = ERROR + "이동할 칸은 대문자 U 또는 D 중 하나여야 합니다.";
-    static final String IS_NOT_PROPER_COMMAND = ERROR + "게임 재시작 여부는 대문자 R 또는 Q 중 하나여야 합니다.";
+    Validator validator = new Validator();
 
     public int readBridgeSize() {
         String bridgeSize = readLine();
-        validateBridgeSize(bridgeSize);
+        validator.validateBridgeSize(bridgeSize);
         return Integer.parseInt(bridgeSize);
     }
 
     public String readMoving() {
         String moving = readLine();
-        validateMoving(moving);
+        validator.validateMoving(moving);
         return moving;
     }
 
     public String readGameCommand() {
         String command = readLine();
-        validateGameCommand(command);
+        validator.validateGameCommand(command);
         return command;
-    }
-
-    private void validateBridgeSize(String input) {
-        int bridgeSize = strToInt(input);
-        isBetweenRange(bridgeSize);
-    }
-
-    private void validateMoving(String input) {
-        if (!input.equals("D") && !input.equals("U")) {
-            throw new IllegalArgumentException(IS_NOT_PROPER_MOVING);
-        }
-    }
-
-    private void validateGameCommand(String input) {
-        if (!input.equals("R") && !input.equals("Q")) {
-            throw new IllegalArgumentException(IS_NOT_PROPER_COMMAND);
-        }
-    }
-
-    private int strToInt(String input) {
-        try {
-            return Integer.parseInt(input);
-        } catch (NumberFormatException e) {
-            throw new IllegalArgumentException(IS_NOT_NUMBER);
-        }
-    }
-
-    private void isBetweenRange(int input) {
-        if (input < MIN_BRIDGE_SIZE || input > MAX_BRIDGE_SIZE) {
-            throw new IllegalArgumentException(IS_NOT_IN_RANGE);
-        }
     }
 }
