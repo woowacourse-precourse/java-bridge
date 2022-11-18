9fe5f37645517e6d03758715e7c140185b3dd9da
JeonghakLee <wjdgkr0527@naver.com>
2022-11-18 13:33:06 +0900

    refactor: 타입 변환시 검증 하도록 수정

--- src/main/java/bridge/domain/BridgeCellType.java
+++ src/main/java/bridge/domain/BridgeCellType.java
@@ -3,26 +3,23 @@ package bridge.domain;
 import java.util.Arrays;
 
 public enum BridgeCellType {
-    UP("U"),
-    DOWN("D"),
 
-    ;
+    DOWN("D"),
+    UP("U");
+    private static final String INVALID_BRIDGE_CELL_TYPE_MESSAGE = "이동할 칸이 올바르지 않습니다. (U or D)";
     private final String cellType;
 
     BridgeCellType(String cellType) {
         this.cellType = cellType;
     }
 
-    public static boolean isNotBridgeCellType(String type) {
-        return Arrays.stream(values())
-            .noneMatch(value -> value.cellType.equals(type));
-    }
-
     public static BridgeCellType of(String type) {
         return Arrays.stream(values())
-            .filter(value -> value.cellType.equals(type)).findFirst()
-            .orElseThrow(IllegalArgumentException::new);
+            .filter(value -> value.cellType.equals(type))
+            .findFirst()
+            .orElseThrow(() -> new IllegalArgumentException(INVALID_BRIDGE_CELL_TYPE_MESSAGE));
     }
+
 
     public String getCellType() {
         return cellType;
