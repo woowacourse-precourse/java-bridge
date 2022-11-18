fcd24d0c73933172732527ec988864e3354959a8
panorama713 <yaiba713@gmail.com>
2022-11-18 15:24:53 +0900

    refactor(bridge): 사용자가 칸을 이동할 떄 사용하는 메서드 수정

--- src/main/java/bridge/BridgeGame.java
+++ src/main/java/bridge/BridgeGame.java
@@ -1,20 +1,24 @@
 package bridge;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * 다리 건너기 게임을 관리하는 클래스
  */
 public class BridgeGame {
     private final static String moveError = "[ERROR] 어디에도 포함되지 않는 값입니다.";
+    InputView inputView = new InputView();
+    OutputView outputView = new OutputView();
+    private List<String> moveMapList = new ArrayList<>();
     /**
      * 사용자가 칸을 이동할 때 사용하는 메서드
      * <p>
      * 이동을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
     public String move(String randomBridge, String input) {
-        if(randomBridge == input) return "O";
-        if(randomBridge != input) return "X";
-        System.out.println(moveError);
-        throw new IllegalArgumentException();
+        if(randomBridge == input) moveMapList.add("O");
+        if(randomBridge != input) moveMapList.add("X");
     }
 
     /**
@@ -23,8 +27,6 @@ public class BridgeGame {
      * 재시작을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
     public void retry() {
-        InputView inputView = new InputView();
-        OutputView outputView = new OutputView();
         if(inputView.readGameCommand().equals("R")){
             inputView.readMoving();
         }
