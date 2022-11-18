a604c8f6da1b2a5b94714c4e20b2b09a0c843102
ddingmin <ddingminyt@gmail.com>
2022-11-19 01:45:08 +0900

    feat(java-bridge): BridgeGame 다리 사이즈 검증 및 생성자 구현

--- src/main/java/bridge/domain/BridgeGame.java
+++ src/main/java/bridge/domain/BridgeGame.java
@@ -1,9 +1,17 @@
 package bridge.domain;
 
+import bridge.ui.Exception;
 /**
  * 다리 건너기 게임을 관리하는 클래스
  */
 public class BridgeGame {
+    private final Exception exception = new Exception();
+    private final int bridgeSize;
+
+    public BridgeGame(int bridgeSize) {
+        validateBridgeSize(bridgeSize);
+        this.bridgeSize = bridgeSize;
+    }
 
     /**
      * 사용자가 칸을 이동할 때 사용하는 메서드
@@ -19,5 +27,12 @@ public class BridgeGame {
      * 재시작을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
     public void retry() {
+    }
+
+    public void validateBridgeSize(int bridgeSize) {
+        if (3 <= bridgeSize && bridgeSize <= 20) {
+            return;
+        }
+        exception.exceptionBridgeSize();
     }
 }
