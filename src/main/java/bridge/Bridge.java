36103c5988c0f899fea0c1ce3522dbef11c117ed
saint6839 <saint6839@gmail.com>
2022-11-18 18:12:40 +0900

    refactor(domain) : length 필드 제거, bridgeAnswers의 size로 다리 길이 유효성 검증 하도록 수정

--- src/main/java/bridge/Bridge.java
+++ src/main/java/bridge/Bridge.java
@@ -1,17 +1,15 @@
 package bridge;
 
-import java.util.ArrayList;
 import java.util.List;
 
 public class Bridge {
     private static final String INVALID_LENGTH_RANGE_MESSAGE = "[ERROR] 다리 길이는 3이상 20이하의 숫자를 입력해주세요.";
-    private final int length;
+
     private final List<String> bridgeAnswers;
 
-    public Bridge(int length) {
-        validateLengthRange(length);
-        this.length = length;
-        bridgeAnswers = new ArrayList<>();
+    public Bridge(List<String> bridgeAnswers) {
+        validateLengthRange(bridgeAnswers.size());
+        this.bridgeAnswers = bridgeAnswers;
     }
 
     private void validateLengthRange(int length) {
@@ -19,10 +17,6 @@ public class Bridge {
             return;
         }
         throw new IllegalArgumentException(INVALID_LENGTH_RANGE_MESSAGE);
-    }
-
-    public int getLength() {
-        return length;
     }
 
     public List<String> getBridgeAnswers() {
