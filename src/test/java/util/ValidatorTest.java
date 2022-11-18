b3c07e7aeff25da4da5d1dd6f6cc97f3e0336e39
Devtist <yt930310@snu.ac.kr>
2022-11-18 21:29:23 +0900

    refactor: 검증할 예외를 추가하여 테스트코드 리팩터링

--- src/test/java/util/ValidatorTest.java
+++ src/test/java/util/ValidatorTest.java
@@ -10,14 +10,14 @@ import org.junit.jupiter.params.provider.ValueSource;
 
 class ValidatorTest {
 	@DisplayName("숫자가 아닌 다리 길이 입력에 대해 예외를 발생한다.")
-	@ValueSource(strings = {"25.1", "abcde", "가나다라", "1ㅁ2a3"})
+	@ValueSource(strings = {"25.1", "abcde", "가나다라", "1ㅁ2a3", "", "-1"})
 	@ParameterizedTest
 	void validateSizeIsNumber(String strings) {
 		assertThatThrownBy(() -> Validator.validateSizeIsNumber(strings)).isInstanceOf(IllegalArgumentException.class);
 	}
 
 	@DisplayName("U와 D가 아닌 이동할 칸 입력에 대한 예외를 발생한다.")
-	@ValueSource(strings = {"25.1", "abcde", "가나다라", "1ㅁ2a3"})
+	@ValueSource(strings = {"25.1", "abcde", "가나다라", "1ㅁ2a3", "u"})
 	@ParameterizedTest
 	void validateCellPosition(String strings) {
 		assertThatThrownBy(() -> Validator.validateCellPosition(strings)).isInstanceOf(IllegalArgumentException.class);
