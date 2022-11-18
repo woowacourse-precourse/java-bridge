3bd2ec33b02a4194566634b922b2caa2d5850ba6
cocomozila <joseyun94@naver.com>
2022-11-19 02:41:32 +0900

    feat(Setting): 이동시 구현할 게임 지도를 표현할 `O`, `X` 상수 추가

--- src/main/java/bridge/setting/Setting.java
+++ src/main/java/bridge/setting/Setting.java
@@ -3,9 +3,11 @@ package bridge.setting;
 public class Setting {
 
     public static final String MOVE_UP_KEY = "U";
-    public static final String MOVE_DOWN_KEY = "K";
+    public static final String MOVE_DOWN_KEY = "D";
     public static final String RETRY_BUTTON = "R";
     public static final String EXIT_BUTTON = "Q";
     public static final int MAX_BRIDGE_SIZE = 20;
     public static final int MIN_BRIDGE_SIZE = 3;
+    public static final String KEY_MATCH = "O";
+    public static final String KEY_NOT_MATCH = "X";
 }
