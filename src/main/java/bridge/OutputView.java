f540ca4a688c865b9cd08ef85372c117176ea512
seoyoung <sy9723@korea.ac.kr>
2022-11-18 11:07:58 +0900

    feat: 기능 추가 (해당 라운드의 결과를 출력한다)

--- src/main/java/bridge/OutputView.java
+++ src/main/java/bridge/OutputView.java
@@ -1,5 +1,10 @@
 package bridge;
 
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 /**
  * 사용자에게 게임 진행 상황과 결과를 출력하는 역할을 한다.
  */
@@ -22,7 +27,39 @@ public class OutputView {
      * <p>
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void printMap() {
+    /** success: 0 -> down 성공, 1 -> down 실패, 2 -> up 성공, 3 -> up 실패 **/
+    public void printMap(List<Integer> movingSuccess) {
+        List<String> upPrintChecker = Arrays.asList("   ", "   ", " O ", " X ");
+        List<String> downPrintChecker = Arrays.asList(" O ", " X ", "   ", "   ");
+        String upBridge = createBridge(movingSuccess, upPrintChecker);
+        String downBridge = createBridge(movingSuccess, downPrintChecker);
+        System.out.println("[" + upBridge + "]");
+        System.out.println("[" + downBridge + "]");
+    }
+
+    private String createBridge(List<Integer> movingSuccess, List<String> PrintChecker){
+        String printBridge = "";
+        for (int i = 0; i < movingSuccess.size(); i++){
+            printBridge = printBridge + PrintChecker.get(movingSuccess.get(i));
+            if (i != movingSuccess.size()-1) {
+                printBridge += "|";
+            }
+        }
+        return printBridge;
+    }
+
+    private Map<String, Object> createUpMovingChecker() {
+        Map<String, Object> movingChecker = new HashMap<>();
+        movingChecker.put("U", " O ");
+        movingChecker.put("D", "   ");
+        return  movingChecker;
+    }
+
+    private Map<String, Object> createDownMovingChecker() {
+        Map<String, Object> movingChecker = new HashMap<>();
+        movingChecker.put("U", "   ");
+        movingChecker.put("D", " O ");
+        return  movingChecker;
     }
 
     /**
