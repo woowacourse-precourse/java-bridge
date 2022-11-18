590dd1c7fa84e522e37fc79555c9334888528858
jootang2 <joohwan.song15b@gmail.com>
2022-11-18 19:40:42 +0900

    :sparkles: 2-1. 1번 다리 생성

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -1,12 +1,19 @@
 package bridge;
 
+import java.util.List;
+
 public class Application {
 
     public static void main(String[] args) {
         InputView inputView = new InputView();
+        BridgeNumberGenerator bridgeNumberGenerator = new BridgeRandomNumberGenerator();
+        BridgeMaker bridgeMaker = new BridgeMaker(bridgeNumberGenerator);
 
         inputView.startMsg();
 
         int bridgeSize = inputView.readBridgeSize();
+
+        List<String> firstBridge = bridgeMaker.makeBridge(bridgeSize);
+
     }
 }
