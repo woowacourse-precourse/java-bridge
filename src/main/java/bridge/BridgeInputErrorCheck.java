d9ecb6f9f32ecfe5a5d1cee188b72bfd7acb2e74
Seokhyun Lee <lseokhyun99@gmail.com>
2022-11-18 15:23:40 +0900

    feat: 게임 재시작 종료 커맨드 오류 체크 메소드 구현
    게임 재시작 종료 커맨드 오류 체크 메소드
    readGameCommandErrorCheck()를 구현하였다.
    
    입력된 커맨드가
    "R", "Q"가 아니면 에러처리한다.

--- src/main/java/bridge/BridgeInputErrorCheck.java
+++ src/main/java/bridge/BridgeInputErrorCheck.java
@@ -7,11 +7,14 @@ public class BridgeInputErrorCheck {
     private static final String NOT_NUMBER_ERROR = ERROR_MESSAGE+" 숫자를 입력해야 합니다.";
     private static final String NUMBER_SIZE_ERROR = ERROR_MESSAGE+" 다리 길이는 3부터 20 사이의 숫자여야 합니다.";
     private static final String MOVING_SPACE_ERROR = ERROR_MESSAGE+" 이동할 칸은 U 혹은 D 중에서 선택해야 합니다.";
+    private static final String COMMAND_ERROR = ERROR_MESSAGE+" 재시도 : R, 종료 : Q 중에서 선택해야 합니다.";
     private static final String NUMBER_PATTERN = "^[0-9]{1,}$";
     private static final int BRIDGE_SIZE_MIN = 3;
     private static final int BRIDGE_SIZE_MAX = 20;
     private static final String MOVE_UP = "U";
     private static final String MOVE_DOWN = "D";
+    private static final String RETRY = "R";
+    private static final String QUIT = "Q";
 
 
     public static void readBridgeSizeErrorCheck(String bridgeSize){
@@ -26,5 +29,10 @@ public class BridgeInputErrorCheck {
     public static void readMovingErrorCheck(String movingSpace){
         if ( !movingSpace.equals(MOVE_UP) && !movingSpace.equals(MOVE_DOWN))
             throw new IllegalArgumentException(MOVING_SPACE_ERROR);
+    }
+
+    public static void readGameCommandErrorCheck(String command){
+        if ( !command.equals(RETRY) && !command.equals(QUIT))
+            throw new IllegalArgumentException(COMMAND_ERROR);
     }
 }
