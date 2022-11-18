362f103dc01f3a752472e54335ad0530bca6228b
charlesuu <tiger0192004@gmail.com>
2022-11-18 18:15:13 +0900

    feat: 브릿지 생성 기능 구현

--- src/main/java/bridge/BridgeMaker.java
+++ src/main/java/bridge/BridgeMaker.java
@@ -1,5 +1,6 @@
 package bridge;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -11,7 +12,6 @@ import java.util.List;
  * BridgeMaker의 메서드의 시그니처(인자, 이름)와 반환 타입은 변경할 수 없다.
  */
 public class BridgeMaker {
-
     private final BridgeNumberGenerator bridgeNumberGenerator;
 
     public BridgeMaker(BridgeNumberGenerator bridgeNumberGenerator) {
@@ -23,6 +23,19 @@ public class BridgeMaker {
      * @return 입력받은 길이에 해당하는 다리 모양. 위 칸이면 "U", 아래 칸이면 "D"로 표현해야 한다.
      */
     public List<String> makeBridge(int size) {
-        return null;
+        ArrayList<String> bridge = new ArrayList<>();
+        for (int i = 0; i < size; i++) {
+            int randomNumber = bridgeNumberGenerator.generate();
+            enrollBridgePath(bridge, randomNumber);
+        }
+        return bridge;
+    }
+
+    private void enrollBridgePath(ArrayList<String> bridge, int randomNumber) {
+        if (randomNumber == 0) {
+            bridge.add("D");;
+            return;
+        }
+        bridge.add("U");
     }
 }
--- src/main/java/bridge/controller/BridgeController.java
+++ src/main/java/bridge/controller/BridgeController.java
@@ -1,5 +1,28 @@
 package bridge.controller;
 
+import bridge.BridgeMaker;
+import bridge.BridgeNumberGenerator;
+import bridge.BridgeRandomNumberGenerator;
+import bridge.view.InputView;
+
+import java.util.List;
+
 public class BridgeController {
+    InputView inputView;
+    BridgeMaker bridgeMaker;
+    BridgeNumberGenerator bridgeNumberGenerator;
+
+    public BridgeController() {
+        inputView = new InputView();
+        bridgeNumberGenerator = new BridgeRandomNumberGenerator();
+        bridgeMaker = new BridgeMaker(bridgeNumberGenerator);
+    }
+
+    public void startProgram() {
+        int bridgeSize = inputView.readBridgeSize();
+        List<String> bridge = bridgeMaker.makeBridge(bridgeSize);
+        //이어서 만들기
+
+    }
 
 }
