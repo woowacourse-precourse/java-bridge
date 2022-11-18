a573bf5bcd3dbd009417fe7458732f837c91deab
Hayoon <hy97@sju.ac.kr>
2022-11-18 23:28:46 +0900

    refactor(GameMachine): 게임 실행 컨트롤러 메서드 리팩토링

--- src/main/java/bridge/controller/GameMachine.java
+++ src/main/java/bridge/controller/GameMachine.java
@@ -1,5 +1,6 @@
 package bridge.controller;
 
+import bridge.model.Bridge;
 import bridge.view.InputView;
 import bridge.view.MessageView;
 import bridge.view.OutputView;
@@ -11,10 +12,11 @@ public class GameMachine {
     public GameMachine(InputView inputView, OutputView outputView) {
         this.inputView = inputView;
         this.outputView = outputView;
+        play();
     }
     void play() {
-        System.out.println(MessageView.PLAY_BRIDGE_GAME.getMessage());
-        System.out.println(MessageView.INPUT_BRIDGE_LENGTH);
         inputView.readBridgeSize();
+        inputView.readMoving();
+
     }
 }
