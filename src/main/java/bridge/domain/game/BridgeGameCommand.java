adec9b92fa7a057a515eeceb6fb23beeae52bd49
hoonn9 <hun9935@gmail.com>
2022-11-18 17:03:30 +0900

    feat(BridgeGameCommand): findByCommand 메소드 구현

--- src/main/java/bridge/domain/game/BridgeGameCommand.java
+++ src/main/java/bridge/domain/game/BridgeGameCommand.java
@@ -1,6 +1,7 @@
 package bridge.domain.game;
 
 import java.util.Arrays;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 public enum BridgeGameCommand {
@@ -20,5 +21,11 @@ public enum BridgeGameCommand {
     public static boolean isContain(String command) {
         return Arrays.stream(BridgeGameCommand.values()).map(BridgeGameCommand::getCommand).collect(Collectors.toList())
                 .contains(command);
+    }
+    
+    public static Optional<BridgeGameCommand> findByCommand(String command) {
+        return Arrays.stream(BridgeGameCommand.values())
+                .filter(bridgeGameCommand -> bridgeGameCommand.getCommand().equals(command))
+                .findAny();
     }
 }
