986e1b5044820945c25a9b75cb6426d4fe5951f7
cuttlefish55 <funade7@gmail.com>
2022-11-18 13:48:17 +0900

    feat: Add contents of BridgeMaker.java

--- src/main/java/bridge/BridgeMaker.java
+++ src/main/java/bridge/BridgeMaker.java
@@ -1,6 +1,7 @@
 package bridge;
 
 import java.util.List;
+import java.util.ArrayList;
 
 /**
  * 다리의 길이를 입력 받아서 다리를 생성해주는 역할을 한다.
@@ -18,6 +19,23 @@ public class BridgeMaker {
      * @return 입력받은 길이에 해당하는 다리 모양. 위 칸이면 "U", 아래 칸이면 "D"로 표현해야 한다.
      */
     public List<String> makeBridge(int size) {
-        return null;
+        List<String> bridge = new ArrayList<>();
+        int bridgePiece;
+        for(int shape = 0; shape < size; shape ++){
+            bridgePiece = bridgeNumberGenerator.generate();
+            bridge.add(makeBridgePiece(bridgePiece));
+        }
+        return bridge;
+    }
+
+    /**
+     * @param bridgePiece BridgeRandomNumberGenerator로 생성된 랜덤 숫자 (0 또는 1)
+     * @return 0이면 D, 그렇지 않으면 U를 리턴함.
+     */
+    public String makeBridgePiece(int bridgePiece){
+        if(bridgePiece == 0){
+            return "D";
+        }
+        return "U";
     }
 }
