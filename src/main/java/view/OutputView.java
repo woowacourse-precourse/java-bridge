9d723568b7e18982378524477b7110a305dd85bf
KangSungBum <peter9706@naver.com>
2022-11-18 17:16:30 +0900

    refactor(OutputView.java): refactor printGameStartMessage method
    출력 요구사항에 맞게 printGameStartMessage 메소드 수정
    - System.out.println() 추가

--- src/main/java/view/OutputView.java
+++ src/main/java/view/OutputView.java
@@ -23,6 +23,7 @@ public class OutputView {
 
     public void printGameStartMessage() {
         System.out.println("다리 건너기 게임을 시작합니다.");
+        System.out.println();
     }
 
     public void printBridgeSizeRequestMessage() {
