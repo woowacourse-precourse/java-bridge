b2ca7e2dc5bd17ebf08c6c51dc999fedabeb5754
tenta <eunoia3802@gmail.com>
2022-11-18 15:00:53 +0900

    feat: 다리를 건널 수 있는지의 유무 판정 기능 구현

--- src/main/java/bridge/BridgeGame.java
+++ src/main/java/bridge/BridgeGame.java
@@ -1,5 +1,7 @@
 package bridge;
 
+import java.util.List;
+
 /**
  * 다리 건너기 게임을 관리하는 클래스
  */
@@ -13,11 +15,21 @@ public class BridgeGame {
     public void move() {
     }
 
-    /**
-     * 사용자가 게임을 다시 시도할 때 사용하는 메서드
-     * <p>
-     * 재시작을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
-     */
-    public void retry() {
+    public boolean isCrossBridge(List<String> bridge, String sideToMove) {
+        for (int index = 0; index < bridge.size(); index++) {
+            String block = bridge.get(index);
+            if (block == sideToMove) {
+                return true;
+            }
+        }
+        return false;
     }
-}
+
+        /**
+         * 사용자가 게임을 다시 시도할 때 사용하는 메서드
+         * <p>
+         * 재시작을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
+         */
+        public void retry () {
+        }
+    }
