b9dfceed04eea371cafd5975495e3f35b530c5bf
2021110806 <2021110806@dgu.ac.kr>
2022-11-18 19:31:46 +0900

    feat(Output):게임 재시작 여부 확인 메시지 추가

--- src/main/java/bridge/Output.java
+++ src/main/java/bridge/Output.java
@@ -5,6 +5,7 @@ public enum Output {
     gameStart("다리 건너기 게임을 시작합니다."),
     bridgeLengthInputRequest("다리의 길이를 입력해주세요."),
     whereToGoInputRequest("이동할 칸을 선택해주세요. (위: U, 아래: D)"),
+    continueOrEndRequest("게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)"),
     winOrLoseResult("게임 성공 여부: "),
     howManyAttempts("총 시도한 횟수: ");
 
