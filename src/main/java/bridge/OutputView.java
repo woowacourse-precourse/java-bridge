f2a88761e9214c28bb2f17159700eb22f9816fd1
jootang2 <joohwan.song15b@gmail.com>
2022-11-18 21:16:39 +0900

    :sparkles: 6-3. 총 시도한 횟수 출력

--- src/main/java/bridge/OutputView.java
+++ src/main/java/bridge/OutputView.java
@@ -3,6 +3,7 @@ package bridge;
 public class OutputView {
 
     static String result;
+    static Integer count = 1;
     static String successOrFail;
 
     public void printMap(String[] bridgeMap) {
@@ -13,6 +14,7 @@ public class OutputView {
         result = "[ " + bridgeMap[0] + " ]\n" + "[ " + bridgeMap[1] + " ]";
         successOrFail = "성공";
         if (bridgeMap[0].contains("X") || bridgeMap[1].contains("X")) {
+            count ++;
             successOrFail = "실패";
             bridgeGame.retry();
         }
@@ -22,5 +24,11 @@ public class OutputView {
         System.out.println("최종 게임 결과");
         System.out.println(result);
         System.out.println("게임 성공 여부: " + successOrFail);
+        if(successOrFail.equals("성공")){
+            System.out.println("총 시도한 횟수: " + count);
+        }
+        if(successOrFail.equals("실패")){
+            System.out.println("총 시도한 횟수: " + (count-1));
+        }
     }
 }
