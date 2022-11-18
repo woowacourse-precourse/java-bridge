2eb8e553741489551c02c6d2e9adef87bc38f16b
chlwodyd9829 <chlwodyd9829@naver.com>
2022-11-18 15:08:54 +0900

    refactor: 진행중인 게임과 저장할 게임 분리

--- src/main/java/bridge/Repository.java
+++ src/main/java/bridge/Repository.java
@@ -5,7 +5,6 @@ import java.util.List;
 
 public class Repository {
 
-    private static final String BLANK = " ";
     /**
      * 사용자가 진행한 모든 게임의 결과를 저장하는 리스트
      */
@@ -13,27 +12,26 @@ public class Repository {
     /**
      * 현재 진행 중인 게임을 저장하는 리스트
      */
-    private Game game;
+    private int times;
 
 
-    public Game getGame() {
-        return game;
+    public Repository() {
+        times = 0;
     }
-    public void init(){
-        game = new Game();
-    }
-    public void write(String direction, String answer){
-        if (direction.equals("U")){
-            game.getPlaying().get(0).add(answer);
-            game.getPlaying().get(1).add(BLANK);
-            return;
-        }
-        game.getPlaying().get(1).add(answer);
-        game.getPlaying().get(0).add(BLANK);
+    public Game getResult() {
+        return result;
     }
 
-    public void save(){
-        if (result.getLocation() < game.getLocation()) {
+    public int getTimes() {
+        return times;
+    }
+
+    public void setTimes(int times) {
+        this.times = times;
+    }
+
+    public void save(Game game){
+        if (result == null || result.getLocation() < game.getLocation()) {
             result = game;
         }
     }
