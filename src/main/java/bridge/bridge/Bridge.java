fb0ab00856286e397c06e70c717bf3a0cf5e739f
GwansuShin <happysun7080@naver.com>
2022-11-18 15:23:35 +0900

    feat(Bridge): 다리 클래스 정의
    생성된 다리를 관리

---
+++ src/main/java/bridge/bridge/Bridge.java
@@ -0,0 +1,53 @@
+package bridge.bridge;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Bridge {
+    private static final String MOVABLE = "O";
+    private static final String IMMOVABLE = "X";
+    private static final String EMPTY = " ";
+
+    private final List<String> bridge;
+    private final List<String> up;
+    private final List<String> down;
+
+    public Bridge(List<String> bridge) {
+        this.bridge = bridge;
+        this.up = new ArrayList<>();
+        this.down = new ArrayList<>();
+    }
+
+    public static Bridge generate(List<String> bridge) {
+        return new Bridge(bridge);
+    }
+
+    public List<List<String>> makeMap() {
+        return Arrays.asList(up, down);
+    }
+
+    public void move(String direction) {
+        //TODO
+    }
+
+    public void refresh() {
+        up.clear();
+        down.clear();
+    }
+
+    public boolean isEnd() {
+        return isAnyFail()
+                || isAllSuccess();
+    }
+
+    public boolean isAnyFail() {
+        return up.contains(IMMOVABLE)
+                || down.contains(IMMOVABLE);
+    }
+
+    public boolean isAllSuccess() {
+        return up.size() == bridge.size()
+                && !isAnyFail();
+    }
+}
