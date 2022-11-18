a9f7b83def347e90a590e76b663500ee1bd52f7e
abbd2 <ajkety00@gmail.com>
2022-11-18 20:02:52 +0900

    Feat(ErrorMessage): "3 이상 20 이하의 숫자를 입력해주세요."를 출력하는 inputBridgeNumber 메서드 구현

--- src/main/java/bridge/ErrorMessage.java
+++ src/main/java/bridge/ErrorMessage.java
@@ -2,6 +2,7 @@ package bridge;
 
 public enum ErrorMessage {
     ERROR_MESSAGE("[ERROR] "),
+    INPUT_BRIDGE_NUMBER("3 이상 20 이하의 숫자를 입력해주세요."),
     INPUT_MOVE_BRIDGE_ERROR("위 칸은 'U', 아래 칸은 'D'를 입력해주세요.");
 
 
@@ -9,6 +10,10 @@ public enum ErrorMessage {
 
     ErrorMessage(String script) {
         this.script = script;
+    }
+
+    public static void inputBridgeNumber() {
+        System.out.println(ERROR_MESSAGE.script + INPUT_BRIDGE_NUMBER.script);
     }
 
     public static void inputMoveBridgeError() {
