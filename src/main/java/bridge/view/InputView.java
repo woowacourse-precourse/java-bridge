4e55ba99f8b21994ca7fefa34df321b0c404a6ea
Guga <103228463+BGuga@users.noreply.github.com>
2022-11-18 15:04:32 +0900

    feat: 다리의 길이를 입력받는 기능 구현

--- src/main/java/bridge/view/InputView.java
+++ src/main/java/bridge/view/InputView.java
@@ -1,15 +1,44 @@
 package bridge.view;
 
+import bridge.BridgeMaker;
+import bridge.BridgeRandomNumberGenerator;
+import bridge.domain.Bridge;
+import camp.nextstep.edu.missionutils.Console;
+
+import java.util.List;
+import java.util.regex.Pattern;
+
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
  */
 public class InputView {
 
+    private static final String INSERT_BRIDGE_SIZE_MESSAGE = "다리의 길이를 입력해주세요.";
+    private static final String REQUIRE_INTEGER_ERROR_MESSAGE = "[ERROR] 숫자를 입력해주세요.";
+
+    OutputView outputView = new OutputView();
+
+
     /**
      * 다리의 길이를 입력받는다.
      */
     public int readBridgeSize() {
-        return 0;
+        outputView.printMessage(INSERT_BRIDGE_SIZE_MESSAGE);
+        String userInput = Console.readLine();
+        try {
+            intValidation(userInput);
+        } catch (IllegalArgumentException e) {
+            outputView.printErrorMessage(e.getMessage());
+            readBridgeSize();
+        }
+        return Integer.parseInt(userInput);
+    }
+
+    private void intValidation(String input) {
+        final String intRegex = "^[0-9]*$";
+        if (Pattern.matches(intRegex, input)) {
+            throw new IllegalArgumentException(new IllegalArgumentException(REQUIRE_INTEGER_ERROR_MESSAGE));
+        }
     }
 
     /**
