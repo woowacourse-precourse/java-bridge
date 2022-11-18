9003aeddc63f7f06dc490ed4d51c5e4f3e7f6583
HyebinShin <hyeviin.dev@gmail.com>
2022-11-18 22:35:35 +0900

    Change: 총 시도한 횟수 증가 방법 수정
    총 시도한 횟수의 초기값은 0
    Application 클래스의 ongoingGame 메소드에 호출되는 setBridgeGameResult()에서 총 시도한 횟수가 1 증가하도록 수정

--- src/main/java/bridge/BridgeGame.java
+++ src/main/java/bridge/BridgeGame.java
@@ -8,11 +8,12 @@ import java.util.*;
  */
 public class BridgeGame {
     private final HashMap<String, List<String>> bridgeGameResult = new HashMap<>();
-    private int totalCount = 0;
+    private static int totalCount = 0;
 
     public void setBridgeGameResult() {
         bridgeGameResult.put("U", new ArrayList<String>());
         bridgeGameResult.put("D", new ArrayList<String>());
+        totalCount++;
     }
 
     public void clear() {
@@ -22,14 +23,16 @@ public class BridgeGame {
         return bridgeGameResult;
     }
 
+    public static int getTotalCount() {
+        return totalCount;
+    }
+
     /**
      * 사용자가 칸을 이동할 때 사용하는 메서드
      * <p>
      * 이동을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
     public boolean move(Bridge player, Bridge answer, int index) {
-        totalCount++;
-        System.out.println("totalCount: "+totalCount);
         String paintGame = getPaintGame(player, answer, index);
         String position = player.getBridge().get(index);
         String otherPosition = getOtherPosition(position);
