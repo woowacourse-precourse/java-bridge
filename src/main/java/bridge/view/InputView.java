a40d5fbe2d40fa832a10198a87f1d5eb57536624
BioBeBe <76714219+JaegeonYu@users.noreply.github.com>
2022-11-18 14:02:06 +0900

    feat: readGameCommad 메소드 작성

--- src/main/java/bridge/view/InputView.java
+++ src/main/java/bridge/view/InputView.java
@@ -60,6 +60,7 @@ public class InputView {
      * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
      */
     public String readGameCommand() {
-        return null;
+        String gameCommandInput = Console.readLine();
+        return gameCommandInput;
     }
 }
