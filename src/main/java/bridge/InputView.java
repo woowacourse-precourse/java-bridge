775ce385d910b8b48695cb4d56a354d8658d1055
Choi Yoon Chan <cyc0227@gachon.ac.kr>
2022-11-18 12:59:22 +0900

    Feat(InputView): 추가 사용자 입력 뷰 기능
    필요한 정보 입출력, 적절한 예외처리

--- src/main/java/bridge/InputView.java
+++ src/main/java/bridge/InputView.java
@@ -1,5 +1,7 @@
 package bridge;
 
+import camp.nextstep.edu.missionutils.Console;
+
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
  */
@@ -9,20 +11,71 @@ public class InputView {
      * 다리의 길이를 입력받는다.
      */
     public int readBridgeSize() {
-        return 0;
+        System.out.println("다리의 길이를 입력해주세요.");
+
+        int bridgeLen = validateBridgeInputIsNumber(Console.readLine());
+        validateBridgeLen(bridgeLen);
+
+        return bridgeLen;
+    }
+
+    private int validateBridgeInputIsNumber(String input) {
+        int bridgeLen;
+        try {
+            bridgeLen = Integer.parseInt(input);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("다리 길이는 숫자로만 입력 가능합니다.");
+        }
+        return bridgeLen;
+    }
+
+    private void validateBridgeLen(int bridgeLen) {
+        if (!(3 <= bridgeLen && bridgeLen <= 20))
+            throw new IllegalArgumentException("다리 길이는 3 이상 20 이하의 숫자를 입력할 수 있습니다.");
     }
 
     /**
      * 사용자가 이동할 칸을 입력받는다.
      */
-    public String readMoving() {
-        return null;
+    public char readMoving() {
+        System.out.println("이동할 칸을 선택해주세요. (위: U, 아래: D)");
+
+        String alphabeticInput = Console.readLine();
+        validateAlphabeticInputLen(alphabeticInput);
+
+        char moving = alphabeticInput.charAt(0);
+        validateMovingFormat(moving);
+
+        return moving;
+    }
+
+    private void validateMovingFormat(char moving) {
+        if (moving != 'U' && moving != 'D')
+            throw new IllegalArgumentException("이동할 칸은 U 혹은 D를 입력하여야 합니다.");
+    }
+
+    private void validateAlphabeticInputLen(String input) {
+        if (input.length() != 1)
+            throw new IllegalArgumentException("길이가 2 이상인 문자열은 입력할 수 없습니다.");
     }
 
     /**
      * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
      */
-    public String readGameCommand() {
-        return null;
+    public char readRestartCommand() {
+        System.out.println("게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)");
+
+        String alphabeticInput = Console.readLine();
+        validateAlphabeticInputLen(alphabeticInput);
+
+        char restartCommand = alphabeticInput.charAt(0);
+        validateRestartCommandFormat(restartCommand);
+
+        return restartCommand;
+    }
+
+    private void validateRestartCommandFormat(char gameCommand) {
+        if (gameCommand != 'R' && gameCommand != 'Q')
+            throw new IllegalArgumentException("게임 재시도 여부는 R 혹은 Q를 입력하여야 합니다.");
     }
 }
