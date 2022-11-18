6b6b50706f47863ece27677bd46726c22b5dc2e4
연주 <50358956+Yeon-jooKim@users.noreply.github.com>
2022-11-18 22:01:38 +0900

    test: 게임 시도 횟수 출력

--- src/test/java/bridge/OutputViewTest.java
+++ src/test/java/bridge/OutputViewTest.java
@@ -13,7 +13,7 @@ class OutputViewTest {
     OutputView outputView = new OutputView();
 
     @Test
-    void 맵_변환_테스트() {
+    void 이동_로그_출력() {
         OutputStream out = new ByteArrayOutputStream();
         System.setOut(new PrintStream(out));
         char[][] map = {{'O', 'O', 'O', ' '}, {' ', ' ', ' ', 'X'}};
@@ -23,5 +23,18 @@ class OutputViewTest {
         assertThat(output).contains("[ O | O | O |   ]",
                 "[   |   |   | X ]"
         );
+    }
+
+    @Test
+    void 게임_결과_출력(){
+        OutputStream out = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(out));
+        outputView.printResult(3,true);
+        String output = out.toString();
+        assertThat(output).contains(
+                "게임 성공 여부: 성공",
+                "총 시도한 횟수: 3"
+        );
+
     }
 }
