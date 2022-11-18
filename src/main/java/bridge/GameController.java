114220278e7462e43fcd6b96b92d6ed7b58da18c
minhee020 <minhee020@naver.com>
2022-11-18 13:06:26 +0900

    feat : 게임의 기본 뼈대 설꼐

---
+++ src/main/java/bridge/GameController.java
@@ -0,0 +1,22 @@
+package bridge;
+
+public class GameController {
+    public void start() {
+        System.out.println("다리 건너기 게임을 시작합니다.\n");
+        getBridgeSize();
+        startCrossBridge();
+        getGameResult();
+    }
+
+    private void getBridgeSize() {
+
+    }
+
+    private void startCrossBridge() {
+
+    }
+
+    private void getGameResult() {
+
+    }
+}
