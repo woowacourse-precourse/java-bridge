617f62bf89cfa102900160995da99ea8f169da19
HyeonjungYun <guswnd9971@gmail.com>
2022-11-18 19:54:31 +0900

    "style(BridgeGame): 기존 주석 제거, 주석 추가"

--- src/main/java/bridge/domain/BridgeGame.java
+++ src/main/java/bridge/domain/BridgeGame.java
@@ -5,24 +5,19 @@ import bridge.Application;
 import java.util.ArrayList;
 import java.util.List;
 
-/**
- * 다리 건너기 게임을 관리하는 클래스
- */
+// 다리 건너기 게임을 관리하는 클래스
 public class BridgeGame {
 
     private final List<String> randomBridge;
     public List<String> passHistory;
 
-    /**
-     * 사용자가 칸을 이동할 때 사용하는 메서드
-     * <p>
-     * 이동을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
-     */
+    // 클래스 기본 생성자, 랜덤으로 만들어진 randemBridge객체를 받는다.
     public BridgeGame (List<String> randomBridge) {
         this.randomBridge = randomBridge;
         this.passHistory = new ArrayList<>();
     }
 
+    // 칸 이동을 관할하는 메소드, 이동이 끝났을 때는 false, 게속 진행할 때는 true를 반환한다.
     public boolean move(String choiceUpAndDown) {
         clearBridge();
 
@@ -36,6 +31,7 @@ public class BridgeGame {
         return true;
     }
 
+    // 잘못된 칸을 입력하고 재시작을 선택했을 때 기존의 passHistory리스트를 비워주는 메소드
     private void clearBridge() {
 
         if (passHistory.contains("DX") || passHistory.contains("UX")) {
@@ -43,17 +39,14 @@ public class BridgeGame {
         }
     }
 
+    // 자신이 선택한 칸이 이동 가능한 칸인지 확인하는 메소드
     private boolean canPass (String choiceUpAndDown) {
         this.passHistory.add(choiceUpAndDown);
 
         return randomBridge.get(passHistory.size()-1).equals(choiceUpAndDown);
     }
 
-    /**
-     * 사용자가 게임을 다시 시도할 때 사용하는 메서드
-     * <p>
-     * 재시작을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
-     */
+    // 잘못된 칸을 선택했을 때 재시작할지 프로그램을 종료할지 선택하는 메소드
     public boolean retry() {
         Application app = new Application();
         Application.outcome = false;
@@ -64,6 +57,7 @@ public class BridgeGame {
         return app.selectRetry(passHistory);
     }
 
+    // 잘못된 칸을 선택했을 때 선택한 칸이 틀린 칸인 걸 표시해주는 메소드
     private void addFailTrace () {
 
         if(passHistory.get(passHistory.size()-1).equals("U")) {
