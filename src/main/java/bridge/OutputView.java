11d5762c4644f112b78f56641d27f7de7bc93b6c
ouohoon <begreat@kakao.com>
2022-11-19 00:05:20 +0900

    feat(OutputView): 게임을 재시도할지 종료할지 입력받는 문구 출력 기능 구현

--- src/main/java/bridge/OutputView.java
+++ src/main/java/bridge/OutputView.java
@@ -5,12 +5,12 @@ package bridge;
  */
 public class OutputView {
     private final String startText = "다리 건너기 게임을 시작합니다.";
-    private final String bridgeSizeInputText = "다리의 길이를 입력해주세요.";
-    private final String movingInputText = "이동할 칸을 선택해주세요. (위: U, 아래: D)";
+    private final String bridgeSizeInputText = "\n다리의 길이를 입력해주세요.";
+    private final String movingInputText = "\n이동할 칸을 선택해주세요. (위: U, 아래: D)";
     private final String commandInputText =
-            "게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)";
+            "\n게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)";
     private final String resultText = "최종 게임 결과";
-    private final String isClearText = "게임 성공 여부: ";
+    private final String isClearText = "\n게임 성공 여부: ";
     private final String totalTryText = "총 시도한 횟수: ";
     private final String errorText = "[ERROR] ";
     /**
@@ -25,6 +25,13 @@ public class OutputView {
      */
     public void printBridgeSizeInput() {
         System.out.println(bridgeSizeInputText);
+    }
+
+    /**
+     * 게임을 재시도할지 종료할지 입력받는 문구를 출력한다.
+     */
+    public void printCommandInput() {
+        System.out.println(commandInputText);
     }
 
     /**
