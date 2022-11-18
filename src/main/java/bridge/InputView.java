847a38c1db5a69d2c8a91e479a1db6c1338a44e9
penrose15 <seongju99@naver.com>
2022-11-18 17:33:58 +0900

    feat : add input for direction("U" or "D"), quit("Q" or "R")

--- src/main/java/bridge/InputView.java
+++ src/main/java/bridge/InputView.java
@@ -1,5 +1,7 @@
 package bridge;
 
+import camp.nextstep.edu.missionutils.Console;
+
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
  */
@@ -9,20 +11,38 @@ public class InputView {
      * 다리의 길이를 입력받는다.
      */
     public int readBridgeSize() {
-        return 0;
+        String size = Console.readLine();
+        char[] chars = size.toCharArray();
+
+        for (char aChar : chars) {
+            if(!(0 <= aChar - '0' &&  9>= aChar - '0')) {
+                throw new IllegalArgumentException("[ERROR] Invalid input");
+            }
+        }
+
+        return Integer.parseInt(size);
     }
 
     /**
      * 사용자가 이동할 칸을 입력받는다.
      */
     public String readMoving() {
-        return null;
+        String move = Console.readLine();
+        if(!(move.equals("U") || move.equals("D"))) {
+            throw new IllegalArgumentException("[ERROR] invalid input not U or D");
+        }
+        return move;
     }
 
     /**
      * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
      */
     public String readGameCommand() {
-        return null;
+        String quit =  Console.readLine();
+
+        if(!(quit.equals("Q") || quit.equals("R"))) {
+            throw new IllegalArgumentException("[ERROR] invalid input not Q or R");
+        }
+        return quit;
     }
 }
