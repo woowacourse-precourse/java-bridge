425e89124468da53139f54e937e2105faae6a360
bjk1649 <qhwns1649@naver.com>
2022-11-18 20:35:05 +0900

    fix: 사용자에게 입력을 받기 전 안내메시지를 출력하도록 변경

--- src/main/java/bridge/view/InputView.java
+++ src/main/java/bridge/view/InputView.java
@@ -12,6 +12,7 @@ public class InputView {
      * 다리의 길이를 입력받는다.
      */
     public int readBridgeSize() {
+        System.out.println("다리의 길이를 입력해주세요.");
         String input = Console.readLine();
         InputValidator.validateBridgeSize(input);
         return Integer.parseInt(input);
@@ -21,6 +22,7 @@ public class InputView {
      * 사용자가 이동할 칸을 입력받는다.
      */
     public String readMoving() {
+        System.out.println("이동할 칸을 선택해주세요. (위: U, 아래: D)");
         String input = Console.readLine();
         InputValidator.validateMoving(input);
         return input;
@@ -30,6 +32,7 @@ public class InputView {
      * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
      */
     public String readGameCommand() {
+    System.out.println("게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)");
         String input = Console.readLine();
         InputValidator.validateGameCommand(input);
         return input;
