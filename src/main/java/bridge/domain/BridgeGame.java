20a32823b20d87b78157c4605a80eec326048e37
SHEOMM <esh0821@snu.ac.kr>
2022-11-18 23:58:44 +0900

    feat: 재시작 기능 구현

--- src/main/java/bridge/domain/BridgeGame.java
+++ src/main/java/bridge/domain/BridgeGame.java
@@ -3,29 +3,25 @@ package bridge.domain;
 import bridge.model.Answer;
 import bridge.model.Direction;
 import bridge.model.ErrorMessage;
+import bridge.model.Restart;
 import java.util.List;
-import java.util.Objects;
+import java.util.stream.Collectors;
 
-/**
- * 다리 건너기 게임을 관리하는 클래스
- */
 public class BridgeGame {
-    private final List<String> bridge;
+    private final List<Direction> answer;
     private int step;
 
-    BridgeGame(List<String> bridge){
-        this.bridge = bridge;
+    public BridgeGame(List<String> bridge){
+        this.answer = bridge.stream()
+                .map(Direction::getDirectionByInput)
+                .collect(Collectors.toUnmodifiableList());
         step = 0;
     }
 
-    /**
-     * 사용자가 칸을 이동할 때 사용하는 메서드
-     * <p>
-     * 이동을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
-     */
     public Answer move(String input) {
+        System.out.println(answer);
         isValidDirection(input);
-        String nextAnswer = bridge.get(step);
+        String nextAnswer = answer.get(step).getInput();
         if(input.equals(nextAnswer)){
             step++;
             return Answer.CORRECT;
@@ -33,13 +29,18 @@ public class BridgeGame {
         return Answer.INCORRECT;
     }
 
-    /**
-     * 사용자가 게임을 다시 시도할 때 사용하는 메서드
-     * <p>
-     * 재시작을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
-     */
-    public void retry() {
+    public Restart retry(String input) {
+        isValidRetry(input);
+        Restart result = Restart.getRestartByString(input);
+        if(result == Restart.TRUE){
+            step = 0;
+        }
+        return result;
     }
+
+
+
+
 
     private void isValidDirection(String input){
         if(input.equals(Direction.DOWN.getInput())
@@ -49,4 +50,19 @@ public class BridgeGame {
         throw new IllegalArgumentException(ErrorMessage.INVALID_DIRECTION_VALUE.getOutput());
     }
 
+    private void isValidRetry(String input){
+        if(input.equals(Restart.TRUE.getInput())
+        || input.equals(Restart.FALSE.getInput())){
+            return;
+        }
+        throw new IllegalArgumentException(ErrorMessage.INVALID_RESTART_VALUE.getOutput());
+    }
+
+    public List<Direction> getAnswer() {
+        return answer;
+    }
+
+    public int getStep() {
+        return step;
+    }
 }
