955aeb66acd045d0cc9f8baa8fb4adf602160166
JeonSangYong <tkddyd420@naver.com>
2022-11-18 20:42:54 +0900

    feat : 다리 길이 입력

--- src/main/java/bridge/InputView.java
+++ src/main/java/bridge/InputView.java
@@ -1,15 +1,46 @@
 package bridge;
 
+import camp.nextstep.edu.missionutils.Console;
+
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
  */
 public class InputView {
+    private static final int BRIDGE_SIZE_LOWER = 3;
+    private static final int BRIDGE_SIZE_UPPER = 20;
 
     /**
      * 다리의 길이를 입력받는다.
      */
     public int readBridgeSize() {
+        try {
+            String inputBridgeSize = Console.readLine();
+            validBridgeSize(inputBridgeSize);
+            return Integer.parseInt(inputBridgeSize);
+        } catch (IllegalArgumentException e) {
+            System.out.println("[ERROR] " + e.getMessage());
+            readBridgeSize();
+        }
         return 0;
+    }
+
+    private void validBridgeSize(String inputBridgeSize) {
+        validIsNumber(inputBridgeSize);
+        validBridgeSizeInRange(Integer.parseInt(inputBridgeSize));
+    }
+
+    private void validIsNumber(String number) {
+        boolean check = number.chars()
+                .allMatch(Character::isDigit);
+        if (!check) {
+            throw new IllegalArgumentException("입력한 값이 숫자가 아닙니다.");
+        }
+    }
+
+    private void validBridgeSizeInRange(int number) {
+        if (BRIDGE_SIZE_LOWER > number || number > BRIDGE_SIZE_UPPER) {
+            throw new IllegalArgumentException("다리 길이가 3 이상 20 이하가 아닙니다.");
+        }
     }
 
     /**
