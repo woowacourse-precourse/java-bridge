39e4d132a354a1b919473fd4d6582ae27a761e73
Kong-hana01 <gksqlsl11@khu.ac.kr>
2022-11-18 12:22:26 +0900

    feat(GameStart): GameStart 생성자 작성

--- src/main/java/bridge/GameStart.java
+++ src/main/java/bridge/GameStart.java
@@ -1,5 +1,30 @@
 package bridge;
 
+import bridge.bridgeMaker.BridgeMaker;
+import bridge.bridgeMaker.BridgeNumberGenerator;
+import bridge.bridgeMaker.BridgeRandomNumberGenerator;
+import bridge.input.InputView;
+
+import java.util.List;
+
 public class GameStart {
 
+    private BridgeGame bridgeGame;
+    private InputView inputView;
+    private OutputView outputView;
+    private BridgeMaker bridgeMaker;
+    private int bridgeSize;
+    private List<String> bridge;
+
+    public GameStart() {
+        inputView = new InputView();
+        bridgeMaker = new BridgeMaker(new BridgeRandomNumberGenerator());
+        bridgeGame = new BridgeGame();
+        outputView = new OutputView();
+    }
+
+    public void run() {
+
+    }
+    g
 }
