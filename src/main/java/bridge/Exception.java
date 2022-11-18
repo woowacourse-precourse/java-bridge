a7e1db48d56bdb4cb28581f5adc182f750c51ae1
정현수 <96504592+hyunsb@users.noreply.github.com>
2022-11-18 14:42:04 +0900

    feat: 입력 받은 게임 재시작 문자열이 'R'혹은 'Q'가 아닐 시 예외 처리하는 기능 추가

--- src/main/java/bridge/Exception.java
+++ src/main/java/bridge/Exception.java
@@ -8,8 +8,12 @@ public class Exception {
 
     private static final int MAX_BRIDGE_SIZE = 20;
     private static final int MIN_BRIDGE_SIZE = 3;
+
     private static final String USER_MOVE_UP = "U";
     private static final String USER_MOVE_DOWN = "D";
+
+    private static final String COMMAND_RESTART = "R";
+    private static final String COMMAND_QUIT = "Q";
 
     // TODO: 입력 값에 문자가 포함되어 있을 시 예외를 발생시킨다.
     public static int convertStringToInteger(String inputValue) throws IllegalArgumentException{
@@ -31,6 +35,13 @@ public class Exception {
     public static String validateInputValueForMove(String moving){
         if (moving.equals(USER_MOVE_UP) || (moving.equals(USER_MOVE_DOWN)))
             return moving;
+        throw new IllegalArgumentException(ERROR_PREFIX + INVALID_INPUT_VALUE_FOR_MOVE);
+    }
+
+    // TODO: 입력 받은 게임 재시작 문자열이 'R'혹은 'Q'가 아닐 시 예외 처리한다.
+    public static String validateInputValueForReStart(String gameCommand){
+        if (gameCommand.equals(COMMAND_RESTART) || (gameCommand.equals(COMMAND_QUIT)))
+            return gameCommand;
         throw new IllegalArgumentException(ERROR_PREFIX + INVALID_INPUT_VALUE_FOR_MOVE);
     }
 }
--- src/main/java/bridge/Validate.java
+++ src/main/java/bridge/Validate.java
@@ -11,4 +11,6 @@ public class Validate {
     public static String moving(String moving){
         return Exception.validateInputValueForMove(moving);
     }
+
+    
 }
