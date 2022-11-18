18aa9075c20b47eb916aff6addc167c1b0e80e6a
jootang2 <joohwan.song15b@gmail.com>
2022-11-18 21:15:23 +0900

    :sparkles: 6-2. 게임 성공 여부 출력

--- src/main/java/bridge/OutputView.java
+++ src/main/java/bridge/OutputView.java
@@ -3,6 +3,7 @@ package bridge;
 public class OutputView {
 
     static String result;
+    static String successOrFail;
 
     public void printMap(String[] bridgeMap) {
         BridgeGame bridgeGame = new BridgeGame();
@@ -10,7 +11,9 @@ public class OutputView {
         System.out.println("[ " + bridgeMap[0] + " ]");
         System.out.println("[ " + bridgeMap[1] + " ]");
         result = "[ " + bridgeMap[0] + " ]\n" + "[ " + bridgeMap[1] + " ]";
+        successOrFail = "성공";
         if (bridgeMap[0].contains("X") || bridgeMap[1].contains("X")) {
+            successOrFail = "실패";
             bridgeGame.retry();
         }
     }
@@ -18,5 +21,6 @@ public class OutputView {
     public void printResult() {
         System.out.println("최종 게임 결과");
         System.out.println(result);
+        System.out.println("게임 성공 여부: " + successOrFail);
     }
 }
