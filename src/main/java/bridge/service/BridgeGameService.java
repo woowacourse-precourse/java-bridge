7ab8a73e08afbc9b94f9ad0be3901306a3dc1eac
Rovna99 <80767387+Rovna99@users.noreply.github.com>
2022-11-18 21:57:27 +0900

    refactor: 불필요하다 생각해 moveMap 삭제 후 로직 변경

--- src/main/java/bridge/service/BridgeGameService.java
+++ src/main/java/bridge/service/BridgeGameService.java
@@ -1,11 +1,11 @@
 package bridge.service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import bridge.domain.BridgeGame;
 import bridge.domain.BridgeMaker;
 import bridge.domain.BridgeRandomNumberGenerator;
-import bridge.domain.MoveMap;
 
 public class BridgeGameService {
 	private final BridgeMaker bridgeMaker;
@@ -19,8 +19,11 @@ public class BridgeGameService {
 		return bridgeMaker.makeBridge(size);
 	}
 
-	public MoveMap initGameMap() {
-		return new MoveMap();
+	public List<List<String>> initGameMap() {
+		List<List<String>> moveMap = new ArrayList<>();
+		moveMap.add(new ArrayList<>());
+		moveMap.add(new ArrayList<>());
+		return moveMap;
 	}
 
 	public void moveBridge(String moving, BridgeGame bridgeGame, List<List<String>> moveResult) {
