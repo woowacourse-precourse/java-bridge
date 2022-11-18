9588e8709e50a22bbbda00b03783386e96cf7f53
kissshot1104 <kissshot.heartunderblade1104@gmail.com>
2022-11-18 15:20:11 +0900

    Fix(CustomNsTest) : 버퍼가 초기화 안되는 버그 수정
    OutputStream이 초기화 되지 않아 제대로된 테스트가 되지 않았던 버그가 수정되었습니다.

--- src/main/java/bridge/CustomNsTest.java
+++ src/main/java/bridge/CustomNsTest.java
@@ -1,6 +1,5 @@
 package bridge;
 
-import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 
 import java.io.ByteArrayInputStream;
@@ -9,24 +8,19 @@ import java.io.OutputStream;
 import java.io.PrintStream;
 
 public class CustomNsTest {
-    private PrintStream standardOut;
     private OutputStream captor;
 
     @BeforeEach
     protected final void init() {
-        standardOut = System.out;
         captor = new ByteArrayOutputStream();
         System.setOut(new PrintStream(captor));
     }
 
-    @AfterEach
-    protected final void printOutput() {
-        System.setOut(standardOut);
-        System.out.println(output());
-    }
-
     protected final String output() {
-        return captor.toString().trim();
+        String tmpCaptor = captor.toString().trim();
+        captor = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(captor));
+        return tmpCaptor;
     }
 
     protected void command(final String... args) {
