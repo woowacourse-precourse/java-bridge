549849c3d88e760c91cbfa03191642b7a51e4aa8
Park shinyeong <syngh503@gmail.com>
2022-11-18 18:21:51 +0900

    Feat(OutputView): 다리 상태 출력 메소드
    다리 상태를 출력하는 printMap을 구현하였습니다.

--- src/main/java/bridge/View/OutputView.java
+++ src/main/java/bridge/View/OutputView.java
@@ -1,5 +1,6 @@
 package bridge.View;
 
+import bridge.EnumCollections.BridgeResultType;
 import bridge.EnumCollections.OutputSystemMessage;
 
 /**
@@ -12,7 +13,8 @@ public class OutputView {
      * <p>
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void printMap() {
+    public void printMap(String bridgeStatus) {
+        System.out.println(bridgeStatus);
     }
 
     /**
