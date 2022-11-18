6dbffdf35d1e4e7ac70e679e55a0af9c8239d0ec
penrose15 <seongju99@naver.com>
2022-11-18 17:30:00 +0900

    feat : add constant about phrases

---
+++ src/main/java/bridge/utils/constant/Constant.java
@@ -0,0 +1,20 @@
+package bridge.utils.constant;
+
+public enum Constant {
+    PHRASE_WHERE_TO_MOVE("이동할 칸을 선택해주세요. (위: U, 아래: D)"),
+    PHRASE_RESTART_THE_GAME("게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)"),
+    PHRASE_RESULT_OF_THE_GAME("최종 게임 결과"),
+    PHRASE_GAME_SUCCESS("게임 성공 여부: 성공"),
+    PHRASE_GAME_FAILED("게임 성공 여부: 실패"),
+    PHRASE_GAME_ATTEMPT("총 시도한 횟수: ");
+
+    private final String phrase;
+
+    Constant(String phrase) {
+        this.phrase = phrase;
+    }
+
+    public String getPhrase() {
+        return phrase;
+    }
+}
