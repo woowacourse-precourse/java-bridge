ebfeadaaf97e270d8dbe735ad1dbffb79b797325
jjmin9797 <jjmin9797@naver.com>
2022-11-18 16:20:19 +0900

    feat(BridgeGame): 게임 시작시 다리 랜덤 저장 기능 구현

--- src/main/java/bridge/controller/BridgeGame.java
+++ src/main/java/bridge/controller/BridgeGame.java
@@ -1,9 +1,22 @@
 package bridge.controller;
 
+import bridge.BridgeMaker;
+import bridge.BridgeRandomNumberGenerator;
+import bridge.domain.User;
+
+import java.util.List;
+
 /**
  * 다리 건너기 게임을 관리하는 클래스
  */
 public class BridgeGame {
+    private User user;
+
+    public void initGame(String bridgeSize) {
+        BridgeMaker bridgeMaker = new BridgeMaker(new BridgeRandomNumberGenerator());
+        //TODO : bridgeSize 예외처리
+        this.user = new User(bridgeMaker.makeBridge(Integer.parseInt(bridgeSize)));
+    }
 
     /**
      * 사용자가 칸을 이동할 때 사용하는 메서드
@@ -11,6 +24,7 @@ public class BridgeGame {
      * 이동을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
     public void move() {
+        System.out.println(this.user.getBridge());
     }
 
     /**
