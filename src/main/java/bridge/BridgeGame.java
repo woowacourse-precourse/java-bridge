7355a3ec65ed0ad73893c5e9befc09c820d0ebd3
2021110806 <2021110806@dgu.ac.kr>
2022-11-18 22:24:00 +0900

    feat(BridgeGame):재시작 기능 구현, 게임 한 단위를 실행하는 기능 구현

--- src/main/java/bridge/BridgeGame.java
+++ src/main/java/bridge/BridgeGame.java
@@ -28,11 +28,27 @@ public class BridgeGame {
         return howManyAttempts;
     }
 
-    /**
-     * 사용자가 게임을 다시 시도할 때 사용하는 메서드
-     * <p>
-     * 재시작을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
-     */
-    public void retry() {
+    public boolean retry() {
+        String continueOrEnd;
+        outputView.printContinueOrEndRequest();
+        continueOrEnd = inputView.readGameCommand();
+        if (continueOrEnd.equals("R")) {
+            return true;
+        }
+        return false;
+    }
+
+    public boolean play() {
+        int limitSize;
+        int currentSize;
+
+        limitSize = determineBridgeSize();
+        currentSize = move(bridgeMaker.makeBridge(limitSize));
+        outputView.printResult(currentSize!=-1,currentSize);
+
+        if (currentSize==-1) {
+            return retry();
+        }
+        return false;
     }
 }
