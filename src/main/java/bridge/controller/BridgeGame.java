c65140599386a498aa36d7d184fa47a080c66e07
Kim woo jong <skekdnwhd@gmail.com>
2022-11-18 15:50:51 +0900

    Feat : 게임 시작 및 다리길이 생성까지 기능추가

--- src/main/java/bridge/controller/BridgeGame.java
+++ src/main/java/bridge/controller/BridgeGame.java
@@ -1,9 +1,24 @@
-package bridge.service;
+package bridge.controller;
+
+import bridge.domain.Bridge;
+import bridge.view.InputView;
+import bridge.view.OutputView;
 
 /**
  * 다리 건너기 게임을 관리하는 클래스
  */
 public class BridgeGame {
+
+    private final InputView inputView;
+    private final OutputView outputView;
+    private final Bridge bridge;
+
+    public BridgeGame() {
+        this.inputView = new InputView();
+        this.outputView = new OutputView();
+        outputView.printStartGame();
+        this.bridge = createBridge();
+    }
 
     /**
      * 사용자가 칸을 이동할 때 사용하는 메서드
@@ -11,6 +26,7 @@ public class BridgeGame {
      * 이동을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
     public void move() {
+        String readMoving = inputView.readMoving();
     }
 
     /**
@@ -19,5 +35,10 @@ public class BridgeGame {
      * 재시작을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
     public void retry() {
+    }
+
+    private Bridge createBridge() {
+        int bridgeSize = inputView.readBridgeSize();
+        return new Bridge(bridgeSize);
     }
 }
