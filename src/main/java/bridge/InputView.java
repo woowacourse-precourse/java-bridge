838647a0a11f13eeb4863181a52602b980344924
hingjae <ljh23270@naver.com>
2022-11-18 16:37:24 +0900

    다리 생성 : 다리 길이 입력

--- src/main/java/bridge/InputView.java
+++ src/main/java/bridge/InputView.java
@@ -1,5 +1,7 @@
 package bridge;
 
+import camp.nextstep.edu.missionutils.Console;
+
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
  */
@@ -9,7 +11,8 @@ public class InputView {
      * 다리의 길이를 입력받는다.
      */
     public int readBridgeSize() {
-        return 0;
+        String input = isNum(inputBridgeLength());
+        return stringToInt(input);
     }
 
     /**
@@ -24,5 +27,24 @@ public class InputView {
      */
     public String readGameCommand() {
         return null;
+    }
+
+    public String inputBridgeLength() {
+        System.out.println("다리의 길이를 입력해주세요.");
+        String input = Console.readLine();
+        return input;
+    }
+
+    public static String isNum(String input) {
+        try{
+            Integer.parseInt(input);
+        } catch (Exception e) {
+            throw new IllegalArgumentException("숫자만 입력할 수 있습니다.");
+        }
+        return input;
+    }
+
+    public static int stringToInt(String input) {
+        return Integer.parseInt(input);
     }
 }
