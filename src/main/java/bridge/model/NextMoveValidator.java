e2f7a8f075a58c7311f3e1e90e627e9b9696cb2d
imsongj <imsongj@gmail.com>
2022-11-18 15:32:18 +0900

    refactor(bridge) : 상수 enum 클래스명 변경

--- src/main/java/bridge/model/NextMoveValidator.java
+++ src/main/java/bridge/model/NextMoveValidator.java
@@ -3,7 +3,7 @@ package bridge.model;
 public class NextMoveValidator implements Validator {
     @Override
     public void validateInput(String input) {
-        if (!input.equals(GameMove.UP.getLetter()) || !input.equals(GameMove.DOWN.getLetter())) {
+        if (!input.equals(Direction.UP.getValue()) || !input.equals(Direction.DOWN.getValue())) {
             throw new IllegalArgumentException();
         }
     }
