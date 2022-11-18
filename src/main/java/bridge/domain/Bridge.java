5834cf7b4488a4c8296248d7cefe14e28ecf0ecf
sosow0212 <sosow0212@naver.com>
2022-11-18 16:38:22 +0900

    feat : (Bridge) 게임 진행을 위해 nowIndex 필드와 Getter, Setter 추가

--- src/main/java/bridge/domain/Bridge.java
+++ src/main/java/bridge/domain/Bridge.java
@@ -4,9 +4,27 @@ import java.util.List;
 
 public class Bridge {
     private final List<String> bridge;
+    private int nowIndex;
 
     public Bridge(List<String> bridge) {
         this.bridge = bridge;
+        this.nowIndex = 1;
+    }
+
+    public int getNowIndex() {
+        return nowIndex;
+    }
+
+    public void setNowIndex(int index) {
+        this.nowIndex = index;
+    }
+
+    public void nowIndexUpdate() {
+        setNowIndex(this.nowIndex + 1);
+    }
+
+    public void resetGame() {
+        setNowIndex(1);
     }
 
     public List<String> getBridge() {
