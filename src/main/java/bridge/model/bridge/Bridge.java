2b3ddbd8115280db7e7cc21b65863a079a12f878
Kayoung <rkdkdudjd@gmail.com>
2022-11-18 17:36:22 +0900

    feat(Bridge): 클래스와 생성자 정의
    클래스를 정의하고 BridgeMaker에서 사용할 수 있도록 정적 팩토리 메서드를 추가했습니다.

--- src/main/java/bridge/model/bridge/Bridge.java
+++ src/main/java/bridge/model/bridge/Bridge.java
@@ -1,11 +1,20 @@
 package bridge.model.bridge;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Bridge {
     private List<Node> nodes;
 
-    private Bridge() {
-        // TODO: nodes 랜덤으로 생성하기
+    private Bridge(int size) {
+        nodes = new ArrayList<>(size);
+    }
+
+    public static Bridge of(List<String> generatedBridge) {
+        Bridge bridge = new Bridge(generatedBridge.size());
+        for (String position : generatedBridge) {
+            bridge.nodes.add(Node.valueOf(position));
+        }
+        return bridge;
     }
 }
