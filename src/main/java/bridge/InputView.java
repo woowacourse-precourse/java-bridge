b2adb1283ebbc92060c3d052685e6afc6a8d3157
BaeDongHyeon <sasd730@gmail.com>
2022-11-18 22:30:25 +0900

    feat: 다리 길이 예외처리
    - 다리 길이 값이 정수가 아닐 경우
    - 다리 길이 값이 3이상 20이하의 정수가 아닐 경우

--- src/main/java/bridge/InputView.java
+++ src/main/java/bridge/InputView.java
@@ -1,5 +1,7 @@
 package bridge;
 
+import camp.nextstep.edu.missionutils.Console;
+
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
  */
@@ -9,7 +11,7 @@ public class InputView {
      * 다리의 길이를 입력받는다.
      */
     public int readBridgeSize() {
-        return 0;
+        return bridgeSizeValidate(Console.readLine());
     }
 
     /**
@@ -24,5 +26,16 @@ public class InputView {
      */
     public String readGameCommand() {
         return null;
+    }
+
+    private int bridgeSizeValidate(String inputBridgeSize) {
+        if (!inputBridgeSize.matches("-?\\d+")) {
+            throw new IllegalArgumentException("[ERROR] 정수만 입력 할 수 있습니다.");
+        }
+        int inputBridgeSizeToDigit = Integer.parseInt(inputBridgeSize);
+        if (inputBridgeSizeToDigit < 3 || 20 < inputBridgeSizeToDigit) {
+            throw new IllegalArgumentException("[ERROR] 다리 길이는 3이상 20이하의 정수만 입력 할 수 있습니다.");
+        }
+        return inputBridgeSizeToDigit;
     }
 }
