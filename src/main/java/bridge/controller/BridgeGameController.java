54b4f0408f498a05fbbb720c029aa4892ca8d5e8
hwikyungkim-1215 <buc06137@naver.com>
2022-11-19 00:31:29 +0900

    feat: [기능1] 다리 길이 입력받는 기능 구현

--- src/main/java/bridge/controller/BridgeGameController.java
+++ src/main/java/bridge/controller/BridgeGameController.java
@@ -1,4 +1,7 @@
 package bridge.controller;
 
+import static bridge.view.InputView.readBridgeSize;
+
 public class BridgeGameController {
+    int bridgeLen = readBridgeSize();
 }
--- src/main/java/bridge/view/InputView.java
+++ src/main/java/bridge/view/InputView.java
@@ -1,5 +1,7 @@
 package bridge.view;
 
+import static camp.nextstep.edu.missionutils.Console.readLine;
+
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
  */
@@ -8,8 +10,20 @@ public class InputView {
     /**
      * 다리의 길이를 입력받는다.
      */
-    public int readBridgeSize() {
-        return 0;
+    public static int readBridgeSize() {
+        int bridgeLen = 0;
+        System.out.println("다리의 길이를 입력해주세요.");
+        try{
+            bridgeLen = Integer.parseInt(readLine());
+        }catch (IllegalArgumentException e){
+            System.out.println("[ERROR] 다리 길이는 3부터 20 사이의 숫자여야 합니다.");
+        }
+
+        if(bridgeLen > 20 || bridgeLen < 3){
+            throw new IllegalArgumentException("[ERROR] 다리 길이는 3부터 20 사이의 숫자여야 합니다.");
+        }
+
+        return bridgeLen;
     }
 
     /**
