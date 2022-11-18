9733cd74ea67956b1a590a8b12398b99c92008ab
Sungjaeyeong <wodud2587@gmail.com>
2022-11-19 00:01:43 +0900

    feat(bridge): 다리 길이 입력

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -2,8 +2,24 @@ package bridge;
 
 public class Application {
 
+    private static final String GAME_START = "다리 건너기 게임을 시작합니다.";
+
     public static void main(String[] args) {
         // TODO: 프로그램 구현
-        System.out.println("다리 건너기 게임을 시작합니다.");
+        OutputView.printMessage(GAME_START);
+        while (getBridgeSize() == 0) {
+            getBridgeSize();
+        }
+
+    }
+
+    public static int getBridgeSize() {
+        InputView inputView = new InputView();
+        try {
+            return inputView.readBridgeSize();
+        } catch (IllegalArgumentException e) {
+            OutputView.printMessage(e.getMessage());
+            return 0;
+        }
     }
 }
--- src/main/java/bridge/InputView.java
+++ src/main/java/bridge/InputView.java
@@ -1,15 +1,39 @@
 package bridge;
 
+import camp.nextstep.edu.missionutils.Console;
+
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
  */
 public class InputView {
 
+    private static final String ENTER_BRIDGE_SIZE = "다리의 길이를 입력해주세요.";
+    private static final String ERROR_MESSAGE_ONLY_NUMBER = "[ERROR] 숫자만 입력해주세요.";
+    private static final String ERROR_MESSAGE_SIZE_RANGE = "[ERROR] 3이상 20이하로 입력해주세요.";
+
     /**
      * 다리의 길이를 입력받는다.
      */
     public int readBridgeSize() {
-        return 0;
+        OutputView.printMessage(ENTER_BRIDGE_SIZE);
+        String enterValue = Console.readLine();
+        isValidBridgeSize(enterValue);
+        return Integer.parseInt(enterValue);
+    }
+
+    private void isValidBridgeSize(String str) {
+        isNumber(str);
+        if (Integer.parseInt(str) < 3 || Integer.parseInt(str) > 20) {
+            throw new IllegalArgumentException(ERROR_MESSAGE_SIZE_RANGE);
+        }
+    }
+
+    private void isNumber(String str) {
+        for (int i = 0; i < str.length(); i++) {
+            if (!('0' <= str.charAt(i) && str.charAt(i) <= '9')) {
+                throw new IllegalArgumentException(ERROR_MESSAGE_ONLY_NUMBER);
+            }
+        }
     }
 
     /**
