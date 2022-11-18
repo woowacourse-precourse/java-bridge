df48c2d786bb8109f49020e967eb5b77e160f426
backFox <jorealbusiness@gmail.com>
2022-11-18 14:13:23 +0900

    feat(bridgeMessageMaker): 다리 표현 - 다리의 이동 가능 여부를 표시하는 기능 구현

--- src/main/java/bridge/system/util/BridgeMessageMaker.java
+++ src/main/java/bridge/system/util/BridgeMessageMaker.java
@@ -1,18 +1,58 @@
 package bridge.system.util;
 
+import bridge.vo.BridgeStep;
+import bridge.vo.StepResult;
+
 import java.util.List;
 
 public class BridgeMessageMaker {
 
     public static final String BRIDGE_START = "[";
+    public static final String SPACE = "   ";
+    public static final String CORRECT = " O ";
+    public static final String FAIL = " X ";
+    public static final String BRIDGE_END = "]";
 
-    public String makeBridgeMessage(List<Boolean> answers) {
+    public String makeBridgeMessage(List<StepResult> results) {
         StringBuilder upBridgeBuilder = new StringBuilder();
         StringBuilder downBridgeBuilder = new StringBuilder();
 
         makeStartOfBridge(upBridgeBuilder, downBridgeBuilder);
 
+        for (StepResult result : results) {
+            if (result.isCorrect()) {
+                handleCorrect(upBridgeBuilder, downBridgeBuilder, result.getBridgeStep());
+            }
+            if (!result.isCorrect()) {
+                handleFailure(upBridgeBuilder, downBridgeBuilder, result.getBridgeStep());
+            }
+        }
+
+        makeEndOfBridge(upBridgeBuilder, downBridgeBuilder);
+
         return makeResult(upBridgeBuilder, downBridgeBuilder);
+    }
+
+    private void handleCorrect(StringBuilder upBridgeBuilder, StringBuilder downBridgeBuilder, BridgeStep bridgeStep) {
+        if (bridgeStep == BridgeStep.U) {
+            upBridgeBuilder.append(CORRECT);
+            downBridgeBuilder.append(SPACE);
+        }
+        if (bridgeStep == BridgeStep.D) {
+            upBridgeBuilder.append(SPACE);
+            downBridgeBuilder.append(CORRECT);
+        }
+    }
+
+    private void handleFailure(StringBuilder upBridgeBuilder, StringBuilder downBridgeBuilder, BridgeStep bridgeStep) {
+        if (bridgeStep == BridgeStep.U) {
+            upBridgeBuilder.append(FAIL);
+            downBridgeBuilder.append(SPACE);
+        }
+        if (bridgeStep == BridgeStep.D) {
+            upBridgeBuilder.append(SPACE);
+            downBridgeBuilder.append(FAIL);
+        }
     }
 
     private String makeResult(StringBuilder upBridgeBuilder, StringBuilder downBridgeBuilder) {
