3487a8c4a207b1273e6aa2f387428680c6806bdd
양원준 <97458548+ywj9811@users.noreply.github.com>
2022-11-19 05:27:30 +0900

    test: BridgeGameTest 이동 입력에 대한 예외 테스트

--- src/test/java/bridge/BridgeGameTest.java
+++ src/test/java/bridge/BridgeGameTest.java
@@ -1,13 +1,18 @@
 package bridge;
 
+import camp.nextstep.edu.missionutils.test.NsTest;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
-public class BridgeGameTest {
-    private BridgeGame bridgeGame = new BridgeGame();
+import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class BridgeGameTest extends NsTest {
+    private static final String ERROR_MESSAGE = "[ERROR]";
+    private static BridgeGame bridgeGame = new BridgeGame();
     
     @Test
     @DisplayName("결과_참_확인")
@@ -21,5 +26,27 @@ public class BridgeGameTest {
     public void falseCheck() {
         boolean actual = bridgeGame.move("D", List.of("U", "D", "U"), 0);
         Assertions.assertThat(actual).isEqualTo(false);
+    }
+
+    @Test
+    @DisplayName("이동_입력_예외1")
+    public void moveFail1() {
+        assertSimpleTest(() -> {
+            run("3", "G");
+            assertThat(output()).contains(ERROR_MESSAGE);
+        });
+    }
+    @Test
+    @DisplayName("이동_입력_예외2")
+    public void moveFail2() {
+        assertSimpleTest(() -> {
+            run("3", "");
+            assertThat(output()).contains(ERROR_MESSAGE);
+        });
+    }
+
+    @Override
+    protected void runMain() {
+        Application.main(new String[]{});
     }
 }
