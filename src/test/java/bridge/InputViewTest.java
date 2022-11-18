8970846ab6b6702b3118db58c0ac475522e13b9d
abbd2 <ajkety00@gmail.com>
2022-11-18 19:33:59 +0900

    Test(InputViewTest): isBridgeSizeNumber의 테스트 추가

---
+++ src/test/java/bridge/InputViewTest.java
@@ -0,0 +1,18 @@
+package bridge;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class InputViewTest {
+    
+    @DisplayName("다리의 길이로 지정이 가능한 숫자인지 확인하는 메서드")
+    @Test
+    void isBridgeSizeNumber(){
+        InputView inputView = new InputView();
+        assertThat(inputView.isBridgeSizeNumber("1"));
+    }
+
+}
