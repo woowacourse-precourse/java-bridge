11884f4db730b6ea4554646d20dd86504dfc1186
OhJintaek <sunshot@hanyang.ac.kr>
2022-11-19 02:11:10 +0900

    type:feat 모든 메소드를 main함수에 통합

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -1,8 +1,42 @@
 package bridge;
 
 public class Application {
+    static InputView inputView;
+    static OutputView outputView;
+    static BridgeRandomNumberGenerator bridgeRandomNumberGenerator;
+    static BridgeMaker bridgeMaker;
+    static BridgeGame bridgeGame;
 
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        startState();
+
+        gameState();
+
+        endState();
+    }
+
+    private static void startState() {
+        bridgeRandomNumberGenerator = new BridgeRandomNumberGenerator();
+        bridgeMaker = new BridgeMaker(bridgeRandomNumberGenerator);
+        bridgeGame = new BridgeGame(bridgeMaker.makeBridge(inputView.readBridgeSize()));
+        inputView = new InputView(bridgeGame);
+        outputView = new OutputView();
+    }
+
+    private static void gameState() {
+        while (!bridgeGame.isGameEnd()) {
+            inputView.readMoving();
+            outputView.printMap(bridgeGame.getUpperRoad(), bridgeGame.getLowerRoad());
+            if (!bridgeGame.isRightFoothold()) {
+                if (inputView.readGameCommand().equals(Command.QUIT.getValue())) {
+                    break;
+                }
+                bridgeGame.retry();
+            }
+        }
+    }
+
+    private static void endState() {
+        outputView.printResult(bridgeGame);
     }
 }
