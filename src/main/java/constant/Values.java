bdbafc6812692e146ab96df990b45b1ec6b61ccd
nn98 <jkllhgb@gmail.com>
2022-11-18 15:01:05 +0900

    Feat: 상수 Values 생성-출력을 위한 enum 메시지 정의

--- src/main/java/constant/Values.java
+++ src/main/java/constant/Values.java
@@ -21,4 +21,29 @@ public class Values {
         }
 
     }
+
+    public enum Message {
+        GAME_START("다리 건너기 게임을 시작합니다."),
+        INPUT_LENGTH("다리의 길이를 입력해주세요."),
+        SELECT_MOVE("이동할 칸을 선택해주세요. (위: U, 아래: D)"),
+        BRIDGE_OPEN("["),
+        BRIDGE_SUCCESS("O"),
+        BRIDGE_FAIL("X"),
+        BRIDGE_CLOSE("]"),
+        SELECT_RETRY("게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)"),
+        GAME_RESULT("최종 게임 결과"),
+        GAME_SUCCESS("게임 성공 여부: %s"),
+        SUCCESS("성공"),
+        FAIL("실패"),
+        GAME_TRY("총 시도한 횟수: %d");
+        private String message;
+
+        private Message(String message) {
+            this.message = message;
+        }
+
+        private String getMessage() {
+            return message;
+        }
+    }
 }
