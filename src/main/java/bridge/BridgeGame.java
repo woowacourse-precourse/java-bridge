6b537e1ef4f75ae472b362bbdd11d38356c9bc23
01192mg <01192mg@gmail.com>
2022-11-19 07:37:18 +0900

    feat(BridgeGame): 게임을 재시도하는 기능 구현
    게임을 재시도하면 플레이어의 위치를 초기화하고 시도 횟수를 하나 증가시킨다.

--- src/main/java/bridge/BridgeGame.java
+++ src/main/java/bridge/BridgeGame.java
@@ -9,8 +9,10 @@ public class BridgeGame {
     public static final String IMMOVABLE = "Can't move";
     public static final String FINISH = "Finish";
     public static final String SUCCESS = "Success";
+    public static final int START_POSITION = 0;
     private final List<String> bridge;
     private int position;
+    private int attempt = 1;
 
     public BridgeGame(List<String> bridge) {
         this.bridge = bridge;
@@ -56,5 +58,15 @@ public class BridgeGame {
      * 재시작을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
     public void retry() {
+        resetPlayerPosition();
+        increaseAttempts();
+    }
+
+    private void resetPlayerPosition() {
+        position = START_POSITION;
+    }
+
+    private void increaseAttempts() {
+        attempt++;
     }
 }
--- src/test/java/bridge/BridgeGameTest.java
+++ src/test/java/bridge/BridgeGameTest.java
@@ -1,7 +1,9 @@
 package bridge;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.lang.reflect.Field;
 import java.util.List;
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -14,7 +16,7 @@ class BridgeGameTest {
 
         String responseMessage = bridgeGame.move("D");
 
-        Assertions.assertThat(responseMessage).isEqualTo(BridgeGame.IMMOVABLE);
+        assertThat(responseMessage).isEqualTo(BridgeGame.IMMOVABLE);
     }
 
     @DisplayName("건널 수 있는 칸을 선택하면 SUCCESS 메세지를 응답한다.")
@@ -24,7 +26,7 @@ class BridgeGameTest {
 
         String responseMessage = bridgeGame.move("U");
 
-        Assertions.assertThat(responseMessage).isEqualTo(BridgeGame.SUCCESS);
+        assertThat(responseMessage).isEqualTo(BridgeGame.SUCCESS);
     }
 
     @DisplayName("다리를 다 건너면 FINISHED 메세지를 응답한다.")
@@ -35,6 +37,34 @@ class BridgeGameTest {
         bridgeGame.move("U");
         String responseMessage = bridgeGame.move("D");
 
-        Assertions.assertThat(responseMessage).isEqualTo(BridgeGame.FINISH);
+        assertThat(responseMessage).isEqualTo(BridgeGame.FINISH);
+    }
+
+    @DisplayName("재시도하면 플레이어의 위치가 초기화된다.")
+    @Test
+    void retryThenPositionReset() throws Exception {
+        Field position = BridgeGame.class.getDeclaredField("position");
+        position.setAccessible(true);
+        BridgeGame bridgeGame = new BridgeGame(List.of("U", "D"));
+        bridgeGame.move("U");
+
+        bridgeGame.retry();
+
+        int actualPosition = (int) position.get(bridgeGame);
+        assertThat(actualPosition).isEqualTo(0);
+    }
+
+    @DisplayName("재시도하면 시도 횟수를 하나 증가시킨다.")
+    @Test
+    void retryThenIncreaseAttempt() throws NoSuchFieldException, IllegalAccessException {
+        Field attempt = BridgeGame.class.getDeclaredField("attempt");
+        attempt.setAccessible(true);
+        BridgeGame bridgeGame = new BridgeGame(List.of("U", "D"));
+        bridgeGame.move("U");
+
+        bridgeGame.retry();
+
+        int actualAttempt = (int) attempt.get(bridgeGame);
+        assertThat(actualAttempt).isEqualTo(2);
     }
 }
