565e5d8692a17cd88b73f1bedc043420801990a3
김태용 <amico741@naver.com>
2022-11-19 02:20:59 +0900

    feat: Player 입력방향과 다리 비교 기능 추가

--- src/main/java/bridge/domain/Bridge.java
+++ src/main/java/bridge/domain/Bridge.java
@@ -1,12 +1,30 @@
 package bridge.domain;
 
 
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Queue;
 
 public class Bridge {
     private final List<String> directions;
 
     public Bridge(List<String> directions) {
         this.directions = directions;
+    }
+
+    public boolean hasSameDirection(Player player) {
+        Queue<String> playerInputs = new LinkedList<>(player.getInputs());
+        int start = 0;
+        while(!playerInputs.isEmpty()) {
+            if(isDifferent(directions.get(start), playerInputs.poll())) {
+                return false;
+            }
+            start++;
+        }
+        return true;
+    }
+
+    public boolean isDifferent(String original, String expected) {
+        return !original.equals(expected);
     }
 }
