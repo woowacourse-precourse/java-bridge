4f75213367a2abca779d2a7c0bc4fef584de57fb
Jaeyeop-Jung <jjy5349@naver.com>
2022-11-18 22:30:07 +0900

    test: Parser가 입력한 값이 숫자인지 판단을 제대로 하는지 테스트 추가

---
+++ src/test/java/bridge/ParserTest.java
@@ -0,0 +1,36 @@
+package bridge;
+
+import static org.assertj.core.api.Assertions.assertThatCode;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+public class ParserTest {
+
+    @Nested
+    @DisplayName("숫자 변환에")
+    class In_numerical_conversion {
+
+        @ParameterizedTest
+        @ValueSource(strings = {"0", "1", "2"})
+        @DisplayName("숫자를 넣었을 때 정상적으로 파싱을 한다")
+        void if_input_number_then_convert_normally(String input) {
+            // when, then
+            assertThatCode(() -> Parser.parseToInt(input))
+                    .doesNotThrowAnyException();
+        }
+
+        @ParameterizedTest
+        @ValueSource(strings = {"", " ", "test"})
+        @DisplayName("문자를 넣었을 경우")
+        void when_input_string(String input) {
+            // when, then
+            assertThatThrownBy(() -> Parser.parseToInt(input))
+                    .doesNotThrowAnyException();
+        }
+
+    }
+}
