d4a1dd4a029def3ca2ca62023ae626b05999ef18
KYR <freekyr7529@gmail.com>
2022-11-18 21:32:19 +0900

    feature : BridgeGame 클래스에 시작 메시지 출력을 구현

--- src/main/java/bridge/BridgeGame.java
+++ src/main/java/bridge/BridgeGame.java
@@ -4,9 +4,11 @@ package bridge;
  * 다리 건너기 게임을 관리하는 클래스
  */
 public class BridgeGame {
+    private final InputView inputView = new InputView();
+    private final OutputView outputView = new OutputView();
     public void run() {
         try {
-
+            outputView.startMsg();
         } catch (IllegalArgumentException exception) {
             OutputView.printMsg("[ERROR] " + exception.getMessage());
         }
