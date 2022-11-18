6a00c11e0097ab2c6da76cb2c51e0adc049e50c9
ji-mango <susie000301@naver.com>
2022-11-18 22:19:23 +0900

    feat: 다리 길이를 입력받는 기능

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -3,6 +3,12 @@ package bridge;
 public class Application {
 
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        InputView inputView = new InputView();
+
+        System.out.println("다리 건너기 게임을 시작합니다.");
+        System.out.println("\n다리의 길이를 입력해주세요.");
+
+        int bridgeSize = inputView.readBridgeSize();
+        System.out.println(bridgeSize);
     }
 }
--- src/main/java/bridge/InputView.java
+++ src/main/java/bridge/InputView.java
@@ -1,5 +1,7 @@
 package bridge;
 
+import camp.nextstep.edu.missionutils.Console;
+
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
  */
@@ -9,7 +11,13 @@ public class InputView {
      * 다리의 길이를 입력받는다.
      */
     public int readBridgeSize() {
-        return 0;
+        String input = Console.readLine();
+        checkIsDigit(input);
+
+        int bridgeSize = Integer.parseInt(input);
+        checkBridgeLength(bridgeSize);
+
+        return bridgeSize;
     }
 
     /**
@@ -24,5 +32,23 @@ public class InputView {
      */
     public String readGameCommand() {
         return null;
+    }
+
+    public void checkIsDigit(String checkNumber) {
+        for (int i = 0; i < checkNumber.length(); i++) {
+            checkEachIsDigit(checkNumber.charAt(i));
+        }
+    }
+
+    public void checkEachIsDigit(char checkNumber) {
+        if (!Character.isDigit(checkNumber)) {
+            throw new IllegalArgumentException("[ERROR] 자연수를 입력해야 합니다.");
+        }
+    }
+
+    public void checkBridgeLength(int length) {
+        if (length < 3 || length > 20) {
+            throw new IllegalArgumentException("[ERROR] 다리 길이는 3이상 20이하로 입력해야 합니다.");
+        }
     }
 }
