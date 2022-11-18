99ba41175017b36c20399cd4a4a317bc3b13c812
making-a-scene <4936joo@naver.com>
2022-11-18 17:42:31 +0900

    fix: 위 칸과 아래 칸의 각 이동 결과를 저장하는 리스트가 하나의 동일한 객체를 참조하고 있었던 문제 수정

--- src/main/java/bridge/BridgeMakerImpl.java
+++ src/main/java/bridge/BridgeMakerImpl.java
@@ -13,10 +13,12 @@ public class BridgeMakerImpl {
     // 게임 시작 전 초기 다리 생성
     public UsersBridgeCrossStatus makeInitialBridge(int length) {
         List<List<String>> newBridge = new ArrayList<>();
-
-        List<String> initialBridgeShape = Arrays.asList(HEAD_OF_BRIDGE, TAIL_OF_BRIDGE);
-        newBridge.add(DOWN.getIndex(), initialBridgeShape);
-        newBridge.add(UP.getIndex(), initialBridgeShape);
+        List<String> initialUpSpace = new ArrayList<>();
+        initialUpSpace.add(HEAD_OF_BRIDGE);
+        initialUpSpace.add(TAIL_OF_BRIDGE);
+        List<String> initialDownSpace = new ArrayList<>(initialUpSpace);
+        newBridge.add(DOWN.getIndex(), initialUpSpace);
+        newBridge.add(UP.getIndex(), initialDownSpace);
 
         return new UsersBridgeCrossStatus(newBridge, length);
     }
