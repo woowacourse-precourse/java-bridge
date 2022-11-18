2a06f5f5320a9e541875328b10be9dc8c51260b2
DESKTOP-1ANNTGI\USER <cvc100326@naver.com>
2022-11-18 22:39:30 +0900

    feat: 다시시작하기 기능 추가

--- src/main/java/bridge/BridgeGame.java
+++ src/main/java/bridge/BridgeGame.java
@@ -10,6 +10,7 @@ public class BridgeGame {
     private final List<String> bridge;
     private List<String> userBridgeHistroy = new ArrayList<>();
     private int movingCount;
+    private int tryCount;
 
     public boolean isSuccess;
 
@@ -48,14 +49,19 @@ public class BridgeGame {
         return false;
     }
 
-
     /**
      * 사용자가 게임을 다시 시도할 때 사용하는 메서드
      * <p>
      * 재시작을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
     public void retry() {
-
+        movingCount = 0;
+        userBridgeHistroy.clear();
+        tryCount++;
+        isSuccess = true;
+    }
+    public int getTryCount(){
+        return tryCount;
     }
 
     private void validate(String moving) {
