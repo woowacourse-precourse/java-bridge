5bd337128a56ce7b64e21be7f829ab1481f3fb48
kdkdhoho <hkim4410@naver.com>
2022-11-18 17:22:34 +0900

    test(AnswerBridgeTest): 유저의 선택과 answerBridge 비교 결과 테스트 간결화 리팩토링

--- src/test/java/bridge/AnswerBridgeTest.java
+++ src/test/java/bridge/AnswerBridgeTest.java
@@ -5,7 +5,6 @@ import bridge.model.User;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.List;
 
@@ -14,37 +13,37 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class AnswerBridgeTest {
 
     AnswerBridge answerBridge = new AnswerBridge(List.of("U", "D", "U"));
+    User user;
 
-    // TODO: 리팩토링
-    @DisplayName("유저의 선택과 answerBridge 비교 결과 테스트")
+    @BeforeEach
+    void setUp() {
+        user = new User();
+    }
+
+    @DisplayName("유저의 선택과 answerBridge 비교 결과 테스트 - ")
     @Test
-    void compareToTest() {
-        User user = new User();
+    void compareToTest_empty() {
+        List<Boolean> firstResult = makeSituation("U");
+        assertThat(firstResult).isEqualTo(List.of(true));
 
-        List<Boolean> firstResult = answerBridge.compareTo(user);
-        assertThat(firstResult).isEmpty();
+        List<Boolean> secondResult = makeSituation("D");
+        assertThat(secondResult).isEqualTo(List.of(true, true));
 
-        user.addChoice("U");
-        List<Boolean> secondResult = answerBridge.compareTo(user);
-        assertThat(secondResult).isEqualTo(List.of(true));
+        List<Boolean> thirdResult = makeSituation("D");
+        assertThat(thirdResult).isEqualTo(List.of(true, true, false));
+    }
 
-        user.addChoice("D");
-        List<Boolean> thirdResult = answerBridge.compareTo(user);
-        assertThat(thirdResult).isEqualTo(List.of(true, true));
-
-        user.addChoice("D");
-        List<Boolean> fourthResult = answerBridge.compareTo(user);
-        assertThat(fourthResult).isEqualTo(List.of(true, true, false));
+    private List<Boolean> makeSituation(String choice) {
+        user.addChoice(choice);
+        return answerBridge.compareTo(user);
     }
 
     @DisplayName("유저가 끝까지 도달했을 경우 테스트")
     @Test
     void isApproachEndTest() {
-        User user = new User();
         user.addChoice("U");
         user.addChoice("D");
         user.addChoice("U");
-
         boolean result = answerBridge.isApproachEnd(user.getChoices());
         assertThat(result).isTrue();
     }
@@ -52,7 +51,6 @@ public class AnswerBridgeTest {
     @DisplayName("유저가 현재 선택한 결과를 반환하는 함수 테스트")
     @Test
     void isCorrect() {
-        User user = new User();
         user.addChoice("U");
         boolean result = answerBridge.isCorrect(user);
         assertThat(result).isTrue();
