525cb3265d1c897449d29f2fff2895a40c70b231
sosow0212 <sosow0212@naver.com>
2022-11-18 15:04:48 +0900

    test : (UserTest) User 도메인 메서드 테스트

---
+++ src/test/java/bridge/domain/UserTest.java
@@ -0,0 +1,64 @@
+package bridge.domain;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class UserTest {
+    @Test
+    @DisplayName("생성자, getter 테스트")
+    public void userConstructorAndGetterTest() {
+        // given
+
+        // when
+        User user = new User();
+
+        // then
+        assertThat(user.isGameDoneStatus()).isEqualTo(false);
+        assertThat(user.getGameTryCount()).isEqualTo(0);
+    }
+
+    @Test
+    @DisplayName("user setter 테스트")
+    public void userSetterTest() {
+        // given
+        User user = new User();
+        int gameTryCount = 10;
+        boolean gameDoneSuccess = true;
+
+        // when
+        user.setGameTryCount(gameTryCount);
+        user.setGameDoneStatus(gameDoneSuccess);
+
+        // then
+        assertThat(user.getGameTryCount()).isEqualTo(gameTryCount);
+        assertThat(user.isGameDoneStatus()).isEqualTo(gameDoneSuccess);
+    }
+
+    @Test
+    @DisplayName("게임 성공 메서드 테스트")
+    public void gameDoneSuccessTest() {
+        // given
+        User user = new User();
+
+        // when
+        user.gameDoneSuccess();
+
+        // then
+        assertThat(user.isGameDoneStatus()).isEqualTo(true);
+    }
+
+    @Test
+    @DisplayName("게임 재시작시 gameTryCount 카운팅 테스트")
+    public void gameRetryTest() {
+        // given
+        User user = new User();
+
+        // when
+        user.gameRetry();
+
+        // then
+        assertThat(user.getGameTryCount()).isEqualTo(1);
+    }
+}
