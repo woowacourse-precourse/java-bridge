04540c47d79c6ec8a55ceb3715fcd86e6b22dc46
watterricecake <fruturum@nate.com>
2022-11-18 20:57:27 +0900

    feat: 입력한 다리 갯수만큼 random 다리 생성

--- src/main/java/bridge/BridgeMaker.java
+++ src/main/java/bridge/BridgeMaker.java
@@ -1,14 +1,16 @@
 package bridge;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
  * 다리의 길이를 입력 받아서 다리를 생성해주는 역할을 한다.
  */
 public class BridgeMaker {
-
+    private final String UPPER = "U";
+    private final String LOWER = "D";
+    private final Integer UPPER_INCLUSIVE = 1;
     private final BridgeNumberGenerator bridgeNumberGenerator;
-
     public BridgeMaker(BridgeNumberGenerator bridgeNumberGenerator) {
         this.bridgeNumberGenerator = bridgeNumberGenerator;
     }
@@ -18,6 +20,15 @@ public class BridgeMaker {
      * @return 입력받은 길이에 해당하는 다리 모양. 위 칸이면 "U", 아래 칸이면 "D"로 표현해야 한다.
      */
     public List<String> makeBridge(int size) {
-        return null;
+        List<String> newList = new ArrayList<>();
+        for(int i=0;i<size;i++){
+            newList.add(upDown(bridgeNumberGenerator.generate()));
+        }
+        return newList;
+    }
+
+    private String upDown(Integer position){
+        if(position.equals(UPPER_INCLUSIVE)) return UPPER;
+        return LOWER;
     }
 }
