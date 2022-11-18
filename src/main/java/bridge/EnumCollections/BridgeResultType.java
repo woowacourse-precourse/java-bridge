2fbfaffedd2f7ebba97f84d5866f911ff8fdcb94
Park shinyeong <syngh503@gmail.com>
2022-11-18 20:05:25 +0900

    Fix(BridgeResultType): BridgeResultType 데이터 추가
    BridgeResultType에, 성공, 실패 메시지를 추가하였습니다.

--- src/main/java/bridge/EnumCollections/BridgeResultType.java
+++ src/main/java/bridge/EnumCollections/BridgeResultType.java
@@ -1,18 +1,23 @@
 package bridge.EnumCollections;
 
 public enum BridgeResultType {
-    POSSIBLE("O"),
-    IMPOSSIBLE("X"),
-    NONE("");
+    POSSIBLE("O", "성공"),
+    IMPOSSIBLE("X", "실패"),
+    NONE("", "");
 
 
     private String result;
+    private String message;
 
-    BridgeResultType(String result) {
+    BridgeResultType(String result, String message) {
         this.result = result;
+        this.message = message;
     }
 
     public String getResult() {
         return result;
+    }
+    public String getMessage() {
+        return message;
     }
 }
