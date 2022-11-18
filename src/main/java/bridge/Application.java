de434cc96097f45393e8b30666480a8d7dd667eb
hdsung98 <hdsung98@gmail.com>
2022-11-19 03:05:21 +0900

    feat: 반복 루프 로직 구현 및 README.md 업데이트 (아래 참조)
    1. "이동-> 이동 유효성 검증 및 정오 판단 -> 현재까지 결과 출력"의 루프를 일부 구현했다.
    2. README.md 컨벤션 맟게 공백,개행 수정 + 테스트,버젼 업데이트

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -16,6 +16,16 @@ public class Application {
             int bridgeSize = inputView.readBridgeSize();
             BridgeMaker bridgeMaker = new BridgeMaker(new BridgeRandomNumberGenerator());
             List<String> bridge = bridgeMaker.makeBridge(bridgeSize);
+
+            do {
+                BridgeGame bridgeGame = new BridgeGame();
+                outputView.printEnterMove();
+                String direction = inputView.readMoving();
+                bridgeGame.move(direction);
+                // direction 정오 판단
+                // 현재까지의 결과 출력
+            } while (false); // 탈출 조건: 다 맞거나, 현 단계 예측 틀릴때까지
+
         } catch (IllegalArgumentException e) {
             outputView.printError(e.getMessage());
         }
