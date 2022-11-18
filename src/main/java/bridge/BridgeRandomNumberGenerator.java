070b5b381364d988becc9455979b3356926f106a
Dennis-K1 <dennis.k1.dev@gmail.com>
2022-11-18 21:08:47 +0900

    docs: add 'code not modifiable' message

--- src/main/java/bridge/BridgeRandomNumberGenerator.java
+++ src/main/java/bridge/BridgeRandomNumberGenerator.java
@@ -4,6 +4,13 @@ import camp.nextstep.edu.missionutils.Randoms;
 
 public class BridgeRandomNumberGenerator implements BridgeNumberGenerator {
 
+    /* 코드 변경 불가
+
+
+
+
+
+     */
     private static final int RANDOM_LOWER_INCLUSIVE = 0;
     private static final int RANDOM_UPPER_INCLUSIVE = 1;
 
