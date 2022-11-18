2be831f4cf6fca1f2f762d212423750ce10d7d46
TaeyeonRoyce <skdlxmtpqms@gmail.com>
2022-11-19 01:44:16 +0900

    refactor(BridgeSize): 검증 메서드를 종류에 따라 분리합니다

--- src/main/java/bridge/ui/input/InputErrorText.java
+++ src/main/java/bridge/ui/input/InputErrorText.java
@@ -1,7 +1,8 @@
 package bridge.ui.input;
 
 public enum InputErrorText {
-    ERROR_BRIDGE_SIZE("다리 길이는 3부터 20 사이의 숫자만 입력 가능 합니다.");
+    ERROR_BRIDGE_NUMBER("다리 길이는 숫자만 입력 가능 합니다."),
+    ERROR_BRIDGE_SIZE("다리 길이는 3 ~ 20 사이 이어야 합니다.");
 
     private static final String ERROR_PREFIX = "[ERROR] ";
     private final String errorText;
--- src/main/java/bridge/ui/input/dto/BridgeSize.java
+++ src/main/java/bridge/ui/input/dto/BridgeSize.java
@@ -4,6 +4,8 @@ import bridge.ui.input.InputErrorText;
 import java.util.regex.Pattern;
 
 public class BridgeSize {
+    private static final int BRIDGE_MIN_SIZE = 3;
+    private static final int BRIDGE_MAX_SIZE = 20;
     private final String bridgeSize;
 
     public BridgeSize(String bridgeSize) {
@@ -16,10 +18,22 @@ public class BridgeSize {
     }
 
     private static void validate(String userInput) {
-        String bridgeSizeRegex = "^([3-9]|1[0-9]|20)$";
+        validateNumber(userInput);
+        validateRange(userInput);
+    }
+
+    private static void validateNumber(String userInput) {
+        String bridgeSizeRegex = "^([0-9]*)$";
 
         if (!Pattern.matches(bridgeSizeRegex, userInput)) {
             throw new IllegalArgumentException(InputErrorText.ERROR_BRIDGE_SIZE.errorText());
+        }
+    }
+
+    private static void validateRange(String userInput) {
+        int bridgeSize = Integer.parseInt(userInput);
+        if (bridgeSize < BRIDGE_MIN_SIZE || bridgeSize > BRIDGE_MAX_SIZE) {
+            throw new IllegalArgumentException(InputErrorText.ERROR_BRIDGE_NUMBER.errorText());
         }
     }
 
