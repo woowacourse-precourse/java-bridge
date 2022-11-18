18499b77e97d6da9713383db3f56b536c2b02fa9
minhee020 <minhee020@naver.com>
2022-11-18 14:59:19 +0900

    feat : move 함수 기능 구현

--- src/main/java/bridge/BridgeGame.java
+++ src/main/java/bridge/BridgeGame.java
@@ -1,16 +1,53 @@
 package bridge;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * 다리 건너기 게임을 관리하는 클래스
  */
 public class BridgeGame {
+
+    private final List<String> bridge;
+    private List<String> upperClass;
+    private List<String> lowerClass;
+
+    public BridgeGame(List<String> bridge) {
+        this.bridge = bridge;
+        this.upperClass = new ArrayList<>(bridge.size());
+        this.lowerClass = new ArrayList<>(bridge.size());
+    }
 
     /**
      * 사용자가 칸을 이동할 때 사용하는 메서드
      * <p>
      * 이동을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void move() {
+    public boolean move(String moving, int count) {
+        if (moving.equals("U")) {
+            if (moving.equals(bridge.get(count))) {
+                upperClass.add(count, "O");
+                lowerClass.add(count, " ");
+                return true;
+            } else {
+                upperClass.add(count,"X");
+                lowerClass.add(count, " ");
+                return false;
+            }
+        }
+        if (moving.equals("D")) {
+            if (moving.equals(bridge.get(count))) {
+                lowerClass.add("O");
+                upperClass.add(" ");
+                return true;
+            } else {
+                lowerClass.add("X");
+                upperClass.add(" ");
+                return false;
+            }
+        }
+
+        return false;
     }
 
     /**
@@ -19,5 +56,9 @@ public class BridgeGame {
      * 재시작을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
     public void retry() {
+    }
+
+    public List<List<String>> getResult() {
+        return List.of(upperClass, lowerClass);
     }
 }
