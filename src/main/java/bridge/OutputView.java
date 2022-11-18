ca9d046ef1c4315c5166ef7df0f8c57a0aeca603
SuhHyeonjun <dmsgktn0208@gmail.com>
2022-11-18 12:01:32 +0900

    feat : 현재 진행상황 출력

--- src/main/java/bridge/OutputView.java
+++ src/main/java/bridge/OutputView.java
@@ -1,5 +1,9 @@
 package bridge;
 
+import static bridge.UpDownBridge.downBridge;
+import static bridge.UpDownBridge.makeUpDownBridge;
+import static bridge.UpDownBridge.upBridge;
+
 /**
  * 사용자에게 게임 진행 상황과 결과를 출력하는 역할을 한다.
  */
@@ -10,7 +14,14 @@ public class OutputView {
      * <p>
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void printMap() {
+    public void printMap(String inputDirection, String moveResult) {
+        makeUpDownBridge(inputDirection, moveResult);
+        printProgress();
+    }
+
+    public static void printProgress() {
+        System.out.println(Message.LEFT_BAR.getMessage() + upBridge + Message.RIGHT_BAR.getMessage());
+        System.out.println(Message.LEFT_BAR.getMessage() + downBridge + Message.RIGHT_BAR.getMessage());
     }
 
     /**
@@ -18,6 +29,10 @@ public class OutputView {
      * <p>
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void printResult() {
+    public static void printResult(boolean keepGoing) {
+        System.out.println(Message.GAME_RESULT.getMessage());
+    }
+
+    public static void printTotalCount(int count) {
     }
 }
