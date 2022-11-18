782bbbdcc802d3f592d6ab6c88e016f7c5dca68b
kimdahee0815 <diana815@naver.com>
2022-11-18 11:05:11 +0900

    fix(retry) : 입력에 관련된 부분은 모두 InputView 클래스로 옮기기

--- src/main/java/bridge/BridgeGame.java
+++ src/main/java/bridge/BridgeGame.java
@@ -38,7 +38,8 @@ public class BridgeGame {
 	 */
 	public void retry(boolean compareResult) {
 		if(compareResult == false) {
-			System.out.println("게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q");
+			String restartOrQuit = inputView.readGameCommand();
+			dh
 		}
 	}
 }
