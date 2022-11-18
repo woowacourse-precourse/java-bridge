0985cc1fb18a0cb97dfeaee020d36aaee15666aa
Park shinyeong <syngh503@gmail.com>
2022-11-18 17:51:47 +0900

    Feat(StartGameService): 게임 시작 기능 구현
    게임 시작 시 입력값을 받고 다리를 생성하는 startGame() 메소드를 생성하였습니다.

--- src/main/java/bridge/Controller/Service/StartGameService.java
+++ src/main/java/bridge/Controller/Service/StartGameService.java
@@ -1,4 +1,28 @@
 package bridge.Controller.Service;
 
+import Utils.ValidateUtils;
+import bridge.BridgeRandomNumberGenerator;
+import bridge.Domain.Bridge;
+import bridge.Domain.BridgeMaker;
+import bridge.View.InputView;
+import bridge.View.OutputView;
+
+import java.util.List;
+
 public class StartGameService {
+    private BridgeMaker bridgeMaker;
+    private Bridge bridge;
+
+    public StartGameService(Bridge bridge) {
+        this.bridge = bridge;
+        bridgeMaker = new BridgeMaker(new BridgeRandomNumberGenerator());
+    }
+
+    public void startGame() throws IllegalArgumentException {
+        OutputView.printGameStart();
+        String bridgeSize = InputView.readBridgeSize();
+        ValidateUtils.isValidateLength(bridgeSize);
+        List<String> newBridge = bridgeMaker.makeBridge(Integer.parseInt(bridgeSize));
+        this.bridge = new Bridge(newBridge);
+    }
 }
