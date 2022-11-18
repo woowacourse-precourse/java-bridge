1850f01fd7b75466ed0d7e2c5dab74c15c2c1b2e
Polite-Jehu <werber112@kyonggi.ac.kr>
2022-11-18 16:34:45 +0900

    refactor(bridge): assemble code

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -6,42 +6,43 @@ public class Application {
     private OutputView outputView;
     private BridgeMaker bridgeMaker;
     private boolean runGame;
-    private int tryNumber=0;
-    public Application(){
-        inputView  = new InputView();
+    private int tryNumber = 0;
+
+    public Application() {
+        inputView = new InputView();
         outputView = new OutputView();
         bridgeMaker = new BridgeMaker(new BridgeRandomNumberGenerator());
         boolean runGame = true;
     }
+
     public static void main(String[] args) {
         Application application = new Application();
-        while(application.runGame)
-        {
+        application.gameInitialize();
+        while (application.runGame) {
             application.runGame = application.gameRun();
-
         }
     }
 
-    private boolean gameRun(){
-        gameInitialize();
-
+    private boolean gameRun() {
+        while (!bridgeGame.isGameOver()) {
+            gameDo();
+        }
         //after must modify Later;
         return false;
     }
 
-    private void gameInitialize(){
+    private void gameInitialize() {
         outputView.printStart();
         outputView.printInputBridgeLength();
         bridgeGame = new BridgeGame(bridgeMaker.makeBridge(inputView.readBridgeSize()));
     }
-    private void gameDo(){
-        while(!bridgeGame.isGameOver())
-        {
-            bridgeGame.move(inputView.readMoving());
-        }
+
+    private void gameDo() {
+        outputView.printMoveBridge();
+        String lastInput = inputView.readMoving();
+        bridgeGame.move(lastInput);
+        outputView.printMap(bridgeGame.getBridge(), bridgeGame.getNowOn(), lastInput);
     }
-
-
 
 
 }
