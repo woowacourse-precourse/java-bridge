cd92208ab813b04dd404263acde27fe9676835b0
jeong-yeji <jeongyeji.mail@gmail.com>
2022-11-19 01:41:58 +0900

    refactor(Application): 메소드 분리

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -4,44 +4,57 @@ public class Application {
 
     private static final InputView input = new InputView();
     private static final OutputView output = new OutputView();
+    private static int position = 0;
+    private static boolean stopFlag = false;
     private static BridgeGame bridgeGame;
-    private static int trying = 1;
+    private static int bridgeSize;
 
     public static void main(String[] args) {
         try {
             output.startGame();
-            int bridgeSize = input.readBridgeSize();
+            bridgeSize = input.readBridgeSize();
             bridgeGame = new BridgeGame(bridgeSize);
-            int position = 0;
-            while (true) {
-                boolean available = moveAndPrint(position);
-
-                if (available) {
-                    if (position == bridgeSize - 1) {
-                        output.printResult(bridgeSize - 1, true, bridgeGame);
-                        break;
-                    }
-                    position += 1;
-                    continue;
-                }
-
-                if (!available) {
-                    String retry = input.readGameCommand();
-                    if (!bridgeGame.retry(retry)) {
-                        output.printResult(position, false, bridgeGame);
-                        break;
-                    }
-                }
-            }
+            runGame();
         } catch (Exception e) {
             System.out.println(e.getMessage());
         }
     }
 
+    private static void runGame() {
+        while (!stopFlag) {
+            boolean available = moveAndPrint(position);
+            if (available) {
+                executeInAvailable();
+            }
+            if (!available) {
+                executeInUnavailable();
+            }
+        }
+    }
+
     private static boolean moveAndPrint(int position) {
         String moving = input.readMoving();
-        boolean available = bridgeGame.isAvailable(moving, position);
+        boolean available = bridgeGame.move(moving, position);
         output.printMap(position, available, bridgeGame.bridge);
         return available;
+    }
+
+    private static void executeInAvailable() {
+        position += 1;
+        if (position == bridgeSize) {
+            stopGame(bridgeSize - 1, true);
+        }
+    }
+
+    private static void executeInUnavailable() {
+        String retry = input.readGameCommand();
+        if (!bridgeGame.retry(retry)) {
+            stopGame(position, false);
+        }
+    }
+
+    private static void stopGame(int position, boolean available) {
+        output.printResult(position, available, bridgeGame);
+        stopFlag = true;
     }
 }
