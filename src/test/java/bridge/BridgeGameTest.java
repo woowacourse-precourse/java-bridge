5eca8c5ab1bcfdbc95b46d98838277ad8daf5c17
jaehyuenjung <86586206+jaehyuenjung@users.noreply.github.com>
2022-11-18 13:22:15 +0900

    feat(bridge): 사용자가 칸을 이동하는 기능 추가

---
+++ src/test/java/bridge/BridgeGameTest.java
@@ -0,0 +1,33 @@
+package bridge;
+
+import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
+import static org.assertj.core.api.Assertions.assertThat;
+
+import bridge.model.UserRoadMap;
+import java.util.List;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class BridgeGameTest {
+    private UserRoadMap userRoadMap;
+    private BridgeGame bridgeGame;
+
+    @BeforeEach
+    void setBridgeGame(){
+        userRoadMap = new UserRoadMap(List.of("U"));
+        bridgeGame = new BridgeGame(userRoadMap);
+    }
+
+    @DisplayName("사용자가 칸을 이동한다.")
+    @Test
+    void testMove(){
+        String direction = "U";
+        assertSimpleTest(() -> {
+            int originBridgeSize = userRoadMap.getSize();
+           bridgeGame.move(direction);
+           assertThat(userRoadMap.getSize())
+                   .isEqualTo(originBridgeSize + 1);
+        });
+    }
+}
