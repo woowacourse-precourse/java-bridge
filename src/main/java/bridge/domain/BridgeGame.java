c7a8cc431b1e523797b78d7f4084802c8e60398b
dev_kong <ahdjd5@gmail.com>
2022-11-18 17:20:03 +0900

    feat(BridgeGame): add User instance field

--- src/main/java/bridge/domain/BridgeGame.java
+++ src/main/java/bridge/domain/BridgeGame.java
@@ -4,9 +4,10 @@ import java.util.List;
 
 public class BridgeGame {
     List<String> answerBridge;
-
-    public BridgeGame(List<String> answerBridge) {
+    User user;
+    public BridgeGame(List<String> answerBridge, User user) {
         this.answerBridge = answerBridge;
+        this.user = user;
     }
 
     public void move() {
