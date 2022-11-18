e60588990192a83b0183fcb2dcf3e0aa55b4243a
LHS-11 <ghktjq1119@naver.com>
2022-11-18 22:21:12 +0900

    [Feat] : 재시도 여부 입력 받기 구현

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -9,5 +9,6 @@ public class Application {
         InputView inputView = new InputView();
         inputView.readBridgeSize();
         inputView.readMoving();
+        inputView.readGameCommand();
     }
 }
