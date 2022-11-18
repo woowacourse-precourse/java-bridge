0db70b96eefdfc691e975a42b371a3bb748ecb9a
Hayoon <hy97@sju.ac.kr>
2022-11-18 23:31:16 +0900

    feat(Player): 클래스 변수 접근 제어자 변경

--- src/main/java/bridge/model/Player.java
+++ src/main/java/bridge/model/Player.java
@@ -1,18 +1,13 @@
 package bridge.model;
 
 public class Player {
-    private static int currentLocation;
+    public static int currentLocation;
 
     public Player() {
         currentLocation = -1;
     }
 
     public void moveStraight() {
-        this.currentLocation += 1;
+        currentLocation += 1;
     }
-
-    public static int getCurrentLocation() {
-        return currentLocation;
-    }
-
 }
