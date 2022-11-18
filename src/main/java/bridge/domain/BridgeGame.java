17b5eee944ea63f91a722d68d072cef3032eb50f
aak2075 <35948985+aak2075@users.noreply.github.com>
2022-11-18 22:15:25 +0900

    feat(bridge) : 다리를 건너는 기능 추가

--- src/main/java/bridge/domain/BridgeGame.java
+++ src/main/java/bridge/domain/BridgeGame.java
@@ -1,16 +1,40 @@
 package bridge.domain;
 
+import java.util.List;
+
 /**
  * 다리 건너기 게임을 관리하는 클래스
  */
 public class BridgeGame {
+    private final List<String> bridge;
+    private int position;
+    private boolean alive;
+
+    public BridgeGame(BridgeMaker bridgeMaker, int size) {
+        bridge = bridgeMaker.makeBridge(size);
+        alive = true;
+    }
 
     /**
      * 사용자가 칸을 이동할 때 사용하는 메서드
      * <p>
      * 이동을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void move() {
+    public void move(String input) {
+        position = position + 1;
+        alive = isCorrect(input);
+    }
+
+    private boolean isCorrect(String input) {
+        return bridge.get(position - 1).equals(input);
+    }
+
+    public boolean isAlive() {
+        return alive;
+    }
+
+    public boolean isPlaying() {
+        return position < bridge.size();
     }
 
     /**
