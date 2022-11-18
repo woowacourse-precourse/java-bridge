b7499b9ff0f617eab7009bef79b0fe0ceb5df8c8
Hayoon <hy97@sju.ac.kr>
2022-11-18 21:44:00 +0900

    feat(Application): 메인 메서드 게임 컨트롤러 구현

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -1,8 +1,20 @@
 package bridge;
 
+import bridge.controller.GameMachine;
+import bridge.model.*;
+import bridge.view.InputView;
+import bridge.view.OutputView;
+
 public class Application {
 
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        Bridge bridge = new Bridge();
+        BridgeRandomNumberGenerator numberGenerator = new BridgeRandomNumberGenerator();
+        GameStatistics gameStatistics = new GameStatistics();
+        BridgeGame bridgeGame = new BridgeGame(new BridgeMaker(numberGenerator), new Player(), gameStatistics, bridge);
+        InputView inputView = new InputView(bridgeGame, new BridgeException(), gameStatistics);
+        OutputView outputView = new OutputView(bridgeGame, gameStatistics, bridge);
+        new GameMachine(inputView, outputView);
     }
 }
