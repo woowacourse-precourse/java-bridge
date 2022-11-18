013cdc3cf6b321bc852ec637f4697e8406aeba40
MuSeung <peter0757@nvaer.com>
2022-11-18 17:47:16 +0900

    refactor: InputView의 반복되는 구조를 메서드로 만들고 inputError에서 에러와 예외를 관리하도록 함
    InputView
        - readBridgeSize()
        - readMoving()
        - readGameCommand()
        : 예외 테스트를 inputError의 메서드에서 구현
        - readInput() : readLine()사용 중 공백이 입력될 경우 IllegalArgumentException에러가 발생하도록 함
    InputError
        - 에러 메세지를 저장하고 에러 조건을 체크함
        - nonAllowedInputCheck(input, inputError) : 허용된 입력 이외의 입력이 주어진 경우 에러 발생
        - inputLengthCheck(input) : 길이가 1이 아닌 경우 에러 발생
        - boundaryCheck(size) : 사이즈가 3~20이 아닌 경우 에러 발생
        - makeError(InputError) : 객체에 저장된 메세지를 담아서 IllegalArgumentException 를 발생시킴

--- src/main/java/bridge/BridgeState.java
+++ src/main/java/bridge/BridgeState.java
@@ -5,6 +5,9 @@ public enum BridgeState {
     DOWN(0, "D");
     final int bridgeNumber;
     final String bridgeMark;
+
+    public static final int MIN_SIZE = 3;
+    public static final int MAX_SIZE = 20;
     BridgeState(int bridgeNumber, String bridgeMark) {
         this.bridgeNumber = bridgeNumber;
         this.bridgeMark = bridgeMark;
--- src/main/java/bridge/InputView.java
+++ src/main/java/bridge/InputView.java
@@ -1,59 +1,39 @@
 package bridge;
 
+import static bridge.InputError.*;
 import static camp.nextstep.edu.missionutils.Console.readLine;
 
 import java.util.NoSuchElementException;
 
-/**
- * 사용자로부터 입력을 받는 역할을 한다.
- */
 public class InputView {
 
-    /**
-     * 다리의 길이를 입력받는다.
-     */
     public int readBridgeSize() {
         String input = readInput();
-        String onlyNumber = input.replaceAll("[^0-9]", "");
-        if (input.length() != onlyNumber.length()) {
-            throw new IllegalArgumentException();
-        }
+        nonAllowedInputCheck(input, NOT_NUMBER);
         int size = Integer.parseInt(input);
-        if (size < 3 || size > 20) {
-            throw new IllegalArgumentException();
-        }
+        boundaryCheck(size);
         return size;
     }
-
-    /**
-     * 사용자가 이동할 칸을 입력받는다.
-     */
     public String readMoving() {
         String input = readInput();
-        if(!(input.equals("D")||input.equals("U"))){
-            throw new IllegalArgumentException();
-        }
+        inputLengthCheck(input);
+        nonAllowedInputCheck(input, WRONG_MOVING_INPUT);
         return input;
     }
 
-    /**
-     * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
-     */
     public String readGameCommand() {
         String input = readInput();
-        if(!(input.equals("Q")||input.equals("R"))){
-            throw new IllegalArgumentException();
-        }
+        inputLengthCheck(input);
+        nonAllowedInputCheck(input, WRONG_GAME_COMMAND_INPUT);
         return input;
     }
-    private String readInput(){
-        String input;
+    private String readInput() {
+        String input = "";
         try {
             input = readLine();
-        }catch(NoSuchElementException e){
-            throw new IllegalArgumentException();
+        } catch (NoSuchElementException unentered) {
+            makeError(UNENTERED);
         }
         return input;
     }
-
 }
