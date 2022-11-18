b9154b7375da67174d0139a2e8bd3d41e0678a5e
PgmJun <84304802+PgmJun@users.noreply.github.com>
2022-11-18 20:33:39 +0900

    fix : 메세지 오류 수정

--- src/main/java/bridge/constant/Constants.java
+++ src/main/java/bridge/constant/Constants.java
@@ -25,7 +25,7 @@ public class Constants {
         public static final String CHECK_RETRY_MESSAGE = "게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)";
         public static final String GAME_RESULT_MESSAGE = "최종 게임 결과";
         public static final String IS_GAME_WIN_MESSAGE = "게임 성공 여부: %s";
-        public static final String TOTAL_TRY_CNT_MESSAGE = "총 시도한 횟수: $d";
+        public static final String TOTAL_TRY_CNT_MESSAGE = "총 시도한 횟수: %d";
     }
 
     public static class Error {
