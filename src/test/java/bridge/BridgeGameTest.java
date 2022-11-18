5b5a5fbbb3893a62d05ac25c6451315fe47ecd36
saint6839 <saint6839@gmail.com>
2022-11-18 19:31:18 +0900

    test(domain) : 사용자가 index번째 입력한 이동할 칸 값과 index번째 다리의 정답이 일치하지 않을때 거짓인지 테스트하는 테스트 작성

--- src/test/java/bridge/BridgeGameTest.java
+++ src/test/java/bridge/BridgeGameTest.java
@@ -11,7 +11,7 @@ class BridgeGameTest {
 
     @DisplayName("사용자의 index번째에 입력한 이동할 칸 값과 index번째 다리의 정답이 일치할때 참을 반환하는지 테스트")
     @Test
-    void move() {
+    void moveTrue() {
         //given
         Bridge bridge = new Bridge(List.of("U", "D", "U"));
         String moving = "U";
@@ -23,5 +23,21 @@ class BridgeGameTest {
         //then
         assertThat(isMove)
                 .isTrue();
+    }
+
+    @DisplayName("사용자의 index번째에 입력한 이동할 칸 값과 index번째 다리의 정답이 일치하지 않을 때 거짓을 반환하는지 테스트")
+    @Test
+    void moveFalse() {
+        //given
+        Bridge bridge = new Bridge(List.of("U", "D", "U"));
+        String moving = "D";
+        int index = 2;
+
+        BridgeGame bridgeGame = new BridgeGame();
+        //when
+        boolean isMove = bridgeGame.move(bridge, moving, index);
+        //then
+        assertThat(isMove)
+                .isFalse();
     }
 }
