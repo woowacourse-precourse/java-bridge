d2fae2d4a7fe33cd9c4b10c84cc487641d78d281
ykmxxi <ykmxxi97@gmail.com>
2022-11-18 14:52:40 +0900

    feat: 이동할 칸 입력 예외 처리 skeleton code 구현

---
+++ src/main/java/bridge/validation/MovingValidation.java
@@ -0,0 +1,13 @@
+package bridge.validation;
+
+public class MovingValidation {
+
+    public void isNull(String moving) {
+    }
+
+    public void isBlank(String moving) {
+    }
+
+    public void isUpperCaseUD(String moving) {
+    }
+}
