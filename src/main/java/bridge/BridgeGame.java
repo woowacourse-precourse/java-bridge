bd36c0b4d1df8b7ca91c08ddc0d605dd6faf992c
juhwang <juhong4930@naver.com>
2022-11-19 00:36:13 +0900

    feat(BridgeGame): create return current bridge method and return the number of try method

--- src/main/java/bridge/BridgeGame.java
+++ src/main/java/bridge/BridgeGame.java
@@ -2,6 +2,8 @@ package bridge;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.Collectors;
 
 /**
  * 다리 건너기 게임을 관리하는 클래스
@@ -11,7 +13,8 @@ public class BridgeGame {
     public static final String RETRY_COMMAND = "R";
 
     private final List<String> bridge;
-    private final List<BridgeLocation> bridgeLocations = new ArrayList<>();
+    private final List<String> userAnswer = new ArrayList<>();
+    private int numberOfTry = 1;
 
     BridgeGame(List<String> bridge) {
         this.bridge = bridge;
@@ -22,10 +25,9 @@ public class BridgeGame {
      * <p>
      * 이동을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public List<BridgeLocation> move(String location) {
-        boolean correctLocation = bridge.get(bridgeLocations.size()).equals(location);
-        bridgeLocations.add(BridgeLocation.createLocation(location, correctLocation));
-        return bridgeLocations;
+    public boolean move(String location) {
+        userAnswer.add(location);
+        return bridge.get(userAnswer.size()).equals(location);
     }
 
     /**
@@ -34,6 +36,26 @@ public class BridgeGame {
      * 재시작을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
     public boolean retry(String retryCommand) {
-        return RETRY_COMMAND.equals(retryCommand);
+        return countRetry(RETRY_COMMAND.equals(retryCommand));
+    }
+
+    private boolean countRetry(boolean retry) {
+        if (retry) {
+            userAnswer.clear();
+            numberOfTry++;
+        }
+        return retry;
+    }
+
+    public List<BridgeLocation> currentBridge() {
+        AtomicInteger startBridge = new AtomicInteger();
+        return userAnswer.stream()
+                .map(location ->
+                        BridgeLocation.createLocation(location, bridge.get(startBridge.getAndIncrement()).equals(location)))
+                .collect(Collectors.toList());
+    }
+
+    public int getNumberOfTry() {
+        return numberOfTry;
     }
 }
