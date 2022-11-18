3ab1924d675249855e4e654a77f1429981f75979
SuhHyeonjun <dmsgktn0208@gmail.com>
2022-11-18 11:37:42 +0900

    refactor : 무작위 값이 0인 경우 아래 칸, 1인 경우 위 칸

--- src/main/java/bridge/BridgeMaker.java
+++ src/main/java/bridge/BridgeMaker.java
@@ -7,6 +7,7 @@ import java.util.List;
  * 다리의 길이를 입력 받아서 다리를 생성해주는 역할을 한다.
  */
 public class BridgeMaker {
+    private List<String> bridges = new ArrayList<>();
     private static final String UP = "U";
     private static final String Down = "D";
     private final BridgeNumberGenerator bridgeNumberGenerator;
@@ -20,8 +21,7 @@ public class BridgeMaker {
      * @return 입력받은 길이에 해당하는 다리 모양. 위 칸이면 "U", 아래 칸이면 "D"로 표현해야 한다.
      */
     public List<String> makeBridge(int size) {
-        List<String> bridges = new ArrayList<>(size);
-        String[] UpDown = {UP, Down};
+        String[] UpDown = {Down, UP};
         for (int i = 0 ; i < size ; i++){
             bridges.add(UpDown[bridgeNumberGenerator.generate()]);
         }
