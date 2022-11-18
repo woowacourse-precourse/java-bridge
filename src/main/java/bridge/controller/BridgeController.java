43e3b9a0e9aa37db13763111a2b2b199abff7a21
Hongsugyeong <olivia258@naver.com>
2022-11-19 00:26:24 +0900

    (feat) : 플레이어 이동할 칸 입력 예외 처리

--- src/main/java/bridge/controller/BridgeController.java
+++ src/main/java/bridge/controller/BridgeController.java
@@ -4,6 +4,7 @@ import bridge.BridgeMaker;
 import bridge.BridgeNumberGenerator;
 import bridge.BridgeRandomNumberGenerator;
 import bridge.domain.Bridge;
+import bridge.domain.Move;
 import bridge.view.InputView;
 
 public class BridgeController {
@@ -11,6 +12,7 @@ public class BridgeController {
     InputView inputView = new InputView();
     Bridge bridge = new Bridge();
     BridgeMaker bridgeMaker = new BridgeMaker(new BridgeRandomNumberGenerator());
+    Move mm = new Move();
 
     public void start() {
         String size = inputView.readBridgeSize();
@@ -19,6 +21,7 @@ public class BridgeController {
         int s = Integer.parseInt(size);
         bridgeMaker.makeBridge(s);
         String move = inputView.readMoving();
+        mm.isProper(move);
 
     }
 
--- src/main/java/bridge/view/ExceptionMessage.java
+++ src/main/java/bridge/view/ExceptionMessage.java
@@ -9,4 +9,8 @@ public class ExceptionMessage {
     public static String isRangeNumberMsg() {
         return "3이상 20이하의 숫자를 입력해야합니다.";
     }
+
+    public static String isProperMsg() {
+        return "U 또는 D를 입력해주세요";
+    }
 }
