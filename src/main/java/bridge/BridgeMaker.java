297142ee74806d1b334c45dfc073b06d25a9ffcb
이현진 <ehj1107@naver.com>
2022-11-19 01:09:29 +0900

    feat: 입력받은 값의 크기만큼 다리 생성 구현

--- src/main/java/bridge/BridgeMaker.java
+++ src/main/java/bridge/BridgeMaker.java
@@ -1,11 +1,22 @@
 package bridge;
 
 import java.util.List;
+import java.util.ArrayList;
 
 /**
  * 다리의 길이를 입력 받아서 다리를 생성해주는 역할을 한다.
  */
 public class BridgeMaker {
+    public enum Move{
+        D(0),
+        U(1);
+
+        public final int value;
+
+        Move(int value) {
+            this.value = value;
+        }
+    }
 
     private final BridgeNumberGenerator bridgeNumberGenerator;
 
@@ -18,6 +29,23 @@ public class BridgeMaker {
      * @return 입력받은 길이에 해당하는 다리 모양. 위 칸이면 "U", 아래 칸이면 "D"로 표현해야 한다.
      */
     public List<String> makeBridge(int size) {
+        List<String> bridge = new ArrayList<>();
+
+        for(int i = 0; i < size; i++){
+            int random = bridgeNumberGenerator.generate();
+            Move move = getMove(random);
+            bridge.add(move.name());
+        }
+
+        return bridge;
+    }
+
+    private Move getMove(int value){
+        for(Move move : Move.values()){
+            if(move.value == value)
+                return move;
+        }
+
         return null;
     }
 }
