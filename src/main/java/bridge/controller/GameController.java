5ef9cb4b126c80977d86ec9797826166759213a8
Kayoung <rkdkdudjd@gmail.com>
2022-11-18 18:06:06 +0900

    refactor(GameController): 매직넘버는 상수로

--- src/main/java/bridge/controller/GameController.java
+++ src/main/java/bridge/controller/GameController.java
@@ -5,6 +5,7 @@ import bridge.view.InputView;
 import bridge.view.OutputView;
 
 public class GameController {
+    private static final String QUIT_COMMAND = "Q";
     private static final BridgeGame game = new BridgeGame();
     private static final InputView input = new InputView();
     private static final OutputView output = new OutputView();
@@ -33,10 +34,14 @@ public class GameController {
 
     private static void quitOrRetry() {
         String command = input.readGameCommand();
-        if (command.equals("Q")) {
+        if (wantToQuit(command)) {
             game.quit();
             return;
         }
         game.retry();
+    }
+
+    private static boolean wantToQuit(String command) {
+        return command.equals(QUIT_COMMAND);
     }
 }
