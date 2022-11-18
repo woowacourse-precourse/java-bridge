823d8b6023faae679df3277ed908a7ac13522b5e
devjordy14 <devjordy14@gmail.com>
2022-11-18 18:06:50 +0900

    feat(InputView): add features for readBridgeSize function

--- src/main/java/bridge/InputView.java
+++ src/main/java/bridge/InputView.java
@@ -1,15 +1,25 @@
 package bridge;
 
+import camp.nextstep.edu.missionutils.Console;
+
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
  */
 public class InputView {
 
+    private final int MIN_LENGTH_OF_BRIDGE = 3;
+    private final int MAX_LENGTH_OF_BRIDGE = 20;
+    private final String ERROR_MESSAGE = "[ERROR] 다리 길이는 3부터 20 사이의 숫자여야 합니다.\n다시 입력해 주세요.";
+
     /**
      * 다리의 길이를 입력받는다.
      */
     public int readBridgeSize() {
-        return 0;
+        System.out.println("다리 건너기 게임을 시작합니다.\n");
+        System.out.println("다리의 길이를 입력해주세요.");
+        String input = Console.readLine();
+        String checkedInput = receiveInputUntilCondition(input);
+        return stringToInteger(checkedInput);
     }
 
     /**
@@ -24,5 +34,41 @@ public class InputView {
      */
     public String readGameCommand() {
         return null;
+    }
+
+    private String receiveInputUntilCondition(String input){
+        while ((!isNumber(input))||(!isSizeOfNumber(input))) {
+            input = Console.readLine();
+        }
+        return input;
+    }
+
+    private boolean isNumber(String str) {
+        try {
+            Double.parseDouble(str);
+            return true;
+        } catch (IllegalArgumentException e) {
+            System.out.println(ERROR_MESSAGE);
+            return false;
+        }
+    }
+
+    private boolean isSizeOfNumber(String input) {
+        int number = stringToInteger(input);
+        if (MIN_LENGTH_OF_BRIDGE <= number && number <= MAX_LENGTH_OF_BRIDGE) {
+            return true;
+        }
+        System.out.println(ERROR_MESSAGE);
+        return false;
+    }
+
+    private int stringToInteger(String str) {
+        try {
+            int number = Integer.parseInt(str);
+            return number;
+        } catch (IllegalArgumentException e) {
+            System.out.println(ERROR_MESSAGE);
+            return 0;
+        }
     }
 }
