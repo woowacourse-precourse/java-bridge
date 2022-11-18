2eac6e302ced2f3f468d1d5bf5280069fe0772b2
nn98 <jkllhgb@gmail.com>
2022-11-18 14:35:11 +0900

    Feat: 다리 생성 기능 정의-BridgeMaker 호출

--- src/main/java/bridge/BridgeGame.java
+++ src/main/java/bridge/BridgeGame.java
@@ -1,10 +1,18 @@
 package bridge;
 
+import java.util.List;
+
 /**
  * 다리 건너기 게임을 관리하는 클래스
  */
 public class BridgeGame {
 
+    private List<String> bridge;
+    public BridgeGame(int size) {
+        BridgeMaker bridgeMaker=new BridgeMaker(new BridgeRandomNumberGenerator());
+        bridge=bridgeMaker.makeBridge(size);
+        System.out.println(bridge);
+    }
     /**
      * 사용자가 칸을 이동할 때 사용하는 메서드
      * <p>
