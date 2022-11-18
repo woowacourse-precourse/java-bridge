7a21ff3db8a029397682b8473890ad237b76db1b
jsc1014 <wotlr5209@gmail.com>
2022-11-19 00:47:51 +0900

    feat: 다리 길이 입력

--- src/main/java/bridge/InputView.java
+++ src/main/java/bridge/InputView.java
@@ -1,5 +1,7 @@
 package bridge;
 
+import camp.nextstep.edu.missionutils.Console;
+
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
  */
@@ -8,10 +10,34 @@ public class InputView {
     /**
      * 다리의 길이를 입력받는다.
      */
-    public int readBridgeSize() {
-        return 0;
+    public static int readBridgeSize() {
+        int bridgeSize = 0;
+        System.out.println("다리의 길이를 입력해 주세요.");
+        bridgeSize = Integer.parseInt(repeatUntilRightInput());
+        return bridgeSize;
     }
 
+    //  10줄 초과! 리팩토링 필요  //
+    private static String repeatUntilRightInput(){
+        String input = "";
+        while(true){
+            input = Console.readLine();
+            try{
+                errorBridgeSizeOverRange(input);
+                break;
+            } catch (IllegalArgumentException e) {
+                System.out.println("[ERROR] 3~20 사이의 값을 입력해야 합니다.");
+            }
+        }
+        return input;
+    }
+
+    public static void errorBridgeSizeOverRange(String input){
+        int bridgeSize = Integer.parseInt(input);
+        if(bridgeSize < 3 || 20 < bridgeSize){
+            throw new IllegalArgumentException();
+        }
+    }
     /**
      * 사용자가 이동할 칸을 입력받는다.
      */
