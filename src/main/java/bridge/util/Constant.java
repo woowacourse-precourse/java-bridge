fa2dc6c47971e45b0b2903677d772235c25c8ff0
asas6978 <asas6978@naver.com>
2022-11-19 01:00:34 +0900

    feat: 상수 추가

--- src/main/java/bridge/util/Constant.java
+++ src/main/java/bridge/util/Constant.java
@@ -1,6 +1,9 @@
 package bridge.util;
 
 public class Constant {
+    public static final String SUCCESS_OR_FAIL = "성공/실패";
+    public static final String NUMBER_OF_ATTEMPTS = "성공 횟수";
+    public static final String EMPTY = " ";
     public static final String UP = "U";
     public static final String DOWN = "D";
     public static final String RETRY = "R";
