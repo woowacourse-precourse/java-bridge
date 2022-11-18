b6618df6827f92f85c5b44f7b1fc262b6e4ae1ed
맹순영 <symaeng98@naver.com>
2022-11-18 21:07:35 +0900

    feat(BridgeGame): 다시 시작 기능 구현
    리스트 및 현재 칸 초기화, 시도 횟수 증가

--- src/main/java/bridge/BridgeGame.java
+++ src/main/java/bridge/BridgeGame.java
@@ -41,6 +41,10 @@ public class BridgeGame {
      * 재시작을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
     public void retry() {
+        this.upBoardCheckList.clear();
+        this.downBoardCheckList.clear();
+        this.stage = 0;
+        this.tryCount += 1;
     }
 
     private boolean addCheckToUpList(String board) {
