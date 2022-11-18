f5998d5b9ac0b74158b0026b8892fd656a98944f
gyuturn <rbtjrrbals@gmail.com>
2022-11-18 21:47:21 +0900

    fix: 콘솔출력 줄바꿈 수정
    요구사항과 같은 출력사항을 맞추기 위함

--- src/main/java/util/ConsoleMessage.java
+++ src/main/java/util/ConsoleMessage.java
@@ -1,7 +1,7 @@
 package util;
 
 public class ConsoleMessage {
-    public static final String START_BRIDGE_GAME="다리 건너기 게임을 시작합니다.";
+    public static final String START_BRIDGE_GAME="다리 건너기 게임을 시작합니다.\n";
 
     public static final String RECEIVE_BRIDGE_SIZE="다리의 길이를 입력해주세요.";
 }
