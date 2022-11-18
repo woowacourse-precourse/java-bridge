c22a7ae75beceb26ee8407678b601eb1467b16d0
Devtist <yt930310@snu.ac.kr>
2022-11-18 21:29:38 +0900

    feat: 이동할 칸의 입력이 유효한지 검증하는 기능 구현

--- src/main/java/util/Validator.java
+++ src/main/java/util/Validator.java
@@ -2,6 +2,8 @@ package util;
 
 import java.util.List;
 
+import bridgeConstant.Cell;
+
 public class Validator {
 	private static final String NUMBER_REGEX = "\\d+";
 
@@ -14,6 +16,12 @@ public class Validator {
 	public static void validateSizeIsNumber(String number) {
 		if (!number.matches(NUMBER_REGEX)) {
 			throw new IllegalArgumentException("[ERROR] 다리의 길이는 숫자여야 합니다.");
+		}
+	}
+
+	public static void validateCellPosition(String position) {
+		if (!position.equals(Cell.upPosition()) && !position.equals(Cell.downPosition())) {
+			throw new IllegalArgumentException("[ERROR] 이동할 칸은 U와 D 둘 중 하나여야 합니다.");
 		}
 	}
 }
