f7d09e20a39e7f26f57b43d546bbf9a72653367a
khsrla9806 <kimhunsope@naver.com>
2022-11-18 22:34:45 +0900

    feat(Application): 프로그램의 main 함수 구현

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -1,8 +1,17 @@
 package bridge;
 
-public class Application {
+import bridge.controller.BridgeGameController;
+import bridge.domain.BridgeGame;
+import bridge.view.InputView;
+import bridge.view.OutputView;
 
+public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        InputView inputView = new InputView();
+        OutputView outputView = new OutputView();
+        BridgeGame bridgeGame = new BridgeGame();
+        BridgeGameController bridgeGameController = new BridgeGameController(inputView, outputView, bridgeGame);
+
+        bridgeGameController.playBridgeGame();
     }
 }
