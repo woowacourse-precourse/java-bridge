e17ff1fb9370b9261f55b428397f9292755281ea
2021110806 <2021110806@dgu.ac.kr>
2022-11-18 19:39:53 +0900

    style(Output):enum값 표기법 변환

--- src/main/java/bridge/Output.java
+++ src/main/java/bridge/Output.java
@@ -2,12 +2,12 @@ package bridge;
 
 public enum Output {
 
-    gameStart("다리 건너기 게임을 시작합니다."),
-    bridgeLengthInputRequest("다리의 길이를 입력해주세요."),
-    whereToGoInputRequest("이동할 칸을 선택해주세요. (위: U, 아래: D)"),
-    continueOrEndRequest("게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)"),
-    winOrLoseResult("게임 성공 여부: "),
-    howManyAttempts("총 시도한 횟수: ");
+    GAME_START("다리 건너기 게임을 시작합니다."),
+    BRIDGE_LENGTH_INPUT_REQUEST("다리의 길이를 입력해주세요."),
+    WHERE_TO_GO_INPUT_REQUEST("이동할 칸을 선택해주세요. (위: U, 아래: D)"),
+    CONTINUE_OR_END_REQUEST("게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)"),
+    WIN_OR_LOSE("게임 성공 여부: "),
+    HOW_MANY_ATTEMPTS("총 시도한 횟수: ");
 
     private final String output;
 
--- src/main/java/bridge/OutputView.java
+++ src/main/java/bridge/OutputView.java
@@ -5,15 +5,15 @@ import java.util.List;
 public class OutputView {
 
     public void printGameStart() {
-        System.out.println(Output.gameStart.getOutput());
+        System.out.println(Output.GAME_START.getOutput());
     }
 
     public void printBridgeLengthInputRequest() {
-        System.out.println(Output.bridgeLengthInputRequest.getOutput());
+        System.out.println(Output.BRIDGE_LENGTH_INPUT_REQUEST.getOutput());
     }
 
     public void printWhereToGoInputRequest() {
-        System.out.println(Output.whereToGoInputRequest.getOutput());
+        System.out.println(Output.WHERE_TO_GO_INPUT_REQUEST.getOutput());
     }
 
     public void printMap(List<String> mapUpper, List<String> mapLower) {
@@ -26,15 +26,15 @@ public class OutputView {
     }
 
     public void printContinueOrEndRequest() {
-        System.out.println(Output.continueOrEndRequest.getOutput());
+        System.out.println(Output.CONTINUE_OR_END_REQUEST.getOutput());
     }
 
     public void printResult(boolean isWin, int numberOfAttempt) {
         if (isWin) {
-            System.out.println(Output.winOrLoseResult.getOutput()+"성공");
+            System.out.println(Output.WIN_OR_LOSE.getOutput()+"성공");
             return;
         }
-        System.out.println(Output.howManyAttempts.getOutput()+"실패");
+        System.out.println(Output.HOW_MANY_ATTEMPTS.getOutput()+"실패");
     }
 
 }
