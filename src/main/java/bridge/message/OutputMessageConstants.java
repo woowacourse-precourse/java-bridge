cf2901bdcb2c2733c127e391f2967dd8007513ac
kimjeonghan <[rlawjdgks318@naver.com]>
2022-11-18 16:52:05 +0900

    feat(OutputMessageConstants): 사용자 출력 메시지 상수 정의 추가

---
+++ src/main/java/bridge/message/OutputMessageConstants.java
@@ -0,0 +1,10 @@
+package bridge.message;
+
+public interface OutputMessageConstants {
+
+	String START_MESSAGE = "다리 건너기 게임을 시작합니다.\n\n";
+
+	String GAME_RESULT_TITLE_MESSAGE = "최종 게임 결과\n";
+	String GAME_RESULT_MESSAGE_FORMAT = "게임 성공 여부: %s\n";
+	String TOTAL_PLAY_COUNT_MESSAGE_FORMAT = "총 시도한 횟수: %d\n";
+}
