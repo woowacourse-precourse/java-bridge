8e6514b68d51df7a88bdb2a3ca4ff1d63fd78759
lee <whddlsk123@naver.com>
2022-11-19 03:46:41 +0900

    feat(initBridgeGame) : 다리의 길이를 입력 받은 후 길이 만큼 다리를 생성

--- src/main/java/bridge/config/BridgeConfig.java
+++ src/main/java/bridge/config/BridgeConfig.java
@@ -1,19 +1,32 @@
 package bridge.config;
 
+import bridge.BridgeNumberGenerator;
 import bridge.BridgeRandomNumberGenerator;
 
 import bridge.domain.BridgeGame;
 import bridge.generator.BridgeMaker;
+import bridge.validator.Validator;
+import bridge.view.InputView;
 
 public class BridgeConfig {
 
     public BridgeMaker bridgeMaker() {
-        return new BridgeMaker(new BridgeRandomNumberGenerator());
+        return new BridgeMaker(bridgeNumberGenerator());
+    }
+    public BridgeNumberGenerator bridgeNumberGenerator() {
+        return new BridgeRandomNumberGenerator();
     }
 
     public BridgeGame bridgeGame(int size) {
         BridgeMaker bridgeMaker = bridgeMaker();
         return new BridgeGame(bridgeMaker.makeBridge(size));
     }
-    
+
+    public InputView inputView() {
+        return new InputView(validator());
+    }
+
+    public Validator validator() {
+        return new Validator();
+    }
 }
--- src/main/java/bridge/view/Messages.java
+++ src/main/java/bridge/view/Messages.java
@@ -4,7 +4,11 @@ public enum Messages {
     ERROR_INPUT_NUMBER("[ERROR] 입력은 숫자만 가능합니다."),
     ERROR_BRIDGE_SIZE("[ERROR] 다리의 길이는 3 ~ 20까지만 입력 가능합니다."),
     ERROR_MOVE("[ERROR] \"U\"와 \"D\"만 입력 가능합니다."),
-    ERROR_GAME_COMMAND("[ERROR] \"R\"과 \"Q\"만 입력 가능합니다.");
+    ERROR_GAME_COMMAND("[ERROR] \"R\"과 \"Q\"만 입력 가능합니다."),
+    VIEW_START_GAME("다리 건너기 게임을 시작합니다."),
+    VIEW_SELECT_UP_DOWN("이동할 칸을 선택해주세요. (위: U, 아래: D)"),
+    VIEW_RESTART("게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)");
+
 
     private String message;
 
