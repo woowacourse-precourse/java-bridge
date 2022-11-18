d42ab05a7963ca2007fd358ae915a2c3aad43726
juhee park <jouhi3322@gmail.com>
2022-11-19 02:22:58 +0900

    feat : 다리의 길이를 입력 기능

--- src/main/java/bridge/InputView.java
+++ src/main/java/bridge/InputView.java
@@ -1,15 +1,40 @@
 package bridge;
 
+import camp.nextstep.edu.missionutils.Console;
+
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
  */
 public class InputView {
+    private static final String ERROR_BRIDGE_LENGTH_NOT_NUMBER = "[ERROR] 다리 길이는 숫자여야 합니다.";
+    private static final String ERROR_BRIDGE_LENGTH_RANGE = "[ERROR] 다리 길이는 3부터 20 사이의 숫자여야 합니다.";
+    private static final int BRIDGE_RANGE_START = 3;
+    private static final int BRIDGE_RANGE_END = 20;
+
 
     /**
      * 다리의 길이를 입력받는다.
      */
     public int readBridgeSize() {
-        return 0;
+        String bridgeLength = Console.readLine();
+        return validateBridgeSize(bridgeLength);
+    }
+
+    //숫자이고 가능한 길이 인지 확인한다.
+    public int validateBridgeSize(String bridgeLength) {
+        int bridgeLen = StringToInteger(bridgeLength);
+        if (BRIDGE_RANGE_START <= bridgeLen && bridgeLen <= BRIDGE_RANGE_END) {
+            return bridgeLen;
+        }
+        throw new IllegalArgumentException(ERROR_BRIDGE_LENGTH_RANGE);
+    }
+
+    private int StringToInteger(String bridgeLength) {
+        try {
+            return Integer.parseInt(bridgeLength);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException(ERROR_BRIDGE_LENGTH_NOT_NUMBER);
+        }
     }
 
     /**
