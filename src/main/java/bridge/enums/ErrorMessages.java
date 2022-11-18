b686321c306af376e3db413c068701ea923c8928
jeongwle <dldl4568@naver.com>
2022-11-18 18:40:08 +0900

    chore: enum명 수정

--- src/main/java/bridge/enums/ErrorMessages.java
+++ src/main/java/bridge/enums/ErrorMessages.java
@@ -1,7 +1,7 @@
 package bridge.enums;
 
 public enum ErrorMessages {
-    BRIDGE_LENGTH_ERROR_MESSAGE("다리의 길이는 3이상 20이하의 숫자여야 합니다."),
+    BRIDGE_SIZE_ERROR_MESSAGE("다리의 길이는 3이상 20이하의 숫자여야 합니다."),
     MOVING_DIRECTION_ERROR_MESSAGE("이동방향은 U 또는 D여야 합니다.(위: U, 아래: D)"),
     GAME_COMMAND_ERROR_MESSAGE("게임 재시작 여부는 R 또는 Q여야 합니다.(재시작: R, 종료: Q)"),
     ;
