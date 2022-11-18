2a164421494667580c91a4d2de1316473fcfd191
HamSeungChan <head0618@naver.com>
2022-11-18 09:28:28 +0900

    feat : 이동할 칸 입력 기능 구현

--- src/main/java/bridge/view/OutputView.java
+++ src/main/java/bridge/view/OutputView.java
@@ -4,6 +4,7 @@ public class OutputView {
 
     private static final String PRINT_GAME_START_MESSAGE = "다리 건너기 게임을 시작합니다.\n";
     private static final String INPUT_BRIDGE_SIZE_MESSAGE = "다리의 길이를 입력해주세요.";
+    private static final String INPUT_MOVE_MESSAGE = "\n이동할 칸을 선택해주세요. (위: U, 아래: D)";
 
     public void printMap() {
     }
@@ -17,6 +18,10 @@ public class OutputView {
 
     public static void inputBridgeSize() {
         System.out.println(INPUT_BRIDGE_SIZE_MESSAGE);
+    }
+
+    public static void inputMove() {
+        System.out.println(INPUT_MOVE_MESSAGE);
     }
 
 }
