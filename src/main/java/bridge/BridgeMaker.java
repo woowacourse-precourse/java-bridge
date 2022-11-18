afb4896c8771c00641aa982911406048b75c61fb
apptie <appti@naver.com>
2022-11-18 15:27:49 +0900

    feat(BridgeMaker): 외부에서 다리 생성 전략을 받아 다리를 생성하는 기능 추가

--- src/main/java/bridge/BridgeMaker.java
+++ src/main/java/bridge/BridgeMaker.java
@@ -1,6 +1,9 @@
 package bridge;
 
+import bridge.domain.game.BridgeTile;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * 다리의 길이를 입력 받아서 다리를 생성해주는 역할을 한다.
@@ -18,6 +21,19 @@ public class BridgeMaker {
      * @return 입력받은 길이에 해당하는 다리 모양. 위 칸이면 "U", 아래 칸이면 "D"로 표현해야 한다.
      */
     public List<String> makeBridge(int size) {
-        return null;
+        return makeBridgeNumbers(size)
+                .stream()
+                .map(BridgeTile::mapToCommand)
+                .collect(Collectors.toList());
+    }
+
+    private List<Integer> makeBridgeNumbers(int size) {
+        List<Integer> bridge = new ArrayList<>();
+
+        while (bridge.size() < size) {
+            bridge.add(bridgeNumberGenerator.generate());
+        }
+
+        return bridge;
     }
 }
