a006dab21c27c7ec9e70f8fdb3372c8578c1af92
ckn1070 <ckn1070@naver.com>
2022-11-18 20:06:50 +0900

    feat(bridge): implement a feature of reading bridge size

--- src/main/java/bridge/InputView.java
+++ src/main/java/bridge/InputView.java
@@ -1,5 +1,7 @@
 package bridge;
 
+import camp.nextstep.edu.missionutils.Console;
+
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
  */
@@ -9,7 +11,9 @@ public class InputView {
      * 다리의 길이를 입력받는다.
      */
     public int readBridgeSize() {
-        return 0;
+        System.out.println(inputMessage.INPUT_LENGTH.get());
+        int bridgeSize = Integer.parseInt(Console.readLine());
+        return bridgeSize;
     }
 
     /**
@@ -24,5 +28,22 @@ public class InputView {
      */
     public String readGameCommand() {
         return null;
+    }
+}
+
+enum inputMessage {
+
+    INPUT_LENGTH("다리의 길이를 입력해주세요."),
+    INPUT_MOVE("\n이동할 칸을 선택해주세요. (위: U, 아래: D)"),
+    INPUT_RETRY("\n게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)");
+
+    private final String message;
+
+    inputMessage(String message) {
+        this.message = message;
+    }
+
+    public String get() {
+        return message;
     }
 }
