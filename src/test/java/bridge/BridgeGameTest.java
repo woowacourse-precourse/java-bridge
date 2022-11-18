2f33d27ddcc41e214079ff0240ccdb77771c399d
saint6839 <saint6839@gmail.com>
2022-11-18 19:29:04 +0900

    test(domain) : 사용자가 index번째 입력한 이동할 칸 값과 index번째 다리의 정답이 일치할때 참인지 테스트하는 테스트 작성

---
+++ src/test/java/bridge/BridgeGameTest.java
@@ -0,0 +1,27 @@
+package bridge;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class BridgeGameTest {
+
+    @DisplayName("사용자의 index번째에 입력한 이동할 칸 값과 index번째 다리의 정답이 일치할때 참을 반환하는지 테스트")
+    @Test
+    void move() {
+        //given
+        Bridge bridge = new Bridge(List.of("U", "D", "U"));
+        String moving = "U";
+        int index = 2;
+
+        BridgeGame bridgeGame = new BridgeGame();
+        //when
+        boolean isMove = bridgeGame.move(bridge, moving, index);
+        //then
+        assertThat(isMove)
+                .isTrue();
+    }
+}
