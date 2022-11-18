b958db6b3faf668f92bf21934b89f8fa3716361a
wlsh44 <k87913j@gmail.com>
2022-11-18 10:05:23 +0900

    feat: 다리 입력 문구 및 입력 기능 추가

--- src/main/java/bridge/BridgeApplication.java
+++ src/main/java/bridge/BridgeApplication.java
@@ -1,16 +1,24 @@
 package bridge;
 
+import bridge.view.InputView;
 import bridge.view.OutputView;
 
 public class BridgeApplication {
 
     private final OutputView outputView;
+    private final InputView inputView;
 
     public BridgeApplication() {
         this.outputView = new OutputView();
+        this.inputView = new InputView();
     }
 
     public void run() {
-        outputView.printStartMessage();
+        try {
+            outputView.printStartMessage();
+            int bridgeSize = inputView.readBridgeSize();
+        } catch (IllegalArgumentException e) {
+
+        }
     }
 }
--- src/main/java/bridge/view/InputView.java
+++ src/main/java/bridge/view/InputView.java
@@ -1,5 +1,7 @@
 package bridge.view;
 
+import camp.nextstep.edu.missionutils.Console;
+
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
  */
@@ -9,7 +11,15 @@ public class InputView {
      * 다리의 길이를 입력받는다.
      */
     public int readBridgeSize() {
-        return 0;
+        return toInteger(Console.readLine());
+    }
+
+    private Integer toInteger(String input) {
+        try {
+            return Integer.parseInt(input);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("올바른 입력이 아닙니다.", e);
+        }
     }
 
     /**
