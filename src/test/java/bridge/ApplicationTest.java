30577a5d7f46f0998d2fc9e6887d7dc69c0af19f
K-Diger <kitten.diger@gmail.com>
2022-11-18 20:38:18 +0900

    test(ApplicationTest) : RanDomNumberHandler 의 패키지 이동으로 인한 import 구문 변경

--- src/test/java/bridge/ApplicationTest.java
+++ src/test/java/bridge/ApplicationTest.java
@@ -5,8 +5,8 @@ import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.util.Lists.newArrayList;
 
-import bridge.model.service.BridgeMaker;
-import bridge.model.service.BridgeNumberGenerator;
+import bridge.util.numberhandler.RandomNumberHandler;
+import bridge.util.numberhandler.RandomNumberHandlerImpl;
 import camp.nextstep.edu.missionutils.test.NsTest;
 import java.util.List;
 import org.junit.jupiter.api.Test;
@@ -15,10 +15,12 @@ class ApplicationTest extends NsTest {
 
     private static final String ERROR_MESSAGE = "[ERROR]";
 
+    private final RandomNumberHandler randomNumberHandler = new RandomNumberHandlerImpl();
+
     @Test
     void 다리_생성_테스트() {
         BridgeNumberGenerator numberGenerator = new TestNumberGenerator(newArrayList(1, 0, 0));
-        BridgeMaker bridgeMaker = new BridgeMaker(numberGenerator);
+        BridgeMaker bridgeMaker = new BridgeMaker(numberGenerator, randomNumberHandler);
         List<String> bridge = bridgeMaker.makeBridge(3);
         assertThat(bridge).containsExactly("U", "D", "D");
     }
