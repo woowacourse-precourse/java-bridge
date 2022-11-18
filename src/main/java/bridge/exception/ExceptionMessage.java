e1539d491a77bf4192970f93ebe55511247ee3af
GwansuShin <happysun7080@naver.com>
2022-11-18 15:25:46 +0900

    feat(InputValidator): 다리 입력값 검증
    다리의 길이가 정수형인지 검증
    다리의 길이가 3이상 20이하인지 검증

--- src/main/java/bridge/exception/ExceptionMessage.java
+++ src/main/java/bridge/exception/ExceptionMessage.java
@@ -6,7 +6,10 @@ public enum ExceptionMessage implements Message {
 
     NULL("입력값이 null입니다."),
     EMPTY_STRING("입력값이 빈 문자열입니다"),
-    INTEGER_TYPE_MISMATCH("정수형 입력이 아닙니다.");
+    INTEGER_TYPE_MISMATCH("정수형 입력이 아닙니다."),
+    LOWER_BRIDGE_SIZE("다리의 길이 입력값이 3미만입니다."),
+    UPPER_BRIDGE_SIZE("다리의 길이 입력값이 20초과입니다."),
+    ;
 
     private final String message;
 
---
+++ src/main/java/bridge/view/input/InputValidator.java
@@ -0,0 +1,41 @@
+package bridge.view.input;
+
+import bridge.Validator;
+import bridge.exception.ExceptionHandler;
+import bridge.exception.ExceptionMessage;
+
+public class InputValidator extends Validator {
+
+    private static final int BRIDGE_LOWER_INCLUSIVE = 3;
+    private static final int BRIDGE_UPPER_INCLUSIVE = 20;
+
+    public static int validateBridgeSize(int bridgeSize) {
+        validateLowerBridgeSize(bridgeSize);
+        validateUpperBridgeSize(bridgeSize);
+        return bridgeSize;
+    }
+
+    public static String validateMoving(String moving) {
+        //TODO
+        return moving;
+    }
+
+    public static String validateGameCommand(String command) {
+        //TODO
+        return command;
+    }
+
+    private static void validateLowerBridgeSize(int bridgeSize) {
+        if (bridgeSize < BRIDGE_LOWER_INCLUSIVE) {
+            ExceptionHandler.throwException(new IllegalArgumentException(), ExceptionMessage.LOWER_BRIDGE_SIZE);
+        }
+    }
+
+    private static void validateUpperBridgeSize(int bridgeSize) {
+        if (bridgeSize > BRIDGE_UPPER_INCLUSIVE) {
+            ExceptionHandler.throwException(new IllegalArgumentException(), ExceptionMessage.UPPER_BRIDGE_SIZE);
+        }
+    }
+
+
+}
