6bab6c4904b0a60a36b39ce793120c3f38cd651e
whibaek <jibaek0513@gmail.com>
2022-11-19 02:16:10 +0900

    feat(User): 재시작을 위해서 다리의 이전 상태 저장하는 변수 추가

--- src/main/java/bridge/User.java
+++ src/main/java/bridge/User.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 
 public class User {
     private ArrayList<Field> userBridge = new ArrayList<>();
+    private ArrayList<Field> recentBridge = new ArrayList<>();
     private int gameCount = 0;
 
     public int getGameCount() {
@@ -12,5 +13,17 @@ public class User {
 
     public ArrayList<Field> getUserBridge() {
         return userBridge;
+    }
+
+    public ArrayList<Field> getRecentBridge() {
+        return recentBridge;
+    }
+
+    public void refreshBridge(){
+        recentBridge = userBridge;
+    }
+
+    public void increaseGameCount(){
+        gameCount+=1;
     }
 }
