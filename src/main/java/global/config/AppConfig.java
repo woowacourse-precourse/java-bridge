8413c16002ce602d0993d418abc2ed6e11c9a4e3
sangwon <swchoi1997@naver.com>
2022-11-19 00:44:51 +0900

    refactor: BridgeNumberGenerator 의존성 분리

--- src/main/java/global/config/AppConfig.java
+++ src/main/java/global/config/AppConfig.java
@@ -2,10 +2,15 @@ package global.config;
 
 import bridge.BridgeFactory;
 import bridge.BridgeMaker;
+import bridge.BridgeNumberGenerator;
 import bridge.BridgeRandomNumberGenerator;
 
 public class AppConfig {
     public BridgeFactory bridgeFactory (){
-        return new BridgeMaker(new BridgeRandomNumberGenerator());
+        return new BridgeMaker(bridgeNumberGenerator());
+    }
+
+    public BridgeNumberGenerator bridgeNumberGenerator(){
+        return new BridgeRandomNumberGenerator();
     }
 }
