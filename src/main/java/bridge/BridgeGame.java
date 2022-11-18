836fcf90537383bde5e6d728153740d72576b316
MIN-04 <kimji_0424@naver.com>
2022-11-18 15:53:25 +0900

    refactor(BridgeGate.java): refactor method move
    Refactored method move:
    - O, X만 판별하던 move 메서드를 아예 사용자가 처음 시작한 게임의 로직으로 리팩토링

--- src/main/java/bridge/BridgeGame.java
+++ src/main/java/bridge/BridgeGame.java
@@ -1,5 +1,6 @@
 package bridge;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -7,16 +8,35 @@ import java.util.List;
  */
 public class BridgeGame {
 
+    private InputView inputView;
+    private OutputView outputView;
+
+    public BridgeGame(InputView inputView, OutputView outputView) {
+        this.inputView = inputView;
+        this.outputView = outputView;
+    }
+
     /**
      * 사용자가 칸을 이동할 때 사용하는 메서드
      * <p>
      * 이동을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public String move(String movingInput, String bridgeValue) {
-        if (movingInput.equals(bridgeValue)) {
-            return "O";
+    public String move(List<String> bridge) {
+        List<String> result = new ArrayList<>();
+        for (int i = 0; i < bridge.size(); i++) {
+            String movingValue = inputView.readMoving();
+
+            if (movingValue.equals(bridge.get(i))) {
+                result.add("O");
+            } else {
+                result.add("X");
+            }
+            outputView.printMap(result, bridge);
+            if (result.contains("X")) {
+                return "fail";
+            }
         }
-        return "X";
+        return "success";
     }
 
     /**
