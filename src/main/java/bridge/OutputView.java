cd8504adf4967fa444aa348c7636390956f3043a
panorama713 <yaiba713@gmail.com>
2022-11-18 15:04:29 +0900

    feat(bridge): 현재까지 이동한 다리의 정해진 형식에 맞춰 출력하는 기능

--- src/main/java/bridge/OutputView.java
+++ src/main/java/bridge/OutputView.java
@@ -1,5 +1,6 @@
 package bridge;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -7,16 +8,39 @@ import java.util.List;
  */
 public class OutputView {
     private final static String start = "다리 건너기 게임을 시작합니다.";
+
     public void printStart() {
         System.out.println(start);
     }
+
     /**
      * 현재까지 이동한 다리의 상태를 정해진 형식에 맞춰 출력한다.
      * <p>
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
+     * U D U D
+     * U U
+     * U D D
      */
-    public void printMap(List<String> mapValidate) {
+    public void printUpperMap(List<String> bridge, List<String> moveMapList) {
+        System.out.print("[ ");
+        for(int i=0; i<moveMapList.size(); i++){
+            if (i > 0) System.out.print(" | ");
+            if (bridge.get(i).equals("U") && moveMapList.get(i).equals("U")) System.out.print("O");
+            if (bridge.get(i).equals("U") && moveMapList.get(i).equals("D")) System.out.print(" ");
+            if (bridge.get(i).equals("D") && moveMapList.get(i).equals("U")) System.out.print("X");
+        }
+        System.out.println(" ]");
+    }
 
+    public void printLowerMap(List<String> bridge, List<String> moveMapList) {
+        System.out.print("[ ");
+        for(int i=0; i<moveMapList.size(); i++){
+            if (i > 0) System.out.print(" | ");
+            if (bridge.get(i).equals("D") && moveMapList.get(i).equals("D")) System.out.print("O");
+            if (bridge.get(i).equals("D") && moveMapList.get(i).equals("U")) System.out.print(" ");
+            if (bridge.get(i).equals("U") && moveMapList.get(i).equals("D")) System.out.print("X");
+        }
+        System.out.println(" ]");
     }
 
     /**
