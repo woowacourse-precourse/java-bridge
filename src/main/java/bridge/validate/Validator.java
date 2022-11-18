8cc3df8d88cdb2bacd3c94b7f35d2c3f49e2655c
splitube <voicesplit@daum.net>
2022-11-18 15:11:07 +0900

    feat(Validator): 게임 재시도 여부 검증 기능 구현

--- src/main/java/bridge/validate/Validator.java
+++ src/main/java/bridge/validate/Validator.java
@@ -7,6 +7,8 @@ public class Validator {
     private static final int MAX_BRIDGE_LENGTH = 20;
     private static final String MOVE_UPPER_BRIDGE = "U";
     private static final String MOVE_LOWER_BRIDGE = "D";
+    private static final String RETRY = "R";
+    private static final String QUIT = "Q";
 
     public static void isNum(String input) {
         input.chars().forEach(c -> {
@@ -34,6 +36,14 @@ public class Validator {
         final List<String> properMoves = List.of(MOVE_UPPER_BRIDGE, MOVE_LOWER_BRIDGE);
         if (!properMoves.contains(move)) {
             String errorMessage = "" + ErrorMessage.ERROR + ErrorMessage.NOT_PROPER_MOVE;
+            throw new IllegalArgumentException(errorMessage);
+        }
+    }
+
+    public static void isProperGameCommand(String gameCommand) {
+        final List<String> properMoves = List.of(RETRY, QUIT);
+        if (!properMoves.contains(gameCommand)) {
+            String errorMessage = "" + ErrorMessage.ERROR + ErrorMessage.NOT_PROPER_GAME_COMMAND;
             throw new IllegalArgumentException(errorMessage);
         }
     }
