efe51570c8e041e35ef2c253576aac929ca88d5c
은지 <midzy0228@gmail.com>
2022-11-18 14:05:01 +0900

    feat: 게임 흐름 구현

--- src/main/java/bridge/GameController.java
+++ src/main/java/bridge/GameController.java
@@ -3,24 +3,42 @@ package bridge;
 import java.util.List;
 
 public class GameController {
+    private static final String RETRY_COMMAND = "R";
+    private static final String POSSIBLE_MOVE = "O";
     private final InputView inputView = new InputView();
     private final OutputView outputView = new OutputView();
 
     public void play() {
         outputView.printGameStartMessage();
+        Integer size = inputView.readBridgeSize();
         BridgeNumberGenerator bridgeNumberGenerator = new BridgeRandomNumberGenerator();
         BridgeMaker bridgeMaker = new BridgeMaker(bridgeNumberGenerator);
-        Integer size = inputView.readBridgeSize();
         List<String> bridge = bridgeMaker.makeBridge(size);
-
         BridgeGame bridgeGame = new BridgeGame();
+        walk(bridgeGame, bridge);
+    }
 
-        for (int turn = 0; turn < size; turn++) {
-            String moving = inputView.readMoving();
-            String movePossible = bridgeGame.move(moving, bridge.get(turn));
-            if (movePossible.equals("O")) {
+    private void walk(BridgeGame bridgeGame, List<String> bridge) {
+        for (String bridgeStatus : bridge) {
+            if (isMovePossible(bridgeGame, bridgeStatus)) {
+                outputView.printMap();
                 continue;
             }
+            String command = inputView.readGameCommand();
+            if (isRetry(command)) {
+                bridgeGame.retry();
+            }
+            break;
         }
+    }
+
+    private boolean isMovePossible(BridgeGame bridgeGame, String bridgeStatus) {
+        String moving = inputView.readMoving();
+        String movePossible =  bridgeGame.move(moving, bridgeStatus);
+        return movePossible.equals(POSSIBLE_MOVE);
+    }
+
+    private boolean isRetry(String command) {
+        return command.equals(RETRY_COMMAND);
     }
 }
