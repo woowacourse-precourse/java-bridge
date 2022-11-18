ee2eec275a7c03fa92ba942425e184865480317f
JaeWon Choi <87649850+codesver@users.noreply.github.com>
2022-11-18 16:57:18 +0900

    feat(Movement): rename and change type of type

--- src/main/java/bridge/constant/Movement.java
+++ src/main/java/bridge/constant/Movement.java
@@ -1,28 +1,28 @@
 package bridge.constant;
 
 public enum Movement {
-    UP_AND_O("U", "O"),
-    UP_AND_X("U", "X"),
-    DOWN_AND_O("D", "O"),
-    DOWN_AND_X("D", "X");
+    UP_AND_O("U", 'O'),
+    UP_AND_X("U", 'X'),
+    DOWN_AND_O("D", 'O'),
+    DOWN_AND_X("D", 'X');
 
     private final String direction;
-    private final String type;
+    private final char moveable;
 
-    Movement(String direction, String type) {
+    Movement(String direction, char moveable) {
         this.direction = direction;
-        this.type = type;
+        this.moveable = moveable;
     }
 
     public String direction() {
         return direction;
     }
 
-    public String type() {
-        return type;
+    public char moveable() {
+        return moveable;
     }
 
     public boolean isAvailable() {
-        return type.equals("O");
+        return moveable == 'O';
     }
 }
