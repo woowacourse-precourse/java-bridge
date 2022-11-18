55b4495977771aa677fa4a096dbd70b7d8bd5549
Jzakka <mouse4786@gmail.com>
2022-11-19 00:43:11 +0900

    feat: 입력 기능 구현

--- src/main/java/bridge/view/InputView.java
+++ src/main/java/bridge/view/InputView.java
@@ -1,5 +1,12 @@
 package bridge.view;
 
+import bridge.Exception.CommandException;
+import bridge.Exception.MoveException;
+import bridge.Exception.Validation;
+import camp.nextstep.edu.missionutils.Console;
+
+import java.util.function.Function;
+
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
  */
@@ -9,20 +16,34 @@ public class InputView {
      * 다리의 길이를 입력받는다.
      */
     public int readBridgeSize() {
-        return 0;
+        String input = Console.readLine();
+
+        return (Integer) validateInput(Integer::parseInt, input, "R와 Q 중 하나만 입력하세요");
     }
 
     /**
      * 사용자가 이동할 칸을 입력받는다.
      */
     public String readMoving() {
-        return null;
+        String input = Console.readLine();
+
+        return (String) validateInput(Validation::validateCommand, input, "U와 D 중 하나만 입력하세요");
     }
 
     /**
      * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
      */
     public String readGameCommand() {
-        return null;
+        String input = Console.readLine();
+
+        return (String) validateInput(Validation::validateCommand, input, "R와 Q 중 하나만 입력하세요");
+    }
+
+    private Object validateInput(Function<String, Object> callback, String input, String message){
+        try {
+            return callback.apply(input);
+        } catch (NumberFormatException | CommandException | MoveException e) {
+            throw new IllegalArgumentException(message);
+        }
     }
 }
