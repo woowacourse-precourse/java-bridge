9121289d49a91fcc313aaf97e184f869652a465a
김선동 <mok03127@gmail.com>
2022-11-18 16:16:12 +0900

    feat: 다리의 길이를 입력받는 기능 구현

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -4,6 +4,10 @@ public class Application {
 
     public static void main(String[] args) {
         OutputView outputView = new OutputView();
+        InputView inputView = new InputView();
+
         outputView.printGameStartMessage();
+        int bridgeSize = inputView.readBridgeSize();
+        System.out.println("bridgeSize = " + bridgeSize);
     }
 }
