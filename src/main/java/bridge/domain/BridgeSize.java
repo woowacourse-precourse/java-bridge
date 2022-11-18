013ad9c9635c74d20916ce7f2450ec6bcefc5721
semi-cloud <sksmsrkdtpal@naver.com>
2022-11-18 14:57:39 +0900

    feat(BridgeSize): 다리 길이 범위에 대한 예외 처리 구현
    3 이상 20 이하 범위만 가능

--- src/main/java/bridge/domain/BridgeSize.java
+++ src/main/java/bridge/domain/BridgeSize.java
@@ -1,9 +1,21 @@
 package bridge.domain;
 
 public class BridgeSize {
+
+    private static final int MIN_BRIDGE_SIZE = 3;
+    private static final int MAX_BRIDGE_SIZE = 20;
+
     private final int size;
 
     public BridgeSize(int size) {
+        validateSize(size);
         this.size = size;
     }
+
+    private void validateSize(int size) {
+        if (size < MIN_BRIDGE_SIZE || size > MAX_BRIDGE_SIZE) {
+            throw new IllegalArgumentException("[ERROR] 다리 길이는 3부터 20 사이의 숫자여야 합니다.");
+        }
+    }
 }
+
