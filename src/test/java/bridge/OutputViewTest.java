30cdc68b49e339977718b340b97021f70e75b465
MuSeung <peter0757@nvaer.com>
2022-11-18 22:13:32 +0900

    test: OutputView가 성공한 다리를 출력하는 기능 테스트 구현
    bridgeUDDSuccess
        - UDD인 다리가 성공한 경우를 알맞게 출력하는지 확인

---
+++ src/test/java/bridge/OutputViewTest.java
@@ -0,0 +1,28 @@
+package bridge;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.io.ByteArrayOutputStream;
+import java.io.OutputStream;
+import java.io.PrintStream;
+import java.util.List;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class OutputViewTest {
+    OutputView outputView = new OutputView();
+
+    @DisplayName("UDD로 이루어진 다리를 성공적으로 통과한 경우")
+    @Test
+    void bridgeUDDSuccess() {
+        OutputStream out = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(out));
+
+        List<String> answerBridge = List.of("U", "D", "D");
+        List<String> bridgeTrack = List.of("U", "D", "D");
+
+        outputView.printMap(bridgeTrack, answerBridge);
+        assertThat(out.toString()).isEqualTo("[ O |   |   ]\n[   | O | O ]");
+    }
+
+}
