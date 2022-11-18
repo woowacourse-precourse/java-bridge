f8daaa278cce0f8eaf1b6da0f2ab60e895034e67
gnoesnooj <306yyy@naver.com>
2022-11-18 21:53:37 +0800

    feat(InputView) : 다리 사이즈를 입력받는 메소드 작성

--- src/main/java/bridge/view/InputView.java
+++ src/main/java/bridge/view/InputView.java
@@ -1,5 +1,11 @@
 package bridge.view;
 
+import static bridge.rule.BridgeSizeRange.isBridgeSizeOutOfRange;
+
+import bridge.exception.InputNotNumberException;
+import bridge.exception.NumberOutOfRangeException;
+import camp.nextstep.edu.missionutils.Console;
+
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
  */
@@ -9,7 +15,16 @@ public class InputView {
      * 다리의 길이를 입력받는다.
      */
     public int readBridgeSize() {
-        return 0;
+        try {
+            String input = Console.readLine();
+            int number = Integer.parseInt(input);
+
+            validateBridgeSize(number);
+
+            return number;
+        } catch (NumberFormatException e){
+            throw new InputNotNumberException();
+        }
     }
 
     /**
@@ -24,5 +39,15 @@ public class InputView {
      */
     public String readGameCommand() {
         return null;
+    }
+
+    private void validateBridgeSize(int number){
+        validateSizeOutOfRange(number);
+    }
+
+    private void validateSizeOutOfRange(int number){
+        if(isBridgeSizeOutOfRange(number)){
+            throw new NumberOutOfRangeException();
+        }
     }
 }
