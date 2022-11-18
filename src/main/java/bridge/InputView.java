013b9024d3e42c9e1cdf3696b688739f4d5d9c90
김선동 <mok03127@gmail.com>
2022-11-18 16:51:53 +0900

    feat: 플레이어로부터 이동할 칸을 입력받는 기능 구현

--- src/main/java/bridge/InputView.java
+++ src/main/java/bridge/InputView.java
@@ -7,6 +7,7 @@ import camp.nextstep.edu.missionutils.Console;
  */
 public class InputView {
     private static final String INPUT_BRIDGE_SIZE = "다리의 길이를 입력해주세요.";
+    private static final String INPUT_MOVE = "이동할 칸을 선택해주세요. (위: U, 아래: D)";
     private static final String TYPE_ERROR = "[ERROR] 숫자만 입력 가능합니다.";
     /**
      * 다리의 길이를 입력받는다.
@@ -20,7 +21,8 @@ public class InputView {
      * 사용자가 이동할 칸을 입력받는다.
      */
     public String readMoving() {
-        return null;
+        System.out.println(INPUT_MOVE);
+        return Console.readLine();
     }
 
     /**
