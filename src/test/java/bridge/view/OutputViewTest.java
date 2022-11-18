d098d6e7f6919a4ce127cad6f043c1e40987c7e3
PgmJun <84304802+PgmJun@users.noreply.github.com>
2022-11-18 20:14:09 +0900

    test : 이동상태 출력 테스트 구현

---
+++ src/test/java/bridge/view/OutputViewTest.java
@@ -0,0 +1,29 @@
+package bridge.view;
+
+import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import java.util.ArrayList;
+import java.util.List;
+
+import static bridge.constant.Constants.BridgeSign.*;
+import static bridge.view.OutputView.*;
+import static org.assertj.core.api.Assertions.assertThat;
+
+class OutputViewTest {
+
+    @Test
+    void printMapTest() {
+        List<String> topGameBoard = new ArrayList<>(List.of(PASS,EMPTY,PASS));
+        List<String> bottomGameBoard = new ArrayList<>(List.of(EMPTY,PASS,EMPTY));
+        String newLine = System.lineSeparator();
+
+        ByteArrayOutputStream output = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(output));
+        printMap(topGameBoard, bottomGameBoard);
+        String expectString = "[ O |   | O ]"+newLine+"[   | O |   ]"+newLine;
+
+        assertThat(output.toString()).contains(expectString);
+    }
+}
