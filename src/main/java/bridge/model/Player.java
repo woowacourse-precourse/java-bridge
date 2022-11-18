6966abc96e4760622cbbb89769b01d57dc205c0b
dooboocookie <bkh3030@gmail.com>
2022-11-18 15:29:29 +0900

    feat: Player 초기 위치와 한칸씩 전진하는 기능 추가

--- src/main/java/bridge/model/Player.java
+++ src/main/java/bridge/model/Player.java
@@ -4,14 +4,15 @@ public class Player {
     private int position;
 
     public Player() {
-
+        initPosition();
     }
 
     public void initPosition() {
-
+        this.position = -1;
     }
 
-    public void nextStep() {
-
+    public int nextStep() {
+        this.position = this.position + 1;
+        return this.position;
     }
 }
