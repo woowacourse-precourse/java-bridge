b7d6ceac9b0eb2214f35af78199efc45e048a84c
Ye Seok Hong <yshong1998@gmail.com>
2022-11-19 03:02:09 +0900

    Feat : 사용자가 칸을 이동하는 기능 추가

--- src/main/java/bridge/BridgeGame.java
+++ src/main/java/bridge/BridgeGame.java
@@ -4,13 +4,22 @@ package bridge;
  * 다리 건너기 게임을 관리하는 클래스
  */
 public class BridgeGame {
+    private int placeNumber = 0;
+    private final int gameRepeatCount = 0;
+    InputView inputView = new InputView();
 
     /**
      * 사용자가 칸을 이동할 때 사용하는 메서드
      * <p>
      * 이동을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void move() {
+
+    public void move(String eachBridge, String playerMoving) {
+        if(eachBridge.equals(playerMoving)){
+            placeNumber++;
+        } else if(!eachBridge.equals(playerMoving)){
+            placeNumber = 0;
+        }
     }
 
     /**
@@ -19,5 +28,8 @@ public class BridgeGame {
      * 재시작을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
     public void retry() {
+
     }
+
+
 }
