89e6884b52b28bb6111e98ae72289035fde20ca1
JJ <jeongjae1996@naver.com>
2022-11-19 03:14:56 +0900

    refactor: Gameplay 객체를 생성하며 게임이 시작되도록 구현

--- src/main/java/bridge/GamePlay.java
+++ src/main/java/bridge/GamePlay.java
@@ -4,19 +4,23 @@ import java.util.List;
 
 public class GamePlay {
     private final InputView inputView;
-    private final BridgeMaker bridgeMaker;
+    private final List<String> bridge;
+    // private final BridgeMaker bridgeMaker;
 
     public GamePlay() {
         this.inputView = new InputView();
-        this.bridgeMaker = new BridgeMaker(new BridgeRandomNumberGenerator());
+        this.bridge = start();
+
     }
 
     private List<String> start() {
         final String EVENT_MESSAGE = "다리 건너기 게임을 시작합니다.\n\n다리의 길이를 입력해주세요.";
 
+        BridgeMaker bridgeMaker = new BridgeMaker(new BridgeRandomNumberGenerator());
+
         System.out.println(EVENT_MESSAGE);
         int bridgeSize = this.inputView.readBridgeSize();
-        List<String> bridge = this.bridgeMaker.makeBridge(bridgeSize);
+        List<String> bridge = bridgeMaker.makeBridge(bridgeSize);
 
         return bridge;
     }
