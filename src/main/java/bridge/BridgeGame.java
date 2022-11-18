130b00f5a870bb803f922a870f94ad6047ac5f3f
kissshot1104 <kissshot.heartunderblade1104@gmail.com>
2022-11-18 17:32:55 +0900

    Feat(BridgeGame) : 사용자가 칸을 이동하는 기능 추가
    public String move()
    메소드를 추가했습니다.
    이 메소드는 사용자가 칸을 이동할 떄 사용하는 메서드입니다.

--- src/main/java/bridge/BridgeGame.java
+++ src/main/java/bridge/BridgeGame.java
@@ -5,12 +5,19 @@ package bridge;
  */
 public class BridgeGame {
 
+    InputView inputView = new InputView();
     /**
      * 사용자가 칸을 이동할 때 사용하는 메서드
      * <p>
      * 이동을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void move() {
+    public String move() {
+        while (true){
+            System.out.println("이동할 칸을 선택해주세요. (위: U, 아래: D)");
+            String tmp = inputView.readMoving();
+            if (tmp.length() != 0)
+                return tmp;
+        }
     }
 
     /**
