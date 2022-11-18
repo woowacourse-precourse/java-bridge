b26541a0f89932210a8e33d57027820c2d1ff3bc
김태용 <amico741@naver.com>
2022-11-18 13:32:19 +0900

    feat: Bridge 클래스 생성

---
+++ src/main/java/bridge/domain/Bridge.java
@@ -0,0 +1,12 @@
+package bridge.domain;
+
+
+import java.util.List;
+
+public class Bridge {
+    private final List<String> bridges;
+
+    public Bridge(List<String> bridges) {
+        this.bridges = bridges;
+    }
+}
