f0351f1398316d07e97647dfa247da9a370ab496
JaeHong-Ahn <63873742+JaeHong-Ahn@users.noreply.github.com>
2022-11-18 16:57:04 +0900

    <feat> 다리 길이를 입력받음
    Console.readline으로 String 형태로 입력을 받아 int 형태로 반환

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -3,6 +3,12 @@ package bridge;
 public class Application {
 
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+
+        System.out.println("다리 건너기 게임을 시작합니다.");
+        System.out.println();
+        InputView makeBridge = new InputView();
+        int bridgeLength = makeBridge.readBridgeSize(); // 다리의 길이
+        System.out.println(bridgeLength);
+
     }
 }
