af57942fcafee249f8d46c783c32cb398ca9ffe3
saint6839 <saint6839@gmail.com>
2022-11-18 20:56:23 +0900

    test(domain) : 중복코드 @BeforeEach로 분리, BridgeGame 필드 추가에 따른 조건 변경

--- src/test/java/bridge/BridgeGameTest.java
+++ src/test/java/bridge/BridgeGameTest.java
@@ -1,5 +1,6 @@
 package bridge;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -9,17 +10,22 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 class BridgeGameTest {
 
+    Bridge bridge;
+    @BeforeEach
+    void setUp() {
+        bridge = new Bridge(List.of("U", "D", "U"));
+    }
+
     @DisplayName("사용자의 index번째에 입력한 이동할 칸 값과 index번째 다리의 정답이 일치할때 참을 반환하는지 테스트")
     @Test
     void moveTrue() {
         //given
-        Bridge bridge = new Bridge(List.of("U", "D", "U"));
         String moving = "U";
         int index = 2;
 
-        BridgeGame bridgeGame = new BridgeGame();
+        BridgeGame bridgeGame = new BridgeGame(bridge);
         //when
-        boolean isMove = bridgeGame.move(bridge, moving, index);
+        boolean isMove = bridgeGame.move(moving, index);
         //then
         assertThat(isMove)
                 .isTrue();
@@ -29,13 +35,12 @@ class BridgeGameTest {
     @Test
     void moveFalse() {
         //given
-        Bridge bridge = new Bridge(List.of("U", "D", "U"));
         String moving = "D";
         int index = 2;
 
-        BridgeGame bridgeGame = new BridgeGame();
+        BridgeGame bridgeGame = new BridgeGame(bridge);
         //when
-        boolean isMove = bridgeGame.move(bridge, moving, index);
+        boolean isMove = bridgeGame.move(moving, index);
         //then
         assertThat(isMove)
                 .isFalse();
