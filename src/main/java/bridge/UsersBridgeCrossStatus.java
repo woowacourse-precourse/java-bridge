5b647adf45caf52269d01bd7df4f4272022398d6
making-a-scene <4936joo@naver.com>
2022-11-18 18:34:59 +0900

    fix: 다리 건너기 결과 추가 저장 위치(인덱스) 오류 수정

--- src/main/java/bridge/UsersBridgeCrossStatus.java
+++ src/main/java/bridge/UsersBridgeCrossStatus.java
@@ -1,10 +1,14 @@
 package bridge;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class UsersBridgeCrossStatus {
     private final List<List<String>> currentBridge;
     private final int length;
+    private static final int FIRST_BRIDGE = 1;
+    private static final int INITIAL_BRIDGE_SIZE = 2;
     private static final String BLANK = " ";
     private static final String DELIMITER = "|";
 
@@ -17,29 +21,38 @@ public class UsersBridgeCrossStatus {
         if (currBridgeOrder != FIRST_BRIDGE) {
             int spaceIdx = selectedSpace.getIndex();
             addDelimiter(currentBridge.get(spaceIdx).size());
-        }
+        } // 현재 이동하고 있는 다리가 첫 번째 다리가 아니라면 앞에 구분자 "|"를 추가해서 이전의 이동 결과와 구분함.
+        List<String> toBeAddedInSelectedSpace = getCharsNeededToAdd(true, movingResult);
+        List<String> toBeAddedInOtherSpace = getCharsNeededToAdd(false, null);
 
-        addResultsForEachSpace(true, selectedSpace, movingResult);
-        Space otherSpace = Space.getOtherSpace(selectedSpace);
-        addResultsForEachSpace(false, otherSpace, movingResult);
+        updateCurrentBridge(selectedSpace.getIndex(), toBeAddedInSelectedSpace, toBeAddedInOtherSpace);
     }
 
-
-    private void addResultsForEachSpace(boolean isSelected, Space objectSpace, String movingResult) {
-        int objectSpaceIndex = objectSpace.getIndex();
-        int objectSpaceSize = currentBridge.get(objectSpaceIndex).size();
-
-        // objectSpaceSize - 2 : 다리의 끝인 "]"가 저장된 인덱스의 직전 인덱스 값.
+    private List<String> getCharsNeededToAdd(boolean isSelected, String movingResult) {
+        List<String> charsNeededToAdd = new ArrayList<>();
         if (isSelected) {
-            currentBridge.get(objectSpaceIndex).addAll(objectSpaceSize - 2, List.of(BLANK, movingResult, BLANK));
-            return;
+            charsNeededToAdd.addAll(new ArrayList<>(Arrays.asList(BLANK, movingResult, BLANK)));
+            return charsNeededToAdd;
         }
-        currentBridge.get(objectSpaceSize).addAll(objectSpaceSize - 2, List.of(BLANK, BLANK, BLANK));
+        charsNeededToAdd.addAll(new ArrayList<>(Arrays.asList(BLANK, BLANK, BLANK)));
+        return charsNeededToAdd;
     }
 
     private void addDelimiter(int size) {
-        currentBridge.get(Space.UP.getIndex()).add(size - 2, DELIMITER);
-        currentBridge.get(Space.DOWN.getIndex()).add(size - 2, DELIMITER);
+        currentBridge.get(Space.UP.getIndex()).add(size - 1, DELIMITER);
+        currentBridge.get(Space.DOWN.getIndex()).add(size - 1, DELIMITER);
+    }
+
+    private void updateCurrentBridge(int selectedIdx, List<String> forSelected, List<String> forNotSelected) {
+        int currBridgeSize = currentBridge.get(selectedIdx).size();
+        int addPoint = -1;
+        if (currBridgeSize == INITIAL_BRIDGE_SIZE) {
+            addPoint = 1;
+        } else if (currBridgeSize > INITIAL_BRIDGE_SIZE) {
+            addPoint = currBridgeSize - 1;
+        }
+        currentBridge.get(selectedIdx).addAll(addPoint, forSelected);
+        currentBridge.get(Space.getOtherSpaceIndex(selectedIdx)).addAll(addPoint, forNotSelected);
     }
 
     public List<List<String>> getCurrentBridge() {
