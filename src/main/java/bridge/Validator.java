58381dcea493bc153db3608abac352106abd5578
Amyhds <dasomhan524@gmail.com>
2022-11-18 12:36:36 +0900

    feat(validator): 검증 클래스 생성 다리 길이 숫자 검증 메서드 생성 다리 길이 범위 검증 메서드 생성 이동 칸 입력 검증 메서드 생성 게임 시도 및 종료 여부 입력 검증 메서드 생성

---
+++ src/main/java/bridge/Validator.java
@@ -0,0 +1,19 @@
+package bridge;
+
+public class Validator {
+    public void validateNumber(String bridgeNumber) {
+
+    }
+
+    public void validateRange(int bridgeNumber) {
+
+    }
+
+    public void validateMoving(String moving) {
+
+    }
+
+    public void validateGameCommand(String gameCommand) {
+
+    }
+}
