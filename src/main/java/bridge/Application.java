13f1e556353dcc236beb7dd343ebae5b223a9980
kyliecamila <kyliecamila@naver.com>
2022-11-18 16:13:33 +0900

    feat: 다리 길이 저장

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -4,9 +4,11 @@ public class Application {
 
     public static void main(String[] args) {
         PrintCommand printCommand= new PrintCommand();
-        printCommand.gameStart();
         BridgeGame game = new BridgeGame();
         InputView input = new InputView();
+
+        printCommand.gameStart();
+        int bridgeLength=input.readBridgeSize();
 
 
     }
