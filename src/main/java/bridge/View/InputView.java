33fda8f51306be544c98f24c7d289ea6779f481c
Kwon-sang <kwonsg514@gmail.com>
2022-11-18 13:04:03 +0900

    [InputView class 구현 완료] 추후 추가사항 : 에러메시지관리

--- src/main/java/bridge/View/InputView.java
+++ src/main/java/bridge/View/InputView.java
@@ -1,12 +1,6 @@
-
 package bridge.View;
 
 import camp.nextstep.edu.missionutils.*;
-/*
-* InputView 패키지는 변경할 수 있다.
-* InputView 메서드의 시그니처(인자, 이름)와 반환 타입은 변경할 수 있다.
-* 사용자 값 입력을 위해 필요한 메서드를 추가할 수 있다.
-*/
 
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
@@ -17,47 +11,61 @@ public class InputView {
      * 다리의 길이를 입력받는다.
      */
     public int readBridgeSize() {
-        System.out.println(Messeges.INPUT_BRIDGE_LENGTH.msg);
-        String bridgeSizeCommand = readGameCommand();
+        System.out.println(MSG.INPUT_BRIDGE_SIZE.message);
+        String bridgeSizeCommand = Console.readLine();
         try {
             validateBridgeSize(bridgeSizeCommand);
         }catch (IllegalArgumentException e) {
+            //에러메시지 출력부
             readBridgeSize();
         }
         return Integer.parseInt(bridgeSizeCommand);
+    }
+    private void validateBridgeSize(String bridgeSizeCommand) {
+        String regex = "^[3-9]$|^[1][0-9]$|^20$";
+        if (!bridgeSizeCommand.matches(regex)) {
+            throw new IllegalArgumentException();
+        }
     }
 
     /**
      * 사용자가 이동할 칸을 입력받는다.
      */
     public String readMoving() {
-        System.out.println(Messeges.INPUT_SELECT_MOVING_POSITION.msg);
-        String movingCommand = readGameCommand();
+        System.out.println(MSG.INPUT_MOVING.message);
+        String movingCommand = Console.readLine();
         try {
             validateMoving(movingCommand);
         }catch (IllegalArgumentException e) {
+            //에러메시지 출력부
             readMoving();
         }
         return movingCommand;
+    }
+    private void validateMoving(String movingCommand) {
+        String regex = "[UD]";
+        if (!movingCommand.matches(regex)) {
+            throw new IllegalArgumentException();
+        }
     }
 
     /**
      * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
      */
     public String readGameCommand() {
-        return Console.readLine();
-    }
-
-    private void validateBridgeSize(String bridgeSizeCommand) {
-        String regex = "^[3-9]$|^[1][0-9]$|^[2][0]$";
-        if (!bridgeSizeCommand.matches(regex)) {
-            throw new IllegalArgumentException();
+        System.out.println(MSG.INPUT_GAME.message);
+        String gameCommand = Console.readLine();
+        try {
+            validateGame(gameCommand);
+        }catch (IllegalArgumentException e) {
+            //에러메시지 출력부
+            readGameCommand();
         }
+        return gameCommand;
     }
-
-    private void validateMoving(String movingCommand) {
-        String regex = "[UD]";
-        if (!movingCommand.matches(regex)) {
+    private void validateGame(String gameCommand) {
+        String regex = "[RQ]";
+        if (!gameCommand.matches(regex)) {
             throw new IllegalArgumentException();
         }
     }
---
+++ src/main/java/bridge/View/MSG.java
@@ -0,0 +1,19 @@
+package bridge.View;
+
+public enum MSG {
+
+    INPUT_START("다리 건너기 게임을 시작합니다."),
+    INPUT_BRIDGE_SIZE("다리의 길이를 입력해주세요."),
+    INPUT_MOVING("이동할 칸을 선택해주세요. (위: U, 아래: D)"),
+    INPUT_GAME("게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)"),
+
+    OUTPUT_GAME_RESULT("최종 게임 결과"),
+    OUTPUT_PASS_FAIL("게임 성공 여부: "),
+    OUTPUT_TOTAL_TRY_COUNT("총 시도한 횟수: ");
+
+    final String message;
+
+    MSG(String msg) {
+        this.message = msg;
+    }
+}
