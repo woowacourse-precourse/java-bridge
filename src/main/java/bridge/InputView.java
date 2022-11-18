9eccd5b3aec27aab90aab33ec67cb130730775f1
qdeu22 <qdeu22@gmail.com>
2022-11-18 11:48:40 +0900

    feat(bridge): 숫자가 아니면 예외처리하는 기능 구현

--- src/main/java/bridge/InputView.java
+++ src/main/java/bridge/InputView.java
@@ -1,5 +1,7 @@
 package bridge;
 
+import camp.nextstep.edu.missionutils.Console;
+
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
  */
@@ -9,7 +11,19 @@ public class InputView {
      * 다리의 길이를 입력받는다.
      */
     public int readBridgeSize() {
-        return 0;
+        System.out.println("다리의 길이를 입력해주세요.");
+        return numberValidate(Console.readLine());
+    }
+
+    //숫자 검증
+    public int numberValidate(String input) {
+        try {
+            return (Integer.parseInt(input));
+        } catch (NumberFormatException e) {
+            System.out.println(e.getMessage());
+            System.out.println("[ERROR] 숫자가 아닙니다.");
+            return readBridgeSize();
+        }
     }
 
     /**
