e18e8a0d0332773a9cc88bad5378d6821276ac4c
jjmin9797 <jjmin9797@naver.com>
2022-11-18 21:04:32 +0900

    test(BridgeSizeException): 다리의 크기가 3에서 20사이의 숫자가 아니면 예외 발생 테스트

--- src/test/java/bridge/service/exception/BridgeSizeExceptionTest.java
+++ src/test/java/bridge/service/exception/BridgeSizeExceptionTest.java
@@ -2,6 +2,8 @@ package bridge.service.exception;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
@@ -14,6 +16,14 @@ class BridgeSizeExceptionTest {
     void checkConvertToNumberTest() {
         String bridgeSize = "이십줄";
         assertThatThrownBy(() -> BridgeSizeException.checkConvertToNumber(bridgeSize))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("다리의 크기가 3에서 20사이의 숫자가 아니면 예외가 발생한다.")
+    @ValueSource(strings = {"0","1","2","21","100"})
+    @ParameterizedTest
+    void checkBridgeSizeIsCorrectRangeTest(String size) {
+        assertThatThrownBy(() -> BridgeSizeException.checkBridgeSizeIsCorrectRange(size))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 }
