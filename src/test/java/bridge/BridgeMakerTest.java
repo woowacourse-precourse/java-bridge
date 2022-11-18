3c07cdf93b552808a5f6ad5f02832ba51c4ea26a
Sinryuji <siler2474@gmail.com>
2022-11-18 17:59:41 +0900

    style(BirdgeMakerTest): 상수 이름 컨벤션에 맞춰 수정

--- src/test/java/bridge/BridgeMakerTest.java
+++ src/test/java/bridge/BridgeMakerTest.java
@@ -8,13 +8,13 @@ import org.junit.jupiter.api.Test;
 
 public class BridgeMakerTest {
     private final BridgeMaker bridgeMaker = new BridgeMaker(new BridgeRandomNumberGenerator());
-    private static final int bridgeSize = 10;
-    private final List<String> bridge = bridgeMaker.makeBridge(bridgeSize);
+    private static final int BRIDGE_SIZE = 10;
+    private final List<String> bridge = bridgeMaker.makeBridge(BRIDGE_SIZE);
 
     @DisplayName("다리의 길이가 인자로 주어진 값 만큼 만들어지는지 테스트")
     @Test
     void makeBridgeSizeTest() {
-        assertThat(bridge.size()).isEqualTo(bridgeSize);
+        assertThat(bridge.size()).isEqualTo(BRIDGE_SIZE);
     }
 
     @DisplayName("만들어진 다리가 U와 D만 포함하고 있는지 테스트")
