a38f175ef53a8146d4b3623173a89b780e914e19
hoonn9 <hun9935@gmail.com>
2022-11-18 17:07:59 +0900

    test(BridgeGameCommand): findByCommand 메소드 테스트 추가

--- src/test/java/bridge/game/BridgeGameCommandTest.java
+++ src/test/java/bridge/game/BridgeGameCommandTest.java
@@ -3,6 +3,7 @@ package bridge.game;
 import static org.assertj.core.api.Assertions.*;
 
 import bridge.domain.game.BridgeGameCommand;
+import java.util.Optional;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
@@ -11,7 +12,7 @@ import org.junit.jupiter.params.provider.EnumSource;
 public class BridgeGameCommandTest {
     
     @DisplayName("해당 인수가 command에 포함되면 true를 반환한다.")
-    @EnumSource
+    @EnumSource(BridgeGameCommand.class)
     @ParameterizedTest
     void isContainByValid(BridgeGameCommand command) {
         assertThat(BridgeGameCommand.isContain(command.getCommand())).isEqualTo(true);
@@ -21,5 +22,18 @@ public class BridgeGameCommandTest {
     @Test
     void isContainByInvalid() {
         assertThat(BridgeGameCommand.isContain("A")).isEqualTo(false);
+    }
+    
+    @DisplayName("해당 인수가 command에 맞는 BridgeGameCommand Enum을 반환한다.")
+    @EnumSource(BridgeGameCommand.class)
+    @ParameterizedTest
+    void findByCommandByValid(BridgeGameCommand command) {
+        assertThat(BridgeGameCommand.findByCommand(command.getCommand())).isEqualTo(Optional.of(command));
+    }
+    
+    @DisplayName("해당 인수가 command에 맞는 BridgeGameCommand Enum이 없을 때 Empty를 반환한다.")
+    @Test
+    void findByCommandByInvalid() {
+        assertThat(BridgeGameCommand.findByCommand("A")).isEqualTo(Optional.empty());
     }
 }
