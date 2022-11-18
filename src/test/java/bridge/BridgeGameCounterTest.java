2fbc8732dd3096260693987da6330f525c7d1dd9
woosung1223 <dntjd991223@naver.com>
2022-11-18 20:57:49 +0900

    test(BridgeGameCounter): 리셋 기능에 대한 테스트 코드 생성

--- src/test/java/bridge/BridgeGameCounterTest.java
+++ src/test/java/bridge/BridgeGameCounterTest.java
@@ -9,34 +9,19 @@ import org.junit.jupiter.params.provider.CsvSource;
 
 class BridgeGameCounterTest {
     @Nested
-    @DisplayName("isGameOver 메소드 테스트")
-    class IsGameOver {
-        @ParameterizedTest
-        @CsvSource(value = {"3", "5", "10", "10000", "100"})
-        void 카운터의_값이_한계값에_도달했는지_테스트(int limit) {
-            BridgeGameCounter bridgeGameCounter = new BridgeGameCounter(limit);
-
-            for (int i = 0; i < limit; i++) {
-                bridgeGameCounter.addCount();
-            }
-            assertThat(bridgeGameCounter.isGameOver()).isTrue();
-        }
-    }
-
-    @Nested
     @DisplayName("reset 메소드 테스트")
     class Reset {
         @ParameterizedTest
         @CsvSource(value = {"3", "5", "10", "10000", "100"})
-        void 리셋_기능을_테스트(int limit) {
-            BridgeGameCounter bridgeGameCounter = new BridgeGameCounter(limit);
+        void 리셋_기능을_테스트(int size) {
+            BridgeGameCounter bridgeGameCounter = new BridgeGameCounter();
 
-            for (int i = 0; i < limit; i++) {
+            for (int i = 0; i < size; i++) {
                 bridgeGameCounter.addCount();
             }
             bridgeGameCounter.reset();
 
-            assertThat(bridgeGameCounter.isGameOver()).isFalse();
+            assertThat(bridgeGameCounter.getCount()).isEqualTo(1);
         }
     }
 }
