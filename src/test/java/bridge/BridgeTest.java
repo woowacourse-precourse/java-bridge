53533c79aa386652080cb781aca0e9f3d72ddf3b
TaeHeyeon_Kim <thk98k@gmail.com>
2022-11-18 17:09:55 +0900

    test: InputView 입력값 테스트케이스 작성

--- src/test/java/bridge/BridgeTest.java
+++ src/test/java/bridge/BridgeTest.java
@@ -4,6 +4,10 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+import java.util.Scanner;
+
 
 public class BridgeTest {
     BridgeNumberGenerator bridgeNumberGenerator = new BridgeRandomNumberGenerator();
@@ -14,5 +18,64 @@ public class BridgeTest {
         int num = 5;
 
         Assertions.assertThat(bridgeMaker.makeBridge(num).size()).isEqualTo(num);
+    }
+}
+
+class IOTest {
+    protected void systemIn(String input) {
+        System.setIn(new ByteArrayInputStream(input.getBytes()));
+    }
+}
+
+class InputTest extends IOTest{
+    InputView inputView = new InputView();
+    @DisplayName("readMove() 메서드에 U를 입력하면 U가 출력된다.")
+    @Test
+    public void readMovingTest1() {
+        systemIn("U");
+        test1();
+    }
+
+    void test1() {
+        Scanner scanner = new Scanner(System.in);
+        Assertions.assertThat(inputView.readMoving()).isEqualTo("U");
+    }
+
+    @DisplayName("readMove() 메서드에 D를 입력하면 D가 출력된다.")
+    @Test
+    public void readMovingTest2() {
+        systemIn("D");
+        test2();
+    }
+
+    void test2() {
+        Scanner scanner = new Scanner(System.in);
+        Assertions.assertThat(inputView.readMoving()).isEqualTo("D");
+    }
+
+
+    @DisplayName("readGameCommand() 메서드에 R를 입력하면 R가 출력된다.")
+    @Test
+    public void readMovingTest3() {
+        systemIn("R");
+        test3();
+    }
+
+    void test3() {
+        Scanner scanner = new Scanner(System.in);
+        Assertions.assertThat(inputView.readGameCommand()).isEqualTo("R");
+    }
+
+
+    @DisplayName("readGameCommand() 메서드에 Q를 입력하면 Q가 출력된다.")
+    @Test
+    public void readMovingTest4() {
+        systemIn("Q");
+        test4();
+    }
+
+    void test4() {
+        Scanner scanner = new Scanner(System.in);
+        Assertions.assertThat(inputView.readGameCommand()).isEqualTo("Q");
     }
 }
