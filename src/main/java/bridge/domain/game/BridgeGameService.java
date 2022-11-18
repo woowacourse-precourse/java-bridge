3a5e3b9738f364b873416015e24c686af4781765
hoonn9 <hun9935@gmail.com>
2022-11-18 20:46:33 +0900

    feat(BridgeGameService): BridgeGame 서비스 구현

---
+++ src/main/java/bridge/domain/game/BridgeGameService.java
@@ -0,0 +1,29 @@
+package bridge.domain.game;
+
+import bridge.BridgeMove;
+
+public class BridgeGameService {
+    
+    private final BridgeGame game;
+    
+    public BridgeGameService(BridgeGame game) {
+        this.game = game;
+    }
+    
+    public boolean tryMove(BridgeMove selectMove) {
+        if (game.canMoveToNextPosition(selectMove)) {
+            game.move();
+            return true;
+        }
+        return false;
+    }
+    
+    public void retry() {
+        game.retry();
+    }
+    
+    public boolean isFinish() {
+        return game.isReachedLastPosition();
+    }
+    
+}
