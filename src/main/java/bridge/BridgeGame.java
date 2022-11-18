93e2027c6dbe957309f2fe08a3e3f2a50d88e10e
swonny <wonny921@gmail.com>
2022-11-18 21:19:21 +0900

    feat(BridgeGame): 새로운 bridge 인스턴스 생성

--- src/main/java/bridge/BridgeGame.java
+++ src/main/java/bridge/BridgeGame.java
@@ -1,9 +1,16 @@
 package bridge;
 
+import java.util.List;
+
 /**
  * 다리 건너기 게임을 관리하는 클래스
  */
 public class BridgeGame {
+    private Bridge bridge;
+
+    public BridgeGame(List<String> availablePositions) {
+        this.bridge = new Bridge(availablePositions);
+    }
 
     /**
      * 사용자가 칸을 이동할 때 사용하는 메서드
