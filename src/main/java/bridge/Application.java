8fd5663d420f8dcc2d7223ebbc1e6ddc29494c90
JaeHong-Ahn <63873742+JaeHong-Ahn@users.noreply.github.com>
2022-11-18 19:05:17 +0900

    <feat> 특정 문구 출력 및 재시작, 종료 여부 확인
    게임 재시작 시 처음 입력받았던 다리의 길이만큼 재시작하는 기능 및 게임 종료시 게임 성공 여부와 함께 재시도한 횟수 출력

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -2,24 +2,42 @@ package bridge;
 
 public class Application {
 
+    public static int countChar(String str, char ch) {
+        int count = 0;
+
+        for (int i = 0; i < str.length(); i++) {
+            if (str.charAt(i) == ch) {
+                count++;
+            }
+        }
+
+        return count;
+    }
+
     public static void main(String[] args) {
 
         System.out.println("다리 건너기 게임을 시작합니다.");
         System.out.println();
+        //이동할 칸 입력받음
+        InputView makeBridge = new InputView();
+
+        int bridgeLength = makeBridge.readBridgeSize();             // 다리의 길이
+        int howManyGames = 0;                                       //게임 시도한 횟수 count
+
+        String reOrStop = "";
 
         while(true) {
-            //이동할 칸 입력받음
-            InputView makeBridge = new InputView();
-
             String yesOrNo = "";
+            howManyGames += 1;
 
-            int bridgeLength = makeBridge.readBridgeSize();         // 다리의 길이
             int maxBridgeLength = 0;
-            while(yesOrNo != "X") {
+
+            String mainBridge1 = "[ ";                                //윗층
+            String mainBridge2 = "[ ";                                //아랫층
+
+            while(yesOrNo != "X" || maxBridgeLength == bridgeLength) {
                 String movingUpDown = makeBridge.readMoving();          // U이면 위, D이면 아래
 
-                String mainBridge1 = "[ ";                                //윗층
-                String mainBridge2 = "[ ";                                //아랫층
 
                 BridgeRandomNumberGenerator randomNumber = new BridgeRandomNumberGenerator();
                 int whichBridge = randomNumber.generate();              // 1이면 U, 0이면 D가 맞는거!
@@ -68,9 +86,27 @@ public class Application {
 
                 System.out.println(mainBridge1 + " ]");
                 System.out.println(mainBridge2 + " ]");
-//                mainBridge1 += " | ";
-//                mainBridge2 += " | ";
                 maxBridgeLength += 1;
+            }
+            int cntMainB1 = 0;
+            int cntMainB2 = 0;
+
+            if(countChar(mainBridge1,'O') + countChar(mainBridge2, 'O') == bridgeLength){
+                System.out.println("게임 성공 여부: 성공");
+                System.out.println("총 시도한 횟수: " + howManyGames);
+            }
+            reOrStop = makeBridge.readGameCommand();
+
+            if(reOrStop.equals("R")){
+                continue;
+            }else if(reOrStop.equals("Q")){
+                System.out.println("최종 게임 결과");
+                System.out.println(mainBridge1 + " ]");
+                System.out.println(mainBridge2 + " ]");
+
+                System.out.println("게임 성공 여부: 실패");
+                System.out.println("총 시도한 횟수: " + howManyGames);
+                break;
             }
         }
     }
