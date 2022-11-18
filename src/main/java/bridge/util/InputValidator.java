b1a2f31f08877c2ed44b421ee46011139b359e51
situodys <situodys@gmail.com>
2022-11-18 18:27:50 +0900

    feat(InputValidator): 숫자 외의 값이 포함된 입력인 경우 검증 구현

--- src/main/java/bridge/util/InputValidator.java
+++ src/main/java/bridge/util/InputValidator.java
@@ -2,14 +2,22 @@ package bridge.util;
 
 public class InputValidator {
     private static final String BLANK_INPUT_MSG = "비어 있는 입력값 입니다.";
+    private static final String NOT_NUMERIC_INPUT_MSG = "숫자 이외의 값이 존재하는 입력값입니다.";
 
     public void validateBridgeSize(String userInput) {
         validateBlank(userInput);
+        validateNumeric(userInput);
     }
 
     private void validateBlank(String userInput) {
         if (userInput.isBlank()) {
             throw new IllegalArgumentException(BLANK_INPUT_MSG);
+        }
+    }
+
+    private void validateNumeric(String userInput) {
+        if (!userInput.chars().allMatch(Character::isDigit)) {
+            throw new IllegalArgumentException(NOT_NUMERIC_INPUT_MSG);
         }
     }
 }
--- src/test/java/bridge/input/InputValidatorTest.java
+++ src/test/java/bridge/input/InputValidatorTest.java
@@ -22,4 +22,19 @@ public class InputValidatorTest {
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage(BLANK_INPUT_MSG);
     }
+
+    @ParameterizedTest
+    @DisplayName("다리생성을 위한 사용자 입력에 숫자 외 값이 포함된 경우 에러 메세지와 함께 IllegalArgumentException이 발생한다.")
+    @ValueSource(strings = {"1,","1a","1가","!12","abc","가나다","1:b",})
+    public void validateNotNumericInput() throws Exception{
+        //given
+        InputValidator inputValidator = new InputValidator();
+        final String NOT_NUMERIC_INPUT_MSG = "숫자 이외의 값이 존재하는 입력값입니다.";
+        String userInput = "1.1";
+        //when
+        //then
+        Assertions.assertThatThrownBy(() -> inputValidator.validateBridgeSize(userInput))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(NOT_NUMERIC_INPUT_MSG);
+    }
 }
