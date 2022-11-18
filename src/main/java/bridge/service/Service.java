0d3b13f221b9b8acebdd73ea16517ce3e8c801ac
dev_kong <ahdjd5@gmail.com>
2022-11-18 16:35:44 +0900

    feat(Service): add prepareGame method

--- src/main/java/bridge/service/Service.java
+++ src/main/java/bridge/service/Service.java
@@ -1,4 +1,21 @@
 package bridge.service;
 
+import bridge.domain.BridgeGame;
+import bridge.domain.BridgeMaker;
+import bridge.domain.BridgeRandomNumberGenerator;
+import bridge.utils.Converter;
+
+import java.util.List;
+
 public class Service {
+    BridgeRandomNumberGenerator bridgeRandomNumberGenerator = new BridgeRandomNumberGenerator();
+    BridgeMaker bridgeMaker = new BridgeMaker(bridgeRandomNumberGenerator);
+    Converter converter = new Converter();
+    BridgeGame game;
+
+    public void prepareGame(String bridgeSizeInput) {
+        int bridgeSize = converter.toIntFromString(bridgeSizeInput);
+        List<String> answerBridge = bridgeMaker.makeBridge(bridgeSize);
+        game = new BridgeGame(answerBridge);
+    }
 }
