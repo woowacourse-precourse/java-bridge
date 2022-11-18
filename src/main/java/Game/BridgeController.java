de2eccefd0cf42dd0fcb33ff9464f17fd0118141
TaeHeyeon_Kim <thk98k@gmail.com>
2022-11-18 23:34:09 +0900

    feat(game): 다리를 생성하는 기능 구현
    유저로부터 숫자를 입력받아 다리를 생성하는 메서드 구현

---
+++ src/main/java/Game/BridgeController.java
@@ -0,0 +1,25 @@
+package Game;
+
+import bridge.*;
+
+import java.util.List;
+
+
+public class BridgeController {
+    private InputView inputView = new InputView();
+    private OutputView outputView = new OutputView();
+    private BridgeMaker bridgeMaker = new BridgeMaker(new BridgeRandomNumberGenerator());
+
+    public void run() {
+        System.out.println("다리 건너기 게임을 시작합니다.");
+        System.out.println("다리의 길이를 입력해주세요.");
+        List<String> bridge = getBridge();
+        System.out.println("bridge = " + bridge);
+    }
+
+
+    private List<String> getBridge() {
+        List<String> bridge = bridgeMaker.makeBridge(inputView.readBridgeSize());
+        return bridge;
+    }
+}
