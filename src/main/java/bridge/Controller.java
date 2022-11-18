f3e91e7015e60f5f4bd5c06913b43adc1aff7edd
h0ngwon <w___on@naver.com>
2022-11-18 11:09:49 +0900

    style : 변수 명 변경

--- src/main/java/bridge/Controller.java
+++ src/main/java/bridge/Controller.java
@@ -7,29 +7,29 @@ public class Controller {
 
     public void run () {
         BridgeGame bridgeGame = BridgeGame.getInstance();
-        String bridgeNumber;
+        String bridgeSize;
 
         outputView.printGameStart();
-        bridgeNumber = inputView.readBridgeSize();
-        validate(bridgeNumber);
+        bridgeSize = inputView.readBridgeSize();
+        validate(bridgeSize);
     }
 
-    private void validate(String bridgeNumber) {
-        validateNumber(bridgeNumber);
-        validateRange(bridgeNumber);
+    private void validate(String bridgeSize) {
+        validateNumber(bridgeSize);
+        validateRange(bridgeSize);
     }
-    private void validateNumber (String bridgeNumber) {
+    private void validateNumber (String bridgeSize) {
         int number;
 
         try {
-            Integer.parseInt(bridgeNumber);
+            Integer.parseInt(bridgeSize);
         }catch(NumberFormatException e) {
             throw new IllegalArgumentException(ERROR_MESSAGE + " 숫자가 아닙니다.");
         }
     }
 
-    private void validateRange(String bridgeNumber) {
-        int number = Integer.parseInt(bridgeNumber);
+    private void validateRange(String bridgeSize) {
+        int number = Integer.parseInt(bridgeSize);
 
         if(number < 3 || number > 20) {
             throw new IllegalArgumentException(ERROR_MESSAGE + "다리의 길이는 3 ~ 20 사이의 수 여야 합니다");
