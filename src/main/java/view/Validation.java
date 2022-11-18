c138873692b56f41477abbb257ab782fccb38a43
1eejisoo <jisu991012@gmail.com>
2022-11-18 17:28:23 +0900

    feat: 입력 값 유효성 검증 - 공백을 포함해 입력하였을 경우 예외 발생

--- src/main/java/view/Validation.java
+++ src/main/java/view/Validation.java
@@ -3,11 +3,18 @@ package view;
 public class Validation {
     public void validate(String input) {
         validateEmpty(input);
+        validateContainBlank(input);
     }
 
     public void validateEmpty(String input) {
         if (input.isBlank()) {
             throw new IllegalArgumentException("값이 입력되지 않았습니다.");
+        }
+    }
+
+    public void validateContainBlank(String input) {
+        if (input.contains(" ")) {
+            throw new IllegalArgumentException("입력 값에 공백이 포함되지 않아야 합니다.");
         }
     }
 }
