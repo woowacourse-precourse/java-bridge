7aab66d30ba5f815325c30585b240ca4562b8e23
hoonn9 <hun9935@gmail.com>
2022-11-18 20:03:53 +0900

    feat(Bridge): 인덱스 접근 예외 처리 추가

--- src/main/java/bridge/domain/bridge/Bridge.java
+++ src/main/java/bridge/domain/bridge/Bridge.java
@@ -16,6 +16,17 @@ public class Bridge {
     }
     
     public boolean canMoveToPosition(BridgeMove selectMove, int position) {
+        validatePosition(position);
         return bridge.get(position).equals(selectMove.getDirection());
+    }
+    
+    private void validatePosition(int position) {
+        if (position < 0) {
+            throw new IllegalArgumentException("다리 위치는 0보다 작을 수 없습니다.");
+        }
+        
+        if (position > size() - 1) {
+            throw new IllegalArgumentException("다리 길이보다 큰 위치로 이동할 수 없습니다.");
+        }
     }
 }
