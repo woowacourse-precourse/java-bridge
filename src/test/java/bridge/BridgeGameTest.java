f3706663b835f917e7217631bd21dcfcb8994941
맹순영 <symaeng98@naver.com>
2022-11-18 21:22:42 +0900

    feat(BridgeGame): 칸 이동 후 성공 여부 기능 구현

--- src/test/java/bridge/BridgeGameTest.java
+++ src/test/java/bridge/BridgeGameTest.java
@@ -23,4 +23,15 @@ class BridgeGameTest {
         BridgeGame bridgeGame = new BridgeGame(bridge, outputView);
         Assertions.assertThat(bridgeGame.move("D")).isEqualTo(false);
     }
+
+    @Test
+    void 올발른_칸_이동_후_성공_테스트() {
+        List<String> bridge = new ArrayList<>(List.of("U","U","D"));
+        OutputView outputView = new OutputView();
+        BridgeGame bridgeGame = new BridgeGame(bridge, outputView);
+        bridgeGame.move("U");
+        bridgeGame.move("U");
+        bridgeGame.move("D");
+        Assertions.assertThat(bridgeGame.getSuccess()).isEqualTo(true);
+    }
 }
