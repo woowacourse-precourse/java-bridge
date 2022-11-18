63a4390ff3d747c9d93f6d7590e039dc1fe25892
UJ15 <dbwogml15@naver.com>
2022-11-18 17:13:13 +0900

    feat : create Bridge class

--- src/main/java/bridge/model/Bridge.java
+++ src/main/java/bridge/model/Bridge.java
@@ -1,12 +1,24 @@
 package bridge.model;
 
 import java.util.List;
+import java.util.Objects;
 
 public class Bridge {
 
     private final List<String> bridge;
 
     public Bridge(List<String> bridge) {
+        if (Objects.isNull(bridge)) {
+            throw new IllegalArgumentException();
+        }
+        validate(bridge);
+
         this.bridge = bridge;
+    }
+
+    private void validate(List<String> bridge) {
+        if (!bridge.stream().allMatch(v -> v.equals("U") || v.equals("D"))) {
+            throw new IllegalArgumentException();
+        }
     }
 }
