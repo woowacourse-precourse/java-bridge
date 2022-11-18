3cef7df2a9cdff374bf45c9d5bf6462f32a6ef05
Egenieee <hhgg0925@naver.com>
2022-11-18 16:57:38 +0900

    feat: InputView 이용해서 다리 길이 입력받는 기능 추가

--- src/main/java/controller/Controller.java
+++ src/main/java/controller/Controller.java
@@ -1,4 +1,13 @@
 package controller;
 
+import view.InputView;
+import view.OutputView;
+
 public class Controller {
+    InputView inputView = new InputView();
+    OutputView outputView = new OutputView();
+
+    public void run() {
+        int bridgeSize = inputView.readBridgeSize();
+    }
 }
