12ef33000cdfe7d397adea2ec4873e05d2e25ec8
semi-cloud <sksmsrkdtpal@naver.com>
2022-11-18 17:56:21 +0900

    feat(Square): 이동할 칸을 여러개 입력받는 예외 처리 구현

--- src/main/java/bridge/domain/Square.java
+++ src/main/java/bridge/domain/Square.java
@@ -3,12 +3,15 @@ package bridge.domain;
 public class Square {
 
     private static final String REG_XP_MOVE_TYPE = "[UD]+";
+    private static final int MOVE_COUNT = 1;
 
     private final int position;
+
     private final String moveType;
 
     public Square(int position, String moveType) {
         isValidMoveType(moveType);
+        isValidMoveTypeCount(moveType);
         this.position = position;
         this.moveType = moveType;
     }
@@ -16,6 +19,12 @@ public class Square {
     private void isValidMoveType(String moveType) {
         if (!moveType.matches(REG_XP_MOVE_TYPE)) {
             throw new IllegalArgumentException("[ERROR] 이동할 칸은 U와 D 문자만 입력 가능합니다.");
+        }
+    }
+
+    private void isValidMoveTypeCount(String moveType) {
+        if (moveType.length() != MOVE_COUNT) {
+            throw new IllegalArgumentException("[ERROR] 이동할 칸은 U와 D 중에 하나만 입력 가능합니다.");
         }
     }
 }
