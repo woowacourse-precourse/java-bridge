31ecd40171611d8bd518391f6aa7f864036c53bd
juhwang <juhong4930@naver.com>
2022-11-18 20:00:27 +0900

    refactor(BridgeLocation): add enumerations and methods

--- src/main/java/bridge/BridgeLocation.java
+++ src/main/java/bridge/BridgeLocation.java
@@ -3,15 +3,21 @@ package bridge;
 import java.util.Arrays;
 
 public enum BridgeLocation {
-    UP(1, "U"),
-    DOWN(0, "D");
+    UP_FALSE(1, false, "U", "X"),
+    UP_TRUE(1, true, "U", "O"),
+    DOWN_FALSE(0, false, "D", "X"),
+    DOWN_TRUE(0, true, "D", "O");
 
     private final int locationNumber;
+    private final boolean correctLocation;
     private final String location;
+    private final String stateLocation;
 
-    BridgeLocation(int locationNumber, String location) {
+    BridgeLocation(int locationNumber, boolean correctLocation, String location, String stateLocation) {
         this.locationNumber = locationNumber;
+        this.correctLocation = correctLocation;
         this.location = location;
+        this.stateLocation = stateLocation;
     }
 
     public static BridgeLocation createLocation(int locationNumber) {
@@ -21,14 +27,27 @@ public enum BridgeLocation {
                 .orElseThrow(() -> new IllegalArgumentException("[ERROR] 존재하지 않는 정보입니다."));
     }
 
-    public static BridgeLocation createLocation(String location) {
+    public static BridgeLocation createLocation(String location, boolean correctLocation) {
         return Arrays.stream(values())
-                .filter(locationValue -> locationValue.location.equals(location))
+                .filter(locationValue -> isLocation(locationValue, location)
+                        && isCorrectLocation(locationValue, correctLocation))
                 .findFirst()
                 .orElseThrow(() -> new IllegalArgumentException("[ERROR] U 또는 D를 입력해 주시기 바랍니다."));
     }
 
+    private static boolean isCorrectLocation(BridgeLocation locationValue, boolean correctLocation) {
+        return locationValue.correctLocation == correctLocation;
+    }
+
+    private static boolean isLocation(BridgeLocation locationValue, String location) {
+        return locationValue.location.equals(location);
+    }
+
     public String getLocation() {
         return location;
+    }
+
+    public String getStateLocation() {
+        return stateLocation;
     }
 }
