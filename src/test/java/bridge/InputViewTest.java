576be3b476c9816080e0a4bffa8048f8aea748fe
NotableDeveloper <castlereal@naver.com>
2022-11-17 23:02:33 -0800

    <test> : 게임 재시작 여부를 잘못 입력한 경우에 대한 테스트 코드 작성 게임 재시작 여부를 입력할 때, 입력 값이 단일 문자가 아닌 경우와 정해진 값 이외의 입력이 발생한 경우에 대한 테스트 코드를 작성함.

--- src/test/java/bridge/InputViewTest.java
+++ src/test/java/bridge/InputViewTest.java
@@ -49,6 +49,26 @@ public class InputViewTest extends NsTest {
         });
     }
 
+    @DisplayName("게임 재시작 여부를 입력할 때, 입력 값이 단일 문자가 아니면 예외가 발생한다.")
+    @Test
+    void enterGameCommandByNotSingleCharacter() {
+        assertSimpleTest(() -> {
+            assertThatThrownBy(() -> runException("3", "U", "Ab"))
+                    .isInstanceOf(IllegalArgumentException.class)
+                    .hasMessage("[ERROR] 입력 값이 단일 문자가 아닙니다.");
+        });
+    }
+
+    @DisplayName("게임 재시작 여부를 입력할 때, 이미 정해진 값 외의 입력 값은 예외가 발생한다.")
+    @Test
+    void enterGameCommandByInvalidValue() {
+        assertSimpleTest(() -> {
+            assertThatThrownBy(() -> runException("3", "U", "A"))
+                    .isInstanceOf(IllegalArgumentException.class)
+                    .hasMessage("[ERROR] 재시작 여부를 잘못 입력하셨습니다.");
+        });
+    }
+
     @Override
     protected void runMain() { Application.main(new String[]{}); }
 }
