11c19f2e5874c82daf7e358402f84e099db42038
Sinryuji <siler2474@gmail.com>
2022-11-18 18:09:36 +0900

    refactor(BridgeGame): bridge 필드 final로 변경

--- src/main/java/bridge/BridgeGame.java
+++ src/main/java/bridge/BridgeGame.java
@@ -4,7 +4,7 @@ package bridge;
  * 다리 건너기 게임을 관리하는 클래스
  */
 public class BridgeGame {
-    private Bridge bridge;
+    private final Bridge bridge;
 
     public BridgeGame(Bridge bridge) {
         this.bridge = bridge;
