cad4fa5bc38463cd5994267c5c41c0f4bcb3d01e
Sieun Park <psieun0606@gmail.com>
2022-11-19 05:07:10 +0900

    refactor: static으로 변경

--- src/main/java/bridge/View/OutputView.java
+++ src/main/java/bridge/View/OutputView.java
@@ -4,9 +4,9 @@ package bridge.View;
  * 사용자에게 게임 진행 상황과 결과를 출력하는 역할을 한다.
  */
 public class OutputView {
-    private final String GAME_START = "다리 건너기 게임을 시작합니다.";
+    private static final String GAME_START = "다리 건너기 게임을 시작합니다.";
 
-    public void printGameStart() {
+    public static void printGameStart() {
         System.out.println(GAME_START);
     }
 
