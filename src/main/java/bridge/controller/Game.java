8959da0384145ca38bcd6491113d9f591132e81c
Voyager003 <85725033+Voyager003@users.noreply.github.com>
2022-11-18 15:50:21 +0900

    Feat: 추가 - Game bridgeMaker 다리를 생성하는 BridgeMaker를 컨트롤러에서 구현

--- src/main/java/bridge/controller/Game.java
+++ src/main/java/bridge/controller/Game.java
@@ -1,16 +1,22 @@
 package bridge.controller;
 
+import bridge.domain.BridgeMaker;
+import bridge.domain.BridgeRandomNumberGenerator;
 import bridge.view.InputView;
 
-public class Game {
+import java.util.List;
 
+public class Game {
     private InputView inputView;
+    private BridgeMaker bridgeMaker;
 
     public Game() {
         inputView = new InputView();
+        bridgeMaker = new BridgeMaker(new BridgeRandomNumberGenerator());
     }
 
     public void play() {
         int input = inputView.readBridgeSize();
+        List<String> bridge = bridgeMaker.makeBridge(input);
     }
 }
