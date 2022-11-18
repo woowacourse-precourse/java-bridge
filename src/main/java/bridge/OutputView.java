7bd461ba15a3d914d641acb89952cf89b2d49572
KyungKyu Park <rudrb0505@gmail.com>
2022-11-18 10:24:06 +0900

    test(OutputViewTest): 이동가능에 따라 선택한 칸을 표시하는 테스트

--- src/main/java/bridge/OutputView.java
+++ src/main/java/bridge/OutputView.java
@@ -7,17 +7,23 @@ package bridge;
  * 값 출력을 위해 필요한 메서드를 추가할 수 있다.
  */
 
+import java.util.HashMap;
+
 /**
  * 사용자에게 게임 진행 상황과 결과를 출력하는 역할을 한다.
  */
 public class OutputView {
+	private HashMap<String, String> status = new HashMap<>();
 
 	/**
 	 * 현재까지 이동한 다리의 상태를 정해진 형식에 맞춰 출력한다.
 	 * <p>
 	 * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
 	 */
-	public void printMap() {
+	public HashMap<String, String> printMap(String userSelectedCell, String bridgeLetter) {
+		status.put("UpperCell", "[ O ]");
+		status.put("LowerCell", "[   ]");
+		return status;
 	}
 
 	/**
---
+++ src/test/java/bridge/OutputViewTest.java
@@ -0,0 +1,78 @@
+package bridge;
+
+import static org.assertj.core.api.Assertions.*;
+
+import java.util.HashMap;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class OutputViewTest {
+	@DisplayName("위 칸이 이동 가능한 칸인 경우 O 표시 출력")
+	@Test
+	void printSuccessUpperCell() {
+		// given
+		String userSelectedCell = "U";
+		String bridgeLetter = "U";
+		HashMap<String, String> expectedStatus = new HashMap<>();
+		expectedStatus.put("UpperCell", "[ O ]");
+		expectedStatus.put("LowerCell", "[   ]");
+
+		// when
+		HashMap<String, String> actualStatus = new OutputView().printMap(userSelectedCell, bridgeLetter);
+
+		// then
+		assertThat(actualStatus).isEqualTo(expectedStatus);
+	}
+
+	@DisplayName("위 칸이 이동 불가능한 칸인 경우 X 표시 출력")
+	@Test
+	void printFailUpperCell() {
+		// given
+		String userSelectedCell = "U";
+		String bridgeLetter = "D";
+		HashMap<String, String> expectedStatus = new HashMap<>();
+		expectedStatus.put("UpperCell", "[ X ]");
+		expectedStatus.put("LowerCell", "[   ]");
+
+		// when
+		HashMap<String, String> actualStatus = new OutputView().printMap(userSelectedCell, bridgeLetter);
+
+		// then
+		assertThat(actualStatus).isEqualTo(expectedStatus);
+	}
+
+	@DisplayName("아래 칸이 이동 가능한 칸인 경우 O 표시 출력")
+	@Test
+	void printSuccessLowerCell() {
+		// given
+		String userSelectedCell = "D";
+		String bridgeLetter = "D";
+		HashMap<String, String> expectedStatus = new HashMap<>();
+		expectedStatus.put("UpperCell", "[   ]");
+		expectedStatus.put("LowerCell", "[ O ]");
+
+		// when
+		HashMap<String, String> actualStatus = new OutputView().printMap(userSelectedCell, bridgeLetter);
+
+		// then
+		assertThat(actualStatus).isEqualTo(expectedStatus);
+	}
+
+	@DisplayName("아래 칸이 이동 불가능한 칸인 경우 X 표시 출력")
+	@Test
+	void printFailLowerCell() {
+		// given
+		String userSelectedCell = "D";
+		String bridgeLetter = "U";
+		HashMap<String, String> expectedStatus = new HashMap<>();
+		expectedStatus.put("UpperCell", "[   ]");
+		expectedStatus.put("LowerCell", "[ X ]");
+
+		// when
+		HashMap<String, String> actualStatus = new OutputView().printMap(userSelectedCell, bridgeLetter);
+
+		// then
+		assertThat(actualStatus).isEqualTo(expectedStatus);
+	}
+}
