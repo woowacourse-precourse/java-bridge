958db01f94683b04875bd76ba87d664831275198
jeujeon <1132jjw@naver.com>
2022-11-19 02:29:28 +0900

    feat: 다리 건너기 결과 기록하는 기능 구현

---
+++ src/main/java/bridge/domain/Record.java
@@ -0,0 +1,23 @@
+package bridge.domain;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class Record {
+	private static final List<List<String>> result = new ArrayList<>();
+
+	public void recordResult(String move, Boolean moveResult) {
+		if (moveResult == true) {
+			result.add(List.of(move, "O"));
+		}
+		if (moveResult == false) {
+			result.add(List.of(move, "X"));
+		}
+	}
+
+	public List<List<String>> getResult() {
+		return result;
+	}
+}
