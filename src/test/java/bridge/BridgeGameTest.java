2bf2d3b5f6c24913754443f5a3306f4f12130687
skck0226 <skck0226@ajou.ac.kr>
2022-11-19 01:11:33 +0900

    [test] (BridgeGameTest) 한칸 이동 테스트 수정, 추가

--- src/test/java/bridge/BridgeGameTest.java
+++ src/test/java/bridge/BridgeGameTest.java
@@ -4,9 +4,11 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.List;
+import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -16,28 +18,42 @@ class BridgeGameTest {
 
     @BeforeEach
     void init() {
-        bridgeGame = new BridgeGame(List.of("U", "D", "D", "D", "U", "D"));
+        bridgeGame = new BridgeGame(List.of("U", "D", "D", "D", "U"));
     }
 
     @DisplayName("한칸 이동 성공 테스트")
     @Test
     void 한칸이동_성공_테스트() {
-        assertTrue(bridgeGame.move("U"));
-        assertTrue(bridgeGame.move("D"));
-        assertTrue(bridgeGame.move("D"));
-        assertTrue(bridgeGame.move("D"));
-        assertTrue(bridgeGame.move("U"));
-        assertTrue(bridgeGame.move("D"));
+        assertEquals(List.of("[ O ]","[   ]"),bridgeGame.move("U"));
+        assertEquals(List.of("[ O |   ]","[   | O ]"),bridgeGame.move("D"));
+        assertEquals(List.of("[ O |   |   ]","[   | O | O ]"),bridgeGame.move("D"));
+        assertEquals(List.of("[ O |   |   |   ]","[   | O | O | O ]"),bridgeGame.move("D"));
+        assertEquals(List.of("[ O |   |   |   | O ]","[   | O | O | O |   ]"),bridgeGame.move("U"));
     }
 
     @DisplayName("한칸 이동 실패 테스트")
+    @ParameterizedTest()
+    @MethodSource("generateData")
+    void 한칸이동_실패_테스트(List<String> param) {
+        List<String> movingResult = bridgeGame.move(param.get(0));
+        assertEquals(param.subList(1,3),movingResult.subList(0,2));
+        assertEquals("FAIL",movingResult.get(2));
+    }
+
+    static Stream<List<String>> generateData() {
+        return Stream.of(
+            List.of("D","[   ]","[ X ]"),
+            List.of("D","[   ]","[ X ]")
+        );
+    }
+
+    @DisplayName("한칸 이동 성공_실패 테스트")
     @Test
-    void 한칸이동_실패_테스트() {
-        assertFalse(bridgeGame.move("D"));
-        assertFalse(bridgeGame.move("U"));
-        assertFalse(bridgeGame.move("U"));
-        assertFalse(bridgeGame.move("U"));
-        assertFalse(bridgeGame.move("D"));
-        assertFalse(bridgeGame.move("U"));
+    void 한칸이동_성공_실패_테스트() {
+        assertEquals(List.of("[ O ]","[   ]"),bridgeGame.move("U"));
+        assertEquals(List.of("[ O |   ]","[   | O ]"),bridgeGame.move("D"));
+        assertEquals(List.of("[ O |   |   ]","[   | O | O ]"),bridgeGame.move("D"));
+        assertEquals(List.of("[ O |   |   |   ]","[   | O | O | O ]"),bridgeGame.move("D"));
+        assertEquals(List.of("[ O |   |   |   |   ]","[   | O | O | O | X ]","FAIL"),bridgeGame.move("D"));
     }
 }
