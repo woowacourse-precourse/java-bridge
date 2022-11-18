59b6262606e55f70959aba48e584ff5a53fcb790
semi-cloud <sksmsrkdtpal@naver.com>
2022-11-18 15:28:59 +0900

    feat(OutputView): 게임 시작 문구를 출력하는 기능 구현

--- src/main/java/bridge/controller/BridgeGameController.java
+++ src/main/java/bridge/controller/BridgeGameController.java
@@ -2,12 +2,15 @@ package bridge.controller;
 
 import bridge.domain.BridgeSize;
 import bridge.view.InputView;
+import bridge.view.OutputView;
 
 public class BridgeGameController {
 
     private final InputView inputView = new InputView();
+    private final OutputView outputView = new OutputView();
 
     public void run() {
+        outputView.printInit();
         int size = inputView.readBridgeSize();
         BridgeSize bridgeSize = new BridgeSize(size);
     }
