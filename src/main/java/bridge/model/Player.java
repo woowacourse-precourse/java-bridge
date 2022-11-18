46d8cb4af542b858940f81be2755908701d2ec8e
jungle <minuet1215@jbnu.ac.kr>
2022-11-18 15:22:28 +0900

    Feat(Player): 좌표 정보와 재시도 횟수를 변수로 갖는 Player 클래스 구현

--- src/main/java/bridge/model/Player.java
+++ src/main/java/bridge/model/Player.java
@@ -1,2 +1,30 @@
-package bridge.model;public class Player {
+package bridge.model;
+
+public class Player {
+    private Position position;
+    private int tryCount;
+
+    public Player() {
+        this.position = new Position();
+        this.tryCount = 0;
+    }
+
+    public Position getPosition() {
+        return position;
+    }
+
+    public void setPosition() {
+    }
+
+    public int getTryCount() {
+        return tryCount;
+    }
+
+    public void setTryCount() {
+        this.tryCount += 1;
+    }
+
+    public void initializePosition() {
+        this.position = new Position();
+    }
 }
