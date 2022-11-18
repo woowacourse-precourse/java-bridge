327a0010976df6e56cfa5c7386377472275a9875
anneu <b.imnuj@gmail.com>
2022-11-18 13:28:10 +0900

    test: 플레이어가 선택한 칸으로 이동할 수 있는지 확인하는 기능 테스트

---
+++ src/test/java/bridge/BridgeTest.java
@@ -0,0 +1,27 @@
+package bridge;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.List;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+class BridgeTest {
+
+    private final Bridge bridge = new Bridge(List.of("D", "U", "U"));
+
+    @DisplayName("건널 수 있는 칸인지 확인한다.")
+    @CsvSource(value = {
+            "0, D, true",
+            "0, U, false",
+            "1, D, false",
+            "1, U, true",
+            "2, D, false",
+            "2, U, true"})
+    @ParameterizedTest
+    void checkMobility(int play, String moving, boolean expected) {
+        boolean actual = bridge.isPossibleMoving(play, moving);
+        assertThat(actual).isEqualTo(expected);
+    }
+}
