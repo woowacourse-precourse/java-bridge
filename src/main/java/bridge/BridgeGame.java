d0569d2fe55f2f77fd2df2c04892fb3422e1a0f5
HyebinShin <hyeviin.dev@gmail.com>
2022-11-18 20:56:06 +0900

    Feat: 다리 건너기 메소드 작성하기
    Bridge 객체 두 개를 서로 비교한다.
    비교한 값이 일치하면 O, 아니면 X를 저장한다.
    X가 저장될 경우에 재시작한다.

--- src/main/java/bridge/BridgeGame.java
+++ src/main/java/bridge/BridgeGame.java
@@ -1,16 +1,63 @@
 package bridge;
 
+import java.sql.SQLOutput;
+import java.util.*;
+
 /**
  * 다리 건너기 게임을 관리하는 클래스
  */
 public class BridgeGame {
+    private final HashMap<String, List<String>> bridgeGameResult = new HashMap<>();
+    private int totalCount = 0;
+
+    public void setBridgeGameResult() {
+        bridgeGameResult.put("U", new ArrayList<String>());
+        bridgeGameResult.put("D", new ArrayList<String>());
+    }
+
+    public void clear() {
+        bridgeGameResult.clear();
+    }
+    public HashMap<String, List<String>> getBridgeGameResult() {
+        return bridgeGameResult;
+    }
 
     /**
      * 사용자가 칸을 이동할 때 사용하는 메서드
      * <p>
      * 이동을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void move() {
+    public void move(Bridge player, Bridge answer, int index) {
+        totalCount++;
+        String paintGame = getPaintGame(player, answer, index);
+        String position = player.getBridge().get(index);
+        String otherPosition = getOtherPosition(position);
+        paintBridgeGameResult(position, otherPosition, paintGame);
+
+        if(bridgeGameResult.get(position).contains("X")) {
+            retry();
+        }
+    }
+
+    private String getPaintGame(Bridge player, Bridge answer, int index) {
+        if(answer.isSamePosition(player, index)) {
+            return "O";
+        }
+        return "X";
+    }
+
+    private String getOtherPosition(String position) {
+        if("U".equals(position)) {
+            return "D";
+        }
+        return "U";
+    }
+
+    private void paintBridgeGameResult(String position, String otherPosition, String paintGame) {
+        List<String> positionResult = bridgeGameResult.get(position);
+        List<String> otherResult = bridgeGameResult.get(otherPosition);
+        positionResult.add(paintGame);
+        otherResult.add(" ");
     }
 
     /**
@@ -19,5 +66,6 @@ public class BridgeGame {
      * 재시작을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
     public void retry() {
+        System.out.println("retry!");
     }
 }
