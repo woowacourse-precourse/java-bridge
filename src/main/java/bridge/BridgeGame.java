161cdebe61a7d820620a16a18ce3957882391116
Soomin Lim <augustth511@gmail.com>
2022-11-18 16:51:56 +0900

    feat(BridgeGame): BridgeGame이 다리 길이를 인자로 받아 다리를 관리하도록 구현

--- src/main/java/bridge/BridgeGame.java
+++ src/main/java/bridge/BridgeGame.java
@@ -1,9 +1,21 @@
 package bridge;
 
+import bridge.domain.Bridge;
+
 /**
  * 다리 건너기 게임을 관리하는 클래스
  */
 public class BridgeGame {
+
+    private final BridgeMaker bridgeMaker;
+    private final BridgeNumberGenerator bridgeNumberGenerator;
+    private final Bridge bridge;
+
+    public BridgeGame(int size) {
+        this.bridgeNumberGenerator = new BridgeRandomNumberGenerator();
+        this.bridgeMaker = new BridgeMaker(bridgeNumberGenerator);
+        this.bridge = new Bridge(bridgeMaker.makeBridge(size));
+    }
 
     /**
      * 사용자가 칸을 이동할 때 사용하는 메서드
