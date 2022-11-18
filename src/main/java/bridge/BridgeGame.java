94be0a90cceb8b3aded315cbbb8a53f77673f029
jootang2 <joohwan.song15b@gmail.com>
2022-11-18 20:38:24 +0900

    :sparkles: 4-2. 입력 값에 따른 1번 다리 출력문 생성

--- src/main/java/bridge/BridgeGame.java
+++ src/main/java/bridge/BridgeGame.java
@@ -1,14 +1,56 @@
 package bridge;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * 다리 건너기 게임을 관리하는 클래스
  */
 public class BridgeGame {
 
+    static int count = 0;
+    static Map<Integer, String> firstStorage = new HashMap<>();
+    static Map<Integer, String> secondStorage = new HashMap<>();
+
     public void move(List<String> firstBridge, List<String> secondBridge, String move) {
         bridgeSet(firstBridge, secondBridge);
+        String firstBridgeMap = moveFirstBridge(firstBridge, move);
+        count++;
+    }
+
+    private String moveFirstBridge(List<String> firstBridge, String move) {
+        StringBuilder firstSb = new StringBuilder();
+        firstStorage.put(-1, "");
+        firstSb.append(firstStorage.get(count - 1));
+        firstBridgeMoveDown(firstSb, move);
+        firstBridgeMoveUp(firstSb, firstBridge, move);
+        firstStorage.put(count, firstSb.toString());
+        return firstSb.toString();
+    }
+
+    private void firstBridgeMoveUp(StringBuilder firstSb, List<String> firstBridge, String move) {
+        if (move.equals("U")) {
+            if (count != 0) {
+                firstSb.append(" | ");
+                firstSb.append(firstBridge.get(count));
+            }
+            if (count == 0) {
+                firstSb.append(firstBridge.get(count));
+            }
+        }
+    }
+
+    private void firstBridgeMoveDown(StringBuilder firstSb, String move) {
+        if (move.equals("D")) {
+            if (count != 0) {
+                firstSb.append(" | ");
+                firstSb.append(" ");
+            }
+            if (count == 0) {
+                firstSb.append(" ");
+            }
+        }
     }
 
     private void bridgeSet(List<String> firstBridge, List<String> secondBridge) {
