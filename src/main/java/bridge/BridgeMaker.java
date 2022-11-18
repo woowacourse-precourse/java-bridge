5556ba618d6a85f39b3b6715ab52ee0c48a41141
Lee Sang Bin <rhkd2612@naver.com>
2022-11-18 22:57:13 +0900

    feat: fill bridge list for random

--- src/main/java/bridge/BridgeMaker.java
+++ src/main/java/bridge/BridgeMaker.java
@@ -1,5 +1,6 @@
 package bridge;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -18,6 +19,23 @@ public class BridgeMaker {
      * @return 입력받은 길이에 해당하는 다리 모양. 위 칸이면 "U", 아래 칸이면 "D"로 표현해야 한다.
      */
     public List<String> makeBridge(int size) {
-        return null;
+        List<String> randomBridge = new ArrayList<>();
+
+        for(int i = 0; i < size; i++){
+            int randBridgeNumber = bridgeNumberGenerator.generate();
+
+            randomBridge.add(convertBridgeNumberToString(randBridgeNumber));
+        }
+
+        return randomBridge;
+    }
+
+    /**
+     * @return 입력된 다리 정수를 문자열로 반환. 0이면 "D", 1이면 "U"으로 반환한다.
+     */
+    private String convertBridgeNumberToString(int randBridgeNumber) {
+        if(randBridgeNumber == 0)
+            return "D";
+        return "U";
     }
 }
