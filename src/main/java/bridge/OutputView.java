986b96c5af0b79887f24a3b4680ee2dce011bc90
ckn1070 <ckn1070@naver.com>
2022-11-18 21:01:15 +0900

    feat(bridge): make output messages by enum

--- src/main/java/bridge/OutputView.java
+++ src/main/java/bridge/OutputView.java
@@ -21,3 +21,31 @@ public class OutputView {
     public void printResult() {
     }
 }
+
+enum outputMessage {
+
+    OUTPUT_BRIDGE_HEAD("[\n["),
+    OUTPUT_BRIDGE_TAIL("]\n]"),
+    OUTPUT_BRIDGE_MIDDLE("|\n|"),
+
+    OUTPUT_MOVING_CORRECT(" O "),
+    OUTPUT_MOVING_INCORRECT(" X "),
+    OUTPUT_WITHOUT_MOVING("   "),
+
+    OUTPUT_SUCCESS("성공"),
+    OUTPUT_FAILURE("실패"),
+
+    OUTPUT_TOTAL("\n최종 게임 결과"),
+    OUTPUT_RESULT("\n게임 성공 여부: "),
+    OUTPUT_TRIAL("총 시도한 횟수: ");
+
+    private final String message;
+
+    outputMessage(String message) {
+        this.message = message;
+    }
+
+    public String get() {
+        return message;
+    }
+}
