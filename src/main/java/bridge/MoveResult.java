ff1bbda5e64b5309193ad7d75df126507feabb16
은지 <midzy0228@gmail.com>
2022-11-18 20:04:26 +0900

    feat: 다리 상황을 만들어줄 클래스 생성

---
+++ src/main/java/bridge/MoveResult.java
@@ -0,0 +1,53 @@
+package bridge;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MoveResult {
+    private List<String> highBridge = new ArrayList<>();
+    private List<String> lowBridge = new ArrayList<>();
+
+    public void makeResultBridge(String moving, boolean movingPossible) {
+        if (moving.equals("U")) {
+            up(movingPossible);
+        }
+        if (moving.equals("D")) {
+            down(movingPossible);
+        }
+    }
+
+    public void clearHistory() {
+        lowBridge.clear();
+        highBridge.clear();
+    }
+
+    public List<String> getHighBridge() {
+        return highBridge;
+    }
+
+    public List<String> getLowBridge() {
+        return lowBridge;
+    }
+
+    private void up(boolean success) {
+        if (success) {
+            highBridge.add("O");
+            lowBridge.add("  ");
+        }
+        if (!success) {
+            highBridge.add("X");
+            lowBridge.add("  ");
+        }
+    }
+
+    private void down(boolean success) {
+        if (success) {
+            highBridge.add("  ");
+            lowBridge.add("O");
+        }
+        if (!success) {
+            highBridge.add("  ");
+            lowBridge.add("X");
+        }
+    }
+}
