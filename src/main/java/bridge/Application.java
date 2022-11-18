7b78f30ae3611cdf96084001763e3a7aeb615cb2
SEONGJOON KIM <jobcho6320@naver.com>
2022-11-18 15:47:14 +0900

    refactor: main 메소드 로직 도메인 및 컨트롤러로 이동

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -1,9 +1,11 @@
 package bridge;
 
+가import bridge.controller.BridgeController;
 import bridge.domain.BridgeGame;
 import bridge.domain.BridgeMaker;
 import bridge.view.InputView;
 import bridge.view.OutputView;
+import camp.nextstep.edu.missionutils.Console;
 
 import java.util.List;
 
@@ -14,91 +16,7 @@ public class Application {
     private static final String BLANK = " ";
     public static void main(String[] args) {
 
-
-        BridgeRandomNumberGenerator bridgeRandomNumberGenerator = new BridgeRandomNumberGenerator();
-
-        OutputView.printGameStartMessage();
-        int bridgeSize = InputView.inputBridgeSize();
-        BridgeMaker bridgeMaker = new BridgeMaker(bridgeRandomNumberGenerator);
-
-
-        List<String> bridgeResult = bridgeMaker.makeBridge(bridgeSize);
-        int tryCount = 1;
-        BridgeGame bridgeGame = new BridgeGame(bridgeResult, tryCount);
-        System.out.println("다리 결과물 : " + bridgeResult);
-
-
-
-
-        StringBuilder upSideResult = new StringBuilder("[");
-        StringBuilder downSideResult = new StringBuilder("[");
-
-
-        for (int i = 0; i < bridgeGame.size(); i++) {
-            String moveWord = InputView.inputMoving();
-            if (bridgeGame.matchMoving(i, moveWord) && moveWord.equals("U")) {
-                upSideResult.insert(4*i+1, " ");
-                upSideResult.insert(4*i+2, "O");
-                upSideResult.insert(4*i+3, " ");
-                downSideResult.insert(4*i+1, " ");
-                downSideResult.insert(4*i+2, " ");
-                downSideResult.insert(4*i+3, " ");
-                if (i != bridgeGame.size() - 1) {
-                    upSideResult.insert(4*i+4, "|");
-                    downSideResult.insert(4*i+4, "|");
-                }
-                if (i == bridgeGame.size() - 1) {
-                    upSideResult.insert(4*i+4, "]");
-                    downSideResult.insert(4*i+4, "]");
-                }
-            }
-
-            if (bridgeGame.matchMoving(i, moveWord) && moveWord.equals("D")) {
-                upSideResult.insert(4*i+1, " ");
-                upSideResult.insert(4*i+2, " ");
-                upSideResult.insert(4*i+3, " ");
-                downSideResult.insert(4*i+1, " ");
-                downSideResult.insert(4*i+2, "O");
-                downSideResult.insert(4*i+3, " ");
-                if (bridgeGame.size() - 1 != i) {
-                    upSideResult.insert(4*i+4, "|");
-                    downSideResult.insert(4*i+4, "|");
-                }
-                if (bridgeGame.size() - 1 == i) {
-                    upSideResult.insert(4*i+4, "]");
-                    downSideResult.insert(4*i+4, "]");
-                }
-            }
-            // 게임에서 졌을 다
-            if (!bridgeGame.matchMoving(i, moveWord) && moveWord.equals("U")) {
-                upSideResult.insert(4*i+1, " ");
-                upSideResult.insert(4*i+2, "X");
-                upSideResult.insert(4*i+3, " ");
-                upSideResult.insert(4*i+4, "]");
-                downSideResult.insert(4*i+1, " ");
-                downSideResult.insert(4*i+2, " ");
-                downSideResult.insert(4*i+3, " ");
-                downSideResult.insert(4*i+4, "]");
-                bridgeGame.retry();
-            }
-
-            if (!bridgeGame.matchMoving(i, moveWord) && moveWord.equals("D")) {
-                upSideResult.insert(4*i+1, " ");
-                upSideResult.insert(4*i+2, " ");
-                upSideResult.insert(4*i+3, " ");
-                upSideResult.insert(4*i+4, "]");
-                downSideResult.insert(4*i+1, " ");
-                downSideResult.insert(4*i+2, "X");
-                downSideResult.insert(4*i+3, " ");
-                downSideResult.insert(4*i+4, "]");
-                bridgeGame.retry();
-            }
-            OutputView.printMap(upSideResult, downSideResult);
-        }
-        OutputView.printResultMessage();
-        OutputView.printMap(upSideResult, downSideResult);
-        OutputView.printResult(tryCount, true);
-
-
+        BridgeController bridgeController = new BridgeController();
+        bridgeController.run();
     }
 }
