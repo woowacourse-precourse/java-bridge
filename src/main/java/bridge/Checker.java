41ce71ed4ca362e9f3e9260d379b40001909b98d
nn98 <jkllhgb@gmail.com>
2022-11-18 14:50:13 +0900

    Feat: 입력값 유효성 검사 구현

--- src/main/java/bridge/Checker.java
+++ src/main/java/bridge/Checker.java
@@ -1,16 +1,31 @@
 package bridge;
 
 public class Checker {
-    public int checkValidate(String input) {
-        return 0;
+    public int checkValidate(String input) throws IllegalArgumentException {
+        return checkNumeric(input);
     }
-    public int checkNumeric(String input) {
-        return 0;
+
+    public int checkNumeric(String input) throws IllegalArgumentException {
+        int inputNumber;
+        try {
+            inputNumber = Integer.parseInt(input);
+        } catch (IllegalArgumentException illegalArgumentException) {
+            throw new IllegalArgumentException();
+        }
+        return checkNegative(inputNumber);
     }
-    public int checkNegative(int input) {
-        return input;
+
+    public int checkNegative(int input) throws IllegalArgumentException {
+        if (input < 0) {
+            throw new IllegalArgumentException();
+        }
+        return checkBoundary(input);
     }
-    public int checkBoundary(int input) {
+
+    public int checkBoundary(int input) throws IllegalArgumentException {
+        if (input < 3 || input > 20) {
+            throw new IllegalArgumentException();
+        }
         return input;
     }
 }
