75ea93e7b523822bbf9a2af802c203815a6ec769
hoonn9 <hun9935@gmail.com>
2022-11-18 20:07:54 +0900

    feat(BridgeGame): BridgeGame 모델 구현

---
+++ src/main/java/bridge/domain/game/BridgeGame.java
@@ -0,0 +1,56 @@
+package bridge.domain.game;
+
+import bridge.BridgeMove;
+import bridge.domain.bridge.Bridge;
+
+public class BridgeGame {
+    
+    private final Bridge bridge;
+    private Integer currentPosition = -1;
+    private Integer tryCount = 1;
+    
+    public BridgeGame(Bridge bridge) {
+        this.bridge = bridge;
+    }
+    
+    public Bridge getBridge() {
+        return bridge;
+    }
+    
+    public Integer getCurrentPosition() {
+        return currentPosition;
+    }
+    
+    public void setCurrentPosition(Integer currentPosition) {
+        this.currentPosition = currentPosition;
+    }
+    
+    public Integer getTryCount() {
+        return tryCount;
+    }
+    
+    public void setTryCount(Integer tryCount) {
+        this.tryCount = tryCount;
+    }
+    
+    private int getNextPosition() {
+        return getCurrentPosition() + 1;
+    }
+    
+    public boolean isReachedLastPosition() {
+        return getBridge().size() <= getCurrentPosition();
+    }
+    
+    public void move() {
+        setCurrentPosition(getCurrentPosition() + 1);
+    }
+    
+    public void retry() {
+        setCurrentPosition(-1);
+        setTryCount(getTryCount() + 1);
+    }
+    
+    public boolean canMoveToNextPosition(BridgeMove move) {
+        return getBridge().canMoveToPosition(move, getNextPosition());
+    }
+}
