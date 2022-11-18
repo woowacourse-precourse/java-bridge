a4590d362a8735cf8fc0c775571538011e3bd8e5
imsongj <imsongj@gmail.com>
2022-11-18 16:24:56 +0900

    fix(bridge) : 이동할 칸 입력 예외 처리 기능 오류 수정

--- src/main/java/bridge/model/validator/NextMoveValidator.java
+++ src/main/java/bridge/model/validator/NextMoveValidator.java
@@ -5,7 +5,7 @@ import bridge.model.Direction;
 public class NextMoveValidator implements Validator {
     @Override
     public void validateInput(String input) {
-        if (!input.equals(Direction.UP.getValue()) || !input.equals(Direction.DOWN.getValue())) {
+        if (!input.equals(Direction.UP.getValue()) && !input.equals(Direction.DOWN.getValue())) {
             throw new IllegalArgumentException();
         }
     }
