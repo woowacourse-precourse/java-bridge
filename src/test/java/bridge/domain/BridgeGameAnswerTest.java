59cb4e75429c387ef4ca8b38f2aaeed3738887eb
slbin-park <smpts00@naver.com>
2022-11-19 00:15:24 +0900

    test(BridgeGameAnswerTest) : 기능 변경으로 테스트 코드 변경
    - check_all_collect_result 마지막 인덱스 제외하고 문자열 뒤에 "|" 문자열 추가

---
+++ src/test/java/bridge/domain/BridgeGameAnswerTest.java
@@ -0,0 +1,47 @@
+package bridge.domain;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BridgeGameAnswerTest {
+
+    private BridgeGameAnswer bridgeGameAnswer;
+    private static final List<String> answer = List.of("U", "D", "U", "D");
+
+    public BridgeGameAnswerTest() {
+        bridgeGameAnswer = new BridgeGameAnswer(answer);
+    }
+
+    @DisplayName("정답을 확인하는 기능 테스트")
+    @Test
+    public void is_answer_test() {
+        String collect = "U";
+        String wrong = "D";
+        int index = 0;
+        boolean answer = true;
+        boolean wrongAnswer = false;
+        assertAll(
+                () -> assertEquals(bridgeGameAnswer.isAnswer(collect, index), answer),
+                () -> assertEquals(bridgeGameAnswer.isAnswer(wrong, index), wrongAnswer)
+        );
+    }
+
+    @DisplayName("정답을 가져오는 테스트")
+    @Test
+    public void check_all_collect_result() {
+        List<List<String>> answer = new ArrayList<>();
+        answer.add(List.of(" O |", "   |"));
+        answer.add(List.of("   |", " O |"));
+        answer.add(List.of(" O |", "   |"));
+        answer.add(List.of("   ", " O "));
+        List<String> history = List.of("U", "D", "U", "D");
+        List<List<String>> return_answer = bridgeGameAnswer.getResultByHistory(history);
+        assertEquals(answer,return_answer);
+    }
+}
