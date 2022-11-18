7b964e00df7db0ab6e943cfb4b563abbff65f6d7
wugawuga <dnr3655@naver.com>
2022-11-18 14:46:57 +0900

    feat: 랜덤 다리 생성

--- src/main/java/bridge/BridgeMaker.java
+++ src/main/java/bridge/BridgeMaker.java
@@ -1,10 +1,9 @@
 package bridge;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
-/**
- * 다리의 길이를 입력 받아서 다리를 생성해주는 역할을 한다.
- */
 public class BridgeMaker {
 
     private final BridgeNumberGenerator bridgeNumberGenerator;
@@ -13,11 +12,25 @@ public class BridgeMaker {
         this.bridgeNumberGenerator = bridgeNumberGenerator;
     }
 
-    /**
-     * @param size 다리의 길이
-     * @return 입력받은 길이에 해당하는 다리 모양. 위 칸이면 "U", 아래 칸이면 "D"로 표현해야 한다.
-     */
     public List<String> makeBridge(int size) {
-        return null;
+        List<Integer> randomNumbers = makeRandomIntegers(size);
+        return randomNumbers.stream()
+                .map(this::changeUpDown)
+                .collect(Collectors.toList());
+    }
+
+    private List<Integer> makeRandomIntegers(int size) {
+        List<Integer> random = new ArrayList<>();
+        for (int i = 0; i < size; i++) {
+            random.add(bridgeNumberGenerator.generate());
+        }
+        return random;
+    }
+
+    private String changeUpDown(int random) {
+        if (random == 0) {
+            return "U";
+        }
+        return "D";
     }
 }
--- src/main/java/bridge/GameController.java
+++ src/main/java/bridge/GameController.java
@@ -1,11 +1,18 @@
 package bridge;
 
+import java.util.List;
+
 public class GameController {
     private final InputView input = new InputView();
     private final OutputView output = new OutputView();
+    private final BridgeMaker maker = new BridgeMaker(new BridgeRandomNumberGenerator());
 
     public void start() {
         output.printStart();
+        makeBridge();
+    }
+
+    private List<String> makeBridge() {
         output.printInputBridgeSize();
         int bridgeSize;
         while (true) {
@@ -16,5 +23,6 @@ public class GameController {
                 System.out.println("[ERROR]" + e.getMessage());
             }
         }
+        return maker.makeBridge(bridgeSize);
     }
 }
