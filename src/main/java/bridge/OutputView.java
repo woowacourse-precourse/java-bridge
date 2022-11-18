58248f18f05abb737a60d833625c297d647235c6
eyeofsol <kingjihong@naver.com>
2022-11-18 13:32:59 +0900

    feat(OutputView): 현재까지 이동한 다리의 상태 출력 뷰 구현
    브릿지와 플레이어의 정보(List<Sting>을 받아와서 출력해주는 뷰를 구현했습니다.
    윗다리와 아랫다리를 구분해서 출력하였고, 메서드로 분리했습니다.

--- src/main/java/bridge/OutputView.java
+++ src/main/java/bridge/OutputView.java
@@ -1,5 +1,7 @@
 package bridge;
 
+import java.util.List;
+
 /**
  * 사용자에게 게임 진행 상황과 결과를 출력하는 역할을 한다.
  */
@@ -10,7 +12,56 @@ public class OutputView {
      * <p>
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void printMap() {
+    public void printMap(List<String> bridge, List<String> player) {
+        String status;
+
+        printUpperBridge(bridge, player);
+        printLowwerBridge(bridge, player);
+    }
+    private void printUpperBridge(List<String> bridge, List<String> player) {
+        System.out.print("[");
+        for (int i = 0; i < player.size(); i++) {
+            printUpperBridgeStatus(bridge.get(i), player.get(i));
+            if (i != (player.size() - 1)) {
+                System.out.print("|");
+            }
+        }
+        System.out.print("]\n");
+    }
+
+    private void printUpperBridgeStatus(String bridgeStatus, String playerMovement) {
+        if (bridgeStatus.equals("U") && bridgeStatus.equals(playerMovement)) {
+            System.out.print(" O ");
+            return;
+        }
+        if (bridgeStatus.equals("U") && !bridgeStatus.equals(playerMovement)) {
+            System.out.print(" X ");
+            return;
+        }
+        System.out.print("   ");
+    }
+
+    private void printLowwerBridge(List<String> bridge, List<String> player) {
+        System.out.print("[");
+        for (int i = 0; i < player.size(); i++) {
+            printLowerBridgeStatus(bridge.get(i), player.get(i));
+            if (i != (player.size() - 1)) {
+                System.out.print("|");
+            }
+        }
+        System.out.print("]\n");
+    }
+
+    private void printLowerBridgeStatus(String bridgeStatus, String playerMovement) {
+        if (bridgeStatus.equals("D") && bridgeStatus.equals(playerMovement)) {
+            System.out.print(" O ");
+            return;
+        }
+        if (bridgeStatus.equals("D") && !bridgeStatus.equals(playerMovement)) {
+            System.out.print(" X ");
+            return;
+        }
+        System.out.print("   ");
     }
 
     /**
@@ -20,4 +71,5 @@ public class OutputView {
      */
     public void printResult() {
     }
+
 }
