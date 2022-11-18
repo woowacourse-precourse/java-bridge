fd06afd41f2725c6d1fc3f6fcd5d5285c59b3808
saint6839 <saint6839@gmail.com>
2022-11-18 14:51:31 +0900

    feat(domain) : 다리에 대한 데이터를 담는 Bridge 클래스 추가
    - 다리 길이가 3이상 20이하인지 유효성을 검증하는 메서드 추가

---
+++ src/main/java/bridge/Bridge.java
@@ -0,0 +1,18 @@
+package bridge;
+
+public class Bridge {
+    private static final String INVALID_LENGTH_RANGE_MESSAGE = "[ERROR] 다리 길이는 3이상 20이하의 숫자를 입력해주세요.";
+    int length;
+
+    public Bridge(int length) {
+        validateLengthRange(length);
+        this.length = length;
+    }
+
+    private void validateLengthRange(int length) {
+        if (length >= 3 && length <= 20) {
+            return;
+        }
+        throw new IllegalArgumentException(INVALID_LENGTH_RANGE_MESSAGE);
+    }
+}
