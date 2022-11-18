cd5516be3d1e4a7074f0c5b75aa061287c6494e9
kimjeonghan <[rlawjdgks318@naver.com]>
2022-11-18 16:57:35 +0900

    feat(OutputView): 현재까지 이동한 다리 상태를 형식에 맞춰 출력하는 기능 추가

--- src/main/java/bridge/view/OutputView.java
+++ src/main/java/bridge/view/OutputView.java
@@ -1,17 +1,28 @@
 package bridge.view;
 
+import static bridge.message.MessageConstants.*;
+
+import java.util.List;
+
+import bridge.domain.ResultMessageStatus;
+
 /**
  * 사용자에게 게임 진행 상황과 결과를 출력하는 역할을 한다.
  */
 public class OutputView {
 
-    /**
-     * 현재까지 이동한 다리의 상태를 정해진 형식에 맞춰 출력한다.
-     * <p>
-     * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
-     */
-    public void printMap() {
-    }
+	/**
+	 * 현재까지 이동한 다리의 상태를 정해진 형식에 맞춰 출력한다.
+	 * <p>
+	 * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
+	 */
+	public void printMap(List<ResultMessageStatus> bridgeStatuses) {
+		String upFloorResultMessage = makeUpFloorMessage(bridgeStatuses);
+		String downFloorResultMessage = makeDownFloorMessage(bridgeStatuses);
+
+		System.out.println(upFloorResultMessage);
+		System.out.println(downFloorResultMessage + "\n");
+	}
 
     /**
      * 게임의 최종 결과를 정해진 형식에 맞춰 출력한다.
@@ -20,4 +31,26 @@ public class OutputView {
      */
     public void printResult() {
     }
+
+	private String makeUpFloorMessage(List<ResultMessageStatus> bridgeStatuses) {
+		StringBuilder result = new StringBuilder();
+		result.append(BRIDGE_START_SYMBOL);
+		for (ResultMessageStatus bridgeStatus : bridgeStatuses) {
+			result.append(" ").append(bridgeStatus.getUpFloor()).append(" ").append(BRIDGE_MIDDLE_SYMBOL);
+		}
+		result.setLength(result.length() - 1);
+		result.append(BRIDGE_END_SYMBOL);
+		return result.toString();
+	}
+
+	private String makeDownFloorMessage(List<ResultMessageStatus> bridgeStatuses) {
+		StringBuilder result = new StringBuilder();
+		result.append(BRIDGE_START_SYMBOL);
+		for (ResultMessageStatus bridgeStatus : bridgeStatuses) {
+			result.append(" ").append(bridgeStatus.getDownFloor()).append(" ").append(BRIDGE_MIDDLE_SYMBOL);
+		}
+		result.setLength(result.length() - 1);
+		result.append(BRIDGE_END_SYMBOL);
+		return result.toString();
+	}
 }
