ea36eaac97b83258d2b5f12aa46b04f826f73446
woo-chang <clllickme@naver.com>
2022-11-18 18:02:33 +0900

    feat(BridgeGame): 다리를 이동하는 기능 구현

--- src/main/java/bridge/service/BridgeGame.java
+++ src/main/java/bridge/service/BridgeGame.java
@@ -1,16 +1,31 @@
 package bridge.service;
 
+import bridge.model.Bridge;
+import bridge.model.Direction;
+import bridge.model.GameCondition;
+import bridge.model.Player;
+
 /**
  * 다리 건너기 게임을 관리하는 클래스
  */
 public class BridgeGame {
+
+    private final Bridge bridge;
+    private final Player player = new Player();
+
+    public BridgeGame(Bridge bridge) {
+        this.bridge = bridge;
+    }
 
     /**
      * 사용자가 칸을 이동할 때 사용하는 메서드
      * <p>
      * 이동을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void move() {
+    public boolean move(Direction direction) {
+        bridge.validatePlayerStatus(player);
+        player.move(direction);
+        return !bridge.isDone(player);
     }
 
     /**
@@ -18,6 +33,8 @@ public class BridgeGame {
      * <p>
      * 재시작을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void retry() {
+    public void retry(GameCondition condition) {
+
     }
+
 }
