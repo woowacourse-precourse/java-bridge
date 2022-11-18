a1ec0b23c0d6c9986253587f968a7ac42a9b30b6
realslimtaek <89964264+realslimtaek@users.noreply.github.com>
2022-11-18 13:08:41 +0900

    feat : 다리 출력을 위한 구현 완료

--- src/main/java/bridge/BridgeMaker.java
+++ src/main/java/bridge/BridgeMaker.java
@@ -2,6 +2,7 @@ package bridge;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 /**
  * 다리의 길이를 입력 받아서 다리를 생성해주는 역할을 한다.
@@ -10,8 +11,11 @@ public class BridgeMaker {
 
 
     private final BridgeNumberGenerator bridgeNumberGenerator;
-    static int now_size = 1;
+    static int now_size = 0;
     static List<String> bridge_answer = new ArrayList<>();
+    static StringBuilder upBridge = new StringBuilder();
+    static StringBuilder downBridge = new StringBuilder();
+
     public BridgeMaker(BridgeNumberGenerator bridgeNumberGenerator) {
         this.bridgeNumberGenerator = bridgeNumberGenerator;
     }
@@ -21,6 +25,7 @@ public class BridgeMaker {
      * @return 입력받은 길이에 해당하는 다리 모양. 위 칸이면 "U", 아래 칸이면 "D"로 표현해야 한다.
      */
     public List<String> makeBridge(int size) {
+
         BridgeRandomNumberGenerator bridgeRandomNumberGenerator = new BridgeRandomNumberGenerator();
         for (int i = 0; i < size; i++) {
             int a = bridgeRandomNumberGenerator.generate();
@@ -32,9 +37,53 @@ public class BridgeMaker {
         return bridge_answer;
     }
 
-    public String resultBridge(int size){
+    public void resultBridge(List<String> user_answer ) {
+        System.out.println("나우 사이즈 = " + now_size);
+        upBridge.setLength(0);
+        downBridge.setLength(0);
+        upBridge.append("[ ");
+        downBridge.append("[ ");
+            for (int i = 0; i < now_size + 1; i++) {
+                System.out.println("브릿지 앤서 = " + bridge_answer.get(i) + ", 유저앤서 = " + user_answer.get(i));
+                if (Objects.equals(bridge_answer.get(i), user_answer.get(i))) {
+                    if (Objects.equals(bridge_answer.get(i), "U")) {
+                        System.out.println("U 정답");
+                        upBridge.append("O ");
+                        downBridge.append("  ");
+                    } else if (Objects.equals(bridge_answer.get(i), "D")) {
+                        System.out.println("D 정답");
+                        upBridge.append("  ");
+                        downBridge.append("O ");
+                    }
+                } else {
+                    if (Objects.equals(user_answer.get(i), "U")) {
+                        System.out.println("U O답");
+                        upBridge.append("X ");
+                        downBridge.append("  ");
+                    } else if (Objects.equals(user_answer.get(i), "D")) {
+                        System.out.println("D O답");
+                        upBridge.append("  ");
+                        downBridge.append("X ");
+                    }
+                }
+                if (now_size == i) {
+                    System.out.println("U 정답 끝");
+                    upBridge.append("]");
+                    downBridge.append("]");
+                } else {
+                    System.out.println("U 정답 안끝");
+                    upBridge.append("| ");
+                    downBridge.append("| ");
+                }
+            }
 
-        return null;
+            now_size++;
+            showBridge();
+    }
+    public static void showBridge(){
+        System.out.println(upBridge.toString());
+        System.out.println(downBridge.toString());
+
     }
 
 
