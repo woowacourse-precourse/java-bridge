99ece9ffb3618066774110de745e481876fbfdf4
tdmdgh <tdmdgh@gmail.com>
2022-11-18 18:29:19 +0800

    feat(BridgeMaker): 다리 생성 매서드 구현

--- src/main/java/bridge/GameController.java
+++ src/main/java/bridge/GameController.java
@@ -1,15 +1,22 @@
 package bridge;
+
+import java.util.List;
+
 /**
  * 게임의 진행을 관리하는 역할을 한다.
  */
 public class GameController {
     public void begin(){
         OutputView outputView =new OutputView();
-        InputView inputView = new InputView();
         outputView.gameStartMessage();
         outputView.bridgeSizeMessage();
+
+        InputView inputView = new InputView();
         int size = inputView.readBridgeSize();
+        System.out.println(size);
 
-
+        BridgeMaker bridgeMaker = new BridgeMaker(new BridgeRandomNumberGenerator());
+        List<String> answer_bridge = bridgeMaker.makeBridge(size);
+        System.out.println(answer_bridge);
     }
 }
