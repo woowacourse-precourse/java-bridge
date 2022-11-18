c364906cf662ec2b6820c6232f8dd02252891bb5
ma-an-jong <alswhd1113@gmail.com>
2022-11-18 22:07:45 +0900

    fix(BridgeGameInfo): 다리 게임에 쓰일 전역 변수를 위한 클래스를 생성
    BREAKING CHANGE: 다리 게임에서 사용될 전역 변수를 모아둔 클래스를 생성
    InputView에 선언된 전역변수 모두 이동
    
    Before :
    
        InputViewTest
        {
            private static final Integer MIN_BRIDGE_SIZE = 3;
            private static final Integer MAX_BRIDGE_SIZE = 20;
            private static final String UPPER_MOVE = "U";
            private static final String LOWER_MOVE = "D";
            private static final String GAME_RESTART_COMMAND = "R";
            private static final String GAME_QUIT_COMMAND = "Q";
        }
    
    After :
    
        InputViewTest
        {
    
        }
    
        BridgeGameInfo
        {
            public static final Integer MIN_BRIDGE_SIZE = 3;
            public static final Integer MAX_BRIDGE_SIZE = 20;
            public static final String UPPER_MOVE = "U";
            public static final String LOWER_MOVE = "D";
            public static final String GAME_RESTART_COMMAND = "R";
            public static final String GAME_QUIT_COMMAND = "Q";
        }
    
        다리 게임에서 사용될 전역 변수를 관리하는 클래스를 생성

---
+++ src/main/java/bridge/util/BridgeGameInfo.java
@@ -0,0 +1,10 @@
+package bridge.util;
+
+public class BridgeGameInfo {
+    public static final Integer MIN_BRIDGE_SIZE = 3;
+    public static final Integer MAX_BRIDGE_SIZE = 20;
+    public static final String UPPER_MOVE = "U";
+    public static final String LOWER_MOVE = "D";
+    public static final String GAME_RESTART_COMMAND = "R";
+    public static final String GAME_QUIT_COMMAND = "Q";
+}
--- src/main/java/bridge/view/InputView.java
+++ src/main/java/bridge/view/InputView.java
@@ -1,22 +1,18 @@
 package bridge;
 
+import bridge.util.BridgeGameInfo;
 import bridge.util.Message;
 import camp.nextstep.edu.missionutils.Console;
 
 public class InputView {
 
-    private static final Integer MIN_BRIDGE_SIZE = 3;
-    private static final Integer MAX_BRIDGE_SIZE = 20;
-    private static final String UPPER_MOVE = "U";
-    private static final String LOWER_MOVE = "D";
-    private static final String GAME_RESTART_COMMAND = "R";
-    private static final String GAME_QUIT_COMMAND = "Q";
+
     private String readLine()
     {
         return Console.readLine();
     }
     private void validateBridgeSize(int bridgeSize) {
-        if(MIN_BRIDGE_SIZE <= bridgeSize && bridgeSize <= MAX_BRIDGE_SIZE) return;
+        if(BridgeGameInfo.MIN_BRIDGE_SIZE <= bridgeSize && bridgeSize <= BridgeGameInfo.MAX_BRIDGE_SIZE) return;
 
         throw new IllegalArgumentException(Message.ILLEGAL_BRIDGE_SIZE_ERROR_MESSAGE);
     }
@@ -32,7 +28,7 @@ public class InputView {
     }
     private void validateUserInputForMove(String userInputForMove)
     {
-        if(userInputForMove.equals(LOWER_MOVE) || userInputForMove.equals(UPPER_MOVE)) return;
+        if(userInputForMove.equals(BridgeGameInfo.LOWER_MOVE) || userInputForMove.equals(BridgeGameInfo.UPPER_MOVE)) return;
 
         throw new IllegalArgumentException(Message.ILLEGAL_MOVE_REQUEST_ERROR_MESSAGE);
     }
@@ -44,7 +40,7 @@ public class InputView {
     }
     private void validateGameCommand(String gameCommand)
     {
-        if(gameCommand.equals(GAME_RESTART_COMMAND) || gameCommand.equals(GAME_QUIT_COMMAND)) return;
+        if(gameCommand.equals(BridgeGameInfo.GAME_RESTART_COMMAND) || gameCommand.equals(BridgeGameInfo.GAME_QUIT_COMMAND)) return;
 
         throw new IllegalArgumentException(Message.ILLEGAL_GAME_COMMAND_ERROR_MESSAGE);
     }
