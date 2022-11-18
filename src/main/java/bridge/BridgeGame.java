f05819185b6e2508f65ddf4634333027865f1637
K-IMjihun <62210749+K-IMjihun@users.noreply.github.com>
2022-11-18 20:57:54 +0900

    BridgeGame.move // 이동할 위치와 랜덤 값 비교

--- src/main/java/bridge/BridgeGame.java
+++ src/main/java/bridge/BridgeGame.java
@@ -1,23 +1,50 @@
 package bridge;
 
+
+import java.util.List;
+
 /**
  * 다리 건너기 게임을 관리하는 클래스
+// * 제공된 BridgeGame 클래스를 활용해 구현해야 한다.
+// * BridgeGame에 필드(인스턴스 변수)를 추가할 수 있다.
+// * BridgeGame의 패키지는 변경할 수 있다.
+// * BridgeGame의 메서드의 이름은 변경할 수 없고, 인자와 반환 타입은 필요에 따라 추가하거나 변경할 수 있다.
+// * 게임 진행을 위해 필요한 메서드를 추가 하거나 변경할 수 있다.
  */
 public class BridgeGame {
 
-    /**
-     * 사용자가 칸을 이동할 때 사용하는 메서드
-     * <p>
-     * 이동을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
-     */
+    InputView inputView = new InputView();
+    OutputView outputView = new OutputView();
+
+    static String failOrTure ="성공";
+    static List<String> randomBridge;
+    static int count = 1;
+
     public void move() {
+        BridgeRandomNumberGenerator bridgeRandomNumberGenerator = new BridgeRandomNumberGenerator();
+        BridgeMaker bridgeMaker = new BridgeMaker(bridgeRandomNumberGenerator);
+        randomBridge = bridgeMaker.makeBridge(inputView.bridgeLength);
+        System.out.println(randomBridge);
+        for(int i = 0; i < randomBridge.size(); i++){
+            String upAndDown = inputView.readMoving();
+            if(!randomBridge.get(i).equals(upAndDown)) {
+                outputView.wrongPrintMap(randomBridge.get(i));
+                failOrTure = "실패";
+                break;
+            }
+            if(randomBridge.get(i).equals(upAndDown)){
+                outputView.printMap(randomBridge.get(i));
+                failOrTure = "성공";
+            }
+        }
+        if(failOrTure.equals("성공")){
+            outputView.printResult(failOrTure, count);
+        }
     }
 
-    /**
-     * 사용자가 게임을 다시 시도할 때 사용하는 메서드
-     * <p>
-     * 재시작을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
-     */
-    public void retry() {
+
+    public String retry() {
+        return null;
     }
+
 }
