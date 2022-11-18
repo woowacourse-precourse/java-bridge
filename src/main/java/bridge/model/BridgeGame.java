4035318310a5deae5e107b4a6195822924eda386
KNH <nh2004@naver.com>
2022-11-19 02:52:18 +0900

    refactor(BridgeGame) : Getter 추가

--- src/main/java/bridge/model/BridgeGame.java
+++ src/main/java/bridge/model/BridgeGame.java
@@ -6,14 +6,26 @@ import java.util.List;
  * 다리 건너기 게임을 관리하는 클래스
  */
 public class BridgeGame {
-    private int pointer;
+    private int userLocation;
     private final List<String> bridge;
     private GameState gameState;
 
     public BridgeGame(List<String> bridge) {
-        this.pointer = 0;
+        this.userLocation = 0;
         this.bridge = bridge;
         this.gameState = GameState.NOT_FINISH;
+    }
+
+    public int getUserLocation() {
+        return userLocation;
+    }
+
+    public List<String> getBridge() {
+        return bridge;
+    }
+
+    public GameState getGameState() {
+        return gameState;
     }
 
     /**
@@ -22,16 +34,16 @@ public class BridgeGame {
      * 이동을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
     public void move(String direction) {
-        if (! bridge.get(pointer).equals(direction))
+        if (! bridge.get(userLocation).equals(direction))
             gameState = GameState.FINISH_FAIL;
 
         if (gameState.equals(GameState.NOT_FINISH))
-            pointer++;
+            userLocation++;
         finishGame();
     }
 
     private void finishGame(){
-        if (this.pointer == this.bridge.size())
+        if (this.userLocation == this.bridge.size())
             gameState = GameState.FINISH_SUCCESS;
     }
 
