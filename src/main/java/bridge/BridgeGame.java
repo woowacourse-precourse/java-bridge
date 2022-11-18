cac087c5dff36903f5be4f9c2149ea170ccd6e9a
OhJintaek <sunshot@hanyang.ac.kr>
2022-11-18 23:37:37 +0900

    type:feat 현재 다리 상태를 저장하는 기능 추가

--- src/main/java/bridge/BridgeGame.java
+++ src/main/java/bridge/BridgeGame.java
@@ -8,12 +8,13 @@ import java.util.List;
  */
 public class BridgeGame {
     private final List<String> bridge;
-    private int tryNumber;
-    private List<String> selectedFootHold = new ArrayList<>();
+    private int tryNumber = 0;
+    private List<String> selectedFoothold = new ArrayList<>();
+    private List<String> upperRoad = new ArrayList<>();
+    private List<String> lowerRoad = new ArrayList<>();
 
     public BridgeGame(List<String> bridge) {
         this.bridge = bridge;
-        this.tryNumber = 0;
     }
 
     /**
@@ -23,20 +24,45 @@ public class BridgeGame {
      */
     public void move(String userInput) {
         if (userInput.equals(Command.UP.getValue())) {
-            selectedFootHold.add(Command.UP.getValue());
+            selectedFoothold.add(Command.UP.getValue());
         }
         if (userInput.equals(Command.DOWN.getValue())) {
-            selectedFootHold.add(Command.DOWN.getValue());
+            selectedFoothold.add(Command.DOWN.getValue());
         }
         tryNumber++;
     }
 
     public boolean isRightFoothold() {
         int index = tryNumber - 1;
-        if (selectedFootHold.get(index) == bridge.get(index)) {
+        if (selectedFoothold.get(index) == bridge.get(index)) {
             return true;
         }
         return false;
+    }
+
+    public void makeRoad() {
+        String result = Command.FAILURE.getValue();
+        if (isRightFoothold()) {
+            result = Command.SUCCESS.getValue();
+        }
+        makeUpperRoad(result);
+        makeLowerRoad(result);
+    }
+
+    public void makeUpperRoad(String result) {
+        if (selectedFoothold.get(selectedFoothold.size() - 1) == Command.UP.getValue()) {
+            upperRoad.add(result);
+            return;
+        }
+        upperRoad.add(" ");
+    }
+
+    public void makeLowerRoad(String result) {
+        if (selectedFoothold.get(selectedFoothold.size() - 1) == Command.DOWN.getValue()) {
+            lowerRoad.add(result);
+            return;
+        }
+        lowerRoad.add(" ");
     }
 
     public boolean isGameEnd() {
