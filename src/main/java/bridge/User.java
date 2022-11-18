5856186d72ffd579e2f7ea4c409b7b16b531eeff
JeongHunHui <deca3248@gmail.com>
2022-11-18 12:09:22 +0900

    feat: 이동상태를 저장하고, 옳바르게 이동했는지 확인할수있는 User클래스 생성

---
+++ src/main/java/bridge/User.java
@@ -0,0 +1,21 @@
+package bridge;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class User {
+  private List<String> moveStatus;
+
+  public User() {
+    this.moveStatus = new ArrayList<>();
+  }
+
+  public void move(String direction) {
+    moveStatus.add(direction);
+  }
+
+  public boolean isCorrectlyMove(List<String> bridge) {
+    final int index = moveStatus.size() - 1;
+    return bridge.get(index).equals(moveStatus.get(index));
+  }
+}
