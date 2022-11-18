46551c613bf4179f0820ebdcfe5754e3a53f1897
ddingmin <ddingminyt@gmail.com>
2022-11-19 05:41:06 +0900

    feat(java-bridge): Main 다리 게임 객체 생성

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -1,8 +1,15 @@
 package bridge;
 
+import bridge.domain.BridgeGame;
+import bridge.ui.InputView;
+import bridge.ui.OutputView;
+
 public class Application {
+    private static final InputView inputView = new InputView();
+    private static final OutputView outputView = new OutputView();
 
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        BridgeGame bridgeGame = new BridgeGame(inputView.readBridgeSize());
     }
 }
