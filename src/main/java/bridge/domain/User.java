8a9541602de59f148f0ba591f5ec7df8e1aa7342
sosow0212 <sosow0212@naver.com>
2022-11-18 14:58:31 +0900

    feat : (User) 생성자와 Getter, Setter, 상태 변경 기능 추가

--- src/main/java/bridge/domain/User.java
+++ src/main/java/bridge/domain/User.java
@@ -3,4 +3,28 @@ package bridge.domain;
 public class User {
     private boolean isGameDoneStatus;
     private int gameTryCount;
+
+    public boolean isGameDoneStatus() {
+        return isGameDoneStatus;
+    }
+
+    public void setGameDoneStatus(boolean gameDoneStatus) {
+        isGameDoneStatus = gameDoneStatus;
+    }
+
+    public void gameDoneSuccess() {
+        setGameDoneStatus(true);
+    }
+
+    public int getGameTryCount() {
+        return gameTryCount;
+    }
+
+    public void setGameTryCount(int gameTryCount) {
+        this.gameTryCount = gameTryCount;
+    }
+
+    public void gameRetry() {
+        setGameTryCount(this.getGameTryCount() + 1);
+    }
 }
