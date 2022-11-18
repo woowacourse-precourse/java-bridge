cb88957c8ce301e0598df212f72de2bb4cbeac09
kdkdhoho <hkim4410@naver.com>
2022-11-18 16:54:45 +0900

    refactor(Message): 이동할 칸을 선택해주세요 구문 앞에 개행 추가

--- src/main/java/bridge/model/constant/Message.java
+++ src/main/java/bridge/model/constant/Message.java
@@ -3,7 +3,7 @@ package bridge.model.constant;
 public class Message {
     public static final String START_MESSAGE = "다리 건너기 게임을 시작합니다.\n";
     public static final String INPUT_BRIDGE_LENGTH = "다리의 길이를 입력해주세요.";
-    public static final String INPUT_MOVING = "이동할 칸을 선택해주세요. (위: U, 아래: D)";
+    public static final String INPUT_MOVING = "\n이동할 칸을 선택해주세요. (위: U, 아래: D)";
     public static final String INPUT_RETRY_COMMAND = "게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)";
     public static final String ERROR_PREFIX = "[ERROR] ";
     public static final String ERROR_BRIDGE_SIZE = "잘못 입력했습니다. 3 이상 20 이하의 숫자를 입력해주세요.";
