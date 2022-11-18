7f9192f3fe33d8df3cef0d17cf33ad80680f47a4
taeyun-work <wmfajr1357@gmail.com>
2022-11-18 20:49:32 +0900

    feat: 게임을 시작하는 문구 출력하는 기능 구현
    OutputView클래스에 게임을 시작하는 문구를 출력하는 메소드를 구현함

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -2,7 +2,10 @@ package bridge;
 
 public class Application {
 
+    private static OutputView outputView = new OutputView();
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+
+        outputView.printStartGame();
     }
 }
