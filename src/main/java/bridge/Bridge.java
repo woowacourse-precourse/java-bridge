51194ef8b63c3a43ce780d6bf3b1ae7f63b4d6ca
Minjune <minjune8506@gmail.com>
2022-11-18 12:02:34 +0900

    feat(Bridge Game): 이동할 수 있는 칸인지 검사하는 기능 구현

--- src/main/java/bridge/Bridge.java
+++ src/main/java/bridge/Bridge.java
@@ -8,4 +8,12 @@ public class Bridge {
     public Bridge(List<String> bridge) {
         this.bridge = bridge;
     }
+
+    public boolean canCross(int location, String direction) {
+        return bridge.get(location).equals(direction);
+    }
+
+    public int size() {
+        return bridge.size();
+    }
 }
