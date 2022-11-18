4f7750e1eae25a9050099fc85aa372290ca14b1e
jinny-codes <77732679+jinny-codes@users.noreply.github.com>
2022-11-18 13:53:07 +0900

    refactor(readMoving): 과거의 moves를 저장할 수 있도록 input 메소드 리팩토링 (작동하는지 미지수)

--- src/main/java/bridge/InputView.java
+++ src/main/java/bridge/InputView.java
@@ -1,6 +1,8 @@
 package bridge;
 import camp.nextstep.edu.missionutils.Console;
 
+import java.util.List;
+
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
  */
@@ -18,9 +20,10 @@ public class InputView {
     /**
      * 사용자가 이동할 칸을 입력받는다.
      */
-    public boolean readMoving() {
+    public boolean readMoving(List<String> moves) {
         String moveString = Console.readLine();
         validator.isValidMove(moveString);
+        moves.add(moveString); // does this work?
         return (moveString.equals("U"));
     }
 
