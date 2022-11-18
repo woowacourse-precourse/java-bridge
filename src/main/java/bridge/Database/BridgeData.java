469bbf1d9ad46090ec4b7a08e16cbeadb3e79a9c
Nitroblack1 <109022435+Nitroblack1@users.noreply.github.com>
2022-11-18 16:52:53 +0900

    feat(main\...\Database) : declare BridgeData fields
    - bridge stores bridge design
    - bridgeLayOutByUser stores bridge design until user found out
    - totalAttempt stores user's attempt

---
+++ src/main/java/bridge/Database/BridgeData.java
@@ -0,0 +1,9 @@
+package bridge.Database;
+
+import java.util.List;
+
+public class BridgeData {
+    private List<Integer> bridge;
+    private List<Integer> bridgeLayOutByUser;
+    private int totalAttempt;
+}
