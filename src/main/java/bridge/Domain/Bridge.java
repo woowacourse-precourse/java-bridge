013e1cb458dd575b4ad291e65ea4b0656e749bed
Jenn Kim <grdd0419@gmail.com>
2022-11-19 02:04:05 +0900

    feat: 다리 정보를 저장하는 Bridge 클래스 생성

---
+++ src/main/java/bridge/Domain/Bridge.java
@@ -0,0 +1,11 @@
+package bridge.Domain;
+
+import java.util.List;
+
+public class Bridge {
+    private final List<String> spaces;
+
+    public Bridge(List<String> spaces) {
+        this.spaces = spaces;
+    }
+}
