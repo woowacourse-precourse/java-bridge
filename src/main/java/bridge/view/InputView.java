6b7bfc5868ca97fec5956d4e26105cab5fca08b4
youngkyunIm <yee0230@naver.com>
2022-11-18 21:16:30 +0900

    chore(InputView): remove import static

--- src/main/java/bridge/view/InputView.java
+++ src/main/java/bridge/view/InputView.java
@@ -1,6 +1,6 @@
 package bridge.view;
 
-import static camp.nextstep.edu.missionutils.Console.readLine;
+import camp.nextstep.edu.missionutils.Console;
 
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
@@ -14,7 +14,7 @@ public class InputView {
      */
     public int readBridgeSize() {
         System.out.println("다리의 길이를 입력해주세요.");
-        return inputHandler.stringToInt(readLine());
+        return inputHandler.stringToInt(Console.readLine());
     }
 
     /**
