27870ad3623d8e5d0f32f5524a9a8645adca8456
watterricecake <fruturum@nate.com>
2022-11-18 21:35:20 +0900

    feat: 구현한 게임 시작, 길이입력 다리생성 까지 application에 추가

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -1,8 +1,16 @@
 package bridge;
 
+import bridge.view.InputView;
+
 public class Application {
 
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        System.out.println("다리 건너기 게임을 시작합니다.");
+        InputView inputView = new InputView(new InputConsole());
+        BridgeMaker bridgeMaker = new BridgeMaker(new BridgeRandomNumberGenerator());
+        bridgeMaker.makeBridge(inputView.readBridgeSize());
+
+
     }
 }
