5abbebe137f0c589f1d2d05333b8028d27c4c2a8
UJ15 <dbwogml15@naver.com>
2022-11-18 16:50:13 +0900

    fix : change readBridgeSize method in InputView

--- src/main/java/bridge/view/InputView.java
+++ src/main/java/bridge/view/InputView.java
@@ -1,5 +1,8 @@
 package bridge.view;
 
+import bridge.model.BridgeSize;
+import camp.nextstep.edu.missionutils.Console;
+
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
  */
@@ -8,8 +11,10 @@ public class InputView {
     /**
      * 다리의 길이를 입력받는다.
      */
-    public int readBridgeSize() {
-        return 0;
+    public BridgeSize readBridgeSize() {
+        String inputSize = Console.readLine();
+
+        return new BridgeSize(inputSize);
     }
 
     /**
