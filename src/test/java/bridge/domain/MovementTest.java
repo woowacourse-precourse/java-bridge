d2e07af137a392543249acec5bd3bac1974fc13b
Jimin <thswlals0219@gmail.com>
2022-11-18 15:27:07 +0900

    test(Movement): 이동 값이 U 또는 D 상태만 가지는지 검증 테스트 추가

---
+++ src/test/java/bridge/domain/MovementTest.java
@@ -0,0 +1,26 @@
+package bridge.domain;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+class MovementTest {
+    @DisplayName("이동은 U 또는 D 상태만 가질 수 있다.")
+    @ParameterizedTest
+    @ValueSource(strings = {"D", "U"})
+    void 사용자_이동값_검증(String move) {
+
+        assertThat(Movement.valueOf(move) instanceof Movement).isEqualTo(true);
+    }
+
+    @DisplayName("이동은 U 또는 D 상태가 아닌 경우 예외가 발생한다.")
+    @ParameterizedTest
+    @ValueSource(strings = {"A", "1"})
+    void 사용자_이동값_예외처리(String move) {
+        assertThatThrownBy(() -> Movement.toMovement(move))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+}
