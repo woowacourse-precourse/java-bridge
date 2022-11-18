317ac752a99b4c7e57087f368b69e5e9d87ca36f
tjrqhd121@naver.com <tjrqhd121@naver.com>
2022-11-19 02:24:38 +0900

    feat(InputView): 입력 메서드 구현
    입력 메서드 구현 및 검증메서드 구현
    ErrorMessage: 에러 처리 관련 메세지 상수
    InputMessage: 입력 메서지 관련 상수

--- src/main/java/bridge/InputView.java
+++ src/main/java/bridge/InputView.java
@@ -1,5 +1,11 @@
 package bridge;
 
+import camp.nextstep.edu.missionutils.Console;
+import domain.ErrorMessage;
+import domain.InputMessage;
+
+import java.util.regex.Pattern;
+
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
  */
@@ -9,20 +15,85 @@ public class InputView {
      * 다리의 길이를 입력받는다.
      */
     public int readBridgeSize() {
-        return 0;
+        startMessage();
+        String size = Console.readLine();
+        validateBridge(size);
+        return Integer.parseInt(size);
     }
 
+    private void startMessage() {
+        System.out.println(InputMessage.START_MESSAGE.getMessage());
+        System.out.println();
+        System.out.println(InputMessage.INPUT_BRIDGE_MESSAGE.getMessage());
+    }
+
+    private void validateBridge(String bridge) {
+        isNumber(bridge);
+        isOutOfSize(bridge);
+    }
+
+    private void isNumber(String bridge) {
+        String regex = "^\\d*$";
+        if(Pattern.matches(regex,bridge)){
+            throw new IllegalArgumentException(ErrorMessage.NOT_NUMBER.getMessage());
+        }
+    }
+
+    private void isOutOfSize(String bridge) {
+        int number = Integer.parseInt(bridge);
+        if(number < 3 || number > 20) {
+            throw new IllegalArgumentException(ErrorMessage.NOT_BRIDGE_SIZE.getMessage());
+        }
+    }
     /**
      * 사용자가 이동할 칸을 입력받는다.
      */
     public String readMoving() {
-        return null;
+        movingMessage();
+        String command = Console.readLine();
+        validateMovingCommand(command);
+        return command;
+    }
+
+    private void movingMessage() {
+        System.out.println();
+        System.out.println(InputMessage.INPUT_MOVING_MESSAGE.getMessage());
+    }
+
+    private void validateMovingCommand(String command) {
+        isMovingCommand(command);
+    }
+
+    private void isMovingCommand(String command) {
+        String regex = "^[DU]$";
+        if(Pattern.matches(regex, command)){
+            throw new IllegalArgumentException(ErrorMessage.NOT_MATCHING_MOVING_COMMAND.getMessage());
+        }
     }
 
     /**
      * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
      */
     public String readGameCommand() {
+        endMessage();
+        String command = Console.readLine();
+        validateGameCommand(command);
         return null;
+    }
+
+    private void endMessage() {
+        System.out.println();
+        System.out.println(InputMessage.INPUT_GAME_MESSAGE.getMessage());
+    }
+
+    private void validateGameCommand(String command) {
+        isGameCommand(command);
+    }
+
+    private void isGameCommand(String command) {
+        String regex = "^[RQ]$";
+        if(Pattern.matches(regex, command)) {
+            throw new IllegalArgumentException(ErrorMessage.NOT_MATCHING_GAME_COMMAND.getMessage());
+        }
     }
 }
