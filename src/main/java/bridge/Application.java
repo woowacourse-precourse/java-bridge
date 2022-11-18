5c9a7f2fb429c5425150be65e96efbe175b5293f
bando99 <bando991225@gmail.com>
2022-11-18 21:06:47 +0900

    게임 진행 과정

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -1,8 +1,42 @@
 package bridge;
 
-public class Application {
+import org.w3c.dom.stylesheets.LinkStyle;
 
+import java.util.ArrayList;
+import java.util.List;
+
+public class Application {
+    static BridgeNumberGenerator bridgeNumberGenerator = new BridgeRandomNumberGenerator();
+    static BridgeMaker bridgeMaker = new BridgeMaker(bridgeNumberGenerator);
+    static InputView inputView = new InputView();
+    static OutputView outputView = new OutputView();
+    static final boolean START = true;
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        List<String> bridge;
+        int bridgeSize;
+        boolean state = START;
+
+        while(state){ // retry
+            //사이즈 입력
+            bridgeSize = inputView.readBridgeSize();
+            // bridge를 만듦
+            bridge = bridgeMaker.makeBridge(bridgeSize);
+            // bridge를 이용한 게임 생성
+            BridgeGame bridgeGame = new BridgeGame(bridge);
+            // bridge move
+            boolean ingameState = true;
+            while(ingameState){//move
+                String direction = inputView.readMoving();
+                ingameState = bridgeGame.move(direction);
+//                outputView.printMap();
+            }
+//            outputView.printResult();
+
+            state = bridgeGame.retry();
+
+
+        }
+
+
     }
 }
