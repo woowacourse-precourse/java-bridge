12ea961afc7ef73fd0fe79f5a07ccb464df953f3
RumbleKAT <reki318@naver.com>
2022-11-18 23:47:40 +0900

    (feat) 1-1. 다리 사이즈 입력 및 예외처리

--- src/main/java/bridge/InputView.java
+++ src/main/java/bridge/InputView.java
@@ -1,5 +1,9 @@
 package bridge;
 
+import camp.nextstep.edu.missionutils.Console;
+
+import java.util.IllegalFormatException;
+
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
  */
@@ -9,7 +13,13 @@ public class InputView {
      * 다리의 길이를 입력받는다.
      */
     public int readBridgeSize() {
-        return 0;
+        int size = 0;
+        try{
+            size = Integer.parseInt(Console.readLine());
+        }catch(NumberFormatException e){
+            System.out.println("[ERROR] 잘못된 입력 형식 입니다.");
+        }
+        return size;
     }
 
     /**
