55a315da2c189911730c3ef696d419ad9defb383
WonJeongYeon <wonjungyoun@naver.com>
2022-11-18 18:16:07 +0900

    refactor: 예외처리 코드 전용 클래스로 리팩토링

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -10,9 +10,7 @@ public class Application {
     public static void main(String[] args) {
         initClass();
         System.out.println("다리 건너기 게임을 시작합니다.");
-        int size = inputSize();
-
-        final List<String> bridge = bridgeMaker.makeBridge(size);
+        final List<String> bridge = bridgeMaker.makeBridge(inputSize());
         System.out.println(bridge);
         BridgeGame.gameProcess(bridge);
     }
@@ -26,47 +24,26 @@ public class Application {
 
     public static int inputSize() {
         String readValue = inputView.readBridgeSize();
-        int size;
-        try {
-            size = checkSizeType(readValue);
-            checkSizeRange(size);
-        } catch (IllegalArgumentException e) {
-            System.out.println(e.getMessage());
-            size = inputSize();
-        }
-        return size;
-    }
-
-    public static int checkSizeType(String str) {
-        int size;
-        try {
-            size = Integer.parseInt(str);
-        } catch (NumberFormatException e) {
-            throw new IllegalArgumentException("[ERROR] 다리 길이는 숫자만 입력할 수 있습니다.");
-        }
-        return size;
-    }
-
-    public static void checkSizeRange(int size) {
-        if (size < 3 || size > 20)
-            throw new IllegalArgumentException("[ERROR] 다리 길이는 3~20 사이의 숫자여야 합니다.");
+        return ExceptionHandler.sizeExceptionHandler(readValue);
     }
 
     public static String inputMove() {
-        return inputView.readMoving();
+        String move = inputView.readMoving();
+        ExceptionHandler.moveExceptionHandler(move);
+        return move;
     }
 
     public static void printMap(List<List<String>> userPlaying) {
         outputView.printMap(userPlaying);
     }
 
-    public static String inputRetryCommand() {
-        return inputView.readGameCommand();
+    public static String inputGameCommand() {
+        String gameCommand = inputView.readGameCommand();
+        ExceptionHandler.gameCommandExceptionHandler(gameCommand);
+        return gameCommand;
     }
 
     public static void printResult(String isSuccess, int tryCount, List<List<String>> userPlaying) {
         outputView.printResult(isSuccess, tryCount, userPlaying);
     }
-
-
 }
