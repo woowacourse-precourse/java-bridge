21a7e7b7d8d6b939d70f2eb51e2d9e22232220f2
sosow0212 <sosow0212@naver.com>
2022-11-18 16:23:19 +0900

    feat : (BridgeService) 다리를 생성하고 반환해주는 메소드 구현

--- src/main/java/bridge/service/BridgeService.java
+++ src/main/java/bridge/service/BridgeService.java
@@ -1,7 +1,20 @@
 package bridge.service;
 
+import bridge.BridgeMaker;
+import bridge.BridgeRandomNumberGenerator;
+import bridge.domain.Bridge;
+
 public class BridgeService {
     private final InputService inputService = new InputService();
+    private final BridgeRandomNumberGenerator bridgeRandomNumberGenerator = new BridgeRandomNumberGenerator();
+    private final BridgeMaker bridgeMaker = new BridgeMaker(bridgeRandomNumberGenerator);
+
+    public Bridge bridgeMaker() {
+        gameStart();
+        int bridgeSize = getBridgeSize();
+        Bridge bridge = new Bridge(bridgeMaker.makeBridge(bridgeSize));
+        return bridge;
+    }
 
     public void gameStart() {
         inputService.printGameStart();
