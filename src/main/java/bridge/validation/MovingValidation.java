fa90897ffb468dd0208f78503b40a7e6312bd03a
ykmxxi <ykmxxi97@gmail.com>
2022-11-18 15:06:39 +0900

    feat: 이동할 칸 입력 예외 처리기능[빈 문자열 처리] 구현

--- src/main/java/bridge/validation/MovingValidation.java
+++ src/main/java/bridge/validation/MovingValidation.java
@@ -1,8 +1,15 @@
 package bridge.validation;
 
+import bridge.validation.enums.ErrorMessage;
+
 public class MovingValidation {
+    private ErrorMessage message;
 
     public void isNull(String moving) {
+        if (moving.isEmpty()) {
+            message = ErrorMessage.valueOf("MOVING_NULL_EXCEPTION");
+            throw new NullPointerException(message.getMessage());
+        }
     }
 
     public void isBlank(String moving) {
