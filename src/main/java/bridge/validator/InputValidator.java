b5d0278c5fcef1e4602b60ba97a7c52442f08b65
bjk1649 <qhwns1649@naver.com>
2022-11-18 20:17:07 +0900

    refactor: 다리 길이 제한에 대한 정보를 상수로 사용하도록 변경

--- src/main/java/bridge/validator/InputValidator.java
+++ src/main/java/bridge/validator/InputValidator.java
@@ -3,16 +3,17 @@ package bridge.validator;
 import java.util.regex.Pattern;
 
 public class InputValidator {
+    private static final int BRIDGE_SIZE_MIN = 3;
+    private static final int BRIDGE_SIZE_MAX = 20;
 
     public static void validateBridgeSize(String input) throws IllegalArgumentException {
         String condition = "^[0-9]+$";
-
         if (!Pattern.matches(condition, input)) {
             System.out.println("[ERROR] 다리의 길이는 3에서 20 사이의 정수입니다.");
             throw new IllegalArgumentException();
         }
         int bridgeSize = Integer.parseInt(input);
-        if (bridgeSize < 3 || bridgeSize > 20) {
+        if ((bridgeSize < BRIDGE_SIZE_MIN) || (bridgeSize > BRIDGE_SIZE_MAX)) {
             System.out.println("[ERROR] 다리의 길이는 3에서 20 사이의 정수입니다.");
             throw new IllegalArgumentException();
         }
