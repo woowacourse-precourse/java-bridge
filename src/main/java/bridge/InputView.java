c824a740695d71852ceb2d5cfdeb5450584b6820
asas6978 <asas6978@naver.com>
2022-11-19 01:25:11 +0900

    feat: 재시작 여부 입력 시 메시지 출력 추가

--- src/main/java/bridge/InputView.java
+++ src/main/java/bridge/InputView.java
@@ -31,6 +31,11 @@ public class InputView {
      * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
      */
     public String readGameCommand() {
+        printInputGameCommand();
         return Console.readLine();
+    }
+
+    private void printInputGameCommand() {
+        System.out.println(GuideMessage.ENTER_RETRY_OR_QUIT);
     }
 }
