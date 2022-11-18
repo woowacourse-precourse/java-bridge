c3ea2eef67b1bbdc878091116aa2784b62cfce58
EUNJEONGMUN <munej26@gmail.com>
2022-11-18 14:13:50 +0900

    feat: 정답 마크와 오답 마크, 공백 상수 추가

--- src/main/java/bridge/constant/Constant.java
+++ src/main/java/bridge/constant/Constant.java
@@ -10,4 +10,7 @@ public class Constant {
     public static final String RETRY = "R";
     public static final String QUIT = "Q";
 
+    public static final String CORRECT_MARK = "O";
+    public static final String WRONG_MARK = "X";
+    public static final String BLANK = " ";
 }
