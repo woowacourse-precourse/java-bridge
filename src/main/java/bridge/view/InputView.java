a40b0103c6277e6bf7a82db6b26bc5e951459fee
Hayoon <hy97@sju.ac.kr>
2022-11-18 23:30:10 +0900

    feat(InputView): 사용자가 이동할 칸 뷰 의존성 관련 메서드 재구성

--- src/main/java/bridge/view/InputView.java
+++ src/main/java/bridge/view/InputView.java
@@ -1,32 +1,63 @@
 package bridge.view;
 
+import bridge.model.*;
 import camp.nextstep.edu.missionutils.Console;
-import camp.nextstep.edu.missionutils.Console.*;
+
+import java.util.List;
 
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
  */
 public class InputView {
+    private BridgeGame bridgeGame;
+    private BridgeException bridgeException;
+    private GameStatistics gameStatistics;
+    private OutputView outputView;
+
+    public InputView(BridgeGame bridgeGame, BridgeException bridgeException, GameStatistics gameStatistics, OutputView outputView) {
+        this.bridgeGame = bridgeGame;
+        this.bridgeException = bridgeException;
+        this.gameStatistics = gameStatistics;
+        this.outputView = outputView;
+    }
 
     /**
      * 다리의 길이를 입력받는다.
      */
-    public int readBridgeSize() {
+    public void readBridgeSize() {
+        System.out.println(MessageView.PLAY_BRIDGE_GAME.getMessage());
         System.out.println(MessageView.INPUT_BRIDGE_LENGTH.getMessage());
-        try {
-            String bridgeLength = Console.readLine();
-            return Integer.parseInt(bridgeLength);
-        } catch (NumberFormatException e) {
-            throw new IllegalArgumentException(ErrorView.error + ErrorView.INVALID_BRIDGE_LENGTH);
-        }
+        String bridgeLength = Console.readLine();
+        bridgeException.invalidLengthInputValue(bridgeLength);
+        bridgeGame.getBridge().setSize(Integer.parseInt(bridgeLength));
+        this.gameStatistics.setAnswerRoad(bridgeGame.getBridgeMaker().makeBridge(Integer.parseInt(bridgeLength)));
     }
 
     /**
      * 사용자가 이동할 칸을 입력받는다.
      */
-    public String readMoving() {
-        System.out.println(MessageView.SELECT_TO_MOVE.getMessage());
-        return Console.readLine();
+    public void readMoving() {
+        while (true) {
+            System.out.println(MessageView.SELECT_TO_MOVE.getMessage());
+            String moveDirection = Console.readLine();
+            bridgeException.invalidMovingInputValue(moveDirection);
+            gameStatistics.getCheckRoad().add(bridgeGame.move(moveDirection, gameStatistics.getAnswerRoad(), bridgeGame.getPlayer()));
+            //
+            System.out.println(gameStatistics.getCheckRoad());
+            //
+            boolean roundResult = bridgeGame.constructBridge();
+            outputView.printMap();
+            if (!roundResult) {
+                if (readGameCommand().equals("Q")) {
+                    break;
+                }
+            } else if (Player.currentLocation == bridgeGame.getBridge().getSize()) {
+                gameStatistics.setGameResult("성공");
+                System.out.println();
+                outputView.printResult();
+                break;
+            }
+        }
     }
 
     /**
@@ -34,6 +65,11 @@ public class InputView {
      */
     public String readGameCommand() {
         System.out.println(MessageView.INPUT_RETRY_OR_NOT_GAME.getMessage());
-        return Console.readLine();
+        String retryGame = Console.readLine();
+        bridgeException.invalidRetryGame(retryGame);
+        if (retryGame.equals("Q")) {
+            gameStatistics.setGameResult("실패");
+            outputView.printResult();
+        } return retryGame;
     }
 }
