42307118893fcb3a2327445b7b6935b8cbf9f2be
KNH <nh2004@naver.com>
2022-11-19 07:33:29 +0900

    feat(Application) : 전체 기능 구현

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -1,8 +1,14 @@
 package bridge;
 
+import bridge.config.GameConfig;
+import bridge.controller.GameController;
+
 public class Application {
 
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        GameConfig gameConfig = new GameConfig();
+        GameController gameController = gameConfig.gameController();
+        gameController.start();
     }
 }
