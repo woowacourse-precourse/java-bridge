348da79bdf8d83e8fb87a4692b603491375b2857
ho2n <jungho2n@gmail.com>
2022-11-18 15:30:15 +0900

    feat(ErrorMessage): 요구하는 에러 메시지를 표현하는 문자열 상수 추가

--- src/main/java/constant/ErrorMessage.java
+++ src/main/java/constant/ErrorMessage.java
@@ -1,6 +1,11 @@
 package constant;
 
+/**
+ * 에러 메시지 형식 : [ERROR] 다리 길이는 3부터 20 사이의 숫자여야 합니다.
+ */
 public class ErrorMessage {
-    public static final String ERROR_MESSAGE = "[ERROR]";
-    public static final String WRONG_LENGTH = "다리 길이는 3부터 20 사이의 숫자여야 합니다.";
+    public static final String ERROR_HEAD_MESSAGE = "[ERROR]";
+    public static final String WRONG_LENGTH = "%s 다리 길이는 3부터 20 사이의 숫자여야 합니다.";
+    public static final String ASK_INPUT_VALUE = "%s 입력값이 필요합니다.";
+    public static final String ASK_INPUT_NUMBER = "%s 숫자를 입력하셔야 합니다.";
 }
