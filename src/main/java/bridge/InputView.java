04e02154dc5f0d3d06210b668553600a786fcd08
JUNG-jihoon <73165763+JUNG-jihoon@users.noreply.github.com>
2022-11-18 19:23:19 +0900

    Feat : "추가 다리 길이 입력 및 이동 입력"
    다리 만들 때 필요한 길이를 입력 받아 반환하고 다리를 이동할 때 필요한 칸을 입력 받아 출력하였습니다.

--- src/main/java/bridge/InputView.java
+++ src/main/java/bridge/InputView.java
@@ -1,15 +1,22 @@
 package bridge;
 
+import camp.nextstep.edu.missionutils.Console;
+
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
  */
 public class InputView {
 
-    /**
-     * 다리의 길이를 입력받는다.
-     */
+public void InputView_start(){
+    System.out.println("다리 건너기 게임을 시작합니다.");
+    return;
+}
     public int readBridgeSize() {
-        return 0;
+        System.out.println("다리 건너기 게임을 시작합니다.");
+        System.out.println("다리의 길이를 입력해주세요.");
+        String Bridge_length = Console.readLine();
+        int size = Integer.parseInt(Bridge_length);
+        return size;
     }
 
     /**
@@ -23,6 +30,8 @@ public class InputView {
      * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
      */
     public String readGameCommand() {
-        return null;
+        System.out.println("이동할 칸을 선택해주세요. (위: U, 아래: D)");
+        String Move_Enter = Console.readLine();
+        return Move_Enter;
     }
 }
