6673106d9d53dcc96447bf1b47a956c2231af719
KangSungBum <peter9706@naver.com>
2022-11-18 17:18:13 +0900

    refactor(ValidationUtil.java): refactor static method keyword
    각 메소드 static 키워드 삭제 및 기본 생성자 추가
    - static memory 고려
    - static 대신 생성자를 통한 정적 메소드 고려 중

--- src/main/java/util/ValidationUtil.java
+++ src/main/java/util/ValidationUtil.java
@@ -5,21 +5,24 @@ import response.ErrorResponse;
 
 public class ValidationUtil {
 
-    public static void validateBridgeSizeInput(int sizeInput) {
+    public void validateBridgeSizeInput(int sizeInput) {
         if (sizeInput < 3 || sizeInput > 20) {
             throw new UserInputException(ErrorResponse.INPUT_BRIDE_SIZE_RANGE_ERROR);
         }
     }
 
-    public static void validateUserMovement(String movement) {
+    public void validateUserMovement(String movement) {
         if (!movement.equals("U") || !movement.equals("D")) {
             throw new UserInputException(ErrorResponse.INPUT_MOVEMENT_ERROR);
         }
     }
 
-    public static void validateExitCode(String exitCode) {
+    public void validateExitCode(String exitCode) {
         if (!exitCode.equals("R") || !exitCode.equals("Q")) {
             throw new UserInputException(ErrorResponse.INPUT_EXITCODE_ERROR);
         }
+    }
+
+    public ValidationUtil() {
     }
 }
