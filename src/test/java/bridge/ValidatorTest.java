f146a0648e0c402d6718f96ff85e6e88c4a7e1cd
jaeyeon kim <jakind@naver.com>
2022-11-18 20:10:03 +0900

    test(ValidatorTest): 다리 길이 입력 유효성 검증 테스트 코드

---
+++ src/test/java/bridge/ValidatorTest.java
@@ -0,0 +1,37 @@
+package bridge;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+public class ValidatorTest {
+
+    @Test
+    @DisplayName("다리 길이 입력이 유효한 경우")
+    @ParameterizedTest
+    @ValueSource(strings = {"3", "5"})
+    void bridgeLengthCorrectValue(String length) {
+        // given
+        Validator validator = new Validator();
+
+        // then
+        validator.bridgeLengthValidate(length);
+    }
+
+    @Test
+    @DisplayName("다리 길이 입력이 유효하지 않은 경우")
+    @ParameterizedTest
+    @ValueSource(strings = {"1", "21", "a"})
+    void bridgeLengthWrongValue(String length) {
+        // given
+        Validator validator = new Validator();
+
+        // then
+        assertThatThrownBy(() -> validator.bridgeLengthValidate(length))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(ErrorPhrase.WRONG_INPUT);
+    }
+}
