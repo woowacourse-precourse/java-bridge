0976d235c5df8c003b4781db3592faae815bb6eb
seoyoung <sy9723@korea.ac.kr>
2022-11-18 10:03:06 +0900

    refactor: printMessageForMove 메서드 이름 변경

--- src/main/java/bridge/OutputView.java
+++ src/main/java/bridge/OutputView.java
@@ -13,7 +13,7 @@ public class OutputView {
         System.out.println("다리의 길이를 입력해주세요.");
     }
 
-    public void printMessageForMove() {
+    public void printMessageForMoving() {
         System.out.println("이동할 칸을 선택해주세요. (위: U, 아래: D)");
     }
 
