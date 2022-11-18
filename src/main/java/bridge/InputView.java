d1c74124340d5b45a0635b9a1ea99a38e96ba6c5
bjm7701 <bjm7701@naver.com>
2022-11-18 21:23:31 +0900

    feat : InputView.readBridgeSize 함수 작성
    Console.readline 실행 후
    정수형으로 변환
    변환 실패 시 throw exception

--- src/main/java/bridge/InputView.java
+++ src/main/java/bridge/InputView.java
@@ -1,5 +1,5 @@
 package bridge;
-
+import camp.nextstep.edu.missionutils.Console;
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
  */
@@ -9,7 +9,13 @@ public class InputView {
      * 다리의 길이를 입력받는다.
      */
     public int readBridgeSize() {
-        return 0;
+        String tmp = Console.readLine();
+        try {
+            int res = Integer.parseInt(tmp);
+            return res;
+        }catch(Exception e){
+            throw new IllegalArgumentException();
+        }
     }
 
     /**
