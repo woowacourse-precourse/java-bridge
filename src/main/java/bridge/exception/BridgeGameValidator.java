013b6f21b15309a4a7bff87c4d3e31e208a982d2
yeonhee <ydol2@naver.com>
2022-11-19 00:03:05 +0900

    feat(BridgeGameValidator) : U,D 입력인지 검증하는 로직 추가

--- src/main/java/bridge/exception/BridgeGameValidator.java
+++ src/main/java/bridge/exception/BridgeGameValidator.java
@@ -3,6 +3,8 @@ package bridge.exception;
 public class BridgeGameValidator {
 
     private static final String ERROR_MESSAGE_BRIDGE_SIZE = "[ERROR] 다리 길이는 3부터 20 사이의 숫자여야 합니다.";
+    private static final String ERROR_MESSAGE_UP_AND_DOWN = "[ERROR] '위: U 아래: D'로 입력하셔야 합니다.";
+
 
     public static void isValidGameNumber(int number) {
         if (!(number >= 3 && number <= 20)) {
@@ -16,6 +18,12 @@ public class BridgeGameValidator {
             return stringToInt;
         }catch (Exception e){
             throw new IllegalArgumentException(ERROR_MESSAGE_BRIDGE_SIZE);
+        }
+    }
+
+    public static void validateUpAndDown(String str){
+        if(!(str=="U" || str=="D")){
+            throw new IllegalArgumentException(ERROR_MESSAGE_UP_AND_DOWN);
         }
     }
 }
