24e3249272eccaa90a2c11d04b69272154876fe4
Jzakka <mouse4786@gmail.com>
2022-11-18 23:22:30 +0900

    feat: User 재시작 설정 기능 구현

--- src/main/java/bridge/domain/BridgeGame.java
+++ src/main/java/bridge/domain/BridgeGame.java
@@ -34,6 +34,11 @@ public class BridgeGame {
      * 재시작을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
     public boolean retry(String decision) {
-        return Retry.getValue(decision);
+        boolean gameRetry = Retry.getValue(decision);
+        if (gameRetry) {
+            user.resetPosition();
+        }
+
+        return gameRetry;
     }
 }
--- src/main/java/bridge/domain/User.java
+++ src/main/java/bridge/domain/User.java
@@ -5,9 +5,19 @@ import java.util.List;
 
 public class User {
     private List<String > position = new ArrayList<>();
+    private Integer retry = 0;
+
+    public Integer getRetry() {
+        return retry;
+    }
 
     public void move(String currentPosition) {
         position.add(currentPosition);
+    }
+
+    public void resetPosition() {
+        position.clear();
+        retry++;
     }
 
     @Override
