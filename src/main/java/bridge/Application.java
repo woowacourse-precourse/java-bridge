db46ba32948a20ee2b4d82fd3fc44fcc008c4a8a
kimjeonghan <[rlawjdgks318@naver.com]>
2022-11-18 17:09:09 +0900

    feat(Application): 어플리케이션 실행 로직 추가

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -1,8 +1,13 @@
 package bridge;
 
+import bridge.service.BridgeService;
+
 public class Application {
 
-    public static void main(String[] args) {
-        // TODO: 프로그램 구현
-    }
+	public static void main(String[] args) {
+		// TODO: 프로그램 구현
+		BridgeService bridgeService = new BridgeService();
+
+		bridgeService.gameStart();
+	}
 }
