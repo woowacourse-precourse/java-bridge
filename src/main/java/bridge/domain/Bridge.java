48c957d31a80dc45d40cc80aa8bf5415aefbc1cc
Kim woo jong <skekdnwhd@gmail.com>
2022-11-18 15:18:32 +0900

    Feat : Bridge 생성기능 추가

--- src/main/java/bridge/domain/Bridge.java
+++ src/main/java/bridge/domain/Bridge.java
@@ -1,4 +1,15 @@
 package bridge.domain;
 
+import bridge.BridgeMaker;
+import bridge.BridgeRandomNumberGenerator;
+
+import java.util.List;
+
 public class Bridge {
+    private final List<String> shape;
+
+    public Bridge(int size) {
+        BridgeMaker bridgeMaker = new BridgeMaker(new BridgeRandomNumberGenerator());
+        this.shape = bridgeMaker.makeBridge(size);
+    }
 }
