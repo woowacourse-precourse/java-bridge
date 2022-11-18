c883544bab2d3a2ef2cef0e37765260484d1f434
KYR <freekyr7529@gmail.com>
2022-11-18 09:50:23 +0900

    refactor : Rule 클래스는 다리 길이 상수만 관리하기 때문에 이름을 BridgeRule로 변경

---
+++ src/main/java/bridge/BridgeRule.java
@@ -0,0 +1,21 @@
+package bridge;
+
+public enum BridgeRule {
+    MIN_LENGTH(3),
+    MAX_LENGTH(20);
+
+    private final int value;
+
+    BridgeRule(int value) {
+        this.value = value;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString();
+    }
+}
