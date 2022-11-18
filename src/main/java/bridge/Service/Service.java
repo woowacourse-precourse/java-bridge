04fb85838b5254036f955617ed26d49d7c4fffae
cocomozila <joseyun94@naver.com>
2022-11-19 03:18:33 +0900

    feat(Service): 게임 시작 메세지 출력 기능

--- src/main/java/bridge/Service/Service.java
+++ src/main/java/bridge/Service/Service.java
@@ -1,4 +1,24 @@
 package bridge.Service;
 
+import bridge.domain.Bridge;
+import bridge.domain.BridgeGame;
+import bridge.view.InputView;
+import bridge.view.OutputView;
+
 public class Service {
+
+    private InputView inputView;
+    private OutputView outputView;
+    private BridgeGame bridgeGame;
+
+    public Service() {
+        this.inputView = new InputView();
+        this.outputView = new OutputView();
+        this.bridgeGame = new BridgeGame();
+    }
+
+    // 게임 시작 메세지 출력 기능
+    public void welcomeGame() {
+        outputView.printWelcomeMessage();
+    }
 }
