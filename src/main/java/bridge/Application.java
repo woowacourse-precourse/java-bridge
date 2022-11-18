75a518ade662d2ebec35eeb65dc1a2dccab97601
nn98 <jkllhgb@gmail.com>
2022-11-18 14:33:41 +0900

    Feat: 입출력 뷰/다리 생성 기능 구조 정의

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -2,7 +2,22 @@ package bridge;
 
 public class Application {
 
+    private InputView inputView = new InputView();
+    private OutputView outputView = new OutputView();
+    private BridgeGame bridgeGame;
+
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+    }
+    private void play() {
+        bridgeGame=bridgeGameStart();
+    }
+    private BridgeGame bridgeGameStart() {
+        int bridgeSize= inputBridgeSize();
+        BridgeGame bridgeGame=new BridgeGame(bridgeSize);
+        return bridgeGame;
+    }
+    private int inputBridgeSize() {
+        return inputView.readBridgeSize();
     }
 }
