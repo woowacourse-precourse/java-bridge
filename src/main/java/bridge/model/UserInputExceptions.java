dda8fd70ef79d11f108ca015da93350250d57215
ysng_ysng <yeasung67@gmail.com>
2022-11-18 14:02:54 +0900

    feat(UserInputExceptions): Implement the methods to check the valid movable command and end command
    유효한 이동명령인지 판단하는 메서드와 유효한 종료명령인지 판단하는 메서드를 구현했습니다.

--- src/main/java/bridge/model/UserInputExceptions.java
+++ src/main/java/bridge/model/UserInputExceptions.java
@@ -5,6 +5,14 @@ public class UserInputExceptions {
 		return isNotNumber(length) || isNotInRangeNumber(length);
 	}
 
+	public static boolean isNotValidMove(String movableCommand) {
+		return isNotOneLength(movableCommand) || isNotValidMovableCommand(movableCommand);
+	}
+
+	public static boolean isNotValidEndCommand(String endCommand) {
+		return isNotOneLength(endCommand) || isNotEndCommand(endCommand);
+	}
+
 	private static boolean isNotNumber(String length) {
 		try {
 			Integer.parseInt(length);
@@ -17,5 +25,18 @@ public class UserInputExceptions {
 	private static boolean isNotInRangeNumber(String length) {
 		int number = Integer.parseInt(length);
 		return number < 3 || number > 20;
+	}
+
+
+	private static boolean isNotOneLength(String movableCommand) {
+		return movableCommand.length() != 1;
+	}
+
+	private static boolean isNotValidMovableCommand(String movableCommand) {
+		return !movableCommand.equals("U") && !movableCommand.equals("D");
+	}
+
+	private static boolean isNotEndCommand(String endCommand) {
+		return !endCommand.equals("Q") && !endCommand.equals("R");
 	}
 }
