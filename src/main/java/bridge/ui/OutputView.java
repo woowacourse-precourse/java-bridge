5d85c20c75fc3fdad4cbd201b01ccd0aed9d42ad
SHEOMM <esh0821@snu.ac.kr>
2022-11-18 14:52:04 +0900

    feat: 최종 다리 결과 출력 함수 구현

--- src/main/java/bridge/ui/OutputView.java
+++ src/main/java/bridge/ui/OutputView.java
@@ -1,5 +1,7 @@
 package bridge.ui;
 
+import bridge.model.GameMessage;
+
 public class OutputView {
 
     public void printMap(String upperSide, String downSide) {
@@ -11,5 +13,11 @@ public class OutputView {
     public void printResult(String isSuccess, String totalTry) {
         System.out.println(isSuccess);
         System.out.print(totalTry);
+    }
+
+    public void printMapResult(String upperSide, String downSide){
+        System.out.println(GameMessage.GAME_RESULT.getOutput());
+        System.out.println(upperSide);
+        System.out.println(downSide);
     }
 }
