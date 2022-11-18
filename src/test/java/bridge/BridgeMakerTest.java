6c3080a1d2afa45ffa0da098590631759f1a1fa1
Jimin <thswlals0219@gmail.com>
2022-11-18 10:24:30 +0900

    test(BridgeMaker): 다리의 길이만큼 다리를 생성하는 기능 테스트 추가

---
+++ src/test/java/bridge/BridgeMakerTest.java
@@ -0,0 +1,20 @@
+package bridge;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.util.Lists.newArrayList;
+import static org.junit.jupiter.api.Assertions.*;
+
+import bridge.ApplicationTest.TestNumberGenerator;
+import java.util.List;
+import org.junit.jupiter.api.DisplayName;
+
+class BridgeMakerTest {
+
+    @DisplayName("입력받은 다리의 길이 만큼 U 또는 D로 다리 모양을 나타내어 생성한다.")
+    void 다리_생성_테스트() {
+        BridgeNumberGenerator numberGenerator = new TestNumberGenerator(newArrayList(1, 0, 0));
+        BridgeMaker bridgeMaker = new BridgeMaker(numberGenerator);
+        List<String> bridge = bridgeMaker.makeBridge(3);
+        assertThat(bridge).containsExactly("U", "D", "D");
+    }
+}
