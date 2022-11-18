a1c53fba70eadd4db84c706b1ffa0e389318f946
Donguk <95donguk.dev@gmail.com>
2022-11-19 01:56:15 +0900

    refactor(BridgeSizeValidator): 메서드 분리 리팩토링

--- src/main/java/bridge/validator/BridgeSizeValidator.java
+++ src/main/java/bridge/validator/BridgeSizeValidator.java
@@ -8,17 +8,14 @@ import bridge.utils.message.ErrorMessage;
 public class BridgeSizeValidator implements Validator {
     @Override
     public void validate(String inputValue) {
+        checkNonNumericValue(inputValue);
+        checkBlankValue(inputValue);
+        checkInvalidBridgeSize(inputValue);
+    }
+
+    private void checkNonNumericValue(String inputValue) {
         if (isNonNumericValue(inputValue)) {
             System.out.println(ErrorMessage.NON_NUMERIC_VALUE.getMessage());
-            throw new IllegalArgumentException();
-        }
-        if (inputValue.isBlank()) {
-            System.out.println(ErrorMessage.BLANK_VALUE.getMessage());
-            throw new IllegalArgumentException();
-        }
-        if (isInValidBridgeSize(inputValue)) {
-            System.out.println(String.format(ErrorMessage.INVALID_BRIDGE_SIZE.getMessage(), MINIMUM_BRIDGE_SIZE,
-                    MAXIMUM_BRIDGE_SIZE));
             throw new IllegalArgumentException();
         }
     }
@@ -27,7 +24,22 @@ public class BridgeSizeValidator implements Validator {
         return !inputValue.chars().allMatch(Character::isDigit);
     }
 
-    private boolean isInValidBridgeSize(String inputValue) {
+    private void checkBlankValue(String inputValue) {
+        if (inputValue.isBlank()) {
+            System.out.println(ErrorMessage.BLANK_VALUE.getMessage());
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private void checkInvalidBridgeSize(String inputValue) {
+        if (isInvalidBridgeSize(inputValue)) {
+            System.out.println(String.format(ErrorMessage.INVALID_BRIDGE_SIZE.getMessage(), MINIMUM_BRIDGE_SIZE,
+                    MAXIMUM_BRIDGE_SIZE));
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private boolean isInvalidBridgeSize(String inputValue) {
         int bridgeSize = Integer.parseInt(inputValue);
         return bridgeSize < MINIMUM_BRIDGE_SIZE || bridgeSize > MAXIMUM_BRIDGE_SIZE;
     }
