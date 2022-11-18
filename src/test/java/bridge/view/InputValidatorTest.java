2790b8b754a8425ba52cc2c0bcc3755b8a5257b9
eastsage <dpwk8@naver.com>
2022-11-19 01:52:33 +0900

    test: 입력된 움직일 칸을 검증하는 함수 테스트 추가

--- src/test/java/bridge/view/InputValidatorTest.java
+++ src/test/java/bridge/view/InputValidatorTest.java
@@ -5,12 +5,13 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.EmptySource;
 import org.junit.jupiter.params.provider.NullAndEmptySource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 class InputValidatorTest {
     @Nested
-    @DisplayName("다리 길이에 대한 검증 테스트")
+    @DisplayName("다리 길이 입력에 대한 검증 테스트")
     class ValidateSizeTest {
         @ParameterizedTest(name = "입력값 -> {0}")
         @ValueSource(strings = {"eng", "한글", "!", "3.5"})
@@ -18,7 +19,7 @@ class InputValidatorTest {
         @DisplayName("사용자 입력이 자연수가 아니면 예외가 발생한다.")
         void toNumber_SizeIsNotNumeric_ExceptionThrown(String input) {
             assertThatThrownBy(() -> new InputValidator().validateSize(input))
-                    .isInstanceOf(IllegalArgumentException.class);
+                    .isInstanceOf(NumberFormatException.class);
         }
 
         @ParameterizedTest(name = "입력값 -> {0}")
@@ -30,4 +31,16 @@ class InputValidatorTest {
         }
     }
 
+    @Nested
+    @DisplayName("이동할 칸 입력에 대한 검증 테스트")
+    class ValidateMovingTest {
+        @ParameterizedTest(name = "입력값 -> {0}")
+        @ValueSource(strings = {"u", "d", "A", "0"})
+        @EmptySource
+        @DisplayName("사용자 입력이 U 또는 D가 아니면 예외가 발생한다.")
+        void toNumber_SizeIsNotNumeric_ExceptionThrown(String input) {
+            assertThatThrownBy(() -> new InputValidator().validateMoving(input))
+                    .isInstanceOf(IllegalArgumentException.class);
+        }
+    }
 }
