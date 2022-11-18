92da432c3bb671bddc352668e7afc508c65fe653
SHEOMM <esh0821@snu.ac.kr>
2022-11-18 14:39:39 +0900

    feat: 다리 건너는 input View 함수 구현

--- src/main/java/bridge/ui/InputView.java
+++ src/main/java/bridge/ui/InputView.java
@@ -15,18 +15,13 @@ public class InputView {
         return getInput();
     }
 
-    /**
-     * 사용자가 이동할 칸을 입력받는다.
-     */
     public String readMoving() {
-        return null;
+        printGetMoveMessage();
+        return getInput();
     }
 
-    /**
-     * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
-     */
     public String readGameCommand() {
-        return null;
+        return "";
     }
 
     private void printStartMessage(){
