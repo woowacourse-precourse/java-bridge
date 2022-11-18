b3f86eff340249feb827bec3c7991b4cecfbffc4
chaos0103 <lyt1228@naver.com>
2022-11-18 15:54:49 +0900

    refactor(Bridge): 코드 정리 및 상수로 변경

--- src/main/java/bridge/BridgeMaker.java
+++ src/main/java/bridge/BridgeMaker.java
@@ -1,7 +1,10 @@
 package bridge;
 
-import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+import static bridge.Constants.*;
 
 /**
  * 다리의 길이를 입력 받아서 다리를 생성해주는 역할을 한다.
@@ -19,18 +22,16 @@ public class BridgeMaker {
      * @return 입력받은 길이에 해당하는 다리 모양. 위 칸이면 "U", 아래 칸이면 "D"로 표현해야 한다.
      */
     public List<String> makeBridge(int size) {
-        List<String> result = new ArrayList<>();
-        for (int i = 0; i < size; i++) {
-            int number = bridgeNumberGenerator.generate();
-            result.add(getUpAndDown(number));
-        }
-        return result;
+        return IntStream.range(0, size)
+                .map(i -> bridgeNumberGenerator.generate())
+                .mapToObj(this::getUpAndDown)
+                .collect(Collectors.toList());
     }
 
     private String getUpAndDown(int number) {
         if (number == 0) {
-            return "D";
+            return DOWN;
         }
-        return "U";
+        return UP;
     }
 }
