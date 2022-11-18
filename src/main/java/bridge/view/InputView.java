f39257e758ff28f9c1a91f1d1bc5c166f23d7fdb
cocomozila <joseyun94@naver.com>
2022-11-19 02:45:31 +0900

    feat(InputView): 게임을 다시 시도할지 종료할지 여부를 입력받는 기능

--- src/main/java/bridge/view/InputView.java
+++ src/main/java/bridge/view/InputView.java
@@ -64,10 +64,9 @@ public class InputView {
         }
     }
 
-    /**
-     * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
-     */
+    // 게임을 다시 시도할지 종료할지 여부를 입력받는 기능
     public String readGameCommand() {
-        return null;
+        String input = Console.readLine();
+        return input;
     }
 }
