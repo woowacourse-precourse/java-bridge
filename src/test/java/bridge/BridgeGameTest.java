57e68dccb1ee84a9f04cc295e3589e6994ed6152
skck0226 <skck0226@ajou.ac.kr>
2022-11-19 23:15:23 +0900

    [refactor] (BridgeGameTest) 한칸 이동 결과 반환값 변경에 따른 테스트 수정

--- src/test/java/bridge/BridgeGameTest.java
+++ src/test/java/bridge/BridgeGameTest.java
@@ -24,34 +24,34 @@ class BridgeGameTest {
     @DisplayName("한칸 이동 성공 테스트")
     @Test
     void 한칸이동_성공_테스트() {
-        assertEquals(List.of("[ O ]","[   ]"),bridgeGame.move("U"));
-        assertEquals(List.of("[ O |   ]","[   | O ]"),bridgeGame.move("D"));
-        assertEquals(List.of("[ O |   |   ]","[   | O | O ]"),bridgeGame.move("D"));
-        assertEquals(List.of("[ O |   |   |   ]","[   | O | O | O ]"),bridgeGame.move("D"));
-        assertEquals(List.of("[ O |   |   |   | O ]","[   | O | O | O |   ]"),bridgeGame.move("U"));
+        assertEquals(List.of("O","[ O ]","[   ]"),bridgeGame.move("U"));
+        assertEquals(List.of("O","[ O |   ]","[   | O ]"),bridgeGame.move("D"));
+        assertEquals(List.of("O","[ O |   |   ]","[   | O | O ]"),bridgeGame.move("D"));
+        assertEquals(List.of("O","[ O |   |   |   ]","[   | O | O | O ]"),bridgeGame.move("D"));
+        assertEquals(List.of("O","[ O |   |   |   | O ]","[   | O | O | O |   ]"),bridgeGame.move("U"));
     }
 
     @DisplayName("한칸 이동 실패 테스트")
     @ParameterizedTest()
     @MethodSource("generateData")
     void 한칸이동_실패_테스트(List<String> param) {
-        assertEquals(param.subList(1,3),bridgeGame.move(param.get(0)));
+        assertEquals(param.subList(1,4),bridgeGame.move(param.get(0)));
     }
 
     static Stream<List<String>> generateData() {
         return Stream.of(
-            List.of("D","[   ]","[ X ]"),
-            List.of("D","[   ]","[ X ]")
+            List.of("D","X", "[   ]","[ X ]"),
+            List.of("D","X", "[   ]","[ X ]")
         );
     }
 
     @DisplayName("한칸 이동 성공_실패 테스트")
     @Test
     void 한칸이동_성공_실패_테스트() {
-        assertEquals(List.of("[ O ]","[   ]"),bridgeGame.move("U"));
-        assertEquals(List.of("[ O |   ]","[   | O ]"),bridgeGame.move("D"));
-        assertEquals(List.of("[ O |   |   ]","[   | O | O ]"),bridgeGame.move("D"));
-        assertEquals(List.of("[ O |   |   |   ]","[   | O | O | O ]"),bridgeGame.move("D"));
-        assertEquals(List.of("[ O |   |   |   |   ]","[   | O | O | O | X ]"),bridgeGame.move("D"));
+        assertEquals(List.of("O","[ O ]","[   ]"),bridgeGame.move("U"));
+        assertEquals(List.of("O","[ O |   ]","[   | O ]"),bridgeGame.move("D"));
+        assertEquals(List.of("O","[ O |   |   ]","[   | O | O ]"),bridgeGame.move("D"));
+        assertEquals(List.of("O","[ O |   |   |   ]","[   | O | O | O ]"),bridgeGame.move("D"));
+        assertEquals(List.of("X","[ O |   |   |   |   ]","[   | O | O | O | X ]"),bridgeGame.move("D"));
     }
 }
