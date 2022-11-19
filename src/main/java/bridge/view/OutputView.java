d0a3b6567746ddf44b4fe9644d1173e04faec16a
thdwoqor <celebarte01@gmail.com>
2022-11-19 19:54:00 +0900

    feat: 최종 게임 결과 메시지 출력 기능 추가

--- src/main/java/bridge/view/OutputView.java
+++ src/main/java/bridge/view/OutputView.java
@@ -47,6 +47,10 @@ public class OutputView {
         System.out.println(String.join("", result));
     }
 
+    public static void printFinal() {
+        System.out.println(Final_OUTPUT_MESSAGE);
+    }
+    
     /**
      * 게임의 최종 결과를 정해진 형식에 맞춰 출력한다.
      * <p>
