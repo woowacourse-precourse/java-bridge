48024a90432d4536d57f85a487c4367962d09f5e
whdcks420 <whdcks420@gmail.com>
2022-11-19 23:42:51 +0900

    feat: BridgeGame 기능

--- src/main/java/bridge/domain/BridgeGame.java
+++ src/main/java/bridge/domain/BridgeGame.java
@@ -1,16 +1,36 @@
-package bridge;
+package bridge.domain;
+
+import static bridge.domain.BridgeMapConstant.*;
 
 /**
  * 다리 건너기 게임을 관리하는 클래스
  */
 public class BridgeGame {
+    private static final String RESTART = "R";
+
+    private final Bridge bridge;
+    private final GameCount gameCount;
+
+    private boolean status;
+    private int mapCoordinate = START_INDEX;
+    private String winning;
+
+    public BridgeGame(Bridge bridge) {
+        this.bridge = bridge;
+        gameCount = new GameCount();
+    }
 
     /**
      * 사용자가 칸을 이동할 때 사용하는 메서드
      * <p>
      * 이동을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void move() {
+    public BridgeMap move(String direction) {
+        if (checkCrossBridge(direction, mapCoordinate)) {
+            return getCurrentMap(this.status = true);
+
+        }
+        return getCurrentMap(this.status = false);
     }
 
     /**
@@ -18,6 +38,49 @@ public class BridgeGame {
      * <p>
      * 재시작을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void retry() {
+    public boolean retry(String gameCommand) {
+        if (gameCommand.equals(RESTART)) {
+            gameInit();
+            return true;
+        }
+        return false;
+    }
+
+    private void gameInit() {
+        initializeMapCount();
+        gameCount.increase();
+    }
+
+    private boolean checkCrossBridge(String direction, int count) {
+        this.mapCoordinate++;
+        return this.bridge.getBridge().get(count).equals(direction);
+    }
+
+    private void initializeMapCount() {
+        this.mapCoordinate = START_INDEX;
+    }
+
+    public boolean checkStatus() {
+        return status;
+    }
+
+    public boolean isWinningBrideGame() {
+        if (this.mapCoordinate == bridge.getBridge().size()) {
+            return successWhether("성공", true);
+        }
+        return successWhether("실패", false);
+    }
+
+    private boolean successWhether(String winning, boolean status) {
+        this.winning = winning;
+        return status;
+    }
+
+    public Result getResult() {
+        return new Result(getCurrentMap(status), winning, gameCount.getGameCount());
+    }
+
+    private BridgeMap getCurrentMap(boolean status) {
+        return new BridgeMap(bridge, mapCoordinate, status);
     }
 }
