5cc3926f7ac844856868f0533439234687a5353e
jaeyeon kim <jakind@naver.com>
2022-11-19 19:44:37 +0900

    test(GameHostTest): Bridge 의 일부분 , Alive, Index 를 반환하는 기능 테스트

---
+++ src/test/java/bridge/GameHostTest.java
@@ -0,0 +1,44 @@
+package bridge;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class GameHostTest {
+
+    private GameHost gameHost;
+    private final List<String> testBridge = List.of("U", "D", "D", "U");
+
+    @BeforeEach
+    void beforeEach() {
+        GameHost gameHost = new GameHost();
+        gameHost.setBridge(testBridge);
+    }
+
+    @DisplayName("Index 가 주어지면 해당 다리의 부분을 반환하는 기능")
+    @ParameterizedTest
+    @ValueSource(ints = {0, 1, 2})
+    void stepOfIndexInBridge(int index) {
+        // then
+        assertThat(gameHost.stepOfIndexInBridge(index)).isEqualsTo(testBridge.get(index));
+    }
+
+    @Test
+    @DisplayName("플레이어가 살아있는지 반환하는 기능")
+    void isPlayerAlive() {
+        assertThat(gameHost.isPlayerAlive()).isTrue();
+    }
+
+    @Test
+    @DisplayName("플레이어의 Index 를 반환하는 기능")
+    void getInitialPlayerIndex() {
+        assertThat(gameHost.getPlayerIndex()).isEqualTo(-1);
+    }
+
+}
