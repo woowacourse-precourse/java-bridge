12e2141da54bf29c1d37270c915861093870065b
김현재 <41482946+galug@users.noreply.github.com>
2022-11-20 00:04:16 +0900

    feat(output): 게임을 진행시키기 위한 출력 기능 추가

---
+++ src/main/java/bridge/view/OutputView.java
@@ -0,0 +1,38 @@
+package bridge.view;
+
+/**
+ * 사용자에게 게임 진행 상황과 결과를 출력하는 역할을 한다.
+ */
+public class OutputView {
+    public void printStartGame() {
+        System.out.println(Constants.START_GAME_OUTPUT);
+    }
+
+    public void printRequestBridgeSizeInput() {
+        System.out.println(Constants.REQUEST_BRIDGE_SIZE_INPUT);
+    }
+    public void printSelectUpOrDownInput() {
+        System.out.println(Constants.SELECT_UP_OR_DOWN_INPUT);
+    }
+    public void printSelectRetryOrNotInput() {
+        System.out.println(Constants.SELECT_RETRY_OR_NOT_INPUT);
+    }
+    public void printFinalGameResult() {
+        System.out.println(Constants.FINAL_GAME_RESULT_OUTPUT);
+    }
+
+    /**
+     * 현재까지 이동한 다리의 상태를 정해진 형식에 맞춰 출력한다.
+     * <p>
+     * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
+     */
+    public void printMap(String map) {
+    }
+    /**
+     * 게임의 최종 결과를 정해진 형식에 맞춰 출력한다.
+     * <p>
+     * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
+     */
+    public void printResult(String map, boolean isSuccess, int numberOfTry) {
+    }
+}
