289c0278e24c626da086ca463787889fbc1f31d3
jiwon <ljwon77@daum.net>
2022-11-20 00:42:04 +0900

    chore: modify output message
    결과 메시지에 개행 문자를 추가하였습니다.

--- src/main/java/bridge/constant/message/InputMessage.java
+++ src/main/java/bridge/constant/message/InputMessage.java
@@ -5,7 +5,7 @@ public enum InputMessage {
     START("다리 건너기 게임을 시작합니다.")
     , BRIDGE_LENGTH("다리의 길이를 입력해주세요.")
     , SELECT_MOVE("이동할 칸을 선택해주세요. (위: %s, 아래: %s)\n")
-    , PLAY_CONTROL("게임을 다시 시도할지 여부를 입력해주세요. (재시도: %s, 종료: %s")
+    , PLAY_CONTROL("게임을 다시 시도할지 여부를 입력해주세요. (재시도: %s, 종료: %s)\n")
     ;
 
     private String message;
