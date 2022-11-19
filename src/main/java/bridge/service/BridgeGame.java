0aedcd01279626088deb8157044822ee8e54f5c5
woo-chang <clllickme@naver.com>
2022-11-19 16:53:02 +0900

    refactor(BridgeGame): 플레이어의 이동 후 검증하는 순서로 수정

--- src/main/java/bridge/service/BridgeGame.java
+++ src/main/java/bridge/service/BridgeGame.java
@@ -23,8 +23,8 @@ public class BridgeGame {
      * 이동을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
     public boolean move(Direction direction) {
-        bridge.validatePlayerStatus(player);
         player.move(direction);
+        bridge.validatePlayerStatus(player);
         return !bridge.isDone(player);
     }
 
@@ -39,6 +39,14 @@ public class BridgeGame {
             return true;
         }
         return false;
+    }
+
+    public Bridge getBridge() {
+        return bridge;
+    }
+
+    public Player getPlayer() {
+        return player;
     }
 
 }
