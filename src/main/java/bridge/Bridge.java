e362c2acc21d255cf7c1284173cf8ab20958c933
Devtist <yt930310@snu.ac.kr>
2022-11-19 20:29:51 +0900

    feat: Bridge에 get메소드들 추가

--- src/main/java/bridge/Bridge.java
+++ src/main/java/bridge/Bridge.java
@@ -8,4 +8,16 @@ public class Bridge {
 	public Bridge(List<String> bridge) {
 		this.bridge = bridge;
 	}
+
+	public boolean isCorrectMoving(int index, String moving) {
+		return bridge.get(index).equals(moving);
+	}
+
+	public int getBridgeSize() {
+		return bridge.size();
+	}
+
+	public List<String> getBridge() {
+		return bridge;
+	}
 }
