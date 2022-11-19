2f8002db06c098f934f608945f86db056d532993
meunrud <y3a0522@gmail.com>
2022-11-20 03:11:03 +0900

    :art: (GameResultCondition) enum 순서 변경 및 filter 조건 추가

--- src/main/java/bridge/GameResultCondition.java
+++ src/main/java/bridge/GameResultCondition.java
@@ -3,19 +3,37 @@ package bridge;
 import java.util.Arrays;
 
 public enum GameResultCondition {
-    PASS("O"),
-    FAIL("X");
+    UPPERPASS("O","U","U"),
+    LOWERPASS("O","D","D"),
+    FAIL("X","U","D");
 
     private String result;
+    private String inputMoving;
+    private String bridge;
 
-    GameResultCondition(String result) {
+    GameResultCondition(String result, String inputMoving, String bridge) {
+        this.inputMoving = inputMoving;
+        this.bridge = bridge;
         this.result = result;
     }
-
-    static GameResultCondition of(String commande){
-        return Arrays.stream(values())
-                .filter(v -> commande.equals(v.result))
-                .findFirst()
-                .orElseThrow(()-> new IllegalArgumentException("[ERROR]"));
+    public String getResult(){
+        return result;
     }
+    public String getInputMoving(){
+        return inputMoving;
+    }
+    public String getBridge(){
+        return bridge;
+    }
+
+    public static GameResultCondition of(final String inputMoving, final String bridge){
+        return Arrays.stream(values())
+                .filter(v -> inputMoving.equals(bridge))
+                .filter(v -> inputMoving.equals(v.inputMoving))
+                .filter(v -> bridge.equals(v.bridge))
+                .findFirst()
+                .orElse(FAIL);
+    }
+
+
 }
