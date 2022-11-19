651406835c171a4dc2a3c6b3c20d3961c6006e76
yejin <kwonyj1022@naver.com>
2022-11-19 23:55:37 +0900

    fix: InputView readGameCommand() 메소드 오타 수정

--- src/main/java/bridge/view/InputView.java
+++ src/main/java/bridge/view/InputView.java
@@ -39,7 +39,7 @@ public class InputView {
     public String readGameCommand() {
         System.out.println("게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)");
         String gameCommand = Console.readLine();
-        new Validation().validateMoving(gameCommand);
+        new Validation().validateGameCommand(gameCommand);
         return gameCommand;
     }
 }
