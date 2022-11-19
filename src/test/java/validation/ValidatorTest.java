54a21b04928cc1af81071989c237ac478c1537f7
CMHyuk <alsgur9710@naver.com>
2022-11-19 18:26:02 +0900

    test(ValidatorTest): 다리 길이 입력 테스트

---
+++ src/test/java/validation/ValidatorTest.java
@@ -0,0 +1,27 @@
+package validation;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+class ValidatorTest {
+
+    Validator validator = new Validator();
+
+    @DisplayName("다리 길이 입력 시 숫자가 아닌 다른 입력의 경우 예외가 발생한다.")
+    @Test
+    void isNumber() {
+        assertThatThrownBy(() -> validator.validateBridgeLength("a"))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("다리 길이는 3부터 20 사이의 숫자여야 합니다.");
+    }
+
+    @DisplayName("다리 길이 입력 시 3 ~ 20의 숫자가 아닐 경우 예외가 발생한다.")
+    @Test
+    void isCorrectRange() {
+        assertThatThrownBy(() -> validator.validateBridgeLength("21"))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("다리 길이는 3부터 20 사이의 숫자여야 합니다.");
+    }
+}
