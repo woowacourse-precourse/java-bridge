2247a8a4d02828c878a0436ccd9b8a6b233f03ae
jkroh1995 <85445649+jkroh1995@users.noreply.github.com>
2022-11-19 18:02:53 +0900

    refactor : 컨트롤러 객체 변경에 따른 생성자 변경

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -1,12 +1,12 @@
 package bridge;
 
-import bridge.domain.BridgeGame;
+import bridge.controller.BridgeGameController;
 import bridge.view.OutputView;
 
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
         new OutputView().printStartMessage();
-        new BridgeGame().runGame();
+        new BridgeGameController().runGame();
     }
 }
