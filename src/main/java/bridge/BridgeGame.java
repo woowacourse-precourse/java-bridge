4f3ce6988f7559fd7bd4e07e4a372c18ae3ba130
Sungjaeyeong <wodud2587@gmail.com>
2022-11-19 22:41:17 +0900

    feat(bridge): 칸 이동 구현

--- src/main/java/bridge/BridgeGame.java
+++ src/main/java/bridge/BridgeGame.java
@@ -1,16 +1,28 @@
 package bridge;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * 다리 건너기 게임을 관리하는 클래스
  */
 public class BridgeGame {
 
+    private final List<String> bridge;
+
+    public BridgeGame(List<String> bridge) {
+        this.bridge = bridge;
+    }
+
     /**
      * 사용자가 칸을 이동할 때 사용하는 메서드
-     * <p>
+     * @param locationIndex 이동할 다리의 인덱스 값
+     * @param selectedMoving 이동할 칸 ("U" or "D")
+     * @return 이동 성공 여부
      * 이동을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void move() {
+    public boolean move(int locationIndex, String selectedMoving) {
+        return bridge.get(locationIndex).equals(selectedMoving);
     }
 
     /**
