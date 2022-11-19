7e0d99716f375be528b65f900cc367bef539fc0e
MyunghyunNero <mh1kim0910@gmail.com>
2022-11-19 17:12:23 +0900

    feat : OutPut 클래스 printMap() 메서드 구현

--- src/main/java/bridge/OutputView.java
+++ src/main/java/bridge/OutputView.java
@@ -1,16 +1,39 @@
 package bridge;
 
+import java.util.List;
+import java.util.Map;
+
 /**
  * 사용자에게 게임 진행 상황과 결과를 출력하는 역할을 한다.
  */
 public class OutputView {
+
+    private BridgeGame bridgeGame;
+
+    OutputView(BridgeGame bridgeGame){
+        this.bridgeGame = bridgeGame;
+    }
 
     /**
      * 현재까지 이동한 다리의 상태를 정해진 형식에 맞춰 출력한다.
      * <p>
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void printMap() {
+    public void printMap(String input) {
+        Map<String, List<String>> bridge = bridgeGame.move(input);
+        List<String> upBridge = bridge.get("up");
+        List<String> downBridge = bridge.get("down");
+        printSubBridge(upBridge);
+        printSubBridge(downBridge);
+    }
+
+    private void printSubBridge(List<String> subBridge){
+        System.out.println("[ ");
+        int size = subBridge.size();
+        for(int i = 0; i < size-1 ; i++ ){
+            System.out.println(subBridge.get(i)+" | ");
+        }
+        System.out.println(subBridge.get(size-1)+" ]");
     }
 
     /**
