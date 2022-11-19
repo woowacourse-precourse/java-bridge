b937911cc5e67714fc0fa09d3823bb05af8e36cd
JuHyeokParkk <hyeok5596@naver.com>
2022-11-20 01:58:14 +0900

    feat: 게임 시도 횟수가 일정 범위를 넘어가면 예외를 발생시키는 기능 구현(오버플로우 방지)

--- src/main/java/bridge/BridgeGame.java
+++ src/main/java/bridge/BridgeGame.java
@@ -24,7 +24,14 @@ public class BridgeGame {
      * 재시작을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
     public void retry() {
+        checkGameTryCountOverIntegerMaxValue();
         gameTryCount++;
+    }
+
+    public void checkGameTryCountOverIntegerMaxValue() {
+        if(gameTryCount == Integer.MAX_VALUE) {
+            throw new IllegalArgumentException("[ERROR] 너무 많은 게임 시도는 건강에 해롭습니다. 게임을 종료합니다.");
+        }
     }
 
     public int getGameTryCount() {
