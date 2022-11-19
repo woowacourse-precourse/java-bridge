7961b2d71a8eef9bf9a68eef610a48ea12a6b112
MJoon-Jung <gjgjajaj31@gmail.com>
2022-11-20 00:52:40 +0900

    refactor<Application>: 함수 분리 적용
    - try catch를 이용하여 input받는 함수에서 에러 날 경우 다시 입력 받을 수 있도록 변경
    - 함수 분리 10줄 이하 유지
    - InputView, OutputView 클래스 변수 선언
    - OutputView error message 출력

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -3,20 +3,44 @@ package bridge;
 import bridge.model.Bridge;
 import bridge.model.BridgeSize;
 import bridge.model.Direction;
+import bridge.model.GameBoard;
+import bridge.model.Player;
 import bridge.view.InputView;
 import bridge.view.OutputView;
 
 public class Application {
+    private static final InputView inputView = new InputView();
+    private static final OutputView outputView = new OutputView();
+
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
-        InputView inputView = new InputView();
-        OutputView outputView = new OutputView();
         outputView.printGameStart();
-        String size = inputView.readBridgeSize();
-        BridgeSize bridgeSize = new BridgeSize(size);
+
+        BridgeGame bridgeGame = new BridgeGame(createBridge(), new Player());
+        GameBoard gameBoard = new GameBoard();
+    }
+
+    private static BridgeSize inputBridgeSize() {
+        try {
+            String size = inputView.readBridgeSize();
+            return new BridgeSize(size);
+        } catch (IllegalArgumentException e) {
+            outputView.printErrorMessage(e.getMessage());
+            return inputBridgeSize();
+        }
+    }
+
+    private static Bridge createBridge() {
+        BridgeSize bridgeSize = inputBridgeSize();
         BridgeMaker bridgeMaker = new BridgeMaker(new BridgeRandomNumberGenerator());
-        Bridge bridge = new Bridge(bridgeMaker.makeBridge(bridgeSize.getSize()));
-        Direction direction = Direction.fromInput(inputView.readDirection());
-        System.out.println(direction);
+        return new Bridge(bridgeMaker.makeBridge(bridgeSize.getSize()));
+    }
+
+    private static Direction inputDirection() {
+        try {
+            return Direction.fromInput(inputView.readDirection());
+        } catch (IllegalArgumentException e) {
+            outputView.printErrorMessage(e.getMessage());
+            return inputDirection();
+        }
     }
 }
--- src/main/java/bridge/view/OutputView.java
+++ src/main/java/bridge/view/OutputView.java
@@ -25,4 +25,8 @@ public class OutputView {
      */
     public void printResult() {
     }
+
+    public void printErrorMessage(String message) {
+        System.out.println(message);
+    }
 }
