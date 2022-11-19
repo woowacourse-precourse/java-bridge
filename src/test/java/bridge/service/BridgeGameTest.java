93a517a78425082028a8feae2a08d4cc60e5bb64
sosow0212 <sosow0212@naver.com>
2022-11-19 20:34:13 +0900

    test : (BridgeGameTest) 유저가 이동하는 모든 경우 테스트

---
+++ src/test/java/bridge/service/BridgeGameTest.java
@@ -0,0 +1,43 @@
+package bridge.service;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import bridge.domain.Bridge;
+import bridge.domain.User;
+import java.util.List;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class BridgeGameTest {
+    private BridgeGame bridgeGame = new BridgeGame();
+
+    @DisplayName("move 테스트 (유저가 이동을 했지만, 아직까지 완료되지 않은 상태)")
+    @Test
+    public void moveTest() {
+        // given
+        Bridge bridge = new Bridge(List.of("U", "D", "D"));
+        User user = new User();
+
+        // when
+        bridgeGame.move(bridge, user);
+
+        // then
+        assertThat(bridge.getNowIndex()).isEqualTo(1);
+    }
+
+    @DisplayName("move 테스트 (유저가 이동을 하고, 게임을 성공적으로 마무리 한 경우)")
+    @Test
+    public void moveTest2() {
+        // given
+        Bridge bridge = new Bridge(List.of("U", "D", "D"));
+        User user = new User();
+        bridge.setNowIndex(2);
+
+        // when
+        bridgeGame.move(bridge, user);
+
+        // then
+        assertThat(user.isGameSuccess()).isEqualTo(true);
+        assertThat(user.isGameSuccess()).isEqualTo(true);
+    }
+}
