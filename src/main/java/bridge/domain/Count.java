eea970f189b8f15046864ccdeae42710fd2505a2
wugawuga <dnr3655@naver.com>
2022-11-19 15:17:51 +0900

    feat: 게임 총 횟수가 1미만일 때 예외

--- src/main/java/bridge/domain/Count.java
+++ src/main/java/bridge/domain/Count.java
@@ -1,5 +1,7 @@
 package bridge.domain;
 
+import java.util.Objects;
+
 public class Count {
     private final int count;
 
@@ -8,6 +10,7 @@ public class Count {
     }
 
     public Count(int count) {
+        validate(count);
         this.count = count;
     }
 
@@ -17,5 +20,28 @@ public class Count {
 
     public String printCount() {
         return String.valueOf(this.count);
+    }
+
+    private void validate(int count) {
+        if (count < 1) {
+            throw new IllegalArgumentException("[ERROR] 게임 총 횟수는 1미만이 될 수 없습니다");
+        }
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Count count1 = (Count) o;
+        return count == count1.count;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(count);
     }
 }
