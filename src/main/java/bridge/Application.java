8a1b8c82bf6c5f69a1f3ac3f307256262e60d174
SEONGJOON KIM <jobcho6320@naver.com>
2022-11-20 02:53:14 +0900

    fix: 예외 테스트 통과 할 수 있도록 수정
    - 에러를 메세지로 받아서 출력되도록 변경

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -1,17 +1,15 @@
 package bridge;
 
 import bridge.controller.BridgeController;
-import bridge.domain.BridgeMaker;
-
 
 public class Application {
-
-
-    private static final BridgeMaker bridgeMaker = new BridgeMaker(null);
-    private static final String BLANK = " ";
     public static void main(String[] args) {
+        try {
+            BridgeController bridgeController = new BridgeController();
+            bridgeController.run();
+        } catch (Exception error) {
+            System.out.println(error.getMessage());
+        }
 
-        BridgeController bridgeController = new BridgeController();
-        bridgeController.run();
     }
 }
