e7c93414b3537e5632699c6cf543ad20d706a917
Bue-von-hon <dkssudvn2@gmail.com>
2022-11-19 20:00:26 +0900

    feat : 게임기 이동, 재시작, 상태 구현

--- src/main/java/bridge/BridgeGame.java
+++ src/main/java/bridge/BridgeGame.java
@@ -1,16 +1,36 @@
 package bridge;
 
+import bridge.constant.Error;
+import bridge.constant.State;
+import java.util.List;
+
 /**
  * 다리 건너기 게임을 관리하는 클래스
  */
 public class BridgeGame {
+
+    private int position = 0;
+    private State state = State.Progress;
 
     /**
      * 사용자가 칸을 이동할 때 사용하는 메서드
      * <p>
      * 이동을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void move() {
+    public boolean move(final List<String> result, final String direction) {
+        require(!"U".equals(direction) && !"D".equals(direction), Error.MOVE);
+        require(state != State.Progress, Error.STATE);
+        if (!result.get(position).equals(direction)) {
+            state = State.Loss;
+            return false;
+        }
+        position++;
+        if (result.size() == position) state = State.Win;
+        return true;
+    }
+
+    private void require(boolean condition, Error error) {
+        if (condition) throw new IllegalArgumentException(error.getMsg());
     }
 
     /**
@@ -19,5 +39,11 @@ public class BridgeGame {
      * 재시작을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
     public void retry() {
+        state = State.Progress;
+        position = 0;
+    }
+
+    public State getGameState() {
+        return state;
     }
 }
