250c30ffc3f57b2fab59fd1d59e5e23e7d75f8d3
chanminmaxscaler <jcm04@naver.com>
2022-11-19 16:10:08 +0900

    test: Referee 클래스의 사용자의 입력 방향과 다리의 방향을 비교해 판정하는 기능을 테스트한다
    checkDirection 메소드에 다리, 방향, 위치가 입력되었을 때 방향이 맞는지 판단한 Judgement 객체를 반환하는지 확인한다.

---
+++ src/test/java/bridge/domain/referee/RefereeTest.java
@@ -0,0 +1,49 @@
+package bridge.domain.referee;
+
+import bridge.domain.bridge.Bridge;
+import bridge.domain.direction.Direction;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class RefereeTest {
+
+    private Referee referee;
+    private Bridge bridge;
+
+    @BeforeEach
+    void setUp() {
+        referee = new Referee();
+        List<Direction> directions = Stream.of("U", "D", "U", "D", "U")
+                .map(Direction::new)
+                .collect(Collectors.toList());
+        bridge = new Bridge(directions);
+    }
+
+    @DisplayName("checkDirection 메소드에 다리, 방향, 위치가 입력되었을 때 방향이 맞는지 판단한 Judgement 객체를 반환하는지 확인")
+    @ParameterizedTest
+    @MethodSource("provideArgumentsForCheckingTest")
+    void checkDirection_test(Direction direction, int location, boolean expected) {
+        Judgement judgement = referee.checkDirection(bridge, direction, location);
+
+        assertThat(judgement.isTrue()).isEqualTo(expected);
+    }
+
+    static Stream<Arguments> provideArgumentsForCheckingTest() {
+        return Stream.of(
+                Arguments.of(new Direction("U"), 0, true),
+                Arguments.of(new Direction("D"), 1, true),
+                Arguments.of(new Direction("D"), 2, false),
+                Arguments.of(new Direction("D"), 3, true),
+                Arguments.of(new Direction("D"), 4, false)
+        );
+    }
+}
