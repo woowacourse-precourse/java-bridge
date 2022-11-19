d10560f06928cbc699cc0f98fff2081fd7a09d1e
masiljangajji <hjk172262@gmail.com>
2022-11-19 11:57:58 +0900

    feat: 다리 길이를 입력받는 기능 추가

---
+++ src/main/java/view/InputView.java
@@ -0,0 +1,13 @@
+package view;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class InputView {
+    public String inputBridgeSize() {
+        String bridgeSize=Console.readLine();
+        return bridgeSize;
+    }
+
+
+
+}
