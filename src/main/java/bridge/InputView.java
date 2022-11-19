1cfc03647071b3e95293ffaab547ba45afb666b2
Sunghyun-0 <zkdldk95@naver.com>
2022-11-19 19:25:15 +0900

    readBridgeSize 기능 작성

--- src/main/java/bridge/InputView.java
+++ src/main/java/bridge/InputView.java
@@ -1,15 +1,20 @@
 package bridge;
 
+import camp.nextstep.edu.missionutils.Console;
+
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
  */
 public class InputView {
-
+    ValidationChecker validationChecker = new ValidationChecker();
     /**
      * 다리의 길이를 입력받는다.
      */
     public int readBridgeSize() {
-        return 0;
+        String bridgeSize = Console.readLine();
+        validationChecker.checkInputIsInteger(bridgeSize);
+        validationChecker.checkInputIsNaturalNumber(bridgeSize);
+        return Integer.parseInt(bridgeSize);
     }
 
     /**
@@ -25,4 +30,5 @@ public class InputView {
     public String readGameCommand() {
         return null;
     }
+
 }
