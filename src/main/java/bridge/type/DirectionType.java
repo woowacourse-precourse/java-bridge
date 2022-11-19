6d2a18cd6ebe2369c5e1ce91297d16828cc56fb6
qkrdmswl <eunji303@naver.com>
2022-11-19 18:06:27 +0900

    feat(ResultType): define direction type for moving

--- src/main/java/bridge/type/DirectionType.java
+++ src/main/java/bridge/type/DirectionType.java
@@ -1,2 +1,16 @@
-package bridge.type;public class DirectionType {
+package bridge.type;
+
+public enum DirectionType {
+    UP("U"),
+    DOWN("D");
+
+    private final String direction;
+
+    DirectionType(String direction) {
+        this.direction = direction;
+    }
+
+    public String getDirection() {
+        return direction;
+    }
 }
