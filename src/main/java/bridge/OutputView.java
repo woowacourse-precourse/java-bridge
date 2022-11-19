b4990769a7b84be7b13ef4747628eea6356fad99
JIHYEISWISDOM <wlgp8958@naver.com>
2022-11-19 17:06:46 +0900

    feat(OutputView): 다리 길이 입력 안내 문구 출력

--- src/main/java/bridge/OutputView.java
+++ src/main/java/bridge/OutputView.java
@@ -6,9 +6,13 @@ package bridge;
 public class OutputView {
 
     private final static String GAME_START_MESSAGE = "다리 건너기 게임을 시작합니다.";
-
+    private final static String BRIDGE_SIZE_INPUT_MESSAGE = "다리의 길이를 입력해주세요.";
     public void printGameStartMessage() {
         System.out.println(GAME_START_MESSAGE);
+    }
+
+    public void printBridgeLengthInputMessage() {
+        System.out.println(BRIDGE_SIZE_INPUT_MESSAGE);
     }
     /**
      * 현재까지 이동한 다리의 상태를 정해진 형식에 맞춰 출력한다.
