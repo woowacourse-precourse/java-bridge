1f81bc70e0b1fafd730ab796b71dcb5d8b44004f
impati <impati0716@icloud.com>
2022-11-19 11:03:51 +0900

    test(InputView) : 다리 길이에 대한 입력 Integer 변환 - 숫자가 아닌 겨우 변환 실패

--- src/test/java/bridge/InputViewTest.java
+++ src/test/java/bridge/InputViewTest.java
@@ -18,4 +18,18 @@ class InputViewTest {
         Integer len = Integer.parseInt(bridgeSize);
         Assertions.assertThat(len).isEqualTo(4);
     }
+
+    @Test
+    @DisplayName("다리 길이에 대한 입력 Integer 변환 - 숫자가 아닌 겨우 변환 실패")
+    public void Convert_fail_NotNumber() throws Exception{
+        validation = new Validation();
+        String bridgeSize = "hi";
+        Assertions.assertThatThrownBy(()->
+            validation.bridgeLen(bridgeSize))
+                .isInstanceOf(IllegalArgumentException.class);
+
+    }
+
+
+
 }
