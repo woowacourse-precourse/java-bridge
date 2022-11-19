9af9dc539b8ff7489402e8834c622a2368f45bd1
jaehyeon <jhjh0022@naver.com>
2022-11-20 03:55:22 +0900

    feat(BridgeShape): StringBuilder를 사용하여 칸 마다

--- src/main/java/bridge/BridgeShape.java
+++ src/main/java/bridge/BridgeShape.java
@@ -6,67 +6,82 @@ import java.util.Objects;
 
 public class BridgeShape {
 
-    private final String UP_BRIDGE ="U";
+    private final String UP_BRIDGE = "U";
     private final String DOWN_BRIDGE = "D";
+    private final String OPEN = "[";
+    private final String CLOSE = "]";
+    private final String CONNECT = "|";
     private final String PASS_STEP = " O ";
     private final String FAIL_STEP = " X ";
     private final String EMPTY_STEP = "   ";
     private final List<String> upBridgeShape = new ArrayList<>();
     private final List<String> downBridgeShape = new ArrayList<>();
 
-    public void moveBridge(String whereBridge, boolean checkBridge){
+    public void moveBridge(String whereBridge, boolean checkBridge) {
 
         moveNextStep(whereBridge, checkBridge);
 
     }
 
-    private void moveNextStep(String whereBridge, boolean checkBridge){
-        if(Objects.equals(whereBridge, UP_BRIDGE)){
+    private void moveNextStep(String whereBridge, boolean checkBridge) {
+        if (Objects.equals(whereBridge, UP_BRIDGE)) {
             moveUpBridge(checkBridge);
             notMoveDownBridge();
         }
-        if(Objects.equals(whereBridge, DOWN_BRIDGE)){
+        if (Objects.equals(whereBridge, DOWN_BRIDGE)) {
             moveDownBridge(checkBridge);
             notMoveUpBridge();
         }
     }
 
-    private void moveUpBridge(boolean checkBridge){
-        if(checkBridge){
+    private void moveUpBridge(boolean checkBridge) {
+        if (checkBridge) {
             upBridgeShape.add(PASS_STEP);
         }
-        if(!checkBridge){
+        if (!checkBridge) {
             upBridgeShape.add(FAIL_STEP);
         }
     }
 
-    private void moveDownBridge(boolean checkBridge){
-        if(checkBridge){
+    private void moveDownBridge(boolean checkBridge) {
+        if (checkBridge) {
             downBridgeShape.add(PASS_STEP);
         }
-        if(!checkBridge){
+        if (!checkBridge) {
             downBridgeShape.add(FAIL_STEP);
         }
     }
 
-    private void notMoveUpBridge(){
+    private void notMoveUpBridge() {
         upBridgeShape.add(EMPTY_STEP);
     }
 
-    private void notMoveDownBridge(){
+    private void notMoveDownBridge() {
         downBridgeShape.add(EMPTY_STEP);
     }
 
-    public void initializeBridgeShape(){
+    public void initializeBridgeShape() {
         upBridgeShape.clear();
         downBridgeShape.clear();
     }
 
-    public List<String> getUpBridge(){
-        return upBridgeShape;
+    public StringBuilder getUpBridge() {
+        return printBridge(upBridgeShape);
     }
 
-    public List<String> getDownBridge(){
-        return downBridgeShape;
+    public StringBuilder getDownBridge() {
+        return printBridge(downBridgeShape);
+    }
+
+    private StringBuilder printBridge(List<String> bridgeShape) {
+        StringBuilder bridge = new StringBuilder();
+
+        return bridge.append(OPEN)
+                .append(getCONNECT(bridgeShape))
+                .append(CLOSE);
+    }
+
+    private String getCONNECT(List<String> bridgeShape) {
+        return String.join(CONNECT, bridgeShape);
     }
 }
