b7cf7fc5e237712e37a21179ff508fad9901d2f8
minhee020 <minhee020@naver.com>
2022-11-19 15:25:13 +0900

    test : 다리 길이 입력) 범위를 벗어난 input test 구현

--- src/test/java/bridge/InputViewTest.java
+++ src/test/java/bridge/InputViewTest.java
@@ -28,6 +28,33 @@ public class InputViewTest extends NsTest {
         });
     }
 
+    @DisplayName("Bridge Size _ NOT_NUMBER 숫자 + 알파벳")
+    @Test
+    void readBridgeSize_NotInTheRightRange_Negative() {
+        assertSimpleTest(() -> {
+            runException("-4");
+            assertThat(output()).contains(ERROR_MESSAGE);
+        });
+    }
+
+    @DisplayName("Bridge Size _ NOT_NUMBER 숫자 + 알파벳")
+    @Test
+    void readBridgeSize_NotInTheRightRange_Zero() {
+        assertSimpleTest(() -> {
+            runException("0");
+            assertThat(output()).contains(ERROR_MESSAGE);
+        });
+    }
+
+    @DisplayName("Bridge Size _ NOT_NUMBER 숫자 + 알파벳")
+    @Test
+    void readBridgeSize_NotInTheRightRange_OutOfBounds() {
+        assertSimpleTest(() -> {
+            runException("21");
+            assertThat(output()).contains(ERROR_MESSAGE);
+        });
+    }
+
     @Override
     protected void runMain() {
         Application.main(new String[]{});
