35e6819b51f21879b935295b5c8bc6db5de95efe
Choi-JJunho <junho5336@gmail.com>
2022-11-19 23:43:18 +0900

    feat(User): add move, validate method

--- src/main/java/bridge/domain/User.java
+++ src/main/java/bridge/domain/User.java
@@ -3,14 +3,31 @@ package bridge.domain;
 public class User {
     private int step = 0;
     private boolean live = true;
+    private static final String UP_STARE = "U";
+    private static final String DOWN_STARE = "D";
 
     public void init() {
         this.step = 0;
         this.live = true;
     }
 
-    public void move() {
+    public void move(Bridge bridge, String input) {
+        validateStatus(input);
+        if (!bridge.isSafe(step, input)) {
+            live = false;
+            return;
+        }
+        step++;
+    }
 
+    private void validateStatus(String status) {
+        if (isNotStatus(status)) {
+            throw new IllegalArgumentException("[ERROR]");
+        }
+    }
+
+    private boolean isNotStatus(String status) {
+        return !UP_STARE.equals(status) && !DOWN_STARE.equals(status);
     }
 
     public int getStep() {
