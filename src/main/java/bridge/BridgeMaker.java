5984d946d417dee93ff5fb4a2e1e946578683caa
KSH <tkdgns543@gmail.com>
2022-11-19 20:01:03 +0900

    feat: 주어진 정수 길이 만큼의 무작위 다리 리스트

--- src/main/java/bridge/BridgeMaker.java
+++ src/main/java/bridge/BridgeMaker.java
@@ -1,5 +1,6 @@
 package bridge;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -18,6 +19,13 @@ public class BridgeMaker {
      * @return 입력받은 길이에 해당하는 다리 모양. 위 칸이면 "U", 아래 칸이면 "D"로 표현해야 한다.
      */
     public List<String> makeBridge(int size) {
-        return null;
+        List<String> result = new ArrayList<>();
+        String[] converter = {"D", "U"};
+        int index = bridgeNumberGenerator.generate();
+
+        for (int adder = 0; adder < size; adder++) {
+            result.add(converter[index]);
+        }
+        return result;
     }
 }
