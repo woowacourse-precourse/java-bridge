a9ea2d2487e0ff41668d9565cdbfd15cbb266571
lvalentine6 <ysr1227@naver.com>
2022-11-19 21:59:16 +0900

    feat : 메인 메서드 테스트

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -1,8 +1,20 @@
 package bridge;
 
+import bridge.model.BridgeMaker;
+import bridge.model.BridgeRandomNumberGenerator;
+import bridge.view.InputView;
+import bridge.view.OutputView;
+import java.util.List;
+
 public class Application {
 
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        BridgeMaker bridgeMaker = new BridgeMaker(new BridgeRandomNumberGenerator());
+        InputView inputView = new InputView();
+        OutputView outputView = new OutputView();
+        outputView.gameStart();
+        int size = inputView.readBridgeSize();
+        List<String> brigeResult = bridgeMaker.makeBridge(size);
+        System.out.println(brigeResult);
     }
 }
