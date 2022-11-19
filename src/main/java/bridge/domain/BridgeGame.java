203c24f6b3659e851bb5178c140ba2528dec2d19
Woojin Lee <dhkep03@gmail.com>
2022-11-20 04:51:26 +0900

    refactor(BridgeGame): 게임 상태를 받아서 저장하는 방식으로 수정

--- src/main/java/bridge/domain/BridgeGame.java
+++ src/main/java/bridge/domain/BridgeGame.java
@@ -28,11 +28,9 @@ public class BridgeGame {
      * 이동을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
     public String move(String moving) {
-        boolean isCorrectMoving = this.bridgeCalculator.go(moving);
-        this.bridgeMonitor.record(moving, isCorrectMoving);
-        if (!isCorrectMoving) {
-            this.gameStatus = FAIL;
-        }
+        GameStatus gameStatusAfterMoving = this.bridgeCalculator.go(moving);
+        this.gameStatus = gameStatusAfterMoving;
+        this.bridgeMonitor.record(moving, gameStatusAfterMoving);
         return this.bridgeMonitor.getPicture();
     }
 
