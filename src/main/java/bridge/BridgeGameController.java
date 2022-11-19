dbc3d49f250d98abd22cd3d71923650b532cfdc8
kimtaehoonDev <taehoon_dev@naver.com>
2022-11-19 12:05:41 +0900

    feat(Controller): 다리의 크기를 입력 받는 기능 구현

--- src/main/java/bridge/BridgeGameController.java
+++ src/main/java/bridge/BridgeGameController.java
@@ -1,4 +1,40 @@
 package bridge;
 
+import bridge.domain.Bridge;
+import bridge.domain.BridgeGame;
+import bridge.utils.BridgeMaker;
+import bridge.view.InputView;
+import bridge.view.OutputView;
+import java.util.List;
+
 public class BridgeGameController {
+    private BridgeGame game;
+    private final BridgeMaker bridgeMaker;
+    private final InputView inputView;
+    private final OutputView outputView;
+
+    private BridgeGameController() {
+        this.game = new BridgeGame();
+        this.bridgeMaker = new BridgeMaker(new BridgeRandomNumberGenerator());
+        this.inputView = new InputView();
+        this.outputView = new OutputView();
+    }
+
+    public static BridgeGameController init() {
+        return new BridgeGameController();
+    }
+
+    public void run() {
+        System.out.println("다리 건너기 게임을 시작합니다"); // 위치 여기가 맞을까?
+        Bridge bridge = this.makeBridge();
+    }
+
+    private Bridge makeBridge() {
+        try {
+            Integer bridgeSize = inputView.readBridgeSize();
+            return new Bridge(bridgeMaker.makeBridge(bridgeSize));
+        } catch (IllegalArgumentException e ) {
+            return makeBridge();
+        }
+    }
 }
--- src/main/java/bridge/view/InputView.java
+++ src/main/java/bridge/view/InputView.java
@@ -2,6 +2,9 @@ package bridge.view;
 
 // TODO 시그니처(인자, 이름)와 반환 타입은 변경 가능합니다
 //  메서드 추가도 가능합니다
+
+import camp.nextstep.edu.missionutils.Console;
+
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
  */
@@ -10,8 +13,13 @@ public class InputView {
     /**
      * 다리의 길이를 입력받는다.
      */
-    public int readBridgeSize() {
-        return 0;
+    public Integer readBridgeSize() {
+        System.out.println("다리의 길이를 입력해주세요");
+        String input = Console.readLine();
+        if (!input.matches("^[0-9]*$")) {
+            throw new IllegalArgumentException("숫자를 입력해주세요");
+        }
+        return Integer.parseInt(input);
     }
 
     /**
