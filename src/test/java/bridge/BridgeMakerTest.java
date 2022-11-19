3c0c834124373f3d5f1c3caaad389ef141292223
Limjongho <whdgh9873@icloud.com>
2022-11-19 14:22:26 +0900

    test(BridgeMakerTest): 다리 생성 테스트 추가

---
+++ src/test/java/bridge/BridgeMakerTest.java
@@ -0,0 +1,21 @@
+package bridge;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BridgeMakerTest {
+
+    BridgeMaker bridgeMaker = new BridgeMaker(new BridgeRandomNumberGenerator());
+
+    @DisplayName("다리 생성 테스트")
+    @Test
+    void makingBridgeTest() {
+        List<String> strings = bridgeMaker.makeBridge(10);
+        Assertions.assertThat(strings.contains("U") || strings.contains("D")).isEqualTo(true);
+    }
+}
