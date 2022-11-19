d3ea1103306aa058ce2a673dd08bf9058f9a5a95
JeonSangYong <tkddyd420@naver.com>
2022-11-19 15:16:17 +0900

    feat : 게임 로직 구현

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -1,8 +1,34 @@
 package bridge;
 
+import java.util.List;
+
 public class Application {
 
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        OutputView outputView = new OutputView();
+        outputView.printOf(OutputView.MESSAGE_GAME_START);
+        outputView.printOf(OutputView.MESSAGE_INPUT_BRIDGE_SIZE);
+        InputView inputView = new InputView();
+        int bridgeSize = inputView.readBridgeSize();
+        BridgeMaker bridgeMaker = new BridgeMaker(new BridgeRandomNumberGenerator());
+        List<String> bridge = bridgeMaker.makeBridge(bridgeSize);
+        BridgeGame bridgeGame = new BridgeGame(bridge);
+        while (true) {
+            outputView.printOf(OutputView.MESSAGE_INPUT_BRIDGE_MOVING);
+            bridgeGame.move(inputView.readMoving());
+            outputView.printMap(bridgeGame.resultBridgeMoving());
+            if (bridgeGame.isFinishBridge()) {
+                break;
+            }
+
+            if (bridgeGame.isGameEnd()) {
+                outputView.printOf(OutputView.MESSAGE_RETRY_OR_EXIT);
+                String gameCommand = inputView.readGameCommand();
+                if (!bridgeGame.retry(gameCommand)) {
+                    break;
+                }
+            }
+        }
+        outputView.printResult(bridgeGame.isSuccess(), bridgeGame.getGameCount());
     }
 }
