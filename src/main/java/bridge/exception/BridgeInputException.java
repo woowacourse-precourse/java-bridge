fcdaf0d60789ad7177a778e7b6a208c0b49adb33
yeeun <dpdms9621@naver.com>
2022-11-19 16:05:34 +0900

    feat(BridgeInputException): 다리 길이의 범위가 적절한지 판별하는 기능

--- src/main/java/bridge/exception/BridgeInputException.java
+++ src/main/java/bridge/exception/BridgeInputException.java
@@ -5,7 +5,8 @@ import bridge.message.ErrorMessage;
 public class BridgeInputException {
     public BridgeInputException() {}
 
-
+    private final int bridgeStartNum = 3;
+    private final int bridgeFinalNum = 20;
 
     // 숫자인지 아닌지 판별하는 기능
     public void validCharacter(String input) {
@@ -16,7 +17,13 @@ public class BridgeInputException {
         }
     }
 
-
+    // 숫자의 범위가 적절한지 판별하는 기능
+    public void validNumberRange(String input) {
+        int size = Integer.parseInt(input);
+        if(!(bridgeStartNum <= size && size <= bridgeFinalNum)) {
+            throw new IllegalArgumentException(ErrorMessage.BRIDGE_RANGE_ERROR_MESSAGE);
+        }
+    }
 
 
 }
