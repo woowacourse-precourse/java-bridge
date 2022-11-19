dd8c23e8c23dea0f18c5563d23be1d5921f3dcfe
egoing <dudqk9696@naver.com>
2022-11-19 14:48:48 +0900

    feat : 시도 횟수 저장

---
+++ src/main/java/domain/BridgeGame.java
@@ -0,0 +1,54 @@
+package domain;
+
+import view.InputView;
+
+/**
+ * 다리 건너기 게임을 관리하는 클래스
+ */
+public class BridgeGame {
+    InputView inputView = new InputView();
+
+    private int userPos;
+    private int attempts;
+
+    public BridgeGame() {
+        userPos = 0;
+        attempts = 1;
+    }
+
+    public int getUserPos() {
+        return userPos;
+    }
+    public int getAttempts() { return attempts; }
+
+    public boolean move(String choice,AllBridge allBridge) {
+        if(choice.equals(allBridge.getBridge().get(userPos))) {
+            userPos += 1;
+            return false;
+        }
+        if(retryOrEnd()) {
+            return true;
+        }
+        return false;
+    }
+
+    private boolean retryOrEnd() {
+        if(inputView.readGameCommand().equals("Q")) {
+            return true;
+        }
+        retry();
+        return false;
+    }
+
+    public boolean checkEnd(AllBridge allBridge) {
+        if(userPos == allBridge.getBridge().size()) {
+            return true;
+        }
+        return false;
+    }
+
+    public void retry() {
+        userPos = 0;
+        attempts += 1;
+    }
+}
