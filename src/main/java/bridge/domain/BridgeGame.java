2e082e71ba933668f390ae694728b0a980a3b3db
alstjr706 <alstjr706@naver.com>
2022-11-19 15:36:05 +0900

    feat : (다리 건너기) 사용자 입력 U/D 와 무작위 숫자가 같은지 판단하는 로직

--- src/main/java/bridge/domain/BridgeGame.java
+++ src/main/java/bridge/domain/BridgeGame.java
@@ -1,25 +1,49 @@
 package bridge.domain;
 
+import bridge.constants.Command;
+import java.util.HashMap;
+import java.util.List;
+
 /**
  * 다리 건너기 게임을 관리하는 클래스
  */
 public class BridgeGame {
 
+    private final HashMap<String, StringBuilder> bridgeState;
+
+    public BridgeGame(HashMap<String, StringBuilder> bridgeState) {
+        this.bridgeState = bridgeState;
+    }
+
     /**
      * 사용자가 칸을 이동할 때 사용하는 메서드
      * <p>
-     * 이동을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
+     * 이동을 위해 필요한 메서드의 반환 타입(return type),
+     * 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
+     * BridgeGame 클래스에서 InputView, OutputView 를 사용하지 않는다.
      */
-    public String move(String readMoving, String randomNumber) {
-        String printBridge = "";
-        if (readMoving.equals(randomNumber)) {
-            printBridge = "O";
+    public HashMap<String, StringBuilder> move(String playerMoving, String bridgeJudgment) {
+        if (playerMoving.equals("U") && bridgeJudgment.equals("O")) {
+            bridgeState.get(0).append(bridgeJudgment);
+            bridgeState.get(1).append(" ");
         }
 
-        if (!(readMoving.equals(randomNumber))) {
-            printBridge = "X";
+        if (playerMoving.equals("U") && bridgeJudgment.equals("X")) {
+            bridgeState.get(0).append(bridgeJudgment);
+            bridgeState.get(1).append(" ");
         }
-        return printBridge;
+
+        if (playerMoving.equals("D") && bridgeJudgment.equals("O")) {
+            bridgeState.get(0).append(" ");
+            bridgeState.get(1).append(bridgeJudgment);
+        }
+
+        if (playerMoving.equals("D") && bridgeJudgment.equals("X")) {
+            bridgeState.get(0).append(" ");
+            bridgeState.get(1).append(bridgeJudgment);
+        }
+
+        return bridgeState;
     }
 
     /**
