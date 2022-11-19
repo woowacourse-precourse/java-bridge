ea849cc0adeb8f9203e8f575af5cacef61887aaf
kimtaesoo99 <107785279+kimtaesoo99@users.noreply.github.com>
2022-11-19 23:56:22 +0900

    feat: (InputView) 다리길이를 입력받는 메서드 구현

---
+++ src/main/java/bridge/view/InputView.java
@@ -0,0 +1,39 @@
+package bridge.view;
+
+import static camp.nextstep.edu.missionutils.Console.readLine;
+
+import bridge.exception.BridgeGameInputException;
+
+/**
+ * 사용자로부터 입력을 받는 역할을 한다.
+ */
+public class InputView {
+    private final BridgeGameInputException bridgeGameInputException = new BridgeGameInputException();
+
+    /**
+     * 다리의 길이를 입력받는다.
+     */
+    public int readBridgeSize() {
+        try {
+            int bridgeSize = Integer.parseInt(readLine());
+            bridgeGameInputException.validateBridgeSize(bridgeSize);
+            return bridgeSize;
+        } catch (NumberFormatException exception) {
+            throw new IllegalArgumentException("[ERROR] 다리의 길이는 숫자만 입력해야합니다.");
+        }
+    }
+
+    /**
+     * 사용자가 이동할 칸을 입력받는다.
+     */
+    public String readMoving() {
+        return null;
+    }
+
+    /**
+     * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
+     */
+    public String readGameCommand() {
+        return null;
+    }
+}
