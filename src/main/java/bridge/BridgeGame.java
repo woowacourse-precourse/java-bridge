519a0b778eebce0955240647a891a971ff00304a
Nugur1i <duckhee092@naver.com>
2022-11-19 14:43:28 +0900

    feat(BridgeGame): implement bridge game all function

--- src/main/java/bridge/BridgeGame.java
+++ src/main/java/bridge/BridgeGame.java
@@ -24,16 +24,7 @@ public class BridgeGame {
         // 맞는지 틀린지 확인
         boolean retryOrNot = true;
         while(retryOrNot){
-            for (int order = 0; order <bridgeList.size(); order++) {
-                String upDownUserInput = inputView.readMoving();
-                if (Compare(bridgeList,upDownUserInput, order)){
-                    //출력
-                    continue;
-                }
-                //출력
-                retryOrNot = retry();
-                break;
-            }
+            bridgeMainGame(bridgeList,retryOrNot);
         }
     }
 
@@ -47,19 +38,30 @@ public class BridgeGame {
         return false;
     }
 
+    private boolean bridgeMainGame(List<String> bridgeList, boolean retryOrNot){
+        for (int order = 0; order <bridgeList.size(); order++) {
+            String upDownUserInput = inputView.readMoving();
+            if (Compare(bridgeList,upDownUserInput, order)){
+                //출력
+                retryOrNot = true;
+                continue;
+            }
+            //출력
+            retryOrNot = retry();
+            break;
+        }
+        return retryOrNot;
+    }
+
     /**
      * 사용자가 게임을 다시 시도할 때 사용하는 메서드
      * <p>
      * 재시작을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
     public boolean retry() {
-
         if (inputView.readGameCommand() == "R"){
             return true;
         }
-
         return false;
     }
-
-
 }
