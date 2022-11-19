f7ed134c7813fdc8e3044fad2b2136b3aa98d608
yun <yshyeon102@gmail.com>
2022-11-19 17:38:42 +0900

    feat(Validator): 다리 길이 입력에 대한 예외 검증 구현

---
+++ src/main/java/bridge/validator/Validator.java
@@ -0,0 +1,24 @@
+package bridge.validator;
+
+import bridge.exception.ExceptionType;
+
+public class Validator {
+    public static void validateBridgeSize(String input) {
+        validateIntegerType(input);
+        validateRange(Integer.parseInt(input));
+    }
+
+    private static void validateIntegerType(String input) {
+        try {
+            Integer.parseInt(input);
+        } catch (Exception e) {
+            throw new IllegalArgumentException(ExceptionType.INTEGER_TYPE_EXCEPTION.getErrorMessage());
+        }
+    }
+
+    private static void validateRange(int num) {
+        if (num < 3 || num > 20) {
+            throw new IllegalArgumentException(ExceptionType.BRIDGE_RANGE_EXCEPTION.getErrorMessage());
+        }
+    }
+}
