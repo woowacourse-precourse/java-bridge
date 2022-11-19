c9e63f973f9b3dfe34becf91cc9cd8b47dd9bc79
seonghoo1217 <seonghoo1217@naver.com>
2022-11-19 15:58:34 +0900

    feat (BridgeController,BridgeMaker) : add make bridge function
    bridge function element must be 'U' and 'D'
    
    bridge_list consist of BridgeNumberGenerator generate() method
    
    generate return 0 bridge_list 'D'
    else add 'U'

--- src/main/java/controller/BridgeController.java
+++ src/main/java/controller/BridgeController.java
@@ -1,11 +1,15 @@
 package controller;
 
-import bridge.InputView;
-import bridge.OutputView;
+import bridge.*;
+
+import java.util.List;
 
 public class BridgeController {
 	private final OutputView outputView = new OutputView();
 	private final InputView inputView = new InputView();
+	private final BridgeNumberGenerator bridgeNumberGenerator = new BridgeRandomNumberGenerator();
+	private final BridgeMaker bridgeMaker=new BridgeMaker(bridgeNumberGenerator);
+	private List<String> bridge_list;
 
 	public void BridgeGameStart(){
 		outputView.printStartGame();
@@ -14,7 +18,11 @@ public class BridgeController {
 		if(bridgeSize==0){
 			throw new IllegalStateException("[ERROR] 예기치 못한 에러를 만났습니다.");
 		}
+		bridgeMakeBySize(bridgeSize);
+	}
 
+	private void bridgeMakeBySize(int bridge_size){
+		bridge_list = bridgeMaker.makeBridge(bridge_size);
 	}
 
 }
