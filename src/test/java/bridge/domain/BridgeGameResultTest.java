1c3a50b4ca5d71f0a19398564624234f6471b3d7
anneu <b.imnuj@gmail.com>
2022-11-20 01:27:24 +0900

    test: 현재까지 이동한 다리 상태를 초기화하는 기능 테스트 추가

--- src/test/java/bridge/domain/BridgeGameResultTest.java
+++ src/test/java/bridge/domain/BridgeGameResultTest.java
@@ -2,7 +2,6 @@ package bridge.domain;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-import bridge.domain.BridgeGameResult;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -15,5 +14,18 @@ class BridgeGameResultTest {
     void checkIncreaseTryCount() {
         int actual = gameResult.increaseTryCount();
         assertThat(actual).isEqualTo(2);
+    }
+
+    @DisplayName("현재까지 이동한 다리 상태를 초기화한다.")
+    @Test
+    void checkInitMoveState() {
+        BridgeMoveState moveState = gameResult.getMoveState();
+        gameResult.updateMoveState("U", true);
+
+        gameResult.initMoveState();
+        BridgeMoveState newMoveState = gameResult.getMoveState();
+
+        boolean actual = moveState.equals(newMoveState);
+        assertThat(actual).isFalse();
     }
 }
