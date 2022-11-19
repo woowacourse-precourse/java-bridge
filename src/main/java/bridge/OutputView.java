6817fbe7fa1620c64ef43ed9bdb6001d320411b7
Your Name <you@example.com>
2022-11-19 14:35:24 +0900

    fix: 게임 시작 문구와 이동할 칸 선택 문구 작성

--- src/main/java/bridge/OutputView.java
+++ src/main/java/bridge/OutputView.java
@@ -23,11 +23,11 @@ public class OutputView {
     }
 
     public void printStart() {
-        System.out.println("다리 건너기 게임을 시작합니다.");
+        System.out.println("다리 건너기 게임을 시작합니다.\n다리의 길이를 입력해주세요.");
     }
 
     public void printSelectMove() {
-        System.out.println("다리의 길이를 입력해주세요.");
+        System.out.println("이동할 칸을 선택해주세요. (위: U, 아래: D)");
     }
 
     public void printGameOver() {
