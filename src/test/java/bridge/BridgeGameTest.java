bec8755dfd494e490044d72d691f8e4cbf2ac275
Devtist <yt930310@snu.ac.kr>
2022-11-19 21:12:19 +0900

    test: BridgeGame에 대한 테스트코드 작성

---
+++ src/test/java/bridge/BridgeGameTest.java
@@ -0,0 +1,26 @@
+package bridge;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class BridgeGameTest {
+
+	@DisplayName("다리가 유효한지 검증한다.")
+	@Test
+	void move() {
+	}
+
+	@Test
+	void retry() {
+	}
+
+	@Test
+	void isSuccess() {
+	}
+
+	@Test
+	void getCount() {
+	}
+}
