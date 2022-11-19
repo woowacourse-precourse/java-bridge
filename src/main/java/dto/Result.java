f3106ae1ba0e0a2f23ed80d0950a0b2260170ed7
CMHyuk <alsgur9710@naver.com>
2022-11-19 20:26:49 +0900

    feat(Result): 게임 결과(성공 여부, 시도 횟수)에 사용할 클래스

---
+++ src/main/java/dto/Result.java
@@ -0,0 +1,20 @@
+package dto;
+
+public class Result {
+
+    private String gameResult;
+    private int attempt;
+
+    public Result(String gameResult, int attempt) {
+        this.gameResult = gameResult;
+        this.attempt = attempt;
+    }
+
+    public int getAttempt() {
+        return attempt;
+    }
+
+    public String getGameResult() {
+        return gameResult;
+    }
+}
