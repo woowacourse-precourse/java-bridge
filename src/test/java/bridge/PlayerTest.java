186e01f9b989a0edab1bbba5888f1fcfddee0236
JUHA <khabh@naver.com>
2022-11-20 08:42:51 +0900

    test(Player): 사용자가 이동한 거리 추가 기능 테스트

--- src/test/java/bridge/PlayerTest.java
+++ src/test/java/bridge/PlayerTest.java
@@ -1,7 +1,11 @@
 package bridge;
 
 import bridge.controller.PlayerController;
+import bridge.domain.player.Player;
+import bridge.repository.PlayerRepository;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
@@ -15,6 +19,12 @@ import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 class PlayerTest {
     private final PlayerController playerController = new PlayerController();
+    private final PlayerRepository playerRepository = PlayerRepository.getInstance();
+
+    @BeforeEach
+    void setUp() {
+        playerController.generatePlayer();
+    }
 
     @DisplayName("이동할 칸이 올바른 형식이 아닌 경우")
     @ValueSource(strings = {"a", "u", "1", "ab"})
@@ -30,5 +40,14 @@ class PlayerTest {
             playerController.getMovingDirection();
         } catch (NoSuchElementException ignore) {}
         assertThat(out.toString()).contains(ERROR_MESSAGE);
+    }
+
+    @DisplayName("사용자의 이동 거리 증가")
+    @Test
+    void increaseMovedDistance() {
+        playerController.increaseMovedDistance();
+        Player player = playerRepository.get();
+
+        assertThat(player.getMovedDistance().toInt()).isEqualTo(1);
     }
 }
