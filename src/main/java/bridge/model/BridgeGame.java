e61224589814f03e1def218b48c2753ce360afe2
wkdtmdgnsp <wkdtmdgnsp@gmail.com>
2022-11-19 10:58:46 +0900

    feat(bridge) : 진행중인 위, 아래 다리 가져오는 기능 구현

--- src/main/java/bridge/model/BridgeGame.java
+++ src/main/java/bridge/model/BridgeGame.java
@@ -42,6 +42,14 @@ public class BridgeGame {
         return "X";
     }
 
+    public List<String> getUp() {
+        return up;
+    }
+
+    public List<String> getDown() {
+        return down;
+    }
+
     /**
      * 사용자가 게임을 다시 시도할 때 사용하는 메서드
      * <p>
