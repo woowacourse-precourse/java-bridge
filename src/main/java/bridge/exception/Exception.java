6ee8361622648db8a938aee8b2e92d47b85b0475
viyott-lover <jackk0616@gmail.com>
2022-11-19 20:51:49 +0900

    feat(Exception): 다리 번호가 0 또는 1이 아닐 경우 예외 처리

--- src/main/java/bridge/exception/Exception.java
+++ src/main/java/bridge/exception/Exception.java
@@ -1,7 +1,8 @@
 package bridge.exception;
 
 public enum Exception {
-    RANGE_EXCEPTION(new IllegalArgumentException("[ERROR] " + "3 ~ 20 사이의 수를 입력해주세요"));
+    SIZE_RANGE_EXCEPTION(new IllegalArgumentException("[ERROR] " + "3 ~ 20 사이의 수를 입력해주세요")),
+    BRIDGE_NUMBER_RANGE_EXCEPTION(new IllegalArgumentException("[ERROR] " + "0 또는 1이 아닙니다."));
     private IllegalArgumentException exception;
 
     Exception(IllegalArgumentException exception) {
