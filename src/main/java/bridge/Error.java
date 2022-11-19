6f1c757afa41fd504a700f63919dbfffbe37e10b
OhJintaek <sunshot@hanyang.ac.kr>
2022-11-20 00:35:35 +0900

    type:refactor 에러 메시지 수정

--- src/main/java/bridge/Error.java
+++ src/main/java/bridge/Error.java
@@ -8,19 +8,19 @@ public class Error {
 
     static void isNumber(String userInput) {
         if (!userInput.matches("[0-9]+")) {
-            throw new IllegalArgumentException("[ERROR] 3이상 20이하의 숫자를 입력해야 합니다.");
+            throw new IllegalArgumentException("[ERROR] 다리 길이는 3부터 20 사이의 숫자여야 합니다.");
         }
     }
 
     static void isStartZero(String userInput) {
         if (userInput.charAt(0) == '0') {
-            throw new IllegalArgumentException("[ERROR] 입력하는 숫자가 0으로 시작되지 않아야 합니다.");
+            throw new IllegalArgumentException("[ERROR] 다리 길이가 0으로 시작되지 않아야 합니다.");
         }
     }
 
     static void isValidNumber(int number) {
         if (number < minBridgeLength || number > maxBridgeLength) {
-            throw new IllegalArgumentException("[ERROR] 3이상 20이하의 숫자를 입력해야 합니다.");
+            throw new IllegalArgumentException("[ERROR] 다리 길이는 3부터 20 사이의 숫자여야 합니다.");
         }
     }
 
