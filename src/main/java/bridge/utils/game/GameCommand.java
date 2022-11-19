5439a98d35e21d4782dbec8e589fa2d0efcb316e
apptie <appti@naver.com>
2022-11-19 14:32:14 +0900

    feat(GameCommand): 게임을 다시 시도할지 여부를 선택하는 커맨드를 관리하는 기능 추가

---
+++ src/main/java/bridge/utils/game/GameCommand.java
@@ -0,0 +1,26 @@
+package bridge.utils.game;
+
+import bridge.utils.message.ExceptionMessageUtils;
+import java.util.Arrays;
+
+public enum GameCommand {
+    RETRY("Q", GameStatus.GAME_EXIT),
+    EXIT("R", GameStatus.GAME_PLAY);
+
+    private final String command;
+    private final GameStatus gameStatus;
+
+    GameCommand(String command, GameStatus gameStatus) {
+        this.command = command;
+        this.gameStatus = gameStatus;
+    }
+
+    public static GameStatus findNextGameOver(String command) {
+        return Arrays.stream(GameCommand.values())
+                .filter(gameCommand -> gameCommand.command.equals(command))
+                .map(gameCommand -> gameCommand.gameStatus)
+                .findAny()
+                .orElseThrow(() -> new IllegalArgumentException(ExceptionMessageUtils
+                        .WRONG_GAME_COMMAND.getMessage()));
+    }
+}
