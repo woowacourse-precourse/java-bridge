bdb5c0c408deec09510c87abc1138304d3f58550
Donguk <95donguk.dev@gmail.com>
2022-11-19 23:19:52 +0900

    refactor(Application): try catch 문 삭제

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -1,17 +1,12 @@
 package bridge;
 
 import bridge.controller.BridgeController;
-import bridge.utils.message.ErrorMessage;
 
 public class Application {
 
     public static void main(String[] args) {
-        try {
-            BridgeController bridgeController = new BridgeController();
-            bridgeController.startGame();
-            bridgeController.playGame();
-        } catch (IllegalArgumentException exception) {
-            System.out.println(ErrorMessage.EXIT.getMessage());
-        }
+        BridgeController bridgeController = new BridgeController();
+        bridgeController.startGame();
+        bridgeController.playGame();
     }
 }
