235af324970cdaafeebd877c66aa2b9e7d3193e4
dydtmd6801 <dydtmd6801@naver.com>
2022-11-19 13:10:22 +0900

    feat(Exception): 3보다 크고 20보다 작은지 확인하는 기능

--- src/main/java/bridge/Exception.java
+++ src/main/java/bridge/Exception.java
@@ -4,4 +4,8 @@ public class Exception {
     public Boolean isEmpty(String checkEmpty) {
         return checkEmpty.isEmpty() && checkEmpty.isBlank();
     }
+
+    public Boolean isRangeInThreeAndTwenty(int bridgeSize) {
+        return bridgeSize < 3 || bridgeSize > 20;
+    }
 }
