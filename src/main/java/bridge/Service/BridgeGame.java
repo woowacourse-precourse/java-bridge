1975572b6c3f6c0dc060a00f362d3ef1efd5c7ed
jinju9553 <69393506+jinju9553@users.noreply.github.com>
2022-11-20 00:48:55 +0900

    Feat(service) 기능5 - 칸을 이동했음을 나타내는 메소드 추가

--- src/main/java/bridge/Service/BridgeGame.java
+++ src/main/java/bridge/Service/BridgeGame.java
@@ -7,12 +7,18 @@ public class BridgeGame {
 
     //TODO: BridgeGame 클래스에서 InputView, OutputView 를 사용하지 않는다.
     //TODO: 필드 추가 가능, 인자와 반환타입 변경 가능, 그러나 메서드 이름 변경 금지
+
     /**
      * 사용자가 칸을 이동할 때 사용하는 메서드
      * <p>
      * 이동을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void move() {
+    public int move(boolean isCrossable, int progress) {
+        if (isCrossable) {
+            progress++;
+        }
+
+        return progress;
     }
 
     /**
@@ -22,9 +28,8 @@ public class BridgeGame {
      */
     public void retry() {
     }
-
-    //TODO: 게임 진행을 위해 필요한 메서드를 추가 하거나 변경할 수 있다.
-    public void isCleared(){
+    
+    public void isCleared() {
         //게임을 모두 클리어했는지 판별한다.
     }
 }
