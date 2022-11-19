0d582f85d5e47ee724afc8b55acbf2424faca1d8
yongseungLeeDec <yonson.dev@gmail.com>
2022-11-19 12:22:53 +0900

    refactor: 주석 삭제
    프로젝트 초기 설정 시 있던 주석을 삭제함.

--- src/main/java/bridge/InputView.java
+++ src/main/java/bridge/InputView.java
@@ -1,13 +1,8 @@
 package bridge;
 
-/**
- * 사용자로부터 입력을 받는 역할을 한다.
- */
 public class InputView {
     private static final String PROMPT_FOR_BRIDGE_LENGTH = "다리의 길이를 입력해주세요.";
-    /**
-     * 다리의 길이를 입력받는다.
-     */
+
     public void printPromptForBridgeLength() {
         System.out.println(PROMPT_FOR_BRIDGE_LENGTH);
     }
@@ -16,16 +11,10 @@ public class InputView {
         return 0;
     }
 
-    /**
-     * 사용자가 이동할 칸을 입력받는다.
-     */
     public String readMoving() {
         return null;
     }
 
-    /**
-     * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
-     */
     public String readGameCommand() {
         return null;
     }
