caf6859f67ec0c1d849dfed02a0cf73353acd6c7
impati <impati0716@icloud.com>
2022-11-19 11:53:39 +0900

    feat (Validation) : 이동할 칸에 대한 입력 validation

--- src/main/java/bridge/Validation.java
+++ src/main/java/bridge/Validation.java
@@ -2,7 +2,7 @@ package bridge;
 
 public class Validation {
     private final String BRIDGE_MESSAGE = "[ERROR] 다리 길이 입력은 3이상 20이하의 숫자를 입력할 수 있습니다.";
-
+    private final String MOVE_MESSAGE = "[ERROR] 이동할 칸은 (위: U, 아래: D)만 입력해주세요.";
 
     public void bridgeLen(String input){
         notNumberFormat(input);
@@ -15,5 +15,9 @@ public class Validation {
         }catch (NumberFormatException e){
             throw new IllegalArgumentException(BRIDGE_MESSAGE);
         }
+    }
+    public void moveCommand(String cmd){
+        if(!(cmd.equals("U") || cmd.equals("D")))
+            throw new IllegalArgumentException(MOVE_MESSAGE);
     }
 }
