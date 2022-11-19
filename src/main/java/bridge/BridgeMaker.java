3db67f246dcf684ff57caccf8023d0d51aa1aa87
RumbleKAT <reki318@naver.com>
2022-11-19 10:52:40 +0900

    (feat) 3. 다리 생성 객체 구현

--- src/main/java/bridge/BridgeMaker.java
+++ src/main/java/bridge/BridgeMaker.java
@@ -1,5 +1,6 @@
 package bridge;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -18,6 +19,13 @@ public class BridgeMaker {
      * @return 입력받은 길이에 해당하는 다리 모양. 위 칸이면 "U", 아래 칸이면 "D"로 표현해야 한다.
      */
     public List<String> makeBridge(int size) {
-        return null;
+        if(size < 3 || size > 21) throw new IllegalArgumentException("[ERROR] 다리 길이는 3부터 20 사이의 숫자여야 합니다.");
+        List<String> bridge = new ArrayList<>();
+        for(int i =0;i<size;i++){
+            int cur = bridgeNumberGenerator.generate();
+            if(cur == 0) bridge.add("D");
+            else if(cur == 1) bridge.add("U");
+        }
+        return bridge;
     }
 }
