40133735b5a4301ba88b43ef2ca84bfcc6e4fd00
JUHA <khabh@naver.com>
2022-11-19 23:39:28 +0900

    refactor(Player): 이동할 칸 입력 PlayerController로 이동

---
+++ src/main/java/bridge/controller/PlayerController.java
@@ -0,0 +1,29 @@
+package bridge.controller;
+
+import bridge.service.PlayerService;
+import bridge.validator.MovingDirectionValidator;
+import bridge.view.InputView;
+
+public class PlayerController {
+
+    private final PlayerService playerService = new PlayerService();
+
+    public void generatePlayer() {
+        playerService.initPlayer();
+    }
+
+    public int getCurrentDistance() {
+        return playerService.getMovedDistance();
+    }
+
+    public String getMovingDirection() {
+        try {
+            String movingDirection = InputView.readMoving();
+            MovingDirectionValidator.validateDirection(movingDirection);
+            return movingDirection;
+        } catch (IllegalArgumentException exception) {
+            System.out.println(exception.getMessage());
+            return getMovingDirection();
+        }
+    }
+}
