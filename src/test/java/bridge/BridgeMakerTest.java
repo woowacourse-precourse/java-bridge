aca9944f4ccdc0bf0632e38182df8b1e4cd43524
jjmin9797 <jjmin9797@naver.com>
2022-11-19 17:40:29 +0900

    test(BridgeMakerTest): 다리 랜덤 생성 테스트

---
+++ src/test/java/bridge/BridgeMakerTest.java
@@ -0,0 +1,23 @@
+package bridge;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
+
+@DisplayName("[다리생성테스트]")
+class BridgeMakerTest {
+
+    @DisplayName("다리 랜덤 생성 테스트")
+    @ValueSource(ints = {3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20})
+    @ParameterizedTest
+    void makeBridgeTest(int size) {
+        BridgeMaker bridgeMaker = new BridgeMaker(new BridgeRandomNumberGenerator());
+        assertThat(bridgeMaker.makeBridge(size))
+                .isNotEmpty()
+                .hasSize(size);
+    }
+}
