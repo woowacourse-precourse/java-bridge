af1c2bf5a8a49e6913f11a6f217db8e9227a242e
shinD <huipulci1@naver.com>
2022-11-19 18:02:20 +0900

    style: MoveCommandTest import문 정리

--- src/test/java/bridge/domain/command/MoveCommandTest.java
+++ src/test/java/bridge/domain/command/MoveCommandTest.java
@@ -1,12 +1,9 @@
 package bridge.domain.command;
 
-import bridge.domain.direction.Direction;
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
 
 class MoveCommandTest {
 
