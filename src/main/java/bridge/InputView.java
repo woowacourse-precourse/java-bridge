a3d4bb95d6a48623af1114f80fc64ab98a866514
kylew1004 <kylew1004@gmail.com>
2022-11-19 20:31:10 +0900

    feat: 출력에 사용될 enum 작성

--- src/main/java/bridge/InputView.java
+++ src/main/java/bridge/InputView.java
@@ -3,26 +3,54 @@ package bridge;
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
  */
+
+import camp.nextstep.edu.missionutils.Console;
+
 public class InputView {
 
     /**
      * 다리의 길이를 입력받는다.
      */
+
     public int readBridgeSize() {
-        return 0;
+        int bridgeSize = Integer.parseInt(Console.readLine());
+        checkBridgeSize(bridgeSize);
+        return bridgeSize;
     }
 
     /**
      * 사용자가 이동할 칸을 입력받는다.
      */
     public String readMoving() {
-        return null;
+        String moving = Console.readLine();
+        checkMoving(moving);
+        return moving;
     }
 
     /**
      * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
      */
     public String readGameCommand() {
-        return null;
+        String gameCommand = Console.readLine();
+        checkGameCommand(gameCommand);
+        return gameCommand;
+    }
+
+    private void checkBridgeSize(int bridgeSize) {
+        if(bridgeSize < 3 || bridgeSize > 20) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private void checkMoving(String moving) {
+        if(moving != "U" || moving != "D") {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private void checkGameCommand(String gameCommand) {
+        if(gameCommand != "R" || gameCommand != "Q") {
+            throw new IllegalArgumentException();
+        }
     }
 }
