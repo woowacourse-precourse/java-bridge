8feb53eacc9f83b4008f423525bcd0b2aad32776
watterricecake <fruturum@nate.com>
2022-11-19 12:40:36 +0900

    refactor: test코드 상속

--- src/test/java/bridge/ViewTest.java
+++ src/test/java/bridge/ViewTest.java
@@ -5,7 +5,7 @@ import bridge.model.User;
 import bridge.view.Input;
 import bridge.view.InputView;
 import bridge.view.OutputView;
-import camp.nextstep.edu.missionutils.test.NsTest;
+
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
@@ -13,7 +13,7 @@ import java.util.List;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.assertj.core.util.Lists.newArrayList;
 
-public class ViewTest extends NsTest {
+public class ViewTest extends ApplicationTest {
     private static final Integer TEST_INT = 3;
     private static final String TEST_STR = "U";
     private TestInput testInput = new TestInput(TEST_INT, TEST_STR);
@@ -69,11 +69,6 @@ public class ViewTest extends NsTest {
                 "총 시도한 횟수: 3"
         );
     }
-    @Override
-    protected void runMain() {
-        Application.main(new String[]{});
-    }
-
     static class TestInput implements Input {
         private Integer inputInt;
         private String inputStr;
@@ -89,19 +84,6 @@ public class ViewTest extends NsTest {
         @Override
         public String inputString() {
             return this.inputStr;
-        }
-    }
-    static class TestNumberGenerator implements BridgeNumberGenerator {
-
-        private final List<Integer> numbers;
-
-        TestNumberGenerator(List<Integer> numbers) {
-            this.numbers = numbers;
-        }
-
-        @Override
-        public int generate() {
-            return numbers.remove(0);
         }
     }
 }
