3fd8dc41344f430f561e489375183ffee0b7f3c0
Yerim lee <103623292+lyrWinterCat@users.noreply.github.com>
2022-11-19 19:26:34 +0900

    feat (InputView) : 사용자 이동 칸 입력 구현

--- src/main/java/bridge/view/InputView.java
+++ src/main/java/bridge/view/InputView.java
@@ -21,7 +21,9 @@ public class InputView {
      * 사용자가 이동할 칸을 입력받는다.
      */
     public String readMoving() {
-        return null;
+        String moving = Console.readLine();
+        Validator.validateMoving(moving);
+        return moving;
     }
 
     /**
