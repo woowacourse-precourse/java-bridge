7d78c6ca6a4d95e7fefff77c08348eeaeadff831
antaeho <an981022@naver.com>
2022-11-19 16:14:34 +0900

    Test: 종합 테스트 추가

--- src/test/java/bridge/ApplicationTest.java
+++ src/test/java/bridge/ApplicationTest.java
@@ -88,6 +88,29 @@ class ApplicationTest extends NsTest {
     }
 
     @Test
+    void 종합_테스트(){
+        assertRandomNumberInRangeTest(() -> {
+            run( "a", "123", "5", "a", "U", "1", "D", "asd", "D", "r", "R",  "U", "D", "U", "U", "U", "R", "U", "D", "U", "U", "D");
+            assertThat(output()).contains(
+                    ERROR_MESSAGE,
+                    "최종 게임 결과",
+                    "[ O |   |   ]",
+                    "[   | O | X ]",
+                    "[ O |   | O | O | X ]",
+                    "[   | O |   |   |   ]",
+                    "[ O |   | O | O |   ]",
+                    "[   | O |   |   | O ]",
+                    "게임 성공 여부: 성공",
+                    "총 시도한 횟수: 3"
+            );
+
+            int upSideIndex = output().indexOf("[ O |   | O | O |   ]");
+            int downSideIndex = output().indexOf("[   | O |   |   | O ]");
+            assertThat(upSideIndex).isLessThan(downSideIndex);
+        }, 1, 0, 1,1,0);
+    }
+
+    @Test
     void 예외_테스트() {
         assertSimpleTest(() -> {
             runException("a");
