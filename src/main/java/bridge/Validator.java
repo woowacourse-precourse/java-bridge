4ddbcbcda33ea418d3859e4df6f841231faaca1c
goldcollar95 <yjs7298@naver.com>
2022-11-19 21:05:54 +0900

    feat(Validator): 모든 예외사항처리 구현

--- src/main/java/bridge/Validator.java
+++ src/main/java/bridge/Validator.java
@@ -1,4 +1,33 @@
 package bridge;
 
+import java.util.regex.Pattern;
+
 public class Validator {
+    private static final Pattern NUMBER_PATTERN = Pattern.compile("^[0-9]+$");
+
+    public static void validatorOnlyNumber(String input){
+        if(!NUMBER_PATTERN.matcher(input).matches()){
+            throw new IllegalArgumentException();
+        }
+    }
+
+    public static void validatorRangeNumber(String input){
+        int InputToInt = Integer.parseInt(input);
+
+        if(InputToInt > 20 || InputToInt < 3){
+            throw new IllegalArgumentException();
+        }
+    }
+
+    public static void validatorMoving(String input){
+        if(!(input.equals("U") || input.equals("D"))){
+            throw new IllegalArgumentException();
+        }
+    }
+
+    public static void validatorRestartOREnd(String input){
+        if(!(input.equals("R") || input.equals("Q"))){
+            throw new IllegalArgumentException();
+        }
+    }
 }
