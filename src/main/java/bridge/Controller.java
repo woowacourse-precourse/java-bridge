254f5f8d2b7e7713d7ffffe6fbe4f03a368e541f
Cheon-DM <76736548+Cheon-DM@users.noreply.github.com>
2022-11-19 22:10:00 +0900

    feat(controller): 다리 건너기 기능 컨트롤러 추가

--- src/main/java/bridge/Controller.java
+++ src/main/java/bridge/Controller.java
@@ -1,25 +1,33 @@
 package bridge;
 
 import bridge.view.InputView;
-
-import java.util.List;
+import bridge.view.OutputView;
 
 public class Controller {
 
     private final InputView inputView;
+    private final OutputView outputView;
 
-    public Controller(InputView inputView) {
+    public Controller(InputView inputView, OutputView outputView) {
         this.inputView = inputView;
+        this.outputView = outputView;
     }
 
     public void run(){
         inputView.getBridgeGameStartMsg();
         int bridgeSize = inputView.readBridgeSize();
         BridgeMaker bridgeMaker = new BridgeMaker(new BridgeRandomNumberGenerator());
-        List<String> bridge = bridgeMaker.makeBridge(bridgeSize);
-        while(true){
+        Bridge bridge = new Bridge(bridgeMaker.makeBridge(bridgeSize));
+        BridgeGame bridgeGame = new BridgeGame(bridge);
+        int step = 0;
+        while(step != bridgeSize){
             String moving = inputView.readMoving();
+            boolean isEqual = bridgeGame.move(moving, step);
+            outputView.printMap(bridgeGame);
+            if (!isEqual){
 
+            }
+            step++;
         }
     }
 }
