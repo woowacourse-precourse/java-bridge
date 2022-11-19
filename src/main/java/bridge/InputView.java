9952bcfb4d1634224fe4ae1fbbd30b4eb79c2b3b
Dan-HeeYoung9182 <ldan9182@gmail.com>
2022-11-20 00:38:13 +0900

    게임 재시작 여부 입력 예외 처리

--- src/main/java/bridge/InputView.java
+++ src/main/java/bridge/InputView.java
@@ -12,6 +12,7 @@ public class InputView{
     static final String ERROR_NUMBER = "[ERROR] 다리 길이는 3부터 20 사이의 숫자여야 합니다.";
     static final String ERROR_STRING = "[ERROR] 숫자를 입력하여 주세요.";
     static final String ERROR_UP_DOWN = "[ERROR] U 또는 D를 입력하여 주세요.";
+    static final String ERROR_RETRY = "[ERROR] R 또는 Q를 입력하여 주세요.";
     static final String MOVE = "이동할 칸을 선택해주세요. (위: U, 아래: D)";
     static final String RETRY = "게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)";
     static final int UPPER_BOUND = 20;
@@ -46,7 +47,15 @@ public class InputView{
      */
     public String readGameCommand() {
         System.out.println(RETRY);
-        return Console.readLine();
+        String redoOrQuit= "";
+        try{
+            redoOrQuit = Console.readLine();
+            redoOrQuitCheck(redoOrQuit);
+        } catch(IllegalArgumentException err){
+            System.out.println(err.getMessage());
+            readGameCommand();
+        }
+        return redoOrQuit;
     }
 
     public static int catchException() {
@@ -81,6 +90,10 @@ public class InputView{
 
     public static void upOrDownCheck(String move) {
         if (!(move.equals(OutputView.UP) || move.equals(OutputView.DOWN))) throw new IllegalArgumentException(ERROR_UP_DOWN);
+    }
+
+    public static void redoOrQuitCheck(String move) {
+        if (!(move.equals(BridgeGame.REDO) || move.equals(BridgeGame.QUIT))) throw new IllegalArgumentException(ERROR_RETRY);
     }
 
 }
