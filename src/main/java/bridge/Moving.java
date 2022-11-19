4a0e2ba4b746fb1e252e1696c87401a91a72f776
Sangwoo Shin <sangwoonoel@naver.com>
2022-11-19 22:45:34 +0900

    feat(Moving): 움직일 칸에 대한 상수를 관리하는 enum  생성

---
+++ src/main/java/bridge/Moving.java
@@ -0,0 +1,17 @@
+package bridge;
+
+public enum Moving {
+
+    Up("U"),
+    Down("D");
+
+    private final String moving;
+
+    Moving(String moving) {
+        this.moving = moving;
+    }
+
+    public String getMoving() {
+        return moving;
+    }
+}
