605604325e7db26f20765070bc8a01167c11cb26
panorama713 <yaiba713@gmail.com>
2022-11-19 15:41:32 +0900

    Test(bridge): InputViewTest 뼈대코드 추가

---
+++ src/test/java/bridge/InputViewTest.java
@@ -0,0 +1,31 @@
+package bridge;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * packageName : bridge
+ * fileName : InputViewTest
+ * author : gim-yeong-geun
+ * date : 2022/11/19
+ * description :
+ * ===========================================================
+ * DATE            AUTHOR             NOTE
+ * -----------------------------------------------------------
+ * 2022/11/19         gim-yeong-geun          최초 생성
+ */
+class InputViewTest {
+
+    @Test
+    void readBridgeSize() {
+    }
+
+    @Test
+    void readMoving() {
+    }
+
+    @Test
+    void readGameCommand() {
+    }
+}
