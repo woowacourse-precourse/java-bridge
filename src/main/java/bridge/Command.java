6b27c1fb53f5aab56c1aefbfeae5a73cef07dfd0
OhJintaek <sunshot@hanyang.ac.kr>
2022-11-19 23:14:25 +0900

    type:refactor Command 열거형에서 사용되지 않는 commandNum 요소를 삭제

--- src/main/java/bridge/Command.java
+++ src/main/java/bridge/Command.java
@@ -1,25 +1,19 @@
 package bridge;
 
 public enum Command {
-    UP("U", 0),
-    DOWN("D", 1),
-    RESTART("R", 2),
-    QUIT("Q", 3),
-    SUCCESS("O", 4),
-    FAILURE("X", 5);
+    UP("U"),
+    DOWN("D"),
+    RESTART("R"),
+    QUIT("Q"),
+    SUCCESS("O"),
+    FAILURE("X");
 
     private final String value;
-    private final int commandNum;
-    private Command(String value, int commandNum) {
+    private Command(String value) {
         this.value = value;
-        this.commandNum = commandNum;
     }
 
     public String getValue() {
         return value;
-    }
-
-    public int getCommandNum() {
-        return commandNum;
     }
 }
