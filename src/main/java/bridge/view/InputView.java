42089483893feb7552f48325bb5e2fb54f156b22
yujiyea <54650556+yujiyea@users.noreply.github.com>
2022-11-19 20:02:40 +0900

    feat(view/InputView.java): 게임 시작 문구 출력하는 함수 구현

--- src/main/java/bridge/view/InputView.java
+++ src/main/java/bridge/view/InputView.java
@@ -1,28 +1,13 @@
-package bridge;
+package bridge.view;
 
-/**
- * 사용자로부터 입력을 받는 역할을 한다.
- */
+import camp.nextstep.edu.missionutils.Console;
+
+import java.util.regex.Pattern;
+
+import static bridge.view.InputViewString.*;
+
 public class InputView {
-
-    /**
-     * 다리의 길이를 입력받는다.
-     */
-    public int readBridgeSize() {
-        return 0;
-    }
-
-    /**
-     * 사용자가 이동할 칸을 입력받는다.
-     */
-    public String readMoving() {
-        return null;
-    }
-
-    /**
-     * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
-     */
-    public String readGameCommand() {
-        return null;
+    public void startMessage(){
+        System.out.println(START_MESSAGE);
     }
 }
