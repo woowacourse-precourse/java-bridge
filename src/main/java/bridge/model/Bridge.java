7a530b4e9d43f913849028483f2db7dccd1cd3fe
MJoon-Jung <gjgjajaj31@gmail.com>
2022-11-19 15:57:41 +0900

    feat<Bridge>: BridgeMaker에서 만든 String list를 이용해 Bridge 생성

--- src/main/java/bridge/model/Bridge.java
+++ src/main/java/bridge/model/Bridge.java
@@ -1,4 +1,16 @@
 package bridge.model;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class Bridge {
+    private final List<Direction> bridge;
+
+    public Bridge(List<String> bridge) {
+        this.bridge = toDirection(bridge);
+    }
+
+    private List<Direction> toDirection(List<String> bridge) {
+        return bridge.stream().map(Direction::valueOf).collect(Collectors.toList());
+    }
 }
