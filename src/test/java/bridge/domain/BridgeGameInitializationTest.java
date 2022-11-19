c742bfdb321fd18022ef2605075b8d90e16d9603
baexxbin <soo3886@gmail.com>
2022-11-19 19:46:32 +0900

    test(BridgeGameInitialization): 다리길이입력, 초기화, 다리객체 반환의 흐름 테스트

---
+++ src/test/java/bridge/domain/BridgeGameInitializationTest.java
@@ -0,0 +1,41 @@
+package bridge.domain;
+
+import static org.assertj.core.api.Assertions.*;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.PrintStream;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class BridgeGameInitializationTest {
+	private static final BridgeGameInitialization bridgeGameInitialization = new BridgeGameInitialization();
+
+	// @BeforeAll
+	// static void beforeAll() {
+	// 	bridgeGameInitialization = new BridgeGameInitialization();
+	// }
+
+	@DisplayName("다리 초기화 검사")
+	@Test
+	void initBridge() {
+		//	given
+		BridgeGameInitialization bridgeGameInitialization = new BridgeGameInitialization();
+
+		String notNumber = "3a";
+		String outboundRange = "25";
+		String correct = "5";
+
+		//	when
+		OutputStream out = new ByteArrayOutputStream();
+		System.setOut(new PrintStream(out));
+
+		InputStream in = new ByteArrayInputStream(correct.getBytes());
+		System.setIn(in);
+
+		assertThat(bridgeGameInitialization.initBridge().showBridge()).contains("U", "D");
+	}
+}
