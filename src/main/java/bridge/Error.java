36995f9152ffc2aa270b0d35d394f081580ec52f
OhJintaek <sunshot@hanyang.ac.kr>
2022-11-19 20:06:26 +0900

    type:feat 숫자가 아닌 입력이 들어올 경우 에외를 발생하는 기능 추가

--- src/main/java/bridge/Error.java
+++ src/main/java/bridge/Error.java
@@ -4,6 +4,12 @@ public class Error {
     static final int minBridgeLength = 3;
     static final int maxBridgeLength = 20;
 
+    static void isNumber(String userInput) {
+        if (!userInput.matches("[0-9]+")) {
+            throw new IllegalArgumentException("[ERROR] 숫자를 입력해야 합니다.");
+        }
+    }
+
     static void isValidNumber(int number) {
         if (number < minBridgeLength || number < maxBridgeLength) {
             throw new IllegalArgumentException("[ERROR] 3에서 20 사이의 숫자를 입력해야 합니다.");
