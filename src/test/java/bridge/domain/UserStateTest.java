2108ec18f6d97e6c1492989a65fad82d51b59c16
recicoco068 <gkapf838@gmail.com>
2022-11-19 11:08:55 +0900

    test(UserStateTest.java) : 다리 다 건넜는지 여부 얻는 기능 테스트

--- src/test/java/bridge/domain/UserStateTest.java
+++ src/test/java/bridge/domain/UserStateTest.java
@@ -15,4 +15,19 @@ class UserStateTest {
         userState.moveToNext(false, Command.UP.getCommand());
         assertThat(userState.toString()).isEqualTo("[ O |   | X ]\n[   | O |   ]");
     }
+
+    @DisplayName("다리를 다 건넜는지 여부를 알려주는 기능")
+    @Test
+    public void 다리_건너기_성공_여부() {
+        UserState userState = new UserState();
+        userState.moveToNext(true, Command.UP.getCommand());
+        userState.moveToNext(true, Command.DOWN.getCommand());
+        userState.moveToNext(true, Command.UP.getCommand());
+        assertThat(userState.isSuccessOrFail(3)).isTrue();
+
+        assertThat(userState.isSuccessOrFail(4)).isFalse();
+
+        userState.moveToNext(false, Command.UP.getCommand());
+        assertThat(userState.isSuccessOrFail(4)).isFalse();
+    }
 }
