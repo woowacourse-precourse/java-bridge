99acb6ecd3ca18703131d3f749ef75ee9b8395ba
HyeonJun0530 <nexus2697@pukyong.ac.kr>
2022-11-19 20:14:15 +0900

    style(BridgeGame): 코드 포맷팅

--- src/main/java/bridge/domain/BridgeGame.java
+++ src/main/java/bridge/domain/BridgeGame.java
@@ -17,8 +17,7 @@ public class BridgeGame {
      * 이동을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
     public boolean move(String moveInput, List<String> bridge, int index) {
-        if (moveInput.equals(bridge.get(index)))
-            return true;
+        if (moveInput.equals(bridge.get(index))) return true;
 
         return false;
     }
@@ -28,9 +27,8 @@ public class BridgeGame {
      * <p>
      * 재시작을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public boolean retry(String restartInput) {
-        if (restartInput.equals(RESTART_MSG.getMsg()))
-            return true;
+    public boolean retry(String retryInput) {
+        if (retryInput.equals(RESTART_MSG.getMsg())) return true;
 
         return false;
     }
