65fd9b5d556bbaca9b7be837a8b22515bdb0c489
alivejuicy <kkddhh1998@gmail.com>
2022-11-20 03:29:57 +0900

    Rename : provider 패키지 생성 후 변경

--- src/main/java/bridge/domain/provider/BridgeGame.java
+++ src/main/java/bridge/domain/provider/BridgeGame.java
@@ -1,4 +1,4 @@
-package bridge;
+package bridge.domain.provider;
 
 /**
  * 다리 건너기 게임을 관리하는 클래스
--- src/main/java/bridge/domain/provider/BridgeNumberGenerator.java
+++ src/main/java/bridge/domain/provider/BridgeNumberGenerator.java
@@ -1,4 +1,4 @@
-package bridge;
+package bridge.domain.provider;
 
 @FunctionalInterface
 public interface BridgeNumberGenerator {
