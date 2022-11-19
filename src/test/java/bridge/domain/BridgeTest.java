951712094fd1f108d1ed57227240fa42c3da32f1
Hyun-Seo Oh <ohseo8813@gmail.com>
2022-11-19 12:18:47 +0900

    <test>(BridgeTest.java): Bridge 생성 검증 테스트

---
+++ src/test/java/bridge/domain/BridgeTest.java
@@ -0,0 +1,42 @@
+package bridge.domain;
+
+import static bridge.constant.BridgeConstant.MAX_SIZE;
+import static bridge.constant.BridgeConstant.MIN_SIZE;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import bridge.BridgeMaker;
+import bridge.BridgeRandomNumberGenerator;
+import java.util.List;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+class BridgeTest {
+
+    BridgeMaker bridgeMaker = new BridgeMaker(new BridgeRandomNumberGenerator());
+
+    @DisplayName("Bridge를 생성할때")
+    @Nested
+    class Validate {
+
+        @DisplayName("길이가 범위를 벗어날 경우 예외를 반환한다")
+        @ValueSource(ints = {MIN_SIZE - 1, MAX_SIZE + 1})
+        @ParameterizedTest
+        void test1(int wrongSize) {
+            assertThatThrownBy(() -> new Bridge(bridgeMaker.makeBridge(wrongSize)))
+                    .isInstanceOf(IllegalArgumentException.class);
+        }
+
+        @DisplayName("잘못된 값을 가지고 있을 경우 예외를 반환한다.")
+        @ValueSource(ints = {MIN_SIZE})
+        @ParameterizedTest
+        void test2(int rightSize) {
+            List<String> list = bridgeMaker.makeBridge(rightSize);
+            list.add("wrongValue");
+            assertThatThrownBy(() -> new Bridge(list))
+                    .isInstanceOf(IllegalArgumentException.class);
+        }
+    }
+
+}
