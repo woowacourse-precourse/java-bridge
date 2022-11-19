d259f753e089bbce8d15e3f5fa6a0486f55fc7a5
dydtmd6801 <dydtmd6801@naver.com>
2022-11-19 15:23:59 +0900

    test(BridgeTest): 생성할 다리의 길이의 값에 대한 예외처리 테스트
    생성할 다리의 길이의 값이
    공백일 경우
    - NullPointerException 호출
    숫자가 아닌 문자, 특수문자, 공백이 섞여있을 경우
    - NumberFormatException 호출
    3미만 20초과인 경우
    - IllegalArgumentException 호출

--- src/test/java/bridge/BridgeTest.java
+++ src/test/java/bridge/BridgeTest.java
@@ -1,4 +1,42 @@
 package bridge;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
 public class BridgeTest {
+
+    InputView inputView = new InputView();
+
+    @DisplayName("생성할 다리의 길이의 값이 공백일 경우 예외처리 테스트")
+    @Test
+    void bridgeSizeIsEmpty() {
+        String bridgeSize = " ";
+        assertThatThrownBy(() -> inputView.checkException(bridgeSize)).isInstanceOf(NullPointerException.class);
+    }
+
+    @DisplayName("생성할 다리의 길이의 값이 정수형이 아닌 경우 예외처리 테스트")
+    @ParameterizedTest
+    @ValueSource(strings={"1r2","5#7","9 7"})
+    void bridgeSizeIsNumber(String bridgeSize) {
+        assertThatThrownBy(() -> inputView.checkException(bridgeSize)).isInstanceOf(NumberFormatException.class);
+    }
+
+    @DisplayName("생성할 다리의 길이의 값이 3미만, 20 초과일 경우 예외처리 테스트")
+    @Test
+    void bridgeSizeOutOfRange() {
+        String bridgeSize = "25";
+        assertThatThrownBy(() -> inputView.checkException(bridgeSize)).isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("생성할 다리의 길이의 값이 올바를 경우 테스트")
+    @Test
+    void bridgeSizeCorrect() {
+        String bridgeSize = "15";
+        assertThat(inputView.checkException(bridgeSize)).isEqualTo(15);
+    }
 }
