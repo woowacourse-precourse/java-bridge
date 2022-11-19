dfe2c1d5bb40a592e989694579d7a5d2a6adc500
김현재 <41482946+galug@users.noreply.github.com>
2022-11-19 17:14:15 +0900

    feat(retry): 재시도 관련 자료 모은 enum 추가

---
+++ src/main/java/bridge/model/Retry.java
@@ -0,0 +1,22 @@
+package bridge.model;
+
+public enum Retry {
+    R("R", true),
+    Q("Q", false);
+
+    private final String value;
+    private final boolean isRetry;
+
+    Retry(String value, boolean isRetry) {
+        this.value = value;
+        this.isRetry = isRetry;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public boolean isRetry() {
+        return isRetry;
+    }
+}
