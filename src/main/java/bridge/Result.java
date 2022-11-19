4ef5990c5a86ce2d9a1769791117b9a8bebbcc1d
Sangwoo Shin <sangwoonoel@naver.com>
2022-11-19 22:55:23 +0900

    feat(Result): 다리를 건너는 결과에 대한 값을 관리하는 enum  생성

---
+++ src/main/java/bridge/Result.java
@@ -0,0 +1,24 @@
+package bridge;
+
+public enum Result {
+    UP_SUCCESS("U", "O"),
+    UP_FAIL("U", "X"),
+    DOWN_SUCCESS("D", "O"),
+    DOWN_FAIL("D", "X");
+
+    private final String moving;
+    private final String acceptability;
+
+    Result(String moving, String acceptability) {
+        this.moving = moving;
+        this.acceptability = acceptability;
+    }
+
+    public String getMoving() {
+        return moving;
+    }
+
+    public String getAcceptability() {
+        return acceptability;
+    }
+}
