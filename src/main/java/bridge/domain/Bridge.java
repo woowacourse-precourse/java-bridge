16ad49a0cc742d401220c314b6c00620a7fc9353
code-tree <dltjdqls1200@naver.com>
2022-11-19 14:42:57 +0900

    refactor: Bridge가 EnumMap 대신 처음 생성되는 다리 List를 가지도록 변경

--- src/main/java/bridge/domain/Bridge.java
+++ src/main/java/bridge/domain/Bridge.java
@@ -1,49 +1,26 @@
 package bridge.domain;
 
-import java.util.ArrayList;
-import java.util.EnumMap;
 import java.util.List;
-import java.util.Map;
 
 public class Bridge {
-    private final Map<Move, List<String>> bridges;
+    private final List<String> bridges;
 
-    public Bridge(Map<Move, List<String>> bridges) {
+    public Bridge(List<String> bridges) {
         this.bridges = bridges;
-    }
-
-    public static Bridge from(List<String> bridge) {
-        Map<Move, List<String>> bridges = initMap();
-        for (String direction : bridge) {
-            makeBlock(bridges, direction);
-        }
-
-        return new Bridge(bridges);
     }
 
     public String moveResult(Move move, int distance, String direction) {
         if (move.direction.equals(direction)) {
-            return bridges.get(move).get(distance);
+            if (bridges.get(distance).equals(direction)) {
+                return "O";
+            }
+            return "X";
         }
-
         return " ";
     }
 
-    private static void makeBlock(Map<Move, List<String>> bridges, String direction) {
-        for (Move move : Move.values()) {
-            if (move.direction.equals(direction)) {
-                bridges.get(move).add("O");
-                continue;
-            }
-            bridges.get(move).add("X");
-        }
-    }
-
-    private static Map<Move, List<String>> initMap() {
-        Map<Move, List<String>> bridges = new EnumMap<>(Move.class);
-        bridges.put(Move.UP, new ArrayList<>());
-        bridges.put(Move.DOWN, new ArrayList<>());
-
-        return bridges;
+    @Override
+    public String toString() {
+        return bridges.toString();
     }
 }
