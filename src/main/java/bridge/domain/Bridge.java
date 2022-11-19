6a1258465277bf872f1466ff314d735865f8de09
kimtaehoonDev <taehoon_dev@naver.com>
2022-11-19 16:28:01 +0900

    feat(Bridge): 입력값이 크기를 검증한다

--- src/main/java/bridge/domain/Bridge.java
+++ src/main/java/bridge/domain/Bridge.java
@@ -7,10 +7,12 @@ public class Bridge {
     private List<FootrestLocation> structure;
 
     public Bridge(List<String> structure) {
+        if (structure.size() < 3 || 20 < structure.size()) {
+            throw new IllegalArgumentException("3 이상, 20 이하의 크기로만 입력이 가능합니다");
+        }
         this.structure = structure.stream().map(each ->
                         FootrestLocation.valueOfUsingUserInput(each))
                 .collect(Collectors.toList());
-        System.out.println(this.structure);
     }
 
     public boolean canMove(Integer order, FootrestLocation footrestLocation) {
@@ -19,5 +21,9 @@ public class Bridge {
 
     public boolean isLast(Integer order) {
         return structure.size() == order;
+    }
+
+    public Integer size() {
+        return structure.size();
     }
 }
--- src/test/java/bridge/domain/BridgeTest.java
+++ src/test/java/bridge/domain/BridgeTest.java
@@ -1,48 +1,79 @@
 package bridge.domain;
 
+import java.util.ArrayList;
+import java.util.List;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 @Nested
 class BridgeTest {
     @Test
-    void 정상_입력값이_범위안() {
+    void 정상_입력값이_20개() {
+        List<String> temp = new ArrayList<>();
+        for (int i = 0; i < 20; i++) {
+            temp.add("U");
+        }
+        Bridge bridge = new Bridge(temp);
+        Assertions.assertThat(bridge.size()).isEqualTo(20);
+    }
 
+    @Test
+    void 정상_입력값이_3개() {
+        List<String> temp = new ArrayList<>();
+        for (int i = 0; i < 3; i++) {
+            temp.add("D");
+        }
+        Bridge bridge = new Bridge(temp);
+        Assertions.assertThat(bridge.size()).isEqualTo(3);
     }
 
     @Test
     void 예외_입력값_3보다_작다() {
-
+        List<String> temp = new ArrayList<>();
+        for (int i = 0; i < 2; i++) {
+            temp.add("D");
+        }
+        Assertions.assertThatThrownBy(() -> new Bridge(temp)).isInstanceOf(IllegalArgumentException.class);
     }
 
     @Test
     void 예외_입력값_20보다_크다() {
-
+        List<String> temp = new ArrayList<>();
+        for (int i = 0; i < 21; i++) {
+            temp.add("D");
+        }
+        Assertions.assertThatThrownBy(() -> new Bridge(temp)).isInstanceOf(IllegalArgumentException.class);
     }
 
     @Test
     void 예외_입력값에_U_D_이외_값() {
-
+        List<String> temp = new ArrayList<>();
+        for (int i = 0; i < 10; i++) {
+            temp.add("D");
+        }
+        temp.add("NOT");
+        Assertions.assertThatThrownBy(() -> new Bridge(temp)).isInstanceOf(IllegalArgumentException.class);
     }
 
     @Test
     void 정상_다리가_존재하는_인덱스_물어봄() {
-
+        Assertions.fail("TODO");
     }
 
     @Test
     void 예외_다리_범위_바깥_인덱스_물어봄() {
-
+        Assertions.fail("TODO");
     }
 
     @Test
     void 예외_0보다_작은_인덱스로_물어봄() {
-
+        Assertions.fail("TODO");
     }
 
     @Test
     void 예외_입력값이_FootrestLocation_아님() {
-
+        Assertions.fail("TODO");
     }
 
 }
