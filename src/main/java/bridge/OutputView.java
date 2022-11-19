d5dccbc309dd3a9f1d6e6771e984150fdd1adfd8
JUNG-jihoon <73165763+JUNG-jihoon@users.noreply.github.com>
2022-11-19 16:17:55 +0900

    Feat : "추가 이동한 칸 출력"
    사용자가 입력한 이동한 칸을 출력하기 위한 함수들을 추가하였습니다. List로 출력될 문자들을 저장하였으며 사용자가 입력한 값에 따라서  O X 으로 표현하였습니다.

--- src/main/java/bridge/OutputView.java
+++ src/main/java/bridge/OutputView.java
@@ -1,16 +1,69 @@
 package bridge;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 /**
  * 사용자에게 게임 진행 상황과 결과를 출력하는 역할을 한다.
  */
 public class OutputView {
 
-    /**
-     * 현재까지 이동한 다리의 상태를 정해진 형식에 맞춰 출력한다.
-     * <p>
-     * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
-     */
+    private static List<String> Map_Up = new ArrayList<>();
+    private static List<String> Map_Down = new ArrayList<>();
+
+    public OutputView(){
+        Map_Up.add("");
+        Map_Down.add("");
+    }
     public void printMap() {
+        String UP = Map_Up.toString();
+        String Down = Map_Down.toString();
+        UP =UP.replaceAll(",","");
+        UP = UP.substring(0,UP.length()-2);
+        Down =Down.replaceAll(",","");
+        Down = Down.substring(0,Down.length()-2);
+        System.out.println(UP+"]");
+        System.out.println(Down+"]");
+    }
+    public void Area_Division(List<String> Bridge, int Correct[], int Location){
+        System.out.println("Location : "+ Location);
+        if(Correct[0]==1){
+            Print_Up(Bridge.get(Location));
+            Print_Middle();
+            return;
+        }
+        if(Correct[1]==1){
+            Print_Down(Bridge.get(Location));
+            Print_Middle();
+        }
+
+    }
+    private void Print_Up(String Bridge){
+        if(Bridge.equals("U")){
+            Map_Up.add("O");
+            Map_Down.add(" ");
+            return;
+        }
+        if(Bridge.equals("D")){
+            Map_Up.add("X");
+            Map_Down.add(" ");
+        }
+    }
+    private void Print_Down(String Bridge){
+        if(Bridge.equals("U")){
+            Map_Up.add(" ");
+            Map_Down.add("X");
+            return;
+        }
+        if(Bridge.equals("D")){
+            Map_Up.add(" ");
+            Map_Down.add("O");
+        }
+    }
+    private void Print_Middle(){
+        Map_Up.add("|");
+        Map_Down.add("|");
     }
 
     /**
