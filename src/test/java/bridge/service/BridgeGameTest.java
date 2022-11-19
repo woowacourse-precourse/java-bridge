cdf313880d1dc9ae3374338d3b8e2c0482693153
KIMSEI1124 <workju1124@gmail.com>
2022-11-19 13:30:25 +0900

    test: 플레이어가 이동할 칸을 잘못 입력시 예외 처리 테스트 구현

---
+++ src/test/java/bridge/service/BridgeGameTest.java
@@ -0,0 +1,40 @@
+package bridge.service;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+public class BridgeGameTest {
+
+    private final BridgeGame bridgeGame = new BridgeGame();
+
+    @Nested
+    @DisplayName("입력한 이동할 칸의 문자의 길이가 1이 아니면 예외를 던짐")
+    class notMoveCommendLengthOneTest {
+        @ParameterizedTest
+        @CsvSource({
+                "DD", "UDD", "DDUU"
+        })
+        void test(String moveCommend) {
+            Assertions.assertThatThrownBy(() -> bridgeGame.move(moveCommend))
+                    .isInstanceOf(IllegalArgumentException.class)
+                    .hasMessageContaining("[ERROR]");
+        }
+    }
+
+    @Nested
+    @DisplayName("입력한 이동할 칸의 문자가 \"D\" 또는 \"U\"가 아님")
+    class moveCommendNotEqual_DorE {
+        @ParameterizedTest
+        @CsvSource({
+                "Q", "0", "!"
+        })
+        void test(String moveCommend) {
+            Assertions.assertThatThrownBy(() -> bridgeGame.move(moveCommend))
+                    .isInstanceOf(IllegalArgumentException.class)
+                    .hasMessageContaining("[ERROR]");
+        }
+    }
+}
