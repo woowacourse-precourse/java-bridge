6d0f21ce6e9d6d855b91bacc9d2e404f1c5d26c4
CMHyuk <alsgur9710@naver.com>
2022-11-19 19:27:16 +0900

    test(BridgeGameTest): 게임 재시작, 종료 반환 테스트

--- src/test/java/bridge/BridgeGameTest.java
+++ src/test/java/bridge/BridgeGameTest.java
@@ -24,4 +24,14 @@ class BridgeGameTest {
         boolean lastResult = bridgeGame.move("U", answerBridge, 2);
         assertThat(lastResult).isTrue();
     }
+
+    @DisplayName("게임 재시작 입력은 true, 종료는 false를 반환한다.")
+    @Test
+    void isRestartOrQuit() {
+        boolean retryGame = bridgeGame.retry("R");
+        assertThat(retryGame).isTrue();
+
+        boolean endGame = bridgeGame.retry("Q");
+        assertThat(endGame).isFalse();
+    }
 }
