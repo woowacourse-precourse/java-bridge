adc18b866bbefb485ecbcf68b8af95d402ae295a
김현재 <41482946+galug@users.noreply.github.com>
2022-11-20 02:03:34 +0900

    feat(gamecontroller): 사용자가 이동할 칸을 입력하는 기능 추가

--- src/main/java/bridge/controller/GameController.java
+++ src/main/java/bridge/controller/GameController.java
@@ -17,4 +17,18 @@ public class GameController {
         PassingSpace passingSpace = new PassingSpace();
         return new BridgeGame(bridge, passingSpace);
     }
+
+    public void playGame(BridgeGame bridgeGame) {
+        // TODO: 게임을 플레이하는 기능
+    }
+
+    public String selectMoving() {
+        try {
+            outputView.printSelectUpOrDownInput();
+            String readMoving = inputView.readMoving();
+            return readMoving;
+        } catch (IllegalArgumentException illegalArgumentException) {
+            return selectMoving();
+        }
+    }
 }
