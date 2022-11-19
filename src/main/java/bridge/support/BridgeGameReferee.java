681c96eb5de4519a77f22cf0183de168409758b2
saint6839 <saint6839@gmail.com>
2022-11-20 01:09:26 +0900

    refactor(domain) : OutputView 의존성 제거

--- src/main/java/bridge/support/BridgeGameReferee.java
+++ src/main/java/bridge/support/BridgeGameReferee.java
@@ -6,12 +6,6 @@ import bridge.view.OutputView;
 
 public class BridgeGameReferee {
 
-    private final OutputView outputView;
-
-    public BridgeGameReferee(OutputView outputView) {
-        this.outputView = outputView;
-    }
-
     public Result updateResult(Result result, MoveDirection direction, boolean isAnswer) {
         result.updateIsAnswers(direction, isAnswer);
         this.printMap(result);
@@ -19,6 +13,7 @@ public class BridgeGameReferee {
     }
 
     private void printMap(Result result) {
+        OutputView outputView = new OutputView();
         outputView.printMap(result);
     }
 }
