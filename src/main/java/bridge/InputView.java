9055b4781b198b1612a70894aeb7c12568b0d953
parkmuhyeun <pjhg410@gmail.com>
2022-11-19 16:58:55 +0900

    feat: 이동할 칸 입력 view 구현

--- src/main/java/bridge/InputView.java
+++ src/main/java/bridge/InputView.java
@@ -23,7 +23,8 @@ public class InputView {
      * 사용자가 이동할 칸을 입력받는다.
      */
     public String readMoving() {
-        return null;
+        System.out.println(INPUT_MOVING);
+        return Console.readLine();
     }
 
     /**
--- src/main/java/bridge/NoticeMessage.java
+++ src/main/java/bridge/NoticeMessage.java
@@ -3,4 +3,5 @@ package bridge;
 public class NoticeMessage {
     public static final String START = "다리 건너기 게임을 시작합니다.";
     public static final String INPUT_BRIDGE_SIZE = "다리의 길이를 입력해주세요.";
+    public static final String INPUT_MOVING = "이동할 칸을 선택해주세요. (위: U, 아래: D)";
 }
