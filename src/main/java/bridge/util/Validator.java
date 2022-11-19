ceabf70eac8e2407be40c625f7d8f493a39075c5
이건회 <rg970604@naver.com>
2022-11-19 20:17:51 +0900

    feat(Validator.java) : 유효성 검증 로직

--- src/main/java/bridge/util/Validator.java
+++ src/main/java/bridge/util/Validator.java
@@ -2,4 +2,30 @@ package bridge.util;
 
 public class Validator {
 
+    private static final int MIN_BRIDGE_SIZE = 3;
+    private static final int MAX_BRIDGE_SIZE = 20;
+
+    public static void validateNumericInputType(String input) {
+        if (input.chars().allMatch(Character::isDigit)) {
+            throw new IllegalArgumentException("숫자만 입력 가능합니다");
+        }
+    }
+
+    public static void validateBridgeSize(int size) {
+        if (size < MIN_BRIDGE_SIZE || size > MAX_BRIDGE_SIZE) {
+            throw new IllegalArgumentException("3~20 이내로만 입력 가능합니다");
+        }
+    }
+
+    public static void validateMovingInput(String input) {
+        if (!input.equals("U") || input.equals("D")) {
+            throw new IllegalArgumentException("U 또는 D만 입력 가능합니다");
+        }
+    }
+
+    public static void validateGameCommandInput(String input) {
+        if (!input.equals("R") || input.equals("Q")) {
+            throw new IllegalArgumentException("R 또는 Q만 입력 가능합니다");
+        }
+    }
 }
