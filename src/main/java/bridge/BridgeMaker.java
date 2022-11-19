8b127a53957173bdb78141913d7fb82cfc7a754d
shinD <huipulci1@naver.com>
2022-11-19 14:47:42 +0900

    feat: 다리 생성 기능 구현

--- src/main/java/bridge/BridgeMaker.java
+++ src/main/java/bridge/BridgeMaker.java
@@ -1,6 +1,11 @@
 package bridge;
 
+import bridge.domain.direction.Direction;
+
 import java.util.List;
+
+import static java.util.stream.Collectors.toList;
+import static java.util.stream.IntStream.generate;
 
 /**
  * 다리의 길이를 입력 받아서 다리를 생성해주는 역할을 한다.
@@ -18,6 +23,9 @@ public class BridgeMaker {
      * @return 입력받은 길이에 해당하는 다리 모양. 위 칸이면 "U", 아래 칸이면 "D"로 표현해야 한다.
      */
     public List<String> makeBridge(int size) {
-        return null;
+        return generate(bridgeNumberGenerator::generate)
+                .limit(size)
+                .mapToObj(Direction::mapNumberToSymbol)
+                .collect(toList());
     }
 }
---
+++ src/main/java/bridge/domain/direction/Direction.java
@@ -0,0 +1,29 @@
+package bridge.domain.direction;
+
+import static java.lang.String.format;
+import static java.util.Arrays.stream;
+
+public enum Direction {
+
+    UP("U", 1),
+    DOWN("D", 0),
+    ;
+
+    private static final String NOT_MATCH_SYMBOL_BY_NUMBER = "%d에 대응되는 심볼이 없습니다.";
+    private final String symbol;
+    private final int mappingNumber;
+
+    Direction(final String symbol, final int mappingNumber) {
+        this.symbol = symbol;
+        this.mappingNumber = mappingNumber;
+    }
+
+    public static String mapNumberToSymbol(final int number) {
+        return stream(values())
+                .filter(direction -> direction.mappingNumber == number)
+                .map(direction -> direction.symbol)
+                .findAny()
+                .orElseThrow(() ->
+                        new IllegalArgumentException(format(NOT_MATCH_SYMBOL_BY_NUMBER, number)));
+    }
+}
