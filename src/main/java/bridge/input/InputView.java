59cba4ee30f95a27193e5a83840737787b462409
odo27 <odo27@naver.com>
2022-11-19 13:44:39 +0900

    style: 주석 제거

--- src/main/java/bridge/input/InputView.java
+++ src/main/java/bridge/input/InputView.java
@@ -3,14 +3,8 @@ package bridge.input;
 import bridge.output.OutputView;
 import camp.nextstep.edu.missionutils.Console;
 
-/**
- * 사용자로부터 입력을 받는 역할을 한다.
- */
 public class InputView {
 
-    /**
-     * 다리의 길이를 입력받는다.
-     */
     public int readBridgeSize() {
         while (true) {
             OutputView.printReadBridgeSizeMessage();
@@ -25,9 +19,6 @@ public class InputView {
         }
     }
 
-    /**
-     * 사용자가 이동할 칸을 입력받는다.
-     */
     public String readMoving() {
         while (true) {
             OutputView.printReadMovingMessage();
@@ -42,9 +33,6 @@ public class InputView {
         }
     }
 
-    /**
-     * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
-     */
     public String readGameCommand() {
         while (true) {
             OutputView.printReadGameCommandMessage();
