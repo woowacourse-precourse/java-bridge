0b5bdb7f0ec31fb8c3230843c0ca26595bb41000
hdsung98 <hdsung98@gmail.com>
2022-11-20 01:31:12 +0900

    fix: InputView 예외 메시지 삽입 등 에러해결
    1. throw는 하지만 예외메시지가 null이었던 문제 해결
    
    2. readGameCommand 입력값 리턴 하지않던 문제 해결

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -2,16 +2,11 @@ package view;
 
 import camp.nextstep.edu.missionutils.Console;
 
-/**
- * 사용자로부터 입력을 받는 역할을 한다. InputView 클래스에서만 camp.nextstep.edu.missionutils.Console 의 readLine()을 이용해 사용자의 입력을 받을 수 있다.
- * InputView의 패키지는 변경할 수 있다. InputView의 메서드의 시그니처(인자, 이름)와 반환 타입은 변경할 수 있다.
- */
 public class InputView {
 
     public static final char UPPER_START = 'A';
     public static final char UPPER_END = 'Z';
-    public static final char LOWER_START = 'a';
-    public static final char LOWER_END = 'z';
+    public static final String ERROR_INVALID_INPUT = "[ERROR] 유효한 입력값이 아닙니다.";
 
     public int readBridgeSize() {
         String size = Console.readLine();
@@ -21,11 +16,11 @@ public class InputView {
 
     private void checkNumberValidity(String size) throws IllegalArgumentException {
         if (size.length() != 1) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException(ERROR_INVALID_INPUT);
         }
         char validLengthSize = size.charAt(0);
         if (Character.isDigit(validLengthSize) == false) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException(ERROR_INVALID_INPUT);
         }
     }
 
@@ -38,17 +33,17 @@ public class InputView {
     public String readGameCommand() {
         String command = Console.readLine();
         CheckAlphabetValidity(command);
-        return null;
+        return command;
     }
 
     private void CheckAlphabetValidity(String input) throws IllegalArgumentException {
         if (input.length() != 1) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException(ERROR_INVALID_INPUT);
         }
         char validLengthInput = input.charAt(0);
-        if (!(validLengthInput >= UPPER_START && validLengthInput <= UPPER_END)
-                && !(validLengthInput >= LOWER_START && validLengthInput <= LOWER_END)) {
-            throw new IllegalArgumentException();
+        if (!(validLengthInput >= UPPER_START
+                && validLengthInput <= UPPER_END)) {
+            throw new IllegalArgumentException(ERROR_INVALID_INPUT);
         }
     }
 }
