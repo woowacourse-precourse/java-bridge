73a1d900629fd3a911113f3bd41c386ff48093e8
JunTaeINC <juntea3523@naver.com>
2022-11-19 21:18:00 +0900

    feat : (OutputView) printStartGame , printMap 기능 구현

--- src/main/java/bridge/OutputView.java
+++ src/main/java/bridge/OutputView.java
@@ -1,16 +1,19 @@
 package bridge;
 
-/**
- * 사용자에게 게임 진행 상황과 결과를 출력하는 역할을 한다.
- */
+import java.util.List;
+
+import static bridge.enums.Sentence.*;
+
 public class OutputView {
 
-    /**
-     * 현재까지 이동한 다리의 상태를 정해진 형식에 맞춰 출력한다.
-     * <p>
-     * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
-     */
-    public void printMap() {
+    public static void printStartGame() {
+        System.out.println(GAME_START.getValue());
+        System.out.println();
+    }
+
+    public static void printMap(List<String> upLine, List<String> downLine) {
+        System.out.println(upLine.toString().replace(", ", "|"));
+        System.out.println(downLine.toString().replace(", ", "|"));
     }
 
     /**
