9ccea09a125198e822a526bfb935767ad463210c
5nam <ohnam90@gmail.com>
2022-11-19 14:11:14 +0900

    feat: 결과가 담긴 다리 객체 받아서 다리 생성하고 화면에 띄우는 기능 추가

--- src/main/java/bridge/View/OutputView.java
+++ src/main/java/bridge/View/OutputView.java
@@ -18,8 +18,14 @@ public class OutputView {
      * <p>
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void printMap() {
+    public void printMap(Bridge bridge, int count) {
+        bridgeMapUp = printBridge(bridge.getUp(), count);
+        bridgeMapDown = printBridge(bridge.getDown(), count);
 
+        bridgeMapUp.stream().forEach(System.out::print);
+        System.out.println();
+        bridgeMapDown.stream().forEach(System.out::print);
+        System.out.println();
     }
 
     private List<String> printBridge(List<String> result, int count) {
