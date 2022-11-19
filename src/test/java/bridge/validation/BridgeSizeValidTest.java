156e0203a0b89993da1016878d432f5c4dbd6694
kang-seongbeom <56289258+kang-seongbeom@users.noreply.github.com>
2022-11-19 17:54:36 +0900

    test : 이동할 칸 유효성 테스트
    테스트 코드를 통해 이동할 칸의 유효성을 확인한다.

--- src/test/java/bridge/validation/BridgeSizeValidTest.java
+++ src/test/java/bridge/validation/BridgeSizeValidTest.java
@@ -1,17 +1,24 @@
 package bridge.validation;
 
 import bridge.Message.ErrorMessage;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class BridgeSizeValidTest {
 
+    private static Validation validation;
+
+    @BeforeAll
+    public static void init(){
+        validation = new BridgeSizeValidation();
+    }
+
     @Test
     void 다리_크기_테스트(){
         // given
         String size = "10";
-        Validation validation = new BridgeSizeValidation();
 
         // when, then
         validation.validate(size);
@@ -21,7 +28,6 @@ public class BridgeSizeValidTest {
     void 다리_크기_범위_미만_예외_테스트(){
         // given
         String size = "2";
-        Validation validation = new BridgeSizeValidation();
 
         // when, then
         assertThatThrownBy(() -> validation.validate(size))
@@ -33,7 +39,6 @@ public class BridgeSizeValidTest {
     void 다리_크기_범위_초과_예외_테스트(){
         // given
         String size = "21";
-        Validation validation = new BridgeSizeValidation();
 
         // when, then
         assertThatThrownBy(() -> validation.validate(size))
@@ -45,7 +50,6 @@ public class BridgeSizeValidTest {
     void 다리_크기_문자_예외_테스트(){
         // given
         String size = "T";
-        Validation validation = new BridgeSizeValidation();
 
         // when, then
         assertThatThrownBy(() -> validation.validate(size))
