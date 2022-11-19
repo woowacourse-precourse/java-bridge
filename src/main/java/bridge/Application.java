172dd208b13f9a57b2cabf27e2e3510877de1a2e
Hoogokok <85615666+Hoogokok@users.noreply.github.com>
2022-11-19 19:36:01 +0900

    fix: main, OutPutView에 미완성 코드 지움.

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -3,9 +3,6 @@ package bridge;
 public class Application {
 
     public static void main(String[] args) {
-        int size = 3;
-        BridgeMaker bridgeMaker = new BridgeMaker(new BridgeRandomNumberGenerator());
-        Bridge bridge = new Bridge(bridgeMaker.makeBridge(size));
-        BridgeGame bridgeGame = new BridgeGame(bridge);
+
     }
 }
