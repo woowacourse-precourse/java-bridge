bc883245bc3b84dee2ef6476d457a0d700f905bb
Jaesun Park <41979078+ppakjae@users.noreply.github.com>
2022-11-20 00:45:41 +0900

    feat: 입력받은 재시작/종료 여부에 따라 게임을 재시작하거나 종료하는 기능 구현

--- src/main/java/bridge/BridgeGame.java
+++ src/main/java/bridge/BridgeGame.java
@@ -24,16 +24,28 @@ public class BridgeGame {
         System.out.println("다리 건너기 게임을 시작합니다.\n");
         int bridgeSize = inputView.readBridgeSize();
         List<String> bridge = bridgeMaker.makeBridge(bridgeSize);
-        List<String> upperBridge = new ArrayList<>();
-        List<String> downBridge = new ArrayList<>();
-        for (int i = 0; i < bridge.size(); i++) {
-            String moving = inputView.readMoving();
-            if (move(moving, bridge.get(i))) {
-                outputView.makeMap(moving, "O", upperBridge, downBridge);
-            } else if (!move(moving, bridge.get(i))) {
-                outputView.makeMap(moving, "X", upperBridge, downBridge);
+        while (true) {
+            boolean flag = false;
+            List<String> upperBridge = new ArrayList<>();
+            List<String> downBridge = new ArrayList<>();
+            for (int i = 0; i < bridge.size(); i++) {
+                String moving = inputView.readMoving();
+                if (move(moving, bridge.get(i))) {
+                    outputView.makeMap(moving, "O", upperBridge, downBridge);
+                    outputView.printMap(upperBridge, downBridge);
+                } else if (!move(moving, bridge.get(i))) {
+                    outputView.makeMap(moving, "X", upperBridge, downBridge);
+                    outputView.printMap(upperBridge, downBridge);
+                    String gameCommand = inputView.readGameCommand();
+                    if (retry(gameCommand)) {
+                        flag = true;
+                    }
+                    break;
+                }
             }
-            outputView.printMap(upperBridge, downBridge);
+            if (!flag) {
+                break;
+            }
         }
     }
 
@@ -51,6 +63,12 @@ public class BridgeGame {
      * <p>
      * 재시작을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void retry() {
+    public boolean retry(String gameCommand) {
+        if (gameCommand.equals("R")) {
+            return true;
+        } else if (gameCommand.equals("Q")) {
+            return false;
+        }
+        return false;
     }
 }
