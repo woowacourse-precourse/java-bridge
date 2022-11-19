8cb965f9cc103d48de15b55262287304609a0abe
June <68291395+june-777@users.noreply.github.com>
2022-11-19 21:24:09 +0900

    feat(OutputViewConstants): OutputView와 관련있는 상수는 클래스로 관리

---
+++ src/main/java/view/OutputViewConstants.java
@@ -0,0 +1,13 @@
+package view;
+
+public class OutputViewConstants {
+	public static final String OUTPUT_TEXT_OPEN_SQUARE_BRACKETS = "[";
+	public static final String OUTPUT_TEXT_CLOSE_SQUARE_BRACKETS = "]";
+	public static final String OUTPUT_TEXT_VERTICAL_BAR = "|";
+	public static final String OUTPUT_TEXT_DO_NOT_GO = "   ";
+	public static final String OUTPUT_TEXT_MOVING_SUCCESS = " O ";
+	public static final String OUTPUT_TEXT_MOVING_FAIL = " X ";
+	
+	private OutputViewConstants() {
+	}
+}
