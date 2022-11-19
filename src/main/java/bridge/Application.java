fe71a3f7285a3ba5a7a6d4154c46803f44e898e7
E1psycongr00 <39326175+E1psycongr00@users.noreply.github.com>
2022-11-19 16:28:33 +0900

    feat(application): 의존성 주입 및 컨트롤러 실행 구현

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -2,7 +2,25 @@ package bridge;
 
 public class Application {
 
+    private static final String ERROR = "[ERROR]";
+    private static BridgeController bridgeController;
+
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        dependencyInjection();
+        try {
+            bridgeController.run();
+        } catch (IllegalArgumentException | IllegalStateException e) {
+            System.out.println(ERROR + " " + e.getMessage());
+        }
+    }
+
+    private static void dependencyInjection() {
+        BridgeStatusSaver bridgeStatusSaver = new BridgeStatusSaver();
+        BridgeRandomNumberGenerator bridgeRandomNumberGenerator = new BridgeRandomNumberGenerator();
+        BridgeMaker bridgeMaker = new BridgeMaker(bridgeRandomNumberGenerator);
+        BridgeGame bridgeGame = new BridgeGame(bridgeMaker, bridgeStatusSaver);
+        InputView inputView = new InputView();
+        OutputView outputView = new OutputView();
+        bridgeController = new BridgeController(bridgeGame, inputView, outputView);
     }
 }
