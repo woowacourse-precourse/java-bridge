c685d16553549cd74a2f436d6652108716c0b348
RO-OR <kyk3531@gmail.com>
2022-11-20 07:40:25 +0900

    fix($view) :InputView 클래스의 메소드들에 예외 처리 기능을 추가 수정하였습니다.
    InputView 클래스의 메소드들에 예외를 처리할 수 있는 기능을 추가하여 수정했습니다.
    
    InputView 클래스의 메소드들에 예외처리 기능을 추가하였습니다.

--- src/main/java/bridge/view/InputView.java
+++ src/main/java/bridge/view/InputView.java
@@ -1,5 +1,6 @@
 package bridge.view;
 
+import bridge.ErrorMessage;
 import camp.nextstep.edu.missionutils.Console;
 
 /**
@@ -12,22 +13,34 @@ public class InputView {
      */
     public String readBridgeSize() {
         System.out.println("다리의 길이를 입력해주세요.");
-        return Console.readLine();
+        String sizeInput = Console.readLine();
+        if(Integer.parseInt(sizeInput)<3 || Integer.parseInt(sizeInput)>20){
+            throw new IllegalArgumentException(ErrorMessage.BRIDGE_LENGTH_RANGE.getMessage());
+        }
+        return sizeInput;
     }
 
     /**
      * 사용자가 이동할 칸을 입력받는다.
      */
     public String readMoving() {
-        System.out.println("이동할 칸을 선택해주세요.");
-        return Console.readLine();
+        System.out.println("이동할 칸을 선택해주세요. (위: U, 아래: D)");
+        String movingInput = Console.readLine();
+        if(!movingInput.equals("U")&&!movingInput.equals("D")){
+            throw new IllegalArgumentException(ErrorMessage.BRIDGE_CROSS_INPUT.getMessage());
+        }
+        return movingInput;
     }
 
     /**
      * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
      */
     public String readGameCommand() {
-        System.out.println("게임을 다시 시도할지 여부를 입력해주세요.");
-        return Console.readLine();
+        System.out.println("게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)");
+        String retry = Console.readLine();
+        if(!retry.equals("R")&&!retry.equals("Q")){
+            throw new IllegalArgumentException(ErrorMessage.BRIDGE_RESTART_INPUT.getMessage());
+        }
+        return retry;
     }
 }
