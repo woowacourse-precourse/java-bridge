e4933b9c8baf448101148f2296d26f6af46bce35
JuHyeokParkk <hyeok5596@naver.com>
2022-11-19 16:51:32 +0900

    test: 플레이어가 이동한 방향을 저장하는지에 대한 테스트 코드 작성

---
+++ src/test/java/bridge/UserStatusTest.java
@@ -0,0 +1,28 @@
+package bridge;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+
+public class UserStatusTest {
+
+    @DisplayName("플레이어가 이동한 방향을 저장한다.")
+    @Test
+    void 플레이어의_이동_방향_저장() {
+        String userDirectInputs = "U\nD\nU\nU\n";
+        InputStream in = new ByteArrayInputStream(userDirectInputs.getBytes());
+        System.setIn(in);
+
+        InputView inputView = new InputView();
+        UserStatus userStatus = new UserStatus();
+
+        for (int length = 0; length < userDirectInputs.length() / 2; length++) {
+            userStatus.addCrossedHistory(inputView.readMoving());
+        }
+
+        Assertions.assertThat(userStatus.getCurrentCrossedBridge()).containsExactly("U", "D", "U", "U");
+    }
+}
