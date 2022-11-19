f343998776301a6f0612c15ab093ba069c02e8b8
Gibeom Gwon <gb.gwon@stackframe.dev>
2022-11-20 00:35:55 +0900

    feat: 게임 생성 기능 추가

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -2,9 +2,23 @@ package bridge;
 
 public class Application {
 
+    private static InputView input = new InputView();
     private static OutputView output = new OutputView();
+    private static BridgeGame game;
 
     public static void main(String[] args) {
         output.printGameStart();
+        game = createGame();
+    }
+
+    private static BridgeGame createGame() {
+        output.printRequestInputBridgeSize();
+        while (true) {
+            try {
+                return new BridgeGame(input.readBridgeSize());
+            } catch (IllegalArgumentException e) {
+                output.printErrorMessage(e.getMessage());
+            }
+        }
     }
 }
