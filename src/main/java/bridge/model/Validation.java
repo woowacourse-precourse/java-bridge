090589d53aa3087eb60303ad611a56c244efc035
Hi-Imjaeyoung <imjy2757@naver.com>
2022-11-19 16:05:49 +0900

    feat(Validation) : 예외처리 기능 구현.
    return 타입을 boolean으로 설정.

--- src/main/java/bridge/model/Validation.java
+++ src/main/java/bridge/model/Validation.java
@@ -4,16 +4,22 @@ import bridge.constant.ErrorMessage;
 
 public class Validation {
     private static final String ERROR = "[ERROR]";
-    public static void bridgeSizeValidationTest(String input) throws IllegalArgumentException{
-        valueInRange(isNumber(input));
+    public static boolean bridgeSizeValidationTest(String input){
+        try{
+            valueInRange(isNumber(input));
+            return true;
+        }catch (IllegalArgumentException e){
+            System.out.println(e.getMessage());
+            return false;
+        }
     }
-    private static int isNumber(String input){
+    private static int isNumber(String input) throws IllegalArgumentException{
         if(input.length() != input.replaceAll("[^\\d]","").length()){
             throw new IllegalArgumentException(ERROR+ ErrorMessage.NO_NUMERIC_VALUE.getErrorMessage());
         }
         return Integer.parseInt(input);
     }
-    private static void valueInRange(int input){
+    private static void valueInRange(int input) throws IllegalArgumentException{
         if(input<3 || input>20){
             throw new IllegalArgumentException(ERROR+ ErrorMessage.NUMBER_RANGE_IS_WRONG.getErrorMessage());
         }
