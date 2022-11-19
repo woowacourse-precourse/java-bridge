2eadaec37cb58b340f2fa49c0935c8a9d06a3cdb
KSH <tkdgns543@gmail.com>
2022-11-20 00:09:26 +0900

    feat(domain): 전체 다리 구조를 나타내는 인스턴스 구현

--- src/main/java/bridge/domain/BridgeGame.java
+++ src/main/java/bridge/domain/BridgeGame.java
@@ -1,9 +1,21 @@
 package bridge.domain;
 
+import bridge.BridgeMaker;
+import bridge.BridgeRandomNumberGenerator;
+
+import java.util.List;
+
 /**
  * 다리 건너기 게임을 관리하는 클래스
  */
 public class BridgeGame {
+
+    private final List<String> bridge;
+
+    BridgeGame(int bridgeLength) {
+        BridgeMaker bridgeMaker = new BridgeMaker(new BridgeRandomNumberGenerator());
+        bridge = bridgeMaker.makeBridge(bridgeLength);
+    }
 
     /**
      * 사용자가 칸을 이동할 때 사용하는 메서드
