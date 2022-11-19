0d5dae94ec1e7f91f306a1196d1dad038b7ff5d4
Sojeong <k5k5s5s5j5j5@naver.com>
2022-11-19 20:54:35 +0900

    test(test/OutputViewTest): 이동 결과 Map 테스트 추가
    - OutputView.printMapBuild(): 프린트할 내용을 String으로 반환
    - OutputView.printMap(): 실제 결과를 프린트
    - OutputViewTest.printMap(): 잘 프린트하는지 확인

--- src/main/java/bridge/View/OutputView.java
+++ src/main/java/bridge/View/OutputView.java
@@ -12,39 +12,48 @@ public class OutputView {
      * <p>
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    private void printMapUntilPosition(List<String> bridge, int position, String upDown) {
-        System.out.print("[");
+    private String printMapUntilPosition(List<String> bridge, int position, String upDown) {
+        StringBuilder output = new StringBuilder();
+        output.append("[");
         for (int i = 0; i < position; i++) {
             String answer = bridge.get(i);
             if (answer.equals(upDown)) {
-                System.out.print(" O ");
+                output.append(" O ");
             } else {
-                System.out.print("   ");
+                output.append("   ");
             }
-            System.out.print("|");
+            output.append("|");
         }
+        return output.toString();
     }
 
-    private void printMapNowPosition(boolean print, boolean correct) {
+    private String printMapNowPosition(boolean print, boolean correct) {
+        StringBuilder output = new StringBuilder();
         if (print) {
             if (correct) {
-                System.out.print(" O ");
+                output.append(" O ");
             } else {
-                System.out.print(" X ");
+                output.append(" X ");
             }
         } else {
-            System.out.print("   ");
+            output.append("   ");
         }
-        System.out.println("]");
+        output.append("]\n");
+        return output.toString();
+    }
+
+    public String printMapBuild(List<String> bridge, int position, boolean correct) {
+        StringBuilder output = new StringBuilder();
+        for (String upDown : new String[]{"U", "D"}) {
+            output.append(printMapUntilPosition(bridge, position, upDown));
+            boolean print = bridge.get(position).equals(upDown);
+            output.append(printMapNowPosition(print, correct));
+        }
+        return output.toString();
     }
 
     public void printMap(List<String> bridge, int position, boolean correct) {
-        for (String upDown : new String[]{"U", "D"}) {
-            printMapUntilPosition(bridge, position, upDown);
-            boolean print = bridge.get(position).equals(upDown);
-            printMapNowPosition(print, correct);
-        }
-
+        System.out.println(printMapBuild(bridge, position, correct));
     }
 
     /**
---
+++ src/test/java/bridge/OutputViewTest.java
@@ -0,0 +1,24 @@
+package bridge;
+
+import bridge.View.OutputView;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class OutputViewTest {
+
+    @DisplayName("이동 결과를 프린트한다.")
+    @Test
+    void printMap() {
+        List<String> bridge = List.of("U", "U", "D", "U", "D");
+        int position = 2;
+        boolean correct = false;
+        OutputView outputView = new OutputView();
+        String output = outputView.printMapBuild(bridge, position, correct);
+        String answer = "[ O | O |   ]\n" + "[   |   | X ]\n";
+        assertThat(output).isEqualTo(answer);
+    }
+}
