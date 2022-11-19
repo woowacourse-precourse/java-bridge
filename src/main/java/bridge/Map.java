a306da6c7fe2e3e1dceffdb228fd24a17ac044a6
JJ <jeongjae1996@naver.com>
2022-11-19 14:35:48 +0900

    feat: map을 만들기 위해 필요한 기호들 상수로 부여
    - map을 만드는데에 있어서 공용으로 사용될 기호를 상수로 부여했습니다.

--- src/main/java/bridge/Map.java
+++ src/main/java/bridge/Map.java
@@ -5,6 +5,13 @@ import java.util.Arrays;
 import java.util.List;
 
 public class Map {
+    private static final String MAP_BEGINNING = "[ ";
+    private static final String MAP_END = " ]";
+    private static final String DIVISION_LINE = " | ";
+    private static final String CORRECT = "O";
+    private static final String WRONG = "X";
+    private static final String BLANK_SPACE = " ";
+
     private List<String> map;
 
     public Map() {
@@ -12,8 +19,6 @@ public class Map {
     }
 
     private List<String> initializeMap() {
-        final String MAP_BEGINNING = "[ ";
-
         List<String> map = new ArrayList<>(
                 Arrays.asList(MAP_BEGINNING, MAP_BEGINNING)
         );
