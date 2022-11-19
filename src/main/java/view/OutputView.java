1a9926010a09ef0219cd8867d7511f524bbfce6a
masiljangajji <hjk172262@gmail.com>
2022-11-19 12:14:08 +0900

    feat: 게임 시작 메시지 출력 기능 추가

--- src/main/java/view/OutputView.java
+++ src/main/java/view/OutputView.java
@@ -4,6 +4,12 @@ import java.util.List;
 
 
 public class OutputView {
+
+    public void printStart() {
+        System.out.println(Message.START_GAME.getMessage());
+    }
+
+
     public void printMap(List<String> list) {
         for (int i = 0; i < list.size(); i++) {
             System.out.println(list.get(i));
