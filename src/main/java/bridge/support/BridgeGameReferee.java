506c2fa8ab6cf4fb61240999b168eb4677deb42f
saint6839 <saint6839@gmail.com>
2022-11-20 01:08:38 +0900

    refactor(domain) : 반복문을 외부에서 수행하고, result 값에 관한 단일 책임만 갖도록 수정
    - InputView 의존성 제거

--- src/main/java/bridge/support/BridgeGameReferee.java
+++ src/main/java/bridge/support/BridgeGameReferee.java
@@ -1,38 +1,21 @@
 package bridge.support;
 
-import bridge.BridgeGame;
 import bridge.MoveDirection;
 import bridge.Result;
-import bridge.view.InputView;
 import bridge.view.OutputView;
 
 public class BridgeGameReferee {
 
-    private final InputView inputView;
     private final OutputView outputView;
 
-    public BridgeGameReferee(InputView inputView, OutputView outputView) {
-        this.inputView = inputView;
+    public BridgeGameReferee(OutputView outputView) {
         this.outputView = outputView;
     }
 
-    public Result doJudge(int bridgeSize, BridgeGame bridgeGame) {
-        Result result = new Result();
-
-        for (int i = 0; i < bridgeSize; i++) {
-            MoveDirection direction = this.readMoving();
-            result.updateIsAnswers(direction, bridgeGame.move(direction, i));
-            this.printMap(result);
-            if(result.hasWrong()) {
-                return result;
-            }
-        }
+    public Result updateResult(Result result, MoveDirection direction, boolean isAnswer) {
+        result.updateIsAnswers(direction, isAnswer);
+        this.printMap(result);
         return result;
-    }
-
-    private MoveDirection readMoving() {
-        outputView.printInputMoveDirection();
-        return inputView.readMoving();
     }
 
     private void printMap(Result result) {
