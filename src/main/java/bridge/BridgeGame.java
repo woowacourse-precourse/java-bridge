7faf0fd505532505aa99eede2d60eb3f8b1d2e9a
Sanmgmin <sangmins930@gmail.com>
2022-11-19 16:43:34 +0900

    feat(BridgeGame): implement convertResultOStepToString

--- src/main/java/bridge/BridgeGame.java
+++ src/main/java/bridge/BridgeGame.java
@@ -21,18 +21,19 @@ public class BridgeGame {
      * 이동을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
     public void move(String direction) {
-
     }
 
-    public String getResultOfStep(String direction) {
+    public boolean isResultOfStep(String direction) {
         int round = user.getRound();
-        String resultOfStep = "X";
-        if (bridge.get(round).equals(direction)) {
-            resultOfStep = "O";
-        }
-        return resultOfStep;
+        return bridge.get(round).equals(direction);
     }
 
+    public String convertResultOfStepToString (boolean stepOfResult){
+        if (stepOfResult){
+            return "O";
+        }
+        return "X";
+    }
     /**
      * 사용자가 게임을 다시 시도할 때 사용하는 메서드
      * <p>
