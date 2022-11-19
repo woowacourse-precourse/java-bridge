62e5cb05621fb964674d30f2978f8e62e871bc01
seoyoung <sy9723@korea.ac.kr>
2022-11-19 15:54:16 +0900

    feat: 예외 처리 추가 ([이동할 칸] 입력 값이 U 또는 D가 아닌 경우)

--- src/main/java/bridge/Validate.java
+++ src/main/java/bridge/Validate.java
@@ -1,24 +1,33 @@
 package bridge;
 
+import java.util.Objects;
+
 public class Validate {
 
-    public int validateLengthOfBridge(String lengthOfBridge) {
+    public static int validateLengthOfBridge(String lengthOfBridge) {
         validateIsDigit(lengthOfBridge);
         return validateRangeOfLength(lengthOfBridge);
     }
 
-    private void validateIsDigit(String number) {
+    private static void validateIsDigit(String number) {
         if (!number.matches("^[0-9]+$")) {
             throw new IllegalArgumentException("[ERROR] 입력 값은 숫자여야 합니다.");
         }
     }
 
-    private int validateRangeOfLength(String number) {
+    private static int validateRangeOfLength(String number) {
         int lengthOfBridge = Integer.parseInt(number);
         if (lengthOfBridge > 20 || lengthOfBridge < 3) {
             throw new IllegalArgumentException("[ERROR] 입력 값은 3이상 20이하여야 합니다.");
         }
         return lengthOfBridge;
+    }
+
+    public static String validateMoving(String move) {
+        if (!(Objects.equals(move, "U") || Objects.equals(move, "D"))) {
+            throw new IllegalArgumentException("[ERROR] 입력 값은 U 또는 D여야 합니다.");
+        }
+        return move;
     }
 
 
