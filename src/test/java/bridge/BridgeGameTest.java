9a113e43738d561017bab59ad03e3ce51f4a25c7
49ehyeon42 <49ehyeon42@gmail.com>
2022-11-19 20:21:18 +0900

    test: BridgeGame 중 move 메서드 테스트 케이스 추가

---
+++ src/test/java/bridge/BridgeGameTest.java
@@ -0,0 +1,72 @@
+package bridge;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+import java.util.List;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+
+class BridgeGameTest {
+
+    @Nested
+    @DisplayName("move 테스트")
+    class MoveTest {
+
+        @Test
+        @DisplayName("move 동작 확인 1")
+        void moveTest1() {
+            // given
+            List<String> bridge = List.of("U", "D", "D");
+            BridgeGame bridgeGame = new BridgeGame(bridge);
+            // when
+            bridgeGame.move("U");
+            // then
+            assertThat(bridgeGame.getUpBridgeToString()).isEqualTo(" O ");
+            assertThat(bridgeGame.getDownBridgeToString()).isEqualTo("   ");
+        }
+
+        @Test
+        @DisplayName("move 동작 확인 2")
+        void moveTest2() {
+            // given
+            List<String> bridge = List.of("U", "D", "D");
+            BridgeGame bridgeGame = new BridgeGame(bridge);
+            // when
+            bridgeGame.move("U");
+            bridgeGame.move("U");
+            // then
+            assertThat(bridgeGame.getUpBridgeToString()).isEqualTo(" O | X ");
+            assertThat(bridgeGame.getDownBridgeToString()).isEqualTo("   |   ");
+        }
+
+        @Test
+        @DisplayName("move 동작 확인 3")
+        void moveTest3() {
+            // given
+            List<String> bridge = List.of("U", "D", "D");
+            BridgeGame bridgeGame = new BridgeGame(bridge);
+            // when
+            bridgeGame.move("U");
+            bridgeGame.move("D");
+            // then
+            assertThat(bridgeGame.getUpBridgeToString()).isEqualTo(" O |   ");
+            assertThat(bridgeGame.getDownBridgeToString()).isEqualTo("   | O ");
+        }
+
+        @Test
+        @DisplayName("move 동작 확인 4")
+        void moveTest4() {
+            // given
+            List<String> bridge = List.of("U", "D", "D");
+            BridgeGame bridgeGame = new BridgeGame(bridge);
+            // when
+            bridgeGame.move("U");
+            bridgeGame.move("D");
+            bridgeGame.move("D");
+            // then
+            assertThat(bridgeGame.getUpBridgeToString()).isEqualTo(" O |   |   ");
+            assertThat(bridgeGame.getDownBridgeToString()).isEqualTo("   | O | O ");
+        }
+    }
+}
