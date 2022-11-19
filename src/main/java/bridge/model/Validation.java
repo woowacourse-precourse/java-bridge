3bacb9b3a57813962d9fe579c8082a744dd371ee
Hi-Imjaeyoung <imjy2757@naver.com>
2022-11-19 15:46:56 +0900

    feat(bridgeSizeValidationTest) : 다리 사이즈 입력값 예외 검사 기능 구현.

--- src/main/java/bridge/model/Validation.java
+++ src/main/java/bridge/model/Validation.java
@@ -1,13 +1,21 @@
 package bridge.model;
 
+import bridge.constant.ErrorMessage;
+
 public class Validation {
+    private static final String ERROR = "[ERROR]";
     public static void bridgeSizeValidationTest(String input) throws IllegalArgumentException{
         valueInRange(isNumber(input));
     }
     private static int isNumber(String input){
-
+        if(input.length() != input.replaceAll("[^\\d]","").length()){
+            throw new IllegalArgumentException(ERROR+ ErrorMessage.NO_NUMERIC_VALUE.getErrorMessage());
+        }
+        return Integer.parseInt(input);
     }
     private static void valueInRange(int input){
-
+        if(input<3 || input>20){
+            throw new IllegalArgumentException(ERROR+ ErrorMessage.NUMBER_RANGE_IS_WRONG.getErrorMessage());
+        }
     }
 }
