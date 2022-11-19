86498797e6448a88ec47095ea34354d7cfdc8e28
2021110806 <2021110806@dgu.ac.kr>
2022-11-20 01:06:26 +0900

    feat(Application):에러 발생 시 정상 종료 구현

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -3,10 +3,11 @@ import bridge.BridgeGame;
 public class Application {
 
     public static void main(String[] args) {
-
-        BridgeGame bridgeGame = new BridgeGame();
-
-        bridgeGame.controller();
-
+        try {
+            Controller controller = new Controller();
+            controller.play();
+        } catch (IllegalArgumentException e){
+            System.out.println(e.getMessage());
+        }
     }
 }
