d2b52d72d9fa6857fc7834549f58d539dd7e562c
meunrud <y3a0522@gmail.com>
2022-11-19 16:23:47 +0900

    :sparkles: (BridgeSizeDTO) 다리 길이 입력 값 접근 위한 DTO 클래스 생성

---
+++ src/main/java/bridge/DTO/BridgeSizeDTO.java
@@ -0,0 +1,15 @@
+package bridge.DTO;
+
+public class BridgeSizeDTO {
+
+    private final int size;
+
+    public BridgeSizeDTO(int size){
+        this.size = size;
+    }
+
+    public int getSize(){
+        return size;
+    }
+
+}
--- src/main/java/bridge/InputView.java
+++ src/main/java/bridge/InputView.java
@@ -1,25 +1,30 @@
 package bridge;
 
+import bridge.DTO.BridgeSizeDTO;
 import camp.nextstep.edu.missionutils.Console;
+import bridge.InputValidator;
 
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
  */
 public class InputView {
 
+
     /**
      * 다리의 길이를 입력받는다.
      */
-    public int readBridgeSize() {
+    public BridgeSizeDTO readBridgeSize() {
         System.out.println("다리 길이를 입력해주세요");
         String inputBridgeLength = Console.readLine();
-        return 0;
+        return new BridgeSizeDTO(InputValidator.validateInputBridgeLength(inputBridgeLength));
     }
 
     /**
      * 사용자가 이동할 칸을 입력받는다.
      */
     public String readMoving() {
+        System.out.println("이동할 칸을 선택해주세요. (위: U, 아래 : D)");
+        String inputMove = Console.readLine();
         return null;
     }
 
