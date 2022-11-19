007c2a2039c315854716dd48e7be708e67029b74
lseok <lseokhyun99@gmail.com>
2022-11-19 15:59:24 +0900

    feat: 게임 최종 결과를 출력하는 메소드 구현
    게임의 최종 결과를 출력하는 메소드
    printResult()를 구현하였다.
    
    printMap()는 호출 시 마다 인스턴스 변수
    resultBridge, resultPath에 다리, 경로를 저장한다.
    
    printResult는 저장된
    resultBridge, resultPath를 이용해 결과를 출력한다.

--- src/main/java/bridge/OutputView.java
+++ src/main/java/bridge/OutputView.java
@@ -1,5 +1,6 @@
 package bridge;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -15,12 +16,18 @@ public class OutputView {
     private static final String BRIDGE_START = "[";
     private static final String BRIDGE_END = "\b]";
 
+    private List<String> resultBridge = new ArrayList<>();
+    private List<String> resultPath = new ArrayList<>();
+
     /**
      * 현재까지 이동한 다리의 상태를 정해진 형식에 맞춰 출력한다.
      * <p>
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
     public void printMap(List<String> bridge, List<String> path) {
+        resultBridge = bridge;
+        resultPath = path;
+
         System.out.print(BRIDGE_START);
         printBridge(UP, bridge, path);
         System.out.println(BRIDGE_END);
@@ -36,15 +43,22 @@ public class OutputView {
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
     public void printResult() {
+        System.out.print("[");
+        printBridge(UP, resultBridge, resultPath);
+        System.out.println("\b]");
+
+        System.out.print("[");
+        printBridge(DOWN, resultBridge, resultPath);
+        System.out.println("\b]");
     }
 
-    private void printBridge(String UpDown, List<String> bridge, List<String> path){
-        for(int i = 0 ; i < path.size() ; i++) {
-            if(path.get(i).equals(UpDown) && path.get(i).equals(bridge.get(i)))
+    private void printBridge(String UpDown, List<String> bridge, List<String> path) {
+        for (int i = 0; i < path.size(); i++) {
+            if (path.get(i).equals(UpDown) && path.get(i).equals(bridge.get(i)))
                 System.out.print(PATH_BRIDGE_EQUAL);
-            if(path.get(i).equals(UpDown) && !path.get(i).equals(bridge.get(i)))
+            if (path.get(i).equals(UpDown) && !path.get(i).equals(bridge.get(i)))
                 System.out.print(PATH_BRIDGE_NOT_EQUAL);
-            if(!path.get(i).equals(UpDown))
+            if (!path.get(i).equals(UpDown))
                 System.out.print(BRIDGE_NOT_SELECTED);
             System.out.print(SEPARATOR);
         }
