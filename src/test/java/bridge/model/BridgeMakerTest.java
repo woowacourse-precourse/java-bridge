6bcbbc1a0fff8270a63b640bc87e109c4ee94777
YuHayeon <youha0930@gmail.com>
2022-11-19 15:39:50 +0900

    test(BridgeMaker): 입력 길이에 따라 생성되는 다리 길이가 올바른지 테스트

---
+++ src/test/java/bridge/model/BridgeMakerTest.java
@@ -0,0 +1,29 @@
+package bridge.model;
+
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import bridge.BridgeMaker;
+import bridge.BridgeRandomNumberGenerator;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+class BridgeMakerTest {
+
+    private BridgeMaker bridgeMaker;
+
+    @BeforeEach
+    void setUp() {
+        bridgeMaker = new BridgeMaker(new BridgeRandomNumberGenerator());
+    }
+
+    @DisplayName("입력받은 다리의 길이를 검증 후 다리를 생성하여 길이를 비교한다.")
+    @ParameterizedTest
+    @ValueSource(ints = {3, 4, 9, 20})
+    void makeBridgeByValidatedSize(int size) {
+        assertThat(bridgeMaker.makeBridge(size).size()).isEqualTo(size);
+    }
+
+}
