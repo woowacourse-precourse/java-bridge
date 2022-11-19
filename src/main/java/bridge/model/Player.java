caf56ba2b75cd3145e77602e1d701ab25ecda3db
ma-an-jong <alswhd1113@gmail.com>
2022-11-19 14:41:03 +0900

    feat(Player): 다리 게임을 수행할 Player 객체를 만들어 다리 게임의 진행 상태를 관리
    BREAKING CHANGE: 다리 게임 이동과 관련 요청을 처리하는 Player 객체를 만들고 게임 상태를 저장
    
    Before :
    
        Player
        {
    
        }
    
    After :
    
        Player
        {
            move : 다음 칸으로 이동하는 함수
            restart : 게임이 재시작 되었을때 원래 자리도 돌아오는 함수
            isFinish : 게임에서 완주했는지 확인하는 함수
        }
    
        다리게임에서 이동과 관련된 요청을 처리하고 게임 상태를 관리하는 클래스 생성

---
+++ src/main/java/bridge/model/Player.java
@@ -0,0 +1,51 @@
+package bridge.model;
+
+import java.util.List;
+
+public class Player {
+
+    private int position;
+    private int tryCount;
+    private Bridge bridge;
+
+    public Player(Bridge bridge)
+    {
+        this.bridge = bridge;
+        this.position = 0;
+        this.tryCount = 0;
+    }
+
+    public Player(List<String> bridge)
+    {
+        this.bridge = new Bridge(bridge);
+        this.position = 0;
+        this.tryCount = 0;
+    }
+
+    public boolean move(String nextMove)
+    {
+         boolean flag = bridge.isMovable(position,nextMove);
+         tryCount++;
+         if(flag) position += 1;
+         return flag;
+    }
+
+    public void restart()
+    {
+        position--;
+    }
+
+    public boolean isFinish()
+    {
+        return bridge.isFinish(position);
+    }
+
+    @Override
+    public String toString()
+    {
+        String gameFinish = isFinish() ? "성공" : "실패";
+        String gameFinishMessage = String.format("게임 성공 여부: %s",gameFinish);
+        String tryCountMessage = String.format("총 시도한 횟수: %d\n",tryCount);
+        return String.join("\n",gameFinishMessage,tryCountMessage);
+    }
+}
