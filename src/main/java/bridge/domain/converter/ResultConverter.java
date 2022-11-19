4102bda322160cb8e86eaecacabb4ea82225879c
yummygyudon <bang2brew@gmail.com>
2022-11-19 09:36:41 +0900

    FEAT(domain.converter): 다리 통과 여부 결과 변환 기능 구현
    - 클래스 명 : ResultConverter
      - 메서드 명 : `convertToMark`
      - 매개변수 타입 : boolean
      - 매개변수 값 : 다리 통과 여부(isCrossable)
      - 처리
        - 통과(true)했다면 "O" 반환
        - 아니라면 "X" 반환

---
+++ src/main/java/bridge/domain/converter/ResultConverter.java
@@ -0,0 +1,12 @@
+package bridge.domain.converter;
+
+public class ResultConverter {
+
+    public String convertToMark(boolean isCrossable){
+        if(isCrossable){
+            return "O" ;
+        }
+        return "X";
+    }
+
+}
