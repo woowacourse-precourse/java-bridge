a6a6b0b04d478869da199325a2c66cfc7226a4b3
5nam <ohnam90@gmail.com>
2022-11-19 14:10:10 +0900

    feat: index 에 따라 결과 다리 생성하는 함수 추가

--- src/main/java/bridge/View/OutputView.java
+++ src/main/java/bridge/View/OutputView.java
@@ -1,9 +1,17 @@
 package bridge.View;
 
+import bridge.Model.Bridge;
+
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * 사용자에게 게임 진행 상황과 결과를 출력하는 역할을 한다.
  */
 public class OutputView {
+    private static List<String> bridgeMapUp = new ArrayList<>();
+    private static List<String> bridgeMapDown = new ArrayList<>();
+
 
     /**
      * 현재까지 이동한 다리의 상태를 정해진 형식에 맞춰 출력한다.
@@ -11,6 +19,31 @@ public class OutputView {
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
     public void printMap() {
+
+    }
+
+    private List<String> printBridge(List<String> result, int count) {
+        List<String> bridgeMap = new ArrayList<>();
+        int resultIndex = 0;
+        for(int index = 0; index<count*2+1; index++) {
+            if(index == 0) {
+                bridgeMap.add("[ ");
+                continue;
+            }
+            if(index%2 != 0) {
+                bridgeMap.add(result.get(resultIndex));
+                resultIndex++;
+                continue;
+            }
+            if(index%2 == 0 && index != count*2) {
+                bridgeMap.add(" | ");
+                continue;
+            }
+            if(index == count*2) {
+                bridgeMap.add(" ]");
+            }
+        }
+        return bridgeMap;
     }
 
     /**
