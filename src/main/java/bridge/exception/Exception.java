cff28c7b7f0b47d203853dc7c19a1a2e80376e45
viyott-lover <jackk0616@gmail.com>
2022-11-20 05:03:17 +0900

    feat(Exception): 입력이 숫자가 아닌 경우 예외 처리 추가

--- src/main/java/bridge/exception/Exception.java
+++ src/main/java/bridge/exception/Exception.java
@@ -2,7 +2,8 @@ package bridge.exception;
 
 public enum Exception {
     SIZE_RANGE_EXCEPTION(new IllegalArgumentException("[ERROR] " + "3 ~ 20 사이의 수를 입력해주세요")),
-    BRIDGE_NUMBER_RANGE_EXCEPTION(new IllegalArgumentException("[ERROR] " + "0 또는 1이 아닙니다."));
+    BRIDGE_NUMBER_RANGE_EXCEPTION(new IllegalArgumentException("[ERROR] " + "0 또는 1이 아닙니다.")),
+    IS_NOT_NUMBER_EXCEPTION(new IllegalArgumentException("[ERROR] " + "입력이 숫자가 아닙니다."));
     private IllegalArgumentException exception;
 
     Exception(IllegalArgumentException exception) {
