e62e1dc1195f2716ee93a258df0cfa7b405d13f1
jeongjihun <wlgns2234@naver.com>
2022-11-19 15:23:16 +0900

    feat(view): 현재 다리그림을 출력하는 기능구현

--- docs/README.md
+++ docs/README.md
@@ -7,7 +7,7 @@
 - 다리 건너기 게임 시작 출력 - [ x ] OutputView#printStart()
 - 다리의 길이를 숫자로 입력받는다(3~20)(에러 발생시키기) - 다시 입력받음 - [ x ] InputView#readBridgeSize()
 - 플레이어는 이동할 칸을 선택한다(U or D)(에러 발생시키기) - 다시 입력받음 - [ x ] InputView#readMoving()
-- 플레이어 이동 현황 o/x (다리 출력) - OutputView#printMap()
+- 플레이어 이동 현황 o/x (다리 출력) - [ x ] OutputView#printMap()
 - 다리를 건너다가 실패 했다면 재시작 or 종료(R or Q)(에러 발생시키기) - 다시 입력받음 - [ x ] InputView#readGameCommand()
 - 최종 게임 결과 출력 - OutputView#printResult()
 - 게임 성공 여부: 출력 - OutputView#printSuccessFailure()
@@ -18,9 +18,9 @@
   - 생성 시 무작위로  0, 1 중 무작위로 건널 수 있는지 없는지 생성 [ x ] BridgeRandomNumberGenerator#generate()
 - 플레이어 이동(현재 이동상태 리스트에 U, D 삽입) - [ x ] BridgeGame#move()
   - 현재 까지 이동한 다리 모양을 생성 - ???
-- 총 시도한 횟수 반환한다. BridgeGame#getTryNumber()
+- 총 시도한 횟수 반환한다. [ x ] BridgeGame#getTryNumber()
 
 ### controlloer
-- 게임을 시작한다 - BridgeGame#start()
-- 게임 재시작한다 - BridgeGame#retry()
-- 게임을 종료한다 - BridgeGame#end()
+- 게임을 시작한다 - [ x ] BridgeGame#start()
+- 게임 재시작한다 - [ x ] BridgeGame#retry()
+- 게임을 종료한다 - [ x ] BridgeGame#end()
--- src/main/java/bridge/view/OutputView.java
+++ src/main/java/bridge/view/OutputView.java
@@ -2,6 +2,8 @@ package bridge.view;
 
 import bridge.constant.BridgeGameInfo;
 
+import java.util.List;
+
 /**
  * 사용자에게 게임 진행 상황과 결과를 출력하는 역할을 한다.
  */
@@ -12,8 +14,68 @@ public class OutputView {
      * <p>
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void printMap() {
-        System.out.println("현재 이동한 다리 상태 출력(미완성)");
+    public static void printMap(List<String> targetBridge, List<String> preStatus) {
+        System.out.println(targetBridge);
+        System.out.println(preStatus);
+        // 윗줄 출력
+        System.out.print("[ ");
+        for (int i = 0; i < preStatus.size(); i++) {
+            if (preStatus.get(i).equals("U")) {
+                if (preStatus.get(i).equals(targetBridge.get(i))) {
+                    if (i == preStatus.size()-1) {
+                        System.out.print("O ");
+                        continue;
+                    }
+                    System.out.print("O | ");
+                    continue;
+                }
+
+                if (i == preStatus.size()-1) {
+                    System.out.print("X ");
+                    continue;
+                }
+                System.out.print("X | ");
+            }
+
+            if (preStatus.get(i).equals("D")) {
+                if (i == preStatus.size()-1) {
+                    System.out.print("  ");
+                    continue;
+                }
+                System.out.print("  | ");
+            }
+        }
+        System.out.println("]");
+
+        // 아랫줄 출력
+        System.out.print("[ ");
+        for (int i = 0; i < preStatus.size(); i++) {
+            if (preStatus.get(i).equals("D")) {
+                if (preStatus.get(i).equals(targetBridge.get(i))) {
+                    if (i == preStatus.size()-1) {
+                        System.out.print("O ");
+                        continue;
+                    }
+                    System.out.print("O | ");
+                    continue;
+                }
+
+                if (i == preStatus.size()-1) {
+                    System.out.print("X ");
+                    continue;
+                }
+                System.out.print("X | ");
+            }
+
+            if (preStatus.get(i).equals("U")) {
+                if (i == preStatus.size()-1) {
+                    System.out.print("  ");
+                    continue;
+                }
+                System.out.print("  | ");
+            }
+        }
+        System.out.println("]");
     }
 
     /**
@@ -21,8 +83,8 @@ public class OutputView {
      * <p>
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void printResult() {
-        System.out.println(BridgeGameInfo.RESULT);
+    public static void printResult() {
+        System.out.println(BridgeGameInfo.RESULT.message);
     }
 
     // 게임시작 메세지 출력
@@ -31,12 +93,14 @@ public class OutputView {
     }
 
     // 시도 횟수 출력
-    public void printTryNumber() {
-        System.out.println(BridgeGameInfo.TOTAL_TRY.message);
+    public static void printTryNumber(int number) {
+        System.out.print(BridgeGameInfo.TOTAL_TRY.message);
+        System.out.println(number);
     }
 
     // 성공 여부 출력
-    public void printSuccessFailure() {
-        System.out.println(BridgeGameInfo.SUCSSES_FAILURE);
+    public static void printSuccessFailure() {
+        System.out.println(BridgeGameInfo.SUCSSES_FAILURE.message);
+
     }
 }
