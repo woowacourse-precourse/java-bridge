b705c5144ea3dbb2a0d0d0c828168e7abf238244
recicoco068 <gkapf838@gmail.com>
2022-11-19 10:56:38 +0900

    feat(UserState.java) : 유저가 다리를 이동한 현황을 문자열로 반환하는 기능 구현

--- src/main/java/bridge/domain/UserState.java
+++ src/main/java/bridge/domain/UserState.java
@@ -6,13 +6,48 @@ import java.util.List;
 public class UserState {
     private boolean quit;
     private int tryCount;
-    List<String> upState;
-    List<String> downState;
+    private final List<String> upLine;
+    private final List<String> downLine;
 
     public UserState() {
         this.quit = false;
         this.tryCount = 1;
-        this.upState = new ArrayList<>();
-        this.downState = new ArrayList<>();
+        this.upLine = new ArrayList<>();
+        this.downLine = new ArrayList<>();
+    }
+
+    public void quitChange() {
+        quit = !quit;
+    }
+
+    public void tryCountIncrease() {
+        tryCount++;
+    }
+
+    public boolean isQuit() {
+        return quit;
+    }
+
+    public int getTryCount() {
+        return tryCount;
+    }
+
+    public int getStep() {
+        return upLine.size();
+    }
+
+    public String toString() {
+        String up = makeLineOutput(upLine);
+        String down = makeLineOutput(downLine);
+        return up + "\n" + down;
+    }
+
+    private String makeLineOutput(List<String> line) {
+        StringBuilder output = new StringBuilder("[");
+        for (String c : line) {
+            output.append(" ").append(c).append(" |");
+        }
+        output.replace(output.length() - 1, output.length(), "]");
+        return String.valueOf(output);
     }
 }
