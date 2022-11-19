05cc0bb4c997fb7616316f3c28b3d07c27d3cc89
Kimhyeyun <cqqwer@gmail.com>
2022-11-20 01:35:20 +0900

    Fix: 오타 수정

--- src/main/java/bridge/constant/Message.java
+++ src/main/java/bridge/constant/Message.java
@@ -16,7 +16,7 @@ public class Message {
 
     public static final String ERROR_MESSAGE = "[ERROR] ";
 
-    public static final String RESULT_MESSAGE = "최종 결과 게임";
+    public static final String RESULT_MESSAGE = "최종 게임 결과";
     public static final String IS_SUCCESS_OR_FAIL = "게임 성공 여부: ";
     public static final String SUCCESS = "성공";
     public static final String FAIL = "실패";
