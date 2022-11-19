d677ecdc0e372c9322e45340b7427dd81a8d095f
박원종 <wonjong1030@naver.com>
2022-11-19 21:25:21 +0900

    feat(BridgeController): 게임 시작 메소드 구현

--- src/main/java/bridge/controller/BridgeController.java
+++ src/main/java/bridge/controller/BridgeController.java
@@ -1,2 +1,38 @@
-package bridge.controller;public class BridgeController {
+package bridge.controller;
+
+import bridge.BridgeMaker;
+import bridge.BridgeRandomNumberGenerator;
+import bridge.service.BridgeGame;
+import bridge.view.InputView;
+import bridge.view.OutputView;
+
+public class BridgeController {
+    private final InputView inputView;
+    private final OutputView outputView;
+
+    public BridgeController(InputView inputView, OutputView outputView) {
+        this.inputView = inputView;
+        this.outputView = outputView;
+    }
+
+    public void run() throws IllegalArgumentException {
+        BridgeMaker bridgeMaker = new BridgeMaker(new BridgeRandomNumberGenerator());
+        BridgeGame bridgeGame = new BridgeGame(bridgeMaker.makeBridge(inputView.readBridgeSize()));
+        while (true) {
+            bridgeGame.move(inputView.readMoving());
+            outputView.printMap(bridgeGame.getResult());
+            if (bridgeGame.isEnd()) {
+                outputView.printResult(bridgeGame.getResult(), bridgeGame.isGameFail(), BridgeGame.tryCount);
+                return;
+            }
+            if (bridgeGame.isGameFail()) {
+                if (inputView.readGameCommand().equals("Q")) {
+                    outputView.printResult(bridgeGame.getResult(), true, BridgeGame.tryCount);
+                    return;
+                }
+                bridgeGame.retry();
+            }
+        }
+
+    }
 }
