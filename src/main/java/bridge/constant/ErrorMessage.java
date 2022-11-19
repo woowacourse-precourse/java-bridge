ccb92b0cc95f571b97d50fe742b8f7d8dcfad4e1
skydreamer21 <skydreamer210@gmail.com>
2022-11-19 17:19:09 +0900

    feat(BridgeLength): 다리 길이 Wrapping Class 구현
    [세부사항]
    - Wrapping 클래스 안에 유효성 검증 기능 포함

--- src/main/java/bridge/constant/ErrorMessage.java
+++ src/main/java/bridge/constant/ErrorMessage.java
@@ -3,6 +3,8 @@ package bridge.constant;
 public enum ErrorMessage {
     EMPTY_VALUE("값을 입력하지 않았습니다."),
     NOT_NUMBER("숫자만 입력해주세요."),
+    INVALID_BRIDGE_LENGTH("다리의 길이로는 " + BridgeProperties.MIN_LENGTH.get() + "부터 "
+            + BridgeProperties.MAX_LENGTH.get() +"까지의 숫자만 허용됩니다.");
 
     private final String ERROR_PREFIX = "[ERROR] ";
     private final String errorMessage;
---
+++ src/main/java/bridge/domain/BridgeLength.java
@@ -0,0 +1,27 @@
+package bridge.domain;
+
+import bridge.constant.BridgeProperties;
+import bridge.constant.ErrorMessage;
+
+public class BridgeLength {
+    private int length;
+
+    public BridgeLength (int length) {
+        validate(length);
+        this.length = length;
+    }
+
+    private void validate (int length) {
+        if (!isCorrectRange(length)) {
+            throw new IllegalArgumentException(ErrorMessage.INVALID_BRIDGE_LENGTH.get());
+        }
+    }
+
+    private boolean isCorrectRange(int length) {
+        return length >= BridgeProperties.MIN_LENGTH.get() && length <= BridgeProperties.MAX_LENGTH.get();
+    }
+
+    public int getLength() {
+        return length;
+    }
+}
