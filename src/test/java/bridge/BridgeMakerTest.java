0a8b5cd51b44d791e841901813b2ca4b8bd81dfa
nn98 <jkllhgb@gmail.com>
2022-11-20 00:17:02 +0900

    Test: ExceptionTest-bridgeSize

--- src/test/java/bridge/BridgeMakerTest.java
+++ src/test/java/bridge/BridgeMakerTest.java
@@ -1,2 +1,38 @@
-package bridge;public class BridgeMakerTest {
+package bridge;
+
+import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.util.Lists.newArrayList;
+
+import bridge.ApplicationTest.TestNumberGenerator;
+import camp.nextstep.edu.missionutils.test.NsTest;
+import java.util.List;
+import org.junit.jupiter.api.Test;
+
+public class BridgeMakerTest extends NsTest {
+
+    private static final String ERROR_MESSAGE = "[ERROR]";
+
+    @Test
+    void 예외_테스트_다리길이_음수() {
+        assertSimpleTest(() -> {
+            BridgeMaker bridgeMaker = new BridgeMaker(new BridgeRandomNumberGenerator());
+            bridgeMaker.makeBridge(-5);
+            assertThat(output()).contains(ERROR_MESSAGE);
+        });
+    }
+
+    @Test
+    void 예외_테스트_다리길이_범위초과() {
+        assertSimpleTest(() -> {
+            BridgeMaker bridgeMaker = new BridgeMaker(new BridgeRandomNumberGenerator());
+            bridgeMaker.makeBridge(1);
+            assertThat(output()).contains(ERROR_MESSAGE);
+        });
+    }
+
+    @Override
+    protected void runMain() {
+        Application.main(new String[]{});
+    }
 }
