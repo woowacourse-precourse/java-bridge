d2f90846be4d5252dd7acb615bf4f1f3a3df5b38
Haechan Lee <dlgockssla@gmail.com>
2022-11-19 10:20:11 +0900

    feat(InputView): read moving and return Direction

--- src/main/java/bridge/InputView.java
+++ src/main/java/bridge/InputView.java
@@ -22,8 +22,8 @@ public class InputView {
     /**
      * 사용자가 이동할 칸을 입력받는다.
      */
-    public String readMoving() {
-        return Console.readLine();
+    public Direction readMoving() {
+        return Direction.fromSymbol(Console.readLine());
     }
 
     /**
