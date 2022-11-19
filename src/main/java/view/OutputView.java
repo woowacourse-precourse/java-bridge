3663f33d87acbc2fa6302f433498de7258e66777
June <68291395+june-777@users.noreply.github.com>
2022-11-19 21:11:48 +0900

    feat(OutputView): 게임 위쪽 상태와 게임 아래쪽 상태의 문자열을 합치는 기능 구현

--- src/main/java/view/OutputView.java
+++ src/main/java/view/OutputView.java
@@ -1,23 +1,36 @@
 package view;
 
+import static view.OutputViewConstants.*;
+
 /**
  * 사용자에게 게임 진행 상황과 결과를 출력하는 역할을 한다.
  */
 public class OutputView {
 
-    /**
-     * 현재까지 이동한 다리의 상태를 정해진 형식에 맞춰 출력한다.
-     * <p>
-     * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
-     */
-    public void printMap() {
-    }
+	private static String currentGameStateUpMap = OUTPUT_TEXT_OPEN_SQUARE_BRACKETS;
+	private static String currentGameStarteDownMap = OUTPUT_TEXT_OPEN_SQUARE_BRACKETS;
 
-    /**
-     * 게임의 최종 결과를 정해진 형식에 맞춰 출력한다.
-     * <p>
-     * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
-     */
-    public void printResult() {
-    }
+	/**
+	 * 현재까지 이동한 다리의 상태를 정해진 형식에 맞춰 출력한다.
+	 * <p>
+	 * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
+	 */
+	public void printMap(String moving, int movingResult) {
+		String currentGameStateMap = combineGameStateUpMapAndDownMap();
+	}
+
+	private String combineGameStateUpMapAndDownMap() {
+		StringBuilder sb = new StringBuilder();
+		String currentGameStateMap = sb.append(currentGameStateUpMap).append("\r\n").append(currentGameStarteDownMap)
+				.toString();
+		return currentGameStateMap;
+	}
+
+	/**
+	 * 게임의 최종 결과를 정해진 형식에 맞춰 출력한다.
+	 * <p>
+	 * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
+	 */
+	public void printResult() {
+	}
 }
