895fdda9321c8efb5cd654a7dc96211eb54b0e46
JeonSangYong <tkddyd420@naver.com>
2022-11-19 21:59:04 +0900

    test : 다리 이동 성공 테스트

---
+++ src/test/java/bridge/BridgeGameTest.java
@@ -0,0 +1,20 @@
+package bridge;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.*;
+
+class BridgeGameTest {
+    
+    @Test
+    void 다리_이동_성공() {
+        //given
+        BridgeGame bridgeGame = new BridgeGame(List.of("U", "U", "U"));
+        //when
+        bridgeGame.move("U");
+        //then
+        assertThat(bridgeGame.isGameEnd()).isFalse();
+    }
+}
