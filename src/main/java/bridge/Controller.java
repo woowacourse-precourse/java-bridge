a026dacdaedca8b493e57e0eac439b3cde945e5e
swonny <wonny921@gmail.com>
2022-11-20 01:17:16 +0900

    feat(Controller): 이동할 칸을 입력 받아 사용자 위치 이동하기 구현

--- src/main/java/bridge/Controller.java
+++ src/main/java/bridge/Controller.java
@@ -1,14 +1,35 @@
 package bridge;
 
+import enumCollections.GameStatus;
 import enumCollections.GuideMessage;
 
 public class Controller {
+    OutputView outputView;
+    InputView  inputView;
+
+    public Controller(OutputView outputView, InputView inputView) {
+        this.outputView = outputView;
+        this.inputView = inputView;
+    }
+
     public void startGame(BridgeGame bridgeGame) {
         bridgeGame.generateBridge(getBridgeSize());
+        play(bridgeGame, GameStatus.CONTINUE);
     }
 
     private int getBridgeSize() {
         new OutputView().printGuideMessage(GuideMessage.START);
-        return new InputView().readBridgeSize();
+        return this.inputView.readBridgeSize();
+    }
+
+    private GameStatus play(BridgeGame bridgeGame, GameStatus gameStatus) {
+        while (gameStatus == GameStatus.CONTINUE) {
+            gameStatus = bridgeGame.tryMoveTo(getMoving());
+        }
+    }
+
+    private String getMoving() {
+        outputView.printGuideMessage(GuideMessage.GET_MOVING);
+        return inputView.readMoving();
     }
 }
