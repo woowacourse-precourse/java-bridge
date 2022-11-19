d6dd1cdc5a1656842a6c6b70eaebd998a6aecc85
최원용 <nyong@choewon-yong-ui-MacBookPro.local>
2022-11-20 01:27:44 +0900

    feat(Test): 이동할 블럭을 입력 받을 때 1자리의 문자열이 아니거나, U 또는 D 이외의 문자 입력시 예외가 발생한다.

--- src/test/java/bridge/validate/ValidateInputTest.java
+++ src/test/java/bridge/validate/ValidateInputTest.java
@@ -8,7 +8,7 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class ValidateInputTest {
 
-    @DisplayName("입력받은 다리 길이가 정수가 아니거나 입력받은 3이상 20이하의 수가 아니라면 예외가 발생한다.")
+    @DisplayName("입력받은 다리 길이가 정수가 아니거나, 입력받은 3이상 20이하의 수가 아니라면 예외가 발생한다.")
     @Test
     void 다리길이가_정수가_아니거나_3이상_20이하의_수가_아니면_예외_발생() {
         assertThrows(IllegalArgumentException.class, () -> {
@@ -20,4 +20,16 @@ class ValidateInputTest {
         });
     }
 
+
+    @DisplayName("이동할 블럭을 입력 받을 때 1자리의 문자열이 아니거나, U 또는 D 이외의 문자 입력시 예외가 발생한다.")
+    @Test
+    void 이동할_블럭이_1자리_문자열이_아니거나_U_또는_D가_아니라면_예외_발생() {
+        assertThrows(IllegalArgumentException.class, () -> {
+            ValidateInput.validateMovingBlock("UU");
+        });
+
+        assertThrows(IllegalArgumentException.class, () -> {
+            ValidateInput.validateMovingBlock("S");
+        });
+    }
 }
