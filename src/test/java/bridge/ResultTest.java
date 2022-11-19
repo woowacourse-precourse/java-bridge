70b4dc5623c0fdbcc26aebee0d3b25037d0790f1
saint6839 <saint6839@gmail.com>
2022-11-19 22:38:31 +0900

    test(domain) : 사용자가 입력한 방향과 정답 여부에 따른 판별 값 반환 테스트 작성

--- src/test/java/bridge/ResultTest.java
+++ src/test/java/bridge/ResultTest.java
@@ -1,30 +1,89 @@
 package bridge;
 
-import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 class ResultTest {
+    private static final String UP_SIGN = "U";
+    private static final String DOWN_SIGN = "D";
+
+    private Result result;
+    @BeforeEach
+    void setUp() {
+        result = new Result();
+    }
 
     @DisplayName("정답 여부가 결과에 입력되었을 때, 정답 여부 리스트와 시도 횟수가 잘 저장되는지 테스트")
     @Test
     void updateIsAnswers() {
         //given
-        Result result = new Result();
-
         //when
-        result.updateIsAnswers(new MoveDirection("U"), true);
-        result.updateIsAnswers(new MoveDirection("D"), true);
-        result.updateIsAnswers(new MoveDirection("D"), false);
-
+        result.updateIsAnswers(new MoveDirection(UP_SIGN), true);
+        result.updateIsAnswers(new MoveDirection(DOWN_SIGN), true);
+        result.updateIsAnswers(new MoveDirection(DOWN_SIGN), false);
         //then
-        Assertions.assertThat(result.getTryCount())
+        assertThat(result.getTryCount())
                   .isEqualTo(3);
-        Assertions.assertThat(result.getIsAnswers()
+        assertThat(result.getIsAnswers()
                                     .get(0)).isTrue();
-        Assertions.assertThat(result.getIsAnswers()
+        assertThat(result.getIsAnswers()
                                     .get(1)).isTrue();
-        Assertions.assertThat(result.getIsAnswers()
+        assertThat(result.getIsAnswers()
                                     .get(2)).isFalse();
+    }
+
+    @DisplayName("사용자가 입력한 방향이 U이고, 실제 정답일 경우 참을 반환하는지 테스트")
+    @Test
+    void isUpSignAnswer() {
+        //given
+        boolean isAnswer = true;
+        MoveDirection direction = new MoveDirection(UP_SIGN);
+        //when
+        boolean upSignAnswer = result.isUpSignAnswer(isAnswer, direction);
+        //then
+        assertThat(upSignAnswer)
+                  .isTrue();
+    }
+
+    @DisplayName("사용자가 입력한 방향이 U이고, 실제 정답이 아닐 경우 참을 반환하는지 테스트")
+    @Test
+    void isUpSignWrongAnswer() {
+        //given
+        boolean isAnswer = false;
+        MoveDirection direction = new MoveDirection(UP_SIGN);
+        //when
+        boolean upSignWrongAnswer = result.isUpSignWrongAnswer(isAnswer, direction);
+        //then
+        assertThat(upSignWrongAnswer)
+                .isTrue();
+    }
+
+    @DisplayName("사용자가 입력한 방향이 D이고, 실제 정답일 경우 참을 반환하는지 테스트")
+    @Test
+    void isDownSignAnswer() {
+        //given
+        boolean isAnswer = true;
+        MoveDirection direction = new MoveDirection(DOWN_SIGN);
+        //when
+        boolean downSignAnswer = result.isDownSignAnswer(isAnswer, direction);
+        //then
+        assertThat(downSignAnswer)
+                .isTrue();
+    }
+
+    @DisplayName("사용자가 입력한 방향이 D이고, 실제 정답이 아닐 경우 참을 반환하는지 테스트")
+    @Test
+    void isDownSignWrongAnswer() {
+        //given
+        boolean isAnswer = false;
+        MoveDirection direction = new MoveDirection(DOWN_SIGN);
+        //when
+        boolean downSignWrongAnswer = result.isDownSignWrongAnswer(isAnswer, direction);
+        //then
+        assertThat(downSignWrongAnswer)
+                .isTrue();
     }
 }
