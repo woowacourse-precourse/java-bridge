222d41be5434ddd55c9edce3605f6d6d65dbcbf1
junpakPark <112045553+junpakPark@users.noreply.github.com>
2022-11-19 16:50:03 +0900

    test(ExitOptionTest): 테스트 코드 리팩토링

--- src/test/java/bridge/model/ExitOptionTest.java
+++ src/test/java/bridge/model/ExitOptionTest.java
@@ -5,109 +5,36 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import bridge.Error;
 
 class ExitOptionTest {
 	@DisplayName("R 혹은 Q를 입력하면 Boolean을 리턴한다.")
-	@Test
-	void case1() {
-		assertTrue(ExitOption.of("R").isReplay());
-		assertTrue(ExitOption.of("r").isReplay());
-		assertFalse(ExitOption.of("Q").isReplay());
-		assertFalse(ExitOption.of("q").isReplay());
+	@Nested
+	class BooleanTest {
+		@ValueSource(strings = {"R", "r"})
+		@ParameterizedTest
+		void trueCase(String value) {
+			assertTrue(ExitOption.of(value).isReplay());
+		}
+
+		@ValueSource(strings = {"Q", "q"})
+		@ParameterizedTest
+		void falseCase(String value) {
+			assertFalse(ExitOption.of(value).isReplay());
+		}
 	}
 
 	@DisplayName("R 혹은 Q 외의 것을 입력하면 에러 메시지를 출력한다")
-	@Nested
-	class IsReplayTest {
-		@Test
-		void case1() {
-			Throwable exception = assertThrows(IllegalArgumentException.class, () -> {
-				ExitOption.of("a");
-			});
-			Assertions.assertEquals(Error.EXIT_OPTION.getMessage(), exception.getMessage());
-		}
-
-		@Test
-		void case2() {
-			Throwable exception = assertThrows(IllegalArgumentException.class, () -> {
-				ExitOption.of("Z");
-			});
-			assertEquals(Error.EXIT_OPTION.getMessage(), exception.getMessage());
-		}
-
-		@Test
-		void case3() {
-			Throwable exception = assertThrows(IllegalArgumentException.class, () -> {
-				ExitOption.of("0");
-			});
-			assertEquals(Error.EXIT_OPTION.getMessage(), exception.getMessage());
-		}
-
-		@Test
-		void case4() {
-			Throwable exception = assertThrows(IllegalArgumentException.class, () -> {
-				ExitOption.of("-1");
-			});
-			assertEquals(Error.EXIT_OPTION.getMessage(), exception.getMessage());
-		}
-
-		@Test
-		void case5() {
-			Throwable exception = assertThrows(IllegalArgumentException.class, () -> {
-				ExitOption.of("100000");
-			});
-			assertEquals(Error.EXIT_OPTION.getMessage(), exception.getMessage());
-		}
-
-		@Test
-		void case6() {
-			Throwable exception = assertThrows(IllegalArgumentException.class, () -> {
-				ExitOption.of("");
-			});
-			assertEquals(Error.EXIT_OPTION.getMessage(), exception.getMessage());
-		}
-
-		@Test
-		void case7() {
-			Throwable exception = assertThrows(IllegalArgumentException.class, () -> {
-				ExitOption.of(" ");
-			});
-			assertEquals(Error.EXIT_OPTION.getMessage(), exception.getMessage());
-		}
-
-		@Test
-		void case8() {
-			Throwable exception = assertThrows(IllegalArgumentException.class, () -> {
-				ExitOption.of("U");
-			});
-			assertEquals(Error.EXIT_OPTION.getMessage(), exception.getMessage());
-		}
-
-		@Test
-		void case9() {
-			Throwable exception = assertThrows(IllegalArgumentException.class, () -> {
-				ExitOption.of("u");
-			});
-			assertEquals(Error.EXIT_OPTION.getMessage(), exception.getMessage());
-		}
-
-		@Test
-		void case10() {
-			Throwable exception = assertThrows(IllegalArgumentException.class, () -> {
-				ExitOption.of("D");
-			});
-			assertEquals(Error.EXIT_OPTION.getMessage(), exception.getMessage());
-		}
-
-		@Test
-		void case11() {
-			Throwable exception = assertThrows(IllegalArgumentException.class, () -> {
-				ExitOption.of("d");
-			});
-			assertEquals(Error.EXIT_OPTION.getMessage(), exception.getMessage());
-		}
+	@ValueSource(strings = {"", " ", "a", "d", "u", "D", "U", "Z", "0", "-1", "100000"})
+	@ParameterizedTest
+	void exceptionCase(String value) {
+		Throwable exception = assertThrows(IllegalArgumentException.class, () -> {
+			ExitOption.of(value);
+		});
+		Assertions.assertEquals(Error.EXIT_OPTION.getMessage(), exception.getMessage());
 	}
+
 }
