84c4f6e6012459b5890e7e6ce59b0b28bf34943b
Park shinyeong <syngh503@gmail.com>
2022-11-19 16:07:16 +0900

    Fix(StartGameService): gameResultGenerator 필드 추가
    bridgeGame 생성 시 gameResultGenerator이 필요하기 때문에 startGameService 생성 시 gameresultGenerator을 전달하도록 수정하였습니다.

--- src/main/java/bridge/Controller/Service/StartGameService.java
+++ src/main/java/bridge/Controller/Service/StartGameService.java
@@ -3,15 +3,22 @@ package bridge.Controller.Service;
 import Utils.ValidateUtils;
 import bridge.Domain.Bridge;
 import bridge.Domain.BridgeGame;
+import bridge.Domain.GameResultGenerator;
 import bridge.View.InputView;
 import bridge.View.OutputView;
 
 public class StartGameService {
+    GameResultGenerator gameResultGenerator;
+
+    public StartGameService(GameResultGenerator gameResultGenerator) {
+        this.gameResultGenerator = gameResultGenerator;
+    }
 
     public BridgeGame startGame() throws IllegalArgumentException {
         OutputView.printGameStart();
         String bridgeSize = InputView.readBridgeSize();
         ValidateUtils.isValidateLength(bridgeSize);
-        return new BridgeGame(new Bridge(Integer.parseInt(bridgeSize)));
+        int size = Integer.parseInt(bridgeSize);
+        return new BridgeGame(new Bridge(size), gameResultGenerator);
     }
 }
