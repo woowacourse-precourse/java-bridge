47b1e8771f5177920eb41ea88bcef2d1759dde6d
Your Name <you@example.com>
2022-11-19 16:10:59 +0900

    feat: 최종 게임 결과 출력 구현

--- src/main/java/bridge/OutputView.java
+++ src/main/java/bridge/OutputView.java
@@ -19,7 +19,21 @@ public class OutputView {
      * <p>
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void printResult() {
+    public void printResult(String resultMap, int tryNum, boolean isSucces) {
+        System.out.println("최종 게임 결과");
+
+        System.out.println(resultMap);
+
+        System.out.println("게임 성공 여부: " + printSucces(isSucces, resultMap));
+
+        System.out.println("총 시도한 횟수: " + tryNum);
+        
+    }
+
+    private String printSucces(boolean isSucces, String succes) {
+        if(isSucces)
+            return "성공";
+        return "실패";
     }
 
     public void printStart() {
