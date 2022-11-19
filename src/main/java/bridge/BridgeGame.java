aebb8f6d6b7240ac2561d8cdad15a2c059a46c00
Lee Min Woo <lmw7414@naver.com>
2022-11-20 02:12:52 +0900

    feat : BridgeGame 클래스 필드 추가 및 Getter메서드 생성
    BridgeGame 클래스에는 bridge의 정보와 사용자가 입력하는 답 리스트, 그리고 시도 횟수가 필요하다. 따라서 해당 필드 정보를 반환하는 Getter 메서드와 일급 컬렉션인 Bridge 클래스를 초기화 할 수 있는 initBridge() 메서드를 추가하였다.

--- src/main/java/bridge/BridgeGame.java
+++ src/main/java/bridge/BridgeGame.java
@@ -1,9 +1,25 @@
 package bridge;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * 다리 건너기 게임을 관리하는 클래스
  */
 public class BridgeGame {
+
+    private Bridge bridge;
+    private List<String> userAnswer;
+    private int attempt;
+
+    public BridgeGame() {
+        this.userAnswer = new ArrayList<>();
+        this.attempt = 1;
+    }
+
+    public void initBridge(int size) {
+        bridge = new Bridge(size);
+    }
 
     /**
      * 사용자가 칸을 이동할 때 사용하는 메서드
@@ -14,10 +30,22 @@ public class BridgeGame {
     }
 
     /**
-     * 사용자가 게임을 다시 시도할 때 사용하는 메서드
+     * 사용자가 칸을 이동할 때 사용하는 메서드
      * <p>
-     * 재시작을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
+     * 이동을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
     public void retry() {
+    }
+    
+    public Bridge getBridge() {
+        return bridge;
+    }
+
+    public List<String> getUserAnswer() {
+        return userAnswer;
+    }
+
+    public int getAttempt() {
+        return attempt;
     }
 }
