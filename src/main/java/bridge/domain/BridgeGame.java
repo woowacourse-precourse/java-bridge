dd89f36279300a6b93f982adace0e0aaf85d1ef2
Jaeyeop-Jung <jjy5349@naver.com>
2022-11-19 21:55:31 +0900

    feat: 게임 커맨드가 재시작을 원하는지 판단하는 기능 추가

--- src/main/java/bridge/domain/BridgeGame.java
+++ src/main/java/bridge/domain/BridgeGame.java
@@ -1,5 +1,7 @@
 package bridge.domain;
 
+import bridge.constant.GameRetryCommand;
+
 /**
  * 다리 건너기 게임을 관리하는 클래스
  */
@@ -18,7 +20,7 @@ public class BridgeGame {
      * <p>
      * 이동을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void move() {
+    public void move(String direction) {
     }
 
     /**
@@ -26,7 +28,12 @@ public class BridgeGame {
      * <p>
      * 재시작을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void retry() {
+    public Boolean retry(String command) {
+        if (command.equals(GameRetryCommand.RETRY.getFirstLetter())) {
+            user.increaseRetryCount();
+            return true;
+        }
+        return false;
     }
 
     public Boolean isEnd() {
