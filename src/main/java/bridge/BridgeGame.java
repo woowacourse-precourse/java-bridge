e89763384007c6d80d8d95ca17d874f864533403
nn98 <jkllhgb@gmail.com>
2022-11-20 02:05:46 +0900

    Feat: 이동 기능 구현을 위한 인스턴스 추가, 이동 메소드 파라미터 추가

--- src/main/java/bridge/BridgeGame.java
+++ src/main/java/bridge/BridgeGame.java
@@ -8,10 +8,13 @@ import java.util.List;
 public class BridgeGame {
 
     private List<String> bridge;
+    private int index;
 
     public BridgeGame(int size) {
         BridgeMaker bridgeMaker = new BridgeMaker(new BridgeRandomNumberGenerator());
         bridge = bridgeMaker.makeBridge(size);
+        index=0;
+
         System.out.println(bridge);
     }
 
@@ -20,7 +23,8 @@ public class BridgeGame {
      * <p>
      * 이동을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void move() {
+    public void move(String choice) {
+
     }
 
     /**
