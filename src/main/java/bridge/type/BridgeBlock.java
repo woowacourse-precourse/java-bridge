4049f12ae70f0dbf4e8f7a58870f5f7a46e21da6
jinhyeon <vmflwu123@gmail.com>
2022-11-19 22:21:42 +0900

    refactor(BridgeBlock): 변수명 변경
    valueType -> blockNumber
    blockType -> blockSymbol

--- src/main/java/bridge/type/BridgeBlock.java
+++ src/main/java/bridge/type/BridgeBlock.java
@@ -5,17 +5,17 @@ public enum BridgeBlock {
     DOWN(0, "D"),
     UP(1, "U");
 
-    private final Integer valueType;
-    private final String blockType;
+    private final Integer blockNumber;
+    private final String blockSymbol;
 
-    BridgeBlock(Integer valueType, String blockType) {
-        this.valueType = valueType;
-        this.blockType = blockType;
+    BridgeBlock(Integer blockNumber, String blockSymbol) {
+        this.blockNumber = blockNumber;
+        this.blockSymbol = blockSymbol;
     }
 
-    public static String getBlockType(Integer valueType) {
-        if (valueType == DOWN.valueType) return DOWN.blockType;
-        if (valueType == UP.valueType) return UP.blockType;
+    public static String getBlockType(Integer blockNumber) {
+        if (blockNumber == DOWN.blockNumber) return DOWN.blockSymbol;
+        if (blockNumber == UP.blockNumber) return UP.blockSymbol;
         return null;
     }
 }
