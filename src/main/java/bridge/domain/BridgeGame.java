6d31bbd670bc598d39092ea15df9e5a6cd4a821b
alstjr706 <alstjr706@naver.com>
2022-11-20 02:42:00 +0900

    refactor : (다리 건너기) 칸을 이동하는 메서드 책임 분리 리팩토링

--- src/main/java/bridge/domain/BridgeGame.java
+++ src/main/java/bridge/domain/BridgeGame.java
@@ -24,23 +24,23 @@ public class BridgeGame {
      */
     public HashMap<String, StringBuilder> move(String playerMoving, String bridgeJudgment) {
         if (playerMoving.equals("U") && bridgeJudgment.equals("O")) {
-            bridgeState.get(0).append(bridgeJudgment);
-            bridgeState.get(1).append(" ");
+            bridgeState.get(Command.UP.getCommand()).append(bridgeJudgment);
+            bridgeState.get(Command.DOWN.getCommand()).append(" ");
         }
 
         if (playerMoving.equals("U") && bridgeJudgment.equals("X")) {
-            bridgeState.get(0).append(bridgeJudgment);
-            bridgeState.get(1).append(" ");
+            bridgeState.get(Command.UP.getCommand()).append(bridgeJudgment);
+            bridgeState.get(Command.DOWN.getCommand()).append(" ");
         }
 
         if (playerMoving.equals("D") && bridgeJudgment.equals("O")) {
-            bridgeState.get(0).append(" ");
-            bridgeState.get(1).append(bridgeJudgment);
+            bridgeState.get(Command.UP.getCommand()).append(" ");
+            bridgeState.get(Command.DOWN.getCommand()).append(bridgeJudgment);
         }
 
         if (playerMoving.equals("D") && bridgeJudgment.equals("X")) {
-            bridgeState.get(0).append(" ");
-            bridgeState.get(1).append(bridgeJudgment);
+            bridgeState.get(Command.UP.getCommand()).append(" ");
+            bridgeState.get(Command.DOWN.getCommand()).append(bridgeJudgment);
         }
 
         return bridgeState;
@@ -49,10 +49,54 @@ public class BridgeGame {
     /**
      * 사용자가 게임을 다시 시도할 때 사용하는 메서드
      * <p>
-     * 재시작을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
+     * 재시작을 위해 필요한 메서드의 반환 타입(return type),
+     * 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    // TODO: 작성하기.
-    public void retry() {
-
+    // TODO: 꼭 hashmap이여야 하는가?
+    public String retry(String gameCommand) {
+        String result = "Q";
+        if (gameCommand.equals("R")) {
+            bridgeState.get(Command.UP.getCommand()).setLength(0);
+            bridgeState.get(Command.DOWN.getCommand()).setLength(0);
+            result = "R";
+        }
+        return result;
     }
+
+
+
+    public String judgment(String playerMoving, String designBridge) {
+        String resultBridge = "";
+
+        if (playerMoving.equals(designBridge)) {
+            resultBridge = "O";
+        }
+
+        if (!(playerMoving.equals(designBridge))) {
+            resultBridge = "X";
+        }
+
+        return resultBridge;
+    }
+
+
+    public HashMap<String, StringBuilder> bridgeConnection(int bridgeLength,
+            String bridgeJudgment, int i) {
+
+        if (i != bridgeLength - 1 && bridgeJudgment.equals("O")) {
+            bridgeState.get(Command.UP.getCommand()).append(" | ");
+            bridgeState.get(Command.DOWN.getCommand()).append(" | ");
+        }
+
+        if (i == bridgeLength - 1 || bridgeJudgment.equals("X")) {
+            bridgeState.get(Command.UP.getCommand()).append(" ]");
+            bridgeState.get(Command.DOWN.getCommand()).append(" ]");
+        }
+        return bridgeState;
+    }
+
+
+
+
+
 }
