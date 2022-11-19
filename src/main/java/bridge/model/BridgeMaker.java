0525a65e35715f4ec6b8e26d5adb24e034aab5bf
viyott-lover <jackk0616@gmail.com>
2022-11-19 20:47:17 +0900

    feat(BridgeMaker): 입력받은 사이즈만큼 다리를 작성
    makeBridge의 인자를 바꾸지 말라는 요구사항이 있었기 때문에 int로 받은 사이즈가 3 ~ 20 사이인지 검증하는 코드는 여기서 작성하고
    inputView에서 입력 받을 때 숫자가 제대로 입력되었는지 등의 예외는 따로 처리한다.

--- src/main/java/bridge/model/BridgeMaker.java
+++ src/main/java/bridge/model/BridgeMaker.java
@@ -1,5 +1,8 @@
 package bridge.model;
 
+import bridge.exception.Exception;
+
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -18,6 +21,25 @@ public class BridgeMaker {
      * @return 입력받은 길이에 해당하는 다리 모양. 위 칸이면 "U", 아래 칸이면 "D"로 표현해야 한다.
      */
     public List<String> makeBridge(int size) {
-        return null;
+        validateSize(size);
+        List<String> bridge = new ArrayList<>();
+        for (int i = 0; i < size; i++) {
+            int number = bridgeNumberGenerator.generate();
+            bridge.add(topOrBottom(number));
+        }
+        return bridge;
+    }
+
+    private void validateSize(int size) {
+        if(size < 3 ||size > 20) {
+            throw Exception.RANGE_EXCEPTION.getException();
+        }
+    }
+
+    private String topOrBottom(int num) {
+        if(num == Space.TOP.getNumber()) {
+            return Space.TOP.getDirection();
+        }
+        return Space.BOTTOM.getDirection();
     }
 }
