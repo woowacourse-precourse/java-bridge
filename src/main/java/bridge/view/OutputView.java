3c2c4fd79ccd8b2c7299ed16e24feebaa4b10290
sjk <80821744+ShinjaeKwon@users.noreply.github.com>
2022-11-20 01:51:44 +0900

    remove : 주석 제거

--- src/main/java/bridge/view/OutputView.java
+++ src/main/java/bridge/view/OutputView.java
@@ -2,28 +2,16 @@ package bridge.view;
 
 import java.util.List;
 
-/**
- * 사용자에게 게임 진행 상황과 결과를 출력하는 역할을 한다.
- */
 public class OutputView {
 
-	/**
-	 * 현재까지 이동한 다리의 상태를 정해진 형식에 맞춰 출력한다.
-	 * <p>
-	 * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
-	 */
-	public void printMap(List<String> bridge, List<String> currentBridgeState) {
+	public boolean printMap(List<String> bridge, List<String> currentBridgeState) {
 		String map = mapDraw(bridge, currentBridgeState);
-		System.out.println(map);
+		System.out.println(map + '\n');
+		return isAnswer(bridge, currentBridgeState, currentBridgeState.size() - 1);
 	}
 
-	/**
-	 * 게임의 최종 결과를 정해진 형식에 맞춰 출력한다.
-	 * <p>
-	 * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
-	 */
-	public void printFinalMapResult(List<String> bridge, List<String> currentBridgeState) {
-		printMap(bridge, currentBridgeState);
+	public boolean printFinalMapResult(List<String> bridge, List<String> currentBridgeState) {
+		return printMap(bridge, currentBridgeState);
 	}
 
 	private String mapDraw(List<String> bridge, List<String> currentBridgeState) {
@@ -44,6 +32,7 @@ public class OutputView {
 		sbBuilder(upSb, "]", downSb, "]");
 	}
 
+	//TODO : 리팩터링 - 메서드의 파라미터 개수는 최대 3개까지만 허용한다.
 	private static void upAndDownDraw(List<String> bridge, List<String> currentBridgeState, StringBuilder upSb,
 		StringBuilder downSb, int position) {
 		if (isUpper(currentBridgeState, position)) {
