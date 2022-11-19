a9ef4edae8a7ce6259f77d0506ab10030b19ccc9
bumwoo <gld55602@gmail.com>
2022-11-20 01:54:06 +0900

    test(BridgeGameTest): 플레이어 이동 테스트 코드 작성

---
+++ src/test/java/bridge/BridgeGameTest.java
@@ -0,0 +1,36 @@
+package bridge;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+
+public class BridgeGameTest {
+    private BridgeGame bridgeGame = new BridgeGame();
+    private UserBridge userBridge = new UserBridge();
+    private static Direction[] directions = {Direction.D, Direction.D, Direction.U, Direction.U};
+    private static Direction[] now = {Direction.D, Direction.U, Direction.U, Direction.D};
+
+    @Test
+    void 이동_테스트() {
+        Direction[] directions = {Direction.D, Direction.D, Direction.U, Direction.U};
+        Direction[] now = {Direction.D, Direction.U, Direction.U, Direction.D};
+        UserBridge result = new UserBridge();
+
+        for (int i = 0; i < 4; i++) {
+            moveResult(result, i);
+            bridgeGame.move(userBridge, directions[i], now[i]);
+        }
+
+        assertThat(userBridge).isEqualTo(result);
+    }
+
+    private static void moveResult(UserBridge result, int i) {
+        if (now[i].equals(Direction.D)) {
+            result.moveDownBridge(directions[i]);
+        }
+        if (now[i].equals(Direction.U)) {
+            result.moveUpBridge(directions[i]);
+        }
+        result.addBlank(now[i]);
+    }
+}
