43074399543a6383d1eeb9d0a1b216c0b130d26f
ddubidubap <park62000@gmail.com>
2022-11-20 00:50:05 +0900

    feat: 전체 게임 시작 또는 에러 메시지 출력

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -1,8 +1,14 @@
 package bridge;
 
-public class Application {
+import bridge.controller.GameController;
 
+public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        try {
+            GameController controller = new GameController();
+            controller.start();
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+        }
     }
 }
