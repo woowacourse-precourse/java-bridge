d460f27ab09ad0ae5bf74a433d03a6e7a1fb34f9
JeonSangYong <tkddyd420@naver.com>
2022-11-19 21:59:30 +0900

    test : 다리 이동 실패 테스트

--- src/test/java/bridge/BridgeGameTest.java
+++ src/test/java/bridge/BridgeGameTest.java
@@ -7,7 +7,7 @@ import java.util.List;
 import static org.assertj.core.api.Assertions.*;
 
 class BridgeGameTest {
-    
+
     @Test
     void 다리_이동_성공() {
         //given
@@ -16,5 +16,15 @@ class BridgeGameTest {
         bridgeGame.move("U");
         //then
         assertThat(bridgeGame.isGameEnd()).isFalse();
+    }
+    
+    @Test
+    void 다리_이동_실패() {
+        //given
+        BridgeGame bridgeGame = new BridgeGame(List.of("U", "U", "U"));
+        //when
+        bridgeGame.move("D");
+        //then
+        assertThat(bridgeGame.isGameEnd()).isTrue();
     }
 }
