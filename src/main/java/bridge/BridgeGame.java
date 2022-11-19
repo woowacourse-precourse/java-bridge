843b29d6e76cebec948a96f811c996be768e7b68
E1psycongr00 <39326175+E1psycongr00@users.noreply.github.com>
2022-11-19 13:45:33 +0900

    feat(bridgeGame): 재시도 요청시 응답 기능 구현

--- src/main/java/bridge/BridgeGame.java
+++ src/main/java/bridge/BridgeGame.java
@@ -2,6 +2,8 @@ package bridge;
 
 import bridge.domain.BridgeStatus;
 import bridge.domain.MovingCommand;
+import bridge.domain.RetryCommand;
+import com.sun.net.httpserver.Authenticator.Retry;
 import java.util.List;
 
 public class BridgeGame {
@@ -28,11 +30,8 @@ public class BridgeGame {
         return bridgeStatus;
     }
 
-    /**
-     * 사용자가 게임을 다시 시도할 때 사용하는 메서드
-     * <p>
-     * 재시작을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
-     */
-    public void retry() {
+    public boolean retry(String input) {
+        RetryCommand retryCommand = RetryCommand.nameOf(input);
+        return retryCommand.equals(RetryCommand.RETRY);
     }
 }
