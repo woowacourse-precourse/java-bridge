8b1acdcc295a3d52b6340a341d7f77e453647cb2
Sanmgmin <sangmins930@gmail.com>
2022-11-19 14:54:36 +0900

    feat(User): create User class, constructor, and get instance function

---
+++ src/main/java/bridge/User.java
@@ -0,0 +1,19 @@
+package bridge;
+
+public class User {
+    private String pathOfUpperBridge;
+    private String pathOfLowerBridge;
+
+    public User() {
+        this.pathOfUpperBridge = "";
+        this.pathOfLowerBridge = "";
+    }
+
+    public String getPathOfUpperBridge() {
+        return pathOfUpperBridge;
+    }
+
+    public String getPathOfLowerBridge() {
+        return pathOfLowerBridge;
+    }
+}
