5b27e5615eb5385a23c344437d711d35268f4f06
Choi-JY <Choi-JY1107@github.com>
2022-11-20 01:31:54 +0900

    feat: 횡단 기록 DTO 반환 메소드 구현

--- src/main/java/bridge/dto/BridgeCrossingDTO.java
+++ src/main/java/bridge/dto/BridgeCrossingDTO.java
@@ -1,2 +1,27 @@
-package bridge.dto;public class BridgeCrossingDTO {
+package bridge.dto;
+
+import java.util.List;
+
+public class BridgeCrossingDTO {
+    private final List<List<String>> footPrint;
+    private final int tryCount;
+    private final boolean crossComplete;
+
+    public BridgeCrossingDTO(List<List<String>> footPrint, int tryCount, boolean crossComplete) {
+        this.footPrint = footPrint;
+        this.crossComplete = crossComplete;
+        this.tryCount = tryCount;
+    }
+
+    public List<List<String>> getFootPrint() {
+        return footPrint;
+    }
+
+    public int getTryCount() {
+        return tryCount;
+    }
+
+    public boolean isCrossComplete() {
+        return crossComplete;
+    }
 }
