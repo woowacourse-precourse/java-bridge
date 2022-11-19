80e0fd2d5630f0a01e3b10a0d733a936590f1dbd
marinesnow34 <m4rinesnow34@gmail.com>
2022-11-19 16:42:58 +0900

    feat: 사용자 재시작 입력 메시지 추가

--- src/main/java/bridge/view/InputView.java
+++ src/main/java/bridge/view/InputView.java
@@ -9,6 +9,8 @@ public class InputView {
     private static final String ERROR_INPUT_INT = "[ERROR] 숫자만 입력 가능합니다.";
     private static final String MESSAGE_INPUT_BRIDGE_SIZE = "다리의 길이를 입력해주세요.";
     private static final String MESSAGE_INPUT_MOVING = "이동할 칸을 선택해주세요. (위: U, 아래: D)";
+    private static final String MESSAGE_INPUT_RESTART = "게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)";
+
 
     /**
      * 다리의 길이를 입력받는다.
@@ -35,6 +37,7 @@ public class InputView {
      * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
      */
     public String readGameCommand() {
+        System.out.println(MESSAGE_INPUT_RESTART);
         return readLine();
     }
 }
