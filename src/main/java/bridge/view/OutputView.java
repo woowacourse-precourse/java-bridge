4be3c14657ab0ac15d7426620a9aa5c5ef2e13b0
splitube <voicesplit@daum.net>
2022-11-19 22:45:08 +0900

    refactor(OutputView): 출력할 다리상태 만드는 기능 제거
    Mapmaker 객체로 분리

--- src/main/java/bridge/view/OutputView.java
+++ src/main/java/bridge/view/OutputView.java
@@ -1,71 +1,15 @@
 package bridge.view;
 
 import bridge.game.BridgeGame;
+import bridge.game.MapMaker;
 import bridge.game.Progress;
-import bridge.game.Result;
-import bridge.generator.AreaStatus;
-
-import java.util.ArrayList;
 import java.util.List;
 
 public class OutputView {
-    private final String START = "[";
-    private final String DIVIDE = "|";
-    private final String END = "]";
-
-    public void printMap(BridgeGame bridgeGame) {
-        StringBuilder save = new StringBuilder();
-        List<Progress> currentResult = bridgeGame.showCurrentResult();
-        saveContent(makeResults(currentResult, AreaStatus.UP), save);
-        saveContent(makeResults(currentResult, AreaStatus.DOWN), save);
-        System.out.println(save);
-    }
-
-    public void saveContent(List<Result> results, StringBuilder map) {
-        map.append(START);
-        for (Result result : results) {
-            map.append(result.toString() + DIVIDE);
-        }
-        int lastIndex = map.length() - 1;
-        map.deleteCharAt(lastIndex);
-        map.append(END + "\n");
-    }
-
-    public List<Result> makeResults(List<Progress> progresses, AreaStatus area) {
-        List<Result> result = new ArrayList<>();
-        for (Progress progress : progresses) {
-            if (area.symbol.equals(AreaStatus.UP.symbol)) {
-                saveUpperMapResult(progress, result);
-            }
-            if (area.symbol.equals(AreaStatus.DOWN.symbol)) {
-                saveLowerMapResult(progress, result);
-            }
-        }
-        return result;
-    }
-
-    public void saveUpperMapResult(Progress progress, List<Result> results) {
-        if (progress.getDestination().equals(AreaStatus.DOWN.symbol)) {
-            results.add(Result.NONE);
-            return;
-        }
-        if (progress.isSuccess()) {
-            results.add(Result.SUCCESS);
-            return;
-        }
-        results.add(Result.FAIL);
-    }
-
-    public void saveLowerMapResult(Progress progress, List<Result> results) {
-        if (progress.getDestination().equals(AreaStatus.UP.symbol)) {
-            results.add(Result.NONE);
-            return;
-        }
-        if (progress.isSuccess()) {
-            results.add(Result.SUCCESS);
-            return;
-        }
-        results.add(Result.FAIL);
+    public void printMap(List<Progress> progresses) {
+        MapMaker mapMaker = new MapMaker();
+        String map = mapMaker.makeMap(progresses);
+        System.out.println(map);
     }
 
     public void start() {
@@ -86,20 +30,20 @@ public class OutputView {
 
     public void printResult(BridgeGame bridgeGame) {
         System.out.println(Message.FINAL_GAME_RESULT);
-        printMap(bridgeGame);
+        printMap(bridgeGame.showCurrentResult());
         printSuccess(bridgeGame);
         printTotalTry(bridgeGame);
     }
 
-    public void printSuccess(BridgeGame bridgeGame) {
+    private void printSuccess(BridgeGame bridgeGame) {
         System.out.println(Message.IS_GAME_SUCCESS + bridgeGame.showSuccess());
     }
 
-    public void printTotalTry(BridgeGame bridgeGame){
+    private void printTotalTry(BridgeGame bridgeGame) {
         System.out.println(Message.TOTAL_TRY.toString() + bridgeGame.showTotalTry());
     }
 
-    public void printError(Exception e){
+    public static void printError(Exception e) {
         System.out.println(e.getMessage());
     }
 }
