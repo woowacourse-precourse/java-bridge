c79729852275767bcd304f5e9a286e6b04c45b86
Rovna99 <80767387+Rovna99@users.noreply.github.com>
2022-11-19 20:56:53 +0900

    refactor: InputView 상수화

--- src/main/java/bridge/view/InputView.java
+++ src/main/java/bridge/view/InputView.java
@@ -3,6 +3,10 @@ package bridge.view;
 import camp.nextstep.edu.missionutils.Console;
 
 public class InputView {
+	private static final String INPUT_MOVE_COMMAND_MESSAGE = "이동할 칸을 선택해주세요. (위: U, 아래: D)";
+	private static final String INPUT_RESTART_COMMAND_MESSAGE = "게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)";
+	private static final String INPUT_SIZE_ERROR_MESSAGE = "[ERROR] 다리의 길이는 숫자여야 합니다.";
+	private static final String INPUT_COMMAND_ERROR_MESSAGE = "[ERROR] 커맨드는 대문자 알파벳으로 입력해야 합니다.";
 
 	private InputView(){}
 
@@ -21,7 +25,7 @@ public class InputView {
 
 	private static void validSize(String input) {
 		if (!checkDigit(input)) {
-			throw new IllegalArgumentException("[ERROR] 다리의 길이는 숫자여야 합니다.");
+			throw new IllegalArgumentException(INPUT_SIZE_ERROR_MESSAGE);
 		}
 	}
 
@@ -34,14 +38,14 @@ public class InputView {
 	}
 
 	public static String readMoving() {
-		System.out.println("이동할 칸을 선택해주세요. (위: U, 아래: D)");
+		System.out.println(INPUT_MOVE_COMMAND_MESSAGE);
 		String input = Console.readLine();
 		validCommand(input);
 		return input;
 	}
 
 	public static String readGameCommand() {
-		System.out.println("게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)");
+		System.out.println(INPUT_RESTART_COMMAND_MESSAGE);
 		String input = Console.readLine();
 		validCommand(input);
 		return input;
@@ -55,7 +59,7 @@ public class InputView {
 
 	private static void checkUpperCase(char input) {
 		if (!Character.isUpperCase(input)) {
-			throw new IllegalArgumentException("[ERROR] 커맨드는 대문자 알파벳으로 입력해야 합니다.");
+			throw new IllegalArgumentException(INPUT_COMMAND_ERROR_MESSAGE);
 		}
 	}
 }
