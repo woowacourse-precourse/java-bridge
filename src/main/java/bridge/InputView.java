6b5ff1ecb93f74ea0cb0cbf7d5f8f3aa5e7e2c15
sangho-jeon <jeonsangho1587@gmail.com>
2022-11-19 22:46:57 +0900

    feat      : inputView + validator added. using State constants

--- src/main/java/bridge/InputView.java
+++ src/main/java/bridge/InputView.java
@@ -1,5 +1,9 @@
 package bridge;
 
+import bridge.Util.STATE_CONSTANTS;
+import bridge.Util.Validator;
+import camp.nextstep.edu.missionutils.Console;
+
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
  */
@@ -8,21 +12,21 @@ public class InputView {
     /**
      * 다리의 길이를 입력받는다.
      */
-    public int readBridgeSize() {
-        return 0;
+    public static int readBridgeSize() {
+        return Validator.validateBridgeInput(Console.readLine());
     }
 
     /**
      * 사용자가 이동할 칸을 입력받는다.
      */
-    public String readMoving() {
-        return null;
+    public static String readMoving() {
+        return Validator.validateCommand(Console.readLine(), STATE_CONSTANTS.MOVE_STATE);
     }
 
     /**
      * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
      */
-    public String readGameCommand() {
-        return null;
+    public static String readGameCommand() {
+        return Validator.validateCommand(Console.readLine(), STATE_CONSTANTS.RETRY_STATE);
     }
 }
