9aa41a2c70540314048d81100b43fc9558523867
whdcks420 <whdcks420@gmail.com>
2022-11-19 23:43:10 +0900

    feat: 브릿지 어플리케이션 기능

--- src/main/java/bridge/domain/BridgeApplication.java
+++ src/main/java/bridge/domain/BridgeApplication.java
@@ -1,2 +1,42 @@
-package bridge.domain;public class BridgeApplication {
+package bridge.domain;
+
+import bridge.ui.InputView;
+import bridge.ui.OutputView;
+
+public class BridgeApplication {
+    private final InputView inputView;
+    private final OutputView outputView;
+
+    public BridgeApplication(InputView inputView, OutputView outputView) {
+        this.inputView = inputView;
+        this.outputView = outputView;
+    }
+
+    public void run() {
+        outputView.printGameStartMessage();
+
+        Bridge bridge = new Bridge(inputView.readBridgeSize());
+
+        gameProcess(new BridgeGame(bridge));
+    }
+
+    private void gameProcess(BridgeGame bridgeGame) {
+        do {
+            BridgeMap bridgeMap = bridgeGame.move(inputView.readMoving());
+            outputView.printMap(bridgeMap.getMap());
+        } while (checkWinning(bridgeGame) && (checkGameStatus(bridgeGame) || checkRestart(bridgeGame)));
+        outputView.printResult(bridgeGame.getResult());
+    }
+
+    private boolean checkGameStatus(BridgeGame bridgeGame) {
+        return bridgeGame.checkStatus();
+    }
+
+    private boolean checkRestart(BridgeGame bridgeGame) {
+        return bridgeGame.retry(inputView.readGameCommand());
+    }
+
+    private boolean checkWinning(BridgeGame bridgeGame) {
+        return !bridgeGame.isWinningBrideGame();
+    }
 }
