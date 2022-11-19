7d76a0b8e694d606406c6d2b7007ea108e3a5466
yeeun <dpdms9621@naver.com>
2022-11-19 17:12:58 +0900

    feat(OutputView): 유저 List의 U와 D를 비교하여 두개의 List에 저장하는 기능

--- src/main/java/bridge/view/OutputView.java
+++ src/main/java/bridge/view/OutputView.java
@@ -1,11 +1,31 @@
 package bridge.view;
 
 
+import java.util.List;
+
 /**
  * 사용자에게 게임 진행 상황과 결과를 출력하는 역할을 한다.
  */
 public class OutputView {
 
+    List<String> upList;
+    List<String> downList;
+
+
+    // 유저 List의 U와 D를 비교하여 두개의 List에 저장하는 기능
+    public void addUpAndDownList(List<String> makeBridgeList, List<String> userSelectList) {
+        for (int i = 0 ; i < userSelectList.size(); i++) {
+            String ox = compareOX(makeBridgeList.get(i),userSelectList.get(i));
+            if (userSelectList.get(i).equals("U")) {
+                upList.add(ox);
+                downList.add(" ");
+            }
+            if (userSelectList.get(i).equals("D")) {
+                upList.add(" ");
+                downList.add(ox);
+            }
+        }
+    }
 
     // 유저 List와 정답 List를 비교하여 OX를 저장하는 기능
     public String compareOX(String makeBridge, String userSelect) {
