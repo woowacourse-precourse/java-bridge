479d47c4330f8799cdda4fdaab2350acb01f3c05
sosow0212 <sosow0212@naver.com>
2022-11-19 18:04:32 +0900

    feat : (BridgeGameException) 다리 이동 입력 예외처리 구현

--- src/main/java/bridge/exception/BridgeGameException.java
+++ src/main/java/bridge/exception/BridgeGameException.java
@@ -4,22 +4,31 @@ public class BridgeGameException {
     private static final String ONLY_CONTAINS_NUMBER_REGEX = "^[0-9]*$";
     private static final int MIN_BRIDGE_SIZE = 3;
     private static final int MAX_BRIDGE_SIZE = 20;
+    private static final String UP_DIRECTION = "U";
+    private static final String DOWN_DIRECTION = "D";
+    private static final String ERROR = "[ERROR] ";
 
     public void validateBridgeSize(String input) {
         validateBridgeSizeOnlyContainsNumber(input);
         validateBridgeSizePermittedLength(input);
     }
 
+    public void validateDirection(String input) {
+        if(!(input.equals(UP_DIRECTION) || input.equals(DOWN_DIRECTION))) {
+            throw new IllegalArgumentException(ERROR + "이동할 칸은 U 혹은 D만 가능합니다.");
+        }
+    }
+
     public void validateBridgeSizeOnlyContainsNumber(String input) {
         if(!input.matches(ONLY_CONTAINS_NUMBER_REGEX)) {
-            throw new IllegalArgumentException("[ERROR] 다리의 길이는 숫자만 입력이 가능합니다.");
+            throw new IllegalArgumentException(ERROR + "다리의 길이는 숫자만 입력이 가능합니다.");
         }
     }
 
     public void validateBridgeSizePermittedLength(String input) {
         int bridgeSize = Integer.parseInt(input);
         if(!(bridgeSize >= MIN_BRIDGE_SIZE && bridgeSize <= MAX_BRIDGE_SIZE)) {
-            throw new IllegalArgumentException("[ERROR] 다리의 길이는 3~20 사이의 수입니다.");
+            throw new IllegalArgumentException(ERROR + "다리의 길이는 3~20 사이의 수입니다.");
         }
     }
 }
