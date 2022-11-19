7e40cd01bcb69cd6aaf515b28fbba59d98308899
kimtaesoo99 <107785279+kimtaesoo99@users.noreply.github.com>
2022-11-19 23:53:52 +0900

    feat: (BridgeGameInputException) 게임 재시작 여부 예외처리 추가

--- src/main/java/bridge/exception/BridgeGameInputException.java
+++ src/main/java/bridge/exception/BridgeGameInputException.java
@@ -13,5 +13,12 @@ public class BridgeGameInputException {
             throw new IllegalArgumentException("[ERROR] 이동할 칸은 U또는 D만 입력가능합니다.");
         }
     }
+
+    public void validateGameCommand(String gameCommand) {
+        if (!(gameCommand.equals("R") || gameCommand.equals("Q"))) {
+            throw new IllegalArgumentException("[ERROR] 게임 재시작여부는 R또는 Q만 입력가능합니다.");
+        }
+    }
+
 }
 
