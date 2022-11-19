90ab32a0186d2afd7b3b6a52966df54ad0b7e481
abbd2 <ajkety00@gmail.com>
2022-11-19 20:28:14 +0900

    Feat(Bridge): Bridge 클래스 재생성

--- src/main/java/bridge/Bridge.java
+++ src/main/java/bridge/Bridge.java
@@ -6,11 +6,11 @@ public class Bridge {
 
     private List<String> bridge;
 
-    public List<String> getBridge() {
-        return bridge;
+    public Bridge(List<String> bridge){
+        this.bridge = bridge;
     }
 
-    public void setBridge(List<String> bridge) {
-        this.bridge = bridge;
+    public List<String> getBridge() {
+        return bridge;
     }
 }
