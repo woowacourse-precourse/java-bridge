6456c07b54ec75f809457aabc49d5c30be5a3c68
alstjr706 <alstjr706@naver.com>
2022-11-19 09:52:30 +0900

    feat : (다리 건너기) 커맨드 enum 클래스 구현

---
+++ src/main/java/bridge/constants/Command.java
@@ -0,0 +1,25 @@
+package bridge.constants;
+
+public enum Command {
+    UP(0, "U"),
+    DOWN(1, "D"),
+    RE_START(2, "R"),
+    END(3, "Q");
+
+    private final int sequence;
+    private final String command;
+
+    Command(int sequence, String command) {
+        this.sequence = sequence;
+        this.command = command;
+    }
+
+    public int getSequence() {
+        return sequence;
+    }
+
+    public String getCommand() {
+        return command;
+    }
+
+}
