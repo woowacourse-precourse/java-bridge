fd4329d5593c18a9de22f30b9922ed8a5dc4699f
양원준 <97458548+ywj9811@users.noreply.github.com>
2022-11-20 00:38:57 +0900

    refactor: 주석 제거

--- src/main/java/bridge/view/InputView.java
+++ src/main/java/bridge/view/InputView.java
@@ -1,21 +1,15 @@
 package bridge.view;
 
-import bridge.ExceptionString;
+import bridge.view.enums.ExceptionString;
 import bridge.exception.InputException;
 
 import java.util.NoSuchElementException;
 
 import static camp.nextstep.edu.missionutils.Console.*;
 
-/**
- * 사용자로부터 입력을 받는 역할을 한다.
- */
 public class InputView {
     private static InputException inputException = new InputException();
 
-    /**
-     * 다리의 길이를 입력받는다.
-     */
     public int readBridgeSize() {
         try {
             String inputSize = readLine();
@@ -26,9 +20,6 @@ public class InputView {
         }
     }
 
-    /**
-     * 사용자가 이동할 칸을 입력받는다.
-     */
     public String readMoving() {
         try {
             String move = readLine();
@@ -39,9 +30,6 @@ public class InputView {
         }
     }
 
-    /**
-     * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
-     */
     public String readGameCommand() {
         try {
             String retry = readLine();
