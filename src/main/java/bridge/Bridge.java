f94518786d9493a2204b91c14190e4e44572bf80
impati <impati0716@icloud.com>
2022-11-19 14:59:32 +0900

    fix(Brige): bridge 접근 제어자 protected로 변경

--- src/main/java/bridge/Bridge.java
+++ src/main/java/bridge/Bridge.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Bridge {
-    private List<String> bridge;
+    protected List<String> bridge;
     public Bridge(List<String> bridge) {
         this.bridge = bridge;
     }
