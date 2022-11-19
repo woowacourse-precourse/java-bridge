86172eef95f06ca8a04f98a77945d11061768a72
chanminmaxscaler <jcm04@naver.com>
2022-11-19 23:18:29 +0900

    feat: Pedestrian 클래스를 구현한다
    Pedestrian 클래스는 MovingRecord 객체를 멤버변수로 갖고 해당 객체를 사용하는 클래스이다.
    게임에 직접적으로 참여하는 주체이며, 게임과 MovingRecord를 떨어뜨리기 위해 사용한다.

---
+++ src/main/java/bridge/domain/pedestrian/Pedestrian.java
@@ -0,0 +1,29 @@
+package bridge.domain.pedestrian;
+
+import bridge.domain.direction.Direction;
+import bridge.domain.movingrecord.MovingRecord;
+import bridge.domain.referee.Judgement;
+
+import java.util.Map;
+import java.util.stream.Stream;
+
+public class Pedestrian {
+
+    private final MovingRecord movingRecord;
+
+    public Pedestrian() {
+        this.movingRecord = new MovingRecord();
+    }
+
+    public void move(Judgement judgement, Direction direction) {
+        movingRecord.write(judgement, direction);
+    }
+
+    public Stream<Map.Entry<Judgement, Direction>> readRecord() {
+        return movingRecord.read();
+    }
+
+    public int findLocation() {
+        return movingRecord.size();
+    }
+}
