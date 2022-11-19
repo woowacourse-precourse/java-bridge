600685e78e3694c23eec72ff992678d52803a864
Heebeom Yang <takeny1998@gmail.com>
2022-11-19 17:50:33 +0900

    test: BridgeMove domain test

---
+++ src/test/java/bridge/BridgeMoveTest.java
@@ -0,0 +1,23 @@
+package bridge;
+
+import bridge.domain.BridgeMove;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+public class BridgeMoveTest {
+    @DisplayName("이동 방향이 U거나 D가 아니면 예외가 발생한다. (다른 문자)")
+    @Test
+    void createBridgeMoveByOutOfRange() {
+        assertThatThrownBy(() -> new BridgeMove("F"))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+    @DisplayName("이동 방향이 U거나 D가 아니면 예외가 발생한다. (숫자)")
+    @Test
+    void createBridgeMoveByNumber() {
+        assertThatThrownBy(() -> new BridgeMove("12"))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+}
