cc358ce793cf159f46cee9f06ab2b6b1435a5e21
wlsh44 <k87913j@gmail.com>
2022-11-19 10:48:08 +0900

    feat: 이동 입력 기능 추가

--- src/main/java/bridge/view/InputView.java
+++ src/main/java/bridge/view/InputView.java
@@ -7,10 +7,14 @@ import camp.nextstep.edu.missionutils.Console;
  */
 public class InputView {
 
+    public static final String READ_BRIDGE_SIZE = "다리 길이를 입력해주세요.";
+    public static final String READ_MOVING = "이동할 칸을 선택해주세요. (위: U, 아래: D)";
+
     /**
      * 다리의 길이를 입력받는다.
      */
     public int readBridgeSize() {
+        System.out.println(READ_BRIDGE_SIZE);
         return toInteger(Console.readLine());
     }
 
@@ -26,7 +30,8 @@ public class InputView {
      * 사용자가 이동할 칸을 입력받는다.
      */
     public String readMoving() {
-        return null;
+        System.out.println(READ_MOVING);
+        return Console.readLine();
     }
 
     /**
