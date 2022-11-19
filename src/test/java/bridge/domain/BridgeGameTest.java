71b4267c37e869b4fc3680b8bb0e4916299f2523
Jzakka <mouse4786@gmail.com>
2022-11-19 10:53:09 +0900

    test: 테스트 코드 변경

--- src/test/java/bridge/domain/BridgeGameTest.java
+++ src/test/java/bridge/domain/BridgeGameTest.java
@@ -1,6 +1,7 @@
 package bridge.domain;
 
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
@@ -9,18 +10,20 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class BridgeGameTest {
 
-    private BridgeGame bridgeGame = new BridgeGame(new Bridge(List.of("U", "D", "D")), new User());
+    private BridgeGame bridgeGame = new BridgeGame();
+    private Bridge bridge = new Bridge(List.of("U", "D", "U"));
+    private User user = new User();
 
     @Test
     void move() {
-        Assertions.assertThat(bridgeGame.move(0, "U")).isTrue();
-        Assertions.assertThat(bridgeGame.move(1, "D")).isTrue();
-        Assertions.assertThat(bridgeGame.move(2, "U")).isFalse();
+        Assertions.assertThat(bridgeGame.move(0, "U", bridge, user)).isTrue();
+        Assertions.assertThat(bridgeGame.move(1, "D", bridge, user)).isTrue();
+        Assertions.assertThat(bridgeGame.move(2, "U", bridge, user)).isTrue();
     }
 
     @Test
     void retry(){
-        Assertions.assertThat(bridgeGame.retry("R")).isTrue();
-        Assertions.assertThat(bridgeGame.retry("Q")).isFalse();
+        Assertions.assertThat(bridgeGame.retry("R", user)).isTrue();
+        Assertions.assertThat(bridgeGame.retry("Q",user)).isFalse();
     }
 }
