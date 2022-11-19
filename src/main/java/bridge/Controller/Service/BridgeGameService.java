56a929d7efd3976ce38e8798c0f08587be85e52d
Park shinyeong <syngh503@gmail.com>
2022-11-19 16:10:30 +0900

    Fix(BridgeGameService): gameResultGenerator 필드 삭제
    BridgeGame에서 gameResult를 받아올 수 있어, gameResultGenerator을 삭제하였습니다.

--- src/main/java/bridge/Controller/Service/BridgeGameService.java
+++ src/main/java/bridge/Controller/Service/BridgeGameService.java
@@ -1,7 +1,6 @@
 package bridge.Controller.Service;
 
 import Utils.ValidateUtils;
-import bridge.Domain.Bridge;
 import bridge.Domain.BridgeGame;
 import bridge.Domain.GameResult;
 import bridge.Domain.GameResultGenerator;
@@ -9,27 +8,17 @@ import bridge.View.InputView;
 import bridge.View.OutputView;
 
 public class BridgeGameService {
-    private Bridge bridge;
-    private GameResultGenerator gameResultGenerator;
     private BridgeGame bridgeGame;
 
-    public BridgeGameService(Bridge bridge, GameResultGenerator gameResultGenerator) {
-        this.bridge = bridge;
-        this.gameResultGenerator = gameResultGenerator;
-        this.bridgeGame = new BridgeGame(bridge);
+    public BridgeGameService(BridgeGame bridgeGame) {
+        this.bridgeGame = bridgeGame;
     }
 
     public void onePhaseBridgeGame() throws IllegalArgumentException {
         String positionInput = InputView.readMoving();
         ValidateUtils.isValidatePosition(positionInput);
-        GameResult gameResult = bridgeGame.move(positionInput);
-        String bridgeStatus = getBridgeStatus(gameResult);
+        bridgeGame.move(positionInput);
+        String bridgeStatus = bridgeGame.getBridgeStatusAfterMove();
         OutputView.printMap(bridgeStatus);
-    }
-
-    public String getBridgeStatus(GameResult gameResult) {
-        gameResultGenerator.setUpperBridgeResult(gameResult);
-        gameResultGenerator.setLowerBridgeResult(gameResult);
-        return gameResultGenerator.getGameResult();
     }
 }
