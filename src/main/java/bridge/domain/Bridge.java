99a2cf84e531c815d49a8abc0e6296941e2b8305
0chil <0@chll.it>
2022-11-19 14:20:34 +0900

    feat(Bridge): 특정 데크 특정 방향이 이동가능한지 알 수 있다

---
+++ src/main/java/bridge/domain/Bridge.java
@@ -0,0 +1,21 @@
+package bridge.domain;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import bridge.constant.Direction;
+
+public class Bridge {
+
+    private List<Deck> decks;
+
+    public Bridge(List<String> capitalLetters) {
+        this.decks = capitalLetters.stream()
+                .map(Deck::new)
+                .collect(Collectors.toList());
+    }
+
+    public boolean isMovable(int position, Direction direction) {
+        return decks.get(position).isMovable(direction.capitalLetter());
+    }
+}
---
+++ src/test/java/bridge/domain/BridgeTest.java
@@ -0,0 +1,29 @@
+package bridge.domain;
+
+import static bridge.constant.Direction.LOWER;
+import static bridge.constant.Direction.UPPER;
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.junit.jupiter.api.Test;
+
+import bridge.constant.Direction;
+
+public class BridgeTest {
+
+    @Test
+    void 특정_데크_특정_방향이_이동가능한지_알_수_있다() {
+        List<Direction> bridgeDirections = List.of(UPPER, LOWER, UPPER, LOWER, UPPER);
+        Bridge bridge = new Bridge(getCapitalLetters(bridgeDirections));
+
+        assertThat(bridge.isMovable(0, UPPER)).isTrue();
+    }
+
+    private List<String> getCapitalLetters(List<Direction> bridgeDirections) {
+        return bridgeDirections.stream()
+                .map(Direction::capitalLetter)
+                .collect(Collectors.toList());
+    }
+}
