e3c3b1ec56e655e48de0281023e863e686ce2771
Cyma-s <ssol0319@naver.com>
2022-11-20 01:55:33 +0900

    feat(BridgeState) : separate of state string and integer value

--- src/main/java/bridge/domain/BridgeState.java
+++ src/main/java/bridge/domain/BridgeState.java
@@ -1,15 +1,27 @@
 package bridge.domain;
 
-public enum BridgeState {
-    DOWN(0),
-    UP(1);
-    private final int state;
+import java.util.Arrays;
 
-    BridgeState(int state) {
+public enum BridgeState {
+    DOWN("D", 0),
+    UP("U", 1);
+    private final String state;
+    private final int stateValue;
+
+    BridgeState(String state, int stateValue) {
         this.state = state;
+        this.stateValue = stateValue;
     }
 
-    public int getState() {
+    public static BridgeState makeBridgeState(int stateValue) {
+        BridgeState bridgeState = Arrays.stream(values())
+                .filter(value -> value.stateValue == stateValue)
+                .findAny()
+                .orElse(null);
+        return bridgeState;
+    }
+
+    public String getState() {
         return state;
     }
 }
