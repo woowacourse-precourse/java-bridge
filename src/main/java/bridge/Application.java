d558362517938fe92ba151b12fccea43c89a2170
Kimhyeyun <cqqwer@gmail.com>
2022-11-20 01:37:51 +0900

    Feat: Application 구현 완

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -1,8 +1,17 @@
 package bridge;
 
+import bridge.controller.GameController;
+
 public class Application {
 
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        try {
+            final GameController gameController = new GameController();
+            gameController.play();
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+
+        }
     }
 }
