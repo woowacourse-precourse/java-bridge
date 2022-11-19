e9f286b542d1932988c399881386851fbaffbfe6
49ehyeon42 <49ehyeon42@gmail.com>
2022-11-19 16:33:19 +0900

    feat: 이동할 칸을 읽고 결과를 반환하는 코드 추가

--- src/main/java/bridge/InputView.java
+++ src/main/java/bridge/InputView.java
@@ -32,7 +32,13 @@ public class InputView {
      * 사용자가 이동할 칸을 입력받는다.
      */
     public String readMoving() {
-        return null;
+        System.out.println("이동할 칸을 선택해주세요. (위: U, 아래: D)");
+
+        String moving = Console.readLine();
+
+        validateReadMoving(moving);
+
+        return moving;
     }
 
     private void validateReadMoving(String moving) {
