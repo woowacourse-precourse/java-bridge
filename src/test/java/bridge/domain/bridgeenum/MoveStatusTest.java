4189661c09a6aee3cab50c8ce799e680dfa40b10
somsom13 <somin1113@naver.com>
2022-11-19 22:42:35 +0900

    test(MoveStatusTest): 이동 성공 여부에 따른 표시 결과 테스트
    MoveStatus enum 에서 이동 성공 여부에 따라 다리에 표시할 값 반환 됨을 테스트 한다
    
    1. 성공 여부가 true 일 때, O 가 반환된다.
    2. 성공 여부가 false 일 때, X 가 반환된다.
    3. 성공 여부가 null 일 때 공백이 반환된다.

---
+++ src/test/java/bridge/domain/bridgeenum/MoveStatusTest.java
@@ -0,0 +1,17 @@
+package bridge.domain.bridgeenum;
+
+import static org.assertj.core.api.Assertions.*;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+class MoveStatusTest {
+
+    @DisplayName("이동 성공 여부에 따른 표기 (O/X/공백) 이 정상적으로 반환되는지 확인한다")
+    @ParameterizedTest
+    @CsvSource(value = {"true:O", "false:X", "null: "}, delimiter = ':', nullValues = "null", ignoreLeadingAndTrailingWhitespace = false)
+    void findMarkByValue(Boolean isSuccess, String mark) {
+        assertThat(MoveStatus.findMarkByValue(isSuccess)).isEqualTo(mark);
+    }
+}
