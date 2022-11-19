4c8ea91f0bb88c837d547f745840512b8a60b645
tenta <eunoia3802@gmail.com>
2022-11-19 18:42:43 +0900

    feat: 게임 실패 시 재시작 기능 구현

--- src/main/java/bridge/BridgeController.java
+++ src/main/java/bridge/BridgeController.java
@@ -23,17 +23,24 @@ public class BridgeController {
 
     public void moveController(List<String> bridge, int blockCount) {
         BridgeGame bridgeGame = new BridgeGame();
+        System.out.println(bridge);
         for (int tryCount = 0; tryCount < blockCount; tryCount++) {
             String sideToMove = new InputView().readMoving();
             moveBridgeResult = bridgeGame.move(bridge, tryCount, sideToMove);
             new OutputView().printMap(moveBridgeResult);
             int resultSize = moveBridgeResult.size();
-            if (moveBridgeResult.get(resultSize - 1).contains("X") || moveBridgeResult.get(resultSize - 2).contains("X")) {
+            if (moveBridgeResult.get(resultSize - 1).contains("X")
+                    || moveBridgeResult.get(resultSize - 2).contains("X")) {
                 String gameCommand = new InputView().readGameCommand();
+                if (gameCommand.equals("R")) {
+                    moveBridgeResult.clear();
+                    new BridgeGame().retry(bridge, blockCount);
+                    tryCount = blockCount;
+                }
                 if (gameCommand.equals("Q")) {
                     new OutputView().printResult(moveBridgeResult, tryCount + 1);
+                    break;
                 }
-                break;
             }
         }
     }
