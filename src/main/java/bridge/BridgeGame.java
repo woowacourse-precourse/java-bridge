cf90f548e4594f5f45fc5fd4255134a99455013e
limzzum <chfhddl1234@naver.com>
2022-11-19 20:29:56 +0900

    feat : 다리 이동 메서드

--- src/main/java/bridge/BridgeGame.java
+++ src/main/java/bridge/BridgeGame.java
@@ -1,12 +1,14 @@
 package bridge;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class BridgeGame {
     InputView inputView = new InputView();
+    OutputView outputView = new OutputView();
+    List<String> visited = new ArrayList<>();
     BridgeMaker bridgeMaker;
     Bridge bridge;
-
 
     public void init(){
         int bridgeLen = inputView.readBridgeSize();
@@ -16,8 +18,18 @@ public class BridgeGame {
     /**
      * 이동을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void move() {
+    public boolean move() {
+        String direction = inputView.readMoving();
+        boolean isRightDirection = bridge.isRightDirection(visited.size(),direction);
+        if(isRightDirection){
+            outputView.printMap(visited,direction,"O");
+            visited.add(direction);
+            return true;
+        }
+        outputView.printMap(visited, direction, "X");
+        return false;
     }
+
 
     /**
      * 사용자가 게임을 다시 시도할 때 사용하는 메서드
