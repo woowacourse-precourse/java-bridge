c14262df7e535d93b72c594aac22b152db72aec6
meunrud <y3a0522@gmail.com>
2022-11-19 16:24:20 +0900

    :art: (InputValidator) 메서드 static 으로 선언

--- src/main/java/bridge/InputValidator.java
+++ src/main/java/bridge/InputValidator.java
@@ -2,17 +2,17 @@ package bridge;
 
 public class InputValidator {
 
-    public int ValidateInputBridgeLength(String inputLength){
+    public static int validateInputBridgeLength(String inputLength){
         int bridgeLength = toInt(inputLength);
         validateBridgeLength(bridgeLength);
         return bridgeLength;
     }
 
-    public int toInt (String inputLength){
+    public static int toInt (String inputLength){
         return Integer.parseInt(inputLength);
     }
 
-    public void validateBridgeLength(int inputLength){
+    public static void validateBridgeLength(int inputLength){
         if(inputLength < 3 || inputLength > 20 ){
             throw new IllegalArgumentException("[ERROR] 다리 길이는 3에서 20 사이로 입력해주세요. ");
         }
