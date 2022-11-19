dbc404baeae90793e69fe93c46cd224a54477a9f
yongseungLeeDec <yonson.dev@gmail.com>
2022-11-19 12:29:58 +0900

    feat: 다리 길이 입력 검증 세부기능 구현 (허용되지 않은 문자 검사)
    BridgeLengthValidator의 hasNoIllegalCharacter 메서드를 구현함.
    - 해당 메서드는 사용자가 '다리 길이'로서 입력한 문자열이 정수로 변환 가능한 문자열인지 여부를 boolean 타입으로 반환함.

--- src/main/java/bridge/BridgeLengthValidator.java
+++ src/main/java/bridge/BridgeLengthValidator.java
@@ -2,4 +2,14 @@ package bridge;
 
 public class BridgeLengthValidator {
 
+    private boolean hasNoIllegalCharacter(String bridgeLengthInput) {
+        assert (bridgeLengthInput != null);
+
+        try {
+            Integer.parseInt(bridgeLengthInput);
+        } catch (NumberFormatException | NullPointerException exception) {
+            return false;
+        }
+        return true;
+    }
 }
