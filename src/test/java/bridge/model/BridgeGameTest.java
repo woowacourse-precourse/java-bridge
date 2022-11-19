4fe18e80b32256b68366fc148c7f58b94bd6b295
wkdtmdgnsp <wkdtmdgnsp@gmail.com>
2022-11-19 11:39:03 +0900

    test(bridge) : 다리 이동 테스트

---
+++ src/test/java/bridge/model/BridgeGameTest.java
@@ -0,0 +1,26 @@
+package bridge.model;
+
+import bridge.view.OutputView;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+class BridgeGameTest {
+
+    @Test
+    void 다리_이동_테스트1() {
+        BridgeGame bridgeGame = new BridgeGame();
+        bridgeGame.move("U", Arrays.asList("U"));
+        assertThat(bridgeGame.getUp()).isEqualTo(Arrays.asList("O"));
+    }
+
+    @Test
+    void 다리_이동_테스트2() {
+        BridgeGame bridgeGame = new BridgeGame();
+        bridgeGame.move("U", Arrays.asList("D"));
+        assertThat(bridgeGame.getUp()).isEqualTo(Arrays.asList("X"));
+    }
+}
