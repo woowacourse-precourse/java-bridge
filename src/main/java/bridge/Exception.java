4c7ee76369661ec0e6f59376251d6437a5bf6169
dydtmd6801 <dydtmd6801@naver.com>
2022-11-19 13:33:16 +0900

    feat(BridgeGame, Exception): 예외처리 기능
    BridgeGame : 사용자가 입력한 값을 가지고 예외처리 후, 생성 다리의 길이 완성
    - 공백, 공라인이라면 NullPointerException에러 발생
    - 정수형이 아니라면 NumberFormatException에러 발생
    - 3보다 작고 20보다 크면 IllegalArgumentException에러 발생
    
    Exception : 예외처리를 위한 기능
    - and 연산자를 or 연산자로 변경

--- src/main/java/bridge/Exception.java
+++ src/main/java/bridge/Exception.java
@@ -2,7 +2,7 @@ package bridge;
 
 public class Exception {
     public Boolean isEmpty(String checkEmpty) {
-        return checkEmpty.isEmpty() && checkEmpty.isBlank();
+        return checkEmpty.isEmpty() || checkEmpty.isBlank();
     }
 
     public Boolean isRangeInThreeAndTwenty(int bridgeSize) {
