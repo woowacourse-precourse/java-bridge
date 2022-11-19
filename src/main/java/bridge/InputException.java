2abb7e59ffbf71c20dd36c5bc90f3da8aada9c30
2do1 <dowon3036@naver.com>
2022-11-19 20:31:06 +0900

    feat: 입력한 다리 길이의 값이 3부터 20 사이의 숫자가 아닐때 예외처리 구현

--- src/main/java/bridge/InputException.java
+++ src/main/java/bridge/InputException.java
@@ -2,10 +2,20 @@ package bridge;
 
 public class InputException {
     static final String NOT_INTEGER_ERROR_MESSAGE = "[ERROR] 숫자를 입력해주세요.";
+    static final String BRIDGE_SIZE_RANGE_ERROR_MESSAGE = "[ERROR] 다리 길이는 3부터 20 사이의 숫자여야 합니다.";
+
+    static final int MIN_BRIDGE_SIZE = 3;
+    static final int MAX_BRIDGE_SIZE = 20;
 
     public static void checkIsBridgeSizeInteger(String bridgeSize) {
         if (!bridgeSize.matches("-?\\d+(\\.\\d+)?")) {
             throw new IllegalArgumentException(NOT_INTEGER_ERROR_MESSAGE);
+        }
+    }
+
+    public static void checkBridgeSizeRange(int bridgeSize) {
+        if (bridgeSize < MIN_BRIDGE_SIZE || bridgeSize > MAX_BRIDGE_SIZE) {
+            throw new IllegalArgumentException(BRIDGE_SIZE_RANGE_ERROR_MESSAGE);
         }
     }
 }
