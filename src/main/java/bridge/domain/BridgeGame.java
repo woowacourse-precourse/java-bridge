d3e937ec4671317da62d2a2a27e745f8b424495a
yeeun <dpdms9621@naver.com>
2022-11-19 16:50:31 +0900

    feat(BridgeGame): 사용자의 입력으로 게임을 다시 시도할 때 사용하는 메서드 기능

--- src/main/java/bridge/domain/BridgeGame.java
+++ src/main/java/bridge/domain/BridgeGame.java
@@ -24,6 +24,16 @@ public class BridgeGame {
         return true;
     }
 
+    //사용자가 게임을 다시 시도할 때 사용하는 메서드
+    public boolean retry(String isRestart) {
+        if (isRestart.equals("R")) {
+            this.userSelectList = new ArrayList<>();
+            bridgeMoving.setMoveCount();
+            return true;
+        }
+        return false;
+    }
+
     public void setBridgeMake(int size) {
         bridgeMoving.setBridgeMaker(size);
     }
