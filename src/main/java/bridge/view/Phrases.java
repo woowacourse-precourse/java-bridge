0c1232f349bba581879a983f686a8098ebe6bc9e
62hoon99 <98267772+62hoon99@users.noreply.github.com>
2022-11-19 09:17:09 +0900

    feat(view): 다리길이 입력 기능 구현

---
+++ src/main/java/bridge/view/Phrases.java
@@ -0,0 +1,11 @@
+package bridge.view;
+
+public enum Phrases {
+    readBridgeSize("다리의 길이를 입력해주세요.");
+
+    private String phrase;
+
+    Phrases(String phrase) {
+        this.phrase = phrase;
+    }
+}
