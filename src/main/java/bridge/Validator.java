7390cdb43026538bae21690e85300f343ffbaa89
ChanghyunKim <variey82p@gmail.com>
2022-11-19 19:42:07 +0900

    feat(Validator) : 다리길이 범위를 검증하는 메서드 추가

--- src/main/java/bridge/Validator.java
+++ src/main/java/bridge/Validator.java
@@ -2,13 +2,23 @@ package bridge;
 
 import java.util.regex.Pattern;
 
-import static javax.swing.JOptionPane.ERROR_MESSAGE;
+import static bridge.GameMessage.ERROR_MESSAGE;
+
 
 public class Validator {
+    private static final int BRIDGE_SIZE_LOWER_INCLUSIVE = 3;
+    private static final int BRIDGE_SIZE_UPPER_INCLUSIVE = 20;
     public void validateNumeric(String input){
         String numericPattern = "^[0-9]*$";
         if(!Pattern.matches(numericPattern, input)){
             throw new IllegalArgumentException(ERROR_MESSAGE + "숫자 형태가 아닙니다");
+        }
+    }
+
+    public void validateBridgeSize(String input){
+        int bridgeSize = Integer.parseInt(input);
+        if(bridgeSize < BRIDGE_SIZE_LOWER_INCLUSIVE || BRIDGE_SIZE_LOWER_INCLUSIVE > BRIDGE_SIZE_UPPER_INCLUSIVE){
+            throw new IllegalArgumentException(ERROR_MESSAGE + "다리 길이는 3부터 20 사이의 숫자여야 합니다.");
         }
     }
 }
