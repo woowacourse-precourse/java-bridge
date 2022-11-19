84cd2ccdabd177aad06cbb27e60c12aafc1fcb95
김현재 <41482946+galug@users.noreply.github.com>
2022-11-20 00:31:16 +0900

    feat(inputview): 이동할 칸 입력에 대한 검증 기능 추가

--- src/main/java/bridge/view/InputView.java
+++ src/main/java/bridge/view/InputView.java
@@ -3,13 +3,13 @@ package bridge.view;
 import camp.nextstep.edu.missionutils.Console;
 
 public class InputView {
-    private static final String BRIDGE_SIZE_FORM_ERROR = "[ERROR] 2자리의 숫자로 입력해주세요.";
+    private static final String BRIDGE_SIZE_INPUT_ERROR = "[ERROR] 2자리의 숫자로 입력해주세요.";
+    private static final String WRONG_MOVING_MARK_INPUT_ERROR = "[ERROR] U 혹은 D를 입력해주세요.";
+    private static final String WRONG_RETRY_INPUT_ERROR = "[ERROR] U 혹은 D를 입력해주세요.";
 
-    /**
-     * 다리의 길이를 입력받는다.
-     */
     public int readBridgeSize() {
         String bridgeSizeInput = input();
+
         validateBridgeSizeInput(bridgeSizeInput);
         return Integer.valueOf(bridgeSizeInput);
     }
@@ -17,26 +17,24 @@ public class InputView {
     private void validateBridgeSizeInput(String bridgeSizeInput) {
         String match = "^\\d{1,2}$";
         if (!bridgeSizeInput.matches(match)) {
-            throw new IllegalArgumentException(BRIDGE_SIZE_FORM_ERROR);
+            throw new IllegalArgumentException(BRIDGE_SIZE_INPUT_ERROR);
         }
     }
 
-    /**
-     * 사용자가 이동할 칸을 입력받는다.
-     */
     public String readMoving() {
         String selectUpOrDownInput = input();
+
         validateMovingInput(selectUpOrDownInput);
         return selectUpOrDownInput;
     }
 
     private void validateMovingInput(String selectUpOrDownInput) {
-
+        String match = "^[U|D]$";
+        if (!selectUpOrDownInput.matches(match)) {
+            throw new IllegalStateException(WRONG_MOVING_MARK_INPUT_ERROR);
+        }
     }
 
-    /**
-     * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
-     */
     public String readGameCommand() {
         return null;
     }
