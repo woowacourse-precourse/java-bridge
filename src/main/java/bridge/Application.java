fabb54b86175846a27d52efc91b79cbaf28d49dc
박원종 <wonjong1030@naver.com>
2022-11-19 21:26:12 +0900

    feat(Application): run 메소드 호출 구현, 기능 완성

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -1,8 +1,20 @@
 package bridge;
 
+import bridge.controller.BridgeController;
+import bridge.view.InputView;
+import bridge.view.OutputView;
+
 public class Application {
 
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        InputView inputView = new InputView();
+        OutputView outputView = new OutputView();
+        BridgeController bridgeController = new BridgeController(inputView, outputView);
+        try {
+            bridgeController.run();
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+        }
     }
 }
