0e920c865adcac5fa1d581287f1408e1982e97f9
anneu <b.imnuj@gmail.com>
2022-11-19 21:04:02 +0900

    refactor: 다리를 이동하는 기능 테스트 수정

--- src/test/java/bridge/BridgeGameTest.java
+++ src/test/java/bridge/BridgeGameTest.java
@@ -4,20 +4,26 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import bridge.domain.Bridge;
 import java.util.List;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
 class BridgeGameTest {
 
-    private final BridgeGame bridgeGame = new BridgeGame();
+    private BridgeGame bridgeGame;
+
+    @BeforeEach
+    void setUp() {
+        Bridge bridge = new Bridge(List.of("D", "D", "D"));
+        bridgeGame = new BridgeGame(bridge);
+    }
 
     @DisplayName("이동이 가능하면 ture, 이동이 불가능하면 false 확인")
     @CsvSource(value = {"D, true", "U, false"})
     @ParameterizedTest
     void move(String moving, boolean expected) {
-        Bridge bridge = new Bridge(List.of("D", "U", "U"));
-        Boolean actual = bridgeGame.move(bridge, 0, moving);
+        Boolean actual = bridgeGame.move(0, moving);
         assertThat(actual).isEqualTo(expected);
     }
 }
