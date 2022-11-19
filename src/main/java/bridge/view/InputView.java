8e1fa2a9f504a80b160378d8a32c76bdb6273e13
jjdone <ehdlfaos@gmail.com>
2022-11-19 17:35:15 +0900

    feat: View 설정
    InputView 클래스 메시지 출력 구현 및 필요 메서드 생성

--- src/main/java/bridge/view/InputView.java
+++ src/main/java/bridge/view/InputView.java
@@ -1,28 +1,37 @@
-package bridge;
+package bridge.view;
 
-/**
- * 사용자로부터 입력을 받는 역할을 한다.
- */
+import camp.nextstep.edu.missionutils.Console;
+
 public class InputView {
+    private static final String BRIDGE_SIZE_MESSAGE = "다리의 길이를 입력해주세요.";
+    private static final String MOVING_MESSAGE = "이동할 칸을 선택해주세요. (위: U, 아래: D)";
+    private static final String RESTART_GAME_MESSAGE = "게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)";
 
     /**
      * 다리의 길이를 입력받는다.
      */
     public int readBridgeSize() {
-        return 0;
+        System.out.println(BRIDGE_SIZE_MESSAGE);
+        return stringToInteger(Console.readLine());
     }
 
     /**
      * 사용자가 이동할 칸을 입력받는다.
      */
     public String readMoving() {
-        return null;
+        System.out.println(MOVING_MESSAGE);
+        return Console.readLine();
     }
 
     /**
      * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
      */
     public String readGameCommand() {
-        return null;
+        System.out.println(RESTART_GAME_MESSAGE);
+        return Console.readLine();
+    }
+
+    private int stringToInteger(String input) {
+        return Integer.parseInt(input);
     }
 }
