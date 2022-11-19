9664b6e3a96a175bc20a92a47ee8019bfebc147c
seoyoung <sy9723@korea.ac.kr>
2022-11-19 16:50:08 +0900

    feat: 기능 추가 (이동한 칸을 건널 수 있는지 없는지 확인한다)

--- src/main/java/bridge/OutputView.java
+++ src/main/java/bridge/OutputView.java
@@ -10,15 +10,15 @@ import java.util.Map;
  */
 public class OutputView {
 
-    public void printStartMessage() {
+    public static void printStartMessage() {
         System.out.println("다리 건너기 게임을 시작합니다.");
     }
 
-    public void printMessageForBridgeSize() {
+    public static void printMessageForBridgeSize() {
         System.out.println("다리의 길이를 입력해주세요.");
     }
 
-    public void printMessageForMoving() {
+    public static void printMessageForMoving() {
         System.out.println("이동할 칸을 선택해주세요. (위: U, 아래: D)");
     }
 
@@ -28,16 +28,17 @@ public class OutputView {
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
     /** success: 0 -> down 성공, 1 -> down 실패, 2 -> up 성공, 3 -> up 실패 **/
-    public void printMap(List<Integer> movingSuccess) {
+    public static boolean printMap(List<Integer> movingSuccess) {
         List<String> upPrintChecker = Arrays.asList("   ", "   ", " O ", " X ");
         List<String> downPrintChecker = Arrays.asList(" O ", " X ", "   ", "   ");
         String upBridge = createBridge(movingSuccess, upPrintChecker);
         String downBridge = createBridge(movingSuccess, downPrintChecker);
         System.out.println("[" + upBridge + "]");
         System.out.println("[" + downBridge + "]");
+        return checkResult(movingSuccess);
     }
 
-    private String createBridge(List<Integer> movingSuccess, List<String> PrintChecker){
+    private static String createBridge(List<Integer> movingSuccess, List<String> PrintChecker){
         String printBridge = "";
         for (int i = 0; i < movingSuccess.size(); i++){
             printBridge = printBridge + PrintChecker.get(movingSuccess.get(i));
@@ -60,6 +61,10 @@ public class OutputView {
         movingChecker.put("U", "   ");
         movingChecker.put("D", " O ");
         return  movingChecker;
+    }
+
+    private static boolean checkResult(List<Integer> movingSuccess) {
+        return movingSuccess.get(movingSuccess.size() - 1) == 0 || movingSuccess.get(movingSuccess.size() - 1) == 2;
     }
 
     /**
