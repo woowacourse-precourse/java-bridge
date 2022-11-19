f77acd0e2bbabde54a80b699eb8033155b85ce04
Haechan Lee <dlgockssla@gmail.com>
2022-11-19 10:06:18 +0900

    feat(Direction): get Direction from number or symbol

--- src/main/java/bridge/Direction.java
+++ src/main/java/bridge/Direction.java
@@ -1,18 +1,34 @@
 package bridge;
 
 public enum Direction {
-    DOWN( 0, "D"), UP( 1,"U");
+    DOWN(0, "D"), UP(1, "U");
 
-    private final int id;
+    private final int bridgeNumber;
     private final String symbol;
 
-    Direction(int id, String symbol) {
-        this.id = id;
+    Direction(int bridgeNumber, String symbol) {
+        this.bridgeNumber = bridgeNumber;
         this.symbol = symbol;
     }
 
-    public int getNumber() {
-        return id;
+    static Direction fromBridgeNumber(int bridgeNumber){
+        for (Direction direction : values()) {
+            if (direction.bridgeNumber == bridgeNumber) {
+                return direction;
+            }
+        }
+
+        throw new IllegalArgumentException(bridgeNumber + "는 유효하지 않은 값입니다.");
+    }
+
+    public static Direction fromSymbol(String symbol) {
+        for (Direction direction : values()) {
+            if (direction.symbol.equals(symbol)){
+                return direction;
+            }
+        }
+
+        throw new IllegalArgumentException(symbol + "는 유효하지 않은 값입니다.");
     }
 
     public String getSymbol() {
