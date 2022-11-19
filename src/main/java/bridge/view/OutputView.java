54c71a1b8ea240dac9b01861a35a4b14a5d50085
CMHyuk <alsgur9710@naver.com>
2022-11-19 20:21:03 +0900

    feat(OutputView): 게임 재시작 시 다리 원상태로 변경을 위한 메서드

--- src/main/java/bridge/view/OutputView.java
+++ src/main/java/bridge/view/OutputView.java
@@ -1,7 +1,5 @@
 package bridge.view;
 
-import dto.Result;
-
 /**
  * 사용자에게 게임 진행 상황과 결과를 출력하는 역할을 한다.
  */
@@ -9,6 +7,14 @@ public class OutputView {
 
     private static String upBridge = "[]";
     private static String downBridge = "[]";
+
+    public void setUpBridge() {
+        upBridge = "[]";
+    }
+
+    public void setDownBridge() {
+        downBridge = "[]";
+    }
 
     /**
      * 현재까지 이동한 다리의 상태를 정해진 형식에 맞춰 출력한다.
