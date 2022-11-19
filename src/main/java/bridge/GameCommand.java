0e8c0d1a6a9503a01be8ccbbacde5c1dc87cbd68
rt3310 <btac3310@gmail.com>
2022-11-20 02:24:25 +0900

    refactor: 게임 재시작 입력 값 Enum 선언 -> GameCommand

--- src/main/java/bridge/GameCommand.java
+++ src/main/java/bridge/GameCommand.java
@@ -1,2 +1,16 @@
-package bridge;public enum GameCommand {
+package bridge;
+
+public enum GameCommand {
+    RETRY("R"),
+    QUIT("Q");
+
+    private String command;
+
+    GameCommand(String command) {
+        this.command = command;
+    }
+
+    public boolean isEqual(String command) {
+        return this.command.equals(command);
+    }
 }
