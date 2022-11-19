c4a357c933c72733f9ae0527aca23871d4acda0a
sosow0212 <sosow0212@naver.com>
2022-11-19 18:12:19 +0900

    feat : (InputView) 게임 입력에 대한 예외 처리

--- src/main/java/bridge/view/InputView.java
+++ src/main/java/bridge/view/InputView.java
@@ -1,30 +1,31 @@
 package bridge.view;
 
+import static camp.nextstep.edu.missionutils.Console.readLine;
+
+import bridge.exception.BridgeGameException;
 import bridge.util.BridgeInputConverter;
-import camp.nextstep.edu.missionutils.Console;
 
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
  */
 public class InputView {
+    private final BridgeGameException bridgeGameException = new BridgeGameException();
 
     /**
      * 다리의 길이를 입력받는다.
      */
     public int readBridgeSize() {
-        try {
-            String bridgeSize = Console.readLine();
-            return BridgeInputConverter.bridgeSizeConverter(bridgeSize);
-        } catch (IllegalArgumentException exception) {
-            throw new IllegalArgumentException(exception.getMessage());
-        }
+        String bridgeSize = readLine();
+        bridgeGameException.validateBridgeSize(bridgeSize);
+        return BridgeInputConverter.bridgeSizeConverter(bridgeSize);
     }
 
     /**
      * 사용자가 이동할 칸을 입력받는다.
      */
     public String readMoving() {
-        String move = Console.readLine();
+        String move = readLine();
+        bridgeGameException.validateDirection(move);
         return move;
     }
 
@@ -32,7 +33,8 @@ public class InputView {
      * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
      */
     public String readGameCommand() {
-        String command = Console.readLine();
+        String command = readLine();
+        bridgeGameException.validateGameStatusCommand(command);
         return command;
     }
 }
