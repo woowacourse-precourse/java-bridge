e98666385eac1e416897b330ada53d4da35e9dc9
lseok <lseokhyun99@gmail.com>
2022-11-19 15:42:12 +0900

    feat: 현재까지 이동한 다리의 상태 출력 메소드 구현
    다리를 상태를 출력하는 메소드
    printBridge()를 추가하여
    현재까지 이동한 전체 상태를 출력하는 메소드
    printMap() 에서 2번 호출하였다.
    
    printBridge() 메소드는 주어진 인자에 따라
    위쪽 다리 혹은 아래쪽 다리 상태를 출력한다.

--- src/main/java/bridge/OutputView.java
+++ src/main/java/bridge/OutputView.java
@@ -1,16 +1,33 @@
 package bridge;
 
+import java.util.List;
+
 /**
  * 사용자에게 게임 진행 상황과 결과를 출력하는 역할을 한다.
  */
 public class OutputView {
+    private static final String UP = "U";
+    private static final String DOWN = "D";
+    private static final String PATH_BRIDGE_EQUAL = " O ";
+    private static final String PATH_BRIDGE_NOT_EQUAL = " X ";
+    private static final String BRIDGE_NOT_SELECTED = "   ";
+    private static final String SEPARATOR = "|";
+    private static final String BRIDGE_START = "[";
+    private static final String BRIDGE_END = "\b]";
 
     /**
      * 현재까지 이동한 다리의 상태를 정해진 형식에 맞춰 출력한다.
      * <p>
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void printMap() {
+    public void printMap(List<String> bridge, List<String> path) {
+        System.out.println(BRIDGE_START);
+        printBridge(UP, bridge, path);
+        System.out.println(BRIDGE_END);
+
+        System.out.println(BRIDGE_START);
+        printBridge(DOWN, bridge, path);
+        System.out.println(BRIDGE_END);
     }
 
     /**
@@ -19,5 +36,17 @@ public class OutputView {
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
     public void printResult() {
+    }
+
+    private void printBridge(String UpDown, List<String> bridge, List<String> path){
+        for(int i = 0 ; i < path.size() ; i++) {
+            if(path.get(i).equals(UpDown) && path.get(i).equals(bridge.get(i)))
+                System.out.print(PATH_BRIDGE_EQUAL);
+            if(path.get(i).equals(UpDown) && !path.get(i).equals(bridge.get(i)))
+                System.out.print(PATH_BRIDGE_NOT_EQUAL);
+            if(!path.get(i).equals(UpDown))
+                System.out.print(BRIDGE_NOT_SELECTED);
+            System.out.print(SEPARATOR);
+        }
     }
 }
