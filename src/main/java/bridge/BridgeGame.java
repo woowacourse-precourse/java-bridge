730ae74fad56d8fa2d31742485e0d88dc222de9d
JungHun Jang <ijj5258@naver.com>
2022-11-20 00:30:05 +0900

    feat : retry 기능 구현

--- src/main/java/bridge/BridgeGame.java
+++ src/main/java/bridge/BridgeGame.java
@@ -1,7 +1,5 @@
 package bridge;
 
-import java.util.List;
-
 /**
  * 다리 건너기 게임을 관리하는 클래스
  */
@@ -24,6 +22,10 @@ public class BridgeGame {
      * <p>
      * 재시작을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void retry(List<String> bridge) {
+    public boolean retry(String command) {
+    	if(command.equals("Q")) {
+    		return false;
+    	}
+    	return true;
     }
 }
