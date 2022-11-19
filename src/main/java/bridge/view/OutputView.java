f7e753329dc7ea01ec00553d9c9251b8ce483aee
김현재 <41482946+galug@users.noreply.github.com>
2022-11-20 00:09:28 +0900

    feat(output): 게임 결과를 출력하는 기능 추가

--- src/main/java/bridge/view/OutputView.java
+++ src/main/java/bridge/view/OutputView.java
@@ -11,29 +11,38 @@ public class OutputView {
     public void printRequestBridgeSizeInput() {
         System.out.println(Constants.REQUEST_BRIDGE_SIZE_INPUT);
     }
+
     public void printSelectUpOrDownInput() {
         System.out.println(Constants.SELECT_UP_OR_DOWN_INPUT);
     }
+
     public void printSelectRetryOrNotInput() {
         System.out.println(Constants.SELECT_RETRY_OR_NOT_INPUT);
     }
+
     public void printFinalGameResult() {
         System.out.println(Constants.FINAL_GAME_RESULT_OUTPUT);
     }
 
-    /**
-     * 현재까지 이동한 다리의 상태를 정해진 형식에 맞춰 출력한다.
-     * <p>
-     * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
-     */
     public void printMap(String map) {
         System.out.println(map);
     }
-    /**
-     * 게임의 최종 결과를 정해진 형식에 맞춰 출력한다.
-     * <p>
-     * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
-     */
+
     public void printResult(String map, boolean isSuccess, int numberOfTry) {
+        printMap(map);
+        printSuccess(isSuccess);
+        printNumberOfTry(numberOfTry);
+    }
+
+    private void printSuccess(boolean isSuccess) {
+        if (isSuccess) {
+            System.out.println(Constants.GAME_SUCCESS_OUTPUT);
+            return;
+        }
+        System.out.println(Constants.GAME_FAILURE_OUTPUT);
+    }
+
+    private void printNumberOfTry(int numberOfTry) {
+        System.out.println(Constants.TOTAL_NUMBER_OF_TRY_OUTPUT);
     }
 }
