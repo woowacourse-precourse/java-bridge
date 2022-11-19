ae4a7c9c60f01855565754dac5f8f2fb6fbf2e51
lcw3176 <lcw3176@naver.com>
2022-11-19 18:23:31 +0900

    feat: '다리 건너기 게임을 시작합니다.' 문구 출력

---
+++ src/main/java/bridge/view/output/OutputMessage.java
@@ -0,0 +1,11 @@
+package bridge.view.output;
+
+public class OutputMessage {
+
+    private OutputMessage() {
+
+    }
+    
+    public static final String WELCOME_MESSAGE = "다리 건너기 게임을 시작합니다.";
+
+}
