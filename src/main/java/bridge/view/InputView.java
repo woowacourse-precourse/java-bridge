7424f0756a328cbc2e0ec249c2ac4644409cb286
greeng00se <thegreengoose7@gmail.com>
2022-11-19 16:22:52 +0900

    feat(InputView): 재시도 여부를 입력하는 메서드 추가

--- src/main/java/bridge/view/InputView.java
+++ src/main/java/bridge/view/InputView.java
@@ -9,6 +9,7 @@ public class InputView {
 
     private static final String READ_BRIDGE_SIZE_MESSAGE = "\n다리의 길이를 입력해주세요.";
     private static final String READ_MOVING_MESSAGE = "\n이동할 칸을 선택해주세요. (위: U, 아래: D)";
+    private static final String READ_GAME_COMMAND_MESSAGE = "\n게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)";
 
     private final InputValidator inputValidator = new InputValidator();
 
@@ -36,6 +37,9 @@ public class InputView {
      * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
      */
     public String readGameCommand() {
-        return null;
+        System.out.println(READ_GAME_COMMAND_MESSAGE);
+        String command = Console.readLine();
+        inputValidator.validateCommand(command);
+        return command;
     }
 }
