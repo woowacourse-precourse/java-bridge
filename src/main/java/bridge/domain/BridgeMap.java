9502ea7cabfdb7cce2f927052aab917c4ffee6ad
Isu Shin <isu010614@naver.com>
2022-11-20 00:31:34 +0900

    feat: 마지막 요소를 삭제하기 위한 기능 구현

--- src/main/java/bridge/domain/BridgeMap.java
+++ src/main/java/bridge/domain/BridgeMap.java
@@ -11,6 +11,11 @@ public class BridgeMap {
         this.map.append(component);
     }
 
+    public void deleteLastComponent(String component) {
+        this.map.deleteCharAt(
+                this.map.lastIndexOf(component));
+    }
+
     @Override
     public String toString() {
         return map.toString();
