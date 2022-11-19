42ba75e089c5ad8442eec1e5de257f3c26edb154
Sinryuji <siler2474@gmail.com>
2022-11-19 17:26:59 +0900

    refactor(Application): 기존 main() 로직 -> playGame(), 기존 playGame() 로직 -> playLoop()

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -7,15 +7,19 @@ public class Application {
     private static final BridgeMaker bridgeMaker = new BridgeMaker(new BridgeRandomNumberGenerator());
 
     public static void main(String[] args) {
+        playGame();
+    }
+
+    private static void playGame() {
         int size = inputView.readBridgeSize();
         Bridge bridge = new Bridge(bridgeMaker.makeBridge(size), size);
         BridgeGame bridgeGame = new BridgeGame(bridge);
         OutputView outputView = new OutputView(bridge, bridgeGame);
-        playGame(bridge, bridgeGame, outputView);
+        playLoop(bridge, bridgeGame, outputView);
         outputView.printResult();
     }
 
-    private static void playGame(Bridge bridge, BridgeGame bridgeGame, OutputView outputView) {
+    private static void playLoop(Bridge bridge, BridgeGame bridgeGame, OutputView outputView) {
         while (!bridge.getGameResult()) {
             bridgeGame.move(inputView.readMoving());
             outputView.printMap();
