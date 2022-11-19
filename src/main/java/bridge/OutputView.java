74c5224cb37b8154f2d90488426448b4fdffec16
bsh1603 <bsh1603@github.com>
2022-11-19 14:00:52 +0900

    Feat: 사용자 이동에 따른 O,X 출력

--- src/main/java/bridge/OutputView.java
+++ src/main/java/bridge/OutputView.java
@@ -1,5 +1,7 @@
 package bridge;
 
+import java.util.List;
+
 /**
  * 사용자에게 게임 진행 상황과 결과를 출력하는 역할을 한다.
  */
@@ -32,5 +34,17 @@ public class OutputView {
     public void printMapEnd(){
         sbUp.append("]");
         sbDo.append("]");
+    }
+
+    public void printMapBetween(){
+        sbUp.append("|");
+        sbDo.append("|");
+    }
+
+    public String printMoveUpDown(List<String> bridge, String input, int step){
+        if(bridge.get(step).equals(input)){
+            return "O";
+        }
+        return "X";
     }
 }
