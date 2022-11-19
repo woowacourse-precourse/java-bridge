b0c407370eafe92da40144083fb2e171fa4686de
Nitroblack1 <109022435+Nitroblack1@users.noreply.github.com>
2022-11-19 21:12:19 +0900

    feat(BridgeData) : implement getter of bridge and increasing attempt function

--- src/main/java/bridge/Database/BridgeData.java
+++ src/main/java/bridge/Database/BridgeData.java
@@ -5,18 +5,30 @@ import java.util.List;
 public class BridgeData {
 
     private List<String> bridge;
-    private List<String> bridgeLayOutByUser;
+    private List<String> bridgeDesignByUser;
     private int totalAttempt;
 
     public void setBridge(List<String> bridge) {
         this.bridge = bridge;
     }
 
-    public void setBridgeLayOutByUser(List<String> bridgeLayOutByUser) {
-        this.bridgeLayOutByUser = bridgeLayOutByUser;
+    public void setBridgeDesignByUser(List<String> bridgeDesignByUser) {
+        this.bridgeDesignByUser = bridgeDesignByUser;
     }
 
     public List<String> getBridge() {
         return this.bridge;
+    }
+
+    public List<String> getBridgeDesignByUser() {
+        return this.bridgeDesignByUser;
+    }
+
+    public void increaseAttempts() {
+        this.totalAttempt += 1;
+    }
+
+    public BridgeData() {
+        this.totalAttempt = 0;
     }
 }
