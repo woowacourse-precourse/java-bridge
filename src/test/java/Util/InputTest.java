058507eab42bf71dd3810ec73852b4dd203a4e97
sangho-jeon <jeonsangho1587@gmail.com>
2022-11-19 22:57:33 +0900

    test      : input validation test added.

---
+++ src/test/java/Util/InputTest.java
@@ -0,0 +1,26 @@
+package Util;
+
+import bridge.Util.Validator;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+
+public class InputTest {
+
+    @DisplayName("다리 생성 input의 타입 오류")
+    @Test
+    void 다리_생성_타입_예외(){
+        assertThatThrownBy(() -> Validator.validateBridgeInput("jp"))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("다리 생성 input의 크기 오류")
+    @Test
+    void 다리_생성_크기_예외(){
+        assertThatThrownBy(() -> Validator.validateBridgeInput("25"))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+}
