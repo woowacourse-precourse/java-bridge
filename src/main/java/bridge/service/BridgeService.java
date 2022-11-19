e353d5f857e03a3fac6ce5967db2336e00efae55
yeeun <dpdms9621@naver.com>
2022-11-19 17:07:15 +0900

    feat(BridgeService): 유저 결과와 다리 결과가 같은지 확인하는 기능

--- src/main/java/bridge/service/BridgeService.java
+++ src/main/java/bridge/service/BridgeService.java
@@ -79,4 +79,9 @@ public class BridgeService {
         return isCorrect;
     }
 
+    // 유저 결과와 다리 결과가 같은지 확인
+    public boolean isSuccess() {
+        return bridgeGame.getMakeBridgeList().equals(bridgeGame.getUserSelectList());
+    }
+
 }
