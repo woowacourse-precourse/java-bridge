7671002cf15c520086023040ce5e6a278954666c
RO-OR <kyk3531@gmail.com>
2022-11-20 07:09:44 +0900

    feat($bridge) : BridgeGmae클래스에 crossSelect메소드를 추가 하였습니다.
    crossSelect메소드를 구현하기 위한 재시작을 선택하는 retry메소드를 추가하였고,
    input값에 따른 에러처리 메소드 crossSelectError, retryError 메소드를 추가하였습니다,
    마지막으로 건넌 다리 리스트를 출력하고 retry함수를 호출하는 crossSelect메소드를 작성하였습니다.
    
    BridgeGame클래스에 crossSelect메소드를 추가하였습니다.

--- src/main/java/bridge/BridgeGame.java
+++ src/main/java/bridge/BridgeGame.java
@@ -1,13 +1,17 @@
 package bridge;
 
 
+import bridge.view.InputView;
+import bridge.view.OutputView;
+
+import java.util.ArrayList;
 import java.util.List;
 
 /**
  * 다리 건너기 게임을 관리하는 클래스
  */
 public class BridgeGame {
-
+    public static int playcount = 1 ;
     /**
      * 사용자가 칸을 이동할 때 사용하는 메서드
      * <p>
@@ -22,7 +26,6 @@ public class BridgeGame {
             bridgeView.add(input);
             bridgeView.add("X");
         }
-
         return bridgeView;
     }
 
@@ -31,6 +34,49 @@ public class BridgeGame {
      * <p>
      * 재시작을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void retry() {
+    private List<String> retry(int count, List<String> bridgeView, List<String> cross) {
+        List<String> retryBridge = new ArrayList<>();
+        String input = retryError();
+        if(input.equals("Q")){
+            return bridgeView;
+        }
+        if(input.equals("R")){
+            playcount++;
+            return crossSelect(count, retryBridge, cross);
+        } //게임 진행 메소드 넣기
+        return null;
+    }
+
+    public List<String> crossSelect(int count, List<String> bridgeView, List<String> cross){
+        for(int i =0; i<count; i++){
+            String input = crossSelectError();
+            new OutputView().printMap(move(bridgeView,input,cross.get(i)));
+            if(!input.equals(cross.get(i))){
+                return retry(count, bridgeView, cross);
+            }
+        }
+        return bridgeView;
+    }
+
+    private String crossSelectError(){
+        String input;
+        try{
+            input = new InputView().readMoving();
+        }catch (IllegalArgumentException e){
+            System.out.println(ErrorMessage.BRIDGE_CROSS_INPUT.getMessage());
+            return crossSelectError();
+        }
+        return input;
+    }
+
+    private String retryError(){
+        String input;
+        try{
+            input = new InputView().readGameCommand();
+        }catch (IllegalArgumentException e){
+            System.out.println(ErrorMessage.BRIDGE_RESTART_INPUT.getMessage());
+            return retryError();
+        }
+        return input;
     }
 }
