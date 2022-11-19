5ac166e8fc508f52364bbb975a97d8d1f6c1484f
DESKTOP-1ANNTGI\USER <cvc100326@naver.com>
2022-11-19 11:41:24 +0900

    test: 테스트코드 변경 및 추가
    - move 테스트 변경
    - 재시작 기능 테스트 추가

--- src/test/java/bridge/BridgeGameTest.java
+++ src/test/java/bridge/BridgeGameTest.java
@@ -13,12 +13,12 @@ public class BridgeGameTest {
     @Test
     void move() {
         BridgeGame bridgeGame = new BridgeGame(List.of("U", "D", "D"));
-        assertThat(bridgeGame.move("U")).isEqualTo(List.of("U", "O"));
-        assertThat(bridgeGame.move("D")).isEqualTo(List.of("U", "D", "O"));
-        assertThat(bridgeGame.move("U")).isEqualTo(List.of("U", "D", "U", "X"));
+        assertThat(bridgeGame.move("U")).isEqualTo(true);
+        assertThat(bridgeGame.move("D")).isEqualTo(true);
+        assertThat(bridgeGame.move("U")).isEqualTo(false);
     }
 
-    @DisplayName("잘못된 입력값인 경우")
+    @DisplayName("이동시 잘못된 입력값인 경우")
     @Test
     void moveByInvaildInput() {
         BridgeGame bridgeGame = new BridgeGame(List.of("U", "D", "D"));
@@ -26,12 +26,35 @@ public class BridgeGameTest {
                 () -> bridgeGame.move("u")).isInstanceOf(IllegalArgumentException.class);
     }
 
-    @DisplayName("종료 재시작 선택 시 잘못된 입력값인 경우")
+    @DisplayName("올바른 재시작 명령어")
+    @Test
+    void retryByCommand() {
+        BridgeGame bridgeGame = new BridgeGame(List.of("U", "D", "D"));
+        assertThat(bridgeGame.retry("R")).isEqualTo(true);
+    }
+
+    @DisplayName("올바른 종료 명령어")
+    @Test
+    void quitByCommand() {
+        BridgeGame bridgeGame = new BridgeGame(List.of("U", "D", "D"));
+        assertThat(bridgeGame.retry("Q")).isEqualTo(false);
+    }
+
+    @DisplayName("재시작,종료시 잘못된 명령어 입력")
     @Test
     void commandByInvaildInput() {
         BridgeGame bridgeGame = new BridgeGame(List.of("U", "D", "D"));
-        ;
         assertThatThrownBy(
                 () -> bridgeGame.retry("q")).isInstanceOf(IllegalArgumentException.class);
     }
 }
+//    @DisplayName("승리 확인")
+//    @Test
+//    void checkwin() {
+//        BridgeGame bridgeGame = new BridgeGame(List.of("U", "D", "D"));
+//        bridgeGame.move("U");
+//        bridgeGame.move("D");
+//        bridgeGame.move("D");
+//        assertThat(bridgeGame.checkWin()).isEqualTo(true);
+//    }
+//}
