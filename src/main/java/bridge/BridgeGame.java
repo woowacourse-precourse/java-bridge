eb392d465e0979f24861eba525857e08d76dbb29
bsh1603 <bsh1603@github.com>
2022-11-19 14:27:11 +0900

    Fix: 다리 길이 입력 받기 오류

--- src/main/java/bridge/BridgeGame.java
+++ src/main/java/bridge/BridgeGame.java
@@ -1,5 +1,7 @@
 package bridge;
 
+import java.util.List;
+
 /**
  * 다리 건너기 게임을 관리하는 클래스
  */
@@ -13,15 +15,13 @@ public class BridgeGame {
 
     public int requestBridgeSize(){
         System.out.println("다리 길이를 입력해주세요.");
-        while(true){
-            try{
-                inputView.readBridgeSize();
-                return inputView.readBridgeSize();
-            }catch (IllegalArgumentException e){
-                System.out.println("다리 길이를 입력해주세요");
-                inputView.readBridgeSize();
-            }
+        try{
+            return inputView.readBridgeSize();
+        }catch (IllegalArgumentException e){
+            System.out.println("다리 길이를 입력해주세요");
+            inputView.readBridgeSize();
         }
+        return inputView.readBridgeSize();
     }
 
     /**
@@ -30,6 +30,7 @@ public class BridgeGame {
      * 이동을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
     public void move() {
+
     }
 
     /**
