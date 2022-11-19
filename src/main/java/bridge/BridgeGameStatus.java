7a1c6942cbfc77746e4172bef355b02e5703b1cf
saint6839 <saint6839@gmail.com>
2022-11-19 23:29:17 +0900

    feat(domain) : 게임 진행, 중단 상태를 가지는 BridgeGameStatus 클래스 추가

---
+++ src/main/java/bridge/BridgeGameStatus.java
@@ -0,0 +1,13 @@
+package bridge;
+
+public class BridgeGameStatus {
+    private boolean isRunning;
+
+    public BridgeGameStatus(boolean isRunning) {
+        this.isRunning = isRunning;
+    }
+
+    public boolean isRunning() {
+        return isRunning;
+    }
+}
