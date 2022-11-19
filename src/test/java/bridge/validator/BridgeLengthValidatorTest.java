9965cb33f1960210f243abaa2a9e5ea8d5a1e8a8
nakalic <nakalic28@gmail.com>
2022-11-19 13:38:49 +0900

    test(BridgeLengthValidatorTest): 다리 길이 입력값을 검증하는 기능 테스트 코드 추가

---
+++ src/test/java/bridge/validator/BridgeLengthValidatorTest.java
@@ -0,0 +1,31 @@
+package bridge.validator;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+class BridgeLengthValidatorTest {
+
+    @DisplayName("자연수가 아니라면 예외를 발생시킨다.")
+    @ParameterizedTest
+    @ValueSource(strings = {"1.1", "안", "aaa"})
+    void createNotNaturalNumber(String input) {
+        assertThatThrownBy(() ->BridgeLengthValidator.validateNaturalNumber(input))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(BridgeLengthValidator.BRIDGE_LENGTH_MESSAGE);
+
+    }
+
+    @DisplayName("3 이상 20 이하의 숫자가 아니면 예외를 발생시킨다.")
+    @ParameterizedTest()
+    @ValueSource(strings = {"-1", "0", "21"})
+    void createNumberOutOfRange(String input) {
+        assertThatThrownBy(() ->BridgeLengthValidator.validateRange(input))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(BridgeLengthValidator.BRIDGE_LENGTH_MESSAGE);
+
+    }
+}
