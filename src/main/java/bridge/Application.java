1a8fb1549a0e63513531d0f38cd40a95ef1eee26
Hoo93 <117077999+Hoo93@users.noreply.github.com>
2022-11-19 22:32:17 +0900

    FEAT[Application]: 게임 진행 및 control part 구현

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -1,8 +1,38 @@
 package bridge;
 
+import java.util.List;
+
 public class Application {
 
+    private static final BridgeNumberGenerator bridgeNumberGenerator = new BridgeRandomNumberGenerator();
+    private static final InputView inputView = new InputView();
+    private static final OutputView outputView = new OutputView();
+
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        BridgeMaker bridgeMaker = new BridgeMaker(bridgeNumberGenerator);
+        List<String> bridge = bridgeMaker.makeBridge(inputView.readBridgeSize());
+        System.out.println(bridge);
+
+        int gameCount = 1;
+        boolean game =true;
+
+        while (game) {
+            BridgeGame bridgeGame = new BridgeGame(bridge,inputView, outputView);
+            if (bridgeGame.move()) {
+                outputView.printEndMessage();
+                outputView.printMap(bridgeGame.upperLine,bridgeGame.lowerLine);
+                outputView.printResult(true, gameCount);
+                break;
+            }
+            game = bridgeGame.retry(inputView.readGameCommand());
+            if (!game) {
+                outputView.printEndMessage();
+                outputView.printMap(bridgeGame.upperLine,bridgeGame.lowerLine);
+                outputView.printResult(false, gameCount);
+                break;
+            }
+            gameCount++;
+        }
+
     }
 }
