49ca902f7cdb8cb6565aa98069aeaa8b06482a5d
yun <yshyeon102@gmail.com>
2022-11-19 20:46:39 +0900

    test(Validator): 이동 및 게임 진행 관련한 테스트 작성

--- src/test/java/bridge/validator/ValidatorTest.java
+++ src/test/java/bridge/validator/ValidatorTest.java
@@ -34,9 +34,44 @@ class ValidatorTest {
 
     @DisplayName("다리 길이 입력 경계 값 테스트")
     @ParameterizedTest
-    @ValueSource(strings = {"3","20"})
+    @ValueSource(strings = {"3", "20"})
     void 다리_길이_입력에_대한_범위_검증_테스트_성공(String bridgeSize) {
         assertThat(Validator.validateBridgeSize(bridgeSize))
                 .isEqualTo(Integer.parseInt(bridgeSize));
+    }
+
+
+    @DisplayName("이동 관련 입력시 U 또는 D를 입력하지 않은 경우 실패")
+    @ParameterizedTest
+    @ValueSource(strings = {"A", "1", "x"})
+    void 이동_입력값에_대한_검증_테스트_실패(String movement) {
+        assertThatThrownBy(() -> Validator.validateMoving(movement))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(ExceptionType.MOVE_TYPE_EXCEPTION.getErrorMessage());
+    }
+
+    @DisplayName("이동 관련 입력시 U 또는 D를 입력한 경우 성공")
+    @ParameterizedTest
+    @ValueSource(strings = {"U", "D"})
+    void 이동_입력값에_대한_검증_테스트_성공(String movement) {
+        assertThat(Validator.validateMoving(movement))
+                .isEqualTo(movement);
+    }
+
+    @DisplayName("게임 재시작 관련 입력시 Q 또는 R을 입력하지 않은 경우 실패")
+    @ParameterizedTest
+    @ValueSource(strings = {"A", "1", "x"})
+    void 게임_진행_입력값에_대한_검증_테스트_실패(String gameCommand) {
+        assertThatThrownBy(() -> Validator.validateGameCommand(gameCommand))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(ExceptionType.GAME_COMMAND_EXCEPTION.getErrorMessage());
+    }
+
+    @DisplayName("게임 재시작 관련 입력시 Q 또는 R를 입력한 경우 성공")
+    @ParameterizedTest
+    @ValueSource(strings = {"Q", "R"})
+    void 게임_진행_입력값에_대한_검증_테스트_성공(String gameCommand) {
+        assertThat(Validator.validateGameCommand(gameCommand))
+                .isEqualTo(gameCommand);
     }
 }
