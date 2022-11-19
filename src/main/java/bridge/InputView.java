6c3a5d6ba4a4d3e940f6943cf2a3f078bd40b632
yongseungLeeDec <yonson.dev@gmail.com>
2022-11-19 20:48:56 +0900

    fix(error): 다리입력 받아오기 기능 에러 수정
    InputView의 readBridgeSize가 내부적으로 호출하는 readLine이 입력 스트림에서 아무것도 읽지 못할 경우 NoSuchElementException을 throw 하므로, 이 경우 빈 문자열을 반환하도록 수정함.

--- src/main/java/bridge/InputView.java
+++ src/main/java/bridge/InputView.java
@@ -1,8 +1,11 @@
 package bridge;
 
+import java.util.NoSuchElementException;
+
 import static camp.nextstep.edu.missionutils.Console.readLine;
 
 public class InputView {
+    private static final String EMPTY_STRING = "";
     private static final String TEXT_GAME_START = "다리 건너기 게임을 시작합니다.";
     private static final String PROMPT_FOR_BRIDGE_SIZE = "다리의 길이를 입력해주세요.";
 
@@ -15,7 +18,15 @@ public class InputView {
     }
 
     public String readBridgeSize() {
-        return readLine();
+        String bridgeSizeInput;
+
+        try {
+            bridgeSizeInput = readLine();
+        } catch (NoSuchElementException exception) {
+            bridgeSizeInput = EMPTY_STRING;
+        }
+
+        return bridgeSizeInput;
     }
 
     public String readMoving() {
