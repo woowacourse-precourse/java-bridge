9214a76bbb7fb1f78b2dbb6b6fb3e9e3c93c1c19
ho2n <jungho2n@gmail.com>
2022-11-19 23:15:26 +0900

    refactor(InputViewTest): 테스트 메서드명 수정

--- src/test/java/view/InputViewTest.java
+++ src/test/java/view/InputViewTest.java
@@ -19,6 +19,7 @@ class InputViewTest {
 
     @Nested
     class BridgeLengthInputTest extends NsTest {
+
         @ParameterizedTest
         @ValueSource(strings = {"a,2,3", "b,1,10"})
         void 성공적인_다리의_길이_입력값을_받을때까지_입력받는지_테스트(String input) {
@@ -35,13 +36,21 @@ class InputViewTest {
     class MovingNextInputTest extends NsTest {
         @ParameterizedTest
         @ValueSource(strings = {"u,d,UU,U ,U"})
-        void U_or_D를_건널칸으로_입력값을때까지_입력받는지_테스트(String input) {
+        void U_or_D를_입력받을때까지_입력받는지_테스트(String input) {
             run(input.split(","));
         }
 
         @Override
         public void runMain() {
             inputView.readMoving();
+        }
+    }
+
+    @Nested
+    class CommandInputTest extends NsTest {
+        @Override
+        public void runMain() {
+
         }
     }
 }
