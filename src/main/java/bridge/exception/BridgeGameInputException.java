388ae96a525dbf983e044da7407b6b555f9098a4
kimtaesoo99 <107785279+kimtaesoo99@users.noreply.github.com>
2022-11-19 23:53:28 +0900

    feat: (BridgeGameInputException) 이동할 입력 예외처리 메서드 추가

--- src/main/java/bridge/exception/BridgeGameInputException.java
+++ src/main/java/bridge/exception/BridgeGameInputException.java
@@ -7,5 +7,11 @@ public class BridgeGameInputException {
             throw new IllegalArgumentException("[ERROR] 다리의 길이는 3에서 20사이입니다.");
         }
     }
+
+    public void validateMovingChoice(String move) {
+        if (!(move.equals("U") || move.equals("D"))) {
+            throw new IllegalArgumentException("[ERROR] 이동할 칸은 U또는 D만 입력가능합니다.");
+        }
+    }
 }
 
