1ef2414ff5d513f55a437408a4739ff3758c597c
Hi-Imjaeyoung <imjy2757@naver.com>
2022-11-19 15:26:37 +0900

    feat(bridgeSizeValidationTest) : 사용자로부터 다리 사이즈를 입력의 예외를 처리하는 기능 뼈대구현

---
+++ src/main/java/bridge/model/Validation.java
@@ -0,0 +1,13 @@
+package bridge.model;
+
+public class Validation {
+    public static void bridgeSizeValidationTest(String input) throws IllegalArgumentException{
+        valueInRange(isNumber(input));
+    }
+    private static int isNumber(String input){
+
+    }
+    private static void valueInRange(int input){
+
+    }
+}
