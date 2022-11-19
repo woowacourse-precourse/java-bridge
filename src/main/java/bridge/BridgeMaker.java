76b1090f8e294c5c1e22bd37523ad3cbf9f9e05a
Sunghyun-0 <zkdldk95@naver.com>
2022-11-19 19:20:14 +0900

    makeBridge 기능 작성

--- src/main/java/bridge/BridgeMaker.java
+++ src/main/java/bridge/BridgeMaker.java
@@ -1,10 +1,13 @@
 package bridge;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 /**
  * 다리의 길이를 입력 받아서 다리를 생성해주는 역할을 한다.
  */
+
 public class BridgeMaker {
 
     private final BridgeNumberGenerator bridgeNumberGenerator;
@@ -18,6 +21,14 @@ public class BridgeMaker {
      * @return 입력받은 길이에 해당하는 다리 모양. 위 칸이면 "U", 아래 칸이면 "D"로 표현해야 한다.
      */
     public List<String> makeBridge(int size) {
-        return null;
+        List<String> Bridge = new ArrayList<>();
+        for(int i=0; i<size; i++){
+            int number = bridgeNumberGenerator.generate();
+            Bridge.add(String.valueOf(number));
+        }
+        Collections.replaceAll(Bridge,"0","D");
+        Collections.replaceAll(Bridge,"1","U");
+
+        return Bridge;
     }
 }
