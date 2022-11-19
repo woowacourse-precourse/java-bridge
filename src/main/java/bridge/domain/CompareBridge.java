84c9b424e75223d3f3070f01e49ddd4216c27aa8
ykmxxi <ykmxxi97@gmail.com>
2022-11-19 13:46:31 +0900

    feat: 생성된 다리와 플레이어의 입력을 비교하는 기능 skeleton code 구현

---
+++ src/main/java/bridge/domain/CompareBridge.java
@@ -0,0 +1,19 @@
+package bridge.domain;
+
+import java.util.List;
+
+public class CompareBridge {
+    private Player player;
+    private List<String> bridge;
+
+    public CompareBridge(Player player, List<String> bridge) {
+        this.player = player;
+        this.bridge = bridge;
+    }
+
+    public void isSame() {
+    }
+
+    public void stepping() {
+    }
+}
---
+++ src/main/java/bridge/domain/Player.java
@@ -0,0 +1,11 @@
+package bridge.domain;
+
+import java.util.List;
+
+public class Player {
+    private final List<String> moving;
+
+    public Player(List<String> moving) {
+        this.moving = moving;
+    }
+}
