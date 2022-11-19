875f43482940b11100822c78d21457622de91823
Dongmin Kim <kimdongmin68@naver.com>
2022-11-19 19:36:06 +0900

    fix(bridge): 다리의 길이 입력 시 출력되는 문자열 수정

--- src/main/java/bridge/InputView.java
+++ src/main/java/bridge/InputView.java
@@ -3,7 +3,7 @@ package bridge;
 import camp.nextstep.edu.missionutils.Console;
 
 public class InputView {
-    private static final String INPUT_BRIDGE_SIZE = "다리 건너기 게임을 시작합니다.";
+    private static final String INPUT_BRIDGE_SIZE = "다리의 길이를 입력해주세요.";
 
     public int readBridgeSize() {
         System.out.println(INPUT_BRIDGE_SIZE);
