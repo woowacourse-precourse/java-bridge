ab18b5021243a52f2c8af2d35332498fcf1bde30
hoin <apicad0307@gmail.com>
2022-11-19 15:42:26 +0900

    feat(input/InputString.java): 입력 부분의 문자열 상수화 구현

--- src/main/java/bridge/input/InputView.java
+++ src/main/java/bridge/input/InputView.java
@@ -38,6 +38,10 @@ public class InputView {
      * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
      */
     public String readGameCommand() {
-        return null;
+        String input = Console.readLine();
+        if(input != "R" || input != "Q"){
+            throw new IllegalArgumentException("[ERROR] R 또는 Q를 입력해주세요");
+        }
+        return input;
     }
 }
