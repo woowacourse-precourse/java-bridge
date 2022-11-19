43932fc34f8d2063c8c185d76e2e0ec3ee660ee5
hdsung98 <hdsung98@gmail.com>
2022-11-19 13:54:05 +0900

    test: BridgeTest 생성 및 BridgeGameTest 보완

--- src/test/java/bridge/BridgeGameTest.java
+++ src/test/java/bridge/BridgeGameTest.java
@@ -6,6 +6,7 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import dto.BridgeGameDto;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class BridgeGameTest {
@@ -13,17 +14,21 @@ class BridgeGameTest {
     public static final String VALID_DIRECTION = "유효한 방향은 U 또는 D 입니다.";
 
     @Test
+    @DisplayName("이동 함수 기능테스트")
     void move_기능_테스트() {
         BridgeGame bridgeGame = new BridgeGame();
         bridgeGame.move("U");
         bridgeGame.move("D");
         bridgeGame.move("D");
         BridgeGameDto bridgeGameDto = bridgeGame.sendDto();
-        assertThat(bridgeGameDto.currentPosition).isEqualTo(2);
-        assertThat(bridgeGameDto.totalTrial).isEqualTo(3);
+        assertThat(bridgeGameDto.currentPosition)
+                .isEqualTo(2);
+        assertThat(bridgeGameDto.totalTrial)
+                .isEqualTo(3);
     }
 
     @Test
+    @DisplayName("이동 함수 잘못된 입력 예외처리")
     void move_예외_테스트() {
         assertThatThrownBy(() -> {
             BridgeGame bridgeGame = new BridgeGame();
