890a8e3d37625b5b6604dce87902f27b8d453453
HubCreator <chocomilk8604@gmail.com>
2022-11-19 11:00:12 +0900

    refactor(ResultInformation) : 배열 초기화 로직을 메서드화

--- src/main/java/bridge/domain/ResultInformation.java
+++ src/main/java/bridge/domain/ResultInformation.java
@@ -9,16 +9,13 @@ public class ResultInformation {
     private final char[] upBridge;
     private final char[] downBridge;
     private final int bridgeLength;
-    private int tryCount = 1;
+    private int tryCount = 0;
 
     public ResultInformation(int size) {
         this.bridgeLength = size;
         this.upBridge = new char[size];
         this.downBridge = new char[size];
-        for (int i = 0; i < size; i++) {
-            upBridge[i] = BLANK;
-            downBridge[i] = BLANK;
-        }
+        clear();
     }
 
     void updateUpBridge(Position position, char flag) {
