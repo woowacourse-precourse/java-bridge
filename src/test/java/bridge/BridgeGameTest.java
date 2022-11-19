f9ea5a5fa9193efaa80771cfcd16fe94f1019d20
jinhyeon <vmflwu123@gmail.com>
2022-11-20 01:13:53 +0900

    test(BridgeGameTest): move 메서드 테스트

---
+++ src/test/java/bridge/BridgeGameTest.java
@@ -0,0 +1,37 @@
+package bridge;
+
+import bridge.domain.Bridge;
+import bridge.type.GameStatus;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+class BridgeGameTest {
+
+    static Bridge bridge;
+    static GameStatusOperator gameStatusOperator;
+
+    @BeforeAll
+    static void initBridgeGame() {
+        bridge = new Bridge(List.of("U", "D", "U", "D"));
+        gameStatusOperator = new GameStatusOperator(-1, 0, GameStatus.START);
+    }
+
+    @DisplayName("플레이어가 다리에서 한 칸 이동한다.")
+    @Test
+    void move() {
+        BridgeGame bridgeGame = new BridgeGame(bridge, gameStatusOperator);
+        bridgeGame.move();
+        Integer currentPosition = gameStatusOperator.getCurrentPosition();
+        Integer numberOfTry = gameStatusOperator.getNumberOfTry();
+        assertThat(currentPosition).isEqualTo(0);
+        assertThat(numberOfTry).isEqualTo(1);
+    }
+
+}
