3febd9cd6d906474cb9a53d77fed13bcb1084217
49ehyeon42 <49ehyeon42@gmail.com>
2022-11-19 15:21:13 +0900

    feat: 다리의 길이를 읽고 결과를 반환하는 코드 추가

--- src/main/java/bridge/InputView.java
+++ src/main/java/bridge/InputView.java
@@ -11,7 +11,8 @@ public class InputView {
      * 다리의 길이를 입력받는다.
      */
     public int readBridgeSize() {
-        return 0;
+        System.out.println("다리의 길이를 입력해주세요.");
+        return validateBridgeSize(Console.readLine());
     }
 
     private int validateBridgeSize(String string) {
