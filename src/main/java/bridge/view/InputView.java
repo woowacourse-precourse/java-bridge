759011c93dcf62b3cccdec71bdfcdcec03aa00ad
yujiyea <54650556+yujiyea@users.noreply.github.com>
2022-11-19 20:03:24 +0900

    feat(view/InputView.java): 값이 비어있는지 확인하는 함수 구현

--- src/main/java/bridge/view/InputView.java
+++ src/main/java/bridge/view/InputView.java
@@ -10,4 +10,10 @@ public class InputView {
     public void startMessage(){
         System.out.println(START_MESSAGE);
     }
+
+    private static void checkEmpty(String input){
+        if (input == null || input.isBlank()){
+            throw new IllegalArgumentException(EMPTY_INPUT.toString());
+        }
+    }
 }
