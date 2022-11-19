28095256c96cd03a702c9bb4d8d1dcee4eaebc41
yongseungLeeDec <yonson.dev@gmail.com>
2022-11-19 12:34:54 +0900

    feat: 다리 길이 입력 검증 세부기능 구현 (유효한 숫자 형식 검사)
    BridgeLengthValidator의 isValidNumberFormat 메서드를 구현함.
    - 해당 메서드는 사용자가 '다리 길이'로서 입력한 문자열이 올바른 숫자 형식(부호 없는 10진수 양수 표기 형식)인지 여부를 boolean 타입으로 반환함.

--- src/main/java/bridge/BridgeLengthValidator.java
+++ src/main/java/bridge/BridgeLengthValidator.java
@@ -12,4 +12,15 @@ public class BridgeLengthValidator {
         }
         return true;
     }
+
+    private boolean isValidNumberFormat(String bridgeLengthInput) {
+        assert (hasNoIllegalCharacter(bridgeLengthInput));
+
+        char firstLetter = bridgeLengthInput.charAt(0);
+
+        if (bridgeLengthInput.length() == 1 && firstLetter == '0') {
+            return true;
+        }
+        return firstLetter >= '1' && firstLetter <= '9';
+    }
 }
