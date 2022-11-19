ee35b860adc4105269b4c9b0870ee120b0d39b5c
yummygyudon <bang2brew@gmail.com>
2022-11-19 10:28:24 +0900

    FEAT(ui): OutputView.printMap 구현 및 기본 출력 메서드 추가
    - 추가 메서드
      1. `printMessage()`
        - 매개변수 : message
        : 매개변수로 입력받은 값을 System.out.print로 출력하는 메서드
      2. `printStartMessage()`
        : `printMessage()` 메서드를 사용하여
        Commands Enum의 START_MESSAGE와 EMPTY_LINE 의 message값을 출력하는 메서드
    - `printMap()` 구현 내용
      - 매개변수 추가
        1. upBridge (Player가 진행한 upBridge 형태 String 값)
        2. downBridge (Player가 진행한 downBridge 형태 String 값)
      : `printMessage()`메서드를 사용하여
      upBridge값, downBridge값, Commands.EMPTY_LINE의 message값을 출력하는 메서드

---
+++ src/main/java/bridge/ui/OutputView.java
@@ -0,0 +1,39 @@
+package bridge.ui;
+
+import bridge.domain.PlayerBoard;
+import bridge.util.Commands;
+import bridge.util.Results;
+
+/**
+ * 사용자에게 게임 진행 상황과 결과를 출력하는 역할을 한다.
+ */
+public class OutputView {
+
+    /**
+     * 현재까지 이동한 다리의 상태를 정해진 형식에 맞춰 출력한다.
+     * <p>
+     * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
+     */
+    public void printMap(String upBridge, String downBridge) {
+        printMessage(upBridge);
+        printMessage(downBridge);
+        printMessage(Commands.EMPTY_LINE.message());
+    }
+
+    /**
+     * 게임의 최종 결과를 정해진 형식에 맞춰 출력한다.
+     * <p>
+     * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
+     */
+    public void printResult() {
+    }
+
+    public void printStartMessage(){
+        printMessage(Commands.START_MESSAGE.message());
+        printMessage(Commands.EMPTY_LINE.message());
+    }
+
+    public void printMessage(String message){
+        System.out.print(message);
+    }
+}
