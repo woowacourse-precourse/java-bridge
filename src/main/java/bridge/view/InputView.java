3c15d9f9f47fc9acc475b7cfe87dfab0e665a946
jeomxon <jeomxon@gmail.com>
2022-11-19 22:54:28 +0900

    feat: 입력을 받기 위한 전체 기능 구현

--- src/main/java/bridge/view/InputView.java
+++ src/main/java/bridge/view/InputView.java
@@ -1,4 +1,4 @@
-package bridge;
+package bridge.view;
 
 import camp.nextstep.edu.missionutils.Console;
 
@@ -21,7 +21,7 @@ public class InputView {
      * 사용자가 이동할 칸을 입력받는다.
      */
     public String readMoving() {
-        System.out.println("이동할 칸을 선택해주세요. (위: U, 아래: D");
+        System.out.println("이동할 칸을 선택해주세요. (위: U, 아래: D)");
 
         return Console.readLine();
     }
@@ -30,8 +30,7 @@ public class InputView {
      * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
      */
     public String readGameCommand() {
-        System.out.println();
-        System.out.println("게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q");
+        System.out.println("게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)");
 
         return Console.readLine();
     }
