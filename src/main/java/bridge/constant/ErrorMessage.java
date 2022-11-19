194000a1f97d319e1ba7204ea639494bc7f1b9b8
Jaeyeop-Jung <jjy5349@naver.com>
2022-11-19 16:41:40 +0900

    test: 다리 만들기와 관련된 테스트 추가
    입력한 크기가 게임이 정한 다리 크기 범위 안인지와 원하는 크기만큼 다리가 만들어졌는지 테스트 추가

--- src/main/java/bridge/constant/ErrorMessage.java
+++ src/main/java/bridge/constant/ErrorMessage.java
@@ -2,7 +2,8 @@ package bridge.constant;
 
 public enum ErrorMessage {
     ERROR_MESSAGE_PREFIX("[Error] "),
-    INPUT_FORMAT_IS_INCORRECT("게임이 요구한 입력 형식과 틀립니다");
+    INPUT_FORMAT_IS_INCORRECT("게임이 요구한 입력 형식과 틀립니다"),
+    BRIDGE_SIZE_MUST_BE_FROM_THREE_TO_TWENTY("다리 크기는 3에서 20 사이여야 합니다");
 
     private final String message;
 
