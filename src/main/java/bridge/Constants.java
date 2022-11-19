97b405bae34a88df4e3f7ce0185c0a30dab1f27e
taeyun-work <wmfajr1357@gmail.com>
2022-11-20 03:29:04 +0900

    refactor: Validation에 사용되는 Error메시지를 상수 클래스로 분리

---
+++ src/main/java/bridge/Constants.java
@@ -0,0 +1,11 @@
+package bridge;
+
+public final class Constants {
+    public final class ErrorState {
+        public static final String BRIDGE_SIZE = "[ERROR] 3이상 20이하의 숫자를 입력해 주세요";
+        public static final String DIRECTION = "[ERROR] 위로가려면 U, 아래로 가려면 D를 입력해 주세요";
+        public static final String COMMAND = "[ERROR] 재시도를 하려면 R, 종료하려면 Q를 입력해 주세요";
+
+        private ErrorState() {}
+    }
+}
--- src/main/java/bridge/Validator.java
+++ src/main/java/bridge/Validator.java
@@ -1,24 +1,26 @@
 package bridge;
 
+import bridge.Constants.ErrorState;
+
 public class Validator {
     private Validator() {}
 
     public static void size(String size) {
         String sizeRegex = "^([3-9]{1})$|^(1[0-9]{1})$|^20$";
         if (!size.matches(sizeRegex)) {
-            throw new IllegalArgumentException("[ERROR] 3이상 20이하의 숫자를 입력해 주세요");
+            throw new IllegalArgumentException(ErrorState.BRIDGE_SIZE);
         }
     }
 
     public static void direction(String direction) {
         if (!(direction.equals("U") || direction.equals("D"))) {
-            throw new IllegalArgumentException("[ERROR] 위로가려면 U, 아래로 가려면 D를 입력해 주세요");
+            throw new IllegalArgumentException(ErrorState.DIRECTION);
         }
     }
 
     public static void command(String command) {
         if(!(command.equals("R") || command.equals("Q"))) {
-            throw new IllegalArgumentException("[ERROR] 재시도를 하려면 R, 종료하려면 Q를 입력해 주세요");
+            throw new IllegalArgumentException(ErrorState.COMMAND);
         }
     }
 }
