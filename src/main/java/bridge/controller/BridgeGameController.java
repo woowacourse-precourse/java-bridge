05015e3e9473445560059cea5a6eaf67862b597f
seung-won-kim <hesjs5@skuniv.ac.kr>
2022-11-19 18:31:16 +0900

    feat(BridgeGameController): 다리 생성 기능 구현

--- src/main/java/bridge/controller/BridgeGameController.java
+++ src/main/java/bridge/controller/BridgeGameController.java
@@ -1,21 +1,27 @@
 package bridge.controller;
 
+import bridge.BridgeMaker;
+import bridge.BridgeRandomNumberGenerator;
 import bridge.InputView;
 import bridge.OutputView;
+import java.util.List;
 
 public class BridgeGameController {
 
     private final InputView inputView;
     private final OutputView outputView;
+    private final BridgeMaker bridgeMaker;
 
     public BridgeGameController() {
         this.inputView = new InputView();
         this.outputView = new OutputView();
+        this.bridgeMaker = new BridgeMaker(new BridgeRandomNumberGenerator());
     }
 
     public void gameStart() {
         outputView.printIntroMessage();
 
         int bridgeSize = inputView.readBridgeSize();
+        List<String> bridge = bridgeMaker.makeBridge(bridgeSize);
     }
 }
