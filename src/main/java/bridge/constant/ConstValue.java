87b6fd41a6c1c9085471a9564f850c907859cd35
Kimhyeyun <cqqwer@gmail.com>
2022-11-19 23:59:37 +0900

    Feat: 메세지/상수 값 설정

---
+++ src/main/java/bridge/constant/ConstValue.java
@@ -0,0 +1,12 @@
+package bridge.constant;
+
+public class ConstValue {
+    public static final int BRIDGE_SIZE_MINIMUM = 3;
+    public static final int BRIDGE_SIZE_MAXIMUM = 20;
+
+    public static final String USER_UP_MOVING = "U";
+    public static final String USER_DOWN_MOVING = "D";
+    public static final String RESTART = "R";
+    public static final String QUIT = "Q";
+    public static final String DIGIT_REGEX = "^[\\d]+$";
+}
