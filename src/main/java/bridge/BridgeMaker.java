cb4ebd04c87b0025c0ce449a3d088c95d7ea9544
ParangBird <seekerman@naver.com>
2022-11-20 01:14:57 +0900

    feat: BridgeMaker : 입력받은 int size 크기만큼 랜덤으로 U 혹은 D가 들어간 List 을 만들어야 한다.

--- src/main/java/bridge/BridgeMaker.java
+++ src/main/java/bridge/BridgeMaker.java
@@ -1,5 +1,6 @@
 package bridge;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -8,6 +9,10 @@ import java.util.List;
 public class BridgeMaker {
 
     private final BridgeNumberGenerator bridgeNumberGenerator;
+    private static final int NUMBER_UP = 1;
+    private static final int NUMBER_DOWN = 0;
+    private static final String STRING_UP = "U";
+    private static final String STRING_DOWN = "D";
 
     public BridgeMaker(BridgeNumberGenerator bridgeNumberGenerator) {
         this.bridgeNumberGenerator = bridgeNumberGenerator;
@@ -18,6 +23,19 @@ public class BridgeMaker {
      * @return 입력받은 길이에 해당하는 다리 모양. 위 칸이면 "U", 아래 칸이면 "D"로 표현해야 한다.
      */
     public List<String> makeBridge(int size) {
-        return null;
+        List<String> bridge = new ArrayList<>();
+        for (int i = 0; i < size; i++) {
+            int randomNumber = bridgeNumberGenerator.generate();
+            makeOneBlock(bridge, randomNumber);
+        }
+        return bridge;
+    }
+
+    public void makeOneBlock(List<String> bridge, int randomNumber) {
+        if (randomNumber == NUMBER_DOWN) {
+            bridge.add(STRING_DOWN);
+        } else if (randomNumber == NUMBER_UP) {
+            bridge.add(STRING_UP);
+        }
     }
 }
