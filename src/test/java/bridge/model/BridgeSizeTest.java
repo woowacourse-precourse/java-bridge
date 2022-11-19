2f74a6c8150a3e27182c5f8e8899da41a8efc0a3
junpakPark <112045553+junpakPark@users.noreply.github.com>
2022-11-19 16:36:24 +0900

    test(BridgeSizeTest): 테스트코드 리팩토링

--- src/test/java/bridge/model/BridgeSizeTest.java
+++ src/test/java/bridge/model/BridgeSizeTest.java
@@ -3,70 +3,28 @@ package bridge.model;
 import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import bridge.Error;
 
 class BridgeSizeTest {
 	@DisplayName("3 에서 20 사이 자연수를 입력하면 해당 숫자를 반환한다")
 	@Test
-	void case1() {
+	void inRangeTest() {
 		for (int i = 3; i <= 20; i++) {
 			assertEquals(new BridgeSize(i).getSize(), i);
 		}
 	}
 
 	@DisplayName("3 에서 20 사이의 자연수 외에 정수를 입력하면 에러 메시지를 출력한다")
-	@Nested
-	class RangeTest {
-		@Test
-		void case1() {
-			Throwable exception = assertThrows(IllegalArgumentException.class, () -> {
-				BridgeSize bridgeSize = new BridgeSize(1);
-			});
-			assertEquals(Error.RANGE.getMessage(), exception.getMessage());
-		}
-
-		@Test
-		void case2() {
-			Throwable exception = assertThrows(IllegalArgumentException.class, () -> {
-				BridgeSize bridgeSize = new BridgeSize(2);
-			});
-			assertEquals(Error.RANGE.getMessage(), exception.getMessage());
-		}
-
-		@Test
-		void case3() {
-			Throwable exception = assertThrows(IllegalArgumentException.class, () -> {
-				BridgeSize bridgeSize = new BridgeSize(21);
-			});
-			assertEquals(Error.RANGE.getMessage(), exception.getMessage());
-		}
-
-		@Test
-		void case4() {
-			Throwable exception = assertThrows(IllegalArgumentException.class, () -> {
-				BridgeSize bridgeSize = new BridgeSize(10000);
-			});
-			assertEquals(Error.RANGE.getMessage(), exception.getMessage());
-		}
-
-		@Test
-		void case5() {
-			Throwable exception = assertThrows(IllegalArgumentException.class, () -> {
-				BridgeSize bridgeSize = new BridgeSize(0);
-			});
-			assertEquals(Error.RANGE.getMessage(), exception.getMessage());
-		}
-
-		@Test
-		void case6() {
-			Throwable exception = assertThrows(IllegalArgumentException.class, () -> {
-				BridgeSize bridgeSize = new BridgeSize(-1);
-			});
-			assertEquals(Error.RANGE.getMessage(), exception.getMessage());
-		}
+	@ValueSource(ints = {-1, 0, 1, 2, 21, 10000})
+	@ParameterizedTest
+	void notInRangeTest(int value) {
+		Throwable exception = assertThrows(IllegalArgumentException.class, () -> {
+			BridgeSize bridgeSize = new BridgeSize(value);
+		});
+		assertEquals(Error.RANGE.getMessage(), exception.getMessage());
 	}
-
 }
