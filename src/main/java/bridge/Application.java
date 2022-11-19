ecf66c3631769aaf71b0863d2373c98be5819960
NoseSSG <dmlcks1998@naver.com>
2022-11-20 01:11:32 +0900

    [REFACTOR] BridgeGame 에서 InputView, OutputView를 호출하지 않도록 수정

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -1,10 +1,62 @@
 package bridge;
 
+import java.util.List;
+
 public class Application {
+    static OutputView output = new OutputView();
+    InputView input = new InputView();
+    CheckError check = new CheckError();
 
     public static void main(String[] args) {
         // TODO: 프로그램 구현
         BridgeGame bridge = new BridgeGame();
+
+        output.printStart();
         bridge.start();
     }
+
+    public int firstStep(){
+        output.printFirst();
+        int size;
+        try {
+            size = input.readBridgeSize();
+            check.checkBridgeSize(size);
+        }catch(IllegalArgumentException e){
+            System.out.println(e.getMessage());
+            return firstStep();
+        }
+        return size;
+    }
+    public String secondStep(){
+        String input_temp = "";
+        output.printSecond();
+        try {
+            input_temp = input.readMoving();
+            check.checkChoice(input_temp);
+        }catch (IllegalArgumentException e){
+            System.out.println(e.getMessage());
+            return secondStep();
+        }
+        return input_temp;
+    }
+    public String thirdStep(){
+        String input_temp = "";
+        output.printRetry();
+        try {
+            input_temp = input.readGameCommand();
+            check.checkRetry(input_temp);
+        }catch (IllegalArgumentException e){
+            System.out.println(e.getMessage());
+            return thirdStep();
+        }
+        return input_temp;
+    }
+
+    public String[] printMap(List<String> bridge_input, String choice, boolean same){
+        return output.printMap(bridge_input, choice, same);
+    }
+    public void printResult(String[] Bridge_out,boolean result,int count){
+        output.printResult(Bridge_out,result,count);
+    }
+
 }
--- src/main/java/bridge/BridgeGame.java
+++ src/main/java/bridge/BridgeGame.java
@@ -6,11 +6,7 @@ import java.util.List;
  * 다리 건너기 게임을 관리하는 클래스
  */
 public class BridgeGame {
-    OutputView output = new OutputView();
-    InputView input = new InputView();
-    CheckError check = new CheckError();
-    BridgeRandomNumberGenerator Maker = new BridgeRandomNumberGenerator();
-    BridgeMaker Bridge = new BridgeMaker(Maker);
+    Application app = new Application();
     int size;
     List<String> bridge_answer;
     List<String> bridge_input = new ArrayList<>();
@@ -25,8 +21,10 @@ public class BridgeGame {
     public boolean move(String choice) {
         bridge_input.add(choice);
         int num = bridge_input.size() - 1;
-        boolean same =  bridge_input.get(num).equals(bridge_answer.get(num));
-        Bridge_out =  output.printMap(bridge_input,choice,same);
+        String bridge_now = bridge_answer.get(num);
+        String input_now = bridge_input.get(num);
+        boolean same =  input_now.equals(bridge_now);
+        Bridge_out =  app.printMap(bridge_input,choice,same);
         return same;
     }
 
@@ -36,10 +34,10 @@ public class BridgeGame {
      * 재시작을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
     public void retry() {
-        String retry_choice =  thirdStep();
+        String retry_choice =  app.thirdStep();
         if(retry_choice.equals("R")) clear_global();
         if(retry_choice.equals("Q")){
-            output.printResult(Bridge_out,false,count);
+            app.printResult(Bridge_out,false,count);
         }
     }
     public void clear_global(){
@@ -47,57 +45,23 @@ public class BridgeGame {
         repeat_input();
     }
     public void start(){
-        output.printStart();
-        firstStep();
+        size = app.firstStep();
+        BridgeRandomNumberGenerator Maker = new BridgeRandomNumberGenerator();
+        BridgeMaker Bridge = new BridgeMaker(Maker);
         bridge_answer =  Bridge.makeBridge(size);
         repeat_input();
-    }
-
-    public void firstStep(){
-        output.printFirst();
-        try {
-            size = input.readBridgeSize();
-            check.checkBridgeSize(size);
-        }catch(IllegalArgumentException e){
-            System.out.println(e.getMessage());
-            firstStep();
-        }
-    }
-
-    public String secondStep(){
-        String input_temp = "";
-        output.printSecond();
-        try {
-            input_temp = input.readMoving();
-            check.checkChoice(input_temp);
-        }catch (IllegalArgumentException e){
-            System.out.println(e.getMessage());
-            return secondStep();
-        }
-        return input_temp;
     }
 
     public void repeat_input(){
         count++;
         boolean answer = true;
         for(int i = 0 ; i < size ; i++) {
-            String choice = secondStep();
+            String choice = app.secondStep();
             answer =  move(choice);
             if(answer == false) break;
         }
-        if(answer == true) output.printResult(Bridge_out,true,count);
+        if(answer == true) app.printResult(Bridge_out,true,count);
         if(answer == false) retry();
     }
-    public String thirdStep(){
-        String input_temp = "";
-        output.printRetry();
-        try {
-            input_temp = input.readGameCommand();
-            check.checkRetry(input_temp);
-        }catch (IllegalArgumentException e){
-            System.out.println(e.getMessage());
-            return thirdStep();
-        }
-        return input_temp;
-    }
+
 }
