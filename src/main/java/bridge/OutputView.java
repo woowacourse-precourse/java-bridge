6ccf374da551b909cdf859c09508b557b9299eb9
limzzum <chfhddl1234@naver.com>
2022-11-20 01:30:49 +0900

    refactor : 이동한 다리 출력 수정

--- src/main/java/bridge/OutputView.java
+++ src/main/java/bridge/OutputView.java
@@ -16,54 +16,24 @@ public class OutputView {
      * <p>
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void printMap(List<String> visited,String direction, String result) {
-        upBridge(visited, direction, result);
-        downBridge(visited, direction, result);
-    }
-    public void printMap(List<String> visited) {
-        upBridge(visited, "","");
-        downBridge(visited, "","");
-    }
-    private void upBridge(List<String> visited, String direction, String result) {
+    public void printMap(List<String> results) {
         System.out.println("[");
-        for (String visit : visited) {
-            checkEqual(visit, UP,SUCCESS);
+        for(int i=0; i<results.size(); i++){
+            System.out.println(results.get(i));
+            if(i==results.size()-1){
+                break;
+            }
             System.out.println("|");
-        }
-        if(!result.isEmpty()){
-            checkEqual(direction,UP,result);
         }
         System.out.println("]");
     }
 
-    private void downBridge(List<String> visited, String direction, String result) {
-        System.out.println("[");
-        for (String visit : visited) {
-            checkEqual(visit, DOWN, SUCCESS);
-            System.out.println("|");
-        }
-        if(!result.isEmpty()){
-            checkEqual(direction, DOWN, result);
-        }
-        System.out.println("]");
-    }
-
-    private void checkEqual(String visited,String direction, String result) {
-        if(visited.equals(direction)){
-            System.out.println(result);
-            return;
-        }
-        System.out.println(" ");
-    }
     /**
      * 게임의 최종 결과를 정해진 형식에 맞춰 출력한다.
      * <p>
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void printResult(List<String> visited, String result, int tryNum) {
-        System.out.println("최종 게임 결과");
-        printMap(visited);
-        System.out.println("게임 성공 여부: " + result);
-        System.out.println("총 시도한 횟수: " + tryNum);
+    public void printResult(String result, int tryNum) {
+
     }
 }
