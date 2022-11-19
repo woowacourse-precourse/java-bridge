f143281e40c31b684971ac271737ae1db23acfdf
Nitroblack1 <109022435+Nitroblack1@users.noreply.github.com>
2022-11-19 14:42:22 +0900

    feat(main\...\UI) : implement method that receives bridge length and validate it
    - also adjusted ment of error message for null input and non-number input in constans.ErrorMessages class

--- src/main/java/bridge/UI/InputView.java
+++ src/main/java/bridge/UI/InputView.java
@@ -1,20 +1,39 @@
 package bridge.UI;
 
+import static camp.nextstep.edu.missionutils.Console.readLine;
+
+import bridge.Constants.FrontMan;
+import bridge.Constants.ErrorMessages;
+import java.util.Objects;
+
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
  */
 public class InputView {
 
-    /**
-     * 다리의 길이를 입력받는다.
-     */
-    public int readBridgeSize() {
-        return 0;
+    public int readBridgeLength() {
+        System.out.println(FrontMan.INPUT_BRIDGE_LENGTH);
+        String userInput = readLine();
+        validateBridgeLength(userInput);
+        return Integer.parseInt(userInput);
+    }
+
+    public void validateBridgeLength(String userInput) {
+        if (Objects.equals(userInput, ErrorMessages.NULL) || !userInput.matches(
+                ErrorMessages.ONLY_NUMBER)) {
+            throw new IllegalArgumentException(ErrorMessages.SHOULD_ENTER_NUMBER);
+        }
+        if (Integer.parseInt(userInput) < 3 || Integer.parseInt(userInput) > 20) {
+            throw new IllegalArgumentException(
+                    ErrorMessages.BRIDGE_LENGTH_SHOULD_BE_NUMBER_BETWEEN_3_AND_20);
+        }
     }
 
     /**
      * 사용자가 이동할 칸을 입력받는다.
      */
+
+
     public String readMoving() {
         return null;
     }
