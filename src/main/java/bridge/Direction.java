5ed2ebae9cf9867c71bb705f64ae2da21c46c0ef
bumwoo <gld55602@gmail.com>
2022-11-20 00:44:42 +0900

    feat(Direction): 이동 칸 enum값 생성

---
+++ src/main/java/bridge/Direction.java
@@ -0,0 +1,29 @@
+package bridge;
+
+import java.util.Arrays;
+
+public enum Direction {
+    U(1), D(0);
+    private int directionCode;
+
+    Direction(int directionCode) {
+        this.directionCode = directionCode;
+    }
+
+    public static Direction from(int directionCode) {
+        return Arrays.stream(Direction.values()).filter(direction -> direction.directionCode == directionCode)
+                .findAny().orElseThrow(IllegalStateException::new);
+    }
+
+    public static Direction from(String direction) {
+        return Direction.valueOf(direction);
+    }
+
+    public static boolean isUp(Direction now) {
+        return now.equals(U);
+    }
+
+    public static boolean isDown(Direction now) {
+        return now.equals(D);
+    }
+}
