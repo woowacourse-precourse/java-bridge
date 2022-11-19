ba60bd21b18c9fe4b851328915d55290d15c8f52
bsh1603 <bsh1603@github.com>
2022-11-19 14:28:05 +0900

    Feat: 다리 만들기 동작 확인

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -1,11 +1,17 @@
 package bridge;
 
+import java.util.List;
+
 public class Application {
 
     public static void main(String[] args) {
         // TODO: 프로그램 구현
         BridgeGame bridgeGame = new BridgeGame();
         bridgeGame.gameStart();
-        bridgeGame.requestBridgeSize();
+        int size = bridgeGame.requestBridgeSize();
+        System.out.println(size);
+
+        BridgeMaker bridgeMaker = new BridgeMaker(new BridgeRandomNumberGenerator());
+        List<String> bridge = bridgeMaker.makeBridge(size);
     }
 }
