52d539aff69214d11da9c1adf4bd9e81fb1e9e37
impati <impati0716@icloud.com>
2022-11-19 10:44:12 +0900

    feat(Validation) : notNumberFormat 예외 처리

--- src/main/java/bridge/Validation.java
+++ src/main/java/bridge/Validation.java
@@ -1,5 +1,12 @@
 package bridge;
 
 public class Validation {
-
+    private final String BRIDGE_MESSAGE = "[ERROR] 다리 길이 입력은 3이상 20이하의 숫자를 입력할 수 있습니다.";
+    private void notNumberFormat(String input){
+        try {
+            Integer.parseInt(input);
+        }catch (NumberFormatException e){
+            throw new IllegalArgumentException(BRIDGE_MESSAGE);
+        }
+    }
 }
