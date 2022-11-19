2badca0f893a6332d9460a318eff0b8cab2997fc
woosung1223 <dntjd991223@naver.com>
2022-11-20 00:59:56 +0900

    feat(BridgeSizeVerifier): 브리지 관련 검증 기능 생성

---
+++ src/main/java/bridge/verifier/BridgeSizeVerifier.java
@@ -0,0 +1,39 @@
+package bridge.verifier;
+
+import bridge.system.ExceptionMessage;
+
+import java.math.BigInteger;
+
+public class BridgeSizeVerifier {
+    private final int BRIDGE_SIZE_LOWERBOUND = 3;
+    private final int BRIDGE_SIZE_UPPERBOUND = 20;
+
+    public void check(String target) {
+        checkNumeric(target);
+        checkIntegerRange(target);
+        checkRange(target);
+    }
+
+    public void checkRange(String target) {
+        int bridgeSize = Integer.parseInt(target);
+        if (bridgeSize < BRIDGE_SIZE_LOWERBOUND || bridgeSize > BRIDGE_SIZE_UPPERBOUND) {
+            throw new IllegalArgumentException(ExceptionMessage.OUT_OF_RANGE);
+        }
+    }
+
+    private void checkNumeric(String target) {
+        try {
+            new BigInteger(target);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException(ExceptionMessage.IS_NOT_NUMERIC);
+        }
+    }
+
+    private void checkIntegerRange(String target) {
+        try {
+            Integer.parseInt(target);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException(ExceptionMessage.OUT_OF_TYPE_RANGE);
+        }
+    }
+}
