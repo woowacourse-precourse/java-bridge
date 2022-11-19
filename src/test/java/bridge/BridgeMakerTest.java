b327f38947e94b726be63f357e29515b87c32065
ho2n <jungho2n@gmail.com>
2022-11-19 14:21:19 +0900

    test(BridgeMakerTest): 숫자 0과 1에 대응하는 칸을 정상적으로 만드는지 테스트하는 코드 추가

---
+++ src/test/java/bridge/BridgeMakerTest.java
@@ -0,0 +1,42 @@
+package bridge;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.params.provider.Arguments.arguments;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Stream;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+class BridgeMakerTest {
+    private List<String> bridge;
+
+    @BeforeEach
+    void setUp() {
+        bridge = new ArrayList<>();
+    }
+
+    @ParameterizedTest
+    @MethodSource("returnNumberGenerator")
+    void 숫자_0과1에_대응하는_칸을_추가하는지_테스트(BridgeNumberGenerator generator, String expected) {
+        //given
+        BridgeMaker maker = new BridgeMaker(generator);
+
+        //when
+        maker.appendBridge(bridge);
+
+        //then
+        assertThat(bridge).contains(expected);
+    }
+
+    static Stream<Arguments> returnNumberGenerator() {
+        return Stream.of(
+                arguments(new NumberZeroGenerator(), "D"),
+                arguments(new NumberOneGenerator(), "U")
+        );
+    }
+}
