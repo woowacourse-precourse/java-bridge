e4c77dd75935e27aa53d9b3e152203f08f4874fc
HGB <hsw8147@gmail.com>
2022-11-19 20:39:04 +0900

    feat: Inputview method
    Inputview 메서드 추가

--- src/main/java/bridge/InputView.java
+++ src/main/java/bridge/InputView.java
@@ -1,28 +1,56 @@
 package bridge;
 
+import camp.nextstep.edu.missionutils.Console;
+
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
  */
 public class InputView {
+    public boolean readBridgeSizeValidation(String input) {
+        if(input.matches("(.*)[^0-9](.*)")){
+            return false;
+        }
+        if(Integer.parseInt(input)<3 || Integer.parseInt(input) > 20){
+            return false;
+        }
+        return true;
+    }
+
+    public boolean readMovingValidation(String input){
+        if(input.equals("U") || input.equals("D")){
+            return true;
+        }
+        return false;
+    }
+
+    public boolean readGameCommandValidation(String input){
+        if(input.equals("R") || input.equals("Q")){
+            return true;
+        }
+        return false;
+    }
 
     /**
      * 다리의 길이를 입력받는다.
      */
-    public int readBridgeSize() {
-        return 0;
+    public String readBridgeSize() {
+        String input = Console.readLine();
+        return input;
     }
 
     /**
      * 사용자가 이동할 칸을 입력받는다.
      */
     public String readMoving() {
-        return null;
+        String input = Console.readLine();
+        return input;
     }
 
     /**
      * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
      */
     public String readGameCommand() {
-        return null;
+        String input = Console.readLine();
+        return input;
     }
 }
