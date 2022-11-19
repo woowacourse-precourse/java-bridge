34b69e7f0edc2994501f299922b2abbbed97d82b
Park shinyeong <syngh503@gmail.com>
2022-11-19 16:20:48 +0900

    Feat(GameController): 게임 시작 메소드 구현
    게임 시작 시 BridgeGame을 생성하고, bridgeGame을 bridgeGameService, endGameService에 넘겨주는 beginBridgeGame을 생성하였습니다.

--- src/main/java/bridge/Controller/GameController.java
+++ src/main/java/bridge/Controller/GameController.java
@@ -1,4 +1,28 @@
 package bridge.Controller;
 
+import bridge.Controller.Service.BridgeGameService;
+import bridge.Controller.Service.EndGameService;
+import bridge.Controller.Service.StartGameService;
+import bridge.Domain.BridgeGame;
+import bridge.Domain.GameResult;
+import bridge.Domain.GameResultGenerator;
+
 public class GameController {
+    private StartGameService startGameService;
+    private BridgeGameService bridgeGameService;
+    private EndGameService endGameService;
+    private GameResultGenerator gameResultGenerator;
+    private BridgeGame bridgeGame;
+
+    public GameController() {
+        this.gameResultGenerator = new GameResultGenerator();
+        this.startGameService = new StartGameService(gameResultGenerator);
+    }
+
+
+    public void beginBridgeGame() {
+        this.bridgeGame = startGameService.startGame();
+        this.bridgeGameService = new BridgeGameService(bridgeGame);
+        this.endGameService = new EndGameService(bridgeGame);
+    }
 }
