1617c2a61b963f36b86c49c0575aa51795067398
Yerim lee <103623292+lyrWinterCat@users.noreply.github.com>
2022-11-19 20:48:33 +0900

    feat : (BridgeGame) : User, Bridge를 통해 BridgeGame 클래스의 메서드 구현

--- src/main/java/bridge/domain/BridgeGame.java
+++ src/main/java/bridge/domain/BridgeGame.java
@@ -1,16 +1,31 @@
 package bridge.domain;
 
+import bridge.view.InputView;
+import bridge.view.OutputView;
+
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * 다리 건너기 게임을 관리하는 클래스
  */
 public class BridgeGame {
+
+    private Bridge bridge;
+    private User user;
+
+    public void setBridge(List<String> makeBridge){
+        bridge = new Bridge(makeBridge);
+    }
+
 
     /**
      * 사용자가 칸을 이동할 때 사용하는 메서드
      * <p>
      * 이동을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void move() {
+    public void move(String moveMessage) {
+        user.addUserMoving(moveMessage);
     }
 
     /**
@@ -18,6 +33,9 @@ public class BridgeGame {
      * <p>
      * 재시작을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void retry() {
+    public void retry(String restartMessage) {
+        if(restartMessage.equals("R")){
+            user.clearUserMoving();
+        }
     }
 }
