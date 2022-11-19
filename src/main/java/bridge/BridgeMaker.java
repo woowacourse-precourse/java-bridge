10e90b8b6b22a7fa67b955ce061cf9d060f91e2f
meunrud <y3a0522@gmail.com>
2022-11-19 19:04:13 +0900

    :art: (BridgeMaker) 입력 받은 길이대로 위 아래 다리 생성 메서드 구현

--- src/main/java/bridge/BridgeMaker.java
+++ src/main/java/bridge/BridgeMaker.java
@@ -1,5 +1,6 @@
 package bridge;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -18,6 +19,25 @@ public class BridgeMaker {
      * @return 입력받은 길이에 해당하는 다리 모양. 위 칸이면 "U", 아래 칸이면 "D"로 표현해야 한다.
      */
     public List<String> makeBridge(int size) {
-        return null;
+        List<String> bridge = new ArrayList<>();
+        for(int i = 0; i<size*2; i++){
+            if(upperBridge(i)){
+                bridge.add("U");
+            }
+            if(lowerBridge(i)){
+                bridge.add("D");
+            }
+        }
+
+        return bridge;
     }
+
+    public boolean upperBridge(int index){
+        return index%2 ==0;
+    }
+
+    public boolean lowerBridge(int index){
+        return index ==1 || index%2 ==1;
+    }
+
 }
