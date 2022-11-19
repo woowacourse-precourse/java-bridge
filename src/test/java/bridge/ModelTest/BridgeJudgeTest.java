0716d96508d6f944d34a7ecf7cbed22c54d486b1
5nam <ohnam90@gmail.com>
2022-11-19 10:01:58 +0900

    test(refactor): 사용자 값과 비교하는 방식을 바꿨으므로 테스트 함수 수정

--- src/test/java/bridge/ModelTest/BridgeJudgeTest.java
+++ src/test/java/bridge/ModelTest/BridgeJudgeTest.java
@@ -1,5 +1,6 @@
 package bridge.ModelTest;
 
+import bridge.Model.Bridge;
 import bridge.Model.BridgeJudge;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -14,9 +15,10 @@ public class BridgeJudgeTest {
     @DisplayName("생성된 다리 ArrayList 와 사용자 방향 입력값을 받아서 두 개를 비교하여 같으면 True 반환, 다르면 False 를 반환한다.")
     @Test
     void 방향_입력값과_생성된_다리_비교() {
-        int index = 0;
         String userInput = "U";
-        List<String> bridge = new ArrayList<>(List.of("U","D","D","U","U"));
-        assertThat(bridgeJudge.judgeInput(userInput, bridge, index)).isEqualTo(true);
+        String madeBridge = "U";
+        Bridge bridge = new Bridge();
+        bridgeJudge.judgeInput(userInput, madeBridge, bridge);
+        assertThat(bridge.getUp().get(0)).isEqualTo("O");
     }
 }
