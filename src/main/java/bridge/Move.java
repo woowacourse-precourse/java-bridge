6e9c697bd1e41169a4a9dd30d09ed3b2841177e3
JunTaeINC <juntea3523@naver.com>
2022-11-19 21:21:01 +0900

    feat : (OutputView) addRightOrWrong 기능 구현

--- src/main/java/bridge/Move.java
+++ src/main/java/bridge/Move.java
@@ -2,28 +2,29 @@ package bridge;
 
 import java.util.List;
 
-import static enums.DorU.*;
+import static bridge.enums.DorU.*;
 import static bridge.BridgeGame.move;
+
 public class Move {
 
-    private final String RIGHT = " O |";
-    private final String WRONG = " X |";
-    private final String BLANK = "   |";
+    private final String RIGHT = " O ";
+    private final String WRONG = " X ";
+    private final String BLANK = "   ";
 
 
     public List<String> addRightOrWrong(String computer, String userInput) {
-        if (move(computer,userInput)) {
-            if(userInput.equals(UP.getName())){
-                return List.of(RIGHT , BLANK);
+        if (move(computer, userInput)) {
+            if (userInput.equals(UP.getName())) {
+                return List.of(RIGHT, BLANK);
             }
-            return List.of (BLANK , RIGHT);
+            return List.of(BLANK, RIGHT);
         }
         return wrongUpOrDown(userInput);
     }
 
-    public List<String> wrongUpOrDown(String userInput){
-        if (userInput.equals(UP.getName())){
-            return List.of(WRONG , BLANK);
+    public List<String> wrongUpOrDown(String userInput) {
+        if (userInput.equals(UP.getName())) {
+            return List.of(WRONG, BLANK);
         }
         return List.of(BLANK, WRONG);
     }
