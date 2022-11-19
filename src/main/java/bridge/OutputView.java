03b8e7fce14a75c3513d6c48bf4a853c66a936dc
yg0585 <yg0585@naver.com>
2022-11-19 19:09:49 +0900

    feat: 재시작 문구 출력 추가

--- src/main/java/bridge/OutputView.java
+++ src/main/java/bridge/OutputView.java
@@ -30,6 +30,10 @@ public class OutputView {
         System.out.println("다리 건너기 게임을 시작합니다.");
     }
 
+    public void printRestart() {
+        System.out.println("게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)");
+    }
+
     public void printRequestLenOfBridge() {
         System.out.println("다리의 길이를 입력해주세요.");
     }
