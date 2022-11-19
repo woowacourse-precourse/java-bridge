7d2e769da194a22c8f8a942ec9baf69a16bad508
impati <impati0716@icloud.com>
2022-11-19 10:50:11 +0900

    test(Validation) : 다리 길이 예외 처리 - 3미만

--- src/test/java/bridge/ValidationTest.java
+++ src/test/java/bridge/ValidationTest.java
@@ -26,5 +26,16 @@ class ValidationTest {
         }
     }
 
+    private void bridgeLen(String input){
+        notNumberFormat(input);
+        Integer len = Integer.parseInt(input);
+        if(len < 3 || len > 20) throw new IllegalArgumentException("[ERROR] 다리 길이 입력은 3이상 20이하의 숫자를 입력할 수 있습니다.");
+    }
 
+    @Test
+    @DisplayName("다리 기능 입력에 대한 validation - 3미만")
+    public void 다리길이가_3미만() throws Exception{
+        Assertions.assertThatThrownBy(() ->bridgeLen("2"))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
 }
