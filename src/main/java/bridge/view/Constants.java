e5f3de571095bd8742165befae3047b2655bf8fa
김현재 <41482946+galug@users.noreply.github.com>
2022-11-20 00:03:40 +0900

    feat(constants): 게임을 진행시키기 위한 문자열 상수 모음 추가

---
+++ src/main/java/bridge/view/Constants.java
@@ -0,0 +1,14 @@
+package bridge.view;
+
+public class Constants {
+    public static String START_GAME_OUTPUT = "다리 건너기 게임을 시작합니다.";
+    public static String REQUEST_BRIDGE_SIZE_INPUT = "다리의 길이를 입력해주세요.";
+    public static String SELECT_UP_OR_DOWN_INPUT = "이동할 칸을 선택해주세요. (위: U, 아래: D)";
+    public static String SELECT_RETRY_OR_NOT_INPUT = "게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)";
+    public static String FINAL_GAME_RESULT_OUTPUT = "최종 게임 결과";
+    public static String GAME_SUCCESS_OUTPUT = "게임 성공 여부: 성공";
+    public static String GAME_FAILURE_OUTPUT = "게임 성공 여부: 실패";
+
+    public static String TOTAL_NUMBER_OF_TRY_OUTPUT = "총 시도한 횟수: %d";
+
+}
