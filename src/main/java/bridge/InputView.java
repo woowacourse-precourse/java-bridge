7ddec5fb0ccb02eef28c27fb225b87a4628a75fa
Sungjaeyeong <wodud2587@gmail.com>
2022-11-20 00:29:16 +0900

    feat(bridge): 재시도 / 종료 선택

--- src/main/java/bridge/InputView.java
+++ src/main/java/bridge/InputView.java
@@ -9,9 +9,11 @@ public class InputView {
 
     private static final String ENTER_BRIDGE_SIZE = "다리의 길이를 입력해주세요.";
     private static final String ENTER_SELECT_BLOCK = "이동할 칸을 선택해주세요. (위: U, 아래: D)";
+    private static final String ENTER_RETRY_COMMAND = "게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)";
     private static final String ERROR_MESSAGE_ONLY_NUMBER = "[ERROR] 숫자만 입력해주세요.";
     private static final String ERROR_MESSAGE_SIZE_RANGE = "[ERROR] 3이상 20이하로 입력해주세요.";
     private static final String ERROR_MESSAGE_MOVING_BLOCK = "[ERROR] 올바른 값을 입력해주세요. (위: U, 아래: D)";
+    private static final String ERROR_MESSAGE_RETRY_GAME = "[ERROR] 올바른 값을 입력해주세요. (재시도: R, 종료: Q)";
 
     public int getBridgeSize() {
         OutputView.printMessage(ENTER_BRIDGE_SIZE);
@@ -78,10 +80,31 @@ public class InputView {
         }
     }
 
+    public String getGameCommand() {
+        OutputView.printMessage(ENTER_RETRY_COMMAND);
+        String result = readGameCommand(Console.readLine());
+        while (result == null) {
+            result = readGameCommand(Console.readLine());
+        }
+        return result;
+    }
+
     /**
      * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
      */
-    public String readGameCommand() {
-        return null;
+    public String readGameCommand(String enterValue) {
+        try {
+            isValidRetryCommand(enterValue);
+        } catch (IllegalArgumentException e) {
+            OutputView.printMessage(e.getMessage());
+            return null;
+        }
+        return enterValue;
+    }
+
+    private void isValidRetryCommand(String str) {
+        if (!(str.equals("R") || str.equals("Q"))) {
+            throw new IllegalArgumentException(ERROR_MESSAGE_RETRY_GAME);
+        }
     }
 }
