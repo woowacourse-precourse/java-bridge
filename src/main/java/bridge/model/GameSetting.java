a482ea81cb4e31ea53018e7337d18a7c3ecda491
ysng_ysng <yeasung67@gmail.com>
2022-11-19 19:40:26 +0900

    feat(GameSetting): Add the static final variables
    GameSetting에 대한 코드에 상수를 추가했습니다.

--- src/main/java/bridge/model/GameSetting.java
+++ src/main/java/bridge/model/GameSetting.java
@@ -6,18 +6,19 @@ import bridge.dto.GameResultDto;
 import java.util.List;
 
 public class GameSetting {
-	private static final int FIRST_TRY = 1;
+	public static final int START_STEP = 0;
+	public static final int FIRST_TRY = 1;
 
-	public List<String> setDataForStartGame(int bridgeLength) {
+	public static List<String> setBridgeForStartGame(int bridgeLength) {
 		BridgeMaker bridgeMaker = new BridgeMaker(new BridgeRandomNumberGenerator());
 		return bridgeMaker.makeBridge(bridgeLength);
 	}
 
-	public int setDataForRestartGame() {
-		return FIRST_TRY;
+	public static int setBridgeStepForRestartGame() {
+		return START_STEP;
 	}
 
-	public GameResultDto setDataForEndGame(int totalTry, boolean successFlag) {
+	public static GameResultDto setDataForEndGame(int totalTry, boolean successFlag) {
 		return new GameResultDto(totalTry, successFlag);
 	}
 }
