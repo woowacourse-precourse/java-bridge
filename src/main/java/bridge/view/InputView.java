e521b82874a741cfaa3c446ac51b71843c0df028
이건회 <rg970604@naver.com>
2022-11-19 20:18:53 +0900

    feat(InputView.java) : 게임 재시작 또는 종료 입력

--- src/main/java/bridge/view/InputView.java
+++ src/main/java/bridge/view/InputView.java
@@ -1,5 +1,8 @@
 package bridge.view;
 
+import bridge.util.Validator;
+import camp.nextstep.edu.missionutils.Console;
+
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
  */
@@ -29,6 +32,8 @@ public class InputView {
      * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
      */
     public String readGameCommand() {
-        return null;
+        String input = Console.readLine();
+        Validator.validateGameCommandInput(input);
+        return input;
     }
 }
