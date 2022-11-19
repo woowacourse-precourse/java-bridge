988a47c53cb11a693cfe0a55ae59234ca3ff170c
HyunggiPark <phg9587@naver.com>
2022-11-20 01:11:45 +0900

    feat(BridgeGame): move 메서드 구현

--- src/main/java/bridge/BridgeGame.java
+++ src/main/java/bridge/BridgeGame.java
@@ -35,7 +35,17 @@ public class BridgeGame {
      * <p>
      * 이동을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void move() {
+    public String move(String userMoving) {
+        currentGameResults.add(userMoving);
+        String curBridgeField = currentGameBridge.get(currentGameIndex);
+
+        if (userMoving.equals(curBridgeField)) {
+            currentGameIndex++;
+            return "O";
+        }
+
+        currentGameResults.set(currentGameIndex, "X");
+        return "X";
     }
 
     /**
