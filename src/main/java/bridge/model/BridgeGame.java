da2e4e533da4004da9461adb84bacb6277a82691
Hayoon <hy97@sju.ac.kr>
2022-11-19 16:37:14 +0900

    feat(BridgeGame): 다리 상태 초기화값 호출 메서드 구현

--- src/main/java/bridge/model/BridgeGame.java
+++ src/main/java/bridge/model/BridgeGame.java
@@ -28,7 +28,7 @@ public class BridgeGame {
      * 이동을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
     public boolean move(String moveDirection, List<String> roadMap, Player player) {
-        player.moveStraight();
+        player.moveForward();
         return roadMap.get(Player.currentLocation).equals(moveDirection);
     }
 
@@ -58,6 +58,10 @@ public class BridgeGame {
                 bridge.setDownBridge("X");
             }
         } return checkProcess;
+    }
+
+    public void backUpBridge() {
+        bridge.resetBridge();
     }
 
     public Player getPlayer() {
