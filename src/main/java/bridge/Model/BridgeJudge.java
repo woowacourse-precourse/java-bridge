07b3854c40a90ef58f694ff0156597f0d2d0883f
5nam <ohnam90@gmail.com>
2022-11-19 10:03:14 +0900

    refactor: 사용자 입력 방향과 만들어진 다리 방향을 비교하여 Bridge up, down 에 반영

--- src/main/java/bridge/Model/BridgeJudge.java
+++ src/main/java/bridge/Model/BridgeJudge.java
@@ -1,9 +1,34 @@
 package bridge.Model;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class BridgeJudge {
-    public boolean judgeInput(String userInput, List<String> bridge, int index) {
-        return userInput.equals(bridge.get(index));
+    public void judgeInput(String userInput, String madeBridge, Bridge bridge) {
+        List<String> up = new ArrayList<>();
+        List<String> down = new ArrayList<>();
+        if(userInput.equals(madeBridge)) {
+            if(userInput.equals("U")) {
+                up.add("O");
+                down.add(null);
+            }
+            if(userInput.equals("D")) {
+                down.add("O");
+                up.add(null);
+            }
+        }
+
+        if(!userInput.equals(madeBridge)) {
+            if(userInput.equals("U")) {
+                up.add("X");
+                down.add(null);
+            }
+            if(userInput.equals("D")) {
+                down.add("X");
+                up.add(null);
+            }
+        }
+        bridge.setUp(up);
+        bridge.setDown(down);
     }
 }
