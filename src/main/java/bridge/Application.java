f9f975c7445633254f67ae2a54ca62fe35159c3b
Sungjaeyeong <wodud2587@gmail.com>
2022-11-19 18:19:15 +0900

    feat(bridge): 다리 생성

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -1,5 +1,7 @@
 package bridge;
 
+import java.util.List;
+
 public class Application {
 
     private static final String GAME_START = "다리 건너기 게임을 시작합니다.";
@@ -8,8 +10,10 @@ public class Application {
         // TODO: 프로그램 구현
         OutputView.printMessage(GAME_START);
         InputView inputView = new InputView();
-        inputView.getBridgeSize();
+        int bridgeSize = inputView.getBridgeSize();
 
+        BridgeMaker bridgeMaker = new BridgeMaker(new BridgeRandomNumberGenerator());
+        List<String> stringList = bridgeMaker.makeBridge(bridgeSize);
     }
 
 }
