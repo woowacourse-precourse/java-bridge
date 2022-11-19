3dfe6b2abf06e029c2d1eb4bd8f6e5475cc2cb22
HiiWee <wpdlzhf159@gmail.com>
2022-11-19 21:38:21 +0900

    feat: 다리의 각 칸의 상태를 알기 위한 enum 추가

---
+++ src/main/java/bridge/domain/enums/CrossStatus.java
@@ -0,0 +1,16 @@
+package bridge.domain.enums;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public enum CrossStatus {
+    SUCCESS, FAIL, YET;
+
+    public static List<CrossStatus> createInitializationStatuses(int bridgeSize) {
+        List<CrossStatus> crossStatuses = new ArrayList<>();
+        for (int bridgeIndex = 0; bridgeIndex < bridgeSize; bridgeIndex++) {
+            crossStatuses.add(YET);
+        }
+        return crossStatuses;
+    }
+}
