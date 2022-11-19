ef7a1bba1bc0c9f5578c8d7124da3203ed6f8939
JuneYub <ajsjdlwj0123@gmail.com>
2022-11-19 21:17:19 +0900

    <test> 다리의 다음 칸을 확인하는 테스트

--- src/test/java/bridge/BridgeTest.java
+++ src/test/java/bridge/BridgeTest.java
@@ -1,14 +1,18 @@
 package bridge;
 
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
 import java.util.ArrayList;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class BridgeTest {
+    BridgeNumberGenerator bridgeNumberGenerator = new BridgeRandomNumberGenerator();
+    InputView inputView = new InputView();
+    OutputView outputView = new OutputView();
+    BridgeMaker bridgeMaker = new BridgeMaker(bridgeNumberGenerator);
 
     @DisplayName("입력받은 다리 길이에 맞게 다리 생성")
     @Test
@@ -17,6 +21,30 @@ public class BridgeTest {
         BridgeMaker bridgeMaker = new BridgeMaker(bridgeRandomNumberGenerator);
         assertEquals(bridgeMaker.makeBridge(3).size(), 3);
     }
+
+    @DisplayName("범위를 벗어난 숫자에 대한 예외처리")
+    @Test
+    public void makeIllegalArgumentByOutRange() {
+        int overNum = 30;
+        Assertions.assertThrows(IllegalArgumentException.class, () -> {
+            inputView.checkInRange(overNum);
+        });
+    }
+
+    @DisplayName("다리의 다음 칸을 확인")
+    @Test
+    public void moveBridgeTest() {
+        BridgeGame bridgeGame = new BridgeGame(inputView, bridgeMaker, outputView);
+        List<String> testInput = new ArrayList<>() {{
+                add("U");
+                add("D");
+                add("U");
+            }};
+        bridgeGame.setBridge(testInput);
+        bridgeGame.setBridgeStepCnt(2);
+        assertThat(bridgeGame.move("U")).isTrue();
+    }
+
 
 
 
