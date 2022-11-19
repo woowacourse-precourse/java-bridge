97fdf3009545f38009f015684c58e2ae7cbed659
ksh-beginner <ohk9134@naver.com>
2022-11-19 09:05:49 +0900

    refactor(BridgeGame) : 이동 단계에서 이동할 칸 입력 단계 분리
    refactor(BridgeGame) : 이동 단계에서 이동할 칸 입력 단계 분리
    
    refactor(BridgeGame) : 이동할 칸 입력 단계 추가

--- src/main/java/bridge/controller/BridgeGame.java
+++ src/main/java/bridge/controller/BridgeGame.java
@@ -98,10 +98,16 @@ public class BridgeGame {
 
     private void moveUntilSuccessOrFailure() {
         while (canMove && (isGameSuccess == false)) {
+            inputNextStep();
             move();
             printCurrentBridge();
             judgeGameStatus();
         }
+    }
+
+    public void inputNextStep() {
+        outputView.printInputMovingStepMessage();
+        inputNextStep = inputView.inputMovingStep();
     }
 
     /**
@@ -110,8 +116,6 @@ public class BridgeGame {
      * 이동을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
     public void move() {
-        outputView.printInputMovingStepMessage();
-        inputNextStep = inputView.inputMovingStep();
         canMove = generatedBridge.canMoveNextStep(nextStepIndex, inputNextStep);
         playerBridge.moveNextStep(inputNextStep, canMove);
         nextStepIndex++;
