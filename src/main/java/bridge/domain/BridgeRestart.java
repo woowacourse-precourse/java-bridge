ee8db053d0e78db43236a695784f636441b79846
hyowonhan <wain1719@gmail.com>
2022-11-19 19:59:41 +0900

    refactor: 재시작 여부 메서드 분리

--- src/main/java/bridge/domain/BridgeRestart.java
+++ src/main/java/bridge/domain/BridgeRestart.java
@@ -6,13 +6,25 @@ import bridge.view.OutputView;
 import java.util.List;
 
 public class BridgeRestart {
-    public void bridgeRestart(String playerCommand, List correctBridge) {
-        int quitRetryCommand = 0;
+    int quitRetryCommand = 0;
+    public void bridgeRestartCheck(String playerCommand, List correctBridge) {
         quitRetryCommand = BridgeGame.retry(playerCommand);
         if (quitRetryCommand == 0) {
-            OutputView outputView = new OutputView();
-            outputView.printMap(correctBridge);
+            bridgeReStart(correctBridge);
+        } if(quitRetryCommand == 1) {
+            bridgeQuit();
         }
+    }
+
+    public void bridgeReStart(List correctBridge) {
+        OutputView.tryCount += 1;
+        OutputView outputView = new OutputView();
+        outputView.printMap(correctBridge);
+    }
+
+    public void bridgeQuit() {
+        OutputView.successFail = "실패";
+
     }
 
 }
