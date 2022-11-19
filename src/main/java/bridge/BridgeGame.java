dc6e9b932a429571cdf64a0d2a7cd31c75eba8e4
ym2629 <dydals2629@naver.com>
2022-11-19 19:49:40 +0900

    refactor: seperate User class from BridgeGame class

--- src/main/java/bridge/BridgeGame.java
+++ src/main/java/bridge/BridgeGame.java
@@ -6,14 +6,12 @@ import java.util.List;
  * 다리 건너기 게임을 관리하는 클래스
  */
 public class BridgeGame {
-
-    private User user = new User();
     /**
      * 사용자가 칸을 이동할 때 사용하는 메서드
      * <p>
      * 이동을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void move() {
+    public void move(User user) {
         user.addUserPosition();
     }
 
@@ -22,21 +20,20 @@ public class BridgeGame {
      * <p>
      * 재시작을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void retry() {
+    public void retry(User user) {
         user.initializeUserPosition();
         user.addUserAttempt();
     }
 
-    public boolean checkBridgeValueInUserPosition(String moving, List<String> bridge){
-        int userPosition = user.getUserPosition();
-        if(bridge.get(userPosition) == moving){
+    public boolean checkBridgeValueInUserPosition(User user, String moving, List<String> bridge){
+        if(bridge.get(user.getUserPosition()) == moving){
             return true;
         }
         return false;
     }
 
-    public boolean checkGameIsOver(String moving, int size, List<String> bridge){
-        if(checkBridgeValueInUserPosition(moving,bridge) && user.getUserPosition() == size){
+    public boolean checkGameIsOver(User user, String moving, int size, List<String> bridge){
+        if(checkBridgeValueInUserPosition(user, moving, bridge) && user.getUserPosition() == size){
             return true;
         }
         return false;
