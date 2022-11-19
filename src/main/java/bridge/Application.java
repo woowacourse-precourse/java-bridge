73245cc14740cfd7331ccea93a4f3db07b999790
Koonayoung518 <koony000415@gmail.com>
2022-11-19 22:50:17 +0900

    feat(view): 게임 시작 문구 출력하기 기능 추가

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -1,8 +1,11 @@
 package bridge;
 
+import view.OutputView;
+
 public class Application {
 
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        OutputView outputView = new OutputView();
+        outputView.printStart();
     }
 }
