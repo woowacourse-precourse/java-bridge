485b96119989a9b83659ad3f94fcb7b15461ecc1
iamjooon2 <iamjooon2@gmail.com>
2022-11-19 13:03:19 +0900

    refactor: 시작 메시지와 입력값 생성을 BridgeGame 생성시 받도록 변경

--- src/main/java/bridge/BridgeGame.java
+++ src/main/java/bridge/BridgeGame.java
@@ -6,6 +6,13 @@ package bridge;
 public class BridgeGame {
     OutputView outputView = new OutputView();
     InputView inputView = new InputView();
+    private final int bridgeSize;
+
+    public BridgeGame(){
+        outputView.printMessage(Message.START);
+        outputView.enterLine();
+        bridgeSize = inputView.readBridgeSize();
+    }
 
     /**
      * 사용자가 칸을 이동할 때 사용하는 메서드
@@ -23,9 +30,4 @@ public class BridgeGame {
     public void retry() {
     }
 
-    public void run() {
-        outputView.printMessage(Message.START);
-        outputView.enterLine();
-        int bridgeSize = inputView.readBridgeSize();
-    }
 }
