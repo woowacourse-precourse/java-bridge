ad01b02a593fc6043d3c2c2f44db579c593289f4
hectick <chyeon1223@cau.ac.kr>
2022-11-19 16:03:09 +0900

    feat: 재시작하는 기능 추가

--- src/main/java/bridge/model/BridgeGame.java
+++ src/main/java/bridge/model/BridgeGame.java
@@ -26,10 +26,21 @@ public class BridgeGame {
      * 재시작을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
     public void retry() {
+        updateTryCount();
+        initializeRound();
+        initializeGameStatus();
     }
 
     private void updateTryCount(){
         tryCount++;
+    }
+
+    private void initializeRound(){
+        round = 0;
+    }
+
+    private void initializeGameStatus(){
+        gameStatus = GameStatus.READY;
     }
     
     public void createBridge(int size){
