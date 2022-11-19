4620b09b50c930a0e153310e8d303a5246376086
impati <impati0716@icloud.com>
2022-11-19 10:42:21 +0900

    test : notNumberFormat 예외 처리

---
+++ src/test/java/bridge/ValidationTest.java
@@ -0,0 +1,30 @@
+package bridge;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.text.NumberFormat;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ValidationTest {
+
+
+    @Test
+    @DisplayName("다리 기능 입력에 대한 validation - 숫자 입력이 아닌 경우")
+    public void bridgeLenTest() throws Exception{
+        Assertions.assertThatThrownBy(()->
+            notNumberFormat("숫자가 아님")
+        ).isInstanceOf(IllegalArgumentException.class);
+    }
+    private void notNumberFormat(String input){
+        try {
+            Integer.parseInt(input);
+        }catch (NumberFormatException e){
+            throw new IllegalArgumentException("[ERROR] 다리 길이 입력은 3이상 20이하의 숫자를 입력할 수 있습니다.");
+        }
+    }
+
+
+}
