06a05315cb48883239fecfe03b2d275ca771ff61
bjm7701 <bjm7701@naver.com>
2022-11-19 20:30:59 +0900

    feat : 주 구현부 작성

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -1,8 +1,31 @@
 package bridge;
 
 public class Application {
-
+    private static InputView iv;
+    private static int cnt = 1;
+    public static int choose(BridgeGame bg){
+        if(bg.move() == 0)//틀려서 끝남
+            if(iv.readGameCommand().equals("R")) {
+                bg.retry();
+                cnt++;
+                return 1;
+            } else return 0;
+        if(bg.curidx == bg.curbridge.size()) return 2;
+        return 1; //맞춤
+    }
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        try{
+        iv = new InputView();
+        BridgeGame bg = new BridgeGame(iv.readBridgeSize());
+        int flag = 0;
+        while(true){
+            int c = choose(bg);
+            if(c == 0) {flag = 1; break;}
+            else if(c == 2){flag = 0; break;}
+        }
+        //최종 결과 출력
+        OutputView ov = new OutputView(bg.curidx,bg.curbridge);
+        ov.printResult(flag,cnt);
+        } catch(Exception e) {System.out.println("[ERROR]");}
     }
 }
