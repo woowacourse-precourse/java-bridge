e2cfb3a7a45604764a9d10b78af67d4f7be90585
wormjoo <wormjoo@naver.com>
2022-11-19 16:31:48 +0900

    feat: 플레이어가 이동할 칸을 입력받는 기능 구현

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -3,7 +3,7 @@ package bridge;
 public class Application {
 
     public static void main(String[] args) {
-        GameManager gameManager = new GameManager(new InputView(), new OutputView());
+        GameManager gameManager = new GameManager(new InputView(), new OutputView(), new BridgeRandomNumberGenerator());
         gameManager.start();
     }
 }
--- src/main/java/bridge/GameManager.java
+++ src/main/java/bridge/GameManager.java
@@ -2,20 +2,28 @@ package bridge;
 
 import java.util.List;
 
+/**
+ * 게임을 관리해주는 역할을 한다.
+ */
 public class GameManager {
     InputView inputView;
     OutputView ouputView;
+    BridgeMaker bridgeMaker;
 
 
-    public GameManager(InputView input, OutputView output) {
+    public GameManager(InputView input, OutputView output, BridgeRandomNumberGenerator generator) {
         this.inputView = input;
         this.ouputView = output;
+        this.bridgeMaker = new BridgeMaker(generator);
     }
 
     public void start() {
         ouputView.printGameStart();
         int bridgeSize = inputView.readBridgeSize();
-        BridgeMaker bridgeMaker = new BridgeMaker(new BridgeRandomNumberGenerator());
         List<String> bridge = bridgeMaker.makeBridge(bridgeSize);
+        String moving = "";
+        for (int i = 0; i < bridgeSize; i++) {
+            moving = inputView.readMoving();
+        }
     }
 }
