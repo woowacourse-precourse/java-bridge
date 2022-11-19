1b5f055d93abb5b4889a8ad83d72a4653abbd179
Devtist <yt930310@snu.ac.kr>
2022-11-20 01:36:57 +0900

    refactor: MapRenderer 클래스 리팩터링
    메서드를 분리함으로써 중복을 없애고 가독성을 높였습니다.

--- src/main/java/bridge/MapRenderer.java
+++ src/main/java/bridge/MapRenderer.java
@@ -5,48 +5,104 @@ import java.util.List;
 import bridgeConstant.Cell;
 
 public class MapRenderer {
-	public String render(List<String> subBridge, boolean isCorrectMoving) {
-		StringBuilder upperRow = new StringBuilder();
-		StringBuilder lowerRow = new StringBuilder();
-		upperRow.append("[");
-		lowerRow.append("[");
-		for (int index = 0; index < subBridge.size(); index++) {
-			if (index == subBridge.size() - 1) {
-				if (isCorrectMoving) {
-					if (subBridge.get(index).equals(Cell.upPosition())) {
-						upperRow.append(" O ");
-						lowerRow.append("   ");
-					}
-					if (subBridge.get(index).equals(Cell.downPosition())) {
-						upperRow.append("   ");
-						lowerRow.append(" O ");
-					}
-				}
-				if (!isCorrectMoving) {
-					if (subBridge.get(index).equals(Cell.upPosition())) {
-						upperRow.append("   ");
-						lowerRow.append(" X ");
-					}
-					if (subBridge.get(index).equals(Cell.downPosition())) {
-						upperRow.append(" X ");
-						lowerRow.append("   ");
-					}
-				}
-				break;
-			}
-			if (subBridge.get(index).equals(Cell.upPosition())) {
-				upperRow.append(" O ");
-				lowerRow.append("   ");
-			}
-			if (subBridge.get(index).equals(Cell.downPosition())) {
-				upperRow.append("   ");
-				lowerRow.append(" O ");
-			}
-			upperRow.append("|");
-			lowerRow.append("|");
+	private static final String ENTRANCE = "[";
+	private static final String EXIT = "]";
+	private static final String DIVISION_LINE = "|";
+	private static final String CIRCLE = " O ";
+	private static final String CROSS = " X ";
+	private static final String SPACE = "   ";
+	private static final String LINE_BREAK_CHARACTER = "\n";
+
+	StringBuilder upperRow;
+	StringBuilder lowerRow;
+
+	public String render(List<String> partOfBridge, boolean isCorrectMoving) {
+		init();
+		addEntrance();
+		if (partOfBridge.size() == 1) {
+			return renderWhenSizeIsOne(partOfBridge, isCorrectMoving);
 		}
-		upperRow.append("]");
-		lowerRow.append("]");
-		return upperRow + "\n" + lowerRow;
+		return renderWhenSizeBiggerThanOne(partOfBridge, isCorrectMoving);
+	}
+
+	private String renderWhenSizeIsOne(List<String> partOfBridge, boolean isCorrectMoving) {
+		addLastCell(partOfBridge.get(0), isCorrectMoving);
+		addExit();
+		return upperRow.toString() + LINE_BREAK_CHARACTER + lowerRow.toString();
+	}
+
+	private String renderWhenSizeBiggerThanOne(List<String> partOfBridge, boolean isCorrectMoving) {
+		partOfBridge.subList(0, partOfBridge.size() - 1).forEach(cell -> {
+			addCircle(cell);
+			addDivisionLine();
+		});
+		addLastCell(partOfBridge.get(partOfBridge.size() - 1), isCorrectMoving);
+		addExit();
+		return upperRow.toString() + LINE_BREAK_CHARACTER + lowerRow.toString();
+	}
+
+	private void init() {
+		upperRow = new StringBuilder();
+		lowerRow = new StringBuilder();
+	}
+
+	private void addEntrance() {
+		upperRow.append(ENTRANCE);
+		lowerRow.append(ENTRANCE);
+	}
+
+	private void addExit() {
+		upperRow.append(EXIT);
+		lowerRow.append(EXIT);
+	}
+
+	private void addDivisionLine() {
+		upperRow.append(DIVISION_LINE);
+		lowerRow.append(DIVISION_LINE);
+	}
+
+	private void addLastCell(String cell, boolean isCorrectMoving) {
+		if (isCorrectMoving) {
+			addCircle(cell);
+		}
+		if (!isCorrectMoving) {
+			addCross(cell);
+		}
+	}
+
+	private void addCircle(String cell) {
+		if (cell.equals(Cell.upPosition()))
+			addCircleToUpperRow();
+		if (cell.equals(Cell.downPosition()))
+			addCircleToLowerRow();
+	}
+
+	private void addCross(String cell) {
+		if (cell.equals(Cell.upPosition())) {
+			addCrossToLowerRow();
+		}
+		if (cell.equals(Cell.downPosition())) {
+			addCrossToUpperRow();
+		}
+	}
+
+	private void addCircleToUpperRow() {
+		upperRow.append(CIRCLE);
+		lowerRow.append(SPACE);
+	}
+
+	private void addCircleToLowerRow() {
+		upperRow.append(SPACE);
+		lowerRow.append(CIRCLE);
+	}
+
+	private void addCrossToLowerRow() {
+		upperRow.append(SPACE);
+		lowerRow.append(CROSS);
+	}
+
+	private void addCrossToUpperRow() {
+		upperRow.append(CROSS);
+		lowerRow.append(SPACE);
 	}
 }
