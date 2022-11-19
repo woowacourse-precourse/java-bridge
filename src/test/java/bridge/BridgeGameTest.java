1772b7660e28af10a5e5b76416578fa5d34de44c
K-Diger <kitten.diger@gmail.com>
2022-11-19 17:17:38 +0900

    test(BridgeGame) : 최종 도메인 테스트 추가

--- src/test/java/bridge/BridgeGameTest.java
+++ src/test/java/bridge/BridgeGameTest.java
@@ -15,7 +15,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 class BridgeGameTest {
 
-    private List<String> loggiingResult = new ArrayList<>();
+    private List<String> loggingResult = new ArrayList<>();
     private StringBuilder upMovingLoggingResult = new StringBuilder("[");
     private StringBuilder downMovingLoggingResult = new StringBuilder("[");
 
@@ -59,7 +59,7 @@ class BridgeGameTest {
     @DisplayName("위쪽으로 이동 했고, 그 위치가 다리의 끝자락 일때의 결과값 검증")
     @MethodSource("generateBridgeForUpMoving")
     @ParameterizedTest
-    public void leftBridgeSuccessFinalLogging(List<String> bridge) {
+    public void upBridgeSuccessFinalLogging(List<String> bridge) {
         String moving = "U";
         int index = 2;
         if (checkBridge(moving, bridge.get(index)) && moving.equals("U") && index == bridge.size() - 1) {
@@ -129,5 +129,83 @@ class BridgeGameTest {
         }
         assertThat(upMovingLoggingResult.toString()).isEqualTo("[   ]");
         assertThat(downMovingLoggingResult.toString()).isEqualTo("[ X ]");
+    }
+
+
+    // --------------------------------------------------- //
+
+    public void upBridgeSuccessLoggingForFinalTest(List<String> bridge, String moving, int index) {
+        if (checkBridge(moving, bridge.get(index)) && moving.equals("U") && index < bridge.size() - 1) {
+            upMovingLoggingResult.append(" O |");
+            downMovingLoggingResult.append("   |");
+        }
+    }
+
+    public void upBridgeSuccessFinalLoggingForFinalTest(List<String> bridge, String moving, int index) {
+        if (checkBridge(moving, bridge.get(index)) && moving.equals("U") && index == bridge.size() - 1) {
+            upMovingLoggingResult.append(" O ]");
+            downMovingLoggingResult.append("   ]");
+        }
+    }
+
+    public void downBridgeSuccessLoggingForFinalTest(List<String> bridge, String moving, int index) {
+        if (checkBridge(moving, bridge.get(index)) && moving.equals("D") && index < bridge.size() - 1) {
+            upMovingLoggingResult.append("   |");
+            downMovingLoggingResult.append(" O |");
+        }
+    }
+
+    public void downBridgeSuccessFinalLoggingForFinalTest(List<String> bridge, String moving, int index) {
+        if (checkBridge(moving, bridge.get(index)) && moving.equals("D") && index == bridge.size() - 1) {
+            upMovingLoggingResult.append("   ]");
+            downMovingLoggingResult.append(" O ]");
+        }
+    }
+
+    public void upBridgeFailLoggingForFinalTest(List<String> bridge, String moving, int index) {
+        if (!checkBridge(moving, bridge.get(index)) && moving.equals("U")) {
+            upMovingLoggingResult.append(" X ]");
+            downMovingLoggingResult.append("   ]");
+        }
+    }
+
+    public void downBridgeFailLoggingForFinalTest(List<String> bridge, String moving, int index) {
+        if (!checkBridge(moving, bridge.get(index)) && moving.equals("D")) {
+            upMovingLoggingResult.append("   ]");
+            downMovingLoggingResult.append(" X ]");
+        }
+    }
+
+    public List<String> loggingGameResult(List<String> bridge, String moving, int index) {
+        upBridgeSuccessFinalLoggingForFinalTest(bridge, moving, index);
+        downBridgeSuccessFinalLoggingForFinalTest(bridge, moving, index);
+        upBridgeSuccessLoggingForFinalTest(bridge, moving, index);
+        downBridgeSuccessLoggingForFinalTest(bridge, moving, index);
+        upBridgeFailLoggingForFinalTest(bridge, moving, index);
+        downBridgeFailLoggingForFinalTest(bridge, moving, index);
+        List<String> result = new ArrayList<>();
+        result.add(upMovingLoggingResult.toString());
+        result.add(downMovingLoggingResult.toString());
+        return result;
+    }
+
+    static Stream<Arguments> generateBridgeAndUserMovingForFinalTest() {
+        return Stream.of(
+                Arguments.of(Arrays.asList("D", "D", "D", "U", "U"), Arrays.asList("D", "D", "D", "U", "U"))
+        );
+    }
+
+    @DisplayName("실제 유저의 이동 테스트")
+    @MethodSource(value = {"generateBridgeAndUserMovingForFinalTest"})
+    @ParameterizedTest
+    public void move(List<String> bridge, List<String> moving) {
+        for (int index = 0; index < bridge.size(); index++) {
+            loggingResult = loggingGameResult(bridge, moving.get(index), index);
+        }
+        String upMovingLog = loggingResult.get(0);
+        String downMovingLog = loggingResult.get(1);
+        assertThat(upMovingLog).isEqualTo("[   |   |   | O | O ]");
+        assertThat(downMovingLog).isEqualTo("[ O | O | O |   |   ]");
+
     }
 }
