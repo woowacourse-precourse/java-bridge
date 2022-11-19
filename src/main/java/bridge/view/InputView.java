55016f9185b9f35eded1448f62f90782056e70bc
ostar11 <cjw5877@gmail.com>
2022-11-19 16:54:02 +0900

    feat(InputView): readBridgeSize 구현

--- src/main/java/bridge/view/InputView.java
+++ src/main/java/bridge/view/InputView.java
@@ -1,5 +1,7 @@
 package bridge.view;
 
+import camp.nextstep.edu.missionutils.Console;
+
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
  */
@@ -9,7 +11,14 @@ public class InputView {
      * 다리의 길이를 입력받는다.
      */
     public int readBridgeSize() {
-        return 0;
+        String bridgeSize = Console.readLine();
+        if (bridgeSize.chars().allMatch(Character::isDigit)) {
+            throw new IllegalArgumentException("[ERROR] 다리 길이는 숫자를 입력해야 합니다.");
+        }
+        if (Integer.parseInt(bridgeSize) < 3 || Integer.parseInt(bridgeSize) > 20) {
+            throw new IllegalArgumentException("[ERROR] 다리 길이는 3부터 20 사이의 숫자여야 합니다.");
+        }
+        return Integer.parseInt(bridgeSize);
     }
 
     /**
