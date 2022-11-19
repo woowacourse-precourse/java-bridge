34b24153359bbf8376e9ed6c3547260187d88573
HubCreator <chocomilk8604@gmail.com>
2022-11-19 10:57:56 +0900

    docs : 기능 목록 보완

--- src/main/java/bridge/domain/Bridge.java
+++ src/main/java/bridge/domain/Bridge.java
@@ -6,24 +6,24 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Bridge {
-    private final List<Key> bridge;
+    private final List<Key> boxes; // 여러 개의 박스가 하나의 Bridge를 이루고 있음
 
     public Bridge(List<String> stringBridge) {
-        this.bridge = new ArrayList<>();
+        this.boxes = new ArrayList<>();
         mapBridge(stringBridge);
     }
 
     private void mapBridge(List<String> stringBridge) {
         for (String string : stringBridge) {
-            this.bridge.add(Key.valueOf(string));
+            this.boxes.add(Key.valueOf(string));
         }
     }
 
     public int size() {
-        return bridge.size();
+        return boxes.size();
     }
 
     public Key get(int index) {
-        return bridge.get(index);
+        return boxes.get(index);
     }
 }
