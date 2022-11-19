282748dcf60e47b871e0479d36174339ff147f8e
yun <yshyeon102@gmail.com>
2022-11-19 20:34:14 +0900

    fix(Validator): 검증 통과 시 반환 값이 있도록 수정

--- src/main/java/bridge/validator/Validator.java
+++ src/main/java/bridge/validator/Validator.java
@@ -3,34 +3,38 @@ package bridge.validator;
 import bridge.exception.ExceptionType;
 
 public class Validator {
-    public static void validateGameCommand(String input) {
+    public static String validateGameCommand(String input) {
         if (!"Q".equals(input) && !"R".equals(input)) {
             throw new IllegalArgumentException(ExceptionType.GAME_COMMAND_EXCEPTION.getErrorMessage());
         }
+        return input;
     }
 
-    public static void validateMoving(String input) {
+    public static String validateMoving(String input) {
         if (!"U".equals(input) && !"D".equals(input)) {
             throw new IllegalArgumentException(ExceptionType.MOVE_TYPE_EXCEPTION.getErrorMessage());
         }
+        return input;
     }
 
-    public static void validateBridgeSize(String input) {
-        validateIntegerType(input);
-        validateRange(Integer.parseInt(input));
+    public static int validateBridgeSize(String input) {
+        int bridgeSize = validateIntegerType(input);
+        validateRange(bridgeSize);
+        return bridgeSize;
     }
 
-    private static void validateIntegerType(String input) {
+    private static int validateIntegerType(String input) {
         try {
-            Integer.parseInt(input);
+            return Integer.parseInt(input);
         } catch (Exception e) {
             throw new IllegalArgumentException(ExceptionType.INTEGER_TYPE_EXCEPTION.getErrorMessage());
         }
     }
 
-    private static void validateRange(int num) {
+    private static int validateRange(int num) {
         if (num < 3 || num > 20) {
             throw new IllegalArgumentException(ExceptionType.BRIDGE_RANGE_EXCEPTION.getErrorMessage());
         }
+        return num;
     }
 }
