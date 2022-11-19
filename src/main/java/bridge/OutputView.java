59ac3511d2dfb583d08299a3cf234c11e23901ed
nn98 <jkllhgb@gmail.com>
2022-11-20 03:07:27 +0900

    Feat: 결과 출력 구현

--- src/main/java/bridge/OutputView.java
+++ src/main/java/bridge/OutputView.java
@@ -3,6 +3,7 @@ package bridge;
 import static constant.Values.Message.*;
 
 import constant.Values.BridgeCase;
+import java.util.List;
 
 /**
  * 사용자에게 게임 진행 상황과 결과를 출력하는 역할을 한다.
@@ -26,7 +27,22 @@ public class OutputView {
      * <p>
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void printMap() {
+    public void printMap(BridgeGame bridgeGame) {
+        System.out.println(makeMap(bridgeGame));
+    }
+
+    public String makeMap(BridgeGame bridgeGame) {
+        List<String> bridge = bridgeGame.getBridge();
+        List<String> moves = bridgeGame.getMoves();
+        StringBuffer stringBuffer = new StringBuffer(BRIDGE_OPEN.getMessage());
+        for(int index=0;index<bridge.size();++index) {
+
+        }
+        return "";
+    }
+
+    public void appendUpBridge(boolean equals,String value) {
+
     }
 
     public void printSelectRetry() {
@@ -38,8 +54,8 @@ public class OutputView {
      * <p>
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void printResult() {
-        printMap();
+    public void printResult(BridgeGame bridgeGame) {
+        printMap(bridgeGame);
     }
 
     public void printGameSuccess(BridgeCase bridgeCase) {
