1cdd428e10996c5ad0677b5432f7099ddbf4dd33
E1psycongr00 <39326175+E1psycongr00@users.noreply.github.com>
2022-11-19 12:02:41 +0900

    feat(inputView): view를 상속받아 입력 및 입력 전 보여주는 화면 구현

--- src/main/java/bridge/InputView.java
+++ src/main/java/bridge/InputView.java
@@ -1,28 +1,31 @@
 package bridge;
 
-/**
- * 사용자로부터 입력을 받는 역할을 한다.
- */
-public class InputView {
+import camp.nextstep.edu.missionutils.Console;
 
-    /**
-     * 다리의 길이를 입력받는다.
-     */
+public class InputView extends View {
+
+    private static final String ERROR_NUMBER_FORMAT = "숫자 형태로 입력해주세요.";
+    private static final String REQUEST_BRIDGE_SIZE = "다리의 길이를 입력해주세요.";
+    private static final String REQUEST_MOVING_COMMAND = "이동할 칸을 선택해주세요. (위: U, 아래: D)";
+    private static final String REQUEST_RETRY_COMMAND = "게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)";
+
     public int readBridgeSize() {
-        return 0;
+        print(REQUEST_BRIDGE_SIZE);
+        String input = Console.readLine();
+        try {
+            return Integer.parseInt(input);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException(ERROR_NUMBER_FORMAT);
+        }
     }
 
-    /**
-     * 사용자가 이동할 칸을 입력받는다.
-     */
     public String readMoving() {
-        return null;
+        print(REQUEST_MOVING_COMMAND);
+        return Console.readLine();
     }
 
-    /**
-     * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
-     */
     public String readGameCommand() {
-        return null;
+        print(REQUEST_RETRY_COMMAND);
+        return Console.readLine();
     }
 }
