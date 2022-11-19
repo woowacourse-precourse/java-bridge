98a7668327a856bdafe75115b52171341b314d64
Devtist <yt930310@snu.ac.kr>
2022-11-20 00:09:35 +0900

    refactor: OutputView 리팩터링

--- src/main/java/view/OutputView.java
+++ src/main/java/view/OutputView.java
@@ -7,23 +7,28 @@ public class OutputView {
 
 	/**
 	 * 현재까지 이동한 다리의 상태를 정해진 형식에 맞춰 출력한다.
-	 * <p>
-	 * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
 	 */
 	public void printMap(String map) {
 		System.out.println(map);
-		System.out.println();
+		printEmptyLine();
 	}
 
 	/**
 	 * 게임의 최종 결과를 정해진 형식에 맞춰 출력한다.
-	 * <p>
-	 * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
 	 */
 	public void printResult(String result, boolean isSuccess, int count) {
+		printFinalResultMap(result);
+		printSuccessOrFail(isSuccess);
+		printTotalPlayCount(count);
+	}
+
+	private void printFinalResultMap(String result) {
 		System.out.println(ViewConstant.FINAL_RESULT_MESSAGE);
 		System.out.println(result);
-		System.out.println();
+		printEmptyLine();
+	}
+
+	private void printSuccessOrFail(boolean isSuccess) {
 		System.out.print(ViewConstant.SUCCESS_OR_FAIL_MESSAGE);
 		if (isSuccess) {
 			System.out.println(ViewConstant.SUCCESS);
@@ -31,12 +36,19 @@ public class OutputView {
 		if (!isSuccess) {
 			System.out.println(ViewConstant.FAIL);
 		}
-		System.out.printf(ViewConstant.TOTAL_PLAY_TIME_MESSAGE);
+	}
+
+	private void printTotalPlayCount(int count) {
+		System.out.printf(ViewConstant.TOTAL_PLAY_COUNT_MESSAGE);
 		System.out.println(count);
 	}
 
 	public void printStartMessage() {
 		System.out.println(ViewConstant.GAME_START_MESSAGE);
+		printEmptyLine();
+	}
+
+	private void printEmptyLine() {
 		System.out.println();
 	}
 }
--- src/main/java/view/ViewConstant.java
+++ src/main/java/view/ViewConstant.java
@@ -9,5 +9,5 @@ public class ViewConstant {
 	public static final String SUCCESS_OR_FAIL_MESSAGE = "게임 성공 여부: ";
 	public static final String SUCCESS = "성공";
 	public static final String FAIL = "실패";
-	public static final String TOTAL_PLAY_TIME_MESSAGE = "총 시도한 횟수: ";
+	public static final String TOTAL_PLAY_COUNT_MESSAGE = "총 시도한 횟수: ";
 }
