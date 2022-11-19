5f62df3da15e960efca9f911288d849b9e76c433
HyunggiPark <phg9587@naver.com>
2022-11-20 01:22:16 +0900

    feat(OutputView): 현재까지의 게임 결과를 출력하는 메서드 구현

--- src/main/java/ui/OutputView.java
+++ src/main/java/ui/OutputView.java
@@ -2,6 +2,9 @@ package ui;
 
 import bridge.BridgeGame;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 /**
  * 사용자에게 게임 진행 상황과 결과를 출력하는 역할을 한다.
  */
@@ -31,5 +34,59 @@ public class OutputView {
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
     public void printResult() {
+    }
+
+    public void makeResults(String userMoving, String moveResult) {
+        List<String> gameResults = bridgeGame.getCurrentGameResults();
+        String commonResult = makeCommonResult(gameResults);
+        String upBridgeResult = makeUpBridgeResult(commonResult, moveResult, userMoving);
+        String downBridgeResult = makeDownBridgeResult(commonResult, moveResult, userMoving);
+
+        mapData = new String[]{upBridgeResult, downBridgeResult};
+    }
+
+
+    private String makeCommonResult(List<String> gameResults) {
+        return gameResults.stream().
+                collect(Collectors.joining(" | ", "[ ", " ]"));
+    }
+
+    private String makeUpBridgeResult(String commonResult, String moveResult, String userMoving) {
+        String upBridgeResult = commonResult.replace("U", "O").
+                replace("D", " ");
+
+        upBridgeResult = changeUpBridgeResultToDeleteX(upBridgeResult, moveResult, userMoving);
+
+        return upBridgeResult;
+    }
+
+    private String makeDownBridgeResult(String commonResult, String moveResult, String userMoving) {
+        String downBridgeResult = commonResult.replace("U", " ").
+                replace("D", "O");
+
+        downBridgeResult = changeDownBridgeResultToDeleteX(downBridgeResult, moveResult, userMoving);
+
+        return downBridgeResult;
+    }
+
+    private String changeUpBridgeResultToDeleteX(String upBridgeResult, String moveResult, String userMoving) {
+        if (moveResult.equals("X") && userMoving.equals("D")) {
+            upBridgeResult = upBridgeResult.replace("X", " ");
+        }
+
+        return upBridgeResult;
+    }
+
+    private String changeDownBridgeResultToDeleteX(String downBridgeResult, String moveResult, String userMoving) {
+        if (moveResult.equals("X") && userMoving.equals("U")) {
+            downBridgeResult = downBridgeResult.replace("X", " ");
+        }
+
+        return downBridgeResult;
+    }
+
+    public void printCurrentGameResult(String userMoving, String moveResult) {
+        makeResults(userMoving, moveResult);
+        printMap();
     }
 }
