5348c80d3f9842c3173cfa658865bef9666710d8
yongseungLeeDec <yonson.dev@gmail.com>
2022-11-19 12:38:15 +0900

    feat: 다리 길이 입력 검증코드 반환기능 구현
    BridgeLengthValidator의 validateBridgeLength 메서드를 구현함.
    - 해당 메서드는 내부적으로 입력을 검증하기 위한 세부 기능 메서드를 호출하고, 해당 결과를 바탕으로 호출자에게 입력 검증코드를 BridgeLengthValidationCode 열거형 타입으로 반환함.

--- src/main/java/bridge/BridgeLengthValidator.java
+++ src/main/java/bridge/BridgeLengthValidator.java
@@ -4,6 +4,19 @@ public class BridgeLengthValidator {
     private static final int BRIDGE_LENGTH_LOWER_BOUND_INCLUSIVE = 3;
     private static final int BRIDGE_LENGTH_UPPER_BOUND_INCLUSIVE = 20;
 
+    public BridgeLengthValidationCode validateBridgeLength(String bridgeLengthInput) {
+        if (!hasNoIllegalCharacter(bridgeLengthInput)) {
+            return BridgeLengthValidationCode.ILLEGAL_CHARACTER;
+        }
+        if (!isValidNumberFormat(bridgeLengthInput)) {
+            return BridgeLengthValidationCode.INVALID_NUMBER_FORMAT;
+        }
+        if (!isWithinValidRange(Integer.parseInt(bridgeLengthInput))) {
+            return BridgeLengthValidationCode.NUMBER_OUT_OF_RANGE;
+        }
+        return BridgeLengthValidationCode.VALID;
+    }
+
     private boolean hasNoIllegalCharacter(String bridgeLengthInput) {
         assert (bridgeLengthInput != null);
 
