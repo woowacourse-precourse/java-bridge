e7105fc5f909f916a1dd080f11a59fa0beebd577
sosow0212 <sosow0212@naver.com>
2022-11-19 15:01:23 +0900

    feat : (OutputView) 맵을 출력하기 위한 기능 구현

--- src/main/java/bridge/view/OutputView.java
+++ src/main/java/bridge/view/OutputView.java
@@ -1,13 +1,21 @@
 package bridge.view;
 
+import bridge.domain.Bridge;
+
 /**
  * 사용자에게 게임 진행 상황과 결과를 출력하는 역할을 한다.
  */
 public class OutputView {
+    private StringBuilder upView = new StringBuilder();
+    private StringBuilder downView = new StringBuilder();
+
+    public void initMapView() {
+        upView = new StringBuilder();
+        downView = new StringBuilder();
+    }
 
     public void printGameStart() {
-        System.out.println("다리 건너기 게임을 시작합니다.");
-        System.out.println();
+        System.out.println("다리 건너기 게임을 시작합니다.\n");
     }
 
     public void printRequestBridgeSize() {
@@ -17,13 +25,57 @@ public class OutputView {
     public void printRequestMove() {
         System.out.println("이동할 칸을 선택해주세요. (위: U, 아래: D)");
     }
-    
+
+    public void printRequestRestart() {
+        System.out.println("게임을 다시 시도할지 여부를 입력해주세여.(재시도: R, 종료: Q)");
+    }
+
     /**
      * 현재까지 이동한 다리의 상태를 정해진 형식에 맞춰 출력한다.
      * <p>
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void printMap() {
+    public void printMap(int indexOfNow, String moveDirection, boolean isUserCorrect) {
+        if (indexOfNow >= 1) {
+            upView.append(" | ");
+            downView.append(" | ");
+        }
+        appendDirection(moveDirection, isUserCorrect);
+        printResultMap();
+    }
+
+    public void printResultMap() {
+        System.out.println("[ " + upView + " ]");
+        System.out.println("[ " + downView + " ]");
+    }
+
+    private void appendDirection(String moveDirection, boolean isUserCorrect) {
+        if (moveDirection.equals("U")) {
+            appendUpView(isUserCorrect);
+        }
+        if (moveDirection.equals("D")) {
+            appendDownView(isUserCorrect);
+        }
+    }
+
+    private void appendUpView(boolean isUserCorrect) {
+        if (isUserCorrect) {
+            upView.append("O");
+        }
+        if (!isUserCorrect) {
+            upView.append("X");
+        }
+        downView.append(" ");
+    }
+
+    private void appendDownView(boolean check) {
+        if (check) {
+            downView.append("O");
+        }
+        if (!check) {
+            downView.append("X");
+        }
+        upView.append(" ");
     }
 
     /**
@@ -33,4 +85,5 @@ public class OutputView {
      */
     public void printResult() {
     }
+
 }
