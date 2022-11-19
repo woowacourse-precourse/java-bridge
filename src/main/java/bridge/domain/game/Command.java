b61356af5d4957a9e03ac64ab9cc75bb367cb0fd
semi-cloud <sksmsrkdtpal@naver.com>
2022-11-19 17:11:04 +0900

    feat(Command): 명령어를 두 개 이상 입력하는 예외 처리 구현

--- src/main/java/bridge/domain/game/Command.java
+++ src/main/java/bridge/domain/game/Command.java
@@ -2,8 +2,8 @@ package bridge.domain.game;
 
 public class Command {
 
-    private static final String REG_XP_RETRY_COMMAND= "[RQ]+";
-    private static final int COMMAND_SIZE= 1;
+    private static final String REG_XP_RETRY_COMMAND = "[RQ]+";
+    private static final int COMMAND_SIZE = 1;
 
     private final String command;
 
@@ -25,7 +25,7 @@ public class Command {
 
     private void validateCommandSize(String command) {
         if (command.length() == COMMAND_SIZE) {
-            throw new IllegalArgumentException("[ERROR] 재시작 여부 명령어는 R와 Q 문자만 입력 가능합니다.");
+            throw new IllegalArgumentException("[ERROR] 재시작 여부 명령어는 R와 Q 문자 중 하나만 입력 가능합니다.");
         }
     }
 }
