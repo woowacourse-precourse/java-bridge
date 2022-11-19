8e08504bbb7a3f502e7484518353de61af052038
viyott-lover <jackk0616@gmail.com>
2022-11-20 05:05:16 +0900

    feat(InputVIew): 다리 길이를 입력받는 메소드 구현
    3 ~ 20 사이의 수 인지는 BridgeMaker에서 구현했기 때문에 입력이 숫자인지 유효성 검사만 추가

--- src/main/java/bridge/view/InputView.java
+++ src/main/java/bridge/view/InputView.java
@@ -1,15 +1,29 @@
 package bridge.view;
 
+import bridge.exception.Exception;
+import camp.nextstep.edu.missionutils.Console;
+
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
  */
 public class InputView {
 
+    private static final String enterBridgeSize = "다리의 길이를 입력해주세요.";
+
     /**
      * 다리의 길이를 입력받는다.
      */
     public int readBridgeSize() {
-        return 0;
+        System.out.println(enterBridgeSize);
+        String input = Console.readLine();
+        validateDigit(input);
+        return Integer.parseInt(input);
+    }
+
+    private void validateDigit(String input) {
+        if(!input.matches("[+-]?\\d*(\\.\\d+)?")) {
+            throw Exception.IS_NOT_NUMBER_EXCEPTION.getException();
+        }
     }
 
     /**
