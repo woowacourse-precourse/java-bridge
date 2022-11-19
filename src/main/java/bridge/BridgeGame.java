74d44d894ab32941d5253d951948d0cbacc47a39
limzzum <chfhddl1234@naver.com>
2022-11-19 20:44:14 +0900

    feat : 게임 재시작 여부 입력 함수

--- src/main/java/bridge/BridgeGame.java
+++ src/main/java/bridge/BridgeGame.java
@@ -1,5 +1,7 @@
 package bridge;
 
+import camp.nextstep.edu.missionutils.Console;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -9,6 +11,7 @@ public class BridgeGame {
     List<String> visited = new ArrayList<>();
     BridgeMaker bridgeMaker;
     Bridge bridge;
+    private int tryNum = 0;
 
     public void init(){
         int bridgeLen = inputView.readBridgeSize();
@@ -36,13 +39,21 @@ public class BridgeGame {
         }
         return false;
     }
-
-
     /**
      * 사용자가 게임을 다시 시도할 때 사용하는 메서드
      * <p>
      * 재시작을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void retry() {
+    public boolean retry() {
+        String select = inputView.readGameCommand();
+        if(select.equals("R")){
+            reStartInit();
+            return true;
+        }
+        return false;
+    }
+    public void reStartInit(){
+        visited = new ArrayList<>();
+        tryNum += 1;
     }
 }
