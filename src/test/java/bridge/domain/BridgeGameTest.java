3c53edd57d9e073f478d42dd89f888e0c269d49e
Jaeyeop-Jung <jjy5349@naver.com>
2022-11-19 21:43:15 +0900

    test: 게임 재시작이 제대로 동작하는지 테스트 추가 및 BeforeEach를 통해 중복되는 코드 수정

--- src/test/java/bridge/domain/BridgeGameTest.java
+++ src/test/java/bridge/domain/BridgeGameTest.java
@@ -3,11 +3,23 @@ package bridge.domain;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import java.util.List;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 class BridgeGameTest {
+
+    private BridgeGame bridgeGame;
+    private Bridge bridge;
+    private User user;
+
+    @BeforeEach
+    void init() {
+        bridge = new Bridge(List.of("U", "U", "U"));
+        user = new User();
+        bridgeGame = new BridgeGame(bridge, user);
+    }
 
     @Nested
     @DisplayName("게임이")
@@ -17,12 +29,9 @@ class BridgeGameTest {
         @DisplayName("끝났다면 true 리턴을 테스트")
         void end_then_return_true() {
             // given
-            Bridge bridge = new Bridge(List.of("U", "U", "U"));
-            User user = new User();
             for (int stepForward = 0; stepForward < bridge.getSize(); stepForward++) {
                 user.stepForward();
             }
-            BridgeGame bridgeGame = new BridgeGame(bridge, user);
 
             // when
             boolean end = bridgeGame.isEnd();
@@ -35,15 +44,44 @@ class BridgeGameTest {
         @DisplayName("끝나지 않았다면 false 리턴을 테스트")
         void does_not_end_then_return_false() {
             // given
-            Bridge bridge = new Bridge(List.of("U", "U", "U"));
-            User user = new User();
-            BridgeGame bridgeGame = new BridgeGame(bridge, user);
 
             // when
             boolean end = bridgeGame.isEnd();
 
             // then
             assertThat(end).isFalse();
+        }
+    }
+
+    @Nested
+    @DisplayName("게임 재시작 커맨드가")
+    class If_game_restart_command_is {
+
+        @Test
+        @DisplayName("R이면 true를 반환한다")
+        void R_then_return_true() {
+            // given
+            String command = "R";
+
+            // when
+            Boolean isRetry = bridgeGame.retry(command);
+
+            // then
+            assertThat(isRetry).isTrue();
+
+        }
+
+        @Test
+        @DisplayName("Q면 false를 반환한다")
+        void Q_then_return_false() {
+            // given
+            String command = "Q";
+
+            // when
+            Boolean isRetry = bridgeGame.retry(command);
+
+            // then
+            assertThat(isRetry).isFalse();
         }
     }
 }
