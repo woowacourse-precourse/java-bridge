158516ff7825eb4e9f7432842313d8f5e592f9e1
Ridealist <cjsn92@gmail.com>
2022-11-19 18:24:59 +0900

    feat(InputView): 이동할 칸 입력, 게임 다시 시도 여부 입력 요청 기능 구현

--- src/main/java/bridge/views/InputView.java
+++ src/main/java/bridge/views/InputView.java
@@ -24,13 +24,19 @@ public class InputView {
      * 사용자가 이동할 칸을 입력받는다.
      */
     public String readMoving() {
-        return null;
+        System.out.println("이동할 칸을 선택해주세요. (위: U, 아래: D)");
+        String input = Console.readLine();
+        Validator.isUOrD(input);
+        return input;
     }
 
     /**
      * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
      */
     public String readGameCommand() {
-        return null;
+        System.out.println("게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)");
+        String input = Console.readLine();
+        Validator.isROrQ(input);
+        return input;
     }
 }
