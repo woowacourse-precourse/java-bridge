a3192071c2b19e47e606de137216bd21300ebd4f
masiljangajji <hjk172262@gmail.com>
2022-11-19 12:09:59 +0900

    feat: 다리 건너기 결과 출력 기능 추가

---
+++ src/main/java/view/OutputView.java
@@ -0,0 +1,17 @@
+package view;
+
+import java.util.List;
+
+
+public class OutputView {
+    public void printMap(List<String> list) {
+        for (int i = 0; i < list.size(); i++) {
+            System.out.println(list.get(i));
+        }
+    }
+    public void printResult(List<String> list) {
+        for (int i = 0; i < list.size(); i++) {
+            System.out.println(list.get(i));
+        }
+    }
+}
