e669ac22aac3b6dfe8b7b08b736ded25d5144291
hoin <apicad0307@gmail.com>
2022-11-19 15:44:15 +0900

    feat(input/InputView.java): 에러 문구와 UI로직 상수화

--- src/main/java/bridge/input/InputView.java
+++ src/main/java/bridge/input/InputView.java
@@ -1,6 +1,8 @@
-package bridge;
+package bridge.input;
 
 import camp.nextstep.edu.missionutils.Console;
+
+import static bridge.input.InputString.*;
 
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
@@ -11,14 +13,15 @@ public class InputView {
      * 다리의 길이를 입력받는다.
      */
     public int readBridgeSize() {
+        System.out.println(BRIDGE_LENGTH_INPUT);
         int inputLength;
         try{
             inputLength = Integer.parseInt(Console.readLine());
         }catch (Exception e){
-            throw new IllegalArgumentException("[ERROR] 숫자를 입력해주세요.");
+            throw new IllegalArgumentException(ERROR_NO_NUMBER_INPUT);
         }
         if(inputLength < 3 || inputLength > 20 ) {
-            throw new IllegalArgumentException("[ERROR] 3~20 사이의 수를 입력해주세요.");
+            throw new IllegalArgumentException(ERROR_NOT_IN_RANGE);
         }
         return inputLength;
     }
@@ -27,9 +30,10 @@ public class InputView {
      * 사용자가 이동할 칸을 입력받는다.
      */
     public String readMoving() {
+        System.out.println(MOVE_INPUT);
         String input = Console.readLine();
         if(input != "U" || input != "D"){
-            throw new IllegalArgumentException("[ERROR] U 또는 D를 입력해주세요.");
+            throw new IllegalArgumentException(ERROR_MOVE_INPUT);
         }
         return input;
     }
@@ -38,9 +42,10 @@ public class InputView {
      * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
      */
     public String readGameCommand() {
+        System.out.println(RETRY_INPUT);
         String input = Console.readLine();
         if(input != "R" || input != "Q"){
-            throw new IllegalArgumentException("[ERROR] R 또는 Q를 입력해주세요");
+            throw new IllegalArgumentException(ERROR_RESTART_INPUT);
         }
         return input;
     }
