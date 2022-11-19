63b9c2909cb2203fccb7aa46fc946f30c56f093e
MuSeung <peter0757@nvaer.com>
2022-11-19 14:37:28 +0900

    refactor: OutputViewTest에서 out을 정의하는 것을 따로 뺴서 매 테스트 시작 실행하도록 함

--- src/test/java/bridge/OutputViewTest.java
+++ src/test/java/bridge/OutputViewTest.java
@@ -6,19 +6,23 @@ import java.io.ByteArrayOutputStream;
 import java.io.OutputStream;
 import java.io.PrintStream;
 import java.util.List;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class OutputViewTest {
 
     OutputView outputView = new OutputView();
+    OutputStream out;
+    @BeforeEach
+    void setup(){
+        out = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(out));
+    }
 
     @DisplayName("UDD로 이루어진 다리를 성공적으로 통과한 경우")
     @Test
     void bridgeUDDSuccess() {
-        OutputStream out = new ByteArrayOutputStream();
-        System.setOut(new PrintStream(out));
-
         List<String> answerBridge = List.of("U", "D", "D");
         List<String> bridgeTrack = List.of("U", "D", "D");
 
@@ -28,8 +32,6 @@ public class OutputViewTest {
     @DisplayName("UDD로 이루어진 다리를 UU로 실패한 경우")
     @Test
     void bridgeUDDFailByUU() {
-        OutputStream out = new ByteArrayOutputStream();
-        System.setOut(new PrintStream(out));
         List<String> answerBridge = List.of("U", "D", "D");
         List<String> bridgeTrack = List.of("U", "U");
         outputView.printMap(bridgeTrack, answerBridge);
@@ -38,8 +40,6 @@ public class OutputViewTest {
     @DisplayName("UDD로 이루어진 다리를 2회 시도하여 UDD로 성공한 경우의 결과 출력")
     @Test
     void resultOFBridgeUDDSuccess() {
-        OutputStream out = new ByteArrayOutputStream();
-        System.setOut(new PrintStream(out));
         List<String> answerBridge = List.of("U", "D", "D");
         List<String> bridgeTrack = List.of("U", "D", "D");
         outputView.printResult(bridgeTrack, answerBridge, 2);
@@ -49,8 +49,6 @@ public class OutputViewTest {
     @DisplayName("1회 시도한 UDD로 이루어진 다리를 1회 시도하여 UU로 실패한 경우의 결과 출력")
     @Test
     void resultOFBridgeUDDFailByUU() {
-        OutputStream out = new ByteArrayOutputStream();
-        System.setOut(new PrintStream(out));
         List<String> answerBridge = List.of("U", "D", "D");
         List<String> bridgeTrack = List.of("U", "U");
         outputView.printResult(bridgeTrack, answerBridge, 1);
