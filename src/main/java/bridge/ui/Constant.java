cc1fcd1adfa70a38fd90875a25d02488735bbf66
ddingmin <ddingminyt@gmail.com>
2022-11-20 03:56:09 +0900

    feat(java-bridge): 상수 클래스 구현

---
+++ src/main/java/bridge/ui/Constant.java
@@ -0,0 +1,7 @@
+package bridge.ui;
+
+public class Constant {
+    public static final int GAME_CLEAR = 1;
+    public static final int GAME_RESTART = -1;
+    public static final int GAME_FAIL = 0;
+}
