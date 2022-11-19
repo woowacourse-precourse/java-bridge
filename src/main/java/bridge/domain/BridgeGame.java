40f50c7b6fdcffa5d703983f796cefac31d27b4d
KSH <tkdgns543@gmail.com>
2022-11-20 01:27:15 +0900

    feat(domain): 플레이어의 위치를 통해 그 칸의 이동 성공을 확인, 현 상태 최신화

--- src/main/java/bridge/domain/BridgeGame.java
+++ src/main/java/bridge/domain/BridgeGame.java
@@ -26,7 +26,13 @@ public class BridgeGame {
      * <p>
      * 이동을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void move() {
+    public boolean move(String answer, Player player) {
+        int nextPosition = player.move();
+        boolean result= bridge.get(nextPosition).equals(answer);
+        player.saveLastChoice(answer,result);
+        player.saveHistory();
+
+        return result;
     }
 
     /**
