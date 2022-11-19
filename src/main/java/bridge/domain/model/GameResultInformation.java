23db0d600983469b242366d5704f2ef105d8983e
Sehbeom <j3beom@gmail.com>
2022-11-20 02:18:50 +0900

    feat(model): 게임 결과 정보 모델 기능 구현
    게임 결과 정보 모델 GameResultInformation 추가
    외부 활용에 용이하도록 static 으로 설정

---
+++ src/main/java/bridge/domain/model/GameResultInformation.java
@@ -0,0 +1,24 @@
+package bridge.domain.model;
+
+import bridge.constants.GameResult;
+
+public class GameResultInformation {
+    private static GameResult GAME_RESULT;
+    private static int countOfTry = 0;
+
+    public static void setGameResult(GameResult result) {
+        GAME_RESULT = result;
+    }
+
+    public static void increaseCountOfTry() {
+        countOfTry++;
+    }
+
+    public static GameResult getGameResult() {
+        return GAME_RESULT;
+    }
+
+    public static int getCountOfTry() {
+        return countOfTry;
+    }
+}
