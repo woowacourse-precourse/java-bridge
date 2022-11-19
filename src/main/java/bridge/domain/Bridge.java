0e1eed2163a7b7b1b681b48a20f80e9c8e80c248
Choi-JJunho <junho5336@gmail.com>
2022-11-20 00:24:16 +0900

    refactor(Bridge): change Constructor parameter to int

--- src/main/java/bridge/domain/Bridge.java
+++ src/main/java/bridge/domain/Bridge.java
@@ -1,12 +1,20 @@
 package bridge.domain;
 
+import bridge.BridgeMaker;
+import bridge.BridgeRandomNumberGenerator;
+
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Bridge {
     private final List<Floor> floors;
 
-    Bridge(List<Floor> floors) {
-        this.floors = floors;
+    public Bridge(int size) {
+        BridgeMaker bridgeMaker = new BridgeMaker(new BridgeRandomNumberGenerator());
+        List<String> bridge = bridgeMaker.makeBridge(size);
+        this.floors = bridge.stream()
+                .map(each -> new Floor(each))
+                .collect(Collectors.toList());
     }
 
     public boolean isSafe(int index, String input) {
