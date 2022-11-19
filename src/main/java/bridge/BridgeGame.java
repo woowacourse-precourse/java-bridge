712ef2e74a689a4d6b0fb5f3944dfd59d0eadd7c
impati <impati0716@icloud.com>
2022-11-19 14:57:20 +0900

    feat(BridgeGame) : isComplete 와 isContinue 도입

--- src/main/java/bridge/BridgeGame.java
+++ src/main/java/bridge/BridgeGame.java
@@ -1,10 +1,26 @@
 package bridge;
 
+import java.util.ArrayList;
+
 /**
  * 다리 건너기 게임을 관리하는 클래스
  */
 public class BridgeGame {
-
+    private final Bridge bridge;
+    private CurrentBridge currentBridge;
+    private Integer Count = 0;
+    public BridgeGame(Bridge bridge) {
+        this.bridge = bridge;
+        currentBridge = new CurrentBridge(new ArrayList<>());
+    }
+    public boolean isComplete(){
+        if(currentBridge.getBridgeStatus() == BridgeStatus.COMPLETE)return true;
+        return false;
+    }
+    public boolean isContinue(){
+        if(currentBridge.getBridgeStatus() == BridgeStatus.SUCCESS) return true;
+        else return false;
+    }
     /**
      * 사용자가 칸을 이동할 때 사용하는 메서드
      * <p>
