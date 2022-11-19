ca50b16bbb6a9541b7f987660078cb4ebe5a887f
yongseungLeeDec <yonson.dev@gmail.com>
2022-11-19 14:29:56 +0900

    fix(add): 게임 실행 로직에 다리 길이 입력 받기 및 다리 생성 단계 추가
    BridgeGame의 runGame 메서드에 다리 길이를 입력 받는 단계와, 이에 따라 다리를 생성하는 단계를 진행하는 코드를 추가함.
    - 이를 위한 의존성을 멤버변수로 추가하고, 이 의존성들은 BridgeGame 생성시 생성자 주입되도록 처리함.

--- src/main/java/bridge/BridgeGame.java
+++ src/main/java/bridge/BridgeGame.java
@@ -1,16 +1,22 @@
 package bridge;
 
+import java.util.List;
+
 public class BridgeGame {
     private final InputView inputView;
     private final BridgeLengthValidator bridgeLengthValidator;
+    private final BridgeMaker bridgeMaker;
 
     public BridgeGame() {
         this.inputView = new InputView();
         this.bridgeLengthValidator = new BridgeLengthValidator();
+        this.bridgeMaker = new BridgeMaker(new BridgeRandomNumberGenerator());
     }
 
     public void runGame() {
         notifyGameStart();
+        System.out.println();
+        List<String> bridge = this.bridgeMaker.makeBridge(getBridgeLength());
         System.out.println();
     }
 
