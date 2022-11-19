aec46919b3237c4df751c52a2a755d95c48155a2
donghae <donghae1999@gmail.com>
2022-11-19 22:48:38 +0900

    refactor: Controller 리팩토링

--- src/main/java/bridge/Controller.java
+++ src/main/java/bridge/Controller.java
@@ -5,49 +5,52 @@ import java.util.List;
 public class Controller {
     private BridgeGame bridgeGame;
     private OutputView outputView;
-    private static int tryNumber=1;
+    private static int tryNumber = 1;
     public static List<String> answerBridge;
 
-    public void run(){
+    public void run() {
         System.out.println(MessageView.START_GAME_MESSAGE);
         BridgeMaker bridgeMaker = new BridgeMaker(new BridgeRandomNumberGenerator());
         answerBridge = bridgeMaker.makeBridge(getBridgeSize());
         boolean isSuccess = moveBridgeController();
-        printBridgeResult(isSuccess,tryNumber);
+        printBridgeResult(isSuccess, tryNumber);
     }
 
-    private int getBridgeSize(){
+    private int getBridgeSize() {
         InputView inputView = new InputView();
-        int result=0;
-        System.out.println(MessageView.PRINT_BRIDGE_INPUT_MESSAGE);
-        while(true) {
+        int result = 0;
+        result = getBridgeSizeInput(result, inputView);
+        return result;
+    }
+
+    private int getBridgeSizeInput(int result, InputView inputView) {
+        while (true) {
+            System.out.println(MessageView.PRINT_BRIDGE_INPUT_MESSAGE);
             try {
-                result = inputView.readBridgeSize();
+                return result = inputView.readBridgeSize();
             } catch (IllegalArgumentException e) {
                 System.out.println(e.getMessage());
                 continue;
             }
-            return result;
         }
     }
 
-    private boolean moveBridgeController(){
+    private boolean moveBridgeController() {
         InputView inputView = new InputView();
         boolean isSuccess;
-        while(isSuccess = !moveBridge()){
-            if(!moveBridgeError(inputView))
+        while (isSuccess = !moveBridge()) {
+            if (!moveBridgeInput(inputView))
                 break;
             tryNumber++;
         }
         return isSuccess;
     }
-    private boolean moveBridgeError(InputView inputView){
-        while(true) {
+
+    private boolean moveBridgeInput(InputView inputView) {
+        while (true) {
             System.out.println(MessageView.PRINT_GAME_RESTART_MESSAGE);
             try {
-                if (!bridgeGame.retry(inputView.readGameCommand()))
-                    return false;
-                return true;
+                return bridgeGame.retry(inputView.readGameCommand());
             } catch (IllegalArgumentException e) {
                 System.out.println(e.getMessage());
                 continue;
@@ -55,28 +58,23 @@ public class Controller {
         }
     }
 
-    private boolean moveBridge(){
+    private boolean moveBridge() {
         InputView inputView = new InputView();
         bridgeGame = new BridgeGame();
         outputView = new OutputView();
-        int addNumber=0;
-        while(addNumber++ !=answerBridge.size()) {
-            if(moveBridgePrint(inputView)==true)
-                continue;
-            return false;
+        int addNumber = 0;
+        while (addNumber++ != answerBridge.size()) {
+            if (!moveBridgePrint(inputView) == true)
+                return false;
         }
         return true;
     }
 
-    private boolean moveBridgePrint(InputView inputView){
-        while(true) {
+    private boolean moveBridgePrint(InputView inputView) {
+        while (true) {
             try {
-                System.out.println(MessageView.PRINT_BRIDGE_MOVE_MESSAGE);
-                if (bridgeGame.move(inputView.readMoving())) {
-                    outputView.printMap(bridgeGame.upBridge, bridgeGame.downBridge);
+                if(moveBridgePrintInput(inputView))
                     return true;
-                }
-                outputView.printMap(bridgeGame.upBridge,bridgeGame.downBridge);
                 return false;
             } catch (IllegalArgumentException e) {
                 System.out.println(e.getMessage());
@@ -84,10 +82,19 @@ public class Controller {
             }
         }
     }
+    private boolean moveBridgePrintInput(InputView inputView){
+        System.out.println(MessageView.PRINT_BRIDGE_MOVE_MESSAGE);
+        if (bridgeGame.move(inputView.readMoving())) {
+            outputView.printMap(bridgeGame.upBridge, bridgeGame.downBridge);
+            return true;
+        }
+        outputView.printMap(bridgeGame.upBridge, bridgeGame.downBridge);
+        return false;
+    }
 
-    private void printBridgeResult(boolean isSuccess,int tryNumber){
+    private void printBridgeResult(boolean isSuccess, int tryNumber) {
         System.out.println(MessageView.PRINT_GAME_RESULT);
-        outputView.printMap(bridgeGame.upBridge,bridgeGame.downBridge);
-        outputView.printResult(isSuccess,tryNumber);
+        outputView.printMap(bridgeGame.upBridge, bridgeGame.downBridge);
+        outputView.printResult(isSuccess, tryNumber);
     }
 }
