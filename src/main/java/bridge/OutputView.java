0bbf440b9eebe7b87ce3bf215ae7aaa1d314cd74
JunTaeINC <juntea3523@naver.com>
2022-11-20 01:21:35 +0900

    chore : (OutputView) printResult 문자 포멧팅 형으로 전환

--- src/main/java/bridge/OutputView.java
+++ src/main/java/bridge/OutputView.java
@@ -5,7 +5,6 @@ import java.util.List;
 import static bridge.enums.Sentence.*;
 
 public class OutputView {
-    private final Move move = new Move();
 
     public static void printStartGame() {
         System.out.println(GAME_START.getValue());
@@ -17,15 +16,10 @@ public class OutputView {
         System.out.println(downLine.toString().replace(", ", "|"));
     }
 
-    /**
-     * 게임의 최종 결과를 정해진 형식에 맞춰 출력한다.
-     * <p>
-     * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
-     */
-    public void printResult() {
-        // printf 로 변경예정
+    public void printResult(String result, int tryCount) {
         System.out.println();
-        System.out.println(SUCCESS_OR_FAILURE.getValue());
-        System.out.println(TRY_COUNT.getValue());
+        System.out.printf(SUCCESS_OR_FAILURE.getValue(), result);
+        System.out.println();
+        System.out.printf(TRY_COUNT.getValue(), tryCount);
     }
 }
