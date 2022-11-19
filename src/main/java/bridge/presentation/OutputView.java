44a0409a2f6f0faec0d5439006aa10f704e77974
koo9b9h <90121024+koo9b9h@users.noreply.github.com>
2022-11-19 20:04:38 +0900

    feat : 이동안내에 대한 출력 메시지 추가

--- src/main/java/bridge/presentation/OutputView.java
+++ src/main/java/bridge/presentation/OutputView.java
@@ -1,17 +1,19 @@
 package bridge.presentation;
 
+import java.util.List;
+
 /**
  * 사용자에게 게임 진행 상황과 결과를 출력하는 역할을 한다.
  */
 public class OutputView {
-    private final String guide = "다리 건너기 게임을 시작합니다." + System.lineSeparator() + "다리의 길이를 입력해주세요.";
-
+    private final String startingSentence = "다리 건너기 게임을 시작합니다." + System.lineSeparator() + "다리의 길이를 입력해주세요.";
+    private final String movingSentence = "이동할 칸을 선택해주세요. (위: U, 아래: D)";
     /**
      * 현재까지 이동한 다리의 상태를 정해진 형식에 맞춰 출력한다.
      * <p>
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void printMap() {
+    public void printMap(List<String> userResult) {
     }
 
     /**
@@ -23,6 +25,10 @@ public class OutputView {
     }
 
     public void startGuide(){
-        System.out.println(guide);
+        System.out.println(startingSentence);
     }//startGuide
+
+    public void moveGuide(){
+        System.out.println(movingSentence);
+    }//moveGuide
 }
