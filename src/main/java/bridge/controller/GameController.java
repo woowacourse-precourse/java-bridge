14b9838c65d59513c16ab6032272800a8c6f7108
jkroh1995 <85445649+jkroh1995@users.noreply.github.com>
2022-11-20 04:22:53 +0900

    refactor : 요구사항 충족 및 mvc패턴에 부합한 객체 역할 설정을 위한 메소드 수정

--- src/main/java/bridge/controller/GameController.java
+++ src/main/java/bridge/controller/GameController.java
@@ -2,9 +2,12 @@ package bridge.controller;
 
 import bridge.domain.BridgeMaker;
 import bridge.domain.BridgeGame;
+import bridge.domain.RealTimeBridge;
+import bridge.domain.Result;
 import bridge.BridgeNumberGenerator;
 import bridge.BridgeRandomNumberGenerator;
 import bridge.utils.InputHandler;
+import bridge.view.OutputView;
 import java.util.List;
 
 public class GameController {
@@ -12,10 +15,66 @@ public class GameController {
     private final InputHandler inputHandler = new InputHandler();
     private final BridgeNumberGenerator bridgeNumberGenerator = new BridgeRandomNumberGenerator();
     private final BridgeMaker bridgeMaker = new BridgeMaker(bridgeNumberGenerator);
+    private final RealTimeBridge realTimeBridge = new RealTimeBridge();
+    private final OutputView outputView = new OutputView();
+    private final BridgeGame bridgeGame = new BridgeGame();
+    private final String[][] realTimeMap = realTimeBridge.getMap();
+
+    private boolean isAnswer = true;
+    private int count = 1;
+    private int index = 0;
 
     public void runGame() {
+        outputView.printStartMessage();
         int size = inputHandler.getBridgeSize();
         List<String> bridge = bridgeMaker.makeBridge(size);
-        new BridgeGame().start(size, bridge);
+        while (isAnswer&&index<size) {
+            play(bridge);
+        }
+        String gameResult = new Result().returnResult(isAnswer);
+        outputView.printResult(gameResult, count, realTimeMap);
+    }
+
+    private void play(List<String> bridge) {
+        String userMove = inputHandler.getMoving();
+        if (isCorrect(bridge, userMove)) {
+            return;
+        }
+        isWrong(userMove);
+    }
+
+    private void isWrong(String userMove) {
+        stopPlaying(userMove);
+        String command = inputHandler.getGameCommand();
+        if (bridgeGame.retry(command)) {
+            initialize();
+        }
+    }
+
+    private boolean isCorrect(List<String> bridge, String userMove) {
+        if (bridgeGame.move(userMove, bridge.get(index))) {
+            keepPlaying(userMove);
+            return true;
+        }
+        return false;
+    }
+
+    private void initialize() {
+        count++;
+        realTimeBridge.initialize();
+        index=0;
+        isAnswer=true;
+    }
+
+    private void stopPlaying(String userMove) {
+        realTimeBridge.makeWrongMap(userMove);
+        outputView.printMap(realTimeMap);
+        isAnswer=false;
+    }
+
+    private void keepPlaying(String userMove) {
+        realTimeBridge.makeCorrectMap(userMove);
+        outputView.printMap(realTimeMap);
+        index++;
     }
 }
--- src/main/java/bridge/domain/BridgeGame.java
+++ src/main/java/bridge/domain/BridgeGame.java
@@ -1,34 +1,22 @@
 package bridge.domain;
 
-import bridge.utils.InputHandler;
-import bridge.utils.enums.GameCommand;
-import bridge.utils.enums.MovingResult;
-import bridge.view.OutputView;
-import java.util.List;
+import bridge.domain.enums.GameCommand;
 
 /**
  * 다리 건너기 게임을 관리하는 클래스
  */
 public class BridgeGame {
 
-    private final RealTimeBridge realTimeBridge = new RealTimeBridge();
-    private final OutputView outputView = new OutputView();
-    private final InputHandler inputHandler = new InputHandler();
-    private final String[][] realTimeMap = realTimeBridge.getMap();
-
-    private int count = 0;
-    private boolean isAnswer = true;
-
     /**
      * 사용자가 칸을 이동할 때 사용하는 메서드
      * <p>
      * 이동을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    private String move(String bridgeText, String userMove) {
-        if (!bridgeText.equals(userMove)) {
-            return MovingResult.WRONG.getValue();
+    public boolean move(String userMove, String answer) {
+        if (!answer.equals(userMove)) {
+            return false;
         }
-        return MovingResult.CORRECT.getValue();
+        return true;
     }
 
     /**
@@ -37,55 +25,8 @@ public class BridgeGame {
      * 재시작을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
 
-    private boolean retry() {
-        if (!isAnswer) {
-            String gameCommand = inputHandler.getGameCommand();
-            return gameCommand.equals(GameCommand.RETRY.getValue());
-        }
-        return false;
+    public boolean retry(String command) {
+        return command.equals(GameCommand.RETRY.getValue());
     }
 
-    private void initializeVariables() {
-        isAnswer = true;
-        count++;
-        realTimeBridge.initialize();
-    }
-
-    public void start(int size, List<String> bridge) {
-        outputView.printStartMessage();
-        do {
-            play(size, bridge);
-        } while (retry());
-        outputView.printResult(isAnswer, count, realTimeMap);
-    }
-
-    private void play(int size, List<String> bridge) {
-        initializeVariables();
-        for (int block = 0; block < size; block++) {
-            if (compareInputActual(bridge, block)) {
-                break;
-            }
-        }
-    }
-
-    private boolean compareInputActual(List<String> bridge, int index) {
-        String userMove = inputHandler.getMoving();
-        String moveResult = move(userMove, bridge.get(index));
-        return isWrong(realTimeMap, userMove, moveResult);
-    }
-
-    private boolean isWrong(String[][] realTimeMap, String userMove, String moveResult) {
-        if (moveResult.equals(MovingResult.WRONG.getValue())) {
-            printRealTimeMap(realTimeMap, userMove, moveResult);
-            isAnswer = false;
-            return true;
-        }
-        printRealTimeMap(realTimeMap, userMove, moveResult);
-        return false;
-    }
-
-    private void printRealTimeMap(String[][] realTimeMap, String userMove, String moveResult) {
-        realTimeBridge.makeRealTimeBridge(userMove, moveResult);
-        outputView.printMap(realTimeMap);
-    }
 }
