70668046f5c03d8c6f59d7ff50e94488a2eb6ddb
Yerim lee <103623292+lyrWinterCat@users.noreply.github.com>
2022-11-19 20:47:55 +0900

    feat (User, Bridge) : 유저, 다리 클래스 구현
    유저 클래스 : 유저의 이동 경로를 저장할 수 있는 리스트, 유저의 게임 횟수를 가지고 있음
    다리 클래스 : 정답 다리 경로를 가지고 있음

--- src/main/java/bridge/domain/Bridge.java
+++ src/main/java/bridge/domain/Bridge.java
@@ -1,4 +1,17 @@
 package bridge.domain;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Bridge {
+
+    private final List<String> answerBridge;
+
+    public Bridge(List<String> answerBridge) {
+        this.answerBridge = answerBridge;
+    }
+
+    public List<String> getAnswerBridge() {
+        return answerBridge;
+    }
 }
--- src/main/java/bridge/domain/User.java
+++ src/main/java/bridge/domain/User.java
@@ -1,4 +1,36 @@
 package bridge.domain;
 
+import java.util.ArrayList;
+
 public class User {
+
+    private final ArrayList<String> userMovings = new ArrayList<>();
+    private int count;
+
+    public User() {
+        this.count = 1;
+    }
+
+    public void addUserMoving(String moving) {
+        this.userMovings.add(moving);
+
+    }
+
+    public ArrayList<String> getUserMovings() {
+        return userMovings;
+    }
+
+    public void clearUserMoving() {
+        this.userMovings.clear();
+        addCount();
+    }
+
+    private void addCount() {
+        count++;
+    }
+
+    public int getCount() {
+        return count;
+    }
+
 }
