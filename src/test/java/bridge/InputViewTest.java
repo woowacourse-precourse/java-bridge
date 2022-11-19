de33d5ee5d9eeff2e53144c48497f04b4852c500
JuHyeokParkk <hyeok5596@naver.com>
2022-11-19 14:44:32 +0900

    test: 잘못된 다리 길이 입력에 대한 예외 테스트 코드 작성

--- src/test/java/bridge/InputViewTest.java
+++ src/test/java/bridge/InputViewTest.java
@@ -7,6 +7,7 @@ import org.junit.jupiter.params.provider.ValueSource;
 import java.io.ByteArrayInputStream;
 import java.io.InputStream;
 
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 public class InputViewTest {
@@ -21,5 +22,18 @@ public class InputViewTest {
         int length = inputView.readBridgeSize();
 
         assertThat(length).isEqualTo(Integer.parseInt(input));
+    }
+
+    @DisplayName("3에서 20 사이의 숫자 외에 공백, 한글 등 조건에 맞지 않는 값이 입력되면, 예외가 발생한다.")
+    @ParameterizedTest
+    @ValueSource(strings = {" ", "22222222222222222222222222", "-1", "기", "aa", "2", "\n"})
+    public void 잘못된_다리_길이_입력에_대한_예외_테스트(String input) {
+        InputStream in = new ByteArrayInputStream(input.getBytes());
+        System.setIn(in);
+        InputView inputView = new InputView();
+
+        assertThatThrownBy(() -> inputView.readBridgeSize())
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("[ERROR] 다리 길이는 3부터 20 사이의 숫자여야 합니다.");
     }
 }
