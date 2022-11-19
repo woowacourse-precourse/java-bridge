989bcc6c0995ad523d027f4f2140423015c2a0e8
Devtist <yt930310@snu.ac.kr>
2022-11-20 00:08:48 +0900

    feat: 중복으로 사용되는 문자열 상수 선언

---
+++ src/main/java/bridgeConstant/Constant.java
@@ -0,0 +1,8 @@
+package bridgeConstant;
+
+public class Constant {
+	public static final String RESTART = "R";
+	public static final String QUIT = "Q";
+	public static final String UPPER_POSITION = "U";
+	public static final String LOWER_POSITION = "D";
+}
