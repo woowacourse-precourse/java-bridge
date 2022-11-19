7ab2d922d81d13f200532550c4f6a5db9b7182f5
bnbnac <bnbnac@naver.com>
2022-11-19 17:33:57 +0900

    feat: 입력 받은 다리 길이를 검증한다.

--- src/main/java/bridge/Validator.java
+++ src/main/java/bridge/Validator.java
@@ -1,2 +1,35 @@
-package bridge;public class Validator {
+package bridge;
+
+public class Validator {
+    public static Integer validatedBridgeSize(String input) throws IllegalArgumentException {
+        Integer bridgeSize;
+        try {
+            bridgeSize = isNumber(input);
+            isLargerOrEqual(Constant.MIN_BRIDGE_SIZE, bridgeSize);
+            isSmallerOrEqual(Constant.MAX_BRIDGE_SIZE, bridgeSize);
+            return bridgeSize;
+        } catch (IllegalArgumentException e) {
+            throw new IllegalArgumentException(Constant.BRIDGE_SIZE_ERROR);
+        }
+    }
+
+    public static Integer isNumber(String input) {
+        try {
+            return Integer.valueOf(input);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    public static void isLargerOrEqual(Integer min, Integer input) {
+        if (input < min) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    public static void isSmallerOrEqual(Integer max, Integer input) {
+        if (input > max) {
+            throw new IllegalArgumentException();
+        }
+    }
 }
