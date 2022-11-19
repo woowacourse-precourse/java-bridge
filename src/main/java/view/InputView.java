0f6ae262f8ee68f909b60641f5e919be657a5f02
jeongwle <dldl4568@naver.com>
2022-11-20 02:31:27 +0900

    feat: 재시작 여부 입력받는 기능

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -1,8 +1,10 @@
 package view;
 
+import static bridge.enums.ErrorMessages.GAME_COMMAND_ERROR_MESSAGE;
 import static bridge.enums.ErrorMessages.MOVING_DIRECTION_ERROR_MESSAGE;
 import static bridge.enums.ErrorMessages.BRIDGE_SIZE_ERROR_MESSAGE;
 import static bridge.enums.Regex.BRIDGE_SIZE_REGEX;
+import static bridge.enums.Regex.GAME_COMMAND_REGEX;
 import static bridge.enums.Regex.MOVING_DIRECTION_REGEX;
 import static bridge.enums.Range.STARTING_POINT_INCLUSIVE;
 import static bridge.enums.Range.ENDING_POINT_INCLUSIVE;
@@ -37,7 +39,9 @@ public class InputView {
      * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
      */
     public String readGameCommand() {
-        return null;
+        String command = Console.readLine();
+        validateCommand(command);
+        return command;
     }
 
     private void validateSize(String size) {
@@ -54,13 +58,19 @@ public class InputView {
                 && bridgeSize <= ENDING_POINT_INCLUSIVE.getPoint());
     }
 
-    private int toInt(String input) {
-        return Integer.parseInt(input);
-    }
-
     private void validateDirection(String direction) {
         if (!Pattern.matches(MOVING_DIRECTION_REGEX.toString(), direction)) {
             throw new IllegalArgumentException(MOVING_DIRECTION_ERROR_MESSAGE.toString());
         }
+    }
+
+    private void validateCommand(String command) {
+        if (!Pattern.matches(GAME_COMMAND_REGEX.toString(), command)) {
+            throw new IllegalArgumentException(GAME_COMMAND_ERROR_MESSAGE.toString());
+        }
+    }
+
+    private int toInt(String input) {
+        return Integer.parseInt(input);
     }
 }
