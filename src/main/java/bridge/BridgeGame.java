74ff6af7a03a4873b3d97d3f9a14756954d6314a
donghae <donghae1999@gmail.com>
2022-11-19 23:28:31 +0900

    refactor: BridgeGame 리팩토링

--- src/main/java/bridge/BridgeGame.java
+++ src/main/java/bridge/BridgeGame.java
@@ -16,6 +16,8 @@ public class BridgeGame {
      * 이동을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
     public boolean move(String input) {
+        upBridge = new ArrayList<>();
+        downBridge = new ArrayList<>();
         userBridge.add(input);
         if(!validateMove(userBridge,Controller.answerBridge)){
             return false;
@@ -24,32 +26,35 @@ public class BridgeGame {
     }
 
     private boolean validateMove(List<String> userBridge, List<String> answerBridge){
-        upBridge = new ArrayList<>();
-        downBridge = new ArrayList<>();
         for(int i=0; i<userBridge.size(); i++){
             if(!userBridge.get(i).equals(answerBridge.get(i))){
-                if(userBridge.get(i).equals("U")){
-                    upBridge.add("X");
-                    downBridge.add(" ");
-                }
-                else if(userBridge.get(i).equals("D")){
-                    upBridge.add(" ");
-                    downBridge.add("X");
-                }
+                validateMoveAddX(i);
                 return false;
             }
-            if(userBridge.get(i).equals("U")){
-                upBridge.add("O");
-                downBridge.add(" ");
-            }
-            else if(userBridge.get(i).equals("D")){
-                upBridge.add(" ");
-                downBridge.add("O");
-            }
+            validateMoveAddO(i);
         }
         return true;
     }
-
+    private void validateMoveAddX(int index){
+        if(userBridge.get(index).equals("U")){
+            upBridge.add("X");
+            downBridge.add(" ");
+        }
+        else if(userBridge.get(index).equals("D")){
+            upBridge.add(" ");
+            downBridge.add("X");
+        }
+    }
+    private void validateMoveAddO(int index){
+        if(userBridge.get(index).equals("U")){
+            upBridge.add("O");
+            downBridge.add(" ");
+        }
+        else if(userBridge.get(index).equals("D")){
+            upBridge.add(" ");
+            downBridge.add("O");
+        }
+    }
     /**
      * 사용자가 게임을 다시 시도할 때 사용하는 메서드
      * <p>
