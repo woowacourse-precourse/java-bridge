2b9bd56775488d25e672e38090e0a59cfa6b9150
impati <impati0716@icloud.com>
2022-11-19 10:56:14 +0900

    fix(Validation) : 다리 길이 예외 처리 접근 제어 수정

--- src/main/java/bridge/Validation.java
+++ src/main/java/bridge/Validation.java
@@ -4,7 +4,7 @@ public class Validation {
     private final String BRIDGE_MESSAGE = "[ERROR] 다리 길이 입력은 3이상 20이하의 숫자를 입력할 수 있습니다.";
 
 
-    private void bridgeLen(String input){
+    public void bridgeLen(String input){
         notNumberFormat(input);
         Integer len = Integer.parseInt(input);
         if(len < 3 || len > 20) throw new IllegalArgumentException(BRIDGE_MESSAGE);
