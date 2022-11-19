81eaca0448d7b9e8b29b145f5cc060822e60d511
jini_11 <jieun21124@naver.com>
2022-11-20 00:34:31 +0900

    test: 플레이어가 이동할 칸 검증하는 테스트 케이스 구현

---
+++ src/test/java/bridge/domain/UserBridgeTest.java
@@ -0,0 +1,46 @@
+package bridge.domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+public class UserBridgeTest {
+
+    @DisplayName("플레이어가 올바른 방향을 입력시 정상적으로 동작한다.")
+    @ValueSource(strings = {"U", "D"})
+    @ParameterizedTest
+    void createRightInput(String input) {
+        UserBridge userBridge = new UserBridge(input);
+        assertThat(userBridge.getUserInput()).isEqualTo(input);
+    }
+
+    @DisplayName("플레이어가 널값 입력 시 에러가 발생한다.")
+    @Test
+    void createNullInput() {
+        assertThatThrownBy(() -> new UserBridge(""))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(ErrorMessage.SPACE_TYPE.getMessage());
+    }
+
+    @DisplayName("플레이어가 U와 D 외 문자 입력 시 에러가 발생한다.")
+    @ValueSource(strings = {"A", "u", "d", "UA", "DB"})
+    @ParameterizedTest
+    void createInputNotInRange(String input) {
+        assertThatThrownBy(() -> new UserBridge(input))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(ErrorMessage.SPACE_TYPE.getMessage());
+    }
+
+    @DisplayName("플레이어가 U와 D를 같이 입력 시 에러가 발생한다.")
+    @ValueSource(strings = {"UD", "DU", "DUU"})
+    @ParameterizedTest
+    void createAllInput(String input) {
+        assertThatThrownBy(() -> new UserBridge(input))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(ErrorMessage.SPACE_TYPE.getMessage());
+    }
+}
