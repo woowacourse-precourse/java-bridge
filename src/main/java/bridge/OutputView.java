e3d152bd458aeb7d021cb720c681ae9cc27fd088
impati <impati0716@icloud.com>
2022-11-19 16:56:12 +0900

    feat(OutputView) : 다리 이동 상태 출력

--- src/main/java/bridge/OutputView.java
+++ src/main/java/bridge/OutputView.java
@@ -4,15 +4,26 @@ package bridge;
  * 사용자에게 게임 진행 상황과 결과를 출력하는 역할을 한다.
  */
 public class OutputView {
+    private final String START = "[";
+    private final String DEL ="|";
+    private final String END = "]";
 
-    /**
-     * 현재까지 이동한 다리의 상태를 정해진 형식에 맞춰 출력한다.
-     * <p>
-     * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
-     */
-    public void printMap() {
+    public void printMap(String map) {
+        int len = map.length();
+        System.out.println(printMap(map,0,len / 2));
+        System.out.println(printMap(map,len / 2  ,len));
     }
-
+    private String printMap(String map,int s,int e){
+        StringBuffer builder = new StringBuffer();
+        builder.append(START);
+        for(int i = s;i < e;i++) {
+            builder.append(" " + map.charAt(i) + " ");
+            if(i + 1 != e)
+                builder.append(DEL);
+        }
+        builder.append(END);
+        return builder.toString();
+    }
     /**
      * 게임의 최종 결과를 정해진 형식에 맞춰 출력한다.
      * <p>
