57918d4a30d8dd8e43863a1bfecc594d436353b8
impati <impati0716@icloud.com>
2022-11-19 15:38:52 +0900

    feat(CurrentBridge) :현재 다리 상태 변경

--- src/main/java/bridge/CurrentBridge.java
+++ src/main/java/bridge/CurrentBridge.java
@@ -14,4 +14,16 @@ public class CurrentBridge extends Bridge{
         bridge.add(moveCommand);
     }
 
+    public void changeStatus(Bridge otherBridge){
+        if(isMove(otherBridge) && this.bridgeLength() == otherBridge.bridgeLength())this.bridgeStatus = BridgeStatus.COMPLETE;
+        if(isMove(otherBridge) && this.bridgeLength() != otherBridge.bridgeLength())this.bridgeStatus = BridgeStatus.SUCCESS;
+        if(!isMove(otherBridge))this.bridgeStatus = BridgeStatus.FAIL;
+    }
+
+    private boolean isMove(Bridge otherBridge){
+        int index = this.bridge.size() - 1;
+        if(otherBridge.getBridge(index).equals(this.getBridge(index))) return true;
+        return false;
+    }
+
 }
