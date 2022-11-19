5272bc2fd88c0b07f348a25f71ad5032c01047c7
ChanghyunKim <variey82p@gmail.com>
2022-11-19 19:45:10 +0900

    refactor(Validator) : 메서드 범위를 static으로 변경

--- src/main/java/bridge/Validator.java
+++ src/main/java/bridge/Validator.java
@@ -9,19 +9,19 @@ public class Validator {
     private static final int BRIDGE_SIZE_LOWER_INCLUSIVE = 3;
     private static final int BRIDGE_SIZE_UPPER_INCLUSIVE = 20;
 
-    public void validateBridgeInput(String input){
+    public static void validateBridgeInput(String input){
         validateNumeric(input);
         validateBridgeSize(input);
     }
 
-    public void validateNumeric(String input){
+    public static void validateNumeric(String input){
         String numericPattern = "^[0-9]*$";
         if(!Pattern.matches(numericPattern, input)){
             throw new IllegalArgumentException(ERROR_MESSAGE + "숫자 형태가 아닙니다");
         }
     }
 
-    public void validateBridgeSize(String input){
+    public static void validateBridgeSize(String input){
         int bridgeSize = Integer.parseInt(input);
         if(bridgeSize < BRIDGE_SIZE_LOWER_INCLUSIVE || BRIDGE_SIZE_LOWER_INCLUSIVE > BRIDGE_SIZE_UPPER_INCLUSIVE){
             throw new IllegalArgumentException(ERROR_MESSAGE + "다리 길이는 3부터 20 사이의 숫자여야 합니다.");
