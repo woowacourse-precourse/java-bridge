938a87ef8282161be826a468294f59101bfb621d
lecocococo <chemwang13@naver.com>
2022-11-19 20:51:53 +0900

    feat(BridgeGame): 플레이어가 게임을 다시 시도할때 플레이어의 다리 결과를 리셋하는 기능 추가
    - 정답인 다리는 그대로 놔두고 플레이어만 다시 시작함으로 플레이어의 결과를 리셋함

--- src/main/java/bridge/domain/BridgeGame.java
+++ src/main/java/bridge/domain/BridgeGame.java
@@ -1,12 +1,12 @@
 package bridge.domain;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
  * 다리 건너기 게임을 관리하는 클래스
  */
 public class BridgeGame {
-
     /**
      * 사용자가 칸을 이동할 때 사용하는 메서드
      * <p>
@@ -27,6 +27,7 @@ public class BridgeGame {
      * <p>
      * 재시작을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void retry() {
+    public List<String> retry(List<String> playerBridge) {
+        return new ArrayList<>();
     }
 }
