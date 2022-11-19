b0131692ebe647fdb90c1f12e148f3bf40b51880
jjdone <ehdlfaos@gmail.com>
2022-11-19 17:39:04 +0900

    feat: Controller 설정 및 bridge 생성 구현

--- src/main/java/bridge/controller/BridgeGame.java
+++ src/main/java/bridge/controller/BridgeGame.java
@@ -1,9 +1,23 @@
-package bridge.Control;
+package bridge.controller;
+
+import bridge.BridgeRandomNumberGenerator;
+import bridge.model.Bridge;
 
 /**
  * 다리 건너기 게임을 관리하는 클래스
  */
 public class BridgeGame {
+    private static final String GAME_START_MESSAGE = "다리 건너기 게임을 시작합니다.";
+
+    private Bridge bridge;
+
+    public BridgeGame() {
+    }
+
+    public void gameStart() {
+        System.out.println(GAME_START_MESSAGE);
+        makeBridge();
+    }
 
     /**
      * 사용자가 칸을 이동할 때 사용하는 메서드
@@ -11,6 +25,7 @@ public class BridgeGame {
      * 이동을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
     public void move() {
+
     }
 
     /**
@@ -19,5 +34,11 @@ public class BridgeGame {
      * 재시작을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
     public void retry() {
+    }
+
+    private void makeBridge() {
+        BridgeMaker bridgeMaker = new BridgeMaker(new BridgeRandomNumberGenerator());
+        int size = bridgeMaker.inputBridgeSize();
+        bridge = new Bridge(bridgeMaker.makeBridge(size));
     }
 }
--- src/main/java/bridge/controller/BridgeMaker.java
+++ src/main/java/bridge/controller/BridgeMaker.java
@@ -1,4 +1,4 @@
-package bridge.model;
+package bridge.controller;
 
 import bridge.BridgeNumberGenerator;
 import bridge.view.InputView;
