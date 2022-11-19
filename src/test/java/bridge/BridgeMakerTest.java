32346a734427dc9aa2f6618a40a9f0f27b327190
E1psycongr00 <39326175+E1psycongr00@users.noreply.github.com>
2022-11-19 12:27:58 +0900

    test(bridgeMaker): 랜덤 생성기가 잘 못 생성한 경우 예외 테스트 완료

--- src/test/java/bridge/BridgeMakerTest.java
+++ src/test/java/bridge/BridgeMakerTest.java
@@ -1,8 +1,10 @@
 package bridge;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.mockito.Mockito.when;
 
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 import org.mockito.Mockito;
@@ -21,5 +23,15 @@ class BridgeMakerTest {
         assertThatThrownBy(() -> bridgeMaker.makeBridge(input)).isInstanceOf(IllegalArgumentException.class);
     }
 
+    @DisplayName("랜덤 생성기가 1, 0 이외에 잘못된 값을 리턴한 경우")
+    @Test
+    void throwExceptionWhenRandomGeneratorCreateInvalidNumber() {
+        // given
+        when(bridgeNumberGenerator.generate()).thenReturn(3);
+        final int VALID_NUMBER = 10;
+
+        // expect
+        assertThatThrownBy(() -> bridgeMaker.makeBridge(VALID_NUMBER)).isInstanceOf(IllegalArgumentException.class);
+    }
 
 }
