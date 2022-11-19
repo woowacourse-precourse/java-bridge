12f9d8f361ac79ad5267886f7bae1ffad4cf8ec7
Sehbeom <j3beom@gmail.com>
2022-11-20 04:06:17 +0900

    refactor(model): 다리 모델 리팩토링
    이동 기능 동작 방식에 맞추어 다리 모델 리팩토리 수행

--- src/main/java/bridge/domain/model/Bridge.java
+++ src/main/java/bridge/domain/model/Bridge.java
@@ -1,24 +1,30 @@
 package bridge.domain.model;
 
+import bridge.constants.MovingPossibility;
 import java.util.ArrayList;
 import java.util.List;
 
 public class Bridge {
     private static List<String> bridge = new ArrayList<>();
-    private static boolean canEdit = true;
+    private static boolean bridgeCreated = false;
 
-    public Bridge(List<String> bridge) {
-        if (Bridge.canEdit) {
-            Bridge.bridge = bridge;
-            Bridge.canEdit = false;
+    public static boolean setBridge(List<String> passedBridge) {
+        if (!Bridge.bridgeCreated) {
+            Bridge.bridge = passedBridge;
+            Bridge.bridgeCreated = true;
+            return true;
         }
+        return false;
     }
 
-    public static boolean canCross(Integer space, String direction) {
-        if (Bridge.canEdit) {
+    public static MovingPossibility checkMovingPossibility(Integer space, String direction) {
+        if (!Bridge.bridgeCreated) {
             throw new IllegalStateException("[ERROR] 다리가 생성되지 않았습니다.");
         }
 
-        return bridge.get(space).equals(direction);
+        if (bridge.get(space).equals(direction)) {
+            return MovingPossibility.CAN_MOVE;
+        }
+        return MovingPossibility.CAN_NOT_MOVE;
     }
 }
--- src/test/java/bridge/model/BridgeTest.java
+++ src/test/java/bridge/model/BridgeTest.java
@@ -3,29 +3,37 @@ package bridge.model;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
+import bridge.constants.MovingPossibility;
 import bridge.domain.model.Bridge;
 import java.util.ArrayList;
 import java.util.List;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 public class BridgeTest {
+
     List<String> testBridge = new ArrayList<>(List.of("U", "D", "D"));
 
     void initializeBridge() {
-        Bridge bridge = new Bridge(testBridge);
+        Bridge.setBridge(testBridge);
     }
 
     @DisplayName("다리 초기화 - 다리는 U와 D로만 구성되어야 한다.")
     @Test
+    @Order(1)
     void bridgeInitializeWithNoneUOrDTest() {
         // 예외 처리 구현 과정에서 개발 예정
     }
 
     @DisplayName("다리 초기화 - 다리의 길이는 3~20 사이여야 한다.")
     @Test
+    @Order(2)
     void bridgeInitializeWithOutOfRangeLengthTest() {
         // 예외 처리 구현 과정에서 개발 예정
     }
@@ -33,23 +41,44 @@ public class BridgeTest {
     @DisplayName("건널 수 있는지 확인 기능 - 아직 초기화 되지 않은 다리에 대해 메소드를 호출할 경우, 예외가 발생한다.")
     @ParameterizedTest
     @CsvSource({"0, 'U'"})
+    @Order(3)
     void canCrossNotInitializedExceptionTest(int space, String direction) {
         assertThatThrownBy(() -> {
-            Bridge.canCross(space, direction);
+            Bridge.checkMovingPossibility(space, direction);
         })
                 .isInstanceOf(IllegalStateException.class)
                 .hasMessageContaining("[ERROR]");
     }
 
-    @DisplayName("건널 수 있는지 확인 기능 - 위치와 문자가 입력되었을 때 건널 수 있는지 여부를 반환해야 한다.")
-    @ParameterizedTest
-    @CsvSource({"0, 'U', true",
-            "1, 'U', false"})
-    void canCrossTest(int space, String direction, boolean canCross) {
-        initializeBridge();
+    @DisplayName("다리 초기화 - 다리는 한번만 초기화 될 수 있으며, 초기화 되었는지 여부를 반환한다.")
+    @Test
+    @Order(4)
+    void bridgeInitializeOnceWithTrueReturnTest() {
+        assertThat(Bridge.setBridge(testBridge)).isTrue();
 
-        assertThat(Bridge.canCross(space, direction)).isEqualTo(canCross);
+        List<String> otherBridge = new ArrayList<>(List.of("U", "U", "U"));
+        assertThat(Bridge.setBridge(otherBridge)).isFalse();
     }
 
+    @DisplayName("건널 수 있는지 확인 기능 - 건널 수 있는 위치와 문자가 전달되면 CAN_MOVE 를 반환한다.")
+    @ParameterizedTest
+    @CsvSource({"0, 'U'"})
+    @Order(6)
+    void canMovePossibleTest(int space, String direction) {
+        initializeBridge();
 
+        assertThat(Bridge.checkMovingPossibility(space, direction)).isEqualTo(
+                MovingPossibility.CAN_MOVE);
+    }
+
+    @DisplayName("건널 수 있는지 확인 기능 - 건널 수 없는 위치와 문자가 전달되면 CAN_NOT_MOVE 를 반환한다.")
+    @ParameterizedTest
+    @CsvSource({"1, 'U'"})
+    @Order(7)
+    void canMoveImpossibleTest(int space, String direction) {
+        initializeBridge();
+
+        assertThat(Bridge.checkMovingPossibility(space, direction)).isEqualTo(
+                MovingPossibility.CAN_NOT_MOVE);
+    }
 }
