4a4578384d6c8b98649299901aba0f79e0eaab16
Ridealist <cjsn92@gmail.com>
2022-11-19 23:02:36 +0900

    refactor(Controller): 메소드 길이 제한 반영한 메소드 분리

--- src/main/java/bridge/Controller.java
+++ src/main/java/bridge/Controller.java
@@ -4,59 +4,62 @@ import bridge.models.BridgeGame;
 import bridge.models.BridgeMaker;
 import bridge.utils.BridgeNumberGenerator;
 import bridge.utils.BridgeRandomNumberGenerator;
+import bridge.utils.Setting;
 import bridge.views.InputView;
 import bridge.views.OutputView;
 
 public class Controller {
-    public static final String RESTART = "R";
 
     public static BridgeGame makeBridgeMap() {
-        int bridgeSize;
-        while (true) {
-            try {
-                bridgeSize = InputView.readBridgeSize();
-                break;
-            } catch (IllegalArgumentException e) {
-                System.out.println(e.getMessage());
-            }
-        }
+        int bridgeSize = getBridgeSize();
         BridgeNumberGenerator bridgeNumberGenerator = new BridgeRandomNumberGenerator();
         BridgeMaker bridgeMaker = new BridgeMaker(bridgeNumberGenerator);
         return new BridgeGame(bridgeMaker, bridgeSize);
     }
 
+    public static int getBridgeSize() {
+        try {
+            return InputView.readBridgeSize();
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+            return getBridgeSize();
+        }
+    }
+
     public static void startRound(BridgeGame bridgeGame, OutputView outputView) {
         do {
-            String directions;
-            while (true) {
-                try {
-                    directions = InputView.readMoving();
-                    break;
-                } catch (IllegalArgumentException e) {
-                    System.out.println(e.getMessage());
-                }
-            }
+            String directions = getMovingDirection();
             bridgeGame.move(directions);
-            outputView.printMap(directions, bridgeGame.checkStatus(directions));
-
+            outputView.printMap(directions, bridgeGame.checkStatus(directions), bridgeGame.getBridgeIndex());
             if (bridgeGame.gameSuccess()) {
                 break;
             }
         } while (bridgeGame.isGameContinue());
     }
 
-    public static void restartGame(BridgeGame bridgeGame) {
-        String gameCommand;
+    public static String getMovingDirection() {
         while (true) {
             try {
-                gameCommand = InputView.readGameCommand();
-                break;
+                return InputView.readMoving();
             } catch (IllegalArgumentException e) {
                 System.out.println(e.getMessage());
             }
         }
-        if (gameCommand.equals(RESTART)) {
+    }
+
+    public static void restartGame(BridgeGame bridgeGame) {
+        String gameCommand = getGameCommand();
+        if (gameCommand.equals(Setting.GAME_RESTART)) {
             bridgeGame.retry();
+        }
+    }
+
+    public static String getGameCommand() {
+        try {
+            return InputView.readGameCommand();
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+            return getGameCommand();
         }
     }
 }
