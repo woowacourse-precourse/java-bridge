1e61128502a4fb39039efde954514d1e4aff219c
MyunghyunNero <mh1kim0910@gmail.com>
2022-11-19 17:58:59 +0900

    feat: Game 클래스를 만들어 전체 진행과정 구현

---
+++ src/main/java/bridge/Game.java
@@ -0,0 +1,36 @@
+package bridge;
+
+public class Game {
+    private InputView inputView = new InputView();
+    private OutputView outputView ;
+    public void start(){
+        BridgeGame bridgeGame = new BridgeGame();
+        outputView = new OutputView(bridgeGame);
+        System.out.println("다리 건너기 게임을 시작합니다.");
+        int sizeInput = inputView.readBridgeSize();
+        bridgeGame.init(sizeInput);
+        while(bridgeGame.getSuccess()){
+            int i;
+            for(i=1 ;i<=sizeInput;i++){
+                if(!bridgeGame.getSuccess()){
+                    break;
+                }
+                String moveInput = inputView.readMoving();
+                outputView.printMap(moveInput);
+            }
+            if(!bridgeGame.getSuccess()){
+                String retryInput = inputView.readGameCommand();
+                bridgeGame.retry(retryInput);
+            }
+            if(!bridgeGame.getSuccess()){
+                outputView.printResult();
+                outputView.printgetInfo();
+            }
+            if(bridgeGame.getSuccess() && i==sizeInput){
+                outputView.printResult();
+                outputView.printgetInfo();
+                break;
+            }
+        }
+    }
+}
