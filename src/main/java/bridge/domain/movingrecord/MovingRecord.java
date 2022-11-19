2a8e1864a74d3c694f3fbe13f11daf59179b107d
chanminmaxscaler <jcm04@naver.com>
2022-11-19 21:12:11 +0900

    feat: MovingRecord 클래스에 사용자의 방향과 판정을 기록하고 활용하는 기능을 구현한다
    MovingRecord 클래스 Map<Judgement, Direction> record를 유일한 멤버변수로 갖는다.
    생성자에 의해 record는 LinkedHashMap으로 초기화된다.
    record에 사용자의 방향과 판정을 기록되며, MovingRecord는 이를 활용하는 기능을 갖는다.
    
    write 메소드는 방향과 판정을 기록받아 record에 해당 값을 put한다. 판정이 key, 방향이 value가 된다.
    read 메소드는 Map의 Entry를 스트림으로 변환해 반환한다.
    size 메소드는 record의 크기를 반환한다.

---
+++ src/main/java/bridge/domain/movingrecord/MovingRecord.java
@@ -0,0 +1,36 @@
+package bridge.domain.movingrecord;
+
+import bridge.domain.direction.Direction;
+import bridge.domain.referee.Judgement;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.stream.Stream;
+
+public class MovingRecord {
+
+    private final Map<Judgement, Direction> record;
+
+    public MovingRecord() {
+        this.record = new LinkedHashMap<>();
+    }
+
+    public void write(Judgement judgement, Direction direction) {
+        record.put(judgement, direction);
+    }
+
+    public Stream<Map.Entry<Judgement, Direction>> read() {
+        return record.entrySet().stream();
+    }
+
+    public int size() {
+        return record.size();
+    }
+
+    @Override
+    public String toString() {
+        return "MovingRecord{" +
+                "record=" + record +
+                '}';
+    }
+}
