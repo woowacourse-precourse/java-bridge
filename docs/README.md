ca9b987af05a4cfb78a292dbff64fed5e0fd176b
donghae <donghae1999@gmail.com>
2022-11-19 16:55:38 +0900

    feat: 방향에 따라 정답 다리와 비교 추가

--- docs/README.md
+++ docs/README.md
@@ -13,7 +13,7 @@
 - [ ] 다리 건너기 BridgeGame#move
   - [x] 사용자로부터 U와 D 방향 입력받기 InputView#readMoving
   - [x] 사용자 입력 예외처리 InputView#validateMove
-  - [ ] 방향에 따라 정답 다리와 비교 
+  - [x] 방향에 따라 정답 다리와 비교 
   - [ ] 사용자 결과 출력 OutputView#printMap
 - [ ] 실패 시 게임 재시도 여부 출력 MessageView#PRINT_GAME_RESTART_MESSAGE
 - [ ] 게임 재시도 BridgeGame#retry
--- src/main/java/bridge/BridgeGame.java
+++ src/main/java/bridge/BridgeGame.java
@@ -1,16 +1,53 @@
 package bridge;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * 다리 건너기 게임을 관리하는 클래스
  */
 public class BridgeGame {
-
+    private List<String> userBridge= new ArrayList<>();
+    public List<String> upBridge;
+    public List<String> downBridge;
     /**
      * 사용자가 칸을 이동할 때 사용하는 메서드
      * <p>
      * 이동을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void move() {
+    public boolean move(String input) {
+        userBridge.add(input);
+        if(!validateMove(userBridge,Controller.answerBridge)){
+            return false;
+        }
+        return true;
+    }
+
+    private boolean validateMove(List<String> userBridge, List<String> answerBridge){
+        upBridge = new ArrayList<>();
+        downBridge = new ArrayList<>();
+        for(int i=0; i<userBridge.size(); i++){
+            if(!userBridge.get(i).equals(answerBridge.get(i))){
+                if(userBridge.get(i).equals("U")){
+                    upBridge.add("X");
+                    downBridge.add(" ");
+                }
+                else if(userBridge.get(i).equals("D")){
+                    upBridge.add(" ");
+                    downBridge.add("X");
+                }
+                return false;
+            }
+            if(userBridge.get(i).equals("U")){
+                upBridge.add("O");
+                downBridge.add(" ");
+            }
+            else if(userBridge.get(i).equals("D")){
+                upBridge.add(" ");
+                downBridge.add("O");
+            }
+        }
+        return true;
     }
 
     /**
@@ -20,4 +57,5 @@ public class BridgeGame {
      */
     public void retry() {
     }
+
 }
