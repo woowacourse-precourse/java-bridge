13cdf856499a6543d07b91b0f2cc0af028c93e43
Dongmin Kim <kimdongmin68@naver.com>
2022-11-19 19:27:26 +0900

    feat(bridge): 다리의 길이 입력 시 범위 내 숫자가 아닌 경우 예외 처리 구현

--- src/main/java/bridge/InputValidator.java
+++ src/main/java/bridge/InputValidator.java
@@ -2,18 +2,27 @@ package bridge;
 
 public class InputValidator {
     private static final String NOT_NUMBER = "[ERROR] 숫자를 입력해주세요.";
+    private static final String NOT_IN_RANGE = "[ERROR] 3 이상 20 이하의 숫자를 입력해주세요.";
 
     public static void inputBridgeSizeValidate(String bridgeSize) {
         checkNumber(bridgeSize);
+        checkSizeInRange(bridgeSize);
     }
 
-    public static void checkNumber(String bridgeSize) {
+    private static void checkNumber(String bridgeSize) {
         try {
             Integer.parseInt(bridgeSize);
         } catch (NumberFormatException e) {
             throw new IllegalArgumentException(NOT_NUMBER);
         } catch (NullPointerException e) {
             throw new IllegalArgumentException(NOT_NUMBER);
+        }
+    }
+
+    private static void checkSizeInRange(String bridgeSize) {
+        int size = Integer.parseInt(bridgeSize);
+        if (size < 3 || 20 < size) {
+            throw new IllegalArgumentException(NOT_IN_RANGE);
         }
     }
 }
