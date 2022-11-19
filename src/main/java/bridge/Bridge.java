abd51beb42448fa4e9b4213bb09ddafe404c0fd3
Limjongho <whdgh9873@icloud.com>
2022-11-19 14:54:05 +0900

    feat(Bridge): 다리 생성시 예외처리 추가

--- src/main/java/bridge/Bridge.java
+++ src/main/java/bridge/Bridge.java
@@ -6,10 +6,17 @@ public class Bridge {
     private final List<String> movableBlocks;
 
     public Bridge(List<String> movableBlocks) {
+        validate(movableBlocks);
         this.movableBlocks = movableBlocks;
     }
 
     public boolean isMovableBlock(int position, String BlockInput) {
         return BlockInput.equals(movableBlocks.get(position));
+    }
+
+    private void validate(List<String> movableBlocks) {
+        if (!movableBlocks.contains("D") && !movableBlocks.contains("U")) {
+            throw new IllegalArgumentException("[ERROR] 유효하지 않은 문자가 포함되어 있습니다.");
+        }
     }
 }
