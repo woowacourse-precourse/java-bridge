c0a1dc719ef35c9acfb386e67e6f2494d2cc1a47
masiljangajji <hjk172262@gmail.com>
2022-11-19 14:28:51 +0900

    feat: 사용자가 이동한 칸을 초기화 하는 기능 추가

--- src/main/java/bridge/BridgeGame.java
+++ src/main/java/bridge/BridgeGame.java
@@ -46,10 +46,13 @@ public class BridgeGame {
     public boolean checkFail(List<String> list){
         for(int i=0;i<list.size();i++){
             if(list.get(i).contains("X")){
-                return false;
+                return true;
             }
         }
-        return true;
+        return false;
+    }
+    public void initializeUserMove(){
+        userMove.clear();
     }
 
 }
