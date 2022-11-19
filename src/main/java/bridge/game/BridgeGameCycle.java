177c070cea2a3480c09729fba6e26a77fdff93b6
BioBeBe <76714219+JaegeonYu@users.noreply.github.com>
2022-11-19 14:03:37 +0900

    feat: 필드 추가

--- src/main/java/bridge/game/BridgeGameCycle.java
+++ src/main/java/bridge/game/BridgeGameCycle.java
@@ -1,8 +1,30 @@
 package bridge.game;
 
+import bridge.util.BridgeMaker;
+import bridge.util.BridgeRandomNumberGenerator;
+import bridge.view.InputView;
+
+import java.util.List;
+
+import static bridge.enums.IntEnum.*;
+import static bridge.enums.StringEnum.*;
+import static bridge.view.OutputView.*;
+
 public class BridgeGameCycle {
-    private int coin = 0;
-    public void play(){
+    private static final boolean CONTINUE = true;
+    private static final boolean FINISH = false;
+    private static final InputView inputView = new InputView();
+    private int coin = 1;
+    private int bridgeLength;
+    private int nowState;
+    private String finalMap;
+
+
+
+    public void play() throws IllegalArgumentException {
+
 
     }
+
+
 }
