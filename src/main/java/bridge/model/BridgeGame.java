091539c7c993c56a2926cf3f7351ce9829ade41a
김현재 <41482946+galug@users.noreply.github.com>
2022-11-20 02:39:42 +0900

    feat(bridgegame): 재시작을 위한 리셋 기능 추가

--- src/main/java/bridge/model/BridgeGame.java
+++ src/main/java/bridge/model/BridgeGame.java
@@ -5,27 +5,36 @@ public class BridgeGame {
     private final PassingSpace passingSpace;
     private int numberOfTry;
     private int position;
+    private boolean isRightSpace;
 
     public BridgeGame(Bridge bridge, PassingSpace passingSpace) {
         this.bridge = bridge;
         this.passingSpace = passingSpace;
         numberOfTry = 0;
+        reset();
+    }
+
+    private void reset() {
         position = 0;
+        passingSpace.reset();
+        isRightSpace = true;
     }
 
     public void move(String selectedSpace) {
         Space space = Space.valueOf(selectedSpace);
         Movable movable = bridge.compareSpace(position, selectedSpace);
         passingSpace.add(space, movable);
+        judgeSuccessMoving(selectedSpace);
+        position += 1;
     }
 
     public String drawPassingSpace() {
         return passingSpace.toString();
     }
 
-    public boolean isRightSpace(String selectedSpace) {
+    public void judgeSuccessMoving(String selectedSpace) {
         Movable movable = bridge.compareSpace(position, selectedSpace);
-        return movable.isMovable();
+        isRightSpace =  movable.isMovable();
     }
 
     public boolean retry(String selectRetry) {
@@ -33,7 +42,7 @@ public class BridgeGame {
 
         if (retry.isRetry()) {
             numberOfTry += 1;
-            passingSpace.reset();
+            reset();
         }
         return retry.isRetry();
     }
@@ -44,5 +53,9 @@ public class BridgeGame {
 
     public int getNumberOfTry() {
         return numberOfTry;
+    }
+
+    public boolean isRightSpace() {
+        return isRightSpace;
     }
 }
