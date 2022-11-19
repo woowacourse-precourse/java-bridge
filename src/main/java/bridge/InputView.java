82cdb9692b2e34fbbe5a3f12763fcebed4ec5684
JuneYub <ajsjdlwj0123@gmail.com>
2022-11-19 19:26:37 +0900

    <feat> 사용자에게 다리 길이를 받는 기능

--- src/main/java/bridge/InputView.java
+++ src/main/java/bridge/InputView.java
@@ -1,5 +1,7 @@
 package bridge;
 
+import camp.nextstep.edu.missionutils.Console;
+
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
  */
@@ -8,21 +10,56 @@ public class InputView {
     /**
      * 다리의 길이를 입력받는다.
      */
-    public int readBridgeSize() {
-        return 0;
+    public int inputBridgeSize() {
+        while (true) {
+            try {
+                String sizeTmp = Console.readLine();
+                if(checkInputSizeString(sizeTmp)) return Integer.parseInt(sizeTmp);
+            }
+            catch (IllegalArgumentException e) {
+                System.out.println(e);
+            }
+        }
     }
+
+    public boolean checkInputSizeString(String inputSize) {
+        if(isNumeric(inputSize) && checkInRange(Integer.parseInt(inputSize))) {
+            return true;
+        }
+        return false;
+    }
+
+
 
     /**
      * 사용자가 이동할 칸을 입력받는다.
      */
     public String readMoving() {
-        return null;
+
     }
 
     /**
      * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
      */
     public String readGameCommand() {
-        return null;
+
     }
+
+    public boolean checkInRange(int input) {
+        if(3 <= input && input <= 20) {
+            return true;
+        }
+        throw new IllegalArgumentException("[ERROR] 3과 20 사이의 숫자를 입력해주세요");
+    }
+
+    public boolean isNumeric(String s) {
+        try {
+            Double.parseDouble(s);
+            return true;
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("[ERROR] 숫자를 입력해주세요");
+        }
+    }
+
+
 }
