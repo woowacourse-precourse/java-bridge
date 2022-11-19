d5d29fdc67ebee7a49c2eabfdb2f386014662336
ym2629 <dydals2629@naver.com>
2022-11-19 18:34:01 +0900

    feat: print one part of upper bridge

--- src/main/java/bridge/OutputView.java
+++ src/main/java/bridge/OutputView.java
@@ -7,12 +7,6 @@ import java.util.*;
  */
 public class OutputView {
 
-    private static final int RANDOM_LOWER_INCLUSIVE = 0;
-    private static final int RANDOM_UPPER_INCLUSIVE = 1;
-    private static final int FIRST_BRIDGE = 0;
-    private static final int SECOND_BRIDGE = 1;
-
-
     public void startGame(){
         System.out.println("다리 건너기 게임을 시작합니다.");
     }
@@ -22,9 +16,8 @@ public class OutputView {
      * <p>
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void printMap(int userPosition, boolean checkBridge, List<List<Integer>> map) {
-        printPartOfBridge(userPosition, checkBridge, getOneInDoubleList(FIRST_BRIDGE, map));
-        printPartOfBridge(userPosition, checkBridge, getOneInDoubleList(SECOND_BRIDGE, map));
+    public void printMap(int userPosition, String moving, boolean checkUserDecision, List<String> bridge) {
+
     }
 
     /**
@@ -35,62 +28,13 @@ public class OutputView {
     public void printResult() {
     }
 
-    public void printPartOfBridge(int userPosition, boolean checkBridge, List<Integer> partOfBridge){
-        printMapStart();
-        for(int i = 0; i < userPosition; i++){
-            printNone(i, partOfBridge);
-            printCorrect(i, partOfBridge);
-            printSeparation();
-        }
-        printUserPositionValue(userPosition, checkBridge, partOfBridge);
-        printMapEnd();
-    }
-
-    public List<Integer> getOneInDoubleList(int partNumber, List<List<Integer>> map){
-        return map.get(partNumber);
-    }
-
-    public void printMapStart(){
-        System.out.print("[");
-    }
-
-    public void printNone(int index, List<Integer> partOfBridge){
-        if(partOfBridge.get(index) == RANDOM_LOWER_INCLUSIVE){
-            System.out.print("   ");
-        }
-    }
-
-    public void printCorrect(int index, List<Integer> partOfBridge){
-        if(partOfBridge.get(index) == RANDOM_UPPER_INCLUSIVE){
+    public void printOnePartOfUpperBridge(int index, List<String> bridge){
+        if(bridge.get(index) == "U"){
             System.out.print(" O ");
-        }
-    }
-
-    public void printIncorrect(int index, List<Integer> partOfBridge){
-        if(partOfBridge.get(index) == RANDOM_LOWER_INCLUSIVE){
-            System.out.print(" X ");
-        }
-    }
-
-    public void printSeparation(){
-        System.out.print("|");
-    }
-
-    public void printMapEnd(){
-        System.out.print("]");
-    }
-
-    public void printUserPositionValue(int userPosition, boolean checkBridge,  List<Integer> partOfBridge){
-        if(checkBridge == true && partOfBridge.get(userPosition) == RANDOM_UPPER_INCLUSIVE){
-            System.out.print(" O ");
-            return;
-        }
-
-        if(checkBridge == false && partOfBridge.get(userPosition) == RANDOM_LOWER_INCLUSIVE){
-            System.out.print(" X ");
             return;
         }
 
         System.out.print("   ");
     }
+
 }
