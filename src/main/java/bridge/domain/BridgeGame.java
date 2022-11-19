7c39a1e116472ce3e79cdc40e57465134822d122
SuhHyeonjun <dmsgktn0208@gmail.com>
2022-11-19 17:45:03 +0900

    refactor : "InputView를 사용하지 않는다." 규칙 적용

--- src/main/java/bridge/domain/BridgeGame.java
+++ src/main/java/bridge/domain/BridgeGame.java
@@ -1,15 +1,12 @@
 package bridge.domain;
 
-import static bridge.controller.BridgeGameController.restartGame;
-
+import bridge.controller.BridgeGameController;
 import bridge.messages.Message;
-import bridge.view.InputView;
 
 /**
  * 다리 건너기 게임을 관리하는 클래스
  */
 public class BridgeGame {
-    private static final InputView inputView = new InputView();
 
     /**
      * 사용자가 칸을 이동할 때 사용하는 메서드
@@ -28,12 +25,7 @@ public class BridgeGame {
      * <p>
      * 재시작을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void retry(String moveResult) {
-        if (moveResult.equals("X")) {
-            String input = inputView.inputGameRestart();
-            if (input.equals(Message.RESTART.getMessage())) {
-                restartGame();
-            }
-        }
+    public void retry() {
+        BridgeGameController bridgeGameController = new BridgeGameController();
     }
 }
