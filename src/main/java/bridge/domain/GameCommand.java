80e102548fe4c3f6053a23de9c1c7ed99cabf296
Basak <chgy2131@naver.com>
2022-11-20 01:03:52 +0900

    feat(GameCommand): 게임 진행여부와 관련된 용어들을 저장하는 클래스 추가

--- src/main/java/bridge/domain/GameCommand.java
+++ src/main/java/bridge/domain/GameCommand.java
@@ -1,4 +1,25 @@
 package bridge.domain;
 
 public enum GameCommand {
+    RESTART("R"),
+    QUIT("Q"),
+    ;
+    private String command;
+
+    GameCommand(String command) {
+        this.command = command;
+    }
+
+    public static GameCommand of(String command) {
+        for (GameCommand c : GameCommand.values()) {
+            if (c.getCommand().equals(command)) {
+                return c;
+            }
+        }
+        throw new IllegalArgumentException("[ERROR]");
+    }
+
+    public String getCommand() {
+        return command;
+    }
 }
