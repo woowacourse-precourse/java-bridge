705d8da274558abc7fcd93b68dd62095cf5c4ad9
qkrdmswl <eunji303@naver.com>
2022-11-19 18:05:40 +0900

    feat(ResultType): define game result type

--- src/main/java/bridge/type/ResultType.java
+++ src/main/java/bridge/type/ResultType.java
@@ -1,2 +1,16 @@
-package bridge.type;public enum ResultType {
+package bridge.type;
+
+public enum ResultType {
+    SUCCESS("성공"),
+    FAIL("실패");
+
+    private final String resultType;
+
+    ResultType(String resultType) {
+        this.resultType = resultType;
+    }
+
+    public String getResultType() {
+        return resultType;
+    }
 }
