1b0a74fd92fec39337589ae5a944bd64a3914d51
Your Name <you@example.com>
2022-11-19 11:54:31 +0900

    feat: 사용자 이동 구현

--- src/main/java/bridge/BridgeGame.java
+++ src/main/java/bridge/BridgeGame.java
@@ -1,16 +1,40 @@
 package bridge;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * 다리 건너기 게임을 관리하는 클래스
  */
 public class BridgeGame {
+    int stepNum = 0;
+    int tryNum = 0;
+    List<String> answer;
+    List<String> playerMove;
+
+    BridgeGame() {
+        stepNum = 0;
+        tryNum = 0;
+        answer = null;
+        playerMove = new ArrayList<>();
+    }
 
     /**
      * 사용자가 칸을 이동할 때 사용하는 메서드
      * <p>
      * 이동을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void move() {
+    public boolean move(String playerMoveInput) {
+        setMoveValue(playerMoveInput);
+        int step = stepNum - 1;
+        if(answer.get(step) == playerMoveInput)
+            return true;
+        return false;
+    }
+
+    private void setMoveValue(String playerMoveInput) {
+        stepNum++;
+        playerMove.add(playerMoveInput);
     }
 
     /**
@@ -19,5 +43,14 @@ public class BridgeGame {
      * 재시작을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
     public void retry() {
+        stepNum = 0;
+        tryNum++;
+        playerMove = null;
+        playerMove = new ArrayList<>();
     }
+
+    public void setBridge(List<String> bridge) {
+        this.answer = bridge;
+    }
+
 }
