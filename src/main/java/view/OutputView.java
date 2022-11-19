b80ba4c47b6a75586bb60dd8360a929ddc997fb9
Devtist <yt930310@snu.ac.kr>
2022-11-19 20:30:17 +0900

    feat: 결과를 출력하는 기능 구현

--- src/main/java/view/OutputView.java
+++ src/main/java/view/OutputView.java
@@ -5,23 +5,35 @@ package view;
  */
 public class OutputView {
 
-    /**
-     * 현재까지 이동한 다리의 상태를 정해진 형식에 맞춰 출력한다.
-     * <p>
-     * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
-     */
-    public void printMap() {
-    }
+	/**
+	 * 현재까지 이동한 다리의 상태를 정해진 형식에 맞춰 출력한다.
+	 * <p>
+	 * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
+	 */
+	public void printMap(String map) {
+		System.out.println(map);
+	}
 
-    /**
-     * 게임의 최종 결과를 정해진 형식에 맞춰 출력한다.
-     * <p>
-     * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
-     */
-    public void printResult() {
-    }
+	/**
+	 * 게임의 최종 결과를 정해진 형식에 맞춰 출력한다.
+	 * <p>
+	 * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
+	 */
+	public void printResult(String result, boolean isSuccess, int count) {
+		System.out.println(ViewConstant.FINAL_RESULT_MESSAGE);
+		System.out.println(result);
+		System.out.print(ViewConstant.SUCCESS_OR_FAIL_MESSAGE);
+		if (isSuccess) {
+			System.out.println(ViewConstant.SUCCESS);
+		}
+		if (!isSuccess) {
+			System.out.println(ViewConstant.FAIL);
+		}
+		System.out.printf(ViewConstant.TOTAL_PLAY_TIME_MESSAGE);
+		System.out.println(count);
+	}
 
-    public void printStartMessage() {
-        System.out.println(ViewConstant.GAME_START_MESSAGE);
-    }
+	public void printStartMessage() {
+		System.out.println(ViewConstant.GAME_START_MESSAGE);
+	}
 }
