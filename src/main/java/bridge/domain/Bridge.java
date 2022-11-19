4bf9a92b753878a528835f093383e6432b7bd3d8
Cyma-s <ssol0319@naver.com>
2022-11-20 02:22:44 +0900

    feat(Bridge) : add Bridge class and a function that makes bridge
    makeBridge function : convert a variable given in the form of List<String> to List<BridgeState>

---
+++ src/main/java/bridge/domain/Bridge.java
@@ -0,0 +1,20 @@
+package bridge.domain;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Bridge {
+    List<BridgeState> bridge;
+
+    Bridge(List<String> bridge) {
+        this.bridge = new ArrayList<>();
+        makeBridge(bridge);
+    }
+
+    public void makeBridge(List<String> bridge) {
+        for(String square: bridge) {
+            BridgeState state = BridgeState.makeBridgeState(square);
+            this.bridge.add(state);
+        }
+    }
+}
