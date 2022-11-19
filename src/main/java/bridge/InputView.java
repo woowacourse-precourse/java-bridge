c71cefd5ca4d4384b2c3a25cee07f2be8a0df8b1
JungHun Jang <ijj5258@naver.com>
2022-11-19 21:30:04 +0900

    feat : 만들 다리의 숫자를 입력받아 유효성 검사를 하는 기능 구현

--- src/main/java/bridge/InputView.java
+++ src/main/java/bridge/InputView.java
@@ -1,15 +1,27 @@
 package bridge;
 
+import camp.nextstep.edu.missionutils.Console;
+
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
  */
 public class InputView {
-
+	private static final String INPUT_MAKE_BRIDGE_NUMBER = "다리의 길이를 입력해주세요.";
+	private static final int MINIMUM_BRIDGE_NUMBER = 3; 
+	private static final int MAXIMUM_BRIDGE_NUMBER = 20; 
     /**
      * 다리의 길이를 입력받는다.
      */
     public int readBridgeSize() {
-        return 0;
+    	System.out.println(INPUT_MAKE_BRIDGE_NUMBER);
+    	String bridgeNumber = Console.readLine();
+    	try {
+    		validateInput(bridgeNumber);
+    	} catch(IllegalArgumentException e) {
+    		System.out.println(e);
+    		return readBridgeSize();
+    	}
+        return Integer.parseInt(bridgeNumber);
     }
 
     /**
@@ -24,5 +36,25 @@ public class InputView {
      */
     public String readGameCommand() {
         return null;
+    }
+    
+    private static int validateInput(final String bridgeNumber) {
+    	int convertBridgeNumberToInt = Integer.parseInt(validateInteger(bridgeNumber));
+    	return validateNumberRange(convertBridgeNumberToInt);
+    }
+    
+    private static String validateInteger(final String bridgeNumber) {
+    	if (!bridgeNumber.chars().allMatch(Character::isDigit)) {
+			throw new IllegalArgumentException("[ERROR] 숫자를 입력해주셔야 합니다.");
+		}
+    	return bridgeNumber;
+    }
+    
+    private static int validateNumberRange(final int value) {
+		if (value < MINIMUM_BRIDGE_NUMBER || MAXIMUM_BRIDGE_NUMBER < value) {
+			throw new IllegalArgumentException("[ERROR] 유효한 숫자 범위는 "
+					+ MINIMUM_BRIDGE_NUMBER + "~" + MAXIMUM_BRIDGE_NUMBER + "입니다.");
+		}
+		return value;
     }
 }
