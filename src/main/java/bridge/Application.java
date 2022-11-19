f608e77c9881a7ae533fba7a71f468e387031eed
baeseohyeon <formnarago@naver.com>
2022-11-19 22:15:52 +0900

    feat(Application): 컨트롤러가 사용할 모델, 뷰 필드 추가

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -1,8 +1,25 @@
 package bridge;
 
+import bridge.model.BridgeGame;
+import bridge.model.BridgeMaker;
+import bridge.view.InputView;
+import bridge.view.OutputView;
+
 public class Application {
 
+    private static InputView inputView;
+    private static OutputView outputView;
+    private static BridgeMaker bridgeMaker;
+    private static BridgeGame bridgeGame;
+
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        init();
+    }
+
+    private static void init() {
+        inputView = new InputView();
+        outputView = new OutputView();
+        bridgeMaker = new BridgeMaker(new BridgeRandomNumberGenerator());
+        bridgeGame = new BridgeGame(bridgeMaker.makeBridge(inputView.readBridgeSize()));
     }
 }
