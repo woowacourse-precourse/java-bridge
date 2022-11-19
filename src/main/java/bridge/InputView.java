e35de0dee36c13474f165adaf255927a5c0bcd6b
impati <impati0716@icloud.com>
2022-11-19 13:35:56 +0900

    feat(InputView) : 이동할 칸에 대한 입력

--- src/main/java/bridge/InputView.java
+++ src/main/java/bridge/InputView.java
@@ -25,7 +25,9 @@ public class InputView {
      * 사용자가 이동할 칸을 입력받는다.
      */
     public String readMoving() {
-        return null;
+        String command = Console.readLine();
+        validation.moveCommand(command);
+        return command;
     }
 
     /**
