7ce019748bb26e382651646963bef259126e3c5c
yoonjuho <dbswn990@naver.com>
2022-11-20 06:01:12 +0900

    feat(BridgeMaker): 다리 길이의 범위가 정해져 있다

--- src/main/java/bridge/model/BridgeMaker.java
+++ src/main/java/bridge/model/BridgeMaker.java
@@ -19,7 +19,12 @@ public class BridgeMaker {
      * @param size 다리의 길이
      * @return 입력받은 길이에 해당하는 다리 모양. 위 칸이면 "U", 아래 칸이면 "D"로 표현해야 한다.
      */
+
     public List<String> makeBridge(int size) {
         return null;
     }
+    public void validateSize(int size){
+        if(size < 3 || 20 < size){
+            throw new IllegalArgumentException();
+        }}
 }
