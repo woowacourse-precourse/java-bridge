17f39247c331432af3e49f116337aaceee17928d
situodys <situodys@gmail.com>
2022-11-20 01:53:37 +0900

    feat(Application): 게임 시작 기능 구현

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -1,8 +1,17 @@
 package bridge;
 
+import bridge.controller.GameController;
+import bridge.view.InputView;
+import bridge.view.OutputView;
+
 public class Application {
 
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        final InputView inputView = new InputView();
+        final OutputView outputView =new OutputView();
+        GameController gameController = new GameController(inputView,outputView);
+
+        gameController.start();
     }
 }
