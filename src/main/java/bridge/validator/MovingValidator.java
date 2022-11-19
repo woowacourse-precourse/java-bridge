6512336271aa188d7bae3afa38d0a8801b333fea
Donguk <95donguk.dev@gmail.com>
2022-11-19 23:59:45 +0900

    feat: 다리 이동 명령 입력 값이 소문자 u, d 라면 예외 처리 기능 구현

--- src/main/java/bridge/validator/MovingValidator.java
+++ src/main/java/bridge/validator/MovingValidator.java
@@ -4,6 +4,7 @@ import static bridge.utils.command.MoveCommand.DOWN;
 import static bridge.utils.command.MoveCommand.UP;
 import static bridge.utils.message.ErrorMessage.BLANK_VALUE;
 import static bridge.utils.message.ErrorMessage.INVALID_MOVING;
+import static bridge.utils.message.ErrorMessage.LOWERCASE_VALUE;
 
 import java.util.Objects;
 
@@ -11,7 +12,29 @@ public class MovingValidator implements Validator {
     @Override
     public void validate(String inputValue) {
         checkBlankValue(inputValue);
+        checkLowercaseValue(inputValue);
         checkInvalidMoving(inputValue);
+    }
+
+    private void checkBlankValue(String inputValue) {
+        if (inputValue.isBlank()) {
+            System.out.println(BLANK_VALUE.getMessage());
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private void checkLowercaseValue(String inputValue) {
+        if (isLowercaseValue(inputValue)) {
+            System.out.println(LOWERCASE_VALUE.getMessage());
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private boolean isLowercaseValue(String inputValue) {
+        String lowercaseUpCommand = UP.getCommand().toLowerCase();
+        String lowercaseDownCommand = DOWN.getCommand().toLowerCase();
+
+        return Objects.equals(inputValue, lowercaseUpCommand) || Objects.equals(inputValue, lowercaseDownCommand);
     }
 
     private void checkInvalidMoving(String inputValue) {
@@ -23,12 +46,5 @@ public class MovingValidator implements Validator {
 
     private boolean isValidMoving(String inputValue) {
         return Objects.equals(inputValue, UP.getCommand()) || Objects.equals(inputValue, DOWN.getCommand());
-    }
-
-    private void checkBlankValue(String inputValue) {
-        if (inputValue.isBlank()) {
-            System.out.println(BLANK_VALUE.getMessage());
-            throw new IllegalArgumentException();
-        }
     }
 }
