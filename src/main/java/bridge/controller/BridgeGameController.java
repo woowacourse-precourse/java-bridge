9bc1330096fbd78d90e2cb62898c5bfb36455ff5
kimtaesoo99 <107785279+kimtaesoo99@users.noreply.github.com>
2022-11-20 00:24:02 +0900

    feat: (BridgeGameController) 게임을 시작하는 컨트롤러 클래스 추가

---
+++ src/main/java/bridge/controller/BridgeGameController.java
@@ -0,0 +1,26 @@
+package bridge.controller;
+
+
+import bridge.BridgeMaker;
+import bridge.BridgeNumberGenerator;
+import bridge.BridgeRandomNumberGenerator;
+import bridge.domain.Bridge;
+import bridge.domain.User;
+import bridge.service.BridgeGame;
+import bridge.service.InputViewService;
+import bridge.view.OutputView;
+
+public class BridgeGameController {
+    private final BridgeNumberGenerator bridgeNumberGenerator = new BridgeRandomNumberGenerator();
+    private final BridgeMaker bridgeMaker = new BridgeMaker(bridgeNumberGenerator);
+    private final OutputView outputView = new OutputView();
+    private final InputViewService inputViewService = new InputViewService();
+    private final BridgeGame bridgeGame = new BridgeGame();
+
+    public void start() {
+        printStartGame();
+        Bridge bridge = setBridge();
+        User user = new User();
+        movingBridge(bridge, user);
+    }
+}
