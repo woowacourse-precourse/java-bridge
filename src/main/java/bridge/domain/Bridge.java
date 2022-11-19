2102300b013860643b5d7a30c74508c8cdf7c8aa
Soomin Lim <augustth511@gmail.com>
2022-11-20 02:38:37 +0900

    refactor(Bridge): 불필요한 유효성 검사 로직 삭제

--- src/main/java/bridge/domain/Bridge.java
+++ src/main/java/bridge/domain/Bridge.java
@@ -4,21 +4,10 @@ import java.util.List;
 
 public class Bridge {
 
-    public static final String up = "U";
-    public static final String down = "D";
-
     private final List<String> bridge;
 
     public Bridge(List<String> bridge) {
-        validate(bridge);
         this.bridge = bridge;
-    }
-
-    private void validate(List<String> bridge) {
-        if (bridge.stream()
-                .anyMatch(element -> !element.equals(up) && !element.equals(down))) {
-            throw new IllegalArgumentException("다리는 위, 아래 두 칸만으로 이루어져 있습니다.");
-        }
     }
 
     public boolean compareWithPosition(int position, String str) {
