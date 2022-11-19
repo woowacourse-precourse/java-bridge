66f24fe1cd2b17ee8e0abb0c0c4d3aee2ae70c9e
jinhyeon <vmflwu123@gmail.com>
2022-11-19 22:49:49 +0900

    chore(domain & view): domain과 view 패키지 생성
    1. Bridge를 domain 패키지로 이동
    2. InputView와 OuputView를 view 패키지로 이동

--- src/test/java/bridge/BridgeTest.java
+++ src/test/java/bridge/BridgeTest.java
@@ -1,14 +1,13 @@
 package bridge;
 
+import bridge.domain.Bridge;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.*;
-import static org.junit.jupiter.api.Assertions.*;
 
 class BridgeTest {
 
