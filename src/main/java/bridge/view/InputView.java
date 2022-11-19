6bb0ed89fbc64aafe85bc309c15988db1ee93e2f
Jeongjihun <wlgns2234@naver.com>
2022-11-20 02:14:02 +0900

    refactor: input함수 분리

--- src/main/java/bridge/view/InputView.java
+++ src/main/java/bridge/view/InputView.java
@@ -3,28 +3,25 @@ package bridge.view;
 import camp.nextstep.edu.missionutils.Console;
 import bridge.constant.BridgeGameInfo;
 
-/**
- * 사용자로부터 입력을 받는 역할을 한다.
- */
 public class InputView {
 
-    /**
-     * 다리의 길이를 입력받는다.
-     */
+    private static String input() {
+        return Console.readLine();
+    }
+
     public static int readBridgeSize() throws IllegalArgumentException {
         System.out.println(BridgeGameInfo.INPUT_BRIDGE_SIZE.message);
         // 다리 길이 입력받기
-        String userInputBridgeSize = Console.readLine();
+        String input = input();
 
         // 숫자 형태가 아니면 예외
-        for (int i = 0; i < userInputBridgeSize.length(); i++) {
-            char input = userInputBridgeSize.charAt(i);
-            if ((int) input < 48 || (int) input > 57) {
+        for (int i = 0; i < input.length(); i++) {
+            if ((int) input.charAt(i) < 48 || (int) input.charAt(i) > 57) {
                 throw new IllegalArgumentException();
             }
         }
 
-        int size = Integer.parseInt(userInputBridgeSize);
+        int size = Integer.parseInt(input);
 
         // 범위 벗어나면 예외
         if (size < 3 || 19 < size) {
@@ -40,7 +37,7 @@ public class InputView {
     public static String readMoving() throws IllegalArgumentException {
         System.out.println(BridgeGameInfo.SELECT_UP_DOWN.message);
         // 이동할 칸 입력받기
-        String input = Console.readLine();
+        String input = input();
 
         if (!(input.equals("U") || input.equals("D"))) {
             throw new IllegalArgumentException();
@@ -55,7 +52,7 @@ public class InputView {
     public static String readGameCommand() throws IllegalArgumentException {
         System.out.println(BridgeGameInfo.RESTART.message);
         // 다시 시도지 종료할지 입력받는다.
-        String input = Console.readLine();
+        String input = input();
 
         if (!(input.equals("Q") || input.equals("R"))) {
             throw new IllegalArgumentException();
