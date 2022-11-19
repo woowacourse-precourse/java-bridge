3ff501326952ae2ebfa48d92a01f81640d66dc40
making-a-scene <4936joo@naver.com>
2022-11-19 20:28:55 +0900

    test: Validation 클래스의 수정 사항에 맞추어 테스트 코드 리팩토링

--- src/test/java/bridge/ExceptionSituationTest.java
+++ src/test/java/bridge/ExceptionSituationTest.java
@@ -1,54 +1,38 @@
 package bridge;
 
 import bridge.domain.Validation;
-import bridge.ui.InputView;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-
-import java.io.ByteArrayInputStream;
-import java.io.InputStream;
 
 import static org.assertj.core.api.Assertions.*;
 
 public class ExceptionSituationTest {
-    private static final InputView inputView = new InputView();
-
-    private InputStream setInputStream(String input) {
-        return new ByteArrayInputStream(input.getBytes());
-    }
 
     @DisplayName("다리의 길이로 정수가 아닌 값이 입력되면 예외가 발생한다.")
     @Test
     void inputBridgeSizeNotIntTest() {
-        InputStream inputStream = setInputStream("notInt");
-        System.setIn(inputStream);
-        assertThatThrownBy(inputView::readBridgeSize).isInstanceOf(IllegalArgumentException.class);
+        assertThatThrownBy(() -> Validation.validateBridgeSize("notInt"))
+                .isInstanceOf(IllegalArgumentException.class);
     }
 
     @DisplayName("입력된 다리의 길이가 3 미만, 20 초과이면 예외가 발생한다.")
     @Test
     void inputBridgeSizeOutOfBoundsTest() {
-        InputStream inputStream = setInputStream("30");
-        System.setIn(inputStream);
-        assertThatThrownBy(inputView::readBridgeSize)
+        assertThatThrownBy(() -> Validation.validateBridgeSize("30"))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
     @DisplayName("입력된 이동할 칸 정보가 U나 D가 아닌 경우 예외가 발생한다.")
     @Test
     void inputForMoveNotUOrDTest() {
-        InputStream inputStream = setInputStream("wrong input");
-        System.setIn(inputStream);
-        assertThatThrownBy(inputView::readMoving)
+        assertThatThrownBy(() -> Validation.validateSpace("wrongInput"))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
     @DisplayName("재시도 여부에 대한 응답이 R 또는 Q가 아닌 경우 예외가 발생한다." )
     @Test
     void inputAfterFailureNotROrQTest() {
-        InputStream inputStream = setInputStream("wrong input");
-        System.setIn(inputStream);
-        assertThatThrownBy(inputView::readGameCommand)
+        assertThatThrownBy(() -> Validation.validateResponseAfterFailure("wrongInput"))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
