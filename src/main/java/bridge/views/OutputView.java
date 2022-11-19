83ad583b3287f06daf01a5f3fef32b5821f9f9a5
Ridealist <cjsn92@gmail.com>
2022-11-19 17:25:53 +0900

    feat(OutputView): 라운드별 게임 상황 출력 기능 구현

--- src/main/java/bridge/views/OutputView.java
+++ src/main/java/bridge/views/OutputView.java
@@ -8,13 +8,61 @@ package bridge.views;
  * - 값 출력을 위해 필요한 메서드를 추가할 수 있다.
  */
 public class OutputView {
+    private static final String START = "[";
+    private static final String END = "]";
+    private static final String SEPARATOR = "|";
+    private static final String CORRECT = "O";
+    private static final String INCORRECT = "X";
+    private static final String BLANK = " ";
 
+    private String mapUpstairs = "";
+    private String mapDownstairs = "";
     /**
      * 현재까지 이동한 다리의 상태를 정해진 형식에 맞춰 출력한다.
      * <p>
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void printMap() {
+    public void printMap(String direction, boolean isCorrect) {
+        String basicMap = " %s ";
+        if (direction.equals("U")) {
+            if (isCorrect) {
+                if (mapUpstairs.isEmpty()) {
+                    this.mapUpstairs += String.format(basicMap, CORRECT);
+                    this.mapDownstairs += String.format(basicMap, BLANK);
+                } else {
+                    this.mapUpstairs += SEPARATOR + String.format(basicMap, CORRECT);
+                    this.mapDownstairs += SEPARATOR + String.format(basicMap, BLANK);
+                }
+            } else {
+                if (mapUpstairs.isEmpty()) {
+                    this.mapUpstairs += String.format(basicMap, INCORRECT);
+                    this.mapDownstairs += String.format(basicMap, BLANK);
+                } else {
+                    this.mapUpstairs += SEPARATOR + String.format(basicMap, INCORRECT);
+                    this.mapDownstairs += SEPARATOR + String.format(basicMap, BLANK);
+                }
+            }
+        } else {
+            if (isCorrect) {
+                if (mapUpstairs.isEmpty()) {
+                    this.mapUpstairs += String.format(basicMap, BLANK);
+                    this.mapDownstairs += String.format(basicMap, CORRECT);
+                } else {
+                    this.mapUpstairs += SEPARATOR + String.format(basicMap, BLANK);
+                    this.mapDownstairs += SEPARATOR + String.format(basicMap, CORRECT);
+                }
+            } else {
+                if (mapUpstairs.isEmpty()) {
+                    this.mapUpstairs += String.format(basicMap, BLANK);
+                    this.mapDownstairs += String.format(basicMap, INCORRECT);
+                } else {
+                    this.mapUpstairs += SEPARATOR + String.format(basicMap, BLANK);
+                    this.mapDownstairs += SEPARATOR + String.format(basicMap, INCORRECT);
+                }
+            }
+        }
+        System.out.println(START + mapUpstairs + END);
+        System.out.println(START + mapDownstairs + END);
     }
 
     /**
