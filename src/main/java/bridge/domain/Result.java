e975d132e7f57660ee0a0294489521227c7a219c
kimtaehoonDev <taehoon_dev@naver.com>
2022-11-19 16:58:24 +0900

    feat(Result): 게임의 결과를 저장

--- src/main/java/bridge/domain/Result.java
+++ src/main/java/bridge/domain/Result.java
@@ -1,4 +1,24 @@
 package bridge.domain;
 
 public class Result {
+    private Boolean isSuccess;
+    private Integer tryCnt;
+    private FootPrint finalResult;
+
+    public Result() {
+        isSuccess = false;
+        tryCnt = 0;
+    }
+
+    public void recordFail(FootPrint finalResult) {
+        isSuccess = false;
+        tryCnt += 1;
+        this.finalResult = finalResult;
+    }
+
+    public void recordSuccess(FootPrint finalResult) {
+        isSuccess = true;
+        tryCnt += 1;
+        this.finalResult = finalResult;
+    }
 }
---
+++ src/test/java/bridge/domain/ResultTest.java
@@ -0,0 +1,8 @@
+package bridge.domain;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class ResultTest {
+}
