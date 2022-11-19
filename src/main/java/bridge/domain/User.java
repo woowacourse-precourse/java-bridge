9f33cbca37ddb6fdebf5e2aef1a9827cc64748f5
kimtaesoo99 <107785279+kimtaesoo99@users.noreply.github.com>
2022-11-19 23:44:50 +0900

    feat: (User) 성공여부를 저장하는 변수 선언 및 메서드 추가

--- src/main/java/bridge/domain/User.java
+++ src/main/java/bridge/domain/User.java
@@ -2,12 +2,16 @@ package bridge.domain;
 
 public class User {
     private int tryCount = 1;
+    private boolean isSuccess;
 
     public int getTryCount() {
         return tryCount;
     }
 
-    public void increaseTryCount(){
+    public void increaseTryCount() {
         tryCount++;
+    }
+    public void setSuccess(){
+        isSuccess = true;
     }
 }
