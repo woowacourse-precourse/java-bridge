ccbf1ff7b08459ade280cc68663bd0ef6912fe46
sjk <80821744+ShinjaeKwon@users.noreply.github.com>
2022-11-20 01:51:13 +0900

    remove : 주석 제거

--- src/main/java/bridge/view/InputView.java
+++ src/main/java/bridge/view/InputView.java
@@ -3,32 +3,18 @@ package bridge.view;
 import bridge.handler.check.CheckHandler;
 import camp.nextstep.edu.missionutils.Console;
 
-/**
- * 사용자로부터 입력을 받는 역할을 한다.
- */
 public class InputView {
 
-	public static final String GAME_START = "다리 건너기 게임을 시작합니다.";
-
-	/**
-	 * 다리의 길이를 입력받는다.
-	 */
 	public int readBridgeSize() {
 		Message.printMessage(Message.INPUT_BRIDGE_LENGTH);
 		return Integer.parseInt(CheckHandler.isBridgeLengthInput(Console.readLine()));
 	}
 
-	/**
-	 * 사용자가 이동할 칸을 입력받는다.
-	 */
 	public String readMoving() {
 		Message.printMessage(Message.INPUT_MOVE_COMMAND);
 		return CheckHandler.isMoveInput(Console.readLine());
 	}
 
-	/**
-	 * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
-	 */
 	public String readGameCommand() {
 		Message.printMessage(Message.INPUT_GAME_COMMAND);
 		return CheckHandler.isGameCommandInput(Console.readLine());
