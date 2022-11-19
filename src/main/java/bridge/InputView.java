698fbdfb762b603e262696830e7bc1dba63807f7
heozzi <hz0819@naver.com>
2022-11-19 17:24:25 +0900

    1차 완료

--- src/main/java/bridge/InputView.java
+++ src/main/java/bridge/InputView.java
@@ -1,28 +1,60 @@
 package bridge;
 
+import camp.nextstep.edu.missionutils.Console;
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
  */
 public class InputView {
-
+    static String Regex = "[^0-9]";
     /**
      * 다리의 길이를 입력받는다.
      */
     public int readBridgeSize() {
-        return 0;
+        String InputVal = Console.readLine();
+        try {
+            int BridgeSize = Integer.parseInt(InputVal);
+            if (InputVal.replaceAll(Regex, "").length() != InputVal.length()) {
+                throw new IllegalArgumentException("[ERROR] 다리 길이는 3부터 20 사이의 숫자여야 합니다.");
+            }
+
+            if (BridgeSize < 3 || BridgeSize > 20) {
+                throw new IllegalArgumentException("[ERROR] 다리 길이는 3부터 20 사이의 숫자여야 합니다.");
+            }
+        }
+        catch (IllegalArgumentException e) {
+            System.out.println("[ERROR] 다리 길이는 3부터 20 사이의 숫자여야 합니다.");
+            readBridgeSize();
+        }
+        return Integer.parseInt(InputVal);
     }
 
     /**
      * 사용자가 이동할 칸을 입력받는다.
      */
     public String readMoving() {
-        return null;
+
+        String InputVal = Console.readLine();
+        try {
+            if (((!InputVal.contains("U") && !InputVal.contains("D"))) || InputVal.length() > 1) {
+                throw new IllegalArgumentException("[ERROR] U,D 이외의 값을 입력하였습니다.");
+            }
+        }
+        catch (IllegalArgumentException e) {
+            System.out.println("[ERROR] 다리 길이는 3부터 20 사이의 숫자여야 합니다.");
+            readMoving();
+        }
+        return InputVal;
+
     }
 
     /**
      * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
      */
     public String readGameCommand() {
-        return null;
+        String InputVal = Console.readLine();
+        if ((InputVal.contains("R") || InputVal.contains("Q")) && InputVal.length()==1) {
+            return InputVal;
+        }
+        throw new IllegalArgumentException("[ERROR] R,Q 이외의 값을 입력하였습니다.");
     }
 }
--- src/main/java/bridge/OutputView.java
+++ src/main/java/bridge/OutputView.java
@@ -1,16 +1,34 @@
 package bridge;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * 사용자에게 게임 진행 상황과 결과를 출력하는 역할을 한다.
  */
 public class OutputView {
+
+    private String MakeMap(List<String> NowBorad,int NowValue,String CheckC) {
+        String vstr = "[";
+        for (int i=0; i<NowBorad.size();i++) {
+            String tmpstr = "   ";
+            if (NowBorad.get(i).contains(CheckC)) tmpstr = " O ";
+            if (i==NowBorad.size()-1 && NowValue==0 && NowBorad.get(i).contains(CheckC)) tmpstr = " X ";
+            vstr = vstr.concat(tmpstr);
+            if (i<NowBorad.size()-1) vstr = vstr.concat("|");
+        }
+        vstr = vstr.concat("]");
+        return vstr;
+    }
 
     /**
      * 현재까지 이동한 다리의 상태를 정해진 형식에 맞춰 출력한다.
      * <p>
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void printMap() {
+    public void printMap(List<String> NowBoard,int NowValue) {
+        System.out.println(MakeMap(NowBoard,NowValue,"U"));
+        System.out.println(MakeMap(NowBoard,NowValue,"D"));
     }
 
     /**
@@ -18,6 +36,8 @@ public class OutputView {
      * <p>
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void printResult() {
+    public void printResult(String Result,int TryNumber) {
+        System.out.println("게임 성공 여부: "+Result);
+        System.out.println("총 시도한 횟수: "+TryNumber);
     }
 }
