2b6500a79ab549906ce86cf3ca041d8fde5552a5
김현재 <41482946+galug@users.noreply.github.com>
2022-11-20 01:24:25 +0900

    feat(bridgecontroller): 다리의 길이를 입력받고 다리를 생성하는 기능 추가

---
+++ src/main/java/bridge/controller/BridgeController.java
@@ -0,0 +1,33 @@
+package bridge.controller;
+
+import bridge.BridgeMaker;
+import bridge.BridgeRandomNumberGenerator;
+import bridge.model.Bridge;
+import bridge.view.InputView;
+import bridge.view.OutputView;
+
+import java.util.List;
+
+public class BridgeController {
+    private final InputView inputView = new InputView();
+    private final OutputView outputView = new OutputView();
+    private final BridgeMaker bridgeMaker = new BridgeMaker(new BridgeRandomNumberGenerator());
+
+    public Bridge createBridge() {
+        try {
+            int bridgeSize = inputBridgeSize();
+            List<String> bridgeProtoType = bridgeMaker.makeBridge(bridgeSize);
+            Bridge bridge = new Bridge(bridgeProtoType);
+            return bridge;
+        } catch (IllegalArgumentException illegalArgumentException) {
+            return createBridge();
+        }
+    }
+
+    private int inputBridgeSize() {
+        outputView.printRequestBridgeSizeInput();
+        int bridgeSize = inputView.readBridgeSize();
+        return bridgeSize;
+    }
+
+}
