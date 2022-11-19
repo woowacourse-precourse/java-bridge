b2031c574df8d06301a79b069788670ec3338474
ddubidubap <park62000@gmail.com>
2022-11-20 00:47:08 +0900

    feat: 오답일 경우 재시작 여부 입력 받기
    재시작 O: 답안 초기화
    재시작 X: 게임 종료

--- src/main/java/bridge/controller/GameController.java
+++ src/main/java/bridge/controller/GameController.java
@@ -12,11 +12,24 @@ import java.util.List;
 public class GameController {
     InputView inputView = new InputView();
     OutputView outputView = new OutputView();
+    private boolean gaming = true;
+    private int bridgeNum = 0;
 
     private List<String> prepareBridge(int bridgeSize) {
         BridgeNumberGenerator bridgeNumberGenerator = new BridgeRandomNumberGenerator();
         BridgeMaker bridgeMaker = new BridgeMaker(bridgeNumberGenerator);
         return bridgeMaker.makeBridge(bridgeSize);
+    }
+
+    private void failed(BridgeGame bridgeGame) {
+        String command = inputCommand();
+        if (command.equals("R")) {
+            bridgeGame.retry();
+            bridgeNum = 0;
+        }
+        if (command.equals("Q")) {
+            gaming = false;
+        }
     }
 
     private int inputBridgeSize() {
