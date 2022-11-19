4d305c4a7ee0cc79c646fbd0633fc9927104bd30
Nitroblack1 <109022435+Nitroblack1@users.noreply.github.com>
2022-11-19 14:13:55 +0900

    feat(test\...\UITeset) : implement test for InputView test

---
+++ src/test/java/bridge/UITest/InputViewTest.java
@@ -0,0 +1,44 @@
+package bridge.UITest;
+
+import bridge.UI.InputView;
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+import org.mockito.Mock;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.assertj.core.api.Assertions.in;
+
+public class InputViewTest {
+
+    private static final String ERROR_MESSAGE = "[ERROR]";
+
+    @Mock
+    // given
+    InputView inputView = new InputView();
+
+    @DisplayName("다리의 길이가 올바른 형식으로 입력되었는지 확인한 후 받는다.")
+    @Test
+    void readBridgeLengthTest() {
+        // when
+        String expected = "3";
+        InputStream in = new ByteArrayInputStream(expected.getBytes());
+        System.setIn(in);
+
+        //then
+        assertThat(inputView.readBridgeLength()).isEqualTo(Integer.parseInt(expected));
+    }
+
+    @DisplayName("다리의 길이가 올바르지 않은 형식이라면 예외를 던진다.")
+    @ParameterizedTest
+    @ValueSource(strings = {"2", "21", "", " ", "#", "q"})
+    void readBridgeLengthExceptionHandlingTest(String testCase) {
+        assertThatThrownBy(() -> inputView.validateBridgeLengthInput(testCase))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(ERROR_MESSAGE);
+    }
+}
