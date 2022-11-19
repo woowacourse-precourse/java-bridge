5d174d886e5f4f4e28b0d5c3663b1095daf1f5f5
EUNJEONGMUN <munej26@gmail.com>
2022-11-19 12:29:47 +0900

    refactor: BridgeMap을 BridgeGame 내부로 이동

--- src/main/java/bridge/domain/Movement.java
+++ src/main/java/bridge/domain/Movement.java
@@ -1,13 +1,13 @@
 package bridge.domain;
 
 import bridge.constant.Constant;
+import bridge.domain.Bridge;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class Movement {
     private final Bridge bridge;
-    private final BridgeMap bridgeMap = new BridgeMap();
     private List<String> movement = new ArrayList<>();
     private int tryCount = Constant.INITIAL_COUNT;
 
@@ -22,28 +22,15 @@ public class Movement {
 
     public void saveMoving(String moving) {
         this.movement.add(moving);
-        saveCompareResult(moving);
     }
+
     public boolean isFinish() {
         return bridge.isFinish(movement);
     }
 
     public void clearMoving() {
         this.movement = new ArrayList<>();
-        bridgeMap.clearMap();
         tryCount++;
-    }
-
-    public String[] getMap() {
-        return bridgeMap.getMap();
-    }
-
-    public void saveCompareResult(String moving) {
-        String mark = Constant.CORRECT_MARK;
-        if (!canMove()) {
-            mark = Constant.WRONG_MARK;
-        }
-        bridgeMap.addMap(moving, mark);
     }
 
     public int getTryCount() {
--- src/main/java/bridge/view/OutputView.java
+++ src/main/java/bridge/view/OutputView.java
@@ -1,20 +1,18 @@
 package bridge.view;
 
-import bridge.constant.Messages;
 import bridge.domain.Movement;
+import bridge.constant.Messages;
 
 public class OutputView {
 
-    public static void printMap(Movement movement) {
-        String[] map = movement.getMap();
+    public static void printMap(String[] map) {
         for (String s : map) {
             System.out.println("[ "+s+" ]");
         }
     }
-
-    public static void printResult(Movement movement) {
+    public static void printResult(String[] map, Movement movement) {
         System.out.println(Messages.FINISH_RESULT_MESSAGE.getMessage());
-        printMap(movement);
+        printMap(map);
         System.out.println(String.format(Messages.SUCCESS_INFO_MESSAGE.getMessage(), movement.isSuccess()));
         System.out.println(String.format(Messages.TRY_COUNT_INFO_MESSAGE.getMessage(), movement.getTryCount()));
     }
