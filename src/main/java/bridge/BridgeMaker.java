ac6dfc075c459a4339a3d6aeebe5eae0d1a2877e
rabbit0216 <rabbit1999k@gmail.com>
2022-11-19 14:44:55 +0900

    feat: 다리 생성 함수 구현

--- src/main/java/bridge/BridgeMaker.java
+++ src/main/java/bridge/BridgeMaker.java
@@ -1,6 +1,9 @@
 package bridge;
 
+import java.util.ArrayList;
 import java.util.List;
+
+import static bridge.Constants.*;
 
 /**
  * 다리의 길이를 입력 받아서 다리를 생성해주는 역할을 한다.
@@ -18,6 +21,27 @@ public class BridgeMaker {
      * @return 입력받은 길이에 해당하는 다리 모양. 위 칸이면 "U", 아래 칸이면 "D"로 표현해야 한다.
      */
     public List<String> makeBridge(int size) {
-        return null;
+        List<String> bridgeList = new ArrayList<>();
+        for(int count = 0; count < size; count++) {
+            try {
+                int number = bridgeNumberGenerator.generate();
+                String bridgeShape = discriminateBridgeShape(number);
+                bridgeList.add(bridgeShape);
+            } catch(IllegalArgumentException e){
+                e.printStackTrace();
+            }
+        }
+        return bridgeList;
+    }
+
+    String discriminateBridgeShape(int bridgeNumber){
+        if(bridgeNumber == UP) {
+           return "U";
+        }
+        if(bridgeNumber == DOWN){
+            return "D";
+        }
+        System.out.println(ERROR_PREFIX + RANDOM_ERROR);
+        throw new IllegalArgumentException(ERROR_PREFIX + RANDOM_ERROR);
     }
 }
