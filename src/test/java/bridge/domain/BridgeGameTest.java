711f0120fc84cedd2a128ba2191fe0e6dbd5b24a
semi-cloud <sksmsrkdtpal@naver.com>
2022-11-19 13:55:21 +0900

    test(BridgeGame): 다리 건너면 현재 위치가 변하는 기능 테스트
    현재 위치에서 1 증가

---
+++ src/test/java/bridge/domain/BridgeGameTest.java
@@ -0,0 +1,30 @@
+package bridge.domain;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class BridgeGameTest {
+
+    private BridgeGame bridgeGame;
+
+    @BeforeEach
+    void init() {
+        Bridge bridge = Bridge.valueOf(List.of("U", "D", "U"));
+        bridgeGame = new BridgeGame(bridge);
+    }
+
+    @DisplayName("다리를 건넌다면 현재 위치가 1 증가한다.")
+    @Test
+    void increasePosition() {
+        int curPosition = bridgeGame.getPosition();
+
+        bridgeGame.move();
+
+        assertThat(bridgeGame.getPosition()).isEqualTo(curPosition + 1);
+    }
+}
