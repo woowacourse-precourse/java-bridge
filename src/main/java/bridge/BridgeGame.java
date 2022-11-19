ff52b36bdc22eb9df8b8e329d533994944f5f511
masiljangajji <hjk172262@gmail.com>
2022-11-19 13:12:22 +0900

    feat: 저장된 다리를 반환하는 기능 추가

--- src/main/java/bridge/BridgeGame.java
+++ src/main/java/bridge/BridgeGame.java
@@ -7,41 +7,40 @@ import java.util.List;
 public class BridgeGame {
 
     private String bridgeSize;
-    private List<String> userMove=new ArrayList<>();
+    private List<String> userMove = new ArrayList<>();
     private String retry;
-    private List<String> bridge=new ArrayList<>();
+    private List<String> bridge = new ArrayList<>();
 
-    public void bridgeSize(String bridgeSize){
-        this.bridgeSize=bridgeSize;
+    public void bridgeSize(String bridgeSize) {
+        this.bridgeSize = bridgeSize;
     }
+
     public void move(String userMove) {
         this.userMove.add(userMove);
     }
 
-    public void retry(String retry){
-        this.retry=retry;
+    public void retry(String retry) {
+        this.retry = retry;
     }
 
-    public void bridge(List<String>bridge){
-        this.bridge=bridge;
+    public void bridge(List<String> bridge) {
+        this.bridge = bridge;
     }
 
-    public int getBridgeSize(){
+    public int getBridgeSize() {
         return Integer.valueOf(bridgeSize);
     }
 
-    public List<String> getMoveList(){
+    public List<String> getMoveList() {
         return userMove;
     }
 
-    public String getRetry(){
+    public String getRetry() {
         return retry;
     }
 
-
-
-
-
-
+    public List<String> getBridge() {
+        return bridge;
+    }
 
 }
