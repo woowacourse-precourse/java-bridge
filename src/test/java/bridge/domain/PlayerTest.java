5b875c08ecf8f2f514862d26475ff1c93f3e61dd
code-tree <dltjdqls1200@naver.com>
2022-11-19 14:54:26 +0900

    test: Player의 다리 이동 테스트 코드 작성

--- src/test/java/bridge/domain/PlayerTest.java
+++ src/test/java/bridge/domain/PlayerTest.java
@@ -1,4 +1,28 @@
-import static org.junit.jupiter.api.Assertions.*;
+package bridge.domain;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+import java.util.Map;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 class PlayerTest {
-  
+    @Test
+    public void playerMoveTest() {
+        //given
+        Bridge bridge = new Bridge(List.of("U", "U", "D", "U"));
+        Player player = new Player();
+
+        //when
+        player.move(bridge, "U");
+        player.move(bridge, "U");
+        player.move(bridge, "D");
+        player.move(bridge, "D");
+
+        //then
+        Map<Move, List<String>> playerMoved = player.getPlayerMoved();
+        assertThat(playerMoved.get(Move.UP)).isEqualTo(List.of("O", "O", " ", " "));
+        assertThat(playerMoved.get(Move.DOWN)).isEqualTo(List.of(" ", " ", "O", "X"));
+    }
 }
