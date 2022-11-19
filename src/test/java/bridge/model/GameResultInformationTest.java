e5c9f67af226a17bec37a27b7f8cbb50a1e34628
Sehbeom <j3beom@gmail.com>
2022-11-20 02:17:31 +0900

    test(model): 게임 결과 정보 모델 테스트
    게임 결과 정보 모델에 대한 테스트 코드 작성

---
+++ src/test/java/bridge/model/GameResultInformationTest.java
@@ -0,0 +1,30 @@
+package bridge.model;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import bridge.constants.GameResult;
+import bridge.domain.model.GameResultInformation;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class GameResultInformationTest {
+
+    @DisplayName("게임 성공 여부를 설정하고, 반환받을 수 있다.")
+    @Test
+    void setAndGetGameResultTest() {
+        GameResultInformation.setGameResult(GameResult.SUCCESS);
+
+        assertThat(GameResultInformation.getGameResult()).isEqualTo(GameResult.SUCCESS);
+    }
+
+    @DisplayName("시도한 횟수를 증가시키고, 반환받을 수 있다.")
+    @Test
+    void increaseAndGetCountOfTry() {
+        int expected = 2;
+
+        GameResultInformation.increaseCountOfTry();
+        GameResultInformation.increaseCountOfTry();
+
+        assertThat(GameResultInformation.getCountOfTry()).isEqualTo(expected);
+    }
+}
