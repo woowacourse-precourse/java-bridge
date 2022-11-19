fd27be4f2c4a92a6aeea97f7368fed4330acd7e8
sangwon <swchoi1997@naver.com>
2022-11-19 13:59:36 +0900

    feat: 다리를 건넜는지 판별하는 기능 추가
    현재 사용자가 건넌 정보를 bridge 로 보내어 판단한 값을 리턴한다.

--- src/main/java/bridge/model/Score.java
+++ src/main/java/bridge/model/Score.java
@@ -13,9 +13,18 @@ public class Score {
         this.retryCount = 1;
     }
 
+    public Score(List<String> userStep) {
+        this.userStep = userStep;
+        this.retryCount = 1;
+    }
+
     public boolean goOneStep(Bridge bridge, String moving) {
         this.userStep.add(moving);
         return bridge.canGoOrNot(this.userStep, moving);
+    }
+
+    public boolean isCrossing(Bridge bridge) {
+        return bridge.isCrossing(this.userStep.size());
     }
 
 }
