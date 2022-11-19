5d227eb398a7a107d9b8ecf6dff0ba9df1dda160
E1psycongr00 <39326175+E1psycongr00@users.noreply.github.com>
2022-11-19 14:11:18 +0900

    test(bridgeGame): 움직임 명령시 다리 상태 영속화 테스트 완료

--- src/test/java/bridge/BridgeGameTest.java
+++ src/test/java/bridge/BridgeGameTest.java
@@ -5,6 +5,8 @@ import static org.mockito.ArgumentMatchers.anyInt;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
+import bridge.domain.BridgeStatus;
+import java.lang.reflect.Field;
 import java.util.List;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -23,6 +25,21 @@ class BridgeGameTest {
 
         // when
         bridgeGame.createBridge(3);
+
+        // then
+        assertThat(bridgeStatusSaver.getBridgeStatus()).isNotNull();
+    }
+
+    @DisplayName("움직임 명령 요청시 영속화 테스트")
+    @Test
+    void persistBridgeStatusWhenRequestMovingCommand() {
+        // given
+        when(bridgeMaker.makeBridge(anyInt())).thenReturn(List.of("U", "D", "U"));
+        bridgeGame.createBridge(3);
+        BridgeStatus original = bridgeStatusSaver.getBridgeStatus();
+
+        // when
+        bridgeGame.move("U");
 
         // then
         assertThat(bridgeStatusSaver.getBridgeStatus()).isNotNull();
