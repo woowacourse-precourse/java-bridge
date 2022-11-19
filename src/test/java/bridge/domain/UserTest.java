8b41b4c2f7bf71ebfba07aa4666969f073a95fcd
kimtaesoo99 <107785279+kimtaesoo99@users.noreply.github.com>
2022-11-20 00:10:25 +0900

    test: (UserTest) User 도메인 테스트 구현

---
+++ src/test/java/bridge/domain/UserTest.java
@@ -0,0 +1,75 @@
+package bridge.domain;
+
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class UserTest {
+
+    @DisplayName("시도 횟수 getter 테스트")
+    @Test
+    public void getTryCountTest() {
+        //given
+
+        //when
+        User user = new User();
+
+        //then
+        assertThat(user.getTryCount()).isEqualTo(1);
+    }
+
+
+    @DisplayName("시도 횟수 증가 테스트")
+    @Test
+    public void increaseTryCountTest() {
+        //given
+        User user = new User();
+
+        //when
+        user.increaseTryCount();
+
+        //then
+        assertThat(user.getTryCount()).isEqualTo(2);
+    }
+
+    @DisplayName("게임 성공 테스트")
+    @Test
+    public void isSuccessCompleteTest() {
+        //given
+        User user = new User();
+
+        //when
+        user.setSuccess();
+
+        //then
+        assertThat(user.isSuccessComplete()).isEqualTo(true);
+    }
+
+    @DisplayName("게임 실패 테스트")
+    @Test
+    public void isNotSuccessCompleteTest() {
+        //given
+        User user = new User();
+
+        //when
+
+        //then
+        assertThat(user.isSuccessComplete()).isEqualTo(false);
+    }
+
+
+    @DisplayName("게임 종료 테스트")
+    @Test
+    public void isGameOverTest() {
+        //given
+        User user = new User();
+
+        //when
+        user.setGameOver();
+
+        //then
+        assertThat(user.isGameOver()).isEqualTo(true);
+    }
+}
