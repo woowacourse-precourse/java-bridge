db41648105c3c8405ce82f391a982ef49a6bf5a9
김태용 <amico741@naver.com>
2022-11-19 16:24:16 +0900

    test: 로직 리팩토링으로 인해 테스트 코드 제거

--- src/test/java/bridge/BridgeTest.java
+++ src/test/java/bridge/BridgeTest.java
@@ -19,21 +19,9 @@ public class BridgeTest {
     @BeforeEach
     void setUp() {
         bridge = new Bridge(List.of("D", "U", "D", "U"));
-        success = new Player();
-        fail = new Player();
+        success = new Player(4);
+        fail = new Player(4);
         List.of("D", "U").forEach(e -> success.inputDirection(e));
         List.of("D", "U", "D", "D").forEach(e -> fail.inputDirection(e));
-    }
-
-    @Test
-    @DisplayName("플레이어와 입력과 같다면 true를 return한다.")
-    void 플레이어와_입력이_같다면_정상_true() {
-        assertThat(bridge.hasSameDirection(success)).isTrue();
-    }
-
-    @Test
-    @DisplayName("플레이어와 입력과 같지 않다면 false를 return한다.")
-    void 플레이어와_입력이_같지_않다면_false() {
-        assertThat(bridge.hasSameDirection(fail)).isFalse();
     }
 }
