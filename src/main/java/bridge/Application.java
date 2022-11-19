3f658fc91750b555e16fbc7544c5ba248aa8e8a7
Cheon-DM <76736548+Cheon-DM@users.noreply.github.com>
2022-11-20 01:39:31 +0900

    feat(main): 다리 건너기 실행

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -1,8 +1,12 @@
 package bridge;
 
+import bridge.view.InputView;
+import bridge.view.OutputView;
+
 public class Application {
 
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        Controller controller = new Controller(new InputView(), new OutputView());
+        controller.run();
     }
 }
