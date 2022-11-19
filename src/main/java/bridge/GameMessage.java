9350875a26dcb471e78c9fe7fb215bb4e5f04e6e
cmong0516 <cmong0516@gmail.com>
2022-11-19 20:55:36 +0900

    feat(GameMessage Enum): Add Enum

--- src/main/java/bridge/GameMessage.java
+++ src/main/java/bridge/GameMessage.java
@@ -6,7 +6,8 @@ public enum GameMessage {
     REQUEST_MOVING_MESSAGE("이동할 칸을 선택해주세요. (위: U, 아래: D)"),
     REQUEST_RETRY_MESSAGE("게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)"),
     GAME_RESULT_MESSAGE("최종 게임 결과"),
-    GAME_SUC_FAIL_MESSAGE("게임 성공 여부: "),
+    GAME_SUC_MESSAGE("게임 성공 여부: 성공"),
+    GAME_FAIL_MESSAGE("게임 성공 여부: 실패"),
     GAME_TRY_CNT_MESSAGE("총 시도한 횟수: ");
 
 
