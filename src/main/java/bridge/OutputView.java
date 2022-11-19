79ee63bafb4d3cd8db489aa1a1c3de71a3269d97
limzzum <chfhddl1234@naver.com>
2022-11-19 20:03:42 +0900

    feat : 이동한 다리 출력

--- src/main/java/bridge/OutputView.java
+++ src/main/java/bridge/OutputView.java
@@ -1,18 +1,53 @@
 package bridge;
 
+import java.util.List;
+
 /**
  * 사용자에게 게임 진행 상황과 결과를 출력하는 역할을 한다.
  */
 public class OutputView {
+    private final String SUCCESS = "O";
+    private final String UP = "U";
+    private final String DOWN = "D";
+
 
     /**
      * 현재까지 이동한 다리의 상태를 정해진 형식에 맞춰 출력한다.
      * <p>
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void printMap() {
+    public void printMap(List<String> visited,String direction, String result) {
+        upBridge(visited, direction, result);
+        downBridge(visited, direction, result);
     }
 
+    private void upBridge(List<String> visited, String direction, String result) {
+        System.out.println("[");
+        for (String visit : visited) {
+            checkEqual(visit, UP,SUCCESS);
+            System.out.println("|");
+        }
+        checkEqual(direction,UP,result);
+        System.out.println("]");
+    }
+
+    private void downBridge(List<String> visited, String direction, String result) {
+        System.out.println("[");
+        for (String visit : visited) {
+            checkEqual(visit, DOWN, SUCCESS);
+            System.out.println("|");
+        }
+        checkEqual(direction, DOWN, result);
+        System.out.println("]");
+    }
+
+    private void checkEqual(String visited,String direction, String result) {
+        if(visited.equals(direction)){
+            System.out.println(result);
+            return;
+        }
+        System.out.println(" ");
+    }
     /**
      * 게임의 최종 결과를 정해진 형식에 맞춰 출력한다.
      * <p>
