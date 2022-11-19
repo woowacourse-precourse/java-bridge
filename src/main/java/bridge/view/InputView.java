b24949da9c87959f615ea6455b55fa106ce674e1
ysng_ysng <yeasung67@gmail.com>
2022-11-20 01:59:23 +0900

    feat(InputView): Implement the methods to print input message
    유저의 입력을 요구하는 메시지를 출력하는 메서드를 구현했습니다.

--- src/main/java/bridge/view/InputView.java
+++ src/main/java/bridge/view/InputView.java
@@ -1,28 +1,33 @@
 package bridge.view;
 
+import camp.nextstep.edu.missionutils.Console;
+
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
  */
 public class InputView {
+	// todo: 출력 양식 맞출 필요가 있음
+	/**
+	 * 다리의 길이를 입력받는다.
+	 */
+	public String readBridgeSize() {
+		System.out.println(InputMessages.BRIDGE_LENGTH_REQUEST);
+		return Console.readLine();
+	}
 
-    /**
-     * 다리의 길이를 입력받는다.
-     */
-    public int readBridgeSize() {
-        return 0;
-    }
+	/**
+	 * 사용자가 이동할 칸을 입력받는다.
+	 */
+	public String readMoving() {
+		System.out.println(InputMessages.MOVE_REQUEST);
+		return Console.readLine();
+	}
 
-    /**
-     * 사용자가 이동할 칸을 입력받는다.
-     */
-    public String readMoving() {
-        return null;
-    }
-
-    /**
-     * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
-     */
-    public String readGameCommand() {
-        return null;
-    }
+	/**
+	 * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
+	 */
+	public String readGameCommand() {
+		System.out.println(InputMessages.QUIT_OR_RETRY);
+		return Console.readLine();
+	}
 }
