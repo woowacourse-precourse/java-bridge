38db2b727d2fc4caa87763e4d02cb76a913673bb
swonny <wonny921@gmail.com>
2022-11-19 20:41:28 +0900

    feat(BridgeGame): 게임 재도전을 위한 환경 세팅 구현

--- src/main/java/bridge/BridgeGame.java
+++ src/main/java/bridge/BridgeGame.java
@@ -20,11 +20,6 @@ public class BridgeGame {
         this.trial = 0;
     }
 
-    /**
-     * 사용자가 칸을 이동할 때 사용하는 메서드
-     * <p>
-     * 이동을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
-     */
     private GameStatus move() {
         player.moveForward();
         if (bridge.getLastIndex() == player.getCurrentPosition()) {
@@ -34,21 +29,22 @@ public class BridgeGame {
     }
 
     public GameStatus tryMoveTo(String squareToMove) {
+        addTrial();
         if (canMoveTo(squareToMove)) {
             return move();
         }
         return GameStatus.FAILURE;
     }
 
+    private void addTrial() {
+        this.trial++;
+    }
+
     private boolean canMoveTo(String squareToMove) {
         return bridge.isNextAvailable(squareToMove, player.getCurrentPosition());
     }
 
-    /**
-     * 사용자가 게임을 다시 시도할 때 사용하는 메서드
-     * <p>
-     * 재시작을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
-     */
     public void retry() {
+        player.initializePosition();
     }
 }
