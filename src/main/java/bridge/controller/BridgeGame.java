c37dcd577f98b397b345fee3969c0c03db2da88f
squareyun <squareyun@gmail.com>
2022-11-19 20:55:33 +0900

    feat(bridge): 다리의 길이 입력 받기

---
+++ src/main/java/bridge/controller/BridgeGame.java
@@ -0,0 +1,47 @@
+package bridge.controller;
+
+import static org.assertj.core.util.Lists.newArrayList;
+
+import bridge.BridgeMaker;
+import bridge.BridgeNumberGenerator;
+import bridge.view.InputView;
+import bridge.view.OutputView;
+
+/**
+ * 다리 건너기 게임을 관리하는 클래스
+ */
+public class BridgeGame {
+    private final BridgeNumberGenerator bridgeNumberGenerator;
+    private final BridgeMaker bridgeMaker;
+    private final InputView inputView;
+    private final OutputView outputView;
+    private int bridgeSize;
+
+    public BridgeGame(BridgeNumberGenerator bridgeNumberGenerator) {
+        this.bridgeNumberGenerator = bridgeNumberGenerator;
+        this.bridgeMaker = new BridgeMaker(bridgeNumberGenerator);
+        this.inputView = new InputView();
+        this.outputView = new OutputView();
+        this.bridgeSize = inputView.readBridgeSize();
+    }
+
+    public void start() {
+
+    }
+
+    /**
+     * 사용자가 칸을 이동할 때 사용하는 메서드
+     * <p>
+     * 이동을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
+     */
+    public void move() {
+    }
+
+    /**
+     * 사용자가 게임을 다시 시도할 때 사용하는 메서드
+     * <p>
+     * 재시작을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
+     */
+    public void retry() {
+    }
+}
