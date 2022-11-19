b6d36992f039ffb218c952e6377c5dc0882f65ab
JIHYEISWISDOM <wlgp8958@naver.com>
2022-11-19 22:44:02 +0900

    feat(TextValidator): U, D가 아닌 경우 예외 처리

--- src/main/java/bridge/validator/TextValidator.java
+++ src/main/java/bridge/validator/TextValidator.java
@@ -1,5 +1,11 @@
 package bridge.validator;
 
 public class TextValidator {
+    private final static String MOVE_WAY_ERROR_MESSAGE = "[ERROR] 이동 방향은 U또는 D만 입력 가능합니다.";
 
+    public static void validateMoveWay(String input) {
+        if (!input.equals("U") && !input.equals("D")) {
+            throw new IllegalArgumentException(MOVE_WAY_ERROR_MESSAGE);
+        }
+    }
 }
