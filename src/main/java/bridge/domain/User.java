8691f166cc6d787600998f5e607899b4e65665c3
sosow0212 <sosow0212@naver.com>
2022-11-19 16:34:20 +0900

    feat : (User) 게임 성공 유무를 판단하는 필드 및 필드를 다루는 메서드 추가

--- src/main/java/bridge/domain/User.java
+++ src/main/java/bridge/domain/User.java
@@ -2,15 +2,33 @@ package bridge.domain;
 
 public class User {
     private boolean isGameDoneStatus;
+    private boolean isGameSuccess;
     private int gameTryCount;
 
     public User() {
         this.gameTryCount = 1;
         this.isGameDoneStatus = false;
+        this.isGameSuccess = false;
+    }
+
+    public boolean isGameSuccess() {
+        return this.isGameSuccess;
+    }
+
+    public void setGameSuccess(boolean gameSuccess) {
+        isGameSuccess = gameSuccess;
+    }
+
+    public void gameSuccess() {
+        setGameSuccess(true);
+    }
+
+    public void gameFail() {
+        setGameSuccess(false);
     }
 
     public boolean isGameDoneStatus() {
-        return isGameDoneStatus;
+        return this.isGameDoneStatus;
     }
 
     public void setGameDoneStatus(boolean gameDoneStatus) {
