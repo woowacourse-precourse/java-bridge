b33334600a9c29bfd2a08a554b4160a40c1f62d0
김동규 <2016025196@hanyang.ac.kr>
2022-11-19 16:59:03 +0900

    에러코드용 Enum 작성

---
+++ src/main/java/Constant/ErrorMessage.java
@@ -0,0 +1,13 @@
+package Constant;
+
+public enum ErrorMessage {
+    INVALID_DIGIT("입력받은 내용이 숫자가 아닙니다.");
+    private static String errorBase="[ERROR]";
+    private final String errorMsg;
+    ErrorMessage(String s) {
+        this.errorMsg=s;
+    }
+    public String getErrorMsg(){
+        return errorBase+errorMsg;
+    }
+}
