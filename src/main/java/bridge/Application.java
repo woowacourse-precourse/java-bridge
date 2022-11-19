38a80f26e509e543b1a0053216f84940c209b5ac
Your Name <you@example.com>
2022-11-19 16:12:44 +0900

    feat: 최종 메인 작성

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -1,8 +1,76 @@
 package bridge;
 
+import java.util.List;
+
 public class Application {
 
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        OutputView outputView = new OutputView();
+        InputView inputView = new InputView();
+
+        startGame(outputView, inputView);
+    }
+
+    private static void startGame(OutputView outputView, InputView inputView) {
+        BridgeGame bridgeGame = new BridgeGame();
+
+        makeBridge(outputView, inputView, bridgeGame);
+
+        while (inGame(outputView, inputView, bridgeGame)) {
+            if (bridgeGame.gameSucces())
+                break;
+        }
+
+        outputView.printResult(bridgeGame.createResultMap(), bridgeGame.getTryNum(), bridgeGame.getCurrentState());
+    }
+
+    private static boolean inGame(OutputView outputView, InputView inputView,
+            BridgeGame bridgeGame) {
+        String gameCommand = "";  
+        if (!move(outputView, inputView, bridgeGame)) {
+            printMap(outputView, bridgeGame);
+            gameCommand = gameEnd(outputView, inputView);
+        }
+        printMap(outputView, bridgeGame);
+        
+        return continueGame(gameCommand, bridgeGame);
+    }
+
+    private static boolean continueGame(String gameCommand, BridgeGame bridgeGame) {
+        if (gameCommand == "Q")
+            return false;
+        if (gameCommand == "R")
+            bridgeGame.retry();
+        return true;
+    }
+
+    private static String gameEnd(OutputView outputView, InputView inputView) {
+        String gameCommand;
+        outputView.printGameOver();
+        gameCommand = inputView.readGameCommand();
+        return gameCommand;
+    }
+
+    private static void printMap(OutputView outputView, BridgeGame bridgeGame) {
+        String currentMap = bridgeGame.createResultMap();
+        outputView.printMap(currentMap);
+    }
+
+    private static boolean move(OutputView outputView, InputView inputView, BridgeGame bridgeGame) {
+        outputView.printSelectMove();
+        String playerMove = inputView.readMoving();
+        
+        return bridgeGame.move(playerMove);
+    }
+
+    private static void makeBridge(OutputView outputView, InputView inputView,
+            BridgeGame bridgeGame) {
+        BridgeNumberGenerator bridgeNumberGenerator = new BridgeRandomNumberGenerator();
+        BridgeMaker bridgeMaker = new BridgeMaker(bridgeNumberGenerator);
+
+        outputView.printStart();
+        int bridgeSize = inputView.readBridgeSize();
+        List<String> bridge = bridgeMaker.makeBridge(bridgeSize);
+        bridgeGame.setBridge(bridge);
     }
 }
