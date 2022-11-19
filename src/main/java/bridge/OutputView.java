77a8f265ee1485b88ec4b289c1a247e5a0fb2c59
swonny <wonny921@gmail.com>
2022-11-20 01:44:43 +0900

    feat(OutputView): 게임 시작, 다리 길이 입력, 이동할 칸 입력, 게임 지속 여부 입력 안내문구 출력 기능 구현

--- src/main/java/bridge/OutputView.java
+++ src/main/java/bridge/OutputView.java
@@ -1,5 +1,7 @@
 package bridge;
 
+import enumCollections.GuideMessage;
+
 /**
  * 사용자에게 게임 진행 상황과 결과를 출력하는 역할을 한다.
  */
@@ -20,5 +22,13 @@ public class OutputView {
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
     public void printResult() {
+    }
+
+    public void printAskGameCommand() {
+        printGuideMessage(GuideMessage.GET_GAME_COMMAND);
+    }
+
+    public void printGuideMessage(GuideMessage situation) {
+        System.out.println(GuideMessage.getMessage(situation));
     }
 }
