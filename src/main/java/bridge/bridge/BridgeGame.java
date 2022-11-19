2d66786de6b2e7c07962b2d9b5fa7617431fc103
Min Ho CHO <chominho96@naver.com>
2022-11-19 20:14:49 +0900

    feat(bridge game): add bridge move method
    1. Added makeBridge method to BridgeGame
    2. Added move method to BridgeGame

--- src/main/java/bridge/bridge/BridgeGame.java
+++ src/main/java/bridge/bridge/BridgeGame.java
@@ -1,23 +1,61 @@
 package bridge.bridge;
 
+import bridge.BridgeRandomNumberGenerator;
+
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * 다리 건너기 게임을 관리하는 클래스
+ * @version 1.0.0
+ * @author CHO Min Ho
  */
 public class BridgeGame {
 
+    private List<String> bridge = new ArrayList<>();
+
     /**
-     * 사용자가 칸을 이동할 때 사용하는 메서드
-     * <p>
-     * 이동을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
+     * 다리를 생성하는 메서드입니다.
+     * @param size 다리의 크기
      */
-    public void move() {
+    public void makeBridge(int size) {
+        BridgeMaker bridgeMaker = new BridgeMaker(new BridgeRandomNumberGenerator());
+        bridge = bridgeMaker.makeBridge(size);
+    }
+
+    /**
+     * 사용자가 칸을 이동할 때 사용하는 메서드입니다.
+     * 옳은 길을 선택한 경우, 게임 결과를 출력하고 성공한 경우에 해당하는 메서드가 실행됩니다.
+     * 틀린 길을 선택한 경우, 게임 결과를 출력하고 실패한 경우에 해당하는 메서드가 실행됩니다.
+     * @param direction 사용자가 입력한 방향
+     * @param cellIndex 현재 몇 번째 칸인지를 표현하는 index
+     */
+    public void move(String direction, int cellIndex) {
+        if (bridge.get(cellIndex).equals(direction)) {
+            printResult(cellIndex, true);
+            moveSuccess();
+            return;
+        }
+        printResult(cellIndex, false);
+        moveFail();
     }
 
     /**
      * 사용자가 게임을 다시 시도할 때 사용하는 메서드
-     * <p>
-     * 재시작을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
     public void retry() {
+
+    }
+
+    private void moveSuccess() {
+
+    }
+
+    private void moveFail() {
+
+    }
+
+    private void printResult(int currentIndex, boolean isSuccess) {
+
     }
 }
