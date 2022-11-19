45e15d4b3575f045e01b10e7a3339291f6ed231c
Sehbeom <j3beom@gmail.com>
2022-11-20 02:23:53 +0900

    feat(view): 이동할 칸 선택까지 안내문 출력 기능 구현
    게임 시작부터 이동할 칸 선택 과정까지 안내문 출력 기능 구현

--- src/main/java/bridge/domain/view/OutputView.java
+++ src/main/java/bridge/domain/view/OutputView.java
@@ -1,9 +1,31 @@
-package bridge;
+package bridge.domain.view;
 
 /**
  * 사용자에게 게임 진행 상황과 결과를 출력하는 역할을 한다.
  */
+
+import bridge.constants.GuideSentences;
+
+/**
+ * 패키지 변경 가능
+ * 메소드 인자와 반환 타입 추가 혹은 변경 가능
+ * 값 출력을 위해 필요한 메소드 추가 가능
+ *
+ * 메소드 이름 변경 불가능
+ */
 public class OutputView {
+
+    public void printStartGuide() {
+        System.out.println(GuideSentences.START.getSentence());
+    }
+
+    public void printInputBridgeLengthGuide() {
+        System.out.println(GuideSentences.INPUT_BRIDGE_LENGTH.getSentence());
+    }
+
+    public void printInputMoveDirectionGuide() {
+        System.out.println(GuideSentences.INPUT_MOVING_DIRECTION.getSentence());
+    }
 
     /**
      * 현재까지 이동한 다리의 상태를 정해진 형식에 맞춰 출력한다.
