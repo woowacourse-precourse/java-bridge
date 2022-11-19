621505f57f9af7cdc5ce34adf68b722aa4e10da2
ho2n <jungho2n@gmail.com>
2022-11-19 14:50:46 +0900

    feat(OutputView): 시작 안내 문구를 출력하는 기능 추가

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -1,12 +1,18 @@
 package bridge;
 
+import utils.Validation;
 import view.InputView;
 import view.OutputView;
 
 public class Application {
 
     public static void main(String[] args) {
-        BridgeGame game = new BridgeGame(new InputView(), new OutputView());
+        InputView inputView = new InputView(new Validation());
+        OutputView outputView = new OutputView();
+
+        outputView.printStartMessage();
+
+        BridgeGame game = new BridgeGame(inputView, outputView);
         game.start();
     }
 }
