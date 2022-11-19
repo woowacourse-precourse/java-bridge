61ec44d7d3803172fec50477df19cd96b1cf6126
Rovna99 <80767387+Rovna99@users.noreply.github.com>
2022-11-19 21:05:04 +0900

    refactor: BridgeGameService 상수화

--- src/main/java/bridge/service/BridgeGameService.java
+++ src/main/java/bridge/service/BridgeGameService.java
@@ -1,14 +1,20 @@
 package bridge.service;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import bridge.domain.BridgeGame;
 import bridge.domain.BridgeMaker;
 import bridge.domain.BridgeRandomNumberGenerator;
 import bridge.domain.ProgressMap;
 
 public class BridgeGameService {
+	private static final int MIN_SIZE = 3;
+	private static final int MAX_SIZE = 20;
+	private static final String BRIDGE_SIZE_ERROR_MESSAGE = "[ERROR] 다리 길이는 3부터 20 사이의 숫자여야 합니다.";
+	private static final String MOVE_COMMAND_ERROR_MESSAGE = "[ERROR] 이동 커맨드는 U 또는 D 입니다.";
+	private static final String RESTART_COMMAND_ERROR_MESSAGE = "[ERROR] 재시작, 종료 커맨드는 R 또는 Q 입니다.";
+	private static final String UP_MOVE_COMMAND = "U";
+	private static final String DOWN_MOVE_COMMAND = "D";
+	private static final String RESTART_GAME_COMMAND = "R";
+	private static final String QUIT_GAME_COMMAND = "Q";
 	private final BridgeMaker bridgeMaker;
 
 	public BridgeGameService() {
@@ -34,25 +40,25 @@ public class BridgeGameService {
 	}
 
 	private void validSize(int size) {
-		if (size < 3 || size > 20) {
-			throw new IllegalArgumentException("[ERROR] 다리 길이는 3부터 20 사이의 숫자여야 합니다.");
+		if (size < MIN_SIZE || size > MAX_SIZE) {
+			throw new IllegalArgumentException(BRIDGE_SIZE_ERROR_MESSAGE);
 		}
 	}
 
 	private void validMoving(String moving) {
-		if (!moving.equals("U") && !moving.equals("D")) {
-			throw new IllegalArgumentException("[ERROR] 이동 커맨드는 U 또는 D 입니다.");
+		if (!moving.equals(UP_MOVE_COMMAND) && !moving.equals(DOWN_MOVE_COMMAND)) {
+			throw new IllegalArgumentException(MOVE_COMMAND_ERROR_MESSAGE);
 		}
 	}
 
 	public boolean retryOrEnd(String command) {
 		validCommand(command);
-		return command.equals("R");
+		return command.equals(RESTART_GAME_COMMAND);
 	}
 
 	private void validCommand(String command) {
-		if (!command.equals("Q") && !command.equals("R")) {
-			throw new IllegalArgumentException("[ERROR] 재시작, 종료 커맨드는 R 또는 Q 입니다.");
+		if (!command.equals(RESTART_GAME_COMMAND) && !command.equals(QUIT_GAME_COMMAND)) {
+			throw new IllegalArgumentException(RESTART_COMMAND_ERROR_MESSAGE);
 		}
 	}
 }
