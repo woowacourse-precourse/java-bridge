1fd7fc07d921cee0e85d853b4e9e6e3938d8f7b4
thdwoqor <celebarte01@gmail.com>
2022-11-19 20:00:16 +0900

    feat: `BridgeGameController` 객체 생성 후 실행

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -1,8 +1,14 @@
 package bridge;
 
-public class Application {
+import bridge.controller.BridgeGameController;
 
+public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        try {
+            BridgeGameController bridgeGameController = new BridgeGameController();
+            bridgeGameController.gameProcess();
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+        }
     }
 }
