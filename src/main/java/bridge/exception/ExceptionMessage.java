abc0cf0c83bd3c98fad4abcd1df1976dfd82b17d
broden-horizon <rlghd1698@naver.com>
2022-11-19 20:13:38 +0900

    ✨feat: 예외 코드 추가

--- src/main/java/bridge/exception/ExceptionMessage.java
+++ src/main/java/bridge/exception/ExceptionMessage.java
@@ -2,7 +2,8 @@ package bridge.exception;
 
 public enum ExceptionMessage {
     INPUT_SHOULD_BE_U_OR_D("U 또는 D를 입력해야합니다."),
-    NOT_VALID_BRIDGE_SIZE("0과 이상이거나 다리 사이즈 미만이어야합니다.");
+    NOT_VALID_BRIDGE_SIZE("0과 이상이거나 다리 사이즈 미만이어야합니다."),
+    NOT_VALID_RANDOM_NUMBER_GENERATED("0 또는 1 이외의 값이 생성되었습니다.");
 
     private String message;
 
