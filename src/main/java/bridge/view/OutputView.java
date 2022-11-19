521244a0ca33eea610caa10071ff3856e7f59069
kimtaesoo99 <107785279+kimtaesoo99@users.noreply.github.com>
2022-11-20 00:26:50 +0900

    feat: (OutputView) 게임 상황을 출력할 메서드 추가

--- src/main/java/bridge/view/OutputView.java
+++ src/main/java/bridge/view/OutputView.java
@@ -6,7 +6,8 @@ package bridge.view;
  * 사용자에게 게임 진행 상황과 결과를 출력하는 역할을 한다.
  */
 public class OutputView {
-
+    private StringBuilder upState = new StringBuilder();
+    private StringBuilder downState = new StringBuilder();
 
     public void printStartGame() {
         System.out.println("다리 건너기 게임을 시작합니다.\n");
@@ -25,4 +26,63 @@ public class OutputView {
     }
 
 
+    public void printResultMap() {
+        System.out.println("[ " + upState + " ]");
+        System.out.println("[ " + downState + " ]");
+    }
+
+    /**
+     * 현재까지 이동한 다리의 상태를 정해진 형식에 맞춰 출력한다.
+     * <p>
+     * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
+     */
+    public void printMap(int index, boolean check, String moveUpOrDown) {
+        if (index >= 1) {
+            upState.append(" | ");
+            downState.append(" | ");
+        }
+        checkUpOrDown(moveUpOrDown, check);
+        printResultMap();
+    }
+
+    private void checkUpOrDown(String UpAndDown, boolean check) {
+        if (UpAndDown.equals("U")) {
+            setUpState(check);
+        }
+        if (UpAndDown.equals("D")) {
+            setDownState(check);
+        }
+    }
+
+
+    private void setUpState(boolean check) {
+        if (check) {
+            upState.append("O");
+        }
+        if (!check) {
+            upState.append("X");
+        }
+        downState.append(" ");
+    }
+
+    private void setDownState(boolean check) {
+        if (check) {
+            downState.append("O");
+        }
+        if (!check) {
+            downState.append("X");
+        }
+        upState.append(" ");
+    }
+
+    public void initUpOrDownState() {
+        upState = new StringBuilder();
+        downState = new StringBuilder();
+    }
+
+    /**
+     * 게임의 최종 결과를 정해진 형식에 맞춰 출력한다.
+     * <p>
+     * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
+     */
 }
