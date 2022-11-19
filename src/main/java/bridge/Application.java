275d9da96b1ac0ef7f51c9fe8684f5d53934b491
hyowonhan <wain1719@gmail.com>
2022-11-19 20:01:26 +0900

    feat: 게임 실행 기능 구현

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -1,8 +1,18 @@
 package bridge;
 
+import bridge.domain.BridgeMaker;
+import bridge.domain.BridgeGame;
+import bridge.view.InputView;
+import bridge.view.OutputView;
+
+import java.util.List;
+
 public class Application {
 
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        BridgeGame bridgeGame = new BridgeGame();
+        List<String> correctBridge = bridgeGame.makeCorrectBridge(InputView.readBridgeSize());
+        OutputView outputView = new OutputView();
+        outputView.printMap(correctBridge);
     }
 }
