9adfbe199036f2b067e9c72e4098f893c2e273e1
akskflwn <akskflwn@gmail.com>
2022-11-20 01:27:28 +0900

    refactor(InputView) : InputView 메서드 리펙토링 - 모든 문자열 상수화 - validateBlank() : 빈 문자열 예외 처리하는 메서드 분리 - validateNumberForm,validateStringForm 문자 또는 숫자가 아닌 경우 예외처리 메서드 분리 - validateOption() : 입력 형식에 맞지 않는 예외 처리 메서드 분리

--- src/main/java/bridge/view/InputView.java
+++ src/main/java/bridge/view/InputView.java
@@ -7,49 +7,77 @@ import java.util.regex.Pattern;
  * 사용자로부터 입력을 받는 역할을 한다.
  */
 public class InputView {
-    private static final String STRING_REGEX = "^[A-Z]*$";
+    private static final String GET_BRIDGE_SIZE_MESSAGE = "다리의 길이를 입력해주세요.";
+    private static final String GET_PLAYER_MOVE_MESSAGE = "이동할 칸을 선택해주세요. (위: U, 아래: D)";
+    private static final String GET_GAME_COMMAND_MESSAGE = "게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)";
+    private static final String STRING_REGEX = "^[a-zA-Z]*$";
     private static final String NUMBER_REGEX = "^[1-9]*$";
-    private static final String REPLAY_GAME = "R";
+    private static final String NUMBER_FORMAT_ERROR_MESSAGE = "[ERROR] 숫자만 입력해주세요 : %s";
+    private static final String STRING_FORMAT_ERROR_MESSAGE = "[ERROR] 문자만 입력해주세요 : %s";
+    private static final String BLANK_ERROR_MESSAGE = "[ERROR] 빈 문자열은 입력될 수 없습니다.";
+    private static final String FORMAT_OPTION_ERROR_MESSAGE = "[ERROR] %s 또는 %s를 입력해주세요";
+    private static final String GAME_REPLAY = "R";
+    private static final String GAME_QUIT = "Q";
+    private static final String UP_STAIR = "U";
+    private static final String DOWN_STAIR = "D";
 
-    /**
-     * 다리의 길이를 입력받는다.
-     */
+
     public static int readBridgeSize() {
-        System.out.println("다리의 길이를 입력해주세요.");
+        System.out.println(GET_BRIDGE_SIZE_MESSAGE);
         String input = Console.readLine();
-        validateBrideSize(input);
+        validateNumberForm(input);
         return Integer.parseInt(input);
     }
 
-    private static void validateBrideSize(String input) {
-        if(!Pattern.matches(NUMBER_REGEX,input)){
-            throw new IllegalArgumentException("[ERROR]");
+    private static void validateNumberForm(String input) {
+        validateBlank(input);
+        if (!Pattern.matches(NUMBER_REGEX, input)) {
+            throw new IllegalArgumentException(String.format(NUMBER_FORMAT_ERROR_MESSAGE, input));
         }
     }
 
-    /**
-     * 사용자가 이동할 칸을 입력받는다.
-     */
     public static String readMoving() {
-        System.out.println("이동할 칸을 선택해주세요. (위: U, 아래: D)");
+        System.out.println(GET_PLAYER_MOVE_MESSAGE);
         String input = Console.readLine();
-        validate(input);
+        validateMove(input);
         return input;
     }
 
-    /**
-     * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
-     */
     public static boolean readGameCommand() {
-        System.out.println("게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)");
+        System.out.println(GET_GAME_COMMAND_MESSAGE);
         String input = Console.readLine();
-        validate(input);
-        return input.equals(REPLAY_GAME);
+        validateCommand(input);
+        return input.equals(GAME_REPLAY);
     }
 
-    private static void validate(String input) {
-        if(!Pattern.matches(STRING_REGEX,input)){
-            throw new IllegalArgumentException("[ERROR]");
+    private static void validateMove(String input) {
+        validateBlank(input);
+        validateStringForm(input);
+        validateOption(input,UP_STAIR,DOWN_STAIR);
+    }
+
+    private static void validateCommand(String input) {
+        validateBlank(input);
+        validateStringForm(input);
+        validateOption(input,GAME_REPLAY,GAME_QUIT);
+    }
+
+    private static void validateStringForm(String input) {
+        if (!Pattern.matches(STRING_REGEX, input)) {
+            throw new IllegalArgumentException(String.format(STRING_FORMAT_ERROR_MESSAGE, input));
         }
     }
+
+    private static void validateOption(String input, String format1, String format2) {
+        if (!input.equals(format1) && !input.equals(format2)) {
+            throw new IllegalArgumentException(String.format(FORMAT_OPTION_ERROR_MESSAGE,format1,format2));
+        }
+    }
+
+    private static void validateBlank(String input) {
+        if (input.isBlank()) {
+            throw new IllegalArgumentException(BLANK_ERROR_MESSAGE);
+        }
+    }
+
 }
