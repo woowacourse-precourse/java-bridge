db23dc555175f1605bb9fdc2af192c1b7cce5799
sosow0212 <sosow0212@naver.com>
2022-11-19 16:40:31 +0900

    feat : (BridgeController) 게임 시작시 출력 메시지를 BridgeController 에서 출력하도록 메소드 구현

--- src/main/java/bridge/controller/BridgeController.java
+++ src/main/java/bridge/controller/BridgeController.java
@@ -4,14 +4,21 @@ import bridge.domain.Bridge;
 import bridge.domain.User;
 import bridge.service.BridgeGame;
 import bridge.service.BridgeService;
+import bridge.view.OutputView;
 
 public class BridgeController {
     private final BridgeService bridgeService = new BridgeService();
     private final BridgeGame bridgeGame = new BridgeGame();
+    private final OutputView outputView = new OutputView();
 
     public void start() {
+        gameStartMessage();
         Bridge bridge = bridgeService.bridgeMaker();
         User user = new User();
         bridgeGame.gameStart(bridge, user);
+    }
+
+    public void gameStartMessage() {
+        outputView.printGameStart();
     }
 }
