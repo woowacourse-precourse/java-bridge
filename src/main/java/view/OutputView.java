e82b089a5932c190503cf0c5e53fcd7e9dda93d9
masiljangajji <hjk172262@gmail.com>
2022-11-19 12:16:52 +0900

    feat: 다리 길이 입력 메시지 출력 기능 추가

--- src/main/java/view/OutputView.java
+++ src/main/java/view/OutputView.java
@@ -9,7 +9,9 @@ public class OutputView {
         System.out.println(Message.START_GAME.getMessage());
     }
 
-
+    public void printInputBridgeSize() {
+        System.out.println(Message.BRIDGE_SIZE.getMessage());
+    }
     public void printMap(List<String> list) {
         for (int i = 0; i < list.size(); i++) {
             System.out.println(list.get(i));
