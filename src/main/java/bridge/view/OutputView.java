6173b2ac044393057728e07a602f90e50b578381
woo-chang <clllickme@naver.com>
2022-11-19 16:29:55 +0900

    feat(OutputView): 이동 결과를 출력하는 기능 구현

--- src/main/java/bridge/view/OutputView.java
+++ src/main/java/bridge/view/OutputView.java
@@ -1,5 +1,11 @@
 package bridge.view;
 
+import bridge.model.Bridge;
+import bridge.model.Direction;
+import bridge.model.Player;
+import java.util.List;
+import java.util.StringJoiner;
+
 /**
  * 사용자에게 게임 진행 상황과 결과를 출력하는 역할을 한다.
  */
@@ -12,7 +18,38 @@ public class OutputView {
      * <p>
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void printMap() {
+    public void printMap(Bridge bridge, Player player) {
+        StringJoiner joiner = new StringJoiner("\n");
+        joiner.add(generateDirectionLineResult(bridge, player, Direction.UP));
+        joiner.add(generateDirectionLineResult(bridge, player, Direction.DOWN));
+        System.out.println(joiner);
+    }
+
+    private String generateDirectionLineResult(Bridge bridge, Player player, Direction direction) {
+        StringJoiner joiner = new StringJoiner(" ");
+        joiner.add("[");
+        joiner.add(matchingMap(bridge.getBridge(), player.getDirections(), direction));
+        joiner.add("]");
+        return joiner.toString();
+    }
+
+    public String matchingMap(List<String> bridge, List<String> directions, Direction direction) {
+        int size = Math.min(bridge.size(), directions.size());
+        StringJoiner joiner = new StringJoiner(" | ");
+        for (int i = 0; i < size; i++) {
+            joiner.add(generateResult(bridge.get(i), directions.get(i), direction.getDirection()));
+        }
+        return joiner.toString();
+    }
+
+    private String generateResult(String bridge, String direction, String target) {
+        if (!direction.equals(target)) {
+            return " ";
+        }
+        if (direction.equals(bridge)) {
+            return "O";
+        }
+        return "X";
     }
 
     /**
