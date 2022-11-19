9c607d3da41afe01551fa4be61ac98bbb937b657
taeyun-work <wmfajr1357@gmail.com>
2022-11-20 04:24:28 +0900

    refactor: 게임 재시작시 bridge 초기화 및 게임 시도 횟수 증가 메소드 분리

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -21,8 +21,7 @@ public class Application {
         // 다리길이 입력받기 + 예외 시 추가적으로 계속 입력
         while (true) {
             try {
-                bridgeSize = inputView.readBridgeSize();
-                return bridgeSize;
+                return inputView.readBridgeSize();
             } catch (IllegalArgumentException e) {
                 outputView.printErrorState(e);
             }
@@ -39,30 +38,34 @@ public class Application {
         // 다리 생성
         createBridge(bridgeSize);
     }
+    private static void setGame(){
+        bridgeGame = new BridgeGame();
+        BridgeGame.setCountAttempt();
+    }
+    private static String getDirection() {
+        while(true) {
+            try {
+                return inputView.readMoving();
+            } catch (IllegalArgumentException e) {
+                outputView.printErrorState(e);
+            }
+        }
+    }
+
     public static void main(String[] args) {
         // TODO: 프로그램 구현
         // 게임을 시작하는 문구 출력
         startGame();
         settingBridge();
 
-
         while(true){
-            bridgeGame = new BridgeGame();
-            String direction;
-
-            BridgeGame.setCountAttempt();
+            setGame();
             // 이동이 성공했을 시에 계속 입력 받기
             while(true) {
                 outputView.printInputMoveDirection();
                 // 이동할 칸 입력 받기
-                while(true) {
-                    try {
-                        direction = inputView.readMoving();
-                        break;
-                    } catch (IllegalArgumentException e) {
-                        outputView.printErrorState(e);
-                    }
-                }
+                String direction = getDirection();
+
                 boolean isCorrect = bridgeGame.move(direction);
                 outputView.printMap(bridgeGame);
                 if(!isCorrect) {
