568c1ca5c0718a6a4400347a165b8b35a59df978
Sehbeom <j3beom@gmail.com>
2022-11-20 04:32:06 +0900

    feat(view): 이동할 칸 입력 기능 구현
    이동할 칸 입력 기능 readMoving 구현

--- src/main/java/bridge/domain/view/InputView.java
+++ src/main/java/bridge/domain/view/InputView.java
@@ -29,7 +29,9 @@ public class InputView {
      * 사용자가 이동할 칸을 입력받는다.
      */
     public String readMoving() {
-        return null;
+        String movingInput = Console.readLine();
+
+        return movingInput;
     }
 
     /**
