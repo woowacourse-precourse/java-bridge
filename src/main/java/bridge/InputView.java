65388259afac9d80c6243440338365aac10bc724
impati <impati0716@icloud.com>
2022-11-19 11:07:18 +0900

    feat(InputView) : 다리 길이에 대한 Integer 변환

--- src/main/java/bridge/InputView.java
+++ src/main/java/bridge/InputView.java
@@ -1,15 +1,24 @@
 package bridge;
 
+import camp.nextstep.edu.missionutils.Console;
+
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
  */
 public class InputView {
+    private final Validation validation;
+
+    public InputView(Validation validation) {
+        this.validation = validation;
+    }
 
     /**
      * 다리의 길이를 입력받는다.
      */
     public int readBridgeSize() {
-        return 0;
+        String bridgeSize = Console.readLine();
+        validation.bridgeLen(bridgeSize);
+        return Integer.parseInt(bridgeSize);
     }
 
     /**
