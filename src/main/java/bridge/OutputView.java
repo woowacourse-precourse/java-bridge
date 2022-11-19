ddead23a11fb5964e93989f5026e8675e39c19b2
Sunghyun-0 <zkdldk95@naver.com>
2022-11-19 19:46:17 +0900

    (시작, 선택, 재시작)Message 출력 기능 작성

--- src/main/java/bridge/OutputView.java
+++ src/main/java/bridge/OutputView.java
@@ -5,6 +5,18 @@ package bridge;
  */
 public class OutputView {
 
+    public void printGameStartMessage(){
+        System.out.println("다리 건너기 게임을 시작합니다.\n\n다리의 길이를 입력해주세요.");
+    }
+
+    public void printSelectMessage() {
+        System.out.println("이동할 칸을 선택해주세요. (위: U, 아래: D)");
+    }
+
+    public void printRestartMessage() {
+        System.out.println("게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)");
+    }
+
     /**
      * 현재까지 이동한 다리의 상태를 정해진 형식에 맞춰 출력한다.
      * <p>
@@ -12,6 +24,9 @@ public class OutputView {
      */
     public void printMap() {
     }
+
+
+
 
     /**
      * 게임의 최종 결과를 정해진 형식에 맞춰 출력한다.
