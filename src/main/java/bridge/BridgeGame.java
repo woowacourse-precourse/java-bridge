123d0384a6515bbd62cfb7cbed7ce619d26763d5
seung-won-kim <hesjs5@skuniv.ac.kr>
2022-11-19 18:51:56 +0900

    feat(BridgeGameController): 이동할 칸을 선택하는 기능 구현

--- src/main/java/bridge/BridgeGame.java
+++ src/main/java/bridge/BridgeGame.java
@@ -8,6 +8,14 @@ import java.util.List;
  */
 public class BridgeGame {
 
+    private final BridgeGameStat bridgeGameStat;
+    private int countOfRound;
+
+    public BridgeGame() {
+        this.bridgeGameStat = new BridgeGameStat();
+        this.countOfRound = 0;
+    }
+
     /**
      * 사용자가 칸을 이동할 때 사용하는 메서드
      * <p>
@@ -28,5 +36,13 @@ public class BridgeGame {
      * 재시작을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
     public void retry() {
+    }
+
+    public boolean isCountOfRoundLessThan(int bridgeSize) {
+        return this.countOfRound < bridgeSize;
+    }
+
+    public int getCountOfRound() {
+        return countOfRound;
     }
 }
--- src/main/java/bridge/controller/BridgeGameController.java
+++ src/main/java/bridge/controller/BridgeGameController.java
@@ -1,9 +1,13 @@
 package bridge.controller;
 
+import bridge.BridgeGame;
 import bridge.BridgeMaker;
+import bridge.BridgeMap;
 import bridge.BridgeRandomNumberGenerator;
 import bridge.InputView;
 import bridge.OutputView;
+import bridge.constant.BridgeMove;
+import bridge.constant.MoveResult;
 import java.util.List;
 
 public class BridgeGameController {
@@ -11,11 +15,13 @@ public class BridgeGameController {
     private final InputView inputView;
     private final OutputView outputView;
     private final BridgeMaker bridgeMaker;
+    private final BridgeGame bridgeGame;
 
     public BridgeGameController() {
         this.inputView = new InputView();
         this.outputView = new OutputView();
         this.bridgeMaker = new BridgeMaker(new BridgeRandomNumberGenerator());
+        this.bridgeGame = new BridgeGame();
     }
 
     public void gameStart() {
@@ -23,5 +29,26 @@ public class BridgeGameController {
 
         int bridgeSize = inputView.readBridgeSize();
         List<String> bridge = bridgeMaker.makeBridge(bridgeSize);
+
+        doGame(bridge, bridgeSize);
+    }
+
+    private void doGame(List<String> bridge, int bridgeSize) {
+        do {
+            round(bridge);
+        } while (bridgeGame.isCountOfRoundLessThan(bridgeSize));
+    }
+
+    private void round(List<String> bridge) {
+        BridgeMap bridgeMap = movingBridge(bridge);
+    }
+
+    private BridgeMap movingBridge(List<String> bridge) {
+        String playerBridgeMove = inputView.readMoving();
+
+        int countOfRound = bridgeGame.getCountOfRound();
+        MoveResult moveResult = bridgeGame.move(bridge, playerBridgeMove, countOfRound);
+
+        return new BridgeMap(BridgeMove.findByInput(playerBridgeMove), moveResult);
     }
 }
