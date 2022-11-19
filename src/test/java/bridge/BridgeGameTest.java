ec285877313cd67462eabe2554695401ff34ee1a
elizabethgim <yujingim@kookmin.ac.kr>
2022-11-19 19:31:44 +0900

    test: BridgeGame의 move 함수 테스팅

---
+++ src/test/java/bridge/BridgeGameTest.java
@@ -0,0 +1,45 @@
+package bridge;
+
+import camp.nextstep.edu.missionutils.test.NsTest;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestFactory;
+import org.junit.jupiter.params.provider.ValueSource;
+import org.mockito.Mock;
+
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.*;
+
+public class BridgeGameTest extends NsTest {
+    private static BridgeGame bridgeGame;
+
+    @BeforeAll
+    static void initialize(){
+        bridgeGame = new BridgeGame();
+        BridgeRandomNumberGenerator bridgeRandomNumberGenerator = new BridgeRandomNumberGenerator();
+        BridgeMaker bridge = new BridgeMaker(bridgeRandomNumberGenerator);
+    }
+
+    @Test
+    void testMove() {
+        List<String> testBridge = new ArrayList<>(Arrays.asList("U", "D", "U"));
+        String example = "U\nD\nU\n";
+        InputStream stream = new ByteArrayInputStream(example.getBytes(StandardCharsets.UTF_8));
+        InputStream stdin = System.in;
+        System.setIn(stream);
+        assertThat(bridgeGame.move(testBridge)).isTrue();
+        System.setIn(stdin);
+    }
+
+    @Override
+    protected void runMain() {
+
+    }
+}
