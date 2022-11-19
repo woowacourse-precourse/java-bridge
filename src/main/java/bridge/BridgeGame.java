621e98114d833578d1d0b017936e6c2d666439d6
sjh910805@gmail.com <sjh910805@gmail.com>
2022-11-19 21:13:29 +0900

    feat: 다리 생성

--- src/main/java/bridge/BridgeGame.java
+++ src/main/java/bridge/BridgeGame.java
@@ -1,13 +1,24 @@
 package bridge;
 
+import java.util.List;
+
 /**
  * 다리 건너기 게임을 관리하는 클래스
- * 필드(인스턴스 변수)를 추가할 수 있다.
+ * 필드(인스턴스 변수)를 추가할 수 "있다".
  * 메서드의 이름은 변경할 수 없고, 인자와 반환 타입은 필요에 따라 추가하거나 변경할 수 있다.
  * 게임 진행을 위해 필요한 메서드를 추가 하거나 변경할 수 "있다".
  * BridgeGame 클래스에서 InputView, OutputView 를 사용하지 않는다.
  */
 public class BridgeGame {
+    private final BridgeMaker bridgeMaker;
+    private List<String> bridge;
+    public BridgeGame() {
+        bridgeMaker = new BridgeMaker(new BridgeRandomNumberGenerator());
+    }
+    public void requestBridge() {
+        int bridgeSize = bridgeMaker.requestReadBridegeSize();
+        bridge = bridgeMaker.makeBridge(bridgeSize);
+    }
 
     /**
      * 사용자가 칸을 이동할 때 사용하는 메서드
--- src/main/java/bridge/BridgeMaker.java
+++ src/main/java/bridge/BridgeMaker.java
@@ -1,11 +1,12 @@
 package bridge;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
  * 다리의 길이를 입력 받아서 다리를 생성해주는 역할을 한다.
  * 필드(인스턴스 변수)를 변경할 수 없다.
- * 메서드의 시그니처(인자, 이름)와 반환 타입은 변경할 수 없다.
+ * 메서드의 시그니처(인자, 이름)와 반환 타입은 변경할 수 "없다".
  */
 public class BridgeMaker {
 
@@ -14,14 +15,14 @@ public class BridgeMaker {
     public BridgeMaker(BridgeNumberGenerator bridgeNumberGenerator) {
         this.bridgeNumberGenerator = bridgeNumberGenerator;
     }
-    public int requestReadBridegesize() {
+    public int requestReadBridegeSize() {
         InputView inputView = new InputView();
         int number = inputView.readBridgeSize();
         try {
             isValid(number);
         } catch (IllegalArgumentException e) {
             e.getMessage();
-            number = requestReadBridegesize();
+            number = requestReadBridegeSize();
         }
         return number;
     }
@@ -38,7 +39,10 @@ public class BridgeMaker {
      * @return 입력받은 길이에 해당하는 다리 모양. 위 칸이면 "U", 아래 칸이면 "D"로 표현해야 한다.
      */
     public List<String> makeBridge(int size) {
-        int bridgeSize = requestReadBridegesize();
-
+        List<String> bridge = new ArrayList<>();
+        for(int i = 0; i<size; i++) {
+            bridge.add(String.valueOf(bridgeNumberGenerator.generate()));
+        }
+        return bridge;
     }
 }
