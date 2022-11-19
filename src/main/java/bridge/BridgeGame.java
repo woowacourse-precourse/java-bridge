1ec2c184c9383f220cb0be898620ab9890a5113b
Devtist <yt930310@snu.ac.kr>
2022-11-19 21:13:20 +0900

    feat: 사용자가 칸을 이동하는 기능 구현

--- src/main/java/bridge/BridgeGame.java
+++ src/main/java/bridge/BridgeGame.java
@@ -1,19 +1,20 @@
 package bridge;
 
+import java.util.List;
+
 /**
  * 다리 건너기 게임을 관리하는 클래스
  */
 public class BridgeGame {
+	boolean isSuccess;
+	int count = 1;
 
 	/**
 	 * 사용자가 칸을 이동할 때 사용하는 메서드
 	 */
-	public void move() {
-	}
+	public boolean move(List<String> subBridge, String moving) {
+		isSuccess = subBridge.get(subBridge.size() - 1).equals(moving);
+		return isSuccess;
+	}}
 
-	/**
-	 * 사용자가 게임을 다시 시도할 때 사용하는 메서드
-	 */
-	public void retry() {
-	}
-}
+
