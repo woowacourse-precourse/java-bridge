fdecc9ab24833f3124a0c3f648775fe74bcafd31
recicoco068 <gkapf838@gmail.com>
2022-11-19 11:23:44 +0900

    fix(InputView.java) : 입력 함수 실행 안내 문구 추가

--- src/main/java/bridge/view/InputView.java
+++ src/main/java/bridge/view/InputView.java
@@ -13,6 +13,7 @@ public class InputView {
 
     public int readBridgeSize() {
         try {
+            System.out.println("다리의 길이를 입력해주세요.");
             String input = Console.readLine();
             validateAllNumber(input);
             validateNumberRange(input);
@@ -24,6 +25,7 @@ public class InputView {
 
     public String readMoving() {
         try {
+            System.out.println("이동할 칸을 선택해주세요. (위: U, 아래: D)");
             String input = Console.readLine();
             validateMoveCommand(input);
             return input;
@@ -34,6 +36,7 @@ public class InputView {
 
     public String readGameCommand() {
         try {
+            System.out.println("게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)");
             String input = Console.readLine();
             validateGameCommand(input);
             return input;
