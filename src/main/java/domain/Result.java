efa09d7876cb2511f0c98af475857d3099a52440
seoeunbae <enyoupark@gmail.com>
2022-11-19 19:33:11 +0900

    feat : 게임 결과저장 도메인 클래스 생성 및 출력 enum 생성

---
+++ src/main/java/domain/Result.java
@@ -0,0 +1,14 @@
+package domain;
+
+public class Result {
+    private boolean isSuccess;
+    private int trial;
+
+    public void setIsSuccess(boolean isSuccess){
+        this.isSuccess = isSuccess;
+    }
+
+    public void setTrial(int trial){
+        this.trial = trial;
+    }
+}
