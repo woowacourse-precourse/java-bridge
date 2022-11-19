7ef1c1efdf92501484265464fdd57bc27dcaa0c1
Devtist <yt930310@snu.ac.kr>
2022-11-20 01:37:21 +0900

    chore: TODO 추가

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -8,6 +8,7 @@ public class Application {
         // TODO: 프로그램 구현
         // TODO: View 에서는 최소한의 검증만 -> 알파벳 대문자인지만 확인하고 도메인에서 확인할 것
         // TODO: Dto 적용해보기, Service, Repository 적용해보기
+        // TODO: 마지막에 실패한 경우 예외 처리
         try {
             GameController gameController = new GameController();
             gameController.control();
