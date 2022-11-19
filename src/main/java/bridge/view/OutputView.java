cad055b6a1f632c3915961b8d55b993d25843208
akskflwn <akskflwn@gmail.com>
2022-11-19 10:31:37 +0900

    refactor(OutputView) : 게임 결과 출력 메서드 분리 - 수정  printResult 호출시 printMap을 호출해 코드 재사용, 중복 코드 제거 - 수정  printMap : 호출시 윗칸과 아래칸으로 분리해 코드 재사용, 중복 코드 제거 - 추가  printDependOn : 이미 통과한 다리 출력과 마지막으로 통과한 다리 출력 분리

---
+++ src/main/java/bridge/view/OutputView.java
@@ -0,0 +1,70 @@
+package bridge.view;
+
+import bridge.domain.BridgeGame;
+import bridge.domain.Player;
+import java.util.List;
+
+public class OutputView {
+
+    private static final String GAME_RESULT_MESSAGE = "최종 게임 결과";
+    private static final String SUCCESS_OR_FAILURE = "게임 성공 여부: %s";
+    private static final String TRIAL_COUNT_MESSAGE = "총 시도한 횟수: %d";
+    private static final String UP_STAIR = "U";
+    private static final String DOWN_STAIR = "D";
+
+    public static void printMap(BridgeGame bridgeGame) {
+        printDependOn(UP_STAIR, bridgeGame);
+        printDependOn(DOWN_STAIR, bridgeGame);
+        System.out.println();
+    }
+
+    private static void printDependOn(String stair, BridgeGame bridgeGame) {
+        StringBuilder sb = new StringBuilder("[");
+        sb.append(getPrevious(bridgeGame, stair));
+        sb.append(getNext(bridgeGame, stair));
+        sb.append("]");
+        System.out.println(sb);
+    }
+
+    private static String getNext(BridgeGame bridgeGame, String stage) {
+        Player player = bridgeGame.getPlayer();
+        if (player.isAlive() && stage.equals(player.getLastSelection())) {
+            return " O ";
+        }
+        if (!player.isAlive() && stage.equals(player.getLastSelection())) {
+            return " X ";
+        }
+
+        return "   ";
+    }
+
+    private static String getPrevious(BridgeGame bridgeGame, String stage) {
+        StringBuilder previous = new StringBuilder();
+
+        int stageNumber = bridgeGame.getStageNumber();
+        List<String> bridge = bridgeGame.getBridge();
+
+        for (int i = 0; i < stageNumber; i++) {
+            if (!stage.equals(bridge.get(i))) {
+                previous.append("   |");
+            } else {
+                previous.append(" O |");
+            }
+        }
+        return previous.toString();
+    }
+
+    public static void printResult(BridgeGame bridgeGame) {
+        System.out.println(GAME_RESULT_MESSAGE);
+        printMap(bridgeGame);
+        System.out.println(String.format(SUCCESS_OR_FAILURE, isSuccess(bridgeGame)));
+        System.out.println(String.format(TRIAL_COUNT_MESSAGE, bridgeGame.getTrialCount()));
+    }
+
+    private static String isSuccess(BridgeGame bridgeGame) {
+        if (bridgeGame.isPlayerAlive()) {
+            return "성공";
+        }
+        return "실패";
+    }
+}
