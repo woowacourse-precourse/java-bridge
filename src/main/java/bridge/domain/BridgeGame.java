ba0d8bcdb488ce288a491a634a1b0554b93d1608
jkroh1995 <85445649+jkroh1995@users.noreply.github.com>
2022-11-19 18:47:27 +0900

    refactor : 변수 상수화 및 메소드 범위 수정

--- src/main/java/bridge/domain/BridgeGame.java
+++ src/main/java/bridge/domain/BridgeGame.java
@@ -9,20 +9,20 @@ import java.util.List;
  */
 public class BridgeGame {
 
-    RealTimeBridge realTimeBridge = new RealTimeBridge();
-    OutputView outputView = new OutputView();
-    InputHandler inputHandler = new InputHandler();
+    private final RealTimeBridge realTimeBridge = new RealTimeBridge();
+    private final OutputView outputView = new OutputView();
+    private final InputHandler inputHandler = new InputHandler();
+    private final String[][] realTimeMap = realTimeBridge.getMap();
 
     private int count = 0;
-    boolean isAnswer = true;
-    public String[][] realTimeMap = realTimeBridge.getMap();
+    private boolean isAnswer = true;
 
     /**
      * 사용자가 칸을 이동할 때 사용하는 메서드
      * <p>
      * 이동을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public String move(String bridgeText, String userMove) {
+    private String move(String bridgeText, String userMove) {
         if (!bridgeText.equals(userMove)) {
             return " X ";
         }
