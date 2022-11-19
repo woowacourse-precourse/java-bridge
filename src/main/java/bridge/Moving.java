6ac86193909c7e01ee42bcc940ab2d5769f39f47
Sangwoo Shin <sangwoonoel@naver.com>
2022-11-19 22:57:12 +0900

    fix(Moving): number 추가

--- src/main/java/bridge/Moving.java
+++ src/main/java/bridge/Moving.java
@@ -2,16 +2,22 @@ package bridge;
 
 public enum Moving {
 
-    Up("U"),
-    Down("D");
+    Up("U" , 1),
+    Down("D", 0);
 
     private final String moving;
+    private final int number;
 
-    Moving(String moving) {
+    Moving(String moving, int number) {
         this.moving = moving;
+        this.number = number;
     }
 
     public String getMoving() {
         return moving;
+    }
+
+    public int getNumber() {
+        return number;
     }
 }
