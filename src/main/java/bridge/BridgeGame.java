59b5d5e7a946c426d5c03839cbaf9e468ee56860
JunTaeINC <juntea3523@naver.com>
2022-11-19 23:32:24 +0900

    feat : (BridgeGame) gaming 기능 구현

--- src/main/java/bridge/BridgeGame.java
+++ src/main/java/bridge/BridgeGame.java
@@ -9,6 +9,9 @@ package bridge;
 
 import java.util.List;
 
+import static bridge.InputView.*;
+import static bridge.enums.Sentence.*;
+
 public class BridgeGame {
 
     private final Move move = new Move();
@@ -17,6 +20,17 @@ public class BridgeGame {
         BridgeNumberGenerator bridgeNumberGenerator = new BridgeRandomNumberGenerator();
         BridgeMaker bridgeMaker = new BridgeMaker(bridgeNumberGenerator);
         return bridgeMaker.makeBridge(userInput);
+    }
+
+    public void gaming(List<String> bridge) {
+        for (int i = 0; i < bridge.size(); i++) {
+            String userMoveInput = getMoveString();
+            move.moving(bridge.get(i), userMoveInput);
+            move.printMoving();
+            if (move.checkWrong()) {
+                return;
+            }
+        }
     }
 
     public static boolean move(String computer, String userInput) {
@@ -28,6 +42,7 @@ public class BridgeGame {
      * <p>
      * 재시작을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public static void retry(String a) {
+    public boolean retry() {
+        return false;
     }
 }
