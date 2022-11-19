91175ac4b778613f417f68d8615bc2d7cb499dbb
wath1457 <wath1457@gmail.com>
2022-11-19 15:51:34 +0900

    feat(GameController): GameController 초기 작성

--- src/main/java/bridge/GameController.java
+++ src/main/java/bridge/GameController.java
@@ -14,11 +14,34 @@ public class GameController {
         Validation.isInRange(bridgeSize);
         BridgeMaker bridgeMaker = new BridgeMaker(new BridgeRandomNumberGenerator());
         List<String> bridge = bridgeMaker.makeBridge(bridgeSize);
+        BridgeGame bridgeGame = new BridgeGame(bridge);
+        List<String> userPath = new ArrayList<String>();
+        boolean clear;
+        int tryCount = 1;
         while(true) {
-            BridgeGame bridgeGame = new BridgeGame(bridge);
-            List<String> userPath = new ArrayList<String>();
-            bridgeGame.move();
+            String userSelect = inputView.readMoving();
+            userPath.add(userSelect);
+            boolean canMove = bridgeGame.move(userSelect);
+            if (bridgeGame.checkArriveDestination(canMove)) {
+                outputView.printMapResult(bridge, userPath);
+                clear = true;
+                break; // 클리어 -> 게임종료
+            }
+            // 틀렸으므로 재시작 여부 확인
+            if (!canMove && inputView.readGameCommand().equals("Q")) { // 재시작x
+                clear = false;
+                break;
+            }
+            if (!canMove && inputView.readGameCommand().equals("R")) { // 재시작 o
+                userPath = bridgeGame.retry(userPath);
+                tryCount += 1;
+                continue;
+            }
+            outputView.printMap(bridge, userPath);
         }
+        // 게임종료 절차
+        outputView.printGameResult(clear);
+        outputView.printTotalTry(tryCount);
     }
 
     public int createBridgeSize(InputView inputView) {
