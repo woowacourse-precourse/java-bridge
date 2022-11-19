c9c1f653b3bee44ce957c0920cfaf810916bb08d
opuopuo <pureuml.dev@gmail.com>
2022-11-20 03:51:20 +0900

    test(InputValidatorTest): 이동 방향 예외 테스트 코드 구현

--- src/test/java/bridge/InputValidatorTest.java
+++ src/test/java/bridge/InputValidatorTest.java
@@ -1,6 +1,7 @@
 package bridge;
 
 import static bridge.constant.ErrorMessage.INPUT_BRIDGE_SIZE_ERROR;
+import static bridge.constant.ErrorMessage.INPUT_DIRECTION_ERROR;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import org.junit.jupiter.api.DisplayName;
@@ -25,7 +26,7 @@ public class InputValidatorTest {
 //    }
 
     @DisplayName("3 ~ 20 범위 숫자가 아닌 다리 길이 입력 예외 처리")
-    @ValueSource(strings = {"0", "2", "21", "4.1", " 14"})
+    @ValueSource(strings = {"0", "2", "21", "4.1", "-14"})
     @ParameterizedTest
     @EmptySource
     void notValidBridgeSize(String input) {
@@ -33,5 +34,16 @@ public class InputValidatorTest {
         assertThatThrownBy(() -> inputValidator.isInRangeSize())
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining(String.valueOf(INPUT_BRIDGE_SIZE_ERROR));
+    }
+
+    @DisplayName("U, D 아닌 이동 방향 입력 예외 처리")
+    @ValueSource(strings = {"A", "U1", "Ua", ".D"})
+    @ParameterizedTest
+    @EmptySource
+    void notValidDirection(String input) {
+        InputValidator inputValidator = new InputValidator(input);
+        assertThatThrownBy(() -> inputValidator.isUpOrDown())
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(String.valueOf(INPUT_DIRECTION_ERROR));
     }
 }
