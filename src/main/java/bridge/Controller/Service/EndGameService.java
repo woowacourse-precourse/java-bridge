b9d2d165645c33dcfaf89669934c24318c301af5
Park shinyeong <syngh503@gmail.com>
2022-11-19 16:13:39 +0900

    Fix(EndGameService): gameResultGenerator 필드 삭제
    BridgeGame에서 gameResultGenerator을 조작해서 endGameService에서 삭제해주었습니다.

--- src/main/java/bridge/Controller/Service/EndGameService.java
+++ src/main/java/bridge/Controller/Service/EndGameService.java
@@ -2,19 +2,15 @@ package bridge.Controller.Service;
 
 import Utils.ValidateUtils;
 import bridge.Domain.BridgeGame;
-import bridge.Domain.GameResult;
-import bridge.Domain.GameResultGenerator;
 import bridge.EnumCollections.GameRestartType;
 import bridge.View.InputView;
 import bridge.View.OutputView;
 
 public class EndGameService {
     private BridgeGame bridgeGame;
-    private GameResultGenerator gameResultGenerator;
 
-    public EndGameService(BridgeGame bridgeGame, GameResultGenerator gameResultGenerator) {
+    public EndGameService(BridgeGame bridgeGame) {
         this.bridgeGame = bridgeGame;
-        this.gameResultGenerator = gameResultGenerator;
     }
 
     public boolean isGameRestart() throws IllegalArgumentException {
@@ -24,10 +20,14 @@ public class EndGameService {
         return input.equals(GameRestartType.RESTART.getRestartStatus());
     }
 
-    public void endGame(GameResult gameResult) {
-        String bridgeStatus = gameResultGenerator.getGameResult();
-        int gameGount = bridgeGame.getGameStartCount();
-        boolean gameSuccess = gameResult.isGameSuccess();
-        OutputView.printResult(bridgeStatus, gameSuccess, gameGount);
+    public void endGame() {
+        String bridgeStatus = bridgeGame.getBridgeResult();
+        int gameCount = bridgeGame.getGameStartCount();
+        boolean gameSuccess = bridgeGame.isMoveSuccess();
+        OutputView.printResult(bridgeStatus, gameSuccess, gameCount);
+    }
+
+    public void restartGame() {
+        bridgeGame.retry();
     }
 }
