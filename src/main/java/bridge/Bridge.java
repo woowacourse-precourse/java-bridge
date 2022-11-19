f316dc83f9eb6b66d4cdb6e9531ca6fc67e30a00
impati <impati0716@icloud.com>
2022-11-19 14:32:11 +0900

    feat (Bridge) : 다리 객체 도입

---
+++ src/main/java/bridge/Bridge.java
@@ -0,0 +1,11 @@
+package bridge;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Bridge {
+    private List<String> bridge;
+    public Bridge(List<String> bridge) {
+        this.bridge = bridge;
+    }
+}
