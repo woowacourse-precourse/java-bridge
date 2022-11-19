1f57af9af02706b8355d525615ddbbbfa055a54a
Choi-JJunho <junho5336@gmail.com>
2022-11-19 23:09:55 +0900

    feat(Floor): add validateStatus method

--- src/main/java/bridge/domain/Floor.java
+++ src/main/java/bridge/domain/Floor.java
@@ -1,10 +1,24 @@
 package bridge.domain;
 
 public class Floor {
+    private static final String UP_STARE = "U";
+    private static final String DOWN_STARE = "D";
+
     private final String status;
 
     Floor(String status) {
+        validateStatus(status);
         this.status = status;
+    }
+
+    private void validateStatus(String status) {
+        if (isNotStatus(status)) {
+            throw new IllegalArgumentException("[ERROR]");
+        }
+    }
+
+    private boolean isNotStatus(String status) {
+        return !UP_STARE.equals(status) && !DOWN_STARE.equals(status);
     }
 
     public boolean isSafe(String input) {
