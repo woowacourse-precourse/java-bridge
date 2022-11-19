71a669eaf66993518844c92b0c2cdc7477530cce
E1psycongr00 <39326175+E1psycongr00@users.noreply.github.com>
2022-11-19 16:23:51 +0900

    feat(outputView): outputView 구현

--- src/main/java/bridge/OutputView.java
+++ src/main/java/bridge/OutputView.java
@@ -1,23 +1,70 @@
 package bridge;
 
-/**
- * 사용자에게 게임 진행 상황과 결과를 출력하는 역할을 한다.
- */
-public class OutputView {
+import bridge.domain.BridgeStatus;
+import bridge.domain.GameStatus;
+import bridge.domain.MovingCommand;
+import java.util.ArrayList;
+import java.util.List;
 
-    /**
-     * 현재까지 이동한 다리의 상태를 정해진 형식에 맞춰 출력한다.
-     * <p>
-     * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
-     */
-    public void printMap() {
+public class OutputView extends View {
+
+    private static final String LETS_START = "다리 건너기 게임을 시작합니다.";
+    private static final String FINAL_RESULT = "최종 게임 결과";
+    private static final String GAME_STATUS = "게임 성공 여부: %s";
+    private static final String TOTAL_TRY_COUNT = "총 시도한 횟수: %d";
+
+    public void printMap(BridgeStatus bridgeStatus) {
+        List<String> userBridge = bridgeStatus.getUserBridge();
+        List<String> first = new ArrayList<>();
+        List<String> second = new ArrayList<>();
+        for (String command : userBridge) {
+            if (command.equals(MovingCommand.UP.toString())) {
+                first.add("O");
+                second.add(" ");
+            }
+            if (command.equals(MovingCommand.DOWN.toString())) {
+                first.add(" ");
+                second.add("O");
+            }
+        }
+        GameStatus gameStatus = bridgeStatus.getGameStatus();
+        if (gameStatus.equals(GameStatus.FAIL)) {
+            int lastIndex = first.size() - 1;
+            first.remove(lastIndex);
+            second.remove(lastIndex);
+            if (userBridge.get(lastIndex).equals(MovingCommand.UP.toString())) {
+                first.add(" ");
+                second.add("X");
+            }
+            if (userBridge.get(lastIndex).equals(MovingCommand.DOWN.toString())) {
+                first.add("X");
+                second.add(" ");
+            }
+        }
+        String firstLine = first.toString()
+                .replaceAll(", ", " | ")
+                .replaceAll("\\[", "[ ")
+                .replaceAll("]", " ]");
+        String secondLine = second.toString()
+                .replaceAll(", ", " | ")
+                .replaceAll("\\[", "[ ")
+                .replaceAll("]", " ]");
+        print(firstLine + "\n" + secondLine);
     }
 
-    /**
-     * 게임의 최종 결과를 정해진 형식에 맞춰 출력한다.
-     * <p>
-     * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
-     */
-    public void printResult() {
+    public void printResult(BridgeStatus bridgeStatus) {
+        print(FINAL_RESULT);
+        printMap(bridgeStatus);
+        printEmptyLine();
+        print(String.format(GAME_STATUS, bridgeStatus.getGameStatus().getDetails()));
+        print(String.format(TOTAL_TRY_COUNT, bridgeStatus.getTryCount()));
+    }
+
+    public void printEmptyLine() {
+        print();
+    }
+
+    public void printStart() {
+        print(LETS_START);
     }
 }
