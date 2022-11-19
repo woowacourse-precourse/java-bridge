f9c0bb138bae31b7a965d337bfa229eb026f90e3
SEONGJOON KIM <jobcho6320@naver.com>
2022-11-19 23:13:49 +0900

    feat: 게임 시도 횟수 반환 메소드 구현

--- src/main/java/bridge/domain/BridgeGame.java
+++ src/main/java/bridge/domain/BridgeGame.java
@@ -13,22 +13,21 @@ public class BridgeGame {
 
     private static final StringBuilder SUCCESS_MOVE_MARK = new StringBuilder(" O ");
     private static final StringBuilder FAIL_MOVE_MARK = new StringBuilder(" X ");
-    private List<String> bridgGame;
+    private final List<String> bridgeGame;
     private int tryCount;
 
-    public BridgeGame(List<String> bridgGame, int tryCount) {
-        this.bridgGame = bridgGame;
+    public BridgeGame(List<String> bridgeGame, int tryCount) {
+        this.bridgeGame = bridgeGame;
         this.tryCount = tryCount;
     }
 
     public int size() {
-        return bridgGame.size();
+        return bridgeGame.size();
     }
 
     public String toString(List<String> result) {
         return result.toString();
     }
-
 
     /**
      * 사용자가 칸을 이동할 때 사용하는 메서드
@@ -36,7 +35,7 @@ public class BridgeGame {
      * 이동을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
     public boolean move(String moveSide, int index) {
-        return bridgGame.get(index).equals(moveSide);
+        return bridgeGame.get(index).equals(moveSide);
     }
 
     public StringBuilder createMoveMark(String moveWord, int index) {
@@ -46,6 +45,9 @@ public class BridgeGame {
         return FAIL_MOVE_MARK;
     }
 
+    public int getTryCount() {
+        return tryCount;
+    }
 
     public void retryCount() {
         tryCount ++;
@@ -60,13 +62,11 @@ public class BridgeGame {
         OutputView.printRetryMessage();
         String command = InputView.readGameCommand();
         if (command.equals("R")){
-            retryCount();
             return true;
         }
         if (command.equals("Q")){
             return false;
         }
         throw new IllegalArgumentException("[ERROR] 'R' 또는 'Q'를 입력해 주세요.");
-
     }
 }
