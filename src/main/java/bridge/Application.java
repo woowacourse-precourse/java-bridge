a36f80be91dac77552bf4537c9d99c0fef10e086
taeyun-work <wmfajr1357@gmail.com>
2022-11-20 03:06:48 +0900

    feat: 입력 받은 문자가 R이면 게임 재시작하는 기능 구현

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -8,11 +8,10 @@ public class Application {
     private static InputView inputView = new InputView();
     private static BridgeRandomNumberGenerator bridgeRandomNumberGenerator = new BridgeRandomNumberGenerator();
     private static BridgeMaker bridgeMaker = new BridgeMaker(bridgeRandomNumberGenerator);
-    private static BridgeGame bridgeGame = new BridgeGame();
 
     public static void main(String[] args) {
         // TODO: 프로그램 구현
-
+        BridgeGame bridgeGame = new BridgeGame();
         // 게임을 시작하는 문구 출력
         outputView.printStartGame();
 
@@ -32,39 +31,47 @@ public class Application {
 
         // 다리 생성
         List<String> bridge = bridgeMaker.makeBridge(bridgeSize);
-        bridgeGame.setBridge(bridge);
+        BridgeGame.setBridge(bridge);
 
-        outputView.printInputMoveDirection();
-        String direction;
-        
-        // 이동이 성공했을 시에 계속 입력 받기
-        while(true) {
-            // 이동할 칸 입력 받기
+        while(true){
+            bridgeGame = new BridgeGame();
+            String direction;
+
+            BridgeGame.setCountAttempt();
+            // 이동이 성공했을 시에 계속 입력 받기
             while(true) {
-                try {
-                    direction = inputView.readMoving();
-                    break;
-                } catch (IllegalArgumentException e) {
-                    System.out.println(e);
-                }
-            }
-            boolean isCorrect = bridgeGame.move(direction);
-            outputView.printMap(bridge, direction, bridgeGame);
-            if(!isCorrect) {
-                String command;
+                outputView.printInputMoveDirection();
+                // 이동할 칸 입력 받기
                 while(true) {
                     try {
-                        command = inputView.readGameCommand();
+                        direction = inputView.readMoving();
                         break;
                     } catch (IllegalArgumentException e) {
                         System.out.println(e);
                     }
                 }
+                boolean isCorrect = bridgeGame.move(direction);
+                outputView.printMap(bridgeGame);
+                if(!isCorrect) {
+                    String command;
+                    while(true) {
+                        try {
+                            outputView.printInputRetryCommand();
+                            command = inputView.readGameCommand();
+                            break;
+                        } catch (IllegalArgumentException e) {
+                            System.out.println(e);
+                        }
+                    }
+                    if(command.equals("Q")){
+                        outputView.printResult(bridgeGame);
+                        return;
+                    }
+
+                    break;
+                }
+
             }
         }
-        
-
-
-
     }
 }
