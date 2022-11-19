bc30e18e56be505de80d4afe442638ffc00b0a28
nakalic <nakalic28@gmail.com>
2022-11-19 13:37:36 +0900

    feat(BridgeLengthValidator): 다리 길이 입력값을 검증하는 기능 구현

---
+++ src/main/java/bridge/validator/BridgeLengthValidator.java
@@ -0,0 +1,22 @@
+package bridge.validator;
+
+public class BridgeLengthValidator {
+
+    private static final int MIN_RANGE = 3;
+    private static final int MAX_RANGE = 20;
+    public static final String BRIDGE_LENGTH_MESSAGE = "[ERROR] 다리 길이는 3 이상 20 이하 자연수여야 합니다.";
+
+    public static void validateNaturalNumber(String input) {
+        try {
+            Integer.parseInt(input);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException(BRIDGE_LENGTH_MESSAGE);
+        }
+    }
+
+    public static void validateRange(String input) {
+        if (Integer.parseInt(input) < MIN_RANGE || Integer.parseInt(input) > MAX_RANGE) {
+            throw new IllegalArgumentException(BRIDGE_LENGTH_MESSAGE);
+        }
+    }
+}
