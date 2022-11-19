3d0e2029ec49f900803e2bd84eeec0c32b30ed78
skfkgla <narahim.lee@gmail.com>
2022-11-19 23:58:32 +0900

    feat: 게임 시작 문구를 출력하는 기능 구현

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -1,8 +1,9 @@
 package bridge;
 
 public class Application {
-
+    private static final OutputView outputView = new OutputView();
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        outputView.printStart();
     }
 }
