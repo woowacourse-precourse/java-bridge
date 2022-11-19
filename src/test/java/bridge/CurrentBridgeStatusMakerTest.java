5890839994a41a9e563ef618b5cf6fffb907c8df
JuHyeokParkk <hyeok5596@naver.com>
2022-11-20 00:52:02 +0900

    test: 플레이어가 현재까지 지나온 다리를 문자열로 나타낼 수 있는지 확인하는 테스트 코드 작성

---
+++ src/test/java/bridge/CurrentBridgeStatusMakerTest.java
@@ -0,0 +1,25 @@
+package bridge;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+public class CurrentBridgeStatusMakerTest {
+
+    CurrentBridgeStatusMaker currentBridgeStatusMaker = new CurrentBridgeStatusMaker();
+
+    @DisplayName("플레이어가 지나온 다리를 문자열로 나타낼 수 있다.")
+    @Test
+    public void 다리_기록_출력() {
+        List<String> playerBridgeStatus = List.of("U", "D", "U");
+        List<String> createdBridgeStatus = List.of("U", "D", "U");
+        List<StringBuilder> currentBridgeStatus =
+                currentBridgeStatusMaker.convertCurrentBridgeStatus(playerBridgeStatus, createdBridgeStatus);
+
+        Assertions.assertThat(currentBridgeStatus.get(1).toString()).isEqualTo("[ O |   | O ]");
+        Assertions.assertThat(currentBridgeStatus.get(0).toString()).isEqualTo("[   | O |   ]");
+    }
+
+}
