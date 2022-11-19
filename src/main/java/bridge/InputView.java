d3a9aaba2c7ee497254685d9f0f25b92254628ac
jinyeong-afk <xoeh7002@naver.com>
2022-11-19 19:06:14 +0900

    feat(InputView): input 입력 문구 추가

--- src/main/java/bridge/InputView.java
+++ src/main/java/bridge/InputView.java
@@ -7,10 +7,15 @@ import camp.nextstep.edu.missionutils.Console;
  */
 public class InputView {
 
+    private static final String BRIDGE_SIZE_TEXT = "다리의 길이를 입력해주세요.";
+    private static final String MOVE_COMMAND_TEXT = "이동할 칸을 선택해주세요. (위: U, 아래: D)";
+    private static final String GAME_COMMAND_TEXT = "게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)";
+
     /**
      * 다리의 길이를 입력받는다.
      */
     public int readBridgeSize() {
+        System.out.println(BRIDGE_SIZE_TEXT);
         String bridgeSize = Console.readLine();
         return Integer.parseInt(bridgeSize);
     }
@@ -19,6 +24,7 @@ public class InputView {
      * 사용자가 이동할 칸을 입력받는다.
      */
     public String readMoving() {
+        System.out.println(MOVE_COMMAND_TEXT);
         String moveCommand = Console.readLine();
         return moveCommand;
     }
@@ -27,6 +33,7 @@ public class InputView {
      * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
      */
     public String readGameCommand() {
+        System.out.println(GAME_COMMAND_TEXT);
         String gameCommand = Console.readLine();
         return gameCommand;
     }
