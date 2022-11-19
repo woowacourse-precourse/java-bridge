276129a7da45599322a41941e1a30772df3ef9ea
JIHYEISWISDOM <wlgp8958@naver.com>
2022-11-20 01:09:19 +0900

    test(NumberValidatorTest): 값이 올바른 경우 추가, 각 테이스 케이스에 다양한 변수 추가

--- src/test/java/bridge/validator/NumberValidatorTest.java
+++ src/test/java/bridge/validator/NumberValidatorTest.java
@@ -2,7 +2,9 @@ package bridge.validator;
 
 import bridge.NumberValidator;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 
@@ -10,19 +12,30 @@ class NumberValidatorTest {
     private final static String NON_NUMERIC_ERROR_MESSAGE = "[ERROR] 숫자만 입력 가능합니다.";
     private static final String RANGE_ERROR_MESSAGE = "[ERROR] 다리 길이는 3~20 중의 숫자여야 합니다.";
 
-    @DisplayName("다리 길이가 숫자가 아닐 시 예외가 발생한다.")
-    @Test
-    void numberValidateError() {
-        assertThatThrownBy(() -> NumberValidator.validateNonNumeric("r"))
-                .isInstanceOf(IllegalArgumentException.class)
-                .hasMessage(NON_NUMERIC_ERROR_MESSAGE);
+    @DisplayName("다리 길이가 3~20중의 숫자가 아닐 시 예외가 발생한다.")
+    @Nested
+    class BridgeSizeTest {
+        @CsvSource({"3", "4", "13", "19", "20"})
+        @ParameterizedTest
+        void 값이_올바른_경우(String input) {
+            NumberValidator.validateNonNumeric(input);
+            NumberValidator.validateRange(input);
         }
 
-    @DisplayName("다리 길이에 3~20이 아닌 숫자를 입력 시 예외가 발생한다.")
-    @Test
-    void rangeValidateError() {
-        assertThatThrownBy(() -> NumberValidator.validateRange("54"))
-                .isInstanceOf(IllegalArgumentException.class)
-                .hasMessage(RANGE_ERROR_MESSAGE);
+        @CsvSource({"!", "ㄱ", "p", "~", "ㅕ7", "4a"})
+        @ParameterizedTest
+        void 숫자가_아닌_경우(String input) {
+            assertThatThrownBy(() -> NumberValidator.validateNonNumeric(input))
+                    .isInstanceOf(IllegalArgumentException.class)
+                    .hasMessage(NON_NUMERIC_ERROR_MESSAGE);
+        }
+
+        @CsvSource({"-1", "1", "0", "55", "985"})
+        @ParameterizedTest
+        void 숫자의_범위가_아닌_경우(String input) {
+            assertThatThrownBy(() -> NumberValidator.validateRange(input))
+                    .isInstanceOf(IllegalArgumentException.class)
+                    .hasMessage(RANGE_ERROR_MESSAGE);
+        }
     }
-    }
+}
