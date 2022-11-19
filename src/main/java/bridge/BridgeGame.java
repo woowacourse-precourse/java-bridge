355b39f67b79e63d6093b49f5e02f400d3e46fbe
Hoo93 <117077999+Hoo93@users.noreply.github.com>
2022-11-19 22:07:42 +0900

    FEAT[BridgeGame]: 생성자와 다리를 한 칸 건너는 메소드 구현
    생성자에는 정답, 인풋, 아웃풋을 클래스 변수로 선언
    클래스 변수로 출력용 리스트를 가짐
    moveOne 메소드는 한 칸 옮기고 출력용 리스트를 수정

--- src/main/java/bridge/BridgeGame.java
+++ src/main/java/bridge/BridgeGame.java
@@ -1,16 +1,60 @@
 package bridge;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * 다리 건너기 게임을 관리하는 클래스
  */
 public class BridgeGame {
 
+    private static final String BLANK = " ";
+    private static final String CORRECT = "O";
+    private static final String WRONG = "X";
+
+
+    private final List<String> bridge;
+    private final InputView inputView;
+    private final OutputView outputView;
+
+    public List<String> upperLine = new ArrayList<>();
+    public List<String> lowerLine = new ArrayList<>();
+
+    public BridgeGame(List<String> bridge, InputView inputView, OutputView outputView){
+        this.bridge = bridge;
+        this.inputView = inputView;
+        this.outputView = outputView;
+    }
     /**
      * 사용자가 칸을 이동할 때 사용하는 메서드
      * <p>
      * 이동을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
     public void move() {
+    }
+
+    public boolean moveOne(int index) {
+        String userInput = inputView.readMoving();
+        if(bridge.get(index).equals(userInput)){
+            if ( userInput.equals("U")) {
+                upperLine.add(CORRECT);
+                lowerLine.add(BLANK);
+            }
+            if ( userInput.equals("D")){
+                upperLine.add(BLANK);
+                lowerLine.add(CORRECT);
+            }
+            return true;
+        }
+        if ( userInput.equals("U")) {
+            upperLine.add(WRONG);
+            lowerLine.add(BLANK);
+        }
+        if ( userInput.equals("D")){
+            upperLine.add(BLANK);
+            lowerLine.add(WRONG);
+        }
+        return false;
     }
 
     /**
