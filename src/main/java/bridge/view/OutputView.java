cbe02c04e9ef9192d90d3a69b948dc4feb131c25
situodys <situodys@gmail.com>
2022-11-19 17:25:11 +0900

    feat(OutputView): 다리 진행 상태 출력 기능 구현

--- src/main/java/bridge/view/OutputView.java
+++ src/main/java/bridge/view/OutputView.java
@@ -1,6 +1,6 @@
 package bridge.view;
 
-import java.sql.SQLOutput;
+import java.util.List;
 
 /**
  * 사용자에게 게임 진행 상황과 결과를 출력하는 역할을 한다.
@@ -10,6 +10,10 @@ public class OutputView {
     private static final String BRIDGE_SIZE_DEMAND_MSG = "다리의 길이를 입력해주세요.";
     private static final String BRIDGE_MOVE_DEMAND_MSG = "이동할 칸을 선택해주세요. (위: U, 아래: D)";
     private static final String ERROR_PREFIX = "[ERROR] ";
+    private static final String MAP_PREFIX = "[ ";
+    private static final String MAP_SUFFIX = " ]";
+    private static final String STRING_DELIMITER = "";
+    private static final String MAP_DELIMITER = " | ";
 
 
     /**
@@ -17,7 +21,13 @@ public class OutputView {
      * <p>
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void printMap() {
+    public void printMap(List<String> bridges) {
+        System.out.println(MAP_PREFIX + toPrintForm(bridges.get(0)) +MAP_SUFFIX);
+        System.out.println(MAP_PREFIX + toPrintForm(bridges.get(1)) +MAP_SUFFIX);
+    }
+
+    private String toPrintForm(String bridge) {
+        return String.join(MAP_DELIMITER, bridge.split(STRING_DELIMITER));
     }
 
     /**
