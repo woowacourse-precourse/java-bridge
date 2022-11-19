094768d8e58c0e67ddf159666c9082b6e661c939
yoonjuho <dbswn990@naver.com>
2022-11-20 05:03:34 +0900

    feat(InputView): 다리의 길이는 숫자로만 구성된다

--- src/main/java/bridge/view/InputView.java
+++ src/main/java/bridge/view/InputView.java
@@ -1,14 +1,20 @@
-package bridge;
+package bridge.view;
+
+import java.io.Console;
 
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
  */
 public class InputView {
 
+
     /**
      * 다리의 길이를 입력받는다.
      */
-    public int readBridgeSize() {
+    public int readBridgeSize(String input) {
+        if(!input.matches("^[0-9]")){
+            throw new IllegalArgumentException();
+        }
         return 0;
     }
 
