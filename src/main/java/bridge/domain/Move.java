bca0bf4688dd13de3538bbdc030fad8f6f4f1c2b
Basak <chgy2131@naver.com>
2022-11-20 00:40:41 +0900

    feat(Move): 움직임을 나타내는 용어들을 저장하는 클래스 추가

--- src/main/java/bridge/domain/Move.java
+++ src/main/java/bridge/domain/Move.java
@@ -1,4 +1,26 @@
 package bridge.domain;
 
+import java.util.Objects;
+
 public enum Move {
+    UP("U", 1),
+    DOWN("D", 0)
+    ;
+    private String direction;
+    private int number;
+
+    Move(String direction, int number) {
+        this.direction = direction;
+        this.number = number;
+    }
+
+    public static Move fromString(String direction) {
+        if("U".equals(direction) || "1".equals(direction)) {
+            return Move.UP;
+        }
+        if("D".equals(direction) || "0".equals(direction)) {
+            return Move.DOWN;
+        }
+        throw new IllegalArgumentException();
+    }
 }
