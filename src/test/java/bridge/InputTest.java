e4d1c908da7a73ad12ff74f4bdac8c03ae7a2b26
dldbdud314 <dldbdud314@naver.com>
2022-11-19 20:39:37 +0900

    test(InputBridgeTest): 다리 크기 입력 테스트

--- src/test/java/bridge/InputTest.java
+++ src/test/java/bridge/InputTest.java
@@ -1,11 +1,57 @@
-package bridge.ui;
+package bridge;
 
+import static bridge.ui.MessageUtil.*;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.*;
 
+import bridge.domain.Bridge;
+import bridge.ui.InputView;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 @DisplayName("입력값 검증 관련 테스트")
 class InputTest {
-    class
+
+    InputView inputView;
+
+    @BeforeEach
+    void setUp(){
+        inputView = new InputView();
+    }
+
+    @Nested
+    class InputBridgeSize {
+        @ParameterizedTest
+        @ValueSource(strings = {"aa", " ", "1b", "-."})
+        @DisplayName("숫자가 아닌 값을 입력하면 예외 발생")
+        void exceptionTest_notNumeric(String input){
+            assertThatThrownBy(() -> inputView.readBridgeSize(input))
+                    .isInstanceOf(IllegalArgumentException.class)
+                    .hasMessage(INVALID_BRIDGE_SIZE.message);
+        }
+
+        @ParameterizedTest
+        @ValueSource(strings = {"1", "67", "43"})
+        @DisplayName("범위 밖의 숫자 입력 시 예외 발생")
+        void exceptionTest_overRange(String input){
+            assertThatThrownBy(() -> inputView.readBridgeSize(input))
+                    .isInstanceOf(IllegalArgumentException.class)
+                    .hasMessage(INVALID_BRIDGE_SIZE.message);
+        }
+
+        @ParameterizedTest
+        @CsvSource(value = {"3", "20", "12"})
+        @DisplayName("입력값과 같은 크기의 다리가 만들어졌는지 확인")
+        void validInputTest(String input){
+            int size = inputView.readBridgeSize(input);
+            Bridge bridge = new Bridge(size);
+            assertEquals(bridge.getBridgeSize(), Integer.parseInt(input));
+        }
+    }
+
+
 }
