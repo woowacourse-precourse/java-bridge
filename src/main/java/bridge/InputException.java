f96ceef5bb060964d4e4a79b0071830cde885c68
2do1 <dowon3036@naver.com>
2022-11-19 21:16:18 +0900

    feat: 이동할 칸을 입력받을 때 예외처리 구현

--- src/main/java/bridge/InputException.java
+++ src/main/java/bridge/InputException.java
@@ -3,7 +3,9 @@ package bridge;
 public class InputException {
     static final String NOT_INTEGER_ERROR_MESSAGE = "[ERROR] 숫자를 입력해주세요.";
     static final String BRIDGE_SIZE_RANGE_ERROR_MESSAGE = "[ERROR] 다리 길이는 3부터 20 사이의 숫자여야 합니다.";
-
+    static final String MOVING_ERROR_MESSAGE = "[ERROR] U(위 칸) 또는 D(아래 칸) 중 하나를 입력해주세요.";
+    static final String MOVING_UP = "U";
+    static final String MOVING_DOWN = "D";
     static final int MIN_BRIDGE_SIZE = 3;
     static final int MAX_BRIDGE_SIZE = 20;
 
@@ -16,6 +18,12 @@ public class InputException {
     public static void checkBridgeSizeRange(int bridgeSize) {
         if (bridgeSize < MIN_BRIDGE_SIZE || bridgeSize > MAX_BRIDGE_SIZE) {
             throw new IllegalArgumentException(BRIDGE_SIZE_RANGE_ERROR_MESSAGE);
+        }
+    }
+
+    public static void checkMoving(String moving) {
+        if (!moving.equals(MOVING_UP) && !moving.equals(MOVING_DOWN)) {
+            throw new IllegalArgumentException(MOVING_ERROR_MESSAGE);
         }
     }
 }
