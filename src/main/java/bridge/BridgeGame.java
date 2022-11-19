1e1cd48c8b4edf43f75146ff068d6a980e2a2c0d
lseok <lseokhyun99@gmail.com>
2022-11-19 20:11:56 +0900

    feat: 게임을 다시 시도할 때 사용하는 메소드 구현
    게임을 다시 시도할 때 사용하는 메소드
    retry()를 구현하였다.
    
    반환형은 boolean 타입이고, 인자로 받은
    gameRetryCommand이
    RETRY와 같으면 true를 반환
    QUIT와 같으면 false를 반환한다.
    
    gameRetryCommand이 RETRY, QUIT이 아니면
    에러를 발생 시킨다.

--- src/main/java/bridge/BridgeGame.java
+++ src/main/java/bridge/BridgeGame.java
@@ -6,6 +6,8 @@ import java.util.List;
  * 다리 건너기 게임을 관리하는 클래스
  */
 public class BridgeGame {
+    private static final String RETRY = "R";
+    private static final String QUIT = "Q";
 
     /**
      * 사용자가 칸을 이동할 때 사용하는 메서드
@@ -27,6 +29,12 @@ public class BridgeGame {
      * <p>
      * 재시작을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void retry() {
+    public boolean retry(String gameRetryCommand) {
+        if (gameRetryCommand.equals(RETRY))
+            return true;
+        if (gameRetryCommand.equals(QUIT))
+            return false;
+
+        throw new IllegalArgumentException();
     }
 }
