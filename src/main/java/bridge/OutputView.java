7f0a55a933b418137a50487fd36aaa36df0d1e8a
Haechan Lee <dlgockssla@gmail.com>
2022-11-19 10:04:29 +0900

    feat(OutputView): ask retry, ask size, ask direction

--- src/main/java/bridge/OutputView.java
+++ src/main/java/bridge/OutputView.java
@@ -5,6 +5,14 @@ package bridge;
  */
 public class OutputView {
 
+    public static void askRetry() {
+        System.out.println("\n게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)");
+    }
+
+    public static void askSize() {
+        System.out.println("\n다리의 길이를 입력해주세요.");
+    }
+
     /**
      * 현재까지 이동한 다리의 상태를 정해진 형식에 맞춰 출력한다.
      * <p>
@@ -19,9 +27,14 @@ public class OutputView {
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
     public void printResult() {
+        System.out.println("\n게임 끝");
     }
 
     public void printBegin() {
         System.out.println("다리 건너기 게임을 시작합니다.");
+    }
+
+    public void askDirection() {
+        System.out.println("\n이동할 칸을 선택해주세요. (위: U, 아래: D)");
     }
 }
