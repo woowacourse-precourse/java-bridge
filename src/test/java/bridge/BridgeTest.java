4087d15128bb89ddd422e98c3d5671a2d5810325
김태용 <amico741@naver.com>
2022-11-19 17:11:47 +0900

    test: Player 움직임 여부 테스트 추가

--- src/test/java/bridge/BridgeTest.java
+++ src/test/java/bridge/BridgeTest.java
@@ -21,7 +21,19 @@ public class BridgeTest {
         bridge = new Bridge(List.of("D", "U", "D", "U"));
         success = new Player(4);
         fail = new Player(4);
-        List.of("D", "U").forEach(e -> success.inputDirection(e));
+        List.of("D", "U", "D", "U").forEach(e -> success.inputDirection(e));
         List.of("D", "U", "D", "D").forEach(e -> fail.inputDirection(e));
+    }
+
+    @Test
+    @DisplayName("플레이어의 입력 횟수가 다리 끝에 도달하면 true를 반환한다.")
+    void 플레이어가_끝에_도달하면_정상종료() {
+        assertThat(success.isFinish()).isTrue();
+    }
+
+    @Test
+    @DisplayName("플레이어의 입력이 움직일 수 있는 방향이 아니면 true를 반환한다.")
+    void 플레이어의_입력방향이_틀리면_정상종료() {
+        assertThat(fail.hasNotSameDirection(bridge)).isTrue();
     }
 }
