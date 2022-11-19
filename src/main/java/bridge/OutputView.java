1cfad48341d4e3bffd9b7b7adf6dd24d4a8650d0
Nugur1i <duckhee092@naver.com>
2022-11-19 19:11:57 +0900

    feat(OutputView): implement map 2 dimension and test

--- src/main/java/bridge/OutputView.java
+++ src/main/java/bridge/OutputView.java
@@ -8,7 +8,8 @@ import java.util.List;
  * 사용자에게 게임 진행 상황과 결과를 출력하는 역할을 한다.
  */
 public class OutputView {
-    private static List<String> map = new ArrayList<>(Arrays.asList(new String[]{"[ ", " ]"}));
+    private static List<String> mapUp = new ArrayList<>(Arrays.asList(new String[]{"[ ", " ]"}));
+    private static List<String> mapDown = new ArrayList<>(Arrays.asList(new String[]{"[ ", " ]"}));
     private static final String CONTOUR = " | ";
 
     /**
@@ -16,15 +17,24 @@ public class OutputView {
      * <p>
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void printMap(String ox, int order) {
+    public void printMap(String ox, int order, String upDown) {
         String addOxContour = CONTOUR + ox;
+        String emptyContour = CONTOUR + " ";
+
         if(order == 0){
             addOxContour = ox;
+            emptyContour = " ";
         }
 
-        map.add(map.size()-1,addOxContour);
+        if(upDown.equals("U")){
+            mapUp.add(mapUp.size()-1,addOxContour);
+            mapDown.add(mapDown.size()-1,emptyContour);
+        } else if (upDown.equals("D")) {
+            mapUp.add(mapUp.size()-1,emptyContour);
+            mapDown.add(mapDown.size()-1,addOxContour);
+        }
 
-        String str = String.join("",map);
+        String str = String.join("", mapUp) + "\n" + String.join("", mapDown);
 
         System.out.println(str);
     }
--- src/test/java/bridge/OutputViewTest.java
+++ src/test/java/bridge/OutputViewTest.java
@@ -10,15 +10,17 @@ class OutputViewTest extends NsTest {
 
     private static String ox;
     private static int order;
+    private static String upDown;
 
 
     @Test
     void 맵_생성_테스트_첫번째(){
         this.ox = "O";
         this.order = 0;
+        this.upDown = "U";
         run();
         assertThat(output()).contains(
-                "[ O ]"
+                "[ O ]\n[   ]"
         );
     }
 
@@ -26,12 +28,14 @@ class OutputViewTest extends NsTest {
     void 맵_생성_테스트_두번째(){
         this.ox = "O";
         this.order = 0;
+        this.upDown = "U";
         run();
 
         this.order++;
+        this.upDown = "D";
         run();
         assertThat(output()).contains(
-                "[ O | O ]"
+                "[ O |   ]\n[   | O ]"
         );
     }
 
@@ -39,7 +43,7 @@ class OutputViewTest extends NsTest {
     @Override
     protected void runMain() {
         OutputView outputView = new OutputView();
-        outputView.printMap(this.ox, this.order);
+        outputView.printMap(this.ox, this.order, this.upDown);
 
 
     }
