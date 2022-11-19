422fb9bce498b4e01e6d9dfd461c476077de9a80
YuHayeon <youha0930@gmail.com>
2022-11-20 03:42:35 +0900

    refactor(BridgeGame): 출력을 위해 MapDto으로 반환 타입 변경
    출력에서 dto를 사용하기 위해 출력에 필요한 MapDto를 반환하는 것으로 변경

--- src/main/java/bridge/controller/BridgeGame.java
+++ src/main/java/bridge/controller/BridgeGame.java
@@ -1,5 +1,6 @@
 package bridge.controller;
 
+import bridge.dto.MapDto;
 import bridge.model.Bridge;
 import bridge.model.BridgeResult;
 
@@ -26,7 +27,9 @@ public class BridgeGame {
      * <p>
      * 이동을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void move() {
+    public MapDto move(String moving) {
+        bridgeResult.add(bridge, moving);
+        return new MapDto(bridge, bridgeResult);
     }
 
     /**
