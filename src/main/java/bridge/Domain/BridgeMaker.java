d5df1126fbafd7c50fa2bf9cc92daa1a925551b2
Nitroblack1 <109022435+Nitroblack1@users.noreply.github.com>
2022-11-19 18:00:22 +0900

    feat(main\...\Domain) : implement method that makes bridge design

--- src/main/java/bridge/Domain/BridgeMaker.java
+++ src/main/java/bridge/Domain/BridgeMaker.java
@@ -1,7 +1,10 @@
 package bridge.Domain;
 
 import bridge.BridgeNumberGenerator;
+import java.util.ArrayList;
 import java.util.List;
+import bridge.Constants.StandardTools;
+import java.util.Objects;
 
 /**
  * 다리의 길이를 입력 받아서 다리를 생성해주는 역할을 한다.
@@ -19,6 +22,14 @@ public class BridgeMaker {
      * @return 입력받은 길이에 해당하는 다리 모양. 위 칸이면 "U", 아래 칸이면 "D"로 표현해야 한다.
      */
     public List<String> makeBridge(int size) {
-        return null;
+        List<String> bridge = new ArrayList<>();
+        for (int i = 0; i < size; i++) {
+            if (Objects.equals(bridgeNumberGenerator.generate(), StandardTools.UP_SIDE)) {
+                bridge.add("U");
+                continue;
+            }
+                bridge.add("D");
+        }
+        return bridge;
     }
 }
