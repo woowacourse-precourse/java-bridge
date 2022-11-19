4edcb75e0e85352353a1f17396bd1b8d7265f2f8
Nitroblack1 <109022435+Nitroblack1@users.noreply.github.com>
2022-11-19 15:15:02 +0900

    feat(test\...\UITest) : implement validation for next step input

--- src/test/java/bridge/UITest/InputViewTest.java
+++ src/test/java/bridge/UITest/InputViewTest.java
@@ -4,8 +4,10 @@ import bridge.UI.InputView;
 import java.io.ByteArrayInputStream;
 import java.io.InputStream;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
 import org.mockito.Mock;
 
@@ -20,24 +22,52 @@ public class InputViewTest {
     // given
     InputView inputView = new InputView();
 
-    @DisplayName("다리의 길이가 올바른 형식으로 입력되었는지 확인한 후 받는다.")
-    @Test
-    void readBridgeLengthTest() {
-        // when
-        String expected = "3";
-        InputStream in = new ByteArrayInputStream(expected.getBytes());
-        System.setIn(in);
+    @Nested
+    @DisplayName("다리 길이 입력값 테스트")
+    class BridgeLengthInputTest {
 
-        //then
-        assertThat(inputView.readBridgeLength()).isEqualTo(Integer.parseInt(expected));
+        @Test
+        @DisplayName("다리의 길이가 올바른 형식으로 입력되었는지 확인한 후 받는다.")
+        void wrightBridgeLengthInput() {
+            // when
+            String expected = "3";
+            InputStream in = new ByteArrayInputStream(expected.getBytes());
+            System.setIn(in);
+
+            //then
+            assertThat(inputView.readBridgeLength()).isEqualTo(Integer.parseInt(expected));
+        }
+
+        @DisplayName("다리의 길이가 올바르지 않은 형식이라면 예외를 던진다.")
+        @ParameterizedTest
+        @ValueSource(strings = {"2", "21", "", " ", "#", "q"})
+        void wrongBridgeLengthInput(String testCase) {
+            assertThatThrownBy(() -> inputView.validateBridgeLength(testCase))
+                    .isInstanceOf(IllegalArgumentException.class)
+                    .hasMessageContaining(ERROR_MESSAGE);
+        }
     }
 
-    @DisplayName("다리의 길이가 올바르지 않은 형식이라면 예외를 던진다.")
-    @ParameterizedTest
-    @ValueSource(strings = {"2", "21", "", " ", "#", "q"})
-    void readBridgeLengthExceptionHandlingTest(String testCase) {
-        assertThatThrownBy(() -> inputView.validateBridgeLength(testCase))
-                .isInstanceOf(IllegalArgumentException.class)
-                .hasMessageContaining(ERROR_MESSAGE);
+    @Nested
+    @DisplayName("입력할 칸을 입력받을 때")
+    class NextStepInputTest {
+
+        @DisplayName("U나 D중 하나의 값을 받는다.")
+        @ParameterizedTest
+        @CsvSource({"U,1", "D,0"})
+        void wrightNextStepInput(String nextStep, int expected) {
+            InputStream in = new ByteArrayInputStream(nextStep.getBytes());
+            System.setIn(in);
+            assertThat(inputView.readMoving()).isEqualTo(expected);
+        }
+
+        @DisplayName("U나 D가 아닌 값을 받으면 예외 처리를 한다.")
+        @ParameterizedTest
+        @ValueSource(strings = {"q", "r", "", " ", "3"})
+        void wrongNextStepInput(String wrongInput) {
+            assertThatThrownBy(() -> inputView.validateBridgeLength(wrongInput))
+                    .isInstanceOf(IllegalArgumentException.class)
+                    .hasMessageContaining(ERROR_MESSAGE);
+        }
     }
 }
