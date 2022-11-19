dbeb63b603adac9c7062cb14402d008a16f9b4f3
HyeonJun0530 <nexus2697@pukyong.ac.kr>
2022-11-19 20:18:36 +0900

    feat(controller): 컨트롤러 기능 구현

---
+++ src/main/java/bridge/controller/BridgeController.java
@@ -0,0 +1,64 @@
+package bridge.controller;
+
+import bridge.service.BridgeService;
+import bridge.view.InputView;
+import bridge.view.OutputView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class BridgeController {
+
+    private InputView inputView = new InputView();
+    private OutputView outputView = new OutputView();
+    private BridgeService bridgeService = new BridgeService();
+    private List<String> bridge = new ArrayList<>();
+    private List<List<String>> upAndDown = new ArrayList<>();
+    private boolean isMove = true;
+    private int index = 0;
+    private int attempt = 1;
+
+
+    public void startGame() {
+        int size = Integer.valueOf(inputView.readBridgeSize());
+        this.bridge = bridgeService.bridgeMake(size);
+    }
+
+    public void playingGame() {
+        playGame();
+        if(isMove)
+            successGame();
+        else if (!isMove) {
+            failGame();
+        }
+    }
+
+    private void playGame() {
+        while (isMove && bridge.size() != index) {
+            String inputMove = inputView.readMoving();
+
+            isMove = bridgeService.isMove(inputMove, this.bridge);
+            this.upAndDown = bridgeService.upAndDownProgress(inputMove, isMove);
+
+            outputView.printMap(upAndDown.get(0), upAndDown.get(1));
+            index++;
+        }
+    }
+
+    private void failGame() {
+        String inputRetry = inputView.readGameCommand();
+
+        while (bridgeService.retryJudge(inputRetry)) {
+            attempt++;
+            index = 0;
+            playGame();
+        }
+        outputView.printResult(upAndDown.get(0), upAndDown.get(1));
+        outputView.printFailResult(attempt);
+    }
+
+    private void successGame() {
+        outputView.printResult(upAndDown.get(0), upAndDown.get(1));
+        outputView.printSuccessResult(attempt);
+    }
+}
