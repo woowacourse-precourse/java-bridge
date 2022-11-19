5d1ab0c910418f72adfd931210091be872db526e
ysng_ysng <yeasung67@gmail.com>
2022-11-19 22:00:36 +0900

    feat(BridgeGame): Implement the retry method
    게임의 종료와 재시작을 담당하는 retry메서드를 구현했습니다.

--- src/main/java/bridge/model/BridgeGame.java
+++ src/main/java/bridge/model/BridgeGame.java
@@ -1,23 +1,93 @@
 package bridge.model;
 
+import bridge.dto.GameResultDto;
+import bridge.dto.MoveCommandDto;
+
+import java.util.List;
+
 /**
  * 다리 건너기 게임을 관리하는 클래스
  */
 public class BridgeGame {
+	private List<String> bridge;
+	private int currentStep;
+	private int totalTry;
+	private boolean gameClear;
 
-    /**
-     * 사용자가 칸을 이동할 때 사용하는 메서드
-     * <p>
-     * 이동을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
-     */
-    public void move() {
-    }
+	public void initializeBridge(String bridgeLength) {
+		if (UserInputExceptions.isNotValidLength(bridgeLength)) {
+			throw new IllegalArgumentException(Messages.errorLength);
+		}
 
-    /**
-     * 사용자가 게임을 다시 시도할 때 사용하는 메서드
-     * <p>
-     * 재시작을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
-     */
-    public void retry() {
-    }
+		int convertedLength = Utils.convertBridgeLengthToNumber(bridgeLength);
+		this.bridge = GameSetting.setBridgeForStartGame(convertedLength);
+		currentStep = GameSetting.START_STEP;
+		totalTry = GameSetting.FIRST_TRY;
+		gameClear = GameSetting.GAME_FAIL;
+	}
+
+	/**
+	 * 사용자가 칸을 이동할 때 사용하는 메서드
+	 * <p>
+	 * 이동을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
+	 */
+	public MoveCommandDto move(String moveCommand) {
+		if (UserInputExceptions.isNotValidMove(moveCommand)) {
+			throw new IllegalArgumentException(Messages.errorMovableCommand);
+		}
+
+		boolean moveFlag = BridgeChecker.isMovableLocation(bridge, moveCommand, currentStep);
+		currentStep += addStepIfMove(moveFlag);
+		moveFlag = isClear();
+
+		return new MoveCommandDto(moveCommand, moveFlag);
+	}
+
+	/**
+	 * 사용자가 게임을 다시 시도할 때 사용하는 메서드
+	 * <p>
+	 * 재시작을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
+	 */
+	public GameResultDto retry(String endCommand) {
+		if (UserInputExceptions.isNotValidEndCommand(endCommand)) {
+			throw new IllegalArgumentException(Messages.errorEndCommand);
+		}
+
+		if (endCommand.equals("R")) {
+			currentStep = GameSetting.START_STEP;
+			totalTry++;
+		}
+
+		return GameSetting.setDataForRetryGame(totalTry, gameClear);
+	}
+
+	private int addStepIfMove(boolean moveFlag) {
+		if (moveFlag) {
+			return 1;
+		}
+		return 0;
+	}
+
+	private boolean isClear() {
+		if (bridge.size() == currentStep) {
+			gameClear = GameSetting.GAME_SUCCESS;
+			return false;
+		}
+		return true;
+	}
+
+	@Override public String toString() {
+		StringBuilder sb = new StringBuilder();
+
+		sb.append("다리의 정보 : ");
+		for (String bridgeUnit : bridge) {
+			sb.append(bridgeUnit).append(" ");
+		}
+		sb.append("\n");
+		sb.append("다리의 길이 : ").append(bridge.size()).append("\n");
+
+		sb.append("현재 다리에서의 위치 : ").append(currentStep).append(" \n총 시도횟수 : ").append(totalTry);
+		sb.append("\n게임을 성공했습니까? : ").append(gameClear).append("\n");
+		return sb.toString();
+	}
 }
