db5bd173473f55a312ffeb654587f14c11d09035
gyumin159 <gyumin159@naver.com>
2022-11-19 12:48:26 +0900

    refactoring : 에러메세지 enum 으로 정리

--- src/main/java/bridge/ValidateInput.java
+++ src/main/java/bridge/ValidateInput.java
@@ -4,7 +4,7 @@ import java.util.regex.Pattern;
 
 public class ValidateInput {
     private enum regexPatterns{
-        ONLY_DIGIT("^[0-9]+$"),
+        ONLY_DIGIT("^[0-9]+{1,2}$"),
         U_OR_D("^[UD]$"),
         R_OR_Q("^[RQ]$");
 
@@ -15,10 +15,23 @@ public class ValidateInput {
         }
     }
 
+    private enum ErrorMassage{
+        DIGIT_ONLY("숫자로만 구성되어야 합니다."),
+        NOT_IN_RANGE("3이상 20이하이어야 합니다."),
+        NOT_U_OR_D("이동은 U 또는 D만 가능합니다."),
+        NOT_R_OR_Q("게임 재시작은 R, 종료는 Q를 입력해주세요.");
+
+        final String line;
+
+        ErrorMassage(String line){
+            this.line = line;
+        }
+    }
+
     public static boolean isAllDigit(String rawInput){
         boolean isDigit = Pattern.matches(regexPatterns.ONLY_DIGIT.regex, rawInput);
         if (!isDigit) {
-            throw new IllegalArgumentException("숫자로만 구성되어야 합니다.");
+            throw new IllegalArgumentException(ErrorMassage.DIGIT_ONLY.line);
         }
         return true;
     }
@@ -26,7 +39,7 @@ public class ValidateInput {
     public static boolean isInRange(String rawInput) { // "0004" 이런 입력은 올바른 입력으로 통과됨.
         int target = Integer.parseInt(rawInput);
         if (target < 3 || target > 20) {
-            throw new IllegalArgumentException("3이상 20이하이어야 합니다.");
+            throw new IllegalArgumentException(ErrorMassage.NOT_IN_RANGE.line);
         }
         return true;
     }
@@ -34,7 +47,7 @@ public class ValidateInput {
     public static boolean isUorD(String rawInput){
         boolean isUorD = Pattern.matches(regexPatterns.U_OR_D.regex, rawInput);
         if (!isUorD){
-            throw new IllegalArgumentException("이동은 U 또는 D만 가능합니다.");
+            throw new IllegalArgumentException(ErrorMassage.NOT_U_OR_D.line);
         }
         return true;
     }
@@ -42,7 +55,7 @@ public class ValidateInput {
     public static boolean isRorQ(String rawInput){
         boolean isRorQ = Pattern.matches(regexPatterns.R_OR_Q.regex, rawInput);
         if (!isRorQ){
-            throw new IllegalArgumentException("게임 재시작은 R, 종료는 Q를 입력해주세요.");
+            throw new IllegalArgumentException(ErrorMassage.NOT_R_OR_Q.line);
         }
         return true;
     }
