ce64a7f38a765c5a853c5448d66115b0f4ef871f
NotableDeveloper <castlereal@naver.com>
2022-11-19 07:24:50 -0800

    <refactoring> : 다리 생성 테스트 코드 리팩토링 기존에 ApplicationTest에 작성되어 있었던 다리 생성 관련 테스트 코드로 수정함.

--- src/test/java/bridge/ApplicationTest.java
+++ src/test/java/bridge/ApplicationTest.java
@@ -13,16 +13,7 @@ import java.util.List;
 import org.junit.jupiter.api.Test;
 
 class ApplicationTest extends NsTest {
-
     private static final String ERROR_MESSAGE = "[ERROR]";
-
-    @Test
-    void 다리_생성_테스트() {
-        BridgeNumberGenerator numberGenerator = new TestNumberGenerator(newArrayList(1, 0, 0));
-        BridgeMaker bridgeMaker = new BridgeMaker(numberGenerator);
-        List<String> bridge = bridgeMaker.makeBridge(3);
-        assertThat(bridge).containsExactly("U", "D", "D");
-    }
 
     @Test
     void 기능_테스트() {
@@ -53,19 +44,5 @@ class ApplicationTest extends NsTest {
     @Override
     protected void runMain() {
         Application.main(new String[]{});
-    }
-
-    static class TestNumberGenerator implements BridgeNumberGenerator {
-
-        private final List<Integer> numbers;
-
-        TestNumberGenerator(List<Integer> numbers) {
-            this.numbers = numbers;
-        }
-
-        @Override
-        public int generate() {
-            return numbers.remove(0);
-        }
     }
 }
--- src/test/java/bridge/BridgeMakerTest.java
+++ src/test/java/bridge/BridgeMakerTest.java
@@ -1,18 +1,38 @@
 package bridge;
 
 import bridge.BridgeMaker.BridgeMaker;
+import bridge.BridgeMaker.NumberGenerator.BridgeNumberGenerator;
 import bridge.BridgeMaker.NumberGenerator.BridgeRandomNumberGenerator;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.util.Lists.newArrayList;
 
 public class BridgeMakerTest {
+    static class TestNumberGenerator implements BridgeNumberGenerator {
+        private final List<Integer> numbers;
+
+        TestNumberGenerator(List<Integer> numbers) {
+            this.numbers = numbers;
+        }
+
+        @Override
+        public int generate() {
+            return numbers.remove(0);
+        }
+    }
+
     @DisplayName("길이가 3인 다리를 생성하는 메서드를 호출하면 그 결과로 길이가 3인 다리가 생성되어야 한다.")
     @Test
     void makeBridgeSize3ToSuccessful() {
-        BridgeRandomNumberGenerator generator = new BridgeRandomNumberGenerator();
-        BridgeMaker bridgeMaker = new BridgeMaker(generator);
-        assertThat(bridgeMaker.makeBridge(3).size() == 3);
+        BridgeNumberGenerator numberGenerator = new TestNumberGenerator(newArrayList(1, 0, 0));
+        BridgeMaker bridgeMaker = new BridgeMaker(numberGenerator);
+        List<String> bridge = bridgeMaker.makeBridge(3);
+        assertThat(bridge.size() == 3);
     }
 }
+
+
