dac00b7ce5b8d2f4aafd0d7c81f94ebc8d68dd0d
62hoon99 <98267772+62hoon99@users.noreply.github.com>
2022-11-19 09:23:38 +0900

    feat(view): 플레이어가 이동할 칸을 입력 받는 기능 구현

--- src/main/java/bridge/view/Phrases.java
+++ src/main/java/bridge/view/Phrases.java
@@ -1,7 +1,8 @@
 package bridge.view;
 
 public enum Phrases {
-    readBridgeSize("다리의 길이를 입력해주세요.");
+    readBridgeSize("다리의 길이를 입력해주세요."),
+    readMoving("이동할 칸을 선택해주세요. (위: U, 아래: D)");
 
     private String phrase;
 
