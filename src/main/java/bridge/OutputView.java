c51f6166a4410de458d55509c734e82b142f97da
Sangwoo Shin <sangwoonoel@naver.com>
2022-11-19 23:56:22 +0900

    feat(OutputView): 출력 형식에 맞게 출력 폼을 만들어주는 기능 구현

--- src/main/java/bridge/OutputView.java
+++ src/main/java/bridge/OutputView.java
@@ -1,13 +1,16 @@
 package bridge;
 
+import java.util.ArrayList;
 import java.util.List;
+
+import static bridge.Result.*;
 
 /**
  * 사용자에게 게임 진행 상황과 결과를 출력하는 역할을 한다.
  */
 public class OutputView {
 
-    private List<Result> results;
+    private List<Result> results = new ArrayList<>();
     /**
      * 현재까지 이동한 다리의 상태를 정해진 형식에 맞춰 출력한다.
      * <p>
@@ -18,6 +21,33 @@ public class OutputView {
 
     private void addResult(Result result) {
         results.add(result);
+    }
+
+    private List<List<String>> makePrintForm() {
+        List<List<String>> printForm = new ArrayList<>();
+        List<String> up = new ArrayList<>();
+        List<String> down = new ArrayList<>();
+        for (int i = 0; i < results.size(); i++) {
+            if (results.get(i) == UP_SUCCESS) {
+                up.add(UP_SUCCESS.getAcceptability());
+                down.add(" ");
+            }
+            if (results.get(i) == UP_FAIL) {
+                up.add(" ");
+                down.add(UP_FAIL.getAcceptability());
+            }
+            if (results.get(i) == DOWN_SUCCESS) {
+                up.add(" ");
+                down.add(DOWN_SUCCESS.getAcceptability());
+            }
+            if (results.get(i) == DOWN_FAIL) {
+                up.add(DOWN_FAIL.getAcceptability());
+                down.add(" ");
+            }
+        }
+        printForm.add(up);
+        printForm.add(down);
+        return printForm;
     }
 
     /**
