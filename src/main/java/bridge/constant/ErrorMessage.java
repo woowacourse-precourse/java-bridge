dbe5f9ec86d804584dc6d8926974f949d8e533cb
Hi-Imjaeyoung <imjy2757@naver.com>
2022-11-19 15:42:33 +0900

    feat(ErrorMessage) : 메시지 getter 기능구현 및 숫자 범위에 관한 에러메시지 추가.

--- src/main/java/bridge/constant/ErrorMessage.java
+++ src/main/java/bridge/constant/ErrorMessage.java
@@ -1,9 +1,12 @@
 package bridge.constant;
 
 public enum ErrorMessage {
-    NO_NUMERIC_VALUE(" 숫자를 입력해주세요.");
+    NO_NUMERIC_VALUE(" 숫자를 입력해주세요."), NUMBER_RANGE_IS_WRONG(" 3이상 20이하의 숫자를 입력해주세요.");
     String errorMessage;
     ErrorMessage(String errorMessage){
         this.errorMessage = errorMessage;
+    }
+    public String getErrorMessage(){
+        return errorMessage;
     }
 }
