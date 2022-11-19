75caa13f746c7b721c5907e79b2c83ee8b5add6b
wonju-dev <wonjulee.dev@gmail.com>
2022-11-19 18:32:59 +0900

    feat(InputView): 다리 길이 입력 기능 구현
    InputView의 readBridgeSize는 사용자로부터 다리 길이를 입력받습니다.
    유효하지 않은 입력이 들어온 경우, IllegalArgumentException을 반환합니다.

--- src/main/java/bridge/InputView.java
+++ src/main/java/bridge/InputView.java
@@ -1,7 +1,19 @@
 package bridge;
 
+import bridge.condition.Condition;
+import bridge.condition.ConditionGenerator;
+import bridge.validator.Validator;
+import camp.nextstep.edu.missionutils.Console;
+
+import java.util.List;
+
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
+ * 제공된 InputView 클래스를 활용해 구현해야 한다.
+ * InputView의 패키지는 변경할 수 있다.
+ * InputView의 메서드의 시그니처(인자, 이름)와 반환 타입은 변경할 수 있다.
+ * 사용자 값 입력을 위해 필요한 메서드를 추가할 수 있다.
+ * InputView 클래스에서만 camp.nextstep.edu.missionutils.Console 의 readLine() 메서드를 이용해 사용자의 입력을 받을 수 있다.
  */
 public class InputView {
 
@@ -9,7 +21,16 @@ public class InputView {
      * 다리의 길이를 입력받는다.
      */
     public int readBridgeSize() {
-        return 0;
+        String input = Console.readLine();
+        List<Condition> conditions = ConditionGenerator.getBridgeSizeCondition();
+
+        Condition notPassCondition = Validator.getNotPassCondition(conditions, input);
+
+        if (notPassCondition == null) {
+            throw new IllegalArgumentException();
+        }
+
+        return Integer.parseInt(input);
     }
 
     /**
