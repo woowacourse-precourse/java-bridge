18c5e2b14af4de6e75ca8230c2e6fc33cfc8aae7
JUHA <khabh@naver.com>
2022-11-20 08:19:57 +0900

    test(BridgeTest): 이동할 칸이 이동 가능한지 확인하는 기능 테스트

--- src/test/java/bridge/BridgeTest.java
+++ src/test/java/bridge/BridgeTest.java
@@ -1,7 +1,10 @@
 package bridge;
 
 import bridge.controller.BridgeController;
+import bridge.dto.BridgeDto;
+import bridge.service.BridgeService;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
@@ -9,12 +12,14 @@ import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.OutputStream;
 import java.io.PrintStream;
+import java.util.List;
 import java.util.NoSuchElementException;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 class BridgeTest {
     private final BridgeController bridgeController = new BridgeController();
+    private final BridgeService bridgeService = new BridgeService();
 
     @DisplayName("다리 사이즈가 올바른 형식이 아닌 경우")
     @ValueSource(strings = {"a", "가", "1", "-1", "21"})
@@ -32,5 +37,13 @@ class BridgeTest {
         } catch (NoSuchElementException ignore) {}
 
         assertThat(out.toString()).contains(ERROR_MESSAGE);
+    }
+
+    @DisplayName("플레이어가 이동할 칸이 건널 수 있는 칸인지 확인")
+    @Test
+    void isBridgeBlockPassable() {
+        bridgeService.initBridge(BridgeDto.from(List.of("U", "D")));
+        assertThat(bridgeService.isPassable(0, "U")).isTrue();
+        assertThat(bridgeService.isPassable(1,"U")).isFalse();
     }
 }
