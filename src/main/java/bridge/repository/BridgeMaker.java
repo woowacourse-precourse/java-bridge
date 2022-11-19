5e1fcb010285d2a24c8faa8bd615e9c3e8d27268
sjk <80821744+ShinjaeKwon@users.noreply.github.com>
2022-11-20 01:50:19 +0900

    remove : 주석 제거

--- src/main/java/bridge/repository/BridgeMaker.java
+++ src/main/java/bridge/repository/BridgeMaker.java
@@ -5,35 +5,27 @@ import java.util.List;
 
 import bridge.BridgeNumberGenerator;
 
-/**
- * 다리의 길이를 입력 받아서 다리를 생성해주는 역할을 한다.
- */
 public class BridgeMaker {
 
-    private final BridgeNumberGenerator bridgeNumberGenerator;
+	private final BridgeNumberGenerator bridgeNumberGenerator;
 
-    public BridgeMaker(BridgeNumberGenerator bridgeNumberGenerator) {
-        this.bridgeNumberGenerator = bridgeNumberGenerator;
-    }
+	public BridgeMaker(BridgeNumberGenerator bridgeNumberGenerator) {
+		this.bridgeNumberGenerator = bridgeNumberGenerator;
+	}
 
-    /**
-     * @param size 다리의 길이
-     * @return 입력받은 길이에 해당하는 다리 모양. 위 칸이면 "U", 아래 칸이면 "D"로 표현해야 한다.
-     */
+	public List<String> makeBridge(int size) {
+		List<String> bridge = new ArrayList<>();
+		for (int i = 0; i < size; i++) {
+			bridge.add(putBridgeNumberToString(bridgeNumberGenerator.generate()));
+		}
+		return bridge;
+	}
 
-    public List<String> makeBridge(int size) {
-        List<String> bridge = new ArrayList<>();
-        for (int i = 0; i < size; i++) {
-            bridge.add(putBridgeNumberToString(bridgeNumberGenerator.generate()));
-        }
-        return bridge;
-    }
-
-    private String putBridgeNumberToString(int generate) {
-        if (generate == 1) {
-            return "U";
-        }
-        return "D";
-    }
+	private String putBridgeNumberToString(int generate) {
+		if (generate == 1) {
+			return "U";
+		}
+		return "D";
+	}
 
 }
