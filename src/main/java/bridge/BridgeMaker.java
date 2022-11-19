8f1869e6cb6d2081ee658f34bcdb3ac26c2594cd
Heosejin98 <sejin3319@naver.com>
2022-11-19 18:51:47 +0900

    Feat : 입력 길이만큼 다리 생성 기능 추가

--- src/main/java/bridge/BridgeMaker.java
+++ src/main/java/bridge/BridgeMaker.java
@@ -1,5 +1,6 @@
 package bridge;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -7,17 +8,29 @@ import java.util.List;
  */
 public class BridgeMaker {
 
-    private final BridgeNumberGenerator bridgeNumberGenerator;
+	private final static int UP_NUMBER = 1;
+	private final BridgeNumberGenerator bridgeNumberGenerator;
 
-    public BridgeMaker(BridgeNumberGenerator bridgeNumberGenerator) {
-        this.bridgeNumberGenerator = bridgeNumberGenerator;
-    }
+	public BridgeMaker(BridgeNumberGenerator bridgeNumberGenerator) {
+		this.bridgeNumberGenerator = bridgeNumberGenerator;
+	}
 
-    /**
-     * @param size 다리의 길이
-     * @return 입력받은 길이에 해당하는 다리 모양. 위 칸이면 "U", 아래 칸이면 "D"로 표현해야 한다.
-     */
-    public List<String> makeBridge(int size) {
-        return null;
-    }
+	/**
+	 * @param size 다리의 길이
+	 * @return 입력받은 길이에 해당하는 다리 모양. 위 칸이면 "U", 아래 칸이면 "D"로 표현해야 한다.
+	 */
+	public List<String> makeBridge(int size) {
+		List<String> bridge = new ArrayList<>();
+		for (int bridgeNumber = 0; bridgeNumber < size; bridgeNumber++) {
+			bridge.add(isUpAndDown(bridgeNumberGenerator.generate()));
+		}
+		return bridge;
+	}
+
+	private String isUpAndDown(int bridgeNumber) {
+		if (bridgeNumber == UP_NUMBER) {
+			return "D";
+		}
+		return "U";
+	}
 }
