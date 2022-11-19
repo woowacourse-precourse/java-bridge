c6ba92f20868156d8477c36d6c881da3ad37df4f
sosow0212 <sosow0212@naver.com>
2022-11-19 18:09:26 +0900

    feat : (BridgeGameException) 게임 재시작 혹은 종료 입력 예외 테스트

--- src/main/java/bridge/exception/BridgeGameException.java
+++ src/main/java/bridge/exception/BridgeGameException.java
@@ -6,6 +6,8 @@ public class BridgeGameException {
     private static final int MAX_BRIDGE_SIZE = 20;
     private static final String UP_DIRECTION = "U";
     private static final String DOWN_DIRECTION = "D";
+    private static final String RESTART_GAME = "R";
+    private static final String QUIT_GAME = "Q";
     private static final String ERROR = "[ERROR] ";
 
     public void validateBridgeSize(String input) {
@@ -14,20 +16,26 @@ public class BridgeGameException {
     }
 
     public void validateDirection(String input) {
-        if(!(input.equals(UP_DIRECTION) || input.equals(DOWN_DIRECTION))) {
+        if (!(input.equals(UP_DIRECTION) || input.equals(DOWN_DIRECTION))) {
             throw new IllegalArgumentException(ERROR + "이동할 칸은 U 혹은 D만 가능합니다.");
         }
     }
 
+    public void validateGameStatusCommand(String input) {
+        if (!(input.equals(RESTART_GAME) || input.equals(QUIT_GAME))) {
+            throw new IllegalArgumentException(ERROR + "게임 재시작은 R, 종료는 Q를 입력해주세요.");
+        }
+    }
+
     public void validateBridgeSizeOnlyContainsNumber(String input) {
-        if(!input.matches(ONLY_CONTAINS_NUMBER_REGEX)) {
+        if (!input.matches(ONLY_CONTAINS_NUMBER_REGEX)) {
             throw new IllegalArgumentException(ERROR + "다리의 길이는 숫자만 입력이 가능합니다.");
         }
     }
 
     public void validateBridgeSizePermittedLength(String input) {
         int bridgeSize = Integer.parseInt(input);
-        if(!(bridgeSize >= MIN_BRIDGE_SIZE && bridgeSize <= MAX_BRIDGE_SIZE)) {
+        if (!(bridgeSize >= MIN_BRIDGE_SIZE && bridgeSize <= MAX_BRIDGE_SIZE)) {
             throw new IllegalArgumentException(ERROR + "다리의 길이는 3~20 사이의 수입니다.");
         }
     }
