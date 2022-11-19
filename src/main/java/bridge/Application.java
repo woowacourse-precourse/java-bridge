1ae10e121e6f0f9da0289ca80b75c549cbb1c15b
RumbleKAT <reki318@naver.com>
2022-11-19 11:42:46 +0900

    (feat) 2. 출력 객체 구현 1) 현재까지 이동한 다리의 상태 출력 2) 게임의 최종 결과 출력
    stash commit

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -1,8 +1,30 @@
 package bridge;
 
+import java.util.List;
+
 public class Application {
 
+    private static InputView inputView;
+    private static BridgeGame bridgeGame;
+    private static BridgeMaker bridgeMaker;
+    private static OutputView outputView;
+
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        init();
+
+    }
+
+    private static void init(){
+        inputView = new InputView();
+        outputView = new OutputView();
+        bridgeMaker = new BridgeMaker(new BridgeRandomNumberGenerator());
+        bridgeGame = new BridgeGame(new BridgeRandomNumberGenerator());
+
+        bridgeGame.setTryCnt(1);
+        outputView.printInit();
+        int size = inputView.readBridgeSize();
+        bridgeGame.setBridge(bridgeMaker.makeBridge(size));
+
+        String command = inputView.readGameCommand();
     }
 }
