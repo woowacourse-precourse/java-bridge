5e060030e8c6919ba92f912799adeb73d0f57580
seonghoo1217 <seonghoo1217@naver.com>
2022-11-19 11:54:26 +0900

    refactor(InputValidationTest) : modify test case logic
    * Why modify?
    - this logic must be to get Error
    - So, I modify test case used to try / catch
    
    Refactor(commit_id) : #759751c

--- src/test/java/bridge/validation/InputValidationTest.java
+++ src/test/java/bridge/validation/InputValidationTest.java
@@ -1,29 +1,40 @@
 package bridge.validation;
 
+import bridge.InputView;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
+import validation.InputValidation;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class InputValidationTest {
 
 	private final String error_message= "[ERROR]";
 
-	private InputValidation iv;
+	private static InputValidation iv;
+
+	private static InputView inputView;
 
 	@BeforeAll
 	static void initailize(){
-		iv=new InputValidationTest();
+		iv=new InputValidation();
+		inputView=new InputView();
 	}
 
 	@ParameterizedTest
 	@ValueSource(strings = {"1","-23","한글입니다","It's English","21000000000","21"})
 	@DisplayName("유저의_다리길이_입력값_유효성검사")
 	void user_input_bridge_length_validation(String source){
-		assertThatThrownBy(()->iv.user_input_bridge_length_validation(source))
-				.isInstanceOf(IllegalArgumentException.class)
-				.hasMessageContaining(error_message);
+		try{
+			assertThatThrownBy(()-> {
+				String bridge_size =iv.userInputBridgeSizeValidation(source);
+				if(bridge_size.equals("[ERROR]")) throw new IllegalArgumentException("[ERROR] 다리 길이는 3부터 20 사이의 숫자여야 합니다.");
+			});
+		}catch (IllegalArgumentException e){
+			assertThat(e.getMessage()).isEqualTo("[ERROR] 다리 길이는 3부터 20 사이의 숫자여야 합니다.");
+		}
 	}
 }
