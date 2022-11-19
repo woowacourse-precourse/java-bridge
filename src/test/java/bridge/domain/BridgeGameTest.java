0d1ebab8a816055ccf7c70b03e0811fb6ab508da
jiwon1217 <koagjw@gmail.com>
2022-11-20 03:03:29 +0900

    test: BridgeGameTest 테스트 메서드 추가

--- src/test/java/bridge/domain/BridgeGameTest.java
+++ src/test/java/bridge/domain/BridgeGameTest.java
@@ -1,12 +1,13 @@
 package bridge.domain;
 
-import bridge.exception.InputException;
 import java.util.List;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
+import static bridge.exception.InputException.*;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
@@ -16,7 +17,7 @@ public class BridgeGameTest {
     @ParameterizedTest
     @ValueSource(strings = {"1", "u", "d"})
     void readMoving(String direction) {
-        assertThatThrownBy(() -> InputException.isValidMovementDirection(direction))
+        assertThatThrownBy(() -> isValidMovementDirection(direction))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
@@ -26,5 +27,25 @@ public class BridgeGameTest {
     void judgeMovementDirection(int index, String direction) {
         Bridge bridge = new Bridge(List.of("U", "U", "D"));
         assertThat(bridge.isPossibleMove(index, direction)).isTrue();
+    }
+
+    @DisplayName("재시작 및 종료에 대한 입력이 R 또는 Q가 아닐 경우 예외가 발생한다.")
+    @ParameterizedTest
+    @ValueSource(strings = {"r", "q", "1", "RR"})
+    void readGameCommand(String command) {
+        assertThatThrownBy(() -> isValidRestartOrQuitCommand(command))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("재시작할 경우 처음에 만든 다리를 재사용한다.")
+    @Test
+    void reuseBridge() {
+        Bridge originBridge = new Bridge(List.of("U", "U", "D"));
+        BridgeGame bridgeGame = new BridgeGame(originBridge);
+
+        bridgeGame.retry(originBridge);
+        Bridge retryBridge = bridgeGame.getBridge();
+
+        assertThat(originBridge.getBridge()).isEqualTo(retryBridge.getBridge());
     }
 }
