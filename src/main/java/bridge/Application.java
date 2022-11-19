092dd46f75e37fd3ce556d3e0a6ebb1de68c2be9
Sinryuji <siler2474@gmail.com>
2022-11-19 16:21:21 +0900

    feat(Application): 기능 구현 완ㄹ

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -1,8 +1,27 @@
 package bridge;
 
+import java.awt.font.OpenType;
+
 public class Application {
+    private static final InputView inputView = new InputView();
+    private static final BridgeMaker bridgeMaker = new BridgeMaker(new BridgeRandomNumberGenerator());
 
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        int size = inputView.readBridgeSize();
+        Bridge bridge = new Bridge(bridgeMaker.makeBridge(size), size);
+        OutputView outputView = new OutputView(bridge);
+        BridgeGame bridgeGame = new BridgeGame(bridge);
+        playGame(bridge, bridgeGame, outputView);
+        outputView.printResult();
+    }
+
+    private static void playGame(Bridge bridge, BridgeGame bridgeGame, OutputView outputView) {
+        while (!bridge.getGameResult()) {
+            bridgeGame.move(inputView.readMoving());
+            outputView.printMap();
+            if (!bridge.getCorrect() && !bridgeGame.retry(inputView.readGameCommand())) {
+                break;
+            }
+        }
     }
 }
