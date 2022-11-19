af551062b4c4dbe65d390d71f23070953898075a
HyeonJun0530 <nexus2697@pukyong.ac.kr>
2022-11-19 20:31:22 +0900

    fix(BridgeService): 다시 시작이 되지 않던 버그 수정

--- src/main/java/bridge/service/BridgeService.java
+++ src/main/java/bridge/service/BridgeService.java
@@ -66,7 +66,7 @@ public class BridgeService {
     }
 
     public boolean retryJudge(String inputRetry) {
-        if(inputRetry.equals(RESTART_MSG)){
+        if(inputRetry.equals(RESTART_MSG.getMsg())){
             cleanSide();
             return true;
         }
