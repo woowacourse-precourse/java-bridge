4326534d7a55a993b0a1c1ec6fe00ac332cc7183
Devtist <yt930310@snu.ac.kr>
2022-11-19 22:55:08 +0900

    feat: 전체 프로그램에서 예외 메세지를 출력하는 기능 구현

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -1,8 +1,16 @@
 package bridge;
 
-public class Application {
+import controller.GameController;
 
+public class Application {
+    //TODO 리드미 확인할 것
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        try {
+            GameController gameController = new GameController();
+            gameController.control();
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+        }
     }
 }
