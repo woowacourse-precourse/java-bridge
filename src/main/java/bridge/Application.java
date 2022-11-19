4fdf70b2fdb1cae37e38061b1d41fb839976665b
Nitroblack1 <109022435+Nitroblack1@users.noreply.github.com>
2022-11-19 18:45:52 +0900

    feat(Application) : implement game triggering system

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -5,6 +5,10 @@ import bridge.Domain.BridgeGame;
 public class Application {
 
     public static void main(String[] args) {
-        BridgeGame game = new BridgeGame();
+        try {
+            BridgeGame game = new BridgeGame();
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+        }
     }
 }
