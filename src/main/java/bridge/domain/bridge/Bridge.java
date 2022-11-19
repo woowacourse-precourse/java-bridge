b2d487c3e5004b57b03eee4ea987d733a15e0279
chanminmaxscaler <jcm04@naver.com>
2022-11-19 16:00:55 +0900

    feat: Bridge 클래스에 사용자 입력 방향과 다리의 방향을 비교하는 기능을 구현한다
    사용자 입력 방향과 다리의 방향을 비교하는 기능을 hasElementOf 메소드로 구현한다
    hasElementOf 메소드는 Direction 객체와 위치를 입력받는다.
    이후 멤버변수 bridge 리스트에서 입력된 위치의 Direction과 입력된 Direction 객체를 비교해 반환한다.
    hasElementOf 메소드는 위치 입력값을 validateIndexValue 메소드로 검증한다.
    validateIndexValue 메소드는 위치 입력값이 bridge 리스트의 길이를 벗어나면 IndexOutOfBoundsException을 발생시킨다.

--- src/main/java/bridge/domain/bridge/Bridge.java
+++ src/main/java/bridge/domain/bridge/Bridge.java
@@ -11,4 +11,15 @@ public class Bridge {
     public Bridge(List<Direction> bridge) {
         this.bridge = bridge;
     }
+
+    public boolean hasElementOf(Direction direction, int location) {
+        validateIndexValue(location);
+        return direction.equals(bridge.get(location));
+    }
+
+    private void validateIndexValue(int location) {
+        if (location >= bridge.size() || location < 0) {
+            throw new IndexOutOfBoundsException("[ERROR] 다리의 길이를 벗어난 위치 값이 입력되었습니다.");
+        }
+    }
 }
--- src/test/java/bridge/domain/factory/BridgeFactoryTest.java
+++ src/test/java/bridge/domain/factory/BridgeFactoryTest.java
@@ -6,9 +6,6 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
-import java.util.List;
-import java.util.function.Predicate;
-
 import static org.assertj.core.api.Assertions.assertThat;
 
 class BridgeFactoryTest {
