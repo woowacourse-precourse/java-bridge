2b11b2872a3f640f2b8c1acd3bd06c70c358e668
JunTaeINC <juntea3523@naver.com>
2022-11-20 01:19:28 +0900

    feat : (Sentence) 문자 포맷팅 / returnSuccessOrFailure 기능 구현

--- src/main/java/bridge/enums/Sentence.java
+++ src/main/java/bridge/enums/Sentence.java
@@ -7,19 +7,26 @@ public enum Sentence {
     MOVE("이동할 칸을 선택해주세요. (위: U, 아래: D)"),
     RESTART_OF_END("게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)"),
     RESULT("최종 게임 결과"),
-    SUCCESS_OR_FAILURE("게임 성공 여부:"),
+    SUCCESS_OR_FAILURE("게임 성공 여부: %s"),
     SUCCESS("성공"),
     FAILURE("실패"),
-    TRY_COUNT("총 시도한 횟수:");
+    TRY_COUNT("총 시도한 횟수: %d");
 
 
     private final String value;
 
-    Sentence(String value){
+    Sentence(String value) {
         this.value = value;
     }
 
     public String getValue() {
         return value;
+    }
+
+    public static String returnSuccessOrFailure(boolean trueOrFalse) {
+        if (!trueOrFalse) {
+            return FAILURE.getValue();
+        }
+        return SUCCESS.getValue();
     }
 }
