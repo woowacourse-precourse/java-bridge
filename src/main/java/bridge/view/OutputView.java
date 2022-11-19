f5af663440996d7503bf2f9c7e88ab012dfc5e30
hyowonhan <wain1719@gmail.com>
2022-11-19 16:30:33 +0900

    feat: 진행사항 출력 기능 구현

--- src/main/java/bridge/view/OutputView.java
+++ src/main/java/bridge/view/OutputView.java
@@ -1,19 +1,34 @@
 package bridge.view;
 
 import bridge.domain.BridgeGame;
+
+import java.util.List;
+
 /**
  * 사용자에게 게임 진행 상황과 결과를 출력하는 역할을 한다.
  */
 public class OutputView {
-    BridgeGame bridgeGame = new BridgeGame();
     /**
      * 현재까지 이동한 다리의 상태를 정해진 형식에 맞춰 출력한다.
      * <p>
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
+    private BridgeGame bridgeGame = new BridgeGame();
     public void printMap() {
-        System.out.println(bridgeGame.upBridge);
-        System.out.println(bridgeGame.downBridge);
+        List<String> correctBridge = bridgeGame.makeCorrectBridge(InputView.readBridgeSize());
+        OutputView outputView = new OutputView();
+        printUpDown(correctBridge);
+    }
+
+    private void printUpDown(List<String> correctBridge) {
+        for(int i=0; i<correctBridge.size(); i++) {
+            String playerMove = InputView.readMoving();
+            bridgeGame.move(correctBridge.get(i), playerMove);
+            String strUp =  String.join("|", bridgeGame.getUpBridge());
+            String strDown =  String.join("|", bridgeGame.getDownBridge());
+            System.out.println("["+strUp+"]");
+            System.out.println("["+strDown+"]");
+        }
     }
 
     /**
