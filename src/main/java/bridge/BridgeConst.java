3e6e782bd6f9ce18f9281aa808d2fdacf479126a
impati <impati0716@icloud.com>
2022-11-19 17:11:43 +0900

    feat (BridgeConst) : RETRY , QUIT 상수 정의

--- src/main/java/bridge/BridgeConst.java
+++ src/main/java/bridge/BridgeConst.java
@@ -3,4 +3,7 @@ package bridge;
 public abstract class BridgeConst {
     public static final String UP = "U";
     public static final String DOWN = "D";
+    public static final String RETRY = "R";
+    public static final String QUIT = "Q";
+
 }
