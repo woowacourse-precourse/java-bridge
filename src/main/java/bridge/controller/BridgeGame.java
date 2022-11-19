f0aff0cc6e35fc72da1c3e6ef8469746ac43e767
recicoco068 <gkapf838@gmail.com>
2022-11-19 11:47:40 +0900

    feat(BridgeGame.java) : 게임 중 게임 재시작 기능 구현

--- src/main/java/bridge/controller/BridgeGame.java
+++ src/main/java/bridge/controller/BridgeGame.java
@@ -1,6 +1,7 @@
 package bridge.controller;
 
 import bridge.domain.Bridge;
+import bridge.domain.Command;
 import bridge.domain.UserState;
 import bridge.view.InputView;
 import bridge.view.OutputView;
@@ -9,6 +10,7 @@ import bridge.view.OutputView;
  * 다리 건너기 게임을 관리하는 클래스
  */
 public class BridgeGame {
+
     private static final InputView inputView = new InputView();
     private static final OutputView outputView = new OutputView();
 
@@ -24,11 +26,15 @@ public class BridgeGame {
         }
     }
 
-    /**
-     * 사용자가 게임을 다시 시도할 때 사용하는 메서드
-     * <p>
-     * 재시작을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
-     */
-    public void retry() {
+    public void retry(UserState userState, Bridge bridge) {
+        while (!userState.isQuit()) {
+            userState.resetMove();
+            userState.tryCountIncrease();
+            move(userState, bridge);
+            if (userState.isSuccessOrFail(bridge.getSize())) {
+                return;
+            }
+            userState.quitChange(inputView.readGameCommand().equals(Command.QUIT.getCommand()));
+        }
     }
 }
