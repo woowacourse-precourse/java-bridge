5801d1c126350d2a8d8b18ec1d84e06d4bfb3848
gmu1026 <gmu1026@gmail.com>
2022-11-19 15:49:20 +0900

    feat: 다리 생성을 위한 사용자 입력 기능 구현

--- src/main/java/bridge/InputView.java
+++ src/main/java/bridge/InputView.java
@@ -1,15 +1,43 @@
 package bridge;
 
+import camp.nextstep.edu.missionutils.Console;
+
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
  */
 public class InputView {
+    private static final String INPUT_BRIDGE_SIZE_MESSAGE = "다리의 길이를 입력해주세요.";
 
+    private static final int BRIDGE_MIN_RANGE = 3;
+    private static final int BRIDGE_MAX_RANGE = 20;
     /**
      * 다리의 길이를 입력받는다.
      */
     public int readBridgeSize() {
-        return 0;
+        System.out.println(INPUT_BRIDGE_SIZE_MESSAGE);
+        String inputBridgeSize = Console.readLine();
+        validateInputBridgeSize(inputBridgeSize);
+
+        return Integer.parseInt(inputBridgeSize);
+    }
+
+    private void validateInputBridgeSize(String inputBridgeSize) {
+        validateNumberFormat(inputBridgeSize);
+        validateRange(Integer.parseInt(inputBridgeSize));
+    }
+
+    private void validateNumberFormat(String inputValue) {
+        try {
+            Integer.valueOf(inputValue);
+        } catch (NumberFormatException exception) {
+            throw new IllegalArgumentException("[ERROR] 숫자만 입력해주세요.");
+        }
+    }
+
+    private void validateRange(int bridgeSize) {
+        if (bridgeSize < BRIDGE_MIN_RANGE || bridgeSize > BRIDGE_MAX_RANGE) {
+            throw new IllegalArgumentException("[ERROR] 다리 길이는 3부터 20 사이의 숫자여야 합니다.");
+        }
     }
 
     /**
