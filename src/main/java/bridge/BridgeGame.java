78c892c54362d3dd9bf8da1b84e0be89143525e9
Sangwoo Shin <sangwoonoel@naver.com>
2022-11-20 00:17:37 +0900

    feat(BridgeGame): 갈 수 없는 길로 가려고 했는지 확인해주는 기능 구현

--- src/main/java/bridge/BridgeGame.java
+++ src/main/java/bridge/BridgeGame.java
@@ -12,6 +12,8 @@ import static bridge.Result.*;
 public class BridgeGame {
 
     private final List<String> bridge;
+    private boolean isWrongApproach = false;
+    private int tryCount = 1;
 
     public BridgeGame(List<String> bridge) {
         this.bridge = bridge;
@@ -29,6 +31,7 @@ public class BridgeGame {
             }
             return DOWN_SUCCESS;
         }
+        isWrongApproach = true;
         if (bridge.get(index) == UP.getMoving()) {
             return UP_FAIL;
         }
@@ -41,5 +44,10 @@ public class BridgeGame {
      * 재시작을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
     public void retry() {
+
+    }
+
+    public boolean isWrongApproach() {
+        return isWrongApproach;
     }
 }
