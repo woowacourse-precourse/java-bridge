33128f3996719a8c711a527af40ff7431cbbab4e
June <68291395+june-777@users.noreply.github.com>
2022-11-19 21:21:19 +0900

    refactor(OutputView): 게임 상태 문자열 갱신 기능들 메소드화

--- src/main/java/view/OutputView.java
+++ src/main/java/view/OutputView.java
@@ -1,12 +1,11 @@
 package view;
 
-import static view.OutputViewConstants.*;
-
-import static view.InputViewConstants.MOVING_UP;
-import static view.InputViewConstants.MOVING_DOWN;
-
 import static model.BridgeGameConstants.MOVING_SUCCESS;
 import static model.BridgeGameConstants.MOVING_FAIL_WRONG_MOVING;
+import static model.BridgeGameConstants.MOVING_UP;
+import static model.BridgeGameConstants.MOVING_DOWN;
+
+import static view.OutputViewConstants.*;
 
 /**
  * 사용자에게 게임 진행 상황과 결과를 출력하는 역할을 한다.
@@ -22,11 +21,10 @@ public class OutputView {
 	 * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
 	 */
 	public void printMap(String moving, int movingResult) {
-		updateGameStateMapVerticalBar();
-		updateGameStateMapWhenMovingUp(moving, movingResult);
-		updateGameStateMapWhenMovingDown(moving, movingResult);
-		updateGameStateMapCloseSquareBrackets();
+		updateGameStateMap(moving, movingResult);
 		String currentGameStateMap = combineGameStateUpMapAndDownMap();
+		System.out.println(currentGameStateMap);
+		System.out.println();
 	}
 
 	private String combineGameStateUpMapAndDownMap() {
@@ -36,13 +34,20 @@ public class OutputView {
 		return currentGameStateMap;
 	}
 
+	private void updateGameStateMap(String moving, int movingResult) {
+		updateGameStateMapVerticalBar();
+		updateGameStateMapWhenMovingUp(moving, movingResult);
+		updateGameStateMapWhenMovingDown(moving, movingResult);
+		updateGameStateMapCloseSquareBrackets();
+	}
+
 	private void updateGameStateMapVerticalBar() {
 		currentGameStateUpMap = currentGameStateUpMap.replace(OUTPUT_TEXT_CLOSE_SQUARE_BRACKETS,
 				OUTPUT_TEXT_VERTICAL_BAR);
 		currentGameStarteDownMap = currentGameStarteDownMap.replace(OUTPUT_TEXT_CLOSE_SQUARE_BRACKETS,
 				OUTPUT_TEXT_VERTICAL_BAR);
 	}
-	
+
 	private void updateGameStateMapWhenMovingUp(String moving, int movingResult) {
 		if (moving.equals(MOVING_UP) && movingResult == MOVING_SUCCESS) {
 			currentGameStateUpMap += OUTPUT_TEXT_MOVING_SUCCESS;
@@ -53,7 +58,7 @@ public class OutputView {
 			currentGameStarteDownMap += (OUTPUT_TEXT_DO_NOT_GO);
 		}
 	}
-	
+
 	private void updateGameStateMapWhenMovingDown(String moving, int movingResult) {
 		if (moving.equals(MOVING_DOWN) && movingResult == MOVING_SUCCESS) {
 			currentGameStateUpMap += OUTPUT_TEXT_DO_NOT_GO;
@@ -64,17 +69,17 @@ public class OutputView {
 			currentGameStarteDownMap += OUTPUT_TEXT_MOVING_FAIL;
 		}
 	}
-	
-	public void clearGameStateMap() {
-		currentGameStateUpMap = OUTPUT_TEXT_OPEN_SQUARE_BRACKETS;
-		currentGameStarteDownMap = OUTPUT_TEXT_OPEN_SQUARE_BRACKETS;
-	}
-	
+
 	private void updateGameStateMapCloseSquareBrackets() {
 		currentGameStateUpMap += OUTPUT_TEXT_CLOSE_SQUARE_BRACKETS;
 		currentGameStarteDownMap += OUTPUT_TEXT_CLOSE_SQUARE_BRACKETS;
 	}
-	
+
+	public void clearGameStateMap() {
+		currentGameStateUpMap = OUTPUT_TEXT_OPEN_SQUARE_BRACKETS;
+		currentGameStarteDownMap = OUTPUT_TEXT_OPEN_SQUARE_BRACKETS;
+	}
+
 	/**
 	 * 게임의 최종 결과를 정해진 형식에 맞춰 출력한다.
 	 * <p>
