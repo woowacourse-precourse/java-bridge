ac4c3c882fd2720feeac66f60324e9c48ba0ed84
lcw3176 <lcw3176@naver.com>
2022-11-20 01:04:59 +0900

    chore(InputMessage, OutputMessage): 생성자 위치 변경

--- src/main/java/bridge/view/input/InputMessage.java
+++ src/main/java/bridge/view/input/InputMessage.java
@@ -2,11 +2,12 @@ package bridge.view.input;
 
 public class InputMessage {
 
-    private InputMessage() {
-
-    }
 
     public static final String READ_BRIDGE_SIZE = "\n다리의 길이를 입력해주세요.";
     public static final String READ_MOVING_DIRECTION = "\n이동할 칸을 선택해주세요. (위: U, 아래: D)";
     public static final String READ_RETRY_OR_QUIT = "\n게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)";
+
+    private InputMessage() {
+
+    }
 }
--- src/main/java/bridge/view/output/OutputMessage.java
+++ src/main/java/bridge/view/output/OutputMessage.java
@@ -2,10 +2,11 @@ package bridge.view.output;
 
 public class OutputMessage {
 
+
+    public static final String WELCOME_MESSAGE = "다리 건너기 게임을 시작합니다.";
+
     private OutputMessage() {
 
     }
-    
-    public static final String WELCOME_MESSAGE = "다리 건너기 게임을 시작합니다.";
 
 }
