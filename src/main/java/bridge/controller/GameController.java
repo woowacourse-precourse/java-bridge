066446ba25fe1e50c01621f541520ee750f9649c
김현재 <41482946+galug@users.noreply.github.com>
2022-11-20 01:58:20 +0900

    feat(gamecontroller): 다리 게임을 생성하는 기능 추가

---
+++ src/main/java/bridge/controller/GameController.java
@@ -0,0 +1,20 @@
+package bridge.controller;
+
+import bridge.BridgeMaker;
+import bridge.BridgeRandomNumberGenerator;
+import bridge.model.Bridge;
+import bridge.model.BridgeGame;
+import bridge.model.PassingSpace;
+import bridge.view.InputView;
+import bridge.view.OutputView;
+
+public class GameController {
+    private final InputView inputView = new InputView();
+    private final OutputView outputView = new OutputView();
+    private final BridgeMaker bridgeMaker = new BridgeMaker(new BridgeRandomNumberGenerator());
+
+    public BridgeGame createBridgeGame(Bridge bridge) {
+        PassingSpace passingSpace = new PassingSpace();
+        return new BridgeGame(bridge, passingSpace);
+    }
+}
