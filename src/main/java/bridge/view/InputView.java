a9770cea50ffdb335bf645b82bd37dd0b69b1418
youngkyunIm <yee0230@naver.com>
2022-11-19 21:29:46 +0900

    feat(view): add input,output

--- src/main/java/bridge/view/InputView.java
+++ src/main/java/bridge/view/InputView.java
@@ -9,29 +9,29 @@ public class InputView {
 
     final InputHandler inputHandler = new InputHandler();
 
-    public void printStartBridgeGame() {
-        System.out.println("다리 건너기 게임을 시작합니다.\n");
-    }
-
     /**
      * 다리의 길이를 입력받는다.
      */
     public int readBridgeSize() {
         System.out.println("다리의 길이를 입력해주세요.");
-        return inputHandler.stringToInt(Console.readLine());
+        int bridgeSize = inputHandler.stringToInt(Console.readLine());
+        System.out.println();
+        return bridgeSize;
     }
 
     /**
      * 사용자가 이동할 칸을 입력받는다.
      */
     public String readMoving() {
-        return null;
+        System.out.println("이동할 칸을 선택해주세요. (위: U, 아래: D)");
+        return Console.readLine();
     }
 
     /**
      * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
      */
     public String readGameCommand() {
-        return null;
+        System.out.println("게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)");
+        return Console.readLine();
     }
 }
--- src/main/java/bridge/view/OutputView.java
+++ src/main/java/bridge/view/OutputView.java
@@ -1,16 +1,39 @@
 package bridge.view;
 
+import bridge.domain.BridgeGame;
+import bridge.domain.vo.PlayerMap;
+import java.util.List;
+
 /**
  * 사용자에게 게임 진행 상황과 결과를 출력하는 역할을 한다.
  */
 public class OutputView {
+
+    public void printStartBridgeGame() {
+        System.out.println("다리 건너기 게임을 시작합니다.\n");
+    }
 
     /**
      * 현재까지 이동한 다리의 상태를 정해진 형식에 맞춰 출력한다.
      * <p>
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void printMap() {
+    public void printMap(PlayerMap playerMap) {
+        for (List<String> playerMapLine : playerMap.getPlayerMap()) {
+            System.out.print("[ ");
+            printMiddleMap(playerMapLine);
+            System.out.println(" ]");
+        }
+        System.out.println();
+    }
+
+    private void printMiddleMap(List<String> playerMap) {
+        for (int index = 0; index < playerMap.size(); index++) {
+            System.out.print(playerMap.get(index));
+            if (index != playerMap.size() - 1) {
+                System.out.print(" | ");
+            }
+        }
     }
 
     /**
@@ -18,6 +41,10 @@ public class OutputView {
      * <p>
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void printResult() {
+    public void printResult(PlayerMap playerMap, BridgeGame bridgeGame) {
+        System.out.println("최종 게임 결과");
+        printMap(playerMap);
+        System.out.println("게임 성공 여부: "+bridgeGame.getStatus());
+        System.out.println("총 시도한 횟수: "+bridgeGame.getCount());
     }
 }
