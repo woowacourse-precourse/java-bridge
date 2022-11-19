f885b918870c019513c52d04622f28b31a74af26
saint6839 <saint6839@gmail.com>
2022-11-20 00:08:11 +0900

    feat(view) : 상수 변수 추가, 게임 시작 메시지 출력 메서드 추가

--- src/main/java/bridge/view/OutputView.java
+++ src/main/java/bridge/view/OutputView.java
@@ -9,6 +9,13 @@ import java.util.List;
  * 사용자에게 게임 진행 상황과 결과를 출력하는 역할을 한다.
  */
 public class OutputView {
+    private static final String GAME_START_MESSAGE = "다리 건너기 게임을 시작합니다.";
+    private static final String INPUT_BRIDGE_SIZE_MESSAGE = "\n다리의 길이를 입력해주세요.";
+    private static final String INPUT_MOVE_DIRECTION_MESSAGE = "\n이동할 칸을 선택해주세요. (위: U, 아래: D)";
+    private static final String INPUT_GAME_RETRY_MESSAGE = "\n게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)";
+    private static final String GAME_RESULT_MESSAGE = "최종 게임 결과";
+    private static final String GAME_IS_SUCCEED_MESSAGE = "\n게임 성공 여부: ";
+    private static final String TOTAL_TRY_COUNT = "총 시도한 횟수: ";
     /**
      * 현재까지 이동한 다리의 상태를 정해진 형식에 맞춰 출력한다.
      * <p>
@@ -77,6 +84,10 @@ public class OutputView {
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
     public void printResult() {
+    }
+
+    public void printGameStart() {
+        System.out.println(GAME_START_MESSAGE);
     }
 
     private void printOpenSquareBracket(StringBuilder sb) {
