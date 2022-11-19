438e1b3b46085b6ab16a2c813d1174b3b56ff559
김현재 <41482946+galug@users.noreply.github.com>
2022-11-19 16:00:15 +0900

    feat(movable): 이동과 관련된 자료를 모은 enum 타입 추가

---
+++ src/main/java/bridge/model/Movable.java
@@ -0,0 +1,21 @@
+package bridge.model;
+
+public enum Movable {
+    O("O", true), X("X", false), BLANK(" ", false);
+
+    private final String mark;
+    private final boolean canMovable;
+
+    Movable(String mark, boolean canMovable) {
+        this.mark = mark;
+        this.canMovable = canMovable;
+    }
+
+    public String getMark() {
+        return mark;
+    }
+
+    public boolean isMovable() {
+        return canMovable;
+    }
+}
