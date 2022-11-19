401cfa6b48226ba14a440f4e52b2c8f9371f2ee0
ostar11 <cjw5877@gmail.com>
2022-11-19 22:31:59 +0900

    feat(BridgeMaker): makeBridge 구현

---
+++ src/test/java/bridge/BridgeMakerTest.java
@@ -0,0 +1,16 @@
+package bridge;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class BridgeMakerTest {
+
+    @Test
+    void 다리_만들기_테스트() {
+        BridgeRandomNumberGenerator bridgeRandomNumberGenerator = new BridgeRandomNumberGenerator();
+        BridgeMaker bridgeMaker = new BridgeMaker(bridgeRandomNumberGenerator);
+
+    }
+}
