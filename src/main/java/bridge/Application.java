bbbf17398cf5eb10116c64714a28c4407655babb
CMHyuk <alsgur9710@naver.com>
2022-11-19 20:35:31 +0900

    feat(Application): 다리 건너기 실행

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -1,8 +1,18 @@
 package bridge;
 
+import bridge.config.ClassConfig;
+
 public class Application {
+
+    private static final String ERROR_MESSAGE = "[ERROR] ";
 
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        ClassConfig classConfig = new ClassConfig();
+        try {
+            classConfig.bridgeController().run();
+        } catch (IllegalArgumentException e) {
+            System.out.println((ERROR_MESSAGE + e.getMessage()));
+        }
     }
 }
