35bc06dddeb061c3039524ec1171b430a04f122c
baeseohyeon <formnarago@naver.com>
2022-11-19 21:49:45 +0900

    feat(BridgeGame): Bridge, Player 클래스 필드 추가

--- src/main/java/bridge/model/BridgeGame.java
+++ src/main/java/bridge/model/BridgeGame.java
@@ -1,9 +1,19 @@
 package bridge.model;
 
+import java.util.List;
+
 /**
  * 다리 건너기 게임을 관리하는 클래스
  */
 public class BridgeGame {
+
+    private final Bridge bridge;
+    private Player player;
+
+    public BridgeGame(List<String> bridge) {
+        this.player = new Player();
+        this.bridge = new Bridge(bridge);
+    }
 
     /**
      * 사용자가 칸을 이동할 때 사용하는 메서드
