e467334a5a859e4db5dd2956c51efb32d78b9ce9
Donguk <95donguk.dev@gmail.com>
2022-11-19 23:09:18 +0900

    feat: 다리 명령 입력 시 빈 값인 경우 예외 처리 기능 구현

--- src/main/java/bridge/validator/MovingValidator.java
+++ src/main/java/bridge/validator/MovingValidator.java
@@ -2,6 +2,7 @@ package bridge.validator;
 
 import static bridge.utils.command.MoveCommand.DOWN;
 import static bridge.utils.command.MoveCommand.UP;
+import static bridge.utils.message.ErrorMessage.BLANK_VALUE;
 import static bridge.utils.message.ErrorMessage.INVALID_MOVING;
 
 import java.util.Objects;
@@ -10,6 +11,7 @@ public class MovingValidator implements Validator {
     @Override
     public void validate(String inputValue) {
         checkInvalidMoving(inputValue);
+        checkBlankValue(inputValue);
     }
 
     private void checkInvalidMoving(String inputValue) {
@@ -21,5 +23,12 @@ public class MovingValidator implements Validator {
 
     private boolean isValidMoving(String inputValue) {
         return Objects.equals(inputValue, UP.getCommand()) || Objects.equals(inputValue, DOWN.getCommand());
+    }
+
+    private void checkBlankValue(String inputValue) {
+        if (inputValue.isBlank()) {
+            System.out.println(BLANK_VALUE.getMessage());
+            throw new IllegalArgumentException();
+        }
     }
 }
