5fdf969cacb0559ce983e646d80d915f42dc9271
alstjr706 <alstjr706@naver.com>
2022-11-19 15:37:48 +0900

    refactor : (다리 건너기) 메인 로직 컨벤션에 맞게 리팩토링 (중간 커밋)

--- src/main/java/bridge/domain/BridgeGameMachine.java
+++ src/main/java/bridge/domain/BridgeGameMachine.java
@@ -2,175 +2,60 @@ package bridge.domain;
 
 import bridge.BridgeMaker;
 import bridge.BridgeRandomNumberGenerator;
+import bridge.constants.Command;
 import bridge.input.InputView;
 import bridge.output.OutputView;
+import java.util.HashMap;
 import java.util.List;
 
 public class BridgeGameMachine {
 
     public void run() {
         InputView inputView = new InputView();
+        // 총길이
         int bridgeLength = inputView.readBridgeSize();
 
         BridgeRandomNumberGenerator randomGenerator = new BridgeRandomNumberGenerator();
         BridgeMaker bridgeMaker = new BridgeMaker(randomGenerator);
+        // [U, D, U]
         List<String> designBridge = bridgeMaker.makeBridge(bridgeLength);
-        BridgeGame bridgeGame = new BridgeGame();
-        OutputView outputView = new OutputView();
 
-        boolean gameSuccess = true;
-        int gameCount = 0;
-        String restartVariable = "";
+        HashMap<String, StringBuilder> bridgeState = new HashMap<>();
 
-        Loop1:
-        while (!(restartVariable.equals("R"))) {
+        bridgeState.put(Command.UP.getCommand(), new StringBuilder("[ "));
+        bridgeState.put(Command.DOWN.getCommand(), new StringBuilder("[ "));
 
-            String str1 = "[ ";
-            String str2 = "[ ";
+        PlayerAndRandom playerAndRandom = new PlayerAndRandom();
+        BridgeGame bridgeGame = new BridgeGame(bridgeState);
 
-            Loop2:
-            for (int i = 0; i < designBridge.size() - 1; i++) {
+        for (int i = 0; i < bridgeLength; i++) {
+            String playerMoving = inputView.readMoving();
 
-                gameCount++;
+            String bridgeJudgment = playerAndRandom.Judgment(playerMoving, designBridge.get(i));
 
-                String moveLocation = inputView.readMoving();
+            HashMap<String, StringBuilder> bridgePlace = bridgeGame.move(playerMoving, bridgeJudgment);
 
-                String discriminant = bridgeGame.move(moveLocation, designBridge.get(i));
+            bridgeConnection(bridgeLength, bridgeState, i);
 
-                if (moveLocation.equals("U") && discriminant.equals("O")) {
-                    str1 += discriminant;
-
-                    if (designBridge.get(i + 1).equals("1") || designBridge.get(i + 1).equals("X") || i == designBridge.size() - 1) {
-                        str1 += " ]";
-                    }
-
-                    if (!(designBridge.get(i + 1).equals("1") || designBridge.get(i + 1).equals("X") || i == designBridge.size() - 1)) {
-                        str1 += " | ";
-                    }
-
-                    // TODO: 여기서 부터는 str2 관한 부분
-
-                    str2 += " ";
-
-                    if (designBridge.get(i + 1).equals("1") || designBridge.get(i + 1).equals("X") || i == designBridge.size() - 1) {
-                        str2 += " ]";
-                    }
-
-                    if (!(designBridge.get(i + 1).equals("1") || designBridge.get(i + 1).equals("X") || i == designBridge.size() - 1)) {
-                        str2 += " | ";
-                    }
-
-                }
-
-                // TODO: U == X 경우
-                if (moveLocation.equals("U") && discriminant.equals("X")) {
-                    gameSuccess = false;
-                    str1 += discriminant;
-
-                    if (designBridge.get(i + 1).equals("1") || designBridge.get(i + 1).equals("X") || i == designBridge.size() - 1) {
-                        str1 += " ]";
-                    }
-
-                    if (!(designBridge.get(i + 1).equals("1") || designBridge.get(i + 1).equals("X") || i == designBridge.size() - 1)) {
-                        str1 += " | ";
-                    }
-
-                    str2 += " ";
-
-                    if (designBridge.get(i + 1).equals("1") || designBridge.get(i + 1).equals("X") || i == designBridge.size() - 1) {
-                        str2 += " ]";
-                    }
-
-                    if (!(designBridge.get(i + 1).equals("1") || designBridge.get(i + 1).equals("X") || i == designBridge.size() - 1)) {
-                        str2 += " | ";
-                    }
-
-
-                }
-
-                if (moveLocation.equals("D") && discriminant.equals("O")) {
-                    str2 += discriminant;
-
-                    if (designBridge.get(i + 1).equals("1") || designBridge.get(i + 1).equals("X") || i == designBridge.size() - 1) {
-                        str2 += " ]";
-                    }
-
-                    if (!(designBridge.get(i + 1).equals("1") || designBridge.get(i + 1).equals("X") || i == designBridge.size() - 1)) {
-                        str2 += " | ";
-                    }
-
-                    // TODO: 여기서부터는 str1 관한 부분
-                    str1 += " ";
-
-                    if (designBridge.get(i + 1).equals("1") || designBridge.get(i + 1).equals("X") || i == designBridge.size() - 1) {
-                        str1 += " ]";
-                    }
-
-                    if (!(designBridge.get(i + 1).equals("1") || designBridge.get(i + 1).equals("X") || i == designBridge.size() - 1)) {
-                        str1 += " | ";
-                    }
-                }
-
-                //TODO: D == X인 경우
-                if (moveLocation.equals("D") && discriminant.equals("X")) {
-                    gameSuccess = false;
-                    str2 += discriminant;
-
-                    if (designBridge.get(i + 1).equals("1") || designBridge.get(i + 1).equals("X") || i == designBridge.size() - 1) {
-                        str2 += " ]";
-                    }
-
-                    if (!(designBridge.get(i + 1).equals("1") || designBridge.get(i + 1).equals("X") || i == designBridge.size() - 1)) {
-                        str2 += " | ";
-                    }
-
-                    str1 += " ";
-
-                    if (designBridge.get(i + 1).equals("1") || designBridge.get(i + 1).equals("X") || i == designBridge.size() - 1) {
-                        str1 += " ]";
-                    }
-
-                    if (!(designBridge.get(i + 1).equals("1") || designBridge.get(i + 1).equals("X") || i == designBridge.size() - 1)) {
-                        str1 += " | ";
-                    }
-                }
+            //TODO: 게임을 다시 시도할지 여부를 입력해주세요. -> 구현해야됨.
+        }
+    }
 
 
 
-                outputView.printMap(str1, str2);
 
 
+    private void bridgeConnection(int bridgeLength, HashMap<String, StringBuilder> bridgeState, int i) {
+        if (i != bridgeLength -1) {
+            bridgeState.get(0).append(" |");
+            bridgeState.get(1).append(" |");
+        }
 
-                if (str1.contains("X") || str2.contains("X")) {
-
-                    System.out.println("재시작/종료 입력받기: ");
-                    String reStart = inputView.readGameCommand();
-
-                    if (reStart.equals("Q")) {
-                        outputView.printResult(str1, str2, gameCount, gameSuccess);
-                        restartVariable = "R";
-                        break;
-                    }
-
-                    //TODO: 문제점
-                    if (reStart.equals("R")) {
-                        gameSuccess = true;
-                        str1 = "";
-                        str2 = "";
-                        break Loop2;
-                    }
-                }
-
-
-                //TODO: 성공 출력
-                if (i == designBridge.size() -2) {
-                    System.out.println("게임 성공 로직 메시지");
-                    outputView.printResult(str1, str2, gameCount, gameSuccess);
-                    restartVariable = "R";
-                    break;
-                }
-
-            }
+        if (i == bridgeLength -1) {
+            bridgeState.get(0).append(" ]");
+            bridgeState.get(1).append(" ]");
         }
     }
 }
+
+
