4976c131cb84a1652f607b3bae4fc3c46dc9b010
yu-heejin <jkgt0521@naver.com>
2022-11-20 05:44:57 +0900

    feat: 다리 길이 입력받는 기능 구현

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -4,5 +4,9 @@ public class Application {
 
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        InputView inputView = new InputView();
+
+        int bridgeSize = inputView.readBridgeSize();
+        System.out.println(bridgeSize);
     }
 }
