3ce72a780128f982cabf599f303f8c3303d444ae
Choi-JJunho <junho5336@gmail.com>
2022-11-20 00:35:43 +0900

    feat(BridgeGame): add base status and method

--- src/main/java/bridge/BridgeGame.java
+++ src/main/java/bridge/BridgeGame.java
@@ -1,9 +1,24 @@
 package bridge;
 
+import bridge.domain.Bridge;
+import bridge.domain.User;
+
+import static bridge.GameStatus.PLAYING;
+
 /**
  * 다리 건너기 게임을 관리하는 클래스
  */
 public class BridgeGame {
+    private final GameStatus status = PLAYING;
+    private final int tryCount = 1;
+
+    private final User user;
+    private final Bridge bridge;
+
+    BridgeGame(int bridgeSize) {
+        user = new User();
+        bridge = new Bridge(bridgeSize);
+    }
 
     /**
      * 사용자가 칸을 이동할 때 사용하는 메서드
@@ -19,5 +34,13 @@ public class BridgeGame {
      * 재시작을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
     public void retry() {
+    }
+
+    public GameStatus getStatus() {
+        return status;
+    }
+
+    public int getTryCount() {
+        return tryCount;
     }
 }
---
+++ src/main/java/bridge/GameStatus.java
@@ -0,0 +1,7 @@
+package bridge;
+
+public enum GameStatus {
+    WIN,
+    LOSE,
+    PLAYING
+}
