38c01fcde1df907ddfe84df52cd36e3f89646894
Kimhyeyun <cqqwer@gmail.com>
2022-11-20 01:35:45 +0900

    Feat: get메서드 / 상태 저장 메서드 생성

--- src/main/java/bridge/domain/User.java
+++ src/main/java/bridge/domain/User.java
@@ -5,10 +5,35 @@ import java.util.List;
 
 public class User {
     private List<String> userMoveState;
-    private List<String> moveMatchState;
+    private List<Boolean> moveMatchState;
 
     public User() {
         this.userMoveState = new ArrayList<>();
         this.moveMatchState = new ArrayList<>();
+    }
+
+    public void move(String moving) {
+        userMoveState.add(moving);
+    }
+
+    public int getUserLastIndex() {
+        return userMoveState.size() - 1;
+    }
+
+    public void saveState(boolean checkCorrectMoving) {
+        moveMatchState.add(checkCorrectMoving);
+    }
+
+    public String getUserMoveState(int index) {
+        return userMoveState.get(index);
+    }
+
+    public boolean getMoveMatchState(int index) {
+        return moveMatchState.get(index);
+    }
+
+    public void clear() {
+        moveMatchState.clear();
+        userMoveState.clear();
     }
 }
