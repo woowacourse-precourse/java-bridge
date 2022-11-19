42eb9e5e9f71e6a9841f3351bcfcecee785d45fd
jthugg <jthug@naver.com>
2022-11-20 06:43:28 +0900

    Feat<BridgeGame>: 게임 실행 전 초기화 기능

--- src/main/java/bridge/BridgeGame.java
+++ src/main/java/bridge/BridgeGame.java
@@ -4,6 +4,17 @@ package bridge;
  * 다리 건너기 게임을 관리하는 클래스
  */
 public class BridgeGame {
+    private InputController input;
+    private Bridge bridge;
+    private OutputView output;
+
+    public void init() {
+        BridgeNumberGenerator numberGenerator = new BridgeRandomNumberGenerator();
+        BridgeMaker bridgeMaker = new BridgeMaker(numberGenerator);
+        input = new InputController();
+        bridge = new Bridge(bridgeMaker.makeBridge(input.requestBridgeSize()));
+        output = new OutputView();
+    }
 
     /**
      * 사용자가 칸을 이동할 때 사용하는 메서드
