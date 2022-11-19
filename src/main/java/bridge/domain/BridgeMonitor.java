825079aa7a81f34c15f20d7c60a76ba4cc611f42
Woojin Lee <dhkep03@gmail.com>
2022-11-20 04:10:15 +0900

    feat(BridgeMonitor): 생성자 구현 - 각 라인에 괄호 열기

--- src/main/java/bridge/domain/BridgeMonitor.java
+++ src/main/java/bridge/domain/BridgeMonitor.java
@@ -1,4 +1,17 @@
 package bridge.domain;
 
 public class BridgeMonitor {
+    private StringBuilder upperLine;
+    private StringBuilder lowerLine;
+    private static final String START_BRACKET = "[";
+    private static final String SPACE = "   ";
+    private static final String CORRECT_MOVING = " O ";
+    private static final String WRONG_MOVING = " X ";
+    private static final String BETWEEN_LINE = "|";
+    private static final String END_BRACKET = "]";
+
+    public BridgeMonitor() {
+        this.upperLine = new StringBuilder(START_BRACKET);
+        this.lowerLine = new StringBuilder(START_BRACKET);
+    }
 }
