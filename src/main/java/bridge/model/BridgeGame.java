36701439acbcc33839c6ab9a2090f3bab36c4c5d
Jeongjihun <wlgns2234@naver.com>
2022-11-20 04:18:31 +0900

    style: 주석 삭제

--- src/main/java/bridge/model/BridgeGame.java
+++ src/main/java/bridge/model/BridgeGame.java
@@ -3,18 +3,10 @@ package bridge.model;
 import java.util.ArrayList;
 import java.util.List;
 
-/**
- * 다리 건너기 게임을 관리하는 클래스
- */
 public class BridgeGame {
     private int tryNumber;
     private List<String> preStatus = new ArrayList<>();
 
-    /**
-     * 사용자가 칸을 이동할 때 사용하는 메서드
-     * <p>
-     * 이동을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
-     */
     public void move(String whereMoving) {
         this.preStatus.add(whereMoving);
     }
@@ -23,20 +15,15 @@ public class BridgeGame {
         this.preStatus.remove(step);
     }
 
-    /**
-     * 사용자가 게임을 다시 시도할 때 사용하는 메서드
-     * <p>
-     * 재시작을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
-     */
     public void retry() {
         this.tryNumber ++;
     }
 
-    public void start() {
+    public void setTryNumber() {
         this.tryNumber = 1;
     }
 
-    public void end() {
+    public void resetTryNumber() {
         this.tryNumber = 0;
     }
 
@@ -47,6 +34,4 @@ public class BridgeGame {
     public List<String> getPreStatus() {
         return this.preStatus;
     }
-
-
 }
