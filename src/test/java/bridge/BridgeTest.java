eeb7db33ce96f92ed90d8bd0cf00d9f006a80bfc
jinhyeon <vmflwu123@gmail.com>
2022-11-19 22:40:49 +0900

    test(BridgeTest): checkPassableBlock 메서드 테스트

---
+++ src/test/java/bridge/BridgeTest.java
@@ -0,0 +1,22 @@
+package bridge;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+class BridgeTest {
+
+    @DisplayName("플레이어가 선택한 칸이 건널 수 있는 칸이면 true를 반환한다.")
+    @ParameterizedTest
+    @ValueSource(ints = {-1, 1, 3})
+    void checkPassableBlock(Integer currentPosition) {
+        Bridge bridge = new Bridge(List.of("U", "D", "U", "D", "U"));
+        assertThat(bridge.checkPassableBlock(currentPosition, "U")).isTrue();
+    }
+}
