2d5a22c9b23918a1ae1aa1a85eea3a2b8de56261
Nitroblack1 <109022435+Nitroblack1@users.noreply.github.com>
2022-11-19 20:25:22 +0900

    feat(BridgeData) : implement setter method in bridgeData class

--- src/main/java/bridge/Database/BridgeData.java
+++ src/main/java/bridge/Database/BridgeData.java
@@ -3,7 +3,16 @@ package bridge.Database;
 import java.util.List;
 
 public class BridgeData {
+
     private List<Integer> bridge;
     private List<Integer> bridgeLayOutByUser;
     private int totalAttempt;
+
+    public void setBridge(List<Integer> bridge) {
+        this.bridge = bridge;
+    }
+
+    public void setBridgeLayOutByUser(List<Integer> bridgeLayOutByUser) {
+        this.bridgeLayOutByUser = bridgeLayOutByUser;
+    }
 }
