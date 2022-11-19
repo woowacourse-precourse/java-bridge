d4aa2c75b4ebace9b930bbd2a8c58d5b59567c01
bnbnac <bnbnac@naver.com>
2022-11-19 17:34:32 +0900

    test: 다리 길이 입력 검증에 대한 테스트 작성

--- src/main/java/bridge/Constant.java
+++ src/main/java/bridge/Constant.java
@@ -1,2 +1,15 @@
-package bridge;public class Constant {
+package bridge;
+
+public class Constant {
+    public static final String ERROR = "[ERROR] ";
+
+    public static final Integer MIN_BRIDGE_SIZE = 3;
+    public static final Integer MAX_BRIDGE_SIZE = 20;
+
+    public static final String READ_BRIDGE_SIZE = "다리의 길이를 입력해주세요.";
+
+    public static final String BRIDGE_SIZE_ERROR = "다리 길이는 " +
+            MIN_BRIDGE_SIZE + "부터 " + MAX_BRIDGE_SIZE + " 사이의 숫자여야 합니다.";
+
+
 }
