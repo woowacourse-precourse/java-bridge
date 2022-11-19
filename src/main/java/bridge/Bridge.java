741d02865c6e3ed490d5cdfb06e666b41abe4cba
java-saeng <gusdn85554@naver.com>
2022-11-19 22:07:50 +0900

    feat : Bridge 클래스 생성

---
+++ src/main/java/bridge/Bridge.java
@@ -0,0 +1,55 @@
+package bridge;
+
+import java.util.List;
+
+public class Bridge {
+
+    private final List<String> upBridge;
+    private final List<String> downBridge;
+
+    public Bridge(List<String> upBridge, List<String> downBridge) {
+        this.upBridge = upBridge;
+        this.downBridge = downBridge;
+    }
+
+    public void moveUpBridgeCorrect() {
+        upBridge.add("O");
+        moveNothing(downBridge);
+    }
+
+    public void moveDownBridgeCorrect() {
+        moveNothing(upBridge);
+        downBridge.add("O");
+    }
+
+    public void moveUpBridgeInCorrect() {
+        upBridge.add("X");
+        moveNothing(downBridge);
+    }
+
+    public void moveDownBridgeInCorrect() {
+        moveNothing(upBridge);
+        downBridge.add("X");
+    }
+
+    private void moveNothing(List<String> bridge) {
+        bridge.add(" ");
+    }
+
+    private StringBuilder prettyPrint(List<String> bridge) {
+        StringBuilder stringBuilder = new StringBuilder("[ ");
+
+        stringBuilder.append(String.join(" | ", bridge));
+
+        stringBuilder.append(" ]");
+
+        return stringBuilder;
+    }
+
+    @Override
+    public String toString() {
+        return prettyPrint(upBridge).append("\n")
+                                    .append(prettyPrint(downBridge))
+                                    .toString();
+    }
+}
