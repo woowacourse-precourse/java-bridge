cf263184d1d0fd57553c13ad5a1fccc85572b5e1
Jaeyeop-Jung <jjy5349@naver.com>
2022-11-19 16:11:07 +0900

    test: 사용자 관련 테스트 추가
    사용자를 생성한 뒤 초기 step과 retryCount가 제대로 입력되었는지 테스트

---
+++ src/main/java/bridge/domain/User.java
@@ -0,0 +1,15 @@
+package bridge.domain;
+
+public class User {
+
+    private Integer step = 0;
+    private Integer retryCount = 0;
+
+    public Integer getStep() {
+        return step;
+    }
+
+    public Integer getRetryCount() {
+        return retryCount;
+    }
+}
