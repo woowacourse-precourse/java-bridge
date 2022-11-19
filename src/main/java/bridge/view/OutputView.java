2131dd4e57f5c75bcaf26311d3bd046a331aff1e
Guga <103228463+BGuga@users.noreply.github.com>
2022-11-19 19:18:36 +0900

    style: Bridge Console출력 시 공백 추가

--- src/main/java/bridge/view/OutputView.java
+++ src/main/java/bridge/view/OutputView.java
@@ -9,11 +9,11 @@ import bridge.dto.UserState;
  * 사용자에게 게임 진행 상황과 결과를 출력하는 역할을 한다.
  */
 public class OutputView {
-    private static final String GAME_BEGINNING_MESSAGE = "다리 건너기 게임을 시작합니다.";
+    private static final String GAME_BEGINNING_MESSAGE = "다리 건너기 게임을 시작합니다.\n";
     private static final String GAME_RESULT_INTRO_MESSAGE = "최종 게임 결과";
     private static final String IS_GAME_SUCCESS = "게임 성공 여부: %s";
     private static final String NUMBER_OF_ATTEMPTS = "총 시도한 횟수: %d";
-    private static final String BRIDGE_FRAME = "[%s]\n[%s]";
+    private static final String BRIDGE_FRAME = "[%s]\n[%s]\n";
     private static final String CORRECT_DIRECTION = " O ";
     private static final String INCORRECT_DIRECTION = " X ";
     private static final String EMPTY_SPACE = "   ";
