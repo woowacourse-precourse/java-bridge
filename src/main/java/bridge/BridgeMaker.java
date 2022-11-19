e05d354f789015331c9237c3e39099b377e59c17
osy9757 <osy9757@gmail.com>
2022-11-19 23:49:51 +0900

    docs : 클래스 요구사항 추가

--- src/main/java/bridge/BridgeMaker.java
+++ src/main/java/bridge/BridgeMaker.java
@@ -4,6 +4,9 @@ import java.util.List;
 
 /**
  * 다리의 길이를 입력 받아서 다리를 생성해주는 역할을 한다.
+ * 제공된 BridgeMaker 클래스를 활용해 구현해야 한다.
+ * BridgeMaker의 필드(인스턴스 변수)를 변경할 수 없다.
+ * BridgeMaker의 메서드의 시그니처(인자, 이름)와 반환 타입은 변경할 수 없다.
  */
 public class BridgeMaker {
 
