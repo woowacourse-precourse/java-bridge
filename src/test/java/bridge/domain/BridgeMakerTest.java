c701b4f456fb85b985e43a65428eb36150c13c04
hongsikcho <jjdd9758@naver.com>
2022-11-19 19:35:07 +0900

    test : bridgeMaker클래스 테스트 추가

---
+++ src/test/java/bridge/domain/BridgeMakerTest.java
@@ -0,0 +1,22 @@
+package bridge.domain;
+
+import bridge.BridgeRandomNumberGenerator;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+class BridgeMakerTest {
+
+    @DisplayName("입력한 사이즈만큼의 다리를 생성한다.")
+    @Test
+    void makeBridge() {
+        BridgeMaker bridgeMaker = new BridgeMaker(new BridgeRandomNumberGenerator());
+        List<String> bridgeStatus = bridgeMaker.makeBridge(3);
+        assertThat(bridgeStatus.size()).isEqualTo(3);
+
+    }
+}
