7f15e111b77690aa2625e202d7fc92d16eaed982
zjadofl0408 <zjadofl0408@gmail.com>
2022-11-20 02:04:14 +0900

    feat(InputView) : 게임의 재시작 또는 종료를 입력받는 기능 구현

--- src/main/java/bridge/InputView.java
+++ src/main/java/bridge/InputView.java
@@ -9,11 +9,15 @@ public class InputView {
     private static final String GAME_STARTING_MESSAGE = "다리 건너기 게임을 시작합니다.";
     private static final String BRIDGE_LENGTH_MESSAGE = "다리의 길이를 입력해주세요.";
     private static final String CHOOSE_MOVING_MESSAGE = "이동할 칸을 선택해주세요. (위: U, 아래: D)";
+    private static final String CHOOSE_RETRY_OR_QUIT_MESSAGE = "게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)";
     private static final String OUT_OF_RANGE_ERROR_MESSAGE = "[ERROR] 다리의 길이는 3 이상 20이하의 숫자여야 합니다.";
     private static final String NOT_NUMERIC_ERROR_MESSAGE = "[ERROR] 숫자만 입력해주세요.";
-    private static final String NOT_UP_OR_DOWN_ERROR_MESSAGE = "U 또는 D만 입력해주세요.";
+    private static final String NOT_UP_OR_DOWN_ERROR_MESSAGE = "[ERROR] U 또는 D만 입력해주세요.";
+    private static final String NOT_RETRY_OR_QUIT_ERROR_MESSAGE = "[ERROR] R 또는 Q만 입력해주세요.";
     private static final String UP = "U";
     private static final String DOWN = "D";
+    private static final String RETRY = "R";
+    private static final String QUIT = "Q";
 
     /**
      * 다리의 길이를 입력받는다.
@@ -42,6 +46,11 @@ public class InputView {
      * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
      */
     public String readGameCommand() {
+        printRetryOrQuit();
+        String input = Console.readLine();
+        if (!(input.equals(RETRY) || input.equals(QUIT))) {
+            throw new IllegalArgumentException(NOT_RETRY_OR_QUIT_ERROR_MESSAGE);
+        }
         return null;
     }
 
@@ -55,6 +64,10 @@ public class InputView {
 
     public void printChooseMoving() {
         System.out.println(CHOOSE_MOVING_MESSAGE);
+    }
+
+    public void printRetryOrQuit() {
+        System.out.println(CHOOSE_RETRY_OR_QUIT_MESSAGE);
     }
 
     private void validateBridgeSize(int size) throws IllegalArgumentException {
