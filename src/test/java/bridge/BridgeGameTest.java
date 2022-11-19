0db92f8852d420c3a621a7f59240ee98ea0fdd34
은지 <midzy0228@gmail.com>
2022-11-19 14:04:16 +0900

    test: 다리와 사용자 입력 비교 테스트 추가

---
+++ src/test/java/bridge/BridgeGameTest.java
@@ -0,0 +1,26 @@
+package bridge;
+
+import bridge.model.BridgeGame;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class BridgeGameTest {
+    @DisplayName("이동할 수 있을 때 true를 반환한다.")
+    @Test
+    void createMovable() {
+        BridgeGame bridgeGame = new BridgeGame();
+        boolean move = bridgeGame.move("U", "U");
+        assertTrue(move);
+    }
+
+    @DisplayName("이동할 수 없을 때 false를 반환한다.")
+    @Test
+    void createUnMovable() {
+        BridgeGame bridgeGame = new BridgeGame();
+        boolean move = bridgeGame.move("D", "U");
+        assertFalse(move);
+    }
+}
