a2dfaf9e1db6858b6035db8e27b05cb8959863b8
황민주 <minjoo@hwangminjooui-MacBookAir.local>
2022-11-19 19:55:12 +0900

    feat(BridgeGame): retry (src/main/java/bridge/BridgeGame.java)
    retry 메소드 추가:
    -재시도를 하는지 아니면 게임을 끝내는지 결과를 반환하기 위해 추가함
    
    feat(Utility): Utility (src/main/java/bridge/Utility.java)
    
    Utility 클래스 추가 후 REGAME, QUIT static 문자열 추가:
    -잡다한 것들을 저장하기 위해 Utility 클래스 추가함
    -재시도(REGAME)와 그만두기(QUIT)의 문자열을 저장함

--- src/main/java/bridge/BridgeGame.java
+++ src/main/java/bridge/BridgeGame.java
@@ -2,6 +2,9 @@ package bridge;
 
 import java.util.List;
 
+import static bridge.Utility.QUIT;
+import static bridge.Utility.REGAME;
+
 /**
  * 다리 건너기 게임을 관리하는 클래스
  */
@@ -24,6 +27,13 @@ public class BridgeGame {
      * <p>
      * 재시작을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void retry() {
+    public boolean retry(String reGame) {
+        if(reGame.equals(REGAME)){
+            return true;
+        }
+        if(reGame.equals(QUIT)){
+            return false;
+        }
+        return false;
     }
 }
---
+++ src/main/java/bridge/Utility.java
@@ -0,0 +1,6 @@
+package bridge;
+
+public class Utility {
+    public static String REGAME = "R";
+    public static String QUIT = "Q";
+}
