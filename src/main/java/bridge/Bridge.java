1987ed8265b97d71045d3b323a455da50a08b494
Cheon-DM <76736548+Cheon-DM@users.noreply.github.com>
2022-11-19 22:08:10 +0900

    feat(bridge): 다리 관련 클래스 선언

---
+++ src/main/java/bridge/Bridge.java
@@ -0,0 +1,19 @@
+package bridge;
+
+import java.util.List;
+
+public class Bridge {
+    private final List<String> bridge;
+
+    public Bridge(List<String> bridge) {
+        this.bridge = bridge;
+    }
+
+    public List<String> getBridge() {
+        return bridge;
+    }
+
+    public void setBridge(String component){
+        bridge.add(component);
+    }
+}
