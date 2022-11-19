1a676306540fe4321c8cf83e2c7c351dbdc64cde
Song kyeong seok <skj0922@kookmin.ac.kr>
2022-11-19 18:05:42 +0900

    test(BridgeMakerTest): 다리 생성 사이즈 테스트 코드 구현

---
+++ src/test/java/bridge/BridgeMakerTest.java
@@ -0,0 +1,23 @@
+package bridge;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.util.List;
+
+public class BridgeMakerTest {
+
+    BridgeRandomNumberGenerator bridgeRandomNumberGenerator = new BridgeRandomNumberGenerator();
+    BridgeMaker bridgeMaker = new BridgeMaker(bridgeRandomNumberGenerator);
+
+    @DisplayName("다리 생성 사이즈 테스트")
+    @ParameterizedTest
+    @ValueSource(ints = {3, 4, 5, 20})
+    public void createBridgeSizeTest(int size) {
+        List<String> bridge = bridgeMaker.makeBridge(size);
+        Assertions.assertThat(bridge.size()).isEqualTo(size);
+    }
+}
