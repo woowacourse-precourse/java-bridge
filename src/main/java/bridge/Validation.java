7f9363b5d39218e79fd4cbf371158a8c9892137a
impati <impati0716@icloud.com>
2022-11-19 10:53:12 +0900

    feat(Validation) : 다리 길이 예외 처리

--- src/main/java/bridge/Validation.java
+++ src/main/java/bridge/Validation.java
@@ -2,6 +2,13 @@ package bridge;
 
 public class Validation {
     private final String BRIDGE_MESSAGE = "[ERROR] 다리 길이 입력은 3이상 20이하의 숫자를 입력할 수 있습니다.";
+
+
+    private void bridgeLen(String input){
+        notNumberFormat(input);
+        Integer len = Integer.parseInt(input);
+        if(len < 3 || len > 20) throw new IllegalArgumentException(BRIDGE_MESSAGE);
+    }
     private void notNumberFormat(String input){
         try {
             Integer.parseInt(input);
