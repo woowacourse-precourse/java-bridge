b496d1ae90f5be82bebb5c1f377806d081f55ad1
rabbit0216 <rabbit1999k@gmail.com>
2022-11-19 13:59:46 +0900

    feat: 다리 길이를 입력받기 + 다리길이 예외 체크 함수 구현

--- src/main/java/bridge/InputView.java
+++ src/main/java/bridge/InputView.java
@@ -1,5 +1,8 @@
 package bridge;
 
+import static bridge.Constants.*;
+import static camp.nextstep.edu.missionutils.Console.readLine;
+
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
  */
@@ -9,7 +12,10 @@ public class InputView {
      * 다리의 길이를 입력받는다.
      */
     public int readBridgeSize() {
-        return 0;
+        String tempBridgeSize = readLine();
+        bridgeValidate(tempBridgeSize);
+        int bridgeSize = Integer.parseInt(tempBridgeSize);
+        return bridgeSize;
     }
 
     /**
@@ -24,5 +30,28 @@ public class InputView {
      */
     public String readGameCommand() {
         return null;
+    }
+
+    void bridgeValidate(String bridgeSize){
+        if(!isNumber(bridgeSize)){
+            System.out.println(ERROR_PREFIX + BRIDGE_ERROR);
+            throw new IllegalArgumentException(ERROR_PREFIX + BRIDGE_ERROR);
+        }
+        int convertedBridgeSize = Integer.parseInt(bridgeSize);
+        if(convertedBridgeSize < 3 || convertedBridgeSize > 20){
+            System.out.println(ERROR_PREFIX + BRIDGE_ERROR);
+            throw new IllegalArgumentException(ERROR_PREFIX + BRIDGE_ERROR);
+
+        }
+    }
+
+    boolean isNumber(String bridgeSize){
+        boolean answer = true;
+        for(int idx = 0; idx < bridgeSize.length(); idx++){
+            if(!Character.isDigit(bridgeSize.charAt(idx))){
+                answer = false;
+            }
+        }
+        return answer;
     }
 }
