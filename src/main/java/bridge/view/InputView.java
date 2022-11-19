5bcfb847f385c73880f2b5dcc7874c29975e142d
김현재 <41482946+galug@users.noreply.github.com>
2022-11-20 00:16:07 +0900

    feat(inputview): 사용자가 이동할 칸 입력하는 기능 추가

--- src/main/java/bridge/view/InputView.java
+++ src/main/java/bridge/view/InputView.java
@@ -25,7 +25,13 @@ public class InputView {
      * 사용자가 이동할 칸을 입력받는다.
      */
     public String readMoving() {
-        return null;
+        String selectUpOrDownInput = input();
+        validateMovingInput(selectUpOrDownInput);
+        return selectUpOrDownInput;
+    }
+
+    private void validateMovingInput(String selectUpOrDownInput) {
+
     }
 
     /**
