9b015f08348cec442c3faa09be8c96cf38a240c2
Cheon-DM <76736548+Cheon-DM@users.noreply.github.com>
2022-11-20 01:41:38 +0900

    feat(controller): 게임 컨트롤러 기능 추가
    게임 컨트롤러 기능
    - 게임 시작 메시지 출력
    - 다리 건너기 이동
    - 다리 건너기 결과 출력

--- src/main/java/bridge/Controller.java
+++ src/main/java/bridge/Controller.java
@@ -14,20 +14,53 @@ public class Controller {
     }
 
     public void run(){
-        inputView.getBridgeGameStartMsg();
+        printGameStartMsg();
         int bridgeSize = inputView.readBridgeSize();
         BridgeMaker bridgeMaker = new BridgeMaker(new BridgeRandomNumberGenerator());
         Bridge bridge = new Bridge(bridgeMaker.makeBridge(bridgeSize));
+        int userTry = 1;
+        move(bridge, bridgeSize, userTry);
+    }
+
+    private void printGameStartMsg(){
+        inputView.getBridgeGameStartMsg();
+        inputView.getInputBridgeLengthMsg();
+    }
+
+    private void move(Bridge bridge, int bridgeSize, int userTry){
         BridgeGame bridgeGame = new BridgeGame(bridge);
         int step = 0;
+        boolean quitFlag = false;
+        boolean retryFlag = false;
         while(step != bridgeSize){
+            inputView.getInputMoveUpOrDownMsg();
             String moving = inputView.readMoving();
             boolean isEqual = bridgeGame.move(moving, step);
             outputView.printMap(bridgeGame);
             if (!isEqual){
-
+                inputView.getInputRestartOrQuitMsg();
+                String gameCommand = inputView.readGameCommand();
+                if (gameCommand.equals("Q")) {
+                    quitFlag = true;
+                    break;
+                }
+                if (gameCommand.equals("R")){
+                    retryFlag = true;
+                    break;
+                }
             }
             step++;
         }
+        if (quitFlag || step == bridgeSize){
+            printResult(bridgeGame, quitFlag, userTry);
+        }
+        if (retryFlag){
+            bridgeGame.retry();
+            move(bridge, bridgeSize, userTry+1);
+        }
+    }
+
+    private void printResult(BridgeGame bridgeGame, boolean quitFlag, int userTry){
+        outputView.printResult(bridgeGame, quitFlag, userTry);
     }
 }
