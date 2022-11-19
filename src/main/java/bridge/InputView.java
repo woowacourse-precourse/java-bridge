612b1c7dab115629bf641c172642c1cd38e126ce
이정우 <acg6138@naver.com>
2022-11-19 10:58:18 +0900

    feat(InputView): 재시작 여부에 대한 입력 값을 검증하고 반환하는 기능 구현

--- src/main/java/bridge/InputView.java
+++ src/main/java/bridge/InputView.java
@@ -20,7 +20,9 @@ public class InputView {
     }
 
     public String readGameCommand() {
-        return null;
+        String input = Console.readLine();
+        validateCommand(input);
+        return input;
     }
 
     public void validateBridgeSize(String input) {
@@ -39,6 +41,12 @@ public class InputView {
     public void validateMoveInput(String input) {
         if (!input.equals("U") && !input.equals("D")) {
             throw new IllegalArgumentException("[ERROR] 이동할 칸은 U 또는 D만 입력할 수 있습니다.");
+        }
+    }
+
+    public void validateCommand(String input) {
+        if (!input.equals("Q") && !input.equals("R")) {
+            throw new IllegalArgumentException("[ERROR] 게임 재시작 여부는 R 또는 Q만 입력할 수 있습니다.");
         }
     }
 }
