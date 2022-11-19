64f135520e77d407bbbf200f74387a79b106b849
이정우 <acg6138@naver.com>
2022-11-19 10:53:50 +0900

    feat(InputView): 다리 길이 값을 검증하고 반환하는 기능 구현

--- src/main/java/bridge/InputView.java
+++ src/main/java/bridge/InputView.java
@@ -1,28 +1,33 @@
 package bridge;
 
-/**
- * 사용자로부터 입력을 받는 역할을 한다.
- */
+import camp.nextstep.edu.missionutils.Console;
+
 public class InputView {
 
-    /**
-     * 다리의 길이를 입력받는다.
-     */
     public int readBridgeSize() {
-        return 0;
+        String input = Console.readLine();
+        validateBridgeSize(input);
+        return Integer.parseInt(input);
     }
 
-    /**
-     * 사용자가 이동할 칸을 입력받는다.
-     */
     public String readMoving() {
         return null;
     }
 
-    /**
-     * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
-     */
     public String readGameCommand() {
         return null;
+    }
+
+    public void validateBridgeSize(String input) {
+        if (!input.matches("^[0-9][1,2]") && !isInRange(Integer.parseInt(input))) {
+            throw new IllegalArgumentException("[ERROR] 다리 길이는 3부터 20사이의 숫자여야 합니다.");
+        }
+    }
+
+    public boolean isInRange(int input) {
+        if (input < 3 && input > 20) {
+            return false;
+        }
+        return true;
     }
 }
