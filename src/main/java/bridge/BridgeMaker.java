3e244025fd332f28b1ca26a022b863de82697a56
abbd2 <ajkety00@gmail.com>
2022-11-19 20:24:51 +0900

    Feat(BridgeMaker): makeBridgeList 재구현

--- src/main/java/bridge/BridgeMaker.java
+++ src/main/java/bridge/BridgeMaker.java
@@ -19,21 +19,26 @@ public class BridgeMaker {
      * @return 입력받은 길이에 해당하는 다리 모양. 위 칸이면 "U", 아래 칸이면 "D"로 표현해야 한다.
      */
     // 인자, 이름, 반환 타입이 변경 불가한 메서드
+    // Test 완료
     public List<String> makeBridge(int size) {
         List<String> bridge = new ArrayList<>();
-        for (int i = 0; i < 2; i++) {
-            bridge.add("[   ]");
-        }
+            for (int i = 0; i < size; i++) {
+                bridge.add("[   ]");
+            }
         return bridge;
     }
 
-    public boolean isBridgeSize(List<String> bridge, int bridgeSize){
-        if(bridge.size() >= bridgeSize){
-            return true;
-        }
-        return false;
-    }
+    public List<Bridge> makeBridgeList(int size) {
 
+        List<Bridge> bridgeList = new ArrayList<>();
+        final int BRIDGE_DEPTH = 2;
+
+        for (int i = 0; i < BRIDGE_DEPTH; i++) {
+            List<String> makeBridge = makeBridge(size);
+            Bridge bridge = new Bridge(makeBridge);
+        }
+        return bridgeList;
+    }
 
     // BridgeMaker를 선언하는 곳에서 구현해야 함
     public int bridgeRandomNumber() {
