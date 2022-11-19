02a2ee8bbf8a1eeab78e603137fd4a353988ca72
jkde7721 <jkde7721@naver.com>
2022-11-19 22:26:39 +0900

    test: InputViewTest 메소드 public 제거

--- src/test/java/bridge/view/InputViewTest.java
+++ src/test/java/bridge/view/InputViewTest.java
@@ -16,7 +16,7 @@ class InputViewTest {
     @DisplayName("다리 길이로 정수 아닌 수 또는 문자 입력 시 예외가 발생한다.")
     @ParameterizedTest
     @ValueSource(strings = {"12j", " ", "abc", "a123e", "-3.5", "1.23", "1,000"})
-    public void readBridgeSizeNumberFormatExceptionTest(String bridgeSize) {
+    void readBridgeSizeNumberFormatExceptionTest(String bridgeSize) {
         final byte[] buf = String.join("\n", bridgeSize).getBytes();
         System.setIn(new ByteArrayInputStream(buf));
 
@@ -28,7 +28,7 @@ class InputViewTest {
     @DisplayName("다리 길이로 3 ~ 20 이외의 정수 입력 시 예외가 발생한다.")
     @ParameterizedTest
     @ValueSource(strings = {"2", "21", "0", "-3", "-11"})
-    public void readBridgeSizeRangeExceptionTest(String bridgeSize) {
+    void readBridgeSizeRangeExceptionTest(String bridgeSize) {
         final byte[] buf = String.join("\n", bridgeSize).getBytes();
         System.setIn(new ByteArrayInputStream(buf));
 
@@ -40,7 +40,7 @@ class InputViewTest {
     @DisplayName("이동할 칸으로 U 또는 D 이외의 문자 입력 시 예외가 발생한다.")
     @ParameterizedTest
     @ValueSource(strings = {"u", "d", "UD", " ", "abcde"})
-    public void readMovingExceptionTest(String moving) {
+    void readMovingExceptionTest(String moving) {
         final byte[] buf = String.join("\n", moving).getBytes();
         System.setIn(new ByteArrayInputStream(buf));
 
@@ -52,7 +52,7 @@ class InputViewTest {
     @DisplayName("게임 재시도 여부로 R 또는 Q 이외의 문자 입력 시 예외가 발생한다.")
     @ParameterizedTest
     @ValueSource(strings = {"r", "q", "RQ", " ", "fghij"})
-    public void readGameCommandExceptionTest(String gameCommand) {
+    void readGameCommandExceptionTest(String gameCommand) {
         final byte[] buf = String.join("\n", gameCommand).getBytes();
         System.setIn(new ByteArrayInputStream(buf));
 
