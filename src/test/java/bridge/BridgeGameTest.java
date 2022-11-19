b8b2da7e218f8aab14d97c9b3484ab1aa76080ef
Dongmin Kim <kimdongmin68@naver.com>
2022-11-20 01:17:31 +0900

    test(bridge): 오타 수정

--- src/test/java/bridge/BridgeGameTest.java
+++ src/test/java/bridge/BridgeGameTest.java
@@ -10,12 +10,12 @@ public class BridgeGameTest {
 
     @Test
     void 다리_이동_성공_여부_테스트() {
-        List<String> bridge = List.of("N", "N", "N", "D", "D");
-        List<String> inputMoving = List.of("N", "N", "D", "D", "N");
+        List<String> bridge = List.of("U", "U", "U", "D", "D");
+        List<String> inputMoving = List.of("U", "U", "D");
         BridgeGame game = new BridgeGame();
         for (int i = 0; i < inputMoving.size(); i++) {
             game.move(bridge, inputMoving.get(i));
         }
-        assertThat(game.getMovingResult()).isEqualTo(List.of(true, true, false, true, false));
+        assertThat(game.getMovingResult()).isEqualTo(List.of(true, true, false));
     }
 }
