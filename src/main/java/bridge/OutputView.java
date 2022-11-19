dc6c4e912ecf4d9e26fdd2bccd6278cc04571e78
Sinryuji <siler2474@gmail.com>
2022-11-19 15:13:46 +0900

    feat(OutputView): 그려진 다리 출력하는 기능 구현

--- src/main/java/bridge/OutputView.java
+++ src/main/java/bridge/OutputView.java
@@ -4,6 +4,11 @@ package bridge;
  * 사용자에게 게임 진행 상황과 결과를 출력하는 역할을 한다.
  */
 public class OutputView {
+    private final Bridge bridge;
+
+    public OutputView(Bridge bridge) {
+        this.bridge = bridge;
+    }
 
     /**
      * 현재까지 이동한 다리의 상태를 정해진 형식에 맞춰 출력한다.
@@ -11,6 +16,7 @@ public class OutputView {
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
     public void printMap() {
+        System.out.println(bridge.toString());
     }
 
     /**
