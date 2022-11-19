392ddbca0ad24bcb4e8a245499b4200f51ebd099
Choi-JJunho <junho5336@gmail.com>
2022-11-20 00:12:07 +0900

    test(BridgeMaker): add validateField test

--- src/test/java/bridge/BridgeMakerTest.java
+++ src/test/java/bridge/BridgeMakerTest.java
@@ -1,8 +1,14 @@
 package bridge;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
+
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
@@ -17,5 +23,16 @@ class BridgeMakerTest {
             bridgeMaker.makeBridge(size);
         });
     }
-    
+
+    @Test
+    @DisplayName("다리에는 U, D값만 들어있다.")
+    void validateField() {
+        List<String> bridge = bridgeMaker.makeBridge(20);
+        Pattern pattern = Pattern.compile("[^U,^D]");
+        bridge.forEach(floor -> {
+            Matcher matcher = pattern.matcher(floor);
+            Assertions.assertFalse(matcher.find());
+        });
+    }
+
 }
