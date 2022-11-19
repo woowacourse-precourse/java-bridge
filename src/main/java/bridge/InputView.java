7208e3970610331489e7f34ea606134704d6ad6a
OhJintaek <sunshot@hanyang.ac.kr>
2022-11-19 20:07:25 +0900

    type:refactor 사용자의 입력이 잘못되었을 때 에러를 발생하도록 수정

--- src/main/java/bridge/InputView.java
+++ src/main/java/bridge/InputView.java
@@ -1,6 +1,9 @@
 package bridge;
 
 import camp.nextstep.edu.missionutils.Console;
+
+import static bridge.Error.*;
+
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
  */
@@ -18,10 +21,11 @@ public class InputView {
         System.out.println("다리 건너기 게임을 시작합니다.\n\n다리의 길이를 입력해주세요.");
         String input = Console.readLine();
         System.out.println();
+
+        isNumber(input);
         int size = Integer.valueOf(input);
 
-        //에러 판독
-
+        isValidNumber(size);
         return size;
     }
 
@@ -32,7 +36,7 @@ public class InputView {
         System.out.println("이동할 칸을 선택해주세요. (위: U, 아래: D)");
         String input = Console.readLine();
 
-        //에러 판독
+        isValidFootHold(input);
 
         bridgeGame.move(input);
     }
@@ -44,7 +48,7 @@ public class InputView {
         System.out.println("게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)");
         String input = Console.readLine();
 
-        //에러 판독
+        isValidCommand(input);
 
         return input;
     }
