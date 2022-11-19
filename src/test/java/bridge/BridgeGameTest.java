096b98ae7a8a6d6e928b5c9d8809ea3eb87b6f03
naaaaag <115922748+naaaaag@users.noreply.github.com>
2022-11-19 16:53:01 +0900

    <test>BridgeGameTest : 입력 받은 방향으로 이동 시도하는 기능 테스트 코드 작성

---
+++ src/test/java/bridge/BridgeGameTest.java
@@ -0,0 +1,17 @@
+package bridge;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.List;
+
+public class BridgeGameTest {
+    @Test
+    void moveTest() {
+        BridgeGame tester = new BridgeGame(List.of("U", "D", "D", "U", "D"));
+
+        assertThat(tester.move("U", "U")).isEqualTo(true);
+    }
+}
+
