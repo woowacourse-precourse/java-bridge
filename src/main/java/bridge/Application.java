5e65e3654c79cd233816f5224393962ae2ffbbae
JUNG-jihoon <73165763+JUNG-jihoon@users.noreply.github.com>
2022-11-19 14:45:34 +0900

    Style : "삭제 같은 클래스 선언"
    지역에 선언되어 있는 클래스들 중 전역에 선언되어 중복되어 있다면 삭제하였습니다.

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -1,22 +1,27 @@
 package bridge;
 
 
+import java.util.Arrays;
+
 import static bridge.BridgeGame.Bridge_U;
 
 public class Application {
     static InputView input = new InputView();
     static BridgeRandomNumberGenerator bridgeRandomNumberGenerator = new BridgeRandomNumberGenerator();
     static BridgeMaker maker = new BridgeMaker(bridgeRandomNumberGenerator);
+    static BridgeGame bridgeGame = new BridgeGame();
 
     public static void main(String[] args) {
         // TODO: 프로그램 구현
         int size = input.readBridgeSize();
-        Bridge_Make(size);
+        Bridge_U = maker.makeBridge(size);
+        Bridge_Move();
     }
 
-    public static void Bridge_Make(int size) {
-        BridgeRandomNumberGenerator bridgeRandomNumberGenerator = new BridgeRandomNumberGenerator();
-        BridgeMaker maker = new BridgeMaker(bridgeRandomNumberGenerator);
-        Bridge_U = maker.makeBridge(size);
+    public static void Bridge_Move() {
+        String Command = input.readGameCommand();
+        int Correct[] = bridgeGame.move(Command);
+        System.out.println(Arrays.toString(Correct));
+
     }
 }
