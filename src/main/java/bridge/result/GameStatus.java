c4d830de5229230821455a376de2ce60e7b05262
kimminsu <xjfcnfw3@naver.com>
2022-11-19 19:54:07 +0900

    refactor: GameStatus Enum 불필요한 인스턴스 제거와 코드 수정

--- src/main/java/bridge/result/GameStatus.java
+++ src/main/java/bridge/result/GameStatus.java
@@ -1,28 +1,23 @@
 package bridge.result;
 
 public enum GameStatus {
-    FAIL("fail", false, true),
-    SUCCESS("Success", false, false),
-    PROGRESS("Progress", true, false);
+    FAIL("실패", true),
+    SUCCESS("성공", true),
+    PROGRESS("진행", false);
 
     private final String name;
     private final boolean isStopGame;
-    private final boolean isFailGame;
 
-    public String getName() {
+    @Override
+    public String toString() {
         return name;
-    }
-
-    public boolean isFailGame() {
-        return isFailGame;
     }
 
     public boolean isStopGame() {
         return isStopGame;
     }
 
-    GameStatus(String name, boolean isFailGame, boolean isStopGame) {
-        this.isFailGame = isFailGame;
+    GameStatus(String name, boolean isStopGame) {
         this.isStopGame = isStopGame;
         this.name = name;
     }
