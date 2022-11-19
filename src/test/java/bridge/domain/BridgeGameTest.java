c5d91a976cbd800449766fc190af0b29d618be43
Jzakka <mouse4786@gmail.com>
2022-11-19 23:39:03 +0900

    test: 코드 변겅으로 test 삭제

--- src/test/java/bridge/domain/BridgeGameTest.java
+++ src/test/java/bridge/domain/BridgeGameTest.java
@@ -10,18 +10,11 @@ class BridgeGameTest {
 
     private BridgeGame bridgeGame = new BridgeGame();
     private Bridge bridge = new Bridge(List.of("U", "D", "U"));
-    private User user = new User();
 
     @Test
     void move() {
         Assertions.assertThat(bridgeGame.move(0, "U", bridge)).isTrue();
         Assertions.assertThat(bridgeGame.move(1, "U", bridge)).isFalse();
         Assertions.assertThat(bridgeGame.move(2, "U", bridge)).isTrue();
-    }
-
-    @Test
-    void retry(){
-        Assertions.assertThat(bridgeGame.retry("R")).isTrue();
-        Assertions.assertThat(bridgeGame.retry("Q")).isFalse();
     }
 }
