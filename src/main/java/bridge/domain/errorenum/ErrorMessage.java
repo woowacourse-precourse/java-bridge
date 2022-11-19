ebfbe10ee723e34fa855189669dbf121094b603a
somsom13 <somin1113@naver.com>
2022-11-19 18:31:05 +0900

    feat(InputValidator): 문자 입력 검증
    플레이어가 입력한 값이 문자로만 구성되어야 할 때, 이를 검증하고 예외를 발생시킨다.

--- src/main/java/bridge/domain/errorenum/ErrorMessage.java
+++ src/main/java/bridge/domain/errorenum/ErrorMessage.java
@@ -1,11 +1,14 @@
 package bridge.domain.errorenum;
 
 import bridge.domain.bridgeenum.BridgeCondition;
+import bridge.domain.bridgeenum.MoveCondition;
 
 public enum ErrorMessage {
     HEAD("[ERROR] "),
     NOT_NUMBER("다리 길이는 자연수만 입력할 수 있습니다."),
-    NOT_IN_RANGE(String.format("다리 길이는 %d이상 %d 이하 여야 합니다.", BridgeCondition.MIN.getValue(), BridgeCondition.MAX.getValue()));
+    NOT_IN_RANGE(String.format("다리 길이는 %d이상 %d 이하 여야 합니다.", BridgeCondition.MIN.getValue(), BridgeCondition.MAX.getValue())),
+    NOT_UPPER_CASE("알파벳 대문자만 입력할 수 있습니다."),
+    NOT_IN_MOVE_CONDITION(String.format("이동할 칸은 %s, %s 중 하나만 선택할 수 있습니다.", MoveCondition.UPPER.getPosition(), MoveCondition.LOWER.getPosition()));
 
     private final String value;
 
--- src/main/java/bridge/util/InputValidator.java
+++ src/main/java/bridge/util/InputValidator.java
@@ -4,6 +4,7 @@ import bridge.domain.errorenum.ErrorMessage;
 
 public class InputValidator {
     private static final String NUMBER_REGEX = "[0-9]+";
+    private static final String UPPER_CASE_REGEX = "[A-Z]+";
 
     private InputValidator() {
     }
@@ -13,6 +14,13 @@ public class InputValidator {
             return;
         }
         throw new IllegalArgumentException(ErrorMessage.NOT_NUMBER.printMessage());
+    }
+
+    public static void validateOnlyString(String input) {
+        if (input.matches(UPPER_CASE_REGEX)) {
+            return;
+        }
+        throw new IllegalArgumentException(ErrorMessage.NOT_UPPER_CASE.printMessage());
     }
 
 }
