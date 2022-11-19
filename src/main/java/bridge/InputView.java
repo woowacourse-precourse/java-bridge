b92c15e493e71aebed293c34d0926524b4079e2d
a1dotls <meeae9408@gmail.com>
2022-11-19 15:08:44 +0900

    feat: 1. 게임시작 메시지 출력 2. 다리 길이 입력 메시지 출력, 입력받기 추가

--- src/main/java/bridge/InputView.java
+++ src/main/java/bridge/InputView.java
@@ -1,15 +1,18 @@
 package bridge;
 
+import camp.nextstep.edu.missionutils.Console;
+
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
  */
 public class InputView {
-
     /**
      * 다리의 길이를 입력받는다.
      */
-    public int readBridgeSize() {
-        return 0;
+    public String readBridgeSize() {
+        System.out.println("다리 건너기 게임을 시작합니다.\n");
+        System.out.println("다리의 길이를 입력해주세요.");
+        return Console.readLine().replace(" ", "");
     }
 
     /**
