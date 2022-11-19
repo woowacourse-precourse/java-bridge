ca5fde60ad682e036017b242bc069cf23d31293e
DESKTOP-1ANNTGI\USER <cvc100326@naver.com>
2022-11-20 00:10:03 +0900

    refactor: 클래스 이름 변경 및 개선
    - BridgeMap 에서 MapMaker로 클래스 이름 변경
    - 인스턴스 변수, getter 삭제
    - 메서드 위치 변경
    - makeMap 메서드 반환타입 변경

--- src/main/java/bridge/MapMaker.java
+++ src/main/java/bridge/MapMaker.java
@@ -1,10 +1,11 @@
 package bridge;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.StringJoiner;
 
-public class BridgeMap {
+public class MapMaker {
     private static final String BLANK = " ";
     private static final String SUCCESS_CODE = "O";
     private static final String FAIL_CODE = "X";
@@ -12,14 +13,8 @@ public class BridgeMap {
     private static final String PREFIX_BRACKET = "[";
     private static final String SUFFIX_BRACKET = "]";
 
-    private String top;
-    private String bottom;
 
-    public BridgeMap(List<String> userBridgeHistroy, boolean hasMoved) {
-        makeMap(userBridgeHistroy, hasMoved);
-    }
-
-    private void makeMap(List<String> userBridgeHistroy, boolean hasMoved) {
+    public List<String> makeMap(List<String> userBridgeHistroy, boolean hasMoved) {
         StringBuilder top = new StringBuilder();
         StringBuilder bottom = new StringBuilder();
         for (int i = 0; i < userBridgeHistroy.size(); i++) {
@@ -27,15 +22,25 @@ public class BridgeMap {
         }
         checkResult(top, hasMoved);
         checkResult(bottom, hasMoved);
-        this.top = toMap(top);
-        this.bottom = toMap(bottom);
+        return new ArrayList<>(Arrays.asList(toMap(top),toMap(bottom)));
     }
 
-    public List<String> getMap() {
-        List<String> map = new ArrayList<>();
-        map.add(top);
-        map.add(bottom);
-        return map;
+    private void addHistory(StringBuilder top, StringBuilder bottom, String moving) {
+        if (moving.equals(Direction.Code.UP.getName())) {
+            top.append(SUCCESS_CODE);
+            bottom.append(BLANK);
+        }
+        if (moving.equals(Direction.Code.DOWN.getName())) {
+            top.append(BLANK);
+            bottom.append(SUCCESS_CODE);
+        }
+    }
+
+    private void checkResult(StringBuilder line, boolean didmove) {
+        int endIndex = line.length() - 1;
+        if (!didmove && String.valueOf(line.charAt(endIndex)).equals(SUCCESS_CODE)) {
+            line.replace(endIndex, endIndex + 1, FAIL_CODE);
+        }
     }
 
     private String toMap(CharSequence result) {
@@ -55,24 +60,6 @@ public class BridgeMap {
 
     public String addBracket(CharSequence charSequence, String prefix, String suffix) {
         return prefix + charSequence + suffix;
-    }
-
-    private void addHistory(StringBuilder top, StringBuilder bottom, String moving) {
-        if (moving.equals(Direction.Code.UP.getName())) {
-            top.append(SUCCESS_CODE);
-            bottom.append(BLANK);
-        }
-        if (moving.equals(Direction.Code.DOWN.getName())) {
-            top.append(BLANK);
-            bottom.append(SUCCESS_CODE);
-        }
-    }
-
-    private void checkResult(StringBuilder line, boolean didmove) {
-        int endIndex = line.length() - 1;
-        if (!didmove && String.valueOf(line.charAt(endIndex)).equals(SUCCESS_CODE)) {
-            line.replace(endIndex, endIndex + 1, FAIL_CODE);
-        }
     }
 }
 
