61cb003c7347a5d48bad1b1cf80c9f82e9979a1f
Heebeom Yang <takeny1998@gmail.com>
2022-11-19 16:34:27 +0900

    test: BrideSize domain test

---
+++ src/test/java/bridge/BrideSizeTest.java
@@ -0,0 +1,36 @@
+package bridge;
+
+import bridge.domain.BridgeSize;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+public class BrideSizeTest {
+    @DisplayName("다리 길이에 문자가 있으면 예외가 발생한다. (문자열)")
+    @Test
+    void createBridgeSizeByString() {
+        assertThatThrownBy(() -> new BridgeSize("100!"))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+    @DisplayName("다리 길이에 문자가 있으면 예외가 발생한다. (실수)")
+    @Test
+    void createBridgeSizeByFloat() {
+        assertThatThrownBy(() -> new BridgeSize("23.5"))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("다리 길이가 3부터 20 까지의 숫자가 아니면 예외가 발생한다. (OverFlow)")
+    @Test
+    void createBridgeSizeByOverSize() {
+        assertThatThrownBy(() -> new BridgeSize("23"))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("다리 길이가 3부터 20 까지의 숫자가 아니면 예외가 발생한다. (underFlow)")
+    @Test
+    void createBridgeSizeByUnderSize() {
+        assertThatThrownBy(() -> new BridgeSize("-1"))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+}
