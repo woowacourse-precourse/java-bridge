ef066320cc0c8e89fe1cc92a1de0ee972bb0a124
JUNG-jihoon <73165763+JUNG-jihoon@users.noreply.github.com>
2022-11-19 16:41:12 +0900

    Feat : "추가 사용자 입력 출력"
    사용자가 입력하여 다리의 상태를 볼 수 있도록 OutputView와 printMap() 함수를 main문에 추가하였습니다.

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -3,25 +3,34 @@ package bridge;
 
 import java.util.Arrays;
 
-import static bridge.BridgeGame.Bridge_U;
+import static bridge.BridgeGame.Bridge;
 
 public class Application {
     static InputView input = new InputView();
     static BridgeRandomNumberGenerator bridgeRandomNumberGenerator = new BridgeRandomNumberGenerator();
     static BridgeMaker maker = new BridgeMaker(bridgeRandomNumberGenerator);
     static BridgeGame bridgeGame = new BridgeGame();
+    static OutputView outputView = new OutputView();
+    static int Location = 0;
 
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        init();
+        System.out.println(Bridge);
+        for (int i = 0; i < Bridge.size(); i++) {
+            Bridge_Move();
+        }
+    }
+
+    public static void init() {
         int size = input.readBridgeSize();
-        Bridge_U = maker.makeBridge(size);
-        Bridge_Move();
+        Bridge = maker.makeBridge(size);
     }
 
     public static void Bridge_Move() {
         String Command = input.readGameCommand();
-        int Correct[] = bridgeGame.move(Command);
-        System.out.println(Arrays.toString(Correct));
-
+        outputView.Area_Division(Bridge, Command, Location);
+        Location++;
+        outputView.printMap();
     }
 }
