0784d9d5f0a40000c770ff1ea3fce36609d5fa71
pilyang <30036534+pilyang@users.noreply.github.com>
2022-11-19 23:52:51 +0900

    docs: javadoc 수정

--- src/main/java/bridge/util/ValidateReader.java
+++ src/main/java/bridge/util/ValidateReader.java
@@ -3,9 +3,10 @@ package bridge.util;
 public class ValidateReader {
 
     /**
-     * @param expression
-     * @param <T>
-     * @return
+     * 사용자 입력을 받는 expression 실행 중 예외 발생시 예외가 발생하지 않을때까지 expression을 반복하여 다시 실행
+     *
+     * @param expression Input을 받는 함수 작성
+     * @return expression의 반환 데이터 그대로 return
      */
     public static <T> T readUntilValidate(InputFunction<T> expression) {
         T input = null;
