8a81bfefc515bb3e37f4a05d952cacb0095d02e2
skydreamer21 <skydreamer210@gmail.com>
2022-11-19 17:19:57 +0900

    test(BridgeLengthTest): 다리 길이 domain 유효성 검증 테스트 추가
    [세부사항]
    - 테스트 목록
    1. 유효한 범위의 다리 길이를 생성
    2. 유효하지 않은 범위의 다리 길이를 생성 시 IllegalArgumentException 에러를 발생

---
+++ src/test/java/bridge/domain/BridgeLengthTest.java
@@ -0,0 +1,31 @@
+package bridge.domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+public class BridgeLengthTest {
+    @DisplayName("유효한 범위의 다리 길이를 생성한다.")
+    @ParameterizedTest ( name = "{index}) bridge length = {0}")
+    @ValueSource(ints = { 3, 7, 10, 14, 20 })
+    void createBridgeLength(int length) {
+        BridgeLength bridgeLength = new BridgeLength(length);
+        assertThat(bridgeLength.getLength()).isEqualTo(length);
+    }
+
+    @DisplayName("유효하지 않은 범위의 다리 길이를 생성 시 IllegalArgumentException 에러를 발생시킨다.")
+    @ParameterizedTest ( name = "{index}) bridge length = {0}")
+    @ValueSource(ints = { 1, 2, 21, 100 })
+    void createBridgeLengthWithOutOfRangeLength(int length) {
+        assertThatThrownBy(() -> new BridgeLength(length))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(
+                        "[ERROR]",
+                        "값을 입력하지 않았습니다."
+                );
+    }
+}
