21af0ad689c1368860843badb022ecafd41ce5ee
Nitroblack1 <109022435+Nitroblack1@users.noreply.github.com>
2022-11-19 17:04:26 +0900

    chore(Application) : move Application file to bridge package

--- src/test/java/bridge/ApplicationTest.java
+++ src/test/java/bridge/ApplicationTest.java
@@ -5,7 +5,6 @@ import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.util.Lists.newArrayList;
 
-import bridge.Domain.Application;
 import bridge.Domain.BridgeMaker;
 import camp.nextstep.edu.missionutils.test.NsTest;
 import java.util.List;
