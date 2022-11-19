d102b57fa475957926bd00fbc48f2908cf5713b6
Your Name <you@example.com>
2022-11-19 18:38:10 +0900

    feat(ui) : 사용자에게 다리 길이 입력 받기 구현

--- src/main/java/bridge/ui/InputView.java
+++ src/main/java/bridge/ui/InputView.java
@@ -1,16 +1,11 @@
 package bridge.ui;
 
+import camp.nextstep.edu.missionutils.Console;
+
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
  */
 public class InputView {
-
-    /**
-     * 다리의 길이를 입력받는다.
-     */
-    public int readBridgeSize() {
-        return 0;
-    }
 
     /**
      * 사용자가 이동할 칸을 입력받는다.
@@ -24,5 +19,46 @@ public class InputView {
      */
     public String readGameCommand() {
         return null;
+    }
+
+    /**
+     * 사용자에게 다리의 길이를 입력받는다.
+     */
+    public int readBridgeSize() {
+        String bridgeSizeInput = Console.readLine();
+        validateNumber(bridgeSizeInput);
+        validateBridgeSizeLength(bridgeSizeInput);
+        int bridgeSize = Integer.parseInt(bridgeSizeInput);
+        validateBridgeSizeRange(bridgeSize);
+        return bridgeSize;
+    }
+
+    /**
+     * 검증 로직 : 숫자가 맞는지 검증한다.
+     */
+    private void validateNumber(String inputValue) {
+        for (int i = 0; i < inputValue.length(); ++i) {
+            if (Character.isDigit(inputValue.charAt(i))) {
+                throw new IllegalArgumentException("[ERROR] 다리 길이는 숫자여야 합니다.");
+            }
+        }
+    }
+
+    /**
+     * 검증 로직 : 다리 길이가 일의 자리와 십의 자리 사이인지 검증한다.
+     */
+    private void validateBridgeSizeLength(String bridgeSizeInput) {
+        if (bridgeSizeInput.length() != 1 && bridgeSizeInput.length() != 2) {
+            throw new IllegalArgumentException("[ERROR] 다리 길이는 3부터 20 사이의 숫자여야 합니다.");
+        }
+    }
+
+    /**
+     * 검증 로직 : 다리 길이 범위를 검증한다.
+     */
+    private void validateBridgeSizeRange(int size) {
+        if (size < 3 || size > 20) {
+            throw new IllegalArgumentException("[ERROR] 다리 길이는 3부터 20 사이의 숫자여야 합니다.");
+        }
     }
 }
