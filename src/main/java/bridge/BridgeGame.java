cdffd441c6eef2ed11b0d4bee3f16ed5619c6751
Dongmin Kim <kimdongmin68@naver.com>
2022-11-20 01:55:28 +0900

    feat(bridge): 게임을 재시작하는 경우 기능 구현

--- src/main/java/bridge/BridgeGame.java
+++ src/main/java/bridge/BridgeGame.java
@@ -11,6 +11,8 @@ public class BridgeGame {
     private static final boolean WRONG_BRIDGE = false;
 
     private List<Boolean> movingResult = new LinkedList<>();
+
+    private int tryCount = 1;
     /**
      * 사용자가 칸을 이동할 때 사용하는 메서드
      * <p>
@@ -36,5 +38,7 @@ public class BridgeGame {
      * 재시작을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
     public void retry() {
+        tryCount++;
+        movingResult = new LinkedList<>();
     }
 }
