dd7d2d7daf7b47a2190b680ade2b8034c70bd59b
skck0226 <skck0226@ajou.ac.kr>
2022-11-19 18:15:09 +0900

    [feat] (InputView) 재시도 or 종료 입력

--- src/main/java/bridge/View/InputView.java
+++ src/main/java/bridge/View/InputView.java
@@ -29,6 +29,8 @@ public class InputView {
      * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
      */
     public String readGameCommand() {
-        return null;
+        String gameCommand = Console.readLine().trim();
+        InputValidator.validateGameCommand(gameCommand);
+        return gameCommand;
     }
 }
