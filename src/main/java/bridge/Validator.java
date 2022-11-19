41b4b526235e7617b4049ad027ec44b9a7582844
SeongukBaek <bsu6235@gmail.com>
2022-11-19 17:09:58 +0900

    feat: implement exception handling can be thrown by readMoving()

--- src/main/java/bridge/Validator.java
+++ src/main/java/bridge/Validator.java
@@ -1,10 +1,13 @@
 package bridge;
 
+import java.util.Objects;
 import java.util.regex.Pattern;
 
 public class Validator {
     static final int MIN_SIZE = 3;
     static final int MAX_SIZE = 20;
+    static final String UP = "U";
+    static final String DOWN = "D";
 
     private Validator() {}
 
@@ -26,6 +29,33 @@ public class Validator {
     public static void isInRange(int number) {
         if (MIN_SIZE > number || number > MAX_SIZE) {
             throw new IllegalArgumentException("[ERROR] 다리 길이는 3부터 20 사이의 숫자여야 합니다.");
+        }
+    }
+
+    /**
+     * 입력값이 한 문자가 아니라면 예외를 반환한다.
+     * */
+    public static void isOneString(String input) {
+        if (input.length() != 1) {
+            throw new IllegalArgumentException("[ERROR] 이동할 칸은 한 문자여야 합니다.");
+        }
+    }
+
+    /**
+    * 입력값이 대문자가 아니라면 예외를 반환한다.
+    * */
+    public static void isUppercase(String input) {
+        if (!Character.isUpperCase(input.charAt(0))) {
+            throw new IllegalArgumentException("[ERROR] 이동할 칸은 대문자여야 합니다.");
+        }
+    }
+
+    /**
+     * 입력값이 U 또는 D가 아니라면 예외를 반환한다.
+     * */
+    public static void isUpOrDown(String input) {
+        if (!(input.equals(UP) || input.equals(DOWN))) {
+            throw new IllegalArgumentException("[ERROR] 이동할 칸은 U 또는 D여야 합니다.");
         }
     }
 }
