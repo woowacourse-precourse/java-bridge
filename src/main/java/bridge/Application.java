52695aa52682b4ff11128e3d9c65306d67a3a971
noah.lee <rhkd2612@naver.com>
2022-11-19 15:52:26 +0900

    feat: play one step of bridge game

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -1,8 +1,39 @@
 package bridge;
 
+import java.util.List;
+
 public class Application {
+    final static InputView inputView = new InputView();
+    final static OutputView outputView = new OutputView();
+    final static BridgeRandomNumberGenerator bridgeRandomNumberGenerator = new BridgeRandomNumberGenerator();
+    final static BridgeMaker bridgeMaker = new BridgeMaker(bridgeRandomNumberGenerator);
+
+    final static String errorMessage = "[ERROR]";
 
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        try {
+            List<String> stageBridge = bridgeMaker.makeBridge(inputView.readBridgeSize());
+            BridgeGame bridgeGame = new BridgeGame(stageBridge);
+            playGame(bridgeGame);
+        } catch (IllegalArgumentException e) {
+            System.out.println(errorMessage + e.getMessage());
+        }
     }
+
+    /**
+     * 게임을 한판 진행하는 메서드
+     * @param bridgeGame 현재 진행되는 브릿지게임
+     * @return 유저가 끝까지 도달하였는지 확인 (true : 도달 성공, false : 도달 실패)
+     */
+    private static boolean playGame(final BridgeGame bridgeGame) {
+        for(int i = 0; i < bridgeGame.getBridgeSize(); i++) {
+            boolean moveSuccess = bridgeGame.move(inputView.readMoving());
+            outputView.printMap();
+
+            if(!moveSuccess)
+                return false;
+        }
+        return true;
+    }
+
 }
--- src/main/java/bridge/BridgeGame.java
+++ src/main/java/bridge/BridgeGame.java
@@ -1,16 +1,34 @@
 package bridge;
 
+import java.util.List;
+
 /**
  * 다리 건너기 게임을 관리하는 클래스
  */
 public class BridgeGame {
+    private final List<String> stageBridge;
+    private int currentPosition = 0;
+
+    public BridgeGame(List<String> stageBridge) {
+        this.stageBridge = stageBridge;
+        this.currentPosition = 0;
+    }
+
+    public int getBridgeSize(){
+        return this.stageBridge.size();
+    }
 
     /**
      * 사용자가 칸을 이동할 때 사용하는 메서드
-     * <p>
-     * 이동을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
+     * @param userMove 유저가 입력한 이동 메시지
+     * @throws IllegalStateException 이동할 수 없는데 입력이 들어온 경우
+     * @return 유저가 움직이는데 성공하였는지 반환 true : 이동 성공, false : 이동 실패
      */
-    public void move() {
+    public boolean move(String userMove) {
+        if(currentPosition == stageBridge.size())
+            throw new IllegalStateException("더 이상 이동할 수 없습니다.");
+
+        return stageBridge.get(currentPosition++).equals(userMove);
     }
 
     /**
