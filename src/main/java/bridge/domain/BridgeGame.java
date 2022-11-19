5911bfef8e9606959e001b4c133a8ea1f9a3a288
Isu Shin <isu010614@naver.com>
2022-11-19 19:38:04 +0900

    feat: 가장 최근 저장된 사용자의 이동 상태를 확인하는 기능 구현

---
+++ src/main/java/bridge/domain/BridgeGame.java
@@ -0,0 +1,45 @@
+package bridge.domain;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 다리 건너기 게임을 관리하는 클래스
+ */
+public class BridgeGame {
+    private static final List<String> movingStatus = new ArrayList<>();
+
+    public List<String> getMovingStatus() {
+        return movingStatus;
+    }
+
+    /**
+     * 사용자가 칸을 이동할 때 사용하는 메서드
+     * <p>
+     * 이동을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
+     */
+    public void move(final List<String> randomBridge, final String moving) {
+
+    }
+
+    /**
+     * 사용자가 게임을 다시 시도할 때 사용하는 메서드
+     * <p>
+     * 재시작을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
+     */
+    public void retry() {
+    }
+
+    private boolean isCorrectBridge(final List<String> randomBridge, final String moving) {
+        int firstIndex = 0;
+        int lastIndex = movingStatus.size() - 1;
+        if (movingStatus.isEmpty()) {
+            return compareBridge(randomBridge.get(firstIndex), moving);
+        }
+        return compareBridge(randomBridge.get(lastIndex), moving);
+    }
+
+    private boolean compareBridge(final String bridge, final String moving) {
+        return bridge.equals(moving);
+    }
+}
