a47cebdbada84519a229de914902735c40eb3dcd
hongjk7777 <51441261+hongjk7777@users.noreply.github.com>
2022-11-19 14:37:39 +0900

    feat: 게임 초기화 기능 구현

--- src/main/java/bridge/BridgeGame.java
+++ src/main/java/bridge/BridgeGame.java
@@ -1,14 +1,29 @@
 package bridge;
 
+
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * 다리 건너기 게임을 관리하는 클래스
  */
 public class BridgeGame {
 
+    private static final int START_STEP = 0;
+    private List<String> bridge = new ArrayList<>();
+    private int curStep = START_STEP;
+
+    public void initGame(List<String> bridge) {
+        curStep = START_STEP;
+        this.bridge = bridge;
+    }
+
     /**
      * 사용자가 칸을 이동할 때 사용하는 메서드
      * <p>
      * 이동을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
+     *
+     * @return
      */
     public void move() {
     }
@@ -19,5 +34,13 @@ public class BridgeGame {
      * 재시작을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
     public void retry() {
+    }
+
+    public List<String> getBridge() {
+        return bridge;
+    }
+
+    public int getCurStep() {
+        return curStep;
     }
 }
