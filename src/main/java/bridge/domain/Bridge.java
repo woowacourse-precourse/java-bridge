830587bbebe7ad87ba802ba91584c8ab2b6b0f31
recicoco068 <gkapf838@gmail.com>
2022-11-19 10:15:23 +0900

    feat(Bridge.java) : 다리가 안전한지 확인하는 기능 구현

--- src/main/java/bridge/domain/Bridge.java
+++ src/main/java/bridge/domain/Bridge.java
@@ -8,4 +8,8 @@ public class Bridge {
     public Bridge(List<String> bridge) {
         this.bridge = bridge;
     }
+
+    public boolean isSafe(int step, String command) {
+        return bridge.get(step).equals(command);
+    }
 }
