cc559fd4d4fc846a4c5ee0b0ebb6e716a48d6bcd
iamjooon2 <iamjooon2@gmail.com>
2022-11-20 02:00:42 +0900

    refactor: 다리길이 범위 제어 부분을 bridgeMaker로 이동

--- src/main/java/bridge/BridgeGameController.java
+++ src/main/java/bridge/BridgeGameController.java
@@ -1,21 +1,20 @@
 package bridge;
 
+import bridge.view.InputView;
+import bridge.view.OutputView;
+import java.util.List;
+
 public class BridgeGameController {
     OutputView outputView = new OutputView();
     InputView inputView = new InputView();
+    BridgeRandomNumberGenerator bridgeRandomNumberGenerator = new BridgeRandomNumberGenerator();
+    BridgeMaker bridgeMaker = new BridgeMaker(bridgeRandomNumberGenerator);
 
-    public void startGame(){
+    public void startGame() {
         outputView.printMessage(Message.START);
         outputView.enterLine();
         int bridgeSize = inputView.readBridgeSize();
-        validate(bridgeSize);
+        List<String> bridge = bridgeMaker.makeBridge(bridgeSize);
     }
-
-    public void validate(int bridgeSize) {
-        if (bridgeSize > 20 || bridgeSize < 3) {
-            throw new IllegalArgumentException(Message.ERROR_BRIDGE_SIZE_RANGE.getMessage());
-        }
-    }
-
 
 }
--- src/main/java/bridge/BridgeMaker.java
+++ src/main/java/bridge/BridgeMaker.java
@@ -19,13 +19,22 @@ public class BridgeMaker {
      * @return 입력받은 길이에 해당하는 다리 모양. 위 칸이면 "U", 아래 칸이면 "D"로 표현해야 한다.
      */
     public List<String> makeBridge(int size) {
+        validateBridgeSizeRange(size);
         List<String> bridge = new ArrayList<>();
         for (int i = 0; i < size; i++) {
             int bridgeNumber = bridgeNumberGenerator.generate();
             bridge = addStringByBridgeNumber(bridge, bridgeNumber);
         }
+
         return bridge;
     }
+
+    public void validateBridgeSizeRange(int bridgeSize) {
+        if (bridgeSize > 20 || bridgeSize < 3) {
+            throw new IllegalArgumentException(Message.ERROR_BRIDGE_SIZE_RANGE.getMessage());
+        }
+    }
+
 
     private List<String> addStringByBridgeNumber(List<String> bridge, int bridgeNumber) {
         if (bridgeNumber == 0) {
