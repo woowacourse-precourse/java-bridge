24a572c3296efdf90e9a3216dff03e01cab44b4a
jasonoh22 <jasonoh22@naver.com>
2022-11-19 17:06:41 +0900

    FEAT[java-bridge] 다리 생성

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -1,8 +1,16 @@
 package bridge;
 
+import java.util.*;
+
 public class Application {
 
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        InputView inputView = new InputView();
+        BridgeNumberGenerator BridgeNumberGenerator = null;
+        BridgeMaker BridgeMaker = new BridgeMaker(BridgeNumberGenerator);
+        List<String> Bridge = BridgeMaker.makeBridge(inputView.readBridgeSize());
+        for (int i = 0; i <Bridge.size() ; i++) {
+            System.out.print(Bridge.get(i));
+        }
     }
 }
