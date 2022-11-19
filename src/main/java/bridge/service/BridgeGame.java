fccf6ad44e250d79ef21702e04b287733015266a
sosow0212 <sosow0212@naver.com>
2022-11-19 16:39:15 +0900

    feat : (BridgeGame) 게임 시작을 위한 기능 추가 (초안, 추후 리팩토링 필요)

--- src/main/java/bridge/service/BridgeGame.java
+++ src/main/java/bridge/service/BridgeGame.java
@@ -1,20 +1,53 @@
 package bridge.service;
 
-/**
- * 다리 건너기 게임을 관리하는 클래스
- */
+import bridge.domain.Bridge;
+import bridge.domain.User;
+import bridge.view.OutputView;
+
 public class BridgeGame {
 
-    // 여기서 View를 쓰면 안됨!
-    // 메서드 이름은 변경하면 안됨! / 인자와 반환은 추가 및 변경 가능
-    // 메서드 추가 가능
+    private final InputService inputService = new InputService();
+    private final OutputView outputView = new OutputView();
+
+    public void gameStart(Bridge bridge, User user) {
+        while (!user.isGameDoneStatus()) {
+            move(bridge, user);
+        }
+        outputView.printResult(user);
+    }
 
     /**
      * 사용자가 칸을 이동할 때 사용하는 메서드
      * <p>
      * 이동을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void move() {
+    public void move(Bridge bridge, User user) {
+        String moveDirection = inputService.requestMove();
+        if (moveDirection.equals(bridge.getBridge().get(bridge.getNowIndex()))) {
+            // 맞춘 경우
+            outputView.printMap(bridge.getNowIndex(), moveDirection, true);
+            if (bridge.getNowIndex() == bridge.getBridgeSize() - 1) {
+                // 전부 다 맞춘 경우
+                user.gameDoneSuccess();
+                user.gameSuccess();
+            }
+            bridge.nowIndexUpdate();
+            return;
+        } else {
+            // 틀린 경우
+            outputView.printMap(bridge.getNowIndex(), moveDirection, false);
+            String command = inputService.requestStatusOfGame();
+            if (command.equals("R")) {
+                // 재시도
+                retry(bridge, user);
+                return;
+            } else {
+                // 종료
+                user.gameDoneSuccess();
+                user.gameFail();
+                return;
+            }
+        }
     }
 
     /**
@@ -22,6 +55,9 @@ public class BridgeGame {
      * <p>
      * 재시작을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void retry() {
+    public void retry(Bridge bridge, User user) {
+        outputView.initMapView();
+        bridge.resetGame();
+        user.gameRetry();
     }
 }
