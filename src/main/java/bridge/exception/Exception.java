a2a8c6faf6a0279e394912530cbb93f133d25909
viyott-lover <jackk0616@gmail.com>
2022-11-20 05:28:06 +0900

    feat(Exception): 사용자가 이동할 칸을 입력받을 때 필요한 예외 처리 추가

--- src/main/java/bridge/exception/Exception.java
+++ src/main/java/bridge/exception/Exception.java
@@ -3,7 +3,10 @@ package bridge.exception;
 public enum Exception {
     SIZE_RANGE_EXCEPTION(new IllegalArgumentException("[ERROR] " + "3 ~ 20 사이의 수를 입력해주세요")),
     BRIDGE_NUMBER_RANGE_EXCEPTION(new IllegalArgumentException("[ERROR] " + "0 또는 1이 아닙니다.")),
-    IS_NOT_NUMBER_EXCEPTION(new IllegalArgumentException("[ERROR] " + "입력이 숫자가 아닙니다."));
+    IS_NOT_NUMBER_EXCEPTION(new IllegalArgumentException("[ERROR] " + "입력이 숫자가 아닙니다.")),
+    WRONG_LENGTH_EXCEPTION(new IllegalArgumentException("[ERROR] " + "입력의 길이가 다릅니다.")),
+    IS_NOT_LETTER_EXCEPTION(new IllegalArgumentException("[ERROR] " + "입력이 문자가 아닙니다.")),
+    IS_NOT_COURSE_EXCEPTION(new IllegalArgumentException("[ERROR] " + "U 또는 D를 입력해주세요."));
     private IllegalArgumentException exception;
 
     Exception(IllegalArgumentException exception) {
