2763565e9fea54043c22dfdb41ebbdb7c7ea9e11
thdwoqor <celebarte01@gmail.com>
2022-11-19 17:24:42 +0900

    feat: 다리 길이가 유효한 범위 값인지 확인하는 기능 추가

--- src/main/java/bridge/BridgeMaker.java
+++ src/main/java/bridge/BridgeMaker.java
@@ -1,17 +1,19 @@
 package bridge;
 
+import bridge.exception.Validator;
+import bridge.model.Result;
+
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
-
-import static bridge.Bridge.getSymbol;
 
 /**
  * 다리의 길이를 입력 받아서 다리를 생성해주는 역할을 한다.
  */
 public class BridgeMaker {
-
     private final BridgeNumberGenerator bridgeNumberGenerator;
+    private final int startInclusive=3;
+    private final int endInclusive=20;
 
     public BridgeMaker(BridgeNumberGenerator bridgeNumberGenerator) {
         this.bridgeNumberGenerator = bridgeNumberGenerator;
@@ -22,8 +24,9 @@ public class BridgeMaker {
      * @return 입력받은 길이에 해당하는 다리 모양. 위 칸이면 "U", 아래 칸이면 "D"로 표현해야 한다.
      */
     public List<String> makeBridge(int size) {
+        Validator.inRange(size,startInclusive,endInclusive);
         return IntStream.range(0, size)
-                .mapToObj(e->getSymbol(bridgeNumberGenerator.generate()))
+                .mapToObj(e -> Result.getSymbol(bridgeNumberGenerator.generate()))
                 .collect(Collectors.toList());
     }
 }
