b56bcc0b98879ddf6bb256e54321c26a6edddbce
Devtist <yt930310@snu.ac.kr>
2022-11-20 00:38:44 +0900

    refactor: 상수 선언된 에러 메세지들로 리팩터링

--- src/main/java/util/Validator.java
+++ src/main/java/util/Validator.java
@@ -2,8 +2,8 @@ package util;
 
 import java.util.List;
 
-import bridgeConstant.Cell;
 import bridgeConstant.Constant;
+import bridgeConstant.ErrorLog;
 
 public class Validator {
 	private static final String NUMBER_REGEX = "\\d+";
@@ -13,37 +13,38 @@ public class Validator {
 
 	public static void validateBridgeSize(List<String> bridge, int size) {
 		if (bridge.size() != size) {
-			throw new IllegalArgumentException("[ERROR] 다리의 길이가 입력받은 길이와 같지 않습니다.");
+			throw new IllegalArgumentException(ErrorLog.LENGTH_IS_NOT_MATCHED_EXCEPTION.log());
 		}
 	}
 
 	public static void validateBridgeSizeRange(int size) {
 		if (size < SIZE_LOWER_INCLUSIVE || size > SIZE_UPPER_INCLUSIVE) {
-			throw new IllegalArgumentException("[ERROR] 다리의 길이는 3부터 20 사이여야 합니다.");
+			throw new IllegalArgumentException(ErrorLog.INVALID_LENGTH_RANGE_EXCEPTION.log());
 		}
 	}
 
 	public static void validateSizeIsNaturalNumber(String number) {
 		if (!number.matches(NUMBER_REGEX)) {
-			throw new IllegalArgumentException("[ERROR] 다리의 길이는 자연수여야 합니다.");
+			throw new IllegalArgumentException(ErrorLog.NOT_NATURAL_NUMBER_EXCEPTION.log());
 		}
 	}
 
 	public static void validateMovingIsCellPosition(String moving) {
 		if (!moving.equals(Constant.UPPER_POSITION) && !moving.equals(Constant.LOWER_POSITION)) {
-			throw new IllegalArgumentException("[ERROR] 입력은 U와 D 둘 중 하나여야 합니다.");
+			throw new IllegalArgumentException(ErrorLog.INVALID_MOVING_INPUT_EXCEPTION.log());
 		}
 	}
 
 	public static void validateGameCommand(String gameCommand) {
 		if (!gameCommand.equals(Constant.RESTART) && !gameCommand.equals(Constant.QUIT)) {
-			throw new IllegalArgumentException("[ERROR] 입력은 R과 Q 둘 중 하나여야 합니다.");
+			throw new IllegalArgumentException(ErrorLog.INVALID_GAME_COMMAND_EXCEPTION.log());
 		}
 	}
 
 	public static void validateBridgeComponent(List<String> bridge) {
-		if (bridge.stream().anyMatch(cell -> !cell.equals(Constant.UPPER_POSITION) && !cell.equals(Constant.LOWER_POSITION))) {
-			throw new IllegalArgumentException("[ERROR] 다리는 U와 D로 이루어져야 합니다.");
+		if (bridge.stream()
+			.anyMatch(cell -> !cell.equals(Constant.UPPER_POSITION) && !cell.equals(Constant.LOWER_POSITION))) {
+			throw new IllegalArgumentException(ErrorLog.INVALID_BRIDGE_COMPONENT_EXCEPTION.log());
 		}
 	}
 }
