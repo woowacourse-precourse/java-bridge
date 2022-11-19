1d206bcb87788a5d2c2c8e453de223c12c02052a
jiwon <ljwon77@daum.net>
2022-11-20 03:03:35 +0900

    feat: add bridge map const variable
    다리 맵을 출력하기 위한 상수 변수를 추가했습니다.

--- src/main/java/bridge/constant/BridgeConst.java
+++ src/main/java/bridge/constant/BridgeConst.java
@@ -6,6 +6,10 @@ public enum BridgeConst {
     , FAILURE("실패")
     , POSSIBLE("O")
     , IMPOSSIBLE("X")
+    , MAP_START("[")
+    , MAP_END("]")
+    , MAP_SEPARATOR("|")
+    , MAP_BLANK(" ")
     ;
 
     private final String identifier;
