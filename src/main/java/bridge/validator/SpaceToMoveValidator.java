f4c483da7dc11e4241c531a0a74e76749cc4dc44
nakalic <nakalic28@gmail.com>
2022-11-20 01:34:29 +0900

    fix(SpaceToMoveValidator): 이동할 칸 입력을 잘못 검증하는 버그 수정

--- src/main/java/bridge/validator/SpaceToMoveValidator.java
+++ src/main/java/bridge/validator/SpaceToMoveValidator.java
@@ -5,7 +5,7 @@ public class SpaceToMoveValidator {
     public static final String NEXT_SPACE_IS_U_OR_D = "[ERROR] 이동할 칸은 위 : U, 아래 : D로 입력해주세요.";
 
     public static void validateSpaceToMove(String nextSpace) {
-        if (nextSpace != "U" || nextSpace != "D") {
+        if (!nextSpace.equals("U") && !nextSpace.equals("D")) {
             throw new IllegalArgumentException(NEXT_SPACE_IS_U_OR_D);
         }
     }
