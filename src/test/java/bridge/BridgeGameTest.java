206158136c3fead4e7e9d6f2ee8e56867ddc169d
JeonSangYong <tkddyd420@naver.com>
2022-11-19 21:59:50 +0900

    test : 게임 재시작 테스트

--- src/test/java/bridge/BridgeGameTest.java
+++ src/test/java/bridge/BridgeGameTest.java
@@ -17,7 +17,7 @@ class BridgeGameTest {
         //then
         assertThat(bridgeGame.isGameEnd()).isFalse();
     }
-    
+
     @Test
     void 다리_이동_실패() {
         //given
@@ -26,5 +26,16 @@ class BridgeGameTest {
         bridgeGame.move("D");
         //then
         assertThat(bridgeGame.isGameEnd()).isTrue();
+    }
+
+    @Test
+    void 게임_재시작() {
+        //given
+        BridgeGame bridgeGame = new BridgeGame(List.of("U", "U", "U"));
+        //when
+        bridgeGame.retry("R");
+        //then
+        assertThat(bridgeGame.getGameCount()).isEqualTo(2);
+        assertThat(bridgeGame.isGameEnd()).isFalse();
     }
 }
