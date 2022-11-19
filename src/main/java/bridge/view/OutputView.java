2dc64bded5abbc9080038dc69eff5a2352d75305
Lee Min Woo <lmw7414@naver.com>
2022-11-20 02:37:05 +0900

    fix : OutputView 문자열 비교 부분 수정
    975c9be 와 같이 문자열 비교하는 부분을 equals 메서드를 사용하는 것으로 수정하였다. 또한 불필요한 스트링 메시지를 상수를 활용하여 하드코딩하지 않도록 하였다.

--- src/main/java/bridge/view/OutputView.java
+++ src/main/java/bridge/view/OutputView.java
@@ -5,6 +5,9 @@ import bridge.view.utils.CustomStringBuilderForBridge;
 
 import java.util.List;
 
+import static bridge.constant.Constants.SUCCESS_OR_NOT_MESSAGE;
+import static bridge.constant.Constants.TOTAL_NUMBER_OF_ATTEMPTS_MESSAGE;
+
 /**
  * 사용자에게 게임 진행 상황과 결과를 출력하는 역할을 한다.
  */
@@ -30,8 +33,8 @@ public class OutputView {
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
     public void printResult(String result, int attempt) {
-        System.out.print("게임 성공 여부: " + result);
-        System.out.println("총 시도한 횟수: " + attempt);
+        System.out.println(SUCCESS_OR_NOT_MESSAGE + result);
+        System.out.println(TOTAL_NUMBER_OF_ATTEMPTS_MESSAGE + attempt);
     }
 
     public static void printGameMessage(String message) {
@@ -44,9 +47,9 @@ public class OutputView {
 
     private void compareUserAndAnswer(Bridge bridge, List<String> userAnswer, CustomStringBuilderForBridge stringBuilderBridge) {
         for (int i = 0; i < userAnswer.size(); i++) {
-            if (bridge.getBridge().get(i) == userAnswer.get(i)) {
+            if (bridge.getBridge().get(i).equals(userAnswer.get(i))) {
                 stringBuilderBridge.addO(userAnswer.get(i));
-            } else if (bridge.getBridge().get(i) != userAnswer.get(i)) {
+            } else if (!bridge.getBridge().get(i).equals(userAnswer.get(i))) {
                 stringBuilderBridge.addX(userAnswer.get(i));
             }
             if (i != userAnswer.size() - 1) {
@@ -58,6 +61,7 @@ public class OutputView {
     private void printUpperAndLowerBridge(CustomStringBuilderForBridge stringBuilderBridge) {
         System.out.println(stringBuilderBridge.getUpperBridge());
         System.out.println(stringBuilderBridge.getLowerBridge());
+        System.out.println();
     }
-    
+
 }
