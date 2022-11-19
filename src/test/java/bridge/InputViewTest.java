982493d60b89d06ec44b823102669536d714c4cd
jsyeo97 <jsyeo97@naver.com>
2022-11-19 19:24:26 +0900

    test: 사용자가 입력한 문자가 U와D 중 하나인지 테스트하는 코드 구현

--- src/test/java/bridge/InputViewTest.java
+++ src/test/java/bridge/InputViewTest.java
@@ -7,11 +7,20 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class InputViewTest {
     @Test
-    @DisplayName("size 3~20 사이의 수인지 테스트")
+    @DisplayName("size가 3~20 사이의 수인지 테스트")
     void validSizeTest() {
         assertThat(InputView.isValidSize(3)).isTrue();
         assertThat(InputView.isValidSize(20)).isTrue();
         assertThat(InputView.isValidSize(2)).isFalse();
         assertThat(InputView.isValidSize(21)).isFalse();
+    }
+
+    @Test
+    @DisplayName("사용자가 입력한 문자가 U와D 중 하나인지 테스트")
+    void validDirectionTest() {
+        assertThat(InputView.isValidDirection("U")).isTrue();
+        assertThat(InputView.isValidDirection("D")).isTrue();
+        assertThat(InputView.isValidDirection("")).isFalse();
+        assertThat(InputView.isValidDirection("A")).isFalse();
     }
 }
