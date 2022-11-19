e0e212b29e591efc670e85f0374559e3f8436be0
Hoo93 <117077999+Hoo93@users.noreply.github.com>
2022-11-19 22:18:25 +0900

    FEAT[BrigeGame]: 여러 칸을 움직이는 move() 메소드 구현

--- src/main/java/bridge/BridgeGame.java
+++ src/main/java/bridge/BridgeGame.java
@@ -25,12 +25,17 @@ public class BridgeGame {
         this.inputView = inputView;
         this.outputView = outputView;
     }
-    /**
-     * 사용자가 칸을 이동할 때 사용하는 메서드
-     * <p>
-     * 이동을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
-     */
-    public void move() {
+
+    public boolean move() {
+        for (int index = 0; index < bridge.size(); index++) {
+            if (!moveOne(index)) {
+                outputView.printMap(upperLine, lowerLine);
+                return false;
+            }
+            outputView.printMap(upperLine,lowerLine);
+        }
+        outputView.printMap(upperLine,lowerLine);
+        return true;
     }
 
     public boolean moveOne(int index) {
