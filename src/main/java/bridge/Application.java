6b6749efb498ecf683a540a7be4d6b031f2d5c51
Sojeong <k5k5s5s5j5j5@naver.com>
2022-11-19 21:28:19 +0900

    feat(main/BridgeGame): 맞게 이동할 경우 다시 입력값을 받는 기능 추가
    - BridgeGame.move(): 값을 입력받아 이동하고 결과를 출력, 맞게 이동했는지를 boolean 값으로 리턴

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -1,10 +1,15 @@
 package bridge;
 
 import bridge.domain.BridgeGame;
+
 public class Application {
 
     public static void main(String[] args) {
         BridgeGame bridgeGame = new BridgeGame();
         bridgeGame.start();
+        boolean correct = bridgeGame.move();
+        while (correct) {
+            correct = bridgeGame.move();
+        }
     }
 }
--- src/main/java/bridge/domain/BridgeGame.java
+++ src/main/java/bridge/domain/BridgeGame.java
@@ -25,22 +25,26 @@ public class BridgeGame {
         position = 0;
     }
 
+    public String getMoving(){
+        return inputView.readMoving();
+    }
+
+    public boolean checkMoving(String moving) {
+        return bridge.get(position).equals(moving);
+    }
+
     /**
      * 사용자가 칸을 이동할 때 사용하는 메서드
      * <p>
      * 이동을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void move() {
-        String moving = inputView.readMoving();
-        boolean correct = bridge.get(position).equals(moving);
+    public boolean move() {
+        boolean correct = checkMoving(getMoving());
+        outputView.printMap(bridge, position, correct);
         if (correct) {
-            outputView.printMap(bridge, position, correct);
             position++;
-        } else if (!correct) {
-            outputView.printMap(bridge, position, correct);
         }
-
-
+        return correct;
     }
 
     /**
