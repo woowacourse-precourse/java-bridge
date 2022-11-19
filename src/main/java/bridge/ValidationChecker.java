4256db8990e4f8672433193cbfa945317809fe58
Sunghyun-0 <zkdldk95@naver.com>
2022-11-19 19:19:07 +0900

    ValidationChecker Class 생성

---
+++ src/main/java/bridge/ValidationChecker.java
@@ -0,0 +1,25 @@
+package bridge;
+
+import java.util.List;
+
+/**
+ * 값이 유효한지 확인하는 역할을 한다.
+ */
+public class ValidationChecker {
+    /**
+     * 숫자가 범위 내에 있는지 확인한다.
+     */
+    public void checkIntegerRange(int target, int start, int end){return;}
+
+    /**
+     * 입력 값이 숫자로 이루어져 있는지 확인한다.
+     */
+    public void checkInputIsInteger(String target){return;}
+
+    /**
+     * 입력 값이 특정 문자로 이루어져 있는지 확인한다.
+     */
+    public void checkInputIsSpecificString(String target, List<String> specificString){return;}
+
+
+}
