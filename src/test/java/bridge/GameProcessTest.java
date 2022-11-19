b1f66811dcee22b064bc33f2eb276ab1bf444dd5
Jzakka <mouse4786@gmail.com>
2022-11-19 20:24:44 +0900

    test: 테스트 케이스 추가

--- src/test/java/bridge/GameProcessTest.java
+++ src/test/java/bridge/GameProcessTest.java
@@ -4,11 +4,8 @@ import camp.nextstep.edu.missionutils.test.NsTest;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import java.util.List;
-
 import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeTest;
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
 
 class GameProcessTest extends NsTest {
     ApplicationContext context = new ApplicationContext();
@@ -22,7 +19,7 @@ class GameProcessTest extends NsTest {
     }
 
     @Test
-    void 게임_진행_테스트() {
+    void 게임_진행_클리어() {
         assertRandomNumberInRangeTest(() -> {
             run("x", "7", "U", "D", "D", "R",
                     "U", "D", "U", "U", "-", "R",
@@ -36,8 +33,29 @@ class GameProcessTest extends NsTest {
                     "총 시도한 횟수: 3"
             );
 
-            int upSideIndex = output().indexOf("[ O |   | O ]");
-            int downSideIndex = output().indexOf("[   | O |   ]");
+            int upSideIndex = output().indexOf("[ O |   | O |   | O |   |   ]");
+            int downSideIndex = output().indexOf("[   | O |   | O |   | O | O ]");
+            assertThat(upSideIndex).isLessThan(downSideIndex);
+        }, 1, 0, 1, 0, 1, 0, 0);
+    }
+
+    @Test
+    void 게임_진행_실패() {
+        assertRandomNumberInRangeTest(() -> {
+            run("x", "7", "U", "D", "D", "R",
+                    "U", "D", "U", "U", "-", "Q",
+                    "a", "b", "U", "D", "U", "D", "U", "D", "D");
+            assertThat(output()).contains(
+                    ERROR_MESSAGE,
+                    "최종 게임 결과",
+                    "[ O |   | O | X ]",
+                    "[   | O |   |   ]",
+                    "게임 성공 여부: 실패",
+                    "총 시도한 횟수: 2"
+            );
+
+            int upSideIndex = output().indexOf("[ O |   | O | X ]");
+            int downSideIndex = output().indexOf("[   | O |   |   ]");
             assertThat(upSideIndex).isLessThan(downSideIndex);
         }, 1, 0, 1, 0, 1, 0, 0);
     }
@@ -45,19 +63,5 @@ class GameProcessTest extends NsTest {
     @Override
     protected void runMain() {
         Application.main(new String[]{});
-    }
-
-    static class TestNumberGenerator implements BridgeNumberGenerator {
-
-        private final List<Integer> numbers;
-
-        TestNumberGenerator(List<Integer> numbers) {
-            this.numbers = numbers;
-        }
-
-        @Override
-        public int generate() {
-            return numbers.remove(0);
-        }
     }
 }
