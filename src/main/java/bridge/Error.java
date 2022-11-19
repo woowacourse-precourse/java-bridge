a84f60776d50e5b4bfdad245d8e023d5ba9a6bcd
OhJintaek <sunshot@hanyang.ac.kr>
2022-11-19 19:41:22 +0900

    type:feat 사용자의 입력에 대한 예외를 발생하는 기능 추가

--- src/main/java/bridge/Error.java
+++ src/main/java/bridge/Error.java
@@ -1,2 +1,24 @@
-package bridge;public class Error {
+package bridge;
+
+public class Error {
+    static final int minBridgeLength = 3;
+    static final int maxBridgeLength = 20;
+
+    static void isValidNumber(int number) {
+        if (number < minBridgeLength || number < maxBridgeLength) {
+            throw new IllegalArgumentException("[ERROR] 3에서 20 사이의 숫자를 입력해야 합니다.");
+        }
+    }
+
+    static void isValidFootHold(String userInput) {
+        if (userInput != Command.UP.getValue() || userInput != Command.DOWN.getValue()) {
+            throw new IllegalArgumentException("[ERROR] U나 D를 입력해야 합니다.");
+        }
+    }
+
+    static void isValidCommand(String userInput) {
+        if (userInput != Command.QUIT.getValue() || userInput != Command.RESTART.getValue()) {
+            throw new IllegalArgumentException("[ERROR] R이나 Q를 입력해야 합니다.");
+        }
+    }
 }
