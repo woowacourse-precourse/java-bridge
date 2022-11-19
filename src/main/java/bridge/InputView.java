7d77324f9e1e4deadfe08c69ca193be507cef470
이정우 <acg6138@naver.com>
2022-11-19 23:17:38 +0900

    fix(InputView): 다리 길이 검증 로직 오류 수정

--- src/main/java/bridge/InputView.java
+++ src/main/java/bridge/InputView.java
@@ -25,26 +25,26 @@ public class InputView {
         return input;
     }
 
-    public void validateBridgeSize(String input) {
-        if (!input.matches("^[0-9][1,2]") && !isInRange(Integer.parseInt(input))) {
+    public static void validateBridgeSize(String input) {
+        if (!input.matches("^[0-9]+") || !isInRange(Integer.parseInt(input))) {
             throw new IllegalArgumentException("[ERROR] 다리 길이는 3부터 20사이의 숫자여야 합니다.");
         }
     }
 
-    public boolean isInRange(int input) {
-        if (input < 3 && input > 20) {
-            return false;
+    public static boolean isInRange(int input) {
+        if (input >= 3 && input <= 20) {
+            return true;
         }
-        return true;
+        return false;
     }
 
-    public void validateMoveInput(String input) {
+    public static void validateMoveInput(String input) {
         if (!input.equals("U") && !input.equals("D")) {
             throw new IllegalArgumentException("[ERROR] 이동할 칸은 U 또는 D만 입력할 수 있습니다.");
         }
     }
 
-    public void validateCommand(String input) {
+    public static void validateCommand(String input) {
         if (!input.equals("Q") && !input.equals("R")) {
             throw new IllegalArgumentException("[ERROR] 게임 재시작 여부는 R 또는 Q만 입력할 수 있습니다.");
         }
