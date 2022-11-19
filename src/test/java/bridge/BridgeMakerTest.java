8a8fff76a73b1ef4717dd71bea3c7bcbb07987ca
NotableDeveloper <castlereal@naver.com>
2022-11-19 07:32:21 -0800

    <test> : 다리의 내용 검증 추가 생성한 다리가 올바른 값을 갖는 지를 검증하는 내용을 추가함.

--- src/test/java/bridge/BridgeMakerTest.java
+++ src/test/java/bridge/BridgeMakerTest.java
@@ -2,14 +2,12 @@ package bridge;
 
 import bridge.BridgeMaker.BridgeMaker;
 import bridge.BridgeMaker.NumberGenerator.BridgeNumberGenerator;
-import bridge.BridgeMaker.NumberGenerator.BridgeRandomNumberGenerator;
+import org.assertj.core.util.Lists;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.util.Lists.newArrayList;
 
 public class BridgeMakerTest {
     static class TestNumberGenerator implements BridgeNumberGenerator {
@@ -24,14 +22,14 @@ public class BridgeMakerTest {
             return numbers.remove(0);
         }
     }
-
     @DisplayName("길이가 3인 다리를 생성하는 메서드를 호출하면 그 결과로 길이가 3인 다리가 생성되어야 한다.")
     @Test
     void makeBridgeSize3ToSuccessful() {
-        BridgeNumberGenerator numberGenerator = new TestNumberGenerator(newArrayList(1, 0, 0));
-        BridgeMaker bridgeMaker = new BridgeMaker(numberGenerator);
+        BridgeNumberGenerator generator = new TestNumberGenerator(Lists.newArrayList(1, 0, 0));
+        BridgeMaker bridgeMaker = new BridgeMaker(generator);
         List<String> bridge = bridgeMaker.makeBridge(3);
         assertThat(bridge.size() == 3);
+        assertThat(bridge).containsExactly("D", "U", "U");
     }
 }
 
