9885714ffbbf8477b7d8e3e7b35a7ae7e62d4e1b
yummygyudon <bang2brew@gmail.com>
2022-11-19 11:34:25 +0900

    FEAT(domain.calculator): 플레이어 입력 값 이동 가능 여부 계산 기능 구현
    - 클래스 명 : BridgeCalculator
    - 필드 : crossableBridge (정답 Bridge 객체)
    - 생성자
      - 매개변수 : crossableBridge (정답 Bridge 객체)
    - 메서드
      - 이름 : `isCrossable()`
      - 매개변수
        1. gameRound (게임 회차)
        2. playerInput (사용자가 입력한 이동 값)
      - 처리
        : 해당 gameRound에 건널 수 있는 값을 crossableBridge에서 추출하여
        playerInput값과 동일한지 비교한 결과를 반환
        (일치하지 않으면 건널 수 없기 때문에 false 반환)

---
+++ src/main/java/bridge/domain/calculator/BridgeCalculator.java
@@ -0,0 +1,15 @@
+package bridge.domain.calculator;
+
+import java.util.List;
+
+public class BridgeCalculator {
+    private final List<String> crossableBridge ;
+
+    public BridgeCalculator(List<String> crossableBridge){
+        this.crossableBridge = crossableBridge;
+    }
+
+    public boolean isCrossable(int gameRound, String playerInput){
+        return crossableBridge.get(gameRound).equals(playerInput);
+    }
+}
