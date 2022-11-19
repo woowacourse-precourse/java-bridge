5bdbaefd41e69767666d86500711dc3003d32dc2
Jaesun Park <41979078+ppakjae@users.noreply.github.com>
2022-11-19 22:57:28 +0900

    feat: 다리 건너기 게임을 시작하는 기능 구현

--- src/main/java/bridge/BridgeGame.java
+++ src/main/java/bridge/BridgeGame.java
@@ -4,6 +4,10 @@ package bridge;
  * 다리 건너기 게임을 관리하는 클래스
  */
 public class BridgeGame {
+    
+    public void startGame() {
+        System.out.println("다리 건너기 게임을 시작합니다.\n");
+    }
 
     /**
      * 사용자가 칸을 이동할 때 사용하는 메서드
