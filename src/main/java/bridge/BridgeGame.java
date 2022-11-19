7dfa647d6b6b872d767931325d9da43fb65af4d0
JeonSangYong <tkddyd420@naver.com>
2022-11-19 15:47:44 +0900

    refactor : 하드코딩한 값을 상수로 변경

--- src/main/java/bridge/BridgeGame.java
+++ src/main/java/bridge/BridgeGame.java
@@ -10,12 +10,24 @@ public class BridgeGame {
 
     private final static String SUCCESS = "성공";
     private final static String FAIL = "실패";
+    private final static String RESULT_SUCCESS = "O";
+    private final static String RESULT_FAIL = "X";
+    private final static String UP = "U";
+    private final static String RETRY = "R";
+    private final static String BLANK_SPACE = " ";
+
 
     private final List<List<String>> result = new ArrayList<>();
     private final List<String> bridge;
     private boolean gameEnd = false;
     private int movingCount = 0;
     private int gameCount = 1;
+
+    public BridgeGame(List<String> bridge) {
+        this.bridge = bridge;
+        result.add(new ArrayList<>());
+        result.add(new ArrayList<>());
+    }
 
     /**
      * 사용자가 칸을 이동할 때 사용하는 메서드
@@ -40,29 +52,29 @@ public class BridgeGame {
     }
 
     private void addMovingResult(String moving) {
-        if (moving.equals("U")) {
-            addLetterToFirstFloor("O");
+        if (moving.equals(UP)) {
+            addLetterToFirstFloor(RESULT_SUCCESS);
             return;
         }
-        addLetterToSecondFloor("O");
+        addLetterToSecondFloor(RESULT_SUCCESS);
     }
 
     private void addLetterToFirstFloor(String letter) {
         result.get(0).add(letter);
-        result.get(1).add(" ");
+        result.get(1).add(BLANK_SPACE);
     }
 
     private void addLetterToSecondFloor(String letter) {
-        result.get(0).add(" ");
+        result.get(0).add(BLANK_SPACE);
         result.get(1).add(letter);
     }
 
     private void addFailMovingResult(String moving) {
-        if (moving.equals("U")) {
-            addLetterToFirstFloor("X");
+        if (moving.equals(UP)) {
+            addLetterToFirstFloor(RESULT_FAIL);
             return;
         }
-        addLetterToSecondFloor("X");
+        addLetterToSecondFloor(RESULT_FAIL);
     }
 
     /**
@@ -71,7 +83,7 @@ public class BridgeGame {
      * 재시작을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
     public boolean retry(String gameCommand) {
-        if (gameCommand.equals("R")) {
+        if (gameCommand.equals(RETRY)) {
             gameCount++;
             movingCount = 0;
             gameEnd = false;
@@ -91,12 +103,6 @@ public class BridgeGame {
 
     public int getGameCount() {
         return gameCount;
-    }
-
-    public BridgeGame(List<String> bridge) {
-        this.bridge = bridge;
-        result.add(new ArrayList<>());
-        result.add(new ArrayList<>());
     }
 
     public boolean isFinishBridge() {
