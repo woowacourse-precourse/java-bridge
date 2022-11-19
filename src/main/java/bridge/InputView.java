4db7a111187dec416e550c5583e8ee8059f6e7dc
BoyCho <iijokv@naver.com>
2022-11-20 03:40:32 +0900

    feat : 사용자 입력 예외 처리 추가  - 다리 길이 3 미만 20 초과의 숫자 입력 시 예외 처리  - 이동할 칸 U 또는 D 외에 입력 시 예외 처리  - 재시도 여부 R 또는 Q 외에 입력 시 예외 처리

--- src/main/java/bridge/InputView.java
+++ src/main/java/bridge/InputView.java
@@ -11,7 +11,13 @@ public class InputView {
      */
     public int readBridgeSize() {
         System.out.println("다리의 길이를 입력해주세요.");
-        return Integer.parseInt(Console.readLine());
+        return checkBridgeSize(Integer.parseInt(Console.readLine()));
+    }
+
+    public int checkBridgeSize(int bridgeSize) {
+        if (bridgeSize < 3 || 20 < bridgeSize)
+            throw new IllegalArgumentException("[ERROR] 3 이상 20 이하의 숫자를 입력하여야 합니다.");
+        return bridgeSize;
     }
 
     /**
@@ -19,7 +25,13 @@ public class InputView {
      */
     public String readMoving() {
         System.out.println("이동할 칸을 선택해주세요. (위: U, 아래: D)");
-        return Console.readLine();
+        return checkMoving(Console.readLine());
+    }
+
+    public String checkMoving(String moving) {
+        if (!moving.equals("U") && !moving.equals("D"))
+            throw new IllegalArgumentException("[ERROR] U 와 D 중 하나의 문자를 입력하여야 합니다.");
+        return moving;
     }
 
     /**
@@ -27,6 +39,12 @@ public class InputView {
      */
     public String readGameCommand() {
         System.out.println("게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)");
-        return Console.readLine();
+        return checkGameCommand(Console.readLine());
+    }
+
+    public String checkGameCommand(String gameCommand) {
+        if (!gameCommand.equals("R") && !gameCommand.equals("Q"))
+            throw new IllegalArgumentException("[ERROR] R 와 Q 중 하나의 문자를 입력하여야 합니다.");
+        return gameCommand;
     }
 }
