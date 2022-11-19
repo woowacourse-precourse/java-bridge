714350769f53d13330e03198940b62a39a9c0925
impati <impati0716@icloud.com>
2022-11-19 14:35:00 +0900

    feat(CurrentBridge) : 진행중인 다리

---
+++ src/main/java/bridge/CurrentBridge.java
@@ -0,0 +1,10 @@
+package bridge;
+
+import java.util.List;
+
+public class CurrentBridge extends Bridge{
+    private BridgeStatus bridgeStatus;
+    public CurrentBridge(List<String> bridge) {
+        super(bridge);
+    }
+}
