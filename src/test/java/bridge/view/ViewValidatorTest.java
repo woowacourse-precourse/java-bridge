99270a7ecd3431e2c6476df2486bf96900ce4469
skydreamer21 <skydreamer210@gmail.com>
2022-11-19 22:00:49 +0900

    test(ViewValidatorTest): 이동할 칸의 입력 유효성 검증 테스트 추가
    [세부사항]
    - 테스트 목록
    1. 올바른 칸의 입력 (U, D)에 대해 에러를 발생시키지 않음.
    2. 움직일 칸의 입력으로 비어있는 값을 입력했을 때 IllegalArgumentException 에러를 발생
    3. 올바르지 않은 칸의 입력에 대해 IllegalArgumentException 에러를 발생

--- src/test/java/bridge/view/ViewValidatorTest.java
+++ src/test/java/bridge/view/ViewValidatorTest.java
@@ -1,10 +1,11 @@
 package bridge.view;
 
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.assertj.core.api.Assertions.*;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 public class ViewValidatorTest {
     @DisplayName("비어있는 값을 입력했을 때 IllegalArgumentException 에러를 발생시킨다.")
@@ -28,6 +29,38 @@ public class ViewValidatorTest {
                 .hasMessageContaining(
                         "[ERROR]",
                         "숫자만 입력해주세요."
+                );
+    }
+
+    @DisplayName("올바른 칸의 입력 (U, D)에 대해 에러를 발생시키지 않는다.")
+    @ParameterizedTest(name = "{index}) Panel Input = {0}")
+    @ValueSource(strings = { "U", "D" })
+    void validatePanelInput(String input) {
+        assertThatCode(() -> ViewValidator.validatePanelInput(input))
+                .doesNotThrowAnyException();
+    }
+
+    @DisplayName("움직일 칸의 입력으로 비어있는 값을 입력했을 때 IllegalArgumentException 에러를 발생시킨다.")
+    @Test
+    void validatePanelInputWithEmptyValue() {
+        String input = "";
+        assertThatThrownBy(() -> ViewValidator.validatePanelInput(input))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(
+                        "[ERROR]",
+                        "값을 입력하지 않았습니다."
+                );
+    }
+
+    @DisplayName("올바르지 않은 칸의 입력에 대해 IllegalArgumentException 에러를 발생시킨다.")
+    @ParameterizedTest(name = "{index}) Panel Input = {0}")
+    @ValueSource(strings = { "E", "R", "Q", "1", "u", "d" })
+    void validatePanelInputWithInvalidValue(String input) {
+        assertThatThrownBy(() -> ViewValidator.validatePanelInput(input))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(
+                        "[ERROR]",
+                        "U(위), D(아래) 중 하나만 입력해주세요."
                 );
     }
 }
