46fb5c331fe1e557e72de4a04f1457c82d0b4805
YuHayeon <youha0930@gmail.com>
2022-11-19 14:24:51 +0900

    feat(Bridge): 입력받은 길이만큼 다리를 생성하여 저장
    입력받은 길이를 검증 후 다리를 생성하여 저장

--- src/main/java/bridge/model/Bridge.java
+++ src/main/java/bridge/model/Bridge.java
@@ -1,8 +1,18 @@
 package bridge.model;
 
+import bridge.BridgeMaker;
+import bridge.BridgeRandomNumberGenerator;
 import bridge.exception.BridgeSizeException;
+import java.util.List;
 
 public class Bridge {
+
+    private List<String> bridge;
+
+    public void make(String size) {
+        int validatedSize = validateSize(size);
+        this.bridge = new BridgeMaker(new BridgeRandomNumberGenerator()).makeBridge(validatedSize);
+    }
 
     private int validateSize(String size) {
         BridgeSizeException.validate(size);
