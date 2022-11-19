7a8c4d641fd3d3e67fbc1025ece813b4468bd963
yeeun <dpdms9621@naver.com>
2022-11-19 16:49:17 +0900

    feat(BridgeGame): 사용자의 입력으로 칸을 이동할 때 사용하는 메서드 기능

--- src/main/java/bridge/domain/BridgeGame.java
+++ src/main/java/bridge/domain/BridgeGame.java
@@ -9,9 +9,19 @@ import java.util.List;
 public class BridgeGame {
 
     private BridgeMoving bridgeMoving;
+    private List<String> userSelectList;
 
     public BridgeGame() {
         this.bridgeMoving = new BridgeMoving();
+        this.userSelectList = new ArrayList<>();
+    }
+
+    // 사용자가 칸을 이동할 때 사용하는 메서드
+    public boolean move(String moving) {
+        this.userSelectList.add(moving);
+        if (bridgeMoving.movingCount()) return false;
+        if (!(bridgeMoving.compareList(moving))) return false;
+        return true;
     }
 
     public void setBridgeMake(int size) {
