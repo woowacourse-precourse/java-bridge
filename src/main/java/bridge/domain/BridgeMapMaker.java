26f3f0f5405593cf95cddcd762d2274d48d2fe58
Isu Shin <isu010614@naver.com>
2022-11-20 00:47:52 +0900

    feat: 이동상태를 확인하여 다리의 중간 구성 요소를 완성하는 기능 구현
    - 이동 상태가 입력된 만큼 이동상태와 구분자를 반복하여 더한다.
    - 마지막 구분자를 삭제한다.

--- src/main/java/bridge/domain/BridgeMapMaker.java
+++ src/main/java/bridge/domain/BridgeMapMaker.java
@@ -6,9 +6,19 @@ public class BridgeMapMaker {
     public BridgeMap makeBridgeMap(final List<String> movingStatus, final String side) {
         BridgeMap bridgeMap = new BridgeMap(new StringBuilder());
         bridgeMap.concatComponent(BridgeComponent.BRIDGE_BEGINNING);
-
+        bridgeMap.concatComponent(bridgeContent(movingStatus, side));
+        bridgeMap.deleteLastComponent(BridgeComponent.BRIDGE_SEPARATOR);
         bridgeMap.concatComponent(BridgeComponent.BRIDGE_END);
         return bridgeMap;
+    }
+
+    private String bridgeContent(final List<String> movingStatus, final String side) {
+        BridgeMap bridgeMap = new BridgeMap(new StringBuilder());
+        for (String moving : movingStatus) {
+            bridgeMap.concatComponent(crossOrNot(moving, side));
+            bridgeMap.concatComponent(BridgeComponent.BRIDGE_SEPARATOR);
+        }
+        return bridgeMap.toString();
     }
 
     private String crossOrNot(final String moving, final String side) {
