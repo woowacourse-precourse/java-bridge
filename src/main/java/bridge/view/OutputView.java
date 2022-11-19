2f52efcb13c877a5f5e3ab6e3fe023b86050b8d4
Yerim lee <103623292+lyrWinterCat@users.noreply.github.com>
2022-11-19 20:49:29 +0900

    feat (OutputView, ErrorMessage) : 필요한 메세지 변수 생성

--- src/main/java/bridge/view/OutputView.java
+++ src/main/java/bridge/view/OutputView.java
@@ -4,6 +4,11 @@ package bridge.view;
  * 사용자에게 게임 진행 상황과 결과를 출력하는 역할을 한다.
  */
 public class OutputView {
+    private static final String START_MESSAGE = "다리 건너기 게임을 시작합니다.";
+    private static final String BRIDGE_SIZE_INPUT = "다리의 길이를 입력해주세요.";
+    private static final String BRIDGE_MOVE_INPUT = "이동할 칸을 선택해주세요. (위: U, 아래: D)";
+    private static final String RESTART_GAME_INPUT = "게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)";
+    private static final String FINAL_MESSAGE = "최종 게임 결과";
 
     /**
      * 현재까지 이동한 다리의 상태를 정해진 형식에 맞춰 출력한다.
