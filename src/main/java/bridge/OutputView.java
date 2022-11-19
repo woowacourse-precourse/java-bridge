c235f604d336342b056b87fbfd51d9b3db349289
ym2629 <dydals2629@naver.com>
2022-11-19 13:47:57 +0900

    feat: print parts of printing map

--- src/main/java/bridge/OutputView.java
+++ src/main/java/bridge/OutputView.java
@@ -6,6 +6,10 @@ import java.util.*;
  * 사용자에게 게임 진행 상황과 결과를 출력하는 역할을 한다.
  */
 public class OutputView {
+
+    private static final int RANDOM_LOWER_INCLUSIVE = 0;
+    private static final int RANDOM_UPPER_INCLUSIVE = 1;
+
     public void startGame(){
         System.out.println("다리 건너기 게임을 시작합니다.");
     }
@@ -15,7 +19,8 @@ public class OutputView {
      * <p>
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void printMap() {
+    public void printMap(int userPosition, Boolean checkBridge, List<List<Integer>> map) {
+
     }
 
     /**
@@ -24,5 +29,38 @@ public class OutputView {
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
     public void printResult() {
+    }
+
+    public void printFirstBridge(int userPosition, boolean checkBridge, List<Integer> firstBridge){
+    }
+
+    public void printMapStart(){
+        System.out.print("[");
+    }
+
+    public void printNone(int index, List<Integer> firstBridge){
+        if(firstBridge.get(index) == RANDOM_LOWER_INCLUSIVE){
+            System.out.print("   ");
+        }
+    }
+
+    public void printCorrect(int index, List<Integer> firstBridge){
+        if(firstBridge.get(index) == RANDOM_UPPER_INCLUSIVE){
+            System.out.print(" O ");
+        }
+    }
+
+    public void printIncorrect(int index, List<Integer> firstBridge){
+        if(firstBridge.get(index) == RANDOM_LOWER_INCLUSIVE){
+            System.out.print(" X ");
+        }
+    }
+
+    public void printSeparation(){
+        System.out.print("|");
+    }
+
+    public void printMapEnd(){
+        System.out.print("]");
     }
 }
