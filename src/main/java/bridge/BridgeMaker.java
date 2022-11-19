674dea882150f00d1c17bf4028ba32127fc426f0
twoosky <lhjae3@naver.com>
2022-11-20 00:15:25 +0900

    feat(BridgeMaker): 다리 생성 랜덤값 반환 메소드 구현 및 랜덤값 예외 처리

--- src/main/java/bridge/BridgeMaker.java
+++ src/main/java/bridge/BridgeMaker.java
@@ -1,11 +1,18 @@
 package bridge;
 
+import java.util.ArrayList;
 import java.util.List;
+
+import static bridge.utils.ErrorMessages.*;
 
 /**
  * 다리의 길이를 입력 받아서 다리를 생성해주는 역할을 한다.
  */
 public class BridgeMaker {
+    private static final int LOWER_INCLUSIVE = 0;
+    private static final int UPPER_INCLUSIVE = 1;
+    private static final int SIZE_MIN_RANGE = 3;
+    private static final int SIZE_MAX_RANGE = 20;
 
     private final BridgeNumberGenerator bridgeNumberGenerator;
 
@@ -18,6 +25,17 @@ public class BridgeMaker {
      * @return 입력받은 길이에 해당하는 다리 모양. 위 칸이면 "U", 아래 칸이면 "D"로 표현해야 한다.
      */
     public List<String> makeBridge(int size) {
-        return null;
+    }
+
+    private int generateBridgeRandomNumber() {
+        int randomNumber = bridgeNumberGenerator.generate();
+        validateBridgeRandomNumber(randomNumber);
+        return randomNumber;
+    }
+
+    private void validateBridgeRandomNumber(int shape) {
+        if (!(shape == LOWER_INCLUSIVE || shape == UPPER_INCLUSIVE)) {
+            throw new IllegalArgumentException(ERROR_INVALID_BRIDGE_NUMBER);
+        }
     }
 }
