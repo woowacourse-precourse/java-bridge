9ff3ee29eb1398d575a7175e92db492f0017b805
dydtmd6801 <dydtmd6801@naver.com>
2022-11-19 15:21:57 +0900

    refactor(BridgeGame, InputView): 예외처리 기능 이동
    BridgeGame
    - move 메소드 안의 예외처리 기능을 메소드 이름과 맞지않아서 제거
    
    InputView
    - 새로운 메소드를 만들어서 InputView 클래스 안에서 예외처리 후, 값 세팅

--- src/main/java/bridge/InputView.java
+++ src/main/java/bridge/InputView.java
@@ -7,11 +7,42 @@ import camp.nextstep.edu.missionutils.Console;
  */
 public class InputView {
 
+    private final Exception exception = new Exception();
+
     /**
      * 다리의 길이를 입력받는다.
      */
-    public int readBridgeSize() {
-        return Integer.parseInt(Console.readLine());
+    public String readBridgeSize() {
+        return Console.readLine();
+    }
+
+    public int setBridgeSize() {
+        String bridgeSize = readBridgeSize();
+        int bridge = 0;
+        try {
+            bridge = checkException(bridgeSize);
+            return bridge;
+        } catch (NullPointerException NPE) {
+            System.out.println("[ERROR] 값을 입력해주세요");
+        } catch (NumberFormatException NFE) {
+            System.out.println("[ERROR] 숫자만 입력해주세요");
+        } catch (IllegalArgumentException IAE) {
+            System.out.println("[ERROR] 3 이상 20 이하의 값을 입력해주세요");
+        }
+        return 0;
+    }
+
+    public int checkException(String bridgeSize) {
+        if (exception.isEmpty(bridgeSize)) {
+            throw new NullPointerException("NPE");
+        }
+        if (!exception.isNumber(bridgeSize)) {
+            throw new NumberFormatException("NFE");
+        }
+        if (exception.isRangeInThreeAndTwenty(Integer.parseInt(bridgeSize))) {
+            throw new IllegalArgumentException("IAE");
+        }
+        return Integer.parseInt(bridgeSize);
     }
 
     /**
