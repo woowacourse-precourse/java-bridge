180f06f1f3a5c14bf6e1bf20b219f882a9ac34c7
KSH <tkdgns543@gmail.com>
2022-11-19 23:43:42 +0900

    feat(domain): Player 클래스에 게임 진척도 저장하는 인스턴스 변수 구현

--- src/main/java/bridge/domain/Player.java
+++ src/main/java/bridge/domain/Player.java
@@ -1,4 +1,11 @@
 package bridge.domain;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Player {
+    private int position;
+    private List<String> gameHistory = new ArrayList<>();
+    private String lastChoice;
+    private boolean checkAnswer;
 }
