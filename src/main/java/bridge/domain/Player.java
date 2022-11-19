c9373c888c8515c10005ce696a4ae46ff1c774dc
0chil <0@chll.it>
2022-11-20 02:43:35 +0900

    feat(Player): 다리를 이동할 수 있다

---
+++ src/main/java/bridge/domain/Player.java
@@ -0,0 +1,20 @@
+package bridge.domain;
+
+import bridge.constant.Direction;
+
+public class Player {
+
+    private final Bridge bridge;
+    private int position;
+
+    public Player(Bridge bridge) {
+        this.bridge = bridge;
+        this.position = 0;
+    }
+
+    public boolean moveNext(Direction direction) {
+        boolean isMovable = bridge.isMovable(position, direction);
+        position += 1;
+        return isMovable;
+    }
+}
---
+++ src/test/java/bridge/domain/PlayerTest.java
@@ -0,0 +1,32 @@
+package bridge.domain;
+
+import static bridge.constant.Direction.LOWER;
+import static bridge.constant.Direction.UPPER;
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.junit.jupiter.api.Test;
+
+import bridge.constant.Direction;
+
+public class PlayerTest {
+
+    @Test
+    void 다리를_이동할_수_있다() {
+        List<Direction> bridgeDirections = List.of(UPPER, LOWER, LOWER, LOWER, UPPER);
+        Bridge bridge = new Bridge(getCapitalLetters(bridgeDirections));
+
+        Player player = new Player(bridge);
+        assertThat(player.moveNext(UPPER)).isTrue();
+        assertThat(player.moveNext(LOWER)).isTrue();
+        assertThat(player.moveNext(UPPER)).isFalse();
+    }
+
+    private List<String> getCapitalLetters(List<Direction> bridgeDirections) {
+        return bridgeDirections.stream()
+                .map(Direction::capitalLetter)
+                .collect(Collectors.toList());
+    }
+}
