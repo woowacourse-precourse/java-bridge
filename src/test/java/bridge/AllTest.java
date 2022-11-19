7add5bd47cb40bc62f1b889e80aca0618c21d842
yeonhee <ydol2@naver.com>
2022-11-19 23:39:59 +0900

    test(AllTest) : 전체 기능 테스트 추가

--- src/test/java/bridge/AllTest.java
+++ src/test/java/bridge/AllTest.java
@@ -1,6 +1,8 @@
 package bridge;
 
+import bridge.view.OutputView;
 import camp.nextstep.edu.missionutils.test.NsTest;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -10,43 +12,29 @@ import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberI
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class AllTest extends NsTest {
-
-    @DisplayName("전체 기능을 테스트 한다.")
-    @Test
-    void 기능_테스트1() {
-        assertRandomNumberInRangeTest(() -> {
-            run("3", "U", "D", "U");
-            assertThat(output()).contains(
-                    "최종 게임 결과",
-                    "[ O |   | O ]",
-                    "[   | O |   ]",
-                    "게임 성공 여부: 성공",
-                    "총 시도한 횟수: 1"
-            );
-
-            int upSideIndex = output().indexOf("[ O |   | O ]");
-            int downSideIndex = output().indexOf("[   | O |   ]");
-            assertThat(upSideIndex).isLessThan(downSideIndex);
-        }, 1, 0, 1);
+    private static final OutputView outputView = new OutputView();
+    @BeforeEach()
+    void initTestCode() {
+        outputView.initMap();
     }
 
     @DisplayName("전체 기능을 테스트 한다.")
     @Test
-    void 기능_테스트2() {
+    void 기능_테스트() {
         assertRandomNumberInRangeTest(() -> {
-            run("3", "U", "D", "D");
+            run("5", "U", "D", "D","D", "U");
             assertThat(output()).contains(
                     "최종 게임 결과",
-                    "[ O |   |   ]",
-                    "[   | O | O ]",
+                    "[ O |   |   |   | O ]",
+                    "[   | O | O | O |   ]",
                     "게임 성공 여부: 성공",
                     "총 시도한 횟수: 1"
             );
 
-            int upSideIndex = output().indexOf("[ O |   |   ]");
-            int downSideIndex = output().indexOf("[   | O | O ]");
+            int upSideIndex = output().indexOf("[ O |   |   |   | O ]");
+            int downSideIndex = output().indexOf("[   | O | O | O |   ]");
             assertThat(upSideIndex).isLessThan(downSideIndex);
-        }, 1, 0, 0);
+        }, 1, 0, 0,0,1);
     }
 
     @Override
