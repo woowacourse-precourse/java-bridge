562959b8907cfc7976c2b4138b34c1eb2c749c4d
E1psycongr00 <39326175+E1psycongr00@users.noreply.github.com>
2022-11-19 13:41:07 +0900

    feat(bridgeGame): 브리지 게임 다리 생성 기능 구현

--- src/main/java/bridge/BridgeGame.java
+++ src/main/java/bridge/BridgeGame.java
@@ -1,15 +1,24 @@
 package bridge;
 
-/**
- * 다리 건너기 게임을 관리하는 클래스
- */
+import bridge.domain.BridgeStatus;
+import java.util.List;
+
 public class BridgeGame {
 
-    /**
-     * 사용자가 칸을 이동할 때 사용하는 메서드
-     * <p>
-     * 이동을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
-     */
+    private final BridgeMaker bridgeMaker;
+    private final BridgeStatusSaver bridgeStatusSaver;
+
+    public BridgeGame(BridgeMaker bridgeMaker, BridgeStatusSaver bridgeStatusSaver) {
+        this.bridgeMaker = bridgeMaker;
+        this.bridgeStatusSaver = bridgeStatusSaver;
+    }
+
+    public void createBridge(int size) {
+        List<String> bridge = bridgeMaker.makeBridge(size);
+        BridgeStatus bridgeStatus = BridgeStatus.createBridgeStatus(bridge);
+        bridgeStatusSaver.setBridgeStatus(bridgeStatus);
+    }
+
     public void move() {
     }
 
