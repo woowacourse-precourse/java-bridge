e670dbdd8be80b2876709afde430689ac4177fa1
rio <75190035+HyoJongPark@users.noreply.github.com>
2022-11-19 19:14:52 +0900

    test(UserInputValidatorTest): 예외 테스트가 예외 메시지도 검증하도록 변경

--- src/test/java/bridge/utils/UserInputValidatorTest.java
+++ src/test/java/bridge/utils/UserInputValidatorTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
+import static bridge.utils.Constants.*;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -14,7 +15,8 @@ class UserInputValidatorTest {
     @Test
     void 다리_길이는_숫자() {
         assertThatThrownBy(() -> UserInputValidator.validateBridgeSize("C"))
-                .isInstanceOf(IllegalArgumentException.class);
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(ERROR_MESSAGE_NOT_DIGIT);
     }
 
     @DisplayName("다리 길이는 3이상 20이하여야 한다.")
@@ -22,7 +24,8 @@ class UserInputValidatorTest {
     @ValueSource(strings = {"2", "21"})
     void 다리_길이는_3이상_20이하(String bridgeLength) {
         assertThatThrownBy(() -> UserInputValidator.validateBridgeSize(bridgeLength))
-                .isInstanceOf(IllegalArgumentException.class);
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(ERROR_MESSAGE_OUT_OF_RANGE_BRIDGE_SIZE);
     }
 
     @DisplayName("다리 길이 - 정상 입력")
@@ -37,15 +40,17 @@ class UserInputValidatorTest {
     @ValueSource(strings = {"", "DU", "A "})
     void 다리_칸은_하나의_문자만_입력_가능(String spaceToMove) {
         assertThatThrownBy(() -> UserInputValidator.validateSpaceToMove(spaceToMove))
-                .isInstanceOf(IllegalArgumentException.class);
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(ERROR_MESSAGE_INPUT_SIZE);
     }
 
-    @DisplayName("다리 칸은 U,D만 입력 가능하다.")
+    @DisplayName("이동할 칸은 U,D만 입력 가능하다.")
     @ParameterizedTest
     @ValueSource(strings = {"A", "W", " "})
     void 다리_칸은_U_D만_입력_가능(String spaceToMove) {
         assertThatThrownBy(() -> UserInputValidator.validateSpaceToMove(spaceToMove))
-                .isInstanceOf(IllegalArgumentException.class);
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(ERROR_MESSAGE_NOT_VALID_SPACE_NAME);
     }
 
     @DisplayName("다리 칸 - 정상 입력")
@@ -53,5 +58,21 @@ class UserInputValidatorTest {
     @ValueSource(strings = {"U", "D"})
     void 다리_칸_정상_입력(String spaceToMove) {
         assertDoesNotThrow(() -> UserInputValidator.validateSpaceToMove(spaceToMove));
+    }
+
+    @DisplayName("재시작 코드는 R, Q만 입력 가능하다.")
+    @ParameterizedTest
+    @ValueSource(strings = {"A", "W", " "})
+    void 재시작_코드는_Q_R_가능(String gameCommand) {
+        assertThatThrownBy(() -> UserInputValidator.validateGameCommand(gameCommand))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(ERROR_MESSAGE_NOT_VALID_GAME_COMMAND_NAME);
+    }
+
+    @DisplayName("재시작 코드는 R, Q만 입력 가능하다.")
+    @ParameterizedTest
+    @ValueSource(strings = {"R", "Q"})
+    void 재시작_코드_정상_입력(String gameCommand) {
+        assertDoesNotThrow(() -> UserInputValidator.validateGameCommand(gameCommand));
     }
 }
