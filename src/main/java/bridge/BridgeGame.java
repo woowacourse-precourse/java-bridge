d3e726e48e68591c29835a57c9391e54507baad8
Cheon-DM <76736548+Cheon-DM@users.noreply.github.com>
2022-11-19 22:09:33 +0900

    feat(bridgeGame): 다리 건너기 게임 관리 기능 추가
    다리 건너기 게임 관리 기능
    - 주어진 다리와 유저가 생각하는 다리 고르는 거 비교
    - 유저가 생각하는 다리 선택 추가

--- src/main/java/bridge/BridgeGame.java
+++ src/main/java/bridge/BridgeGame.java
@@ -1,23 +1,38 @@
 package bridge;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * 다리 건너기 게임을 관리하는 클래스
  */
 public class BridgeGame {
 
-    private static final String UP = "U";
-    private static final String DOWN = "D";
-    private static final String RETRY = "R";
-    private static final String QUIT = "Q";
-    private static final String ERROR_MSG = "[ERROR]";
+    private final Bridge bridge;
+    private final Bridge userThinkBridge;
+
+    public BridgeGame(Bridge bridge) {
+        this.userThinkBridge = new Bridge(new ArrayList<>());
+        this.bridge = bridge;
+    }
+
+    public List<String> getBridge() {
+        return bridge.getBridge();
+    }
+
+    public List<String> getUserThinkBridge() {
+        return userThinkBridge.getBridge();
+    }
 
     /**
      * 사용자가 칸을 이동할 때 사용하는 메서드
      * <p>
      * 이동을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void move(String moving) {
-        checkUpDownOrThrowException(moving);
+    public boolean move(String moving, int step) {
+        boolean isEqual = compareBridgeAndUserThink(moving, step);
+        addUserThinkBridge(isEqual);
+        return isEqual;
     }
 
     /**
@@ -28,9 +43,15 @@ public class BridgeGame {
     public void retry() {
     }
 
-    private void checkUpDownOrThrowException(String input){
-        if (!input.equals(UP) && !input.equals(DOWN)){
-            throw new IllegalArgumentException(ERROR_MSG + "U, D 이외의 문자는 입력할 수 없습니다.");
+    private boolean compareBridgeAndUserThink(String moving, int step){
+        return moving.equals(bridge.getBridge().get(step));
+    }
+
+    private void addUserThinkBridge(boolean isEqual){
+        if (isEqual){
+            userThinkBridge.setBridge("O");
+            return;
         }
+        userThinkBridge.setBridge("X");
     }
 }
