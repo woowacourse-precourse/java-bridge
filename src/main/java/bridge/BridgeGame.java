8b74fa5b2589fee1a1788c83fc6ae69c62d3b8f1
Sangwoo Shin <sangwoonoel@naver.com>
2022-11-19 23:05:42 +0900

    feat(BridgeGame): 이동 결과를 알려주는 기능 구현

--- src/main/java/bridge/BridgeGame.java
+++ src/main/java/bridge/BridgeGame.java
@@ -1,16 +1,38 @@
 package bridge;
 
+import java.util.List;
+import java.util.Map;
+
+import static bridge.Moving.UP;
+import static bridge.Result.*;
+
 /**
  * 다리 건너기 게임을 관리하는 클래스
  */
 public class BridgeGame {
+
+    private final List<String> bridge;
+
+    public BridgeGame(List<String> bridge) {
+        this.bridge = bridge;
+    }
 
     /**
      * 사용자가 칸을 이동할 때 사용하는 메서드
      * <p>
      * 이동을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void move() {
+    public Result move(int index, String moving) {
+        if (bridge.get(index) == moving) {
+            if (moving == UP.getMoving()) {
+                return UP_SUCCESS;
+            }
+            return DOWN_SUCCESS;
+        }
+        if (moving == UP.getMoving()) {
+            return UP_FAIL;
+        }
+        return DOWN_FAIL;
     }
 
     /**
