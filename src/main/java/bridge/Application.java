a095c3ef17198f4783224247caa2379d83b5fa2a
ddingmin <ddingminyt@gmail.com>
2022-11-20 02:14:35 +0900

    feat(java-bridge): Main 메인 함수에서 Referee를 사용하도록 구현

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -1,6 +1,6 @@
 package bridge;
 
-import bridge.domain.BridgeGame;
+import bridge.domain.Referee;
 import bridge.ui.InputView;
 import bridge.ui.OutputView;
 
@@ -10,6 +10,9 @@ public class Application {
 
     public static void main(String[] args) {
         // TODO: 프로그램 구현
-        BridgeGame bridgeGame = new BridgeGame(inputView.readBridgeSize());
+        Referee referee = new Referee();
+        referee.start();
+
+
     }
 }
