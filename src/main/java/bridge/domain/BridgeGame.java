2c88edf0fd0c4f261ce0d2357be8734f669d08dc
lecocococo <chemwang13@naver.com>
2022-11-19 19:53:24 +0900

    feat(BridgeGame): 플레이어의 이동한 칸의 명령에 따라 움직이는 기능 추가
    - 다음 위치의 값을 입력받고 갈 수 있다면 point를 다음으로 옮기고 막혀있다면 현재 칸의 위치를 반환하도록 함

--- src/main/java/bridge/domain/BridgeGame.java
+++ src/main/java/bridge/domain/BridgeGame.java
@@ -1,4 +1,6 @@
-package bridge;
+package bridge.domain;
+
+import java.util.List;
 
 /**
  * 다리 건너기 게임을 관리하는 클래스
@@ -9,8 +11,15 @@ public class BridgeGame {
      * 사용자가 칸을 이동할 때 사용하는 메서드
      * <p>
      * 이동을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
+     *
+     * @return
      */
-    public void move() {
+    public int move(List<String> bridge, int point, String movement) {
+        if (bridge.get(point).equals(movement)) {
+            return ++point;
+        }
+
+        return point;
     }
 
     /**
