8a5f3123781621203d177ff79ba15c934c3cb25d
yongseungLeeDec <yonson.dev@gmail.com>
2022-11-19 14:20:46 +0900

    feat: 다리 생성 기능 구현
    BridgeMaker의 makeBridge 메서드를 구현함.
    - 이 메서드는 정수로 변환된 입력받은 다리 길이를 인자로 받아, 그만한 크기의 다리 개체를 List<String> 타입으로 생성하여 호출자에게 반환함.

--- src/main/java/bridge/BridgeMaker.java
+++ src/main/java/bridge/BridgeMaker.java
@@ -1,11 +1,13 @@
 package bridge;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
  * 다리의 길이를 입력 받아서 다리를 생성해주는 역할을 한다.
  */
 public class BridgeMaker {
+    private static final BridgeStep[] STEPS_FOR_GENERATED_NUMBERS = new BridgeStep[] {BridgeStep.DOWN, BridgeStep.UP};
 
     private final BridgeNumberGenerator bridgeNumberGenerator;
 
@@ -18,6 +20,15 @@ public class BridgeMaker {
      * @return 입력받은 길이에 해당하는 다리 모양. 위 칸이면 "U", 아래 칸이면 "D"로 표현해야 한다.
      */
     public List<String> makeBridge(int size) {
-        return null;
+        List<String> bridge = new ArrayList<>();
+        int count = 0;
+
+        while (count < size) {
+            BridgeStep step = STEPS_FOR_GENERATED_NUMBERS[this.bridgeNumberGenerator.generate()];
+            bridge.add(step.getValue());
+            count++;
+        }
+
+        return bridge;
     }
 }
