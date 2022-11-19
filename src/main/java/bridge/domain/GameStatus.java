164883624ea139485a6d14a380470bc355e58f51
E1psycongr00 <39326175+E1psycongr00@users.noreply.github.com>
2022-11-19 16:02:00 +0900

    feat(gameStatus): 성공, 실패, 진행중 의 details 상태 추가 및 getter 구현

--- src/main/java/bridge/domain/GameStatus.java
+++ src/main/java/bridge/domain/GameStatus.java
@@ -1,17 +1,23 @@
 package bridge.domain;
 
 public enum GameStatus {
-    SUCCESS(false),
-    IN_PROGRESS(false),
-    FAIL(true);
+    SUCCESS(false, "성공"),
+    IN_PROGRESS(false, "진행중"),
+    FAIL(true, "실패");
 
     private final boolean retry;
+    private final String details;
 
-    GameStatus(boolean retry) {
+    GameStatus(boolean retry, String details) {
         this.retry = retry;
+        this.details = details;
     }
 
     public boolean needCallRetry() {
         return this.retry;
+    }
+
+    public String getDetails() {
+        return details;
     }
 }
