8de4bb2843f781615cfc89e3d4a13fb8c2d85838
BuildTools <unconfigured@null.spigotmc.org>
2022-11-19 21:19:54 +0900

    test: 추가 플레이어가 이동하는 기능테스트

---
+++ src/test/java/bridge/BridgeGameTest.java
@@ -0,0 +1,66 @@
+package bridge;
+
+import bridge.domain.Bridge;
+import bridge.domain.Player;
+import bridge.service.BridgeGame;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.assertj.core.util.Lists.newArrayList;
+
+public class BridgeGameTest {
+
+    private static final String EXCEPTION_MESSAGE_INVALID_MOVE = "[ERROR] 이동 가능 칸은 U 또는 D 만 있습니다.";
+    BridgeGame bridgeGame;
+
+    @BeforeEach
+    void beforeEach() {
+        bridgeGame = new BridgeGame();
+        BridgeNumberGenerator numberGenerator = new BridgeMakerTest.TestNumberGenerator(
+                newArrayList(1, 1, 0, 1, 1, 0, 1, 1, 0, 1
+                        , 1, 0, 1, 1, 0, 1, 1, 0, 1, 1));
+        bridgeGame.makeBridge(20, numberGenerator);
+    }
+
+    @DisplayName("move 함수가 잘못된 값이 들어올시 예외를 발생한다.")
+    @ParameterizedTest
+    @ValueSource(strings = {"0", "1", "UP", "DOWN", "C", "E", "T", "V"})
+    void moveByInvalidValue(String input) {
+        assertThatThrownBy(() -> bridgeGame.move(input))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(EXCEPTION_MESSAGE_INVALID_MOVE);
+    }
+
+    @DisplayName("move 함수가 정상적으로 true를 반환한다.")
+    @ParameterizedTest
+    @CsvSource({"U, U, D, U, U, D"})
+    void executeMoveToTrue(String input1, String input2, String input3,
+                           String input4, String input5, String input6) {
+        assertThat(true).isEqualTo(bridgeGame.move(input1));
+        assertThat(true).isEqualTo(bridgeGame.move(input2));
+        assertThat(true).isEqualTo(bridgeGame.move(input3));
+        assertThat(true).isEqualTo(bridgeGame.move(input4));
+        assertThat(true).isEqualTo(bridgeGame.move(input5));
+        assertThat(true).isEqualTo(bridgeGame.move(input6));
+    }
+
+    @DisplayName("move 함수가 정상적으로 false를 반환한다.")
+    @ParameterizedTest
+    @CsvSource({"D, D, U, D, D, U"})
+    void executeMoveToFalse(String input1, String input2, String input3,
+                            String input4, String input5, String input6) {
+        assertThat(false).isEqualTo(bridgeGame.move(input1));
+        assertThat(false).isEqualTo(bridgeGame.move(input2));
+        assertThat(false).isEqualTo(bridgeGame.move(input3));
+        assertThat(false).isEqualTo(bridgeGame.move(input4));
+        assertThat(false).isEqualTo(bridgeGame.move(input5));
+        assertThat(false).isEqualTo(bridgeGame.move(input6));
+    }
+}
---
+++ src/test/java/bridge/PlayerTest.java
@@ -0,0 +1,101 @@
+package bridge;
+
+import bridge.domain.Bridge;
+import bridge.domain.Player;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+public class PlayerTest {
+
+    Player player;
+    @BeforeEach
+    void beforeEach() {
+        player = new Player();
+    }
+
+    @DisplayName("최대 도전 횟수를 오버플로시 예외가 발생한다.")
+    @Test
+    void overTotalTry() {
+        boolean result = false;
+        long tryNumber = (Integer.MAX_VALUE + 1L);
+        try {
+            while (tryNumber != 0) {
+                tryNumber--;
+                player.initRetryPlayer();
+            }
+        }catch (IllegalArgumentException exception){
+            result = true;
+        }
+        assertThat(true).isEqualTo(result);
+    }
+
+    @DisplayName("isPlayerMove 함수가 true를 정상적으로 반환한다.")
+    @Test
+    void executeIsPlayerMoveToTrue() {
+        Bridge bridge = new Bridge();
+        bridge.setBridge(List.of("U", "D", "U"));
+        player.updateMoveInfo("U");
+        player.updateMoveInfo("D");
+        player.updateMoveInfo("U");
+        assertThat(true).isEqualTo(player.isPlayerMove(bridge));
+    }
+
+    @DisplayName("isPlayerMove 함수가 false를 정상적으로 반환한다.")
+    @Test
+    void executeIsPlayerMoveToFalse() {
+        Bridge bridge = new Bridge();
+        bridge.setBridge(List.of("U", "D", "D"));
+        player.updateMoveInfo("U");
+        player.updateMoveInfo("D");
+        player.updateMoveInfo("U");
+        assertThat(false).isEqualTo(player.isPlayerMove(bridge));
+    }
+
+    @DisplayName("isClearGame 함수가 true를 정상적으로 반환한다.")
+    @Test
+    void executeIsClearGameToTrue() {
+        Bridge bridge = new Bridge();
+        bridge.setBridge(List.of("U", "D", "U"));
+        player.updateMoveInfo("U");
+        player.updateMoveInfo("D");
+        player.updateMoveInfo("U");
+        player.setMoveResult(player.isPlayerMove(bridge));
+        assertThat(true).isEqualTo(player.isClearGame(bridge));
+    }
+
+    @DisplayName("isClearGame 함수가 false를 정상적으로 반환한다.")
+    @ParameterizedTest
+    @CsvSource({"U, D, U, U, D, D"})
+    void executeIsClearGameToFalse(String bridge1, String bridge2, String bridge3,
+                                   String move1, String move2, String move3) {
+        Bridge bridge = new Bridge();
+        bridge.setBridge(List.of(bridge1, bridge2, bridge3));
+        player.updateMoveInfo(move1);
+        player.updateMoveInfo(move2);
+        player.updateMoveInfo(move3);
+        player.setMoveResult(player.isPlayerMove(bridge));
+        assertThat(false).isEqualTo(player.isClearGame(bridge));
+    }
+
+    @DisplayName("isClearGame 함수가 false를 정상적으로 반환한다.")
+    @ParameterizedTest
+    @CsvSource({"U, D, U, U, D", "U, D, U, U, U"})
+    void executeIsClearGameToFalse2(String bridge1, String bridge2, String bridge3,
+                                   String move1, String move2) {
+        Bridge bridge = new Bridge();
+        bridge.setBridge(List.of(bridge1, bridge2, bridge3));
+        player.updateMoveInfo(move1);
+        player.updateMoveInfo(move2);
+        player.setMoveResult(player.isPlayerMove(bridge));
+        assertThat(false).isEqualTo(player.isClearGame(bridge));
+    }
+
+}
