9115e8f8448c8579eec4e12deb09a2702a2dc599
abbd2 <ajkety00@gmail.com>
2022-11-20 02:10:23 +0900

    Feat(BridgeGame): 1 또는 0을 랜덤으로 반환하는 BridgeNumberGenerator 메서드 구현

--- src/main/java/bridge/BridgeGame.java
+++ src/main/java/bridge/BridgeGame.java
@@ -1,5 +1,7 @@
 package bridge;
 
+import java.util.stream.DoubleStream;
+
 /**
  * 다리 건너기 게임을 관리하는 클래스
  */
@@ -13,9 +15,10 @@ public class BridgeGame {
      * <p>
      * 이동을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public boolean move(String moving, int randomNumber) { // 이동할 칸과 랜덤 값을 받음
+    public boolean move(String moving) {
+        int randomNumber = BridgeNumberGenerator();
         // U == 1, D == 0일 때 성공
-        if((moving.equals("U") && randomNumber == 1) || (moving.equals("D") && randomNumber == 0)){
+        if ((moving.equals("U") && randomNumber == 1) || (moving.equals("D") && randomNumber == 0)) {
             System.out.println("성공");
             return true;
         }
@@ -23,20 +26,20 @@ public class BridgeGame {
         return false;
     }
 
+
+
+    public int BridgeNumberGenerator() {
+        BridgeRandomNumberGenerator bridgeRandomNumberGenerator = new BridgeRandomNumberGenerator();
+        int randomNumber = bridgeRandomNumberGenerator.generate();
+        return randomNumber;
+    }
+
     /**
      * 사용자가 게임을 다시 시도할 때 사용하는 메서드
      * <p>
      * 재시작을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public boolean retry(String retry) {
-        if(retry.equals("R")){
-            tryCount++; // 시도 횟수 증가
-            return true;
-        }else if(retry.equals("Q")){
-
-            return false;
-        }
-        return false;
+    public void retry() {
     }
 
 }
