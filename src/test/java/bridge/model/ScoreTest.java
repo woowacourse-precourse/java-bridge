b194c522a0d10b1204f04ffe14070851174da25b
sangwon <swchoi1997@naver.com>
2022-11-19 13:59:46 +0900

    test: 다리를 건넜는지 판별하는 기능 테스트

--- src/test/java/bridge/model/ScoreTest.java
+++ src/test/java/bridge/model/ScoreTest.java
@@ -1,15 +1,18 @@
 package bridge.model;
 
-import static org.assertj.core.api.Assertions.*;
-import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.params.provider.Arguments.arguments;
 
 import java.util.List;
-import org.assertj.core.api.Assertions;
+import java.util.stream.Stream;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.MethodSource;
 
 class ScoreTest {
 
@@ -27,11 +30,40 @@ class ScoreTest {
         assertThat(score.goOneStep(new Bridge(List.of(bridge.split(""))), userStep)).isTrue();
     }
 
-    @DisplayName("유저가 선택한 다리로 지나갈 수 없다..")
+    @DisplayName("유저가 선택한 다리로 지나갈 수 없다.")
     @ParameterizedTest
     @CsvSource({"D, UDD", "U, DDD"})
     void canNotGo(String userStep, String bridge) {
         assertThat(score.goOneStep(new Bridge(List.of(bridge.split(""))), userStep)).isFalse();
+    }
+
+    @DisplayName("유저가 다리를 건넜으면 true를 리턴한다.")
+    @ParameterizedTest
+    @MethodSource("listProviderIsCrossing")
+    void isCrossing(List<String> userStep, List<String> bridge) {
+        assertTrue(new Score(userStep).isCrossing(new Bridge(bridge)));
+    }
+
+    static Stream<Arguments> listProviderIsCrossing() {
+        return Stream.of(
+                arguments(List.of("U", "D", "D", "D", "D", "D", "D", "D", "D"),
+                        List.of("U", "D", "D", "D", "D", "D", "D", "D", "D")),
+                arguments(List.of("D", "D", "D", "D", "D", "D"), List.of("D", "D", "D", "D", "D", "D"))
+        );
+    }
+
+    @DisplayName("유저가 다리를 건너지 않았으면 false를 리턴한다.")
+    @ParameterizedTest
+    @MethodSource("listProviderIsNotCrossing")
+    void isNotCrossing(List<String> userStep, List<String> bridge) {
+        assertFalse(new Score(userStep).isCrossing(new Bridge(bridge)));
+    }
+
+    static Stream<Arguments> listProviderIsNotCrossing() {
+        return Stream.of(
+                arguments(List.of("U", "D", "D", "D", "D"), List.of("U", "D", "D", "D", "D", "D", "D", "D", "D")),
+                arguments(List.of("D", "D", "D", "D"), List.of("D", "D", "D", "D", "D", "D"))
+        );
     }
 
 }
