71e0511bca07120d2fbb3f9b693b3e29d08d8987
yongseungLeeDec <yonson.dev@gmail.com>
2022-11-19 12:22:04 +0900

    feat: 다리 길이 입력 요청 문구 출력 기능 구현
    InputView의 printPromptForBridgeLength 메서드를 구현함.
    - 해당 메시지는 사용자에게 콘솔에 다리 길이 입력을 요청하는 문구를 출력함.

--- src/main/java/bridge/InputView.java
+++ src/main/java/bridge/InputView.java
@@ -4,10 +4,14 @@ package bridge;
  * 사용자로부터 입력을 받는 역할을 한다.
  */
 public class InputView {
-
+    private static final String PROMPT_FOR_BRIDGE_LENGTH = "다리의 길이를 입력해주세요.";
     /**
      * 다리의 길이를 입력받는다.
      */
+    public void printPromptForBridgeLength() {
+        System.out.println(PROMPT_FOR_BRIDGE_LENGTH);
+    }
+
     public int readBridgeSize() {
         return 0;
     }
