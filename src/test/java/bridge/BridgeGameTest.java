efe344e23be76e655e58fff339a7dd91d1fa0628
JuHyeokParkk <hyeok5596@naver.com>
2022-11-20 02:06:35 +0900

    test: 게임 시도 횟수에 대한 오버플로우가 방지되는지 테스트하는 코드 작성

--- src/test/java/bridge/BridgeGameTest.java
+++ src/test/java/bridge/BridgeGameTest.java
@@ -2,8 +2,11 @@ package bridge;
 
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class BridgeGameTest {
 
@@ -13,10 +16,24 @@ public class BridgeGameTest {
     public void 게임_시도_횟수_기록(int targetTryCount) {
         BridgeGame bridgeGame = new BridgeGame();
 
+        iterateGameTry(bridgeGame, targetTryCount);
+
+        Assertions.assertThat(bridgeGame.getGameTryCount()).isEqualTo(targetTryCount + 1);
+    }
+
+    @DisplayName("오버플로우가 발생할 수 있을 정도로 게임을 시도하면, 예외를 발생시킨다.")
+    @Test
+    public void 게임_시도_횟수_오버플로우_방지() {
+        BridgeGame bridgeGame = new BridgeGame();
+
+        assertThatThrownBy(() -> iterateGameTry(bridgeGame, Integer.MAX_VALUE))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("[ERROR] 너무 많은 게임 시도는 건강에 해롭습니다. 게임을 종료합니다.");
+    }
+
+    public void iterateGameTry(BridgeGame bridgeGame, int targetTryCount) {
         for (int count = 0; count < targetTryCount; count++) {
             bridgeGame.retry();
         }
-
-        Assertions.assertThat(bridgeGame.getGameTryCount()).isEqualTo(targetTryCount + 1);
     }
 }
