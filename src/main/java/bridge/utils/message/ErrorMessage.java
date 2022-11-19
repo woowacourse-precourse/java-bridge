cc336f41b1fb9162a73bd34c09e437edf2634730
Donguk <95donguk.dev@gmail.com>
2022-11-19 23:00:36 +0900

    style(ErrorMessage): 이동 명령 에러 메시지 출력문 수정

--- src/main/java/bridge/utils/message/ErrorMessage.java
+++ src/main/java/bridge/utils/message/ErrorMessage.java
@@ -5,7 +5,7 @@ public enum ErrorMessage {
     NON_NUMERIC_VALUE("숫자가 아닌 값을 입력하셨습니다."),
     BLANK_VALUE("빈 값을 입력하셨습니다."),
     INVALID_BRIDGE_SIZE("다리 길이는 %d부터 %d 사이의 숫자여야 합니다."),
-    INVALID_MOVING("위로 이동하실려면 %s, 아래로 이동하실려면 %s를 입력해주세요.");
+    INVALID_MOVING("유효하지 않은 이동 명령어 입니다.");
 
     private static final String ERROR_MESSAGE = "[ERROR] ";
 
