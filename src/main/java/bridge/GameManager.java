5ea87c0b69b61b5a23d22a604ae2f53f1bf746b5
JIHYEISWISDOM <wlgp8958@naver.com>
2022-11-20 21:55:59 +0900

    feat(GameManager): 게임 실패 확인 객체 생성

---
+++ src/main/java/bridge/GameManager.java
@@ -0,0 +1,38 @@
+package bridge;
+
+import bridge.view.InputView;
+import bridge.view.OutputView;
+
+public class GameManager {
+    private final BridgeGame bridgeGame;
+
+    public GameManager(BridgeMaker bridgeMaker) {
+        Bridge bridge = new Bridge(bridgeMaker.makeBridge(InputView.readBridgeSize()));
+        bridgeGame = new BridgeGame(bridge);
+    }
+
+    public void play() {
+        boolean status;
+        do {
+            status = moveForward();
+        } while (status && isRetry());
+
+        OutputView.printResult(bridgeGame);
+    }
+
+    private boolean moveForward() {
+        while (bridgeGame.canMove()) {
+            bridgeGame.move(InputView.readMoving());
+            OutputView.printMap(bridgeGame);
+        }
+        return bridgeGame.isFail();
+    }
+
+    private boolean isRetry() {
+        if (Key.matchEnd(InputView.readGameCommand())) {
+            return false;
+        }
+        bridgeGame.retry();
+        return true;
+    }
+}
