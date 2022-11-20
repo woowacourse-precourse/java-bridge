bed0e72baa053c23d5e2eda382235a7c6154fb9a
apptie <appti@naver.com>
2022-11-20 17:51:55 +0900

    refactor(GuideView): 메세지에서 누락된 글자 추가

--- src/main/java/bridge/view/GuideView.java
+++ src/main/java/bridge/view/GuideView.java
@@ -23,7 +23,7 @@ public final class GuideView {
     }
 
     private enum GuideViewMessage {
-        GAME_START("다리 건너기 게임을 시작합니다"),
+        GAME_START("다리 건너기 게임을 시작합니다."),
         GAME_RESULT("최종 게임 결과");
 
         private String message;
