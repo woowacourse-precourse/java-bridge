bba04d731ddbe1930693fdef1954c418c694cc44
Beginner <dkstmdwn17@naver.com>
2022-11-20 23:57:44 +0900

    feature : 다리 만드는 부분과 게임을 시작하는 부분을 구현
    Controller : 다리만드는 부분을
    BridgeMaker 객체를 이용하여
    생성자로 BridgeRandomNumberGenerator()
    넘겨주어서 사용했다.
    
    게임을 실행하는데 필요한 메서드
    와 변수를 BridgeGame에 정의
    하였다.

--- src/main/java/bridge/controller/BridgeGameController.java
+++ src/main/java/bridge/controller/BridgeGameController.java
@@ -1,16 +1,21 @@
 package bridge.controller;
 
 import bridge.BridgeGame;
-import bridge.BridgeNumberGenerator;
+import bridge.BridgeMaker;
 import bridge.BridgeRandomNumberGenerator;
 import bridge.view.InputView;
 import bridge.view.OutputView;
+
+import java.util.List;
 
 public class BridgeGameController {
 
     private final InputView inputView;
     private final OutputView outputView;
     private final BridgeGame bridgeGame;
+    private final BridgeMaker bridgeMaker = new BridgeMaker(new BridgeRandomNumberGenerator());
+    private List<String> bridges;
+    private boolean isRunning;
 
     public BridgeGameController(InputView inputView, OutputView outputView, BridgeGame bridgeGame) {
         this.inputView = inputView;
@@ -21,8 +26,37 @@ public class BridgeGameController {
     public void run() {
         try {
             int bridgeSize = inputView.readBridgeSize();
+            bridges = bridgeMaker.makeBridge(bridgeSize);
+            bridgeGame.setBridges(bridges);
+            gameStart();
         }catch (IllegalArgumentException e){
             System.out.println(e.getMessage());
         }
+    }
+
+    private void gameStart() {
+        int index = 0;
+        isRunning = true;
+        System.out.println(bridgeGame.getBridges());
+        while(index != bridges.size()){
+            boolean isSuccess = bridgeGame.move(bridges.get(index++), inputView.readMoving());
+            outputView.printMap(bridgeGame.getBridges(), index, isSuccess);
+            if(!isSuccess){ // 움직이는데 실패
+                index = getIndex(index);
+            }
+            if(!isRunning) break;
+        }
+        outputView.printResult();
+    }
+
+    private int getIndex(int index) {
+        boolean isRetry = bridgeGame.isRetry(inputView.readGameCommand());
+        if(isRetry){
+            index = 0;
+        }
+        if(!isRetry){
+            isRunning = false;
+        }
+        return index;
     }
 }
