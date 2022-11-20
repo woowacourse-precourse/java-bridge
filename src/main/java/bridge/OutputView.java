41af7d1c15da40f3a62be4b55713fd5d61e9c3f6
Ilchul Kim <huj0159@gmail.com>
2022-11-21 05:48:39 +0900

    feat: add print game process methods

--- src/main/java/bridge/OutputView.java
+++ src/main/java/bridge/OutputView.java
@@ -1,16 +1,32 @@
 package bridge;
 
+import bridge.model.Map;
+
 /**
  * 사용자에게 게임 진행 상황과 결과를 출력하는 역할을 한다.
  */
+//BridgeGame 클래스에서 InputView, OutputView 를 사용하지 않는다.
 public class OutputView {
+    public void printGameStart(){
+        System.out.println("다리 건너기 게임을 시작합니다.\n");
+        System.out.println("다리의 길이를 입력해주세요.");
+    }
+
+    public void askMove(){
+        System.out.println("\n이동할 칸을 선택해주세요. (위: U, 아래: D)");
+    }
+
+    public void askRestart(){
+        System.out.println("게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)");
+    }
 
     /**
      * 현재까지 이동한 다리의 상태를 정해진 형식에 맞춰 출력한다.
      * <p>
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void printMap() {
+    public void printMap(Map map) {
+        System.out.println(map.toString());
     }
 
     /**
@@ -19,5 +35,7 @@ public class OutputView {
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
     public void printResult() {
+        System.out.println("최종 게임 결과");
+
     }
 }
