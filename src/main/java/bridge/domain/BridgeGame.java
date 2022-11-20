c9d077be824b9d89f9cfe962efeddfd5a3b3e380
rt3310 <btac3310@gmail.com>
2022-11-20 20:55:25 +0900

    style: 주석 제거 및 import 정리

--- src/main/java/bridge/domain/BridgeGame.java
+++ src/main/java/bridge/domain/BridgeGame.java
@@ -2,17 +2,12 @@ package bridge.domain;
 
 import bridge.util.BridgeBlock;
 import bridge.util.BridgePosition;
-import bridge.util.ErrorMessage;
 import bridge.util.GameCommand;
-
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
-/**
- * 다리 건너기 게임을 관리하는 클래스
- */
 public class BridgeGame {
 
     private final Bridge bridge;
@@ -25,22 +20,12 @@ public class BridgeGame {
         initMovements();
     }
 
-    /**
-     * 사용자가 칸을 이동할 때 사용하는 메서드
-     * <p>
-     * 이동을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
-     */
     public boolean move(String movingPosition) {
         boolean possibleToMove = bridge.isMovable(movingPosition, movements.size());
         movements.add(movingPosition);
         return possibleToMove;
     }
 
-    /**
-     * 사용자가 게임을 다시 시도할 때 사용하는 메서드
-     * <p>
-     * 재시작을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
-     */
     public boolean retry(String selectedProgress) {
         if (GameCommand.RETRY.isEqual(selectedProgress)) {
             initMovements();
