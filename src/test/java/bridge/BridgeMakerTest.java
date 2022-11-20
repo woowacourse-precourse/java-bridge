5302e093c7fb9772f0c38bf636d1c3a9d7a17995
Gibeom Gwon <gb.gwon@stackframe.dev>
2022-11-20 13:25:02 +0900

    fix: ArrayNumberGenerator로 BridgeMaker 테스트

--- src/test/java/bridge/BridgeMakerTest.java
+++ src/test/java/bridge/BridgeMakerTest.java
@@ -10,15 +10,7 @@ class MakeBridgeTest {
 
     @Test
     void 다리_생성_테스트() {
-        BridgeMaker maker = new BridgeMaker(new BridgeNumberGenerator() {
-            List<Integer> number = List.of(1, 0, 1, 1, 0);
-            int idx = 0;
-
-            @Override
-            public int generate() {
-                return number.get(idx++);
-            }
-        });
+        BridgeMaker maker = new BridgeMaker(new ArrayNumberGenerator(List.of(1, 0, 1, 1, 0)));
 
         List<String> result = List.of("U", "D", "U", "U", "D");
         assertThat(maker.makeBridge(5)).containsExactlyElementsOf(result);
