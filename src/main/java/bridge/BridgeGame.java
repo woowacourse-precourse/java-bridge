64dde0d0b2239345a8c33c791a04eceaacfcbe83
chener <49267211+chanosong@users.noreply.github.com>
2022-11-20 22:32:59 +0900

    feat(BridgeGame): Make cursor to zero
    + retry(): Make cursor to zero

--- src/main/java/bridge/BridgeGame.java
+++ src/main/java/bridge/BridgeGame.java
@@ -32,11 +32,8 @@ public class BridgeGame {
         cursor += 5 - (canMove(direction) + "").length();
     }
 
-    /**
-     * 사용자가 게임을 다시 시도할 때 사용하는 메서드
-     * <p>
-     * 재시작을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
-     */
+    // Make cursor to zero
     public void retry() {
+        cursor = 0;
     }
 }
