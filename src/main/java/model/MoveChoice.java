bfb2694bf2bd0f14c9f279df8aacac59afe34e14
hodomaroo <enghemdwkd@gmail.com>
2022-11-20 14:31:55 +0900

    [Feat/MoveChoice] MoveChoice 인스턴스 및 기능 메서드 정의
    MoveChoice의 인스턴스인 UP / DOWN을 선언하였으며,
    0또는 1의 값을 입력할 경우 알맞은 UP/DOWN 객체의
    moving(String)값을 반환하는getMoving메서드를 정의하였습니다.:

--- src/main/java/model/MoveChoice.java
+++ src/main/java/model/MoveChoice.java
@@ -1,5 +1,20 @@
 package model;
 
-public class MoveChoice {
+import static model.BridgeGameExceptions.NOT_A_MOVE_CHOICE;
 
+import java.util.Arrays;
+
+public enum MoveChoice {
+    UP("U", 1), DOWN("D", 0);
+
+    private final String moving;
+    private final int positionNumber;
+
+    MoveChoice(String moving, int positionNumber) {
+        this.moving = moving;
+        this.positionNumber = positionNumber;
+    }
+
+    public static String getMoving(int positionNumber) {
+    }
 }
