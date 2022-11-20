02beded28333ed5d1051e5c2a04617e7daee7f05
Basak <chgy2131@naver.com>
2022-11-20 14:33:17 +0900

    feat(GameResult): 게임 결과와 관련된 정보를 저장하는 기능 추가
    - 게임 결과와 관련된 정보를 저장
    - 총 시도횟수도 나타내며, 시도한 횟수를 증가시키는 기능 포함
    - 답으로 제출한 정보를
    보관하며, 이를 갱신하는 기능 포함

--- src/main/java/bridge/domain/GameResult.java
+++ src/main/java/bridge/domain/GameResult.java
@@ -1,4 +1,14 @@
 package bridge.domain;
 
 public class GameResult {
+    private int numberOfAttempts = 0;
+    private Bridge bridge;
+    private GameProgress progress = GameProgress.PLAYING;
+
+    public void increaseAttempt() {
+        numberOfAttempts++;
+    }
+    public void addBlock(String upDown) {
+        bridge.addBlock(upDown);
+    }
 }
