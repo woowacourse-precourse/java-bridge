e1d389ce48bbba3241d845b7b218c3868eb575c5
seonghoo1217 <seonghoo1217@naver.com>
2022-11-20 21:27:29 +0900

    feat(BridgeController, OutputView) : add print move result by StringBuilder and if User move_result is X program done
    * Why StringBuilder?
    - StringBuilder is efficient in processing loop
    
    * Why done?
    - After work use the end state

--- src/main/java/controller/BridgeController.java
+++ src/main/java/controller/BridgeController.java
@@ -23,6 +23,40 @@ public class BridgeController {
 
 	private void bridgeMakeBySize(int bridge_size){
 		bridge_list = bridgeMaker.makeBridge(bridge_size);
+		crossingTheBridge();
 	}
 
+	private void crossingTheBridge(){
+		System.out.println(bridge_list);
+		for (int index=0;index<bridge_list.size();index++){
+			String bridge_move_result = compareInputAndIndex(bridgeMoveOutputAndInput(), bridge_list.get(index));
+			printMapByStringBuilder(bridge_move_result,index);
+			if (bridge_move_result.contains("X")){
+				break;
+			}
+		}
+	}
+
+	private String bridgeMoveOutputAndInput(){
+		outputView.printMove();
+		return inputView.readMoving();
+	}
+
+	private String compareInputAndIndex(String bridge_move,String list_index){
+		if (bridge_move.equals(list_index)&&list_index.equals("U")){
+			return "OU";
+		}
+		if (bridge_move.equals(list_index)&&list_index.equals("D")){
+			return "OD";
+		}
+		if (!bridge_move.equals(list_index)&&list_index.equals("U")){
+			return "XD";
+		}
+		return "XU";
+	}
+
+	private void printMapByStringBuilder(String bridge_move_result,int index){
+		StringBuilder stringBuilder = outputView.printMap(bridge_move_result, index);
+		System.out.println(stringBuilder);
+	}
 }
