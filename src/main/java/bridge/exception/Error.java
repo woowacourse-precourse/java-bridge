d710c069d680071346137589fd8c9bd86592ac35
im-shung <seoyoung1113@gmail.com>
2022-11-20 16:26:43 +0900

    refactor: :recycle: 다리의 길이를 하드코딩에서 BrideLength 값으로 리팩토링

--- src/main/java/bridge/exception/Error.java
+++ src/main/java/bridge/exception/Error.java
@@ -1,10 +1,12 @@
 package bridge.exception;
 
+import bridge.domain.BrideLength;
+
 public enum Error {
     NOT_INT("숫자를 입력하세요."),
-    WRONG_BRIDGE_LENGTH("다리 길이는 3부터 20 사이의 숫자여야 합니다.");
+    WRONG_BRIDGE_LENGTH("다리 길이는 " + BrideLength.MIN + "부터 " + BrideLength.MAX + " 사이의 숫자여야 합니다.");
 
-    private final String ERROR = "[ERROR]";
+    private final String ERROR = "[ERROR] ";
     private final String message;
 
     Error(String message) {
