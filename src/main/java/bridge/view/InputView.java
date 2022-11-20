061028f97360eb9ac3b8dd0c51f93ea2f79499bd
이건회 <rg970604@naver.com>
2022-11-20 18:47:51 +0900

    refactor(InputView.java) : static 선언

--- src/main/java/bridge/view/InputView.java
+++ src/main/java/bridge/view/InputView.java
@@ -11,7 +11,8 @@ public class InputView {
     /**
      * 다리의 길이를 입력받는다.
      */
-    public int readBridgeSize() {
+    public static int readBridgeSize() {
+        System.out.println("다리의 길이를 입력해주세요.");
         String bridgeSize = Console.readLine();
         Validator.validateNumericInputType(bridgeSize);
         int size = Integer.parseInt(bridgeSize);
@@ -22,7 +23,7 @@ public class InputView {
     /**
      * 사용자가 이동할 칸을 입력받는다.
      */
-    public String readMoving() {
+    public static String readMoving() {
         String input = Console.readLine();
         Validator.validateMovingInput(input);
         return input;
@@ -31,7 +32,8 @@ public class InputView {
     /**
      * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
      */
-    public String readGameCommand() {
+    public static String readGameCommand() {
+        System.out.println("게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)");
         String input = Console.readLine();
         Validator.validateGameCommandInput(input);
         return input;
