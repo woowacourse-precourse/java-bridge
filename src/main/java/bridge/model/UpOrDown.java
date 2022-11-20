89f534498c66ac8bf5e452fd4c776761fa6eec2e
UJ15 <dbwogml15@naver.com>
2022-11-20 15:24:10 +0900

    feat : create UpOrDown class

---
+++ src/main/java/bridge/model/UpOrDown.java
@@ -0,0 +1,39 @@
+package bridge.model;
+
+import bridge.error.Error;
+import java.util.Objects;
+
+public class UpOrDown {
+
+    public static final String UP_SIGN = "U";
+    public static final String DOWN_SIGN = "D";
+    private static final int VALUE_LENGTH = 1;
+
+    private final String value;
+
+    public UpOrDown(String value) {
+        if (Objects.isNull(value)) {
+            throw new IllegalArgumentException(Error.SYSTEM_ERROR.getMessage());
+        }
+        validate(value);
+
+        this.value = value;
+    }
+
+    public boolean isUp() {
+        return value.equals(UP_SIGN);
+    }
+
+    public boolean isDown() {
+        return value.equals(DOWN_SIGN);
+    }
+
+    private void validate(String value) {
+        if (value.length() != VALUE_LENGTH) {
+            throw new IllegalArgumentException(Error.DIRECTION_INPUT_ERROR.getMessage());
+        }
+        if (!value.equals(UP_SIGN) && !value.equals(DOWN_SIGN)) {
+            throw new IllegalArgumentException(Error.DIRECTION_INPUT_ERROR.getMessage());
+        }
+    }
+}
