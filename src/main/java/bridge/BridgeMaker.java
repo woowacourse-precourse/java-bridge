df83651e09e0b057bdf4660cf7414a3e03c59649
User <uoon97@hanmil.net>
2022-11-20 13:17:52 +0900

    feat: BridgeMaker) makeBridge(다리 만들기) 기능 구현

--- src/main/java/bridge/BridgeMaker.java
+++ src/main/java/bridge/BridgeMaker.java
@@ -1,6 +1,7 @@
 package bridge;
 
 import java.util.List;
+import java.util.stream.*;
 
 /**
  * 다리의 길이를 입력 받아서 다리를 생성해주는 역할을 한다.
@@ -18,6 +19,12 @@ public class BridgeMaker {
      * @return 입력받은 길이에 해당하는 다리 모양. 위 칸이면 "U", 아래 칸이면 "D"로 표현해야 한다.
      */
     public List<String> makeBridge(int size) {
-        return null;
+        List<Integer> bridgeGenerated = Stream.generate(()->
+                bridgeNumberGenerator.generate())
+                .limit(size).collect(Collectors.toList());
+        List<String> bridge = bridgeGenerated.stream()
+                .map(step -> (step == 0) ? "D" : "U")
+                .collect(Collectors.toList());
+        return bridge;
     }
 }
