9665d7f9d519695b544eabf15c5206caa18f99b1
3juhwan <13selfesteem91@naver.com>
2022-11-21 00:46:41 +0900

    feat(bridge): 게임 시작 로직 구현

--- src/main/java/bridge/controller/GameStart.java
+++ src/main/java/bridge/controller/GameStart.java
@@ -1,2 +1,22 @@
-package bridge.controller;public class GameStart {
+package bridge.controller;
+
+import bridge.domain.Bridge;
+import bridge.domain.BridgeMaker;
+import bridge.view.InputView;
+import bridge.view.OutputView;
+
+public class GameStart {
+    private static final InputView inputView = new InputView();
+    private static final OutputView outputView = new OutputView();
+
+    public static Bridge gameStart(BridgeMaker bridgeMaker) {
+        outputView.printStart();
+        while (true) {
+            try {
+                return new Bridge(bridgeMaker.makeBridge(inputView.readBridgeSize()));
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            }
+        }
+    }
 }
