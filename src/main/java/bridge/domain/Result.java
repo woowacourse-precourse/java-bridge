9494dc1d2400951d6870b3f9aaf240909e7ff886
situodys <situodys@gmail.com>
2022-11-20 17:39:31 +0900

    style(Result): status -> moveStatuses 변수명 변경

--- src/main/java/bridge/domain/Result.java
+++ src/main/java/bridge/domain/Result.java
@@ -6,33 +6,33 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Result {
-    private final List<MoveStatus> status;
+    private final List<MoveStatus> moveStatuses;
     private int location;
 
     public Result() {
-        this.status = new ArrayList<>();
+        this.moveStatuses = new ArrayList<>();
         this.location = 0;
     }
 
     public void update(Bridge bridge, Direction userDirection) {
-        status.add(MoveStatus.of(bridge.canMove(userDirection, location), userDirection));
+        moveStatuses.add(MoveStatus.of(bridge.canMove(userDirection, location), userDirection));
         location++;
     }
 
     public boolean isEnd(Bridge bridge) {
-        return !status.get(status.size() - 1).didCross() || bridge.isEnd(location);
+        return !moveStatuses.get(moveStatuses.size() - 1).didCross() || bridge.isEnd(location);
     }
 
     public boolean isSuccess(Bridge bridge) {
-        return status.get(status.size() - 1).didCross() && bridge.isEnd(location);
+        return moveStatuses.get(moveStatuses.size() - 1).didCross() && bridge.isEnd(location);
     }
 
     public void reset() {
-        this.status.clear();
-        this.location=0;
+        this.moveStatuses.clear();
+        this.location = 0;
     }
 
-    public List<MoveStatus> getStatus() {
-        return this.status;
+    public List<MoveStatus> getMoveStatuses() {
+        return this.moveStatuses;
     }
 }
