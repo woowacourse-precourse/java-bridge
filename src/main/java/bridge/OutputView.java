49e2705cde117c4de98022fb7b47a84f6fa18ec9
Ye Seok Hong <yshong1998@gmail.com>
2022-11-20 20:52:05 +0900

    Feat : 게임 진행 상황을 보여주는 기능 추가

--- src/main/java/bridge/OutputView.java
+++ src/main/java/bridge/OutputView.java
@@ -1,16 +1,25 @@
 package bridge;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * 사용자에게 게임 진행 상황과 결과를 출력하는 역할을 한다.
  */
 public class OutputView {
-
+    List<String> bridgeMap = new ArrayList<>();
     /**
      * 현재까지 이동한 다리의 상태를 정해진 형식에 맞춰 출력한다.
      * <p>
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void printMap() {
+    public void printMap(String playerMoving, String bridge) {
+        if(playerMoving.equals(bridge)) {
+            bridgeMap.add("O");
+        } else if(!playerMoving.equals(bridge)){
+            bridgeMap.add("X");
+        }
+        System.out.println(bridgeMap);
     }
 
     /**
@@ -19,5 +28,9 @@ public class OutputView {
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
     public void printResult() {
+    }
+
+    public void printGameStart(){
+        System.out.printf("다리 건너기 게임을 시작합니다.\n\n");
     }
 }
