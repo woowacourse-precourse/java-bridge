7c5cb21ba4193f79f46faebc1afc423d538dddec
chordpli <chordplaylist@gmail.com>
2022-11-21 02:22:33 +0900

    feat: printResult

--- src/main/java/bridge/OutputView.java
+++ src/main/java/bridge/OutputView.java
@@ -6,8 +6,6 @@ import java.util.List;
  * 사용자에게 게임 진행 상황과 결과를 출력하는 역할을 한다.
  */
 public class OutputView {
-
-
     /**
      * 현재까지 이동한 다리의 상태를 정해진 형식에 맞춰 출력한다.
      * <p>
@@ -42,6 +40,14 @@ public class OutputView {
      * <p>
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void printResult() {
+    public String printResult(BridgeGame bridgeGame, int bridgeSize, int count, String game) {
+        if (bridgeGame.getBridgeCount() == bridgeSize) {
+            System.out.println("최종 게임 결과");
+            printMap(bridgeGame.getUpBridge(), bridgeGame.getDownBridge());
+            System.out.println("게임 성공 여부: 성공");
+            System.out.println("총 시도한 횟수: " + count);
+            return "E";
+        }
+        return "R";
     }
 }
