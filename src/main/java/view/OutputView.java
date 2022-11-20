baf927b2fa572b74f4a6d1d38ab71fbd05d7a803
KangSungBum <peter9706@naver.com>
2022-11-20 11:02:28 +0900

    feat(OutputView.java): add printResult code
    printResult 메소드 코드 추가
    - 다리 정보를 받은 다음 출력 요구 사항에 맞게 변환하여 출력
    - 성공 및 도전 횟수 추가 필요

--- src/main/java/view/OutputView.java
+++ src/main/java/view/OutputView.java
@@ -1,5 +1,7 @@
 package view;
 
+import java.util.List;
+
 /**
  * 사용자에게 게임 진행 상황과 결과를 출력하는 역할을 한다.
  */
@@ -18,7 +20,29 @@ public class OutputView {
      * <p>
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void printResult() {
+    public void printResult(List<String> bridge) {
+        StringBuilder upBridge = new StringBuilder();
+        StringBuilder downBridge = new StringBuilder();
+
+        upBridge.append("[");
+        downBridge.append("[");
+
+        for (String unit : bridge) {
+            if (unit.equals("U")) {
+                upBridge.append(" O |");
+                downBridge.append("   |");
+                continue;
+            }
+
+            upBridge.append("   |");
+            downBridge.append(" O |");
+
+        }
+        upBridge.append("]");
+        downBridge.append("]");
+
+        System.out.println(upBridge.toString());
+        System.out.println(downBridge.toString());
     }
 
     public void printGameStartMessage() {
