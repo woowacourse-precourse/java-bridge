133f03cd6f77b759452f80310d0699083fdbe613
Identityless <rlandwnd555@naver.com>
2022-11-21 03:20:46 +0900

    fix : init() 메서드 접근 지정자 private로 변경

--- src/main/java/bridge/BridgeGame.java
+++ src/main/java/bridge/BridgeGame.java
@@ -31,14 +31,15 @@ public class BridgeGame {
      * 이동을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
     public void move() {
-        while(flag)
+        while(flag) {
             System.out.println("이동할 칸을 선택해주세요. (위: U, 아래: D)");
             move = inputView.readMoving();
-
         }
     }
 
-    public
+    public void buildUserMap() {
+
+    }
 
     /**
      * 사용자가 게임을 다시 시도할 때 사용하는 메서드
@@ -48,7 +49,7 @@ public class BridgeGame {
     public void retry() {
     }
 
-    public void init() {
+    private void init() {
         System.out.println("다리의 길이를 입력해주세요.");
         bridgesize = inputView.readBridgeSize();
         randomMap = bridgeMaker.makeBridge(bridgesize);
