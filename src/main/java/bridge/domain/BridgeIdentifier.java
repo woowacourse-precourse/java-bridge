64271a8ce33615b472cf62591817143d99d56609
kokodak <kokodakadokok@gmail.com>
2022-11-21 00:42:30 +0900

    feat(bridgeIdentifier): 0, 1 의 값을 통해서 U, D 를 찾는 기능 구현

--- src/main/java/bridge/domain/BridgeIdentifier.java
+++ src/main/java/bridge/domain/BridgeIdentifier.java
@@ -1,5 +1,7 @@
 package bridge.domain;
 
+import java.util.Arrays;
+
 public enum BridgeIdentifier {
 
     UP("U", 1),
@@ -11,5 +13,12 @@ public enum BridgeIdentifier {
     BridgeIdentifier(String valueOfString, int valueOfInt) {
         this.valueOfString = valueOfString;
         this.valueOfInt = valueOfInt;
+    }
+
+    public static String convertIntToString(int valueOfInt) {
+        return Arrays.stream(values())
+                .filter(identifier -> identifier.valueOfInt == valueOfInt)
+                .findFirst()
+                .get().valueOfString;
     }
 }
