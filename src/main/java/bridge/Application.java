221541696d2acf626e3d4a6678ab0901affa2a8f
NoseSSG <dmlcks1998@naver.com>
2022-11-21 00:02:36 +0900

    [REFACTOR] OutputView 파일은 출력만 수행하도록 수정

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -59,4 +59,45 @@ public class Application {
         output.printResult(Bridge_out,result,count);
     }
 
+    public String[] beforeMap(List<String> bridge_input){
+        String[] Bridge_out = new String[2];
+        Bridge_out[0] = ""; Bridge_out[1] ="";
+        for(int i = 0 ; i < bridge_input.size() - 1 ; i++){
+            Bridge_out = addResult(Bridge_out,bridge_input.get(i));
+            Bridge_out[0] += "|";
+            Bridge_out[1] += "|";
+        }
+        return Bridge_out;
+    }
+
+    public String[] addResult(String[] Bridge_out , String choice){
+        if(choice.equals("U")){
+            Bridge_out[0] += " O ";
+            Bridge_out[1] += "   ";
+        } else if(choice.equals("D")){
+            Bridge_out[0] += "   ";
+            Bridge_out[1] += " O ";
+        }
+        return Bridge_out;
+    }
+
+    public String[] addNow(String[] Brdige_out, String choice ,boolean same){
+        if(same == true){
+            Brdige_out = add_OorX(Brdige_out,choice," O ");
+        }else if(same == false){
+            Brdige_out = add_OorX(Brdige_out,choice," X ");
+        }
+        return Brdige_out;
+    }
+    public String[] add_OorX(String[] Bridge_out,String choice, String OorX){
+        if(choice.equals("U")){
+            Bridge_out[0] += OorX;
+            Bridge_out[1] += "   ";
+        }else if(choice.equals("D")){
+            Bridge_out[0] += "   ";
+            Bridge_out[1] += OorX;
+        }
+        return Bridge_out;
+    }
+
 }
--- src/main/java/bridge/OutputView.java
+++ src/main/java/bridge/OutputView.java
@@ -9,14 +9,15 @@ import java.util.List;
  * 사용자에게 게임 진행 상황과 결과를 출력하는 역할을 한다.
  */
 public class OutputView {
+    Application app = new Application();
     /**
      * 현재까지 이동한 다리의 상태를 정해진 형식에 맞춰 출력한다.
      * <p>
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
     public String[] printMap(List<String> bridge_input ,String choice, boolean same) {
-        String[] Bridge_out = beforeMap(bridge_input);
-        Bridge_out = addNow(Bridge_out,choice,same);
+        String[] Bridge_out = app.beforeMap(bridge_input);
+        Bridge_out = app.addNow(Bridge_out,choice,same);
         Bridge_out[0] = "[" + Bridge_out[0] + "]";
         Bridge_out[1] = "[" + Bridge_out[1] + "]";
         System.out.println(Bridge_out[0]);
@@ -36,46 +37,6 @@ public class OutputView {
         if(yes_no==true) System.out.println("게임 성공 여부: 성공");
         else if(yes_no == false) System.out.println("게임 성공 여부: 실패");
         System.out.println("총 시도한 횟수: " + count);
-    }
-    public String[] beforeMap(List<String> bridge_input){
-        String[] Bridge_out = new String[2];
-        Bridge_out[0] = ""; Bridge_out[1] ="";
-        for(int i = 0 ; i < bridge_input.size() - 1 ; i++){
-            Bridge_out = addResult(Bridge_out,bridge_input.get(i));
-            Bridge_out[0] += "|";
-            Bridge_out[1] += "|";
-        }
-        return Bridge_out;
-    }
-
-    public String[] addResult(String[] Bridge_out , String choice){
-        if(choice.equals("U")){
-            Bridge_out[0] += " O ";
-            Bridge_out[1] += "   ";
-        } else if(choice.equals("D")){
-            Bridge_out[0] += "   ";
-            Bridge_out[1] += " O ";
-        }
-        return Bridge_out;
-    }
-
-    public String[] addNow(String[] Brdige_out, String choice ,boolean same){
-        if(same == true){
-            Brdige_out = add_OorX(Brdige_out,choice," O ");
-        }else if(same == false){
-            Brdige_out = add_OorX(Brdige_out,choice," X ");
-        }
-        return Brdige_out;
-    }
-    public String[] add_OorX(String[] Bridge_out,String choice, String OorX){
-        if(choice.equals("U")){
-            Bridge_out[0] += OorX;
-            Bridge_out[1] += "   ";
-        }else if(choice.equals("D")){
-            Bridge_out[0] += "   ";
-            Bridge_out[1] += OorX;
-        }
-        return Bridge_out;
     }
 
     public void printStart(){
