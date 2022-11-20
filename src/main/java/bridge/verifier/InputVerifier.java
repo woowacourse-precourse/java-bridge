4dad0d917238fd4cb59ad19bbcde0e9302d56561
leeyongjun <leeyongjun0310@naver.com>
2022-11-21 07:10:42 +0900

    feat(bridge): 입력된 재시작/종료 여부가 R, Q가 아니면 예외가 발생하는 기능 구현

--- src/main/java/bridge/verifier/InputVerifier.java
+++ src/main/java/bridge/verifier/InputVerifier.java
@@ -4,6 +4,8 @@ public class InputVerifier {
     private static final String REGEX_BRIDGE_SIZE_RANGE = "^[3-9]{1}$|^1{1}[0-9]{1}$|^2{1}0{1}$";
     private static final String MOVING_UP = "U";
     private static final String MOVING_DOWN = "D";
+    private static final String GameCommand_RESTART = "R";
+    private static final String GameCommand_QUIT = "Q";
 
     public static void BridgeSize(String bridgeSize) {
         if (!bridgeSize.matches(REGEX_BRIDGE_SIZE_RANGE)) {
@@ -16,4 +18,11 @@ public class InputVerifier {
             throw new IllegalArgumentException("[ERROR] 이동할 칸 입력 오류");
         }
     }
+
+    public static void gameCommand(String gameCommand) {
+        if (!(gameCommand.equals(GameCommand_RESTART) || gameCommand.equals(GameCommand_QUIT))) {
+            throw new IllegalArgumentException("[ERROR] 게임을 다시 시도할지 입력 오류");
+        }
+    }
+
 }
--- src/main/java/bridge/view/InputView.java
+++ src/main/java/bridge/view/InputView.java
@@ -37,6 +37,8 @@ public class InputView {
      */
     public String readGameCommand() {
         System.out.println("게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)");
-        return Console.readLine();
+        String gameCommand = Console.readLine();
+        InputVerifier.gameCommand(gameCommand);
+        return gameCommand;
     }
 }
