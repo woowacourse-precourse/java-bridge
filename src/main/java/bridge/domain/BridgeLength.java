c317bb13b414c9d113324c1e93c8229ce65fd3e0
Isu Shin <isu010614@naver.com>
2022-11-20 18:10:02 +0900

    refactor: 다리의 길이를 사용할 때의 예외처리에 대한 클래스 분리
    -InputValidator 에서 BridgeLength 클래스로 분리함.

---
+++ src/main/java/bridge/domain/BridgeLength.java
@@ -0,0 +1,32 @@
+package bridge.domain;
+
+public class BridgeLength {
+    private static final String INTEGER_REGEX = "^[0-9]*$";
+    private static final int START_LENGTH_RANGE = 3;
+    private static final int END_LENGTH_RANGE = 20;
+
+    private int length;
+
+    private BridgeLength(int length) {
+        this.length = length;
+    }
+
+    public static BridgeLength createBridgeLength(String inputLength) {
+        validateNumeric(inputLength);
+        int length = Integer.parseInt(inputLength);
+        validateInputLength(length);
+        return new BridgeLength(length);
+    }
+
+    private static void validateInputLength(int length) {
+        if (length < START_LENGTH_RANGE || length > END_LENGTH_RANGE) {
+            throw new IllegalArgumentException(ExceptionMessage.NOT_IN_RANGE);
+        }
+    }
+
+    private static void validateNumeric(String inputLength) {
+        if (!inputLength.matches(INTEGER_REGEX)) {
+            throw new IllegalArgumentException(ExceptionMessage.NOT_NUMBER);
+        }
+    }
+}
--- src/main/java/bridge/view/InputValidator.java
+++ src/main/java/bridge/view/InputValidator.java
@@ -4,27 +4,6 @@ import bridge.domain.ExceptionMessage;
 import bridge.domain.MovingStatus;
 
 public class InputValidator {
-   static final int START_LENGTH_RANGE = 3;
-   static final int END_LENGTH_RANGE = 20;
-
-    public void validateInputLength(String inputLength) {
-        if (!isNumber(inputLength)){
-            throw new IllegalArgumentException(ExceptionMessage.NOT_NUMBER);
-        }
-        if(!isInRange(inputLength)){
-            throw new IllegalArgumentException(ExceptionMessage.NOT_IN_RANGE);
-        }
-    }
-
-    private boolean isNumber(String inputLength){
-        return inputLength.matches("^[0-9]*$");
-    }
-
-    private boolean isInRange(String inputLength){
-        int bridgeLength = Integer.parseInt(inputLength);
-        return bridgeLength >= START_LENGTH_RANGE && bridgeLength <= END_LENGTH_RANGE;
-    }
-
     public void validateMoving(String moving){
         if(!isUpOrDown(moving)){
             throw new IllegalArgumentException(ExceptionMessage.NOT_UP_OR_DOWN);
