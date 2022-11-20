df741b995ebcc0009d777884c94002dc9e49cc52
kokodak <kokodakadokok@gmail.com>
2022-11-20 16:47:50 +0900

    feat(bridgeGame): 필드 값에 대한 getter 메서드 추가

--- src/main/java/bridge/domain/BridgeGame.java
+++ src/main/java/bridge/domain/BridgeGame.java
@@ -1,6 +1,7 @@
 package bridge.domain;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 /**
@@ -43,5 +44,17 @@ public class BridgeGame {
      * 재시작을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
     public void retry() {
+    }
+
+    public List<String> getMatchingStatus() {
+        return Collections.unmodifiableList(matchingStatus);
+    }
+
+    public int getTotalAttempts() {
+        return totalAttempts;
+    }
+
+    public boolean isGameFailed() {
+        return gameFailed;
     }
 }
