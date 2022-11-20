9d0dbf18437656025d8e2ab1cb39d89452e268d8
Sangkyun Seo <tkdrbs0802@naver.com>
2022-11-20 16:13:34 +0900

    feat: 다리 건넘 유무 및 다리 구분선 출력 메서드 작성

--- src/main/java/bridge/BridgeGame.java
+++ src/main/java/bridge/BridgeGame.java
@@ -12,10 +12,10 @@ public class BridgeGame {
      * <p>
      * 이동을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public boolean move(List<String> bridge, int index, String moveBrdige) {
+    public String move(List<String> bridge, int index, String moveBrdige) {
         if(bridge.get(index).equals(moveBrdige))
-            return true;
-        return false;
+            return "O";
+        return "X";
     }
 
     /**
