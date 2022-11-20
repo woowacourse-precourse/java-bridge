6a9d3c4dc8221b92ce5a64744785a6551805752c
nylonmusk <106235834+nylonmusk@users.noreply.github.com>
2022-11-20 22:29:36 +0900

    refactor(InputView): BridgeSizeValidator로 판별되게 수정 및 클래스 분리

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -1,10 +1,10 @@
 package view;
 
-import constant.ErrorMessage;
-import constant.GameConstant;
-import constant.StringConstant;
-
 import camp.nextstep.edu.missionutils.Console;
+import util.BridgeSizeValidator;
+import util.Converter;
+import util.GameCommandValidator;
+import util.MovingValidator;
 
 public class InputView {
     private static final InputView inputView = new InputView();
@@ -18,55 +18,19 @@ public class InputView {
 
     public int readBridgeSize() throws IllegalArgumentException {
         String size = Console.readLine();
-        isValidBridgeSize(size);
-        return Integer.parseInt(size);
+        BridgeSizeValidator.isValid(size);
+        return Converter.convertStringToInt(size);
     }
 
     public String readMoving() throws IllegalArgumentException {
-        String moving = Console.readLine();
-        isValidMove(moving);
+        String moving = Converter.convertToUpperCase(Console.readLine());
+        MovingValidator.isValid(moving);
         return moving;
     }
 
     public String readGameCommand() throws IllegalArgumentException {
-        String gameCommand = Console.readLine();
-        isValidRetryOrQuit(gameCommand);
+        String gameCommand = Converter.convertToUpperCase(Console.readLine());
+        GameCommandValidator.isValid(gameCommand);
         return gameCommand;
-    }
-
-    public int convertStringToInt(String size) {
-        return Integer.parseInt(size);
-    }
-    
-    public void isValidBridgeSize(String size) throws IllegalArgumentException {
-        isBridgeSizeComponentNumber(size);
-        isValidBridgeSizeRange(convertStringToInt(size));
-    }
-
-    public void isBridgeSizeComponentNumber(String size) {
-        if (!size.matches(StringConstant.BRIDGE_SIZE_REGEX.getConstant())) {
-            throw new IllegalArgumentException(ErrorMessage.BRIDGE_SIZE_NOT_NUMBER.getErrorMessage());
-        }
-    }
-
-    public void isValidBridgeSizeRange(int size) {
-        if (GameConstant.BRIDGE_SIZE_START_INCLUSIVE.getConstant() > size
-                || size > GameConstant.BRIDGE_SIZE_END_INCLUSIVE.getConstant()) {
-            throw new IllegalArgumentException(ErrorMessage.BRIDGE_SIZE_INVALID_RANGE.getErrorMessage());
-        }
-    }
-
-    public void isValidMove(String moving) {
-        if (!moving.equals(StringConstant.UP_SIDE.getConstant())
-                && !moving.equals(StringConstant.DOWN_SIDE.getConstant())) {
-            throw new IllegalArgumentException(ErrorMessage.MOVE_INVALID.getErrorMessage());
-        }
-    }
-
-    public void isValidRetryOrQuit(String gameCommand) {
-        if (!gameCommand.equals(StringConstant.QUIT.getConstant())
-                && !gameCommand.equals(StringConstant.RETRY.getConstant())) {
-            throw new IllegalArgumentException(ErrorMessage.RETRY_OR_QUIT_INVALID.getErrorMessage());
-        }
     }
 }
