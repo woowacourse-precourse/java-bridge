38f8a9dfb523053dec9270d185bf1353333b8974
SonMinGyu <littleking13@naver.com>
2022-11-20 22:13:35 +0900

    [FEAT] 게임 시작 문구 출력 기능 구현
    - 게임 시작시 시작문구를 출력하는 기능을 구현함

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -4,5 +4,12 @@ public class Application {
 
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+
+        printStart();
+
+    }
+
+    public static void printStart() {
+        System.out.println("다리 건너기 게임을 시작합니다.");
     }
 }
