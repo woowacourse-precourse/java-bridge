ba349fd4559d1d847fa00bc5a41021f08cb68385
taekwon-dev <taekwon_dev@naver.com>
2022-11-20 21:44:38 +0900

    refactor: (OutputView) 다리 건너기 게임 결과 출력 시 형식 지정

--- src/main/java/bridge/view/output/OutputView.java
+++ src/main/java/bridge/view/output/OutputView.java
@@ -12,8 +12,11 @@ public class OutputView {
 
     private static final String NOTIFY_GAME_START = "다리 건너기 게임을 시작합니다.";
     private static final String NOTIFY_INPUT_BRIDGE_LENGTH = "다리의 길이를 입력해주세요.";
-    private static final String NOTIFY_INPUT_ARROW = "이동할 칸을 선택해주세요.";
+    private static final String NOTIFY_INPUT_ARROW = "이동할 칸을 선택해주세요. (위: U, 아래: D)";
     private static final String NOTIFY_INPUT_RETRY_OR_NOT = "게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)";
+    private static final String MAP_RESULT = "최종 게임 결과";
+    private static final String GAME_RESULT = "게임 성공 여부: %s%n";
+    private static final String TOTAL_GAME_ATTEMPTS = "총 시도한 횟수: %d";
 
     public void printGameStartNotification() {
         System.out.println(NOTIFY_GAME_START + "\n");
@@ -39,6 +42,7 @@ public class OutputView {
     public void printMap(BridgeState bridgeState) {
         System.out.println(OPEN_SQUARE_BRACKETS + bridgeState.getUpBridgeState() + CLOSE_SQUARE_BRACKETS);
         System.out.println(OPEN_SQUARE_BRACKETS + bridgeState.getDownBridgeState() + CLOSE_SQUARE_BRACKETS);
+        System.out.println();
     }
 
     /**
@@ -47,18 +51,19 @@ public class OutputView {
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
     public void printResult(BridgeState bridgeState) {
+        System.out.println(MAP_RESULT);
         printMap(bridgeState);
     }
 
     public void printGameResult(BridgeGame bridgeGame) {
         if (bridgeGame.isWon()) {
-            System.out.println(GAME_RESULT_SUCCESS);
+            System.out.printf(GAME_RESULT, GAME_RESULT_SUCCESS);
             return;
         }
-        System.out.println(GAME_RESULT_FAILURE);
+        System.out.printf(GAME_RESULT, GAME_RESULT_FAILURE);
     }
 
     public void printTotalGameAttempts(BridgeGame bridgeGame) {
-        System.out.println(bridgeGame.getTotalGameAttempts());
+        System.out.printf(TOTAL_GAME_ATTEMPTS, bridgeGame.getTotalGameAttempts());
     }
 }
