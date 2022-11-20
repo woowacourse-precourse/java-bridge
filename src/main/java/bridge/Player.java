fccbbcc308a05b4d2d19db1f4b05d30a508ef768
heesang <dlagmltkd12@naver.com>
2022-11-20 22:08:14 +0900

    refactor: Player 디폴트 게임 시도 횟수 매직 넘버 포장

--- src/main/java/bridge/Player.java
+++ src/main/java/bridge/Player.java
@@ -7,12 +7,13 @@ import java.util.List;
 
 public class Player {
 
+    public static final int DEFAULT_GAME_ATTEMPT = 1;
     private final List<BridgeMark> record;
     private int attempt;
 
     public Player() {
         this.record = new ArrayList<>();
-        this.attempt = 1;
+        this.attempt = DEFAULT_GAME_ATTEMPT;
     }
 
     public void record(BridgeMark bridgeMark) {
