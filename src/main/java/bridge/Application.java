217afc7fcc4808e149839d510506eddeec9b53ad
이현진 <ehj1107@naver.com>
2022-11-20 14:59:03 +0900

    feat: 메인 함수에서 게임 진행하는 기능 추가

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -1,8 +1,40 @@
 package bridge;
 
+import java.util.List;
+import java.util.ArrayList;
+
 public class Application {
+    static int bridgeSize;
+    static List<String> bridge;
+    static List<String> playerMove;
+    static InputView inputview = new InputView();
+    static BridgeMaker bridgeMaker = new BridgeMaker(new BridgeRandomNumberGenerator());
+    static BridgeGame bridgeGame = new BridgeGame();
 
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        System.out.println("다리 건너기 게임을 시작합니다.");
+        GetBridge();
+        GameStart();
+    }
+
+    private static void GetBridge() {
+        GetBridgeSize();
+        bridge = bridgeMaker.makeBridge(bridgeSize);
+    }
+
+    private static void GetBridgeSize() {
+        bridgeSize = inputview.readBridgeSize();
+    }
+
+    private static void GameStart() {
+        playerMove = new ArrayList<>();
+        while(true){
+            String move = inputview.readMoving();
+            CheckMove(move);
+        }
+    }
+
+    private static void CheckMove(String move) {
+        playerMove.add(move);
     }
 }
