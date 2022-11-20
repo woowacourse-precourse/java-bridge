2fe89d0b95bf4640dc559806d0b0a3116fbf6fee
yenawee <yaena0319@naver.com>
2022-11-20 15:46:53 +0900

    feat : 다음 이동할 칸, 게임 재시작 여부 입력

--- src/main/java/bridge/InputView.java
+++ src/main/java/bridge/InputView.java
@@ -7,11 +7,14 @@ import camp.nextstep.edu.missionutils.Console;
  */
 public class InputView {
 
+    public String readline(){
+        String input = Console.readLine();
+        return (input);
+    }
     /**
      * 다리의 길이를 입력받는다.
      */
-    public int readBridgeSize() {
-        String input = Console.readLine();
+    public int readBridgeSize(String input) {
         int bridgeSize = Integer.parseInt(input);
         if (bridgeSize < 3 || bridgeSize > 20){
             throw new IllegalArgumentException("3 이상 20 이하의 숫자여야 합니다.");
@@ -22,14 +25,20 @@ public class InputView {
     /**
      * 사용자가 이동할 칸을 입력받는다.
      */
-    public String readMoving() {
-        return null;
+    public String readMoving(String input) {
+        if (!input.equals("U") && !input.equals("D")){
+            throw new IllegalArgumentException();
+        }
+        return (input);
     }
 
     /**
      * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
      */
-    public String readGameCommand() {
-        return null;
+    public String readGameCommand(String input) {
+        if (!input.equals("R") && !input.equals("Q")){
+            throw new IllegalArgumentException();
+        }
+        return (input);
     }
 }
