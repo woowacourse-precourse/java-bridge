98c82376b8f1c7fcd98c4f62fe867d932f35b182
YongGwan <32181771@dankook.ac.kr>
2022-11-20 20:22:26 +0900

    feat : 클래스 구조 설정, 시작 로고 출력

---
+++ src/main/java/Game/Controller.java
@@ -0,0 +1,11 @@
+package Game;
+
+import View.OutputView;
+
+public class Controller {
+
+   public void run() {
+       OutputView outputView = new OutputView();
+       outputView.printStartMsg();
+   }
+}
--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -1,9 +1,11 @@
 package bridge;
 
+import Game.Controller;
+
 public class Application {
 
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
-
+        Controller controller = new Controller();
+        controller.run();
     }
 }
