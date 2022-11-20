7184d9f28b3e0becfda68bfba548cfca7c0b6344
Dan-HeeYoung9182 <ldan9182@gmail.com>
2022-11-21 08:15:46 +0900

    갈수 없는 칸을 선택했을 경우, 유저 인풋이 R 또는 Q가 아니라면 IllegalArgumentException 발생 테스트 코드

--- src/test/java/bridge/BridgeGameTest.java
+++ src/test/java/bridge/BridgeGameTest.java
@@ -9,14 +9,14 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class BridgeGameTest extends NsTest {
 
-//    @DisplayName("제시작때 받는 인풋이 R 또는 Q가 아닐경우에는 IllegalArgumentException 발생")
-//    @ValueSource(strings = {"U", "D", "100", "a", "r", "q", "!"})
-//    @ParameterizedTest
-//    void 제시작_테스트(String input) {
-//        BridgeGame bg = new BridgeGame();
-//        assertThatThrownBy(() -> bg.retry(input))
-//                .isInstanceOf(IllegalArgumentException.class);
-//    }
+    @DisplayName("제시작때 받는 인풋이 R 또는 Q가 아닐경우에는 IllegalArgumentException 발생")
+    @ValueSource(strings = {"U", "D", "100", "a", "r", "q", "!"})
+    @ParameterizedTest
+    void 제시작_테스트(String input) {
+        BridgeGame bg = new BridgeGame();
+        assertThatThrownBy(() -> bg.retry(input))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
     @Override
     protected void runMain() {}
 }
