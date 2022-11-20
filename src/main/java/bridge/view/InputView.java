90dc299d7b611abead9a1934ca566c1dcae6d78d
GilHun <suleemon1@naver.com>
2022-11-20 19:53:37 +0900

    feat(InputView): 입력 및 검증 기능 구현

--- src/main/java/bridge/view/InputView.java
+++ src/main/java/bridge/view/InputView.java
@@ -5,14 +5,14 @@ import camp.nextstep.edu.missionutils.Console;
  * 사용자로부터 입력을 받는 역할을 한다.
  */
 public class InputView {
-    private final String INPUT_BRIDGE_SIZE = "다리의 길이를 입력해주세요.";
-    private final String INPUT_MOVE_SQUARE = "이동할 칸을 선택해주세요. (위: U, 아래: D)";
-    private final String INPUT_STOP_RETRY = "게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)";
+    private static final String INPUT_BRIDGE_SIZE = "다리의 길이를 입력해주세요.";
+    private static final String INPUT_MOVE_SQUARE = "이동할 칸을 선택해주세요. (위: U, 아래: D)";
+    private static final String INPUT_STOP_RETRY = "게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)";
 
     /**
      * 다리의 길이를 입력받는다.
      */
-    public int readBridgeSize() throws IllegalArgumentException{
+    public static int readBridgeSize() throws IllegalArgumentException{
         System.out.println(INPUT_BRIDGE_SIZE);
         int bridgeSize = 0;
         try{
@@ -27,34 +27,34 @@ public class InputView {
     /**
      * 사용자가 이동할 칸을 입력받는다.
      */
-    public String readMoving() throws IllegalArgumentException{
+    public static String readMoving() throws IllegalArgumentException{
         System.out.println(INPUT_MOVE_SQUARE);
         String moveSquare = Console.readLine();
-        if(!upAndDownValidate(moveSquare)) throw new IllegalArgumentException("[ERROR] U 또는 D를 입력해주세요.");
+        if(!movingValidate(moveSquare)) throw new IllegalArgumentException("[ERROR] U 또는 D를 입력해주세요.");
         return moveSquare;
     }
 
     /**
      * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
      */
-    public String readGameCommand() throws IllegalArgumentException{
+    public static String readGameCommand() throws IllegalArgumentException{
         System.out.println(INPUT_STOP_RETRY);
-        String stopOrRetry = Console.readLine();
-        if(!stopOrRetryValidate(stopOrRetry)) throw new IllegalArgumentException("[ERROR] R 또는 Q를 입력해주세요.");
-        return null;
+        String gameCommand = Console.readLine();
+        if(!gameCommandValidate(gameCommand)) throw new IllegalArgumentException("[ERROR] R 또는 Q를 입력해주세요.");
+        return gameCommand;
     }
 
-    public boolean sizeRangeValidate(int size){
+    public static boolean sizeRangeValidate(int size){
         if(size >= 3 && size <= 20) return true;
         return false;
     }
 
-    public boolean upAndDownValidate(String moveSquare){
+    public static boolean movingValidate(String moveSquare){
         if(moveSquare.equals("D") || moveSquare.equals("U")) return true;
         return false;
     }
 
-    public boolean stopOrRetryValidate(String stopOrRetry){
+    public static boolean gameCommandValidate(String stopOrRetry){
         if(stopOrRetry.equals("R") || stopOrRetry.equals("Q")) return true;
         return false;
     }
