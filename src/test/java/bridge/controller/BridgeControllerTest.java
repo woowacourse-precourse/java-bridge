51f9141cec3b26d853b16174d7ecfaf2b086a150
yoonjuho <dbswn990@naver.com>
2022-11-20 18:24:48 +0900

    test(BridgeControllerTest): 이동할 때 특정 문자열만 입력된다

--- src/test/java/bridge/controller/BridgeControllerTest.java
+++ src/test/java/bridge/controller/BridgeControllerTest.java
@@ -9,11 +9,18 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class BridgeControllerTest {
 
-    @DisplayName("다리의 길이를 숫자로 입력받지 않는 경우 예외처리.")
+    @DisplayName("다리의 길이를 숫자로 입력 받지 않는 경우 예외처리.")
     @Test
     void inputBridgeLengthByNotNumeric() {
         BridgeController bridgeController = new BridgeController();
         assertThatThrownBy(() -> bridgeController.validateNumeric("h"))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+    @DisplayName("이동할 때 다른 문자가 입력 되는 경우 예외처리.")
+    @Test
+    void inputMoveLetterByAnotherLetter() {
+        BridgeController bridgeController = new BridgeController();
+        assertThatThrownBy(() -> bridgeController.validateLetter("a"))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 }
