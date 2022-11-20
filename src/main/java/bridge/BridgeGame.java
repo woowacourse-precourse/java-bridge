da2a36af4cbe778af664ac94c3cbffbfb3117021
ckn1070 <ckn1070@naver.com>
2022-11-20 16:22:33 +0900

    fix(bridge): modify '==' sign to equals

--- src/main/java/bridge/BridgeGame.java
+++ src/main/java/bridge/BridgeGame.java
@@ -31,14 +31,14 @@ public class BridgeGame {
     }
 
     public String getMoveUPStatus(int position, String userInput) {
-        if (userInput == bridgeShape.get(position)) {
+        if (userInput.equals(bridgeShape.get(position))) {
             return MoveStatus.UP_CORRECT.get();
         }
         return MoveStatus.UP_INCORRECT.get();
     }
 
     public String getMoveDownStatus(int position, String userInput) {
-        if (userInput == bridgeShape.get(position)) {
+        if (userInput.equals(bridgeShape.get(position))) {
             return MoveStatus.DOWN_CORRECT.get();
         }
         return MoveStatus.DOWN_INCORRECT.get();
@@ -50,7 +50,7 @@ public class BridgeGame {
      * 재시작을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
     public boolean retry(String userInput) {
-        if (userInput == Retry.QUIT.get()) {
+        if (userInput.equals(Retry.QUIT.get())) {
             return false;
         }
         return true;
