41a4626d607d97822fea43a2fed0eba5061a1e1c
ESeokhwan <huan99@hanmail.net>
2022-11-20 15:37:04 +0900

    feat: 다리 생성 String List 생성 기능 추가

--- src/main/java/bridge/model/BridgeMaker.java
+++ src/main/java/bridge/model/BridgeMaker.java
@@ -2,6 +2,7 @@ package bridge.model;
 
 import bridge.BridgeNumberGenerator;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -20,6 +21,21 @@ public class BridgeMaker {
      * @return 입력받은 길이에 해당하는 다리 모양. 위 칸이면 "U", 아래 칸이면 "D"로 표현해야 한다.
      */
     public List<String> makeBridge(int size) {
-        return null;
+        List<String> output = new ArrayList<>();
+        for(int i = 0; i < size; i++) {
+            int bridgeNumber = bridgeNumberGenerator.generate();
+            output.add(getBridgeInfoText(bridgeNumber));
+        }
+        return output;
+    }
+
+    private String getBridgeInfoText(int number) {
+        if(number == 0) {
+            return "D";
+        }
+        if(number == 1) {
+            return "U";
+        }
+        throw new IllegalArgumentException("다리를 생성할 때는 1 혹은 0의 숫자 중 하나를 이용해 생성합니다.");
     }
 }
