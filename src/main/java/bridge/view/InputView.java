8d040fd444a2798513684294729051547a18ac1f
leesangjin <tkdwls0628@gmail.com>
2022-11-21 01:00:49 +0900

    feat(InputView): 이동할 칸 입력 및 검증 기능 구현

--- src/main/java/bridge/view/InputView.java
+++ src/main/java/bridge/view/InputView.java
@@ -1,11 +1,13 @@
 package bridge.view;
 
 import bridge.validator.BridgeSizeValidator;
+import bridge.validator.MovingValidator;
 import camp.nextstep.edu.missionutils.Console;
 
 public class InputView {
 
     private static final String REQUEST_BRIDGE_SIZE = "다리의 길이를 입력해주세요.";
+    private static final String REQUEST_MOVING = "이동할 칸을 선택해주세요. (위: U, 아래: D)";
 
     public static int readBridgeSize() {
         System.out.println(REQUEST_BRIDGE_SIZE);
@@ -15,7 +17,10 @@ public class InputView {
     }
 
     public static String readMoving() {
-        return null;
+        System.out.println(REQUEST_MOVING);
+        String input = Console.readLine();
+        MovingValidator.validateMoving(input);
+        return input;
     }
 
     public static String readGameCommand() {
