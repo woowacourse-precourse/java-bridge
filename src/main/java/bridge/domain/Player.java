5e3bc658c848a0dd1b86c15a0fb2b66fa604b4b8
2jun0 <soo28819@gmail.com>
2022-11-20 12:05:44 +0900

    Refactor(Player): movingMap -> movingList and remove unused methods.

--- src/main/java/bridge/domain/Player.java
+++ src/main/java/bridge/domain/Player.java
@@ -4,33 +4,30 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Player {
-    private final List<String> movingMap;
+
+    private final List<String> movingList;
 
     public Player() {
-        movingMap = new ArrayList<>();
+        movingList = new ArrayList<>();
     }
 
     public void move(String moving) {
-        movingMap.add(moving);
-    }
-
-    public List<String> getMovingMap() {
-        return movingMap;
+        movingList.add(moving);
     }
 
     public String getLatestMoving() {
-        if (movingMap.size() == 0) {
+        if (movingList.size() == 0) {
             throw new IllegalStateException("아직 이동하지 않았습니다. 한번 이상은 이동 해야 합니다.");
         }
 
-        return movingMap.get(getPosition());
+        return movingList.get(getPosition());
     }
 
     public int getPosition() {
-        return movingMap.size() - 1;
+        return movingList.size() - 1;
     }
 
     public void reset() {
-        movingMap.clear();
+        movingList.clear();
     }
 }
