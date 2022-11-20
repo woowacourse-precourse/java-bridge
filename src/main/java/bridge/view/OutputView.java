29aa41f7ad1d5bc66749657265da986a1fe1f6d6
marinesnow34 <m4rinesnow34@gmail.com>
2022-11-21 04:19:14 +0900

    feat: 사용자 이동 결과 출력

--- src/main/java/bridge/view/OutputView.java
+++ src/main/java/bridge/view/OutputView.java
@@ -1,16 +1,34 @@
 package bridge.view;
 
+import java.util.List;
+import java.util.StringJoiner;
+
 /**
  * 사용자에게 게임 진행 상황과 결과를 출력하는 역할을 한다.
  */
 public class OutputView {
+    private static final String FORMAT_MAP_START = "[ ";
+    private static final String FORMAT_MAP_DIVISION = " | ";
+    private static final String FORMAT_MAP_END = " ]";
 
     /**
      * 현재까지 이동한 다리의 상태를 정해진 형식에 맞춰 출력한다.
      * <p>
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void printMap() {
+    public void printMap(List<List<String>> gameMap) {
+        for (List<String> gameBridge : gameMap) {
+            String playerMovingMap = toBridgeFormat(gameBridge);
+            System.out.println(playerMovingMap);
+        }
+    }
+
+    private String toBridgeFormat(List<String> bridge) {
+        StringJoiner stringJoiner = new StringJoiner(FORMAT_MAP_DIVISION, FORMAT_MAP_START, FORMAT_MAP_END);
+        for (String step : bridge) {
+            stringJoiner.add(step);
+        }
+        return stringJoiner.toString();
     }
 
     /**
