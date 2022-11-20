d074fcf1725d0d51b6efb2f56513e8974a1d7168
KangSungBum <peter9706@naver.com>
2022-11-21 01:48:56 +0900

    feat(OutputView.java): code clean up for printMap
    사용자 이동 입력에 따른 다리 출력 모듈화
    - initTopBridge: 위 다리 초기화
    - initBottomBridge: 아래 다리 초기화
    - closeTopBridge: 위 다리 출력 종료
    - closeBottomBridge: 아래 다리 출력 종료

--- src/main/java/view/OutputView.java
+++ src/main/java/view/OutputView.java
@@ -12,24 +12,116 @@ public class OutputView {
      * <p>
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void printMap(List<String> bridge, int index) {
-        StringBuilder upBridge = new StringBuilder();
-        StringBuilder downBridge = new StringBuilder();
-        upBridge.append("[");
-        downBridge.append("[");
-        for (int i = 0; i < index; i++) {
-            if (bridge.get(i).equals("U")) {
-                upBridge.append(" O ");
-                downBridge.append("   ");
+    public void printMap(List<String> bridge, List<String> userBridge) {
+        StringBuilder topBridge = initTopBridge();
+        StringBuilder bottomBridge = initBottomBridge();
+
+        for (int i = 0; i < userBridge.size(); i++) {
+            if (userBridge.get(i).equals("U")) {
+                if (bridge.get(i).equals("U")) {
+                    topBridge.append(" O ");
+                    bottomBridge.append("   ");
+                    topBridge.append("|");
+                    bottomBridge.append("|");
+                    continue;
+                }
+                topBridge.append(" X ");
+                bottomBridge.append("   ");
+                topBridge.append("|");
+                bottomBridge.append("|");
                 continue;
+            }else{
+                if (bridge.get(i).equals("U")) {
+                    topBridge.append(" X ");
+                    bottomBridge.append("   ");
+                    topBridge.append("|");
+                    bottomBridge.append("|");
+                    continue;
+                }
+                topBridge.append(" O ");
+                bottomBridge.append("   ");
+                topBridge.append("|");
+                bottomBridge.append("|");
             }
 
-            upBridge.append("   ");
-            downBridge.append(" O ");
         }
 
-        upBridge.append("]");
-        downBridge.append("]");
+        topBridge = printTopBridgeStatus(bridge, userBridge, topBridge);
+        bottomBridge = printBottomBridgeStatus(bridge, userBridge, bottomBridge);
+
+        System.out.println(closeTopBridge(topBridge));
+        System.out.println(closeBottomBridge(bottomBridge));
+    }
+
+    /**
+     * printMap을 위한 methods
+     * initTopBridge(),initBottomBridge() - 위, 아래 두개의 다리 시작("[")
+     * closeTopBridge(), closeBottomBridge() - 위, 아래 두개의 다리 끝("]")
+     * printBridgeMapStatus() - 사용자 입력 다리 현 상태 출력
+     */
+    private StringBuilder initTopBridge() {
+        StringBuilder topBridge = new StringBuilder();
+        topBridge.append("[");
+
+        return topBridge;
+    }
+
+    private StringBuilder initBottomBridge() {
+        StringBuilder bottomBridge = new StringBuilder();
+        bottomBridge.append("[");
+
+        return bottomBridge;
+    }
+
+    private String closeTopBridge(StringBuilder topBridge) {
+        topBridge.append("]");
+
+        return topBridge.toString();
+    }
+
+    private String closeBottomBridge(StringBuilder bottomBridge) {
+        bottomBridge.append("]");
+
+        return bottomBridge.toString();
+    }
+
+    private StringBuilder printTopBridgeStatus(List<String> answerBridge, List<String> userBridge, StringBuilder topBridge) {
+        for (int i = 0; i < userBridge.size(); i++) {
+            if (checkIfUserInputIsCorrect(answerBridge.get(i), userBridge.get(i))) {
+                topBridge.append(" O ");
+                continue;
+            }
+            topBridge.append("   ");
+
+            if (i == userBridge.size() - 1) {
+                break;
+            }
+            topBridge.append("|");
+        }
+        return topBridge;
+    }
+
+    private StringBuilder printBottomBridgeStatus(List<String> answerBridge, List<String> userBridge, StringBuilder bottomBridge) {
+        for (int i = 0; i < userBridge.size(); i++) {
+            if (checkIfUserInputIsCorrect(answerBridge.get(i), userBridge.get(i))) {
+                bottomBridge.append(" O ");
+                continue;
+            }
+            bottomBridge.append("   ");
+
+            if (i == userBridge.size() - 1) {
+                break;
+            }
+            bottomBridge.append("|");
+        }
+        return bottomBridge;
+    }
+
+    private boolean checkIfUserInputIsCorrect(String answerBridge, String userBridge) {
+        if (answerBridge.equals(userBridge)) {
+            return true;
+        }
+        return false;
     }
 
     /**
@@ -40,7 +132,7 @@ public class OutputView {
     public void printResult(List<String> bridge, boolean successOrNot, int trialCount) {
         System.out.println("최종 게임 결과");
 
-        printMap(bridge,0);
+//        printMap(bridge,0);
 
         if (successOrNot) {
             printSuccessMessage(trialCount);
@@ -52,12 +144,12 @@ public class OutputView {
     /**
      * Print Game Result Message
      */
-    public void printSuccessMessage(int trial) {
+    private void printSuccessMessage(int trial) {
         System.out.println("게임 성공 여부: 성공");
         System.out.println("총 시도한 횟수: "+ trial);
     }
 
-    public void printFailureMessage(int trial) {
+    private void printFailureMessage(int trial) {
         System.out.println("게임 성공 여부: 실패");
         System.out.println("총 시도한 횟수: " + trial);
     }
