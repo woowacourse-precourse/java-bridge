08e93b72c709b6ad881fa083e937354e9d3a1780
황민주 <minjoo@hwangminjooui-MacBookAir.local>
2022-11-20 11:21:04 +0900

    feat(InputView): readInput (src/main/java/bridge/view/InputView.java)
    readInput 메소드 추가:
    -사용자가 입력한 문자열을 받기 위해 추가함

--- src/main/java/bridge/view/InputView.java
+++ src/main/java/bridge/view/InputView.java
@@ -1,9 +1,16 @@
 package bridge.view;
 
+import camp.nextstep.edu.missionutils.Console;
+
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
  */
 public class InputView {
+
+    public String readInput(){
+        String input = Console.readLine();
+        return input;
+    }
 
     /**
      * 다리의 길이를 입력받는다.
