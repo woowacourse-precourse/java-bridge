fbe3fab94c3d6454c89c6c88d39b5579ac6d1fd7
bumwoo <gld55602@gmail.com>
2022-11-20 19:26:19 +0900

    refactor(Constant): O, X 상수 클래스 관리

--- src/main/java/bridge/BridgeMaker.java
+++ src/main/java/bridge/BridgeMaker.java
@@ -1,5 +1,6 @@
 package bridge;
 
+import bridge.constant.Direction;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
---
+++ src/main/java/bridge/constant/Constant.java
@@ -0,0 +1,7 @@
+package bridge.constant;
+
+public class Constant {
+    public static final String CROSS = "O";
+    public static final String CROSS_FAIL = "X";
+    public static final String BLANK = " ";
+}
