b5e3db0be81d79445eb758e587d78825f5ac8121
lcw3176 <lcw3176@naver.com>
2022-11-20 11:23:45 +0900

    refactor(Retry): Retry convert 메소드 추가

--- src/main/java/bridge/status/Retry.java
+++ src/main/java/bridge/status/Retry.java
@@ -24,4 +24,14 @@ public enum Retry {
 
         return false;
     }
+
+    public static Retry convert(String text) {
+        for (Retry i : Retry.values()) {
+            if (i.getText().equals(text)) {
+                return i;
+            }
+        }
+
+        throw new IllegalArgumentException("해당되는 재시도 명령을 찾을 수 없습니다");
+    }
 }
