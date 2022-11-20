5b2f879fcf64455e76a06a3143ab12a37388c12e
Kayoung <rkdkdudjd@gmail.com>
2022-11-20 23:05:34 +0900

    docs(Bridge): 필드 설명 추가

--- src/main/java/bridge/model/bridge/Bridge.java
+++ src/main/java/bridge/model/bridge/Bridge.java
@@ -6,7 +6,11 @@ import java.util.List;
 public class Bridge {
     public static final int MAX_SIZE = 20;
     public static final int MIN_SIZE = 3;
-    private List<Node> nodes;
+
+    /**
+     * 게임 시작 시 만들어지는 다리(정답)을 저장해두는 리스트
+     */
+    private final List<Node> nodes;
 
     private Bridge(int size) {
         nodes = new ArrayList<>(size);
