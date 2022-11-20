d9757bf2d4afb052a38f694d3234805529b76d63
안광민 <creativeduck0518@gmail.com>
2022-11-21 01:48:13 +0900

    feat(PlayerMap): 생성자 추가

--- src/main/java/bridge/PlayerMap.java
+++ src/main/java/bridge/PlayerMap.java
@@ -6,6 +6,11 @@ public class PlayerMap {
     private final int bridgeVerticalSize = 2;
     private final String[][] playerMap;
 
+    public PlayerMap(int bridgeSize) {
+        playerMap = new String[bridgeVerticalSize][bridgeSize];
+        initPlayerMap(bridgeSize);
+    }
+    
     public void initPlayerMap(int bridgeSize) {
         for (int i = 0; i < 2; i++) {
             for (int j = 0; j < bridgeSize; j++) {
