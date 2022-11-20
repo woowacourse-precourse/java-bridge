64dd4d2bb6e084b66f56ec8f95e022a4b0becd01
ho2n <jungho2n@gmail.com>
2022-11-20 23:15:10 +0900

    feat(ErrorMessage): 재시작 여부를 입력받는 문구의 문자열을 상수로 추가

--- src/main/java/constant/ErrorMessage.java
+++ src/main/java/constant/ErrorMessage.java
@@ -9,4 +9,5 @@ public class ErrorMessage {
     public static final String ASK_INPUT_VALUE = "%s 입력값이 필요합니다.";
     public static final String ASK_INPUT_NUMBER = "%s 숫자를 입력하셔야 합니다.";
     public static final String ASK_INPUT_CHARACTER_U_OR_D = "%s 'U'나 'D'를 입력하셔야 합니다.";
+    public static final String ASK_INPUT_CHARACTER_R_OR_Q = "%s 'R'나 'Q'를 입력하셔야 합니다.";
 }
