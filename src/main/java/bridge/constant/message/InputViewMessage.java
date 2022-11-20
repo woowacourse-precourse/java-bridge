d9a9d7c721c5d539fabe94a2784075a8126d3441
Haaae <101785628+Haaae@users.noreply.github.com>
2022-11-21 07:33:53 +0900

    refactor(InputViewMessage) : InputView가 사용하는 문구를 상수로 관리

---
+++ src/main/java/bridge/constant/message/InputViewMessage.java
@@ -0,0 +1,11 @@
+package bridge.constant.message;
+
+public class InputViewMessage {
+
+    public static final String BRIDGE_LENGTH_REQUEST = "다리의 길이를 입력해주세요.";
+    public static final String MOVING_REQUEST = "이동할 칸을 선택해주세요. (위: U, 아래: D)";
+    public static final String RETRY_REQUEST = "게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)";
+
+    private InputViewMessage() {}
+
+}
