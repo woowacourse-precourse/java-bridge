590b141a96925ca6c94f71a8600bb5ce629770c6
Dongmin Kim <kimdongmin68@naver.com>
2022-11-20 15:48:26 +0900

    feat(bridge): 다리를 끝까지 건너는 경우 종료 여부 테스트

--- src/test/java/bridge/BridgeGameTest.java
+++ src/test/java/bridge/BridgeGameTest.java
@@ -1,5 +1,6 @@
 package bridge;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
@@ -8,14 +9,32 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class BridgeGameTest {
 
+    private static final boolean END_GAME = true;
+
+    List<String> bridge;
+
+    @BeforeEach
+    void init() {
+        bridge = List.of("U", "U", "U", "D", "D");
+    }
+
     @Test
     void 다리_이동_성공_여부_테스트() {
-        List<String> bridge = List.of("U", "U", "U", "D", "D");
         List<String> inputMoving = List.of("U", "U", "D");
         BridgeGame game = new BridgeGame();
         for (int i = 0; i < inputMoving.size(); i++) {
             game.move(bridge, inputMoving.get(i));
         }
         assertThat(game.getMovingResult()).isEqualTo(List.of(true, true, false));
+    }
+
+    @Test
+    void 다리를_끝까지_건너는_경우_종료_여부_테스트() {
+        List<String> inputMoving = List.of("U", "U", "U", "D", "D");
+        BridgeGame game = new BridgeGame();
+        for (int i = 0; i < inputMoving.size(); i++) {
+            game.move(bridge, inputMoving.get(i));
+        }
+        assertThat(game.isEnd(bridge)).isEqualTo(END_GAME);
     }
 }
