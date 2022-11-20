a6f22b982a1acfed463888b26576b4a4cb429920
taeyun-work <wmfajr1357@gmail.com>
2022-11-20 21:05:35 +0900

    refactor: bridgeSizeValidation 테스트 중복 방지
    @ValueSource와 @ParameterizedTest를 활용하여 반복되는 테스트로 인한
    불필요한 코드를 방지하도록 수정

--- src/test/java/bridge/ValidatorTest.java
+++ src/test/java/bridge/ValidatorTest.java
@@ -4,13 +4,16 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 public class ValidatorTest {
 
     @DisplayName("3이상 20이하의 숫자를 입력하지 않으면 예외가 발생한다")
-    @Test
-    void bridgeSizeValidation(){
-        assertThatThrownBy(() -> Validator.size("100"))
+    @ValueSource(strings = {"2", "21", "100", "0", "abc"})
+    @ParameterizedTest
+    void bridgeSizeValidation(String input){
+        assertThatThrownBy(() -> Validator.size(input))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
