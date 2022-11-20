e90c0ad9a547e3feb278a67ca540dc573b3cd214
hingjae <ljh23270@naver.com>
2022-11-21 01:14:04 +0900

    게임 진행 : 이동 방향 입력

--- src/main/java/bridge/InputView.java
+++ src/main/java/bridge/InputView.java
@@ -15,20 +15,6 @@ public class InputView {
         return stringToInt(input);
     }
 
-    /**
-     * 사용자가 이동할 칸을 입력받는다.
-     */
-    public String readMoving() {
-        return null;
-    }
-
-    /**
-     * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
-     */
-    public String readGameCommand() {
-        return null;
-    }
-
     public String inputBridgeLength() {
         System.out.println("다리의 길이를 입력해주세요.");
         String input = Console.readLine();
@@ -42,6 +28,34 @@ public class InputView {
             throw new IllegalArgumentException("숫자만 입력할 수 있습니다.");
         }
         return input;
+    }
+
+    /**
+     * 사용자가 이동할 칸을 입력받는다.
+     */
+    public String readMoving() {
+        String uOrD = isUOrD(inputMoving());
+        return uOrD;
+    }
+
+    public String inputMoving() {
+        System.out.println("이동할 칸을 선택해주세요. (위: U, 아래: D)");
+        String input = Console.readLine();
+        return input;
+    }
+
+    public String isUOrD(String input) {
+        if("U".equals(input) || "D".equals(input)) {
+            return input;
+        }
+        throw new IllegalArgumentException("U 또는 D만 입력할 수 있습니다.");
+    }
+
+    /**
+     * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
+     */
+    public String readGameCommand() {
+        return null;
     }
 
     public static int stringToInt(String input) {
