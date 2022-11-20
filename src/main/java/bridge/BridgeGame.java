4250f79f4f427fb0a1ea9d2b76475a7f2b3ec0b0
hojunking96 <junsong96@naver.com>
2022-11-20 21:36:03 +0900

    feats(BridgeGame.java) : 게임 진행 기능, 게임 초기화 기능 구현

--- src/main/java/bridge/BridgeGame.java
+++ src/main/java/bridge/BridgeGame.java
@@ -1,20 +1,17 @@
 package bridge;
 
-import java.util.ArrayList;
-import java.util.List;
-
 /**
  * 다리 건너기 게임을 관리하는 클래스
  */
 public class BridgeGame {
-    public static List<String> movingInputs = new ArrayList<>();
 
     /**
      * 사용자가 칸을 이동할 때 사용하는 메서드
      * <p>
      * 이동을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void move() {
+    public static void move() {
+        MoveInsert.moveInsert();
     }
 
     /**
@@ -22,6 +19,10 @@ public class BridgeGame {
      * <p>
      * 재시작을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void retry() {
+    public static void retry() {
+        Application.upSide = new UpSide();
+        Application.downSide = new DownSide();
+        Application.movingTurn = 1;
+        Application.totalTrial++;
     }
 }
