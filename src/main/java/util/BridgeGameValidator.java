3e08c6f30fdf483a997ae557bd994bf0e6dc4729
nylonmusk <106235834+nylonmusk@users.noreply.github.com>
2022-11-20 23:27:23 +0900

    refactor(BridgeGameValidator): isMoveDone 메소드 생성

--- src/main/java/util/BridgeGameValidator.java
+++ src/main/java/util/BridgeGameValidator.java
@@ -6,6 +6,15 @@ import java.util.List;
 
 public class BridgeGameValidator {
 
+    public static boolean isMoveDone(List<String> bridge, List<List<String>> bothSide) {
+        return BridgeGameValidator.isUserInputDone(bridge, bothSide)
+                || BridgeGameValidator.isContainWrongPath(bothSide);
+    }
+
+    private static boolean isUserInputDone(List<String> bridge, List<List<String>> bothSide) {
+        return bridge.size() == bothSide.get(NumberConstant.UP_SIDE.getConstant()).size();
+    }
+
     public static boolean isBridgeEqualsD(String bridgeIndex) {
         return bridgeIndex.equals(StringConstant.DOWN_SIDE.getConstant());
     }
@@ -16,7 +25,8 @@ public class BridgeGameValidator {
 
     public static boolean isContainWrongPath(List<List<String>> bothSide) {
         return bothSide.get(NumberConstant.UP_SIDE.getConstant()).contains(StringConstant.WRONG_PATH.getConstant())
-                || bothSide.get(NumberConstant.DOWN_SIDE.getConstant()).contains(StringConstant.WRONG_PATH.getConstant());
+                || bothSide.get(NumberConstant.DOWN_SIDE.getConstant())
+                .contains(StringConstant.WRONG_PATH.getConstant());
     }
 
     public static boolean isUserInputRetry(String retryOrQuit) {
