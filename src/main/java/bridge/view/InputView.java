5a456069232258a8663419e60795ee5df7424f50
yoonjuho <dbswn990@naver.com>
2022-11-20 20:28:40 +0900

    feat(InputView): 문자열 입력

--- src/main/java/bridge/view/InputView.java
+++ src/main/java/bridge/view/InputView.java
@@ -1,6 +1,6 @@
 package bridge.view;
 
-import java.io.Console;
+import camp.nextstep.edu.missionutils.Console;
 
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
@@ -12,20 +12,24 @@ public class InputView {
      * 다리의 길이를 입력받는다.
      */
     public String readBridgeSize() {
-        return null;
+        String input = Console.readLine();
+        return input;
     }
 
     /**
      * 사용자가 이동할 칸을 입력받는다.
      */
-    public String readMoving() {
-        return null;
+    public String readMoving()
+    {
+        String input = Console.readLine();
+        return input;
     }
 
     /**
      * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
      */
     public String readGameCommand() {
-        return null;
+        String input = Console.readLine();
+        return input;
     }
 }
