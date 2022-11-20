3b5206228df248b7b42ce463a5dd33552799d380
kevstevie <jianna442@gmail.com>
2022-11-20 20:14:16 +0900

    refactor: 초기 위치와 시도 횟수 상수로 만듬

--- src/main/java/bridge/model/BridgeGame.java
+++ src/main/java/bridge/model/BridgeGame.java
@@ -6,15 +6,16 @@ import java.util.List;
  * 다리 건너기 게임을 관리하는 클래스
  */
 public class BridgeGame {
-
+    private static final int INITIAL_POSITION = 0;
+    private static final long FIRST_TRY = 1;
     private int position;
     private long playTime;
     private final List<String> gameMap;
 
     public BridgeGame(List<String> gameMap) {
         this.gameMap = gameMap;
-        position = 0;
-        playTime = 1;
+        position = INITIAL_POSITION;
+        playTime = FIRST_TRY;
     }
 
     /**
@@ -36,7 +37,7 @@ public class BridgeGame {
      * 재시작을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
     public long retry() {
-        position = 0;
+        position = INITIAL_POSITION;
         playTime++;
         return playTime;
     }
