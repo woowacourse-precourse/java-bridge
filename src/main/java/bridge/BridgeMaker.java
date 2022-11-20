a92f5a29a1138987037ec5fd00d02b6c5200fdac
realslimtaek <89964264+realslimtaek@users.noreply.github.com>
2022-11-20 18:30:06 +0900

    fix : 0 일때 D ,1 일때 U로 수정

--- src/main/java/bridge/BridgeMaker.java
+++ src/main/java/bridge/BridgeMaker.java
@@ -2,36 +2,29 @@ package bridge;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Objects;
 
-/**
- * 다리의 길이를 입력 받아서 다리를 생성해주는 역할을 한다.
- */
 public class BridgeMaker {
 
-
     private final BridgeNumberGenerator bridgeNumberGenerator;
+    public static List<String> bridge = new ArrayList<>();
 
     public BridgeMaker(BridgeNumberGenerator bridgeNumberGenerator) {
         this.bridgeNumberGenerator = bridgeNumberGenerator;
     }
 
     public List<String> makeBridge(int size) {
-        BridgeRandomNumberGenerator bridgeRandomNumberGenerator = new BridgeRandomNumberGenerator();
+        bridge.clear();
         for (int i = 0; i < size; i++) {
-            int a = bridgeRandomNumberGenerator.generate();
-            if (a == 0)
-                BridgeGame.bridge_answer.add("U");
-            if (a == 1)
+            int a = this.bridgeNumberGenerator.generate();
+            if (a == 0) {
                 BridgeGame.bridge_answer.add("D");
+                bridge.add("D");
+            }
+            if (a == 1) {
+                BridgeGame.bridge_answer.add("U");
+                bridge.add("U");
+            }
         }
-        return null;
+        return bridge;
     }
-
-
-
-
-
-
-
 }
