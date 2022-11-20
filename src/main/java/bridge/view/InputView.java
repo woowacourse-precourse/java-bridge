1fcadd9906fdf9d0f021e410e794b64ddedc17f8
KYR <freekyr7529@gmail.com>
2022-11-21 01:47:40 +0900

    refactor : 사용하지 않는 객체 제거
    InputView에서 사용하지 않는 OutputView 객체 제거

--- src/main/java/bridge/view/InputView.java
+++ src/main/java/bridge/view/InputView.java
@@ -1,14 +1,13 @@
 package bridge.view;
 
-import bridge.BridgeRule;
-import bridge.Command;
+import bridge.constant.BridgeRule;
+import bridge.constant.Command;
 import camp.nextstep.edu.missionutils.Console;
 
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
  */
 public class InputView {
-    OutputView outputView = new OutputView();
 
     public String readValue() {
         return Console.readLine();
