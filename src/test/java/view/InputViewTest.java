d4e2d041389b5baa3e48017e8342b31793d3cdb3
23Yong <1239824@naver.com>
2022-11-20 15:17:58 +0900

    refactor(InputViewTest): 다양한 테스트 케이스를 고려한 파라미터 테스트 코드 작성
    - `@ParameterizedTest`를 이용해 반복적으로 중복되는 값을 처리해 테스트 코드를 작성한다
    - 재시작/종료 여부가 `R` 혹은 `Q`가 아닐 경우에 대한 잘못된 `@DisplayName`을 수정한다

--- src/test/java/view/InputViewTest.java
+++ src/test/java/view/InputViewTest.java
@@ -2,6 +2,8 @@ package view;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import java.io.ByteArrayInputStream;
 import java.util.NoSuchElementException;
@@ -26,7 +28,8 @@ class InputViewTest {
     }
 
     @DisplayName("다리 길이에 문자가 포함된 경우 예외를 발생시킨다.")
-    @Test
+    @ValueSource(strings = {"1l", " ", "1 ", "j"})
+    @ParameterizedTest
     void 다리_길이에_문자가_포함된_경우() {
         // given
         InputView inputView = new InputView();
@@ -37,32 +40,35 @@ class InputViewTest {
     }
 
     @DisplayName("다리 길이가 유효한 범위를 벗어나는 경우 에외를 발생시킨다.")
-    @Test
-    void 다리_길이가_범위를_벗어나는_경우() {
+    @ValueSource(strings = {"2", "21"})
+    @ParameterizedTest
+    void 다리_길이가_범위를_벗어나는_경우(String bridgeSize) {
         // given
         InputView inputView = new InputView();
-        System.setIn(new ByteArrayInputStream("2\n21".getBytes()));
+        System.setIn(new ByteArrayInputStream(bridgeSize.getBytes()));
 
         // when & then
         assertThatThrownBy(inputView::readBridgeSize).isInstanceOf(NoSuchElementException.class);
     }
 
     @DisplayName("이동할 칸이 U 혹은 D일 경우 입력받는데 성공한다.")
-    @Test
-    void 이동할_칸이_U_혹은_D인_경우() {
+    @ValueSource(strings = {"U", "D"})
+    @ParameterizedTest
+    void 이동할_칸이_U_혹은_D인_경우(String userInput) {
         // given
         InputView inputView = new InputView();
-        System.setIn(new ByteArrayInputStream("U".getBytes()));
+        System.setIn(new ByteArrayInputStream(userInput.getBytes()));
 
         // when
         String moving = inputView.readMoving();
 
         // then
-        assertThat(moving).isEqualTo("U");
+        assertThat(moving).isEqualTo(userInput);
     }
 
     @DisplayName("이동할 칸이 U 혹은 D가 아닌 경우 예외를 발생시킨다.")
-    @Test
+    @ValueSource(strings = {"I", "u", "1", ";"})
+    @ParameterizedTest
     void 이동할_칸이_U_혹은_D가_아닐_경우() {
         // given
         InputView inputView = new InputView();
@@ -74,21 +80,23 @@ class InputViewTest {
     }
 
     @DisplayName("재시작/종료 여부가 R 혹은 Q일 경우 입력받는데 성공한다.")
-    @Test
-    void 재시작_종료_여부가_R_혹은_Q일_경우() {
+    @ValueSource(strings = {"R", "Q"})
+    @ParameterizedTest
+    void 재시작_종료_여부가_R_혹은_Q일_경우(String userInput) {
         // given
         InputView inputView = new InputView();
-        System.setIn(new ByteArrayInputStream("R".getBytes()));
+        System.setIn(new ByteArrayInputStream(userInput.getBytes()));
 
         // when
         String command = inputView.readGameCommand();
 
         // then
-        assertThat(command).isEqualTo("R");
+        assertThat(command).isEqualTo(userInput);
     }
 
-    @DisplayName("재시작/종료 여부가 R 혹은 Q일 경우 입력받는데 성공한다.")
-    @Test
+    @DisplayName("재시작/종료 여부가 R 혹은 Q이 아닐 경우 예외를 발생시킨다.")
+    @ValueSource(strings = {"D", "r", "1", ";"})
+    @ParameterizedTest
     void 재시작_종료_여부가_R_혹은_Q가_아닐_경우() {
         // given
         InputView inputView = new InputView();
