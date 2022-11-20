bce1d1ff5c40b624698e65f0603b0e68af786550
UngHeung <t01047377477@gmail.com>
2022-11-21 06:23:36 +0900

    feat : (Validate) 다리 크기 문자 입력 시 예외 처리 할 수 있도록 수정

--- src/main/java/domain/Validate.java
+++ src/main/java/domain/Validate.java
@@ -2,23 +2,23 @@ package domain;
 
 public class Validate {
 	private String errorMessage;
-	
-	public void validateSize(int size) {
-		if(size < 3 || 20 < size) {
+
+	public void validateSize(String size) {
+		if (size.charAt(0) - 48 < 3 || 20 < size.charAt(0) - 48) {
 			errorMessage = ErrorMessage.WRONG_SIZE.getErrorMessage();
 			throw new IllegalArgumentException(errorMessage);
 		}
 	}
-	
+
 	public void validateMove(String move) {
-		if(!move.equals("U") && !move.equals("D")) {
+		if (!move.equals("U") && !move.equals("D")) {
 			errorMessage = ErrorMessage.WRONG_MOVING.getErrorMessage();
 			throw new IllegalArgumentException(errorMessage);
 		}
 	}
-	
+
 	public void validateGameCommand(String gameCommand) {
-		if(!gameCommand.equals("R") && !gameCommand.equals("Q")) {
+		if (!gameCommand.equals("R") && !gameCommand.equals("Q")) {
 			errorMessage = ErrorMessage.WRONG_COMMAND.getErrorMessage();
 			throw new IllegalArgumentException(errorMessage);
 		}
