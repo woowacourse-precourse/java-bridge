86b0e3b85ded1600f8f20a1f3b18af65cf95b878
안광민 <creativeduck0518@gmail.com>
2022-11-21 00:40:27 +0900

    feat(OutputView): 게임 시작 문구 출력 기능 구현

--- src/main/java/bridge/presentation/OutputView.java
+++ src/main/java/bridge/presentation/OutputView.java
@@ -6,6 +6,8 @@ import bridge.data.GameMessage;
 import static bridge.data.GameMark.*;
 
 public class OutputView {
-
+    public void printGameStart() {
+        System.out.println(GameMessage.GAME_START.getMessage());
+    }
 }
 
