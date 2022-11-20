53db0c76d7caa6b479b6a062a6d687836741b738
eastsage <dpwk8@naver.com>
2022-11-21 03:08:10 +0900

    feat: 재시작이 입력 되었을 때 게임을 재개하는 함수 추가
    retry 함수 구현

--- src/main/java/bridge/model/BridgeGame.java
+++ src/main/java/bridge/model/BridgeGame.java
@@ -1,6 +1,10 @@
 package bridge.model;
 
+import bridge.BridgeMaker;
+import bridge.BridgeNumberGenerator;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Stack;
 
 /**
  * 다리 건너기 게임을 관리하는 클래스
@@ -40,6 +44,19 @@ public class BridgeGame {
      * <p>
      * 재시작을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void retry(String input) {
+    public void retry() {
+        //TODO: IllegalStatementException
+        movement.pop();
+    }
+
+    public boolean failToMove() {
+        if (bridge.canMove(movement)) {
+            return false;
+        }
+        return true;
+    }
+
+    public boolean isEndOfBridge() {
+        return bridge.arriveAtTheEnd(movement);
     }
 }
