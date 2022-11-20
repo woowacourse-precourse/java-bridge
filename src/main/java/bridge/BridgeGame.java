1a2f5a968ebc2d16ebb8e6611493e3c43772885c
leesangjin <tkdwls0628@gmail.com>
2022-11-21 02:02:58 +0900

    feat(BridgeGame): 결과 출력을 위한 초기화 기능 구현

--- src/main/java/bridge/BridgeGame.java
+++ src/main/java/bridge/BridgeGame.java
@@ -1,6 +1,19 @@
 package bridge;
 
+import bridge.constant.Constant;
+import java.util.ArrayList;
+import java.util.List;
+
 public class BridgeGame {
+
+    private List<List<String>> bridgeMap = new ArrayList<>();
+
+    public void initMap() {
+        bridgeMap.clear();
+        for (int i = 0; i < Constant.BRIDGE_WIDTH; i++) {
+            bridgeMap.add(new ArrayList<>());
+        }
+    }
 
     public void move() {
     }
