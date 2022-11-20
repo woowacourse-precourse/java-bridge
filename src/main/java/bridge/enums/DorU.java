35472f5dfce2752465a5eccd737362013e7191b4
JunTaeINC <juntea3523@naver.com>
2022-11-20 21:57:26 +0900

    style : (DorU) 코드 정리

--- src/main/java/bridge/enums/DorU.java
+++ src/main/java/bridge/enums/DorU.java
@@ -1,26 +1,24 @@
 package bridge.enums;
 
 public enum DorU {
-    DOWN("D",0),
-    UP("U",1);
 
+    UP("U", 1),
+    DOWN("D", 0);
 
     private final String name;
     private final Integer number;
 
-    DorU(String name, Integer number){
+    DorU(String name, Integer number) {
         this.name = name;
         this.number = number;
     }
+
     public String getName() {
         return name;
     }
-    public Integer getNumber() {
-        return number;
-    }
 
-    public static String numberConvertName(int object){
-        if (object == DOWN.number){
+    public static String numberConvertName(int object) {
+        if (object == DOWN.number) {
             return DOWN.name;
         }
         return UP.name;
