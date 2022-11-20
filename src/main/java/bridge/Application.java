5a542092e12ee81fb91e8d96690ec893da26fd3a
윤서영 <yoonseoyoung@yunseoyeong-ui-MacBookPro.local>
2022-11-20 23:14:03 +0900

    feat : 다리 생성하기

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -1,5 +1,7 @@
 package bridge;
 
+import java.util.List;
+
 import static bridge.view.InputView.readBridgeSize;
 
 public class Application {
@@ -7,6 +9,9 @@ public class Application {
     public static void main(String[] args) {
         try {
             int bridgeSize = readBridgeSize();
+            BridgeMaker bridgeMaker = new BridgeMaker(new BridgeRandomNumberGenerator());
+            List<String> bridge = bridgeMaker.makeBridge(bridgeSize);
+
         } catch (IllegalArgumentException e) {
             System.out.println(e.getMessage());
         }
