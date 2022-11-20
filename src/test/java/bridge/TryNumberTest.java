829ed3e099f1b1b5c1d5d0460c144634b9810c9c
은지 <midzy0228@gmail.com>
2022-11-21 00:05:54 +0900

    test(TryNumber): 시도한 횟수가 올바르게 카운트 되는지 확인

---
+++ src/test/java/bridge/TryNumberTest.java
@@ -0,0 +1,28 @@
+package bridge;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import bridge.model.BridgeGame;
+import bridge.model.TryNumber;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class TryNumberTest {
+    @DisplayName("첫시도에 성공시 올바른 시도횟수를 반환한다.")
+    @Test
+    void createTryNumberByFirstTry() {
+        TryNumber tryNumber = new TryNumber();
+        assertThat(tryNumber.getTryNumber()).isEqualTo(1);
+    }
+
+    @DisplayName("세번 시도했을 시 올바른 시도횟수를 반환한다.")
+    @Test
+    void createTryNumberThirdTry() {
+        TryNumber tryNumber = new TryNumber();
+        BridgeGame bridgeGame = new BridgeGame();
+        for (int count = 0; count < 2; count++) {
+            bridgeGame.retry(tryNumber);
+        }
+        assertThat(tryNumber.getTryNumber()).isEqualTo(3);
+    }
+}
