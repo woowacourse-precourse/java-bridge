776124ca940176de83977dbea4421c6138bbd697
JunginPark4364 <wjddlsee@gmail.com>
2022-11-21 04:02:54 +0900

    feat: 다리 길이 입력값 검증

--- src/main/java/bridge/dto/BridgeSize.java
+++ src/main/java/bridge/dto/BridgeSize.java
@@ -1,6 +1,14 @@
 package bridge.dto;
 
+import bridge.util.StringUtils;
+
+import java.util.regex.Pattern;
+
 public class BridgeSize {
+    private static final String BRIDGE_SIZE_PATTERN = "^[3-9]{1}$|^[1]{1}[0-9]{1}$|^[2]{1}[0]{1}$";    // 3 ~ 20
+    private static final String RANGE_EXCEPTION_MESSAGE = "다리 길이는 3부터 20 사이의 숫자여야 합니다.";
+    private static final String BLANK_EXCEPTION_MESSAGE = "Null 또는 Empty 를 입력할 수 없습니다.";
+
     private final int bridgeSize;
 
     public BridgeSize(String bridgeSize) {
@@ -12,8 +20,25 @@ public class BridgeSize {
     }
 
     private static int parseBridgeSize(final String bridgeSize) {
-
+        validate(bridgeSize);
         return Integer.parseInt(bridgeSize);
+    }
+
+    private static void validate(final String bridgeSize) {
+        validateBlank(bridgeSize);
+        validateRange(bridgeSize);
+    }
+
+    private static void validateRange(final String bridgeSize) {
+        if (!Pattern.matches(BRIDGE_SIZE_PATTERN, bridgeSize)) {
+            throw new IllegalArgumentException(RANGE_EXCEPTION_MESSAGE);
+        }
+    }
+
+    private static void validateBlank(final String bridgeSize) {
+        if (StringUtils.isBlank(bridgeSize)) {
+            throw new IllegalArgumentException(BLANK_EXCEPTION_MESSAGE);
+        }
     }
 
     public int getBridgeSize() {
---
+++ src/main/java/bridge/util/StringUtils.java
@@ -0,0 +1,15 @@
+package bridge.util;
+
+import java.util.Objects;
+
+public class StringUtils {
+    private static final String UTILITY_CLASS_CREATE_EXCEPTION_MESSAGE = "생성할 수 없는 Utility 클래스입니다.";
+
+    private StringUtils() {
+        throw new IllegalStateException(UTILITY_CLASS_CREATE_EXCEPTION_MESSAGE);
+    }
+
+    public static boolean isBlank(final String input) {
+        return Objects.isNull(input) || input.isEmpty();
+    }
+}
