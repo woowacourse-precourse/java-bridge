f2292c44dfcdd1601629f4da24129e821e494141
lacram <chemin9898@gmail.com>
2022-11-20 15:31:47 +0900

    feat: 다리의 길이를 검증하는 기능 - 숫자가 맞다면 3~20사이의 숫자인지 검증하는 기능 추가

--- src/main/java/bridge/Validation.java
+++ src/main/java/bridge/Validation.java
@@ -11,4 +11,10 @@ public class Validation {
         }
     }
 
+    // 숫자가 맞다면 3~20사이의 숫자인지 검증하는 기능
+    public static void isInRange(int bridgeSize) {
+        if (bridgeSize < 3 || bridgeSize > 20) {
+            throw new IllegalArgumentException("[ERROR] 3~20사이의 숫자만 입력해 주세요.");
+        }
+    }
 }
