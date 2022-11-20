c565f4f6d23bbd3ea0fd6c46e00ee0be513273b3
limjae <limjae96@gmail.com>
2022-11-20 22:17:18 +0900

    feat: 재시도 기능 추가

--- src/main/java/bridge/scenario/BasicBridgeScenario.java
+++ src/main/java/bridge/scenario/BasicBridgeScenario.java
@@ -8,6 +8,7 @@ import bridge.game.BridgeGame;
 import bridge.game.BridgeGameStatus;
 import bridge.view.input.InputView;
 import bridge.view.output.OutputView;
+import java.util.Arrays;
 import java.util.List;
 
 public class BasicBridgeScenario implements BridgeScenario {
@@ -20,32 +21,63 @@ public class BasicBridgeScenario implements BridgeScenario {
     public void play() {
         output.printStart();
 
-        int size = input.readBridgeSize();
-
-        List<String> answer = bridgeMaker.makeBridge(size);
-        BridgeGame game = new BridgeGame(answer);
+        BridgeGame game = retryableReadSizeAndInitGame();
 
         while (!game.status().equals(FINISH)) {
-            // move
-            if(game.status().equals(RUNNING)){
-                String command = input.readMoving();
-                game.move(command);
-                List<List<String>> progress = game.progress();
-                output.printMap(progress);
+            if (game.status().equals(RUNNING)) {
+                retryableMove(game);
             }
-            // retry
-            if(game.status().equals(STOP)){
-                String command = input.readGameCommand();
-                if (command.equals("Q")) {
-                    break;
-                }
-                if (!command.equals("R")) {
-                    throw new IllegalArgumentException("[ERROR]~~");
-                }
-                game.retry();
+            if (game.status().equals(STOP) && askEndGame(game)) {
+                break;
             }
         }
 
+        endGame(game);
+    }
+
+    private BridgeGame retryableReadSizeAndInitGame() {
+        try {
+            int size = input.readBridgeSize();
+            List<String> answer = bridgeMaker.makeBridge(size);
+            return new BridgeGame(answer);
+        } catch (IllegalArgumentException e) {
+            output.printException(e);
+            return retryableReadSizeAndInitGame();
+        }
+    }
+
+    private void retryableMove(BridgeGame game) {
+        try {
+            move(game);
+        } catch (IllegalArgumentException e) {
+            output.printException(e);
+            retryableMove(game);
+        }
+    }
+
+    private void move(BridgeGame game) {
+        String command = input.readMoving();
+
+        game.move(command);
+
+        List<List<String>> progress = game.progressMap();
+        output.printMap(progress);
+    }
+
+    private boolean askEndGame(BridgeGame game) {
+        String command = input.readGameCommand();
+        if (command.equals("Q")) {
+            return true;
+        }
+        if (!command.equals("R")) {
+            throw new IllegalArgumentException("[ERROR]~~");
+        }
+        game.retry();
+        return false;
+    }
+
+    private void endGame(BridgeGame game) {
+        output.printAnswer(game.answerMap());
         output.printResult(game.status().text(), game.tryTimes());
     }
 }
