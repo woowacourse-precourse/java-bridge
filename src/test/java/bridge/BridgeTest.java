f85465a1ec8d18ad37c582f8fb21ec5aea895eb7
dlrghks2090 <dlrghks2090@gmail.com>
2022-11-20 22:28:36 +0900

    test: 테스트 메서드 이름 오타 수정

--- src/test/java/bridge/BridgeTest.java
+++ src/test/java/bridge/BridgeTest.java
@@ -14,7 +14,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 class BridgeTest {
     @DisplayName("다리 길이는 입력이 3이상 20이하의 수가 아니면 에러가 발생한다.")
     @Test
-    void ceateBridgeLengthvalidateNumberRange() {
+    void createBridgeLengthVgalidateNumberRange() {
         assertThatThrownBy(() -> new Bridge(21))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage(ErrorMessage.BRIDGE_LENGTH_IS_THREE_TO_TWENTY.getErrorMessage());
@@ -23,7 +23,7 @@ class BridgeTest {
     @DisplayName("다리 길이는 입력이 3이상 20이하의 수일때 정상적으로 작동한다.")
     @ParameterizedTest
     @CsvSource({"3,3", "10,10", "20,20"})
-    void ceateRightBridgeLength(int bridgeLength, int expected) {
+    void createRightBridgeLength(int bridgeLength, int expected) {
         Bridge bridge = new Bridge(bridgeLength);
         assertThat(bridge.getBridgeLength()).isEqualTo(expected);
     }
