8b9b5e068cc030f3d03f294b5123a38314dcdec9
fakerdeft <whakswp123@naver.com>
2022-11-21 04:41:29 +0900

    feat: 다리 만들기 변수명 변경

--- src/main/java/bridge/model/BridgeMaker.java
+++ src/main/java/bridge/model/BridgeMaker.java
@@ -19,11 +19,11 @@ public class BridgeMaker {
      * @return 입력받은 길이에 해당하는 다리 모양. 위 칸이면 "U", 아래 칸이면 "D"로 표현해야 한다.
      */
     public List<String> makeBridge(int size) {
-        List<String> bridgeResult = new ArrayList<>();
+        List<String> bridgeAnswer = new ArrayList<>();
         for (int i = 0; i < size; i++) {
-            bridgeResult.add(makeBridgeResult(bridgeNumberGenerator.generate()));
+            bridgeAnswer.add(makeBridgeResult(bridgeNumberGenerator.generate()));
         }
-        return bridgeResult;
+        return bridgeAnswer;
     }
 
     public String makeBridgeResult(int number) {
