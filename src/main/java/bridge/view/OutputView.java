b1bab029675e65292fcc6c4ed48d4b410cef81fa
leesangjin <tkdwls0628@gmail.com>
2022-11-21 02:05:34 +0900

    feat(OutputView): 이동 기능 출력 기능 구현

--- src/main/java/bridge/view/OutputView.java
+++ src/main/java/bridge/view/OutputView.java
@@ -1,5 +1,9 @@
 package bridge.view;
 
+import bridge.constant.Constant;
+
+import java.util.List;
+
 public class OutputView {
 
     private static final String START_BRIDGE_GAME = "다리 건너기 게임을 시작합니다.";
@@ -8,7 +12,10 @@ public class OutputView {
         System.out.println(START_BRIDGE_GAME);
     }
 
-    public static void printMap() {
+    public static void printMap(List<List<String>> map) {
+        for (int i = 0; i < Constant.BRIDGE_WIDTH; i++) {
+            System.out.println("[ " +String.join(" | ", map.get(i)) + " ]");
+        }
     }
 
     public static void printResult() {
