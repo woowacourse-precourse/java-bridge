969861ec72c1f820254830d73d6948898f792f76
Isu Shin <isu010614@naver.com>
2022-11-20 19:26:20 +0900

    refactor: 사용자의 이동을 입력받을 기능의 클래스 분리
    -InputValidator 에서 Moving 클래스로 분리함.

---
+++ src/main/java/bridge/domain/Moving.java
@@ -0,0 +1,47 @@
+package bridge.domain;
+
+import java.util.Objects;
+
+public class Moving {
+    private static final int END_LENGTH_RANGE = 20;
+
+    private final String moving;
+
+    private Moving(String moving) {
+        this.moving = moving;
+    }
+
+    public static Moving createMoving(String inputMoving) {
+        validateMoving(inputMoving);
+        return new Moving(inputMoving);
+    }
+
+    private static void validateMoving(String moving) {
+        if (!isUpOrDown(moving)) {
+            throw new IllegalArgumentException(ExceptionMessage.NOT_UP_OR_DOWN);
+        }
+    }
+
+    private static boolean isUpOrDown(String moving) {
+        return moving.equals(MovingStatus.UP.getMovingKey())
+                || moving.equals(MovingStatus.DOWN.getMovingKey());
+    }
+
+    @Override
+    public String toString() {
+        return moving;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Moving moving1 = (Moving) o;
+        return Objects.equals(moving, moving1.moving);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(moving);
+    }
+}
