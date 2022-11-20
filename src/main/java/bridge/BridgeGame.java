21ed6db493c5e65cb1aae0ef33095fc21a845b7b
DaeyeonKim97 <kdysy1130@khu.ac.kr>
2022-11-21 00:03:08 +0900

    feature(bridge-game) : checkSuccess method

--- src/main/java/bridge/BridgeGame.java
+++ src/main/java/bridge/BridgeGame.java
@@ -78,6 +78,13 @@ public class BridgeGame {
         return false;
     }
 
+    public boolean checkSuccess(){
+        if (bridge.size() == answerPath.size()) {
+            return true;
+        }
+        return false;
+    }
+
 
     /**
      * 사용자가 게임을 다시 시도할 때 사용하는 메서드
