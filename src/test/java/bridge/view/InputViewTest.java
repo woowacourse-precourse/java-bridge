4bf74fb42bd07ab4a0b93af949cde70fc4e1d220
yejin <kwonyj1022@naver.com>
2022-11-20 12:20:04 +0900

    test: InputView 테스트 추가

--- src/test/java/bridge/view/InputViewTest.java
+++ src/test/java/bridge/view/InputViewTest.java
@@ -34,4 +34,58 @@ class InputViewTest {
         assertThatThrownBy(() -> new InputView().readBridgeSize())
                 .isInstanceOf(IllegalArgumentException.class);
     }
+
+    @Test
+    void 이동할칸_입력_테스트() {
+        String input = "U";
+        InputStream in = new ByteArrayInputStream(input.getBytes());
+        System.setIn(in);
+        String moving = new InputView().readMoving();
+        assertThat(moving).isEqualTo("U");
+    }
+
+    @Test
+    void 이동할칸_입력_예외_테스트1() {
+        String input = "u";
+        InputStream in = new ByteArrayInputStream(input.getBytes());
+        System.setIn(in);
+        assertThatThrownBy(() -> new InputView().readMoving())
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void 이동할칸_입력_예외_테스트2() {
+        String input = "2";
+        InputStream in = new ByteArrayInputStream(input.getBytes());
+        System.setIn(in);
+        assertThatThrownBy(() -> new InputView().readMoving())
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void 재시도여부_입력_테스트() {
+        String input = "R";
+        InputStream in = new ByteArrayInputStream(input.getBytes());
+        System.setIn(in);
+        String gameCommand = new InputView().readGameCommand();
+        assertThat(gameCommand).isEqualTo("R");
+    }
+
+    @Test
+    void 재시도여부_입력_예외_테스트1() {
+        String input = "r";
+        InputStream in = new ByteArrayInputStream(input.getBytes());
+        System.setIn(in);
+        assertThatThrownBy(() -> new InputView().readGameCommand())
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void 재시도여부_입력_예외_테스트2() {
+        String input = "no";
+        InputStream in = new ByteArrayInputStream(input.getBytes());
+        System.setIn(in);
+        assertThatThrownBy(() -> new InputView().readGameCommand())
+                .isInstanceOf(IllegalArgumentException.class);
+    }
 }
