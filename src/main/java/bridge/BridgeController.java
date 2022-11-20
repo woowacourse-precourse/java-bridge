eef05a2c5360b1d5a971caf82cfdbbd348331194
Yunsik-Choi <choiys0311@gmail.com>
2022-11-21 00:05:51 +0900

    ⚡feat : BridgeController 다리를 건너다 실패하면 게임 커맨드를 입력하여 게임을 재시작하거나 종료하는 기능 구현

--- src/main/java/bridge/BridgeController.java
+++ src/main/java/bridge/BridgeController.java
@@ -2,7 +2,9 @@ package bridge;
 
 import bridge.domain.Bridge;
 import bridge.domain.BridgeGame;
+import bridge.domain.BridgeGameResult;
 import bridge.domain.BridgeShape;
+import bridge.domain.GameCommand;
 import bridge.view.InputView;
 import bridge.view.OutputView;
 import java.util.List;
@@ -20,16 +22,31 @@ public class BridgeController {
 
     public void gameStart() {
         BridgeGame bridgeGame = new BridgeGame(createBridge());
-        while (bridgeGame.isNotEnd()) {
-            move(bridgeGame);
-            outputView.printMap(bridgeGame.result());
-        }
+        playGame(bridgeGame);
     }
 
     private Bridge createBridge() {
         int bridgeSize = inputView.readBridgeSize();
         List<String> bridgeShapeValues = bridgeMaker.makeBridge(bridgeSize);
         return Bridge.createByBridgeShapeValue(bridgeShapeValues);
+    }
+
+    private void playGame(BridgeGame bridgeGame) {
+        while (bridgeGame.isNotEnd()) {
+            move(bridgeGame);
+            BridgeGameResult result = bridgeGame.result();
+            outputView.printMap(result);
+            if (result.isFail()) {
+                retryGame(bridgeGame);
+                return;
+            }
+        }
+    }
+
+    private void retryGame(BridgeGame bridgeGame) {
+        if (GameCommand.of(inputView.readGameCommand()).isRetry()) {
+            playGame(bridgeGame.retry());
+        }
     }
 
     private void move(BridgeGame bridgeGame) {
