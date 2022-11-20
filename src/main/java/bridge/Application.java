58bc9f411116e7b1725b44e9fe7812ffe7ad8fb0
임원정 <oddish0513@gmail.com>
2022-11-20 22:14:09 +0900

    feat(bridge): UI 출력

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -1,18 +1,23 @@
 package bridge;
 
+import static bridge.Constants.*;
+
 public class Application {
 
 	public static void main(String[] args) {
 		InputView inputView = new InputView();
 
+		System.out.println(START);
 
 		// todo 다리 길이 입력
+		System.out.println(WRITE_BRIDGE_SIZE);
 		int size = inputView.readBridgeSize();
 
 		// 다리 생성
 		Bridge bridge = new Bridge(new BridgeMaker(new BridgeRandomNumberGenerator()).makeBridge(size));
 
 		// todo 사용자 U, D 입력
+		System.out.println(WRITE_WANNA_MOVE);
 		String want = inputView.readMoving();
 
 		// todo move
--- src/main/java/bridge/Constants.java
+++ src/main/java/bridge/Constants.java
@@ -1,6 +1,10 @@
 package bridge;
 
 public final class Constants {
+	public static final String START = "다리 건너기 게임을 시작합니다.";
+	public static final String WRITE_BRIDGE_SIZE = "\n다리의 길이를 입력해주세요.";
+	public static final String WRITE_WANNA_MOVE = "\n이동할 칸을 선택해주세요. (위: U, 아래: D)";
+	public static final String WRITE_WANNA_RESTART = "\n게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)";
 	public static final String ERROR_WRONG_SIZE = "[ERROR] 3이상 20이하의 정수를 입력하시오.";
 	public static final String ERROR_WRONG_BRIDGE_VALUE = "[ERROR] U 또는 D를 입력하시오.";
 }
