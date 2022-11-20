30b9b2221f39042bd817040e75f7a3fe3a5916e9
Ji-Un-Gil <wldns2577@gmail.com>
2022-11-21 00:03:37 +0900

    feat(Application.java): choice function Added function: - 사용자에게 칸을 입력받아 다리에서 이동한 뒤 결과를 받아오는 기능 구현

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -1,5 +1,7 @@
 package bridge;
 
+import java.util.List;
+
 public class Application {
 
     private static InputView input;
@@ -14,7 +16,18 @@ public class Application {
         game.makeBridgeGame(input.readBridgeSize());
     }
 
+    /**
+     * 사용자에게 칸을 입력받아 다리에서 이동한 결과를 반환한다.
+     */
+    private static List<String> choice(){
+        output.printMoving();
+        return game.move(input.readMoving());
+    }
+
     public static void main(String[] args) {
         start();
+        while (!game.isDone()){
+            choice();
+        }
     }
 }
