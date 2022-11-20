b10e08cbbb3c7e6ff6d8df5cb9d5adbe6c914ba4
Yunsik-Choi <choiys0311@gmail.com>
2022-11-21 00:58:26 +0900

    ♻️ refactor :  InputView 안으로 InputValidator 로직 이동 후 InputViewValidator 클래스 제거

--- src/main/java/bridge/view/InputView.java
+++ src/main/java/bridge/view/InputView.java
@@ -1,20 +1,28 @@
 package bridge.view;
 
+import bridge.exception.BridgeException;
+import bridge.exception.view.BridgeSizeRangeException;
+import bridge.exception.view.GameCommandInputException;
+import bridge.exception.view.MoveInputException;
 import camp.nextstep.edu.missionutils.Console;
 
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
  */
 public class InputView {
+    private static final int BRIDGE_LENGTH_MIN = 3;
+    private static final int BRIDGE_LENGTH_MAX = 20;
     private static final String READ_BRIDGE_SIZE_GUIDE = "다리의 길이를 입력해주세요.";
     private static final String READ_MOVING_GUIDE = "이동할 칸을 선택해주세요. (위: U, 아래: D)";
     private static final String READ_GAME_COMMAND_GUIDE = "게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)";
+    private static final String INPUT_NUMBER_EXCEPTION_MESSAGE = "숫자가 아닙니다.";
+    private static final String UP = "U";
+    private static final String DOWN = "D";
+    private static final String MOVE_REGEX = String.join("|", UP, DOWN);
+    private static final String RESTART = "R";
+    private static final String QUIT = "Q";
+    private static final String GAME_COMMAND_REGEX = String.join("|", RESTART, QUIT);
 
-    private final InputValidator inputValidator;
-
-    public InputView() {
-        this.inputValidator = new InputValidator();
-    }
 
     /**
      * 다리의 길이를 입력받는다.
@@ -23,13 +31,27 @@ public class InputView {
         System.out.println(READ_BRIDGE_SIZE_GUIDE);
         try {
             String text = Console.readLine();
-            int bridgeSize = inputValidator.validateNumber(text);
-            inputValidator.validateBridgeSizeRange(bridgeSize);
+            int bridgeSize = validateNumber(text);
+            validateBridgeSizeRange(bridgeSize);
             return bridgeSize;
         } catch (IllegalArgumentException exception) {
             System.out.println(exception.getMessage());
         }
         return readBridgeSize();
+    }
+
+    public void validateBridgeSizeRange(int size) {
+        if (size < BRIDGE_LENGTH_MIN || size > BRIDGE_LENGTH_MAX) {
+            throw new BridgeSizeRangeException(BRIDGE_LENGTH_MIN, BRIDGE_LENGTH_MAX);
+        }
+    }
+
+    public int validateNumber(String text) {
+        try {
+            return Integer.parseInt(text);
+        } catch (NumberFormatException exception) {
+            throw new BridgeException(INPUT_NUMBER_EXCEPTION_MESSAGE);
+        }
     }
 
     /**
@@ -39,12 +61,18 @@ public class InputView {
         System.out.println(READ_MOVING_GUIDE);
         try {
             String moving = Console.readLine();
-            inputValidator.validateInputMoving(moving);
+            validateInputMoving(moving);
             return moving;
         } catch (IllegalArgumentException exception) {
             System.out.println(exception.getMessage());
         }
         return readMoving();
+    }
+
+    public void validateInputMoving(String moving) {
+        if (moving == null || !moving.matches(MOVE_REGEX)) {
+            throw new MoveInputException(DOWN, UP);
+        }
     }
 
     /**
@@ -54,11 +82,17 @@ public class InputView {
         System.out.println(READ_GAME_COMMAND_GUIDE);
         try {
             String gameCommand = Console.readLine();
-            inputValidator.validateInputGameCommand(gameCommand);
+            validateInputGameCommand(gameCommand);
             return gameCommand;
         } catch (IllegalArgumentException exception) {
             System.out.println(exception.getMessage());
         }
         return readGameCommand();
+    }
+
+    public void validateInputGameCommand(String gameCommand) {
+        if (gameCommand == null || !gameCommand.matches(GAME_COMMAND_REGEX)) {
+            throw new GameCommandInputException(RESTART, QUIT);
+        }
     }
 }
--- src/test/java/bridge/view/InputViewTest.java
+++ src/test/java/bridge/view/InputViewTest.java
@@ -11,12 +11,12 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.NullAndEmptySource;
 import org.junit.jupiter.params.provider.ValueSource;
 
-class InputValidatorTest {
-    private InputValidator inputValidator;
+class InputViewTest {
+    private InputView inputView;
 
     @BeforeEach
     void setUp() {
-        this.inputValidator = new InputValidator();
+        this.inputView = new InputView();
     }
 
     @DisplayName("다리 길이 입력 테스트")
@@ -25,28 +25,28 @@ class InputValidatorTest {
         @ParameterizedTest(name = "3 이상 20 이하가 아니면 예외 처리한다")
         @ValueSource(ints = {1, 2, 21, 22})
         void inputBridgeLengthOutOfRange(int size) {
-            assertThatThrownBy(() -> inputValidator.validateBridgeSizeRange(size))
+            assertThatThrownBy(() -> inputView.validateBridgeSizeRange(size))
                     .isInstanceOf(IllegalArgumentException.class);
         }
 
         @ParameterizedTest(name = "3 이상 20 이하의 숫자를 입력할 수 있다.")
         @ValueSource(ints = {3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20})
         void inputBridgeLength(int size) {
-            assertDoesNotThrow(() -> inputValidator.validateBridgeSizeRange(size));
+            assertDoesNotThrow(() -> inputView.validateBridgeSizeRange(size));
         }
 
         @ParameterizedTest(name = "숫자가 아닌 문자면 예외 처리한다.")
         @NullAndEmptySource
         @ValueSource(strings = {" ", "abc", ",./", "가나다", "@#%^&"})
         void validateNumberNotDigit(String text) {
-            assertThatThrownBy(() -> inputValidator.validateNumber(text))
+            assertThatThrownBy(() -> inputView.validateNumber(text))
                     .isInstanceOf(IllegalArgumentException.class);
         }
 
         @ParameterizedTest(name = "숫자를 입력한다.")
         @ValueSource(strings = {"3", "4", "19", "20"})
         void validateNumber(String text) {
-            assertThat(inputValidator.validateNumber(text)).isEqualTo(Integer.parseInt(text));
+            assertThat(inputView.validateNumber(text)).isEqualTo(Integer.parseInt(text));
         }
     }
 
@@ -56,14 +56,14 @@ class InputValidatorTest {
         @ParameterizedTest(name = "U(위 칸)와 D(아래 칸) 중 하나의 문자를 입력할 수 있다.")
         @ValueSource(strings = {"U", "D"})
         void inputMoving() {
-            assertDoesNotThrow(() -> inputValidator.validateInputMoving("U"));
+            assertDoesNotThrow(() -> inputView.validateInputMoving("U"));
         }
 
         @ParameterizedTest(name = "U(위 칸)와 D(아래 칸) 중 하나의 문자를 입력할 수 있으며 올바른 값이 아니면 예외 처리한다.")
         @NullAndEmptySource
         @ValueSource(strings = {"u", "d", "UD", "DU", " ", "abc"})
         void inputMovingException(String moving) {
-            assertThatThrownBy(() -> inputValidator.validateInputMoving(moving))
+            assertThatThrownBy(() -> inputView.validateInputMoving(moving))
                     .isInstanceOf(IllegalArgumentException.class);
         }
     }
@@ -74,14 +74,14 @@ class InputValidatorTest {
         @ParameterizedTest(name = "R(재시작)과 Q(종료) 중 하나의 문자를 입력할 수 있다.")
         @ValueSource(strings = {"R", "Q"})
         void inputGameCommand(String gameCommand) {
-            assertDoesNotThrow(() -> inputValidator.validateInputGameCommand(gameCommand));
+            assertDoesNotThrow(() -> inputView.validateInputGameCommand(gameCommand));
         }
 
         @ParameterizedTest(name = "R(재시작)과 Q(종료) 중 하나의 문자를 입력할 수 있으며 올바른 값이 아니면 예외 처리한다.")
         @NullAndEmptySource
         @ValueSource(strings = {"r", "q", "Rq", "Qr", "ab", "cd"})
         void inputGameCommandException(String gameCommand) {
-            assertThatThrownBy(() -> inputValidator.validateInputGameCommand(gameCommand))
+            assertThatThrownBy(() -> inputView.validateInputGameCommand(gameCommand))
                     .isInstanceOf(IllegalArgumentException.class);
         }
     }
