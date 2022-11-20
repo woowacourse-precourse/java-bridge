335da550440c9bd179109e6bbab92e99f2d377ea
realslimtaek <89964264+realslimtaek@users.noreply.github.com>
2022-11-20 15:24:19 +0900

    refactor : BridgeMaker.java 에 만들었던 함수를 OutputView 및 BridgeGame 으로 이동

--- src/main/java/bridge/BridgeGame.java
+++ src/main/java/bridge/BridgeGame.java
@@ -1,9 +1,19 @@
 package bridge;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
 /**
  * 다리 건너기 게임을 관리하는 클래스
  */
 public class BridgeGame {
+    final InputView inputView = new InputView();
+    final OutputView outputView = new OutputView();
+    static int now_size = 0;
+    static List<String> bridge_answer = new ArrayList<>();
+
+
 
     /**
      * 사용자가 칸을 이동할 때 사용하는 메서드
@@ -11,6 +21,8 @@ public class BridgeGame {
      * 이동을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
     public void move() {
+
+
     }
 
     /**
@@ -20,4 +32,71 @@ public class BridgeGame {
      */
     public void retry() {
     }
+
+
+    public void resultBridge(List<String> user_answer) {
+        initBridgeStringBuilder();
+        for (int i = 0; i < OutputView.now_size + 1; i++) {
+            if (checkAnswer(bridge_answer.get(i), user_answer.get(i))) {
+                addAnswer(bridge_answer.get(i), i);
+            }
+            if (!checkAnswer(bridge_answer.get(i), user_answer.get(i))) {
+                addWrongAnswer(bridge_answer.get(i), i);
+            }
+        }
+        now_size++;
+        outputView.printResult();
+    }
+
+    public static void initBridgeStringBuilder() {
+        OutputView.upBridge.setLength(0);
+        OutputView.downBridge.setLength(0);
+        OutputView.upBridge.append("[ ");
+        OutputView.downBridge.append("[ ");
+    }
+
+    public static Boolean checkAnswer(String a, String b) {
+        return a.equals(b);
+    }
+
+    public static void addAnswer(String a, int i) {
+        if (Objects.equals(a, "U")) {
+            OutputView.upBridge.append("O ");
+            OutputView.downBridge.append("  ");
+        }
+        if (Objects.equals(a, "D")) {
+            OutputView.upBridge.append("  ");
+            OutputView.downBridge.append("O ");
+        }
+        addAndOr(now_size, i);
+    }
+
+    public static void addWrongAnswer(String a, int i) {
+        if (Objects.equals(a, "D")) {
+            OutputView.upBridge.append("X ");
+            OutputView.downBridge.append("  ");
+        }
+        if (Objects.equals(a, "U")) {
+            OutputView.upBridge.append("  ");
+            OutputView.downBridge.append("X ");
+        }
+        addAndOr(now_size, i);
+    }
+
+    public static void addAndOr(int a, int b) {
+        if (a == b) {
+            OutputView.upBridge.append("]");
+            OutputView.downBridge.append("]");
+            return;
+        }
+        OutputView.upBridge.append("| ");
+        OutputView.downBridge.append("| ");
+    }
+
+
+    public List<String> getBridgeAnswer(){
+        return bridge_answer;
+    }
+
+
 }
--- src/main/java/bridge/BridgeMaker.java
+++ src/main/java/bridge/BridgeMaker.java
@@ -11,91 +11,27 @@ public class BridgeMaker {
 
 
     private final BridgeNumberGenerator bridgeNumberGenerator;
-    static int now_size = 0;
-    static List<String> bridge_answer = new ArrayList<>();
-    static StringBuilder upBridge = new StringBuilder();
-    static StringBuilder downBridge = new StringBuilder();
 
     public BridgeMaker(BridgeNumberGenerator bridgeNumberGenerator) {
         this.bridgeNumberGenerator = bridgeNumberGenerator;
     }
 
     public List<String> makeBridge(int size) {
-
         BridgeRandomNumberGenerator bridgeRandomNumberGenerator = new BridgeRandomNumberGenerator();
         for (int i = 0; i < size; i++) {
             int a = bridgeRandomNumberGenerator.generate();
             if (a == 0)
-                bridge_answer.add("U");
+                BridgeGame.bridge_answer.add("U");
             if (a == 1)
-                bridge_answer.add("D");
+                BridgeGame.bridge_answer.add("D");
         }
         return null;
     }
 
-    public void resultBridge(List<String> user_answer) {
-        initBridgeStringBuilder();
-        for (int i = 0; i < now_size + 1; i++) {
-            if (checkAnswer(bridge_answer.get(i), user_answer.get(i))) {
-                addAnswer(bridge_answer.get(i), i);
-            }
-            if (!checkAnswer(bridge_answer.get(i), user_answer.get(i))) {
-                addWrongAnswer(bridge_answer.get(i), i);
-            }
-        }
-        now_size++;
-        showBridge();
-    }
 
-    public static void showBridge() {
-        System.out.println(upBridge.toString());
-        System.out.println(downBridge.toString());
-    }
 
-    public static void initBridgeStringBuilder() {
-        upBridge.setLength(0);
-        downBridge.setLength(0);
-        upBridge.append("[ ");
-        downBridge.append("[ ");
-    }
 
-    public static Boolean checkAnswer(String a, String b) {
-        return a.equals(b);
-    }
 
-    public static void addAnswer(String a, int i) {
-        if (Objects.equals(a, "U")) {
-            upBridge.append("O ");
-            downBridge.append("  ");
-        }
-        if (Objects.equals(a, "D")) {
-            upBridge.append("  ");
-            downBridge.append("O ");
-        }
-        addAndOr(now_size, i);
-    }
 
-    public static void addWrongAnswer(String a, int i) {
-        if (Objects.equals(a, "D")) {
-            upBridge.append("X ");
-            downBridge.append("  ");
-        }
-        if (Objects.equals(a, "U")) {
-            upBridge.append("  ");
-            downBridge.append("X ");
-        }
-        addAndOr(now_size, i);
-    }
 
-    public static void addAndOr(int a, int b) {
-        if (a == b) {
-            System.out.println("U 정답 끝");
-            upBridge.append("]");
-            downBridge.append("]");
-            return;
-        }
-        System.out.println("U 정답 안끝");
-        upBridge.append("| ");
-        downBridge.append("| ");
-    }
 }
