579dfaafe7974228c5fe8fe2b54bdf423f0ca237
hodomaroo <enghemdwkd@gmail.com>
2022-11-20 23:02:27 +0900

    [Feat/OutputView] OutputView 클래스 구현
    게임 결과에 대한 출력을 담당하는 OutputView 클래스의 메서드들을
    구현하였습니다.

--- src/main/java/view/OutputView.java
+++ src/main/java/view/OutputView.java
@@ -1,16 +1,31 @@
 package view;
 
+import dto.GameResult;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+import model.enums.MoveResult;
+
 /**
  * 사용자에게 게임 진행 상황과 결과를 출력하는 역할을 한다.
  */
 public class OutputView {
+
+    private final String GAME_INIT_COMMENT = "다리 건너기 게임을 시작합니다.";
+    private final String SUCCESS_FORMAT = "게임 성공 여부: %s";
+    private final String TOTAL_TRY_COUNT_FORMAT = "총 시도한 횟수: %d";
+    private final Map<Boolean, String> successMapper = Map.of(false, "실패", true, "성공");
+    private final String ERROR_FORMAT = "[ERROR] %s";
 
     /**
      * 현재까지 이동한 다리의 상태를 정해진 형식에 맞춰 출력한다.
      * <p>
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void printMap() {
+    public void printMap(GameResult gameResult) {
+        for (List<MoveResult> rowMoveResults : gameResult.getMoveResult()) {
+            System.out.println(getFormattedRow(rowMoveResults));
+        }
     }
 
     /**
@@ -18,6 +33,26 @@ public class OutputView {
      * <p>
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void printResult() {
+    public void printResult(GameResult gameResult) {
+        System.out.println("최종 게임 결과");
+        printMap(gameResult);
+        System.out.println();
+
+        System.out.println(String.format(SUCCESS_FORMAT, successMapper.get(gameResult.getStatus().succeed())));
+        System.out.println(String.format(TOTAL_TRY_COUNT_FORMAT, gameResult.getTryCount()));
     }
+
+    public void printError(String message) {
+        System.out.println(String.format(ERROR_FORMAT, message));
+    }
+
+    private String getFormattedRow(List<MoveResult> rowMoveResults) {
+        StringBuilder builder = new StringBuilder("[ ");
+        builder.append(String.join(" | ", rowMoveResults.stream().map((moveResult) -> moveResult.getStringValue())
+                .collect(Collectors.toUnmodifiableList())));
+        builder.append(" ]");
+        return builder.toString();
+    }
+
+
 }
