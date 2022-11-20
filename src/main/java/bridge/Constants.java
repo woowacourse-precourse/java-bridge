8dce9d4ec52afe2cd7b0fbcf98400453c7787d1f
LeeHaiLim <hailim1997@gmail.com>
2022-11-21 03:06:17 +0900

    feat(bridge): implement class for constants
    Added string constants
    - code up
    - code down
    - code retry
    - code quit

---
+++ src/main/java/bridge/Constants.java
@@ -0,0 +1,8 @@
+package bridge;
+
+public class Constants {
+    public static final String CODE_UP = "U";
+    public static final String CODE_DOWN = "D";
+    public static final String CODE_RETRY = "R";
+    public static final String CODE_QUIT = "Q";
+}
