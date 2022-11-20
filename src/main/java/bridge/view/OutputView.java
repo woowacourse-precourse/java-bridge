54c4a741b63ca1f7dfcc5f47008c7998a287fdba
jingyu9804 <swc05161@gmail.com>
2022-11-20 20:45:47 +0900

    feat(bridge) : 출력 메시지 상수 선언

--- src/main/java/bridge/view/OutputView.java
+++ src/main/java/bridge/view/OutputView.java
@@ -1,10 +1,13 @@
-package bridge;
+package bridge.view;
 
 /**
  * 사용자에게 게임 진행 상황과 결과를 출력하는 역할을 한다.
  */
 public class OutputView {
-
+    private static final String GAME_START_MESSAGE = "다리 건너기 게임을 시작합니다.";
+    private static final String FINAL_GAME_RESULT = "최종 게임 결과";
+    private static final String GAME_SUCCESS_RESULT = "게임 성공 여부";
+    private static final String TOTAL_TRY_TIME = "총 시도한 횟수";
     /**
      * 현재까지 이동한 다리의 상태를 정해진 형식에 맞춰 출력한다.
      * <p>
@@ -19,5 +22,9 @@ public class OutputView {
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
     public void printResult() {
+    }
+
+    public void printGameStart(){
+        System.out.println(GAME_START_MESSAGE);
     }
 }
