0b9bb0a32034dcd5bca6f243b10f6062708d6e4a
cgk95 <kjkj5868@gmail.com>
2022-11-21 00:50:13 +0900

    feat(Model) : 사용자 입력을 저장할 Stack<String> user 정의

--- src/main/java/bridge/Model.java
+++ src/main/java/bridge/Model.java
@@ -1,13 +1,14 @@
 package bridge;
 
 import java.util.List;
+import java.util.Stack;
 
 
 public class Model {
     public static List<String> bridge; // BridgeMaker에서 조작
+    public static Stack<String> user = new Stack<>(); // BridgeGame에서 조작
 
     public static int size;
-
 
 
 }
