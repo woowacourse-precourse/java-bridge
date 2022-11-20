96becf4f0483b42011f2a38f4e2d31a8dc03f1fa
yumble <hnjn1101@gmail.com>
2022-11-21 00:11:52 +0900

    feat(inputview) : 다리 길이 입력 및 예외체크
    readBridgeSize Function : 다리길이 입력받기
    toInts : 다리 길이에 관한 변수인 bridgeSize를 string에서 int 로 전환
    isBlank : 사용자에게 받은 input 이 whitespace나 빈칸이 아닌지 확인
    checkBridgeSizeRange : 다리길이가 요구사항 범위 안인지 확인

--- src/main/java/bridge/InputView.java
+++ src/main/java/bridge/InputView.java
@@ -1,5 +1,7 @@
 package bridge;
 
+import camp.nextstep.edu.missionutils.Console;
+
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
  */
@@ -8,9 +10,14 @@ public class InputView {
     /**
      * 다리의 길이를 입력받는다.
      */
-    public int readBridgeSize() {
-        return 0;
+    public int readBridgeSize() throws IllegalArgumentException {
+        String bridgeSizeStr =  Console.readLine();
+        if (isBlank(bridgeSizeStr) ){
+            throw new IllegalArgumentException();
+        }
+        return checkBridgeSizeRange(toInts(bridgeSizeStr));
     }
+
 
     /**
      * 사용자가 이동할 칸을 입력받는다.
@@ -24,5 +31,18 @@ public class InputView {
      */
     public String readGameCommand() {
         return null;
+    }
+
+    public int toInts(String bridgeSize) {
+        return Integer.parseInt(bridgeSize);
+    }
+    public boolean isBlank(String text){
+        return text == null || text.trim().isEmpty();
+    }
+    public int checkBridgeSizeRange(int bridgeSize) throws IllegalArgumentException {
+        if(bridgeSize < 3 || bridgeSize > 20) {
+            throw new IllegalArgumentException();
+        }
+        return bridgeSize;
     }
 }
