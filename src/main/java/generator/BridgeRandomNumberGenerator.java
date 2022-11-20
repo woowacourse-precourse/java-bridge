6b6039492d4e8582e115d8880ecf78b4eebf34cf
nylonmusk <106235834+nylonmusk@users.noreply.github.com>
2022-11-20 21:56:03 +0900

    refactor(BridgeRandomNumberGenerator): numberConstant명 변경에 따른 수정

--- src/main/java/generator/BridgeRandomNumberGenerator.java
+++ src/main/java/generator/BridgeRandomNumberGenerator.java
@@ -1,13 +1,13 @@
 package generator;
 
 import camp.nextstep.edu.missionutils.Randoms;
-import constant.GameConstant;
+import constant.NumberConstant;
 
 public class BridgeRandomNumberGenerator implements BridgeNumberGenerator {
 
     @Override
     public int generate() {
-        return Randoms.pickNumberInRange(GameConstant.RANDOM_LOWER_INCLUSIVE.getConstant()
-                , GameConstant.RANDOM_UPPER_INCLUSIVE.getConstant());
+        return Randoms.pickNumberInRange(NumberConstant.RANDOM_LOWER_INCLUSIVE.getConstant()
+                , NumberConstant.RANDOM_UPPER_INCLUSIVE.getConstant());
     }
 }
