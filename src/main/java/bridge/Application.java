57755d0c19a5dede5f15cac3b553a4cc25724bb5
Wooseobee <dntjq0807@naver.com>
2022-11-21 01:14:11 +0900

    feat: 다리 건너기 게임 구현 - 다리 생성 구현

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -1,21 +1,23 @@
 package bridge;
 
-import camp.nextstep.edu.missionutils.Console;
+import java.util.List;
 
 public class Application {
 
     public static void main(String[] args) {
         int n;
         boolean gameOver = false;
+        List<String> bridge;
 
         System.out.println("다리 건너기 게임을 시작합니다.\n");
         System.out.println("다리의 길이를 입력해주세요.");
         n = InputView.readBridgeSize();
         System.out.println();
+        bridge = new BridgeMaker(new BridgeRandomNumberGenerator()).makeBridge(n);
 
         do {
             System.out.println("이동할 칸을 선택해주세요. (위: U, 아래: D)");
-            InputView.readBridgeSize();
+            InputView.readMoving();
 
             if (gameOver) {
                 System.out.println("\n게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)");
