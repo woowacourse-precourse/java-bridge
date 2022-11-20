d36d9d502c4e741e35b27d941c77f3c2a20ed858
junwon <todayhappymod365@gmail.com>
2022-11-21 07:55:09 +0900

    Refactor: 테스트 코드 방식 변경
    System.in을 이용해서 입력 받는 방식이 아닌
    매개변수를 통해 테스트하기 쉽게 한다.

--- src/main/java/bridge/InputView.java
+++ src/main/java/bridge/InputView.java
@@ -4,25 +4,19 @@ package bridge;
  * 사용자로부터 입력을 받는 역할을 한다.
  */
 public class InputView {
-
-    /**
-     * 다리의 길이를 입력받는다.
-     */
-    public int readBridgeSize() {
+    public int readBridgeSize(int size) {
         return 0;
     }
-
-    /**
-     * 사용자가 이동할 칸을 입력받는다.
-     */
-    public String readMoving() {
+    public String readMoving(String direction) {
+        return null;
+    }
+    public String readGameCommand(String command) {
         return null;
     }
 
-    /**
-     * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
-     */
-    public String readGameCommand() {
-        return null;
-    }
 }
+/*
+  readBridgeSize -> 길이를 입력받는다.
+  readMoving -> 이동할 칸
+  readGame -> 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
+ */
--- src/test/java/bridge/InputTest.java
+++ src/test/java/bridge/InputTest.java
@@ -13,49 +13,35 @@ public class InputTest {
     @DisplayName("다리 길이를 빈 값을 입력시 에러 발생")
     void 다리길이_빈값_테스트(){
         assertThatThrownBy(() -> {
-            command("");
-            Application.main(new String[]{
-                    String.valueOf(new InputView())
-            });
+            new InputView().readBridgeSize(2);
         }).hasMessageContaining(ERROR_MESSAGE);
     }
-
     @Test
     @DisplayName("다리 길이를 숫자가 아닌 값을 입력시 에러 발생")
-    void 다리길이_숫자_테스트(){
+    void 다리길이_숫자_테스트() {
         assertThatThrownBy(() -> {
-            command("U");
-            Application.main(new String[]{
-                    String.valueOf(new InputView())
-            });
+            new InputView().readBridgeSize(Integer.parseInt("U"));
         }).hasMessageContaining(ERROR_MESSAGE);
     }
-
     @Test
     @DisplayName("게임 시 규칙에 맞지 않은 값을 입력시 에러 발생")
     void 게임_숫자_테스트(){
         assertThatThrownBy(() -> {
-            command("0");
-            Application.main(new String[]{
-                    String.valueOf(new InputView())
-            });
+            new InputView().readMoving("0");
         }).hasMessageContaining(ERROR_MESSAGE);
     }
-    
     @Test
     @DisplayName("게임 시 빈 값을 입력시 에러 발생")
     void 게임_빈값_테스트(){
         assertThatThrownBy(() -> {
-            command("");
-            Application.main(new String[]{
-                    String.valueOf(new InputView())
-            });
+            new InputView().readMoving("");
         }).hasMessageContaining(ERROR_MESSAGE);
     }
-
-
-    private void command(final String... args) {
-        final byte[] buf = String.join("\n", args).getBytes();
-        System.setIn(new ByteArrayInputStream(buf));
+    @Test
+    @DisplayName("재시작 시 규칙에 맞지 않은 값을 입력시 에러 발생")
+    void 재시작_빈값_테스트(){
+        assertThatThrownBy(() -> {
+            new InputView().readGameCommand("U");
+        }).hasMessageContaining(ERROR_MESSAGE);
     }
 }
