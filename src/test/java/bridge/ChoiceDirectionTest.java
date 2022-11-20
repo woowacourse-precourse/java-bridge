acc1f98cd9c457806fdb1bdad9242dd6e2b89263
TaeMin <xoals3094@naver.com>
2022-11-21 08:47:13 +0900

    test(ChoiceDirectionTest) : 다른 테스트를 진행하기 위해 Nested 클래스로 선언합니다

--- src/test/java/bridge/ChoiceDirectionTest.java
+++ src/test/java/bridge/ChoiceDirectionTest.java
@@ -2,26 +2,31 @@ package bridge;
 
 import bridge.Model.WrappingType.ChoiceDirection;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.Assertions.*;
 
 public class ChoiceDirectionTest {
-    @ParameterizedTest
-    @ValueSource(strings = {"U", "D"})
-    @DisplayName("유효")
-    void valid(String choice) {
-        ChoiceDirection userChoice = new ChoiceDirection(choice);
-        assertThat(userChoice).isInstanceOf(ChoiceDirection.class);
-    }
+    @Nested
+    @DisplayName("유효값 테스트")
+    class validDataTest {
+        @ParameterizedTest
+        @ValueSource(strings = {"U", "D"})
+        @DisplayName("유효")
+        void valid(String choice) {
+            ChoiceDirection userChoice = new ChoiceDirection(choice);
+            assertThat(userChoice).isInstanceOf(ChoiceDirection.class);
+        }
 
-    @ParameterizedTest
-    @ValueSource(strings = {"U12", "D3", "2", "안녕하세요"})
-    @DisplayName("예외")
-    void notValid(String choice) {
-        assertThatThrownBy(() -> {
-            new ChoiceDirection(choice);
-        }).isInstanceOf(IllegalArgumentException.class);
+        @ParameterizedTest
+        @ValueSource(strings = {"U12", "D3", "2", "안녕하세요"})
+        @DisplayName("예외")
+        void notValid(String choice) {
+            assertThatThrownBy(() -> {
+                new ChoiceDirection(choice);
+            }).isInstanceOf(IllegalArgumentException.class);
+        }
     }
 }
