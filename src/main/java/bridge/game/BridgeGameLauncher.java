1b460f65377383001c9a95be5d46b82651a270d1
JaehongDev <println@kakao.com>
2022-11-20 19:07:17 +0900

    feature (BridgeGameLauncher) : 런처가 게임의 상태를 시작 상태가 초기상태로 설정함

--- src/main/java/bridge/game/BridgeGameLauncher.java
+++ src/main/java/bridge/game/BridgeGameLauncher.java
@@ -1,7 +1,23 @@
 package bridge.game;
 
-public class BridgeGameLauncher {
-    public void execute(){
+import bridge.game.context.BridgeGameContext;
+import bridge.game.status.BridgeGameStatus;
+import bridge.game.status.InitStatus;
+import bridge.view.BridgeGameView;
 
+public class BridgeGameLauncher {
+    private final BridgeGameContext context;
+    private final BridgeGameView view;
+    private BridgeGameStatus status = new InitStatus();
+
+    public BridgeGameLauncher(final BridgeGameContext context, final BridgeGameView view) {
+        this.context = context;
+        this.view = view;
+    }
+
+    public void execute() {
+        while (status.runnable()) {
+            status = status.next(context, view);
+        }
     }
 }
