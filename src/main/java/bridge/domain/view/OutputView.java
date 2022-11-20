6a85f7f511c2a6bb942ad26fb4413fdcce759805
Sehbeom <j3beom@gmail.com>
2022-11-21 00:45:20 +0900

    refactor(view): 게임 결과 출력 기능 리팩토링
    게임 결과 출력 과정 별 함수 분리

--- src/main/java/bridge/domain/view/OutputView.java
+++ src/main/java/bridge/domain/view/OutputView.java
@@ -60,11 +60,27 @@ public class OutputView {
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
     public void printResult() {
+        printFinalRecord();
+        printSuccessOrFail();
+        printCountOfTry();
+    }
+
+    private void printFinalRecord() {
         System.out.println(GuideSentences.RESULT.getSentence());
         printMap();
-        System.out.println(
-                GuideSentences.IS_SUCCESS.getSentence() + GameResultInformation.getGameResult()
-                        .getResult());
-        System.out.println(GuideSentences.COUNT_OF_TRY.getSentence() + GameResultInformation.getCountOfTry());
+    }
+
+    private void printSuccessOrFail() {
+        String sentence =
+                GuideSentences.IS_SUCCESS.getSentence() +
+                        GameResultInformation.getGameResult().getResult();
+        System.out.println(sentence);
+    }
+
+    private void printCountOfTry() {
+        String sentence =
+                GuideSentences.COUNT_OF_TRY.getSentence() +
+                        GameResultInformation.getCountOfTry();
+        System.out.println(sentence);
     }
 }
