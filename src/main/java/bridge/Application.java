ffd4e3953d6718125bec47befb78c6d6a40396fe
K-IMjihun <62210749+K-IMjihun@users.noreply.github.com>
2022-11-20 16:36:43 +0900

    Application // 규격에 맞게 리팩토링

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -1,28 +1,16 @@
 package bridge;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-import java.util.Scanner;
-
 public class Application {
     static InputView inputView = new InputView();
     static BridgeGame bridgeGame = new BridgeGame();
 
     public static void main(String[] args) {
-        inputView.readBridgeSize();
-        bridgeGame.move();
-        retry();
-    }
-
-    public static void retry() {
-        while (true) {
-            if (bridgeGame.failOrTure.equals("실패")) {
-                bridgeGame.retry();
-            }
-            if (bridgeGame.failOrTure.equals("성공") || bridgeGame.retry().equals("Q")) {
-                break;
-            }
+        try {
+            inputView.readBridgeSize();
+            bridgeGame.move();
+        }
+        catch(IllegalArgumentException e) {
+            System.out.println("[ERROR]");
         }
     }
 }
