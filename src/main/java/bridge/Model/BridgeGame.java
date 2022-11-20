8272eb929b83b173c43c25915d492a55c9c84f1a
TaeMin <xoals3094@naver.com>
2022-11-20 16:49:26 +0900

    feat(BridgeGame) : BridgeGame 인스턴스 생성자 및 게임 제어 변수 추가

--- src/main/java/bridge/Model/BridgeGame.java
+++ src/main/java/bridge/Model/BridgeGame.java
@@ -1,15 +1,25 @@
 package bridge.Model;
 
+import bridge.BridgeMaker;
+import bridge.BridgeRandomNumberGenerator;
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * 다리 건너기 게임을 관리하는 클래스
  */
 public class BridgeGame {
+    List<String> choiceResults = new ArrayList<>();
+    Bridge bridge;
 
-    /**
-     * 사용자가 칸을 이동할 때 사용하는 메서드
-     * <p>
-     * 이동을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
-     */
+    public BridgeGame(BridgeSize bridgeSize){
+        BridgeRandomNumberGenerator bridgeRandomNumberGenerator = new BridgeRandomNumberGenerator();
+        BridgeMaker bridgeMaker = new BridgeMaker(bridgeRandomNumberGenerator);
+
+        int size = bridgeSize.getSize();
+        bridge = new Bridge(bridgeMaker.makeBridge(size));
+    }
+
     public void move() {
     }
 
