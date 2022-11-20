14ce739d3546ef21096aa41a8b3f01628874b18a
hojunking96 <junsong96@naver.com>
2022-11-20 21:36:39 +0900

    feats(Application.java) : 공통 변수 선언, 게임 진행 기능 구현

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -1,8 +1,25 @@
 package bridge;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Application {
+    public static int size;
+    public static List<String> bridge;
+    public static String movingInput;
+    public static int movingTurn = 0;
+    public static int totalTrial = 1;
+    public static UpSide upSide = new UpSide();
+    public static DownSide downSide = new DownSide();
+
 
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        Initialization.init();
+        while (StartGame.startGame()) {
+            if (movingTurn == size)
+                break;
+        }
+        OutputView.printResult();
+        //종료 출력
     }
 }
