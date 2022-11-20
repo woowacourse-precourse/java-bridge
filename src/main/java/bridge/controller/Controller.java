a254672b1430acb23215d72264d7f74522bae2a5
lecocococo <chemwang13@naver.com>
2022-11-20 23:51:13 +0900

    feat(Controller): 플레어어의 U, D 입력을 받아 정답을 맞춰가는 기능 추가
    - 에러 메시지를 출력 후 그 부분부터 입력을 다시 받는 기능
    - 틀린경우 맞춘 곳가지의 정답을 player가 가질수 있도록 함
    - U, D 입력이 아니라면 전부 예외처리
    - 리펙터링 필요

--- src/main/java/bridge/controller/Controller.java
+++ src/main/java/bridge/controller/Controller.java
@@ -23,6 +23,7 @@ public class Controller {
         System.out.println(GAME_START);
 
         Bridge answerBridge = makeAnswerBridge(bridgeMaker);
+        Bridge playerBridge = playingGame(answerBridge);
     }
 
     private Bridge makeAnswerBridge( BridgeMaker bridgeMaker) {
@@ -39,5 +40,39 @@ public class Controller {
     private Bridge setAnswerBridge(BridgeMaker bridgeMaker) {
         int bridgeSize = inputView.readBridgeSize();
         return new Bridge(bridgeMaker.makeBridge(bridgeSize));
+    }
+
+    private Bridge playingGame(Bridge answerBridge) {
+        Bridge playerBridge = new Bridge(new ArrayList<>());
+        try{
+            playerBridge = readPlayerBridge(playerBridge, answerBridge);
+        } catch (IllegalArgumentException ex) {
+            System.out.println(ex.getMessage());
+            playerBridge = playingGame(answerBridge);
+        }
+        return playerBridge;
+    }
+
+    private Bridge readPlayerBridge(Bridge playerBridge, Bridge answerBridge){
+        List<String> playerInput = playerBridge.getBridge();
+        int cnt = 0;
+        int position;
+        while(playerInput.size() != answerBridge.getBridge().size()) {
+            // 매칭 하는 부분 따로 빼서 리펙토링
+            String moveMessage = inputView.readMoving();
+            playerInput.add(moveMessage);
+
+            BridgeGame bridgeGame = new BridgeGame();
+            position = bridgeGame.move(answerBridge.getBridge(), cnt, moveMessage);
+
+            outputView.printMap(answerBridge.getBridge(), playerInput);
+
+            if (cnt == position) {
+                playerInput.remove(position);
+                break;
+            }
+            cnt = position;
+        }
+        return new Bridge(playerInput);
     }
 }
