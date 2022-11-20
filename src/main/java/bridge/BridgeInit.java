86435cf7733e5aeeadce2f54a7875268e5559923
jw0293 <k38836919@gmail.com>
2022-11-20 20:22:33 +0900

    feat : Check possible move step [건널 수 있는 칸인지 확인]

--- src/main/java/bridge/BridgeInit.java
+++ src/main/java/bridge/BridgeInit.java
@@ -1,2 +1,25 @@
-package bridge;public class BridgeInit {
+package bridge;
+
+import java.util.List;
+
+public class BridgeService {
+
+    private final InputView inputView;
+    private final OutputView outputView;
+    private final BridgeMaker bridgeMaker;
+
+    BridgeService(InputView inputView, OutputView outputView, BridgeMaker bridgeMaker){
+        this.inputView = inputView;
+        this.outputView = outputView;
+        this.bridgeMaker = bridgeMaker;
+    }
+
+    public List<String> makeGame(){
+        int bridgeLength = inputView.readBridgeSize();
+        return bridgeMaker.makeBridge(bridgeLength);
+    }
+
+    private String getNextStep(){
+        return inputView.readMoving();
+    }
 }
