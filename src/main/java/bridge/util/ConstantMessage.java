7d855306cfcdca9d92d685160aa4087b62310b48
fakerdeft <whakswp123@naver.com>
2022-11-21 02:24:34 +0900

    feat: 게임 재시작, 게임 결과 문구 추가

--- src/main/java/bridge/util/ConstantMessage.java
+++ src/main/java/bridge/util/ConstantMessage.java
@@ -1,9 +1,11 @@
 package bridge.util;
 
 public class ConstantMessage {
-    public static final String GAME_START = "다리 건너기 게임을 시작합니다.";
+    public static final String GAME_START_PHRASE = "다리 건너기 게임을 시작합니다.";
     public static final String BRIDGE_LENGTH_INPUT_PHRASE = "다리의 길이를 입력해주세요.";
     public static final String MOVING_BLOCK_INPUT_PHRASE = "이동할 칸을 선택해주세요. (위: U, 아래: D)";
+    public static final String RESTART_GAME_PHRASE = "게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)";
+    public static final String GAME_RESULT_PHRASE = "최종 게임 결과";
     public static final String BRIDGE_LENGTH_ERROR_MESSAGE = "[ERROR] 3이상 20이하의 숫자만 입력하세요.";
     public static final String MOVING_BLOCK_ERROR_MESSAGE = "[ERROR] 'U' 또는 'D'만 입력하세요.";
 }
