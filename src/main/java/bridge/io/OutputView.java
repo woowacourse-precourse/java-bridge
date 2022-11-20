51196ce92e83bc6e74334132a8f3564bb0c7b81c
cse0518 <g787@naver.com>
2022-11-20 18:34:41 +0900

    feat: OutputView 게임 안내 메시지 구현

--- src/main/java/bridge/io/OutputView.java
+++ src/main/java/bridge/io/OutputView.java
@@ -5,12 +5,28 @@ package bridge.io;
  */
 public class OutputView {
 
+    public static void printStartMessage() {
+        System.out.println("다리 건너기 게임을 시작합니다.");
+    }
+
+    public static void printGetBridgeSize() {
+        System.out.println("다리의 길이를 입력해주세요.");
+    }
+
+    public static void printGetBridgeCourse() {
+        System.out.println("이동할 칸을 선택해주세요. (위: U, 아래: D)");
+    }
+
     /**
      * 현재까지 이동한 다리의 상태를 정해진 형식에 맞춰 출력한다.
      * <p>
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void printMap() {
+    public static void printMap() {
+    }
+
+    public static void printGetRestartCommand() {
+        System.out.println("게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)");
     }
 
     /**
@@ -18,6 +34,6 @@ public class OutputView {
      * <p>
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void printResult() {
+    public static void printResult() {
     }
 }
