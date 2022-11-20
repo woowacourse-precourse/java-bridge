5bf255d8b54372eb55e5b51c656f416739cd918f
CAEL0 <kjongmin26@gmail.com>
2022-11-20 19:34:03 +0900

    feat(BridgeConstants): 게임 성공 여부 관련 상수 추가

--- src/main/java/bridge/constant/BridgeConstants.java
+++ src/main/java/bridge/constant/BridgeConstants.java
@@ -13,4 +13,7 @@ public class BridgeConstants {
     public static final String SUCCESS = "O";
     public static final String FAILURE = "X";
     public static final String SPACE = " ";
+
+    public static final String WIN = "성공";
+    public static final String LOSE = "실패";
 }
