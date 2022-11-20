8c4051d3ce15a448df2ff7824704661ca8d21a5e
Umin <adorecamus@gmail.com>
2022-11-20 23:40:39 +0900

    feat(Application): 다리 생성까지 구현

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -1,8 +1,19 @@
 package bridge;
 
+import java.util.List;
+
 public class Application {
 
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        try {
+            System.out.println("다리 건너기 게임을 시작합니다.");
+            InputView playerInput = new InputView();
+            int size = playerInput.readBridgeSize();
+            BridgeMaker bridgeMaker = new BridgeMaker(new BridgeRandomNumberGenerator());
+            List<String> bridge = bridgeMaker.makeBridge(size);
+
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+        }
     }
 }
