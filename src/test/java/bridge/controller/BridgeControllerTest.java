055afd93b08ec92501f63470a478cc9a39a13c03
yoonjuho <dbswn990@naver.com>
2022-11-20 21:34:59 +0900

    test(BridgeControllerTest): 테스트 코드 개선

--- src/test/java/bridge/controller/BridgeControllerTest.java
+++ src/test/java/bridge/controller/BridgeControllerTest.java
@@ -1,6 +1,8 @@
 package bridge.controller;
 
 import bridge.view.InputView;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -8,27 +10,29 @@ import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.*;
 
 class BridgeControllerTest {
+    ValidateInput validate;
+    @BeforeEach
+    void beforeEach() {
+        validate = new ValidateInput();
+    }
 
     @DisplayName("다리의 길이를 숫자로 입력 받지 않는 경우 예외처리.")
     @Test
     void inputBridgeLengthByNotNumeric() {
-        BridgeController bridgeController = new BridgeController();
-        assertThatThrownBy(() -> bridgeController.validateNumeric("h"))
+        assertThatThrownBy(() -> validate.numeric("h"))
                 .isInstanceOf(IllegalArgumentException.class);
     }
     @DisplayName("이동할 때 다른 문자가 입력 되는 경우 예외처리.")
     @Test
     void inputMoveLetterByAnotherLetter() {
-        BridgeController bridgeController = new BridgeController();
-        assertThatThrownBy(() -> bridgeController.validateLetter("a"))
+        assertThatThrownBy(() -> validate.letter("a"))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
     @DisplayName("게임이 실패 하고 다른 문자가 입력 되는 경우 예외처리.")
     @Test
     void inputEndLetterByAnotherLetter() {
-        BridgeController bridgeController = new BridgeController();
-        assertThatThrownBy(() -> bridgeController.validateEndLetter("b"))
+        assertThatThrownBy(() -> validate.endLetter("b"))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 }
