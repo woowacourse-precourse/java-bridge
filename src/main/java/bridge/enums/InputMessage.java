cec608504fa69a950f9e2bc16ab82a47fd1421c8
3juhwan <13selfesteem91@naver.com>
2022-11-20 10:15:42 +0900

    feat(bridge): 입력 메세지 열거형 구현

---
+++ src/main/java/bridge/enums/InputMessage.java
@@ -0,0 +1,19 @@
+package bridge.enums;
+
+public enum InputMessage {
+
+    ENTER_BRIDGE_LENGTH_MESSAGE("다리의 길이를 입력해주세요."),
+    SELECT_CELL_TO_MOVE_MESSAGE("이동할 칸을 선택해주세요. (위: U, 아래: D)"),
+    ENTER_WHETHER_TO_CONTINUE_GAME_MESSAGE("게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)"),
+    ;
+
+    private final String message;
+
+    InputMessage(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+}
