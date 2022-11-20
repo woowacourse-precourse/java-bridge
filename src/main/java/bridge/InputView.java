0764422baefc185bd324055eaf5b7cc35fb12b0b
wonseokjee <wsji9404@gmail.com>
2022-11-21 06:31:40 +0900

    Feat(InputView): 추가 userInput함수
    readline()을 받는 userInput 함수 생성

--- src/main/java/bridge/InputView.java
+++ src/main/java/bridge/InputView.java
@@ -1,21 +1,26 @@
 package bridge;
 
 import static camp.nextstep.edu.missionutils.Console.readLine;
+
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
  */
 public class InputView {
     BridgeGame bridgeGame = new BridgeGame();
     ValidateError validateError = new ValidateError();
+
     /**
      * 다리의 길이를 입력받는다. -> bridgemaker
      */
-    public int readBridgeSize() {
+
+    public String userInput() {
+        return readLine();
+    }
+
+    public int readBridgeSize(String userInput) {
         bridgeGame.printInputBridgeSize();
-        String userInput = readLine();
         validateError.validIsNumber(userInput);
         validateError.validIsInRange(userInput);
-
         return Integer.parseInt(userInput);
     }
 
@@ -23,9 +28,8 @@ public class InputView {
     /**
      * 사용자가 이동할 칸을 입력받는다.
      */
-    public String readMoving() {
+    public String readMoving(String userInput) {
         bridgeGame.printSelectBridgeUpDown();
-        String userInput = readLine();
         validateError.validIsNotSuitableMovingCommand(userInput);
         return userInput;
     }
@@ -33,9 +37,8 @@ public class InputView {
     /**
      * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
      */
-    public String readGameCommand() {
+    public String readGameCommand(String userInput) {
         bridgeGame.printRestartOrQuit();
-        String userInput = readLine();
         validateError.validIsNotSuitableEndCommand(userInput);
         return userInput;
     }
