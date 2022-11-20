5840ecf0139d5dbafaa01c3122b6461f4cd0ab5c
euna <christinejung10@daum.net>
2022-11-21 07:26:29 +0900

    fix(BridgeGameTest): fix tests to check method move

--- src/test/java/bridge/game/BridgeGameTest.java
+++ src/test/java/bridge/game/BridgeGameTest.java
@@ -1,8 +1,7 @@
 package bridge.game;
 
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.ValueSource;
+import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
@@ -10,16 +9,21 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class BridgeGameTest {
 
-    private static List<String> bridge;
     private static BridgeGame bridgeGame = new BridgeGame();
+    private static List<String> bridge;
 
+    @DisplayName("사용자의 이동 위치 저장 테스트 - 아래 칸")
+    @Test
+    void move_down_테스트() {
+        bridgeGame.move(BridgeGame.BridgeShape.DOWN.getStringValue());
+        assertThat(bridgeGame.getBridge_userMove()).contains("D");
+    }
 
-    @DisplayName("사용자의 이동 위치 저장 테스트")
-    @ValueSource(strings = {"D", "U"})
-    @ParameterizedTest
-    void move_테스트(String userMoveDirection) {
-        bridgeGame.move(userMoveDirection);
-        assertThat(bridgeGame.getBridge_userMove()).contains(userMoveDirection);
+    @DisplayName("사용자의 이동 위치 저장 테스트 - 위 칸")
+    @Test
+    void move_up_테스트() {
+        bridgeGame.move(BridgeGame.BridgeShape.UP.getStringValue());
+        assertThat(bridgeGame.getBridge_userMove()).contains("U");
     }
 
 }
