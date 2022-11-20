cc3e185e624910bd3f862bb3d36e8bfb3f6a1477
haisley77 <qhfg3145@naver.com>
2022-11-21 00:00:53 +0900

    feat(InputView): 예외 처리

--- src/main/java/bridge/InputView.java
+++ src/main/java/bridge/InputView.java
@@ -14,6 +14,10 @@ public class InputView {
         OutputView.printStart();
         System.out.println("\n다리의 길이를 입력해주세요.");
         bridge_size = Integer.parseInt(Console.readLine());
+        if (bridge_size < 3 || bridge_size > 20){
+            System.out.println("[ERROR] 다리 길이는 3부터 20 사이의 숫자여야 합니다.");
+            throw new IllegalArgumentException();
+        }
         return bridge_size;
     }
 
@@ -23,6 +27,10 @@ public class InputView {
     public String readMoving() {
         System.out.println("\n이동할 칸을 선택해주세요. (위: U, 아래: D)");
         String moving = Console.readLine();
+        if (!(moving.equals("U")) && !(moving.equals("D"))){
+            System.out.println("[ERROR] 이동할 칸은 U/D 중 하나를 입력해야 합니다.");
+            throw new IllegalArgumentException();
+        }
         return moving;
     }
 
@@ -32,6 +40,10 @@ public class InputView {
     public String readGameCommand() {
         System.out.println("\n게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)");
         String game_command = Console.readLine();
+        if (!(game_command.equals("R")) && !(game_command.equals("Q"))){
+            System.out.println("[ERROR] 재시작 여부는 R/Q 중 하나를 입력해야 합니다.");
+            throw new IllegalArgumentException();
+        }
         return game_command;
     }
 }
