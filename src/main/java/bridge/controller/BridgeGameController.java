9e7ba376599b201ec1bb35def5806c88f74e2bc5
kim0914 <kosa0914@naver.com>
2022-11-20 14:00:07 +0900

    feat(BridgeController): 게임에 필요한 객체를 생성하는 메서드 추가

--- src/main/java/bridge/controller/BridgeGameController.java
+++ src/main/java/bridge/controller/BridgeGameController.java
@@ -2,6 +2,7 @@ package bridge.controller;
 
 import bridge.BridgeRandomNumberGenerator;
 import bridge.domain.*;
+import bridge.service.BridgeGame;
 import bridge.view.InputView;
 import bridge.view.OutputView;
 
@@ -19,5 +20,13 @@ public class BridgeGameController {
         inputView = new InputView();
         outputView = new OutputView();
         bridgeMaker = new BridgeMaker(new BridgeRandomNumberGenerator());
+    }
+
+    public void run() {
+        inputView.showStartMessage();
+
+        Bridge bridge = new Bridge(bridgeMaker.makeBridge(inputView.readBridgeSize()));
+        User user = new User(INIT_POSITION, INIT_TRIAL);
+        BridgeGame bridgeGame = new BridgeGame(bridge, user);
     }
 }
