82d396f0b33270095a4e5814fea829be47901cc7
thdwoqor <celebarte01@gmail.com>
2022-11-21 02:09:54 +0900

    fix: 게임 실패시에도 성공이라고 출력되는 오류 수정

--- src/main/java/bridge/view/OutputView.java
+++ src/main/java/bridge/view/OutputView.java
@@ -15,7 +15,7 @@ import java.util.stream.IntStream;
 public class OutputView {
     private static final String START_OUTPUT_MESSAGE = "다리 건너기 게임을 시작합니다.\n";
     private static final String SUCCESS_OUTPUT_MESSAGE = "게임 성공 여부: 성공";
-    private static final String FAIL_OUTPUT_MESSAGE = "게임 성공 여부: 성공";
+    private static final String FAIL_OUTPUT_MESSAGE = "게임 성공 여부: 실패";
     private static final String RETRY_COUNT_OUTPUT_MESSAGE = "총 시도한 횟수: %d";
     private static final String Final_OUTPUT_MESSAGE = "최종 게임 결과";
 
