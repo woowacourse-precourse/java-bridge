2574be2a90c2ef7ee305e50fd65d6d42a2acfa7b
ho2n <jungho2n@gmail.com>
2022-11-21 00:10:47 +0900

    feat(BridgeGame): 끝까지 건넜거나 종료를 입력하여 종료하였을 때 최종 게임 결과와 이동한 결과를 출력하는 기능을 추가

--- src/main/java/constant/Message.java
+++ src/main/java/constant/Message.java
@@ -17,13 +17,16 @@ public class Message {
     public static final String WRONG = "X";
     public static final String EMPTY = " ";
 
+
     public static final String PRINT_START_MESSAGE = "다리 건너기 게임을 시작합니다.\n";
     public static final String INPUT_BRIDGE_LENGTH = "다리의 길이를 입력해주세요.";
     public static final String SELECT_UP_AND_DOWN = "이동할 칸을 선택해주세요. (위: %s, 아래: %s)";
     public static final String SELECT_REGAME_OR_QUIT = "게임을 다시 시도할지 여부를 입력해주세요. (재시도: %s, 종료: %s)";
     public static final String PRINT_GAME_RESULT = "최종 게임 결과";
-    public static final String PRINT_WHETHER_SUCCESS_OR_NOT = "게임 성공 여부: 성공";
+    public static final String PRINT_WHETHER_SUCCESS_OR_NOT = "게임 성공 여부: %s";
     public static final String TOTAL_NUMBERS_OF_ATTEMPTS = "총 시도한 횟수: %d";
     public static final String NEWLINE = "\n";
 
-    }
+    public static final String SUCCESS = "성공";
+    public static final String FAILURE = "실패";
+}
--- src/main/java/view/OutputView.java
+++ src/main/java/view/OutputView.java
@@ -1,5 +1,7 @@
 package view;
 
+import static constant.Message.*;
+
 import bridge.Bridge;
 import constant.Message;
 
@@ -15,19 +17,26 @@ public class OutputView {
      */
     public void printMap() {
     }
-
     /**
      * 게임의 최종 결과를 정해진 형식에 맞춰 출력한다.
      * <p>
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void printResult(Bridge bridge) {
-        System.out.println(Message.PRINT_GAME_RESULT);
-        bridge.minusCurrent();
-        bridge.printSuccess();
+    public void printResult(Bridge bridge, String result) {
+        System.out.println(PRINT_GAME_RESULT);
+        if (result == SUCCESS) {
+            bridge.minusCurrent();
+            bridge.printSuccess();
+            System.out.println(String.format(PRINT_WHETHER_SUCCESS_OR_NOT, result));
+            System.out.println(String.format(TOTAL_NUMBERS_OF_ATTEMPTS, bridge.getGameCount()));
+            return;
+        }
+        bridge.printFailure();
+        System.out.println(String.format(PRINT_WHETHER_SUCCESS_OR_NOT, result));
+        System.out.println(String.format(TOTAL_NUMBERS_OF_ATTEMPTS, bridge.getGameCount()));
     }
 
     public void printStartMessage() {
-        System.out.println(Message.PRINT_START_MESSAGE);
+        System.out.println(PRINT_START_MESSAGE);
     }
 }
