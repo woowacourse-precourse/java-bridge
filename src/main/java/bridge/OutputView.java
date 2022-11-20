be655eea832e15b5afdd555e9b4bf284216df0eb
Hyunseok Lee <hyun104467@naver.com>
2022-11-21 02:45:15 +0900

    fix(OutputView): 메소드 스태틱화

--- src/main/java/bridge/OutputView.java
+++ src/main/java/bridge/OutputView.java
@@ -4,28 +4,28 @@ import java.util.List;
 
 public class OutputView {
 
-    public void printMap(List<Integer> upperBridge, List<Integer> bottomBridge) {
+    public static void printMap(List<Integer> upperBridge, List<Integer> bottomBridge) {
         upperBridgeMap(upperBridge);
         bottomBridgeMap(bottomBridge);
     }
 
-    public void printResult(List<Integer> upperBridge, List<Integer> bottomBridge) {
+    public static void printResult(List<Integer> upperBridge, List<Integer> bottomBridge) {
         System.out.println("최종 게임 결과");
         printMap(upperBridge, bottomBridge);
-        System.out.println("\n 게임 성공 여부:");
+        System.out.print("\n게임 성공 여부: ");
         checkFinalResult(upperBridge, bottomBridge);
         System.out.println("총 시도한 횟수: " + BridgeGame.tryCount);
     }
 
-    private void checkFinalResult(List<Integer> upperBridge, List<Integer> bottomBridge) {
+    private static void checkFinalResult(List<Integer> upperBridge, List<Integer> bottomBridge) {
         if (!(upperBridge.contains(0)) && !(bottomBridge.contains(0))) {
-            System.out.print("성공");
+            System.out.println("성공");
             return;
         }
-        System.out.print("실패");
+        System.out.println("실패");
     }
 
-    private void upperBridgeMap(List<Integer> upperBridge) {
+    private static void upperBridgeMap(List<Integer> upperBridge) {
         System.out.print("[");
         for (int i = 0; i < upperBridge.size(); i++) {
             printBridgeUnit(upperBridge.get(i));
@@ -36,7 +36,7 @@ public class OutputView {
         System.out.println("]");
     }
 
-    private void bottomBridgeMap(List<Integer> bottomBridge) {
+    private static void bottomBridgeMap(List<Integer> bottomBridge) {
         System.out.print("[");
         for (int i = 0; i < bottomBridge.size(); i++) {
             printBridgeUnit(bottomBridge.get(i));
@@ -47,19 +47,19 @@ public class OutputView {
         System.out.println("]");
     }
 
-    private void printBridgeUnit(Integer bridgeUnit) {
+    private static void printBridgeUnit(Integer bridgeUnit) {
         if (!printStatus(bridgeUnit)){
             skipBridgeUnit(bridgeUnit);
         }
     }
 
-    private void skipBridgeUnit(Integer bridgeUnit) {
+    private static void skipBridgeUnit(Integer bridgeUnit) {
         if (bridgeUnit == 2) {
             System.out.print("   ");
         }
     }
 
-    private Boolean printStatus(Integer bridgeUnit) {
+    private static Boolean printStatus(Integer bridgeUnit) {
         if (bridgeUnit == 1) {
             System.out.print(" O ");
         }
