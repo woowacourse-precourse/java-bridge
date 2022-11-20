219d9b3161fb9e259409ab0fbc00cc133470d2b0
This2sho <zx00018@naver.com>
2022-11-20 20:08:57 +0900

    refactor(PlayerTest): @DisplayName 적용해서 테스트 더 직관적으로 알 수 있게 변경

--- src/test/java/bridge/domain/PlayerTest.java
+++ src/test/java/bridge/domain/PlayerTest.java
@@ -2,11 +2,13 @@ package bridge.domain;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class PlayerTest {
 
 	@Test
+	@DisplayName("플레이어의 죽음을 나타내는 상태 테스트")
 	void 플레이어_상태_테스트() {
 		//given
 		Player player = new Player();
@@ -18,7 +20,8 @@ public class PlayerTest {
 	}
 
 	@Test
-	public void 플레이어_이동_테스트() {
+	@DisplayName("플레이어 이동 후 위치 변화 테스트")
+	void 플레이어_이동_테스트() {
 	    //given
 		Player player = new Player();
 		int currentLocation = player.getCurrentLocation();
