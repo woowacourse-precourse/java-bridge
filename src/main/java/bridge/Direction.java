27c3997c4da3b6f59243490d5544c5cdfa510fa0
yumble <hnjn1101@gmail.com>
2022-11-20 23:48:04 +0900

    feat : 다리 생성 기능 추가
    BridgeMaker(makeBridge) : size를 받으면 그 길이만큼 다리 리스트 생성
    Direction : random 숫자가 생성되면 그에 맞게 위아래 string 반환

---
+++ src/main/java/bridge/Direction.java
@@ -0,0 +1,26 @@
+package bridge;
+
+import java.util.Arrays;
+
+public enum Direction {
+
+    LOWER_DIRECTION(0, "D"),
+    UPPER_DIRECTION(1, "U"),
+    EMPTY(-1, "없음");
+
+    private final int directionNumber;
+    private final String directionString;
+
+
+    Direction(int directionNumber, String directionString) {
+        this.directionNumber = directionNumber;
+        this.directionString = directionString;
+    }
+
+    public static String findByDirectionString(int directionNumber) {
+        return Arrays.stream(Direction.values())
+                .filter(bridge -> bridge.directionNumber == directionNumber)
+                .findAny().get().directionString;
+
+    }
+}
