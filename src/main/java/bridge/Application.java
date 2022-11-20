5582c6b76e9c90448d7f32a1e8ea1a537c04ed6b
alpacaHyun <flaehdan@gmail.com>
2022-11-21 01:26:39 +0900

    type : feat 첫 커밋

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -1,8 +1,10 @@
 package bridge;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
 
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+
     }
 }
