641062c7af0d129b21e345593c97ab7b63bf2e16
leesangjin <tkdwls0628@gmail.com>
2022-11-21 02:04:12 +0900

    feat(BridgeGame): 입력한 칸을 이동하는 기능 구현

--- src/main/java/bridge/BridgeGame.java
+++ src/main/java/bridge/BridgeGame.java
@@ -15,7 +15,25 @@ public class BridgeGame {
         }
     }
 
-    public void move() {
+    public void move(String answer, String moving) {
+        // TODO : 중복 제거해서 메서드 길이 줄일 것 / else 문 없애도록 노력할 것
+        if (answer.equals(moving)) {
+            if (moving.equals(Constant.UP)) {
+                bridgeMap.get(0).add("O");
+                bridgeMap.get(1).add(" ");
+            } else {
+                bridgeMap.get(0).add(" ");
+                bridgeMap.get(1).add("O");
+            }
+        } else {
+            if (moving.equals(Constant.UP)) {
+                bridgeMap.get(0).add("X");
+                bridgeMap.get(1).add(" ");
+            } else {
+                bridgeMap.get(0).add(" ");
+                bridgeMap.get(1).add("X");
+            }
+        }
     }
 
     public void retry() {
