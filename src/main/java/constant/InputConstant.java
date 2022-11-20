d6f706c1a7b9ef97d1f22a6bf7710bc5de1920c9
nylonmusk <106235834+nylonmusk@users.noreply.github.com>
2022-11-21 00:04:58 +0900

    refactor(InputConstant): 사용자의 입력값 관련 상수 클래스 분리

---
+++ src/main/java/constant/InputConstant.java
@@ -0,0 +1,20 @@
+package constant;
+
+public enum InputConstant {
+    UP_SIDE("U"),
+    DOWN_SIDE("D"),
+    RETRY("R"),
+    QUIT("Q");
+
+    public final String inputConstant;
+
+    InputConstant(final String inputConstant) {
+        this.inputConstant = inputConstant;
+    }
+
+    public String getInputConstant() {
+        return inputConstant;
+    }
+
+
+}
