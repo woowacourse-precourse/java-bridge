1e4429f325f60c99f98acc8fd50c8a1f98f47edd
desfox <desfox99@naver.com>
2022-11-21 00:19:22 +0900

    feat(BridgeGameValidation): 이동 불가능한 칸으로 이동하지 않았는지 검증하는 로직 구현

--- src/main/java/bridge/validation/BridgeGameValidation.java
+++ src/main/java/bridge/validation/BridgeGameValidation.java
@@ -12,4 +12,14 @@ public class BridgeGameValidation {
             }
         }
     }
+
+    public void validateNotMoveWhenUnavailable(String movingCommand, List<String> bridge, List<String> bridgeCurrStatus) {
+        int lastIdx = bridge.size() - 1;
+
+        if (! movingCommand.equals(bridge.get(lastIdx))) {
+            if (bridgeCurrStatus.get(lastIdx).equals("O")) {
+                throw new IllegalArgumentException("[ERROR] 이동할 수 없는 칸으로 이동했습니다.");
+            }
+        }
+    }
 }
