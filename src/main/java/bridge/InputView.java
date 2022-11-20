4ef08940f2105ca125924657be930472445e7097
Sangkyun Seo <tkdrbs0802@naver.com>
2022-11-20 15:11:33 +0900

    feat: 이동할 칸 입력 메서드 작성

--- src/main/java/bridge/InputView.java
+++ src/main/java/bridge/InputView.java
@@ -19,7 +19,10 @@ public class InputView {
      * 사용자가 이동할 칸을 입력받는다.
      */
     public String readMoving() {
-        return null;
+        String moveBridge = "";
+        System.out.println("이동할 칸을 선택해주세요. (위: U, 아래: D)");
+        moveBridge = Console.readLine();
+        return moveBridge;
     }
 
     /**
