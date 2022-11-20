535c67f614987a7cc0705b0af4ecbe4506e6544c
Ji-Un-Gil <wldns2577@gmail.com>
2022-11-20 23:35:01 +0900

    feat(BridgeGame.java): move function Updated field, function: - 현재 사용자가 이동할 위치를 담는 인스턴스 변수 index 추가 - 이에 따른 move에서 index와 관련된 기능 변경 style: - move, retry 불필요한 주석 삭제

--- src/main/java/bridge/BridgeGame.java
+++ src/main/java/bridge/BridgeGame.java
@@ -15,6 +15,7 @@ public class BridgeGame {
     private List<String> bridgeStatus = new ArrayList<>();
     private boolean flag = false;
     private int count = 1;
+    private int index = 0;
     private int size;
 
     /**
@@ -34,25 +35,20 @@ public class BridgeGame {
 
     /**
      * 사용자가 칸을 이동할 때 사용하는 메서드
-     * <p>
-     * 이동을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public List<String> move(String input, int index) {
-        String status = input;
+    public List<String> move(String input) {
         if (input.equals(bridgeList.get(index))){
-            status += "O";
-            bridgeStatus.add(status);
+            bridgeStatus.add(input + "O");
+            index++;
             return bridgeStatus;
         }
-        status += "X";
-        bridgeStatus.add(status);
+        bridgeStatus.add(input + "X");
+        index++;
         return bridgeStatus;
     }
 
     /**
      * 사용자가 게임을 다시 시도할 때 사용하는 메서드
-     * <p>
-     * 재시작을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
     public void retry() {
         bridgeStatus.clear();
