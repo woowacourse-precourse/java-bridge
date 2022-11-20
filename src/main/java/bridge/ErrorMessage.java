9d3f7217b0b59bc70e97a1c0d981f65a3b9f4cfd
dlrghks2090 <dlrghks2090@gmail.com>
2022-11-20 21:33:32 +0900

    feat: 입력 기능 추가

--- src/main/java/bridge/ErrorMessage.java
+++ src/main/java/bridge/ErrorMessage.java
@@ -3,7 +3,9 @@ package bridge;
 public enum ErrorMessage {
 
     BRIDGE_LENGTH_IS_ONLY_NUMBER("[ERROR] 숫자만 입력해야 합니다."),
-    BRIDGE_LENGTH_IS_THREE_TO_TWENTY("[ERROR] 다리의 길이는 3이상 20이하여야 합니다.");
+    BRIDGE_LENGTH_IS_THREE_TO_TWENTY("[ERROR] 다리의 길이는 3이상 20이하여야 합니다."),
+    MOVE_BRIDGE_SPACE_IS_U_OR_D("[ERROR] 이동할 다리의 칸은 U 또는 D 중 하나만 입력해야 합니다."),
+    RETRY_ANSWER_IS_R_OR_Q("[ERROR] 재시도 여부에 대한 입력은 R 또는 Q 중 하나만 입력해야 합니다.");
 
     private final String errorMessage;
 
--- src/main/java/bridge/view/InputView.java
+++ src/main/java/bridge/view/InputView.java
@@ -9,6 +9,8 @@ import camp.nextstep.edu.missionutils.Console;
 public class InputView {
 
     private static final String BRIDGE_LENGTH_QUESTION = "다리의 길이를 입력해주세요.";
+    private static final String MOVE_BRIDGE_SPACE_QUESTION = "이동할 칸을 선택해주세요. (위: U, 아래: D)";
+    private static final String RETRY_QUESTION = "게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)";
     /**
      * 다리의 길이를 입력받는다.
      */
@@ -34,13 +36,36 @@ public class InputView {
      * 사용자가 이동할 칸을 입력받는다.
      */
     public String readMoving() {
-        return null;
+        String moveBridgeSpace;
+
+        System.out.println(MOVE_BRIDGE_SPACE_QUESTION);
+        moveBridgeSpace = Console.readLine();
+        validateUpOrDown(moveBridgeSpace);
+
+        return moveBridgeSpace;
+    }
+
+    private void validateUpOrDown(String moveBridgeSpace) {
+        if (!moveBridgeSpace.equals("U") && moveBridgeSpace.equals("D")) {
+            throw new IllegalArgumentException(ErrorMessage.MOVE_BRIDGE_SPACE_IS_U_OR_D.getErrorMessage());
+        }
     }
 
     /**
      * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
      */
     public String readGameCommand() {
-        return null;
+        String retryAnswer;
+
+        System.out.println(RETRY_QUESTION);
+        retryAnswer = Console.readLine();
+        validateRetryOrQuit(retryAnswer);
+        return retryAnswer;
+    }
+
+    private void validateRetryOrQuit(String retryAnswer) {
+        if (!retryAnswer.equals("R") && !retryAnswer.equals("Q")) {
+            throw new IllegalArgumentException(ErrorMessage.RETRY_ANSWER_IS_R_OR_Q.getErrorMessage());
+        }
     }
 }
