07f25860a0d4b7ab68889e7053e9a67aaf25126a
BoyCho <iijokv@naver.com>
2022-11-21 04:16:31 +0900

    feat : 게임 진행 기능 추가  - 게임 종료 유무를 문자열로 반환받음

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -1,8 +1,22 @@
 package bridge;
 
+import java.util.List;
+
 public class Application {
 
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        System.out.println("다리 건너기 게임을 시작합니다.");
+
+        try {
+            int bridgeSize = new InputView().readBridgeSize();
+            List<String> bridge = new BridgeMaker(new BridgeRandomNumberGenerator()).makeBridge(bridgeSize);
+            BridgeGame bridgeGame = new BridgeGame(bridge);
+            while(true) {
+                if (bridgeGame.move().equals("GameOver")) break;
+            }
+        }catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+
     }
 }
