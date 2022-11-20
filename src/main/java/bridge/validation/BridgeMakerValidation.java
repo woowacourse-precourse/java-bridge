b9b65942eb2ff42e8ba56a71db2be118a2150fdb
desfox <desfox99@naver.com>
2022-11-20 21:57:29 +0900

    refactor(BridgeMakerValidation): 매직넘버 리팩토링

--- src/main/java/bridge/validation/BridgeMakerValidation.java
+++ src/main/java/bridge/validation/BridgeMakerValidation.java
@@ -3,8 +3,11 @@ package bridge.validation;
 import java.util.List;
 
 public class BridgeMakerValidation {
+    private static final int RANDOM_NUM_LOWER = 0;
+    private static final int RANDOM_NUM_UPPER = 1;
+
     public void validateGeneratedNumValue(int generatedNum) {
-        if (generatedNum != 0 && generatedNum != 1) {
+        if (generatedNum != RANDOM_NUM_LOWER && generatedNum != RANDOM_NUM_UPPER) {
             throw new IllegalArgumentException("[ERROR] 0 또는 1의 값만 생성되어야 합니다.");
         }
     }
@@ -17,7 +20,7 @@ public class BridgeMakerValidation {
     public void validateGeneratedNumZeroAndLastBoxLower(int generatedNum, List<String> bridge) {
         int lastIdx = bridge.size() - 1;
 
-        if (generatedNum == 0) {
+        if (generatedNum == RANDOM_NUM_LOWER) {
             if (! bridge.get(lastIdx).equals("D")) {
                 throw new IllegalArgumentException("[ERROR] 랜덤값이 0일 경우, 다리에 아래 칸이 추가되어야 합니다.");
             }
@@ -27,7 +30,7 @@ public class BridgeMakerValidation {
     public void validateGeneratedNumOneAndLastBoxUpper(int generatedNum, List<String> bridge) {
         int lastIdx = bridge.size() - 1;
 
-        if (generatedNum == 1) {
+        if (generatedNum == RANDOM_NUM_UPPER) {
             if (! bridge.get(lastIdx).equals("U")) {
                 throw new IllegalArgumentException("[ERROR] 랜덤값이 1일 경우, 다리에 위 칸이 추가되어야 합니다.");
             }
