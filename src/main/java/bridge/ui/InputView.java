012ec94a3121c37642dbdded2d666ce265ba4145
hanuel <ezzeezz@naver.com>
2022-11-20 17:22:15 +0900

    fix(ui): 입력 받기 전 문구 추가

--- src/main/java/bridge/ui/InputView.java
+++ src/main/java/bridge/ui/InputView.java
@@ -7,12 +7,15 @@ import camp.nextstep.edu.missionutils.Console;
  */
 public class InputView {
 
-    public final String RETRYMENT = "게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)";
+    public final String BRIDGE_SIZE_MENT = "다리의 길이를 입력해주세요";
+    public final String MOVING_MENT = "이동할 칸을 선택해주세요. (위: U, 아래: D)";
+    public final String RETRY_MENT = "게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)";
 
     /**
      * 다리의 길이를 입력받는다.
      */
     public int readBridgeSize() {
+        System.out.println(BRIDGE_SIZE_MENT);
         String s = Console.readLine();
         // validateBridgeSize(s)
         int bridgeSize = Integer.parseInt(s);
@@ -23,6 +26,7 @@ public class InputView {
      * 사용자가 이동할 칸을 입력받는다.
      */
     public String readMoving() {
+        System.out.println(MOVING_MENT);
         String s = Console.readLine();
         // validateMoving(s)
         return s;
@@ -32,7 +36,7 @@ public class InputView {
      * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
      */
     public String readGameCommand() {
-        System.out.println(RETRYMENT);
+        System.out.println(RETRY_MENT);
         String s = Console.readLine();
         // validateGameCommand(s);
         return s;
