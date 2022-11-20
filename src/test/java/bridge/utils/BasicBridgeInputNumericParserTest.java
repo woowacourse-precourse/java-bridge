c9560c565174097e2fc5aa38ba86d6b9da0157dd
miffyKing <whqudghk28@gmail.com>
2022-11-20 15:51:33 +0900

    test: Change Test code to check utility's numeric checking class.

--- src/test/java/bridge/utils/BasicBridgeInputNumericParserTest.java
+++ src/test/java/bridge/utils/BasicBridgeInputNumericParserTest.java
@@ -3,12 +3,15 @@ package bridge.utils;
 
 import bridge.Application;
 import camp.nextstep.edu.missionutils.test.NsTest;
+import java.util.List;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.assertj.core.api.Assertions.assertThat;
 
+import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomUniqueNumbersInRangeTest;
+import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
+import static org.assertj.core.api.Assertions.assertThat;
 class BasicBridgeInputNumericParserTest extends NsTest {
 //check if util numeric parser do trim and verify if numeric.
     
@@ -35,11 +38,17 @@ class BasicBridgeInputNumericParserTest extends NsTest {
 
     @DisplayName("입력된 값이 숫자가 맞는지 확인하기 - 숫자가 맞는 경우 (범위 밖)")
     @Test
-    void checkBridgeLengthException4() {
-        BasicBridgeInputNumericParser.parseBridgeLengthAmount("2");
-        assertThat(output()).contains("[ERROR]");
+    void 기능_테스트() {
+        assertRandomUniqueNumbersInRangeTest(
+            () -> {
+                run("30");
+                assertThat(output()).contains(
+                    "[ERROR]"
+                );
+            },
+            List.of()
+        );
     }
-
 
     @Override
     public void runMain() {
