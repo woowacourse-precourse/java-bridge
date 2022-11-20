cb9b9e913a89ce58ee69f4e0d5bb61bfc8b00433
cgk95 <kjkj5868@gmail.com>
2022-11-21 00:43:50 +0900

    refactor(BridgeMaker) : 기능 구현 완료

--- src/main/java/bridge/BridgeMaker.java
+++ src/main/java/bridge/BridgeMaker.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 /**
  * 다리의 길이를 입력 받아서 다리를 생성해주는 역할을 한다.
+ * Controller라고 할 수 있다.
  */
 public class BridgeMaker {
 
@@ -12,8 +13,8 @@ public class BridgeMaker {
 
     public BridgeMaker(BridgeNumberGenerator bridgeNumberGenerator) {
         this.bridgeNumberGenerator = bridgeNumberGenerator;
+        Model.bridge=makeBridge(Model.size); // 데이터베이스 역할을 하는 모델에 기록
     }
-
     /**
      * @param size 다리의 길이
      * @return 입력받은 길이에 해당하는 다리 모양. 위 칸이면 "U", 아래 칸이면 "D"로 표현해야 한다.
