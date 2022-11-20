4be249b1afd695b11f35f4626fba9ff89ac5821e
leesangjin <tkdwls0628@gmail.com>
2022-11-21 00:47:59 +0900

    feat(Constant): 상수 추가 정의

--- src/main/java/bridge/constant/Constant.java
+++ src/main/java/bridge/constant/Constant.java
@@ -2,6 +2,9 @@ package bridge.constant;
 
 public class Constant {
 
+    public static final String UP = "U";
+    public static final String DOWN = "D";
+
     public static final int BRIDGE_WIDTH = 2;
     public static final int MIN_BRIDGE_SIZE = 3;
     public static final int MAX_BRIDGE_SIZE = 20;
