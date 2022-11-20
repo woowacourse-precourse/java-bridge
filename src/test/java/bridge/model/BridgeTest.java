7d28a638098ba294b8c17cd9827ffb80186b3163
choyeram <choyeram8@gmail.com>
2022-11-20 23:44:17 +0900

    test: 입력 받은 길이만큼의 다리 생성 확인

--- src/test/java/bridge/model/BridgeTest.java
+++ src/test/java/bridge/model/BridgeTest.java
@@ -2,10 +2,8 @@ package bridge.model;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
-import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.List;
 
