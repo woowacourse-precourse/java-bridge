81032df1c5e533a35f8d4f875810ea912bb844ac
SuhHyeonjun <dmsgktn0208@gmail.com>
2022-11-20 15:58:54 +0900

    refactor : 기본 필드 외의 필드 제거

--- src/main/java/bridge/domain/BridgeMaker.java
+++ src/main/java/bridge/domain/BridgeMaker.java
@@ -11,7 +11,6 @@ import java.util.List;
  */
 public class BridgeMaker {
     private final BridgeNumberGenerator bridgeNumberGenerator;
-    private final List<String> bridges = new ArrayList<>();
 
     public BridgeMaker(BridgeNumberGenerator bridgeNumberGenerator) {
         this.bridgeNumberGenerator = bridgeNumberGenerator;
@@ -22,6 +21,7 @@ public class BridgeMaker {
      * @return 입력받은 길이에 해당하는 다리 모양. 위 칸이면 "U", 아래 칸이면 "D"로 표현해야 한다.
      */
     public List<String> makeBridge(int size) {
+        List<String> bridges = new ArrayList<>();
         String[] UpDown = {Message.Down.getMessage(), Message.UP.getMessage()};
         for (int i = 0 ; i < size ; i++){
             bridges.add(UpDown[bridgeNumberGenerator.generate()]);
