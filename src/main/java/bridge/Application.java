8a2eab2e98b39cefa387f0cb151241c3eb26c304
yeonsu <jack0810@kookmin.ac.kr>
2022-11-20 20:41:04 +0900

    feat($outputView) OutputView 의 출력문을 만들었습니다.

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -1,18 +1,60 @@
 package bridge;
 
-public class Application {
+import java.util.List;
 
+import static org.assertj.core.util.Lists.newArrayList;
+
+public class Application {
+    public static List<String> bridge;
+    public static boolean success;
+    public static int currentLocation;
+    public static int tryCount;
+
+    public static void execute(){
+        InputView inputView = new InputView();
+        int bridgeSize = inputView.readBridgeSize();
+
+        BridgeRandomNumberGenerator bridgeRandomNumberGenerator = new BridgeRandomNumberGenerator();
+        BridgeMaker bridgeMaker = new BridgeMaker(bridgeRandomNumberGenerator);
+        bridge = bridgeMaker.makeBridge(bridgeSize);
+
+        currentLocation = 0;
+        tryCount = 1;
+
+        BridgeGame bridgeGame = new BridgeGame();
+        String inputMove;
+        String correctMove;
+        String gameCommand;
+        boolean safe;
+        success = true;
+
+        OutputView outputView = new OutputView();
+        while(currentLocation < bridgeSize){
+            inputMove = inputView.readMoving();
+            correctMove = bridge.get(currentLocation);
+            safe = bridgeGame.move(inputMove, correctMove);
+            currentLocation += 1;
+            outputView.printMap();
+            if(safe){
+                continue;
+            }
+            gameCommand = inputView.readGameCommand();
+            if(gameCommand.equals("Q")){
+                success = false;
+                break;
+            }
+            success = true;
+            tryCount += 1;
+        }
+        outputView.printResult();
+    }
     public static void main(String[] args) {
         // TODO: 프로그램 구현
-//        InputView inputView = new InputView();
-//        try{
-//            inputView.readBridgeSize();
-//        }catch (IllegalArgumentException e){
-//            System.out.println("[ERROR] : " + e.getMessage());
-//        }
+        try {
+            execute();
+        }catch (IllegalArgumentException e){
+            System.out.println("[ERROR] " + e);
+        }
 
-        BridgeNumberGenerator bridgeNumberGenerator = null;
-        BridgeMaker bridgeMaker = new BridgeMaker(null);
-        System.out.println(bridgeMaker.makeBridge(3));
     }
 }
