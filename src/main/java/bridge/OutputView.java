1c34c9f14eab5cbb247a7f3703429579a79eddbf
Gibeom Gwon <gb.gwon@stackframe.dev>
2022-11-20 16:58:50 +0900

    feat: 게임 재시도 입력 요청 출력 메소드 추가

--- src/main/java/bridge/OutputView.java
+++ src/main/java/bridge/OutputView.java
@@ -17,6 +17,10 @@ public class OutputView {
         System.out.println("이동할 칸을 선택해주세요. (위: U, 아래: D).");
     }
 
+    public void printRequestInputRetry() {
+        System.out.println("게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)");
+    }
+
     public void printErrorMessage(String message) {
         System.out.println("[ERROR] " + message);
     }
