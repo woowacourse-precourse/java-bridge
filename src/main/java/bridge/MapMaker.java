bb5f45c4353e4fc66140ccddaf14e9166f094b65
Life is a ninanino <tvxqinak@naver.com>
2022-11-21 02:57:23 +0900

    refactor : 메소드 수정 및 기능 구현

--- src/main/java/bridge/MapMaker.java
+++ src/main/java/bridge/MapMaker.java
@@ -2,26 +2,55 @@ package bridge;// @ author ninaaano
 
 import bridge.view.MapView;
 
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
 public class MapMaker {
     StringBuilder upState = new StringBuilder();
     StringBuilder downState = new StringBuilder();
 
-    public void printMap(){
-        System.out.println("[ "+upState+" ]");
-        System.out.println("[ "+downState+" ]");
+    private Queue<String> bridge;
+
+    public MapMaker() {
+        this.bridge = new LinkedList<>(bridge);
     }
 
-    public void addMap(String input){
+    public List<String> printMap(){
+        return List.of("[ "+upState+" ]","[ "+downState+" ]");
+    }
+
+    public void addMapO(String input){
         if(input.equals("U")){
             upState.append(MapView.MAP_O);
-            upState.append(MapView.MAP_MIDDLE);
             upState.append(MapView.MAP_BLANK);
         }
         if(input.equals("D")){
+            downState.append(MapView.MAP_BLANK);
             downState.append(MapView.MAP_O);
-            downState.append(MapView.MAP_MIDDLE);
+        }
+    }
+
+    public void addMapX(String input){
+        if(input.equals("U")){
+            upState.append(MapView.MAP_BLANK);
+            upState.append(MapView.MAP_X);
+        }
+        if(input.equals("D")){
+            downState.append(MapView.MAP_X);
             downState.append(MapView.MAP_BLANK);
         }
+    }
+
+    public void addMap(boolean answer){
+        if(upState.length() != 0 && downState.length() != 0){
+            upState.append(MapView.MAP_MIDDLE);
+            downState.append(MapView.MAP_MIDDLE);
+        }
+        if(answer)
+            addMapO(bridge.remove());
+        if(!answer)
+            addMapX(bridge.remove());
     }
 
 }
