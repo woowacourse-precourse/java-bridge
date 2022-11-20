790be77c9db223a17d2cd1a080cd5fa79ec2d8e1
JangAJang <99702271+JangAJang@users.noreply.github.com>
2022-11-20 21:48:23 +0900

    feat: BridgelengthException 문자열에서 값을 확인 후 예외처리하고 예외가 없을 시 정수형으로 반환하는 기능 구현

--- src/main/java/bridge/exception/Exceptions.java
+++ src/main/java/bridge/exception/Exceptions.java
@@ -4,7 +4,7 @@ public class Exceptions {
 
     private static final String ERROR_COMMENT = "[ERROR] ";
 
-    private void executeException(String comment){
+    public void executeException(String comment){
         System.out.println(ERROR_COMMENT + comment);
     }
 }
