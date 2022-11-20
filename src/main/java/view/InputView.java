7dc12f195f83c45ec0e24a5ff5eb51c8382d6e2b
hodomaroo <enghemdwkd@gmail.com>
2022-11-20 23:00:43 +0900

    [Feat/InputView] InputView 클래스 구현
    사용자로부터 입력을 받아 최소한의 검증(숫자인지 여부 등)을 수행 후
    반환하는 getInteger/  getString 메서드를 구현하였으며,
    
    각각의 입력 단계에서 필요한 출력 문자열 목록을 상수로 정의하였습니다.

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -1,28 +1,49 @@
 package view;
 
+import camp.nextstep.edu.missionutils.Console;
+import model.BridgeGameExceptions;
+
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
  */
 public class InputView {
+    private final String GET_BRIDGE_SIZE_COMMENT = "다리의 길이를 입력해주세요.";
+    private final String GET_MOVE_CHOICE_COMMENT = "이동할 칸을 선택해주세요. (위: U, 아래: D)";
+    private final String GET_RETRY_INTENTION_COMMENT = "게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)";
 
     /**
      * 다리의 길이를 입력받는다.
      */
     public int readBridgeSize() {
-        return 0;
+        System.out.println(GET_BRIDGE_SIZE_COMMENT);
+        return getInteger();
     }
 
     /**
      * 사용자가 이동할 칸을 입력받는다.
      */
     public String readMoving() {
-        return null;
+        System.out.println(GET_MOVE_CHOICE_COMMENT);
+        return getString();
     }
 
     /**
      * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
      */
     public String readGameCommand() {
-        return null;
+        System.out.println(GET_RETRY_INTENTION_COMMENT);
+        return getString();
+    }
+
+    private String getString() {
+        return Console.readLine();
+    }
+
+    private int getInteger() {
+        try {
+            return Integer.parseInt(Console.readLine());
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException(BridgeGameExceptions.NOT_A_NUMBER);
+        }
     }
 }
