ce3ccb195087eb2038ce320ae4fe245b74fdeb0c
Hyun-Seo Oh <ohseo8813@gmail.com>
2022-11-20 15:56:38 +0900

    <test>(BridgeTest.java): BridgeMaker 없이 내부 메소드를 이용하도록 리팩토링

--- src/test/java/bridge/domain/BridgeTest.java
+++ src/test/java/bridge/domain/BridgeTest.java
@@ -2,19 +2,21 @@ package bridge.domain;
 
 import static bridge.domain.BridgeSize.MAX_SIZE;
 import static bridge.domain.BridgeSize.MIN_SIZE;
+import static bridge.domain.command.MoveCommand.MOVE_TO_LOWER_BLOCK;
+import static bridge.domain.command.MoveCommand.MOVE_TO_UPPER_BLOCK;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
-import bridge.BridgeMaker;
-import bridge.BridgeRandomNumberGenerator;
 import java.util.List;
+import java.util.Random;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
 class BridgeTest {
-
-    BridgeMaker bridgeMaker = new BridgeMaker(new BridgeRandomNumberGenerator());
 
     @DisplayName("Bridge를 생성할때")
     @Nested
@@ -24,7 +26,7 @@ class BridgeTest {
         @ValueSource(ints = {MIN_SIZE - 1, MAX_SIZE + 1})
         @ParameterizedTest
         void test1(int wrongSize) {
-            assertThatThrownBy(() -> new Bridge(bridgeMaker.makeBridge(wrongSize)))
+            assertThatThrownBy(() -> new Bridge(createBlocks(wrongSize)))
                     .isInstanceOf(IllegalArgumentException.class);
         }
 
@@ -32,11 +34,31 @@ class BridgeTest {
         @ValueSource(ints = {MIN_SIZE})
         @ParameterizedTest
         void test2(int rightSize) {
-            List<String> blocks = bridgeMaker.makeBridge(rightSize);
+            List<String> blocks = createBlocks(rightSize);
             blocks.add("wrongValue");
             assertThatThrownBy(() -> new Bridge(blocks))
                     .isInstanceOf(IllegalArgumentException.class);
         }
+
+        @DisplayName("값이 문제없으면 정상적으로 생성한다")
+        @ValueSource(ints = {MIN_SIZE})
+        @ParameterizedTest
+        void test3(int rightSize) {
+            Assertions.assertThatNoException()
+                    .isThrownBy(() -> new Bridge(createBlocks(rightSize)));
+        }
+
     }
 
+    private List<String> createBlocks(int size) {
+        return IntStream.range(0, size)
+                .mapToObj(idx -> getRandomBlock())
+                .collect(Collectors.toList());
+    }
+
+    private String getRandomBlock() {
+        Random random = new Random();
+        List<String> correctValues = List.of(MOVE_TO_UPPER_BLOCK, MOVE_TO_LOWER_BLOCK);
+        return correctValues.get(random.nextInt(correctValues.size()));
+    }
 }
