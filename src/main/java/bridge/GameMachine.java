0c925ca5557f9ef9a1125bf82cc5f68192dc6201
SeokyungKim <kskyung0624@gmail.com>
2022-11-20 15:32:24 +0900

    fix(BridgeGame) : 요구하는 기능 요구사항에 맞춰 로직 변경

--- src/main/java/bridge/GameMachine.java
+++ src/main/java/bridge/GameMachine.java
@@ -14,14 +14,24 @@ public class GameMachine {
     private BridgeMaker bridgeMaker = new BridgeMaker(new BridgeRandomNumberGenerator());
 
     public void run() {
-        int counter = 0;
+        view.printBridgeSizeRequest();
+        List<BridgeType> bridge = makeBridge();
+        BridgeGame bridgeGame = new BridgeGame(bridge);
+
+        int counter = 1;
         view.printStart();
         RetryCommand retry = RetryCommand.RETRY;
         while (retry == RetryCommand.RETRY) {
-            play();
-            view.printRestartRequest();
-            retry = RetryCommand.of(ui.readGameCommand());
-            counter++;
+            bridgeGame.retry();
+            MoveResult gameResult = play(bridge, bridgeGame);
+
+            if (gameResult == MoveResult.FAIL) {
+                view.printRestartRequest();
+                retry = RetryCommand.of(ui.readGameCommand());
+                counter++;
+                continue;
+            }
+            retry = RetryCommand.QUIT;
         }
         view.printResult();
         view.printGameCount(counter);
@@ -32,20 +42,17 @@ public class GameMachine {
      * todo: Exception이 아닌 IllegalArgumentException, IllegalStateException 등과 같은 명확한 유형을 처리한다.
      * todo: 실패했을 때 게임을 재시작할 수 있어야 한다.
      */
-    private void play() {
-        view.printBridgeSizeRequest();
-        List<BridgeType> bridge = makeBridge();
-        bridge.stream().forEach(a -> System.out.println(a));
-        BridgeGame bridgeGame = new BridgeGame(bridge);
+    private MoveResult play(List<BridgeType> bridge, BridgeGame bridgeGame) {
         for (int location = 0; location < bridge.size(); location++) {
             view.printMoveTypeRequest();
             BridgeType userInput = BridgeType.of(ui.readMoving());
             if (MoveResult.FAIL == bridgeGame.move(userInput)) {
                 view.printMap(bridge, location, MoveResult.FAIL);
-                return;
+                return MoveResult.FAIL;
             }
             view.printMap(bridge, location, MoveResult.PASS);
         }
+        return MoveResult.PASS;
     }
 
     private List<BridgeType> makeBridge() {
