e1da6da2f5c86517a76df5fb3cda176c9460b739
lecocococo <chemwang13@naver.com>
2022-11-20 23:32:36 +0900

    feat(Controller): 플레이어의 다리 길이 입력값을 받는 기능 추가
    - 에러 메시지를 출력 후 그 부분부터 입력을 다시 받는 기능
    - 3 이상 20 이하의 숫자가 아니면 에외처리
    - 숫자가 아니라면 예외처리

--- src/main/java/bridge/controller/Controller.java
+++ src/main/java/bridge/controller/Controller.java
@@ -1,30 +1,43 @@
 package bridge.controller;
 
+import bridge.BridgeMaker;
+import bridge.BridgeNumberGenerator;
+import bridge.BridgeRandomNumberGenerator;
+import bridge.domain.Bridge;
+import bridge.domain.BridgeGame;
 import bridge.view.InputView;
+import bridge.view.OutputView;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Controller {
     private static final String GAME_START = "다리 건너기 게임을 시작합니다.\n";
 
+    private final InputView inputView = new InputView();
+    private final OutputView outputView = new OutputView();
+
     public void run() {
-        InputView inputView = new InputView();
+        BridgeNumberGenerator bridgeNumberGenerator = new BridgeRandomNumberGenerator();
+        BridgeMaker bridgeMaker = new BridgeMaker(bridgeNumberGenerator);
+
         System.out.println(GAME_START);
-        startGame(inputView);
-        playingGame(inputView);
+
+        Bridge answerBridge = makeAnswerBridge(bridgeMaker);
     }
 
-    private void startGame(InputView inputView) {
+    private Bridge makeAnswerBridge( BridgeMaker bridgeMaker) {
+        Bridge answerBridge;
         try {
-            inputView.readBridgeSize();
+            answerBridge = setAnswerBridge(bridgeMaker);
         } catch (IllegalArgumentException ex) {
             System.out.println(ex.getMessage());
+            answerBridge = makeAnswerBridge(bridgeMaker);
         }
+        return answerBridge;
     }
 
-    private void playingGame(InputView inputView){
-        try {
-            inputView.readMoving();
-        } catch (IllegalArgumentException ex) {
-            System.out.println(ex.getMessage());
-        }
+    private Bridge setAnswerBridge(BridgeMaker bridgeMaker) {
+        int bridgeSize = inputView.readBridgeSize();
+        return new Bridge(bridgeMaker.makeBridge(bridgeSize));
     }
 }
