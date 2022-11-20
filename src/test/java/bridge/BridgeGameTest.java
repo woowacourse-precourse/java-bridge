79ce881df00b79073c16a4832b0f839745336c15
ho2n <jungho2n@gmail.com>
2022-11-21 00:13:49 +0900

    test(BridgeGameTest): 중간에 종료를 입력하여 종료했을 때 출력 형식에 맞게 출력하는지 테스트하는 코드를 추게

--- src/test/java/bridge/BridgeGameTest.java
+++ src/test/java/bridge/BridgeGameTest.java
@@ -52,6 +52,39 @@ class BridgeGameTest extends NsTest {
         }, 1, 0, 1);
     }
 
+    @Test
+    void Q라면_최종_게임_결과와_이동한_결과를_출력하고_종료하는지_테스트() {
+        assertRandomNumberInRangeTest(() -> {
+            run("3", "U", "U", "R", "U", "D", "D", "R", "U", "D", "D", "Q");
+            assertThat(output()).contains(
+                    "[ O ]",
+                    "[   ]",
+                    "[ O | X ]",
+                    "[   |   ]",
+
+                    "[ O ]",
+                    "[   ]",
+                    "[ O |   ]",
+                    "[   | O ]",
+                    "[ O |   |   ]",
+                    "[   | O | X ]",
+
+                    "[ O ]",
+                    "[   ]",
+                    "[ O |   ]",
+                    "[   | O ]",
+                    "[ O |   |   ]",
+                    "[   | O | X ]",
+
+                    "최종 게임 결과",
+                    "[ O |   |   ]",
+                    "[   | O | X ]",
+                    "게임 성공 여부: 실패",
+                    "총 시도한 횟수: 3"
+            );
+        }, 1, 0, 1);
+    }
+
     @Override
     protected void runMain() {
         Application.main(new String[]{});
