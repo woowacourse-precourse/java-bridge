4fd29df5208ea6470f666f0a9e5889268f5e34ee
LHS-11 <ghktjq1119@naver.com>
2022-11-20 15:43:02 +0900

    [Docs] : 기능 구현 목록 수정

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -1,18 +1,18 @@
 package bridge;
 
+import bridge.controller.BridgeGameController;
 import bridge.model.BridgeMaker;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 
 public class Application {
 
     public static void main(String[] args) {
         // TODO: 프로그램 구현
-        BridgeMaker bridgeMaker = new BridgeMaker(new BridgeRandomNumberGenerator());
-        List<String> bridge = bridgeMaker.makeBridge(5);
-        for (String s : bridge) {
-            System.out.println(s);
-        }
-        System.out.println(bridge.size());
+        BridgeGameController bridgeGameController = new BridgeGameController();
+        bridgeGameController.start();
+
     }
 }
--- src/main/java/bridge/BridgeGame.java
+++ src/main/java/bridge/BridgeGame.java
@@ -1,16 +1,27 @@
-package bridge.controller;
+package bridge;
 
 /**
  * 다리 건너기 게임을 관리하는 클래스
  */
 public class BridgeGame {
 
+    private int location;
+
+    private int retryCount;
+
+    public BridgeGame(){
+        this.location=-1;
+        this.retryCount=1;
+    }
+
     /**
      * 사용자가 칸을 이동할 때 사용하는 메서드
      * <p>
      * 이동을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void move() {
+    public int move() {
+        location+=1;
+        return location;
     }
 
     /**
@@ -19,5 +30,11 @@ public class BridgeGame {
      * 재시작을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
     public void retry() {
+
+    }
+
+    public int countRetry(){
+        retryCount+=1;
+        return retryCount;
     }
 }
