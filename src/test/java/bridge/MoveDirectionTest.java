8b97e1e0509eda168a4d4dd02bd389f76db1f0f5
saint6839 <saint6839@gmail.com>
2022-11-20 18:44:43 +0900

    test(domain) : 사용자가 입력한 이동 방향 값이 잘못되었을 경우 예외를 던지는 테스트 작성

---
+++ src/test/java/bridge/MoveDirectionTest.java
@@ -0,0 +1,21 @@
+package bridge;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class MoveDirectionTest {
+
+    @DisplayName("사용자가 입력한 이동 방향이 U또는 D가 아닐 경우 예외를 던지는지 테스트")
+    @Test
+    void createMoveDirectionByWrongInput() {
+        //given
+        String direction = "C";
+        //when
+        //then
+        Assertions.assertThatThrownBy(() -> {
+                      new MoveDirection(direction);
+                  })
+                  .isInstanceOf(IllegalArgumentException.class);
+    }
+}
