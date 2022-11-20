5511c784673aa7ca6dd621bac4a533c1fc652357
H4R1B0 <qkrguswns25@naver.com>
2022-11-20 20:42:15 +0900

    refactor: change criteria for if statements

--- src/main/java/bridge/Calculator.java
+++ src/main/java/bridge/Calculator.java
@@ -2,11 +2,10 @@ package bridge;
 
 public class Calculator {
     public static Bridge moveCalculate(String current, String moving) {
-        if (!current.equals(moving)) {
-            if (current.equals("U"))
-                return new Bridge(" ", "X");
+        if (!current.equals(moving) && current.equals("U"))
+            return new Bridge(" ", "X");
+        if (!current.equals(moving) && current.equals("D"))
             return new Bridge("X", " ");
-        }
         if (current.equals("U"))
             return new Bridge("O", " ");
         return new Bridge(" ", "O");
