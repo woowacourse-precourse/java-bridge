b637901384a17128a652b157ffd0ff212d15fd5d
JunTaeINC <juntea3523@naver.com>
2022-11-20 22:00:19 +0900

    refactor : GameController , BridgeGame 로직 분리

--- src/main/java/bridge/BridgeGame.java
+++ src/main/java/bridge/BridgeGame.java
@@ -1,66 +1,17 @@
 package bridge;
 
-import java.util.List;
-
-import static bridge.enums.Sentence.*;
-
 public class BridgeGame {
-
-    private final Move move = new Move();
-    private final ReEnter reEnter = new ReEnter();
-
-    private boolean isTrue = true;
-    private boolean isClear = true;
-    private int TRY_COUNT = 0;
-
-    public static List<String> gameSet(int userInput) {
-        BridgeNumberGenerator bridgeNumberGenerator = new BridgeRandomNumberGenerator();
-        BridgeMaker bridgeMaker = new BridgeMaker(bridgeNumberGenerator);
-        return bridgeMaker.makeBridge(userInput);
-    }
-
-    public void gamingSet(List<String> bridge) {
-        TRY_COUNT++;
-        for (int i = 0; i < bridge.size(); i++) {
-            String userMoveInput = reEnter.reGetMoveString();
-            move.moving(bridge.get(i), userMoveInput);
-            move.printMoving();
-            if (move.checkWrong()) {
-                return;
-            }
-        }
-        isClear = false;
-    }
-
-    public void playTheGaming() {
-        List<String> bridge = gameSet(reEnter.reGetBridgeLength());
-        while (isTrue) {
-            gamingSet(bridge);
-            if (!isClear) {
-                printMovingAndResult();
-                break;
-            }
-            retry();
-        }
-    }
 
     public static boolean move(String computer, String userInput) {
         return computer.equals(userInput);
     }
 
-    public boolean retry() {
-        if (reEnter.reGetExitOrRestart().equals("R")) {
-            move.clearMoving();
-            return isTrue = true;
+    public static boolean retry() {
+        if (ReEnter.reGetExitOrRestart().equals("R")) {
+            Moving.clearMoving();
+            return true;
         }
-        printMovingAndResult();
-        return isTrue = false;
+        GameController.printMovingAndResult();
+        return false;
     }
-
-    public void printMovingAndResult() {
-        System.out.println(RESULT.getValue());
-        move.printMoving();
-        outputView.printResult(returnSuccessOrFailure(!isClear), TRY_COUNT);
-    }
-
 }
---
+++ src/main/java/bridge/GameController.java
@@ -0,0 +1,53 @@
+package bridge;
+
+import bridge.views.OutputView;
+
+import java.util.List;
+
+import static bridge.enums.Sentence.*;
+
+public class GameController {
+
+    private final Moving moving = new Moving();
+    private final ReEnter reEnter = new ReEnter();
+    private boolean isTrue = true;
+    private static boolean isClear = true;
+    private static int TRY_COUNT = 0;
+
+    public static List<String> gameSet(int userInput) {
+        BridgeNumberGenerator bridgeNumberGenerator = new BridgeRandomNumberGenerator();
+        BridgeMaker bridgeMaker = new BridgeMaker(bridgeNumberGenerator);
+        return bridgeMaker.makeBridge(userInput);
+    }
+
+    public void gamingSet(List<String> bridge) {
+        TRY_COUNT++;
+        for (String s : bridge) {
+            String userMoveInput = reEnter.reGetMoveString();
+            moving.moving(s, userMoveInput);
+            moving.printMoving();
+            if (moving.checkWrong()) {
+                return;
+            }
+        }
+        isClear = false;
+    }
+
+    public void playTheGaming() {
+        List<String> bridge = gameSet(reEnter.reGetBridgeLength());
+        while (isTrue) {
+            gamingSet(bridge);
+            if (!isClear) {
+                printMovingAndResult();
+                break;
+            }
+            isTrue = BridgeGame.retry();
+        }
+    }
+
+    public static void printMovingAndResult() {
+        System.out.println(RESULT.getValue());
+        Moving.printMoving();
+        OutputView.printResult(returnSuccessOrFailure(!isClear), TRY_COUNT);
+    }
+}
