b958db85422ca175f28e90dc2c73457453b0db74
desfox <desfox99@naver.com>
2022-11-20 16:19:10 +0900

    feat(ReadBridgeSizeValidation): 다리 길이가 3이상 20이하인지 검증하는 로직 구현

--- src/main/java/bridge/validation/ReadBridgeSizeValidation.java
+++ src/main/java/bridge/validation/ReadBridgeSizeValidation.java
@@ -8,4 +8,13 @@ public class ReadBridgeSizeValidation {
             throw new IllegalArgumentException("[ERROR] 다리의 길이를 숫자로 입력해주세요.");
         }
     }
+
+    public void validateSizeRange(String inputBridgeSize) {
+        int bridgeSize = Integer.parseInt(inputBridgeSize);
+
+        if (bridgeSize < 3 || bridgeSize > 20) {
+            throw new IllegalArgumentException("[ERROR] 다리의 길이는 3이상, 20이하여야 합니다.");
+        }
+    }
+
 }
