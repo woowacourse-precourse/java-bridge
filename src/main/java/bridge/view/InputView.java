96c1ebc1c8e4d54b997c26cb405d10f9e53f5372
Miseong Kim <miseongk@naver.com>
2022-11-21 01:31:28 +0900

    feat(InputView): input bridge size, moving and command

--- src/main/java/bridge/view/InputView.java
+++ src/main/java/bridge/view/InputView.java
@@ -1,28 +1,50 @@
 package bridge.view;
 
+import bridge.Validation;
+import camp.nextstep.edu.missionutils.Console;
+
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
  */
 public class InputView {
 
+    private final Validation validation;
+
+    public InputView() {
+        validation = new Validation();
+    }
+
     /**
      * 다리의 길이를 입력받는다.
      */
     public int readBridgeSize() {
-        return 0;
+        String number = Console.readLine();
+        int size = 0;
+
+        if (validation.isInteger(number)) {
+            size = Integer.parseInt(number);
+        }
+
+        return size;
     }
 
     /**
      * 사용자가 이동할 칸을 입력받는다.
      */
     public String readMoving() {
-        return null;
+        String moving = Console.readLine();
+        validation.validateInputPosition(moving);
+
+        return moving;
     }
 
     /**
      * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
      */
     public String readGameCommand() {
-        return null;
+        String retryCommand = Console.readLine();
+        validation.validateInputRetry(retryCommand);
+
+        return retryCommand;
     }
 }
