8ff8e2130331b96e605f7ef2490bd3a641593aea
aak2075 <35948985+aak2075@users.noreply.github.com>
2022-11-20 20:49:11 +0900

    feat(bridge) : 다리의 길이를 입력받는 기능 추가

--- src/main/java/bridge/view/InputView.java
+++ src/main/java/bridge/view/InputView.java
@@ -1,5 +1,7 @@
 package bridge.view;
 
+import camp.nextstep.edu.missionutils.Console;
+
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
  */
@@ -9,6 +11,7 @@ public class InputView {
      * 다리의 길이를 입력받는다.
      */
     public int readBridgeSize() {
+        Console.readLine();
         return 0;
     }
 
@@ -16,7 +19,7 @@ public class InputView {
      * 사용자가 이동할 칸을 입력받는다.
      */
     public String readMoving() {
-        return null;
+        return Console.readLine();
     }
 
     /**
