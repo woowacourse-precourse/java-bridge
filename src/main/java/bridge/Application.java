5871dd6f442721e5091f9aec5360fa6daafaf4e4
heesang <dlagmltkd12@naver.com>
2022-11-21 06:32:06 +0900

    fix: Apllication 예외를 발생 시키고 종료하는 버그 수정

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -7,10 +7,6 @@ public class Application {
 
     public static void main(String[] args) {
         GameController gameController = new GameController(new BridgeGame());
-        try {
-            gameController.run();
-        } catch (IllegalArgumentException e) {
-            System.out.println(e.getMessage());
-        }
+        gameController.run();
     }
 }
