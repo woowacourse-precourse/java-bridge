de2fa61f48c6bf13bcbd31c99e8d5d8dc3a7cfdb
SuhHyeonjun <dmsgktn0208@gmail.com>
2022-11-20 13:03:49 +0900

    refactor : 클래스 변수 지역변수로 변경

--- src/main/java/bridge/view/InputView.java
+++ src/main/java/bridge/view/InputView.java
@@ -1,38 +1,26 @@
 package bridge.view;
 
-import static bridge.Validator.validateInputDIRECTION;
-import static bridge.Validator.validateInputDirectionLowerCase;
-import static bridge.Validator.validateInputDirectionNull;
-import static bridge.Validator.validateInputGameRestart;
-import static bridge.Validator.validateInputGameRestartLowerCase;
-import static bridge.Validator.validateInputGameRestartNull;
-import static bridge.Validator.validateInputSize;
-import static bridge.Validator.validateInputSizeNull;
-import static bridge.Validator.validateInputSizeType;
+import static bridge.view.Validator.*;
 
 import static camp.nextstep.edu.missionutils.Console.readLine;
 
-import bridge.Message;
+import bridge.messages.Message;
 
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
  */
 public class InputView {
-    private static int bridgeSize;
-    private static String inputSize;
-    private static String inputDirection;
-    private static String inputRestart;
 
     /**
      * 다리의 길이를 입력받는다.
      */
     public int inputBridgeSize() {
         System.out.println(Message.INPUT_SIZE.getMessage());
-        inputSize = readLine();
+        String inputSize = readLine();
         System.out.println();
         validateInputSizeNull(inputSize);
         validateInputSizeType(inputSize);
-        bridgeSize = Integer.parseInt(inputSize);
+        int bridgeSize = Integer.parseInt(inputSize);
         validateInputSize(bridgeSize);
         return bridgeSize;
     }
@@ -42,9 +30,9 @@ public class InputView {
      */
     public String inputMovingDirection() {
         System.out.println(Message.SELECT_DIRECTION.getMessage());
-        inputDirection = readLine();
+        String inputDirection = readLine();
         validateInputDirectionNull(inputDirection);
-        validateInputDIRECTION(inputDirection);
+        validateInputDirection(inputDirection);
         validateInputDirectionLowerCase(inputDirection);
         return inputDirection;
     }
@@ -54,7 +42,7 @@ public class InputView {
      */
     public String inputGameRestart() {
         System.out.println(Message.ASK_RESTART.getMessage());
-        inputRestart = readLine();
+        String inputRestart = readLine();
         validateInputGameRestartNull(inputRestart);
         validateInputGameRestart(inputRestart);
         validateInputGameRestartLowerCase(inputRestart);
