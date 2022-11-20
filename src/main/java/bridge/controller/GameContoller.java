de10cd2b21e3530a4302b702e6805ad7921ed3e5
leeyongjun <leeyongjun0310@naver.com>
2022-11-21 07:19:50 +0900

    feat(bridge): '다리 건너기 게임을 시작합니다.'를 출력하는 기능 구현

--- src/main/java/bridge/controller/GameContoller.java
+++ src/main/java/bridge/controller/GameContoller.java
@@ -2,12 +2,16 @@ package bridge.controller;
 
 
 import bridge.view.InputView;
+import bridge.view.OutputView;
 
 public class GameContoller {
+    OutputView outputView = new OutputView();
+
     public void run() {
 //        System.out.println(InputController.readBridgeSize());
-        InputView inputView = new InputView();
+//        InputView inputView = new InputView();
 //        System.out.println(InputController.readMoving());
-        System.out.println(InputController.readGameCommand());
+//        System.out.println(InputController.readGameCommand());
+        outputView.printStart();
     }
 }
