eaaebfa01e90112aa17428581665e511a180f9b7
hodomaroo <enghemdwkd@gmail.com>
2022-11-20 17:42:34 +0900

    [Feat/Bridge] Bridge 클래스의 필드 및 메서드 선언
    Bridge 클래스의 필드 선언 및, 인덱스와 이동 방향을 입력받아 이동 가능
    여부를 반환하는 moveable 메서드를 선언하였습니다.

--- src/main/java/model/Bridge.java
+++ src/main/java/model/Bridge.java
@@ -3,10 +3,15 @@ package model;
 import java.util.List;
 
 public class Bridge {
-    List<MoveChoice> moveChoices;
 
+    List<String> moveChoices;
+    public static final int LOWER_BOUND = 3;
+    public static final int UPPER_BOUND = 20;
 
-    public boolean movable(int position, MoveChoice moveChoice){
-        return false;
-    };
+    public Bridge(List<String> moveChoices) {
+        this.moveChoices = moveChoices;
+    }
+
+    public boolean movable(int position, String moveChoice) {
+    }
 }
