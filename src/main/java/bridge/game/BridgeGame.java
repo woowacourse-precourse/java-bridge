622c91ce8452cf1ccceee3bf7bfee91157aead28
JaehongDev <println@kakao.com>
2022-11-20 21:17:37 +0900

    feature (BridgeGame) : 다리 건너기 게임에서 이동하는 기능과 건널 수 있는 상태 여부를 출력하는 코드 구현

--- src/main/java/bridge/game/BridgeGame.java
+++ src/main/java/bridge/game/BridgeGame.java
@@ -1,16 +1,33 @@
 package bridge.game;
 
+import bridge.domain.bridge.Bridge;
+import bridge.domain.history.History;
+import bridge.domain.position.BridgePosition;
+import bridge.domain.position.SurviveStatus;
+
 /**
  * 다리 건너기 게임을 관리하는 클래스
  */
 public class BridgeGame {
+
+    private final Bridge bridge;
+    private final History history;
+    private SurviveStatus surviveStatus = SurviveStatus.SURVIVE;
+
+    public BridgeGame(Bridge madeBridge) {
+        this.bridge = madeBridge;
+        this.history = new History();
+    }
 
     /**
      * 사용자가 칸을 이동할 때 사용하는 메서드
      * <p>
      * 이동을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void move() {
+    public History move(BridgePosition movePosition) {
+        this.surviveStatus = bridge.next(movePosition);
+        history.record(surviveStatus, movePosition);
+        return this.history;
     }
 
     /**
@@ -19,5 +36,13 @@ public class BridgeGame {
      * 재시작을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
     public void retry() {
+    }
+
+    public boolean canPlayerMove() {
+        return surviveStatus.isAlive() && bridge.canMove();
+    }
+
+    public boolean isClear() {
+        return surviveStatus.isAlive() && bridge.canNotMove();
     }
 }
