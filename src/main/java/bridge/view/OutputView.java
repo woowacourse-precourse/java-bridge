850781c964079dbe4074249e44c0583957d13046
Yunsik-Choi <choiys0311@gmail.com>
2022-11-21 01:02:43 +0900

    ♻️ refactor : OutputFormat 로직 OutputView로 이동 후 OutputFormat 클래스 제거

--- src/main/java/bridge/view/OutputView.java
+++ src/main/java/bridge/view/OutputView.java
@@ -1,7 +1,12 @@
 package bridge.view;
 
+import bridge.domain.Bridge;
 import bridge.domain.BridgeGameResult;
+import bridge.domain.BridgeShape;
 import bridge.domain.TryCount;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 /**
  * 사용자에게 게임 진행 상황과 결과를 출력하는 역할을 한다.
@@ -12,14 +17,13 @@ public class OutputView {
     private static final String FAIL = "실패";
     private static final String TRY_COUNT_GUIDE = "총 시도한 횟수: ";
     private static final String PRINT_RESULT_GUIDE = "최종 게임 결과";
+    private static final String MOVE_SUCCESS = " O ";
+    private static final String MOVE_FAIL = " X ";
+    private static final String MOVE_NOT_SELECT = "   ";
+    private static final String BRIDGE_DELIMITER = "|";
+    private static final String BRIDGE_END = "]";
+    private static final String BRIDGE_START = "[";
     private static final String NEW_LINE = "\n";
-
-    private final OutputFormat outputFormat;
-
-    public OutputView() {
-        this.outputFormat = new OutputFormat();
-    }
-
 
     /**
      * 현재까지 이동한 다리의 상태를 정해진 형식에 맞춰 출력한다.
@@ -27,7 +31,7 @@ public class OutputView {
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
     public void printMap(BridgeGameResult result) {
-        System.out.println(outputFormat.printMapFormat(result));
+        System.out.println(printMapFormat(result));
     }
 
     /**
@@ -37,17 +41,56 @@ public class OutputView {
      */
     public void printResult(BridgeGameResult bridgeGameResult, TryCount tryCount) {
         StringBuilder result = new StringBuilder();
-        result.append(PRINT_RESULT_GUIDE);
-        result.append(outputFormat.printMapFormat(bridgeGameResult));
-        result.append(NEW_LINE);
-        result.append(printGameSuccessFormat(bridgeGameResult));
-        result.append(NEW_LINE);
+        result.append(PRINT_RESULT_GUIDE).append(NEW_LINE);
+        result.append(printMapFormat(bridgeGameResult)).append(NEW_LINE);
+        result.append(printGameSuccessFormat(bridgeGameResult)).append(NEW_LINE);
         result.append(printTryCountFormat(tryCount));
         System.out.println(result);
     }
 
-    public String printTryCountFormat(TryCount tryCount) {
-        return TRY_COUNT_GUIDE + tryCount.count();
+    public String printMapFormat(BridgeGameResult result) {
+        Bridge bridge = result.bridge();
+        List<BridgeShape> bridgeShapes = bridge.bridgeShapes();
+        List<Boolean> booleans = result.attemptsResult();
+        return String.join(NEW_LINE, upSideFormat(bridgeShapes, booleans), downSideFormat(bridgeShapes, booleans));
+    }
+
+    private String upSideFormat(List<BridgeShape> bridgeShapes, List<Boolean> booleans) {
+        List<String> upSidesTexts = IntStream.range(0, booleans.size())
+                .mapToObj(index -> moveUpSideFormat(booleans.get(index), bridgeShapes.get(index)))
+                .collect(Collectors.toList());
+        return bridgeFormat(upSidesTexts);
+    }
+
+    private String moveUpSideFormat(boolean move, BridgeShape bridgeShape) {
+        if (move && bridgeShape == BridgeShape.UP) {
+            return MOVE_SUCCESS;
+        }
+        if (!move && bridgeShape == BridgeShape.DOWN) {
+            return MOVE_FAIL;
+        }
+        return MOVE_NOT_SELECT;
+    }
+
+    private String downSideFormat(List<BridgeShape> bridgeShapes, List<Boolean> booleans) {
+        List<String> downSideTexts = IntStream.range(0, booleans.size())
+                .mapToObj(index -> moveDownSideFormat(booleans.get(index), bridgeShapes.get(index)))
+                .collect(Collectors.toList());
+        return bridgeFormat(downSideTexts);
+    }
+
+    private String moveDownSideFormat(boolean move, BridgeShape bridgeShape) {
+        if (move && bridgeShape == BridgeShape.DOWN) {
+            return MOVE_SUCCESS;
+        }
+        if (!move && bridgeShape == BridgeShape.UP) {
+            return MOVE_FAIL;
+        }
+        return MOVE_NOT_SELECT;
+    }
+
+    private String bridgeFormat(List<String> moveSideTexts) {
+        return BRIDGE_START + String.join(BRIDGE_DELIMITER, moveSideTexts) + BRIDGE_END;
     }
 
     public String printGameSuccessFormat(BridgeGameResult bridgeGameResult) {
@@ -55,5 +98,9 @@ public class OutputView {
             return GAME_SUCCESS_GUIDE + SUCCESS;
         }
         return GAME_SUCCESS_GUIDE + FAIL;
+    }
+
+    public String printTryCountFormat(TryCount tryCount) {
+        return TRY_COUNT_GUIDE + tryCount.count();
     }
 }
--- src/test/java/bridge/view/OutputViewTest.java
+++ src/test/java/bridge/view/OutputViewTest.java
@@ -46,4 +46,48 @@ class OutputViewTest {
     void printGameSuccessFormat(BridgeGameResult bridgeGameResult, String expected) {
         assertThat(outputView.printGameSuccessFormat(bridgeGameResult)).isEqualTo(expected);
     }
+
+    @MethodSource
+    private static Stream<Arguments> provideForPrintMapFormat() {
+        return Stream.of(
+                Arguments.of(
+                        Bridge.createByBridgeShapeValue(List.of("D", "U", "D")),
+                        List.of(true, true, true),
+                        List.of("[   | O |   ]\n[ O |   | O ]")
+                ),
+                Arguments.of(
+                        Bridge.createByBridgeShapeValue(List.of("D", "U", "D")),
+                        List.of(true, true, false),
+                        List.of("[   | O | X ]\n[ O |   |   ]")
+                ),
+                Arguments.of(
+                        Bridge.createByBridgeShapeValue(List.of("U", "U", "U")),
+                        List.of(true, true, true),
+                        List.of("[ O | O | O ]\n[   |   |   ]")
+                ),
+                Arguments.of(
+                        Bridge.createByBridgeShapeValue(List.of("U", "U", "U")),
+                        List.of(false, false, false),
+                        List.of("[   |   |   ]\n[ X | X | X ]")
+                ),
+                Arguments.of(
+                        Bridge.createByBridgeShapeValue(List.of("D", "D", "D")),
+                        List.of(true, true, true),
+                        List.of("[   |   |   ]\n[ O | O | O ]")
+                ),
+                Arguments.of(
+                        Bridge.createByBridgeShapeValue(List.of("D", "D", "D")),
+                        List.of(false, false, false),
+                        List.of("[ X | X | X ]\n[   |   |   ]")
+                )
+        );
+    }
+
+    @ParameterizedTest(name = "다리 건너기 결과로 변환한다.")
+    @MethodSource("provideForPrintMapFormat")
+    void printMapFormat(Bridge bridge, List<Boolean> attempts, List<String> outputs) {
+        BridgeGameResult bridgeGameResult = new BridgeGameResult(bridge, attempts);
+
+        assertThat(outputView.printMapFormat(bridgeGameResult)).containsSequence(outputs);
+    }
 }
