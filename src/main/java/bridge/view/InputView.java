b25beb3ee1d3f7dcfd05ae56bbb6a6283a68bdbf
Lee Min Woo <lmw7414@naver.com>
2022-11-20 22:59:37 +0900

    style : 클래스 및 메서드 설명 주석 제거
    구현 완료된 기능 및 클래스에 대해 불필요한 주석을 제거하였다.

--- src/main/java/bridge/view/InputView.java
+++ src/main/java/bridge/view/InputView.java
@@ -5,14 +5,8 @@ import camp.nextstep.edu.missionutils.Console;
 
 import static bridge.constant.Constants.*;
 
-/**
- * 사용자로부터 입력을 받는 역할을 한다.
- */
 public class InputView {
 
-    /**
-     * 다리의 길이를 입력받는다.
-     */
     public int readBridgeSize() {
         try {
             int inputToInt = getUserInputToInt();
@@ -24,9 +18,6 @@ public class InputView {
         }
     }
 
-    /**
-     * 사용자가 이동할 칸을 입력받는다.
-     */
     public String readMoving() {
         try {
             OutputView.printGameMessage(SELECT_UP_OR_DOWN_MESSAGE);
@@ -39,9 +30,6 @@ public class InputView {
         }
     }
 
-    /**
-     * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
-     */
     public String readGameCommand() {
         try {
             OutputView.printGameMessage(SELECT_RESTART_OR_QUIT_MESSAGE);
