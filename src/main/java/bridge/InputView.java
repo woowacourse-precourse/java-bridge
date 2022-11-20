caa09801e1fd7ed0c280b417f8b035159f94bb9b
hojunking96 <junsong96@naver.com>
2022-11-20 16:52:14 +0900

    feats(InputView.java,OutputView.java) : 정적 메소드로 변환

--- src/main/java/bridge/InputView.java
+++ src/main/java/bridge/InputView.java
@@ -13,7 +13,7 @@ public class InputView {
     /**
      * 다리의 길이를 입력받는다.
      */
-    public int readBridgeSize() throws IllegalArgumentException {
+    public static int readBridgeSize() throws IllegalArgumentException {
         String bridgeSizeInput = Console.readLine();
         bridgeSizeNumberCheck(bridgeSizeInput);
         int bridgeSize = Integer.parseInt(bridgeSizeInput);
@@ -24,17 +24,17 @@ public class InputView {
     /**
      * 사용자가 이동할 칸을 입력받는다.
      */
-    public String readMoving() throws IllegalArgumentException {
+    public static String readMoving() throws IllegalArgumentException {
         String movingInput = Console.readLine();
         movingInputErrorCheck(movingInput);
-        BridgeGame.movingInputs.add(movingInput);
+        Application.movingInputs.add(movingInput);
         return movingInput;
     }
 
     /**
      * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
      */
-    public String readGameCommand() {
+    public static String readGameCommand() {
         /**
          "게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)"
          */
