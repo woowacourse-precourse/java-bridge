6221e29b5a861d2ef551d5ffd3f02774803c6b26
UngHeung <t01047377477@gmail.com>
2022-11-21 06:27:53 +0900

    feat : (OutputView) 게임 결과 출력 메소드 추가

--- src/main/java/service/OutputView.java
+++ src/main/java/service/OutputView.java
@@ -1,11 +1,21 @@
 package service;
 
+import domain.GameMessage;
+
 public class OutputView {
 	public void printMap(String map) {
 		System.out.println(map + "\n");
 	}
 
-	public void printResult() {
+	public void printResult(String map, String result, int count) {
+		StringBuilder resultBuilder = new StringBuilder();
+
+		resultBuilder.append(GameMessage.RESULT.getGameMessage());
+		resultBuilder.append("\n" + map + "\n");
+		resultBuilder.append("\n게임 성공 여부: " + result);
+		resultBuilder.append("\n총 시도한 횟수: " + count);
+
+		System.out.println(resultBuilder.toString());
 	}
 
 }
