df0ed785924a882167701972e1a389540f836e11
ensk26 <tjensk26@gmail.com>
2022-11-21 02:30:22 +0900

    fix(OutputType): 출력할때 나오는 메시지를 수정함
    기존 enum 수정 to OutputType
    - 기존 출력 형식을 쪼개서 배열크기에 상관없이 적용할 수 있도록 함

--- src/main/java/bridge/OutputType.java
+++ src/main/java/bridge/OutputType.java
@@ -1,12 +1,12 @@
 package bridge;
 
 public enum OutputType {
-    PRINT_GAME_START("다리 건너기 게임을 시작합니다."),
-    PRINT_MAP("최종 게임 결과\n" +
-            "[ %s | %s | %s ]\n" +
-            "[ %s | %s | %s ]"),
-    PRINT_RESULT("게임 성공 여부: %s\n" +
-            "총 시도한 횟수: %d");
+    PRINT_GAME_START("다리 건너기 게임을 시작합니다.\n"),
+    PRINT_MAP_TITLE("최종 게임 결과"),
+    PRINT_MAP_FRONT("[ %s "),
+    PRINT_MAP_MID("| %s "),
+    PRINT_MAP_BACK("]\n"),
+    PRINT_RESULT("게임 성공 여부: %s\n" + "총 시도한 횟수: %d");
 
     private final String message;
 
