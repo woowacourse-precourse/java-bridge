810540a508c287b2fea44aff849fb6141b62b16e
EUNSH1 <tkdgus968@naver.com>
2022-11-21 07:32:24 +0900

    test : 사용자 다리 사이즈 입력 예외사항 테스트 코드

--- src/test/java/bridge/InputViewTest.java
+++ src/test/java/bridge/InputViewTest.java
@@ -3,6 +3,7 @@ package bridge;
 import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeTest;
 import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.assertj.core.util.Lists.newArrayList;
 
 import camp.nextstep.edu.missionutils.test.NsTest;
@@ -12,6 +13,7 @@ import java.util.List;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
 
@@ -25,5 +27,15 @@ class InputViewTest {
         InputStream in = new ByteArrayInputStream("3".getBytes());
         System.setIn(in);
         Assertions.assertThat(inputView.readBridgeSize()).isEqualTo(3);
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"-1","21","4j"})
+    @DisplayName("올바르지 않은 입력값")
+    void 사용자_입력_오류(String inputSize) {
+        ByteArrayInputStream in = new ByteArrayInputStream(inputSize.getBytes());
+        System.setIn(in);
+        assertThatThrownBy(() -> inputView.readBridgeSize())
+                .isInstanceOf(IllegalArgumentException.class);
     }
 }
