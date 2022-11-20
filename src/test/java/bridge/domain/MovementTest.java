cae4ff0a8d104ef017746b8810e3a760d2490dd2
EUNJEONGMUN <munej26@gmail.com>
2022-11-21 01:39:42 +0900

    test: 움직임 저장 테스트 코드

--- src/test/java/bridge/domain/MovementTest.java
+++ src/test/java/bridge/domain/MovementTest.java
@@ -1,46 +1,18 @@
 package bridge.domain;
 
-import bridge.constant.Constant;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.CsvSource;
-
-import java.util.List;
 
 class MovementTest {
 
-    @DisplayName("canMove 메서드는 움직일 수 있다면 true를, 없다면 false를 반환한다.")
-    @ParameterizedTest
-    @CsvSource(value = {"U:true", "D:false"}, delimiter = ':')
-    void canMoveTest(String input, boolean expected) {
-        Bridge bridge = new Bridge(List.of("U"));
-        Movement movement = new Movement(bridge);
+    @DisplayName("saveMoving메서드는 움직임을 저장하고 moveCount를 1증가시킨다.")
+    @Test
+    void saveMoving() {
+        Movement movement = new Movement();
+        String input = "U";
         movement.saveMoving(new Moving(input));
-        Assertions.assertThat(movement.canMove()).isEqualTo(expected);
-    }
-
-    @DisplayName("사용자가 모두 정답을 입력하면 게임은 성공으로 종료된다.")
-    @Test
-    void isSuccessFinishTest() {
-        Bridge bridge = new Bridge(List.of("U", "D", "D"));
-        Movement movement = new Movement(bridge);
-        movement.saveMoving(new Moving("U"));
-        movement.saveMoving(new Moving("D"));
-        movement.saveMoving(new Moving("D"));
-        Assertions.assertThat(movement.isSuccess()).isEqualTo(Constant.SUCCESS);
-    }
-
-
-    @DisplayName("사용자가 끝까지 정답을 입력했지만, 마지막 움직임이 실패하면 실패한다.")
-    @Test
-    void isFailureFinishTest() {
-        Bridge bridge = new Bridge(List.of("U", "D", "D"));
-        Movement movement = new Movement(bridge);
-        movement.saveMoving(new Moving("U"));
-        movement.saveMoving(new Moving("D"));
-        movement.saveMoving(new Moving("U"));
-        Assertions.assertThat(movement.isSuccess()).isEqualTo(Constant.FAILURE);
+        Assertions.assertThat(movement.getMoveCount()).isEqualTo(1);
+        Assertions.assertThat(movement.getMoving()).isEqualTo(input);
     }
 }
