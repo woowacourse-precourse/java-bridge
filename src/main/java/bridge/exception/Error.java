8c6385e15e99a8342f72aaf42478e40ef80e3b4f
im-shung <seoyoung1113@gmail.com>
2022-11-20 16:15:57 +0900

    feat: :sparkles: 숫자를 입력하지 않았을 경우의 에러 메세지를 가진 NOT_INT 필드 추가

--- src/main/java/bridge/exception/Error.java
+++ src/main/java/bridge/exception/Error.java
@@ -1,6 +1,17 @@
 package bridge.exception;
 
 public enum Error {
+    NOT_INT("숫자를 입력하세요.");
 
+    private final String ERROR = "[ERROR]";
+    private final String message;
+
+    Error(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return ERROR + message;
+    }
 
 }
