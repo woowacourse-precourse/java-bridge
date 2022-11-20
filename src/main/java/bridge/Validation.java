d93386f4a230c256f2d86bf2a74fa9eff073d3f7
impati <impati0716@icloud.com>
2022-11-20 16:40:02 +0900

    refactor(Validation) : 메세지에 개행문자 추가

--- src/main/java/bridge/Validation.java
+++ src/main/java/bridge/Validation.java
@@ -1,9 +1,9 @@
 package bridge;
 
 public class Validation {
-    private final String BRIDGE_MESSAGE = "[ERROR] 다리 길이 입력은 3이상 20이하의 숫자를 입력할 수 있습니다.";
-    private final String MOVE_MESSAGE = "[ERROR] 이동할 칸은 (위: U, 아래: D)만 입력해주세요.";
-    private final String RETRY_MESSAGE = "[ERROR] 다시 시도 여부는 (재시도: R, 종료: Q)만 입력해주세요.";
+    private final String BRIDGE_MESSAGE = "[ERROR] 다리 길이 입력은 3이상 20이하의 숫자를 입력할 수 있습니다.\n";
+    private final String MOVE_MESSAGE = "[ERROR] 이동할 칸은 (위: U, 아래: D)만 입력해주세요.\n";
+    private final String RETRY_MESSAGE = "[ERROR] 다시 시도 여부는 (재시도: R, 종료: Q)만 입력해주세요.\n";
     public void bridgeLen(String input){
         notNumberFormat(input);
         Integer len = Integer.parseInt(input);
