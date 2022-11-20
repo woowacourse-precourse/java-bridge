753027f6d1f93050c5151dc9518e5887e332803f
areyouhun <areyouhun@gmail.com>
2022-11-20 22:16:23 +0900

    test(input): 이동할 칸 입력값에 대한 검증 테스트

--- src/test/java/InputExceptionTest.java
+++ src/test/java/InputExceptionTest.java
@@ -18,19 +18,19 @@ public class InputExceptionTest {
                 .hasMessageContaining("[ERROR] 입력된 값이 없습니다.");
     }
 
-    @DisplayName("다리 길이 입력 시 숫자 형태가 아닌 값을 입력하면 예외가 발생한다.")
+    @DisplayName("다리 길이 입력 시 숫자 형태가 아닌 값이 들어가 있으면 예외가 발생한다.")
     @ParameterizedTest
     @ValueSource(strings = {"one", "둘", "8teen", "7곱"})
     void nonDigitInputException(String input) {
         assertThatThrownBy(() -> InputValidator.bridgeSize(input))
                 .isInstanceOf(IllegalArgumentException.class)
-                .hasMessageContaining("[ERROR] 입력값은 숫자 형태만 받을 수 있습니다.");
+                .hasMessageContaining("[ERROR] 다리 길이는 오직 숫자 형태만 받을 수 있습니다.");
     }
 
     @DisplayName("다리 길이 입력 시 3 ~ 20 사이의 숫자가 아닌 값을 입력하면 예외가 발생한다.")
     @ParameterizedTest
     @ValueSource(strings = {"1", "0", "21", "2"})
-    void nonDigitInput(String input) {
+    void InputOutOfRangeException(String input) {
         assertThatThrownBy(() -> InputValidator.bridgeSize(input))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining(String.format(
@@ -38,5 +38,31 @@ public class InputExceptionTest {
                         MINIMUM_BRIDGE_SIZE,
                         MAXIMUM_BRIDGE_SIZE
                 ));
+    }
+
+    @DisplayName("이동할 칸 입력 시 아무 것도 입력하지 않으면 예외가 발생한다.")
+    @Test
+    void noInputException2() {
+        assertThatThrownBy(() -> InputValidator.moving(""))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("[ERROR] 입력된 값이 없습니다.");
+    }
+
+    @DisplayName("이동할 칸 입력 시 알파벳 형태가 아닌 값이 들어가 있으면 예외가 발생한다.")
+    @ParameterizedTest
+    @ValueSource(strings = {"위", "d0wn", "올라up", "u!"})
+    void nonAlphabeticInputException(String input) {
+        assertThatThrownBy(() -> InputValidator.moving(input))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("[ERROR] 이동할 칸은 오직 알파벳 형태만 받을 수 있습니다.");
+    }
+
+    @DisplayName("이동할 칸 입력 시 \"U\" 또는 \"D\"와 일치하지 않으면 예외가 발생한다.")
+    @ParameterizedTest
+    @ValueSource(strings = {"r", "l", "up", "down"})
+    void noUpOrDownException(String input) {
+        assertThatThrownBy(() -> InputValidator.moving(input))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("[ERROR] 이동할 칸은 \"U\" 또는 \"D\"로 입력되어야 합니다.");
     }
 }
