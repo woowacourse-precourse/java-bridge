0156b889ab85a38ee58d6f806dfba9e31ffb153e
AnTaeWook <antk7894@naver.com>
2022-11-21 01:44:48 +0900

    <기능> 어플리케이션 개발
    게임 순서에 맞게 어플리케이션을 구현하였음.
    
    사용자의 입력값을 유효할 때 까지 받게 하기 위해
    입력값을 받는 메서드를 재귀적으로 구현함.
    
    초기화, 게임 진행, 결과출력 및 종료 3단계로 분리하여
    메인 로직의 크기를 줄였음.

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -2,7 +2,74 @@ package bridge;
 
 public class Application {
 
+    private static BridgeGame bridgeGame;
+    private static InputView inputView;
+    private static OutputView outputView;
+    private static int bridgeSize;
+    private static boolean continueFlag = true;
+
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        init();
+        play();
+        outputView.printResult(bridgeGame.getBridge(), bridgeGame.getUserSteps(), bridgeGame.getTrial());
+    }
+
+    private static void play() {
+        while (continueFlag) {
+            boolean result = bridgeGame.retry();
+            while (result && bridgeGame.getUserSteps().size() < bridgeSize) {
+                result = moveStep();
+            }
+            if (bridgeGame.getUserSteps().size() >= bridgeSize && result) {
+                break;
+            }
+            continueFlag = getValidCommandInput().equals("R");
+        }
+    }
+
+    private static boolean moveStep() {
+        String direction = getValidDirectionInput();
+        boolean result = bridgeGame.move(direction);
+        outputView.printMap(bridgeGame.getUserSteps());
+        return result;
+    }
+
+    private static void init() {
+        System.out.println("다리 건너기 게임을 시작합니다.\n");
+        bridgeGame = new BridgeGame();
+        inputView = new InputView();
+        outputView = new OutputView();
+
+        BridgeMaker bridgeMaker = new BridgeMaker(new BridgeRandomNumberGenerator());
+        bridgeSize = getValidBridgeInput();
+        bridgeGame.buildBridge(bridgeMaker.makeBridge(bridgeSize));
+        outputView.setBridge(bridgeGame.getBridge());
+    }
+
+    private static int getValidBridgeInput() {
+        try {
+            return inputView.readBridgeSize();
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage() + "\n");
+            return getValidBridgeInput();
+        }
+    }
+
+    private static String getValidDirectionInput() {
+        try {
+            return inputView.readMoving();
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage() + "\n");
+            return getValidDirectionInput();
+        }
+    }
+
+    private static String getValidCommandInput() {
+        try {
+            return inputView.readGameCommand();
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage() + "\n");
+            return getValidCommandInput();
+        }
     }
 }
