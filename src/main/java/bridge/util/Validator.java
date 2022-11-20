24c880483faac8e5e98ca69a4983c9883dc547f5
MINJU <alswn9938@gmail.com>
2022-11-20 20:34:28 +0900

    feat(InputView): add 다리 길이 입력 받는 함수

--- src/main/java/bridge/util/Validator.java
+++ src/main/java/bridge/util/Validator.java
@@ -1,4 +1,20 @@
 package bridge.util;
 
+import java.util.List;
+import java.util.Set;
+
 public class Validator {
+
+    public static void validateBridgeLength(int length){
+        if(length < 3 || length > 20){
+            throw new IllegalArgumentException("[ERROR] 다리 길이는 3 이상 20 이하여야 합니다.");
+        }
+    }
+
+    public static void validateStringIsNumeric(String input){
+        if(!input.matches("[+-]?\\d*(\\.\\d+)?")){
+            throw new IllegalArgumentException("[ERROR] 입력 값은 숫자여야합니다.");
+        }
+    }
 }
+
--- src/main/java/bridge/view/InputView.java
+++ src/main/java/bridge/view/InputView.java
@@ -1,28 +1,34 @@
 package bridge.view;
 
+import bridge.util.Validator;
+import camp.nextstep.edu.missionutils.Console;
+
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
  */
 public class InputView {
 
-    /**
-     * 다리의 길이를 입력받는다.
-     */
-    public int readBridgeSize() {
-        return 0;
+    public int readBridgeLength() {
+        String userInput = getUserInput();
+        int bridgeLength = stringToInt(userInput);
+        Validator.validateBridgeLength(bridgeLength);
+        return bridgeLength;
     }
 
-    /**
-     * 사용자가 이동할 칸을 입력받는다.
-     */
     public String readMoving() {
         return null;
     }
 
-    /**
-     * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
-     */
     public String readGameCommand() {
         return null;
+    }
+
+    private String getUserInput(){
+        return Console.readLine();
+    }
+
+    private int stringToInt(String input){
+        Validator.validateStringIsNumeric(input);
+        return Integer.parseInt(input);
     }
 }
