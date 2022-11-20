8146746d0f16371442cba68214ac29b2e9c71879
chaerim <qkrdbsk28@naver.com>
2022-11-21 03:21:29 +0900

    refactor: 출력 문자열 변수로 선언하여 사용

--- src/main/java/bridge/view/OutputView.java
+++ src/main/java/bridge/view/OutputView.java
@@ -7,6 +7,10 @@ import java.util.List;
  */
 public class OutputView {
     private final static String START_MESSAGE = "다리 건너기 게임을 시작합니다.";
+    private final static String GAME_RESULT_MESSAGE = "최종 게임 결과";
+    private final static String SUCCESS_MESSAGE = "게임 성공 여부: 성공";
+    private final static String FAIL_MESSAGE = "게임 성공 여부: 실패";
+    private final static String GAME_COUNT_MESSAGE = "총 시도한 횟수: ";
 
     /**
      * 현재까지 이동한 다리의 상태를 정해진 형식에 맞춰 출력한다.
@@ -25,15 +29,15 @@ public class OutputView {
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
     public void printResult(List<String> resultBridge, int gameCount, boolean isSuccess) {
-        System.out.println("최종 게임 결과");
+        System.out.println(GAME_RESULT_MESSAGE);
         printMap(resultBridge);
         if (isSuccess) {
-            System.out.println("게임 성공 여부: 성공");
-            System.out.println("총 시도한 횟수: " + gameCount);
+            System.out.println(SUCCESS_MESSAGE);
+            System.out.println(GAME_COUNT_MESSAGE + gameCount);
             return;
         }
-        System.out.println("게임 성공 여부: 실패");
-        System.out.println("총 시도한 횟수: " + gameCount);
+        System.out.println(FAIL_MESSAGE);
+        System.out.println(GAME_COUNT_MESSAGE + gameCount);
     }
 
     /**
