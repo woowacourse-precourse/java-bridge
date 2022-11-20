e214409624926676e352bf826b33f63ab13fee5f
yoonjuho <dbswn990@naver.com>
2022-11-20 20:25:10 +0900

    feat(BridgeGame): 다리 이동시 비교

--- src/main/java/bridge/model/BridgeGame.java
+++ src/main/java/bridge/model/BridgeGame.java
@@ -1,16 +1,30 @@
 package bridge.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * 다리 건너기 게임을 관리하는 클래스
  */
 public class BridgeGame {
-
+    private List<String> bridge;
+    BridgeGame(){
+        bridge = new ArrayList<>();
+    }
     /**
      * 사용자가 칸을 이동할 때 사용하는 메서드
      * <p>
      * 이동을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void move() {
+    public void start(int size){
+        BridgeMaker bridgeMaker = new BridgeMaker(new BridgeRandomNumberGenerator());
+        this.bridge = bridgeMaker.makeBridge(size);
+    }
+    public boolean move(int step, String input) {
+        if(isPass(bridge.get(step),input)){
+            return true;
+        }
+        return false;
     }
     public boolean isPass(String answer, String input){
         if(answer.equals(input)){
