2aa77eb5b95670df0341991e6a2517af4bb1ec45
KimJaeSung <behappy0916@naver.com>
2022-11-20 23:17:16 +0900

    refactor: 변수 이름 변경 및 메소드 분리
    leftBridge -> upBridge
    rightBridge -> downBridge
    문자열을 추가할 때 type별로 할 수 있게끔 메소드 분리

--- src/main/java/bridge/view/OutputView.java
+++ src/main/java/bridge/view/OutputView.java
@@ -15,8 +15,8 @@ public class OutputView {
     private static final String NOT_CORRECT_1 = " X ";
     private static final String NOT_CORRECT_2 = "| X ";
 
-    private static String leftBridge = "";
-    private static String rightBridge = "";
+    private static String upBridge = "";
+    private static String downBridge = "";
 
     /**
      * 현재까지 이동한 다리의 상태를 정해진 형식에 맞춰 출력한다.
@@ -25,46 +25,49 @@ public class OutputView {
      */
     public static String printMap(List<String> bridge, String inputMove, int count) {
         if (inputMove.equals("U")) {
-            leftBridge += printLeftBridge(bridge, count, inputMove);
-            return "[" + leftBridge + "]\n" + "[" + rightBridge + "]";
+            upBridge += createUpBridge(bridge, count, inputMove);
+            return "[" + upBridge + "]\n" + "[" + downBridge + "]";
         }
-        rightBridge += printRightBridge(bridge, count, inputMove);
-        return "[" + leftBridge + "]\n" + "[" + rightBridge + "]";
+        downBridge += createDownBridge(bridge, count, inputMove);
+        return "[" + upBridge + "]\n" + "[" + downBridge + "]";
     }
 
-    private static String printLeftBridge(List<String> bridge, int count, String inputMove) {
+    private static String createUpBridge(List<String> bridge, int count, String inputMove) {
         if (count == 0) {
-            rightBridge += BLANK_1;
-            if (bridge.get(count).equals(inputMove)) {
-                return CORRECT_1;
-            }
-            return NOT_CORRECT_1;
+            downBridge += BLANK_1;
+            return addBridgeType_1(bridge, count, inputMove);
         }
-        rightBridge += BLANK_2;
-        if (bridge.get(count).equals(inputMove)) {
-            return CORRECT_2;
-        }
-        return NOT_CORRECT_2;
+        downBridge += BLANK_2;
+        return addBridgeType_2(bridge, count, inputMove);
     }
 
-    private static String printRightBridge(List<String> bridge, int count, String inputMove) {
+    private static String createDownBridge(List<String> bridge, int count, String inputMove) {
         if (count == 0) {
-            leftBridge += BLANK_1;
-            if (bridge.get(count).equals(inputMove)) {
-                return CORRECT_1;
-            }
-            return NOT_CORRECT_1;
+            upBridge += BLANK_1;
+            return addBridgeType_1(bridge, count, inputMove);
         }
-        leftBridge += BLANK_2;
-        if (bridge.get(count).equals(inputMove)) {
-            return CORRECT_2;
-        }
-        return NOT_CORRECT_2;
+        upBridge += BLANK_2;
+        return addBridgeType_2(bridge, count, inputMove);
     }
 
     public static void reset() {
-        leftBridge = "";
-        rightBridge = "";
+        upBridge = "";
+        downBridge = "";
+    }
+
+    private static String addBridgeType_1(List<String> bridge, int count, String inputMove) {
+        if (bridge.get(count).equals(inputMove)) {
+            return CORRECT_1;
+        }
+        return NOT_CORRECT_1;
+
+    }
+
+    private static String addBridgeType_2(List<String> bridge, int count, String inputMove) {
+        if (bridge.get(count).equals(inputMove)) {
+            return CORRECT_2;
+        }
+        return NOT_CORRECT_2;
     }
 
     /**
@@ -74,10 +77,10 @@ public class OutputView {
      */
     public static String printResult(boolean successGame, int retryCount) {
         if (successGame) {
-            return "최종 게임 결과\n" + "[" + leftBridge + "]\n" + "[" + rightBridge + "]\n" + "게임 성공 여부: 성공\n" + "총 시도한 횟수: "
+            return "최종 게임 결과\n" + "[" + upBridge + "]\n" + "[" + downBridge + "]\n" + "게임 성공 여부: 성공\n" + "총 시도한 횟수: "
                     + retryCount;
         }
-        return "최종 게임 결과\n" + "[" + leftBridge + "]\n" + "[" + rightBridge + "]\n" + "게임 성공 여부: 실패\n" + "총 시도한 횟수: "
+        return "최종 게임 결과\n" + "[" + upBridge + "]\n" + "[" + downBridge + "]\n" + "게임 성공 여부: 실패\n" + "총 시도한 횟수: "
                 + retryCount;
     }
 }
