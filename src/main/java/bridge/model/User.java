7c15ce9f32178441cf15e22176dfc9d9d4ae8ed9
sooyoungh <sooyoung.h8@gmail.com>
2022-11-20 22:50:48 +0900

    ✨ feat(User): 한 칸 이동하는 기능 구현

---
+++ src/main/java/bridge/model/User.java
@@ -0,0 +1,14 @@
+package bridge.model;
+
+public class User {
+
+    private String moveDirection;
+
+    public User(String moveDirection) {
+        this.moveDirection = moveDirection;
+    }
+
+    public String getMoveDirection() {
+        return moveDirection;
+    }
+}
