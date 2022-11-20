1a5dc6f0363c3eb54f8db868fb5711bba28412cc
ESeokhwan <huan99@hanmail.net>
2022-11-20 16:34:13 +0900

    refactor: SpotInfo 생성하는 메소드를 Bridge 의 private 메소드에서 SpotInfo 의 public static 메소드로 이동

--- src/main/java/bridge/model/Bridge.java
+++ src/main/java/bridge/model/Bridge.java
@@ -9,7 +9,7 @@ public class Bridge {
     public Bridge(List<String> bridgeInfo) {
         bridgeMap = new ArrayList<>();
         for(String spotInfoText : bridgeInfo) {
-            bridgeMap.add(makeSpotInfo(spotInfoText));
+            bridgeMap.add(SpotInfo.makeSpotInfo(spotInfoText));
         }
     }
 
@@ -24,15 +24,5 @@ public class Bridge {
     public boolean isSafeSpot(int index, SpotInfo spotInfo) {
         SpotInfo spotInfoOnIndex = bridgeMap.get(index - 1);
         return spotInfoOnIndex.equals(spotInfo);
-    }
-
-    private SpotInfo makeSpotInfo(String spotInfoText) {
-        if(spotInfoText.equals("U")) {
-            return SpotInfo.UP;
-        }
-        if(spotInfoText.equals("D")) {
-            return SpotInfo.DOWN;
-        }
-        throw new IllegalArgumentException("다리를 생성할 때는 U 혹은 D 중 하나를 이용해 생성합니다.");
     }
 }
--- src/main/java/bridge/model/SpotInfo.java
+++ src/main/java/bridge/model/SpotInfo.java
@@ -2,5 +2,15 @@ package bridge.model;
 
 public enum SpotInfo {
     UP,
-    DOWN
+    DOWN;
+
+    public static SpotInfo makeSpotInfo(String spotInfoText) {
+        if(spotInfoText.equals("U")) {
+            return SpotInfo.UP;
+        }
+        if(spotInfoText.equals("D")) {
+            return SpotInfo.DOWN;
+        }
+        throw new IllegalArgumentException("다리를 생성할 때는 U 혹은 D 중 하나를 이용해 생성합니다.");
+    }
 }
