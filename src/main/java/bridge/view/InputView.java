947a06aebd658a0c2acdde20cae465b339450fd2
LeeHaiLim <hailim1997@gmail.com>
2022-11-21 02:58:30 +0900

    feat(view): implement class input view
    Added methods
    - read bridge size
    - read moving
    - read game command

---
+++ src/main/java/bridge/view/InputView.java
@@ -0,0 +1,38 @@
+package bridge.view;
+
+import camp.nextstep.edu.missionutils.Console;
+/**
+ * 사용자로부터 입력을 받는 역할을 한다.
+ */
+public class InputView {
+
+    /**
+     * 다리의 길이를 입력받는다.
+     */
+    public String readBridgeSize() {
+        System.out.println(InputMessage.BRIDGE_SIZE.message());
+        String bridgeSize = Console.readLine();
+
+        return bridgeSize;
+    }
+
+    /**
+     * 사용자가 이동할 칸을 입력받는다.
+     */
+    public String readMoving() {
+        System.out.println(InputMessage.MOVING.message());
+        String moving = Console.readLine();
+
+        return moving;
+    }
+
+    /**
+     * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
+     */
+    public String readGameCommand() {
+        System.out.println(InputMessage.GAME_COMMAND.message());
+        String gameCommand = Console.readLine();
+
+        return gameCommand;
+    }
+}
