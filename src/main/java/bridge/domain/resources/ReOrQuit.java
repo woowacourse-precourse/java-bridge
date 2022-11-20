393ae7f0aa598a9772fc2f3d69a5e21975820807
yback1223 <yoyob1223@gmail.com>
2022-11-20 21:22:16 +0900

    feat: Add ReOrQuit class
    When user type R or Q this class Instance is made. The validation is
    also included. Only when user type R or Q the instance is made.
    
    Closes #13

---
+++ src/main/java/bridge/domain/resources/ReOrQuit.java
@@ -0,0 +1,34 @@
+package bridge.domain.resources;
+
+import bridge.domain.exception.RQIllegalArgumentException;
+
+public class ReOrQuit {
+
+    public final static String RE_GAME = "R";
+    public final static String QUIT_GAME = "Q";
+
+    private final String inputRQ;
+
+    private ReOrQuit(String inputRQ) {
+        this.inputRQ = inputRQ;
+    }
+
+    public static ReOrQuit of(String inputRQ) {
+        validateRorQ(inputRQ);
+        return new ReOrQuit(inputRQ);
+    }
+
+    private static void validateRorQ(String inputRQ) {
+        if (isInputRQ(inputRQ)) {
+            throw new RQIllegalArgumentException();
+        }
+    }
+
+    private static boolean isInputRQ(String inputRQ) {
+        return !inputRQ.equals(RE_GAME) && !inputRQ.equals(QUIT_GAME);
+    }
+
+    public String getInputRQ() {
+        return inputRQ;
+    }
+}
