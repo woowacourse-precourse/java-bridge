b07884422b749ee9c70e4df2d1d8b30912971b20
syahn <496300@naver.com>
2022-11-20 14:38:25 +0900

    feat: BridgeGame 구현
    - answer인지 확인하는 기능
    - 게임 재시작/종료인지 확인하는 기능
    - 게임 재시작 기능
    - 다리 한 칸 이동 기능

--- src/main/java/bridge/BridgeGame.java
+++ src/main/java/bridge/BridgeGame.java
@@ -1,23 +1,78 @@
 package bridge;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * 다리 건너기 게임을 관리하는 클래스
  */
 public class BridgeGame {
+    private int movementCount;
+    private int round;
+    private final Bridge bridge;
+    private BridgeMap bridgeMap;
+    private boolean isAnswer;
 
-    /**
-     * 사용자가 칸을 이동할 때 사용하는 메서드
-     * <p>
-     * 이동을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
-     */
-    public void move() {
+    public BridgeGame(Bridge bridge) {
+        this.movementCount = 0;
+        this.round = 1;
+        this.bridge = bridge;
+        this.bridgeMap = new BridgeMap(new ArrayList<>());
+        this.isAnswer = false;
     }
 
-    /**
-     * 사용자가 게임을 다시 시도할 때 사용하는 메서드
-     * <p>
-     * 재시작을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
-     */
-    public void retry() {
+    void move(String movement) {
+        checkAnswer(movement);
+        markMove(movement);
+        movementCount += 1;
+    }
+
+    void retry() {
+        round += 1;
+        movementCount = 0;
+        bridgeMap = new BridgeMap(new ArrayList<>());
+    }
+
+    void markMove(String movement) {
+        String mark = createMark();
+        List<String> partition = new ArrayList<>(List.of(" ", " "));
+        int moving = MovingStatus.findByCommand(movement).getMovingNumber();
+        partition.set(moving, mark);
+        bridgeMap.updateMap(partition);
+    }
+
+    String createMark() {
+        if (isAnswer)
+            return "O";
+        return "X";
+    }
+
+    void checkAnswer(String movement) {
+        this.isAnswer = bridge.getAnswerByCount(movementCount).equals(movement);
+    }
+
+    boolean getIsAnswer() {
+        return this.isAnswer;
+    }
+
+    boolean isExit(String command) {
+        if (command.equals("R")) {
+            retry();
+            return false;
+        }
+        return true;
+
+    }
+
+    boolean checkSuccess() {
+        return (bridge.getBridgeSize() == movementCount && this.isAnswer);
+    }
+
+    int getRound() {
+        return this.round;
+    }
+
+    BridgeMap getBridgeMap() {
+        return this.bridgeMap;
     }
 }
