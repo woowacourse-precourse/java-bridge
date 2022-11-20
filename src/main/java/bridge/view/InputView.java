7cdb8617e8103e7b9bbad04df31685d1adcd163c
anneu <b.imnuj@gmail.com>
2022-11-21 01:26:38 +0900

    refactor: 사용자 입력 값에 대한 검증 기능 분리

--- src/main/java/bridge/view/InputView.java
+++ src/main/java/bridge/view/InputView.java
@@ -4,23 +4,16 @@ import camp.nextstep.edu.missionutils.Console;
 
 public class InputView {
 
-    private static final String UP = "U";
-    private static final String DOWN = "D";
-    private static final String RESTART = "R";
-    private static final String QUIT = "Q";
-    private static final String ERROR = "[ERROR] ";
-    private static final String MOVING_ERROR = ERROR + UP + " 또는 " + DOWN + "를 입력해주세요.";
-    private static final String COMMAND_ERROR = ERROR + RESTART + " 또는 " + QUIT + "를 입력해주세요.";
-    private static final String NUMBER_FORMAT_ERROR = ERROR + "숫자를 입력해주세요.";
+    private final InputViewValidator validator = new InputViewValidator();
 
     public int readBridgeSize() {
-        return checkNumberFormat(input());
+        return validator.checkNumberFormat(input());
     }
 
     public String readMoving() {
         while (true) {
             try {
-                return checkMoving(input());
+                return validator.checkMoving(input());
             } catch (IllegalArgumentException e) {
                 System.out.println(e.getMessage());
             }
@@ -30,40 +23,10 @@ public class InputView {
     public String readGameCommand() {
         while (true) {
             try {
-                return checkCommand(input());
+                return validator.checkCommand(input());
             } catch (IllegalArgumentException e) {
                 System.out.println(e.getMessage());
             }
-        }
-    }
-
-    private String checkCommand(String command) {
-        if (isWrongCommand(command)) {
-            throw new IllegalArgumentException(COMMAND_ERROR);
-        }
-        return command;
-    }
-
-    private boolean isWrongCommand(String command) {
-        return !(command.equals(RESTART) || command.equals(QUIT));
-    }
-
-    private String checkMoving(String moving) {
-        if (isWrongMoving(moving)) {
-            throw new IllegalArgumentException(MOVING_ERROR);
-        }
-        return moving;
-    }
-
-    private boolean isWrongMoving(String moving) {
-        return !(moving.equals(UP) || moving.equals(DOWN));
-    }
-
-    private int checkNumberFormat(String number) {
-        try {
-            return Integer.parseInt(number);
-        } catch (NumberFormatException e) {
-            throw new IllegalArgumentException(NUMBER_FORMAT_ERROR);
         }
     }
 
---
+++ src/main/java/bridge/view/InputViewValidator.java
@@ -0,0 +1,42 @@
+package bridge.view;
+
+public class InputViewValidator {
+
+    private static final String UP = "U";
+    private static final String DOWN = "D";
+    private static final String RESTART = "R";
+    private static final String QUIT = "Q";
+    private static final String ERROR = "[ERROR] ";
+    private static final String MOVING_ERROR = ERROR + UP + " 또는 " + DOWN + "를 입력해주세요.";
+    private static final String COMMAND_ERROR = ERROR + RESTART + " 또는 " + QUIT + "를 입력해주세요.";
+    private static final String NUMBER_FORMAT_ERROR = ERROR + "숫자를 입력해주세요.";
+
+    public String checkCommand(String command) {
+        if (isWrongCommand(command)) {
+            throw new IllegalArgumentException(COMMAND_ERROR);
+        }
+        return command;
+    }
+
+    public String checkMoving(String moving) {
+        if (isWrongMoving(moving)) {
+            throw new IllegalArgumentException(MOVING_ERROR);
+        }
+        return moving;
+    }
+
+    public int checkNumberFormat(String number) {
+        try {
+            return Integer.parseInt(number);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException(NUMBER_FORMAT_ERROR);
+        }
+    }
+
+    private boolean isWrongCommand(String command) {
+        return !(command.equals(RESTART) || command.equals(QUIT));
+    }
+    private boolean isWrongMoving(String moving) {
+        return !(moving.equals(UP) || moving.equals(DOWN));
+    }
+}
