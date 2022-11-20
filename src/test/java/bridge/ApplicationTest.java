bdf6b1dc29572a276eac5983972f4d2f9c3d99da
Eunkeee <eunkeeeee@gmail.com>
2022-11-20 16:26:15 +0900

    test(Application): 재시작 후 성공 테스트 코드 작성

--- src/test/java/bridge/ApplicationTest.java
+++ src/test/java/bridge/ApplicationTest.java
@@ -40,6 +40,36 @@ class ApplicationTest extends NsTest {
     }
 
     @Test
+    void 재시작_후_성공_테스트() {
+        assertRandomNumberInRangeTest(() -> {
+            run("3", "U", "R", "D", "D", "U");
+            assertThat(output()).contains(
+                    "[ X ]",
+                    "[   ]",
+                    "게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)",
+                    "이동할 칸을 선택해주세요. (위: U, 아래: D)",
+                    "[   ]",
+                    "[ O ]",
+                    "이동할 칸을 선택해주세요. (위: U, 아래: D)",
+                    "[   |   ]",
+                    "[ O | O ]",
+                    "이동할 칸을 선택해주세요. (위: U, 아래: D)",
+                    "[   |   | O ]",
+                    "[ O | O |   ]",
+                    "최종 게임 결과",
+                    "[   |   | O ]",
+                    "[ O | O |   ]",
+                    "게임 성공 여부: 성공",
+                    "총 시도한 횟수: 2"
+            );
+
+            int upSideIndex = output().indexOf("[   |   | O ]");
+            int downSideIndex = output().indexOf("[ O | O |   ]");
+            assertThat(upSideIndex).isLessThan(downSideIndex);
+        }, 0, 0, 1);
+    }
+
+    @Test
     void 예외_테스트() {
         assertSimpleTest(() -> {
             runException("a");
