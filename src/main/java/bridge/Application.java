820f95c11a6a8f93619664d91d5e1a136a883320
yimo22 <yimo22@naver.com>
2022-11-20 18:06:59 +0900

    feat : BridgeGame
    featured : BridgeGame.retry / BridgeGame.move
    - move() : 사용자의 입력을 받아 move 기능 추가
    - retry() : 재시작 기능 추가

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -1,8 +1,12 @@
 package bridge;
 
 public class Application {
-
+    private static BridgeGame bridgeGame;
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        bridgeGame = new BridgeGame(new InputView().readBridgeSize());
+        while(!bridgeGame.isEnd()){
+            bridgeGame.move();
+        }
     }
 }
--- src/main/java/bridge/BridgeGame.java
+++ src/main/java/bridge/BridgeGame.java
@@ -1,9 +1,24 @@
 package bridge;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * 다리 건너기 게임을 관리하는 클래스
  */
 public class BridgeGame {
+    private int retryCount;
+    private final List<String> bridge;
+    private final InputView inputView;
+    private final OutputView outputView;
+    private final List<String> userList;
+    public BridgeGame(int bridgeSize) {
+        this.bridge = new BridgeMaker(new BridgeRandomNumberGenerator()).makeBridge(bridgeSize);
+        this.inputView = new InputView();
+        this.outputView = new OutputView();
+        this.userList = new ArrayList<>();
+        retryCount = 0;
+    }
 
     /**
      * 사용자가 칸을 이동할 때 사용하는 메서드
@@ -11,6 +26,11 @@ public class BridgeGame {
      * 이동을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
     public void move() {
+        String userInput = inputView.readMoving();
+        int idx = userList.size();
+        userList.add(userInput);
+        outputView.printMap();
+        if(!bridge.get(idx).equals(userInput)) retry();
     }
 
     /**
@@ -19,5 +39,16 @@ public class BridgeGame {
      * 재시작을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
     public void retry() {
+        retryCount++;
+        userList.clear();
+    }
+
+    public boolean isEnd(){
+        if(userList.size() != bridge.size()) return false;
+        for(int i=0;i<bridge.size();i++){
+            if(!bridge.get(i).equals(userList.get(i))) return false;
+        }
+        outputView.printResult(userList, retryCount);
+        return true;
     }
 }
