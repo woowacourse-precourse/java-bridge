1b8d8234eba6fdd7af3c8c7f6937a29501ad7777
ashlovesliitea <skyrider1811@gmail.com>
2022-11-20 16:23:52 +0900

    refactor: BridgeMaker - 랜덤으로 다리 모양 생성하는 기능 추출

--- src/main/java/bridge/BridgeMaker.java
+++ src/main/java/bridge/BridgeMaker.java
@@ -19,17 +19,19 @@ public class BridgeMaker {
      * @return 입력받은 길이에 해당하는 다리 모양. 위 칸이면 "U", 아래 칸이면 "D"로 표현해야 한다.
      */
     public List<String> makeBridge(int size) {
-        //TODO - if 리팩토링하기
         List<String> spaces = new ArrayList<>();
         for(int i=0;i<size;i++){
-            int generateNum=bridgeNumberGenerator.generate();
-            if(generateNum==1){
-                spaces.add("U");
-            }
-            if(generateNum==0){
-                spaces.add("D");
-            }
+           String space = generate();
+           spaces.add(space);
         }
         return spaces;
+    }
+
+    private String generate(){
+        int generateNum=bridgeNumberGenerator.generate();
+        if(generateNum==1){
+            return "U";
+        }
+        return "D";
     }
 }
