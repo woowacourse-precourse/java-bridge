79a08f4644828f4f11564fc2161a0fca308816b3
SeokyungKim <kskyung0624@gmail.com>
2022-11-21 01:27:13 +0900

    refactor(GameMachine) : 메서드 길이가 10줄이 넘는 run의 메서드를 추출한다

--- src/main/java/bridge/GameMachine.java
+++ src/main/java/bridge/GameMachine.java
@@ -13,46 +13,64 @@ public class GameMachine {
     private OutputView view = new OutputView();
     private BridgeMaker bridgeMaker = new BridgeMaker(new BridgeRandomNumberGenerator());
 
-    public void run() {
-        view.printBridgeSizeRequest();
-        List<BridgeType> bridge = makeBridge();
-        BridgeGame bridgeGame = new BridgeGame(bridge);
+    private List<BridgeType> bridge;
+    private BridgeGame bridgeGame;
 
-        int counter = 1;
-        view.printStart();
+    private int counter;
+
+    public GameMachine() {
+        bridge = makeBridge();
+        bridgeGame = new BridgeGame(bridge);
+        counter = 1;
+    }
+
+    public void run() {
+        start();
         RetryCommand retry = RetryCommand.RETRY;
         while (retry == RetryCommand.RETRY) {
             bridgeGame.retry();
-            MoveResult gameResult = play(bridge, bridgeGame);
-
-            if (gameResult == MoveResult.FAIL) {
-                view.printRestartRequest();
-                retry = RetryCommand.of(ui.readGameCommand());
-                counter++;
-                continue;
-            }
-            retry = RetryCommand.QUIT;
+            MoveResult gameResult = play();
+            retry = gameAfter(gameResult);
         }
+        end();
+    }
+
+    private void start() {
+        view.printBridgeSizeRequest();
+        view.printStart();
+    }
+
+    private RetryCommand gameAfter(MoveResult gameResult) {
+        if (gameResult == MoveResult.FAIL) {
+            view.printRestartRequest();
+            counter++;
+            return RetryCommand.of(ui.readGameCommand());
+        }
+        return RetryCommand.QUIT;
+    }
+
+    private void end() {
         view.printResult();
         view.printGameCount(counter);
     }
 
-    /**
-     * todo: 재시작해도 처음에 만든 다리로 재사용한다.
-     * todo: Exception이 아닌 IllegalArgumentException, IllegalStateException 등과 같은 명확한 유형을 처리한다.
-     * todo: 실패했을 때 게임을 재시작할 수 있어야 한다.
-     */
-    private MoveResult play(List<BridgeType> bridge, BridgeGame bridgeGame) {
+    private MoveResult play() {
         for (int location = 0; location < bridge.size(); location++) {
-            view.printMoveTypeRequest();
-            BridgeType userInput = BridgeType.of(ui.readMoving());
-            if (MoveResult.FAIL == bridgeGame.move(userInput)) {
+            MoveResult moveResult = move();
+            if (moveResult == MoveResult.FAIL) {
                 view.printMap(bridge, location, MoveResult.FAIL);
                 return MoveResult.FAIL;
             }
             view.printMap(bridge, location, MoveResult.PASS);
         }
         return MoveResult.PASS;
+    }
+
+    private MoveResult move() {
+        view.printMoveTypeRequest();
+        BridgeType userInput = BridgeType.of(ui.readMoving());
+        MoveResult moveResult = bridgeGame.move(userInput);
+        return moveResult;
     }
 
     private List<BridgeType> makeBridge() {
