8519d6b032fe7643a95331cf529496d96419919b
yoonjuho <dbswn990@naver.com>
2022-11-20 19:42:14 +0900

    refactor(Notice): 상수처리 클래스 명 변경

--- src/main/java/bridge/view/Notice.java
+++ src/main/java/bridge/view/Notice.java
@@ -1,6 +1,6 @@
 package bridge.view;
 
-public class GameMessage {
+public class Notice {
     public static final String STARTING_GAME = "다리 건너기 게임을 시작합니다.";
     public static final String SET_GAME = "다리의 길이를 입력해주세요.";
     public static final String RUNNING_GAME = "이동할 칸을 선택해주세요. (위: U, 아래: D)";
