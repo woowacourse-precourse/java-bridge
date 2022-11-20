760108cb896eb2835f663f500a0294ff348dca06
woosung1223 <dntjd991223@naver.com>
2022-11-20 17:27:01 +0900

    test(Bridge): 도메인 검증 기능 테스트 생성

--- src/test/java/bridge/domain/BridgeTest.java
+++ src/test/java/bridge/domain/BridgeTest.java
@@ -1,8 +1,9 @@
 package bridge.domain;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
-import bridge.domain.Bridge;
+import bridge.system.ExceptionMessage;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.TestInstance;
@@ -17,11 +18,34 @@ class BridgeTest {
 
     @TestInstance(TestInstance.Lifecycle.PER_CLASS)
     @Nested
+    @DisplayName("도메인 로직 예외처리 테스트")
+    class DomainException {
+        @ParameterizedTest
+        @MethodSource("parameterProvider")
+        void 브리지를_생성할때_유효한_값이_아니면_예외처리_하는지_테스트(List<String> target) {
+            assertThatThrownBy(() -> new Bridge(target))
+                    .isInstanceOf(IllegalArgumentException.class)
+                    .hasMessageContaining(ExceptionMessage.BRIDGE_ELEMENT_INVALID);
+        }
+
+        private Stream<Arguments> parameterProvider() {
+            return Stream.of(
+                    Arguments.of(List.of("U", "u")),
+                    Arguments.of(List.of("1", "2", "3")),
+                    Arguments.of(List.of("D", "u", "D")),
+                    Arguments.of(List.of("d", "D", "#")),
+                    Arguments.of(List.of("U", "u", " "))
+            );
+        }
+    }
+
+    @TestInstance(TestInstance.Lifecycle.PER_CLASS)
+    @Nested
     @DisplayName("isPartOfBridge 메소드 테스트")
     class IsPartOfBridge {
         @ParameterizedTest
         @MethodSource("parameterProvider")
-        void 한_다리가_다른_다리의_일부분인지_체크하는_기능을_테스트(Bridge source, Bridge target) {
+        void 한_브리지가_다른_브리지의_일부분인지_체크하는_기능을_테스트(Bridge source, Bridge target) {
             assertThat(source.isPartOfBridge(target)).isTrue();
         }
 
@@ -43,7 +67,7 @@ class BridgeTest {
     class ContainsAll {
         @ParameterizedTest
         @MethodSource("parameterProvider")
-        void 한_다리가_다른_리스트의_값을_포함하는지_체크하는_기능을_테스트(Bridge source, List<String> target) {
+        void 한_브리지가_다른_리스트의_값을_포함하는지_체크하는_기능을_테스트(Bridge source, List<String> target) {
             assertThat(source.containsAll(target)).isTrue();
         }
 
