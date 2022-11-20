e033f2604621b6400746a7afbbc935f032c25331
kimtaesoo99 <107785279+kimtaesoo99@users.noreply.github.com>
2022-11-20 21:00:42 +0900

    refactor: (BridgeGameInputException) 가독성을 위해 상수 처리

--- src/main/java/bridge/exception/BridgeGameInputException.java
+++ src/main/java/bridge/exception/BridgeGameInputException.java
@@ -1,6 +1,14 @@
 package bridge.exception;
 
 public class BridgeGameInputException {
+    private static final String ONLY_NUMBER = "^[0-9]*$";
+    private static final int MIN_BRIDGE_LENGTH = 3;
+    private static final int MAX_BRIDGE_LENGTH = 20;
+    private static final String UP = "U";
+    private static final String DOWN = "D";
+    private static final String RETRY = "R";
+    private static final String QUIT = "Q";
+    private static final String ERROR_MESSAGE = "[ERROR]";
 
     public void validateBridgeSize(String size) {
         validateBridgeSizeBlank(size);
@@ -9,33 +17,33 @@ public class BridgeGameInputException {
     }
 
     private void validateBridgeSizeNumber(String size) {
-        if (!size.matches("^[0-9]*$")) {
-            throw new IllegalArgumentException("[ERROR] 다리의 길이는 숫자만 입력가능합니다.");
+        if (!size.matches(ONLY_NUMBER)) {
+            throw new IllegalArgumentException(ERROR_MESSAGE + "다리의 길이는 숫자만 입력가능합니다.");
         }
     }
 
     private void validateBridgeSizeRange(String size) {
         int bridgeSize = Integer.parseInt(size);
-        if (bridgeSize < 3 || bridgeSize > 20) {
-            throw new IllegalArgumentException("[ERROR] 다리의 길이는 3에서 20사이입니다.");
+        if (bridgeSize < MIN_BRIDGE_LENGTH || bridgeSize > MAX_BRIDGE_LENGTH) {
+            throw new IllegalArgumentException(ERROR_MESSAGE + "다리의 길이는 3에서 20사이입니다.");
         }
     }
 
     private void validateBridgeSizeBlank(String size) {
         if (size.isBlank()) {
-            throw new IllegalArgumentException("[ERROR] 다리의 길이를 입력해주세요.");
+            throw new IllegalArgumentException(ERROR_MESSAGE + "다리의 길이를 입력해주세요.");
         }
     }
 
     public void validateMovingChoice(String move) {
-        if (!(move.equals("U") || move.equals("D"))) {
-            throw new IllegalArgumentException("[ERROR] 이동할 칸은 U또는 D만 입력가능합니다.");
+        if (!(move.equals(UP) || move.equals(DOWN))) {
+            throw new IllegalArgumentException(ERROR_MESSAGE + "이동할 칸은 U또는 D만 입력가능합니다.");
         }
     }
 
     public void validateGameCommand(String gameCommand) {
-        if (!(gameCommand.equals("R") || gameCommand.equals("Q"))) {
-            throw new IllegalArgumentException("[ERROR] 게임 재시작여부는 R또는 Q만 입력가능합니다.");
+        if (!(gameCommand.equals(RETRY) || gameCommand.equals(QUIT))) {
+            throw new IllegalArgumentException(ERROR_MESSAGE + "게임 재시작여부는 R또는 Q만 입력가능합니다.");
         }
     }
 
