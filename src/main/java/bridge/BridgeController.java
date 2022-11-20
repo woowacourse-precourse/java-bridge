57c2db83d93d3ebb16ae456db4a6755f59f9e570
Yunsik-Choi <choiys0311@gmail.com>
2022-11-20 20:21:24 +0900

    ⚡feat : 위아래 두 칸으로 이루어진 다리를 건너는 기능 구현

--- src/main/java/bridge/BridgeController.java
+++ src/main/java/bridge/BridgeController.java
@@ -1,6 +1,8 @@
 package bridge;
 
 import bridge.domain.Bridge;
+import bridge.domain.BridgeGame;
+import bridge.domain.BridgeShape;
 import bridge.view.InputView;
 import java.util.List;
 
@@ -13,9 +15,19 @@ public class BridgeController {
         this.bridgeMaker = new BridgeMaker(bridgeNumberGenerator);
     }
 
+    public void gameStart() {
+        BridgeGame bridgeGame = new BridgeGame(createBridge());
+        move(bridgeGame);
+    }
+
     private Bridge createBridge() {
         int bridgeSize = inputView.readBridgeSize();
         List<String> bridgeShapeValues = bridgeMaker.makeBridge(bridgeSize);
         return Bridge.createByBridgeShapeValue(bridgeShapeValues);
+    }
+
+    private void move(BridgeGame bridgeGame) {
+        BridgeShape moveBridgeShape = BridgeShape.of(inputView.readMoving());
+        bridgeGame.move(moveBridgeShape);
     }
 }
