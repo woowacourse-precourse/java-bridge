10a8bec4cfda8fac3133a829fa14ea9fa4174203
JangAJang <99702271+JangAJang@users.noreply.github.com>
2022-11-20 22:00:34 +0900

    feat: BridgeMaker 랜덤한 정수를 반환하는 기능 구현

--- src/main/java/bridge/util/BridgeMaker.java
+++ src/main/java/bridge/util/BridgeMaker.java
@@ -1,6 +1,12 @@
 package bridge.util;
 
+import bridge.model.BridgeType;
+
+import java.util.ArrayList;
 import java.util.List;
+
+import static bridge.model.BridgeType.LOWER;
+import static bridge.model.BridgeType.UPPER;
 
 /**
  * 다리의 길이를 입력 받아서 다리를 생성해주는 역할을 한다.
@@ -18,6 +24,10 @@ public class BridgeMaker {
      * @return 입력받은 길이에 해당하는 다리 모양. 위 칸이면 "U", 아래 칸이면 "D"로 표현해야 한다.
      */
     public List<String> makeBridge(int size) {
-        return null;
+
+    }
+
+    private int chooseSideOfBridge(){
+        return bridgeNumberGenerator.generate();
     }
 }
