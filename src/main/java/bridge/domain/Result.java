c4c9c55a880d66311cf8e56f2702dc6d6996ee37
김태용 <amico741@naver.com>
2022-11-20 23:17:17 +0900

    feat: 결과물 클래스 생성

---
+++ src/main/java/bridge/domain/Result.java
@@ -0,0 +1,15 @@
+package bridge.domain;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Result {
+    private static final int EMPTY = 0;
+    private final List<String> upStairs;
+    private final List<String> downsStairs;
+
+    public Result() {
+        this.upStairs = new ArrayList<>();
+        this.downsStairs = new ArrayList<>();
+    }
+}
