79fd65e7e00e60a1cc1943fb5c170b8fe07a4874
desfox <desfox99@naver.com>
2022-11-20 21:27:48 +0900

    feat(BridgeMakerValidation): 랜덤값에 대응하는 칸이 다리에 추가되는지 검증하는 로직 구현

--- src/main/java/bridge/validation/BridgeMakerValidation.java
+++ src/main/java/bridge/validation/BridgeMakerValidation.java
@@ -1,9 +1,25 @@
 package bridge.validation;
 
+import java.util.List;
+
 public class BridgeMakerValidation {
     public void validateGeneratedNumValue(int generatedNum) {
         if (generatedNum != 0 && generatedNum != 1) {
             throw new IllegalArgumentException("[ERROR] 0 또는 1의 값만 생성되어야 합니다.");
+        }
+    }
+
+    public void validateCorrespondingBoxAddedToBridge(int generatedNum, List<String> bridge) {
+        if (generatedNum == 0) {
+            if (! bridge.get(bridge.size() - 1).equals("D")) {
+                throw new IllegalArgumentException("[ERROR] 랜덤값이 0일 경우, 다리에 아래 칸이 추가되어야 합니다.");
+            }
+        }
+
+        if (generatedNum == 1) {
+            if (! bridge.get(bridge.size() - 1).equals("U")) {
+                throw new IllegalArgumentException("[ERROR] 랜덤값이 1일 경우, 다리에 위 칸이 추가되어야 합니다.");
+            }
         }
     }
 }
