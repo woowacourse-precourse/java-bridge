7cd0928281b18141b7ec3175efe05b6cabb351cc
impati <impati0716@icloud.com>
2022-11-20 15:09:16 +0900

    feat (InputView) : 입력 가이드 문장 출력

--- src/main/java/bridge/InputView.java
+++ src/main/java/bridge/InputView.java
@@ -7,7 +7,9 @@ import camp.nextstep.edu.missionutils.Console;
  */
 public class InputView {
     private final Validation validation;
-
+    private final String BRIDGE_SIZE_MESSAGE = "다리의 길이를 입력해주세요.";
+    private final String MOVE_MESSAGE = "이동할 칸을 선택해주세요. (위: U, 아래: D)";
+    private final String RETRY_MESSAGE = "게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)";
     public InputView(Validation validation) {
         this.validation = validation;
     }
@@ -16,6 +18,7 @@ public class InputView {
      * 다리의 길이를 입력받는다.
      */
     public int readBridgeSize() {
+        System.out.println(BRIDGE_SIZE_MESSAGE);
         String bridgeSize = Console.readLine();
         validation.bridgeLen(bridgeSize);
         return Integer.parseInt(bridgeSize);
@@ -25,6 +28,7 @@ public class InputView {
      * 사용자가 이동할 칸을 입력받는다.
      */
     public String readMoving() {
+        System.out.println(MOVE_MESSAGE);
         String command = Console.readLine();
         validation.moveCommand(command);
         return command;
@@ -34,6 +38,7 @@ public class InputView {
      * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
      */
     public String readGameCommand() {
+        System.out.println(RETRY_MESSAGE);
         String command = Console.readLine();
         validation.retryCommand(command);
         return command;
