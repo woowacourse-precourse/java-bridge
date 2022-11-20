1c74b021238ca0fe4de14a09e3c440a66f840c02
ysng_ysng <yeasung67@gmail.com>
2022-11-21 03:15:19 +0900

    feat(OutputMessages): Implement the class to use printing output messages
    게임의 결과를 출력하는데 사용할 메시지를 가진 클래스를 만들었습니다.

---
+++ src/main/java/bridge/view/OutputMessages.java
@@ -0,0 +1,9 @@
+package bridge.view;
+
+public class OutputMessages {
+	public static final String FINAL_RESULT = "최종 게임 결과";
+	public static final String SUCCESS_OR_FAIL = "게임 성공 여부: ";
+	public static final String GAME_SUCCESS = "성공";
+	public static final String GAME_FAIL = "실패";
+	public static final String TOTAL_TRY = "총 시도한 횟수: ";
+}
