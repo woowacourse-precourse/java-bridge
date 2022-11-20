defff68225e2e827554210490c46e3cbd312c4a3
ykmxxi <ykmxxi97@gmail.com>
2022-11-20 11:29:43 +0900

    feat: 재시작 여부 입력 예외 처리기능[공백의 문자열 처리] 구현

--- src/main/java/bridge/validation/GameCommandValidation.java
+++ src/main/java/bridge/validation/GameCommandValidation.java
@@ -8,6 +8,10 @@ public class GameCommandValidation {
     public GameCommandValidation() {
     }
 
+    public void throwError(String message) {
+        throw new IllegalArgumentException(message);
+    }
+
     public void isNull(String gameCommand) {
         if (gameCommand.isEmpty()) {
             message = ErrorMessage.valueOf("COMMAND_NULL_EXCEPTION");
@@ -16,6 +20,10 @@ public class GameCommandValidation {
     }
 
     public void isBlank(String gameCommand) {
+        if (gameCommand.isBlank() && gameCommand.length() != 0) {
+            message = ErrorMessage.valueOf("MOVING_BLANK_EXCEPTION");
+            throwError(message.getMessage());
+        }
     }
 
     public void isUpperCaseRQ(String gameCommand) {
--- src/main/java/bridge/validation/enums/ErrorMessage.java
+++ src/main/java/bridge/validation/enums/ErrorMessage.java
@@ -8,7 +8,8 @@ public enum ErrorMessage {
     MOVING_NULL_EXCEPTION("[ERROR] 빈 문자열이 입력되었습니다. 대문자 U(위칸), D(아래칸)중 하나를 입력해 주세요."),
     MOVING_BLANK_EXCEPTION("[ERROR] 공백의 문자열이 입력되었습니다. 대문자 U(위칸), D(아래칸)중 하나를 입력해 주세요."),
     MOVING_TYPE_EXCEPTION("[ERROR] 입력 타입과 일치하지 않습니다. 대문자 U(위칸), D(아래칸)중 하나를 입력해 주세요."),
-    COMMAND_NULL_EXCEPTION("[ERROR] 빈 문자열이 입력되었습니다. 대문자 R(재시도), Q(종료)중 하나를 입력해 주세요.");
+    COMMAND_NULL_EXCEPTION("[ERROR] 빈 문자열이 입력되었습니다. 대문자 R(재시도), Q(종료)중 하나를 입력해 주세요."),
+    COMMAND_BLANK_EXCEPTION("[ERROR] 공백의 문자열이 입력되었습니다. 대문자 R(재시도), Q(종료)중 하나를 입력해 주세요.");
 
     private final String message;
 
