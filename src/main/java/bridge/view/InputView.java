dcc5f9b98d7eb2528f4de00f7f75657435894668
DaWoon Lee <qkgk61@gmail.com>
2022-11-20 20:32:02 +0900

    Feat : InputView 클래스에서 게임 커맨드를 입력받는 메서드 구현

--- src/main/java/bridge/view/InputView.java
+++ src/main/java/bridge/view/InputView.java
@@ -46,7 +46,17 @@ public class InputView {
      * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
      */
     public String readGameCommand() {
-        return null;
+        System.out.println("게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)");
+        String gameCommand = Console.readLine();
+
+        try {
+            InputValidator.validateGameCommand(gameCommand);
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+            readGameCommand();
+        }
+
+        return gameCommand;
     }
 
 }
