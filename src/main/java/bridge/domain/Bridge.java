031cce6a9e2b2f97c752fc12e6243a1560fd2ac6
Miseong Kim <miseongk@naver.com>
2022-11-21 01:44:20 +0900

    feat(Bridge): validate bride size is between 3 and 20

--- src/main/java/bridge/domain/Bridge.java
+++ src/main/java/bridge/domain/Bridge.java
@@ -8,12 +8,18 @@ public class Bridge {
     private final int size;
 
     public Bridge(List<String> bridge, int size) {
+        validate(size);
         this.bridge = bridge;
         this.size = size;
+    }
+
+    private void validate(int size) {
+        if (size < 3 || size > 20) {
+            throw new IllegalArgumentException("[ERROR] 다리 길이는 3부터 20 사이의 숫자여야 합니다.");
+        }
     }
 
     public boolean checkToMove(int position, String moveTo) {
         return bridge.get(position).equals(moveTo);
     }
-
 }
