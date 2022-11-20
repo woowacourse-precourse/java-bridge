9f5db9878643633be3522239fef44b88c28be29b
areyouhun <areyouhun@gmail.com>
2022-11-20 17:35:41 +0900

    docs(readme): 기능 목록 작성

--- src/main/java/bridge/InputView.java
+++ src/main/java/bridge/InputView.java
@@ -5,7 +5,7 @@ package bridge;
  * 1. InputView의 패키지는 변경할 수 🌴있다.🌴
  * 2. InputView의 메소드 시그니처 (인자, 이름)와 반환 타입은 변경할 수 🌴있다.🌴
  * 3. 사용자 값 입력을 위해 필요한 메소드를 추가할 수 🌴있다.🌴
- * 4. InputView 클래스에서만 readLine() 메서드를 이용해 사용자의 입력을 받을 수 🌴있다.🌴
+ * 4. InputView 클래스에서만 Console.readLine() 메소드를 이용해 사용자의 입력을 받을 수 🌴있다.🌴
  */
 public class InputView {
 
