7756f6801f1a639b2a1ac67f6d8a61f0f7eaaf66
Isu Shin <isu010614@naver.com>
2022-11-21 00:41:49 +0900

    feat: 재시작 카운트 기능 구현
    - 게임을 재시작 할 때마다 게임 시도 횟수를 더해준다.

--- src/main/java/bridge/domain/BridgeGame.java
+++ src/main/java/bridge/domain/BridgeGame.java
@@ -7,9 +7,11 @@ import java.util.List;
  */
 public class BridgeGame {
     private final MovingStatusSaver movingStatusSaver;
+    private int retryCount;
 
-    public BridgeGame(MovingStatusSaver movingStatusSaver) {
+    public BridgeGame(MovingStatusSaver movingStatusSaver, int retryCount) {
         this.movingStatusSaver = movingStatusSaver;
+        this.retryCount = retryCount;
     }
 
     /**
@@ -40,6 +42,15 @@ public class BridgeGame {
      * 재시작을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
     public void retry(final String command) {
+        movingStatusSaver.clearMovingStatus();
+        addRetryCount();
+    }
 
+    private void addRetryCount(){
+        this.retryCount++;
+    }
+
+    public int getRetryCount(){
+        return this.retryCount;
     }
 }
