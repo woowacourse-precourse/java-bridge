f5a7d8366986e8bd63f1ab099ed7eea75311e08c
yenawee <yaena0319@naver.com>
2022-11-20 18:21:08 +0900

    fix : 게임 실패 시 성공으로 나오는 오류 수정

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -5,6 +5,8 @@ import java.util.List;
 public class Application {
 
     public static void main(String[] args) {
+        int SUCCESS = 1;
+        int FAILURE = 0;
         BridgeNumberGenerator numberGenerator = new BridgeRandomNumberGenerator();
         BridgeMaker bridgeMaker = new BridgeMaker(numberGenerator);
         InputView inputView = new InputView();
@@ -15,23 +17,25 @@ public class Application {
         System.out.println("다리의 길이를 입력해주세요.");
         int bridgeSize = inputView.readBridgeSize(inputView.readline());
         List<String> bridge = bridgeMaker.makeBridge(bridgeSize);
+        System.out.println(bridge);
         while (true){
             System.out.println("이동할 칸을 선택해주세요. (위: U, 아래: D)");
             String readMove = inputView.readMoving(inputView.readline());
             boolean move = bridgeGame.move(bridge, readMove);
             outputView.printMap(readMove, move);
             bridgeGame.index++;
-            if (!move) {
+            if (bridgeGame.index == bridgeSize || !move) {
                 break ;
             }
         }
         if (bridgeGame.index == bridgeSize){
-            outputView.printResult();
+            outputView.printResult(SUCCESS);
             return ;
         }
+        System.out.println("게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)");
         String retry = inputView.readGameCommand(inputView.readline());
         if (retry.equals("Q")){
-            outputView.printResult();
+            outputView.printResult(FAILURE);
             return ;
         }
         bridgeGame.retry();
