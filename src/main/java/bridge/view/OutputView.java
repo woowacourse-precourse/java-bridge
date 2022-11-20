e1312149f34df1aea62510419967fb914241c341
kimtaesoo99 <107785279+kimtaesoo99@users.noreply.github.com>
2022-11-20 21:12:31 +0900

    refactor: (OutputView) 가독성을 위해 상수 처리

--- src/main/java/bridge/view/OutputView.java
+++ src/main/java/bridge/view/OutputView.java
@@ -7,6 +7,14 @@ import bridge.domain.User;
  * 사용자에게 게임 진행 상황과 결과를 출력하는 역할을 한다.
  */
 public class OutputView {
+    private static final String START_FORM = "[ ";
+    private static final String END_FORM = " ]";
+    private static final String BOUNDARY_LINE = " | ";
+    private static final String UP = "U";
+    private static final String DOWN = "D";
+    private static final String CORRECT = "O";
+    private static final String WRONG = "X";
+    private static final String SPACE = " ";
     private StringBuilder upState = new StringBuilder();
     private StringBuilder downState = new StringBuilder();
 
@@ -26,12 +34,10 @@ public class OutputView {
         System.out.println("게임을 다시 시도할지 여부를 입력해주세여.(재시도: R, 종료: Q)");
     }
 
-
     public void printResultMap() {
-        System.out.println("[ " + upState + " ]");
-        System.out.println("[ " + downState + " ]");
+        System.out.println(START_FORM + upState + END_FORM);
+        System.out.println(START_FORM + downState + END_FORM);
     }
-
     /**
      * 현재까지 이동한 다리의 상태를 정해진 형식에 맞춰 출력한다.
      * <p>
@@ -39,41 +45,40 @@ public class OutputView {
      */
     public void printMap(int index, boolean check, String moveUpOrDown) {
         if (index >= 1) {
-            upState.append(" | ");
-            downState.append(" | ");
+            upState.append(BOUNDARY_LINE);
+            downState.append(BOUNDARY_LINE);
         }
         checkUpOrDown(moveUpOrDown, check);
         printResultMap();
     }
 
     private void checkUpOrDown(String UpAndDown, boolean check) {
-        if (UpAndDown.equals("U")) {
+        if (UpAndDown.equals(UP)) {
             setUpState(check);
         }
-        if (UpAndDown.equals("D")) {
+        if (UpAndDown.equals(DOWN)) {
             setDownState(check);
         }
     }
 
-
     private void setUpState(boolean check) {
         if (check) {
-            upState.append("O");
+            upState.append(CORRECT);
         }
         if (!check) {
-            upState.append("X");
+            upState.append(WRONG);
         }
-        downState.append(" ");
+        downState.append(SPACE);
     }
 
     private void setDownState(boolean check) {
         if (check) {
-            downState.append("O");
+            downState.append(CORRECT);
         }
         if (!check) {
-            downState.append("X");
+            downState.append(WRONG);
         }
-        upState.append(" ");
+        upState.append(SPACE);
     }
 
     public void clearMap() {
