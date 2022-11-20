1cb1586bbfd0b3830d9753a9f39f18ba5a9ca906
hojunking96 <junsong96@naver.com>
2022-11-20 21:00:59 +0900

    feats(StartGame.java) : 게임 진행하는 클래스 추가, 코드 분리

---
+++ src/main/java/bridge/StartGame.java
@@ -0,0 +1,47 @@
+package bridge;
+
+import static bridge.Application.movingInput;
+
+public class StartGame {
+
+    public static boolean startGame() {
+        OutputView.printSelect();
+        movingInput = InputView.readMoving();
+        BridgeGame.move();
+        OutputView.printMap();
+        if (BridgeGame.isWrong()) {
+            if (wantRetry()) {
+                BridgeGame.retry();
+                return true;
+            }
+            //게임 종료
+            return false;
+        }
+        return true;
+    }
+
+    public static void init() {
+        OutputView.printStart();
+        selectBridgeSize();
+        makeBridge();
+    }
+
+    public static void selectBridgeSize() {
+        OutputView.printRequestSize();
+        Application.size = InputView.readBridgeSize();
+    }
+
+    public static void makeBridge() {
+        BridgeNumberGenerator bridgeNumberGenerator = new BridgeRandomNumberGenerator();
+        BridgeMaker bridgeMaker = new BridgeMaker(bridgeNumberGenerator);
+        Application.bridge = bridgeMaker.makeBridge(Application.size);
+    }
+
+    public static boolean wantRetry() {
+        OutputView.printRetry();
+        String retryOrNot = InputView.readGameCommand();
+        if (retryOrNot.equals("R"))
+            return true;
+        return false;
+    }
+}
