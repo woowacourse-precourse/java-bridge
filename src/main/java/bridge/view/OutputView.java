9e304f9c4c79427e17b15e3a100edede82c45a40
KangHyeon3315 <son2786w@gmail.com>
2022-11-20 18:22:58 +0900

    feat: 플레이어가 이동한 기록을 정해진 형식으로 출력하도록 구현

--- src/main/java/bridge/view/OutputView.java
+++ src/main/java/bridge/view/OutputView.java
@@ -1,12 +1,21 @@
 package bridge.view;
 
 import bridge.model.OutputMessage;
+import bridge.model.UserKeySet;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * 사용자에게 게임 진행 상황과 결과를 출력하는 역할을 한다.
  */
 public class OutputView {
 
+    private static final String LINE_FORMAT = "[ %s ]\n";
+    private static final String CELL_SEPARATOR = " | ";
+    private static final String NOT_MOVED = " ";
+    private static final String CORRECT_MOVED = "O";
+    private static final String WRONG_MOVED = "X";
     public void printGameStartMsg() {
         System.out.println(OutputMessage.GAME_START_MSG);
         System.out.println();
@@ -26,11 +35,44 @@ public class OutputView {
 
     /**
      * 현재까지 이동한 다리의 상태를 정해진 형식에 맞춰 출력한다.
-     * <p>
-     * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
+     *
+     * @param bridge 다리 정보
+     * @param movements 플레이어가 이동한 기록
      */
-    public void printMap() {
+    public void printMap(List<String> bridge, List<String> movements) {
+        for(String line : List.of(UserKeySet.UP.toString(), UserKeySet.DOWN.toString())) {
+            printMapLine(bridge, movements, line);
+        }
     }
+
+    private void printMapLine(List<String> bridge, List<String> movements, String line) {
+        String mapLine = getMapLine(bridge, movements, line);
+        System.out.printf(LINE_FORMAT, mapLine);
+    }
+
+    private String getMapLine(List<String> bridge, List<String> movements, String line) {
+        List<String> mapLine = new ArrayList<>();
+        for(int i = 0; i < movements.size(); i++) {
+            String answer = bridge.get(i);
+            String movement = movements.get(i);
+
+            mapLine.add(getMapCell(line,answer, movement));
+        }
+        return String.join(CELL_SEPARATOR, mapLine);
+    }
+
+    private String getMapCell(String line, String answer, String movement) {
+        if(!movement.equals(line)) {
+            return NOT_MOVED;
+        }
+
+        if(!answer.equals(movement)) {
+            return WRONG_MOVED;
+        }
+
+        return CORRECT_MOVED;
+    }
+
 
     /**
      * 게임의 최종 결과를 정해진 형식에 맞춰 출력한다.
