8ae4b482d43bb183094e7dcd2dd14c5549674f4d
KYR <freekyr7529@gmail.com>
2022-11-21 05:43:00 +0900

    feature : Command Enum 확장
    Command Enum의 항목에 해당 명령에 대한 설명을 추가

--- src/main/java/bridge/constant/Command.java
+++ src/main/java/bridge/constant/Command.java
@@ -2,14 +2,22 @@ package bridge.constant;
 
 public enum Command {
 
-    DO_RETRY("R"),
-    DO_QUIT("Q");
+    DO_RETRY("R", "재시도"),
+    DO_QUIT("Q", "종료");
 
     private final String command;
+    private final String detail;
 
-    Command(String command) { this.command = command; }
+    Command(String command, String detail) {
+        this.command = command;
+        this.detail = detail;
+    }
 
     public String getCommand() {
         return command;
+    }
+
+    public String getDetail() {
+        return detail;
     }
 }
