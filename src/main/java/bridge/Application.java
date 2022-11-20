83cbace389a2351f8f1daf4570b2ad78256d4475
zeunala <zeunala@naver.com>
2022-11-20 22:31:27 +0900

    feat: 다리 길이 입력받는 기능 추가

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -1,8 +1,12 @@
 package bridge;
 
+import bridge.view.InputView;
+
 public class Application {
 
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        InputView inputView = new InputView();
+        int bridgeSize = inputView.readBridgeSize();
     }
 }
---
+++ src/main/java/bridge/view/InputView.java
@@ -0,0 +1,63 @@
+package bridge.view;
+
+import camp.nextstep.edu.missionutils.Console;
+
+/**
+ * 사용자로부터 입력을 받는 역할을 한다.
+ */
+public class InputView {
+    public static final String GAME_START_MESSAGE = "다리 건너기 게임을 시작합니다.\n";
+    public static final String BRIDGE_SIZE_INPUT_MESSAGE = "다리의 길이를 입력해주세요.";
+
+    private void printBridgeSizeMessage() {
+        System.out.println(GAME_START_MESSAGE);
+        System.out.println(BRIDGE_SIZE_INPUT_MESSAGE);
+    }
+
+    private int readNumber() throws IllegalArgumentException {
+        String inputNumber = Console.readLine();
+        System.out.println();
+        try {
+            return Integer.parseInt(inputNumber);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("[ERROR] 다리 길이는 3부터 20 사이의 숫자여야 합니다.");
+        }
+    }
+
+    private int readValidRangeNumber() throws IllegalArgumentException {
+        int number = readNumber();
+
+        if (number < 3 || number > 20) {
+            throw new IllegalArgumentException("[ERROR] 다리 길이는 3부터 20 사이의 숫자여야 합니다.");
+        }
+        return number;
+    }
+
+    /**
+     * 다리의 길이를 입력받는다.
+     */
+    public int readBridgeSize() {
+        while (true) {
+            try {
+                printBridgeSizeMessage();
+                return readValidRangeNumber();
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            }
+        }
+    }
+
+    /**
+     * 사용자가 이동할 칸을 입력받는다.
+     */
+    public String readMoving() {
+        return null;
+    }
+
+    /**
+     * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
+     */
+    public String readGameCommand() {
+        return null;
+    }
+}
