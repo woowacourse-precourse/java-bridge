bf7b4b380abb3b0f0defac57ca8259a466124fda
KYR <freekyr7529@gmail.com>
2022-11-21 01:52:50 +0900

    refactor : 메서드 이
    run 메서드를 GameController 클래스로 이동

--- src/main/java/bridge/BridgeGame.java
+++ src/main/java/bridge/BridgeGame.java
@@ -2,23 +2,13 @@ package bridge;
 
 import bridge.view.InputView;
 import bridge.view.OutputView;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * 다리 건너기 게임을 관리하는 클래스
  */
 public class BridgeGame {
-    private final InputView inputView = new InputView();
-    private final OutputView outputView = new OutputView();
-    private final BridgeRandomNumberGenerator generator = new BridgeRandomNumberGenerator();
-    private final BridgeMaker bridgeMaker = new BridgeMaker(generator);
-    public void run() {
-        try {
-            outputView.startMsg();
-            bridgeMaker.makeBridge(inputView.readBridgeSize());
-        } catch (IllegalArgumentException exception) {
-            OutputView.printMsg("[ERROR] " + exception.getMessage());
-        }
-    }
     /**
      * 사용자가 칸을 이동할 때 사용하는 메서드
      * <p>
