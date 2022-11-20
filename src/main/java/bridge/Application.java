20678a4e3d44cbd547cdcd9d589d722a1a1e8e63
Wooseobee <dntjq0807@naver.com>
2022-11-21 00:34:00 +0900

    feat: 다리 건너기 게임 구현 - 게임 진행 문구 구현

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -1,8 +1,28 @@
 package bridge;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
 
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        int n;
+        boolean gameOver = false;
+
+        System.out.println("다리 건너기 게임을 시작합니다.\n");
+        System.out.println("다리의 길이를 입력해주세요.");
+        n = Integer.parseInt(Console.readLine());
+        System.out.println();
+
+        do {
+            System.out.println("이동할 칸을 선택해주세요. (위: U, 아래: D)");
+
+            if (gameOver) {
+                System.out.println("\n게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)");
+            }
+
+        } while(BridgeGame.retry());
+
+        System.out.println("게임 성공 여부: ");
+        System.out.println("총 시도한 횟수: ");
     }
 }
