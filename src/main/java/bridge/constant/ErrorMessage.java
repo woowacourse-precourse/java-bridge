710bf822a8b9d63693f34d84b4dc5f496cb9b814
leesangjin <tkdwls0628@gmail.com>
2022-11-21 00:19:17 +0900

    feat(ErrorMessage): 다리 길이와 관련된 에러 메시지 정의

---
+++ src/main/java/bridge/constant/ErrorMessage.java
@@ -0,0 +1,9 @@
+package bridge.constant;
+
+public class ErrorMessage {
+
+    public static final String ERROR = "[ERROR] ";
+
+    public static final String WRONG_BRIDGE_SIZE = ERROR + "다리의 길이는 3이상 20이하의 정수 입니다.";
+    public static final String WRONG_BRIDGE_SIZE_TYPE = ERROR + "다리의 길이는 정수값이여야 합니다.";
+}
