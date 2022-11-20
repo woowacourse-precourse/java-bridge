191ac2afe9fbc3b480d4cf1c22fb8f9f65b7079b
kimminsu <xjfcnfw3@naver.com>
2022-11-20 14:14:07 +0900

    feat: bridgeGame 플레이어가 이동하는 기능 구현

--- src/main/java/bridge/BridgeGame.java
+++ src/main/java/bridge/BridgeGame.java
@@ -1,16 +1,35 @@
 package bridge;
 
+import bridge.domain.BridgeController;
+import bridge.result.Result;
+
 /**
  * 다리 건너기 게임을 관리하는 클래스
  */
 public class BridgeGame {
+
+    private final BridgeController bridgeController;
+    private Integer gameCount;
+
+    public BridgeGame() {
+        this.bridgeController = new BridgeController(new BridgeRandomNumberGenerator());
+        this.gameCount = 0;
+    }
 
     /**
      * 사용자가 칸을 이동할 때 사용하는 메서드
      * <p>
      * 이동을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void move() {
+    public Result move() {
+        boolean moving = true;
+        Result result = Result.getEmptyResult();
+        bridgeController.restartGame();
+        while (moving) {
+            result = bridgeController.inputPlayerMove();
+            moving = !result.getGameStatus().isStopGame();
+        }
+        return result;
     }
 
     /**
