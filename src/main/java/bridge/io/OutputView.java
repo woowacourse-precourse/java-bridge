e91bb3c13f33a02bb7f87b8199886bebe499a66a
hongjk7777 <51441261+hongjk7777@users.noreply.github.com>
2022-11-20 15:45:49 +0900

    refactor: 클래스 분리

--- src/main/java/bridge/io/OutputView.java
+++ src/main/java/bridge/io/OutputView.java
@@ -8,6 +8,7 @@ import java.util.List;
  * 사용자에게 게임 진행 상황과 결과를 출력하는 역할을 한다.
  */
 public class OutputView {
+    private final TileView tileView = new TileView();
 
     /**
      * 현재까지 이동한 다리의 상태를 정해진 형식에 맞춰 출력한다.
@@ -21,13 +22,13 @@ public class OutputView {
 
     private void printUpperMap(List<String> bridge, List<String> userMoving) {
         printLeftWall();
-        printTiles(bridge, userMoving, MovingType.UP_MOVING.getValue());
+        tileView.printTiles(bridge, userMoving, MovingType.UP_MOVING.getValue());
         printRightWall();
     }
 
     private void printLowerMap(List<String> bridge, List<String> userMoving) {
         printLeftWall();
-        printTiles(bridge, userMoving, MovingType.DOWN_MOVING.getValue());
+        tileView.printTiles(bridge, userMoving, MovingType.DOWN_MOVING.getValue());
         printRightWall();
     }
 
@@ -39,47 +40,7 @@ public class OutputView {
         System.out.println("]");
     }
 
-    private void printTiles(List<String> bridge, List<String> userMoving, String direction) {
-        for (int curTile = 0; curTile < userMoving.size(); curTile++) {
-            printTile(bridge, userMoving, direction, curTile);
-            printSeparator(userMoving, curTile);
-        }
-    }
 
-    private static void printTile(List<String> bridge, List<String> userMoving, String direction, int curTile) {
-        System.out.print(" ");
-        printMark(bridge.get(curTile), userMoving.get(curTile), direction);
-        System.out.print(" ");
-    }
-
-    private static void printMark(String answer, String userInput, String direction) {
-        if (isIncorrectInput(answer, userInput, direction)) {
-            System.out.print("X");
-            return;
-        } else if (isCorrectInput(answer, userInput, direction)) {
-            System.out.print("O");
-            return;
-        }
-        System.out.print(" ");
-    }
-
-    private static boolean isIncorrectInput(String answer, String userInput, String direction) {
-        return !answer.equals(userInput) && !answer.equals(direction);
-    }
-
-    private static boolean isCorrectInput(String answer, String userInput, String direction) {
-        return answer.equals(userInput) && answer.equals(direction);
-    }
-
-    private static void printSeparator(List<String> userMoving, int curTile) {
-        if (isLastTile(curTile, userMoving.size())) {
-            System.out.print("|");
-        }
-    }
-
-    private static boolean isLastTile(int tile, int tileSize) {
-        return tile != (tileSize - 1);
-    }
 
     /**
      * 게임의 최종 결과를 정해진 형식에 맞춰 출력한다.
