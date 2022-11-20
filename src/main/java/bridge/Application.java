d5a5c7531cb94007926633fd8f15d964b3ea48f7
Kwak-JunYoung <klop9090@g.skku.edu>
2022-11-21 02:38:06 +0900

    feat(Application/main) Print game start message.

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -5,5 +5,6 @@ public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
         System.out.println("다리 건너기 게임을 시작합니다.");
+
     }
 }
--- src/main/java/bridge/InputView.java
+++ src/main/java/bridge/InputView.java
@@ -11,6 +11,7 @@ public class InputView {
      * 다리의 길이를 입력받는다.
      */
     public int readBridgeSize() {
+        System.out.println("다리의 길이를 입력해주세요.");
         int bridgeSize = Integer.parseInt(readLine());
         if ((bridgeSize < 3) || (bridgeSize > 20))
             throw new IllegalArgumentException("[ERROR] 다리 길이는 3부터 20 사이의 숫자여야 합니다.");
