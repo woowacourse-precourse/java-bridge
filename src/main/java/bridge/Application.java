128dba5e66405e7551ce4f2ee29da25eb69342f8
lcw3176 <lcw3176@naver.com>
2022-11-20 19:32:32 +0900

    feat(Application): 기능 작동 구현

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -1,8 +1,22 @@
 package bridge;
 
+
+import bridge.step.Step;
+import bridge.step.StepFactory;
+import bridge.step.StepFactoryType;
+import bridge.step.StepLauncher;
+import java.util.ArrayList;
+
 public class Application {
 
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        StepLauncher stepLauncher = new StepLauncher(new ArrayList<>());
+        Step init = StepFactory.makeStep(StepFactoryType.INIT);
+        Step game = StepFactory.makeStep(StepFactoryType.GAME);
+
+        stepLauncher.addStep(init);
+        stepLauncher.addStep(game);
+
+        stepLauncher.run();
     }
 }
