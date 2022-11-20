bb5406a345ad85311cf48cc1cd58c0187c7779dc
bellljun3395 <195850@jnu.ac.kr>
2022-11-20 18:26:18 +0900

    feat(Bridge): 생성 객체

---
+++ src/main/java/bridge/structure/Bridge.java
@@ -0,0 +1,17 @@
+package bridge.structure;
+
+
+import java.util.List;
+
+public class Bridge {
+    private final List<String> bridge;
+
+    public Bridge(List<String> bridge) {
+        this.bridge = bridge;
+    }
+
+    public List<String> getBridge() {
+        return bridge;
+    }
+
+}
