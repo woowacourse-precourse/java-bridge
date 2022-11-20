d12a0260510668582204293d8914d18be2130eb7
ddogga <hayeonlee60327@gmail.com>
2022-11-20 19:52:21 +0900

    게임의 전체적 루틴을 routine 메서드로 분리, main메서드에서 NewGame 객체 생성 및 사용

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -7,8 +7,10 @@ public class Application{
     public static void main(String[] args) {
         // TODO: 프로그램 구현
 
+        BridgeGame bridgeGame = new BridgeGame();
+        NewGame newGame = new NewGame(bridgeGame);
 
-
+        newGame.startNewGame();
 
 
     }
--- src/main/java/bridge/NewGame.java
+++ src/main/java/bridge/NewGame.java
@@ -1,20 +1,26 @@
 package bridge;
 
-public class newGame {
+public class NewGame {
+
+    private final BridgeGame bridgeGame;
+
+    public NewGame(BridgeGame bridgeGame) {
+        this.bridgeGame = bridgeGame;
+    }
 
     public void startNewGame() {
         System.out.println("다리 건너기 게임을 시작합니다.");
-        BridgeGame bridgeGame = new BridgeGame();
-        String gameResult = "F";
         int tryCount = 0;
+        routine(tryCount);
+    }
+
+    private void routine(int tryCount) {
+        String gameResult ="F";
         while (true){
             tryCount += 1;
             gameResult = bridgeGame.play();
-            if (!gameResult.equals("R")) {
-                break;
-            }
+            if (!gameResult.equals("R")) {break;}
         }
         bridgeGame.finish(gameResult,tryCount);
     }
-
 }
