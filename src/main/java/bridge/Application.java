9b8450532e48d567a68679d06b949e0a05ba9394
alstjr706 <alstjr706@naver.com>
2022-11-20 09:07:24 +0900

    refactor : (다리 건너기) 다리 게임 머신 생성자 주입

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -1,11 +1,14 @@
 package bridge;
 
-import bridge.domain.BridgeGameMachine;
+import bridge.controller.BridgeGameMachine;
+import bridge.view.InputView;
+import bridge.view.OutputView;
 
 public class Application {
 
     public static void main(String[] args) {
-        BridgeGameMachine bridgeGameMachine = new BridgeGameMachine();
+        BridgeGameMachine bridgeGameMachine = new BridgeGameMachine(
+                new InputView(), new OutputView());
         bridgeGameMachine.run();
     }
 }
