a80246d2bed2dd26e7b3d72d1b1b0adcaed09809
realslimtaek <89964264+realslimtaek@users.noreply.github.com>
2022-11-20 15:54:36 +0900

    refactor : 코드 간소화 및 정리

--- src/main/java/bridge/InputView.java
+++ src/main/java/bridge/InputView.java
@@ -2,13 +2,19 @@ package bridge;
 
 import camp.nextstep.edu.missionutils.Console;
 
+import java.io.BufferedWriter;
+import java.io.OutputStreamWriter;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
  */
 public class InputView {
+
+    final static BridgeGame bridgeGame = new BridgeGame();
+    final static OutputView outputView = new OutputView();
 
     static int size = 1;
     static List<String> move = new ArrayList<>();
@@ -33,9 +39,6 @@ public class InputView {
         return answer;
     }
 
-    /**
-     * 사용자가 이동할 칸을 입력받는다.
-     */
     public void readMoving() {
         System.out.println("이동할 칸을 선택해주세요. (위: U, 아래: D)");
         try {
@@ -56,11 +59,21 @@ public class InputView {
         return answer;
     }
 
-    /**
-     * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
-     */
-    public String readGameCommand() {
-        return null;
+    public static void readGameCommand() {
+        OutputView.now_size = OutputView.bridge_answer.size();
+        System.out.println("게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)");
+        String a = Console.readLine();
+        if(Objects.equals(a, "R")){
+            ++BridgeGame.play_count;
+            OutputView.now_size = 0;
+            Application.playBridgeGame();
+        }
+        if(Objects.equals(a,"Q")){
+            System.out.println("최종 게임 결과");
+            outputView.printResult();
+            System.out.println("\n 게임 성공 여부 : 실패");
+            System.out.println("총 시도한 횟수 : " + BridgeGame.play_count);
+        }
     }
 
     public int getSize() {
