62a8786ff05a1833d41220225706755679e87e2c
ykmxxi <ykmxxi97@gmail.com>
2022-11-20 11:34:03 +0900

    feat: 재시작 여부 입력 예외 처리기능[대문자 R, Q가 아닌 경우] 구현

--- src/main/java/bridge/validation/GameCommandValidation.java
+++ src/main/java/bridge/validation/GameCommandValidation.java
@@ -1,9 +1,12 @@
 package bridge.validation;
 
 import bridge.validation.enums.ErrorMessage;
+import bridge.validation.enums.RegEx;
+import java.util.regex.Pattern;
 
 public class GameCommandValidation {
     private ErrorMessage message;
+    private RegEx regEx;
 
     public GameCommandValidation() {
     }
@@ -21,11 +24,16 @@ public class GameCommandValidation {
 
     public void isBlank(String gameCommand) {
         if (gameCommand.isBlank() && gameCommand.length() != 0) {
-            message = ErrorMessage.valueOf("MOVING_BLANK_EXCEPTION");
+            message = ErrorMessage.valueOf("COMMAND_BLANK_EXCEPTION");
             throwError(message.getMessage());
         }
     }
 
     public void isUpperCaseRQ(String gameCommand) {
+        regEx = RegEx.valueOf("COMMAND_TYPE_REG_EX");
+        if (!Pattern.matches(regEx.getRegEx(), gameCommand)) {
+            message = ErrorMessage.valueOf("COMMAND_TYPE_EXCEPTION");
+            throwError(message.getMessage());
+        }
     }
 }
--- src/main/java/bridge/validation/enums/RegEx.java
+++ src/main/java/bridge/validation/enums/RegEx.java
@@ -3,7 +3,8 @@ package bridge.validation.enums;
 public enum RegEx {
     NUMBER_REG_EX("^[0-9]*$"),
     BRIDGE_SIZE_REG_EX("^[3-9]{1}$|^1{1}[0-9]{1}$|^20$"),
-    MOVING_TYPE_REG_EX("^U$|^D$");
+    MOVING_TYPE_REG_EX("^U$|^D$"),
+    COMMAND_TYPE_REG_EX("^R$|^Q$");
 
     private final String regEx;
 
