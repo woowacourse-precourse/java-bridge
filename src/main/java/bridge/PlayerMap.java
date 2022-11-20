7f630b23d66860ee5385cf0ef2e628ac07c0029b
안광민 <creativeduck0518@gmail.com>
2022-11-21 01:47:55 +0900

    feat(PlayerMap): 사용자 맵 초기화 기능 구현

--- src/main/java/bridge/PlayerMap.java
+++ src/main/java/bridge/PlayerMap.java
@@ -1,7 +1,16 @@
 package bridge;
 
+import static bridge.data.GameMark.BLANK;
+
 public class PlayerMap {
     private final int bridgeVerticalSize = 2;
     private final String[][] playerMap;
 
+    public void initPlayerMap(int bridgeSize) {
+        for (int i = 0; i < 2; i++) {
+            for (int j = 0; j < bridgeSize; j++) {
+                playerMap[i][j] = BLANK.getMark();
+            }
+        }
+    }
 }
