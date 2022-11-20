d3294b94de7736202f4dd95bc4e3c5f2bab0aab7
Gyu-Bin-Yoon <gyubinyoon99@gmail.com>
2022-11-21 01:58:28 +0900

    feat(BridgeGame) : add a function that compares selected bridge with answer bridge

--- src/main/java/bridge/BridgeGame.java
+++ src/main/java/bridge/BridgeGame.java
@@ -1,9 +1,14 @@
 package bridge;
 
+import java.util.List;
+
 /**
  * 다리 건너기 게임을 관리하는 클래스
  */
 public class BridgeGame {
+
+    private final String PASS = "O";
+    private final String FAIL = "X";
 
     /**
      * 사용자가 칸을 이동할 때 사용하는 메서드
@@ -19,5 +24,13 @@ public class BridgeGame {
      * 재시작을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
     public void retry() {
+    }
+
+    public String checkAnswer(List<String> bridge, String selectedBridge, int round) {
+        String answer = bridge.get(round - 1);
+        if (answer.equals(selectedBridge)) {
+            return PASS;
+        }
+        return FAIL;
     }
 }
