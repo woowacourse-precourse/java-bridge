d4f8d2272dd475e2f33dda68c72a63a5b6c1d748
haisley77 <qhfg3145@naver.com>
2022-11-20 20:40:02 +0900

    feat(printResult): 최종 게임 결과 출력 기능

--- src/main/java/bridge/OutputView.java
+++ src/main/java/bridge/OutputView.java
@@ -1,5 +1,7 @@
 package bridge;
 
+import java.util.List;
+
 /**
  * 사용자에게 게임 진행 상황과 결과를 출력하는 역할을 한다.
  */
@@ -13,7 +15,23 @@ public class OutputView {
      * <p>
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void printMap() {
+    public void printMap(List<String> up_bridge, List<String> down_bridge) {
+        System.out.print("[");
+        for (int i = 0;i<BridgeMaker.bridge_index;i++){
+            System.out.print(up_bridge.get(i));
+            if (i!=BridgeMaker.bridge_index-1){
+                System.out.print("|");
+            }
+        }
+        System.out.println("]");
+        System.out.print("[");
+        for (int i = 0;i<BridgeMaker.bridge_index;i++){
+            System.out.print(down_bridge.get(i));
+            if (i!=BridgeMaker.bridge_index-1){
+                System.out.print("|");
+            }
+        }
+        System.out.println("]");
     }
 
     /**
@@ -21,6 +39,17 @@ public class OutputView {
      * <p>
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void printResult() {
+    public void printResult(List<String> up_bridge, List<String> down_bridge, int result) {
+        System.out.println("최종 게임 결과");
+        printMap(up_bridge,down_bridge);
+
+        if (result == 1){
+            System.out.println("\n게임 성공 여부: 성공");
+        }
+        if (result == 0){
+            System.out.println("\n게임 성공 여부: 실패");
+        }
+        System.out.println("총 시도한 횟수: "+BridgeGame.try_count);
+
     }
 }
