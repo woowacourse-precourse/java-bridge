bac8ce8126768971b8fcd7b57f17f45789631a87
ckn1070 <ckn1070@naver.com>
2022-11-20 20:12:11 +0900

    feat(bridge): add up and down commands to enum

--- src/main/java/bridge/BridgeGame.java
+++ src/main/java/bridge/BridgeGame.java
@@ -50,7 +50,7 @@ public class BridgeGame {
      * 재시작을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
     public boolean retry(String userInput) {
-        if (userInput.equals(Retry.QUIT.get())) {
+        if (userInput.equals(Command.QUIT.get())) {
             return false;
         }
         return true;
@@ -74,13 +74,15 @@ enum MoveStatus {
     }
 }
 
-enum Retry {
+enum Command {
+    UP("U"),
+    DOWN("D"),
     RETRY("R"),
     QUIT("Q");
 
     private String condition;
 
-    Retry(String condition) {
+    Command(String condition) {
         this.condition = condition;
     }
 
