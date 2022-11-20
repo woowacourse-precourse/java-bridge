8fc1e7b0650bb19200f79964032ce25c7d069c4c
yoonjuho <dbswn990@naver.com>
2022-11-20 19:10:26 +0900

    feat(BridgeController): 게임 실패 시 특정 문자열 입력을 받는다

--- src/main/java/bridge/controller/BridgeController.java
+++ src/main/java/bridge/controller/BridgeController.java
@@ -3,6 +3,8 @@ package bridge.controller;
 import bridge.model.Row;
 
 public class BridgeController {
+    private static final String RETRY = "R";
+    private static final String END = "Q";
 
     void validateNumeric(String input){
         if(!input.matches("^[0-9]")){
@@ -10,6 +12,11 @@ public class BridgeController {
         }}
     void validateLetter(String input){
         if(!Row.isValidateLetter(input)){
+            throw new IllegalArgumentException();
+        }
+    }
+    void validateEndLetter(String input){
+        if(!input.equals(RETRY) || !input.equals(END)){
             throw new IllegalArgumentException();
         }
     }
