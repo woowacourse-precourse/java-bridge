e5aa5f4db60c7e22543d2e8254c89356cb4ba36d
yenawee <yaena0319@naver.com>
2022-11-20 16:40:55 +0900

    feat : 다리 정보 생성. 건널수 있는 칸은 무작위값

--- src/main/java/bridge/BridgeMaker.java
+++ src/main/java/bridge/BridgeMaker.java
@@ -1,5 +1,6 @@
 package bridge;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -18,6 +19,12 @@ public class BridgeMaker {
      * @return 입력받은 길이에 해당하는 다리 모양. 위 칸이면 "U", 아래 칸이면 "D"로 표현해야 한다.
      */
     public List<String> makeBridge(int size) {
-        return null;
+        String[] step = {"D", "U"};
+        List<String> bridgeAnswer = new ArrayList<>();
+        while (size-- > 0){
+            int number = bridgeNumberGenerator.generate();
+            bridgeAnswer.add(step[number]);
+        }
+        return (bridgeAnswer);
     }
 }
