8ac1c63f417fe70571173861db95f34854e0f201
olrlobt <317tmdgjs@naver.com>
2022-11-20 19:12:31 +0900

    feat: 다리 길이로 숫자가 아닌 값이 입력되면, 예외처리

--- src/main/java/bridge/util/Utils.java
+++ src/main/java/bridge/util/Utils.java
@@ -3,6 +3,10 @@ package bridge.util;
 public class Utils {
 
     public static int inputToNumber(String input){
-        return Integer.parseInt(input);
+        try {
+            return Integer.parseInt(input);
+        }catch (NumberFormatException e) {
+            throw new IllegalArgumentException();
+        }
     }
 }
