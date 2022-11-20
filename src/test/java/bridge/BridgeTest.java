b3d605486a372be96b9551a972bb38d962a064a9
ho2n <jungho2n@gmail.com>
2022-11-20 21:49:19 +0900

    test(BridgeTest): 끝까지 건널 때까지 혹은 건널 수 없는 칸을 입력받을 때까지 입력받고 이동한 결과를 출력하는지 테스트하는 코드를 추가

--- src/test/java/bridge/BridgeTest.java
+++ src/test/java/bridge/BridgeTest.java
@@ -1,5 +1,6 @@
 package bridge;
 
+import static org.assertj.core.api.Assertions.as;
 import static org.assertj.core.util.Lists.newArrayList;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.assertj.core.api.Assertions.assertThat;
@@ -20,18 +21,46 @@ class BridgeTest extends NsTest {
     private Bridge bridge;
     @BeforeEach
     void setUp() {
-        List<String> elements = newArrayList("U");
+        List<String> elements = newArrayList("U", "D", "U");
         bridge = new Bridge(elements);
     }
 
     @ParameterizedTest
     @CsvSource(value = {"U -> [ O ],[   ]", "D -> [   ],[ X ]"}, delimiterString = " -> ")
     @Test
-    void move(String input, String expected) {
+    void 한개의_입력값에따른_결과출력_테스트(String input, String expected) {
         assertSimpleTest(() -> {
             run(input);
             assertThat(output()).contains(
                     expected.split(",")
+            );
+        });
+    }
+
+    @Test
+    void 끝까지_건널때까지_입력받고_이동한결과를_출력하는지_테스트() {
+        assertSimpleTest(() -> {
+            run("U", "D", "U");
+            assertThat(output()).contains(
+                    "[ O ]",
+                    "[   ]",
+                    "[ O |   ]",
+                    "[   | O ]",
+                    "[ O |   | O ]",
+                    "[   | O |   ]"
+            );
+        });
+    }
+
+    @Test
+    void 건널수없는칸을_입력받을때까지_입력받고_이동한결과를_출력하는지_테스트() {
+        assertSimpleTest(() -> {
+            run("U", "U");
+            assertThat(output()).contains(
+                    "[ O ]",
+                    "[   ]",
+                    "[ O | X ]",
+                    "[   |   ]"
             );
         });
     }
