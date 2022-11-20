4bd0d5bcf58cadcd23ac0e2a90516cf8c804d7fb
kimtaesoo99 <107785279+kimtaesoo99@users.noreply.github.com>
2022-11-20 19:41:26 +0900

    test: (BridgeGameTest) 유저가 잘 이동되는지 테스트

---
+++ src/test/java/bridge/service/BridgeGameTest.java
@@ -0,0 +1,28 @@
+package bridge.service;
+
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import bridge.domain.Bridge;
+import bridge.domain.User;
+import java.util.List;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class BridgeGameTest {
+    private final BridgeGame bridgeGame = new BridgeGame();
+
+    @DisplayName("유저가 잘 이동되는지 테스트")
+    @Test
+    public void moveTest() {
+        //given
+        Bridge bridge = new Bridge(List.of("U", "D", "D"));
+        User user = new User();
+
+        //when
+        bridgeGame.move(bridge, user);
+
+        //then
+        assertThat(bridge.getLocation()).isEqualTo(1);
+    }
+}
