3eb968ee77afe9ec5798c0240c940a3f6004360a
UngHeung <t01047377477@gmail.com>
2022-11-20 22:06:50 +0900

    feat : (Validate) 다리 크기 입력 값이 3 이상 20 이하의 숫자인지 확인하는 기능 추가

--- src/main/java/domain/Validate.java
+++ src/main/java/domain/Validate.java
@@ -3,4 +3,10 @@ package domain;
 public class Validate {
 	private String errorMessage;
 	
+	public void validateSize(int size) {
+		if(size < 3 || 20 < size) {
+			errorMessage = ErrorMessage.WRONG_SIZE.getErrorMessage();
+			throw new IllegalArgumentException(errorMessage);
+		}
+	}
 }
