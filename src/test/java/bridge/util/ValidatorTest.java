b0287178f1e84bb83e4ad7a8c010f992dacddeae
Min-ji99 <61926751+Min-ji99@users.noreply.github.com>
2022-11-20 18:41:09 +0900

    Test : 추가 입력받은 이동할 칸이 U 또는 D이면 true, 아니면 예외처리가 잘 되는지 확인하는 테스트코드

--- src/test/java/bridge/util/ValidatorTest.java
+++ src/test/java/bridge/util/ValidatorTest.java
@@ -24,10 +24,22 @@ class ValidatorTest {
         assertThat(Validator.checkRange("13")).isTrue();
     }
     @Test
-    @DisplayName("입력한 다리 길이가 3~20사이 일때 true 반환하는지 확인")
-    void falseCheckBridgeRange(){
+    @DisplayName("입력한 다리 길이가 3~20사이 일때 예외처리가 발생하는지 확인")
+    void exceptionCheckBridgeRange(){
         assertThatThrownBy(()->Validator.checkRange("21"))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("[ERROR] 다리의 길이는 3부터 20 사이의 숫자여야 합니다.");
+    }
+    @Test
+    @DisplayName("입력받은 이동할 칸이 U 또는 D인경우 true 반환하는지 확인")
+    void trueCheckMoving(){
+        assertThat(Validator.checkMoving("U")).isTrue();
+    }
+    @Test
+    @DisplayName("입력받은 이동할 칸이 U 또는 D가 아닌 경우 예외처리가 발생하는지 확인")
+    void exceptionCheckMoving(){
+        assertThatThrownBy(()->Validator.checkMoving("I"))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("[ERROR] 이동할 칸은 U 또는 D를 입력해야 합니다.");
     }
 }
