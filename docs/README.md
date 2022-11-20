fc1e57e6639675923aad1b2ccbd10f71fac15c9b
Hyunseok Lee <hyun104467@naver.com>
2022-11-20 20:49:04 +0900

    feat(BridgeMaker): checkUpperBridge

--- docs/README.md
+++ docs/README.md
@@ -51,8 +51,10 @@
   - (3) BridgeMaker
     - makeBridge
       - [] 다리의 길이와 모양에 따른 모양 출력
-      - 예외처리
-        - 올바르지 않은 다리의 길이 입력
+      - checkUpperBridge : 위칸인지 확인 하는 메소드
+    - 예외처리
+      - 올바르지 않은 다리의 길이 입력
+      
 ---
   - (4) BridgeGame
     - move
--- src/main/java/bridge/InputView.java
+++ src/main/java/bridge/InputView.java
@@ -1,12 +1,6 @@
 package bridge;
 
-/**
- * 사용자로부터 입력을 받는 역할을 한다.
- */
 public class InputView {
-    /**
-     * 다리의 길이를 입력받는다.
-     */
     public int readBridgeSize() {
         String bridgeSize;
         while (true) {
@@ -18,9 +12,6 @@ public class InputView {
         return Integer.parseInt(bridgeSize);
     }
 
-    /**
-     * 사용자가 이동할 칸을 입력받는다.
-     */
     public String readMoving() {
         String moving;
         while (true) {
@@ -32,9 +23,6 @@ public class InputView {
         return moving;
     }
 
-    /**
-     * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
-     */
     public String readGameCommand() {
 
         return null;
