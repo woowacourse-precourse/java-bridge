af664479db49607e78f6f90497f289e12c5f07ed
woosung1223 <dntjd991223@naver.com>
2022-11-20 16:07:54 +0900

    refactor(InputView): 메세지 상수화

--- src/main/java/bridge/view/InputView.java
+++ src/main/java/bridge/view/InputView.java
@@ -8,8 +8,14 @@ import bridge.verifier.MovingVerifier;
 import camp.nextstep.edu.missionutils.Console;
 
 public class InputView {
+    private final String READ_BRIDGE_MESSAGE = "다리의 길이를 입력해주세요.";
+    private final String READ_MOVING_MESSAGE =
+            String.format("이동할 칸을 선택해주세요. (위: %s, 아래: %s)\n", GameMoving.UP, GameMoving.DOWN);
+    private final String READ_GAME_COMMAND_MESSAGE =
+            String.format("게임을 다시 시도할지 여부를 입력해주세요. (재시도: %s, 종료: %s)\n", GameControl.RETRY, GameControl.QUIT);
+
     public int readBridgeSize() {
-        System.out.println("다리의 길이를 입력해주세요.");
+        System.out.println(READ_BRIDGE_MESSAGE);
         String bridgeSize = Console.readLine();
 
         BridgeSizeVerifier bridgeSizeVerifier = new BridgeSizeVerifier();
@@ -18,7 +24,7 @@ public class InputView {
     }
 
     public String readMoving() {
-        System.out.printf("이동할 칸을 선택해주세요. (위: %s, 아래: %s)\n", GameMoving.UP, GameMoving.DOWN);
+        System.out.printf(READ_MOVING_MESSAGE);
         String moving = Console.readLine();
 
         MovingVerifier movingVerifier = new MovingVerifier();
@@ -27,7 +33,7 @@ public class InputView {
     }
 
     public String readGameCommand() {
-        System.out.printf("게임을 다시 시도할지 여부를 입력해주세요. (재시도: %s, 종료: %s)\n", GameControl.RETRY, GameControl.QUIT);
+        System.out.printf(READ_GAME_COMMAND_MESSAGE);
         String gameCommand = Console.readLine();
 
         GameCommandVerifier gameCommandVerifier = new GameCommandVerifier();
