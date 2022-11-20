5ecbcdba42e847d6f1b3f0eee44f8b9e9c354896
bellljun3395 <195850@jnu.ac.kr>
2022-11-20 18:38:28 +0900

    feat(BridgeGame): 구현 move

--- src/main/java/bridge/game/bridgeGame/BridgeGame.java
+++ src/main/java/bridge/game/bridgeGame/BridgeGame.java
@@ -10,6 +10,8 @@ import bridge.structure.BridgeMoveValue;
  */
 public class BridgeGame {
     private final int INITIAL_TURN_COUNT = 1;
+    private final int ADJUST_TURN_COUNT = -1;
+
 
     private final Bridge bridge;
     private int turnCount;
@@ -30,4 +32,34 @@ public class BridgeGame {
     public BridgeGameResults getBridgeResults() {
         return bridgeResults;
     }
+
+    /**
+     * 사용자가 칸을 이동할 때 사용하는 메서드
+     * <p>
+     * 이동을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
+     */
+    public void move(BridgeMoveValue bridgeMoveValue) {
+        if (bridge.canGo(bridgeMoveValue, this.turnCount + ADJUST_TURN_COUNT)) {
+            addBridgeResult(bridgeMoveValue, true);
+            isMoveAll();
+            return;
+        }
+
+        addBridgeResult(bridgeMoveValue, false);
+        this.bridgeGameStatus = BridgeGameStatus.FALL;
+    }
+
+    private void addBridgeResult(BridgeMoveValue bridgeMoveValue, boolean pass) {
+        this.bridgeResults.add(new BridgeGameResult(bridgeMoveValue, pass));
+    }
+
+    private void isMoveAll() {
+        if (this.bridge.getBridge()
+                .size() == this.turnCount) {
+            this.bridgeGameStatus = BridgeGameStatus.CORRECT;
+            return;
+        }
+        this.turnCount++;
+    }
+
 }
