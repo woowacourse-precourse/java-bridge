1d696a9fd150be7ca681ccfa46a59bb99a675f2b
alstjr706 <alstjr706@naver.com>
2022-11-20 15:53:27 +0900

    feat : (다리 건너기) 다리 길이 입력 검증 예외처리 로직 작성

--- src/main/java/bridge/view/InputView.java
+++ src/main/java/bridge/view/InputView.java
@@ -1,18 +1,24 @@
 package bridge.view;
 
+import bridge.constants.ExceptionMessage;
 import camp.nextstep.edu.missionutils.Console;
+import java.util.HashSet;
+import java.util.List;
 
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
  */
 public class InputView {
+    private static final String REGULAR_PATTERN = "[^0-9]";
 
     /**
      * 다리의 길이를 입력받는다.
      */
     public int readBridgeSize() {
         String bridgeInput = Console.readLine();
-        return Integer.parseInt(bridgeInput);
+        int bridgeLength = Integer.parseInt(bridgeInput);
+        bridgeSizeVerification(bridgeLength);
+        return bridgeLength;
     }
 
     /**
@@ -28,4 +34,33 @@ public class InputView {
     public String readGameCommand() {
         return Console.readLine();
     }
+
+    public void bridgeSizeVerification(int bridgeLength) {
+        validateNull(bridgeLength);
+        validateBetween(bridgeLength);
+        validateNumber(bridgeLength);
+    }
+
+    public void validateNull(int bridgeLength) {
+        String bridge = String.valueOf(bridgeLength);
+        if (bridge.equals("")) {
+            throw new IllegalArgumentException(ExceptionMessage.NOT_INPUT);
+        }
+    }
+
+    // 3 이상 20 이하
+    public void validateBetween(int bridgeLength) {
+        if (!(bridgeLength >= 3 && bridgeLength <= 20)) {
+            throw new IllegalArgumentException(ExceptionMessage.NUMBERS_BETWEEN);
+        }
+    }
+
+    public void validateNumber(int bridgeLength) {
+        String bridge = String.valueOf(bridgeLength);
+        if (!(bridge.equals(REGULAR_PATTERN))) {
+            throw new IllegalArgumentException(ExceptionMessage.NOT_NUMBER_CHARACTERS);
+        }
+    }
 }
+
+
