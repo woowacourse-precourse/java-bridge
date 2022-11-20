212ea1d6b7f67e1ca7e9856c0bbc16bfbac18c05
Kong-hana01 <gksqlsl11@khu.ac.kr>
2022-11-20 17:42:55 +0900

    feat(OutputView): printResult 메서드 작성

--- src/main/java/bridge/GameStart.java
+++ src/main/java/bridge/GameStart.java
@@ -16,6 +16,7 @@ public class GameStart {
     private int bridgeSize;
     private List<String> bridge;
     private int countOfPlay;
+    private int countOfMove;
 
     public GameStart() {
         inputView = new InputView();
@@ -28,7 +29,7 @@ public class GameStart {
     public void run() {
         createBridge();
         boolean isSuccess = play();
-        outputView.printResult();
+        outputView.printResult(bridge, countOfMove, isSuccess, countOfPlay);
     }
 
     private void createBridge() {
@@ -48,10 +49,10 @@ public class GameStart {
         boolean isRestart = true;
         do {
             countOfPlay++;
-            for (int i = 0; i < bridgeSize; i++) {
+            for (countOfMove = 0; countOfMove < bridgeSize; countOfMove++) {
                 String moveBlock = move();
                 boolean isMove = bridgeGame.move(moveBlock);
-                outputView.printMap(bridge, i, isMove);
+                outputView.printMap(bridge, countOfMove, isMove);
                 if (!isMove) {
                     isRestart = readRestart();
                     break;
--- src/main/java/bridge/OutputView.java
+++ src/main/java/bridge/OutputView.java
@@ -56,7 +56,15 @@ public class OutputView {
      * <p>
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void printResult() {
+    public void printResult(List<String> bridge, int countOfMove, boolean isSuccess, int countOfPlay) {
+        System.out.println("최종 게임 결과");
+        printMap(bridge, countOfMove, isSuccess);
+        String successMessage = "성공";
+        if (!isSuccess) {
+            successMessage = "실패";
+        }
+        System.out.println("\n게임 성공 여부: " + successMessage);
+        System.out.println("총 시도한 횟수: " + countOfPlay);
     }
 
     public void printStartMessage() {
