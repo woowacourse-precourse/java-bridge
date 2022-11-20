728c882d55b197927e3c09e5b4e43299c5190537
hyungkya <hyungkya@uci.edu>
2022-11-21 03:44:07 +0900

    feat: 게임 진행 상태 불러오기

--- src/main/java/bridge/BridgeGame.java
+++ src/main/java/bridge/BridgeGame.java
@@ -22,6 +22,10 @@ public class BridgeGame {
         return userPath;
     }
 
+    public GameStatus getGameStatus() {
+        return gameStatus;
+    }
+
     /**
      * 사용자가 칸을 이동할 때 사용하는 메서드
      * <p>
