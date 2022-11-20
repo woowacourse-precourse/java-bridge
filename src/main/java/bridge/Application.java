6be68ce8241f696c7defcdc71f84ed878df33039
이현진 <ehj1107@naver.com>
2022-11-20 22:51:10 +0900

    feat: 게임 재시도와 종료하는 작업 구현

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -8,15 +8,28 @@ public class Application {
     static List<String> bridge;
     static List<String> playerMove;
     static InputView inputview = new InputView();
+    static OutputView outputView = new OutputView();
     static BridgeMaker bridgeMaker = new BridgeMaker(new BridgeRandomNumberGenerator());
     static BridgeGame bridgeGame = new BridgeGame();
     static boolean success;
-    static int tryCount = 0;
+    static int tryCount;
 
     public static void main(String[] args) {
         System.out.println("다리 건너기 게임을 시작합니다.");
+        initialize();
         getBridge();
         gameStart();
+        gameResult();
+    }
+
+    private static void gameResult() {
+        outputView.printResult();
+    }
+
+    private static void initialize() {
+        playerMove = new ArrayList<>();
+        success = true;
+        tryCount = 0;
     }
 
     private static void getBridge() {
@@ -28,13 +41,13 @@ public class Application {
         bridgeSize = inputview.readBridgeSize();
     }
 
-    private static void gameStart() {
-        playerMove = new ArrayList<>();
-        success = true;
+    public static void gameStart() {
+        tryCount++;
         while(success){
             bridgeGame.move();
             success = CheckSuccess();
         }
+        CheckRetry();
     }
 
     private static boolean CheckSuccess() {
@@ -43,5 +56,13 @@ public class Application {
         if(bridge.get(index).equals(lastMove))
             return true;
         return false;
+    }
+
+    private static void CheckRetry(){
+        String retry = inputview.readGameCommand();
+        if(retry == "R")
+            bridgeGame.retry();
+
+        return;
     }
 }
