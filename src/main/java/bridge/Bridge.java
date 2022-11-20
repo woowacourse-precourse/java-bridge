365724ebecfc4d3c7f78b71fdaec1daf3e2b1490
junyong <jymoon1121@gmail.com>
2022-11-20 13:59:34 +0900

    feat: 생성된 다리 리스트를 가진 객체 클래스 추가

---
+++ src/main/java/bridge/Bridge.java
@@ -0,0 +1,17 @@
+package bridge;
+
+import java.util.List;
+
+public class Bridge {
+    private List<String> answerBridge;
+
+    public Bridge(List<String> answerBridge) {
+        this.answerBridge = answerBridge;
+    }
+   public boolean isRightDirection(int index, String direction) {
+        if(answerBridge.get(index).equals(direction)) {
+            return true;
+        }
+        return false;
+   }
+}
