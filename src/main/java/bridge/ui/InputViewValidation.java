d8f3d159978599ffb911ca2e55c7bfebcc0f1380
jungwoo <rlawjddn103@naver.com>
2022-11-20 21:26:54 +0900

    feat(InputViewValidation): 입력이 'R' 혹은 'Q'인지 확인하고 아니면 예외 처리 하는 기능

--- src/main/java/bridge/ui/InputViewValidation.java
+++ src/main/java/bridge/ui/InputViewValidation.java
@@ -8,6 +8,7 @@ public class InputViewValidation {
     private static final String ERROR_NOT_INTEGER = "[ERROR] 숫자가 아닙니다.";
     private static final String ERROR_NOT_IN_RANGE = "[ERROR] 숫자가 범위를 벗어났습니다.";
     private static final String ERROR_NOT_U_OR_D = "[ERROR] U나 D를 입력하셔야합니다.";
+    private static final String ERROR_NOT_R_OR_Q = "[ERROR] R이나 Q를 입력하셔야합니다.";
 
     public static void validateIsInteger(String input) {
         if (!NUMBER_PATTERN.matcher(input).find()) {
@@ -25,6 +26,12 @@ public class InputViewValidation {
     public static void validateIsUorD(String input){
         if (!(input.equals("U") || input.equals("D"))) {
             throw new IllegalArgumentException(ERROR_NOT_U_OR_D);
+        }
+    }
+
+    public static void validateIsRorQ(String input){
+        if (!(input.equals("R") || input.equals("Q"))) {
+            throw new IllegalArgumentException(ERROR_NOT_R_OR_Q);
         }
     }
 }
