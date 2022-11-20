15c5b27d480f567adbd82df6ee195f41547e0ce7
TaeMin <xoals3094@naver.com>
2022-11-20 15:03:49 +0900

    feat(Bridge) : 다리에 대한 책임을 갖는 Bridge 클래스 추가

---
+++ src/main/java/bridge/Model/Bridge.java
@@ -0,0 +1,11 @@
+package bridge.Model;
+
+import java.util.List;
+
+public class Bridge {
+    List<String> bridge;
+
+    public Bridge(BridgeSize bridgeSize, List<String> bridge){
+        this.bridge = bridge;
+    }
+}
