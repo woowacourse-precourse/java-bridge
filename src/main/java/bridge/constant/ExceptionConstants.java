3c10523c49b3f138d56c21ff0efebcfab3703ff8
CAEL0 <kjongmin26@gmail.com>
2022-11-21 00:35:45 +0900

    feat(ExceptionConstants): 재시작 여부 관련 에러 메시지 추가

--- src/main/java/bridge/constant/ExceptionConstants.java
+++ src/main/java/bridge/constant/ExceptionConstants.java
@@ -5,7 +5,9 @@ public enum ExceptionConstants {
     INCORRECT_RANGE(String.format("다리 길이는 %d부터 %d 사이의 숫자여야 합니다.",
             BridgeConstants.MINIMUM_LENGTH, BridgeConstants.MAXIMUM_LENGTH)),
     INCORRECT_MOVING(String.format("입력은 %s 또는 %s이어야 합니다.",
-            BridgeConstants.UP, BridgeConstants.DOWN));
+            BridgeConstants.UP, BridgeConstants.DOWN)),
+    INCORRECT_GAME_COMMAND(String.format("입력은 %s 또는 %s이어야 합니다.",
+            BridgeConstants.RESTART, BridgeConstants.QUIT));
 
     private final String message;
 
