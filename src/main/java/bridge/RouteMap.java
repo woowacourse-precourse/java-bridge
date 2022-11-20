df38097809adddf821489b3dedf5fda81c14371b
sonsoowon <yuwoo1018@gmail.com>
2022-11-20 17:41:49 +0900

    feat(RouteMap): 이동한 다리 상태를 나타내는 클래스 추가

---
+++ src/main/java/bridge/RouteMap.java
@@ -0,0 +1,16 @@
+package bridge;
+
+import java.util.List;
+
+public class RouteMap {
+    private final List<String> upSide;
+    private final List<String> downSide;
+
+    public RouteMap(List<String> upSide, List<String> downSide) {
+        this.upSide = upSide;
+        this.downSide = downSide;
+    }
+
+
+
+}
