19882a593a3fc22d15b49c548b5889fbdc8bfb2e
Dongmin Kim <kimdongmin68@naver.com>
2022-11-20 17:03:57 +0900

    refactor(bridge): 에러 메시지 enum 클래스 생성

--- src/main/java/bridge/exception/InputValidator.java
+++ src/main/java/bridge/exception/InputValidator.java
@@ -1,10 +1,8 @@
 package bridge.exception;
 
+import bridge.common.Error;
+
 public class InputValidator {
-    private static final String NOT_NUMBER = "[ERROR] 숫자를 입력해주세요.";
-    private static final String NOT_IN_RANGE = "[ERROR] 3 이상 20 이하의 숫자를 입력해주세요.";
-    private static final String NOT_BRIDGE = "[ERROR] U(위) 또는 D(아래) 를 입력해주세요.";
-    private static final String NOT_COMMAND = "[ERROR] R(재시작) 또는 Q(종료) 를 입력해주세요.";
     private static String UP_BRIDGE = "U";
     private static String DOWN_BRIDGE = "D";
     private static String RETRY_GAME = "R";
@@ -25,13 +23,13 @@ public class InputValidator {
 
     private static void checkCorrectCommand(String command) {
         if(!(RETRY_GAME.equals(command) || END_GAME.equals(command))) {
-            throw new IllegalArgumentException(NOT_COMMAND);
+            throw new IllegalArgumentException(Error.NOT_RETRY_COMMAND.getMessage());
         }
     }
 
     private static void checkCorrectMoving(String moving) {
         if(!(UP_BRIDGE.equals(moving) || DOWN_BRIDGE.equals(moving))) {
-            throw new IllegalArgumentException(NOT_BRIDGE);
+            throw new IllegalArgumentException(Error.NOT_BRIDGE_COMMAND.getMessage());
         }
     }
 
@@ -39,16 +37,16 @@ public class InputValidator {
         try {
             Integer.parseInt(bridgeSize);
         } catch (NumberFormatException e) {
-            throw new IllegalArgumentException(NOT_NUMBER);
+            throw new IllegalArgumentException(Error.NOT_NUMBER.getMessage());
         } catch (NullPointerException e) {
-            throw new IllegalArgumentException(NOT_NUMBER);
+            throw new IllegalArgumentException(Error.NOT_NUMBER.getMessage());
         }
     }
 
     private static void checkSizeInRange(String bridgeSize) {
         int size = Integer.parseInt(bridgeSize);
         if (size < 3 || 20 < size) {
-            throw new IllegalArgumentException(NOT_IN_RANGE);
+            throw new IllegalArgumentException(Error.NOT_IN_RANGE.getMessage());
         }
     }
 }
