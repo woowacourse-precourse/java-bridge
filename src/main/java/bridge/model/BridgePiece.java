64a50cd2bbc8ef184588ba2f36207b792feb289b
miffyKing <whqudghk28@gmail.com>
2022-11-21 00:50:44 +0900

    refactor: Add validity code for return value check

--- src/main/java/bridge/model/BridgePiece.java
+++ src/main/java/bridge/model/BridgePiece.java
@@ -4,6 +4,11 @@ public class BridgePiece {
 
     private static int currentPlace = 0;
     private final String userInputMove;
+    private static final int REACHED_END = 1;
+    private static final int COMPARE_SAME = 2;
+    private static final int COMPARE_DIFFERENT = 3;
+    private static final int ZERO = 0;
+
 
     BridgePiece(String userInput) {
         this.userInputMove = userInput;
@@ -11,17 +16,17 @@ public class BridgePiece {
 
     public int compareWithAnswer(BridgeAnswer bridgeAnswer) {
         int compareResult = bridgeAnswer.compareWithUserMove(currentPlace, userInputMove);
-        if (compareResult == 1) {
-            return 1;
-        } else if (compareResult == 2) {
+        if (compareResult == REACHED_END) {
+            return REACHED_END;
+        } else if (compareResult == COMPARE_SAME) {
             currentPlace++;
-            return 2;
+            return COMPARE_SAME;
         }
-        return 3;
+        return COMPARE_DIFFERENT;
     }
 
     public static void replayCurrentPlaceReset(BridgeAnswer bridgeAnswer) {
-        currentPlace = 0;
+        currentPlace = ZERO;
         bridgeAnswer.resetCurrentSpot();
     }
 }
