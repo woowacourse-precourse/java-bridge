abdc4e01300b0031945995274d55f0d666d85992
inhwanK <79566221+inhwanK@users.noreply.github.com>
2022-11-21 01:38:42 +0900

    feat(BridgeGame): retry 메서드 구현 및 start 메서드 삭제

--- src/main/java/bridge/BridgeGame.java
+++ src/main/java/bridge/BridgeGame.java
@@ -7,10 +7,6 @@ import java.util.List;
  */
 public class BridgeGame {
 
-    public void start() {
-
-    }
-
     /**
      * 사용자가 칸을 이동할 때 사용하는 메서드
      * <p>
@@ -28,6 +24,10 @@ public class BridgeGame {
      * <p>
      * 재시작을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void retry() {
+    public boolean retry(String gameCommand) {
+        if(gameCommand.equals("R")) {
+            return true;
+        }
+        return false;
     }
 }
