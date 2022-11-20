69f16a0220d25c6b02dfd0426791233fa2faf6a5
임원정 <oddish0513@gmail.com>
2022-11-20 21:40:44 +0900

    refactor(bridge): space 를 tab으로

--- src/main/java/bridge/BridgeRandomNumberGenerator.java
+++ src/main/java/bridge/BridgeRandomNumberGenerator.java
@@ -4,11 +4,11 @@ import camp.nextstep.edu.missionutils.Randoms;
 
 public class BridgeRandomNumberGenerator implements BridgeNumberGenerator {
 
-    private static final int RANDOM_LOWER_INCLUSIVE = 0;
-    private static final int RANDOM_UPPER_INCLUSIVE = 1;
+	private static final int RANDOM_LOWER_INCLUSIVE = 0;
+	private static final int RANDOM_UPPER_INCLUSIVE = 1;
 
-    @Override
-    public int generate() {
-        return Randoms.pickNumberInRange(RANDOM_LOWER_INCLUSIVE, RANDOM_UPPER_INCLUSIVE);
-    }
+	@Override
+	public int generate() {
+		return Randoms.pickNumberInRange(RANDOM_LOWER_INCLUSIVE, RANDOM_UPPER_INCLUSIVE);
+	}
 }
