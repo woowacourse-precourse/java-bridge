6799eb2305246c988e29432e9427fed28aea475a
ykmxxi <ykmxxi97@gmail.com>
2022-11-20 11:18:28 +0900

    feat: 재시작 여부 입력 예외 사항 skeleton code 구현

---
+++ src/main/java/bridge/validation/GameCommandValidation.java
@@ -0,0 +1,15 @@
+package bridge.validation;
+
+public class GameCommandValidation {
+    public GameCommandValidation() {
+    }
+
+    public void isNull() {
+    }
+
+    public void isBlank() {
+    }
+
+    public void isUpperCaseRQ() {
+    }
+}
