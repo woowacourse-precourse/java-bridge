71c154fce53a7f7b24a6bab0e85d77352b666076
hojunking96 <junsong96@naver.com>
2022-11-20 19:33:06 +0900

    feats(Upside.java) : 값 추가 기능 구현

--- src/main/java/bridge/UpSide.java
+++ src/main/java/bridge/UpSide.java
@@ -5,15 +5,9 @@ import java.util.List;
 
 public class UpSide {
     private String upSide;
-    public List<String> upSideArr;
-
-    public UpSide() {
-        this.upSide = makeUpSide();
-    }
+    public List<String> upSideArr = new ArrayList<>();
 
     public String makeUpSide() {
-        upSideArr = new ArrayList<>();
-        convertUpSide();
         String upSideString = String.join(" | ", upSideArr);
         upSide = "[ ";
         upSide += upSideString;
@@ -21,18 +15,7 @@ public class UpSide {
         return upSide;
     }
 
-    public void convertUpSide() {
-        for (String e : Application.movingInputs) {
-            if (e.equals("U"))
-                upSideArr.add("O");
-            if (e.equals("D"))
-                upSideArr.add(" ");
-            if (e.equals("X"))
-                upSideArr.add("X");
-        }
-    }
-
-    public String getUpSide() {
-        return this.upSide;
+    public String getUpSide(){
+        return upSide;
     }
 }
