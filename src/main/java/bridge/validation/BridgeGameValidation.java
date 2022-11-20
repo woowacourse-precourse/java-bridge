f22c83f8702f397447414800cd1912824ae69f2a
desfox <desfox99@naver.com>
2022-11-21 00:27:19 +0900

    refactor(BridgeGameValidation): 매직리터럴 리팩토링

--- src/main/java/bridge/validation/BridgeGameValidation.java
+++ src/main/java/bridge/validation/BridgeGameValidation.java
@@ -3,11 +3,14 @@ package bridge.validation;
 import java.util.List;
 
 public class BridgeGameValidation {
+    private static final String UNEXPECTED_WHEN_EQUAL = "X";
+    private static final String UNEXPECTED_WHEN_UNEQUAL = "O";
+
     public void validateMoveWhenAvailable(String movingCommand, List<String> bridge, List<String> bridgeCurrStatus) {
         int lastIdx = bridge.size() - 1;
 
         if (movingCommand.equals(bridge.get(lastIdx))) {
-            if (bridgeCurrStatus.get(lastIdx).equals("X")) {
+            if (bridgeCurrStatus.get(lastIdx).equals(UNEXPECTED_WHEN_EQUAL)) {
                 throw new IllegalArgumentException("[ERROR] 이동 가능한 칸이지만 이동하지 않았습니다.");
             }
         }
@@ -17,7 +20,7 @@ public class BridgeGameValidation {
         int lastIdx = bridge.size() - 1;
 
         if (! movingCommand.equals(bridge.get(lastIdx))) {
-            if (bridgeCurrStatus.get(lastIdx).equals("O")) {
+            if (bridgeCurrStatus.get(lastIdx).equals(UNEXPECTED_WHEN_UNEQUAL)) {
                 throw new IllegalArgumentException("[ERROR] 이동할 수 없는 칸으로 이동했습니다.");
             }
         }
