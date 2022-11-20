4e8104122bc7d830540de04caef773788964fd14
안광민 <creativeduck0518@gmail.com>
2022-11-21 01:13:36 +0900

    feat(BridgeMaker): 숫자에 해당하는 다리 칸 생성 기능 구현

--- src/main/java/bridge/BridgeMaker.java
+++ src/main/java/bridge/BridgeMaker.java
@@ -2,10 +2,13 @@ package bridge;
 
 import java.util.List;
 
-/**
- * 다리의 길이를 입력 받아서 다리를 생성해주는 역할을 한다.
- */
+import static bridge.data.GameCommand.DOWN;
+import static bridge.data.GameCommand.UP;
+
 public class BridgeMaker {
+
+    private final int DOWN_NUMBER = 0;
+    private final int UP_NUMBER = 1;
 
     private final BridgeNumberGenerator bridgeNumberGenerator;
 
@@ -13,11 +16,15 @@ public class BridgeMaker {
         this.bridgeNumberGenerator = bridgeNumberGenerator;
     }
 
-    /**
-     * @param size 다리의 길이
-     * @return 입력받은 길이에 해당하는 다리 모양. 위 칸이면 "U", 아래 칸이면 "D"로 표현해야 한다.
-     */
     public List<String> makeBridge(int size) {
         return null;
+    }
+
+    private String createCommand(int number) {
+        if (number == UP_NUMBER) {
+            return UP.getCommand();
+        }
+
+        return DOWN.getCommand();
     }
 }
