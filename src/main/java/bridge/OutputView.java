8c8bb53211fb7ba2ed55d5968948ab00510d3f13
Seiyon Park <seiyonpdev@gmail.com>
2022-11-21 08:55:03 +0900

    feat(OutputView): 메소드 printMap 구현
    이동한 만큼의 지도를 출력하는 메소드를 구현했다

--- src/main/java/bridge/OutputView.java
+++ src/main/java/bridge/OutputView.java
@@ -12,7 +12,9 @@ public class OutputView {
      * <p>
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void printMap() {
+    public static void printMap(List<String> upMap, List<String> downMap) {
+        printUpMap(upMap);
+        printDownMap(downMap);
     }
 
     /**
