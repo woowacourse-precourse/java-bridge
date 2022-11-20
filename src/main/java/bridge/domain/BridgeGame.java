9cb19e219af69268eff3c2f6d77d9174a1305de6
김태용 <amico741@naver.com>
2022-11-20 23:24:28 +0900

    refactor: 결과기록 클래스를 대입하도록 리팩토링

--- src/main/java/bridge/domain/BridgeGame.java
+++ src/main/java/bridge/domain/BridgeGame.java
@@ -7,35 +7,39 @@ import java.util.List;
 public class BridgeGame {
     private final Bridge bridge;
     private List<String> inputs;
-    private final Result result;
+    private Result result;
 
-    public BridgeGame(Bridge bridge) {
+    public BridgeGame(Bridge bridge, Result result) {
         this.bridge = bridge;
         this.inputs = new ArrayList<>();
-        this.result = new Result();
+        this.result = result;
     }
 
-    public boolean move(String direction) {
+    public void move(String direction) {
+        inputDirection(direction);
         if(hasNotSameDirection()) {
             retry();
         }
-        if(isFinish()) {
-            throw new IndexOutOfBoundsException();
-        }
-        inputDirection(direction);
-        return true;
     }
 
     public void retry() throws IllegalArgumentException {
-        throw new IllegalArgumentException();
+        if(inputs.get(inputs.size() - 1).equals("U")) {
+            result.changeUpStairsRecord();
+        }
+        if(inputs.get(inputs.size() - 1).equals("D")) {
+            result.changeDownStairsRecord();
+        }
+        throw new IllegalArgumentException("게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)");
     }
 
     public void inputDirection(String direction) {
         inputs.add(direction);
+        result.recordDirection(direction);
     }
 
-    public void initialize() {
+    public void initialize(Result result) {
         inputs = new ArrayList<>();
+        this.result = result;
     }
 
     public boolean isFinish() {
