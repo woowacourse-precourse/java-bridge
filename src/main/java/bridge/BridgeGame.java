b041159cae8560d36373271650bf88365eb73610
Identityless <rlandwnd555@naver.com>
2022-11-21 04:01:12 +0900

    fix : 게임 다시 시작하는 로직 수정

--- src/main/java/bridge/BridgeGame.java
+++ src/main/java/bridge/BridgeGame.java
@@ -48,8 +48,8 @@ public class BridgeGame {
     private boolean isSuccess() {
         boolean success = idx == randomMap.size();
         count++;
-        outputView.printResult(success, List.of(UpMap, DownMap), count);
         if(success) {
+            outputView.printResult(success, List.of(UpMap, DownMap), count);
             return true;
         }
         return false;
@@ -82,8 +82,12 @@ public class BridgeGame {
      * 재시작을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
     public boolean retry() {
+        boolean re = inputView.readGameCommand();
         System.out.println("게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)");
-        return inputView.readGameCommand();
+        if(!re) {
+            outputView.printResult(false, List.of(UpMap, DownMap), count);
+        }
+        return re;
     }
 
     public void init() {
