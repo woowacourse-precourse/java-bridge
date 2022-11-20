ac43e4cfcb108484881e214015536ef7698db19f
hojunking96 <junsong96@naver.com>
2022-11-20 18:00:23 +0900

    test(Upside.java) : 정답이 아닐 경우에 대한 테스트 추가

--- src/test/java/bridge/UpSideTest.java
+++ src/test/java/bridge/UpSideTest.java
@@ -4,17 +4,24 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.Arrays;
-import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class UpSideTest {
 
-    @DisplayName("로또 번호의 개수가 6개가 넘어가면 예외가 발생한다.")
+    @DisplayName("정답 시 다리 윗부분을 제대로 생성하는 지 확인한다.")
     @Test
     void UpSide_생성_테스트() {
-        List<String> bridge = Arrays.asList("U", "D", "U");
-        UpSide upSide = new UpSide(bridge);
-        assertEquals(upSide.getUpSide(),"[ O |   | O ]");
+        Application.movingInputs = Arrays.asList("U", "D", "U");
+        UpSide upSide = new UpSide();
+        assertEquals(upSide.getUpSide(), "[ O |   | O ]");
+    }
+
+    @DisplayName("정답이 아닐 시 다리 윗부분을 제대로 생성하는 지 확인한다.")
+    @Test
+    void UpSide_생성_테스트2() {
+        Application.movingInputs = Arrays.asList("U", "D", "X");
+        UpSide upSide = new UpSide();
+        assertEquals(upSide.getUpSide(), "[ O |   | X ]");
     }
 }
