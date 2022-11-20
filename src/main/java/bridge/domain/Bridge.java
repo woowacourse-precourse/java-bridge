f790b89aa9aedfc7b1b02db33393ded918477c00
CAEL0 <kjongmin26@gmail.com>
2022-11-20 21:21:46 +0900

    feat(Bridge): 생성자 수정

--- src/main/java/bridge/domain/Bridge.java
+++ src/main/java/bridge/domain/Bridge.java
@@ -1,12 +1,14 @@
 package bridge.domain;
 
+import bridge.BridgeRandomNumberGenerator;
+
 import java.util.List;
 
 public class Bridge {
     private final List<String> bridge;
 
-    Bridge(List<String> bridge) {
-        this.bridge = bridge;
+    Bridge(int size) {
+        bridge = new BridgeMaker(new BridgeRandomNumberGenerator()).makeBridge(size);
     }
 
     public List<String> getBridge() {
