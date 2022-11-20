1eef6780d8f346855181295f1a7526648edc111c
JangAJang <99702271+JangAJang@users.noreply.github.com>
2022-11-20 22:38:22 +0900

    feat: InputView 입력을 요청하고 받아서 리턴하는 기능 구현

--- src/main/java/bridge/view/InputView.java
+++ src/main/java/bridge/view/InputView.java
@@ -1,6 +1,12 @@
 package bridge.view;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class InputView {
+
+    private static final String INQUIRE_BRIDGE_LENGTH = "다리의 길이를 입력해주세요.";
+    private static final String INQUIRE_BRIDGE_MOVEMENT = "이동할 칸을 선택해주세요. (위: U, 아래: D)";
+    private static final String INQUIRE_GAME_STOP = "게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)";
 
     public int readBridgeSize() {
         return 0;
@@ -15,7 +21,8 @@ public class InputView {
     }
 
     private String readInput(String comment){
-        return null;
+        System.out.println(comment);
+        return Console.readLine();
     }
 
     private int scanBridgeSize(){
