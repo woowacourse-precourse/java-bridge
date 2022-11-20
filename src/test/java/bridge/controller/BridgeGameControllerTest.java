93401f27332ef52fcb205c6db426c1bfef1e9c06
62hoon99 <98267772+62hoon99@users.noreply.github.com>
2022-11-20 10:55:59 +0900

    feat(test): 플레이어가 이동한 칸이 건널 수 있는지 확인하는 기능 테스트

--- src/test/java/bridge/controller/BridgeGameControllerTest.java
+++ src/test/java/bridge/controller/BridgeGameControllerTest.java
@@ -14,7 +14,6 @@ import static org.assertj.core.api.Assertions.*;
 public class BridgeGameControllerTest {
 
     private static final BridgeMaker bridgeMaker= new BridgeMaker(new BridgeRandomNumberGenerator());
-
     @ValueSource(ints = {3,10,18})
     @ParameterizedTest
     void 다리_길이_테스트(int size){
@@ -35,5 +34,18 @@ public class BridgeGameControllerTest {
                 .collect(Collectors.toList());
         //then
         assertThat(collect).isEqualTo(bridge);
+    }
+
+    @ValueSource(ints = {0,6,8})
+    @ParameterizedTest
+    void 플레이어가_이동한_칸이_건널_수_있는지_확인하는_기능_테스트(int index){
+        //given
+        BridgeGame bridgeGame = new BridgeGame(bridgeMaker.makeBridge(10));
+        List<String> bridge = bridgeGame.getBridge();
+        //when
+        String mark = bridge.get(index);
+        bridgeGame.move(index, mark);
+        //then
+        assertThat(bridgeGame.success());
     }
 }
