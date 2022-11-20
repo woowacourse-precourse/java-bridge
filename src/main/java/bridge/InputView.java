d8d813453b7a510efc24b5c70318939536f02c55
namtndus <rnrmf0721@gmail.com>
2022-11-21 01:44:32 +0900

    feat(InputView): readScopeOfBridgeSizeOfNumberException함수의 기능 구현
    '매개변수를 Integer타입으로 형변환하여 size에 저장'를 구현

--- src/main/java/bridge/InputView.java
+++ src/main/java/bridge/InputView.java
@@ -51,4 +51,8 @@ public class InputView {
             throw new IllegalArgumentException("[ERROR] 다리 길이는 3부터 20 사이의 숫자여야 합니다.");
         }
     }
+
+    public void readScopeOfBridgeSizeOfNumberException(String bridgeSize){
+        int size = Integer.parseInt(bridgeSize);
+    }
 }
