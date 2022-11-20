144fa4d4b463c74a2a75c857842f414759d41c1b
Invidam <hansupark0208@gmail.com>
2022-11-20 20:04:18 +0900

    feat(bridge-game-controller): 입력 기능과의 연결 기능 추가

---
+++ src/main/java/bridge/controller/BridgeGameController.java
@@ -0,0 +1,63 @@
+package bridge.controller;
+
+import static bridge.BridgeGame.createGame;
+import static bridge.domain.game.GameRunStatus.makeGameRunStatus;
+import static bridge.value.GameCommand.QUIT;
+
+import bridge.BridgeGame;
+import bridge.domain.game.GameRunStatus;
+import bridge.value.BridgeCharacter;
+import bridge.value.GameCommand;
+import bridge.view.InputView;
+import bridge.view.OutputView;
+
+public class BridgeGameController {
+    private final InputView inputView;
+    private final OutputView outputView;
+    private final GameRunStatus gameRunStatus;
+    private BridgeGame bridgeGame;
+
+    public BridgeGameController(InputView inputView, OutputView outputView) {
+        this.inputView = inputView;
+        this.outputView = outputView;
+        this.gameRunStatus = makeGameRunStatus();
+    }
+
+    public void start() {
+        while (!gameRunStatus.isStop()) {
+            initGame();
+
+            run();
+            //결과 표시
+        }
+        //최종 결과 표시
+    }
+
+    private void initGame() {
+        this.bridgeGame = createGame(inputView.readBridgeSize());
+    }
+
+    public void run() {
+        BridgeCharacter bridgeCharacter = inputView.readMoving();
+
+        if (!bridgeGame.canMove(bridgeCharacter)) {
+            processFor(inputView.readGameCommand());
+
+            return;
+        }
+
+        bridgeGame.move();
+
+        if (bridgeGame.isGameSuccess()) {
+            gameRunStatus.stop();
+        }
+    }
+
+    private void processFor(GameCommand gameCommand) {
+        if (gameCommand == QUIT) {
+            gameRunStatus.stop();
+            return;
+        }
+        bridgeGame.retry();
+    }
+}
