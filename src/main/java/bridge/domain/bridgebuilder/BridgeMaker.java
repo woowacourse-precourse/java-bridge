ec61f0d8462feb8882cb8111978d8b4bb12c2c0d
yback1223 <yoyob1223@gmail.com>
2022-11-20 20:47:44 +0900

    feat: Add BridgeMaker class
    The class acts as a builder of answer bridge. It uses BridgeConverter
    class to convert binary List to String list.
    
    Closes #10

---
+++ src/main/java/bridge/domain/bridgebuilder/BridgeMaker.java
@@ -0,0 +1,25 @@
+package bridge.domain.bridgebuilder;
+
+import bridge.BridgeNumberGenerator;
+import java.util.ArrayList;
+import java.util.List;
+
+public class BridgeMaker {
+
+    private final BridgeNumberGenerator bridgeNumberGenerator;
+
+    public BridgeMaker(BridgeNumberGenerator bridgeNumberGenerator) {
+        this.bridgeNumberGenerator = bridgeNumberGenerator;
+    }
+
+    public List<String> makeUDBridge(int size) {
+        return BridgeConverter.createRealBridge(makeBinaryBridge(size, new ArrayList<>()));
+    }
+
+    private List<Integer> makeBinaryBridge(int size, List<Integer> bridge) {
+        for (int i = 0; i < size; i++) {
+            bridge.add(bridgeNumberGenerator.generate());
+        }
+        return bridge;
+    }
+}
