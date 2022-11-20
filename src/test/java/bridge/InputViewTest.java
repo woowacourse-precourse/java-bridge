1be0938cfc87ee61c5f4a2294eaf0804fac064e5
EUNSH1 <tkdgus968@naver.com>
2022-11-21 06:41:49 +0900

    test : 다리 사이즈 입력 테스트

---
+++ src/test/java/bridge/InputViewTest.java
@@ -0,0 +1,29 @@
+package bridge;
+
+import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeTest;
+import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.util.Lists.newArrayList;
+
+import camp.nextstep.edu.missionutils.test.NsTest;
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+import java.util.List;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.provider.ValueSource;
+
+
+class InputViewTest {
+
+    private static final InputView inputView = new InputView();
+
+    @DisplayName("다리 사이즈를 입력받아서 반환")
+    @Test
+    void inputBridgeSize() {
+        InputStream in = new ByteArrayInputStream("3".getBytes());
+        System.setIn(in);
+        Assertions.assertThat(inputView.readBridgeSize()).isEqualTo(3);
+    }
+}
