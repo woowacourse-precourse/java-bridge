8e15204e873052f28564a15ff74320a79a4ec682
jin8073 <jin8073@gmail.com>
2022-11-20 14:25:53 +0900

    feat: 생성할 다리의 길이를 입력 받음

---
+++ src/main/java/bridge/Message.java
@@ -0,0 +1,6 @@
+package bridge;
+
+public class Message {
+    public static final String START_MESSAGE = "다리 건너기 게임을 시작합니다.";
+    public static final String INPUT_BRIDGE_LENGTH_MESSAGE = "\n다리의 길이를 입력해주세요.";
+}
