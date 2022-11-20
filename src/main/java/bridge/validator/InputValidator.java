8edc56d464d6f7ac5fdc70b0bb5ff067f77b5b95
DaWoon Lee <qkgk61@gmail.com>
2022-11-20 22:37:00 +0900

    Fix : InputValidator 클래스에서 입력받은 Bridge의 크기가 숫자가 아닌 경우를 예외 처리 하도록 메서드 validateBridgeSize 수정

--- src/main/java/bridge/validator/InputValidator.java
+++ src/main/java/bridge/validator/InputValidator.java
@@ -13,10 +13,9 @@ public class InputValidator {
         GAME_COMMAND_SET = Set.of("R", "Q");
     }
 
-    public static void validateBridgeSize(int size) {
-        if (size < BridgeSpec.MIN.size() || size > BridgeSpec.MAX.size()) {
-            throw new IllegalArgumentException("[ERROR] 3과 20 사이의 수를 입력하세요.");
-        }
+    public static void validateBridgeSize(String input) throws IllegalArgumentException {
+        validateNumber(input);
+        validateBridgeSize(Integer.parseInt(input));
     }
 
     public static void validateMoving(String moving) {
@@ -28,6 +27,20 @@ public class InputValidator {
     public static void validateGameCommand(String gameCommand) {
         if (!GAME_COMMAND_SET.contains(gameCommand)) {
             throw new IllegalArgumentException("[ERROR] 다시 시작하려면 R을, 종료하려면 Q를 입력하세요.");
+        }
+    }
+
+    private static void validateNumber(String input) {
+        for (char cur : input.toCharArray()) {
+            if (cur < '0' || cur > '9') {
+                throw new IllegalArgumentException("[ERROR] 3과 20 사이의 자연수를 입력하세요.");
+            }
+        }
+    }
+
+    private static void validateBridgeSize(int size) {
+        if (size < BridgeSpec.MIN.size() || size > BridgeSpec.MAX.size()) {
+            throw new IllegalArgumentException("[ERROR] 3과 20 사이의 자연수를 입력하세요.");
         }
     }
 
