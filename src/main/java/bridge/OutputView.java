984730d8ed937551cb1f9e26956bb83342c79a1a
KyungKyu Park <rudrb0505@gmail.com>
2022-11-20 16:53:24 +0900

    feat(OutputView): 게임 종료 시 최종 다리 상황을 출력하는 기능 구현

--- src/main/java/bridge/OutputView.java
+++ src/main/java/bridge/OutputView.java
@@ -14,6 +14,7 @@ import java.util.HashMap;
  */
 public class OutputView {
 	private HashMap<String, String> status = new HashMap<>();
+	BridgeStatus bridgeStatus = BridgeStatus.getInstance();
 
 	/**
 	 * 현재까지 이동한 다리의 상태를 정해진 형식에 맞춰 출력한다.
@@ -21,7 +22,6 @@ public class OutputView {
 	 * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
 	 */
 	public HashMap<String, String> printMap(String userSelectedCell, String bridgeLetter) {
-		BridgeStatus bridgeStatus = BridgeStatus.getInstance();
 		status = bridgeStatus.loadStatus(userSelectedCell, bridgeLetter);
 		return status;
 	}
@@ -32,6 +32,7 @@ public class OutputView {
 	 * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
 	 */
 	public HashMap<String, String> printResult() {
-		return null;
+		status = bridgeStatus.loadStatus(" ", " ");
+		return status;
 	}
 }
