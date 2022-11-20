bf2bd7c0d0c9570eb8abece1bb61f43ebd1addd2
박승민 <tmddudals369@naver.com>
2022-11-20 23:41:23 +0900

    refactor: 가독성을 위해 if문 조건 긍정적인 조건으로 변경

--- src/main/java/bridge/utils/validator/GameValidator.java
+++ src/main/java/bridge/utils/validator/GameValidator.java
@@ -1,23 +1,25 @@
 package bridge.utils.validator;
 
-import static bridge.utils.constants.BridgeConstants.UP_CROSS;
 import static bridge.utils.constants.BridgeConstants.DOWN_CROSS;
-import static bridge.utils.constants.ErrorConstants.NOT_MATCH_MOVING_OPTION;
+import static bridge.utils.constants.BridgeConstants.UP_CROSS;
 import static bridge.utils.constants.ErrorConstants.NOT_MATCH_GAME_COMMAND;
-import static bridge.utils.constants.GameConstants.RETRY_GAME;
+import static bridge.utils.constants.ErrorConstants.NOT_MATCH_MOVING_OPTION;
 import static bridge.utils.constants.GameConstants.EXIT_GAME;
+import static bridge.utils.constants.GameConstants.RETRY_GAME;
 
 public class GameValidator {
 
     public static void validMovingOption(String movingOption) {
-        if (!movingOption.equals(UP_CROSS) || !movingOption.equals(DOWN_CROSS)) {
-            throw new IllegalArgumentException(NOT_MATCH_MOVING_OPTION);
+        if (movingOption.equals(UP_CROSS) || movingOption.equals(DOWN_CROSS)) {
+            return;
         }
+        throw new IllegalArgumentException(NOT_MATCH_MOVING_OPTION);
     }
 
     public static void validGameCommand(String gameCommand) {
-        if (!gameCommand.equals(RETRY_GAME) || !gameCommand.equals(EXIT_GAME)) {
-            throw new IllegalArgumentException(NOT_MATCH_GAME_COMMAND);
+        if (gameCommand.equals(RETRY_GAME) || gameCommand.equals(EXIT_GAME)) {
+            return;
         }
+        throw new IllegalArgumentException(NOT_MATCH_GAME_COMMAND);
     }
 }
