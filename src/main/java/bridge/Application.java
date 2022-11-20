a5c5d9bcb5eb8dba60530a8f7b2d2823b5df7a0a
hyungkya <hyungkya@uci.edu>
2022-11-21 01:34:39 +0900

    feat: 다리 길이 입력 요청 출력하기

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -5,5 +5,6 @@ public class Application {
     public static void main(String[] args) {
         OutputView outputView = new OutputView();
         outputView.printGreeting();
+        outputView.printBridgeSizeInputMessage();
     }
 }
--- src/main/java/bridge/OutputView.java
+++ src/main/java/bridge/OutputView.java
@@ -24,4 +24,8 @@ public class OutputView {
     public void printGreeting() {
         System.out.println("다리 건너기 게임을 시작합니다.");
     }
+
+    public void printBridgeSizeInputMessage() {
+        System.out.println("다리의 길이를 입력해주세요.");
+    }
 }
