c3ef784c2058ed15281cbb82a23aefb46ddd1e55
jungwoo <rlawjddn103@naver.com>
2022-11-21 00:23:39 +0900

    refactor(Bridge): Bridge 생성자가 BridgeNumberGenerator를 인자로 받도록 변경

--- src/main/java/bridge/domain/Bridge.java
+++ src/main/java/bridge/domain/Bridge.java
@@ -6,8 +6,8 @@ public class Bridge {
 
     private final List<String> bridge;
 
-    public Bridge(int size) {
-        BridgeMaker bridgeMaker = new BridgeMaker(new BridgeRandomNumberGenerator());
+    public Bridge(int size, BridgeNumberGenerator bridgeNumberGenerator) {
+        BridgeMaker bridgeMaker = new BridgeMaker(bridgeNumberGenerator);
         this.bridge = bridgeMaker.makeBridge(size);
     }
 
