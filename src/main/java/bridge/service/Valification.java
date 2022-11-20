32b449fd198d03dae8837bd18647a0784496711f
jaehyeon <jhjh0022@naver.com>
2022-11-21 01:07:39 +0900

    refactor(Valification): service 패키지로 옮김

--- src/main/java/bridge/service/Valification.java
+++ src/main/java/bridge/service/Valification.java
@@ -10,21 +10,25 @@ public class Valification {
     private final int MIN_NUMBER = 3;
     private final int MAX_NUMBER = 20;
 
+    private final String ERROR_MESSAGE_SIZE = "다리 길이는 3부터 20 사이의 숫자여야 합니다.";
+    private final String ERROR_MOVE_UD = "U, D 두개 중에 하나 선택 해야 합니다.";
+    private final String ERROR_RETRY_RQ = "R, Q 두개 중에 하나 선택 해야 합니다.";
+
     public void verifyBridgeSize(int size) {
         if (!(MIN_NUMBER <= size && size <= MAX_NUMBER)) {
-            throw new IllegalArgumentException("다리 길이는 3부터 20 사이의 숫자여야 합니다.");
+            throw new IllegalArgumentException(ERROR_MESSAGE_SIZE);
         }
     }
 
     public void verifyUorD(String move) {
         if (!(Objects.equals(move, Move.UPBRIDGE) || Objects.equals(move, Move.DOWNBRIDGE))) {
-            throw new IllegalArgumentException("U, D 두개 중에 하나 선택 해야 합니다.");
+            throw new IllegalArgumentException(ERROR_MOVE_UD);
         }
     }
 
-    public void verifyQorR(String hi) {
-        if (!(Objects.equals(hi, Command.RETRY) || Objects.equals(hi, Command.QUIT))) {
-            throw new IllegalArgumentException("R, Q 두개 중에 하나 선택 해야 합니다.");
+    public void verifyQorR(String retry) {
+        if (!(Objects.equals(retry, Command.RETRY) || Objects.equals(retry, Command.QUIT))) {
+            throw new IllegalArgumentException(ERROR_RETRY_RQ);
         }
     }
 }
