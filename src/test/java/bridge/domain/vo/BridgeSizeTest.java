f8d08f3d746754c84ac602db7b1647af63ecbc27
great-park <cksgh1735@naver.com>
2022-11-20 16:01:19 +0900

    test: BridgeSize에 대한 성공 test case 추가

--- src/test/java/bridge/domain/vo/BridgeSizeTest.java
+++ src/test/java/bridge/domain/vo/BridgeSizeTest.java
@@ -4,10 +4,23 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static bridge.domain.vo.BridgeSize.createBridgeSize;
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
-import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.*;
 
 class BridgeSizeTest {
+
+    @DisplayName("다리의 길이를 3 이상 20 이하의 숫자로 입력 시 예외가 발생하지 않는다.")
+    @Test
+    void SuccessCase() {
+        assertThatCode(() -> createBridgeSize("15"))
+                .doesNotThrowAnyException();
+    }
+
+    @DisplayName("다리의 길이를 3 이상 20 이하의 숫자로 입력 시 성공적으로 BridgeSize를 생성한다.")
+    @Test
+    void createBridgeSizeTest() {
+        assertThat(createBridgeSize("15"))
+                .isInstanceOf(BridgeSize.class);
+    }
 
     @DisplayName("다리의 길이를 숫자가 아닌 문자로 입력 시 예외가 발생한다.")
     @Test
