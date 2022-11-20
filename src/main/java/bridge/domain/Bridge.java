4382362c6e964c90fb652300b0f954362ce5d026
HiiWee <wpdlzhf159@gmail.com>
2022-11-21 02:20:57 +0900

    refactor: 칸 건넌 상태 외부에서 생성후 주입으로 변경

--- src/main/java/bridge/domain/Bridge.java
+++ src/main/java/bridge/domain/Bridge.java
@@ -1,15 +1,11 @@
 package bridge.domain;
 
-import bridge.domain.enums.BlockStatus;
-import bridge.domain.enums.CrossStatus;
-import java.util.List;
-
 public class Bridge {
     private final Blocks blocks;
     private final CrossStatuses crossStatuses;
 
-    public Bridge(final List<BlockStatus> blocks) {
-        this.blocks = new Blocks(blocks);
-        this.crossStatuses = CrossStatus.createInitializationStatuses(blocks.size());
+    public Bridge(final Blocks blocks, CrossStatuses crossStatuses) {
+        this.blocks = blocks;
+        this.crossStatuses = crossStatuses;
     }
 }
