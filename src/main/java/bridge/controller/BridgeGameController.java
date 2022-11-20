a1cd72980b12850a875acedb6ee751ef85e78df6
core <jinjs6@gmail.com>
2022-11-20 14:29:44 +0900

    feat(bridge): 다리 건너기 게임 시작 문구 출력 기능 생성

--- src/main/java/bridge/controller/BridgeGameController.java
+++ src/main/java/bridge/controller/BridgeGameController.java
@@ -1,8 +1,11 @@
 package bridge.controller;
 
+import bridge.view.OutputView;
+
 public class BridgeGameController {
 
     public static void execute() {
-
+        //게임 시작 문구 출력하기
+        OutputView.printGameStartMessage();
     }
 }
