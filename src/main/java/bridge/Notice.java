923008ece329e011b3532cb1b5e615bdb2144092
yj2980 <gum0945@naver.com>
2022-11-20 20:38:40 +0900

    feat : notice Enum 클래스 추가

---
+++ src/main/java/bridge/Notice.java
@@ -0,0 +1,22 @@
+package bridge;
+
+public enum Notice {
+	START("다리 건너기 게임을 시작합니다."),
+	RETRY("게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)"),
+	FINAL_RESULT("최종 게임 결과\n"),
+	WHETHER("\n게임 성공 여부: "),
+	SUCCESS("성공"),
+	FAIL("실패"),
+	COUNT("총 시도한 횟수: "),
+	CHOICE_SIZE("다리의 길이를 입력해주세요."),
+	CHOICE_MOVE("이동할 칸을 선택해주세요. (위: U, 아래: D)");
+	private String message;
+
+	Notice(String message) {
+		this.message = message;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+}
