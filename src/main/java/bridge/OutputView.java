b1b21532b6bf42b6ac95859af2575e0a058d1d67
Hyunseok Lee <hyun104467@naver.com>
2022-11-20 23:43:15 +0900

    feat(OutputView): upperBridgeMap

--- src/main/java/bridge/OutputView.java
+++ src/main/java/bridge/OutputView.java
@@ -2,31 +2,26 @@ package bridge;
 
 import java.util.List;
 
-/**
- * 사용자에게 게임 진행 상황과 결과를 출력하는 역할을 한다.
- */
 public class OutputView {
 
-    /**
-     * 현재까지 이동한 다리의 상태를 정해진 형식에 맞춰 출력한다.
-     * <p>
-     * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
-     */
     public void printMap(List<Integer> upperBridge, List<Integer> bottomBridge) {
-        System.out.println("[ ");
-        for (Integer upperUnit: upperBridge) {
 
-        }
-        System.out.println(" ]");
+        System.out.println("\n");
+
     }
 
-    /**
-     * 게임의 최종 결과를 정해진 형식에 맞춰 출력한다.
-     * <p>
-     * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
-     */
     public void printResult() {
+    }
 
+    private void upperBridgeMap(List<Integer> upperBridge) {
+        System.out.println("[ ");
+        for (int i = 0; i < upperBridge.size(); i++) {
+            printBridgeUnit(upperBridge.indexOf(i));
+            if ((i != 0) && (i != upperBridge.size() - 1)) {
+                System.out.println(" | ");
+            }
+        }
+        System.out.println(" ]");
     }
 
     private void printBridgeUnit(Integer bridgeUnit) {
