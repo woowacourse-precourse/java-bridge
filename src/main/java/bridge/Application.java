75c8c17c496ebb9d8a0e5da1699dfa1fe5c9e882
ryanha <ryanha@handong.ac.kr>
2022-11-20 16:18:47 +0900

    feat(application): 게임을 실행하는 controller를 실행하는 로직 추가

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -1,8 +1,9 @@
 package bridge;
 
-public class Application {
+import bridge.controller.BridgeGameController;
 
+public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        BridgeGameController.run();
     }
 }
