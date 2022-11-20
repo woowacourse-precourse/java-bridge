3fda9b344b7b3d714c70339f1d6ea0ded9c15ecd
nakalic <nakalic28@gmail.com>
2022-11-20 17:08:49 +0900

    feat(BridgeGame): 이동할 칸을 건널 수 있는지 여부에 따라 O,X 표시하는 기능 구현

--- src/main/java/bridge/BridgeGame.java
+++ src/main/java/bridge/BridgeGame.java
@@ -1,5 +1,8 @@
 package bridge;
 
+import bridge.domain.Bridge;
+import bridge.domain.User;
+
 /**
  * 다리 건너기 게임을 관리하는 클래스
  */
@@ -10,7 +13,33 @@ public class BridgeGame {
      * <p>
      * 이동을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void move() {
+    public boolean move(User user, Bridge bridge, String s) {
+        if (s.equals("U")) {
+            if (bridge.getBridge().get(0).equals(s)) {
+                user.topBridge.add("O");
+                user.bottomBridge.add(" ");
+                bridge.getBridge().remove(0);
+                return true;
+            }
+            if (!bridge.getBridge().get(0).equals(s)) {
+                user.topBridge.add("X");
+                user.bottomBridge.add(" ");
+            }
+        }
+
+        if (s.equals("D")) {
+            if (bridge.getBridge().get(0).equals(s)) {
+                user.bottomBridge.add("O");
+                user.topBridge.add(" ");
+                bridge.getBridge().remove(0);
+                return true;
+            }
+            if (!bridge.getBridge().get(0).equals(s)) {
+                user.bottomBridge.add("X");
+                user.topBridge.add(" ");
+            }
+        }
+        return false;
     }
 
     /**
