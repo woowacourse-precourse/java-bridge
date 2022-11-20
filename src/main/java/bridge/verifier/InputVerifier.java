29db4abfe5232c805ef5439f2ea493d1adc10c2f
leeyongjun <leeyongjun0310@naver.com>
2022-11-21 06:58:56 +0900

    feat(bridge): 입력된 이동할 칸이 U, D가 아니면 예외가 발생하는 기능 구현

--- src/main/java/bridge/verifier/InputVerifier.java
+++ src/main/java/bridge/verifier/InputVerifier.java
@@ -2,10 +2,18 @@ package bridge.verifier;
 
 public class InputVerifier {
     private static final String REGEX_BRIDGE_SIZE_RANGE = "^[3-9]{1}$|^1{1}[0-9]{1}$|^2{1}0{1}$";
+    private static final String MOVING_UP = "U";
+    private static final String MOVING_DOWN = "D";
 
     public static void BridgeSize(String bridgeSize) {
         if (!bridgeSize.matches(REGEX_BRIDGE_SIZE_RANGE)) {
             throw new IllegalArgumentException("[ERROR] 다리의 길이 입력 오류");
+        }
+    }
+
+    public static void moving(String moving){
+        if (!(moving.equals(MOVING_UP) || moving.equals(MOVING_DOWN))){
+            throw new IllegalArgumentException("[ERROR] 이동할 칸 입력 오류");
         }
     }
 }
--- src/main/java/bridge/view/InputView.java
+++ src/main/java/bridge/view/InputView.java
@@ -27,7 +27,9 @@ public class InputView {
      */
     public String readMoving() {
         System.out.println("이동할 칸을 선택해주세요. (위: U, 아래: D)");
-        return Console.readLine();
+        String moving = Console.readLine();
+        InputVerifier.moving(moving);
+        return moving;
     }
 
     /**
