47df34caafaac2920e097acf184e4f9d349f31a1
roasted-penguin <swkwon25@hanyang.ac.kr>
2022-11-21 01:43:57 +0900

    feat : GameLauncher의 parameter들, run method 추가

--- src/main/java/bridge/service/GameLauncher.java
+++ src/main/java/bridge/service/GameLauncher.java
@@ -7,5 +7,36 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+
 public class GameLauncher {
+
+    private final BridgeRandomNumberGenerator bridgeRandomNumberGenerator = new BridgeRandomNumberGenerator();
+    private final BridgeGame bridgeGame = new BridgeGame();
+    private final BridgeMaker bridgeMaker = new BridgeMaker(bridgeRandomNumberGenerator);
+    private final InputView inputView = new InputView();
+    private final OutputView outputView = new OutputView();
+
+
+    static List<String> str = new ArrayList<>(Arrays.asList("[]","[]"));
+    static boolean PF = false;
+    static int gameCount = 0;
+
+    public void run(){
+        outputView.startBridge();
+        int size = inputView.readBridgeSize();
+        List<String> bridge = bridgeMaker.makeBridge(size);
+        while(true){
+            gameCount++;
+            if (!doGame(bridge,size)) break;
+        }
+        endGame(str,PF,gameCount);
+    }
+
+    private boolean doGame(List<String> bridge, int size){
+    }
+    
+
+    private void endGame(List<String> str, boolean PF, int gameCount){
+    }
+
 }
