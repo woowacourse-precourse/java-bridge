9e21c374fcb7d29acec45e1c1cc2d4f481fe019f
oreo <ohsimon77@naver.com>
2022-11-20 20:41:18 +0900

    Feat : makeBridge 메소드를 만들어보기 일단 위칸이면 U 아래 칸이면 D를 넣어주는 리스트를 만들어준다.

--- src/main/java/bridge/BridgeMaker.java
+++ src/main/java/bridge/BridgeMaker.java
@@ -1,5 +1,7 @@
 package bridge;
 
+import java.security.spec.RSAOtherPrimeInfo;
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -8,6 +10,7 @@ import java.util.List;
 public class BridgeMaker {
 
     private final BridgeNumberGenerator bridgeNumberGenerator;
+    public static List<String> bridge = new ArrayList<>();
 
     public BridgeMaker(BridgeNumberGenerator bridgeNumberGenerator) {
         this.bridgeNumberGenerator = bridgeNumberGenerator;
@@ -17,7 +20,19 @@ public class BridgeMaker {
      * @param size 다리의 길이
      * @return 입력받은 길이에 해당하는 다리 모양. 위 칸이면 "U", 아래 칸이면 "D"로 표현해야 한다.
      */
-    public List<String> makeBridge(int size) {
-        return null;
+    public static List<String> makeBridge(int size) {
+        //List<String> bridge = new ArrayList<>();
+        BridgeRandomNumberGenerator bridgeRandomNumberGenerator = new BridgeRandomNumberGenerator();
+        for(int i=0;i<size;i++){
+            int number = bridgeRandomNumberGenerator.generate();
+            if (number==0){
+                bridge.add("U");
+            }
+            else{
+                bridge.add("D");
+            }
+        }
+        System.out.println(bridge); //sout
+        return bridge;
     }
 }
