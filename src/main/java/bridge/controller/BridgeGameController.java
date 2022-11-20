502d6efc831cbeb6db65160df25130fe9619ac1e
MINJU <alswn9938@gmail.com>
2022-11-20 20:35:15 +0900

    feat(Bridge): add 길이에 맞는 다리 생성 함수

--- src/main/java/bridge/controller/BridgeGameController.java
+++ src/main/java/bridge/controller/BridgeGameController.java
@@ -1,4 +1,26 @@
 package bridge.controller;
 
+import bridge.domain.Bridge;
+import bridge.domain.BridgeMaker;
+import bridge.domain.generator.BridgeRandomNumberGenerator;
+import bridge.view.InputView;
+import bridge.view.OutputView;
+
 public class BridgeGameController {
+    OutputView outputView = new OutputView();
+    InputView inputView = new InputView();
+    BridgeMaker bridgeMaker = new BridgeMaker(new BridgeRandomNumberGenerator());
+
+     public void start(){
+         outputView.printStartGuide();
+         Bridge bridge  = createBridge();
+
+     }
+
+     public Bridge createBridge(){
+         outputView.printInputBridgeLengthGuide();
+         int bridgeLength = inputView.readBridgeLength();
+
+         return new Bridge(bridgeMaker.makeBridge(bridgeLength));
+     }
 }
--- src/main/java/bridge/domain/Bridge.java
+++ src/main/java/bridge/domain/Bridge.java
@@ -1,4 +1,12 @@
 package bridge.domain;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Bridge {
+    List<String> bridge;
+
+    public Bridge(List<String> bridge){
+        this.bridge = new ArrayList<>(bridge);
+   }
 }
