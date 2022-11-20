3304b478bb2c3f6d67c665fdb534359d49fafe09
alstjr706 <alstjr706@naver.com>
2022-11-20 18:42:31 +0900

    refactor : (다리 건너기) 인풋 검증 로직 리팩토링

--- src/main/java/bridge/view/InputView.java
+++ src/main/java/bridge/view/InputView.java
@@ -2,65 +2,74 @@ package bridge.view;
 
 import bridge.constants.ExceptionMessage;
 import camp.nextstep.edu.missionutils.Console;
-import java.util.HashSet;
-import java.util.List;
 
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
  */
 public class InputView {
-    private static final String REGULAR_PATTERN = "[^0-9]";
+    private static final String REGULAR_PATTERN = "^\\d+$";
 
     /**
      * 다리의 길이를 입력받는다.
      */
     public int readBridgeSize() {
         String bridgeInput = Console.readLine();
-        int bridgeLength = Integer.parseInt(bridgeInput);
-        bridgeSizeVerification(bridgeLength);
-        return bridgeLength;
+        bridgeSizeVerification(bridgeInput);
+        return Integer.parseInt(bridgeInput);
     }
 
     /**
      * 사용자가 이동할 칸을 입력받는다.
      */
     public String readMoving() {
-        return Console.readLine();
+        String playerMoving = Console.readLine();
+        udValidateCharacter(playerMoving);
+        return playerMoving;
     }
 
     /**
      * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
      */
     public String readGameCommand() {
-        return Console.readLine();
+        String restartInput = Console.readLine();
+        rqValidateCharacter(restartInput);
+        return restartInput;
     }
 
-    public void bridgeSizeVerification(int bridgeLength) {
+    public void bridgeSizeVerification(String bridgeLength) {
         validateNull(bridgeLength);
-        validateBetween(bridgeLength);
         validateNumber(bridgeLength);
+        validateBetween(bridgeLength);
     }
 
-    public void validateNull(int bridgeLength) {
-        String bridge = String.valueOf(bridgeLength);
-        if (bridge.equals("")) {
+    public void validateNull(String bridgeLength) {
+        if (bridgeLength.equals("")) {
             throw new IllegalArgumentException(ExceptionMessage.NOT_INPUT);
         }
     }
 
-    // 3 이상 20 이하
-    public void validateBetween(int bridgeLength) {
-        if (!(bridgeLength >= 3 && bridgeLength <= 20)) {
+    public void validateBetween(String bridgeLength) {
+        int bridge = Integer.parseInt(bridgeLength);
+        if (!(bridge >= 3 && bridge <= 20)) {
             throw new IllegalArgumentException(ExceptionMessage.NUMBERS_BETWEEN);
         }
     }
 
-    public void validateNumber(int bridgeLength) {
-        String bridge = String.valueOf(bridgeLength);
-        if (!(bridge.equals(REGULAR_PATTERN))) {
+    public void validateNumber(String bridgeLength) {
+        if (!(bridgeLength.matches(REGULAR_PATTERN))) {
             throw new IllegalArgumentException(ExceptionMessage.NOT_NUMBER_CHARACTERS);
         }
     }
+
+    public void udValidateCharacter(String playerMoving) {
+        if (!(playerMoving.equals("U") || playerMoving.equals("D"))) {
+            throw new IllegalArgumentException(ExceptionMessage.NOT_UP_DOWN_CHARACTERS);
+        }
+    }
+
+    public void rqValidateCharacter(String playerMoving) {
+        if (!(playerMoving.equals("R") || playerMoving.equals("Q"))) {
+            throw new IllegalArgumentException(ExceptionMessage.NOT_RESTART_QEND_CHARACTERS);
+        }
+    }
 }
-
-
