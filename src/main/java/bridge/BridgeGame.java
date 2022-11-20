da1b46f6a6fc48a0f49b34559f74e5c4722fc4a1
realslimtaek <89964264+realslimtaek@users.noreply.github.com>
2022-11-20 18:30:49 +0900

    refactor : 사용하지 않는 변수 삭제

--- src/main/java/bridge/BridgeGame.java
+++ src/main/java/bridge/BridgeGame.java
@@ -4,39 +4,12 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 
-/**
- * 다리 건너기 게임을 관리하는 클래스
- */
 public class BridgeGame {
-    final static InputView inputView = new InputView();
-    final OutputView outputView = new OutputView();
     static List<String> bridge_answer = new ArrayList<>();
 
     static int play_count = 1;
 
-
-
-    /**
-     * 사용자가 칸을 이동할 때 사용하는 메서드
-     * <p>
-     * 이동을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
-     */
-    public void move() {
-
-
-    }
-
-    /**
-     * 사용자가 게임을 다시 시도할 때 사용하는 메서드
-     * <p>
-     * 재시작을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
-     */
-    public void retry() {
-    }
-
-
     public void resultBridge(List<String> user_answer) {
-        System.out.println("resultBridge실행");
         initBridgeStringBuilder();
         for (int i = 0; i < OutputView.now_size+1 ; i++) {
             if (checkAnswer(bridge_answer.get(i), user_answer.get(i))) {
