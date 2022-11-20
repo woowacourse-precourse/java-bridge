61551e60b7f6ffba205538b07729f0ee0447a296
Kwak-JunYoung <klop9090@g.skku.edu>
2022-11-21 02:40:04 +0900

    fix(InputView) Add input-requiring messages

--- src/main/java/bridge/InputView.java
+++ src/main/java/bridge/InputView.java
@@ -22,7 +22,7 @@ public class InputView {
      * 사용자가 이동할 칸을 입력받는다.
      */
     public String readMoving() {
-        System.out.println("이동할 칸을 선택해주세요.");
+        System.out.println("이동할 칸을 선택해주세요. (위: U, 아래: D)");
         String moving = readLine();
         if(!"UD".contains(moving)) {
             throw new IllegalArgumentException("[ERROR] 사용자는 'U' 혹은 'D'를 통해서만 이동할 수 있습니다.");
@@ -34,6 +34,7 @@ public class InputView {
      * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
      */
     public String readGameCommand() {
+        System.out.println("게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)");
         String gameCommand = readLine();
         if(!"RQ".contains(gameCommand)) {
             throw new IllegalArgumentException("[ERROR] 사용자는 'R' 혹은 'Q'를 통해서만 게임을 다시 시도할지 여부를 입력할 수 있습니다.");
