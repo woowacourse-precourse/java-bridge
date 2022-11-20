a08bfa780a01db2a70688d570c0a13e1a5a5e5ba
UJ15 <dbwogml15@naver.com>
2022-11-20 16:36:02 +0900

    feat : add field in BridgeGame

--- src/main/java/bridge/engine/BridgeGame.java
+++ src/main/java/bridge/engine/BridgeGame.java
@@ -1,9 +1,17 @@
 package bridge.engine;
 
+import bridge.model.TryCount;
+
 /**
  * 다리 건너기 게임을 관리하는 클래스
  */
 public class BridgeGame {
+
+    private final TryCount tryCount;
+
+    public BridgeGame() {
+        this.tryCount = new TryCount();
+    }
 
     /**
      * 사용자가 칸을 이동할 때 사용하는 메서드
