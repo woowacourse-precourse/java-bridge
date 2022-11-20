5b7df3745273b8a3a702851b754a7cf5ba5a1e60
Cyma-s <ssol0319@naver.com>
2022-11-20 22:13:47 +0900

    feat(InputView) : add a function that checks end game command is blank

--- src/main/java/bridge/view/InputView.java
+++ src/main/java/bridge/view/InputView.java
@@ -75,6 +75,14 @@ public class InputView {
      * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
      */
     public String readGameCommand() {
-        return null;
+        String gameCommand = Console.readLine();
+        validateBlankCommand(gameCommand);
+        return gameCommand;
+    }
+
+    public void validateBlankCommand(String uncheckedInput) {
+        if(uncheckedInput.isBlank()) {
+            throw new IllegalArgumentException(InputException.BLANK_GAME_END_INPUT.getExceptionMessage());
+        }
     }
 }
