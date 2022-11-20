d6987664ec56ae7356b0bb95d5c20d7268663a74
myeongju00 <j51677767@gmail.com>
2022-11-20 16:42:15 +0900

    refactor : 다리 생성할 때, U, D를 저장하도록 변경

--- src/main/java/bridge/domain/BridgeMaker.java
+++ src/main/java/bridge/domain/BridgeMaker.java
@@ -1,5 +1,7 @@
 package bridge.domain;
 
+import static bridge.domain.GameBridge.makeUpDown;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -21,7 +23,7 @@ public class BridgeMaker {
     public List<String> makeBridge(int size) {
         List<String> bridge = new ArrayList<>();
         for(int i = 0; i< size; i++) {
-            bridge.add(String.valueOf(bridgeNumberGenerator.generate()));
+            bridge.add(makeUpDown(bridgeNumberGenerator.generate()));
         }
         return bridge;
     }
--- src/main/java/bridge/domain/GameBridge.java
+++ src/main/java/bridge/domain/GameBridge.java
@@ -2,6 +2,7 @@
 package bridge.domain;
 
 import java.util.Arrays;
+import java.util.stream.Collectors;
 
 public enum GameBridge {
     U(1, "U"),
@@ -13,5 +14,12 @@ public enum GameBridge {
     GameBridge(int bridgeNumber, String direction) {
         this.bridgeNumber = bridgeNumber;
         this.direction = direction;
+    }
+
+    public static String makeUpDown(int bridgeNumber) {
+        return Arrays.stream(GameBridge.values())
+                .filter(o -> o.bridgeNumber == bridgeNumber)
+                .map(o -> o.direction)
+                .collect(Collectors.joining());
     }
 }
