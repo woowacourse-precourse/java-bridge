7dcc8ddc397e53998b22417ef7ef96b6dd1f7b54
yujiyea <54650556+yujiyea@users.noreply.github.com>
2022-11-21 04:12:34 +0900

    feat(view/OutputView.java): 게임 성공 여부, 시도한 횟수를 출력하는 함수 구현

--- src/main/java/bridge/view/OutputView.java
+++ src/main/java/bridge/view/OutputView.java
@@ -23,6 +23,7 @@ public class OutputView {
      * <p>
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void printResult() {
+    public static void printResult(String whether, String count) {
+        System.out.println(GAME_RESULT.print(whether, count));
     }
 }
