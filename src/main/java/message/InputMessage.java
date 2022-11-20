7f3a2149326f6fd2e0b59cb70cb74ab98b60e0c1
장한울 <swingjhw@naver.com>
2022-11-20 16:17:33 +0900

    chore(#message) : InputMessage 총 시도한 횟수 이름 수정

--- src/main/java/message/InputMessage.java
+++ src/main/java/message/InputMessage.java
@@ -7,5 +7,5 @@ public class InputMessage {
     public static final String INPUT_GAME_RESTART = "게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)";
     public static final String TOTAL_RESULT = "최종 게임 결과";
     public static final String IS_SUCCESS_GAME = "게임 성공 여부: ";
-    public static final String TOTAL_TRIAL = "총 시도한 횟수: ";
+    public static final String TOTAL_TRY = "총 시도한 횟수: ";
 }
