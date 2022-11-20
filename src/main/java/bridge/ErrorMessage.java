b26de89854734dd7980955ed4a3f751168c0733c
jinny-l <jinny.l.dev@gmail.com>
2022-11-20 15:50:50 +0900

    refactor(ErrorMessage): 파라미터을 받아오는 객체 변경

--- src/main/java/bridge/ErrorMessage.java
+++ src/main/java/bridge/ErrorMessage.java
@@ -1,18 +1,21 @@
 package bridge;
 
+import bridge.domain.Block;
+import bridge.domain.Bridge;
+
 public enum ErrorMessage {
 
     INPUT_IS_EMPTY_ERROR("입력 값이 없습니다."),
     NOT_VALID_INPUT_TYPE_ERROR("다리 길이는 숫자만 입력 가능합니다."),
     NOT_VALID_SIZE_ERROR(
             String.format("다리 길이는 %d부터 %d 사이의 숫자여야 합니다.",
-                    BridgeMaker.getBridgeMinSize(),
-                    BridgeMaker.getBridgeMaxSize())
+                    Bridge.getBridgeMinSize(),
+                    Bridge.getBridgeMaxSize())
     ),
     NOT_VALID_MOVING_INPUT_ERROR(
             String.format("이동할 칸은 %s 또는 %s만 입력 가능합니다.",
-                    BridgeGame.getUpBridgeCharacter(),
-                    BridgeGame.getDownBridgeCharacter())
+                    Block.UP_BRIDGE.getString(),
+                    Block.DOWN_BRIDGE.getString())
     ),
     NOT_VALID_RETRY_OR_QUIT_INPUT_ERROR(
             String.format("게임을 다시 시도하려면 %s, 종료하려면 %s를 입력해주세요.",
