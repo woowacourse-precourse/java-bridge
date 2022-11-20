f5a640ed4212786eef15617738d6c30829929659
wonseokjee <wsji9404@gmail.com>
2022-11-21 00:04:28 +0900

    Feat(BridgeGame): 추가 printStartGame, printInputBridgeSize, printSelectBridgeUpDown

--- src/main/java/bridge/BridgeGame.java
+++ src/main/java/bridge/BridgeGame.java
@@ -20,4 +20,14 @@ public class BridgeGame {
      */
     public void retry() {
     }
+
+    public void printStartGame() {
+        System.out.println("다리 건너기 게임을 시작합니다.");
+    }
+    public void printInputBridgeSize() {
+        System.out.println("다리의 길이를 입력해주세요.");
+    }
+    public void printSelectBridgeUpDown() {
+        System.out.println("이동할 칸을 선택해주세요. (위: U, 아래: D");
+    }
 }
