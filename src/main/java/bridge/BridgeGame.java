860ee56ed7792ca14870687c8c2e53ecbd567c61
jmj005 <jmj00005@naver.com>
2022-11-21 04:46:40 +0900

    refactor(BridgeGame): BridgeGame 클래스 refactoring

--- src/main/java/bridge/BridgeGame.java
+++ src/main/java/bridge/BridgeGame.java
@@ -2,7 +2,6 @@ package bridge;
 
 import static bridge.Messages.*;
 
-import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -11,45 +10,16 @@ import java.util.List;
 
 public class BridgeGame {
 
+    public static int retryCount = 1;
     public static boolean gameFlag = true;
-
-    InputView inputView = new InputView();
-    OutputView outputView = new OutputView();
-    BridgeNumberGenerator numberGenerator = new BridgeRandomNumberGenerator();
-    BridgeMaker bridgeMaker = new BridgeMaker(numberGenerator);
-
-    public void run(int size) {
-        int retryCount = 1;
-        List<String> upSideDownSideMove;
-        List<String> bridge = bridgeMaker.makeBridge(size);
-        do {
-            upSideDownSideMove = move(bridge);
-            if (isFailure(upSideDownSideMove)) {
-                retryCount = retry(retryCount);
-            }
-        } while (gameFlag && isFailure(upSideDownSideMove));
-        outputView.printResult(upSideDownSideMove, retryCount);
-    }
 
     /**
      * 사용자가 칸을 이동할 때 사용하는 메서드
      * <p>
      * 이동을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public List<String> move(List<String> bridge) {
-        List<String> upSideDownSideMove = new ArrayList<>(List.of("", ""));
-        for (String bridgeElement : bridge) {
-            upSideDownSideMove = updatePlayerMove(upSideDownSideMove, bridgeElement);
-            outputView.printMap(upSideDownSideMove);
-            if (isFailure(upSideDownSideMove)) {
-                break;
-            }
-        }
-        return upSideDownSideMove;
-    }
 
-    public List<String> updatePlayerMove(List<String> upSideDownSideMove, String bridgeElement) {
-        CompareBridge playerMove = new CompareBridge(inputView.readMoving());
+    public List<String> move(CompareBridge playerMove, List<String> upSideDownSideMove, String bridgeElement) {
         upSideDownSideMove.set(0, upSideDownSideMove.get(0)
                 + playerMove.getUpSideMove(playerMove, bridgeElement).toString());
         upSideDownSideMove.set(1, upSideDownSideMove.get(1)
@@ -67,8 +37,7 @@ public class BridgeGame {
      * <p>
      * 재시작을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public int retry(int retryCount) {
-        String gameCommand = inputView.readGameCommand();
+    public void retry(String gameCommand) {
         if (gameCommand.equals(RETRY)) {
             retryCount += 1;
             gameFlag = true;
@@ -76,6 +45,5 @@ public class BridgeGame {
         if (gameCommand.equals(QUIT)) {
             gameFlag = false;
         }
-        return retryCount;
     }
 }
