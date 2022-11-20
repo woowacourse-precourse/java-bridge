db728a6d9cd794000b1e547c6d1ef5ec7cffb3f4
Donguk <95donguk.dev@gmail.com>
2022-11-21 01:43:49 +0900

    feat: 다리 한 칸 이동 성공 실패 확인 기능 구현

--- src/main/java/bridge/BridgeGame.java
+++ src/main/java/bridge/BridgeGame.java
@@ -1,5 +1,8 @@
 package bridge;
 
+import java.util.List;
+import java.util.Objects;
+
 /**
  * 다리 건너기 게임을 관리하는 클래스
  */
@@ -9,8 +12,12 @@ public class BridgeGame {
      * 사용자가 칸을 이동할 때 사용하는 메서드
      * <p>
      * 이동을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
+     * @param moving
+     * @param block
+     * @return
      */
-    public void move() {
+    public boolean move(String moving, String block) {
+        return Objects.equals(moving, block);
     }
 
     /**
