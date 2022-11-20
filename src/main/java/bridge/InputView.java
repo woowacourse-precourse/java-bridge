038f12be868f648cdc597c99b1aea459725bcbde
chener <49267211+chanosong@users.noreply.github.com>
2022-11-20 19:39:30 +0900

    feat(InputView): Get moving direction
    + readMoving(): Get moving direction and return it

--- src/main/java/bridge/InputView.java
+++ src/main/java/bridge/InputView.java
@@ -6,20 +6,25 @@ import static camp.nextstep.edu.missionutils.Console.readLine;
  * 사용자로부터 입력을 받는 역할을 한다.
  */
 public class InputView {
-    static final String READ_MESSAGE = "다리의 길이를 입력해주세요.";
+    static final String READ_BRIDGE_SIZE_MESSAGE = "다리의 길이를 입력해주세요.";
+    static final String READ_MOVING_MESSAGE = "이동할 칸을 선택해주세요. (위: u, 아래: D)";
 
     // 다리 길이 입력 받고 Integer 캐스팅 후 변환
     public int readBridgeSize() {
-        System.out.println(READ_MESSAGE);
+        System.out.println(READ_BRIDGE_SIZE_MESSAGE);
 
         return Integer.parseInt(readLine());
     }
 
-    /**
-     * 사용자가 이동할 칸을 입력받는다.
-     */
+
+    // 사용자가 이동할 칸을 입력 받고 반환
     public String readMoving() {
-        return null;
+        String direction;
+
+        System.out.println(READ_MOVING_MESSAGE);
+        direction = readLine();
+
+        return direction;
     }
 
     /**
