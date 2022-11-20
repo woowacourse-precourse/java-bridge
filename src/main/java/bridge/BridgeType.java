2b98ad82d86656a06947db04811878c747cd1538
Koonayoung518 <koony000415@gmail.com>
2022-11-20 12:53:58 +0900

    feat(bridge): 다리 생성하기 기능 추가
    BridgeMaker.makeBridge(): 파라미터로 받은 크기 만큼 다리를 생성한다.

---
+++ src/main/java/bridge/BridgeType.java
@@ -0,0 +1,25 @@
+package bridge;
+
+import java.util.Arrays;
+
+public enum BridgeType {
+    LOWER("D", 0),
+    UPPER("U", 1),
+    EMPTY("없음", Integer.MAX_VALUE);
+
+    private String name;
+    private int number;
+
+    BridgeType(String name, int number) {
+        this.name = name;
+        this.number = number;
+    }
+
+    public static String findNameByNumber(int number) {
+        return Arrays.stream(BridgeType.values()).filter(bridgeType -> bridgeType.number == number)
+                .findAny()
+                .orElse(EMPTY)
+                .name;
+
+    }
+}
