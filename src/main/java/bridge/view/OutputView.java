cf17d381bc3c5a5adc86006ef624d0ad36a7e76f
Haaae <101785628+Haaae@users.noreply.github.com>
2022-11-21 07:54:26 +0900

    refactor(MessageFormatUtil, OutputView) : OutputView에서 사용하는 문자 상수화하여 관리

--- src/main/java/bridge/view/OutputView.java
+++ src/main/java/bridge/view/OutputView.java
@@ -1,7 +1,8 @@
 package bridge.view;
 
-
-import bridge.constant.OutputViewConstants;
+import bridge.constant.message.OutputViewMessage;
+import bridge.constant.utils.MessageFormatUtil;
+import java.util.List;
 
 /**
  * 사용자에게 게임 진행 상황과 결과를 출력하는 역할을 한다.
@@ -13,8 +14,13 @@ public class OutputView {
      * <p>
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public static void printMap(String bridgeMapFormat) {
-        System.out.printf(bridgeMapFormat);
+    public static void printMap(List<List<String>> bridgeMap) {
+        for (List<String> side : bridgeMap) {
+            String format = MessageFormatUtil.START_BRACKET
+                    + String.join(MessageFormatUtil.VERTICAL_BAR, side)
+                            + MessageFormatUtil.END_BRACKET;
+            System.out.println(format);
+        }
         System.out.println();
     }
 
@@ -23,28 +29,15 @@ public class OutputView {
      * <p>
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public static void printResult(String bridgeMapFormat, String successOrNot, int numberOfAttempts) {
-        System.out.println(OutputViewConstants.FINAL_GAME_RESULT);
+    public static void printResult(List<List<String>> bridgeMapFormat, String successOrNot, int numberOfAttempts) {
+        System.out.println(OutputViewMessage.FINAL_GAME_RESULT);
         printMap(bridgeMapFormat);
-        System.out.println(OutputViewConstants.GAME_SUCCESS_OR_NOT + successOrNot);
-        System.out.println(OutputViewConstants.TOTAL_NUMBER_OF_ATTEMPTS + numberOfAttempts + "\n");
+        System.out.println(OutputViewMessage.GAME_SUCCESS_OR_NOT + successOrNot);
+        System.out.println(OutputViewMessage.TOTAL_NUMBER_OF_ATTEMPTS + numberOfAttempts + "\n");
 
     }
 
     public static void printGameStart() {
-        System.out.println(OutputViewConstants.GAME_START_NOTICE);
-    }
-
-    static void printBridgeLengthRequest() {
-        System.out.println();
-        System.out.println(OutputViewConstants.BRIDGE_LENGTH_REQUEST);
-    }
-
-    static void printMovingRequest() {
-        System.out.println(OutputViewConstants.MOVING_REQUEST);
-    }
-
-    static void printRetryRequest() {
-        System.out.println(OutputViewConstants.RETRY_REQUEST);
+        System.out.println(OutputViewMessage.GAME_START_NOTICE);
     }
 }
