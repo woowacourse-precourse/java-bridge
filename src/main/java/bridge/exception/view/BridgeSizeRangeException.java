3f3772962389329e8539c43897d88c367b0b7544
Yunsik-Choi <choiys0311@gmail.com>
2022-11-20 17:18:00 +0900

    ♻️ refactor : BridgeSizeRangeException 예외 메시지 변경

--- src/main/java/bridge/exception/view/BridgeSizeRangeException.java
+++ src/main/java/bridge/exception/view/BridgeSizeRangeException.java
@@ -3,7 +3,7 @@ package bridge.exception.view;
 import bridge.exception.BridgeException;
 
 public class BridgeSizeRangeException extends BridgeException {
-    private static final String BRIDGE_SIZE_RANGE_EXCEPTION_MESSAGE = "%s 이상 %s 이하의 숫자를 입력할 수 있습니다.";
+    private static final String BRIDGE_SIZE_RANGE_EXCEPTION_MESSAGE = "다리 길이는 %s부터 %s 사이의 숫자여야 합니다.";
 
     public BridgeSizeRangeException(int bridgeSizeMin, int bridgeSizeMax) {
         super(String.format(BRIDGE_SIZE_RANGE_EXCEPTION_MESSAGE, bridgeSizeMin, bridgeSizeMax));
