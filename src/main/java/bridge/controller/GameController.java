c41eb6a6d49df98d796fd9ba97aab52141d2e72a
DESKTOP-1ANNTGI\USER <cvc100326@naver.com>
2022-11-20 21:11:44 +0900

    feat: 예외 발생시 그시점에서 다시 시작 기능 추가
    - 다리사이즈 입력할때
    - 이동할 칸 입력할때
    - 재시작 종료 커맨드 입력할때

--- src/main/java/bridge/controller/GameController.java
+++ src/main/java/bridge/controller/GameController.java
@@ -7,42 +7,73 @@ import bridge.MapMaker;
 import bridge.view.InputView;
 import bridge.view.OutputView;
 
+
 import java.util.List;
 
 public class GameController {
     InputView inputView = new InputView();
     OutputView outputView = new OutputView();
     BridgeRandomNumberGenerator bridgeRandomNumberGenerator = new BridgeRandomNumberGenerator();
-    BridgeGame bridgeGame;
+    BridgeMaker bridgeMaker = new BridgeMaker(bridgeRandomNumberGenerator);
     MapMaker mapMaker = new MapMaker();
+    BridgeGame bridgeGame;
+
     boolean hasMoved;
     boolean hasRetried;
     boolean hasWin;
 
+
+    private void makeBridgeByInput() {
+        try {
+            bridgeGame = new BridgeGame(bridgeMaker.makeBridge(inputView.readBridgeSize()));
+        } catch (IllegalArgumentException e) {
+            outputView.printErrorNotice(e);
+            makeBridgeByInput();
+        }
+    }
+
+
     public void start() {
         outputView.printStartNotice();
-        BridgeMaker bridgeMaker = new BridgeMaker(bridgeRandomNumberGenerator);
         outputView.printBridgeSizeInputNotice();
-        int bridgeSize = inputView.readBridgeSize();
-        bridgeGame = new BridgeGame(bridgeMaker.makeBridge(bridgeSize));
+        makeBridgeByInput();
     }
 
     public void play() {
         do {
             outputView.printMovingDirectionInputNotice();
-            String moving = inputView.readMoving();
-            hasMoved = bridgeGame.move(moving);
+            hasMoved = moveByInput();
             outputView.printMap(mapMaker.makeMap(bridgeGame.getUserBridgeHistroy(), hasMoved));
-            if(bridgeGame.checkWin()){
+            if (bridgeGame.checkWin()) {
                 break;
             }
         } while (hasMoved);
     }
 
+    private boolean moveByInput() {
+        try {
+            String moving = inputView.readMoving();
+            return bridgeGame.move(moving);
+        } catch (IllegalArgumentException e) {
+            outputView.printErrorNotice(e);
+            return moveByInput();
+        }
+    }
+
+
     public void reTry() {
         outputView.printRestartOrQuitInputNotice();
-        String command = inputView.readGameCommand();
-        hasRetried = bridgeGame.retry(command);
+        hasRetried = retryByInput();
+    }
+
+    private boolean retryByInput() {
+        try {
+            String gameCommand = inputView.readGameCommand();
+            return bridgeGame.retry(gameCommand);
+        } catch (IllegalArgumentException e) {
+            outputView.printErrorNotice(e);
+            return retryByInput();
+        }
     }
 
     public void end() {
