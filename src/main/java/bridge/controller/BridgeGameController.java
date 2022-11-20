ceda1527ac666939305994e69a3dcac1a47187d3
core <jinjs6@gmail.com>
2022-11-20 16:42:52 +0900

    docs(bridge): 기능 목록 3번에 대한 내용 수정

--- src/main/java/bridge/controller/BridgeGameController.java
+++ src/main/java/bridge/controller/BridgeGameController.java
@@ -6,11 +6,13 @@ import bridge.view.OutputView;
 public class BridgeGameController {
 
     private static String bridgeLength;
+    private static int bridgeLengthInt;
 
     public static void execute() {
         OutputView.printGameStartMessage();
         bridgeLength = InputView.readBridgeSize();
         InputView.validateBridgeLength(bridgeLength);
-        InputView.validateBridgeLengthRange(Integer.parseInt(bridgeLength));
+        bridgeLengthInt = Integer.parseInt(bridgeLength);
+        InputView.validateBridgeLengthRange(bridgeLengthInt);
     }
 }
