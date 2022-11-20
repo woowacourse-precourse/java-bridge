6f388332ec6b2d38a5bbfb0a29bb07eb0759bad4
3juhwan <13selfesteem91@naver.com>
2022-11-21 00:47:46 +0900

    feat(bridge): 게임 어플리케이션 뼈대 구현

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -1,8 +1,22 @@
 package bridge;
 
+import bridge.domain.Bridge;
+import bridge.domain.BridgeMaker;
+import bridge.util.BridgeRandomNumberGenerator;
+
+import static bridge.controller.GameResult.gameResult;
+import static bridge.controller.GameRun.gameRun;
+import static bridge.controller.GameStart.gameStart;
+
 public class Application {
+    private static BridgeMaker bridgeMaker = new BridgeMaker(new BridgeRandomNumberGenerator());
+    private static Bridge bridge;
 
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        int retryCnt = -1;
+        bridge = gameStart(bridgeMaker);
+        gameRun(bridge, retryCnt);
+        gameResult(retryCnt);
     }
+
 }
