e754eda6e9a45f9bfa8db2ff92ec9f96b2953a70
hunko <rhwlgns4386@naver.com>
2022-11-20 14:54:00 +0900

    fix : modify leg size twice

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -7,17 +7,15 @@ import bridge.view.OutputView;
 
 public class Application {
 
-    private static final InputView inputView = new InputView();
-    private static final OutputView outputView = new OutputView();
+    private static final InputView inputView=new InputView();
+    private static final OutputView outputView=new OutputView();
     private static BridgeGame bridgeGame;
 
     public static void main(String[] args) {
         // TODO: 프로그램 구현
         new GameStartView().printStartMessage();
-        new BridgeMaker(new BridgeRandomNumberGenerator()).makeBridge(inputView.readBridgeSize());
+        bridgeGame=new BridgeGame(new BridgeMaker(new BridgeRandomNumberGenerator()).makeBridge(inputView.readBridgeSize()));
 
-        bridgeGame = new BridgeGame(new BridgeMaker(new BridgeRandomNumberGenerator()).makeBridge(inputView.readBridgeSize()));
         outputView.printMap(bridgeGame.move(inputView.readMoving()));
-
     }
 }
