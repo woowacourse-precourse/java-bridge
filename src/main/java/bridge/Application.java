14317449c3613beba9e96c07795861a0286e6a19
JaehongDev <println@kakao.com>
2022-11-20 19:08:17 +0900

    feature (Application) : 게임을 실행하도록 설정

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -1,8 +1,18 @@
 package bridge;
 
+import bridge.game.BridgeGameLauncher;
+import bridge.game.context.BridgeGameContext;
+import bridge.view.BridgeGameView;
+import bridge.view.InputView;
+import bridge.view.OutputView;
+
 public class Application {
 
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        new BridgeGameLauncher(
+                new BridgeGameContext(),
+                new BridgeGameView(new InputView(), new OutputView())
+        ).execute();
     }
 }
