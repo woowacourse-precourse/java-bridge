3af9bbd57141b0b8582cd702cb5de229c76262a6
alpacaHyun <flaehdan@gmail.com>
2022-11-21 08:36:28 +0900

    type : feat ✨ 게임 시작 메세지 출력
    body :

--- src/main/java/bridge/view/OutputView.java
+++ src/main/java/bridge/view/OutputView.java
@@ -5,6 +5,11 @@ package bridge.view;
  */
 public class OutputView {
 
+    private static final String START_GAME_MESSAGE = "다리 건너기 게임을 시작합니다.\n";
+    public void printGameStartMessage() {
+        System.out.println(START_GAME_MESSAGE);
+    }
+
     /**
      * 현재까지 이동한 다리의 상태를 정해진 형식에 맞춰 출력한다.
      * <p>
