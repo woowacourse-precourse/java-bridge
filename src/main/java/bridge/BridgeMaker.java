90772bb502b7834cda645afdebf07eb2d1276eab
CordHouse <qkqktl5310@gmail.com>
2022-11-20 17:47:09 +0900

    refactor : makeBridge Function Add else -> answerBridge.add("D")

--- src/main/java/bridge/BridgeMaker.java
+++ src/main/java/bridge/BridgeMaker.java
@@ -23,7 +23,9 @@ public class BridgeMaker {
         for(int i = 0; i < size; i++){
             if(bridgeNumberGenerator.generate() == 1)
                 answerBridge.add("U");
-            answerBridge.add("D");
+            else {
+                answerBridge.add("D");
+            }
         }
         return answerBridge;
     }
