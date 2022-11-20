06bf5a8b62149c9024b6100ac2268087cf644826
Jenn Kim <grdd0419@gmail.com>
2022-11-20 23:51:46 +0900

    refactor: 다리 출력 코드 controller에서 삭제

--- src/main/java/bridge/Controller/BridgeController.java
+++ src/main/java/bridge/Controller/BridgeController.java
@@ -6,6 +6,7 @@ import bridge.BridgeRandomNumberGenerator;
 import bridge.Domain.Bridge;
 import bridge.Domain.History;
 import bridge.View.InputView;
+import bridge.View.OutputView;
 
 import java.util.List;
 
@@ -36,63 +37,24 @@ public class BridgeController {
             while(playGame) {
                 // 이동할 칸을 입력받는다
                 String moveTo = InputView.readMoving();
-                gameHistory.move(moveTo);
-                System.out.println(gameHistory);
 
                 // 다리를 건넌다
-                System.out.print("[");
-                for (int i = 0; i < location; i++) {
-                    String space = gameHistory.getSpaceByLocation(i);
-
-                    if (space.equals("U")) {
-                        System.out.print(" O ");
-                    }
-                    if (space.equals("D")) {
-                        System.out.print("   ");
-                    }
-                    System.out.print("|");
+                String space = bridge.getSpaceByLocation(location);
+                if(space.equals("U") && moveTo.equals("U")) {
+                    gameHistory.move("O", " ");
                 }
-                if (moveTo.equals("U")) {
-                    String space = bridge.getSpaceByLocation(location);
-
-                    if (space.equals("U")) {
-                        System.out.print(" O ");
-                    }
-                    if (space.equals("D")) {
-                        System.out.print(" X ");
-                        playGame = false;
-                    }
-                } else {
-                    System.out.print("   ");
+                if(space.equals("U") && moveTo.equals("D")) {
+                    gameHistory.move(" ", "X");
+                    playGame = false;
                 }
-                System.out.println("]");
-
-                System.out.print("[");
-                for (int i = 0; i < location; i++) {
-                    String space = gameHistory.getSpaceByLocation(i);
-
-                    if (space.equals("U")) {
-                        System.out.print("   ");
-                    }
-                    if (space.equals("D")) {
-                        System.out.print(" O ");
-                    }
-                    System.out.print("|");
+                if(space.equals("D") && moveTo.equals("U")) {
+                    gameHistory.move("X", " ");
+                    playGame = false;
                 }
-                if (moveTo.equals("D")) {
-                    String space = bridge.getSpaceByLocation(location);
-
-                    if (space.equals("U")) {
-                        System.out.print(" X ");
-                        playGame = false;
-                    }
-                    if (space.equals("D")) {
-                        System.out.print(" O ");
-                    }
-                } else {
-                    System.out.print("   ");
+                if(space.equals("D") && moveTo.equals("D")) {
+                    gameHistory.move(" ", "O");
                 }
-                System.out.println("]");
+                OutputView.printMap(gameHistory, location+1);
 
                 if(!playGame)
                     break;
