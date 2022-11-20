5e1a3da50e7b9473176032b594a7bcf4064393c6
Isu Shin <isu010614@naver.com>
2022-11-20 23:57:59 +0900

    feat: 사용자가 칸을 이동할 때 사용하는 메서드 구현
    - 정지 상황
    - 이동 상태 저장 리스트 반환
    - 이동 상태 저장 리스트의 크기 반환

--- src/main/java/bridge/domain/BridgeGame.java
+++ src/main/java/bridge/domain/BridgeGame.java
@@ -1,24 +1,36 @@
 package bridge.domain;
 
-import bridge.BridgeRandomNumberGenerator;
-
-import java.util.ArrayList;
 import java.util.List;
 
 /**
  * 다리 건너기 게임을 관리하는 클래스
  */
 public class BridgeGame {
-    private final MovingStatusSaver movingStatusSaver = new MovingStatusSaver(new ArrayList<>());
+    private final MovingStatusSaver movingStatusSaver;
+
+    public BridgeGame(MovingStatusSaver movingStatusSaver) {
+        this.movingStatusSaver = movingStatusSaver;
+    }
 
     /**
      * 사용자가 칸을 이동할 때 사용하는 메서드
      * <p>
      * 이동을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public List<String> move(final List<String> randomBridge, final String moving) {
-        movingStatusSaver.saveMovingStatus(randomBridge, moving);
+    public void move(final String moving) {
+        movingStatusSaver.saveMovingStatus(moving);
+    }
+
+    public boolean isStopCondition(){
+        return movingStatusSaver.isStopCondition();
+    }
+
+    public List<String> getSavedStatus(){
         return movingStatusSaver.getMovingStatus();
+    }
+
+    public int getStatusSize() {
+        return movingStatusSaver.getStatusSize();
     }
 
     /**
