5fd461ba227910b63f1ef0e28daf4df2d04a0615
hojunking96 <junsong96@naver.com>
2022-11-20 21:40:21 +0900

    refactor(BridgeGame.java,InputView.java,OutputView.java) : 주석 제거

--- src/main/java/bridge/InputView.java
+++ src/main/java/bridge/InputView.java
@@ -2,17 +2,10 @@ package bridge;
 
 import camp.nextstep.edu.missionutils.Console;
 
-
 import static bridge.exceptions.InputException.*;
 
-/**
- * 사용자로부터 입력을 받는 역할을 한다.
- */
 public class InputView {
 
-    /**
-     * 다리의 길이를 입력받는다.
-     */
     public static int readBridgeSize() throws IllegalArgumentException {
         String bridgeSizeInput = Console.readLine();
         bridgeSizeNumberCheck(bridgeSizeInput);
@@ -21,22 +14,13 @@ public class InputView {
         return bridgeSize;
     }
 
-    /**
-     * 사용자가 이동할 칸을 입력받는다.
-     */
     public static String readMoving() throws IllegalArgumentException {
         String movingInput = Console.readLine();
         movingInputErrorCheck(movingInput);
         return movingInput;
     }
 
-    /**
-     * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
-     */
     public static String readGameCommand() {
-        /**
-         "게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)"
-         */
         String gameCommandInput = Console.readLine();
         gameCommandInputErrorCheck(gameCommandInput);
         return gameCommandInput;
