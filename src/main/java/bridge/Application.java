c3d3236bbb1016178635d2ad20fb35036cd234ff
KangSungBum <peter9706@naver.com>
2022-11-20 22:37:11 +0900

    feat(Application.java): add basic sudo codes
    리팩토링 전 기본 요구사항 코드 완성
    - generateBridge: BridgeMaker 통한 다리 생성
    - getBridgeSize: 사용자로부터 다리 길이 입력
    - bridgeGameProcess: 다리 게임 프로세스

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -1,8 +1,67 @@
 package bridge;
 
+import response.BridgeGameException;
+import response.UserInputException;
+import view.InputView;
+import view.OutputView;
+
+import java.util.List;
+
 public class Application {
 
+    private static BridgeGame game = new BridgeGame();
+    private static int trialCount = 0;
+    private static InputView inputView = new InputView();
+    private static OutputView outputView = new OutputView();
+
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        try {
+            startGame();
+        } catch (UserInputException inputException) {
+            System.out.println(inputException.getMessage());
+        } catch (BridgeGameException gameException) {
+            System.out.println(gameException.getMessage());
+        }
+    }
+
+    private static void startGame() {
+        game.trialCount++;
+        outputView.printGameStartMessage();
+        List<String> generatedBridge = generateBridge();
+        if (!bridgeGameProcess(generatedBridge)) {
+            outputView.printExitCodeRequestMessage();
+            String exitCode = inputView.readGameCommand();
+            if (exitCode.equals("Q")) {
+                return;
+            }
+            game.retry();
+            bridgeGameProcess(generatedBridge);
+        }
+
+        outputView.printSuccessResult(generatedBridge, game.trialCount);
+
+    }
+
+    private static List<String> generateBridge() {
+        int bridgeSize = getBridgeSize();
+        return game.initBridge(bridgeSize);
+    }
+
+    private static int getBridgeSize() {
+        outputView.printBridgeSizeRequestMessage();
+        return inputView.readBridgeSize();
+    }
+
+    private static boolean bridgeGameProcess(List<String> bridge) {
+        boolean success = true;
+        for (int i = 0; i < bridge.size(); i++) {
+            outputView.printMovementRequestMessage();
+            if (!game.move(bridge, i)) {
+                success = false;
+                break;
+            }
+        }
+
+        return success;
     }
 }
