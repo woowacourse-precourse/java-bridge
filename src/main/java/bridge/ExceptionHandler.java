f937986758876861d71a3bedab04c2c091397df4
안광민 <creativeduck0518@gmail.com>
2022-11-20 09:44:19 +0900

    feat(ExceptionHandler): 3 이상 20 이하가 아닐 경우 예외 발생 기능 구현

--- src/main/java/bridge/ExceptionHandler.java
+++ src/main/java/bridge/ExceptionHandler.java
@@ -4,6 +4,9 @@ import bridge.data.Error;
 
 public class ExceptionHandler {
 
+    private static final int START_NUMBER = 3;
+    private static final int END_NUMBER = 20;
+
     public static void checkNullInput(String input) throws IllegalArgumentException {
         if (input.length() <= 0) {
             throw new IllegalArgumentException(Error.NULL_INPUT.getMessage());
@@ -13,6 +16,12 @@ public class ExceptionHandler {
     public static void checkIsNumber(String input) throws IllegalArgumentException {
         if (!input.matches("^[0-9]*$")) {
             throw new IllegalArgumentException(Error.NOT_NUMBER.getMessage());
+        }
+    }
+
+    public static void checkOutOfRange(int bridgeSize) throws IllegalArgumentException {
+        if (bridgeSize < START_NUMBER || bridgeSize > END_NUMBER) {
+            throw new IllegalArgumentException(Error.OUT_OF_RANGE.getMessage());
         }
     }
 }
