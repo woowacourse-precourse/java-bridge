3144743606c972451c8c67ad8600e25c10718faa
cgk95 <kjkj5868@gmail.com>
2022-11-21 00:53:57 +0900

    feat(Application) : 프로그램의 시작점 역할을 하는 Application 클래스의 뼈대 구현

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -3,6 +3,10 @@ package bridge;
 public class Application {
 
     public static void main(String[] args) {
-
+        try{
+            new BridgeGame();
+        }catch(IllegalArgumentException e){
+            System.out.println("에러났숑");
+        }
     }
 }
