d5cb34503f259b966254308740cf7241a41e62be
splitube <voicesplit@daum.net>
2022-11-20 20:12:05 +0900

    test(GameCharacter): 단위 테스트 추가

--- src/test/java/bridge/game/GameCharacterTest.java
+++ src/test/java/bridge/game/GameCharacterTest.java
@@ -25,58 +25,75 @@ class GameCharacterTest {
         character = new GameCharacter();
     }
 
+    @DisplayName("다음 이동할 목적지 반환 기능 테스트")
+    @Test
+    void showNextDestination() {
+        //given
+        //when
+        String destination = character.showNextDestination();
+        //then
+        assertThat(destination).isEqualTo(null);
+    }
+
     @DisplayName("다음 이동지를 입력 받는 기능 테스트")
     @ValueSource(strings = {"U", "D"})
     @ParameterizedTest
     void setNextMove(String input) {
+        //given
+        //when
         character.setNextMove(input);
+        //then
         String destination = character.showNextDestination();
         assertThat(destination).isEqualTo(input);
-    }
-
-    @DisplayName("다음 구역으로 이동 기능 테스트")
-    @Test
-    void move() {
-        int startLocation = character.showCurrentLocation();
-        character.move();
-        int movedLocation = character.showCurrentLocation();
-        assertThat(startLocation).isEqualTo(-1);
-        assertThat(movedLocation).isEqualTo(0);
     }
 
     @DisplayName("현재 위치를 조회하는 기능 테스트")
     @Test
     void showCurrentLocation() {
+        //given
+        //when
         int startLocation = character.showCurrentLocation();
+        //then
         assertThat(startLocation).isEqualTo(-1);
+    }
+
+    @DisplayName("다음 구역으로 이동 기능 테스트")
+    @Test
+    void move() {
+        //given
+        int startLocation = character.showCurrentLocation();
+        //when
+        character.move();
+        //then
+        int movedLocation = character.showCurrentLocation();
+        assertThat(startLocation).isEqualTo(-1);
+        assertThat(movedLocation).isEqualTo(0);
     }
 
     @DisplayName("다음 이동할 구역 반환 기능 테스트")
     @Test
     void showNextArea() {
+        //given
+        //when
         int nextArea = character.showNextArea();
+        //then
         assertThat(nextArea).isEqualTo(0);
     }
 
-    @DisplayName("다음 이동할 목적지 반환 기능 테스트")
-    @ValueSource(strings = {"U", "D"})
-    @ParameterizedTest
-    void showNextDestination(String input) {
-        character.setNextMove(input);
-        String destination = character.showNextDestination();
-        assertThat(destination).isEqualTo(input);
-    }
-
     @DisplayName("캐릭터 위치 초기화 기능 테스트")
-    @Test
-    void reset() {
-        character.move();
-        character.move();
-        character.move();
+    @ValueSource(ints = {1, 2, 3, 4, 5, 6})
+    @ParameterizedTest
+    void reset(int moveCount) {
+        //given
+        for (int i = 0; i < moveCount; i++) {
+            character.move();
+        }
         int movedLocation = character.showCurrentLocation();
+        //when
         character.reset();
+        //then
         int resetLocation = character.showCurrentLocation();
-        assertThat(movedLocation).isEqualTo(2);
+        assertThat(movedLocation).isEqualTo(moveCount - 1);
         assertThat(resetLocation).isEqualTo(-1);
 
     }
