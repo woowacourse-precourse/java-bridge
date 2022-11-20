61c7c9dd466bc53eb29c0fe9ea34c768b391aaae
jungwoo <rlawjddn103@naver.com>
2022-11-20 20:54:08 +0900

    refactor(InputViewValidation): Validation 함수 및 변수를 클래스의 기능을 고려하여 static으로 변경

--- src/main/java/bridge/ui/InputViewValidation.java
+++ src/main/java/bridge/ui/InputViewValidation.java
@@ -4,18 +4,18 @@ import java.util.regex.Pattern;
 
 public class InputViewValidation {
 
-    private final Pattern NUMBER_PATTERN = Pattern.compile("^[0-9]*$");
-    private final String ERROR_NOT_INTEGER = "[ERROR] 숫자가 아닙니다.";
-    private final String ERROR_NOT_IN_RANGE = "[ERROR] 숫자가 범위를 벗어났습니다.";
+    private static final Pattern NUMBER_PATTERN = Pattern.compile("^[0-9]*$");
+    private static final String ERROR_NOT_INTEGER = "[ERROR] 숫자가 아닙니다.";
+    private static final String ERROR_NOT_IN_RANGE = "[ERROR] 숫자가 범위를 벗어났습니다.";
 
 
-    public void validateIsInteger(String input) {
+    public static void validateIsInteger(String input) {
         if (!NUMBER_PATTERN.matcher(input).find()) {
             throw new IllegalArgumentException(ERROR_NOT_INTEGER);
         }
     }
 
-    public void validateIsInRange(String input) {
+    public static void validateIsInRange(String input) {
         int number = Integer.parseInt(input);
         if (number < 3 || number > 20) {
             throw new IllegalArgumentException(ERROR_NOT_IN_RANGE);
