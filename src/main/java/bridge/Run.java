c384400c6cb19be10fede77901b59a14705fd4b3
yejin <kwonyj1022@naver.com>
2022-11-21 00:48:31 +0900

    feat: Run (게임 진행) 구현

---
+++ src/main/java/bridge/Run.java
@@ -0,0 +1,48 @@
+package bridge;
+
+import bridge.constant.SuccessFail;
+import bridge.view.InputView;
+import bridge.view.OutputView;
+
+import java.util.List;
+
+import static bridge.constant.SuccessFail.FAIL;
+
+public class Run {
+    int attempts = 1;
+
+    public Run() {
+        int bridgeSize = new InputView().readBridgeSize();
+        BridgeMaker bridgeMaker = new BridgeMaker(new BridgeRandomNumberGenerator());
+        List<String> bridge = bridgeMaker.makeBridge(bridgeSize);
+        BridgeGame bridgeGame = new BridgeGame(bridge);
+
+        play(bridgeGame);
+    }
+
+    private void play(BridgeGame bridgeGame) {
+        playOneSet(bridgeGame);
+        SuccessFail isSuccess = bridgeGame.isSuccess(bridgeGame.bridge, bridgeGame.player);
+
+        if (isSuccess == FAIL) {
+            String gameCommand = new InputView().readGameCommand();
+            if (gameCommand.equals("R")) {
+                attempts++;
+                bridgeGame.retry();
+                play(bridgeGame);
+            }
+        }
+
+        new OutputView().printResult(bridgeGame.bridge, bridgeGame.player, attempts);
+    }
+
+    private void playOneSet(BridgeGame bridgeGame) {
+        boolean isFinish;
+        do {
+            String moving = new InputView().readMoving();
+            bridgeGame.move(moving);
+            new OutputView().printMap(bridgeGame.bridge, bridgeGame.player);    // 작성 필요
+            isFinish = bridgeGame.isFinish();
+        } while(!isFinish);
+    }
+}
