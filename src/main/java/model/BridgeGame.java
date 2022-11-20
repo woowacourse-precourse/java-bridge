bee8ae66cbf6ae2a039b1a3532551b8558d99015
hodomaroo <enghemdwkd@gmail.com>
2022-11-20 22:34:50 +0900

    [Feat/BridgeGame] 1번의 이동을 진행하는 move 기능 구현
    이동 방향을 입력받아 현재까지의 이동 결과를 업데이트하는 move 메서드를
    구현하였습니다.

--- src/main/java/model/BridgeGame.java
+++ src/main/java/model/BridgeGame.java
@@ -1,5 +1,6 @@
 package model;
 
+import static model.enums.MoveChoice.getPositionNumber;
 import static model.enums.MoveResult.FAIL;
 import static model.enums.MoveResult.NULL;
 import static model.enums.MoveResult.getMatchResult;
@@ -8,7 +9,6 @@ import dto.GameResult;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
-import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 import model.enums.GameStatus;
 import model.enums.MoveResult;
@@ -34,6 +34,8 @@ public class BridgeGame {
      * 이동을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
     public void move(String moveChoice) {
+        int targetIndex = moveResults.get(0).size();
+        updateMoveResults(getPositionNumber(moveChoice), bridge.movable(targetIndex, moveChoice));
     }
 
     /**
