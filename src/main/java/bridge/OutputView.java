961150f28727546d772f5828b924ba019abebc1f
jthugg <jthug@naver.com>
2022-11-20 11:47:54 +0900

    Feat<OutputView>: 최종 게임 결과를 출력하는 기능 추가

--- src/main/java/bridge/OutputView.java
+++ src/main/java/bridge/OutputView.java
@@ -54,6 +54,14 @@ public class OutputView {
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
     public void printResult(boolean status, int gameTry) {
+        if(status) {
+            System.out.println("게임 성공 여부: 성공");
+            System.out.println("총 시도한 횟수: " + gameTry);
+            return;
+        }
+        System.out.println("게임 성공 여부: 실패");
+        System.out.println("총 시도한 횟수: " + gameTry);
+
     }
 
     public void printStart() {
