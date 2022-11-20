cb247c765959630d596c34afa78533d1d0c3c601
kokodak <kokodakadokok@gmail.com>
2022-11-20 20:07:25 +0900

    refactor(application): 필드 추가 및 메서드 분리

--- src/main/java/bridge/controller/Application.java
+++ src/main/java/bridge/controller/Application.java
@@ -6,23 +6,37 @@ import bridge.view.OutputView;
 
 public class Application {
 
+    private static InputView inputView;
+    private static OutputView outputView;
+
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
-        InputView inputView = new InputView();
-        OutputView outputView = new OutputView();
-        int bridgeSize = inputView.readBridgeSize();
-        BridgeGame bridgeGame = new BridgeGame(bridgeSize);
+        viewInitialize();
+        BridgeGame bridgeGame = makeBridgeGame();
         do {
-            playGame(inputView, outputView, bridgeGame);
-            if (bridgeGame.isGameFailed()) {
-                String gameCommand = inputView.readGameCommand();
-                bridgeGame.retry(gameCommand);
-            }
+            playGame(bridgeGame);
+            checkWhetherGameRetry(bridgeGame);
         } while (!bridgeGame.isGameEnd());
         outputView.printResult(bridgeGame);
     }
 
-    private static void playGame(InputView inputView, OutputView outputView, BridgeGame bridgeGame) {
+    private static void viewInitialize() {
+        inputView = new InputView();
+        outputView = new OutputView();
+    }
+
+    private static BridgeGame makeBridgeGame() {
+        int bridgeSize = inputView.readBridgeSize();
+        return new BridgeGame(bridgeSize);
+    }
+
+    private static void checkWhetherGameRetry(BridgeGame bridgeGame) {
+        if (bridgeGame.isGameFailed()) {
+            String gameCommand = inputView.readGameCommand();
+            bridgeGame.retry(gameCommand);
+        }
+    }
+
+    private static void playGame(BridgeGame bridgeGame) {
         do {
             String moving = inputView.readMoving();
             bridgeGame.move(moving);
