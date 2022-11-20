4d9069f96298e2942c71fb12efcc2dc8c2b97e73
yoonjuho <dbswn990@naver.com>
2022-11-20 20:01:20 +0900

    refactor(GameMessage): 문자열 상수 처리 클래스 변경

--- src/main/java/bridge/view/Notice.java
+++ src/main/java/bridge/view/Notice.java
@@ -7,12 +7,6 @@ public class Notice {
     public static final String GAME_OVER = "게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)";
     public static final String GAME_RESULT = "최종 게임 결과";
     public static final String GAME_WIN_OR_LOSE = "게임 성공 여부: ";
-    public static final String WIN = "성공";
-    public static final String LOSE = "실패";
     public static final String ATTEMPT_COUNT = "총 시도한 횟수: %d";
-    public static final String START = "[";
-    public static final String CONTOUR = "|";
-    public static final String END = "]";
-    public static final String RIGHT = " O ";
-    public static final String WRONG = " X ";
+
 }
