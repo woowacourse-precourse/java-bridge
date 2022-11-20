46035698b6fe8fe5c4066b63030a228a51ed65e2
Identityless <rlandwnd555@naver.com>
2022-11-21 02:33:33 +0900

    feat : 게임의 최종 결과 출력 기능 추가

--- src/main/java/bridge/OutputView.java
+++ src/main/java/bridge/OutputView.java
@@ -1,7 +1,6 @@
 package bridge;
 
 import java.util.List;
-import java.util.Map;
 
 /**
  * 사용자에게 게임 진행 상황과 결과를 출력하는 역할을 한다.
@@ -37,6 +36,20 @@ public class OutputView {
      * <p>
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void printResult() {
+    public void printResult(boolean flag, List<List<String>> map, int count) {
+        System.out.println("최종 게임 결과");
+        printMap(map);
+        printFaliorSuccess(flag);
+        System.out.println("총 시도한 횟수: "+count);
+    }
+
+    private void printFaliorSuccess(boolean flag) {
+        System.out.print("게임 성공 여부: ");
+        if(flag) {
+            System.out.println("성공");
+        }
+        if(!flag) {
+            System.out.println("실패");
+        }
     }
 }
