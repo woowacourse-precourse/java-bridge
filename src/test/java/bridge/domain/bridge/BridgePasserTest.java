0f44d7b3c193ca11f4e99272e4a691e255feb1bc
Invidam <hansupark0208@gmail.com>
2022-11-20 11:27:49 +0900

    test(bridge-passer): 다음 칸으로 이동한지 판별하는 테스트코드 추가

---
+++ src/test/java/bridge/domain/bridge/BridgePasserTest.java
@@ -0,0 +1,50 @@
+package bridge.domain.bridge;
+
+import static bridge.domain.bridge.BridgePasser.makeBridgePasser;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.assertj.core.api.Assertions.assertThat;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import bridge.value.BridgeCharacter;
+import java.util.List;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class BridgePasserTest {
+
+    private BridgePasser bridgePasser;
+
+    @BeforeEach
+    void setUp() {
+        List<String> bridgeChars = List.of("U", "D", "U", "D");
+        Bridge bridge = Bridge.convert(bridgeChars);
+        this.bridgePasser = makeBridgePasser(bridge);
+    }
+
+    @Test
+    void 이동_가능_문자_판별_테스트() {
+        boolean expected = bridgePasser.canMove(BridgeCharacter.UP);
+
+        assertThat(expected).isTrue();
+    }
+
+    @Test
+    void 이동_불가능_문자_판별_테스트() {
+        boolean expected = bridgePasser.canMove(BridgeCharacter.DOWN);
+
+        assertThat(expected).isFalse();
+    }
+
+    @Test
+    void 이동_불가능_위치_판별_테스트() {
+        bridgePasser.move();
+        bridgePasser.move();
+        bridgePasser.move();
+        bridgePasser.move();
+
+        assertThatThrownBy(() -> {
+            bridgePasser.canMove(BridgeCharacter.DOWN);
+        }).isInstanceOf(IllegalStateException.class);
+    }
+}
