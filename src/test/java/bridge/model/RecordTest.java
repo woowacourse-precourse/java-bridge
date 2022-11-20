6e582ef07007eb630492e37d0e518b6943875ee9
sooyoungh <sooyoung.h8@gmail.com>
2022-11-20 23:22:11 +0900

    ✅ test(RecordTest): 이동 결과를 "O", "X"로 기록하는 기능 테스트 작성

---
+++ src/test/java/bridge/model/RecordTest.java
@@ -0,0 +1,42 @@
+package bridge.model;
+
+import bridge.constant.Score;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class RecordTest {
+
+    Record record;
+
+    @BeforeEach
+    void setUp() {
+        record = new Record();
+        record.updateBridgeRecord(Score.PASS, "U");
+        record.updateBridgeRecord(Score.PASS, "U");
+        record.updateBridgeRecord(Score.FAIL, "D");
+    }
+
+    @AfterEach
+    void afterEach() {
+        record = null;
+    }
+
+    @DisplayName("이동 결과가 정상적으로 기록되었는 지 확인한다.")
+    @Test
+    void updateBridgeRecordTest() {
+        List<List<String>> bridgeRecord = record.getBridgeRecord();
+        List<String> upBridgeRecord = bridgeRecord.get(0);
+        List<String> downBridgeRecord = bridgeRecord.get(1);
+
+        assertThat(upBridgeRecord)
+                .containsExactly("O", "O", " ");
+        assertThat(downBridgeRecord)
+                .containsExactly(" ", " ", "X");
+    }
+}
