a29515a63df0af3adcba32498c3d3bc4f53ae893
Hyeonjun Park <phjppo0918@gmail.com>
2022-11-20 18:29:03 +0900

    feat(InputValidator) : 이동 요청에 대한 검증 구현

--- src/main/java/bridge/InputValidator.java
+++ src/main/java/bridge/InputValidator.java
@@ -6,9 +6,31 @@ public class InputValidator {
         validateNumber(bridgeSize);
     }
 
+    private void validateInputMove(String command) {
+        validateIsOneChar(command);
+        validateIsMoveCommand(command.charAt(0));
+    }
+
+    private void validateIsMoveCommand(char command) {
+        if(isNotMoveCommand(command)) {
+            throw new IllegalArgumentException(ErrorMessage.WRONG_INPUT.toString());
+        }
+    }
+
+    private static boolean isNotMoveCommand(char command) {
+        return command != InputType.UP.getValue() && command != InputType.DOWN.getValue();
+    }
+
     private void validateNumber(String number) {
-        if(number.matches(String.valueOf(Regex.NUMBER))) {
+        if(!number.matches(String.valueOf(Regex.NUMBER))) {
             throw new IllegalArgumentException(ErrorMessage.BRIDGE_OUT_OF_SIZE.toString());
         }
     }
+
+    private void validateIsOneChar(String command) {
+        if(!command.matches(Regex.ONE_CHAR.toString())) {
+            throw new IllegalArgumentException(ErrorMessage.WRONG_INPUT.toString());
+        }
+    }
+
 }
