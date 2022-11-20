ba7562a979ea44e7db1829a1e0438e8f0255e96c
chener <49267211+chanosong@users.noreply.github.com>
2022-11-20 22:13:11 +0900

    feat(BridgeGame): Init game with size
    + start(): Get size of bridge and init

--- src/main/java/bridge/BridgeGame.java
+++ src/main/java/bridge/BridgeGame.java
@@ -1,9 +1,20 @@
 package bridge;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * 다리 건너기 게임을 관리하는 클래스
  */
 public class BridgeGame {
+
+    BridgeRandomNumberGenerator bridgeRandomNumberGenerator = new BridgeRandomNumberGenerator();
+    BridgeMaker bridgeMaker = new BridgeMaker(bridgeRandomNumberGenerator);
+    private List<String> bridge = new ArrayList<>();
+
+    public void init(int size) {
+        bridge = bridgeMaker.makeBridge(size);
+    }
 
     /**
      * 사용자가 칸을 이동할 때 사용하는 메서드
