38b7c6863941cc31b83830e48561bea40c81f0c6
Jinseo Song <82093280+rxb8k@users.noreply.github.com>
2022-11-20 22:57:13 +0900

    feat: 생성할 다리 길이 입력 기능 추가

--- src/main/java/bridge/InputView.java
+++ src/main/java/bridge/InputView.java
@@ -1,15 +1,16 @@
 package bridge;
 
+import camp.nextstep.edu.missionutils.Console;
+
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
  */
 public class InputView {
 
-    /**
-     * 다리의 길이를 입력받는다.
-     */
     public int readBridgeSize() {
-        return 0;
+        String bridgeSizeInput =Console.readLine();
+        int bridgeSize = Integer.valueOf(bridgeSizeInput);
+        return bridgeSize;
     }
 
     /**
