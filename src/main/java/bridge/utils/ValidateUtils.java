da35747912980f09d08472723f80547a32e63bb6
song-wooseok <sws981202@gmail.com>
2022-11-20 23:51:46 +0900

    feat: 다리 개수 입력값을 (3~20 사이의 숫자인지) 검증하는 기능

---
+++ src/main/java/bridge/utils/ValidateUtils.java
@@ -0,0 +1,17 @@
+package bridge.utils;
+
+import bridge.utils.Constants.BridgeSize;
+import java.util.regex.Pattern;
+
+public class ValidateUtils {
+
+    public static void isCorrectRangeNumbers(String input){
+        if(!Pattern.matches(BridgeSize.BRIDGE_SIZE_REGEX, input)){
+            throwIllegalArgumentException(BridgeSize.ERROR_BRIDGE_SIZE_MESSAGE);
+        }
+    }
+
+    public static void throwIllegalArgumentException(String message) throws IllegalArgumentException{
+        throw new IllegalArgumentException(message);
+    }
+}
