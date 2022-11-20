8a45e54ff4239d30dd02f029982718a6c7e5c941
yejin <kwonyj1022@naver.com>
2022-11-20 15:34:14 +0900

    test: OutputView - printResult (게임의 최종 결과를 정해진 형식에 맞춰 출력) 테스트

--- src/test/java/bridge/view/OutputViewTest.java
+++ src/test/java/bridge/view/OutputViewTest.java
@@ -22,8 +22,35 @@ class OutputViewTest extends NsTest {
         );
     }
 
+    @Test
+    void printResultTest_성공() {
+        List<String> bridge = List.of("U", "D", "D", "U");
+        List<String> player = List.of("U", "D", "D", "U");
+        new OutputView().printResult(bridge, player, 3);
+        assertThat(output()).contains(
+                "최종 게임 결과",
+                "[ O |   |   | O ]",
+                "[   | O | O |   ]",
+                "게임 성공 여부: 성공",
+                "총 시도한 횟수: 3"
+        );
+    }
+
+    @Test
+    void printResultTest_실패() {
+        List<String> bridge = List.of("U", "D", "D", "U");
+        List<String> player = List.of("U", "D", "U");
+        new OutputView().printResult(bridge, player, 3);
+        assertThat(output()).contains(
+                "최종 게임 결과",
+                "[ O |   | X ]",
+                "[   | O |   ]",
+                "게임 성공 여부: 실패",
+                "총 시도한 횟수: 3"
+        );
+    }
+
     @Override
     protected void runMain() {
-
     }
 }
