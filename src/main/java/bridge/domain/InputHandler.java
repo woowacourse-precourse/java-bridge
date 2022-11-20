e646e38bb8f12ee369d61408b89a2a3020483661
baksohyeon <bori12370@gmail.com>
2022-11-20 20:19:25 +0900

    refactor: private 메서드 삭제
    새로 알게 된 것: interface 에는 public 메서드만 적어야 한다.

--- src/main/java/bridge/domain/InputHandler.java
+++ src/main/java/bridge/domain/InputHandler.java
@@ -4,9 +4,4 @@ public interface InputHandler {
     int getBridgeSize(String rawInput);
     String getMovingCommand(String rawInput);
     String getGameCommand(String rawInput);
-    int parseInteger(String rawInput);
-    String removeSpacesFromString(String rawInput);
-    void validateBridgeLength(int length);
-    void validateMoving(String command);
-    void validateGameCommand(String command);
 }
