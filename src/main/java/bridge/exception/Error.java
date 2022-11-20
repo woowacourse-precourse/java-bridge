bbd59a2686819a881c2800afc18fc0cfbb40426d
im-shung <seoyoung1113@gmail.com>
2022-11-20 16:16:42 +0900

    feat: :sparkles: 3이상 20이하의 숫자를 입력하지 않았을 경우의 에러 메세지를 가진 WRONG_BRIDGE_LENGTH필드 추가

--- src/main/java/bridge/exception/Error.java
+++ src/main/java/bridge/exception/Error.java
@@ -1,7 +1,8 @@
 package bridge.exception;
 
 public enum Error {
-    NOT_INT("숫자를 입력하세요.");
+    NOT_INT("숫자를 입력하세요."),
+    WRONG_BRIDGE_LENGTH("다리 길이는 3부터 20 사이의 숫자여야 합니다.");
 
     private final String ERROR = "[ERROR]";
     private final String message;
