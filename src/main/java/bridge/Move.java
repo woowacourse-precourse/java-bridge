954703cf73803edf219d77e42443df068727b177
Ilchul Kim <huj0159@gmail.com>
2022-11-21 00:10:30 +0900

    feat: move store order and location

--- src/main/java/bridge/Move.java
+++ src/main/java/bridge/Move.java
@@ -1,16 +1,22 @@
 package bridge;
 
 public enum Move {
-    D(1),
-    U(0);
+    D(1, "D"),
+    U(0, "U");
 
-    private final int location;
+    private final int order;
+    private final String location;
 
-    Move(int location) {
+    Move(int order, String location) {
+        this.order = order;
         this.location = location;
     }
 
-    public int getLocation() {
+    public String getLocation() {
         return location;
+    }
+
+    public int getOrder() {
+        return order;
     }
 }
