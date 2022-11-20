4052bd1f3de44dfb0f8f241f5fef49691a574a47
young2866 <62212610+young2866@users.noreply.github.com>
2022-11-21 00:30:53 +0900

    feat(InputView): 사용자가 이동할 칸을 입력받는 기능 구현

--- src/main/java/bridge/InputView.java
+++ src/main/java/bridge/InputView.java
@@ -8,6 +8,7 @@ import static camp.nextstep.edu.missionutils.Console.readLine;
 public class InputView {
 
     private static final String INPUT_BRIDGE_SIZE = "다리의 길이를 입력해주세요:";
+    private static final String INPUT_MOVE_TARGET = "이동할 칸을 선택해주세요. (위: U, 아래: D)";
 
     /**
      * 다리의 길이를 입력받는다.
@@ -24,7 +25,10 @@ public class InputView {
      * 사용자가 이동할 칸을 입력받는다.
      */
     public String readMoving() {
-        return null;
+        System.out.println(INPUT_MOVE_TARGET);
+        String inputMovingTarget = readLine();
+
+        return  inputMovingTarget;
     }
 
     /**
