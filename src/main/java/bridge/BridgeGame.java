1dd3e4e87a038661e2057505e37127b6d10ad46e
Gibeom Gwon <gb.gwon@stackframe.dev>
2022-11-20 10:50:29 +0900

    feat: 이동 메소드 추가

--- src/main/java/bridge/BridgeGame.java
+++ src/main/java/bridge/BridgeGame.java
@@ -1,6 +1,7 @@
 package bridge;
 
 import java.util.List;
+import java.util.ArrayList;
 
 /**
  * 다리 건너기 게임을 관리하는 클래스
@@ -9,6 +10,7 @@ public class BridgeGame {
 
     private BridgeMaker bridgeMaker = new BridgeMaker(new BridgeRandomNumberGenerator());
     private List<String> bridge;
+    private List<String> userSelect = new ArrayList<>();
 
     public BridgeGame(int size) {
         this.bridge = bridgeMaker.makeBridge(size);
@@ -19,7 +21,11 @@ public class BridgeGame {
      * <p>
      * 이동을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void move() {
+    public void move(int updown) {
+        if (updown == 1)
+            this.userSelect.add("U");
+        else if (updown == 0)
+            this.userSelect.add("D");
     }
 
     /**
