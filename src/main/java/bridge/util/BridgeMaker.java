021d56d0056018217e6ea8d8effc3b41afe114f6
marinesnow34 <m4rinesnow34@gmail.com>
2022-11-21 03:06:43 +0900

    refactor: 브릿지 생성 메소드 분리

--- src/main/java/bridge/util/BridgeMaker.java
+++ src/main/java/bridge/util/BridgeMaker.java
@@ -1,7 +1,6 @@
 package bridge.util;
 
-import java.util.ArrayList;
-import java.util.List;
+import bridge.model.Bridge;
 
 /**
  * 다리의 길이를 입력 받아서 다리를 생성해주는 역할을 한다.
@@ -18,17 +17,19 @@ public class BridgeMaker {
      * @param size 다리의 길이
      * @return 입력받은 길이에 해당하는 다리 모양. 위 칸이면 "U", 아래 칸이면 "D"로 표현해야 한다.
      */
-    public List<String> makeBridge(int size) {
-        List<String> bridge = new ArrayList<>();
+    public Bridge makeBridge(int size) {
+        Bridge bridge = new Bridge();
         for (int i = 0; i < size; i++) {
-            int stage = bridgeNumberGenerator.generate();
-            if (stage == 0) {
-                bridge.add("D");
-            }
-            if (stage == 1) {
-                bridge.add("U");
-            }
+            int stageStatus = bridgeNumberGenerator.generate();
+            bridge.addBridge(converStage(stageStatus));
         }
         return bridge;
+    }
+
+    private String converStage(int stageStatus) {
+        if (stageStatus == 0) {
+            return "D";
+        }
+        return "U";
     }
 }
