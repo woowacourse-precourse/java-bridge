d0a8a221fe09749905b9e8cb60ae5350facc68b4
namtndus <rnrmf0721@gmail.com>
2022-11-20 14:25:33 +0900

    feat(Application): Application의 인스턴스 변수 생성
    'bridgeMaker 변수 생성'를 생성

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -5,6 +5,7 @@ public class Application {
     InputView inputView = new InputView();
     OutputView outputView = new OutputView();
     BridgeGame bridgeGame = new BridgeGame();
+    BridgeMaker bridgeMaker = new BridgeMaker(new BridgeRandomNumberGenerator());
 
     public static void main(String[] args) {
         // TODO: 프로그램 구현
