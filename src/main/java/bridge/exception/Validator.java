6a77887ed07a6e23358e9cbc102de34fbb8003ca
im-shung <seoyoung1113@gmail.com>
2022-11-20 22:46:09 +0900

    feat: :sparkles: 이동할 수 있는 칸(U,D)이 아닌 값을 입력했을 때 예외 처리

--- src/main/java/bridge/exception/Validator.java
+++ src/main/java/bridge/exception/Validator.java
@@ -1,12 +1,21 @@
 package bridge.exception;
 
 import bridge.domain.BrideLength;
+import bridge.domain.BridgeSpace;
+import java.util.Objects;
 
 public class Validator {
 
     public static void validBridgeLength(int bridgeLength) {
         if (bridgeLength < BrideLength.MIN.getSize() || bridgeLength > BrideLength.MAX.getSize()) {
             throw new IllegalArgumentException(Error.WRONG_BRIDGE_LENGTH.getMessage());
+        }
+    }
+
+    public static void validBridgeSpaceType(String input) {
+        if (!Objects.equals(input, BridgeSpace.DOWN.getType()) &&
+                    !Objects.equals(input, BridgeSpace.UP.getType())) {
+            throw new IllegalArgumentException(Error.WRONG_BRIDGE_SPACE_TYPE.getMessage());
         }
     }
 }
