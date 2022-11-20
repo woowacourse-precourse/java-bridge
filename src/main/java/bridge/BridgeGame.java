624e37ecd54d1e455f9d1eb66dd6ac5ca79766c5
yejin <kwonyj1022@naver.com>
2022-11-20 22:35:14 +0900

    feat: BridgeGame - isSuccess(), isFinish(), isAlive() 구현 완료

--- src/main/java/bridge/BridgeGame.java
+++ src/main/java/bridge/BridgeGame.java
@@ -7,8 +7,12 @@ package bridge;
 // 메서드의 인자와 반환 타입은 필요에 따라 추가하거나 변경 가능
 // 게임 진행을 위해 필요한 메서드를 추가하거나 변경 가능
 
+import bridge.constant.SuccessFail;
+
 import java.util.ArrayList;
 import java.util.List;
+
+import static bridge.constant.SuccessFail.*;
 
 /**
  * 다리 건너기 게임을 관리하는 클래스
@@ -29,6 +33,33 @@ public class BridgeGame {
      */
     public void move(String moving) {
         player.add(moving);
+    }
+
+    public SuccessFail isSuccess(List<String> bridge, List<String> player) {
+        if (bridge.equals(player))
+            return SUCCESS;
+        return FAIL;
+    }
+    public boolean isFinish() {
+        if (isAlive() && (player.size() < bridge.size()))
+            return false;
+
+        if (!isAlive() || player.size() == bridge.size())
+            return true;
+
+        if (bridge.size() == player.size())
+            return true;
+        return false;
+    }
+
+    private boolean isAlive() {
+        int nowIdx = player.size() - 1;
+        String nowBridge = bridge.get(nowIdx);
+        String nowPlayer = player.get(nowIdx);
+        if (nowPlayer.equals(nowBridge)) {
+            return true;
+        }
+        return false;
     }
 
     /**
