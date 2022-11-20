27476d347ebeae789b81acff13c40bc9aa3fcefb
yujiyea <54650556+yujiyea@users.noreply.github.com>
2022-11-21 04:12:00 +0900

    feat(view/OutputView.java): 다리 출력하는 함수 구현

--- src/main/java/bridge/view/OutputView.java
+++ src/main/java/bridge/view/OutputView.java
@@ -1,5 +1,8 @@
 package bridge.view;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 import static bridge.view.OutputViewString.*;
 
 public class OutputView {
@@ -8,4 +11,18 @@ public class OutputView {
         System.out.println(START_MESSAGE.print());
     }
 
+    public static void printMap(List<List<String>> bridgeCrossingResult){
+        for (int i = 0; i < bridgeCrossingResult.size(); i++){
+            System.out.println(bridgeCrossingResult.get(i).stream()
+                    .collect(Collectors.joining(" | ", "[ ", " ]")));
+        }
+    }
+
+    /**
+     * 게임의 최종 결과를 정해진 형식에 맞춰 출력한다.
+     * <p>
+     * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
+     */
+    public void printResult() {
+    }
 }
