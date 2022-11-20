553cf2f2230a30c66683aa604d750c32bb7ce129
CAEL0 <kjongmin26@gmail.com>
2022-11-20 14:40:05 +0900

    feat(OutputView): 다리 길이를 묻는 메시지 출력

--- src/main/java/bridge/view/OutputView.java
+++ src/main/java/bridge/view/OutputView.java
@@ -12,6 +12,10 @@ public class OutputView {
         System.out.println();
     }
 
+    public void printAskingBridgeLength() {
+        System.out.println(ViewConstants.ASKING_BRIDGE_LENGTH);
+    }
+
     /**
      * 현재까지 이동한 다리의 상태를 정해진 형식에 맞춰 출력한다.
      * <p>
