96715fd88a2cb31ef540ef9397f461bd650727dd
namtndus <rnrmf0721@gmail.com>
2022-11-21 02:03:05 +0900

    feat(InputView): readScopeOfBridgeSizeOfNumberException함수의 기능 구현
    'false이면, 예외 던짐'를 구현

--- src/main/java/bridge/InputView.java
+++ src/main/java/bridge/InputView.java
@@ -56,6 +56,7 @@ public class InputView {
     public void readScopeOfBridgeSizeOfNumberException(String bridgeSize){
         int size = Integer.parseInt(bridgeSize);
         if(size<3 || size>20){
+            throw new IllegalArgumentException("[ERROR] 다리 길이는 3부터 20 사이의 숫자여야 합니다.");
         }
     }
 }
