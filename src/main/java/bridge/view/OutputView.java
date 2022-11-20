5b8b4dd6cc02d9d6a934661b9448ba0894f85387
Namyunsuk <yunsuk0328@naver.com>
2022-11-21 03:28:51 +0900

    feat(OutputView): 게임 문구 출력 기능 구현

--- src/main/java/bridge/view/OutputView.java
+++ src/main/java/bridge/view/OutputView.java
@@ -1,9 +1,43 @@
 package bridge.view;
 
-/**
- * 사용자에게 게임 진행 상황과 결과를 출력하는 역할을 한다.
- */
+
 public class OutputView {
+
+    private static final String GAME_START = "다리 건너기 게임을 시작합니다.\n\n";
+    private static final String BRIDGE_SIZE_INPUT_MESSAGE = "다리의 길이를 입력해주세요.\n";
+    private static final String SELECT_MESSAGE = "\n이동할 칸을 선택해주세요. (위: U, 아래: D)\n";
+    private static final String RESTART_MESSAGE = "\n게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)\n";
+    private static final String FINAL_GAME_RESULT_MESSAGE = "최종 게임 결과\n";
+    private static final String GAME_SUCCESS_OR_NOT_MESSAGE = "게임 성공 여부: %s\n";
+    private static final String TOTAL_ATTEMPTS = "총 시도한 횟수: %d\n";
+
+    public static void printGameStart() {
+        System.out.printf(GAME_START);
+    }
+
+    public static void printInputSize() {
+        System.out.printf(BRIDGE_SIZE_INPUT_MESSAGE);
+    }
+
+    public static void printSelectMessage() {
+        System.out.printf(SELECT_MESSAGE);
+    }
+
+    public static void printGameResultMessage() {
+        System.out.printf(FINAL_GAME_RESULT_MESSAGE);
+    }
+
+    public static void printRestartMessage() {
+        System.out.printf(RESTART_MESSAGE);
+    }
+
+    public static void printGameSuccessOrNot(String result) {
+        System.out.printf(GAME_SUCCESS_OR_NOT_MESSAGE, result);
+    }
+
+    public static void printGameSuccessOrNot(int attempts) {
+        System.out.printf(TOTAL_ATTEMPTS, attempts);
+    }
 
     /**
      * 현재까지 이동한 다리의 상태를 정해진 형식에 맞춰 출력한다.
