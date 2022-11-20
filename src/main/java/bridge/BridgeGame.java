d377c8891b675e7bc9f13643a098104dda793016
Reznov <reznov03048@gmail.com>
2022-11-20 16:17:08 +0900

    feat: 이동 명령을 받고 이동하는 기능 구현

--- src/main/java/bridge/BridgeGame.java
+++ src/main/java/bridge/BridgeGame.java
@@ -1,7 +1,6 @@
 package bridge;
 
-import bridge.view.InputView;
-import bridge.view.OutputView;
+import bridge.domain.GameRound;
 
 import java.util.List;
 
@@ -10,13 +9,53 @@ import java.util.List;
  */
 public class BridgeGame {
 
+    private BridgeMaker bridgeMaker;
+    private GameRound gameRound;
+    private List<String> bridge;
+
+
+    public BridgeGame() {
+        bridgeMaker = new BridgeMaker(new BridgeRandomNumberGenerator());
+    }
+
+    public void generateBridge(int size) {
+        bridge = bridgeMaker.makeBridge(size);
+    }
+
+    public void generateNewRound() {
+        gameRound = new GameRound();
+    }
 
     /**
      * 사용자가 칸을 이동할 때 사용하는 메서드
      * <p>
      * 이동을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void move() {
+    public String move(String movingCommand) {
+        int nowBridgeIndex = gameRound.getBridgeIndex();
+        String correctResult = checkBridge(movingCommand, bridge.get(nowBridgeIndex));
+
+        gameRound.recordResult(movingCommand, bridge.get(nowBridgeIndex));
+
+        if(correctResult.equals("0")) {
+            gameRound.setBridgeIndex(gameRound.getBridgeIndex() + 1);
+        }
+        checkGameWin();
+
+        return correctResult;
+    }
+
+    private void checkGameWin() {
+        if(gameRound.getBridgeIndex() == bridge.size()) {
+            gameRound.setWin(true);
+        }
+    }
+
+    private String checkBridge(String command, String bridgeLocation) {
+        if(command.equals(bridgeLocation)) {
+            return "0";
+        }
+        return "X";
     }
 
     /**
