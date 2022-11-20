09421d62926b66e0adea773375c17902ce6f22f6
wonwoo6y <wonwoo6y@gmail.com>
2022-11-21 00:55:56 +0900

    feat(BridgeBluePrint): 다리 길이를 저장하는 클래스 구현

---
+++ src/main/java/bridge/BridgeBluePrint.java
@@ -0,0 +1,18 @@
+package bridge;
+
+public class BridgeBluePrint {
+
+    private int size;
+
+    public BridgeBluePrint(int size) {
+        if (!BridgeRange.isWithinRange(size)) {
+            throw new IllegalStateException(
+                    "다리 길이는 " + BridgeRange.getMinValue() + "보다 크거나 같고 " + BridgeRange.getMaxValue() + "보다 작거나 같아야 합니다.");
+        }
+        this.size = size;
+    }
+
+    public int getSize() {
+        return size;
+    }
+}
