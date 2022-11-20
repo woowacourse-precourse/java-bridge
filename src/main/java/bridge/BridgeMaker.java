f63d6e0683171b6e6636d44a26187511834132b0
kkimhaji <kkim.haji@gmail.com>
2022-11-21 02:41:41 +0900

    feat: BridgeMaker method update - makeBridge
    입력된 다리 길이 만큼 U, D를 랜덤으로 생성 후 추가

--- src/main/java/bridge/BridgeMaker.java
+++ src/main/java/bridge/BridgeMaker.java
@@ -1,6 +1,11 @@
 package bridge;
 
+import java.util.ArrayList;
 import java.util.List;
+/**
+ * - `BridgeMaker`의 필드(인스턴스 변수)를 변경할 수 없다.
+ * - `BridgeMaker`의 메서드의 시그니처(인자, 이름)와 반환 타입은 변경할 수 없다.
+ */
 
 /**
  * 다리의 길이를 입력 받아서 다리를 생성해주는 역할을 한다.
@@ -18,6 +23,16 @@ public class BridgeMaker {
      * @return 입력받은 길이에 해당하는 다리 모양. 위 칸이면 "U", 아래 칸이면 "D"로 표현해야 한다.
      */
     public List<String> makeBridge(int size) {
-        return null;
+        List<String> step = new ArrayList<>();
+        for (int i = 0; i < size; i++) {
+            int cell = bridgeNumberGenerator.generate();
+            if (cell==0){
+                step.add("U");
+            }
+            if (cell==1){
+                step.add("D");
+            }
+        }
+        return step;
     }
 }
--- src/main/java/bridge/BridgeRandomNumberGenerator.java
+++ src/main/java/bridge/BridgeRandomNumberGenerator.java
@@ -7,8 +7,13 @@ public class BridgeRandomNumberGenerator implements BridgeNumberGenerator {
     private static final int RANDOM_LOWER_INCLUSIVE = 0;
     private static final int RANDOM_UPPER_INCLUSIVE = 1;
 
+    /**
+     * BridgeRandomNumberGenerator, BridgeNumberGenerator 클래스의 코드는 변경할 수 없다.
+     **/
     @Override
     public int generate() {
         return Randoms.pickNumberInRange(RANDOM_LOWER_INCLUSIVE, RANDOM_UPPER_INCLUSIVE);
     }
+
+
 }
