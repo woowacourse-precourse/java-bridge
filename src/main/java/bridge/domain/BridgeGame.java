7a8cd950d07671db00b1a2c4ab59e250a94e731c
yujiyea <54650556+yujiyea@users.noreply.github.com>
2022-11-21 03:55:27 +0900

    feat(domain/BridgeGame.java): retry함수(시도한 횟수 +1하고 다리 건너기 결과 변수 리셋하기) 구현

--- src/main/java/bridge/domain/BridgeGame.java
+++ src/main/java/bridge/domain/BridgeGame.java
@@ -6,6 +6,7 @@ import java.util.List;
 public class BridgeGame {
     private final List<String> bridge;
     private final List<String> bridgeCrossingResult = new ArrayList<>();
+    private int retryCount = 1;
 
     public BridgeGame(List<String> bridge){
         this.bridge = bridge;
@@ -20,11 +21,13 @@ public class BridgeGame {
         return bridgeCrossingResult;
     }
 
-    /**
-     * 사용자가 게임을 다시 시도할 때 사용하는 메서드
-     * <p>
-     * 재시작을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
-     */
     public void retry() {
+        bridgeCrossingResult.clear();
+        retryCount += 1;
+    }
+
+    @Override
+    public String toString(){
+        return String.valueOf(retryCount);
     }
 }
