c73725f7f45c0bb69011e2c5850e248ffa86b616
myeongju00 <j51677767@gmail.com>
2022-11-20 19:01:16 +0900

    refactor(GameBridge) : GameBridge -> BridgeElememt으로 클래스 이름 변경

--- src/main/java/bridge/domain/BridgeElememt.java
+++ src/main/java/bridge/domain/BridgeElememt.java
@@ -4,20 +4,20 @@ package bridge.domain;
 import java.util.Arrays;
 import java.util.stream.Collectors;
 
-public enum GameBridge {
+public enum BridgeElememt {
     U(1, "U"),
     D(0, "D");
 
     private final int bridgeNumber;
     private final String direction;
 
-    GameBridge(int bridgeNumber, String direction) {
+    BridgeElememt(int bridgeNumber, String direction) {
         this.bridgeNumber = bridgeNumber;
         this.direction = direction;
     }
 
     public static String makeUpDown(int bridgeNumber) {
-        return Arrays.stream(GameBridge.values())
+        return Arrays.stream(BridgeElememt.values())
                 .filter(o -> o.bridgeNumber == bridgeNumber)
                 .map(o -> o.direction)
                 .collect(Collectors.joining());
