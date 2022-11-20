383fb100164c3f58c1b3356a9609abd40a2fa1d3
nylonmusk <106235834+nylonmusk@users.noreply.github.com>
2022-11-21 00:06:43 +0900

    refactor(CleanerUtil): Cleaner 클래스명 수정

--- src/main/java/util/CleanerUtil.java
+++ src/main/java/util/CleanerUtil.java
@@ -1,13 +1,14 @@
 package util;
 
+import constant.BothSideConstant;
 import constant.NumberConstant;
 import java.util.List;
 import model.BridgeGame;
 
-public class Cleaner {
+public class CleanerUtil {
     public static void clearBothSide(List<List<String>> bothSide) {
-        bothSide.get(NumberConstant.UP_SIDE.getConstant()).clear();
-        bothSide.get(NumberConstant.DOWN_SIDE.getConstant()).clear();
+        bothSide.get(BothSideConstant.UP_SIDE.getBothSideConstant()).clear();
+        bothSide.get(BothSideConstant.DOWN_SIDE.getBothSideConstant()).clear();
     }
 
     public static void clearTryNumber() {
