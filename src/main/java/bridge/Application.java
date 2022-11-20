a1c477ecbb38d48b183fe24c7bf090ad0d186aa2
yu-heejin <jkgt0521@naver.com>
2022-11-21 01:50:21 +0900

    feat: 다리 생성하기 무작위 숫자 생성 후 건널 수 있는 칸 삽입

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -4,10 +4,12 @@ public class Application {
 
     public static void main(String[] args) {
         InputView inputView = new InputView();
+        BridgeMaker bridgeMaker = new BridgeMaker(new BridgeRandomNumberGenerator());
 
         int bridgeSize = inputView.readBridgeSize();
         String moveSpaceAnswer = inputView.readMoving();
 
-        System.out.println(moveSpaceAnswer);
+        System.out.println("다리 생성 결과");
+        System.out.println(bridgeMaker.makeBridge(bridgeSize));
     }
 }
