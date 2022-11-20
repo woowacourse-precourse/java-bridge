815d3dd2faa11e3eba736bb05ad86c2364e7973e
KYR <freekyr7529@gmail.com>
2022-11-21 01:51:54 +0900

    refactor : 참조 객체 변경
    BridgeGame을 참조하지 않고 GameController를 참조하도록 코드 변경

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -1,9 +1,11 @@
 package bridge;
 
+import bridge.controller.GameController;
+
 public class Application {
 
     public static void main(String[] args) {
-        BridgeGame bridgeGame = new BridgeGame();
-        bridgeGame.run();
+        GameController gameController = new GameController();
+        gameController.run();
     }
 }
