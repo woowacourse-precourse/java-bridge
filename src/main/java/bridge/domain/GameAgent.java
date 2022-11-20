14e5321d65b74df8e48e5981344bf02aba0c81e9
gomljo <gomljo0307@naver.com>
2022-11-20 17:35:21 +0900

    feat(GameAgent): 다리에 대한 연산를 진행하는 GameAgent 도메인 생성

---
+++ src/main/java/bridge/domain/GameAgent.java
@@ -0,0 +1,16 @@
+package bridge.domain;
+
+import bridge.constant.BridgeDirection;
+import bridge.constant.CrossingState;
+
+import java.util.*;
+
+public class GameAgent{
+
+    private List<String> bridge;
+    
+    public GameAgent(List<String> bridge){
+        this.bridge=bridge;
+    }
+
+}
