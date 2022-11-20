c5018ba8eb4f76e359ba95053d11fe68b197f825
HyeokJoon Kong <orijoon98@gmail.com>
2022-11-20 21:50:28 +0900

    feat: add BridgeMovable enum

--- src/main/java/bridge/model/BridgeConstant.java
+++ src/main/java/bridge/model/BridgeConstant.java
@@ -2,8 +2,6 @@ package bridge.model;
 
 public abstract class BridgeConstant {
 
-    public static final String ABLE = "O";
-    public static final String UNABLE = "X";
     public static final String RESTART = "R";
     public static final String QUIT = "Q";
     public static final String BRIDGE_START = "[ ";
