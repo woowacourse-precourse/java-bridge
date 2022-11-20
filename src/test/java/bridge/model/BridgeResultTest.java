e8bec3b6eefcf41341671776512fc1d836a3a6a4
YuHayeon <youha0930@gmail.com>
2022-11-21 01:32:32 +0900

    test(BridgeResult): 올바르게 마지막 결과를 반환하는지 확인

--- src/test/java/bridge/model/BridgeResultTest.java
+++ src/test/java/bridge/model/BridgeResultTest.java
@@ -1,19 +1,17 @@
-//package bridge.model;
-//
-//import static org.assertj.core.api.Assertions.assertThat;
-//
-//import java.util.List;
-//import org.junit.jupiter.api.Test;
-//
-//class BridgeResultTest {
-//
-//    @Test
-//    void canMove() {
-//        Bridge bridge = new Bridge(List.of("U", "D", "D"));
-//        assertThat(new BridgeResult(List.of(false)).canMove(bridge)).isEqualTo(false);
-//        assertThat(new BridgeResult(List.of(true, true)).canMove(bridge)).isEqualTo(true);
-//        assertThat(new BridgeResult(List.of(true, true, false)).canMove(bridge)).isEqualTo(false);
-//        assertThat(new BridgeResult(List.of(true, true, true)).canMove(bridge)).isEqualTo(false);
-//    }
-//
-//}
+package bridge.model;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.List;
+import org.junit.jupiter.api.Test;
+
+class BridgeResultTest {
+
+    @Test
+    void isRightLastResult() {
+        assertThat(new BridgeResult(List.of(false)).getLast()).isEqualTo(false);
+        assertThat(new BridgeResult(List.of(true, true)).getLast()).isEqualTo(true);
+        assertThat(new BridgeResult(List.of(true, true, false)).getLast()).isEqualTo(false);
+        assertThat(new BridgeResult(List.of(true, true, true)).getLast()).isEqualTo(true);
+    }
+}
