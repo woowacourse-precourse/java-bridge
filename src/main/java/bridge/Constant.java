082ab6f031ce1ae168465467d7cc42e18248be2b
Limjongho <whdgh9873@icloud.com>
2022-11-20 15:03:24 +0900

    feat(Constant): 다리 길이 범위 상수 추가

--- src/main/java/bridge/Constant.java
+++ src/main/java/bridge/Constant.java
@@ -6,4 +6,7 @@ package bridge;
 public class Constant {
     public static final String MOVING_UP = "U";
     public static final String MOVING_DOWN = "D";
+
+    public static final int MIN_BRIDGE_LENGTH = 3;
+    public static final int MAX_BRIDGE_LENGTH = 20;
 }
