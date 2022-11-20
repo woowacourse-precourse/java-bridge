339230fe2034cddb532dc9cf286e2d9abf391be5
im-shung <seoyoung1113@gmail.com>
2022-11-20 16:28:59 +0900

    feat: :sparkles: 다리의 길이를 검증하는 기능 추가

--- src/main/java/bridge/exception/Validator.java
+++ src/main/java/bridge/exception/Validator.java
@@ -1,5 +1,12 @@
 package bridge.exception;
 
+import bridge.domain.BrideLength;
+
 public class Validator {
 
+    public static void checkBridgeLength(int bridgeLength) {
+        if (bridgeLength < BrideLength.MIN.getSize() || bridgeLength > BrideLength.MAX.getSize()) {
+            throw new IllegalArgumentException(Error.WRONG_BRIDGE_LENGTH.getMessage());
+        }
+    }
 }
