4add5ad6b0c50663b375c8c2b9b5d6a5c7b673dc
kanghyki <kanghyki@gmail.com>
2022-11-21 01:22:59 +0900

    test: BridgeGameTest 테스트 별 @DisplayName 설정

--- src/test/java/bridge/BridgeGameTest.java
+++ src/test/java/bridge/BridgeGameTest.java
@@ -5,6 +5,7 @@ import static org.assertj.core.util.Lists.newArrayList;
 import bridge.ApplicationTest.TestNumberGenerator;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class BridgeGameTest {
@@ -18,6 +19,7 @@ class BridgeGameTest {
         game.initBridge(bridgeMaker.makeBridge(3));
     }
 
+    @DisplayName("move() 메서드 동작 테스트")
     @Test
     void move() {
         game.move(BridgeGame.CMD_UP);
@@ -25,6 +27,7 @@ class BridgeGameTest {
         Assertions.assertThat(game.getLastCommand()).isEqualTo(BridgeGame.CMD_UP);
     }
 
+    @DisplayName("재시작 메서드 동작 테스트")
     @Test
     void retry() {
         game.retry();
@@ -33,6 +36,7 @@ class BridgeGameTest {
         Assertions.assertThat(game.getTryCount()).isEqualTo(3);
     }
 
+    @DisplayName("플레이어 생존 확인 메서드 동작 테스트")
     @Test
     void isPlayerAlive() {
         Assertions.assertThat(game.isPlayerAlive()).isEqualTo(true);
@@ -40,6 +44,7 @@ class BridgeGameTest {
         Assertions.assertThat(game.isPlayerAlive()).isEqualTo(false);
     }
 
+    @DisplayName("플레이어 승리 확인 메서드 (성공) 동작 테스트")
     @Test
     void isSuccess_true() {
         game.move(BridgeGame.CMD_UP);
@@ -48,6 +53,7 @@ class BridgeGameTest {
         Assertions.assertThat(game.isSuccess()).isEqualTo(true);
     }
 
+    @DisplayName("플레이어 성공 확인 메서드 (실패) 동작 테스트")
     @Test
     void isSuccess_false() {
         game.move(BridgeGame.CMD_UP);
@@ -56,6 +62,7 @@ class BridgeGameTest {
         Assertions.assertThat(game.isSuccess()).isEqualTo(false);
     }
 
+    @DisplayName("결과값 문자열 변환 메서드 (성공) 동작 테스트")
     @Test
     void getGameResultToString_success() {
         game.move(BridgeGame.CMD_UP);
@@ -64,6 +71,7 @@ class BridgeGameTest {
         Assertions.assertThat(game.getGameResultToString()).isEqualTo("성공");
     }
 
+    @DisplayName("결과값 문자열 변환 메서드 (실패) 동작 테스트")
     @Test
     void getGameResultToString_failed() {
         game.move(BridgeGame.CMD_UP);
