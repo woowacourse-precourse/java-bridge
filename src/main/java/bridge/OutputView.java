b247b40b592d223848e57bf704d57bd22f3eb503
이요환 <91039622+bumstead-bumstead@users.noreply.github.com>
2022-11-20 22:29:07 +0900

    <feat>(OutputView.java): 메세지 출력 상수/메서드 생성
     - 시작 메세지, 이동 메세지, 재시도 메세지 출력하는 상수/메서드 작성

--- src/main/java/bridge/OutputView.java
+++ src/main/java/bridge/OutputView.java
@@ -5,6 +5,10 @@ package bridge;
  */
 public class OutputView {
 
+    private static final String START_MESSAGE = "다리 건너기 게임을 시작합니다.";
+    private static final String MOVE_MESSAGE = "이동할 칸을 선택해주세요. (위: U, 아래: D)";
+    private static final String RETRY_MESSAGE = "게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)";
+
     /**
      * 현재까지 이동한 다리의 상태를 정해진 형식에 맞춰 출력한다.
      * <p>
@@ -19,5 +23,17 @@ public class OutputView {
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
     public void printResult() {
+    }
+
+    public void printStartMessage() {
+        System.out.println(START_MESSAGE);
+    }
+
+    public void printMoveMessage() {
+        System.out.println(MOVE_MESSAGE);
+    }
+
+    public void printRetryMessage() {
+        System.out.println(RETRY_MESSAGE);
     }
 }
