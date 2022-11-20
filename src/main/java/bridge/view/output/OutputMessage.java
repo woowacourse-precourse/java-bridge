c35629369232f431fa85d8ee91ac2090e1e2e30a
lcw3176 <lcw3176@naver.com>
2022-11-20 11:35:26 +0900

    chore(OutputMessage): 빈줄 삽입 추가

--- src/main/java/bridge/view/output/OutputMessage.java
+++ src/main/java/bridge/view/output/OutputMessage.java
@@ -4,7 +4,7 @@ public class OutputMessage {
 
 
     public static final String WELCOME_MESSAGE = "다리 건너기 게임을 시작합니다.";
-    public static final String GAME_RESULT = "최종 게임 결과";
+    public static final String GAME_RESULT = "\n최종 게임 결과";
 
     private OutputMessage() {
 
