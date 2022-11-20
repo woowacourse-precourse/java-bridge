3fdf2ed4b32c8618d7750e640f1db53a94b10428
KYR <freekyr7529@gmail.com>
2022-11-21 08:03:22 +0900

    feature : Command 클래스에 searchCommand 메서드 생성
    입력한 문자에 부합하는 Enum 항목을 찾는 메서드 생성

--- src/main/java/bridge/constant/Command.java
+++ src/main/java/bridge/constant/Command.java
@@ -1,5 +1,7 @@
 package bridge.constant;
 
+import java.util.Arrays;
+
 public enum Command {
 
     DO_RETRY("R", "재시도"),
@@ -19,5 +21,12 @@ public enum Command {
 
     public String getDetail() {
         return detail;
+    }
+
+    public static Command searchCommand(String input) {
+        return Arrays.stream(Command.values())
+                .filter(command -> input.equals(command.command))
+                .findAny()
+                .orElseThrow(() -> new IllegalArgumentException("허용되지 않는 값! R(재시작) Q(종료) 중 하나를 입력하셔야 합니다!"));
     }
 }
