bafb281f081dbbd0919683b08898cc983b4ef34b
ykmxxi <ykmxxi97@gmail.com>
2022-11-20 15:28:58 +0900

    feat: 게임 진행 기능 skeleton code 구현

---
+++ src/main/java/bridge/controller/BridgeGameController.java
@@ -0,0 +1,29 @@
+package bridge.controller;
+
+import bridge.service.BridgeGame;
+
+public class BridgeGameController {
+    private final BridgeGame game;
+
+    public BridgeGameController(BridgeGame game) {
+        this.game = game;
+    }
+
+    public void startGame() {
+    }
+
+    public void getBridgeSize() {
+    }
+
+    public void getMoving() {
+    }
+
+    public void printMap() {
+    }
+
+    public void getGameCommand() {
+    }
+
+    public void printResult() {
+    }
+}
