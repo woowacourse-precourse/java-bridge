9570f2f2c8bc67bf07cd7976f74a26ed0dc9f9cf
ttaehee <ttaehee1105@gmail.com>
2022-11-20 22:08:23 +0900

    feat : 이동 가능 칸인지 체크 기능 구현

---
+++ src/main/java/bridge/Bridge.java
@@ -0,0 +1,16 @@
+package bridge;
+
+import java.util.List;
+
+public class Bridge {
+    private final List<String> bridgeRoute;
+
+    public Bridge(List<String> bridgeRoute) {
+        this.bridgeRoute = bridgeRoute;
+    }
+
+    public Mark matchRoute(String inputDirection, int movingCount) {
+        boolean isRight = bridgeRoute.get(movingCount).equals(inputDirection);
+        return Mark.toMark(inputDirection, isRight);
+    }
+}
