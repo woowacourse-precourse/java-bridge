4f956b423367e20b1311820c2201408a0adadd92
jungwoo <rlawjddn103@naver.com>
2022-11-21 00:55:33 +0900

    feat(User): 생성자 및 변수 초기화

--- src/main/java/bridge/domain/User.java
+++ src/main/java/bridge/domain/User.java
@@ -1,14 +1,25 @@
 package bridge.domain;
 
+import bridge.ui.InputView;
+import bridge.ui.OutputView;
+
+import java.util.ArrayList;
 import java.util.List;
 
 public class User {
 
-    private List<String> route;
-    private int attemptNum;
-    private int location;
+    private List<String> route = new ArrayList<>();
+    private int attemptNum = 1;
+    private int location = -1;
     private int bridgeSize;
-    private Status status;
+    private Status status = Status.DOING;
     private Bridge bridge;
+    private InputView inputView = new InputView();
+    private OutputView outputView = new OutputView();
+
+    User(){
+        this.bridgeSize = inputView.readBridgeSize();
+        this.bridge = new Bridge(this.bridgeSize,new BridgeRandomNumberGenerator());
+    }
 
 }
