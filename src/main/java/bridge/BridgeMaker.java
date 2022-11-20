11e9a4091ae8b38d0e9d4e75435d14dbde936bda
hyungkya <hyungkya@uci.edu>
2022-11-21 01:40:27 +0900

    feat: 다리 길이 검증하기
    조건:
    - 3과 20 사이의 수

--- src/main/java/bridge/BridgeMaker.java
+++ src/main/java/bridge/BridgeMaker.java
@@ -18,6 +18,13 @@ public class BridgeMaker {
      * @return 입력받은 길이에 해당하는 다리 모양. 위 칸이면 "U", 아래 칸이면 "D"로 표현해야 한다.
      */
     public List<String> makeBridge(int size) {
+        validate(size);
         return null;
+    }
+
+    private void validate(int bridgeSize) {
+        if (bridgeSize < 3 || bridgeSize > 20) {
+            throw new IllegalArgumentException("[ERROR] 다리 길이는 3이상 20이하여야 합니다.");
+        }
     }
 }
