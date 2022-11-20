c471a5e63c3bb24c86a692cf6d935218ea076de2
kang jihyeong <her4386@gmail.com>
2022-11-20 21:46:15 +0900

    Refactor / 다리 개수 환경 설정

---
+++ src/main/java/bridge/service/BridgeMaker.java
@@ -0,0 +1,34 @@
+package bridge.domain;
+
+import bridge.exception.BridgeIllegalArgumentException;
+import bridge.service.BridgeNumberGenerator;
+import bridge.setting.BridgeMakerEnum;
+import bridge.setting.Setting;
+import bridge.view.InputView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class BridgeMaker {
+
+    private final BridgeNumberGenerator bridgeNumberGenerator;
+
+    public BridgeMaker(BridgeNumberGenerator bridgeNumberGenerator) {
+        this.bridgeNumberGenerator = bridgeNumberGenerator;
+    }
+
+    public List<String> makeBridge(int size) {
+        List<String> bridge = new ArrayList<>();
+
+        for (int i = 0; i < size; i++) {
+            bridge.add(String.valueOf(judgeRandomNumber(bridgeNumberGenerator.generate())));
+        }
+
+        return bridge;
+    }
+
+    private char judgeRandomNumber(int randomNumber) {
+        return BridgeMakerEnum.valuesOfBridge(randomNumber);
+    }
+
+}
--- src/main/java/bridge/setting/Setting.java
+++ src/main/java/bridge/setting/Setting.java
@@ -2,6 +2,8 @@ package bridge.setting;
 
 public class Setting {
 
+    public static final int BRIDGE_COUNT = 2;
+
     public static final int MIN_BRIDGE_SIZE = 3;
     public static final int MAX_BRIDGE_SIZE = 20;
 
