d696ac32f6568f6663a2f187dd2987df223fb2fa
aak2075 <35948985+aak2075@users.noreply.github.com>
2022-11-20 20:57:00 +0900

    fix(bridge) : 다리 길이 입력받는 잘못된 로직 수정

--- src/main/java/bridge/view/InputView.java
+++ src/main/java/bridge/view/InputView.java
@@ -11,8 +11,7 @@ public class InputView {
      * 다리의 길이를 입력받는다.
      */
     public int readBridgeSize() {
-        Console.readLine();
-        return 0;
+        return Integer.parseInt(Console.readLine());
     }
 
     /**
