98eecf6d09452fa3d1f50d0088bb58464e8f5ecd
K-IMjihun <62210749+K-IMjihun@users.noreply.github.com>
2022-11-20 16:37:54 +0900

    OutputView // 올바른 값이 출력되도록 수정

--- src/main/java/bridge/OutputView.java
+++ src/main/java/bridge/OutputView.java
@@ -2,6 +2,7 @@ package bridge;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * 사용자에게 게임 진행 상황과 결과를 출력하는 역할을 한다.
@@ -11,37 +12,45 @@ import java.util.List;
  // * 값 출력을 위해 필요한 메서드를 추가할 수 있다.
  */
 public class OutputView {
-    static List<String> printMap1 = new ArrayList<>();
-    static List<String> printMap2 = new ArrayList<>();
+    static List<String> upByPrintMap = new ArrayList<>();
+    static List<String> downByPrintMap = new ArrayList<>();
+
 
     /**
      * 현재까지 이동한 다리의 상태를 정해진 형식에 맞춰 출력한다.
      * <p>
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void printMap(String bridge) {
-        if(bridge == "U"){
-             printMap1.add("O");
-             printMap2.add(" ");
-        }
-        if(bridge == "D"){
-            printMap1.add(" ");
-            printMap2.add("O");
-        }
-        System.out.println(printMap1);
-        System.out.println(printMap2);
+    public void printMap(String upAndDown) {
+        trueCalculation(upAndDown);     // 성공시 입력값에 따라 O 삽입
+        System.out.println(upByPrintMap.stream().collect(Collectors.joining("|","[","]")) + "\n" + downByPrintMap.stream().collect(Collectors.joining("|","[","]")));
     }
-    public void wrongPrintMap(String bridge){
-        if(bridge == "U"){
-            printMap1.add(" ");
-            printMap2.add("X");
+
+    //11줄
+    public void falsePrintMap(String upAndDown){
+        falseCalculation(upAndDown);    // 실패시 입력값에 따라 X 삽입
+        System.out.println(upByPrintMap.stream().collect(Collectors.joining("|","[","]")) + "\n" + downByPrintMap.stream().collect(Collectors.joining("|","[","]")));
+    }
+
+    static void trueCalculation(String upAndDown){
+        if(upAndDown.equals("U")){
+            upByPrintMap.add(" O ");
+            downByPrintMap.add("   ");
         }
-        if(bridge == "D"){
-            printMap1.add("X");
-            printMap2.add(" ");
+        if(upAndDown.equals("D")){
+            upByPrintMap.add("   ");
+            downByPrintMap.add(" O ");
         }
-        System.out.println(printMap1);
-        System.out.println(printMap2);
+    }
+    static void falseCalculation(String upAndDown){
+        if(upAndDown.equals("U")){
+            upByPrintMap.add(" X ");
+            downByPrintMap.add("   ");
+        }
+        if(upAndDown.equals("D")){
+            upByPrintMap.add("   ");
+            downByPrintMap.add(" X ");
+        }
     }
 
     /**
@@ -50,10 +59,8 @@ public class OutputView {
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
     public void printResult(String result, int count) {
-        System.out.println();
-        System.out.println("최종 게임 결과");
-        System.out.println(printMap1);
-        System.out.println(printMap2);
+        System.out.println("\n" + "최종 게임 결과");
+        System.out.println(upByPrintMap.stream().collect(Collectors.joining("|","[","]")) + "\n" + downByPrintMap.stream().collect(Collectors.joining("|","[","]")));
         System.out.println("게임 성공 여부: " + result);
         System.out.println("총 시도한 횟수: " + count);
     }
