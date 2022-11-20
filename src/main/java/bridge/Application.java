fa73a8fb1eef74282dd8b66b9722ec4b8c5f1b35
yenawee <yaena0319@naver.com>
2022-11-20 17:23:31 +0900

    feat : 사용자가 입력한 값과 다리 값 비교하면서 true/false 반환

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -1,8 +1,22 @@
 package bridge;
 
+import java.util.List;
+
 public class Application {
 
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        BridgeNumberGenerator numberGenerator = new BridgeRandomNumberGenerator();
+        BridgeMaker bridgeMaker = new BridgeMaker(numberGenerator);
+        InputView inputView = new InputView();
+        BridgeGame bridgeGame = new BridgeGame();
+        int bridgeSize = inputView.readBridgeSize(inputView.readline());
+        List<String> bridge = bridgeMaker.makeBridge(bridgeSize);
+        while (true){
+            String readMove = inputView.readMoving(inputView.readline());
+            boolean move = bridgeGame.move(bridge, readMove);
+
+
+        }
+
     }
 }
--- src/main/java/bridge/BridgeGame.java
+++ src/main/java/bridge/BridgeGame.java
@@ -1,16 +1,24 @@
 package bridge;
 
+import java.util.List;
+
+import static org.assertj.core.util.Lists.newArrayList;
+
 /**
  * 다리 건너기 게임을 관리하는 클래스
  */
 public class BridgeGame {
-
+    static int index = 0;
     /**
      * 사용자가 칸을 이동할 때 사용하는 메서드
      * <p>
      * 이동을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void move() {
+    public boolean move(List<String> bridge, String readMoving) {
+        if (bridge.get(index).equals(readMoving)){
+            return (true);
+        }
+        return (false);
     }
 
     /**
@@ -19,5 +27,7 @@ public class BridgeGame {
      * 재시작을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
     public void retry() {
+        index = 0;
+
     }
 }
