25e336d1125af5cd41e03fabae30fd31671335d5
oreo <ohsimon77@naver.com>
2022-11-20 20:41:44 +0900

    Feat : 간단한 출력함수 만들어보고 메인함수 수정하기

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -4,5 +4,8 @@ public class Application {
 
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        BridgeMaker.makeBridge(3);
+        OutputView outputView = new OutputView();
+        outputView.printMap(BridgeMaker.bridge);
     }
 }
