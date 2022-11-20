75b621df8bc8ef4b1f0c0ea07cef562497d629e4
impati <impati0716@icloud.com>
2022-11-20 17:04:40 +0900

    feat (ProxyInputView) : 에러 출력 기능 추가

---
+++ src/main/java/bridge/ProxyInputView.java
@@ -0,0 +1,40 @@
+package bridge;
+
+public class ProxyInputView implements Input{
+    private final Input input;
+    public ProxyInputView(Validation validation) {
+        input = new InputView(validation);
+    }
+    @Override
+    public int readBridgeSize() {
+        while (true) {
+            try {
+                return input.readBridgeSize();
+            } catch (IllegalArgumentException e) {
+                System.out.println(e);
+            }
+        }
+    }
+
+    @Override
+    public String readMoving() {
+        while (true) {
+            try {
+                return input.readMoving();
+            } catch (IllegalArgumentException e) {
+                System.out.println(e);
+            }
+        }
+    }
+
+    @Override
+    public String readGameCommand() {
+        while (true) {
+            try {
+                return input.readGameCommand();
+            } catch (IllegalArgumentException e) {
+                System.out.println(e);
+            }
+        }
+    }
+}
