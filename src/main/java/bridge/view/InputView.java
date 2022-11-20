06b9be3c149d61515787a9f797cbc6a37dd87828
alpacaHyun <flaehdan@gmail.com>
2022-11-21 08:32:49 +0900

    type : feat ✨ 위 아래 방향을 입력받는다.
    body :

--- src/main/java/bridge/view/InputView.java
+++ src/main/java/bridge/view/InputView.java
@@ -26,7 +26,9 @@ public class InputView {
      * 사용자가 이동할 칸을 입력받는다.
      */
     public String readMoving() {
-
+        System.out.println(INPUT_MOVING_DIRECTION);
+        String inputMoving = Console.readLine();
+        return inputMoving;
     }
 
     /**
