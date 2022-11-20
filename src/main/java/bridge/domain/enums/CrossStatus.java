32048213f2d056e4bf72b16d8403799c896f084c
HiiWee <wpdlzhf159@gmail.com>
2022-11-21 02:24:02 +0900

    refactor: 일급 컬렉션이 아닌 리스트를 반환하도록 변경

--- src/main/java/bridge/domain/enums/CrossStatus.java
+++ src/main/java/bridge/domain/enums/CrossStatus.java
@@ -1,17 +1,16 @@
 package bridge.domain.enums;
 
-import bridge.domain.CrossStatuses;
 import java.util.ArrayList;
 import java.util.List;
 
 public enum CrossStatus {
     SUCCESS, FAIL, YET;
 
-    public static CrossStatuses createInitializationStatuses(final int bridgeSize) {
+    public static List<CrossStatus> createInitializationStatuses(final int bridgeSize) {
         List<CrossStatus> crossStatuses = new ArrayList<>();
         for (int bridgeIndex = 0; bridgeIndex < bridgeSize; bridgeIndex++) {
             crossStatuses.add(YET);
         }
-        return new CrossStatuses(crossStatuses);
+        return crossStatuses;
     }
 }
