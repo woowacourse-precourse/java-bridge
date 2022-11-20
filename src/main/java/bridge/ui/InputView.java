9e55ed8d37075a2d64b116203a2557bff5a4f517
jungwoo <rlawjddn103@naver.com>
2022-11-20 20:57:23 +0900

    feat(InputView): 다리의 길이를 입력 받고 입력값에 예외 처리 적용 후 숫자로 반환하는 기능 추가

--- src/main/java/bridge/ui/InputView.java
+++ src/main/java/bridge/ui/InputView.java
@@ -1,27 +1,22 @@
 package bridge.ui;
 
-import java.util.regex.Pattern;
+import camp.nextstep.edu.missionutils.Console;
 
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
  */
 public class InputView {
 
-    private final Pattern NUMBER_PATTERN = Pattern.compile("^[0-9]*$");
-    private final String ERROR_NOT_INTEGER = "[ERROR] 숫자가 아닙니다.";
-
     /**
      * 다리의 길이를 입력받는다.
      */
     public int readBridgeSize() {
-        return 0;
+        String input = Console.readLine();
+        InputViewValidation.validateIsInteger(input);
+        InputViewValidation.validateIsInRange(input);
+        return Integer.parseInt(input);
     }
 
-    private void validateIsInteger(String input) {
-        if(!NUMBER_PATTERN.matcher(input).find()){
-            throw new IllegalArgumentException(ERROR_NOT_INTEGER);
-        }
-    }
     /**
      * 사용자가 이동할 칸을 입력받는다.
      */
