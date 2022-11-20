902219ae810c8844788e3721aa99ba86f58849e5
namtndus <rnrmf0721@gmail.com>
2022-11-20 14:24:31 +0900

    feat(Application): Application의 인스턴스 변수 생성
    'bridgeGame 변수 생성'를 생성

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -4,6 +4,7 @@ public class Application {
 
     InputView inputView = new InputView();
     OutputView outputView = new OutputView();
+    BridgeGame bridgeGame = new BridgeGame();
 
     public static void main(String[] args) {
         // TODO: 프로그램 구현
