f6b3014e5000d8fc9444470c8e29b48f7ce973cd
inhwanK <79566221+inhwanK@users.noreply.github.com>
2022-11-21 01:40:54 +0900

    fix(Application): 메서드 분리 및 리팩토링

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -11,29 +11,68 @@ public class Application {
     private static OutputView output = new OutputView();
     private static BridgeMaker maker = new BridgeMaker(new BridgeRandomNumberGenerator());
     private static BridgeGame game = new BridgeGame();
+    
     private static List<String> bridge = new ArrayList<>();
-
+    private static GameState state = GameState.START;
+    private static int spaceNum;
+    private static int bridgeSize;
 
     public static void main(String[] args) {
         try {
             // 게임 시작
             output.printStartMessage();
-            output.printBridgeLengthInputRequestMessage();
-            int bridgeSize = input.readBridgeSize();
-            bridge = maker.makeBridge(bridgeSize); // 다리 만들기
-//            System.out.println(bridge.toString());
+            createBridge();
 
-            int spaceNum = 0;
+            // 게임
+            while(state == GameState.START || state == GameState.CORRECT || state == GameState.RESTART) {
+                goToNextSpace();
 
-            output.printMoveSpaceInputRequestMessage();
-            String moveSpace = input.readMoving();
-            Boolean result = game.move(moveSpace, spaceNum, bridge);
-            spaceNum++;
+                if(state == GameState.INCORRECT) {
+                    chooseGameContinue();
+                }
 
+                if(spaceNum == bridgeSize && state == GameState.CORRECT) {
+                    state = GameState.QUIT;
+                    System.out.println("게임 성공 여부: 성공");
+                }
+            }
         } catch (IllegalArgumentException e) {
             System.out.printf("[ERROR] %s", e.getMessage());
         }
 
         // TODO: 프로그램 구현
+    }
+
+    private static void goToNextSpace() {
+        output.printMoveSpaceInputRequestMessage();
+        String moveSpace = input.readMoving();
+        boolean correct = game.move(moveSpace, spaceNum, bridge);
+        spaceNum++;
+        if(correct) {
+            state = GameState.CORRECT;
+            return;
+        }
+        state = GameState.INCORRECT;
+    }
+
+    private static void createBridge() {
+        output.printBridgeLengthInputRequestMessage();
+
+        bridgeSize = input.readBridgeSize();
+        bridge = maker.makeBridge(bridgeSize);
+
+        System.out.println(bridge.toString());
+    }
+
+    private static void chooseGameContinue() {
+        output.printChoseRetryInputRequestMessage();
+        String gameCommand = input.readGameCommand();
+        boolean restart = game.retry(gameCommand);
+        if(restart) {
+            state = GameState.RESTART;
+            spaceNum = 0;
+            return;
+        }
+        state = GameState.QUIT;
     }
 }
