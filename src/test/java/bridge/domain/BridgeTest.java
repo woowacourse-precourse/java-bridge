06fcc62705c55b07887c0bab5759d35131bf481f
Miseong Kim <miseongk@naver.com>
2022-11-21 01:44:49 +0900

    test(Bridge): validate throw exception when bride size is not between 3 and 20

--- src/test/java/bridge/domain/BridgeTest.java
+++ src/test/java/bridge/domain/BridgeTest.java
@@ -2,11 +2,13 @@ package bridge.domain;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
 import java.util.List;
 
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.*;
 
 class BridgeTest {
@@ -15,6 +17,16 @@ class BridgeTest {
     @BeforeEach
     void init() {
         bridge = new Bridge(List.of("U", "D", "D"), 3);
+    }
+
+    @DisplayName("다리 길이가 3부터 20 사이여야 한다.")
+    @Test
+    void validateBridgeSize() {
+        assertThatThrownBy(() -> new Bridge(List.of("U", "D"), 2))
+                .isInstanceOf(IllegalArgumentException.class);
+        assertThatThrownBy(() -> new Bridge(List.of("U", "D", "U", "D", "U", "D", "U", "D",
+                "U", "D", "U", "D", "U", "D", "U", "D", "U", "D", "U", "D", "U"), 21))
+                .isInstanceOf(IllegalArgumentException.class);
     }
 
     @DisplayName("사용자가 입력한 칸이 이동 가능한지 테스트")
