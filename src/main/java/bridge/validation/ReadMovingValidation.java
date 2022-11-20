a432c7a0b29f663635fa668fe40cdda8b0b0b8f1
desfox <desfox99@naver.com>
2022-11-20 22:54:37 +0900

    refactor(ReadMovingValidation): 매직리터럴 리팩토링

--- src/main/java/bridge/validation/ReadMovingValidation.java
+++ src/main/java/bridge/validation/ReadMovingValidation.java
@@ -1,8 +1,11 @@
 package bridge.validation;
 
 public class ReadMovingValidation {
+    private static final String DOWN_MOVING_COMMAND = "D";
+    private static final String UP_MOVING_COMMAND = "U";
+
     public void validateInputFormat(String movingCommand) {
-        if ((! movingCommand.equals("D")) && (! movingCommand.equals("U"))) {
+        if ((! movingCommand.equals(DOWN_MOVING_COMMAND)) && (! movingCommand.equals(UP_MOVING_COMMAND))) {
             throw new IllegalArgumentException("[ERROR] 이동할 칸을 D 또는 U로 입력해주세요.");
         }
     }
