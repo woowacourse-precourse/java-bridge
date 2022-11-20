a07d9df3cd7486b07a9f6f68134ea7416344ea85
miffyKing <whqudghk28@gmail.com>
2022-11-21 01:23:36 +0900

    feat: Make BridgeGame Class to distinguish 1 length alphabet from user.

---
+++ src/main/java/bridge/model/BridgeGame.java
@@ -0,0 +1,72 @@
+package bridge.model;
+
+import bridge.view.Sentence;
+
+/**
+ * 다리 건너기 게임을 관리하는 클래스
+ */
+public class BridgeGame {
+
+    private final String UpDownRetryQuit;
+    private static final int UP_DOWN_MODE = 1;
+    private static final int RETRY_MODE = 2;
+
+    private BridgeGame(String userInput, int mode) {
+        validityInput(userInput, mode);
+        this.UpDownRetryQuit = userInput;
+    }
+
+    public static BridgeGame from(final String userInput, int mode) {
+        return new BridgeGame(userInput, mode);
+    }
+
+    private void validityInput(String userInput, int mode) {
+        if (mode == UP_DOWN_MODE) {
+            checkUpDownValidity(userInput);
+        }
+        if (mode == RETRY_MODE) {
+            checkRetryValidity(userInput);
+        }
+    }
+
+    private void checkUpDownValidity(String userInput) {
+        if (!"UD".contains(userInput)) {
+            throw new IllegalArgumentException(Sentence.INPUT_NOT_U_OR_D.getValue());
+        }
+    }
+
+    private void checkRetryValidity(String userInput) {
+        if (!"RQ".contains(userInput)) {
+            throw new IllegalArgumentException(Sentence.INPUT_NOT_R_OR_Q.getValue());
+        }
+    }
+
+    /**
+     * 사용자가 칸을 이동할 때 사용하는 메서드
+     * <p>
+     * 이동을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
+     */
+    public int move(BridgeAnswer bridgeAnswer) {
+        BridgePiece bridgePiece = new BridgePiece(UpDownRetryQuit);
+        int compareResult = bridgePiece.compareWithAnswer(bridgeAnswer);
+        if (compareResult == 1) {
+            return 1;
+        } else if (compareResult == 2) {
+            return 2;
+        }
+        return 3;
+    }
+
+    /**
+     * 사용자가 게임을 다시 시도할 때 사용하는 메서드
+     * <p>
+     * 재시작을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
+     */
+    public boolean retry(BridgeAnswer bridgeAnswer) {
+        BridgePiece.replayCurrentPlaceReset(bridgeAnswer);
+        if (UpDownRetryQuit.equals(Sentence.RETRY_CHUNK.getValue())) {
+            return true;
+        }
+        return false;
+    }
+}
--- src/main/java/bridge/view/Sentence.java
+++ src/main/java/bridge/view/Sentence.java
@@ -3,11 +3,20 @@ package bridge.view;
 public enum Sentence {
     INPUT_MONEY("구입금액을 입력해 주세요."),
     YOU_PURCHASED("\n%d개를 구매했습니다."),
+    SUCCESS("성공"),
+    FAILURE("실패"),
+
+    UP_AND_DOWN("UD"),
+    RETRY_AND_QUIT("QR"),
 
     UP_CHUNK("U"),
     DOWN_CHUNK("D"),
+    RETRY_CHUNK("R"),
+    QUIT_CHUNK("Q"),
 
-    LENGTH_NOT_IN_RANGE("[ERROR] Input is not in range");
+    LENGTH_NOT_IN_RANGE("[ERROR] Input is not in range"),
+    INPUT_NOT_U_OR_D("[ERROR] Input is not U or D"),
+    INPUT_NOT_R_OR_Q("[ERROR] Input is not Q or R");
 
     private final String s;
 
