76cfde8011d89269fa47914ca3965b9d367a44d5
taehyun <didls1643@gmail.com>
2022-11-20 21:34:08 +0900

    feat: 다리 게임에서 움직이는 기능 구현

--- src/main/java/bridge/BridgeGame.java
+++ src/main/java/bridge/BridgeGame.java
@@ -1,16 +1,36 @@
 package bridge;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * 다리 건너기 게임을 관리하는 클래스
  */
 public class BridgeGame {
+
+    private int tryNum;
+    private List<String> move;
+    final private List<String> bridge;
+
+    public BridgeGame(int bridgeSize){
+        BridgeRandomNumberGenerator bridgeRandomNumberGenerator = new BridgeRandomNumberGenerator();
+        BridgeMaker bridgeMaker = new BridgeMaker(bridgeRandomNumberGenerator);
+        move = new ArrayList<>();
+        tryNum = 0;
+        bridge = bridgeMaker.makeBridge(bridgeSize);
+    }
 
     /**
      * 사용자가 칸을 이동할 때 사용하는 메서드
      * <p>
      * 이동을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void move() {
+    public boolean move(String input) {
+        if(bridge.get(move.size()) != input){
+            return false;
+        }
+        move.add(input);
+        return true;
     }
 
     /**
