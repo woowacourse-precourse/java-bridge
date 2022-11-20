a0c1c6bcb37951a0df5a54cb19dc3442e5c138fc
fakerdeft <whakswp123@naver.com>
2022-11-20 22:26:42 +0900

    feat: 게임 시작 메서드, 다리 만드는 메서드 변경

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -4,5 +4,19 @@ public class Application {
 
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        gameStart();
+        makeBridge();
+    }
+
+    public static void gameStart() {
+        System.out.println(OutputView.printGameStartPhrase());
+        System.out.println();
+        System.out.println(OutputView.printBridgeLengthInputPhrase());
+    }
+
+    public static void makeBridge() {
+        BridgeNumberGenerator numberGenerator = new BridgeRandomNumberGenerator();
+        BridgeMaker bridgeMaker = new BridgeMaker(numberGenerator);
+        bridgeMaker.makeBridge(InputView.readBridgeSize());
     }
 }
