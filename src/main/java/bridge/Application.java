a20b662ebdaa71d3eceefb9b06a722284948fa48
SonMinGyu <littleking13@naver.com>
2022-11-20 23:08:19 +0900

    [FEAT] 입력받은 길이만큼 다리를 생성하는 기능 구현
    - 사용자가 입력받은 다리의 길이만큼 다리를 생성하는 기능을 구현함
    - BridgeNumberGenerator의 generate가 0이면 U인 다리를, 1이면 D인 다리를 생성하여 List<String>형의 변수에 추가하고 return 한다

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -1,14 +1,19 @@
 package bridge;
 
+import java.util.List;
+
 public class Application {
 
     public static void main(String[] args) {
         // TODO: 프로그램 구현
 
         printStart();
+        BridgeNumberGenerator bridgeNumberGenerator = new BridgeRandomNumberGenerator();
         InputView inputView = new InputView();
+        BridgeMaker bridgeMaker = new BridgeMaker(bridgeNumberGenerator);
 
         int bridgeSize = inputView.readBridgeSize();
+        List<String> bridge = bridgeMaker.makeBridge(bridgeSize);
 
     }
 
