9c40c67b4bbefb4fd583e5eb1b6798ff467bc675
LHS-11 <ghktjq1119@naver.com>
2022-11-20 15:55:47 +0900

    [Test] : 이동할 칸 문자 validate 테스트 구현

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -1,6 +1,5 @@
 package bridge;
 
-import bridge.controller.BridgeGameController;
 import bridge.model.BridgeMaker;
 
 import java.util.ArrayList;
@@ -11,8 +10,6 @@ public class Application {
 
     public static void main(String[] args) {
         // TODO: 프로그램 구현
-        BridgeGameController bridgeGameController = new BridgeGameController();
-        bridgeGameController.start();
 
     }
 }
