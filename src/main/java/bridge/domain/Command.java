d81402f6caff3f2d4e860f55b43189809f65a2f0
MoonJeWoong <munjin0201@naver.com>
2022-11-21 00:57:02 +0900

    feat(domain): 입력 커맨드에 대한 Enum 클래스 생성

---
+++ src/main/java/bridge/domain/Command.java
@@ -0,0 +1,21 @@
+package bridge.domain;
+
+import java.util.Arrays;
+
+public enum Command {
+
+    MOVE_UP("U"),
+    MOVE_DOWN("D"),
+    RETRY_GAME("R"),
+    QUIT_GAME("Q");
+
+    final private String value;
+
+    Command(String value){
+        this.value = value;
+    }
+
+    public String getValue(){
+        return this.value;
+    }
+}
