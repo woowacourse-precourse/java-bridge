2a449bb465a7fe77c496afe99255ae4ba646d10a
Yunsik-Choi <choiys0311@gmail.com>
2022-11-21 01:52:33 +0900

    ✨style : BridgeGame 메서드 주석 제거

--- src/main/java/bridge/domain/BridgeGame.java
+++ src/main/java/bridge/domain/BridgeGame.java
@@ -22,20 +22,10 @@ public class BridgeGame {
         this.tryCount = tryCount;
     }
 
-    /**
-     * 사용자가 칸을 이동할 때 사용하는 메서드
-     * <p>
-     * 이동을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
-     */
     public void move(BridgeShape moveBridgeShape) {
         attempts.connect(moveBridgeShape);
     }
 
-    /**
-     * 사용자가 게임을 다시 시도할 때 사용하는 메서드
-     * <p>
-     * 재시작을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
-     */
     public BridgeGame retry() {
         return new BridgeGame(bridge, tryCount.plusOne());
     }
