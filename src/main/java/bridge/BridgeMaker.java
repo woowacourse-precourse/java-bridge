ea7ca64b8d1c08a4d7e96b53862c9a03473bbbad
chanminmaxscaler <jcm04@naver.com>
2022-11-20 10:45:53 +0900

    feat: BridgeMaker 클래스에 다리의 길이 입력을 검증하는 기능을 구현한다
    BridgeMaker 클래스에 validateSize 메소드로 다리의 길이 입력을 검증하는 기능을 구현한다.
    validateSize 메소드는 입력된 int size의 범위가 3 미만 혹은 20 초과의 범위에 속한다면 IllegalArgumentException을 발생시킨다.

--- src/main/java/bridge/BridgeMaker.java
+++ src/main/java/bridge/BridgeMaker.java
@@ -6,9 +6,6 @@ import java.util.List;
 import static bridge.domain.direction.Direction.DOWNWARD_DIRECTION;
 import static bridge.domain.direction.Direction.UPWARD_DIRECTION;
 
-/**
- * 다리의 길이를 입력 받아서 다리를 생성해주는 역할을 한다.
- */
 public class BridgeMaker {
 
     private final BridgeNumberGenerator bridgeNumberGenerator;
@@ -22,12 +19,19 @@ public class BridgeMaker {
      * @return 입력받은 길이에 해당하는 다리 모양. 위 칸이면 "U", 아래 칸이면 "D"로 표현해야 한다.
      */
     public List<String> makeBridge(int size) {
+        validateSize(size);
         List<String> bridge = new ArrayList<>();
         for (int i = 0; i < size; i++) {
-            String direction = mapToDirection(bridgeNumberGenerator.generate());
-            bridge.add(direction);
+            int number = bridgeNumberGenerator.generate();
+            bridge.add(mapToDirection(number));
         }
         return bridge;
+    }
+
+    private void validateSize(int size) {
+        if (size < 3 || size > 20) {
+            throw new IllegalArgumentException("[ERROR] 다리 길이는 3부터 20 사이의 숫자여야 합니다.");
+        }
     }
 
     private String mapToDirection(int number) {
