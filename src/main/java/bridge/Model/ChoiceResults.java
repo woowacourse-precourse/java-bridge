492315a306ae1da4fd923b1a5f9baff8c6bd052c
TaeMin <xoals3094@naver.com>
2022-11-20 20:51:38 +0900

    refactor : 선택 결과 리스트를 일급 컬렉션으로 변경

---
+++ src/main/java/bridge/Model/ChoiceResults.java
@@ -0,0 +1,12 @@
+package bridge.Model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ChoiceResults {
+    List<String> choiceResults;
+
+    public ChoiceResults(){
+        this.choiceResults = new ArrayList<>();
+    }
+}
