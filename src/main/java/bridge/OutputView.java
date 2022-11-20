e1a9e116ef0d9cbe1f9f0ba822c3757f1db2ae9d
JH-TT <leejongho9803@gmail.com>
2022-11-21 01:11:25 +0900

    refactor(outputview) : 출력에 관련된 메시지들을 전부 Enum으로 처리

--- src/main/java/bridge/OutputView.java
+++ src/main/java/bridge/OutputView.java
@@ -3,6 +3,8 @@ package bridge;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import static bridge.OutputEnum.*;
+
 /**
  * 사용자에게 게임 진행 상황과 결과를 출력하는 역할을 한다.
  */
@@ -16,7 +18,7 @@ public class OutputView {
      * <p>
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public static void printMap(List<String> bridge) {
+    public static void printMap(final List<String> bridge) {
         printMapUp(bridge);
         printMapDown(bridge);
     }
@@ -39,11 +41,11 @@ public class OutputView {
      * <p>
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public static void printResult(List<String> bridge, int retCnt, String res) {
-        System.out.println("최종 게임 결과");
+    public static void printResult(final List<String> bridge,final int retCnt,final String res) {
+        OUTPUT_RESULT.getMessage();
         printMap(bridge);
         System.out.println();
-        System.out.println("게임 성공 여부: " + res);
-        System.out.println("총 시도한 횟수: " + retCnt);
+        IS_SUCCESS.getMessage(res);
+        TRY_COUNT.getMessage(Integer.toString(retCnt));
     }
 }
