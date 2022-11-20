202adc5ba148ef7d5ba6c531215f1d4568fbb611
Voyager003 <85725033+Voyager003@users.noreply.github.com>
2022-11-20 22:22:24 +0900

    Feat: 추가 - Player 클래스 이동할 칸을 입력받아 리스트에 저장하는 기능

--- src/main/java/bridge/domain/Player.java
+++ src/main/java/bridge/domain/Player.java
@@ -1,2 +1,17 @@
-package bridge.domain;public class Player {
+package bridge.domain;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Player {
+
+    private final List<String> playerMoving;
+
+    public Player() {
+        playerMoving = new ArrayList<>();
+    }
+
+    public void movePlayer(String moving) {
+        playerMoving.add(moving);
+    }
 }
