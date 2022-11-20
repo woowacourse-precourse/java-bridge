5cb9ddfaeb2eb66eb7f0dff49be0d6ec037eb2f7
ksh-beginner <ohk9134@naver.com>
2022-11-20 09:14:38 +0900

    refactor(Application) : BridgeGameController를 통해 게임 실행하도록 리팩토링

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -1,11 +1,11 @@
 package bridge;
 
-import bridge.controller.BridgeGame;
+import bridge.controller.BridgeGameController;
 
 public class Application {
 
     public static void main(String[] args) {
-        BridgeGame bridgeGame = new BridgeGame();
-        bridgeGame.playGame();
+        BridgeGameController bridgeGameController = new BridgeGameController();
+        bridgeGameController.playGame();
     }
 }
