e8cc7cfd1a297cd2c20e773535b354faae3b03d3
himjjong <jjijji5728@tukorea.ac.kr>
2022-11-21 02:32:55 +0900

    feat : 게임을 진행하며 나오는 출력문 상수로 정의하여 구현

--- src/main/java/bridge/view/OrderView.java
+++ src/main/java/bridge/view/OrderView.java
@@ -4,7 +4,11 @@ public class OrderView {
     public final String START_ORDER = "다리 건너기 게임을 시작합니다.";
     public final String INPUT_BRIDGE_LENGTH = "다리의 길이를 입력해주세요.";
     public final String ERROR_ORDER = "[ERROR]";
-
+    public final String MOVE_STEP = "이동할 칸을 선택해주세요. (위: U, 아래: D)";
+    public final String EXIT_OR_CONTINUE = "게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)";
+    public final String THE_GAME_RESULT = "최종 게임 결과";
+    public final String SUCCESS_OR_FAIL = "게임 성공 여부:";
+    public final String TOTAL_COUNT = "총 시도한 횟수:";
     public void lineSkip(){
         System.out.println();
     }
