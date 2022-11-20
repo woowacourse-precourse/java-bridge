a73b3c3305f8763193af587d6dcaccc713d0ad35
김현재 <41482946+galug@users.noreply.github.com>
2022-11-20 14:47:29 +0900

    refactor(bridge): 다리 길이 검증 기능을 bridge class 로 이동
    객체에 관한 검증은 일급 컬렉션에서 관리

--- src/main/java/bridge/model/Bridge.java
+++ src/main/java/bridge/model/Bridge.java
@@ -3,10 +3,20 @@ package bridge.model;
 import java.util.List;
 
 public class Bridge {
+    private static final int MAX_BRIDGE_SIZE = 20;
+    private static final int MIN_BRIDGE_SIZE = 3;
+    private static final String BRIDGE_SIZE_ERROR = "[ERROR] 다리의 크기는 3~20 입니다.";
     private final List<String> bridge;
 
     public Bridge(List<String> bridge) {
+        validateBridgeSize(bridge.size());
         this.bridge = bridge;
+    }
+
+    private void validateBridgeSize(int size) {
+        if (size < MIN_BRIDGE_SIZE || size > MAX_BRIDGE_SIZE) {
+            throw new IllegalArgumentException(BRIDGE_SIZE_ERROR);
+        }
     }
 
     public Movable compareSpace(int position, String selectedSpace) {
