a8758ddd2a6ed6c6c73c34ab8eb2fdeebc875522
bellljun3395 <195850@jnu.ac.kr>
2022-11-20 19:15:54 +0900

    feat(Game): 생성 객체

---
+++ src/main/java/bridge/game/Game.java
@@ -0,0 +1,23 @@
+package bridge.game;
+
+import bridge.BridgeRandomNumberGenerator;
+import bridge.game.bridgeGame.BridgeGame;
+import bridge.view.InputView;
+import bridge.view.OutputView;
+
+public class Game {
+
+    private final InputView inputView;
+    private final OutputView outputView;
+    private final GameStatus gameStatus;
+    private final BridgeRandomNumberGenerator bridgeRandomNumberGenerator;
+    private BridgeGame bridgeGame;
+
+    public Game() {
+        this.inputView = new InputView();
+        this.outputView = new OutputView();
+        this.gameStatus = new GameStatus();
+        this.bridgeRandomNumberGenerator = new BridgeRandomNumberGenerator();
+    }
+
+}
