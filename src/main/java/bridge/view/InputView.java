0e0d5cbffb569dac7f8e1a97f54fe067caa02848
alpacaHyun <flaehdan@gmail.com>
2022-11-21 08:33:48 +0900

    type : feat ✨ 게임 재시작 여부 입력받기
    body :

--- src/main/java/bridge/view/InputView.java
+++ src/main/java/bridge/view/InputView.java
@@ -28,6 +28,7 @@ public class InputView {
     public String readMoving() {
         System.out.println(INPUT_MOVING_DIRECTION);
         String inputMoving = Console.readLine();
+        //U , D 대문자만 입력 가능하도록, 숫자이면 예외, 공백도 예외
         return inputMoving;
     }
 
@@ -35,6 +36,9 @@ public class InputView {
      * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
      */
     public String readGameCommand() {
-
+        System.out.println(INPUT_RETRY_GAME);
+        String inputGameCommand = Console.readLine();
+        //예외처리 하기(Q, R만 입력하도록,숫자 다른언어 시 예외처리)
+        return inputGameCommand;
     }
 }
