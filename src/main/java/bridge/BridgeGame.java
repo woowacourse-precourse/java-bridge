a80e5d469e9bf2dee6efc02edb6ee6d53ed056f3
yejin <kwonyj1022@naver.com>
2022-11-20 21:52:13 +0900

    feat: BridgeGame - moving, retry 메서드 구현 완료

--- src/main/java/bridge/BridgeGame.java
+++ src/main/java/bridge/BridgeGame.java
@@ -7,17 +7,28 @@ package bridge;
 // 메서드의 인자와 반환 타입은 필요에 따라 추가하거나 변경 가능
 // 게임 진행을 위해 필요한 메서드를 추가하거나 변경 가능
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * 다리 건너기 게임을 관리하는 클래스
  */
 public class BridgeGame {
+
+    public final List<String> bridge;
+    public List<String> player = new ArrayList<>();
+
+    public BridgeGame(List<String> bridge) {
+        this.bridge = bridge;
+    }
 
     /**
      * 사용자가 칸을 이동할 때 사용하는 메서드
      * <p>
      * 이동을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void move() {
+    public void move(String moving) {
+        player.add(moving);
     }
 
     /**
@@ -26,5 +37,6 @@ public class BridgeGame {
      * 재시작을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
     public void retry() {
+        player = new ArrayList<>();
     }
 }
