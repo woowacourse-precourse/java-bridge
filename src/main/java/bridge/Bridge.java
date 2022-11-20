bb3ea592fbe43ff97720917b1f86588daca5e9e4
ashlovesliitea <skyrider1811@gmail.com>
2022-11-20 15:58:18 +0900

    feat: Bridge - 각 칸의 상태 setting 기능 구현

---
+++ src/main/java/bridge/Bridge.java
@@ -0,0 +1,20 @@
+package bridge;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Bridge {
+    private int length;
+    private List<String> spaces;
+
+    public Bridge(int length) {
+        this.length=length;
+    }
+
+    public void setSpaces(int length){
+        BridgeRandomNumberGenerator bridgeRandomNumberGenerator = new BridgeRandomNumberGenerator();
+        BridgeMaker bridgeMaker = new BridgeMaker(bridgeRandomNumberGenerator);
+        this.spaces = bridgeMaker.makeBridge(length);
+    }
+
+}
