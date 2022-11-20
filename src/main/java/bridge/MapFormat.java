4c2ab3513c5be9f2d88818ffbed7cd7c3649a26f
sonsoowon <yuwoo1018@gmail.com>
2022-11-20 18:27:06 +0900

    feat(MapFormat): 이동 경로 형식 클래스 추가

---
+++ src/main/java/bridge/MapFormat.java
@@ -0,0 +1,27 @@
+package bridge;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public enum MapFormat {
+
+    FORMAT("[ ", " | ", " ]");
+    private final String start;
+    private final String delimiter;
+    private final String end;
+
+    MapFormat(String start, String delimiter, String end) {
+        this.start = start;
+        this.delimiter = delimiter;
+        this.end = end;
+    }
+
+    public List<String> convertToFormatStrings(List<List<String>> maps) {
+        return maps.stream()
+                .map(this::convertToFormatString)
+                .collect(Collectors.toList());
+    }
+    private String convertToFormatString(List<String> map) {
+        return start + String.join(delimiter, map) + end;
+    }
+}
---
+++ src/test/java/bridge/MapFormatTest.java
@@ -0,0 +1,22 @@
+package bridge;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class MapFormatTest {
+    @Test
+    void convertToStringByFormatTest() {
+        MapFormat mapFormat = MapFormat.FORMAT;
+        assertThat(mapFormat.convertToFormatStrings(
+                List.of(
+                        List.of("O", "X", " "),
+                        List.of(" ", " ", " "))
+        )).isEqualTo(List.of(
+                "[ O | X |   ]",
+                "[   |   |   ]"));
+
+    }
+}
