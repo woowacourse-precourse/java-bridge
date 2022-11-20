310759337f71b8d115dbccad83985d9472373a79
EUNJEONGMUN <munej26@gmail.com>
2022-11-21 01:16:26 +0900

    refactor: Movement를 움직임을 리스트에 저장하는 것이 아닌, 현재의 움직임만을 가지고 있는 객체로 변환

--- src/main/java/bridge/domain/Movement.java
+++ src/main/java/bridge/domain/Movement.java
@@ -1,39 +1,19 @@
 package bridge.domain;
 
-import bridge.constant.Constant;
-
-import java.util.ArrayList;
-import java.util.List;
-
 public class Movement {
-    private final Bridge bridge;
-    private List<Moving> movement = new ArrayList<>();
-
-    public Movement(Bridge bridge) {
-        this.bridge = bridge;
-    }
-
-    public boolean canMove() {
-        int nowIndex = movement.size() - 1;
-        return bridge.canMove(nowIndex, movement.get(nowIndex));
-    }
+    private int moveCount;
+    private Moving moving;
 
     public void saveMoving(Moving moving) {
-        this.movement.add(moving);
+        moveCount++;
+        this.moving = moving;
     }
 
-    public boolean isFinish() {
-        return bridge.isFinish(movement);
+    public int getMoveCount() {
+        return moveCount;
     }
 
-    public void clearMoving() {
-        this.movement = new ArrayList<>();
-    }
-
-    public String isSuccess() {
-        if (isFinish() && canMove()) {
-            return Constant.SUCCESS;
-        }
-        return Constant.FAILURE;
+    public Moving getMoving() {
+        return moving;
     }
 }
