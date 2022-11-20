43e22ae61e24443dd9ed80ff2a14d674d65b54a2
song-wooseok <sws981202@gmail.com>
2022-11-21 00:05:47 +0900

    feat: 사용자에게 생성할 다리 개수를 입력 받는 기능

--- src/main/java/bridge/InputView.java
+++ src/main/java/bridge/InputView.java
@@ -1,15 +1,25 @@
 package bridge;
 
+import bridge.utils.ValidateUtils;
+import static bridge.utils.Constants.BridgeSize;
+
+import camp.nextstep.edu.missionutils.Console;
+
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
  */
 public class InputView {
 
-    /**
-     * 다리의 길이를 입력받는다.
-     */
     public int readBridgeSize() {
-        return 0;
+        try{
+            System.out.println(BridgeSize.READ_BRIDGE_SIZE_MESSAGE);
+            String input = Console.readLine();
+            ValidateUtils.isCorrectRangeNumbers(input);
+            return Integer.parseInt(input);
+        }catch (IllegalArgumentException e){
+            System.out.println(BridgeSize.ERROR_BRIDGE_SIZE_MESSAGE);
+            return readBridgeSize();
+        }
     }
 
     /**
@@ -25,4 +35,6 @@ public class InputView {
     public String readGameCommand() {
         return null;
     }
+
+
 }
