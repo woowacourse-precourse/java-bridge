12ed6537cf90c28ba2796e3b224a2c99c699177c
Hyunseok Lee <hyun104467@naver.com>
2022-11-20 21:50:52 +0900

    feat(BridgeGame): constructor

--- src/main/java/bridge/BridgeGame.java
+++ src/main/java/bridge/BridgeGame.java
@@ -1,6 +1,14 @@
 package bridge;
 
+import java.util.List;
+
 public class BridgeGame {
+
+    private final List<String> bridge;
+
+    public BridgeGame(List<String> bridge) {
+        this.bridge = bridge;
+    }
 
     public Boolean move(String inputMoving, String nextBridge) {
         return nextBridge.equals(inputMoving);
