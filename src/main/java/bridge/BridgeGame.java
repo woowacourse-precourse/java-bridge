2118286289a35725745869b8075a7793c8c23682
EUNJEONGMUN <munej26@gmail.com>
2022-11-21 01:28:44 +0900

    refactor: GameController로 일부 코드 이동에 따른 수정

--- src/main/java/bridge/BridgeGame.java
+++ src/main/java/bridge/BridgeGame.java
@@ -2,46 +2,24 @@ package bridge;
 
 import bridge.constant.Constant;
 import bridge.domain.*;
-import bridge.service.BridgeMaker;
-import bridge.view.InputView;
-import bridge.view.OutputView;
+
+import java.util.List;
 
 public class BridgeGame {
-    private final BridgeMaker bridgeMaker;
     private Movement movement;
     private final BridgeMap bridgeMap = new BridgeMap();
     private int tryCount = Constant.INITIAL_COUNT;
 
-    public BridgeGame(BridgeMaker bridgeMaker) {
-        this.bridgeMaker = bridgeMaker;
+    public BridgeGame(Bridge bridge) {
+        this.bridge = bridge;
     }
 
-    public void startGame() {
-        BridgeSize bridgeSize = InputView.readBridgeSize();
-        Bridge bridge = new Bridge(bridgeMaker.makeBridge(bridgeSize.getSize()));
-        movement = new Movement(bridge);
-        repeatGame();
-    }
-
-    private void repeatGame() {
-        boolean isContinue = true;
-        do {
-            move(InputView.readMoving());
-            if (!movement.canMove()) {
-                isContinue = retry();
-            }
-        } while (!movement.isFinish() && isContinue);
-        OutputView.printResult(bridgeMap.getMap(), movement.isSuccess(), tryCount);
-    }
-
-    private void move(Moving moving) {
+    public void move(Moving moving) {
         movement.saveMoving(moving);
-        saveCompareResult(moving);
-        OutputView.printMap(bridgeMap.getMap());
+        bridgeMap.addMap(moving, canMove());
     }
 
-    private boolean retry() {
-        Command command = InputView.readGameCommand();
+    public boolean retry(Command command) {
         if (command.isRetry()) {
             tryCount++;
             clearGame();
