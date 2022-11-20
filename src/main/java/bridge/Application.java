a82939af9a83935d81c2ff228bc8993c7df54f7d
bfly_notebook <kkyxw@soongsil.ac.kr>
2022-11-20 18:24:32 +0900

    refactor: revise the code by using 'BridgeGameManager'

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -1,40 +1,12 @@
 package bridge;
 
-import bridge.domain.BridgeGame;
-import bridge.view.InputView;
-import bridge.view.OutputView;
+import bridge.domain.BridgeGameManager;
 
 public class Application {
 
     public static void main(String[] args) {
         // TODO: 프로그램 구현
-        InputView inputView = new InputView();
-        OutputView outputView = new OutputView();
-
-        int bridgeSize = inputView.readBridgeSize();
-        BridgeGame bridgeGame = new BridgeGame(bridgeSize);
-
-        boolean roundResult = false;
-        while (!(bridgeGame.doesCrossedBridge())) {
-            String userPath = inputView.readMoving(); //InputView 클래스 사용
-            roundResult = bridgeGame.move(userPath); //BridgeGame 클래스 사용
-            outputView.printMap(bridgeGame.getPassedPaths(), roundResult); //OutputView 클래스 사용
-            if (roundResult == false) {
-                String retryOrEnd = inputView.readGameCommand();
-                if (retryOrEnd.equals("R")) {
-                    bridgeGame.retry();
-                    continue;
-                }
-
-                if (retryOrEnd.equals("Q")) {
-                    break;
-                }
-            }
-        }
-
-        System.out.println();
-
-        outputView.printResult(bridgeGame, roundResult);
-
+        BridgeGameManager bridgeGameManager = new BridgeGameManager();
+        bridgeGameManager.run();
     }
 }
