9dc06e996f97efd35d8274935279724a9fa47ab7
AnTaeWook <antk7894@naver.com>
2022-11-20 19:11:42 +0900

    <기능> BridgeGame클래스 구현
    BridgeGame 클래스의 메서드들을 검증하는
    테스트 클래스를 작성하고,
    해당 테스트를 통과하기 위해 BridgeGame클래스의
    기능을 구현하였음.

--- src/main/java/bridge/BridgeGame.java
+++ src/main/java/bridge/BridgeGame.java
@@ -1,16 +1,27 @@
 package bridge;
 
+import java.util.List;
+
 /**
  * 다리 건너기 게임을 관리하는 클래스
  */
 public class BridgeGame {
+
+    private int trial = 1;
+    private int currentUserStep = 0;
+    private List<String> bridge;
 
     /**
      * 사용자가 칸을 이동할 때 사용하는 메서드
      * <p>
      * 이동을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void move() {
+    public boolean move(String directions) {
+        if (directions.equals(bridge.get(currentUserStep))) {
+            currentUserStep++;
+            return true;
+        }
+        return false;
     }
 
     /**
@@ -19,5 +30,19 @@ public class BridgeGame {
      * 재시작을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
     public void retry() {
+        currentUserStep = 0;
+        trial++;
+    }
+
+    public void buildBridge(List<String> bridge) {
+        this.bridge = bridge;
+    }
+
+    public int getTrial() {
+        return trial;
+    }
+
+    public int getCurrentUserStep() {
+        return currentUserStep;
     }
 }
