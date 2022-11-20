d1a0cd69f15dbfd4d86e6015f4a6ca4cee69a917
code-tree <dltjdqls1200@naver.com>
2022-11-20 17:29:17 +0900

    test: 다리를 끝까지 건너면 true를 반환하는지 테스트

--- src/test/java/bridge/domain/BridgeTest.java
+++ src/test/java/bridge/domain/BridgeTest.java
@@ -15,9 +15,16 @@ import static org.assertj.core.api.Assertions.*;
 
 class BridgeTest {
 
-    @DisplayName("입력에 따라 다리 건너기 테스트")
-    @MethodSource("crossBridgeData")
-    @ParameterizedTest(name = "bridge= {0}, input= {1}, results= {2}")
+    @DisplayName("다리를 끝까지 건너면 true가 반환된다.")
+    @MethodSource("bridgeFinishTestData")
+    @ParameterizedTest(name = "다리:{0}, 길이:{1}, 결과:{2}")
+    public void bridgeFinishTest(Bridge bridge, int distance, boolean result) {
+        assertThat(bridge.isFinish(distance)).isEqualTo(result);
+    }
+
+    @DisplayName("다리 건너기 입력에 따라 MoveResult 결과(SUCCESS, FAIL)가 반환된다.")
+    @MethodSource("crossBridgeTestData")
+    @ParameterizedTest(name = "다리:{0}, 입력: {1}, 결과: {2}")
     public void crossBridgeTest(Bridge bridge, List<Move> input, List<MoveResult> expectResults) {
         List<MoveResult> realResults = new ArrayList<>();
 
@@ -28,7 +35,7 @@ class BridgeTest {
         assertThat(realResults).isEqualTo(expectResults);
     }
 
-    static Stream<Arguments> crossBridgeData() {
+    static Stream<Arguments> crossBridgeTestData() {
         Bridge bridge = new Bridge(List.of("U", "D", "U", "D"));
         return Stream.of(
                 Arguments.of(bridge,
@@ -37,6 +44,17 @@ class BridgeTest {
                         List.of(DOWN, UP, DOWN, UP), List.of(FAIL, FAIL, FAIL, FAIL)),
                 Arguments.of(bridge,
                         List.of(UP, DOWN, DOWN, UP), List.of(SUCCESS, SUCCESS, FAIL, FAIL))
+        );
+    }
+
+    static Stream<Arguments> bridgeFinishTestData() {
+        Bridge bridge = new Bridge(List.of("U", "U", "D"));
+        return Stream.of(
+                Arguments.of(bridge, 0, false),
+                Arguments.of(bridge, 1, false),
+                Arguments.of(bridge, 2, false),
+                Arguments.of(bridge, 3, true),
+                Arguments.of(bridge, 4, false)
         );
     }
 }
