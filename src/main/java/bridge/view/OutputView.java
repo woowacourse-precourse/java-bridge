a9b2f083146fd5adcac7e6fefcb72f06b047ce06
YuHayeon <youha0930@gmail.com>
2022-11-20 17:26:29 +0900

    feat(OutputView): 현재까지 이동한 다리의 상태를 출력
    생성된 다리와 현재까지 이동한 다리의 결과값을 받아 정해진 형식대로 출력

--- src/main/java/bridge/view/OutputView.java
+++ src/main/java/bridge/view/OutputView.java
@@ -1,14 +1,24 @@
 package bridge.view;
 
+import bridge.dto.MapDto;
+import java.util.List;
+
 /**
  * 사용자에게 게임 진행 상황과 결과를 출력하는 역할을 한다.
  */
 public class OutputView {
 
     private static final String START_MESSAGE = "다리 건너기 게임을 시작합니다.";
+    private static final String TRUE = " O ";
+    private static final String FALSE = " X ";
+    private static final String UP = "U";
+    private static final String DOWN = "D";
+    private static final String BLANK = "   ";
+    private static final String DIVIDER = "|";
 
     public void printStart() {
         System.out.println(START_MESSAGE);
+        System.out.println();
     }
 
     /**
@@ -16,7 +26,49 @@ public class OutputView {
      * <p>
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void printMap() {
+    public void printMap(MapDto mapDto) {
+        StringBuilder up = new StringBuilder();
+        StringBuilder down = new StringBuilder();
+        List<String> bridge = mapDto.getBridge();
+        List<Boolean> result = mapDto.getResult();
+        for (int i = 0; i < result.size(); i++) {
+            up.append(makeUpMap(result.get(i), bridge.get(i)));
+            down.append(makeDownMap(result.get(i), bridge.get(i)));
+        }
+        makeBothEnds(up, down);
+        System.out.println(up);
+        System.out.println(down);
+        System.out.println();
+    }
+
+    private void makeBothEnds(StringBuilder up, StringBuilder down) {
+        up.insert(0, "[");
+        down.insert(0, "[");
+        up.deleteCharAt(up.length() - 1);
+        down.deleteCharAt(down.length() - 1);
+        up.append("]");
+        down.append("]");
+    }
+
+    private String makeUpMap(boolean result, String moving) {
+        if (moving.equals(UP) && result || !moving.equals(UP) && !result) {
+            return getResultMessage(result) + DIVIDER;
+        }
+        return BLANK + DIVIDER;
+    }
+
+    private String makeDownMap(boolean result, String moving) {
+        if (moving.equals(DOWN) && result || !moving.equals(DOWN) && !result) {
+            return getResultMessage(result) + DIVIDER;
+        }
+        return BLANK + DIVIDER;
+    }
+
+    private String getResultMessage(boolean result) {
+        if (result) {
+            return TRUE;
+        }
+        return FALSE;
     }
 
     /**
