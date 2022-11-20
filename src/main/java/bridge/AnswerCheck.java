dcd49c9309f1f8f1eaa9c42970363229bc9cd130
hojunking96 <junsong96@naver.com>
2022-11-20 20:14:31 +0900

    refactor(AnswerCheck.java) : 클래스 삭제 및 메소드 이동

--- src/main/java/bridge/AnswerCheck.java
+++
@@ -1,8 +0,0 @@
-package bridge;
-
-public class AnswerCheck {
-    public static boolean isNotCorrect(String movingInput) {
-        boolean isNotCorrect = Application.movingInputs.get(Application.turn).equals(movingInput);
-        return isNotCorrect;
-    }
-}
