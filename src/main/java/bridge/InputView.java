ea4eb5d8661c23585e99bb39b12024abe00d18bc
정석준 <tjrwns1010@naver.com>
2022-11-21 04:46:07 +0900

    feat: 사용자 입력용 InputView 클래스 구현 및 예외처리

--- src/main/java/bridge/InputView.java
+++ src/main/java/bridge/InputView.java
@@ -1,5 +1,7 @@
 package bridge;
 
+import camp.nextstep.edu.missionutils.Console;
+
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
  */
@@ -8,21 +10,38 @@ public class InputView {
     /**
      * 다리의 길이를 입력받는다.
      */
-    public int readBridgeSize() {
-        return 0;
+    public int readBridgeSize() throws IllegalArgumentException {
+        String userInput = Console.readLine();
+        if (userInput == null || !userInput.matches("^[0-9]{1,2}$")) {
+            throw new IllegalArgumentException();
+        }
+
+        int convertedInput = Integer.parseInt(userInput);
+        if (convertedInput < 3 || convertedInput > 20) {
+            throw new IllegalArgumentException();
+        }
+        return convertedInput;
     }
 
     /**
      * 사용자가 이동할 칸을 입력받는다.
      */
-    public String readMoving() {
-        return null;
+    public String readMoving() throws IllegalArgumentException {
+        String userInput = Console.readLine();
+        if (userInput == null || !userInput.matches("^[U,D]$")) {
+            throw new IllegalArgumentException();
+        }
+        return userInput;
     }
 
     /**
      * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
      */
     public String readGameCommand() {
-        return null;
+        String userInput = Console.readLine();
+        if (userInput == null | !userInput.matches("^[R,Q]$")) {
+            throw new IllegalArgumentException();
+        }
+        return userInput;
     }
 }
