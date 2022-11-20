a8f653317a93f6a1f755076455b8a8ec3f52610b
codingvegeta <hong090992@gmail.com>
2022-11-20 15:53:34 +0900

    feat: "플레이어가 이동할 칸을 선택하는 메서드를 작성했습니다."

--- src/main/java/bridge/InputView.java
+++ src/main/java/bridge/InputView.java
@@ -7,6 +7,7 @@ import camp.nextstep.edu.missionutils.Console;
  */
 public class InputView {
 
+
     /**
      * 다리의 길이를 입력받는다.
      */
@@ -26,7 +27,11 @@ public class InputView {
      * 사용자가 이동할 칸을 입력받는다.
      */
     public String readMoving() {
-        return null;
+        System.out.println("이동할 칸을 선택해주세요. (위: U, 아래: D)");
+        String str = Console.readLine();
+        isAvailableWord(str);
+
+        return str;
     }
 
     /**
@@ -39,6 +44,12 @@ public class InputView {
     public void isAvailableNumber(int bridgeSize) {
         if (!(2 < bridgeSize && bridgeSize < 21)) {
             throw new IllegalArgumentException("[ERROR] 다리 길이는 3부터 20 사이의 숫자여야 합니다.");
+        }
+    }
+
+    public void isAvailableWord(String word) {
+        if (!word.equals("U") && !word.equals("D")) {
+            throw new IllegalArgumentException("[ERROR] U와 D만 입력 가능합니다");
         }
     }
 }
