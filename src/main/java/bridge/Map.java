1fd6ecbc5446497c2ea99e845e8d2d1f9bb8c50e
JJ <jeongjae1996@naver.com>
2022-11-20 11:46:32 +0900

    refactor: 매번 전체 map을 그릴 필요 없이, 매 라운드마다 유저가 선택한 경로만 갱신하도록 변경

--- src/main/java/bridge/Map.java
+++ src/main/java/bridge/Map.java
@@ -7,19 +7,15 @@ import java.util.List;
 public class Map {
     private static final String MAP_BEGINNING = "[ ";
     private static final String MAP_END = " ]";
-    private static final String DIVISION_LINE = " | ";
+    private static final String DIVISION_LINE = "| ";
     private static final String CORRECT = "O";
     private static final String WRONG = "X";
     private static final String BLANK_SPACE = " ";
-    private static final boolean IS_USER_CHOICE = true;
 
-    private final List<String> userPath;
     private List<String> map;
 
-    public Map(List<String> userPath, boolean isCorrectPath) {
-        this.userPath = userPath;
+    public Map() {
         this.map = initializeMap();
-        drawMap(isCorrectPath);
     }
 
     private List<String> initializeMap() {
@@ -30,43 +26,13 @@ public class Map {
         return map;
     }
 
-    private String prepareMidMap(boolean isUserChoice) {
-        if (isUserChoice) {
-            return CORRECT + DIVISION_LINE;
+    public void drawMap(String userPath, boolean isCorrectPath) {
+        int userChoice = BridgeStatus.convertLetterToNumber(userPath);
+        String mark = CORRECT;
+        if (!isCorrectPath) {
+            mark = WRONG;
         }
-        return BLANK_SPACE + DIVISION_LINE;
-    }
-
-    private String prepareEndMap(boolean isUserChoice, boolean isCorrectPath) {
-        if (!isUserChoice) {
-            return BLANK_SPACE + MAP_END;
-        }
-        else if (isCorrectPath) {
-            return CORRECT + MAP_END;
-        }
-        return WRONG + MAP_END;
-    }
-
-    private void drawMidMap(int userChoice) {
-        this.map.set(userChoice, this.map.get(userChoice) + prepareMidMap(IS_USER_CHOICE));
-        this.map.set(Math.abs(userChoice - 1), this.map.get(Math.abs(userChoice - 1)) + prepareMidMap(!IS_USER_CHOICE));
-    }
-
-    private void drawEndMap(int userChoice, boolean isCorrectPath) {
-        this.map.set(userChoice, this.map.get(userChoice) + prepareEndMap(IS_USER_CHOICE, isCorrectPath));
-        this.map.set(Math.abs(userChoice - 1), this.map.get(Math.abs(userChoice - 1)) + prepareEndMap(!IS_USER_CHOICE, isCorrectPath));
-    }
-
-    private void drawMap(boolean isCorrectPath) {
-        for (int i = 0; i < this.userPath.size(); i++) {
-            int userChoice = BridgeStatus.convertLetterToNumber(this.userPath.get(i));
-
-            if (i < this.userPath.size() - 1) {
-                drawMidMap(userChoice);
-            }
-            else if (i == this.userPath.size() - 1) {
-                drawEndMap(userChoice, isCorrectPath);
-            }
-        }
+        this.map.set(userChoice, this.map.get(userChoice).replace(MAP_END, DIVISION_LINE) + mark + MAP_END);
+        this.map.set(Math.abs(userChoice - 1), this.map.get(Math.abs(userChoice - 1)).replace(MAP_END, DIVISION_LINE) + BLANK_SPACE + MAP_END);
     }
 }
