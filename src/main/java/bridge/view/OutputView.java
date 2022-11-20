b0f324a48a0457dfcd75f2af0a7a8e8553a22259
Yunsik-Choi <choiys0311@gmail.com>
2022-11-21 02:40:16 +0900

    ✨style : OutputView 주석 정리

--- src/main/java/bridge/view/OutputView.java
+++ src/main/java/bridge/view/OutputView.java
@@ -27,8 +27,6 @@ public class OutputView {
 
     /**
      * 현재까지 이동한 다리의 상태를 정해진 형식에 맞춰 출력한다.
-     * <p>
-     * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
     public void printMap(BridgeGameResult result) {
         StringBuilder printMap = new StringBuilder();
@@ -38,8 +36,6 @@ public class OutputView {
 
     /**
      * 게임의 최종 결과를 정해진 형식에 맞춰 출력한다.
-     * <p>
-     * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
     public void printResult(BridgeGameResult bridgeGameResult, TryCount tryCount) {
         StringBuilder result = new StringBuilder();
