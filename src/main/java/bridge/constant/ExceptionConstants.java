70e9a7c98350bccede0da0213a39e0fab4830c13
CAEL0 <kjongmin26@gmail.com>
2022-11-20 17:58:13 +0900

    feat(ExceptionConstants): 이동 관련 에러 메시지 추가

--- src/main/java/bridge/constant/ExceptionConstants.java
+++ src/main/java/bridge/constant/ExceptionConstants.java
@@ -3,7 +3,9 @@ package bridge.constant;
 public enum ExceptionConstants {
     NOT_NATURAL_NUMBER("다리 길이는 자연수여야 합니다."),
     INCORRECT_RANGE(String.format("다리 길이는 %d부터 %d 사이의 숫자여야 합니다.",
-            BridgeConstants.MINIMUM_LENGTH, BridgeConstants.MAXIMUM_LENGTH));
+            BridgeConstants.MINIMUM_LENGTH, BridgeConstants.MAXIMUM_LENGTH)),
+    INCORRECT_MOVING(String.format("입력은 %s 또는 %s이어야 합니다.",
+            BridgeConstants.UP, BridgeConstants.DOWN));
 
     private final String message;
 
