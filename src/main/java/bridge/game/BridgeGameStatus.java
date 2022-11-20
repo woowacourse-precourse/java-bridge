7156c06b81b6f44d5f194bc87597a7f001dd9ce2
limjae <limjae96@gmail.com>
2022-11-20 17:17:07 +0900

    feat: 상태 Enum에 String 반환 로직 추가

--- src/main/java/bridge/game/BridgeGameStatus.java
+++ src/main/java/bridge/game/BridgeGameStatus.java
@@ -1,7 +1,17 @@
 package bridge.game;
 
 public enum BridgeGameStatus {
-    RUNNING,
-    STOP,
-    FINISH
+    RUNNING("진행중"),
+    STOP("실패"),
+    FINISH("성공");
+
+    private final String text;
+
+    BridgeGameStatus(String text) {
+        this.text = text;
+    }
+
+    public String text() {
+        return this.text;
+    }
 }
