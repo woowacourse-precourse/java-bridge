c359d6021e6d1506a672e78c22387fd527dfb2c4
siwookim97 <tldn12gh@gmail.com>
2022-11-21 03:50:00 +0900

    test: 다리의 길이를 입력받고 생성하는 로직 수정에 따른 테스트 코드 추가
    다리의 길이를 입력받고 생성하는 로직 수정에 따른 테스트 코드 추가

--- src/test/java/bridge/util/validator/BridgeMakerValidatorTest.java
+++ src/test/java/bridge/util/validator/BridgeMakerValidatorTest.java
@@ -1,13 +1,20 @@
 package bridge.util.validator;
 
+import bridge.BridgeNumberGenerator;
+import bridge.BridgeRandomNumberGenerator;
+import bridge.service.BridgeMaker;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.assertj.core.util.Lists.newArrayList;
 
 class BridgeMakerValidatorTest {
 
     private static String error = "[ERROR]";
+    private static BridgeMaker bridgeMaker = new BridgeMaker(new BridgeRandomNumberGenerator());
 
     @Test
     @DisplayName("다리 생성의 예외 테스트_1")
@@ -39,5 +46,37 @@ class BridgeMakerValidatorTest {
         assertThatThrownBy(() -> new BridgeMakerValidator("a"))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining(error);
+    }
+
+    @Test
+    @DisplayName("다리 생성 메서드 makeBridge 사전 테스트1")
+    void makeBridgeTest1() {
+        BridgeNumberGenerator numberGenerator = new TestNumberGenerator(newArrayList(1, 0, 0));
+        BridgeMaker bridgeMaker = new BridgeMaker(numberGenerator);
+        List<String> bridge = bridgeMaker.makeBridge(3);
+        assertThat(bridge).containsExactly("U", "D", "D");
+    }
+
+    @Test
+    @DisplayName("다리 생성 메서드 makeBridge 사전 테스트2")
+    void makeBridgeTest2() {
+        BridgeNumberGenerator numberGenerator = new TestNumberGenerator(newArrayList(1, 0, 0, 1, 0, 1));
+        BridgeMaker bridgeMaker = new BridgeMaker(numberGenerator);
+        List<String> bridge = bridgeMaker.makeBridge(6);
+        assertThat(bridge).containsExactly("U", "D", "D", "U", "D", "U");
+    }
+
+    static class TestNumberGenerator implements BridgeNumberGenerator {
+
+        private final List<Integer> numbers;
+
+        TestNumberGenerator(List<Integer> numbers) {
+            this.numbers = numbers;
+        }
+
+        @Override
+        public int generate() {
+            return numbers.remove(0);
+        }
     }
 }
