859647216aee49a1713cc6e6be2da63b97c31ad3
aak2075 <35948985+aak2075@users.noreply.github.com>
2022-11-21 00:49:22 +0900

    feat(bridge) : 선택한 칸을 건너는 기능 추가
    - 끝까지 건넜는지 체크
    - 건널 수 없는지 체크
    - 건너는 데에 성공했는지 체크

--- src/main/java/bridge/domain/BridgeGame.java
+++ src/main/java/bridge/domain/BridgeGame.java
@@ -6,11 +6,13 @@ import java.util.List;
  * 다리 건너기 게임을 관리하는 클래스
  */
 public class BridgeGame {
+    private static final int START_POSITION = 0;
     private final List<String> bridge;
     private int position;
 
     public BridgeGame(BridgeMaker bridgeMaker, int size) {
         bridge = bridgeMaker.makeBridge(size);
+        position = START_POSITION;
     }
 
     /**
@@ -22,11 +24,26 @@ public class BridgeGame {
         position = position + 1;
     }
 
+    public boolean isClear(Moving moving) {
+        return isSafe(moving)
+                && position == bridge.size();
+    }
+
+    public boolean isDead(Moving moving) {
+        return !bridge.get(position-1).equals(moving.name());
+    }
+
+    public boolean isSafe(Moving moving) {
+        return bridge.get(position-1).equals(moving.name());
+    }
+
+
     /**
      * 사용자가 게임을 다시 시도할 때 사용하는 메서드
      * <p>
      * 재시작을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
     public void retry() {
+        position = START_POSITION;
     }
 }
