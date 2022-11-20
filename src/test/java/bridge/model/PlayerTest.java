04638e0eb116e63f1e1fc93f3063f7e9751c7f85
Albert <guang7226961@naver.com>
2022-11-21 07:15:59 +0900

    test($bridge) test 추가

--- src/test/java/bridge/model/PlayerTest.java
+++ src/test/java/bridge/model/PlayerTest.java
@@ -6,23 +6,40 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class PlayerTest {
-    @Test
+
     @DisplayName("플레이어를 이동한다.")
+    @Test
     void move() {
         Player player = new Player();
         assertThat(player.move("U")).isEqualTo(1);
         assertThat(player.move("U")).isEqualTo(2);
     }
 
-    @Test
     @DisplayName("이동 완료 여부 확인")
+    @Test
     void is_move_complete() {
         Player player = new Player();
         player.move("U");
         player.move("D");
-        assertThat(player.isCompleted(1)).isFalse();
-        assertThat(player.isCompleted(2)).isTrue();
+        assertThat(player.isCompletedGame(3)).isFalse();
+        assertThat(player.isCompletedGame(2)).isTrue();
     }
 
+    @DisplayName("성공시 리스트 출력")
+    @Test
+    void print_success_result(){
+        Player player = new Player();
+        player.move("U");
+        player.move("D");
+        assertThat(player.printSuccessResult()).isEqualTo("[ O |   ]\n[   | O ]\n");
+    }
 
+    @DisplayName("실패시 리스트 출력")
+    @Test
+    void print_failure_result(){
+        Player player = new Player();
+        player.move("U");
+        player.move("D");
+        assertThat(player.printFailureResult()).isEqualTo("[ O |   ]\n[   | X ]\n");
+    }
 }
