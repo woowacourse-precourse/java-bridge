45825014b1488e813f226d994f86c1d2bc97ccc0
HyeokJoon Kong <orijoon98@gmail.com>
2022-11-20 16:12:30 +0900

    fix: change Bridge class fields and constructor

--- src/main/java/bridge/model/Bridge.java
+++ src/main/java/bridge/model/Bridge.java
@@ -1,15 +1,17 @@
 package bridge.model;
 
+import bridge.BridgeRandomNumberGenerator;
 import java.util.ArrayList;
 import java.util.List;
 
 public class Bridge {
 
-    private int size;
+    private BridgeMaker bridgeMaker;
     private final List<String> bridge = new ArrayList<>();
 
-    public Bridge(int size, List<String> bridge) {
-        this.size = size;
+    public Bridge(int size) {
+        this.bridgeMaker = new BridgeMaker(new BridgeRandomNumberGenerator());
+        List<String> bridge = this.bridgeMaker.makeBridge(size);
         this.bridge.addAll(bridge);
     }
 }
