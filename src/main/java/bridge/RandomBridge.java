834ed9ab36980eb7bca1c8c940966950d8d423e4
kissshot1104 <kissshot.heartunderblade1104@gmail.com>
2022-11-20 18:05:13 +0900

    Feat(RandomBridge) : 다리 크기 입력 기능 추가
    private int readBridgeSize()
    메소드를 추가했습니다.
    이 메소드는 랜덤한 값을 할당할 다리의 길이를 입력받는 역할을 합니다.

--- src/main/java/bridge/RandomBridge.java
+++ src/main/java/bridge/RandomBridge.java
@@ -11,6 +11,16 @@ public class RandomBridge {
         inputView = new InputView();
     }
 
+    //브릿지의 크기를 입력
+    private int readBirdgeSize() {
+        while (true) {
+            System.out.println("다리의 길이를 입력해주세요.");
+            int bridgeSize = inputView.readBridgeSize();
+            if (bridgeSize != -1)
+                return bridgeSize;
+        }
+    }
+
     // 사이즈를 받아서 그 크기만큼 랜덤 할당
     private void makeBridge(int bridgeSize) {
         BridgeMaker bridgeMaker = new BridgeMaker(new BridgeRandomNumberGenerator());
