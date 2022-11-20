0c875be3844b7386f2812b0ca9c848225e53e5e1
JUHA <khabh@naver.com>
2022-11-20 10:55:28 +0900

    test(GameMap): 이동 결과에 따른 게임 맵 추가 기능 테스트

--- src/test/java/bridge/GameMapTest.java
+++ src/test/java/bridge/GameMapTest.java
@@ -4,6 +4,7 @@ import bridge.domain.bridgegame.GameMap;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
@@ -14,5 +15,14 @@ public class GameMapTest {
     void generateMapString() {
         GameMap gameMap = GameMap.generateMap(List.of(" ", "O"), List.of("O", " "));
         assertThat(gameMap.toString()).contains("[   | O ]", "[ O |   ]");
+    }
+
+    @DisplayName("사용자의 이동 결과에 따라 게임 맵 추가")
+    @Test
+    void addMapElement() {
+        GameMap gameMap = GameMap.generateMap(new ArrayList<>(), new ArrayList<>());
+        gameMap.addGameResult("U", true);
+        gameMap.addGameResult("D", false);
+        assertThat(gameMap.toString()).contains("[ O |   ]", "[   | X ]");
     }
 }
