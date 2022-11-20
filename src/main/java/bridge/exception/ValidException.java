6fcaece47ba8d6c02e93a92a0ef8271e7fc30f93
jw0293 <k38836919@gmail.com>
2022-11-20 19:03:26 +0900

    feat : Add Check input Next Bridge Step [다리 이동을 위한 다음 위치 이동 유효성 검증 기능]

--- src/main/java/bridge/exception/ValidException.java
+++ src/main/java/bridge/exception/ValidException.java
@@ -3,8 +3,10 @@ package bridge.exception;
 public class ValidException {
 
     private static final String ERROR_MESSAGE = "[ERROR]";
-    private static final String INPUT_INTEGER = "다리 길이는 정수형으로 입력되어야 합니다.";
-    private static final String INPUT_VALID_RANGE = "다리 길이는 3부터 20 사이의 숫자여야 합니다.";
+    private static final String INPUT_INTEGER = " 다리 길이는 정수형으로 입력되어야 합니다.";
+    private static final String INPUT_VALID_RANGE = " 다리 길이는 3부터 20 사이의 숫자여야 합니다.";
+    private static final String UP_STEP = "U";
+    private static final String DOWN_STEP = "D";
 
     public void validInteger(String inputString){
         try{
@@ -18,6 +20,25 @@ public class ValidException {
     public void validIntegerRange(int length){
         if(!(3 <= length && length <= 20)){
             System.out.println(ERROR_MESSAGE + INPUT_VALID_RANGE);
+            throw new IllegalArgumentException();
+        }
+    }
+
+    public void validMoveNextStep(String next){
+        validMoveStringLength(next);
+        validMoveStringType(next);
+    }
+
+    private void validMoveStringLength(String next){
+        if(next.length() != 1){
+            System.out.println(ERROR_MESSAGE);
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private void validMoveStringType(String next){
+        if(!next.equals(UP_STEP) || !next.equals(DOWN_STEP)) {
+            System.out.println(ERROR_MESSAGE);
             throw new IllegalArgumentException();
         }
     }
