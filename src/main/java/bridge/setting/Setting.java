39b0a4980970e8dade768ea83aac4be3607024b4
kang jihyeong <her4386@gmail.com>
2022-11-20 18:47:15 +0900

    Feat / 프로그램 세팅 값 설정

---
+++ src/main/java/bridge/setting/Setting.java
@@ -0,0 +1,14 @@
+package bridge.setting;
+
+public class Setting {
+
+    public static final int MIN_BRIDGE_SIZE = 3;
+    public static final int MAX_BRIDGE_SIZE = 20;
+
+    public static final char GAME_EXIT = 'Q';
+    public static final char GAME_RESTART = 'R';
+
+    public static final char PLAYER_MOVE_UP = 'U';
+    public static final char PLAYER_MOVE_DOWN = 'D';
+
+}
