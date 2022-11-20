48d9828f6475cf7de2aca99448eddf6f75e9b61e
impati <impati0716@icloud.com>
2022-11-20 16:39:15 +0900

    feat (Application) : 프로그램 실행

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -1,8 +1,43 @@
 package bridge;
 
 public class Application {
+    private static InputView inputView = new InputView(new Validation());
+    private static OutputView outputView = new OutputView();
 
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        startGame();
+        BridgeGame bridgeGame = new BridgeGame(makeBridge());
+        do {
+            playingGame(bridgeGame);
+        }while (isRetry(bridgeGame));
+        finalResult(bridgeGame);
+    }
+    static void finalResult(BridgeGame bridgeGame){
+        outputView.printResult(bridgeGame.returnMap(),bridgeGame.getCount());
+    }
+    static boolean isRetry(BridgeGame bridgeGame){
+        if(bridgeGame.isComplete())return false;
+        String command = inputView.readGameCommand();
+        if(command.equals(BridgeConst.QUIT)) return false;
+        bridgeGame.retry();
+        return true;
+    }
+    static void playingGame(BridgeGame bridgeGame){
+        while(bridgeGame.isContinue()) {
+            String moving = inputView.readMoving();
+            bridgeGame.move(moving);
+            bridgeGame.changeStatus();
+            String map = bridgeGame.returnMap();
+            outputView.printMap(map);
+        }
+    }
+    static void startGame(){
+        System.out.println("다리 건너기 게임을 시작합니다.");
+    }
+    static Bridge makeBridge(){
+        BridgeMaker bridgeMaker = new BridgeMaker(new BridgeRandomNumberGenerator());
+        int size = inputView.readBridgeSize();
+        Bridge bridge = new Bridge(bridgeMaker.makeBridge(size));
+        return bridge;
     }
 }
