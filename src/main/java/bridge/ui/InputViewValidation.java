9b85693dd76a4b42601ff8b92628c444320ad911
jungwoo <rlawjddn103@naver.com>
2022-11-20 20:51:35 +0900

    feat(InputViewValidation): 숫자만 입력했는지 정규식으로 확인하고 아니면 예외 처리 하는 기능

--- src/main/java/bridge/ui/InputViewValidation.java
+++ src/main/java/bridge/ui/InputViewValidation.java
@@ -1,2 +1,24 @@
-package bridge.ui;public class InputViewValidation {
+package bridge.ui;
+
+import java.util.regex.Pattern;
+
+public class InputViewValidation {
+
+    private final Pattern NUMBER_PATTERN = Pattern.compile("^[0-9]*$");
+    private final String ERROR_NOT_INTEGER = "[ERROR] 숫자가 아닙니다.";
+    private final String ERROR_NOT_IN_RANGE = "[ERROR] 숫자가 범위를 벗어났습니다.";
+
+
+    public void validateIsInteger(String input) {
+        if (!NUMBER_PATTERN.matcher(input).find()) {
+            throw new IllegalArgumentException(ERROR_NOT_INTEGER);
+        }
+    }
+
+    public void validateIsInRange(String input) {
+        int number = Integer.parseInt(input);
+        if (number < 3 || number > 20) {
+            throw new IllegalArgumentException(ERROR_NOT_IN_RANGE);
+        }
+    }
 }
