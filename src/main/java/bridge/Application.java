135e902ff116e9b5f5bec2aeb3a78ed97f1cdc16
이요환 <91039622+bumstead-bumstead@users.noreply.github.com>
2022-11-21 01:25:33 +0900

    <feat>(Application.java): main 메서드 기능 구현

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -2,7 +2,38 @@ package bridge;
 
 public class Application {
 
+    private static InputView inputView = new InputView();
+    private static OutputView outputView = new OutputView();
+    private static BridgeGame bridgeGame;
+
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        outputView.printStartMessage();
+
+        try {
+            bridgeGame = new BridgeGame(inputView.readBridgeSize());
+            System.out.println(bridgeGame.getRealBridge());
+
+            while (true) {
+                if (bridgeGame.hasSucceeded()) {
+                    outputView.printResult(bridgeGame);
+                    break;
+                }
+                boolean isCorrect = bridgeGame.move(inputView.readMoving());
+                outputView.printMap(bridgeGame);
+                if (isCorrect) {
+                    continue;
+                }
+                if (inputView.readGameCommand().equals("R")) {
+                    bridgeGame.retry();
+                    continue;
+                }
+                outputView.printResult(bridgeGame);
+                break;
+            }
+        } catch (IllegalArgumentException e) {
+            outputView.printErrorMessage(e);
+            return;
+        }
     }
+
 }
