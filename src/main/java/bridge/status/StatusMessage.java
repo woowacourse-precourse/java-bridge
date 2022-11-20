5dc2467d80ef133304dca7871f9069926c0f546b
lcw3176 <lcw3176@naver.com>
2022-11-20 19:16:55 +0900

    refactor(status): 상태 메세지 분리

---
+++ src/main/java/bridge/status/StatusMessage.java
@@ -0,0 +1,12 @@
+package bridge.status;
+
+public class StatusMessage {
+    public static final String ON_SUCCESS = " O ";
+    public static final String ON_FAILED = " X ";
+    public static final String ON_NOTHING = "   ";
+    public static final String STATUS_SUCCESS = "\n게임 성공 여부: 성공";
+    public static final String STATUS_FAILED = "\n게임 성공 여부: 실패";
+    public static final String TRY_COUNT = "총 시도한 횟수: ";
+
+
+}
--- src/main/java/bridge/status/UserStatus.java
+++ src/main/java/bridge/status/UserStatus.java
@@ -1,5 +1,6 @@
 package bridge.status;
 
+import bridge.step.StepType;
 import java.util.List;
 
 public class UserStatus {
@@ -8,13 +9,7 @@ public class UserStatus {
     private int position;
     private boolean available;
     private long tryCount;
-
-    private static final String ON_SUCCESS = " O ";
-    private static final String ON_FAILED = " X ";
-    private static final String ON_NOTHING = "   ";
-    private static final String STATUS_SUCCESS = "\n게임 성공 여부: 성공";
-    private static final String STATUS_FAILED = "\n게임 성공 여부: 실패";
-    private static final String TRY_COUNT = "총 시도한 횟수: ";
+    private StepType nowStep;
 
     public UserStatus(List<String> directions) {
         this.directions = directions;
@@ -51,14 +46,14 @@ public class UserStatus {
 
     public String getStatusMessage() {
         if (isAvailable()) {
-            return STATUS_SUCCESS;
+            return StatusMessage.STATUS_SUCCESS;
         }
 
-        return STATUS_FAILED;
+        return StatusMessage.STATUS_FAILED;
     }
 
     public String getTryCountMessage() {
-        return TRY_COUNT + this.tryCount;
+        return StatusMessage.TRY_COUNT + this.tryCount;
     }
 
     public void tryAgain() {
@@ -72,18 +67,26 @@ public class UserStatus {
         String userDirection = this.directions.get(position);
 
         if (userDirection.equals(compareDirection) && getPosition() > position) {
-            return ON_SUCCESS;
+            return StatusMessage.ON_SUCCESS;
         }
 
         if (userDirection.equals(compareDirection) && isAvailable() && getPosition() == position) {
-            return ON_SUCCESS;
+            return StatusMessage.ON_SUCCESS;
         }
 
         if (userDirection.equals(compareDirection) && !isAvailable() && getPosition() == position) {
-            return ON_FAILED;
+            return StatusMessage.ON_FAILED;
         }
 
-        return ON_NOTHING;
+        return StatusMessage.ON_NOTHING;
+    }
+
+    public void changeNextStep(StepType stepType) {
+        this.nowStep = stepType;
+    }
+
+    public StepType getNowStep() {
+        return this.nowStep;
     }
 
 }
