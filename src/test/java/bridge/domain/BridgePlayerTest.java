e35de1c8cb5aa74d9c845b4e7d698a8e0164251c
hyunseo <goatsfish@gmail.com>
2022-11-21 01:23:21 +0900

    test : 플레이어와 다리 이동 정답과 모두 동일한지 확인 성공 테스트

--- src/test/java/bridge/domain/BridgePlayerTest.java
+++ src/test/java/bridge/domain/BridgePlayerTest.java
@@ -15,7 +15,7 @@ class BridgePlayerTest {
 
     @ParameterizedTest(name = "[{index}] playerBridge = {0}, compareBridge = {1}")
     @MethodSource("whenCheckLastMoveTypeNotSameThenSuccessDummy")
-    @DisplayName("두 종류의 마지막 다리 이동 타입이 서로 같지 않은지 확인에 성공한다.")
+    @DisplayName("플레이어를 다리에서 이동시키고 마지막 다리 이동 타입이 서로 동일하지 않은지 확인을 성공한다.")
     void whenCheckLastMoveTypeNotSameThenSuccessTest(List<BridgeMoveType> playerBridge, List<BridgeMoveType> compareBridge) {
         // given
         BridgePlayer bridgePlayer = new BridgePlayer();
@@ -28,6 +28,21 @@ class BridgePlayerTest {
         Assertions.assertThat(isLastMoveTypeNotSame).isTrue();
     }
 
+    @ParameterizedTest(name = "[{index}] playerBridge = {0}, compareBridge = {1}")
+    @MethodSource("whenCheckMoveTypesAllSameThenSuccessDummy")
+    @DisplayName("플레이어를 다리에서 이동시키고 다리 이동 정답과 모두 동일한지 확인을 성공한다.")
+    void whenCheckMoveTypesAllSameThenSuccessTest(List<BridgeMoveType> playerBridge, List<BridgeMoveType> compareBridge) {
+        // given
+        BridgePlayer bridgePlayer = new BridgePlayer();
+        playerBridge.forEach(bridgePlayer::moveTo);
+
+        // when
+        boolean isAllMoveTypesSame = bridgePlayer.isAllMoveTypeSameAs(compareBridge);
+
+        // then
+        Assertions.assertThat(isAllMoveTypesSame).isTrue();
+    }
+
     static Stream<Arguments> whenCheckLastMoveTypeNotSameThenSuccessDummy() {
         return Stream.of(
                 Arguments.arguments(List.of(UP, UP, UP, DOWN, DOWN), List.of(UP, UP, DOWN, DOWN, UP)),
@@ -37,6 +52,18 @@ class BridgePlayerTest {
                 Arguments.arguments(List.of(UP, DOWN, UP, UP, DOWN), List.of(UP, DOWN, UP, DOWN, UP)),
                 Arguments.arguments(List.of(UP, UP, DOWN, DOWN, DOWN), List.of(UP, UP, UP, DOWN, UP)),
                 Arguments.arguments(List.of(DOWN, UP, UP, DOWN, DOWN), List.of(UP, DOWN, UP, DOWN, UP))
+        );
+    }
+
+    static Stream<Arguments> whenCheckMoveTypesAllSameThenSuccessDummy() {
+        return Stream.of(
+                Arguments.arguments(List.of(UP, UP, UP, DOWN, DOWN), List.of(UP, UP, UP, DOWN, DOWN)),
+                Arguments.arguments(List.of(DOWN, DOWN, UP, UP, DOWN), List.of(DOWN, DOWN, UP, UP, DOWN)),
+                Arguments.arguments(List.of(UP, DOWN, UP, DOWN, DOWN), List.of(UP, DOWN, UP, DOWN, DOWN)),
+                Arguments.arguments(List.of(DOWN, UP, UP, DOWN, DOWN), List.of(DOWN, UP, UP, DOWN, DOWN)),
+                Arguments.arguments(List.of(UP, DOWN, UP, UP, DOWN), List.of(UP, DOWN, UP, UP, DOWN)),
+                Arguments.arguments(List.of(UP, UP, DOWN, DOWN, DOWN), List.of(UP, UP, DOWN, DOWN, DOWN)),
+                Arguments.arguments(List.of(DOWN, UP, UP, DOWN, DOWN), List.of(DOWN, UP, UP, DOWN, DOWN))
         );
     }
 }
