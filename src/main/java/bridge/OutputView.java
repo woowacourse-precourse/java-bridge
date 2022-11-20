57ca2f1b93cf2edc7ed0a43955bfea3e5e4faad4
Myungyulee <102199101+Myungyulee@users.noreply.github.com>
2022-11-20 15:52:38 +0900

    feat: 게임 진행 메세지 출력 구현

--- src/main/java/bridge/OutputView.java
+++ src/main/java/bridge/OutputView.java
@@ -4,6 +4,24 @@ package bridge;
  * 사용자에게 게임 진행 상황과 결과를 출력하는 역할을 한다.
  */
 public class OutputView {
+    private final String START_MESSAGE = "다리 건너기 게임을 시작합니다.";
+    private final String BRIDGE_SIZE_INPUT_MESSAGE = "다리의 길이를 입력해주세요.";
+    private final String MOVE_SELECT_MESSAGE = "이동할 칸을 선택해주세요. (위: U, 아래: D)";
+    private final String RETRY_SELECT_MESSAGE = "게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)";
+    private final String FINAL_GAME_RESULT_MESSAGE = "최종 게임 결과";
+
+    public void printStartMessage(){
+        System.out.println(START_MESSAGE);
+    }
+    public void printBridgeSizeInputMessage(){
+        System.out.println(BRIDGE_SIZE_INPUT_MESSAGE);
+    }
+    public void printMoveSelectMessage(){
+        System.out.println(MOVE_SELECT_MESSAGE);
+    }
+    public void printRetrySelectMessage(){
+        System.out.println(RETRY_SELECT_MESSAGE);
+    }
 
     /**
      * 현재까지 이동한 다리의 상태를 정해진 형식에 맞춰 출력한다.
