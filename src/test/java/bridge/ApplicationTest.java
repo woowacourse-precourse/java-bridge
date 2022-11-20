bb807c8863918f19dfb7b724d780847fe02d521b
June <68291395+june-777@users.noreply.github.com>
2022-11-20 17:10:37 +0900

    test(ApplicationTest): 최종 게임 결과 출력기능을 포함한 view와 model  도메인 로직 단위 테스트

--- src/test/java/bridge/ApplicationTest.java
+++ src/test/java/bridge/ApplicationTest.java
@@ -6,6 +6,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.util.Lists.newArrayList;
 
 import static model.BridgeGameConstants.MOVING_FAIL_WRONG_MOVING;
+import static model.BridgeGameConstants.MOVING_SUCCESS_GAME_END;
 
 import camp.nextstep.edu.missionutils.test.NsTest;
 import model.BridgeGame;
@@ -76,8 +77,8 @@ class ApplicationTest extends NsTest {
 	}
 
 	@Nested
-	@DisplayName("view 도메인로직 단위 테스트")
-	class ViewTest {
+	@DisplayName("다리 건너기 게임 view model 도메인 테스트")
+	class ViewModelTest {
 
 		private ByteArrayOutputStream outputStream;
 		private OutputView outputView = new OutputView();
@@ -85,20 +86,25 @@ class ApplicationTest extends NsTest {
 		private BridgeGame bridgeGame;
 
 		@BeforeEach
-		void setOutputStreamBeforeEach() {
+		private void setOutputStreamBeforeEach() {
 			outputStream = new ByteArrayOutputStream();
 			System.setOut(new PrintStream(outputStream));
 		}
 
 		@AfterEach
-		void printMapClearAndRestoreOutputStream() {
+		private void printMapClearAndRestoreOutputStream() {
 			outputView.clearGameStateMap();
 			System.setOut(System.out);
 		}
 
+		private void readLine(String playerInput) {
+			InputStream in = new ByteArrayInputStream(playerInput.getBytes());
+			System.setIn(in);
+		}
+
 		@Test
-		@DisplayName("다리건너기 게임 시작, 다리 길이 입력, 이동할 칸 선택, 게임 상태 출력, 게임 재시작 여부 통합 테스트")
-		void viewDomainTest() {
+		@DisplayName("첫 번째 라운드는 실패하고 재시작 두 번째 라운드는 성공하고 종료하는 테스트")
+		void firstRoundFailAndRestartSecondRoundSuccessGameEnd() {
 
 			String expectedPrintMap = "다리의 길이를 입력해주세요.\r\n"
 //					+ "3\r\n"
@@ -114,7 +120,8 @@ class ApplicationTest extends NsTest {
 //					+ "D\r\n"
 					+ "[ O |   ]\r\n" + "[   | O ]\r\n" + "\r\n" + "이동할 칸을 선택해주세요. (위: U, 아래: D)\r\n"
 //					+ "D\r\n"
-					+ "[ O |   |   ]\r\n" + "[   | O | O ]\r\n" + "\r\n";
+					+ "[ O |   |   ]\r\n" + "[   | O | O ]\r\n" + "\r\n" + "최종 게임 결과\r\n" + "[ O |   |   ]\r\n"
+					+ "[   | O | O ]\r\n" + "\r\n" + "게임 성공 여부: 성공\r\n" + "총 시도한 횟수: 2";
 			// given
 			List<String> bridge = List.of("U", "D", "D");
 			bridgeGame = new BridgeGame(bridge);
@@ -122,7 +129,7 @@ class ApplicationTest extends NsTest {
 			List<String> movingCommandsRoundOne = List.of("U", "U");
 			List<String> movingCommandsRoundTwo = List.of("U", "D", "D");
 
-			// when: 다리길이 입력
+			// when: 첫번째 라운드 -> 게임 실패, 재시작
 			readLine(bridgeSize);
 			inputView.readBridgeSize();
 
@@ -144,7 +151,7 @@ class ApplicationTest extends NsTest {
 				}
 			}
 
-			// when: moving 실패
+			// 조두: 두번째 라운드 -> 게임 성공, 종료
 			for (int moveCount = 0; moveCount < movingCommandsRoundTwo.size(); moveCount++) {
 				String moving = movingCommandsRoundTwo.get(moveCount);
 				readLine(moving);
@@ -153,15 +160,11 @@ class ApplicationTest extends NsTest {
 				int movingResult = bridgeGame.move(moving);
 
 				outputView.printMap(moving, movingResult);
+				if (movingResult == MOVING_SUCCESS_GAME_END || movingResult == MOVING_FAIL_WRONG_MOVING) {
+					outputView.printResult(moving, movingResult, 2);
+				}
 			}
 			assertThat(outputStream.toString()).isEqualTo(expectedPrintMap);
 		}
-
-		private void readLine(String playerInput) {
-			InputStream in = new ByteArrayInputStream(playerInput.getBytes());
-			System.setIn(in);
-		}
-
 	}
-
 }
