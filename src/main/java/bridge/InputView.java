b2980c531c0ffb26c18984eb181a7470155dde96
eyeofsol <kingjihong@naver.com>
2022-11-20 12:17:50 +0900

    feat(InputView): 브릿지 사이즈 입력받는 기능 구현
    while문을 이용해서 try catch를 구현했습니다. 예외를 받고 종료가 아닌,
    입력을 계속 받아야 하기 때문에 이렇게 구현했습니다.
    try에서 예외가 발생하면 그곳에서 바로 catch로 이동합니다.
    따라서 return 이전에 parseInt를 진행하고, 또 거기서 예외가 발생한다면
    바로 catch로 빠져나오기 때문에 예외가 발생하지 않을 때만 리턴하게 됩니다.

--- src/main/java/bridge/InputView.java
+++ src/main/java/bridge/InputView.java
@@ -1,5 +1,9 @@
 package bridge;
 
+import camp.nextstep.edu.missionutils.Console;
+
+import java.util.Collections;
+
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
  */
@@ -9,7 +13,14 @@ public class InputView {
      * 다리의 길이를 입력받는다.
      */
     public int readBridgeSize() {
-        return 0;
+        while(true) {
+            try {
+                String input = Console.readLine();
+                return Integer.parseInt(input);
+            } catch (NumberFormatException e) {
+                System.out.println("[ERROR] 다리는 숫자여야 합니다.");
+            }
+        }
     }
 
     /**
