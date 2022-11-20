28bd139ad0ecad2514054420c5362cf44a001fdf
jjdone <ehdlfaos@gmail.com>
2022-11-20 16:50:09 +0900

    feat: Player 클래스 구현
    InputView를 직접 사용하여 움직임(move)를 관리한다.
    또한 재시작(retry)도 관리한다.
    움직임(move)의 예외처리 구현

--- src/main/java/bridge/Player.java
+++ src/main/java/bridge/Player.java
@@ -1,16 +1,34 @@
-package bridge.model;
-
-import bridge.view.InputView;
+package bridge;
 
 public class Player {
+    private static final String MOVING_ERROR = "[ERROR] U와 D 중에 입력해주세요.";
+
     private InputView moving;
     public Player() {
         moving = new InputView();
     }
 
     public String move() {
-        return moving.readMoving();
+        try {
+            String input = moving.readMoving();
+            if (!validateMoving(input)) {
+                throw new IllegalArgumentException();
+            }
+            return input;
+        } catch (IllegalArgumentException e) {
+            System.out.println(MOVING_ERROR);
+            return move();
+        }
     }
 
+    public String retry() {
+        return moving.readGameCommand();
+    }
 
+    private boolean validateMoving(String input) {
+        if(input.equals("U") || input.equals("D")) {
+            return true;
+        }
+        return false;
+    }
 }
