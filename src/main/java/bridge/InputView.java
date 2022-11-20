8b1aee1b556f2701c6fc30d6498132aedbb108d9
minji <heunhanireum@gmail.com>
2022-11-20 23:11:29 +0900

    feat : 유저가 입력한 다리의 길이가 숫자인지, 3이상 20이하인지 검증

--- src/main/java/bridge/InputView.java
+++ src/main/java/bridge/InputView.java
@@ -1,5 +1,7 @@
 package bridge;
 
+import camp.nextstep.edu.missionutils.Console;
+
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
  */
@@ -9,7 +11,22 @@ public class InputView {
      * 다리의 길이를 입력받는다.
      */
     public int readBridgeSize() {
+        String userBridgeSizeInput = Console.readLine();
         return 0;
+    }
+
+    public void userBridgeSizeInputValidation (String userBridgeSizeInput) {
+        try {
+            Integer.parseInt(userBridgeSizeInput);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("[ERROR] 숫자를 입력해야 합니다");
+        }
+    }
+
+    public void userBridgeSizeValidation(int userBridgeSize) {
+        if (userBridgeSize < 3 || 20 < userBridgeSize) {
+            throw new IllegalArgumentException("[ERROR] 다리 길이는 3부터 20 사이의 숫자여야 합니다.");
+        }
     }
 
     /**
