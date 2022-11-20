7c897d8c0044b4898913ccddef6e14db667d5f28
Hyeonjae-K <href.hjk@gmail.com>
2022-11-20 13:10:20 +0900

    feat: 게임 초기화 기능 구현

--- src/main/java/bridge/BridgeGame.java
+++ src/main/java/bridge/BridgeGame.java
@@ -1,9 +1,32 @@
 package bridge;
 
+import java.util.List;
+
 /**
  * 다리 건너기 게임을 관리하는 클래스
  */
 public class BridgeGame {
+    private static final OutputView outputView = new OutputView();
+    private static final InputView inputView = new InputView();
+    private static final BridgeNumberGenerator bridgeNumberGenerator = new BridgeRandomNumberGenerator();
+    private static final BridgeMaker bridgeMaker = new BridgeMaker(bridgeNumberGenerator);
+    private static List<String> bridge;
+
+    public void init() {
+        outputView.printStart();
+        int bridgeSize = getBridgeSize();
+        bridge = bridgeMaker.makeBridge(bridgeSize);
+    }
+
+    public static int getBridgeSize() {
+        while (true) {
+            try {
+                return inputView.readBridgeSize();
+            } catch (IllegalArgumentException exception) {
+                System.out.println(exception);
+            }
+        }
+    }
 
     /**
      * 사용자가 칸을 이동할 때 사용하는 메서드
