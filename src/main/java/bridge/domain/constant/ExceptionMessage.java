c05cfe5f1506500ac154555f41f7530b54f77e0d
Isu Shin <isu010614@naver.com>
2022-11-20 23:36:39 +0900

    feat: 재시작/ 종료 예외 메시지 추가

--- src/main/java/bridge/domain/constant/ExceptionMessage.java
+++ src/main/java/bridge/domain/constant/ExceptionMessage.java
@@ -1,9 +1,10 @@
-package bridge.domain;
+package bridge.domain.constant;
 
 public class ExceptionMessage {
     public static final String NOT_NUMBER = "[ERROR] 숫자만 입력해야 합니다.";
     public static final String NOT_IN_RANGE = "[ERROR] 다리 길이는 3부터 20 사이의 숫자여야 합니다.";
-    public static final String NOT_UP_OR_DOWN = "[ERROR] U(위), 혹은 D(아래)만 입력할 수 있습니다.";
+    public static final String NOT_UP_OR_DOWN = "[ERROR] U(위), 혹은 D(아래)를 입력해주세요.";
+    public static final String NOT_APPROPRIATE_COMMAND = "[ERROR] R(재시도), 혹은 Q(종료)를 입력해주세요.";
 
     private ExceptionMessage() {
     }
