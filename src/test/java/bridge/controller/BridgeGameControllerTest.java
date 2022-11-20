51fbf562dc95e0ffd81a6af9644a0a07292768a2
62hoon99 <98267772+62hoon99@users.noreply.github.com>
2022-11-20 10:42:46 +0900

    feat(test): 다리가 UD로만 이루어져 있는지 테스트 기능 구현

--- src/test/java/bridge/controller/BridgeGameControllerTest.java
+++ src/test/java/bridge/controller/BridgeGameControllerTest.java
@@ -6,6 +6,9 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 import static org.assertj.core.api.Assertions.*;
 
 public class BridgeGameControllerTest {
@@ -19,5 +22,18 @@ public class BridgeGameControllerTest {
         BridgeGame bridgeGame = new BridgeGame(bridgeMaker.makeBridge(size));
         //then
         assertThat(bridgeGame.getBridgeSize()).isEqualTo(size);
+    }
+
+    @Test
+    void 다리가_U_D_로만_이루어져_있는지_테스트(){
+        //given
+        BridgeGame bridgeGame = new BridgeGame(bridgeMaker.makeBridge(10));
+        List<String> bridge = bridgeGame.getBridge();
+        //when
+        List<String> collect = bridge.stream()
+                .filter(mark -> mark.equals("U") || mark.equals("D"))
+                .collect(Collectors.toList());
+        //then
+        assertThat(collect).isEqualTo(bridge);
     }
 }
