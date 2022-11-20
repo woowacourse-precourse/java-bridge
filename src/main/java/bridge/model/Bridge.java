cb743a48ae6222613314196db52da5feb61cd0fc
UJ15 <dbwogml15@naver.com>
2022-11-20 15:26:06 +0900

    refactor : change code to const

--- src/main/java/bridge/model/Bridge.java
+++ src/main/java/bridge/model/Bridge.java
@@ -1,5 +1,8 @@
 package bridge.model;
 
+import static bridge.model.UpOrDown.DOWN_SIGN;
+import static bridge.model.UpOrDown.UP_SIGN;
+
 import bridge.error.Error;
 import java.util.List;
 import java.util.Objects;
@@ -18,7 +21,7 @@ public class Bridge {
     }
 
     private void validate(List<String> bridge) {
-        if (!bridge.stream().allMatch(v -> v.equals("U") || v.equals("D"))) {
+        if (!bridge.stream().allMatch(v -> v.equals(UP_SIGN) || v.equals(DOWN_SIGN))) {
             throw new IllegalArgumentException();
         }
     }
