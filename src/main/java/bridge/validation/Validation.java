8f2832258025154912f734d9af7adeca7a89feae
Beginner <dkstmdwn17@naver.com>
2022-11-20 18:00:04 +0900

    feature : 유효한 입력인지 확인하는 유효성 검증을 구현
    사용자에게 입력이 들어왔을 때
    유효한 입력이 들어왔는지
    확인하고 유효하지 않으면
    에러를 던진다.

---
+++ src/main/java/bridge/validation/Validation.java
@@ -0,0 +1,52 @@
+package bridge.validation;
+
+import bridge.validation.enumeration.Mode;
+
+public class Validation {
+    private static final String IS_NUMBER = "[0-9]+";
+    private static final String UP = "U";
+    private static final String DOWN = "D";
+    private static final String RETRY = "R";
+    private static final String QUIT = "Q";
+    // 사용자 입력이 올바른지 확인 - 숫자인지
+    public static void verifyUserInputBridgeLength(String input){
+        verifyUserInputIsEmpty(input.length());
+        verifyUserInputIsNumber(input);
+        verifyUserInputIsInScope(Integer.parseInt(input));
+    }
+
+    private static void verifyUserInputIsNumber(String input) {
+        if(!input.matches(IS_NUMBER)) throw new IllegalArgumentException("숫자가 아닙니다.");
+    }
+
+    private static void verifyUserInputIsInScope(int input) {
+        if(!(input >= 3 && input <= 20)) throw new IllegalArgumentException("범위를 넘어섰습니다.");
+    }
+
+    private static void verifyUserInputIsEmpty(int length) {
+        if(length == 0) throw new IllegalArgumentException("입력이 없습니다.");
+    }
+
+    // 사용자 입력이 올바른지 확인 - 문자 하나
+    public static void verifyUserInputIsCharacter(String input, Mode mode){
+        verifyUserInputIsEmpty(input.length());
+        verifyUserInputIsCommand(input, mode);
+    }
+
+    private static void verifyUserInputIsCommand(String input, Mode mode) {
+        if(mode == Mode.UpOrDown) verifyUpOrDown(input, mode);
+        if(mode == Mode.RetryOrQuit) verifyRetryOrQuit(input, mode);
+    }
+
+    private static void verifyUpOrDown(String input, Mode mode) {
+        if(!(mode == Mode.UpOrDown && (input.equals(UP) || input.equals(DOWN)))){
+            throw new IllegalArgumentException("올바른 문자를 입력해주세요.");
+        }
+    }
+
+    private static void verifyRetryOrQuit(String input, Mode mode) {
+        if(!(mode == Mode.RetryOrQuit && (input.equals(RETRY) || input.equals(QUIT)))){
+            throw new IllegalArgumentException("올바른 문자를 입력해주세요.");
+        }
+    }
+}
