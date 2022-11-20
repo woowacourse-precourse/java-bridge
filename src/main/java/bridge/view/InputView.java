f05669bb1ee78326a7b4e8c5b9993c4989eb7c1a
SuhHyeonjun <dmsgktn0208@gmail.com>
2022-11-20 16:14:28 +0900

    refactor : 메서드 라인이 10이 넘어가지 않도록 수정

--- src/main/java/bridge/view/InputView.java
+++ src/main/java/bridge/view/InputView.java
@@ -6,8 +6,6 @@ import static bridge.view.Validator.validateInputSizeException;
 
 import static camp.nextstep.edu.missionutils.Console.readLine;
 
-import bridge.messages.Message;
-
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
  */
@@ -17,7 +15,6 @@ public class InputView {
      * 다리의 길이를 입력받는다.
      */
     public int inputBridgeSize() {
-        System.out.println(Message.INPUT_SIZE.getMessage());
         String inputSize = readLine();
         System.out.println();
         try {
@@ -33,7 +30,6 @@ public class InputView {
      * 사용자가 이동할 칸을 입력받는다.
      */
     public String inputMovingDirection() {
-        System.out.println(Message.SELECT_DIRECTION.getMessage());
         String inputDirection = readLine();
         try {
             validateInputDirectionException(inputDirection);
@@ -48,7 +44,6 @@ public class InputView {
      * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
      */
     public String inputGameRestart() {
-        System.out.println(Message.ASK_RESTART.getMessage());
         String inputRestart = readLine();
         try {
         validateInputGameRestartException(inputRestart);
@@ -58,5 +53,4 @@ public class InputView {
         }
         return inputRestart;
     }
-
 }
