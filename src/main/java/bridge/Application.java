6c6795ca085d1d0c444a2f3604e99e962a17f301
Sangkyun Seo <tkdrbs0802@naver.com>
2022-11-20 15:47:23 +0900

    feat: 메인 메서드 다리 생성 기능 작성

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -1,9 +1,17 @@
 package bridge;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Application {
-    static String printgameStart = "다리 건너기 게임을 시작합니다.";
+    static String printGameStart = "다리 건너기 게임을 시작합니다.";
+    static BridgeNumberGenerator bridgeNumberGenerator;
+    static List<String> bridge = new ArrayList<>();
     public static void main(String[] args) {
-        System.out.println(printgameStart);
+        System.out.println(printGameStart);
+        InputView inputView = new InputView();
+        BridgeMaker bridgeMaker = new BridgeMaker(bridgeNumberGenerator);
+        bridge = bridgeMaker.makeBridge(inputView.readBridgeSize());
 
 
     }
