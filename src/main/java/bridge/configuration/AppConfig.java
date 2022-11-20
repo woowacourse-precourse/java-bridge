83b30bd19e8376412c76e89c64554de254fb852d
Invidam <hansupark0208@gmail.com>
2022-11-20 21:25:45 +0900

    fix(app-config): 주입 객체 추가

--- src/main/java/bridge/configuration/AppConfig.java
+++ src/main/java/bridge/configuration/AppConfig.java
@@ -2,10 +2,17 @@ package bridge.configuration;
 
 import bridge.BridgeMaker;
 import bridge.BridgeRandomNumberGenerator;
+import bridge.controller.BridgeGameController;
+import bridge.view.io.InputView;
+import bridge.view.io.OutputView;
 
 public class AppConfig {
 
     public static BridgeMaker bridgeMaker() {
         return new BridgeMaker(new BridgeRandomNumberGenerator());
+    }
+
+    public static BridgeGameController bridgeGameController() {
+        return new BridgeGameController(new InputView(), new OutputView());
     }
 }
