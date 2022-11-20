af6cc31613b70422d2c580a1ec7d6ea970433536
3juhwan <13selfesteem91@naver.com>
2022-11-21 00:06:23 +0900

    fix(bridge): 유저가 이동한 칸이 없는 경우 true 반환

--- src/main/java/bridge/controller/Judgement.java
+++ src/main/java/bridge/controller/Judgement.java
@@ -6,7 +6,7 @@ public class Judgement {
 
     public static boolean judgeUserMovement(List<Boolean> compareResult) {
         if (compareResult.size() == 0) {
-            return false;
+            return true;
         }
         return compareResult.get(compareResult.size() - 1);
     }
