d9bb4292f8400c3b55bb9b79c57554bc1e5ec9fa
hyunseo <goatsfish@gmail.com>
2022-11-21 00:21:34 +0900

    test : 잘못된 입력된 int 타입의 다리 이동 코드를 통한 이동 타입 검색 실패하는 예외처리 테스트

--- src/test/java/bridge/domain/BridgeMoveTypeTest.java
+++ src/test/java/bridge/domain/BridgeMoveTypeTest.java
@@ -4,11 +4,14 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.stream.Stream;
 
 import static bridge.domain.BridgeMoveType.*;
+import static bridge.exception.BridgeTypeExceptionMessage.READ_WRONG_TYPE_EXCEPTION;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
 
 class BridgeMoveTypeTest {
 
@@ -17,7 +20,7 @@ class BridgeMoveTypeTest {
     @DisplayName("입력된 int 타입의 다리 이동 코드를 이용하여 다리 이동 타입 검색을 성공한다.")
     void whenIntTypeSearchThenSuccessTest(int moveTypeCode, String moveType) {
         // given & when
-        String findMoveType = of(moveTypeCode);
+        String findMoveType = BridgeMoveType.of(moveTypeCode);
 
         // then
         assertThat(findMoveType).isEqualTo(moveType);
@@ -28,10 +31,19 @@ class BridgeMoveTypeTest {
     @DisplayName("입력된 String 타입의 다리 이동 코드를 이용하여 다리 이동 타입 검색을 성공한다.")
     void whenStringTypeSearchThenSuccessTest(String moveType, BridgeMoveType bridgeMoveType) {
         // given & when
-        BridgeMoveType findMoveType = of(moveType);
+        BridgeMoveType findMoveType = BridgeMoveType.of(moveType);
 
         // then
         assertThat(findMoveType).isEqualTo(bridgeMoveType);
+    }
+
+    @ParameterizedTest(name = "[{index}] moveTypeCode = {0}")
+    @ValueSource(ints = {-10000000, -10, -1, 2, 3, 4, 100, 100000000})
+    @DisplayName("잘못 입력된 int 타입의 다리 이동 코드를 이용하여 다리 이동 타입 검색을 실패하여 예외처리 된다.")
+    void whenWrongIntTypeSearchThenExceptionTest(int moveTypeCode) {
+        assertThatExceptionOfType(IllegalArgumentException.class)
+                .isThrownBy(() -> BridgeMoveType.of(moveTypeCode))
+                .withMessage(READ_WRONG_TYPE_EXCEPTION.getMessage());
     }
 
     static Stream<Arguments> whenIntTypeSearchThenSuccessDummy() {
