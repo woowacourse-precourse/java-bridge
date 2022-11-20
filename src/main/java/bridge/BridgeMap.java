5197b26ef049060a061b755b98bfd2b56ada6b9d
sonsoowon <yuwoo1018@gmail.com>
2022-11-20 18:56:37 +0900

    feat(BridgeMap): 특정 지점까지의 지도 반환

--- src/main/java/bridge/BridgeMap.java
+++ src/main/java/bridge/BridgeMap.java
@@ -1,6 +1,7 @@
 package bridge;
 
 import java.util.List;
+import java.util.ArrayList;
 
 public class BridgeMap extends RouteMap {
 
@@ -8,4 +9,10 @@ public class BridgeMap extends RouteMap {
         super(upSide, downSide);
     }
 
+    public RouteMap getRouteAt(int distance) {
+        List<String> slicedUpSide = new ArrayList<>(upSide.subList(0, distance));
+        List<String> slicedDownSide = new ArrayList<>(downSide.subList(0, distance));
+
+        return new RouteMap(slicedUpSide, slicedDownSide);
+    }
 }
---
+++ src/test/java/bridge/BridgeMapTest.java
@@ -0,0 +1,30 @@
+package bridge;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class BridgeMapTest {
+    private static final BridgeMap successMap = new BridgeMap(
+            List.of("O", " ", " "),
+            List.of(" ", "O", "O")
+    );
+
+    private static final BridgeMap failureMap = new BridgeMap(
+            List.of(" ", "X", "X"),
+            List.of("X", " ", " ")
+    );
+
+    @Test
+    void getRouteTest() {
+        RouteMap expected = new RouteMap(
+                List.of("O", " "),
+                List.of(" ", "O")
+        );
+        assertThat(successMap.getRouteAt(2))
+                .isEqualTo(expected);
+
+    }
+}
