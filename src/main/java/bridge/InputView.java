f4fd4a3e5c9081807bb641f09ad675148a2c1a13
Hyeonjae-K <href.hjk@gmail.com>
2022-11-20 11:06:44 +0900

    feat: 사용자 입력 반환 함수 구현

--- src/main/java/bridge/InputView.java
+++ src/main/java/bridge/InputView.java
@@ -1,5 +1,7 @@
 package bridge;
 
+import camp.nextstep.edu.missionutils.Console;
+
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
  */
@@ -24,5 +26,18 @@ public class InputView {
      */
     public String readGameCommand() {
         return null;
+    }
+
+    /**
+     * 안내 문구를 출력하고 사용자로부터 입력받은 문자열을 반환한다.
+     *
+     * @param guide 출력해줄 안내 문구
+     * @return 사용자가 입력한 문자열 반환
+     */
+    public String getInput(String guide) {
+        System.out.println(guide);
+        String input = Console.readLine();
+        System.out.println();
+        return input;
     }
 }
