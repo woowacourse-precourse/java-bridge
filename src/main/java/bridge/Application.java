b471679d8754932dd4bab8cef76d7e93ecb74a6c
eastsage <dpwk8@naver.com>
2022-11-21 03:11:45 +0900

    feat: 게임 시작 호출 추가

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -1,8 +1,18 @@
 package bridge;
 
+import bridge.model.BridgeGame;
+import bridge.model.Direction;
+import bridge.view.InputView;
+import bridge.view.OutputView;
+import java.util.List;
+import java.util.Stack;
+
 public class Application {
 
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        GameController gameController =
+                new GameController(new InputView(), new OutputView(), new BridgeGame());
+        gameController.start();
     }
 }
