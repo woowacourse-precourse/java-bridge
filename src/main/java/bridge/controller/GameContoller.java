e85824223b0aaada55c3d7e769ef4066e2169c95
leeyongjun <leeyongjun0310@naver.com>
2022-11-21 06:54:09 +0900

    feat(bridge): 플레이어가 이동할 칸을 입력 받는 기능 구현

--- src/main/java/bridge/controller/GameContoller.java
+++ src/main/java/bridge/controller/GameContoller.java
@@ -1,8 +1,12 @@
 package bridge.controller;
 
 
+import bridge.view.InputView;
+
 public class GameContoller {
     public void run() {
-        System.out.println(InputController.readBridgeSize());
+//        System.out.println(InputController.readBridgeSize());
+        InputView inputView = new InputView();
+        System.out.println(inputView.readMoving());
     }
 }
