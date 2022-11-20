1b8f0afd5eeb4513dd4f89cec4a47dbd6e45e891
leesangjin <tkdwls0628@gmail.com>
2022-11-21 01:05:22 +0900

    feat(OutputView): 게임 시작 문구 출력

--- src/main/java/bridge/view/OutputView.java
+++ src/main/java/bridge/view/OutputView.java
@@ -1,10 +1,16 @@
-package bridge;
+package bridge.view;
 
 public class OutputView {
 
-    public void printMap() {
+    private static final String START_BRIDGE_GAME = "다리 건너기 게임을 시작합니다.";
+
+    public static void printStartGame() {
+        System.out.println(START_BRIDGE_GAME);
     }
 
-    public void printResult() {
+    public static void printMap() {
+    }
+
+    public static void printResult() {
     }
 }
