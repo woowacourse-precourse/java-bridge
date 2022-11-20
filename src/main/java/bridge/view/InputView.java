24cb6ce592fd85e913fa1c929c7bf214c371674f
kang jihyeong <her4386@gmail.com>
2022-11-20 23:00:03 +0900

    Feat / 이동할 칸 출력 기능

--- src/main/java/bridge/view/InputView.java
+++ src/main/java/bridge/view/InputView.java
@@ -5,17 +5,20 @@ import static camp.nextstep.edu.missionutils.Console.readLine;
 public class InputView {
 
     public String readBridgeSize() {
-        System.out.println("\n다리의 길이를 입력해주세요.");
+        System.out.println();
+        System.out.println("다리의 길이를 입력해주세요.");
         return readLine();
     }
 
     public String readMoving() {
-        System.out.println("\n이동할 칸을 선택해주세요. (위: U, 아래: D)");
+        System.out.println();
+        System.out.println("이동할 칸을 선택해주세요. (위: U, 아래: D)");
         return readLine();
     }
 
     public String readGameCommand() {
-        System.out.println("\n게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)");
+        System.out.println();
+        System.out.println("게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)");
         return readLine();
     }
 }
--- src/main/java/bridge/view/OutputView.java
+++ src/main/java/bridge/view/OutputView.java
@@ -1,23 +1,27 @@
 package bridge.view;
 
+import bridge.setting.Setting;
+
+import java.util.List;
+
 public class OutputView {
 
-    public void printGameStart(){
+    public void printGameStart() {
         System.out.println("다리 건너기 게임을 시작합니다.");
     }
 
-    /**
-     * 현재까지 이동한 다리의 상태를 정해진 형식에 맞춰 출력한다.
-     * <p>
-     * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
-     */
-    public void printMap() {
+    public void printMap(String bridge) {
+        System.out.print("[");
+        System.out.print(bridge);
+        System.out.println("]");
     }
 
-    public void printResult(String resultMessage, int tryCount) {
-        System.out.println("\n최종 게임 결과");
-        printMap();
-        System.out.println(String.format("\n게임 성공 여부: %s", resultMessage));
+    public void printResult(String bridge, String resultMessage, int tryCount) {
+        System.out.println();
+        System.out.println("최종 게임 결과");
+        printMap(bridge);
+
+        System.out.println(String.format("%n게임 성공 여부: %s", resultMessage));
         System.out.println(String.format("총 시도한 횟수: %d", tryCount));
     }
 }
