3fef269ec040d031a9010ec22018ce4b25b3c346
yumyeonghan <mgtmh991013@gmail.com>
2022-11-20 17:59:10 +0900

    test: Add test case
    Execute retry method test

--- src/test/java/bridge/BridgeGameTest.java
+++ src/test/java/bridge/BridgeGameTest.java
@@ -6,6 +6,8 @@ import org.junit.jupiter.api.Test;
 
 import java.util.Arrays;
 
+import static org.assertj.core.api.Assertions.*;
+
 class BridgeGameTest {
 
     private BridgeGame bridgeGame;
@@ -18,8 +20,13 @@ class BridgeGameTest {
     @Test
     void 칸을_이동하는_메서드_테스트() {
 
-        Assertions.assertThat(bridgeGame.move(4, "U", Arrays.asList("U", "D", "U"))).isFalse();
+        assertThat(bridgeGame.move(4, "U", Arrays.asList("U", "D", "U"))).isFalse();
 
-        Assertions.assertThat(bridgeGame.move(2, "U", Arrays.asList("U", "D", "U"))).isTrue();
+        assertThat(bridgeGame.move(2, "U", Arrays.asList("U", "D", "U"))).isTrue();
+    }
+
+    @Test
+    void 재시작_메서드_테스트() {
+        assertThat(bridgeGame.retry("R")).isTrue();
     }
 }
