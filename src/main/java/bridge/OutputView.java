9f55b2b8ec04cd018af12b791b2038440a801f7f
이요환 <91039622+bumstead-bumstead@users.noreply.github.com>
2022-11-20 22:54:20 +0900

    <feat>(OutputView.java): printMap() 메서드 작성
     - 현재 다리 상태 출력하는 메서드 작성

--- src/main/java/bridge/OutputView.java
+++ src/main/java/bridge/OutputView.java
@@ -19,6 +19,8 @@ public class OutputView {
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
     public void printMap(Map<String, List<Integer>> moveStatus) {
+        System.out.println(formatMoveStatus(moveStatus.get("U")));
+        System.out.println(formatMoveStatus(moveStatus.get("D")));
     }
 
     /**
