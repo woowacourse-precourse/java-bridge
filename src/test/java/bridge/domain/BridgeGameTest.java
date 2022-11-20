c088f0c6b3c96229706d93c93e0371ef5b0e7792
YSEO <1park4170@gmail.com>
2022-11-20 19:44:21 +0900

    test<BridgeGameTest> : BridgeGame에 대한 테스트 추가
    - @ParameterizedTest와 source를 활용해 여러 값에 대한 테스트를 획일화
    - move,retry 모두 boolean을 리턴하므로 각각 true와 false를 리턴하는 경우에 대하여 테스트함.

---
+++ src/test/java/bridge/domain/BridgeGameTest.java
@@ -0,0 +1,79 @@
+package bridge.domain;
+
+import bridge.Bridge;
+import camp.nextstep.edu.missionutils.test.NsTest;
+import org.assertj.core.api.AssertProvider;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import java.util.List;
+
+import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
+import static org.junit.jupiter.api.Assertions.*;
+
+class BridgeGameTest {
+    private Bridge bridge;
+    private BridgeGame bridgeGame = new BridgeGame();
+    private PrintStream standardOut;
+    ByteArrayOutputStream captor;
+    @BeforeEach
+    void setUp() {
+        List<String> bridgeContent = List.of("U","D","U","D");
+        bridge = new Bridge(bridgeContent);
+    }
+
+    @AfterEach
+    void tearDown() {
+        bridge = null;
+    }
+
+    @DisplayName("move메소드 올바른 경우 테스트.")
+    @ParameterizedTest
+    @CsvSource({
+            "U, 0",
+            "D, 1",
+            "U, 2",
+            "D, 3"
+    })
+    void move_right(String userChoice, int index) {
+        boolean bool = bridgeGame.move(bridge, userChoice, index);
+        Assertions.assertThat(bool).isEqualTo(true);
+    }
+
+    @DisplayName("move메소드 잘못된 경우 테스트.")
+    @ParameterizedTest
+    @CsvSource({
+            "D, 0",
+            "U, 1",
+            "D, 2",
+            "U, 3"
+    })
+    void move_false(String userChoice, int index) {
+        boolean bool = bridgeGame.move(bridge, userChoice, index);
+        Assertions.assertThat(bool).isEqualTo(false);
+    }
+
+    @DisplayName("retry메소드 그만두는 경우 테스트.")
+    @ParameterizedTest
+    @ValueSource(strings = {"Q","q"})
+    void retry_false(String userChoice) {
+        boolean retry = bridgeGame.retry(userChoice);
+        Assertions.assertThat(retry).isEqualTo(false);
+    }
+
+    @DisplayName("retry메소드 재시도하는 경우 테스트.")
+    @ParameterizedTest
+    @ValueSource(strings = {"R","r"})
+    void retry_true(String userChoice) {
+        boolean retry = bridgeGame.retry(userChoice);
+        Assertions.assertThat(retry).isEqualTo(true);
+    }
+}
