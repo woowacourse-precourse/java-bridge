7d5eb225221356149883136063177fbd6f81ba8c
miffyKing <whqudghk28@gmail.com>
2022-11-20 19:26:11 +0900

    fix: Change length instance final and change string to enum class

--- src/main/java/bridge/model/BridgeLength.java
+++ src/main/java/bridge/model/BridgeLength.java
@@ -1,14 +1,16 @@
 package bridge.model;
 
+import bridge.view.Sentence;
+
 public class BridgeLength {
 
-    private Integer length;
+    private final Integer length;
     private static final int MIN_LIMIT = 3;
     private static final int MAX_LIMIT = 20;
 
     private BridgeLength(final int length) {
         if (!isBetweenRange(length)) {
-            throw new IllegalArgumentException("input not in range");
+            throw new IllegalArgumentException(Sentence.LENGTH_NOT_IN_RANGE.getValue());
         }
         this.length = length;
     }
