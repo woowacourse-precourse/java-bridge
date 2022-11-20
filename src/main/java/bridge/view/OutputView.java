5c317758321a8d3c85d733c608af4b2878795823
이건회 <rg970604@naver.com>
2022-11-20 18:47:28 +0900

    feat(OutputView.java) : 게임 최종 결과 출력

--- src/main/java/bridge/view/OutputView.java
+++ src/main/java/bridge/view/OutputView.java
@@ -1,9 +1,15 @@
 package bridge.view;
 
+import bridge.domain.Bridge;
+
 /**
  * 사용자에게 게임 진행 상황과 결과를 출력하는 역할을 한다.
  */
 public class OutputView {
+
+    public static void printStart() {
+        System.out.println("다리 건너기 게임을 시작합니다.");
+    }
 
     /**
      * 현재까지 이동한 다리의 상태를 정해진 형식에 맞춰 출력한다.
@@ -18,6 +24,8 @@ public class OutputView {
      * <p>
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void printResult() {
+    public static void printResult(String result, int count) {
+        System.out.println("게임 성공 여부 : " + result);
+        System.out.println("총 시도한 횟수 :" + count);
     }
 }
