c0b02a68dfea7d4dbaffc77367dec6e8651faef4
lacram <chemin9898@gmail.com>
2022-11-20 15:44:04 +0900

    feat: 플레이어가 이동할 칸을 입력 받는 기능 추가

--- src/main/java/bridge/InputView.java
+++ src/main/java/bridge/InputView.java
@@ -2,7 +2,7 @@ package bridge;
 
 import camp.nextstep.edu.missionutils.Console;
 
-import static bridge.Validation.validateBridgeSize;
+import static bridge.Validation.*;
 
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
@@ -23,7 +23,10 @@ public class InputView {
      * 사용자가 이동할 칸을 입력받는다.
      */
     public String readMoving() {
-        return null;
+        System.out.println("이동할 칸을 선택해주세요. (위: U, 아래: D)");
+        String enteredMoving = Console.readLine();
+        validateMoving(enteredMoving);
+        return enteredMoving;
     }
 
     /**
