67cd351b8bf0d44db976244cf2b180e8a9d2b4c7
Invidam <hansupark0208@gmail.com>
2022-11-20 21:04:56 +0900

    feat(output-view): 게임 제어 메시지 출력 기능 추가

--- src/main/java/bridge/view/OutputView.java
+++ src/main/java/bridge/view/OutputView.java
@@ -1,5 +1,7 @@
 package bridge.view;
 
+import bridge.message.SystemMessage;
+
 /**
  * 사용자에게 게임 진행 상황과 결과를 출력하는 역할을 한다.
  */
@@ -13,6 +15,9 @@ public class OutputView {
     public void printMap() {
     }
 
+    public void printMessage(SystemMessage message) {
+        System.out.println(message.getMessage());
+    }
     /**
      * 게임의 최종 결과를 정해진 형식에 맞춰 출력한다.
      * <p>
