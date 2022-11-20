5a2ded2f42aedaab69452f2c0d58167bc52d2797
leesangjin <tkdwls0628@gmail.com>
2022-11-21 00:57:19 +0900

    feat(ErrorMessage): 이동과 관련된 에러 메시지 정의

--- src/main/java/bridge/constant/ErrorMessage.java
+++ src/main/java/bridge/constant/ErrorMessage.java
@@ -6,4 +6,6 @@ public class ErrorMessage {
 
     public static final String WRONG_BRIDGE_SIZE = ERROR + "다리의 길이는 3이상 20이하의 정수 입니다.";
     public static final String WRONG_BRIDGE_SIZE_TYPE = ERROR + "다리의 길이는 정수값이여야 합니다.";
+
+    public static final String WRONG_MOVING_VALUE = ERROR + "이동할 수 있는 칸은 U 또는 D 입니다.";
 }
