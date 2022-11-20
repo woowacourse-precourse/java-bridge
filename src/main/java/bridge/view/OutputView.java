34ef62e55c6656ce33b05f4c41efdfb44b853756
eastsage <dpwk8@naver.com>
2022-11-21 03:09:44 +0900

    feat: 게임 결과를 출력하는 함수 추가

--- src/main/java/bridge/view/OutputView.java
+++ src/main/java/bridge/view/OutputView.java
@@ -41,6 +41,19 @@ public class OutputView {
      * <p>
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void printResult() {
+    public void printResult(boolean isWin, String tryCount) {
+        System.out.println("최종 게임 결과");
+        System.out.println(map + System.lineSeparator());
+        printIsWin(isWin);
+        System.out.println("총 시도한 횟수: " + tryCount);
+    }
+
+    private void printIsWin(boolean isWin) {
+        if (isWin) {
+            System.out.println("게임 성공 여부: 성공");
+        }
+        if (!isWin) {
+            System.out.println("게임 성공 여부: 실패");
+        }
     }
 }
