fdb6c53fed3dd198fc7500d43fcc5219c0bd360f
NotableDeveloper <castlereal@naver.com>
2022-11-19 22:01:08 -0800

    <feat> : 다리 윗쪽길 출력 기능 구현중 다리의 윗쪽 길을 출력하는 기능 구현중. Main 메서드에 다리 출력 기능을 추가한 상태임.

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -1,12 +1,24 @@
 package bridge;
 
+import bridge.BridgeMaker.BridgeMaker;
+import bridge.BridgeMaker.NumberGenerator.BridgeRandomNumberGenerator;
 import bridge.UI.Input.InputView;
+import bridge.UI.Output.OutputView;
+
+import java.util.List;
 
 public class Application {
     public static void main(String[] args) {
         InputView inputView = new InputView();
-        inputView.readBridgeSize();
-        inputView.readMoving();
+        BridgeRandomNumberGenerator generator = new BridgeRandomNumberGenerator();
+        BridgeMaker bridgeMaker = new BridgeMaker(generator);
+
+        int bridgeSize = inputView.readBridgeSize();
+        List<String> bridge = bridgeMaker.makeBridge(bridgeSize);
+        OutputView outputView = new OutputView(bridge);
+
+        String move = inputView.readMoving();
+        outputView.printMap(0, move);
         inputView.readGameCommand();
     }
 }
