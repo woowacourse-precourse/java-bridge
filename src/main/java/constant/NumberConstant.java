5b77805881173c8b9eef6dd6ecce6ae1b364465f
nylonmusk <106235834+nylonmusk@users.noreply.github.com>
2022-11-20 21:43:48 +0900

    refactor(NumberConstant): 한글자 두글자 상수 추가

--- src/main/java/constant/NumberConstant.java
+++ src/main/java/constant/NumberConstant.java
@@ -7,7 +7,9 @@ public enum NumberConstant {
     DOWN_SIDE(0),
     BRIDGE_SIZE_START_INCLUSIVE(3),
     BRIDGE_SIZE_END_INCLUSIVE(20),
-    FIRST_TRY(1);
+    FIRST_TRY(1),
+    ONE_LETTER(1),
+    TWO_LETTERS(2);
 
     public final int numberConstant;
 
