d23611de778f2aa2d6d5752a7c2b1758b9c84264
이요환 <91039622+bumstead-bumstead@users.noreply.github.com>
2022-11-21 00:04:18 +0900

    <fix>(InputView.java): 안내 메세지 출력 기능 추가

--- src/main/java/bridge/InputView.java
+++ src/main/java/bridge/InputView.java
@@ -9,6 +9,7 @@ public class InputView {
     private static final String bridgeSizeInputExceptionMessage = "다리 길이는 3부터 20 사이의 숫자여야 합니다.";
     private static final String movingInputExceptionMessage = "U와 D 중 하나로 입력해야 합니다.";
     private static final String gameCommandInputExceptionMessage = "R과 Q 중 하나로 입력해야 합니다.";
+    private static OutputView outputView = new OutputView();
 
     /**
      * 다리의 길이를 입력받는다.
@@ -17,7 +18,7 @@ public class InputView {
     
     //모든 메서드에서 안내 메세지 출력 필요 -> OutputView 클래스에 구현하깅
     public int readBridgeSize() {
-        //안내 메세지 출력 필요
+        outputView.printLengthMessage();
         String bridgeSize = Console.readLine();
         validateBridgeSizeInput(bridgeSize);
 
@@ -28,7 +29,7 @@ public class InputView {
      * 사용자가 이동할 칸을 입력받는다.
      */
     public String readMoving() {
-        //안내 메세지 출력 필요
+        outputView.printMoveMessage();
         String direction = Console.readLine();
         validateMovingInput(direction);
         return direction;
@@ -38,7 +39,7 @@ public class InputView {
      * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
      */
     public String readGameCommand() {
-        //안내 메세지 출력 필요
+        outputView.printRetryMessage();
         String gameCommand = Console.readLine();
         validateGameCommand(gameCommand);
         return gameCommand;
