9d1508f820f01f9ed91258c601b45fa127183da6
hojunking96 <junsong96@naver.com>
2022-11-20 16:03:07 +0900

    feats(OutputView.java) : 다리 건너기 결과 출력 기능 구현

--- src/main/java/bridge/OutputView.java
+++ src/main/java/bridge/OutputView.java
@@ -11,6 +11,10 @@ public class OutputView {
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
     public void printMap() {
+        UpSide upSide = new UpSide(BridgeGame.movingInputs);
+        System.out.println(upSide.getUpSide());
+        DownSide downSide = new DownSide(BridgeGame.movingInputs);
+        System.out.println(downSide.getDownSide());
     }
 
     /**
