ed7ba4f6ee31b417ba85b4bd64ba00713e038707
nylonmusk <106235834+nylonmusk@users.noreply.github.com>
2022-11-21 00:35:57 +0900

    refactor(NumberConstant): 관련없는 상수 클래스 분리

--- src/main/java/constant/NumberConstant.java
+++ src/main/java/constant/NumberConstant.java
@@ -3,10 +3,6 @@ package constant;
 public enum NumberConstant {
     RANDOM_LOWER_INCLUSIVE(0),
     RANDOM_UPPER_INCLUSIVE(1),
-    UP_SIDE(1),
-    DOWN_SIDE(0),
-    BRIDGE_SIZE_START_INCLUSIVE(3),
-    BRIDGE_SIZE_END_INCLUSIVE(20),
     FIRST_TRY(1),
     ONE_LETTER(1),
     TWO_LETTERS(2),
