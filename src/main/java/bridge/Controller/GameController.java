c5caf8ff27b4e73b5aa19be9ae1e3bc3855dc1e7
jinju9553 <69393506+jinju9553@users.noreply.github.com>
2022-11-20 18:16:18 +0900

    Feat(controller) : 기능6 - 비즈니스 로직을 호출

--- src/main/java/bridge/Controller/GameController.java
+++ src/main/java/bridge/Controller/GameController.java
@@ -1,16 +1,39 @@
 package bridge.Controller;
 
 import bridge.Model.Bridge;
+import bridge.Service.BridgeGame;
 import bridge.View.InputView;
+import bridge.View.OutputView;
 
 public class GameController {
-    public void run(Bridge bridge) {
+    private BridgeGame bridgeGame = new BridgeGame();
+    private int progress;
 
+    public void run(Bridge bridge) {
+        boolean isCrossable = true;
+        progress = 0;
+
+        while (isCrossable) {
+            String input = handleInput();
+            String currentResult = bridge.getResult(input, progress);
+            isCrossable = bridgeGame.isCrossable(currentResult);
+
+            progress = bridgeGame.move(progress);
+            printResult(currentResult, input);
+        }
     }
 
-    public void handleInput() {
+    public String handleInput() {
         InputView inputView = new InputView();
 
         String input = inputView.readMoving();
+
+        return input;
+    }
+
+    public void printResult(String currentResult, String input) {
+        OutputView outputView = new OutputView();
+
+        outputView.printMap(currentResult, input, progress);
     }
 }
