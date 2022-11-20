c4413fd3491cb195f8bca99415ec3bc7e9e96b46
Plastic_hong <101045399+plastichong@users.noreply.github.com>
2022-11-20 18:58:39 +0900

    feat(InputManager) numberRangeCheck() 기능추가
    다리길이 숫자값 범위검증 추가, 메소드 분할

--- src/main/java/bridge/InputManager.java
+++ src/main/java/bridge/InputManager.java
@@ -7,12 +7,23 @@ public class InputManager {
     public int getBridgeSize() {
         System.out.println("다리의 길이를 입력해주세요.");
         String RawInputValue = Console.readLine();
+        int bridgeSize = validateNumber(RawInputValue);
+        numberRangeCheck(bridgeSize);
+        return bridgeSize;
+    }
+
+    private Integer validateNumber(String RawInputValue) {
         try {
             return Integer.parseInt(RawInputValue);
         } catch (NumberFormatException e) {
-            System.out.println("[ERROR] 다리 길이는 3부터 20 사이의 숫자여야 합니다.");
+            throw new IllegalArgumentException("[ERROR] 다리 길이는 3부터 20 사이의 숫자여야 합니다.");
         }
-        return 0;
     }
 
+
+    private void numberRangeCheck(int bridgeSize) {
+        if(bridgeSize>20||3>bridgeSize){
+            throw new IllegalArgumentException("[ERROR] 다리 길이는 3부터 20 사이의 숫자여야 합니다.");
+        }
+    }
 }
