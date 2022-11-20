1825495e57beab6a562bcebd83ce787fc9da47c7
JaehongDev <println@kakao.com>
2022-11-20 21:45:50 +0900

    feature (InputView) : 요청에 맞는 입력값을 반환해주는 기능 추가

--- src/main/java/bridge/view/InputView.java
+++ src/main/java/bridge/view/InputView.java
@@ -1,7 +1,10 @@
 package bridge.view;
 
 import bridge.domain.bridge.BridgeSize;
+import bridge.domain.position.BridgePosition;
+import bridge.domain.position.RestartCommand;
 import camp.nextstep.edu.missionutils.Console;
+import java.util.NoSuchElementException;
 
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
@@ -12,21 +15,31 @@ public class InputView extends IOPrinter {
      * 다리의 길이를 입력받는다.
      */
     public BridgeSize readBridgeSize() {
-        this.printBeforeNextLine("다리의 길이를 입력해주세요.");
+        this.printlnBeforeNextLine("다리의 길이를 입력해주세요.");
         return new BridgeSize(Console.readLine());
     }
 
     /**
      * 사용자가 이동할 칸을 입력받는다.
      */
-    public String readMoving() {
-        return null;
+    public BridgePosition readMoving() {
+        this.printlnBeforeNextLine("이동할 칸을 선택해주세요. (위: U, 아래: D)");
+        return BridgePosition.convertCodeToPosition(readLine());
     }
 
     /**
      * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
      */
-    public String readGameCommand() {
-        return null;
+    public RestartCommand readGameCommand() {
+        this.println("게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)");
+        return RestartCommand.generateCommand(readLine());
+    }
+
+    private String readLine() {
+        try {
+            return Console.readLine();
+        } catch (NoSuchElementException exception) {
+            throw new IllegalStateException("[ERROR] 입력할 수 없는 값입니다.");
+        }
     }
 }
