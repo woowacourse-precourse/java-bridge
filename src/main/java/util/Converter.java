2455c99fd196e76597ec6c3e22b85c9d6f896db5
nylonmusk <106235834+nylonmusk@users.noreply.github.com>
2022-11-20 21:51:01 +0900

    refactor(Converter): 변환하는 메소드들 클래스로 분리

---
+++ src/main/java/util/Converter.java
@@ -0,0 +1,11 @@
+package util;
+
+public class Converter {
+    public static int convertStringToInt(String size) {
+        return Integer.parseInt(size);
+    }
+
+    public static String convertToUpperCase(String input) {
+        return input.toUpperCase();
+    }
+}
