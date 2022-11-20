176f1596130b407dab52d52d1873d9950535ca68
Minjune <minjune8506@gmail.com>
2022-11-20 13:44:27 +0900

    style(Bridge Game): 주석 추가

--- src/main/java/bridge/domain/BridgeGame.java
+++ src/main/java/bridge/domain/BridgeGame.java
@@ -22,7 +22,7 @@ public class BridgeGame {
      * <p>
      * 이동을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      *
-     * @param direction
+     * @param direction 이동할 방향
      */
     public GameStatus move(Direction direction) {
         int userStatus = user.cross(bridge, direction);
@@ -47,6 +47,8 @@ public class BridgeGame {
      * 사용자가 게임을 다시 시도할 때 사용하는 메서드
      * <p>
      * 재시작을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
+     *
+     * @param gameCommand 재시작 / 종료 명령
      */
     public GameStatus retry(GameCommand gameCommand) {
         if (gameCommand == GameCommand.RETRY) {
--- src/main/java/bridge/view/OutputView.java
+++ src/main/java/bridge/view/OutputView.java
@@ -17,7 +17,7 @@ public class OutputView {
      * <p>
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      *
-     * @param footPrints
+     * @param footPrints 현재까지 이동한 다리의 상태
      */
     public void printMap(String footPrints) {
         System.out.println(footPrints);
@@ -28,8 +28,8 @@ public class OutputView {
      * <p>
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      *
-     * @param bridgeGame
-     * @param status
+     * @param bridgeGame 다리 건너기 게임 정보
+     * @param status     게임 상태
      */
     public void printResult(BridgeGame bridgeGame, GameStatus status) {
         System.out.println("최종 게임 결과");
