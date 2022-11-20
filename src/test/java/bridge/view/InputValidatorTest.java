f0d4a4696c939d731ece965c13b87e12b62a0eaa
dhkim <akdlzmf1123@gmail.com>
2022-11-20 14:39:43 +0900

    test(InputValidatorTest): 다리 길이 validation 기능 테스트 추가

--- src/test/java/bridge/view/InputValidatorTest.java
+++ src/test/java/bridge/view/InputValidatorTest.java
@@ -4,25 +4,41 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
-import static org.assertj.core.api.Assertions.assertThatNoException;
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.*;
 
 class InputValidatorTest {
-    @ParameterizedTest(name = "이동할 칸이 {0}이면 예외 던진다.")
+    @ParameterizedTest(name = "사용자 입력: {0}이면 형식에 맞다")
+    @ValueSource(strings = {"U", "D"})
+    void validateMoving_메서드는_사용자_입력을_받아_형식에_맞는지_확인한다(String moving) {
+        assertThatNoException().isThrownBy(() -> {
+            InputValidator.validateMoving(moving);
+        });
+    }
+
+    @ParameterizedTest(name = "사용자 입력: {0}이면 예외 던진다")
     @ValueSource(strings = {"u", "d", "UD", "DU", " U", "D ", " ", ""})
-    void 이동할_칸_형식_예외_던지는_기능(String moving) {
+    void validateMoving_메서드는_사용자_입력을_받아_형식에_안맞으면_예외_던진다(String moving) {
         assertThatThrownBy(() -> {
             InputValidator.validateMoving(moving);
         }).isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining("[ERROR]");
     }
 
-    @ParameterizedTest(name = "이동할 칸은 {0}의 형식이다.")
-    @ValueSource(strings = {"U", "D"})
-    void 이동할_칸_형식_U_D는_받아들이는_기능(String moving) {
+    @ParameterizedTest(name = "사용자 입력: {0}이면 0 이상의 정수이다")
+    @ValueSource(strings = {"3", "20", "123"})
+    void validateBridgeSize_메서드는_사용자_입력을_받아_0_이상의_정수가_맞는지_확인한다(String bridgeSize) {
         assertThatNoException().isThrownBy(() -> {
-            InputValidator.validateMoving(moving);
+            InputValidator.validateBridgeSize(bridgeSize);
         });
+    }
+
+    @ParameterizedTest(name = "사용자 입력: {0}이면 0 이상의 정수가 아니다")
+    @ValueSource(strings = {" ", "-3", "", "5j", "a"})
+    void validateBridgeSize_메서드는_사용자_입력을_받아_0_이상의_정수가_아니면_예외_던진다(String bridgeSize) {
+        assertThatThrownBy(() -> {
+            InputValidator.validateBridgeSize(bridgeSize);
+        }).isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("[ERROR]");
     }
 }
