69d091ba8c84a4401b0667771b4d041033bef433
박승민 <tmddudals369@naver.com>
2022-11-20 23:37:49 +0900

    refactor: 게임 진행 예외 처리 조건 문자열 상수로 분리

--- src/main/java/bridge/utils/validator/GameValidator.java
+++ src/main/java/bridge/utils/validator/GameValidator.java
@@ -1,18 +1,22 @@
 package bridge.utils.validator;
 
+import static bridge.utils.constants.BridgeConstants.UP_CROSS;
+import static bridge.utils.constants.BridgeConstants.DOWN_CROSS;
 import static bridge.utils.constants.ErrorConstants.NOT_MATCH_MOVING_OPTION;
 import static bridge.utils.constants.ErrorConstants.NOT_MATCH_GAME_COMMAND;
+import static bridge.utils.constants.GameConstants.RETRY_GAME;
+import static bridge.utils.constants.GameConstants.EXIT_GAME;
 
 public class GameValidator {
 
     public static void validMovingOption(String movingOption) {
-        if (!movingOption.equals("U") || !movingOption.equals("D")) {
+        if (!movingOption.equals(UP_CROSS) || !movingOption.equals(DOWN_CROSS)) {
             throw new IllegalArgumentException(NOT_MATCH_MOVING_OPTION);
         }
     }
 
     public static void validGameCommand(String gameCommand) {
-        if (!gameCommand.equals("R") || !gameCommand.equals("Q")) {
+        if (!gameCommand.equals(RETRY_GAME) || !gameCommand.equals(EXIT_GAME)) {
             throw new IllegalArgumentException(NOT_MATCH_GAME_COMMAND);
         }
     }
