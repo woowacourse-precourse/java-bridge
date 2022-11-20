a1fbe007bbbb36b53056646a68558450410f9182
Wooseobee <dntjq0807@naver.com>
2022-11-21 03:28:39 +0900

    feat: 다리 건너기 게임 구현 - 게임 진행 구현

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -5,8 +5,8 @@ import java.util.List;
 public class Application {
 
     public static void main(String[] args) {
-        int n;
-        boolean gameOver = false;
+        int n, i = 0, cnt = 1;
+        boolean gameOver = false, retry = true, success = false;
         List<String> bridge;
 
         System.out.println("다리 건너기 게임을 시작합니다.\n");
@@ -17,16 +17,30 @@ public class Application {
 
         do {
             System.out.println("이동할 칸을 선택해주세요. (위: U, 아래: D)");
-            InputView.readMoving();
+
+            if (InputView.readMoving().equals(bridge.get(i))) {
+                i++;
+            }
+
+            gameOver = OutputView.printMap(bridge, i - 1);
 
             if (gameOver) {
                 System.out.println("\n게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)");
-                InputView.readGameCommand();
+                String s = InputView.readGameCommand();
+                retry = BridgeGame.retry(s);
+                i = 0;
+                cnt = BridgeGame.countGameNum(s, cnt);
+                gameOver = false;
             }
 
-        } while(BridgeGame.retry());
+            if (i == n) {
+                success = true;
+                retry = false;
+            }
 
-        System.out.println("게임 성공 여부: ");
-        System.out.println("총 시도한 횟수: ");
+        } while(retry);
+
+        System.out.println("게임 성공 여부: " + OutputView.isGameSuccess(success));
+        System.out.println("총 시도한 횟수: " + cnt);
     }
 }
--- src/main/java/bridge/BridgeGame.java
+++ src/main/java/bridge/BridgeGame.java
@@ -18,7 +18,18 @@ public class BridgeGame {
      * <p>
      * 재시작을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public static boolean retry() {
+    public static boolean retry(String s) {
+        if (s.equals("R")) {
+            return true;
+        }
         return false;
     }
+
+    public static int countGameNum(String s, int cnt) {
+        if (s.equals("R")) {
+            return cnt + 1;
+        }
+        return cnt;
+    }
+
 }
