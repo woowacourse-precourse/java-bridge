cc5550b9de2e7e2eda6c9c370b82737688e24db4
ddogga <hayeonlee60327@gmail.com>
2022-11-20 22:43:18 +0900

    InputViewExceptionTest생성 및 테스트 목록 작성

---
+++ src/test/java/bridge/InputViewExceptionTest.java
@@ -0,0 +1,13 @@
+package bridge;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class InputViewExceptionTest {
+
+    //다리 길이 입력값 예외 테스트
+
+    //사용자 이동 입력값 예외 테스트
+
+    //게임 재시작, 종료 여부 입력값 예외 테스트
+
+}
