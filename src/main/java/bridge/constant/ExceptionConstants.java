47abebf0ca087bc8ed88bc38c3fba071949df218
CAEL0 <kjongmin26@gmail.com>
2022-11-20 17:30:38 +0900

    feat(ExceptionConstants): 다리 길이 관련 에러 메시지 추가

--- src/main/java/bridge/constant/ExceptionConstants.java
+++ src/main/java/bridge/constant/ExceptionConstants.java
@@ -1,7 +1,9 @@
 package bridge.constant;
 
 public enum ExceptionConstants {
-    NOT_NATURAL_NUMBER("다리 길이는 자연수여야 합니다.");
+    NOT_NATURAL_NUMBER("다리 길이는 자연수여야 합니다."),
+    INCORRECT_RANGE(String.format("다리 길이는 %d부터 %d 사이의 숫자여야 합니다.",
+            BridgeConstants.MINIMUM_LENGTH, BridgeConstants.MAXIMUM_LENGTH));
 
     private final String message;
 
