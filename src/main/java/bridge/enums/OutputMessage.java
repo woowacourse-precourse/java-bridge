2cfd4634fd20e11caad83ef5ff2e0da77e012bcc
3juhwan <13selfesteem91@naver.com>
2022-11-20 10:17:53 +0900

    feat(bridge): 출력 메세지 열거형 구현

--- src/main/java/bridge/enums/OutputMessage.java
+++ src/main/java/bridge/enums/OutputMessage.java
@@ -1,2 +1,20 @@
-package bridge.enums;public enum OutputMessage {
+package bridge.enums;
+
+public enum OutputMessage {
+
+    GAME_START_MESSAGE("다리 건너기 게임을 시작합니다."),
+    FINAL_GAME_RESULT_MESSAGE("최종 게임 결과"),
+    GAME_STATUS_MESSAGE("게임 성공 여부: "),
+    TOTAL_ATTEMPTS_MESSAGE("총 시도한 횟수: "),
+    ;
+
+    private final String message;
+
+    OutputMessage(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
 }
