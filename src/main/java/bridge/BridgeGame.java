be447112e6a7cea9d7e2766e6b4c4fee8c133a61
DaWoon Lee <qkgk61@gmail.com>
2022-11-20 20:33:37 +0900

    Feat : BridgeGame 클래스에서 게임 재시작 여부를 반환하는 메서드 구현

--- src/main/java/bridge/BridgeGame.java
+++ src/main/java/bridge/BridgeGame.java
@@ -1,7 +1,7 @@
 package bridge;
 
 import bridge.domain.MovingResult;
-import bridge.validator.InputValidator;
+import bridge.view.InputView;
 import java.util.List;
 
 /**
@@ -30,8 +30,9 @@ public class BridgeGame {
      * <p>
      * 재시작을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void retry(InputView inputView) {
-
+    public boolean retry(InputView inputView) {
+        String gameCommand = inputView.readGameCommand();
+        return gameCommand.equals("R");
     }
 
 }
--- src/main/java/bridge/validator/InputValidator.java
+++ src/main/java/bridge/validator/InputValidator.java
@@ -6,9 +6,11 @@ import java.util.Set;
 public class InputValidator {
 
     private static final Set<String> MOVING_SET;
+    private static final Set<String> GAME_COMMAND_SET;
 
     static {
         MOVING_SET = Set.of("D", "U");
+        GAME_COMMAND_SET = Set.of("R", "Q");
     }
 
     public static void validateBridgeSize(int size) {
@@ -20,6 +22,12 @@ public class InputValidator {
     public static void validateMoving(String moving) {
         if (!MOVING_SET.contains(moving)) {
             throw new IllegalArgumentException("[ERROR] 위로 이동하려면 U를, 아래로 이동하려면 D를 입력하세요.");
+        }
+    }
+
+    public static void validateGameCommand(String gameCommand) {
+        if (!GAME_COMMAND_SET.contains(gameCommand)) {
+            throw new IllegalArgumentException("[ERROR] 다시 시작하려면 R을, 종료하려면 Q를 입력하세요.");
         }
     }
 
