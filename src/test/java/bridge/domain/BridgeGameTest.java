3ca2fe4afbac2d8a489d87c8f35e9816cb0d484e
Rovna99 <80767387+Rovna99@users.noreply.github.com>
2022-11-20 21:21:07 +0900

    test: BridgeGameTest 코드 수정

--- src/test/java/bridge/domain/BridgeGameTest.java
+++ src/test/java/bridge/domain/BridgeGameTest.java
@@ -8,47 +8,42 @@ import java.util.List;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
 
 class BridgeGameTest {
 	List<String> bridge;
 	BridgeGame bridgeGame;
 	ProgressMap gameProgress;
+	List<List<String>> expect;
+	String upInput = "U";
+	String downInput = "D";
 
 	@BeforeEach
 	void setup() {
 		bridge = new ArrayList<>(List.of("U", "D", "U"));
 		bridgeGame = new BridgeGame(bridge);
 		gameProgress = new ProgressMap();
+		expect = new ArrayList<>();
 	}
 
-	@Test
+	@ParameterizedTest
 	@DisplayName("결과 반영 테스트")
 	void correctMoveTest() {
-		String input = "U";
-		String input2 = "D";
-		String input3 = "U";
-		List<List<String>> expect = new ArrayList<>();
 		expect.add(List.of(" O ", "   ", " O "));
 		expect.add(List.of("   ", " O ", "   "));
-		bridgeGame.move(input, gameProgress);
-		bridgeGame.move(input2, gameProgress);
-		bridgeGame.move(input3, gameProgress);
-
+		bridgeGame.move(upInput, gameProgress);
+		bridgeGame.move(downInput, gameProgress);
+		bridgeGame.move(upInput, gameProgress);
 		assertEquals(expect, gameProgress.getProgressMap());
 	}
 
 	@Test
 	@DisplayName("게임을 통과하지 못했을 때 결과 테스트")
 	void incorrectMoveTest() {
-		String input = "U";
-		String input2 = "U";
-		List<List<String>> expect = new ArrayList<>();
 		expect.add(List.of(" O ", " X "));
 		expect.add(List.of("   ", "   "));
-
-		bridgeGame.move(input, gameProgress);
-		bridgeGame.move(input2, gameProgress);
-
+		bridgeGame.move(upInput, gameProgress);
+		bridgeGame.move(upInput, gameProgress);
 		assertEquals(expect, gameProgress.getProgressMap());
 	}
 }
