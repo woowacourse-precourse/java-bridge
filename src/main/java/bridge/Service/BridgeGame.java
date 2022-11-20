ff9784c6a2379bf17c0d7bbd89fe8df6066cc7e2
jinju9553 <69393506+jinju9553@users.noreply.github.com>
2022-11-20 18:12:40 +0900

    Feat(service) : 기능6 - 다리 이동가능여부 판별 및 이동하는 메소드 작성

--- src/main/java/bridge/Service/BridgeGame.java
+++ src/main/java/bridge/Service/BridgeGame.java
@@ -1,5 +1,7 @@
 package bridge.Service;
 
+import bridge.Model.GameResult;
+
 /**
  * 다리 건너기 게임을 관리하는 클래스
  */
@@ -7,16 +9,16 @@ public class BridgeGame {
 
     //TODO: BridgeGame 클래스에서 InputView, OutputView 를 사용하지 않는다.
     //TODO: 필드 추가 가능, 인자와 반환타입 변경 가능, 그러나 메서드 이름 변경 금지
+    private static final String SUCCESS = GameResult.SUCCESS.getResult();
+    private static final String FAIL = GameResult.FAIL.getResult();
 
     /**
      * 사용자가 칸을 이동할 때 사용하는 메서드
      * <p>
      * 이동을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public int move(boolean isCrossable, int progress) {
-        if (isCrossable) {
-            progress++;
-        }
+    public int move(int progress) {
+        progress += 1;
 
         return progress;
     }
@@ -28,8 +30,15 @@ public class BridgeGame {
      */
     public void retry() {
     }
-    
+
     public void isCleared() {
         //게임을 모두 클리어했는지 판별한다.
+    }
+
+    public boolean isCrossable(String currentResult) {
+        if (currentResult.equals(SUCCESS)) {
+            return true;
+        }
+        return false;
     }
 }
