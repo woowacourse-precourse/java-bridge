1f4f03d9473d9b1daa5a5b6378d38c683eb311dc
bellljun3395 <195850@jnu.ac.kr>
2022-11-20 18:35:25 +0900

    feat(BridgeGameStatus): 생성 Enum

---
+++ src/main/java/bridge/game/bridgeGame/BridgeGameStatus.java
@@ -0,0 +1,38 @@
+//package bridge.game.bridgeGame;
+//
+//public class BridgeGameStatus {
+//
+//    private boolean correct;
+//    private boolean fall;
+//
+//    public boolean isCorrect() {
+//        return correct;
+//    }
+//
+//    public boolean isFall() {
+//        return fall;
+//    }
+//
+//
+//    public void correct() {
+//        this.correct = true;
+//    }
+//
+//    public void fall() {
+//        this.fall = true;
+//    }
+//
+//}
+//
+
+package bridge.game.bridgeGame;
+
+public enum BridgeGameStatus {
+    BEFORE_START,
+    CORRECT,
+    FALL,
+    ;
+
+}
+
+
