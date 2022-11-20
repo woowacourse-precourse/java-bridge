caadc286bb44b396ffc605ac0f6955876048cceb
5nam <ohnam90@gmail.com>
2022-11-20 13:27:10 +0900

    feat: 게임 최종 결과를 정해진 형식에 맞춰 출력하는 기능 추가

--- src/main/java/bridge/View/OutputView.java
+++ src/main/java/bridge/View/OutputView.java
@@ -75,5 +75,9 @@ public class OutputView {
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
     public void printResult() {
+        bridgeMapUp.stream().forEach(System.out::print);
+        System.out.println();
+        bridgeMapDown.stream().forEach(System.out::print);
+        System.out.println();
     }
 }
