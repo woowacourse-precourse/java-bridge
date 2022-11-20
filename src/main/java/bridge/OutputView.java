47e55ad52d4d7548e9bd3d761c2a7650c8abe60f
Kwak-JunYoung <klop9090@g.skku.edu>
2022-11-21 02:51:04 +0900

    feat(OutputView.java) Added basic output formats
    Added messages related to the game result
    - The final game result
    - Whether the game was cleared successfully
    - The number of trials made
    
    Those added are only the messages. Not the exact results.

--- src/main/java/bridge/OutputView.java
+++ src/main/java/bridge/OutputView.java
@@ -11,6 +11,7 @@ public class OutputView {
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
     public void printMap() {
+        System.out.println("최종 게임 결과");
     }
 
     /**
@@ -18,6 +19,8 @@ public class OutputView {
      * <p>
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void printResult() {
+    public void printResult(String success, int trials) {
+        System.out.println("게임 성공 여부: " + success);
+        System.out.println("총 시도한 횟수: " + trials);
     }
 }
