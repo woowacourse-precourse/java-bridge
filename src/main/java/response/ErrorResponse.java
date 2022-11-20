329b59dd28ce001fa92c9f6a5d555880dc1b4049
KangSungBum <peter9706@naver.com>
2022-11-21 00:19:26 +0900

    feat(ErrorResponse.java): add new Error response value
    새로운 error response value 추가
    - INPUT_MOVEMENT_NULL_ERROR 추가
    - 사용자 이동 입력이 공백인 경우 예외 처리

--- src/main/java/response/ErrorResponse.java
+++ src/main/java/response/ErrorResponse.java
@@ -5,6 +5,7 @@ public enum ErrorResponse {
     INPUT_BRIDGE_SIZE_NOT_NUMBER_ERROR("[ERROR] 다리 길이 입력은 정수로 입력해야 합니다."),
     INPUT_BRIDGE_SIZE_NULL_ERROR("[ERROR] 다리 길이를 입력하세요."),
     INPUT_MOVEMENT_ERROR("[ERROR] 이동 입력은 대문자 U 또는 대문자 D만 허용됩니다."),
+    INPUT_MOVEMENT_NULL_ERROR("[ERROR] 이동(대문자 U 또는 D)을 입력하세요."),
     INPUT_EXITCODE_ERROR("[ERROR] 종료 선택 입력은 대문자 R 또는 대문자 Q만 허용됩니다."),
     ;
 
