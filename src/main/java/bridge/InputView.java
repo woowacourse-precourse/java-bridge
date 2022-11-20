e99c8a99a035e0132a866a855e190522a6502186
K-IMjihun <62210749+K-IMjihun@users.noreply.github.com>
2022-11-20 16:37:36 +0900

    InputView // 입력 메서드 수정 및 리팩토링

--- src/main/java/bridge/InputView.java
+++ src/main/java/bridge/InputView.java
@@ -1,6 +1,7 @@
 package bridge;
 
-import java.util.Scanner;
+import camp.nextstep.edu.missionutils.Console;
+
 
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
@@ -8,58 +9,55 @@ import java.util.Scanner;
  * 패키지, 메서드의 시그니처(인자, 이름)와 반환 타입 변경 및 메서드를 추가 가능.
  */
 public class InputView {
-    private Scanner sc = new Scanner(System.in);
-
     static int bridgeLength;
 
     public void readBridgeSize() {
         System.out.println("다리 건너기 게임을 시작합니다.");
         System.out.println("\n" + "다리의 길이를 입력해주세요.");
-        String bridgeLength = sc.nextLine();
-        bridgeSizeCheck(bridgeLength);
+        String bridgeLength = Console.readLine();
+        bridgeIntCheck(bridgeLength);       //숫자가 아닐 시 예외처리
+        bridgeSizeCheck(bridgeLength);      //3 ~ 20 사이의 수가 아닐 시 예외처리
         this.bridgeLength = Integer.parseInt(bridgeLength);
     }
-    public void bridgeSizeCheck(String bridgeLength){
-        if(bridgeLength.equals("")){
-            throw new IllegalArgumentException("[ERROR]");
-        }
-        for(int i = 0; i < bridgeLength.length(); i++){
-            if(!Character.isDigit(bridgeLength.charAt(i))){
-                throw new IllegalArgumentException("[ERROR]");
-            }
-        }
-        if(Integer.parseInt(bridgeLength) < 3 || Integer.parseInt(bridgeLength) > 20){
-            throw new IllegalArgumentException("[ERROR]");
-        }
-    }
+
     public String readMoving() {
             System.out.println("이동할 칸을 선택해주세요. (위: U, 아래: D)");
-            String moving = sc.next();
-            movingCheck(moving);
+            String moving = Console.readLine();
+            movingCheck(moving);             //U, D가 아니면 예외처리
             return moving;
-    }
-    public String movingCheck(String moving) {
-            if (moving.equals("")) {
-                throw new IllegalArgumentException("[ERROR]");
-            }
-            if (!moving.equals("U") && !moving.equals("D")) {
-                throw new IllegalArgumentException("[ERROR]");
-            }
-        return moving;
     }
 
         public String readGameCommand () {
             System.out.println("게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)");
-            String retry = sc.next();
-            retryCheck(retry);
+            String retry = Console.readLine();
+            retryCheck(retry);                 //R, Q가 아니면 예외처리
             return retry;
         }
-    public void retryCheck(String retry) {
-            if(retry.equals("")){
-                throw new IllegalArgumentException("[ERROR]");
+
+    public void bridgeIntCheck(String bridgeLength){
+        if(bridgeLength.equals("")){
+            throw new IllegalArgumentException("[ERROR] : 숫자를 입력해주세요");
+        }
+        for(int i = 0; i < bridgeLength.length(); i++){
+            if(!Character.isDigit(bridgeLength.charAt(i))){
+                throw new IllegalArgumentException("[ERROR] : 숫자를 입력해주세요");
             }
+        }
+    }
+    public void bridgeSizeCheck(String bridgeLength){
+        if(Integer.parseInt(bridgeLength) < 3 || Integer.parseInt(bridgeLength) > 20){
+            throw new IllegalArgumentException("[ERROR] : 3 ~ 20 사이의 숫자를 입력해주세요");
+        }
+    }
+    public String movingCheck(String moving) {
+        if (!moving.equals("U") && !moving.equals("D")) {
+            throw new IllegalArgumentException("[ERROR] : U, D만 입력 가능합니다");
+        }
+        return moving;
+    }
+    public void retryCheck(String retry) {
         if (!retry.equals("R") && !retry.equals("Q")) {
-            throw new IllegalArgumentException("[ERROR]");
+            throw new IllegalArgumentException("[ERROR] : R, Q만 입력 가능합니다");
         }
     }
 }
