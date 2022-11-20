f8a39e04536c731f889e4c31adac733d1873416f
splitube <voicesplit@daum.net>
2022-11-20 20:00:43 +0900

    test(BridgeGame): 단위 테스트 추가

--- src/test/java/bridge/game/BridgeGameTest.java
+++ src/test/java/bridge/game/BridgeGameTest.java
@@ -1,14 +1,13 @@
 package bridge.game;
 
+import static org.assertj.core.api.Assertions.*;
+
+import java.util.List;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
-
-import java.util.List;
-
-import static org.assertj.core.api.Assertions.*;
 
 /*
 [X] 목적지를 주면 캐릭터가 다음 목적지를 알고 결과를 저장하고 움직이는 테스트 : move(String destination)
@@ -37,35 +36,42 @@ class BridgeGameTest {
         bridgeGame = new BridgeGame(character, BRIDGE_SIZE);
     }
 
+    @DisplayName("다리의 원하는 구역에 이동가능 위치를 확인하는 기능 테스트")
+    @ValueSource(ints = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10})
+    @ParameterizedTest
+    void showRightDestinationInArea(int area) {
+        //given
+        List<String> bridge = bridgeGame.showBridge();
+        //when
+        String destination = bridgeGame.showRightDestinationInArea(area);
+        //then
+        assertThat(bridge.get(area)).isEqualTo(destination);
+    }
+
     @DisplayName("현재까지 이동 결과 조회 기능 테스트")
     @ValueSource(strings = {"U", "D"})
     @ParameterizedTest
     void showCurrentResult(String destination) {
+        //given
         bridgeGame.moveCycle(destination);
-        List<Move> moves = bridgeGame.showCurrentResult();
         String answer = bridgeGame.showRightDestinationInArea(0);
+        //when
+        List<Move> moves = bridgeGame.showCurrentResult();
+        //then
         assertThat(moves.size()).isEqualTo(1);
         assertThat(moves.get(0).getDestination()).isEqualTo(destination);
         assertThat(moves.get(0).isSuccess()).isEqualTo(destination.equals(answer));
     }
 
-    @DisplayName("다리의 원하는 구역에 이동가능 위치를 확인하는 기능 테스트")
-    @ValueSource(ints = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10})
-    @ParameterizedTest
-    void showRightDestinationInArea(int area) {
-        List<String> bridge = bridgeGame.showBridge();
-        String destination = bridgeGame.showRightDestinationInArea(area);
-        assertThat(bridge.get(area)).isEqualTo(destination);
-    }
-
     @DisplayName("재도전 기능 테스트")
     @Test
     void retry() {
+        //given
         bridgeGame.moveCycle("D");
         bridgeGame.moveCycle("U");
-        assertThat(bridgeGame.showCurrentResult().size()).isEqualTo(2);
-        assertThat(character.showCurrentLocation()).isEqualTo(1);
+        //when
         bridgeGame.retry();
+        //when
         assertThat(bridgeGame.showCurrentResult().size()).isEqualTo(0);
         assertThat(character.showCurrentLocation()).isEqualTo(-1);
     }
@@ -74,9 +80,12 @@ class BridgeGameTest {
     @ValueSource(strings = {"U", "D"})
     @ParameterizedTest
     void moveResult(String destination) {
+        //given
         bridgeGame.moveCycle(destination);
         String answer = bridgeGame.showRightDestinationInArea(0);
+        //when
         Result result = bridgeGame.moveResult();
+        //then
         if (destination.equals(answer)) {
             assertThat(result).isEqualTo(Result.CONTINUE);
         }
@@ -88,20 +97,46 @@ class BridgeGameTest {
     @DisplayName("총 도전 횟수 조회")
     @Test
     void showTotalTry() {
+        //given
+        //when
+        int start = bridgeGame.showTotalTry();
+        //then
+        assertThat(start).isEqualTo(0);
+    }
+
+    @DisplayName("도전 횟수 추가")
+    @Test
+    void addTotalTry() {
+        //given
         int before = bridgeGame.showTotalTry();
+        //when
         bridgeGame.addTotalTry();
+        //then
         int after = bridgeGame.showTotalTry();
         assertThat(before).isEqualTo(0);
         assertThat(after).isEqualTo(1);
     }
 
+    @DisplayName("성공 조회 기능 테스트")
+    @Test
+    void isSuccess() {
+        //given
+        //when
+        boolean defaultSuccess = bridgeGame.isSuccess();
+        //then
+        assertThat(defaultSuccess).isFalse();
+    }
+
     @DisplayName("성공 체크 기능 테스트")
     @Test
     void success() {
+        //given
         final boolean FAIL = false;
         final boolean SUCCESS = true;
         boolean before = bridgeGame.isSuccess();
+        //when
         bridgeGame.success();
+        //then
         boolean after = bridgeGame.isSuccess();
         assertThat(before).isEqualTo(FAIL);
         assertThat(after).isEqualTo(SUCCESS);
@@ -110,11 +145,14 @@ class BridgeGameTest {
     @DisplayName("성공 여부 표시 테스트")
     @Test
     void showSuccess() {
+        //given
         final String FAIL = "실패";
         final String SUCCESS = "성공";
+        //when
         String fail = bridgeGame.showSuccess();
         bridgeGame.success();
         String success = bridgeGame.showSuccess();
+        //then
         assertThat(fail).isEqualTo(FAIL);
         assertThat(success).isEqualTo(SUCCESS);
     }
@@ -166,7 +204,7 @@ class BridgeGameTest {
     @DisplayName("입력한 곳으로 캐릭터가 이동시 결과 저장 후 이동")
     @ValueSource(strings = {"U", "D"})
     @ParameterizedTest
-    void move(String destination) {
+    void moveCycle(String destination) {
         //given
         //when
         bridgeGame.moveCycle(destination);
