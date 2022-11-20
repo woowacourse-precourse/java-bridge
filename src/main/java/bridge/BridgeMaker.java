ba7d9936f53b1462045fd35805bb7d3b439da05a
miffyKing <whqudghk28@gmail.com>
2022-11-20 19:51:00 +0900

    feat: Generate bridgeMaker class that generate Answer List consist of "U", "D".

--- src/main/java/bridge/BridgeMaker.java
+++ src/main/java/bridge/BridgeMaker.java
@@ -1,6 +1,9 @@
 package bridge;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 /**
  * 다리의 길이를 입력 받아서 다리를 생성해주는 역할을 한다.
@@ -18,6 +21,32 @@ public class BridgeMaker {
      * @return 입력받은 길이에 해당하는 다리 모양. 위 칸이면 "U", 아래 칸이면 "D"로 표현해야 한다.
      */
     public List<String> makeBridge(int size) {
-        return null;
+//        List<Integer> generatedAnswerNumeric = new ArrayList<>();
+//        for (int i = 0; i < size; i++) {
+//            generatedAnswerNumeric.add(bridgeNumberGenerator.generate());
+//        }
+//        return changeToUD(generatedAnswerNumeric);
+        List<String> generatedAnswer = new ArrayList<>();
+        for (int i = 0; i < size; i++) {
+            generatedAnswer.add((changeToUD(bridgeNumberGenerator.generate())));
+        }
+        return generatedAnswer;
     }
+
+    private List<String> changeToUD(List<Integer> generatedValue) {
+        List<String> convertedValue = new ArrayList<>();
+        for (Integer integer : generatedValue) {
+            addChunkToConverted(convertedValue, integer);
+        }
+        return convertedValue;
+    }
+
+    private static void addChunkToConverted(List<String> convertedValue, Integer integer) {
+        if (integer == 1) {
+            convertedValue.add("U");
+        } else if (integer == 0) {
+            convertedValue.add("D");
+        }
+    }
+
 }
