22d64290f403f9655d799d429f81cbb41954123f
poter0501 <poter0501@gmail.com>
2022-11-21 08:49:02 +0900

    test(InputViewTest): InputViewTest 파일 생성

---
+++ src/test/java/bridge/InputViewTest.java
@@ -0,0 +1,12 @@
+package bridge;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class InputViewTest {
+
+    private static final InputViewTest INPUT_VIEW_TEST= new InputViewTest();
+
+}
