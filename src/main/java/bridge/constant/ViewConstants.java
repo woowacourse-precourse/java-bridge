6e17d169c70ea31e7b319000baaad0e20f098b9f
CAEL0 <kjongmin26@gmail.com>
2022-11-20 17:55:42 +0900

    refactor(ViewConstants): next move -> moving으로 수정

--- src/main/java/bridge/constant/ViewConstants.java
+++ src/main/java/bridge/constant/ViewConstants.java
@@ -3,7 +3,7 @@ package bridge.constant;
 public class ViewConstants {
     public static final String STARTING_GAME = "다리 건너기 게임을 시작합니다.";
     public static final String ASKING_BRIDGE_LENGTH = "다리의 길이를 입력해주세요.";
-    public static final String ASKING_NEXT_MOVE = "이동할 칸을 선택해주세요. (위: U, 아래: D)";
+    public static final String ASKING_MOVING = "이동할 칸을 선택해주세요. (위: U, 아래: D)";
     public static final String ASKING_RESTARTING_GAME = "게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)";
     public static final String GAME_RESULT = "최종 게임 결과";
     public static final String SUCCESS_OR_FAILURE = "게임 성공 여부: %s";
