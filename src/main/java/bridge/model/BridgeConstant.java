d214c3dbcb219e11164e900278f17a9c455fd957
HyeokJoon Kong <orijoon98@gmail.com>
2022-11-20 15:23:59 +0900

    feat: add BridgeConstant class

---
+++ src/main/java/bridge/model/BridgeConstant.java
@@ -0,0 +1,12 @@
+package bridge.model;
+
+public abstract class BridgeConstant {
+
+    public static final String ABLE = "O";
+    public static final String UNABLE = "X";
+    public static final String RESTART = "R";
+    public static final String QUIT = "Q";
+    public static final String BRIDGE_START = "[ ";
+    public static final String BRIDGE_END = " ]";
+    public static final String BRIDGE_SEPERATOR = " | ";
+}
