00777df76ecdad36135cc20bdf73ad6bc4a1925f
zcae1020 <zcae1020@naver.com>
2022-11-20 15:57:23 +0900

    feat: map을 출력하는 기능 구현

--- src/main/java/bridge/OutputView.java
+++ src/main/java/bridge/OutputView.java
@@ -25,14 +25,35 @@ public class OutputView {
 
     /**
      * 현재까지 이동한 다리의 상태를 정해진 형식에 맞춰 출력한다.
+     *
      * @param userRoute
      * @param bridge
      */
     public void printMap(List<String> userRoute, List<String> bridge) {
+        printFloor("U", userRoute, bridge);
+        printFloor("D", userRoute, bridge);
+    }
+
+    private void printFloor(String direction, List<String> userRoute, List<String> bridge) {
+        System.out.print("[");
+        for (int i = 0; i < userRoute.size(); i++) {
+            String OX = getOX(direction, userRoute.get(i), bridge.get(i));
+            System.out.print(" " + OX + " ");
+            if (i == userRoute.size() - 1) {
+                break;
+            }
+            System.out.print("|");
+        }
+        System.out.println("]");
+    }
+
+    private String getOX(String direction, String userDirection, String bridgeDirection) {
+        return null;
     }
 
     /**
      * 게임의 최종 결과를 정해진 형식에 맞춰 출력한다.
+     *
      * @param trial
      * @param bridge
      * @param userRoute
