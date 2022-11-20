2a13bfcfcfe5f3ecfef40d804eecf69c6d9c9aac
Your Name <you@example.com>
2022-11-20 23:57:13 +0900

    feat(ui) : 게임 최종 결과 출력 구현

--- src/main/java/bridge/ui/OutputView.java
+++ src/main/java/bridge/ui/OutputView.java
@@ -32,7 +32,11 @@ public class OutputView {
      * <p>
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void printResult() {
+    public void printResult(List<String> moveResult, boolean isFailFinalGame, int retryCount) {
+        System.out.println("최종 게임 결과");
+        printMap(moveResult);
+        printGameSuccessOrFail(isFailFinalGame);
+        System.out.println("총 시도한 횟수: " + retryCount);
     }
 
     /**
@@ -86,5 +90,16 @@ public class OutputView {
                            String format) {
         bridgeUpperResult.append(format);
         bridgeLowerResult.append(format);
+    }
+
+    /**
+     * 최종 게임 성공 여부를 정해진 형식에 맞춰 출력한다.
+     */
+    private void printGameSuccessOrFail(boolean isFailFinalGame) {
+        if (isFailFinalGame) {
+            System.out.println("게임 성공 여부: 실패");
+            return;
+        }
+        System.out.println("게임 성공 여부: 성공");
     }
 }
