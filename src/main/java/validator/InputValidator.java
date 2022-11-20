10494f528fcb192ea93871ce363cf04c18819149
hodomaroo <enghemdwkd@gmail.com>
2022-11-20 16:54:56 +0900

    [Feat/InputValidator] 다리 길이에 대한 유효성 검증 기능 구현
    다리 길이를 입력받아 3~20 범위 밖의 숫자가 들어오면 예외를 발생시키는
    validateBridgeLength() 메서드를 구현, 테스트 코드를 통해
    검증하였습니다.

--- src/main/java/validator/InputValidator.java
+++ src/main/java/validator/InputValidator.java
@@ -1,8 +1,19 @@
 package validator;
 
+import static model.BridgeGameExceptions.*;
+
+import java.util.Arrays;
+import model.Bridge;
+import model.BridgeGameExceptions;
+import model.MoveChoice;
+
 public class InputValidator {
 
     void validateBridgeLength(int bridgeLength) {
+        if (Bridge.LOWER_BOUND <= bridgeLength && bridgeLength <= Bridge.UPPER_BOUND) {
+            return;
+        }
+        throw new IllegalArgumentException(OUT_OF_RANGE);
     }
 
     void validateMoveChoice(String moving) {
