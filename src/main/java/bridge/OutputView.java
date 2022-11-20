2da19fa260c886e8edbbdfbb8af330ce9d1c26b1
이요환 <91039622+bumstead-bumstead@users.noreply.github.com>
2022-11-20 23:22:31 +0900

    <feat>(OutputView.java): printResult() 메서드 추가 및 기능 수정
     - 게임 결과를 출력하는 메서드 작성
     - printMap, printResult 메서드가 BridgeGame 객체롤 바로 인자로 받도록 변경
     - printMap() 메서드에 공백 줄 출력 기능 추가

--- src/main/java/bridge/OutputView.java
+++ src/main/java/bridge/OutputView.java
@@ -1,5 +1,6 @@
 package bridge;
 
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 import java.util.Map;
@@ -12,15 +13,19 @@ public class OutputView {
     private static final String START_MESSAGE = "다리 건너기 게임을 시작합니다.";
     private static final String MOVE_MESSAGE = "이동할 칸을 선택해주세요. (위: U, 아래: D)";
     private static final String RETRY_MESSAGE = "게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)";
+    private static final String FINAL_RESULT_MESSAGE = "최종 게임 결과";
+    private static final String WHETHER_SUCCESS_MESSAGE = "게임 성공 여부: ";
+    private static final String TRY_NUMBER_MESSAGE = "총 시도한 횟수: ";
 
     /**
      * 현재까지 이동한 다리의 상태를 정해진 형식에 맞춰 출력한다.
      * <p>
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void printMap(Map<String, List<Integer>> moveStatus) {
-        System.out.println(formatMoveStatus(moveStatus.get("U")));
-        System.out.println(formatMoveStatus(moveStatus.get("D")));
+    public void printMap(BridgeGame bridgeGame) {
+        System.out.println(formatMoveStatus(bridgeGame.getMoveStatus().get("U")));
+        System.out.println(formatMoveStatus(bridgeGame.getMoveStatus().get("D")));
+        System.out.println();
     }
 
     /**
@@ -28,7 +33,16 @@ public class OutputView {
      * <p>
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void printResult() {
+    public void printResult(BridgeGame bridgeGame) {
+        System.out.println(FINAL_RESULT_MESSAGE);
+        printMap(bridgeGame);
+        if (bridgeGame.hasSucceeded()) {
+            System.out.println(WHETHER_SUCCESS_MESSAGE + "성공");
+        }
+        if (!bridgeGame.hasSucceeded()) {
+            System.out.println(WHETHER_SUCCESS_MESSAGE + "실패");
+        }
+        System.out.println(TRY_NUMBER_MESSAGE + bridgeGame.getRestartNumber());
     }
 
     public void printStartMessage() {
@@ -43,10 +57,10 @@ public class OutputView {
         System.out.println(RETRY_MESSAGE);
     }
 
-    private String formatMoveStatus(List<Integer> statusList) {
+    private String formatMoveStatus(List<String> statusList) {
         StringBuilder result = new StringBuilder();
         result.append("[ ");
-        for (int status : statusList) {
+        for (String status : statusList) {
             result.append(status).append(" | ");
         }
         result.replace(result.length() - 2, result.length(), "]");
