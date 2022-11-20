cb26fe42eba5745bcce775b88a60054b9192df4e
wltks98 <wltks98@hanmail.net>
2022-11-20 15:49:50 +0900

    fix : 잘못된 에러 메세지 수정

--- src/main/java/bridge/validator/GameCommandValidator.java
+++ src/main/java/bridge/validator/GameCommandValidator.java
@@ -7,7 +7,7 @@ public class GameCommandValidator {
 
 	public static String validate(String board) {
 		if (!board.equals(GameRule.RETRY_COMMAND) && !board.equals(GameRule.QUIT_COMMAND)) {
-			throw new IllegalArgumentException(ErrorMessage.BRIDGE_BOARD_ERROR.getMessage());
+			throw new IllegalArgumentException(ErrorMessage.GAME_COMMAND_ERROR.getMessage());
 		}
 		return board;
 	}
