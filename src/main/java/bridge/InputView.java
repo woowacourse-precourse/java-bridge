58410ef94be998212ed14c28ee6812dd3349f366
Dabeen Jeong <hello70825@gmail.com>
2022-11-20 19:45:10 +0900

    feat: 입력값을 받을 수 있는 기능 추가

--- src/main/java/bridge/InputView.java
+++ src/main/java/bridge/InputView.java
@@ -1,28 +1,39 @@
 package bridge;
 
+import camp.nextstep.edu.missionutils.Console;
+
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
  */
 public class InputView {
+    public static final String READ_BRIDGE_SIZE_MESSAGE = "다리의 길이를 입력해주세요.";
+    public static final String READ_MOVING_MESSAGE = "이동할 칸을 선택해주세요. (위: U, 아래: D)";
+    public static final String READ_GAME_COMMAND_MESSAGE = "게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)";
 
     /**
      * 다리의 길이를 입력받는다.
      */
     public int readBridgeSize() {
-        return 0;
+        System.out.println(READ_BRIDGE_SIZE_MESSAGE);
+        int inputValue = Integer.parseInt(Console.readLine());
+        return inputValue;
     }
 
     /**
      * 사용자가 이동할 칸을 입력받는다.
      */
     public String readMoving() {
-        return null;
+        System.out.println(READ_MOVING_MESSAGE);
+        String inputValue = Console.readLine();
+        return inputValue;
     }
 
     /**
      * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
      */
     public String readGameCommand() {
-        return null;
+        System.out.println(READ_GAME_COMMAND_MESSAGE);
+        String inputValue = Console.readLine();
+        return inputValue;
     }
 }
