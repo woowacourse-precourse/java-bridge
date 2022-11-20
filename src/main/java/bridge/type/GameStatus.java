5e2f9bcc1595980c56bc94937149c066f1745e5e
jinhyeon <vmflwu123@gmail.com>
2022-11-20 22:21:35 +0900

    feat(GameStatus): getGameStatus 메서드 추가
    문자열 입력값을 해당하는 GameStatus값으로 반환하는 메서드

--- src/main/java/bridge/type/GameStatus.java
+++ src/main/java/bridge/type/GameStatus.java
@@ -1,10 +1,23 @@
 package bridge.type;
 
-public enum GameStatus {
+public enum GameStatus implements ProcessCondition {
 
-    START,
-    ONGOING,
-    RESTART,
-    QUIT;
+    START("S"),
+    ONGOING("O"),
+    RESTART("R"),
+    QUIT("Q");
 
+    private final String statusSymbol;
+
+    GameStatus(String statusSymbol) {
+        this.statusSymbol = statusSymbol;
+    }
+
+    public static GameStatus getGameStatus(String statusSymbol) {
+        if (statusSymbol.equals(START.statusSymbol)) return START;
+        if (statusSymbol.equals(ONGOING.statusSymbol)) return ONGOING;
+        if (statusSymbol.equals(RESTART.statusSymbol)) return RESTART;
+        if (statusSymbol.equals(QUIT.statusSymbol)) return QUIT;
+        return null;
+    }
 }
