968cfdb579984182185779ff05e318f8357a0555
DevJay1024 <devjay1024@gmail.com>
2022-11-20 18:33:46 +0900

    test: 주어진 다리 길이만큼 안전경로를 만드는 기능의 테스트 케이스 작성

---
+++ src/test/java/bridge/BridgeMakerTest.java
@@ -0,0 +1,22 @@
+package bridge;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.util.Lists.newArrayList;
+
+import bridge.ApplicationTest.TestNumberGenerator;
+import java.util.List;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class BridgeMakerTest {
+
+    @Test
+    @DisplayName("안전 경로는 생성된다.")
+    void 안전경로_생성_테스트() {
+        BridgeNumberGenerator numberGenerator = new TestNumberGenerator(newArrayList(1, 0, 0));
+        BridgeMaker bridgeMaker = new BridgeMaker(numberGenerator);
+        List<String> bridge = bridgeMaker.makeBridge(3);
+        assertThat(bridge).containsExactly("U", "D", "D");
+    }
+
+}
