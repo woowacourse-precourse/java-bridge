6fd341b11313d1e13f4f57fb636f1d91956c5722
miffyKing <whqudghk28@gmail.com>
2022-11-20 16:15:47 +0900

    feat: Make util package's alphabet parser that check if input is all consist of alphabets

---
+++ src/main/java/bridge/utils/BasicBridgeInputAlphabetParser.java
@@ -0,0 +1,27 @@
+package bridge.utils;
+
+import bridge.model.BridgeGame;
+
+public class BasicBridgeInputAlphabetParser {
+
+
+    private BasicBridgeInputAlphabetParser() {
+
+    }
+
+    public static BridgeGame parseBridgeGameInput(final String input, int mode) {
+        return parseWithCheckingEmpty(input, mode);
+    }
+
+    private static BridgeGame parseWithCheckingEmpty(final String input, int mode) {
+        EmptyChecker.check(input);
+        return parse(input, mode);
+    }
+
+    private static BridgeGame parse(final String input, int mode) {
+        if (input.length() != 1 || !(input.charAt(0) >= 'A' && input.charAt(0) <= 'Z')) {
+            throw new IllegalArgumentException("input Alphabet is not Valid");
+        }
+        return BridgeGame.from(input, mode);
+    }
+}
