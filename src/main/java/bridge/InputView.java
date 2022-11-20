83f5513c901b277f91a2aaf8f89b34e43a419e40
dsaf2007 <dsaf2007@naver.com>
2022-11-20 20:54:36 +0900

    feat : 다리의 길이와 위/아래 이동 명령 입력 받는 기능 추가 길이 입력이 숫자가 아닐경우 예외처리 위 아래 이동 명령 입력 시 U 나 D 이외의 입력일 경우 예외처리 발생

--- src/main/java/bridge/InputView.java
+++ src/main/java/bridge/InputView.java
@@ -1,22 +1,42 @@
 package bridge;
 
+import static org.junit.jupiter.api.Assertions.fail;
+import java.util.regex.Pattern;
+import camp.nextstep.edu.missionutils.Console;
+
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
  */
 public class InputView {
-
+    
+    private static String SIZE_RANGE_ERROR = "[ERROR] size out of range";
+    private static String INPUT_NOT_NUMBER_ERROR = "[ERROR] input is not number";
+    private static String INPUT_IS_U_OR_D_ERROR = "[ERROR] input must be U or D";
     /**
      * 다리의 길이를 입력받는다.
      */
     public int readBridgeSize() {
-        return 0;
+        String input = Console.readLine();
+        if(checkInputisNumber(input) == false){
+            throw new IllegalArgumentException();
+        }
+        int size = Integer.parseInt(input);
+        if(checkBridgeSize(size) == false){
+            throw new IllegalArgumentException();
+        }
+        return size;
     }
 
     /**
      * 사용자가 이동할 칸을 입력받는다.
      */
     public String readMoving() {
-        return null;
+        String input = Console.readLine();
+        if(checkInputisUpDown(input) == false)
+        {
+            throw new IllegalArgumentException();
+        }
+        return input;
     }
 
     /**
@@ -24,5 +44,37 @@ public class InputView {
      */
     public String readGameCommand() {
         return null;
+    }
+
+    private boolean checkBridgeSize(int size)
+    {
+        if(size < 3 || size > 20)
+        {
+            System.out.println(SIZE_RANGE_ERROR);
+            return false;
+            //throw new IllegalArgumentException();
+        }
+        return true;
+    }
+
+    private boolean checkInputisNumber(String input)
+    {
+        String pattern = "^[0-9]+$";
+        if (!Pattern.matches(pattern, input)) {
+            System.out.println(INPUT_NOT_NUMBER_ERROR);
+            // throw new IllegalArgumentException(INPUT_NOT_NJMBER);
+            return false;
+        }
+        return true;
+    }
+
+    private boolean checkInputisUpDown(String input)
+    {
+        if(input.equals("U") || input.equals("D"))
+        {
+            System.out.println(INPUT_IS_U_OR_D_ERROR);
+            return true;
+        }
+        return false;
     }
 }
