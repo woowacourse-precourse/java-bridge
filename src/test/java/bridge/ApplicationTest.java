db84392680d226a17d22bfcb4714afd03214ef7d
Choi-JY <Choi-JY1107@github.com>
2022-11-21 01:55:48 +0900

    test: 재시작 여부 입력 시 예외 검증 테스트

--- src/test/java/bridge/ApplicationTest.java
+++ src/test/java/bridge/ApplicationTest.java
@@ -93,6 +93,44 @@ class ApplicationTest extends NsTest {
         }, 1, 0, 1);
     }
 
+    @Test
+    void 재시작_여부_예외_후_성공_테스트() {
+        assertRandomNumberInRangeTest(() -> {
+            run("3", "D", "TT", "R", "U", "D", "U");
+            assertThat(output()).contains(
+                "최종 게임 결과",
+                "[ O |   | O ]",
+                "[   | O |   ]",
+                "게임 성공 여부: 성공",
+                "총 시도한 횟수: 2"
+            );
+
+            int upSideIndex = output().indexOf("[ O |   | O ]");
+            int downSideIndex = output().indexOf("[   | O |   ]");
+            assertThat(upSideIndex).isLessThan(downSideIndex);
+        }, 1, 0, 1);
+    }
+
+    @Test
+    void 재시작_여부_예외_후_실패_테스트() {
+        assertRandomNumberInRangeTest(() -> {
+            run("3", "D", "TT", "R", "U", "D", "D", "Q");
+            assertThat(output()).contains(
+                "최종 게임 결과",
+                "[ O |   |   ]",
+                "[   | O | X ]",
+                "게임 성공 여부: 실패",
+                "총 시도한 횟수: 2"
+            );
+
+            int upSideIndex = output().indexOf("[ O |   |   ]");
+            int downSideIndex = output().indexOf("[   | O | X ]");
+            assertThat(upSideIndex).isLessThan(downSideIndex);
+        }, 1, 0, 1);
+    }
+
+
+
     @Override
     protected void runMain() {
         Application.main(new String[] {});
