2fd4d28baef3df217ad3ed25d73f2a6f9a53a420
CAEL0 <kjongmin26@gmail.com>
2022-11-20 17:46:17 +0900

    feat(BridgeConstants): 입력 문자 관련 상수 추가

--- src/main/java/bridge/constant/BridgeConstants.java
+++ src/main/java/bridge/constant/BridgeConstants.java
@@ -3,4 +3,10 @@ package bridge.constant;
 public class BridgeConstants {
     public static final int MINIMUM_LENGTH = 3;
     public static final int MAXIMUM_LENGTH = 20;
+
+    public static final String DOWN = "D";
+    public static final String UP = "U";
+
+    public static final String RESTART = "R";
+    public static final String QUIT = "Q";
 }
