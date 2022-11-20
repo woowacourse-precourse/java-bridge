9937b1595098598e8fb72b57f4d9e9d5b99913e0
Hyun-Seo Oh <ohseo8813@gmail.com>
2022-11-20 16:57:16 +0900

    <test>(BridgeMakerTest.java): 들어온 값에 따라 정확하게 반환하는지 테스트

--- src/test/java/bridge/BridgeMakerTest.java
+++ src/test/java/bridge/BridgeMakerTest.java
@@ -3,9 +3,12 @@ package bridge;
 import static bridge.domain.command.MoveCommand.MOVE_TO_LOWER_BLOCK;
 import static bridge.domain.command.MoveCommand.MOVE_TO_UPPER_BLOCK;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import java.util.List;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
@@ -28,5 +31,40 @@ class BridgeMakerTest {
         List<String> bridge = bridgeMaker.makeBridge(anyValue);
         assertThat(bridge.stream()
                 .allMatch(value -> value.equals(MOVE_TO_UPPER_BLOCK) || value.equals(MOVE_TO_LOWER_BLOCK))).isTrue();
+    }
+
+    @DisplayName("numberGenerator를 통해 들어온 값이")
+    @Nested
+    class ChangeNumber {
+
+        @DisplayName("1이면 " + MOVE_TO_UPPER_BLOCK + "를 반환한다.")
+        @Test
+        void test1() {
+            BridgeMaker bridgeMaker = new BridgeMaker(() -> 1);
+            boolean result = isCreateSpecificValue(bridgeMaker, MOVE_TO_UPPER_BLOCK);
+            assertThat(result).isTrue();
+        }
+
+        @DisplayName("0이면 " + MOVE_TO_LOWER_BLOCK + "를 반환한다.")
+        @Test
+        void test2() {
+            BridgeMaker bridgeMaker = new BridgeMaker(() -> 0);
+            boolean result = isCreateSpecificValue(bridgeMaker, MOVE_TO_LOWER_BLOCK);
+            assertThat(result).isTrue();
+        }
+
+        @DisplayName("그외의 값이면 예외를 반환한다.")
+        @Test
+        void test3() {
+            BridgeMaker bridgeMaker = new BridgeMaker(() -> 2);
+            assertThatThrownBy(() -> bridgeMaker.makeBridge(10))
+                    .isInstanceOf(IllegalStateException.class);
+        }
+
+        private boolean isCreateSpecificValue(BridgeMaker bridgeMaker, String moveToUpperBlock) {
+            List<String> bridge = bridgeMaker.makeBridge(10);
+            return bridge.stream()
+                    .allMatch(value -> value.equals(moveToUpperBlock));
+        }
     }
 }
