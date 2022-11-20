16b86d5193bdcf12c3038a04d30bd91e3018af12
Haechan Lee <dlgockssla@gmail.com>
2022-11-20 23:00:55 +0900

    feat(Command): create Command class
    define action between user input and bridge game commands

---
+++ src/main/java/bridge/Command.java
@@ -0,0 +1,37 @@
+package bridge;
+
+import java.util.function.Function;
+
+public enum Command {
+    RETRY((BridgeGame bridgeGame)-> {
+        bridgeGame.retry();
+        return null;
+    }, "R"),
+    QUIT((BridgeGame bridgeGame)-> {
+        bridgeGame.setGiveUp(true);
+        return null;
+    }, "Q");
+
+    private final Function<BridgeGame, Void> expression;
+    private final String symbol;
+
+    Command(Function<BridgeGame, Void> expression, String symbol) {
+        this.expression = expression;
+        this.symbol = symbol;
+    }
+
+    public static Command fromSymbol(String symbol) {
+        for (Command command : values()) {
+            if (command.symbol.equals(symbol)){
+                return command;
+            }
+        }
+
+        throw new IllegalArgumentException("[ERROR] 올바르지 않은 명령어입니다.");
+    }
+
+    public void exec(BridgeGame bridgeGame){
+        this.expression.apply(bridgeGame);
+    }
+
+}
