a285dba7d7e7b3f519f3c63c5936f9153842df68
khj1 <dhepd8849@gmail.com>
2022-11-20 14:16:53 +0900

    feat: 게임 성공 여부를 출력하는 기능 추가

--- src/main/java/bridge/OutputView.java
+++ src/main/java/bridge/OutputView.java
@@ -42,7 +42,15 @@ public class OutputView {
      * <p>
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void printResult() {
+    public void printResult(MovingHistory movingHistory, BridgeGame bridgeGame, MovingResult movingResult) {
+        System.out.println(GuidanceMessage.GAME_OUTRO);
+        printMap(movingHistory);
+        printGameResult(bridgeGame, movingResult);
+    }
+
+    private void printGameResult(BridgeGame bridgeGame, MovingResult movingResult) {
+        System.out.print(GuidanceMessage.INFORM_IS_SUCCESS);
+        System.out.println(bridgeGame.getGameResult(movingResult));
     }
 
     public void printError(IllegalArgumentException error) {
