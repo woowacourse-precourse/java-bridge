da5c6e5e8d33b4a86e5b98fe6823561ac1560c61
UJ15 <dbwogml15@naver.com>
2022-11-20 18:09:58 +0900

    feat : create PassOrFail class

---
+++ src/main/java/bridge/model/PassOrFail.java
@@ -0,0 +1,14 @@
+package bridge.model;
+
+public class PassOrFail {
+
+    private final boolean value;
+
+    public PassOrFail(boolean value) {
+        this.value = value;
+    }
+
+    public boolean getValue() {
+        return value;
+    }
+}
