3e2eb69f37c305e59d2f3437f637d268cf9e31ee
areyouhun <areyouhun@gmail.com>
2022-11-20 22:32:00 +0900

    test(bridge): 이동할 칸과 정답이 일치하면 O를 반환하고 일치하지 않으면 X를 반환하는지 테스트

---
+++ src/test/java/bridge/BridgeGameTest.java
@@ -0,0 +1,25 @@
+package bridge;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+class BridgeGameTest {
+    private final BridgeGame bridgeGame = new BridgeGame();
+
+    @DisplayName("이동할 칸을 건널 수 있으면 \"O\"를 반환")
+    @ParameterizedTest
+    @CsvSource(value = {"U:U:O", "D:D:O"}, delimiter = ':')
+    void correctMoving(String answer, String playerMoving, String expected) {
+        assertThat(bridgeGame.move(answer, playerMoving)).isEqualTo(expected);
+    }
+
+    @DisplayName("이동할 칸을 건널 수 없으면 \"X\"를 반환")
+    @ParameterizedTest
+    @CsvSource(value = {"U:D:X", "D:U:X"}, delimiter = ':')
+    void incorrectMoving(String answer, String playerMoving, String expected) {
+        assertThat(bridgeGame.move(answer, playerMoving)).isEqualTo(expected);
+    }
+}
