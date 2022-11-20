2605f8a514897e75727b1aa67cadb2bf87b8476b
62hoon99 <98267772+62hoon99@users.noreply.github.com>
2022-11-20 10:59:51 +0900

    refactor: 테스트 메서드 10줄 이하로 수정

--- src/test/java/bridge/controller/BridgeGameControllerTest.java
+++ src/test/java/bridge/controller/BridgeGameControllerTest.java
@@ -14,6 +14,8 @@ import static org.assertj.core.api.Assertions.*;
 public class BridgeGameControllerTest {
 
     private static final BridgeMaker bridgeMaker= new BridgeMaker(new BridgeRandomNumberGenerator());
+    private static final BridgeGame bridgeGame = new BridgeGame(bridgeMaker.makeBridge(10));
+
     @ValueSource(ints = {3,10,18})
     @ParameterizedTest
     void 다리_길이_테스트(int size){
@@ -26,11 +28,9 @@ public class BridgeGameControllerTest {
     @Test
     void 다리가_U_D_로만_이루어져_있는지_테스트(){
         //given
-        BridgeGame bridgeGame = new BridgeGame(bridgeMaker.makeBridge(10));
         List<String> bridge = bridgeGame.getBridge();
         //when
-        List<String> collect = bridge.stream()
-                .filter(mark -> mark.equals("U") || mark.equals("D"))
+        List<String> collect = bridge.stream().filter(mark -> mark.equals("U") || mark.equals("D"))
                 .collect(Collectors.toList());
         //then
         assertThat(collect).isEqualTo(bridge);
@@ -40,7 +40,6 @@ public class BridgeGameControllerTest {
     @ParameterizedTest
     void 플레이어가_이동한_칸이_건널_수_있는지_확인하는_기능_테스트(int index){
         //given
-        BridgeGame bridgeGame = new BridgeGame(bridgeMaker.makeBridge(10));
         List<String> bridge = bridgeGame.getBridge();
         //when
         String mark = bridge.get(index);
