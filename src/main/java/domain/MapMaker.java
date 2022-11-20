bd8207a5ac348d4516a86c01c2e122306f8a9fdc
UngHeung <t01047377477@gmail.com>
2022-11-20 21:56:32 +0900

    feat : (MapMaker) 사용자 입력값으로 지도를 만드는 클래스 추가

---
+++ src/main/java/domain/MapMaker.java
@@ -0,0 +1,21 @@
+package domain;
+
+import java.util.List;
+
+import service.InputView;
+import service.OutputView;
+
+public class MapMaker {
+	private InputView in = new InputView();
+	private OutputView out = new OutputView();
+	
+	private final List<String> bridge;
+	private StringBuilder upperBridge, lowerBridge;
+	
+	public MapMaker(List<String> bridge) {
+		this.bridge = bridge;
+		upperBridge = new StringBuilder();
+		lowerBridge = new StringBuilder();
+		
+	}
+}
