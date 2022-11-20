3b26d09f97aa9212538f6c348fa8a1ab38dfafa4
egoing <dudqk9696@naver.com>
2022-11-20 16:08:57 +0900

    feat : 결과 메시지 enum 클래스 작성

---
+++ src/main/java/view/OutputMessage.java
@@ -0,0 +1,18 @@
+package view;
+
+public enum OutputMessage {
+    GAME_RESULT("최종 게임 결과"),
+    WIN_GAME("게임 성공 여부: 성공"),
+    LOSE_GAME("게임 성공 여부: 실패"),
+    TRY("총 시도한 횟수: ");
+
+    private final String outputMsg;
+
+    OutputMessage(String outputMsg) {
+        this.outputMsg = outputMsg;
+    }
+
+    public String getOutputMsg() {
+        return outputMsg;
+    }
+}
