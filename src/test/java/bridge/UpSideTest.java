94c9a18514eb283f162824e8208c06563e6a413d
hojunking96 <junsong96@naver.com>
2022-11-21 00:08:55 +0900

    test(UpSideTest.java) : 테스트 코드 수정

--- src/test/java/bridge/UpSideTest.java
+++ src/test/java/bridge/UpSideTest.java
@@ -12,16 +12,9 @@ class UpSideTest {
     @DisplayName("정답 시 다리 윗부분을 제대로 생성하는 지 확인한다.")
     @Test
     void UpSide_생성_테스트() {
-        Application.movingInputs = Arrays.asList("U", "D", "U");
         UpSide upSide = new UpSide();
+        upSide.upSideArr = Arrays.asList("O", " ", "O");
+        upSide.makeUpSide();
         assertEquals(upSide.getUpSide(), "[ O |   | O ]");
-    }
-
-    @DisplayName("정답이 아닐 시 다리 윗부분을 제대로 생성하는 지 확인한다.")
-    @Test
-    void UpSide_생성_테스트2() {
-        Application.movingInputs = Arrays.asList("U", "D", "X");
-        UpSide upSide = new UpSide();
-        assertEquals(upSide.getUpSide(), "[ O |   | X ]");
     }
 }
