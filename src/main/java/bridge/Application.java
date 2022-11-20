0e612fff15d67897946172ea7012b30cfbfe60a5
wltks98 <wltks98@hanmail.net>
2022-11-20 15:29:07 +0900

    refactor : 필요 없는 예외처리 제거

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -7,11 +7,6 @@ public class Application {
 
     public static void main(String[] args) {
         GameController gameController = new GameController();
-
-        try {
-            gameController.play();
-        } catch (IllegalArgumentException e){
-            System.out.println(e.getMessage());
-        }
+        gameController.play();
     }
 }
