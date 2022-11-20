b69c9d85a44f483b92810b207ebdbe6393a6518f
황민주 <minjoo@hwangminjooui-MacBookAir.local>
2022-11-20 14:50:09 +0900

    feat(OutputView): printResult (src/main/java/bridge/view/OutputView.java)
    printResult 메소드 추가:
    -게임의 최종 결과를 출력하기 위해 추가함

--- src/main/java/bridge/view/OutputView.java
+++ src/main/java/bridge/view/OutputView.java
@@ -11,8 +11,14 @@ import static bridge.Utility.MOVE_NAME;
  */
 public class OutputView {
 
+    public static final String gameStartMessage = "다리 건너기 게임을 시작합니다.";
+    public static final String inputBridgeLengthMessage = "다리의 길이를 입력해주세요.";
+    public static final String selectUpDownMessage = "이동할 칸을 선택해주세요. (위: U, 아래: D)";
+    public static final String reGameOrNotMessage = "게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)";
     public static final String finalResultMessage = "최종 게임 결과";
     public static final String gameResultMessage = "게임 성공 여부: ";
+    public static final String successMessage = "성공";
+    public static final String failMessage = "실패";
     public static final String totalTriesMessage = "총 시도한 횟수: ";
 
     public static final String mapStart = "[ ";
@@ -21,6 +27,7 @@ public class OutputView {
     public static final String mapRight = "O";
     public static final String mapWrong = "X";
     public static final String mapBlank = " ";
+
     public void printMessage(String message){
         System.out.println(message);
     }
@@ -44,7 +51,16 @@ public class OutputView {
      * <p>
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
+    public void printResult(List <String> totalMove, List<String> bridge, String gameResult, int cntTries) {
+        OutputView outputView = new OutputView();
 
+        outputView.printMessage(finalResultMessage);
+        printMap(totalMove, bridge);
+        String resultMessage = gameResultMessage+gameResult;
+        String triesMessage = totalTriesMessage+cntTries;
+        outputView.printMessage(resultMessage);
+        outputView.printMessage(triesMessage);
+    }
 
     public String createMapByLine(List<String> totalMove, List <String> bridge, String type){
         String mapResult = "";
