711cbc4da15e1d553f8037c7e966ca4b6939d7ad
TaeMin <xoals3094@naver.com>
2022-11-20 14:30:03 +0900

    test(UserChoice) : 인스턴스 생성 유효값 체크 테스트 추가

---
+++ src/test/java/bridge/UserChoiceTest.java
@@ -0,0 +1,24 @@
+package bridge;
+
+import bridge.Model.UserChoice;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+import static org.assertj.core.api.Assertions.*;
+
+public class UserChoiceTest {
+    @ParameterizedTest
+    @ValueSource(strings = { "U", "D" })
+    @DisplayName("유효")
+    void valid(String choice){
+        UserChoice userChoice = new UserChoice(choice);
+        assertThat(userChoice).isInstanceOf(UserChoice.class);
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = { "U12", "D3", "2", "안녕하세요" })
+    @DisplayName("예외")
+    void notValid(String choice){
+        assertThatThrownBy(() -> { new UserChoice(choice); }).isInstanceOf(IllegalArgumentException.class);
+    }
+}
