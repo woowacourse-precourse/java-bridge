6ab68eb3c44c0ca5bd8787777e97464dd1965e1e
Sanmgmin <sangmins930@gmail.com>
2022-11-20 16:55:24 +0900

    feat(InputValidator): implement validateRetry

--- src/main/java/bridge/InputValidator.java
+++ src/main/java/bridge/InputValidator.java
@@ -3,8 +3,11 @@ package bridge;
 public class InputValidator {
     private static final String ERROR_FOR_LENGTH_OF_BRIDGE = "[ERROR] 다리의 길이는 3이상 20이하의 숫자만 가능합니다.";
     private static final String ERROR_FOR_UP_OR_DOWN = "[ERROR] 위: \"U\", 아래: \"D\"로만 가능합니다.";
+    private static final String ERROR_FOR_RETRY_OR_QUIT = "[ERROR] 재시도: \"R\", 종료: \"Q\"로만 가능합니다";
     private static final String UP = "U";
     private static final String DOWN = "D";
+    private static final String RETRY = "R";
+    private static final String QUIT = "Q";
     public static int validateNumeric(String input) {
         try {
             return Integer.parseInt(input);
@@ -24,4 +27,11 @@ public class InputValidator {
             throw new IllegalArgumentException(ERROR_FOR_UP_OR_DOWN);
         }
     }
+
+    public static void validateRetry(String input){
+        if (!(input.equals(RETRY) || input.equals(QUIT))){
+            throw new IllegalArgumentException(ERROR_FOR_RETRY_OR_QUIT);
+        }
+    }
+
 }
