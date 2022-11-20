bdf6d2fb0b07b868de2171ce3a966ec50ee75dd2
namtndus <rnrmf0721@gmail.com>
2022-11-20 11:50:28 +0900

    feat(OutputView): printGameStartMessage함수 기능 구현
    ''다리 건너기 게임을 시작합니다.'를 출력'를 구현

--- src/main/java/bridge/OutputView.java
+++ src/main/java/bridge/OutputView.java
@@ -7,6 +7,10 @@ import java.util.List;
  */
 public class OutputView {
 
+    public void printGameStartMessage(){
+        System.out.println("다리 건너기 게임을 시작합니다.\n");
+    }
+
     /**
      * 현재까지 이동한 다리의 상태를 정해진 형식에 맞춰 출력한다.
      * <p>
