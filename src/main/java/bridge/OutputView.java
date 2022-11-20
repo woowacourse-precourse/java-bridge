d5ea644bf1ddee07626942c6c23b1ab0f2aeeec0
ensk26 <tjensk26@gmail.com>
2022-11-21 02:14:49 +0900

    feat(OutputView): 게임 최종 결과 출력 메서드
    새로운 메서드 추가 to OutputView
    - 게임 최종 결과 출력
    - result는 성공, 실패
    - count 총 시도 횟수

--- src/main/java/bridge/OutputView.java
+++ src/main/java/bridge/OutputView.java
@@ -1,5 +1,7 @@
 package bridge;
 
+import static bridge.OutputType.PRINT_RESULT;
+
 /**
  * 사용자에게 게임 진행 상황과 결과를 출력하는 역할을 한다.
  */
@@ -18,6 +20,7 @@ public class OutputView {
      * <p>
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void printResult() {
+    public void printResult(String result, int count) {
+        System.out.println(String.format(PRINT_RESULT.getMessage(), result, count));
     }
 }
