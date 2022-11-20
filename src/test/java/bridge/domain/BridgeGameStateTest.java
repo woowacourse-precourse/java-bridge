404bf46c611b468b63fa9a581bdd30d65de4ba89
hyunseo <goatsfish@gmail.com>
2022-11-21 07:13:37 +0900

    test : 다리 게임이 진행되는지 확인했을 때 진행 실패 테스트

--- src/test/java/bridge/domain/BridgeGameStateTest.java
+++ src/test/java/bridge/domain/BridgeGameStateTest.java
@@ -7,8 +7,7 @@ import org.junit.jupiter.params.provider.MethodSource;
 
 import java.util.stream.Stream;
 
-import static bridge.domain.BridgeGameState.CONTINUE;
-import static bridge.domain.BridgeGameState.START;
+import static bridge.domain.BridgeGameState.*;
 import static org.assertj.core.api.Assertions.assertThat;
 
 class BridgeGameStateTest {
@@ -17,17 +16,35 @@ class BridgeGameStateTest {
     @MethodSource("whenCheckBridgeGameContinueThenSuccessDummy")
     @DisplayName("다리 게임이 계속 진행되는지 확인했을 때 진행 성공 테스트")
     void whenCheckBridgeGameContinueThenSuccessTest(BridgeGameState gameState) {
-        // when
+        // given & when
         boolean isBridgeGameContinue = gameState.isBridgeGameContinue();
 
         // then
         assertThat(isBridgeGameContinue).isTrue();
     }
 
+    @ParameterizedTest(name = "[{index}] gameState = {0}")
+    @MethodSource("whenCheckBridgeGameContinueThenFailDummy")
+    @DisplayName("다리 게임이 계속 진행되는지 확인했을 때 진행 실패 테스트")
+    void whenCheckBridgeGameContinueThenFailTest(BridgeGameState gameState) {
+        // given & when
+        boolean isBridgeGameContinue = gameState.isBridgeGameContinue();
+
+        // then
+        assertThat(isBridgeGameContinue).isFalse();
+    }
+
     static Stream<Arguments> whenCheckBridgeGameContinueThenSuccessDummy() {
         return Stream.of(
                 Arguments.arguments(START),
                 Arguments.arguments(CONTINUE)
+        );
+    }
+
+    static Stream<Arguments> whenCheckBridgeGameContinueThenFailDummy() {
+        return Stream.of(
+                Arguments.arguments(SUCCESS),
+                Arguments.arguments(FAIL)
         );
     }
 }
