61ae8ebfdbdc5fd128eef141e69ddccc9652bfce
UngHeung <t01047377477@gmail.com>
2022-11-21 05:49:30 +0900

    feat : (GameMessage) 최종 결과 변수 추가

--- src/main/java/domain/GameMessage.java
+++ src/main/java/domain/GameMessage.java
@@ -4,14 +4,15 @@ public enum GameMessage {
 	START("다리 건너기 게임을 시작합니다."),
 	BRIDGE_SIZE("다리의 길이를 입력해주세요."),
 	MOVING("이동할 칸을 선택해주세요. (위: U, 아래: D)"),
-	COMMAND("게임을 다시 시도할지 여부를 입력해주세요.(재시도: R, 종료: Q)");
-	
+	COMMAND("게임을 다시 시도할지 여부를 입력해주세요.(재시도: R, 종료: Q)"),
+	RESULT("최종 게임 결과");
+
 	private final String gameMessage;
-	
+
 	private GameMessage(String gameMessage) {
 		this.gameMessage = gameMessage;
 	}
-	
+
 	public String getGameMessage() {
 		return gameMessage;
 	}
