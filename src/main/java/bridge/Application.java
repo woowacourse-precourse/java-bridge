17467cd5445a63e875503a7944dc9388dc361383
inhwanK <79566221+inhwanK@users.noreply.github.com>
2022-11-20 23:38:12 +0900

    feat(BridgeMaker): 다리를 만들기 구현
    makeBridge 메서드 구현
    다리를 만들 객체 maker 필드 추가
    만든 다리를 저장할 bridge 필드 추가

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -3,17 +3,25 @@ package bridge;
 import bridge.view.InputView;
 import bridge.view.OutputView;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Application {
     private static InputView input = new InputView();
     private static OutputView output = new OutputView();
-    private static int bridgeSize;
+    private static BridgeMaker maker = new BridgeMaker(new BridgeRandomNumberGenerator());
+    private static BridgeGame game = new BridgeGame();
+    private static List<String> bridge = new ArrayList<>();
+
 
     public static void main(String[] args) {
         try {
             // 게임 시작
             output.printStartMessage();
             output.printBridgeLengthInputRequestMessage();
-            bridgeSize = input.readBridgeSize();
+            int bridgeSize = input.readBridgeSize();
+            bridge = maker.makeBridge(bridgeSize); // 다리 만들기
+//            System.out.println(bridge.toString());
 
             System.out.println(bridgeSize);
 
