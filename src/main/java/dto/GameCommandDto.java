d74000cbb2608d8d2d2ecad363fd61b3fbdaade3
Devtist <yt930310@snu.ac.kr>
2022-11-21 02:29:39 +0900

    feat: dto 패턴 구현을 위해 재시작 여부를 전달하는 GameCommandDto 구현

---
+++ src/main/java/dto/GameCommandDto.java
@@ -0,0 +1,13 @@
+package dto;
+
+public class GameCommandDto {
+	private final String gameCommand;
+
+	public GameCommandDto(String gameCommand) {
+		this.gameCommand = gameCommand;
+	}
+
+	public String getGameCommand() {
+		return gameCommand;
+	}
+}
