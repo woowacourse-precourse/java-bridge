6a07e7377e3938c3b5eba0f90e9e4aea23bb72c5
dlrghks2090 <dlrghks2090@gmail.com>
2022-11-20 22:10:28 +0900

    feat: 예외 처리 추가

--- src/main/java/bridge/validate/Exception.java
+++ src/main/java/bridge/validate/Exception.java
@@ -4,5 +4,21 @@ import bridge.ErrorMessage;
 
 public class Exception {
 
+    private void validateThreeToTwenty(int bridgeCount) {
+        if (bridgeCount < 3 || bridgeCount > 20) {
+            throw new IllegalArgumentException(ErrorMessage.BRIDGE_LENGTH_IS_THREE_TO_TWENTY.getErrorMessage());
+        }
+    }
 
+    private void validateUpOrDown(String moveBridgeSpace) {
+        if (!moveBridgeSpace.equals("U") && !moveBridgeSpace.equals("D")) {
+            throw new IllegalArgumentException(ErrorMessage.MOVE_BRIDGE_SPACE_IS_U_OR_D.getErrorMessage());
+        }
+    }
+
+    private void validateRetryOrQuit(String retryAnswer) {
+        if (!retryAnswer.equals("R") && !retryAnswer.equals("Q")) {
+            throw new IllegalArgumentException(ErrorMessage.RETRY_ANSWER_IS_R_OR_Q.getErrorMessage());
+        }
+    }
 }
