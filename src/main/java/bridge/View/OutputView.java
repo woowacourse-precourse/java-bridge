871e06e66ec67045c8691c77850cad44ae8cc064
jinju9553 <69393506+jinju9553@users.noreply.github.com>
2022-11-20 22:52:37 +0900

    Feat(OutputView) : 기능9 - 게임 최종 결과 출력 메소드 추가

--- src/main/java/bridge/View/OutputView.java
+++ src/main/java/bridge/View/OutputView.java
@@ -33,12 +33,31 @@ public class OutputView {
      * <p>
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void printResult() {
-        System.out.println(upperBuilder.toString());
-        System.out.println(lowerBuilder.toString());
+    public void printResult(boolean isCrossable, int totalTries) { //TODO: 라인 줄이기
+        StringBuilder stringBuilder = new StringBuilder();
+        String result = createResult(isCrossable);
+
+        stringBuilder.append("\n최종 게임 결과\n");
+        stringBuilder.append(upperBuilder + "\n")
+                .append(lowerBuilder + "\n");
+        stringBuilder.append("\n게임 성공 여부: ")
+                .append(result);
+
+        stringBuilder.append("\n총 시도한 횟수: ")
+                .append(totalTries);
+
+        System.out.println(stringBuilder.toString());
     }
 
-    public String createUpperString(String currentInput, String currentResult) {
+    private String createResult(boolean isCrossable) {
+        if (isCrossable) { //TODO: enum으로 처리해보기
+            return "성공";
+        }
+
+        return "실패";
+    }
+
+    private String createUpperString(String currentInput, String currentResult) {
         String upperString = "";
 
         if (currentInput.equals(UP)) {
@@ -52,7 +71,7 @@ public class OutputView {
         return upperString;
     }
 
-    public String createLowerString(String currentInput, String currentResult) {
+    private String createLowerString(String currentInput, String currentResult) {
         String lowerString = "";
 
         if (currentInput.equals(UP)) {
@@ -66,7 +85,7 @@ public class OutputView {
         return lowerString;
     }
 
-    public void createResultByProgress(int progress, String upperString, String lowerString) {
+    private void createResultByProgress(int progress, String upperString, String lowerString) {
         if (progress == 1) {
             upperBuilder = new StringBuilder();
             lowerBuilder = new StringBuilder();
@@ -80,7 +99,7 @@ public class OutputView {
         }
     }
 
-    public void initResult(String currentResult, StringBuilder stringBuilder) {
+    private void initResult(String currentResult, StringBuilder stringBuilder) {
         stringBuilder.append("[")
                 .append(" ");
         stringBuilder.append(currentResult)
@@ -88,7 +107,7 @@ public class OutputView {
         stringBuilder.append("]");
     }
 
-    public void appendResult(String currentResult, StringBuilder stringBuilder) {
+    private void appendResult(String currentResult, StringBuilder stringBuilder) {
         int endIndex = stringBuilder.indexOf("]");
         stringBuilder.deleteCharAt(endIndex);
 
