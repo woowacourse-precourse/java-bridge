42dbacb8912775763d44d0320c2f2581f4babf0a
hojunking96 <junsong96@naver.com>
2022-11-20 20:12:20 +0900

    feats(OutputView.java) : 출력값 추가

--- src/main/java/bridge/OutputView.java
+++ src/main/java/bridge/OutputView.java
@@ -11,10 +11,11 @@ public class OutputView {
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
     public static void printMap() {
-        UpSide upSide = new UpSide();
-        System.out.println(upSide.getUpSide());
-        DownSide downSide = new DownSide();
-        System.out.println(downSide.getDownSide());
+        Application.upSide.makeUpSide();
+        System.out.println(Application.upSide.getUpSide());
+        Application.downSide.makeDownSide();
+        System.out.println(Application.downSide.getDownSide());
+        System.out.println();
     }
 
     /**
@@ -23,6 +24,20 @@ public class OutputView {
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
     public void printResult() {
+        System.out.println("최종 게임 결과");
+    }
+    public static void printStart() {
+        System.out.println("다리 건너기 게임을 시작합니다.");
+        System.out.println();
+    }
+    public static void printRequestSize(){
+        System.out.println("다리의 길이를 입력해주세요.");
+    }
 
+    public static void printSelect() {
+        System.out.println("이동할 칸을 선택해주세요. (위: U, 아래: D)");
+    }
+    public static void printRetry(){
+        System.out.println("게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)");
     }
 }
