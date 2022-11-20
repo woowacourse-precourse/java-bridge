ac5545290909055161f131e583b372cdba6c01e4
Jin409 <sualng123@gmail.com>
2022-11-20 17:59:02 +0900

    refactor(OutputView): 출력되는 문구를 지역변수로 수정하기
    - 수정 이유 : 전역에서 사용될 이유가 없고 재사용되지 않으므로 수정

--- src/main/java/bridge/view/OutputView.java
+++ src/main/java/bridge/view/OutputView.java
@@ -10,7 +10,7 @@ public class OutputView {
     }
 
     public void printStartMessage() {
-        System.out.println(START_MESSAGE);
+        System.out.println("다리 건너기 게임을 시작합니다.");
     }
 
     /**
