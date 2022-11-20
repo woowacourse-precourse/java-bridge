9696d7e34383c7c478b91c5f96adeba660224b9d
bellljun3395 <195850@jnu.ac.kr>
2022-11-20 18:45:44 +0900

    feat(GameCommend): 구현 validateInput

--- src/main/java/bridge/game/GameCommend.java
+++ src/main/java/bridge/game/GameCommend.java
@@ -9,6 +9,8 @@ public enum GameCommend {
     QUIT("Q"),
     ;
 
+    private static final String WARM = "[ERROR]";
+    private static final String WARM_RESTART_CHOICE = WARM + " 게임을 재시도 여부를 바르게 입력해주세요. (재시도: R, 종료: Q)";
     private final String stringIdentifier;
 
     GameCommend(String stringIdentifier) {
@@ -17,5 +19,12 @@ public enum GameCommend {
 
     public String getStringIdentifier() {
         return stringIdentifier;
+    }
+
+    public static void validateInput(String input) {
+        Arrays.stream(GameCommend.values())
+                .filter(gameCommend -> gameCommend.stringIdentifier.equals(input))
+                .findFirst()
+                .orElseThrow(() -> new IllegalArgumentException(WARM_RESTART_CHOICE));
     }
 }
