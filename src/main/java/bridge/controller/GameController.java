3db637fef29c6d747d9f340f479fa91a982dfbd0
lvalentine6 <ysr1227@naver.com>
2022-11-21 03:38:08 +0900

    feat : 예외 처리
    U,D 이외 문자 예외 처리, R, Q 이외 문자 예외 처리 기능 구현

--- src/main/java/bridge/controller/GameController.java
+++ src/main/java/bridge/controller/GameController.java
@@ -1,21 +1,28 @@
 package bridge.controller;
 
+import bridge.model.BridgeGame;
 import bridge.model.BridgeMaker;
 import bridge.model.BridgeRandomNumberGenerator;
+import bridge.model.Comparison;
 import bridge.view.InputView;
 import bridge.view.OutputView;
 import bridge.view.OutputView.ErrorMessage;
 import java.util.List;
 
 public class GameController {
+    public static int gameCount = 1;
+    public static boolean failOrSuccess = false;
+
     public void gameStart() {
         BridgeMaker bridgeMaker = new BridgeMaker(new BridgeRandomNumberGenerator());
         InputView inputView = new InputView();
         OutputView outputView = new OutputView();
         outputView.gameStart();
-        int size = inputView.readBridgeSize();
+        int size = InputView.readBridgeSize();
         List<String> bridgeResult = bridgeMaker.makeBridge(size);
         System.out.println(bridgeResult);
+        BridgeGame.move(inputView, outputView, bridgeResult);
+        outputView.printResult();
     }
 
     public static boolean isValidBridgeNumber(String input) {
@@ -35,6 +42,30 @@ public class GameController {
         try {
             if (Integer.parseInt(input) < 3 || Integer.parseInt(input) > 20) {
                 throw new IllegalArgumentException(ErrorMessage.ERROR_RANGE.printMessage());
+            }
+        } catch (IllegalArgumentException illegalArgumentException) {
+            System.out.println(illegalArgumentException.getMessage());
+            return true;
+        }
+        return false;
+    }
+
+    public static boolean isValidBridgeAction(String input) {
+        try {
+            if (!(input.equals("U") || input.equals("D"))) {
+                throw new IllegalArgumentException(ErrorMessage.ERROR_UP_DOWN.printMessage());
+            }
+        } catch (IllegalArgumentException illegalArgumentException) {
+            System.out.println(illegalArgumentException.getMessage());
+            return true;
+        }
+        return false;
+    }
+
+    public static boolean isValidBridgeRestart(String input) {
+        try {
+            if (!(input.equals("R") || input.equals("Q"))) {
+                throw new IllegalArgumentException(ErrorMessage.ERROR_RESTART_QUIT.printMessage());
             }
         } catch (IllegalArgumentException illegalArgumentException) {
             System.out.println(illegalArgumentException.getMessage());
