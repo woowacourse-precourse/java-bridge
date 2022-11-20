6e036180f5335921032d72282259513509b13610
eastsage <dpwk8@naver.com>
2022-11-21 02:54:08 +0900

    feat: 다리 랜덤 생성 기능을 통해 다리 객체 생성하는 기능 추가
    makeBridge 함수 구현

--- src/main/java/bridge/model/BridgeGame.java
+++ src/main/java/bridge/model/BridgeGame.java
@@ -6,8 +6,23 @@ import java.util.List;
  * 다리 건너기 게임을 관리하는 클래스
  */
 public class BridgeGame {
-    private List<String> bridge;
-    private Integer position;
+    Bridge bridge;
+    Stack<Direction> movement;
+    MakeMap makeMap;
+
+    public BridgeGame() {
+        movement = new Stack<>();
+        makeMap = new MakeMap();
+    }
+
+    public void makeBridge(int size, BridgeNumberGenerator bridgeNumberGenerator) {
+        BridgeMaker bridgeMaker = new BridgeMaker(bridgeNumberGenerator);
+        List<Direction> newBridge = new ArrayList<>();
+        for (String direction : bridgeMaker.makeBridge(size)) {
+            newBridge.add(Direction.valueOf(direction));
+        }
+        bridge = new Bridge(newBridge);
+    }
 
     /**
      * 사용자가 칸을 이동할 때 사용하는 메서드
