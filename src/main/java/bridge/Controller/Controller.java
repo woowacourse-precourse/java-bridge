1054149c5e2f052b7d483c73b7e74868cff14d22
ljh <dusk5588@naver.com>
2022-11-20 23:20:34 +0900

    docs(README) : 구현할 기능 목록 사항 작성

--- src/main/java/bridge/Controller/Controller.java
+++ src/main/java/bridge/Controller/Controller.java
@@ -3,7 +3,20 @@ package bridge.Controller;
 public class Controller {
 
     public void run(){
-        
+        System.out.println("다리 건너기 게임을 시작합니다.");
+        System.out.println();
+
+        //다리 길이 입력 받기
+
+        //다리 생성하기
+
+        //게임 시작
+
+        ////성공
+
+        ////실패
+
+        //결과 출력
     }
 
 }
