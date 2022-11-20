5fc325110ddd535be0119a54ac00db33e7c0ca1f
KangSungBum <peter9706@naver.com>
2022-11-20 11:03:58 +0900

    feat(BridgeGame.java): add move code
    move 메소드 코드 추가
    - 다리 게임 move에 대한 코드만 추가
    - 다음 다리로 건너는 것에 대한 성공 실패 결과 반환

--- src/main/java/bridge/BridgeGame.java
+++ src/main/java/bridge/BridgeGame.java
@@ -9,13 +9,22 @@ import java.util.List;
  * 다리 건너기 게임을 관리하는 클래스
  */
 public class BridgeGame {
+    private InputView inputModule = new InputView();
+    private OutputView outputModule = new OutputView();
+
 
     /**
      * 사용자가 칸을 이동할 때 사용하는 메서드
      * <p>
      * 이동을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void move() {
+    public boolean move(List<String> bridge, int index) {
+        String userMovement = inputModule.readMoving();
+        if (!bridge.get(index).equals(userMovement)) {
+            return false;
+        }
+        outputModule.printMap();
+        return true;
     }
 
     /**
@@ -23,39 +32,11 @@ public class BridgeGame {
      * <p>
      * 재시작을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void retry() {
-    }
+    public void retry() {}
 
     public List<String> initBridge(int size) {
         BridgeNumberGenerator numberGenerator = new BridgeRandomNumberGenerator();
         BridgeMaker bridgeMaker = new BridgeMaker(numberGenerator);
-        List<String> gameBridge = bridgeMaker.makeBridge(size);
-
-        return gameBridge;
-    }
-
-    /**
-     * 아래 코드는 임시 흐름 코드
-     * skeleton code
-     */
-    public void start() {
-        InputView inputModule = new InputView();
-        OutputView outputModule = new OutputView();
-
-        outputModule.printGameStartMessage();
-
-        outputModule.printBridgeSizeRequestMessage();
-        inputModule.readBridgeSize();
-
-        while (true) {
-            move();
-            outputModule.printMap();
-
-            if (1 == 1) {
-                break;
-            }
-        }
-
-        outputModule.printResult();
+        return bridgeMaker.makeBridge(size);
     }
 }
