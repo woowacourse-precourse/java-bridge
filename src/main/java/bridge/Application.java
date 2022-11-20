ff313f716b1b9df8ca36359f8b76cc4c0db4fa04
haisley77 <qhfg3145@naver.com>
2022-11-20 22:37:20 +0900

    feat(main): 메인 함수 동작 코드 작성

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -1,8 +1,89 @@
 package bridge;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Application {
 
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        // 변수 선언
+        OutputView output = new OutputView();
+        InputView input = new InputView();
+        BridgeNumberGenerator bridgeNumberGenerator = new BridgeRandomNumberGenerator();
+        BridgeMaker maker = new BridgeMaker(bridgeNumberGenerator);
+        BridgeGame game = new BridgeGame();
+
+
+        maker.makeBridge(input.readBridgeSize());
+
+        // 다리 위, 아래 리스트
+        List<String> up_bridge = new ArrayList<>(InputView.bridge_size);
+        List<String> down_bridge = new ArrayList<>(InputView.bridge_size);
+
+        while (BridgeMaker.bridge_index < InputView.bridge_size) {
+            String moving = input.readMoving();
+            String result = game.move(moving, up_bridge, down_bridge);
+
+
+            if (result.equals("U")) {
+                up_bridge.add(" O ");
+                down_bridge.add("   ");
+                BridgeMaker.bridge_index++;
+                output.printMap(up_bridge, down_bridge);
+
+                if (BridgeMaker.bridge_index == InputView.bridge_size) {
+                    output.printResult(up_bridge, down_bridge, 1);
+                    break;
+                }
+
+            }
+            if (result.equals("D")) {
+                up_bridge.add("   ");
+                down_bridge.add(" O ");
+                BridgeMaker.bridge_index++;
+                output.printMap(up_bridge, down_bridge);
+
+                if (BridgeMaker.bridge_index == InputView.bridge_size) {
+                    output.printResult(up_bridge, down_bridge, 1);
+                    break;
+                }
+
+            }
+            if (result.equals("UX")) {
+                up_bridge.add(" X ");
+                down_bridge.add("   ");
+                BridgeMaker.bridge_index++;
+                output.printMap(up_bridge, down_bridge);
+
+                String command = input.readGameCommand();
+                if (command.equals("R")) {
+                    game.retry(up_bridge,down_bridge);
+                }
+                if (command.equals("Q")) {
+                    output.printResult(up_bridge, down_bridge, 0);
+                    break;
+                }
+            }
+            if (result.equals("DX")) {
+                up_bridge.add("   ");
+                down_bridge.add(" X ");
+                BridgeMaker.bridge_index++;
+                output.printMap(up_bridge, down_bridge);
+
+                String command = input.readGameCommand();
+                if (command.equals("R")) {
+                    game.retry(up_bridge,down_bridge);
+                    System.out.println(BridgeMaker.bridge_index);
+                }
+                if (command.equals("Q")) {
+                    output.printResult(up_bridge, down_bridge, 0);
+                    break;
+                }
+            }
+
+
+
+        }
+
     }
 }
