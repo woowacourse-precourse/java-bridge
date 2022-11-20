937f3aa53975fd5cbeaa701602fd3076ee07068f
DevJay1024 <devjay1024@gmail.com>
2022-11-20 22:15:49 +0900

    feat: 다리의 안전경로를 유효하게 캡슐화(추가/수정/삭제 불가)하는 기능

---
+++ src/main/java/bridge/Bridge.java
@@ -0,0 +1,29 @@
+package bridge;
+
+import bridge.exceptions.CustomInternalOperationError;
+import bridge.identifiers.Direction;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Bridge {
+
+    private final List<Direction> securePath;
+    private final int length;
+
+    public Bridge(List<String> securePath, int length){
+        validate(securePath, length);
+        this.length = length;
+        this.securePath = securePath.stream()
+                .map(Direction::valueOf)
+                .collect(Collectors.toList());
+    }
+
+    private void validate(List<String> securePath, int length){
+        if(securePath == null || securePath.size() != length)
+            throw new CustomInternalOperationError(
+                    "다리 길이가 사용자가 입력한 길이와 다릅니다."
+            );
+    }
+
+}
