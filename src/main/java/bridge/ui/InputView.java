39f6b64021c752dac6a1aaefe34fc2885faaf49e
KSH <tkdgns543@gmail.com>
2022-11-20 15:31:14 +0900

    feat(ui): 최초 다리 길이 입력 기능 구현
    에외 검사를 통과하지 못하면 문자열 인스턴스 변수는 null이다
    문자열이 null이 아니게 될때까지 재입력 받다가 반환한다

--- src/main/java/bridge/ui/InputView.java
+++ src/main/java/bridge/ui/InputView.java
@@ -1,15 +1,47 @@
 package bridge.ui;
 
+import camp.nextstep.edu.missionutils.Console;
+
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
  */
 public class InputView {
+    private Integer bridgeLength;
 
     /**
      * 다리의 길이를 입력받는다.
      */
     public int readBridgeSize() {
-        return 0;
+        while (bridgeLength == null) {
+            receiveLength();
+        }
+
+        return bridgeLength;
+    }
+
+    private void receiveLength() {
+        try {
+            String bridgeSize = Console.readLine();
+            int sizeNumber = makeSureNumber(bridgeSize);
+            checkLengthRange(sizeNumber);
+            bridgeLength = sizeNumber;
+        } catch (IllegalArgumentException illegalArgumentException) {
+            System.out.println(illegalArgumentException.getMessage());
+        }
+    }
+
+    private int makeSureNumber(String input) throws IllegalArgumentException {
+        try {
+            return Integer.parseInt(input);
+        } catch (NumberFormatException numberFormatException) {
+            throw new IllegalArgumentException("[ERROR] 다리 길이는 3부터 20 사이의 숫자여야 합니다.");
+        }
+    }
+
+    private void checkLengthRange(int input) throws IllegalArgumentException{
+        if (!(input >= 3 && input <= 20)) {
+            throw new IllegalArgumentException("[ERROR] 다리 길이는 3부터 20 사이의 숫자여야 합니다.");
+        }
     }
 
     /**
