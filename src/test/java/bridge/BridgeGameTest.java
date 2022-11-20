dbc9056dd80c6d6f99866c9b856049fc73db6f6b
parkmuhyeun <pjhg410@gmail.com>
2022-11-20 17:34:38 +0900

    test: BridgeGame 테스트 추가

---
+++ src/test/java/bridge/BridgeGameTest.java
@@ -0,0 +1,34 @@
+package bridge;
+
+import bridge.dto.PathDTO;
+import bridge.model.Command;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static bridge.model.Command.RETRY;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+
+class BridgeGameTest {
+    private static final BridgeGame bridgeGame = new BridgeGame();
+
+    @Test
+    @DisplayName("사용자가 칸을 이동")
+    void move() {
+        List<PathDTO> user = new ArrayList<>();
+        bridgeGame.move(user, any(), any());
+
+        assertEquals(1, user.size());
+    }
+
+    @Test
+    @DisplayName("사용자가 게임을 다시 시도")
+    void retry() {
+        String input = RETRY.getName();
+
+        assertTrue(bridgeGame.retry(input));
+    }
+}
