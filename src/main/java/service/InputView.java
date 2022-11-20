20b991dae4af8550ba93b0816929063577898dfd
UngHeung <t01047377477@gmail.com>
2022-11-20 22:11:33 +0900

    feat : (InputView) 재시작 여부를 입력 받는 메소드 추가

--- src/main/java/service/InputView.java
+++ src/main/java/service/InputView.java
@@ -19,6 +19,8 @@ public class InputView {
     }
 
     public String readGameCommand() {
-    	return null;
+    	String gameCommand = Console.readLine();
+    	validate.validateGameCommand(gameCommand);
+        return gameCommand;
     }
 }
