c4ed8e3fca3343468e7d3606ecdf1a14a62cdd89
cgk95 <kjkj5868@gmail.com>
2022-11-21 01:01:27 +0900

    feat(BridgeGame) : 사용자가 게임을 다시 시도할지 말지 선택하는 기능 구현

--- src/main/java/bridge/BridgeGame.java
+++ src/main/java/bridge/BridgeGame.java
@@ -49,7 +49,11 @@ public class BridgeGame {
      * 재시작을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
     public void retry() {
-        new InputView().readGameCommand();
+        if(new InputView().readGameCommand().equals("Q")){
+            System.out.println("종료멘트 출력");
+            return;
+        }
+        run();
     }
 
 
