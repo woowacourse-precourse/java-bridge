6b475380143c52032b3b874f48290f10db401422
JeongHunHui <deca3248@gmail.com>
2022-11-20 19:01:49 +0900

    feat: GameCommand를 관리하는 Enum 클래스 생성

---
+++ src/main/java/bridge/GameCommand.java
@@ -0,0 +1,26 @@
+package bridge;
+
+import bridge.exception.InvalidGameCommandException;
+
+public enum GameCommand {
+  RETRY("R"),
+  QUIT("Q");
+  private final String command;
+
+  GameCommand(String command) {
+    this.command = command;
+  }
+
+  public static void validateGameCommand(String inputGameCommand) {
+    for (GameCommand gameCommand : GameCommand.values()) {
+      if (gameCommand.command.equals(inputGameCommand)) {
+        return;
+      }
+    }
+    throw new InvalidGameCommandException();
+  }
+
+  public String getCommand(){
+    return this.command;
+  }
+}
