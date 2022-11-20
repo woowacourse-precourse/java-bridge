98797f1b02c75ceee01c2880b9e806b4b791d691
Yunsik-Choi <choiys0311@gmail.com>
2022-11-20 17:11:57 +0900

    ⚡feat : InputValidator 숫자가 아니면 예외 처리하는 기능 구현

--- src/main/java/bridge/view/InputValidator.java
+++ src/main/java/bridge/view/InputValidator.java
@@ -1,14 +1,24 @@
 package bridge.view;
 
+import bridge.exception.BridgeException;
 import bridge.exception.view.BridgeSizeRangeException;
 
 public class InputValidator {
     private static final int BRIDGE_LENGTH_MIN = 3;
     private static final int BRIDGE_LENGTH_MAX = 20;
+    private static final String INPUT_NUMBER_EXCEPTION_MESSAGE = "숫자가 아닙니다.";
 
     public void validateBridgeSizeRange(int size) {
         if (size < BRIDGE_LENGTH_MIN || size > BRIDGE_LENGTH_MAX) {
             throw new BridgeSizeRangeException(BRIDGE_LENGTH_MIN, BRIDGE_LENGTH_MAX);
+        }
+    }
+
+    public int inputNumber(String text) {
+        try {
+            return Integer.parseInt(text);
+        } catch (NumberFormatException exception) {
+            throw new BridgeException(INPUT_NUMBER_EXCEPTION_MESSAGE);
         }
     }
 }
--- src/test/java/bridge/view/InputValidatorTest.java
+++ src/test/java/bridge/view/InputValidatorTest.java
@@ -1,5 +1,6 @@
 package bridge.view;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 
@@ -7,6 +8,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 class InputValidatorTest {
@@ -31,6 +33,26 @@ class InputValidatorTest {
         @ValueSource(ints = {3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20})
         void inputBridgeLength(int size) {
             assertDoesNotThrow(() -> inputValidator.validateBridgeSizeRange(size));
+        }
+
+        @ParameterizedTest(name = "숫자가 아닌 문자면 예외 처리한다.")
+        @ValueSource(strings = {" ", "abc", ",./", "가나다", "@#%^&"})
+        void inputNumberNotDigit(String text) {
+            assertThatThrownBy(() -> inputValidator.inputNumber(text))
+                    .isInstanceOf(IllegalArgumentException.class);
+        }
+
+        @ParameterizedTest(name = "숫자가 공백 또는 Null 이면 예외 처리한다.")
+        @NullAndEmptySource
+        void inputNumberNull(String text) {
+            assertThatThrownBy(() -> inputValidator.inputNumber(text))
+                    .isInstanceOf(IllegalArgumentException.class);
+        }
+
+        @ParameterizedTest(name = "숫자를 입력한다.")
+        @ValueSource(strings = {"3", "4", "19", "20"})
+        void inputNumber(String text) {
+            assertThat(inputValidator.inputNumber(text)).isEqualTo(Integer.parseInt(text));
         }
     }
 }
