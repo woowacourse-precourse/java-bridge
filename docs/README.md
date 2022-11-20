5e4c3116e153ccb39a685cf5e813b6003b390c94
doongu <must1080@naver.com>
2022-11-21 02:35:23 +0900

    feat(User): 이동한 기록을 살펴보는 기능 구현

--- docs/README.md
+++ docs/README.md
@@ -94,7 +94,7 @@ public class BridgeMaker {
 
 **User**
 >- [x] 이동할 칸을 입력 받는 기능 ( addNext )
->- [ ] 이동한 기록을 살펴보는 기능 ( getMoveLog )
+>- [x] 이동한 기록을 살펴보는 기능 ( getMoveLog )
 >- [ ] 이동한 기록을 초기화하는 기능 ( clearLog )
 
 **GameStatus**
--- src/main/java/bridge/domain/User.java
+++ src/main/java/bridge/domain/User.java
@@ -1,6 +1,7 @@
 package bridge.domain;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class User {
     ArrayList<String> moveLog;
@@ -8,4 +9,9 @@ public class User {
     public void addNext(String moving){
         moveLog.add(moving);
     }
+
+    public ArrayList<String> getMove(){
+        return this.moveLog;
+    }
+
 }
