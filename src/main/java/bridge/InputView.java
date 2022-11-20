38cc15cf2088dbc6ad5eee2e6f76cf0909224e27
fakerdeft <whakswp123@naver.com>
2022-11-20 22:23:30 +0900

    feat: 다리사이즈입력 메서드 추가

--- src/main/java/bridge/InputView.java
+++ src/main/java/bridge/InputView.java
@@ -1,5 +1,7 @@
 package bridge;
 
+import camp.nextstep.edu.missionutils.Console;
+
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
  */
@@ -8,8 +10,16 @@ public class InputView {
     /**
      * 다리의 길이를 입력받는다.
      */
-    public int readBridgeSize() {
-        return 0;
+    public static int readBridgeSize() {
+        String readLine = "";
+        while (true) {
+            readLine = Exceptions.validateBridgeLength(Console.readLine());
+            if (!readLine.equals("[ERROR] 3이상 20이하의 숫자만 입력하세요.")) {
+                break;
+            }
+            System.out.println(readLine);
+        }
+        return Integer.parseInt(readLine);
     }
 
     /**
