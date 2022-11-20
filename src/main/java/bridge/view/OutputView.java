07a8ff74a703819057c8b5ff64fab26f7fc917f6
akskflwn <akskflwn@gmail.com>
2022-11-20 23:52:04 +0900

    refactor(OutputView): OutputView 상수화 OuputView에 있는 상수 BridgeGameConstatns 로 분리

--- src/main/java/bridge/view/OutputView.java
+++ src/main/java/bridge/view/OutputView.java
@@ -4,15 +4,10 @@ import bridge.domain.BridgeGame;
 import bridge.domain.Player;
 import java.util.List;
 
+import static bridge.view.OutputConstants.*;
 public class OutputView {
-    private static final String GAME_START_MESSAGE = "다리 건너기 게임을 시작합니다.";
-    private static final String GAME_RESULT_MESSAGE = "최종 게임 결과";
-    private static final String SUCCESS_OR_FAILURE = "게임 성공 여부: %s";
-    private static final String TRIAL_COUNT_MESSAGE = "총 시도한 횟수: %d";
-    private static final String UP_STAIR = "U";
-    private static final String DOWN_STAIR = "D";
 
-    public static void printStartMessage(){
+    public static void printStartMessage() {
         System.out.println(GAME_START_MESSAGE);
         System.out.println();
     }
@@ -24,23 +19,22 @@ public class OutputView {
     }
 
     private static void printDependOn(String stair, BridgeGame bridgeGame) {
-        StringBuilder sb = new StringBuilder("[");
+        StringBuilder sb = new StringBuilder(START_PREFIX);
         sb.append(getPrevious(bridgeGame, stair));
         sb.append(getNext(bridgeGame, stair));
-        sb.append("]");
+        sb.append(END_REGEX);
         System.out.println(sb);
     }
 
     private static String getNext(BridgeGame bridgeGame, String stage) {
         Player player = bridgeGame.getPlayer();
         if (player.isAlive() && stage.equals(player.getLastSelection())) {
-            return " O ";
+            return CORRECT;
         }
         if (!player.isAlive() && stage.equals(player.getLastSelection())) {
-            return " X ";
+            return WRONG;
         }
-
-        return "   ";
+        return BLANK;
     }
 
     private static String getPrevious(BridgeGame bridgeGame, String stage) {
@@ -51,10 +45,11 @@ public class OutputView {
 
         for (int i = 0; i < stageNumber; i++) {
             if (!stage.equals(bridge.get(i))) {
-                previous.append("   |");
+                previous.append(BLANK);
             } else {
-                previous.append(" O |");
+                previous.append(CORRECT);
             }
+            previous.append(DELIMITER);
         }
         return previous.toString();
     }
@@ -68,8 +63,8 @@ public class OutputView {
 
     private static String isSuccess(BridgeGame bridgeGame) {
         if (bridgeGame.isPlayerAlive()) {
-            return "성공";
+            return SUCCESS;
         }
-        return "실패";
+        return FAILURE;
     }
 }
