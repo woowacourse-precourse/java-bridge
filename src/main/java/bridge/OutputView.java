19552b69cbdeab943cf5b6ba9d3f1c3ef4fc8d7f
fakerdeft <whakswp123@naver.com>
2022-11-20 21:01:43 +0900

    feat: 게임 시작 문구 메서드, 다리 길이 입력 문구 메서드 추가

--- src/main/java/bridge/OutputView.java
+++ src/main/java/bridge/OutputView.java
@@ -4,10 +4,10 @@ package bridge;
  * 사용자에게 게임 진행 상황과 결과를 출력하는 역할을 한다.
  */
 public class OutputView {
-    public String printGameStart(){
+    public static String printGameStartPhrase(){
         return "다리 건너기 게임을 시작합니다.";
     }
-    public String printInputingBridgeLength(){
+    public static String printBridgeLengthInputPhrase(){
         return "다리의 길이를 입력해주세요.";
     }
 
