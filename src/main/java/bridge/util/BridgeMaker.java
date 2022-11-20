8c29fd9375d5f1c8a7ef6aef94b0b9e5a42c1321
JangAJang <99702271+JangAJang@users.noreply.github.com>
2022-11-20 22:02:04 +0900

    feat: BridgeMaker 사이즈만큼 랜덤한 U, D의 다리들을 만들어주는 기능 구현

--- src/main/java/bridge/util/BridgeMaker.java
+++ src/main/java/bridge/util/BridgeMaker.java
@@ -24,7 +24,11 @@ public class BridgeMaker {
      * @return 입력받은 길이에 해당하는 다리 모양. 위 칸이면 "U", 아래 칸이면 "D"로 표현해야 한다.
      */
     public List<String> makeBridge(int size) {
-
+        List<String> bridges = new ArrayList<>();
+        while(bridges.size() < size){
+            bridges.add(constructBridgeAtPoint());
+        }
+        return bridges;
     }
 
     private String constructBridgeAtPoint(){
