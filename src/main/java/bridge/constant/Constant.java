d36c4907e9013fecab242e5323ccf9536a950f7a
leesangjin <tkdwls0628@gmail.com>
2022-11-21 00:18:58 +0900

    feat(Constant): 다리와 관련된 상수 정의

---
+++ src/main/java/bridge/constant/Constant.java
@@ -0,0 +1,8 @@
+package bridge.constant;
+
+public class Constant {
+
+    public static final int BRIDGE_WIDTH = 2;
+    public static final int MIN_BRIDGE_SIZE = 3;
+    public static final int MAX_BRIDGE_SIZE = 20;
+}
