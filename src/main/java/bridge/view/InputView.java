f2c7ed546532f3dce08e3233e761cd6bd194fc97
Beginner <dkstmdwn17@naver.com>
2022-11-20 18:12:37 +0900

    feature : 다리의 길이를 입력 받는 기능 구현
    다리의 길이을 입력 받고
    유효성 테스트를 거쳐
    에러가 던져지는 상황이 아니면
    통과해서 int 형으로 리턴한다.

--- src/main/java/bridge/view/InputView.java
+++ src/main/java/bridge/view/InputView.java
@@ -1,5 +1,8 @@
 package bridge.view;
 
+import bridge.validation.Validation;
+import camp.nextstep.edu.missionutils.Console;
+
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
  */
@@ -9,7 +12,12 @@ public class InputView {
      * 다리의 길이를 입력받는다.
      */
     public int readBridgeSize() {
-        return 0;
+        System.out.println("다리 건너기 게임을 시작합니다.");
+        System.out.println();
+        System.out.println("다리의 길이를 입력해주세요.");
+        String userInput = Console.readLine();
+        Validation.verifyUserInputBridgeLength(userInput);
+        return Integer.parseInt(userInput);
     }
 
     /**
