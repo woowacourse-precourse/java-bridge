af95a14053d60335a3ee37cc19072c2c74b419b6
chordpli <chordplaylist@gmail.com>
2022-11-21 02:10:03 +0900

    feat: getter / retry
    1. getter 추가
    2. retry 구현

--- src/main/java/bridge/BridgeGame.java
+++ src/main/java/bridge/BridgeGame.java
@@ -60,6 +60,22 @@ public class BridgeGame {
      * <p>
      * 재시작을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void retry() {
+    public void retry(List<String> bridge) {
+        upBridge = new ArrayList<>();
+        downBridge = new ArrayList<>();
+        bridge.remove(bridge.size()-1);
+        bridgeCount = 0;
+    }
+
+    public List<String> getUpBridge() {
+        return upBridge;
+    }
+
+    public List<String> getDownBridge() {
+        return downBridge;
+    }
+
+    public int getBridgeCount() {
+        return bridgeCount;
     }
 }
