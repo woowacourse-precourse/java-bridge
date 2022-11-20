f7dd99424c20a7776c949135c81bfc111e57ac6a
jungle <minuet1215@jbnu.ac.kr>
2022-11-20 21:27:23 +0900

    Refactor(BridgeMaker): 2차원 bridge를 만드는 메서드 분리

--- src/main/java/bridge/domain/BridgeMaker.java
+++ src/main/java/bridge/domain/BridgeMaker.java
@@ -7,6 +7,7 @@ import bridge.view.OutputView;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import static bridge.util.BridgeConstant.DOWN_KEY;
 import static bridge.util.BridgeConstant.DOWN_POSITION;
@@ -19,8 +20,10 @@ import static bridge.util.BridgeConstant.FALL_BLOCK;
  * 다리의 길이를 입력 받아서 다리를 생성해주는 역할을 한다.
  */
 public class BridgeMaker {
-    private static final int DOWN = 0;
+    private final int DOWN = 0;
+    private final int BRIDGE_WIDTH = 2;
     private final BridgeNumberGenerator bridgeNumberGenerator;
+
     InputView inputView = InputView.getInputView();
     OutputView outputView = OutputView.getOutputView();
 
@@ -55,17 +58,21 @@ public class BridgeMaker {
     }
 
     public String[][] make2DBridge(List<String> bridge) {
-        String[][] bridge2D = new String[2][bridge.size()];
+        String[][] bridge2D = new String[BRIDGE_WIDTH][bridge.size()];
         for (int x = 0; x < bridge.size(); x++) {
             String key = bridge.get(x);
-            if (key.equals(DOWN_KEY)) {
-                bridge2D[UP_POSITION][x] = FALL_BLOCK;
-                bridge2D[DOWN_POSITION][x] = key;
-            } else if (key.equals(UP_KEY)) {
-                bridge2D[DOWN_POSITION][x] = FALL_BLOCK;
-                bridge2D[UP_POSITION][x] = key;
-            }
+            bridge2D[UP_POSITION][x] = getKey(UP_POSITION, key);
+            bridge2D[DOWN_POSITION][x] = getKey(DOWN_POSITION, key);
         }
         return bridge2D;
+    }
+
+    private String getKey(int index, String key) {
+        if (index == UP_POSITION && key.equals(UP_KEY)) {
+            return UP_KEY;
+        } else if (index == DOWN_POSITION && key.equals(DOWN_KEY)) {
+            return DOWN_KEY;
+        }
+        return FALL_BLOCK;
     }
 }
