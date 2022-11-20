372a2d1fd1b1b399b58061ba4773e9629b2e7306
roasted-penguin <swkwon25@hanyang.ac.kr>
2022-11-21 01:40:16 +0900

    feat : BridgeGame - move, addString

--- src/main/java/bridge/service/BridgeGame.java
+++ src/main/java/bridge/service/BridgeGame.java
@@ -16,14 +16,20 @@ public class BridgeGame {
      * <p>
      * 이동을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void move() {
+    public List<String> move(List<String> str, String ox, String moving) {
+        String toadd = "";
+        if(str.get(0).length()!=2) toadd = toadd + "|";
+        List<String> OorX = new ArrayList<>();
+
+        if(moving.equals("U")) OorX = Arrays.asList(ox," ");
+        if(moving.equals("D")) OorX = Arrays.asList(" ",ox);
+        return addString(str,OorX,toadd);
     }
 
-    /**
-     * 사용자가 게임을 다시 시도할 때 사용하는 메서드
-     * <p>
-     * 재시작을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
-     */
-    public void retry() {
+    private List<String> addString(List<String> str, List<String> OorX, String toadd){
+        List<String> result = new ArrayList<>();
+        result.add(str.get(0).substring(0, str.get(0).length() - 1) + toadd + " " + OorX.get(0) + " ]");
+        result.add(str.get(1).substring(0, str.get(1).length() - 1) + toadd + " " + OorX.get(1) + " ]");
+        return result;
     }
 }
