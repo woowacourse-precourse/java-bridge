37c01a359b86b07f5dee08cb7e9e4ad751a88aaa
Taehwan2 <taehwansw@gmail.com>
2022-11-20 18:45:05 +0900

    리펙토리 전 가장 간단한 코드 구현

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -1,8 +1,35 @@
 package bridge;
 
+import java.util.List;
+
 public class Application {
 
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        InputView input = new InputView();
+        BridgeRandomNumberGenerator bRNG = new BridgeRandomNumberGenerator();
+        BridgeMaker BM = new BridgeMaker(bRNG);
+        BridgeGame BG = new BridgeGame();
+        OutputView OP = new OutputView();
+        System.out.println("다리 건너기 게임을 시작합니다.");
+        System.out.println("다리의 길이를 입력해주세요.");
+        int bridgeLength = input.readBridgeSize();
+        boolean check =true;
+       List<String> bridge_check=BM.makeBridge(bridgeLength);
+       do {
+           System.out.println("이동할 칸을 선택해주세요. (위: U, 아래: D)");
+           String bridgeBlank = input.readMoving();
+           check= BG.move(bridgeBlank, bridge_check);
+           if(check==false||bridge_check.isEmpty()){
+               System.out.println("게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)");
+               String bridgegameRecomand = input.readGameCommand();
+               if(bridgegameRecomand.equals("R")){
+                   BG.retry();
+                   check=true;
+                   continue;
+               }
+           }
+           bridge_check.remove(0);
+       }while (check && !bridge_check.isEmpty());
+        OP.printResult(BG,bridge_check.size());
     }
 }
--- src/main/java/bridge/InputView.java
+++ src/main/java/bridge/InputView.java
@@ -4,11 +4,21 @@ package bridge;
  * 사용자로부터 입력을 받는 역할을 한다.
  */
 public class InputView {
-
     /**
      * 다리의 길이를 입력받는다.
      */
+
     public int readBridgeSize() {
+        try {
+            String temp = camp.nextstep.edu.missionutils.Console.readLine();
+            System.out.println((int)temp.charAt(0));
+            if(48>temp.charAt(0) || 57<temp.charAt(0))throw  new IllegalArgumentException("[ERROR]");
+            int i=Integer.parseInt(temp);
+            return i;
+
+        }catch (IllegalArgumentException E){
+            System.out.println("[ERROR]");
+        }
         return 0;
     }
 
@@ -16,13 +26,13 @@ public class InputView {
      * 사용자가 이동할 칸을 입력받는다.
      */
     public String readMoving() {
-        return null;
+        return camp.nextstep.edu.missionutils.Console.readLine();
     }
 
     /**
      * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
      */
     public String readGameCommand() {
-        return null;
+        return camp.nextstep.edu.missionutils.Console.readLine();
     }
 }
