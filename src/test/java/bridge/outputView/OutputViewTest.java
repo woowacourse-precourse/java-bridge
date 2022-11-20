a113d2168ef2fc2152ad2f2a8db96739a24bed19
Kong-hana01 <gksqlsl11@khu.ac.kr>
2022-11-20 18:23:55 +0900

    test(OutputView): printResult test 코드 작성

--- src/test/java/bridge/outputView/OutputViewTest.java
+++ src/test/java/bridge/outputView/OutputViewTest.java
@@ -2,11 +2,10 @@ package bridge.outputView;
 
 import bridge.OutputView;
 import camp.nextstep.edu.missionutils.test.NsTest;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
-import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -19,7 +18,8 @@ public class OutputViewTest extends NsTest {
 
     OutputView outputView = new OutputView();
 
-    @MethodSource("provideParameters")
+    @DisplayName("한 칸씩 움직일 때마다의 출력 테스트 코드")
+    @MethodSource("provideParametersForPrintMap")
     @ParameterizedTest
     void printMapTest(String printedMap1, String printedMap2, Integer countOfMove, Boolean isMove) {
         List<String> bridge = new ArrayList<String>(List.of("U", "D", "D", "U"));
@@ -27,11 +27,36 @@ public class OutputViewTest extends NsTest {
         assertThat(output()).contains(printedMap1, printedMap2);
     }
 
-    private static Stream<Arguments> provideParameters() {
+    @DisplayName("최종 결과에 대한 출력 테스트")
+    @MethodSource("provideParametersForPrintResult")
+    @ParameterizedTest
+    void printResultTest(List<String> printedMap, Integer countOfMove, Boolean isSuccess, Integer countOfPlay) {
+        List<String> bridge = new ArrayList<String>(List.of("U", "D", "D", "U"));
+        outputView.printResult(bridge, countOfMove, isSuccess, countOfPlay);
+        String successMessage = "성공";
+        if (!isSuccess) {
+            successMessage = "실패";
+        }
+        assertThat(output()).contains(
+                printedMap.get(0),
+                printedMap.get(1),
+                successMessage,
+                countOfPlay.toString());
+    }
+
+    private static Stream<Arguments> provideParametersForPrintMap() {
         return Stream.of(
                 Arguments.of("[ O |   |   | O ]", "[   | O | O |   ]", 3, true),
                 Arguments.of( "[ O |   |   | X ]", "[   | O | O |   ]", 3, false),
                 Arguments.of( "[ O |   ]", "[   | X ]", 1, false)
+        );
+    }
+
+    private static Stream<Arguments> provideParametersForPrintResult() {
+        return Stream.of(
+                Arguments.of(List.of("[ O |   |   | O ]", "[   | O | O |   ]"), 3, true, 3),
+                Arguments.of( List.of("[ O |   |   | X ]", "[   | O | O |   ]"), 3, false, 2),
+                Arguments.of( List.of("[ O |   ]", "[   | X ]"), 1, false, 2)
         );
     }
 
