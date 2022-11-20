2b5d3153c4963daf814c66bbaae90d041a07ab9c
Hyeonjun Park <phjppo0918@gmail.com>
2022-11-20 22:56:52 +0900

    refactor(InputView) : 예외처리 부분 메서드 추출

--- src/main/java/bridge/InputView.java
+++ src/main/java/bridge/InputView.java
@@ -6,7 +6,6 @@ import camp.nextstep.edu.missionutils.Console;
  * 사용자로부터 입력을 받는 역할을 한다.
  */
 public class InputView {
-
     private final InputValidator inputValidator = new InputValidator();
 
     /**
@@ -15,20 +14,25 @@ public class InputView {
     public int readBridgeSize() {
         System.out.println(ConsoleMessage.GAME_START);
         String input;
-        while (true) {
-            try {
-                System.out.println(ConsoleMessage.REQUEST_INPUT_BRIDGE_LENGTH);
-                input = Console.readLine();
-                System.out.println(); // 빈 줄 출력
-                inputValidator.validateInputBridgeSize(input);
-            }catch (IllegalArgumentException e) {
-                System.out.println(e.getMessage());
-                continue;
-            }
-            break;
-        }
+        do {
+            input = inputBridgeSize();
+        } while (input == null);
 
         return Integer.parseInt(input);
+    }
+
+    private String inputBridgeSize() {
+        String input;
+        try {
+            System.out.println(ConsoleMessage.REQUEST_INPUT_BRIDGE_LENGTH);
+            input = Console.readLine();
+            System.out.println(); // 빈 줄 출력
+            inputValidator.validateInputBridgeSize(input);
+        }catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+            return null;
+        }
+        return input;
     }
 
     /**
@@ -36,40 +40,49 @@ public class InputView {
      */
     public InputType readMoving() {
         String input;
-        while (true) {
-            try {
-                System.out.println(ConsoleMessage.REQUEST_INPUT_MOVE);
-                input = Console.readLine();
-                inputValidator.validateInputMove(input);
-            } catch (IllegalArgumentException e) {
-                System.out.println(e.getMessage());
-                continue;
-            }
-            break;
-        }
+        do {
+            input = inputMoveCommand();
+        } while (input == null);
 
         return getInputType(input);
+    }
+
+    private String inputMoveCommand() {
+        String input;
+        try {
+            System.out.println(ConsoleMessage.REQUEST_INPUT_MOVE);
+            input = Console.readLine();
+            inputValidator.validateInputMove(input);
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+            return null;
+        }
+        return input;
     }
 
     /**
      * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
      */
     public InputType readGameCommand() {
-
         String input;
-        while (true) {
-            try {
-                System.out.println(ConsoleMessage.REQUEST_INPUT_RETRY);
-                input = Console.readLine();
-                inputValidator.validateInputRetry(input);
-            } catch (IllegalArgumentException e) {
-                System.out.println(e.getMessage());
-                continue;
-            }
-            break;
-        }
+        do {
+            input = inputRetryCommand();
+        } while (input == null);
 
         return getInputType(input);
+    }
+
+    private String inputRetryCommand() {
+        String input;
+        try {
+            System.out.println(ConsoleMessage.REQUEST_INPUT_RETRY);
+            input = Console.readLine();
+            inputValidator.validateInputRetry(input);
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+            return null;
+        }
+        return input;
     }
 
     private InputType getInputType(String command) {
