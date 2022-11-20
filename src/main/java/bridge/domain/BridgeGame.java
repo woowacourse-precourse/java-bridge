de27f52b6817227c7409925a574bd3dd906eed02
alstjr706 <alstjr706@naver.com>
2022-11-20 09:09:06 +0900

    refactor : (다리 건너기) 다리 연결 메서드 리팩토링

--- src/main/java/bridge/domain/BridgeGame.java
+++ src/main/java/bridge/domain/BridgeGame.java
@@ -2,7 +2,6 @@ package bridge.domain;
 
 import bridge.constants.Command;
 import java.util.HashMap;
-import java.util.List;
 
 /**
  * 다리 건너기 게임을 관리하는 클래스
@@ -18,7 +17,8 @@ public class BridgeGame {
     /**
      * 사용자가 칸을 이동할 때 사용하는 메서드
      * <p>
-     * 이동을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다. BridgeGame 클래스에서
+     * 이동을 위해 필요한 메서드의 반환 타입(return type),
+     * 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다. BridgeGame 클래스에서
      * InputView, OutputView 를 사용하지 않는다.
      */
     public HashMap<String, StringBuilder> move(String playerMoving, String bridgeJudgment) {
@@ -45,20 +45,18 @@ public class BridgeGame {
         return bridgeState;
     }
 
+
     /**
      * 사용자가 게임을 다시 시도할 때 사용하는 메서드
      * <p>
      * 재시작을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    // TODO: 꼭 hashmap이여야 하는가?
     public String retry(String gameCommand) {
-        String result = "Q";
         if (gameCommand.equals("R")) {
-            bridgeState.get(Command.UP.getCommand()).setLength(0);
-            bridgeState.get(Command.DOWN.getCommand()).setLength(0);
-            result = "R";
+            bridgeState.reset();
+            return "R";
         }
-        return result;
+        return gameCommand;
     }
 
 
@@ -68,27 +66,12 @@ public class BridgeGame {
         if (playerMoving.equals(designBridge)) {
             resultBridge = "O";
         }
-
-        if (!(playerMoving.equals(designBridge))) {
-            resultBridge = "X";
-        }
-
-        return resultBridge;
+        return "X";
     }
 
 
-    public HashMap<String, StringBuilder> bridgeConnection(int bridgeLength,
-            String bridgeJudgment, int i) {
-
-        if (i != bridgeLength - 1 && bridgeJudgment.equals("O")) {
-            bridgeState.get(Command.UP.getCommand()).append(" | ");
-            bridgeState.get(Command.DOWN.getCommand()).append(" | ");
-        }
-
-        if (i == bridgeLength - 1 || bridgeJudgment.equals("X")) {
-            bridgeState.get(Command.UP.getCommand()).append(" ]");
-            bridgeState.get(Command.DOWN.getCommand()).append(" ]");
-        }
+    public BridgeState bridgeConnection(int bridgeLength, String bridgeJudgment, int i) {
+        bridgeState.connection(bridgeLength, bridgeJudgment, i);
         return bridgeState;
     }
 
