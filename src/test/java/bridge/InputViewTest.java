62d76201e787a8579e61c6b1b65af7b26f1fbbc1
minhee020 <minhee020@naver.com>
2022-11-20 18:45:49 +0900

    test : 방향키 입력 test 구현

--- src/test/java/bridge/InputViewTest.java
+++ src/test/java/bridge/InputViewTest.java
@@ -28,7 +28,7 @@ public class InputViewTest extends NsTest {
         });
     }
 
-    @DisplayName("Bridge Size _ NOT_NUMBER 숫자 + 알파벳")
+    @DisplayName("Bridge Size _ 음수")
     @Test
     void readBridgeSize_NotInTheRightRange_Negative() {
         assertSimpleTest(() -> {
@@ -37,7 +37,7 @@ public class InputViewTest extends NsTest {
         });
     }
 
-    @DisplayName("Bridge Size _ NOT_NUMBER 숫자 + 알파벳")
+    @DisplayName("Bridge Size _ 0")
     @Test
     void readBridgeSize_NotInTheRightRange_Zero() {
         assertSimpleTest(() -> {
@@ -46,11 +46,83 @@ public class InputViewTest extends NsTest {
         });
     }
 
-    @DisplayName("Bridge Size _ NOT_NUMBER 숫자 + 알파벳")
+    @DisplayName("Bridge Size _ 20보다 큰 수")
     @Test
     void readBridgeSize_NotInTheRightRange_OutOfBounds() {
         assertSimpleTest(() -> {
             runException("21");
+            assertThat(output()).contains(ERROR_MESSAGE);
+        });
+    }
+
+    @DisplayName("Moving _ 소문자 u 입력")
+    @Test
+    void readMoving_smallU() {
+        assertSimpleTest(() -> {
+            runException("3", "u");
+            assertThat(output()).contains(ERROR_MESSAGE);
+        });
+    }
+
+    @DisplayName("Moving _ 소문자 d 입력")
+    @Test
+    void readMoving_smallD() {
+        assertSimpleTest(() -> {
+            runException("3", "d");
+            assertThat(output()).contains(ERROR_MESSAGE);
+        });
+    }
+
+    @DisplayName("Moving _ 숫자 입력")
+    @Test
+    void readMoving_NUMBER() {
+        assertSimpleTest(() -> {
+            runException("3", "1");
+            assertThat(output()).contains(ERROR_MESSAGE);
+        });
+    }
+
+    @DisplayName("Moving _ 개행 입력")
+    @Test
+    void readMoving_TAB() {
+        assertSimpleTest(() -> {
+            runException("3", "\t");
+            assertThat(output()).contains(ERROR_MESSAGE);
+        });
+    }
+
+    @DisplayName("GameCommend _ 소문자 R 입력")
+    @Test
+    void readGameCommend_smallR() {
+        assertSimpleTest(() -> {
+            runException("3", "r");
+            assertThat(output()).contains(ERROR_MESSAGE);
+        });
+    }
+
+    @DisplayName("GameCommend _ 소문자 Q 입력")
+    @Test
+    void readGameCommend_smallQ() {
+        assertSimpleTest(() -> {
+            runException("3", "q");
+            assertThat(output()).contains(ERROR_MESSAGE);
+        });
+    }
+
+    @DisplayName("GameCommend _ 숫자 입력")
+    @Test
+    void readGameCommend_NUMBER() {
+        assertSimpleTest(() -> {
+            runException("3", "1");
+            assertThat(output()).contains(ERROR_MESSAGE);
+        });
+    }
+
+    @DisplayName("GameCommend _ 개행 입력")
+    @Test
+    void readGameCommend_TAB() {
+        assertSimpleTest(() -> {
+            runException("3", "\t");
             assertThat(output()).contains(ERROR_MESSAGE);
         });
     }
