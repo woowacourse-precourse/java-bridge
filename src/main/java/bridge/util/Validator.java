226743cd7d451fc9da94a5a487fe5b4c0fe76908
seung-won-kim <hesjs5@skuniv.ac.kr>
2022-11-20 16:09:21 +0900

    refactor(Validator): 매직 스트링을 ErrorType enum으로 대체

--- src/main/java/bridge/util/Validator.java
+++ src/main/java/bridge/util/Validator.java
@@ -1,5 +1,9 @@
 package bridge.util;
 
+import static bridge.constant.ErrorType.BRIDGE_RANGE_ERROR;
+import static bridge.constant.ErrorType.GAME_COMMAND_ERROR;
+import static bridge.constant.ErrorType.MOVING_INPUT_ERROR;
+
 import bridge.constant.BridgeMove;
 import bridge.constant.BridgeRange;
 import bridge.constant.GameCommand;
@@ -8,7 +12,7 @@ public class Validator {
 
     public static void validateNumberInBridgeRange(int number) {
         if (isNumberNotInBridgeRange(number)) {
-            throw new IllegalArgumentException("[ERROR] 다리 길이는 3부터 20 사이의 숫자여야 합니다.");
+            throw new IllegalArgumentException(BRIDGE_RANGE_ERROR.getMessage());
         }
     }
 
@@ -23,7 +27,7 @@ public class Validator {
         BridgeMove bridgeMove = BridgeMove.findByInput(input);
 
         if (bridgeMove.isMiss()) {
-            throw new IllegalArgumentException("[ERROR] 이동할 칸은 U 또는 D를 입력해야 합니다.");
+            throw new IllegalArgumentException(MOVING_INPUT_ERROR.getMessage());
         }
     }
 
@@ -31,7 +35,7 @@ public class Validator {
         GameCommand gameCommand = GameCommand.findByString(string);
 
         if (gameCommand.isMiss()) {
-            throw new IllegalArgumentException("[ERROR] R 또는 Q를 입력해주세요.");
+            throw new IllegalArgumentException(GAME_COMMAND_ERROR.getMessage());
         }
     }
 }
