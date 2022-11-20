618ff61e4020caabeb415f64daec9269a50ce001
leesangjin <tkdwls0628@gmail.com>
2022-11-21 01:27:19 +0900

    feat(Constant): 상수 추가 정의

--- src/main/java/bridge/constant/Constant.java
+++ src/main/java/bridge/constant/Constant.java
@@ -4,6 +4,10 @@ public class Constant {
 
     public static final String UP = "U";
     public static final String DOWN = "D";
+    public static final String RESTART = "R";
+    public static final String QUIT = "Q";
+    public static final String SUCCESS = "성공";
+    public static final String FAIL = "실패";
 
     public static final int BRIDGE_WIDTH = 2;
     public static final int MIN_BRIDGE_SIZE = 3;
