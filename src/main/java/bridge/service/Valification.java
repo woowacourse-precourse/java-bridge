a722af196fe4a1eb34c2d5b5ce7655529b211f25
jaehyeon <jhjh0022@naver.com>
2022-11-20 21:31:16 +0900

    refactor(Valification): 예외 문자 추가

--- src/main/java/bridge/service/Valification.java
+++ src/main/java/bridge/service/Valification.java
@@ -12,19 +12,19 @@ public class Valification {
 
     public void verifyBridgeSize(int size) {
         if (!(MIN_NUMBER <= size && size <= MAX_NUMBER)) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException("다리 길이는 3부터 20 사이의 숫자여야 합니다.");
         }
     }
 
     public void verifyUorD(String move) {
         if (!(Objects.equals(move, Move.UPBRIDGE) || Objects.equals(move, Move.DOWNBRIDGE))) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException("U, D 두개 중에 하나 선택 해야 합니다.");
         }
     }
 
     public void verifyQorR(String hi) {
         if (!(Objects.equals(hi, Command.RETRY) || Objects.equals(hi, Command.QUIT))) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException("R, Q 두개 중에 하나 선택 해야 합니다.");
         }
     }
 }
