f6489a0f92808d288280a85b1b7709164df5a291
shinD <huipulci1@naver.com>
2022-11-20 19:30:24 +0900

    refactor: 지도 화면에 출력하는 기능 OutputView에 위임

--- src/main/java/bridge/view/OutputView.java
+++ src/main/java/bridge/view/OutputView.java
@@ -1,14 +1,23 @@
 package bridge.view;
 
 import bridge.domain.bridge.CrossStatus;
+import bridge.domain.map.BridgeMap;
+import bridge.domain.mapmaker.MapSymbol;
 import bridge.domain.trycount.TryCount;
 
+import java.util.List;
+
 import static bridge.domain.bridge.CrossStatus.SUCCESS;
+import static bridge.domain.mapmaker.MapSymbol.END;
 
 /**
  * 사용자에게 게임 진행 상황과 결과를 출력하는 역할을 한다.
  */
 public class OutputView {
+
+    private static final String NEW_LINT = "\n";
+    private static final String SEPARATOR_REGEX = "\\|";
+    private static final String EMPTY_STRING = "";
 
     private static final String GAME_START = "다리 건너기 게임을 시작합니다.\n";
     private static final String FINAL_GAME_RESULT = "최종 게임 결과";
@@ -23,8 +32,9 @@ public class OutputView {
      * <p>
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void printMap(final String map) {
-        System.out.println(map);
+    public void printMap(final BridgeMap map) {
+
+        System.out.println(mapToString(map));
         System.out.println(EMPTY);
     }
 
@@ -35,15 +45,32 @@ public class OutputView {
      */
     public void printResult(final TryCount tryCount,
                             final CrossStatus status,
-                            final String map) {
+                            final BridgeMap map) {
 
         System.out.println(FINAL_GAME_RESULT);
 
-        System.out.println(map);
+        System.out.println(mapToString(map));
 
         System.out.printf(GAME_STATUS_FORMAT, judgeResult(status));
 
         System.out.printf(GAME_TRY_COUNT_FORMAT, tryCount.count());
+    }
+
+    private String mapToString(BridgeMap map) {
+        List<MapSymbol> topLine = map.getTopLine();
+        List<MapSymbol> bottomLine = map.getBottomLine();
+        return mapLineToString(topLine) + NEW_LINT + mapLineToString(bottomLine);
+    }
+
+    private String mapLineToString(List<MapSymbol> line) {
+        StringBuilder sb = new StringBuilder();
+        for (MapSymbol mapSymbol : line) {
+            sb.append(mapSymbol.symbol());
+        }
+
+        sb.append(END.symbol());
+
+        return sb.toString().replaceFirst(SEPARATOR_REGEX, EMPTY_STRING);
     }
 
     private String judgeResult(final CrossStatus status) {
