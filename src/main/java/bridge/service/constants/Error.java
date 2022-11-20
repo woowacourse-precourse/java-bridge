8117bc89e5e30e437462c6f95750870499828695
jaehyeon <jhjh0022@naver.com>
2022-11-21 03:30:23 +0900

    fix(Error): enum에서 class 로 수정

--- src/main/java/bridge/service/constants/Error.java
+++ src/main/java/bridge/service/constants/Error.java
@@ -1,15 +1,5 @@
 package bridge.service.constants;
 
-public enum Error {
-    ERROR("[ERROR] ");
-
-    private final String error;
-
-    Error(String error){
-        this.error = error;
-    }
-
-    public String getError(){
-        return error;
-    }
+public class Error {
+    public static final String ERROR_MESSAGE = "[ERROR] ";
 }
