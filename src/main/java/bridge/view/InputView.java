320c8a296169ff51a6ca6658caa403444c06ac6d
jinhyeon <vmflwu123@gmail.com>
2022-11-20 09:53:44 +0900

    fix(view/InputView): 플레이어 입력 메서드 수정
    각 메서드에 static 키워드 추가

--- src/main/java/bridge/view/InputView.java
+++ src/main/java/bridge/view/InputView.java
@@ -8,17 +8,17 @@ public class InputView {
     private static final String SELECT_BRIDGE_BLOCK = "이동할 칸을 선택해주세요. (위: U, 아래: D)";
     private static final String SELECT_RETRY_OR_QUIT = "게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)";
 
-    public Integer readBridgeLength() {
+    public static Integer readBridgeLength() {
         System.out.println(INPUT_BRIDGE_LENGTH);
         return Integer.valueOf(Console.readLine());
     }
 
-    public String readSelectedBridgeBlock() {
+    public static String readSelectedBridgeBlock() {
         System.out.println(SELECT_BRIDGE_BLOCK);
         return Console.readLine();
     }
 
-    public String readGameCommand() {
+    public static String readGameCommand() {
         System.out.println(SELECT_RETRY_OR_QUIT);
         return Console.readLine();
     }
