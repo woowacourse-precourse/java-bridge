ddca6c832ddce70c31deb230224cddc18f7b0d30
SuhHyeonjun <dmsgktn0208@gmail.com>
2022-11-20 13:17:00 +0900

    feat : 위,아래 다리 생성 테스트

---
+++ src/test/java/bridge/BridgeTest.java
@@ -0,0 +1,35 @@
+package bridge;
+
+import static bridge.domain.UpDownBridge.getDownBridge;
+import static bridge.domain.UpDownBridge.getUpBridge;
+import static bridge.domain.UpDownBridge.makeUpDownBridge;
+
+import static org.assertj.core.util.Lists.newArrayList;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import bridge.ApplicationTest.TestNumberGenerator;
+import bridge.domain.BridgeGame;
+import bridge.domain.BridgeMaker;
+
+import java.util.List;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class BridgeTest {
+    BridgeGame bridgeGame = new BridgeGame();
+    BridgeNumberGenerator bridgeNumberGenerator = new TestNumberGenerator(newArrayList(1, 0, 0));
+    BridgeMaker bridgeMaker = new BridgeMaker(bridgeNumberGenerator);
+    List<String> bridges = bridgeMaker.makeBridge(3);
+    String moveResult = bridgeGame.move(bridges.get(0), "U");
+
+    @DisplayName("위,아래 다리 값 테스트")
+    @Test
+    void makeUpDownBridgeTest() {
+        makeUpDownBridge("U", moveResult);
+        String upBridge = getUpBridge().get(0);
+        String downBridge = getDownBridge().get(0);
+        assertEquals(upBridge, "O");
+        assertEquals(downBridge, " ");
+    }
+}
