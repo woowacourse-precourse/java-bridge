ee230b5289fbd9f9982f3ea26fff06e50063691e
Sangkyun Seo <tkdrbs0802@naver.com>
2022-11-20 16:02:42 +0900

    feat: 사용자가 다리 이동할때 처리하는 메서드 작성

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -7,11 +7,20 @@ public class Application {
     static String printGameStart = "다리 건너기 게임을 시작합니다.";
     static BridgeNumberGenerator bridgeNumberGenerator;
     static List<String> bridge = new ArrayList<>();
+    static String moveBridge = "";
     public static void main(String[] args) {
         System.out.println(printGameStart);
         InputView inputView = new InputView();
+        OutputView outputView = new OutputView();
         BridgeMaker bridgeMaker = new BridgeMaker(bridgeNumberGenerator);
+        BridgeGame bridgeGame = new BridgeGame();
         bridge = bridgeMaker.makeBridge(inputView.readBridgeSize());
+
+        for(int i=0;i<bridge.size();i++){
+            outputView.printMap(i,bridgeGame.move(bridge,i,inputView.readMoving()));
+
+        }
+
 
 
     }
