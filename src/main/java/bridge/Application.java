4d2b2624b2b39294c509f62a70d71ac29c42fb2f
zcae1020 <zcae1020@naver.com>
2022-11-20 16:23:30 +0900

    fix: error message 출력할 수 있는 기능 구현

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -3,7 +3,11 @@ package bridge;
 public class Application {
 
     public static void main(String[] args) {
-        GameSimulator gameSimulator = new GameSimulator();
-        gameSimulator.simulateGame();
+        try {
+            GameSimulator gameSimulator = new GameSimulator();
+            gameSimulator.simulateGame();
+        } catch (IllegalArgumentException exception) {
+            System.out.println(exception.getMessage());
+        }
     }
 }
