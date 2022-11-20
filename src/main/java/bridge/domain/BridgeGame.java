a9db7695677cbe90623f7960b9cc5b1dfc45009e
Haaae <101785628+Haaae@users.noreply.github.com>
2022-11-21 01:28:43 +0900

    feat(BridgeGame) : 이동결과확인 기능 구현

--- src/main/java/bridge/domain/BridgeGame.java
+++ src/main/java/bridge/domain/BridgeGame.java
@@ -1,16 +1,37 @@
 package bridge.domain;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * 다리 건너기 게임을 관리하는 클래스
  */
 public class BridgeGame {
+
+    private final List<String> bridge;
+    private final List<String> movings;
+
+    public BridgeGame(List<String> bridge) {
+        this.bridge = bridge;
+        this.movings = new ArrayList<>(bridge.size());
+    }
 
     /**
      * 사용자가 칸을 이동할 때 사용하는 메서드
      * <p>
      * 이동을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void move() {
+    public void move(String direction) {
+
+    }
+
+    public boolean isOver() {
+        int movingCount = movings.size() - 1;
+        return !movings.get(movingCount).equals(bridge.get(movingCount));
+    }
+
+    public boolean isSucess() {
+        return bridge.size() == movings.size();
     }
 
     /**
