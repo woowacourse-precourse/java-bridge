265c1faf82b64fa5add50756a0a9beb50a59302b
DevJay1024 <devjay1024@gmail.com>
2022-11-20 22:30:57 +0900

    feat: 사용자의 이동 경로를 저장하는 기능 구현

---
+++ src/main/java/bridge/PlayerPath.java
@@ -0,0 +1,22 @@
+package bridge;
+
+import bridge.identifiers.Direction;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class PlayerPath {
+
+    private List<Direction> currentPath = new ArrayList<>();
+
+    public PlayerPath(){}
+
+    public void saveDirection(Direction direction){
+        currentPath.add(direction);
+    }
+
+    public List<Direction> getPath(){
+        return Collections.unmodifiableList(currentPath);
+    }
+}
