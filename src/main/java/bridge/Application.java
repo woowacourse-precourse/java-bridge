6a2c55a37cb1e0dd762d78fa79e57afecb23a5b3
JUHA <khabh@naver.com>
2022-11-20 16:06:58 +0900

    feat: 전체 게임 실행 기능 구현

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -3,7 +3,6 @@ package bridge;
 import bridge.controller.BridgeController;
 import bridge.controller.BridgeGameController;
 import bridge.controller.PlayerController;
-import bridge.view.OutputView;
 
 public class Application {
     private static final BridgeGameController bridgeGameController = new BridgeGameController();
@@ -11,17 +10,36 @@ public class Application {
     private static final BridgeController bridgeController = new BridgeController();
 
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
-        OutputView.startGame();
+        init();
+        playGame();
+    }
+
+    private static void init() {
         bridgeGameController.generateBridgeGame();
         bridgeController.generateBridge();
         playerController.generatePlayer();
+    }
 
+    private static boolean startCrossing(int bridgeSize) {
         String movingDirection = playerController.getMovingDirection();
         int currentDistance = playerController.getCurrentDistance();
-        boolean isPassable = bridgeController.isPassable(currentDistance, movingDirection);
-        if (isPassable) {
-            playerController.increaseMovedDistance();
+        boolean movingSuccess = bridgeController.isPassable(currentDistance, movingDirection);
+        bridgeGameController.crossBridge(movingDirection, movingSuccess);
+
+        if (currentDistance + 1 == bridgeSize || !movingSuccess) {
+            return movingSuccess;
         }
+        return startCrossing(bridgeSize);
+    }
+
+    private static boolean playGame() {
+        boolean gameResult = startCrossing(bridgeController.getBridgeSize());
+        if (!gameResult && playerController.retryGame()) {
+            bridgeGameController.retryGame();
+            return playGame();
+        }
+        bridgeGameController.printResult(gameResult);
+        playerController.printGameStatics();
+        return gameResult;
     }
 }
