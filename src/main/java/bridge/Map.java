87d06e436d0c4f62da7ea31af3c856b28501218a
swonny <wonny921@gmail.com>
2022-11-20 16:05:57 +0900

    refactor(Map): 다리 이동 위치를 저장하는 클래스 수정

--- src/main/java/bridge/Map.java
+++ src/main/java/bridge/Map.java
@@ -1,29 +1,35 @@
 package bridge;
 
 import enumCollections.GameResult;
+import enumCollections.GameStatus;
 import enumCollections.Position;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class Map {
-    private List<String> ups = new ArrayList<>();
-    private List<String> downs = new ArrayList<>();
+    private List<List<String>> bridgeUserInterface = new ArrayList<>();
 
-    public void addUpper(String result) {
-        ups.add(result);
-        downs.add(GameResult.getUi(GameResult.EMPTY));
+    public Map() {
+        bridgeUserInterface.add(new ArrayList<>());
+        bridgeUserInterface.add(new ArrayList<>());
     }
 
-    public void addBelow(String result) {
-        downs.add(result);
-        downs.add(GameResult.getUi(GameResult.EMPTY));
+    public void add(Position position, String isSucceed) {
+        this.bridgeUserInterface.get(Position.getIndex(position))
+                .add(isSucceed);
     }
 
-    public List<List<String>> getMaps() {
-        List<List<String>> result = new ArrayList<>();
-        result.add(ups);
-        result.add(downs);
-        return result;
+    public String get() {
+        return this.bridgeUserInterface.toString();
+    }
+
+    public void add(BridgeGame bridgeGame, GameStatus gameStatus) {
+        for (int bridgeIndex = 0; bridgeIndex < bridgeGame.getCurrentPosition(); bridgeIndex++) {
+            add(Position.getPosition(
+                    bridgeGame.getAvailableSquare(bridgeIndex)),
+                    GameResult.getUi(GameResult.RIGHT_POSITION)
+            );
+        }
     }
 }
