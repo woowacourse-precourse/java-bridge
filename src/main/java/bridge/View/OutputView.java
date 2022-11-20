4734d07ffb0369db0fa366212034d73e170114ef
Sieun Park <psieun0606@gmail.com>
2022-11-21 02:49:58 +0900

    feat: 위쪽과 아랫쪽 줄 프린트를 위한 리스트 세팅 기능 추가

--- src/main/java/bridge/View/OutputView.java
+++ src/main/java/bridge/View/OutputView.java
@@ -1,16 +1,21 @@
 package bridge.View;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * 사용자에게 게임 진행 상황과 결과를 출력하는 역할을 한다.
  */
 public class OutputView {
     private static final String GAME_START = "다리 건너기 게임을 시작합니다.";
+    private List<String> upSide = new ArrayList<>();
+    private List<String> downSide = new ArrayList<>();
 
-    public static void printGameStart() {
+    public void printGameStart() {
         System.out.println(GAME_START);
     }
 
-    public static void printEmptyLine() {
+    public void printEmptyLine() {
         System.out.println();
     }
 
@@ -19,7 +24,38 @@ public class OutputView {
      * <p>
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void printMap() {
+    public void printMap(int index) {
+    }
+
+    public void setUpandDownSide(String userMove, String answer) {
+        if (userMove.equals("U")) {
+            setUpSide(userMove, answer);
+        }
+        if (userMove.equals("D")) {
+            setDownSide(userMove, answer);
+        }
+    }
+
+    private void setUpSide(String userMove, String answer) {
+        if (userMove.equals(answer)) {
+            upSide.add(" O ");
+            downSide.add("   ");
+        }
+        if (!userMove.equals(answer)) {
+            upSide.add(" X ");
+            downSide.add("   ");
+        }
+    }
+
+    private void setDownSide(String userMove, String answer) {
+        if (userMove.equals(answer)) {
+            upSide.add("   ");
+            downSide.add(" O ");
+        }
+        if (!userMove.equals(answer)) {
+            upSide.add("   ");
+            downSide.add(" X ");
+        }
     }
 
     /**
