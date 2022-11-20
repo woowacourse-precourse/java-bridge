4f6d8d933f6fb70914c17547e3aba52c86f15767
Eungyu-code <Agyu625@gmail.com>
2022-11-20 17:31:05 +0900

    test(BridgeGameTest class): moveTest 메소드 추가
    move, bridgeTopExist, bridgeBottomExist 메소드를 테스트하는 메소드

--- src/test/java/bridge/BridgeGameTest.java
+++ src/test/java/bridge/BridgeGameTest.java
@@ -1,4 +1,23 @@
 package bridge;
 
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.*;
+
 public class BridgeGameTest {
+
+    @Test
+    void moveTest() {
+        BridgeGame bridgeGame = new BridgeGame();
+        List<String> topBridge = new ArrayList<>();
+        List<String> bottomBridge = new ArrayList<>();
+        bridgeGame.move(topBridge, bottomBridge, "U");
+
+        assertThat(List.of("O")).isEqualTo(topBridge);
+        assertThat(List.of(" ")).isEqualTo(bottomBridge);
+    }
 }
