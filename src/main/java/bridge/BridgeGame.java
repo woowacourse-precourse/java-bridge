b370b10eaf3e792c3e440c9338a74593735629cf
HGB <hsw8147@gmail.com>
2022-11-21 06:04:32 +0900

    feat: BridgeGame method
    BridgeGame class 메서드 추가

--- src/main/java/bridge/BridgeGame.java
+++ src/main/java/bridge/BridgeGame.java
@@ -4,13 +4,15 @@ package bridge;
  * 다리 건너기 게임을 관리하는 클래스
  */
 public class BridgeGame {
-
+    private int position = 0;
+    private int retryNumber = 0;
     /**
      * 사용자가 칸을 이동할 때 사용하는 메서드
      * <p>
      * 이동을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
     public void move() {
+        this.position += 1;
     }
 
     /**
@@ -19,5 +21,15 @@ public class BridgeGame {
      * 재시작을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
     public void retry() {
+        this.position -= 1;
+        this.retryNumber += 1;
+    }
+
+    public int getPosition() {
+        return position;
+    }
+
+    public int getRetryNumber() {
+        return retryNumber;
     }
 }
