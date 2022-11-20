50820adb9896a5b1b10c63d215feb7b112d56483
junyong <jymoon1121@gmail.com>
2022-11-20 14:14:21 +0900

    feat(BridgeGame): 다리건너기 게임 초기 세팅을 위한 init() 메서드 생성

--- src/main/java/bridge/BridgeGame.java
+++ src/main/java/bridge/BridgeGame.java
@@ -1,10 +1,21 @@
 package bridge;
 
+import java.util.List;
+
 /**
  * 다리 건너기 게임을 관리하는 클래스
  */
 public class BridgeGame {
 
+    InputView inputview = new InputView();
+    BridgeMaker bridgeMaker;
+    Bridge bridge;
+
+    public void init() {
+        int bridgeLen = inputview.readBridgeSize();
+        List<String> answerBridge = bridgeMaker.makeBridge(bridgeLen);
+        bridge = new Bridge(answerBridge);
+    }
     /**
      * 사용자가 칸을 이동할 때 사용하는 메서드
      * <p>
