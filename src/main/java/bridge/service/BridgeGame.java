d09e755fb011d9153d48fdec32df042ecb0feaac
ksh-beginner <ohk9134@naver.com>
2022-11-20 09:11:46 +0900

    refactor(BridgeGame) : 서비스 로직으로 리팩토링

--- src/main/java/bridge/service/BridgeGame.java
+++ src/main/java/bridge/service/BridgeGame.java
@@ -1,56 +1,31 @@
-package bridge.controller;
+package bridge.service;
 
 import bridge.BridgeMaker;
 import bridge.BridgeRandomNumberGenerator;
-import bridge.constant.ErrorConstant;
+
 import bridge.model.GeneratedBridge;
 import bridge.model.PlayerBridge;
 import bridge.model.PrintBridge;
-import bridge.validator.GameRetryOrEndCommandValidator;
-import bridge.validator.InputBridgeSizeValidator;
-import bridge.validator.InputNextStepValidator;
-import bridge.view.InputView;
-import bridge.view.OutputView;
+
+
 
 import java.util.List;
-import java.util.regex.Pattern;
+
 
 /**
  * 다리 건너기 게임을 관리하는 클래스
  */
 public class BridgeGame {
 
-    private static final String RETRY_COMMAND = "R";
-    private static final String END_COMMAND = "Q";
 
     private final BridgeMaker bridgeMaker = new BridgeMaker(new BridgeRandomNumberGenerator());
     private GeneratedBridge generatedBridge;
     private final PlayerBridge playerBridge = new PlayerBridge();
     private final PrintBridge printBridge = new PrintBridge();
 
-    private String bridgeSize;
     private boolean canMove;
     private int nextStepIndex;
-    private String inputNextStep;
-    private String currentPlayerBridgeUpShape;
-    private String currentPlayerBridgeDownShape;
     private boolean isGameSuccess;
-    private String retryOrEndCommand;
-    private int tryCount = 1;
-    private String successOrFailure;
-
-    public void printBridgeGameStartMessage() {
-        outputView.printBridgeGameStartMessage();
-    }
-
-    public void playGame() {
-        initGame();
-        printBridgeGameStartMessage();
-        inputBridgeSize();
-        generateBridge();
-        moveUntilSuccessOrFailure();
-        gameSuccessOrFailure();
-    }
 
     public void initGame() {
         nextStepIndex = 0;
@@ -58,44 +33,14 @@ public class BridgeGame {
         isGameSuccess = false;
     }
 
-    public void inputBridgeSize() {
-        while (true) {
-            try {
-                outputView.printInputBridgeSizeMessage();
-                bridgeSize = inputView.inputBridgeSize();
-                inputBridgeSizeValidator.validate(bridgeSize);
-                break;
-            } catch (IllegalArgumentException e) {
-                System.out.println(e.getMessage());
-            }
-        }
-    }
-
-    public void generateBridge() {
+    public void generateBridge(String bridgeSize) {
         List<String> bridgeShape = bridgeMaker.makeBridge(Integer.parseInt(bridgeSize));
         generatedBridge = new GeneratedBridge(bridgeShape);
     }
 
-    private void moveUntilSuccessOrFailure() {
-        while (canMove && (isGameSuccess == false)) {
-            inputNextStep();
-            move();
-            printCurrentBridge();
-            judgeGameStatus();
-        }
-    }
 
-    public void inputNextStep() {
-        while (true) {
-            try {
-                outputView.printInputMovingStepMessage();
-                inputNextStep = inputView.inputMovingStep();
-                inputNextStepValidator.validate(inputNextStep);
-                break;
-            } catch (IllegalArgumentException e) {
-                System.out.println(e.getMessage());
-            }
-        }
+    public boolean canMoveAndGameNotFinished() {
+        return canMove && (isGameSuccess == false);
     }
 
     /**
@@ -103,48 +48,32 @@ public class BridgeGame {
      * <p>
      * 이동을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void move() {
+    public void move(String inputNextStep) {
         canMove = generatedBridge.canMoveNextStep(nextStepIndex, inputNextStep);
         playerBridge.moveNextStep(inputNextStep, canMove);
         nextStepIndex++;
     }
 
-    public void printCurrentBridge() {
-        currentPlayerBridgeUpShape = printBridge.generatePrintUpShape(playerBridge.getUpShape());
-        currentPlayerBridgeDownShape = printBridge.generatePrintDownShape(playerBridge.getDownShape());
-        outputView.printMap(currentPlayerBridgeUpShape, currentPlayerBridgeDownShape);
-
+    public String getCurrentBridgeUpShape() {
+        return printBridge.generatePrintUpShape(playerBridge.getUpShape());
     }
 
-    public void judgeGameStatus() {
-        if (nextStepIndex == Integer.parseInt(bridgeSize) && canMove) {
+    public String getCurrentBridgeDownShape() {
+        return printBridge.generatePrintDownShape(playerBridge.getDownShape());
+    }
+
+    public void judgeGameStatus(String inputBridgeSize) {
+        if (nextStepIndex == Integer.parseInt(inputBridgeSize) && canMove) {
             isGameSuccess = true;
         }
     }
 
-    private void retryOrEnd() {
-        inputRetryOrEndCommand();
-        if (retryOrEndCommand.equals(RETRY_COMMAND)) {
-            tryCount++;
-            playerBridge.initializeShape();
-            retry();
-        }
-        if (retryOrEndCommand.equals(END_COMMAND)) {
-            printGameResult();
-        }
+    public boolean isGameSuccess() {
+        return isGameSuccess == true;
     }
 
-    public void inputRetryOrEndCommand() {
-        while (true) {
-            try {
-                outputView.printRetryOrEndMessage();
-                retryOrEndCommand = inputView.inputRetryOrEndCommand();
-                gameRetryOrEndCommandValidator.validate(retryOrEndCommand);
-                break;
-            } catch (IllegalArgumentException e) {
-                System.out.println(e.getMessage());
-            }
-        }
+    public boolean isGameFailure() {
+        return isGameSuccess == false;
     }
 
     /**
@@ -154,24 +83,6 @@ public class BridgeGame {
      */
     public void retry() {
         initGame();
-        moveUntilSuccessOrFailure();
-        gameSuccessOrFailure();
-    }
-
-    private void gameSuccessOrFailure() {
-        if (isGameSuccess) {
-            successOrFailure = "성공";
-            printGameResult();
-        }
-        if (isGameSuccess == false) {
-            successOrFailure = "실패";
-            retryOrEnd();
-        }
-    }
-
-    public void printGameResult() {
-        outputView.printGameResultMessage();
-        outputView.printMap(currentPlayerBridgeUpShape, currentPlayerBridgeDownShape);
-        outputView.printResult(successOrFailure, tryCount);
+        playerBridge.initializeShape();
     }
 }
