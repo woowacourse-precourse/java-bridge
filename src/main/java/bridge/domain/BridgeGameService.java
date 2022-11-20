956df6f90b80d039dc570b093b1ebe39930114fc
baksohyeon <bori12370@gmail.com>
2022-11-20 20:46:35 +0900

    refactor: interface 적용

---
+++ src/main/java/bridge/domain/BridgeGameService.java
@@ -0,0 +1,8 @@
+package bridge.domain;
+
+import java.util.List;
+
+public interface BridgeGameService {
+    void move(String step);
+    void retry(String command);
+}
--- src/main/java/bridge/service/BridgeGame.java
+++ src/main/java/bridge/service/BridgeGame.java
@@ -1,9 +1,16 @@
 package bridge.service;
 
+import bridge.domain.BridgeGameService;
+
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * 다리 건너기 게임을 관리하는 클래스
  */
-public class BridgeGame {
+public class BridgeGame implements BridgeGameService {
+
+
 
     /**
      * 사용자가 칸을 이동할 때 사용하는 메서드
@@ -16,7 +23,9 @@ public class BridgeGame {
      * 해당 클래스 내에서 View 사용 불가능
      */
 
-    public void move() {
+
+    @Override
+    public void move(String step) {
     }
 
     /**
@@ -24,7 +33,8 @@ public class BridgeGame {
      * <p>
      * 재시작을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void retry() {
+    @Override
+    public void retry(String command) {
     }
 
 }
