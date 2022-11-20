c303595e58246a35a7f4be0db16bf2745f5ff5a4
ensk26 <tjensk26@gmail.com>
2022-11-20 22:08:59 +0900

    feat(InputValidation): 입력 검증 클래스
    새로운 클래스 추가 to InputValidation
    - 다리의 길이 입력 검증 메서드
    - 사용자가 이동할 칸 입력 검증 메서드
    - 사용자가 게임을 다시 시도할지 종료할지 여부 입력 검증 메서드
    - 테스트를 편하게 하기 위해 검증부분만 따로 클래스 생성

---
+++ src/main/java/bridge/InputValidation.java
@@ -0,0 +1,16 @@
+package bridge;
+
+public class InputValidation {
+
+    public int readBridgeSizeValidation() {
+        return 0;
+    }
+
+    public String readMovingValidation() {
+        return null;
+    }
+
+    public String readGameCommandValidation() {
+        return null;
+    }
+}
