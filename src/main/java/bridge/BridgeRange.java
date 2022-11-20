630cd270e402153c4002881de9744a540947e915
wonwoo6y <wonwoo6y@gmail.com>
2022-11-21 00:48:12 +0900

    feat(BridgeRange): 다리 길이 범위 선언 클래스 생성

---
+++ src/main/java/bridge/BridgeRange.java
@@ -0,0 +1,32 @@
+package bridge;
+
+public enum BridgeRange {
+    MIN(3),
+    MAX(20),
+    EXCEPTION(-1);
+
+    private final int range;
+
+    BridgeRange(int range) {
+        this.range = range;
+    }
+
+    public static boolean isWithinRange(int value) {
+        if (value < MIN.range || MAX.range < value) {
+            return false;
+        }
+        return true;
+    }
+
+    public static int getMinValue() {
+        return MIN.range;
+    }
+
+    public static int getMaxValue() {
+        return MAX.range;
+    }
+
+    public static int getExceptionValue() {
+        return EXCEPTION.range;
+    }
+}
