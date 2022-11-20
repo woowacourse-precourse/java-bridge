9b97d36d34e3d5e8afbead7ac6abcc43471693a0
Identityless <rlandwnd555@naver.com>
2022-11-21 00:53:04 +0900

    feat : 다리 길이 입력 기능 추가

--- src/main/java/bridge/InputView.java
+++ src/main/java/bridge/InputView.java
@@ -1,15 +1,38 @@
 package bridge;
 
+import java.util.Scanner;
+
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
  */
 public class InputView {
 
+    private Scanner scanner;
+
+    public InputView() {
+        scanner = new Scanner(System.in);
+    }
     /**
      * 다리의 길이를 입력받는다.
      */
-    public int readBridgeSize() {
-        return 0;
+    public int readBridgeSize() throws IllegalAccessException {
+        String size = scanner.nextLine();
+        validate(size);
+
+        return Integer.parseInt(size);
+    }
+
+    private void validate(String size) throws IllegalAccessException {
+        int isize;
+        for(int i = 0 ; i < size.length() ; i++) {
+            if(!Character.isDigit(size.charAt(i))) {
+                throw new IllegalAccessException();
+            }
+        }
+        isize = Integer.parseInt(size);
+        if(isize<3 || isize>20) {
+            throw new IllegalAccessException();
+        }
     }
 
     /**
