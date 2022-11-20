bf18e1038b6f70009b9cc652c06d0e6d8d065ca1
jinny-l <jinny.l.dev@gmail.com>
2022-11-20 16:10:07 +0900

    refactor(BridgeGameController): BridgeGame과 View를 이어주는 역할을 책임지도록 수정
    - 참고:
    수정 전: Controller에서 데이터 저장 및 변경의 역할까지 담당
    수정 후: 데이터 저장 및 변경은 BridgeGame에서 담당

---
+++ src/main/java/bridge/Controller/BridgeGameController.java
@@ -0,0 +1,89 @@
+package bridge.Controller;
+
+import bridge.BridgeGame;
+import bridge.InputView;
+import bridge.OutputView;
+import bridge.domain.Block;
+import bridge.repository.BridgeResultData;
+
+public class BridgeGameController {
+
+    private static final String WIN_GAME = "성공";
+    private static final String LOSE_GAME = "실패";
+
+    private int trials = 1;
+    private String moving;
+    private boolean retry = true;
+
+    BridgeGame bridgeGame;
+
+    public void run() {
+        initialize();
+        while (retry) {
+            moving();
+            if (bridgeGame.movingIsWrong(moving)) {
+                retryOrQuit();
+            }
+            if (bridgeGame.winBridgeGame(moving)) {
+                printResult(WIN_GAME);
+                break;
+            }
+        }
+    }
+
+    private void initialize() {
+        OutputView.printStart();
+        makeBridge();
+        System.out.println();
+    }
+
+    private void makeBridge() {
+        try {
+            int size = InputView.readBridgeSize();
+            bridgeGame = new BridgeGame(size);
+        } catch (IllegalArgumentException ie) {
+            System.out.println(ie.getMessage());
+            makeBridge();
+        }
+    }
+
+    private void moving() {
+        readMoving();
+        bridgeGame.move(moving);
+        OutputView.printMap(BridgeResultData.getUpBridgeResults(), BridgeResultData.getDownBridgeResults());
+    }
+
+    private void readMoving() {
+        try {
+            moving = Block.from(InputView.readMoving());
+        } catch (IllegalArgumentException ie) {
+            System.out.println(ie.getMessage());
+            readMoving();
+        }
+    }
+
+    private void retryOrQuit() {
+        readRetry();
+        if (retry) {
+            bridgeGame.retry();
+            trials++;
+        }
+        if (!retry) {
+            printResult(LOSE_GAME);
+        }
+    }
+
+    private void readRetry() {
+        try {
+            this.retry = bridgeGame.readRetry(InputView.readGameCommand());
+        } catch (IllegalArgumentException ie) {
+            System.out.println(ie.getMessage());
+            readRetry();
+        }
+    }
+
+    private void printResult(String gameResult) {
+        OutputView.printLastMap(BridgeResultData.getUpBridgeResults(), BridgeResultData.getDownBridgeResults());
+        OutputView.printResult(gameResult, trials);
+    }
+}
