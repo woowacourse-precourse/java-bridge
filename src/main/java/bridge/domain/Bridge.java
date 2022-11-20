26f0b3bc67ce6d6855d8167a1bb683dc842eec33
3juhwan <13selfesteem91@naver.com>
2022-11-20 22:45:03 +0900

    feat(bridge): 다리 모델 구현

--- src/main/java/bridge/domain/Bridge.java
+++ src/main/java/bridge/domain/Bridge.java
@@ -1,4 +1,21 @@
 package bridge.domain;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Bridge {
+    private final List<String> bridge;
+
+    Bridge(List<String> bridgeInfo) {
+        this.bridge = bridgeInfo;
+    }
+
+    public List<Boolean> compareWithUserMovement(List<String> userMovement) {
+        List<Boolean> compareResult = new ArrayList<>();
+        for (int i = 0; i < userMovement.size(); i++) {
+            compareResult.add(userMovement.get(i) == bridge.get(i));
+        }
+        return compareResult;
+    }
+
 }
