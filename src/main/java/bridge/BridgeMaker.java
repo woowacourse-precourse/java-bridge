03f1b02a6e5ec3ef126796cf54ef45de837f4bec
haisley77 <qhfg3145@naver.com>
2022-11-20 23:32:30 +0900

    refactor(BridgeMaker): 다리 정보 초기화 위치 수정

--- src/main/java/bridge/BridgeMaker.java
+++ src/main/java/bridge/BridgeMaker.java
@@ -7,10 +7,7 @@ import java.util.List;
  * 다리의 길이를 입력 받아서 다리를 생성해주는 역할을 한다.
  */
 public class BridgeMaker {
-
-    // 다리 통행 가능 정보를 담고 있는 리스트
-    public static List<String> bridge_info;
-    // 현재 체크 중인 다리 인덱스
+    public static List<String> bridge_info = new ArrayList<>();
     public static int bridge_index = 0;
 
     private final BridgeNumberGenerator bridgeNumberGenerator;
@@ -24,7 +21,6 @@ public class BridgeMaker {
      * @return 입력받은 길이에 해당하는 다리 모양. 위 칸이면 "U", 아래 칸이면 "D"로 표현해야 한다.
      */
     public List<String> makeBridge(int size) {
-        bridge_info = new ArrayList<>(size);
         for (int i = 0; i < size; i++){
             int random_bridge_number = bridgeNumberGenerator.generate();
             if (random_bridge_number == 0){
