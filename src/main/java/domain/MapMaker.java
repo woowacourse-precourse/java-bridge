11fdf5202d832fed5b0cbe8c50a10b31f184ab81
UngHeung <t01047377477@gmail.com>
2022-11-21 05:56:11 +0900

    refactor : (MapMaker) BridgeGame과 연동할 수 있도록 코드 수정

--- src/main/java/domain/MapMaker.java
+++ src/main/java/domain/MapMaker.java
@@ -1,84 +1,76 @@
 package domain;
 
-import java.util.List;
-
 import service.InputView;
 import service.OutputView;
 
 public class MapMaker {
 	private InputView in = new InputView();
 	private OutputView out = new OutputView();
-	
-	private final List<String> bridge;
-	private StringBuilder upperBridge, lowerBridge;
-	
-	public MapMaker(List<String> bridge) {
-		this.bridge = bridge;
-		upperBridge = new StringBuilder();
-		lowerBridge = new StringBuilder();
-		
-		makeMap();
-	}
-	
-	private void makeMap() {
-		for (int i = 0; i < bridge.size(); i++) {
-			out.printGameMessage("MOVING");
-			String move = in.readMoving();
 
-			firstCheck(i);
-			
-			if (!bridgeCheck(bridge.get(i), move)) {
-				return;
-			}
-		}
+	private StringBuilder upperMap, lowerMap;
+	private int index;
+
+	public MapMaker() {
+		upperMap = new StringBuilder();
+		lowerMap = new StringBuilder();
+
+		index = 0;
 	}
-	
-	private boolean bridgeCheck(String rightCondition, String move) {
-		if(!conditionCheck(rightCondition, move)) {
-			out.printMap(getMap());
-			return false;
-		}
-		
-		out.printMap(getMap());
-		return true;
-	}
-	
-	private void firstCheck(int index) {
-		if (index != 0) {
-			upperBridge.append("|");
-			lowerBridge.append("|");
-		}
-	}
-	
-	private boolean conditionCheck(String rightCondition, String move) {
-		String condition = BridgeCondition.WRONG.getCondition();
-		
-		if(rightCondition.equals(move)) {
-			condition = BridgeCondition.RIGHT.getCondition();
-			upOrDownCheck(move, condition);
+
+	public boolean conditionCheck(String rightCondition) {
+		out.printGameMessage("MOVING");
+		String move = in.readMoving();
+
+		if (matchBridge(rightCondition, move)) {
+			out.printMap(toString());
 			return true;
 		}
-		
-		upOrDownCheck(move, condition);
+
+		out.printMap(toString());
 		return false;
 	}
-	
-	private void upOrDownCheck(String move, String condition) {
+
+	private boolean matchBridge(String rightCondition, String move) {
+		if (rightCondition.equals(move)) {
+			upOrDownCheck(move, true);
+			return true;
+		}
+
+		upOrDownCheck(move, false);
+		return false;
+	}
+
+	private void upOrDownCheck(String move, boolean condition) {
+		setPartition();
+
 		if (move.equals("U")) {
-			upperBridge.append(condition);
-			lowerBridge.append(BridgeCondition.EMPTY.getCondition());
-			
+			upperMap.append(setCondition(condition));
+			lowerMap.append(BridgeCondition.EMPTY.getCondition());
 			return;
 		}
 
-		upperBridge.append(BridgeCondition.EMPTY.getCondition());
-		lowerBridge.append(condition);
+		upperMap.append(BridgeCondition.EMPTY.getCondition());
+		lowerMap.append(setCondition(condition));
 	}
-	
-	public String getMap() {
-		String bridgeMap = "[" + upperBridge.toString()	+ "]\n"
-				+ "[" + lowerBridge.toString() + "]";
-		
-		return bridgeMap;
+
+	private String setCondition(boolean condition) {
+		if (condition) {
+			return BridgeCondition.RIGHT.getCondition();
+		}
+
+		return BridgeCondition.WRONG.getCondition();
+	}
+
+	private void setPartition() {
+		if (index != 0) {
+			upperMap.append("|");
+			lowerMap.append("|");
+		}
+
+		index++;
+	}
+
+	public String toString() {
+		return "[" + upperMap + "]\n" + "[" + lowerMap + "]";
 	}
 }
