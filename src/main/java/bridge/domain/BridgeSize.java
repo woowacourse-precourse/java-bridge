cf7ba3f8a82ceb4ba3450fbb5a396e185b2cf293
KTH-96 <taehyun960@gmail.com>
2022-11-20 20:13:36 +0900

    feat(BridgeSize): bridge size를 보유하는 클래스 생성
    다리의 크기를 변환 및 예외처리하는 기능 추가

---
+++ src/main/java/bridge/domain/BridgeSize.java
@@ -0,0 +1,23 @@
+package bridge.domain;
+
+import static bridge.utils.Constant.BRIDGE_MIN_SIZE;
+import static bridge.utils.Constant.LESS_THAN_MIN_SIZE_MESSAGE;
+
+import bridge.utils.Converter;
+
+public class BridgeSize {
+
+    private final int bridgeSize;
+
+    public BridgeSize(String bridgeSize) {
+        int size = Converter.parseToInteger(bridgeSize);
+        validGreaterThanZero(size);
+        this.bridgeSize = size;
+    }
+
+    private void validGreaterThanZero(int size) {
+        if (size < BRIDGE_MIN_SIZE) {
+            throw new IllegalArgumentException(LESS_THAN_MIN_SIZE_MESSAGE);
+        }
+    }
+}
