40f8a518b392378f9c7b613a658f5dca93b39809
miffyKing <whqudghk28@gmail.com>
2022-11-20 20:02:26 +0900

    refactor: Change generating "U", "D" list algorithm and add values to Sentence enum class

--- src/main/java/bridge/BridgeMaker.java
+++ src/main/java/bridge/BridgeMaker.java
@@ -1,5 +1,6 @@
 package bridge;
 
+import bridge.view.Sentence;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.IntStream;
@@ -21,16 +22,11 @@ public class BridgeMaker {
      * @return 입력받은 길이에 해당하는 다리 모양. 위 칸이면 "U", 아래 칸이면 "D"로 표현해야 한다.
      */
     public List<String> makeBridge(int size) {
-//        List<Integer> generatedAnswerNumeric = new ArrayList<>();
-//        for (int i = 0; i < size; i++) {
-//            generatedAnswerNumeric.add(bridgeNumberGenerator.generate());
-//        }
-//        return changeToUD(generatedAnswerNumeric);
-        List<String> generatedAnswer = new ArrayList<>();
+        List<Integer> generatedAnswerNumeric = new ArrayList<>();
         for (int i = 0; i < size; i++) {
-            generatedAnswer.add((changeToUD(bridgeNumberGenerator.generate())));
+            generatedAnswerNumeric.add(bridgeNumberGenerator.generate());
         }
-        return generatedAnswer;
+        return changeToUD(generatedAnswerNumeric);
     }
 
     private List<String> changeToUD(List<Integer> generatedValue) {
@@ -43,10 +39,9 @@ public class BridgeMaker {
 
     private static void addChunkToConverted(List<String> convertedValue, Integer integer) {
         if (integer == 1) {
-            convertedValue.add("U");
+            convertedValue.add(Sentence.UP_CHUNK.getValue());
         } else if (integer == 0) {
-            convertedValue.add("D");
+            convertedValue.add(Sentence.DOWN_CHUNK.getValue());
         }
     }
-
 }
--- src/main/java/bridge/view/Sentence.java
+++ src/main/java/bridge/view/Sentence.java
@@ -4,6 +4,9 @@ public enum Sentence {
     INPUT_MONEY("구입금액을 입력해 주세요."),
     YOU_PURCHASED("\n%d개를 구매했습니다."),
 
+    UP_CHUNK("U"),
+    DOWN_CHUNK("D"),
+
     LENGTH_NOT_IN_RANGE("[ERROR] Input is not in range");
 
     private final String s;
