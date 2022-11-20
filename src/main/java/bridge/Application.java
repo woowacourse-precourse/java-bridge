25488af173d866a88fc84c565730978d73774fa9
Ran <lan4250@naver.com>
2022-11-20 22:53:05 +0900

    FEAT/ 게임 시작 메서드 startBridgeGame()

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -2,7 +2,17 @@ package bridge;
 
 public class Application {
 
+
+    public static void startBridgeGame() {
+        System.out.println("다리 건너기 게임을 시작합니다.");
+    }
+
+
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+
+        startBridgeGame();
+
+
     }
 }
