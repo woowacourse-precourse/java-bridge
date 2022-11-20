ed734bd336ae3ffac2252426d78f2df30622c7a0
Haaae <101785628+Haaae@users.noreply.github.com>
2022-11-21 07:29:00 +0900

    chore(OutputViewMessage, OutputViewConstants) : 출력 문구 상수를 constants.message 패키지로 이동

---
+++ src/main/java/bridge/constant/message/OutputViewMessage.java
@@ -0,0 +1,11 @@
+package bridge.constant.message;
+
+public class OutputViewMessage {
+
+    public static final String FINAL_GAME_RESULT = "최종 게임 결과";
+    public static final String GAME_SUCCESS_OR_NOT= "게임 성공 여부: ";
+    public static final String TOTAL_NUMBER_OF_ATTEMPTS = "총 시도한 횟수: ";
+    public static final String GAME_START_NOTICE = "다리 건너기 게임을 시작합니다.";
+
+    private OutputViewMessage() {}
+}
