6920c2bf6ec575ac98450436df32311a992a46d2
Invidam <hansupark0208@gmail.com>
2022-11-20 21:20:19 +0900

    refactor(game-status-view): 게임 진행 과정 출력 객체 이름 변경

--- src/main/java/bridge/view/GameStatusView.java
+++ src/main/java/bridge/view/GameStatusView.java
@@ -9,21 +9,21 @@ import bridge.domain.bridge.BridgeAndPasser;
 import bridge.value.BridgeCharacter;
 import java.util.List;
 
-public class BridgeAndPasserView {
+public class GameStatusView {
 
     private final List<BridgeCharacter> bridgeCharacters;
     private final BridgeLocation endLocation;
 
-    public BridgeAndPasserView(BridgeAndPasser bridgeAndPasser) {
+    public GameStatusView(BridgeAndPasser bridgeAndPasser) {
         this.bridgeCharacters = bridgeAndPasser.getBridge().characters();
         this.endLocation = bridgeAndPasser.getBridgePasser().getLocation();
     }
 
-    public static BridgeAndPasserView makeGameStatusView(BridgeAndPasser bridgeAndPasser) {
-        return new BridgeAndPasserView(bridgeAndPasser);
+    public static GameStatusView makeGameStatusView(BridgeAndPasser bridgeAndPasser) {
+        return new GameStatusView(bridgeAndPasser);
     }
 
-    public String render() {
+    public String renderStatus() {
         StringBuffer stringBuffer = new StringBuffer();
 
         fillLine(stringBuffer, UP);
@@ -36,19 +36,40 @@ public class BridgeAndPasserView {
     private void fillLine(StringBuffer stringBuffer, BridgeCharacter currLocationCharacter) {
         BridgeLocation currLocation = initBridgeLocation();
 
-        stringBuffer.append("[");
-        while (!currLocation.equals(endLocation)) {
-            stringBuffer.append(characterFor(currLocationCharacter, bridgeCharacters, currLocation));
-            stringBuffer.append("|");
-
-            currLocation =  currLocation.next();
-        }
-        popLastCharacter(stringBuffer);
+        fillStartCharacter(stringBuffer);
+        fillBridge(stringBuffer, currLocationCharacter, currLocation);
 //        if (bridgeCharacters.get(endLocation.value()) == currLocationCharacter) {
 //            stringBuffer.append("X");
 //        }
 
+        fillEndCharacter(stringBuffer);
+    }
+
+    private void fillBridge(StringBuffer stringBuffer, BridgeCharacter currLocationCharacter,
+                           BridgeLocation currLocation) {
+        while (!currLocation.equals(endLocation)) {
+            fillBridgeCharacter(stringBuffer, currLocationCharacter, currLocation);
+            fillSeparator(stringBuffer);
+            currLocation =  currLocation.next();
+        }
+        popLastCharacter(stringBuffer);
+    }
+
+    private void fillBridgeCharacter(StringBuffer stringBuffer, BridgeCharacter currLocationCharacter,
+                           BridgeLocation currLocation) {
+        stringBuffer.append(characterFor(currLocationCharacter, bridgeCharacters, currLocation));
+    }
+
+    private static void fillEndCharacter(StringBuffer stringBuffer) {
         stringBuffer.append("]");
+    }
+
+    private static void fillSeparator(StringBuffer stringBuffer) {
+        stringBuffer.append("|");
+    }
+
+    private static void fillStartCharacter(StringBuffer stringBuffer) {
+        stringBuffer.append("[");
     }
 
     private static void popLastCharacter(StringBuffer stringBuffer) {
--- src/main/java/bridge/view/OutputView.java
+++ src/main/java/bridge/view/OutputView.java
@@ -12,8 +12,8 @@ public class OutputView {
      * <p>
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void printMap(BridgeAndPasserView bridgeAndPasserView) {
-        System.out.println(bridgeAndPasserView.render());
+    public void printMap(GameStatusView gameStatusView) {
+        System.out.println(gameStatusView.renderStatus());
     }
 
     public void printMessage(SystemMessage message) {
