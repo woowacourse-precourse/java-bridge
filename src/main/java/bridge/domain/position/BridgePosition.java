1cf4ad49f7c4aac318f3905daf68e280bfa7f2f4
JaehongDev <println@kakao.com>
2022-11-20 20:20:15 +0900

    feature (BridgePosition) : 사용자의 입력을 명령어로 바꾸는 기능 추가

--- src/main/java/bridge/domain/position/BridgePosition.java
+++ src/main/java/bridge/domain/position/BridgePosition.java
@@ -7,6 +7,9 @@ public enum BridgePosition {
     DOWN("D", 0);
 
     public static final String ERROR_NOT_FOUND_POSITION_VALUE = "[ERROR] 0과 1이외의 값을 가질 수는 없습니다.";
+    public static final String ERROR_DID_NOT_FOUND_CODE = "[ERROR] 입력 할 수 없는 위치값입니다.";
+
+
     private final String positionCode;
     private final Integer positionValue;
 
@@ -16,11 +19,18 @@ public enum BridgePosition {
         this.positionValue = value;
     }
 
-    public static String convertValueToCode(Integer input) {
+    public static String convertValueToCode(final Integer input) {
         return Arrays.stream(BridgePosition.values())
                 .filter(position -> position.positionValue.equals(input))
                 .findAny()
                 .orElseThrow(() -> new IllegalArgumentException(ERROR_NOT_FOUND_POSITION_VALUE))
                 .positionCode;
+    }
+
+    public static BridgePosition convertCodeToPosition(final String code) {
+        return Arrays.stream(BridgePosition.values())
+                .filter(position -> position.positionCode.equals(code))
+                .findAny()
+                .orElseThrow(() -> new IllegalArgumentException(ERROR_NOT_FOUND_POSITION_VALUE));
     }
 }
