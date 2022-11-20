f8dc12b8fd11a7fca59da8d90ccf236847aa19b5
jungwoo <rlawjddn103@naver.com>
2022-11-20 21:13:24 +0900

    feat(InputViewValidation): 입력이 'U' 혹은 'D'인지 확인하고 아니면 예외 처리 하는 기능

--- src/main/java/bridge/ui/InputViewValidation.java
+++ src/main/java/bridge/ui/InputViewValidation.java
@@ -7,7 +7,7 @@ public class InputViewValidation {
     private static final Pattern NUMBER_PATTERN = Pattern.compile("^[0-9]*$");
     private static final String ERROR_NOT_INTEGER = "[ERROR] 숫자가 아닙니다.";
     private static final String ERROR_NOT_IN_RANGE = "[ERROR] 숫자가 범위를 벗어났습니다.";
-
+    private static final String ERROR_NOT_U_OR_D = "[ERROR] U나 D를 입력하셔야합니다.";
 
     public static void validateIsInteger(String input) {
         if (!NUMBER_PATTERN.matcher(input).find()) {
@@ -19,6 +19,12 @@ public class InputViewValidation {
         int number = Integer.parseInt(input);
         if (number < 3 || number > 20) {
             throw new IllegalArgumentException(ERROR_NOT_IN_RANGE);
+        }
+    }
+
+    public static void validateIsUorD(String input){
+        if (!(input.equals("U") || input.equals("D"))) {
+            throw new IllegalArgumentException(ERROR_NOT_U_OR_D);
         }
     }
 }
