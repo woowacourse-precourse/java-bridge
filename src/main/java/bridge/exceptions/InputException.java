5ea21247baf6f98eb0135a1146423054547c34b5
hojunking96 <junsong96@naver.com>
2022-11-21 00:30:36 +0900

    refactor(InputException.java) : 메소드 생성하여 코드 분리

--- src/main/java/bridge/exceptions/InputException.java
+++ src/main/java/bridge/exceptions/InputException.java
@@ -1,7 +1,14 @@
 package bridge.exceptions;
 
 public class InputException {
-    public static void bridgeSizeNumberCheck(String bridgeSizeInput) {
+    public static int bridgeSizeInputError(String bridgeSizeInput) {
+        bridgeSizeNumberError(bridgeSizeInput);
+        int bridgeSize = Integer.parseInt(bridgeSizeInput);
+        bridgeSizeRangeError(bridgeSize);
+        return bridgeSize;
+    }
+
+    public static void bridgeSizeNumberError(String bridgeSizeInput) {
         for (int i = 0; i < bridgeSizeInput.length(); i++) {
             if (!Character.isDigit(bridgeSizeInput.charAt(i))) {
                 throw new IllegalArgumentException();
@@ -9,19 +16,19 @@ public class InputException {
         }
     }
 
-    public static void bridgeSizeRangeCheck(int bridgeSize) {
+    public static void bridgeSizeRangeError(int bridgeSize) {
         if (bridgeSize < 3 || bridgeSize > 20) {
             throw new IllegalArgumentException();
         }
     }
 
-    public static void movingInputErrorCheck(String movingInput) {
+    public static void movingInputError(String movingInput) {
         if (!movingInput.equals("U") && !movingInput.equals("D")) {
             throw new IllegalArgumentException();
         }
     }
 
-    public static void gameCommandInputErrorCheck(String gameCommandInput) {
+    public static void gameCommandInputError(String gameCommandInput) {
         if (!gameCommandInput.equals("R") && !gameCommandInput.equals("Q")) {
             throw new IllegalArgumentException();
         }
