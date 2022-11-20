11bb67a2a7ece34872a5bb0edf823db145b46782
olrlobt <317tmdgjs@naver.com>
2022-11-20 21:29:28 +0900

    feat: 건널 수 있는 칸을 U,D로 나타낸 다리를 생성

--- src/main/java/bridge/domain/Bridge.java
+++ src/main/java/bridge/domain/Bridge.java
@@ -1,19 +1,33 @@
 package bridge.domain;
 
+import bridge.BridgeMaker;
+import bridge.BridgeNumberGenerator;
+import bridge.BridgeRandomNumberGenerator;
+import java.util.List;
+
 public class Bridge {
 
     private final int size;
     private final int MAX_SIZE = 20;
     private final int MIN_SIZE = 3;
+    private List<String> bridge;
 
     public Bridge(int size) {
         validateSize(size);
         this.size = size;
+        this.bridge = buildBridge();
     }
 
     private void validateSize(int size) {
         if (size < MIN_SIZE || size > MAX_SIZE) {
             throw new IllegalArgumentException();
         }
+    }
+
+    public List<String> buildBridge() {
+        BridgeNumberGenerator bridgeNumberGenerator = new BridgeRandomNumberGenerator();
+        BridgeMaker bridgeMaker = new BridgeMaker(bridgeNumberGenerator);
+
+        return bridgeMaker.makeBridge(size);
     }
 }
