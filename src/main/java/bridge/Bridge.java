29bb4408470f3e6cab54df3a6128c5cbb4f2f435
dlrghks2090 <dlrghks2090@gmail.com>
2022-11-20 22:19:40 +0900

    refactor: 다리를 나타내는 기능에 만들었던 예외처리 메서드 삭제

--- src/main/java/bridge/Bridge.java
+++ src/main/java/bridge/Bridge.java
@@ -1,21 +1,14 @@
 package bridge;
 
-public class Bridge {
+import bridge.validate.Exception;
 
-    private static final int MINIMUM_BRIDGE_LENGTH = 3;
-    private static final int MAXIMUM_BRIDGE_LENGTH = 20;
+public class Bridge {
 
     private final int bridgeLength;
 
     public Bridge(int bridgeLength) {
-        validateNumberRange(bridgeLength);
+        Exception.validateThreeToTwenty(bridgeLength);
         this.bridgeLength = bridgeLength;
-    }
-
-    private void validateNumberRange(int bridgeLength) {
-        if (bridgeLength < MINIMUM_BRIDGE_LENGTH || bridgeLength >MAXIMUM_BRIDGE_LENGTH) {
-            throw new IllegalArgumentException(ErrorMessage.BRIDGE_LENGTH_IS_THREE_TO_TWENTY.getErrorMessage());
-        }
     }
 
     public int getBridgeLength() {
--- src/main/java/bridge/validate/Exception.java
+++ src/main/java/bridge/validate/Exception.java
@@ -4,19 +4,22 @@ import bridge.ErrorMessage;
 
 public class Exception {
 
-    private void validateThreeToTwenty(int bridgeCount) {
-        if (bridgeCount < 3 || bridgeCount > 20) {
+    private static final int MINIMUM_BRIDGE_LENGTH = 3;
+    private static final int MAXIMUM_BRIDGE_LENGTH = 20;
+
+    public static void validateThreeToTwenty(int bridgeCount) {
+        if (bridgeCount < MINIMUM_BRIDGE_LENGTH || bridgeCount > MAXIMUM_BRIDGE_LENGTH) {
             throw new IllegalArgumentException(ErrorMessage.BRIDGE_LENGTH_IS_THREE_TO_TWENTY.getErrorMessage());
         }
     }
 
-    private void validateUpOrDown(String moveBridgeSpace) {
+    public static void validateUpOrDown(String moveBridgeSpace) {
         if (!moveBridgeSpace.equals("U") && !moveBridgeSpace.equals("D")) {
             throw new IllegalArgumentException(ErrorMessage.MOVE_BRIDGE_SPACE_IS_U_OR_D.getErrorMessage());
         }
     }
 
-    private void validateRetryOrQuit(String retryAnswer) {
+    public static void validateRetryOrQuit(String retryAnswer) {
         if (!retryAnswer.equals("R") && !retryAnswer.equals("Q")) {
             throw new IllegalArgumentException(ErrorMessage.RETRY_ANSWER_IS_R_OR_Q.getErrorMessage());
         }
