5e6ec055d81ef859fb50f3b3901b4745583d5345
roasted-penguin <swkwon25@hanyang.ac.kr>
2022-11-21 01:38:53 +0900

    feat : BridgeMaker - makeBridge

--- src/main/java/bridge/service/BridgeMaker.java
+++ src/main/java/bridge/service/BridgeMaker.java
@@ -18,5 +18,12 @@ public class BridgeMaker {
      * @return 입력받은 길이에 해당하는 다리 모양. 위 칸이면 "U", 아래 칸이면 "D"로 표현해야 한다.
      */
     public List<String> makeBridge(int size) {
+        List<String> result = new ArrayList<>();
+        for(int i=0;i<size;i++){
+            int UD = bridgeNumberGenerator.generate();
+            if(UD==0) result.add("U");
+            if(UD==1) result.add("D");
+        }
+        return result;
     }
 }
