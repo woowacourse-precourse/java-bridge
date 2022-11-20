4f6edcbbb4356e2fcc404a163712c660590cf13c
taekwon-dev <taekwon_dev@naver.com>
2022-11-20 15:11:41 +0900

    fix: (Constants) 출력 기대 값에 맞춰 O, X, 공백 표시 값 수정
    - "O" -> " O "
    - "X" -> " X "
    - " " -> "   "

--- src/main/java/bridge/domain/Constants.java
+++ src/main/java/bridge/domain/Constants.java
@@ -10,8 +10,8 @@ public class Constants {
     public static final boolean BRIDGE_GAME_RESULT_INIT = false;
     public static final String UP = "U";
     public static final String DOWN = "D";
-    public static final String O = "O";
-    public static final String X = "X";
-    public static final String SPACE = " ";
+    public static final String O = " O ";
+    public static final String X = " X ";
+    public static final String SPACE = "   ";
     public static final String DELIMITER = "|";
 }
