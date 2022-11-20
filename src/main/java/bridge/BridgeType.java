9cc46244f3e21ae3ed2f2f565c975456a60f6c00
SeokyungKim <kskyung0624@gmail.com>
2022-11-20 12:34:00 +0900

    refactor : enum으로 타입을 강제하기 위해 BridgeType enum 도입
    List<String>으로 다리 모양을 저장하지 않고 List<BridgeType>으로 Bridge를 저장해서 타입을 강제할 수 있도록 한다

---
+++ src/main/java/bridge/BridgeType.java
@@ -0,0 +1,31 @@
+package bridge;
+
+import java.util.Arrays;
+import java.util.List;
+
+public enum BridgeType {
+    U("U"), D("D");
+
+    private String label;
+
+    BridgeType(String bridgeType) {
+        this.label = bridgeType;
+    }
+
+    static BridgeType of(String bridgeType) {
+        return Arrays.stream(values())
+                .filter((v) -> bridgeType.equals(v))
+                .findFirst()
+                .orElseThrow(() -> new IllegalArgumentException("[ERROR] 해당하는 bridgeType이 존재하지 않습니다."));
+    }
+
+    static List<BridgeType> toBridge(List<String> target) {
+        return Arrays.asList(
+                target.stream()
+                        .map((str) -> of(str))
+                        .toArray(BridgeType[]::new)
+        );
+
+    }
+
+}
