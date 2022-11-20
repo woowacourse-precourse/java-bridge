41cb1a545b16620031a2f46c7bb4a88c6e6988fe
Jenn Kim <grdd0419@gmail.com>
2022-11-20 18:35:00 +0900

    feat: 다리 이동 결과를 저장하는 클래스 추가

---
+++ src/main/java/bridge/Domain/UserBridge.java
@@ -0,0 +1,21 @@
+package bridge.Domain;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class UserBridge {
+    private List<String> spaces;
+
+    public UserBridge() {
+        this.spaces = new ArrayList<>();
+    }
+
+    public String getSpaceByLocation(int location) {
+        return spaces.get(location);
+    }
+
+    public void saveSpaceResult(String move) {
+        spaces.add(move);
+    }
+
+}
