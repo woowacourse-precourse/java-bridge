b135167cbaa3b1613a534179fea76a8f1eef949b
nylonmusk <106235834+nylonmusk@users.noreply.github.com>
2022-11-20 21:23:38 +0900

    refactor(NumberConstant): 메소드명 변경

---
+++ src/main/java/constant/NumberConstant.java
@@ -0,0 +1,21 @@
+package constant;
+
+public enum NumberConstant {
+    RANDOM_LOWER_INCLUSIVE(0),
+    RANDOM_UPPER_INCLUSIVE(1),
+    UP_SIDE(1),
+    DOWN_SIDE(0),
+    BRIDGE_SIZE_START_INCLUSIVE(3),
+    BRIDGE_SIZE_END_INCLUSIVE(20),
+    FIRST_TRY(1);
+
+    public final int numberConstant;
+
+    NumberConstant(final int numberConstant) {
+        this.numberConstant = numberConstant;
+    }
+
+    public int getConstant() {
+        return numberConstant;
+    }
+}
