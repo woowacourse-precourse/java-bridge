c97885225c9e2553180f6617c44dfe234aa717dc
SeokyungKim <kskyung0624@gmail.com>
2022-11-20 22:22:57 +0900

    refactor(Application) : Exception을 IllegalArgumentException으로 명확하게 받도록 처리한다

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -6,7 +6,7 @@ public class Application {
         // TODO: 프로그램 구현
         try {
             new GameMachine().run();
-        } catch (Exception e) {
+        } catch (IllegalArgumentException e) {
             System.out.println(e.getMessage());
         }
     }
