8ebe4f2e53e2a7ae93dfc551f6e7f72d24346951
Nitroblack1 <109022435+Nitroblack1@users.noreply.github.com>
2022-11-21 00:38:45 +0900

    feat(BridgeGame) : add boolean varivable for game status and getter of that

--- src/main/java/bridge/Domain/BridgeGame.java
+++ src/main/java/bridge/Domain/BridgeGame.java
@@ -15,6 +15,7 @@ public class BridgeGame {
     public final BridgeNumberGenerator bridgeNumberGenerator = new BridgeRandomNumberGenerator();
     public final BridgeMaker bridgeMaker = new BridgeMaker(bridgeNumberGenerator);
     public final InputView inputView = new InputView();
+    private boolean isGameSucceed;
 
     public BridgeGame() {
         System.out.println(FrontMan.BRIDGE_GAME_IS_BEGINNING + "\n");
@@ -40,5 +41,9 @@ public class BridgeGame {
      * 재시작을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
     public void retry() {
+    }
+
+    public boolean getIsGameSucceed() {
+        return this.isGameSucceed;
     }
 }
