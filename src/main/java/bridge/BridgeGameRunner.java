ac8bfb104844ef62f5438d2a88a5b26c0992fc44
kanghyki <kanghyki@gmail.com>
2022-11-20 21:16:06 +0900

    comment: BridgeGameRunner 메서드 설명 추가

--- src/main/java/bridge/BridgeGameRunner.java
+++ src/main/java/bridge/BridgeGameRunner.java
@@ -5,12 +5,18 @@ public class BridgeGameRunner {
     private OutputView output;
     BridgeGame game;
 
+    /**
+     * 생성자
+     */
     public BridgeGameRunner() {
         input = new InputView();
         output = new OutputView();
         game = new BridgeGame();
     }
 
+    /**
+     * 브릿지 게임의 전체 프로세스를 진행하는 메서드
+     */
     public void run() {
         output.printStartMessage();
         game.setBridge(new Bridge(game.getPlayer(), input.readBridgeSize()));
@@ -24,6 +30,10 @@ public class BridgeGameRunner {
         output.printResult(game);
     }
 
+    /**
+     * 라운드를 다시 시도할건지 묻는 메서드
+     * @return 다시 시도한다면 true 아니라면 false
+     */
     private boolean askRetry() {
         if (input.readGameCommand().equals(BridgeGame.CMD_RETRY)) {
             return true;
@@ -31,6 +41,9 @@ public class BridgeGameRunner {
         return false;
     }
 
+    /**
+     * 한 라운드가 끝날때까지 움직임을 반복하는 메서드
+     */
     private void repeatMove() {
         while (game.isPlayerAlive() && !game.isSuccess()) {
             game.move(input.readMoving());
