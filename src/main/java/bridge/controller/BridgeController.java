bc8aa00552d7c1dea229eca0a4b5ebf489a6da5f
keon0711 <fnc6488@gmail.com>
2022-11-21 00:46:43 +0900

    feat(bridge): 다리 게임 실행을 위한 controller 추가

---
+++ src/main/java/bridge/controller/BridgeController.java
@@ -0,0 +1,62 @@
+package bridge.controller;
+
+import bridge.domain.BridgeGame;
+import bridge.domain.Player;
+import bridge.view.InputView;
+import bridge.view.OutputView;
+
+public class BridgeController {
+
+    BridgeGame bridgeGame;
+
+    public void run() {
+        InputView.printStartMessage();
+        bridgeGame = makeSizedBridge();
+        Player player = bridgeGame.getPlayer();
+        playBridgeGame(player);
+        OutputView.printResult(bridgeGame);
+    }
+
+    private void playBridgeGame(Player player) {
+        do {
+            playUntilFailure(player);
+        } while (!bridgeGame.isReached() && retry());
+    }
+
+    private void playUntilFailure(Player player) {
+        do {
+            move();
+            OutputView.printMap(bridgeGame);
+        } while (!bridgeGame.isReached() && player.getMoving());
+    }
+
+    private BridgeGame makeSizedBridge() {
+        try {
+            int size = InputView.readBridgeSize();
+            return new BridgeGame(size);
+        } catch (IllegalArgumentException e) {
+            OutputView.printErrorMessage(e);
+            return makeSizedBridge();
+        }
+    }
+
+    private boolean move() {
+        try {
+            String moveCommand = InputView.readMoving();
+            return bridgeGame.move(moveCommand);
+        } catch (IllegalArgumentException e) {
+            OutputView.printErrorMessage(e);
+            return move();
+        }
+    }
+
+    private boolean retry() {
+        try {
+            String gameCommand = InputView.readGameCommand();
+            return bridgeGame.retry(gameCommand);
+        } catch (IllegalArgumentException e) {
+            OutputView.printErrorMessage(e);
+            return retry();
+        }
+    }
+}
