951571b734b6d29c6976c99b553c6c65faee3829
fakerdeft <whakswp123@naver.com>
2022-11-20 23:52:18 +0900

    feat: 이동할 칸 에러 메세지 추가

---
+++ src/main/java/bridge/util/ConstantMessage.java
@@ -0,0 +1,6 @@
+package bridge.util;
+
+public class ConstantMessage {
+    public static final String BRIDGE_LENGTH_ERROR_MESSAGE = "[ERROR] 3이상 20이하의 숫자만 입력하세요.";
+    public static final String MOVING_BLOCK_ERROR_MESSAGE = "[ERROR] 'U' 또는 'D'만 입력하세요.";
+}
