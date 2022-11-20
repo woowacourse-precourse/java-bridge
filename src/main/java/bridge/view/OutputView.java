4d2bced351db73453388e672f586608f48bcfbe9
LeeHaiLim <hailim1997@gmail.com>
2022-11-21 03:04:39 +0900

    feat(view): implement class output view
    Added methods
    - print map
    - partial map

---
+++ src/main/java/bridge/view/OutputView.java
@@ -0,0 +1,45 @@
+package bridge.view;
+
+import bridge.Constants;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+/**
+ * 사용자에게 게임 진행 상황과 결과를 출력하는 역할을 한다.
+ */
+public class OutputView {
+
+    /**
+     * 현재까지 이동한 다리의 상태를 정해진 형식에 맞춰 출력한다.
+     * <p>
+     * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
+     */
+    public void printMap(List<String> moves, boolean isFail) {
+        List<String> upperMap = partialMap(moves, isFail, Constants.CODE_UP);
+        List<String> lowerMap = partialMap(moves, isFail, Constants.CODE_DOWN);
+
+        System.out.println(Map.START.code() + String.join(Map.JOIN.code(), upperMap) + Map.END.code());
+        System.out.println(Map.START.code() + String.join(Map.JOIN.code(), lowerMap) + Map.END.code());
+    }
+
+    private List<String> partialMap(List<String> moves, boolean isFail, String code) {
+        return IntStream.range(0, moves.size()).mapToObj(index -> {
+            if (isFail && index == moves.size() - 1 && moves.get(index).equals(code)) {
+                return Map.MOVE_FAIL.code();
+            }
+            if (moves.get(index).equals(code)) {
+                return Map.MOVE_SUCCESS.code();
+            }
+            return Map.MOVE_EMPTY.code();
+        }).collect(Collectors.toList());
+    }
+
+    /**
+     * 게임의 최종 결과를 정해진 형식에 맞춰 출력한다.
+     * <p>
+     * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
+     */
+    public void printResult() {
+    }
+}
