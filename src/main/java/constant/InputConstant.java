e6e80d6cd0a46a420a9a912554941b4e6111c4d8
nylonmusk <106235834+nylonmusk@users.noreply.github.com>
2022-11-21 00:35:20 +0900

    refactor(InputConstant): 메소드명 getConstant로 변경

--- src/main/java/constant/InputConstant.java
+++ src/main/java/constant/InputConstant.java
@@ -12,7 +12,7 @@ public enum InputConstant {
         this.inputConstant = inputConstant;
     }
 
-    public String getInputConstant() {
+    public String getConstant() {
         return inputConstant;
     }
 
