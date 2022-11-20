69f59051a705472c84f7266b495c086421b711d7
yejin <kwonyj1022@naver.com>
2022-11-21 01:16:26 +0900

    feat: Application 구현 (Run 실행 과정에서 발생하는 예외들 처리)

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -3,6 +3,11 @@ package bridge;
 public class Application {
 
     public static void main(String[] args) {
+        try{
+            new Run();
+        } catch(IllegalArgumentException e){
+            System.out.println(e.getMessage());
+        }
         // TODO: 프로그램 구현
     }
 }
