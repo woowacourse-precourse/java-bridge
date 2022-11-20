498dace7376c09505581895bfdb8c5d4ea650807
namtndus <rnrmf0721@gmail.com>
2022-11-20 14:34:46 +0900

    feat(Application): main함수 기능 구현
    'readBridgeSize함수를 실행 후, 반환값을 bridgeSize에 저장'를 구현

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -10,5 +10,6 @@ public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
         outputView.printGameStartMessage();
+        int brideSize = inputView.readBridgeSize();
     }
 }
