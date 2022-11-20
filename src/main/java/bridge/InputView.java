937776d494e50205febeecd05fe997de5cf0a7ae
Sangkyun Seo <tkdrbs0802@naver.com>
2022-11-20 15:26:23 +0900

    feat: 다리 생성 메서드 작성

--- src/main/java/bridge/InputView.java
+++ src/main/java/bridge/InputView.java
@@ -12,6 +12,7 @@ public class InputView {
         int bridgeLength = 0;
         System.out.println("다리의 길이를 입력해주세요.");
         bridgeLength = Integer.parseInt(Console.readLine());
+        System.out.println(bridgeLength);
         return bridgeLength;
     }
 
@@ -22,6 +23,7 @@ public class InputView {
         String moveBridge = "";
         System.out.println("이동할 칸을 선택해주세요. (위: U, 아래: D)");
         moveBridge = Console.readLine();
+        System.out.println(moveBridge);
         return moveBridge;
     }
 
@@ -32,6 +34,7 @@ public class InputView {
         String gameCommand = "";
         System.out.println("게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)");
         gameCommand = Console.readLine();
+        System.out.println(gameCommand);
         return gameCommand;
     }
 }
