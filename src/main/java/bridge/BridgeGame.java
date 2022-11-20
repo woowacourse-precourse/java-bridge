f031fcf1c406d857ee7a58edf85624bae9b16dcb
qdeu22 <qdeu22@gmail.com>
2022-11-20 13:50:35 +0900

    feat(bridge): 사용자가 입력한 값과 무작위로 저장된 다리의 값을 비교하는 기능 구현

--- src/main/java/bridge/BridgeGame.java
+++ src/main/java/bridge/BridgeGame.java
@@ -1,16 +1,31 @@
 package bridge;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 /**
  * 다리 건너기 게임을 관리하는 클래스
  */
 public class BridgeGame {
+
+    private List<String> bridge;
+
+    public BridgeGame(List<String> bridge) {
+        this.bridge = bridge;
+    }
 
     /**
      * 사용자가 칸을 이동할 때 사용하는 메서드
      * <p>
      * 이동을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void move() {
+    public boolean move(int index, String moving) {
+        if (bridge.get(index).equals(moving)) {
+            return true;
+        }
+        return false;
     }
 
     /**
@@ -18,6 +33,4 @@ public class BridgeGame {
      * <p>
      * 재시작을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void retry() {
-    }
 }
