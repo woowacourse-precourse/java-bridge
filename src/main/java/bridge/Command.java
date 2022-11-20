35dade41b106b3afdb313f479835902f28c52bc8
KYR <freekyr7529@gmail.com>
2022-11-20 21:53:00 +0900

    refactor : Enum MoveRule을 Command로 변경
    MoveRule의 이름을 변경하고 String형 상수를 관리하도록 대상을 넓힘

---
+++ src/main/java/bridge/Command.java
@@ -0,0 +1,16 @@
+package bridge;
+
+public enum Command {
+    MOVE_UP("U"),
+    MOVE_DOWN("D"),
+    DO_RETRY("R"),
+    DO_QUIT("Q");
+
+    private final String command;
+
+    Command(String command) { this.command = command; }
+
+    public String getCommand() {
+        return command;
+    }
+}
