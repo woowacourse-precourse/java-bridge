11b9a8713741b990574d663d1ae1f9b724d593c0
JaehongDev <println@kakao.com>
2022-11-20 19:19:37 +0900

    refactor (InputView, OutputView) : IOPrinter를 상속받아 사용하도록 변경

--- src/main/java/bridge/view/OutputView.java
+++ src/main/java/bridge/view/OutputView.java
@@ -3,7 +3,7 @@ package bridge.view;
 /**
  * 사용자에게 게임 진행 상황과 결과를 출력하는 역할을 한다.
  */
-public class OutputView {
+public class OutputView extends IOPrinter {
 
     /**
      * 현재까지 이동한 다리의 상태를 정해진 형식에 맞춰 출력한다.
@@ -19,5 +19,9 @@ public class OutputView {
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
     public void printResult() {
+    }
+
+    public void printGameStartMessage() {
+        println("다리 건너기 게임을 시작합니다.");
     }
 }
