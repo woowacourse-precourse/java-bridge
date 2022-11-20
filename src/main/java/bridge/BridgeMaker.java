ef5fac09f30e746d67906835a4bb3684d6106c42
Dasol Jung <greteljds0251@gmail.com>
2022-11-21 00:31:56 +0900

    feat: 다리 모양 만들기

--- src/main/java/bridge/BridgeMaker.java
+++ src/main/java/bridge/BridgeMaker.java
@@ -1,6 +1,9 @@
 package bridge;
 
+import java.util.ArrayList;
 import java.util.List;
+
+import static org.assertj.core.util.Lists.newArrayList;
 
 /**
  * 다리의 길이를 입력 받아서 다리를 생성해주는 역할을 한다.
@@ -18,6 +21,21 @@ public class BridgeMaker {
      * @return 입력받은 길이에 해당하는 다리 모양. 위 칸이면 "U", 아래 칸이면 "D"로 표현해야 한다.
      */
     public List<String> makeBridge(int size) {
+        List<String> bridgeShape = new ArrayList<>();
+        for(int i = 0; i < size; i++) {
+            int number = bridgeNumberGenerator.generate();
+            bridgeShape.add(getGenerate(number));
+        }
+        return bridgeShape;
+    }
+
+    public String getGenerate(int number) {
+        if (number == 0) {
+            return "D";
+        }
+        if (number == 1) {
+            return "U";
+        }
         return null;
     }
 }
