e0e6f19948ef0825ce48cc40e8a3875c57339092
황민주 <minjoo@hwangminjooui-MacBookAir.local>
2022-11-20 13:37:51 +0900

    refactor(OutputView) printMap (src/main/java/bridge/view/OutputView.java)
    printMap 메소드 수정:
    -1과 0 대신에 enum의 값으로부터 1(UP)과 0(DOWN)을 가져옴

--- src/main/java/bridge/view/OutputView.java
+++ src/main/java/bridge/view/OutputView.java
@@ -27,7 +27,7 @@ public class OutputView {
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
     public void printMap(List<String> totalMove, int curLocation, List <String> bridge) {
-        for(int i=1; i>=0; i--){
+        for(int i=Move.UP.getNum(); i>=Move.DOWN.getNum(); i--){
             String _curMove = MOVE_NAME.get(i);
             String curMove = Move.valueOf(_curMove).getMoveType();
             String mapResult = createMapByLine(totalMove, curLocation, bridge, curMove);
