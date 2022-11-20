32201f680864d66600bde574b7ab8f6ce9930246
Life is a ninanino <tvxqinak@naver.com>
2022-11-21 02:56:31 +0900

    refactor : static으로 변경

--- src/main/java/bridge/view/InputView.java
+++ src/main/java/bridge/view/InputView.java
@@ -22,14 +22,14 @@ public class InputView {
     /**
      * 사용자가 이동할 칸을 입력받는다.
      */
-    public String readMoving() {
+    public static String readMoving() {
         return Validator.validateMove(input());
     }
 
     /**
      * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
      */
-    public String readGameCommand() {
+    public static String readGameCommand() {
         return Validator.validateRestartOrQuit(input());
     }
 
