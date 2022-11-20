edb31beec3eadf6f57d6765c9ef535c3e2cdfb7d
Ji-Un-Gil <wldns2577@gmail.com>
2022-11-20 23:18:35 +0900

    feat(Application.java): start function Added function: - 게임 시작 시 화면 구성 및 다리를 생성하는 기능 구현

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -2,7 +2,19 @@ package bridge;
 
 public class Application {
 
+    private static InputView input;
+    private static OutputView output;
+    private static BridgeGame game;
+
+    /**
+     * 게임 시작 화면 구성 후 다리 길이를 입력받아 랜덤 다리를 생성한다.
+     */
+    private static void start(){
+        output.printStart();
+        game.makeBridgeGame(input.readBridgeSize());
+    }
+
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        start();
     }
 }
