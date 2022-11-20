d8170a21159834dd1532ab6ad24876897094ac1d
Jzakka <mouse4786@gmail.com>
2022-11-20 11:42:59 +0900

    style: 메서드 이름 변경

--- src/main/java/bridge/view/InputView.java
+++ src/main/java/bridge/view/InputView.java
@@ -19,8 +19,8 @@ public class InputView {
      */
     public int readBridgeSize(Consumer<IllegalArgumentException> exceptionHandler) {
         System.out.println("다리의 길이를 입력해주세요.");
-        return inputUntilValid(
-                () -> validateInput(Integer::parseInt, readLine(), "숫자를 입력하세요"),
+        return read(
+                () -> validate(Integer::parseInt, readLine(), "숫자를 입력하세요"),
                 exceptionHandler);
     }
 
@@ -29,8 +29,8 @@ public class InputView {
      */
     public String readMoving(Consumer<IllegalArgumentException> exceptionHandler) {
         System.out.println("이동할 칸을 선택해주세요. (위: U, 아래: D)");
-        return inputUntilValid(
-                () -> validateInput(Validation::validateMove, readLine(), "U와 D 중 하나만 입력하세요"),
+        return read(
+                () -> validate(Validation::validateMove, readLine(), "U와 D 중 하나만 입력하세요"),
                 exceptionHandler
         );
     }
@@ -40,13 +40,13 @@ public class InputView {
      */
     public String readGameCommand(Consumer<IllegalArgumentException> exceptionHandler) {
         System.out.println("게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)");
-        return inputUntilValid(
-                () -> validateInput(Validation::validateCommand, readLine(), "R와 Q 중 하나만 입력하세요"),
+        return read(
+                () -> validate(Validation::validateCommand, readLine(), "R와 Q 중 하나만 입력하세요"),
                 exceptionHandler
         );
     }
 
-    private <T> T validateInput(Function<String, T> callback, String input, String message) {
+    private <T> T validate(Function<String, T> callback, String input, String message) {
         try {
             return callback.apply(input);
         } catch (NumberFormatException | CommandException | MoveException e) {
@@ -54,15 +54,15 @@ public class InputView {
         }
     }
 
-    private <T> T inputUntilValid(Supplier<T> readFunction, Consumer<IllegalArgumentException> exceptionHandler) {
+    private <T> T read(Supplier<T> readFunction, Consumer<IllegalArgumentException> exceptionHandler) {
         T inputValue = null;
         while (inputValue == null) {
-            inputValue = readInput(readFunction, exceptionHandler);
+            inputValue = input(readFunction, exceptionHandler);
         }
         return inputValue;
     }
 
-    private <T> T readInput(Supplier<T> readFunction, Consumer<IllegalArgumentException> exceptionHandler) {
+    private <T> T input(Supplier<T> readFunction, Consumer<IllegalArgumentException> exceptionHandler) {
         try {
             return readFunction.get();
         } catch (IllegalArgumentException e) {
