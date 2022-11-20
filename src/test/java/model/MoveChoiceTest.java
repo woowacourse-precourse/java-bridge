ddd03b1af93e54ef29b955af2ddf466138647a41
hodomaroo <enghemdwkd@gmail.com>
2022-11-20 21:30:31 +0900

    [Test/MoveChoice] getPositionNumber 테스트 코드 작성
    이동 방향 선택 문자열을 입력받아 알맞은 문자열인 경우 대응되는
    positionNumber값을, 그렇지 않은 경우 예외를 발생시키는
    getPostionNumber메서드의 테스트 코드를 작성하였습니다..

--- src/test/java/model/MoveChoiceTest.java
+++ src/test/java/model/MoveChoiceTest.java
@@ -10,9 +10,13 @@ import org.junit.jupiter.api.Test;
 
 public class MoveChoiceTest {
 
-    private static final int UP_MOVE = MoveChoice.UP.positionNumber;
-    private static final int DOWN_MOVE = MoveChoice.DOWN.positionNumber;
-    private static final int ERROR_MOVE = 10;
+    private static final int UP_NUMBER = MoveChoice.UP.positionNumber;
+    private static final int DOWN_NUMBER = MoveChoice.DOWN.positionNumber;
+    private static final String UP_MOVING = MoveChoice.UP.moving;
+    private static final String DOWN_MOVING = MoveChoice.DOWN.moving;
+    private static final int ERROR_NUMBER = 10;
+    private static final String ERROR_MOVING = "u";
+
 
     @Nested
     @DisplayName("getMoving 메서드는 숫자 값을 입력받아")
@@ -20,15 +24,33 @@ public class MoveChoiceTest {
 
         @Test
         @DisplayName("알맞은 MoveChoice 객체의 moving 문자열을 반환한다.")
-        void returnMovingTest(){
-            assertThat(MoveChoice.getMoving(UP_MOVE)).isEqualTo(MoveChoice.UP.moving);
-            assertThat(MoveChoice.getMoving(DOWN_MOVE)).isEqualTo(MoveChoice.DOWN.moving);
+        void returnMovingTest() {
+            assertThat(MoveChoice.getMoving(UP_NUMBER)).isEqualTo(MoveChoice.UP.moving);
+            assertThat(MoveChoice.getMoving(DOWN_NUMBER)).isEqualTo(MoveChoice.DOWN.moving);
         }
 
         @Test
         @DisplayName("유효하지 않은 입력인 경우 예외를 발생시킨다.")
-        void throwExceptionTest(){
-            assertThatThrownBy(() -> MoveChoice.getMoving(ERROR_MOVE)).isInstanceOf(IllegalArgumentException.class);
+        void throwExceptionTest() {
+            assertThatThrownBy(() -> MoveChoice.getMoving(ERROR_NUMBER)).isInstanceOf(IllegalArgumentException.class);
+        }
+    }
+
+    @Nested
+    @DisplayName("getPositionNumber 메서드는 문자열을 입력받아")
+    class describe_getPositionNumber {
+
+        @Test
+        @DisplayName("알맞은 MoveChoice 객체의 getPositionNumber 값을 반환한다.")
+        void returnMovingTest() {
+            assertThat(MoveChoice.getPositionNumber(UP_MOVING)).isEqualTo(MoveChoice.UP.positionNumber);
+            assertThat(MoveChoice.getPositionNumber(DOWN_MOVING)).isEqualTo(MoveChoice.DOWN.positionNumber);
+        }
+
+        @Test
+        @DisplayName("유효하지 않은 입력인 경우 예외를 발생시킨다.")
+        void throwExceptionTest() {
+            assertThatThrownBy(() -> MoveChoice.getPositionNumber(ERROR_MOVING)).isInstanceOf(IllegalArgumentException.class);
         }
     }
 }
