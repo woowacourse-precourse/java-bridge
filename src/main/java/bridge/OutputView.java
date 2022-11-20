6c24cc9d43992fefb37ddb4291be6f32ef0f40aa
kanghyki <kanghyki@gmail.com>
2022-11-21 01:20:21 +0900

    refactor(OutputView): 문자열 상수화

--- src/main/java/bridge/OutputView.java
+++ src/main/java/bridge/OutputView.java
@@ -7,11 +7,17 @@ public class OutputView {
     private enum Line {
         TOP, BOTTOM;
     }
+
     private static final String CORRECT_CELL = " O ";
     private static final String INCORRECT_CELL = " X ";
     private static final String EMPTY_CELL = "   ";
     private static final String SEPARATOR = "|";
     private static final String START_MESSAGE = "다리 건너기 게임을 시작합니다.";
+    private static final String LEFT_BRACKET = "[";
+    private static final String RIGHT_BRACKET = "]";
+    private static final String GAME_RESULT_MESSAGE = "최종 게임 결과";
+    private static final String WHETHER_GAME_RESULT_MESSAGE = "게임 성공 여부: ";
+    private static final String TOTAL_TRY_MESSAGE = "총 시도한 횟수: ";
 
     public void printStartMessage() {
         System.out.println(START_MESSAGE);
@@ -66,8 +72,8 @@ public class OutputView {
     }
 
     private void appendBracketBothSide(StringBuilder builder) {
-        builder.insert(0, "[");
-        builder.append("]");
+        builder.insert(0, LEFT_BRACKET);
+        builder.append(RIGHT_BRACKET);
     }
 
     /**
@@ -76,9 +82,9 @@ public class OutputView {
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
     public void printResult(BridgeGame game) {
-        System.out.println("최종 게임 결과");
+        System.out.println(GAME_RESULT_MESSAGE);
         printMap(game);
-        System.out.println("게임 성공 여부: " + game.getGameResultToString());
-        System.out.println("총 시도한 횟수: " + game.getTryCount());
+        System.out.println(WHETHER_GAME_RESULT_MESSAGE + game.getGameResultToString());
+        System.out.println(TOTAL_TRY_MESSAGE + game.getTryCount());
     }
 }
