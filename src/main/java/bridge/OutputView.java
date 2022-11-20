8bbd3b08474bf2ab3dd987227cda99dfc7449cda
BoyCho <iijokv@naver.com>
2022-11-21 00:19:09 +0900

    refactor : 다리 상태 출력 방식 수정  - 클래스간 호환성을 위한 출력 방식 및 인스턴스 삭제, 자료형 수정

--- src/main/java/bridge/OutputView.java
+++ src/main/java/bridge/OutputView.java
@@ -1,82 +1,77 @@
 package bridge;
 
-import java.util.List;
+import java.util.ArrayDeque;
 
 /**
  * 사용자에게 게임 진행 상황과 결과를 출력하는 역할을 한다.
  */
 public class OutputView {
 
-    List<String> bridge;
-
-    public OutputView(List<String> bridge) {
-        this.bridge = bridge;
-    }
-
-    public void printMap(int currentPosition, String moving) {
-        printUpperSideRecord(currentPosition);
-        printUpperSideNewRecord(currentPosition, moving);
-        printLowerSideRecord(currentPosition);
-        printLowerSideNewRecord(currentPosition, moving);
+    public void printMap(ArrayDeque<String> bridge) {
+        printUpperSideRecord(bridge);
+        printUpperSideNewRecord(bridge.getLast());
+        printLowerSideRecord(bridge);
+        printLowerSideNewRecord(bridge.getLast());
     }
 
     // 이전 다리 상태 위쪽 출력
-    private void printUpperSideRecord(int currentPosition) {
+    private void printUpperSideRecord(ArrayDeque<String> bridge) {
         System.out.print("[");
-        for (int i = 0; i < currentPosition; i++) {
-            if (this.bridge.get(i).equals("U")) {
+        for (String position : bridge) {
+            if (position.equals("U")) {
                 System.out.print(" O |");
-                continue;
             }
-            System.out.print("   |");
+            if (position.equals("D")) {
+                System.out.print("   |");
+            }
         }
     }
 
     // 새로 생기는 다리 위쪽 출력
-    private void printUpperSideNewRecord(int currentPosition, String moving) {
-        if (this.bridge.get(currentPosition).equals("U")) {
-            if (this.bridge.get(currentPosition).equals(moving)) {
-                System.out.println(" O ]");
-                return;
-            }
+    private void printUpperSideNewRecord(String lastPosition) {
+        if (lastPosition.equals("UO")) {
+            System.out.println(" O ]");
+            return;
+        }
+        if (lastPosition.equals("UX")) {
             System.out.println(" X ]");
+            return;
         }
-        if (this.bridge.get(currentPosition).equals("D")) {
-            System.out.println("   ]");
-        }
+        System.out.println("   ]");
     }
 
     // 이전 다리 상태 아래쪽 출력
-    private void printLowerSideRecord(int currentPosition) {
+    private void printLowerSideRecord(ArrayDeque<String> bridge) {
         System.out.print("[");
-        for (int i = 0; i < currentPosition; i++) {
-            if (this.bridge.get(i).equals("D")) {
+        for (String position : bridge) {
+            if (position.equals("D")) {
                 System.out.print(" O |");
-                continue;
             }
-            System.out.print("   |");
+            if (position.equals("U")) {
+                System.out.print("   |");
+            }
         }
     }
 
     // 새로 생기는 다리 아래쪽 출력
-    private void printLowerSideNewRecord(int currentPosition, String moving) {
-        if (this.bridge.get(currentPosition).equals("D")) {
-            if (this.bridge.get(currentPosition).equals(moving)) {
-                System.out.println(" O ]");
-                return;
-            }
+    private void printLowerSideNewRecord(String lastPosition) {
+        if (lastPosition.equals("DO")) {
+            System.out.println(" O ]");
+            return;
+        }
+        if (lastPosition.equals("DX")) {
             System.out.println(" X ]");
+            return;
         }
-        if (this.bridge.get(currentPosition).equals("U")) {
-            System.out.println("   ]");
-        }
+        System.out.println("   ]");
     }
+
     /**
      * 게임의 최종 결과를 정해진 형식에 맞춰 출력한다.
      * <p>
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void printResult(int trialCount, int bestRecordPosition, boolean SuccessOrFail) {
+    public void printResult(ArrayDeque<String> bridge, int trialCount) {
         System.out.println("최종 게임 결과");
         System.out.println("게임 성공 여부 : 실패");
         System.out.println("게임 성공 여부 : 성공");
