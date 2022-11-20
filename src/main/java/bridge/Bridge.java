b5bc41bf97f2edeca1f896b3dd78ac955134a94d
ashlovesliitea <skyrider1811@gmail.com>
2022-11-20 17:12:58 +0900

    feat: Bridge - 플레이어가 끝에 도착했는지 확인하는 기능

--- src/main/java/bridge/Bridge.java
+++ src/main/java/bridge/Bridge.java
@@ -5,19 +5,26 @@ import java.util.List;
 
 public class Bridge {
     private List<String> spaces;
+    private BridgeMaker bridgeMaker;
 
-    public Bridge(int length) {
+    public Bridge(int length,BridgeNumberGenerator bridgeNumberGenerator) {
+        bridgeMaker = new BridgeMaker(bridgeNumberGenerator);
         spaces=decideSpaces(length);
     }
 
     public List<String> decideSpaces(int length){
-        BridgeRandomNumberGenerator bridgeRandomNumberGenerator = new BridgeRandomNumberGenerator();
-        BridgeMaker bridgeMaker = new BridgeMaker(bridgeRandomNumberGenerator);
         return bridgeMaker.makeBridge(length);
     }
 
     public Boolean isSuccess(int nxtLocation,String nxtStep){
-        if(nxtStep.equals(this.spaces.get(nxtLocation))){
+        if(nxtStep.equals(this.spaces.get(nxtLocation-1))){
+            return true;
+        }
+        return false;
+    }
+
+    public Boolean isFinish(Player player){
+        if(player.getCurrentSpace()==spaces.size()-1){
             return true;
         }
         return false;
