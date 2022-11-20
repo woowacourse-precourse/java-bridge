6f2d313c6c1ef9eda0f4086698f4242774d47932
3juhwan <13selfesteem91@naver.com>
2022-11-20 12:25:13 +0900

    feat(bridge): 입력값 유효성 검증 로직 구현

--- src/main/java/bridge/util/ValidationUtil.java
+++ src/main/java/bridge/util/ValidationUtil.java
@@ -1,4 +1,39 @@
 package bridge.util;
 
+import bridge.enums.ErrorMessage;
+
 public class ValidationUtil {
+
+    /**
+     * 다리 길이가 1 이상인지 확인한다.
+     *
+     * @param size 다리의 길이
+     */
+    public static void validateBridgeSize(int size) {
+        if (size <= 0) {
+            throw new IllegalArgumentException(ErrorMessage.INVALID_BRIDGE_LENGTH_INPUT.getMessage());
+        }
+    }
+
+    /**
+     * 이동 방향이 U 또는 D가 맞는지 확인한다.
+     *
+     * @param direction 다음 이동 방향 (U/D)
+     */
+    public static void validateMove(String direction) {
+        if (!direction.equals("U") && !direction.equals("D")) {
+            throw new IllegalArgumentException(ErrorMessage.INVALID_DIRECTION_INPUT.getMessage());
+        }
+    }
+
+    /**
+     * 게임 재개 입력이 R 또는 Q가 맞는지 확인한다.
+     *
+     * @param retry 게임 재개 여부 (R/Q)
+     */
+    public static void validateWhetherToRetry(String retry) {
+        if (!retry.equals("R") && !retry.equals("Q")) {
+            throw new IllegalArgumentException(ErrorMessage.INVALID_RETRY_INPUT.getMessage());
+        }
+    }
 }
