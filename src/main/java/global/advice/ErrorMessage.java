dba798cfd101ea753eeddf0ebe971802d602c3d4
sangwon <swchoi1997@naver.com>
2022-11-20 23:48:32 +0900

    fix: 에러메세지 수정

--- src/main/java/global/advice/ErrorMessage.java
+++ src/main/java/global/advice/ErrorMessage.java
@@ -3,7 +3,7 @@ package global.advice;
 public enum ErrorMessage {
     COMMON("[ERROR] "),
     CHECK_SIZE("3 ~ 20 사이의 숫자를 입력 해 주세요 | 입력: "),
-    CHECK_SIZE_DIGIT("문자 이외의 숫자를 입력하셨습니다. | 입력: "),
+    CHECK_SIZE_DIGIT("숫자 이외의 값를 입력하셨습니다. | 입력: "),
     CHECK_MOVING("윗 칸: U / 아래 칸: D 입니다. 올바른 입력을 해 주세요 | 입력: "),
     CHECK_RETRY("재시작: R / 종료: Q 입니다. 올바른 입력을 해 주세요 | 입력: ");
 
