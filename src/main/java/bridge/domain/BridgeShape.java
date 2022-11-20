120363ccd63821084344ac03471878c3ca441d61
slbin-park <smpts00@naver.com>
2022-11-20 23:04:53 +0900

    feat(BridgeShape) : BridgeShape enum 생성
    - 출력 양식을 열거형 enum을 사용하여 가져오도록 변경하기 위해 생성

---
+++ src/main/java/bridge/domain/BridgeShape.java
@@ -0,0 +1,26 @@
+package bridge.domain;
+
+import java.util.List;
+
+public enum BridgeShape {
+    COLLECT_UP(List.of(" O |", "   |"), List.of(" O |", "   |")),
+    COLLECT_DOWN(List.of(" O |", "   |"), List.of("   ", " O ")),
+    WRONG_UP(List.of("   |", " X |"), List.of("  ", " X ")),
+    WRONG_DOWN(List.of(" X ", "   "), List.of(" X |", "   |"));
+
+    private List<String> shape;
+    private List<String> lastShape;
+
+    BridgeShape(List<String> shape, List<String> lastShape) {
+        this.shape = shape;
+        this.lastShape = lastShape;
+    }
+
+    public List<String> getShapeMessages(boolean isLast) {
+        if (isLast) {
+            return lastShape;
+        }
+        return shape;
+    }
+
+}
