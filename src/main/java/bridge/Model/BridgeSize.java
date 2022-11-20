14315e21e20b8e3679a6de9ce6b28531452dac8c
TaeMin <xoals3094@naver.com>
2022-11-20 15:04:23 +0900

    refactor(BridgeSize) : 다리 생성에 대한 메소드 삭제

--- src/main/java/bridge/Model/BridgeSize.java
+++ src/main/java/bridge/Model/BridgeSize.java
@@ -1,10 +1,5 @@
 package bridge.Model;
 
-import bridge.BridgeMaker;
-import bridge.BridgeRandomNumberGenerator;
-
-import java.util.List;
-
 public class BridgeSize {
     private final static int MIN_SIZE = 3;
     private final static int MAX_SIZE = 20;
@@ -23,8 +18,7 @@ public class BridgeSize {
         throw new IllegalArgumentException("다리 길이는 3부터 20 사이의 숫자여야 합니다.");
     }
 
-    public List<String> makeBridge(){
-        BridgeMaker bridgeMaker = new BridgeMaker(new BridgeRandomNumberGenerator());
-        return bridgeMaker.makeBridge(size);
+    public int getSize(){
+        return size;
     }
 }
