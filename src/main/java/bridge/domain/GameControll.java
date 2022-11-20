72af5e176683203aa9cc31a80711e2411088aeea
jaehyeon <jhjh0022@naver.com>
2022-11-20 15:55:28 +0900

    feat(GameControll): 게임을 전반적으로 컨트롤 해주는 요소들 구현

---
+++ src/main/java/bridge/domain/GameControll.java
@@ -0,0 +1,46 @@
+package bridge.domain;
+
+public class GameControll {
+
+    public GameControll() {
+    }
+
+    private int idx;
+    private int cnt = 0;
+    private boolean gamePowerWhether;
+    private boolean gameSucessWheter = false;
+
+    public void plusIdx() {
+        idx += 1;
+    }
+
+    public void plusCnt() {
+        cnt += 1;
+    }
+
+    public void turnoffGamePower() {
+        gamePowerWhether = false;
+    }
+
+    public void initialize() {
+        idx = 0;
+        gamePowerWhether = true;
+    }
+
+    public int getIdx() {
+        return idx;
+    }
+
+    public int getCnt() {
+        return cnt;
+    }
+
+    public boolean getGamePower() {
+        return gamePowerWhether;
+    }
+
+    public boolean getGameSucess() {
+        return gameSucessWheter;
+    }
+
+}
