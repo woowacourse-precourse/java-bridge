a38051057fc69819386e72bfb586bc08d17083a7
dooboocookie <bkh3030@gmail.com>
2022-11-20 14:58:38 +0900

    feat: 전체적인 게임흐름 controller를 Application.java에서 호출

--- src/main/java/bridge/controller/BridgeGameController.java
+++ src/main/java/bridge/controller/BridgeGameController.java
@@ -1,8 +1,12 @@
 package bridge.controller;
 
 import bridge.model.BridgeGame;
+import bridge.model.Plate;
+import bridge.util.Rules;
 import bridge.view.InputView;
 import bridge.view.OutputView;
+
+import java.util.List;
 
 public class BridgeGameController {
     InputView inputView;
@@ -24,8 +28,49 @@ public class BridgeGameController {
         bridgeGame.newBridge(bridgeLength);
     }
 
+    public void play() {
+        bridgeGame.retry();
+        playTurn();
+    }
+
     public void playTurn() {
+        newTurn();
+        if (bridgeGame.isSuccess()) {
+            resultSuccessGame();
+        }
+        if (!bridgeGame.isSuccess()) {
+            askRetry();
+        }
+    }
+
+    public void newTurn() {
         String nextStep = inputView.readMoving();
         bridgeGame.move(nextStep);
+        boolean isSuccess = bridgeGame.isSuccess();
+        List<Plate> playerPath = bridgeGame.getPlayerPath();
+        outputView.printMap(isSuccess, playerPath);
+    }
+
+    public void resultSuccessGame() {
+        if (bridgeGame.isEndOfBridge()) {
+            gameResult();
+        }
+        if (!bridgeGame.isEndOfBridge()) {
+            playTurn();
+        }
+    }
+
+    public void askRetry() {
+        String retry = inputView.readGameCommand();
+        if (retry.equals(Rules.GAME_RETRY)) {
+            play();
+        }
+        if (!retry.equals(Rules.GAME_QUIT)) {
+            gameResult();
+        }
+    }
+
+    public void gameResult() {
+
     }
 }
