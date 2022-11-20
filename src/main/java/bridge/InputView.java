3fcc055e949feece8f9b6419fd71d4b406540151
Yoobi <quddlr96@naver.com>
2022-11-20 22:28:57 +0900

    feat(InputView): add methods to get user input value

--- src/main/java/bridge/InputView.java
+++ src/main/java/bridge/InputView.java
@@ -1,28 +1,42 @@
 package bridge;
 
+import camp.nextstep.edu.missionutils.Console;
+
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
  */
 public class InputView {
 
+    private final Validator validator;
+
+    public InputView() {
+        this.validator = new Validator();
+    }
+
     /**
      * 다리의 길이를 입력받는다.
      */
     public int readBridgeSize() {
-        return 0;
+        String inputValue = Console.readLine();
+        validator.validateBridgeSize(inputValue);
+        return Integer.parseInt(inputValue);
     }
 
     /**
      * 사용자가 이동할 칸을 입력받는다.
      */
     public String readMoving() {
-        return null;
+        String inputValue = Console.readLine();
+        validator.validateMoving(inputValue);
+        return inputValue;
     }
 
     /**
      * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
      */
     public String readGameCommand() {
-        return null;
+        String inputValue = Console.readLine();
+        validator.validateGameCommand(inputValue);
+        return inputValue;
     }
 }
