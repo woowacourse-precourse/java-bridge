a87b3164fd51472c47548fd11fd3285e63ab6d92
im-shung <seoyoung1113@gmail.com>
2022-11-20 21:47:10 +0900

    refactor: :recycle: BridgeRepository 내부에서 다리의 길이와 칸 초기화 하도록 리팩토링

--- src/main/java/bridge/repository/BridgeRepository.java
+++ src/main/java/bridge/repository/BridgeRepository.java
@@ -1,17 +1,24 @@
 package bridge.repository;
 
+import static bridge.exception.Validator.checkBridgeLength;
+
+import bridge.BridgeRandomNumberGenerator;
+import bridge.maker.BridgeMaker;
 import java.util.List;
 
 public class BridgeRepository {
 
     private int size;
-    private List<String> bridges;
+    private List<String> bridge;
 
     public BridgeRepository(int size) {
+        checkBridgeLength(size);
         this.size = size;
+        setBridge();
     }
 
-    public void setBridges(List<String> bridges) {
-        this.bridges = bridges;
+    public void setBridge() {
+        BridgeMaker bridgeMaker = new BridgeMaker(new BridgeRandomNumberGenerator());
+        this.bridge = bridgeMaker.makeBridge(size);
     }
 }
--- src/main/java/bridge/service/BrideGameService.java
+++ src/main/java/bridge/service/BrideGameService.java
@@ -1,33 +1,28 @@
 package bridge.service;
 
 import static bridge.exception.Error.*;
-import static bridge.exception.Validator.*;
 import static bridge.view.InputView.*;
 import static bridge.view.OutputView.*;
 
-import bridge.BridgeRandomNumberGenerator;
-import bridge.maker.BridgeMaker;
-import java.util.List;
+import bridge.repository.BridgeRepository;
 
 public class BrideGameService {
 
-    private int bridgeSize;
-    private List<String> bridges;
+    private BridgeRepository bridgeRepository;
 
     public void execute() {
         printBridgeGameStartMessage();
 
         try {
-            bridgeSize = readBridgeSize();
-            checkBridgeLength(bridgeSize);
+            initializeBridgeRepositoryByBridgeSize(readBridgeSize());
         } catch (NumberFormatException e) {
             throw new NumberFormatException(NOT_INT.getMessage());
         }
 
-        BridgeMaker bridgeMaker = new BridgeMaker(new BridgeRandomNumberGenerator());
-        bridges = bridgeMaker.makeBridge(bridgeSize);
-
     }
 
+    private void initializeBridgeRepositoryByBridgeSize(int size) {
+        bridgeRepository = new BridgeRepository(readBridgeSize());
+    }
 
 }
