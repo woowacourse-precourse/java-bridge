453d26e0cb0ee658aa60db0320dfa4692dd891b3
JunTaeINC <juntea3523@naver.com>
2022-11-20 15:23:36 +0900

    feat : (InputView) reGetBridgeLength 기능 구현 (예외 발생시 재입력)

--- src/main/java/bridge/BridgeGame.java
+++ src/main/java/bridge/BridgeGame.java
@@ -33,7 +33,7 @@ public class BridgeGame {
     }
 
     public void playTheGaming() {
-        List<String> bridge = gameSet(getBridgeLength());
+        List<String> bridge = gameSet(reGetBridgeLength());
         while (isTrue) {
             gamingSet(bridge);
             if (!isClear) {
@@ -48,11 +48,6 @@ public class BridgeGame {
         return computer.equals(userInput);
     }
 
-    /**
-     * 사용자가 게임을 다시 시도할 때 사용하는 메서드
-     * <p>
-     * 재시작을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
-     */
     public boolean retry() {
         System.out.println(RESTART_OF_END.getValue());
         if (getExitOrRestart().equals("R")) {
--- src/main/java/bridge/InputView.java
+++ src/main/java/bridge/InputView.java
@@ -2,27 +2,19 @@ package bridge;
 
 import camp.nextstep.edu.missionutils.Console;
 
+import static bridge.OutputView.printStartGame;
 import static bridge.enums.DorU.*;
 import static bridge.enums.ErrorMessage.*;
 import static bridge.enums.Sentence.*;
-/*
- ** 제공된 InputView 클래스를 활용해 구현해야 한다.
- ** InputView의 패키지는 변경할 수 있다.
- ** InputView의 메서드의 시그니처(인자, 이름)와 반환 타입은 변경할 수 있다.
- ** 사용자 값 입력을 위해 필요한 메서드를 추가할 수 있다.
- */
 
-/**
- * 사용자로부터 입력을 받는 역할을 한다.
- */
 public class InputView {
-    /**
-     * 다리의 길이를 입력받는다.
-     */
+
     private static final ValidityCheck validityCheck = new ValidityCheck();
     private final static String EXIT = "Q";
     private final static String RESTART = "R";
+
     public static int getBridgeLength() {
+        printStartGame();
         System.out.println(INPUT_BRIDGE_LENGTH.getValue());
         String inputBridgeLength = Console.readLine();
         if (!validityCheck.numberRangeChecker(inputBridgeLength) || !validityCheck.stringChecker(inputBridgeLength)) {
@@ -30,6 +22,16 @@ public class InputView {
         }
         return Integer.parseInt(inputBridgeLength);
     }
+
+    public static int reGetBridgeLength() {
+        try {
+            return getBridgeLength();
+        } catch (IllegalArgumentException illegalArgumentException) {
+            illegalArgumentException.getMessage();
+            return reGetBridgeLength();
+        }
+    }
+
 
     /**
      * 사용자가 이동할 칸을 입력받는다.
@@ -43,11 +45,11 @@ public class InputView {
         return inputDorU;
     }
 
+
     /**
      * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
      */
     public static String getExitOrRestart() {
-        System.out.println(RESTART_OF_END);
         String exitOrRestart = Console.readLine();
         if (!(exitOrRestart.equals(EXIT) || exitOrRestart.equals(RESTART))) {
             throw new IllegalArgumentException(ER_NOT_Q_OR_R.getMessage());
