12f73f020dea2950d6ff2889911872fa8c1546b5
안광민 <creativeduck0518@gmail.com>
2022-11-20 11:04:28 +0900

    feat(InputView): 다리 길이 입력 기능 구현

--- src/main/java/bridge/presentation/InputView.java
+++ src/main/java/bridge/presentation/InputView.java
@@ -4,5 +4,15 @@ import bridge.ExceptionHandler;
 import camp.nextstep.edu.missionutils.Console;
 
 public class InputView {
-    
+
+    public int readBridgeSize() {
+        String input = Console.readLine();
+        ExceptionHandler.checkNullInput(input);
+        ExceptionHandler.checkIsNumber(input);
+
+        int bridgeSize = Integer.parseInt(input);
+        ExceptionHandler.checkOutOfRange(bridgeSize);
+
+        return bridgeSize;
+    }
 }
