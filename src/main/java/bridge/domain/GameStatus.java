73252144a130ce6b39da7465c6c5e4fba5a9ed19
doongu <must1080@naver.com>
2022-11-21 02:47:49 +0900

    feat(GameStatus): 게임 시작 횟수를 더하는 기능

--- src/main/java/bridge/domain/GameStatus.java
+++ src/main/java/bridge/domain/GameStatus.java
@@ -3,12 +3,19 @@ package bridge.domain;
 import java.util.ArrayList;
 
 public class GameStatus {
-    public boolean isCorrect;
+    private boolean isCorrect;
+    private int gameCount;
+
+
     public void setMatchCorrect(boolean isCorrect){
         this.isCorrect = isCorrect;
     }
 
     public boolean getMatchCorrect(boolean isCorrect){
         return this.isCorrect;
+    }
+
+    public void addGameCount(boolean gameCount){
+        this.gameCount += 1;
     }
 }
