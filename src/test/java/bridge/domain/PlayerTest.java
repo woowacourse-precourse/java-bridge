281dcbcf590cfe0aa0a55d3c5131af4b7b1875c8
This2sho <zx00018@naver.com>
2022-11-20 21:28:57 +0900

    refactor(PlayerTest): @Nested 를 사용하여 계층형 테스트로 변경

--- src/test/java/bridge/domain/PlayerTest.java
+++ src/test/java/bridge/domain/PlayerTest.java
@@ -4,44 +4,61 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
+@SuppressWarnings("NonAsciiCharacters")
+@DisplayName("Player 클래스")
 public class PlayerTest {
 
-	@Test
-	@DisplayName("플레이어의 죽음을 나타내는 상태 테스트")
-	void 플레이어_상태_테스트() {
+	@Nested
+	@DisplayName("플레이어의 상태는")
+	class Describe_status {
+
 		//given
 		Player player = new Player();
-		assertThat(player.isAlive()).isTrue();
-		//when
-		player.die();
-		//then
-		assertThat(player.isDead()).isTrue();
+
+		@Test
+		@DisplayName("처음 생성되었을 때는 ALIVE 이다.")
+		void player_alive() {
+			assertThat(player.isAlive()).isTrue();
+		}
+
+		@Test
+		@DisplayName("죽은 후에는 DEAD 이다.")
+		void player_die() {
+			//when
+			player.die();
+			//then
+			assertThat(player.isDead()).isTrue();
+		}
 	}
 
-	@Test
-	@DisplayName("플레이어 이동 후 위치 변화 테스트")
-	void 플레이어_이동_테스트() {
-	    //given
-		Player player = new Player();
-		int currentLocation = player.getCurrentLocation();
-	    //when
-		player.move();
-		int moveLocation = player.getCurrentLocation();
-	    //then
-		assertThat(currentLocation).isNotEqualTo(moveLocation);
-	}
+	@Nested
+	@DisplayName("move 메소드는")
+	class Describe_move {
 
-	@Test
-	@DisplayName("플레이어가 죽었을 때 이동하는 예외처리 테스트")
-	void 플레이어_사망_이동_테스트() {
-		//given
 		Player player = new Player();
-		//when
-		player.die();
-		//then
-		Assertions.assertThatThrownBy(player::move)
-			.isInstanceOf(IllegalStateException.class);
+
+		@Test
+		@DisplayName("이동 후 위치가 변화한다.")
+		void 플레이어_이동_테스트() {
+			int currentLocation = player.getCurrentLocation();
+			//when
+			player.move();
+			int moveLocation = player.getCurrentLocation();
+			//then
+			assertThat(currentLocation).isNotEqualTo(moveLocation);
+		}
+
+		@Test
+		@DisplayName("죽었을 때 이동하면 예외가 발생한다.")
+		void 플레이어_사망_이동_테스트() {
+			//when
+			player.die();
+			//then
+			Assertions.assertThatThrownBy(player::move)
+				.isInstanceOf(IllegalStateException.class);
+		}
 	}
 }
