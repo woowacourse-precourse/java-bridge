5a2a6b405caf1e3566f8f73c2e5a49b249b7180e
황민주 <minjoo@hwangminjooui-MacBookAir.local>
2022-11-20 09:36:06 +0900

    feat(BridgeMaker): makeBridge (src/main/java/bridge/BridgeMaker.java)
    makeBridge 메소드 추가:
    -다리 사이즈를 입력하면 다리를 1또는 0의 리스트 형태로 생성해서 반환해주기 위해 추가함
    -Uility 클래스에 Move enum의 이름을 가져올 수 있는 리스트  MOVE_NAME도 생성

--- src/main/java/bridge/Utility.java
+++ src/main/java/bridge/Utility.java
@@ -1,6 +1,11 @@
 package bridge;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class Utility {
     public static String REGAME = "R";
     public static String QUIT = "Q";
+
+    public static List<String> MOVE_NAME = Arrays.asList("DOWN", "UP");
 }
