6ebc82053d107621b3c6fc7cf637f74f3283d7a2
bellljun3395 <195850@jnu.ac.kr>
2022-11-20 18:40:42 +0900

    feat(InputView): 구현 readBridgeSize

---
+++ src/main/java/bridge/view/InputView.java
@@ -0,0 +1,39 @@
+package bridge.view;
+
+import bridge.game.GameCommend;
+import bridge.structure.BridgeMoveValue;
+import bridge.view.util.Transfer;
+import camp.nextstep.edu.missionutils.Console;
+
+/**
+ * 사용자로부터 입력을 받는 역할을 한다.
+ */
+public class InputView {
+
+    /**
+     * 다리의 길이를 입력받는다.
+     */
+    public int readBridgeSize() {
+        try {
+            String bridgeSize = Console.readLine();
+            return Transfer.toNumber(bridgeSize);
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+            return this.readBridgeSize();
+        }
+    }
+
+    /**
+     * 사용자가 이동할 칸을 입력받는다.
+     */
+    public BridgeMoveValue readMoving() {
+        return null;
+    }
+
+    /**
+     * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
+     */
+    public GameCommend readGameCommand() {
+        return null;
+    }
+}
