e1a7fd34368b71ace534d2caeecf1193d7074985
Umin <adorecamus@gmail.com>
2022-11-20 21:19:19 +0900

    feat: 사용자로부터 다리의 길이 입력받음

--- src/main/java/bridge/InputView.java
+++ src/main/java/bridge/InputView.java
@@ -1,5 +1,7 @@
 package bridge;
 
+import camp.nextstep.edu.missionutils.Console;
+
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
  */
@@ -9,13 +11,20 @@ public class InputView {
      * 다리의 길이를 입력받는다.
      */
     public int readBridgeSize() {
-        return 0;
+        System.out.println("다리의 길이를 입력해주세요.");
+        String input = Console.readLine();
+        int size = changeToNumber(input);
+        if (size < 3 || size > 20) {
+            throw new IllegalArgumentException("[ERROR] 다리 길이는 3부터 20 사이의 숫자여야 합니다.");
+        }
+        return size;
     }
 
     /**
      * 사용자가 이동할 칸을 입력받는다.
      */
     public String readMoving() {
+        // 위 칸은 U, 아래 칸은 D 입력
         return null;
     }
 
@@ -23,6 +32,18 @@ public class InputView {
      * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
      */
     public String readGameCommand() {
+        // 재시작은 R, 종료는 Q 입력
+        // Q 입력 시 printResult()
         return null;
+    }
+
+    public int changeToNumber(String input) throws IllegalArgumentException {
+        int number;
+        try {
+            number = Integer.parseInt(input);
+        } catch (Exception e) {
+            throw new IllegalArgumentException("[ERROR] 숫자를 입력해야 합니다.");
+        }
+        return number;
     }
 }
