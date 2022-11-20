2cd991c3ce28a98525991922c9e06bf5c2cfbb43
MINJU <alswn9938@gmail.com>
2022-11-21 00:53:47 +0900

    feat(RoundController): add 다리 이동 결과 관련 함수
    테스트하기 힘든 로직으로 리팩토링 필요

--- src/main/java/bridge/view/OutputView.java
+++ src/main/java/bridge/view/OutputView.java
@@ -1,16 +1,21 @@
 package bridge.view;
 
+import java.util.List;
+
 /**
  * 사용자에게 게임 진행 상황과 결과를 출력하는 역할을 한다.
  */
 public class OutputView {
-
+    StringBuilder sb = new StringBuilder();
+    StringBuilder tmp = new StringBuilder();
     /**
      * 현재까지 이동한 다리의 상태를 정해진 형식에 맞춰 출력한다.
      * <p>
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void printMap() {
+    public void printMap(List<String> upperBridge, List<String> downBridge) {
+        String bridgeStatus = getBridgeStatus(upperBridge, downBridge);
+        System.out.println(bridgeStatus);
     }
 
     /**
@@ -43,6 +48,33 @@ public class OutputView {
 
     public void printFinalResultGuide(){
         System.out.println("최종 게임 결과");
+    }
+
+    public String getBridgeStatus(List<String> upperBridge, List<String> downBridge){
+        sb.setLength(0);
+        sb.append(getSingleBridgeStatus(upperBridge))
+                .append("\n")
+                .append(getSingleBridgeStatus(downBridge))
+                .append("\n");
+        return sb.toString();
+    }
+
+    private String getSingleBridgeStatus(List<String> bridge){
+        tmp.setLength(0);
+
+        tmp.append(getStartBridgeNotation())
+                .append(String.join(" | ", bridge))
+                .append(getEndBridgeNotation());
+        return tmp.toString();
+    }
+
+
+    private String getStartBridgeNotation(){
+        return "[ ";
+    }
+
+    private String getEndBridgeNotation(){
+        return " ]";
     }
 
 }
