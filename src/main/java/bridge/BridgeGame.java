540213a8d12d6b1bd9d10604c289aeb60e76df10
DaWoon Lee <qkgk61@gmail.com>
2022-11-20 20:49:21 +0900

    Refactor : InputView 클래스를 View 클래스에 포함시킴에 따른 변수 타입 수정

--- src/main/java/bridge/BridgeGame.java
+++ src/main/java/bridge/BridgeGame.java
@@ -1,7 +1,7 @@
 package bridge;
 
 import bridge.domain.MovingResult;
-import bridge.view.InputView;
+import bridge.view.View;
 import java.util.List;
 
 /**
@@ -14,8 +14,8 @@ public class BridgeGame {
      * <p>
      * 이동을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public MovingResult move(InputView inputView, List<String> bridge, int next) {
-        String moving =  inputView.readMoving();
+    public MovingResult move(View view, List<String> bridge, int next) {
+        String moving = view.readMoving();
         String target = bridge.get(next);
 
         if (target.equals(moving)) {
@@ -30,8 +30,8 @@ public class BridgeGame {
      * <p>
      * 재시작을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public boolean retry(InputView inputView) {
-        String gameCommand = inputView.readGameCommand();
+    public boolean retry(View view) {
+        String gameCommand = view.readGameCommand();
         return gameCommand.equals("R");
     }
 
