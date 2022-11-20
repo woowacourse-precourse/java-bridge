5c5f9916c0072e077e93b851550a0e8cdee6e6cc
JeonginKim <47661695+mywnajsldkf@users.noreply.github.com>
2022-11-21 08:02:34 +0900

    feat : 다리 길이 입력 메서드 추가, 다리 길이 타입 검증 메서드 추가

--- src/main/java/bridge/InputView.java
+++ src/main/java/bridge/InputView.java
@@ -1,15 +1,36 @@
 package bridge;
 
+import camp.nextstep.edu.missionutils.Console;
+
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
  */
 public class InputView {
+    private static final String BRIDGE_SIZE_INPUT_MESSAGE = "다리의 길이를 입력해주세요.";
+    private static final String BRIDGE_DIRECTION_SELECT_MESSAGE = "이동할 칸을 선택해주세요. (위: U, 아래: D)";
+    private static final String INPUT_TYPE_IS_NOT_PROPER = "[ERROR] 잘못된 입력 형식입니다.";
 
     /**
      * 다리의 길이를 입력받는다.
      */
     public int readBridgeSize() {
-        return 0;
+        System.out.println(BRIDGE_SIZE_INPUT_MESSAGE);
+        return validateSizeType(Console.readLine());
+    }
+
+    /**
+     * 다리의 길이 형식을 검증한다.
+     * @param size
+     * @return
+     */
+    private int validateSizeType(String size) {
+        int bridgeSize;
+        try {
+            bridgeSize = Integer.parseInt(size);
+        } catch (IllegalArgumentException e) {
+            throw new RuntimeException(INPUT_TYPE_IS_NOT_PROPER);
+        }
+        return bridgeSize;
     }
 
     /**
@@ -24,5 +45,9 @@ public class InputView {
      */
     public String readGameCommand() {
         return null;
+    }
+
+    public void printSelectDirection() {
+        System.out.println(BRIDGE_DIRECTION_SELECT_MESSAGE);
     }
 }
