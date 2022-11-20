15f3cab34e0aca041ee2bcbfea84b9d99dc88349
himjjong <jjijji5728@tukorea.ac.kr>
2022-11-21 02:57:19 +0900

    feat : 생성된 다리 길이 만큼 건너갈 수 있는 값 무작위로 정하기

--- src/main/java/bridge/controller/BridgeController.java
+++ src/main/java/bridge/controller/BridgeController.java
@@ -5,18 +5,20 @@ import bridge.domain.BridgeNumberGenerator;
 import bridge.domain.BridgeRandomNumberGenerator;
 import bridge.view.InputView;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class BridgeController {
     InputView inputView = new InputView();
     BridgeNumberGenerator bridgeNumberGenerator = new BridgeRandomNumberGenerator();
     BridgeMaker bridgeMaker = new BridgeMaker(bridgeNumberGenerator);
+    List<String> bridgeData = new ArrayList<>();
+
     public void run() {
-            try{
-                bridgeMaker.makeBridge(Integer.parseInt(inputView.readBridgeSize()));
-            } catch (IllegalArgumentException e) {
-                System.out.println(e.getMessage());
-            }
+        try {
+            bridgeData = bridgeMaker.makeBridge(Integer.parseInt(inputView.readBridgeSize()));
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
         }
     }
-
+}
