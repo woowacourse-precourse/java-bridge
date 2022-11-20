5c175f62609c9b8de86d87c28717dd10e7272124
seo <fungod19@gmail.com>
2022-11-20 11:38:20 +0900

    feat: 다리 개수 입력값 받기

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -1,8 +1,16 @@
 package bridge;
 
+import bridge.view.InputView;
+
 public class Application {
+
+    public static final InputView inputview = new InputView();
 
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        System.out.println("다리 건너기 게임을 시작합니다.");
+        System.out.println("다리의 길이를 입력해주세요.");
+
+
     }
 }
--- src/main/java/bridge/view/InputView.java
+++ src/main/java/bridge/view/InputView.java
@@ -1,4 +1,6 @@
-package bridge;
+package bridge.view;
+
+import camp.nextstep.edu.missionutils.Console;
 
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
@@ -9,7 +11,16 @@ public class InputView {
      * 다리의 길이를 입력받는다.
      */
     public int readBridgeSize() {
-        return 0;
+            String input = Console.readLine();
+            int size = Integer.parseInt(input);
+            validateNumber(size);
+            return size;
+    }
+
+    private void validateNumber(int size) {
+        if(size < 3 || size > 20){
+            throw new IllegalArgumentException("[ERROR] 다리 길이는 3부터 20 사이의 숫자여야 합니다.");
+        }
     }
 
     /**
