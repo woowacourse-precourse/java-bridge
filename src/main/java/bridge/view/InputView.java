3c5e71e0f2b9c934c22c4311fd4a202346a3b499
KYR <freekyr7529@gmail.com>
2022-11-21 05:47:48 +0900

    refactor : 리팩터링에 의한 코드 변경
    BridgeRule을 BridgeLengthRule로 변경한 활동에 의한 코드 변경

--- src/main/java/bridge/view/InputView.java
+++ src/main/java/bridge/view/InputView.java
@@ -1,7 +1,8 @@
 package bridge.view;
 
-import bridge.constant.BridgeRule;
+import bridge.constant.BridgeLengthRule;
 import bridge.constant.Command;
+import bridge.constant.Bridge;
 import camp.nextstep.edu.missionutils.Console;
 
 /**
@@ -26,7 +27,7 @@ public class InputView {
      */
     public int readBridgeSize() {
         int bridgeSize = convertToInteger(readValue());
-        if (bridgeSize < BridgeRule.MIN_LENGTH.getValue() || bridgeSize > BridgeRule.MAX_LENGTH.getValue()) {
+        if (bridgeSize < BridgeLengthRule.MIN_LENGTH.getValue() || bridgeSize > BridgeLengthRule.MAX_LENGTH.getValue()) {
             throw new IllegalArgumentException("다리 길이는 3 이상 20 이하여야 합니다!");
         }
         return bridgeSize;
@@ -35,22 +36,20 @@ public class InputView {
     /**
      * 사용자가 이동할 칸을 입력받는다.
      */
-    public String readMoving() {
+    /*public Bridge readMoving() {
         String move = readValue();
-        if (move.equals(Command.MOVE_UP.getCommand()) || move.equals(Command.MOVE_DOWN.getCommand())) {
-            return move;
-        }
-        throw new IllegalArgumentException("허용되지 않는 값! U(위쪽) D(아래쪽) 중 하나를 입력하셔야 합니다!");
-    }
+
+        return Bridge.searchBridgeType(move);
+    }*/
 
     /**
      * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
      */
-    public String readGameCommand() {
+    /*public String readGameCommand() {
         String command = readValue();
         if (command.equals(Command.DO_QUIT.getCommand()) || command.equals(Command.DO_RETRY.getCommand())) {
             return command;
         }
         throw new IllegalArgumentException("허용되지 않는 값! R(재시작) Q(종료) 중 하나를 입력하셔야 합니다!");
-    }
+    }*/
 }
