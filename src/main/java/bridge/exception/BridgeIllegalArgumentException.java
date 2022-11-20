7ec2fa3e745d1ba401d624a9955c40de791a8b6e
kang jihyeong <her4386@gmail.com>
2022-11-20 19:47:57 +0900

    Feat / IllegalArgumentException 에러 처리

---
+++ src/main/java/bridge/exception/BridgeIllegalArgumentException.java
@@ -0,0 +1,11 @@
+package bridge.exception;
+
+public class BridgeIllegalArgumentException extends IllegalArgumentException{
+
+    private final static String ERROR_PREFIX = "[ERROR]";
+
+    public BridgeIllegalArgumentException(String errorMessage){
+        super(ERROR_PREFIX + errorMessage);
+    }
+
+}
