3c6a6adfd0cabba2fe6027deb82e3275fc3a0c44
abbd2 <ajkety00@gmail.com>
2022-11-20 22:32:34 +0900

    Feat(OutputView): 최종 결과 값을 출력하는 printResult 메서드 구현

--- src/main/java/bridge/OutputView.java
+++ src/main/java/bridge/OutputView.java
@@ -7,13 +7,14 @@ import java.util.List;
  */
 public class OutputView {
 
+    Application application = new Application();
+
     /**
      * 현재까지 이동한 다리의 상태를 정해진 형식에 맞춰 출력한다.
      * <p>
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
     public void printMap(List<Bridge> bridgeList, int bridgeCount) {
-        System.out.println(bridgeCount);
         for (Bridge bridge : bridgeList) {
             System.out.print("[");
             for (int i = 0; i < bridgeCount; i++) {
@@ -28,6 +29,12 @@ public class OutputView {
      * <p>
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void printResult() {
+    public void printResult(List<Bridge> bridgeList, int bridgeCount) {
+        Message.printResult();
+        printMap(bridgeList, bridgeCount);
+        printIsGameSuccess();
+        printTryCount();
     }
+
+
 }
