1faee06f660ce7e16d9fa5829f8ed528c11b5e50
Ridealist <cjsn92@gmail.com>
2022-11-20 18:17:35 +0900

    refactor(OutputView): 출력 문자열 상수 선언 추가, 인스턴스 변수 이름 변경

--- src/main/java/bridge/views/OutputView.java
+++ src/main/java/bridge/views/OutputView.java
@@ -2,13 +2,6 @@ package bridge.views;
 
 import bridge.models.BridgeGame;
 
-/**
- * 사용자에게 게임 진행 상황과 결과를 출력하는 역할을 한다.
- * - 제공된 `OutputView` 클래스를 활용해 구현해야 한다.
- * - `OutputView`의 패키지는 변경할 수 있다.
- * - `OutputView`의 메서드의 이름은 변경할 수 없고, 인자와 반환 타입은 필요에 따라 추가하거나 변경할 수 있다.
- * - 값 출력을 위해 필요한 메서드를 추가할 수 있다.
- */
 public class OutputView {
     private static final String START = "[";
     private static final String END = "]";
@@ -16,11 +9,12 @@ public class OutputView {
     private static final String CORRECT = " O ";
     private static final String INCORRECT = " X ";
     private static final String BLANK = "   ";
-    private static final String GAME_SUCCESS = "성공";
-    private static final String GAME_FAIL = "실패";
+    private static final String GAME_RESULT = "최종 게임 결과";
+    private static final String GAME_SUCCESS_MESSAGE = "성공";
+    private static final String GAME_FAIL_MESSAGE = "실패";
 
-    private final StringBuilder mapUpstairs = new StringBuilder();
-    private final StringBuilder mapDownstairs = new StringBuilder();
+    private final StringBuilder upperBridge = new StringBuilder();
+    private final StringBuilder lowerBridge = new StringBuilder();
     /**
      * 현재까지 이동한 다리의 상태를 정해진 형식에 맞춰 출력한다.
      * <p>
@@ -28,20 +22,21 @@ public class OutputView {
      */
     public void printMap(String direction, boolean isCorrect, int bridgeIndex) {
         makeMap(direction, isCorrect, bridgeIndex);
-        System.out.println(START + mapUpstairs + END);
-        System.out.println(START + mapDownstairs + END);
+        System.out.println(START + upperBridge + END);
+        System.out.println(START + lowerBridge + END);
+        System.out.println();
     }
 
     private void makeMap(String direction, boolean isCorrect, int bridgeIndex) {
-        addSeparator(mapUpstairs, bridgeIndex);
-        addSeparator(mapDownstairs, bridgeIndex);
+        addSeparator(upperBridge, bridgeIndex);
+        addSeparator(lowerBridge, bridgeIndex);
         if (direction.equals("U")) {
-            addStatus(mapUpstairs, isCorrect);
-            addBlank(mapDownstairs);
+            addStatus(upperBridge, isCorrect);
+            addBlank(lowerBridge);
         // TODO else 예약어 없앨 방법은?
         } else if (direction.equals("D")) {
-            addBlank(mapUpstairs);
-            addStatus(mapDownstairs, isCorrect);
+            addBlank(upperBridge);
+            addStatus(lowerBridge, isCorrect);
         }
     }
 
@@ -68,17 +63,18 @@ public class OutputView {
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
     public void printResult(BridgeGame bridgeGame) {
-        System.out.printf("%n최종 게임 결과%n");
-        System.out.println(START + mapUpstairs + END);
-        System.out.println(START + mapDownstairs + END);
+        System.out.println(GAME_RESULT);
+        System.out.println(START + upperBridge + END);
+        System.out.println(START + lowerBridge + END);
+        System.out.println();
         System.out.printf("게임 성공 여부: %s%n", getResult(bridgeGame));
         System.out.printf("총 시도한 횟수: %d", bridgeGame.getTrialCount());
     }
 
     private String getResult(BridgeGame bridgeGame) {
         if (bridgeGame.gameSuccess()) {
-            return GAME_SUCCESS;
+            return GAME_SUCCESS_MESSAGE;
         }
-        return GAME_FAIL;
+        return GAME_FAIL_MESSAGE;
     }
 }
