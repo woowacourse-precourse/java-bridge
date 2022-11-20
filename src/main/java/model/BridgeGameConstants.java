03ec04847c74b201313bced2f9c78a041a3267cf
June <68291395+june-777@users.noreply.github.com>
2022-11-20 16:35:49 +0900

    refactor(BridgeGameConstants): BridgeGame과 연관있는 상수 의미에 맞게 변수명 수정

--- src/main/java/model/BridgeGameConstants.java
+++ src/main/java/model/BridgeGameConstants.java
@@ -1,9 +1,9 @@
 package model;
 
 public class BridgeGameConstants {
-	public static final int MOVING_FAIL_ALREADY_END = 1;
-	public static final int MOVING_FAIL_WRONG_MOVING = 2;
-	public static final int MOVING_SUCCESS = 3;
+	public static final int MOVING_FAIL_WRONG_MOVING = 1;
+	public static final int MOVING_SUCCESS = 2;
+	public static final int MOVING_SUCCESS_GAME_END = 3;
 	
 	public static final String MOVING_UP = "U";
 	public static final String MOVING_DOWN = "D";
