c70b15200653b06e0ff27befa063a5907a616935
Kayoung <rkdkdudjd@gmail.com>
2022-11-20 12:30:50 +0900

    feat(OutputView): 결과출력 기능

---
+++ src/main/java/bridge/model/GameResult.java
@@ -0,0 +1,18 @@
+package bridge.model;
+
+import java.util.List;
+
+/**
+ * 게임 결과를 저장하는 클래스
+ */
+public class GameResult {
+    public List<String> map;
+    public boolean succeed;
+    public int numberOfAttempts;
+    
+    public GameResult(List<String> map, boolean succeed, int numberOfAttempts) {
+        this.map = map;
+        this.succeed = succeed;
+        this.numberOfAttempts = numberOfAttempts;
+    }
+}
--- src/main/java/bridge/view/OutputView.java
+++ src/main/java/bridge/view/OutputView.java
@@ -1,5 +1,6 @@
 package bridge.view;
 
+import bridge.model.GameResult;
 import java.util.List;
 
 /**
@@ -7,11 +8,27 @@ import java.util.List;
  */
 public class OutputView {
     private static final String START_MESSAGE = "다리 건너기 게임을 시작합니다.";
+    private static final String RESULT_TITLE = "최종 게임 결과";
+    private static final String SUCCESS = "성공";
+    private static final String FAIL = "실패";
+    private static final String GAME_SUCCESS_MESSAGE_FORMAT = "게임 성공 여부: %s\n";
+    private static final String NUMBER_OF_ATTEMPTS_FORMAT = "총 시도한 횟수: %d\n";
 
     public void printStart() {
         System.out.println(START_MESSAGE);
     }
 
+    public void printTitle() {
+        System.out.println(RESULT_TITLE);
+    }
+
+    public void printEndMessageForSuccess() {
+        System.out.printf(GAME_SUCCESS_MESSAGE_FORMAT, SUCCESS);
+    }
+
+    public void printEndMessageForFailure() {
+        System.out.printf(GAME_SUCCESS_MESSAGE_FORMAT, FAIL);
+    }
     /**
      * 현재까지 이동한 다리의 상태를 정해진 형식에 맞춰 출력한다.
      * <p>
@@ -21,6 +38,7 @@ public class OutputView {
         for (int i = 0; i < 2; i++) {
             printOneRow(currentPath.get(i));
         }
+        System.out.println();
     }
 
     private void printOneRow(String rowOfBridge) {
@@ -50,6 +68,23 @@ public class OutputView {
      * <p>
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void printResult() {
+    public void printResult(GameResult result) {
+        printTitle();
+        printMap(result.map);
+        printEndMessage(result);
+        printNumberOfAttempts(result.numberOfAttempts);
     }
+
+    private void printEndMessage(GameResult result) {
+        if (result.succeed) {
+            printEndMessageForSuccess();
+            return;
+        }
+        printEndMessageForFailure();
+    }
+
+    private void printNumberOfAttempts(int attempts) {
+        System.out.printf(NUMBER_OF_ATTEMPTS_FORMAT, attempts);
+    }
+
 }
