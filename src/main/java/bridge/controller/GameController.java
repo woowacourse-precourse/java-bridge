42e05e570bccad328473899b294a6f64e2f3f083
junpakPark <112045553+junpakPark@users.noreply.github.com>
2022-11-20 17:31:16 +0900

    refactor(GameController): Map 및 GameResult 인스턴스 변수 삭제 및 관련 메서드 리팩토링

--- src/main/java/bridge/controller/GameController.java
+++ src/main/java/bridge/controller/GameController.java
@@ -2,8 +2,6 @@ package bridge.controller;
 
 import java.util.List;
 
-import bridge.model.GameResult;
-import bridge.model.Map;
 import bridge.model.Stairs;
 import bridge.service.BridgeGame;
 import bridge.view.OutputView;
@@ -11,50 +9,45 @@ import bridge.view.OutputView;
 public class GameController {
 
 	private final BridgeGame bridgeGame;
-	private final Map map;
-	private final GameResult gameResult;
 	private final OutputView outputView;
 
 	public GameController() {
 		this.bridgeGame = new BridgeGame();
-		this.map = new Map();
-		this.gameResult = new GameResult();
 		this.outputView = new OutputView();
 	}
 
 	public void run() {
 		outputView.printStartMessage();
-		List<String> bridges = bridgeGame.setUpGame(InputController.getBridgeSize());
+		List<String> bridges = bridgeGame.makeBridge(InputController.getBridgeSize());
 		playGame(bridges);
-		outputView.printResult(map, gameResult);
+		outputView.printResult(bridgeGame.getMap(), bridgeGame.getResult());
 	}
 
 	private void playGame(List<String> bridges) {
 		do {
-			bridgeGame.setUpRound(gameResult, map);
+			bridgeGame.setUpRound();
 			playRound(bridges);
 		} while (isKeepGaming());
 	}
 
 	private void playRound(List<String> bridges) {
 		for (String square : bridges) {
-			if (!isRightStep(square)) {
-				gameResult.changeResultToFail();
+			if (!isRightStep(square, InputController.getStairs())) {
+				bridgeGame.changeResultToFail();
 				break;
 			}
 		}
 	}
 
-	private boolean isRightStep(String square) {
-		Stairs stairs = InputController.getStairs();
-		List<String> footPrints = bridgeGame.move(stairs, square);
-		map.drawMap(footPrints);
-		outputView.printMap(map);
-		return stairs.isEquals(square);
+	private boolean isRightStep(String square, Stairs stairs) {
+		boolean isEquals = stairs.isEquals(square);
+		bridgeGame.move(stairs, isEquals);
+		outputView.printMap(bridgeGame.getMap());
+		return isEquals;
 	}
 
 	private boolean isKeepGaming() {
-		if (gameResult.isGameLose()) {
+		if (bridgeGame.isLose()) {
 			return bridgeGame.retry(InputController.getExitOption());
 		}
 		return false;
