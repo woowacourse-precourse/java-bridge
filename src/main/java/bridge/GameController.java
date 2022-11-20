010cf93f85c71a972dd621e19f6082665232cf04
zjadofl0408 <zjadofl0408@gmail.com>
2022-11-20 23:13:33 +0900

    feat(GameController) : 라운드마다 플레이어가 이동할 칸을 입력받는 기능 구현

--- src/main/java/bridge/GameController.java
+++ src/main/java/bridge/GameController.java
@@ -2,23 +2,30 @@ package bridge;
 
 public class GameController {
     private BridgeMaker bridgeMaker;
+    private int size = 0;
     InputView inputView = new InputView();
     public void startGame() {
-        inputView.printStartGame();
-        setBridgeSize();
-    }
-
-    public void setBridgeSize() {
         try {
-            bridgeMaker = new BridgeMaker(new BridgeRandomNumberGenerator());
-            String input = inputView.readBridgeSize();
-            int size = Validator.convertNumeric(input);
-            Validator.validateBridgeSize(size);
-            bridgeMaker.makeBridge(size);
+            inputView.printStartGame();
+            setBridgeSize();
+            setMoving();
         } catch (IllegalArgumentException exception) {
             System.out.println(exception.getMessage());
         }
     }
 
+    public void setBridgeSize() throws IllegalArgumentException {
+        bridgeMaker = new BridgeMaker(new BridgeRandomNumberGenerator());
+        String input = inputView.readBridgeSize();
+        size = Validator.convertNumeric(input);
+        Validator.validateBridgeSize(size);
+        bridgeMaker.makeBridge(size);
+    }
 
+    public void setMoving() throws IllegalArgumentException {
+        for (int index = 0; index < size; index++) {
+            String input = inputView.readMoving();
+            Validator.checkUpOrDown(input);
+        }
+    }
 }
