33853695f273a5083d1588ebbbf8bffd5f7e437e
egoing <dudqk9696@naver.com>
2022-11-20 16:36:19 +0900

    refactor : 결과 메시지 추가

--- src/main/java/view/OutputMessage.java
+++ src/main/java/view/OutputMessage.java
@@ -4,7 +4,13 @@ public enum OutputMessage {
     GAME_RESULT("최종 게임 결과"),
     WIN_GAME("게임 성공 여부: 성공"),
     LOSE_GAME("게임 성공 여부: 실패"),
-    TRY("총 시도한 횟수: ");
+    TRY("총 시도한 횟수: "),
+    CORRECT(" O |"),
+    WRONG(" X |"),
+    JUMP_BAR("   |"),
+    START("["),
+    BAR("|"),
+    END("]");
 
     private final String outputMsg;
 
