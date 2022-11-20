959d8f94341eb15bebaadb91efd5029cf0585592
roasted-penguin <swkwon25@hanyang.ac.kr>
2022-11-21 01:37:40 +0900

    feat : add OutputView

--- src/main/java/bridge/view/OutputView.java
+++ src/main/java/bridge/view/OutputView.java
@@ -7,6 +7,7 @@ import java.util.List;
  */
 public class OutputView {
     public void startBridge(){
+        System.out.println("다리 건너기 게임을 시작합니다.\n");
     }
 
     /**
@@ -14,7 +15,9 @@ public class OutputView {
      * <p>
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void printMap() {
+    public void printMap(List<String> str) {
+        System.out.println(str.get(0));
+        System.out.println(str.get(1));
     }
 
     /**
@@ -22,6 +25,11 @@ public class OutputView {
      * <p>
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void printResult() {
+    public void printResult(List<String> str,boolean PF, int gameCount) {//표, 게임 성공 여부: 성공 ,총 시도한 횟수: 2
+        System.out.println("최종 게임 결과");
+        printMap(str);
+        if(PF) System.out.println("게임 성공 여부: 성공");
+        if(!PF) System.out.println("게임 성공 여부: 실패");
+        System.out.println("총 시도한 횟수: "+gameCount);
     }
 }
