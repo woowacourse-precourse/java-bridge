cf90277e939082514736eb16caec5386b58e2726
chordpli <chordplaylist@gmail.com>
2022-11-21 00:27:05 +0900

    feat: move
    1. 위다리, 아래 다리 arrayList 추가
    2. moveBridge : 맵에 표시
    3. printMap으로 출력

--- src/main/java/bridge/BridgeGame.java
+++ src/main/java/bridge/BridgeGame.java
@@ -1,16 +1,56 @@
 package bridge;
 
+import java.util.ArrayList;
+import java.util.List;
+
+
 /**
  * 다리 건너기 게임을 관리하는 클래스
  */
 public class BridgeGame {
+
+    private List<String> upBridge = new ArrayList<>();
+    private List<String> downBridge = new ArrayList<>();
+    private int bridgeCount = 0;
+
+    OutputView outputView = new OutputView();
 
     /**
      * 사용자가 칸을 이동할 때 사용하는 메서드
      * <p>
      * 이동을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void move() {
+    public void move(List<String> bridge, String moving) {
+        moveBridge(bridge, moving);
+
+
+        outputView.printMap(upBridge, downBridge);
+    }
+
+    private void moveBridge(List<String> bridge, String moving) {
+        if (bridge.get(bridgeCount).equals(moving) && moving.equals("U")) {
+            upBridge.add("O");
+            downBridge.add(" ");
+            bridgeCount++;
+        }
+
+        if (bridge.get(bridgeCount).equals(moving) && moving.equals("D")) {
+            upBridge.add(" ");
+            downBridge.add("O");
+            bridgeCount++;
+        }
+
+        if (!bridge.get(bridgeCount).equals(moving) && moving.equals("U")) {
+            upBridge.add("X");
+            downBridge.add(" ");
+            bridge.add("X");
+        }
+
+        if (!bridge.get(bridgeCount).equals(moving) && moving.equals("D")) {
+            upBridge.add(" ");
+            downBridge.add("X");
+            bridge.add("X");
+        }
     }
 
     /**
