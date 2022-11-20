ed535527a637d26b0558e9d301829a6f8495297f
kjb95 <topmedia44@gmail.com>
2022-11-20 23:13:19 +0900

    test: 게임결과 출력에 대한 테스트 코드 작성

--- src/test/java/bridge/view/OutputViewTest.java
+++ src/test/java/bridge/view/OutputViewTest.java
@@ -1,5 +1,6 @@
 package bridge.view;
 
+import bridge.enums.SuccessOrFail;
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
 import java.util.List;
@@ -60,5 +61,53 @@ class OutputViewTest {
 
         Assertions.assertThat(byteArrayOutputStream.toString())
                 .isEqualTo("[ O |   |   | O |   | X ]\n" + "[   | O | O |   | O |   ]\n" + "\n");
+    }
+
+    @Test
+    void 게임결과_크기3_재시도1회_성공_출력결과() {
+        List<Integer> bridgeMap = List.of(1, 0, 0);
+        SuccessOrFail success = SuccessOrFail.SUCCESS;
+        int retryCount = 1;
+
+        outputView.printResult(bridgeMap, success, retryCount);
+
+        Assertions.assertThat(byteArrayOutputStream.toString())
+                .isEqualTo("최종 게임 결과\n" + "[ O |   |   ]\n" + "[   | O | O ]\n" + "\n" + "게임 성공 여부: 성공\n" + "총 시도한 횟수: 1");
+    }
+
+    @Test
+    void 게임결과_크기4_재시도2회_성공_출력결과() {
+        List<Integer> bridgeMap = List.of(1, 0, 0, 1);
+        SuccessOrFail success = SuccessOrFail.SUCCESS;
+        int retryCount = 2;
+
+        outputView.printResult(bridgeMap, success, retryCount);
+
+        Assertions.assertThat(byteArrayOutputStream.toString())
+                .isEqualTo("최종 게임 결과\n" + "[ O |   |   | O ]\n" + "[   | O | O |   ]\n" + "\n" + "게임 성공 여부: 성공\n" + "총 시도한 횟수: 2");
+    }
+
+    @Test
+    void 게임결과_크기5_재시도3회_실패_출력결과() {
+        List<Integer> bridgeMap = List.of(1, 0, 0, 1, 0);
+        SuccessOrFail success = SuccessOrFail.FAIL;
+        int retryCount = 3;
+
+        outputView.printResult(bridgeMap, success, retryCount);
+
+        Assertions.assertThat(byteArrayOutputStream.toString())
+                .isEqualTo("최종 게임 결과\n" + "[ O |   |   | O | X ]\n" + "[   | O | O |   |   ]\n" + "\n" + "게임 성공 여부: 실패\n" + "총 시도한 횟수: 3");
+    }
+
+    @Test
+    void 게임결과_크기6_재시도4회_실패_출력결과() {
+        List<Integer> bridgeMap = List.of(1, 0, 0, 1, 0, 0);
+        SuccessOrFail success = SuccessOrFail.FAIL;
+        int retryCount = 4;
+
+        outputView.printResult(bridgeMap, success, retryCount);
+
+        Assertions.assertThat(byteArrayOutputStream.toString())
+                .isEqualTo("최종 게임 결과\n" + "[ O |   |   | O |   | X ]\n" + "[   | O | O |   | O |   ]\n" + "\n" + "게임 성공 여부: 실패\n" + "총 시도한 횟수: 4");
     }
 }
