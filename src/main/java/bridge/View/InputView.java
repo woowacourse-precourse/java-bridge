615178c3aea650583cd2a4cf2de09c2583c3557a
jinju9553 <69393506+jinju9553@users.noreply.github.com>
2022-11-20 21:52:42 +0900

    Feat(view) : 기능7 - 재시작 명령어를 입력받는 함수 추가

--- src/main/java/bridge/View/InputView.java
+++ src/main/java/bridge/View/InputView.java
@@ -10,6 +10,7 @@ public class InputView {
 
     private static final String READ_SIZE_MESSAGE = "다리의 길이를 입력해주세요.";
     private static final String READ_MOVING_MESSAGE = "이동할 칸을 선택해주세요. (위: U, 아래: D)";
+    private static final String READ_COMMAND_MESSAGE = "게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)";
     private InputValidator inputValidator = new InputValidator();
 
     /**
@@ -45,6 +46,12 @@ public class InputView {
      * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
      */
     public String readGameCommand() {
-        return null;
+        System.out.println(READ_COMMAND_MESSAGE);
+
+        String input = Console.readLine();
+
+        inputValidator.validateCommand(input);
+
+        return input;
     }
 }
