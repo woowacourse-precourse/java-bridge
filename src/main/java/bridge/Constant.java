59cf68b6c8e5c697a390e4ccb6339dc8f6da8a5e
Limjongho <whdgh9873@icloud.com>
2022-11-20 15:09:45 +0900

    feat(Constant): 재시도 관련 상수 추가

--- src/main/java/bridge/Constant.java
+++ src/main/java/bridge/Constant.java
@@ -6,6 +6,8 @@ package bridge;
 public class Constant {
     public static final String MOVING_UP = "U";
     public static final String MOVING_DOWN = "D";
+    public static final String RETRY_GAME = "R";
+    public static final String QUIT_GAME = "Q";
 
     public static final int MIN_BRIDGE_LENGTH = 3;
     public static final int MAX_BRIDGE_LENGTH = 20;
