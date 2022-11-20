7978c3ada03eb424dadf185178247af6b08d4519
lseok <lseokhyun99@gmail.com>
2022-11-20 16:23:35 +0900

    feat: 다리 게임 실행 메소드 구현
    다리 게임 실행 메소드
    runBridgeGame() 을 구현하였다.
    
    일단 실행되는 것을 확인하기 위해
    메소드를 분리하지 않고 작성하여 리팩토링이 필요하다.

--- src/main/java/bridge/BridgeGameRun.java
+++ src/main/java/bridge/BridgeGameRun.java
@@ -1,4 +1,49 @@
 package bridge;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class BridgeGameRun {
+    private static final InputView input = new InputView();
+    private static final OutputView output = new OutputView();
+
+    public static void runBridgeGame() {
+
+        System.out.println("다리 건너기 게임을 시작합니다.");
+
+        BridgeMaker maker = new BridgeMaker(new BridgeRandomNumberGenerator());
+        int bridgeSize = input.readBridgeSize();
+
+        List<String> bridge = maker.makeBridge(bridgeSize);
+
+        List<String> path = new ArrayList<>();
+
+        BridgeGame game = new BridgeGame();
+
+        int gameTryCount = 0;
+        boolean gameComplete;
+        String gameRetryCommand;
+        String judge = "실패";
+
+        do {
+            gameTryCount++;
+            path.clear();
+            do {
+                path.add(input.readMoving());
+
+                gameComplete = game.move(bridge, path);
+
+                output.printMap(bridge, path);
+
+            } while (gameComplete);
+
+            if (game.judgeSuccessFailure(bridge, path)) {
+                judge = "성공";
+                break;
+            }
+            gameRetryCommand = input.readGameCommand();
+        } while (game.retry(gameRetryCommand));
+
+        output.printResult(judge, gameTryCount);
+    }
 }
