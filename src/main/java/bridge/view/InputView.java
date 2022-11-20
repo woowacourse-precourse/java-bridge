58adb68143b084536f1c0e5ca457266aa5e9edc0
Heosejin98 <sejin3319@naver.com>
2022-11-20 18:25:16 +0900

    Feat : 입력 예외 검증 기능 추가

--- src/main/java/bridge/view/InputView.java
+++ src/main/java/bridge/view/InputView.java
@@ -7,23 +7,34 @@ import camp.nextstep.edu.missionutils.Console;
  */
 public class InputView {
 
-    /**
-     * 다리의 길이를 입력받는다.
-     */
-    public int readBridgeSize() {
-        return Integer.parseInt(Console.readLine());
-    }
-    /**
-     * 사용자가 이동할 칸을 입력받는다.
-     */
-    public String readMoving() {
-        return Console.readLine();
-    }
+	/**
+	 * 다리의 길이를 입력받는다.
+	 */
+	public int readBridgeSize() {
+		return Integer.parseInt(Console.readLine());
+	}
 
-    /**
-     * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
-     */
-    public String readGameCommand() {
-        return Console.readLine();
-    }
+	/**
+	 * 사용자가 이동할 칸을 입력받는다.
+	 */
+	public String readMoving() {
+		String moveInput = Console.readLine();
+
+		if (moveInput.equals("U") || moveInput.equals("D")) {
+			return moveInput;
+		}
+		throw new IllegalArgumentException("U or D를 입력하세요");
+	}
+
+	/**
+	 * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
+	 */
+	public String readGameCommand() {
+		String retryInput = Console.readLine();
+
+		if (retryInput.equals("R") || retryInput.equals("Q")) {
+			return retryInput;
+		}
+		throw new IllegalArgumentException("R or Q를 입력하세요");
+	}
 }
