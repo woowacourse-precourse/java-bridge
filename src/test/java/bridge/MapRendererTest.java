5537b7ef3972a48becf49d4e8fe87ed8a8b9f1cf
Devtist <yt930310@snu.ac.kr>
2022-11-21 02:33:33 +0900

    test: Map 생성에 대한 테스트코드 추가 작성

--- src/test/java/bridge/MapRendererTest.java
+++ src/test/java/bridge/MapRendererTest.java
@@ -13,14 +13,14 @@ class MapRendererTest {
 	@DisplayName("모두 옳은 칸을 고른 다리의 상태를 정해진 형식대로 만든다.")
 	@Test
 	void renderSuccess() {
-		MapRenderer mapRenderer = new MapRenderer();
-		assertThat(mapRenderer.render(List.of("U","D","D","U"), true)).isEqualTo("[ O |   |   | O ]\n[   | O | O |   ]");
+		MapRenderer mapRenderer = new MapRenderer(List.of("U","D","D","U"), true);
+		assertThat(mapRenderer.toMapDto().getMap()).isEqualTo("[ O |   |   | O ]\n[   | O | O |   ]");
 	}
 
 	@DisplayName("틀린 칸을 고른 다리의 상태를 정해진 형식대로 만든다.")
 	@Test
 	void renderFail() {
-		MapRenderer mapRenderer = new MapRenderer();
-		assertThat(mapRenderer.render(List.of("U","D","D","U"), false)).isEqualTo("[ O |   |   |   ]\n[   | O | O | X ]");
+		MapRenderer mapRenderer = new MapRenderer(List.of("U","D","D","U"), false);
+		assertThat(mapRenderer.toMapDto().getMap()).isEqualTo("[ O |   |   |   ]\n[   | O | O | X ]");
 	}
 }
