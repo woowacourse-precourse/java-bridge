b97e369ba5b3fd3deca40e97f1aff1eb9f0868ea
DaWoon Lee <qkgk61@gmail.com>
2022-11-20 20:40:53 +0900

    Feat : OutputView 클래스에서 게임 결과를 출력하는 printResult 메서드 구현

--- src/main/java/bridge/view/OutputView.java
+++ src/main/java/bridge/view/OutputView.java
@@ -1,5 +1,6 @@
 package bridge.view;
 
+import bridge.domain.GameResult;
 import bridge.domain.MovingResult;
 import java.util.Map;
 
@@ -39,6 +40,14 @@ public class OutputView {
      * <p>
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void printResult() {
+    public void printResult(Map<String, StringBuilder> map, GameResult gameResult) {
+        System.out.println("최종 게임 결과");
+        for (StringBuilder printer : map.values()) {
+            System.out.println(printer.toString());
+        }
+        System.out.println();
+
+        System.out.println("게임 성공 여부: " + gameResult.getResult());
+        System.out.println("총 시도한 횟수: " + gameResult.getTotalTry());
     }
 }
