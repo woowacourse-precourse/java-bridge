1ad99d1bef8412fe033938eb6fd4c9ead4a8ae34
ljh <dusk5588@naver.com>
2022-11-20 23:28:52 +0900

    feat(Controller) : Controller에 InputView 멤버로 추가

--- src/main/java/bridge/Controller/Controller.java
+++ src/main/java/bridge/Controller/Controller.java
@@ -1,20 +1,27 @@
 package bridge.Controller;
 
-public class Controller {
+import bridge.View.InputView;
 
+public class Controller {
+    public InputView inputView;
     public void run(){
+        inputView = new InputView();
+
         System.out.println("다리 건너기 게임을 시작합니다.");
         System.out.println();
 
         //다리 길이 입력 받기
+        int bridgeLen = inputView.readBridgeSize();
 
         //다리 생성하기
 
         //게임 시작
-
+        String moving = inputView.readMoving();
+        
         ////성공
 
         ////실패
+        String gameCommand = inputView.readGameCommand();
 
         //결과 출력
     }
