a6d7f90c617dccda10e8a4c0748f5d067ac4a88a
Yunsik-Choi <choiys0311@gmail.com>
2022-11-21 00:53:45 +0900

    ⚡feat : 예외 상황 시 에러 문구를 출력해야 한다.  "[ERROR]"로 시작하는 에러 메시지를 출력 하는 기능 구현

--- src/main/java/bridge/exception/BridgeException.java
+++ src/main/java/bridge/exception/BridgeException.java
@@ -1,7 +1,9 @@
 package bridge.exception;
 
-public class BridgeException extends IllegalArgumentException{
+public class BridgeException extends IllegalArgumentException {
+    private static final String ERROR_MESSAGE = "[ERROR] : ";
+
     public BridgeException(String message) {
-        super(message);
+        super(ERROR_MESSAGE + message);
     }
 }
