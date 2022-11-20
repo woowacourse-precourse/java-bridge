3ecc3ef841c7f92dfb861a78315305dcbb2597bf
jaehyeon <jhjh0022@naver.com>
2022-11-20 21:28:36 +0900

    refactor(Valification): 특정 문자들 상수화로 인한 리팩토링

--- src/main/java/bridge/service/Valification.java
+++ src/main/java/bridge/service/Valification.java
@@ -1,23 +1,29 @@
-package bridge;
+package bridge.service;
+
+import bridge.service.constants.Command;
+import bridge.service.constants.Move;
 
 import java.util.Objects;
 
 public class Valification {
 
-    public void verifyBridgeSize(int size){
-        if(!(3<= size && size <=20)){
+    private final int MIN_NUMBER = 3;
+    private final int MAX_NUMBER = 20;
+
+    public void verifyBridgeSize(int size) {
+        if (!(MIN_NUMBER <= size && size <= MAX_NUMBER)) {
             throw new IllegalArgumentException();
         }
     }
 
-    public void verifyUorD (String move){
-        if(!(Objects.equals(move, "U") || Objects.equals(move, "D"))){
+    public void verifyUorD(String move) {
+        if (!(Objects.equals(move, Move.UPBRIDGE) || Objects.equals(move, Move.DOWNBRIDGE))) {
             throw new IllegalArgumentException();
         }
     }
 
-    public void verifyQorR (String hi){
-        if(!(Objects.equals(hi, "R") || Objects.equals(hi, "Q"))){
+    public void verifyQorR(String hi) {
+        if (!(Objects.equals(hi, Command.RETRY) || Objects.equals(hi, Command.QUIT))) {
             throw new IllegalArgumentException();
         }
     }
