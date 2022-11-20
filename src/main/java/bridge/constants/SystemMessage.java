780f82ae8e27d19e878ce3f1d6a231a8010f3080
alstjr706 <alstjr706@naver.com>
2022-11-21 07:35:21 +0900

    fix : (다리 건너기) 상수 네이밍 명확히 수정

--- src/main/java/bridge/constants/SystemMessage.java
+++ src/main/java/bridge/constants/SystemMessage.java
@@ -9,5 +9,5 @@ public final class SystemMessage {
     public static final String LAST_GAME_RESULT = "최종 게임 결과";
     public static final String GAME_SUCCESS = "게임 성공 여부: 성공";
     public static final String GAME_FAILURE = "게임 성공 여부: 실패";
-    public static final String ALL_TRY_COUNT = "총 시도한 횟수: ";
+    public static final String TOTAL_TRY_COUNT = "총 시도한 횟수: ";
 }
