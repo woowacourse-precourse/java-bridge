b53c98e03479873966c07d633211b7c22469d26c
Basak <chgy2131@naver.com>
2022-11-20 14:09:06 +0900

    feat(Bridge): 다리의 정보를 저장하는 클래스 추가

--- src/main/java/bridge/domain/Bridge.java
+++ src/main/java/bridge/domain/Bridge.java
@@ -1,4 +1,37 @@
 package bridge.domain;
 
+import java.util.List;
+
 public class Bridge {
+    private int length;
+    private List<String> bridge;
+
+    Bridge(int length, List<String> bridge) {
+        validate(length, bridge);
+        this.length = length;
+        this.bridge = bridge;
+    }
+
+    Bridge(List<String> bridge) {
+        this.length = bridge.size();
+        this.bridge = bridge;
+    }
+
+    private void validate(int length, List<String> bridge) {
+        if (bridge.size() != length) {
+            throw new IllegalArgumentException("[ERROR] bridge size mismatch");
+        }
+    }
+
+    @Override
+    public String toString() {
+        if (bridge.size() == 0) {
+            return "";
+        }
+        String result = "";
+        for (String bridge : this.bridge) {
+            result += bridge;
+        }
+        return result;
+    }
 }
