914246690fcb24631168c3bc629363063382bf1b
KYR <freekyr7529@gmail.com>
2022-11-21 05:45:59 +0900

    refactor : 별도의 메서드를 생성해 다리 생성 동작을 분리
    makeBridge 메서드를 생성하여 인스턴스 변수를 지역 변수로 바꿈

--- src/main/java/bridge/controller/GameController.java
+++ src/main/java/bridge/controller/GameController.java
@@ -1,22 +1,30 @@
 package bridge.controller;
 
+import bridge.BridgeGame;
 import bridge.BridgeMaker;
 import bridge.BridgeRandomNumberGenerator;
+import bridge.constant.Bridge;
 import bridge.view.InputView;
 import bridge.view.OutputView;
+import java.util.List;
 
 public class GameController {
     private final InputView inputView = new InputView();
     private final OutputView outputView = new OutputView();
-    private final BridgeRandomNumberGenerator generator = new BridgeRandomNumberGenerator();
-    private final BridgeMaker bridgeMaker = new BridgeMaker(generator);
 
     public void run() {
         try {
             outputView.startMsg();
-            bridgeMaker.makeBridge(inputView.readBridgeSize());
+            List<String> bridge = makeBridge();
         } catch (IllegalArgumentException exception) {
             outputView.printException(exception.getMessage());
         }
     }
+
+    public List<String> makeBridge() {
+        BridgeMaker bridgeMaker = new BridgeMaker(new BridgeRandomNumberGenerator());
+        return bridgeMaker.makeBridge(inputView.readBridgeSize());
+    }
 }
+
+
