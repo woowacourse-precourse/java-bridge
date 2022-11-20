9e61b94bb1d361a0898b8686cbad92d642a0f09c
choyeram <choyeram8@gmail.com>
2022-11-20 23:01:48 +0900

    feat: 이동할 칸 입력 실패 시 재입력 요청

--- src/main/java/bridge/controller/BridgeGameController.java
+++ src/main/java/bridge/controller/BridgeGameController.java
@@ -1,8 +1,6 @@
 package bridge.controller;
 
-import bridge.model.Bridge;
 import bridge.model.BridgeGame;
-import bridge.model.Player;
 import bridge.view.OutputView;
 
 public class BridgeGameController {
@@ -34,12 +32,24 @@ public class BridgeGameController {
 
     private void play() {
         boolean isMoving = true;
+        boolean verified;
         do {
-            OutputView.askMove();
-            isMoving = bridgeGame.move();
-            OutputView.printMap(bridgeGame.getPlayer().getWay(), bridgeGame.getBridge().getBridge());
-        } while (isMoving && bridgeGame.isOnGoing());
+            try {
+                verified = true;
+                isMoving = askMove();
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+                verified = false;
+            }
+        } while (isMoving && bridgeGame.isOnGoing() || !verified);
         determinePlay(bridgeGame.isOnGoing());
+    }
+
+    private boolean askMove() {
+        OutputView.askMove();
+        boolean isMoving = bridgeGame.move();
+        OutputView.printMap(bridgeGame.getPlayer().getWay(), bridgeGame.getBridge().getBridge());
+        return isMoving;
     }
 
     private void determinePlay(boolean onGoing) {
