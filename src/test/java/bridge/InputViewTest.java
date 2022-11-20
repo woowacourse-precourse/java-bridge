e758a07ede93a8b7cf42462b10280ec03f9394b9
poter0501 <poter0501@gmail.com>
2022-11-21 08:32:07 +0900

    test(InputViewTest): InputViewTest 파일 생성 및 스켈레톤 코드 추가

---
+++ src/test/java/bridge/InputViewTest.java
@@ -0,0 +1,24 @@
+package bridge;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class InputViewTest {
+
+    @Test
+    void readBridgeSize() {
+    }
+
+    @Test
+    void readMoving() {
+    }
+
+    @Test
+    void readGameCommand() {
+    }
+
+    @Test
+    void tryParseInt() {
+    }
+}
