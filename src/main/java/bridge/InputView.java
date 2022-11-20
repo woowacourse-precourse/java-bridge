9ca5e3ed92f7bb46ae7b75a491afb7779613e281
ensk26 <tjensk26@gmail.com>
2022-11-21 00:44:11 +0900

    feat(InputView): 다리 길이 입력 메서드
    새로운 메서드 추가 to InputView
    - 다리 길이 입력, 메시지 출력
    - 다리 길이 반환

--- src/main/java/bridge/InputView.java
+++ src/main/java/bridge/InputView.java
@@ -1,5 +1,9 @@
 package bridge;
 
+import camp.nextstep.edu.missionutils.Console;
+
+import static bridge.InputType.READ_BRIDGE_SIZE;
+
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
  */
@@ -8,8 +12,10 @@ public class InputView {
     /**
      * 다리의 길이를 입력받는다.
      */
-    public int readBridgeSize() {
-        return 0;
+    public String readBridgeSize() {
+        System.out.println(READ_BRIDGE_SIZE.getMessage());
+        String bridgeSize = Console.readLine();
+        return bridgeSize;
     }
 
     /**
