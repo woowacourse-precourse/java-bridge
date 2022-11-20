01360787a2d8d33fab1bc185100aa9586a594276
hyunseo <goatsfish@gmail.com>
2022-11-20 18:44:13 +0900

    feat : 다리 이동 타입 비교 기능 추가

--- src/main/java/bridge/domain/BridgeMoveType.java
+++ src/main/java/bridge/domain/BridgeMoveType.java
@@ -4,30 +4,34 @@ import java.util.Arrays;
 
 import static bridge.exception.BridgeTypeExceptionMessage.READ_WRONG_TYPE_EXCEPTION;
 
-public enum BridgeType {
+public enum BridgeMoveType {
     DOWN("U", 0),
     UP("D", 1);
 
     private final String type;
     private final int code;
 
-    BridgeType(String type, int code) {
+    BridgeMoveType(String type, int code) {
         this.type = type;
         this.code = code;
     }
 
-    public static BridgeType of(String bridgeType) {
-        return Arrays.stream(BridgeType.values())
+    public static BridgeMoveType of(String bridgeType) {
+        return Arrays.stream(BridgeMoveType.values())
                 .filter(bridge -> bridge.type.equals(bridgeType))
                 .findFirst()
                 .orElseThrow(() -> new IllegalArgumentException(READ_WRONG_TYPE_EXCEPTION.getMessage()));
     }
 
     public static String typeOf(int bridgeCode) {
-        return Arrays.stream(BridgeType.values())
+        return Arrays.stream(BridgeMoveType.values())
                 .filter(bridge -> bridge.code == bridgeCode)
                 .findFirst()
                 .orElseThrow(() -> new IllegalArgumentException(READ_WRONG_TYPE_EXCEPTION.getMessage()))
                 .type;
+    }
+
+    public boolean isSame(BridgeMoveType bridgeMoveType) {
+        return this == bridgeMoveType;
     }
 }
