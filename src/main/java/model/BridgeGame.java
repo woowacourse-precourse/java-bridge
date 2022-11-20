314b89fd3061394c5a4516116a83c9adc0f37292
hodomaroo <enghemdwkd@gmail.com>
2022-11-20 20:25:06 +0900

    [Feat/BridgeGame] Docs 명세에 따른 BridgeGame 클래스 선언
    Docs 명세에 따라 BridgeGame 클래스의 필드 및 기능 메서드를
    선언하였습니다.

--- src/main/java/model/BridgeGame.java
+++ src/main/java/model/BridgeGame.java
@@ -1,9 +1,18 @@
 package model;
 
+import dto.GameResult;
+import model.enums.GameStatus;
+import model.enums.MoveResult;
+import java.util.List;
+
 /**
  * 다리 건너기 게임을 관리하는 클래스
  */
 public class BridgeGame {
+
+    private List<MoveResult> moveResults;
+    private Bridge bridge;
+    private int tryCount;
 
     /**
      * 사용자가 칸을 이동할 때 사용하는 메서드
@@ -19,5 +28,21 @@ public class BridgeGame {
      * 재시작을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
     public void retry() {
+    }
+
+    public GameResult getGameResult() {
+        return null;
+    }
+
+    public GameStatus getGameStatus() {
+        return null;
+    }
+
+    private boolean succeed() {
+        return false;
+    }
+
+    private boolean failed() {
+        return false;
     }
 }
