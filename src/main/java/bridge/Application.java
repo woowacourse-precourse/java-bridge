721cfd201d947b12d463527915af7c4c8c6fe5c2
coldbucks <met.hsw@gmail.com>
2022-11-21 08:52:17 +0900

    feat: 사용자로부터 수 를 입력 받는다

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -4,9 +4,10 @@ public class Application {
 
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        InputView inputView = new InputView();
 
         System.out.println("다리 건너기 게임을 시작합니다.");
 
-
+        inputView.readBridgeSize();
     }
 }
--- src/main/java/bridge/InputView.java
+++ src/main/java/bridge/InputView.java
@@ -1,5 +1,7 @@
 package bridge;
 
+import camp.nextstep.edu.missionutils.Console;
+
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
  */
@@ -9,7 +11,10 @@ public class InputView {
      * 다리의 길이를 입력받는다.
      */
     public int readBridgeSize() {
-        return 0;
+        System.out.println("다리의 길이를 입력해주세요.");
+        String bridgeLength = Console.readLine();
+        int bridgeLengthNo = Integer.valueOf(bridgeLength);
+        return bridgeLengthNo;
     }
 
     /**
