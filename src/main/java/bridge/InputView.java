b7e14e88dce6845098b4ed831dbc92ed2b688ad3
wonseokjee <wsji9404@gmail.com>
2022-11-21 00:43:08 +0900

    Feat(InputView): 추가 readBridgeSize, readMoving, readGameCommand함수

--- src/main/java/bridge/InputView.java
+++ src/main/java/bridge/InputView.java
@@ -1,31 +1,42 @@
 package bridge;
 
+import static camp.nextstep.edu.missionutils.Console.readLine;
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
  */
 public class InputView {
-
+    BridgeGame bridgeGame = new BridgeGame();
+    ValidateError validateError = new ValidateError();
     /**
-     * 다리의 길이를 입력받는다. ->bridgemaker
+     * 다리의 길이를 입력받는다. -> bridgemaker
      */
     public int readBridgeSize() {
-        return 0;
-    }
+        bridgeGame.printInputBridgeSize();
+        String userInput = readLine();
+        validateError.validIsNumber(userInput);
+        validateError.validIsInRange(userInput);
 
-    /*3~20의 숫자일때 통과되도록*/
+        return Integer.parseInt(userInput);
+    }
 
 
     /**
      * 사용자가 이동할 칸을 입력받는다.
      */
     public String readMoving() {
-        return null;
+        bridgeGame.printSelectBridgeUpDown();
+        String userInput = readLine();
+        validateError.validIsNotSuitableMovingCommand(userInput);
+        return userInput;
     }
 
     /**
      * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
      */
     public String readGameCommand() {
-        return null;
+        bridgeGame.printRestartOrQuit();
+        String userInput = readLine();
+        validateError.validIsNotSuitableEndCommand(userInput);
+        return userInput;
     }
 }
