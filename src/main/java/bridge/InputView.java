5f307f2854e67152a1435e9426808b006ddf9380
yimo22 <yimo22@naver.com>
2022-11-20 17:00:45 +0900

    feat : InputView 기능 구현
    featured : InputView
    - readBridgeSize()
    - readMoving()
    - readGameCommand()

--- src/main/java/bridge/InputView.java
+++ src/main/java/bridge/InputView.java
@@ -1,5 +1,7 @@
 package bridge;
 
+import camp.nextstep.edu.missionutils.Console;
+
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
  */
@@ -9,20 +11,33 @@ public class InputView {
      * 다리의 길이를 입력받는다.
      */
     public int readBridgeSize() {
-        return 0;
+        System.out.println("다리의 길이를 입력해주세요.");
+        String buff = Console.readLine();
+        // Exception 처리
+        // case 1. 해당 숫자가 integer로 parsing 불가
+        return Integer.parseInt(buff);
     }
 
     /**
      * 사용자가 이동할 칸을 입력받는다.
      */
     public String readMoving() {
-        return null;
+        System.out.println("이동할 칸을 선택해주세요. (위: U, 아래: D)");
+        String buff = Console.readLine();
+        // Exception 처리
+        // case 1. 사용자의 이동 칸 입력이 'U' or 'D' 이 아닌경우
+        // case 2. 앞뒤로 trim 이 필요한 경우
+        return buff;
     }
 
     /**
      * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
      */
     public String readGameCommand() {
-        return null;
+        System.out.println("게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)");
+        String buff = Console.readLine();
+        // Exception 처리
+        // case 1. 사용자가 지정된 문자 외의 문자를 입력하는 경우
+        return buff;
     }
 }
