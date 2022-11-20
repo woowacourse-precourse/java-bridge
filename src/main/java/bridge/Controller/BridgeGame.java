823049fb9e6f9a2218442b4f3a0ad88742f5d26a
5nam <ohnam90@gmail.com>
2022-11-20 13:47:11 +0900

    refactor: retry 를 위한 초기화 함수 수정 및 move 예외처리 로직 추가

--- src/main/java/bridge/Controller/BridgeGame.java
+++ src/main/java/bridge/Controller/BridgeGame.java
@@ -9,6 +9,7 @@ import bridge.Model.InputView;
 import bridge.View.OutputView;
 import camp.nextstep.edu.missionutils.Console;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -20,26 +21,39 @@ public class BridgeGame {
     private int count = 1;
     private int tried = 1;
     String gameResult = "성공";
-    List<String> madeBridge;
+    List<String> madeBridge = new ArrayList<>();
+    String userDirection = "";
     /**
      * 사용자가 칸을 이동할 때 사용하는 메서드
      * <p>
      * 이동을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void start() {
+    public void startGame() {
         BridgeNumberGenerator bridgeNumberGenerator = new BridgeRandomNumberGenerator();
         BridgeMaker bridgeMaker = new BridgeMaker(bridgeNumberGenerator);
 
         System.out.println("다리 건너기 게임을 시작합니다.\n");
-        System.out.println("다리의 길이를 입력해주세요.");
-        size = InputView.getInstance.readBridgeSize(Console.readLine());
-
+        while (true) {
+            System.out.println("다리의 길이를 입력해주세요.");
+            try {
+                size = InputView.getInstance.readBridgeSize(Console.readLine());
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+                continue;
+            }
+            break;
+        }
         madeBridge = bridgeMaker.makeBridge(size);
     }
     public void move() {
         while (count <= size) {
             System.out.println("이동할 칸을 선택해주세요. (위: U, 아래: D)");
-            String userDirection = InputView.getInstance.readMoving(Console.readLine());
+            try {
+                userDirection = InputView.getInstance.readMoving(Console.readLine());
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+                continue;
+            }
             BridgeJudge.getInstance.judgeInput(userDirection, madeBridge.get(index), Bridge.bridge);
             OutputView.getInstance.printMap(Bridge.bridge, count);
             index++;
@@ -63,14 +77,11 @@ public class BridgeGame {
             System.out.println("게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)");
             String cont = InputView.getInstance.readGameCommand(Console.readLine());
             if (cont.equals("R")) {
-                this.allInit();
-                this.tried++;
+                this.retryInit();
                 return true;
             }
-            if (cont.equals("Q")) {
-                gameResult = "실패";
-                return false;
-            }
+            gameResult = "실패";
+            return false;
         }
         return true;
     }
@@ -83,14 +94,11 @@ public class BridgeGame {
         System.out.println("총 시도한 횟수: " + tried);
     }
 
-    private void init() {
+    private void retryInit() {
         index = 0;
         count = 1;
-    }
-
-    private void allInit() {
-        this.init();
         Bridge.bridge.init();
         OutputView.getInstance.init();
+        tried++;
     }
 }
