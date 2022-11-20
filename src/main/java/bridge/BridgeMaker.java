d81bdefe7c00b64c707f685bcb04d2bf0da13214
cgk95 <kjkj5868@gmail.com>
2022-11-20 19:04:16 +0900

    feat : 다리의 길이를 입력받아 다리를 생성해주는 기능

--- src/main/java/bridge/BridgeMaker.java
+++ src/main/java/bridge/BridgeMaker.java
@@ -1,6 +1,7 @@
 package bridge;
 
 import java.util.List;
+import bridge.BridgeRandomNumberGenerator;
 
 /**
  * 다리의 길이를 입력 받아서 다리를 생성해주는 역할을 한다.
@@ -18,6 +19,16 @@ public class BridgeMaker {
      * @return 입력받은 길이에 해당하는 다리 모양. 위 칸이면 "U", 아래 칸이면 "D"로 표현해야 한다.
      */
     public List<String> makeBridge(int size) {
-        return null;
+        List<String> trueBridge=new List<String>;
+        for (int i = 0; i < size; i++) {
+            trueBridge.add(addTile());
+        }
+        return trueBridge;
+    }
+    public String addTile(){
+        if (new BridgeRandomNumberGenerator().generate() == 0) {
+            return "D";
+        }
+        return "U";
     }
 }
