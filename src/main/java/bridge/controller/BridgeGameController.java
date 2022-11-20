1a4657ed38dccee41745b5263a047d5a1e5dce9f
ESeokhwan <huan99@hanmail.net>
2022-11-20 23:31:58 +0900

    feat: 전반적인 게임 진행 과정을 컨트롤하는 기능

--- src/main/java/bridge/controller/BridgeGameController.java
+++ src/main/java/bridge/controller/BridgeGameController.java
@@ -1,4 +1,86 @@
 package bridge.controller;
 
+import bridge.BridgeRandomNumberGenerator;
+import bridge.model.*;
+import bridge.view.InputView;
+import bridge.view.OutputView;
+
 public class BridgeGameController {
+    private final OutputView outputView;
+    private final InputView inputView;
+
+    private BridgeGame bridgeGame;
+
+    public BridgeGameController() {
+        outputView = new OutputView();
+        inputView = new InputView();
+    }
+
+    public void runGame() {
+        try {
+            runGameWithoutExceptionControl();
+        } catch (IllegalArgumentException e) {
+            outputView.printError(e.getMessage());
+        }
+    }
+
+    private void runGameWithoutExceptionControl() {
+        printGameStartAlert();
+
+        int bridgeSize = getAndProcessBridgeLengthInput();
+        makeRandomBridge(bridgeSize);
+
+        boolean willRetry = true;
+        do {
+            SpotInfo nextMove = getAndProcessNextMovementInput();
+            bridgeGame.move(nextMove);
+            printBridgeMap();
+
+            if(bridgeGame.getResult().isSuccess()) {
+                willRetry = false;
+                continue;
+            }
+
+            if(bridgeGame.getResult().isFail()) {
+                RestartInfo gameCommand = getAndProcessGameCommandInput();
+                if(gameCommand == RestartInfo.RETRY) {
+                    bridgeGame.retry();
+                    continue;
+                }
+                if(gameCommand == RestartInfo.QUIT) {
+                    willRetry = false;
+                }
+            }
+        } while(willRetry);
+
+        outputView.printResult(bridgeGame.getBridge(), bridgeGame.getResult());
+    }
+
+    private RestartInfo getAndProcessGameCommandInput() {
+        outputView.printRetryInputAlert();
+        return inputView.readGameCommand();
+    }
+
+    private void printBridgeMap() {
+        outputView.printMap(bridgeGame);
+    }
+
+    private SpotInfo getAndProcessNextMovementInput() {
+        outputView.printNextMovementInputAlert();
+        return inputView.readMoving();
+    }
+
+    private void printGameStartAlert() {
+        outputView.printGameStartAlert();
+    }
+
+    private int getAndProcessBridgeLengthInput() {
+        outputView.printBridgeLengthInputAlert();
+        return inputView.readBridgeSize();
+    }
+
+    private void makeRandomBridge(int bridgeSize) {
+        BridgeMaker bridgeMaker = new BridgeMaker(new BridgeRandomNumberGenerator());
+        bridgeGame = new BridgeGame(new Bridge(bridgeMaker.makeBridge(bridgeSize)));
+    }
 }
