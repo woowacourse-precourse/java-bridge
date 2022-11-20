5f45e2eee8e7f692a5297b48400f62eeb638636a
ho2n <jungho2n@gmail.com>
2022-11-21 00:17:04 +0900

    test(BridgeGameTest): 재시작하여 끝까지 건넜을 때 출력 형식에 맞게 결과를 출력하는지 테스트하는 코드 추가

--- src/test/java/bridge/BridgeGameTest.java
+++ src/test/java/bridge/BridgeGameTest.java
@@ -85,6 +85,39 @@ class BridgeGameTest extends NsTest {
         }, 1, 0, 1);
     }
 
+    @Test
+    void 재시작하여_끝까지_건넜을_때_출력_형식에_맞게_결과를_출력하는지_테스트() {
+        assertRandomNumberInRangeTest(() -> {
+            run("3", "U", "U", "R", "U", "D", "D", "R", "U", "D", "U");
+            assertThat(output()).contains(
+                    "[ O ]",
+                    "[   ]",
+                    "[ O | X ]",
+                    "[   |   ]",
+
+                    "[ O ]",
+                    "[   ]",
+                    "[ O |   ]",
+                    "[   | O ]",
+                    "[ O |   |   ]",
+                    "[   | O | X ]",
+
+                    "[ O ]",
+                    "[   ]",
+                    "[ O |   ]",
+                    "[   | O ]",
+                    "[ O |   | O ]",
+                    "[   | O |   ]",
+
+                    "최종 게임 결과",
+                    "[ O |   | O ]",
+                    "[   | O |   ]",
+                    "게임 성공 여부: 성공",
+                    "총 시도한 횟수: 3"
+            );
+        }, 1, 0, 1);
+    }
+
     @Override
     protected void runMain() {
         Application.main(new String[]{});
