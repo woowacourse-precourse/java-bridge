7364a79fa98d86463dabc778aa957cf208f6f085
miffyKing <whqudghk28@gmail.com>
2022-11-20 19:25:13 +0900

    test: Make test class to check BridgeLength Class

--- src/test/java/bridge/model/BridgeLengthTest.java
+++ src/test/java/bridge/model/BridgeLengthTest.java
@@ -1,7 +1,31 @@
 package bridge.model;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
-class BridgeLengthTest {
+import bridge.Application;
+import bridge.utils.BasicBridgeInputAlphabetParser;
+import camp.nextstep.edu.missionutils.test.NsTest;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 
+class BridgeLengthTest extends NsTest {
+
+    @DisplayName("사용자의 입력이 숫자 범위가 맞는가 - 더 작은 경우 ")
+    @Test
+    void checkBridgeLengthException1() {
+        assertThatThrownBy(() -> BridgeLength.from(2))
+            .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("사용자의 입력이 숫자 범위가 맞는가 - 더 큰 경우 ")
+    @Test
+    void checkBridgeLengthException2() {
+        assertThatThrownBy(() -> BridgeLength.from(25))
+            .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Override
+    public void runMain() {
+        Application.main(new String[]{});
+    }
 }
