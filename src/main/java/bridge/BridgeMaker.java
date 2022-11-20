6ecd75d3dd1e08f1a486dd601eeddfb2fd9b62bd
Ran <lan4250@naver.com>
2022-11-20 22:48:52 +0900

    FEAT/ makeBridge() 다리 길이 예외 처리

--- src/main/java/bridge/BridgeMaker.java
+++ src/main/java/bridge/BridgeMaker.java
@@ -18,6 +18,18 @@ public class BridgeMaker {
      * @return 입력받은 길이에 해당하는 다리 모양. 위 칸이면 "U", 아래 칸이면 "D"로 표현해야 한다.
      */
     public List<String> makeBridge(int size) {
+
+        // 숫자가 아니거나
+        if (!Character.isDigit(size)) {
+            throw new IllegalArgumentException("[ERROR] 숫자만 입력 가능합니다.");
+
+        }
+        if (size < 3 || size > 20) {
+            throw new IllegalArgumentException("[ERROR] 3 이상 20 이하의 숫자만 입력 가능합니다.");
+
+        }
+
+
         return null;
     }
 }
