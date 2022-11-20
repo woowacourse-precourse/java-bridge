fe797e075b03117aad7ebcf6b9040ed21eec3e03
Identityless <rlandwnd555@naver.com>
2022-11-21 03:50:24 +0900

    refactor : init() 메서드 public 으로 변경

--- src/main/java/bridge/BridgeGame.java
+++ src/main/java/bridge/BridgeGame.java
@@ -84,7 +84,7 @@ public class BridgeGame {
         return inputView.readGameCommand();
     }
 
-    private void init() {
+    public void init() {
         System.out.println("다리의 길이를 입력해주세요.");
         bridgesize = inputView.readBridgeSize();
         randomMap = bridgeMaker.makeBridge(bridgesize);
