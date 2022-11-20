ca2edc53de33b96ea37b737a5ebea585e540f892
koo9b9h <90121024+koo9b9h@users.noreply.github.com>
2022-11-20 14:20:49 +0900

    test: BridgeComparator 클래스에 isSame 메소드 테스트 추가

--- src/main/java/bridge/model/BridgeComparator.java
+++ src/main/java/bridge/model/BridgeComparator.java
@@ -3,10 +3,10 @@ package bridge.model;
 import java.util.List;
 
 public class BridgeComparator {
-    boolean isSuccess = true;
+    private boolean isSuccess = true;
 
     public boolean isSame(List<String> userBridge, List<String> computerBridge) {
-        if (userBridge.get(userBridge.size() - 1).equals(computerBridge.get(userBridge.size() - 1))) {
+        if (!(userBridge.get(userBridge.size() - 1).equals(computerBridge.get(userBridge.size() - 1)))) {
             isSuccess = false;
         }//if
         return isSuccess;
