3836e556d09995758b51a137d864ae76264a1df1
jinju9553 <69393506+jinju9553@users.noreply.github.com>
2022-11-20 22:02:31 +0900

    Feat(controller) : 기능9 - 비즈니스 로직 호출 추가

--- src/main/java/bridge/Controller/GameController.java
+++ src/main/java/bridge/Controller/GameController.java
@@ -1,19 +1,38 @@
 package bridge.Controller;
 
 import bridge.Model.Bridge;
+import bridge.Model.GameResult;
 import bridge.Service.BridgeGame;
 import bridge.View.InputView;
 import bridge.View.OutputView;
 
 public class GameController {
-    private BridgeGame bridgeGame = new BridgeGame();
+
+    private static final String RETRY = GameResult.RETRY.getResult();
     private int progress;
+    private boolean isCrossable;
+    private BridgeGame bridgeGame = new BridgeGame();
 
     public void run(Bridge bridge) {
-        boolean isCrossable = true;
+        String command;
+
+        do {
+            command = "";
+            boolean isCrossable = proceed(bridge);
+            if (!isCrossable) {
+                command = handleRetryCommand();
+            }
+        } while (command.equals(RETRY));
+
+        printFinalResult();
+    }
+
+    private boolean proceed(Bridge bridge) {
+        final int bridgeSize = bridge.getSize();
+        isCrossable = true;
         progress = 0;
 
-        while (isCrossable) {
+        while (isCrossable && bridgeSize > progress) { //TODO: 라인 줄이기
             String input = handleInput();
             String currentResult = bridge.getResult(input, progress);
             isCrossable = bridgeGame.isCrossable(currentResult);
@@ -21,9 +40,11 @@ public class GameController {
             progress = bridgeGame.move(progress);
             printResult(currentResult, input);
         }
+
+        return isCrossable;
     }
 
-    public String handleInput() {
+    private String handleInput() {
         InputView inputView = new InputView();
 
         String input = inputView.readMoving();
@@ -31,9 +52,23 @@ public class GameController {
         return input;
     }
 
-    public void printResult(String currentResult, String input) {
+    private String handleRetryCommand() {
+        InputView inputView = new InputView(); //인스턴스 중복 생성?
+
+        String input = inputView.readGameCommand();
+
+        return input;
+    }
+
+    private void printResult(String currentResult, String input) {
         OutputView outputView = new OutputView();
 
         outputView.printMap(currentResult, input, progress);
+    }
+
+    private void printFinalResult() {
+        OutputView outputView = new OutputView();
+
+        outputView.printResult();
     }
 }
