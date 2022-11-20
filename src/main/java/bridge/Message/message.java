f84b5aa61f96ed7fc0268926944cebfcd5c4f22e
KimSangHyeok <tkdgur0906@naver.com>
2022-11-20 18:29:35 +0900

    Feat: 출력하는 상수 메시지 클래스 생성

---
+++ src/main/java/bridge/Message/message.java
@@ -0,0 +1,12 @@
+package bridge.Message;
+
+public class message {
+
+    public static String START_GAME = "다리 건너기 게임을 시작합니다.\n";
+    public static String INPUT_BRIDGE_LENGTH = "다리의 길이를 입력해주세요.\n";
+    public static String CHOOSE_MOVE_DIRECTION = "이동할 칸을 선택해주세요. (위: U, 아래: D)\n";
+    public static String CHOOSE_RETRY = "게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)\n";
+    public static String GAME_RESULT = "최종 게임 결과\n";
+    public static String GAME_SUCCESS_OR_FAILURE = "게임 성공 여부: ";
+    public static String TRY_COUNT = "총 시도한 횟수: ";
+}
