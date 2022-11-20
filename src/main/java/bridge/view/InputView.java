3494636df57c17266e922ee82b9d69b1ee913990
leesangjin <tkdwls0628@gmail.com>
2022-11-21 00:18:12 +0900

    feat(InputView): 다리 길이 입력 구현

--- src/main/java/bridge/view/InputView.java
+++ src/main/java/bridge/view/InputView.java
@@ -1,9 +1,16 @@
 package bridge.view;
 
+import bridge.validator.BridgeSizeValidator;
+import camp.nextstep.edu.missionutils.Console;
+
 public class InputView {
 
+    private static final String REQUEST_BRIDGE_SIZE = "다리의 길이를 입력해주세요.";
+
     public int readBridgeSize() {
-        return 0;
+        System.out.println(REQUEST_BRIDGE_SIZE);
+        String input = Console.readLine();
+        return Integer.parseInt(input);
     }
 
     public String readMoving() {
