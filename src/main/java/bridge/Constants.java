1aeba92feddb28ac5cdda84d835a0c98d7cb9177
KyungKyu Park <rudrb0505@gmail.com>
2022-11-20 19:40:34 +0900

    refactor(OutputView): 하드코딩 정리 및 기능별 메소드 분리

--- src/main/java/bridge/Constants.java
+++ src/main/java/bridge/Constants.java
@@ -37,8 +37,8 @@ public class Constants {
 	public static final String INPUT_RESTART_OR_QUIT = "게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)";
 
 	public static final String OUTPUT_GAME_RESULT = "최종 게임 결과";
-
 	public static final String OUTPUT_GAME_REPORT = "게임 성공 여부: ";
+	public static final String NULL = "\" \"";
 	public static final String SUCCESS_MESSAGE = "성공";
 	public static final String FAIL_MESSAGE = "실패";
 	public static final String OUTPUT_TOTAL_TRY_COUNT = "총 시도한 횟수: ";
--- src/main/java/bridge/OutputView.java
+++ src/main/java/bridge/OutputView.java
@@ -34,8 +34,12 @@ public class OutputView {
 	 * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
 	 */
 	public HashMap<String, String> printResult() {
-		status = bridgeStatus.loadStatus(" ", " ");
+		status = getStatus();
 		return status;
+	}
+
+	private HashMap<String, String> getStatus() {
+		return bridgeStatus.loadStatus(NULL, NULL);
 	}
 
 	public String printSuccessOrNot() {
@@ -47,5 +51,9 @@ public class OutputView {
 
 	public int printTotalGameCount() {
 		return bridgeStatus.getTotalGameCount();
+	}
+
+	public void printFinalGameResult() {
+		System.out.println(OUTPUT_GAME_RESULT);
 	}
 }
