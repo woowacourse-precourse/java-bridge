4f797b9bdb019b28e6f159a001ca62d7a373c28d
jin8073 <jin8073@gmail.com>
2022-11-20 16:35:05 +0900

    feat: 생성한 다리 저장

---
+++ src/main/java/bridge/model/Bridge.java
@@ -0,0 +1,12 @@
+package bridge.model;
+
+import java.util.List;
+
+public class Bridge {
+
+    private final List<String> bridgeShape;
+
+    public Bridge(List<String> bridgeShape) {
+        this.bridgeShape = bridgeShape;
+    }
+}
