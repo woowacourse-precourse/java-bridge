67a4095ee2228af07d19953635048803c3c06062
broden-horizon <rlghd1698@naver.com>
2022-11-20 19:26:25 +0900

    ♻️refactor: OutputView Static -> 제거
    전역으로 사용할 필요가 없고, 한 클래스에서만 사용되기 때문에 static 제거

--- src/main/java/bridge/viewer/OutputView.java
+++ src/main/java/bridge/viewer/OutputView.java
@@ -8,13 +8,13 @@ import java.util.List;
  * 사용자에게 게임 진행 상황과 결과를 출력하는 역할을 한다.
  */
 public class OutputView {
-    private static final String startMessage = "다리 건너기 게임을 시작합니다.";
-    private static final String endMessage = "최종 게임 결과";
-    private static final String successMessage = "게임 성공 여부: 성공";
-    private static final String failureMessage = "게임 성공 여부: 실패";
-    private static final String numberOfTrialMessage = "총 시도한 횟수: ";
+    private final String startMessage = "다리 건너기 게임을 시작합니다.";
+    private final String endMessage = "최종 게임 결과";
+    private final String successMessage = "게임 성공 여부: 성공";
+    private final String failureMessage = "게임 성공 여부: 실패";
+    private final String numberOfTrialMessage = "총 시도한 횟수: ";
 
-    public static void printStart() {
+    public void printStart() {
         System.out.println(startMessage);
     }
 
@@ -24,7 +24,7 @@ public class OutputView {
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
     //index => 현재까지 진행된 index
-    public static void printMap(char[][] map, int index) {
+    public void printMap(char[][] map, int index) {
         StringBuilder sb = new StringBuilder();
         for(int i = 0; i < 2; i++) {
             sb.append("[");
@@ -40,17 +40,17 @@ public class OutputView {
      * <p>
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public static void printResult(boolean isSuccess, int numberOfTrials) {
+    public void printResult(boolean isSuccess, int numberOfTrials) {
         if(isSuccess) {
             System.out.println(successMessage);
-            System.out.println(numberOfTrials + numberOfTrials);
+            System.out.println(numberOfTrials);
             return;
         }
         System.out.println(failureMessage);
-        System.out.println(numberOfTrials + numberOfTrials);
+        System.out.println(numberOfTrials);
     }
 
-    public static void printEndMessage() {
+    public void printEndMessage() {
         System.out.println(endMessage);
     }
 }
