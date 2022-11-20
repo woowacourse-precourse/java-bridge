4e47e7035132fa6e53781635d3fd3f915fe71ee7
kimtaesoo99 <107785279+kimtaesoo99@users.noreply.github.com>
2022-11-20 20:42:01 +0900

    test: (BridgeGameControllerTest) 컨트롤러 테스트

---
+++ src/test/java/bridge/controller/BridgeGameControllerTest.java
@@ -0,0 +1,60 @@
+package bridge.controller;
+
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import bridge.domain.Bridge;
+import bridge.domain.User;
+import java.util.List;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class BridgeGameControllerTest {
+    private final BridgeGameController bridgeGameController = new BridgeGameController();
+
+    @DisplayName("한 단계 통과 테스트")
+    @Test
+    public void runPassOrFailCaseTest() {
+        //given
+        Bridge bridge = new Bridge(List.of("U", "U", "D"));
+        User user = new User();
+        boolean pass = true;
+
+        //when
+        bridgeGameController.runPassOrFailCase(pass, bridge, user);
+
+        //then
+        assertThat(bridge.getLocation()).isEqualTo(1);
+    }
+
+    @DisplayName("통과 실패 테스트")
+    @Test
+    public void runFailCaseTest() {
+        //given
+        Bridge bridge = new Bridge(List.of("U", "U", "D"));
+        User user = new User();
+        String gameCommand = "Q";
+
+        //when
+        bridgeGameController.runFailCase(bridge, user, gameCommand);
+
+        //then
+        assertThat(user.isSuccessComplete()).isEqualTo(false);
+    }
+
+    @DisplayName("통과 실패 후 재시작 테스트")
+    @Test
+    public void runFailCaseWithRetryTest() {
+        //given
+        Bridge bridge = new Bridge(List.of("U", "U", "D"));
+        User user = new User();
+        String gameCommand = "R";
+        bridge.increaseLocation();
+
+        //when
+        bridgeGameController.runFailCase(bridge, user, gameCommand);
+
+        //then
+        assertThat(bridge.getLocation()).isEqualTo(0);
+    }
+}
