7f3483e75150789b6a93e0bd1aae48c8eb556192
박준용 <jo101044@naver.com>
2022-11-20 23:41:55 +0900

    feat: 최종 게임 결과 출력 기능 구현

--- src/main/java/bridge/BridgeGame.java
+++ src/main/java/bridge/BridgeGame.java
@@ -1,6 +1,8 @@
 package bridge;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * 다리 건너기 게임을 관리하는 클래스
@@ -15,13 +17,20 @@ public class BridgeGame {
         this.outputView = outputView;
     }
 
-    public void startGame(List<String> bridge) {
+    public void setBridge(List<String> bridge) {
         this.bridge = bridge;
-        int count = 0;
-        while(move(new StringBuilder(), new StringBuilder()) || retry()){
-            count++;
-        }
+    }
 
+    public void startGame() {
+        Map<String, String> result = new HashMap<>();
+        int count = 0;
+
+        do {
+            count++;
+            giveResult(result);
+        } while (endGame(result.get("success")));
+
+        outputView.printResult(result.get("output"), result.get("success"), count);
     }
 
     /**
@@ -30,13 +39,13 @@ public class BridgeGame {
      * 이동을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
     public boolean move(StringBuilder up, StringBuilder down) {
-
         for (String now : bridge) {
             boolean tf = addUpAndDown(up, down, now);
-            outputView.printMap(up, down);
+            outputView.printMap(castMap(up, down));
 
             if (!tf) return false;
         }
+
         return true;
     }
 
@@ -47,15 +56,39 @@ public class BridgeGame {
      */
     public boolean retry() {
         String command = inputView.readGameCommand();
+
         return command.equals("R");
     }
 
+    private void giveResult(Map<String, String> result) {
+        StringBuilder up = new StringBuilder();
+        StringBuilder down = new StringBuilder();
+
+        result.put("success", castSuccess(move(up, down)));
+        result.put("output", castMap(up, down));
+    }
+
+    private String castSuccess(boolean endPoint) {
+        if (endPoint) return "성공";
+        return "실패";
+    }
+
+    private String castMap(StringBuilder up, StringBuilder down) {
+        return "[" + up + "]" + "\n"
+                + "[" + down + "]";
+    }
+
+    private boolean endGame(String success) {
+        if (success.equals("성공")) return false;
+        return retry();
+    }
 
     private boolean addUpAndDown(StringBuilder up, StringBuilder down, String now) {
         String move = inputView.readMoving();
         boolean tf = move.equals(now);
 
         addSideBar(up, down);
+
         up.append(addOX("U", move, tf));
         down.append(addOX("D", move, tf));
 
@@ -68,12 +101,8 @@ public class BridgeGame {
     }
 
     private String addOX(String position, String move, boolean tf) {
-        if (!position.equals(move)) {
-            return "   ";
-        }
-        if (!tf) {
-            return " X ";
-        }
+        if (!position.equals(move)) return "   ";
+        if (!tf) return " X ";
         return " O ";
     }
 
--- src/main/java/bridge/OutputView.java
+++ src/main/java/bridge/OutputView.java
@@ -10,9 +10,8 @@ public class OutputView {
      * <p>
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void printMap(StringBuilder up, StringBuilder down) {
-        System.out.println("["+up+"]");
-        System.out.println("["+down+"]");
+    public void printMap(String map) {
+        System.out.println(map);
     }
 
     /**
@@ -20,6 +19,13 @@ public class OutputView {
      * <p>
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void printResult() {
+    public void printResult(String result, String success, int count) {
+        String output = "최종 게임 결과"+"\n"
+                +result+"\n"
+                +"\n"
+                +"게임 성공 여부: "+success+"\n"
+                +"총 시도한 횟수: "+count;
+
+        System.out.println(output);
     }
 }
