47837769c500c91cfda6717ca7caef3f00749bbc
박준용 <jo101044@naver.com>
2022-11-20 23:42:49 +0900

    feat: 메인 메소드 구현

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -1,8 +1,20 @@
 package bridge;
 
 public class Application {
+    static InputView inputView = new InputView(new Validator());
+    static OutputView outputView = new OutputView();
+    static BridgeGame bridgeGame = new BridgeGame(inputView, outputView);
+    static BridgeRandomNumberGenerator bridgeRandomNumberGenerator = new BridgeRandomNumberGenerator();
+    static BridgeMaker bridgeMaker = new BridgeMaker(bridgeRandomNumberGenerator);
 
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        try {
+            int size = inputView.readBridgeSize();
+            bridgeGame.setBridge(bridgeMaker.makeBridge(size));
+            bridgeGame.startGame();
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+        }
     }
 }
