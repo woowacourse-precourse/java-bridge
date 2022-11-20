786f7e8096ab70c57c36aae77a1d361619b34646
yun <yshyeon102@gmail.com>
2022-11-20 16:20:25 +0900

    feat(OutputView): 맵 그리기 기능 구현

--- src/main/java/bridge/view/OutputView.java
+++ src/main/java/bridge/view/OutputView.java
@@ -1,16 +1,78 @@
-package bridge;
+package bridge.view;
+
+import bridge.GameStatus;
+import bridge.MovementType;
+
+import java.util.List;
 
 /**
  * 사용자에게 게임 진행 상황과 결과를 출력하는 역할을 한다.
  */
 public class OutputView {
+    public static final String BLANK = "   |";
+    public static final String O = " O |";
+    public static final String X = " X |";
 
     /**
      * 현재까지 이동한 다리의 상태를 정해진 형식에 맞춰 출력한다.
      * <p>
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void printMap() {
+    public void printMap(List<String> realBridges, List<String> selectBridges) {
+        String map = makeMaps(realBridges, selectBridges);
+        System.out.println(map);
+    }
+
+    public String makeMaps(List<String> realBridges, List<String> selectBridges) {
+        List<StringBuilder> maps = List.of(new StringBuilder(), new StringBuilder());
+        for (int index = 0; index < selectBridges.size(); index++) {
+            String selectBridge = selectBridges.get(index);
+            String realBridge = realBridges.get(index);
+            updateMaps(maps, realBridge, selectBridge);
+        }
+        return convertStringBuilderListToString(maps);
+    }
+
+    private static void updateMaps(List<StringBuilder> maps, String realBridge, String selectBridge) {
+        if (selectBridge.equals(MovementType.UP.getCode())) {
+            updateUpMap(maps, realBridge, selectBridge);
+            return;
+        }
+        updateDownMap(maps, realBridge, selectBridge);
+    }
+
+    private static void updateUpMap(List<StringBuilder> maps, String realBridge, String selectBridge) {
+        StringBuilder up = maps.get(MovementType.UP.getIndex());
+        StringBuilder down = maps.get(MovementType.DOWN.getIndex());
+        if (realBridge.equals(selectBridge)) {
+            up.append(O);
+            down.append(BLANK);
+            return;
+        }
+        up.append(X);
+        down.append(BLANK);
+    }
+
+    private static void updateDownMap(List<StringBuilder> maps, String realBridge, String selectBridge) {
+        StringBuilder up = maps.get(MovementType.UP.getIndex());
+        StringBuilder down = maps.get(MovementType.DOWN.getIndex());
+        if (realBridge.equals(selectBridge)) {
+            down.append(O);
+            up.append(BLANK);
+            return;
+        }
+        down.append(X);
+        up.append(BLANK);
+    }
+
+    private static String convertStringBuilderListToString(List<StringBuilder> maps) {
+        StringBuilder stringBuilder = new StringBuilder("[");
+        stringBuilder.append(maps.get(MovementType.UP.getIndex()));
+        stringBuilder.replace(stringBuilder.length() - 1, stringBuilder.length(), "]");
+        stringBuilder.append("\n[");
+        stringBuilder.append(maps.get(MovementType.DOWN.getIndex()));
+        stringBuilder.replace(stringBuilder.length() - 1, stringBuilder.length(), "]");
+        return stringBuilder.toString();
     }
 
     /**
