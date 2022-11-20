1e962ea223ccd8ae141cd5ae234c49a6aa1ef05c
Gibeom Gwon <gb.gwon@stackframe.dev>
2022-11-20 13:36:05 +0900

    test: 실패 여부 테스트 추가

--- src/test/java/bridge/BridgeGameTest.java
+++ src/test/java/bridge/BridgeGameTest.java
@@ -19,4 +19,30 @@ class BridgeGameTest {
 
         assertThat(game.getUserSelect()).containsExactlyElementsOf(List.of("U", "U", "U", "D", "U"));
     }
+
+    @Test
+    void 실패_검사_테스트1() {
+        BridgeGame game = new BridgeGame(new ArrayNumberGenerator(List.of(1, 0, 0, 1, 1)), 5);
+
+        game.move(1);
+        game.move(0);
+        game.move(0);
+        game.move(1);
+        game.move(1);
+
+        assertThat(game.isFailed()).isEqualTo(false);
+    }
+
+    @Test
+    void 실패_검사_테스트2() {
+        BridgeGame game = new BridgeGame(new ArrayNumberGenerator(List.of(1, 0, 0, 1, 1)), 5);
+
+        game.move(1);
+        game.move(0);
+        game.move(1);
+        game.move(1);
+        game.move(1);
+
+        assertThat(game.isFailed()).isEqualTo(true);
+    }
 }
