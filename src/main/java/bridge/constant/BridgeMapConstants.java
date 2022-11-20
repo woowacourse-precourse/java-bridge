f9204d0bc1eb6a59855274f003ae8c86eb2914e3
Haaae <101785628+Haaae@users.noreply.github.com>
2022-11-21 07:58:12 +0900

    refactor(BridgeMapConstants) : 다리이동상태를 나타내는 이중리스트 내부 리스트의 인덱스 표시

--- src/main/java/bridge/constant/BridgeMapConstants.java
+++ src/main/java/bridge/constant/BridgeMapConstants.java
@@ -5,6 +5,8 @@ public class BridgeMapConstants {
     public static final String SUCCESSFUL_MOVE_FORMAT = " O ";
     public static final String FAILED_MOVE_MARK = " X ";
     public static final String NEVER_STEPPED_MARK = "   ";
+    public static final int UP_SIDE_INDEX = 0;
+    public static final int DOWN_SIDE_INDEX = 1;
 
     private BridgeMapConstants() {}
 }
