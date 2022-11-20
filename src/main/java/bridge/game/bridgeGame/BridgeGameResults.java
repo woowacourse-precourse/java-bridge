989f2e9de232e46014d30fece62a44c305d29505
bellljun3395 <195850@jnu.ac.kr>
2022-11-20 18:33:35 +0900

    feat(BridgeGameResults): 생성 객체

---
+++ src/main/java/bridge/game/bridgeGame/BridgeGameResults.java
@@ -0,0 +1,14 @@
+package bridge.game.bridgeGame;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class BridgeGameResults {
+
+    private final List<BridgeGameResult> bridgeResults;
+
+    public BridgeGameResults() {
+        this.bridgeResults = new ArrayList<>();
+    }
+
+}
