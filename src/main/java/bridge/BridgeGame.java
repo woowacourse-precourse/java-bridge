6691ec8af567bb098c7eb27118c9e3217cef4f26
BoyCho <iijokv@naver.com>
2022-11-21 04:12:22 +0900

    feat : 출력용 다리 상태 메시지 생성 기능 추가

--- src/main/java/bridge/BridgeGame.java
+++ src/main/java/bridge/BridgeGame.java
@@ -1,23 +1,57 @@
 package bridge;
 
+import java.util.ArrayDeque;
+import java.util.List;
+
 /**
  * 다리 건너기 게임을 관리하는 클래스
  */
 public class BridgeGame {
 
+    private final OutputView outputView;
+    private final List<String> bridge;
+    private ArrayDeque<String> bridgeForPrint;
+    private int bestRecordPosition;
+    private int currentPosition;
+    private int trialCount;
+
+    public BridgeGame(List<String> bridge) {
+        bridgeForPrint = new ArrayDeque<>();
+        this.bridge = bridge;
+        outputView = new OutputView();
+        bestRecordPosition = currentPosition = 0;
+        trialCount = 1;
+    }
+
     /**
      * 사용자가 칸을 이동할 때 사용하는 메서드
-     * <p>
-     * 이동을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
     public void move() {
+        String moving = new InputView().readMoving();
+        makeBridgeForPrint(moving);
     }
 
     /**
      * 사용자가 게임을 다시 시도할 때 사용하는 메서드
-     * <p>
-     * 재시작을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
     public void retry() {
+    }
+
+    private void addTrialCount() {
+        this.trialCount += 1;
+    }
+
+    // 출력용 다리 상태 메시지 생성 : 사용자 입력이 정답 시 O를, 정답 실패 시 X를 메시지 마지막 부분에 삽입
+    private void makeBridgeForPrint(String moving) {
+        if (!bridgeForPrint.isEmpty()) {
+            String lastPosition = bridgeForPrint.getLast().substring(0,1);
+            bridgeForPrint.removeLast();
+            bridgeForPrint.add(lastPosition);
+        }
+        if (moving.equals(bridge.get(currentPosition))) {
+            bridgeForPrint.add(moving + "O");
+            return;
+        }
+        bridgeForPrint.add(moving + "X");
     }
 }
