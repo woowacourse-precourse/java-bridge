ffe247f20f198a1669cb95e32fcc8942a922251e
Hong Jin-Seop <jinsub1999@khu.ac.kr>
2022-11-20 23:17:03 +0900

    feat: A-1 다리 길이를 입력받는 기능 구현

--- src/main/java/bridge/InputView.java
+++ src/main/java/bridge/InputView.java
@@ -1,5 +1,7 @@
 package bridge;
 
+import camp.nextstep.edu.missionutils.Console;
+
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
  */
@@ -9,7 +11,12 @@ public class InputView {
      * 다리의 길이를 입력받는다.
      */
     public int readBridgeSize() {
-        return 0;
+        String bridgeSize = Console.readLine();
+        int result = Integer.parseInt(bridgeSize);
+        if (result < 3 || result > 20) {
+            throw new IllegalArgumentException("Not Valid");
+        }
+        return result;
     }
 
     /**
