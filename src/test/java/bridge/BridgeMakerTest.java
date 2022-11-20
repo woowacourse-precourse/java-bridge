a61132673b932fab9388b9a8fabb4a0e58bfb61b
chosunghyun18 <krismon@naver.com>
2022-11-21 02:40:52 +0900

    test(bridgemaker) : add bridge make size test

---
+++ src/test/java/bridge/BridgeMakerTest.java
@@ -0,0 +1,18 @@
+package bridge;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.List;
+import org.junit.jupiter.api.Test;
+
+class BridgeMakerTest {
+
+    @Test
+    void get_iput_and_build_bridge_checksize() {
+        BridgeMaker bridgeMaker = new BridgeMaker(null);
+        int targetSize = 4;
+        List<String> bridge=bridgeMaker.makeBridge(targetSize);
+        assertThat(bridge.size()).isEqualTo(targetSize);
+    }
+}
