b81217040f1d26a452cb9d7f94c160e4f97d3efe
miffyKing <whqudghk28@gmail.com>
2022-11-20 16:32:20 +0900

    feat: Change package, method's name, and return type of in/output view

---
+++ src/main/java/bridge/view/InputView.java
@@ -0,0 +1,38 @@
+package bridge.view;
+
+import static camp.nextstep.edu.missionutils.Console.readLine;
+
+/**
+ * 사용자로부터 입력을 받는 역할을 한다.
+ */
+public class InputView {
+
+    /**
+     * 다리의 길이를 입력받는다.
+     */
+    public String readBridgeSize() {
+
+        return inputWithMessage("다리 건너기 게임을 시작합니다. \n\n다리의 길이를 입력해 주세요.");
+    }
+
+    /**
+     * 사용자가 이동할 칸을 입력받는다.
+     */
+    public String readMoving() {
+        return inputWithMessage("이동할 칸을 선택해주세요. (위: U, 아래: D)");
+    }
+
+    public String readRetry() {
+        return inputWithMessage("\n게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)");
+    }
+
+    public String inputWithMessage(final String message) {
+        System.out.println(message);
+        return inputBasicString();
+    }
+
+    public String inputBasicString() {
+        return readLine();
+    }
+
+}
