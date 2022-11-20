d8e9526f952b9e053f8b77f4c09da262ade79fb6
Egenieee <hhgg0925@naver.com>
2022-11-20 18:23:48 +0900

    feat: 입력받은 사다리 길이만큼 다리 객체 생성하는 기능 추가
    - Bridge class 새로 만들어 다리 정보 가지고 있도록 함
    - BridgeRandomNumberGenerator generate() 메서드 이용하여 무작위로 다리 모양 생성하도록 함

---
+++ src/main/java/bridge/Bridge.java
@@ -0,0 +1,18 @@
+package bridge;
+
+import java.util.List;
+
+public class Bridge {
+
+    private final List<String> bridge;
+
+    public Bridge(List<String> bridge) {
+        this.bridge = bridge;
+    }
+
+//    public void printBridge() {
+//        for (String one : bridge) {
+//            System.out.print(one + " ");
+//        }
+//    }
+}
--- src/main/java/controller/Controller.java
+++ src/main/java/controller/Controller.java
@@ -1,5 +1,9 @@
 package controller;
 
+import bridge.Bridge;
+import bridge.BridgeMaker;
+import bridge.BridgeNumberGenerator;
+import bridge.BridgeRandomNumberGenerator;
 import view.InputView;
 import view.OutputView;
 
@@ -8,6 +12,15 @@ public class Controller {
     OutputView outputView = new OutputView();
 
     public void run() {
+        Bridge bridge = createBridge();
+    }
+
+    private Bridge createBridge() {
+        BridgeNumberGenerator BridgeRandomNumberGenerator = new BridgeRandomNumberGenerator();
+        BridgeMaker bridgeMaker = new BridgeMaker(BridgeRandomNumberGenerator);
+
         int bridgeSize = inputView.readBridgeSize();
+
+        return new Bridge(bridgeMaker.makeBridge(bridgeSize));
     }
 }
