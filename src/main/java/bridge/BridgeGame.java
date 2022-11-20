460ad53c08a9ff2ecfa79a3c9a7610af7a063228
Danden1 <goh1211@ajou.ac.kr>
2022-11-20 19:50:27 +0900

    fix : OutputView 기능 구현
    출력
    - 게임 시작
    - 다리 길이 입력
    - 이동할 칸 선택
    - 결과 출력
    - 게임 재시도 여부

--- src/main/java/bridge/BridgeGame.java
+++ src/main/java/bridge/BridgeGame.java
@@ -1,10 +1,21 @@
 package bridge;
 
+import java.util.List;
+
 /**
  * 다리 건너기 게임을 관리하는 클래스
  */
 public class BridgeGame {
 
+
+    private Bridge bridge = null;
+    private Bridge userBridge = null;
+    private InputView inputView = new InputView();
+    private OutputView outputView = new OutputView();
+
+    public void startGame(){
+
+    }
     /**
      * 사용자가 칸을 이동할 때 사용하는 메서드
      * <p>
