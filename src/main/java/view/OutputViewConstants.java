126ca9b3a4a75b2cfb36311ed051797026844e90
June <68291395+june-777@users.noreply.github.com>
2022-11-20 17:02:06 +0900

    feat(OutputViewConstants): 최종 게임 결과 출력하기 기능에 사용하는 상수 추가

--- src/main/java/view/OutputViewConstants.java
+++ src/main/java/view/OutputViewConstants.java
@@ -7,6 +7,10 @@ public class OutputViewConstants {
 	public static final String OUTPUT_TEXT_DO_NOT_GO = "   ";
 	public static final String OUTPUT_TEXT_MOVING_SUCCESS = " O ";
 	public static final String OUTPUT_TEXT_MOVING_FAIL = " X ";
+	public static final String OUTPUT_TEXT_FINAL_GAME_RESULT = "최종 게임 결과";
+	public static final String OUTPUT_TEXT_GAME_SUCCESS = "게임 성공 여부: 성공";
+	public static final String OUTPUT_TEXT_GAME_FAIL = "게임 성공 여부: 실패";
+	public static final String OUTPUT_TEXT_TOTAL_GAME_ROUND = "총 시도한 횟수: ";
 	
 	private OutputViewConstants() {
 	}
