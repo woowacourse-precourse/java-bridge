70f1ce1e1cba26ff1fb70feac35fc5e28e5a5346
DevJay1024 <devjay1024@gmail.com>
2022-11-20 19:11:33 +0900

    fix: 안전경로 생성 시, 다리 길이(경로 길이) 유효성 미검증 실수 수정

--- src/main/java/bridge/BridgeMaker.java
+++ src/main/java/bridge/BridgeMaker.java
@@ -1,5 +1,6 @@
 package bridge;
 
+import bridge.exceptions.CustomIllegalArgumentException;
 import bridge.identifiers.Direction;
 import java.util.ArrayList;
 import java.util.List;
@@ -20,6 +21,7 @@ public class BridgeMaker {
      * @return 입력받은 길이에 해당하는 다리 모양. 위 칸이면 "U", 아래 칸이면 "D"로 표현해야 한다.
      */
     public List<String> makeBridge(int size) {
+        validate(size);
         List<String> securePath = new ArrayList<>(size);
         while(size-- != 0) {
             int bridgeNumber = bridgeNumberGenerator.generate();
@@ -29,6 +31,13 @@ public class BridgeMaker {
             securePath.add(direction.getAsString());
         }
         return securePath;
+    }
+
+    private void validate(int size){
+        if(size < 3 || 20 < size)
+            throw new CustomIllegalArgumentException(
+                    "다리의 길이는 3이상 20이하의 값이어야 합니다."
+            );
     }
 
     private Direction convertBridgeNumber(int number) {
--- src/test/java/bridge/BridgeMakerTest.java
+++ src/test/java/bridge/BridgeMakerTest.java
@@ -1,11 +1,11 @@
 package bridge;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.assertj.core.util.Lists.newArrayList;
 
 import bridge.ApplicationTest.TestNumberGenerator;
 import java.util.List;
-import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class BridgeMakerTest {
@@ -19,11 +19,27 @@ class BridgeMakerTest {
     }
 
     @Test
-    void 안전경로_생성_오류_테스트() {
+    void 안전경로_생성_중_랜덤_오류_테스트() {
         BridgeNumberGenerator numberGenerator = new TestNumberGenerator(newArrayList(1, 0, 2));
         BridgeMaker bridgeMaker = new BridgeMaker(numberGenerator);
         List<String> bridge = bridgeMaker.makeBridge(3);
         assertThat(bridge).isEqualTo(null);
+    }
+
+    @Test
+    void 안전경로_생성_중_다리_길이가_가능범위보다_작아서_예외처리된다(){
+        assertThatThrownBy(()-> {
+            BridgeMaker bridgeMaker = new BridgeMaker(new BridgeRandomNumberGenerator());
+            bridgeMaker.makeBridge(2);
+        }).isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void 안전경로_생성_중_다리_길이가_가능범위보다_커서_예외처리된다(){
+        assertThatThrownBy(()-> {
+            BridgeMaker bridgeMaker = new BridgeMaker(new BridgeRandomNumberGenerator());
+            bridgeMaker.makeBridge(2);
+        }).isInstanceOf(IllegalArgumentException.class);
     }
 
 }
