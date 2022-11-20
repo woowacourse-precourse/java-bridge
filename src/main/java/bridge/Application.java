5796316b3fe0f5b5bb91ac0d03876180e7212ac0
bellljun3395 <195850@jnu.ac.kr>
2022-11-20 19:18:31 +0900

    feat(Application): 구현

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -1,8 +1,18 @@
 package bridge;
 
+import bridge.game.Game;
+
 public class Application {
 
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        Game game = new Game();
+        game.start();
+        game.initBridgeGame();
+        while (!game.isQuit()) {
+            game.playBridgeGame();
+            game.isRetry();
+        }
+        game.end();
     }
 }
