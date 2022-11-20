6b38d3613fa4bb3a69182fe96bf18333f3264204
zcae1020 <zcae1020@naver.com>
2022-11-20 16:16:49 +0900

    feat: 결과값을 출력하는 기능 구현

--- src/main/java/bridge/OutputView.java
+++ src/main/java/bridge/OutputView.java
@@ -7,6 +7,8 @@ import java.util.List;
  */
 public class OutputView {
 
+    private final BridgeGame bridgeGame = new BridgeGame();
+
     public void printGameStart() {
         System.out.println("다리 건너기 게임을 시작합니다.");
     }
@@ -64,5 +66,14 @@ public class OutputView {
      * @param userRoute
      */
     public void printResult(int trial, List<String> bridge, List<String> userRoute) {
+        System.out.println("최종 게임 결과");
+        printMap(userRoute, bridge);
+        System.out.println("게임 성공 여부: " +
+                convertSuccessToString(bridgeGame.checkCrossingBridge(bridge)));
+        System.out.println("총 시도한 횟수: " + trial);
+    }
+
+    private String convertSuccessToString(boolean success) {
+        return null;
     }
 }
