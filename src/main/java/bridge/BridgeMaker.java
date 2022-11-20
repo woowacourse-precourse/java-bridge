abe716feff4e0af7a73cf4cd8eb2097a726c5538
안광민 <creativeduck0518@gmail.com>
2022-11-21 01:14:07 +0900

    feat(BridgeMaker): 입력받은 다리 길이만큼의 다리 생성 기능 구현

--- src/main/java/bridge/BridgeMaker.java
+++ src/main/java/bridge/BridgeMaker.java
@@ -1,5 +1,6 @@
 package bridge;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import static bridge.data.GameCommand.DOWN;
@@ -17,7 +18,14 @@ public class BridgeMaker {
     }
 
     public List<String> makeBridge(int size) {
-        return null;
+        List<String> bridge = new ArrayList<>();
+
+        for (int i = 0; i < size; i++) {
+            int number = bridgeNumberGenerator.generate();
+            bridge.add(createCommand(number));
+        }
+
+        return bridge;
     }
 
     private String createCommand(int number) {
