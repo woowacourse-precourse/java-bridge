4c6ec73746defea2a5aba494c3996ffe88a2bf88
Seiyon Park <seiyonpdev@gmail.com>
2022-11-21 07:57:54 +0900

    feat(BridgeGame): 메소드 move 구현
    다리를 한 칸 건너는 메소드를 구현했다

--- src/main/java/bridge/BridgeGame.java
+++ src/main/java/bridge/BridgeGame.java
@@ -1,16 +1,27 @@
 package bridge;
 
+import java.util.*;
+
 /**
  * 다리 건너기 게임을 관리하는 클래스
  */
 public class BridgeGame {
+    private int playerLocationIndex = -1;
+    private Boolean askRetryMarker = false;
+    private List<String> upMap = new ArrayList<>();
+    private List<String> downMap = new ArrayList<>();
 
     /**
      * 사용자가 칸을 이동할 때 사용하는 메서드
      * <p>
      * 이동을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void move() {
+    public void move(List<String> bridge, String playerChoice) {
+        String isRightAnswer;
+
+        playerLocationIndex++;
+        isRightAnswer = checkAnswer(bridge, playerChoice);
+        updateMap(playerChoice, isRightAnswer);
     }
 
     /**
