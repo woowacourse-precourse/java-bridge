05e1e821891785c230ac5ee3cdcfb2804e9b7504
jinhyeon <vmflwu123@gmail.com>
2022-11-20 09:51:56 +0900

    feat(view/InputView): 플레이어 입력 메서드 추가
    1. 다리 길이
    2. 이동할 칸
    3. 재시작 or 종료
    위 값을 입력받을 메서드를 구현

--- src/main/java/bridge/view/InputView.java
+++ src/main/java/bridge/view/InputView.java
@@ -1,28 +1,25 @@
 package bridge.view;
 
-/**
- * 사용자로부터 입력을 받는 역할을 한다.
- */
+import camp.nextstep.edu.missionutils.Console;
+
 public class InputView {
 
-    /**
-     * 다리의 길이를 입력받는다.
-     */
-    public int readBridgeSize() {
-        return 0;
+    private static final String INPUT_BRIDGE_LENGTH = "다리의 길이를 입려해주세요.";
+    private static final String SELECT_BRIDGE_BLOCK = "이동할 칸을 선택해주세요. (위: U, 아래: D)";
+    private static final String SELECT_RETRY_OR_QUIT = "게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)";
+
+    public Integer readBridgeLength() {
+        System.out.println(INPUT_BRIDGE_LENGTH);
+        return Integer.valueOf(Console.readLine());
     }
 
-    /**
-     * 사용자가 이동할 칸을 입력받는다.
-     */
-    public String readMoving() {
-        return null;
+    public String readSelectedBridgeBlock() {
+        System.out.println(SELECT_BRIDGE_BLOCK);
+        return Console.readLine();
     }
 
-    /**
-     * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
-     */
     public String readGameCommand() {
-        return null;
+        System.out.println(SELECT_RETRY_OR_QUIT);
+        return Console.readLine();
     }
 }
