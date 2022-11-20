113f38e7913218db0fd4f9f23c118774cda81d7c
Danden1 <goh1211@ajou.ac.kr>
2022-11-20 18:38:21 +0900

    feat : 이동할 칸 입력받는 기능 및 게임 재시도 여부 입력받는 기능 추가

--- src/main/java/bridge/BridgeMaker.java
+++ src/main/java/bridge/BridgeMaker.java
@@ -1,5 +1,6 @@
 package bridge;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -13,11 +14,16 @@ public class BridgeMaker {
         this.bridgeNumberGenerator = bridgeNumberGenerator;
     }
 
-    /**
-     * @param size 다리의 길이
-     * @return 입력받은 길이에 해당하는 다리 모양. 위 칸이면 "U", 아래 칸이면 "D"로 표현해야 한다.
-     */
-    public List<String> makeBridge(int size) {
-        return null;
+
+    public List<String> makeBridge(int size) throws IllegalArgumentException{
+        List<String> bridge = new ArrayList<>();
+        int input = 0;
+
+        for(int i = 0; i < size; i++){
+            input = bridgeNumberGenerator.generate();
+            bridge.add(BridgeCondition.findByInput(input).getDataValue());
+        }
+
+       return new ArrayList<>(bridge);
     }
 }
