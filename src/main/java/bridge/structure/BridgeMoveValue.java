a0a3b6bb955445850b99ba17ec6cd3e7787f8986
bellljun3395 <195850@jnu.ac.kr>
2022-11-20 18:29:41 +0900

    feat(BridgeMoveValue): 구현 generate

--- src/main/java/bridge/structure/BridgeMoveValue.java
+++ src/main/java/bridge/structure/BridgeMoveValue.java
@@ -1,10 +1,17 @@
 package bridge.structure;
 
+import java.util.Arrays;
+
 public enum BridgeMoveValue {
 
     DOWN(0, "D"),
     UP(1, "U"),
     ;
+
+    private static final String WARM = "[ERROR]";
+    private static final String WARM_INNER_ERROR = WARM + " 죄송합니다. 내부 오류 입니다!";
+    private static final String WARM_CHOICE_AGAIN = WARM + " 디시 선택해 주세요";
+
 
     private final int numberIdentifier;
     private final String stringIdentifier;
@@ -22,4 +29,18 @@ public enum BridgeMoveValue {
         return stringIdentifier;
     }
 
+    public static BridgeMoveValue generate(int numberIdentifier) {
+        return Arrays.stream(BridgeMoveValue.values())
+                .filter(bridgeMove -> bridgeMove.getNumberIdentifier() == numberIdentifier)
+                .findFirst()
+                .orElseThrow(() -> new IllegalArgumentException(WARM_INNER_ERROR));
+    }
+
+    public static BridgeMoveValue generate(String stringIdentifier) {
+        return Arrays.stream(BridgeMoveValue.values())
+                .filter(bridgeMove -> bridgeMove.getStringIdentifier()
+                        .equals(stringIdentifier))
+                .findFirst()
+                .orElseThrow(() -> new IllegalArgumentException(WARM_CHOICE_AGAIN));
+    }
 }
