581f97a06dc7d8b093ee2f37eea627ab17168d20
Ji-Un-Gil <wldns2577@gmail.com>
2022-11-20 19:26:30 +0900

    feat(OutputView.java): printMap function Added function: - 현재까지 이동한 다리의 상태를 정해진 형식에 맞게 출력하는 기능 구현

--- src/main/java/bridge/OutputView.java
+++ src/main/java/bridge/OutputView.java
@@ -12,7 +12,17 @@ public class OutputView {
      * <p>
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void printMap() {
+    public void printMap(List<String> bridgeStatus) {
+        String bridgeUpStatus = "[ ", bridgeDownStatus = "[ ";
+        List<String> currentStatus;
+        for (int i = 0; i < bridgeStatus.size(); i++){
+            currentStatus = getbridgeStatus(bridgeStatus, i);
+            bridgeUpStatus += currentStatus.get(0) + "| ";
+            bridgeDownStatus += currentStatus.get(1) + "| ";
+        }
+        bridgeUpStatus.replaceAll("| $", "]");
+        bridgeDownStatus.replaceAll("| $", "]");
+        System.out.printf(bridgeUpStatus + "\n" + bridgeDownStatus + "\n");
     }
 
     /**
