4131575a73bd109dc0d7900d7000e92baaf55549
saint6839 <saint6839@gmail.com>
2022-11-20 19:05:36 +0900

    test(domain) : 불필요 테스트 케이스 제거

--- src/test/java/bridge/BridgeGameStatusTest.java
+++ src/test/java/bridge/BridgeGameStatusTest.java
@@ -12,16 +12,16 @@ class BridgeGameStatusTest {
 
     @DisplayName("사용자가 재시도,종료 여부에 R또는 Q가 아닌 값을 입력했을 때 예외를 던지는지 테스트")
     @Test
-    void changeBridgeGameStatusByInvalidInput() {
+    void changeStatusByInvalidInput() {
         //given
         String command = "C";
         BridgeGameStatus status = new BridgeGameStatus(command);
         //when
         //then
         assertThatThrownBy(() -> {
-                      status.changeStatus(command);
-                  })
-                  .isInstanceOf(IllegalArgumentException.class);
+            status.changeStatus(command);
+        })
+                .isInstanceOf(IllegalArgumentException.class);
     }
 
     @DisplayName("command 값이 R과 동일해서 계속 실행이 가능한 상태라면 참을 반환하는지 테스트")
@@ -46,9 +46,5 @@ class BridgeGameStatusTest {
         //then
         assertThat(isRunning)
                 .isFalse();
-    }
-
-    @Test
-    void changeStatus() {
     }
 }
