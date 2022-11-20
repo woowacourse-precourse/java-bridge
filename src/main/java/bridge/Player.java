84fc28b2c312ee0c5da7cb6dd68a2203735c207e
kanghyki <kanghyki@gmail.com>
2022-11-20 21:32:50 +0900

    refactor: 플레이어 move 메서드 구현 및 적용

--- src/main/java/bridge/Player.java
+++ src/main/java/bridge/Player.java
@@ -17,20 +17,24 @@ public class Player {
 
     private void validateGetCommand() {
         if (command == null) {
-            throw new IllegalArgumentException(ErrorMessageGenerator.generate("플레이어에게 커맨드가 설정되지 않았습니다."));
+            throw new IllegalStateException(ErrorMessageGenerator.generate("플레이어에게 커맨드가 설정되지 않았습니다."));
+        }
+    }
+
+    public void move(String command) {
+        validateMove(command);
+        this.command = command;
+        this.position += 1;
+    }
+
+    private static void validateMove(String command) {
+        if (!command.equals(BridgeGame.CMD_DOWN) && !command.equals(BridgeGame.CMD_UP)) {
+            throw new IllegalStateException(ErrorMessageGenerator.generate("올바르지 않은 move 명령어입니다."));
         }
     }
 
     public int getIndex() {
         return position - 1;
-    }
-
-    public void setCommand(String command) {
-        this.command = command;
-    }
-
-    public void setPosition(int position) {
-        this.position = position;
     }
 
     public int getPosition() {
