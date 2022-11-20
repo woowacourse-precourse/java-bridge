85f2c3a9aa448e16079f423774b8b9674593aabe
H4R1B0 <qkrguswns25@naver.com>
2022-11-20 21:32:07 +0900

    refactor: remove hard code

--- src/main/java/bridge/Calculator.java
+++ src/main/java/bridge/Calculator.java
@@ -1,13 +1,19 @@
 package bridge;
 
 public class Calculator {
+    private static final String WORD_UP = "U";
+    private static final String WORD_DOWN = "D";
+    private static final String WORD_SPACE = " ";
+    private static final String WORD_SUCCESS = "O";
+    private static final String WORD_FAIL = "X";
+
     public static Bridge moveCalculate(String current, String moving) {
-        if (!current.equals(moving) && current.equals("U"))
-            return new Bridge(" ", "X");
-        if (!current.equals(moving) && current.equals("D"))
-            return new Bridge("X", " ");
-        if (current.equals("U"))
-            return new Bridge("O", " ");
-        return new Bridge(" ", "O");
+        if (!current.equals(moving) && current.equals(WORD_UP))
+            return new Bridge(WORD_SPACE, WORD_FAIL);
+        if (!current.equals(moving) && current.equals(WORD_DOWN))
+            return new Bridge(WORD_FAIL, WORD_SPACE);
+        if (current.equals(WORD_UP))
+            return new Bridge(WORD_SUCCESS, WORD_SPACE);
+        return new Bridge(WORD_SPACE, WORD_SUCCESS);
     }
 }
