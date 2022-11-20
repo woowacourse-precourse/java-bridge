4a73e6f0484bf203ca4778b1e209093a500949a7
eastsage <dpwk8@naver.com>
2022-11-21 02:43:05 +0900

    feat: 게임 진행을 위해 입력 요청문을 출력하는 함수 추가
    AskBridgeLength, AskNextMove, AskRetry

--- src/main/java/bridge/view/OutputView.java
+++ src/main/java/bridge/view/OutputView.java
@@ -8,10 +8,24 @@ public class OutputView {
     private static final String ASK_BRIDGE_LENGTH_MESSAGE = "다리의 길이를 입력해주세요.";
     private static final String ASK_NEXT_MOVE_MESSAGE = "이동할 칸을 선택해주세요. (위: U, 아래: D)";
     private static final String ASK_RETRY_MESSAGE = "게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)";
+    private String map;
 
     public void printStart() {
-        System.out.println(GAME_START_MESSAGE + "\n");
+        System.out.println(GAME_START_MESSAGE + System.lineSeparator());
     }
+
+    public void AskBridgeLength() {
+        System.out.println(ASK_BRIDGE_LENGTH_MESSAGE);
+    }
+
+    public void AskNextMove() {
+        System.out.println(ASK_NEXT_MOVE_MESSAGE);
+    }
+
+    public void AskRetry() {
+        System.out.println(ASK_RETRY_MESSAGE);
+    }
+
     /**
      * 현재까지 이동한 다리의 상태를 정해진 형식에 맞춰 출력한다.
      * <p>
