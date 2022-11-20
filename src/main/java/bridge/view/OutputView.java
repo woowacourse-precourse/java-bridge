af0539fb3cd88a13155494c936917b08af614ec4
JunginPark4364 <wjddlsee@gmail.com>
2022-11-21 02:40:11 +0900

    feat: 시작 메시지 출력

--- src/main/java/bridge/view/OutputView.java
+++ src/main/java/bridge/view/OutputView.java
@@ -1,9 +1,15 @@
-package bridge;
+package bridge.view;
 
 /**
  * 사용자에게 게임 진행 상황과 결과를 출력하는 역할을 한다.
+ *
+ * 메서드 이름 변경X
+ * 인자, 반환 타입은 변경 가능
+ * 값 출력을 위해 필요한 메서드 추가 가능
  */
 public class OutputView {
+
+    private static final String START_MESSAGE = "다리 건너기 게임을 시작합니다.";
 
     /**
      * 현재까지 이동한 다리의 상태를 정해진 형식에 맞춰 출력한다.
@@ -19,5 +25,9 @@ public class OutputView {
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
     public void printResult() {
+    }
+
+    public void printStart() {
+        System.out.println(START_MESSAGE);
     }
 }
