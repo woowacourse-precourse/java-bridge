38042a416940288e87c2698602f3ff0f347af360
alstjr706 <alstjr706@naver.com>
2022-11-20 11:51:50 +0900

    feat : (다리 건너기) 상수로 콘솔 메시지 생성

---
+++ src/main/java/bridge/constants/SystemMessage.java
@@ -0,0 +1,13 @@
+package bridge.constants;
+
+public final class SystemMessage {
+
+    public static final String BRIDGE_GAME_START = "다리 건너기 게임을 시작합니다.";
+    public static final String BRIDGE_LENGTH_INPUT = "다리의 길이를 입력해주세요.";
+    public static final String MOVEMENT_INPUT = "이동할 칸을 선택해주세요. (위: U, 아래: D)";
+    public static final String GAME_RESTART_END = "게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)";
+    public static final String LAST_GAME_RESULT = "최종 게임 결과";
+    public static final String GAME_SUCCESS = "게임 성공 여부: 성공";
+    public static final String GAME_FAILURE = "게임 성공 여부: 실패";
+    public static final String ALL_TRY_COUNT = "총 시도한 횟수: ";
+}
