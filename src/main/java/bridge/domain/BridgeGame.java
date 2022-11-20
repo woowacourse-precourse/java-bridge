5c75a4d2d3508a7b09eb06f7156ec28a19369088
CAEL0 <kjongmin26@gmail.com>
2022-11-21 00:38:53 +0900

    feat(BridgeGame): 재시작 여부를 입력받고 명령 수행

--- src/main/java/bridge/domain/BridgeGame.java
+++ src/main/java/bridge/domain/BridgeGame.java
@@ -77,9 +77,12 @@ public class BridgeGame {
      * <p>
      * 재시작을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void retry() {
-        path.clear();
-        attemptTimes++;
+    public void retry(String gameCommand) {
+        isCorrectGameCommand(gameCommand);
+        if (gameCommand.equals(BridgeConstants.RESTART)) {
+            path.clear();
+            attemptTimes++;
+        }
     }
 
     private void isCorrectGameCommand(String gameCommand) {
