dffa648e277600810a9ede57dc6cccc768b6a53d
HGB <hsw8147@gmail.com>
2022-11-21 03:41:52 +0900

    fix: InputViewTest Import
    InputviewTest에서 쓰이지 않는 import 정리

--- src/test/java/bridge/InputViewTest.java
+++ src/test/java/bridge/InputViewTest.java
@@ -1,9 +1,6 @@
 package bridge;
 
-import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeTest;
-import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.util.Lists.newArrayList;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
