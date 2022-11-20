00d3ccd6f80e34f4c9e3051aec12ac13dc84b5f7
twoosky <lhjae3@naver.com>
2022-11-21 01:21:41 +0900

    fix(BridgeGameResult): 게임 결과값 저장 인스턴스 변수 타입 변경 및 로직 수정

--- src/main/java/bridge/domain/BridgeGameResult.java
+++ src/main/java/bridge/domain/BridgeGameResult.java
@@ -1,28 +1,48 @@
 package bridge.domain;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class BridgeGameResult {
-    private final Map<String, String> result;
+    private static final String BRIDGE_UP_SIDE_SHAPE = "U";
+    private static final String BRIDGE_DOWN_SIDE_SHAPE = "D";
+    private static final String NOT_MOVING_SHAPE = " ";
+    private static final String BRIDGE_NOT_MOVING = "X";
+
+    private final Map<String, List<String>> result;
+    private int count;
 
     public BridgeGameResult() {
         this.result = new HashMap<>();
+        this.count = 0;
+        initResult();
+    }
+
+    private void initResult() {
+        result.put(BRIDGE_UP_SIDE_SHAPE, new ArrayList<>());
+        result.put(BRIDGE_DOWN_SIDE_SHAPE, new ArrayList<>());
     }
 
     public void putMovingResult(String moving, String movingResult) {
-        result.put(moving, movingResult);
+        result.get(moving).add(movingResult);
+        result.entrySet().stream()
+                .filter(entry -> !entry.getKey().equals(moving))
+                .forEach(entry -> entry.getValue().add(NOT_MOVING_SHAPE));
     }
 
-    public boolean isSuccess(int bridgeSize) {
-        return result.size() == bridgeSize;
-    }
-
-    public Map<String, String> getResult() {
-        return result;
+    public boolean isSuccess() {
+        return result.values().stream()
+                .noneMatch(value -> value.contains(BRIDGE_NOT_MOVING));
     }
 
     public void clearResult() {
-        result.clear();
+        result.get(BRIDGE_UP_SIDE_SHAPE).clear();
+        result.get(BRIDGE_DOWN_SIDE_SHAPE).clear();
+    }
+
+    public List<String> getBridgeByShape(String shape) {
+        return result.get(shape);
     }
 }
