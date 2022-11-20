e2d7f82aca969eb20311641edc7b02d2ea50746e
3juhwan <13selfesteem91@naver.com>
2022-11-20 11:09:44 +0900

    refactor(bridge): 메세지 수정

--- src/main/java/bridge/enums/InputMessage.java
+++ src/main/java/bridge/enums/InputMessage.java
@@ -4,7 +4,7 @@ public enum InputMessage {
 
     ENTER_BRIDGE_LENGTH_MESSAGE("다리의 길이를 입력해주세요."),
     SELECT_CELL_TO_MOVE_MESSAGE("이동할 칸을 선택해주세요. (위: U, 아래: D)"),
-    ENTER_WHETHER_TO_CONTINUE_GAME_MESSAGE("게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)"),
+    ENTER_WHETHER_TO_RETRY_GAME_MESSAGE("게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)"),
     ;
 
     private final String message;
