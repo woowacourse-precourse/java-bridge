9f312767f16a4b2580b111624bb2cfea8011e2d9
zzun <736300@naver.com>
2022-11-20 23:03:46 +0900

    feat(Application): 다리 건너기 게임 시작 부분 구현

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -1,8 +1,13 @@
 package bridge;
 
 public class Application {
+    private static final String START_BRIDGE_GAME_MESSAGE = "다리 건너기 게임을 시작합니다.\n";
 
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        Controller controller = new Controller();
+        System.out.println(START_BRIDGE_GAME_MESSAGE);
+        controller.start();
+
     }
 }
