7c2bd566ab9a6f8e98eb1d0e46ad238b58c5a89b
bfly_notebook <kkyxw@soongsil.ac.kr>
2022-11-20 19:30:52 +0900

    style: add the several directory such as domain, view, validator, and move the files to them

--- src/main/java/validator/MoveValidator.java
+++
@@ -1,9 +0,0 @@
-package validator;
-
-public class MoveValidator {
-	public static void validateMovingPath(String input) {
-		if (!(input.equals("U") || input.equals("D"))) {
-			throw new IllegalArgumentException("[ERROR] 이동할 칸은 'U'(위 칸) 또는 'D'(아래 칸)로 입력해야 합니다.");
-		}
-	}
-}
--- src/main/java/validator/RetryValidator.java
+++
@@ -1,9 +0,0 @@
-package validator;
-
-public class RetryValidator {
-	public static void validateRetryCommand(String retryCommand) {
-		if (!(retryCommand.equals("R") || retryCommand.equals("Q"))) {
-			throw new IllegalArgumentException("[ERROR] 재시도/종료 선택을 위해 'R'(재시작) 또는 'Q'(종료)를 입력해야 합니다.");
-		}
-	}
-}
