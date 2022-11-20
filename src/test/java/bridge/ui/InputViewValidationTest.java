b61e794fcca62cc28defdf5b2466c680130ca90a
jungwoo <rlawjddn103@naver.com>
2022-11-20 21:07:26 +0900

    test(InputViewValidationTest): 숫자가 아닌 수를 정확히 예외처리 하는지 테스트

---
+++ src/test/java/bridge/ui/InputViewValidationTest.java
@@ -0,0 +1,16 @@
+package bridge.ui;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+class InputViewValidationTest {
+
+    @Test
+    void validateIsInteger() {
+        assertThatThrownBy(() -> InputViewValidation.validateIsInteger("123가"))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+}
