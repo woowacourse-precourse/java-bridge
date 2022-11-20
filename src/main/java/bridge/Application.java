de51cdf385ff8823ed661ca600be85f6b1bfd531
namtndus <rnrmf0721@gmail.com>
2022-11-20 14:32:18 +0900

    feat(Application): main함수 기능 구현
    'printGameStartMessage함수를 실행'를 구현

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -2,12 +2,13 @@ package bridge;
 
 public class Application {
 
-    InputView inputView = new InputView();
-    OutputView outputView = new OutputView();
-    BridgeGame bridgeGame = new BridgeGame();
-    BridgeMaker bridgeMaker = new BridgeMaker(new BridgeRandomNumberGenerator());
+    static InputView inputView = new InputView();
+    static OutputView outputView = new OutputView();
+    static BridgeGame bridgeGame = new BridgeGame();
+    static BridgeMaker bridgeMaker = new BridgeMaker(new BridgeRandomNumberGenerator());
 
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        outputView.printGameStartMessage();
     }
 }
