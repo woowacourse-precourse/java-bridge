ec3c45551d703258c9bd72c3d9b24728242d66d3
NotableDeveloper <castlereal@naver.com>
2022-11-19 20:43:32 -0800

    <refactoring> : TestNumberGenerator 클래스 추출 테스트를 위한 NumberGenerator를 별도의 클래스로 추출함.

--- src/test/java/bridge/BridgeMakerTest.java
+++ src/test/java/bridge/BridgeMakerTest.java
@@ -10,18 +10,6 @@ import java.util.List;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class BridgeMakerTest {
-    static class TestNumberGenerator implements BridgeNumberGenerator {
-        private final List<Integer> numbers;
-
-        TestNumberGenerator(List<Integer> numbers) {
-            this.numbers = numbers;
-        }
-
-        @Override
-        public int generate() {
-            return numbers.remove(0);
-        }
-    }
     @DisplayName("길이가 3인 다리를 생성하는 메서드를 호출하면 그 결과로 길이가 3인 다리가 생성되어야 한다.")
     @Test
     void makeBridgeSize3ToSuccessful() {
---
+++ src/test/java/bridge/TestNumberGenerator.java
@@ -0,0 +1,17 @@
+package bridge;
+
+import bridge.BridgeMaker.NumberGenerator.BridgeNumberGenerator;
+import java.util.List;
+
+class TestNumberGenerator implements BridgeNumberGenerator {
+    private final List<Integer> numbers;
+
+    TestNumberGenerator(List<Integer> numbers) {
+        this.numbers = numbers;
+    }
+
+    @Override
+    public int generate() {
+        return numbers.remove(0);
+    }
+}
