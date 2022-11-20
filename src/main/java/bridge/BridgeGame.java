a2af18c5fc4d30343a1f07641df3c947c98d7da2
hojunking96 <junsong96@naver.com>
2022-11-21 00:57:33 +0900

    refactor(BridgeGame.java) : 메소드 이동하여 저장

--- src/main/java/bridge/BridgeGame.java
+++ src/main/java/bridge/BridgeGame.java
@@ -1,10 +1,17 @@
 package bridge;
 
 
+import bridge.objects.DownSide;
+import bridge.objects.UpSide;
+
 public class BridgeGame {
 
     public static void move() {
-        MoveInsert.moveInsert();
+        if (StartGame.isRightWay()) {
+            rightWayMove();
+            return;
+        }
+        wrongWayMove();
     }
 
     public static void retry() {
@@ -12,5 +19,27 @@ public class BridgeGame {
         Application.downSide = new DownSide();
         Application.movingTurn = 1;
         Application.totalTrial++;
+    }
+
+    public static void rightWayMove() {
+        if (Application.movingInput.equals("U")) {
+            Application.upSide.upSideArr.add("O");
+            Application.downSide.downSideArr.add(" ");
+        }
+        if (Application.movingInput.equals("D")) {
+            Application.upSide.upSideArr.add(" ");
+            Application.downSide.downSideArr.add("O");
+        }
+    }
+
+    public static void wrongWayMove() {
+        if (Application.movingInput.equals("U")) {
+            Application.upSide.upSideArr.add("X");
+            Application.downSide.downSideArr.add(" ");
+        }
+        if (Application.movingInput.equals("D")) {
+            Application.upSide.upSideArr.add(" ");
+            Application.downSide.downSideArr.add("X");
+        }
     }
 }
