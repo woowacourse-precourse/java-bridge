0f5c1b2954e4088bbda7743d41baa1fd6c9da617
HyeokJoon Kong <orijoon98@gmail.com>
2022-11-20 21:29:35 +0900

    feat: add fields and constructor in BridgeGame class

--- src/main/java/bridge/model/BridgeGame.java
+++ src/main/java/bridge/model/BridgeGame.java
@@ -1,9 +1,45 @@
 package bridge.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * 다리 건너기 게임을 관리하는 클래스
  */
 public class BridgeGame {
+
+    private final List<String> bridge = new ArrayList<>();
+    private final List<String> map = new ArrayList<>();
+    private int countOfAttempts;
+    private boolean needsRetry; // true 일 경우 재시도하거나 종료해야 함
+    private boolean success; // true 일 경우 게임 성공
+
+    public BridgeGame(List<String> bridge) {
+        this.bridge.addAll(bridge);
+        this.countOfAttempts = 1;
+        this.needsRetry = false;
+        this.success = false;
+    }
+
+    public List<String> getBridge() {
+        return bridge;
+    }
+
+    public List<String> getMap() {
+        return map;
+    }
+
+    public int getCountOfAttempts() {
+        return countOfAttempts;
+    }
+
+    public boolean isNeedsRetry() {
+        return needsRetry;
+    }
+
+    public boolean isSuccess() {
+        return success;
+    }
 
     /**
      * 사용자가 칸을 이동할 때 사용하는 메서드
