e8ba137b855872ed70a597b3df86c3e5aa1af2b9
Eungyu-code <Agyu625@gmail.com>
2022-11-20 18:00:38 +0900

    feat(OutputView class): printGameStart 메소드에 '\n' 문자를 출력하는 출력문 추가

--- src/main/java/bridge/OutputView.java
+++ src/main/java/bridge/OutputView.java
@@ -42,6 +42,7 @@ public class OutputView {
 
     public void printGameStart() {
         System.out.println("다리 건너기 게임을 시작합니다.");
+        System.out.println();
     }
 
     public void printBridgeSizeInputBox() {
