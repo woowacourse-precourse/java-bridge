df9fb697f4d13363e4e12008e614e09ae50850fa
yoonjuho <dbswn990@naver.com>
2022-11-20 17:29:36 +0900

    feat(GameMessage): 고정된 문자열 상수 처리

---
+++ src/main/java/bridge/view/GameMessage.java
@@ -0,0 +1,18 @@
+package bridge.view;
+
+public class GameMessage {
+    public static final String STARTING_GAME = "다리 건너기 게임을 시작합니다.";
+    public static final String SET_GAME = "다리의 길이를 입력해주세요.";
+    public static final String RUNNING_GAME = "이동할 칸을 선택해주세요. (위: U, 아래: D)";
+    public static final String GAME_OVER = "게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)";
+    public static final String GAME_RESULT = "최종 게임 결과";
+    public static final String GAME_WIN_OR_LOSE = "게임 성공 여부: ";
+    public static final String WIN = "성공";
+    public static final String LOSE = "실패";
+    public static final String ATTEMPT_COUNT = "총 시도한 횟수: %d";
+    public static final String START = "[";
+    public static final String CONTOUR = "|";
+    public static final String END = "]";
+    public static final String RIGHT = " O ";
+    public static final String WRONG = " X ";
+}
