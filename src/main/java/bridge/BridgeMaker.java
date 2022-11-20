254f12aae2f784329ab2777230d9defc215a0031
haisley77 <qhfg3145@naver.com>
2022-11-20 17:07:20 +0900

    style: 변수 이름 변경

--- src/main/java/bridge/BridgeMaker.java
+++ src/main/java/bridge/BridgeMaker.java
@@ -8,7 +8,7 @@ import java.util.List;
  */
 public class BridgeMaker {
 
-    private static List<String> bridge;
+    public static List<String> bridge_info;
 
     private final BridgeNumberGenerator bridgeNumberGenerator;
 
@@ -21,14 +21,14 @@ public class BridgeMaker {
      * @return 입력받은 길이에 해당하는 다리 모양. 위 칸이면 "U", 아래 칸이면 "D"로 표현해야 한다.
      */
     public List<String> makeBridge(int size) {
-        bridge = new ArrayList<>(size);
+        bridge_info = new ArrayList<>(size);
         for (int i = 0; i < size; i++){
             int random_bridge_number = bridgeNumberGenerator.generate();
             if (random_bridge_number == 0){
-                bridge.add("D");
+                bridge_info.add("D");
             }
             if (random_bridge_number == 1){
-                bridge.add("U");
+                bridge_info.add("U");
             }
         }
         return null;
