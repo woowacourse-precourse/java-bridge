eaa8016f31bbcb9621d5a18bdd166e2dab75ffe0
im-shung <seoyoung1113@gmail.com>
2022-11-20 20:48:23 +0900

    feat: :sparkles: 무작위 값이 0이면 D, 1이면 U를 표현해 다리를 만드는 기능 추가

--- src/main/java/bridge/service/BrideGameService.java
+++ src/main/java/bridge/service/BrideGameService.java
@@ -5,17 +5,29 @@ import static bridge.exception.Validator.*;
 import static bridge.view.InputView.*;
 import static bridge.view.OutputView.*;
 
+import bridge.BridgeRandomNumberGenerator;
+import bridge.maker.BridgeMaker;
+import java.util.List;
+
 public class BrideGameService {
+
+    private int bridgeSize;
+    private List<String> bridges;
 
     public void execute() {
         printBridgeGameStartMessage();
 
         try {
-            int bridgeLength = readBridgeSize();
-            checkBridgeLength(bridgeLength);
+            bridgeSize = readBridgeSize();
+            checkBridgeLength(bridgeSize);
         } catch (NumberFormatException e) {
             throw new NumberFormatException(NOT_INT.getMessage());
         }
+
+        BridgeMaker bridgeMaker = new BridgeMaker(new BridgeRandomNumberGenerator());
+        bridges = bridgeMaker.makeBridge(bridgeSize);
+
     }
+
 
 }
