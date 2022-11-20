295f3334ae42f68e578fcf7f5bc018fc335a23a8
jin8073 <jin8073@gmail.com>
2022-11-20 16:16:38 +0900

    feat: 다리의 길이에 따라 다리 생성

---
+++ src/main/java/bridge/model/BridgeShape.java
@@ -0,0 +1,23 @@
+package bridge.model;
+
+import java.util.Arrays;
+
+public enum BridgeShape {
+    UP("U", 1),
+    DOWN("D", 0);
+
+    private String bridgeShape;
+    private int bridgeRandomNumber;
+
+    BridgeShape(String bridgeShape, int bridgeRandomNumber) {
+        this.bridgeShape = bridgeShape;
+        this.bridgeRandomNumber = bridgeRandomNumber;
+    }
+
+    public static String getShapeFromRandomNumber(int bridgeRandomNumber) {
+        return Arrays.stream(BridgeShape.values())
+                .filter(bridgeShape -> bridgeShape.bridgeRandomNumber == bridgeRandomNumber)
+                .findAny()
+                .orElseThrow().bridgeShape;
+    }
+}
