81c5a1c4b95a6a90dfc73eb585833180c4859b22
jingyu9804 <swc05161@gmail.com>
2022-11-20 20:46:03 +0900

    feat(bridge) : 입력 메시지 상수 선언

--- src/main/java/bridge/view/InputView.java
+++ src/main/java/bridge/view/InputView.java
@@ -1,10 +1,12 @@
-package bridge;
+package bridge.view;
 
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
  */
 public class InputView {
-
+    private static final String INPUT_BRIDGE_SIZE = "다리 길이를 입력해주세요.";
+    private static final String INPUT_READ_MOVING = "이동할 칸을 선택해주세요. (위: U, 아래: D)";
+    private static final String INPUT_READ_GAME_COMMAND = "게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)";
     /**
      * 다리의 길이를 입력받는다.
      */
