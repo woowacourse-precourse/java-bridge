69185eb137356b6908b8a655f4fb55fab7674b85
jungwoo <rlawjddn103@naver.com>
2022-11-21 00:50:06 +0900

    feat(User): User 클래스 생성 및 관리할 인스턴스 변수 구현

---
+++ src/main/java/bridge/domain/User.java
@@ -0,0 +1,14 @@
+package bridge.domain;
+
+import java.util.List;
+
+public class User {
+
+    private List<String> route;
+    private int attemptNum;
+    private int location;
+    private int bridgeSize;
+    private Status status;
+    private Bridge bridge;
+
+}
