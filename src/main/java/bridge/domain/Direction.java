f86f6e2a7f4b17da6bc562dd0d7bcea48553232a
Dabeen Jeong <hello70825@gmail.com>
2022-11-20 20:41:38 +0900

    feat: 이동 방향 enum 추가

---
+++ src/main/java/bridge/domain/Direction.java
@@ -0,0 +1,17 @@
+package bridge.domain;
+
+import java.util.Arrays;
+import java.util.function.Predicate;
+
+public enum Direction {
+    UP("U", (directionValue) -> directionValue == 1),
+    DOWN("D", (directionValue) -> directionValue == 0);
+
+    private final String directionInitial;
+    private final Predicate<Integer> isMatch;
+
+    Direction(String directionInitial, Predicate<Integer> isMatch) {
+        this.directionInitial = directionInitial;
+        this.isMatch = isMatch;
+    }
+}
