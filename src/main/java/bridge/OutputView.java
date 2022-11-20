52fd9d4ba23ca1d061c4da6eaf13be953936d80b
bumwoo <gld55602@gmail.com>
2022-11-21 03:39:50 +0900

    refactor(OutputView): 메소드, 원시값 정리

--- src/main/java/bridge/OutputView.java
+++ src/main/java/bridge/OutputView.java
@@ -7,14 +7,23 @@ import java.util.List;
  * 사용자에게 게임 진행 상황과 결과를 출력하는 역할을 한다.
  */
 public class OutputView {
+    private static final String START = "[ ";
+    private static final String DELIMITER = " | ";
+    private static final String END = " ]";
 
     /**
      * 현재까지 이동한 다리의 상태를 정해진 형식에 맞춰 출력한다.
      * <p>
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void printMap(List<String> map) {
-        System.out.println(map);
+    public void printMap(UserBridge userBridge) {
+        printBridge(userBridge.getUpBridge());
+        printBridge(userBridge.getDownBridge());
+        System.out.println();
+    }
+
+    private static void printBridge(List<String> bridge) {
+        System.out.println(START + String.join(DELIMITER, bridge) + END);
     }
 
     /**
@@ -24,16 +33,14 @@ public class OutputView {
      */
     public void printFailResult(BridgeGameService bridgeGameService) {
         System.out.println("최종 게임 결과");
-        printBridge(bridgeGameService.getUserBridge());
-        System.out.println();
+        printMap(bridgeGameService.getUserBridge());
         System.out.println("게임 성공 여부: 실패");
         printCount(bridgeGameService.getCount());
     }
 
     public void printSuccessResult(BridgeGameService bridgeGameService) {
         System.out.println("최종 게임 결과");
-        printBridge(bridgeGameService.getUserBridge());
-        System.out.println();
+        printMap(bridgeGameService.getUserBridge());
         System.out.println("게임 성공 여부: 성공");
         printCount(bridgeGameService.getCount());
     }
@@ -50,20 +57,11 @@ public class OutputView {
         System.out.println("이동할 칸을 선택해주세요. (위: U, 아래: D)");
     }
 
-    public void printMoving(String moving) {
-        System.out.println(moving);
-    }
-
     public void printCount(int count) {
         System.out.println("총 시도한 횟수: " + count);
     }
 
     public void printRetry() {
         System.out.println("게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)");
-    }
-
-    public void printBridge(UserBridge userBridge) {
-        System.out.println("[ " + String.join(" | ", userBridge.getUpBridge()) + " ]");
-        System.out.println("[ " + String.join(" | ", userBridge.getDownBridge()) + " ]");
     }
 }
