63817bfb09ed16262a4c060fcd4c1e655b7d5d0e
Ridealist <cjsn92@gmail.com>
2022-11-20 21:02:11 +0900

    test(BridgeRandomNumberGeneratorTest): 테스트 클래스 구현

---
+++ src/test/java/bridge/models/BridgeRandomNumberGeneratorTest.java
@@ -0,0 +1,14 @@
+package bridge.models;
+
+import static org.assertj.core.api.Assertions.*;
+
+import org.junit.jupiter.api.RepeatedTest;
+
+class BridgeRandomNumberGeneratorTest {
+
+    @RepeatedTest(50)
+    void generate() {
+        BridgeNumberGenerator bridgeNumberGenerator = new BridgeRandomNumberGenerator();
+        assertThat(bridgeNumberGenerator.generate()).isBetween(0, 1);
+    }
+}
