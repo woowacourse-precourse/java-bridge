5833581aa54338e803d8a7c8908f747b30d6ead8
twoosky <lhjae3@naver.com>
2022-11-21 01:37:46 +0900

    feat(OutputView): 다리 건너기 게임 결과 출력 메서드 구현

--- src/main/java/bridge/view/OutputView.java
+++ src/main/java/bridge/view/OutputView.java
@@ -41,6 +41,10 @@ public class OutputView {
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
     public void printResult(BridgeGameResult bridgeGameResult) {
+        System.out.println(GAME_RESULT);
+        printMap(bridgeGameResult);
+        System.out.println(bridgeGameResult.isSuccess());
+        System.out.println(GAME_COUNT + bridgeGameResult.getCount());
     }
 
     public void printNewLine() {
