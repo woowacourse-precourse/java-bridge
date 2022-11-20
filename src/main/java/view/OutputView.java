c497a2a6c55d1e48c2faf55e1bd81462cd03f3fe
egoing <dudqk9696@naver.com>
2022-11-20 17:09:25 +0900

    feat : 올바른 이동 했을 경우 다리 상태 변경

--- src/main/java/view/OutputView.java
+++ src/main/java/view/OutputView.java
@@ -1,6 +1,7 @@
 package view;
 
 import domain.AllBridge;
+import domain.BridgeGame;
 
 import java.util.List;
 
@@ -8,58 +9,16 @@ import java.util.List;
  * 사용자에게 게임 진행 상황과 결과를 출력하는 역할을 한다.
  */
 public class OutputView {
+    public static String upper = OutputMessage.START.getOutputMsg();
+    public static String lower = OutputMessage.START.getOutputMsg();
 
-    /**
-     * 현재까지 이동한 다리의 상태를 정해진 형식에 맞춰 출력한다.
-     * <p>
-     * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
-     */
-    public void printMap(AllBridge allBridge,int dis) {
-        String upper = makeUpperCase(allBridge,dis);
-        String lower = makeLowerCase(allBridge,dis);
-
-        System.out.println(upper);
-        System.out.println(lower);
-    }
-
-    public String makeUpperCase(AllBridge allBridge,int dis) {
-        List<String> bridge = allBridge.getBridge();
-        String upper = "[";
-        for(int i=0; i<dis-1; i++) {
-            upper += up(bridge.get(i)) + "|";
-        }
-        upper += up(bridge.get(dis-1)) + "]";
-        return upper;
-    }
-
-    public String makeLowerCase(AllBridge allBridge,int dis) {
-        List<String> bridge = allBridge.getBridge();
-        String lower = "[";
-        for(int i=0; i<dis-1; i++) {
-            lower += down(bridge.get(i)) + "|";
-        }
-        lower += down(bridge.get(dis-1)) + "]";
-        return lower;
-    }
-
-    public String up(String str) {
+    public void addString(String str) {
         if(str.equals("U")) {
-            return " O ";
+            upper += OutputMessage.CORRECT.getOutputMsg();
+            lower += OutputMessage.JUMP_BAR.getOutputMsg();
+            return;
         }
-        return "   ";
-    }
-
-    private String down(String str) {
-        if(str.equals("D")) {
-            return " O ";
-        }
-        return "   ";
-    }
-    /**
-     * 게임의 최종 결과를 정해진 형식에 맞춰 출력한다.
-     * <p>
-     * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
-     */
-    public void printResult() {
+        lower += OutputMessage.CORRECT.getOutputMsg();
+        upper += OutputMessage.JUMP_BAR.getOutputMsg();
     }
 }
