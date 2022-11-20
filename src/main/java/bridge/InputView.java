961695105fa3be43898adb1e2d2b96d98b81af6f
ashlovesliitea <skyrider1811@gmail.com>
2022-11-20 18:09:51 +0900

    feat: InputView - 에러 메세지 출력 기능

--- src/main/java/bridge/InputView.java
+++ src/main/java/bridge/InputView.java
@@ -1,16 +1,25 @@
 package bridge;
 
+import static camp.nextstep.edu.missionutils.Console.readLine;
+
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
  */
 public class InputView {
+    private static final String ERROR = "[ERROR] ";
+    private static final String BRIDGE_SIZE_ERROR = "3에서 20 사이의 정수 형태로 입력해주세요.";
 
     /**
      * 다리의 길이를 입력받는다.
      */
-    public int readBridgeSize() {
-        return 0;
+
+    private void printErrorMessage(String errorMsg){
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append(ERROR).append(errorMsg);
+        System.out.println(stringBuilder.toString());
     }
+
+
 
     /**
      * 사용자가 이동할 칸을 입력받는다.
