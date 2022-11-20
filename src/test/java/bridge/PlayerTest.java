fc29389368f6acfcf95307771006b234c573eb8a
60jong <rudwhd515@gmail.com>
2022-11-20 18:59:19 +0900

    test(PlayerTest) : 새로 구현된 Player의 기능을 통해 테스트 케이스 추가

--- src/test/java/bridge/PlayerTest.java
+++ src/test/java/bridge/PlayerTest.java
@@ -9,28 +9,93 @@ import static org.assertj.core.api.Assertions.*;
 
 class PlayerTest {
     @Test
-    public void getLocation_다리_위_자신의_블록_인덱스_계산() throws Exception {
-        Player player = new Player(new Bridge(List.of("U", "U")));
-
-        assertThat(player.getLocation()).isEqualTo(0);
-    }
-
-    @Test
     public void movable_다리_시작점에서_움직일_수_있는지() throws Exception {
-        //given
         Player player = new Player(new Bridge(List.of("U", "U")));
+        player.startRound();
+
         assertThat(player.movable()).isTrue();
     }
 
     @Test
-    public void move_다리_시작점에서_움직이고_현재_위치_반환() throws Exception {
+    public void movable_다리_끝에서_움직일수_있는지() throws Exception {
+        Player player = new Player(new Bridge(List.of("U", "U")));
+        player.startRound();
+
+        player.move("U");
+        player.move("U");
+
+        assertThat(player.movable()).isFalse();
+    }
+
+    @Test
+    public void movable_move실패시_움직일수_있는지() throws Exception {
+        Player player = new Player(new Bridge(List.of("U", "U")));
+        player.startRound();
+
+        player.move("U");
+        player.move("D");
+
+        assertThat(player.movable()).isFalse();
+    }
+
+    @Test
+    public void move_다리_시작점에서_움직임이_성공인지_반환() throws Exception {
         //given
         Player player = new Player(new Bridge(List.of("U", "U")));
+        player.startRound();
+
+        assertThat(player.move("U")).isEqualTo(0);
+    }
+
+    @Test
+    public void move_다리_끝점에서_움직임이_실패인지_반환() throws Exception {
+        //given
+        Player player = new Player(new Bridge(List.of("U", "U")));
+        player.startRound();
+
+        player.move("U");
+        player.move("U");
+        assertThat(player.move("U")).isEqualTo(1);
+    }
+
+    @Test
+    public void 다리_끝까지_온_플레이어가_성공인지_반환() throws Exception {
+        //given
+        Player player = new Player(new Bridge(List.of("U", "U")));
+        player.startRound();
 
         //when
-        int playerLocationAfterMove = player.move("U");
+        player.move("U");
+        player.move("U");
 
         //then
-        assertThat(playerLocationAfterMove).isEqualTo(player.getLocation());
+        assertThat(player.isSuccess()).isTrue();
+    }
+
+    @Test
+    public void 다리_중간의_플레이어가_성공인지_반환() throws Exception {
+        //given
+        Player player = new Player(new Bridge(List.of("U", "U")));
+        player.startRound();
+
+        //when
+        player.move("U");
+
+        //then
+        assertThat(player.isSuccess()).isFalse();
+    }
+
+    @Test
+    public void 다리_중간에서_move_실패한_플레이어가_성공인지_반환() throws Exception {
+        //given
+        Player player = new Player(new Bridge(List.of("U", "U")));
+        player.startRound();
+
+        //when
+        player.move("U");
+        player.move("D");
+
+        //then
+        assertThat(player.isSuccess()).isFalse();
     }
 }
