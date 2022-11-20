6dc6ddf484b0c7b6f8f0dc227de977402818643d
210_reverof <sheltonwon@gmail.com>
2022-11-21 00:06:33 +0900

    feat: add bridge game object with basic methods  (function6)

--- src/main/java/bridge/BridgeGame.java
+++ src/main/java/bridge/BridgeGame.java
@@ -6,16 +6,22 @@ import java.util.*;
  * 다리 건너기 게임을 관리하는 클래스
  */
 public class BridgeGame {
-    List<String> bridge = new ArrayList<>();
-    List<String> inputBridge = new ArrayList<>();
     private int tryCount  = 1;
+    private List<String> bridge;
+    private List<String> inputBridge = new ArrayList<>();
+
+    public BridgeGame(int size) {
+        BridgeMaker bridgeMaker = new BridgeMaker(new BridgeRandomNumberGenerator());
+        this.bridge = bridgeMaker.makeBridge(size);
+    }
 
     /**
      * 사용자가 칸을 이동할 때 사용하는 메서드
      * <p>
      * 이동을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void move() {
+    public void move(String move) {
+        inputBridge.add(move);
     }
 
     /**
@@ -24,5 +30,32 @@ public class BridgeGame {
      * 재시작을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
     public void retry() {
+        inputBridge.clear();
+        tryCount++;
+    }
+
+    public List<String> getBridge() {
+        return bridge;
+    }
+
+    public List<String> getInputBridge() {
+        return inputBridge;
+    }
+
+    public int getTryCount() {
+        return tryCount;
+    }
+
+    public boolean isSuccess() {
+        return bridge.equals(inputBridge);
+    }
+
+    public boolean isRightStep() {
+        for (int i = 0; i < inputBridge.size(); i++) {
+            if (!inputBridge.get(i).equals(bridge.get(i))) {
+                return false;
+            }
+        }
+        return true;
     }
 }
