b626019bb98fecf5a6af62e1ce1272a2205bc042
Sangkyun Seo <tkdrbs0802@naver.com>
2022-11-20 15:08:24 +0900

    feat: main메서드에서  게임 시작 문구 출력

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -1,8 +1,10 @@
 package bridge;
 
 public class Application {
-
+    static String printgameStart = "다리 건너기 게임을 시작합니다.";
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        System.out.println(printgameStart);
+
+
     }
 }
