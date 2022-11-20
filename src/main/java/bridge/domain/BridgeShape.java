915da2c2e52c3115de57fe89512bac1148344eaf
slbin-park <smpts00@naver.com>
2022-11-20 23:27:09 +0900

    refactor(BridgeShape) : 출력양식에 맞게 enum 값 변경

--- src/main/java/bridge/domain/BridgeShape.java
+++ src/main/java/bridge/domain/BridgeShape.java
@@ -3,10 +3,10 @@ package bridge.domain;
 import java.util.List;
 
 public enum BridgeShape {
-    COLLECT_UP(List.of(" O |", "   |"), List.of(" O |", "   |")),
-    COLLECT_DOWN(List.of(" O |", "   |"), List.of("   ", " O ")),
+    COLLECT_UP(List.of(" O |", "   |"), List.of(" O ", "   ")),
+    COLLECT_DOWN(List.of("   |", " O |"), List.of("   ", " O ")),
     WRONG_UP(List.of("   |", " X |"), List.of("  ", " X ")),
-    WRONG_DOWN(List.of(" X ", "   "), List.of(" X |", "   |"));
+    WRONG_DOWN(List.of(" X |", "   |"), List.of(" X ", "   "));
 
     private List<String> shape;
     private List<String> lastShape;
