0c635fe8a347da39add910b1dc4ed4d94ff2d1f3
SeokHwan An <asok369258@naver.com>
2022-11-20 17:09:23 +0900

    feat : 다리 생성시 랜덤으로 건널 수 있는 위치 설정 기능 추가

--- src/main/java/bridge/command/LocationCommand.java
+++ src/main/java/bridge/command/LocationCommand.java
@@ -1,5 +1,7 @@
 package bridge.command;
 
+import static bridge.util.Constant.*;
+
 public enum LocationCommand {
     Up("U", 1),
     Down("D", 0);
@@ -10,6 +12,16 @@ public enum LocationCommand {
     LocationCommand(String direction, int randomNumber) {
         this.direction = direction;
         this.randomNumber = randomNumber;
+    }
+
+    // 건널 수 있는 위치를 나타내는 기능
+    public static String decideLocationToCross(int randomNumber) {
+        if (randomNumber == Up.getRandomNumber()) {
+            return Up.getDirection();
+        } else if (randomNumber == Down.getRandomNumber()) {
+            return Down.getDirection();
+        }
+        throw new IllegalArgumentException(ERROR_MESSAGE_BRIDGE_MAKE_INPUT_ERROR);
     }
 
     public String getDirection() {
--- src/main/java/bridge/util/Constant.java
+++ src/main/java/bridge/util/Constant.java
@@ -10,5 +10,6 @@ public class Constant {
     public static final String ERROR_MESSAGE_BRIDGE_LENGTH_RANGE_3_TO_20 = "[ERROR] 다리의 길이는 3이상 20이하여야 합니다.";
     public static final String ERROR_MESSAGE_BRIDGE_MOVE_COMMAND_INPUT_ERROR = "[ERROR] 다리 이동 입력값은 U 혹은 D이어야 합니다.";
     public static final String ERROR_MESSAGE_BRIDGE_GAME_COMMAND_INPUT_ERROR = "[ERROR] 게임 종료 후 재시작은 R 종료는 Q 입니다.";
+    public static final String ERROR_MESSAGE_BRIDGE_MAKE_INPUT_ERROR = "[ERROR] 다리 생성시 건널 수 있는 위치 입력값은 1 혹은 0이어야 합니다.";
 
 }
