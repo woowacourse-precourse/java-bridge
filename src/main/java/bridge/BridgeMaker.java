66b8648da93f029554c717a29446452c0bb36dd5
cgk95 <kjkj5868@gmail.com>
2022-11-20 19:10:27 +0900

    refactor(BridegeMaker) : 오탈자 수정

--- src/main/java/bridge/BridgeMaker.java
+++ src/main/java/bridge/BridgeMaker.java
@@ -1,7 +1,7 @@
 package bridge;
 
+import java.util.ArrayList;
 import java.util.List;
-import bridge.BridgeRandomNumberGenerator;
 
 /**
  * 다리의 길이를 입력 받아서 다리를 생성해주는 역할을 한다.
@@ -19,14 +19,15 @@ public class BridgeMaker {
      * @return 입력받은 길이에 해당하는 다리 모양. 위 칸이면 "U", 아래 칸이면 "D"로 표현해야 한다.
      */
     public List<String> makeBridge(int size) {
-        List<String> trueBridge=new List<String>;
+        List<String> trueBridge = new ArrayList<>();
         for (int i = 0; i < size; i++) {
             trueBridge.add(addTile());
         }
         return trueBridge;
     }
-    public String addTile(){
-        if (new BridgeRandomNumberGenerator().generate() == 0) {
+
+    public String addTile() {
+        if (bridgeNumberGenerator.generate() == 0) {
             return "D";
         }
         return "U";
