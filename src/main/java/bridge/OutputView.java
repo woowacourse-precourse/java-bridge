4ea06ba1d9e37e10b8c421772a74bd0cb756884f
jwjin95 <jwjin95@yonsei.ac.kr>
2022-11-21 02:23:40 +0900

    feat: 기본 입출력 및 다리 출력 기능 구현

--- src/main/java/bridge/OutputView.java
+++ src/main/java/bridge/OutputView.java
@@ -1,16 +1,26 @@
 package bridge;
 
+import java.util.List;
+
 /**
  * 사용자에게 게임 진행 상황과 결과를 출력하는 역할을 한다.
  */
 public class OutputView {
 
     /**
+     * 게임 시작 시 시작 메시지를 출력한다.
+     */
+    public void printGameStart() {
+        System.out.println("다리 건너기 게임을 시작합니다.\n");
+    }
+
+    /**
      * 현재까지 이동한 다리의 상태를 정해진 형식에 맞춰 출력한다.
      * <p>
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void printMap() {
+    public void printMap(List<String> bridge, int lastLevel, boolean failed) {
+        System.out.println(buildBridgeMap(bridge, lastLevel, failed));
     }
 
     /**
@@ -18,6 +28,47 @@ public class OutputView {
      * <p>
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void printResult() {
+    public void printResult(List<String> bridge, boolean succeeded, int failedLevel, int tried) {
+        System.out.println("최종 게임 결과");
+        if (succeeded) System.out.println(buildBridgeMap(bridge, bridge.size(), false));
+        if (!succeeded) System.out.println(buildBridgeMap(bridge, failedLevel, true));
+        System.out.println("\n최종 게임 결과: ");
+        if(succeeded) System.out.println("성공");
+        if(!succeeded) System.out.println("실패");
+        System.out.println("총 시도한 횟수: " + tried);
     }
+
+    public String buildBridgeMap(List<String> bridge, int lastLevel, boolean failed) {
+        String upperBridge = buildUpperBridgeMap(bridge, lastLevel, failed);
+        String lowerBridge = buildUpperBridgeMap(bridge, lastLevel, failed);
+
+        return upperBridge + "\n" + lowerBridge;
+    }
+
+    public String buildUpperBridgeMap(List<String> bridge, int lastLevel, boolean failed) {
+        StringBuilder bridgeMap = new StringBuilder("[ ");
+        for (int i = 0; i < lastLevel; i++) {
+            if (bridge.get(i).equals("U")) bridgeMap.append("O | ");
+            if (bridge.get(i).equals("D")) bridgeMap.append("  | ");
+        }
+        if (bridge.get(lastLevel).equals("U") && !failed) bridgeMap.append("  ]");
+        if (bridge.get(lastLevel).equals("U") && failed) bridgeMap.append("X ]");
+        if (bridge.get(lastLevel).equals("D")) bridgeMap.append("  ]");
+
+        return bridgeMap.toString();
+    }
+
+    public String buildLowerBridgeMap(List<String> bridge, int lastLevel, boolean failed) {
+        StringBuilder bridgeMap = new StringBuilder("[ ");
+        for (int i = 0; i < lastLevel; i++) {
+            if (bridge.get(i).equals("D")) bridgeMap.append("O | ");
+            if (bridge.get(i).equals("U")) bridgeMap.append("  | ");
+        }
+        if (bridge.get(lastLevel).equals("D") && !failed) bridgeMap.append("  ]");
+        if (bridge.get(lastLevel).equals("D") && failed) bridgeMap.append("X ]");
+        if (bridge.get(lastLevel).equals("U")) bridgeMap.append("  ]");
+
+        return bridgeMap.toString();
+    }
+
 }
