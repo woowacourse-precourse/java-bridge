f5260472e29d8330519b26484353681362b9e1ef
JJ <jeongjae1996@naver.com>
2022-11-21 02:47:19 +0900

    feat: map을 출력하는 기능 구현

--- src/main/java/bridge/OutputView.java
+++ src/main/java/bridge/OutputView.java
@@ -1,5 +1,7 @@
 package bridge;
 
+import java.util.List;
+
 /**
  * 사용자에게 게임 진행 상황과 결과를 출력하는 역할을 한다.
  * OutputView의 메서드의 이름은 변경할 수 없고, 인자와 반환 타입은 필요에 따라 추가하거나 변경할 수 있다.
@@ -11,7 +13,9 @@ public class OutputView {
      * <p>
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void printMap() {
+    public void printMap(List<String> map) {
+        System.out.println(map.get(BridgeStatus.UP.getStatusNumber()));
+        System.out.println(map.get(BridgeStatus.DOWN.getStatusNumber()));
     }
 
     /**
