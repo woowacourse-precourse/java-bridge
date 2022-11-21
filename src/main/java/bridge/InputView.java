13e82e5db22398966a77818fe438a3c90e87b899
limzzum <chfhddl1234@naver.com>
2022-11-22 00:29:25 +0900

    refactor : 출력 줄바꿈 추가

--- src/main/java/bridge/InputView.java
+++ src/main/java/bridge/InputView.java
@@ -11,7 +11,7 @@ public class InputView {
      * 다리의 길이를 입력받는다.
      */
     public String readBridgeSize() {
-        System.out.println("다리의 길이를 입력해주세요.");
+        System.out.println("\n다리의 길이를 입력해주세요.");
         String bridgeLen = Console.readLine();
         return bridgeLen;
     }
@@ -20,7 +20,7 @@ public class InputView {
      * 사용자가 이동할 칸을 입력받는다.
      */
     public String readMoving() {
-        System.out.println("이동할 칸을 선택해주세요. (위: U, 아래: D)");
+        System.out.println("\n이동할 칸을 선택해주세요. (위: U, 아래: D)");
         String direction = Console.readLine();
         return direction;
     }
@@ -29,7 +29,7 @@ public class InputView {
      * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
      */
     public String readGameCommand() {
-        System.out.println("게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)");
+        System.out.println("\n게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)");
         String select = Console.readLine();
         return select;
     }
