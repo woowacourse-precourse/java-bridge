7948ebbf82bcbfc28f931377b51236e8d32c82e3
sonsoowon <yuwoo1018@gmail.com>
2022-11-22 02:03:56 +0900

    refactor(BridgeMaker): List 대신 Map으로 문자 반환

--- src/main/java/bridge/BridgeMaker.java
+++ src/main/java/bridge/BridgeMaker.java
@@ -4,13 +4,17 @@ import bridge.model.utils.BridgeSides;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
+
 
 /**
  * 다리의 길이를 입력 받아서 다리를 생성해주는 역할을 한다.
  */
 public class BridgeMaker {
 
+    private static final int UP_NUMBER = 1;
+    private static final int DOWN_NUMBER = 0;
     private final BridgeNumberGenerator bridgeNumberGenerator;
 
     public BridgeMaker(BridgeNumberGenerator bridgeNumberGenerator) {
@@ -22,7 +26,10 @@ public class BridgeMaker {
      * @return 입력받은 길이에 해당하는 다리 모양. 위 칸이면 "U", 아래 칸이면 "D"로 표현해야 한다.
      */
     public List<String> makeBridge(int size) {
-        List<String> upDown = List.of(BridgeSides.down(), BridgeSides.up());
+        Map<Integer, String> upDown = Map.of(
+                UP_NUMBER, BridgeSides.up(),
+                DOWN_NUMBER, BridgeSides.down()
+        );
         return generateNumbers(size).stream()
                 .map(upDown::get)
                 .collect(Collectors.toList());
