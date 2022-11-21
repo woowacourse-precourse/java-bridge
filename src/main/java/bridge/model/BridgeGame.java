1b0a593b11620e0581aa8107f1040cbdcd8e8b22
KYR <freekyr7529@gmail.com>
2022-11-21 20:15:59 +0900

    feature : BridgeGame 클래스의 move 메소드와 그 외 구현
    이동할 때 사용되는 move 메소드를 구현 해당 메소드에서 호출되는 inspectCondition, isGoalSuccess 메소드 구현

--- src/main/java/bridge/model/BridgeGame.java
+++ src/main/java/bridge/model/BridgeGame.java
@@ -1,20 +1,52 @@
-package bridge;
+package bridge.model;
 
-import bridge.view.InputView;
-import bridge.view.OutputView;
-import java.util.ArrayList;
+import bridge.Progress;
+import bridge.constant.BridgePattern;
+import bridge.constant.GameCondition;
 import java.util.List;
 
 /**
  * 다리 건너기 게임을 관리하는 클래스
  */
 public class BridgeGame {
+    private final int DEFAULT_VALUE = 0;
+    private final List<String> bridge;
+    private final Progress progress;
+    private int pointer = DEFAULT_VALUE;
+
+    public BridgeGame(List<String> bridge, Progress progress) {
+        this.bridge = bridge;
+        this.progress = progress;
+    }
+
     /**
      * 사용자가 칸을 이동할 때 사용하는 메서드
      * <p>
      * 이동을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void move() {
+    public GameCondition move(BridgePattern bridgePattern) {
+        GameCondition gameCondition = inspectCondition(bridgePattern);
+        progress.updateProgress(bridgePattern, gameCondition.getSymbol());
+
+        return isGoalSuccess(gameCondition);
+    }
+
+    private GameCondition inspectCondition(BridgePattern bridgePattern) {
+        String move = bridge.get(pointer++);
+
+        if (bridgePattern.isInputEqual(move)) {
+            return GameCondition.IN_PROGRESS;
+        }
+
+        return GameCondition.FAILURE;
+    }
+
+    private GameCondition isGoalSuccess(GameCondition gameCondition) {
+        if (pointer == bridge.size() && gameCondition == GameCondition.GOAL) {
+            return GameCondition.GOAL;
+        }
+
+        return gameCondition;
     }
 
     /**
