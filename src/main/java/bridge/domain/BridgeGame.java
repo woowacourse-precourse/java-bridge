fa233fbd88647734bdc2a8b17157a297eb9bfc6e
olrlobt <317tmdgjs@naver.com>
2022-11-22 02:25:30 +0900

    feat: 게임 결과를 저장하는 필드 추가

--- src/main/java/bridge/domain/BridgeGame.java
+++ src/main/java/bridge/domain/BridgeGame.java
@@ -13,6 +13,7 @@ public class BridgeGame {
     private int current;
     private List<String> mark;
     private final Bridge bridge;
+    private boolean complete;
 
     public BridgeGame(Bridge bridge) {
         this.bridge = bridge;
@@ -23,16 +24,17 @@ public class BridgeGame {
      * 사용자가 칸을 이동할 때 사용하는 메서드
      * <p>
      * 이동을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
-     *
-     * @return
      */
     public boolean move(String movePlace, Bridge bridge) {
         boolean passable = bridge.checkPassable(movePlace, current);
         mark.add(movePlace);
         current++;
-
-
+        this.complete = gameComplete(passable);
         return passable;
+    }
+
+    private boolean gameComplete(boolean passable) {
+        return current == bridge.getSize() && passable;
     }
 
     /**
@@ -61,5 +63,9 @@ public class BridgeGame {
 
     public int getChallenge() {
         return challenge;
+    }
+
+    public boolean getComplete(){
+        return complete;
     }
 }
