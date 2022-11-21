cd69f32c1f68f05395e0a0937993912caf894a4a
Geun Woo(Kay) Park <64457922+parklaus1078@users.noreply.github.com>
2022-11-21 14:43:10 +0900

    [ADD] 다리 게임 - 주어진 다리 길이로 다리를 만드는 기능 추가

--- src/main/java/bridge/BridgeMaker.java
+++ src/main/java/bridge/BridgeMaker.java
@@ -19,6 +19,23 @@ public class BridgeMaker {
      * @return 입력받은 길이에 해당하는 다리 모양. 위 칸이면 "U", 아래 칸이면 "D"로 표현해야 한다.
      */
     public List<String> makeBridge(int size) {
-        return null;
+        while (size > 0) {
+            int block = this.bridgeNumberGenerator.generate();
+            makeBlock(block);
+            size--;
+        }
+
+        return this.bridge;
+    }
+
+    // 다리의 한 칸을 만듦
+    private void makeBlock(int block) {
+        if (block == Constants.BridgeStatus.DOWN.getStat()) {
+            this.bridge.add(Constants.BridgeStatus.DOWN.getPos());
+        }
+
+        if (block == Constants.BridgeStatus.UPPER.getStat()) {
+            this.bridge.add(Constants.BridgeStatus.UPPER.getPos());
+        }
     }
 }
