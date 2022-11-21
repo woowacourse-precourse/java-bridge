34b1283631ed0c66e1590f58cb2ba54bd965b26b
Donguk <95donguk.dev@gmail.com>
2022-11-22 02:32:49 +0900

    test: 이동 기록 찾기 테스트

---
+++ src/test/java/bridge/domain/BridgeGameOtherTest.java
@@ -0,0 +1,27 @@
+package bridge.domain;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+class BridgeGameOtherTest {
+    BridgeGame bridgeGame;
+
+    @BeforeEach
+    void setUp() {
+        bridgeGame = new BridgeGame();
+        bridgeGame.move("U", "U");
+        bridgeGame.move("F", "F");
+        bridgeGame.move("F", "U");
+    }
+
+    @DisplayName("이동 기록 찾기 테스트")
+    @ParameterizedTest
+    @CsvSource({"0, U", "1, F", "2, F"})
+    void findMovingByIndexTest(int index, String moving) {
+        assertThat(bridgeGame.findMovingByIndex(index)).isEqualTo(moving);
+    }
+}
