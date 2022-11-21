58af1a1e89bfc8f31fb4f2189632afe96875aa60
BaeDongHyeon <sasd730@gmail.com>
2022-11-22 00:19:20 +0900

    feat: 출력을 위해 BridgeResult 반환

--- src/main/java/bridge/BridgeGame.java
+++ src/main/java/bridge/BridgeGame.java
@@ -30,6 +30,10 @@ public class BridgeGame {
         return resultSymbol;
     }
 
+    public BridgeResult printLines() {
+        return bridgeResult;
+    }
+
     /**
      * 사용자가 게임을 다시 시도할 때 사용하는 메서드
      * <p>
