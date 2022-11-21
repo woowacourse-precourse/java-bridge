b3a2138cea60ba51846516d8476f0ed41d800b60
Amyhds <dasomhan524@gmail.com>
2022-11-21 15:30:50 +0900

    refactor(outputview): 인자 개수 변경

--- src/main/java/bridge/OutputView.java
+++ src/main/java/bridge/OutputView.java
@@ -10,8 +10,9 @@ public class OutputView {
      * <p>
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void printMap(String map) {
-        System.out.println(map);
+    public void printMap(String upMap, String downMap) {
+        System.out.println(upMap);
+        System.out.println(downMap);
     }
 
     /**
