f39bc03ee723e5d47633821a85026f3aca559da0
aksmf1442 <aksmf1442@naver.com>
2022-11-21 23:57:59 +0900

    refactor(다리 건너기): 게임 재시도 여부를 물어보는 출력문에서 안내 메시지 추가

--- src/main/java/bridge/OutputView.java
+++ src/main/java/bridge/OutputView.java
@@ -14,11 +14,11 @@ public class OutputView {
     }
 
     public void printGuideForMoveDirection() {
-        System.out.println("이동한 칸을 선택해주세요. (위: U, 아래: D)");
+        System.out.println("이동할 칸을 선택해주세요. (위: U, 아래: D)");
     }
 
     public void printGuideForGameCommand() {
-        System.out.println("게임을 다시 시도할지 여부를 입력해주세요.");
+        System.out.println("게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)");
     }
 
     /**
