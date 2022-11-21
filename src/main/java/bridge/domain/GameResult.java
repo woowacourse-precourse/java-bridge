d5566b805e8fb00ba47687faea0f666e85503438
이정우 <acg6138@naver.com>
2022-11-22 02:22:59 +0900

    refactor(GameResult): 기존 게임의 결과를 출력하는 클래스 삭제, 성공 실패를 나타내는 enum 으로 수정

--- src/main/java/bridge/domain/GameResult.java
+++ src/main/java/bridge/domain/GameResult.java
@@ -1,45 +1,16 @@
 package bridge.domain;
 
-public class GameResult {
-    private static final String SUCCESS = "성공";
-    private static final String FAIL = "실패";
-    private static final String LINE = " | ";
-    private final BridgeGame bridgeGame;
+public enum GameResult {
+    SUCCESS("성공"),
+    FAIL("실패");
 
-    public GameResult(BridgeGame bridgeGame) {
-        this.bridgeGame = bridgeGame;
+    private final String value;
+
+    GameResult(String value) {
+        this.value = value;
     }
 
-    public String getTopResult() {
-        String result = "";
-        for (String top : bridgeGame.getTop()) {
-            result += top + LINE;
-        }
-        result = removeLastLine(result);
-        return result;
-    }
-
-    public String getBottomResult() {
-        String result = "";
-        for (String bottom : bridgeGame.getBottom()) {
-            result += bottom + LINE;
-        }
-        result = removeLastLine(result);
-        return result;
-    }
-
-    public int totalPlayCount() {
-        return bridgeGame.getPlayCount();
-    }
-
-    public String successOrFailure() {
-        if (!bridgeGame.isClear()) {
-            return FAIL;
-        }
-        return SUCCESS;
-    }
-
-    public String removeLastLine(String result) {
-        return result.substring(0, result.length() - 3);
+    public String getValue() {
+        return this.value;
     }
 }
