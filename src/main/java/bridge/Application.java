bf34e8c417a334a8448aa65190e686262aaf8248
Devtist <yt930310@snu.ac.kr>
2022-11-21 17:38:19 +0900

    chore: TODO 정리

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -3,9 +3,8 @@ package bridge;
 import controller.GameController;
 
 public class Application {
-    //TODO 리드미 확인할 것
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        // TODO: 리드미 확인할 것
         // TODO: View 에서는 최소한의 검증만 -> 알파벳 대문자인지만 확인하고 도메인에서 확인할 것
         // TODO: 마지막에 실패한 경우 예외 처리
         // TODO: 요구사항 제발 제대로 찬찬히 읽어보기
