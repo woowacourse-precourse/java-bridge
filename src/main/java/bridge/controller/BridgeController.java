415524cabb64a9e4960d4c8a93e0c126040be917
hoin <apicad0307@gmail.com>
2022-11-21 14:28:20 +0900

    feat(controller/BridgeController.java): 실제 게임을 진행하는 play 함수 구현

--- src/main/java/bridge/controller/BridgeController.java
+++ src/main/java/bridge/controller/BridgeController.java
@@ -3,12 +3,45 @@ package bridge.controller;
 import bridge.BridgeGame;
 import bridge.BridgeMaker;
 import bridge.BridgeRandomNumberGenerator;
+import bridge.input.InputString;
 import bridge.input.InputView;
+import bridge.output.OutputView;
 
 import java.util.List;
 
 public class BridgeController {
     private final BridgeGame bridgeGame = new BridgeGame();
+
+    public void start() {
+        OutputView.printStartGame();
+        List<String> bridge = createBridge();
+        boolean isSuccess = play(bridge);
+        OutputView.printResult(isSuccess, bridgeGame.getFirstRoad(), bridgeGame.getSecondRoad(), bridgeGame.getRoundCount());
+    }
+
+    private boolean play(List<String> bridge) {
+        while (true) {
+            boolean isCorrect = false;
+            for (int count = 0; count < bridge.size(); count++) {
+                String moveCommand = InputView.readMoving();
+                isCorrect = moveCommand.equals(bridge.get(count));
+                bridgeGame.move(moveCommand, isCorrect);
+                OutputView.printMap(bridgeGame.getFirstRoad(), bridgeGame.getSecondRoad());
+                if (!isCorrect) {
+                    String retryCommand = InputView.readGameCommand();
+                    if (isQuit(retryCommand)) return false;
+                    bridgeGame.retry();
+                    break;
+                }
+            }
+            if (!isCorrect) continue;
+            return true;
+        }
+    }
+
+    private static boolean isQuit(String retryCommand) {
+        return retryCommand.equals(InputString.QUIT);
+    }
 
     private List<String> createBridge() {
         int bridgeSize = InputView.readBridgeSize();
