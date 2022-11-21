3789d2e1a08b56dd734be498dd578f2316cd6e66
jiisuniui <dudndi9752@gmail.com>
2022-11-21 10:45:06 +0900

    refactor(GuideMessage): 상수 내용 수정

--- src/main/java/bridge/constants/GuideMessage.java
+++ src/main/java/bridge/constants/GuideMessage.java
@@ -6,7 +6,7 @@ public enum GuideMessage {
     INPUT_MOVE_MESSAGE("이동할 칸을 선택해주세요. (위: U, 아래: D)\n"),
     RESTART_MESSAGE("게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)\n"),
     FINAL_RESULT_MESSAGE("최종 게임 결과\n"),
-    SUCCESS_OR_FAILURE_MESSAGE("\n게임 성공 여부: "),
+    SUCCESS_OR_FAILURE_MESSAGE("게임 성공 여부: "),
     TOTAL_TRY_MESSAGE("총 시도한 횟수: ");
 
     private final String message;
