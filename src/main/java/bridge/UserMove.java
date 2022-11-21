79681eaf607d4bec179c371889754c85266cdf88
yangjiyoung <0112jyoung@naver.com>
2022-11-21 23:17:08 +0900

    feat: 유저 속성 추가

--- src/main/java/bridge/UserMove.java
+++ src/main/java/bridge/UserMove.java
@@ -5,6 +5,15 @@ import java.util.List;
 
 public class UserMove {
     private int tryCount=0;
+    private int moveCount=0;
+
+    public int getMoveCount() {
+        return moveCount;
+    }
+    public void increaseMoveCount(){
+        moveCount++;
+    }
+
     private List<String> userLocation=new ArrayList<>();
 
     public void increaseTryCount(){
