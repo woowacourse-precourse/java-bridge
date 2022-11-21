43dd9071fb53283b6a9518fce019c7171f163bab
zcae1020 <zcae1020@naver.com>
2022-11-21 11:54:37 +0900

    test: inputView 단위 테스트의 displayName 추가

--- src/test/java/bridge/ioView/InputViewTest.java
+++ src/test/java/bridge/ioView/InputViewTest.java
@@ -2,8 +2,10 @@ package bridge.ioView;
 
 import bridge.Application;
 import camp.nextstep.edu.missionutils.test.NsTest;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeTest;
 import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -11,6 +13,7 @@ class InputViewTest extends NsTest {
 
     private static final String ERROR_MESSAGE = "[ERROR]";
 
+    @DisplayName("다리길이입력_숫자아님")
     @Test
     void 다리길이입력_숫자아님() {
         assertSimpleTest(() -> {
@@ -19,6 +22,7 @@ class InputViewTest extends NsTest {
         });
     }
 
+    @DisplayName("다리길이입력_3부터_20아님")
     @Test
     void 다리길이입력_3부터_20아님() {
         assertSimpleTest(() -> {
@@ -27,12 +31,22 @@ class InputViewTest extends NsTest {
         });
     }
 
+    @DisplayName("움직임입력_U나_D아님")
     @Test
     void 움직임입력_U나_D아님() {
         assertSimpleTest(() -> {
             runException("20","U","D","u");
             assertThat(output()).contains(ERROR_MESSAGE);
         });
+    }
+
+    @DisplayName("게임명령어입력_R이나_Q아님")
+    @Test
+    void 게임명령어입력_R이나_Q아님() {
+        assertRandomNumberInRangeTest(() -> {
+            run("3", "U", "D", "D", "r");
+            assertThat(output()).contains(ERROR_MESSAGE);
+        }, 1, 0, 1);
     }
 
     @Override
