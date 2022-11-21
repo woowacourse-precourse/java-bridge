8c7d900eb19117f4b94040a856856aeb45d774a7
ostar11 <cjw5877@gmail.com>
2022-11-22 04:16:11 +0900

    feat(OutputView): makeIndex 구현

--- src/main/java/bridge/constant/OutputMessage.java
+++ src/main/java/bridge/constant/OutputMessage.java
@@ -3,7 +3,10 @@ package bridge.constant;
 public enum OutputMessage {
     GAME_RESULT("최종 게임 결과"),
     GAME_SUCCESS_OR_FAILURE("게임 성공 여부: "),
-    TRY_COUNT("총 시도한 횟수: ");
+    TRY_COUNT("총 시도한 횟수: "),
+    INDEX_START("[ "),
+    INDEX_DELIMITER(" | "),
+    INDEX_END(" ]");
 
     private final String outputMessage;
 
--- src/main/java/bridge/view/OutputView.java
+++ src/main/java/bridge/view/OutputView.java
@@ -4,6 +4,7 @@ import bridge.BridgeGame;
 import bridge.BridgeGameResult;
 import bridge.constant.MovingResult;
 import bridge.constant.OutputMessage;
+import java.util.List;
 
 /**
  * 사용자에게 게임 진행 상황과 결과를 출력하는 역할을 한다.
@@ -11,8 +12,8 @@ import bridge.constant.OutputMessage;
 public class OutputView {
 
     public void printMap(BridgeGameResult bridgeGameResult) {
-        String upSideIndex = "[ " + String.join(" | ", bridgeGameResult.getUpBridge()) + " ]";
-        String downSideIndex = "[ " + String.join(" | ", bridgeGameResult.getDownBridge()) + " ]";
+        String upSideIndex = makeIndex(bridgeGameResult.getUpBridge());
+        String downSideIndex = makeIndex(bridgeGameResult.getDownBridge());
         System.out.println(upSideIndex);
         System.out.println(downSideIndex);
     }
@@ -22,11 +23,16 @@ public class OutputView {
         printMap(bridgeGameResult);
         System.out.println(OutputMessage.GAME_SUCCESS_OR_FAILURE.getOutputMessage() + getSuccessPhrase(
                 bridgeGameResult.getSuccess()));
-        System.out.println(OutputMessage.GAME_RESULT.getOutputMessage() + bridgeGame.getTryCount());
+        System.out.println(OutputMessage.TRY_COUNT.getOutputMessage() + bridgeGame.getTryCount());
     }
 
     public String getSuccessPhrase(boolean comparisonResult) {
         return MovingResult.fromComparison(comparisonResult)
                 .getPhrase();
+    }
+
+    public String makeIndex(List<String> bridge) {
+        return OutputMessage.INDEX_START.getOutputMessage() + String.join(
+                OutputMessage.INDEX_DELIMITER.getOutputMessage(), bridge) + OutputMessage.INDEX_END.getOutputMessage();
     }
 }
