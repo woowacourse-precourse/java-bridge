a2af92d4d640f21ca415e8fce9fdd09dae89345f
zcae1020 <zcae1020@naver.com>
2022-11-21 12:15:11 +0900

    test: outputView의 printResult 메서드 성공 및 실패 test 추가

--- src/test/java/bridge/ioView/OutputViewTest.java
+++ src/test/java/bridge/ioView/OutputViewTest.java
@@ -8,7 +8,6 @@ import org.junit.jupiter.api.Test;
 import java.util.Arrays;
 import java.util.List;
 
-import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
 import static org.assertj.core.api.Assertions.assertThat;
 
 class OutputViewTest extends NsTest {
@@ -46,6 +45,54 @@ class OutputViewTest extends NsTest {
         assertThat(output()).contains(
                 "[ O |   | O |   | O | O ]",
                 "[   | O |   | O |   |   ]"
+        );
+
+        int upSideIndex = output().indexOf("[ O |   | O |   | O | O ]");
+        int downSideIndex = output().indexOf("[   | O |   | O |   |   ]");
+
+        assertThat(upSideIndex).isLessThan(downSideIndex);
+    }
+
+    @DisplayName("printResult_실패")
+    @Test
+    void printResult_실패() {
+        int trial = 1234;
+        List<String> userRoute = Arrays.asList("U","D","U","D","U","D");
+        List<String> bridge = Arrays.asList("U","D","U","D","U","U");
+
+        outputView.printResult(trial, userRoute, bridge);
+
+        assertThat(output()).contains(
+                "최종 게임 결과",
+                "[ O |   | O |   | O |   ]",
+                "[   | O |   | O |   | X ]",
+                "게임 성공 여부: 실패",
+                "총 시도한 횟수: 1234"
+        );
+
+        int upSideIndex = output().indexOf("[ O |   | O |   | O |   ]");
+        int downSideIndex = output().indexOf("[   | O |   | O |   | X ]");
+
+        assertThat(upSideIndex).isLessThan(downSideIndex);
+    }
+
+    @DisplayName("printResult_성공")
+    @Test
+    void printResult_성공() {
+        int trial = 1234;
+        List<String> userRoute = Arrays.asList("U","D","U","D","U","U");
+        List<String> bridge = Arrays.asList("U","D","U","D","U","U");
+
+        outputView.printMap(userRoute, bridge);
+
+        outputView.printResult(trial, userRoute, bridge);
+
+        assertThat(output()).contains(
+                "최종 게임 결과",
+                "[ O |   | O |   | O | O ]",
+                "[   | O |   | O |   |   ]",
+                "게임 성공 여부: 성공",
+                "총 시도한 횟수: 1234"
         );
 
         int upSideIndex = output().indexOf("[ O |   | O |   | O | O ]");
