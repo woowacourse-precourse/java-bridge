9851288e9daa329428c9903eb3fd624b30870a38
Basak <chgy2131@naver.com>
2022-11-22 00:58:39 +0900

    feat(Application): 전체 기능 확인을 위한 메인 로직 작성

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -1,8 +1,33 @@
 package bridge;
 
-public class Application {
+import bridge.domain.GameProgress;
+import bridge.domain.GameResult;
 
+import java.util.regex.Pattern;
+
+public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        // 객체 생성 및 다리 생성
+        BridgeGame game = new BridgeGame();
+
+        // 게임 진행
+        while (true) {
+            // 길 입력받기
+            game.move();
+            // 결과확인
+            GameResult gameResult = game.getResult();
+            BridgeGame.printGameResult(gameResult);
+
+            // 실패시 재시도할지 확인
+            if (gameResult.getProgress() == GameProgress.FAILURE) {
+                if (!game.retry()) {
+                    break;
+                }
+            }
+            // 성공시 게임 종료
+            if (gameResult.getProgress() == GameProgress.SUCCESS) {
+                break;
+            }
+        }
     }
 }
