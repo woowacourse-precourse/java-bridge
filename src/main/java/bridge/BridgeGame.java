1f692ccce300909606bd549e2b7dc423747fa1fe
sohyundoh <lyny123@naver.com>
2022-11-21 12:00:14 +0900

    feat($move): Problem 3. 플레이어가 이동할 칸 입력받

--- src/main/java/bridge/BridgeGame.java
+++ src/main/java/bridge/BridgeGame.java
@@ -1,5 +1,9 @@
 package bridge;
 
+import java.util.List;
+
+import static camp.nextstep.edu.missionutils.Console.readLine;
+
 /**
  * 다리 건너기 게임을 관리하는 클래스
  */
@@ -11,6 +15,7 @@ public class BridgeGame {
      * 이동을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
     public void move() {
+        String move = readLine();
     }
 
     /**
--- src/main/java/bridge/OutputView.java
+++ src/main/java/bridge/OutputView.java
@@ -9,6 +9,10 @@ public class OutputView {
         System.out.println("다리의 길이를 입력해주세요.");
     }
 
+    public static void printGetMove(){
+        System.out.println("이동할 칸을 선택해주세요. (위: U, 아래 : D)");
+    }
+
     /**
      * 현재까지 이동한 다리의 상태를 정해진 형식에 맞춰 출력한다.
      * <p>
