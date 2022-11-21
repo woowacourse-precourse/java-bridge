ebc43cdb089a926a41707c5f466807f5978921ae
Seiyon Park <seiyonpdev@gmail.com>
2022-11-21 10:06:42 +0900

    feat(InputView): 메소드 readGameCommand 구현
    재시작 여부를 입력받는 메소드를 구현했다

--- src/main/java/bridge/InputView.java
+++ src/main/java/bridge/InputView.java
@@ -38,7 +38,14 @@ public class InputView {
     /**
      * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
      */
-    public String readGameCommand() {
-        return null;
+    public static String readGameCommand() {
+        String userInput = Console.readLine();
+
+        try {
+            Validation.validateGameCommand(userInput);
+        } catch (IllegalArgumentException illegalArgumentException) {
+            readGameCommand();
+        }
+        return userInput;
     }
 }
