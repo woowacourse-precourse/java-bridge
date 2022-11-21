266ac1f1430004fe5bf7e168157705ab175c0398
taekwon-dev <taekwon_dev@naver.com>
2022-11-21 11:23:47 +0900

    feat: (Application) 다리 건너기 기능 구현 완료

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -1,8 +1,19 @@
 package bridge;
 
+import bridge.domain.BridgeGameManager;
+
+import java.util.NoSuchElementException;
+
 public class Application {
 
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        try {
+            BridgeGameManager bridgeGameManager = new BridgeGameManager();
+            bridgeGameManager.start();
+        } catch (IllegalArgumentException | IllegalStateException ex) {
+            System.out.println(ex.getMessage());
+            throw new NoSuchElementException();
+        }
     }
 }
