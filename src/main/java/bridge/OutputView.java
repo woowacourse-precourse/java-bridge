3792cf9584ab6307b8ced199a5e6bfaa595bf99e
YooraKim <62877255+YooraKim@users.noreply.github.com>
2022-11-22 05:02:53 +0900

    fix: printResult()에 맵 프린트하는 기능 추가

--- src/main/java/bridge/OutputView.java
+++ src/main/java/bridge/OutputView.java
@@ -1,5 +1,7 @@
 package bridge;
 
+import java.util.List;
+
 /**
  * 사용자에게 게임 진행 상황과 결과를 출력하는 역할을 한다.
  */
@@ -18,6 +20,15 @@ public class OutputView {
      * <p>
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void printResult() {
+    public void printResult(int attemptNum, int currentLocation, int bridgeSize, List<String> record) {
+        System.out.println("최종 게임 결과");
+        if (currentLocation == bridgeSize) {
+            printMap(record,true);
+            System.out.println("게임 성공 여부: 성공");
+        } else {
+            printMap(record, false);
+            System.out.println("게임 성공 여부: 실패");
+        }
+        System.out.println("총 시도한 횟수: " + attemptNum);
     }
 }
