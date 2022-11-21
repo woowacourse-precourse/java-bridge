ba83e38086e2622e5ca1bbe1086148567e55ec91
Isu Shin <isu010614@naver.com>
2022-11-21 14:24:59 +0900

    feat: 아무것도 입력되지 않았을 때 출력되는 에러 메시지 추가

--- src/main/java/bridge/domain/constant/ExceptionMessage.java
+++ src/main/java/bridge/domain/constant/ExceptionMessage.java
@@ -1,6 +1,7 @@
 package bridge.domain.constant;
 
 public class ExceptionMessage {
+    public static final String NULL_INPUT = "[ERROR] 다시 입력해주세요.";
     public static final String NOT_NUMBER = "[ERROR] 숫자만 입력해야 합니다.";
     public static final String NOT_IN_RANGE = "[ERROR] 다리 길이는 3부터 20 사이의 숫자여야 합니다.";
     public static final String NOT_UP_OR_DOWN = "[ERROR] U(위), 혹은 D(아래)를 입력해주세요.";
