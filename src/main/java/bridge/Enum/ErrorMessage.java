f9fe6e507f2f5708ddda94982ea0c4afeb14071f
dlrjs2360 <lhg3670@naver.com>
2022-11-21 16:44:29 +0900

    :bug: fix: ERROR메시지에서 숫자의 범위가 표시되지 않는 버그 수정
    - ENUM에서 최대, 최소 범위를 받아올 때 문자가 아닌 변수명을 출력하는 버그 수정

--- src/main/java/bridge/Enum/ErrorMessage.java
+++ src/main/java/bridge/Enum/ErrorMessage.java
@@ -2,7 +2,7 @@ package bridge.Enum;
 
 public enum ErrorMessage {
     NUMERIC_ERROR("[ERROR] 숫자를 입력해주세요."),
-    BRIDGE_SIZE_ERROR("[ERROR] "+ BridgeInfo.MIN_SIZE+"~"+ BridgeInfo.MAX_SIZE+" 사이의 숫자를 입력해주세요."),
+    BRIDGE_SIZE_ERROR("[ERROR] "+ String.valueOf(BridgeInfo.MIN_SIZE.getInfo())+"~"+ String.valueOf(BridgeInfo.MAX_SIZE.getInfo())+" 사이의 숫자를 입력해주세요."),
     UPDWON_INPUT_ERROR("[ERROR] "+BridgeGameInfo.UP.getWord()+"와 "+BridgeGameInfo.DOWN.getWord()+" 중 하나를 입력해주세요."),
     RESTART_INPUT_ERROR("[ERROR] "+BridgeGameInfo.RESTART.getWord()+"와 "+BridgeGameInfo.QUIT.getWord()+" 중 하나를 입력해주세요.");
     final String message;
