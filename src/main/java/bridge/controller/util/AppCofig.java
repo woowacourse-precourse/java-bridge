8b09075e08a9c75debc6c2a5a12584aca3fe76f0
오승윤 <>
2022-11-21 12:42:11 +0900

    feat (AppConfig) : 의존성 주입을 한번에 관리하는 기능

---
+++ src/main/java/bridge/controller/util/AppCofig.java
@@ -0,0 +1,46 @@
+package bridge.controller.util;
+
+import bridge.BridgeMaker;
+import bridge.BridgeNumberGenerator;
+import bridge.BridgeRandomNumberGenerator;
+import bridge.BridgeSizeGenerator;
+import bridge.GameOverChecker;
+import bridge.GameRetryChecker;
+import bridge.GameWinChecker;
+import bridge.MovementStatusCalculator;
+import bridge.controller.BridgeGameController;
+import bridge.service.BridgeGame;
+import bridge.service.GameChecker;
+
+public class AppCofig {
+
+    public static GameChecker createGameChecker() {
+        return new GameChecker(createGameOverChecker(), createGameWinChecker());
+    }
+
+    private static GameOverChecker createGameOverChecker() {
+        return new GameOverChecker();
+    }
+
+    private static GameWinChecker createGameWinChecker() {
+        return new GameWinChecker();
+    }
+
+    public static BridgeGame createBridgeGame() {
+        return new BridgeGame(new MovementStatusCalculator(), new GameRetryChecker());
+    }
+
+    public static BridgeMaker createBridgeMaker() {
+        return new BridgeMaker(createBridgeNumberGenerator());
+    }
+
+    private static BridgeNumberGenerator createBridgeNumberGenerator() {
+        return new BridgeRandomNumberGenerator();
+    }
+
+    public static BridgeSizeGenerator createBridgeSizeGenerator() {
+        return new BridgeSizeGenerator();
+    }
+
+
+}
