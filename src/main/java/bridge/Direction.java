d38a90ee597a2e71be66922672f44a419a12c68e
Eun-chan Cho <eunchan1001@gmail.com>
2022-11-21 19:54:38 +0900

    feat(Direction): add static methods to convert input to Direction type

--- src/main/java/bridge/Direction.java
+++ src/main/java/bridge/Direction.java
@@ -12,4 +12,23 @@ public enum Direction {
     public String getValue() {
         return value;
     }
+
+    static public Direction getDirection(String value) {
+        for (Direction direction : Direction.values()) {
+            if (direction.getValue().equals(value)) {
+                return direction;
+            }
+        }
+        throw new IllegalArgumentException("잘못된 입력입니다.");
+    }
+
+    static public String getDirection(int number) {
+        if (number == 1) {
+            return UP.getValue();
+        }
+        if (number == 0) {
+            return DOWN.getValue();
+        }
+        throw new IllegalArgumentException("값은 0과 1중 하나여야 합니다.");
+    }
 }
