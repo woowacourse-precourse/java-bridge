401c91e0fc2cb07ddf77d909d6d25c462f15e8af
wnsgh12s <junho1273@gmail.com>
2022-11-21 22:04:11 +0900

    Refactor: 빠뜨린 메시지 추가
    빠뜨린 메시지 추가 및 필드 추가

--- src/main/java/bridge/view/InputView.java
+++ src/main/java/bridge/view/InputView.java
@@ -6,25 +6,27 @@ import camp.nextstep.edu.missionutils.Console;
  * 사용자로부터 입력을 받는 역할을 한다.
  */
 public class InputView {
-
+    private final String BRIDGE_SIZE_MESSAGE = "다리의 길이를 입력해주세요";
+    private final String NUMBER_FORMAT_ERROR = "[ERROR] 다리의 길이는 숫자입니다.";
+    private final String SELECT_CELL_MESSAGE = "이동할 칸을 선택해주세요";
     /**
      * 다리의 길이를 입력받는다.
      */
     public int readBridgeSize() {
-        int userInput = 0;
         try {
-            userInput = Integer.parseInt(Console.readLine());
+            System.out.println(BRIDGE_SIZE_MESSAGE);
+            return Integer.parseInt(Console.readLine());
         } catch (NumberFormatException err) {
-            String NUMBER_FORMAT_ERROR = "[ERROR] 다리의 길이는 숫자입니다.";
             System.out.println(NUMBER_FORMAT_ERROR);
         }
-        return userInput;
+        return 0;
     }
 
     /**
      * 사용자가 이동할 칸을 입력받는다.
      */
-    public String readMoving() {
+    public String readMoving() {;
+        System.out.println(SELECT_CELL_MESSAGE);
         return Console.readLine();
     }
 
