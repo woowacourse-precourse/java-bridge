676a1f7a013f65b472d580ee3858acdca006fcce
Yujin830 <rladbwls30@naver.com>
2022-11-21 23:28:48 +0900

    Feat: 사용자 입력 받는 InputView 완성

--- src/main/java/bridge/InputView.java
+++ src/main/java/bridge/InputView.java
@@ -1,5 +1,5 @@
 package bridge;
-
+import camp.nextstep.edu.missionutils.Console;
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
  */
@@ -9,20 +9,24 @@ public class InputView {
      * 다리의 길이를 입력받는다.
      */
     public int readBridgeSize() {
-        return 0;
+        String input = Console.readLine();
+        int size = Integer.parseInt(input);
+        return size;
     }
 
     /**
      * 사용자가 이동할 칸을 입력받는다.
      */
     public String readMoving() {
-        return null;
+        String move_location = Console.readLine();
+        return move_location;
     }
 
     /**
      * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
      */
     public String readGameCommand() {
-        return null;
+        String re_game = Console.readLine();
+        return re_game;
     }
 }
