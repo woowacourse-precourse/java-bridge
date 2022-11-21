dbbc4ff3d16338fc8aa4f28a467b3e4d3a32d6d1
김형기 <khk211113@naver.com>
2022-11-21 21:01:40 +0900

    refactor: enum을 config로 이동

--- src/test/java/bridge/DirectionTest.java
+++ src/test/java/bridge/DirectionTest.java
@@ -1,6 +1,6 @@
 package bridge;
 
-import bridge.model.Direction;
+import bridge.config.Direction;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
