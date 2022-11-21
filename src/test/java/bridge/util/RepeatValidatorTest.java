57c37a4d32689a32bddf4c6bcec663a531ce4a86
pilyang <30036534+pilyang@users.noreply.github.com>
2022-11-22 01:16:54 +0900

    test(RepeatValidator): 예외 메시지 출력 확인 추가

--- src/test/java/bridge/util/RepeatValidatorTest.java
+++ src/test/java/bridge/util/RepeatValidatorTest.java
@@ -1,8 +1,12 @@
 package bridge.util;
 
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
 
 import static org.assertj.core.api.SoftAssertions.assertSoftly;
 
@@ -10,27 +14,39 @@ class RepeatValidatorTest {
 
     private static int functionRunCount;
 
+    private static final String ERROR_MESSAGE = "[ERROR]";
+
+    private final ByteArrayOutputStream out = new ByteArrayOutputStream();
+    private final PrintStream originalOut = System.out;
+
     @BeforeEach
     private void setUp() {
         functionRunCount = 0;
+        System.setOut(new PrintStream(out));
+    }
+
+    @AfterEach
+    private void restoreInitialStream() {
+        System.setOut(originalOut);
     }
 
     @DisplayName(value = "2회 예외발생 후 3회째 1 반환 테스트")
     @Test
     void readUntilValidateTest() {
-        int actualValue = RepeatValidator.readUntilValidate(() ->
-                returnValueAfter(1, 3));
+        int actualValue = RepeatValidator.readUntilValidate(() -> returnValueAfter(1, 3));
+        String expectedOutput = ERROR_MESSAGE + "\n" + ERROR_MESSAGE + "\n";
 
         assertSoftly(softly -> {
-           softly.assertThat(actualValue).isEqualTo(1);
-           softly.assertThat(functionRunCount).isEqualTo(3);
+            softly.assertThat(actualValue).isEqualTo(1);
+            softly.assertThat(functionRunCount).isEqualTo(3);
+            softly.assertThat(out.toString()).isEqualTo(expectedOutput);
         });
     }
 
     private int returnValueAfter(int value, int tryNumber) {
         functionRunCount++;
-        if(functionRunCount < tryNumber){
-            throw new IllegalArgumentException("[ERROR]");
+        if (functionRunCount < tryNumber) {
+            throw new IllegalArgumentException(ERROR_MESSAGE);
         }
         return value;
     }
