66244aeb195ee572906d7ef55bd1d273f4fcb117
ma-an-jong <alswhd1113@gmail.com>
2022-11-21 22:32:04 +0900

    fix(BridgeMapComponent) : 발생하지 않는 예외를 회피하는 부분 제거
    BREAKING CHANGE: removeLast 메소드에서 IndexOutOfBoundsException이 발생하지 않기 때문에
    예외 회피 구문을 제거
    
    Before :
    
        BridgeMapComponent
        {
             public void removeLast() throws IndexOutOfBoundsException
        }
    
    After :
    
        BridgeMapComponent
        {
             public void removeLast() : 발생하지 않는 예외 회피 제거
        }
    
        발생하지 않는 예외에 대한 처리를 제거

--- src/main/java/bridge/model/BridgeMapComponent.java
+++ src/main/java/bridge/model/BridgeMapComponent.java
@@ -7,27 +7,23 @@ public class BridgeMapComponent {
     private static final String DELIMITER = " | ";
     private final List<String> bridgeMapComponent;
 
-    public BridgeMapComponent()
-    {
+    public BridgeMapComponent() {
         this.bridgeMapComponent = new ArrayList<>();
     }
 
-    public void removeLast() throws IndexOutOfBoundsException
-    {
-        int lastIndex = bridgeMapComponent.size()-1;
-        if(lastIndex < 0) return;
+    public void removeLast() {
+        int lastIndex = bridgeMapComponent.size() - 1;
+        if (lastIndex < 0) return;
         bridgeMapComponent.remove(lastIndex);
     }
 
-    public void add(String pixel)
-    {
+    public void add(String pixel) {
         bridgeMapComponent.add(pixel);
     }
 
     @Override
-    public String toString()
-    {
-        return "[ " + String.join(DELIMITER,bridgeMapComponent) + " ]\n";
+    public String toString() {
+        return "[ " + String.join(DELIMITER, bridgeMapComponent) + " ]\n";
     }
 
 
