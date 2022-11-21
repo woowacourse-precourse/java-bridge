7a37abb9516788062657e089ec14df47f01497b8
swa07016 <swa07016@naver.com>
2022-11-21 20:17:42 +0900

    feat: implement main logic

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -1,8 +1,37 @@
 package bridge;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Application {
 
+    private static BridgeGame bridgeGame;
+    private static BridgeMaker bridgeMaker;
+    private static InputView inputView;
+    private static OutputView outputView;
+
+    private static String GAME_STATE = "R";
+    private static int gameCount = 1;
+    private static int index = 0;
+
+    Application() {
+        bridgeGame = new BridgeGame();
+        bridgeMaker = new BridgeMaker(new BridgeRandomNumberGenerator());
+        inputView = new InputView();
+        outputView = new OutputView();
+    }
+
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        System.out.println("다리 건너기 게임을 시작합니다.");
+        int bridgeSize = inputView.readBridgeSize();
+        List<String> bridge = bridgeMaker.makeBridge(bridgeSize);
+        List<String> userPicks = new ArrayList<>();
+        while(!GAME_STATE.equals("Q")) {
+            String userPick = inputView.readMoving();
+            userPicks.add(userPick);
+            boolean isCorrect = bridgeGame.move();
+            outputView.printMap(bridge, userPicks);
+            if(!isCorrect) GAME_STATE = inputView.readGameCommand();
+        }
     }
 }
