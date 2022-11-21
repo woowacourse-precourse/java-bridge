c93e37c0fbac5ec3740746f10762dda1c58b7add
Hi-Imjaeyoung <imjy2757@naver.com>
2022-11-21 14:02:34 +0900

    refactor(ErrorMessage) : 예외 메시지를 게임 재시도에 관한 예외도 포함하도록 수정.

--- src/main/java/bridge/constant/ErrorMessage.java
+++ src/main/java/bridge/constant/ErrorMessage.java
@@ -2,7 +2,7 @@ package bridge.constant;
 
 public enum ErrorMessage {
     NO_NUMERIC_VALUE(" 숫자를 입력해주세요."), NUMBER_RANGE_IS_WRONG(" 3이상 20이하의 숫자를 입력해주세요."),
-    WRONG_LENGTH(" 하나의 문자를 입력해세요"), WRONG_FORMAT(" U와 D 중,하나의 문자만 입력해주세요");
+    WRONG_LENGTH(" 하나의 문자를 입력해세요"), WRONG_FORMAT(" 괄호 안 문자 중,하나의 문자만 입력해주세요");
     String errorMessage;
     ErrorMessage(String errorMessage){
         this.errorMessage = errorMessage;
