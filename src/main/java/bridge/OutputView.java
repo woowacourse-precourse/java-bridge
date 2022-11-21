7e0b5c2c650338561db3728ade1fe8a9b79e41bd
junyong <jymoon1121@gmail.com>
2022-11-21 15:34:54 +0900

    refactor(Outputview): 출력 줄바꿈 제거 및 정렬

--- src/main/java/bridge/OutputView.java
+++ src/main/java/bridge/OutputView.java
@@ -17,15 +17,15 @@ public class OutputView {
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
     public void printMap(List<String> results) {
-        System.out.println("[");
+        System.out.print("[");
         for(int i = 0; i < results.size(); i++) {
             System.out.println(results.get(i));
             if(i == results.size() -1) {
                 break;
             }
-            System.out.println("|");
+            System.out.print("|");
         }
-        System.out.println("]");
+        System.out.print("]");
     }
     /**
      * 게임의 최종 결과를 정해진 형식에 맞춰 출력한다.
