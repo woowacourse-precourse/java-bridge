f3395d4a8fc4c42fd9c371daa4aa5694ad1c60fc
jeongyeonKimm <wjddus9132@gmail.com>
2022-11-21 13:48:34 +0900

    feat: 다리 길이 입력 예외 처리

--- src/main/java/bridge/InputView.java
+++ src/main/java/bridge/InputView.java
@@ -2,6 +2,8 @@ package bridge;
 
 import camp.nextstep.edu.missionutils.Console;
 
+import static java.lang.Integer.parseInt;
+
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
  */
@@ -9,15 +11,28 @@ public class InputView {
 
     private static final String INPUT_BRIDGE_SIZE = "다리의 길이를 입력해주세요.";
     private static final String EMPTY_LINE = "";
+    private static final String BRIDGE_SIZE_ERROR = "[ERROR] 다리 길이는 숫자만 입력 가능합니다.";
+
     /**
      * 다리의 길이를 입력받는다.
      */
     public int readBridgeSize() {
         System.out.println(INPUT_BRIDGE_SIZE);
-        int bridgeSize = Integer.parseInt(Console.readLine());
         System.out.println(EMPTY_LINE);
-        
+
+        String size = Console.readLine();
+        validateSize(size);
+        int bridgeSize = parseInt(size);
+
         return bridgeSize;
+    }
+
+    private void validateSize(String size) {
+        try {
+            Integer.parseInt(size);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException(BRIDGE_SIZE_ERROR);
+        }
     }
 
     /**
