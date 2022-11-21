c55f0aa56a26079bca98ec3ee67fab781ca6b2da
JONGMIN SHIN <jongmin373@gmail.com>
2022-11-21 16:53:38 +0900

    Feat<InputView> : add readBridgeSize method

--- src/main/java/bridge/InputView.java
+++ src/main/java/bridge/InputView.java
@@ -1,28 +1,32 @@
 package bridge;
 
+import camp.nextstep.edu.missionutils.Console;
+
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
  */
 public class InputView {
+    private Exeption exeption = new Exeption();
 
     /**
      * 다리의 길이를 입력받는다.
      */
     public int readBridgeSize() {
-        return 0;
+        boolean isOK = false;
+        String size = "";
+        while (!isOK) {
+            size = inputBridgeSize();
+            if (!exeption.inputBridgeSizeCheck(size)) {
+                continue;
+            }
+            isOK = true;
+        }
+        return Integer.parseInt(size);
+    }
+    public String inputBridgeSize(){
+        System.out.println("\n다리의 길이를 입력해주세요.");
+        String size = Console.readLine();
+        return size;
     }
 
-    /**
-     * 사용자가 이동할 칸을 입력받는다.
-     */
-    public String readMoving() {
-        return null;
-    }
-
-    /**
-     * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
-     */
-    public String readGameCommand() {
-        return null;
-    }
 }
