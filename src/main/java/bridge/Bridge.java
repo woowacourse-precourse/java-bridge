23720594cfd009cce679d699680a925f3a87ce5b
jihun-24k <gi020477@gmail.com>
2022-11-22 00:33:02 +0900

    Feat(Bridge): 다리 클래스 구현

---
+++ src/main/java/bridge/Bridge.java
@@ -0,0 +1,11 @@
+package bridge;
+
+import java.util.List;
+
+public class Bridge {
+    private final List<String> blocks;
+
+    public Bridge(List<String> blocks) {
+        this.blocks = blocks;
+    }
+}
