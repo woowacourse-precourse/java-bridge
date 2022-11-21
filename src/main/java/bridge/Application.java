77d371c752388c6fbc0dc6fd0e5b3f72ddf989b9
YooByeongIk <quddlr96@naver.com>
2022-11-21 23:37:06 +0900

    fix(Application): change structure to handling exceptions

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -1,17 +1,38 @@
 package bridge;
 
+import java.util.List;
+import java.util.NoSuchElementException;
+
 public class Application {
+
+    private static final InputView inputView = new InputView();
+    private static final OutputView outputView = new OutputView();
 
     public static void main(String[] args) {
         // TODO: 프로그램 구현
-        try {
-            System.out.println("다리 건너기 게임을 시작합니다.");
-            BridgeMaker bridgeMaker = new BridgeMaker(new BridgeRandomNumberGenerator());
-            BridgeGame bridgeGame = new BridgeGame(bridgeMaker);
-            bridgeGame.play();
-        } catch (Exception e) {
-            System.out.println(e.getMessage());
-            throw e;
+        System.out.println("다리 건너기 게임을 시작합니다.");
+        try{
+            BridgeGame bridgeGame = createBridgeGame();
+            playGame(bridgeGame);
+            outputView.printResult(bridgeGame.getAttempts(), bridgeGame.getUser(), bridgeGame.getBridge());
+        }catch (IllegalStateException illegalStateException){
+            System.out.println(illegalStateException.getMessage());
+        }
+    }
+
+    private static BridgeGame createBridgeGame() {
+        BridgeMaker bridgeMaker = new BridgeMaker(new BridgeRandomNumberGenerator());
+        List<String> bridge = bridgeMaker.makeBridge(inputView.readBridgeSize());
+        return new BridgeGame(bridge);
+    }
+
+    public static void playGame(BridgeGame bridgeGame){
+        while(!bridgeGame.getUser().equals(bridgeGame.getBridge())){
+            bridgeGame.move(inputView.readMoving());
+            outputView.printMap(bridgeGame.getUser(), bridgeGame.getBridge());
+            if (!bridgeGame.isCorrect() && !bridgeGame.retry(inputView.readGameCommand())){
+                break;
+            }
         }
     }
 }
