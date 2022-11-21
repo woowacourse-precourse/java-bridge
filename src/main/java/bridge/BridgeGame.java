632e35fd170d89ed009c520769508d521293b36f
KangSungBum <peter9706@naver.com>
2022-11-21 18:38:43 +0900

    feat(BridgeGame.java): add new codes for userInput field list
    게임 객체 내부 userInput list를 위한 코드 추가
    - 사용자 이동 시에 객체 userInput 리스트에 값 추가
    - 사용자 재시도 시에 객체 userInput 리스트 초기화

--- src/main/java/bridge/BridgeGame.java
+++ src/main/java/bridge/BridgeGame.java
@@ -7,8 +7,10 @@ import java.util.List;
  * 다리 건너기 게임을 관리하는 클래스
  */
 public class BridgeGame {
+
     int trialCount = 0;
     List<String> bridge = new ArrayList<>();
+    List<String> userInput = new ArrayList<>();
 
 
     /**
@@ -16,9 +18,13 @@ public class BridgeGame {
      * <p>
      * 이동을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public boolean move(int index, String userMovement) {
-        if (!this.bridge.get(index).equals(userMovement)) {
-            return false;
+    public boolean move(String userMovement) {
+        this.userInput.add(userMovement);
+
+        for (int i = 0; i < this.userInput.size(); i++) {
+            if (!userInput.get(i).equals(this.bridge.get(i))) {
+                return false;
+            }
         }
         return true;
     }
@@ -30,6 +36,7 @@ public class BridgeGame {
      */
     public void retry() {
         this.trialCount++;
+        this.userInput.clear();
     }
 
     public void initBridge(int size) {
