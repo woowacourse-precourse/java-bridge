1eccfd5c500a97b2ba42cff4fea786e26d868fa6
SeungEon <js112099@gmail.com>
2022-11-22 02:39:13 +0900

    feat: bridgeMaker, InputView class

--- src/main/java/bridge/BridgeMaker.java
+++ src/main/java/bridge/BridgeMaker.java
@@ -1,5 +1,6 @@
 package bridge;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -18,6 +19,39 @@ public class BridgeMaker {
      * @return 입력받은 길이에 해당하는 다리 모양. 위 칸이면 "U", 아래 칸이면 "D"로 표현해야 한다.
      */
     public List<String> makeBridge(int size) {
-        return null;
+        int fitSize;
+        try {
+            fitSize = isFitSize(size);
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+            return null;
+        }
+
+        return new ArrayList<>(makeDivision(fitSize));
+    }
+
+    public List<String> makeDivision(int size) {
+        List<String> list = new ArrayList<String>();
+
+        for(int i = 0; i < size; i++) {
+            list.add(downOrUp(bridgeNumberGenerator.generate()));
+        }
+
+        return list;
+    }
+
+    public String downOrUp(int num) {
+        if(num == 0) {
+            return "D";
+        }
+        return "U";
+    }
+
+    public int isFitSize(int size) {
+        if(3 > size || size > 20) {
+            throw new IllegalArgumentException("[ERROR] 다리 길이는 3부터 20 사이의 숫자여야 합니다.");
+        }
+
+        return size;
     }
 }
--- src/main/java/bridge/InputView.java
+++ src/main/java/bridge/InputView.java
@@ -1,5 +1,7 @@
 package bridge;
 
+import camp.nextstep.edu.missionutils.Console;
+
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
  */
@@ -9,20 +11,26 @@ public class InputView {
      * 다리의 길이를 입력받는다.
      */
     public int readBridgeSize() {
-        return 0;
+        int size = Integer.parseInt(Console.readLine());
+
+        return size;
     }
 
     /**
      * 사용자가 이동할 칸을 입력받는다.
      */
     public String readMoving() {
-        return null;
+        String moveData = Console.readLine();
+
+        return moveData;
     }
 
     /**
      * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
      */
     public String readGameCommand() {
-        return null;
+        String gameCommand = Console.readLine();
+
+        return gameCommand;
     }
 }
