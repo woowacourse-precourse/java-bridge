fb16b7edfebb6f614f60a1f61a07b8fbc88e4dfc
delaylee <delaylee219@gmail.com>
2022-11-21 20:17:40 +0900

    feat: 게임을 다시 시도할지 종료할지 여부를 입력받는 기능 구현 (InputView#readGameCommand)

--- src/main/java/bridge/InputView.java
+++ src/main/java/bridge/InputView.java
@@ -42,7 +42,7 @@ public class InputView {
      * 사용자가 이동할 칸을 입력받는다.
      */
     public String readMoving() {
-        System.out.println("다리 길이를 입력해주세요.");
+        System.out.println("이동할 칸을 선택해주세요. (위: U, 아래: D)");
         String moving = Console.readLine();
         movingValidation(moving);
 
@@ -64,6 +64,21 @@ public class InputView {
      * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
      */
     public String readGameCommand() {
-        return null;
+        System.out.println("게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)");
+        String gameCommand = Console.readLine();
+        gameCommandValidation(gameCommand);
+
+        return gameCommand;
+    }
+
+    public void gameCommandValidation(String gameCommand) {
+        String gameCommandErrorMessage = "[ERROR] R(재시도) 또는 Q(종료) 중 하나의 문자여야 합니다";
+
+        if (gameCommand.length() != 1) {
+            throw new IllegalArgumentException(gameCommandErrorMessage);
+        }
+        if (!(gameCommand.charAt(0) == 'R' || gameCommand.charAt(0) == 'Q')) {
+            throw new IllegalArgumentException(gameCommandErrorMessage);
+        }
     }
 }
