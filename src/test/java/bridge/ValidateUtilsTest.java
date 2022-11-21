e5032b72507ca48204f6221afacbb090e03f1d68
song-wooseok <sws981202@gmail.com>
2022-11-21 17:08:14 +0900

    test: 이동할 칸 입력 시 들어갈 입력값 검증 테스트

--- src/test/java/bridge/ValidateUtilsTest.java
+++ src/test/java/bridge/ValidateUtilsTest.java
@@ -22,4 +22,11 @@ public class ValidateUtilsTest {
         assertThatThrownBy(()->ValidateUtils.isCorrectRangeNumbers("21"))
                 .isInstanceOf(IllegalArgumentException.class);
     }
+
+    @DisplayName("U 또는 D 아닌 값을 입력하면 익셉션 발생")
+    @Test
+    void caseBridgeMoveInvalidInput(){
+        assertThatThrownBy(()->ValidateUtils.isCorrectMoveValue("A"))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
 }
