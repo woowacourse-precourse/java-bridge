2aac65ce9f09a74b02e30f4c30be5b19194c6d32
sora5421 <sojeoung536985@gmail.com>
2022-11-22 04:40:36 +0900

    feat : make printContinue method

--- src/main/java/bridge/view/OutputView.java
+++ src/main/java/bridge/view/OutputView.java
@@ -20,6 +20,10 @@ public class OutputView {
         System.out.println(result.get(result.size() - 1) + " ]");
     }
 
+    public void printContinue() {
+        System.out.println("게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)");
+    }
+
     /**
      * 게임의 최종 결과를 정해진 형식에 맞춰 출력한다.
      * <p>
