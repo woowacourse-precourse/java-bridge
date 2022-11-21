eabbfd983973e13831b360a7a6154630a3662e41
EUNSH1 <tkdgus968@naver.com>
2022-11-21 12:03:22 +0900

    feat : 선택한 위치가 이동할 수 있는지 비교하는 기능

--- src/main/java/bridge/BridgeEnum.java
+++ src/main/java/bridge/BridgeEnum.java
@@ -3,14 +3,22 @@ package bridge;
 import java.util.Arrays;
 
 public enum BridgeEnum {
-    U(1,"U"),
-    D(0,"D");
+    UP_TRUE(1,"U",true,"O"),
+    UP_FALSE(1,"U",false,"X"),
+    DOWN_TRUE(0,"D",true,"O"),
+    DOWN_FALSE(0,"D",false,"X");
 
     private final int bridgeNumberType;
     private final String bridgeStringType;
-    BridgeEnum(int numberType, String stringType) {
+    private final boolean correctLocation;
+    private final String checkLocation;
+
+
+    BridgeEnum(int numberType, String stringType,boolean correctLocation,String checkLocation) {
         this.bridgeNumberType = numberType;
         this.bridgeStringType = stringType;
+        this.correctLocation = correctLocation;
+        this.checkLocation = checkLocation;
     }
 
     public static BridgeEnum createBridgeNumberType(int locationNumber) {
@@ -27,10 +35,31 @@ public enum BridgeEnum {
                 .findFirst()
                 .orElseThrow();
     }
+
+    private static boolean isLocationNumber(BridgeEnum locationValue, int locationNumber) {
+        return locationValue.bridgeNumberType == locationNumber;
+    }
+
+    private static boolean isCorrectLocation(BridgeEnum locationValue, boolean correctLocation) {
+        return locationValue.correctLocation == correctLocation;
+    }
+
+    private static boolean isLocation(BridgeEnum locationValue, String location) {
+        return locationValue.bridgeStringType.equals(location);
     }
 
     public String getStringType() {
         return bridgeStringType;
+    }
+    public boolean getCorrectLocation() {
+        return correctLocation;
+    }
+
+    public String getStateLocation(String location) {
+        if (this.bridgeStringType.equals(location)) {
+            return checkLocation;
+        }
+        return " ";
     }
 
 }
--- src/main/java/bridge/OutputView.java
+++ src/main/java/bridge/OutputView.java
@@ -6,6 +6,10 @@ package bridge;
 public class OutputView {
 
     public static final String GAME_START_MESSAGE = "다리 건너기 게임을 시작합니다.\n";
+    public static final String DELIMITER = " | ";
+    public static final String JOIN_LIST_FORMAT = "[ %s ]";
+    public static final String LOCATION_UP_SELECT = "U";
+    public static final String LOCATION_DOWN_SELECT = "D";
     public static void printGameStart(){
         System.out.println(GAME_START_MESSAGE);
     }
@@ -14,7 +18,18 @@ public class OutputView {
      * <p>
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void printMap() {
+    public String printMap(List<BridgeEnum> bridgeLocations) {
+        return new StringJoiner("\n")
+                .add(createBridgeMap(bridgeLocations, LOCATION_UP_SELECT))
+                .add(createBridgeMap(bridgeLocations, LOCATION_DOWN_SELECT)).toString();
+    }
+
+    private String createBridgeMap(List<BridgeEnum> bridgeLocations, String location) {
+        StringJoiner bridge = new StringJoiner(DELIMITER);
+        bridgeLocations.stream()
+                .map(bridgeLocation -> bridgeLocation.getStateLocation(location))
+                .forEach(bridge::add);
+        return String.format(JOIN_LIST_FORMAT,bridge);
     }
 
     /**
