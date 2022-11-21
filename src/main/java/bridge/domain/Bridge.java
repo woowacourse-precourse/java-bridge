13423d6d2128ffc666630dc9149a42dd3f64c9f9
Dabeen Jeong <hello70825@gmail.com>
2022-11-21 22:34:18 +0900

    feat: bridge 객체 생성

---
+++ src/main/java/bridge/domain/Bridge.java
@@ -0,0 +1,21 @@
+package bridge.domain;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Bridge {
+    List<String> bridge;
+
+    public Bridge(List<String> bridge) {
+        this.bridge = bridge;
+    }
+
+    public String get(int idx) {
+        return bridge.get(idx);
+    }
+
+    @Override
+    public String toString() {
+        return bridge.stream().collect(Collectors.joining(" | ", "[ ", " ]"));
+    }
+}
