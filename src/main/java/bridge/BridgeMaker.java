fce4fe2b29c804145903fca7fed90a0a02aa5d75
eunji53 <duqtj00@naver.com>
2022-11-21 20:17:24 +0900

    feat : 초기 다리 상태

--- src/main/java/bridge/BridgeMaker.java
+++ src/main/java/bridge/BridgeMaker.java
@@ -1,5 +1,6 @@
 package bridge;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -18,6 +19,14 @@ public class BridgeMaker {
      * @return 입력받은 길이에 해당하는 다리 모양. 위 칸이면 "U", 아래 칸이면 "D"로 표현해야 한다.
      */
     public List<String> makeBridge(int size) {
+        List<String> bridgeState = initialBridge();
+
         return null;
+    }
+    public List<String> initialBridge() {
+        List<String> initialBridgeState = new ArrayList();
+        initialBridgeState.add("[");
+        initialBridgeState.add("[");
+        return initialBridgeState;
     }
 }
