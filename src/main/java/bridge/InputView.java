d1af5a31a010758fbd2be4efe01d3afd9a6137bf
syr9707 <yrshin9771@gmail.com>
2022-11-21 19:10:57 +0900

    feat(inputView): 시작 문구 출력

--- src/main/java/bridge/InputView.java
+++ src/main/java/bridge/InputView.java
@@ -1,9 +1,27 @@
 package bridge;
 
+import camp.nextstep.edu.missionutils.Console;
+
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
  */
 public class InputView {
+
+    public static int BRIDGE_SIZE;
+    public static String MOVING;
+    public static String GAME_COMMAND;
+
+    public static String ERROR_BRIDGE_SIZE = "[ERROR] 다리 길이는 3부터 20 사이의 숫자여야 합니다.";
+    public static String ERROR_MOVING = "[ERROR] 이동할 칸은 U 또는 D여야 합니다.";
+    public static String ERROR_RESTART = "[ERROR] 게임 다시 시도 여부는 R 또는 Q여야 합니다.";
+
+    public InputView() {
+        System.out.println("다리 건너기 게임을 시작합니다." + "\n");
+    }
+
+    static void errorBridgeSize() {
+        System.out.println(ERROR_BRIDGE_SIZE);
+    }
 
     /**
      * 다리의 길이를 입력받는다.
