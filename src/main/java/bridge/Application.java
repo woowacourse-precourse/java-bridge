41ad90d0663959e3536da909f25527aa51f97780
lullulu__ <75472116+earthyoung@users.noreply.github.com>
2022-11-22 01:54:33 +0900

    fix: 입출력 형식 맞추도록 출력문 수정

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -7,6 +7,7 @@ public class Application {
         int count = 0;
         BridgeMaker bridgeMaker = new BridgeMaker(new BridgeRandomNumberGenerator());
         boolean result = true;
+        System.out.println("다리 건너기 게임을 시작합니다.\n");
         while(result){
             count++;
             InputView inputView = new InputView(bridgeMaker, count);
