0def1ed7c586058cda578bad54c0c7ef18bb20b4
JaehongDev <println@kakao.com>
2022-11-21 20:17:26 +0900

    feature (GameStatus) : 게임이 진행중인지 아닌지 혹은 클리어인지 실패인지 구별하는 기능 추가

---
+++ src/main/java/bridge/domain/code/GameStatus.java
@@ -0,0 +1,38 @@
+package bridge.domain.code;
+
+import bridge.domain.bridge.Bridge;
+
+public enum GameStatus {
+    RUNNING("실행중"),
+    CLEAR("성공"),
+    FAIL("실패");
+
+    private final String code;
+
+    GameStatus(final String code) {
+        this.code = code;
+
+    }
+
+    public static GameStatus generateGameStatus(SurviveStatus surviveStatus, Bridge bridge) {
+        if (surviveStatus.isAlive() && bridge.canMove()) {
+            return RUNNING;
+        }
+        if (surviveStatus.isAlive() && bridge.canNotMove()) {
+            return CLEAR;
+        }
+        return FAIL;
+    }
+
+    public boolean isRunning() {
+        return this.equals(RUNNING);
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public boolean isClear() {
+        return this.equals(CLEAR);
+    }
+}
