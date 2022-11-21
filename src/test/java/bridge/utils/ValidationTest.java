742ad70392c0dd483c29b6a229ac761bb0380abc
yujaemin <woawoa0513@gmail.com>
2022-11-21 15:04:42 +0900

    test: ParametrizedTest 로직 수정
    try-catch문 안에서 메시지를 확인하는 방식은 예외가 발생하지 않았을 떄 제대로 확인을 못 하기 때문에 수정

--- src/test/java/bridge/utils/ValidationTest.java
+++ src/test/java/bridge/utils/ValidationTest.java
@@ -1,7 +1,10 @@
 package bridge.utils;
 
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.assertj.core.api.Assertions.assertThat;
 import static bridge.domain.ErrorMessage.BRIDGE_SIZE_INCORRECT_INPUT;
 import static bridge.domain.ErrorMessage.BRIDGE_INCORRECT_SIZE;
+import static bridge.domain.ErrorMessage.GAME_COMMAND_INCORRECT_INPUT;
 import static bridge.domain.ErrorMessage.MOVING_INCORRECT_INPUT;
 
 import org.junit.jupiter.api.Assertions;
@@ -26,21 +29,29 @@ class ValidationTest {
     @ValueSource(strings = {"1", "2", "21", "30"})
     @DisplayName("다리 길이에 3 미만, 20 초과의 값을 입력하면 예외 발생")
     void 다리길이_3_미만_20_초과_길이_입력(String input) {
-        try {
-            Validation.validateBridgeSize(input);
-        } catch (IllegalArgumentException e) {
-            Assertions.assertEquals(BRIDGE_INCORRECT_SIZE.toString(), e.getMessage());
-        }
+        IllegalArgumentException e = assertThrows(
+                IllegalArgumentException.class,
+                () -> Validation.validateBridgeSize(input));
+        assertThat(e.getMessage()).isEqualTo(BRIDGE_INCORRECT_SIZE.toString());
     }
 
     @ParameterizedTest
     @ValueSource(strings = {"UP", "DOWN", "M", "u", "d"})
     @DisplayName("이동할 칸에 'U' 또는 'D'가 아닌 값을 입력하면 예외 발생")
     void 이동할_칸_U_또는_D가_아닌_입력(String input) {
-        try {
-            Validation.validateMoving(input);
-        } catch (IllegalArgumentException e) {
-            Assertions.assertEquals(MOVING_INCORRECT_INPUT.toString(), e.getMessage());
-        }
+        IllegalArgumentException e = assertThrows(
+                IllegalArgumentException.class,
+                () -> Validation.validateMoving(input));
+        assertThat(e.getMessage()).isEqualTo(MOVING_INCORRECT_INPUT.toString());
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"RESTART", "QUIT", "U", "r", "q"})
+    @DisplayName("게임 재시작/종료 여부에 'R' 또는 'Q'가 아닌 값을 입력하면 예외 발생")
+    void 게임_재시작_여부에_R_또는_Q가_아닌_입력(String input) {
+        IllegalArgumentException e = assertThrows(
+                IllegalArgumentException.class,
+                () -> Validation.validateGameCommand(input));
+        assertThat(e.getMessage()).isEqualTo(GAME_COMMAND_INCORRECT_INPUT.toString());
     }
 }
