5b75c7f50c7fc62cbb7e16c29fd5262bd87b954c
JUHA <khabh@naver.com>
2022-11-21 10:47:11 +0900

    chore: 불필요한 파일 삭제 및 코드 컨벤션 수정

--- src/main/java/bridge/exception/MovingDirectionFormatException.java
+++ src/main/java/bridge/exception/MovingDirectionFormatException.java
@@ -1,6 +1,6 @@
 package bridge.exception;
 
-public class MovingDirectionFormatException extends IllegalArgumentException{
+public class MovingDirectionFormatException extends IllegalArgumentException {
     private final static String ERROR_MESSAGE = "[ERROR] 이동할 칸은 U/D 중에서 선택해야 합니다.";
 
     public MovingDirectionFormatException() {
--- src/main/java/bridge/exception/NotNumericException.java
+++ src/main/java/bridge/exception/NotNumericException.java
@@ -1,6 +1,6 @@
 package bridge.exception;
 
-public class NotNumericException extends IllegalArgumentException{
+public class NotNumericException extends IllegalArgumentException {
     private final static String ERROR_MESSAGE = "[ERROR] 올바른 숫자 입력이 아닙니다.";
 
     public NotNumericException() {
