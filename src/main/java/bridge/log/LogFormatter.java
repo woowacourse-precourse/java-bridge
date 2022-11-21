9e0116890e37bdc5eb840f80baa0db222e6f46fb
lcw3176 <lcw3176@naver.com>
2022-11-21 13:34:40 +0900

    refactor(LogFormatter): 10줄 이내로 재작성

--- src/main/java/bridge/log/LogFormatter.java
+++ src/main/java/bridge/log/LogFormatter.java
@@ -6,20 +6,36 @@ import java.time.format.DateTimeFormatter;
 public class LogFormatter {
 
     public String format(Throwable e) {
+        StringBuilder sb = new StringBuilder();
+        setDate(sb);
+        setMessage(e, sb);
+        setStackTrace(e, sb);
+
+        return sb.toString();
+    }
+
+    private StringBuilder setDate(StringBuilder sb) {
         DateTimeFormatter dateFormat = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
         String date = LocalDateTime.now().format(dateFormat);
-
-        StringBuilder sb = new StringBuilder();
         sb.append(date);
+
+        return sb;
+    }
+
+    private StringBuilder setMessage(Throwable e, StringBuilder sb) {
         sb.append(" [ERROR] ");
         sb.append(e.getMessage());
         sb.append("\n");
 
+        return sb;
+    }
+
+    private StringBuilder setStackTrace(Throwable e, StringBuilder sb) {
         for (StackTraceElement element : e.getStackTrace()) {
             sb.append(element.toString());
             sb.append("\n");
         }
 
-        return sb.toString();
+        return sb;
     }
 }
