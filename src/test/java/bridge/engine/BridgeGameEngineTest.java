2c83ea1212f174d46dd233d3fc2716e87380569d
UJ15 <dbwogml15@naver.com>
2022-11-21 16:09:53 +0900

    test : add cross method test

--- src/test/java/bridge/engine/BridgeGameEngineTest.java
+++ src/test/java/bridge/engine/BridgeGameEngineTest.java
@@ -4,11 +4,19 @@ import bridge.BridgeMaker;
 import bridge.BridgeNumberGenerator;
 import bridge.BridgeRandomNumberGenerator;
 import bridge.model.Bridge;
+import bridge.model.Crossing;
+import bridge.model.UpOrDown;
+import java.util.ArrayList;
+import java.util.stream.Stream;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.TestInstance.Lifecycle;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 class BridgeGameEngineTest {
@@ -43,6 +51,29 @@ class BridgeGameEngineTest {
         void returnBridgeReceiveValidRange(int inputSize) {
             //then
             Assertions.assertThat(gameEngine.makeBridge(inputSize)).isExactlyInstanceOf(Bridge.class);
+        }
+    }
+
+    @Nested
+    @TestInstance(Lifecycle.PER_CLASS)
+    @DisplayName("cross 메서드는")
+    class DescribeCross {
+
+        private Stream<Arguments> provideNullSource() {
+            return Stream.of(
+                    Arguments.of(null, new Bridge(new ArrayList<>()), new UpOrDown("U")),
+                    Arguments.of(new Crossing(new ArrayList<>()), null, new UpOrDown("U")),
+                    Arguments.of(new Crossing(new ArrayList<>()), new Bridge(new ArrayList<>()), null)
+            );
+        }
+
+        @ParameterizedTest
+        @MethodSource("provideNullSource")
+        @DisplayName("인자 중 하나라도 null이라면 IllegalArgumentException을 반환")
+        void throwIllegalArgumentExceptionWhenReceiveNull(Crossing crossing, Bridge bridge, UpOrDown upOrDown) {
+            //then
+            Assertions.assertThatThrownBy(() -> gameEngine.cross(crossing, bridge, upOrDown))
+                    .isInstanceOf(IllegalArgumentException.class);
         }
     }
 }
