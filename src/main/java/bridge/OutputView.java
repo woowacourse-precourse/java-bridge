7ebf3888956e666fe5ec2a2ace8be45d872b6698
WonSubSub <boxer1995@naver.com>
2022-11-21 21:13:59 +0900

    feat(main): 결과 메시지를 출력하는 기능 추가

--- src/main/java/bridge/OutputView.java
+++ src/main/java/bridge/OutputView.java
@@ -1,5 +1,7 @@
 package bridge;
 
+import java.util.List;
+
 /**
  * 사용자에게 게임 진행 상황과 결과를 출력하는 역할을 한다.
  */
@@ -18,6 +20,14 @@ public class OutputView {
      * <p>
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void printResult() {
+    public void printResult(boolean wrongMovement, int count) {
+        if (wrongMovement == true) {
+            System.out.println("게임 성공 여부: 실패");
+            System.out.println(String.format("총 시도한 횟수: %d", count));
+        }
+        if (wrongMovement == false) {
+            System.out.println("게임 성공 여부: 성공");
+            System.out.println(String.format("총 시도한 횟수: %d", count));
+        }
     }
 }
