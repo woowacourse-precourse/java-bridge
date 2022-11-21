850c066aa1f2e2362c5e44aefacf6eab012108f2
saehoon0501 <76736828+saehoon0501@users.noreply.github.com>
2022-11-21 19:12:47 +0900

    feat: InputView readBridgeSize 관련 validation 함수 구현

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -1,15 +1,40 @@
 package view;
 
+import camp.nextstep.edu.missionutils.Console;
+
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
  */
 public class InputView {
 
+    public InputView() {
+    }
+
     /**
      * 다리의 길이를 입력받는다.
      */
     public int readBridgeSize() {
-        return 0;
+        String input = Console.readLine();
+        int size = validateSize(input);
+
+        return size;
+    }
+
+    private int validateSize(String input) {
+        try{
+            int size = Integer.parseInt(input);
+            validateRange(3,20,size);
+
+            return size;
+        }catch (NumberFormatException e){
+            throw new IllegalArgumentException("[ERROR] Only numbers allowed");
+        }
+    }
+
+    private void validateRange(int start, int end, int value){
+        if (value < start || value > end){
+            System.err.println(new IllegalArgumentException("[ERROR] Numbers should be in the range of"+start+"~"+end));
+        }
     }
 
     /**
