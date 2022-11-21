42ce30c8bd2583ed2f804adfc44fbd02074ad9c7
wonwoo6y <wonwoo6y@gmail.com>
2022-11-22 02:03:49 +0900

    feat(BridgeGameCommand): 재시작 여부를 표시하는 enum 클래스 구현

--- src/main/java/bridge/BridgeGameCommand.java
+++ src/main/java/bridge/BridgeGameCommand.java
@@ -1,2 +1,23 @@
-package bridge;public class BridgeGameCommand {
+package bridge;
+
+import java.util.Arrays;
+
+public enum BridgeGameCommand {
+    R(true),
+    Q(false);
+
+    private boolean bool;
+
+    BridgeGameCommand(Boolean bool) {this.bool = bool;}
+
+    public boolean isBool() {
+        return bool;
+    }
+
+    public static boolean toBoolean(String str) {
+        return Arrays.stream(BridgeGameCommand.values())
+                .filter(command -> command.name().equals(str))
+                .findAny()
+                .get().isBool();
+    }
 }
