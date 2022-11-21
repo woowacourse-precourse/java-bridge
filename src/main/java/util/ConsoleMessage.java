6687794096d73ca56a3c62a400ff2e42e5227d22
gyuturn <rbtjrrbals@gmail.com>
2022-11-21 21:25:00 +0900

    feat($view): 유저의 현재 상태 및 결과 출력
    F,I
    - 유저의 현재 상태에 대한 결과 출력
    - printMap을 재사용해서 최종적인 결과 출력 로직

--- src/main/java/util/ConsoleMessage.java
+++ src/main/java/util/ConsoleMessage.java
@@ -6,4 +6,10 @@ public class ConsoleMessage {
     public static final String RECEIVE_BRIDGE_SIZE="다리의 길이를 입력해주세요.";
 
     public static final String SELECT_MOVE = "이동할 칸을 선택해주세요. (위: U, 아래: D)";
+
+    public static final String SELECT_RETRY = "게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)";
+
+    public static final String FINAL_RESULT = "최종 게임 결과";
+    public static final String WHETHER_SUCCESS = "게임 성공 여부: %s\n";
+    public static final String TOTAL_RETRY_COUNT = "총 시도한 횟수: %s\n";
 }
