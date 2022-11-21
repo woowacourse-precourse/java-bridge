025112971f205a0ee7ff9ab57c96ad59401769c8
lvalentine6 <ysr1227@naver.com>
2022-11-22 04:03:27 +0900

    test : 사용자의 입력과 만들어진 칸을 비교하는 기능 테스트 구현

---
+++ src/test/java/bridge/model/ComparisonTest.java
@@ -0,0 +1,18 @@
+package bridge.model;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import bridge.controller.GameController;
+import java.util.ArrayList;
+import java.util.List;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+
+class ComparisonTest {
+
+//    @Test
+//    void comparisonSpace() {
+//        String input = "U";
+//        assertTrue(Comparison.comparisonSpace(input, new ArrayList<>(List.of("U", "D", "D"))));
+//    }
+}
