a23b3c008805e59767b9ec6bc2eb1b29d11ea921
HGB <hsw8147@gmail.com>
2022-11-21 15:42:51 +0900

    fix: Seperate Enum
    다른 클래스에서도 위치의 위,아래를 나타내는 상수값 활용 가능하도록 분리

--- src/main/java/bridge/UpOrDown.java
+++ src/main/java/bridge/UpOrDown.java
@@ -1,2 +1,15 @@
-package bridge;public enum UpOrDown {
+package bridge;
+
+public enum UpOrDown {
+    UP(2),
+    DOWN(1)
+    ;
+    private final int value;
+    UpOrDown(int value){
+        this.value = value;
+    }
+
+    public int getValue() {
+        return value;
+    }
 }
