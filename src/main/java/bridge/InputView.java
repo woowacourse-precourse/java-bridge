e2386467e75aea21a3cd6c76c194f3c7cdcb7119
ji-mango <susie000301@naver.com>
2022-11-21 16:06:52 +0900

    feat: try-catch, 예외 처리하고 다시 입력받는 기능

--- src/main/java/bridge/InputView.java
+++ src/main/java/bridge/InputView.java
@@ -6,11 +6,48 @@ import camp.nextstep.edu.missionutils.Console;
  * 사용자로부터 입력을 받는 역할을 한다.
  */
 public class InputView {
+    public int readBridgeSize() {
+        int bridgeSize = 0;
 
+        try {
+            bridgeSize = tryReadBridgeSize();
+        } catch (IllegalArgumentException exception) {
+            System.out.println(exception.getMessage());
+            return readBridgeSize();
+        }
+
+        return bridgeSize;
+    }
+
+    public String readMoving() {
+        String moving = "";
+
+        try {
+            moving = tryReadMoving();
+        } catch (IllegalArgumentException exception) {
+            System.out.println(exception.getMessage());
+            return readMoving();
+        }
+
+        return moving;
+    }
+
+    public String readGameCommand() {
+        String command = "";
+
+        try {
+            command = tryReadGameCommand();
+        } catch (IllegalArgumentException exception) {
+            System.out.println(exception.getMessage());
+            return readGameCommand();
+        }
+
+        return command;
+    }
     /**
      * 다리의 길이를 입력받는다.
      */
-    public int readBridgeSize() {
+    public int tryReadBridgeSize() {
         System.out.println("\n다리의 길이를 입력해주세요.");
         String input = Console.readLine();
         checkIsDigit(input);
@@ -24,7 +61,7 @@ public class InputView {
     /**
      * 사용자가 이동할 칸을 입력받는다.
      */
-    public String readMoving() {
+    public String tryReadMoving() {
         System.out.println("\n이동할 칸을 선택해주세요. (위: U, 아래: D)");
         String moving = Console.readLine();
         checkMoving(moving);
@@ -35,7 +72,7 @@ public class InputView {
     /**
      * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
      */
-    public String readGameCommand() {
+    public String tryReadGameCommand() {
         System.out.println("\n게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)");
         String command = Console.readLine();
         checkCommand(command);
