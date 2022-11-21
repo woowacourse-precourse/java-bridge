98c401ecd3aae801d4bfe0874cd207d141f2b54e
Eundms <dms011221@gmail.com>
2022-11-21 22:23:25 +0900

    test(ValidationTest): GameCommand

--- src/test/java/bridge/ValidationTest.java
+++ src/test/java/bridge/ValidationTest.java
@@ -1,12 +1,15 @@
 package bridge;
 
 import static bridge.validation.ErrorMessage.ERROR_INVALID_BRIDGE_SIZE;
+import static bridge.validation.ErrorMessage.ERROR_INVALID_GAME_COMMAND;
 import static bridge.validation.ErrorMessage.ERROR_INVALID_MOVE_COMMAND;
 import static bridge.validation.Validation.validateBridgeSize;
+import static bridge.validation.Validation.validateGameCommand;
 import static bridge.validation.Validation.validateMoveCommand;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
@@ -33,8 +36,8 @@ class ValidationTest {
     @DisplayName("[ERROR] R 또는 Q 이외의 값 입력시 예외 발생")
     @CsvSource({"1Q", "1R", "QQ", "RR"})
     void invalidInputInGameCommand(String userInput) {
-        assertThatThrownBy(() -> validateMoveCommand(userInput))
+        assertThatThrownBy(() -> validateGameCommand(userInput))
                 .isInstanceOf(IllegalArgumentException.class)
-                .hasMessage(ERROR_INVALID_MOVE_COMMAND);
+                .hasMessage(ERROR_INVALID_GAME_COMMAND);
     }
 }
