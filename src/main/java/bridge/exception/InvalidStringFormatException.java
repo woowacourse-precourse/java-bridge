9ad27ece0b1540d40cb2cd6688fecd5065abb112
Dabeen Jeong <hello70825@gmail.com>
2022-11-21 21:35:04 +0900

    refactor: 사용하지 않는 import 삭제

--- src/main/java/bridge/exception/InvalidStringFormatException.java
+++ src/main/java/bridge/exception/InvalidStringFormatException.java
@@ -1,7 +1,5 @@
 package bridge.exception;
 
-import java.util.regex.Pattern;
-
 public class InvalidStringFormatException extends IllegalArgumentException {
     private static final String ERROR_MESSAGE = "[ERROR] 문자를 입력해주세요.";
 
