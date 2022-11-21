89864149ba48da642005d1f490f701fea2956595
WonSubSub <boxer1995@naver.com>
2022-11-21 18:01:29 +0900

    feat(main): 이동할 칸, 게임 재시작 여부를 입력받는 기능 추가

--- src/main/java/bridge/InputView.java
+++ src/main/java/bridge/InputView.java
@@ -19,13 +19,15 @@ public class InputView {
      * 사용자가 이동할 칸을 입력받는다.
      */
     public String readMoving() {
-        return null;
+        String alphabet = Console.readLine();
+        return alphabet;
     }
 
     /**
      * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
      */
     public String readGameCommand() {
-        return null;
+        String alphabet = Console.readLine();
+        return alphabet;
     }
 }
