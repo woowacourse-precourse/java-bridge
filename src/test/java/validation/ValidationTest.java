74b7dee5b720a1be5b9b3a8e1f1d424bf0bf1482
thousandcows <milcamboo@gmail.com>
2022-11-21 17:30:22 +0900

    feat: create validateBridgeSize test

--- src/test/java/validation/ValidationTest.java
+++ src/test/java/validation/ValidationTest.java
@@ -1,4 +1,29 @@
 package validation;
 
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+
 public class ValidationTest {
+
+    @Test
+    void validateBridgeSizeTest() {
+        String stringValue = "three";
+        String negativeValue = "-1";
+        String zeroValue = "0";
+        String doubleValue = "1.1";
+
+        InputValidation validator = new InputValidation();
+
+        assertThatThrownBy(() -> validator.validateBridgeSize(stringValue))
+                .isInstanceOf(IllegalArgumentException.class);
+        assertThatThrownBy(() -> validator.validateBridgeSize(negativeValue))
+                .isInstanceOf(IllegalArgumentException.class);
+        assertThatThrownBy(() -> validator.validateBridgeSize(zeroValue))
+                .isInstanceOf(IllegalArgumentException.class);
+        assertThatThrownBy(() -> validator.validateBridgeSize(doubleValue))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
 }
