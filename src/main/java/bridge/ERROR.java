9469a645b338b6523ddf838eeef75cb2f5774bc6
KSB-skyBlueLover <gogotkdqja@yonsei.ac.kr>
2022-11-21 23:13:53 +0900

    feat: implement the function that reads a user input of moving

--- src/main/java/bridge/ERROR.java
+++ src/main/java/bridge/ERROR.java
@@ -2,4 +2,5 @@ package bridge;
 
 public class ERROR {
     static final String BRIDGE_SIZE = "[ERROR] 다리 길이는 3부터 20 사이의 숫자여야 합니다.";
+    static final String MOVING = "[ERROR] 이동할 칸은 한 글자 U 또는 D만을 입력하여야 합니다.";
 }
