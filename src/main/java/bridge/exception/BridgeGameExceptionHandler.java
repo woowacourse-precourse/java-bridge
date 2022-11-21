d599f11fca949a5043e723f8629b9e29997a42bf
Sehbeom <j3beom@gmail.com>
2022-11-22 02:37:30 +0900

    refactor(exception): 이동 기능 예외처리 기능 삭제
    이동 기능 예외처리 과정 불필요함으로 인해 삭제

--- src/main/java/bridge/exception/BridgeGameExceptionHandler.java
+++
@@ -1,9 +0,0 @@
-package bridge.exception;
-
-public class BridgeGameExceptionHandler {
-    public static void handleMoveSpaceOutOfRangeException(int space) {
-        if (!(3 <= space && space <= 20)) {
-            throw new IllegalStateException("[ERROR] 이동 가능 범위(3~20)를 초과하였습니다.");
-        }
-    }
-}
