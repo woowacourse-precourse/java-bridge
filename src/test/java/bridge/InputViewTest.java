220599c6d0697d865c9ae31cbe6a841c09f3f91f
poter0501 <poter0501@gmail.com>
2022-11-21 09:15:17 +0900

    test(InputViewTest): tryParseInt 함수의 예외 사항을 테스트 하는 코드 추가

--- src/test/java/bridge/InputViewTest.java
+++ src/test/java/bridge/InputViewTest.java
@@ -1,12 +1,24 @@
 package bridge;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.assertj.core.api.Assertions.assertThat;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 class InputViewTest {
 
-    private static final InputViewTest INPUT_VIEW_TEST= new InputViewTest();
+    private static final InputView INPUT_VIEW = new InputView();
 
+    @DisplayName("tryParse 예외 사항 테스트")
+    @ParameterizedTest
+    @ValueSource(strings = {"1000j", "", " ", "1 3", "1a3", "1,2,3", " 1", "1 "})
+    void tryParseInt_exception_test(String testInput) {
+        assertThatThrownBy(() -> INPUT_VIEW.tryParseInt(testInput))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
 }
