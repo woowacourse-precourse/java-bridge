b03def65119b393d77b87add7c8cdca5bf81c6ea
whdcks420 <whdcks420@gmail.com>
2022-11-21 18:52:16 +0900

    feat: 브릿지 상태 기능

--- src/main/java/bridge/domain/BridgeStatus.java
+++ src/main/java/bridge/domain/BridgeStatus.java
@@ -1,2 +1,13 @@
-package bridge.domain;public enum BridgeStatus {
+package bridge.domain;
+
+public enum BridgeStatus {
+    INIT("   "),
+    SUCCESS(" O "),
+    FAIL(" X ");
+
+    public String buildBridge;
+
+    BridgeStatus(String printBridge) {
+        this.buildBridge = printBridge;
+    }
 }
