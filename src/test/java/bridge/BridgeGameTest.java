28919033a7ce52f4e3f6f5eb9c34f8115fa539db
HGB <hsw8147@gmail.com>
2022-11-21 09:52:05 +0900

    test: Fix BridgeGame method Test
    BridgeGame 메서드 수정에 따라 테스트 수정

--- src/test/java/bridge/BridgeGameTest.java
+++ src/test/java/bridge/BridgeGameTest.java
@@ -4,19 +4,18 @@ import camp.nextstep.edu.missionutils.Randoms;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class BridgeGameTest {
-
-    @DisplayName("이동 테스트")
+    @DisplayName("이동 횟수 테스트")
     @Test
     public void moveTest(){
         BridgeGame bridgeGame = new BridgeGame();
         int randomNumber = Randoms.pickNumberInRange(1,9);
-        for(int i=0; i<randomNumber; i++){
-            bridgeGame.move();
-        }
-        assertThat(bridgeGame.getPosition()).isEqualTo(randomNumber);
+        repeatMove(bridgeGame, randomNumber);
+        assertThat(bridgeGame.getPosition().get(1)).isEqualTo(randomNumber);
     }
 
     @DisplayName("재시도 테스트")
@@ -24,10 +23,26 @@ public class BridgeGameTest {
     public void retryTest(){
         BridgeGame bridgeGame = new BridgeGame();
         int randomNumber = Randoms.pickNumberInRange(1,9);
-        for(int i=0; i<randomNumber; i++){
-            bridgeGame.retry();
-        }
-        assertThat(bridgeGame.getPosition()).isEqualTo(-randomNumber);
-        assertThat(bridgeGame.getRetryNumber()).isEqualTo(randomNumber);
+        System.out.println(randomNumber);
+        repeatMove(bridgeGame, randomNumber);
+        int positionAfterMove = bridgeGame.getPosition().get(1);
+        bridgeGame.retry();
+        int positionBeforeMove = bridgeGame.getPosition().get(1);
+        assertThat(positionAfterMove).isEqualTo(positionBeforeMove+1);
+        assertThat(bridgeGame.getRetryNumber()).isEqualTo(1);
     }
+
+    private void repeatMove(BridgeGame bridgeGame,int randomNumber){
+        BridgeRandomNumberGenerator bridgeRandomNumberGenerator = new BridgeRandomNumberGenerator();
+        for(int i=0; i<randomNumber; i++){
+            int upOrDownNumber = bridgeRandomNumberGenerator.generate();
+            if(upOrDownNumber == 0){
+                bridgeGame.move("D");
+            }
+            if(upOrDownNumber == 1){
+                bridgeGame.move("U");
+            }
+        }
+    }
+
 }
