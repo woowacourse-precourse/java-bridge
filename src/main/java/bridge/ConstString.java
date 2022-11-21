8d3b2d64409c7cc3e8d735901c9ac4c79b6525a0
taejeong <sontaewjd@nate.com>
2022-11-21 22:34:01 +0900

    feat: 출력 문자열들 하나의 상수 변수로 만들기
    클래스 안의 변수로 상수 선언

---
+++ src/main/java/bridge/ConstString.java
@@ -0,0 +1,11 @@
+package bridge;
+
+public class ConstString {
+    public static final String START_GAME = "다리 건너기 게임을 시작합니다.";
+    public static final String INPUT_SIZE = "다리의 길이를 입력해주세요.";
+    public static final String CHOOSE_STEP = "이동할 칸을 선택해주세요. (위: U, 아래: D)";
+    public static final String RETRY_OR_QUIT = "게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)";
+    public static final String FINAL_RESULT = "최종 게임 결과";
+    public static final String IS_CLEAR = "게임 성공 여부: ";
+    public static final String TRY_COUNT = "총 시도한 횟수: ";
+}
