529c4e14163e26c41bc3d1f71de6170e5dbfb567
chordpli <chordplaylist@gmail.com>
2022-11-21 21:42:10 +0900

    refactor: print 분리 enum 추가
    1. OutputView 분리
    2. enum 사용하여 출력

--- src/main/java/bridge/OutputView.java
+++ src/main/java/bridge/OutputView.java
@@ -2,10 +2,16 @@ package bridge;
 
 import java.util.List;
 
+import static bridge.Message.*;
+
 /**
  * 사용자에게 게임 진행 상황과 결과를 출력하는 역할을 한다.
  */
 public class OutputView {
+    public void startMessage() {
+        System.out.println(PRINT_START.getMessage());
+    }
+
     /**
      * 현재까지 이동한 다리의 상태를 정해진 형식에 맞춰 출력한다.
      * <p>
@@ -35,6 +41,15 @@ public class OutputView {
 
     }
 
+    public void endAfterFailure(BridgeGame bridgeGame, String game, int count) {
+        if (game.equals("Q")) {
+            System.out.println(PRINT_FINAL_RESULT.getMessage());
+            printMap(bridgeGame.getUpBridge(), bridgeGame.getDownBridge());
+            System.out.println(PRINT_IS_SUCCESSFUL.getMessage() + FAIL.getMessage());
+            System.out.println(PRINT_NUMBER_OF_ATTEMPTS.getMessage() + count);
+        }
+    }
+
     /**
      * 게임의 최종 결과를 정해진 형식에 맞춰 출력한다.
      * <p>
@@ -42,10 +57,10 @@ public class OutputView {
      */
     public String printResult(BridgeGame bridgeGame, int bridgeSize, int count, String game) {
         if (bridgeGame.getBridgeCount() == bridgeSize) {
-            System.out.println("최종 게임 결과");
+            System.out.println(PRINT_FINAL_RESULT.getMessage());
             printMap(bridgeGame.getUpBridge(), bridgeGame.getDownBridge());
-            System.out.println("게임 성공 여부: 성공");
-            System.out.println("총 시도한 횟수: " + count);
+            System.out.println(PRINT_IS_SUCCESSFUL.getMessage() + SUCCESS.getMessage());
+            System.out.println(PRINT_NUMBER_OF_ATTEMPTS.getMessage() + count);
             return "E";
         }
         return "R";
