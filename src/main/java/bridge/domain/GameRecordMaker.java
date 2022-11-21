9158ab5981e27f35319f8d39a3115a98f9d45870
gomljo <gomljo0307@naver.com>
2022-11-22 00:42:20 +0900

    feat(GameRecordMaker): 다리 건넌 횟수가 1회 이상인지 확인하는 기능 구현

--- src/main/java/bridge/domain/GameRecordMaker.java
+++ src/main/java/bridge/domain/GameRecordMaker.java
@@ -1,16 +1,22 @@
 package bridge.domain;
 
-import java.util.*;
+import bridge.constant.GameRecordFormat;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class GameRecordMaker {
     private final List<List<String>> TOTAL_RESULT;
     private final int ZERO=0;
-
+    
     private int recordLength;
 
     public GameRecordMaker(){
         recordLength=ZERO;
         TOTAL_RESULT=List.of(new ArrayList<>(), new ArrayList<>());
+    }
+    private boolean isRecordLengthOverOne(){
+        return recordLength==ZERO;
     }
 
 }
