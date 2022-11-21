75b8f16e625abb7d99efebc36e783808fd90dbd9
soyunim <togksk123@naver.com>
2022-11-21 22:35:38 +0900

    feat: 다리 길이 입력

--- src/main/java/bridge/GameProcess.java
+++ src/main/java/bridge/GameProcess.java
@@ -1,2 +1,19 @@
-package bridge;public class GameProcess {
+package bridge;
+
+public class GameProcess {
+    private static final InputView inputView = new InputView();
+    public static String size;
+    public static int length;
+    public static void gameStart(){
+        try{
+            size = inputView.readBridgeSize();
+            length = Integer.parseInt(size);
+        }
+        catch(NumberFormatException e){
+            throw new IllegalArgumentException("[ERROR] 다리 길이 입력값이 잘못됨.");
+        }
+        if(length>20 || length<3){
+            throw new IllegalArgumentException("[ERROR] 다리 길이 입력범위가 잘못됨.");
+        }
+    }
 }
