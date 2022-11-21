e00b8eda6c7e2c5641ed460961cf5b7d0742207a
minaver <psmjoshua@gmail.com>
2022-11-21 13:16:33 +0900

    test(BridgeGameTest): 재시작 테스트 추가

--- src/test/java/bridge/BridgeGameTest.java
+++ src/test/java/bridge/BridgeGameTest.java
@@ -2,6 +2,7 @@ package bridge;
 
 import bridge.bridgeGame.BridgeGame;
 import bridge.bridgeGame.BridgeGameResultDto;
+import bridge.bridgeGame.StepStatus;
 import bridge.enums.UpDown;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -56,6 +57,52 @@ public class BridgeGameTest {
         // then
         assertThat(collectUpDown).containsExactly(UpDown.DOWN);
         assertThat(collectIsCorrect).containsExactly(false);
-
     }
+
+    @DisplayName("재시작 했을 경우 시도횟수가 리셋되는지 확인")
+    @Test
+    void 재시작_시도횟수_테스트() {
+        // given
+        BridgeGame bridgeGame = new BridgeGame(newArrayList("U","D","U","U","D"));
+
+        // when
+        bridgeGame.move("U");
+        bridgeGame.move("D");
+        BridgeGameResultDto beforeRetry = bridgeGame.move("U");
+
+        bridgeGame.retry();
+        BridgeGameResultDto afterRetry = bridgeGame.move("U");
+
+        // then
+        assertThat(beforeRetry.getTotalAttemptNum()).isEqualTo(1);
+        assertThat(afterRetry.getTotalAttemptNum()).isEqualTo(2);
+    }
+
+    @DisplayName("재시작 했을 경우 점수판(gradingBoard) 리셋되는지 확인")
+    @Test
+    void 재시작_점수판_테스트() {
+        // given
+        BridgeGame bridgeGame = new BridgeGame(newArrayList("U","D","U","U","D"));
+
+        // when
+        bridgeGame.move("U");
+        bridgeGame.move("D");
+
+        bridgeGame.retry();
+        BridgeGameResultDto afterRetry = bridgeGame.move("U");
+
+        List<UpDown> collectUpDown = afterRetry.getGradingBoard().stream()
+                .map(v -> v.getUpDown())
+                .collect(Collectors.toList());
+
+        List<Boolean> collectIsCorrect = afterRetry.getGradingBoard().stream()
+                .map(v -> v.isCorrect())
+                .collect(Collectors.toList());
+
+        // then
+        assertThat(collectUpDown).containsExactly(UpDown.UP);
+        assertThat(collectIsCorrect).containsExactly(true);
+    }
+
+
 }
