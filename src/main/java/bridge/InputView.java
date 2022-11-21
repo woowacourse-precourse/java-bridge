d3625edc58ab5bf2c6e7bff6e04ed356563c2e33
kissshot1104 <kissshot.heartunderblade1104@gmail.com>
2022-11-21 17:17:42 +0900

    Refactor(RandomBridge) : 출력문 종료
    출력문이 이곳에 있어야 더 좋을거같아서 옮겼습니다.

--- src/main/java/bridge/InputView.java
+++ src/main/java/bridge/InputView.java
@@ -26,6 +26,7 @@ public class InputView {
      * 사용자가 이동할 칸을 입력받는다.
      */
     public String readMoving() {
+        System.out.println("이동할 칸을 선택해주세요. (위: U, 아래: D)");
         String input = "";
         try {
             input = Console.readLine();
