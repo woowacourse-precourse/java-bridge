21daa70ea67b7ecbacc9dc48eaa394e86bba44ea
Invidam <hansupark0208@gmail.com>
2022-11-21 10:19:02 +0900

    fix(system-message): 출력 방식 변경

--- src/main/java/bridge/message/SystemMessage.java
+++ src/main/java/bridge/message/SystemMessage.java
@@ -6,8 +6,8 @@ public enum SystemMessage {
     INPUT_MOVING("이동할 칸을 선택해주세요. (위: U, 아래: D)"),
     RETRY("게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)"),
     GAME_RESULT("최종 게임 결과"),
-    SUCCESS_RESULT("게임 성공 여부: "),
-    RETRY_COUNT("총 시도한 횟수: ");
+    SUCCESS_RESULT("게임 성공 여부"),
+    RETRY_COUNT("총 시도한 횟수");
 
 
     private final String message;
