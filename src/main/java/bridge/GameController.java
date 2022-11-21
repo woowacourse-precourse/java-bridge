18c340724955413ae2c821b30d832495342ecbdd
tdmdgh <tdmdgh@gmail.com>
2022-11-21 18:07:52 +0800

    refacotr(GameController): successorRetry 매서드 추출

--- src/main/java/bridge/GameController.java
+++ src/main/java/bridge/GameController.java
@@ -9,34 +9,44 @@ public class GameController {
     private OutputView outputView =new OutputView();
     private InputView inputView = new InputView();
     private BridgeMaker bridgeMaker = new BridgeMaker(new BridgeRandomNumberGenerator());
+    private static int trials=1;
     public void begin(){
-        int trials = 1;
+//        int trials = 1;
         outputView.gameStartMessage();
-        outputView.bridgeSizeMessage();
 
+        outputView.bridgeSizeMessage();
         int size = readBridgeSizeStep();//inputView.readBridgeSize();
 
         List<String> answer_bridge = bridgeMaker.makeBridge(size);
         System.out.println(answer_bridge);
 
         BridgeGame bridgeGame = new BridgeGame(answer_bridge);
-        while(true) {
-            move( bridgeGame);
-            if (bridgeGame.isSuccess()) {
-                outputView.printResult("성공", bridgeGame, trials);
-                return;
-            }
-            outputView.restartMessage();
-            String command = readRestartStep();
-            if (command.equals("R"))
-                bridgeGame.retry();
+        boolean keepgo = true;
+        while(keepgo) {
+            move( bridgeGame);//끝까지 성공하거나 중간에 실패하기전까지 이동
 
-            if (command.equals("Q")) {
-                outputView.printResult("실패", bridgeGame, trials);
-                return;
-            }
+            //성공하거나 Q를 입력받으면 false return
+            keepgo = successOrRetry( bridgeGame);
             trials++;
         }
+    }
+    private boolean successOrRetry(BridgeGame bridgeGame){
+        if (bridgeGame.isSuccess()) {
+            outputView.printResult("성공", bridgeGame, trials);
+            return false;
+        }
+
+        outputView.restartMessage();
+        String command = readRestartStep();
+        if (command.equals("R")) {
+            bridgeGame.retry();
+        }
+
+        if (command.equals("Q")) {
+            outputView.printResult("실패", bridgeGame, trials);
+            return false;
+        }
+        return true;
     }
     private void move(BridgeGame bridgeGame) {
         boolean isEnd = false;
--- src/main/java/bridge/Message.java
+++ src/main/java/bridge/Message.java
@@ -7,8 +7,8 @@ public enum Message {
     Bridge_Size_Input("다리의 길이를 입력해주세요."),
     Move_Input("\n이동할 칸을 선택해주세요. (위: U, 아래: D)"),
     Restart_Input("게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)"),
-    Result("최종 게임 결과"),
-    Success("게임 성공 여부: "),
+    ResultMessage("최종 게임 결과"),
+    GameResult("게임 성공 여부: "),
     Trials("총 시도한 횟수: ");
     private String message;
     Message(String message) {
