127c437e88799d5f4fdd23b137f33769f51c12fe
박원종 <wonjong1030@naver.com>
2022-11-21 22:16:18 +0900

    refactor(Application): BridgeController 생성자 내부에서 View 관련 객체를 생성하도록 수정

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -8,9 +8,7 @@ public class Application {
 
     public static void main(String[] args) {
         // TODO: 프로그램 구현
-        InputView inputView = new InputView();
-        OutputView outputView = new OutputView();
-        BridgeController bridgeController = new BridgeController(inputView, outputView);
+        BridgeController bridgeController = new BridgeController();
         try {
             bridgeController.run();
         } catch (IllegalArgumentException e) {
