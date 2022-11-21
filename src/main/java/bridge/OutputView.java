a17757fda4e1acf52e18f2c4954156169ec9f8a7
ashlovesliitea <skyrider1811@gmail.com>
2022-11-21 15:39:55 +0900

    refactor: OutputView - 현재까지 이동한 다리의 상태 출력하는 기능 분리 (printBridge,checkBridgeSpace,checkPlayerCurLocation)

--- src/main/java/bridge/OutputView.java
+++ src/main/java/bridge/OutputView.java
@@ -1,8 +1,5 @@
 package bridge;
 
-import java.sql.SQLOutput;
-import java.util.List;
-
 /**
  * 사용자에게 게임 진행 상황과 결과를 출력하는 역할을 한다.
  */
@@ -22,59 +19,39 @@ public class OutputView {
      * <p>
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void printMap(Bridge bridge,Player player,String inputSpace) {
-        List<String> spaces = bridge.getSpaces();
-        int curSpace = player.getCurrentSpace();
-
-        System.out.print(start);
-        for(int i =0; i<curSpace-1;i++){
-            //up 먼저 출력
-            if(spaces.get(i)=="U"){
-                System.out.print(" O |");
-            }
-            else{
-                System.out.print("   |");
-            }
-        }
-
-        if(inputSpace.equals("U")){
-            if(bridge.isSuccess(curSpace,inputSpace)){
-                System.out.println(" O ");
-            }
-            else{
-                System.out.print(" X ");
-            }
-        }
-        else{
-            System.out.print("   ");
-        }
-        System.out.println(end);
-        System.out.print(start);
-        for(int i =0; i<curSpace-1;i++){
-            //up 먼저 출력
-            if(spaces.get(i)=="D"){
-                System.out.print(" O |");
-            }
-            else{
-                System.out.print("   |");
-            }
-        }
-
-        if(inputSpace.equals("D")){
-            if(bridge.isSuccess(curSpace,inputSpace)){
-                System.out.println(" O ");
-            }
-            else{
-                System.out.print(" X ");
-            }
-        }
-        else{
-            System.out.print("   ");
-        }
-        System.out.println(end);
-
-
+    public void printMap(Bridge bridge,Player player,String inputStep) {
+        int curLocation = player.getCurrentLocation();
+        printBridge(bridge,"U",inputStep,curLocation);
+        printBridge(bridge,"D",inputStep,curLocation);
     }
+
+    private void printBridge(Bridge bridge,String spaceStatus,String inputStep, int curLocation) {
+        System.out.print(start);
+        for(int i = 1; i < curLocation; i++){
+            System.out.print(checkBridgeSpace(bridge, spaceStatus, i));
+        }
+        System.out.print(checkPlayerCurLocation(bridge,spaceStatus,inputStep,curLocation));
+        System.out.println(end);
+    }
+
+    private static String checkBridgeSpace(Bridge bridge, String spaceStatus, int curBridgeLocation) {
+        if(bridge.checkBridgeSpace(curBridgeLocation).equals(spaceStatus)){
+           return rightSpaceWithWall;
+        }
+        return emptySpaceWithWall;
+    }
+
+
+    private String checkPlayerCurLocation(Bridge bridge, String spaceStatus, String inputStep, int curLocation){
+        if(inputStep.equals(spaceStatus)){
+            if(bridge.isSuccess(curLocation,inputStep)){
+                return rightSpaceWithoutWall;
+            }
+            return wrongSpaceWithoutWall;
+        }
+        return emptySpaceWithoutWall;
+    }
+
 
     /**
      * 게임의 최종 결과를 정해진 형식에 맞춰 출력한다.
