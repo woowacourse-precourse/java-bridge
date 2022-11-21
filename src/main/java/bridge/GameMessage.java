f5dfc2886d85553b772dfa1f27f2887a1313970c
ChanghyunKim <variey82p@gmail.com>
2022-11-21 22:44:55 +0900

    feat(GameMessage) : 게임종료시 메시지 추가

--- src/main/java/bridge/GameMessage.java
+++ src/main/java/bridge/GameMessage.java
@@ -6,5 +6,8 @@ public class GameMessage {
     public static final String CHOICE_DIRECT_INPUT_MESSAGE = "이동할 칸을 선택해주세요. (위: U, 아래: D)";
     public static final String WHETHER_EXIT_AGAIN_GAME_MESSAGE = "게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)";
     public static final String GAME_END_MESSAGE = "최종 게임 결과\n";
+    public static final String GAME_SUCCESS_MESSAGE = "게임 성공 여부: ";
+    public static final String GAME_TRY_COUNT_MESSAGE = "총 시도한 횟수: %d";
     public static final String ERROR_MESSAGE = "[ERROR] ";
+
 }
