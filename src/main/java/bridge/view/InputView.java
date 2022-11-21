d96e8e57d66f737fed20ec370539f144d4762bad
sen <seeunchoi99@gmail.com>
2022-11-21 20:55:57 +0900

    ♻️ InputView.readBridgeSize 에서 게임 시작 메세지 출력 분리

--- src/main/java/bridge/view/InputView.java
+++ src/main/java/bridge/view/InputView.java
@@ -7,15 +7,12 @@ import java.util.NoSuchElementException;
  * 사용자로부터 입력을 받는 역할을 한다.
  */
 public class InputView {
-    private static final String START_GAME_MSG = "다리 건너기 게임을 시작합니다.\n";
     private static final String INPUT_LENGTH_OF_BRIDGE_MSG = "다리의 길이를 입력해주세요.";
     private static final String INPUT_SELECT_TO_GO_MSG = "이동할 칸을 선택해주세요. (위: U, 아래: D)";
 
     private String getInput() {
         try {
             return Console.readLine();
-        } catch (NoSuchElementException exception) {
-            throw new IllegalArgumentException();
         } catch (Exception exception) {
             throw new IllegalArgumentException();
         }
@@ -25,7 +22,6 @@ public class InputView {
      * 다리의 길이를 입력받는다.
      */
     public int readBridgeSize() {
-        System.out.println(START_GAME_MSG);
         System.out.println(INPUT_LENGTH_OF_BRIDGE_MSG);
 
         return convertStringToInteger(validateBridgeSize(getInput()));
