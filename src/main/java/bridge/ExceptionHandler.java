1cbb0b09d0f12ae5aaf614395705e8d1a54d30dc
youswim <youswim96@gmail.com>
2022-11-22 08:01:00 +0900

    feat(ExceptionHandler.java): 예외 처리에 사용되는 메세지 클래스 추가

---
+++ src/main/java/bridge/ExceptionHandler.java
@@ -0,0 +1,11 @@
+package bridge;
+
+public class ExceptionHandler {
+
+    public static final String BRIDGE_SIZE_EXCEPTION = "다리 길이는 숫자이고, 범위는 [3,20]이어야 합니다.";
+    public static final String MOVE_ALPHABET_EXCEPTION = "플레이어의 이동은 U 또는 D만 입력 가능합니다.";
+
+    public static final String GAME_COMMAND_EXCEPTION = "게임 재시도 여부는 R 또는 Q만 입력 가능합니다";
+
+
+}
