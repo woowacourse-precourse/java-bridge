a7b8ede16057a6b5a236b287c40d3325d873f40d
phdljr <phdljr@naver.com>
2022-11-22 01:29:52 +0900

    feat: view 설정

--- src/main/java/bridge/view/InputView.java
+++ src/main/java/bridge/view/InputView.java
@@ -1,4 +1,6 @@
-package bridge;
+package bridge.view;
+
+import camp.nextstep.edu.missionutils.Console;
 
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
@@ -9,20 +11,35 @@ public class InputView {
      * 다리의 길이를 입력받는다.
      */
     public int readBridgeSize() {
-        return 0;
+        int bridgeSize = Integer.parseInt(Console.readLine());
+        return bridgeSize;
     }
 
     /**
      * 사용자가 이동할 칸을 입력받는다.
      */
     public String readMoving() {
-        return null;
+        String inputMoving = Console.readLine();
+        return inputMoving;
     }
 
     /**
      * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
      */
     public String readGameCommand() {
-        return null;
+        String inputCommand = Console.readLine();
+        return inputCommand;
+    }
+
+    private void validateBridgeSize(){
+
+    }
+
+    private void validateMoving(){
+
+    }
+
+    private void validateGameCommand(){
+
     }
 }
