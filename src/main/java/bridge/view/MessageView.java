af55909e4f682204c6f2c9088f8dd33a7a832f36
Hayoon <hy97@sju.ac.kr>
2022-11-21 16:18:44 +0900

    feat(MessageView): 게임 메세지 변수 추가

--- src/main/java/bridge/view/MessageView.java
+++ src/main/java/bridge/view/MessageView.java
@@ -8,7 +8,18 @@ public enum MessageView {
     INPUT_RETRY_OR_NOT_GAME("게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)"),
     FINAL_GAME_RESULT("최종 게임 결과"),
     WHETHER_GAME_SUCCESS("게임 성공 여부: "),
-    TOTAL_ATTEMPTS("총 시도한 횟수: ");
+    TOTAL_ATTEMPTS("총 시도한 횟수: "),
+
+    RETURN_RETRY("R"),
+    RETURN_QUIT("Q"),
+    O("O"),
+    X("X"),
+    UP("U"),
+    DOWN("D"),
+    SPACE("   "),
+    INTERVAL("|"),
+    RETURN_SUCCESS("성공"),
+    RETURN_FAIL("실패");
 
     private final String message;
 
