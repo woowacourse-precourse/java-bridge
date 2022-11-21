6d2ddd52a1968fbbd51d63d3e13de34fd8609229
madlimeus <108660314+madlimeus@users.noreply.github.com>
2022-11-22 03:44:05 +0900

    add function
    add print start msg

--- src/main/java/bridge/BridgeGame.java
+++ src/main/java/bridge/BridgeGame.java
@@ -1,9 +1,22 @@
 package bridge;
 
+import java.util.List;
+
 /**
  * 다리 건너기 게임을 관리하는 클래스
  */
 public class BridgeGame {
+    public void GAME_START_MSG(){
+        System.out.println("다리 건너기 게임을 시작합니다.");
+    }
+
+    public boolean IS_MOVE_SAFE(List<String> Bridge_Correct, String Input_History) {
+        boolean result = true;
+        String LAST_STONE = Bridge_Correct.get(Input_History.length()-1);
+        String LAST_JUMP  = Input_History.substring(Input_History.length()-1);
+        if (!LAST_STONE.equals(LAST_JUMP)) { result = false; }
+        return result;
+    }
 
     /**
      * 사용자가 칸을 이동할 때 사용하는 메서드
