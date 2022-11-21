9e399e1d429ea1a1a1e5997f9be9808b6e38aa55
jmj005 <jmj00005@naver.com>
2022-11-22 03:27:59 +0900

    refactor(Application): Application 클래스 refactoring

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -1,16 +1,13 @@
 package bridge;
 
 import bridge.controller.BridgeGameController;
-import bridge.view.InputView;
 
 public class Application {
 
     public static void main(String[] args) {
-        InputView inputView = new InputView();
         BridgeGameController bridgeGameController = new BridgeGameController();
         try {
-            int size = inputView.readBridgeSize();
-            bridgeGameController.run(size);
+            bridgeGameController.run();
         } catch (IllegalArgumentException error) {
             System.out.println("[ERROR] " + error.getMessage());
         }
