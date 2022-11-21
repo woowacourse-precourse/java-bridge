41c2db9a32569438017969536f91a534207b4eb8
박시균\psk98 <psg980331@naver.com>
2022-11-21 14:41:09 +0900

    Feat: 사용자가 입력한 다리길이 입력이 유효한지 확인

--- src/main/java/bridge/service/BridgeService.java
+++ src/main/java/bridge/service/BridgeService.java
@@ -1,2 +1,32 @@
-package bridge.service;public class BridgeService {
+package bridge.service;
+
+import bridge.domain.BridgeGame;
+import bridge.domain.BridgeMaker;
+import bridge.domain.GameRetry;
+
+import java.util.*;
+public class BridgeService {
+
+    static final BridgeRandomNumberGenerator bridgeRandomNumberGenerator = new BridgeRandomNumberGenerator();
+    static final BridgeGame bridgeGame = new BridgeGame(0,0);
+    static final BridgeMaker bridgeMaker = new BridgeMaker(bridgeRandomNumberGenerator);
+    static final InputValidation inputValidation = new InputValidation();
+    static final InputView inputView = new InputView();
+    static final OutputView outputView = new OutputView();
+    
+    public boolean isValidBridgeLengthInput(String bridgeLength){
+        if(!inputValidation.isNumber(bridgeLength)){
+            outputView.printNotNumberBridgeLengthInputErrorMessage();
+            return false;
+        }
+        if(!inputValidation.isValidRange(Integer.parseInt(bridgeLength))){
+            outputView.printInValidRangeBridgeLengthErrorMessage();
+            return false;
+        }
+        return true;
+    }
+
+
+
+
 }
