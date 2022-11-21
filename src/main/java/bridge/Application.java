9855085f5e09d48dfe192bb11dd1c1f48d52b697
hhiyeon <hhyyeon0214@gmail.com>
2022-11-22 03:36:40 +0900

    feat:Application-generate a try catch statment to main method

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -1,8 +1,29 @@
 package bridge;
 
+import bridge.domain.BridgeMaker;
+import bridge.domain.bridgenumber.BridgeRandomNumberGenerator;
+import bridge.service.BridgeGameService;
+import bridge.view.InputView;
+
+import java.util.List;
+
 public class Application {
+    static InputView input = new InputView();
+    static int bridgeSize;
+    static BridgeMaker bridgeMaker;
+    static List<String> crossByBridge;
 
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        BridgeGameService bridgeGameService = new BridgeGameService();
+        bridgeGameService.startBridgeGame();
+        try {
+            bridgeSize = input.readBridgeSize(); // 다리의 길이 입력
+            bridgeMaker = new BridgeMaker(new BridgeRandomNumberGenerator());
+            crossByBridge = bridgeMaker.makeBridge(bridgeSize); // 다리 만들기
+            bridgeGameService = new BridgeGameService(crossByBridge);
+            bridgeGameService.playBridgeGame();
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+        }
     }
 }
