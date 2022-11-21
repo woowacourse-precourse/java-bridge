989e01486791950a1d912d6845d53b253334435e
eastsage <dpwk8@naver.com>
2022-11-22 02:34:13 +0900

    test: 재시도 함수 테스트 추가

--- src/test/java/bridge/model/BridgeGameTest.java
+++ src/test/java/bridge/model/BridgeGameTest.java
@@ -1,7 +1,15 @@
 package bridge.model;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
 import bridge.BridgeNumberGenerator;
+import java.util.EmptyStackException;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 class BridgeGameTest {
 
@@ -18,6 +26,13 @@ class BridgeGameTest {
     void move_RunMove_ReturnMessage(Direction direction, String message) {
         assertThat(bridgeGame.move(direction)).isEqualTo(message);
     }
+
+    @Test
+    @DisplayName("잘못된 위치에서 재시도하면 예외가 발생한다.")
+    void retry_MisuseRetryMethod_ExceptionThrown() {
+        assertThatThrownBy(() -> bridgeGame.retry()).isInstanceOf(EmptyStackException.class);
+    }
+
     private class BridgeStaticNumberGenerator implements BridgeNumberGenerator {
         int count = 0;
 
