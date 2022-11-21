c6bfe1a714f4f70a7baa18174b88ecaa2fa3bdc3
heesang <dlagmltkd12@naver.com>
2022-11-22 06:11:48 +0900

    test: BridgeValidUtilTest 테스트 이름 정리

--- src/test/java/bridge/valid/BridgeValidUtilTest.java
+++ src/test/java/bridge/valid/BridgeValidUtilTest.java
@@ -7,7 +7,7 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class BridgeValidUtilTest {
 
-    @DisplayName("다리 검증 유틸 다리 길이가 2이면 예외 발생")
+    @DisplayName("다리 길이가 3 미만이면 예외 발생")
     @Test
     void validateMinimumSize() {
         assertThatThrownBy(() -> BridgeValidUtil.validateSize(2))
@@ -15,7 +15,7 @@ class BridgeValidUtilTest {
                 .hasMessage("[ERROR] 다리의 길이는 3이상 20이하 이어야 합니다.");
     }
 
-    @DisplayName("다리 검증 유틸 다리 길이가 21이면 예외 발생")
+    @DisplayName("길이가 20 초과이면 예외 발생")
     @Test
     void validateMaximumSize() {
         assertThatThrownBy(() -> BridgeValidUtil.validateSize(21))
