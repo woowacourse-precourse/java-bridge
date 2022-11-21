60a9cf9ad4803d34f7f1dcfbd65f7db8fda9c7a2
권민정 <22alswjd@naver.com>
2022-11-22 00:50:01 +0900

    refactor: OutputView 클래스 메세지를 상수로 선언

--- src/main/java/bridge/view/OutputView.java
+++ src/main/java/bridge/view/OutputView.java
@@ -8,33 +8,42 @@ import java.util.List;
  * 사용자에게 게임 진행 상황과 결과를 출력하는 역할을 한다.
  */
 public class OutputView {
+    private static final String BRIDGE_LENGTH_INPUT_MESSAGE = "다리의 길이를 입력해주세요.";
+    private static final String FAIL = "실패";
+    private static final String FINAL_GAME_RESULT = "최종 게임 결과";
+    private static final String GAME_START_MESSAGE = "다리 건너기 게임을 시작합니다.\n";
+    private static final String GAME_SUCCESS_OR_FAIL = "게임 성공 여부: ";
+    private static final String MOVING_CHOICE_MESSAGE = "이동할 칸을 선택해주세요. (위: U, 아래: D)";
+    private static final String RETRY_MESSAGE = "게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)";
+    private static final String SUCCESS = "성공";
+    private static final String TOTAL_TRY = "총 시도한 횟수: ";
 
     /**
      * 게임 시작을 알리는 메세지를 출력한다.
      */
     public void printGameStartMessage() {
-        System.out.println("다리 건너기 게임을 시작합니다.\n");
+        System.out.println(GAME_START_MESSAGE);
     }
 
     /**
      * 다리 길이 입력을 요청하는 메세지를 출력한다.
      */
     public void printBridgeLengthInputMessage() {
-        System.out.println("다리의 길이를 입력해주세요.");
+        System.out.println(BRIDGE_LENGTH_INPUT_MESSAGE);
     }
 
     /**
      * 이동할 칸을 입력하라는 메세지를 출력한다.
      */
     public void printMoveInputMessage() {
-        System.out.println("이동할 칸을 선택해주세요. (위: U, 아래: D)");
+        System.out.println(MOVING_CHOICE_MESSAGE);
     }
 
     /**
      * 게임 재진행 여부를 확인하는 메세지를 출력한다.
      */
     public void printRetryMessage() {
-        System.out.println("게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)");
+        System.out.println(RETRY_MESSAGE);
     }
 
     /**
@@ -68,18 +77,18 @@ public class OutputView {
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
     public void printResult(List<List<String>> map, boolean success, int tryCount) {
-        System.out.println("최종 게임 결과");
+        System.out.println(FINAL_GAME_RESULT);
         printMap(map);
         printSuccess(success);
-        System.out.println("총 시도한 횟수: " + tryCount);
+        System.out.println(TOTAL_TRY + tryCount);
     }
 
     private void printSuccess(boolean success) {
-        System.out.print("게임 성공 여부: ");
+        System.out.print(GAME_SUCCESS_OR_FAIL);
         if (success) {
-            System.out.println("성공");
+            System.out.println(SUCCESS);
             return;
         }
-        System.out.println("실패");
+        System.out.println(FAIL);
     }
 }
