0ec099d689fa43d53f9ae1611b2477cc868c7146
song-wooseok <sws981202@gmail.com>
2022-11-21 14:44:16 +0900

    feat: 사용자의 플레이 정보(시도 회수, )를 초기화하는 기능

---
+++ src/main/java/bridge/domain/BridgePlayer.java
@@ -0,0 +1,10 @@
+package bridge.domain;
+
+import bridge.utils.constants.BridgeMove;
+
+public class BridgePlayer {
+    private int currentBridgePosition = BridgeMove.START_INDEX;
+    private int gameAttemptCount = 0;
+
+
+}
---
+++ src/main/java/bridge/utils/constants/BridgeMove.java
@@ -0,0 +1,7 @@
+package bridge.utils.constants;
+
+public class BridgeMove {
+    public static final int START_INDEX = -1;
+    public static final int GO_STEP = 1;
+    public static final int BACK_STEP = -1;
+}
