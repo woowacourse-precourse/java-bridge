fd5097ae3aad35a5e9ee3224834a02d7fed93d60
SproutMJ <mjk54565@gmail.com>
2022-11-22 01:15:12 +0900

    feat: 다음 위치를 입력받는 기능 구현 test: U, D 이외의 값을 입력받으면 예외를 던지는지 확인하는 테스트 추가 docs: 메서드에 javadoc 추가

--- src/main/java/bridge/InputView.java
+++ src/main/java/bridge/InputView.java
@@ -7,7 +7,10 @@ import camp.nextstep.edu.missionutils.Console;
  */
 public class InputView {
 
-    /**
+     /**
+     * @return 입력받은 다리 사이즈. 숫자로 입력받는다.
+     * @Exception IllegalArgumentException 숫자가 아니거나 3-20이내의 숫자가 아니면 예외가 발생한다.
+     *
      * 다리의 길이를 입력받는다.
      */
     public int readBridgeSize() {
@@ -24,10 +27,15 @@ public class InputView {
     }
 
     /**
+     * @return 입력받은 이동 위치. 위 칸이면 "U", 아래 칸이면 "D"로 표현해야 한다.
+     * @Exception IllegalArgumentException 현재 D와 U 이외의 값은 지원하지 않는다.
+     *
      * 사용자가 이동할 칸을 입력받는다.
      */
     public String readMoving() {
-        return null;
+        String input = Console.readLine();
+        if (input.equals("U") || input.equals("D")) return input;
+        throw new IllegalArgumentException("이동 위치는 U 또는 D 이어야합니다.");
     }
 
     /**
--- src/test/java/bridge/InputViewTest.java
+++ src/test/java/bridge/InputViewTest.java
@@ -44,4 +44,32 @@ class InputViewTest {
         System.setIn(inputStream);
         assertDoesNotThrow(() -> inputView.readBridgeSize());
     }
+
+    @DisplayName("U 또는 D 이외의 값을 입력하는 경우 예외 발생")
+    @Test
+    void 잘못된_이동_위치_입력(){
+        String input = "A";
+        InputStream inputStream = new ByteArrayInputStream(input.getBytes());
+        System.setIn(inputStream);
+        assertThatThrownBy(() -> inputView.readMoving())
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("정상 이동 위치 U를 입력하는 경우 정상 통과")
+    @Test
+    void 정상_이동_위치_U_입력(){
+        final String input = "U";
+        InputStream inputStream = new ByteArrayInputStream(input.getBytes());
+        System.setIn(inputStream);
+        assertThat(inputView.readMoving()).isEqualTo(input);
+    }
+
+    @DisplayName("정상 이동 위치 D를 입력하는 경우 정상 통과")
+    @Test
+    void 정상_이동_위치_D_입력(){
+        final String input = "D";
+        InputStream inputStream = new ByteArrayInputStream(input.getBytes());
+        System.setIn(inputStream);
+        assertThat(inputView.readMoving()).isEqualTo(input);
+    }
 }
