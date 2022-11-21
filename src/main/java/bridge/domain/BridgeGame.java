9f5b4c0ee42b037391a84d9cf592636226eb4057
ryanha <ryanha@handong.ac.kr>
2022-11-21 22:44:46 +0900

    refactor(domain): BridgeGame, 각 메서드별 접근 제어자, 주석 수정

--- src/main/java/bridge/domain/BridgeGame.java
+++ src/main/java/bridge/domain/BridgeGame.java
@@ -7,9 +7,9 @@ import java.util.List;
  * 다리 건너기 게임을 관리하는 클래스
  */
 public class BridgeGame {
-    private static BridgeGame instance;
-    private List<String> bridgeStatus; // 다리의 상태 저장
-    private final List<String> movingStatus; // 사용자 이동상태 저장
+    private static BridgeGame instance; // BridgeGame 클래스의 인스턴스
+    private List<String> bridgeStatus; // 다리의 상태
+    private final List<String> movingStatus; // 사용자 이동상태
     private int attemptsCount; // 시도한 횟수
 
     private BridgeGame() { // for singleton pattern
@@ -18,7 +18,7 @@ public class BridgeGame {
     }
 
     public static BridgeGame getInstance() {
-        if(instance == null) {
+        if (instance == null) {
             instance = new BridgeGame();
         }
         return instance;
@@ -34,6 +34,16 @@ public class BridgeGame {
         return isMovable(command, bridgeStatus);
     }
 
+    /**
+     * 사용자가 게임을 다시 시도할 때 사용하는 메서드
+     * <p>
+     * 재시작을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
+     */
+    public void retry() {
+        attemptsCount++;
+        movingStatus.clear();
+    }
+
     public void setBridgeStatus(List<String> bridgeStatus) {
         this.bridgeStatus = bridgeStatus;
     }
@@ -47,35 +57,25 @@ public class BridgeGame {
     }
 
     /*
-     * 현재의 이동 명령이 수행 가능한 명령인 지 확인하는 기능
-     * 이동이 가능한 경우 true, 불가한 경우 false를 반환함
-     */
-    public boolean isMovable(String command, List<String> bridge) {
-        return command.equals(bridge.get(movingStatus.size()-1));
-    }
-
-    /*
      * 게임을 성공했는 지에 대한 여부를 확인함
      */
     public boolean isGameSuccess() {
         if (bridgeStatus.size() != movingStatus.size()) {
             return false;
         }
-        for(int i=0 ; i<bridgeStatus.size() ; i++) {
-            if(!bridgeStatus.get(i).equals(movingStatus.get(i))) {
+        for (int i = 0; i < bridgeStatus.size(); i++) {
+            if (!bridgeStatus.get(i).equals(movingStatus.get(i))) {
                 return false;
             }
         }
         return true;
     }
 
-    /**
-     * 사용자가 게임을 다시 시도할 때 사용하는 메서드
-     * <p>
-     * 재시작을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
+    /*
+     * 현재의 이동 명령이 수행 가능한 명령인 지 확인하는 기능
+     * 이동이 가능한 경우 true, 불가한 경우 false를 반환함
      */
-    public void retry() {
-        attemptsCount++;
-        movingStatus.clear();
+    private boolean isMovable(String command, List<String> bridge) {
+        return command.equals(bridge.get(movingStatus.size() - 1));
     }
 }
