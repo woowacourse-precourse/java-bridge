aa0ecce49ca3d051a12a209672cd3f0297c72854
baksohyeon <bori12370@gmail.com>
2022-11-22 00:04:51 +0900

    feat: 명령어 상수 관리하는 클래스 추가

---
+++ src/main/java/bridge/domain/CommandConstant.java
@@ -0,0 +1,11 @@
+package bridge.domain;
+
+public class CommandConstant {
+    private CommandConstant() {
+    }
+
+    public static final String UP = "U";
+    public static final String DOWN = "D";
+    public static final String REDO = "R";
+    public static final String QUIT = "Q";
+}
