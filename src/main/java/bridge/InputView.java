6a03a760ca719ab1ffc5886b361a6483ac3937d9
KJE <12181501@inha.edu>
2022-11-21 15:08:41 +0900

    feat: Input-readBridge 함수 추가

--- src/main/java/bridge/InputView.java
+++ src/main/java/bridge/InputView.java
@@ -1,4 +1,5 @@
 package bridge;
+import camp.nextstep.edu.missionutils.Console;
 
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
@@ -8,21 +9,28 @@ public class InputView {
     /**
      * 다리의 길이를 입력받는다.
      */
-    public int readBridgeSize() {
-        return 0;
+    public static int readBridgeSize() {
+        String bridgeSizeStr = Console.readLine();
+        int bridgeSize = 0;
+        try{
+            bridgeSize = Integer.parseInt(bridgeSizeStr);
+        }catch(NumberFormatException ex){
+            Application.errorHandling("다리의 길이를 숫자로 입력해주세요.");
+        }
+        return bridgeSize;
     }
 
     /**
      * 사용자가 이동할 칸을 입력받는다.
      */
-    public String readMoving() {
+    public static String readMoving() {
         return null;
     }
 
     /**
      * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
      */
-    public String readGameCommand() {
+    public static String readGameCommand() {
         return null;
     }
 }
