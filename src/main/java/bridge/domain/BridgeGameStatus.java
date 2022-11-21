f4b8bde546df5c9d5e252eba4374d0deff53d176
hyun0404woo <hyun0404woo@gmail.com>
2022-11-21 16:23:06 +0900

    feat: 게임 상태 객체를 나타내는 toString 메서드 오버라이딩
    - 게임의 현재 상태를 출력하면 적절한 포맷으로 보이게끔 toString() 메서드를 오버라이딩

--- src/main/java/bridge/domain/BridgeGameStatus.java
+++ src/main/java/bridge/domain/BridgeGameStatus.java
@@ -2,9 +2,17 @@ package bridge.domain;
 
 import java.util.List;
 import java.util.Objects;
+import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 public class BridgeGameStatus {
+
+    private final static String MOVE_MARKER = "O";
+    private final static String WRONG_MOVE_MARKER = "X";
+    private final static String NO_MOVE_MARKER = " ";
+    private final static String MARKER_DELIMETER = " | ";
+    private final static String OPEN_LINE = "[ ";
+    private final static String CLOSE_LINE = " ]";
     
     private final List<String> playerMove;
     private final List<String> gameBridge;
@@ -27,5 +35,26 @@ public class BridgeGameStatus {
         return IntStream.range(0, playerMove.size())
                 .filter(idx -> Objects.equals(playerMove.get(idx), gameBridge.get(idx)))
                 .count() == gameBridge.size();
+    }
+
+    @Override
+    public String toString() {
+        final String upLine = createStatusLine(Moving.UP);
+        final String downLine = createStatusLine(Moving.DOWN);
+        return upLine + "\n" + downLine;
+    }
+
+    private String createStatusLine(Moving moving) {
+        return OPEN_LINE + IntStream.range(0, playerMove.size())
+                .mapToObj(idx -> directionToMarker(playerMove.get(idx), gameBridge.get(idx), moving))
+                .collect(Collectors.joining(MARKER_DELIMETER)) + CLOSE_LINE;
+    }
+
+    private String directionToMarker(String player, String bridge, Moving moving) {
+        if (Objects.equals(player, moving.getDirection())) {
+            if (!Objects.equals(player, bridge)) return WRONG_MOVE_MARKER;
+            return MOVE_MARKER;
+        }
+        return NO_MOVE_MARKER;
     }
 }
