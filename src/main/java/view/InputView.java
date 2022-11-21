db212898d4ae89f0c4cdd86ff8a3de9fa16d841a
Egenieee <hhgg0925@naver.com>
2022-11-21 22:23:31 +0900

    style: 주석 삭제

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -3,17 +3,11 @@ package view;
 import camp.nextstep.edu.missionutils.Console;
 import utils.Validator;
 
-/**
- * 사용자로부터 입력을 받는 역할을 한다.
- */
 public class InputView {
     private static final String BRIDGE_SIZE_INPUT_MESSAGE = "다리의 길이를 입력해주세요.";
     private static final String MOVING_PLACE_INPUT_MESSAGE = "이동할 칸을 선택해주세요. (위: U, 아래: D)";
     private static final String GAME_COMMAND_INPUT_MESSAGE = "게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)";
 
-    /**
-     * 다리의 길이를 입력받는다.
-     */
     public int readBridgeSize() {
         String bridgeSize;
 
@@ -25,9 +19,6 @@ public class InputView {
         return Integer.parseInt(bridgeSize);
     }
 
-    /**
-     * 사용자가 이동할 칸을 입력받는다.
-     */
     public String readMoving() {
         String movingPlace;
 
@@ -39,9 +30,6 @@ public class InputView {
         return movingPlace;
     }
 
-    /**
-     * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
-     */
     public String readGameCommand() {
         String gameCommand;
 
--- src/main/java/view/OutputView.java
+++ src/main/java/view/OutputView.java
@@ -2,9 +2,6 @@ package view;
 
 import bridge.BridgeGame;
 
-/**
- * 사용자에게 게임 진행 상황과 결과를 출력하는 역할을 한다.
- */
 public class OutputView {
     private static final String GAME_START_COMMENT = "다리 건너기 게임을 시작합니다.\n";
     private static final String FINAL_GAME_RESULT_COMMENT = "최종 게임 결과\n";
@@ -15,21 +12,11 @@ public class OutputView {
         System.out.println(GAME_START_COMMENT);
     }
 
-    /**
-     * 현재까지 이동한 다리의 상태를 정해진 형식에 맞춰 출력한다.
-     * <p>
-     * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
-     */
     public void printMap(String currentState) {
         System.out.println(currentState);
         System.out.println();
     }
 
-    /**
-     * 게임의 최종 결과를 정해진 형식에 맞춰 출력한다.
-     * <p>
-     * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
-     */
     public void printResult(BridgeGame bridgeGame, String finalState) {
         System.out.print(FINAL_GAME_RESULT_COMMENT);
         System.out.println(finalState);
