50f3d04aa70a90f74dc556dcb1d458dbeaa9a61e
오승윤 <>
2022-11-21 12:07:25 +0900

    feat (MovementStatusForm) : 다리 이동 상태를 의미하는 Enum

---
+++ src/main/java/bridge/MovementStatusForm.java
@@ -0,0 +1,29 @@
+package bridge;
+
+public enum MovementStatusForm {
+    UP_O(true),
+    UP_X(false),
+    DOWN_O(true),
+    DOWM_X(false);
+
+    boolean isPass;
+
+    MovementStatusForm(boolean isPass) {
+        this.isPass = isPass;
+    }
+
+    public static MovementStatusForm createPassCaseByCode(String movement_command){
+        if(Site.UP.isSameCommand(movement_command)){
+            return UP_O;
+        }
+        return DOWN_O;
+    }
+
+    public static MovementStatusForm createWrongCaseByCode(String movement_command){
+        if(Site.UP.isSameCommand(movement_command)){
+            return UP_X;
+        }
+        return DOWM_X;
+    }
+
+}
