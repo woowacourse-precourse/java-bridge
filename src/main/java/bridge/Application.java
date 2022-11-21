e59567c6581e9fe90bd86f8efad16d6817fb9a53
NAM GIBEOM <rlqja0523@naver.com>
2022-11-22 03:34:48 +0900

    feat: 생성자 주입

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -1,8 +1,25 @@
 package bridge;
 
+import bridge.model.Result;
+import bridge.service.BridgeGame;
+import bridge.view.InputView;
+import bridge.view.OutputView;
+
 public class Application {
+    private static BridgeMaker bridgeMaker;
+    private static InputView inputView;
+    private static OutputView outputView;
+    private static BridgeGame bridgeGame;
+    private static Result result;
 
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+    }
+
+    private static void initialize() {
+        bridgeMaker = new BridgeMaker(new BridgeRandomNumberGenerator());
+        inputView = new InputView();
+        outputView = new OutputView();
+        result = new Result();
     }
 }
