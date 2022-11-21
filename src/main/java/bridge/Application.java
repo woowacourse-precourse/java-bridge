7d4a5744a4111c3586be0615a3cbcaf2157c70b8
Jinseo Song <82093280+rxb8k@users.noreply.github.com>
2022-11-21 22:28:01 +0900

    feat: 칸 이동에 실패했을 경우 해당 게임 종료 기능 추가

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -10,14 +10,24 @@ public class Application {
     public static void main(String[] args) {
         InputView input = new InputView();
 
+        System.out.println("다리의 길이를 입력해주세요.");
         int size = input.readBridgeSize();
 
         Bridge bridge = new Bridge(size);
         BridgeGame game = new BridgeGame(bridge);
 
         while (game.currentStatus == gameStatus.CONTINUE) {
+            System.out.println("이동할 칸을 선택해주세요. (위: U, 아래: D)");
             String direction = input.readMoving();
             game.move(direction);
+
+            if (game.currentStatus == gameStatus.FAIL) {
+                System.out.println("게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)");
+                String gameCommand = input.readGameCommand();
+                if (gameCommand.equals("R")) {
+                    game.retry();
+                }
+            }
         }
     }
 }
--- src/main/java/bridge/domain/BridgeGame.java
+++ src/main/java/bridge/domain/BridgeGame.java
@@ -3,6 +3,7 @@ package bridge.domain;
 public class BridgeGame {
     public Bridge bridge;
     public gameStatus currentStatus;
+    public int attemptCount = 0;
 
     public BridgeGame(Bridge bridge) {
         this.bridge = bridge;
@@ -11,14 +12,11 @@ public class BridgeGame {
 
     public void move(String direction) {
         gameStatus moveResult = bridge.updateUserRoute(direction);
+        System.out.println(moveResult);
         this.currentStatus = moveResult;
     }
 
-    /**
-     * 사용자가 게임을 다시 시도할 때 사용하는 메서드
-     * <p>
-     * 재시작을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
-     */
     public void retry() {
+        attemptCount++;
     }
 }
