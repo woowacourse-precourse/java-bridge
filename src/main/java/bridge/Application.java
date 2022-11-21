55bcaa82b7ceb848ec6d7879d61a95231e58efd4
Hoo93 <117077999+Hoo93@users.noreply.github.com>
2022-11-21 13:18:09 +0900

    FIX[Application]: 실행 부분 코드 수정

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -1,5 +1,6 @@
 package bridge;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Application {
@@ -13,25 +14,45 @@ public class Application {
         List<String> bridge = bridgeMaker.makeBridge(inputView.readBridgeSize());
 
         int gameCount = 1;
-        boolean game =true;
+        boolean restart = true;
+        boolean game = true;
 
-        while (game) {
-            BridgeGame bridgeGame = new BridgeGame(bridge,inputView, outputView);
-            if (bridgeGame.move()) {
-                outputView.printEndMessage();
-                outputView.printMap(bridgeGame.upperLine,bridgeGame.lowerLine);
-                outputView.printResult(true, gameCount);
+        List<String> upperLine = new ArrayList<>();
+        List<String> lowerLine= new ArrayList<>();
+        System.out.println(bridge);
+
+        while (restart){
+
+            game = true;
+            int index = 0;
+            BridgeGame bridgeGame = new BridgeGame(bridge);
+            upperLine = bridgeGame.upperLine;
+            lowerLine = bridgeGame.lowerLine;
+
+            while (game && bridgeGame.lowerLine.size() != bridge.size()){
+
+                game = bridgeGame.move(index, inputView.readMoving());
+                index++;
+                upperLine = bridgeGame.upperLine;
+                lowerLine = bridgeGame.lowerLine;
+
+                System.out.println(index);
+                System.out.println(bridgeGame.upperLine);
+                System.out.println(bridgeGame.lowerLine);
+            }
+
+            if (game){
+                restart = false;
                 break;
             }
-            game = bridgeGame.retry(inputView.readGameCommand());
-            if (!game) {
-                outputView.printEndMessage();
-                outputView.printMap(bridgeGame.upperLine,bridgeGame.lowerLine);
-                outputView.printResult(false, gameCount);
-                break;
-            }
+            restart = bridgeGame.retry(inputView.readGameCommand());
             gameCount++;
+
         }
+
+        outputView.printEndMessage();
+        outputView.printMap(upperLine,lowerLine);
+        outputView.printResult(game,gameCount);
 
     }
 }
