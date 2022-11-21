3d7f3725c1f05cd0946f957c98900fca80093fb9
jinyeong-afk <xoeh7002@naver.com>
2022-11-22 01:15:04 +0900

    feat(InputBridgeGameValidator): 다리 이동 입력, 게임 재시작 및 종료 여부 입력 예외 처리 기능 구현 다리 이동 입력 예외 처리 1. U or D가 아닐 경우 2. 빈 문자열을 입력했을 경우 게임 재시작 및 종료 입력 예외 처리 1. R or Q가 아닐 경우 2. 빈 문자열을 입력했을 경우

--- src/main/java/bridge/InputBridgeGameValidator.java
+++ src/main/java/bridge/InputBridgeGameValidator.java
@@ -6,6 +6,8 @@ public class InputBridgeGameValidator {
     private static final int MAX_BRIDGE_SIZE = 20;
     private static final String IS_DIGIT_ERROR = "[ERROR] 숫자를 입력하여야 합니다.";
     private static final String BRIDGE_SIZE_RANGE_ERROR = "[ERROR] 3 이상 20 이하의 숫자를 입력하여야 합니다.";
+    private static final String MOVE_COMMAND_ERROR = "[ERROR] U와 R 중 하나의 문자를 입력하여야 합니다.";
+    private static final String GAME_COMMAND_ERROR = "[ERROR] R과 Q 중 하나의 문자를 입력하여야 합니다.";
 
     public void isDigit(String bridgeSize) {
         if (bridgeSize.equals("")) {
@@ -30,5 +32,17 @@ public class InputBridgeGameValidator {
     public void validateBridgeSize(String bridgeSize) {
         isDigit(bridgeSize);
         checkBridgeSizeRange(bridgeSize);
+    }
+
+    public void isMoveCommand(String moveCommand) {
+        if (!(moveCommand.equals("U") || moveCommand.equals("R"))) {
+            throw new IllegalArgumentException(MOVE_COMMAND_ERROR);
+        }
+    }
+
+    public void isGameCommand(String gameCommand) {
+        if (!(gameCommand.equals("R") || gameCommand.equals("Q"))) {
+            throw new IllegalArgumentException(GAME_COMMAND_ERROR);
+        }
     }
 }
