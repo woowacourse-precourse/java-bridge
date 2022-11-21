009634377e25bd2c8acb454e3737deb1e9a4f4c4
yoonjuho <dbswn990@naver.com>
2022-11-22 03:16:09 +0900

    feat(BridgeGame): 게임 진행 버퍼 문자열 생성

---
+++ src/main/java/bridge/model/BridgeBuffer.java
@@ -0,0 +1,10 @@
+package bridge.model;
+
+public class BridgeBuffer {
+    public static final String START = "[";
+    public static final String CONTOUR = "|";
+    public static final String END = "]";
+    public static final String RIGHT = " O ";
+    public static final String WRONG = " X ";
+    public static final String BLANK = "   ";
+}
