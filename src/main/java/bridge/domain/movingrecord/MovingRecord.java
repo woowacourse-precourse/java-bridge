341a43f3ed0fbb494d5c702179f2262884a01c59
chanminmaxscaler <jcm04@naver.com>
2022-11-22 01:46:02 +0900

    refactor: MovingRecord 클래스를 리팩토링한다
    변경 전 :
    MovingRecord는 write, read, size 등 메소드를 가졌다.
    
    변경 후 :
    기존에 MovingRecord 클래스의 메소드를 모두 삭제한다.
    MovingRecord 클래스를 Map<Judgement, Direction> records 를 유일한 멤버변수로 갖는 일급 컬렉션으로 수정한다.
    MovingRecord 클래스는 다리 그래픽을 만들기 위한 스케치를 만드는 기능을 수행하도록 한다.
    makeBridgeSketches 메소드로 해당 기능을 구현한다.
    makeBridgeSketches 메소드는 다리의 방향이 입력되었을 때 해당하는 다리의 판정과 방향을 1, 0, -1로 구분한 리스트를 반환한다.
    
    변경 이유 :
    MovingRecord 클래스와 Pedestrian 클래스의 관심사를 분리하려고 하기 때문이다.

--- src/main/java/bridge/domain/movingrecord/MovingRecord.java
+++ src/main/java/bridge/domain/movingrecord/MovingRecord.java
@@ -3,43 +3,43 @@ package bridge.domain.movingrecord;
 import bridge.domain.direction.Direction;
 import bridge.domain.referee.Judgement;
 
-import java.util.LinkedHashMap;
+import java.util.List;
 import java.util.Map;
-import java.util.stream.Stream;
+import java.util.stream.Collectors;
 
 public class MovingRecord {
 
-    private final Map<Judgement, Direction> record;
+    public static final int CORRECT_DIRECTION = 1;
+    public static final int INCORRECT_DIRECTION = 0;
+    public static final int NOTHING = -1;
+    private final Map<Judgement, Direction> records;
 
-    public MovingRecord() {
-        this.record = new LinkedHashMap<>();
+    public MovingRecord(Map<Judgement, Direction> records) {
+        this.records = records;
     }
 
-    public void write(Judgement judgement, Direction direction) {
-        record.put(judgement, direction);
+    public List<Integer> makeBridgeSketches(Direction bridgeDirection) {
+        return records.entrySet().stream()
+                .map(record -> makeSketch(record, bridgeDirection))
+                .collect(Collectors.toUnmodifiableList());
     }
 
-    public Stream<Map.Entry<Judgement, Direction>> read() {
-        return record.entrySet().stream();
-    }
-
-    public int size() {
-        return record.size();
-    }
-
-    public void clear() {
-        record.clear();
-    }
-
-    public boolean hasFalseJudgement() {
-        return record.keySet().stream()
-                .anyMatch(Judgement::isFalse);
+    private int makeSketch(Map.Entry<Judgement, Direction> record, Direction bridgeDirection) {
+        Direction direction = record.getValue();
+        Judgement judgement = record.getKey();
+        if (direction.equals(bridgeDirection) && judgement.isTrue()) {
+            return CORRECT_DIRECTION;
+        }
+        if (direction.equals(bridgeDirection) && judgement.isFalse()) {
+            return INCORRECT_DIRECTION;
+        }
+        return NOTHING;
     }
 
     @Override
     public String toString() {
         return "MovingRecord{" +
-                "record=" + record +
+                "records=" + records +
                 '}';
     }
 }
