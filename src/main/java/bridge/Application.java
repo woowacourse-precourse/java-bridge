ab50b394184e596b07ccc9f6588c9d47945cf620
DaeyeonKim97 <kdysy1130@khu.ac.kr>
2022-11-21 12:50:01 +0900

    feat(application): implement main logic
    logic
    - start game
    - move
    - retry
    - end game

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -6,6 +6,24 @@ public class Application {
     private static BridgeGame bridgeGame = new BridgeGame();
 
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        System.out.println("다리 건너기 게임을 시작합니다.\n");
+        bridgeGame.start(inputView.readBridgeSize());
+        while (!bridgeGame.checkSuccess()) {
+            outputView.printMap(bridgeGame.move(inputView.readMoving()));
+            if (!checkRetry()) {
+                return;
+            }
+        }
+        outputView.printResult(bridgeGame.generateResultMap(), true, bridgeGame.getAttempts());
+    }
+
+    private static boolean checkRetry() {
+        if (bridgeGame.checkFail()) {
+            if (!bridgeGame.retry(inputView.readGameCommand())){
+                outputView.printResult(bridgeGame.generateResultMap(), false, bridgeGame.getAttempts());
+                return false;
+            }
+        }
+        return true;
     }
 }
