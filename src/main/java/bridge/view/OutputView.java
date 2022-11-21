e8c46efab51f41d0bfdabb7500ebb79c3e79e4b6
hhiyeon <hhyyeon0214@gmail.com>
2022-11-22 00:54:47 +0900

    feat:OutputView-initialization stringbuilder for print map

--- src/main/java/bridge/view/OutputView.java
+++ src/main/java/bridge/view/OutputView.java
@@ -2,18 +2,12 @@ package bridge.view;
 
 import bridge.domain.Bridge;
 
-/**
- * 사용자에게 게임 진행 상황과 결과를 출력하는 역할을 한다.
- */
 public class OutputView {
-    /**
-     * 현재까지 이동한 다리의 상태를 정해진 형식에 맞춰 출력한다.
-     * <p>
-     * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
-     */
+    static StringBuilder upStringBuilder;
+    static StringBuilder downStringBuilder;
+
     public void printMap(Bridge bridge) {
-        StringBuilder upStringBuilder = new StringBuilder();
-        StringBuilder downStringBuilder = new StringBuilder();
+        initStringBuilder();
         int crossBridgeSize = bridge.getUpBridge().size();
 
         upStringBuilder.append("[");
@@ -37,16 +31,19 @@ public class OutputView {
         System.out.println(upStringBuilder + downStringBuilder.toString());
     }
 
-    /**
-     * 게임의 최종 결과를 정해진 형식에 맞춰 출력한다.
-     * <p>
-     * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
-     */
+    private void initStringBuilder() {
+        upStringBuilder = new StringBuilder();
+        downStringBuilder = new StringBuilder();
+
+        upStringBuilder.append("[");
+        downStringBuilder.append("[");
+    }
+
     public void printResult(String gameResult, int gameTryCount, String lastMapResult) {
-        printGameFinalBridgeResult(); // 최종 게임 결과
-        System.out.println(lastMapResult); // 게임 정보 출력
-        printGameSuccessOrFailure(gameResult); // 게임 성공 여부
-        printGameTotalTryCount(gameTryCount); // 총 시도한 횟수
+        printGameFinalBridgeResult();
+        System.out.println(lastMapResult);
+        printGameSuccessOrFailure(gameResult);
+        printGameTotalTryCount(gameTryCount);
     }
 
     public void printGameFinalBridgeResult() {
