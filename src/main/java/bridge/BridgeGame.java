574ad0fa77743a8f1be82fd7d9822ce05bb564b3
Haechan Lee <dlgockssla@gmail.com>
2022-11-21 14:34:41 +0900

    fix: add round to history for each move

--- src/main/java/bridge/BridgeGame.java
+++ src/main/java/bridge/BridgeGame.java
@@ -30,7 +30,8 @@ public class BridgeGame {
      * 이동을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
     public void move(Direction direction) {
-        history.add(direction);
+        int nextIdx = history.size();
+        history.add(new Round(direction, direction.equals(bridge.get(nextIdx))));
     }
 
     /**
