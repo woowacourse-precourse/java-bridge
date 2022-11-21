259c4a0948eeac70702dce14ffffbfd7255f55c1
sen <seeunchoi99@gmail.com>
2022-11-21 19:15:02 +0900

    ✨ BridgeMaker.makeBridge 구현

--- src/main/java/bridge/BridgeMaker.java
+++ src/main/java/bridge/BridgeMaker.java
@@ -1,5 +1,6 @@
 package bridge;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -18,6 +19,19 @@ public class BridgeMaker {
      * @return 입력받은 길이에 해당하는 다리 모양. 위 칸이면 "U", 아래 칸이면 "D"로 표현해야 한다.
      */
     public List<String> makeBridge(int size) {
-        return null;
+        List<String> bridge = new ArrayList<>();
+        for (int i = 0;i < size;i++) {
+            bridge.add(directionToString(this.bridgeNumberGenerator.generate()));
+        }
+        return bridge;
+    }
+
+    private String directionToString(int direction) {
+        if (direction == 0) {
+            return "D";
+        } if (direction == 1) {
+            return "U";
+        }
+        throw new IllegalStateException("[ERROR] 다리를 만드는 중 오류가 발생했습니다");
     }
 }
