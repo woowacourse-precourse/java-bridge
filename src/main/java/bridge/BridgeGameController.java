1a24c5c2de940cfe46984997baeb8291a041a068
JJ503 <63184334+JJ503@users.noreply.github.com>
2022-11-21 17:29:10 +0900

    feat(BridgeMaker): 다리 생성 기능 추가
    docs: 다리 생성 관련 기능 완료 체크

--- src/main/java/bridge/BridgeGameController.java
+++ src/main/java/bridge/BridgeGameController.java
@@ -1,9 +1,18 @@
 package bridge;
 
+import java.util.List;
+
 public class BridgeGameController {
     OutputView outputView = new OutputView();
+    InputView inputView = new InputView();
+    BridgeMaker bridgeMaker = new BridgeMaker(new BridgeRandomNumberGenerator());
 
     public void gameStart() {
         outputView.printStartMessage();
+        System.out.println(makeBridge());
+    }
+
+    private List<String> makeBridge() {
+        return bridgeMaker.makeBridge(inputView.readBridgeSize());
     }
 }
--- src/main/java/bridge/BridgeMaker.java
+++ src/main/java/bridge/BridgeMaker.java
@@ -1,11 +1,15 @@
 package bridge;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
  * 다리의 길이를 입력 받아서 다리를 생성해주는 역할을 한다.
  */
 public class BridgeMaker {
+    private final int ANSWER_UP_BRIDGE_INT = 1;
+    private final String ANSWER_UP_BRIDGE_STRING = "U";
+    private final String ANSWER_DOWN_BRIDGE_STRING = "D";
 
     private final BridgeNumberGenerator bridgeNumberGenerator;
 
@@ -18,6 +22,22 @@ public class BridgeMaker {
      * @return 입력받은 길이에 해당하는 다리 모양. 위 칸이면 "U", 아래 칸이면 "D"로 표현해야 한다.
      */
     public List<String> makeBridge(int size) {
-        return null;
+        List<String> bridgeAnswers = new ArrayList<>();
+
+        for (int i = 0; i < size; i++) {
+            bridgeAnswers.add(generateAnswer());
+        }
+
+        return bridgeAnswers;
+    }
+
+    private String generateAnswer() {
+        int answer = bridgeNumberGenerator.generate();
+
+        if (answer == ANSWER_UP_BRIDGE_INT) {
+            return ANSWER_UP_BRIDGE_STRING;
+        }
+
+        return ANSWER_DOWN_BRIDGE_STRING;
     }
 }
