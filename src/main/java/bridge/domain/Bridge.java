e37b2e3a8f6ce79d2c79b3c9d752b9a14c80c944
이건회 <rg970604@naver.com>
2022-11-22 03:32:09 +0900

    feat(Bridge.java) : 목적지 이동 시 안전여부 검사

--- src/main/java/bridge/domain/Bridge.java
+++ src/main/java/bridge/domain/Bridge.java
@@ -39,4 +39,47 @@ public class Bridge {
     public boolean isSafe(String moveTo) {
         return baseBridge.get(bridgePosition).equals(moveTo);
     }
+
+    public void addSafe(String moveTo) {
+        Validator.validateMovingInput(moveTo);
+        if (moveTo.equals(Constant.UP)) {
+            upperBridge.add(Constant.CORRECTSPOT);
+            lowerBridge.add(Constant.NOSPOT);
+            return;
+        }
+        if (moveTo.equals(Constant.DOWN)) {
+            upperBridge.add(Constant.NOSPOT);
+            lowerBridge.add(Constant.CORRECTSPOT);
+        }
+    }
+
+    public void addUnSafe(String moveTo) {
+        Validator.validateMovingInput(moveTo);
+        if (moveTo.equals(Constant.UP)) {
+            upperBridge.add(Constant.UNCORRECTSPOT);
+            lowerBridge.add(Constant.NOSPOT);
+            return;
+        }
+        if (moveTo.equals(Constant.DOWN)) {
+            upperBridge.add(Constant.NOSPOT);
+            lowerBridge.add(Constant.UNCORRECTSPOT);
+        }
+    }
+
+    public void removeUnsafeSpot() {
+        upperBridge.remove(upperBridge.size() - Constant.ONESTEP);
+        lowerBridge.remove(lowerBridge.size() - Constant.ONESTEP);
+    }
+
+    public List<String> getBaseBridge() {
+        return baseBridge;
+    }
+
+    public List<String> getUpperBridge() {
+        return upperBridge;
+    }
+
+    public List<String> getLowerBridge() {
+        return lowerBridge;
+    }
 }
