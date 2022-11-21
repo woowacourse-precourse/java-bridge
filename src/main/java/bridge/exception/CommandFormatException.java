ff38e665b0f13b65125c31fdeb1405b2f388e412
JUHA <khabh@naver.com>
2022-11-21 22:27:43 +0900

    chore: 불필요한 코드 공백 삭제

--- src/main/java/bridge/exception/CommandFormatException.java
+++ src/main/java/bridge/exception/CommandFormatException.java
@@ -1,9 +1,9 @@
 package bridge.exception;
 
-public class CommandFormatException extends IllegalArgumentException{
+public class CommandFormatException extends IllegalArgumentException {
     private final static String ERROR_MESSAGE = "[ERROR] R 또는 Q만 입력할 수 있습니다.";
 
-    public  CommandFormatException() {
+    public CommandFormatException() {
         super(ERROR_MESSAGE);
     }
 }
