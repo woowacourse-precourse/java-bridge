3beb53938b688907a571cb599bfff9bdcf7a8304
hyun0404woo <hyun0404woo@gmail.com>
2022-11-21 16:40:50 +0900

    feat: 게임 실행을 위한 클라이언트 코드 작성
    - 실행 순서
      - initializeGame
      - playGame
      - terminateGame

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -1,8 +1,39 @@
 package bridge;
 
+import bridge.controller.BridgeGame;
+import bridge.domain.BridgeGameStatus;
+import bridge.domain.GameCommand;
+import bridge.domain.Moving;
+import bridge.util.BridgeNumberGenerator;
+import bridge.util.BridgeRandomNumberGenerator;
+import bridge.view.InputView;
+import bridge.view.OutputView;
+
 public class Application {
 
+    private final static BridgeNumberGenerator bridgeNumberGenerator = new BridgeRandomNumberGenerator();
+    private final static InputView inputView = new InputView();
+    private final static OutputView outputView = new OutputView();
+
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        final BridgeGame bridgeGame = new BridgeGame(bridgeNumberGenerator);
+        initializeGame(bridgeGame);
+        playGame(bridgeGame);
+        terminateGame(bridgeGame);
+    }
+
+    private static void initializeGame(BridgeGame bridgeGame) {
+        outputView.printGameStartMessage();
+        final int bridgeSize = inputView.readBridgeSize();
+        bridgeGame.initialize(bridgeSize);
+    }
+
+    private static void playGame(BridgeGame bridgeGame) {
+        
+    }
+
+    private static void terminateGame(BridgeGame bridgeGame) {
+        BridgeGameStatus bridgeGameStatus = bridgeGame.getBridgeGameStatus();
+        outputView.printResult(bridgeGameStatus);
     }
 }
