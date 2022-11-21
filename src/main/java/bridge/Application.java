9786b40369e316a6081216140dd6476da33a37ef
jxwzn-17 <g1990107@naver.com>
2022-11-22 04:59:43 +0900

    InputView 완료

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -1,8 +1,9 @@
 package bridge;
 
 public class Application {
-
+    private static InputView inputView = new InputView();
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        System.out.println("다리 건너기 게임을 시작합니다.");
+        int len = inputView.readBridgeSize();
     }
 }
--- src/main/java/bridge/InputView.java
+++ src/main/java/bridge/InputView.java
@@ -1,5 +1,7 @@
 package bridge;
 
+import static camp.nextstep.edu.missionutils.Console.readLine;
+
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
  */
@@ -9,20 +11,37 @@ public class InputView {
      * 다리의 길이를 입력받는다.
      */
     public int readBridgeSize() {
-        return 0;
+        System.out.println();
+        System.out.println("다리의 길이를 입력해주세요");
+        int size = Integer.parseInt(readLine());
+        if(size<3||size>20){
+            throw new IllegalArgumentException("[ERROR] 다리 길이는 3부터 20 사이의 숫자여야 합니다.");
+        }
+        return size;
     }
 
     /**
      * 사용자가 이동할 칸을 입력받는다.
      */
     public String readMoving() {
-        return null;
+        System.out.println();
+        System.out.println("이동할 칸을 선택해주세요. (위: U, 아래: D)");
+        String move = readLine();
+        if(!move.equals("U")&&!move.equals("D")){
+            throw new IllegalArgumentException("[ERROR] 입력은 U,D 중 하나 입니다.");
+        }
+        return move;
     }
 
     /**
      * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
      */
     public String readGameCommand() {
-        return null;
+        System.out.println("게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)");
+        String RorQ = readLine();
+        if(!RorQ.equals("R")&&!RorQ.equals("Q")){
+            throw new IllegalArgumentException("[ERROR] 입력은 R,Q 중 하나 입니다.");
+        }
+        return RorQ;
     }
 }
