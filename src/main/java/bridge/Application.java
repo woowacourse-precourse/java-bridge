7490a27251d67f59baec56cac6f1d1639c94a676
KJE <12181501@inha.edu>
2022-11-21 15:07:47 +0900

    feat: main-예외처리(IllegalArgumentException throw) 함수 추가

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -3,6 +3,11 @@ package bridge;
 public class Application {
 
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+
+    }
+
+    public static void errorHandling(String errorMessage){
+        System.out.println("[ERROR]" + errorMessage);
+        throw new IllegalArgumentException();
     }
 }
