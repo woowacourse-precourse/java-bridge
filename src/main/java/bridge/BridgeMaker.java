322b557e72b511b8301f5f3fa4ce6baa7e08406d
junwon <todayhappymod365@gmail.com>
2022-11-21 14:11:11 +0900

    Feat: 다리 모양 생성 기능 구현
    BridgeNumberGenerator 클래스의 generate메소드를 통해
    생성한 0과1로 구성된 랜덤 값을 게임 규칙의 다리 모양에 맞게
    위 칸이면 "U", 아래 칸이면 "D"로 필터링하는 upDownFilter
    메소드 구현 후 ApplicationTest에서 다리_생성_테스트 통과
    다리 길이 체크 메소드 구체적인 에러 메서드 출력하게 변경

--- src/main/java/bridge/BridgeMaker.java
+++ src/main/java/bridge/BridgeMaker.java
@@ -1,5 +1,6 @@
 package bridge;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -19,10 +20,16 @@ public class BridgeMaker {
      */
     public List<String> makeBridge(int size) {
         bridgeSizeCheck(size);
-        return null;
+        List<String> bridge=new ArrayList<>();
+        for(int count=0; count<size; count++)
+            bridge.add(upDownFilter(bridgeNumberGenerator.generate()));
+        return bridge;
     }
     private void bridgeSizeCheck(int size){
-        if(size <3 || size>20)  throw new IllegalArgumentException("[ERROR]");
+        if(size <3 || size>20)  throw new IllegalArgumentException("[ERROR] 다리 길이는 3부터 20 사이의 숫자여야 합니다.");
     }
-
+    private String upDownFilter(int RandomNumber){
+        if(RandomNumber==1) return "U";
+        return "D";
+    }
 }
