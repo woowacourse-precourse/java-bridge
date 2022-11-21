9911679fdc77d1519ce03e7a234fcbb35b8076d7
iamjooon2 <iamjooon2@gmail.com>
2022-11-21 18:50:17 +0900

    feat: 게임 재시작 여부 질문을 출력하는 기능

--- src/main/java/bridge/view/InputView.java
+++ src/main/java/bridge/view/InputView.java
@@ -40,6 +40,8 @@ public class InputView {
      * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
      */
     public String readGameCommand() {
+        outputView.printMessage(Message.REGAME);
         return null;
     }
+
 }
