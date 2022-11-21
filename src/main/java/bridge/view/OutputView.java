2fe6d512f375159ceba42770838a16129eb57a14
jyj1143 <jyj1143@gmail.com>
2022-11-21 20:22:58 +0900

    feat(OutPutView): 현재까지 건넌 다리를 모두 출력하는 기능 추가

--- src/main/java/bridge/view/OutputView.java
+++ src/main/java/bridge/view/OutputView.java
@@ -1,5 +1,9 @@
 package bridge.view;
 
+import bridge.domain.BridgeResult;
+
+import java.util.List;
+
 /**
  * 사용자에게 게임 진행 상황과 결과를 출력하는 역할을 한다.
  */
@@ -10,7 +14,10 @@ public class OutputView {
      * <p>
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void printMap() {
+    public void printMap(List<String> playerBridge, List<String> answerBridge) {
+        BridgeResult bridgeResult = new BridgeResult(answerBridge);
+        System.out.println(bridgeResult.matchBridge(playerBridge));
+        System.out.println();
     }
 
     /**
