350a9d05389964bbafc845ebe6d59d486ebc3c67
yejin <kwonyj1022@naver.com>
2022-11-21 17:48:17 +0900

    refactor: 문자열 포장

--- src/main/java/bridge/validation/Validation.java
+++ src/main/java/bridge/validation/Validation.java
@@ -1,28 +1,32 @@
 package bridge.validation;
 
+import static bridge.constant.ErrorMessage.*;
+import static bridge.constant.GameCommand.*;
+import static bridge.constant.Updown.*;
+
 public class Validation {
     public void validateBridgeSize(String bridgeSize) {
         try {
             int size = Integer.parseInt(bridgeSize);
             if ((size < 3) || (size > 20)) {
-                throw new IllegalArgumentException("[ERROR] 다리 길이는 3부터 20 사이의 숫자여야 합니다.");
+                throw new IllegalArgumentException(BRIDGE_SIZE_ERROR);
             }
         } catch(NumberFormatException e) {
-            throw new IllegalArgumentException("[ERROR] 다리 길이는 3부터 20 사이의 숫자여야 합니다.");
+            throw new IllegalArgumentException(BRIDGE_SIZE_ERROR);
         }
     }
 
     public void validateMoving(String moving) {
-        if (moving.equals("U") || moving.equals("D")) {
+        if (moving.equals(UP.getKey()) || moving.equals(DOWN.getKey())) {
             return;
         }
-        throw new IllegalArgumentException("[ERROR] U 또는 D만 입력 가능합니다.");
+        throw new IllegalArgumentException(MOVING_ERROR);
     }
 
     public void validateGameCommand(String gameCommand) {
-        if (gameCommand.equals("R") || gameCommand.equals("Q")) {
+        if (gameCommand.equals(RETRY.getKey()) || gameCommand.equals(QUIT.getKey())) {
             return;
         }
-        throw new IllegalArgumentException("[ERROR] R 또는 Q만 입력 가능합니다.");
+        throw new IllegalArgumentException(GAME_COMMAND_ERROR);
     }
 }
