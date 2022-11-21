e4e1fc701c2a6e72faca80a7d7570e7f6ebc37fa
Frog-Slayer <Frog-Slayer@github.com>
2022-11-21 18:46:23 +0900

    feat(BridgeGame): 추가 주어진 입력에 따라 다음 칸으로  갈수 있는지 여부를 판정

--- src/main/java/bridge/domain/BridgeGame.java
+++ src/main/java/bridge/domain/BridgeGame.java
@@ -1,16 +1,35 @@
 package bridge.domain;
 
+import bridge.BridgeMaker;
+
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * 다리 건너기 게임을 관리하는 클래스
  */
 public class BridgeGame {
 
-    /**
-     * 사용자가 칸을 이동할 때 사용하는 메서드
-     * <p>
-     * 이동을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
-     */
-    public void move() {
+    private Integer currentBlockIndex = -1;
+    private List<String> bridge = new ArrayList<>();
+    private List<String> bridgeProgress = new ArrayList<>();
+    public BridgeGame(List<String> bridge){
+        currentBlockIndex = -1;
+        this.bridge = bridge;
+        this.bridgeProgress = new ArrayList<>();
+    }
+
+    public void move(String moveCommand) {
+        if (isMovable(moveCommand)) {
+            this.bridgeProgress.add("O");
+            currentBlockIndex += 1;
+            return;
+        }
+        this.bridgeProgress.add("X");
+    }
+
+    private Boolean isMovable(String moveCommand){
+        return bridge.get(currentBlockIndex + 1).equals(moveCommand);
     }
 
     /**
