f5f84ed89ca107cd6279a7c55ddbceec65cbed6c
thousandcows <milcamboo@gmail.com>
2022-11-21 21:14:09 +0900

    fix: fix a bug by using class final class field

--- src/main/java/bridge/BridgeMaker.java
+++ src/main/java/bridge/BridgeMaker.java
@@ -19,10 +19,9 @@ public class BridgeMaker {
      * @return 입력받은 길이에 해당하는 다리 모양. 위 칸이면 "U", 아래 칸이면 "D"로 표현해야 한다.
      */
     public List<String> makeBridge(int size) {
-        BridgeRandomNumberGenerator randomNumberGenerator = new BridgeRandomNumberGenerator();
         List<String> bridge = new ArrayList<>();
         for (int i = 0; i < size; i++) {
-            int newRandomNumber = randomNumberGenerator.generate();
+            int newRandomNumber = bridgeNumberGenerator.generate();
             if (newRandomNumber == 0) {
                 bridge.add("D");
                 continue;
