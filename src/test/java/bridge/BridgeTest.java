28b685bc70ca13e791e38bd7fdd7485a2ab54ee1
heesang <dlagmltkd12@naver.com>
2022-11-22 00:44:05 +0900

    test: BridgeTest hasMessage 문장 띄어쓰기로 테스트 실패 문제 해결
    이전의 에러 메시지 상수를 리팩토링 하면서 테스트에 적용 안해줘서 발생한 문제

--- src/test/java/bridge/BridgeTest.java
+++ src/test/java/bridge/BridgeTest.java
@@ -69,7 +69,7 @@ public class BridgeTest {
     void validateMinimumLength() {
         assertThatThrownBy(() -> new Bridge(List.of("U", "U")))
                 .isInstanceOf(IllegalArgumentException.class)
-                .hasMessage("[ERROR] 다리의 길이는 3이상 20이하이어야 합니다.");
+                .hasMessage("[ERROR] 다리의 길이는 3이상 20이하 이어야 합니다.");
     }
 
     @DisplayName("다리의 길이가 20 초과이면 예외 발생")
@@ -80,6 +80,6 @@ public class BridgeTest {
                         "U", "U", "U", "U", "U", "U", "U", "U",
                         "U", "U", "U", "U", "U")))
                 .isInstanceOf(IllegalArgumentException.class)
-                .hasMessage("[ERROR] 다리의 길이는 3이상 20이하이어야 합니다.");
+                .hasMessage("[ERROR] 다리의 길이는 3이상 20이하 이어야 합니다.");
     }
 }
