021e3f60adce83f88b335f81c0f472d7072d332b
qkrdmswl <eunji303@naver.com>
2022-11-21 16:57:47 +0900

    refactor: apply java convention

--- src/main/java/bridge/type/GameOptionType.java
+++ src/main/java/bridge/type/GameOptionType.java
@@ -1,2 +1,15 @@
-package bridge.type;public enum GameOptionType {
+package bridge.type;
+
+public enum GameOptionType {
+    R("RESTART"), Q("QUIT");
+
+    private final String option;
+
+    GameOptionType(String option) {
+        this.option = option;
+    }
+
+    public String getOption() {
+        return option;
+    }
 }
