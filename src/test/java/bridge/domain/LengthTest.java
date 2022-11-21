3d1c535a337da05e762234a1eebd2a34a8ee40f8
minju412 <ln8847@ajou.ac.kr>
2022-11-22 01:17:32 +0900

    test: use CsvSource
    - 반복되는 테스트를 수행하기 위해서

--- src/test/java/bridge/domain/LengthTest.java
+++ src/test/java/bridge/domain/LengthTest.java
@@ -3,29 +3,15 @@ package bridge.domain;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import bridge.domain.bridgeInfo.Length;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 class LengthTest {
 
-    @DisplayName("다리의 길이가 3 이상 20 이하가 아니라면 예외가 발생한다.")
-    @Test
-    void createLengthByOverSize() {
-        assertThatThrownBy(() -> new Length("30"))
-                .isInstanceOf(IllegalArgumentException.class);
-    }
-
-    @DisplayName("다리의 길이가 공백이라면 예외가 발생한다.")
-    @Test
-    void createLengthWithBlank() {
-        assertThatThrownBy(() -> new Length(""))
-                .isInstanceOf(IllegalArgumentException.class);
-    }
-
-    @DisplayName("다리의 길이에 문자가 포함되면 예외가 발생한다.")
-    @Test
-    void createLengthWithCharacter() {
-        assertThatThrownBy(() -> new Length("4$"))
+    @ParameterizedTest
+    @CsvSource({"'30'", "''", "'4$'"})
+    void createLengthBy(String size) {
+        assertThatThrownBy(() -> new Length(size))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 }
