1e04139469cb230d1f1d4f44f86c4c534587b67b
Young37 <younghk37@gmail.com>
2022-11-21 18:08:34 +0900

    feat(view): add the feature of reading direction

--- src/main/java/bridge/view/InputView.java
+++ src/main/java/bridge/view/InputView.java
@@ -9,6 +9,7 @@ import camp.nextstep.edu.missionutils.Console;
  */
 public class InputView {
     private static final String BRIDGE_SIZE_ERROR_MESSAGE = "[ERROR] 다리 길이는 3부터 20 사이의 숫자여야 합니다.";
+    private static final String INVALID_DIRECTION_ERROR_MESSAGE = "[ERROR] 이동할 칸을 올바르게 선택해주세요.(위: U, 아래: D)";
 
     /**
      * 다리의 길이를 입력받는다.
@@ -51,7 +52,22 @@ public class InputView {
      * 사용자가 이동할 칸을 입력받는다.
      */
     public String readMoving() {
-        return null;
+        try {
+            String directcion = Console.readLine();
+            validateDirectcion(directcion);
+            return directcion;
+        } catch (IllegalArgumentException exception) {
+            throw new IllegalArgumentException(INVALID_DIRECTION_ERROR_MESSAGE);
+        }
+    }
+    
+    /**
+     * U 또는 D 문자열과 다르면 IllegalArgumentException을 발생시키는 메서드
+     */
+    private void validateDirectcion(String directcion) {
+        if (!"U".equals(directcion) && !"D".equals(directcion)) {
+            throw new IllegalArgumentException();
+        }
     }
 
     /**
