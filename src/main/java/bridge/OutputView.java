8640abeeaf9aca2c3ba27f3df56fdd7a80f96e1d
고동우 <shp05179@gmail.com>
2022-11-21 17:19:34 +0900

    feat: 최종 결과 정해진 형식에 맞게 출력하는 기능, 게임시작멘트 출력기능
    기존 printMapWithNew를 이용해 맵을 출력한다.

--- src/main/java/bridge/OutputView.java
+++ src/main/java/bridge/OutputView.java
@@ -8,9 +8,16 @@ import java.util.List;
  */
 public class OutputView {
     private static final String ERROR_START_WITH = "[ERROR] ";
+    private static final String START_GAME = "다리 건너기 게임을 시작합니다.";
     private static final String REQUEST_BRIDGE_SIZE = "다리의 길이를 입력해주세요.";
     private static final String REQUEST_UP_DOWN = "이동할 칸을 선택해주세요. (위: U, 아래: D)";
     private static final String REQUEST_RETRY_QUIT = "게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)";
+    private static final String FINAL_RESULT = "최종 게임 결과";
+    private static final String SUCCESS_OR_NOT = "게임 성공 여부: ";
+    private static final String TRIES = "총 시도한 횟수: ";
+    private static final String SUCCESS = "성공";
+    private static final String FAIL = "실패";
+
     private static final String EMPTY = " ";
     private static final String BRIDGE_START = "[ ";
     private static final String BRIDGE_END = " ]";
@@ -56,6 +63,7 @@ public class OutputView {
         String downside = BRIDGE_START + arrToString(getMiddle(crossedBridge, DOWN)) + BRIDGE_END;
         System.out.println(upside);
         System.out.println(downside);
+        System.out.println();
     }
 
     private static String newGuessToString(boolean isSuccess, String position, String newPosition) {
@@ -75,6 +83,7 @@ public class OutputView {
                 + newGuessToString(isSuccess, DOWN, newPosition) + BRIDGE_END;
         System.out.println(upside);
         System.out.println(downside);
+        System.out.println();
     }
 
     /**
@@ -82,11 +91,25 @@ public class OutputView {
      * <p>
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void printResult() {
+    private static String isSuccessToString(boolean isSuccess) {
+        if (isSuccess) {
+            return SUCCESS;
+        }
+        return FAIL;
+    }
+    public static void printResult(List<String> crossedBridge, boolean isSuccess, int tries) {
+        System.out.println(FINAL_RESULT);
+        printMapWithNew(crossedBridge.subList(0,crossedBridge.size()-1), isSuccess, crossedBridge.get(crossedBridge.size()-1));
+        System.out.println(SUCCESS_OR_NOT + isSuccessToString(isSuccess));
+        System.out.println(TRIES+ tries);
     }
 
     public static void printError(String errorMessage) {
         System.out.println(ERROR_START_WITH + errorMessage);
+    }
+
+    public static void printStartGame() {
+        System.out.println(START_GAME);
     }
 
     public static void printRequestBridgeSize() {
