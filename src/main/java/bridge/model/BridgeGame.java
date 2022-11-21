5d81eebdf7a09ce856fdf1c30196a31a9cae6943
viyott-lover <jackk0616@gmail.com>
2022-11-21 16:49:58 +0900

    feat(BridgeGame): 현재 지나온 길의 마지막 즉 현재 위치와 사용자의 입력이 불일치하는지 검증하는 isDiscord() 메소드 작성

--- src/main/java/bridge/model/BridgeGame.java
+++ src/main/java/bridge/model/BridgeGame.java
@@ -39,6 +39,14 @@ public class BridgeGame {
     }
 
     /**
+     * 사용자의 입력과 현재까지 건너온 길의 마지막이 불일치하는지 검증하는 메소드
+     * @return
+     */
+    public boolean isDiscord() {
+        return marks.get(marks.size() - 1).equals("X");
+    }
+
+    /**
      * 사용자가 게임을 다시 시도할 때 사용하는 메서드
      * <p>
      * 재시작을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
