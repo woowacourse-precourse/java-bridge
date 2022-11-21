29e84413f86f2f66ef02e835e7bccb64c3dc32d3
Jaehong Lee <woohong24@naver.com>
2022-11-21 16:04:48 +0900

    feat: Exception 이름(상수)들 추가

---
+++ src/main/java/bridge/exception/ExceptionName.java
@@ -0,0 +1,8 @@
+package bridge.exception;
+
+public class ExceptionName {
+
+    public static final String BRIDGE_MAKER_SIZE_EXCEPTION = "[ERROR] 다리 길이는 3부터 20 사이의 숫자여야 합니다.";
+    public static final String BRIDGE_MOVE_EXCEPTION = "[ERROR] U(위 칸)와 D(아래 칸) 중 하나의 문자만 입력해야 합니다.";
+    public static final String BRIDGE_GAME_RETRY_EXCEPTION = "[ERROR] R(재시작)과 Q(종료) 중 하나의 문자만 입력해야 합니다.";
+}
