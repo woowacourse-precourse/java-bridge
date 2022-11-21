a36f4e82f40390db2363cb30c9cdc19c2e9325b7
jungwoo <rlawjddn103@naver.com>
2022-11-21 14:23:19 +0900

    feat(Application): BridgeGame을 이용한 게임 시작 기능 구현

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -1,8 +1,18 @@
 package bridge;
 
+import bridge.controller.BridgeGame;
+
 public class Application {
 
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        System.out.println("다리 건너기 게임을 시작합니다.");
+        System.out.println();
+        try {
+            BridgeGame bridgeGame = new BridgeGame();
+            bridgeGame.playGame();
+        }catch (IllegalArgumentException illegalArgumentException) {
+            System.out.println(illegalArgumentException.getMessage());
+        }
     }
 }
