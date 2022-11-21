dd4c11a66507313fef55fb2f9af0b77a81e9a4e2
jxixeun <jxixeun@gmail.com>
2022-11-22 00:52:33 +0900

    feat: 다리 길이 입력값 체크 기능 작성

---
+++ src/main/java/bridge/InputValidator.java
@@ -0,0 +1,33 @@
+package bridge;
+
+/**
+ * 사용자의 입력을 검사하는 역할을 한다.
+ */
+public class InputValidator {
+
+    /**
+     * input String값이 숫자면 ture를, 아니면 false를 반환한다.
+     */
+    public boolean isNumeric(String input) {
+        try {
+            Integer.parseInt(input);
+            return true;
+        } catch (NumberFormatException e){
+            return false;
+        }
+    }
+
+    /**
+     * 입력 받은 다리의 길이가 올바른 값인지 검사한다.
+     */
+    public void checkBridgeSize(String input) {
+        if (!isNumeric(input)){
+            throw new IllegalArgumentException("[ERROR] 다리 길이는 3부터 20 사이의 숫자여야 합니다.");
+        }
+        int bridgeSizeInput = Integer.parseInt(input);
+        if (bridgeSizeInput < 3 || bridgeSizeInput > 20) {
+            throw new IllegalArgumentException("[ERROR] 다리 길이는 3부터 20 사이의 숫자여야 합니다.");
+        }
+    }
+
+}
