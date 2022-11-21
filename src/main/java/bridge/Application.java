a15cdc2a68616d482c71a45f412eadef9b1f2a7c
wonho-seo <sjdlrnen0507@naver.com>
2022-11-22 05:30:25 +0900

    feat : feature main
    지금까지의 기능들을 조합하여 구현

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -1,8 +1,19 @@
 package bridge;
 
+import bridge.Setting.OutputViewPrintEnum;
+
 public class Application {
 
     public static void main(String[] args) {
+        System.out.println(OutputViewPrintEnum.GAME_START.getMessage());
+        InputView inputView = new InputView();
+        BridgeGame bridgeGame = new BridgeGame(new BridgeRandomNumberGenerator(), inputView.readBridgeSize());
+        while (bridgeGame.keepMove()){
+            bridgeGame.move();
+            bridgeGame.retry();
+        }
+        OutputView outputView = new OutputView();
+        outputView.printResult(bridgeGame.getResultLines());
         // TODO: 프로그램 구현
     }
 }
