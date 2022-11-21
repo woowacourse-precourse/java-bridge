6b01b5096460e9e67977dbcbf5e017a3bcc9ef24
junyong <jymoon1121@gmail.com>
2022-11-21 16:33:36 +0900

    feat(Validate): 이동할 칸 예외처리 메서드 생성

--- src/main/java/bridge/Validate.java
+++ src/main/java/bridge/Validate.java
@@ -1,15 +1,23 @@
 package bridge;
 
 public class Validate {
-    private final String error = "[ERROR]";
+    private final String ERROR = "[ERROR]";
+    private final String UP = "U";
+    private final String DOWN = "D";
 
     public void validateNumberRange(String input, int min, int max) {
         if (!input.matches("[0-9]")) {
-            throw new IllegalArgumentException(error + " 숫자만 입력 가능합니다.");
+            throw new IllegalArgumentException(ERROR + " 숫자만 입력 가능합니다.");
         }
         int num = Integer.parseInt(input);
         if (num < min || num > max) {
-            throw new IllegalArgumentException(error + min + "이상 " + max + "이하의 숫자만 가능합니다.");
+            throw new IllegalArgumentException(ERROR + min + "이상 " + max + "이하의 숫자만 가능합니다.");
+        }
+    }
+
+    public void validateIsUpOrDown(String input) {
+        if (!input.equals(UP) && !input.equals(DOWN)) {
+            throw new IllegalArgumentException(ERROR + UP + " 또는 " + DOWN + "만 입력 가능합니다.");
         }
     }
 }
