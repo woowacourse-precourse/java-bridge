05e372ec79ca115a2c4f89d7c1be087c6e386ddf
kwang <shinkwang@yonsei.ac.kr>
2022-11-22 04:20:20 +0900

    refactor: Try to make bridgeGame [Not complete]

--- src/main/java/bridge/BridgeGame.java
+++ src/main/java/bridge/BridgeGame.java
@@ -1,9 +1,33 @@
 package bridge;
 
+import bridge.view.InputView;
+import bridge.view.OutputView;
+
 /**
  * 다리 건너기 게임을 관리하는 클래스
  */
 public class BridgeGame {
+
+    private int size;
+
+    private OutputView outputView;
+
+    private InputView inputView;
+
+    public BridgeGame() {
+        this.outputView = new OutputView();
+        this.inputView = new InputView();
+    }
+
+    public void startGame() {
+        outputView.printStatGame();
+        outputView.printSpaceLine();
+        outputView.printInputBridgeSize();
+        inputView.readBridgeSize();
+        outputView.printSpaceLine();
+    }
+
+
 
     /**
      * 사용자가 칸을 이동할 때 사용하는 메서드
