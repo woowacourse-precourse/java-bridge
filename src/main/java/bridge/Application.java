59f12378e45ae437a7f1f9ef9b5bc949973dad68
Hong Jin-Seop <jinsub1999@khu.ac.kr>
2022-11-21 15:01:02 +0900

    feat: 구현한 클래스들로 다리 건너기 게임 구현

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -1,8 +1,32 @@
 package bridge;
 
+import bridge.BridgeGame.STATE;
+
 public class Application {
+    public static void makeOneStep(BridgeGame game, InputView inputView, OutputView outputView) {
+        String moving = inputView.readMoving();
+        game.move(moving);
+        outputView.printMap(game.getBridge(), game.getMoves());
+    }
+
+    public static BridgeGame gameProcess(BridgeMaker bridgeMaker, InputView inputView, OutputView outputView) {
+        BridgeGame game = new BridgeGame(bridgeMaker.makeBridge(inputView.readBridgeSize()));
+        while (game.getState() == STATE.MOVING) {
+            makeOneStep(game, inputView, outputView);
+            if (game.getState() == STATE.FAILED) {
+                String command = inputView.readGameCommand();
+                game.retry(command);
+            }
+        }
+        return game;
+    }
 
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        BridgeMaker bridgeMaker = new BridgeMaker(new BridgeRandomNumberGenerator());
+        InputView inputView = new InputView();
+        OutputView outputView = new OutputView();
+
+        BridgeGame game = gameProcess(bridgeMaker, inputView, outputView);
+        outputView.printResult(game, game.getRetries());
     }
 }
