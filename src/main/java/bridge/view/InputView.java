ee4293c30ae4b8e4cb262f2401dcafae1082ed92
NAM GIBEOM <rlqja0523@naver.com>
2022-11-22 02:15:02 +0900

    feat: 게임 재시작 여부를 입력받는 메서드 구현

--- src/main/java/bridge/view/InputView.java
+++ src/main/java/bridge/view/InputView.java
@@ -1,6 +1,7 @@
 package bridge.view;
 
 import bridge.exception.BridgeSizeException;
+import bridge.exception.GameCommandException;
 import bridge.exception.MovingException;
 import bridge.exception.UserException;
 import camp.nextstep.edu.missionutils.Console;
@@ -34,7 +35,9 @@ public class InputView {
      * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
      */
     public String readGameCommand() {
-        return null;
+        System.out.println("게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)");
+        UserException gameCommandException = new GameCommandException();
+        return checkInput(gameCommandException);
     }
 
     private String checkInput(UserException userException) {
