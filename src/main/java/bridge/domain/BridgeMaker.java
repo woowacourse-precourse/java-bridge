cbecd7b650324579ea7f9942ec2029cddf85e74c
LeeHaiLim <hailim1997@gmail.com>
2022-11-22 03:34:15 +0900

    chore(domain): change package and remove comments

--- src/main/java/bridge/domain/BridgeMaker.java
+++ src/main/java/bridge/domain/BridgeMaker.java
@@ -1,13 +1,12 @@
-package bridge;
+package bridge.domain;
 
+import bridge.BridgeNumberGenerator;
+import bridge.Constants;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
-/**
- * 다리의 길이를 입력 받아서 다리를 생성해주는 역할을 한다.
- */
 public class BridgeMaker {
     private static final Map<Integer, String> UP_OR_DOWN = Map.of(
             1, Constants.CODE_UP,
@@ -20,10 +19,6 @@ public class BridgeMaker {
         this.bridgeNumberGenerator = bridgeNumberGenerator;
     }
 
-    /**
-     * @param size 다리의 길이
-     * @return 입력받은 길이에 해당하는 다리 모양. 위 칸이면 "U", 아래 칸이면 "D"로 표현해야 한다.
-     */
     public List<String> makeBridge(int size) {
         return IntStream.range(0, size)
                 .mapToObj(i -> UP_OR_DOWN.get(bridgeNumberGenerator.generate()))
