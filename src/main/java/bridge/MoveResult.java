b124089b0e0c41d3331d93ef99589dbad93c7683
qdtjj4 <76ylo1@gmail.com>
2022-11-21 20:04:59 +0900

    feat: 출력 형식에 맞는 다리 모양 구현

--- src/main/java/bridge/MoveResult.java
+++ src/main/java/bridge/MoveResult.java
@@ -1,6 +1,8 @@
 package bridge;
 
+import java.util.HashMap;
 import java.util.Map;
+import java.util.StringJoiner;
 
 public class MoveResult {
 
@@ -8,5 +10,31 @@ public class MoveResult {
 
     public MoveResult(Map<String, String> moveResult) {
         this.moveResult = moveResult;
+    }
+
+    public Map<String, String> createPrintForm() {
+        Map<String, String> printForm = new HashMap<>();
+        StringJoiner separator = new StringJoiner(" | ", "[ ", " ]");
+        printForm.put("U", upperPrintForm(separator));
+        printForm.put("D", lowerPrintForm(separator));
+        return printForm;
+    }
+
+    private String upperPrintForm(StringJoiner separator) {
+        String upperResult = moveResult.get("U");
+        for (int space = 0; space < upperResult.length(); space++) {
+            String eachResult = "" + upperResult.charAt(space);
+            separator.add(eachResult);
+        }
+        return separator.toString();
+    }
+
+    private String lowerPrintForm(StringJoiner separator) {
+        String lowerResult = moveResult.get("D");
+        for (int space = 0; space < lowerResult.length(); space++) {
+            String eachResult = "" + lowerResult.charAt(space);
+            separator.add(eachResult);
+        }
+        return separator.toString();
     }
 }
