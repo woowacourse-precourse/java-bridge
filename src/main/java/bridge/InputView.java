e5d6ae789aa95dc242dd3671f2052f537609d813
Juuuic <sn990908a@gmail.com>
2022-11-21 19:44:44 +0900

    feat: readBridgeSize() 기능 구현

--- src/main/java/bridge/InputView.java
+++ src/main/java/bridge/InputView.java
@@ -1,5 +1,6 @@
 package bridge;
 
+import camp.nextstep.edu.missionutils.Console;
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
  */
@@ -9,7 +10,11 @@ public class InputView {
      * 다리의 길이를 입력받는다.
      */
     public int readBridgeSize() {
-        return 0;
+        System.out.print("다리의 길이를 입력해주세요.");
+        int bridgeInput = Integer.parseInt(Console.readLine());
+
+        //다리 길이를 return
+        return bridgeInput;
     }
 
     /**
