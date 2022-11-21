cd1a08bb0f756db9b0898f869b62bb7ef7bd6890
Jinseo Song <82093280+rxb8k@users.noreply.github.com>
2022-11-21 22:31:51 +0900

    feat: 게임 시작 문구 출력 기능 추가

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -4,11 +4,15 @@ import bridge.domain.Bridge;
 import bridge.domain.BridgeGame;
 import bridge.domain.gameStatus;
 import bridge.ui.InputView;
+import bridge.ui.OutputView;
 
 public class Application {
 
     public static void main(String[] args) {
         InputView input = new InputView();
+        OutputView output = new OutputView();
+
+        output.printStartMessage();
 
         System.out.println("다리의 길이를 입력해주세요.");
         int size = input.readBridgeSize();
