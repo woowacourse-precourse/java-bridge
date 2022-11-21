71b33f9e4c5c469d970869dcd304b805238e12b1
이정우 <acg6138@naver.com>
2022-11-21 18:24:54 +0900

    refactor(OutputView): 출력 형식에 맞게 수정

--- src/main/java/bridge/view/OutputView.java
+++ src/main/java/bridge/view/OutputView.java
@@ -1,23 +1,24 @@
 package bridge.view;
 
-/**
- * 사용자에게 게임 진행 상황과 결과를 출력하는 역할을 한다.
- */
-public class OutputView {
+import bridge.domain.GameResult;
 
-    /**
-     * 현재까지 이동한 다리의 상태를 정해진 형식에 맞춰 출력한다.
-     * <p>
-     * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
-     */
-    public void printMap() {
+public class OutputView {
+    private static final String FINAL_RESULT_MESSAGE = "최종 게임 결과";
+    private static final String SUCCESS_OR_FAILURE = System.lineSeparator() + "게임 성공 여부: ";
+    private static final String TOTAL_PLAY_COUNT = "총 시도한 횟수: ";
+    private static final String START_SUFFIX = "[ ";
+    private static final String END_SUFFIX = " ]";
+
+    public void printMap(GameResult gameResult) {
+        System.out.println(START_SUFFIX + gameResult.getTopResult() + END_SUFFIX);
+        System.out.println(START_SUFFIX + gameResult.getBottomResult() + END_SUFFIX + System.lineSeparator());
     }
 
-    /**
-     * 게임의 최종 결과를 정해진 형식에 맞춰 출력한다.
-     * <p>
-     * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
-     */
-    public void printResult() {
+    public void printResult(GameResult gameResult) {
+        System.out.println(FINAL_RESULT_MESSAGE);
+        System.out.println(START_SUFFIX + gameResult.getTopResult() + END_SUFFIX);
+        System.out.println(START_SUFFIX + gameResult.getBottomResult() + END_SUFFIX);
+        System.out.println(SUCCESS_OR_FAILURE + gameResult.successOrFailure());
+        System.out.println(TOTAL_PLAY_COUNT + gameResult.totalPlayCount());
     }
 }
