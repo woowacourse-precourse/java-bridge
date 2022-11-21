cb37b74548862eabdb10ac53768076973dbff75e
qpdh <74190329+qpdh@users.noreply.github.com>
2022-11-22 04:19:06 +0900

    Refactor($InputViewTest) 불필요 import 제거

--- src/test/java/bridge/InputViewTest.java
+++ src/test/java/bridge/InputViewTest.java
@@ -2,15 +2,11 @@ package bridge;
 
 import camp.nextstep.edu.missionutils.test.NsTest;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
-import java.util.List;
-
 import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
 import static org.assertj.core.api.Assertions.*;
-import static org.assertj.core.util.Lists.newArrayList;
 
 class InputViewTest extends NsTest {
 
