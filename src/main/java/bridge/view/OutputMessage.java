96a93ea579bad8c0e255786403e7c7aa90809547
chanminmaxscaler <jcm04@naver.com>
2022-11-21 11:03:37 +0900

    feat: OutputMessage enum 클래스에 상수를 수정한다
    각 문장에 줄바꿈을 추가한다.
    SUCCESS("성공"), FAIL("실패")를 추가한다.

--- src/main/java/bridge/view/OutputMessage.java
+++ src/main/java/bridge/view/OutputMessage.java
@@ -1,18 +1,20 @@
 package bridge.view;
 
 public enum OutputMessage {
-    START("다리 건너기 게임을 시작합니다."),
-    BRIDGE_SIZE("다리의 길이를 입력해주세요."),
-    MOVE("이동할 칸을 선택해주세요. (위: U, 아래: D)"),
-    RESTART("게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)"),
-    RESULT_TITLE("최종 게임 결과\n"),
-    RESULT_CONTENT("게임 성공 여부: %s\n"),
+    START("다리 건너기 게임을 시작합니다.\n"),
+    BRIDGE_SIZE("\n다리의 길이를 입력해주세요.\n"),
+    MOVE("\n이동할 칸을 선택해주세요. (위: U, 아래: D)\n"),
+    RESTART("\n게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)\n"),
+    RESULT_TITLE("\n최종 게임 결과\n"),
+    RESULT_CONTENT("\n게임 성공 여부: %s\n"),
     NUMBER_OF_ATTEMPT("총 시도한 횟수: %d\n"),
-    CORRECT_ANSWER(" O "),
-    INCORRECT_ANSWER(" X "),
-    BLANK("   "),
-    DELIMITER("|"),
-    BRIDGE_EDGE("[%s]\n");
+    CORRECT_ANSWER("O"),
+    INCORRECT_ANSWER("X"),
+    BLANK(" "),
+    DELIMITER(" | "),
+    BRIDGE_EDGE("[ %s ]\n"),
+    SUCCESS("성공"),
+    FAIL("실패");
 
     private final String message;
 
