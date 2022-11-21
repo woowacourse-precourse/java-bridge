8dabdeb27b4c4283b4c76f8f9ffdda8d2124c251
oyoungsun <plusmate@naver.com>
2022-11-21 12:35:53 +0900

    refactor: 10줄 이하로 작성

--- src/main/java/bridge/BridgeGame.java
+++ src/main/java/bridge/BridgeGame.java
@@ -6,6 +6,16 @@ import java.util.List;
 /**
  * 다리 건너기 게임을 관리하는 클래스
  */
+class Move{
+    String direction;
+    int moveIdx;
+    int notMoveIdx;
+    public Move(String direction, int moveIdx, int notMoveIdx){
+        this.direction = direction;
+        this.moveIdx = moveIdx;
+        this.notMoveIdx = notMoveIdx;
+    }
+}
 public class BridgeGame {
     static List<String> map;
     static List<List<String>> userMap = new ArrayList<>();
@@ -39,8 +49,8 @@ public class BridgeGame {
     }
     public boolean play(int idx){
 
-        String direction = inputView.readMoving();
-        boolean success = move(idx, direction);
+        Move move = moveConstructor(inputView.readMoving());
+        boolean success = move(idx, move);
         if(success){
             outputView.printMap(idx+1, this.userMap.get(0), this.userMap.get(1));
             return true;
@@ -48,30 +58,30 @@ public class BridgeGame {
         outputView.printMap(idx+1, this.userMap.get(0), this.userMap.get(1));
         return false;
     }
+
+    private Move moveConstructor(String readMoving) {
+        if(readMoving.equals("U")){
+            return new Move(readMoving, 0,1);
+        }else if(readMoving.equals("D")){
+            return new Move(readMoving, 1, 0);
+        }
+        throw new IllegalArgumentException("[ERROR] 정상적인 입력이 아닙니다.");
+    }
+
     /**
      * 사용자가 칸을 이동할 때 사용하는 메서드
      * <p>
      * 이동을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public boolean move(int length, String direction) {
-        if(direction.equals("U") && map.get(length).equals("U")){
-            userMap.get(0).add("O");
-            userMap.get(1).add(" ");
+    public boolean move(int length, Move step) {
+        if(map.get(length).equals(step.direction)){
+            userMap.get(step.moveIdx).add("O");
+            userMap.get(step.notMoveIdx).add(" ");
             return true;
         }
-        else if(direction.equals("D") && map.get(length).equals("D")){
-            userMap.get(0).add(" ");
-            userMap.get(1).add("O");
-            return true;
-        }
-        else if(direction.equals("U") && map.get(length).equals("D")){
-            userMap.get(0).add("X");
-            userMap.get(1).add(" ");
-            return false;
-        }
-        else if(direction.equals("D") && map.get(length).equals("U")){
-            userMap.get(0).add(" ");
-            userMap.get(1).add("X");
+        else if(!map.get(length).equals(step.direction)){
+            userMap.get(step.moveIdx).add("O");
+            userMap.get(step.notMoveIdx).add(" ");
             return false;
         }
         throw new IllegalArgumentException("[ERROR] 정상적인 입력이 아닙니다.");
