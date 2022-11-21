57cdc0002b8e510702ed08883691f4cf094bb28c
Hi-Imjaeyoung <imjy2757@naver.com>
2022-11-21 14:00:34 +0900

    feat(BridgeConstant) : 게임 재시도에 관한 상수 enum값 추가.

--- src/main/java/bridge/constant/BridgeConstant.java
+++ src/main/java/bridge/constant/BridgeConstant.java
@@ -1,9 +1,13 @@
 package bridge.constant;
 
 public enum BridgeConstant {
-    CAN_GO_UP(1,"U") , CANT_GO_UP(0,"D");
+    CAN_GO_UP(1,"U"), CANT_GO_UP(0,"D"),
+    RETRY("R"), END("Q");
     int intValue;
     String stringValue;
+    BridgeConstant(String stringValue){
+        this.stringValue = stringValue;
+    }
     BridgeConstant(int intValue, String stringValue){
         this.intValue = intValue;
         this.stringValue = stringValue;
