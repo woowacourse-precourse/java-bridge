d0444dbd00bb6ea1263ce38bbf8f94f39d3007dc
yoon-yoo-tak <dbsdbxkrzz@naver.com>
2022-11-21 20:04:34 +0900

    test: BridgeGame의 move메서드 테스트
    move메서드가 의도한대로 동작하는지 테스트하는 테스트를 작성합니다

---
+++ src/test/java/bridge/BridgeGameTest.java
@@ -0,0 +1,42 @@
+package bridge;
+
+import java.util.Arrays;
+import java.util.List;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+
+class BridgeGameTest {
+
+    private BridgeGame bridgeGame;
+    private List<String> bridge = Arrays.asList("D", "U", "D");
+
+    @BeforeEach
+    void setBridgeGame() {
+        bridgeGame = new BridgeGame();
+    }
+
+    @Test
+    void 다리_건너기_올바른_입력_테스트() {
+        assertThat(bridgeGame.move(bridge, "D")).isEqualTo(true);
+    }
+
+    @Test
+    void 다리_건너기_올바른_입력_연속_테스트() {
+        assertThat(bridgeGame.move(bridge, "D")).isEqualTo(true);
+        assertThat(bridgeGame.move(bridge, "U")).isEqualTo(true);
+        assertThat(bridgeGame.move(bridge, "D")).isEqualTo(true);
+    }
+
+    @Test
+    void 다리_건너기_틀린_입력_테스트() {
+        assertThat(bridgeGame.move(bridge, "U")).isEqualTo(false);
+    }
+
+    @Test
+    void 다리_건너기_틀린_입력_연속_테스트() {
+        assertThat(bridgeGame.move(bridge, "D")).isEqualTo(true);
+        assertThat(bridgeGame.move(bridge, "D")).isEqualTo(false);
+    }
+}
