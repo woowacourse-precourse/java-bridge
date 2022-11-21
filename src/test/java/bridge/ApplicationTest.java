374e76f8d82e83ed8052e37433a5d3dd523384d9
kang-seongbeom <56289258+kang-seongbeom@users.noreply.github.com>
2022-11-21 13:43:12 +0900

    test : 통합 테스트 보강
    재시작 및 종료 테스트를 추가하여 테스트를 보강한다.

--- src/test/java/bridge/ApplicationTest.java
+++ src/test/java/bridge/ApplicationTest.java
@@ -5,8 +5,11 @@ import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.util.Lists.newArrayList;
 
+import bridge.message.ErrorMessage;
 import camp.nextstep.edu.missionutils.test.NsTest;
+
 import java.util.List;
+
 import org.junit.jupiter.api.Test;
 
 class ApplicationTest extends NsTest {
@@ -26,17 +29,112 @@ class ApplicationTest extends NsTest {
         assertRandomNumberInRangeTest(() -> {
             run("3", "U", "D", "U");
             assertThat(output()).contains(
-                "최종 게임 결과",
-                "[ O |   | O ]",
-                "[   | O |   ]",
-                "게임 성공 여부: 성공",
-                "총 시도한 횟수: 1"
+                    "최종 게임 결과",
+                    "[ O |   | O ]",
+                    "[   | O |   ]",
+                    "게임 성공 여부: 성공",
+                    "총 시도한 횟수: 1"
             );
 
             int upSideIndex = output().indexOf("[ O |   | O ]");
             int downSideIndex = output().indexOf("[   | O |   ]");
             assertThat(upSideIndex).isLessThan(downSideIndex);
         }, 1, 0, 1);
+    }
+
+    @Test
+    void 재시작_종료_테스트() {
+        assertRandomNumberInRangeTest(() -> {
+            run("5", "U", "D", "R", "U", "D", "Q");
+            assertThat(output()).contains(
+                    "[ O ]",
+                    "[   ]",
+                    "[ O |   ]",
+                    "[   | X ]",
+                    "[ O ]",
+                    "[   ]",
+                    "[ O |   ]",
+                    "[   | X ]",
+                    "최종 게임 결과",
+                    "게임 성공 여부: 실패",
+                    "총 시도한 횟수: 2"
+            );
+
+            int upSideIndex = output().indexOf("[ O |   ]");
+            int downSideIndex = output().indexOf("[   | X ]");
+            assertThat(upSideIndex).isLessThan(downSideIndex);
+        }, 1, 1, 1, 1, 1);
+    }
+
+    @Test
+    void 재시작_성공_테스트() {
+        assertRandomNumberInRangeTest(() -> {
+            run("5", "U", "U", "D", "R", "U", "U", "U", "D", "U");
+            assertThat(output()).contains(
+                    "[ O ]",
+                    "[   ]",
+                    "[ O | O ]",
+                    "[   |   ]",
+                    "[ O | O |   ]",
+                    "[   |   | X ]",
+                    "[ O ]",
+                    "[   ]",
+                    "[ O | O ]",
+                    "[   |   ]",
+                    "[ O | O | O ]",
+                    "[   |   |   ]",
+                    "[ O | O | O |   ]",
+                    "[   |   |   | O ]",
+                    "[ O | O | O |   | O ]",
+                    "[   |   |   | O |   ]",
+                    "최종 게임 결과",
+                    "게임 성공 여부: 성공",
+                    "총 시도한 횟수: 2"
+            );
+
+            int upSideIndex = output().indexOf("[ O | O | O |   | O ]");
+            int downSideIndex = output().indexOf("[   |   |   | O |   ]");
+            assertThat(upSideIndex).isLessThan(downSideIndex);
+        }, 1, 1, 1, 0, 1);
+    }
+
+    @Test
+    void 발판_숫자_입력_예외_테스트() {
+        assertSimpleTest(() -> {
+            runException("3", "1");
+            assertThat(output()).contains(ErrorMessage.NOT_MOVE_VALID_STRING.getMessage());
+        });
+    }
+
+    @Test
+    void 발판_다른문자_입력_예외_테스트() {
+        assertSimpleTest(() -> {
+            runException("3", "T");
+            assertThat(output()).contains(ErrorMessage.NOT_MOVE_VALID_STRING.getMessage());
+        });
+    }
+
+    @Test
+    void 재시작_숫자_입력_예외_테스트() {
+        assertRandomNumberInRangeTest(() -> {
+            run("3", "D", "2");
+            assertThat(output()).contains(
+                    "[   ]",
+                    "[ X ]",
+                    ErrorMessage.NOT_RESTART_QUICK_VALID_STRING.getMessage()
+            );
+        }, 1, 1, 1);
+    }
+
+    @Test
+    void 재시작_다른문자_입력_예외_테스트() {
+        assertRandomNumberInRangeTest(() -> {
+            run("3", "D", "E");
+            assertThat(output()).contains(
+                    "[   ]",
+                    "[ X ]",
+                    ErrorMessage.NOT_RESTART_QUICK_VALID_STRING.getMessage());
+        }, 1, 1, 1);
     }
 
     @Test
