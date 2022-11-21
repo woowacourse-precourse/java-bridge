7c56acf75359aa3771b85e0bc7909f748a53b33f
jwjin95 <jwjin95@yonsei.ac.kr>
2022-11-21 16:00:02 +0900

    feat: 게임 전체 흐름 구현

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -1,8 +1,47 @@
 package bridge;
 
+import java.util.List;
+
 public class Application {
 
+    public static OutputView outputView;
+    public static InputView inputView;
+    public static BridgeGame bridgeGame;
+
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        initiateView();
+
+        outputView.printGameStart();
+        int bridgeSize = inputView.readBridgeSize();
+        initiateBridgeGame(bridgeSize);
+
+        bridgeGamePlay();
+        outputView.printResult(bridgeGame.getBridge(), true, bridgeGame.getBridge().size(), bridgeGame.getTryCount());
     }
+
+    public static void initiateView() {
+        outputView = new OutputView();
+        inputView = new InputView();
+    }
+
+    public static void initiateBridgeGame(int bridgeSize) {
+        bridgeGame = new BridgeGame(1, -1,
+            new BridgeMaker(new BridgeRandomNumberGenerator()).makeBridge(bridgeSize));
+    }
+
+    public static void bridgeGamePlay() {
+        while (true) {
+            String movement = inputView.readMoving();
+            boolean move = bridgeGame.move(movement);
+            outputView.printMap(bridgeGame.getBridge(), bridgeGame.getCurLevel(), move);
+            if (bridgeGame.checkGameEnded(move)) {
+                if (bridgeGame.gameSucceeded()) break;
+                String gameCommand = inputView.readGameCommand();
+                if (gameCommand.equals("Q")) break;
+                if (gameCommand.equals("R")) bridgeGame.retry();
+            }
+        }
+    }
+
+
 }
