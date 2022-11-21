decfc31a137019f24af3b90871ef549307d973f5
yujaemin <woawoa0513@gmail.com>
2022-11-21 13:14:34 +0900

    feat: 다리 길이 입력 함수 구현
    사용자에게 다리의 길이를 입력받아 유효성 검사 후 반환한다.

--- src/main/java/bridge/InputView.java
+++ src/main/java/bridge/InputView.java
@@ -1,5 +1,6 @@
 package bridge;
 
+import static camp.nextstep.edu.missionutils.Console.readLine;
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
  */
@@ -9,7 +10,9 @@ public class InputView {
      * 다리의 길이를 입력받는다.
      */
     public int readBridgeSize() {
-        return 0;
+        String input = readLine();
+        validateBridgeSize(input);
+        return Integer.parseInt(input);
     }
 
     /**
