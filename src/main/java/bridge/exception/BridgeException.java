f4b0a7b7bc226770a64b00e46bb6a81840df0fc5
sangeun-b <baek0007@algonquinlive.com>
2022-11-22 05:38:23 +0900

    feat: input 테스트 변경

--- src/main/java/bridge/exception/BridgeException.java
+++ src/main/java/bridge/exception/BridgeException.java
@@ -1,14 +1,17 @@
 package bridge.exception;
 
 public class BridgeException {
-    private static final String REGEX = "^[0-9]+";
+    private static final String REGEX_BRIDGE = "^[0-9]+$";
+    private static final String REGEX_MOVING = "^U|D$";
+    private static final String REGEX_RESTART = "^R|Q$";
     public static final String SIZE_FORMAT_ERROR = "[ERROR] 다리 길이는 숫자만 입력 가능합니다.";
     public static final String SIZE_LANGE_ERROR = "[ERROR] 다리 길이는 3부터 20 사이의 숫자여야 합니다.";
     public static final String MOVING_ERROR = "[ERROR] 이동할 칸은 대문자 U와 대문자 D만 입력 가능합니다.";
     public static final String RESTART_ERROR = "[ERROR] R(재시작)과 Q(종료) 중 하나의 문자만 입력가능 합니다.";
+    public static final String BLANK_ERROR = "[ERROR] 값을 입력해야합니다.";
 
     public void validateBridgeSizeFormat(String size) {
-        if(!size.matches(REGEX)) {
+        if(!size.matches(REGEX_BRIDGE)) {
             throw new IllegalArgumentException(SIZE_FORMAT_ERROR);
         }
     }
@@ -19,12 +22,12 @@ public class BridgeException {
     }
 
     public void validateMove(String moving){
-        if(!moving.equals("U")&&!moving.equals("D")){
+        if(!moving.matches(REGEX_MOVING)){
             throw new IllegalArgumentException(MOVING_ERROR);
         }
     }
     public void validateRestart(String restart){
-        if(!restart.equals("R") && !restart.equals("Q")){
+        if(!restart.matches(REGEX_RESTART)){
             throw new IllegalArgumentException(RESTART_ERROR);
         }
     }
--- src/test/java/bridge/InputViewTest.java
+++ src/test/java/bridge/InputViewTest.java
@@ -1,44 +1,71 @@
 package bridge;
 
 import bridge.exception.BridgeException;
+import bridge.view.InputView;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.EmptySource;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
 import org.junit.jupiter.params.provider.ValueSource;
 
+
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class InputViewTest {
-    BridgeException bridgeException = new BridgeException();
-
+    InputView inputView = new InputView();
 
     @DisplayName("다리 길이의 입력값이 3-20 사이가 아니면 예외가 발생한다.")
     @ParameterizedTest
     @ValueSource(strings = {"1", "2", "21", "22"})
     void createBridgeByNotInRange(String size) {
-        assertThatThrownBy(() -> bridgeException.validateBridgeSizeLange(size))
-                .isInstanceOf(IllegalArgumentException.class)
-                .hasMessage("[ERROR] 다리 길이는 3부터 20 사이의 숫자여야 합니다.");
-    }
+        InputStream in = new ByteArrayInputStream(size.getBytes());
+        System.setIn(in);
 
+        assertThatThrownBy(() -> inputView.readBridgeSize())
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(BridgeException.SIZE_LANGE_ERROR);
+    }
+    @DisplayName("다리 길이의 입력값이 숫자가 아니면 예외가 발생한다.")
+    @ParameterizedTest
+    @ValueSource(strings = {"!!!","nope"})
+    void createBridgeByNotNumber(String size){
+        InputStream in = new ByteArrayInputStream(size.getBytes());
+        System.setIn(in);
+
+        assertThatThrownBy(() -> inputView.readBridgeSize())
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(BridgeException.SIZE_FORMAT_ERROR);
+
+    }
 
     @DisplayName("이동할 칸의 입력값이 대문자 U나 대문자 D가 아니면 예외가 발생한다.")
     @ParameterizedTest
-    @ValueSource(strings = {"d", "u", "s", "1", " ", ""})
+    @CsvSource(value = {"d", "u", "s", "1"})
     void checkMovingByNotRequired(String moving) {
-        assertThatThrownBy(() -> bridgeException.validateMove(moving))
+        InputStream in = new ByteArrayInputStream(moving.getBytes());
+        System.setIn(in);
+
+        assertThatThrownBy(() -> inputView.readMoving())
                 .isInstanceOf(IllegalArgumentException.class)
-                .hasMessage("[ERROR] 이동할 칸은 대문자 U와 대문자 D만 입력 가능합니다.");
+                .hasMessage(BridgeException.MOVING_ERROR);
     }
 
 
     @DisplayName("게임 재시작시 입력값이 대문자 R이나 대문자 Q이면 예외가 발생한다.")
     @ParameterizedTest
-    @ValueSource(strings = {"r", "q", "1", " ", ""})
+    @ValueSource(strings = {"r", "q", "1","S","R Q","RQ"})
     void checkRestartByNotRequired(String restart) {
-        assertThatThrownBy(() -> bridgeException.validateRestart(restart))
+        InputStream in = new ByteArrayInputStream(restart.getBytes());
+        System.setIn(in);
+
+        assertThatThrownBy(() -> inputView.readGameCommand())
                 .isInstanceOf(IllegalArgumentException.class)
-                .hasMessage("[ERROR] R(재시작)과 Q(종료) 중 하나의 문자만 입력가능 합니다.");
+                .hasMessage(BridgeException.RESTART_ERROR);
     }
+
 }
