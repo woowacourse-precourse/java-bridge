96b169ce76f23de4628e306c9224654dc6113fdf
heesang <dlagmltkd12@naver.com>
2022-11-21 21:30:32 +0900

    test: 게임 결과 dto 게임 상태 반환 값 확인

---
+++ src/test/java/bridge/dto/GameResultDtoTest.java
@@ -0,0 +1,29 @@
+package bridge.dto;
+
+import bridge.constant.BridgeMark;
+import bridge.constant.GameStatus;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static bridge.dto.GameResultDto.GameRecordDto;
+import static org.assertj.core.api.Assertions.assertThat;
+
+class GameResultDtoTest {
+
+    GameResultDto gameResult;
+
+    @BeforeEach
+    void setUp() {
+        GameRecordDto gameRecord = new GameRecordDto(List.of(BridgeMark.UP, BridgeMark.UP, BridgeMark.DOWN), 3);
+        gameResult = new GameResultDto(GameStatus.SUCCESS, gameRecord);
+    }
+
+    @DisplayName("게임 결과 dto 게임 상태 반환 값 확인")
+    @Test
+    void getGameStatus() {
+        assertThat(gameResult.getGameStatus()).isEqualTo(GameStatus.SUCCESS);
+    }
+}
