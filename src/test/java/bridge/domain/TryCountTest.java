e6ef2c94d43451fc7c818451b362ae19f3be48f1
Yunsik-Choi <choiys0311@gmail.com>
2022-11-21 16:43:37 +0900

    ⛑️ test : TryCount 테스트 작성

--- src/test/java/bridge/domain/TryCountTest.java
+++ src/test/java/bridge/domain/TryCountTest.java
@@ -1,8 +1,9 @@
 package bridge.domain;
 
-import static org.assertj.core.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertAll;
 
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -18,7 +19,7 @@ public class TryCountTest {
     @DisplayName("시도 횟수 범위 검증")
     @Test
     void validateTryCountRange() {
-        Assertions.assertAll(
+        assertAll(
                 () -> assertThatThrownBy(() -> new TryCount(0)).isInstanceOf(IllegalArgumentException.class),
                 () -> assertThat(new TryCount()).isEqualTo(new TryCount(1)),
                 () -> assertThat(new TryCount(2_000_000_000)).isEqualTo(new TryCount(2_000_000_000)),
@@ -30,5 +31,14 @@ public class TryCountTest {
     @Test
     void plusOne() {
         assertThat(new TryCount(3).plusOne()).isEqualTo(new TryCount(4));
+    }
+
+    @DisplayName("시도 횟수 값을 반환한다.")
+    @Test
+    void count() {
+        assertAll(
+                () -> assertThat(new TryCount().count()).isEqualTo(1),
+                () -> assertThat(new TryCount(4).count()).isEqualTo(4)
+        );
     }
 }
