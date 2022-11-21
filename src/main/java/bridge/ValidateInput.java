3af75175731b02967c8c0646df00b613fe01380b
gyumin159 <gyumin159@naver.com>
2022-11-21 14:02:50 +0900

    style : 메소드별 설명 추가

--- src/main/java/bridge/ValidateInput.java
+++ src/main/java/bridge/ValidateInput.java
@@ -4,7 +4,7 @@ import java.util.regex.Pattern;
 
 public class ValidateInput {
     private enum regexPatterns{
-        ONLY_DIGIT("^[0-9]+{1,2}$"),
+        ONLY_DIGIT("^[0-9]+$"),
         U_OR_D("^[UD]$"),
         R_OR_Q("^[RQ]$");
 
@@ -28,6 +28,11 @@ public class ValidateInput {
         }
     }
 
+    /**
+     *
+     * @param rawInput 사용자의 입력값
+     * @return 입력이 숫자로만 이루어졌다면 true, 아닐시 IllegalArgumentException 에러
+     */
     public static boolean isAllDigit(String rawInput){
         boolean isDigit = Pattern.matches(regexPatterns.ONLY_DIGIT.regex, rawInput);
         if (!isDigit) {
@@ -36,6 +41,11 @@ public class ValidateInput {
         return true;
     }
 
+    /**
+     *
+     * @param rawInput 사용자의 입력값
+     * @return 입력이 3이상 20이하의 숫자라면 true, 아닐시 IllegalArgumentException 에러
+     */
     public static boolean isInRange(String rawInput) { // "0004" 이런 입력은 올바른 입력으로 통과됨.
         int target = Integer.parseInt(rawInput);
         if (target < 3 || target > 20) {
@@ -44,6 +54,11 @@ public class ValidateInput {
         return true;
     }
 
+    /**
+     *
+     * @param rawInput 사용자의 입력값
+     * @return 입력이 "U" 또는 "D" 라면 true, 아닐시 IllegalArgumentException 에러
+     */
     public static boolean isUorD(String rawInput){
         boolean isUorD = Pattern.matches(regexPatterns.U_OR_D.regex, rawInput);
         if (!isUorD){
@@ -52,6 +67,11 @@ public class ValidateInput {
         return true;
     }
 
+    /**
+     *
+     * @param rawInput 사용자의 입력값
+     * @return 입력이 "R" 또는 "Q" 라면 true, 아닐시 IllegalArgumentException 에러
+     */
     public static boolean isRorQ(String rawInput){
         boolean isRorQ = Pattern.matches(regexPatterns.R_OR_Q.regex, rawInput);
         if (!isRorQ){
