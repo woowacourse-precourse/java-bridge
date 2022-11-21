0b76a3a674fe035a6eb8a00e47f88ea66dd34638
alpacaHyun <flaehdan@gmail.com>
2022-11-21 10:10:44 +0900

    type : feat ✨ 랜덤 다리 생성
    body :

--- src/main/java/bridge/controller/BridgeController.java
+++ src/main/java/bridge/controller/BridgeController.java
@@ -1,4 +1,33 @@
 package bridge.controller;
 
+import bridge.model.BridgeMaker;
+import bridge.model.BridgeRandomNumberGenerator;
+import bridge.view.InputView;
+import bridge.view.OutputView;
+import java.util.ArrayList;
+import java.util.List;
+
 public class BridgeController {
+
+    InputView inputview = new InputView();
+    OutputView outputView = new OutputView();
+    private BridgeMaker bridgeMaker;
+
+    private List<String> randomBridge;
+    public void startGame(){
+        outputView.printGameStartMessage();
+
+        makeBridge();
+
+
+    }
+
+    public List<String> makeBridge() {
+        bridgeMaker = new BridgeMaker(new BridgeRandomNumberGenerator());
+        int size = inputview.readBridgeSize();
+        randomBridge = bridgeMaker.makeBridge(size);
+
+        return randomBridge;
+    }
+
 }
--- src/main/java/bridge/main/Application.java
+++ src/main/java/bridge/main/Application.java
@@ -1,10 +1,17 @@
 package bridge.main;
 
+import bridge.controller.BridgeController;
+import bridge.model.BridgeMaker;
+import bridge.model.BridgeRandomNumberGenerator;
 import camp.nextstep.edu.missionutils.Console;
+import java.util.Map;
 
 public class Application {
 
     public static void main(String[] args) {
+
+        BridgeController bridgeController = new BridgeController();
+        bridgeController.startGame();
 
     }
 }
