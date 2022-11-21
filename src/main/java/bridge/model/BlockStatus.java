863ebbde48ba215152d36bac07802a7083ee2410
김수민(Sumin Kim) <sumin.kim.develop@gmail.com>
2022-11-22 02:18:58 +0900

    refactor(BlockStatus): 필요없는 변수 삭제

--- src/main/java/bridge/model/BlockStatus.java
+++ src/main/java/bridge/model/BlockStatus.java
@@ -1,24 +1,17 @@
 package bridge.model;
 
-import java.util.Arrays;
-
 public enum BlockStatus {
-    O(true, " O "), X(false, " X "), UNSELECT(null, "   ");
+    O(" O "), X(" X "), UNSELECT("   ");
 
-    private final Boolean correct;
     private final String status;
 
-    BlockStatus(Boolean correct, String status) {
-        this.correct = correct;
+    BlockStatus(String status) {
         this.status = status;
     }
 
     public static BlockStatus of(boolean correct) {
-        return Arrays.stream(BlockStatus.values())
-                .filter(blockStatus -> blockStatus.correct != null)
-                .filter(blockStatus -> blockStatus.correct.equals(correct))
-                .findAny()
-                .orElse(UNSELECT);
+        if (correct) return O;
+        return X;
     }
 
     @Override
