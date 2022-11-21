d50599604a4ed4feaac49d4fb90880734646fd18
starwook <star.wookk@gmail.com>
2022-11-21 19:34:12 +0900

    docs(ErrorResource.java): Add ErrorResource.java
    에러 출력을 위한 static 변수들 추가
    input Value에 대한 것

---
+++ src/main/java/bridge/ErrorResource.java
@@ -0,0 +1,12 @@
+package bridge;
+
+public class ErrorResource {
+
+    public static final String ERROR_START="[ERROR] ";
+    public static final String INPUT_BRIDGE_NUMBER_WRONG = "다리 길이는 3부터 20사이의 숫자여야 합니다.";
+    public static final String INPUT_NOT_BRIDGE_NUMBER = "다리 길이는 숫자여야합니다.";
+    public static final String INPUT_MOVE_WRONG_FRONT = "이동할 칸은 ";
+    public static final String OR = "또는";
+    public static final String INPUT_SHOULD_BACK = "여야 합니다.";
+    public static final String INPUT_REPLAY_FRONT ="게임 재시도 여부는 ";
+}
