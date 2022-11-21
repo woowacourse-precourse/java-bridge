2574a865034fe797cadf86188d640d0cde125c3d
thousandcows <milcamboo@gmail.com>
2022-11-21 17:30:12 +0900

    feat: create validateBridgeSize method

--- src/main/java/validation/InputValidation.java
+++ src/main/java/validation/InputValidation.java
@@ -1,5 +1,9 @@
 package validation;
 
 public class InputValidation {
-
+    public void validateBridgeSize(String input) {
+        if (!input.matches("^[1-9]+[0-9]*$")) {
+            throw new IllegalArgumentException(String.valueOf(ErrorMessage.INVALID_BRIDGE_SIZE));
+        }
+    }
 }
