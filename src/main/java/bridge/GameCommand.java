8039037b533c3657d093b1e2ccd5982b8efeebda
Amyhds <dasomhan524@gmail.com>
2022-11-21 17:46:49 +0900

    feat(gamecommand): 생성자 구현 및 명령어 반환 메서드 구현

--- src/main/java/bridge/GameCommand.java
+++ src/main/java/bridge/GameCommand.java
@@ -1,4 +1,15 @@
 package bridge;
 
 public class GameCommand {
+    private String gameCommand;
+
+    public GameCommand(String userGameCommand) {
+        Validator.validateLength(userGameCommand);
+        Validator.validateGameCommand(userGameCommand);
+        gameCommand = userGameCommand;
+    }
+
+    public String getGameCommand() {
+        return gameCommand;
+    }
 }
