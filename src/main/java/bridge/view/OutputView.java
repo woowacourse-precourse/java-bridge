976947a969a98405ecd41ad9c5ead61ddca7b7a3
hongjinwon <herenever@naver.com>
2022-11-22 05:42:14 +0900

    feat(view): OutputView 기능 구현

--- src/main/java/bridge/view/OutputView.java
+++ src/main/java/bridge/view/OutputView.java
@@ -1,5 +1,14 @@
 package bridge.view;
 
+import bridge.model.Result;
+import bridge.model.Step;
+import bridge.utils.GameMessage;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static bridge.utils.GameMessage.*;
+
 /**
  * 사용자에게 게임 진행 상황과 결과를 출력하는 역할을 한다.
  */
@@ -10,7 +19,11 @@ public class OutputView {
      * <p>
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void printMap() {
+    public void printMap(List<Step> steps) {
+        List<String> upBridge = new ArrayList<>();
+        List<String> downBridge = new ArrayList<>();
+        makeUpAndDownBridge(steps,upBridge,downBridge);
+        System.out.println(convertBridgeToMessage(upBridge,downBridge));
     }
 
     /**
@@ -18,6 +31,57 @@ public class OutputView {
      * <p>
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void printResult() {
+    public void printResult(Result result) {
+        List<String> upBridge = new ArrayList<>();
+        List<String> downBridge = new ArrayList<>();
+        System.out.println(NEW_LINE);
+        System.out.println(RESULT_HEADER);
+        makeUpAndDownBridge(result.getSteps(),upBridge,downBridge);
+        System.out.println(convertBridgeToMessage(upBridge,downBridge));
+        System.out.println(NEW_LINE);
+        System.out.println(String.format(RESULT_IS_SUCCESS.message,result.getIsSuccess()));
+        System.out.println(String.format(RESULT_TOTAL_TRIAL.message,result.getTrial()));
+    }
+
+    public void printStart() {
+        System.out.println(GAME_START);
+    }
+
+    public void printBridgeSize() {
+        System.out.println(NEW_LINE);
+        System.out.println(BRIDGE_MAKER);
+    }
+
+    public void printMoving() {
+        System.out.println(NEW_LINE);
+        System.out.println(MOVING);
+    }
+
+    public void printRetry() {
+        System.out.println(NEW_LINE);
+        System.out.println(RETRY);
+    }
+
+    public void printErrorMessage(String message){
+        System.out.println(message);
+    }
+
+    private void makeUpAndDownBridge(List<Step> steps,List<String> upBridge,List<String> downBridge){
+        for (Step step : steps) {
+            if(step.name().contains("UP")){
+                upBridge.add(step.value);
+                downBridge.add("   ");
+            }
+            if(step.name().contains("DOWN")){
+                upBridge.add("   ");
+                downBridge.add(step.value);
+            }
+        }
+    }
+
+    private String convertBridgeToMessage(List<String> upBridge,List<String> downBridge){
+        String upBridgeMessage = "[" + String.join("|",upBridge) + "]";
+        String downBridgeMessage = "[" + String.join("|",downBridge) + "]";
+        return upBridgeMessage + "\n" + downBridgeMessage;
     }
 }
