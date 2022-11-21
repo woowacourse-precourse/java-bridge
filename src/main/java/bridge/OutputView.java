a53001d965795db5d53f52dfb44f78caed591785
Eun-chan Cho <eunchan1001@gmail.com>
2022-11-21 19:50:51 +0900

    feat(OutputView): implement printMap with MapGenerator

--- src/main/java/bridge/OutputView.java
+++ src/main/java/bridge/OutputView.java
@@ -1,16 +1,24 @@
 package bridge;
 
+import java.util.List;
+
 /**
  * 사용자에게 게임 진행 상황과 결과를 출력하는 역할을 한다.
  */
 public class OutputView {
+
 
     /**
      * 현재까지 이동한 다리의 상태를 정해진 형식에 맞춰 출력한다.
      * <p>
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void printMap() {
+    public void printMap(Result result) {
+        MapGenerator mapGenerator = new MapGenerator();
+        List<String> map = mapGenerator.generateMap(result);
+        for (String row : map) {
+            System.out.println(row);
+        }
     }
 
     /**
@@ -18,6 +26,5 @@ public class OutputView {
      * <p>
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void printResult() {
-    }
+    public void printResult() {}
 }
