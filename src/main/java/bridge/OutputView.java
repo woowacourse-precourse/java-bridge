eddf84734f4de20b1e549e31d78ddc2878819b90
pdot715 <pdot715@gmail.com>
2022-11-21 22:28:28 +0900

    feat(OutputView): 메세지 출력 구현

--- src/main/java/bridge/OutputView.java
+++ src/main/java/bridge/OutputView.java
@@ -5,12 +5,16 @@ package bridge;
  */
 public class OutputView {
 
+    public static final String MESSAGE_PREFIX_GAME_RESULT = "게임 성공 여부: ";
+    public static final String MESSAGE_PREFIX_ROUND_COUNT = "총 시도한 횟수: ";
+
     /**
      * 현재까지 이동한 다리의 상태를 정해진 형식에 맞춰 출력한다.
      * <p>
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void printMap() {
+    public static void printMap() {
+
     }
 
     /**
@@ -18,6 +22,12 @@ public class OutputView {
      * <p>
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void printResult() {
+    public static void printResult() {
     }
+
+    public static void printMessage(String message) {
+        System.out.println(message);
+    }
+
+
 }
