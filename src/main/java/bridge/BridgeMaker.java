6e921b6c88dd10b3f43719f117c1bead6a6eb75a
고동우 <shp05179@gmail.com>
2022-11-21 18:23:23 +0900

    feat: 입력된 길이의 다리를 랜덤으로 생성하는 기능
    입력받은 숫자만큼 반복문 돌면서
    bridgeNumberGenerator의 generate수행하고,
    수행한 값이 1이면 U 0이면 D를 추가해 다리를 생성한다.

--- src/main/java/bridge/BridgeMaker.java
+++ src/main/java/bridge/BridgeMaker.java
@@ -1,5 +1,6 @@
 package bridge;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -13,11 +14,22 @@ public class BridgeMaker {
         this.bridgeNumberGenerator = bridgeNumberGenerator;
     }
 
+    private String getRandomDirection() {
+        if (bridgeNumberGenerator.generate() == 1) {
+            return "U";
+        }
+        return "D";
+    }
+
     /**
      * @param size 다리의 길이
      * @return 입력받은 길이에 해당하는 다리 모양. 위 칸이면 "U", 아래 칸이면 "D"로 표현해야 한다.
      */
     public List<String> makeBridge(int size) {
-        return null;
+        List<String> bridge = new ArrayList<>();
+        for (int i = 0; i < size; i++) {
+            bridge.add(getRandomDirection());
+        }
+        return bridge;
     }
 }
