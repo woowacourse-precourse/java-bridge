333bada48ec2784fb5e66aa647b5ebd1b504b2f9
dong young <dong5854@gmail.com>
2022-11-22 08:21:40 +0900

    test: 위아래 입력의 예외 발생시 재입력 테스트

--- src/test/java/bridge/ApplicationTest.java
+++ src/test/java/bridge/ApplicationTest.java
@@ -60,6 +60,24 @@ class ApplicationTest extends NsTest {
         });
     }
 
+    @Test
+    void 위아래_재입력_테스트() {
+        assertRandomNumberInRangeTest(() -> {
+            run("3", "Y", "U", "P", "D", "U");
+            assertThat(output()).contains(
+                    "최종 게임 결과",
+                    "[ O |   | O ]",
+                    "[   | O |   ]",
+                    "게임 성공 여부: 성공",
+                    "총 시도한 횟수: 1"
+            );
+
+            int upSideIndex = output().indexOf("[ O |   | O ]");
+            int downSideIndex = output().indexOf("[   | O |   ]");
+            assertThat(upSideIndex).isLessThan(downSideIndex);
+        }, 1, 0, 1);
+    }
+
     @Override
     protected void runMain() {
         Application.main(new String[]{});
