742d80c21880b8dbdb4c5a8dee821b27639b3f30
josolha <josolha@nate.com>
2022-11-22 04:34:37 +0900

    style(BridgeMaker) : 의미없는 공백 지움

--- src/main/java/bridge/BridgeMaker.java
+++ src/main/java/bridge/BridgeMaker.java
@@ -21,9 +21,7 @@ public class BridgeMaker {
     public List<String> makeBridge(int size) {
         List<String> bridge = new ArrayList<>();
         for (int i = 0; i < size; i++) {
-
             int generate = bridgeNumberGenerator.generate();
-
             if (generate == 0) {
                 bridge.add("D");
             }
