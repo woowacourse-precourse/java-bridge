8d2e829e1530ca88bec58f2dc9f452f09f690717
wonju-dev <wonjulee.dev@gmail.com>
2022-11-22 03:18:38 +0900

    refactor(BridgeGame): 클래스 필드 추가, move 리펙토링
    클래스에 필드가 추가되었습니다. guessBridge는 사용자 입력으로
    구성된 bridge입니다.

--- src/main/java/bridge/BridgeGame.java
+++ src/main/java/bridge/BridgeGame.java
@@ -1,16 +1,55 @@
 package bridge;
 
+import bridge.enums.MoveResult;
+
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * 다리 건너기 게임을 관리하는 클래스
+ * 제공된 BridgeGame 클래스를 활용해 구현해야 한다.
+ * BridgeGame에 필드(인스턴스 변수)를 추가할 수 있다.
+ * BridgeGame의 패키지는 변경할 수 있다.
+ * BridgeGame의 메서드의 이름은 변경할 수 없고, 인자와 반환 타입은 필요에 따라 추가하거나 변경할 수 있다.
+ * 게임 진행을 위해 필요한 메서드를 추가 하거나 변경할 수 있다.
  */
 public class BridgeGame {
+
+    private List<String> answerBridge;
+    private List<String> guessBridge;
+    private Integer tryCount;
+
+    public Integer getTryCount() {
+        return tryCount;
+    }
+
+    public BridgeGame(int size) {
+        this.answerBridge = new BridgeMaker(new BridgeRandomNumberGenerator()).makeBridge(size);
+        this.guessBridge = new ArrayList<>();
+        this.tryCount = 1;
+    }
 
     /**
      * 사용자가 칸을 이동할 때 사용하는 메서드
      * <p>
      * 이동을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void move() {
+    public MoveResult move(String userDirection) {
+        guessBridge.add(userDirection);
+
+        if (isValidDirection()) {
+            return MoveResult.ANSWER;
+        }
+
+        return MoveResult.NOT_ANSWER;
+    }
+
+    private boolean isValidDirection() {
+        Integer lastGuessIndex = guessBridge.size() - 1;
+        String answer = answerBridge.get(lastGuessIndex);
+        String guess = guessBridge.get(lastGuessIndex);
+
+        return answer.equals(guess);
     }
 
     /**
