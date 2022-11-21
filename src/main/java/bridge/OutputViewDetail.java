576381c5a133d609aff9422a06aaa152964369ae
= <kimje1109@gmail.com>
2022-11-21 22:52:11 +0900

    feat: 게임 진행관련 출력 문구 추가

--- src/main/java/bridge/OutputViewDetail.java
+++ src/main/java/bridge/OutputViewDetail.java
@@ -6,7 +6,14 @@ public enum OutputViewDetail {
     NONE("   "),
     START_LINE("["),
     MIDDLE_LINE("|"),
-    END_LINE("]");
+    END_LINE("]"),
+    START_GAME("다리 건너기 게임을 시작합니다."),
+    INPUT_BRIDGE_SIZE("다리의 길이를 입력해주세요."),
+    INPUT_MOVING("이동할 칸을 선택해주세요. (위: U, 아래: D)"),
+    RESTART_OR_QUIT("게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)"),
+    GAME_RESULT("최종 게임 결과"),
+    SUCCESS_OR_FAIL("게임 성공 여부: %s"),
+    TRY_NUMBER("총 시도한 횟수: %d");
 
     private final String message;
 
