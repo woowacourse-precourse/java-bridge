94ec59d4dde0cac3df6842aa499e984b1b0b5d48
Yerim lee <103623292+lyrWinterCat@users.noreply.github.com>
2022-11-21 13:36:24 +0900

    test (ValidatorTest) : 다리 길이 검증 테스트 수정

--- src/test/java/bridge/domain/ValidatorTest.java
+++ src/test/java/bridge/domain/ValidatorTest.java
@@ -14,16 +14,16 @@ class ValidatorTest {
     public void bridgeSizeValidator() throws Exception{
         //given
         String bridgeSize1 = "3abc";
-        String bridgeSize2 = "2";
+        String bridgeSize2 = "-2";
 
         //then
         assertThatThrownBy(()->validator.validateBridgeSize(bridgeSize1))
                 .isInstanceOf(IllegalArgumentException.class)
-                .hasMessageContaining("[ERROR] 다리 길이는 숫자로 입력 가능합니다.");
+                .hasMessageContaining("[ERROR] 다리 길이는 3 이상 20 이하인 숫자로 입력 가능합니다.");
 
         assertThatThrownBy(()->validator.validateBridgeSize(bridgeSize2))
                 .isInstanceOf(IllegalArgumentException.class)
-                .hasMessageContaining("[ERROR] 다리 길이는 3 이상 20 이하로 입력해야 합니다.");
+                .hasMessageContaining("[ERROR] 다리 길이는 3 이상 20 이하인 숫자로 입력 가능합니다.");
 
     }
 
