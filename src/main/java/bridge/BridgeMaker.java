5e52df13dbaf2b081cd36f50c7245d6a59a07336
choijoohee213 <choijoohee@naver.com>
2022-11-22 00:23:28 +0900

    feat: 다리 길이 입력받아 랜덤으로 다리 칸 생성 구현

--- src/main/java/bridge/BridgeMaker.java
+++ src/main/java/bridge/BridgeMaker.java
@@ -1,5 +1,13 @@
 package bridge;
 
+import static bridge.constant.Constant.MAX_BRIDGE_SIZE;
+import static bridge.constant.Constant.MIN_BRIDGE_SIZE;
+import static bridge.constant.ErrorMessage.BRIDGE_SIZE_IS_NOT_IN_RANGE;
+import static bridge.constant.ErrorMessage.BRIDGE_SIZE_IS_NOT_NUMBER;
+
+import bridge.view.InputView;
+import bridge.view.OutputView;
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -13,11 +21,46 @@ public class BridgeMaker {
         this.bridgeNumberGenerator = bridgeNumberGenerator;
     }
 
+    public int inputBridgeSize() {
+        int bridgeSize;
+        while (true) {
+            OutputView.printRequestBridgeSize();
+            String bridgeSizeInput = InputView.readBridgeSize();
+            try {
+                validateIsNumber(bridgeSizeInput);
+                bridgeSize = Integer.parseInt(bridgeSizeInput);
+                validateBridgeSize(bridgeSize);
+            } catch (IllegalArgumentException exception) {
+                OutputView.printErrorMessage(exception);
+                continue;
+            }
+            break;
+        }
+        return bridgeSize;
+    }
+
     /**
      * @param size 다리의 길이
      * @return 입력받은 길이에 해당하는 다리 모양. 위 칸이면 "U", 아래 칸이면 "D"로 표현해야 한다.
      */
     public List<String> makeBridge(int size) {
-        return null;
+        List<String> bridgeNumbers = new ArrayList<>();
+        while (bridgeNumbers.size() < size) {
+            int randomValue = bridgeNumberGenerator.generate();
+            bridgeNumbers.add(BridgeNumber.getMark(randomValue));
+        }
+        return bridgeNumbers;
+    }
+
+    private void validateIsNumber(String bridgeSize) {
+        if (!bridgeSize.chars().allMatch(letter -> Character.isDigit((char) letter))) {
+            throw new IllegalArgumentException(BRIDGE_SIZE_IS_NOT_NUMBER);
+        }
+    }
+
+    private void validateBridgeSize(int bridgeSize) throws IllegalArgumentException {
+        if (bridgeSize < MIN_BRIDGE_SIZE || bridgeSize > MAX_BRIDGE_SIZE) {
+            throw new IllegalArgumentException(BRIDGE_SIZE_IS_NOT_IN_RANGE);
+        }
     }
 }
