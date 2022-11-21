fd7074e46d9b30999eb4814f7096cc35c9dbaa4c
bjk1649 <qhwns1649@naver.com>
2022-11-22 01:42:43 +0900

    feat: 주어진 조건에 맞게 게임의 흐름을 제어하는 클래스 구현

---
+++ src/main/java/bridge/controller/BridgeGameController.java
@@ -0,0 +1,34 @@
+package bridge.controller;
+
+import bridge.bo.Bridge;
+import bridge.service.BridgeGame;
+import bridge.view.InputView;
+import bridge.view.OutputView;
+
+public class BridgeGameController {
+    private final InputView inputView;
+    private final OutputView outputView;
+    private final BridgeGame bridgeGame;
+
+    public BridgeGameController(Bridge bridge) {
+        this.bridgeGame = new BridgeGame(bridge);
+        this.inputView = new InputView();
+        this.outputView = new OutputView();
+    }
+
+    public void doGame() {
+        while (bridgeGame.getBridgeIterator().hasNext()) {
+            String answerMove = bridgeGame.getBridgeIterator().next();
+            String userMove = inputView.readMoving();
+            boolean moveResult = bridgeGame.move(userMove, answerMove);
+            outputView.printMap(bridgeGame);
+            if (!moveResult) {
+                handleFailedMove();
+            }
+        }
+        outputView.printResult(bridgeGame);
+    }
+
+    private void handleFailedMove() {
+    }
+}
