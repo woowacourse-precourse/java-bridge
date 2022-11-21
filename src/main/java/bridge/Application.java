31b51718526331f2cd28b293da5b1cb90b247ffb
eil-you <qldirl123@gmail.com>
2022-11-21 17:50:13 +0900

    feat(printResult): - 최종 결과 출력

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -22,16 +22,45 @@ public class Application {
         int num = 0;
         int moving =1;
 
+        int gameCount = 1;
+
         BridgeGame bridgeGame = new BridgeGame();
         OutputView outputView = new OutputView();
-        while(moving != 0) {
+        System.out.println("size"+bridge.size());
+        while((moving != 0)) {
+            System.out.println("num"+num);
             String move = inputView.readMoving();
             moving = bridgeGame.move(bridge.get(num), move);
 
 
             outputView.printMap(bridge.subList(0, num), move, moving);
+            if(bridge.size() == num+1){
+                break;
+
+            }
             num++;
         }
+        outputView.printResult("성공", gameCount);
+        // 재시작을 위해
+        String command = inputView.readGameCommand();
+        if(command.equals("R")){
+            gameCount++;
+            num=0;
+            moving = 1;
+
+            //bridgeGame.retry();
+
+            while(moving != 0) {
+                String move = inputView.readMoving();
+                moving = bridgeGame.move(bridge.get(num), move);
+
+
+                outputView.printMap(bridge.subList(0, num), move, moving);
+                num++;
+            }
+        }
+
+        outputView.printResult("실패", gameCount);
 
     }
 
--- src/main/java/bridge/InputView.java
+++ src/main/java/bridge/InputView.java
@@ -23,6 +23,7 @@ public class InputView {
             validSize(bridgeSize);
         } catch (IllegalArgumentException e) {
             System.out.println("[ERROR] 다리 길이는 3부터 20 사이의 숫자여야 합니다.");
+
         }
         return bridgeSize;
     }
