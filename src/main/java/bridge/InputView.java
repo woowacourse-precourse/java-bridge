2a33da57ab55ac68cf06bc86adfc07c78713f695
gayeonP <pagaye@naver.com>
2022-11-21 17:25:15 +0900

    [InputView] 각 함수마다 validation 추가

--- src/main/java/bridge/InputView.java
+++ src/main/java/bridge/InputView.java
@@ -9,13 +9,25 @@ public class InputView {
     public static final String BRIDGE_SIZE_INPUT_MESSAGE = "다리의 길이를 입력해주새요.";
     public static final String MOVE_INPUT_MESSAGE = "이동할 칸을 선택해주세요. (위: U, 아래: D)";
     public static final String GAME_RETRY_INPUT_MESSAGE = "게임을 다시 시도할지 여부를 입력해주세여. (재시도: R, 종료: Q)";
+    public static final int BRIDGE_SIZE_RANGE1 = 3;
+    public static final int BRIDGE_SIZE_RANGE2 = 20;
+    public static final String USER_MOVE_UP = "U";
+    public static final String USER_MOVE_DOWN = "D";
+    public static final String USER_RETRY = "R";
+    public static final String USER_OVER = "Q";
+
+    private Validation validation = new Validation();
 
     /**
      * 다리의 길이를 입력받는다.
      */
     public int readBridgeSize() {
         System.out.println(BRIDGE_SIZE_INPUT_MESSAGE);
-        return Integer.valueOf(Console.readLine());
+        int input = Integer.valueOf(Console.readLine());
+        if (!validation.SizeRangeValidate(input, BRIDGE_SIZE_RANGE1, BRIDGE_SIZE_RANGE2)) {
+            throw new IllegalArgumentException();
+        }
+        return input;
     }
 
     /**
@@ -23,7 +35,11 @@ public class InputView {
      */
     public String readMoving() {
         System.out.println(MOVE_INPUT_MESSAGE);
-        return Console.readLine();
+        String input = Console.readLine();
+        if (!validation.CharValidate(input, USER_MOVE_UP, USER_MOVE_DOWN)) {
+            throw new IllegalArgumentException();
+        }
+        return input;
     }
 
     /**
@@ -31,6 +47,10 @@ public class InputView {
      */
     public String readGameCommand() {
         System.out.println(GAME_RETRY_INPUT_MESSAGE);
-        return Console.readLine();
+        String input = Console.readLine();
+        if (!validation.CharValidate(input, USER_RETRY, USER_OVER)) {
+            throw new IllegalArgumentException();
+        }
+        return input;
     }
 }
