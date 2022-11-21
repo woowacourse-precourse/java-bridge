5aa25da065195c59217aa2f273f24653847954ca
Gyu-Bin-Yoon <gyubinyoon99@gmail.com>
2022-11-22 00:33:50 +0900

    refactor(BridgeGame) : change parameter at checkAnswer

--- src/main/java/bridge/BridgeGame.java
+++ src/main/java/bridge/BridgeGame.java
@@ -9,13 +9,23 @@ public class BridgeGame {
 
     private final String PASS = "O";
     private final String FAIL = "X";
+    private final OutputView outputView = new OutputView();
+    private final InputView inputView = new InputView();
+    private int bridgeSize;
+    private int currentRound;
 
+    public void firstSetting(){
+        outputView.printStartMessage();
+        outputView.printBridgeSizeMessage();
+        this.bridgeSize = inputView.readBridgeSize();
+    }
     /**
      * 사용자가 칸을 이동할 때 사용하는 메서드
      * <p>
      * 이동을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
     public void move() {
+
     }
 
     /**
@@ -26,8 +36,8 @@ public class BridgeGame {
     public void retry() {
     }
 
-    public String checkAnswer(List<String> bridge, String selectedBridge, int round) {
-        String answer = bridge.get(round - 1);
+    public String checkAnswer(List<String> bridge, String selectedBridge) {
+        String answer = bridge.get(currentRound - 1);
         if (answer.equals(selectedBridge)) {
             return PASS;
         }
