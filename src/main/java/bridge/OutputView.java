8f60e319d57dfcbbcfb06d74962e0d5a2ca3084c
BaeDongHyeon <sasd730@gmail.com>
2022-11-21 21:08:10 +0900

    feat: 출력 메세지 메소드 추가

--- src/main/java/bridge/OutputView.java
+++ src/main/java/bridge/OutputView.java
@@ -5,6 +5,27 @@ package bridge;
  */
 public class OutputView {
 
+    public void printNewLine() {
+        System.out.println();
+    }
+
+    public void gameStartMessage() {
+        System.out.println("다리 건너기 게임을 시작합니다.");
+        printNewLine();
+    }
+
+    public void inputBridgeSizeMessage() {
+        System.out.println("다리의 길이를 입력해주세요.");
+    }
+
+    public void inputMoveCommandMessage() {
+        System.out.println("이동할 칸을 선택해주세요. (위: U, 아래: D)");
+    }
+
+    public void inputRetryMessage() {
+        System.out.println("게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)");
+    }
+
     /**
      * 현재까지 이동한 다리의 상태를 정해진 형식에 맞춰 출력한다.
      * <p>
