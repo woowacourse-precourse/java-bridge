0a6e66deabd20185d1e19a4272ba53c187741e69
baksohyeon <bori12370@gmail.com>
2022-11-21 20:27:40 +0900

    feat: 입력 값 핸들링 클래스 기능 구현

--- src/main/java/bridge/service/InputHandlerImpl.java
+++ src/main/java/bridge/service/InputHandlerImpl.java
@@ -1,2 +1,62 @@
-package bridge.service;public class InputHandlerImpl {
+package bridge.service;
+
+import bridge.domain.ErrorMessageConstant;
+import bridge.domain.InputHandler;
+
+public class InputHandlerImpl implements InputHandler {
+
+
+
+    @Override
+    public int getBridgeSize(String rawInput) {
+        String refineInput = removeSpacesFromString(rawInput);
+        int size = parseInteger(refineInput);
+        validateBridgeLength(size);
+        return size;
+    }
+
+    @Override
+    public String getMovingCommand(String rawInput) {
+        String refineCommand = removeSpacesFromString(rawInput);
+        validateMoving(refineCommand);
+        return refineCommand;
+    }
+
+    @Override
+    public String getGameCommand(String rawInput) {
+        String refineCommand = removeSpacesFromString(rawInput);
+        validateGameCommand(refineCommand);
+        return refineCommand;
+    }
+
+    private int parseInteger(String rawInput) {
+        try {
+            return Integer.parseInt(rawInput);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException(ErrorMessageConstant.NOT_INTEGER);
+        }
+    }
+
+    private String removeSpacesFromString(String rawInput) {
+        return rawInput.replaceAll("\\s+","");
+    }
+
+    private void validateBridgeLength(int length) {
+        if (length < 2 || length > 20) {
+            throw new IllegalArgumentException(ErrorMessageConstant.INVALID_BRIDGE_LENGTH);
+        }
+    }
+
+    void validateMoving(String command) {
+        if (command.matches("^[UD]$")) {
+            throw new IllegalArgumentException(ErrorMessageConstant.INVALID_MOVING_COMMAND);
+        }
+    }
+
+
+    void validateGameCommand(String command) {
+        if (command.matches("^[RQ]$")) {
+            throw new IllegalArgumentException(ErrorMessageConstant.INVALID_GAME_OVER_CONTROL_COMMAND);
+        }
+    }
 }
