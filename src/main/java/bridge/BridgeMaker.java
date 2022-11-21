6584fb2959a5deca396142b9c81a0f85aced160d
MJoon-Jung <gjgjajaj31@gmail.com>
2022-11-21 19:20:29 +0900

    style: 클래스와 인스턴스 변수 간의 줄 간격 삭제

--- src/main/java/bridge/BridgeMaker.java
+++ src/main/java/bridge/BridgeMaker.java
@@ -9,7 +9,6 @@ import java.util.stream.IntStream;
  * 다리의 길이를 입력 받아서 다리를 생성해주는 역할을 한다.
  */
 public class BridgeMaker {
-
     private final BridgeNumberGenerator bridgeNumberGenerator;
 
     public BridgeMaker(BridgeNumberGenerator bridgeNumberGenerator) {
