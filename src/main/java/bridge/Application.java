ed800d01e1137f2a808e4e1ec0f2406a2eba0ce2
JangAJang <99702271+JangAJang@users.noreply.github.com>
2022-11-21 09:29:43 +0900

    feat: Application에서 BridgeGame으로 다리게임 실행 로직 생성

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -4,5 +4,13 @@ public class Application {
 
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        BridgeGame bridgeGame = new BridgeGame();
+        while(true){
+            bridgeGame.move();
+            if(bridgeGame.isGameDone()) break;
+            if(bridgeGame.isGameQuit())break;
+            bridgeGame.retry();
+        }
+        bridgeGame.stop();
     }
 }
