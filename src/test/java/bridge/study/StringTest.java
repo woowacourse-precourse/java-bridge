fe98e08c36a53fe29b311beb7ab20b9b9a5ce924
Guga <103228463+BGuga@users.noreply.github.com>
2022-11-21 16:04:35 +0900

    test: String.format 인자값 테스트

---
+++ src/test/java/bridge/study/StringTest.java
@@ -0,0 +1,19 @@
+package bridge.study;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class StringTest {
+
+    @Test
+    @DisplayName("String.format은 StringBuilder를 인자로 받을 수 있다")
+    void String_format_인자값_테스트() {
+        StringBuilder test = new StringBuilder();
+        test.append("test");
+
+        assertThat(String.format("[%s]",test)).isEqualTo("[test]");
+    }
+}
