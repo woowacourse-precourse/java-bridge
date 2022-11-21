74e7ce60886e5bb6f36201c5d9181d653ef6a761
JunHyeon <dlwnsgus07@naver.com>
2022-11-21 17:34:12 +0900

    feat: 사용자가 잘못된 입력을 하였을때 에러를 발생시키고 다시 입력받는 기능 추가

--- src/main/java/Controller/BridgeGameController.java
+++ src/main/java/Controller/BridgeGameController.java
@@ -5,39 +5,62 @@ import view.InputView;
 import view.OutputView;
 
 public class BridgeGameController {
+    private static final String ERROR_MESSAGE = "[ERROR]";
+
     private final BridgeNumberGenerator bridgeNumberGenerator;
 
     public BridgeGameController() {
         bridgeNumberGenerator = new BridgeRandomNumberGenerator();
     }
 
-    public void gameStart(){
+    public void gameStart() {
         Bridge bridge = getBridge();
         BridgeGame bridgeGame = new BridgeGame(bridge);
-        while (!bridgeGame.isClear()){
-            String movePosition = InputView.readMoving();
-            bridgeGame.move(movePosition);
+        while (!bridgeGame.isClear()) {
+            move(bridgeGame);
             OutputView.printMap(bridgeGame);
-            restartCaseFailure(bridgeGame);
+            if (bridgeGame.isFailure()) {
+                restart(bridgeGame);
+            }
         }
         OutputView.printResult(bridgeGame);
     }
 
-    private void restartCaseFailure(BridgeGame bridgeGame) {
-        if(bridgeGame.isFailure()){
-            String restartCommand = InputView.readGameCommand();
-            bridgeGame.retry(restartCommand);
+    private void move(BridgeGame bridgeGame) {
+        while (true) {
+            try {
+                String movePosition = InputView.readMoving();
+                bridgeGame.move(movePosition);
+                break;
+            } catch (IllegalArgumentException exception) {
+                System.out.println(ERROR_MESSAGE + exception.getMessage());
+            }
+        }
+    }
+
+    private void restart(BridgeGame bridgeGame) {
+        while (true) {
+            try {
+                String restartCommand = InputView.readGameCommand();
+                bridgeGame.retry(restartCommand);
+                break;
+            } catch (IllegalArgumentException exception) {
+                System.out.println(ERROR_MESSAGE + exception.getMessage());
+            }
         }
     }
 
     private Bridge getBridge() {
-        try {
-            int bridgeSize = InputView.readBridgeSize();
-            BridgeMaker bridgeMaker = new BridgeMaker(bridgeNumberGenerator);
-            return new Bridge(bridgeMaker.makeBridge(bridgeSize));
-        }
-        catch (NumberFormatException exception){
-            throw new IllegalArgumentException("3과 20사이의 숫자만 입력하셔야 합니다.");
+        while (true) {
+            try {
+                int bridgeSize = InputView.readBridgeSize();
+                BridgeMaker bridgeMaker = new BridgeMaker(bridgeNumberGenerator);
+                return new Bridge(bridgeMaker.makeBridge(bridgeSize));
+            } catch (NumberFormatException exception) {
+                System.out.println(ERROR_MESSAGE + "오직 숫자로만 이루어져 있어야 합니다.");
+            } catch (IllegalArgumentException exception) {
+                System.out.println(ERROR_MESSAGE + exception.getMessage());
+            }
         }
     }
 }
