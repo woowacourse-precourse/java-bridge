001cd2f6168b616b8747eec05f31bbbb73c083e9
EUNSH1 <tkdgus968@naver.com>
2022-11-21 15:02:56 +0900

    feat : 재시작하는 입력이 오면 true 값 전달

--- src/main/java/bridge/BridgeGame.java
+++ src/main/java/bridge/BridgeGame.java
@@ -43,6 +43,12 @@ public class BridgeGame {
      * <p>
      * 재시작을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void retry() {
+    public boolean retry(String retryCommand) {
+        return countRetry(RETRY_COMMAND.equals(retryCommand));
+    }
+
+    private boolean countRetry(boolean retry) {
+        return retry;
+    }
     }
 }
