aff3662b5045a56481f3df4df35072857d4d8320
osm <osm0822@naver.com>
2022-11-21 22:12:18 +0900

    feat(BridgeGame) : 게임 생성 구현

--- src/main/java/bridge/BridgeGame.java
+++ src/main/java/bridge/BridgeGame.java
@@ -1,9 +1,20 @@
 package bridge;
 
+import java.util.List;
+
 /**
  * 다리 건너기 게임을 관리하는 클래스
  */
 public class BridgeGame {
+
+    private List<String> bridge;
+    private int step;
+
+    public BridgeGame(int bridgeSize) {
+        BridgeMaker bridgeMaker = new BridgeMaker(new BridgeRandomNumberGenerator());
+        this.bridge = bridgeMaker.makeBridge(bridgeSize);
+        this.step = 0;
+    }
 
     /**
      * 사용자가 칸을 이동할 때 사용하는 메서드
