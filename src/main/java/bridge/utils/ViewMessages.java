bee18134b728c0e6a2d6cec28e5d11e3342653d4
twoosky <lhjae3@naver.com>
2022-11-22 05:13:42 +0900

    feat(InputView): 각 입력 메소드에 대한 검증 로직 추가

--- src/main/java/bridge/utils/ViewMessages.java
+++ src/main/java/bridge/utils/ViewMessages.java
@@ -7,6 +7,6 @@ public class ViewMessages {
 
     public static final String GAME_START = "다리 건너기 게임을 시작합니다.";
     public static final String GAME_RESULT = "최종 게임 결과";
-    public static final String SUCCESS_OR_FAIL = "게임 성공 여부: ";
+    public static final String GAME_SUCCESS_OR_FAIL = "게임 성공 여부: ";
     public static final String GAME_COUNT = "총 시도한 횟수: ";
 }
--- src/main/java/bridge/view/InputView.java
+++ src/main/java/bridge/view/InputView.java
@@ -1,8 +1,10 @@
 package bridge.view;
 
+import bridge.utils.validator.BridgeSizeValidator;
+import bridge.utils.validator.GameCommandValidator;
+import bridge.utils.validator.MovingValidator;
 import camp.nextstep.edu.missionutils.Console;
 
-import static bridge.utils.ErrorMessages.ERROR_NOT_INTEGER;
 import static bridge.utils.ViewMessages.*;
 
 /**
@@ -14,32 +16,64 @@ public class InputView {
      * 다리의 길이를 입력받는다.
      */
     public int readBridgeSize() {
+        while (true) {
+            try {
+                return inputBridgeSize();
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            }
+        }
+    }
+
+    private int inputBridgeSize() {
         System.out.println(BRIDGE_SIZE);
-        String inputBridgeSize = Console.readLine();
-        return toInteger(inputBridgeSize);
+        String bridgeSize = Console.readLine();
+        printNewLine();
+        BridgeSizeValidator.validate(bridgeSize);
+        return Integer.parseInt(bridgeSize);
     }
 
     /**
      * 사용자가 이동할 칸을 입력받는다.
      */
     public String readMoving() {
+        while (true) {
+            try {
+                return inputMoving();
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            }
+        }
+    }
+
+    private String inputMoving() {
         System.out.println(MOVING);
-        return Console.readLine();
+        String moving = Console.readLine();
+        MovingValidator.validate(moving);
+        return moving;
     }
 
     /**
      * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
      */
     public String readGameCommand() {
-        System.out.println(GAME_COMMAND);
-        return Console.readLine();
+        while (true) {
+            try {
+                return inputGameCommand();
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            }
+        }
     }
 
-    private int toInteger(String input) {
-        try {
-            return Integer.parseInt(input);
-        } catch (NumberFormatException e) {
-            throw new IllegalArgumentException(ERROR_NOT_INTEGER);
-        }
+    private String inputGameCommand() {
+        System.out.println(GAME_COMMAND);
+        String gameCommand = Console.readLine();
+        GameCommandValidator.validate(gameCommand);
+        return gameCommand;
+    }
+
+    public void printNewLine() {
+        System.out.println();
     }
 }
