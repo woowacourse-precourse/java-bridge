6161823d2a8bd0efdff836860eed833f7509723b
김수민(Sumin Kim) <sumin.kim.develop@gmail.com>
2022-11-22 01:27:14 +0900

    test(InputView): 다리 길이 입력 테스트 코드 작성

---
+++ src/test/java/bridge/view/InputViewTest.java
@@ -0,0 +1,34 @@
+package bridge.view;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.io.ByteArrayInputStream;
+import java.lang.reflect.Method;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+class InputViewTest {
+
+    @ParameterizedTest
+    @ValueSource(strings = {"1", "-2", "30", "32a"})
+    void bridgeSize(String input) {
+        try {
+            Method method = InputView.class.getDeclaredMethod("bridgeSize", String.class);
+            method.setAccessible(true);
+            assertThatThrownBy(() -> method.invoke(input))
+                    .isInstanceOf(IllegalArgumentException.class);
+        } catch (NoSuchMethodException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"3", "10"})
+    void inputBridgeSize(String input) {
+        System.setIn(new ByteArrayInputStream(input.getBytes()));
+        int result = Integer.parseInt(input);
+        assertThat(result).isEqualTo(new InputView().readBridgeSize());
+    }
+}
