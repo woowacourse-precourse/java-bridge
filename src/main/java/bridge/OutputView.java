7b7c08a99c53cbd14ddc07bae8e46b56c17cd5cd
yongseungLeeDec <yonson.dev@gmail.com>
2022-11-21 16:14:24 +0900

    feat: 이동할 칸 선택 입력을 요청하는 기능 구현
    OutputView의 printPromptForMove 메서드를 구현함.

--- src/main/java/bridge/OutputView.java
+++ src/main/java/bridge/OutputView.java
@@ -3,6 +3,7 @@ package bridge;
 public class OutputView {
     private static final String TEXT_GAME_START = "다리 건너기 게임을 시작합니다.";
     private static final String PROMPT_FOR_BRIDGE_SIZE = "다리의 길이를 입력해주세요.";
+    private static final String PROMPT_FOR_MOVE = "이동할 칸을 선택해주세요. (위: U, 아래: D)";
 
     public void printGameStart() {
         System.out.println(TEXT_GAME_START);
@@ -10,6 +11,10 @@ public class OutputView {
 
     public void printPromptForBridgeSize() {
         System.out.println(PROMPT_FOR_BRIDGE_SIZE);
+    }
+
+    public void printPromptForMove() {
+        System.out.println(PROMPT_FOR_MOVE);
     }
 
     public void printMap() {
