2d6f67f7dfcb8a36527adc77e3fea5e8fe408997
권태현 <kygkth2011@gmail.com>
2022-11-22 08:12:54 +0900

    feat(bridge): 게임 다시 시도에 대한 커멘드 입력 구현

--- src/main/java/bridge/InputValidator/CommandInput.java
+++ src/main/java/bridge/InputValidator/CommandInput.java
@@ -1,2 +1,31 @@
-package bridge.InputValidator;public class CommandInput {
+package bridge.InputValidator;
+
+import java.util.Objects;
+
+public class CommandInput {
+    private String commandInput;
+
+    public CommandInput(String commandInput) {
+        this.commandInput = commandInput;
+        validateCommandInput();
+    }
+
+    public String getCommandInput() {
+        return commandInput;
+    }
+
+    public Boolean isCommandRestart() {
+        return Objects.equals(this.commandInput, "R");
+    }
+
+    public Boolean isCommandQuit() {
+        return Objects.equals(this.commandInput, "Q");
+    }
+
+    private void validateCommandInput(){
+        if (!(Objects.equals(commandInput, "R")) && !(Objects.equals(commandInput, "Q"))){
+            System.out.println("[ERROR] : 게임 커맨드 입력은 'R' 또는 'Q' 가 입력되어야 합니다.");
+            throw new IllegalArgumentException();
+        }
+    }
 }
