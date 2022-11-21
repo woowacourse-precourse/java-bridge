61d95f82430b34325d7d499e5b8e35767c114fa8
Jinseo Song <82093280+rxb8k@users.noreply.github.com>
2022-11-22 01:28:40 +0900

    feat: 각 라운드 종료시마다 칸 이동 결과를 출력하는 기능 추가

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -24,6 +24,7 @@ public class Application {
             System.out.println("이동할 칸을 선택해주세요. (위: U, 아래: D)");
             String direction = input.readMoving();
             game.move(direction);
+            output.printMap(bridge);
 
             if (game.currentStatus == gameStatus.FAIL) {
                 System.out.println("게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)");
