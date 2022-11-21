13244d00ba70799043740cc28539a5fedb3c5792
YE-EUN <musiclove529@naver.com>
2022-11-21 15:04:01 +0900

    feat(Result) : 메서드 추가
    - move() : 사용자가 선택한 칸으로 이동
    - isMovable() : 이동 가능한지 확인
    - isSuccess() : 다리 건너기 성공했는지 확인
    - compare : 다리의 각 칸과 사용자 이동을 비교
    - getter/setter

--- src/main/java/bridge/Domain/Result.java
+++ src/main/java/bridge/Domain/Result.java
@@ -14,11 +14,55 @@ public class Result {
         tryCount = 1;
     }
 
-    public void clearMoves(){
+    public void move(String moveTo){
+        moves.add(moveTo);
+    }
 
+    public void clearMoves(){
+        moves.clear();
     }
 
     public void addTryCount(){
+        tryCount++;
+    }
 
+    public boolean isMovable(Bridge bridge){
+        return false;
+    }
+
+    public boolean isSuccess(Bridge bridge){
+        return false;
+    }
+
+    public boolean compare(List<String> bridge){
+        return false;
+    }
+
+    public int getMovesCount(){
+        return moves.size();
+    }
+
+    public List<String> getMoves() {
+        return moves;
+    }
+
+    public void setMoves(List<String> moves) {
+        this.moves = moves;
+    }
+
+    public boolean isSuccess() {
+        return isSuccess;
+    }
+
+    public void setSuccess(boolean success) {
+        isSuccess = success;
+    }
+
+    public int getTryCount() {
+        return tryCount;
+    }
+
+    public void setTryCount(int tryCount) {
+        this.tryCount = tryCount;
     }
 }
