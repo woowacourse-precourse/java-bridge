cab921b9cade2608657acb68b104d95f35f6d5a1
yun <yshyeon102@gmail.com>
2022-11-21 22:41:55 +0900

    fix(View): 메세지 출력시 공백 줄 먼저 출력하도록 수정

--- src/main/java/bridge/view/OutputView.java
+++ src/main/java/bridge/view/OutputView.java
@@ -81,7 +81,7 @@ public class OutputView {
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
     public void printResult(GameStatus gameStatus, List<String> realBridges, List<String> selectBridges) {
-        System.out.println("최종 게임 결과");
+        System.out.println("\n최종 게임 결과");
         printMap(realBridges, selectBridges);
         if (gameStatus.isSuccess()) {
             System.out.println("\n게임 성공 여부: 성공\n총 시도한 횟수: " + gameStatus.getTryCount());
