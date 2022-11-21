61e054508c2290270110c84b79a4c9bbf593a914
jwPark6 <67006945+jwPark6@users.noreply.github.com>
2022-11-22 01:53:48 +0900

    feat : setter 추가

--- src/main/java/bridge/Bridge.java
+++ src/main/java/bridge/Bridge.java
@@ -5,6 +5,10 @@ import java.util.List;
 public class Bridge {
     private List<String> elementsOfBridge;
 
+    public void setElementsOfBridge(List<String> elementsOfBridge) {
+        this.elementsOfBridge = elementsOfBridge;
+    }
+
     public List<String> getElementsOfBridge() {
         return elementsOfBridge;
     }
