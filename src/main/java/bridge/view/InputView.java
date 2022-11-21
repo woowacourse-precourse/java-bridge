7345f02a02d953e85853e23da96d6d6e4c70c5ca
endlessmomo <soohyuk96@naver.com>
2022-11-21 13:07:30 +0900

    feat(InputView) : InputView 메서드 작성

--- src/main/java/bridge/view/InputView.java
+++ src/main/java/bridge/view/InputView.java
@@ -1,5 +1,8 @@
 package bridge.view;
 
+import static bridge.domain.constnce.Text.*;
+import static camp.nextstep.edu.missionutils.Console.readLine;
+
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
  */
@@ -9,20 +12,26 @@ public class InputView {
      * 다리의 길이를 입력받는다.
      */
     public int readBridgeSize() {
-        return 0;
+        try {
+            System.out.println(BRIDGE_SIZE_MESSAGE);
+            return Integer.parseInt(readLine());
+        } catch (IllegalArgumentException e){
+            throw new IllegalArgumentException();
+        }
     }
-
     /**
      * 사용자가 이동할 칸을 입력받는다.
      */
     public String readMoving() {
-        return null;
+        System.out.println(MOVE_TO_WHERE);
+        return readLine();
     }
 
     /**
      * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
      */
     public String readGameCommand() {
-        return null;
+        System.out.println(RETRY_MESSAGE);
+        return readLine();
     }
 }
