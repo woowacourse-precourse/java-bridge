60062fdbb43d3ec8a7ee902c50586d8f9c1b2cbd
JONGMIN SHIN <jongmin373@gmail.com>
2022-11-21 17:07:25 +0900

    Feat<BridgeGame> : add And refactor move methods

--- src/main/java/bridge/BridgeGame.java
+++ src/main/java/bridge/BridgeGame.java
@@ -1,23 +1,49 @@
 package bridge;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * 다리 건너기 게임을 관리하는 클래스
  */
 public class BridgeGame {
-
+    private List<String> bridge;
+    private List<String> gameState;
+    int count;      // 입력 개수
+    private boolean isSuccess;
+    public BridgeGame(List<String> bridge) {
+        this.bridge = bridge;
+    }
     /**
      * 사용자가 칸을 이동할 때 사용하는 메서드
-     * <p>
-     * 이동을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
+     * @param crossTheBridge : U or D
+     * @return O (true) , X (false)
      */
-    public void move() {
+    public boolean move(String crossTheBridge) {
+        if(bridge.get(count).equals(crossTheBridge)){
+            if(endOfInput()) {
+                return true;
+            }
+            return false;
+        }
+        return crossFail(crossTheBridge);
     }
-
-    /**
-     * 사용자가 게임을 다시 시도할 때 사용하는 메서드
-     * <p>
-     * 재시작을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
-     */
-    public void retry() {
+    // 건너기 실패 (종료)
+    public boolean crossFail(String crossTheBridge){
+        gameState.add(crossTheBridge);
+        isSuccess = false;
+        return false;
+    }
+    // 사용자의 최대 입력 ( 다리의 총 개수) 만큼 입력하였을 때
+    public boolean endOfInput(){
+        gameState.add(bridge.get(count));
+        count++;
+        isSuccess = true;
+        if(bridge.size() == count) {
+            // 게임 승리 (종료)
+            return false;
+        }
+        // 게임 계속 진행
+        return true;
     }
 }
