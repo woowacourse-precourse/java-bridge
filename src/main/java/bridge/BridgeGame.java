fc9a842a7415366b9c6fe27f1da256329ed8584c
bearjun05 <bearjun05@naver.com>
2022-11-22 05:18:50 +0900

    feat : 맵 만들기 기능, 4가지 중 하나의 맵 유형 찾기 기능이 포함된 메인 로직 move 기능 구현

--- src/main/java/bridge/BridgeGame.java
+++ src/main/java/bridge/BridgeGame.java
@@ -1,16 +1,21 @@
 package bridge;
 
+import bridge.utils.MapType;
 import java.util.List;
+import java.util.Stack;
 
 /**
  * 다리 건너기 게임을 관리하는 클래스
  */
 public class BridgeGame {
+    private final BridgeMap bridgeMap;
     private final List<String> bridge;
     private int CurrentLocation = -1;
+    private int cnt;
 
     public BridgeGame(List<String> bridge) {
         this.bridge = bridge;
+        bridgeMap = new BridgeMap();
     }
 
     /**
@@ -18,18 +23,37 @@ public class BridgeGame {
      * <p>
      * 이동을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void move(String movingValue) {
-        isCorrect(movingValue);
+    public List<Stack> move(String movingValue, boolean moveResult) {
+        cnt++;
+        MapType mapType = findMapType(movingValue, moveResult);
+        return makeMap(mapType);
     }
 
-    private boolean isCorrect(String movingValue) {
+    private List<Stack> makeMap(MapType mapType) {
+        List<Stack> stairs;
+        bridgeMap.make(mapType);
+        stairs = bridgeMap.makeListToSend();
+        return stairs;
+    }
+
+    private MapType findMapType(String movingValue, boolean moveResult) {
+        MapType[] mapTypes = MapType.values();
+        for (MapType mapType : mapTypes) {
+            if (movingValue.equals(mapType.getStairs()) && moveResult == mapType.isPass()) {
+                return mapType;
+            }
+        }
+        throw new IllegalArgumentException("[ERROR] 사용자 입력 값이 잘못 되었습니다.");
+    }
+
+    public boolean isCorrect(String movingValue) {
         if (bridge.get(CurrentLocation).equals(movingValue)) {
             return true;
         }
         if (!bridge.get(CurrentLocation).equals(movingValue)) {
             return true;
         }
-        throw new IllegalArgumentException("사용자 입력 값이 잘못 되었습니다.");
+        throw new IllegalArgumentException("[ERROR] 사용자 입력 값이 잘못 되었습니다.");
     }
 
     /**
