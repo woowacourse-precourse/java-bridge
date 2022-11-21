1dd9557d67d25e19547c86b12713e8a5b65596a9
Egenieee <hhgg0925@naver.com>
2022-11-21 22:23:02 +0900

    refactor: Controller 코드 리팩토링
    - 메서드의 길이가 10줄을 넘어가지 않도록 로직 분리

--- src/main/java/controller/Controller.java
+++ src/main/java/controller/Controller.java
@@ -1,6 +1,7 @@
 package controller;
 
 import bridge.*;
+import utils.ConstValue;
 import view.InputView;
 import view.OutputView;
 
@@ -11,12 +12,13 @@ public class Controller {
     public void run() {
         Bridge bridge = createBridge();
         BridgeGame bridgeGame = new BridgeGame(bridge);
+        boolean isQuit;
 
-        while (!bridge.isCompleted()) {
-            move(bridge, bridgeGame);
-        }
+        do {
+            isQuit = onePlaceMove(bridge, bridgeGame);
+        } while (gameContinue(bridge, isQuit));
 
-        outputView.printResult(bridgeGame);
+        outputView.printResult(bridgeGame, bridge.toString());
     }
 
     private Bridge createBridge() {
@@ -30,31 +32,48 @@ public class Controller {
         return new Bridge(bridgeMaker.makeBridge(bridgeSize));
     }
 
-    private void move(Bridge bridge, BridgeGame bridgeGame) {
+    private boolean onePlaceMove(Bridge bridge, BridgeGame bridgeGame) {
         String gameCommand;
         boolean isCorrect;
 
         do {
-            gameCommand = "";
-            String movingPlace = inputView.readMoving();
-            isCorrect = bridgeGame.move(movingPlace);
-            outputView.printMap(bridge.toString());
-            if (!isCorrect) {
-                gameCommand = inputView.readGameCommand();
-            }
+            isCorrect = move(bridge, bridgeGame);
+            gameCommand = getGameCommand(isCorrect);
         } while (isRetry(gameCommand, isCorrect, bridgeGame));
+
+        return isQuit(gameCommand);
+    }
+
+    private boolean move(Bridge bridge, BridgeGame bridgeGame) {
+        boolean isCorrect;
+
+        String movingPlace = inputView.readMoving();
+        isCorrect = bridgeGame.move(movingPlace);
+        outputView.printMap(bridge.toString());
+
+        return isCorrect;
+    }
+
+    private String getGameCommand(boolean isCorrect) {
+        if (!isCorrect) {
+            return inputView.readGameCommand();
+        }
+        return "";
     }
 
     private boolean isRetry(String gameCommand, boolean isCorrect, BridgeGame bridgeGame) {
-        if (gameCommand.equals("Q")) {
-            return false;
-        }
-
-        if (gameCommand.equals("R")) {
+        if (gameCommand.equals(ConstValue.RETRY)) {
             bridgeGame.retry();
         }
 
-        return gameCommand.equals("R") && !isCorrect;
+        return gameCommand.equals(ConstValue.RETRY) && !isCorrect;
     }
 
+    private boolean isQuit(String gameCommand) {
+        return gameCommand.equals(ConstValue.QUIT);
+    }
+
+    private boolean gameContinue(Bridge bridge, boolean isQuit) {
+        return !bridge.isCompleted() && !isQuit;
+    }
 }
