e2d02aeef027b327dd7eaab5f3b41e5a24e02151
TaemHam <tk4003950@gmail.com>
2022-11-21 14:19:54 +0900

    Feat: 사용자 입력 기능 모듈화
    Console.readLine() 메서드를 InputReader 클래스로 래핑
    
    변경 사항:
    * 다음 클래스 추가:
      { InputReader }

---
+++ src/main/java/bridge/utils/InputReader.java
@@ -0,0 +1,10 @@
+package bridge.utils;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class InputReader {
+
+    public static String readLine() {
+        return Console.readLine();
+    }
+}
