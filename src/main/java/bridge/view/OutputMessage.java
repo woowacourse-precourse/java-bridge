3bc98a08682dd178db52999654493746ba506bbc
LeeHaiLim <hailim1997@gmail.com>
2022-11-22 03:28:06 +0900

    feat(view): add string constant game start message

--- src/main/java/bridge/view/OutputMessage.java
+++ src/main/java/bridge/view/OutputMessage.java
@@ -1,6 +1,7 @@
 package bridge.view;
 
 public enum OutputMessage {
+    GAME_START("다리 건너기 게임을 시작합니다.\n"),
     GAME_RESULT("최종 게임 결과"),
     SUCCESS_OR_FAIL("게임 성공 여부: %s\n"),
     ATTEMPT("총 시도한 횟수: %d\n"),
