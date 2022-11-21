c36c155730888f0ceed56c2f634681d6a664de64
Namyunsuk <yunsuk0328@naver.com>
2022-11-21 13:52:49 +0900

    feat(Bridge): 생성한 다리의 정보와 사용자의 입력 값을 비교하여 다리안에 들어갈 값들을 저장하는 기능 구현

--- src/main/java/bridge/domain/Bridge.java
+++ src/main/java/bridge/domain/Bridge.java
@@ -1,19 +1,48 @@
 package bridge.domain;
 
 import bridge.BridgeRandomNumberGenerator;
+import java.util.ArrayList;
 import java.util.List;
 
 public class Bridge {
 
     private static List<String> bridge;
-    private BridgeRandomNumberGenerator bridgeRandomNumberGenerator;
+    private static List<String> upperBridge;
+    private static List<String> lowerBridge;
+    private final BridgeRandomNumberGenerator bridgeRandomNumberGenerator;
 
     public Bridge() {
         bridgeRandomNumberGenerator = new BridgeRandomNumberGenerator();
         bridge = new BridgeMaker(bridgeRandomNumberGenerator).makeBridge(User.getBridgeSize());
     }
 
+    private static void resetUpperBridge() {
+        upperBridge = new ArrayList<String>();
+    }
+
+    private static void resetLowerBridge() {
+        lowerBridge = new ArrayList<String>();
+    }
+
     public static List<String> getBridge() {
         return bridge;
+    }
+
+    public static void makeUpperBridge() {
+        resetUpperBridge();
+        List<String> userBridge = User.getUserMovingRecord();
+        for (int i = 0; i < userBridge.size(); i++) {
+            String keyword = CalculationResult.calculateUpper(bridge.get(i), userBridge.get(i));
+            upperBridge.add(keyword);
+        }
+    }
+
+    public static void makeLowerBridge() {
+        resetLowerBridge();
+        List<String> userBridge = User.getUserMovingRecord();
+        for (int i = 0; i < userBridge.size(); i++) {
+            String keyword = CalculationResult.calculateLower(bridge.get(i), userBridge.get(i));
+            lowerBridge.add(keyword);
+        }
     }
 }
