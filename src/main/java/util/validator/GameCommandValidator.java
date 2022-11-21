5c876645183a92e72eb02eb09e45e3bb59faba29
nylonmusk <106235834+nylonmusk@users.noreply.github.com>
2022-11-21 20:12:12 +0900

    refactor(GameCommandValidator): 가독성 향상을 위해 Constant를 클래스변수로 선언

--- src/main/java/util/validator/GameCommandValidator.java
+++ src/main/java/util/validator/GameCommandValidator.java
@@ -5,21 +5,26 @@ import constant.InputConstant;
 import constant.NumberConstant;
 
 public class GameCommandValidator {
+    private static final int ONE_LETTER = NumberConstant.ONE_LETTER.getConstant();
+    private static final String QUIT = InputConstant.QUIT.getConstant();
+    private static final String RETRY = InputConstant.RETRY.getConstant();
+    private static final String INPUT_LENGTH = ErrorConstant.INPUT_LENGTH.getConstant();
+    private static final String RETRY_OR_QUIT_INVALID = ErrorConstant.RETRY_OR_QUIT_INVALID.getConstant();
+
     public static void isValid(String gameCommand) {
         isOneLetter(gameCommand);
         isEqualsRorQ(gameCommand);
     }
 
     private static void isOneLetter(String moving) {
-        if (moving.length() != NumberConstant.ONE_LETTER.getConstant()) {
-            throw new IllegalArgumentException(ErrorConstant.INPUT_LENGTH.getConstant());
+        if (moving.length() != ONE_LETTER) {
+            throw new IllegalArgumentException(INPUT_LENGTH);
         }
     }
 
     private static void isEqualsRorQ(String gameCommand) {
-        if (!gameCommand.equals(InputConstant.QUIT.getConstant())
-                && !gameCommand.equals(InputConstant.RETRY.getConstant())) {
-            throw new IllegalArgumentException(ErrorConstant.RETRY_OR_QUIT_INVALID.getConstant());
+        if (!gameCommand.equals(QUIT) && !gameCommand.equals(RETRY)) {
+            throw new IllegalArgumentException(RETRY_OR_QUIT_INVALID);
         }
     }
 }
