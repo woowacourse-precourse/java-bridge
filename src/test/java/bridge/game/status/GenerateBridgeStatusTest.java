b3596204c3aaaeb1d1b10790df55f162b4770fe9
JaehongDev <println@kakao.com>
2022-11-21 19:11:05 +0900

    test (GenerateBridgeStatusTest) : 다리 생성 상태의 다음 상태는 사용자 이동 상태입니다.

---
+++ src/test/java/bridge/game/status/GenerateBridgeStatusTest.java
@@ -0,0 +1,27 @@
+package bridge.game.status;
+
+import bridge.game.context.BridgeGameContext;
+import bridge.view.InputView;
+import bridge.view.OutputView;
+import camp.nextstep.edu.missionutils.test.NsTest;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayNameGeneration;
+import org.junit.jupiter.api.DisplayNameGenerator;
+import org.junit.jupiter.api.Test;
+
+@DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
+class GenerateBridgeStatusTest extends NsTest {
+
+
+    @Test
+    void 다리생성_상태_다음_플레이어_이동_상태입니다() {
+        run("3");
+        var actual = new GenerateBridgeStatus().next(new BridgeGameContext(), new InputView(), new OutputView());
+        Assertions.assertThat(actual).isInstanceOf(PlayerUnitMoveStatus.class);
+    }
+
+    @Override
+    protected void runMain() {
+
+    }
+}
