aa2762fc2a7bd51ae096c4866a84e8b9471588c1
kingseungwooya <ksw_0505@nate.com>
2022-11-21 21:28:58 +0900

    feat(utils): add input output classes

---
+++ src/main/java/bridge/view/utils/UserInputUtils.java
@@ -0,0 +1,11 @@
+package bridge.view.utils;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class UserInputUtils {
+
+    public static String readLine() {
+        return Console.readLine();
+    }
+
+}
