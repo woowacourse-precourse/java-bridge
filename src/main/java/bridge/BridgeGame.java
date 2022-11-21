a68e0c6d72374ee22e8485801a31a1196004319b
poter0501 <poter0501@gmail.com>
2022-11-22 00:01:39 +0900

    feat(BridgeGame): 다리에서 이동을 담당하는 기능 반환형 수정
    반환형을 void에서
    HashMap<String, List<String>> 으로 변경

--- src/main/java/bridge/BridgeGame.java
+++ src/main/java/bridge/BridgeGame.java
@@ -26,10 +26,11 @@ public class BridgeGame {
      * <p>
      * 이동을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void move(String movingCommand) {
+    public HashMap<String, List<String>> move(String movingCommand) {
         HashMap<String,String> oppositionCommand = new HashMap<>(Map.of("U","D","D","U"));
         pathCheck(movingCommand, oppositionCommand);
         moveCount++;
+        return path;
     }
     private void pathCheck(String movingCommand, HashMap<String,String> oppositionCommand){
         if (movingCommand.equals(bridge.get(moveCount))){
