e073c2c60ed3d6b6b7cdc6c835dfd88c93ea6385
DevJay1024 <devjay1024@gmail.com>
2022-11-21 12:24:02 +0900

    test: 다리의 안전경로와 사용자의 이동경로의 일치 여부를 확인하는 기능의 테스트 케이스 작성

--- src/test/java/bridge/paths/BridgeTest.java
+++ src/test/java/bridge/paths/BridgeTest.java
@@ -1,16 +1,20 @@
 package bridge.paths;
 
+import bridge.identifiers.Direction;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 
 class BridgeTest {
 
     @Test
     void 다리길이가_안전경로_길이와_다르면_예외처리한다(){
-        Assertions.assertThatThrownBy(()->{
+        assertThatThrownBy(()->{
             List<String> securePath = List.of("U", "D", "D");
             Bridge bridge = new Bridge(securePath, 2);
         }).isInstanceOf(InternalError.class);
@@ -18,8 +22,41 @@ class BridgeTest {
 
     @Test
     void 안전경로가_주어지지_않으면_예외처리한다(){
-        Assertions.assertThatThrownBy(()->{
+        assertThatThrownBy(()->{
             Bridge bridge = new Bridge(null, 0);
         }).isInstanceOf(InternalError.class);
+    }
+
+    @Test
+    void 안전경로가_사용자_경로와_같을_때_확인(){
+        Bridge bridge = new Bridge(List.of("U", "D", "D"), 3);
+        PlayerPath playerPath = new PlayerPath();
+        playerPath.saveDirection(Direction.UP);
+        playerPath.saveDirection(Direction.DOWN);
+        playerPath.saveDirection(Direction.DOWN);
+
+        assertThat(bridge.checkCollapse(playerPath)).isTrue();
+    }
+
+    @Test
+    void 안전경로가_사용자_경로와_다를_때_확인(){
+        Bridge bridge = new Bridge(List.of("U", "D", "D"), 3);
+        PlayerPath playerPath = new PlayerPath();
+        playerPath.saveDirection(Direction.UP);
+        playerPath.saveDirection(Direction.DOWN);
+        playerPath.saveDirection(Direction.UP);
+
+        assertThat(bridge.checkCollapse(playerPath)).isFalse();
+    }
+
+    @Test
+    void 안전경로도_사용자_경로의_마지막_인덱스로_확인하는지_검사(){
+        Bridge bridge = new Bridge(List.of("U", "D", "D"), 3);
+        PlayerPath playerPath = new PlayerPath();
+        playerPath.saveDirection(Direction.UP);
+        playerPath.saveDirection(Direction.UP);
+        playerPath.saveDirection(Direction.DOWN);
+
+        assertThat(bridge.checkCollapse(playerPath)).isTrue();
     }
 }
