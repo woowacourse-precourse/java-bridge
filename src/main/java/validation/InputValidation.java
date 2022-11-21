3845d1dd7e98c8aacef2b2e107e95382b2eb23ce
thousandcows <milcamboo@gmail.com>
2022-11-21 17:41:21 +0900

    feat: create validateMoving method

--- src/main/java/validation/InputValidation.java
+++ src/main/java/validation/InputValidation.java
@@ -1,9 +1,21 @@
 package validation;
 
+import java.util.List;
+
 public class InputValidation {
     public void validateBridgeSize(String input) {
         if (!input.matches("^[1-9]+[0-9]*$")) {
             throw new IllegalArgumentException(String.valueOf(ErrorMessage.INVALID_BRIDGE_SIZE));
         }
     }
+
+    public void validateMoving(String input) {
+
+        List<String> orders = List.of("U", "D");
+
+        if (!orders.contains(input)) {
+            throw new IllegalArgumentException(String.valueOf(ErrorMessage.INVALID_MOVING_ORDER));
+        }
+    }
+
 }
