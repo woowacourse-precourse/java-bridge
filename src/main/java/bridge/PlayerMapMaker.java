81b334b8508a367e5d9b4da4a5b458a8ba31e5ea
sonsoowon <yuwoo1018@gmail.com>
2022-11-21 22:16:47 +0900

    feat(PlayerMapMaker): 플레이어의 이동 경로를 생성하는 클래스 추가

---
+++ src/main/java/bridge/PlayerMapMaker.java
@@ -0,0 +1,26 @@
+package bridge;
+
+import java.util.List;
+
+public class PlayerMapMaker {
+
+    private final PlayerMap successMap;
+    private final PlayerMap failureMap;
+
+    public PlayerMapMaker(List<String> bridge) {
+        BridgeMapMaker bridgeMapMaker = new BridgeMapMaker(bridge);
+        successMap = bridgeMapMaker.getSuccessBridgeMap();
+        failureMap = bridgeMapMaker.getFailureBridgeMap();
+    }
+
+    public PlayerMap makeSuccessPlayerMapTo(int distance) {
+        return successMap.subMapBy(distance);
+    }
+
+    public PlayerMap makeFailurePlayerMapTo(int distance) {
+        PlayerMap successSubMap = successMap.subMapBy(distance - 1);
+        PlayerMap failureElement = failureMap.getElementOf(distance);
+
+        return successSubMap.join(failureElement);
+    }
+}
---
+++ src/test/java/bridge/PlayerMapMakerTest.java
@@ -0,0 +1,67 @@
+package bridge;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.List;
+import java.util.stream.Stream;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class PlayerMapMakerTest {
+
+    private static final List<String> bridge = List.of("U", "D", "D");
+    private static final PlayerMapMaker playerMapMaker = new PlayerMapMaker(bridge);
+
+    @DisplayName("n번째 칸으로 이동한 후 다리 건너기를 성공했을 때의 경로를 반환한다")
+    @ParameterizedTest
+    @MethodSource("makeSuccessPlayerMapTestSource")
+    void makeSuccessPlayerMapTest(int distance, PlayerMap expected) {
+        assertThat(playerMapMaker.makeSuccessPlayerMapTo(distance))
+                .isEqualTo(expected);
+    }
+
+    @DisplayName("n번째 칸으로 이동한 후 다리 건너기를 실패했을 때의 경로를 반환한다")
+    @ParameterizedTest
+    @MethodSource("makeFailurePlayerMapTestSource")
+    void makeFailurePlayerMapTest(int distance, PlayerMap expected) {
+        assertThat(playerMapMaker.makeFailurePlayerMapTo(distance))
+                .isEqualTo(expected);
+    }
+
+    private static Stream<Arguments> makeSuccessPlayerMapTestSource() {
+        return Stream.of(
+                Arguments.of(1, new PlayerMap(
+                        List.of("O"),
+                        List.of(" ")
+                )),
+                Arguments.of(2, new PlayerMap(
+                        List.of("O", " "),
+                        List.of(" ", "O")
+                )),
+                Arguments.of(3, new PlayerMap(
+                        List.of("O", " ", " "),
+                        List.of(" ", "O", "O")
+                ))
+        );
+    }
+
+    private static Stream<Arguments> makeFailurePlayerMapTestSource() {
+        return Stream.of(
+                Arguments.of(1, new PlayerMap(
+                        List.of(" "),
+                        List.of("X")
+                )),
+                Arguments.of(2, new PlayerMap(
+                        List.of("O", "X"),
+                        List.of(" ", " ")
+                )),
+                Arguments.of(3, new PlayerMap(
+                        List.of("O", " ", "X"),
+                        List.of(" ", "O", " ")
+                ))
+        );
+    }
+}
