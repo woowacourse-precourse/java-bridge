ca665eec988896ca26d28cc045953c9377f2153d
phdljr <phdljr@naver.com>
2022-11-22 01:29:27 +0900

    feat: BridgeGame 로직 추가

--- src/main/java/bridge/model/BridgeGame.java
+++ src/main/java/bridge/model/BridgeGame.java
@@ -1,16 +1,24 @@
-package bridge;
+package bridge.model;
+
+import bridge.model.Bridge;
+
+import java.util.ArrayList;
 
 /**
  * 다리 건너기 게임을 관리하는 클래스
  */
 public class BridgeGame {
 
+    private Bridge answerBridge;
+    private Bridge currentBridge;
+
     /**
      * 사용자가 칸을 이동할 때 사용하는 메서드
      * <p>
      * 이동을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void move() {
+    public void move(String input) {
+        currentBridge.forward(input);
     }
 
     /**
@@ -19,5 +27,15 @@ public class BridgeGame {
      * 재시작을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
     public void retry() {
+        currentBridge = new Bridge(new ArrayList<>());
+        answerBridge = null;
+    }
+
+    public boolean correctAnswer(){
+        return currentBridge.matchAll(answerBridge);
+    }
+
+    public void setAnswerBridge(Bridge answerBridge){
+        this.answerBridge = answerBridge;
     }
 }
