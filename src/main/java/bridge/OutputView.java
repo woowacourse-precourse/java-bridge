e184f637a30a2d697b0cc78b1a965db79bb18126
gwj0421 <gwj0421@gmail.com>
2022-11-21 19:02:09 +0900

    feat: 최종 게임 결과(통과된 다리 위치 결과 리스트, 성공 여부, 총 시도 횟수)를 출력하는 함수 추가

--- src/main/java/bridge/OutputView.java
+++ src/main/java/bridge/OutputView.java
@@ -60,6 +60,18 @@ public class OutputView {
      * <p>
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void printResult() {
+    private void showSuccessOrNot(boolean isSuccess) {
+        if (isSuccess) {
+            System.out.println("\n게임 성공 여부: 성공");
+            return;
+        }
+        System.out.println("\n게임 성공 여부: 실패");
+    }
+
+    public void printResult(List<String> playerMap, boolean isSuccess, int numberOfAttempts) {
+        System.out.println("\n최종 게임 결과");
+        constructMap(playerMap);
+        showSuccessOrNot(isSuccess);
+        System.out.println("총 시도한 횟수: " + numberOfAttempts);
     }
 }
