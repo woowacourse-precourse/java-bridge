13e60311363767520d1feae75f0c8e98fdc7b560
LeeHaiLim <hailim1997@gmail.com>
2022-11-22 08:44:56 +0900

    refactor(domain): change error message string

--- src/main/java/bridge/domain/InputErrorMessage.java
+++ src/main/java/bridge/domain/InputErrorMessage.java
@@ -5,7 +5,7 @@ import bridge.Constants;
 public enum InputErrorMessage {
     PREFIX("[ERROR]"),
     BRIDGE_NUMBER(String.format("%s 숫자만 입력해야 합니다.", PREFIX.message())),
-    BRIDGE_SIZE(String.format("%s 다리 길이는 %d 에서 %d 사이의 숫자여야 합니다.",
+    BRIDGE_SIZE(String.format("%s 다리 길이는 %d부터 %d 사이의 숫자여야 합니다.",
             PREFIX.message(), Constants.BRIDGE_SIZE_MIN, Constants.BRIDGE_SIZE_MAX)),
     MOVING(String.format("%s %s, %s 중 하나를 입력해야 합니다.",
             PREFIX.message(), Constants.CODE_UP, Constants.CODE_DOWN)),
