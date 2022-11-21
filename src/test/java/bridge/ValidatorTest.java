cd821e552c76dcc0120bc858f8ba5a6aca373dce
rjsah09 <83952174+rjsah09@users.noreply.github.com>
2022-11-22 00:48:06 +0900

    test: Validator 클래스 추가 테스트

--- src/test/java/bridge/ValidatorTest.java
+++ src/test/java/bridge/ValidatorTest.java
@@ -6,22 +6,22 @@ import org.junit.jupiter.api.Test;
 import java.util.Arrays;
 import java.util.List;
 
-import static org.assertj.core.api.Assertions.assertThatNoException;
+import static org.assertj.core.api.Assertions.*;
 
 public class ValidatorTest {
 
     @Test
-    void validateNumeric_예외검출 () {
+    void validateNumeric_예외검출() {
         List<String> inputs = Arrays.asList("12A", "bbb", "c12d");
 
         for (String input: inputs) {
-            Assertions.assertThatThrownBy(() -> Validator.validateNumeric(input))
+            assertThatThrownBy(() -> Validator.validateNumeric(input))
                     .isInstanceOf(IllegalArgumentException.class);
         }
     }
 
     @Test
-    void validateNumeric_정상값 () {
+    void validateNumeric_정상값() {
         List<String> inputs = Arrays.asList("123", "456", "789");
 
         for (String input: inputs) {
@@ -31,17 +31,17 @@ public class ValidatorTest {
     }
 
     @Test
-    void validateNumericSize_예외검출 () {
+    void validateNumericSize_예외검출() {
         List<String> inputs = Arrays.asList("1", "2", "21", "22");
 
         for (String input: inputs) {
-            Assertions.assertThatThrownBy(() -> Validator.validateNumericSize(3, 20, input))
+            assertThatThrownBy(() -> Validator.validateNumericSize(3, 20, input))
                     .isInstanceOf(IllegalArgumentException.class);
         }
     }
 
     @Test
-    void validateNumericSize_정상값 () {
+    void validateNumericSize_정상값() {
         List<String> inputs = Arrays.asList("3", "4", "19", "20");
 
         for (String input: inputs) {
@@ -49,4 +49,27 @@ public class ValidatorTest {
                     .isThrownBy(() -> Validator.validateNumericSize(3, 20, input));
         }
     }
+
+    @Test
+    void ValidateContains_예외검출() {
+        List<String> correctInputs = Arrays.asList("A", "B", "C");
+        List<String> inputs = Arrays.asList("AS", "ABC", "CD", "123", "a", "b", "c");
+
+        for (String input: inputs) {
+            assertThatThrownBy(() -> Validator.validateContains(correctInputs, input))
+                    .isInstanceOf((IllegalArgumentException.class));
+        }
+    }
+
+    @Test
+    void ValidateContains_정상값() {
+        List<String> correctInputs = Arrays.asList("A", "B", "C", "AD");
+        List<String> inputs = Arrays.asList("A", "C", "B", "AD");
+
+        for (String input: inputs) {
+            assertThatNoException()
+                    .isThrownBy(() -> Validator.validateContains(correctInputs, input));
+        }
+    }
+    
 }
