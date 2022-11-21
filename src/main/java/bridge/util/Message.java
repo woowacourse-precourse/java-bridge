1113de12820639b255495ce7a3dbc4ffae6655f6
jin8073 <jin8073@gmail.com>
2022-11-21 14:19:29 +0900

    feat(OutputView): 종료 시점에서까지의 이동 루트 출력

--- src/main/java/bridge/util/Message.java
+++ src/main/java/bridge/util/Message.java
@@ -6,4 +6,9 @@ public class Message {
     public static final String INPUT_CROSS_THE_BRIDGE_MESSAGE = "\n이동할 칸을 선택해주세요. (위: U, 아래: D)";
     public static final String INPUT_RESTART_OR_EXIT_MESSAGE
             = "\n게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)";
+    public static final String FINAL_GAME_RESULT_MESSAGE = "최종 게임 결과";
+    public static final String GAME_RESULT_MESSAGE = "\n게임 성공 여부: ";
+    public static final String SUCCESS_MESSAGE = "성공";
+    public static final String FAIL_MESSAGE = "실패";
+    public static final String TOTAL_TRY_COUNT_MESSAGE = "총 시도환 횟수: ";
 }
--- src/main/java/bridge/view/OutputView.java
+++ src/main/java/bridge/view/OutputView.java
@@ -1,6 +1,7 @@
 package bridge.view;
 
 import bridge.model.Bridge;
+import bridge.util.Message;
 
 /**
  * 사용자에게 게임 진행 상황과 결과를 출력하는 역할을 한다.
@@ -22,6 +23,11 @@ public class OutputView {
      * <p>
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void printResult() {
+    public void printResult(Bridge bridge) {
+        System.out.println(Message.FINAL_GAME_RESULT_MESSAGE);
+        System.out.println(bridge.getUpsideBridge().toString().replace(", ", "|"));
+        System.out.println(bridge.getDownBridge().toString().replace(", ", "|"));
+        System.out.println(Message.GAME_RESULT_MESSAGE);
+        System.out.println(Message.TOTAL_TRY_COUNT_MESSAGE);
     }
 }
