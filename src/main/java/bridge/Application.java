6dcd9dc224b126ea6a61e67eb8b8d60e846e73f5
JaehongDev <println@kakao.com>
2022-11-21 23:34:53 +0900

    style : indent 정리

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -9,10 +9,7 @@ public class Application {
 
     public static void main(String[] args) {
         // TODO: 프로그램 구현
-        new BridgeGameLauncher(
-                new BridgeGameContextImpl(),
-                new InputView(),
-                new OutputView()
-        ).execute();
+        var gameLauncher = new BridgeGameLauncher(new BridgeGameContextImpl(), new InputView(), new OutputView());
+        gameLauncher.execute();
     }
 }
