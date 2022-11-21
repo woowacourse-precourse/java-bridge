fd6580d422b35043cb09b15ac34f3283124d38fc
2021110806 <2021110806@dgu.ac.kr>
2022-11-21 11:52:12 +0900

    test(ExceptionTest):Exception 클래스에 대한 테스트를 실행하는 ExceptionTest 클래스 추가

--- src/main/java/Util/Exception.java
+++ src/main/java/Util/Exception.java
@@ -2,13 +2,13 @@ package Util;
 
 public class Exception {
 
-    public void throwIllegalArgumentError(String message) throws IllegalArgumentException {
+    public void throwIllegalArgumentExceptionError(String message) throws IllegalArgumentException {
         throw new IllegalArgumentException(message);
     }
 
     public void validateInputNumberRange(String inputNumber) throws IllegalArgumentException {
         if (Integer.parseInt(inputNumber)<3||Integer.parseInt(inputNumber)>20) {
-            throwIllegalArgumentError(ExceptionMessage.INVALID_NUMBER_RANGE_MESSAGE.getMessage());
+            throwIllegalArgumentExceptionError(ExceptionMessage.INVALID_NUMBER_RANGE_MESSAGE.getMessage());
         }
     }
 
@@ -16,19 +16,19 @@ public class Exception {
         try {
             Integer.parseInt(inputNumber);
         } catch (NumberFormatException e) {
-            throwIllegalArgumentError(ExceptionMessage.UNEXPECTED_INPUT_MESSAGE.getMessage());
+            throwIllegalArgumentExceptionError(ExceptionMessage.UNEXPECTED_INPUT_MESSAGE.getMessage());
         }
     }
 
     public void validateMovingInput(String moving) throws IllegalArgumentException {
         if(!(moving.equals("U")||moving.equals("D"))) {
-            throwIllegalArgumentError(ExceptionMessage.UNEXPECTED_INPUT_MESSAGE.getMessage());
+            throwIllegalArgumentExceptionError(ExceptionMessage.UNEXPECTED_INPUT_MESSAGE.getMessage());
         }
     }
 
     public void validateRetryInput(String gameCommand) throws IllegalArgumentException {
         if(!(gameCommand.equals("R")||gameCommand.equals("Q"))) {
-            throwIllegalArgumentError(ExceptionMessage.UNEXPECTED_INPUT_MESSAGE.getMessage());
+            throwIllegalArgumentExceptionError(ExceptionMessage.UNEXPECTED_INPUT_MESSAGE.getMessage());
         }
     }
 }
