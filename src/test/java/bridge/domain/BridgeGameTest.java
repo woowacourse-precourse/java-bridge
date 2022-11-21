8122b34e83cdb0662f5fb754e83f740a0de3ed73
01192mg <01192mg@gmail.com>
2022-11-22 07:03:16 +0900

    test(Package): 테스트 코드 패키지 이동
    main 과 동일한 패키지 위치로 변경했다.

--- src/test/java/bridge/domain/BridgeGameTest.java
+++ src/test/java/bridge/domain/BridgeGameTest.java
@@ -1,9 +1,7 @@
-package bridge;
+package bridge.domain;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-import bridge.domain.BridgeGame;
-import bridge.domain.Moving;
 import java.lang.reflect.Field;
 import java.util.List;
 import org.junit.jupiter.api.DisplayName;
--- src/test/java/bridge/domain/BridgeMapTest.java
+++ src/test/java/bridge/domain/BridgeMapTest.java
@@ -1,10 +1,7 @@
-package bridge;
+package bridge.domain;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-import bridge.domain.BridgeGameState;
-import bridge.domain.BridgeMap;
-import bridge.domain.Moving;
 import java.util.List;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
