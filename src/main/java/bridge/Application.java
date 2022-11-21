2bd1ccc74b55550fb30905f8dd44543507ba80ba
5nam <ohnam90@gmail.com>
2022-11-21 17:08:48 +0900

    feat: 게임 진행 로직 코드 추가

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -1,12 +1,44 @@
 package bridge;
 
 import bridge.Controller.BridgeGame;
+import bridge.Model.Bridge;
+import bridge.Model.BridgeMaker;
+import bridge.Model.InputView;
+import bridge.View.OutputView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Application {
 
     public static void main(String[] args) {
         // TODO: 프로그램 구현
         BridgeGame bridgeGame = new BridgeGame();
-        bridgeGame.move();
+        BridgeNumberGenerator bridgeNumberGenerator = new BridgeRandomNumberGenerator();
+        BridgeMaker bridgeMaker = new BridgeMaker(bridgeNumberGenerator);
+
+        System.out.println("다리 건너기 게임을 시작합니다.\n");
+        // 다리 길이 입력
+        int size = InputView.getInstance.readBridgeSize();
+        List<String> madeBridge = bridgeMaker.makeBridge(size);
+        List<List<String>> userBridge = new ArrayList<>();
+
+        // 방향 입력
+        while(bridgeGame.getCount() <= size) {
+            String userDirection = InputView.getInstance.readMoving();
+            userBridge = bridgeGame.move(madeBridge, userDirection);
+            OutputView.getInstance.printMap(userBridge);
+
+            if(!Bridge.bridge.isRightAnswer()) {
+                String cont = InputView.getInstance.readGameCommand();
+                if(bridgeGame.retry(cont)) {
+                    continue;
+                }
+                break;
+            }
+        }
+
+        // 최종 결과
+        OutputView.getInstance.printResult(userBridge, bridgeGame.getGameResult(), bridgeGame.getTried());
     }
 }
