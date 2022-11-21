2016679a069cd22539be8334c2414ebca5438432
Torres-09 <dla612@naver.com>
2022-11-21 20:20:41 +0900

    feat : 다리의 끝까지 도착했는 지 알려주는 기능 추가

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -5,5 +5,10 @@ public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
         BridgeGame bridgeGame = new BridgeGame(InputView.readBridgeSize());
+        while (!bridgeGame.isEnd()) {
+            if (!bridgeGame.move(InputView.readMoving())) {
+
+            }
+        }
     }
 }
--- src/main/java/bridge/BridgeGame.java
+++ src/main/java/bridge/BridgeGame.java
@@ -20,13 +20,14 @@ public class BridgeGame {
      * 사용자가 칸을 이동할 때 사용하는 메서드
      * <p>
      * 이동을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
-     * 사용자의 입력을 인자로 받는다. 다리의 현재 위치에서 건널 수 있는 곳과 입력이 일치하면 한 칸 나아가고 true 반환, 일치하지 않으면 false 반환
+     * 사용자의 입력을 인자로 받는다. 다리의 현재 위치에서 건널 수 있는 곳과 입력이 일치하면 한 칸 나아가고 true 반환, 일치하지 않으면 위치를 처음으로 돌리고, false 반환(종료)
      */
     public boolean move(String moveInput) {
         if (bridge.get(currentIndex).equals(moveInput)) {
             currentIndex++;
             return true;
         }
+        currentIndex = 0;
         return false;
     }
 
@@ -35,6 +36,19 @@ public class BridgeGame {
      * <p>
      * 재시작을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void retry() {
+    public boolean retry(String gameCommandInput) {
+        if(gameCommandInput.equals("Q"))
+            return false;
+        return true;
+    }
+
+    /**
+     * 다리 끝까지 도달한 상태인 지 알려주는 메서드
+     * @return true 이면 종료, false 이면 진행 중
+     */
+    public boolean isEnd() {
+        if(bridge.size()==currentIndex)
+            return true;
+        return false;
     }
 }
