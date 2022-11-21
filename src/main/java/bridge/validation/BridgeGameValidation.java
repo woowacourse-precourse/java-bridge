cdb7f272b723875f3179fb34561ac3cfe41e49e4
desfox <desfox99@naver.com>
2022-11-21 16:45:30 +0900

    refactor(BridgeGameValidation): 이동 기능 검증 로직 리팩토링

--- src/main/java/bridge/validation/BridgeGameValidation.java
+++ src/main/java/bridge/validation/BridgeGameValidation.java
@@ -6,20 +6,20 @@ public class BridgeGameValidation {
     private static final String UNEXPECTED_WHEN_EQUAL = "X";
     private static final String UNEXPECTED_WHEN_UNEQUAL = "O";
 
-    public void validateMoveWhenAvailable(String movingCommand, List<String> bridge, List<String> bridgeCurrStatus) {
-        int lastIdx = bridge.size() - 1;
+    public void validateMoveWhenAvailable(String movingCommand, String currBox, List<String> bridgeCurrStatus) {
+        if (movingCommand.equals(currBox)) {
+            int lastIdx = bridgeCurrStatus.size() - 1;
 
-        if (movingCommand.equals(bridge.get(lastIdx))) {
             if (bridgeCurrStatus.get(lastIdx).equals(UNEXPECTED_WHEN_EQUAL)) {
                 throw new IllegalArgumentException("[ERROR] 이동 가능한 칸이지만 이동하지 않았습니다.");
             }
         }
     }
 
-    public void validateNotMoveWhenUnavailable(String movingCommand, List<String> bridge, List<String> bridgeCurrStatus) {
-        int lastIdx = bridge.size() - 1;
+    public void validateNotMoveWhenUnavailable(String movingCommand, String currBox, List<String> bridgeCurrStatus) {
+        if (! movingCommand.equals(currBox)) {
+            int lastIdx = bridgeCurrStatus.size() - 1;
 
-        if (! movingCommand.equals(bridge.get(lastIdx))) {
             if (bridgeCurrStatus.get(lastIdx).equals(UNEXPECTED_WHEN_UNEQUAL)) {
                 throw new IllegalArgumentException("[ERROR] 이동할 수 없는 칸으로 이동했습니다.");
             }
