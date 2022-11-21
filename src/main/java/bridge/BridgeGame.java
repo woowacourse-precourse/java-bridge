5f058eec8b75c9a06b5d2e00a1d1f8e7dd6384fc
yun <yshyeon102@gmail.com>
2022-11-21 23:09:47 +0900

    feat(BridgeGame): 브릿지게임에 필요한 필드와 생성자 추가

--- src/main/java/bridge/BridgeGame.java
+++ src/main/java/bridge/BridgeGame.java
@@ -1,9 +1,21 @@
 package bridge;
 
+import bridge.view.InputView;
+import bridge.view.OutputView;
+
 /**
  * 다리 건너기 게임을 관리하는 클래스
  */
 public class BridgeGame {
+    private final BridgeMaker bridgeMaker;
+    private final InputView inputView;
+    private final OutputView outputView;
+
+    public BridgeGame(BridgeMaker bridgeMaker, InputView inputView, OutputView outputView) {
+        this.bridgeMaker = bridgeMaker;
+        this.inputView = inputView;
+        this.outputView = outputView;
+    }
 
     /**
      * 사용자가 칸을 이동할 때 사용하는 메서드
