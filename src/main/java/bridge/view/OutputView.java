07b50e809c8720766cb0e239ec6cee04d7903160
iamjooon2 <iamjooon2@gmail.com>
2022-11-21 18:45:33 +0900

    feat: 현재까지 이동한 다리의 상태를 출력하는 기능

--- src/main/java/bridge/view/OutputView.java
+++ src/main/java/bridge/view/OutputView.java
@@ -1,17 +1,25 @@
 package bridge.view;
 
+import bridge.BridgeType;
+import bridge.MoveLog;
 import bridge.util.Message;
 
 /**
  * 사용자에게 게임 진행 상황과 결과를 출력하는 역할을 한다.
  */
 public class OutputView {
+    private final String BRIDGE_INIT = "[ ";
+    private final String BRIDGE_FIN = " ]";
+    private final String BRIDGE_BLOCK = " | ";
     /**
      * 현재까지 이동한 다리의 상태를 정해진 형식에 맞춰 출력한다.
      * <p>
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void printMap() {
+    public void printMap(MoveLog moveLog) {
+        System.out.println(BRIDGE_INIT + String.format("%s" , String.join(BRIDGE_BLOCK, moveLog.getBridgeMoveLog().get(BridgeType.UP))) + BRIDGE_FIN);
+        System.out.println(BRIDGE_INIT + String.format("%s" , String.join(BRIDGE_BLOCK, moveLog.getBridgeMoveLog().get(BridgeType.DOWN))) + BRIDGE_FIN);
+        enterLine();
     }
 
     /**
