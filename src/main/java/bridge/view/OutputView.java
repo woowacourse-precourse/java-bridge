310696832f59e316e9edd770d0a51112dd27c0ba
정승구 <jeongseung-gu@jeongseung-guui-MacBookAir.local>
2022-11-22 01:18:40 +0900

    refactor: Makeline클래스로 라인 만드는 메서드 따른 클래스로 분리

--- src/main/java/bridge/view/OutputView.java
+++ src/main/java/bridge/view/OutputView.java
@@ -6,6 +6,7 @@ public class OutputView {
     private static final int INCORRECT = 0;
     private static final int CORRECT = 1;
     private static final int RESULT_CORRECT = 2;
+    private MakeLine makeLine = new MakeLine();
 
     public void printMap(BridgeGame game, int isCollect) {
         if (isCollect == CORRECT) {
@@ -21,64 +22,16 @@ public class OutputView {
     }
 
     private void correctPrintMap(BridgeGame game) {
-        System.out.println(makeLine(game, "UP"));
-        System.out.println(makeLine(game, "DOWN"));
+        System.out.println(makeLine.makeLine(game, "UP"));
+        System.out.println(makeLine.makeLine(game, "DOWN"));
         System.out.println();
     }
 
     private void incorrectPrintMap(BridgeGame game) {
-        System.out.println(makeLineIncorrect(game, "UP"));
-        System.out.println(makeLineIncorrect(game, "DOWN"));
+        System.out.println(makeLine.makeLineIncorrect(game, "UP"));
+        System.out.println(makeLine.makeLineIncorrect(game, "DOWN"));
         System.out.println();
     }
-
-    private String makeLine(BridgeGame game, String type) {
-        String Line = PrintSentence.LEFT_WALL.getSentence();
-        for (int i = 0; i < game.getNowPosition() + 1; i++) {
-            Line += checkUpOrNot(game, i, type);
-            Line += checkLast(game, i);
-        }
-        Line += PrintSentence.RIGHT_WALL.getSentence();
-        return Line;
-    }
-
-    private String checkUpOrNot(BridgeGame game, int index, String type) {
-        String line = "";
-        if (game.getBridge().get(index).equals(LineType.valueOf(type).getValue1())) {
-            line += LineType.valueOf(type).getValue3();
-        }
-        if (game.getBridge().get(index).equals(LineType.valueOf(type).getValue2())) {
-            line += " ";
-        }
-        return line;
-    }
-    private String checkLast(BridgeGame game, int index) {
-        if (index < game.getNowPosition()) {
-            return PrintSentence.MIDDLE_WALL.getSentence();
-        }
-        return "";
-    }
-
-
-
-    private String makeLineIncorrect(BridgeGame game, String type) {
-        String line = PrintSentence.LEFT_WALL.getSentence();
-        for (int i = 0; i < game.getNowPosition(); i++) {
-            line += checkUpOrNot(game, i, type);
-            line += checkLast(game, i);
-        }
-        line += makeLineIncorrectLastValue(game, type);
-        line += PrintSentence.RIGHT_WALL.getSentence();
-        return line;
-    }
-
-     private String makeLineIncorrectLastValue(BridgeGame game, String type) {
-        if (type.equals("UP")) {
-            return checkUpOrNot(game, game.getNowPosition(), "UP_FAIL");
-        }
-        return checkUpOrNot(game, game.getNowPosition(), "DOWN_FAIL");
-    }
-
 
     public void printResult(BridgeGame game, int winType) {
         System.out.println(PrintSentence.GAME_RESULT.getSentence());
