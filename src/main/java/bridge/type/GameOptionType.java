0ba666b78201ecb178589c6f387da6d1804f8bdd
qkrdmswl <eunji303@naver.com>
2022-11-22 03:30:21 +0900

    refactor: change GameOptionType enum to string variable

--- src/main/java/bridge/type/GameOptionType.java
+++ src/main/java/bridge/type/GameOptionType.java
@@ -1,15 +1,6 @@
 package bridge.type;
 
-public enum GameOptionType {
-    R("RESTART"), Q("QUIT");
-
-    private final String option;
-
-    GameOptionType(String option) {
-        this.option = option;
-    }
-
-    public String getOption() {
-        return option;
-    }
+public class GameOptionType {
+    public static final String RESTART = "R";
+    public static final String QUIT = "Q";
 }
