932151a4344c57e24b62afb102ca76fd70cde8ff
Geun Woo(Kay) Park <64457922+parklaus1078@users.noreply.github.com>
2022-11-21 14:31:06 +0900

    [ADD] 다리 게임 - 테스트 케이스 추가

--- src/test/java/bridge/ApplicationTest.java
+++ src/test/java/bridge/ApplicationTest.java
@@ -7,11 +7,81 @@ import static org.assertj.core.util.Lists.newArrayList;
 
 import camp.nextstep.edu.missionutils.test.NsTest;
 import java.util.List;
+
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class ApplicationTest extends NsTest {
 
     private static final String ERROR_MESSAGE = "[ERROR]";
+
+
+    @DisplayName("다리 길이 입력값이 3보다 낮을 때")
+    @Test
+    void 예외_테스트2() {
+        assertSimpleTest(() -> {
+            runException("1");
+            assertThat(output()).contains(ERROR_MESSAGE);
+        });
+    }
+
+    @DisplayName("다리 길이 입력값이 20보다 높을 때")
+    @Test
+    void 예외_테스트3() {
+        assertSimpleTest(() -> {
+            runException("40");
+            assertThat(output()).contains(ERROR_MESSAGE);
+        });
+    }
+
+    @DisplayName("다리 길이 입력값이 3과 20 사이의 숫자일 때")
+    @Test
+    void 기능_테스트1() {
+        assertSimpleTest(() -> {
+            runException("6");
+            assertThat(output()).contains("이동할 칸을 선택해주세요.");
+        });
+    }
+
+    @DisplayName("이동 선택 입력값이 U나 D가 아닐 때")
+    @Test
+    void 예외_테스트4() {
+        assertRandomNumberInRangeTest(() -> {
+            run("3", "e", "U", "D", "U");
+            assertThat(output()).contains(ERROR_MESSAGE);
+            assertThat(output()).contains(
+                    "최종 게임 결과",
+                    "[ O |   | O ]",
+                    "[   | O |   ]",
+                    "게임 성공 여부: 성공",
+                    "총 시도한 횟수: 1"
+            );
+
+            int upSideIndex = output().indexOf("[ O |   | O ]");
+            int downSideIndex = output().indexOf("[   | O |   ]");
+            assertThat(upSideIndex).isLessThan(downSideIndex);
+        }, 1, 0, 1);
+    }
+
+    @DisplayName("재시작/종료 선택 입력값이 R이나 Q가 아닐 때")
+    @Test
+    void 예외_테스트5() {
+        assertRandomNumberInRangeTest(() -> {
+            run("3", "U", "U", "x", "Q");
+            assertThat(output()).contains(ERROR_MESSAGE);
+            assertThat(output()).contains(
+                    "최종 게임 결과",
+                    "[ O | X ]",
+                    "[   |   ]",
+                    "게임 성공 여부: 실패",
+                    "총 시도한 횟수: 1"
+            );
+
+            int upSideIndex = output().indexOf("[ O | X ]");
+            int downSideIndex = output().indexOf("[   |   ]");
+            assertThat(upSideIndex).isLessThan(downSideIndex);
+        }, 1, 0, 1);
+    }
 
     @Test
     void 다리_생성_테스트() {
@@ -26,11 +96,11 @@ class ApplicationTest extends NsTest {
         assertRandomNumberInRangeTest(() -> {
             run("3", "U", "D", "U");
             assertThat(output()).contains(
-                "최종 게임 결과",
-                "[ O |   | O ]",
-                "[   | O |   ]",
-                "게임 성공 여부: 성공",
-                "총 시도한 횟수: 1"
+                    "최종 게임 결과",
+                    "[ O |   | O ]",
+                    "[   | O |   ]",
+                    "게임 성공 여부: 성공",
+                    "총 시도한 횟수: 1"
             );
 
             int upSideIndex = output().indexOf("[ O |   | O ]");
@@ -46,6 +116,7 @@ class ApplicationTest extends NsTest {
             assertThat(output()).contains(ERROR_MESSAGE);
         });
     }
+
 
     @Override
     protected void runMain() {
