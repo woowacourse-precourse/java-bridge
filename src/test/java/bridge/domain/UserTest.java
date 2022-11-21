c7781eb99a130ddeeb76158bd8e6939942b02b5c
Choi-JJunho <junho5336@gmail.com>
2022-11-21 12:42:30 +0900

    test(User): refactor Displayname

--- src/test/java/bridge/domain/UserTest.java
+++ src/test/java/bridge/domain/UserTest.java
@@ -11,7 +11,6 @@ import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
-@DisplayName("유저 테스트")
 class UserTest {
 
     Bridge bridge = new Bridge(
@@ -19,7 +18,7 @@ class UserTest {
     );
 
     @Test
-    @DisplayName("사용자의 초기값: step = 0, live = true")
+    @DisplayName("사용자의 초기값은 step = 0, live = true 이다")
     void initValue() {
         User user = new User();
         Assertions.assertThat(user.getStep()).isEqualTo(0);
@@ -31,8 +30,8 @@ class UserTest {
         Assertions.assertThat(user.isLive()).isTrue();
     }
 
-    @DisplayName("걸음수 테스트")
-    @ParameterizedTest(name = "사용자가 {0}번 움직이면 step은 {0}")
+    @DisplayName("사용자의 걸음수가 ")
+    @ParameterizedTest(name = "{0}번 움직이면 step은 {0}")
     @CsvSource({"1", "3", "5"})
     void moveAndCheckStep(int step) {
         User user = new User();
@@ -43,37 +42,33 @@ class UserTest {
     }
 
     @Nested
-    @DisplayName("U U U... Bridge에서 생존여부")
+    @DisplayName("사용자가 다리에서 ")
     class checkInBridge {
         User user = new User();
 
         @Test
-        @DisplayName("사용자가 U로 가면 생존")
+        @DisplayName("떨어지지 않았다.")
         void moveAndCheckLive() {
             user.move(bridge, "U");
             Assertions.assertThat(user.isLive()).isTrue();
         }
 
         @Test
-        @DisplayName("사용자가 D로 가면 사망")
+        @DisplayName("떨어졌다.")
         void moveAndCheckDead() {
             user.move(bridge, "D");
             Assertions.assertThat(user.isLive()).isFalse();
         }
     }
 
-    @Nested
-    @DisplayName("예외 테스트")
-    class badInput {
+    @DisplayName("사용자의 move에 잘못된값 입력시 예외")
+    @ParameterizedTest(name = "입력값: {0}")
+    @CsvSource({"a", "2", "hello world"})
+    void moveWithWrongInput(String input) {
         User user = new User();
 
-        @DisplayName("move에 잘못된 입력시 예외")
-        @ParameterizedTest(name = "{0} 입력시 예외")
-        @CsvSource({"a", "2", "hello world"})
-        void moveWithWrongInput(String input) {
-            assertThrows(IllegalArgumentException.class, () -> {
-                user.move(bridge, input);
-            });
-        }
+        assertThrows(IllegalArgumentException.class, () -> {
+            user.move(bridge, input);
+        });
     }
 }
