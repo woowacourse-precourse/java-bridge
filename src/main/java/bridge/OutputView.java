eef1a053af8a6acb8b29fc83c76f26a023a3b1b6
소대훈 <theogns0681@naver.com>
2022-11-21 16:44:26 +0900

    [refactor] 라인 최소화

--- src/main/java/bridge/OutputView.java
+++ src/main/java/bridge/OutputView.java
@@ -18,10 +18,8 @@ public class OutputView {
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
     public void printMap(int movingIdx, List<String> bridgeList, String input) {
-        String upString = upString(movingIdx, bridgeList, input);
-        String downString = downString(movingIdx, bridgeList, input);
-        System.out.println(upString);
-        System.out.println(downString);
+        System.out.println(upString(movingIdx, bridgeList, input));
+        System.out.println(downString(movingIdx, bridgeList, input));
         System.out.println();
     }
 
