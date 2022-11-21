21347bd0d439d1c5fc1f2fb95e20df04db0d11bc
BioBeBe <76714219+JaegeonYu@users.noreply.github.com>
2022-11-21 14:48:46 +0900

    fix: 메서드 시그니처 인자 변경 불가에 따른 수정

--- src/main/java/bridge/util/BridgeMaker.java
+++ src/main/java/bridge/util/BridgeMaker.java
@@ -24,9 +24,9 @@ public class BridgeMaker {
      * @param size 다리의 길이
      * @return 입력받은 길이에 해당하는 다리 모양. 위 칸이면 "U", 아래 칸이면 "D"로 표현해야 한다.
      */
-    public List<String> makeBridge(int bridgeSize) {
+    public List<String> makeBridge(int size) {
         List<String> bridge = new ArrayList<>();
-        for (int bridgeLength = 0; bridgeLength < bridgeSize; bridgeLength++) {
+        for (int bridgeLength = 0; bridgeLength < size; bridgeLength++) {
             int randomNumber = bridgeNumberGenerator.generate();
             if (randomNumber == UP_INT.num()) bridge.add(UP.key());
             if (randomNumber == DOWN_INT.num()) bridge.add(DOWN.key());
