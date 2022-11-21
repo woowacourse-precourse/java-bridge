897cb01cefde0657dcad9bd47ce5ad678e60f173
jungwoo <rlawjddn103@naver.com>
2022-11-21 14:22:37 +0900

    feat(BridgeGame): User 인스턴스를 선언하고, 해당 인스턴스를 사용해서 게임의 흐름 구현

--- src/main/java/bridge/controller/BridgeGame.java
+++ src/main/java/bridge/controller/BridgeGame.java
@@ -1,16 +1,28 @@
 package bridge.controller;
 
+import bridge.domain.User;
+
 /**
  * 다리 건너기 게임을 관리하는 클래스
  */
 public class BridgeGame {
+    private final User user = new User();
+
+    public void playGame() {
+        do {
+            if (!move()) {
+                retry();
+            }
+        } while (!user.isEnd());
+    }
 
     /**
      * 사용자가 칸을 이동할 때 사용하는 메서드
      * <p>
      * 이동을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void move() {
+    public boolean move() {
+        return user.move();
     }
 
     /**
@@ -19,5 +31,6 @@ public class BridgeGame {
      * 재시작을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
     public void retry() {
+        user.retry();
     }
 }
