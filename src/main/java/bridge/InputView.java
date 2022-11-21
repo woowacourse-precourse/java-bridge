b397d74f12c185b4f025f2edd075a999089baeb6
Minsoo Choi <lucas0209@yonsei.ac.kr>
2022-11-22 01:27:00 +0900

    feat(InputView): user input for moving
    Feature added:
    - Read the user input of moving direction(U,D)
    - Check for invalid input(U or D)
    - If invalid, IllegalArgumentException and error message from enum Messages class

--- src/main/java/bridge/InputView.java
+++ src/main/java/bridge/InputView.java
@@ -25,7 +25,14 @@ public class InputView {
      * 사용자가 이동할 칸을 입력받는다.
      */
     public String readMoving() {
-        return null;
+        System.out.println("이동할 칸을 선택해주세요. (위: U, 아래: D)");
+        String direction = Console.readLine();
+
+        if (checkInvalidDirection(direction)) {
+            throw new IllegalArgumentException(Messages.INVALID_DIRECTION.getErrorMsg());
+        }
+        System.out.println(direction);
+        return direction;
     }
 
     /**
