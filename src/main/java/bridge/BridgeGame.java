18e9efb1e9b7644c9f96b2ddf2723f351b44290f
JONGMIN SHIN <jongmin373@gmail.com>
2022-11-21 17:10:46 +0900

    Update<BridgeGame> : add getter methods

--- src/main/java/bridge/BridgeGame.java
+++ src/main/java/bridge/BridgeGame.java
@@ -13,6 +13,8 @@ public class BridgeGame {
     private boolean isSuccess;
     public BridgeGame(List<String> bridge) {
         this.bridge = bridge;
+        isSuccess = false;
+        gameState = new ArrayList<>();
     }
     /**
      * 사용자가 칸을 이동할 때 사용하는 메서드
@@ -45,5 +47,14 @@ public class BridgeGame {
         }
         // 게임 계속 진행
         return true;
+    }
+
+    // 게임 상황을 반환해주는 메서드
+    public List<String> getGameState() {
+        return gameState;
+    }
+    // 성공 여부를 반환하는 getter
+    public boolean getIsSuccess(){
+        return isSuccess;
     }
 }
