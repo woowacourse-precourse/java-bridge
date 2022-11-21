89905a37164e4bb7f1fc09df84935d0a8cf03f65
Min-ji99 <61926751+Min-ji99@users.noreply.github.com>
2022-11-21 23:34:23 +0900

    Update : 사용하지 않는 라이브러리 import 제거

--- src/test/java/bridge/domain/BridgeTest.java
+++ src/test/java/bridge/domain/BridgeTest.java
@@ -1,17 +1,13 @@
 package bridge.domain;
 
-import bridge.controller.BridgeGame;
 import bridge.util.Constants;
-import camp.nextstep.edu.missionutils.test.NsTest;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
-import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeTest;
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
 
 class BridgeTest{
     Bridge bridge;
