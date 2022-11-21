5c73fcadcc3813a038bb86a9b92f755ae88575a3
고동우 <shp05179@gmail.com>
2022-11-21 14:53:00 +0900

    feat: 에러메세지 출력기능
    인자로 받은 문자열 앞에 [ERROR] 를 붙여서 출력

--- src/main/java/bridge/OutputView.java
+++ src/main/java/bridge/OutputView.java
@@ -4,6 +4,8 @@ package bridge;
  * 사용자에게 게임 진행 상황과 결과를 출력하는 역할을 한다.
  */
 public class OutputView {
+    private static final String error = "[ERROR] ";
+
 
     /**
      * 현재까지 이동한 다리의 상태를 정해진 형식에 맞춰 출력한다.
@@ -19,5 +21,9 @@ public class OutputView {
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
     public void printResult() {
+    }
+
+    public static void printError(String errorMessage) {
+        System.out.println(error + errorMessage);
     }
 }
