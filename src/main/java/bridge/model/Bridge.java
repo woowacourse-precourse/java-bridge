4b79245113b2578ba19bfb842a372868c33a4346
Lee-Jiseung <ljss98@naver.com>
2022-11-21 16:36:38 +0900

    feat: 이동 가능한지 확인하기

--- src/main/java/bridge/model/Bridge.java
+++ src/main/java/bridge/model/Bridge.java
@@ -14,4 +14,16 @@ public class Bridge {
         this.user = new User();
         this.status = Status.GAME_PROCESSING;
     }
+
+    public boolean isMovable(String move) {
+        user.addMove(move);
+        if (!bridge.get(user.getMoveCount() - 1).equals(move)) {
+            this.status = Status.GAME_OVER;
+            return false;
+        }
+        if (bridge.size() == user.getMoveCount()) {
+            this.status = Status.GAME_FINISHED;
+        }
+        return true;
+    }
 }
--- src/main/java/bridge/model/User.java
+++ src/main/java/bridge/model/User.java
@@ -5,4 +5,12 @@ import java.util.List;
 
 public class User {
     private final List<String> moves = new ArrayList<>();
+
+    public void addMove(String move) {
+        moves.add(move);
+    }
+
+    public int getMoveCount() {
+        return moves.size();
+    }
 }
