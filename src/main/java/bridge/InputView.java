804f9ff1f28e5525d3d82693fc39416f6aad6373
CJ <choijun0627@naver.com>
2022-11-21 19:04:22 +0900

    <feat>(InputView.java): 기능 요구 사항 12번 구현

--- src/main/java/bridge/InputView.java
+++ src/main/java/bridge/InputView.java
@@ -57,4 +57,10 @@ public class InputView {
         checkGameCommand(gameCommand);
         return gameCommand;
     }
+
+    public void checkGameCommand(String gameCommand) {
+        if(!gameCommand.equals("R")||!gameCommand.equals("Q")){
+            throw new IllegalArgumentException("[ERROR] 다시 시도할지 여부는 R이나 Q만 입력하세요.");
+        }
+    }
 }
