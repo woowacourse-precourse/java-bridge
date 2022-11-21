91dcc1873d74d7619d88675da66a75ec8e17c8a4
KAispread <pain0121@naver.com>
2022-11-21 21:31:56 +0900

    feat: 생성된 다리의 유효성을 검사한다

--- src/main/java/bridge/domain/BridgeMaker.java
+++ src/main/java/bridge/domain/BridgeMaker.java
@@ -26,14 +26,13 @@ public class BridgeMaker {
      * @return 입력받은 길이에 해당하는 다리 모양. 위 칸이면 "U", 아래 칸이면 "D"로 표현해야 한다.
      */
     public List<String> makeBridge(int size) {
-        bridgeValidator.validateBridgeSize(String.valueOf(size));
         List<String> bridge = new ArrayList<>();
         for (int i = 0; i < size; i++) {
             int value = bridgeNumberGenerator.generate();
             String command = MoveCommand.parseValueToCommand(value);
             bridge.add(command);
         }
-
+        bridgeValidator.validateBridge(bridge);
         return bridge;
     }
 }
