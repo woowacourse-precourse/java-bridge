1ac84c51af58c49aa578af6e032165e3d52eaef4
Hoo93 <117077999+Hoo93@users.noreply.github.com>
2022-11-21 13:15:04 +0900

    FIX[BridgeGame]: 객체 안에 InputView,OutputView 사용 제거

--- src/main/java/bridge/BridgeGame.java
+++ src/main/java/bridge/BridgeGame.java
@@ -3,9 +3,6 @@ package bridge;
 import java.util.ArrayList;
 import java.util.List;
 
-/**
- * 다리 건너기 게임을 관리하는 클래스
- */
 public class BridgeGame {
 
     private static final String BLANK = " ";
@@ -16,28 +13,15 @@ public class BridgeGame {
 
 
     private final List<String> bridge;
-    private final InputView inputView;
-    private final OutputView outputView;
 
     public List<String> upperLine = new ArrayList<>();
     public List<String> lowerLine = new ArrayList<>();
 
-    public BridgeGame(List<String> bridge, InputView inputView, OutputView outputView){
+    public BridgeGame(List<String> bridge){
         this.bridge = bridge;
-        this.inputView = inputView;
-        this.outputView = outputView;
     }
 
-    public boolean move() {
-        for (int index = 0; index < bridge.size(); index++) {
-            if (!moveOne(index)) {
-                outputView.printMap(upperLine, lowerLine);
-                return false;
-            }
-            outputView.printMap(upperLine,lowerLine);
-        }
-        return true;
-    }
+
     public void markCorrect(String userInput){
         if ( userInput.equals(UP)) {
             upperLine.add(CORRECT);
@@ -68,22 +52,15 @@ public class BridgeGame {
             markIncorrect(userInput);
         }
     }
-
-    public boolean moveOne(int index) {
-        String userInput = inputView.readMoving();
-        if(bridge.get(index).equals(userInput)){
-            makeMap(true,userInput);
-            return true;
+    public boolean move(int index,String userInput) {
+        if(!bridge.get(index).equals(userInput)){
+            makeMap(false,userInput);
+            return false;
         }
-        makeMap(false,userInput);
-        return false;
+        makeMap(true,userInput);
+        return true;
     }
 
-    /**
-     * 사용자가 게임을 다시 시도할 때 사용하는 메서드
-     * <p>
-     * 재시작을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
-     */
     public boolean retry(String readCommand) {
         if (readCommand.equals("R")) { return true; }
         return false;
