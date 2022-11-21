83374ef08d23035009710e36ace40172ee3c4460
yujiyea <54650556+yujiyea@users.noreply.github.com>
2022-11-22 03:33:00 +0900

    test(ValidationTest.java): 숫자 범위를 벗어난 경우 테스트

--- src/test/java/bridge/domain/ValidationTest.java
+++ src/test/java/bridge/domain/ValidationTest.java
@@ -6,12 +6,18 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
-import static bridge.domain.Validation.isValidBlankToMove;
-import static bridge.domain.Validation.isValidRetryOrEnd;
-import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static bridge.domain.Validation.*;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 
 public class ValidationTest {
+    @DisplayName("범위를 벗어난 경우")
+    @ParameterizedTest
+    @ValueSource(ints = {1,2,25,30})
+    void overOrUnderRange(int number){
+        assertThatThrownBy(() -> validateRange(number))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
     @DisplayName("U, D이외의 값을 넣은 경우")
     @ParameterizedTest
     @CsvSource({"sdfda", "dddd", "UUU","DDDD","Dkjs","UD"})
