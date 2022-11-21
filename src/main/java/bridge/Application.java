6a52455791bd0472d747dd528d24004eacc71079
Seiyon Park <seiyonpdev@gmail.com>
2022-11-22 08:25:25 +0900

    feat(Application): 메소드 play 구현
    다리 건너기 게임을 실행하는 메소드를 구현했다

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -1,8 +1,20 @@
 package bridge;
 
+import java.util.*;
+
 public class Application {
 
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        play();
+    }
+
+    private static void play() {
+        BridgeGame bridgeGame = new BridgeGame();
+        List<String> bridge;
+
+        OutputView.printStartMessage();
+        bridge = bridgeBundle();
+        moveOrRetry(bridgeGame, bridge);
+        OutputView.printResult(bridgeGame);
     }
 }
