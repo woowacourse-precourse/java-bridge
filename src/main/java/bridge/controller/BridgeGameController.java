0a5be511fcfad5f283bb7dfd129648f923435450
phdljr <phdljr@naver.com>
2022-11-22 02:47:42 +0900

    refactor: controller 로직 수정

--- src/main/java/bridge/controller/BridgeGameController.java
+++ src/main/java/bridge/controller/BridgeGameController.java
@@ -1,13 +1,14 @@
 package bridge.controller;
 
 import bridge.config.BridgeGameConfig;
-import bridge.model.Bridge;
-import bridge.model.BridgeGame;
 import bridge.generator.BridgeMaker;
 import bridge.generator.BridgeRandomNumberGenerator;
+import bridge.model.Bridge;
+import bridge.model.BridgeGame;
 import bridge.view.InputView;
 import bridge.view.OutputView;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class BridgeGameController {
@@ -26,13 +27,41 @@ public class BridgeGameController {
         this.bridgeMaker = new BridgeMaker(new BridgeRandomNumberGenerator());
     }
 
-    public void play(){
-        while(isPlaying){
-            makeBridge();
-            move();
-            showResult();
-            checkRetry();
+    public void play() {
+        makeBridge();
+        move();
+        showResult();
+    }
+
+    private void makeBridge() {
+        int size = inputView.readBridgeSize();
+        List<String> rawAnswerBridge = bridgeMaker.makeBridge(size);
+
+        Bridge answerBridge = new Bridge(rawAnswerBridge);
+        Bridge currentBridge = new Bridge(new ArrayList<>());
+        bridgeGame.setUp(answerBridge, currentBridge);
+
+        outputView.printStart();
+    }
+
+    private void move() {
+        while (isPlaying && !isAnswer()) {
+            String inputMoving = inputView.readMoving();
+            bridgeGame.move(inputMoving);
+            outputView.printMap();
+            if (!checkCurrentState()) {
+                checkRetry();
+            }
         }
+    }
+
+    /**
+     * 현재까지 이동해온 다리가 정답의 일부분인지 확인한다.
+     *
+     * @return 정답의 일부분이라면 true
+     */
+    private boolean checkCurrentState() {
+        return bridgeGame.checkCurrentState();
     }
 
     private void showResult() {
@@ -40,32 +69,18 @@ public class BridgeGameController {
     }
 
     private void checkRetry() {
+        outputView.printGameCommand();
         String command = inputView.readGameCommand();
-        if(command.equals(BridgeGameConfig.GAME_RETRY)) {
+        if (command.equals(BridgeGameConfig.GAME_RETRY)) {
             bridgeGame.retry();
             return;
         }
-        if(command.equals(BridgeGameConfig.GAME_EXIT)) {
+        if (command.equals(BridgeGameConfig.GAME_EXIT)) {
             isPlaying = false;
-        }
-    }
-
-    private void move() {
-        while(!isAnswer()) {
-            String inputMoving = inputView.readMoving();
-            bridgeGame.move(inputMoving);
-            outputView.printMap(inputMoving);
         }
     }
 
     private boolean isAnswer() {
         return bridgeGame.correctAnswer();
-    }
-
-    private void makeBridge() {
-        int size = inputView.readBridgeSize();
-        List<String> rawAnswerBridge = bridgeMaker.makeBridge(size);
-        Bridge answerBridge = new Bridge(rawAnswerBridge);
-        bridgeGame.setAnswerBridge(answerBridge);
     }
 }
