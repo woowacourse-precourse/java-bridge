153319a1d67705051ee4a84e78b24f383ce2a300
thousandcows <milcamboo@gmail.com>
2022-11-21 21:23:38 +0900

    feat: create run method

--- src/main/java/bridge/BridgeGame.java
+++ src/main/java/bridge/BridgeGame.java
@@ -4,7 +4,12 @@ package bridge;
  * 다리 건너기 게임을 관리하는 클래스
  */
 public class BridgeGame {
+    /**
+     * 다리 건너기 게임을 실행하는 메서드
+     */
+    public void run() {
 
+    }
     /**
      * 사용자가 칸을 이동할 때 사용하는 메서드
      * <p>
