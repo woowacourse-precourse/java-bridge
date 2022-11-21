ae875191967c752a3337171bc3f1e5105e341d89
JONGMIN SHIN <jongmin373@gmail.com>
2022-11-21 17:09:17 +0900

    update<Application> : create GameStarter Object

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -1,8 +1,10 @@
 package bridge;
 
-public class Application {
+import java.util.List;
 
+public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        new GameStarter();
     }
 }
