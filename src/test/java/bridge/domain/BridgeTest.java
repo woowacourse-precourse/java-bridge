82413f58f441cb99a73bd813220fb13bab04c74b
LeeHaiLim <hailim1997@gmail.com>
2022-11-22 06:35:49 +0900

    test(domain): refactor test code to reduce duplication

--- src/test/java/bridge/domain/BridgeTest.java
+++ src/test/java/bridge/domain/BridgeTest.java
@@ -3,36 +3,34 @@ package bridge.domain;
 import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeTest;
 import static org.assertj.core.api.Assertions.assertThat;
 
+import java.util.List;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 public class BridgeTest {
-    @DisplayName("다리가 특정 길이로 생성되는 지 테스트 한다.")
+    Bridge bridge = new Bridge(List.of("U","D","U"));
+    
+    @DisplayName("정확한 길이가 반환되는 지 테스트 한다.")
     @Test
     void bridgeSizeTest() {
-        Bridge bridge = new Bridge(10);
-        assertThat(bridge.size()).isEqualTo(10);
+        assertThat(bridge.size()).isEqualTo(3);
     }
 
     @DisplayName("특정 위치로 이동 가능할때 참을 반환하는지 테스트한다.")
-    @Test
-    void bridgeMovableTest() {
-        assertRandomNumberInRangeTest(() -> {
-            Bridge bridge = new Bridge(3);
-            assertThat(bridge.isMovable("U",0)).isTrue();
-            assertThat(bridge.isMovable("D",1)).isTrue();
-            assertThat(bridge.isMovable("U",2)).isTrue();
-        }, 1, 0, 1);
+    @ValueSource(strings = {"U,0","D,1","U,2"})
+    @ParameterizedTest
+    void bridgeMovableTest(String input) {
+        String[] inputs = input.split(",");
+        assertThat(bridge.isMovable(inputs[0],Integer.parseInt(inputs[1]))).isTrue();
     }
 
     @DisplayName("특정 위치로 이동 불가능할때 거짓을 반환하는지 테스트한다.")
-    @Test
-    void bridgeNotMovableTest() {
-        assertRandomNumberInRangeTest(() -> {
-            Bridge bridge = new Bridge(3);
-            assertThat(bridge.isMovable("D",0)).isFalse();
-            assertThat(bridge.isMovable("U",1)).isFalse();
-            assertThat(bridge.isMovable("D",2)).isFalse();
-        }, 1, 0, 1);
+    @ValueSource(strings = {"D,0","U,1","D,2"})
+    @ParameterizedTest
+    void bridgeNotMovableTest(String input) {
+        String[] inputs = input.split(",");
+        assertThat(bridge.isMovable(inputs[0],Integer.parseInt(inputs[1]))).isFalse();
     }
 }
