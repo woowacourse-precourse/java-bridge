75bab505378c70dd9d77a8acbe997ba8557c7ed6
juy4556 <kws05050@naver.com>
2022-11-21 22:10:00 +0900

    refactor: BridgeGame 객체 자율성을 고려하여 멤버변수 추가

--- src/main/java/bridge/BridgeGame.java
+++ src/main/java/bridge/BridgeGame.java
@@ -8,8 +8,10 @@ import java.util.List;
  */
 public class BridgeGame {
     private final List<String> bridge;
-    private final List<String> upBridge;
-    private final List<String> downBridge;
+    private List<String> upBridge;
+    private List<String> downBridge;
+    private boolean isQuit;
+    private int index;
 
     public BridgeGame(List<String> bridge) {
         this.bridge = bridge;
@@ -17,6 +19,8 @@ public class BridgeGame {
         upBridge.add("[");
         this.downBridge = new ArrayList<>();
         downBridge.add("[");
+        this.isQuit = false;
+        this.index = 0;
     }
 
     public List<String> getBridge() {
@@ -31,24 +35,33 @@ public class BridgeGame {
         return downBridge;
     }
 
+    public boolean getIsQuit() {
+        return isQuit;
+    }
+
+    public int getIndex() {
+        return index;
+    }
+
     /**
      * 사용자가 칸을 이동할 때 사용하는 메서드
      * <p>
      * 이동을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void move(String direction, int index) {
+    public void move(String direction) {
         String validBridge = bridge.get(index);
         DirectionType directionType = DirectionType.valueOfString(direction);
         MoveType[] moveResult = initializeMovingResult(validBridge, directionType);
         String downMove = moveResult[0].getValue();
         String upMove = moveResult[1].getValue();
         addBridge(downMove, upMove, index);
+        index += 1;
     }
 
     private void addBridge(String downMove, String upMove, int index) {
         downBridge.add(downMove);
         upBridge.add(upMove);
-        if (index < bridge.size()-1) {
+        if (index < bridge.size() - 1) {
             downBridge.add("|");
             upBridge.add("|");
             return;
