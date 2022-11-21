efa8a6aea3d6c906618cf7c2dbaeffe546836b01
jijongkwon <help3451@naver.com>
2022-11-21 14:22:44 +0900

    refactor : 기존 메시지 enum type 메시지로 대체

--- src/main/java/bridge/InputView.java
+++ src/main/java/bridge/InputView.java
@@ -6,12 +6,13 @@ import camp.nextstep.edu.missionutils.Console;
  * 사용자로부터 입력을 받는 역할을 한다.
  */
 public class InputView {
+    ViewMessage viewMessage;
 
     /**
      * 다리의 길이를 입력받는다.
      */
     public int readBridgeSize() {
-        System.out.println("다리의 길이를 입력해주세요.");
+        System.out.println(viewMessage.INPUT_BRIDGE_SIZE_MESSAGE.getMessage());
         return Integer.parseInt(Console.readLine());
     }
 
@@ -19,7 +20,7 @@ public class InputView {
      * 사용자가 이동할 칸을 입력받는다.
      */
     public String readMoving() {
-        System.out.println("이동할 칸을 선택해주세요. (위: U, 아래: D)");
+        System.out.println(viewMessage.INPUT_DIRECTION_MOVE_BRIDGE_MESSAGE.getMessage());
         return Console.readLine();
     }
 
