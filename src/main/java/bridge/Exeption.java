de6195e7cfd5342c8631271d9f42b6793df7a9ac
JONGMIN SHIN <jongmin373@gmail.com>
2022-11-21 16:51:53 +0900

    Feat<Exeption> : create Exeption class

--- src/main/java/bridge/Exeption.java
+++ src/main/java/bridge/Exeption.java
@@ -5,16 +5,5 @@ package bridge;
  */
 public class Exeption {
 
-    //숫자가 아닐 때 {
-    public boolean isDigit(String number) {
-        try{
-            Integer.parseInt(number);
-        }catch (IllegalArgumentException e) {
-            System.out.println(e + "[ERROR] : 숫자를 입력해 주세요.");
-            return false;
-        }
-        return true;
-    }
-
 
 }
