04ac003eecceac92610846604ab555ad55a17287
KJE <12181501@inha.edu>
2022-11-21 15:15:36 +0900

    feat: InputView- readMoving과 readGameCommand 작성

--- src/main/java/bridge/InputView.java
+++ src/main/java/bridge/InputView.java
@@ -1,14 +1,8 @@
 package bridge;
 import camp.nextstep.edu.missionutils.Console;
 
-/**
- * 사용자로부터 입력을 받는 역할을 한다.
- */
 public class InputView {
 
-    /**
-     * 다리의 길이를 입력받는다.
-     */
     public static int readBridgeSize() {
         String bridgeSizeStr = Console.readLine();
         int bridgeSize = 0;
@@ -20,17 +14,21 @@ public class InputView {
         return bridgeSize;
     }
 
-    /**
-     * 사용자가 이동할 칸을 입력받는다.
-     */
     public static String readMoving() {
+        System.out.println("이동할 칸을 선택해주세요. (위: U, 아래: D)");
+        String moveChar = Console.readLine();
+        if(moveChar.equals("U") || moveChar.equals("D"))
+            return moveChar;
+        Application.errorHandling("U와 D 중에서만 입력해주세요.");
         return null;
     }
 
-    /**
-     * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
-     */
     public static String readGameCommand() {
+        System.out.println("게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)");
+        String moveChar = Console.readLine();
+        if(moveChar.equals("Q") || moveChar.equals("R"))
+            return moveChar;
+        Application.errorHandling("Q와 R 중에서만 입력해주세요.");
         return null;
     }
 }
