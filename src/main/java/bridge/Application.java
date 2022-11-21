b516c1e9544c5b84302d77dc1a13226a29ea43b3
Geun Woo(Kay) Park <64457922+parklaus1078@users.noreply.github.com>
2022-11-21 14:44:59 +0900

    [ADD] 다리 게임 - 다리 길이를 받는 기능 추가

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -1,8 +1,36 @@
 package bridge;
 
+import java.util.List;
+
 public class Application {
+    private static final InputView receiver = new InputView();
+    private static final BridgeNumberGenerator generator = new BridgeRandomNumberGenerator();
+    private static final BridgeMaker bridgeMaker = new BridgeMaker(generator);
 
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        System.out.printf(Constants.Messages.START.getMessage());
+        BridgeGame bridgeGame = createBridge();
+
+    }
+
+    // 다리를 생성
+    private static BridgeGame createBridge() {
+        int size = bridgeSizeUI();
+        List<String> bridge = bridgeMaker.makeBridge(size);
+        BridgeGame bridgeGame = new BridgeGame(bridge);
+
+        return bridgeGame;
+    }
+
+    // 다리 생성 UI : 부적절한 문자가 입력될 시, 다시 입력을 받을 준비
+    private static int bridgeSizeUI() {
+        System.out.printf(Constants.Messages.BRIDGE.getMessage());
+        while (true) {
+            try {
+                return receiver.readBridgeSize();
+            } catch (IllegalArgumentException e) {
+                System.out.println(e);
+            }
+        }
     }
 }
