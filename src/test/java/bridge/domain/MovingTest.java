a6d03a5febd3687439ab4c12258de753fc27b8fd
Isu Shin <isu010614@naver.com>
2022-11-21 18:07:07 +0900

    test: U 혹은 D가 아닌 문자를 입력할 경우 에러 발생 후 메시지 출력하는지 확인하는 테스트 코드 작성

--- src/test/java/bridge/domain/MovingTest.java
+++ src/test/java/bridge/domain/MovingTest.java
@@ -2,6 +2,8 @@ package bridge.domain;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
@@ -10,8 +12,17 @@ public class MovingTest {
     @Test
     void validateNullInput() {
         final String inputMoving = "";
-        assertThatThrownBy(() -> BridgeLength.createBridgeLength(inputMoving))
+        assertThatThrownBy(() -> Moving.createMoving(inputMoving))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("[ERROR] 다시 입력해주세요.");
+    }
+
+    @DisplayName("U나 D가 아닌 문자를 입력할 경우")
+    @ValueSource(strings = {"R", "-", "5", "-3", "-U", "DD", " U"})
+    @ParameterizedTest
+    void validateUpOrDown(String inputMoving) {
+        assertThatThrownBy(() -> Moving.createMoving(inputMoving))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("[ERROR] U(위), 혹은 D(아래)를 입력해주세요.");
     }
 }
