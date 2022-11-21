161a62a949f1c61f716f1de2e84f6c445ca8e59a
sora5421 <sojeoung536985@gmail.com>
2022-11-22 05:35:33 +0900

    feat : make inputBridgeLength method

--- src/main/java/bridge/controller/GameSystem.java
+++ src/main/java/bridge/controller/GameSystem.java
@@ -1,12 +1,25 @@
 package bridge.controller;
 
+import bridge.BridgeMaker;
+import bridge.BridgeRandomNumberGenerator;
+import bridge.data.BridgeGame;
+import bridge.view.InputView;
+import bridge.view.OutputView;
+
 public class GameSystem {
     private String state = "C";
     private int count = 0;
     private int attempt = 1;
 
-    public void inputBridgeLength() {
+    private final OutputView outputView = new OutputView();
+    private final InputView inputView = new InputView();
+    private final BridgeMaker make = new BridgeMaker(new BridgeRandomNumberGenerator());
+    private final BridgeGame game  = new BridgeGame();
 
+    public void inputBridgeLength() {
+        outputView.printWelcome();
+        int saveSize = inputView.readBridgeSize();
+        game.saveBridge(make.makeBridge(saveSize));
     }
 
     public void calculate() {
