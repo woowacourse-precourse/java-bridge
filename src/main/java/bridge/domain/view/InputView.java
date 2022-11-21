8d62317f3979c2fd448119cd23d8cf62c65ab624
peterysh <52229539+peterysh@users.noreply.github.com>
2022-11-21 21:57:13 +0900

    refactor: refactor InputView directory to domain.view

---
+++ src/main/java/bridge/domain/view/InputView.java
@@ -0,0 +1,31 @@
+package bridge.domain.view;
+
+import bridge.global.BridgeMessage;
+import camp.nextstep.edu.missionutils.Console;
+
+/**
+ * 사용자로부터 입력을 받는 역할을 한다.
+ */
+public class InputView {
+
+    /**
+     * 다리의 길이를 입력받는다.
+     */
+    public void readBridgeSize() {
+
+    }
+
+    /**
+     * 사용자가 이동할 칸을 입력받는다.
+     */
+    public void readMoving() {
+
+    }
+
+    /**
+     * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
+     */
+    public void readGameCommand() {
+
+    }
+}
