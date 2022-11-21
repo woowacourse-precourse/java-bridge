7f514f0159a22434ca3caae9ade0408b4ec9c4d2
bearjun05 <bearjun05@naver.com>
2022-11-22 06:22:05 +0900

    feat : retry 메시지 출력 기능 구현

--- src/main/java/bridge/view/OutputView.java
+++ src/main/java/bridge/view/OutputView.java
@@ -41,4 +41,8 @@ public class OutputView {
             System.out.println();
         }
     }
+
+    public void printRetry() {
+        System.out.println("게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)");
+    }
 }
