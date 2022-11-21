df45a6e91ead65d3c693f7017ace0dcc20e76e38
himjjong <jjijji5728@tukorea.ac.kr>
2022-11-21 19:09:39 +0900

    feat : 게임 결과에 따른 두 줄 출력 기능 구현 완료

--- src/main/java/bridge/domain/BridgeGame.java
+++ src/main/java/bridge/domain/BridgeGame.java
@@ -12,17 +12,14 @@ import java.util.List;
  */
 public class BridgeGame {
     OrderView orderView = new OrderView();
-    OutputView outputView = new OutputView();
     List<String> upSide = new ArrayList<>();
     List<String> downSide = new ArrayList<>();
+    private int retryCount = 0;
     /**
      * 사용자가 칸을 이동할 때 사용하는 메서드
      * <p>
      * 이동을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    // moveStep 은 사용자가 이동할 칸을 입력받은것
-    // bridgeRandomData 는 안에 들어간 랜덤무작위 값
-    // 1 == 위 == U         ,       0 == 아래 == D
     public void move(String moveStep, String bridgeIndexData, int index) {
         if(moveStep.equals("U") && bridgeIndexData.equals("0")) { printResult(orderView.wrong(),orderView.space(), index);}
         if(moveStep.equals("U") && bridgeIndexData.equals("1")) { printResult(orderView.correct(),orderView.space(), index);}
@@ -34,12 +31,10 @@ public class BridgeGame {
         upSide.add(up);
         downSide.add(down);
 
-        if(index == 0){
-            System.out.println(upSide.toString());
-            System.out.printf(downSide.toString());
-        }
-        System.out.println(upSide.toString());
-        System.out.printf(downSide.toString());
+        for (String s : upSide) {   System.out.print(s);    }
+        orderView.lineSkip();
+        for (String s : downSide)  {    System.out.print(s);    }
+        orderView.lineSkip();
     }
 
     /**
