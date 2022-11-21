e247af7206828ba2e183b58a15340caf7ab83808
Devtist <yt930310@snu.ac.kr>
2022-11-21 13:06:33 +0900

    docs(README): TODO 추가

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -9,11 +9,8 @@ public class Application {
         // TODO: View 에서는 최소한의 검증만 -> 알파벳 대문자인지만 확인하고 도메인에서 확인할 것
         // TODO: 마지막에 실패한 경우 예외 처리
         // TODO: 요구사항 제발 제대로 찬찬히 읽어보기
-        try {
-            GameController gameController = new GameController();
-            gameController.control();
-        } catch (IllegalArgumentException e) {
-            System.out.println(e.getMessage());
-        }
+        // TODO: DTO 넘어가고 검증 도메인에서 잘 되고 있는지 확인하기 어디서 재실행 되는지 꼮꼭꼮꼭꼭꼭꼭!!!!!!!!!!!!!!!
+        GameController gameController = new GameController();
+        gameController.control();
     }
 }
