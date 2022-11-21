0331d5e582b1d47f4e1e4d1feb0a28ff50e7ea82
Il kwon <alexryu1105@naver.com>
2022-11-21 14:49:48 +0900

    feat: 게임 시작 메시지 출력 기능 구현

---
+++ src/main/java/bridge/BridgeGameConstant.java
@@ -0,0 +1,11 @@
+package bridge;
+
+public class BridgeGameConstant {
+
+    public static final String GAME_START_MESSAGE = "다리 건너기 게임을 시작합니다.\n"
+            + "\n"
+            + "다리의 길이를 입력해주세요.";
+
+    private BridgeGameConstant(){}
+
+}
--- src/main/java/bridge/view/OutputView.java
+++ src/main/java/bridge/view/OutputView.java
@@ -1,4 +1,6 @@
-package bridge;
+package bridge.view;
+
+import static bridge.BridgeGameConstant.GAME_START_MESSAGE;
 
 /**
  * 사용자에게 게임 진행 상황과 결과를 출력하는 역할을 한다.
@@ -19,5 +21,9 @@ public class OutputView {
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
     public void printResult() {
+    }
+
+    public void printStartMessage() {
+        System.out.println(GAME_START_MESSAGE);
     }
 }
