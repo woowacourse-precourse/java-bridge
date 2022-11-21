1996c5b8ae157c83775588c6ca2f69fdf2b1632b
hyun0404woo <hyun0404woo@gmail.com>
2022-11-21 15:25:47 +0900

    feat: 입력값을 적절히 매칭시키기 위한 메서드 생성
    - 사용자의 String 입력을 통해 적절한 enum 값을 반환할 수 있는 Map 생성
    - 사용자의 String 입력이 enum 객체에 포함되어있는 적절한 입력인지 확인하는 메서드

--- src/main/java/bridge/domain/GameCommand.java
+++ src/main/java/bridge/domain/GameCommand.java
@@ -1,13 +1,38 @@
 package bridge.domain;
 
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.stream.Collectors;
+
 public enum GameCommand {
     RESTART("R"),
     QUIT("Q"),
     ;
 
     private final String command;
+    private final static Map<String, GameCommand> gameCommandMap = new HashMap<>();
+
+    static {
+        Arrays.stream(values()).forEach(e -> gameCommandMap.put(e.command, e));
+    }
 
     GameCommand(String command) {
         this.command = command;
+    }
+
+    public String getCommand() {
+        return command;
+    }
+
+    public static boolean contains(final String command) {
+        return Arrays.stream(values())
+                .map(GameCommand::getCommand)
+                .collect(Collectors.toList())
+                .contains(command);
+    }
+
+    public static GameCommand getGameCommand(final String command) {
+        return gameCommandMap.get(command);
     }
 }
--- src/main/java/bridge/domain/Moving.java
+++ src/main/java/bridge/domain/Moving.java
@@ -1,13 +1,38 @@
 package bridge.domain;
 
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.stream.Collectors;
+
 public enum Moving {
     UP("U"),
     DOWN("D"),
     ;
 
     private final String direction;
-    
+    private final static Map<String, Moving> movingMap = new HashMap<>();
+
+    static {
+        Arrays.stream(values()).forEach(e -> movingMap.put(e.direction, e));
+    }
+
     Moving(String direction) {
         this.direction = direction;
+    }
+
+    public String getDirection() {
+        return direction;
+    }
+
+    public static boolean contains(final String direction) {
+        return Arrays.stream(values())
+                .map(Moving::getDirection)
+                .collect(Collectors.toList())
+                .contains(direction);
+    }
+
+    public static Moving getMoveDirection(final String direction) {
+        return movingMap.get(direction);
     }
 }
