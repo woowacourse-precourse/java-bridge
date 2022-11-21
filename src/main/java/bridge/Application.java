f105fa2d5cbb946a38fef80cd4ca98cf74d679e0
Sunghyun-0 <zkdldk95@naver.com>
2022-11-21 22:58:43 +0900

    게임 진행 기능 / 재시작 기능 구현

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -1,8 +1,37 @@
 package bridge;
 
+import static bridge.BridgeGame.*;
+
+import java.util.List;
+
 public class Application {
+    private static final InputView inputView = new InputView();
+    private static final OutputView outputView = new OutputView();
+    private static final BridgeNumberGenerator bridgeNumberGenerator = new BridgeRandomNumberGenerator();
+    private static final BridgeMaker bridgeMaker = new BridgeMaker(bridgeNumberGenerator);
+    private static final BridgeGame bridgeGame = new BridgeGame();
+    private static int bridgesize;
+    private static List<String> bridge;
 
     public static void main(String[] args) {
         // TODO: 프로그램 구현
     }
+
+    public static void preceedGame(){
+        while(userMove.size()<bridgesize && gameQuiter.equals("R")){
+            outputView.printSelectMessage();
+            bridgeGame.move(inputView.readMoving());
+
+            String printedMessage = outputView.printMap(userMove,bridge);
+            determineRetry(printedMessage);
+        }
+    }
+
+    public static void determineRetry(String printedMessage){
+        if(printedMessage.contains("X")){
+            outputView.printRestartMessage();
+            bridgeGame.retry(inputView.readGameCommand());
+        }
+    }
+
 }
