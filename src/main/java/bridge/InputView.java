4fb5d96189b35dac7cef466392b974c9c3badba9
소대훈 <theogns0681@naver.com>
2022-11-21 16:34:19 +0900

    [refactor] 변수명 변경

--- src/main/java/bridge/InputView.java
+++ src/main/java/bridge/InputView.java
@@ -11,15 +11,15 @@ public class InputView {
     final String readBridgeSizeMs = "다리의 길이를 입력해주세요.";
     final String readMovingMs = "이동할 칸을 선택해주세요. (위: U, 아래: D)";
     final String readGameCommandMs = "게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)";
-    final String exceptNumber = "[ERROR] 3이상 20이하의 숫자를 입력해주세요.";
-    final String exceptMoving = "[ERROR] U 혹은 D를 입력해주세요.";
-    final String exceptRetry = "[ERROR] R 혹은 Q를 입력해주세요.";
-    final String startGame = "다리 건너기 게임을 시작합니다.\n";
+    final String sizeExceptionMs = "[ERROR] 3이상 20이하의 숫자를 입력해주세요.";
+    final String movingExceptionMs = "[ERROR] U 혹은 D를 입력해주세요.";
+    final String retryExceptionMs = "[ERROR] R 혹은 Q를 입력해주세요.";
+    final String gameStart = "다리 건너기 게임을 시작합니다.\n";
     int size;
     String input;
 
     public void printStart() {
-        System.out.println(startGame);
+        System.out.println(gameStart);
     }
 
     /**
@@ -42,8 +42,8 @@ public class InputView {
     }
 
     void sizeException() {
-        System.out.println(exceptNumber);
-        throw new IllegalArgumentException(exceptNumber);
+        System.out.println(sizeExceptionMs);
+        throw new IllegalArgumentException(sizeExceptionMs);
     }
 
     private boolean confirmInteger(String input) {
@@ -88,8 +88,8 @@ public class InputView {
     }
 
     private void retryException() {
-        System.out.println(exceptRetry);
-        throw new IllegalArgumentException(exceptRetry);
+        System.out.println(retryExceptionMs);
+        throw new IllegalArgumentException(retryExceptionMs);
     }
 
     private boolean confirmRetry() {
@@ -101,7 +101,7 @@ public class InputView {
     }
 
     void movingException() {
-        System.out.println(exceptMoving);
-        throw new IllegalArgumentException(exceptMoving);
+        System.out.println(movingExceptionMs);
+        throw new IllegalArgumentException(movingExceptionMs);
     }
 }
