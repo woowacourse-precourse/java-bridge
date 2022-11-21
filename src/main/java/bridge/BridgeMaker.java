d0c35ab67f646173185058d1d7c44a76a5eec8be
tjddnr7760 <tjddnr7760@naver.com>
2022-11-21 20:23:18 +0900

    fix : BridgeMaker 결과값 조건에 맞게 출력 재구현

--- src/main/java/bridge/BridgeMaker.java
+++ src/main/java/bridge/BridgeMaker.java
@@ -20,62 +20,17 @@ public class BridgeMaker {
      * @return 입력받은 길이에 해당하는 다리 모양. 위 칸이면 "U", 아래 칸이면 "D"로 표현해야 한다.
      */
     public List<String> makeBridge(int size) {
-        ArrayList<Integer> randomnumbers = new ArrayList<>();
-        StringBuilder sb = new StringBuilder("[");
+        List result = new ArrayList<String>();
 
         for (int loaction = 1; loaction <= size; loaction++) {
             int randomnumber = bridgeNumberGenerator.generate();
-            randomnumbers.add(randomnumber);
+            if(randomnumber == 1) {
+                result.add("U");
+            }
+            if(randomnumber == 0) {
+                result.add("D");
+            }
         }
-
-        sb = setFirstLine(randomnumbers, sb, size);
-        sb = setMiddleLine(sb);
-        sb = setSecondLine(randomnumbers, sb, size);
-        sb = setLastLine(sb);
-
-        List result =  List.of(sb);
         return result;
-    }
-
-    public StringBuilder setFirstLine(ArrayList<Integer> randomnumbers, StringBuilder sb, int size) {
-        for (int location = 0; location < size; location++) {
-
-            if (randomnumbers.get(location) == 1) {
-                sb.append(" U ");
-            }
-            if (randomnumbers.get(location) == 0) {
-                sb.append("   ");
-            }
-            sb.append("|");
-        }
-        return sb;
-    }
-
-    public StringBuilder setSecondLine(ArrayList<Integer> randomnumbers, StringBuilder sb, int size) {
-        for (int location = 0; location < size; location++) {
-            if (randomnumbers.get(location) == 0) {
-                sb.append(" D ");
-            }
-            if (randomnumbers.get(location) == 1) {
-                sb.append("   ");
-            }
-            sb.append("|");
-        }
-        return sb;
-    }
-
-    public StringBuilder setMiddleLine(StringBuilder sb) {
-        sb.deleteCharAt(sb.length()-1);
-        sb.append("]");
-        sb.append(System.lineSeparator());
-        sb.append("[");
-
-        return sb;
-    }
-
-    public StringBuilder setLastLine(StringBuilder sb) {
-        sb.deleteCharAt(sb.length()-1);
-        sb.append("]");
-        return sb;
     }
 }
