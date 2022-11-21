be3992ff1d01bb61aa051e7c9d9cc4815b8e8ffb
yoonjuho <dbswn990@naver.com>
2022-11-21 18:27:15 +0900

    refactor(ValidateInput): 메서드 명 변경

--- src/main/java/bridge/controller/ValidateInput.java
+++ src/main/java/bridge/controller/ValidateInput.java
@@ -3,19 +3,19 @@ package bridge.controller;
 import bridge.model.Column;
 
 public class ValidateInput {
-    int numeric(String input){
+    static int checkNumeric(String input){
         if(!input.matches("^[0-9]")){
             throw new IllegalArgumentException();
         }
         return Integer.parseInt(input);
     }
-    String letter(String input){
+    static String checkLetter(String input){
         if(!Column.isValidateLetter(input)){
             throw new IllegalArgumentException();
         }
         return input;
     }
-    void endLetter(String input){
+    static void checkEndLetter(String input){
         if(!input.equals(GameMessage.RETRY) || !input.equals(GameMessage.CLOSE)){
             throw new IllegalArgumentException();
         }
