a0163dd3ba87077b837b8894fbd103c40d13f5d7
jxixeun <jxixeun@gmail.com>
2022-11-22 04:21:53 +0900

    feat: 칸 이동 메서드 작성

--- src/main/java/bridge/BridgeGame.java
+++ src/main/java/bridge/BridgeGame.java
@@ -1,16 +1,39 @@
 package bridge;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * 다리 건너기 게임을 관리하는 클래스
  */
 public class BridgeGame {
+
+    public static List<String> upCells = new ArrayList<>();
+    public static List<String> downCells = new ArrayList<>();
+
+    public static String valueOfStatus(boolean status) {
+        if (status) {
+            return "O";
+        }
+        return "X";
+    }
 
     /**
      * 사용자가 칸을 이동할 때 사용하는 메서드
      * <p>
      * 이동을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void move() {
+    public boolean move(String moving, String cell) {
+        boolean result = moving.equals(cell);
+        if (cell.equals(CellType.DOWN.getCell())){
+            downCells.add(valueOfStatus(result));
+            upCells.add(" ");
+        }
+        if (cell.equals(CellType.UP.getCell())){
+            downCells.add(" ");
+            upCells.add(valueOfStatus(result));
+        }
+        return result;
     }
 
     /**
