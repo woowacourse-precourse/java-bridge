8af704e45280c08aaeca1fcb931d15d3f6af1d12
김수민(Sumin Kim) <sumin.kim.develop@gmail.com>
2022-11-22 00:47:53 +0900

    feat(Application): main 메서드 구현
    실제 게임을 실행하는 main 메서드 구현

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -1,8 +1,20 @@
 package bridge;
 
+import bridge.controller.GameController;
+
 public class Application {
 
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        GameController gameController = GameController.start();
+        boolean alive;
+        boolean restart;
+        do {
+            alive = gameController.play();
+            if (alive) {
+                break;
+            }
+            restart = gameController.restart();
+        } while (restart);
+        gameController.finish(alive);
     }
 }
