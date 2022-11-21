e03f290c700495dcc8bd044b48b7017430321468
Eunkeee <eunkeeeee@gmail.com>
2022-11-21 15:05:05 +0900

    chore(InputController): 클래스 삭제
    InputController

--- src/main/java/bridge/controller/InputController.java
+++
@@ -1,9 +0,0 @@
-package bridge.controller;
-
-import bridge.view.InputView;
-import bridge.view.OutputView;
-
-public class InputController {
-    private static InputView inputView = new InputView();
-    private static OutputView outputView = new OutputView();
-}
