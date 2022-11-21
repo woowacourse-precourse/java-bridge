260b1a5b2a5f6623454a892cee87cf454a8d2018
w0ntae <bitcampcoding@gmail.com>
2022-11-21 11:21:46 +0900

    feat(count): Add step counting
    Add counting for steps taken

--- src/main/java/bridge/Count.java
+++ src/main/java/bridge/Count.java
@@ -7,11 +7,25 @@ package bridge;
 public class Count {
   private int Attempts = 0;
   
+  private int Steps = 0;
+  
   public void increaseAttempt(){
     this.Attempts++;
   }
   
   public int getAttempts(){
     return this.Attempts;
+  }
+  
+  public void increaseSteps(){
+    this.Steps++;
+  }
+  
+  public int getSteps(){
+    return this.Steps;
+  }
+  
+  public void resetSteps(){
+    this.Steps = 0;
   }
 }
