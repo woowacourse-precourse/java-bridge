5c37885690938f07ad19d7164f946dd62e05025e
Seokhyun Lee <lseokhyun99@gmail.com>
2022-11-21 13:56:59 +0900

    feat(InputTry.java): 다리 크기 입력 시도 메소드 구현
    InputTry 클래스에
    다리 크기 입력 시도 메소드
    readBridgeSizeTry() 를 구현하였다.
    
    try-catch 구문을 이용하여
    입력된 값이 형식에 맞지 않아 오류 발생 시
    올바른 값을 입력할 때 까지 다시 입력 받는다.

--- src/main/java/bridge/InputTry.java
+++ src/main/java/bridge/InputTry.java
@@ -1,4 +1,18 @@
 package bridge;
 
 public class InputTry {
+    private static final InputView input = new InputView();
+
+    public static int readBridgeSizeTry(){
+        Integer bridgeSize = null;
+        while ( bridgeSize == null ) {
+            try {
+                bridgeSize = input.readBridgeSize();
+            } catch (Exception e) {
+                System.out.println(e.getMessage());
+                bridgeSize = null;
+            }
+        }
+        return bridgeSize;
+    }
 }
