e46a7030a5cbdc462a4d6ac3728d024ad2771758
bearjun05 <bearjun05@naver.com>
2022-11-21 19:03:57 +0900

    feat : 사용자가 이동할 칸 입력 기능 구현

--- src/main/java/bridge/view/InputView.java
+++ src/main/java/bridge/view/InputView.java
@@ -1,5 +1,7 @@
 package bridge.view;
 
+import camp.nextstep.edu.missionutils.Console;
+
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
  */
@@ -9,14 +11,16 @@ public class InputView {
      * 다리의 길이를 입력받는다.
      */
     public int readBridgeSize() {
-        return 0;
+        return Integer.parseInt(Console.readLine());
     }
 
     /**
      * 사용자가 이동할 칸을 입력받는다.
      */
     public String readMoving() {
-        return null;
+        String movingValue = Console.readLine();
+        System.out.println(movingValue);
+        return movingValue;
     }
 
     /**
