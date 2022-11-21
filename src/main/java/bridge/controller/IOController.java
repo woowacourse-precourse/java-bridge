1fdc229144cb9786fa35ed4cc47de423eaadbd35
ma-an-jong <alswhd1113@gmail.com>
2022-11-21 23:16:01 +0900

    fix(IOController) : 익명 클래스를 이용한 콜백에서 람다를 이용하도록 변경
    BREAKING CHANGE: IOController에서 익명 클래스를 이용하여 실행할 함수를 전달하는 방식에서
    람다식을 이용하여 실행할 함수를 전달
    
    Before :
    
        IOController
        {
             inputBridgeSize
             inputNextMove
             inputGameCommand
        }
    
    After :
    
        IOController
        {
             inputBridgeSize : 람다를 이용하여 함수 전달
             inputNextMove : 람다를 이용하여 함수 전달
             inputGameCommand : 람다를 이용하여 함수 전달
        }
    
        익명 클래스를 이용하여 함수를 전달하는 방식에서 람다식을 이용하는 방식으로 변경

--- src/main/java/bridge/controller/IOController.java
+++ src/main/java/bridge/controller/IOController.java
@@ -9,73 +9,54 @@ public class IOController {
     private InputView inputView;
     private OutputView outputView;
 
-    public IOController()
-    {
+    public IOController() {
         inputView = new InputView();
         outputView = new OutputView();
     }
 
-    private <T> T inputTemplate(InputCallback<T> inputCallback)
-    {
-        while(true) {
+    private <T> T inputTemplate(InputCallback<T> inputCallback) {
+        while (true) {
             try {
                 return inputCallback.input();
-            }
-            catch (IllegalArgumentException e) {
+            } catch (IllegalArgumentException e) {
                 outputView.printMessage(e.getMessage());
             }
         }
     }
 
-    public Integer inputBridgeSize()
-    {
-        return inputTemplate(new InputCallback<Integer>() {
-            @Override
-            public Integer input() {
-                outputView.printMessage(Message.BRIDGE_SIZE_INPUT_MESSAGE);
-                return inputView.readBridgeSize();
-            }
+    public Integer inputBridgeSize() {
+        return inputTemplate(() -> {
+            outputView.printMessage(Message.BRIDGE_SIZE_INPUT_MESSAGE);
+            return inputView.readBridgeSize();
         });
     }
 
-    public String inputNextMove()
-    {
-        return inputTemplate(new InputCallback<String>() {
-            @Override
-            public String input() {
-                outputView.printMessage(Message.MOVE_INPUT_MESSAGE);
-                return inputView.readMoving();
-            }
+    public String inputNextMove() {
+        return inputTemplate(() -> {
+            outputView.printMessage(Message.MOVE_INPUT_MESSAGE);
+            return inputView.readMoving();
         });
     }
 
-    public String inputGameCommand()
-    {
-        return inputTemplate(new InputCallback<String>() {
-            @Override
-            public String input() {
-                outputView.printMessage(Message.GAME_COMMAND_INPUT_MESSAGE);
-                return inputView.readGameCommand();
-            }
+    public String inputGameCommand() {
+        return inputTemplate(() -> {
+            outputView.printMessage(Message.GAME_COMMAND_INPUT_MESSAGE);
+            return inputView.readGameCommand();
         });
     }
 
-    public void printMap(String bridgeMap)
-    {
+    public void printMap(String bridgeMap) {
         outputView.printMap(bridgeMap);
     }
 
-    public void printResult(String gameResult)
-    {
+    public void printResult(String gameResult) {
         outputView.printMessage(Message.GAME_RESULT_MESSAGE);
         outputView.printResult(gameResult);
     }
 
-    public void printGameStartMessage()
-    {
+    public void printGameStartMessage() {
         outputView.printMessage(Message.GAME_START_MESSAGE);
     }
-
 
 
 }
