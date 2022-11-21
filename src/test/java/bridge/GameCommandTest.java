7e8ce6f2d732c00b1a7d709e54e9e356562f157a
aksmf1442 <aksmf1442@naver.com>
2022-11-22 06:07:37 +0900

    test(다리 건너기): GameCommand 생성하는 기능 테스트 구현

--- src/test/java/bridge/GameCommandTest.java
+++ src/test/java/bridge/GameCommandTest.java
@@ -1,5 +1,6 @@
 package bridge;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import bridge.domain.GameCommand;
@@ -7,6 +8,20 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class GameCommandTest {
+
+    @DisplayName("[성공] R로 GameCommand 생성")
+    @Test
+    void createGameCommandByR() {
+        String gameCommand = "R";
+        assertThat(GameCommand.of(gameCommand)).isEqualTo(GameCommand.R);
+    }
+
+    @DisplayName("[성공] Q로 GameCommand 생성")
+    @Test
+    void createGameCommandByQ() {
+        String gameCommand = "Q";
+        assertThat(GameCommand.of(gameCommand)).isEqualTo(GameCommand.Q);
+    }
 
     @DisplayName("[예외 발생] GameCommand을 생성할 때 R이나 Q가 아니라면 IllegalArgumentException 발생")
     @Test
