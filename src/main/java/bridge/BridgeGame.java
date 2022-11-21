380999ff85177d5c89a38bbf3957c712bede2d98
dsaf2007 <dsaf2007@naver.com>
2022-11-22 02:55:01 +0900

    feat : 게임 진행 기능 추가 이동 시 이동 가능 여부 출력, 이동 불가 지역으로 이동 시 재시작 여부 결정, 게임의 시행 횟수 측정, 성공시 게임 종료

--- src/main/java/bridge/BridgeGame.java
+++ src/main/java/bridge/BridgeGame.java
@@ -1,16 +1,43 @@
 package bridge;
 
+import java.util.List;
+import java.util.ArrayList;
+
 /**
  * 다리 건너기 게임을 관리하는 클래스
  */
 public class BridgeGame {
+
+    private static BridgeRandomNumberGenerator numberGenerator = new BridgeRandomNumberGenerator();
+    private static BridgeMaker bridgeMaker = new BridgeMaker(numberGenerator);
+    // private static InputView inputView = new InputView();
+    public static OutputView outputView = new OutputView();
+
+    private static int position = 0;
+    private int attempt = 1;
+
+    public static List<String> bridge;
+    // private static List<String> upside = new ArrayList<String>();
+    // private static List<String> downside = new ArrayList<String>();
+
+    public BridgeGame(int size) {
+        this.bridge = bridgeMaker.makeBridge(size);
+    }
 
     /**
      * 사용자가 칸을 이동할 때 사용하는 메서드
      * <p>
      * 이동을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void move() {
+    public String move(String moving) {
+        // /String moving = inputView.readMoving();
+        outputView.buildIndex(moving, isTransferable(moving, position), position);
+        outputView.printMap();
+        if (isGameOver(isTransferable(moving, position)) == true) {
+            return isTransferable(moving, position);
+        }
+        this.position++;
+        return moving;
     }
 
     /**
@@ -18,6 +45,41 @@ public class BridgeGame {
      * <p>
      * 재시작을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void retry() {
+    public boolean retry(String command) {
+        if (command.equals("Q")) {
+            outputView.printResult(isSuccess(),this.attempt);
+            return false;
+        }
+        this.position = 0;
+        this.attempt++;
+        outputView.resetIndex();
+        return true;
     }
+
+    public boolean isGameOver(String result) {
+        if (result.equals("X")) {
+            return true;
+        }
+        return false;
+    }
+
+    public String isTransferable(String moving, int index) {
+        if (bridge.get(index).equals(moving)) {
+            return "O";
+        }
+        return "X";
+    }
+
+    public boolean isSuccess() {
+        if (this.position == this.bridge.size()) {
+            return true;
+        }
+        return false;
+    }
+
+    public void getResult()
+    {
+        outputView.printResult(isSuccess(), attempt);
+    }
+
 }
