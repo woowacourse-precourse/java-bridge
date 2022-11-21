7d71972f468c5fc1928b00eba240738112408d78
Yerim lee <103623292+lyrWinterCat@users.noreply.github.com>
2022-11-21 18:38:08 +0900

    refactor (Direction ->Command) : 클래스명 변경 및 객체 추가

---
+++ src/main/java/bridge/vo/Command.java
@@ -0,0 +1,14 @@
+package bridge.vo;
+
+public abstract class Command {
+   public final static String UP = "U";
+   public final static String DOWN = "D";
+   public final static String RESTART = "R";
+   public final static String QUIT = "Q";
+   public final static String CORRECT = " O ";
+   public final static String WRONG = " X ";
+   public final static String BLANK = "   ";
+   public final static String SUCCESS = "성공";
+   public final static String FAIL = "실패";
+
+}
--- src/main/java/bridge/vo/Direction.java
+++
@@ -1,7 +0,0 @@
-package bridge.vo;
-
-public abstract class Direction {
-   public final static String UP = "U";
-   public final static String DOWN = "D";
-
-}
