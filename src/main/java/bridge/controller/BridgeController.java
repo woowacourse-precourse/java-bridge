c6efe65086d7a9dd57559090f248a4b1322f997c
SEONGJOON KIM <jobcho6320@naver.com>
2022-11-21 15:37:39 +0900

    refactor: 컨트롤러에 필드 변수 제거

--- src/main/java/bridge/controller/BridgeController.java
+++ src/main/java/bridge/controller/BridgeController.java
@@ -10,42 +10,41 @@ import bridge.view.OutputView;
 import java.util.List;
 
 public class BridgeController {
-    private final StringBuilder upsideResult = new StringBuilder("[]");
-    private final StringBuilder downsideResult = new StringBuilder("[]");
-    private boolean BRIDGE_GAME_RESULT = true;
     public void run() {
         OutputView.printGameStartMessage();
         int bridgeSize = InputView.inputBridgeSize();
+
         BridgeRandomNumberGenerator bridgeRandomNumberGenerator = new BridgeRandomNumberGenerator();
         BridgeMaker bridgeMaker = new BridgeMaker(bridgeRandomNumberGenerator);
         List<String> bridgeMakeResult = bridgeMaker.makeBridge(bridgeSize);
-        BridgeResult bridgeResult = new BridgeResult(upsideResult, downsideResult);
+        BridgeResult bridgeResult = new BridgeResult(true);
+        boolean gameResult = bridgeResult.getResult();
         BridgeGame bridgeGame = new BridgeGame(bridgeMakeResult, 1);
 
         System.out.println("다리 결과" + bridgeMakeResult);
         for (int bridgeIndex = 0; bridgeIndex < bridgeSize; bridgeIndex++) {
-            bridgeResult.replaceCloseBracket(bridgeIndex);
+            OutputView.replaceCloseBracket(bridgeIndex);
             String moveSide = InputView.inputMoving();
             if (bridgeGame.move(moveSide, bridgeIndex)) {
-                bridgeResult.moveSuccess(bridgeGame,moveSide, bridgeIndex);
-                OutputView.printMap(bridgeResult);
+                OutputView.moveSuccess(bridgeGame,moveSide, bridgeIndex);
+                OutputView.printMap();
                 continue;
             }
             if (!bridgeGame.move(moveSide, bridgeIndex)) {
-                bridgeResult.moveFail(bridgeGame,moveSide, bridgeIndex);
-                OutputView.printMap(bridgeResult);
-                BRIDGE_GAME_RESULT = bridgeGame.retry();
+                OutputView.moveFail(bridgeGame,moveSide, bridgeIndex);
+                OutputView.printMap();
+                gameResult = bridgeGame.retry();
             }
-            if (BRIDGE_GAME_RESULT) {
+            if (gameResult) {
                 bridgeGame.retryCount();
-                bridgeResult.goBackBeforeOneStep(bridgeIndex);
+                OutputView.goBackBeforeOneStep(bridgeIndex);
                 bridgeIndex--;
             }
-            if (!BRIDGE_GAME_RESULT) {
+            if (!gameResult) {
                 break;
             }
         }
-        OutputView.printBridgeResult(bridgeResult,BRIDGE_GAME_RESULT, bridgeGame.getTryCount());
+        OutputView.printBridgeResult(gameResult, bridgeGame.getTryCount());
     }
     /*
     1. 다리 길이 입력
--- src/main/java/bridge/domain/BridgeMaker.java
+++ src/main/java/bridge/domain/BridgeMaker.java
@@ -15,7 +15,6 @@ public class BridgeMaker {
         this.bridgeNumberGenerator = bridgeNumberGenerator;
     }
 
-
     /**
      * @param size 다리의 길이
      * @return 입력받은 길이에 해당하는 다리 모양. 위 칸이면 "U", 아래 칸이면 "D"로 표현해야 한다.
@@ -34,6 +33,5 @@ public class BridgeMaker {
             return "U";
         }
         return "D";
-
     }
 }
