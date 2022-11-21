c3cd9cabde09760226dcea4156153f2abe2d318a
Kijung Park <kjpark4321@naver.com>
2022-11-21 23:45:00 +0900

    feat: BridgeGame 필요 인스터스 변수 추가

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -4,5 +4,7 @@ public class Application {
 
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        BridgeMaker bridgeMaker = new BridgeMaker(new BridgeRandomNumberGenerator());
+        System.out.println(bridgeMaker.makeBridge(3));
     }
 }
--- src/main/java/bridge/BridgeGame.java
+++ src/main/java/bridge/BridgeGame.java
@@ -1,9 +1,23 @@
 package bridge;
 
+import java.util.List;
+
 /**
  * 다리 건너기 게임을 관리하는 클래스
  */
 public class BridgeGame {
+
+    private final BridgeMaker bridgeMaker;
+    private final List<String> answerBridge;
+
+    public BridgeGame(BridgeNumberGenerator bridgeNumberGenerator, List<String> answerBridge) {
+        this.bridgeMaker = new BridgeMaker(new BridgeRandomNumberGenerator());
+        this.answerBridge = answerBridge;
+    }
+
+    public void startGame(){
+
+    }
 
     /**
      * 사용자가 칸을 이동할 때 사용하는 메서드
