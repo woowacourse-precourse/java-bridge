54d49240e192db7f1ec2a6ea1363ea9032b4b9db
qkrdmswl <eunji303@naver.com>
2022-11-21 13:09:05 +0900

    feat(BridgePrintTool): store tool for drawing bridge

---
+++ src/main/java/utils/BridgePrintTool.java
@@ -0,0 +1,9 @@
+package utils;
+
+public class BridgePrintTool {
+    public static final String MATCH = "O";
+    public static final String NOT_MATCH = "X";
+    public static final String START = "[ ";
+    public static final String END = " ]";
+    public static final String BETWEEN = " | ";
+}
