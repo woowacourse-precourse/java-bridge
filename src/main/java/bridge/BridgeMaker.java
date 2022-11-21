0cbb28108e9974a9d8e41c4c92eb8b8fc7223657
w0ntae <bitcampcoding@gmail.com>
2022-11-21 10:54:01 +0900

    feat(bridgemaker): Add makebridge
    Add method to generate a string list for the bridge

--- src/main/java/bridge/BridgeMaker.java
+++ src/main/java/bridge/BridgeMaker.java
@@ -1,5 +1,6 @@
 package bridge;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -18,7 +19,13 @@ public class BridgeMaker {
      * @return 입력받은 길이에 해당하는 다리 모양. 위 칸이면 "U", 아래 칸이면 "D"로 표현해야 한다.
      */
     public List<String> makeBridge(int size) {
-        return null;
+        List<String> bridge = new ArrayList<>();
+        for(int c = 0; c < size; c++){
+            int temp = bridgeNumberGenerator.generate();
+            String result = convert(temp);
+            bridge.add(result);
+        }
+        return bridge;
     }
     
     
