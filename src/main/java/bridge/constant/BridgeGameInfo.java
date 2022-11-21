1f8dd1852db6c55921c8336e42c444e4cd129c3a
jeongjihun <wlgns2234@naver.com>
2022-11-21 18:19:14 +0900

    chore: 오타수정

--- src/main/java/bridge/constant/BridgeGameInfo.java
+++ src/main/java/bridge/constant/BridgeGameInfo.java
@@ -8,7 +8,7 @@ public enum BridgeGameInfo {
     SELECT_UP_DOWN("이동할 칸을 선택해주세요. (위: U, 아래: D)"),
     RESTART("게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)"),
     RESULT("최종 게임 결과"),
-    SUCSSES_FAILURE("게임 성공 여부: "),
+    SUCCESS_FAILURE("게임 성공 여부: "),
     TOTAL_TRY("총 시도한 횟수: ");
 
     public final String message;
