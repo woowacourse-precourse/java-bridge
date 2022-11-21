c404ca94fcb2925193365a17c87d4f3ab386cc98
qkdk <86948395+qkdk@users.noreply.github.com>
2022-11-22 02:11:06 +0900

    feat: 게임 재시작 기능 구현

---
+++ src/main/java/bridge/InputConstants.java
@@ -0,0 +1,19 @@
+package bridge;
+
+public enum InputConstants {
+    UP_MOVE("U"),
+    DOWN_MOVE("D"),
+    RETRY_GAME("R"),
+    QUIT_GAME("Q"),
+    ;
+
+    private String value;
+
+    InputConstants(String value) {
+        this.value = value;
+    }
+
+    public String getValue() {
+        return value;
+    }
+}
