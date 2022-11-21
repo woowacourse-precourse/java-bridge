40b8c85a289d57109258f88e4743b415dcd936fa
MJoon-Jung <gjgjajaj31@gmail.com>
2022-11-21 14:35:31 +0900

    test<GameMap>: 위로 이동 후 아래 없는 경로로 이동하면 지도의 위에는 O 아래에는 X가 그려지는지 확인 테스트 추가

--- src/test/java/bridge/model/GameMapTest.java
+++ src/test/java/bridge/model/GameMapTest.java
@@ -13,4 +13,12 @@ class GameMapTest {
         }
         assertThat(gameMap.toString()).isEqualTo("[ O | O ]\n[   |   ]");
     }
+
+    @Test
+    void 성공적으로_위로_이동후_아래_없는_경로로_이동하면_지도의_위에_O_아래에_X가_그려져야한다() {
+        GameMap gameMap = new GameMap();
+        gameMap.draw(new MoveResult(DrawType.SUCCESS, Direction.U));
+        gameMap.draw(new MoveResult(DrawType.FAIL, Direction.D));
+        assertThat(gameMap.toString()).isEqualTo("[ O |   ]\n[   | X ]");
+    }
 }
