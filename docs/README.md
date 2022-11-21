fa9fe88b937ff56e44441fb1d181b1fa9fdae319
xortmd <tmd2166@naver.com>
2022-11-22 04:11:17 +0900

    OutputView

--- docs/README.md
+++ docs/README.md
@@ -1,2 +1,3 @@
 BridgeMaker
 InputView
+OutputView
--- src/main/java/bridge/OutputView.java
+++ src/main/java/bridge/OutputView.java
@@ -12,7 +12,41 @@ public class OutputView {
      * <p>
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void printMap() {
+    public boolean printMap(List<String> passBridge, String moveInput) {
+        boolean checkTryUp = printMapUpAndDown(passBridge, moveInput, "U");
+        boolean checkTryDown = printMapUpAndDown(passBridge, moveInput, "D");
+        if(!checkTryUp || !checkTryDown)
+            return false;
+        return  true;
+    }
+
+    public boolean printMapUpAndDown(List<String> passBridge, String moveInput, String upAndDown) {
+        boolean tryGame = true;
+        printMapLine(passBridge, upAndDown);
+        if(moveInput.equals(upAndDown))
+            tryGame = printMapLastLocation(passBridge, moveInput);
+        if(!moveInput.equals(upAndDown))
+            System.out.println("   ]");
+        return tryGame;
+    }
+
+    public void printMapLine(List<String> passBridge, String upAndDown) {
+        System.out.print("[");
+        for(int i = 0; i < passBridge.size() - 1; i++) {
+            if(passBridge.get(i).equals(upAndDown))
+                System.out.print(" O |");
+            if(!passBridge.get(i).equals(upAndDown))
+                System.out.print("   |");
+        }
+    }
+
+    public boolean printMapLastLocation(List<String> passBridge, String moveInput) {
+        if(passBridge.get(passBridge.size() - 1).equals(moveInput)) {
+            System.out.println(" O ]");
+            return true;
+        }
+        System.out.println(" X ]");
+        return false;
     }
 
     /**
@@ -20,6 +54,8 @@ public class OutputView {
      * <p>
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void printResult() {
+    public void printResult(List<String> passBridge, String moveInput) {
+        System.out.println("최종 게임 결과");
+        printMap(passBridge, moveInput);
     }
 }
