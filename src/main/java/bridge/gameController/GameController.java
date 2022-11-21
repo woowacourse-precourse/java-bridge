372bf1855bc372fe43bf6ac0e3f42ea875c4db11
josolha <josolha@nate.com>
2022-11-22 01:03:29 +0900

    feat(GameController) : 전체기능 관련 컨트롤 객체 생성

---
+++ src/main/java/bridge/gameController/GameController.java
@@ -0,0 +1,35 @@
+package bridge.gameController;
+
+import bridge.service.BridgeGame;
+import bridge.view.InputView;
+import bridge.view.OutputView;
+import java.util.List;
+
+public class GameController {
+
+    private InputView inputView = new InputView();
+    private OutputView outputView = new OutputView();
+    private BridgeGame bridgeGame = new BridgeGame();
+
+    private List<String> builtBridge;
+
+    private int location;
+    private int cnt;
+
+    public void run() {
+        makeBridge();
+
+        boolean flag = true;
+        while (flag) {
+            cnt++;
+            boolean b = moveBridge();
+            if (b == true) {
+                return;
+            }
+            flag = retryCheck();
+        }
+
+//        outputView.printResult();
+    }
+}
+
