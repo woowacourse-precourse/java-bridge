82e2e370746c95d97368153bc45fc8c01f2b615d
jijongkwon <help3451@naver.com>
2022-11-21 13:08:28 +0900

    feat(BridgeMaker) : 다리생성 구현

--- src/main/java/bridge/BridgeMaker.java
+++ src/main/java/bridge/BridgeMaker.java
@@ -1,5 +1,6 @@
 package bridge;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -18,6 +19,19 @@ public class BridgeMaker {
      * @return 입력받은 길이에 해당하는 다리 모양. 위 칸이면 "U", 아래 칸이면 "D"로 표현해야 한다.
      */
     public List<String> makeBridge(int size) {
-        return null;
+        List<String> bridge = new ArrayList<>();
+
+        for (int i = 0; i < size; i++) {
+            int randomNumber = bridgeNumberGenerator.generate();
+            bridge.add(checkBridgeDirection(randomNumber));
+        }
+        return bridge;
+    }
+
+    public String checkBridgeDirection(int randomNumber) {
+        if (randomNumber == 0) {
+            return "D";
+        }
+        return "U";
     }
 }
