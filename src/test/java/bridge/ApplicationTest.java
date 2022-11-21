3377d3ed8cf9521ff43ffbce6c5a8ac921cc0f7f
Haaae <101785628+Haaae@users.noreply.github.com>
2022-11-22 00:40:36 +0900

    test(ApplicationTest) : 세 가지 입력 예외가 발생하면 예외문구를 출력하고 다시 입력을 받는 기능 테스트

--- src/test/java/bridge/ApplicationTest.java
+++ src/test/java/bridge/ApplicationTest.java
@@ -1,5 +1,8 @@
 package bridge;
 
+import bridge.domain.BridgeMaker;
+import bridge.domain.BridgeNumberGenerator;
+
 import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeTest;
 import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
 import static org.assertj.core.api.Assertions.assertThat;
@@ -7,6 +10,7 @@ import static org.assertj.core.util.Lists.newArrayList;
 
 import camp.nextstep.edu.missionutils.test.NsTest;
 import java.util.List;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class ApplicationTest extends NsTest {
@@ -21,6 +25,7 @@ class ApplicationTest extends NsTest {
         assertThat(bridge).containsExactly("U", "D", "D");
     }
 
+    @DisplayName("일반적인 기능 테스트")
     @Test
     void 기능_테스트() {
         assertRandomNumberInRangeTest(() -> {
@@ -31,6 +36,28 @@ class ApplicationTest extends NsTest {
                 "[   | O |   ]",
                 "게임 성공 여부: 성공",
                 "총 시도한 횟수: 1"
+            );
+
+            int upSideIndex = output().indexOf("[ O |   | O ]");
+            int downSideIndex = output().indexOf("[   | O |   ]");
+            assertThat(upSideIndex).isLessThan(downSideIndex);
+        }, 1, 0, 1);
+    }
+
+    @DisplayName("유효하지 않은 값을 입력했을 때 다시 값을 입력받는 기능 포함 테스트")
+    @Test
+    void 반복_입력_기능_테스트() {
+        assertRandomNumberInRangeTest(() -> {
+            run("1", "3", "u", "U", "D", "D", "r", "R", "U", "D", "U");
+            assertThat(output()).contains(
+                    "[ERROR] 다리 길이는 3과 20 사이의 숫자여야 합니다.",
+                    "[ERROR] 다리 이동은 'U(위)' 또는 'D(아래)'여야 합니다.",
+                    "[ERROR] 재시도 관련 명령어는 'R(재시작)' 또는 'Q(종료)'여야 합니다.",
+                    "최종 게임 결과",
+                    "[ O |   | O ]",
+                    "[   | O |   ]",
+                    "게임 성공 여부: 성공",
+                    "총 시도한 횟수: 2"
             );
 
             int upSideIndex = output().indexOf("[ O |   | O ]");
