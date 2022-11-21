52f9030c9dedb174cefbd5926d84cf1ac4f5ff18
sangeun-b <baek0007@algonquinlive.com>
2022-11-22 01:13:09 +0900

    feat: 게임 재시작 여부 기능 추가

--- src/main/java/bridge/view/InputView.java
+++ src/main/java/bridge/view/InputView.java
@@ -13,6 +13,7 @@ import camp.nextstep.edu.missionutils.Console;
 public class InputView {
     public static final String INPUT_BRIDGE_LENGTH = "다리의 길이를 입력해주세요.";
     public static final String INPUT_MOVE = "이동할 칸을 선택해주세요. (위: U, 아래: D)";
+    public static final String INPUT_RESTART = "게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)";
 
     /**
      * 다리의 길이를 입력받는다.
@@ -35,6 +36,7 @@ public class InputView {
      * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
      */
     public String readGameCommand() {
-        return null;
+        System.out.println(INPUT_RESTART);
+        return Console.readLine();
     }
 }
