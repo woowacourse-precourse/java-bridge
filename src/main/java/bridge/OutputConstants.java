e4d6ee8385ed2a84f0a1bb7014d3495fd57a26f3
hojunking96 <junsong96@naver.com>
2022-11-21 17:49:17 +0900

    feats(OutputConstants.java) : 문자열 상수를 모아둠

---
+++ src/main/java/bridge/OutputConstants.java
@@ -0,0 +1,19 @@
+package bridge;
+
+public class OutputConstants {
+    public final static String RESULT = "최종 게임 결과";
+    public final static String TOTAL_TRY = "총 시도한 횟수: ";
+    public final static String SUCCESS = "게임 성공 여부 : 성공";
+    public final static String FAIL = "게임 성공 여부 : 실패";
+    public final static String START = "다리 건너기 게임을 시작합니다.";
+    public final static String SIZE_REQUEST = "다리의 길이를 입력해주세요.";
+    public final static String MOVE_REQUEST = "이동할 칸을 선택해주세요. (위: U, 아래: D)";
+    public final static String RETRY_REQUEST = "게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)";
+    public final static String UP = "U";
+    public final static String DOWN = "D";
+    public final static String BLANK = " ";
+    public final static String CORRECT = "O";
+    public final static String WRONG = "X";
+    public final static String RETRY = "R";
+
+}
