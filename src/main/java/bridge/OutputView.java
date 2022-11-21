981f6927cc31151c2d4ab431dc23656557af9c91
JIWEON-JEONG <dlektl6@naver.com>
2022-11-21 19:53:49 +0900

    feat(OutputView) 재사용 여부 확인 기능

--- src/main/java/bridge/OutputView.java
+++ src/main/java/bridge/OutputView.java
@@ -48,6 +48,10 @@ public class OutputView {
     public void printResult() {
     }
 
+    public void askReplay() {
+        System.out.println("게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)");
+    }
+
     //printGettingStart
     private void printStartMessage() {
         System.out.println("다리 건너기 게임을 시작합니다.");
