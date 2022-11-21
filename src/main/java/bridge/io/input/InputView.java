f056d9e821a21c7c4023452f03265c7fd2404260
sanghyuk2 <givemesomefoodplz@gmail.com>
2022-11-22 02:12:25 +0900

    refactor: 입력 기능 리팩터링 구현
    각 입력값을 Validator를 통하여 검증하도록 리팩터링

--- src/main/java/bridge/io/input/InputView.java
+++ src/main/java/bridge/io/input/InputView.java
@@ -1,20 +1,32 @@
 package bridge.io.input;
 
+import bridge.util.validator.BridgeLengthValidator;
+import bridge.util.validator.RestartValidator;
+import bridge.util.validator.UpDownValidator;
+
 import static camp.nextstep.edu.missionutils.Console.readLine;
 
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
  */
 public class InputView {
+    private final BridgeLengthValidator blValidator;
+    private final UpDownValidator udValidator;
+    private final RestartValidator rValidator;
+
+    public InputView() {
+        this.blValidator = new BridgeLengthValidator();
+        this.udValidator = new UpDownValidator();
+        this.rValidator = new RestartValidator();
+    }
 
     /**
      * 다리의 길이를 입력받는다.
      */
     public int readBridgeSize() {
         System.out.println("다리의 길이를 입력해주세요.");
-        String bridgeLength = readLine();
 
-        return Integer.parseInt(bridgeLength);
+        return Integer.parseInt(blValidator.validate(readLine()));
     }
 
     /**
@@ -22,9 +34,8 @@ public class InputView {
      */
     public String readMoving() {
         System.out.println("이동할 칸을 선택해주세요. (위: U, 아래: D)");
-        String upOrDown = readLine();
 
-        return upOrDown;
+        return udValidator.validate(readLine());
     }
 
     /**
@@ -32,8 +43,7 @@ public class InputView {
      */
     public String readGameCommand() {
         System.out.println("게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)");
-        String gameCommand = readLine();
 
-        return gameCommand;
+        return rValidator.validate(readLine());
     }
 }
