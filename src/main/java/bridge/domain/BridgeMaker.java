8024ca98d4e3965823912459963cd9dda8332e08
SeongukBaek <bsu6235@gmail.com>
2022-11-21 18:18:54 +0900

    refactor: change makeBridge to use stream

--- src/main/java/bridge/domain/BridgeMaker.java
+++ src/main/java/bridge/domain/BridgeMaker.java
@@ -4,6 +4,8 @@ import bridge.domain.BridgeNumberGenerator;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 /**
  * 다리의 길이를 입력 받아서 다리를 생성해주는 역할을 한다.
@@ -21,11 +23,10 @@ public class BridgeMaker {
      * @return 입력받은 길이에 해당하는 다리 모양. 위 칸이면 "U", 아래 칸이면 "D"로 표현해야 한다.
      */
     public List<String> makeBridge(int size) {
-        List<String> bridge = new ArrayList<>();
-        for (int count = 0; count < size; count++) {
-            bridge.add(intToString(bridgeNumberGenerator.generate()));
-        }
-        return bridge;
+        return Stream.iterate(0, index -> index < size, index -> index + 1)
+                .map(number -> bridgeNumberGenerator.generate())
+                .map(this::intToString)
+                .collect(Collectors.toList());
     }
 
     private String intToString(int number) {
