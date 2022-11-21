c333ff4ac7ef508fade9fb84cc56e2dcb8071fd5
고동우 <shp05179@gmail.com>
2022-11-21 16:18:32 +0900

    feat: 현재까지 이동한 다리의 상태를 정해진 형식에 맞춰 출력하는 기능 추가
    의존성
    printMap
    -> arrToString : getMiddle로 얻은 리스트에 BRIDGE_SEP를 넣어 문자열로 리턴한다
    -> getMiddle : 건넌 다리의 리스트("U"과"D"로만 구성)를 토대로 윗부분 혹은 아랫부분의 상태를 어레이로 리턴한다. (공백 또는 O)
        -> getAvailableOrEmpty : getMiddle의 if분기를 담당한다.

--- src/main/java/bridge/OutputView.java
+++ src/main/java/bridge/OutputView.java
@@ -1,5 +1,8 @@
 package bridge;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * 사용자에게 게임 진행 상황과 결과를 출력하는 역할을 한다.
  */
@@ -8,14 +11,51 @@ public class OutputView {
     private static final String REQUEST_BRIDGE_SIZE = "다리의 길이를 입력해주세요.";
     private static final String REQUEST_UP_DOWN = "이동할 칸을 선택해주세요. (위: U, 아래: D)";
     private static final String REQUEST_RETRY_QUIT = "게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)";
+    private static final String EMPTY = " ";
+    private static final String BRIDGE_START = "[ ";
+    private static final String BRIDGE_END = " ]";
+    private static final String BRIDGE_SEP = " | ";
+    private static final String BRIDGE_AVAILABLE = "O";
+    private static final String BRIDGE_NOT_AVAILABLE = "X";
+    private static final String UP = "U";
+    private static final String DOWN = "D";
 
+
+    private static String getAvailableOrEmpty(String position, String upDown) {
+        if (!upDown.equals(position)) {
+            return EMPTY;
+        }
+        return BRIDGE_AVAILABLE;
+    }
+
+    private static List<String> getMiddle(List<String> crossedBridge, String position) {
+        List<String> middle = new ArrayList();
+        for (String upDown : crossedBridge) {
+            middle.add(getAvailableOrEmpty(position, upDown));
+        }
+        return middle;
+    }
+
+
+    private static String arrToString(List<String> availableCalced) {
+        String result = "";
+        for (String availableOrEmpty : availableCalced) {
+            result = result + availableOrEmpty + BRIDGE_SEP;
+        }
+        result = result.substring(0, result.length() - BRIDGE_SEP.length());
+        return result;
+    }
 
     /**
      * 현재까지 이동한 다리의 상태를 정해진 형식에 맞춰 출력한다.
      * <p>
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void printMap() {
+    public static void printMap(List<String> crossedBridge) {
+        String upside = BRIDGE_START + arrToString(getMiddle(crossedBridge, UP)) + BRIDGE_END;
+        String downside = BRIDGE_START + arrToString(getMiddle(crossedBridge, DOWN)) + BRIDGE_END;
+        System.out.println(upside);
+        System.out.println(downside);
     }
 
     /**
@@ -37,6 +77,7 @@ public class OutputView {
     public static void printRequestUpOrDown() {
         System.out.println(REQUEST_UP_DOWN);
     }
+
     public static void printRequestRetryOrQuit() {
         System.out.println(REQUEST_RETRY_QUIT);
     }
