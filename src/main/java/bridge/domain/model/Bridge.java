8fe12b3be23a2fc999ff8296708071938727c471
Sehbeom <j3beom@gmail.com>
2022-11-22 02:59:47 +0900

    feat(model): 다리 모델 예외처리 적용
    다리 모델 내 예외처리 기능 적용

--- src/main/java/bridge/domain/model/Bridge.java
+++ src/main/java/bridge/domain/model/Bridge.java
@@ -2,15 +2,18 @@ package bridge.domain.model;
 
 import bridge.constants.Direction;
 import bridge.constants.MovingPossibility;
+import bridge.exception.BridgeExceptionHandler;
 import java.util.ArrayList;
 import java.util.List;
 
 public class Bridge {
+
     private static List<String> bridge = new ArrayList<>();
     private static boolean bridgeCreated = false;
 
     public static boolean setBridge(List<String> passedBridge) {
         if (!Bridge.bridgeCreated) {
+            BridgeExceptionHandler.handleNotOnlyUOrDException(passedBridge);
             Bridge.bridge = passedBridge;
             Bridge.bridgeCreated = true;
             return true;
@@ -19,9 +22,8 @@ public class Bridge {
     }
 
     public static MovingPossibility checkMovingPossibility(int space, Direction DIRECTION) {
-        if (!Bridge.bridgeCreated) {
-            throw new IllegalStateException("[ERROR] 다리가 생성되지 않았습니다.");
-        }
+        BridgeExceptionHandler.handleBridgeNotCreatedException(bridgeCreated);
+        BridgeExceptionHandler.handleCheckSpaceOutOfRangeException(space);
 
         if (bridge.get(space).equals(DIRECTION.getDirection())) {
             return MovingPossibility.CAN_MOVE;
@@ -30,6 +32,8 @@ public class Bridge {
     }
 
     public static int getBridgeSize() {
+        BridgeExceptionHandler.handleBridgeNotCreatedException(bridgeCreated);
+
         return Bridge.bridge.size();
     }
 }
