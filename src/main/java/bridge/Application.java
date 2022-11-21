a1c5495b208c9608cc8bc5efe2b115ff2d26bfb5
DESKTOP-A922TNL\LeeJinho <hosboy93@naver.com>
2022-11-21 23:08:44 +0900

    feat($Application): 다리 그리기
    - 현재 위치를 알기 위해 idx 추가
    
    feat($OutputView): 다리 그리기 로직 추가
    
    - canNextMove를 통해 입력한 값과 현재 다리가 일치하는지 확인
    - 이동할 다리와 현재 다리의 값을 비교하여 O, X, 공백을 추가

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Application {
+    private static int idx = 0;
 
     public static void main(String[] args) {
         // TODO: 프로그램 구현
@@ -15,7 +16,7 @@ public class Application {
         int bridgeLength = InputView.readBridgeSize();
 
         // 다리 길이만큼 다리 생성
-        List<Integer> bridge = makeBridge(bridgeLength);
+        List<String> bridge = makeBridge(bridgeLength);
         System.out.print("현재 다리 : ");
         for(int i=0; i<bridge.size(); i++){
             System.out.print(bridge.get(i) + " ");
@@ -23,16 +24,24 @@ public class Application {
 
         // 이동할 칸 선택
         String nextMove = BridgeGame.move();
+
+        // 다리 그리기
+        OutputView.printMap(idx, bridge, nextMove);
     }
 
-    public static List<Integer> makeBridge(int bridgeLength) {
-        BridgeRandomNumberGenerator bridgeRandomNumberGenerator = new BridgeRandomNumberGenerator();
-        List<Integer> bridge = new ArrayList<>();
-
+    public static List<String> makeBridge(int bridgeLength) {
+        List<String> bridge = new ArrayList<>();
         for(int i=0; i<bridgeLength; i++) {
-            bridge.add(bridgeRandomNumberGenerator.generate());
+            bridge.add(getUpOrDown());
         }
-
         return bridge;
+    }
+
+    public static String getUpOrDown() {
+        BridgeRandomNumberGenerator bridgeRandomNumberGenerator = new BridgeRandomNumberGenerator();
+        if(bridgeRandomNumberGenerator.generate() == 1) {
+            return "U";
+        }
+        return "D";
     }
 }
--- src/main/java/bridge/OutputView.java
+++ src/main/java/bridge/OutputView.java
@@ -1,16 +1,26 @@
 package bridge;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
 /**
  * 사용자에게 게임 진행 상황과 결과를 출력하는 역할을 한다.
  */
 public class OutputView {
+    private final String START_POINT = "[ ";
+    private final String END_POINT = " ]";
+    private static final List<String> upBridge = new ArrayList<>();
+    private static final List<String> downBridge = new ArrayList<>();
 
     /**
      * 현재까지 이동한 다리의 상태를 정해진 형식에 맞춰 출력한다.
      * <p>
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void printMap() {
+    public static void printMap(int idx, List<String> bridge, String nextMove) {
+        boolean canNextMove = CheckNextMove(idx, bridge, nextMove);
+        nextMoving(nextMove, canNextMove);
     }
 
     /**
@@ -19,5 +29,52 @@ public class OutputView {
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
     public void printResult() {
+    }
+
+    public static void nextMoving(String nextMove, boolean canNextMove) {
+        if(Objects.equals(nextMove, "U")) {
+            MoveUpBridge(canNextMove);
+            notMoveDownBridge();
+        }
+        if(Objects.equals(nextMove, "D")) {
+            MoveDownBridge(canNextMove);
+            notMoveUpBridge();
+        }
+
+        for(int i=0; i<upBridge.size(); i++) {
+            System.out.print(upBridge.get(i) + " ");
+        }
+        System.out.println();
+        for(int i=0; i<downBridge.size(); i++) {
+            System.out.print(downBridge.get(i) + " ");
+        }
+    }
+
+    public static void notMoveUpBridge() {
+        upBridge.add(" ");
+    }
+    public static void notMoveDownBridge() {
+        downBridge.add(" ");
+    }
+
+    public static void MoveDownBridge(boolean canNextMove) {
+        if(canNextMove) downBridge.add("O");
+        if(!canNextMove) downBridge.add("X");
+    }
+
+    public static void MoveUpBridge(boolean canNextMove) {
+        if(canNextMove) upBridge.add("O");
+        if(!canNextMove)  upBridge.add("X");
+    }
+
+    public static boolean CheckNextMove(int idx, List<String> bridge, String nextMove) {
+        if(Objects.equals(nextMove, "U")) {
+            return SecondCheckNextMove(idx, bridge);
+        }
+        return !(SecondCheckNextMove(idx, bridge));
+    }
+
+    public static boolean SecondCheckNextMove(int idx, List<String> bridge) {
+        return Objects.equals(bridge.get(idx), "U");
     }
 }
