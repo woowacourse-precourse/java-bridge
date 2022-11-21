b7db309c47048cca173e6431864a7c45b5297986
hongjinwon <herenever@naver.com>
2022-11-22 03:58:43 +0900

    docs(view): InputView 구현
    readBridgeSize, readMoving, readGameCommand 구현
    InputView에서는 최소한의 검증만 진행

--- src/main/java/bridge/view/InputView.java
+++ src/main/java/bridge/view/InputView.java
@@ -1,28 +1,49 @@
 package bridge.view;
 
+import bridge.utils.ErrorMessage;
+import camp.nextstep.edu.missionutils.Console;
+
+import static bridge.utils.ErrorMessage.*;
+
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
  */
 public class InputView {
 
+    private final static String bridgeSizePattern = "^\\d*$";
+    private final static String movingPattern = "^[A-Z]$";
+    private final static String commandPattern = "^[A-Z]$";
+
     /**
      * 다리의 길이를 입력받는다.
      */
     public int readBridgeSize() {
-        return 0;
+        String bridgeSize = Console.readLine();
+        if (!bridgeSize.matches(bridgeSizePattern)){
+            throw new IllegalArgumentException(INVALID_BRIDGE_SIZE.message);
+        }
+        return Integer.parseInt(bridgeSize);
     }
 
     /**
      * 사용자가 이동할 칸을 입력받는다.
      */
     public String readMoving() {
-        return null;
+        String move = Console.readLine();
+        if (!move.matches(movingPattern)){
+            throw new IllegalArgumentException(INVALID_MOVING.message);
+        }
+        return move;
     }
 
     /**
      * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
      */
     public String readGameCommand() {
-        return null;
+        String gameCommand = Console.readLine();
+        if (!gameCommand.matches(commandPattern)){
+            throw new IllegalArgumentException(INVALID_RETRY.message);
+        }
+        return gameCommand;
     }
 }
