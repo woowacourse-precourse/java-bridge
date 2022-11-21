2751f8212d3eb7c198a9b60fc26914a299350d1f
jagajang <kosb7544@gmail.com>
2022-11-21 15:12:07 +0900

    dev: add output message

--- src/main/java/bridge/OutputView.java
+++ src/main/java/bridge/OutputView.java
@@ -54,15 +54,18 @@ public class OutputView {
     /** 추가 출력 메세지
      */
     public void printStartGameMessage() {
+        System.out.println("다리 건너기 게임을 시작합니다.");
     }
 
     public void printInputSizeMessage() {
-
+        System.out.println("다리의 길이를 입력해주세요.");
     }
 
     public void printMoveMessage() {
+        System.out.println("이동할 칸을 선택해주세요. (위: U, 아래: D)");
     }
 
     public void printRetryMessage() {
+        System.out.println("게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)");
     }
 }
