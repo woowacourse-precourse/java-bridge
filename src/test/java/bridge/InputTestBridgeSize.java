ffd61f41751a0a5ce763a2d1f5897d6fd6b4bab4
yumble <hnjn1101@gmail.com>
2022-11-21 12:25:47 +0900

    test : input test bridge size 클래스 코드수정

--- src/test/java/bridge/InputTestBridgeSize.java
+++ src/test/java/bridge/InputTestBridgeSize.java
@@ -1,65 +1,74 @@
 package bridge;
 
+import camp.nextstep.edu.missionutils.Console;
 import camp.nextstep.edu.missionutils.test.NsTest;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.util.Lists.newArrayList;
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 
 class InputTestBridgeSize extends NsTest {
 
     private static final String ERROR_MESSAGE = "[ERROR]";
-
-
+    private ValidationCheck validationCheck;
+    @BeforeEach
+    void beforeTestSetting(){
+        validationCheck = new ValidationCheck();
+    }
     @DisplayName("다리 길이 입력 테스트 : 숫자가 아닌 영어 입력")
     @Test
     void testBridgeSizeByNotANumber() {
-        assertSimpleTest(() -> {
-            runException("a");
-            assertThat(output()).contains(ERROR_MESSAGE);
-        });
+        InputStream in = new ByteArrayInputStream("a".getBytes());
+        System.setIn(in);
+        assertThatThrownBy(() -> validationCheck.toInts(Console.readLine()))
+                .isInstanceOf(IllegalArgumentException.class);
     }
     @DisplayName("다리 길이 입력 테스트 : 범위를 벗어나는 숫자 입력")
     @Test
     void testBridgeSizeOutOfRange1() {
-        assertSimpleTest(() -> {
-            runException("1");
-            assertThat(output()).contains(ERROR_MESSAGE);
-        });
+        InputStream in = new ByteArrayInputStream("1".getBytes());
+        System.setIn(in);
+        int bridgeSize = validationCheck.toInts(Console.readLine());
+        assertThatThrownBy(() -> validationCheck.checkBridgeSizeRange(bridgeSize))
+                .isInstanceOf(IllegalArgumentException.class);
     }
     @DisplayName("다리 길이 입력 테스트 : 범위를 벗어나는 숫자 입력 2")
     @Test
     void testBridgeSizeOutOfRange2() {
-        assertSimpleTest(() -> {
-            runException("21");
-            assertThat(output()).contains(ERROR_MESSAGE);
-        });
+        InputStream in = new ByteArrayInputStream("21".getBytes());
+        System.setIn(in);
+        int bridgeSize = validationCheck.toInts(Console.readLine());
+        assertThatThrownBy(() -> validationCheck.checkBridgeSizeRange(bridgeSize))
+                .isInstanceOf(IllegalArgumentException.class);
     }
     @DisplayName("다리 길이 입력 테스트 : 소수 입력")
     @Test
     void testBridgeSizeByFloatingNumber() {
-        assertSimpleTest(() -> {
-            runException("3.5");
-            assertThat(output()).contains(ERROR_MESSAGE);
-        });
+        InputStream in = new ByteArrayInputStream("3.5".getBytes());
+        System.setIn(in);
+        assertThatThrownBy(() -> validationCheck.toInts(Console.readLine()))
+                .isInstanceOf(IllegalArgumentException.class);
     }
     @DisplayName("다리 길이 입력 테스트 : space bar (whitespace 입력)")
     @Test
     void testBridgeSizeBySpaceBar() {
-        assertSimpleTest(() -> {
-            runException(" ");
-            assertThat(output()).contains(ERROR_MESSAGE);
-        });
+        InputStream in = new ByteArrayInputStream(" ".getBytes());
+        System.setIn(in);
+        assertThatThrownBy(() -> validationCheck.isBlank(Console.readLine()))
+                .isInstanceOf(IllegalArgumentException.class);
     }
     @DisplayName("다리 길이 입력 테스트 : 줄바뀜 문자 입력")
     @Test
     void testBridgeSizeByOpeningChar() {
-        assertSimpleTest(() -> {
-            runException("\n");
-            assertThat(output()).contains(ERROR_MESSAGE);
-        });
+        InputStream in = new ByteArrayInputStream("\n".getBytes());
+        System.setIn(in);
+        assertThatThrownBy(() -> validationCheck.isBlank(Console.readLine()))
+                .isInstanceOf(IllegalArgumentException.class);
     }
 
     @Override
