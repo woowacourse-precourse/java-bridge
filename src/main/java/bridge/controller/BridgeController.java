23bce12044f32b0f3f78dde29739f670a476fe50
alpacaHyun <flaehdan@gmail.com>
2022-11-21 15:02:23 +0900

    type : feat ✨ 컨트롤러 생성
    body :

--- src/main/java/bridge/controller/BridgeController.java
+++ src/main/java/bridge/controller/BridgeController.java
@@ -1,50 +1,85 @@
 package bridge.controller;
 
-import bridge.model.BridgeGame;
 import bridge.model.BridgeMaker;
 import bridge.model.BridgeNumberGenerator;
 import bridge.model.BridgeRandomNumberGenerator;
+import bridge.model.CompareBridge;
 import bridge.view.InputView;
 import bridge.view.OutputView;
+import camp.nextstep.edu.missionutils.Console;
 import java.util.ArrayList;
 import java.util.List;
 
 public class BridgeController {
+    // 랜덤한 다리 생성
+    public static List<String> randomBridge = new ArrayList<>();
+    private static BridgeMaker bm = new BridgeMaker(new BridgeRandomNumberGenerator());
+    public static int compareBridgeNum = 0;
+    public static int howManyGame = 1;
+    public static void BridgeGameStart() {
+        //게임 시작 선언
+        OutputView.printGameStart();
+        //다리 길이 입력
+        int bridgeSize = InputView.readBridgeSize();
 
-    private InputView inputView;
-    private OutputView outputView;
-    private BridgeMaker bridgeMaker;
-    private BridgeGame bridgeGame;
-    private int countGame;
-    private boolean restart;
+        //입력받은 길이대로 랜덤한 다리 생성
+        randomBridge = bm.getRandomBridge(bridgeSize);
 
-    private List<String> randomBridge;
-
-
-    public BridgeController() {
-        BridgeNumberGenerator bridgeNumberGenerator = new BridgeRandomNumberGenerator();
-        bridgeMaker = new BridgeMaker(bridgeNumberGenerator);
-        inputView = new InputView();
-        outputView = new OutputView();
-        countGame = 1;
-        restart = true;
-    }
-    public void startGame() {
-        outputView.printGameStartMessage();
-        makeBridge();
-
+        // 비교하기
+        compareBridge();
 
     }
 
-    public List<String> makeBridge() {
-        bridgeMaker = new BridgeMaker(new BridgeRandomNumberGenerator());
-        int size = inputView.readBridgeSize();
-        randomBridge = bridgeMaker.makeBridge(size);
+    public static void compareBridge() {
+        for (compareBridgeNum = 0; compareBridgeNum < CompareBridge.ranBridge.size(); ) {
+            String compare = InputView.readMoving();
+            CompareBridge.udCount++;
 
-        return randomBridge;
+            CompareBridge.compareUp(compareBridgeNum, compare);
+            CompareBridge.compareDown(compareBridgeNum, compare);
+
+        }
+        completeMap();
+    }
+
+    public static void retryGame() {
+        OutputView.printGameRestart();
+        String retry = Console.readLine();
+        restartMap(retry);
+        finishMap(retry);
+    }
+
+    // R 입력시 게임 재시작
+    public static void restartMap(String input) {
+        CompareBridge.udCount = 0;
+        compareBridgeNum = 0;
+        howManyGame++;
+        if (input.equals("R")) {
+            CompareBridge.sbUp.delete(0, CompareBridge.sbUp.length());
+            CompareBridge.sbDown.delete(0, CompareBridge.sbDown.length());
+        }
     }
 
 
+    // Q 입력 시 종료
+    public static void finishMap(String input) {
+        if (input.equals("Q")) {
+            System.out.println("최종 게임 결과");
+            System.out.println("[" + CompareBridge.sbUp + "]");
+            System.out.println("[" + CompareBridge.sbDown + "]");
+            System.out.println();
+            System.out.println("게임 성공 여부: " + "실패");
+            System.out.println("총 시도한 횟수: " + howManyGame);
+        }
+    }
 
-
+    public static void completeMap() {
+        System.out.println("최종 게임 결과");
+        System.out.println("[" + CompareBridge.sbUp + "]");
+        System.out.println("[" + CompareBridge.sbDown + "]");
+        System.out.println();
+        System.out.println("게임 성공 여부: " + "성공");
+        System.out.println("총 시도한 횟수: " + howManyGame);
+    }
 }
+
