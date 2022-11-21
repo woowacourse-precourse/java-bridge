0932bfe1c4d81e0e335b587419da644d5785eda7
cgk95 <kjkj5868@gmail.com>
2022-11-22 08:39:29 +0900

    refactor(Controller) : 불필요한 데이터 조작 기능의 삭제

--- src/main/java/bridge/BridgeMaker.java
+++ src/main/java/bridge/BridgeMaker.java
@@ -1,7 +1,5 @@
 package bridge;
 
-import bridge.Model.BridgeGame;
-
 import java.util.ArrayList;
 import java.util.List;
 
@@ -10,7 +8,6 @@ public class BridgeMaker {
 
     public BridgeMaker(BridgeNumberGenerator bridgeNumberGenerator) {
         this.bridgeNumberGenerator = bridgeNumberGenerator;
-        BridgeGame.bridge = makeBridge(BridgeGame.size); // 데이터베이스 역할을 하는 모델에 기록
     }
 
     public List<String> makeBridge(int size) {
