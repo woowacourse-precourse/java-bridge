9feb0ca0ce0f1464f322015e9dd7603f1217db66
kawkmin <86940335+kawkmin@users.noreply.github.com>
2022-11-21 20:16:26 +0900

    feat: printMap 출력만 구현

--- src/main/java/bridge/view/OutputView.java
+++ src/main/java/bridge/view/OutputView.java
@@ -1,5 +1,10 @@
 package bridge.view;
 
+import bridge.model.BridgeGame;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
 import static bridge.view.ViewEnum.OUTPUT_GAME_START_MESSAGE;
 
 /**
@@ -13,11 +18,41 @@ public class OutputView {
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
 
-    public static void printGameStart(){
+    public static void printGameStart() {
         System.out.println(OUTPUT_GAME_START_MESSAGE.getMessage());
     }
-    public void printMap() {
-    }
+
+    public static void printMap(BridgeGame bridgeGame) {
+        int bridgeSize=bridgeGame.getUpPresentBridge().size();
+
+        System.out.print("[");
+        for(int i=0;i<bridgeSize-1;i++) {
+            if(bridgeGame.getUserMoveList().get(i).equals("U")){
+                System.out.printf(" %s |",bridgeGame.getUpPresentBridge().get(i));
+            } else{
+                System.out.print("   |");
+            }
+        }
+        if(bridgeGame.getUserMoveList().get(bridgeSize-1).equals("U")){
+            System.out.printf(" %s ]\n",bridgeGame.getUpPresentBridge().get(bridgeSize-1));
+        }else{
+            System.out.printf("   ]\n");
+        }
+
+        System.out.print("[");
+        for(int i=0;i<bridgeSize-1;i++) {
+            if(bridgeGame.getUserMoveList().get(i).equals("D")){
+                System.out.printf(" %s |",bridgeGame.getDownPresentBridge().get(i-1));
+            } else{
+                System.out.print("   |");
+            }
+        }
+        if(bridgeGame.getUserMoveList().get(bridgeSize-1).equals("D")){
+            System.out.printf(" %s ]\n",bridgeGame.getDownPresentBridge().get(bridgeSize-1));
+        }else{
+            System.out.printf("   ]\n");
+        }
+}
 
     /**
      * 게임의 최종 결과를 정해진 형식에 맞춰 출력한다.
