5ac88abe6c185b472679b19d377ca698d8731a53
DaeyeonKim97 <kdysy1130@khu.ac.kr>
2022-11-21 12:43:02 +0900

    feat(input-view): readBridgeSize method
    additional implements
    - convertInputSize

--- src/main/java/bridge/InputView.java
+++ src/main/java/bridge/InputView.java
@@ -1,5 +1,7 @@
 package bridge;
 
+import camp.nextstep.edu.missionutils.Console;
+
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
  */
@@ -9,7 +11,22 @@ public class InputView {
      * 다리의 길이를 입력받는다.
      */
     public int readBridgeSize() {
-        return 0;
+        try {
+            System.out.println("다리의 길이를 입력해주세요.");
+            String inputSize = Console.readLine();
+            return convertInputSize(inputSize);
+        } catch (IllegalArgumentException e) {
+            System.out.println("[ERROR] " + e.getMessage());
+            return readBridgeSize();
+        }
+    }
+
+    private int convertInputSize(String inputSize) {
+        int size = Integer.parseInt(inputSize);
+        if (size < 3 || size > 20) {
+            throw new IllegalArgumentException("다리길이는 3이상 20이하여야 합니다");
+        }
+        return size;
     }
 
     /**
