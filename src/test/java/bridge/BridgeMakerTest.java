fb6cc9ff6dd1d484d94a9cde6aec14e012cebdbb
Ryokuman <ryokuman@naver.com>
2022-11-21 19:32:57 +0900

    feature ( BridgeMakerTest ) : add test for BridgeMaker

--- src/test/java/bridge/BridgeMakerTest.java
+++ src/test/java/bridge/BridgeMakerTest.java
@@ -1,4 +1,19 @@
 package bridge;
 
-public class BridgeMakerTest {
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.util.Lists.newArrayList;
+
+
+class BridgeMakerTest {
+    @Test
+    void makeBridgeTest() {
+        BridgeNumberGenerator numberGenerator = new ApplicationTest.TestNumberGenerator(newArrayList(1, 0, 0, 0, 0, 1, 1));
+        BridgeMaker bridgeMaker = new BridgeMaker(numberGenerator);
+        List<String> bridge = bridgeMaker.makeBridge(7);
+        assertThat(bridge).containsExactly("U", "D", "D", "D", "D", "U", "U");
+    }
 }
