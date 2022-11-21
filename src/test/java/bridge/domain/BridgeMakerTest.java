6da6a2fa5b9de43855241e7a76d80dd59a695bf7
Yohan <sprae114@naver.com>
2022-11-21 21:14:46 +0900

    test :  랜덤 다리 생성 길이에 따른 테스트 추가

--- src/test/java/bridge/domain/BridgeMakerTest.java
+++ src/test/java/bridge/domain/BridgeMakerTest.java
@@ -4,15 +4,30 @@ import bridge.BridgeRandomNumberGenerator;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 
 class BridgeMakerTest {
-
     @Test
     void 랜덤_다리_생성(){
         BridgeMaker bridgeMaker = new BridgeMaker(new BridgeRandomNumberGenerator());
         int length = 5;
+        assertThat(bridgeMaker.makeRandomBridgeProcess(length)).size().isEqualTo(length);
+    }
 
-        assertThat(bridgeMaker.makeBridge(length).size()).isEqualTo(length);
+    @Test
+    void 다리_길이가_작을때(){
+        BridgeMaker bridgeMaker = new BridgeMaker(new BridgeRandomNumberGenerator());
+        int length = 2;
+        assertThatThrownBy(() -> bridgeMaker.makeRandomBridgeProcess(length))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void 다리_길이가_클때(){
+        BridgeMaker bridgeMaker = new BridgeMaker(new BridgeRandomNumberGenerator());
+        int length = 21;
+        assertThatThrownBy(() -> bridgeMaker.makeRandomBridgeProcess(length))
+                .isInstanceOf(IllegalArgumentException.class);
     }
 }
