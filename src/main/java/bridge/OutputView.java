907dc73b86ca0c795b25e66c793039f4d09e2a7b
Ye Seok Hong <yshong1998@gmail.com>
2022-11-22 02:10:34 +0900

    Feat : 게임의 최종 결과를 보여주는 기능 추가

--- src/main/java/bridge/OutputView.java
+++ src/main/java/bridge/OutputView.java
@@ -1,36 +1,32 @@
 package bridge;
 
-import java.util.ArrayList;
 import java.util.List;
 
 /**
  * 사용자에게 게임 진행 상황과 결과를 출력하는 역할을 한다.
  */
 public class OutputView {
-    List<String> bridgeMap = new ArrayList<>();
-    /**
-     * 현재까지 이동한 다리의 상태를 정해진 형식에 맞춰 출력한다.
-     * <p>
-     * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
-     */
-    public void printMap(String playerMoving, String bridge) {
-        if(playerMoving.equals(bridge)) {
-            bridgeMap.add("O");
-        } else if(!playerMoving.equals(bridge)){
-            bridgeMap.add("X");
+
+    public void printBridgeMap(List<List<String>> bridgeMap){
+        for (int i = 0; i < bridgeMap.size(); i++) {
+            System.out.println("[ " + String.join(" | ", bridgeMap.get(i)) + " ]");
         }
-        System.out.println(bridgeMap);
     }
 
-    /**
-     * 게임의 최종 결과를 정해진 형식에 맞춰 출력한다.
-     * <p>
-     * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
-     */
-    public void printResult() {
+    public void printResult(List<List<String>> bridgeMap,String successOrNot,int gameRepeatCount) {
+        System.out.println("최종 게임 결과");
+        printBridgeMap(bridgeMap);
+        printSuccessOrNot(successOrNot);
+        printGameRepeatCount(gameRepeatCount);
+    }
+    public void printSuccessOrNot(String successOrNot){
+        System.out.println("게임 성공 여부: " + successOrNot);
+    }
+    public void printGameRepeatCount(int gameRepeatCount){
+        System.out.println("총 시도한 횟수: " + gameRepeatCount);
     }
 
-    public void printGameStart(){
-        System.out.printf("다리 건너기 게임을 시작합니다.\n\n");
+    public void printGameStart() {
+        System.out.print("다리 건너기 게임을 시작합니다.\n\n");
     }
 }
