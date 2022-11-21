f3d1928cd9865ffdfe7473dd20908109af6054ae
Voyager003 <85725033+Voyager003@users.noreply.github.com>
2022-11-22 00:16:11 +0900

    Feat: 추가 - printResult, printGamestatus, printAttempt printResult: 게임 최종 결과를 종합하여 출력하는 메서드 printGamestatus: 게임 성공, 실패 유무를 알려주는 메서드 printAttempt: 시도 횟수를 알려주는 메서드

--- src/main/java/bridge/view/OutputView.java
+++ src/main/java/bridge/view/OutputView.java
@@ -59,6 +59,22 @@ public class OutputView {
      * <p>
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void printResult() {
+    public void printResult(BridgeGame bridgeGame) {
+        System.out.println(Message.GAME_RESULT.getMessage());
+        printMap(bridgeGame);
+        printGamestatus(bridgeGame.getGamestatus());
+        printAttempt(bridgeGame.getAttempt());
+    }
+
+    private void printGamestatus(boolean isGameover) {
+        if (!isGameover) {
+            System.out.println(Message.SUC_OR_FAIL.getMessage() + " 성공");
+            return;
+        }
+        System.out.println(Message.SUC_OR_FAIL.getMessage() + " 실패");
+    }
+
+    private void printAttempt(int attempt) {
+        System.out.println(Message.COUNT_ATTEMPT.getMessage() + attempt);
     }
 }
