33e6a8d943bccc276f5167ed3a881c70d6e4b891
Eeap <kdg97811@naver.com>
2022-11-21 19:01:21 +0900

    feat(BridgeGame): add constructor & field

--- src/main/java/bridge/BridgeGame.java
+++ src/main/java/bridge/BridgeGame.java
@@ -1,10 +1,23 @@
 package bridge;
 
+import java.util.List;
+
 /**
  * 다리 건너기 게임을 관리하는 클래스
  */
 public class BridgeGame {
-
+    private final List<String> randomList;
+    private boolean exitCode = false;
+    private final int size;
+    private int cnt = 1;
+    private ReadPrintDto readPrintDto;
+    private BridgeMaker bridgeMaker;
+    public BridgeGame(int size) {
+        this.readPrintDto = new ReadPrintDto();
+        this.bridgeMaker = new BridgeMaker(new BridgeRandomNumberGenerator());
+        this.size = size;
+        this.randomList = bridgeMaker.makeBridge(size);
+    }
     /**
      * 사용자가 칸을 이동할 때 사용하는 메서드
      * <p>
