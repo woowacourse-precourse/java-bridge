ffe3d72952773d0f5b749b2d2b7f1448dfac51a9
This2sho <zx00018@naver.com>
2022-11-22 07:01:11 +0900

    test(BridgeMaker): 다리가 입력된 길이로 만들어지는 지 확인하는 테스트

---
+++ src/test/java/bridge/BridgeMakerTest.java
@@ -0,0 +1,20 @@
+package bridge;
+
+import static org.assertj.core.api.Assertions.*;
+
+import java.util.List;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class BridgeMakerTest {
+	@Test
+	@DisplayName("입력된 길이로 다리가 만들어지는지 테스트")
+	void 다리_입력_길트_만들기_테스트() {
+		//given
+		BridgeMaker bridgeMaker = new BridgeMaker(new BridgeRandomNumberGenerator());
+		//when
+		List<String> bridges = bridgeMaker.makeBridge(3);
+		//then
+		assertThat(bridges.size()).isEqualTo(3);
+	}
+}
