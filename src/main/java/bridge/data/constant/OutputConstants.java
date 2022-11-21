527df80eb7734aae4c5946c25ca253c51ad2ad38
hojunking96 <junsong96@naver.com>
2022-11-21 23:42:05 +0900

    fix(OutputConstants.java) : 오타 수정

--- src/main/java/bridge/data/constant/OutputConstants.java
+++ src/main/java/bridge/data/constant/OutputConstants.java
@@ -3,8 +3,8 @@ package bridge.data.constant;
 public class OutputConstants {
     public final static String RESULT = "최종 게임 결과";
     public final static String TOTAL_TRY = "총 시도한 횟수: ";
-    public final static String SUCCESS = "게임 성공 여부 : 성공";
-    public final static String FAIL = "게임 성공 여부 : 실패";
+    public final static String SUCCESS = "게임 성공 여부: 성공";
+    public final static String FAIL = "게임 성공 여부: 실패";
     public final static String START = "다리 건너기 게임을 시작합니다.";
     public final static String SIZE_REQUEST = "다리의 길이를 입력해주세요.";
     public final static String MOVE_REQUEST = "이동할 칸을 선택해주세요. (위: U, 아래: D)";
