976130570005009b41d3a227ed2f47f13095b68a
jihong <clara5091@naver.com>
2022-11-22 06:07:07 +0900

    feat(Symbols): 사용되는 문자에 대한 정의 enum으로 분리

--- src/main/java/bridge/InputValidate.java
+++ src/main/java/bridge/InputValidate.java
@@ -1,11 +1,8 @@
 package bridge;
 
-public class InputValidate {
-    private static final String UPPER_ROW="U";
-    private static final String DOWN_ROW="D";
-    private static final String QUIT="Q";
-    private static final String RETRY="R";
+import static bridge.Symbols.*;
 
+public class InputValidate {
     public int validateBridgeSize(int size){
         if(size<3||size>20){
             throw new IllegalArgumentException("다리 길이는 3~20사이의 수여야 합니다.");
@@ -14,14 +11,14 @@ public class InputValidate {
     }
 
     public String validateMove(String move){
-        if(UPPER_ROW.equals(move)||DOWN_ROW.equals(move)){
+        if(UPPER.getSymbol().equals(move)||DOWN.getSymbol().equals(move)){
             return move;
         }
         throw new IllegalArgumentException("이동할 칸은 U/D 중 하나의 문자를 입력할 수 있습니다.");
     }
 
     public String validateRetry(String retry){
-        if(QUIT.equals(retry)||RETRY.equals(retry)){
+        if(QUIT.getSymbol().equals(retry)||RETRY.getSymbol().equals(retry)){
             return retry;
         }
         throw new IllegalArgumentException("재시작/종료 여부는 R/Q 중 하나의 문자를 입력할 수 있습니다.");
