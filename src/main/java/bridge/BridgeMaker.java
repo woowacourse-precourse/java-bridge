d683a4dba3a25cc44bcbaa671437efa1945b80c9
kiarakim <kiarakim413@gmail.com>
2022-11-21 23:58:57 +0900

    feat(bridge) : implement logic to makeBridge()
    return D for 0, U for 1

--- src/main/java/bridge/BridgeMaker.java
+++ src/main/java/bridge/BridgeMaker.java
@@ -1,5 +1,6 @@
 package bridge;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -8,6 +9,9 @@ import java.util.List;
 public class BridgeMaker {
 
     private final BridgeNumberGenerator bridgeNumberGenerator;
+    private final static int DOWN_NUMBER = 0;
+    private final static String DOWN = "D";
+    private final static String UP = "U";
 
     public BridgeMaker(BridgeNumberGenerator bridgeNumberGenerator) {
         this.bridgeNumberGenerator = bridgeNumberGenerator;
@@ -18,6 +22,18 @@ public class BridgeMaker {
      * @return 입력받은 길이에 해당하는 다리 모양. 위 칸이면 "U", 아래 칸이면 "D"로 표현해야 한다.
      */
     public List<String> makeBridge(int size) {
-        return null;
+        List<String> bridge = new ArrayList<>();
+        for (int i = 0; i < size; i++) {
+            String move = getMovement(bridgeNumberGenerator.generate());
+            bridge.add(move);
+        }
+        return bridge;
+    }
+
+    public String getMovement(int number) {
+        if (number == DOWN_NUMBER) {
+            return DOWN;
+        }
+        return UP;
     }
 }
