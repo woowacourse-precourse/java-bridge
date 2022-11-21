f260588170b1ce20510de8d85facbb027d3d53f1
WonSubSub <boxer1995@naver.com>
2022-11-21 18:33:31 +0900

    feat(main): 값 입력받을시 메시지 출력하는 기능 추가

--- src/main/java/bridge/InputView.java
+++ src/main/java/bridge/InputView.java
@@ -7,10 +7,15 @@ import camp.nextstep.edu.missionutils.Console;
  */
 public class InputView {
 
+    public void gameStart() {
+        System.out.println("다리 건너기 게임을 시작합니다.");
+    }
+
     /**
      * 다리의 길이를 입력받는다.
      */
     public int readBridgeSize() {
+        System.out.println("다리의 길이를 입력해주세요.");
         int number = Integer.valueOf(Console.readLine());
         return number;
     }
@@ -19,6 +24,7 @@ public class InputView {
      * 사용자가 이동할 칸을 입력받는다.
      */
     public String readMoving() {
+        System.out.println("이동할 칸을 선택해주세요. (위: U, 아래: D)");
         String alphabet = Console.readLine();
         return alphabet;
     }
@@ -27,6 +33,7 @@ public class InputView {
      * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
      */
     public String readGameCommand() {
+        System.out.println("게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)");
         String alphabet = Console.readLine();
         return alphabet;
     }
