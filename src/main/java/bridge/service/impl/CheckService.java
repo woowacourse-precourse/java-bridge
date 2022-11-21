a9fb1f51616f5fa17e312a097d44363912451bef
qkrdmswl <eunji303@naver.com>
2022-11-22 03:32:25 +0900

    feat: define checkService for checking

--- src/main/java/bridge/service/impl/CheckService.java
+++ src/main/java/bridge/service/impl/CheckService.java
@@ -1,2 +1,12 @@
-package bridge.service.impl;public interface CheckService {
+package bridge.service.impl;
+
+import java.util.List;
+
+public interface CheckService {
+    String isMatch(List<String> bridgeInfo, int idx, List<String> userDirection);
+    String isBetween(List<String> bridgeInfo);
+    boolean isSucceed(List<String> bridgeInfo);
+    boolean checkCrossable(List<String> bridgeInfo, String direction);
+    boolean compare(List<String> bridgeInfo, int idx, String direction);
+
 }
