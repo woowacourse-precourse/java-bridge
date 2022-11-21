bb1e92905ea5f46ca1a39b1819124290a580ed95
EUNSH1 <tkdgus968@naver.com>
2022-11-21 15:01:58 +0900

    feat : 사용자가 다시시도할지 종료할지 입력받는 입력 기능 추가

--- src/main/java/bridge/InputView.java
+++ src/main/java/bridge/InputView.java
@@ -8,14 +8,20 @@ import camp.nextstep.edu.missionutils.Console;
 public class InputView {
     private static final String UP_BRIDGE = "U";
     private static final String DOWN_BRIDGE = "D";
+    private static final String RESTART_GAME = "R";
+    private static final String END_GAME = "Q";
+
     public static final String INPUT_BRIDGE_SIZE = "다리의 길이를 입력해주세요.";
     public static final String INPUT_MOVING = "이동할 칸을 선택해주세요. (위: U, 아래: D)";
+    public static final String INPUT_GAME_COMMAND = "게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)";
     private static final int MIN_BRIDGE_SIZE = 3;
     private static final int MAX_BRIDGE_SIZE = 20;
     private static final String ONLY_CONTAINS_NUMBER_REGEX = "^[0-9]*$";
     private static final String RANGE_ERROR = "[ERROR] 3에서 20까지의 숫자를 입력해 주시기 바랍니다.";
     private static final String NUMBER_ERROR = "[ERROR] 올바른 숫자가 아닙니다.";
-    private static final String MOVING_COMMAND_ERROR = "[ERROR] U 또는 D를 입력해 주시기 바랍니다.\n";
+    private static final String MOVING_COMMAND_ERROR = "[ERROR] 입력은 대문자 'U' 또는 'D'를 입력해 주시기 바랍니다.\n";
+    private static final String RETRY_COMMAND_ERROR = "[ERROR] 입력은 대문자 'R' 또는 'Q'를 입력해 주시기 바랍니다.\n";
+
     /**
      * 다리의 길이를 입력받는다.
      */
@@ -70,6 +76,19 @@ public class InputView {
      * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
      */
     public String readGameCommand() {
-        return null;
+        try {
+            System.out.println(INPUT_GAME_COMMAND);
+            return isGameCommand(Console.readLine());
+        }catch (IllegalArgumentException e){
+            System.out.println(e.getMessage());
+            return readGameCommand();
+        }
+    }
+
+    private String isGameCommand(String readLine) {
+        if (!(RESTART_GAME.equals(readLine) || END_GAME.equals(readLine))) {
+            throw new IllegalArgumentException(RETRY_COMMAND_ERROR);
+        }
+        return readLine;
     }
 }
