67f509d22467c4ca801b0db1d233cf7c15335494
tjddnr7760 <tjddnr7760@naver.com>
2022-11-22 02:01:35 +0900

    feat : 시작 메소드 설정

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -4,5 +4,11 @@ public class Application {
 
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        BridgeGame bridgeGame = new BridgeGame();
+        try {
+            bridgeGame.start();
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+        }
     }
 }
