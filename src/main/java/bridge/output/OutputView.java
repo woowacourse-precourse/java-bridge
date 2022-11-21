59ff8725a390a512552928ed2a2fb28d424210c1
hoin <apicad0307@gmail.com>
2022-11-21 14:20:15 +0900

    feat(output/OutputView.java): 현재 다리 출력시 출력 포맷에 맞추어 출력하는 기능 구현

--- src/main/java/bridge/output/OutputView.java
+++ src/main/java/bridge/output/OutputView.java
@@ -1,11 +1,45 @@
 package bridge.output;
 
-import static bridge.output.OutputString.START_GAME_MESSAGE;
+import static bridge.output.OutputString.*;
 
 public class OutputView {
     public static void printStartGame() {
         System.out.println(START_GAME_MESSAGE);
         System.out.println();
+    }
+
+    public static void printMap(StringBuilder upperRoad, StringBuilder lowerRoad) {
+        StringBuilder upperRoadFormat = new StringBuilder();
+        StringBuilder lowerRoadFormat = new StringBuilder();
+
+        appendSeparator(upperRoad, lowerRoad, upperRoadFormat, lowerRoadFormat);
+        addStartEndSign(upperRoadFormat, lowerRoadFormat);
+
+        System.out.println(upperRoadFormat);
+        System.out.println(lowerRoadFormat);
+        System.out.println();
+    }
+
+    private static void addStartEndSign(StringBuilder upperRoadFormat, StringBuilder lowerRoadFormat) {
+        upperRoadFormat.insert(0, START_SIGN);
+        lowerRoadFormat.insert(0, START_SIGN);
+        upperRoadFormat.append(END_SIGN);
+        lowerRoadFormat.append(END_SIGN);
+    }
+
+    private static void appendSeparator(StringBuilder upperRoad, StringBuilder lowerRoad, StringBuilder upperRoadFormat, StringBuilder lowerRoadFormat) {
+        for (int i = 0; i < upperRoad.length(); i++) {
+            upperRoadFormat.append(upperRoad.charAt(i));
+            if (i != upperRoad.length() - 1) {
+                upperRoadFormat.append(SEPARATOR);
+            }
+        }
+        for (int i = 0; i < lowerRoad.length(); i++) {
+            lowerRoadFormat.append(lowerRoad.charAt(i));
+            if (i != lowerRoad.length() - 1) {
+                lowerRoadFormat.append(SEPARATOR);
+            }
+        }
     }
 
 }
