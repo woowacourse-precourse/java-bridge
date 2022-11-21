fb2724d83c4743ca90a8ca708d40e3958ae9704b
jinny-l <jinny.l.dev@gmail.com>
2022-11-22 03:28:26 +0900

    test: 테스트 입력 값 추가

--- src/test/java/bridge/InputExceptionTest.java
+++ src/test/java/bridge/InputExceptionTest.java
@@ -6,62 +6,69 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import camp.nextstep.edu.missionutils.test.NsTest;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 class InputExceptionTest extends NsTest {
 
     private static final String ERROR_MESSAGE = "[ERROR]";
 
-    @Test
+    @ParameterizedTest
     @DisplayName("예외 테스트: 입력 값이 공백일 때 예외가 발생한다.")
-    void sizeTest1() {
+    @ValueSource(strings = {" ", "   "})
+    void sizeTest1(String input) {
         assertSimpleTest(() -> {
-            runException(" ");
+            runException(input);
             assertThat(output()).contains(ERROR_MESSAGE);
         });
     }
 
-    @Test
+    @ParameterizedTest
     @DisplayName("예외 테스트: 입력 값이 문자일 때 예외가 발생한다.")
-    void sizeTest2() {
+    @ValueSource(strings = {"문자", "English", "*&^"})
+    void sizeTest2(String input) {
         assertSimpleTest(() -> {
-            runException("문자");
+            runException(input);
             assertThat(output()).contains(ERROR_MESSAGE);
         });
     }
 
-    @Test
+    @ParameterizedTest
     @DisplayName("예외 테스트: 입력 값이 2이하 일때 예외가 발생한다.")
-    void sizeTest3() {
+    @ValueSource(strings = {"2", "0", "-1"})
+    void sizeTest3(String input) {
         assertSimpleTest(() -> {
-            runException("2");
+            runException(input);
             assertThat(output()).contains(ERROR_MESSAGE);
         });
     }
 
-    @Test
+    @ParameterizedTest
     @DisplayName("예외 테스트: 입력 값이 21이상 일때 예외가 발생한다.")
-    void sizeTest4() {
+    @ValueSource(strings = {"21", "2222222"})
+    void sizeTest4(String input) {
         assertSimpleTest(() -> {
-            runException("21");
+            runException(input);
             assertThat(output()).contains(ERROR_MESSAGE);
         });
     }
 
-    @Test
+    @ParameterizedTest
     @DisplayName("예외 테스트: 입력 값이 U/D가 가 아닐 때 예외가 발생한다.")
-    void movingTest() {
+    @ValueSource(strings = {"문자", "0", "A"})
+    void movingTest(String input) {
         assertRandomNumberInRangeTest(() -> {
-            run("3", "A", "U", "Q");
+            run("3", input, "U", "Q");
             assertThat(output()).contains(ERROR_MESSAGE);
         }, 0, 0, 0);
     }
 
-    @Test
+    @ParameterizedTest
     @DisplayName("예외 테스트: 입력 값이 R/Q가 아닐 때 예외가 발생한다.")
-    void retryTest() {
+    @ValueSource(strings = {"문자", "0", "A"})
+    void retryTest(String input) {
         assertRandomNumberInRangeTest(() -> {
-            run("3", "U", "A", "Q");
+            run("3", "U", input, "Q");
             assertThat(output()).contains(ERROR_MESSAGE);
         }, 0, 0, 0);
     }
