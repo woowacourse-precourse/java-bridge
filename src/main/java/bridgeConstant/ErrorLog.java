74b50c240babb21d2125070e915764e8af1502d5
Devtist <yt930310@snu.ac.kr>
2022-11-21 18:46:45 +0900

    feat: 입력이 알파벳 대문자가 아닌 경우에 대한 log 추가

--- src/main/java/bridgeConstant/ErrorLog.java
+++ src/main/java/bridgeConstant/ErrorLog.java
@@ -5,6 +5,7 @@ public enum ErrorLog {
 	LENGTH_IS_NOT_MATCHED_EXCEPTION("다리의 길이가 입력받은 길이와 같지 않습니다."),
 	INVALID_LENGTH_RANGE_EXCEPTION("다리의 길이는 3부터 20 사이여야 합니다."),
 	NOT_NATURAL_NUMBER_EXCEPTION("다리의 길이는 자연수여야 합니다."),
+	NOT_CAPITAL_LETTER_EXCEPTION("입력은 알파벳 대문자 하나여야 합니다."),
 	INVALID_MOVING_INPUT_EXCEPTION("입력은 U와 D 둘 중 하나여야 합니다."),
 	INVALID_BRIDGE_COMPONENT_EXCEPTION("다리는 U와 D로 이루어져야 합니다."),
 	INVALID_GAME_COMMAND_EXCEPTION("입력은 R과 Q 둘 중 하나여야 합니다."),
