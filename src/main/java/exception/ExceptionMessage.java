8d0a7bd274dba5f0d67d0494206d04aed7f23b5d
“rotomoo” <quddlf410@naver.com>
2022-11-21 21:52:25 +0900

    feat(ExceptionMessage) : 게임 재시작 종료 예외 메시지 추가

--- src/main/java/exception/ExceptionMessage.java
+++ src/main/java/exception/ExceptionMessage.java
@@ -3,7 +3,9 @@ package exception;
 public enum ExceptionMessage {
     INVALID_BRIDGE_SIZE("잘못된 입력 입니다. 다리 길이는 3부터 20 사이의 숫자여야 합니다."),
     INVALID_MOVE("잘못된 입력 입니다. U(위), D(아래)를 입력해 주세요."),
-    INVALID_INPUT("잘못된 입력 입니다. 알맞게 입력해 주세요.");
+    INVALID_INPUT("잘못된 입력 입니다. 알맞게 입력해 주세요."),
+    INVALID_GAME_COMMAND("잘못된 입력 입니다. R(재시작), Q(종료)를 입력해 주세요");
+
 
     private String message;
 
