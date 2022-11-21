625488c1f60054f2fc9dcc52379fd0c70ee8c88d
qpdh <74190329+qpdh@users.noreply.github.com>
2022-11-22 02:25:48 +0900

    Feat($InputViewTest) 다리_크기_정수_입력_테스트, 다리_크기_자연수_범위_테스트 구현

---
+++ src/test/java/bridge/InputViewTest.java
@@ -0,0 +1,44 @@
+package bridge;
+
+import camp.nextstep.edu.missionutils.test.NsTest;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.util.List;
+
+import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
+import static org.assertj.core.api.Assertions.*;
+import static org.assertj.core.util.Lists.newArrayList;
+
+class InputViewTest extends NsTest {
+
+    private static final String ERROR_MESSAGE = "[ERROR]";
+
+    @DisplayName("다리 크기 값이 정수가 아닐 때의 예외 처리")
+    @ValueSource(strings = {"abc", "a1c", "1.1"})
+    @ParameterizedTest
+    void 다리_크기_정수_입력_테스트(String input) {
+        assertSimpleTest(() -> {
+            assertThatThrownBy(() -> new InputView().validateBridgeSize(input)).isInstanceOf(IllegalArgumentException.class);
+            assertThat(output()).contains(ERROR_MESSAGE);
+        });
+    }
+
+    @DisplayName("다리 크기 값이 3 이상 20 이하의 자연수가 아닐 때의 예외 처리")
+    @ValueSource(strings = {"2", "21", "-1"})
+    @ParameterizedTest
+    void 다리_크기_자연수_범위_테스트(String input) {
+        assertSimpleTest(() -> {
+            assertThatThrownBy(() -> new InputView().validateBridgeSize(input)).isInstanceOf(IllegalArgumentException.class);
+            assertThat(output()).contains(ERROR_MESSAGE);
+        });
+    }
+
+    @Override
+    protected void runMain() {
+        Application.main(new String[]{});
+    }
+
+}
