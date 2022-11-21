f2f36cccd872b0fe8835a175b918efde37889fcd
이성준 <75744297+castlejun-2@users.noreply.github.com>
2022-11-21 20:37:05 +0900

    feat: 게임 시작전 초기값 및 변수 세팅

--- src/main/java/bridge/BridgeGame.java
+++ src/main/java/bridge/BridgeGame.java
@@ -1,16 +1,24 @@
 package bridge;
 
+import java.util.List;
+import java.util.ArrayList;
+
 /**
  * 다리 건너기 게임을 관리하는 클래스
  */
 public class BridgeGame {
-
+    private int tryCount = 1;
+    private List<String> answer;
+    private List<String> user;
+    private int bridgeLength;
+    private final BridgeNumberGenerator bridgeNumberGenerator = new BridgeRandomNumberGenerator();
     /**
      * 사용자가 칸을 이동할 때 사용하는 메서드
      * <p>
      * 이동을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
     public void move() {
+
     }
 
     /**
@@ -19,5 +27,11 @@ public class BridgeGame {
      * 재시작을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
     public void retry() {
+    }
+
+    public void play() {
+        this.bridgeLength = new InputView().readBridgeSize();
+        this.answer = new BridgeMaker(bridgeNumberGenerator).makeBridge(bridgeLength);
+        this.user = new ArrayList<>(bridgeLength);
     }
 }
