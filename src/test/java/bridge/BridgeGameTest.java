2bad7d5ed6f276b9c583eb9bf4a94b3e339e6d73
jmj005 <jmj00005@naver.com>
2022-11-21 19:16:42 +0900

    test(BridgeGameTest): BridgeGameTest 테스트 코드 refactoring

--- src/test/java/bridge/BridgeGameTest.java
+++ src/test/java/bridge/BridgeGameTest.java
@@ -1,47 +1,30 @@
 package bridge;
 
-import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeTest;
+import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Stream;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.MethodSource;
 
-class BridgeGameTest extends ApplicationTest {
+class BridgeGameTest {
 
-    @DisplayName("사용자 칸 이동 테스트 1")
-    @Test
-    void move() {
-        assertRandomNumberInRangeTest(() -> {
-            run("5", "D", "D", "U", "D", "U");
-            assertThat(output()).contains(
-                    "[   |   | O |   | O ]",
-                    "[ O | O |   | O |   ]"
-            );
-            int upSideIndex = output().indexOf("[   |   | O |   | O ]");
-            int downSideIndex = output().indexOf("[ O | O |   | O |   ]");
-            assertThat(upSideIndex).isLessThan(downSideIndex);
-        }, 0, 0, 1, 0, 1);
-    }
-
-    @DisplayName("사용자 칸 이동 테스트 2")
-    @Test
-    void move2() {
-        assertRandomNumberInRangeTest(() -> {
-            run("3", "D", "U", "R", "D", "D", "D");
-            assertThat(output()).contains(
-                    "[   |   |   ]",
-                    "[ O | O | O ]"
-            );
-            int upSideIndex = output().indexOf("[   |   |   ]");
-            int downSideIndex = output().indexOf("[ O | O | O ]");
-            assertThat(upSideIndex).isLessThan(downSideIndex);
-        }, 0, 0, 0, 0, 0);
+    @DisplayName("사용자 칸 이동 테스트")
+    @ParameterizedTest
+    @CsvSource({"U, U, ' O |', '   |", "U, D, ' X |', '   |", "D, D, '   |', ' O |'", "D, U, '   |', ' X |"})
+    void move2(String playerMove, String bridgeElement, String expected) {
+        BridgeGame bridgeGame = new BridgeGame();
+        List<String> upSideDownSideMove = new ArrayList<>(List.of("", ""));
+        CompareBridge move = new CompareBridge(playerMove);
+        assertThat(bridgeGame.move(move, upSideDownSideMove, bridgeElement))
+                .contains(expected);
     }
 
     static Stream<Arguments> parameterProvider() {
@@ -52,6 +35,7 @@ class BridgeGameTest extends ApplicationTest {
         );
     }
 
+    @DisplayName("게임 실패 여부 테스트")
     @ParameterizedTest
     @MethodSource("parameterProvider")
     void isFailure(List<String> upSideDownSideMove) {
@@ -64,13 +48,10 @@ class BridgeGameTest extends ApplicationTest {
     @DisplayName("총 시도한 횟수 테스트")
     @Test
     void retry() {
-        assertRandomNumberInRangeTest(() -> {
-            run("3", "D", "U", "R", "D", "D", "U", "R", "D", "D", "D");
-            assertThat(output()).contains(
-                    "[   |   |   ]",
-                    "[ O | O | O ]"
-            );
-            assertThat(output()).contains("3");
-        }, 0, 0, 0, 0, 0, 0, 0, 0);
+        assertSimpleTest(() -> {
+            BridgeGame bridgeGame = new BridgeGame();
+            bridgeGame.retry("R");
+            assertEquals(2, BridgeGame.retryCount);
+        });
     }
 }
