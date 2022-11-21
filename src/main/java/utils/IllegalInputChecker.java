ea7b77de796ee0600b4ba4b600334d831d4da2a0
qkrdmswl <eunji303@naver.com>
2022-11-21 16:42:08 +0900

    refactor: change retrun type of checkMoveInput DirectionType(enum) to String

--- src/main/java/utils/IllegalInputChecker.java
+++ src/main/java/utils/IllegalInputChecker.java
@@ -1,36 +1,39 @@
 package utils;
 
+import bridge.type.DirectionType;
+import bridge.type.GameOptionType;
+
 public class IllegalInputChecker {
     private static final String BRIDGE_SIZE = "^\\d{1,2}$";
-    private static final String MOVE_INPUT = "^\\[UD]$";
-    private static final String GAME_OPTION = "^\\[RQ]$";
+    private static final String MOVE_INPUT = "^[UD]$";
+    private static final String GAME_OPTION = "^[RQ]$";
 
-    public int checkBridgeSizeInput(String bridgeSize) {
+    public static int checkBridgeSizeInput(String bridgeSize) {
         isBridgeSizeNumeric(bridgeSize);
         return isNumOfBridgeSizeValid(bridgeSize);
     }
 
-    public String checkMoveInput(String moveInput) {
+    public static String checkMoveInput(String moveInput) {
         if (!moveInput.matches(MOVE_INPUT)) {
             throw new IllegalArgumentException(ExceptionMessage.MOVE_INPUT_INVALID);
         }
         return moveInput;
     }
 
-    public String checkRestartOrQuit(String option) {
+    public static GameOptionType checkRestartOrQuit(String option) {
         if (!option.matches(GAME_OPTION)) {
             throw new IllegalArgumentException(ExceptionMessage.GAME_OPTION_INVALID);
         }
-        return option;
+        return GameOptionType.valueOf(option);
     }
 
-    public void isBridgeSizeNumeric (String bridgeSize) {
+    public static void isBridgeSizeNumeric (String bridgeSize) {
         if (!bridgeSize.matches(BRIDGE_SIZE)) {
             throw new IllegalArgumentException(ExceptionMessage.BRIDGE_SIZE_NOT_NUMERIC);
         }
     }
 
-    public int isNumOfBridgeSizeValid (String bridgeSize) {
+    public static int isNumOfBridgeSizeValid (String bridgeSize) {
         if (Integer.parseInt(bridgeSize) < 3 || Integer.parseInt(bridgeSize) > 20) {
             throw new IllegalArgumentException(ExceptionMessage.BRIDGE_SIZE_NUM_INVALID);
         }
