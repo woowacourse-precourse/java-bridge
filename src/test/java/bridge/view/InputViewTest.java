389e9a4e8ee341ba9b8532bdeff29e29c22eddc8
bunsung92 <bunsung92@naver.com>
2022-11-21 22:06:34 +0900

    test(bridge): 이동방향 입력 및 재시도 입력 값 검증 테스트 코드 추가

--- src/test/java/bridge/view/InputViewTest.java
+++ src/test/java/bridge/view/InputViewTest.java
@@ -1,5 +1,8 @@
 package bridge.view;
 
+import static bridge.constant.ErrorConstant.INPUT_DIRECTION_EXCEPTION;
+import static bridge.constant.ErrorConstant.INPUT_GAME_COMMAND_EXCEPTION;
+import static bridge.constant.ErrorConstant.INPUT_NUMBER_EXCEPTION;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.mockito.Mockito.mockStatic;
@@ -43,7 +46,7 @@ class InputViewTest {
             when(Console.readLine()).thenReturn(illegalValue);
 
             assertThatThrownBy(() -> inputView.readBridgeSize()).isInstanceOf(IllegalArgumentException.class)
-                                                                .hasMessageContaining("[ERROR]");
+                                                                .hasMessageContaining(INPUT_NUMBER_EXCEPTION.getMessage());
         }
     }
 
@@ -55,7 +58,53 @@ class InputViewTest {
             when(Console.readLine()).thenReturn(illegalRangeNumber);
 
             assertThatThrownBy(() -> inputView.readBridgeSize()).isInstanceOf(IllegalArgumentException.class)
-                                                                .hasMessageContaining("[ERROR]");
+                                                                .hasMessageContaining(INPUT_NUMBER_EXCEPTION.getMessage());
+        }
+    }
+
+    @DisplayName("정상 시나리오 | 콘솔에서 이동 방향을 입력 받는다.")
+    @ParameterizedTest
+    @CsvSource({"U", "D"})
+    void readDirectionByConsole(String direction) {
+        try (MockedStatic<Console> console = mockStatic(Console.class)) {
+            when(Console.readLine()).thenReturn(direction);
+
+            assertThat(inputView.readDirection()).isEqualTo(direction);
+        }
+    }
+
+    @DisplayName("예외 시나리오 | 이동 방향 값이 'U', 'D'가 아닐 경우 예외 처리")
+    @ParameterizedTest
+    @CsvSource({"u", "3 "})
+    void readDirectionByConsoleIllegalDirection(String illegalDirection) {
+        try (MockedStatic<Console> console = mockStatic(Console.class)) {
+            when(Console.readLine()).thenReturn(illegalDirection);
+
+            assertThatThrownBy(() -> inputView.readDirection()).isInstanceOf(IllegalArgumentException.class)
+                                                                 .hasMessageContaining(INPUT_DIRECTION_EXCEPTION.getMessage());
+        }
+    }
+
+    @DisplayName("정상 시나리오 | 콘솔에서 재시도를 입력 받는다.")
+    @ParameterizedTest
+    @CsvSource({"R", "Q"})
+    void readGameCommandByConsole(String gameCommand) {
+        try (MockedStatic<Console> console = mockStatic(Console.class)) {
+            when(Console.readLine()).thenReturn(gameCommand);
+
+            assertThat(inputView.readGameCommand()).isEqualTo(gameCommand);
+        }
+    }
+
+    @DisplayName("예외 시나리오 | 이동 방향 값이 'U', 'D'가 아닐 경우 예외 처리")
+    @ParameterizedTest
+    @CsvSource({"r", "3"})
+    void readGameCommandByConsoleIllegalDirection(String illegalGameCommand) {
+        try (MockedStatic<Console> console = mockStatic(Console.class)) {
+            when(Console.readLine()).thenReturn(illegalGameCommand);
+
+            assertThatThrownBy(() -> inputView.readGameCommand()).isInstanceOf(IllegalArgumentException.class)
+                                                                 .hasMessageContaining(INPUT_GAME_COMMAND_EXCEPTION.getMessage());
         }
     }
 
