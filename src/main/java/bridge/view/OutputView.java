27039301d0ecfa39ce05359990878153f85a5b97
baekeunsun <eunsun5460@gmail.com>
2022-11-21 17:47:01 +0900

    feat(view/OutputView): "최종 게임 결과" 출력
    - (OutputView/printResult): "최종 게임 결과" 출력

--- src/main/java/bridge/view/OutputView.java
+++ src/main/java/bridge/view/OutputView.java
@@ -18,6 +18,8 @@ public class OutputView {
      * <p>
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void printResult() {
+    public static void printResult() {
+        System.out.println("최종 게임 결과");
+
     }
 }
