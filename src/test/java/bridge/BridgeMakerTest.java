b308745bb0fdddb3680239f7f287161be3f59b6e
bumwoo <gld55602@gmail.com>
2022-11-21 21:08:39 +0900

    test(BridgeMakerTest): BridgeMaker 테스트 코드 작성

---
+++ src/test/java/bridge/BridgeMakerTest.java
@@ -0,0 +1,28 @@
+package bridge;
+
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.List;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class BridgeMakerTest {
+    private BridgeMaker bridgeMaker = new BridgeMaker(new BridgeRandomNumberGenerator());
+
+    @Test
+    void 다리_생성_테스트() {
+        List<String> bridge = bridgeMaker.makeBridge(Randoms.pickNumberInRange(3, 20));
+        assertThat(bridge.stream().allMatch(this::isMoving)).isTrue();
+
+    }
+
+    private boolean isMoving(String moving) {
+        if (moving.equals("U")) {
+            return true;
+        }
+        if (moving.equals("D")) {
+            return true;
+        }
+        return false;
+    }
+}
