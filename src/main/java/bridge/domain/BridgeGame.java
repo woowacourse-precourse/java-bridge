367b53a162bd6290531568c67a1cb17cb79f9f61
yeeun <dpdms9621@naver.com>
2022-11-21 17:58:25 +0900

    style(BridgeGame): if문 코드 중괄호 추가

--- src/main/java/bridge/domain/BridgeGame.java
+++ src/main/java/bridge/domain/BridgeGame.java
@@ -19,8 +19,12 @@ public class BridgeGame {
     // 사용자가 칸을 이동할 때 사용하는 메서드
     public boolean move(String moving) {
         this.userSelectList.add(moving);
-        if (bridgeMoving.movingCount()) return false;
-        if (!(bridgeMoving.compareList(moving))) return false;
+        if (bridgeMoving.movingCount()) {
+            return false;
+        }
+        if (!(bridgeMoving.compareList(moving))) {
+            return false;
+        }
         return true;
     }
 
