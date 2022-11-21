64beaeeb96bcb60487ac85ab94069de996a9a7ee
JONGMIN SHIN <jongmin373@gmail.com>
2022-11-21 17:24:37 +0900

    Test : test 4 bridgeSize And Success Once

--- src/test/java/bridge/BridgeTest.java
+++ src/test/java/bridge/BridgeTest.java
@@ -29,6 +29,65 @@ public class BridgeTest extends NsTest {
             assertThat(output()).contains(ERROR_MESSAGE);
         });
     }
+    @DisplayName("숫자 범위를 잘못 입력했을 때 에러메세지 발생")
+    @Test
+    void 숫자_범위를_잘못_입력_했을_떄1() {
+        assertSimpleTest(() -> {
+            runException("25");
+            assertThat(output()).contains(ERROR_MESSAGE);
+        });
+    }
+    @DisplayName("숫자 범위를 잘못 입력했을 때 에러메세지 발생")
+    @Test
+    void 숫자_범위를_잘못_입력_했을_떄2() {
+        assertSimpleTest(() -> {
+            runException("1");
+            assertThat(output()).contains(ERROR_MESSAGE);
+        });
+    }
+    @DisplayName("숫자 범위 잘못 입력했을 때_IllegalArgumentExcption_발생")
+    @Test
+    void 숫자_범위_잘못_입력_했을_때_IllegalArgumentExcption_발생() {
+        // TODO: 이 테스트가 통과할 수 있게 구현 코드 작성
+        assertThatThrownBy(() -> exeption.isRightRange("-1"))
+                .isInstanceOf(IllegalArgumentException.class);
+        assertThatThrownBy(() -> exeption.isRightRange("21"))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+    @DisplayName("_물음에_잘못된_값을_넣으면_IllegalArgumentExcption_발생")
+    @Test
+    void 유저_입력_물음에_잘못된_값을_넣으면_IllegalArgumentExcption_발생() {
+        // TODO: 이 테스트가 통과할 수 있게 구현 코드 작성
+        assertThatThrownBy(() -> exeption.isUorD("11"))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+    @DisplayName("재시작_물음에_잘못된_값을_넣으면_IllegalArgumentExcption_발생")
+    @Test
+    void 재시작_물음에_잘못된_값을_넣으면_IllegalArgumentExcption_발생() {
+        // TODO: 이 테스트가 통과할 수 있게 구현 코드 작성
+        assertThatThrownBy(() -> exeption.isRorQ("11"))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+
+    @Test
+    void 네개_한번에_성공_테스트() {
+        assertRandomNumberInRangeTest(() -> {
+            run("4", "U", "D", "U","U");
+            assertThat(output()).contains(
+                    "최종 게임 결과",
+                    "[ O |   | O | O ]",
+                    "[   | O |   |   ]",
+                    "게임 성공 여부: 성공",
+                    "총 시도한 횟수: 1"
+            );
+
+            int upSideIndex = output().indexOf("[ O |   | O | O ]");
+            int downSideIndex = output().indexOf("[   | O |   |   ]");
+            assertThat(upSideIndex).isLessThan(downSideIndex);
+        }, 1, 0, 1);
+    }
+
 
     @Override
     protected void runMain() {
