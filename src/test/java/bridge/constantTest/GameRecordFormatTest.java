4473af982d29beee5f6261ff5a6f4f49c83b8785
gomljo <gomljo0307@naver.com>
2022-11-22 00:14:53 +0900

    test(GameRecordFormatTest): 현재 게임 진행 횟수에 따라 알맞은 포맷을 반환하는 기능 검증

--- src/test/java/bridge/constantTest/GameRecordFormatTest.java
+++ src/test/java/bridge/constantTest/GameRecordFormatTest.java
@@ -7,10 +7,14 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.MethodSource;
+import org.w3c.dom.ls.LSException;
 
+import java.util.List;
 import java.util.stream.Stream;
 
+import static org.assertj.core.api.Assertions.as;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class GameRecordFormatTest {
@@ -23,7 +27,22 @@ public class GameRecordFormatTest {
     @ParameterizedTest
     @MethodSource("provideBooleanForFindByGameRecordFormat")
     @DisplayName("첫 회차가 아니라면 BACK_SLASHED 객체를 맞다면 NORMAL 객체를 반환하는지 확인한다.")
-    public void generateGameRecordFormat(boolean isFirst, GameRecordFormat gameRecordFormat){
+    public void findGameRecordFormat(boolean isFirst, GameRecordFormat gameRecordFormat){
         assertThat(GameRecordFormat.findByTrial(isFirst)).isEqualTo(gameRecordFormat);
     }
+    @ParameterizedTest
+    @MethodSource("provideBooleanForGenerateProperFormat")
+    @DisplayName("첫 회차라면 원래의 기록을 반환하고 아니라면 |가 추가된 기록을 반환하는지 확인한다.")
+    public void generateProperGameRecordFormat(boolean isFirst, String transformedResult){
+        String currentResult = "X";
+        GameRecordFormat gameRecordFormat = GameRecordFormat.findByTrial(isFirst);
+        assertThat(gameRecordFormat.generate(currentResult)).isEqualTo(transformedResult);
+    }
+    private static Stream<Arguments> provideBooleanForGenerateProperFormat() {
+        return Stream.of(
+                Arguments.of(true, "X"),
+                Arguments.of(false, " | X")
+        );
+    }
+
 }
