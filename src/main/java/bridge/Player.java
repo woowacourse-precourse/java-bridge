2e08b213a16dc945a22623f1f1edb971d82daf1a
swonny <wonny921@gmail.com>
2022-11-21 11:07:03 +0900

    refactor(Player): 현재 위치 상태 접근 제한, getter메소드 추가

--- src/main/java/bridge/Player.java
+++ src/main/java/bridge/Player.java
@@ -1,18 +1,22 @@
 package bridge;
 
 public class Player {
-    private final int START_POSITION = -1;
-    int currentPosition;
+    private final int START_POSITION = -1; // 다리는 0번 인덱스부터 시작하므로 사용자의 초기화 위치는 -1이다.
+    private int currentPosition;
 
     public Player() {
-        currentPosition = START_POSITION;
+        this.currentPosition = START_POSITION;
     }
 
     public void move() {
-        currentPosition++;
+        this.currentPosition++;
     }
 
     public int getNextPosition() {
-        return currentPosition + 1;
+        return this.currentPosition + 1;
+    }
+
+    public int getCurrentPosition() {
+        return this.currentPosition;
     }
 }
