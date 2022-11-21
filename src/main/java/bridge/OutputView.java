e12753c1f29b3bb3fd3a916e89826f02f4187a0c
JHyun <wogustkfkd11@gmail.com>
2022-11-21 14:45:38 +0900

    OutputView - output commit

--- src/main/java/bridge/OutputView.java
+++ src/main/java/bridge/OutputView.java
@@ -11,7 +11,7 @@ public class OutputView {
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
 
-    public void printMap(BridgeMaker bridgeMake) {
+    public void printMap(BridgeMaker bridgeMake) { // 다리 출력
         System.out.println(bridgeMake.upperMap);
         System.out.println(bridgeMake.lowerMap);
     }
@@ -21,7 +21,7 @@ public class OutputView {
      * <p>
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void printResult(BridgeMaker bridgeMake, int tryCount) {
+    public void printResult(BridgeMaker bridgeMake, int tryCount) { // 게임 결과
         System.out.println("최종 게임 결과");
         printMap(bridgeMake);
         if (bridgeMake.upperMap.contains("X") || bridgeMake.lowerMap.contains("X")) {
