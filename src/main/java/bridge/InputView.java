ab0dcd0fed7ea9cd56032546f69f01a5093836c1
abbd2 <ajkety00@gmail.com>
2022-11-22 06:53:40 +0900

    Refactor(Class): 유효성 검사와 관련된 메서드 Valid 클래스로 이동

--- src/main/java/bridge/InputView.java
+++ src/main/java/bridge/InputView.java
@@ -43,15 +43,6 @@ public class InputView {
         return value;
     }
 
-    // 3 이상 20 이하의 숫자인지 검사
-    public boolean isBridgeSizeNumber(String value) {
-        final String REGEX = "^[3-9]{1}$|^[1]{1}[0-9]{1}$|^2{1}[0]{1}$";
-        if (Pattern.matches(REGEX, value)) {
-            return true;
-        }
-        return false;
-    }
-
     public void startGame(List<String> bridgeList) {
         OutputView outputView = new OutputView();
         loopGame(bridgeList); // 게임 재시도 여부에 따라 반복
@@ -139,22 +130,6 @@ public class InputView {
         String moving = Console.readLine();
         valid.validReadMoving(moving);
         return moving;
-    }
-
-    // 입력 값이 R 또는 Q 인지 검사
-    public boolean isRetry(String retry) {
-        if (retry.equals("R") || retry.equals("Q")) {
-            return true;
-        }
-        return false;
-    }
-
-    // 입력 값이 U 또는 D 인지 검사
-    public boolean isReadMoving(String moving) {
-        if (moving.equals("U") || moving.equals("D")) {
-            return true;
-        }
-        return false;
     }
 
 }
--- src/test/java/bridge/InputViewTest.java
+++ src/test/java/bridge/InputViewTest.java
@@ -10,22 +10,22 @@ class InputViewTest {
     @DisplayName("다리의 길이로 지정이 가능한 숫자인지 검사하는 메서드")
     @Test
     void isBridgeSizeNumber(){
-        InputView inputView = new InputView();
-        assertTrue(inputView.isBridgeSizeNumber("3"));
+        Valid valid = new Valid();
+        assertTrue(valid.isBridgeSizeNumber("3"));
     }
     
     @DisplayName("입력 값이 U 또는 D인지 검사하는 메서드")
     @Test
     void isReadMoving(){
-        InputView inputView = new InputView();
-        assertTrue(inputView.isReadMoving("U"));
+        Valid valid = new Valid();
+        assertTrue(valid.isReadMoving("U"));
     }
 
     @DisplayName("입력 값이 R 또는 Q인지 검사하는 메서드")
     @Test
     void isRetry(){
-        InputView inputView = new InputView();
-        assertTrue(inputView.isRetry("R"));
+        Valid valid = new Valid();
+        assertTrue(valid.isRetry("R"));
     }
 
 }
