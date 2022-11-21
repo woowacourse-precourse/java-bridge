c3df9e9f77dd9f8485af1c1e50411fe105a655f3
limjae <limjae96@gmail.com>
2022-11-21 20:22:27 +0900

    style: import 정리

--- src/test/java/bridge/game/BridgeGameTest.java
+++ src/test/java/bridge/game/BridgeGameTest.java
@@ -1,10 +1,9 @@
 package bridge.game;
 
-import static org.assertj.core.api.Assertions.*;
-import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import java.util.List;
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
