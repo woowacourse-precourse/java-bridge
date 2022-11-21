f7d3d74e18edf159b867b3365da3ba9c0d52e2b4
kimsinhyun <ksh415622@naver.com>
2022-11-21 22:34:31 +0900

    feat: User에 관련된 내용들을 관리하는 클래쓰
    1. appendCurrentUserMovinginput
       - 게임 진행 중 어느 방향으로 갈 것인지를 입력받고 기록하는 함수
    2. checkCorrectness
       - 현재까지의 정답 상태를 기록하는 함수
    3. readUserCommandInput
       - 게임이 끝났을 때 재시작 혹은 종류할 것인지의 커멘드를 받는 함수
    4. getTotalInputLength
       - 현재까지 방향들을 입력한 횟수를 반환

---
+++ src/main/java/bridge/User/User.java
@@ -0,0 +1,56 @@
+package bridge.User;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import bridge.InputView;
+import bridge.OutputView;
+import bridge.Bridge.Bridge;
+
+public class User {
+    public List<String> inputHistory;
+    public List<String> correctnessHistory;
+
+    public User() {
+        inputHistory = new ArrayList<String>();
+        correctnessHistory = new ArrayList<String>();
+    }
+
+    // 게임진행 중 'U' or 'D' 여부 추가
+    public void appendCurrentUserMovinginput(){
+        String currentInput;
+
+        OutputView.printReadMoving();
+        currentInput = InputView.readMoving();
+
+        inputHistory.add(currentInput);
+        // currentInput 정답 확인 후 history에 추가
+        String currentCorrectness = checkCorrectness(currentInput);
+        correctnessHistory.add(currentCorrectness);
+
+        // printMap
+        OutputView.printMap(Bridge.bridgePattern, inputHistory);
+    }
+
+    public String checkCorrectness(String currentInput){
+        int currentIndex = this.getTotalInputLength()-1;
+
+        if(Bridge.bridgePattern.get(currentIndex).equals(currentInput)){
+            if(currentInput.equals("U")){
+                return "U";
+            }
+            if(currentInput.equals("D")){
+                return "D";
+            }
+        }
+        return "X";
+    }
+
+    public String readUserCommandInput(){
+        return InputView.readGameCommand();
+    }
+
+    public int getTotalInputLength(){
+        return inputHistory.size();
+    }
+}
