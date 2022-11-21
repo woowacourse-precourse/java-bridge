f17c5fce4bfbae702dbd2ae2927e4b4977927d80
sosow0212 <sosow0212@naver.com>
2022-11-21 14:33:48 +0900

    refactor : (BridgeController) 기존 컨트롤러에서 진행하던 게임 관련 로직을 BridgeGame으로 위임후 테스트코드 리팩토링

--- src/main/java/bridge/controller/BridgeController.java
+++ src/main/java/bridge/controller/BridgeController.java
@@ -12,7 +12,6 @@ public class BridgeController {
     private final BridgeGame bridgeGame = new BridgeGame();
     private final OutputView outputView = new OutputView();
     private final InputService inputService = new InputService();
-    private final static String RESET = "R";
 
     public void start() {
         gameStartMessage();
@@ -34,26 +33,27 @@ public class BridgeController {
         outputView.printResult(user);
     }
 
+    public boolean isUserAnswerCorrect(String moveDirection, Bridge bridge) {
+        return bridgeGame.isUserAnswerCorrect(moveDirection, bridge);
+    }
+
     public void processGame(Bridge bridge, User user, boolean isUserAnswerCorrect) {
         if (!isUserAnswerCorrect) {
             String gameStatus = inputService.requestStatusOfGame();
-            bridgeGame.doFailCase(bridge, user, gameStatus);
-            if (isResetCase(gameStatus)) {
-                outputView.initMapView();
-            }
+            doFailCase(bridge, user, gameStatus);
             return;
         }
-        bridgeGame.move(bridge, user);
+        doSuccessCase(bridge, user);
     }
 
-    public boolean isResetCase(String gameStatus) {
-        if (gameStatus.equals(RESET)) {
-            return true;
+    public void doFailCase(Bridge bridge, User user, String gameStatus) {
+        bridgeGame.handleFailCaseCommand(bridge, user, gameStatus);
+        if (bridgeGame.isResetCase(gameStatus)) {
+            outputView.initMapView();
         }
-        return false;
     }
 
-    public boolean isUserAnswerCorrect(String moveDirection, Bridge bridge) {
-        return bridge.isCorrectMoveDirection(moveDirection);
+    public void doSuccessCase(Bridge bridge, User user) {
+        bridgeGame.move(bridge, user);
     }
 }
