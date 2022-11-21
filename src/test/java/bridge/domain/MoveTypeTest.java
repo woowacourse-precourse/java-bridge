4ba5efe2d9c6fb9077d9ff635cffbaff47ae3ea5
Song kyeong seok <skj0922@kookmin.ac.kr>
2022-11-21 20:19:05 +0900

    feat(MoveTypeTest): 유저 입력과 MoveType에 정의된 type과 동일한지 확인하는 메서드 테스트

---
+++ src/test/java/bridge/domain/MoveTypeTest.java
@@ -0,0 +1,15 @@
+package bridge.domain;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class MoveTypeTest {
+
+    @Test
+    @DisplayName("MoveType String과 비교 메서드 테스트")
+    public void moveTypeCompareToTest() {
+        boolean result = MoveType.UP_TYPE.compareTo("UP");
+        Assertions.assertThat(result).isFalse();
+    }
+}
