54773e90cf48c95eabf72e97875031a1b4c4f06b
leesuengyeol <48343629+syhojeo@users.noreply.github.com>
2022-11-21 18:46:21 +0900

    feat: 입력된 다리 길이만큼 랜덤한 값으로 구성된 다리 List 만드는 기능 구현

--- src/main/java/bridge/InputView.java
+++ src/main/java/bridge/InputView.java
@@ -14,7 +14,7 @@ public class InputView {
      */
     public int readBridgeSize() {
 
-        System.out.println("다리 건너기 게임을 시작합니다.\n 다리의 길이를 입력해주세요.");
+        System.out.println("다리 건너기 게임을 시작합니다.\n다리의 길이를 입력해주세요.");
         String bridgeSize = Console.readLine();
         checkBridgeSizeValidation(bridgeSize);
 
