b04ede04851e0704e9289a8b06684c427165231e
rabbit0216 <rabbit1999k@gmail.com>
2022-11-21 13:48:29 +0900

    feat: 재시도 여부에 따라 행동하는 함수 구현

--- src/main/java/bridge/BridgeGame.java
+++ src/main/java/bridge/BridgeGame.java
@@ -28,6 +28,17 @@ public class BridgeGame {
      * <p>
      * 재시작을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void retry() {
+    public HashMap<String, Integer> retry(String command, int bridgeSize, int tryCount) {
+        HashMap<String, Integer> retryInfo = new HashMap<>();
+        if(command.equals(RETRY)){
+            //TODO: 재시도
+            retryInfo.put("bridge", 0);
+            retryInfo.put("count", tryCount++);
+            return retryInfo;
+        }
+        //TODO: 종료
+        retryInfo.put("bridge", bridgeSize);
+        retryInfo.put("count", tryCount);
+        return retryInfo;
     }
 }
