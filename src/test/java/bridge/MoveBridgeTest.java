d62d52e2fb209df944ac64c6a61138da254fd8d7
dohyun <ldh19738@gmail.com>
2022-11-21 20:28:53 +0900

    style(MoveBridgeTest): 클래스 이름 변경

--- src/test/java/bridge/MoveBridgeTest.java
+++ src/test/java/bridge/MoveBridgeTest.java
@@ -6,7 +6,7 @@ import org.junit.jupiter.api.Test;
 
 import java.util.NoSuchElementException;
 
-public class MoveBridgeTestSize {
+public class MoveBridgeTest {
     @Test
     void 다리_이동할_칸_입력_예외_테스트() {
         assertThrows(NoSuchElementException.class, () -> new MoveBridge("A"));
