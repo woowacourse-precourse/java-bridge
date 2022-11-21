56c401abc05b99bb8738d737640eacb1b5ee50b3
kimsinhyun <ksh415622@naver.com>
2022-11-21 22:09:27 +0900

    feat: 인게임 내 모든 출력을 관리하는 클래쓰에 moving, command 관련 출력 기능 추가
    1. printReadBridgeSize
       - Bridge Size를 읽을 때의 메시지 출력
    2. printReadBridgeSize
       - Bridge Size를 읽을 때의 메시지 출력
    3. printReadBridgeSize
       - Bridge Size를 읽을 때의 메시지 출력

--- src/main/java/bridge/OutputView.java
+++ src/main/java/bridge/OutputView.java
@@ -1,9 +1,23 @@
 package bridge;
 
+import bridge.Enums.OutputMessage;
+
 /**
  * 사용자에게 게임 진행 상황과 결과를 출력하는 역할을 한다.
  */
 public class OutputView {
+
+    public static void printReadBridgeSize(){
+        OutputMessage.GAME_START_MESSAGE.println();
+    }
+
+    public static void printReadMoving(){
+        OutputMessage.READ_MOVING_MESSAGE.println();
+    }
+
+    public static void printReadCommand(){
+        OutputMessage.READ_COMMAND_MESSAGE.println();
+    }
 
     /**
      * 현재까지 이동한 다리의 상태를 정해진 형식에 맞춰 출력한다.
