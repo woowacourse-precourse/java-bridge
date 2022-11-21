43a412a369e453ef82fe8826aeccb89d5c16c29c
ykmxxi <ykmxxi97@gmail.com>
2022-11-22 01:20:11 +0900

    fix: 시도 횟수 계산 수정

--- src/main/java/bridge/service/BridgeGame.java
+++ src/main/java/bridge/service/BridgeGame.java
@@ -40,8 +40,11 @@ public class BridgeGame {
      * 재시작을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
     public boolean retry(String gameCommand) {
-        attempts++;
-        return gameCommand.equals(GameCommand.RETRY.getType());
+        if (gameCommand.equals(GameCommand.RETRY.getType())) {
+            attempts++;
+            return true;
+        }
+        return false;
     }
 
     public boolean isEnd(String gameCommand, List<String> movingChoices, List<String> bridge) {
