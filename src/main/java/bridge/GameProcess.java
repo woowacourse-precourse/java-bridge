08be9b5faf0f3bde3e35192ec3437ae02204a8a7
soyunim <togksk123@naver.com>
2022-11-21 23:31:46 +0900

    feat: 게임 시작 전 시작 메세지 출력

--- src/main/java/bridge/GameProcess.java
+++ src/main/java/bridge/GameProcess.java
@@ -1,17 +1,26 @@
 package bridge;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class GameProcess {
     private static final InputView inputView = new InputView();
+    private static final OutputView outputView = new OutputView();
     private static final BridgeRandomNumberGenerator bridgeRandomNumberGenerator = new BridgeRandomNumberGenerator();
     private static final BridgeMaker bridgeMaker = new BridgeMaker(bridgeRandomNumberGenerator);
+
+    public static int gameCnt = 0;
     public static String length;
     public static int size;
+    public static List<String> bridge;
+    public static List<String> makingBridge = new ArrayList<>(Arrays.asList("[]","[]"));
 
     public static void gameStart(){
+        outputView.printStart();
         size = getSize();
-        bridgeMaker.makeBridge(size);
+        bridge = bridgeMaker.makeBridge(size);
+        gameRunning(bridge, size);
     }
 
     public static int getSize() {
@@ -26,5 +35,13 @@ public class GameProcess {
             throw new IllegalArgumentException("[ERROR] 다리 길이 입력범위가 잘못됨.");
         }
         return size;
+    }
+
+    public static void gameRunning(List<String> bridge, int size){
+        makingBridge = new ArrayList<>(Arrays.asList("[]","[]"));
+
+    }
+
+    public static void gameEnd(){
     }
 }
