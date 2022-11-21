928115d6ce05e713c30ba07a23b8fd3b56098c11
JONGMIN SHIN <jongmin373@gmail.com>
2022-11-21 17:05:31 +0900

    Feat<InputView> : add ReadMoving methods(U or D)

--- src/main/java/bridge/InputView.java
+++ src/main/java/bridge/InputView.java
@@ -29,4 +29,24 @@ public class InputView {
         return size;
     }
 
+    /**
+     * 사용자가 이동할 칸을 입력받는다.
+     */
+    public String readMoving() {
+        boolean isOK = false;
+        String input = "";
+        while(!isOK) {
+            input = inputMoving();
+            if(!exeption.inputUserMovingCheck(input)) {
+                continue;
+            }
+            isOK = true;
+        }
+        return input;
+    }
+    public String inputMoving(){
+        System.out.println("이동할 칸을 선택해주세요. (위: U, 아래: D)");
+        String input = Console.readLine();
+        return input;
+    }
 }
