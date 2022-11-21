a41a45d0991d22f23d69998c99f21894cdd9f708
nylonmusk <106235834+nylonmusk@users.noreply.github.com>
2022-11-21 20:11:12 +0900

    refactor(BridgeRandomNumberGenerator): 가독성 향상을 위해 Constant를 클래스변수로 선언

--- src/main/java/generator/BridgeRandomNumberGenerator.java
+++ src/main/java/generator/BridgeRandomNumberGenerator.java
@@ -4,10 +4,11 @@ import camp.nextstep.edu.missionutils.Randoms;
 import constant.NumberConstant;
 
 public class BridgeRandomNumberGenerator implements BridgeNumberGenerator {
+    private static final int RANDOM_UPPER_INCLUSIVE = NumberConstant.RANDOM_UPPER_INCLUSIVE.getConstant();
+    private static final int RANDOM_LOWER_INCLUSIVE = NumberConstant.RANDOM_LOWER_INCLUSIVE.getConstant();
 
     @Override
     public int generate() {
-        return Randoms.pickNumberInRange(NumberConstant.RANDOM_LOWER_INCLUSIVE.getConstant()
-                , NumberConstant.RANDOM_UPPER_INCLUSIVE.getConstant());
+        return Randoms.pickNumberInRange(RANDOM_LOWER_INCLUSIVE, RANDOM_UPPER_INCLUSIVE);
     }
 }
