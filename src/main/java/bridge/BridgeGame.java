81bbf90be40ba7c6f331efbcd3a16bcbb81ace5f
parkjungyoon <tbr06057@naver.com>
2022-11-21 18:36:12 +0900

    refactor: move() 메서드에 validation 추가

--- src/main/java/bridge/BridgeGame.java
+++ src/main/java/bridge/BridgeGame.java
@@ -3,6 +3,7 @@ package bridge;
 import java.util.List;
 
 import static bridge.validator.BlockValidator.validateInvalidRetryType;
+import static bridge.validator.BlockValidator.validateInvalidType;
 
 /**
  * 다리 건너기 게임을 관리하는 클래스
@@ -14,6 +15,8 @@ public class BridgeGame {
      * 이동을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
     public boolean move(String command, String bridge) {
+        validateInvalidType(command);
+
         if (command.equals(bridge)) {
             return true;
         }
@@ -27,6 +30,7 @@ public class BridgeGame {
      */
     public boolean retry(String input) {
         validateInvalidRetryType(input);
+
         if (input.equals("R")) return true;
         return false;
     }
