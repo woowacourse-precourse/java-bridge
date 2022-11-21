bf5114a5a98460386fb51e90534f8202d03bc9e3
ChanghyunKim <variey82p@gmail.com>
2022-11-22 00:22:54 +0900

    refactor(BridgeGame) : isSuccess 메서드 이동
    BridgeGameSystem으로 이동

--- src/main/java/bridge/BridgeGame.java
+++ src/main/java/bridge/BridgeGame.java
@@ -2,16 +2,15 @@ package bridge;
 
 import java.util.List;
 
+import static bridge.Bridge.*;
+import static bridge.Bridge.FAILURE_LOWER;
+
 /**
  * 다리 건너기 게임을 관리하는 클래스
  */
 public class BridgeGame {
     private static final InputView inputView = new InputView();
-    /**
-     * 사용자가 칸을 이동할 때 사용하는 메서드
-     * <p>
-     * 이동을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
-     */
+
     public static boolean move(List<String> bridge, String direct, int index) {
         String movableBlock = bridge.get(index);
         if(movableBlock.equals(direct)){
@@ -20,15 +19,7 @@ public class BridgeGame {
         return false;
     }
 
-    /**
-     * 사용자가 게임을 다시 시도할 때 사용하는 메서드
-     * <p>
-     * 재시작을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
-     */
 
-    public static boolean isSuccess(int bridgeSize, int index){
-        return bridgeSize == index;
-    }
     public static boolean retry(Bridge block) {
         boolean availability = block.getResult();
         if(!availability){
