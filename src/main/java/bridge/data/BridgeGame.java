032be0ff280e1b71538672fdcf05ab45404f77f3
sora5421 <sojeoung536985@gmail.com>
2022-11-22 06:43:05 +0900

    feat : add userBridge getter

--- src/main/java/bridge/data/BridgeGame.java
+++ src/main/java/bridge/data/BridgeGame.java
@@ -31,6 +31,10 @@ public class BridgeGame {
         return correct;
     }
 
+    public UserBridege getUserBridge() {
+        return userBridege;
+    }
+
     /**
      * 사용자가 게임을 다시 시도할 때 사용하는 메서드
      * <p>
