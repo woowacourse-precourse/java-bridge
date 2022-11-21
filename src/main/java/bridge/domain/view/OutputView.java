c06c8f23b89cc0b12e4cb934b36065c435bae04e
jiisuniui <dudndi9752@gmail.com>
2022-11-21 10:45:41 +0900

    feat(OutputView): 게임 최종 결과를 출력하는 기능 구현

--- src/main/java/bridge/domain/view/OutputView.java
+++ src/main/java/bridge/domain/view/OutputView.java
@@ -40,5 +40,12 @@ public class OutputView {
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
     public void printResult() {
+        printGuideMessage(GuideMessage.FINAL_RESULT_MESSAGE);
+        printMap();
+        System.out.println();
+        printGuideMessage(GuideMessage.SUCCESS_OR_FAILURE_MESSAGE);
+        System.out.println(crossBridge.getSuccess());
+        printGuideMessage(GuideMessage.TOTAL_TRY_MESSAGE);
+        System.out.println(crossBridge.getTotalTry());
     }
 }
