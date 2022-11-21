3594aa97d105305d9f8771a4a27423c6b24360e3
seonghoo1217 <seonghoo1217@naver.com>
2022-11-21 23:31:18 +0900

    feat(InputValidationTest) : add test case at user input retry condition value
    User input only retry condition R OR Q
    
    If user input another value return "[ERROR] and throw Error IllegalArgumentException

--- src/test/java/bridge/validation/InputValidationTest.java
+++ src/test/java/bridge/validation/InputValidationTest.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 import validation.InputValidation;
+import validation.RetryValidation;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
@@ -15,12 +16,15 @@ class InputValidationTest {
 	private final String error_message= "[ERROR]";
 
 	private static InputValidation iv;
+	private static RetryValidation rv;
+
 
 	private static InputView inputView;
 
 	@BeforeAll
 	static void initailize(){
 		iv=new InputValidation();
+		rv=new RetryValidation();
 		inputView=new InputView();
 	}
 
@@ -55,6 +59,23 @@ class InputValidationTest {
 		}catch (IllegalArgumentException e){
 			//then
 			assertThat(e.getMessage()).isEqualTo("[ERROR] 이동할 칸은 U 또는 D로 입력해야 합니다.");
+		}
+	}
+
+	@ParameterizedTest
+	@ValueSource(strings = {"한글","111111","","r","q","qr","qrQQR"})
+	@DisplayName("유저의_재도전_입력값_유효성검사")
+	void user_input_retry_condition_validation(String source){
+		try{
+			assertThatThrownBy(()-> {
+				//given
+				String bridge_move = rv.userInputRetryConditionValidation(source);
+				//when
+				if(bridge_move.equals(error_message)) throw new IllegalArgumentException("[ERROR] 재도전 여부는 R 또는 Q로 입력해야 합니다.");
+			});
+		}catch (IllegalArgumentException e){
+			//then
+			assertThat(e.getMessage()).isEqualTo("[ERROR] 재도전 여부는 R 또는 Q로 입력해야 합니다.");
 		}
 	}
 }
