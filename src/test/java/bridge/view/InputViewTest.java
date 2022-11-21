051a020b70174945ddb864e890388d404c75f51e
yejin <kwonyj1022@naver.com>
2022-11-21 14:25:41 +0900

    test: InputView - 잘못된 값 입력한 경우 재입력하도록 하는지 테스트

--- src/test/java/bridge/view/InputViewTest.java
+++ src/test/java/bridge/view/InputViewTest.java
@@ -5,8 +5,7 @@ import org.junit.jupiter.api.Test;
 import java.io.ByteArrayInputStream;
 import java.io.InputStream;
 
-import static org.assertj.core.api.AssertionsForClassTypes.*;
-import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
 
 class InputViewTest {
     @Test
@@ -19,20 +18,18 @@ class InputViewTest {
     }
 
     @Test
-    void 다리길이_입력_예외_테스트1() {
-        String input = "100";
-        InputStream in = new ByteArrayInputStream(input.getBytes());
-        System.setIn(in);
-        assertThatThrownBy(() -> new InputView().readBridgeSize())
-                .isInstanceOf(IllegalArgumentException.class);
-    }
-    @Test
-    void 다리길이_입력_예외_테스트2() {
-        String input = "five";
-        InputStream in = new ByteArrayInputStream(input.getBytes());
-        System.setIn(in);
-        assertThatThrownBy(() -> new InputView().readBridgeSize())
-                .isInstanceOf(IllegalArgumentException.class);
+    void 다리길이_입력_재입력_테스트() {
+        String input1 = "100";
+        String input2 = "five";
+        String input3 = "10";
+        InputStream in1 = new ByteArrayInputStream(input1.getBytes());
+        InputStream in2 = new ByteArrayInputStream(input2.getBytes());
+        InputStream in3 = new ByteArrayInputStream(input3.getBytes());
+        System.setIn(in1);
+        System.setIn(in2);
+        System.setIn(in3);
+        int size = new InputView().readBridgeSize();
+        assertThat(size).isEqualTo(10);
     }
 
     @Test
@@ -45,21 +42,18 @@ class InputViewTest {
     }
 
     @Test
-    void 이동할칸_입력_예외_테스트1() {
-        String input = "u";
-        InputStream in = new ByteArrayInputStream(input.getBytes());
-        System.setIn(in);
-        assertThatThrownBy(() -> new InputView().readMoving())
-                .isInstanceOf(IllegalArgumentException.class);
-    }
-
-    @Test
-    void 이동할칸_입력_예외_테스트2() {
-        String input = "2";
-        InputStream in = new ByteArrayInputStream(input.getBytes());
-        System.setIn(in);
-        assertThatThrownBy(() -> new InputView().readMoving())
-                .isInstanceOf(IllegalArgumentException.class);
+    void 이동할칸_입력_재입력_테스트() {
+        String input1 = "d";
+        String input2 = "2";
+        String input3 = "D";
+        InputStream in1 = new ByteArrayInputStream(input1.getBytes());
+        InputStream in2 = new ByteArrayInputStream(input2.getBytes());
+        InputStream in3 = new ByteArrayInputStream(input3.getBytes());
+        System.setIn(in1);
+        System.setIn(in2);
+        System.setIn(in3);
+        String moving = new InputView().readMoving();
+        assertThat(moving).isEqualTo("D");
     }
 
     @Test
@@ -72,20 +66,17 @@ class InputViewTest {
     }
 
     @Test
-    void 재시도여부_입력_예외_테스트1() {
-        String input = "r";
-        InputStream in = new ByteArrayInputStream(input.getBytes());
-        System.setIn(in);
-        assertThatThrownBy(() -> new InputView().readGameCommand())
-                .isInstanceOf(IllegalArgumentException.class);
-    }
-
-    @Test
-    void 재시도여부_입력_예외_테스트2() {
-        String input = "no";
-        InputStream in = new ByteArrayInputStream(input.getBytes());
-        System.setIn(in);
-        assertThatThrownBy(() -> new InputView().readGameCommand())
-                .isInstanceOf(IllegalArgumentException.class);
+    void 재시도여부_입력_재입력_테스트() {
+        String input1 = "q";
+        String input2 = "no";
+        String input3 = "Q";
+        InputStream in1 = new ByteArrayInputStream(input1.getBytes());
+        InputStream in2 = new ByteArrayInputStream(input2.getBytes());
+        InputStream in3 = new ByteArrayInputStream(input3.getBytes());
+        System.setIn(in1);
+        System.setIn(in2);
+        System.setIn(in3);
+        String gameCommand = new InputView().readGameCommand();
+        assertThat(gameCommand).isEqualTo("Q");
     }
 }
