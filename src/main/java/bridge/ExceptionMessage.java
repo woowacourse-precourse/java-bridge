d00883bc1108b05f6f0d1dd137bcf4dad593b024
JJ503 <63184334+JJ503@users.noreply.github.com>
2022-11-22 01:06:45 +0900

    feat(ExceptionMessage): 예외 처리 상수 추가
    INPUT_WRONG_BRIDGE_MOVEMENT_MESSAGE 추가

--- src/main/java/bridge/ExceptionMessage.java
+++ src/main/java/bridge/ExceptionMessage.java
@@ -1,7 +1,8 @@
 package bridge;
 
 public enum ExceptionMessage {
-    INPUT_WRONG_BRIDGE_LENGTH_MESSAGE("다리 길이는 3부터 20 사이의 숫자여야 합니다.");
+    INPUT_WRONG_BRIDGE_LENGTH_MESSAGE("다리 길이는 3부터 20 사이의 숫자여야 합니다."),
+    INPUT_WRONG_BRIDGE_MOVEMENT_MESSAGE("위 칸으로 이동할 시 'U', 아래 칸으로 이동할 시 'D'를 입력해 주셔야 합니다.");
 
     private final String ERROR_TAG = "[ERROR] ";
     private final String message;
