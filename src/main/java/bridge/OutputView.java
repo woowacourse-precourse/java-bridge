98be71149baccd5569ec0988cbc191eb04a769fb
jxixeun <jxixeun@gmail.com>
2022-11-22 04:40:30 +0900

    feat: 현재까지 이동한 다리 상태 출력 기능 작성

--- src/main/java/bridge/OutputView.java
+++ src/main/java/bridge/OutputView.java
@@ -1,5 +1,7 @@
 package bridge;
 
+import java.util.List;
+
 /**
  * 사용자에게 게임 진행 상황과 결과를 출력하는 역할을 한다.
  */
@@ -17,7 +19,23 @@ public class OutputView {
      * <p>
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void printMap() {
+    public void printMap(List<String> upCells, List<String> downCells) {
+        System.out.print("[");
+        for (int i=0;i<upCells.size();i++){
+            System.out.print(" " + upCells.get(i) + " ");
+            if (i < upCells.size()-1){
+                System.out.print("|");
+            }
+        }
+        System.out.println("]");
+        System.out.print("[");
+        for (int i=0;i<downCells.size();i++){
+            System.out.print(" " + downCells.get(i) + " ");
+            if (i < downCells.size()-1){
+                System.out.print("|");
+            }
+        }
+        System.out.println("]");
     }
 
     /**
