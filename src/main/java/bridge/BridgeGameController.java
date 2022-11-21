5f4242d2d034c0c33e1a5a5ca6bfd675d1b04828
gwj0421 <gwj0421@gmail.com>
2022-11-21 19:03:25 +0900

    feat: 다리 게임 통제를 위해 controller 클래스 추가

---
+++ src/main/java/bridge/BridgeGameController.java
@@ -0,0 +1,22 @@
+package bridge;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class BridgeGameController {
+    private BridgeGame bridgeGame;
+    private InputView inputView;
+    private OutputView outputView;
+    private int bridgeSize, attepmtCnt;
+    private boolean isSucess;
+    private List<String> playerBridgeMap;
+    private List<String> RandomBridgeMap;
+
+    public BridgeGameController() {
+        bridgeGame = new BridgeGame();
+        inputView = new InputView(new CheckNum());
+        playerBridgeMap = new ArrayList<>();
+        RandomBridgeMap = new ArrayList<>();
+        outputView = new OutputView();
+    }
+}
