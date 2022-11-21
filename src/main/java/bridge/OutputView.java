4c44da7465effb9396c308c73bcfd980ebfb9401
WonSeok-dd <cluejws@gmail.com>
2022-11-21 23:33:34 +0900

    fix(OutputView): 다리 상태 출력 오류 수정

--- src/main/java/bridge/OutputView.java
+++ src/main/java/bridge/OutputView.java
@@ -24,12 +24,12 @@ public class OutputView {
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
     public static void printMap(List<String> bridgeUpResults, List<String> bridgeDownResults) {
-        printBridge(bridgeUpResults);
-        printBridge(bridgeDownResults);
+        System.out.println(readRangeBridge(bridgeUpResults));
+        System.out.println(readRangeBridge(bridgeDownResults));
         System.out.println();
     }
 
-    private static String printBridge(List<String> bridgeResults){
+    private static String readRangeBridge(List<String> bridgeResults){
         return BRIDGE_START_DELIMITER + readBridge(bridgeResults) + BRIDGE_END_DELIMITER;
     }
 
