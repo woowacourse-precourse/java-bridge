a9df8ba9da9d7b2a09ca9db6c47d136ce8cd5c0d
Jaewoong Kim <kimje1109@gmail.com>
2022-11-21 16:22:24 +0900

    feat: 다리를 한 칸 이동하는 기능 구현

--- src/main/java/bridge/BridgeGame.java
+++ src/main/java/bridge/BridgeGame.java
@@ -1,16 +1,37 @@
 package bridge;
 
+import java.util.List;
+
 /**
  * 다리 건너기 게임을 관리하는 클래스
  */
 public class BridgeGame {
+
+    List<String> bridge;
+    Boolean isMovable;
+    Integer bridgeIndex = 0;
+
+    public BridgeGame(List<String> bridge) {
+        this.bridge = bridge;
+    }
+
+    private boolean canMove(String moving) {
+        return moving.equals(bridge.get(bridgeIndex));
+    }
 
     /**
      * 사용자가 칸을 이동할 때 사용하는 메서드
      * <p>
      * 이동을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void move() {
+    public void move(String moving) {
+        if (canMove(moving)) {
+            isMovable = true;
+        }
+        if (!canMove(moving)) {
+            isMovable = false;
+        }
+        bridgeIndex++;
     }
 
     /**
