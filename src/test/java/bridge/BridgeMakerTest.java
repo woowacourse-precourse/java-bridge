a022242b12ba666d12df8463542bf4ec27b45049
leeyongjun <leeyongjun0310@naver.com>
2022-11-22 00:53:59 +0900

    refactor(bridge): BridgeMaker 객체 private으로 변경

--- src/test/java/bridge/BridgeMakerTest.java
+++ src/test/java/bridge/BridgeMakerTest.java
@@ -4,17 +4,15 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
-import java.util.List;
-
 import static org.assertj.core.api.Assertions.*;
 
 class BridgeMakerTest {
+    private final BridgeMaker bridgeMaker = new BridgeMaker(new BridgeRandomNumberGenerator());
 
     @DisplayName("입력된 크기만큼 다리가 생성되는지 확인")
     @ValueSource(strings = {"10", "3", "100", "123", "999", "1000", "10000"})
     @ParameterizedTest
     void makeBridge_입력된_크기만큼_다리가_생성되는지_확인(Integer size) {
-        BridgeMaker bridgeMaker = new BridgeMaker(new BridgeRandomNumberGenerator());
         assertThat(bridgeMaker.makeBridge(size).size()).isEqualTo(size);
     }
 
@@ -22,7 +20,6 @@ class BridgeMakerTest {
     @ValueSource(strings = {"10", "3", "100", "123", "999", "1000", "10000"})
     @ParameterizedTest
     void makeBridge_U_D_만_포함하는지_확인(Integer size) {
-        BridgeMaker bridgeMaker = new BridgeMaker(new BridgeRandomNumberGenerator());
         assertThat(bridgeMaker.makeBridge(size)).contains("U", "D");
     }
 }
