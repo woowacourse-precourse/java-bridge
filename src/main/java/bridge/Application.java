5e9c37167f5cc4017fc8870df18270bd2c2e46ca
Juuuic <sn990908a@gmail.com>
2022-11-21 19:42:39 +0900

    docs: 기능목록 수정

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -3,6 +3,16 @@ package bridge;
 public class Application {
 
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+
+        System.out.print("다리 건너기 게임을 시작합니다.");
+        // 다리 길이 입력 -> inputView 클래스의 readBridgeSize()
+        // 다리 길이만큼 다리 생성 -> BridgeMaker 클래스의 makeBridge(int size)
+        // 이동할 칸 입력받기 -> InputView 클래스의 readMoving()
+        // 입력받은 값에 따라 이동 -> BridgeGame 클래스의 move()
+        // 이동한 후 다리 출력 -> OutputView 클래스의 printMap()
+        // 성공 시 -> OutputView 클래스의 printResult() -> 종료
+        // 실패 시 재시작 여부 입력 받음-> InputView 클래스의 readGameCommand()
+        // 종료 시 -> OutputView 클래스의 printResult() -> 종료
+        // 재시작 시 -> BridgeGame 클래스의 retry()
     }
 }
