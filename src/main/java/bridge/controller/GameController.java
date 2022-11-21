186818617d40b78501e76281c3f00999e9504420
jijongkwon <help3451@naver.com>
2022-11-21 22:34:50 +0900

    feat(GameController) : 움직임 저장기능 구현

--- src/main/java/bridge/controller/GameController.java
+++ src/main/java/bridge/controller/GameController.java
@@ -13,7 +13,6 @@ public class GameController {
     private InputView inputView;
     private OutputView outputView;
     private List<String> bridge;
-
     private List<String> upBridge;
     private List<String> downBridge;
 
@@ -23,5 +22,33 @@ public class GameController {
         stage = inputView.readBridgeSize();
         outputView.printGameStart();
         bridge = bridgeMaker.makeBridge(stage);
+    }
+
+    public void addBridgeMove(String direction) {
+        if (direction.equals("U")) {
+            addUpMove(bridgeGame.move(bridge, direction, stage));
+            return;
+        }
+        addDownMove(bridgeGame.move(bridge, direction, stage));
+    }
+
+    public void addUpMove(String move) {
+        if (move.equals(" O ")) {
+            upBridge.add(" O ");
+            downBridge.add("   ");
+            return;
+        }
+        upBridge.add(" X ");
+        downBridge.add("   ");
+    }
+
+    public void addDownMove(String move) {
+        if (move.equals(" O ")) {
+            downBridge.add(" O ");
+            upBridge.add("   ");
+            return;
+        }
+        downBridge.add(" X ");
+        upBridge.add("   ");
     }
 }
