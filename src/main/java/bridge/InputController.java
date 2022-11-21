b0f0ba493118c9643ea71a5785a7148242bd3da9
KyungKyu Park <rudrb0505@gmail.com>
2022-11-21 10:07:19 +0900

    feat(InputController): 사용자의 입력을 처리하는 클래스 구현

---
+++ src/main/java/bridge/InputController.java
@@ -0,0 +1,62 @@
+package bridge;
+
+import bridge.validator.BridgeLengthValidate;
+import bridge.validator.CommandValidate;
+
+public class InputController {
+	public static int getBridgeSize() {
+		try {
+			String userInput = InputView.readBridgeSize();
+			checkBridgeSize(userInput);
+			return parseBridgeSize(InputView.readBridgeSize());
+		} catch (IllegalArgumentException e) {
+			System.out.println(e.getMessage());
+			return getBridgeSize();
+		}
+	}
+
+	private static int parseBridgeSize(String userInput) {
+		return Integer.parseInt(userInput);
+	}
+
+	private static void checkBridgeSize(String userInput) {
+		BridgeLengthValidate.validateNumber(userInput);
+		BridgeLengthValidate.validateRange(userInput);
+	}
+
+	public static String getMoving() {
+		try {
+			String userInput = InputView.readMoving();
+			checkMoving(userInput);
+			return userInput;
+		} catch (IllegalArgumentException e) {
+			System.out.println(e.getMessage());
+			return getMoving();
+		}
+	}
+
+	private static void checkMoving(String userInput) {
+		CommandValidate.validateCharacter(userInput);
+		CommandValidate.validateEnglish(userInput);
+		CommandValidate.validateUpperCase(userInput);
+		CommandValidate.validateExactCharacterUD(userInput);
+	}
+
+	public static String getGameCommand() {
+		try {
+			String userInput = InputView.readGameCommand();
+			checkGameCommand(userInput);
+			return userInput;
+		} catch (IllegalArgumentException e) {
+			System.out.println(e.getMessage());
+			return getGameCommand();
+		}
+	}
+
+	private static void checkGameCommand(String userInput) {
+		CommandValidate.validateCharacter(userInput);
+		CommandValidate.validateEnglish(userInput);
+		CommandValidate.validateUpperCase(userInput);
+		CommandValidate.validateExactCharacterRQ(userInput);
+	}
+}
--- src/main/java/bridge/InputView.java
+++ src/main/java/bridge/InputView.java
@@ -21,49 +21,25 @@ public class InputView {
 	/**
 	 * 다리의 길이를 입력받는다.
 	 */
-	public int readBridgeSize() {
+	public static String readBridgeSize() {
 		System.out.println(INPUT_BRIDGE_LENGTH);
-		String userInput = Console.readLine();
-		checkBridgeSize(userInput);
-		return Integer.parseInt(userInput);
+		return Console.readLine();
 	}
 
-	private static void checkBridgeSize(String userInput) {
-		BridgeLengthValidate.validateNumber(userInput);
-		BridgeLengthValidate.validateRange(userInput);
-	}
 
 	/**
 	 * 사용자가 이동할 칸을 입력받는다.
 	 */
-	public String readMoving() {
+	public static String readMoving() {
 		System.out.println(INPUT_DIRECTION);
-		String userInput = Console.readLine();
-		checkMoving(userInput);
-		return userInput;
-	}
-
-	private static void checkMoving(String userInput) {
-		CommandValidate.validateCharacter(userInput);
-		CommandValidate.validateEnglish(userInput);
-		CommandValidate.validateUpperCase(userInput);
-		CommandValidate.validateExactCharacterUD(userInput);
+		return Console.readLine();
 	}
 
 	/**
 	 * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
 	 */
-	public String readGameCommand() {
+	public static String readGameCommand() {
 		System.out.println(INPUT_RESTART_OR_QUIT);
-		String userInput = Console.readLine();
-		checkGameCommand(userInput);
-		return userInput;
-	}
-
-	private static void checkGameCommand(String userInput) {
-		CommandValidate.validateCharacter(userInput);
-		CommandValidate.validateEnglish(userInput);
-		CommandValidate.validateUpperCase(userInput);
-		CommandValidate.validateExactCharacterRQ(userInput);
+		return Console.readLine();
 	}
 }
