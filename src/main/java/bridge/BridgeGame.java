5d3fe9e443a424ea7f49ffe0dac68111603c35cf
sudo-dustle <asdf748596123@gmail.com>
2022-11-22 05:05:50 +0900

    style: 매직 넘버 변환

--- src/main/java/bridge/BridgeGame.java
+++ src/main/java/bridge/BridgeGame.java
@@ -8,6 +8,7 @@ import java.util.List;
 public class BridgeGame {
     public static final int INIT_TRY_COUNT = 1;
     public static final int INIT_BRIDGE_INDEX = 0;
+    public static final String RETRY = "R";
 
     private final List<String> answerBridge;
     private BridgeMap bridgeMap;
@@ -74,7 +75,7 @@ public class BridgeGame {
      * 재시작을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
     public void retry(String userCommand) {
-        if(userCommand.equals("R")) {
+        if(userCommand.equals(RETRY)) {
             init(tryCount + 1);
         }
     }
