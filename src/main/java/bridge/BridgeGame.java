a14c1346fff1f3d5abe6e61479a9c611c3758eb0
poter0501 <poter0501@gmail.com>
2022-11-21 21:35:28 +0900

    chore(BridgeGame): 이동 경로를 저장하는 path 필드의 타입 변경

--- src/main/java/bridge/BridgeGame.java
+++ src/main/java/bridge/BridgeGame.java
@@ -7,7 +7,7 @@ import java.util.List;
  * 다리 건너기 게임을 관리하는 클래스
  */
 public class BridgeGame {
-    private List<Integer> path;
+    private List<String> path;
 
     public BridgeGame(){
         this.path=new ArrayList<>();
