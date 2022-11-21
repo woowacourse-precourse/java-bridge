79d0e0061bb46274ef0d717c1207d8ee739748a8
sdsr <dlxodnr1212@gmail.com>
2022-11-22 01:57:45 +0900

    fix: 하드코딩 제거

--- src/main/java/bridge/ErrorCheck.java
+++ src/main/java/bridge/ErrorCheck.java
@@ -9,18 +9,21 @@ public class ErrorCheck {
     private static final String ICON_DOWN = "D";
     private static final String ICON_RETRY = "R";
     private static final String ICON_QUIT = "Q";
+    private static final int MININUM_BRIDGE_SIZE = 3;
+    private static final int MAXINUM_BRIDGE_SIZE = 2;
+    private static final int ZERO = 0;
 
     public static int stringToInt(String goToNumber) {
         int checkNumber = Integer.parseInt(goToNumber);
-        if(checkNumber<3)
+        if(checkNumber<MININUM_BRIDGE_SIZE)
             throw new CheckBridgeLengthException();
-        if(checkNumber>20)
+        if(checkNumber>MAXINUM_BRIDGE_SIZE)
             throw new CheckBridgeLengthException();
         return checkNumber;
     }
 
     public static void isItNumber(String haveToCheck) {
-        for (int i = 0; i < haveToCheck.length(); i++)
+        for (int i = ZERO; i < haveToCheck.length(); i++)
             if (!Character.isDigit(haveToCheck.charAt(i)))
                 throw new CheckBridgeLengthException();
     }
