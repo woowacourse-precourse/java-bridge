7c0ab0ca005d299ef4e83b8fd6fc32f5b3f70f48
minaver <psmjoshua@gmail.com>
2022-11-21 15:31:14 +0900

    fix(OutputView): printResult method parameter 변경

--- src/main/java/bridge/view/OutputView.java
+++ src/main/java/bridge/view/OutputView.java
@@ -1,5 +1,6 @@
 package bridge.view;
 
+import bridge.bridgeGame.BridgeGameResultDto;
 import bridge.bridgeGame.StepStatus;
 import bridge.enums.GameStatus;
 import bridge.enums.UpDown;
@@ -23,15 +24,14 @@ public class OutputView {
 
     /**
      * 게임의 최종 결과를 정해진 형식에 맞춰 출력한다.
-     * <p>
-     * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
+     * @param bridgeGameResultDto 게임 결과 Dto
      */
-    public void printResult(List<StepStatus> gradingBoard, GameStatus gameStatus, int totalAttemptNum) {
+    public void printResult(BridgeGameResultDto bridgeGameResultDto) {
         System.out.println("최종 게임 결과");
-        printMap(gradingBoard);
+        printMap(bridgeGameResultDto.getGradingBoard());
 
-        System.out.println("게임 성공 여부: " + gameStatus.label());
-        System.out.println("총 시도한 횟수: " + totalAttemptNum);
+        System.out.println("게임 성공 여부: " + bridgeGameResultDto.getGameStatus().label());
+        System.out.println("총 시도한 횟수: " + bridgeGameResultDto.getTotalAttemptNum());
     }
 
     /**
