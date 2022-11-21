80844885c4b76227d7762fdddce0bd678db6051c
Hyeonjun Park <phjppo0918@gmail.com>
2022-11-21 14:11:08 +0900

    test(InputValidatorTest) : 다리 개수 입력시 범위 초과시 예외처리

--- src/test/java/bridge/InputValidatorTest.java
+++ src/test/java/bridge/InputValidatorTest.java
@@ -20,4 +20,28 @@ class InputValidatorTest {
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
+    @Test
+    @DisplayName("다리 개수 입력시 범위 초과시 예외처리")
+    void inputBridgeSizeOutOfSize() throws Exception {
+        //given
+        String input = "25";
+        //when
+        //then
+        assertThatThrownBy(() -> inputValidator.validateInputBridgeSize(input))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    @DisplayName("다리 개수 입력시 최소값 미달시 예외처리")
+    void inputBridgeSizeLess() throws Exception {
+        //given
+        String input = "2";
+        //when
+        //then
+        assertThatThrownBy(() -> inputValidator.validateInputBridgeSize(input))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    
+
 }
