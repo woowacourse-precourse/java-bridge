c2a4b6a2e9eae47c5edecbfc1e9f3bcffa923978
whdcks420 <whdcks420@gmail.com>
2022-11-22 03:39:27 +0900

    refacor: map 로직 이동

--- src/main/java/bridge/domain/BridgeGame.java
+++ src/main/java/bridge/domain/BridgeGame.java
@@ -1,23 +1,18 @@
 package bridge.domain;
 
-import java.util.Arrays;
 import java.util.List;
-import java.util.stream.Collectors;
 
 import static bridge.domain.BridgeGameConstant.START_INDEX;
 import static bridge.domain.BridgeStatus.FAIL;
 import static bridge.domain.BridgeStatus.SUCCESS;
-import static bridge.ui.ViewConstant.*;
 
 /**
  * 다리 건너기 게임을 관리하는 클래스
  */
 public class BridgeGame {
-    private static final String RESTART = "R";
-
     private final List<String> bridge;
-    private final BridgeMap bridgeMap;
     private final GameCount gameCount;
+    private final BridgeMap bridgeMap;
 
     private boolean status;
     private int mapCoordinate;
@@ -43,21 +38,17 @@ public class BridgeGame {
         mapCoordinate++;
     }
 
-    private boolean isPossibleCrossBridge(String direction) {
-        return this.status = bridge.get(mapCoordinate).equals(direction);
-    }
-
     /**
      * 사용자가 게임을 다시 시도할 때 사용하는 메서드
      * <p>
      * 재시작을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public boolean retry(String gameCommand) {
-        if (gameCommand.equals(RESTART)) {
-            setUpRetry();
-            return true;
-        }
-        return false;
+    public void retry() {
+        setUpRetry();
+    }
+
+    private boolean isPossibleCrossBridge(String direction) {
+        return this.status = bridge.get(mapCoordinate).equals(direction);
     }
 
     private void setUpRetry() {
@@ -70,32 +61,35 @@ public class BridgeGame {
         this.mapCoordinate = START_INDEX;
     }
 
-    public boolean checkStatus() {
-        return this.status;
+    public boolean isContinueStatus() {
+        return this.status && !this.isWinningGame();
     }
 
-    public String getMap() {
-        StringBuilder sb = new StringBuilder();
-        Arrays.stream(bridgeMap.bridgeStatuses).forEach(row -> {
-            sb.append(START_BRIDGE_SHAPE);
-            sb.append(buildBridge(row));
-            sb.append(END_BRIDGE_SHAPE);
-        });
-        return sb.toString();
+    public boolean isRetryable() {
+        return !this.status;
     }
 
-    private String buildBridge(BridgeStatus[] row) {
-        return Arrays.asList(row).subList(START_INDEX, mapCoordinate)
-                .stream()
-                .map(bridgeBlockStatus -> bridgeBlockStatus.buildBridge)
-                .collect(Collectors.joining(JOINING_DELIMITER));
-    }
-
-    public boolean checkWinning() {
+    private boolean isWinningGame() {
         return Result.checkWinning(mapCoordinate, bridge) && this.status;
     }
 
     public Result getResult() {
-        return new Result(getMap(), this.status, gameCount.getGameCount());
+        return new Result(this);
+    }
+
+    public BridgeMap getBridgeMap() {
+        return bridgeMap;
+    }
+
+    public GameCount getGameCount() {
+        return gameCount;
+    }
+
+    public boolean isStatus() {
+        return status;
+    }
+
+    public int getMapCoordinate() {
+        return mapCoordinate;
     }
 }
