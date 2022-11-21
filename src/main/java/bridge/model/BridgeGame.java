30878317c44776dba8d78eed54cb5f18ac5dae95
kawkmin <86940335+kawkmin@users.noreply.github.com>
2022-11-21 19:21:19 +0900

    feat: 다리를 위의 다리랑 아래 다리로 나눠서 O, X로 표현된 리스트로 저장 구현

--- src/main/java/bridge/model/BridgeGame.java
+++ src/main/java/bridge/model/BridgeGame.java
@@ -8,15 +8,20 @@ import bridge.view.OutputView;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * 다리 건너기 게임을 관리하는 클래스
  */
 public class BridgeGame {
     private List<String> bridge;
+    private List<String> upPresentBridge;
+    private List<String> downPresentBridge;
 
     public BridgeGame(List<String> bridge) {
         this.bridge=bridge;
+        upPresentBridge=new ArrayList<>();
+        downPresentBridge=new ArrayList<>();
     }
     /**
      * 사용자가 칸을 이동할 때 사용하는 메서드
@@ -24,7 +29,14 @@ public class BridgeGame {
      * 이동을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
 
-    public void move() {
+    public void move(String userMove) {
+        if(userMove=="U"){
+            upPresentBridge.add("O");
+            downPresentBridge.add("X");
+            return;
+        }
+        upPresentBridge.add("X");
+        downPresentBridge.add("O");
     }
 
     /**
@@ -33,5 +45,17 @@ public class BridgeGame {
      * 재시작을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
     public void retry() {
+    }
+
+    public List<String> getUpPresentBridge() {
+        return upPresentBridge;
+    }
+
+    public List<String> getDownPresentBridge() {
+        return downPresentBridge;
+    }
+
+    public List<String> getBridge() {
+        return bridge;
     }
 }
