bad4d806b4f5eefcee6facb92dca2bcc89c196e2
Yerim lee <103623292+lyrWinterCat@users.noreply.github.com>
2022-11-21 14:44:21 +0900

    feat (Application, BridgeGameController) : 게임 실행 컨트롤러, 애플리케이션 구현

--- src/main/java/bridge/controller/BridgeGameController.java
+++ src/main/java/bridge/controller/BridgeGameController.java
@@ -1,4 +1,12 @@
 package bridge.controller;
 
+import bridge.service.BridgeService;
+
 public class BridgeGameController {
+    BridgeService bridgeService = new BridgeService();
+
+    public void run(){
+        bridgeService.startBridgeGame();
+        bridgeService.moveBridge();
+    }
 }
