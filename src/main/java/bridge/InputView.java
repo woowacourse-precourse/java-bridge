22c9198476fa0c24705b6464ff542d02e628d6ba
mng990 <ryoo0504@pusan.ac.kr>
2022-11-22 02:13:15 +0900

    feat(main/java/bridge/InputView.java): InputView.java 작성
    +readBridgeSize()
    +readMoving()
    +readGameCommand()

--- src/main/java/bridge/InputView.java
+++ src/main/java/bridge/InputView.java
@@ -1,28 +1,36 @@
 package bridge;
 
-/**
- * 사용자로부터 입력을 받는 역할을 한다.
- */
+import static camp.nextstep.edu.missionutils.Console.readLine;
+
+
 public class InputView {
 
+    private final static String START_MESSAGE = "다리 건너기 게임을 시작합니다.";
+    private final static String MOVE_MESSAGE = "이동할 칸을 선택해주세요. (위: U, 아래: D)";
+    private final static String COMMAND_MESSAGE = "게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)";
     /**
      * 다리의 길이를 입력받는다.
      */
-    public int readBridgeSize() {
-        return 0;
+    public String readBridgeSize() {
+        System.out.println(START_MESSAGE);
+
+        return readLine();
     }
 
     /**
      * 사용자가 이동할 칸을 입력받는다.
      */
     public String readMoving() {
-        return null;
+        System.out.println(MOVE_MESSAGE);
+        return readLine();
     }
 
     /**
      * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
      */
     public String readGameCommand() {
-        return null;
+        System.out.println(COMMAND_MESSAGE);
+
+        return readLine();
     }
 }
