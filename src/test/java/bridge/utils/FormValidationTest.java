f8f28ae48ed23accca31b5da9d7b7bc26ab30a91
김현재 <41482946+galug@users.noreply.github.com>
2022-11-21 10:36:03 +0900

    test(formvalidationtest): 이동할 칸 입력에 대한 형식 검증 테스트 추가

--- src/test/java/bridge/utils/FormValidationTest.java
+++ src/test/java/bridge/utils/FormValidationTest.java
@@ -6,22 +6,24 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 class FormValidationTest {
 
     private final FormValidation formValidation = new FormValidation();
 
     @DisplayName("다리의 사이즈로 1~2자리의 숫자를 입력받지 않으면 예외를 발생시킨다.")
     @ParameterizedTest
-    @CsvSource({"1a","100","a"})
+    @CsvSource({"1a", "100", "a"})
     void validateBridgeSizeInputForm(String input) {
-        Assertions.assertThatThrownBy(() ->formValidation.validateMovingInputForm(input))
-                        .isInstanceOf(IllegalArgumentException.class);
+        Assertions.assertThatThrownBy(() -> formValidation.validateMovingInputForm(input))
+                .isInstanceOf(IllegalArgumentException.class);
     }
 
-    @Test
-    void validateMovingInputForm() {
+    @DisplayName("이동할 칸으로 U,D 이외에 다른 문자열를 입력하면 예외를 발생시킨다.")
+    @ParameterizedTest
+    @CsvSource({"u", "d", "UU", "DD", "A", "1", "1U"})
+    void validateMovingInputForm(String input) {
+        Assertions.assertThatThrownBy(() -> formValidation.validateMovingInputForm(input))
+                .isInstanceOf(IllegalArgumentException.class);
     }
 
     @Test
