5fe4d2067518af8adc804bddce3be07011c2e521
qkrdmswl <eunji303@naver.com>
2022-11-22 04:02:44 +0900

    feat: generate bridge number in random

--- src/main/java/utils/BridgeNumberGenerator.java
+++ src/main/java/utils/BridgeNumberGenerator.java
@@ -1,4 +1,4 @@
-package bridge;
+package utils;
 
 @FunctionalInterface
 public interface BridgeNumberGenerator {
--- src/main/java/utils/BridgeRandomNumberGenerator.java
+++ src/main/java/utils/BridgeRandomNumberGenerator.java
@@ -1,4 +1,4 @@
-package bridge;
+package utils;
 
 import camp.nextstep.edu.missionutils.Randoms;
 
