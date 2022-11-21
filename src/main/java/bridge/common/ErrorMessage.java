a81a24ab9bf0ed47228d293e39ba9f0adad794b6
jmjmjames <chungjm0711@gmail.com>
2022-11-21 23:02:10 +0900

    feat: ErrorMessage#isInvalidRoundRange 메시지 추가

--- src/main/java/bridge/common/ErrorMessage.java
+++ src/main/java/bridge/common/ErrorMessage.java
@@ -7,6 +7,7 @@ public class ErrorMessage {
     private static final String INVALID_BRIDGE_SIZE = "다리 길이는 3부터 20 사이의 숫자여야 합니다.";
     private static final String INVALID_MOVING = "이동할 칸은 U, D여야 합니다. (위: U, 아래: D)";
     private static final String INVALID_GAME_COMMAND = "게임 재시작/종료 여부는 R, D여야 합니다. (재시작: R, 종료: Q)";
+    private static final String INVALID_ROUND_RANGE = "허용된 라운드 범위를 벗어났습니다.";
 
     public static String isInvalidNumber() {
         return generate(INVALID_NUMBER);
@@ -22,6 +23,10 @@ public class ErrorMessage {
 
     public static String isInvalidGameCommand() {
         return generate(INVALID_GAME_COMMAND);
+    }
+
+    public static String isInvalidRoundRange() {
+        return generate(INVALID_ROUND_RANGE);
     }
 
     private static String generate(String message) {
