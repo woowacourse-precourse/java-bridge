af266933e112689d08cba32e2178447a175cb05b
renechoi90@gmail.com <renechoi90@gmail.com>
2022-11-21 16:03:30 +0900

    test : 중복 코드 제거 및 메서드 간소화

--- src/test/java/bridge/domain/map/BridgeMapTest.java
+++ src/test/java/bridge/domain/map/BridgeMapTest.java
@@ -2,23 +2,30 @@ package bridge.domain.map;
 
 import bridge.domain.game.CrossingDecision;
 import bridge.domain.player.MovementCommand;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.*;
+import static org.assertj.core.util.Lists.*;
 
 class BridgeMapTest {
 
-	@DisplayName("BridgeMap의 리스트 구조를 검증한다 : 첫회, 맞는 경우")
+	private static final List<String> bridgeMocked = newArrayList("U", "U", "D");
+
+	@AfterEach
+	@BeforeEach
+	void clearMap() {
+		BridgeMap.clearBridgeDescription();
+	}
+
+	@DisplayName("BridgeMap의 리스트 구조를 검증한다: 첫회이면서 맞는 경우")
 	@Test
 	void verify_BridgeMap_CanDrawMap_FromBeginning_AsIsCorrect() {
-		MovementCommand movementCommand = MovementCommand.valueOf("U");
-		List<String> bridgeMocked = List.of("U", "U");
-		CrossingDecision crossingDecision = CrossingDecision.judgingBy(movementCommand, bridgeMocked);
-
-		BridgeMap bridgeMap = BridgeMap.constructedBy(crossingDecision, movementCommand);
+		BridgeMap bridgeMap = setUp("U", 1);
 
 		List<String> actual1 = bridgeMap.getUpperBridgeDescription();
 		List<String> actual2 = bridgeMap.getUnderBridgeDescription();
@@ -28,18 +35,12 @@ class BridgeMapTest {
 
 		assertThat(actual1).isEqualTo(expected1);
 		assertThat(actual2).isEqualTo(expected2);
-
-		BridgeMap.clearBridgeDescription();
 	}
 
-	@DisplayName("BridgeMap의 리스트 구조를 검증한다 : 첫회, 틀린 경우")
+	@DisplayName("BridgeMap의 리스트 구조를 검증한다: 첫회이면서 틀린 경우")
 	@Test
 	void verify_BridgeMap_CanDrawMap_FromBeginning_AsIsWrong() {
-		MovementCommand movementCommand = MovementCommand.valueOf("D");
-		List<String> bridgeMocked = List.of("U", "U");
-		CrossingDecision crossingDecision = CrossingDecision.judgingBy(movementCommand, bridgeMocked);
-
-		BridgeMap bridgeMap = BridgeMap.constructedBy(crossingDecision, movementCommand);
+		BridgeMap bridgeMap = setUp("D", 1);
 
 		List<String> actual1 = bridgeMap.getUpperBridgeDescription();
 		List<String> actual2 = bridgeMap.getUnderBridgeDescription();
@@ -49,52 +50,48 @@ class BridgeMapTest {
 
 		assertThat(actual1).isEqualTo(expected1);
 		assertThat(actual2).isEqualTo(expected2);
-
-		BridgeMap.clearBridgeDescription();
 	}
 
-	@DisplayName("BridgeMap의 리스트 구조를 검증한다 : 2회 이상, 맞는 경우")
+	@DisplayName("BridgeMap의 리스트 구조를 검증한다: 2회 이상이면서 맞는 경우")
 	@Test
 	void verify_BridgeMap_CanDrawMap_FollowingBeginning_AsIsCorrect() {
-		MovementCommand movementCommand = MovementCommand.valueOf("U");
-		List<String> bridgeMocked = List.of("U", "U");
-		CrossingDecision crossingDecision = CrossingDecision.judgingBy(movementCommand, bridgeMocked);
+		BridgeMap bridgeMap = setUp("U", 2);
 
-		BridgeMap bridgeMap1 = BridgeMap.constructedBy(crossingDecision, movementCommand); // first call
-		BridgeMap bridgeMap2 = BridgeMap.constructedBy(crossingDecision, movementCommand); // second call
-
-		List<String> actual1 = bridgeMap2.getUpperBridgeDescription();
-		List<String> actual2 = bridgeMap2.getUnderBridgeDescription();
+		List<String> actual1 = bridgeMap.getUpperBridgeDescription();
+		List<String> actual2 = bridgeMap.getUnderBridgeDescription();
 
 		List<String> expected1 = List.of("[ O ", "| O ]");
 		List<String> expected2 = List.of("[   ", "|   ]");
 
 		assertThat(actual1).isEqualTo(expected1);
 		assertThat(actual2).isEqualTo(expected2);
-
-		BridgeMap.clearBridgeDescription();
 	}
 
-	@DisplayName("BridgeMap의 리스트 구조를 검증한다 : 2회 이상, 틀린 경우")
+	@DisplayName("BridgeMap의 리스트 구조를 검증한다: 2회 이상이면서 틀린 경우")
 	@Test
 	void verify_BridgeMap_CanDrawMap_FollowingBeginning_AsIsWrong() {
-		MovementCommand movementCommand = MovementCommand.valueOf("D");
-		List<String> bridgeMocked = List.of("U", "U");
-		CrossingDecision crossingDecision = CrossingDecision.judgingBy(movementCommand, bridgeMocked);
+		BridgeMap bridgeMap = setUp("D", 2);
 
-		BridgeMap bridgeMap1 = BridgeMap.constructedBy(crossingDecision, movementCommand); // first call
-		BridgeMap bridgeMap2 = BridgeMap.constructedBy(crossingDecision, movementCommand); // second call
-
-		List<String> actual1 = bridgeMap2.getUpperBridgeDescription();
-		List<String> actual2 = bridgeMap2.getUnderBridgeDescription();
+		List<String> actual1 = bridgeMap.getUpperBridgeDescription();
+		List<String> actual2 = bridgeMap.getUnderBridgeDescription();
 
 		List<String> expected1 = List.of("[   ", "|   ]");
 		List<String> expected2 = List.of("[ X ", "| X ]");
 
 		assertThat(actual1).isEqualTo(expected1);
 		assertThat(actual2).isEqualTo(expected2);
+	}
 
-		BridgeMap.clearBridgeDescription();
+	private static BridgeMap setUp(String movementCommandMocked, int repeatedCounts) {
+		MovementCommand movementCommand = MovementCommand.valueOf(movementCommandMocked);
+		CrossingDecision crossingDecision = CrossingDecision.judgingBy(movementCommand, bridgeMocked);
+		BridgeMap bridgeMap = BridgeMap.constructedBy(crossingDecision, movementCommand);
+
+		for (int i = 1; i < repeatedCounts; i++) {
+			bridgeMap = BridgeMap.constructedBy(crossingDecision, movementCommand);
+		}
+
+		return bridgeMap;
 	}
 }
 
