231f578b34ed2e6c7442c4f699e4b72967d50a57
swonny <wonny921@gmail.com>
2022-11-21 22:02:50 +0900

    refactor(BridgeGenerationException): 여러 예외 처리에서 공통되는 부분 분리 및 상속

--- src/main/java/exceptionHandler/BridgeGenerationException.java
+++ src/main/java/exceptionHandler/BridgeGenerationException.java
@@ -1,21 +1,13 @@
 package exceptionHandler;
 
-import enumCollections.ExceptionMessage;
-import enumCollections.Side;
-
 import java.util.List;
 
-public class BridgeGenerationException {
+public class BridgeGenerationException extends InputException {
     public static void validate(List<String> movableSides) {
         movableSides.stream()
                 .forEach(side -> {
                     validateSideSelection(side);
+                    validateCharactersOnly(side);
                 });
-    }
-
-    private static void validateSideSelection(String side) {
-        if (!Side.isBridgeSavingFormat(side)) {
-            throw new IllegalArgumentException(ExceptionMessage.getMessage(ExceptionMessage.SELECT_SIDE));
-        }
     }
 }
