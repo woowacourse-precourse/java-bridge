313a1b51fc3384402e7c219bcfc6ac767162204c
josolha <josolha@nate.com>
2022-11-22 05:22:24 +0900

    fix(Validate) : 사용자 움직는 값 D,U 이외 값 예외처리

--- src/main/java/bridge/validator/Validate.java
+++ src/main/java/bridge/validator/Validate.java
@@ -2,7 +2,7 @@ package bridge.validator;
 
 public class Validate {
 
-    public static void vaildateBridgeSize(String bridgeSizeInput) {
+    public static void validateBridgeSize(String bridgeSizeInput) {
         for (int i = 0; i < bridgeSizeInput.length(); i++) {
             if (!Character.isDigit(bridgeSizeInput.charAt(i))) {
                 throw new IllegalArgumentException();
@@ -10,10 +10,15 @@ public class Validate {
         }
 
     }
-    public static void vaildateCheckRetry(String command){
+    public static void validateCheckRetry(String command){
         if(!command.equals("Q") && !command.equals("R")){
             throw new IllegalArgumentException();
         }
 
+    }
+    public static void validateCheckMove(String userMove){
+        if(!userMove.equals("U") && !userMove.equals("D")){
+            throw new IllegalArgumentException();
+        }
     }
 }
