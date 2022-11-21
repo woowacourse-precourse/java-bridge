7f92f80f9611289aed7eb3915fe4a8454c5c4fcd
kissshot1104 <kissshot.heartunderblade1104@gmail.com>
2022-11-21 22:40:51 +0900

    Refactor(ExceptionBasketTest) : 주석 삭제
    필요 없는 주석을 삭제했습니다.
    
    테스트 별로 중괄호를 넣어 가독성을 높였습니다.

--- src/test/java/bridge/ExceptionBasketTest.java
+++ src/test/java/bridge/ExceptionBasketTest.java
@@ -4,7 +4,6 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
-import static org.assertj.core.api.Assertions.assertThat;
 
 class ExceptionBasketTest {
 
@@ -12,32 +11,54 @@ class ExceptionBasketTest {
 
     @Test
     @DisplayName("1~20이외의 숫자일 경우 예외 처리")
-    void inputIsNotRangeExceptionTest(){
-        assertThatThrownBy(() ->
-                ExceptionBasket.inputIsNotRangeException("5.0"))
-                .isInstanceOf(IllegalArgumentException.class)
-                .hasMessageContaining(ERROR_MESSAGE);
-        assertThatThrownBy(() ->
-                ExceptionBasket.inputIsNotRangeException("5.3"))
-                .isInstanceOf(IllegalArgumentException.class)
-                .hasMessageContaining(ERROR_MESSAGE);
-        assertThatThrownBy(() ->
-                ExceptionBasket.inputIsNotRangeException("+5"))
-                .isInstanceOf(IllegalArgumentException.class)
-                .hasMessageContaining(ERROR_MESSAGE);
-        assertThatThrownBy(() ->
-                ExceptionBasket.inputIsNotRangeException("-5"))
-                .isInstanceOf(IllegalArgumentException.class)
-                .hasMessageContaining(ERROR_MESSAGE);
-        assertThatThrownBy(() ->
-                ExceptionBasket.inputIsNotRangeException("05"))
-                .isInstanceOf(IllegalArgumentException.class)
-                .hasMessageContaining(ERROR_MESSAGE);
+    void inputIsNotRangeExceptionTest() {
+
+        //소숫점으로 값을 넣을 경우(에러)
+        {
+            assertThatThrownBy(() ->
+                    ExceptionBasket.inputIsNotRangeException("5.0"))
+                    .isInstanceOf(IllegalArgumentException.class)
+                    .hasMessageContaining(ERROR_MESSAGE);
+        }
+
+        //소숫점으로 값을 넣을 경우(에러)
+        {
+            assertThatThrownBy(() ->
+                    ExceptionBasket.inputIsNotRangeException("5.3"))
+                    .isInstanceOf(IllegalArgumentException.class)
+                    .hasMessageContaining(ERROR_MESSAGE);
+        }
+
+        //앞에 기호를 넣을 경우(에러)
+        {
+            assertThatThrownBy(() ->
+                    ExceptionBasket.inputIsNotRangeException("+5"))
+                    .isInstanceOf(IllegalArgumentException.class)
+                    .hasMessageContaining(ERROR_MESSAGE);
+        }
+
+        //앞에 기호를 넣을 경우(에러)
+        {
+            assertThatThrownBy(() ->
+                    ExceptionBasket.inputIsNotRangeException("-5"))
+                    .isInstanceOf(IllegalArgumentException.class)
+                    .hasMessageContaining(ERROR_MESSAGE);
+        }
+
+        //앞에 0이란 숫자가 있을 경우(에러)
+        {
+            assertThatThrownBy(() ->
+                    ExceptionBasket.inputIsNotRangeException("05"))
+                    .isInstanceOf(IllegalArgumentException.class)
+                    .hasMessageContaining(ERROR_MESSAGE);
+        }
     }
 
     @Test
     @DisplayName("허용된 숫자열이 아닐 시 예외 처리")
-    void inputIsNotNumberExceptionTest(){
+    void inputIsNotNumberExceptionTest() {
+        
+        //숫자가 아닐 시 예외 처리
         assertThatThrownBy(() ->
                 ExceptionBasket.inputIsNotNumberException("a"))
                 .isInstanceOf(IllegalArgumentException.class)
@@ -46,58 +67,60 @@ class ExceptionBasketTest {
 
     @Test
     @DisplayName("게임 플레이중 잘못된 U, D이외를 누를경우 예외 처리")
-    void invalidGameKeyTest(){
-        assertThatThrownBy(() ->
-                ExceptionBasket.invalidGameKey("A"))
-                .isInstanceOf(IllegalArgumentException.class)
-                .hasMessageContaining(ERROR_MESSAGE);
+    void invalidGameKeyTest() {
 
-        assertThatThrownBy(() ->
-                ExceptionBasket.invalidGameKey("UU"))
-                .isInstanceOf(IllegalArgumentException.class)
-                .hasMessageContaining(ERROR_MESSAGE);
+        //U나 D 이외의 문자를 넣을 경우(에러)
+        {
+            assertThatThrownBy(() ->
+                    ExceptionBasket.invalidGameKey("A"))
+                    .isInstanceOf(IllegalArgumentException.class)
+                    .hasMessageContaining(ERROR_MESSAGE);
+        }
 
-        assertThatThrownBy(() ->
-                ExceptionBasket.invalidGameKey("DD"))
-                .isInstanceOf(IllegalArgumentException.class)
-                .hasMessageContaining(ERROR_MESSAGE);
+        //U를 두번 넣을 경우(에러)
+        {
+            assertThatThrownBy(() ->
+                    ExceptionBasket.invalidGameKey("UU"))
+                    .isInstanceOf(IllegalArgumentException.class)
+                    .hasMessageContaining(ERROR_MESSAGE);
+        }
+
+        //D를 두번 넣을 경우(에러)
+        {
+            assertThatThrownBy(() ->
+                    ExceptionBasket.invalidGameKey("DD"))
+                    .isInstanceOf(IllegalArgumentException.class)
+                    .hasMessageContaining(ERROR_MESSAGE);
+        }
     }
 
     @Test
     @DisplayName("다시 시작할지 말지 R, Q이외를 누를경우 예외 처리")
-    void invalidRetryRequestInputKeyTest(){
-        assertThatThrownBy(() ->
-                ExceptionBasket.invalidRetryRequestInputKey("A"))
-                .isInstanceOf(IllegalArgumentException.class)
-                .hasMessageContaining(ERROR_MESSAGE);
+    void invalidRetryRequestInputKeyTest() {
 
-        assertThatThrownBy(() ->
-                ExceptionBasket.invalidRetryRequestInputKey("RR"))
-                .isInstanceOf(IllegalArgumentException.class)
-                .hasMessageContaining(ERROR_MESSAGE);
+        //R이나 Q 이외의 문자를 넣을 경우(에러)
+        {
+            assertThatThrownBy(() ->
+                    ExceptionBasket.invalidRetryRequestInputKey("A"))
+                    .isInstanceOf(IllegalArgumentException.class)
+                    .hasMessageContaining(ERROR_MESSAGE);
+        }
 
-        assertThatThrownBy(() ->
-                ExceptionBasket.invalidRetryRequestInputKey("QQ"))
-                .isInstanceOf(IllegalArgumentException.class)
-                .hasMessageContaining(ERROR_MESSAGE);
+        //R을 두번 넣을 경우(에러)
+        {
+            assertThatThrownBy(() ->
+                    ExceptionBasket.invalidRetryRequestInputKey("RR"))
+                    .isInstanceOf(IllegalArgumentException.class)
+                    .hasMessageContaining(ERROR_MESSAGE);
+        }
+
+        //Q를 두번 넣을 경우(에러)
+        {
+            assertThatThrownBy(() ->
+                    ExceptionBasket.invalidRetryRequestInputKey("QQ"))
+                    .isInstanceOf(IllegalArgumentException.class)
+                    .hasMessageContaining(ERROR_MESSAGE);
+        }
     }
-    /*@Test
-    @DisplayName("허용되지 않은 숫자열 일 시 예외 처리")
-    void invalidNumbericTest() {
-        assertThatThrownBy(() ->
-                ExceptionBasket.invalidNumberic("+5"))
-                .isInstanceOf(IllegalArgumentException.class)
-                .hasMessageContaining(ERROR_MESSAGE);
-
-        assertThatThrownBy(() ->
-                ExceptionBasket.invalidNumberic("-5"))
-                .isInstanceOf(IllegalArgumentException.class)
-                .hasMessageContaining(ERROR_MESSAGE);
-
-        assertThatThrownBy(() ->
-                ExceptionBasket.invalidNumberic("05"))
-                .isInstanceOf(IllegalArgumentException.class)
-                .hasMessageContaining(ERROR_MESSAGE);
-    }*/
 
 }
