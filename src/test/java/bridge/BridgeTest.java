1154d1a17c2f51a95f08e21ce5dbd9398bb19ecd
heesang <dlagmltkd12@naver.com>
2022-11-22 06:34:17 +0900

    test: BridgeTest 중복 테스트 제거

--- src/test/java/bridge/BridgeTest.java
+++ src/test/java/bridge/BridgeTest.java
@@ -4,9 +4,6 @@ import bridge.constant.BridgeMark;
 import bridge.constant.GameStatus;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.Arguments;
-import org.junit.jupiter.params.provider.MethodSource;
 
 import java.util.List;
 
@@ -14,23 +11,6 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class BridgeTest {
-
-    @DisplayName("U, D가 리스트로 주어질 때 enum 리스트로 반환하는 기능")
-    @MethodSource("createTestBridgeStatus")
-    @ParameterizedTest
-    void mapToBridgeMarks(int index, BridgeMark answer) {
-        List<BridgeMark> bridgeMarks = BridgeMark.of(List.of("U", "D", "U", "D"));
-        assertThat(bridgeMarks.get(index)).isEqualTo(answer);
-    }
-
-    private static List<Arguments> createTestBridgeStatus() {
-        return List.of(
-                Arguments.of(0, BridgeMark.UP),
-                Arguments.of(1, BridgeMark.DOWN),
-                Arguments.of(2, BridgeMark.UP),
-                Arguments.of(3, BridgeMark.DOWN)
-        );
-    }
 
     @DisplayName("다리를 한 라운드 건널 때 게임 계속 가능 결과 반환")
     @Test
