78e1ea29356da60a8272218125bcfb4e8ce0ea26
Your Name <you@example.com>
2022-11-21 17:42:50 +0900

    refactor(bridge) : 이동한 방향에 따라 이동 판단 문자 enum 분리

--- src/main/java/bridge/domain/BridgeGame.java
+++ src/main/java/bridge/domain/BridgeGame.java
@@ -1,5 +1,6 @@
 package bridge.domain;
 
+import static bridge.domain.BridgeMove.*;
 import static bridge.domain.BridgeMoveJudgment.*;
 
 import java.util.ArrayList;
@@ -9,6 +10,9 @@ import java.util.List;
  * 다리 건너기 게임을 관리하는 클래스
  */
 public class BridgeGame {
+
+    private static final String UPPER = "U";
+    private static final String LOWER = "D";
 
     private final List<BridgeMoveJudgment> moveResult = new ArrayList<>();
     private int tryCount = 0;
@@ -20,29 +24,12 @@ public class BridgeGame {
      * 이동을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
     public List<BridgeMoveJudgment> move(List<String> bridge, String movingSpace) {
-        if (bridge.get(tryCount++).equals(movingSpace)) {
-            if (movingSpace.equals("U")) {
-                moveResult.add(CORRECT);
-                moveResult.add(EMPTY);
-                return moveResult;
-            }
-            if (movingSpace.equals("D")) {
-                moveResult.add(EMPTY);
-                moveResult.add(CORRECT);
-                return moveResult;
-            }
-        }
-        if (movingSpace.equals("U")) {
-            moveResult.add(WRONG);
-            moveResult.add(EMPTY);
-            return moveResult;
-        }
-        if (movingSpace.equals("D")) {
-            moveResult.add(EMPTY);
-            moveResult.add(WRONG);
-            return moveResult;
-        }
-        throw new IllegalStateException("[ERROR] 메소드를 잘못 사용하셨습니다.");
+        String bridgeSpace = bridge.get(tryCount);
+        tryCount = tryCount + 1;
+        List<BridgeMoveJudgment> bridgeMoveJudgments = compareMovingSpace(movingSpace, bridgeSpace);
+        moveResult.add(bridgeMoveJudgments.get(0));
+        moveResult.add(bridgeMoveJudgments.get(1));
+        return moveResult;
     }
 
     /**
@@ -88,5 +75,44 @@ public class BridgeGame {
      */
     public int checkRetryCount() {
         return retryCount;
+    }
+
+    /**
+     * 사용자 이동한 칸과 다리 칸 비교해서 정해진 형식 리턴 메서드
+     */
+    private List<BridgeMoveJudgment> compareMovingSpace(String movingSpace, String bridgeSpace) {
+        if (movingSpace.equals(UPPER)) {
+            boolean correctSpace = isCorrectSpace(movingSpace, bridgeSpace, UPPER);
+            return choiceCorrectOrWrong(correctSpace, UPPER_CORRECT, UPPER_WRONG);
+        }
+        if (movingSpace.equals(LOWER)) {
+            boolean correctSpace = isCorrectSpace(movingSpace, bridgeSpace, LOWER);
+            return choiceCorrectOrWrong(correctSpace, LOWER_CORRECT, LOWER_WRONG);
+        }
+        throw new IllegalStateException("[ERROR] 메소드를 잘못 사용하셨습니다.");
+    }
+
+    /**
+     * 이동한 칸이 맞는 칸인지 확인 메서드
+     */
+    private boolean isCorrectSpace(String movingSpace, String bridgeSpace, String move) {
+        boolean correctSpace = false;
+        if (movingSpace.equals(move)) {
+            if (bridgeSpace.equals(movingSpace)) {
+                correctSpace = true;
+            }
+        }
+        return correctSpace;
+    }
+
+    /**
+     * 이동한 칸이 맞거나 틀리면 정해진 형식 리턴 메서드
+     */
+    private List<BridgeMoveJudgment> choiceCorrectOrWrong(
+            boolean correctSpace, BridgeMove correct, BridgeMove wrong) {
+        if (correctSpace) {
+            return correct.moveJudgments;
+        }
+        return wrong.moveJudgments;
     }
 }
---
+++ src/main/java/bridge/domain/BridgeMove.java
@@ -0,0 +1,19 @@
+package bridge.domain;
+
+import java.util.List;
+
+import static bridge.domain.BridgeMoveJudgment.*;
+
+public enum BridgeMove {
+
+    UPPER_CORRECT(List.of(CORRECT, EMPTY)),
+    UPPER_WRONG(List.of(WRONG, EMPTY)),
+    LOWER_CORRECT(List.of(EMPTY, CORRECT)),
+    LOWER_WRONG(List.of(EMPTY, WRONG));
+
+    List<BridgeMoveJudgment> moveJudgments;
+
+    BridgeMove(List<BridgeMoveJudgment> moveJudgments) {
+        this.moveJudgments = moveJudgments;
+    }
+}
