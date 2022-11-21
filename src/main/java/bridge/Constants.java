65ed0bf4946ba1c767a078b96248a45c945fe757
hantae <hantae96@naver.com>
2022-11-21 10:39:21 +0900

    Feat : 다리 길이를 입력 받는 기능 추가

---
+++ src/main/java/bridge/Constants.java
@@ -0,0 +1,7 @@
+package bridge;
+
+public final class Constants {
+
+    public static final String START_INFO = "다리 건너기 게임을 시작합니다.";
+    public static final String INPUT_LEGHTH_INFO = "다리의 길이를 입력해주세요.";
+}
--- src/main/java/bridge/InputView.java
+++ src/main/java/bridge/InputView.java
@@ -1,15 +1,26 @@
 package bridge;
-
+import camp.nextstep.edu.missionutils.Console;
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
  */
-public class InputView {
 
+public class InputView {
     /**
      * 다리의 길이를 입력받는다.
      */
+    private int bridgeLength = 0;
+    private char MovingCommand ;
+
+    InputView(){
+        System.out.println(Constants.START_INFO);
+        System.out.println();
+    }
+
     public int readBridgeSize() {
-        return 0;
+        System.out.println(Constants.INPUT_LEGHTH_INFO);
+        bridgeLength= Integer.valueOf(Console.readLine());
+        System.out.println(bridgeLength);
+        return bridgeLength;
     }
 
     /**
