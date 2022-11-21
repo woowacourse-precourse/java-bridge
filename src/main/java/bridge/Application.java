f32b7b63749b4839ccbadf5a18e79bcc483b41b2
chordpli <chordplaylist@gmail.com>
2022-11-21 21:42:51 +0900

    refactor: 메서드 분리, 클래스 이동

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -1,5 +1,6 @@
 package bridge;
 
+import java.net.http.HttpRequest;
 import java.util.List;
 
 public class Application {
@@ -12,7 +13,7 @@ public class Application {
 
     public void run() {
         // output view
-        System.out.println("다리 건너기 게임을 시작합니다.");
+        outputView.startMessage();
         // 자동으로 생성할 다리 길이를 입력 받는다. --> input view
         int bridgeSize = 0;
         while (bridgeSize == 0) {
@@ -24,8 +25,7 @@ public class Application {
         // U 또는 D 입력 받기 --> input view
         while (game.equals("R")) {
             // 이동할 칸을 선택해주세요 --> output view
-            System.out.println("이동할 칸을 선택해주세요");
-            String move = inputView.readMoving();
+            String move = inputView.getMove();
             // 다리를 출력합니다 --> output view
             bridgeGame.move(bridge, move);
             if (bridge.size() > bridgeSize) {
@@ -33,26 +33,16 @@ public class Application {
                 game = inputView.readGameCommand();
                 // R 또는 Q 입력 --> input view
                 restartAfterFailure(bridge);
-                endAfterFailure();
+                outputView.endAfterFailure(bridgeGame, game, count);
             }
-            // 마지막 칸 도착시
-            // 최종 게임 결과 -- > outputView
-            // 다리 출력
-            // 게임 성공 여부 출력
-            // 시도 횟수 출력
+            // 마지막 칸 도착시,최종 게임 결과
+            // 다리 출력, 게임 성공 여부 출력,시도 횟수 출력
             game = outputView.printResult(bridgeGame, bridgeSize, count, game);
         }
     }
 
 
-    private void endAfterFailure() {
-        if (game.equals("Q")) {
-            System.out.println("최종 게임 결과");
-            outputView.printMap(bridgeGame.getUpBridge(), bridgeGame.getDownBridge());
-            System.out.println("게임 성공 여부: 실패");
-            System.out.println("총 시도한 횟수: " + count);
-        }
-    }
+
 
     private void restartAfterFailure(List<String> bridge) {
         if (game.equals("R")) {
