6cca7eba4174aad580ddb3a14dcfbc920cdb06e2
asas6978 <asas6978@naver.com>
2022-11-21 11:48:15 +0900

    refactor: 상수로 변경

--- src/main/java/bridge/util/ExceptionMessage.java
+++ src/main/java/bridge/util/ExceptionMessage.java
@@ -2,8 +2,8 @@ package bridge.util;
 
 public class ExceptionMessage {
     public static final String ERROR_MESSAGE = "[ERROR] ";
-    public static String CANNOT_CONVERT_TO_NUMBER = "입력 값을 숫자로 변환할 수 없습니다.";
-    public static String BRIDGE_SIZE_OUT_OF_RANGE = "다리 길이가 범위 내 숫자가 아닙니다.";
-    public static String INVALID_DIRECTION = "유효하지 않은 방향 값입니다.";
-    public static String INVALID_GAME_COMMAND = "유효하지 않은 명령어 입력 값입니다.";
+    public static final String CANNOT_CONVERT_TO_NUMBER = "입력 값을 숫자로 변환할 수 없습니다.";
+    public static final String BRIDGE_SIZE_OUT_OF_RANGE = "다리 길이가 범위 내 숫자가 아닙니다.";
+    public static final String INVALID_DIRECTION = "유효하지 않은 방향 값입니다.";
+    public static final String INVALID_GAME_COMMAND = "유효하지 않은 명령어 입력 값입니다.";
 }
