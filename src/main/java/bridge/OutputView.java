f682cc18d21dfd90a7b354537e8c40278ffcc240
yoon-yoo-tak <dbsdbxkrzz@naver.com>
2022-11-22 01:11:49 +0900

    feat: 게임 시작 문구 출력 메서드 추가

--- src/main/java/bridge/OutputView.java
+++ src/main/java/bridge/OutputView.java
@@ -13,6 +13,7 @@ public class OutputView {
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
     public void printMap() {
+        
     }
 
     /**
@@ -21,6 +22,10 @@ public class OutputView {
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
     public void printResult() {
+    }
+
+    public static void startGame() {
+        System.out.println(Message.GAME_START.getMessage());
     }
 
     public static void printInputBridgeLength() {
