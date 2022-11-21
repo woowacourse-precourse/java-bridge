fdfeb186e36f9a465af7187c9170976b973191bd
poter0501 <poter0501@gmail.com>
2022-11-21 18:16:30 +0900

    feat(InputView): 게임 재시작 여부를 입력 받는 기능 구현

--- src/main/java/bridge/InputView.java
+++ src/main/java/bridge/InputView.java
@@ -35,7 +35,10 @@ public class InputView {
      * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
      */
     public String readGameCommand() {
-        return null;
+        System.out.println("게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)");
+        String input = Console.readLine();
+        isPermittedCommand(input, PERMITTED_RETRY_COMMAND);
+        return input;
     }
 
     public int tryParseInt(String input){
