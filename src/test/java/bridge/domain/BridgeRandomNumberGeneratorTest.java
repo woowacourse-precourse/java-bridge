900e1fa2576fe9f25f548ce3a04f488ef02b05b1
yg0585 <yg0585@naver.com>
2022-11-21 16:36:29 +0900

    style: convention 적용

--- src/test/java/bridge/domain/BridgeRandomNumberGeneratorTest.java
+++ src/test/java/bridge/domain/BridgeRandomNumberGeneratorTest.java
@@ -7,7 +7,7 @@ import org.junit.jupiter.api.RepeatedTest;
 public class BridgeRandomNumberGeneratorTest {
 
     @RepeatedTest(100)
-    void 랜덤_숫자_0과_1사이_확인(){
+    void 랜덤_숫자_0과_1사이_확인() {
         //given
         BridgeRandomNumberGenerator bridgeRandomNumberGenerator = new BridgeRandomNumberGenerator();
 
