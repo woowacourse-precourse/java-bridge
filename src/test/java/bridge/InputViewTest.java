92b9e52a1b2b4e15998a5cfa0da79324b32d2015
Ryokuman <ryokuman@naver.com>
2022-11-21 19:37:26 +0900

    feature ( test ) : add InputViewTest

---
+++ src/test/java/bridge/InputViewTest.java
@@ -0,0 +1,11 @@
+package bridge;
+
+import org.junit.jupiter.api.Test;
+
+public class InputViewTest {
+    @Test
+    void readBridgeSizeTest(){
+        InputView inputView = new InputView();
+        inputView.readBridgeSize();
+    }
+}
