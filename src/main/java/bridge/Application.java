7c14d264c963e115101f327ea62a732476ea7817
bjk1649 <qhwns1649@naver.com>
2022-11-22 01:46:45 +0900

    feat: Application의 Main이 게임의 시작지점이 되도록 게임 시작 기능 구현

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -1,8 +1,16 @@
 package bridge;
 
+import bridge.bo.Bridge;
+import bridge.controller.BridgeGameController;
+import bridge.util.BridgeMaker;
+import bridge.util.BridgeRandomNumberGenerator;
+import bridge.view.InputView;
+
 public class Application {
 
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        int size = new InputView().readBridgeSize();
+        Bridge bridge = new Bridge(new BridgeMaker(new BridgeRandomNumberGenerator()).makeBridge(size));
+        new BridgeGameController(bridge).doGame();
     }
 }
