24657a5c30da33043cc334d8dbf40ae2a8b7c554
gwj0421 <gwj0421@gmail.com>
2022-11-21 19:44:18 +0900

    fix: 중복 메서드 리팩토링 진행시 오류 발생하여 이를 해결
    비슷한 메서드였지만 반환값이 차이가 있어 오류가 발생하였고 이를 수정하여 해결하였다.

--- src/main/java/bridge/BridgeGame.java
+++ src/main/java/bridge/BridgeGame.java
@@ -8,7 +8,7 @@ import java.util.List;
 public class BridgeGame {
 
     public boolean checkSame(List<String> RandomBridgeMap, int idx, String playrerStep) {
-        if (!RandomBridgeMap.get(idx).equals(playrerStep)) {
+        if (RandomBridgeMap.get(idx).equals(playrerStep)) {
             return true;
         }
         return false;
