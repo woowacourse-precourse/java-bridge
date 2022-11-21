d947fbe040b8858631939af5d9064276471b8104
jaehyeon <jhjh0022@naver.com>
2022-11-22 02:26:42 +0900

    feat(Application): 게임 실행 구현

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -1,8 +1,15 @@
 package bridge;
 
+import bridge.controller.Game;
+import bridge.view.TypeChange;
+import bridge.view.Valification;
+import bridge.view.InputView;
+import bridge.view.OutputView;
+
 public class Application {
 
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        Game game = new Game(new InputView(new Valification(), new TypeChange()), new OutputView());
+        game.play();
     }
 }
