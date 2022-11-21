865dd011f66c6628ce05de79a936552ab5ccc2d9
Sangwoo Shin <sangwoonoel@naver.com>
2022-11-21 20:40:41 +0900

    feat(ErrorMessage): 에러 메세지를 관리하는 enum 생성

---
+++ src/main/java/bridge/ErrorMessage.java
@@ -0,0 +1,18 @@
+package bridge;
+
+public enum ErrorMessage {
+
+    INVALID_BRIDGE_SIZE_ERROR("[ERROR] 다리 길이는 3부터 20 사이의 숫자여야 합니다."),
+    INVALID_MOVING_ERROR("[ERROR] U 또는 D 둘 중 한 값을 입력해주세요."),
+    INVALID_GAME_COMMAND_ERROR("[ERROR] R또는 Q 둘 중 한 값을 입력해주세요.");
+
+    private final String message;
+
+    ErrorMessage(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+}
