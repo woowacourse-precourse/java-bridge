1a25b9cff4dadd2a91388de4c9992b61ed0e6478
tjddnr7760 <tjddnr7760@naver.com>
2022-11-22 02:02:19 +0900

    refactor : 함수 더 작게 세분화

--- src/main/java/bridge/InputView.java
+++ src/main/java/bridge/InputView.java
@@ -14,6 +14,7 @@ public class InputView {
         } catch (IllegalArgumentException e) {
             throw new IllegalArgumentException("[ERROR]");
         }
+        System.out.println();
 
         return bridgelength;
     }
@@ -26,7 +27,7 @@ public class InputView {
     }
 
     //사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
-    public String readGameCommand() {
+    public String readExitCommand() {
         System.out.println("게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)");
         String exittype = Console.readLine();
         return exittype;
