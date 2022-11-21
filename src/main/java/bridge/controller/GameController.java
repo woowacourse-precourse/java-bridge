af34f70610c6022f8dc3629a8cc681276d1381bd
sooyoungh <sooyoung.h8@gmail.com>
2022-11-21 22:39:50 +0900

    ✨ feat(GameController): 랜덤 다리 생성하는 기능 구현

---
+++ src/main/java/bridge/controller/GameController.java
@@ -0,0 +1,20 @@
+package bridge.controller;
+
+import bridge.BridgeMaker;
+import bridge.BridgeNumberGenerator;
+import bridge.BridgeRandomNumberGenerator;
+import bridge.model.BridgeGame;
+
+import java.util.List;
+
+public class GameController {
+
+    private BridgeGame bridgeGame = new BridgeGame();
+
+    private void createBridge(int bridgeSize) {
+        BridgeNumberGenerator numberGenerator = new BridgeRandomNumberGenerator();
+        BridgeMaker bridgeMaker = new BridgeMaker(numberGenerator);
+        List<String> randomBridge = bridgeMaker.makeBridge(bridgeSize);
+        bridgeGame.createBridge(randomBridge);
+    }
+}
