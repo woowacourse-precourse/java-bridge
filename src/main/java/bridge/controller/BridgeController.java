e80e66a98935c38aa8df3f910250b91e088d928d
olrlobt <317tmdgjs@naver.com>
2022-11-21 15:53:48 +0900

    feat: 재시작 하거나 종료 입력을 받으면, 동작 실행

--- src/main/java/bridge/controller/BridgeController.java
+++ src/main/java/bridge/controller/BridgeController.java
@@ -15,17 +15,18 @@ public class BridgeController {
         outputView.printRequestBridgeSize();
         int size = inputView.readBridgeSize();
         Bridge bridge = new Bridge(size);
-
         BridgeGame bridgeGame = new BridgeGame();
 
-        boolean passable = false;
-        do {
-            outputView.printRequestMove();
-            passable = bridgeGame.move(inputView.readMoving(), bridge);
-            outputView.printMap(bridgeGame.getMark(), passable);
-        } while (passable);
+        do{
+            bridgeGame.retry();
+            boolean passable = false;
+            do {
+                outputView.printRequestMove();
+                passable = bridgeGame.move(inputView.readMoving(), bridge);
+                outputView.printMap(bridgeGame.getMark(), passable);
+            } while (passable);
 
-        outputView.printRequestGameCommand();
-        inputView.readGameCommand();
+            outputView.printRequestGameCommand();
+        }while (inputView.readGameCommand());
     }
 }
--- src/main/java/bridge/view/InputView.java
+++ src/main/java/bridge/view/InputView.java
@@ -29,11 +29,14 @@ public class InputView {
     /**
      * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
      */
-    public String readGameCommand() {
+    public boolean readGameCommand() {
         String command=Console.readLine();
-        if(!command.equals("R") && !command.equals("Q") ){
-            throw new IllegalArgumentException();
+
+        if(command.equals("R")){
+            return true;
+        }else if(command.equals("Q")){
+            return false;
         }
-        return command;
+        throw new IllegalArgumentException();
     }
 }
