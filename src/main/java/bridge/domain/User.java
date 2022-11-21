3c6de2e0440d0a756fd2bd453abb67e7e8a74a70
jungwoo <rlawjddn103@naver.com>
2022-11-21 14:05:31 +0900

    feat(User): 현재 status가 DOING이 아니면 마지막 결과를 출력하고, False 반환하는 기능 추가

--- src/main/java/bridge/domain/User.java
+++ src/main/java/bridge/domain/User.java
@@ -8,7 +8,7 @@ import java.util.List;
 
 public class User {
 
-    private final List<String> route = new ArrayList<>();
+    private List<String> route = new ArrayList<>();
     private int attemptNum = 1;
     private int location = -1;
     private Status status = Status.DOING;
@@ -16,30 +16,27 @@ public class User {
     private final InputView inputView = new InputView();
     private final OutputView outputView = new OutputView();
 
-    User() {
+    public User() {
         this.bridge = new Bridge(inputView.readBridgeSize(), new BridgeRandomNumberGenerator());
     }
 
-    public boolean move(){
-        String goal = inputView.readMoving();
-        this.location += 1;
 
-    }
-
-    private boolean isCrossBridge() {
+    private void isCrossBridge() {
         if (location == bridge.size() - 1) {
             status = Status.SUCCESS_END;
+        }
+    }
+
+    public boolean isEnd() {
+        if (status == Status.SUCCESS_END) {
+            outputView.printResult(route, true, attemptNum);
+            return true;
+        }
+        if (status == Status.FAILED_END) {
+            outputView.printResult(route, false, attemptNum);
             return true;
         }
         return false;
-    }
-
-    private boolean moveNext(String goal) {
-        this.route.add(goal);
-        if (isCrossBridge()) {
-            return false;
-        }
-        return true;
     }
 
 }
