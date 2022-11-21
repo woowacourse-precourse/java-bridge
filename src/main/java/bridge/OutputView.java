414ed74ae05c099d3c24fe1fa60b4d77b590f179
c4fiber <qudcjf153@gmail.com>
2022-11-22 01:31:15 +0900

    feat(OutputView): 게임을 시도한 횟수를 출력하는 기능 작성

--- src/main/java/bridge/OutputView.java
+++ src/main/java/bridge/OutputView.java
@@ -30,7 +30,12 @@ public class OutputView {
      * <p>
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void printResult() {
+    public void printResult(boolean didWin, int tried) {
+        String gameResult = "실패";
+        if (didWin) gameResult = "성공";
+
+        System.out.println("게임 성공 여부: " + gameResult);
+        System.out.println("총 시도한 횟수: " + tried);
     }
 
     /**
