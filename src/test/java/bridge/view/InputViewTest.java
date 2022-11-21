7e6d99b1b61c4ed5c2b3d911874d9ad2b3e67c60
김수민(Sumin Kim) <sumin.kim.develop@gmail.com>
2022-11-22 01:58:22 +0900

    test(InputView): 다리 길이 입력 테스트 수정

--- src/test/java/bridge/view/InputViewTest.java
+++ src/test/java/bridge/view/InputViewTest.java
@@ -3,32 +3,37 @@ package bridge.view;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
-import java.io.ByteArrayInputStream;
+import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class InputViewTest {
 
     @ParameterizedTest
     @ValueSource(strings = {"1", "-2", "30", "32a"})
-    void bridgeSize(String input) {
+    void bridgeSizeException(String input) {
         try {
             Method method = InputView.class.getDeclaredMethod("bridgeSize", String.class);
             method.setAccessible(true);
-            assertThatThrownBy(() -> method.invoke(input))
-                    .isInstanceOf(IllegalArgumentException.class);
-        } catch (NoSuchMethodException e) {
+            method.invoke(new InputView(), input);
+        } catch (NoSuchMethodException | IllegalAccessException e) {
             throw new RuntimeException(e);
+        } catch (InvocationTargetException e) {
+            assertThat(e.getCause() instanceof IllegalArgumentException).isTrue();
         }
     }
 
     @ParameterizedTest
     @ValueSource(strings = {"3", "10", "20"})
-    void inputBridgeSize(String input) {
-        System.setIn(new ByteArrayInputStream(input.getBytes()));
-        int result = Integer.parseInt(input);
-        assertThat(result).isEqualTo(new InputView().readBridgeSize());
+    void bridgeSize(String input) {
+        try {
+            Method method = InputView.class.getDeclaredMethod("bridgeSize", String.class);
+            method.setAccessible(true);
+            int result = (int) method.invoke(new InputView(), input);
+            assertThat(result).isEqualTo(Integer.parseInt(input));
+        } catch (NoSuchMethodException | IllegalAccessException | InvocationTargetException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
