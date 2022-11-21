36d83c690d63a87a05c046f3f634b23dd0259aca
cuttlefish55 <funade7@gmail.com>
2022-11-21 11:50:41 +0900

    feat: Add the class about bridge data

---
+++ src/main/java/data/BridgeData.java
@@ -0,0 +1,10 @@
+package data;
+
+public class BridgeData {
+    public static final String UP = "U";
+    public static final String DOWN = "D";
+    public static final String RESTART = "R";
+    public static final String QUIT = "Q";
+    public static final int MINIMUM_LENGTH = 3;
+    public static final int MAXIMUM_LENGTH = 20;
+}
