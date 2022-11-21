a3a5aad346484cd26d77b8b7f67d759369376cdf
Devtist <yt930310@snu.ac.kr>
2022-11-22 05:47:15 +0900

    test: MoveResult 클래스에 대한 테스트코드 작성

--- src/test/java/bridge/MoveResultTest.java
+++ src/test/java/bridge/MoveResultTest.java
@@ -12,14 +12,44 @@ class MoveResultTest {
 	@DisplayName("모두 옳은 칸을 고른 다리의 상태를 정해진 형식대로 만든다.")
 	@Test
 	void renderSuccess() {
+		//given
 		MoveResult moveResult = new MoveResult(List.of("U","D","D","U"), List.of("U", "D", "D"));
-		assertThat(moveResult.getMap()).isEqualTo("[ O |   |   | O ]\n[   | O | O |   ]");
+		//when, then
+		assertThat(moveResult.getMap()).isEqualTo("[ O |   |   ]\n[   | O | O ]");
 	}
 
 	@DisplayName("틀린 칸을 고른 다리의 상태를 정해진 형식대로 만든다.")
 	@Test
 	void renderFail() {
+		//given
 		MoveResult moveResult = new MoveResult(List.of("U","D","D","U"), List.of("U","D","D","D"));
+		//when, then
 		assertThat(moveResult.getMap()).isEqualTo("[ O |   |   |   ]\n[   | O | O | X ]");
+	}
+	@DisplayName("옳은 칸을 고른 횟수를 구한다.")
+	@Test
+	void getNumberOfCorrect() {
+		//given
+		MoveResult moveResult = new MoveResult(List.of("U","D","D","U"), List.of("U","D","D","D"));
+		//when, then
+		assertThat(moveResult.getNumberOfCorrect()).isEqualTo(3);
+	}
+
+	@DisplayName("성공하지 못한 경우 성공 여부를 구한다.")
+	@Test
+	void isSuccessNo() {
+		//given
+		MoveResult moveResult = new MoveResult(List.of("U","D","D","U"), List.of("U","D","D","D"));
+		//when, then
+		assertThat(moveResult.isSuccess()).isEqualTo(false);
+	}
+
+	@DisplayName("성공한 경우 성공 여부를 구한다.")
+	@Test
+	void isSuccessYes() {
+		//given
+		MoveResult moveResult = new MoveResult(List.of("U","D","D","U"), List.of("U","D","D","U"));
+		//when, then
+		assertThat(moveResult.isSuccess()).isEqualTo(true);
 	}
 }
