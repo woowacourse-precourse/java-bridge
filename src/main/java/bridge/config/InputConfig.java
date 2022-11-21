499f7a102c676ba42df8f05d3669b510be9a7057
jhsseonn <hosu0125@gmail.com>
2022-11-21 15:17:54 +0900

    feat: input 구문 추가

---
+++ src/main/java/bridge/config/InputConfig.java
@@ -0,0 +1,11 @@
+package bridge.config;
+
+public class InputConfig {
+    public static final String START_BRIDGE_GAME="다리 건너기 게임을 시작합니다.";
+    public static final String INPUT_BRIDGE_LENGTH="다리 길이를 입력해주세요.";
+    public static final String SELECT_BRIDGE="이동할 칸을 선택해주세요. (위: U, 아래: D)";
+    public static final String GAME_RETRY="게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)";
+    public static final String BRIDGE_RESULT="최종 게임 결과";
+    public static final String BRIDGE_SUCCESS_FAIL="게임 성공 여부: ";
+    public static final String TRY_NUMBER="총 시도한 횟수: ";
+}
