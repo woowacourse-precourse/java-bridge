ea622f89a7409ba2b2dbcccbb6c23376824a8392
ttaehee <ttaehee1105@gmail.com>
2022-11-21 15:03:25 +0900

    feat : 게임 종료여부 입력 기능 구현

--- src/main/java/bridge/io/InputView.java
+++ src/main/java/bridge/io/InputView.java
@@ -25,6 +25,6 @@ public class InputView {
      * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
      */
     public String readGameCommand() {
-        return null;
+        return Console.readLine();
     }
 }
