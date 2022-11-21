3593efcb38b8db8aadd14bf2a912e78535cd8b71
jh868 <2weeks9947@naver.com>
2022-11-22 00:33:06 +0900

    refactor: 게임 진행 메소드 분리

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -1,9 +1,14 @@
 package bridge;
 
 public class Application {
-    static BridgeMaker bridgeMaker = new BridgeMaker(new BridgeRandomNumberGenerator());
     public static void main(String[] args) {
+        BridgeMaker bridgeMaker = new BridgeMaker(new BridgeRandomNumberGenerator());
         BridgeData bridgeData = new BridgeData(bridgeMaker.makeBridge(InputView.readBridgeSize()),0);
+        startGame(bridgeData);
+        OutputView.printResult(bridgeData, BridgeGame.gameStatus, BridgeGame.gameCount);
+    }
+
+    private static void startGame(BridgeData bridgeData){
         while (bridgeData.getBridge().size() > bridgeData.getCurrentPosition() && BridgeGame.gameStatus){
             if (BridgeGame.move(bridgeData, InputView.readMoving())){
                 OutputView.printMap(bridgeData, true);
@@ -12,6 +17,5 @@ public class Application {
             OutputView.printMap(bridgeData, false);
             BridgeGame.retry(bridgeData, InputView.readGameCommand());
         }
-        OutputView.printResult(bridgeData, BridgeGame.gameStatus, BridgeGame.gameCount);
     }
 }
