ae5768ab827feb6e6f14465ae83cbe4eac3dd420
KangSungBum <peter9706@naver.com>
2022-11-21 15:12:51 +0900

    refactor(BridgeGame.java): code clean up BridgeGame Object
    객체 지향적 코드 구성을 위한 코드 리팩토링
    - 객체 필드 값 trialCount, bridge 생성
    - 모든 값 접근은 객체 필드 값 접근을 통해 처리

--- src/main/java/bridge/BridgeGame.java
+++ src/main/java/bridge/BridgeGame.java
@@ -16,8 +16,8 @@ public class BridgeGame {
      * <p>
      * 이동을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public boolean move(List<String> bridge, int index, String userMovement) {
-        if (!bridge.get(index).equals(userMovement)) {
+    public boolean move(int index, String userMovement) {
+        if (!this.bridge.get(index).equals(userMovement)) {
             return false;
         }
         return true;
