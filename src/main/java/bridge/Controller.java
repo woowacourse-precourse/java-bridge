152a8342941fe1004a496f76389ee96c1b40f5a0
jwPark6 <67006945+jwPark6@users.noreply.github.com>
2022-11-22 03:52:30 +0900

    refactor : 게임 실행 기능 분리, 재귀형태로 변경

--- src/main/java/bridge/Controller.java
+++ src/main/java/bridge/Controller.java
@@ -15,33 +15,12 @@ public class Controller {
         this.bridge = new Bridge();
     }
 
-    public void runGame(){
+    public void runGame() {
         beforeStartGame();
-        int count = 1;
-        String result = "";
-        while(true){
-            startGame();
-            if(bridgeGame.isFail()){
-                outputView.printLine("게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)");
-                String s = inputView.readGameCommand();
-                if(bridgeGame.retry(s)){
-                    count++;
-                    continue;
-                }
-                result = "실패";
-                outputView.printResult(bridgeGame.getLines(), result, count);
-                break;
-            }
-            if (bridgeGame.isSuccess(bridge.getElementsOfBridge().size())) {
-                result = "성공";
-                outputView.printResult(bridgeGame.getLines(), result, count);
-                break;
-            }
-        }
-
+        startGame(1);
     }
 
-    public void beforeStartGame(){
+    public void beforeStartGame() {
         outputView.printLine("다리 건너기 게임을 시작합니다.\n");
         outputView.printLine("다리의 길이를 입력해주세요");
         int size = inputView.readBridgeSize();
@@ -49,18 +28,32 @@ public class Controller {
         System.out.println();
     }
 
-    public void startGame(){
-        for(String element : bridge.getElementsOfBridge()){
-            outputView.printLine("이동할 칸을 선택해주세요. (위: U, 아래: D)");
-            String s = inputView.readMoving();
-            bridgeGame.move(element, s);
-            outputView.printMap(bridgeGame.getLines());
-            System.out.println();
+    public void duringGame(String element) {
+        outputView.printLine("이동할 칸을 선택해주세요. (위: U, 아래: D)");
+        String s = inputView.readMoving();
+        bridgeGame.move(element, s);
+        outputView.printMap(bridgeGame.getLines());
+        System.out.println();
+    }
+
+    public void startGame(int count) {
+        for (String element : bridge.getElementsOfBridge()) {
+            duringGame(element);
+            if (bridgeGame.isFailed() && checkRetry(count)) {
+                startGame(count + 1);
+                return;
+            }
+        }
+        outputView.printResult(bridgeGame.getLines(), "성공", count);
+    }
+
+    public boolean checkRetry(int count) {
+        outputView.printLine("게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)");
+        if (bridgeGame.retry(inputView.readGameCommand())) {
+            return true;
+        } else {
+            outputView.printResult(bridgeGame.getLines(), "실패", count);
+            return false;
         }
     }
-
-    public void afterGame(){
-
-    }
-
 }
