754f1f2dad0a41c9c0650e7326fb996bd95e17e5
JJ <jeongjae1996@naver.com>
2022-11-22 00:03:47 +0900

    feat: Play 클래스에서 게임을 플레이하는 과정을 담당하도록 구현

---
+++ src/main/java/bridge/Play.java
@@ -0,0 +1,58 @@
+package bridge;
+
+public class Play {
+    private static final String RETRY_COMMAND = "R";
+
+    private final View view;
+    private final BridgeGame bridgeGame;
+    private final Map map;
+
+    public Play() {
+        this.view = new View();
+        this.bridgeGame = start();
+        this.map = new Map();
+    }
+
+    private BridgeGame start() {
+        int bridgeSize = view.gameStartView();
+
+        return new BridgeGame(bridgeSize);
+    }
+
+    private void round() {
+        String moving = view.moveView();
+        bridgeGame.move(moving);
+        map.drawMap(bridgeGame.getUserPath(), bridgeGame.isCorrectPath());
+        view.mapView(map.getMap());
+        checkRetry();
+        nextRound();
+    }
+
+    private void checkRetry() {
+        if (!bridgeGame.isCorrectPath()) {
+            String retryCommand = view.retryView();
+            if (retryCommand.equals(RETRY_COMMAND)) {
+                bridgeGame.retry(map);
+            }
+        }
+    }
+
+    private void nextRound() {
+        if (bridgeGame.getUserPosition() < bridgeGame.getBridgeSize() - 1) {
+            round();
+        }
+    }
+
+    private void finish() {
+        view.closeView(
+                map.getMap(),
+                bridgeGame.getTryCount(),
+                bridgeGame.isCorrectPath()
+        );
+    }
+
+    public void play() {
+        round();
+        finish();
+    }
+}
