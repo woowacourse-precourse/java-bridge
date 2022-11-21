eaf8ed72831354e371c8e709bf023b48db06b32c
JunHyeon <dlwnsgus07@naver.com>
2022-11-22 02:09:02 +0900

    feat: 프로그램의 버그로 발생할 수 있는 예외사항 처리

--- src/main/java/bridge/Bridge.java
+++ src/main/java/bridge/Bridge.java
@@ -6,7 +6,14 @@ public class Bridge {
     public final List<String> bridge;
 
     public Bridge(List<String> bridge) {
+        bridgeValidation(bridge);
         this.bridge = bridge;
+    }
+
+    private void bridgeValidation(List<String> bridge) {
+        if (bridge.size() < 3 || 20 < bridge.size()) {
+            throw new IllegalStateException("다리의 크기가 3과 20사이가 아닙니다.");
+        }
     }
 
     public List<String> getBridge() {
