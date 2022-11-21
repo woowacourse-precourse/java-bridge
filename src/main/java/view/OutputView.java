20c9324f7a29c23b645f6c7f5f9ad07686d761c8
seoeunbae <enyoupark@gmail.com>
2022-11-21 09:57:17 +0900

    feat : 출럭 메소드 작성 진행중

--- src/main/java/view/OutputView.java
+++ src/main/java/view/OutputView.java
@@ -9,17 +9,19 @@ import java.util.List;
  * 사용자에게 게임 진행 상황과 결과를 출력하는 역할을 한다.
  */
 public class OutputView {
+    static final String divider = "|";
+    static final String start = "[";
 
+    static final String end = "]";
 
     /**
      * 현재까지 이동한 다리의 상태를 정해진 형식에 맞춰 출력한다.
      * <p>
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-
-
-    public void printMap() {
-
+    public void printMap(List<String> up, List<String> down) {
+        StringBuilder upStringBuilder = new StringBuilder().append(start);
+        StringBuilder downStringBuilder = new StringBuilder().append(start);
 
     }
 
