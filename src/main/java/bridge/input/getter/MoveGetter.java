6d50e91e9ddfa3b53c23508687332d6335917b1c
yongseungLeeDec <yonson.dev@gmail.com>
2022-11-21 19:30:16 +0900

    feat: 사용자로부터 이동할 칸 옵션을 받아오는 기능 구현
    MoveGetter 클래스 파일을 추가하고, getMoveFromConsole 메서드를 구현함.
    - 해당 메서드는 MoveValidator를 이용하여 알맞은 입력인지 확인하고 아닐 경우 호출자에게 IllegalArgumentException을 throw 함.
    
    사용자에게 이동할 칸 입력을 요청하는 문구를 출력하는 도우미 함수도 함께 구현함.

---
+++ src/main/java/bridge/input/getter/MoveGetter.java
@@ -0,0 +1,31 @@
+package bridge.input.getter;
+
+import bridge.console.InputView;
+import bridge.input.validator.MoveValidator;
+
+public class MoveGetter {
+    private static final String TEXT_PROMPT_FOR_MOVE = "이동할 칸을 선택해주세요. (위: U, 아래: D)";
+    private static final String ERROR_INVALID_MOVE = "이동은 위칸(\"U\") 또는 아래칸(\"U\")으로만 가능합니다.";
+
+    private final InputView inputView;
+    private final MoveValidator validator;
+
+    public MoveGetter() {
+        this.inputView = new InputView();
+        this.validator = new MoveValidator();
+    }
+
+    public String getMoveFromConsole() throws IllegalArgumentException {
+        printPromptForMove();
+        String userInput = inputView.readMove();
+
+        if (validator.isValidMove(userInput)) {
+            return userInput;
+        }
+        throw new IllegalArgumentException(ERROR_INVALID_MOVE);
+    }
+
+    private void printPromptForMove() {
+        System.out.println(TEXT_PROMPT_FOR_MOVE);
+    }
+}
