0854d38ca63c7f586255810d668a67991437eed8
Hyeonjun Park <phjppo0918@gmail.com>
2022-11-21 14:16:25 +0900

    test(InputValidatorTest) : 재시도 명령 입력시 요청 문자와 다를 때 예외처리

--- src/test/java/bridge/InputValidatorTest.java
+++ src/test/java/bridge/InputValidatorTest.java
@@ -8,7 +8,7 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 class InputValidatorTest {
 
     InputValidator inputValidator = new InputValidator();
-    
+
     @Test
     @DisplayName("다리 개수 입력시 숫자가 아닐 때 예외처리")
     void inputBridgeSizeNotNumber() throws Exception {
@@ -75,4 +75,14 @@ class InputValidatorTest {
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
+    @Test
+    @DisplayName("재시도 명령 입력시 요청 문자와 다를 때 예외처리")
+    void inputRetryCommandNotSupport() throws Exception {
+        //given
+        String input = "U";
+        //when
+        //then
+        assertThatThrownBy(() -> inputValidator.validateInputRetry(input))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
 }
