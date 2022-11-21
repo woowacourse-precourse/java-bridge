866cec80ef6779ce5720bdcc0140e3aaa5871b0d
dooboocookie <bkh3030@gmail.com>
2022-11-21 17:44:58 +0900

    test: 다음 발판으로 넘어가는 테스트

--- src/test/java/bridge/model/PlayerPathTest.java
+++ src/test/java/bridge/model/PlayerPathTest.java
@@ -1,4 +1,27 @@
 package bridge.model;
 
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 public class PlayerPathTest {
+
+    @DisplayName("다음칸으로 넘어가는 테스트")
+    @Test
+    void nextStepTest() {
+        PlayerPath playerPath = new PlayerPath();
+        playerPath.nextStep(Plate.UP_PLATE);
+        playerPath.nextStep(Plate.DOWN_PLATE);
+        playerPath.nextStep(Plate.UP_PLATE);
+        List<Plate> playerPlates = playerPath.getPlayerPath();
+
+        assertThat(playerPlates)
+                .containsExactly(Plate.UP_PLATE, Plate.DOWN_PLATE, Plate.DOWN_PLATE);
+    }
+
+    
 }
