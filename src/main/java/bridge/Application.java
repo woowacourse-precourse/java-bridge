7eed935c93e53538b20c0e8c8e4261c46617aeb6
Yerim lee <103623292+lyrWinterCat@users.noreply.github.com>
2022-11-22 01:44:21 +0900

    refactor (Application) : 라인 정리

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -3,13 +3,9 @@ package bridge;
 import bridge.controller.BridgeGameController;
 
 public class Application {
-
-
-
     public static void main(String[] args) {
         // TODO: 프로그램 구현
         BridgeGameController bridgeGameController = new BridgeGameController();
         bridgeGameController.run();
-
     }
 }
