cb7bf8b641711f91198fb26107ceb02ddd76a006
kite707 <kite707707@gmail.com>
2022-11-22 01:32:31 +0900

    feat(input): 다리 길이 입력받기 및 예외처리 완료

--- src/main/java/bridge/InputView.java
+++ src/main/java/bridge/InputView.java
@@ -1,15 +1,23 @@
 package bridge;
 
+import camp.nextstep.edu.missionutils.Console;
+
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
  */
 public class InputView {
+    Validator validator = new Validator();
 
     /**
      * 다리의 길이를 입력받는다.
      */
     public int readBridgeSize() {
-        return 0;
+        System.out.println("다리의 길이를 입력해주세요.");
+        String inputBridgeSize = Console.readLine();
+        if (validator.bridgeSizeValidator(inputBridgeSize)) {
+            return Integer.parseInt(inputBridgeSize);
+        }
+        throw new IllegalArgumentException("[ERROR] 다리 길이 입력에서 오류가 발생했습니다.");
     }
 
     /**
---
+++ src/main/java/bridge/Validator.java
@@ -0,0 +1,23 @@
+package bridge;
+
+public class Validator {
+    public static final int MINIMUM_BRIDGE_LENGTH = 3;
+    public static final int MAXIMUM_BRIDGE_LENGTH = 20;
+
+
+    public boolean inInRange(int input) {
+        return input >= MINIMUM_BRIDGE_LENGTH && input <= MAXIMUM_BRIDGE_LENGTH;
+    }
+
+    public boolean bridgeSizeValidator(String input) {
+        try {
+            int bridgeSize = Integer.parseInt(input);
+            if (inInRange(bridgeSize)) {
+                return true;
+            }
+            throw new IllegalArgumentException("[ERROR] 3-20사이의 숫자를 입력해주세요!");
+        } catch (NumberFormatException ex) {
+            throw new IllegalArgumentException("[ERROR] 정수를 입력해주세요!");
+        }
+    }
+}
