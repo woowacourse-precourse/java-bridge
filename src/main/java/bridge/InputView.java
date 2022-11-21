44fbf96e63518694808bc07c5ef387be8f5ccf19
taehyun <didls1643@gmail.com>
2022-11-21 22:55:26 +0900

    fix: 에러 메세지 수정

--- src/main/java/bridge/InputView.java
+++ src/main/java/bridge/InputView.java
@@ -6,7 +6,7 @@ import camp.nextstep.edu.missionutils.Console;
  * 사용자로부터 입력을 받는 역할을 한다.
  */
 public class InputView {
-    private final String errorMsg = "ERROR";
+    private final String errorMsg = "[ERROR]";
 
     /**
      * 다리의 길이를 입력받는다.
