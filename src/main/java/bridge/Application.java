28e62105af150e8e84263c07786cddf2599a5f37
이건회 <rg970604@naver.com>
2022-11-22 03:41:36 +0900

    feat(Application.java) : 프로그램 실행

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -1,8 +1,11 @@
 package bridge;
 
+import bridge.play.ProgramStarter;
+
 public class Application {
 
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        ProgramStarter.startProgram();
     }
+
 }
