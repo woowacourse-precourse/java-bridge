1790e6d14905c88d8772a1c055c058bcc151e822
0chil <0@chll.it>
2022-11-22 07:05:32 +0900

    refactor(Player): Bridge를 구성해 비교한다

--- src/main/java/bridge/domain/Player.java
+++ src/main/java/bridge/domain/Player.java
@@ -1,30 +1,39 @@
 package bridge.domain;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import bridge.constant.Direction;
 
 public class Player {
 
     private final Bridge bridge;
-    private Position position;
+    // private Position position;
+
+    private final List<Direction> directions = new ArrayList<>();
 
     public Player(Bridge bridge) {
         this.bridge = bridge;
-        this.position = new Position();
+        // this.position = new Position();
     }
 
     public boolean moveNext(Direction direction) {
-        if (bridge.isMovable(position, direction)) {
-            increasePosition();
-            return true;
-        }
-        return false;
+        directions.add(direction);
+        return bridge.matchesFromStartWith(Bridge.fromDirections(directions));
+
+        // if (bridge.isMovable(position, direction)) {
+        //     increasePosition();
+        //     return true;
+        // }
+        // return false;
     }
 
-    private void increasePosition() {
-        position = position.increase();
-    }
+    // private void increasePosition() {
+    //     position = position.increase();
+    // }
 
     public boolean isArrived() {
-        return bridge.isOver(position);
+        return bridge.hasSameSizeWith(Bridge.fromDirections(directions));
+        // return bridge.isOver(position);
     }
 }
