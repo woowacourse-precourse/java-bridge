857bc73de68b697afdd800312db2cb445e4d92c5
Life is a ninanino <tvxqinak@naver.com>
2022-11-22 01:17:01 +0900

    refactor : collections 변경

--- src/main/java/bridge/MapMaker.java
+++ src/main/java/bridge/MapMaker.java
@@ -1,56 +1,60 @@
 package bridge;// @ author ninaaano
 
+import bridge.view.InputView;
 import bridge.view.MapView;
 
+import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Queue;
 
 public class MapMaker {
-    StringBuilder upState = new StringBuilder();
-    StringBuilder downState = new StringBuilder();
+    private final StringBuilder upState = new StringBuilder();
+    private final StringBuilder downState = new StringBuilder();
 
     private Queue<String> bridge;
 
-    public MapMaker() {
+    public MapMaker(List<String> bridge) {
         this.bridge = new LinkedList<>(bridge);
     }
 
-    public List<String> printMap(){
-        return List.of("[ "+upState+" ]","[ "+downState+" ]");
+    // boolean의 기본으 false다
+    public void addMap(boolean answer){
+        if(answer){
+            upState.append(MapView.MAP_MIDDLE);
+            upState.append(MapView.MAP_MIDDLE);
+        }
+        if(answer)
+            addMapO(bridge.remove());
+        if(!answer)
+            addMapX(bridge.remove());
+
     }
 
     public void addMapO(String input){
         if(input.equals("U")){
             upState.append(MapView.MAP_O);
-            upState.append(MapView.MAP_BLANK);
+            downState.append(MapView.MAP_BLANK);
         }
         if(input.equals("D")){
-            downState.append(MapView.MAP_BLANK);
+            upState.append(MapView.MAP_BLANK);
             downState.append(MapView.MAP_O);
         }
     }
 
     public void addMapX(String input){
         if(input.equals("U")){
-            upState.append(MapView.MAP_BLANK);
             upState.append(MapView.MAP_X);
+            downState.append(MapView.MAP_BLANK);
         }
         if(input.equals("D")){
+            upState.append(MapView.MAP_BLANK);
             downState.append(MapView.MAP_X);
-            downState.append(MapView.MAP_BLANK);
         }
     }
 
-    public void addMap(boolean answer){
-        if(upState.length() != 0 && downState.length() != 0){
-            upState.append(MapView.MAP_MIDDLE);
-            downState.append(MapView.MAP_MIDDLE);
-        }
-        if(answer)
-            addMapO(bridge.remove());
-        if(!answer)
-            addMapX(bridge.remove());
+    public List<String> printMap(){
+        return List.of("[ "+upState+" ]","[ "+downState+" ]");
     }
 
 }
