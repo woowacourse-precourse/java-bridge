7ce7a656c09d010cddb4c7b00903f245c04668cd
zzun <736300@naver.com>
2022-11-21 23:14:38 +0900

    feat(OutputView): 현재까지 이동한 다리 상태를 출력해 주는 printMap 메서드, 게임 최종 결과를 출력해 주는 printResult 메서드 구현

--- src/main/java/bridge/OutputView.java
+++ src/main/java/bridge/OutputView.java
@@ -1,23 +1,42 @@
 package bridge;
 
-/**
- * 사용자에게 게임 진행 상황과 결과를 출력하는 역할을 한다.
- */
 public class OutputView {
 
-    /**
-     * 현재까지 이동한 다리의 상태를 정해진 형식에 맞춰 출력한다.
-     * <p>
-     * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
-     */
-    public void printMap() {
+    private static final String OUTPUT_GAME_RESULT_MESSAGE = "최종 게임 결과";
+    private static final String OUTPUT_SUCCESS_STATUS_MESSAGE = "게임 성공 여부: ";
+    private static final String TOTAL_ATTEMPT_COUNT_MESSAGE = "총 시도한 횟수: ";
+    private static final String SUCCESS_MESSAGE = "성공";
+    private static final String FAILURE_MESSAGE = "실패";
+    private static final String HEAD_MESSAGE = "[";
+    private static final String BODY_MESSAGE = "|";
+    private static final String TAIL_MESSAGE = "]";
+    private static final String BLANK_MESSAGE = " ";
+
+
+    public void printMap(String[][] userBoard, int position) {
+        for (int i = 0; i < 2; i++) {
+            System.out.print(HEAD_MESSAGE);
+            for (int j = 0; j < position; j++) {
+                System.out.print(BLANK_MESSAGE + userBoard[i][j] + BLANK_MESSAGE);
+                if (j != position - 1 && position != 1) {
+                    System.out.print(BODY_MESSAGE);
+                }
+            }
+            System.out.println(TAIL_MESSAGE);
+        }
+        System.out.println();
     }
 
-    /**
-     * 게임의 최종 결과를 정해진 형식에 맞춰 출력한다.
-     * <p>
-     * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
-     */
-    public void printResult() {
+
+    public void printResult(String[][] userBoard, int position, boolean success, int totalAttemptCount) {
+        System.out.println(OUTPUT_GAME_RESULT_MESSAGE);
+        printMap(userBoard, position);
+        if (success) {
+            System.out.println(OUTPUT_SUCCESS_STATUS_MESSAGE + SUCCESS_MESSAGE);
+        }
+        if (success == false) {
+            System.out.println(OUTPUT_SUCCESS_STATUS_MESSAGE + FAILURE_MESSAGE);
+        }
+        System.out.println(TOTAL_ATTEMPT_COUNT_MESSAGE + totalAttemptCount);
     }
 }
