c66d248c2914414fc013d84eccf668a6ba88a1da
jihun-24k <gi020477@gmail.com>
2022-11-22 00:47:55 +0900

    Feat(BridgeGame): 다리 생성클래스 필드 추가

--- src/main/java/bridge/BridgeGame.java
+++ src/main/java/bridge/BridgeGame.java
@@ -4,15 +4,20 @@ public class BridgeGame {
 
     private final InputView inputView;
     private final OutputView outputView;
+    private final BridgeRandomNumberGenerator bridgeRandomNumberGenerator;
+    private final BridgeMaker bridgeMaker;
 
     public BridgeGame() {
         this.inputView = new InputView();
         this.outputView = new OutputView();
+        this.bridgeRandomNumberGenerator = new BridgeRandomNumberGenerator();
+        this.bridgeMaker = new BridgeMaker(this.bridgeRandomNumberGenerator);
     }
 
     public void play() {
         System.out.println("게임 시작");
         int bridgeSize = inputView.readBridgeSize();
+        Bridge bridge = new Bridge(bridgeMaker.makeBridge(bridgeSize));
     }
 
     public void move() {
