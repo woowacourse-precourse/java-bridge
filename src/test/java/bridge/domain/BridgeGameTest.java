3eee8f262fa88c481b3441b53cd3dd3f4827d1b5
dongjun0128 <70315475+dongjun0128@users.noreply.github.com>
2022-11-22 00:41:22 +0900

    refactor : CsvSource를 사용해 테스트 코드 축소

--- src/test/java/bridge/domain/BridgeGameTest.java
+++ src/test/java/bridge/domain/BridgeGameTest.java
@@ -3,6 +3,8 @@ package bridge.domain;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import java.util.List;
 
@@ -17,18 +19,12 @@ class BridgeGameTest {
         bridgeGame=new BridgeGame(List.of("U","D","D"));
     }
 
-    @DisplayName("bridge에 맞는 값을 입력하면 true를 반환")
-    @Test
-    void moveWhenInputCorrectReturnTrue() {
-        boolean result = bridgeGame.move("U");
-        assertThat(result).isTrue();
-    }
-
-    @DisplayName("bridge에 틀린 값을 입력하면 false를 반환")
-    @Test
-    void moveWhenInputIncorrectReturnFalse(){
-        boolean result = bridgeGame.move("D");
-        assertThat(result).isFalse();
+    @DisplayName("맞는 값을 입력하면 true, 틀린 값을 입력하면 false")
+    @ParameterizedTest
+    @CsvSource({"U,true","D,false"})
+    public void move(String userInput, boolean expected){
+        boolean actual = bridgeGame.move(userInput);
+        assertThat(actual).isEqualTo(expected);
     }
 
     @DisplayName("bridge에 맞는 값을 입력하면 userMoving에 맞은 결과를 더한다.")
@@ -39,94 +35,44 @@ class BridgeGameTest {
         assertThat(result).isEqualTo(List.of("U"));
     }
 
-    @DisplayName("U를 입력했을 때 틀리면 userMoving에 UX를 입력한다.")
-    @Test
-    void canNotMoveWhenInputU() {
-        bridgeGame.canNotMove("U");
-        List<String> result = bridgeGame.getUserMoving();
-        assertThat(result).isEqualTo(List.of("UX"));
+    @DisplayName("U가 틀리면 UX, D가 틀리면 DX를 userMoving에 추가")
+    @ParameterizedTest
+    @CsvSource({"U,UX","D,DX"})
+    void canNotMove(String userInput, String expected){
+        bridgeGame.canNotMove(userInput);
+        List<String> actual = bridgeGame.getUserMoving();
+        assertThat(actual.get(0)).isEqualTo(expected);
     }
 
-    @DisplayName("D를 입력했을 때 틀리면 userMoving에 DX를 입력한다.")
-    @Test
-    void canNotMoveWhenInputD(){
-        bridgeGame.canNotMove("D");
-        List<String> result = bridgeGame.getUserMoving();
-        assertThat(result).isEqualTo(List.of("DX"));
+    @DisplayName("R을 입력하면 재시작(true), Q를 입력하면 종료(false)")
+    @ParameterizedTest
+    @CsvSource({"R,true","Q,false"})
+    void retry(String userInput, boolean expected){
+        boolean actual = bridgeGame.retry(userInput);
+        assertThat(actual).isEqualTo(expected);
     }
 
-    @DisplayName("R을 입력하면 재시작 이라는 뜻이므로 true를 반환")
-    @Test
-    void retryWhenInputR() {
-        boolean retry = bridgeGame.retry("R");
-        assertThat(retry).isTrue();
+    @DisplayName("bridgeSize만큼 move하면 true, bridgeSize만큼 move하지 못하면 false")
+    @ParameterizedTest
+    @CsvSource({"U,D,U,true","U,D,D,true","U,D,' ',false"})
+    void isEndOfBridge(String userInput1, String userInput2, String userInput3, boolean expected){
+        bridgeGame.move(userInput1);
+        bridgeGame.move(userInput2);
+        bridgeGame.move(userInput3);
+        boolean actual = bridgeGame.isEndOfBridge();
+
+        assertThat(actual).isEqualTo(expected);
     }
 
-    @DisplayName("Q를 입력하면 false를 반환")
-    @Test
-    void retryWhenInputQ(){
-        boolean retry = bridgeGame.retry("Q");
-        assertThat(retry).isFalse();
-    }
+    @DisplayName("끝까지 가고 마지막이 맞으면 true, 마지막이 틀리면 false")
+    @ParameterizedTest
+    @CsvSource({"U,D,D,true","U,D,U,false"})
+    void isSuccess(String userInput1, String userInput2, String userInput3, boolean expected){
+        bridgeGame.move(userInput1);
+        bridgeGame.move(userInput2);
+        bridgeGame.move(userInput3);
+        boolean actual = bridgeGame.isSuccess();
 
-    @DisplayName("bridgeSize만큼 move하면 마지막 값이 틀려도 True를 반환(isEndOfBridge)")
-    @Test
-    void isEndOfBridgeWhenLastInputIncorrect() {
-        bridgeGame.move("U");
-        bridgeGame.move("D");
-        bridgeGame.move("U");
-
-        boolean retry = bridgeGame.isEndOfBridge();
-
-        assertThat(retry).isTrue();
-    }
-
-    @DisplayName("bridgeSize만큼 move하면 True를 반환(isEndOfBridge)")
-    @Test
-    void isEndOfBridgeWhenLastInputCorrect() {
-        bridgeGame.move("U");
-        bridgeGame.move("D");
-        bridgeGame.move("D");
-
-        boolean retry = bridgeGame.isEndOfBridge();
-
-        assertThat(retry).isTrue();
-    }
-
-    @DisplayName("bridgeSize만큼 move하지 못하면 False를 반환(isEndOfBridge)")
-    @Test
-    void isEndOfBridgeWhenLessThanMoveSize() {
-        bridgeGame.move("U");
-        bridgeGame.move("D");
-
-        boolean retry = bridgeGame.isEndOfBridge();
-
-        assertThat(retry).isFalse();
-    }
-
-    @DisplayName("bridgeSize만큼 move하고, 마지막 입력도 맞으면 True반환(isSuccess)")
-    @Test
-    void isSuccessWhenLastInputCorrect() {
-
-        bridgeGame.move("U");
-        bridgeGame.move("D");
-        bridgeGame.move("D");
-
-        boolean retry = bridgeGame.isSuccess();
-
-        assertThat(retry).isTrue();
-    }
-
-    @DisplayName("bridgeSize만큼 move했지만, 마지막 값이 틀리면 False반환(isSuccess)")
-    @Test
-    void isSuccessWhenLastInputIncorrect() {
-
-        bridgeGame.move("U");
-        bridgeGame.move("D");
-        bridgeGame.move("U");
-
-        boolean retry = bridgeGame.isSuccess();
-
-        assertThat(retry).isFalse();
+        assertThat(actual).isEqualTo(expected);
     }
 }
