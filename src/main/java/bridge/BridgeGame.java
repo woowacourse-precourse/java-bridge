a42aa0870a6d07eef310a61d35af4d92df35530f
이경범 <tkdwk567@naver.com>
2022-11-21 13:28:44 +0900

    feat : 시작할 떄 사용자의 위치를 받는다.

--- src/main/java/bridge/BridgeGame.java
+++ src/main/java/bridge/BridgeGame.java
@@ -6,18 +6,14 @@ package bridge;
 public class BridgeGame {
 
     /**
-     * 사용자가 칸을 이동할 때 사용하는 메서드
-     * <p>
-     * 이동을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
+     * @param position 다리의 위치 ( 아래칸, 위칸)
+     * @param player   사용자
+     *                 <p>
+     *                 다리의 size 만큼 사용자는 칸을 선택하여 List 에 담는다.
      */
-    public void move() {
+    public void start(String position, Player player) {
+        player.selectPosition(position);
     }
 
-    /**
-     * 사용자가 게임을 다시 시도할 때 사용하는 메서드
-     * <p>
-     * 재시작을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
-     */
-    public void retry() {
-    }
+
 }
