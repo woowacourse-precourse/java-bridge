d811b3917b5143b6381362a7145be51a0a0ba7ae
lullulu__ <75472116+earthyoung@users.noreply.github.com>
2022-11-22 01:48:14 +0900

    feat: OutputView - 사용자 이동 경로 출력, 최종 결과 출력

--- src/main/java/bridge/OutputView.java
+++ src/main/java/bridge/OutputView.java
@@ -1,23 +1,86 @@
 package bridge;
 
-/**
- * 사용자에게 게임 진행 상황과 결과를 출력하는 역할을 한다.
- */
+
 public class OutputView {
 
-    /**
-     * 현재까지 이동한 다리의 상태를 정해진 형식에 맞춰 출력한다.
-     * <p>
-     * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
-     */
-    public void printMap() {
+    public final BridgeGame bridgeGame;
+    public String upperResult;
+    public String downResult;
+
+    public OutputView(BridgeGame bridgeGame){
+        this.bridgeGame = bridgeGame;
     }
 
-    /**
-     * 게임의 최종 결과를 정해진 형식에 맞춰 출력한다.
-     * <p>
-     * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
-     */
-    public void printResult() {
+    public void setUpperResult(String upperResult)  {this.upperResult = upperResult;}
+
+    public void setDownResult(String downResult)  {this.downResult = downResult;}
+
+    public String getUpperResult()  {return this.upperResult;}
+
+    public String getDownResult()  {return this.downResult;}
+
+    public BridgeGame getBridgeGame(){
+        return this.bridgeGame;
     }
+
+    public void printMap() {
+        int size = getBridgeGame().getInputList().size();
+        this.upperResult = printBasicMap(size);
+        this.downResult = printBasicMap(size);
+        for(int i=0; i<size; i++){
+            String input = bridgeGame.inputList.get(i);
+            String answer = bridgeGame.getAnswerList().get(i);
+            changeResult(input, answer, i);
+        }
+        System.out.println(this.getUpperResult() + "\n" + this.getDownResult());
+    }
+
+    public String printBasicMap(int size){
+        String result = "";
+        result += ("[ ");
+        for(int i=0; i<(size-1); i++) result += (i + " | ");
+        result += ((size-1) + " ]");
+        return result;
+    }
+
+    public void changeResult(String input, String answer, int index){
+        if(answer.equals("U") && answer.equals(input)) upperEqualsU(index);
+        if(answer.equals("D") && answer.equals(input)) upperEqualsD(index);
+        if(answer.equals("U") && !answer.equals(input)) upperNotEqualsU(index);
+        if(answer.equals("D") && !answer.equals(input)) upperNotEqualsD(index);
+    }
+
+    public void printResult(int count, int total, int totalCount) {
+        System.out.println("최종 게임 결과");
+        printMap();
+        String succeed = isSucceed(count, total);
+        System.out.println("게임 성공 여부: " + succeed);
+        System.out.println("총 시도한 횟수: " + totalCount);
+    }
+
+    public String isSucceed(int count, int total){
+        if(count < total) return "실패";
+        return "성공";
+    }
+
+    private void upperEqualsU(int index){
+        setUpperResult(getUpperResult().replace(String.valueOf(index), "O"));
+        setDownResult(getDownResult().replace(String.valueOf(index), " "));
+    }
+
+    private void upperEqualsD(int index){
+        setUpperResult(getUpperResult().replace(String.valueOf(index), " "));
+        setDownResult(getDownResult().replace(String.valueOf(index), "O"));
+    }
+
+    private void upperNotEqualsU(int index){
+        setUpperResult(getUpperResult().replace(String.valueOf(index), " "));
+        setDownResult(getDownResult().replace(String.valueOf(index), "X"));
+    }
+
+    private void upperNotEqualsD(int index){
+        setUpperResult(getUpperResult().replace(String.valueOf(index), "X"));
+        setDownResult(getDownResult().replace(String.valueOf(index), " "));
+    }
+
 }
