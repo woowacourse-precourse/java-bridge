a0f0510a49f1a45ba704b5035fbbc6bfb4e0457b
jinny-codes <77732679+jinny-codes@users.noreply.github.com>
2022-11-21 17:42:50 +0900

    feat(Application): 줄 제한 무시한 초안

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -1,8 +1,31 @@
 package bridge;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Application {
+    static OutputView output = new OutputView();
+    static InputView input = new InputView();
+    static BridgeNumberGenerator bng = new BridgeRandomNumberGenerator();
+    static BridgeMaker bm = new BridgeMaker(bng);
+    static BridgeGame bg = new BridgeGame();
 
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        output.printStart();
+        List<String> bridge = bm.makeBridge(input.readBridgeSize());
+        List<String> moves = new ArrayList<>();
+        System.out.println(bridge);
+        boolean retry = true, correct = true;
+        int progress = 0;
+        while (retry) {
+            while (correct) {
+                String move = input.readMoving();
+                moves.add(move);
+                progress++;
+                output.printMap(output.makeMap(bridge, moves));
+                correct = bg.move(bridge.get(progress),move);
+            }
+            retry = bg.retry(input.readGameCommand());
+        }
     }
 }
