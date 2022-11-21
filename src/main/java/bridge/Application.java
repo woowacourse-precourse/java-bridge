247604235e0e76efdaa47645d2f5a743a57621db
JJ <jeongjae1996@naver.com>
2022-11-22 00:47:13 +0900

    refactor: InputView에서 예외처리를 하므로 Application에서는 try-catch문 제거

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -4,12 +4,7 @@ public class Application {
 
     public static void main(String[] args) {
         // TODO: 프로그램 구현
-        try {
-            Play play = new Play();
-            play.play();
-        }
-        catch (IllegalArgumentException e) {
-            System.out.println(e.getMessage());
-        }
+        Play play = new Play();
+        play.play();
     }
 }
