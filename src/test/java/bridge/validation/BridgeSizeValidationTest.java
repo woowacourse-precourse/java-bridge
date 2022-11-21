1b1ef752600b2767a9b48b64bc8a7d490669e39f
dongjun0128 <70315475+dongjun0128@users.noreply.github.com>
2022-11-21 19:41:17 +0900

    test : BridgeSizeValidation 테스트 코드 추가

---
+++ src/test/java/bridge/validation/BridgeSizeValidationTest.java
@@ -0,0 +1,34 @@
+package bridge.validation;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+class BridgeSizeValidationTest {
+    private BridgeSizeValidation bridgeSizeValidation;
+
+    @BeforeEach
+    void setUp(){
+        bridgeSizeValidation = new BridgeSizeValidation();
+    }
+
+    @DisplayName("숫자가 아닌 문자가 들어오면 예외처리")
+    @Test
+    void validateWhenInputNotNumber() {
+        assertThatThrownBy(()-> bridgeSizeValidation.validate("a"))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("3~20 범위 이외의 숫자가 들어오면 예외처리")
+    @Test
+    void validateOutOfRange(){
+        assertThatThrownBy(()-> bridgeSizeValidation.validate("1"))
+                .isInstanceOf(IllegalArgumentException.class);
+
+        assertThatThrownBy(()-> bridgeSizeValidation.validate("21"))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+}
