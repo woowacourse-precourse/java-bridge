7921244eefb858c8063b28899c613c79d97991e3
ijung-won <dlwnddnjs96@gmail.com>
2022-11-21 09:56:47 +0900

    feat(ExeptionHandling): 3~20 사이의 숫자 입력이 아닌 경우 예외처리

--- src/main/java/bridge/ExceptionHandling.java
+++ src/main/java/bridge/ExceptionHandling.java
@@ -1,13 +1,19 @@
 package bridge;
 
 public class ExceptionHandling {
-    public static boolean instanceChangeChecking(String input){
+    public static void instanceChangeChecking(String input){
         try{
             int result = Integer.parseInt(input);
         }catch (NumberFormatException numberFormatException){
             System.out.println("[ERROR] 다리 길이는 3부터 20 사이의 숫자여야 합니다.");
             throw new IllegalArgumentException();
         }
-        return true;
+    }
+
+    public static void numberBoundaryChecking(int input){
+        if(input <3 || input > 20){
+            System.out.println("[ERROR] 다리 길이는 3부터 20 사이의 숫자여야 합니다.");
+            throw new IllegalArgumentException();
+        }
     }
 }
