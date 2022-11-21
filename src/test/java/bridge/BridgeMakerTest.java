adda4dc1e9606e5e6c18ebdc7ecc97c84f758d29
xxeol2 <jhj01177@naver.com>
2022-11-22 00:27:59 +0900

    test: 다리를 생성한다

---
+++ src/test/java/bridge/BridgeMakerTest.java
@@ -0,0 +1,36 @@
+package bridge;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.List;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.RepeatedTest;
+
+class BridgeMakerTest {
+
+    BridgeMaker bridgeMaker;
+    BridgeNumberGenerator bridgeNumberGenerator;
+
+    @BeforeEach()
+    void beforeEach() {
+        bridgeNumberGenerator = new BridgeRandomNumberGenerator();
+        bridgeMaker = new BridgeMaker(bridgeNumberGenerator);
+    }
+
+    @DisplayName("생성된 다리는 U와 D로 이루어져야한다.")
+    @RepeatedTest(10)
+    void bridgeCreateStringTest() {
+        List<String> bridge = bridgeMaker.makeBridge(15);
+        for (String row : bridge) {
+            assertThat(List.of("U", "D").contains(row)).isTrue();
+        }
+    }
+
+    @DisplayName("다리의 길이는 사용자에게 입력받은 값이다")
+    @RepeatedTest(10)
+    void bridgeCreateSizeTest() {
+        List<String> bridge = bridgeMaker.makeBridge(15);
+        assertThat(bridge.size()).isEqualTo(15);
+    }
+}
