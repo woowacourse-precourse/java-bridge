afb635948cdea86dab2bb02b06925cc8f6c03ece
Yerim lee <103623292+lyrWinterCat@users.noreply.github.com>
2022-11-22 01:44:05 +0900

    fix (ErrorMessage) : 에러메세지 문구 수정

--- src/main/java/bridge/vo/ErrorMessage.java
+++ src/main/java/bridge/vo/ErrorMessage.java
@@ -1,7 +1,7 @@
 package bridge.vo;
 
 public enum ErrorMessage {
-    BRIDGE_SIZE_NOT_NUMBER("[ERROR] 다리 길이는 3 이상 20 이하인 숫자로 입력 가능합니다."),
+    BRIDGE_SIZE_NOT_NUMBER("[ERROR] 다리 길이는 3부터 20 사이의 숫자여야 합니다."),
     MOVING_WRONG_INPUT("[ERROR] U 또는 D 만 입력 가능합니다. ( U : 윗칸 선택 , D : 아랫칸 선택)"),
     RESTART_WRONG_INPUT("[ERROR] R 또는 Q만 입력 가능합니다. ( R : 재시작, Q : 게임 종료)");
 
