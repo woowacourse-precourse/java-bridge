67ee800f9554a868a7c44ce5128796e9c3e6b011
BaeDongHyeon <sasd730@gmail.com>
2022-11-21 22:56:49 +0900

    feat: BridgeGame move 메소드 구현

--- src/main/java/bridge/BridgeGame.java
+++ src/main/java/bridge/BridgeGame.java
@@ -8,20 +8,23 @@ import java.util.List;
 public class BridgeGame {
 
     private final BridgeMaker bridgeMaker;
+    private Bridge bridges;
 
     public BridgeGame(BridgeMaker bridgeMaker) {
         this.bridgeMaker = bridgeMaker;
     }
 
     public void create(int bridgeSize) {
-        List<String> bridges = bridgeMaker.makeBridge(bridgeSize);
+        bridges = new Bridge(bridgeMaker.makeBridge(bridgeSize));
     }
     /**
      * 사용자가 칸을 이동할 때 사용하는 메서드
      * <p>
      * 이동을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void move() {
+    public ResultSymbol move(String moveCommand) {
+        ResultSymbol resultSymbol = bridges.move(moveCommand);
+        return resultSymbol;
     }
 
     /**
@@ -30,5 +33,9 @@ public class BridgeGame {
      * 재시작을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
     public void retry() {
+    }
+
+    public boolean allTry() {
+        return bridges.getCount() == bridges.getCrossingCount();
     }
 }
