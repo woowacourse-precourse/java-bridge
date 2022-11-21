7a16b5f00a2833445edbd8d45d763f2c3b111f0e
jaehee329 <jaehee329@yonsei.ac.kr>
2022-11-21 16:34:18 +0900

    feat(BridgeGame): 재시작 결과 처리 기능 구현

--- src/main/java/bridge/BridgeGame.java
+++ src/main/java/bridge/BridgeGame.java
@@ -20,8 +20,8 @@ public class BridgeGame {
      */
     public void move() {
         Inputs input = inputView.readMoving();
-        this.bridge.moveNext(input);
-        outputView.printMap(this.bridge);
+        bridge.moveNext(input);
+        outputView.printMap(bridge);
     }
 
     /**
@@ -29,6 +29,17 @@ public class BridgeGame {
      * <p>
      * 재시작을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void retry() {
+    public boolean retry() {
+        String retry = inputView.readGameCommand();
+        if (isRetry(retry)) {
+            bridge.resetMoveStatus();
+            return true;
+        }
+        outputView.printResult(bridge);
+        return false;
+    }
+
+    private boolean isRetry(String retry) {
+        return retry.equals(Inputs.RETRY.getMessage());
     }
 }
