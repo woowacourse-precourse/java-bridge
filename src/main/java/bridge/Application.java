e1eb1b3611406477aa3d7e25b22ab3af63602630
dydtmd6801 <dydtmd6801@naver.com>
2022-11-22 01:23:33 +0900

    feat(Application): 프로그램 최종 구현

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -1,8 +1,12 @@
 package bridge;
 
+import controller.Game;
+
 public class Application {
 
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        Game bridgeCrossGame = new Game();
+        bridgeCrossGame.play();
     }
 }
