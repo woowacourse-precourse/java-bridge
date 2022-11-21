a3910077693e14c565dce42a34f7ec9cdace3df5
aksmf1442 <aksmf1442@naver.com>
2022-11-21 23:05:44 +0900

    test(다리 건너기): 건널 수 없는 칸을 건너려 하면 CannotCrossBridgeException을 발생시키는 기능 테스트 구현

--- src/test/java/bridge/BridgeGameTest.java
+++ src/test/java/bridge/BridgeGameTest.java
@@ -1,7 +1,9 @@
 package bridge;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
+import bridge.exception.CannotCrossBridgeException;
 import java.util.List;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -17,12 +19,22 @@ class BridgeGameTest {
         assertThat(result).isEqualTo(true);
     }
 
-    @DisplayName("[성공] 서로 같은 direction을 비교할 경우 false")
+    @DisplayName("[성공] 서로 다른 direction을 비교할 경우 false")
     @Test
     void compareDirectionByDifferentDirection() {
         List<String> bridge = List.of("U", "D");
         BridgeGame bridgeGame = new BridgeGame(bridge);
         boolean result = bridgeGame.compareDirection("D", 0);
         assertThat(result).isEqualTo(false);
+    }
+
+    @DisplayName("[예외 발생] 건널 수 없는 칸을 건너려 하면 CannotCrossBridgeException 발생")
+    @Test
+    void moveByDifferentDirection() {
+        List<String> bridge = List.of("U", "D");
+        BridgeGame bridgeGame = new BridgeGame(bridge);
+        assertThatThrownBy(() -> bridgeGame.move("U", 1)).isInstanceOf(
+            CannotCrossBridgeException.class);
+
     }
 }
