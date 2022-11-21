5883c6c70c96b35458dc1c3811311605acd52018
phdljr <phdljr@naver.com>
2022-11-22 01:28:03 +0900

    feat: BridgeMaker 로직 추가

---
+++ src/main/java/bridge/generator/BridgeMaker.java
@@ -0,0 +1,38 @@
+package bridge.generator;
+
+import bridge.generator.BridgeNumberGenerator;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static bridge.config.BridgeGameConfig.BRIDGE_UP;
+import static bridge.config.BridgeGameConfig.BRIDGE_UP_NUMBER;
+import static bridge.config.BridgeGameConfig.BRIDGE_DOWN;
+
+/**
+ * 다리의 길이를 입력 받아서 다리를 생성해주는 역할을 한다.
+ */
+public class BridgeMaker {
+
+    private final BridgeNumberGenerator bridgeNumberGenerator;
+
+    public BridgeMaker(BridgeNumberGenerator bridgeNumberGenerator) {
+        this.bridgeNumberGenerator = bridgeNumberGenerator;
+    }
+
+    /**
+     * @param size 다리의 길이
+     * @return 입력받은 길이에 해당하는 다리 모양. 위 칸이면 "U", 아래 칸이면 "D"로 표현해야 한다.
+     */
+    public List<String> makeBridge(int size) {
+        ArrayList<String> bridge = new ArrayList<>();
+        for (int i = 0; i < size; i++) {
+            if (bridgeNumberGenerator.generate() == BRIDGE_UP_NUMBER) {
+                bridge.add(BRIDGE_UP);
+                continue;
+            }
+            bridge.add(BRIDGE_DOWN);
+        }
+        return bridge;
+    }
+}
