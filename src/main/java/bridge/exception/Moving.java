3db270d3f537cc79b3a8f6a3aa0954bb389ca14f
hhiyeon <hhyyeon0214@gmail.com>
2022-11-21 14:51:26 +0900

    feat:moving-check invalid moving input form

--- src/main/java/bridge/exception/Moving.java
+++ src/main/java/bridge/exception/Moving.java
@@ -1,6 +1,8 @@
 package bridge.exception;
 
 public class Moving {
+    private static final String MOVING_UP = "U";
+    private static final String MOVING_DOWN = "D";
     private final String movingInput;
 
     public Moving(String movingInput) {
@@ -9,5 +11,9 @@ public class Moving {
 
     private boolean isNotOneCharacter() {
         return movingInput.length() != 1;
+    }
+
+    private boolean checkInvalidMoving() {
+        return movingInput.equals(MOVING_UP) || movingInput.equals(MOVING_DOWN);
     }
 }
