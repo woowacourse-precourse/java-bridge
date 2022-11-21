352395b3a24d1d196b67f7cab2b72272bc3ee78c
nomushroomplz <aaron97@knou.ac.kr>
2022-11-21 21:19:39 +0900

    feat(InputView): 입력 받는 기능 구현

--- src/main/java/bridge/InputView.java
+++ src/main/java/bridge/InputView.java
@@ -1,28 +1,55 @@
 package bridge;
+import camp.nextstep.edu.missionutils.Console;
 
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
  */
 public class InputView {
+    public static final String Error_NotNumber = "[ERROR] 숫자만 입력해 주세요!";
+    public static final String Error_NotString = "[ERROR] 문자만 입력해 주세요!";
 
     /**
      * 다리의 길이를 입력받는다.
      */
     public int readBridgeSize() {
-        return 0;
+        return inputBridgeSize();
+    }
+
+    private static int inputBridgeSize() {
+        try {
+            return Integer.parseInt(Console.readLine());
+        } catch (RuntimeException e) {
+            throw new IllegalArgumentException(Error_NotNumber);
+        }
     }
 
     /**
      * 사용자가 이동할 칸을 입력받는다.
      */
     public String readMoving() {
-        return null;
+        return inputMoving();
+    }
+
+    private static String inputMoving() {
+        try {
+            return String.valueOf(Console.readLine());
+        } catch (RuntimeException e) {
+            throw new IllegalArgumentException(Error_NotString);
+        }
     }
 
     /**
      * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
      */
     public String readGameCommand() {
-        return null;
+        return inputGameCommand();
+    }
+
+    private static String inputGameCommand() {
+        try {
+            return String.valueOf(Console.readLine());
+        } catch (RuntimeException e) {
+            throw new IllegalArgumentException(Error_NotString);
+        }
     }
 }
