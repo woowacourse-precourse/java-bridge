cffd50b9fb84d7828fc0a6d11ec713c6a97c23b2
hingjae <ljh23270@naver.com>
2022-11-21 12:06:30 +0900

    예외처리 : 이동할 칸 입력, 다시 시도 입력

--- src/main/java/bridge/Exceptions.java
+++ src/main/java/bridge/Exceptions.java
@@ -11,14 +11,14 @@ public class Exceptions {
     }
 
     public static String isUOrD(String input) {
-        if (!(input.equals("U")) || !(input.equals("D"))) {
+        if (!(input.equals("U")) && !(input.equals("D"))) {
             throw new IllegalArgumentException("U 또는 D만 입력할 수 있습니다.");
         }
         return input;
     }
 
     public static String isROrQ(String input) {
-        if (!(input.equals("R")) || !(input.equals("Q"))) {
+        if (!(input.equals("R")) && !(input.equals("Q"))) {
             throw new IllegalArgumentException("R 또는 Q만 입력할 수 있습니다.");
         }
         return input;
