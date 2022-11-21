6d85f3a8e8b37d618b48c7be88e8207197c72524
JDK <ehdrjf2062@gmail.com>
2022-11-21 14:56:27 +0900

    refactor: 컴퓨터 브릿지 유효성

--- src/main/java/bridge/domain/BridgeOfComputer.java
+++ src/main/java/bridge/domain/BridgeOfComputer.java
@@ -1,5 +1,6 @@
 package bridge.domain;
 
+import bridge.utils.Validator;
 import java.util.List;
 
 public class BridgeOfComputer {
@@ -11,6 +12,7 @@ public class BridgeOfComputer {
     }
 
     public boolean match(int stage, String moveSide) {
+        Validator.checkIsSide(moveSide);
         String answer = map.get(stage);
         return answer.equals(moveSide);
     }
