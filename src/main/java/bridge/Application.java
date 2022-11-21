a324e93ee9d1754c05838f5606e18ec45b5492a5
Yujin830 <rladbwls30@naver.com>
2022-11-22 00:32:39 +0900

    Fix: 게임 진행 메소드 분리

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -5,37 +5,12 @@ import java.util.List;
 public class Application {
 
     public static void main(String[] args) {
-        int size, try_count = 1;
-        String move, re = "R";
-        boolean success = true;
+        int size;
 
         Announcement.start();
         size = InputView.readBridgeSize();
 
-        List<String> bridge = make_bridge(size);
-
-        while(re.contains("R")) {
-            for (int i = 0; i < size; i++) {
-                Announcement.move();
-                move = InputView.readMoving();
-                success = BridgeGame.move(bridge, move, i);
-                OutputView.printMap(BridgeGame.getUp_bridge(), BridgeGame.getDown_bridge());
-                if (success == false)
-                    break;
-            }
-
-            Announcement.restart();
-            re = InputView.readGameCommand();
-
-            if (re.contains("R")) {
-                try_count += 1;
-                BridgeGame.retry();
-            }
-        }
-
-        Announcement.bridge_result();
-        OutputView.printMap(BridgeGame.getUp_bridge(), BridgeGame.getDown_bridge());
-        OutputView.printResult(success, try_count);
+        start_game(make_bridge(size), size);
         // TODO: 프로그램 구현
     }
     public static List<String> make_bridge(int size){
@@ -43,5 +18,48 @@ public class Application {
         BridgeMaker bridgeMaker = new BridgeMaker(bridgeRandomNumberGenerator);
         List<String> bridge = bridgeMaker.makeBridge(size);
         return bridge;
+    }
+    public static void start_game(List<String> bridge, int size){
+        String re="R";
+        int try_count = 1;
+        boolean success = true;
+
+        while(re.contains("R")) {
+            success = try_cross(bridge, size);
+            re = ask_retry();
+            try_count = retry_cross(re, try_count);
+        }
+
+        print_result(success, try_count);
+    }
+    public static boolean try_cross(List<String> bridge, int size){
+        boolean success = true;
+        String move;
+
+        for (int i = 0; i < size; i++) {
+            Announcement.move();
+            move = InputView.readMoving();
+            success = BridgeGame.move(bridge, move, i);
+            OutputView.printMap(BridgeGame.getUp_bridge(), BridgeGame.getDown_bridge());
+            if (success == false)
+                break;
+        }
+        return success;
+    }
+    public static String ask_retry(){
+        Announcement.restart();
+        return InputView.readGameCommand();
+    }
+    public static int retry_cross(String re, int try_count){
+        if(re.contains("R")) {
+            try_count += 1;
+            BridgeGame.retry();
+        }
+        return try_count;
+    }
+    public static void print_result(boolean success, int try_count){
+        Announcement.bridge_result();
+        OutputView.printMap(BridgeGame.getUp_bridge(), BridgeGame.getDown_bridge());
+        OutputView.printResult(success, try_count);
     }
 }
