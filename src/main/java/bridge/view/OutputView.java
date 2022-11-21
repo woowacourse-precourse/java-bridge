668774c65b7e6c5b21483a15d1dc75f952113af4
josolha <josolha@nate.com>
2022-11-22 00:55:27 +0900

    feat(OutputView) : 사용자 다리 이동 후 출력 메시지 생성

---
+++ src/main/java/bridge/view/OutputView.java
@@ -0,0 +1,49 @@
+package bridge.view;
+
+import static bridge.constants.OutputConstants.END;
+import static bridge.constants.OutputConstants.MIDDLE;
+import static bridge.constants.OutputConstants.START;
+
+/**
+ * 사용자에게 게임 진행 상황과 결과를 출력하는 역할을 한다.
+ */
+public class OutputView {
+
+    private StringBuffer upSb = new StringBuffer();
+    private StringBuffer downSb = new StringBuffer();
+
+    /**
+     * 현재까지 이동한 다리의 상태를 정해진 형식에 맞춰 출력한다.
+     * <p>
+     * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
+     */
+    public void printMap(boolean moveCheck, String userMove, int location) {
+
+        if (location == 0) {
+            if (userMove.equals("U")) {
+                upSb.append(START.getMessage() + " " + moveResult + " " + END.getMessage());
+                downSb.append(START.getMessage() + "   " + END.getMessage());
+            }
+            if (userMove.equals("D")) {
+                upSb.append(START.getMessage() + "   " + END.getMessage());
+                downSb.append(START.getMessage() + " " + moveResult + " " + END.getMessage());
+            }
+        } else {
+
+            if (userMove.equals("U")) {
+                upSb.deleteCharAt(upSb.length() - 1);
+                upSb.append(MIDDLE.getMessage() + " " + moveResult + " " + END.getMessage());
+                downSb.deleteCharAt(downSb.length() - 1);
+                downSb.append(MIDDLE.getMessage() + "   " + END.getMessage());
+            }
+            if (userMove.equals("D")) {
+                upSb.deleteCharAt(upSb.length() - 1);
+                upSb.append(MIDDLE.getMessage() + "   " + END.getMessage());
+                downSb.deleteCharAt(downSb.length() - 1);
+                downSb.append(MIDDLE.getMessage() + " " + moveResult + " " + END.getMessage());
+
+            }
+        }
+        System.out.println(upSb + "\n" + downSb);
+    }
+}
