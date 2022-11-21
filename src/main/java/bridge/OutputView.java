9f2255942efb022605874e92e9d5245471e48d8d
조미정 <ing06164@naver.com>
2022-11-21 21:32:27 +0900

    feat: 시작문구 출력 - printStart()

--- src/main/java/bridge/OutputView.java
+++ src/main/java/bridge/OutputView.java
@@ -5,6 +5,10 @@ package bridge;
  */
 public class OutputView {
 
+    public void printStart(){
+        System.out.println("* 다리 건너기 게임을 시작합니다. *");
+        System.out.println("다리의 길이를 입력해주세요.");
+    }
     /**
      * 현재까지 이동한 다리의 상태를 정해진 형식에 맞춰 출력한다.
      * <p>
@@ -19,5 +23,9 @@ public class OutputView {
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
     public void printResult() {
+        System.out.println("최종 게임 결과");
+        printMap();
+        System.out.println("게임 성공 여부: ");
+        System.out.println("총 시도한 횟수: ");
     }
 }
