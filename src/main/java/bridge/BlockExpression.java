9002101b77f1a03b0ba8add43eb3d302d47e8440
devMuscle <hongsbien@naver.com>
2022-11-22 02:21:59 +0900

    feat: 브릿지의 한 칸을 표현하는 Enum

---
+++ src/main/java/bridge/BlockExpression.java
@@ -0,0 +1,17 @@
+package bridge;
+
+public enum BlockExpression {
+    Same("O"),
+    Diff("X"),
+    Empty(" ");
+
+    private final String mark;
+
+    private BlockExpression(String mark) {
+        this.mark = mark;
+    }
+
+    public String getMark() {
+        return mark;
+    }
+}
