d68ec369341c5c3fc70f0990c9bd15e1f0baa2a6
dooboocookie <bkh3030@gmail.com>
2022-11-21 17:11:12 +0900

    test: Bridge 밟을 수 있는 발판 여부와 똑같은 경로와 브릿지 비교하는 테스트

--- src/test/java/bridge/model/BridgeTest.java
+++ src/test/java/bridge/model/BridgeTest.java
@@ -1,14 +1,31 @@
 package bridge.model;
 
 import bridge.model.Bridge;
+import camp.nextstep.edu.missionutils.Randoms;
 import org.assertj.core.api.Assertions;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
+import org.mockito.MockedStatic;
+import org.mockito.Mockito;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class BridgeTest {
+    MockedStatic<Randoms> randomsMockedStatic;
+
+    @BeforeEach
+    void beforeAll() {
+        randomsMockedStatic = Mockito.mockStatic(Randoms.class);
+        Mockito.when(Randoms.pickNumberInRange(0,1)).thenReturn(1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0);
+    }
+
+    @AfterEach
+    void afterEach() {
+        randomsMockedStatic.close();
+    }
 
     @DisplayName("Bridge 객체 생성 테스트")
     @Test
@@ -30,5 +47,31 @@ public class BridgeTest {
                 .isInstanceOf(IllegalArgumentException.class);
         assertThatThrownBy(() -> new Bridge(21))
                 .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("도착한 곳이 건널 수 있는 곳인지 아닌지 체크하는 메소드")
+    @Test
+    void possibleNextStepTest() {
+        Bridge bridge = new Bridge(20);
+
+        boolean indexZeroIsUp = bridge.possibleNextStep(0, Plate.UP_PLATE);
+        boolean indexZeroIsNotDown = bridge.possibleNextStep(0, Plate.DOWN_PLATE);
+
+        assertThat(indexZeroIsUp).isTrue();
+        assertThat(indexZeroIsNotDown).isFalse();
+    }
+
+    @DisplayName("플레이어 경로와 다리를 비교하는 테스트")
+    @Test
+    void sameAsTest() {
+        Bridge bridge = new Bridge(20);
+
+        List<Plate> playerPath = new ArrayList<>();
+        for (int i = 0; i < 10; i++) {
+            playerPath.add(Plate.UP_PLATE);
+            playerPath.add(Plate.DOWN_PLATE);
+        }
+
+        assertThat(bridge.sameAs(playerPath)).isTrue();
     }
 }
