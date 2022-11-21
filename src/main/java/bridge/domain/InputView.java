5f074400d7d098081454924409535c9359846877
kimdahee0815 <diana815@naver.com>
2022-11-21 15:48:10 +0900

    refactoring(readMoving) : readMoving 함수의 while문을 do-while문으로 수정하여 함수의 길이를 줄인다

--- src/main/java/bridge/domain/InputView.java
+++ src/main/java/bridge/domain/InputView.java
@@ -9,7 +9,7 @@ import camp.nextstep.edu.missionutils.Console;
 public class InputView {
 	private int bridgeLength = 0;
 	private String restartOrQuit = "";
-
+	private String upOrDown = "";
 	/**
 	 * 다리의 길이를 입력받는다.
 	 */
@@ -43,8 +43,7 @@ public class InputView {
 	 * 사용자가 이동할 칸을 입력받는다.
 	 */
 	public String readMoving() {
-		String upOrDown = "";
-		while (!upOrDown.equals("U") && !upOrDown.equals("D")) {
+		do  {
 			System.out.println("\n이동할 칸을 선택해주세요. (위: U, 아래: D)");
 			upOrDown = Console.readLine();
 			try {
@@ -52,7 +51,7 @@ public class InputView {
 			} catch (IllegalArgumentException e) {
 				System.out.println("[ERROR] U와 D만 선택하세요.");
 			}
-		}
+		} while(!upOrDown.equals("U") && !upOrDown.equals("D"));
 		return upOrDown;
 	}
 
