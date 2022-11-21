a53d56cadef691f4164e8f1530fe08a79961706d
yongseungLeeDec <yonson.dev@gmail.com>
2022-11-21 19:48:34 +0900

    fix(delete): OutputView 클래스에 속하지 않는 기능 삭제
    OutputView 클래스가 게임의 중간 및 결과(output)만 출력하는 클래스로 기능할 수 있도록 관련 없는 메소드를 모두 삭제함.

--- src/main/java/bridge/console/OutputView.java
+++ src/main/java/bridge/console/OutputView.java
@@ -1,20 +1,6 @@
 package bridge.console;
 
 public class OutputView {
-    private static final String TEXT_GAME_START = "다리 건너기 게임을 시작합니다.";
-    private static final String PROMPT_FOR_BRIDGE_SIZE = "다리의 길이를 입력해주세요.";
-    private static final String PROMPT_FOR_MOVE = "이동할 칸을 선택해주세요. (위: U, 아래: D)";
 
-    public void printGameStart() {
-        System.out.println(TEXT_GAME_START);
-    }
-
-    public void printPromptForBridgeSize() {
-        System.out.println(PROMPT_FOR_BRIDGE_SIZE);
-    }
-
-    public void printPromptForMove() {
-        System.out.println(PROMPT_FOR_MOVE);
-    }
 }
 
