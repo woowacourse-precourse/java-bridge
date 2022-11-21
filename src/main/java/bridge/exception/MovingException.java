ca0891eebeb42a12fcf3a682ce334cf960672e6d
NAM GIBEOM <rlqja0523@naver.com>
2022-11-21 21:38:41 +0900

    feat: U 또는 D인지 확인하는 예외 메서드 추가

--- src/main/java/bridge/exception/MovingException.java
+++ src/main/java/bridge/exception/MovingException.java
@@ -1,10 +1,19 @@
 package bridge.exception;
 
+import bridge.service.UserInput;
+
 public class MovingException extends UserException {
     @Override
     public void checkException(String userInput) {
         isEmpty(userInput);
         isBlank(userInput);
         hasBlank(userInput);
+        isNotMoving(userInput);
+    }
+
+    private void isNotMoving(String moving) {
+        if (!UserInput.MOVING.getLetters().contains(moving)) {
+            throw new IllegalArgumentException(ExceptionCode.IS_NOT_MOVING.getMessage());
+        }
     }
 }
