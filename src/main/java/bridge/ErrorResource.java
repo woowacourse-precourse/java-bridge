283c0dcfa0d6084b39a962701b0dd1224ea8a22c
starwook <star.wookk@gmail.com>
2022-11-21 20:21:40 +0900

    refactor(ErrorResource.java): Refactor ErrorResource.java
    에러 스태틱변수들을 조금 더 나누기

--- src/main/java/bridge/ErrorResource.java
+++ src/main/java/bridge/ErrorResource.java
@@ -3,8 +3,9 @@ package bridge;
 public class ErrorResource {
 
     public static final String ERROR_START="[ERROR] ";
-    public static final String INPUT_BRIDGE_NUMBER_WRONG = "다리 길이는 3부터 20사이의 숫자여야 합니다.";
-    public static final String INPUT_NOT_BRIDGE_NUMBER = "다리 길이는 숫자여야합니다.";
+    public static final String BRIDGE_SIZE_FRONT = "다리 길이는 ";
+    public static final String TO = "부터";
+    public static final String NOT_NUMBER = "숫자여야합니다.";
     public static final String INPUT_MOVE_WRONG_FRONT = "이동할 칸은 ";
     public static final String OR = "또는";
     public static final String INPUT_SHOULD_BACK = "여야 합니다.";
