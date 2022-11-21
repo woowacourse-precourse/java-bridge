f801aafb800c6203c84a551f5aac541a1424f11f
SHEOMM <esh0821@snu.ac.kr>
2022-11-22 02:25:12 +0900

    refactor: 불필요한 에러 메세지 통합

--- src/main/java/bridge/model/ErrorMessage.java
+++ src/main/java/bridge/model/ErrorMessage.java
@@ -2,9 +2,6 @@ package bridge.model;
 
 public enum ErrorMessage {
 
-
-    NULL_INPUT_ERROR("값을 입력해주세요"),
-    NOT_NUMERIC_VALUE_ERROR("[ERROR] 숫자인 값을 입력해주세요."),
     INVALID_NUMERIC_VALUE("[ERROR] 3 이상 20 이하의 값을 입력해주세요"),
     INVALID_DIRECTION_VALUE("[ERROR] U 혹은 D를 입력해주세요."),
     INVALID_RESTART_VALUE("[ERROR] R 혹은 Q를 입력해주세요.");
