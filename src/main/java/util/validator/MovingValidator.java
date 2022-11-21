80c126aae60425c58066ed3463befed834a856c2
nylonmusk <106235834+nylonmusk@users.noreply.github.com>
2022-11-21 20:12:53 +0900

    refactor(MovingValidator): 가독성 향상을 위해 Constant를 클래스변수로 선언

--- src/main/java/util/validator/MovingValidator.java
+++ src/main/java/util/validator/MovingValidator.java
@@ -5,21 +5,26 @@ import constant.InputConstant;
 import constant.NumberConstant;
 
 public class MovingValidator {
+    private static final int ONE_LETTER = NumberConstant.ONE_LETTER.getConstant();
+    private static final String UP_SIDE = InputConstant.UP_SIDE.getConstant();
+    private static final String DOWN_SIDE = InputConstant.DOWN_SIDE.getConstant();
+    private static final String INPUT_LENGTH = ErrorConstant.INPUT_LENGTH.getConstant();
+    private static final String MOVE_INVALID = ErrorConstant.MOVE_INVALID.getConstant();
+
     public static void isValid(String moving) {
         isOneLetter(moving);
         isEqualsUOrD(moving);
     }
 
     private static void isOneLetter(String moving) {
-        if (moving.length() != NumberConstant.ONE_LETTER.getConstant()) {
-            throw new IllegalArgumentException(ErrorConstant.INPUT_LENGTH.getConstant());
+        if (moving.length() != ONE_LETTER) {
+            throw new IllegalArgumentException(INPUT_LENGTH);
         }
     }
 
     private static void isEqualsUOrD(String moving) {
-        if (!moving.equals(InputConstant.UP_SIDE.getConstant())
-                && !moving.equals(InputConstant.DOWN_SIDE.getConstant())) {
-            throw new IllegalArgumentException(ErrorConstant.MOVE_INVALID.getConstant());
+        if (!moving.equals(UP_SIDE) && !moving.equals(DOWN_SIDE)) {
+            throw new IllegalArgumentException(MOVE_INVALID);
         }
     }
 }
