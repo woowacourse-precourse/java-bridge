2e522acf77fabb6ca334d072b4d9048ba146d54e
ensk26 <tjensk26@gmail.com>
2022-11-21 17:00:35 +0900

    feat(BridgeGameTest): 다리 결과값을 저장, 재시도 테스트 추가
    새로운 테스트 추가 to BridgeGameTest
    - 다리 결과 값을 저장한 다리 결과가 올바른지 확인
    - 다리 게임 재시도 시 저장된 다리 결과가 올바른지 확인

--- src/test/java/bridge/BridgeGameTest.java
+++ src/test/java/bridge/BridgeGameTest.java
@@ -17,4 +17,30 @@ class BridgeGameTest {
         String direct = "U";
         assertThat(bridgeGame.move(direct)).isTrue();
     }
+
+    @DisplayName("다리의 결과값이 조건에 따라 저장이 되는지 확인")
+    @Test
+    void 다리_결과값_저장_테스트() {
+        List<String> bridge = List.of("U", "D", "D");
+        BridgeGame bridgeGame = new BridgeGame(bridge);
+        String direct = "U";
+        boolean result = true;
+        bridgeGame.bridgeMark(direct, result);
+        assertThat(bridgeGame.getBridgeMap()).containsExactly(List.of("O"), List.of(""));
+    }
+
+    @DisplayName("다리 게임 재시도 시 이전 다리 결과값이 되는지 확인")
+    @Test
+    void 다리_게임_재시도_다리_결과값_테스트() {
+        List<String> bridge = List.of("U", "D", "D");
+        BridgeGame bridgeGame = new BridgeGame(bridge);
+        List<String> direct = List.of("U", "D", "U");
+        List<Boolean> result = List.of(true, true, false);
+
+        for (int i = 0; i < result.size(); i++) {
+            bridgeGame.bridgeMark(direct.get(i), result.get(i));
+        }
+        bridgeGame.retry();
+        assertThat(bridgeGame.getBridgeMap()).containsExactly(List.of("O", ""), List.of("", "O"));
+    }
 }
