2ba0d475011ef3f8d24f9a4580e7944668ef5d2f
jwPark6 <67006945+jwPark6@users.noreply.github.com>
2022-11-22 01:14:14 +0900

    feat : 실패여부 확인 구현

--- src/main/java/bridge/BridgeGame.java
+++ src/main/java/bridge/BridgeGame.java
@@ -56,6 +56,13 @@ public class BridgeGame {
         return false;
     }
 
+    public boolean isFail(){
+        if(firstLine.contains("X") || secondLine.contains("X")){
+            return true;
+        }
+        return false;
+    }
+
     /**
      * 사용자가 게임을 다시 시도할 때 사용하는 메서드
      * <p>
