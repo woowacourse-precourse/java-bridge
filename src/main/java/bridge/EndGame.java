a5dd831e1503e0be9607842ee27b345c906b3600
w0ntae <bitcampcoding@gmail.com>
2022-11-21 11:26:55 +0900

    feat(endgame): Add endgame
    Add method to detect end of game

--- src/main/java/bridge/EndGame.java
+++ src/main/java/bridge/EndGame.java
@@ -1,7 +1,18 @@
 package bridge;
 
 public class EndGame {
-  public static boolean endGame(){
   
+  private final Count count;
+  
+  public EndGame(Count count) {
+    this.count = count;
+  }
+  
+  public boolean endGame(int size){
+    int steps = count.getSteps();
+    if(steps == size){
+      return true;
+    }
+    return false;
   }
 }
