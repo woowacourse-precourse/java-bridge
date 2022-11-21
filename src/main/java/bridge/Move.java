277f3b7b93da3af7955f35f2de3f921aea473501
Ilchul Kim <huj0159@gmail.com>
2022-11-21 22:46:43 +0900

    feat: make static move constructor and check validation of parameter

--- src/main/java/bridge/Move.java
+++ src/main/java/bridge/Move.java
@@ -1,5 +1,7 @@
 package bridge;
 
+import java.util.regex.Pattern;
+
 public enum Move {
     D(1, "D"),
     U(0, "U");
@@ -7,9 +9,20 @@ public enum Move {
     private final int order;
     private final String location;
 
-    Move(int order, String location) {
+    private Move(int order, String location) {
         this.order = order;
         this.location = location;
+    }
+
+    public static Move createMove(String move){
+        isValidMoving(move);
+        return Move.valueOf(move);
+    }
+
+    private static void isValidMoving(String userInput) {
+        if (!Pattern.matches("^[DU]{1}$", userInput)) {
+            throw new IllegalArgumentException("[ERROR] D 혹은 U 한 문자를 입력해야 합니다");
+        }
     }
 
     public String getLocation() {
