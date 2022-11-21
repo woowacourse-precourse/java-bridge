ac442ff2dbfd84137f3844488b2cd89389b529d1
KangSungBum <peter9706@naver.com>
2022-11-21 17:50:48 +0900

    refactor(Application.java): refactor application code
    각 입력, 출력, 기능 별 모듈화
    - start() 함수를 여러 함수로 세분화
    - gameInitProcess(), gameProcess(), gameCommandProcess() 등

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -1,67 +1,89 @@
 package bridge;
 
-import response.BridgeGameException;
 import response.UserInputException;
 import view.InputView;
 import view.OutputView;
 
-import java.util.List;
-
 public class Application {
 
     private static BridgeGame game = new BridgeGame();
-    private static int trialCount = 0;
     private static InputView inputView = new InputView();
     private static OutputView outputView = new OutputView();
 
     public static void main(String[] args) {
         try {
-            startGame();
+//            startGame();
+            start();
+//            List<String> answerBridge = Arrays.asList("U", "D", "D");
+//            List<String> userBridge = Arrays.asList("U", "D", "U");
+//
+//            outputView.printMap(answerBridge, userBridge);
+
         } catch (UserInputException inputException) {
             System.out.println(inputException.getMessage());
-        } catch (BridgeGameException gameException) {
-            System.out.println(gameException.getMessage());
+        } catch (IllegalStateException e) {
+
         }
     }
 
-    private static void startGame() {
+    private static void start() {
         game.trialCount++;
+
+        gameInitProcess();
+
+        gameProcess();
+
+    }
+
+    private static void gameInitProcess() {
         outputView.printGameStartMessage();
-        List<String> generatedBridge = generateBridge();
-        if (!bridgeGameProcess(generatedBridge)) {
-            outputView.printExitCodeRequestMessage();
-            String exitCode = inputView.readGameCommand();
-            if (exitCode.equals("Q")) {
-                return;
-            }
-            game.retry();
-            bridgeGameProcess(generatedBridge);
+        int bridgeSize = getBridgeSize();
+        game.initBridge(bridgeSize);
+    }
+
+    private static void gameProcess() {
+        outputView.printMovementRequestMessage();
+        String movement = inputView.readMoving();
+
+        if (!game.move(movement)) {
+            outputView.printMap(game.bridge, game.userInput);
+            gameCommandProcess();
         }
 
-        outputView.printSuccessResult(generatedBridge, game.trialCount);
+        if (game.userInput.size() == game.bridge.size()) {
+            outputView.printResult(game.bridge, game.userInput, true, game.trialCount);
+            throw new IllegalStateException();
+        }
 
+        outputView.printMap(game.bridge, game.userInput);
+        gameProcess();
     }
 
-    private static List<String> generateBridge() {
-        int bridgeSize = getBridgeSize();
-        return game.initBridge(bridgeSize);
+    private static void gameCommandProcess() {
+        outputView.printExitCodeRequestMessage();
+        String exitCode = inputView.readGameCommand();
+        if (exitCode.equals("R")) {
+            resetProcess();
+            return;
+        }
+
+        quitProcess();
     }
+
+    private static void resetProcess() {
+        game.retry();
+        gameProcess();
+    }
+
+    private static void quitProcess() {
+        outputView.printResult(game.bridge, game.userInput, false, game.trialCount);
+        throw new IllegalStateException();
+    }
+
 
     private static int getBridgeSize() {
         outputView.printBridgeSizeRequestMessage();
         return inputView.readBridgeSize();
     }
 
-    private static boolean bridgeGameProcess(List<String> bridge) {
-        boolean success = true;
-        for (int i = 0; i < bridge.size(); i++) {
-            outputView.printMovementRequestMessage();
-            if (!game.move(bridge, i)) {
-                success = false;
-                break;
-            }
-        }
-
-        return success;
-    }
 }
