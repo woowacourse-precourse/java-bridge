bb1dcc87d8487a6c0ed68af58467817fed742636
Jaewoong Kim <kimje1109@gmail.com>
2022-11-21 13:17:12 +0900

    feat: 다리 길이를 입력받는 기능 구현

--- src/main/java/bridge/InputView.java
+++ src/main/java/bridge/InputView.java
@@ -1,27 +1,21 @@
 package bridge;
 
-/**
- * 사용자로부터 입력을 받는 역할을 한다.
- */
+import camp.nextstep.edu.missionutils.Console;
+
 public class InputView {
 
-    /**
-     * 다리의 길이를 입력받는다.
-     */
+    private final Validate validate = new Validate();
+
     public int readBridgeSize() {
-        return 0;
+        String input = Console.readLine();
+        validate.isBridgeSize(input);
+        return Integer.parseInt(input);
     }
 
-    /**
-     * 사용자가 이동할 칸을 입력받는다.
-     */
     public String readMoving() {
         return null;
     }
 
-    /**
-     * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
-     */
     public String readGameCommand() {
         return null;
     }
---
+++ src/main/java/bridge/Validate.java
@@ -0,0 +1,27 @@
+package bridge;
+
+import java.util.regex.Pattern;
+
+public class Validate {
+    private final static String NUMERIC_PATTERN = "^[0-9]*$";
+    private final static int MAX_BRIDGE_SIZE = 20;
+    private final static int MIN_BRIDGE_SIZE = 3;
+
+    private boolean isNumeric(String input) {
+        return Pattern.matches(NUMERIC_PATTERN, input);
+    }
+
+    private boolean isBridgeSizeRange(String input) {
+        if (isNumeric(input)){
+            return (MIN_BRIDGE_SIZE <= Integer.parseInt(input)
+                    && Integer.parseInt(input) <= MAX_BRIDGE_SIZE);
+        }
+        return false;
+    }
+
+    public void isBridgeSize(String input){
+        if (!isBridgeSizeRange(input)) {
+            throw new IllegalArgumentException();
+        }
+    }
+}
