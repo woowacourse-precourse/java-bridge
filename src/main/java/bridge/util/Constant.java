c6b411ebbd6638b8971efe666649a51e3f357758
SeokHwan An <asok369258@naver.com>
2022-11-21 21:00:38 +0900

    feat : 게임 최종결과를 출력하는 기능 추가

--- src/main/java/bridge/util/Constant.java
+++ src/main/java/bridge/util/Constant.java
@@ -18,6 +18,9 @@ public class Constant {
     public static final String MOVE_INPUT_MSG = "이동할 칸을 선택해주세요. (위: U, 아래: D)";
     public static final String GAME_RESTART_MSG = "게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)";
     public static final String GAME_START_MSG = "다리 건너기 게임을 시작합니다.";
+    public static final String GAME_RESULT_MSG = "최종 게임 결과";
+    public static final String GAME_SUCCESS_OR_FAIL_MSG = "게임 성공 여부: ";
+    public static final String TOTAL_PLAY_COUNT_MSG = "총 시도한 횟수: ";
     public static final String SUCCESS = "성공";
     public static final String FAIL = "실패";
 
