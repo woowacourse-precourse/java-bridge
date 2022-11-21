1de10144ca2cd7356a9c2b1239a22ffbeec3c93c
Ran <lan4250@naver.com>
2022-11-21 16:51:00 +0900

    FEAT/ List<String>다리 만들기

--- src/main/java/bridge/BridgeMaker.java
+++ src/main/java/bridge/BridgeMaker.java
@@ -1,5 +1,7 @@
 package bridge;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 /**
@@ -16,20 +18,77 @@ public class BridgeMaker {
     /**
      * @param size 다리의 길이
      * @return 입력받은 길이에 해당하는 다리 모양. 위 칸이면 "U", 아래 칸이면 "D"로 표현해야 한다.
+     * <p>
      */
     public List<String> makeBridge(int size) {
 
-        // 숫자가 아니거나
-        if (!Character.isDigit(size)) {
-            throw new IllegalArgumentException("[ERROR] 숫자만 입력 가능합니다.");
+        List<String> bridge = new ArrayList<>();
 
-        }
         if (size < 3 || size > 20) {
             throw new IllegalArgumentException("[ERROR] 3 이상 20 이하의 숫자만 입력 가능합니다.");
 
         }
 
+        String str = "";
 
-        return null;
+        for (int i = 0; i < size; i++) {
+            str += bridgeNumberGenerator.generate();
+        }
+        System.out.println(" 다리 " + str);
+
+        int[][] arr = new int[2][str.length()];
+        for (int i = 0; i < str.length(); i++) {
+            if (str.charAt(i) == '0') {
+                arr[1][i] = 1;
+            } else if (str.charAt(i) == '1') {
+                arr[0][i] = 1;
+            }
+        }
+
+        for (int[] a : arr) {
+            System.out.println(Arrays.toString(a));
+        }
+
+
+        return change(str);
     }
+
+
+    public static List<String> change(String bridgeStr) {
+
+        List<String> result = new ArrayList<>();
+        String up = "[";
+        String down = "[";
+
+        for (int i = 0; i < bridgeStr.length(); i++) {
+
+            if (bridgeStr.charAt(i) == '1') {
+
+                if (i == bridgeStr.length() - 1) {
+                    up += " O ";
+                    down += "   ";
+                    continue;
+                }
+                up += " O |";
+                down += "   |";
+            } else if (bridgeStr.charAt(i) == '0') {
+
+                if (i == bridgeStr.length() - 1) {
+                    down += " O ";
+                    up += "   ";
+                    continue;
+                }
+                down += " O |";
+                up += "   |";
+            }
+        }
+
+        up += "]";
+        down += "]";
+        result.add(up);
+        result.add(down);
+        return result;
+
+    }
+
 }
