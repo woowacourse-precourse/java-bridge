f7b797ae4e11f9fe445cac308be338629dc956a3
devMuscle <hongsbien@naver.com>
2022-11-22 02:23:26 +0900

    feat: 현재까지 이동한 다리의 상태 출력

--- src/main/java/bridge/OutputView.java
+++ src/main/java/bridge/OutputView.java
@@ -1,16 +1,41 @@
 package bridge;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 /**
  * 사용자에게 게임 진행 상황과 결과를 출력하는 역할을 한다.
  */
 public class OutputView {
+
+    private final BridgeChecker bridgeChecker;
+
+    public OutputView(BridgeChecker bridgeChecker) {
+        this.bridgeChecker = bridgeChecker;
+    }
 
     /**
      * 현재까지 이동한 다리의 상태를 정해진 형식에 맞춰 출력한다.
      * <p>
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void printMap() {
+    public void printMap(List<String> bridge, List<String> userInput) {
+        List<String> matchingResult = bridgeChecker.checkBridgeMatching(bridge, userInput);
+
+        printUpperBlocks(matchingResult, userInput);
+        printLowerBlocks(matchingResult, userInput);
+    }
+
+    public void printUpperBlocks(List<String> matchingResult, List<String> bridge) {
+        List<String> upperBlocks = bridgeChecker.checkUpperBlocks(matchingResult, bridge);
+
+        System.out.println(upperBlocks.stream().collect(Collectors.joining(" | ", "[ ", " ]")));
+    }
+
+    public void printLowerBlocks(List<String> matchingResult, List<String> bridge) {
+        List<String> lowerBlocks = bridgeChecker.checkLowerBlocks(matchingResult, bridge);
+
+        System.out.println(lowerBlocks.stream().collect(Collectors.joining(" | ", "[ ", " ]")));
     }
 
     /**
