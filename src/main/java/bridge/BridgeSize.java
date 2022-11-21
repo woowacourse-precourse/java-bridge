aa24a8c4639ccbbee526e2a24327a60f3de2a031
Amyhds <dasomhan524@gmail.com>
2022-11-21 18:42:27 +0900

    refactor(bridgesize): 다시 생성자 구현 bridgesize는 한번만 사용되기 때문에 인스턴스로 사용하는 것이 낫다고 판단

--- src/main/java/bridge/BridgeSize.java
+++ src/main/java/bridge/BridgeSize.java
@@ -1,16 +1,16 @@
 package bridge;
 
 public class BridgeSize {
-    private static int bridgeSize;
+    private int bridgeSize;
 
-    public static void validateInput(String userBridgeSize) {
+    public BridgeSize(String userBridgeSize) {
         Validator.validateNumber(userBridgeSize);
         int tempBridgeSize = Converter.convertToNumber(userBridgeSize);
         Validator.validateRange(tempBridgeSize);
         bridgeSize = tempBridgeSize;
     }
 
-    public static int getBridgeSize() {
+    public int getBridgeSize() {
         return bridgeSize;
     }
 }
