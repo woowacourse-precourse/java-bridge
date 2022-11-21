0db1a99d33d69db2b9d18c96077571a70fbe3a71
gomljo <gomljo0307@naver.com>
2022-11-22 02:55:21 +0900

    feat(GameRecordMaker): 다리 횡단 상태를 출력형태로 변환하는 기능 구현

--- src/main/java/bridge/domain/GameRecordMaker.java
+++ src/main/java/bridge/domain/GameRecordMaker.java
@@ -23,12 +23,12 @@ public class GameRecordMaker {
         return recordLength==ZERO;
     }
 
-    public String makeFormat(boolean isFirst, String record){
+    private String makeFormat(boolean isFirst, String record){
         GameRecordFormat gameRecordFormat = GameRecordFormat.findByTrial(isFirst);
         return gameRecordFormat.generate(record);
     }
 
-    public List<String> transformCurrentRecord(boolean isFirst, List<String> gameRecord){
+    private List<String> transformCurrentRecord(boolean isFirst, List<String> gameRecord){
         List<String> transformedRecord = new ArrayList<>();
         for(String record: gameRecord){
             String formattedRecord = makeFormat(isFirst, record);
@@ -40,7 +40,8 @@ public class GameRecordMaker {
     public void updateCurrentRecord(List<String> gameRecord){
         boolean isFirst = isRecordLengthOverOne();
         currentGameRecord=transformCurrentRecord(isFirst, gameRecord);
-        ++recordLength;
+        System.out.println(recordLength);
+
     }
 
     public void updateResult(){
@@ -48,6 +49,16 @@ public class GameRecordMaker {
             List<String> result= TOTAL_RESULT.get(i);
             result.add(currentGameRecord.get(i));
         }
+        recordLength+=1;
+    }
+
+    public String makeMapFormat(List<String> result){
+        StringBuilder formattedResult = new StringBuilder();
+        formattedResult.append(GameRecordSymbol.START_BRACKET.getSymbol());
+        formattedResult.append(String.join(GameRecordSymbol.BLANK.getSymbol(), result));
+        formattedResult.append(GameRecordSymbol.END_BRACKET.getSymbol());
+        formattedResult.append(GameRecordSymbol.NEW_LINE.getSymbol());
+        return formattedResult.toString();
     }
 
 }
