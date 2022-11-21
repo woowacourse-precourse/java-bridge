1cbb4361e793a5ecf01713c673acfc80f18e873c
es <mascoeskim@gmail.com>
2022-11-21 19:45:10 +0900

    refactor(Application): try/catch 적용

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -4,23 +4,31 @@ public class Application {
 
     public static void main(String[] args) {
         // TODO: 프로그램 구현
-        int size = InputView.readBridgeSize();
-        BridgeGame game = new BridgeGame();
-        game.makeBridge(size);
-        while(true){
+        try {
+            int size = InputView.readBridgeSize();
+            BridgeGame game = new BridgeGame();
+            game.makeBridge(size);
+            bridgeGameStart(game);
+        }catch (IllegalArgumentException e) {
+            System.out.println(e);
+        }
+    }
+
+    private static void bridgeGameStart(BridgeGame game) {
+        while (true) {
             while (!game.gameOver && !game.completeBridge) {
                 String moving = InputView.readMoving();
                 game.move(moving);
             }
-            if (game.completeBridge){
+            if (game.completeBridge) {
                 break;
             }
             String restart = InputView.readGameCommand();
             game.retry(restart);
-            if (game.retry){
+            if (game.retry) {
                 game.gameOver = false;
             }
-            if (!game.retry){
+            if (!game.retry) {
                 break;
             }
         }
--- src/main/java/bridge/Validator.java
+++ src/main/java/bridge/Validator.java
@@ -1,28 +1,31 @@
 package bridge;
 
+import java.util.NoSuchElementException;
 import java.util.Objects;
 
 public class Validator {
 	public static void validateBridgeSize (String bridgeSize){
-		try {
+
+		try{
 			int parseIntBridgeSize = Integer.parseInt(bridgeSize);
 			if(parseIntBridgeSize < 3 || parseIntBridgeSize >20){
-				throw new IllegalArgumentException();
+				throw new IllegalArgumentException(ErrorMessage.errorMessage.ONLY_INPUT_BETWEEN_3_20.getValue());
 			}
-		}catch(Exception e){
-			throw e;
+		}catch (NumberFormatException e){
+			System.out.println("들어옴");
+			throw new IllegalArgumentException(ErrorMessage.errorMessage.ONLY_INPUT_INTEGER.getValue());
 		};
-	}
 
+	}
 	public static void validateMoving (String moving){
 		if(!Objects.equals(moving, "U") && !Objects.equals(moving, "D")){
-			throw new IllegalArgumentException();
+			throw new IllegalArgumentException(ErrorMessage.errorMessage.ONLY_INPUT_U_AND_D.getValue());
 		}
 	}
 
 	public static void validateIsRestart (String isRestart){
 		if(!Objects.equals(isRestart, "R") && !Objects.equals(isRestart, "Q")){
-			throw new IllegalArgumentException();
+			throw new IllegalArgumentException((ErrorMessage.errorMessage.ONLY_INPUT_R_AND_Q.getValue()));
 		}
 	}
 }
