174561711d5d7d01d849a065280fa11f4711210c
WonSeok-dd <cluejws@gmail.com>
2022-11-21 19:44:48 +0900

    feat(BridgeGameController): 다리 길이 입력 및 다리 생성 기능 구현 - 기능 목록(1) - 기능 목록(2)

--- src/main/java/bridge/controller/BridgeGameController.java
+++ src/main/java/bridge/controller/BridgeGameController.java
@@ -1,10 +1,12 @@
 package bridge.controller;
 
+import bridge.InputView;
 import bridge.OutputView;
+import bridge.domain.BridgeGame;
 
 public class BridgeGameController {
-
     private int trial = 0;
+    private BridgeGame bridgeGame;
 
     public void run(){
         init();
@@ -13,9 +15,20 @@ public class BridgeGameController {
     private void init(){
         OutputView.printStart();
         initTrial();
+        makeBridge();
     }
 
     private void initTrial(){
         trial += 1;
+    }
+
+    private void makeBridge(){
+        try {
+            int size = InputView.readBridgeSize();
+            bridgeGame = new BridgeGame(size);
+
+        }catch (IllegalArgumentException i){
+            System.out.println(i.getMessage());
+        }
     }
 }
