1efc287af798d78ff97eba601db209e01e3f8707
HamSeungChan <head0618@naver.com>
2022-11-21 12:03:59 +0900

    feat : 이동 가능 여부 확인하는 기능 구현

---
+++ src/main/java/bridge/Move.java
@@ -0,0 +1,26 @@
+package bridge;
+
+import java.util.List;
+
+public class Move {
+    private static final int START = 0;
+    
+    private final List<String> bridge;
+    private int countNumberOfMove;
+
+    Move(List<String> bridge) {
+        this.bridge = bridge;
+        init();
+    }
+
+    private void init() {
+        countNumberOfMove = START;
+    }
+
+    public boolean canMove(String moveDirection) {
+        if (bridge.get(countNumberOfMove++) == moveDirection) {
+            return true;
+        }
+        return false;
+    }
+}
