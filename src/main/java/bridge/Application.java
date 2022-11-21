3103d7bf1cdac7b7e1872acc124a7dd52dc8d087
Kijung Park <kjpark4321@naver.com>
2022-11-21 23:57:28 +0900

    feat: 프로그램 main 메서드 구현

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -3,8 +3,7 @@ package bridge;
 public class Application {
 
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
-        BridgeMaker bridgeMaker = new BridgeMaker(new BridgeRandomNumberGenerator());
-        System.out.println(bridgeMaker.makeBridge(3));
+        BridgeGame bridgeGame = new BridgeGame();
+        bridgeGame.startGame();
     }
 }
