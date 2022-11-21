64d8a76894976a65ead19dc73c405f8013c413da
woosung1223 <dntjd991223@naver.com>
2022-11-21 11:50:14 +0900

    refactor(GameUser): 브리지에서 무빙 검증을 수행하기에 GameUser에서의 검증은 제거

--- src/main/java/bridge/domain/GameUser.java
+++ src/main/java/bridge/domain/GameUser.java
@@ -1,16 +1,11 @@
 package bridge.domain;
 
-import bridge.verifier.MovingVerifier;
-
 import java.util.ArrayList;
 
 public class GameUser {
     private final Bridge userBridge = new Bridge(new ArrayList<>());
 
     public void move(String moving) {
-        MovingVerifier movingVerifier = new MovingVerifier();
-        movingVerifier.check(moving);
-        
         userBridge.addBlock(moving);
     }
 
