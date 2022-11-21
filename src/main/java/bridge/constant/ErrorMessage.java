571eda529481403a3cf80400e4b9662fb9f9b4dc
choijoohee213 <choijoohee@naver.com>
2022-11-22 00:23:52 +0900

    feat: 다리 길이 입력받아 랜덤으로 다리 칸 생성 구현

---
+++ src/main/java/bridge/constant/ErrorMessage.java
@@ -0,0 +1,7 @@
+package bridge.constant;
+
+public class ErrorMessage {
+    private static final String PREV = "[ERROR] ";
+    public static final String BRIDGE_SIZE_IS_NOT_IN_RANGE = PREV + "다리 길이는 3부터 20 사이의 숫자여야 합니다.";
+    public static final String BRIDGE_SIZE_IS_NOT_NUMBER = PREV + "다리 길이는 숫자여야합니다.";
+}
---
+++ src/main/java/bridge/constant/InformationMessage.java
@@ -0,0 +1,5 @@
+package bridge.constant;
+
+public class InformationMessage {
+    public static final String INPUT_BRIDGE_SIZE = "다리 길이를 입력해주세요.";
+}
