f72b4989f63e0462e5758710c1a077ed16e72c1f
bnbnac <bnbnac@naver.com>
2022-11-21 16:23:50 +0900

    feat: 이동에 따른 결과 반환 및 반복 로직 작성

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -1,8 +1,27 @@
 package bridge;
 
+import java.util.List;
+
 public class Application {
 
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        InputView inputView = new InputView();
+
+        BridgeNumberGenerator bridgeRandomNumberGenerator = new BridgeRandomNumberGenerator();
+        BridgeMaker bridgeMaker = new BridgeMaker(bridgeRandomNumberGenerator);
+
+
+        // make controller with these chunk?
+
+        Integer bridgeSize = inputView.readBridgeSize();
+        List<String> bridge = bridgeMaker.makeBridge(bridgeSize);
+        BridgeGame bridgeGame = new BridgeGame(bridge);
+
+        while (bridgeGame.step < bridgeSize) {
+            String moving = inputView.readMoving();
+            bridgeGame.move(moving);
+        }
+//        OutputView.printMap(something, bool or int);
+
     }
 }
--- src/main/java/bridge/BridgeGame.java
+++ src/main/java/bridge/BridgeGame.java
@@ -1,23 +1,34 @@
 package bridge;
 
-/**
- * 다리 건너기 게임을 관리하는 클래스
- */
+import java.util.List;
+
 public class BridgeGame {
 
-    /**
-     * 사용자가 칸을 이동할 때 사용하는 메서드
-     * <p>
-     * 이동을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
-     */
-    public void move() {
+    private final List<String> bridge;
+    public Integer step;
+
+    public BridgeGame(List<String> bridge) {
+        this.bridge = bridge;
+        this.step = 0;
     }
 
-    /**
-     * 사용자가 게임을 다시 시도할 때 사용하는 메서드
-     * <p>
-     * 재시작을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
-     */
+    public void move(String moving) {
+        if (step > bridge.size()) {
+//            succeed
+        }
+        if (match(moving)) {
+            this.step += 1;
+            System.out.println("match~");
+        }
+        System.out.println("die~");
+        this.step = 0;
+//        ask retry
+    }
+
+    public Boolean match(String moving) {
+        return bridge.get(step).equals(moving);
+    }
+
     public void retry() {
     }
 }
