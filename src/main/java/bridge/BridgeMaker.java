b12eebc4a44e278ec543878a85622573fdea99ae
zeunala <zeunala@naver.com>
2022-11-21 18:53:39 +0900

    feat: 무작위 값에 따른 다리 생성 기능 추가

--- src/main/java/bridge/BridgeMaker.java
+++ src/main/java/bridge/BridgeMaker.java
@@ -2,6 +2,7 @@ package bridge;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * 다리의 길이를 입력 받아서 다리를 생성해주는 역할을 한다.
@@ -27,10 +28,26 @@ public class BridgeMaker {
     }
 
     /**
+     * @param number 0 또는 1
+     * @return 0은 "D"(아래 칸), 1은 "U"(위 칸)
+     */
+    private String randomNumberToDirection(int number) {
+        if (number == 0) {
+            return "D";
+        }
+        if (number == 1) {
+            return "U";
+        }
+        return null;
+    }
+
+    /**
      * @param size 다리의 길이
      * @return 입력받은 길이에 해당하는 다리 모양. 위 칸이면 "U", 아래 칸이면 "D"로 표현해야 한다.
      */
     public List<String> makeBridge(int size) {
-        return null;
+        return makeRandomNumberList(size).stream()
+                .map(this::randomNumberToDirection)
+                .collect(Collectors.toList());
     }
 }
