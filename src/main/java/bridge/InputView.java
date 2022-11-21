356b5ceabab9c967ac1fd90ac31c24c52171e163
Hong Jin-Seop <jinsub1999@khu.ac.kr>
2022-11-21 14:01:12 +0900

    fix: 입력 부분 문구 출력 수정

--- src/main/java/bridge/InputView.java
+++ src/main/java/bridge/InputView.java
@@ -12,6 +12,7 @@ public class InputView {
      * 다리의 길이를 입력받는다.
      */
     public int readBridgeSize() {
+        System.out.println("다리의 길이를 입력해주세요.");
         String bridgeSize = Console.readLine();
         int result = Integer.parseInt(bridgeSize);
         if (result < 3 || result > 20) {
@@ -24,6 +25,7 @@ public class InputView {
      * 사용자가 이동할 칸을 입력받는다.
      */
     public String readMoving() {
+        System.out.println("이동할 칸을 선택해주세요. (위: U, 아래: D)");
         String moving = Console.readLine();
         List<String> validMove = List.of("U", "D");
 
@@ -37,6 +39,7 @@ public class InputView {
      * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
      */
     public String readGameCommand() {
+        System.out.println("게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)");
         String gameCommand = Console.readLine();
         List<String> validCommand = List.of("R", "Q");
 
