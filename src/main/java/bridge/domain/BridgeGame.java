340a84d84c40b7e0985dd6ce2de7e49f366360bb
himjjong <jjijji5728@tukorea.ac.kr>
2022-11-21 19:30:49 +0900

    feat : 게임 결과에 따른 출력 기능 메서드 분리

--- src/main/java/bridge/domain/BridgeGame.java
+++ src/main/java/bridge/domain/BridgeGame.java
@@ -1,8 +1,6 @@
 package bridge.domain;
 
 import bridge.view.OrderView;
-import bridge.view.OutputView;
-import net.bytebuddy.matcher.FilterableList;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -21,27 +19,44 @@ public class BridgeGame {
      * 이동을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
     public void move(String moveStep, String bridgeIndexData, int index) {
-        if(moveStep.equals("U") && bridgeIndexData.equals("0")) { printResult(orderView.wrong(),orderView.space(), index);}
-        if(moveStep.equals("U") && bridgeIndexData.equals("1")) { printResult(orderView.correct(),orderView.space(), index);}
-        if(moveStep.equals("D") && bridgeIndexData.equals("0")) { printResult(orderView.space(),orderView.correct(), index);}
-        if(moveStep.equals("D") && bridgeIndexData.equals("1")) { printResult(orderView.space(),orderView.wrong(), index);}
+        if(moveStep.equals("U") && bridgeIndexData.equals("0")) { gameResult(orderView.wrong(),orderView.space(), index);}
+        if(moveStep.equals("U") && bridgeIndexData.equals("1")) { gameResult(orderView.correct(),orderView.space(), index);}
+        if(moveStep.equals("D") && bridgeIndexData.equals("0")) { gameResult(orderView.space(),orderView.correct(), index);}
+        if(moveStep.equals("D") && bridgeIndexData.equals("1")) { gameResult(orderView.space(),orderView.wrong(), index);}
     }
 
-    private void printResult(String up, String down, int index) {
+    private void gameResult(String up, String down, int index) {
         upSide.add(up);
         downSide.add(down);
 
-        for (String s : upSide) {   System.out.print(s);    }
+        if( index == 0) {
+            firstResult(upSide.get(index), downSide.get(index));
+            return ;
+        }
+        restResult(upSide, downSide);
+    }
+    private void firstResult(String upSide, String downSide) {
+        System.out.print(upSide);
         orderView.lineSkip();
-        for (String s : downSide)  {    System.out.print(s);    }
+        System.out.print(downSide);
         orderView.lineSkip();
     }
 
+    private void restResult(List<String> upSide, List<String> downSide) {
+        for(int i = 0; i < upSide.size(); i++) {
+            System.out.print(upSide.get(i));
+        }
+        orderView.lineSkip();
+        for(int i = 0; i < downSide.size(); i++) {
+            System.out.print(downSide.get(i));
+        }
+        orderView.lineSkip();
+    }
     /**
      * 사용자가 게임을 다시 시도할 때 사용하는 메서드
      * <p>
      * 재시작을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void retry() {
+    private void retry() {
     }
 }
