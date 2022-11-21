3ab17a2a10f0ffda6a90b8e9c6d435deec1811c7
sora5421 <sojeoung536985@gmail.com>
2022-11-22 00:43:20 +0900

    feat (state) : make method that return boolean

--- src/main/java/bridge/data/Bridge.java
+++ src/main/java/bridge/data/Bridge.java
@@ -1,4 +1,16 @@
 package bridge.data;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Bridge {
+    private List<String> saveBridge = new ArrayList<>();
+
+    public Bridge (List<String> saveBridge) {
+        this.saveBridge = saveBridge;
+    }
+
+    boolean state(int order, String userInput) {
+        return saveBridge.get(order).equals(userInput);
+    }
 }
