1acff0abf7b5c2e1a52010cfe4e0549ea655922b
minji <heunhanireum@gmail.com>
2022-11-21 23:35:19 +0900

    feat : 출력문구 작성

--- src/main/java/bridge/OutputView.java
+++ src/main/java/bridge/OutputView.java
@@ -9,18 +9,37 @@ import java.util.Map;
  */
 public class OutputView {
 
+    public void start() {
+        System.out.println("다리 건너기 게임을 시작합니다");
+        System.out.println();
+        System.out.println("다리의 길이를 입력해주세요");
+    }
+
+    public void printChooseUpOrDown() {
+        System.out.println("이동할 칸을 선택해주세요. (위: U, 아래: D");
+    }
+
+    public void printRetryOrQuit() {
+        System.out.println("게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q");
+    }
+
+    public void printGameResult() {
+        System.out.println("최종 게임 결과");
+    }
+
     /**
      * 현재까지 이동한 다리의 상태를 정해진 형식에 맞춰 출력한다.
      * <p>
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void printMap(List<String> makeBridgeResult, int makeBridgeResultIndex, String userMoveInput) {
-        List<String> bridgeResult = makeBridgeResult.subList(0, makeBridgeResultIndex+1);
-
-        for (int index = 0 ; index <= makeBridgeResultIndex ; index++) {
+    public void printMap(List<String> userInput) {
+        Map<>
+        for (String input : userInput) {
 
         }
     }
+
+
 
     /**
      * 게임의 최종 결과를 정해진 형식에 맞춰 출력한다.
