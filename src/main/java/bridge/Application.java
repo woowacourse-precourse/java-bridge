4e9eaedf21b1bdf1de2469090f409e56310028bf
Minsoo Choi <lucas0209@yonsei.ac.kr>
2022-11-22 02:05:58 +0900

    feat(Application): main flow of the program until termination
    Features added:
    - Maintain done, succeed fields to know whether to continue while loop or not
    - userSingleMove: proceeds the move and returns if it reached a termination condition
    - checkSucceed: returns true if the user succeed in crossing the entire bridge

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -14,6 +14,8 @@ public class Application {
 
         greetings();
         List<String> answer = init(inputView, bm);
+        System.out.println("answer = " + answer.toString());
+        moveUntilDone(user, bg, inputView, outputView, answer);
     }
 
     public static void greetings() {
@@ -31,6 +33,32 @@ public class Application {
             return init(inputView, bm);
         }
     }
+
+    // 다리 건너기 성공할 때까지 게임 진행
+    private static void moveUntilDone(User user, BridgeGame bg, InputView inputView, OutputView outputView, List<String> answer) {
+        boolean done = false;
+        boolean succeed = false;
+        int trial = 0;
+        while (true) {
+            if (succeed) {
+                break;
+            }
+            if (done) {
+                trial++;
+                if (bg.retry(inputView)) {
+                    user = new User();
+                    done = false;
+                    continue;
+                }
+                outputView.printResult(bg.move(answer, user), "실패", trial);
+                break; // Termination condition: 종료(Q)
+            }
+            done = userSingleMove(user, bg, inputView, outputView, answer);
+            succeed = checkSucceed(bg.move(answer, user), outputView, answer.size(), trial);
+        }
+    }
+
+
 
 
 }
