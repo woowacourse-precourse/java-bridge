28f2b0922d67a251f5dfe6918d7df289e46cb257
bfly_notebook <kkyxw@soongsil.ac.kr>
2022-11-22 00:04:51 +0900

    refactor: delete the if statement in retry() method because of duplication check

--- src/main/java/bridge/domain/BridgeGame.java
+++ src/main/java/bridge/domain/BridgeGame.java
@@ -37,11 +37,9 @@ public class BridgeGame {
      * 재시작을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
     public void retry(String command) {
-        if (command.equals("R")) {
-            initializeUserPaths();
-            userLocation = 0;
-            tryCount++;
-        }
+        initializeUserPaths();
+        userLocation = 0;
+        tryCount++;
     }
 
     private void initializeUserPaths() {
