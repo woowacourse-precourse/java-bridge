2358f1c373f883b6beb74996c12b049f6c5df500
Yohan <sprae114@naver.com>
2022-11-21 21:14:20 +0900

    feat : 랜덤 다리 생성 길이에 따른 예외 추가

--- src/main/java/bridge/domain/BridgeMaker.java
+++ src/main/java/bridge/domain/BridgeMaker.java
@@ -9,12 +9,24 @@ import java.util.stream.IntStream;
 import static java.util.stream.Collectors.*;
 
 public class BridgeMaker {
+    private final int minLength = 3;
+    private final int maxLength = 20;
     private final BridgeNumberGenerator bridgeNumberGenerator;
 
     public final static List<String> UP_DOWN_SELECTOR = List.of("U", "D");
 
     public BridgeMaker(BridgeNumberGenerator bridgeNumberGenerator) {
         this.bridgeNumberGenerator = bridgeNumberGenerator;
+    }
+
+    public List<String> makeRandomBridgeProcess(int size){
+        validationBridge(size);
+        return makeBridge(size);
+    }
+    private void validationBridge(int size){
+        if(size < minLength || size > maxLength){
+            throw new IllegalArgumentException(Errors.BRIDGE_LENGTH.getMessage());
+        }
     }
 
     public List<String> makeBridge(int size) {
--- src/main/java/bridge/domain/Errors.java
+++ src/main/java/bridge/domain/Errors.java
@@ -1,6 +1,7 @@
-package bridge;
+package bridge.domain;
 
 public enum Errors {
+    BRIDGE_LENGTH("[ERROR] 다리 길이는 3부터 20 사이의 숫자여야 합니다."),
     FAIL_INPUT("[ERRORS] 올바른 값을 입력해주세요");
 
     String message;
