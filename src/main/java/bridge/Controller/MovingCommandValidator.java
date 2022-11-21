6aa35b56bc6c224c73fc8c371f58dc41e2d6d630
sypark <soun997@gmail.com>
2022-11-21 13:51:30 +0900

    feat(MovingCommandValidator): validate() 메소드 구현(올바르지 않은 이동 명령 입력 시 예외 처리)

--- src/main/java/bridge/Controller/MovingCommandValidator.java
+++ src/main/java/bridge/Controller/MovingCommandValidator.java
@@ -2,7 +2,11 @@ package bridge.Controller;
 
 public class MovingCommandValidator implements Validator{
 
-    public void validate(String input){
+    private static final String ERROR_NON_EXISTENT_COMMAND = "[ERROR] 올바르지 않은 명령입니다.";
 
+    public void validate(String input){
+        if (!input.equals("D") && !input.equals("U")){
+            throw new IllegalArgumentException(ERROR_NON_EXISTENT_COMMAND);
+        }
     }
 }
