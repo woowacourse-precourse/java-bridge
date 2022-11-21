a6be96a30ce10e0cf8d3057d9cbc7416fe14ce09
namtndus <rnrmf0721@gmail.com>
2022-11-22 04:42:51 +0900

    refactor(Application): main함수 리팩터링
    기능 세분화하여 gameRound함수와 gameExit함수를 추출

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -14,19 +14,31 @@ public class Application {
         outputView.printGameStartMessage();
         int brideSize = inputView.readBridgeSize();
         List<String> answerBridge = bridgeMaker.makeBridge(brideSize);
+        if(!gameRound(answerBridge,brideSize)){
+            gameExit(answerBridge,brideSize);
+        }
+        outputView.printResult(bridgeGame);
+    }
+
+    public static boolean gameRound(List<String> answerBridge, int brideSize){
+        boolean gameChecked;
         do {
             String userRemoveCommand = inputView.readMoving();
-            boolean gameChecked = bridgeGame.move(answerBridge,userRemoveCommand);
+            gameChecked = bridgeGame.move(answerBridge,userRemoveCommand);
             outputView.printMap(bridgeGame.upBridge);
             outputView.printMap(bridgeGame.downBridge);
             if(!gameChecked){
-                String userGameCommand = inputView.readGameCommand();
-                if (userGameCommand.equals("Q")){
-                    break;
-                }
-                bridgeGame.retry();
+                break;
             }
         }while (!bridgeGame.compareMoveCount(brideSize));
-        outputView.printResult(bridgeGame);
+        return gameChecked;
+    }
+
+    public static void gameExit(List<String> answerBridge, int brideSize){
+        String userGameCommand = inputView.readGameCommand();
+        if (userGameCommand.equals("R")){
+            bridgeGame.retry();
+            gameRound(answerBridge,brideSize);
+        }
     }
 }
