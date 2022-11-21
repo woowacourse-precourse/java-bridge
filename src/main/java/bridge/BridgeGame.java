9f2e70412913bc7a4ad91eadf94949a29c50764f
poter0501 <poter0501@gmail.com>
2022-11-21 21:32:36 +0900

    feat(BridgeGame): 이동 경로를 저장하는 path 필드와 생성자 추가

--- src/main/java/bridge/BridgeGame.java
+++ src/main/java/bridge/BridgeGame.java
@@ -1,9 +1,17 @@
 package bridge;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * 다리 건너기 게임을 관리하는 클래스
  */
 public class BridgeGame {
+    private List<Integer> path;
+
+    public BridgeGame(){
+        this.path=new ArrayList<>();
+    }
 
     /**
      * 사용자가 칸을 이동할 때 사용하는 메서드
