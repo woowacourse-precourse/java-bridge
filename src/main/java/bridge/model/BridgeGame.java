ac63b1d535245c2422b33c16ab2ba8cd97a979aa
viyott-lover <jackk0616@gmail.com>
2022-11-21 16:46:08 +0900

    feat(BridgeGame): 사용자가 칸을 이동할 때 사용하는 move() 메소드 작성

--- src/main/java/bridge/model/BridgeGame.java
+++ src/main/java/bridge/model/BridgeGame.java
@@ -1,16 +1,41 @@
 package bridge.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * 다리 건너기 게임을 관리하는 클래스
  */
 public class BridgeGame {
+    private List<String> marks; // "O",
+    private int gameCount;
+    List<String> bridge;
+    BridgeMaker bridgeMaker;
+    private int position;
+
+    public BridgeGame() {
+        this.marks = new ArrayList<>();
+        this.bridgeMaker = new BridgeMaker(new BridgeRandomNumberGenerator());
+        gameCount = 1;
+        position = 0;
+    }
 
     /**
      * 사용자가 칸을 이동할 때 사용하는 메서드
      * <p>
      * 이동을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void move() {
+    public void move(String userInputCourse) {
+        if (!isAbleToCross(userInputCourse)) {
+            marks.add("X");
+            return;
+        }
+        marks.add("O");
+        position++;
+    }
+
+    public boolean isAbleToCross(String course) {
+        return bridge.get(position).equals(course);
     }
 
     /**
@@ -19,5 +44,9 @@ public class BridgeGame {
      * 재시작을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
     public void retry() {
+    }
+
+    public void makeBridge(int size) {
+        bridge = bridgeMaker.makeBridge(size);
     }
 }
