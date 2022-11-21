209184e926704ffdd5772d9bf11887b8aab312b9
Myungyulee <102199101+Myungyulee@users.noreply.github.com>
2022-11-21 11:04:02 +0900

    feat: 입력 검증, 이동가능한 칸인지 확인 및 이동, 게임 성공 구현

--- src/main/java/bridge/Bridge.java
+++ src/main/java/bridge/Bridge.java
@@ -4,16 +4,24 @@ import java.util.List;
 
 public class Bridge {
 
-    List<String> availableSpace;
+    List<String> movableSpace;
 
     public Bridge(List<String> movableSpace){
         validate(movableSpace);
-        this.availableSpace = movableSpace;
+        this.movableSpace = movableSpace;
     }
 
     public void validate(List<String> movableSpace) {
         if (movableSpace.size() < 3 || movableSpace.size() > 20) {
             throw new IllegalArgumentException("다리 길이는 3부터 20 사이의 숫자여야 합니다.");
         }
+    }
+
+    public boolean checkMovable(int step, String selectedSpace) {
+        return movableSpace.get(step).equals(selectedSpace);
+    }
+
+    public int getBridgeSize() {
+        return movableSpace.size();
     }
 }
--- src/main/java/bridge/BridgeGame.java
+++ src/main/java/bridge/BridgeGame.java
@@ -4,13 +4,42 @@ package bridge;
  * 다리 건너기 게임을 관리하는 클래스
  */
 public class BridgeGame {
+    private final static  String UP = "U";
+    private final static  String DOWN = "D";
+
+    private Bridge bridge;
+    private int step;
+
+    public BridgeGame(Bridge bridge) {
+        this.bridge = bridge;
+        this.step = 0;
+    }
 
     /**
      * 사용자가 칸을 이동할 때 사용하는 메서드
      * <p>
      * 이동을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
+    public boolean checkMovable(String selectedSpace){
+        validate(selectedSpace);
+        return bridge.checkMovable(step, selectedSpace);
+    }
+
     public void move() {
+        step += 1;
+    }
+
+    public void validate(String selectedSpace) {
+        if (!selectedSpace.equals(UP) && !selectedSpace.equals(DOWN)) {
+            throw new IllegalArgumentException("U 또는 D를 입력해주세요.");
+        }
+    }
+
+    public boolean checkEnd(){
+        if (step == bridge.getBridgeSize()) {
+            return true;
+        }
+        return false;
     }
 
     /**
@@ -19,5 +48,8 @@ public class BridgeGame {
      * 재시작을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
     public void retry() {
+
+
+
     }
 }
