ff2a23b881c953e72d14702fb7247674af239053
KYR <freekyr7529@gmail.com>
2022-11-21 20:05:07 +0900

    refactor : 클래스 이름 변경
    BridgeManagement 보다 BridgePattern 이 뜻을 더 잘 표현한다고 생각되어 이름을 변경

--- src/main/java/bridge/constant/BridgePattern.java
+++ src/main/java/bridge/constant/BridgePattern.java
@@ -2,7 +2,7 @@ package bridge.constant;
 
 import java.util.Arrays;
 
-public enum BridgeManagement {
+public enum BridgePattern {
     MOVE_UP("U", "위", 1),
     MOVE_DOWN("D", "아래", 0);
 
@@ -10,7 +10,7 @@ public enum BridgeManagement {
     private final String detail;
     private final int number;
 
-    BridgeManagement(String move, String detail, int number) {
+    BridgePattern(String move, String detail, int number) {
         this.move = move;
         this.detail = detail;
         this.number = number;
@@ -25,17 +25,21 @@ public enum BridgeManagement {
     }
 
     public static String convertNumberToMove(int randomNumber) {
-        return Arrays.stream(BridgeManagement.values())
+        return Arrays.stream(BridgePattern.values())
                 .filter(bridge -> bridge.number == randomNumber)
                 .findAny()
                 .orElseThrow(() -> new IllegalArgumentException("입력이 지정된 숫자가 아닙니다! 난수 생성 모듈을 확인하세요."))
                 .move;
     }
 
-    public static BridgeManagement searchBridgeType(String move) {
-        return Arrays.stream(BridgeManagement.values())
+    public static BridgePattern searchBridgeType(String move) {
+        return Arrays.stream(BridgePattern.values())
                 .filter(bridge -> move.equals(bridge.move))
                 .findAny()
                 .orElseThrow(() -> new IllegalArgumentException("허용되지 않는 값! U(위쪽) D(아래쪽) 중 하나를 입력하셔야 합니다!"));
+    }
+
+    public boolean isInputEqual(String input) {
+        return move.equals(input);
     }
 }
