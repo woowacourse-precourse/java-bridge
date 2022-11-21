2fe0c64d3ded856f31c0afc374246783afc59531
song-wooseok <sws981202@gmail.com>
2022-11-21 19:31:33 +0900

    fix: 플레이어 시도 횟수 초기 값 변경 0 -> 1

--- src/main/java/bridge/domain/BridgePlayer.java
+++ src/main/java/bridge/domain/BridgePlayer.java
@@ -4,7 +4,7 @@ import bridge.utils.constants.BridgeMove;
 
 public class BridgePlayer {
     private int currentBridgePosition = BridgeMove.START_INDEX;
-    private int gameAttemptCount = 0;
+    private int gameAttemptCount = 1;
     private final int goStep = BridgeMove.GO_STEP;
 
     public int getNextBridgePosition(){
