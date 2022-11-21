da0bb91a8daf4ceaed7e8bf2eee1859efd394560
brotherstone97 <brotherstone97@gmail.com>
2022-11-21 16:59:15 +0900

    feat(BridgeMaker): 입력받은 길이만큼의 다리 생성
    다리 한칸은 위칸 혹은 아래칸으로 채워진다.

--- src/main/java/bridge/BridgeMaker.java
+++ src/main/java/bridge/BridgeMaker.java
@@ -1,6 +1,8 @@
 package bridge;
 
+import java.util.ArrayList;
 import java.util.List;
+
 import bridge.InputView;
 
 
@@ -21,10 +23,22 @@ public class BridgeMaker {
      * @return 입력받은 길이에 해당하는 다리 모양. 위 칸이면 "U", 아래 칸이면 "D"로 표현해야 한다.
      */
     public List<String> makeBridge(int size) {
-        return null;
+        List<String> bridge = new ArrayList<>();
+        for (int i = 0; i < size; i++) {
+            int number = bridgeNumberGenerator.generate();
+            bridge.add(determineUpOrDown(number));
+        }
+        return bridge;
     }
 
-    public List<String> transmitBridge(){
+    private String determineUpOrDown(int number) {
+        if (number == 0) {
+            return "D";
+        }
+        return "U";
+    }
+
+    public List<String> transmitBridge() {
         InputView inputView = new InputView();
         int size = inputView.readBridgeSize();
         return makeBridge(size);
