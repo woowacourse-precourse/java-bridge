0673ac54a2018161baf8dfc2fe176ebad1b7b1c8
lee <higgs95@naver.com>
2022-11-22 01:14:39 +0900

    feat(OutputConstant): 출력 관련 상수 추가

---
+++ src/main/java/bridge/constant/OutputConstant.java
@@ -0,0 +1,11 @@
+package bridge.constant;
+
+public class OutputConstant {
+
+    public static final String MAP_STARTER = "[ ";
+    public static final String MAP_FINISHER = " ]";
+    public static final String MAP_SEPARATOR = " | ";
+    public static final String FINAL_GAME_RESULT = "최종 게임 결과";
+    public static final String CHECK_CLEAR_FAIL = "게임 성공 여부: ";
+    public static final String TOTAL_GAME_COUNT = "총 시도한 횟수: ";
+}
