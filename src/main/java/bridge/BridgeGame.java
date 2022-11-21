1f26755062c7a89c51ab10de755d2336fcdc1e98
kissshot1104 <kissshot.heartunderblade1104@gmail.com>
2022-11-21 18:32:34 +0900

    Feat(BridgeGame) : 사용자가 게임을 다시 시도하는 기능 추가
    public boolean retry()
    메소드를 추가했습니다.
    이 메소드는 사용자가 게임을 다시 시도할 때 사용하는 메소드입니다.

--- src/main/java/bridge/BridgeGame.java
+++ src/main/java/bridge/BridgeGame.java
@@ -15,6 +15,16 @@ public class BridgeGame {
         count = 1;
     }
 
+    /**
+     * 사용자가 게임을 다시 시도할 때 사용하는 메서드
+     * <p>
+     * 재시작을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
+     */
+    public boolean retry() {
+        boolean doRetry = retryProcessing.readRetry();
+
+        return doRetry;
+    }
 
     //여기서 upSide와 downSide를 더해서 출력 하게끔 하자
     public void endGame() {
