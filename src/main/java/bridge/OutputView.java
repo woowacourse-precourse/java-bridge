725141dc3b211c864464246c6569cc677dfb7153
Dennis-K1 <dennis.k1.dev@gmail.com>
2022-11-21 18:38:01 +0900

    feat: Add output sentences

--- src/main/java/bridge/OutputView.java
+++ src/main/java/bridge/OutputView.java
@@ -19,26 +19,27 @@ public class OutputView {
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
     public void printResult() {
+        System.out.println("최종 게임 결과");
     }
 
     public void printGameStart() {
-
+        System.out.println("다리 건너기 게임을 시작합니다.");
     }
 
     public void printBridgeSizeInput() {
-
+        System.out.println("다리의 길이를 입력해주세요.");
     }
     public void printMovingInput() {
-
+        System.out.println("이동할 칸을 선택해주세요. (위: U, 아래: D)");
     }
     public void printGameOver() {
-
+        System.out.println("게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)");
     }
-    public void printSuccess() {
-
+    public void printSuccess(int status) {
+        System.out.println("게임 성공 여부: ");
     }
     public void printTrialAmount() {
-
+        System.out.println("총 시도한 횟수: 1");
     }
 
     public void printNotMovable() {
