3b306300f50c01d16dd861b8bed3667570d3cd70
김연진 <love_homebody@naver.com>
2022-11-22 04:02:08 +0900

    feat: 사용자 칸 이동 기능 구현

--- src/main/java/bridge/BridgeGame.java
+++ src/main/java/bridge/BridgeGame.java
@@ -1,16 +1,45 @@
 package bridge;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * 다리 건너기 게임을 관리하는 클래스
  */
 public class BridgeGame {
+
+    private final List<String> bridge;
+    private List<String> userAnswerTable;
+    private int tryCount;
+
+    public List<String> getUserAnswerTable() {
+        return userAnswerTable;
+    }
+
+    public int getTryCount() {
+        return tryCount;
+    }
+
+
+
+    public BridgeGame(List<String> bridge) {
+        this.bridge = bridge;
+        userAnswerTable = new ArrayList<>();
+        this.tryCount = 0;
+    }
 
     /**
      * 사용자가 칸을 이동할 때 사용하는 메서드
      * <p>
      * 이동을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void move() {
+    public void move(String userChoice) {
+        String correctAnswer = bridge.get(userAnswerTable.size());
+        userAnswerTable.add("O");
+        if (!correctAnswer.equals(userChoice)) {
+            int curLocation = userAnswerTable.size() - 1;
+            userAnswerTable.set(curLocation, "X");
+        }
     }
 
     /**
