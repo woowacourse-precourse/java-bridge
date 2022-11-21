008fd7d5c0e6fca544f51d178c0aa02eaa9e240b
E1psycongr00 <39326175+E1psycongr00@users.noreply.github.com>
2022-11-21 18:53:17 +0900

    feat(view): view에 예외 메시지 출력 기능 추가

--- src/main/java/bridge/view/View.java
+++ src/main/java/bridge/view/View.java
@@ -2,11 +2,17 @@ package bridge.view;
 
 public class View {
 
+    private static final String ERROR = "[ERROR] ";
+
     protected void print(String message) {
         System.out.println(message);
     }
 
     protected void print() {
         System.out.println();
+    }
+
+    protected void printError(String errorMessage) {
+        System.out.println(ERROR + errorMessage);
     }
 }
