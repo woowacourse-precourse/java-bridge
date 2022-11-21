19cb26d69393c6b1be57b3c90d59310527b8da78
sohyundoh <lyny123@naver.com>
2022-11-21 18:29:58 +0900

    feat($output): Problem5. 이동을 시도한 결과 출력

--- src/main/java/bridge/OutputView.java
+++ src/main/java/bridge/OutputView.java
@@ -1,5 +1,7 @@
 package bridge;
 
+import java.util.List;
+
 /**
  * 사용자에게 게임 진행 상황과 결과를 출력하는 역할을 한다.
  */
@@ -18,9 +20,43 @@ public class OutputView {
      * <p>
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void printMap() {
+    public static void printMap(int trial, List<String> bridge, List<String> inputMove) {
+        System.out.print("[");
+        for(int i = 0; i<(trial+1);i++){
+            printIndex(i);
+            System.out.print(printUp(inputMove.get(i), BridgeGame.validMove(bridge.get(i),inputMove.get(i))));
+        }
+        System.out.println("]");
+        System.out.print("[");
+        for(int i = 0; i<(trial+1);i++){
+            printIndex(i);
+            System.out.print(printDown(inputMove.get(i), BridgeGame.validMove(bridge.get(i),inputMove.get(i))));
+        }
+        System.out.println("]");
     }
-
+    public static void printIndex(int index){
+        if(index != 0){
+            System.out.print("|");
+        }
+    }
+    public static String printMove(boolean valid){
+        if(valid){
+            return "O";
+        }
+        return "X";
+    }
+    public static String printUp(String move, boolean valid){
+        if(move.equals("U")){
+            return printMove(valid);
+        }
+        return " ";
+    }
+    public static String printDown(String move ,boolean valid){
+        if(move.equals("D")){
+            return printMove(valid);
+        }
+        return " ";
+    }
     /**
      * 게임의 최종 결과를 정해진 형식에 맞춰 출력한다.
      * <p>
