54753bafb824a659c72ce19b37114d5da56bac6e
kkirby54 <sdk926@gmail.com>
2022-11-21 22:38:13 +0900

    refactor: view를 static으로 변경

--- src/main/java/bridge/view/InputView.java
+++ src/main/java/bridge/view/InputView.java
@@ -10,10 +10,13 @@ import camp.nextstep.edu.missionutils.Console;
  */
 public class InputView {
 
+    private InputView(){
+    }
+
     /**
      * 다리의 길이를 입력받는다.
      */
-    public int readBridgeSize() {
+    public static int readBridgeSize() {
         try{
             System.out.println("다리의 길이를 입력해주세요.");
             String input = Console.readLine();
@@ -29,7 +32,7 @@ public class InputView {
     /**
      * 사용자가 이동할 칸을 입력받는다.
      */
-    public String readMoving() {
+    public static String readMoving() {
         try {
             System.out.println("이동할 칸을 선택해주세요. (위: U, 아래: D)");
             String input = Console.readLine();
@@ -44,7 +47,7 @@ public class InputView {
     /**
      * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
      */
-    public String readGameCommand() {
+    public static String readGameCommand() {
         try{
             System.out.println("게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)");
             String input = Console.readLine();
