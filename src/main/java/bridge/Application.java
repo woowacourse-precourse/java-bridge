2861f23c2ac793a4b2faea3c83100efe0673d734
leesuengyeol <48343629+syhojeo@users.noreply.github.com>
2022-11-21 19:10:18 +0900

    feat: bridge Map 만들기 기능 구현 완성

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -2,11 +2,13 @@ package bridge;
 
 public class Application {
 
-    private static InputView inputView;
-    private static OutputView outputView;
+    private final static InputView inputView = new InputView();
+    private final static OutputView outputView = new OutputView();
+    private final static BridgeMaker bridgeMaker = new BridgeMaker(new BridgeRandomNumberGenerator());
 
     public static void main(String[] args) {
         // TODO: 프로그램 구현
         int bridgeSize = inputView.readBridgeSize();
+        bridgeMaker.makeBridge(bridgeSize);
     }
 }
