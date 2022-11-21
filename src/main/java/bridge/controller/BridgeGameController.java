1f3d5ba843376283cbe2e19be362ff5c8a7fe3dc
LHS-11 <ghktjq1119@naver.com>
2022-11-22 00:49:40 +0900

    [Fix] : 사용자가 잘못된 값 입력했을 때 에러 메시지 출력 후 다시 입력 받을 수 있도록 수정

--- src/main/java/bridge/controller/BridgeGameController.java
+++ src/main/java/bridge/controller/BridgeGameController.java
@@ -19,45 +19,73 @@ public class BridgeGameController {
         outputView = new OutputView();
     }
 
-    public void go(){
-        try {
-            int size = start();
-            do {
-                play();
-                outputView.printMap(bridgeGame.getUpBridge(),bridgeGame.getDownBridge(),bridgeGame.getLocation());
-            } while (retryGame(size) && !bridgeGame.checkEndPoint());
-            end();
-        }catch (Exception e){
-            outputView.printException(e.getMessage());
-        }
+    public void go() {
+        int size = start();
+        do {
+            play();
+            outputView.printMap(bridgeGame.getUpBridge(), bridgeGame.getDownBridge(), bridgeGame.getLocation());
+        } while (retryGame(size) && !bridgeGame.checkEndPoint());
+        end();
     }
+
+
 
     private int start() {
         outputView.printInit();
-        String size = inputView.readBridgeSize();
-        BridgeLength bridgeLength = BridgeLength.createBridgeLength(size);
+        BridgeLength bridgeLength = getBridgeLength();
         bridgeGame = BridgeGame.createBridgeGame(bridgeLength.getLength());
         return bridgeLength.getLength();
     }
 
+    private BridgeLength getBridgeLength() {
+        try {
+            String size = inputView.readBridgeSize();
+            BridgeLength bridgeLength = BridgeLength.createBridgeLength(size);
+            return bridgeLength;
+        } catch (IllegalArgumentException e) {
+            outputView.printException(e.getMessage());
+            return getBridgeLength();
+        }
+    }
+
     private void play() {
-        String moving = inputView.readMoving();
-        BridgeMoving.createBridgeMoving(moving);
+        String moving = getMoving();
         bridgeGame.move(moving);
     }
 
+    private String getMoving() {
+        try {
+            String moving = inputView.readMoving();
+            BridgeMoving.createBridgeMoving(moving);
+            return moving;
+        } catch (IllegalArgumentException e) {
+            outputView.printException(e.getMessage());
+            return getMoving();
+        }
+    }
+
     private boolean retryGame(int size) {
-        boolean retry=true;
-        if(bridgeGame.isFail()) {
-            String retryOrClose = inputView.readGameCommand();
-            BridgeCommand bridgeCommand = BridgeCommand.createBridgeCommand(retryOrClose);
-            retry = isRetryOrClose(size, retryOrClose);
+        boolean retry = true;
+        if (bridgeGame.isFail()) {
+            String command = getCommand();
+            retry = isRetryOrClose(size, command);
         }
         return retry;
     }
 
+    private String getCommand() {
+        try {
+            String command = inputView.readGameCommand();
+            BridgeCommand bridgeCommand = BridgeCommand.createBridgeCommand(command);
+            return command;
+        } catch (IllegalArgumentException e) {
+            outputView.printException(e.getMessage());
+            return getCommand();
+        }
+    }
+
     private boolean isRetryOrClose(int size, String retryOrClose) {
-        BridgeCommand bridgeCommand =  BridgeCommand.createBridgeCommand(retryOrClose);
+        BridgeCommand bridgeCommand = BridgeCommand.createBridgeCommand(retryOrClose);
         if (bridgeCommand.getCommand()) {
             bridgeGame.retry(size);
             return true;
@@ -66,7 +94,7 @@ public class BridgeGameController {
     }
 
     private void end() {
-        outputView.printFinalMap(bridgeGame.getUpBridge(),bridgeGame.getDownBridge(), bridgeGame.getLocation());
+        outputView.printFinalMap(bridgeGame.getUpBridge(), bridgeGame.getDownBridge(), bridgeGame.getLocation());
         outputView.printResult(bridgeGame.getRetryCount(), !bridgeGame.isFail());
     }
 }
