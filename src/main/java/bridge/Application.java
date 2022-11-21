ad61989c0495dd2f74684736bde364b0fbb6fb88
Soseung Soo <sss9073@naver.com>
2022-11-22 00:47:43 +0900

    refactor(Application): try-catch 삭제

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -1,17 +1,15 @@
 package bridge;
 
 import java.util.List;
+import static bridge.BridgeGameController.outputView;
 
 public class Application {
 
     public static void main(String[] args) {
-        try{
-            List<String> answerBridge = BridgeGameController.startSettingBridge();
-            BridgeGameController bridgeGameController = new BridgeGameController(answerBridge);
-            List<List<String>> result = bridgeGameController.start();
-            bridgeGameController.end(result);
-        }catch (IllegalArgumentException e){
-            System.out.println(e.getMessage());
-        }
+        outputView.printStart();
+        List<String> answerBridge = BridgeGameController.startSettingBridge();
+        BridgeGameController bridgeGameController = new BridgeGameController(answerBridge);
+        List<List<String>> result = bridgeGameController.start();
+        bridgeGameController.end(result);
     }
 }
