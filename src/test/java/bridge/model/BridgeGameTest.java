429a23f709ffe0ab1ab046930228b7e051a38675
eastsage <dpwk8@naver.com>
2022-11-22 02:33:48 +0900

    test: 이동시 지도를 반환하는 함수 테스트 추가

--- src/test/java/bridge/model/BridgeGameTest.java
+++ src/test/java/bridge/model/BridgeGameTest.java
@@ -12,7 +12,12 @@ class BridgeGameTest {
         bridgeGame = new BridgeGame(3, new BridgeStaticNumberGenerator());
     }
 
-    
+    @ParameterizedTest(name = "{0}로 이동 -> {1}")
+    @CsvSource(value = {"U,'[ O ]\n[   ]'", "D,'[   ]\n[ X ]'"})
+    @DisplayName("입력된 방향으로 이동하면 결과를 가진 지도를 반환한다.")
+    void move_RunMove_ReturnMessage(Direction direction, String message) {
+        assertThat(bridgeGame.move(direction)).isEqualTo(message);
+    }
     private class BridgeStaticNumberGenerator implements BridgeNumberGenerator {
         int count = 0;
 
