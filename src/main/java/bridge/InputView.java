f16d9e18fd8ea4671801cd42e042cc26fe540d83
고동우 <shp05179@gmail.com>
2022-11-21 17:57:00 +0900

    feat: 입력기능 추가
    readBridgeSize: 다리 사이즈 입력
    readMoving: U 또는 D 입력
    readGameCommand: R 또는 Q 입력

--- src/main/java/bridge/InputView.java
+++ src/main/java/bridge/InputView.java
@@ -1,28 +1,37 @@
 package bridge;
 
+import camp.nextstep.edu.missionutils.Console;
+
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
  */
 public class InputView {
-
     /**
      * 다리의 길이를 입력받는다.
      */
-    public int readBridgeSize() {
-        return 0;
+    public static int readBridgeSize() {
+        OutputView.printRequestBridgeSize();
+        int size = ValidCheck.bridgeSize(Console.readLine());
+        return size;
     }
 
     /**
      * 사용자가 이동할 칸을 입력받는다.
      */
-    public String readMoving() {
-        return null;
+    public static String readMoving() {
+        OutputView.printRequestUpOrDown();
+        String direction = Console.readLine();
+        ValidCheck.upOrDown(direction);
+        return direction;
     }
 
     /**
      * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
      */
-    public String readGameCommand() {
-        return null;
+    public static String readGameCommand() {
+        OutputView.printRequestRetryOrQuit();
+        String retryOrQuit = Console.readLine();
+        ValidCheck.retryOrQuit(retryOrQuit);
+        return retryOrQuit;
     }
 }
