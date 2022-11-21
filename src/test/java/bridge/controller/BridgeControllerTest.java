dc9a7ee2c0c51fcdb5f200dd2be82816dd41025e
sosow0212 <sosow0212@naver.com>
2022-11-21 14:54:39 +0900

    refactor : (BridgeControllerTest) 필요없는 메소드 제거

--- src/test/java/bridge/controller/BridgeControllerTest.java
+++ src/test/java/bridge/controller/BridgeControllerTest.java
@@ -11,33 +11,6 @@ import org.junit.jupiter.api.Test;
 public class BridgeControllerTest {
     private final BridgeController bridgeController = new BridgeController();
 
-    @DisplayName("유저가 입력한 이동 값이 다리의 값과 일치하는지 isUserAnswerCorrect 테스트")
-    @Test
-    public void isUserAnswerCorrectTest() {
-        // given
-        Bridge bridge = new Bridge(List.of("U", "U", "D"));
-        String moveDirectionFirst = "U";
-        String moveDirectionSecond = "U";
-        String moveDirectionThird = "D";
-        String moveDirectionNotMatched = "U";
-
-        // when
-        boolean firstResult = bridgeController.isUserAnswerCorrect(moveDirectionFirst, bridge);
-
-        bridge.nowIndexUpdate();
-        boolean secondResult = bridgeController.isUserAnswerCorrect(moveDirectionSecond, bridge);
-
-        bridge.nowIndexUpdate();
-        boolean thirdResult = bridgeController.isUserAnswerCorrect(moveDirectionThird, bridge);
-        boolean notMatchedResult = bridgeController.isUserAnswerCorrect(moveDirectionNotMatched, bridge);
-
-        // then
-        assertThat(firstResult).isEqualTo(true);
-        assertThat(secondResult).isEqualTo(true);
-        assertThat(thirdResult).isEqualTo(true);
-        assertThat(notMatchedResult).isEqualTo(false);
-    }
-
     @DisplayName("processGame 테스트 (true 경우)")
     @Test
     public void processTrueCaseTest() {
