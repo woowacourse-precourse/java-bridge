52760e4242d4aa4c82bf7dddeb2d11b4f514caff
gwj0421 <gwj0421@gmail.com>
2022-11-21 18:26:18 +0900

    feat: 랜덤으로 만들어진 수(0 혹은 1)를 통해 변환된 "U"나 "D"를 리스트 형태로 반환해주는 함수 추가

--- src/main/java/bridge/BridgeMaker.java
+++ src/main/java/bridge/BridgeMaker.java
@@ -28,6 +28,11 @@ public class BridgeMaker {
      * @return 입력받은 길이에 해당하는 다리 모양. 위 칸이면 "U", 아래 칸이면 "D"로 표현해야 한다.
      */
     public List<String> makeBridge(int size) {
-        return null;
+        List<String> bridge = new ArrayList<>();
+        for (int i = 0; i < size; i++) {
+            String randomUpOrDown = changeNumToStr(bridgeNumberGenerator.generate());
+            bridge.add(randomUpOrDown);
+        }
+        return bridge;
     }
 }
