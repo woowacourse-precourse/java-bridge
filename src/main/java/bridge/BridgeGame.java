eff24a97c269dc95fe3df5756bdc390bd1811113
NAM GIBEOM <rlqja0523@naver.com>
2022-11-22 02:25:09 +0900

    feat: 사용자가 재시작할 것인지 확인하는 메서드 구현

--- src/main/java/bridge/BridgeGame.java
+++ src/main/java/bridge/BridgeGame.java
@@ -1,5 +1,7 @@
 package bridge;
 
+import bridge.service.UserInput;
+
 import java.util.List;
 
 /**
@@ -31,6 +33,11 @@ public class BridgeGame {
      * <p>
      * 재시작을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void retry() {
+    public boolean retry(String gameCommand) {
+        if (gameCommand.equals(UserInput.InputLetter.GAME_RESTART.getLetter())) {
+            bridgeIndex = 0;
+            return true;
+        }
+        return false;
     }
 }
