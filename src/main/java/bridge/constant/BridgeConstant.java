5bc5f78f1707e8daa12653601a4f7ffe2bc642ea
lee <higgs95@naver.com>
2022-11-21 23:07:08 +0900

    feat(constant): 다리 및 에러 관련 상수 추가

---
+++ src/main/java/bridge/constant/BridgeConstant.java
@@ -0,0 +1,11 @@
+package bridge.constant;
+
+public class BridgeConstant {
+
+    public static final int BRIDGE_LENGTH_MINIMUM = 3;
+    public static final int BRIDGE_LENGTH_MAXIMUM = 20;
+    public static final String PASS = "O";
+    public static final String FAIL = "X";
+    public static final String RETRY = "R";
+    public static final String QUIT = "Q";
+}
