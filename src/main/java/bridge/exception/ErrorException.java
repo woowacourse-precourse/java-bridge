359ede91b6ef9c5251412c7aa3abd7fda761060b
hhiyeon <hhyyeon0214@gmail.com>
2022-11-21 14:01:31 +0900

    feat:BrideError-create error exception for IllegalArgumentException

---
+++ src/main/java/bridge/exception/ErrorException.java
@@ -0,0 +1,19 @@
+package bridge.exception;
+
+public class ErrorException extends IllegalArgumentException {
+    public ErrorException() {
+    }
+
+    public ErrorException(String s) {
+        super(s);
+    }
+
+    public ErrorException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public ErrorException(Throwable cause) {
+        super(cause);
+    }
+}
+
