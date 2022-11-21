27fbe4b7c1e249771583c84243e71e80957ce923
jinhyeon <vmflwu123@gmail.com>
2022-11-22 01:34:01 +0900

    feat(GameInitializer): 다리 길이에 대한 validation 로직 추가
    1. 숫자가 아닌 것이 포함된 경우 (공백, 빈 문자열 포함)
    2. 3 이상 20 이하인 숫자가 아닌 경우

--- src/main/java/bridge/core/GameInitializer.java
+++ src/main/java/bridge/core/GameInitializer.java
@@ -4,13 +4,34 @@ import bridge.domain.Bridge;
 
 import java.util.List;
 
+import static java.util.regex.Pattern.matches;
+
 public class GameInitializer {
 
     public BridgeGame init(String bridgeLength) {
+        validate(bridgeLength);
         BridgeMaker bridgeMaker = BridgeMaker.getBridgeMaker();
         List<String> bridgeMap = bridgeMaker.makeBridge(Integer.valueOf(bridgeLength));
         Bridge bridge = new Bridge(bridgeMap);
         GameStatusOperator gameStatusOperator = GameStatusOperator.initGameStatusOperator();
         return new BridgeGame(bridge, gameStatusOperator);
+    }
+
+    private void validate(String input) {
+        if (validateNonNumeric(input) || validateOutOfRange(input)) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private boolean validateNonNumeric(String input) {
+        String pattern = "^[0-9]+$";
+        if (!matches(pattern, input)) return true;
+        return false;
+    }
+
+    private boolean validateOutOfRange(String input) {
+        Integer number = Integer.valueOf(input);
+        if (number < 3 || number > 20) return true;
+        return false;
     }
 }
