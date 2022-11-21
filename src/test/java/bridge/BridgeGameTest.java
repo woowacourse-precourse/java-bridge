1baf4a5704cec786076aec97a965999aa83ee476
sjh910805@gmail.com <sjh910805@gmail.com>
2022-11-21 14:06:20 +0900

    test: 이동 메소드 테스트 코드

--- src/test/java/bridge/BridgeGameTest.java
+++ src/test/java/bridge/BridgeGameTest.java
@@ -5,30 +5,45 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import java.io.ByteArrayInputStream;
 import java.io.InputStream;
+import java.util.List;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class BridgeGameTest {
     private BridgeGame bridgeGame;
+    private List<String> bridge;
 
     @BeforeEach
     void initiateBridgeGameTest() {
         bridgeGame = new BridgeGame();
-//        String input = "3";
-//        InputStream in = new ByteArrayInputStream(input.getBytes());
-//        System.setIn(in);
-//        bridgeGame.requestBridge();
+        String bridgeSize = "3";
+        InputStream in = new ByteArrayInputStream(bridgeSize.getBytes());
+        System.setIn(in);
+        bridgeGame.requestBridge();
+        bridge = bridgeGame.getBridge();
     }
 
 //    @Test
 //    void requestBridge() {
 //
 //    }
+    @DisplayName("윗칸 이동 명령을 내렸을때, 윗칸이 이동가능하면 success은 true가 "
+            + "아랫칸이 이동 가능하면 false가 된다.")
+    @Test
+    void move() {
+        String moveCommand = "U";
+        InputStream in = new ByteArrayInputStream(moveCommand.getBytes());
+        System.setIn(in);
+        bridgeGame.move();
+        if(!bridge.get(bridgeGame.getCurrentPosition()).equals(moveCommand)) {
+            assertThat(bridgeGame.isSuccess()).isEqualTo(false);
+        }
+        if(bridge.get(bridgeGame.getCurrentPosition()).equals(moveCommand)) {
+            assertThat(bridgeGame.isSuccess()).isEqualTo(true);
+        }
+    }
 
-//    @Test // getBridge 해가지고. 같으면 어쩌구 다르면 어쩌구 하면 되지 않을까.
-//    void move() {
-//    }
     @DisplayName("재시작을 실행했을때, true를 반환하고, currentPosition은 -1이 되고,"
             + "success은 true, totalTryCount은 1이 증가한다.")
     @Test
