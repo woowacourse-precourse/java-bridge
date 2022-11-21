292f66d79112fd2755c456b101bfe044606f4d50
zzun <736300@naver.com>
2022-11-21 22:51:59 +0900

    feat(BridgeGame): 입력한 칸 대로 이동하는 move 메서드, 재시작 여부를 확인하는 retry 메서드 구현

--- src/main/java/bridge/BridgeGame.java
+++ src/main/java/bridge/BridgeGame.java
@@ -1,23 +1,25 @@
 package bridge;
 
-/**
- * 다리 건너기 게임을 관리하는 클래스
- */
 public class BridgeGame {
 
-    /**
-     * 사용자가 칸을 이동할 때 사용하는 메서드
-     * <p>
-     * 이동을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
-     */
-    public void move() {
+    public int move(String command, String[][] bridgeBoard, int position) {
+        if (command.equals("U")) {
+            if (bridgeBoard[0][position].equals(command)) {
+                return position + 1;
+            }
+            return position;
+        }
+
+        if (bridgeBoard[1][position].equals(command)) {
+            return position + 1;
+        }
+        return position;
     }
 
-    /**
-     * 사용자가 게임을 다시 시도할 때 사용하는 메서드
-     * <p>
-     * 재시작을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
-     */
-    public void retry() {
+    public boolean retry(String command) {
+        if (command.equals("R")) {
+            return true;
+        }
+        return false;
     }
 }
