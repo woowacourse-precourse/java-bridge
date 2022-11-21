f84c4a8065cf9e6aaf7149f47216bedb31c823e2
neppiness <0414kjh@naver.com>
2022-11-21 19:31:07 +0900

    test: BridgeGameTest 추가 (src/test/java/bridge)
    - runMain에 오버라이드 하여 move, retry, printMap, printResult 메소드 정상 동작 확인

---
+++ src/test/java/bridge/BridgeGameTest.java
@@ -0,0 +1,82 @@
+package bridge;
+
+import camp.nextstep.edu.missionutils.test.NsTest;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
+import static org.assertj.core.api.Assertions.assertThat;
+
+class BridgeGameTest extends NsTest {
+
+    private static final String ERROR_MESSAGE = "[ERROR]";
+    static BridgeGame bridgeGame;
+
+    @DisplayName("BridgeGame 클래스 종합 테스트")
+    @Test
+    void bridgeGameTest() {
+        assertSimpleTest(() -> {
+            run("U", "D", "D", "U");
+            assertThat(output()).doesNotContain(ERROR_MESSAGE).contains(
+                    "[ O ]",
+                    "[   ]",
+                    "#1 isEnd 반환값: false",
+                    "#1 isMoveWrong 반환값: false",
+                    "[ O |   ]",
+                    "[   | O ]",
+                    "#2 isEnd 반환값: false",
+                    "#2 isMoveWrong 반환값: false",
+                    "[ O |   |   ]",
+                    "[   | O | X ]",
+                    "#3 isEnd 반환값: false",
+                    "#3 isMoveWrong 반환값: true",
+                    "[ O |   | O ]",
+                    "[   | O |   ]",
+                    "#4 isEnd 반환값: true",
+                    "#4 isMoveWrong 반환값: false",
+                    "최종 게임 결과",
+                    "[ O |   | O ]",
+                    "[   | O |   ]",
+                    "게임 성공 여부: 성공",
+                    "총 시도한 횟수: 1000000"
+            );
+        });
+    }
+
+    @Override
+    protected void runMain() {
+        List<String> bridge = List.of("U", "D", "U");
+        bridgeGame = new BridgeGame(bridge);
+
+        int move = 1;
+        bridgeGame.move();
+        bridgeGame.printMap();
+        System.out.printf("#%d isEnd 반환값: %b\n", move, bridgeGame.isEnd());
+        System.out.printf("#%d isMoveWrong 반환값: %b\n", move, bridgeGame.isMoveWrong());
+        System.out.println();
+
+        move++;
+        bridgeGame.move();
+        bridgeGame.printMap();
+        System.out.printf("#%d isEnd 반환값: %b\n", move, bridgeGame.isEnd());
+        System.out.printf("#%d isMoveWrong 반환값: %b\n", move, bridgeGame.isMoveWrong());
+        System.out.println();
+
+        move++;
+        bridgeGame.move();
+        bridgeGame.printMap();
+        System.out.printf("#%d isEnd 반환값: %b\n", move, bridgeGame.isEnd());
+        System.out.printf("#%d isMoveWrong 반환값: %b\n", move, bridgeGame.isMoveWrong());
+        System.out.println();
+
+        move++;
+        bridgeGame.retry();
+        bridgeGame.printMap();
+        System.out.printf("#%d isEnd 반환값: %b\n", move, bridgeGame.isEnd());
+        System.out.printf("#%d isMoveWrong 반환값: %b\n", move, bridgeGame.isMoveWrong());
+
+        bridgeGame.printResult(1000000);
+    }
+}
