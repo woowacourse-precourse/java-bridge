bc5c0fff7155417364679c1b3788dc91ea06893d
dooboocookie <bkh3030@gmail.com>
2022-11-21 18:01:57 +0900

    test: 경로에서 현재 위치와 발판을 가져오는 테스트

--- src/test/java/bridge/model/PlayerPathTest.java
+++ src/test/java/bridge/model/PlayerPathTest.java
@@ -1,6 +1,7 @@
 package bridge.model;
 
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -10,18 +11,31 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class PlayerPathTest {
 
-    @DisplayName("다음칸으로 넘어가는 테스트")
-    @Test
-    void nextStepTest() {
-        PlayerPath playerPath = new PlayerPath();
+    static PlayerPath playerPath;
+
+    @BeforeAll
+    static void beforeAll() {
+        playerPath = new PlayerPath();
         playerPath.nextStep(Plate.UP_PLATE);
         playerPath.nextStep(Plate.DOWN_PLATE);
         playerPath.nextStep(Plate.UP_PLATE);
-        List<Plate> playerPlates = playerPath.getPlayerPath();
+    }
 
+    @DisplayName("다음칸으로 넘어가는 테스트")
+    @Test
+    void nextStepTest() {
+        List<Plate> playerPlates = playerPath.getPlayerPath();
         assertThat(playerPlates)
                 .containsExactly(Plate.UP_PLATE, Plate.DOWN_PLATE, Plate.DOWN_PLATE);
     }
 
-    
+    @DisplayName("경로에서 현재 위치와 발판을 가져오는 테스트")
+    @Test
+    void  currentPositionAndPlateTest() {
+        Plate currentPlate = playerPath.currentPlate();
+        int currentPosition = playerPath.currentPosition();
+
+        assertThat(currentPlate).isEqualTo(Plate.UP_PLATE);
+        assertThat(currentPosition).isEqualTo(2);
+    }
 }
