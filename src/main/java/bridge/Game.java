00052a78179464d2e0a17559bd2a3e55e22131fc
lms0806 <godmlzkf1@naver.com>
2022-11-21 21:55:08 +0900

    feat:게임 입력까지 구현

--- src/main/java/bridge/Game.java
+++ src/main/java/bridge/Game.java
@@ -1,5 +1,27 @@
 package bridge;
 
-public class Game {
+import java.util.List;
 
+public class Game {
+	private final OutputView outputview = new OutputView();
+	private final InputView inputview = new InputView();
+	
+	public void start() {
+		outputview.printgamestart();
+		List<String> bridge = makebridge();
+		//탈출 or 게임 끝나기 전까지 무한 게임 진행
+		//결과 출력
+	}
+	
+	public List<String> makebridge(){
+		outputview.printinputbridgesize();
+		
+		int size = inputview.readBridgeSize();
+		
+		BridgeMaker bridgemaker = new BridgeMaker(new BridgeRandomNumberGenerator());
+		
+		return bridgemaker.makeBridge(size);
+	}
+	
+	
 }
