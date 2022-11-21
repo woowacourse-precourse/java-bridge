8de3228fc2cc89c67ac86bd9addea3bbb505f9fa
yujaemin <woawoa0513@gmail.com>
2022-11-21 14:41:47 +0900

    test: 이동할 칸 유효성 검사 테스트
    이동할 칸으로 'U' 또는 'D'가 아닌 값을 입력했을 떄 예외를 발생시키는지 테스트

--- src/test/java/bridge/utils/ValidationTest.java
+++ src/test/java/bridge/utils/ValidationTest.java
@@ -2,8 +2,10 @@ package bridge.utils;
 
 import static bridge.domain.ErrorMessage.BRIDGE_SIZE_INCORRECT_INPUT;
 import static bridge.domain.ErrorMessage.BRIDGE_INCORRECT_SIZE;
+import static bridge.domain.ErrorMessage.MOVING_INCORRECT_INPUT;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
@@ -26,6 +28,17 @@ class ValidationTest {
             Validation.validateBridgeSize(input);
         } catch (IllegalArgumentException e) {
             Assertions.assertEquals(BRIDGE_INCORRECT_SIZE.toString(), e.getMessage());
+        }
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"UP", "DOWN", "M", "u", "d"})
+    @DisplayName("이동할 칸에 'U' 또는 'D'가 아닌 값을 입력하면 예외 발생")
+    void 이동할_칸_U_또는_D가_아닌_입력(String input) {
+        try {
+            Validation.validateMoving(input);
+        } catch (IllegalArgumentException e) {
+            Assertions.assertEquals(MOVING_INCORRECT_INPUT.toString(), e.getMessage());
         }
     }
 }
