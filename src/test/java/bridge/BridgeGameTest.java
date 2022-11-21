b1c9ae3fb2634aaadf60c28ba6af8da7bfcfcf31
lsw725 <725lsw@hanmail.net>
2022-11-22 02:33:49 +0900

    feat(BridgeGame): 게임을 재시작하는 기능 구현

--- src/test/java/bridge/BridgeGameTest.java
+++ src/test/java/bridge/BridgeGameTest.java
@@ -50,6 +50,23 @@ class BridgeGameTest extends NsTest {
         },1, 0, 1);
     }
 
+    @DisplayName("실패한 게임을 재시작한다.")
+    @Test
+    void retry() {
+        assertRandomNumberInRangeTest(() -> {
+            run("3", "U", "U", "R", "U", "D", "U");
+            assertThat(output()).contains(
+                    "[ O ]", "[   ]",
+                    "[ O | X ]", "[   |   ]",
+                    "[ O ]", "[   ]",
+                    "[ O |   ]", "[   | O ]",
+                    "[ O |   | O ]", "[   | O |   ]",
+                    "게임 성공 여부: 성공",
+                    "총 시도한 횟수: 2"
+            );
+        },1, 0, 1);
+    }
+
     @Override
     protected void runMain() {
         game.start();
