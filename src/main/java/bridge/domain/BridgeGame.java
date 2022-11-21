420851b4f5f57260efdb336f816bf4c6a4ef30c1
Yerim lee <103623292+lyrWinterCat@users.noreply.github.com>
2022-11-21 14:43:21 +0900

    refactor (BridgeGame) : 게임 관리자 구성 수정

--- src/main/java/bridge/domain/BridgeGame.java
+++ src/main/java/bridge/domain/BridgeGame.java
@@ -14,18 +14,29 @@ public class BridgeGame {
     private Bridge bridge;
     private User user;
 
+    public BridgeGame() {
+        user = new User();
+    }
+
     public void setBridge(List<String> makeBridge){
         bridge = new Bridge(makeBridge);
     }
 
+    public void setUser(){
+        user = new User();
+    }
+
+    public User getUser() {
+        return user;
+    }
 
     /**
      * 사용자가 칸을 이동할 때 사용하는 메서드
      * <p>
      * 이동을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void move(String moveMessage) {
-        user.addUserMoving(moveMessage);
+    public void move(String userMove) {
+        user.addUserMoving(userMove);
     }
 
     /**
@@ -33,8 +44,8 @@ public class BridgeGame {
      * <p>
      * 재시작을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void retry(String restartMessage) {
-        if(restartMessage.equals("R")){
+    public void retry(String restartInput) {
+        if(restartInput.equals("R")){
             user.clearUserMoving();
         }
     }
