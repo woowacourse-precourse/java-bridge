b1a4566354d12c685b0f9ddc23626768d0abc509
wonju-dev <wonjulee.dev@gmail.com>
2022-11-22 03:38:55 +0900

    feat(OutputView): OutputView 싱글톤 적용

--- src/main/java/bridge/OutputView.java
+++ src/main/java/bridge/OutputView.java
@@ -4,6 +4,14 @@ package bridge;
  * 사용자에게 게임 진행 상황과 결과를 출력하는 역할을 한다.
  */
 public class OutputView {
+    private static final OutputView instance = new OutputView();
+
+    private OutputView() {
+    }
+
+    public static OutputView getInstance() {
+        return instance;
+    }
 
     /**
      * 현재까지 이동한 다리의 상태를 정해진 형식에 맞춰 출력한다.
