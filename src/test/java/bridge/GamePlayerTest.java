a9bc8e5eb359979282f127b5e2758b53016af63c
JuHyeokParkk <hyeok5596@naver.com>
2022-11-22 01:41:52 +0900

    test: 한 번의 재시도 후 다리 건너기에 성공하는 테스트 코드 작성

--- src/test/java/bridge/GamePlayerTest.java
+++ src/test/java/bridge/GamePlayerTest.java
@@ -23,7 +23,7 @@ public class GamePlayerTest {
     private List<Integer> bridgeNumbers = new ArrayList<>(Arrays.asList(1, 0, 1));
     private GamePlayer gamePlayer = new GamePlayer(new MockNumberGeneratorDependencyContainerImpl(bridgeNumbers));
 
-    private final List<String> resultOutput = List.of("다리 건너기 게임을 시작합니다.",
+    private final List<String> noRetryResultOutput = List.of("다리 건너기 게임을 시작합니다.",
             "다리의 길이를 입력해주세요.", "이동할 칸을 선택해주세요. (위: U, 아래: D)",
             "[ O ]",
             "[   ]",
@@ -38,7 +38,32 @@ public class GamePlayerTest {
             "[   | O |   ]",
             "게임 성공 여부: 성공",
             "총 시도한 횟수: 1");
-
+    private final List<String> oneRetryResultOutput = List.of("다리 건너기 게임을 시작합니다.",
+            "다리의 길이를 입력해주세요.",
+            "이동할 칸을 선택해주세요. (위: U, 아래: D)",
+            "[ O ]",
+            "[   ]",
+            "이동할 칸을 선택해주세요. (위: U, 아래: D)",
+            "[ O |   ]",
+            "[   | O ]",
+            "이동할 칸을 선택해주세요. (위: U, 아래: D)",
+            "[ O |   |   ]",
+            "[   | O | X ]",
+            "게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)",
+            "이동할 칸을 선택해주세요. (위: U, 아래: D)",
+            "[ O ]",
+            "[   ]",
+            "이동할 칸을 선택해주세요. (위: U, 아래: D)",
+            "[ O |   ]",
+            "[   | O ]",
+            "이동할 칸을 선택해주세요. (위: U, 아래: D)",
+            "[ O |   | O ]",
+            "[   | O |   ]",
+            "최종 게임 결과",
+            "[ O |   | O ]",
+            "[   | O |   ]",
+            "게임 성공 여부: 성공",
+            "총 시도한 횟수: 2");
     @BeforeEach
     public void setUpStreams() {
         System.setOut(new PrintStream(outContent));
@@ -57,9 +82,22 @@ public class GamePlayerTest {
 
         gamePlayer.playGame();
 
-        String s = outContent.toString();
+        String gameResultOutput = outContent.toString();
 
-        Assertions.assertThat(s).contains(resultOutput);
+        Assertions.assertThat(gameResultOutput).contains(noRetryResultOutput);
+    }
+
+    @DisplayName("한 번의 재시도 후 다리 건너기에 성공한다.")
+    @Test
+    void 한_번의_재시도_후_게임_진행_전체_단계_테스트() {
+        String userInputs = "3\nU\nD\nD\nR\nU\nD\nU\n";
+        initInputStream(userInputs);
+
+        gamePlayer.playGame();
+
+        String gameResultOutput = outContent.toString();
+
+        Assertions.assertThat(gameResultOutput).contains(oneRetryResultOutput);
     }
 
     void initInputStream(String userInputs) {
