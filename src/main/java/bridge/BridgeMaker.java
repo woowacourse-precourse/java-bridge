3cbf4e058791d36e0a62fbd08306298fdfad25d9
jagajang <kosb7544@gmail.com>
2022-11-21 14:01:09 +0900

    refactor: simplify bridge making method by functional programming

--- src/main/java/bridge/BridgeMaker.java
+++ src/main/java/bridge/BridgeMaker.java
@@ -2,6 +2,8 @@ package bridge;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 /**
  * 다리의 길이를 입력 받아서 다리를 생성해주는 역할을 한다.
@@ -19,12 +21,8 @@ public class BridgeMaker {
      * @return 입력받은 길이에 해당하는 다리 모양. 위 칸이면 "U", 아래 칸이면 "D"로 표현해야 한다.
      */
     public List<String> makeBridge(int size) {
-        // System.out.println(Stream.generate(() -> bridgeNumberGenerator.generate()).limit(size));
-        List<String> bridge = new ArrayList<>();
-
-        for(int i = 0; i < size; i++) {
-            bridge.add(upDown.get(bridgeNumberGenerator.generate()));
-        }
-        return bridge;
+        return Stream.generate(() ->
+                upDown.get(bridgeNumberGenerator.generate())
+        ).limit(size).collect(Collectors.toList());
     }
 }
