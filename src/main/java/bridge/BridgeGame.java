e7619d46d6336cfcd84b8d6655d802db6e376144
unknown <ymecca12@gmail.com>
2022-11-21 13:55:35 +0900

    docs : 각 함수 설명 주석 작성

--- src/main/java/bridge/BridgeGame.java
+++ src/main/java/bridge/BridgeGame.java
@@ -39,8 +39,7 @@ public class BridgeGame {
     }
     /**
      * 사용자가 칸을 이동할 때 사용하는 메서드
-     * <p>
-     * 이동을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
+     * @param history : 사용자가 입력한 값을 누적 기록한 리스트
      */
     public void move(List<String> history, List<String> bridge) {
         String movement = input.readMoving();
@@ -53,6 +52,9 @@ public class BridgeGame {
         }
     }
 
+    /**
+     * 게임이 종료됐을 때, 종료 문구 출력 메서드
+     */
     private void printFinalResult(List<String> history, List<String> bridge) {
         if(history.equals(bridge)){
             System.out.println("최종 게임 결과");
@@ -62,8 +64,6 @@ public class BridgeGame {
 
     /**
      * 사용자가 게임을 다시 시도할 때 사용하는 메서드
-     * <p>
-     * 재시작을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
     public void retry(List<String> bridge) {
         if(input.readGameCommand().equals("R")){
@@ -71,6 +71,9 @@ public class BridgeGame {
             play(bridge);
         }
     }
+    /**
+     * 매번 입력을 받을 때마다 현재까지의 결과를 출력하는 함수
+     */
     public void makeResult(List<String> answer, List<String> history){
         List<String> ups = makeUpList(answer, history);
         List<String> downs = makeDownList(answer, history);
@@ -80,11 +83,17 @@ public class BridgeGame {
         output.printMap(result);
     }
 
+    /**
+     * 현재까지의 결과중 아래 리스트를 만드는 함수
+     */
     private List<String> makeDownList(List<String> answer, List<String> history) {
         DownCommand downCommand = new DownCommand(answer,history);
         return downCommand.executeAll(answer, history);
     }
 
+    /**
+     * 현재까지의 결과중 위 리스트를 만드는 함수
+     */
     private List<String> makeUpList(List<String> answer, List<String> history){
         UpCommand upCommand = new UpCommand(answer, history);
         return upCommand.executeAll(answer,history);
--- src/main/java/bridge/validation/InputValidate.java
+++ src/main/java/bridge/validation/InputValidate.java
@@ -10,25 +10,41 @@ public class InputValidate {
     private static final Integer MIN_INTEGER_SIZE = 3;
     private static final Integer MAX_INTEGER_SIZE = 20;
 
+    /**
+     * 다리 길이가 3 미만, 20 초과 이면 예외 처리
+     * @param length : 입력받은 다리 길이
+     * */
     public void validateSizeFormat(int length){
         if(length < MIN_INTEGER_SIZE || length > MAX_INTEGER_SIZE) {
             throw new IllegalArgumentException(BRIDGE_SIZE_MESSAGE);
         }
     }
+    /**
+     * 움직임 입력이 U 나 D가  아니면 예외 처리
+     * @param moving : 입력받은 움직임
+     * */
     public void validateMovingFormat(String moving){
         if(moving.indexOf("U") == -1 && moving.indexOf("D") == -1){
             throw new IllegalArgumentException(MOVING_FORMAT_MESSAGE);
         }
     }
+    /**
+     * 재시작 입력이 R, Q 가 아니면 예외 처리
+     * @param gameCommend : 입력받은 재시작 명령어 ( R, Q )
+     * */
     public void validateGameCommendFormat(String gameCommend){
         if(gameCommend.indexOf("R") == -1 && gameCommend.indexOf("Q") == -1){
             throw new IllegalArgumentException(RESTART_FORMAT_MESSAGE);
         }
     }
 
-    public void validateSizeIntFormat(String input) {
+    /**
+     * 입력받은 다리 길이가 숫자가 아닌 문자를 입력받았을 때 예외 처리
+     * @param length : 입력받은 다리 길이
+     * */
+    public void validateSizeIntFormat(String length) {
         try{
-            Double.parseDouble(input);
+            Double.parseDouble(length);
         }
         catch(NumberFormatException e){
             System.out.print(BRIDGE_INTEGER_MESSAGE);
