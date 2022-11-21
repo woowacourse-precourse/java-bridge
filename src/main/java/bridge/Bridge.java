7c094d9132548174374f7bf5500ff2134fa2cf7a
ingpyo <juninpyo72@gmail.com>
2022-11-21 20:56:18 +0900

    feat:랜덤의 다리를 저장하는 저장소

---
+++ src/main/java/bridge/Bridge.java
@@ -0,0 +1,12 @@
+package bridge;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Bridge {
+	private final List<String> bridge = new ArrayList<>();
+
+	public void setBridge(List<String> makeBridge) {
+		bridge.addAll(makeBridge);
+	}
+}
