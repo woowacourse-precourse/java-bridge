a670c9c2d1e3f09ce4038b445b1834feb953c7e8
thousandcows <milcamboo@gmail.com>
2022-11-21 22:26:51 +0900

    feat: create printRestart method

--- src/main/java/ui/OutputView.java
+++ src/main/java/ui/OutputView.java
@@ -17,6 +17,12 @@ public class OutputView {
         System.out.println("이동할 칸을 선택해주세요. (위: U, 아래: D)");
     }
     /**
+     * 게임을 재시작할지 안내하는 문구를 출력한다.
+     */
+    public void printRestart() {
+        System.out.println("게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)");
+    }
+    /**
      * 현재까지 이동한 다리의 상태를 정해진 형식에 맞춰 출력한다.
      * <p>
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
