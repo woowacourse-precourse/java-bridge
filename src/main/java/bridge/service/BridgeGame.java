3e94f28ebfcc7cfc4d7775534acb9bdc2bb47d5a
gomljo <gomljo0307@naver.com>
2022-11-22 07:02:04 +0900

    feat(BridgeGame): 게임 상태 초기화 기능 구현

---
+++ src/main/java/bridge/service/BridgeGame.java
@@ -0,0 +1,39 @@
+package bridge.service;
+
+import bridge.BridgeMaker;
+import bridge.BridgeNumberGenerator;
+import bridge.BridgeRandomNumberGenerator;
+import bridge.domain.constant.CrossingState;
+import bridge.domain.model.GameRecordGenerator;
+import bridge.domain.model.GameRecordMaker;
+import bridge.domain.model.GameAgent;
+
+import java.util.List;
+
+/**
+ * 다리 건너기 게임을 관리하는 클래스
+ */
+public class BridgeGame {
+    private static final int ZERO=0;
+    private static final int COUNT=1;
+
+    private GameAgent gameAgent;
+    private GameRecordMaker gameRecordMaker;
+    private GameRecordGenerator gameRecordGenerator;
+    private int numberOfTrial;
+    private int currentBridgeLocation;
+    private boolean gameState;
+    private int bridgeLength;
+    public BridgeGame(){
+        this.bridgeLength=ZERO;
+        gameAgent=new GameAgent();
+        gameRecordGenerator=new GameRecordGenerator();
+        this.numberOfTrial=ZERO;
+    }
+    public void prepare(){
+        this.currentBridgeLocation=ZERO;
+        this.gameState=true;
+        this.gameRecordMaker=new GameRecordMaker();
+    }
+
+}
