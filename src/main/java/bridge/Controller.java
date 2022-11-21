e7aff56cd41d8e62e8e0fd7a01a9af19fba4bb47
shin5774 <babo5774@naver.com>
2022-11-22 00:57:08 +0900

    feat: 다리크기 입력 및 이를 통한 다리 구현 기능
    -Controller 클래스에 기존에 구현했던 기능들을 묶어서 startGame이라는 메서드에 넣음. 이 메서드는 사용자로부터 다리 길이를 입력받고 이를 토대로 다리를 만드는 기능을 한다.

--- src/main/java/bridge/Controller.java
+++
@@ -1,8 +0,0 @@
-package bridge;
-
-public class Controller {
-    public void startProgram(){
-        System.out.println("다리 건너기 게임을 시작합니다.");
-        System.out.println();
-    }
-}
---
+++ src/main/java/bridge/controller/Controller.java
@@ -0,0 +1,64 @@
+package bridge;
+
+import java.util.List;
+
+public class Controller {
+
+    private static InputView inputView;
+    private static OutputView outputView;
+    private BridgeGame bridgeGame;
+    private SingleMap singleMap;
+
+    Controller(){
+        inputView=new InputView();
+        outputView=new OutputView();
+    }
+    public void startProgram(){
+        System.out.println("다리 건너기 게임을 시작합니다.");
+        System.out.println();
+    }
+
+    //아직 커밋안함
+    public void startGame(){
+        int size=inputView.readBridgeSize();
+        try {
+            makeBridge(size);
+        }catch (IllegalStateException e){
+            System.out.println(e.getMessage());
+            makeBridge(size);
+        }
+    }
+
+    private void makeBridge(int size){
+        List<String> bridge=new BridgeMaker(new BridgeRandomNumberGenerator()).makeBridge(size);
+        bridgeGame=new BridgeGame(bridge);
+    }
+
+    public boolean playGame(){
+        do{
+            String move=inputView.readMoving();
+            bridgeGame.move(move);
+            singleMap=new SingleMap(bridgeGame,move);
+            printMap();
+        }while(bridgeGame.isSuccess()&&bridgeGame.checkNotGoal());
+        return bridgeGame.isSuccess();
+    }
+
+    private void printMap(){
+        outputView.printMap(singleMap.convertToSingleMap("U"));
+        outputView.printMap(singleMap.convertToSingleMap("D"));
+    }
+
+    public boolean askRestart(){
+        String command=inputView.readGameCommand();
+        if(command.matches("R")){
+            return true;
+        }
+        return false;
+    }
+
+    public void printResult(){
+        outputView.printResult(singleMap);
+    }
+
+}
