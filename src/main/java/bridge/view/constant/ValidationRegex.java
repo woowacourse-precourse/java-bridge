0a13455c07087a4df9b0bc6768ea24386d8f3e0d
skydreamer21 <skydreamer210@gmail.com>
2022-11-21 17:16:51 +0900

    feat(ValidationRegex): 재시작 옵션 유효성 검증에 대한 정규표현식 추가

--- src/main/java/bridge/view/constant/ValidationRegex.java
+++ src/main/java/bridge/view/constant/ValidationRegex.java
@@ -3,7 +3,8 @@ package bridge.view.constant;
 public enum ValidationRegex {
     EMPTY_VALUE("^$"),
     NUMBER("^[0-9]+$"),
-    VALID_PANEL("^[UD]+$");
+    VALID_PANEL("^[UD]+$"),
+    VALID_RETRY_OPTION("^[RQ]+$");
 
     private final String regex;
 
