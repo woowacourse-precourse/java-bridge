c775fa3a768d7310d6861af6090e911d03de4a48
Kid-Chang <pch3310@gmail.com>
2022-11-22 03:48:10 +0900

    refactor(Bridge): 에러 메시지를 ErrorMsgs라는 클래스에서 통합적으로 관리할 수 있도록 변경

--- src/main/java/bridge/utils/Validator.java
+++ src/main/java/bridge/utils/Validator.java
@@ -1,18 +1,21 @@
 package bridge.utils;
 
 public class Validator {
-    public static String ERROR_BRIDGE_SIZE_NOT_INT = "[ERROR] 받은 다리 길이가 숫자가 아닙니다.";
-    public static String ERROR_BRIDGE_SIZE_LOW = "[ERROR] 입력하는 다리 길이가 3 이상 20 이하의 숫자이어야 합니다.";
 
     public static void checkValueOfReadBridgeSize(Integer value) {
         if (isNumeric(value))
-            throw new IllegalArgumentException(ERROR_BRIDGE_SIZE_NOT_INT);
+            throw new IllegalArgumentException(ErrorMsgs.ERROR_BRIDGE_SIZE_NOT_INT);
         if (value < 3 || value > 20)
-            throw new IllegalArgumentException(ERROR_BRIDGE_SIZE_LOW);
+            throw new IllegalArgumentException(ErrorMsgs.ERROR_BRIDGE_SIZE_LOW);
     }
 
     public static boolean isNumeric(Integer s) {
         String value = Integer.toString(s);
         return value == null || !value.matches("[-+]?\\d*\\.?\\d+");
+    }
+
+    public static void checkValueOfReadBridgeMove(String value) {
+        if (value != "U" || value != "R")
+            throw new IllegalArgumentException(ErrorMsgs.ERROR_BRIDGE_MOVE_TYPE);
     }
 }
