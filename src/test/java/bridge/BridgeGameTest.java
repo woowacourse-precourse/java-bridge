67f2bf3e4278a477c68e58bd2a3a80f221229c1b
이현진 <ehj1107@naver.com>
2022-11-22 05:48:51 +0900

    test: 사용자가 칸을 이동하는 메서드의 테스트

--- src/test/java/bridge/BridgeGameTest.java
+++ src/test/java/bridge/BridgeGameTest.java
@@ -3,14 +3,31 @@ package bridge;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
 import java.util.NoSuchElementException;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.assertj.core.util.Lists.newArrayList;
 import static org.junit.jupiter.api.Assertions.*;
 
 class BridgeGameTest {
     BridgeGame bridgeGame = new BridgeGame();
+
+    @DisplayName("사용자가 칸을 이동할 때 정상적으로 작동하는지 체크")
+    @Test
+    void 사용자가_칸을_이동할_때_정상적으로_작동하는지_체크(){
+        BridgeNumberGenerator numberGenerator = new ApplicationTest.TestNumberGenerator(newArrayList(1, 0, 0));
+        BridgeMaker bridgeMaker = new BridgeMaker(numberGenerator);
+        List<String> bridge = bridgeMaker.makeBridge(3);
+        Application.bridge = bridge;
+
+        assertThat(bridgeGame.move("U")).isEqualTo(true);
+        assertThat(bridgeGame.move("D")).isEqualTo(true);
+        assertThat(bridgeGame.move("U")).isEqualTo(false);
+    }
+
+
 
     @DisplayName("사용자가 게임 재시작을 선택하는 경우")
     @Test
