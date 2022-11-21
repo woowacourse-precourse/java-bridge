fea21eab1da22e29a4a6a16d92a9d898fe3e1979
DaeyeonKim97 <kdysy1130@khu.ac.kr>
2022-11-21 09:56:01 +0900

    test(bridge-game) : move method
    test by
    - whether move resultMap doing well

--- src/test/java/bridge/BridgeGameTest.java
+++ src/test/java/bridge/BridgeGameTest.java
@@ -4,6 +4,7 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import java.lang.reflect.Field;
 import java.util.List;
+import java.util.Map;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
@@ -26,5 +27,24 @@ class BridgeGameTest {
         //then
         assertEquals(3, bridge.size());
     }
+
+    @Test
+    void move_테스트() throws NoSuchFieldException, IllegalAccessException {
+        //given
+        Field field = bridgeGame.getClass().getDeclaredField("bridge");
+        field.setAccessible(true);
+        //when
+        bridgeGame.start(3);
+        List<String> bridge = (List<String>) field.get(bridgeGame);
+        Map<String,List<String>> resultMap =  bridgeGame.move("U");
+        //then
+        if (bridge.get(0) == "U"){
+            assertEquals(List.of("O"),resultMap.get("upperLine"));
+        }
+        if (bridge.get(0) == "D"){
+            assertEquals(List.of("X"),resultMap.get("upperLine"));
+        }
+    }
+
 
 }
