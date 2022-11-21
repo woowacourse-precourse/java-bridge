5b5aa22764d4cae760702e34a3bac1ec1b62e408
yangjiyoung <0112jyoung@naver.com>
2022-11-21 10:31:21 +0900

    style: TODO 제거

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -3,7 +3,6 @@ package bridge;
 public class Application {
 
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
         BridgeGame bridgeGame=new BridgeGame();
         bridgeGame.start();
     }
