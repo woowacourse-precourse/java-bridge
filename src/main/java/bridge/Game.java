cea44014d5259bfe6cbc7be7ced5d1e192573f70
dsaf2007 <dsaf2007@naver.com>
2022-11-22 02:59:26 +0900

    feat : 게임을 진행하는 메인루프 추가

---
+++ src/main/java/bridge/Game.java
@@ -0,0 +1,55 @@
+package bridge;
+
+/*
+ * 게임을 진행하는 클래스
+ */
+
+public class Game {
+
+    private InputView inputView = new InputView();
+    private BridgeGame bridgeGame;
+
+    public void run() {
+        initGame();
+        playGame();
+        endGame();
+    }
+
+    public void initGame() {
+        int size = 0;
+        while (size == 0) {
+            OutputView.gameStart();
+            size = inputView.readBridgeSize();
+            this.bridgeGame = new BridgeGame(size);
+        }
+    }
+
+    public void playGame() {
+        while (true) {
+            String move = inputView.readMoving();
+            String result = bridgeGame.move(move);
+            if (retryGame(bridgeGame.isGameOver(result)) == false) {
+                break;
+            }
+            if (bridgeGame.isSuccess()) {
+                break;
+            }
+        }
+    }
+
+    public void endGame() {
+        bridgeGame.getResult();
+    }
+
+    public boolean retryGame(boolean isGameOver) {
+        if (isGameOver == true) {
+            String command = inputView.readGameCommand();
+            if (bridgeGame.retry(command)) {
+                return true;
+            }
+            return false;
+        }
+        return true;
+    }
+
+}
