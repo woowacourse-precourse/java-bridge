02f07cdd9e5e854fc8242f5285fce4b1a280acd6
nn98 <jkllhgb@gmail.com>
2022-11-21 17:10:09 +0900

    Test: FunctionTest

--- src/test/java/bridge/BridgeMakerTest.java
+++ src/test/java/bridge/BridgeMakerTest.java
@@ -1,14 +1,33 @@
 package bridge;
 
 import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.assertj.core.util.Lists.newArrayList;
 
+import bridge.ApplicationTest.TestNumberGenerator;
 import camp.nextstep.edu.missionutils.test.NsTest;
+import java.util.List;
 import org.junit.jupiter.api.Test;
 
 public class BridgeMakerTest extends NsTest {
 
     private static final String ERROR_MESSAGE = "[ERROR]";
+
+    @Test
+    void 기능_테스트() {
+        BridgeNumberGenerator numberGenerator = new TestNumberGenerator(newArrayList(1, 0, 0));
+        BridgeMaker bridgeMaker = new BridgeMaker(numberGenerator);
+        List<String> bridge = bridgeMaker.makeBridge(3);
+        assertThat(bridge).containsExactly("U", "D", "D");
+    }
+
+    @Test
+    void 다리_생성_테스트() {
+        BridgeNumberGenerator numberGenerator = new TestNumberGenerator(newArrayList(1, 0, 0));
+        BridgeMaker bridgeMaker = new BridgeMaker(numberGenerator);
+        bridgeMaker.makeBridge(3);
+    }
 
     @Test
     void 예외_테스트_다리길이_음수() {
