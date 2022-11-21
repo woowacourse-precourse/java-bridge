710c199344ec6118f32b4e9434fdd5955a7783f6
이건회 <rg970604@naver.com>
2022-11-22 03:33:30 +0900

    feat(BridgeGame.java) : 재시작 기능, 다리 객체 생성 기능, 최종 결과 저장 기능, 시도 횟수 더하기 기능, 종료 가능 여부 판단 기능

--- src/main/java/bridge/service/BridgeGame.java
+++ src/main/java/bridge/service/BridgeGame.java
@@ -1,6 +1,12 @@
 package bridge.service;
 
+import bridge.BridgeMaker;
+import bridge.config.AppConfig;
 import bridge.domain.Bridge;
+import bridge.domain.TryCount;
+import bridge.rule.EndRule;
+import bridge.util.Constant;
+import java.util.List;
 
 /**
  * 다리 건너기 게임을 관리하는 클래스
@@ -12,7 +18,7 @@ public class BridgeGame {
      * <p>
      * 이동을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void move(Bridge bridge) {
+    public static void move(Bridge bridge) {
         bridge.step();
     }
 
@@ -21,6 +27,44 @@ public class BridgeGame {
      * <p>
      * 재시작을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void retry() {
+    public static TryCount retry(Bridge bridge, TryCount tryCount) {
+        bridge.removeUnsafeSpot();
+        return tryCount.addCount();
+    }
+
+    public static Bridge generateBridge(int bridgeSize) {
+        BridgeMaker bridgeMaker = new BridgeMaker(AppConfig.selectNumberGenerator());
+        List<String> baseBridge = bridgeMaker.makeBridge(bridgeSize);
+        return new Bridge(Constant.FIRSTPOSITION, baseBridge);
+    }
+
+    public static TryCount getFinalResult(Bridge bridge, TryCount tryCount) {
+        while (!bridge.isLastStep()) {
+            String moveTo = LocationSelector.selectMoveTo(bridge);
+            if (isQuit(bridge, moveTo)) {
+                return tryCount;
+            }
+            tryCount = moveOrRetry(bridge, tryCount, moveTo);
+        }
+        return tryCount;
+    }
+
+    private static TryCount moveOrRetry(Bridge bridge, TryCount tryCount, String moveTo) {
+        if (!bridge.isSafe(moveTo)) {
+            tryCount = BridgeGame.retry(bridge, tryCount);
+        }
+        if (bridge.isSafe(moveTo)) {
+            BridgeGame.move(bridge);
+        }
+        return tryCount;
+    }
+
+    private static boolean isQuit(Bridge bridge, String moveTo) {
+        if (!bridge.isSafe(moveTo)) {
+            if (EndRule.selectQuit()) {
+                return true;
+            }
+        }
+        return false;
     }
 }
