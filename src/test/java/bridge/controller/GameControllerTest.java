edf34262e730e58980cea5a0504f80ef8c66dd45
junhei Lee <jun2ee22@naver.com>
2022-11-21 18:13:38 +0900

    style: test 정렬 정돈 및 기능목록 수정

--- src/test/java/bridge/controller/GameControllerTest.java
+++ src/test/java/bridge/controller/GameControllerTest.java
@@ -2,6 +2,7 @@ package bridge.controller;
 
 import camp.nextstep.edu.missionutils.test.NsTest;
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -11,25 +12,27 @@ public class GameControllerTest extends NsTest {
 
     BridgeGameController gameController = new BridgeGameController();
 
-    @DisplayName("U, D, U, U, U 다리 1시도 성공")
+    @BeforeEach
+    void setBridge() {
+        gameController.setBridge(List.of("U", "D", "D"));
+    }
+    @DisplayName("다리 1시도 성공")
     @Test
     void controllerSuccessByOneTryTest() {
-        gameController.setBridge(List.of("U", "D", "U", "U", "U"));
-        run("U", "D", "U", "U", "U");
+        run("U", "D", "D");
 
         Assertions.assertThat(output()).contains(
                 "최종 게임 결과",
-                "[ O |   | O | O | O ]",
-                "[   | O |   |   |   ]",
+                "[ O |   |   ]",
+                "[   | O | O ]",
                 "게임 성공 여부: 성공",
                 "총 시도한 횟수: 1"
         );
     }
 
-    @DisplayName("U, D, D 다리 2시도 성공")
+    @DisplayName("다리 2시도 성공")
     @Test
     void controllerSuccessByTwoTryTest() {
-        gameController.setBridge(List.of("U", "D", "D"));
         run("U", "U", "R", "U", "D", "D");
 
         Assertions.assertThat(output()).contains(
@@ -44,10 +47,9 @@ public class GameControllerTest extends NsTest {
         );
     }
 
-    @DisplayName("U, D, D 다리 1시도 실패")
+    @DisplayName("다리 1시도 실패")
     @Test
     void controllerFailByOneTryTest() {
-        gameController.setBridge(List.of("U", "D", "D"));
         run("U", "D", "U", "Q");
 
         Assertions.assertThat(output()).contains(
--- src/test/java/bridge/model/BridgeGameTest.java
+++ src/test/java/bridge/model/BridgeGameTest.java
@@ -1,7 +1,6 @@
 package bridge.model;
 
 import org.assertj.core.api.Assertions;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -11,22 +10,23 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class BridgeGameTest {
     BridgeGame bridgeGame = new BridgeGame();
+    int bridgeSize = 3;
 
 
-    @DisplayName("다리가 위 칸일 때, 사용자가 U과 D을 입력한 경우 테스트")
+    @DisplayName("다리가 위 칸일 때, 사용자가 U와 D를 입력한 경우 테스트")
     @Test
-    void saveStageWhenBridgeStageIsUp() {
-        bridgeGame.move("U", "U", 5);
-        bridgeGame.move("D", "U", 5);
+    void moveStageWhenBridgeStageIsUpTest() {
+        bridgeGame.move("U", "U", bridgeSize);
+        bridgeGame.move("D", "U", bridgeSize);
 
         Assertions.assertThat(bridgeGame.getCurrentPositions()).isEqualTo(List.of(List.of(" ", "O"), List.of("X", " ")));
     }
 
-    @DisplayName("다리가 아래 칸일 때, 사용자가 U과 D을 입력한 경우 테스트")
+    @DisplayName("다리가 아래 칸일 때, 사용자가  U와 D를 입력한 경우 테스트")
     @Test
-    void saveStageWhenBridgeStageIsDown() {
-        bridgeGame.move("U", "D", 5);
-        bridgeGame.move("D", "D", 5);
+    void moveStageWhenBridgeStageIsDownTest() {
+        bridgeGame.move("U", "D", bridgeSize);
+        bridgeGame.move("D", "D", bridgeSize);
 
         Assertions.assertThat(bridgeGame.getCurrentPositions()).isEqualTo(List.of(List.of(" ", "X"), List.of("O", " ")));
     }
@@ -34,45 +34,57 @@ public class BridgeGameTest {
 
     @DisplayName("재시작하는 경우 초기화 확인 테스트")
     @Test
-    void retryThenCurrentMapClear() {
-        bridgeGame.move("D", "U", 5);
-        bridgeGame.move("D", "D", 5);
+    void retryThenCurrentMapClearTest() {
+        bridgeGame.move("D", "U", bridgeSize);
+        bridgeGame.move("D", "D", bridgeSize);
         bridgeGame.retry();
 
         Assertions.assertThat(bridgeGame.getCurrentPositions()).isEmpty();
     }
 
-    @DisplayName("맞히기만 경우 canNotCross()가 false 반환 테스트")
+    @DisplayName("틀려서 초기화한 경우 사이즈와 시도 횟수를 확인하는 테스트")
     @Test
-    void success() {
-        bridgeGame.move("U", "U", 5);
-        bridgeGame.move("D", "D", 5);
+    void retry() {
+        bridgeGame.move("U", "U", bridgeSize);
+        bridgeGame.move("D", "D", bridgeSize);
+        bridgeGame.move("U", "D", bridgeSize);
+        bridgeGame.retry();
+
+        assertThat(bridgeGame.nextIndex()).isEqualTo(0);
+        assertThat(bridgeGame.getNumberOfAttempts()).isEqualTo(2);
+    }
+
+    @DisplayName("맞히기만 경우 isFail()가 false 반환 테스트")
+    @Test
+    void isNotFailTest() {
+        bridgeGame.move("U", "U", bridgeSize);
+        bridgeGame.move("D", "D", bridgeSize);
         assertThat(bridgeGame.isFail()).isFalse();
         assertThat(bridgeGame.nextIndex()).isEqualTo(2);
     }
 
-    @DisplayName("틀린 경우 canNotCross()가 True 반환 테스트")
+    @DisplayName("틀린 경우 isFail()가 True 반환 테스트")
     @Test
-    void fail() {
-        bridgeGame.move("U", "U", 5);
-        bridgeGame.move("D", "D", 5);
-        bridgeGame.move("U", "D", 5);
+    void isFailTest() {
+        bridgeGame.move("U", "U", bridgeSize);
+        bridgeGame.move("D", "D", bridgeSize);
+        bridgeGame.move("U", "D", bridgeSize);
 
         assertThat(bridgeGame.isFail()).isTrue();
         assertThat(bridgeGame.nextIndex()).isEqualTo(3);
         assertThat(bridgeGame.getOutcome()).isEqualTo("실패");
     }
 
-    @DisplayName("틀려서 초기화한 경우 사이즈와 시도 횟수를 확인하는 테스트")
+    @DisplayName("맞은 경우 isSuccess()가 True 반환 테스트")
     @Test
-    void retry() {
-        bridgeGame.move("U", "U", 5);
-        bridgeGame.move("D", "D", 5);
-        bridgeGame.move("U", "D", 5);
-        bridgeGame.retry();
+    void isSuccessTest() {
+        bridgeGame.move("U", "U", bridgeSize);
+        bridgeGame.move("D", "D", bridgeSize);
+        bridgeGame.move("U", "U", bridgeSize);
 
-        assertThat(bridgeGame.nextIndex()).isEqualTo(0);
-        assertThat(bridgeGame.getNumberOfAttempts()).isEqualTo(2);
-
+        assertThat(bridgeGame.isSuccess()).isTrue();
+        assertThat(bridgeGame.nextIndex()).isEqualTo(3);
+        assertThat(bridgeGame.getOutcome()).isEqualTo("성공");
     }
+
 }
