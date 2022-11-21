67dbfbacfd319782d71bd5d179124160a74191f0
egoing <dudqk9696@naver.com>
2022-11-21 14:38:50 +0900

    feat : 전체 흐름 구현

--- src/main/java/controller/GameController.java
+++ src/main/java/controller/GameController.java
@@ -1,18 +1,31 @@
 package controller;
 
-import bridge.InputView;
-import camp.nextstep.edu.missionutils.Console;
-import domain.AllBridge;
+import model.BridgeGame;
+import model.AllBridge;
+import view.InputMessage;
+import view.InputView;
+import view.OutputView;
 
 public class GameController {
-    InputView inputView = new InputView();
-    public void gameStart() {
-        try{
-            AllBridge allBridge = new AllBridge();
-        }
-        catch(IllegalArgumentException e) {
-            System.out.println(e);
-        }
+    InputController inputController = new InputController();
+    OutputView outputView = new OutputView();
+    BridgeGame bridgeGame = new BridgeGame();
 
+    public void gameStart() {
+        System.out.println(InputMessage.GAME_START.getInputMsg());
+        System.out.println(InputMessage.BRIDGE_LENGTH.getInputMsg());
+        AllBridge allBridge = new AllBridge();
+
+        game(allBridge);
+        outputView.printResult(bridgeGame);
+    }
+
+    public void game(AllBridge allBridge) {
+        while(true) {
+            System.out.println(InputMessage.MOVE.getInputMsg());
+            bridgeGame.move(inputController.getMoving(), allBridge);
+            outputView.printMap();
+            if(bridgeGame.retry(allBridge)) { break; }
+        }
     }
 }
