cae291dbace5654e333c1474928a19107d955465
comet7001 <comet7001@gmail.com>
2022-11-21 19:48:37 +0900

    feat(BridgeGameController): 초기 세팅 메서드(게임 시작을 알리고 다리의 길이를 입력받아 다리를 설정한다) 구현

--- src/main/java/bridge/BridgeGameController.java
+++ src/main/java/bridge/BridgeGameController.java
@@ -1,4 +1,23 @@
 package bridge;
 
+import Statements.NormalStatements;
+
+import java.util.List;
+
 public class BridgeGameController {
+    private static List<String> crossableBridge;
+    static InputView inputView = new InputView();
+    static OutputView outputView = new OutputView();
+
+    public BridgeGameController(List<String> crossableBridge){
+        this.crossableBridge = crossableBridge;
+    }
+
+    public static List<String> setBridgeGame() {
+        BridgeNumberGenerator bridgeRandomNumberGenerator = new BridgeRandomNumberGenerator();;
+        BridgeMaker bridgeMaker = new BridgeMaker(bridgeRandomNumberGenerator);
+        outputView.showNormalStatements(NormalStatements.GAME_START.getNormalStatement())
+        int bridgeLength = new InputView().readBridgeSize(); //Validator 객체를 사용해 유효성 검증 후 반환하게 한다.
+        return crossableBridge = bridgeMaker.makeBridge(bridgeLength);
+    }
 }
