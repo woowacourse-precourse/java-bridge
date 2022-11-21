6019e005049ad37b0466b1ad5ddaeda191a40bf1
0chil <0@chll.it>
2022-11-22 02:41:54 +0900

    refactor(Position): 메시지 상수화 취소

--- src/main/java/bridge/domain/Position.java
+++ src/main/java/bridge/domain/Position.java
@@ -4,7 +4,6 @@ public class Position {
 
     private static final int INCREASE_AMOUNT = 1;
     private static final int POSITION_MINIMUM = 0;
-    private static final String NEGATIVE_POSITION_EXCEPTION_MESSAGE = "위치는 음수일 수 없습니다";
 
     private final int position;
 
@@ -19,7 +18,7 @@ public class Position {
 
     private void validateRange(int position) {
         if (position < POSITION_MINIMUM) {
-            throw new IllegalArgumentException(NEGATIVE_POSITION_EXCEPTION_MESSAGE);
+            throw new IllegalArgumentException("위치는 음수일 수 없습니다");
         }
     }
 
