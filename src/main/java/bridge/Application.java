cbc148c144e64a50e11f3a4112c28d5d8d61a4fc
Choi Seongwon <vlflalektzja@naver.com>
2022-11-21 23:25:51 +0900

    feat: 게임 반복되게 구현

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -6,20 +6,50 @@ public class Application {
 
     static InputView inputView = new InputView();
     static OutputView outputView = new OutputView();
+    static int bridgeSize;
 
     public static void main(String[] args) {
         // TODO: 프로그램 구현
         try {
-            int bridgeSize = inputView.readBridgeSize();
+            bridgeSize = inputView.readBridgeSize();
             BridgeGame bridgeGame = new BridgeGame(bridgeSize);
-            run(bridgeGame);
+            start(bridgeGame);
         } catch (IllegalArgumentException e) {
             System.out.println(e.getMessage());
         }
     }
 
-    static void run(BridgeGame bridgeGame) {
+    private static void start(BridgeGame bridgeGame) throws IllegalArgumentException {
+        while (true) {
+            int status = run(bridgeGame);
+            if (status == 0) {
+                break;
+            }
+        }
+    }
+
+    private static int run(BridgeGame bridgeGame) throws IllegalArgumentException {
         String moving = inputView.readMoving();
         List<List<String>> userBridge = bridgeGame.move(moving);
+        outputView.printMap(userBridge);
+        if (!checkUserBridge(userBridge)) {
+            if (inputView.readGameCommand().equals("R")) {
+                bridgeGame.retry();
+            }
+            return -1;
+        }
+        return 0;
+    }
+
+    private static boolean checkUserBridge(List<List<String>> userBridge) {
+        int size = userBridge.get(0).size();
+        if (userBridge.get(BridgeGame.UPWARD).get(size - 1).equals("X") ||
+                userBridge.get(BridgeGame.DOWNWARD).get(size - 1).equals("X")) {
+            return false;
+        }
+        if (userBridge.size() != bridgeSize) {
+            return false;
+        }
+        return true;
     }
 }
