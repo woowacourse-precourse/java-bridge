d4e87b003d232a887ab053d4eae3fb6c9643b212
Isu Shin <isu010614@naver.com>
2022-11-21 18:28:24 +0900

    test: 건널 수 없는 다리를 선택한 경우를 확인할 수 있는지 테스트하는 코드 작성

--- src/test/java/bridge/domain/MovingStatusSaverTest.java
+++ src/test/java/bridge/domain/MovingStatusSaverTest.java
@@ -9,18 +9,18 @@ import java.util.List;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class MovingStatusSaverTest {
-    private final List<String> randomBridge = List.of("U", "D", "U");
+    private final List<String> randomBridge = List.of("U", "D", "U", "U");
     private final MovingStatusSaver saver = new MovingStatusSaver(new ArrayList<>(), randomBridge);
 
     @Test
     @DisplayName("마지막 이동 상태가 다른 경우")
     void validateSaveMovingStatus() {
-        final List<String> movingStatus = List.of("U", "D", "D");
+        final List<String> movingStatus = List.of("U", "D", "U", "D");
         for (String moving : movingStatus) {
             saver.saveMovingStatus(moving);
         }
         assertThat(saver.getMovingStatus())
-                .containsExactly("U", "D", "-D");
+                .containsExactly("U", "D", "U", "-D");
     }
 
     @Test
@@ -34,6 +34,15 @@ public class MovingStatusSaverTest {
                 .containsExactly("U", "D");
     }
 
-    // TODO: 정지 조건을 확인하는 테스트 코드 작성
+    @Test
+    @DisplayName("정지 조건: 건널 수 없는 다리를 선택한 경우")
+    void validateIsWrongBridge() {
+        final List<String> movingStatus = List.of("U", "D", "D");
+        for (String moving : movingStatus) {
+            saver.saveMovingStatus(moving);
+        }
+        assertThat(saver.isWrongBridge()).isTrue();
+    }
+
     // TODO: 이동상태를 저장한 후 길이를 반환하는 테스트 코드 작성
 }
