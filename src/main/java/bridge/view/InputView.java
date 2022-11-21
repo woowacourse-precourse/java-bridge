1e34f9eae7170941c906ec219620245ea7732531
matt <matt1235@naver.com>
2022-11-21 10:38:40 +0900

    fix: 사용자의 입력을 받는 기능 수정

--- src/main/java/bridge/view/InputView.java
+++ src/main/java/bridge/view/InputView.java
@@ -1,21 +1,71 @@
 package bridge.view;
 
+import bridge.util.InputValidator;
 import camp.nextstep.edu.missionutils.Console;
 
 public class InputView {
 
-    public int readBridgeSize() {
-        int bridgeSize = Integer.parseInt(Console.readLine());
-        return bridgeSize;
+    InputValidator inputValidator;
+
+    public InputView() {
+        this.inputValidator = new InputValidator();
+    }
+
+
+    public String readBridgeSize() {
+        System.out.println(Message.INPUT_MESSAGE_BRIDGE_LENGTH);
+        return Console.readLine();
+    }
+
+    public int readBridgeSizeUntilValidate(String bridgeLength, boolean flag){
+        do{
+            try{
+                bridgeLength = readBridgeSize();
+                inputValidator.validateBridgeLength(bridgeLength);
+                flag = false;
+            }catch (IllegalArgumentException e){
+                System.out.println(e.getMessage());
+            }
+        }while(flag);
+        return Integer.parseInt(bridgeLength);
     }
 
     public String readMoving() {
+        System.out.println(Message.INPUT_MESSAGE_MOVING);
         String movingLocation = Console.readLine();
         return movingLocation;
     }
 
+    public String readMovingUntilValidate(String moving, boolean flag){
+        do{
+            try{
+                moving = readMoving();
+                inputValidator.validateMoving(moving);
+                flag = false;
+            }catch (IllegalArgumentException e){
+                System.out.println(e.getMessage());
+            }
+        }while(flag);
+        return moving;
+    }
+
     public String readGameCommand() {
+        System.out.println(Message.INPUT_MESSAGE_GAME_COMMAND);
         String command = Console.readLine();
         return command;
     }
+
+    public String readGameCommandUntilValidate(String command, boolean flag){
+        do{
+            try{
+                command = readGameCommand();
+                inputValidator.validateGameCommand(command);
+                flag = false;
+            }catch (IllegalArgumentException e){
+                System.out.println(e.getMessage());
+            }
+        }while(flag);
+        return command;
+    }
+
 }
