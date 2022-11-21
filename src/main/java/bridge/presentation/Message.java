213960fa2cab2f6646e948e8dcb9e823a7503791
buddy <jang_taehwan@naver.com>
2022-11-22 03:21:52 +0900

    fix(OutputView): 누락된 메세지 추가

--- src/main/java/bridge/presentation/Message.java
+++ src/main/java/bridge/presentation/Message.java
@@ -7,6 +7,7 @@ public class Message {
     public static final String INPUT_MOVING = "이동할 칸을 선택해주세요. (위: U, 아래: D)";
     public static final String INPUT_COMMAND = "게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)";
 
+    public static final String OUTRO = "최종 게임 결과";
     public static final String OUTPUT_GAME_RESULT = "게임 성공 여부: ";
     public static final String OUTPUT_GAME_COUNT = "총 시도한 횟수: ";
 }
