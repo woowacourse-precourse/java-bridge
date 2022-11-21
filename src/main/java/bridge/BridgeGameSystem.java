f93f0c1844a119eeeef636b4e67632c3b86194da
ChanghyunKim <variey82p@gmail.com>
2022-11-21 21:34:46 +0900

    feat(BridgeGameSystem) : 다리를 건넌 결과를 얻는 메서드 구현

--- src/main/java/bridge/BridgeGameSystem.java
+++ src/main/java/bridge/BridgeGameSystem.java
@@ -3,20 +3,40 @@ package bridge;
 import java.sql.SQLOutput;
 import java.util.List;
 
+import static bridge.Bridge.*;
+
 public class BridgeGameSystem {
     private static final InputView inputView = new InputView();
+    private static final OutputView outputView = new OutputView();
+    private static int tryCount = 1;
+    private static int index = 2;
 
     public void startBridgeGame(){
         startGame();
         int bridgeSize = BridgeGame.getBridgeSize();
         List<String> bridge = BridgeGame.getBridge(bridgeSize);
         System.out.println(bridge);
-//        String move = getMoveDirect();
+        String direct = BridgeGame.getMoveDirect();
+        Bridge pos = toCrossBridge(bridge, direct, index);
+        outputView.printMap(bridge, pos, index);
     }
 
     public static void startGame(){
         inputView.printStartMessage();
     }
 
+    public static Bridge toCrossBridge(List<String> bridge, String direct, int index){
+        boolean movingResult = BridgeGame.move(bridge, direct, index);
+        if(movingResult && direct.equals("U")){
+            return SUCCESS_UPPER;
+        }
+        if(!movingResult && direct.equals("U")){
+            return FAILURE_UPPER;
+        }
+        if(movingResult && direct.equals("D")){
+            return SUCCESS_LOWER;
+        }
+        return FAILURE_LOWER;
+    }
 
 }
