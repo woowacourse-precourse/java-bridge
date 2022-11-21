68a8daa02dbb2d91c9592a54274089b5559de51f
UJ15 <dbwogml15@naver.com>
2022-11-21 16:09:46 +0900

    feat : create cross method in BridgeGameEngine

--- src/main/java/bridge/engine/BridgeGameEngine.java
+++ src/main/java/bridge/engine/BridgeGameEngine.java
@@ -1,8 +1,13 @@
 package bridge.engine;
 
 import bridge.BridgeMaker;
+import bridge.error.Error;
 import bridge.model.Bridge;
 import bridge.model.BridgeSize;
+import bridge.model.Crossing;
+import bridge.model.PassOrFail;
+import bridge.model.UpOrDown;
+import java.util.Objects;
 
 public class BridgeGameEngine {
 
@@ -19,5 +24,16 @@ public class BridgeGameEngine {
         int size = bridgeSize.getValue();
 
         return new Bridge(bridgeMaker.makeBridge(size));
+    }
+
+    public void cross(Crossing crossing, Bridge bridge, UpOrDown upOrDown) {
+        if (Objects.isNull(crossing) || Objects.isNull(bridge) || Objects.isNull(upOrDown)) {
+            throw new IllegalArgumentException(Error.SYSTEM_ERROR.getMessage());
+        }
+
+        int nextStep = crossing.getNextStep();
+        PassOrFail passOrFail = new PassOrFail(bridge.isCross(nextStep, upOrDown));
+
+        bridgeGame.move(crossing, passOrFail);
     }
 }
