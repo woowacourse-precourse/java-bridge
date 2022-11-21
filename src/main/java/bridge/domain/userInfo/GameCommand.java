f24b04410067b07305cccb45116ab242c35e18f2
minju412 <ln8847@ajou.ac.kr>
2022-11-22 01:54:59 +0900

    refactor: use constant

--- src/main/java/bridge/domain/userInfo/GameCommand.java
+++ src/main/java/bridge/domain/userInfo/GameCommand.java
@@ -6,7 +6,6 @@ public class GameCommand {
     private static final String QUIT = "Q";
     private static final String COMMAND_BLANK_ERROR = "입력 값은 비어있을 수 없습니다.";
     private static final String COMMAND_FORMAT_ERROR = "R 또는 Q 만 입력해야합니다.";
-
     private final String command;
 
     public GameCommand(String command) {
@@ -34,6 +33,6 @@ public class GameCommand {
     }
 
     public boolean isSameQuit() {
-        return command.equals("Q");
+        return command.equals(QUIT);
     }
 }
