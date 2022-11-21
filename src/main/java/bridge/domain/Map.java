93a068b4215f8c64cfc73d67bb354650db66d3a7
jini_11 <jieun21124@naver.com>
2022-11-21 16:51:56 +0900

    refactor: 공통되는 문자 상수 처리

--- src/main/java/bridge/domain/Map.java
+++ src/main/java/bridge/domain/Map.java
@@ -3,34 +3,38 @@ package bridge.domain;
 import bridge.constant.Bridge;
 
 public class Map {
+    private static final String OPEN_SYMBOL = "[";
+    private static final String MIDDLE_SYMBOL = "|";
+    private static final String CLOSE_SYMBOL = "]";
+
     private String upMap;
     private String downMap;
 
     public Map() {
-        this.upMap = "[";
-        this.downMap = "[";
+        this.upMap = OPEN_SYMBOL;
+        this.downMap = OPEN_SYMBOL;
     }
 
     public void makeUpMap(String result) {
-        upMap += result + "|";
-        downMap += Bridge.BLANK.getLocation() + "|";
+        upMap += result + MIDDLE_SYMBOL;
+        downMap += Bridge.BLANK.getLocation() + MIDDLE_SYMBOL;
     }
 
     public void makeDownMap(String result) {
-        upMap += Bridge.BLANK.getLocation() + "|";
-        downMap += result + "|";
+        upMap += Bridge.BLANK.getLocation() + MIDDLE_SYMBOL;
+        downMap += result + MIDDLE_SYMBOL;
     }
 
     public void resetMap() {
-        upMap = "[";
-        downMap = "[";
+        upMap = OPEN_SYMBOL;
+        downMap = OPEN_SYMBOL;
     }
 
     public String getUpMap() {
-        return upMap.substring(0, upMap.length() - 1) + "]";
+        return upMap.substring(0, upMap.length() - 1) + CLOSE_SYMBOL;
     }
 
     public String getDownMap() {
-        return downMap.substring(0, downMap.length() - 1) + "]";
+        return downMap.substring(0, downMap.length() - 1) + CLOSE_SYMBOL;
     }
 }
