5022ba2aaa76d50879e762c99b4cb1c19648d5c2
Namyunsuk <yunsuk0328@naver.com>
2022-11-21 21:45:10 +0900

    feat(GameControl): 게임을 진행하는 기능 구현

--- src/main/java/bridge/view/InputView.java
+++ src/main/java/bridge/view/InputView.java
@@ -22,7 +22,7 @@ public class InputView {
     /**
      * 사용자가 이동할 칸을 입력받는다.
      */
-    public String readMoving() {
+    public static String readMoving() {
         OutputView.printSelectMessage();
         String userInput = Console.readLine();
         //예외처리
@@ -33,7 +33,7 @@ public class InputView {
     /**
      * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
      */
-    public String readGameCommand() {
+    public static String readGameCommand() {
         OutputView.printRestartMessage();
         String userInput = Console.readLine();
         //예외처리
