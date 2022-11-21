f363054eff3dafe181ed49d2b90b4d1a72f39c7b
phdljr <phdljr@naver.com>
2022-11-22 07:55:40 +0900

    test: 도메인별 테스트 케이스 추가

---
+++ src/test/java/bridge/model/BridgeGameTest.java
@@ -0,0 +1,29 @@
+package bridge.model;
+
+import bridge.generator.BridgeMaker;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+class BridgeGameTest {
+
+    static BridgeGame bridgeGame = new BridgeGame();
+
+    @Test
+    @DisplayName("입력한 다리와 정답이 같은지 확인한다.")
+    void checkAnswer() {
+        Bridge answer = new Bridge(List.of("U", "D", "U"));
+        Bridge input = new Bridge(List.of("U", "D", "U"));
+        Bridge input2 = new Bridge(List.of("U", "D", "D"));
+
+        bridgeGame.setUp(answer, input);
+        assertThat(bridgeGame.checkAnswer()).isEqualTo(true);
+
+        bridgeGame.setUp(answer, input2);
+        assertThat(bridgeGame.checkAnswer()).isEqualTo(false);
+    }
+}
