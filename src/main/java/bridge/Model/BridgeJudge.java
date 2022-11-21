299d2cd5ac3916a5dc57a2b822b908e19121b32c
5nam <ohnam90@gmail.com>
2022-11-21 17:05:02 +0900

    refactor: 판단한 결과를 매개변수로 받아서, Map 을 그리는 기능 추가

--- src/main/java/bridge/Model/BridgeJudge.java
+++ src/main/java/bridge/Model/BridgeJudge.java
@@ -1,8 +1,14 @@
 package bridge.Model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class BridgeJudge {
     public static final BridgeJudge getInstance = new BridgeJudge();
+    private static List<String> bridgeMapUp = new ArrayList<>();
+    private static List<String> bridgeMapDown = new ArrayList<>();
 
+    // userInput 과 madeBridge 를 비교하여 결과를 bridge 에 저장하는 함수
     public void judgeInput(String userInput, String madeBridge, Bridge bridge) {
         if(userInput.equals(madeBridge)) {
             if(userInput.equals("U")) {
@@ -26,5 +32,44 @@ public class BridgeJudge {
                 bridge.updateUp(" ");
             }
         }
+    }
+
+    public static List<String> getBridgeMapDown(List<String> result, int count) {
+        bridgeMapDown = setBridge(result, count);
+        return bridgeMapDown;
+    }
+
+    public static List<String> getBridgeMapUp(List<String> result, int count) {
+        bridgeMapUp = setBridge(result, count);
+        return bridgeMapUp;
+    }
+
+    private static List<String> setBridge(List<String> result, int count) {
+        List<String> bridgeMap = new ArrayList<>();
+        int resultIndex = 0;
+        for(int index = 0; index<count*2+1; index++) {
+            if(index == 0) {
+                bridgeMap.add("[ ");
+                continue;
+            }
+            if(index%2 != 0) {
+                bridgeMap.add(result.get(resultIndex));
+                resultIndex++;
+                continue;
+            }
+            if(index != count*2) {
+                bridgeMap.add(" | ");
+                continue;
+            }
+            if(index == count*2) {
+                bridgeMap.add(" ]");
+            }
+        }
+        return bridgeMap;
+    }
+
+    public void init() {
+        bridgeMapUp = new ArrayList<>();
+        bridgeMapDown = new ArrayList<>();
     }
 }
