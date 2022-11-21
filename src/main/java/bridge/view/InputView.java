95d166d6a689df4b9d672e90166aa430c972a93d
josolha <josolha@nate.com>
2022-11-22 00:00:51 +0900

    feat(InputView) : 다리길이를 입력하는 메소드 생성

--- src/main/java/bridge/view/InputView.java
+++ src/main/java/bridge/view/InputView.java
@@ -1,4 +1,4 @@
-package bridge;
+package bridge.view;
 
 import camp.nextstep.edu.missionutils.Console;
 
@@ -22,13 +22,13 @@ public class InputView {
      * 사용자가 이동할 칸을 입력받는다.
      */
     public String readMoving() {
-        return null;
+
     }
 
     /**
      * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
      */
     public String readGameCommand() {
-        return null;
+
     }
 }
