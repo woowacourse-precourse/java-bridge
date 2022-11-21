9070dd64708405681251c4114c2b196b529f9890
josolha <josolha@nate.com>
2022-11-22 01:35:16 +0900

    refactor(BridgeGame) : 틀렸을 때 재시도 기능 컨트롤 객체에서 옮김

--- src/main/java/bridge/service/BridgeGame.java
+++ src/main/java/bridge/service/BridgeGame.java
@@ -2,6 +2,7 @@ package bridge.service;
 
 import bridge.BridgeMaker;
 import bridge.BridgeRandomNumberGenerator;
+import bridge.view.OutputView;
 import java.util.List;
 
 /**
@@ -10,6 +11,8 @@ import java.util.List;
 public class BridgeGame {
 
     private static int location;
+//    private static List<String> builtbridge;
+
 
     /**
      * 사용자가 칸을 이동할 때 사용하는 메서드
@@ -32,13 +35,20 @@ public class BridgeGame {
         return false;
     }
 
-
     /**
      * 사용자가 게임을 다시 시도할 때 사용하는 메서드
      * <p>
      * 재시작을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public boolean retry() {
+    public boolean retry(String command) throws Exception {
+
+        if (command.equals("Q")) {
+            return false;
+        }
+        if (command.equals("R")) {
+            return true;
+        }
+        throw new Exception();
 
     }
 }
