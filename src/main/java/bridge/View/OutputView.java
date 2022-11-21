3fe9e29858e501f581bc4284336ecbad9bd4bf7a
YE-EUN <musiclove529@naver.com>
2022-11-21 18:35:57 +0900

    feat(OutputView) : printMap() 수정 및 printResult() 구현

--- src/main/java/bridge/View/OutputView.java
+++ src/main/java/bridge/View/OutputView.java
@@ -1,5 +1,7 @@
 package bridge.View;
 
+import bridge.Domain.Result;
+
 import java.util.List;
 
 /**
@@ -8,7 +10,7 @@ import java.util.List;
 public class OutputView {
 
     public void printBridgeGameStart(){
-        System.out.println("다리 건너기 게임을 시작합니다.");
+        System.out.println("다리 건너기 게임을 시작합니다.\n");
     }
 
     /**
@@ -16,17 +18,8 @@ public class OutputView {
      * <p>
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void printMap(List<List<String>> map) {
-        for(List<String> row : map){
-            System.out.println(replaceMapRowForm(row));
-        }
-    }
-
-    public String replaceMapRowForm(List<String> row){
-        return row.toString()
-                .replace("[", "[ ")
-                .replaceAll(",", " |")
-                .replace("]", " ]");
+    public void printMap(Result result) {
+        System.out.println(result.replaceMapStringForm());
     }
 
     /**
@@ -34,6 +27,7 @@ public class OutputView {
      * <p>
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void printResult() {
+    public void printResult(Result result) {
+        System.out.println(result);
     }
 }
