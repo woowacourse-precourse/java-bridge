1cf586966748adad67bd242952baa63fe20ea9e9
JaehongDev <println@kakao.com>
2022-11-21 21:25:27 +0900

    refactor (OutputView) : 출력 방식 변경

--- src/main/java/bridge/view/OutputView.java
+++ src/main/java/bridge/view/OutputView.java
@@ -9,8 +9,6 @@ public class OutputView extends IOPrinter {
      * 현재까지 이동한 다리의 상태를 정해진 형식에 맞춰 출력한다.
      * <p>
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
-     *
-     * @param s
      */
     public void printMap(String recordedHistory) {
         this.println(recordedHistory);
@@ -20,14 +18,11 @@ public class OutputView extends IOPrinter {
      * 게임의 최종 결과를 정해진 형식에 맞춰 출력한다.
      * <p>
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
-     *
-     * @param clear
-     * @param repeatCount
      */
     public void printResult(String recordedHistory, String status, Integer repeatCount) {
-        this.println("최종 게임 결과");
+        this.printlnBeforeNextLine("최종 게임 결과");
         this.printMap(recordedHistory);
-        this.println("게임 성공 여부: " + status);
+        this.printlnBeforeNextLine("게임 성공 여부: " + status);
         this.println("총 시도한 횟수: " + repeatCount);
     }
 
