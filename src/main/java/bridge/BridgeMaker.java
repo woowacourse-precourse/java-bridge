5d32e021c307ec86e12a0d00fcd29a6c0840be01
tseop <77163842+tseop@users.noreply.github.com>
2022-11-22 00:26:26 +0900

    refactor: 요구사항에 맞게 수정
    함수 추가가 가능하다는 요구사항이 없어 생성한 함수 삭제

--- src/main/java/bridge/BridgeMaker.java
+++ src/main/java/bridge/BridgeMaker.java
@@ -5,33 +5,19 @@ import bridge.resource.ErrorMessage;
 import java.util.ArrayList;
 import java.util.List;
 
-/**
- * 다리의 길이를 입력 받아서 다리를 생성해주는 역할을 한다.
- */
 public class BridgeMaker {
-    /**
-     * 필드 변경 불가 (* 추가는 가능하다는 것인가?) 함수의 파라미터 및 반환 값 변경 불가
-     */
     private final BridgeNumberGenerator bridgeNumberGenerator;
 
     public BridgeMaker(BridgeNumberGenerator bridgeNumberGenerator) {
         this.bridgeNumberGenerator = bridgeNumberGenerator;
     }
 
-    /**
-     * @param size 다리의 길이
-     * @return 입력받은 길이에 해당하는 다리 모양. 위 칸이면 "U", 아래 칸이면 "D"로 표현해야 한다.
-     */
     public List<String> makeBridge(int size) {
         List<String> bridge = new ArrayList<>();
         for (int i = 0; i < size; i++) {
-            bridge.add(makePassable());
+            bridge.add(BridgeEnum.getDivision(
+                    bridgeNumberGenerator.generate()));
         }
         return bridge;
-    }
-
-    private String makePassable() {
-        int passable = bridgeNumberGenerator.generate();
-        return BridgeEnum.getDivision(passable);
     }
 }
