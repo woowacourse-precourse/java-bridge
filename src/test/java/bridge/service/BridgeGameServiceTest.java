db599d3b43ce0e74a27d1fc3fc513ea2f4e992f9
wlsh44 <k87913j@gmail.com>
2022-11-22 01:13:21 +0900

    test: 리팩토링에 맞춰 BridgeGameService 테스트 코드 수정

--- src/test/java/bridge/service/BridgeGameServiceTest.java
+++ src/test/java/bridge/service/BridgeGameServiceTest.java
@@ -1,27 +1,37 @@
 package bridge.service;
 
+import bridge.domain.Command;
+import bridge.domain.GameStatus;
 import bridge.domain.bridge.Bridge;
 import bridge.domain.bridge.BridgeMaker;
-import bridge.domain.bridge.Move;
 import bridge.domain.game.BridgeGame;
-import bridge.domain.game.GameStatus;
+import bridge.domain.game.GameProgress;
+import bridge.dto.MapDto;
+import bridge.dto.ResultDto;
 import bridge.support.FakeBridgeNumberGenerator;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.DisplayNameGeneration;
+import org.junit.jupiter.api.DisplayNameGenerator;
 import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import java.util.List;
 
-import static bridge.domain.bridge.Move.DOWN;
-import static bridge.domain.bridge.Move.UP;
-import static bridge.domain.game.GameStatus.FAILED;
-import static bridge.domain.game.GameStatus.SUCCESS;
-import static bridge.support.ErrorMessage.INVALID_COMMAND_ERROR;
+import static bridge.domain.Command.QUIT;
+import static bridge.domain.Command.RETRY;
+import static bridge.domain.GameStatus.FAILED;
+import static bridge.domain.GameStatus.PLAYING;
+import static bridge.domain.GameStatus.SUCCESS;
+import static bridge.domain.Move.DOWN;
+import static bridge.domain.Move.UP;
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.assertj.core.util.Lists.newArrayList;
 
+@DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
 class BridgeGameServiceTest {
 
     BridgeGame bridgeGame;
@@ -77,44 +87,70 @@ class BridgeGameServiceTest {
             //then
             assertThat(status).isEqualTo(SUCCESS);
         }
-
-        @Test
-        void U_D_이외의_값이_넘어오면_예외가_발생한다() {
-            //given
-            String moving = "A";
-
-            //when then
-            assertThatThrownBy(() -> service.crossBridgeUnit(moving))
-                    .isInstanceOf(IllegalArgumentException.class)
-                    .hasMessage(INVALID_COMMAND_ERROR);
-        }
     }
 
     @Nested
-    @DisplayName("executeGameCommand 테스트")
-    class ExecuteGameCommand {
+    @DisplayName("executeIfRetry 테스트")
+    class ExecuteIfRetry {
 
         @Test
-        void 재시작_커맨드가_들어오면_시도_횟수가_올라가고_진행_상황이_초기화된다() {
+        void 재시작_커맨드가_들어오면_시도_횟수가_올라가고_진행_상황이_초기화가_되며_true를_리턴한다() {
             //given
-            String command = "R";
+            GameProgress expect = new GameProgress();
+            Command command = RETRY;
 
             //when
-            GameStatus status = service.executeGameCommand(command);
+            boolean res = service.executeIfRetry(command);
 
             //then
-            assertThat(status).isEqualTo(PLAYING);
+            assertThat(res).isTrue();
             assertThat(bridgeGame.getAttempt()).isEqualTo(2);
-            assertThat(bridgeGame.getGameProgress()).isEqualTo(List.of());
+            assertThat(bridgeGame.getGameProgress())
+                    .usingRecursiveComparison()
+                    .isEqualTo(expect);
+        }
+
+        @Test
+        void 재시작_외의_커맨드가_들어오면_false를_리턴한다() {
+            //given
+            Command command = QUIT;
+
+            //when
+            boolean res = service.executeIfRetry(command);
+
+            //then
+            assertThat(res).isFalse();
         }
     }
 
+    @Order(2)
+    @ParameterizedTest
+    @CsvSource(value = {"D, ,O", "U,X, "}, ignoreLeadingAndTrailingWhitespace = false)
+    void getMapDto_한_칸_이동의_결과를_담은_dto를_리턴한다(String move, String upside, String downSide) {
+        //given
+        MapDto expect = new MapDto(String.format("[ %s ]\n[ %s ]", upside, downSide));
+        bridgeGame.move(move);
 
-    @Test
-    void getResultDto() {
+        //when
+        MapDto res = service.getMapDto();
+
+        //then
+        assertThat(res).usingRecursiveComparison()
+                .isEqualTo(expect);
     }
 
     @Test
-    void getMapDto() {
+    @Order(3)
+    void getResultDto_게임_결과에_관한_정보를_담은_dto를_리턴한다() {
+        //given
+        GameStatus status = FAILED;
+        ResultDto expect = new ResultDto(status, 1, service.getMapDto());
+
+        //when
+        ResultDto resultDto = service.getResultDto(status);
+
+        //then
+        assertThat(resultDto).usingRecursiveComparison()
+                .isEqualTo(expect);
     }
 }
