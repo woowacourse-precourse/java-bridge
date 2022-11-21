3fd0543065e7175804a5ea636f138d929afaa012
hyungkya <hyungkya@uci.edu>
2022-11-22 01:23:52 +0900

    test: 입력부 테스트 케이스 변경 및 추가
    ValueSource와 ParameterizedTest를 이용한 테스트 파라미터 다양화와 반복되는 구문 제거.

--- src/test/java/bridge/InputViewTest.java
+++ src/test/java/bridge/InputViewTest.java
@@ -4,15 +4,16 @@ import static org.assertj.core.api.Assertions.*;
 
 import java.io.ByteArrayInputStream;
 import java.io.InputStream;
-import java.util.List;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 public class InputViewTest {
     @DisplayName("다리 길이 입력 시 숫자가 아닌 경우 예외처리 한다.")
-    @Test
-    void bridgeSizeNotNumeric() {
-        InputStream in = mockUserInput("NonNumeric");
+    @ValueSource(strings = {"hi", "a1f3", "123a", "7,7", "8.0"})
+    @ParameterizedTest
+    void bridgeSizeNotNumeric(String userInput) {
+        InputStream in = mockUserInput(userInput);
         System.setIn(in);
 
         InputView inputView = new InputView();
@@ -21,19 +22,21 @@ public class InputViewTest {
     }
 
     @DisplayName("다리 길이 입력 시 숫자인 경우 정상적으로 변환한다.")
-    @Test
-    void bridgeSizeNumeric() {
-        InputStream in = mockUserInput("3");
+    @ValueSource(strings = {"1", "-500", "20", "40"})
+    @ParameterizedTest
+    void bridgeSizeNumeric(String userInput) {
+        InputStream in = mockUserInput(userInput);
         System.setIn(in);
 
         InputView inputView = new InputView();
-        assertThat(inputView.readBridgeSize()).isEqualTo(3);
+        assertThat(inputView.readBridgeSize()).isEqualTo(Integer.parseInt(userInput));
     }
 
-    @DisplayName("게임 재시도 여부가 'R'나 'Q'가 아닐 경우 예외 처리한다. (Case 1)")
-    @Test
-    void invalidRetryCommandCase1() {
-        InputStream in = mockUserInput("3");
+    @DisplayName("게임 재시도 여부가 'R'나 'Q'가 아닐 경우 예외 처리한다.")
+    @ValueSource(strings = {"3", "X", "q"})
+    @ParameterizedTest
+    void invalidRetryCommandCase(String userInput) {
+        InputStream in = mockUserInput(userInput);
         System.setIn(in);
 
         InputView inputView = new InputView();
@@ -41,26 +44,16 @@ public class InputViewTest {
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
-    @DisplayName("게임 재시도 여부가 'R'나 'Q'가 아닐 경우 예외 처리한다. (Case 2)")
-    @Test
-    void invalidRetryCase2() {
-        InputStream in = mockUserInput("X");
+    @DisplayName("게임 재시도 여부가 'R'나 'Q'일 경우 정상 처리 한다.")
+    @ValueSource(strings = {"R", "Q"})
+    @ParameterizedTest
+    void validRetryCommandCase(String userInput) {
+        InputStream in = mockUserInput(userInput);
         System.setIn(in);
 
         InputView inputView = new InputView();
-        assertThatThrownBy(() -> inputView.readGameCommand())
-                .isInstanceOf(IllegalArgumentException.class);
-    }
-
-    @DisplayName("게임 재시도 여부가 'R'나 'Q'가 아닐 경우 예외 처리한다. (Case 3)")
-    @Test
-    void invalidRetryCase3() {
-        InputStream in = mockUserInput("q");
-        System.setIn(in);
-
-        InputView inputView = new InputView();
-        assertThatThrownBy(() -> inputView.readGameCommand())
-                .isInstanceOf(IllegalArgumentException.class);
+        assertThat(inputView.readGameCommand())
+                .isEqualTo(userInput);
     }
 
     private static InputStream mockUserInput(String input) {
