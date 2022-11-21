4c4819db5470b5fcca650ef7581bf96c71313e8a
MuHyeonSon <smuhyeon84@gmail.com>
2022-11-22 01:15:52 +0900

    feat(BridgeGame): 게임 다시 시도할지 여부 입력 기능 구현

--- src/main/java/bridge/InputView.java
+++ src/main/java/bridge/InputView.java
@@ -26,7 +26,7 @@ public class InputView {
         System.out.println("이동할 칸을 선택해주세요. (위: U, 아래: D)");
         String move = readLine();
         validationInput.validateUpperAlphaBet(move);
-        validationInput.validateUpperAlphaBet(move);
+        validationInput.validateInputSizeIsOne(move);
         validationInput.validateReadMoving(move);
         return move;
     }
@@ -35,6 +35,11 @@ public class InputView {
      * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
      */
     public String readGameCommand() {
-        return null;
+        System.out.println("게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)");
+        String replayOrQuit = readLine();
+        validationInput.validateUpperAlphaBet(replayOrQuit);
+        validationInput.validateInputSizeIsOne(replayOrQuit);
+        validationInput.validateReadGameCommand(replayOrQuit);
+        return replayOrQuit;
     }
 }
