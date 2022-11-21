92d4931d555df5afb7f37b074b6c0393a2cd7ca8
HGB <hsw8147@gmail.com>
2022-11-21 09:49:19 +0900

    fix: BridgeGame class
    BridgeGame class 메서드 수정

--- src/main/java/bridge/BridgeGame.java
+++ src/main/java/bridge/BridgeGame.java
@@ -1,18 +1,41 @@
 package bridge;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * 다리 건너기 게임을 관리하는 클래스
  */
 public class BridgeGame {
-    private int position = 0;
+    private enum UpOrDown{
+        UP(2),
+        DOWN(1)
+        ;
+        private final int value;
+        UpOrDown(int value){
+            this.value = value;
+        }
+    }
+
+    private List<Integer> position = new ArrayList<>(List.of(0,0));
+    private List<Integer> formerPosition = new ArrayList<>(List.of(0,0));
     private int retryNumber = 0;
     /**
      * 사용자가 칸을 이동할 때 사용하는 메서드
      * <p>
      * 이동을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void move() {
-        this.position += 1;
+    public void move(String readMoving) {
+        this.formerPosition.set(0, this.position.get(0));
+        this.formerPosition.set(1, this.position.get(1));
+        if(readMoving.equals("U")){
+            this.position.set(0, UpOrDown.UP.value);
+            this.position.set(1, this.position.get(1)+1);
+        }
+        if(readMoving.equals("D")){
+            this.position.set(0, UpOrDown.DOWN.value);
+            this.position.set(1, this.position.get(1)+1);
+        }
     }
 
     /**
@@ -21,11 +44,12 @@ public class BridgeGame {
      * 재시작을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
     public void retry() {
-        this.position -= 1;
+        this.position.set(0, this.formerPosition.get(0));
+        this.position.set(1, this.formerPosition.get(1));
         this.retryNumber += 1;
     }
 
-    public int getPosition() {
+    public List<Integer> getPosition() {
         return position;
     }
 
