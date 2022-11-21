a3a2a6527aabacf284473ef573cd95cf7332b1fb
HiiWee <wpdlzhf159@gmail.com>
2022-11-21 16:04:25 +0900

    feat: 저장소에 게임 시도를 기록할 수 있도록 Map 자료구조 이용

--- src/main/java/bridge/repository/BridgeGameRepository.java
+++ src/main/java/bridge/repository/BridgeGameRepository.java
@@ -1,13 +1,16 @@
 package bridge.repository;
 
 import bridge.domain.Bridge;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.HashMap;
+import java.util.Map;
 
 public class BridgeGameRepository {
+    public static final int INITIALIZE_COUNT = 1;
+    public static final String BRIDGE = "bridge";
+    public static final String GAME_COUNT = "gameCount";
+
     private static final BridgeGameRepository instance = new BridgeGameRepository();
-    private static final List<Bridge> store = new ArrayList<>();
-    public static final int BRIDGE_GET = 0;
+    private static final Map<String, Object> store = new HashMap<>();
 
     private BridgeGameRepository() {
     }
@@ -18,12 +21,21 @@ public class BridgeGameRepository {
 
     public Bridge storeBridge(final Bridge bridge) {
         clearStore();
-        store.add(bridge);
+        store.put(BRIDGE, bridge);
+        increaseGameCount();
         return findBridge();
     }
 
     public Bridge findBridge() {
-        return store.get(BRIDGE_GET);
+        return (Bridge) store.get(BRIDGE);
+    }
+
+    public int findGameCount() {
+        return (int) store.get(GAME_COUNT);
+    }
+
+    public void increaseGameCount() {
+        store.put(GAME_COUNT, (int) store.getOrDefault(GAME_COUNT, INITIALIZE_COUNT) + 1);
     }
 
     public void clearStore() {
