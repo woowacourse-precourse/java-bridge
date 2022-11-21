7ce2f43013259350fec9aa0d49d92371bb90ef5b
wlsh44 <k87913j@gmail.com>
2022-11-22 02:24:43 +0900

    style: 제공된 주석 제거

--- src/main/java/bridge/domain/bridge/BridgeMaker.java
+++ src/main/java/bridge/domain/bridge/BridgeMaker.java
@@ -8,9 +8,6 @@ import java.util.stream.Stream;
 
 import static bridge.support.ErrorMessage.BRIDGE_MAKER_SIZE_ERROR;
 
-/**
- * 다리의 길이를 입력 받아서 다리를 생성해주는 역할을 한다.
- */
 public class BridgeMaker {
 
     private static final int MINIMUM_BRIDGE_SIZE = 3;
@@ -22,10 +19,6 @@ public class BridgeMaker {
         this.bridgeNumberGenerator = bridgeNumberGenerator;
     }
 
-    /**
-     * @param size 다리의 길이
-     * @return 입력받은 길이에 해당하는 다리 모양. 위 칸이면 "U", 아래 칸이면 "D"로 표현해야 한다.
-     */
     public List<String> makeBridge(int size) {
         return Stream.generate(bridgeNumberGenerator::generate)
                 .map(number -> Move.from(number).getCode())
