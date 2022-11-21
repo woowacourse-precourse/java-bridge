a57b45763843242982ad0d39bce86e4ed9d7d160
bumwoo <gld55602@gmail.com>
2022-11-21 21:07:59 +0900

    test(BridgeGameManager): BridgeGameManager 테스트 코드 작성

---
+++ src/test/java/bridge/BridgeGameManagerTest.java
@@ -0,0 +1,33 @@
+package bridge;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class BridgeGameManagerTest {
+    private BridgeGameManager bridgeGameManager;
+
+    @BeforeEach
+    void setBridgeGameManager() {
+        bridgeGameManager = new BridgeGameManager();
+    }
+
+    @Test
+    void 생성_테스트() {
+        assertThat(bridgeGameManager.getCount()).isEqualTo(1);
+        assertThat(bridgeGameManager.getStep()).isEqualTo(0);
+    }
+
+    @Test
+    void 횟수_테스트() {
+        bridgeGameManager.increaseCount();
+        assertThat(bridgeGameManager.getCount()).isEqualTo(2);
+    }
+
+    @Test
+    void 위치_테스트() {
+        bridgeGameManager.increaseStep();
+        assertThat(bridgeGameManager.getStep()).isEqualTo(1);
+    }
+}
