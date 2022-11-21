e2f0537c06682f342e2b483431b4494ff8d26b7f
Basak <chgy2131@naver.com>
2022-11-21 23:46:34 +0900

    feat(BridgeGame): 플레이어 이동 및 게임재개, 결과 반환 관련 기능 추가
    - 사용자가 칸을 이동한다. - move
        - 이동할 때 위 칸은 대문자 U, 아래 칸은 대문자 D를 입력한다. -
    inputView 연계
        - 이동하는 칸에 대한 정보
            - 이동한 칸을 건널 수 있다면 O로 표시한다.
    
    - 건널 수 없다면 X로 표시한다.
    - 다리를 건너다 실패하면 게임을 재시작하거나 종료할 수 있다. - retry
        -
    플레이어에게 진행여부를 입력받는다. - inputView 연계
        - 재시작해도 처음에 만든 다리로 재사용한다.
    - 게임의
    결과를 반환한다. - getResult
    - 게임 결과를 갱신한다. - updateResult

--- src/main/java/bridge/BridgeGame.java
+++ src/main/java/bridge/BridgeGame.java
@@ -1,8 +1,10 @@
 package bridge;
 
-import bridge.domain.Bridge;
+import bridge.domain.*;
 import bridge.view.InputView;
 import bridge.view.OutputView;
+
+import java.util.List;
 
 /**
  * 다리 건너기 게임을 관리하는 클래스
@@ -12,6 +14,7 @@ public class BridgeGame {
     OutputView outputView = new OutputView();
     BridgeBroker bridgeBroker = new BridgeBroker();
     Bridge bridge;
+    GameResult gameResult = new GameResult();
 
     public Bridge makeBridge() {
         int bridgeSize = inputView.readBridgeSize();
@@ -25,6 +28,9 @@ public class BridgeGame {
      * 이동을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
     public void move() {
+        Move move = inputView.readMoving();
+        gameResult.addBlock(move);
+        updateResult();
     }
 
     /**
@@ -32,12 +38,43 @@ public class BridgeGame {
      * <p>
      * 재시작을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void retry() {
+    public boolean retry() {
+        GameCommand gameCommand = inputView.readGameCommand();
+        if (gameCommand == GameCommand.RESTART) {
+            return true;
+        }
+        return false;
     }
 
-    public void getResult() {
+    public GameResult getResult() {
+        return gameResult;
     }
 
     public void updateResult() {
+        List<String> myAnswer = gameResult.getBridge().getBridge();
+        List<String> answer = bridge.getBridge();
+
+        if (isSameBridge(answer, myAnswer)) {
+            gameResult.setProgress(GameProgress.SUCCESS);
+        }
+        for (int i = 0; i < myAnswer.size(); i++) {
+            if (!answer.get(i).equals(myAnswer.get(i))) {
+                gameResult.setProgress(GameProgress.FAILURE);
+            }
+        }
+    }
+    private boolean isSameBridge(List<String> answer, List<String> myAnswer) {
+        if (!isSameSize(answer, myAnswer)) {
+            return false;
+        }
+        for (int i = 0; i < myAnswer.size(); i++) {
+            if (!answer.get(i).equals(myAnswer.get(i))) {
+                return false;
+            }
+        }
+        return true;
+    }
+    private boolean isSameSize(List<String> answer, List<String> myAnswer) {
+        return answer.size() == myAnswer.size();
     }
 }
