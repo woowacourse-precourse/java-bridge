613f43b04641aeb03412ebc93a12f42e80f88a46
sanghyuk2 <givemesomefoodplz@gmail.com>
2022-11-22 02:40:11 +0900

    feat: 위 칸과 아래 칸 인스턴스 변수 생성
    StringBuilder 형 인스턴스 변수 생성 및 초기화

--- src/main/java/bridge/domain/bridgegame/BridgeGame.java
+++ src/main/java/bridge/domain/bridgegame/BridgeGame.java
@@ -4,7 +4,13 @@ package bridge.domain.bridgegame;
  * 다리 건너기 게임을 관리하는 클래스
  */
 public class BridgeGame {
+    private final StringBuilder upBridge;
+    private final StringBuilder downBridge;
 
+    public BridgeGame() {
+        upBridge = new StringBuilder();
+        downBridge = new StringBuilder();
+    }
     /**
      * 사용자가 칸을 이동할 때 사용하는 메서드
      * <p>
