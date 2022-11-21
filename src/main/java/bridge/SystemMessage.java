80f7a3ba7eb0d079a109957ae75ea6ab110e6391
Yejin Shin <shinyejin0212@gmail.com>
2022-11-21 13:58:57 +0900

    feat(SystemMessage): add system message

---
+++ src/main/java/bridge/SystemMessage.java
@@ -0,0 +1,12 @@
+package bridge;
+
+public class SystemMessage {
+    public static final String BRIDGE_SIZE_INPUT_MESSAGE = "다리의 길이를 입력해주세요";
+    public static final String MOVING_SPACE_INPUT_MESSAGE = "이동할 칸을 선택해주세요. (위: U, 아래: D)";
+    public static final String RETRY_GAME_INPUT_MESSAGE = "게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)";
+
+    public static final String START_GAME_MESSAGE = "다리 건너기 게임을 시작합니다.";
+    public static final String FINAL_GAME_RESULTS_MESSAGE = "최종 게임 결과";
+    public static final String GAME_SUCCESS_MESSAGE = "게임 성공 여부: ";
+    public static final String TOTAL_NUMBER_OF_ATTEMPTS = "총 시도한 횟수: ";
+}
