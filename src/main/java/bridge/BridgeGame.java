3b51f8694025191c6d2fd71844eb5a9b7de8b873
Jaewoong Kim <kimje1109@gmail.com>
2022-11-21 16:40:17 +0900

    refactor: 다리 위치 수정

--- src/main/java/bridge/BridgeGame.java
+++ src/main/java/bridge/BridgeGame.java
@@ -2,14 +2,11 @@ package bridge;
 
 import java.util.List;
 
-/**
- * 다리 건너기 게임을 관리하는 클래스
- */
 public class BridgeGame {
 
     List<String> bridge;
     Boolean isMovable;
-    Integer bridgeIndex = 0;
+    Integer bridgeIndex = -1;
 
     public BridgeGame(List<String> bridge) {
         this.bridge = bridge;
@@ -19,26 +16,16 @@ public class BridgeGame {
         return moving.equals(bridge.get(bridgeIndex));
     }
 
-    /**
-     * 사용자가 칸을 이동할 때 사용하는 메서드
-     * <p>
-     * 이동을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
-     */
     public void move(String moving) {
+        bridgeIndex++;
         if (canMove(moving)) {
             isMovable = true;
         }
         if (!canMove(moving)) {
             isMovable = false;
         }
-        bridgeIndex++;
     }
 
-    /**
-     * 사용자가 게임을 다시 시도할 때 사용하는 메서드
-     * <p>
-     * 재시작을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
-     */
     public void retry() {
     }
 }
