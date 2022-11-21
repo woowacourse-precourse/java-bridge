16e3ea4a61561c39ee1b73c13016fd398a7509c9
jmjmjames <chungjm0711@gmail.com>
2022-11-21 23:54:06 +0900

    refactor: direction initial 에서 command 로 변수명 변경

--- src/main/java/bridge/domain/Direction.java
+++ src/main/java/bridge/domain/Direction.java
@@ -1,5 +1,6 @@
 package bridge.domain;
 
+import bridge.common.ErrorMessage;
 import java.util.Arrays;
 
 public enum Direction {
@@ -7,11 +8,18 @@ public enum Direction {
     DOWN(0, "D");
 
     private final int bridgeNumber;
-    private final String initial;
+    private final String command;
 
-    Direction(int bridgeNumber, String initial) {
+    Direction(int bridgeNumber, String command) {
         this.bridgeNumber = bridgeNumber;
-        this.initial = initial;
+        this.command = command;
+    }
+
+    public static Direction of(String command) {
+        return Arrays.stream(values())
+                .filter(direction -> direction.command.equals(command))
+                .findFirst()
+                .orElseThrow(() -> new IllegalArgumentException(ErrorMessage.isInvalidMoving()));
     }
 
     public static String getInitialByBridgeNumber(int bridgeNumber) {
@@ -19,6 +27,6 @@ public enum Direction {
                 .filter(direction -> direction.bridgeNumber == bridgeNumber)
                 .findFirst()
                 .orElseThrow(IllegalArgumentException::new)
-                .initial;
+                .command;
     }
 }
