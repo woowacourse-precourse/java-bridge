914b2f0b1c856977aa123dcfe4d1f55a0d758965
yxnxx <jh4rgh0@gmail.com>
2022-11-21 20:10:27 +0900

    feat(InputValidator): check size is in range

--- src/main/java/bridge/utils/InputValidator.java
+++ src/main/java/bridge/utils/InputValidator.java
@@ -3,12 +3,17 @@ package bridge.utils;
 import bridge.constant.Error;
 
 public class InputValidator {
-
     public static int isBridgeSizeNumeric(String input) {
         try {
-            return Integer.parseInt(input);
+            int bridgeSize = Integer.parseInt(input);
+            return isSizeinRange(bridgeSize);
         }
         catch (NumberFormatException e) {
+            throw new IllegalArgumentException(Error.BRIDGE_SIZE_VALID.getError());
+        }
+    }
+    public static int isSizeinRange(int size) {
+        if((size < MIN) || (size > MAX)) {
             throw new IllegalArgumentException(Error.BRIDGE_SIZE_VALID.getError());
         }
     }
