7269bbf456b230c2e52232278ec00e75c4f54e2b
endlessmomo <soohyuk96@naver.com>
2022-11-21 13:05:36 +0900

    refactoring(TEXT) : 접근제어자 public으로 변경

--- src/main/java/bridge/domain/constnce/Text.java
+++ src/main/java/bridge/domain/constnce/Text.java
@@ -1,11 +1,11 @@
 package bridge.domain.constnce;
 
 public class Text {
-    private static final String BRIDGE_SIZE_MESSAGE = "다리의 길이를 입력해주세요.";
-    private static final String MOVE_TO_WHERE = "이동할 칸을 선택해주세요. (위: U, 아래: D)";
-    private static final String RETRY_MESSAGE = "게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)";
-    private static final String START_MESSAGE = "다리 건너기 게임을 시작합니다.";
-    private static final String END_MESSAGE = "최종 게임 결과";
-    private static final String RESULT_MESSAge = "게임 성공 여부 : ";
-    private static final String TOTAL_ATTEMPT_MESSAGE = "총 시도한 횟수 : " ;
+    public static final String BRIDGE_SIZE_MESSAGE = "다리의 길이를 입력해주세요.";
+    public static final String MOVE_TO_WHERE = "이동할 칸을 선택해주세요. (위: U, 아래: D)";
+    public static final String RETRY_MESSAGE = "게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)";
+    public static final String START_MESSAGE = "다리 건너기 게임을 시작합니다.";
+    public static final String END_MESSAGE = "최종 게임 결과";
+    public static final String RESULT_MESSAge = "게임 성공 여부 : ";
+    public static final String TOTAL_ATTEMPT_MESSAGE = "총 시도한 횟수 : " ;
 }
