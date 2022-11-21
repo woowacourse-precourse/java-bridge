a9f3fcf35250083e9018762317745e439c9ba34f
doxxx <doxxx93@gmail.com>
2022-11-22 01:04:26 +0900

    feat(BridgeSize): add BridgeSize wrap primitive value

---
+++ src/main/java/bridge/BridgeSize.java
@@ -0,0 +1,44 @@
+package bridge;
+
+import java.util.Objects;
+
+public class BridgeSize {
+    private final int size;
+
+    public BridgeSize(String size) {
+        validate(size);
+        this.size = Integer.parseInt(size);
+    }
+
+    private void validate(final String size) {
+        if (size == null || size.isEmpty()) {
+            throw new IllegalArgumentException("[ERROR] 다리의 길이는 숫자만 입력 가능합니다.");
+        }
+        int brideSize;
+        try {
+            brideSize = Integer.parseInt(size);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("[ERROR] 다리의 길이는 숫자만 입력 가능합니다.");
+        }
+        if (brideSize < 3 || brideSize > 20) {
+            throw new IllegalArgumentException("[ERROR] 다리의 길이는 3 이상 20 이하만 입력 가능합니다.");
+        }
+    }
+
+    @Override
+    public boolean equals(final Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        final BridgeSize that = (BridgeSize) o;
+        return size == that.size;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(size);
+    }
+}
