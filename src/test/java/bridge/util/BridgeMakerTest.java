19278a1dec6f42fa8b847327e077d106ee2a6099
Miseong Kim <miseongk@naver.com>
2022-11-22 01:00:50 +0900

    refactor(BridgeMaker): move package

--- src/test/java/bridge/util/BridgeMakerTest.java
+++ src/test/java/bridge/util/BridgeMakerTest.java
@@ -1,5 +1,7 @@
-package bridge;
+package bridge.util;
 
+import bridge.BridgeNumberGenerator;
+import bridge.BridgeRandomNumberGenerator;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
