d3da634b41b934a8bb30f866c218523595e5a527
soyunim <togksk123@naver.com>
2022-11-21 22:53:56 +0900

    feat: 랜덤으로 숫자를 받고, 정답 다리 만들기

--- src/main/java/bridge/GameProcess.java
+++ src/main/java/bridge/GameProcess.java
@@ -1,19 +1,30 @@
 package bridge;
 
+import java.util.List;
+
 public class GameProcess {
     private static final InputView inputView = new InputView();
-    public static String size;
-    public static int length;
+    private static final BridgeRandomNumberGenerator bridgeRandomNumberGenerator = new BridgeRandomNumberGenerator();
+    private static final BridgeMaker bridgeMaker = new BridgeMaker(bridgeRandomNumberGenerator);
+    public static String length;
+    public static int size;
+
     public static void gameStart(){
+        size = getSize();
+        bridgeMaker.makeBridge(size);
+    }
+
+    public static int getSize() {
         try{
-            size = inputView.readBridgeSize();
-            length = Integer.parseInt(size);
+            length = inputView.readBridgeSize();
+            size = Integer.parseInt(length);
         }
         catch(NumberFormatException e){
             throw new IllegalArgumentException("[ERROR] 다리 길이 입력값이 잘못됨.");
         }
-        if(length>20 || length<3){
+        if(size>20 || size<3){
             throw new IllegalArgumentException("[ERROR] 다리 길이 입력범위가 잘못됨.");
         }
+        return size;
     }
 }
