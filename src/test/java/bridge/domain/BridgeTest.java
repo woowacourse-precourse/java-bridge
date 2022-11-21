f611fdce40d67dc4b3d6d10589148ce821e23875
Donguk <95donguk.dev@gmail.com>
2022-11-21 17:45:23 +0900

    test: Bridge 도메인의 length 메서드 테스트

---
+++ src/test/java/bridge/domain/BridgeTest.java
@@ -0,0 +1,24 @@
+package bridge.domain;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.List;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class BridgeTest {
+    private Bridge bridge;
+    private List<String> testBlocks = List.of("U", "U", "D");
+
+    @BeforeEach
+    void setUp() {
+        bridge = new Bridge(testBlocks);
+    }
+
+    @DisplayName("다리 길이를 구하는 테스트")
+    @Test
+    void getBridgeLengthTest() {
+        assertThat(bridge.length()).isEqualTo(testBlocks.size());
+    }
+}
