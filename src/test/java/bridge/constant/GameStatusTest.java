e8feba710e30b6c0277c698c36d62fc2e1b9b8a2
heesang <dlagmltkd12@naver.com>
2022-11-22 00:38:18 +0900

    test: GameStatus가 가지고 있는 필드 값 검증

--- src/test/java/bridge/constant/GameStatusTest.java
+++ src/test/java/bridge/constant/GameStatusTest.java
@@ -1,5 +1,6 @@
 package bridge.constant;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -7,24 +8,40 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 class GameStatusTest {
 
+    GameStatus continueStatus;
+    GameStatus fail;
+    GameStatus success;
+
+    @BeforeEach
+    void setUp() {
+        continueStatus = GameStatus.CONTINUE;
+        fail = GameStatus.FAIL;
+        success = GameStatus.SUCCESS;
+    }
+
     @DisplayName("GameStatus isContinue 메소드 검증")
     @Test
     void isContinue() {
-        GameStatus aContinue = GameStatus.CONTINUE;
-        assertThat(aContinue.isContinue()).isTrue();
+        assertThat(continueStatus.isContinue()).isTrue();
     }
 
     @DisplayName("GameStatus isFail 메소드 검증")
     @Test
     void isFail() {
-        GameStatus fail = GameStatus.FAIL;
         assertThat(fail.isFail()).isTrue();
     }
 
     @DisplayName("GameStatus isSuccess 메소드 검증")
     @Test
     void isSuccess() {
-        GameStatus success = GameStatus.SUCCESS;
         assertThat(success.isSuccess()).isTrue();
+    }
+
+    @DisplayName("GameStatus가 가지고 있는 필드 값 검증")
+    @Test
+    void getMessage() {
+        assertThat(continueStatus.getMessage()).isEqualTo("계속");
+        assertThat(fail.getMessage()).isEqualTo("실패");
+        assertThat(success.getMessage()).isEqualTo("성공");
     }
 }
