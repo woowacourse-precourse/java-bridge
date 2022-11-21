9ae1d76b6dccad699c1c564a80395221a72be509
E1psycongr00 <39326175+E1psycongr00@users.noreply.github.com>
2022-11-21 17:47:09 +0900

    fix(inputView): 요구 사항에 맞게 예외시 재시도 처리 구현

--- src/main/java/bridge/view/InputView.java
+++ src/main/java/bridge/view/InputView.java
@@ -1,31 +1,49 @@
 package bridge.view;
 
-import camp.nextstep.edu.missionutils.Console;
+import bridge.ConsoleReader;
+import bridge.domain.BridgeLength;
+import bridge.domain.MovingCommand;
+import bridge.domain.RetryCommand;
 
 public class InputView extends View {
 
-    private static final String ERROR_NUMBER_FORMAT = "숫자 형태로 입력해주세요.";
     private static final String REQUEST_BRIDGE_SIZE = "다리의 길이를 입력해주세요.";
     private static final String REQUEST_MOVING_COMMAND = "이동할 칸을 선택해주세요. (위: U, 아래: D)";
     private static final String REQUEST_RETRY_COMMAND = "게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)";
 
-    public int readBridgeSize() {
-        print(REQUEST_BRIDGE_SIZE);
-        String input = Console.readLine();
-        try {
-            return Integer.parseInt(input);
-        } catch (NumberFormatException e) {
-            throw new IllegalArgumentException(ERROR_NUMBER_FORMAT);
+    public BridgeLength readBridgeSize() {
+        while (true) {
+            print(REQUEST_BRIDGE_SIZE);
+            try {
+                int input = ConsoleReader.readInt();
+                return new BridgeLength(input);
+            } catch (IllegalArgumentException e) {
+                printError(e.getMessage());
+            }
         }
     }
 
-    public String readMoving() {
-        print(REQUEST_MOVING_COMMAND);
-        return Console.readLine();
+    public MovingCommand readMoving() {
+        while (true) {
+            print(REQUEST_MOVING_COMMAND);
+            try {
+                String input = ConsoleReader.readString();
+                return MovingCommand.nameOf(input);
+            } catch (IllegalArgumentException e) {
+                printError(e.getMessage());
+            }
+        }
     }
 
-    public String readGameCommand() {
-        print(REQUEST_RETRY_COMMAND);
-        return Console.readLine();
+    public RetryCommand readGameCommand() {
+        while (true) {
+            print(REQUEST_RETRY_COMMAND);
+            try {
+                String input = ConsoleReader.readString();
+                return RetryCommand.nameOf(input);
+            } catch (IllegalArgumentException e) {
+                printError(e.getMessage());
+            }
+        }
     }
 }
