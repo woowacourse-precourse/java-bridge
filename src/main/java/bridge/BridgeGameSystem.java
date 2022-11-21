60bcbd2fba61e293afd90ed88c38a0e01f17a4dd
ChanghyunKim <variey82p@gmail.com>
2022-11-21 19:28:26 +0900

    refactor(BridgeGameSystem) : 게임에 이용하는 메서드 위치 변경
    기존에 BrdigeGameSystem에 존재하는 메서드를 BrdigeGame으로 변경

--- src/main/java/bridge/BridgeGameSystem.java
+++ src/main/java/bridge/BridgeGameSystem.java
@@ -1,5 +1,6 @@
 package bridge;
 
+import java.sql.SQLOutput;
 import java.util.List;
 
 public class BridgeGameSystem {
@@ -7,36 +8,15 @@ public class BridgeGameSystem {
 
     public void startBridgeGame(){
         startGame();
-        int bridgeSize = getBridgeSize();
-        List<String> bridge = getBridge(bridgeSize);
+        int bridgeSize = BridgeGame.getBridgeSize();
+        List<String> bridge = BridgeGame.getBridge(bridgeSize);
+        System.out.println(bridge);
 //        String move = getMoveDirect();
     }
 
     public static void startGame(){
         inputView.printStartMessage();
     }
-    public static int getBridgeSize(){
-        try{
-            return inputView.readBridgeSize();
-        }catch(IllegalArgumentException e){
-            System.out.println(e.getMessage());
-            return getBridgeSize();
-        }
-    }
 
-    public static String getMoveDirect(){
-        try{
-            return inputView.readMoving();
-        }catch(IllegalArgumentException e){
-            System.out.println(e.getMessage());
-            return getMoveDirect();
-        }
-    }
-
-    public static List<String> getBridge(int bridgeSize){
-        BridgeNumberGenerator bridgeNumberGenerator = new BridgeRandomNumberGenerator();
-        BridgeMaker bridgeMaker = new BridgeMaker(bridgeNumberGenerator);
-        return bridgeMaker.makeBridge(bridgeSize);
-    }
 
 }
