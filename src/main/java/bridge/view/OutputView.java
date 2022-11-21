f3a2cae12862528fad9549463d99a6a09f1e14ff
hyun0404woo <hyun0404woo@gmail.com>
2022-11-21 16:37:17 +0900

    feat: 게임 결과 출력 메서드 구현
    - 적절한 형식에 맞게 출력한다

--- src/main/java/bridge/view/OutputView.java
+++ src/main/java/bridge/view/OutputView.java
@@ -1,14 +1,17 @@
 package bridge.view;
 
 import bridge.domain.BridgeGameStatus;
+import bridge.domain.GameResult;
 
 /**
  * 사용자에게 게임 진행 상황과 결과를 출력하는 역할을 한다.
  */
 public class OutputView {
 
+    private final static String FINAL_GAME_RESULT = "게임 성공 여부: ";
+    private final static String TOTAL_RUN_COUNT = "총 시도한 횟수: ";
     private final static String START_MESSAGE = "다리 건너기 게임을 시작합니다.\n";
-
+    private final static String END_MESSAGE = "\n최종 게임 결과";
     public final static String ERROR_PREFIX = "[ERROR]";
 
     public void printGameStartMessage() {
@@ -25,9 +28,14 @@ public class OutputView {
 
     /**
      * 게임의 최종 결과를 정해진 형식에 맞춰 출력한다.
-     * <p>
-     * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
+     * @param bridgeGameStatus
      */
-    public void printResult() {
+    public void printResult(BridgeGameStatus bridgeGameStatus) {
+        System.out.println(END_MESSAGE);
+        System.out.println(bridgeGameStatus);
+        final boolean gameResult = bridgeGameStatus.isGameSuccessfulEnd();
+        final String gameResultMessage = GameResult.getGameResultMessage(gameResult);
+        System.out.println(FINAL_GAME_RESULT + gameResultMessage);
+        System.out.println(TOTAL_RUN_COUNT + bridgeGameStatus.getRunCount());
     }
 }
