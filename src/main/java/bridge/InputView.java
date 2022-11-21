aef4999bdeb090e6892dc0b2e61d6f298a0cf743
khj <bbb6254177@gmail.com>
2022-11-21 23:58:01 +0900

    feat(bridge): add function to get space to be moved from user

--- src/main/java/bridge/InputView.java
+++ src/main/java/bridge/InputView.java
@@ -13,7 +13,7 @@ public class InputView {
     public int readBridgeSize() {
         System.out.println("다리의 길이를 입력해주세요.");
 
-        try{
+        try {
             return Integer.parseInt(Console.readLine());
         } catch (Exception e) {
             throw new IllegalArgumentException("[ERROR] 입력 받은 다리의 길이가 정수가 아닙니다.");
@@ -24,7 +24,14 @@ public class InputView {
      * 사용자가 이동할 칸을 입력받는다.
      */
     public String readMoving() {
-        return null;
+        System.out.println("이동할 칸을 선택해주세요. (위: U, 아래: D)");
+        String input = Console.readLine();
+
+        if (input.equals("U") || input.equals("D")) {
+            return input;
+        }
+
+        throw new IllegalArgumentException("[ERROR] 입력 받은 이동할 칸이 U나 D가 아닙니다.");
     }
 
     /**
