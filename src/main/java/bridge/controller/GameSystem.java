cce70119ca614e2fab5fe84f10d3e96b0324604d
sora5421 <sojeoung536985@gmail.com>
2022-11-22 05:16:19 +0900

    feat : make GameSystem methods structure

--- src/main/java/bridge/controller/GameSystem.java
+++ src/main/java/bridge/controller/GameSystem.java
@@ -1,4 +1,23 @@
 package bridge.controller;
 
 public class GameSystem {
+    private String state = "C";
+    private int count = 0;
+    private int attempt = 1;
+
+    public void inputBridgeLength() {
+
+    }
+
+    public void calculate() {
+
+    }
+
+    public void result() {
+
+    }
+
+    private String updatdState() {
+        return "C";
+    }
 }
