2b5484eac26d2a14fce28bd14e4f9af4061b1308
wjh2012 <wjh2012@naver.com>
2022-11-21 20:57:02 +0900

    feat(MoveRecord): 이동 결과를 저장하는 객체

---
+++ src/main/java/bridge/model/MoveRecord.java
@@ -0,0 +1,23 @@
+package bridge.model;
+
+public class MoveRecord {
+    private final String step;
+    private final boolean success;
+
+    private MoveRecord(String step, boolean success) {
+        this.step = step;
+        this.success = success;
+    }
+
+    public static MoveRecord addResult(String step, boolean success) {
+        return new MoveRecord(step, success);
+    }
+
+    public String getStep() {
+        return step;
+    }
+
+    public boolean isSuccess() {
+        return success;
+    }
+}
