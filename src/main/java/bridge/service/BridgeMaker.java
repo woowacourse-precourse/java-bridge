23a92adce697be0c71587f207d7cce8d31d6230d
hyun0404woo <hyun0404woo@gmail.com>
2022-11-21 15:52:27 +0900

    feat: 다리 생성 메서드 구현
    - 0인 경우 "D", 1인 경우 "U" 로 치환

--- src/main/java/bridge/service/BridgeMaker.java
+++ src/main/java/bridge/service/BridgeMaker.java
@@ -1,8 +1,11 @@
 package bridge.service;
 
+import bridge.domain.Moving;
 import bridge.util.BridgeNumberGenerator;
 
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 /**
  * 다리의 길이를 입력 받아서 다리를 생성해주는 역할을 한다.
@@ -23,6 +26,14 @@ public class BridgeMaker {
      * @return 입력받은 길이에 해당하는 다리 모양. 위 칸이면 "U", 아래 칸이면 "D"로 표현해야 한다.
      */
     public List<String> makeBridge(int size) {
-        return null;
+        return IntStream.range(0, size)
+                .mapToObj(v -> bridgeNumberConverter(bridgeNumberGenerator.generate()))
+                .collect(Collectors.toList());
+    }
+
+    private String bridgeNumberConverter(int number) {
+        if (number == 0) return Moving.DOWN.getDirection();
+        if (number == 1) return Moving.UP.getDirection();
+        throw new IllegalArgumentException();
     }
 }
