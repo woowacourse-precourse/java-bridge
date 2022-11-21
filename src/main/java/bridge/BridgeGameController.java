4c23c365af7f8cd8bae049afa2520213fa8694b6
gwj0421 <gwj0421@gmail.com>
2022-11-21 19:44:44 +0900

    refactor: 구조 개편

--- src/main/java/bridge/BridgeGameController.java
+++ src/main/java/bridge/BridgeGameController.java
@@ -8,22 +8,23 @@ public class BridgeGameController {
     private InputView inputView;
     private OutputView outputView;
     private int bridgeSize, attepmtCnt;
+    private BridgeMaker bridgeMaker;
     private boolean isSucess;
-    private List<String> playerBridgeMap;
-    private List<String> RandomBridgeMap;
+    private List<String> playerBridgeMap,RandomBridgeMap;
 
     public BridgeGameController() {
         bridgeGame = new BridgeGame();
         inputView = new InputView(new CheckNum());
         playerBridgeMap = new ArrayList<>();
         RandomBridgeMap = new ArrayList<>();
+        bridgeMaker = new BridgeMaker(new BridgeRandomNumberGenerator());
         outputView = new OutputView();
     }
 
     public void start() {
         attepmtCnt = 0;
         bridgeSize = inputView.readBridgeSize();
-        BridgeMaker bridgeMaker = new BridgeMaker(new BridgeRandomNumberGenerator());
+
         RandomBridgeMap = bridgeMaker.makeBridge(bridgeSize);
         BridgeAlgorithm();
     }
