6e876a041c8931f4d8ee3170b8bac74161722ff6
bumwoo <gld55602@gmail.com>
2022-11-21 21:08:15 +0900

    test(BridgeGameTest): BridgeGame 테스트 코드 작성

--- src/test/java/bridge/BridgeGameTest.java
+++ src/test/java/bridge/BridgeGameTest.java
@@ -2,12 +2,30 @@ package bridge;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import bridge.constant.Constant;
+import bridge.constant.Direction;
 import bridge.constant.GameCommand;
+import java.util.stream.Stream;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 public class BridgeGameTest {
     private BridgeGame bridgeGame = new BridgeGame();
     private UserBridge userBridge = new UserBridge();
+
+    @ParameterizedTest
+    @MethodSource("parameterProvider")
+    void 이동_테스트(Direction direction, Direction now, String result) {
+        bridgeGame.move(userBridge, direction, now);
+        if (Direction.isUp(now)) {
+            assertThat(userBridge.getUpBridge().contains(result)).isTrue();
+        }
+        if (Direction.isDown(now)) {
+            assertThat(userBridge.getDownBridge().contains(result)).isTrue();
+        }
+    }
 
     @Test
     void 재시작_테스트() {
@@ -18,5 +36,14 @@ public class BridgeGameTest {
         assertThat(userBridge.getUpBridge().isEmpty()).isTrue();
         assertThat(userBridge.getDownBridge().isEmpty()).isTrue();
         assertThat(bridgeGameManager.getCount()).isEqualTo(count+1);
+    }
+
+    static Stream<Arguments> parameterProvider() {
+        return Stream.of(
+                Arguments.arguments(Direction.D, Direction.U, Constant.CROSS_FAIL),
+                Arguments.arguments(Direction.D, Direction.D, Constant.CROSS),
+                Arguments.arguments(Direction.U, Direction.U, Constant.CROSS),
+                Arguments.arguments(Direction.U, Direction.D, Constant.CROSS_FAIL)
+        );
     }
 }
