e0c690c695d05d0d427b500f69cd1fb565a9904d
jeongwle <dldl4568@naver.com>
2022-11-21 14:26:47 +0900

    feat: 게임 끝난 결과 출력하는 기능

--- src/main/java/bridge/enums/GameMessages.java
+++ src/main/java/bridge/enums/GameMessages.java
@@ -5,6 +5,11 @@ public enum GameMessages {
     RECEIVE_BRIDGE_SIZE_MESSAGE("다리의 길이를 입력해주세요."),
     RECEIVE_MOVING_DIRECTION_MESSAGE("이동할 칸을 선택해주세요. (위: U, 아래: D)"),
     RECEIVE_GAME_COMMAND_MESSAGE("게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)"),
+    GAME_RESULT_MESSAGE("\n최종 게임 결과\n"
+            + "{0}\n"
+            + "{1}\n\n"
+            + "게임 성공 여부: {2}\n"
+            + "총 시도한 횟수: {3}\n"),
     ;
     private final String gameMessage;
 
--- src/main/java/view/OutputView.java
+++ src/main/java/view/OutputView.java
@@ -1,8 +1,12 @@
 package view;
 
+import static bridge.enums.GameMessages.GAME_RESULT_MESSAGE;
 import static bridge.enums.GameMessages.GAME_START_MESSAGE;
 import static bridge.enums.GameMessages.RECEIVE_BRIDGE_SIZE_MESSAGE;
+import static bridge.enums.GameMessages.RECEIVE_GAME_COMMAND_MESSAGE;
+import static bridge.enums.GameMessages.RECEIVE_MOVING_DIRECTION_MESSAGE;
 
+import java.text.MessageFormat;
 import model.Bridge;
 
 /**
@@ -25,7 +29,11 @@ public class OutputView {
      * <p>
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void printResult() {
+    // TODO: 함수인자 세개이하로 줄이기
+    public void printResult(Bridge upperBridge, Bridge lowerBridge, String gameResult, int tryCount) {
+        String format = MessageFormat.format(GAME_RESULT_MESSAGE.toString(),
+                upperBridge, lowerBridge, gameResult, tryCount);
+        System.out.println(format);
     }
 
     public void printStartMessage() {
@@ -34,5 +42,13 @@ public class OutputView {
 
     public void printReceiveSizeMessage() {
         System.out.println(RECEIVE_BRIDGE_SIZE_MESSAGE);
+    }
+
+    public void printReceiveMovingDirectionMessage() {
+        System.out.println(RECEIVE_MOVING_DIRECTION_MESSAGE);
+    }
+
+    public void printReceiveGameCommandMessage() {
+        System.out.println(RECEIVE_GAME_COMMAND_MESSAGE);
     }
 }
