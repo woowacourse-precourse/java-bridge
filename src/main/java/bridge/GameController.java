5c87eb094c642755512087e214554dd47758fd40
LJW25 <dbjwl@g.skku.edu>
2022-11-21 15:40:24 +0900

    feat: GameController 선언

---
+++ src/main/java/bridge/GameController.java
@@ -0,0 +1,39 @@
+package bridge;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class GameController {
+
+    private final List<String> answerBridge;
+    private final List<String> curBridge;
+    private final int stageNum;
+    private final String status;
+
+    GameController(){
+        answerBridge = new ArrayList<>();
+        curBridge = new ArrayList<>();
+        stageNum = 0;
+        status = "Playing";
+    }
+
+
+    public static void playGame(){
+        // make bridge
+        // while ()
+        // play new Stage
+        // if success : print result
+        // if fail && end : print result
+    }
+
+
+    private void playNewStage(){
+        // cnt ++
+        // for
+        // get input U or D
+        // move
+
+        //
+    }
+
+}
