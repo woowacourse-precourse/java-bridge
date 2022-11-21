2bd0d5673d5a6b0ba7ae5c963a4001f27c8fe57b
doehy <dbehxks02@naver.com>
2022-11-21 20:57:56 +0900

    feat : 다리 생성

--- src/main/java/bridge/BridgeMaker.java
+++ src/main/java/bridge/BridgeMaker.java
@@ -1,5 +1,6 @@
 package bridge;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -18,6 +19,19 @@ public class BridgeMaker {
      * @return 입력받은 길이에 해당하는 다리 모양. 위 칸이면 "U", 아래 칸이면 "D"로 표현해야 한다.
      */
     public List<String> makeBridge(int size) {
-        return null;
+        List<String> bridgeStructure = new ArrayList<>();
+        for (int i = 0; i < size; i++) {
+            checkZeroOrOne(bridgeNumberGenerator.generate(),bridgeStructure);
+        }
+        return bridgeStructure;
+    }
+
+    public void checkZeroOrOne(int zeroOrOne, List<String> bridge) {
+        if (zeroOrOne == 0) {
+            bridge.add("D");
+        }
+        if (zeroOrOne == 1) {
+            bridge.add("U");
+        }
     }
 }
