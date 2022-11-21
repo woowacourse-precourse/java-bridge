c46ca3a6bcf93f3f2a5ae34c06911ffcca557511
syahn <496300@naver.com>
2022-11-21 14:24:32 +0900

    feat: ExceptionTest 구현
    - 입력에 대한 예외상황 처리 메시지 출력 확인

--- src/test/java/bridge/ExceptionTest.java
+++ src/test/java/bridge/ExceptionTest.java
@@ -1,8 +1,12 @@
 package bridge;
 
+import bridge.Model.Bridge;
+import bridge.Model.BridgeGame;
 import camp.nextstep.edu.missionutils.test.NsTest;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
 
 import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
 import static org.assertj.core.api.Assertions.assertThat;
@@ -23,8 +27,29 @@ public class ExceptionTest extends NsTest {
     @DisplayName("다리 길이 입력이 3부터 20 사이의 수가 아니면 예외가 발생한다.")
     @Test
     void inputWrongRangeNumberInBridgeSize() {
+        BridgeRandomNumberGenerator bridgeRandomNumberGenerator = new BridgeRandomNumberGenerator();
         assertSimpleTest(() -> {
-            runException("2");
+            new BridgeMaker(bridgeRandomNumberGenerator).validateBridgeSize("2");
+            assertThat(output()).contains(ERROR_MESSAGE);
+        });
+    }
+
+    @DisplayName("이동 입력이 U또는 D가 아니면 예외가 발생한다.")
+    @Test
+    void inputWrongStringInMovement() {
+        assertSimpleTest(() -> {
+            Bridge bridge = new Bridge(new ArrayList<>());
+            new BridgeGame(bridge).validateMoving("T");
+            assertThat(output()).contains(ERROR_MESSAGE);
+        });
+    }
+
+    @DisplayName("게임 상태 입력이 R 또는 Q가 아니면 예외가 발생한다.")
+    @Test
+    void inputWrongStringInGameCommand() {
+        assertSimpleTest(() -> {
+            Bridge bridge = new Bridge(new ArrayList<>());
+            new BridgeGame(bridge).validateGameCommand("F");
             assertThat(output()).contains(ERROR_MESSAGE);
         });
     }
