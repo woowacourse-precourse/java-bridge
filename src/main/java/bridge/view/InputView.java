e76cb4983a1eea6e630dcdac95b5c9945ab9188c
phdljr <phdljr@naver.com>
2022-11-22 07:08:51 +0900

    refactor: view 로직/유효성 검사 수정

--- src/main/java/bridge/view/InputView.java
+++ src/main/java/bridge/view/InputView.java
@@ -2,6 +2,11 @@ package bridge.view;
 
 import camp.nextstep.edu.missionutils.Console;
 
+import static bridge.config.BridgeGameCommand.*;
+import static bridge.config.BridgeGameConfig.BRIDGE_LENGTH_MAX;
+import static bridge.config.BridgeGameConfig.BRIDGE_LENGTH_MIN;
+import static bridge.view.message.exception.ExceptionMessage.*;
+
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
  */
@@ -11,35 +16,80 @@ public class InputView {
      * 다리의 길이를 입력받는다.
      */
     public int readBridgeSize() {
-        int bridgeSize = Integer.parseInt(Console.readLine());
-        return bridgeSize;
+        return readValidatedSize(false);
     }
 
     /**
      * 사용자가 이동할 칸을 입력받는다.
      */
     public String readMoving() {
-        String inputMoving = Console.readLine();
-        return inputMoving;
+        return readValidatedMoving(false);
     }
 
     /**
      * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
      */
     public String readGameCommand() {
-        String inputCommand = Console.readLine();
+        return readValidatedCommand(false);
+    }
+
+    private int readValidatedSize(boolean isValid) {
+        int bridgeSize = 0;
+        while (!isValid) {
+            try {
+                bridgeSize = Integer.parseInt(Console.readLine());
+                isValid = validateBridgeSize(bridgeSize);
+            } catch (IllegalArgumentException e) {
+                System.out.println(ERROR_WRONG_LENGTH);
+            }
+        }
+        return bridgeSize;
+    }
+
+    private String readValidatedCommand(boolean isValid) {
+        String inputCommand = "";
+        while (!isValid) {
+            try {
+                inputCommand = Console.readLine();
+                isValid = validateGameCommand(inputCommand);
+            } catch (IllegalArgumentException e) {
+                System.out.println(ERROR_WRONG_COMMAND);
+            }
+        }
         return inputCommand;
     }
 
-    private void validateBridgeSize(){
-
+    private String readValidatedMoving(boolean isValid) {
+        String inputMoving = "";
+        while (!isValid) {
+            try {
+                inputMoving = Console.readLine();
+                isValid = validateMoving(inputMoving);
+            } catch (IllegalArgumentException e) {
+                System.out.println(ERROR_WRONG_MOVING);
+            }
+        }
+        return inputMoving;
     }
 
-    private void validateMoving(){
-
+    private boolean validateBridgeSize(int size) {
+        if (BRIDGE_LENGTH_MIN <= size && size <= BRIDGE_LENGTH_MAX) {
+            return true;
+        }
+        throw new IllegalArgumentException();
     }
 
-    private void validateGameCommand(){
+    private boolean validateMoving(String inputMoving) {
+        if (inputMoving.equals(COMMAND_BRIDGE_UP) || inputMoving.equals(COMMAND_BRIDGE_DOWN)) {
+            return true;
+        }
+        throw new IllegalArgumentException();
+    }
 
+    private boolean validateGameCommand(String inputCommand) {
+        if (inputCommand.equals(COMMAND_GAME_RETRY) || inputCommand.equals(COMMAND_GAME_EXIT)) {
+            return true;
+        }
+        throw new IllegalArgumentException();
     }
 }
--- src/main/java/bridge/view/OutputView.java
+++ src/main/java/bridge/view/OutputView.java
@@ -1,5 +1,13 @@
 package bridge.view;
 
+import bridge.model.Bridge;
+import bridge.model.BridgeGame;
+import bridge.model.Statistics;
+
+import static bridge.config.BridgeGameCommand.COMMAND_BRIDGE_DOWN;
+import static bridge.config.BridgeGameCommand.COMMAND_BRIDGE_UP;
+import static bridge.view.message.io.IOMessage.*;
+
 /**
  * 사용자에게 게임 진행 상황과 결과를 출력하는 역할을 한다.
  */
@@ -10,7 +18,16 @@ public class OutputView {
      * <p>
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void printMap(String inputMoving) {
+    public void printMap(Bridge currentBridge, Bridge answerBridge) {
+        int wrongIndex = currentBridge.findWrongIndex(answerBridge);
+
+        System.out.print(MESSAGE_BRIDGE_PREFIX);
+        printUpperBridge(currentBridge, wrongIndex);
+        System.out.println(MESSAGE_BRIDGE_SUFFIX);
+
+        System.out.print(MESSAGE_BRIDGE_PREFIX);
+        printLowerBridge(currentBridge, wrongIndex);
+        System.out.println(MESSAGE_BRIDGE_SUFFIX);
     }
 
     /**
@@ -18,6 +35,73 @@ public class OutputView {
      * <p>
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void printResult() {
+    public void printResult(BridgeGame bridgeGame) {
+        System.out.println(MESSAGE_RESULT);
+        printMap(bridgeGame.getCurrentBridge(), bridgeGame.getAnswerBridge());
+        printStatistics(bridgeGame.getStatistics());
+    }
+
+    private void printUpperBridge(Bridge currentBridge, int wrongIndex) {
+        for (int index = 0; index < currentBridge.length(); index++) {
+            if (currentBridge.position(index).equals(COMMAND_BRIDGE_UP) && index == wrongIndex) {
+                System.out.print(MESSAGE_BRIDGE_WRONG);
+                return;
+            }
+            printSubBridge(currentBridge, index, COMMAND_BRIDGE_UP);
+        }
+    }
+
+    private void printLowerBridge(Bridge currentBridge, int wrongIndex) {
+        for (int index = 0; index < currentBridge.length(); index++) {
+            if (currentBridge.position(index).equals(COMMAND_BRIDGE_DOWN) && index == wrongIndex) {
+                System.out.print(MESSAGE_BRIDGE_WRONG);
+                return;
+            }
+            printSubBridge(currentBridge, index, COMMAND_BRIDGE_DOWN);
+        }
+    }
+
+    private void printSubBridge(Bridge currentBridge, int index, String position) {
+        if (currentBridge.position(index).equals(position)) {
+            System.out.print(MESSAGE_BRIDGE_CORRECT);
+        }
+        if (!currentBridge.position(index).equals(position)) {
+            System.out.print(MESSAGE_BRIDGE_EMPTY);
+        }
+        if (index < currentBridge.length() - 1) {
+            System.out.print(MESSAGE_BRIDGE_MIDDLE);
+        }
+    }
+
+    private void printStatistics(Statistics statistics) {
+        boolean clear = statistics.isClear();
+        printClear(clear);
+
+        int countRetry = statistics.getCountRetry();
+        System.out.println(MESSAGE_RETRY + countRetry);
+    }
+
+    private void printClear(boolean clear) {
+        if (clear) {
+            System.out.println(MESSAGE_CLEAR);
+            return;
+        }
+        System.out.println(MESSAGE_FAIL);
+    }
+
+    public void printGameCommand() {
+        System.out.println(MESSAGE_INPUT_RETRY);
+    }
+
+    public void printStart() {
+        System.out.println(MESSAGE_START);
+    }
+
+    public void printInputMoving() {
+        System.out.println(MESSAGE_INPUT_MOVING);
+    }
+
+    public void printInputLength() {
+        System.out.println(MESSAGE_INPUT_LENGTH);
     }
 }
