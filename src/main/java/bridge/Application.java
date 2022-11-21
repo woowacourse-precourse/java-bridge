b40150b3a8cea5765bcc024d05e6dbc89f7e16d0
yejin <kwonyj1022@naver.com>
2022-11-21 14:45:57 +0900

    refactor: Activity.java 예외처리문 삭제

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -3,11 +3,6 @@ package bridge;
 public class Application {
 
     public static void main(String[] args) {
-        try{
-            new Run();
-        } catch(IllegalArgumentException e){
-            System.out.println(e.getMessage());
-        }
-        // TODO: 프로그램 구현
+        new Run();
     }
 }
