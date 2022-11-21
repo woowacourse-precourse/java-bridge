db6a00131f33db7873cd8110d0c8bbcecc6f92b8
Sieun Park <psieun0606@gmail.com>
2022-11-21 12:48:34 +0900

    feat: 게임 실행부터 게임 진행까지 진행하는 기능 추가

--- src/main/java/bridge/Controller/BridgeGameController.java
+++ src/main/java/bridge/Controller/BridgeGameController.java
@@ -1,4 +1,30 @@
 package bridge.Controller;
 
+import bridge.Domain.BridgeMaker;
+import bridge.Domain.BridgeNumberGenerator;
+import bridge.Domain.BridgeRandomNumberGenerator;
+import bridge.View.InputView;
+import bridge.View.OutputView;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class BridgeGameController {
+
+    OutputView outputView = new OutputView();
+    List<String> bridge = new ArrayList<>();
+    public void start() {
+        int lengthOfBridge = gameStart();
+    }
+
+    public int gameStart() {
+        outputView.printGameStart();
+        outputView.printEmptyLine();
+        int lengthOfBridge = InputView.readBridgeSize();
+        BridgeNumberGenerator bridgeNumberGenerator = new BridgeRandomNumberGenerator();
+        BridgeMaker bridgeMaker = new BridgeMaker(bridgeNumberGenerator);
+        bridge = bridgeMaker.makeBridge(lengthOfBridge);
+        outputView.printEmptyLine();
+        return lengthOfBridge;
+    }
 }
