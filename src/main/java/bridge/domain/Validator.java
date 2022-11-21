fc2401c1c475903a780a643f89e257e282a72ebd
Yerim lee <103623292+lyrWinterCat@users.noreply.github.com>
2022-11-21 13:36:01 +0900

    refactor (Validator, ErrorMessage) : 검증 메서드 수정
    한가지 검증으로 모든 예외 처리 가능하도록 수정

--- src/main/java/bridge/domain/Validator.java
+++ src/main/java/bridge/domain/Validator.java
@@ -5,8 +5,9 @@ import bridge.vo.ErrorMessage;
 public class Validator {
 
     public static void validateBridgeSize(String bridgeSize) {
-        validateNumber(bridgeSize);
-        validateRange(bridgeSize);
+        if (!bridgeSize.matches("^[3-9]$|^1\\d$|^20$")) {
+            throwIllegalArgumentException(ErrorMessage.BRIDGE_SIZE_NOT_NUMBER);
+        }
     }
 
     public static void validateMoving(String moving) {
@@ -18,18 +19,6 @@ public class Validator {
     public static void validateRestartChoice(String restartChoice) {
         if (!restartChoice.matches("^[R]$|^[Q]$")) {
             throwIllegalArgumentException(ErrorMessage.RESTART_WRONG_INPUT);
-        }
-    }
-
-    private static void validateNumber(String bridgeSize) {
-        if (!bridgeSize.matches("\\d+")) {
-            throwIllegalArgumentException(ErrorMessage.BRIDGE_SIZE_NOT_NUMBER);
-        }
-    }
-
-    private static void validateRange(String bridgeSize) {
-        if (!bridgeSize.matches("^[3-9]$|^1\\d$|^20$")) {
-            throwIllegalArgumentException(ErrorMessage.BRIDGE_SIZE_WRONG_RANGE);
         }
     }
 
--- src/main/java/bridge/vo/ErrorMessage.java
+++ src/main/java/bridge/vo/ErrorMessage.java
@@ -2,7 +2,6 @@ package bridge.vo;
 
 public enum ErrorMessage {
     BRIDGE_SIZE_NOT_NUMBER("[ERROR] 다리 길이는 3 이상 20 이하인 숫자로 입력 가능합니다."),
-    BRIDGE_SIZE_WRONG_RANGE("[ERROR] 다리 길이는 3 이상 20 이하로 입력해야 합니다."),
     MOVING_WRONG_INPUT("[ERROR] U 또는 D 만 입력 가능합니다. ( U : 윗칸 선택 , D : 아랫칸 선택)"),
     RESTART_WRONG_INPUT("[ERROR] R 또는 Q만 입력 가능합니다. ( R : 재시작, Q : 게임 종료)");
 
