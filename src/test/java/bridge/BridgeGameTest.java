eb3aaa08a63873051b3df482ee9ff9fcb98a22b4
ensk26 <tjensk26@gmail.com>
2022-11-22 05:14:24 +0900

    refactor(BridgeGameTest): 테스트 코드 정리
    기존 테스트 수정 to BridgeGameTest
    - 중복되는 기능을 묶어서 메서드로 처리
    - 임의로 넣는 값이 아닌 직접 실행해서 얻은 값으로 테스트 진행
    - 다리 게임을 생성해주는 메서드
    - 다리 결과를 표시해주는 메서드
    - 이동에 따라 성공/실패를 반환하는 메서드

--- src/test/java/bridge/BridgeGameTest.java
+++ src/test/java/bridge/BridgeGameTest.java
@@ -3,6 +3,7 @@ package bridge;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -12,8 +13,7 @@ class BridgeGameTest {
     @DisplayName("다음 위치가 이동할 수 있는 다리 칸인지 확인 ")
     @Test
     void move() {
-        List<String> bridge = List.of("U", "D", "D");
-        BridgeGame bridgeGame = new BridgeGame(bridge);
+        BridgeGame bridgeGame = bridgeGameGenerate(List.of("U", "D", "D"));
         String direct = "U";
         assertThat(bridgeGame.move(direct)).isTrue();
     }
@@ -21,28 +21,41 @@ class BridgeGameTest {
     @DisplayName("다리의 결과값이 조건에 따라 저장이 되는지 확인")
     @Test
     void 다리_결과값_저장_테스트() {
-        List<String> bridge = List.of("U", "D", "D");
-        BridgeGame bridgeGame = new BridgeGame(bridge);
-        String direct = "U";
-        boolean result = true;
-        bridgeGame.bridgeMark(direct, result);
-        assertThat(bridgeGame.getBridgeMap()).containsExactly(List.of("O"), List.of(""));
+        BridgeGame bridgeGame = bridgeGameGenerate(List.of("U", "D", "D"));
+        List<String> direct = List.of("U", "D", "U");
+        bridgeMarkGenerate(bridgeGame, direct);
+        assertThat(bridgeGame.getBridgeMap()).containsExactly(List.of("O", " ", "X"), List.of(" ", "O", " "));
     }
 
     @DisplayName("다리 게임 재시도 시 이전 다리 결과값이 되는지 확인")
     @Test
     void 다리_게임_재시도_다리_결과값_테스트() {
-        List<String> bridge = List.of("U", "D", "D");
-        BridgeGame bridgeGame = new BridgeGame(bridge);
+        BridgeGame bridgeGame = bridgeGameGenerate(List.of("U", "D", "D"));
         List<String> direct = List.of("U", "D", "U");
-        List<Boolean> result = List.of(true, true, false);
-        for (String d : direct) {
-            bridgeGame.move(d);
-        }
+        bridgeMarkGenerate(bridgeGame, direct);
+        bridgeGame.retry();
+        assertThat(bridgeGame.getBridgeMap()).containsExactly(List.of("O", " "), List.of(" ", "O"));
+    }
+
+    private BridgeGame bridgeGameGenerate(List<String> bridge) {
+        return new BridgeGame(bridge);
+    }
+
+    private void bridgeMarkGenerate(BridgeGame bridgeGame, List<String> direct) {
+        List<Boolean> result = moveResultGenerate(bridgeGame, direct);
+
         for (int i = 0; i < result.size(); i++) {
             bridgeGame.bridgeMark(direct.get(i), result.get(i));
         }
-        bridgeGame.retry();
-        assertThat(bridgeGame.getBridgeMap()).containsExactly(List.of("O", ""), List.of("", "O"));
+    }
+
+    private List<Boolean> moveResultGenerate(BridgeGame bridgeGame, List<String> direct) {
+        List<Boolean> result = new ArrayList<>();
+
+        for (String d : direct) {
+            boolean move = bridgeGame.move(d);
+            result.add(move);
+        }
+        return result;
     }
 }
