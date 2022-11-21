de5e505c314500b42789452810aa485285c0f98d
UngHeung <t01047377477@gmail.com>
2022-11-21 15:59:19 +0900

    feat : (TEST) 테스트 입력 될 수 있는 값 추가 및 에러 메시지 확인 기능 추가

--- src/test/java/service/ValidateTest.java
+++ src/test/java/service/ValidateTest.java
@@ -1,6 +1,10 @@
 package service;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -8,50 +12,46 @@ import org.junit.jupiter.api.Test;
 import domain.Validate;
 
 public class ValidateTest {
-	Validate validate = new Validate();
+	private Validate validate = new Validate();
+	List<String> testList = new ArrayList<>();
+	private IllegalArgumentException exception = null;
+	private String errorMessage = null;
 
-	// 다리 크기 입력 예외 오류 테스트
-	@DisplayName("다리 크기가 3보다 작으면 예외 발생")
-	@Test
-	void inputBySmallerSize() {
-		assertThrows(RuntimeException.class, () -> validate.validateSize("2"));
-	}
-	
-	@DisplayName("다리 크기가 20보다 크면 예외 발생")
-	@Test
-	void inputByBiggerSize() {
-		assertThrows(RuntimeException.class, () -> validate.validateSize("21"));
-	}
-	
-	@DisplayName("숫자가 아닌  문자가 입력 될 경우 예외 발생")
+	@DisplayName("다리 크기 입력 예외 오류 발생 및 오류 메시지 테스트")
 	@Test
 	void inputByWrongSize() {
-		assertThrows(RuntimeException.class, () -> validate.validateSize("a"));
+		testList = List.of("a", "ab", "2", "21", "123");
+		
+		for (String testItem : testList) {
+			exception = assertThrows(IllegalArgumentException.class, () -> validate.validateSize(testItem));
+			errorMessage = exception.getMessage();
+			assertEquals("[ERROR] 다리 길이는 3부터 20 사이의 숫자여야 합니다.", errorMessage);
+		}
 	}
 	
-	// 게임 이동 입력 예외 오류 테스트
-	@DisplayName("U, D 외 다른 문자가 입력 될 경우 예외 발생")
+	@DisplayName("이동 입력 예외 오류 발생 및 오류 메시지 테스트")
 	@Test
-	void inputByWrongMoving() {
-		assertThrows(RuntimeException.class, () -> validate.validateGameCommand("A"));
+	void inputByWrongMove() {
+		testList = List.of("u", "UU", "d", "DD", "a", "A", "1", "123");
+		
+		for (String testItem : testList) {
+			exception = assertThrows(IllegalArgumentException.class, () -> validate.validateMove(testItem));
+			errorMessage = exception.getMessage();
+			assertEquals("[ERROR] 위로 이동은 'U', 아래로 이동은 'D'를 입력하여야 합니다.", errorMessage);
+		}
 	}
 	
-	@DisplayName("문자열이 입력 될 경우 예외 발생")
-	@Test
-	void inputByStringMoving() {
-		assertThrows(RuntimeException.class, () -> validate.validateGameCommand("abc"));
-	}
-	
-	// 게임 재시작 여부 입력 예외 오류 테스트
-	@DisplayName("R, Q 외 다른 문자가 입력 될 경우 예외 발생")
+	@DisplayName("게임 재시작 여부 예외 오류 발생 및 오류 메시지 테스트")
 	@Test
 	void inputByWrongCommand() {
-		assertThrows(RuntimeException.class, () -> validate.validateGameCommand("B"));
+		testList = List.of("q", "QQ", "r", "RR", "b", "B", "3", "321");
+		
+		for (String testItem : testList) {
+			exception = assertThrows(IllegalArgumentException.class, () -> validate.validateGameCommand(testItem));
+			errorMessage = exception.getMessage();
+			assertEquals("[ERROR] 재시작은 'R', 종료는 'Q'를 입력하여야 합니다.", errorMessage);
+		}
 	}
 	
-	@DisplayName("문자열이 입력 될 경우 예외 발생")
-	@Test
-	void inputByStringCommand() {
-		assertThrows(RuntimeException.class, () -> validate.validateGameCommand("123"));
-	}
+	
 }
