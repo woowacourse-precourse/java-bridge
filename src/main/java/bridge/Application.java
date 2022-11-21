2e07d524b56f9eec0245fa539de9f99c8f7d12a4
hantae <hantae96@naver.com>
2022-11-21 23:30:24 +0900

    Feat : 커맨드에 맞춰 다리를 출력하는 기능 추가

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -3,16 +3,19 @@ package bridge;
 import java.util.Arrays;
 
 public class Application {
-
     public static void main(String[] args) {
         // TODO: 프로그램 구현
         InputView Input = new InputView();
         int bridgeSize =Input.readBridgeSize();
-//        Input.readMoving();
-//        Input.readGameCommand();
 
         BridgeMaker Bridge = new BridgeMaker(new BridgeRandomNumberGenerator());
         Bridge.makeBridge(bridgeSize);
         System.out.println(Bridge.getBridge());
+
+        BridgeGame Game =  new BridgeGame(bridgeSize,Bridge.getBridge());
+        Game.move(Input);
+//        Input.readGameCommand();
+
+
     }
 }
--- src/main/java/bridge/BridgeGame.java
+++ src/main/java/bridge/BridgeGame.java
@@ -1,16 +1,31 @@
 package bridge;
 
+import java.util.List;
+
 /**
  * 다리 건너기 게임을 관리하는 클래스
  */
 public class BridgeGame {
-
+    static int gameStepCount;
+    static int bridgeSize;
+    static List<String>Bridge;
+    BridgeGame(int bridgeSize, List<String> Bridge){
+        this.bridgeSize = bridgeSize;
+        this.Bridge = Bridge;
+    }
     /**
      * 사용자가 칸을 이동할 때 사용하는 메서드
      * <p>
      * 이동을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void move() {
+    public void move(InputView Input) {
+        while(gameStepCount != bridgeSize){
+            String movingCommand=Input.readMoving();
+            System.out.println(movingCommand);
+            OutputView Output = new OutputView();
+            Output.printMap(gameStepCount,movingCommand,Bridge);
+            gameStepCount++;
+        }
     }
 
     /**
