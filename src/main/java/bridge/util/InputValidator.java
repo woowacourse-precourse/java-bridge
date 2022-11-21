bb115df10627757717cc990bb29fbabc2a40e5bd
matt <matt1235@naver.com>
2022-11-21 10:31:46 +0900

    fix: 입력의 유효성 검사

--- src/main/java/bridge/util/InputValidator.java
+++ src/main/java/bridge/util/InputValidator.java
@@ -1,26 +1,32 @@
 package bridge.util;
 
-import bridge.view.ErrorMessage;
+import bridge.view.Message;
 
 public class InputValidator {
     public InputValidator() {
     }
 
-    public void validateBridgeLength(Integer input) {
-        if(input < 3 || input > 20){
-            throw new IllegalArgumentException(String.valueOf(ErrorMessage.ERROR_MESSAGE_BRIDGE_LENGTH));
+    public void validateBridgeLength(String input) {
+        int value = 0;
+        try {
+            Integer.parseInt(input);
+        }catch (NumberFormatException e){
+            throw new IllegalArgumentException(String.valueOf(Message.ERROR_MESSAGE_BRIDGE_LENGTH));
         }
+        value = Integer.parseInt(input);
+        if(value >= 3 && value <= 20) return;
+        throw new IllegalArgumentException(String.valueOf(Message.ERROR_MESSAGE_BRIDGE_LENGTH));
     }
 
     public void validateMoving(String input) {
         if(!input.equals("U") && !input.equals("D")){
-            throw new IllegalArgumentException(String.valueOf(ErrorMessage.ERROR_MESSAGE_MOVING));
+            throw new IllegalArgumentException(String.valueOf(Message.ERROR_MESSAGE_MOVING));
         }
     }
 
     public void validateGameCommand(String input) {
         if(!input.equals("R") && !input.equals("Q")){
-            throw new IllegalArgumentException(String.valueOf(ErrorMessage.ERROR_MESSAGE_GAME_COMMAND));
+            throw new IllegalArgumentException(String.valueOf(Message.ERROR_MESSAGE_GAME_COMMAND));
         }
     }
 
