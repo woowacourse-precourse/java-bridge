a4e3ed2748ccc8ec55de5c81947a11b1850933f9
CupRaccoon <41440969+CupRaccoon@users.noreply.github.com>
2022-11-22 00:33:05 +0900

    feat: Definite BridgeGame methods and Add member variables

--- src/main/java/bridge/BridgeGame.java
+++ src/main/java/bridge/BridgeGame.java
@@ -1,16 +1,25 @@
 package bridge;
 
+import java.util.List;
+
 /**
  * 다리 건너기 게임을 관리하는 클래스
  */
 public class BridgeGame {
+    private List<String> bridge;
+    private int nowPosition = -1;
 
+    public BridgeGame(List<String> bridge) {
+        this.bridge = bridge;
+    }
     /**
      * 사용자가 칸을 이동할 때 사용하는 메서드
      * <p>
      * 이동을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void move() {
+    public boolean move(String nextMove) {
+        this.nowPosition++;
+        return bridge.get(this.nowPosition).equals(nextMove);
     }
 
     /**
@@ -19,5 +28,6 @@ public class BridgeGame {
      * 재시작을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
     public void retry() {
+        this.nowPosition = -1;
     }
 }
