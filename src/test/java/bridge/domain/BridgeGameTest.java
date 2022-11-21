e98f04399cd88c151742a1603e31629a3fd54cba
K-jun98 <alap_u@naver.com>
2022-11-21 15:57:40 +0900

    test - 입력받은 방향으로 이동테스트

---
+++ src/test/java/bridge/domain/BridgeGameTest.java
@@ -0,0 +1,26 @@
+package bridge.domain;
+
+import java.util.Arrays;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class BridgeGameTest {
+
+    BridgeGame bridgeGame = new BridgeGame(Arrays.asList("U"));
+
+    @DisplayName("UP 방향으로 이동 테스트")
+    @Test
+    void moveUpTest() {
+        bridgeGame = new BridgeGame(Arrays.asList("U"));
+        Assertions.assertThat(bridgeGame.move(Direction.UP)).isTrue();
+    }
+
+    @DisplayName("DOWN 방향으로 이동 테스트")
+    @Test
+    void moveDownTest() {
+        bridgeGame = new BridgeGame(Arrays.asList("D"));
+        Assertions.assertThat(bridgeGame.move(Direction.DOWN)).isTrue();
+    }
+
+}
