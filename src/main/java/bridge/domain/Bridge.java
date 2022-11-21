3021c59588f7b816ab45639314aa10edb98adc29
yujeongkwon <yoojeongkwon@naver.com>
2022-11-21 21:27:56 +0900

    feat(Bridge) : 다리 길이 입력받기 + 예외처리

--- src/main/java/bridge/domain/Bridge.java
+++ src/main/java/bridge/domain/Bridge.java
@@ -1,16 +1,12 @@
 package bridge.domain;
 
+import bridge.exception.BridgeException;
+
 import java.util.List;
 
 public class Bridge {
-    private final int bridgeLength;
-    private final List<String> resultBridge;
-
-    public Bridge(int bridgeLength, List<String> resultBridge) {
-        this.bridgeLength = bridgeLength;
-        this.resultBridge = resultBridge;
-    }
-
+    private int bridgeLength;
+    private List<String> resultBridge;
 
     public int getBridgeLength() {
         return bridgeLength;
@@ -19,4 +15,22 @@ public class Bridge {
     public List<String> getResultBridge() {
         return resultBridge;
     }
+
+    public void inputBridgeLength(String input){
+        validate(input);
+        bridgeLength = Integer.parseInt(input);
+    }
+
+    private void validate(String input){
+        for (int i = 0; i < input.length(); i++) {
+            if (!Character.isDigit(input.charAt(i)))
+                throw new IllegalArgumentException(BridgeException.NOT_DISIT.getErrorMessage());
+        }
+
+        int intInput = Integer.parseInt(input);
+
+        if (intInput > 20 || intInput < 3)
+            throw new IllegalArgumentException(BridgeException.INVALID_LENGTH_RANGE.getErrorMessage());
+    }
+
 }
