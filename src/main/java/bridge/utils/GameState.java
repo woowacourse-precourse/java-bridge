fae152e335cf689d8f9fb4f187c4a0206b0c9446
dev_kong <ahdjd5@gmail.com>
2022-11-21 14:40:22 +0900

    fix(GameState): remove label field

--- src/main/java/bridge/utils/GameState.java
+++ src/main/java/bridge/utils/GameState.java
@@ -1,17 +1,7 @@
 package bridge.utils;
 
 public enum GameState {
-    WIN("win"),
-    LOOSE("loose"),
-    PLAYING("playing");
-
-    private final String label;
-
-    GameState(String label) {
-        this.label =label;
-    }
-
-    public String getLabel() {
-        return label;
-    }
+    WIN,
+    LOOSE,
+    PLAYING;
 }
