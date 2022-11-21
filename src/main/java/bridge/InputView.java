9e39b94adc483dded1666160836914239e73be75
YooraKim <62877255+YooraKim@users.noreply.github.com>
2022-11-22 01:40:40 +0900

    feat: 게임 재시도 여부 입력받는 함수 생성

--- src/main/java/bridge/InputView.java
+++ src/main/java/bridge/InputView.java
@@ -57,6 +57,8 @@ public class InputView {
      * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
      */
     public String readGameCommand() {
-        return null;
+        printGameCommandMsg();
+        String gameCommand = checkGameCommand(Console.readLine());
+        return gameCommand;
     }
 }
