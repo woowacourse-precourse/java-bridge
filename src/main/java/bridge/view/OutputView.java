ee5f24ae851f8335bb6a79143032d289994c9b3f
Voyager003 <85725033+Voyager003@users.noreply.github.com>
2022-11-21 17:04:42 +0900

    Feat: 추가 -  printUpBridgestate, DownBridgestate, printMiddDleline, Rightparenthesis printUpBridgestate, DownBridgestate: 윗칸, 아랫칸의 다리를 따로 출력하는 메서드 printMiddDleline, Rightparenthesis: middleline(|)과 rightparenthesis(])를 조건에 맞도록 출력하는 메서드

--- src/main/java/bridge/view/OutputView.java
+++ src/main/java/bridge/view/OutputView.java
@@ -1,6 +1,10 @@
 package bridge.view;
 
+import bridge.constant.Constant;
+import bridge.constant.Sign;
 import bridge.domain.BridgeGame;
+
+import java.util.List;
 
 /**
  * 사용자에게 게임 진행 상황과 결과를 출력하는 역할을 한다.
@@ -13,19 +17,41 @@ public class OutputView {
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
     public void printMap(BridgeGame bridgeGame) {
-        printUpBridgestate(bridgeGame);
-        printDownBridgestate(bridgeGame);
+        printUpBridgestate(bridgeGame.getplayerMoving(), bridgeGame.getBridgeStatus());
+        printDownBridgestate(bridgeGame.getplayerMoving(), bridgeGame.getBridgeStatus());
     }
 
-    private void printUpBridgestate(BridgeGame bridgeGame) {
-
+    private void printUpBridgestate(List<String> playerMoving, List<String> bridgeStatus) {
+        System.out.print(Sign.LEFT_PARENTHESIS.getSign());
+        for (int i = 0; i < playerMoving.size() - 1; i++) {
+            printMiddleline(playerMoving.get(i), bridgeStatus.get(i), Constant.UP.getConstant());
+        }
+        printRightparenthesis(playerMoving.get(playerMoving.size() - 1), bridgeStatus.get(playerMoving.size() - 1), Constant.UP.getConstant());
     }
 
-    private void printDownBridgestate(BridgeGame bridgeGame) {
-
+    private void printDownBridgestate(List<String> playerMoving, List<String> bridgeStatus) {
+        System.out.print(Sign.LEFT_PARENTHESIS.getSign());
+        for (int i = 0; i < playerMoving.size() - 1; i++) {
+            printMiddleline(playerMoving.get(i), bridgeStatus.get(i), Constant.UP.getConstant());
+        }
+        printRightparenthesis(playerMoving.get(playerMoving.size() - 1), bridgeStatus.get(playerMoving.size() - 1), Constant.DOWN.getConstant());
     }
 
+    private void printMiddleline(String moving, String matching, String updown) {
+        if (moving.equals(updown)) {
+            System.out.printf(Sign.MIDDLE_LINE_STRING.getSign(), matching);
+            return;
+        }
+        System.out.print(Sign.MIDDLE_LINE.getSign());
+    }
 
+    private void printRightparenthesis(String moving, String matching, String updown) {
+        if (moving.equals(updown)) {
+            System.out.printf(Sign.RIGHT__PARENTHESIS_STRING.getSign(), matching);
+            return;
+        }
+        System.out.println(Sign.RIGHT_PARENTHESIS.getSign());
+    }
 
     /**
      * 게임의 최종 결과를 정해진 형식에 맞춰 출력한다.
