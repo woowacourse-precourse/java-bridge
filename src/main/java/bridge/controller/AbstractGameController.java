e9145e50f8b1d6d2c2b1213aab617f16ade02463
qkrdmswl <eunji303@naver.com>
2022-11-21 16:49:52 +0900

    feat: define AbstractGameController

--- src/main/java/bridge/controller/AbstractGameController.java
+++ src/main/java/bridge/controller/AbstractGameController.java
@@ -1,2 +1,21 @@
-package bridge.controller;public class AbstractGameController {
+package bridge.controller;
+
+import bridge.service.BridgeGame;
+import bridge.service.BridgeMaker;
+import bridge.view.InputView;
+import utils.BridgeNumberGenerator;
+import utils.BridgeRandomNumberGenerator;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public abstract class AbstractGameController {
+    BridgeNumberGenerator bridgeNumberGenerator = new BridgeRandomNumberGenerator();
+    InputView inputView = new InputView();
+    List<String> bridgeInfo = new ArrayList<>();
+    BridgeGame bridgeGame = new BridgeGame();
+    BridgeMaker bridgeMaker = new BridgeMaker(bridgeNumberGenerator);
+    int tryCnt;
+
+    boolean run() {return true;}
 }
