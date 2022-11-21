a8cda97197fbce45613343a48e13f6c271c8fe6c
sjkim <seoungjoo01@gmail.com>
2022-11-22 01:08:08 +0900

    feat : 3번 입력받은 만큼 다리생성 기능

--- src/main/java/bridge/BridgeGame.java
+++ src/main/java/bridge/BridgeGame.java
@@ -1,15 +1,15 @@
 package bridge;
 
-/**
- * 다리 건너기 게임을 관리하는 클래스
- */
-public class BridgeGame {
+import java.util.List;
 
-    /**
-     * 사용자가 칸을 이동할 때 사용하는 메서드
-     * <p>
-     * 이동을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
-     */
+public class BridgeGame {
+    private List<String> answerBridge ;
+
+    private final BridgeMaker bridgeMaker = new BridgeMaker(new BridgeRandomNumberGenerator());
+
+    public void makeAnswerBridge(int size){
+        this.answerBridge = bridgeMaker.makeBridge(size);
+    }
     public void move() {
     }
 
@@ -20,4 +20,16 @@ public class BridgeGame {
      */
     public void retry() {
     }
+
+    public int getAnswerBridgeSize(){
+        return answerBridge.size();
+    }
+    public List<String> getAnswerBridge() {
+        return answerBridge;
+    }
+
+    public void setAnswerBridge(List<String> answerBridge) {
+        this.answerBridge = answerBridge;
+    }
+
 }
