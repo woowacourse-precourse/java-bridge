fd363ffd3d2ccff08d71cad18c34a829d4233d8d
rjsah09 <83952174+rjsah09@users.noreply.github.com>
2022-11-22 02:15:23 +0900

    refactor: Output.printMap() 수정, Output.getMap() 추가

--- src/main/java/bridge/OutputView.java
+++ src/main/java/bridge/OutputView.java
@@ -14,7 +14,11 @@ public class OutputView {
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
     public void printMap(List<String> bridge, List<String> crossed) {
-        System.out.println(getCrossedLine("U", bridge, crossed) + "\n" + getCrossedLine("D", bridge, crossed));
+        System.out.println(getMap(bridge, crossed));
+    }
+
+    public String getMap(List<String> bridge, List<String> crossed) {
+        return getCrossedLine("U", bridge, crossed) + "\n" + getCrossedLine("D", bridge, crossed);
     }
 
     public String getCrossedLine(String lineName, List<String> bridge, List<String> crossed) {
