6216b1165b78e2769c6a31d44dc4746108f65b0b
jsk5341 <102288039+jsk5341@users.noreply.github.com>
2022-11-22 06:13:27 +0900

    fix: error on print function for second row

--- src/main/java/bridge/OutputView.java
+++ src/main/java/bridge/OutputView.java
@@ -9,9 +9,9 @@ import java.util.List;
  */
 public class OutputView {
 
-    String correct = "[ O ]";
-    String notSelected = "[   ]";
-    String wrong = "[ X ]";
+    String correct = "O";
+    String notSelected = "   ";
+    String wrong = " X ";
     /**
      * 현재까지 이동한 다리의 상태를 정해진 형식에 맞춰 출력한다.
      * <p>
@@ -27,17 +27,17 @@ public class OutputView {
         return currentResult;
     }
 
-    public List<String> printMapNextRow(List<String> currentResult) {
-        for (int index = 0; index < currentResult.size(); index++) {
-            if (currentResult.get(index).equals(correct)) {
-                currentResult.set(index, notSelected);
-                index++;
+    public List<String> printMapNextRow(List<String> previousRowResult) {
+        List<String> nextRowMap = new ArrayList<>();
+        for (int index = 0; index < previousRowResult.size(); index++) {
+            if (previousRowResult.get(index).equals(correct)||previousRowResult.get(index).equals(wrong)) {
+                nextRowMap.add(notSelected);
             }
-            if (currentResult.get(index).equals(correct)) {
-                currentResult.set(index, notSelected);
+            if (previousRowResult.get(index).equals(notSelected)) {
+                nextRowMap.add(correct);
             }
         }
-        return currentResult;
+        return nextRowMap;
     }
 
     public List<String> printMapCorrect(List<String> bridgeAnswer, List<String> currentResult, int stage) {
@@ -51,10 +51,10 @@ public class OutputView {
     }
 
     public List<String> printMapWrong(List<String> bridgeAnswer, List<String> currentResult, int stage) {
-        if (bridgeAnswer.get(stage).equals("U")) {
+        if (bridgeAnswer.get(stage).equals("D")) {
             currentResult.add(wrong);
         }
-        if (bridgeAnswer.get(stage).equals("D")) {
+        if (bridgeAnswer.get(stage).equals("U")) {
             currentResult.add(notSelected);
         }
 
