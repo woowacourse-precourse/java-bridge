20bbfe1d44280a143b26dbbd4bb3ff65674946f5
jyj1143 <jyj1143@gmail.com>
2022-11-21 16:26:32 +0900

    feat(InputView): 플레이어 이동 기능 추가

--- src/main/java/bridge/view/InputView.java
+++ src/main/java/bridge/view/InputView.java
@@ -6,8 +6,12 @@ import camp.nextstep.edu.missionutils.Console;
  * 사용자로부터 입력을 받는 역할을 한다.
  */
 public class InputView {
-    private static final String INPUT_SIZE_MESSAGE = "다리의 길이를 입력해주세요.\n";
+    private static final String INPUT_SIZE_MESSAGE = "다리의 길이를 입력해주세요.";
     private static final String INPUT_TYPE_ERROR_MESSAGE = "[ERROR] 입력타입이 잘못되었습니다.\n";
+    private static final String INPUT_MOVE_MESSAGE = "이동할 칸을 선택해주세요. (위: U, 아래: D)";
+    private static final String INPUT_MOVE_RANGE_ERROR_MESSAGE = "[ERROR] 이동은 U 또는 D로만 입력할 수 있습니다.\n";
+
+
 
     /**
      * 다리의 길이를 입력받는다.
@@ -35,7 +39,21 @@ public class InputView {
      * 사용자가 이동할 칸을 입력받는다.
      */
     public String readMoving() {
-        return null;
+        try {
+            System.out.println(INPUT_MOVE_MESSAGE);
+            String upDown = Console.readLine();
+            isValidateMove(upDown);
+            return upDown;
+        } catch (IllegalArgumentException e) {
+            OutputView.printException(e);
+            return readMoving();
+        }
+    }
+
+    private static void isValidateMove(String input) {
+        if (!(input.equals("U") || input.equals("D"))) {
+            throw new IllegalArgumentException(INPUT_MOVE_RANGE_ERROR_MESSAGE);
+        }
     }
 
     /**
