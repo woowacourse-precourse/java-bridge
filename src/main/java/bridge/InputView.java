c6a683ec64166eb6c34b759f335289f3a73ffdb7
jhsseonn <hosu0125@gmail.com>
2022-11-21 15:18:12 +0900

    feat: 다리 길이 입력받는 함수 추가

--- src/main/java/bridge/InputView.java
+++ src/main/java/bridge/InputView.java
@@ -1,15 +1,21 @@
 package bridge;
 
+import bridge.config.InputConfig;
+import camp.nextstep.edu.missionutils.Console;
+
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
  */
-public class InputView {
+public class InputView extends InputConfig {
 
     /**
      * 다리의 길이를 입력받는다.
      */
     public int readBridgeSize() {
-        return 0;
+        System.out.println(START_BRIDGE_GAME+"\n");
+        System.out.println(INPUT_BRIDGE_LENGTH);
+
+        return Integer.parseInt(Console.readLine());
     }
 
     /**
