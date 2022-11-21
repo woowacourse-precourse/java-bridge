2d12eda60401d9189b341b13918d24b1c4ec63df
rjsah09 <83952174+rjsah09@users.noreply.github.com>
2022-11-22 02:06:06 +0900

    refactor: OutputView.printCrossedLine() 수정, OutputView.printMap() 수정

--- src/main/java/bridge/OutputView.java
+++ src/main/java/bridge/OutputView.java
@@ -14,11 +14,10 @@ public class OutputView {
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
     public void printMap(List<String> bridge, List<String> crossed) {
-        printCrossedLine("U", bridge, crossed);
-        printCrossedLine("D", bridge, crossed);
+        System.out.println(getCrossedLine("U", bridge, crossed) + "\n" + getCrossedLine("D", bridge, crossed));
     }
 
-    public void printCrossedLine(String lineName, List<String> bridge, List<String> crossed) {
+    public String getCrossedLine(String lineName, List<String> bridge, List<String> crossed) {
         String line = "[";
         for (int i = 0; i < crossed.size(); i++) {
             line += getStageStatus(lineName, bridge.get(i), crossed.get(i));
@@ -27,7 +26,7 @@ public class OutputView {
             }
         }
         line += "]";
-        System.out.println(line);
+        return line;
     }
 
     public String getStageStatus(String lineName, String bridgeElement, String input) {
---
+++ src/test/java/bridge/OutputViewTest.java
@@ -0,0 +1,17 @@
+package bridge;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class OutputViewTest {
+
+    @Test
+    void 다리_현황_출력_테스트() {
+        List<String> bridge = Arrays.asList("U", "U", "D", "U", "D", "D", "U");
+        List<String> crossed = Arrays.asList("U", "U", "D", "U", "D", "U");
+        String lineName = "U";
+
+    }
+}
