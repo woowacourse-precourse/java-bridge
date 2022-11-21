f5380442482660997aea0f5ed17583ddc291101e
qkrdmswl <eunji303@naver.com>
2022-11-21 16:40:44 +0900

    feat(BridgePrintTool): store string variables to make print list

--- src/main/java/utils/BridgePrintTool.java
+++ src/main/java/utils/BridgePrintTool.java
@@ -1,9 +1,11 @@
 package utils;
 
 public class BridgePrintTool {
-    public static final String MATCH = "O";
-    public static final String NOT_MATCH = "X";
+    public static final String MATCH = "O ";
+    public static final String NOT_MATCH = "X ";
     public static final String START = "[ ";
-    public static final String END = " ]";
-    public static final String BETWEEN = " | ";
+    public static final String END = "]";
+    public static final String BETWEEN = "| ";
+    public static final String NONE = "";
+    public static final String BLANK = "  ";
 }
