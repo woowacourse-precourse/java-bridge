64020db23b014026048e9a1fc3b89931ef6d3c45
delaylee <delaylee219@gmail.com>
2022-11-22 00:05:55 +0900

    feat: 게임 시작시 실행되는 메서드 구현 (BridgeGame#start)

--- src/main/java/bridge/BridgeGame.java
+++ src/main/java/bridge/BridgeGame.java
@@ -5,6 +5,17 @@ package bridge;
  */
 public class BridgeGame {
 
+
+    /**
+     * 게임 시작 메서드
+     */
+    public void start() {
+        int size = inputView.readBridgeSize();
+        bridge = bridgeMaker.makeBridge(size);
+        player = new ArrayList<>();
+        tryCount += 1;
+    }
+
     /**
      * 사용자가 칸을 이동할 때 사용하는 메서드
      * <p>
