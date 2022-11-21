2baaf996d5db3d06ae140771b8b89a3991aea105
yoonjuho <dbswn990@naver.com>
2022-11-21 17:53:38 +0900

    refactor(BridgeGame): 리스트의 객체 enum형으로 변경

--- src/main/java/bridge/model/BridgeGame.java
+++ src/main/java/bridge/model/BridgeGame.java
@@ -7,27 +7,35 @@ import java.util.List;
  * 다리 건너기 게임을 관리하는 클래스
  */
 public class BridgeGame {
-    private List<String> bridge;
-    public BridgeGame(){
+    private List<Column> bridge;
+
+    public BridgeGame() {
         bridge = new ArrayList<>();
     }
+
     /**
      * 사용자가 칸을 이동할 때 사용하는 메서드
      * <p>
      * 이동을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void start(int size){
+    public void start(int size) {
         BridgeMaker bridgeMaker = new BridgeMaker(new BridgeRandomNumberGenerator());
-        this.bridge = bridgeMaker.makeBridge(size);
-    }
-    public boolean move(int step, String input) {
-        if(isPass(bridge.get(step),input)){
-            return true;
+        List<String> capitalLettrs = bridgeMaker.makeBridge(size);
+        for (String capitalLetter : capitalLettrs) {
+            bridge.add(Column.valueOfCapitalLetter(capitalLetter));
         }
-        return false;
     }
-    public boolean isPass(String answer, String input){
-        if(answer.equals(input)){
+
+    public Column move(int step, String input) {
+        Column playerColumn = Column.valueOfCapitalLetter(input);
+        if (bridge.get(step) == playerColumn) {
+            return playerColumn;
+        }
+        return Column.NONE;
+    }
+
+    public boolean isPass(String answer, String input) {
+        if (answer.equals(input)) {
             return true;
         }
         return false;
