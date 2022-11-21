d0e9b222eca0f4e2be238c4ac4dbc16eba2b54f3
hgo641 <hgo641@gmail.com>
2022-11-22 04:39:55 +0900

    Feat: RoundResult의 description을 출력하는 기능 추가

--- src/main/java/bridge/domain/RoundResult.java
+++ src/main/java/bridge/domain/RoundResult.java
@@ -1,16 +1,28 @@
 package bridge.domain;
 
 public enum RoundResult {
-    PLAYING, FAIL, CLEAR;
+    PLAYING("진행중"),
+    FAIL("실패"),
+    CLEAR("성공");
 
-    public static RoundResult of(MoveResult moveResult, boolean isClear){
-        if(isClear){
+    final String description;
+
+    RoundResult(String description) {
+        this.description = description;
+    }
+
+    public static RoundResult of(MoveResult moveResult, boolean isClear) {
+        if (isClear) {
             return CLEAR;
         }
-        if(moveResult.equals(MoveResult.FAIL)){
+        if (moveResult.equals(MoveResult.FAIL)) {
             return FAIL;
         }
         return PLAYING;
+    }
+
+    public String getDescription() {
+        return this.description;
     }
 
 }
