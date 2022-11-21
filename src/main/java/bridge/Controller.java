bcea4a1f5e2eb01bfd6af9e64b1eafe72e8bb449
zzun <736300@naver.com>
2022-11-21 22:53:55 +0900

    feat(Controller): 게임을 진행해주는 start 메서드 구현

--- src/main/java/bridge/Controller.java
+++ src/main/java/bridge/Controller.java
@@ -1,8 +1,25 @@
 package bridge;
 
-public class Application {
+import java.util.List;
 
-    public static void main(String[] args) {
-        // TODO: 프로그램 구현
+public class Controller {
+
+    public void start() {
+        InputView inputView = new InputView();
+        int bridgeSize = inputView.readBridgeSize();
+
+        //다리 생성 실패시
+        if (bridgeSize == -1) {
+            return;
+        }
+        BridgeRandomNumberGenerator bridgeRandomNumberGenerator = new BridgeRandomNumberGenerator();
+        BridgeMaker bridgeMaker = new BridgeMaker(bridgeRandomNumberGenerator);
+
+        List<String> bridge;
+        bridge = bridgeMaker.makeBridge(bridgeSize);
+        String[][] bridgeBoard;
+        GameManagement gameManagement = new GameManagement();
+        bridgeBoard = gameManagement.settingBridge(bridge, bridgeSize);
+        gameManagement.gameProgress(bridgeBoard, bridgeSize);
     }
 }
