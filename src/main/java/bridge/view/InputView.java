6a7090ffd5669154e6d33f9f8f05facb96a47232
이정우 <acg6138@naver.com>
2022-11-21 18:23:20 +0900

    refactor(InputView): 출력 형식에 맞게 수정

--- src/main/java/bridge/view/InputView.java
+++ src/main/java/bridge/view/InputView.java
@@ -2,24 +2,30 @@ package bridge.view;
 
 import camp.nextstep.edu.missionutils.Console;
 
-/**
- * 사용자로부터 입력을 받는 역할을 한다.
- */
 public class InputView {
+    private static final String GAME_START_MESSAGE = "다리 건너기 게임을 시작합니다.";
+    private static final String BRIDGE_SIZE_INPUT_MESSAGE = System.lineSeparator() + "다리의 길이를 입력해주세요.";
+    private static final String MOVE_INPUT_MESSAGE = "이동할 칸을 선택해주세요. (위: U, 아래: D)";
+    private static final String RETRY_INPUT_MESSAGE = System.lineSeparator() + "게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)";
 
     public int readBridgeSize() {
+        System.out.println(GAME_START_MESSAGE);
+        System.out.println(BRIDGE_SIZE_INPUT_MESSAGE);
         String input = Console.readLine();
         validateBridgeSize(input);
+        System.out.println();
         return Integer.parseInt(input);
     }
 
     public String readMoving() {
+        System.out.println(MOVE_INPUT_MESSAGE);
         String input = Console.readLine();
         validateMoveInput(input);
         return input;
     }
 
     public String readGameCommand() {
+        System.out.println(RETRY_INPUT_MESSAGE);
         String input = Console.readLine();
         validateCommand(input);
         return input;
