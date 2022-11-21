702bfeb27fd4f9520ba86197f26616b8cf8c7556
qudtjs0753 <qudtjs0753@naver.com>
2022-11-21 18:54:42 +0900

    fix: ErrorMessage 생성 로직 수정
    StringBuilder로 메세지를 생성했었는데 이렇게하면 flush하지 않으면 메세지가 계속 축적되는 문제가 발생하고, 성능상으로도 비효율적이므로 처음 enum값에 생성할 때 이어 붙이는 것으로 수정한다.

--- src/main/java/bridge/constant/ErrorMessage.java
+++ src/main/java/bridge/constant/ErrorMessage.java
@@ -4,29 +4,31 @@ public enum ErrorMessage {
 
     NOTICE("[ERROR] "),
     SIZE_OF_BRIDGE(
-            String.format("다리의 길이는 %d이상 %d이하로 입력하셔야 합니다.",
+            String.format(NOTICE.message + "다리의 길이는 %d이상 %d이하로 입력하셔야 합니다.",
                     BridgeRule.MINIMUM_SIZE.getSize(),
                     BridgeRule.MAXIMUM_SIZE.getSize())
     ),
-    POSITION_IN_FORM_OF_NUMBER("다리 생성 도중 오류가 발생했습니다."),
+    POSITION_IN_FORM_OF_NUMBER(NOTICE.message + "다리 생성 도중 오류가 발생했습니다."),
     NOT_NUMBER(
+            NOTICE.message +
             String.format(
                     "%d이상, %d이하의 \"정수\"만 입력하셔야 합니다.",
                     Integer.MIN_VALUE,
                     Integer.MAX_VALUE
             )
     ),
-    MOVE_COMMAND("U(위 칸), D(아래 칸) 중 하나만 입력하셔야 합니다.")
-    ;
+    MOVE_COMMAND(
+            NOTICE.message +
+                    "U(위 칸), D(아래 칸) 중 하나만 입력하셔야 합니다."
+    );
 
     private final String message;
-    private final StringBuilder sb = new StringBuilder();
 
     ErrorMessage(String message) {
         this.message = message;
     }
 
     public String getMessage() {
-        return sb.append(NOTICE.message).append(this.message).toString();
+        return message;
     }
 }
