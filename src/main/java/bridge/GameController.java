956426f8f3cbb58fb601ceadef97ebc6bc383e4d
bearjun05 <bearjun05@naver.com>
2022-11-22 05:15:15 +0900

    feat : 게임 준비 메시지, 다리 사이즈 입력 기능 구현 & 이동할 칸 입력, 생성된 맵 출력 기능이 포함된 메인 게임 진행 기능 구현

--- src/main/java/bridge/GameController.java
+++ src/main/java/bridge/GameController.java
@@ -3,40 +3,40 @@ package bridge;
 import bridge.view.InputView;
 import bridge.view.OutputView;
 import java.util.List;
+import java.util.Stack;
 
 public class GameController {
-
     private final InputView inputView = new InputView();
     private final OutputView outputView = new OutputView();
     private final BridgeGame bridgeGame;
-    private final BridgeNumberGenerator bridgeNumberGenerator = new BridgeRandomNumberGenerator();
-    private final BridgeMaker bridgeMaker = new BridgeMaker(bridgeNumberGenerator);
+    private final BridgeMaker bridgeMaker;
 
     GameController() {
-        int bridgeSize = start();
+        bridgeMaker = new BridgeMaker(new BridgeRandomNumberGenerator());
+        int bridgeSize = ready();
         List<String> bridge = bridgeMaker.makeBridge(bridgeSize);
         bridgeGame = new BridgeGame(bridge);
         run();
     }
+
+    private int ready() {
+        outputView.printStart();
+        return inputView.readBridgeSize();
+    }
+
     private void run() {
-        boolean isRetry = true;
-        while(isRetry) {
-            play();
-            isRetry = bridgeGame.retry();
-        }
+        play();
     }
 
     private void play() {
         boolean moveResult = true;
-        while(moveResult) {
-            outputView.printPlay();
-            String movingValue = inputView.readMoving();
-            moveResult =bridgeGame.move(movingValue);
+        outputView.printPlay();
+        String movingValue = inputView.readMoving();
+        moveResult = bridgeGame.isCorrect(movingValue);
+        List<Stack> stairs = bridgeGame.move(movingValue, moveResult);
+        outputView.printStairs(stairs);
+        if(moveResult && !bridgeGame.isFinishWithAllCollect()) {
+           play();
         }
-    }
-
-    private int start() {
-        outputView.printStart();
-        return inputView.readBridgeSize();
     }
 }
