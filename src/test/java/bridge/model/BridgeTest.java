d3d370eab90f4c9452db928b5b1df05367a23152
Sehbeom <j3beom@gmail.com>
2022-11-22 03:02:08 +0900

    refactor(test): 다리 모델 테스트 리팩토링
    static 필드 활용으로 인한 테스트 순서 설정 제거
    Field 객체를 활용한 static 필드값 변경 과정으로 대체
    다리 모델 예외처리 기능 테스트 추가

--- src/test/java/bridge/model/BridgeTest.java
+++ src/test/java/bridge/model/BridgeTest.java
@@ -6,17 +6,15 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import bridge.constants.Direction;
 import bridge.constants.MovingPossibility;
 import bridge.domain.model.Bridge;
+import java.lang.reflect.Field;
 import java.util.ArrayList;
 import java.util.List;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.MethodOrderer;
-import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.TestMethodOrder;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
-@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 public class BridgeTest {
 
     List<String> testBridge = new ArrayList<>(List.of("U", "D", "D"));
@@ -25,27 +23,37 @@ public class BridgeTest {
         Bridge.setBridge(testBridge);
     }
 
-    @DisplayName("다리 초기화 - 다리는 U와 D로만 구성되어야 한다.")
+    @BeforeEach
+    void resetBridgeFields() throws NoSuchFieldException, IllegalAccessException {
+        resetBridge();
+        resetBridgeCreated();
+    }
+
+    void resetBridge() throws NoSuchFieldException, IllegalAccessException {
+        Bridge bridgeInstance = new Bridge();
+        Field bridge = Bridge.class.getDeclaredField("bridge");
+
+        bridge.setAccessible(true);
+        bridge.set(bridgeInstance, new ArrayList<>());
+        bridge.setAccessible(false);
+    }
+
+    void resetBridgeCreated() throws NoSuchFieldException, IllegalAccessException {
+        Bridge bridgeInstance = new Bridge();
+        Field bridgeCreated = Bridge.class.getDeclaredField("bridgeCreated");
+
+        bridgeCreated.setAccessible(true);
+        bridgeCreated.set(bridgeInstance, false);
+        bridgeCreated.setAccessible(false);
+    }
+
+    @DisplayName("다리 초기화 - U와 D로만 구성되어 있지 않은 경우, 예외가 발생한다.")
     @Test
-    @Order(1)
     void bridgeInitializeWithNoneUOrDTest() {
-        // 예외 처리 구현 과정에서 개발 예정
-    }
+        List<String> notOnlyUOrD = new ArrayList<>(List.of("A", "D", "U"));
 
-    @DisplayName("다리 초기화 - 다리의 길이는 3~20 사이여야 한다.")
-    @Test
-    @Order(2)
-    void bridgeInitializeWithOutOfRangeLengthTest() {
-        // 예외 처리 구현 과정에서 개발 예정
-    }
-
-    @DisplayName("건널 수 있는지 확인 기능 - 아직 초기화 되지 않은 다리에 대해 메소드를 호출할 경우, 예외가 발생한다.")
-    @ParameterizedTest
-    @ValueSource(ints = 0)
-    @Order(3)
-    void canCrossNotInitializedExceptionTest(int space) {
         assertThatThrownBy(() -> {
-            Bridge.checkMovingPossibility(space, Direction.UP);
+            Bridge.setBridge(notOnlyUOrD);
         })
                 .isInstanceOf(IllegalStateException.class)
                 .hasMessageContaining("[ERROR]");
@@ -53,7 +61,6 @@ public class BridgeTest {
 
     @DisplayName("다리 초기화 - 다리는 한번만 초기화 될 수 있으며, 초기화 되었는지 여부를 반환한다.")
     @Test
-    @Order(4)
     void bridgeInitializeOnceWithTrueReturnTest() {
         assertThat(Bridge.setBridge(testBridge)).isTrue();
 
@@ -64,7 +71,6 @@ public class BridgeTest {
     @DisplayName("건널 수 있는지 확인 기능 - 건널 수 있는 위치와 문자가 전달되면 CAN_MOVE 를 반환한다.")
     @ParameterizedTest
     @ValueSource(ints = 0)
-    @Order(6)
     void canMovePossibleTest(int space) {
         initializeBridge();
 
@@ -75,12 +81,43 @@ public class BridgeTest {
     @DisplayName("건널 수 있는지 확인 기능 - 건널 수 없는 위치와 문자가 전달되면 CAN_NOT_MOVE 를 반환한다.")
     @ParameterizedTest
     @ValueSource(ints = 1)
-    @Order(7)
     void canMoveImpossibleTest(int space) {
         initializeBridge();
 
         assertThat(Bridge.checkMovingPossibility(space, Direction.UP)).isEqualTo(
                 MovingPossibility.CAN_NOT_MOVE);
+    }
+
+    @DisplayName("건널 수 있는지 확인 기능 - 아직 초기화 되지 않은 다리에 대해 메소드를 호출할 경우, 예외가 발생한다.")
+    @ParameterizedTest
+    @ValueSource(ints = 0)
+    void checkMovingPossibilityNotInitializedExceptionTest(int space) {
+        checkMovingPossibilityExceptionTest(space);
+    }
+
+    @DisplayName("건널 수 있는지 확인 기능 - 마지막 인덱스보다 큰 값이나 0보다 작은 값이 전달될 경우, 예외가 발생한다.")
+    @ParameterizedTest
+    @ValueSource(ints = {-1, 3})
+    void checkMovingPossibilityOutOfRangeExceptionTest(int space) {
+        checkMovingPossibilityExceptionTest(space);
+    }
+
+    void checkMovingPossibilityExceptionTest(int space) {
+        assertThatThrownBy(() -> {
+            Bridge.checkMovingPossibility(space, Direction.UP);
+        })
+                .isInstanceOf(IllegalStateException.class)
+                .hasMessageContaining("[ERROR]");
+    }
+
+    @DisplayName("다리 길이 반환 기능 - 아직 초기화 되지 않은 다리에 대해 메소드를 호출할 경우, 예외가 발생한다.")
+    @Test
+    void getBridgeSizeNotInitializedExceptionTest() {
+        assertThatThrownBy(() -> {
+            Bridge.getBridgeSize();
+        })
+                .isInstanceOf(IllegalStateException.class)
+                .hasMessageContaining("[ERROR]");
     }
 
     @DisplayName("다리 길이 반환 기능 - 다리의 길이를 반환한다.")
