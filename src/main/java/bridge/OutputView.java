39ec94fbf59bd8226f868aa425fae90dd796e35c
BaeDongHyeon <sasd730@gmail.com>
2022-11-22 00:59:47 +0900

    refactor: 출력 형태 수정

--- src/main/java/bridge/OutputView.java
+++ src/main/java/bridge/OutputView.java
@@ -45,7 +45,6 @@ public class OutputView {
     public void printResult(BridgeResult bridgeResult, ResultSymbol resultSymbol, int gameTryCount) {
         System.out.println("최종 게임 결과");
         printMap(bridgeResult);
-        printNewLine();
         System.out.printf("게임 성공 여부: %s\n", resultSymbol.getStatus());
         System.out.printf("총 시도한 횟수: %d", gameTryCount);
     }
