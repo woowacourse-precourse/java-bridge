e38fe32e205bbfa81c01c7ac39c15cd05e078332
saehim <saeilee.vxv@gmail.com>
2022-11-22 01:01:36 +0900

    fix (view) : 출력 메시지 오타 수정

--- src/main/java/view/ViewMessage.java
+++ src/main/java/view/ViewMessage.java
@@ -2,7 +2,7 @@ package view;
 
 public class ViewMessage {
 
-    final String START_MESSAGE = "다리 건너기 게임을 시작합니다.\n";
+    public static final String START_MESSAGE = "다리 건너기 게임을 시작합니다.\n";
     static final String ASK_BRIDGE_LONG = "다리의 길이를 입력해주세요.";
     static final String ASK_MOVE_BRIDGE = "이동할 칸을 선택해주세요. (위: U, 아래: D)\n";
     static final String ASK_RETRY = "게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)\n";
