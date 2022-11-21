15109de4d93c07f3ea0e9c70b3b243901da56542
이성준 <75744297+castlejun-2@users.noreply.github.com>
2022-11-21 23:23:30 +0900

    refactor: 사용자 객체로 함수 기능 리팩토링

--- src/main/java/bridge/BridgeGame.java
+++ src/main/java/bridge/BridgeGame.java
@@ -9,14 +9,14 @@ import java.util.ArrayList;
 public class BridgeGame {
     private int tryCount = 1;
     private List<String> answer;
-    private List<String> user;
     private int bridgeLength;
     private final BridgeNumberGenerator bridgeNumberGenerator = new BridgeRandomNumberGenerator();
+    private User user;
 
     public BridgeGame(int bridgeLength) {
         this.bridgeLength = bridgeLength;
         this.answer = new BridgeMaker(bridgeNumberGenerator).makeBridge(bridgeLength);
-        this.user = new ArrayList<>(bridgeLength);
+        this.user = new User(answer, bridgeLength);
     }
 
     /**
@@ -25,9 +25,7 @@ public class BridgeGame {
      * 이동을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
     public void move() {
-        String direction = new InputView().readMoving();
-        user.add(direction);
-        new OutputView().printMap(answer,user);
+        user.userMove();
     }
 
     /**
@@ -36,8 +34,7 @@ public class BridgeGame {
      * 재시작을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
     public boolean retry() {
-        String reGame = new InputView().readGameCommand();
-        if (reGame.equals("R")) {
+        if (user.reTryGame()) {
             setRegame();
             return true;
         }
@@ -51,23 +48,20 @@ public class BridgeGame {
     }
 
     public boolean isFail() {
-        if (user.size() > 0 && !answer.get(user.size()-1).equals(user.get(user.size()-1))) {
+        if (user.isFailure())
             if (!retry())
                 return true;
-        }
         return false;
     }
 
     public boolean isClear() {
-        if (user.size() == bridgeLength) {
-            new OutputView().printResult(answer, user, tryCount);
+        if (user.isSuccess(tryCount))
             return true;
-        }
         return false;
     }
 
     public void setRegame() {
         tryCount++;
-        this.user = new ArrayList<>(bridgeLength);
+        this.user = new User(answer, bridgeLength);
     }
 }
