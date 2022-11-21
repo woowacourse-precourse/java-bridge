e9a8006fe3de4419009b7e9c3be0d1305e7019f8
endlessmomo <soohyuk96@naver.com>
2022-11-21 13:26:12 +0900

    feat(Move) : Move 객체 생성

---
+++ src/main/java/bridge/domain/validation/Move.java
@@ -0,0 +1,51 @@
+package bridge.domain.validation;
+
+import java.util.Arrays;
+
+public enum Move {
+    MOVE_UP("U", 1),
+    MOVE_DOWN("D", 0);
+
+    private final String moveKey;
+    private final int moveValue;
+
+    Move(String moveKey, int moveValue) {
+        this.moveKey = moveKey;
+        this.moveValue = moveValue;
+    }
+
+    // 사용자로부터 입력
+    public static Move of(String input) {
+        validate(input);
+
+        if (MOVE_UP.moveKey.equals(input)) {
+            return MOVE_UP;
+        }
+        return MOVE_DOWN;
+    }
+
+    public static Move of(int randomValue){
+        if(MOVE_UP.moveValue == randomValue){
+            return MOVE_UP;
+        }
+        return MOVE_DOWN;
+    }
+
+    private static void validate(String input) {
+        checkUppercase(input);
+        checkCorretKey(input);
+    }
+
+    private static void checkUppercase(String input) {
+        if (!input.equals(input.toUpperCase())) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private static void checkCorretKey(String input){
+        if (Arrays.stream(Move.values()).noneMatch(e -> e.moveKey.equals(input))) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+}
