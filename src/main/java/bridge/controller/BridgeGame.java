4dd6e35d74c1cd05f53008e948e5be0d0ad97331
khyojun <38220795+khyojun@users.noreply.github.com>
2022-11-21 12:25:32 +0900

    refactor(bridge): 불필요한 값들 삭제

--- src/main/java/bridge/controller/BridgeGame.java
+++ src/main/java/bridge/controller/BridgeGame.java
@@ -11,7 +11,7 @@ import java.util.List;
  * 다리 건너기 게임을 관리하는 클래스
  */
 public class BridgeGame {
-    
+
     private BridgeProcess bridgeProcess = new BridgeProcess();
     private BridgeDestination bridgeDestination = new BridgeDestination();
     private int gameCount = 1;
