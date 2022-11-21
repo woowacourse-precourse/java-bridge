bbe068013f964f2047046683e43a1983ae416e15
김산 <kimsan@gimsan-ui-MacBookAir.local>
2022-11-22 04:46:42 +0900

    feat (Application) : 게임시작해 다리만들어 반환하는 함수 구현

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -1,14 +1,24 @@
 package bridge;
 
+import static bridge.Constant.*;
+
 import bridge.BridgeGame.MoveResult;
+import java.util.List;
 
 public class Application {
 
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        BridgeGame bridgeGame = startGame();
+    }
+
+    public static BridgeGame startGame() {
+        System.out.println(MESSAGE_GAME_START);
+        System.out.println(MESSAGE_ENTER_LENGTH);
         InputView inputView = new InputView();
-        System.out.println(inputView.readBridgeSize());
-        System.out.println(inputView.readMoving());
-        System.out.println(inputView.readGameCommand());
+        int length = inputView.readBridgeSize();
+        BridgeMaker bridgeMaker = new BridgeMaker(new BridgeRandomNumberGenerator());
+        List<String> bridge = bridgeMaker.makeBridge(length);
+        return new BridgeGame(bridge);
     }
 }
