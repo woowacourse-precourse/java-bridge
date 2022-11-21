7d36b6e9615f5aaef88366ed012707044adce3a3
Identityless <rlandwnd555@naver.com>
2022-11-22 01:39:17 +0900

    refactor : BridgeGame에서 InputView, OutputView 객체 이용하는로직 Application 클래스로 옮김

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -1,18 +1,66 @@
 package bridge;
 
+import java.util.List;
+
 public class Application {
 
-    private static BridgeGame bridgeGame;
+    private static InputView inputView = new InputView();
+    private static OutputView outputView = new OutputView();
+    private static int bridgesize;
+    private static BridgeMaker bridgeMaker = new BridgeMaker(new BridgeRandomNumberGenerator());
+
+    private static BridgeGame bridgeGame = new BridgeGame();
+    private static int idx;
     private static boolean flag;
+    private static int count;
 
     public static void main(String[] args) {
-        bridgeGame = new BridgeGame(new InputView(), new OutputView(), new BridgeMaker(new BridgeRandomNumberGenerator()));
+        startGame();
         do {
-            bridgeGame.init();
-            if(bridgeGame.move()){
-                break;
-            }
-            flag = bridgeGame.retry();
-        } while(flag);
+            doGame();
+        } while(flag && idx < bridgesize);
+        printResult();
+    }
+
+    private static void startGame() {
+        bridgeGame.init();
+        System.out.println("다리 건너기 게임을 시작합니다.\n");
+        System.out.println("다리의 길이를 입력해주세요.");
+        bridgesize = inputView.readBridgeSize();
+        bridgeGame.setRandomMap(bridgeMaker.makeBridge(bridgesize));
+        flag = true;
+        idx = 0;
+        count = 0;
+    }
+
+    private static void doGame() {
+        count++;
+        callMove();
+        if(!flag){
+            retry();
+        }
+    }
+
+    private static void callMove() {
+        while(flag && idx < bridgesize) {
+            System.out.println("이동할 칸을 선택해주세요. (위: U, 아래: D)");
+            flag = bridgeGame.move(inputView.readMoving());
+            outputView.printMap(List.of(bridgeGame.getUpMap(), bridgeGame.getDownMap()));
+            idx++;
+        }
+    }
+
+    public static void retry() {
+        boolean re;
+        System.out.println("게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)");
+        re = inputView.readGameCommand();
+        if(re) {
+            bridgeGame.retry();
+            idx = 0;
+            flag = true;
+        }
+    }
+    private static void printResult() {
+            outputView.printResult(flag, List.of(bridgeGame.getUpMap(), bridgeGame.getDownMap()), count);
     }
 }
--- src/main/java/bridge/BridgeGame.java
+++ src/main/java/bridge/BridgeGame.java
@@ -8,53 +8,19 @@ import java.util.List;
  */
 public class BridgeGame {
 
-    private InputView inputView;
-    private OutputView outputView;
-    private BridgeMaker bridgeMaker;
     private List<String> UpMap;
     private List<String> DownMap;
     private List<String> randomMap;
-    private int bridgesize;
-    private boolean flag;
-    private String move;
-    private int count;
     private int idx;
+    private boolean flag;
 
-    public BridgeGame(InputView inputView, OutputView outputView, BridgeMaker bridgeMaker) {
-        this.inputView = inputView;
-        this.outputView = outputView;
-        this.bridgeMaker = bridgeMaker;
-        this.bridgesize = 0;
-        this.flag = false;
-        this.count = 0;
-    }
-
-    /**
-     * 사용자가 칸을 이동할 때 사용하는 메서드
-     * <p>
-     * 이동을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
-     */
-    public boolean move() {
-        while(flag && idx < randomMap.size()) {
-            System.out.println("이동할 칸을 선택해주세요. (위: U, 아래: D)");
-            move = inputView.readMoving();
-            buildUserMap();
-            outputView.printMap(List.of(UpMap, DownMap));
+    public boolean move(String move) {
+            buildUserMap(move);
             idx++;
-        }
-        return isSuccess();
+        return flag;
     }
 
-    private boolean isSuccess() {
-        count++;
-        if(flag) {
-            outputView.printResult(flag, List.of(UpMap, DownMap), count);
-            return true;
-        }
-        return false;
-    }
-
-    private void buildUserMap() {
+    private void buildUserMap(String move) {
         if(move.equals("U")) {
             validateMove("U", UpMap, DownMap);
         }
@@ -75,28 +41,27 @@ public class BridgeGame {
         list2.add(" ");
     }
 
-    /**
-     * 사용자가 게임을 다시 시도할 때 사용하는 메서드
-     * <p>
-     * 재시작을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
-     */
-    public boolean retry() {
-        boolean re;
-        System.out.println("게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)");
-        re = inputView.readGameCommand();
-        if(!re) {
-            outputView.printResult(false, List.of(UpMap, DownMap), count);
-        }
-        return re;
+    public void retry() {
+        init();
     }
 
     public void init() {
-        System.out.println("다리의 길이를 입력해주세요.");
-        bridgesize = inputView.readBridgeSize();
-        randomMap = bridgeMaker.makeBridge(bridgesize);
         UpMap = new ArrayList<>();
         DownMap = new ArrayList<>();
-        flag = true;
         idx = 0;
+        flag = true;
     }
+
+    public void setRandomMap(List<String> randomMap) {
+        this.randomMap = randomMap;
+    }
+
+    public List<String> getUpMap() {
+        return UpMap;
+    }
+
+    public List<String> getDownMap() {
+        return DownMap;
+    }
+
 }
