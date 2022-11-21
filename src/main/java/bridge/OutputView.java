96c6c8b92016c5e2a9dc25c1c27d0c884556bb5a
Hong Jin-Seop <jinsub1999@khu.ac.kr>
2022-11-21 14:55:22 +0900

    feat: A-2 최종 게임 결과를 출력하는 기능 구현

--- src/main/java/bridge/OutputView.java
+++ src/main/java/bridge/OutputView.java
@@ -1,5 +1,6 @@
 package bridge;
 
+import bridge.BridgeGame.STATE;
 import java.util.List;
 
 /**
@@ -37,11 +38,26 @@ public class OutputView {
         printOneLine(bridge, moves, "D");
     }
 
+    public String getStateString(BridgeGame game) {
+        String result = "";
+        if (game.getState() == STATE.FAILED) {
+            result = "실패";
+        }
+        if (game.getState() == STATE.SUCCESS) {
+            result = "성공";
+        }
+        return result;
+    }
+
     /**
      * 게임의 최종 결과를 정해진 형식에 맞춰 출력한다.
      * <p>
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void printResult() {
+    public void printResult(BridgeGame game, int retries) {
+        System.out.println("최종 게임 결과");
+        printMap(game.getBridge(), game.getMoves());
+        System.out.printf("게임 성공 여부: %s\n", getStateString(game));
+        System.out.printf("총 시도한 횟수: %d\n", retries);
     }
 }
