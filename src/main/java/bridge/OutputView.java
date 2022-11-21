5c6d542f7e31ff5572d78f0c2010f10886a89c75
wonju-dev <wonjulee.dev@gmail.com>
2022-11-22 03:41:43 +0900

    feat(OutputView): 분석 결과 객체 초기화 기능 구현

--- src/main/java/bridge/OutputView.java
+++ src/main/java/bridge/OutputView.java
@@ -1,5 +1,10 @@
 package bridge;
 
+import bridge.enums.Direction;
+
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * 사용자에게 게임 진행 상황과 결과를 출력하는 역할을 한다.
  */
@@ -23,6 +28,16 @@ public class OutputView {
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
     public void printMap() {
+    }
+
+    private List<List<String>> init() {
+        List<List<String>> layers = new ArrayList<>();
+
+        for (int i = 0; i < Direction.values().length; i++) {
+            layers.add(new ArrayList<>());
+        }
+
+        return layers;
     }
 
     /**
