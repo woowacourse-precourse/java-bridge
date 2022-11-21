8ce46cfe9eafeb49ba2a0283a61a2b0c83898409
lynne921 <dkfls921@gmail.com>
2022-11-22 06:28:28 +0900

    feat(BridgeGame): 다리 완주 시 상태를 성공으로 변경하는 기능 추가
    - 다리 완주 시 status를 SUCCESS로 변경하는 기능 추가
    - 이를 검증하는 테스트 코드 작성
    
    refactor(BridgeGameTest): 중복되는 선언 코드 분리

--- src/main/java/bridge/BridgeGame.java
+++ src/main/java/bridge/BridgeGame.java
@@ -2,6 +2,7 @@ package bridge;
 
 import java.util.ArrayList;
 import java.util.List;
+
 /**
  * 다리 건너기 게임을 관리하는 클래스
  */
@@ -15,6 +16,7 @@ public class BridgeGame {
         this.answerBridge = answerBridge;
         this.status = StatusType.PLAY;
     }
+
     /**
      * 사용자가 칸을 이동할 때 사용하는 메서드
      */
@@ -33,6 +35,12 @@ public class BridgeGame {
 
     public void quit() {
         status = StatusType.FAIL;
+    }
+
+    public void isSuccess() {
+        if (route.size() == answerBridge.size() && status.equals(StatusType.PLAY)) {
+            status = StatusType.SUCCESS;
+        }
     }
 
     private void ifFail(String move) {
--- src/test/java/bridge/BridgeGameTest.java
+++ src/test/java/bridge/BridgeGameTest.java
@@ -1,5 +1,6 @@
 package bridge;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -8,11 +9,19 @@ import java.util.List;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class BridgeGameTest {
+    BridgeGame bridgeGame;
+    List<String> answerBridge;
+
+    @BeforeEach
+    void init() {
+        answerBridge = List.of("U", "D", "U", "D");
+        bridgeGame = new BridgeGame(answerBridge);
+    }
+
     @DisplayName("틀렸을 때 FAIL이 되어야 합니다.")
     @Test
     void moveTestIfFail() {
-        List<String> answerBridge = List.of("U","D","U","D");
-        BridgeGame bridgeGame = new BridgeGame(answerBridge);
+        init();
         bridgeGame.move("D");
 
         assertThat(bridgeGame.getStatus()).isEqualTo(StatusType.FAIL);
@@ -21,8 +30,7 @@ public class BridgeGameTest {
     @DisplayName("재시도 시 route는 초기화되고, 상태는 PLAY로 변경되어야 합니다.")
     @Test
     void moveTestIfRetry() {
-        List<String> answerBridge = List.of("U","D","U","D");
-        BridgeGame bridgeGame = new BridgeGame(answerBridge);
+        init();
         bridgeGame.move("D");
         bridgeGame.retry();
 
@@ -33,10 +41,21 @@ public class BridgeGameTest {
     @DisplayName("종료 시 상태는 FAIL로 변경되어야 합니다.")
     @Test
     void moveTestIfQuit() {
-        List<String> answerBridge = List.of("U","D","U","D");
-        BridgeGame bridgeGame = new BridgeGame(answerBridge);
+        init();
         bridgeGame.quit();
 
         assertThat(bridgeGame.getStatus()).isEqualTo(StatusType.FAIL);
+    }
+
+    @DisplayName("완주 시 상태는 SUCCESS로 변경되어야 합니다.")
+    @Test
+    void moveTestIfSuccess() {
+        init();
+        for (String s : answerBridge) {
+            bridgeGame.move(s);
+        }
+        bridgeGame.isSuccess();
+
+        assertThat(bridgeGame.getStatus()).isEqualTo(StatusType.SUCCESS);
     }
 }
