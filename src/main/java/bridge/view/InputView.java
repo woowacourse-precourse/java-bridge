7e4998a69e5aa528ed6269689ddaad05b268bd25
baekeunsun <eunsun5460@gmail.com>
2022-11-21 18:09:10 +0900

    feat(view/InputView): "다리 건너기 게임을 시작합니다" 출력
    - (InputView/readBridgeSize): "다리 건너기 게임을 시작합니다\n" 추가 출력

--- src/main/java/bridge/view/InputView.java
+++ src/main/java/bridge/view/InputView.java
@@ -8,6 +8,7 @@ public class InputView {
      * 사용자로부터 입력을 받는 역할을 한다.
      */
     public static int readBridgeSize() {
+        System.out.println("다리 건너기 게임을 시작합니다.\n");
         System.out.println("다리의 길이를 입력해주세요.");
         int input = Integer.valueOf(readLine());
         return input;
