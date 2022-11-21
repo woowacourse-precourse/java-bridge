4cc72ecffcae327d5edb5fd6af8d530502687a48
tjddnr7760 <tjddnr7760@naver.com>
2022-11-21 18:58:02 +0900

    feat : BridgeMaker 클래스에서 랜덤값 받아서 정답 브리지 리턴 구현

--- src/main/java/bridge/BridgeMaker.java
+++ src/main/java/bridge/BridgeMaker.java
@@ -1,6 +1,8 @@
 package bridge;
 
 import java.util.List;
+import java.util.ArrayList;
+import java.lang.StringBuilder;
 
 /**
  * 다리의 길이를 입력 받아서 다리를 생성해주는 역할을 한다.
@@ -18,6 +20,62 @@ public class BridgeMaker {
      * @return 입력받은 길이에 해당하는 다리 모양. 위 칸이면 "U", 아래 칸이면 "D"로 표현해야 한다.
      */
     public List<String> makeBridge(int size) {
-        return null;
+        ArrayList<Integer> randomnumbers = new ArrayList<>();
+        StringBuilder sb = new StringBuilder("[");
+
+        for (int loaction = 1; loaction <= size; loaction++) {
+            int randomnumber = bridgeNumberGenerator.generate();
+            randomnumbers.add(randomnumber);
+        }
+
+        sb = setFirstLine(randomnumbers, sb, size);
+        sb = setMiddleLine(sb);
+        sb = setSecondLine(randomnumbers, sb, size);
+        sb = setLastLine(sb);
+
+        List result =  List.of(sb);
+        return result;
+    }
+
+    public StringBuilder setFirstLine(ArrayList<Integer> randomnumbers, StringBuilder sb, int size) {
+        for (int location = 0; location < size; location++) {
+
+            if (randomnumbers.get(location) == 1) {
+                sb.append(" U ");
+            }
+            if (randomnumbers.get(location) == 0) {
+                sb.append("   ");
+            }
+            sb.append("|");
+        }
+        return sb;
+    }
+
+    public StringBuilder setSecondLine(ArrayList<Integer> randomnumbers, StringBuilder sb, int size) {
+        for (int location = 0; location < size; location++) {
+            if (randomnumbers.get(location) == 0) {
+                sb.append(" D ");
+            }
+            if (randomnumbers.get(location) == 1) {
+                sb.append("   ");
+            }
+            sb.append("|");
+        }
+        return sb;
+    }
+
+    public StringBuilder setMiddleLine(StringBuilder sb) {
+        sb.deleteCharAt(sb.length()-1);
+        sb.append("]");
+        sb.append(System.lineSeparator());
+        sb.append("[");
+
+        return sb;
+    }
+
+    public StringBuilder setLastLine(StringBuilder sb) {
+        sb.deleteCharAt(sb.length()-1);
+        sb.append("]");
+        return sb;
     }
 }
