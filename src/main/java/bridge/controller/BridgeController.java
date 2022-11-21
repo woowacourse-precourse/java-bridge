816c24c6f0f9b23f726372659d851dbc3709a7c3
wwwndus <simock22@gmail.com>
2022-11-22 02:46:25 +0900

    feat(BridgeGame): 값에따라 다리를 이동하고 현재 다리를 출력

--- src/main/java/bridge/controller/BridgeController.java
+++ src/main/java/bridge/controller/BridgeController.java
@@ -1,9 +1,12 @@
 package bridge.controller;
 
 import bridge.BridgeRandomNumberGenerator;
+import bridge.model.BridgeGame;
 import bridge.model.BridgeMaker;
 import bridge.view.InputView;
+import bridge.view.OutputView;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class BridgeController {
@@ -11,6 +14,10 @@ public class BridgeController {
     private static final InputView inputView = new InputView();
     private static List<String> bridges;
     private static Boolean trueOrFalse = true;
+    private static int triedNumber = 0;
+    public static List<List<String>> upAndDown = new ArrayList<>();
+    private static BridgeGame bridgeGame = new BridgeGame();
+    private static final OutputView outputView = new OutputView();
 
     public void init() {
 
@@ -19,9 +26,8 @@ public class BridgeController {
 
         while (trueOrFalse) {
             String inputMoving = inputView.readMoving();
-//            getTriedNumber();
+            triedNumber = getTriedNumber(inputSize, inputMoving);
         }
-//        triedNumber = getTriedNumber(inputSize);
 //        printResult();
     }
 
@@ -32,6 +38,43 @@ public class BridgeController {
     private List<String> makeRandomBridges(int inputSize) {
         BridgeMaker bridgeMaker = new BridgeMaker(new BridgeRandomNumberGenerator());
         return bridgeMaker.makeBridge(inputSize);
+    }
+
+    private int getTriedNumber(int inputSize, String inputMoving) {
+        cleanAndAdd();
+
+        int lengthOfBridge = 0;
+        lengthOfBridge = startGame(inputSize, lengthOfBridge, inputMoving);
+
+        triedNumber++;
+
+        return triedNumber;
+    }
+
+    private void cleanAndAdd() {
+        List<String> upFloor = new ArrayList<>();
+        List<String> downFloor = new ArrayList<>();
+        upAndDown.add(upFloor);
+        upAndDown.add(downFloor);
+    }
+
+    private static int startGame(int inputSize, int lengthOfBridge, String inputMoving) {
+        while (lengthOfBridge < inputSize) {
+            lengthOfBridge = bridgeGame.move(bridges, lengthOfBridge, inputMoving);
+            List<String> toStringBridges = new ArrayList<>();
+            toAddforBridges(toStringBridges);
+            outputView.printMap(toStringBridges);
+            lengthOfBridge++;
+        }
+        return lengthOfBridge;
+    }
+
+    private static void toAddforBridges(List<String> toStringBridges) {
+        String upToString = String.join("", upAndDown.get(0).subList(0, upAndDown.get(0).size()-1));
+        String downToString = String.join("", upAndDown.get(1).subList(0, upAndDown.get(1).size()-1));
+
+        toStringBridges.add("[" + upToString + "]");
+        toStringBridges.add("[" + downToString + "]");
     }
 
 }
--- src/main/java/bridge/model/BridgeGame.java
+++ src/main/java/bridge/model/BridgeGame.java
@@ -1,22 +1,72 @@
 package bridge.model;
 
-/**
- * 다리 건너기 게임을 관리하는 클래스
- */
+import java.util.List;
+
+import static bridge.controller.BridgeController.upAndDown;
+
 public class BridgeGame {
-    /**
-     * 사용자가 칸을 이동할 때 사용하는 메서드
-     * <p>
-     * 이동을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
-     */
-    public void move() {
+
+    public int move(List<String> bridges, int count, String inputMoving) {
+
+        if (inputMoving.equals("U") && bridges.get(count).equals("U")) {
+            isUAndU();
+            return count;
+        }
+        return getCountNotUAndU(bridges, count, inputMoving);
     }
 
-    /**
-     * 사용자가 게임을 다시 시도할 때 사용하는 메서드
-     * <p>
-     * 재시작을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
-     */
+    private int getCountNotUAndU(List<String> bridges, int count, String inputMoving) {
+        if (inputMoving.equals("U") && bridges.get(count).equals("D")) {
+            isUAndD();
+            return 21;
+        }
+        return getCountNotUAndD(bridges, count, inputMoving);
+    }
+
+    private int getCountNotUAndD(List<String> bridges, int count, String inputMoving) {
+        if (inputMoving.equals("D") && bridges.get(count).equals("D")) {
+            isDAndD();
+        }
+        if (inputMoving.equals("D") && bridges.get(count).equals("U")) {
+            isDAndU();
+            count = 21;
+        }
+        return count;
+    }
+
+    private void isDAndU() {
+        upAndDown.get(0).add("   ");
+        upAndDown.get(0).add("|");
+        upAndDown.get(1).add(" X ");
+        upAndDown.get(1).add("|");
+    }
+
+    private void isDAndD() {
+        upAndDown.get(0).add("   ");
+        upAndDown.get(0).add("|");
+        upAndDown.get(1).add(" O ");
+        upAndDown.get(1).add("|");
+    }
+
+    private void isUAndD() {
+        upAndDown.get(0).add(" X ");
+        upAndDown.get(0).add("|");
+        upAndDown.get(1).add("   ");
+        upAndDown.get(1).add("|");
+    }
+
+    private void isUAndU() {
+        upAndDown.get(0).add(" O ");
+        upAndDown.get(0).add("|");
+        upAndDown.get(1).add("   ");
+        upAndDown.get(1).add("|");
+    }
+
+        /**
+         * 사용자가 게임을 다시 시도할 때 사용하는 메서드
+         * <p>
+         * 재시작을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
+         */
     public void retry() {
     }
 }
