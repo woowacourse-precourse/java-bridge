9edc3d9e95610ecf52c77beb277082551a9be7e6
wwwndus <simock22@gmail.com>
2022-11-22 02:13:39 +0900

    feat(InputView): 생성할 다리의 길이 입력 받음

--- src/main/java/bridge/controller/BridgeController.java
+++ src/main/java/bridge/controller/BridgeController.java
@@ -1,18 +1,22 @@
 package bridge.controller;
 
+import bridge.view.InputView;
+
 public class BridgeController {
+
+    private static final InputView inputView = new InputView();
 
     public void init() {
 
-//        int inputSize = getBridgeSize();
+        int inputSize = getBridgeSize();
 //        bridges = makeRandomBridges(inputSize);
 //        triedNumber = getTriedNumber(inputSize);
 //        printResult();
     }
 
-//    private int getBridgeSize() {
-//        return inputView.readBridgeSize();
-//    }
+    private int getBridgeSize() {
+        return inputView.readBridgeSize();
+    }
 
 }
 
--- src/main/java/bridge/view/InputView.java
+++ src/main/java/bridge/view/InputView.java
@@ -1,4 +1,7 @@
 package bridge.view;
+
+import camp.nextstep.edu.missionutils.Console;
+
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
  */
@@ -8,7 +11,12 @@ public class InputView {
      * 다리의 길이를 입력받는다.
      */
     public int readBridgeSize() {
-        return 0;
+        System.out.println("다리 건너기 게임을 시작합니다.\n");
+        System.out.println("다리의 길이를 입력해주세요.");
+
+        int inputSize = Integer.parseInt(Console.readLine());
+        System.out.println(" ");
+        return inputSize;
     }
 
     /**
