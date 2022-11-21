8e79a152c6395492029832c4f6c4753660a96795
Hongsugyeong <olivia258@naver.com>
2022-11-21 16:29:31 +0900

    (refactor) : main 메서드에서 에러 처리 완료

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -6,7 +6,11 @@ public class Application {
 
     public static void main(String[] args) {
         // TODO: 프로그램 구현
-        BridgeController bridgeController = new BridgeController();
-        bridgeController.start();
+        try {
+            BridgeController bridgeController = new BridgeController();
+            bridgeController.start();
+        }catch (IllegalArgumentException e){
+            System.out.println(e.getMessage());
+        }
     }
 }
