3ba4d25224dec1b4a7c74f6cd0879fe1d376a889
YE-EUN <musiclove529@naver.com>
2022-11-21 15:05:12 +0900

    feat(BridgeGame) : 생성자 구현

--- src/main/java/bridge/Controller/BridgeGame.java
+++ src/main/java/bridge/Controller/BridgeGame.java
@@ -1,9 +1,27 @@
 package bridge.Controller;
 
+import bridge.BridgeMaker;
+import bridge.BridgeRandomNumberGenerator;
+import bridge.Domain.Bridge;
+import bridge.Domain.Result;
+import bridge.View.InputView;
+import bridge.View.OutputView;
+
 /**
  * 다리 건너기 게임을 관리하는 클래스
  */
 public class BridgeGame {
+    private final Bridge bridge;
+    private final Result result;
+    private final InputView inputView;
+    private final OutputView outputView;
+
+    public BridgeGame(){
+        bridge = new Bridge(new BridgeMaker(new BridgeRandomNumberGenerator()));
+        result = new Result();
+        inputView = new InputView();
+        outputView = new OutputView();
+    }
 
     public void run(){
 
@@ -13,12 +31,21 @@ public class BridgeGame {
 
     }
 
+    public void crossBridge(){
+
+    }
+
     /**
      * 사용자가 칸을 이동할 때 사용하는 메서드
      * <p>
      * 이동을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
     public void move() {
+
+    }
+
+    public void selectRetry(){
+
     }
 
     /**
@@ -27,6 +54,7 @@ public class BridgeGame {
      * 재시작을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
     public void retry() {
+
     }
 
     public void showResult(){
