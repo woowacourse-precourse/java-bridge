5e8eda3eb47de069bd137e10206825052f76efac
JONGMIN SHIN <jongmin373@gmail.com>
2022-11-21 17:02:21 +0900

    Feat<GameStarter> : add GameInProgress method

--- src/main/java/bridge/GameStarter.java
+++ src/main/java/bridge/GameStarter.java
@@ -14,6 +14,32 @@ public class GameStarter {
         }
     }
 
+    // 게임 시작
+    public static boolean startGame(List<String> bridge, int totalCount) {
+        BridgeGame bridgeGame = new BridgeGame(bridge);
+        boolean continueGame = true;
+        if (gameInProgress(continueGame, bridgeGame)) {    // 재시도
+            return true;
+        }
+        // 최종 결과 보기
+        outputView.printResult(bridgeGame.getGameState(), bridgeGame.getIsSuccess(), totalCount);
+        // 게임이 종료
+        return false;
+    }
+
+    // 다리 생성 후 게임 진행 시작 메서드
+    public static boolean gameInProgress(boolean continueGame, BridgeGame bridgeGame) {
+        while (continueGame) {
+            continueGame = moveForward(bridgeGame);
+            // 게임이 끝났을 때 (실패OR성공) && 실패일 때
+            if (retryCheck(continueGame, bridgeGame)) {
+                return true;
+            }
+            outputView.printMap(bridgeGame.getGameState(), continueGame, bridgeGame.getIsSuccess());
+        }
+        return false;
+    }
+
     // 다리 생성 메서드
     public static List<String> makeBridge() {
         BridgeNumberGenerator bridgeNumberGenerator = new BridgeRandomNumberGenerator();
