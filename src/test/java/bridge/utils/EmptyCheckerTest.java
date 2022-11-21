6a7e230550fea9672901b68230d3a483a5c88ed9
miffyKing <whqudghk28@gmail.com>
2022-11-22 04:55:18 +0900

    test: Generate EmptyChecker test case to check if it gets valid null and blank input result.

--- src/test/java/bridge/utils/EmptyCheckerTest.java
+++ src/test/java/bridge/utils/EmptyCheckerTest.java
@@ -1,16 +1,15 @@
 package bridge.utils;
 
 import bridge.Application;
+
 import camp.nextstep.edu.missionutils.test.NsTest;
-import java.util.List;
-import org.junit.jupiter.api.DisplayName;
+
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.DisplayName;
+
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
-import static org.assertj.core.api.Assertions.assertThat;
 
-
-class EmptyCheckerTest  extends NsTest{
-    // 테스트가 빈 공백, "", null 등 다양한 빈 입력에 대한 처리를 할 수 있는지 확인.
+class EmptyCheckerTest extends NsTest {
 
     @DisplayName("빈 입력을 잘 처리하는가 - '   ' 경우 (공백)")
     @Test
