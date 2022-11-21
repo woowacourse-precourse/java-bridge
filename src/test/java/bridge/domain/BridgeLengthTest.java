cd352d0c4a2dd67697e0c72b5f43b3fb47233914
Isu Shin <isu010614@naver.com>
2022-11-21 16:55:44 +0900

    test: 숫자가 아닌 문자를 입력할 경우 오류 발생 후 메시지 출력하는지 확인하는 테스트 코드 구현

--- src/test/java/bridge/domain/BridgeLengthTest.java
+++ src/test/java/bridge/domain/BridgeLengthTest.java
@@ -2,6 +2,8 @@ package bridge.domain;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
@@ -15,7 +17,14 @@ public class BridgeLengthTest {
                 .hasMessage("[ERROR] 다시 입력해주세요.");
     }
 
-    // 숫자가 아닌 문자를 입력할 경우
-    // 3 이상 20 이하가 아닌 경우
+    @DisplayName("숫자가 아닌 문자를 입력할 경우")
+    @ValueSource(strings = {"U", "-", "-3"})
+    @ParameterizedTest
+    void validateNotNumeric(String inputLength) {
+        assertThatThrownBy(() -> BridgeLength.createBridgeLength(inputLength))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("[ERROR] 숫자만 입력해야 합니다.");
+    }
 
+    // 3 이상 20 이하가 아닌 경우
 }
