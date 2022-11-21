bd2d87d191f0e444d672fc7fea37764262200916
xxeol2 <jhj01177@naver.com>
2022-11-22 00:56:00 +0900

    test: 라운드마다 사용자에게 이동할 칸(U/D)을 입력받는다

---
+++ src/test/java/bridge/validator/MovingValidatorTest.java
@@ -0,0 +1,29 @@
+package bridge.validator;
+
+import static bridge.validator.MovingValidator.validateMoving;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import bridge.exception.WrongMovingException;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+class MovingValidatorTest {
+
+    @DisplayName("이동할 칸은 U또는 D이다.")
+    @ValueSource(strings = {"U", "D"})
+    @ParameterizedTest
+    void movingMustBeUorD(String input) {
+        assertThat(validateMoving(input)).isTrue();
+    }
+
+    @DisplayName("이동할 칸이 U나 D가 아닐경우 예외 처리한다.")
+    @ValueSource(strings = {"A", "B", "-U", "", " "})
+    @ParameterizedTest
+    void exceptWhenMovingNotUorD(String input) {
+        assertThatThrownBy(() -> validateMoving(input))
+                .isInstanceOf(WrongMovingException.class);
+    }
+
+}
