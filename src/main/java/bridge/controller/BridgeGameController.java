096d4aebae9c0b77f0b3df5c73f7fce4301278ca
Your Name <you@example.com>
2022-11-21 16:02:56 +0900

    refactor(bridge) : 다리 게임 실행 순서 관리 로직 메서드 분리

--- src/main/java/bridge/controller/BridgeGameController.java
+++ src/main/java/bridge/controller/BridgeGameController.java
@@ -5,10 +5,11 @@ import bridge.domain.BridgeGame;
 import bridge.ui.InputView;
 import bridge.ui.OutputView;
 
-import java.util.ArrayList;
 import java.util.List;
 
 public class BridgeGameController {
+
+    private static boolean GAME_START = false;
 
     private final BridgeGame bridgeGame;
     private final BridgeMaker bridgeMaker;
@@ -23,33 +24,64 @@ public class BridgeGameController {
     }
 
     public void play() {
+        try {
+            List<String> bridge = startGameMakeBridge();
+            findCorrectBridge(bridge, GAME_START);
+        } catch (RuntimeException e) {
+            System.out.println(e);
+        }
+    }
+
+    private void findCorrectBridge(List<String> bridge, boolean gameQuit) {
+        while (!gameQuit) {
+            List<String> moveResult = moveBridgeResult(bridge);
+            if (bridgeGame.isFailGame()) {
+                gameQuit = retryOrQuit(bridge, moveResult);
+                continue;
+            }
+            gameQuit = isGameQuitFinal(bridge, moveResult);
+        }
+    }
+
+    private boolean retryOrQuit(List<String> bridge, List<String> moveResult) {
+        outputView.printRetryOrQuitChoice();
+        String gameCommand = inputView.readGameCommand();
+        moveResult = bridgeGame.retry(gameCommand);
+        return isGameQuit(gameCommand, moveResult, bridge);
+    }
+
+    private boolean isGameQuitFinal(List<String> bridge, List<String> moveResult) {
+        if (!bridgeGame.isFailFinalGame(bridge)) {
+            outputView.printResult(moveResult, bridgeGame.isFailFinalGame(bridge),
+                    bridgeGame.checkRetryCount());
+            return true;
+        }
+        return false;
+    }
+
+    private boolean isGameQuit(String gameCommand, List<String> moveResult, List<String> bridge) {
+        if (gameCommand.equals("Q")) {
+            outputView.printResult(moveResult, bridgeGame.isFailFinalGame(bridge),
+                    bridgeGame.checkRetryCount());
+            return true;
+        }
+        return false;
+    }
+
+    private List<String> moveBridgeResult(List<String> bridge) {
+        List<String> moveResult;
+        outputView.printMovingSpaceChoice();
+        String moveSpace = inputView.readMoving();
+        moveResult = bridgeGame.move(bridge, moveSpace);
+        outputView.printMap(moveResult);
+        return moveResult;
+    }
+
+    private List<String> startGameMakeBridge() {
         outputView.printGameStart();
         outputView.printBridgeLengthInput();
         int bridgeSize = inputView.readBridgeSize();
         List<String> bridge = bridgeMaker.makeBridge(bridgeSize);
-
-        List<String> moveResult = new ArrayList<>();
-        while (true) {
-            outputView.printMovingSpaceChoice();
-            String moveSpace = inputView.readMoving();
-            moveResult = bridgeGame.move(bridge, moveSpace);
-            outputView.printMap(moveResult);
-            if (bridgeGame.isFailGame()) {
-                outputView.printRetryOrQuitChoice();
-                String gameCommand = inputView.readGameCommand();
-                List<String> retry = bridgeGame.retry(gameCommand);
-                moveResult = retry;
-                if (gameCommand.equals("Q")) {
-                    outputView.printResult(moveResult, bridgeGame.isFailFinalGame(bridge),
-                            bridgeGame.checkRetryCount());
-                    break;
-                }
-                continue;
-            }
-            if (!bridgeGame.isFailFinalGame(bridge)) {
-                outputView.printResult(moveResult, bridgeGame.isFailFinalGame(bridge),
-                        bridgeGame.checkRetryCount());
-            }
-        }
+        return bridge;
     }
 }
