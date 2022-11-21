fdb9c818226486edaeadfea22a4f230a53bdb45f
bunsung92 <bunsung92@naver.com>
2022-11-21 23:40:28 +0900

    test(bridge): 유저 방향 및 재시도 명령어 요청 테스트 코드 추가

--- src/test/java/bridge/domain/ClientTest.java
+++ src/test/java/bridge/domain/ClientTest.java
@@ -1,11 +1,13 @@
 package bridge.domain;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.mockStatic;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 
 import bridge.BridgeMaker;
+import bridge.util.Client;
 import bridge.view.ViewMaker;
 import java.io.Console;
 import org.junit.jupiter.api.BeforeEach;
@@ -33,6 +35,12 @@ class ClientTest {
 
             verify(viewMaker, times(1)).receiveClientRequest();
         }
+    }
+
+    @DisplayName("정상 시나리오 | 게임 유저를 뷰 메이커에게 소개한다.")
+    @Test
+    void introduceGameUserToViewMaker() {
+        assertThat(Client.introduceGameUser(viewMaker)).isInstanceOf(User.class);
     }
 
 }
