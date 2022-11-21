5040e4cf036c9d0e5277a66e7f3caa25aa768a12
이성준 <75744297+castlejun-2@users.noreply.github.com>
2022-11-21 21:02:20 +0900

    feat: 메인 함수 구현

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -4,5 +4,10 @@ public class Application {
 
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        BridgeGame bridgeGame = new BridgeGame();
+        bridgeGame.play();
+
+        while (!bridgeGame.isExit())
+            bridgeGame.move();
     }
 }
