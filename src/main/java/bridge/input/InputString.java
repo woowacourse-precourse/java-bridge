84db6cf5a6c68657fcf84e4e696e319f7964e1d1
hoin <apicad0307@gmail.com>
2022-11-21 13:50:43 +0900

    feat(input/InputString.java): 입력에 사용할 문자열 상수화

--- src/main/java/bridge/input/InputString.java
+++ src/main/java/bridge/input/InputString.java
@@ -9,4 +9,10 @@ public class InputString {
     public static final String BRIDGE_LENGTH_INPUT = "다리의 길이를 입력해주세요.";
     public static final String MOVE_INPUT = "이동할 칸을 선택해주세요. (위: U, 아래: D)";
     public static final String RETRY_INPUT = "게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)";
+
+    public static final String QUIT = "Q";
+    public static final String RESTART = "R";
+
+    public static final String UP = "U";
+    public static final String DOWN = "D";
 }
