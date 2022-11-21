4ca2172c766ceb42019933108daed715fe824d9c
맹순영 <symaeng98@naver.com>
2022-11-21 15:34:26 +0900

    feat(InputView): 재시도 입력 오류 확인 로직 추가

--- src/main/java/bridge/InputView.java
+++ src/main/java/bridge/InputView.java
@@ -9,6 +9,7 @@ public class InputView {
     private final String INPUT_NUMBER_ERROR_SENTENCE = "[ERROR] 숫자를 입력해야 합니다.";
     private final String INPUT_NUMBER_RANGE_ERROR_SENTENCE = "[ERROR] 다리 길이는 3부터 20 사이의 숫자여야 합니다.";
     private final String INPUT_BOARD_ERROR_SENTENCE = "[ERROR] 칸이 잘못 입력되었습니다.";
+    private final String INPUT_RETRY_ERROR_SENTENCE = "[ERROR] 잘못된 문자가 입력되었습니다. 재시도: R, 종료: Q";
 
     /**
      * 다리의 길이를 입력받는다.
@@ -30,8 +31,9 @@ public class InputView {
     /**
      * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
      */
-    public String readGameCommand() {
-        return null;
+    public String readGameCommand(String retry) {
+        validateRetry(retry);
+        return retry;
     }
 
     private int parseInt(String bridgeSize) {
@@ -51,6 +53,12 @@ public class InputView {
     private void validateBoard(String board) {
         if (!board.equals("U") && !board.equals("D")) {
             throw new IllegalArgumentException(INPUT_BOARD_ERROR_SENTENCE);
+        }
+    }
+
+    private void validateRetry(String retry) {
+        if (!retry.equals("R") && !retry.equals("Q")) {
+            throw new IllegalArgumentException(INPUT_RETRY_ERROR_SENTENCE);
         }
     }
 
