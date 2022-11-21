ebe1617fb5e95b174affc1f3487408ac1bb15698
seeunla <107592967+seeunla@users.noreply.github.com>
2022-11-21 18:23:34 +0900

    feat: BridgeGame에 게임시작 구현중

--- src/main/java/bridge/BridgeGame.java
+++ src/main/java/bridge/BridgeGame.java
@@ -1,9 +1,37 @@
 package bridge;
 
+import java.util.List;
+
 /**
  * 다리 건너기 게임을 관리하는 클래스
  */
 public class BridgeGame {
+    private final InputView inputView;
+    private final OutputView outputView;
+    private BridgeMaker bridgeMaker;
+
+    private Bridge bridge;
+
+    public BridgeGame() {
+        this.inputView = new InputView();
+        this.outputView = new OutputView();
+        bridgeMaker = new BridgeMaker(new BridgeRandomNumberGenerator());
+    }
+
+    public void run() {
+
+    }
+
+    private void gameSituation() {
+        List<String> brige = initGame();
+    }
+
+    public List<String> initGame() {
+        int bridgeSize = inputView.readBridgeSize();
+        List<String> newBridge = this.bridgeMaker.makeBridge(bridgeSize);
+        bridge = new Bridge(newBridge);
+        return newBridge;
+    }
 
     /**
      * 사용자가 칸을 이동할 때 사용하는 메서드
@@ -11,6 +39,22 @@ public class BridgeGame {
      * 이동을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
     public void move() {
+        while (true) {
+            int status = bridge.getState();
+            moveNext(status);
+        }
+    }
+
+    private void moveNext(int status) {
+        String direction = inputView.readMoving();
+        String input = input(direction, bridge.getbridge().get(status));
+    }
+
+    private String input(String user, String answer) {
+        if ( !user.equals(answer)) {
+            return "X";
+        }
+        return "O";
     }
 
     /**
