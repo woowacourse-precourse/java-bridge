451923a5708a445b189fe9dac5ab3d3c9369f690
tjddnr7760 <tjddnr7760@naver.com>
2022-11-21 17:24:03 +0900

    feat : 브리지 정보를 저장하는 BridgeInform 클래스 구현, test 통과

--- src/main/java/bridge/BridgeInform.java
+++ src/main/java/bridge/BridgeInform.java
@@ -1,2 +1,57 @@
-package bridge;public class BridgeInform {
+package bridge;
+
+import java.util.List;
+import java.util.ArrayList;
+
+public class BridgeInform {
+    private final int size;
+    private String exit;
+    private List<String> directions = new ArrayList<>();
+
+    BridgeInform(int size) {
+        validateRange(size);
+        this.size = size;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public void setDirection(String direction) {
+        validateCorrectDirection(direction);
+        directions.add(direction);
+    }
+
+    public String getLastDirection() {
+        String lastdirection = directions.get(directions.size());
+        return lastdirection;
+    }
+
+    public String getIndexDirection(int index) {
+        String indexdirection = directions.get(index);
+        return indexdirection;
+    }
+
+    public void setExit(String exit) {
+        validateCorrectExit(exit);
+        this.exit = exit;
+    }
+
+    private void validateRange(int size) {
+        if(size < 3 || size > 20) {
+            throw new IllegalArgumentException("[ERROR]");
+        }
+    }
+
+    private void validateCorrectDirection(String direction) {
+        if(direction.equals("U") == false && direction.equals("D") == false) {
+            throw new IllegalArgumentException("[ERROR]");
+        }
+    }
+
+    private void validateCorrectExit(String direction) {
+        if(direction.equals("R") == false && directions.equals("Q") == false) {
+            throw new IllegalArgumentException("[ERROR]");
+        }
+    }
 }
