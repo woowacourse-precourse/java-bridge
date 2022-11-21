7e03a1d709bcc857bbeca8666fe50db58314e964
josolha <josolha@nate.com>
2022-11-22 01:33:17 +0900

    feat(Application) : 예외처리 기능 추가

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -5,7 +5,12 @@ import bridge.gameController.GameController;
 public class Application {
 
     public static void main(String[] args) {
-        GameController gameController = new GameController();
-        gameController.run();
+        try {
+            GameController gameController = new GameController();
+            gameController.run();
+
+        }catch (Exception e){
+
+        }
     }
 }
