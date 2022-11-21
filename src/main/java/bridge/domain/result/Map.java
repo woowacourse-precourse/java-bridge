f6b62f0b3b044199245644a6a905bb6687ba8c07
kimJaeHyeon <kmr2644@gmail.com>
2022-11-21 19:52:12 +0900

    feat: 위 아래 블럭을 나누어서 move 대한 결과를 담는 객체 구현

---
+++ src/main/java/bridge/domain/result/Map.java
@@ -0,0 +1,42 @@
+package bridge.domain.result;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Map {
+    private final List<ResultType> upBlocks = new ArrayList<>();
+    private final List<ResultType> downBlocks = new ArrayList<>();
+
+    protected void addUpBlock(ResultType resultType) {
+        upBlocks.add(resultType);
+        addDownBlock(ResultType.BLANK);
+    }
+
+    protected void addDownBlock(ResultType resultType) {
+        downBlocks.add(resultType);
+        upBlocks.add(ResultType.BLANK);
+    }
+
+    public void retry() {
+        this.upBlocks.clear();
+        this.downBlocks.clear();
+    }
+
+    @Override
+    public String toString() {
+        return create(upBlocks) + create(downBlocks);
+    }
+
+    private String create(List<ResultType> blocks) {
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append("[ ");
+        for (int i = 0; i < blocks.size(); i++) {
+            stringBuilder.append(blocks.get(i));
+            if (i > 0) {
+                stringBuilder.append(" | ");
+            }
+        }
+        stringBuilder.append(" ]\n");
+        return stringBuilder.toString();
+    }
+}
