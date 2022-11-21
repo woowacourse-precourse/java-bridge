3fa65fbf958329edaee10eecaacbe246a828bb7c
이성준 <75744297+castlejun-2@users.noreply.github.com>
2022-11-21 20:55:27 +0900

    feat: 게임의 재시작 여부를 묻는 출력 함수 생성

--- src/main/java/bridge/OutputView.java
+++ src/main/java/bridge/OutputView.java
@@ -62,4 +62,8 @@ public class OutputView {
     public void printError(String message) {
         System.out.println(message);
     }
+
+    public void printRegameMessage() {
+        System.out.println("게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)");
+    }
 }
