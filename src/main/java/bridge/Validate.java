2768d2fd0f5f3d34fb9170b5a03d1df0e1b2b7aa
yoon-yoo-tak <dbsdbxkrzz@naver.com>
2022-11-21 22:26:11 +0900

    feat: 이동방향 유효성 검증 메서드 추가
    입력된 이동 방향이 유효한 입력인지 검증하는 메서드를 추가합니다

--- src/main/java/bridge/Validate.java
+++ src/main/java/bridge/Validate.java
@@ -23,6 +23,15 @@ public class Validate {
     public static boolean notInRange(String length) {
         return Integer.parseInt(length) < 3 || Integer.parseInt(length) > 20;
     }
+    
+    public static void checkMoveDirection(String direction) {
+        if (invalidDirection(direction)) {
+            throw new IllegalArgumentException(error + Error.MOVE_DIRECTION_INVALID.getMessage());
+        }
+    }
 
+    public static boolean invalidDirection(String direction) {
+        return !(direction.equals("U") || direction.equals("D"));
+    }
 
 }
