ac2d453170660284ddddb48482da84e238879d0d
gmu1026 <gmu1026@gmail.com>
2022-11-22 02:01:51 +0900

    feat: 다리 이동 값 입력 기능 구현

--- src/main/java/bridge/InputView.java
+++ src/main/java/bridge/InputView.java
@@ -1,15 +1,21 @@
 package bridge;
 
 import camp.nextstep.edu.missionutils.Console;
+import java.util.Arrays;
 
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
  */
 public class InputView {
     private static final String INPUT_BRIDGE_SIZE_MESSAGE = "다리의 길이를 입력해주세요.";
+    private static final String INPUT_MOVE_MESSAGE = "이동한 칸을 선택해주세요.";
 
     private static final int BRIDGE_MIN_RANGE = 3;
     private static final int BRIDGE_MAX_RANGE = 20;
+
+    private static final String UP_COMMAND = "U";
+    private static final String DOWN_COMMAND = "D";
+
     /**
      * 다리의 길이를 입력받는다.
      */
@@ -44,7 +50,21 @@ public class InputView {
      * 사용자가 이동할 칸을 입력받는다.
      */
     public String readMoving() {
-        return null;
+        System.out.println(INPUT_MOVE_MESSAGE);
+        String inputMoveDirection = Console.readLine();
+        validateMovingDirection(inputMoveDirection);
+
+        return inputMoveDirection.toUpperCase();
+    }
+
+    private void validateMovingDirection(String inputMoveDirection) {
+        validateMoveCommand(inputMoveDirection.trim());
+    }
+
+    private void validateMoveCommand(String inputMoveDirection) {
+        if (!inputMoveDirection.equalsIgnoreCase(UP_COMMAND) && !inputMoveDirection.equalsIgnoreCase(DOWN_COMMAND)) {
+            throw new IllegalArgumentException("[ERROR] U 또는 D 값만 입력해야 합니다.");
+        }
     }
 
     /**
