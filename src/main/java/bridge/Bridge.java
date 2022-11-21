d370626770a5def0849ca2d1caf716f3146590f8
youswim <youswim96@gmail.com>
2022-11-22 05:43:08 +0900

    feat(Bridge.java): 입력값과 다리 비교 기능 추가

--- src/main/java/bridge/Bridge.java
+++ src/main/java/bridge/Bridge.java
@@ -1,34 +1,28 @@
 package bridge;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Bridge {
 
     private final List<String> bridge;
-    int answerCount = 0;
-    boolean currentResult = true;
+    private final List<Boolean> results = new ArrayList<>();
 
     public Bridge(List<String> bridge) {
         this.bridge = bridge;
     }
 
     public void compareWithBridge(String input) {
-        currentResult = false;
-        if (bridge.get(0).equals(input)) {
-            answerCount++;
-            currentResult = true;
-        }
+        boolean result = bridge.get(results.size()).equals(input);
+        results.add(result);
     }
+
 
     public List<String> getBridge() {
         return bridge;
     }
 
-    public int getAnswerCount() {
-        return answerCount;
-    }
-
-    public boolean isCurrentResult() {
-        return currentResult;
+    public List<Boolean> getResults() {
+        return results;
     }
 }
