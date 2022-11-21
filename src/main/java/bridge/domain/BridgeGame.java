4c786f54524f8a873e7d8794c23ec83d4176dff2
This2sho <zx00018@naver.com>
2022-11-22 01:11:11 +0900

    feat(BridgeGame): 플레이어를 이동시키는 기능 구현

--- src/main/java/bridge/domain/BridgeGame.java
+++ src/main/java/bridge/domain/BridgeGame.java
@@ -1,23 +1,47 @@
 package bridge.domain;
 
+import java.util.List;
+
 /**
  * 다리 건너기 게임을 관리하는 클래스
  */
 public class BridgeGame {
 
-    /**
-     * 사용자가 칸을 이동할 때 사용하는 메서드
-     * <p>
-     * 이동을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
-     */
-    public void move() {
-    }
+	private Player player;
+	private final Bridge bridge;
+	private final GameRecord gameRecord;
 
-    /**
-     * 사용자가 게임을 다시 시도할 때 사용하는 메서드
-     * <p>
-     * 재시작을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
-     */
-    public void retry() {
-    }
+	public BridgeGame(List<String> bridgeInput) {
+		this.player = new Player();
+		this.bridge = new Bridge(bridgeInput);
+		this.gameRecord = new GameRecord();
+	}
+
+	/**
+	 * 사용자가 칸을 이동할 때 사용하는 메서드
+	 * <p>
+	 * 이동을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
+	 */
+	public void move(String direction) {
+		int currentLocation = player.getCurrentLocation();
+		if (bridge.canWalk(currentLocation, direction)) {
+			player.move();
+			gameRecord.recordSuccess(direction);
+			return;
+		}
+		player.die();
+		gameRecord.recordFail(direction);
+	}
+
+	Player getPlayer() {
+		return player;
+	}
+
+	/**
+	 * 사용자가 게임을 다시 시도할 때 사용하는 메서드
+	 * <p>
+	 * 재시작을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
+	 */
+	public void retry() {
+	}
 }
