506ff297642407ef326fc20cae85cf3aa04cb1db
leesangjin <tkdwls0628@gmail.com>
2022-11-21 19:29:06 +0900

    feat(OutputView): 결과 출력 기능 구현

--- src/main/java/bridge/view/OutputView.java
+++ src/main/java/bridge/view/OutputView.java
@@ -7,6 +7,9 @@ import java.util.List;
 public class OutputView {
 
     private static final String START_BRIDGE_GAME = "다리 건너기 게임을 시작합니다.";
+    private static final String RESULT_OF_GAME = "최종 게임 결과";
+    private static final String RESULT_OF_SUCCESS_OR_FAIL= "게임 성공 여부: %s\n";
+    private static final String TOTAL_TRY_COUNT = "총 시도한 횟수: %d";
 
     public static void printStartGame() {
         System.out.println(START_BRIDGE_GAME);
@@ -18,6 +21,10 @@ public class OutputView {
         }
     }
 
-    public static void printResult() {
+    public static void printResult(List<List<String>> map, String result, int totalTry) {
+        System.out.println(RESULT_OF_GAME);
+        printMap(map);
+        System.out.printf((RESULT_OF_SUCCESS_OR_FAIL), result);
+        System.out.printf((TOTAL_TRY_COUNT), totalTry);
     }
 }
