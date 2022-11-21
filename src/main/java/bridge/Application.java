8b83fb0d22b6f2398988307506867e808fd616d9
hayeon <qorgkdus12@naver.com>
2022-11-21 23:20:36 +0900

    feat: input bridge size &input exception

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -1,8 +1,23 @@
 package bridge;
 
 public class Application {
-
+    static final int ERROR=-1;
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        ReadSize readSize = new ReadSize();
+        while (readSize.bridgeSize()==-1){
+            readSize = new ReadSize();
+        }
+    }
+}
+class ReadSize{
+    static final int ERROR=-1;
+    public int bridgeSize(){
+        InputView inputview = new InputView();  int size=0;
+        try { size=inputview.readBridgeSize(); }
+        catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+            return ERROR;
+        }
+        return size;
     }
 }
--- src/main/java/bridge/InputView.java
+++ src/main/java/bridge/InputView.java
@@ -1,4 +1,6 @@
 package bridge;
+import java.util.InputMismatchException;
+import java.util.Scanner;
 
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
@@ -9,20 +11,29 @@ public class InputView {
      * 다리의 길이를 입력받는다.
      */
     public int readBridgeSize() {
-        return 0;
+        System.out.println("다리의 길이를 입력해주세요.");
+        Scanner sc = new Scanner(System.in);    int size;
+        try { size = sc.nextInt();  System.out.println(size); }
+        catch (InputMismatchException e) {throw new IllegalArgumentException("[ERROR] 3에서 20사이의 숫자를 입력하세요.");}
+        if (!(3 <= size && size <= 20)) {throw new IllegalArgumentException("[ERROR] 3에서 20사이의 숫자를 입력하세요.");}
+        return size;
     }
 
     /**
      * 사용자가 이동할 칸을 입력받는다.
      */
     public String readMoving() {
-        return null;
+        Scanner sc=new Scanner(System.in);
+        String move=sc.next();
+        return move;
     }
 
     /**
      * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
      */
     public String readGameCommand() {
+        Scanner sc=new Scanner(System.in);
+        String cmd=sc.next();
         return null;
     }
 }
