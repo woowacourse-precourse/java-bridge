bf3d7af2de5150b5cf22fceaf5a31e540720c9ed
baksohyeon <bori12370@gmail.com>
2022-11-21 21:50:34 +0900

    feat: 다리 생성 클래스 구현 완료 및 에러메시지 추가
    camp.nextstep.edu.missionutils 자체에서 오류를 발생할 경우 에러 핸들링 (0, 1 이외의 숫자 발급)

--- src/main/java/bridge/domain/ErrorMessageConstant.java
+++ src/main/java/bridge/domain/ErrorMessageConstant.java
@@ -10,4 +10,6 @@ public final class ErrorMessageConstant {
     public static final String INVALID_BRIDGE_LENGTH = PREFIX + "다리 길이는 3부터 20 사이의 숫자여야 합니다.";
     public static final String INVALID_MOVING_COMMAND = PREFIX + "이동을 위해서 U 또는 D 만을 입력해 주세요.";
     public static final String INVALID_GAME_OVER_CONTROL_COMMAND = PREFIX + "게임 종료 여부 판단을 위해서 R 또는 Q 만을 입력해 주세요.";
+    public static final String RANDOM_NUMBER_GENERATOR_OPERATION_FAILED = PREFIX + "다리를 만드는 과정에서 버그가 발생하였습니다. 게임을 재시작하여 주세요.";
+
 }
--- src/main/java/bridge/service/BridgeMaker.java
+++ src/main/java/bridge/service/BridgeMaker.java
@@ -1,7 +1,9 @@
 package bridge.service;
 
+import bridge.domain.ErrorMessageConstant;
 import bridge.util.BridgeNumberGenerator;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -21,6 +23,22 @@ public class BridgeMaker {
      * @return 입력받은 길이에 해당하는 다리 모양. 위 칸이면 "U", 아래 칸이면 "D"로 표현해야 한다.
      */
     public List<String> makeBridge(int size) {
-        return null;
+        List<String> bridge = new ArrayList<>();
+        for (int i = 0; i < size; i++) {
+            int randomNumber = bridgeNumberGenerator.generate();
+            String path = designatePath(randomNumber);
+            bridge.add(path);
+        }
+        return bridge;
+    }
+
+    private String designatePath(int randomNumber) {
+        if (randomNumber == 0) {
+            return "D";
+        }
+        if(randomNumber == 1) {
+            return "U";
+        }
+        throw new IllegalArgumentException(ErrorMessageConstant.RANDOM_NUMBER_GENERATOR_OPERATION_FAILED);
     }
 }
