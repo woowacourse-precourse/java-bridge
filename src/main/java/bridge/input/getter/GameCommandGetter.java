80043386aa665dfbf82db79cecbe12b790dc6105
yongseungLeeDec <yonson.dev@gmail.com>
2022-11-21 19:41:48 +0900

    feat: 게임 재시도/종료 입력 요청 및 받아오기 기능 구현
    GameCommandGetter 클래스를 추가하고, getGameCommandFromConsole() 메서드를 구현함.
    - 해당 메서드는 내부적으로 사용자에게 게임 재시도/종료 옵션 입력을 요청하는 printPromptForGameCommand를 호출함.
    - GameCommandValidator의 isValidGameCommand를 호출하여 내부적으로 검증함.

---
+++ src/main/java/bridge/input/getter/GameCommandGetter.java
@@ -0,0 +1,32 @@
+package bridge.input.getter;
+
+import bridge.console.InputView;
+import bridge.input.validator.GameCommandValidator;
+
+public class GameCommandGetter {
+    private static final String TEXT_PROMPT_FOR_GAME_COMMAND = "게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)";
+    private static final String INVALID_GAME_COMMAND = "재시도(\"R\") 또는 종료(\"Q\")만 가능합니다.";
+
+    private final InputView inputView;
+    private final GameCommandValidator validator;
+
+    public GameCommandGetter() {
+        this.inputView = new InputView();
+        this.validator = new GameCommandValidator();
+    }
+
+    public String getGameCommandFromConsole() throws IllegalArgumentException {
+        printPromptForGameCommand();
+        String userInput = inputView.readGameCommand();
+
+        if (validator.isValidGameCommand(userInput)) {
+            return userInput;
+        }
+
+        throw new IllegalArgumentException(INVALID_GAME_COMMAND);
+    }
+
+    private void printPromptForGameCommand() {
+        System.out.println(TEXT_PROMPT_FOR_GAME_COMMAND);
+    }
+}
