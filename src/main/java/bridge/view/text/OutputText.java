81593a7d0c008c4dc31a92c9d7ac456cec9a64a9
Frog-Slayer <Frog-Slayer@github.com>
2022-11-21 17:38:04 +0900

    refactor(OutputText): 변경 입력 요청 메시지들의 이름에 REQUEST를 붙이도록 변경

--- src/main/java/bridge/view/text/OutputText.java
+++ src/main/java/bridge/view/text/OutputText.java
@@ -2,12 +2,16 @@ package bridge.view.text;
 
 public enum OutputText {
     GAME_START("다리 건너기 게임을 시작합니다."),
-    BRIDGE_LENGTH_INPUT("다리의 길이를 입력해주세요."),
-    MOVE_INPUT("이동할 칸을 선택해주세요. (위: %s, 아래: %s)"),
-    RETRY_INPUT("게임을 다시 시도할지 여부를 입력해주세요. (재시도: %s, 종료: %s)"),
+    BRIDGE_LENGTH_INPUT_REQUEST("다리의 길이를 입력해주세요."),
+    MOVE_INPUT_REQUEST("이동할 칸을 선택해주세요. (위: %s, 아래: %s)"),
+    RETRY_INPUT_REQUEST("게임을 다시 시도할지 여부를 입력해주세요. (재시도: %s, 종료: %s)"),
     GAME_RESULT_TITLE("최종 게임 결과"),
     GAME_CLEAR("게임 성공 여부: %s"),
-    TRIAL_COUNT("총 시도한 횟수: ");
+    TRIAL_COUNT("총 시도한 횟수: "),
+    CURRENT_MAP("[%s]"),
+    MOVABLE_BLOCK("O"),
+    UNMOVABLE_BLOCK("X"),
+    BLANK_BLOCK(" ");
 
     private String message;
     OutputText(String message){
