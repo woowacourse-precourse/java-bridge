64e953583e47de6aa905e61be0b2d7ffc89abf2b
Nugur1i <duckhee092@naver.com>
2022-11-21 20:55:00 +0900

    refactor(Application): refactor application class

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -6,6 +6,37 @@ public class Application {
     static InputView inputView = new InputView();
     static BridgeRandomNumberGenerator bridgeRandomNumberGenerator = new BridgeRandomNumberGenerator();
     static BridgeGame bridgeGame = new BridgeGame();
+    static OutputView outputView = new OutputView();
+
+    private static boolean retryOrNot = true;
+    public enum correctWrong{
+        CORRECT(true, "O", "성공"),
+        WRONG(false, "X", "실패");
+
+        boolean trueFalse;
+        String ox;
+        String successFail;
+
+
+        correctWrong(boolean trueFalse, String ox, String successFail) {
+            this.trueFalse = trueFalse;
+            this.ox = ox;
+            this.successFail = successFail;
+        }
+
+        public boolean getTrueFalse(){
+            return trueFalse;
+        }
+
+        public String getOx(){
+            return ox;
+        }
+
+        public String getSuccessFail(){
+            return successFail;
+        }
+    }
+
 
     public static void main(String[] args) {
         // TODO: 프로그램 구현
@@ -13,7 +44,47 @@ public class Application {
         List<String> bridgeList = new BridgeMaker(bridgeRandomNumberGenerator).makeBridge(inputNumber);
         //최초 1회 구현
 
-        bridgeGame.move(bridgeList);
+
+
+        int tryNumber = 0;
+        String ox = null;
+        String successFail = null;
+        while(retryOrNot){
+            outputView.resetMap();
+
+            for (int order = 0; order < bridgeList.size(); order++){
+                String upDownUserInput = inputView.readMoving();
+                boolean trueFalse = bridgeGame.move(bridgeList, upDownUserInput, order);
+
+
+
+                if (trueFalse = true){
+                    correctWrong correct = correctWrong.CORRECT;
+                    ox = correct.getOx();
+                    successFail = correct.getSuccessFail();
+                    outputView.printMap(ox,order,upDownUserInput);
+                    if (order == bridgeList.size() - 1){
+                        trueFalse = false;
+                        break;
+                    }
+                } else if (trueFalse = false){
+                    correctWrong correct = correctWrong.WRONG;
+                    ox = correct.getOx();
+                    successFail = correct.getSuccessFail();
+                    outputView.printMap(ox,order,upDownUserInput);
+                    trueFalse = bridgeGame.retry();
+                    break;
+                }
+
+            }
+
+
+            retryOrNot = trueFalse;
+
+            tryNumber++;
+        }
+
+        outputView.printResult(tryNumber,successFail);
 
 
 
--- src/main/java/bridge/BridgeGame.java
+++ src/main/java/bridge/BridgeGame.java
@@ -19,68 +19,30 @@ public class BridgeGame {
      * <p>
      * 이동을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void move(List<String> bridgeList) {
-        // 몇개 만들지 숫자 받기
-        // 랜덤 다리 만들기
-        // 어디 건널지 입력 받기 (여기까지는 application에 만들어도 될듯?)
-        // 맞는지 틀린지 확인
-        this.retryOrNot = true;
-        int tryNumber = 0;
-        while(retryOrNot){
-            outputView.resetMap();
-            bridgeMainGame(bridgeList,retryOrNot);
-            tryNumber++;
-        }
-
-        outputView.printResult(tryNumber,failSuccess);
-
-    }
-
-
-
-    private boolean bridgeMainGame(List<String> bridgeList, boolean retryOrNot){
-
-
-        for (int order = 0; order <bridgeList.size(); order++) {
-
-            String upDownUserInput = inputView.readMoving();
-
-            if (Compare(bridgeList,upDownUserInput, order)){
-                String ox = "O";
-                outputView.printMap(ox,order,upDownUserInput);
-                this.retryOrNot = true;
-                if (order == bridgeList.size()-1){
-                    this.retryOrNot = false;
-                    this.failSuccess = "성공";
-                }
-                continue;
-            }
-            String ox = "X";
-            outputView.printMap(ox,order,upDownUserInput);
-            this.retryOrNot = retry();
-            this.failSuccess = "실패";
-            break;
-        }
-
-        return this.retryOrNot;
-    }
-    private boolean Compare(List<String> bridgeList, String upDownUserInput,int number) {
-        if (bridgeList.get(number).equals(upDownUserInput)){
+    public boolean move(List<String> bridgeList, String upDownUserInput, int order) {
+        if (bridgeList.get(order).equals(upDownUserInput)){
             /** get을 사용하지 않는다?
              *
              */
-            //outputView사용
+            //그러면 맞은 것을 나타낸다.
 
             return true;
         }
+        //그러면 안맞은 것을 출력한다.
         return false;
+
+
     }
+
+
+
+
     /**
      * 사용자가 게임을 다시 시도할 때 사용하는 메서드
      * <p>
      * 재시작을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    private boolean retry() {
+    public boolean retry() {
         String retryUserInput = inputView.readGameCommand();
         if (retryUserInput.equals("R")){
             return true;
