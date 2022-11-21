e50dd70b12333498d72e131abc801893edab6d6e
이정우 <acg6138@naver.com>
2022-11-22 02:07:32 +0900

    feat(Direction): 방향(UP, DOWN)에 대한 enum구현

---
+++ src/main/java/bridge/domain/Direction.java
@@ -0,0 +1,32 @@
+package bridge.domain;
+
+public enum Direction {
+    DOWN("D", 0),
+    UP("U", 1);
+
+    private final String value;
+    private final int num;
+
+    Direction(String value, int num) {
+        this.value = value;
+        this.num = num;
+    }
+
+    public static Direction get(String input) {
+        if (input.equals("D")) {
+            return DOWN;
+        }
+        if (input.equals("U")) {
+            return UP;
+        }
+        throw new IllegalArgumentException("[ERROR] 방향은 D또는 U만 존재합니다.");
+    }
+
+    public String getValue() {
+        return this.value;
+    }
+
+    public int getNum() {
+        return this.num;
+    }
+}
