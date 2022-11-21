fa4c9c88bab37ed9aeb6e8f0b82423e8da721b7d
sanghyuk2 <givemesomefoodplz@gmail.com>
2022-11-22 01:55:21 +0900

    feat: 다리 생성 기능 구현
    위 칸을 건널 수 있는 경우 U, 아래 칸을 건널 수 있는 경우 D 값으로 나타냄

--- src/main/java/bridge/domain/bridgegame/BridgeMaker.java
+++ src/main/java/bridge/domain/bridgegame/BridgeMaker.java
@@ -3,6 +3,8 @@ package bridge.domain.bridgegame;
 import bridge.BridgeNumberGenerator;
 
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 /**
  * 다리의 길이를 입력 받아서 다리를 생성해주는 역할을 한다.
@@ -20,6 +22,9 @@ public class BridgeMaker {
      * @return 입력받은 길이에 해당하는 다리 모양. 위 칸이면 "U", 아래 칸이면 "D"로 표현해야 한다.
      */
     public List<String> makeBridge(int size) {
-        return null;
+        return IntStream.range(0, size)
+                .boxed()
+                .map(i -> BridgeType.getBridgeType(bridgeNumberGenerator.generate()).getUpOrDown())
+                .collect(Collectors.toList());
     }
 }
