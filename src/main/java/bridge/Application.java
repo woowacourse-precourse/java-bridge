6408af38e2a83f99366a688472129a44c3c3bdae
WonjunS <seowj0710@gmail.com>
2022-11-21 15:20:47 +1100

    update: removed unnecessary codes

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -10,15 +10,15 @@ import static bridge.view.OutputView.*;
 public class Application {
 
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
         try {
             printGameStart();
             boolean run = true;
             int count = 0;
 
             while(run) {
-                String status = "성공";
                 int size = readBridgeSize();
+
+                // 다리 생성
                 BridgeRandomNumberGenerator generator = new BridgeRandomNumberGenerator();
                 BridgeMaker maker = new BridgeMaker(generator);
                 List<String> bridge = maker.makeBridge(size);
@@ -26,33 +26,32 @@ public class Application {
                 BridgeGame game = new BridgeGame();
                 game.clear();
 
+                String status = "성공";
                 int idx = 0;
                 while(idx < size) {
                     String input = readMoving();
-
                     game.move(bridge, idx, input);
-
                     printMap(game.getUpBridge(), game.getDownBridge());
 
-                    // check
+                    // 일치하지 않는 경우
                     if(game.isGameOver(bridge, idx, input)) {
                         status = "실패";
                         break;
                     }
-
                     idx++;
                 }
-
                 count++;
 
+                // 다리 건너기에 성공했으면 결과 리턴
                 if(status.equals("성공")) {
                     printResult(game.getUpBridge(), game.getDownBridge(), status, count);
                     break;
                 }
 
-                // 게임 종료
+                // 게임 종료 여부에 대해 입력 받기 (Quit - false, Retry - true)
                 run = BridgeGame.retry(readGameCommand());
 
+                // 종료 (Q) 옵션을 선택했을 경우에 결과 리턴 후 프로그램 종료
                 if(!run) {
                     printResult(game.getUpBridge(), game.getDownBridge(), status, count);
                 }
