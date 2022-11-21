8ed3a6d69fa16aaf50eecba6831ea9589ffa921b
starwook <star.wookk@gmail.com>
2022-11-22 01:28:23 +0900

    refactor(UserCommand.java): refactor UserCommand.java
    moveCommand 이넘의 요소에 1,0을 추가
    생성자 추가로 생성

--- src/main/java/bridge/UserCommand.java
+++ src/main/java/bridge/UserCommand.java
@@ -1,19 +1,25 @@
 package bridge;
 
 public enum UserCommand {
-    MOVE_UP("U","위"),
-    MOVE_DOWN("D","아래"),
+    MOVE_UP("U","위",1),
+    MOVE_DOWN("D","아래",0),
     REPLAY("R","재시도"),
     END("Q","종료");
 
-    private String moveCommand;
+    private String command;
     private String position;
-    private UserCommand(String moveCommand, String position){
-        this.moveCommand = moveCommand;
+    private int number;
+    private UserCommand(String command, String position,int number){
+        this.command = command;
+        this.position = position;
+        this.number = number;
+    }
+    private UserCommand(String command, String position){
+        this.command = command;
         this.position = position;
     }
-    public String getMoveCommand(){
-        return this.moveCommand;
+    public String getCommand(){
+        return this.command;
     }
     public String getPosition(){
         return this.position;
