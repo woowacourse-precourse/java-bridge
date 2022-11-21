8f27d45feb67b00ad97bf0ed06425c5703d38a95
Hi-Imjaeyoung <imjy2757@naver.com>
2022-11-21 18:42:08 +0900

    refactor(ErrorMessage) : 에러메시지 출력을 enum을 이용하여 간소화.

--- src/main/java/bridge/model/Validation.java
+++ src/main/java/bridge/model/Validation.java
@@ -4,7 +4,7 @@ import bridge.constant.BridgeConstant;
 import bridge.constant.ErrorMessage;
 
 public class Validation {
-    private static final String ERROR = "[ERROR]";
+
     public static boolean bridgeSizeValidationTest(String input){
         try{
             valueInRange(isNumber(input));
@@ -34,21 +34,21 @@ public class Validation {
     }
     private static int isNumber(String input) throws IllegalArgumentException{
         if(input.length() != input.replaceAll("[^\\d]","").length()){
-            throw new IllegalArgumentException(ERROR+ ErrorMessage.NO_NUMERIC_VALUE.getErrorMessage());
+            throw new IllegalArgumentException(ErrorMessage.NO_NUMERIC_VALUE.getErrorMessage());
         }
         return Integer.parseInt(input);
     }
     private static void valueInRange(int input) throws IllegalArgumentException{
         if(input<3 || input>20){
-            throw new IllegalArgumentException(ERROR+ ErrorMessage.NUMBER_RANGE_IS_WRONG.getErrorMessage());
+            throw new IllegalArgumentException(ErrorMessage.NUMBER_RANGE_IS_WRONG.getErrorMessage());
         }
     }
     private static String formatTest(String input) throws IllegalArgumentException{
         if(input.length()!=1){
-            throw new IllegalArgumentException(ERROR+ErrorMessage.WRONG_LENGTH.getErrorMessage());
+            throw new IllegalArgumentException(ErrorMessage.WRONG_LENGTH.getErrorMessage());
         }
         if(!Character.isUpperCase(input.charAt(0))){
-            throw new IllegalArgumentException(ERROR+ErrorMessage.WRONG_FORMAT.getErrorMessage());
+            throw new IllegalArgumentException(ErrorMessage.WRONG_FORMAT.getErrorMessage());
         }
         return input;
     }
@@ -59,7 +59,7 @@ public class Validation {
         if(input.equals(BridgeConstant.CAN_GO_DOWN.getString())){
             return;
         }
-        throw new IllegalArgumentException(ERROR + ErrorMessage.WRONG_FORMAT.getErrorMessage());
+        throw new IllegalArgumentException(ErrorMessage.WRONG_FORMAT.getErrorMessage());
     }
     private static void retryInputValueTest(String input) throws IllegalArgumentException{
         if(input.equals(BridgeConstant.RETRY.getString())){
@@ -68,7 +68,7 @@ public class Validation {
         if(input.equals(BridgeConstant.END.getString())){
             return;
         }
-        throw new IllegalArgumentException(ERROR + ErrorMessage.WRONG_FORMAT.getErrorMessage());
+        throw new IllegalArgumentException(ErrorMessage.WRONG_FORMAT.getErrorMessage());
     }
 
 }
