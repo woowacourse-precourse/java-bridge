1329bd73aa7ee063d19a2f27163222879b45612a
wonwoo6y <wonwoo6y@gmail.com>
2022-11-21 20:06:18 +0900

    feat(BridgeGame): BridgeController에서 BridgeGame으로 컨트롤러 역할 수정

--- src/main/java/bridge/BridgeGame.java
+++ src/main/java/bridge/BridgeGame.java
@@ -1,9 +1,58 @@
 package bridge;
 
+import java.util.List;
+
 /**
  * 다리 건너기 게임을 관리하는 클래스
  */
 public class BridgeGame {
+
+    private InputView inputView;
+    private OutputView outputView;
+    private BridgeBluePrint bluePrint;
+    private BridgeMaker maker;
+    private boolean ready;
+
+
+    public BridgeGame(InputView inputView, OutputView outputView) {
+        this.inputView = inputView;
+        this.outputView = outputView;
+        bluePrint = makeBridgeBluePrint();
+        maker = new BridgeMaker(new BridgeRandomNumberGenerator());
+        ready = true;
+        outputView.printBridgeSizeInputStatement();
+    }
+
+    public boolean isReady() {
+        return ready;
+    }
+
+    ;
+
+    private BridgeBluePrint makeBridgeBluePrint() {
+        BridgeBluePrint bluePrint = null;
+        while (bluePrint == null) {
+            outputView.printBridgeSizeInputStatement();
+            bluePrint = makeBridgeBluePrintOrNull();
+        }
+        return bluePrint;
+    }
+
+    private BridgeBluePrint makeBridgeBluePrintOrNull() {
+        try {
+            int number = inputView.readBridgeSize();
+            return new BridgeBluePrint(number);
+        } catch (IllegalStateException e1) {
+            outputView.printError(e1);
+        } catch (IllegalArgumentException e2) {
+            outputView.printError(e2);
+        }
+        return null;
+    }
+
+    public List<String> makeBridge() {
+        return maker.makeBridge(bluePrint.getSize());
+    }
 
     /**
      * 사용자가 칸을 이동할 때 사용하는 메서드
