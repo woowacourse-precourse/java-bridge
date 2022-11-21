b13d130b57b59f646455104c8740579865d38bed
Devtist <yt930310@snu.ac.kr>
2022-11-21 11:52:34 +0900

    feat: 다리의 길이를 전달하는 BridgeSizeDto 구현

---
+++ src/main/java/dto/BridgeSizeDto.java
@@ -0,0 +1,13 @@
+package dto;
+
+public class BridgeSizeDto {
+	private final int bridgeSize;
+
+	public BridgeSizeDto(int bridgeSize) {
+		this.bridgeSize = bridgeSize;
+	}
+
+	public int getBridgeSize() {
+		return bridgeSize;
+	}
+}
