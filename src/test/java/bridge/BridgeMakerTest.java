9d1ed99e03b49d414143e84505f879d389b97b69
Isu Shin <isu010614@naver.com>
2022-11-21 17:36:30 +0900

    test: 다리 생성 확인하는 테스트 코드 작성

--- src/test/java/bridge/BridgeMakerTest.java
+++ src/test/java/bridge/BridgeMakerTest.java
@@ -1,9 +1,7 @@
 package bridge;
 
-import bridge.domain.BridgeMaker;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.List;
 
@@ -11,12 +9,13 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.util.Lists.newArrayList;
 
 class BridgeMakerTest {
+    BridgeNumberGenerator numberGenerator
+            = new ApplicationTest.TestNumberGenerator(newArrayList(1, 0, 0, 1));
+    BridgeMaker bridgeMaker = new BridgeMaker(numberGenerator);
+
     @Test
     @DisplayName("다리 생성 테스트")
-    void validateBridgeMakerLength() {
-        BridgeNumberGenerator numberGenerator
-                = new ApplicationTest.TestNumberGenerator(newArrayList(1, 0, 0, 1));
-        BridgeMaker bridgeMaker = new BridgeMaker(numberGenerator);
+    void validateBridgeMaker() {
         List<String> bridge = bridgeMaker.makeBridge(4);
         assertThat(bridge).containsExactly("U", "D", "D", "U");
     }
