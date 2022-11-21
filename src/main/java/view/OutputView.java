b5d1694d54493d3fb5342fbc3843e7e150db063e
dydtmd6801 <dydtmd6801@naver.com>
2022-11-21 09:04:51 +0900

    feat(OutputView): 결과를 저장하는 기능
    이동할 칸의 값과 건널 수 있는 다리의 값을 비교 후, 결과를 HashMap에 턴과 같이 저장

--- src/main/java/view/OutputView.java
+++ src/main/java/view/OutputView.java
@@ -1,9 +1,14 @@
 package view;
 
+import java.util.HashMap;
+
 /**
  * 사용자에게 게임 진행 상황과 결과를 출력하는 역할을 한다.
  */
 public class OutputView {
+
+    private HashMap<Integer, Boolean> upBridge = new HashMap<>();
+    private HashMap<Integer, Boolean> downBridge = new HashMap<>();
 
     /**
      * 현재까지 이동한 다리의 상태를 정해진 형식에 맞춰 출력한다.
@@ -11,6 +16,17 @@ public class OutputView {
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
     public void printMap() {
+    }
+
+    public void store(Boolean match, String move, int turn) {
+        if (move.equals("U")) {
+            upBridge.put(turn, match);
+            downBridge.put(turn, null);
+        }
+        if (move.equals("D")) {
+            upBridge.put(turn, null);
+            downBridge.put(turn, match);
+        }
     }
 
     /**
