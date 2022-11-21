95dcc18707dba30112bccefb73e184a2989298c0
ensk26 <tjensk26@gmail.com>
2022-11-22 06:16:42 +0900

    refactor(InputType): enum이 적용된 단어를 문자열 포멧으로 받도록 만든다.
    기존 메서드 변경 to InputType
    - "U","D" 를 필요할 값을 넣어서 진행 함
    - 값을 따로 안바꿔도 되는 장점

--- src/main/java/bridge/InputType.java
+++ src/main/java/bridge/InputType.java
@@ -2,8 +2,8 @@ package bridge;
 
 public enum InputType {
     READ_BRIDGE_SIZE("다리의 길이를 입력해주세요."),
-    READ_MOVING("이동할 칸을 선택해주세요. (위: U, 아래: D)"),
-    READ_GAME_COMMAND("게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)");
+    READ_MOVING("이동할 칸을 선택해주세요. (위: %s, 아래: %S)"),
+    READ_GAME_COMMAND("게임을 다시 시도할지 여부를 입력해주세요. (재시도: %s, 종료: %s)");
 
     private final String message;
 
