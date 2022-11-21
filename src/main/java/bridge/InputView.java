c477ea185b6344d886196691a78d7f767060a55d
ijung-won <dlwnddnjs96@gmail.com>
2022-11-21 10:45:36 +0900

    이동칸 입력받는 메소드

--- src/main/java/bridge/InputView.java
+++ src/main/java/bridge/InputView.java
@@ -22,7 +22,8 @@ public class InputView {
      * 사용자가 이동할 칸을 입력받는다.
      */
     public String readMoving() {
-        return null;
+        String input = Console.readLine();
+        return input;
     }
 
     /**
