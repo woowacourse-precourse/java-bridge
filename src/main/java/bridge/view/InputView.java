7608226e7af5d363d1e5a138c3e4f1b2b7d71774
Dongyeol <eastheat10@gmail.com>
2022-11-21 15:54:03 +0900

    refactor: 입력시 공백 삭제

--- src/main/java/bridge/view/InputView.java
+++ src/main/java/bridge/view/InputView.java
@@ -12,20 +12,20 @@ public class InputView {
      * 다리의 길이를 입력받는다.
      */
     public static int readBridgeSize() {
-        return Integer.parseInt(Console.readLine());
+        return Integer.parseInt(Console.readLine().trim());
     }
 
     /**
      * 사용자가 이동할 칸을 입력받는다.
      */
     public static Direction readMoving() {
-        return Direction.getPosition(Console.readLine());
+        return Direction.getPosition(Console.readLine().trim());
     }
 
     /**
      * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
      */
     public static String readGameCommand() {
-        return Console.readLine();
+        return Console.readLine().trim();
     }
 }
