4d9121558af6ef1b69ac2d428e27bfce0595afaf
ykmxxi <ykmxxi97@gmail.com>
2022-11-22 00:14:07 +0900

    refactor: Player 불필요한 메소드 제거

--- src/main/java/bridge/domain/Player.java
+++ src/main/java/bridge/domain/Player.java
@@ -3,7 +3,7 @@ package bridge.domain;
 import java.util.List;
 
 public class Player {
-    private List<String> movingInputs;
+    private final List<String> movingInputs;
 
     public Player(List<String> movingInputs) {
         this.movingInputs = movingInputs;
@@ -11,13 +11,5 @@ public class Player {
 
     public List<String> getMovingInputs() {
         return movingInputs;
-    }
-
-    public String informMoving() {
-        return movingInputs.get(movingInputs.size() - 1);
-    }
-
-    public int informMovingIndex() {
-        return movingInputs.size() - 1;
     }
 }
