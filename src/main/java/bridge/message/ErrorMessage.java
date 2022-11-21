c5ab32efd3abadceb36deef9acb5a76576587fc7
jagajang <kosb7544@gmail.com>
2022-11-21 16:35:46 +0900

    fix: wrong user input message followed by UserInput configuration

--- src/main/java/bridge/message/ErrorMessage.java
+++ src/main/java/bridge/message/ErrorMessage.java
@@ -1,9 +1,13 @@
 package bridge.message;
 
+import bridge.config.UserInput;
+
 public enum ErrorMessage {
     BRIDGE_SIZE_OUT_RANGE("다리 길이는 3부터 20 사이의 숫자여야 합니다."),
-    INPUT_UD("U 또는 D를 입력해야 합니다."),
-    INPUT_RQ("R 또는 Q를 입력해야 합니다."),
+    INPUT_UD(UserInput.UP.getValue()+" 또는 "
+            +UserInput.DOWN.getValue()+"를 입력해야 합니다."),
+    INPUT_RQ(UserInput.RETRY.getValue()+" 또는 "
+            +UserInput.QUIT.getValue()+"를 입력해야 합니다."),
     INPUT_INTEGER("정수를 입력해야 합니다."),
     INPUT_ERROR("입력 과정에서 오류가 발생했습니다."),
     INPUT_ONE_CHARACTER("알파벳 하나를 입력해야 합니다.");
