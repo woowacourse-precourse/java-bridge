ffa21a81f49c167b42a93d970c0492325dca1f00
endlessmomo <soohyuk96@naver.com>
2022-11-22 07:56:02 +0900

    feat(BridgeGame) : BridgeGame 객체 생성

--- src/main/java/bridge/model/Service/BridgeGame.java
+++ src/main/java/bridge/model/Service/BridgeGame.java
@@ -1,16 +1,61 @@
 package bridge.model.Service;
 
+import bridge.BridgeMaker;
+import bridge.BridgeRandomNumberGenerator;
+import bridge.model.domain.BridgeResult;
+import bridge.model.domain.Map;
+import bridge.model.domain.RoundTrace;
+import bridge.model.validation.Command;
+import bridge.model.validation.Move;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 /**
  * 다리 건너기 게임을 관리하는 클래스
  */
 public class BridgeGame {
+    private static final BridgeMaker bridgeMaker = new BridgeMaker(new BridgeRandomNumberGenerator());
+    private final List<String> bridges;
+    private final Map map;
+    private final BridgeResult bridgeResult;
+
+    public BridgeGame() {
+        this.map = new Map();
+        this.bridgeResult = new BridgeResult();
+        this.bridges = new ArrayList <>();
+    }
+
+    public void retryGameSetting() {
+        map.reset();
+    }
+
+    public void makeBridge(int input) {
+        bridges.addAll(new ArrayList <>(bridgeMaker.makeBridge(input)));
+    }
+
+    public List<String> getBridges(){
+        return Collections.unmodifiableList(bridges);
+    }
+
+    public Map getMap() {
+        return map;
+    }
+
+    public BridgeResult getBridgeResult() {
+        return bridgeResult;
+    }
 
     /**
      * 사용자가 칸을 이동할 때 사용하는 메서드
      * <p>
      * 이동을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void move() {
+    public void move(int randomValue, Move move) {
+        boolean isCorrect = move.isEquals(randomValue, move);
+        List <String> roundTrace = RoundTrace.makeTrace(randomValue, isCorrect);
+        map.draw(roundTrace);
     }
 
     /**
@@ -18,6 +63,7 @@ public class BridgeGame {
      * <p>
      * 재시작을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void retry() {
+    public boolean retry(String input) {
+        return Command.of(input).isRetry();
     }
 }
