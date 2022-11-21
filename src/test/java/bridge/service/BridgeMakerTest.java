1a1540bba5ad1fec2700358c8d323a6607553939
EUNJEONGMUN <munej26@gmail.com>
2022-11-21 21:28:35 +0900

    test: 다리 구성 마크 테스트와 사이즈 테스트 분리

--- src/test/java/bridge/service/BridgeMakerTest.java
+++ src/test/java/bridge/service/BridgeMakerTest.java
@@ -1,17 +1,29 @@
 package bridge.service;
 
 import bridge.constant.Constant;
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
-import static org.assertj.core.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
 
 class BridgeMakerTest {
 
-    @DisplayName("만들어진 다리는 U와 D로 이루어져 있으며, 입력한 size와 같은 길이이다.")
+    @DisplayName("만들어진 다리는 U와 D로 이루어져 있다.")
+    @Test
+    void makeBridgeTest() {
+        BridgeNumberGenerator bridgeNumberGenerator = new BridgeRandomNumberGenerator();
+        BridgeMaker bridgeMaker = new BridgeMaker(bridgeNumberGenerator);
+
+        int size = 3;
+        List<String> bridge = bridgeMaker.makeBridge(size);
+        assertThat(bridge.stream()
+                .filter(s -> s.equals(Constant.UP) || s.equals(Constant.DOWN))
+                .count()).isEqualTo(size);
+    }
+
+    @DisplayName("만들어진 다리는 입력한 size와 같은 길이이다.")
     @Test
     void makeBridge() {
         BridgeNumberGenerator bridgeNumberGenerator = new BridgeRandomNumberGenerator();
@@ -20,8 +32,6 @@ class BridgeMakerTest {
         int size = 3;
         List<String> bridge = bridgeMaker.makeBridge(size);
 
-        assertThat(bridge.stream()
-                        .filter(s -> s.equals(Constant.UP) || s.equals(Constant.DOWN))
-                        .count()).isEqualTo(size);
+        assertThat(bridge.size()).isEqualTo(size);
     }
 }
