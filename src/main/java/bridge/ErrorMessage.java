648861ab2a7f0aec0293c3c6a3f4c0a51c112ae9
hojunking96 <junsong96@naver.com>
2022-11-21 17:35:19 +0900

    feats(ErrorMessage.java) : 오류메세지 ENUM 추가

---
+++ src/main/java/bridge/ErrorMessage.java
@@ -0,0 +1,18 @@
+package bridge;
+
+public enum ErrorMessage {
+    BRIDGE_SIZE_ERROR("다리 길이는 3부터 20 사이의 숫자여야 합니다."),
+    MOVING_INPUT_ERROR("U 또는 D를 입력해야 합니다."),
+    COMMAND_INPUT_ERROR("R 또는 Q를 입력해야 합니다.");
+
+    private final String errorLog;
+
+    public String getErrorLog() {
+        return "[ERROR] " + this.errorLog;
+    }
+
+    ErrorMessage(String errorLog) {
+        this.errorLog = errorLog;
+    }
+
+}
