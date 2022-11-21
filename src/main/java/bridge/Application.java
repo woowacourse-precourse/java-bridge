d4ce1003c7ddd1f8e73c0fd5f8a83124f8bb7fba
rabbit0216 <rabbit1999k@gmail.com>
2022-11-21 13:41:12 +0900

    feat: 메인 틀 짜기 + 플레이어로부터 다리 길이 입력받기 구현 완료

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -1,8 +1,25 @@
 package bridge;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+import static bridge.Constants.*;
+
 public class Application {
 
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        OutputView outputView = new OutputView();
+        InputView inputView = new InputView();
+        BridgeRandomNumberGenerator bridgeRandomNumberGenerator = new BridgeRandomNumberGenerator();
+        BridgeMaker bridgeMaker = new BridgeMaker(bridgeRandomNumberGenerator);
+        BridgeGame bridgeGame = new BridgeGame();
+
+        outputView.printInit();
+        int bridgeSize = inputView.readBridgeSize();
+        
     }
+
+
 }
