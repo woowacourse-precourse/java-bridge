5d962d74aa50db31985e15368ad9e55d4200b925
JH-TT <leejongho9803@gmail.com>
2022-11-22 04:26:46 +0900

    refactor(inputview) : bridge에서 예외처리 하기때문에 inputview에 있는 예외처리를 제거

--- src/main/java/bridge/InputView.java
+++ src/main/java/bridge/InputView.java
@@ -3,9 +3,7 @@ package bridge;
 
 import camp.nextstep.edu.missionutils.Console;
 
-import java.util.Scanner;
-
-import static bridge.InputEnum.*;
+import static bridge.Enums.InputEnum.*;
 
 
 /**
@@ -19,44 +17,27 @@ public class InputView {
     public static int readBridgeSize() {
         INPUT_SIZE.getMessage();
         String bridgeSize = Console.readLine();
-        try{
-            Validate.isInputBridgeSizeValid(bridgeSize);
-            return Integer.parseInt(bridgeSize);
-        } catch (IllegalArgumentException e) {
-            System.out.println(e.getMessage());
-            return readBridgeSize();
-        }
+        Validate.isInputBridgeSizeValid(bridgeSize);
+        return Integer.parseInt(bridgeSize);
     }
 
     /**
      * 사용자가 이동할 칸을 입력받는다.
      */
     public static String readMoving() {
-        while(true) {
-            INPUT_MOVING.getMessage();
-            String upOrDown = Console.readLine();
-            try{
-                Validate.isUOrD(upOrDown);
-                return upOrDown.toUpperCase();
-            } catch(IllegalArgumentException e) {
-                System.out.println(e.getMessage());
-            }
-        }
+        INPUT_MOVING.getMessage();
+        String upOrDown = Console.readLine();
+        Validate.isUOrD(upOrDown);
+        return upOrDown.toUpperCase();
     }
 
     /**
      * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
      */
     public static String readGameCommand() {
-        while(true) {
-            INPUT_RETRY.getMessage();
-            String retryOrQuit = Console.readLine();
-            try{
-                Validate.isROrQ(retryOrQuit);
-                return retryOrQuit.toUpperCase();
-            } catch (IllegalArgumentException e) {
-                System.out.println(e.getMessage());
-            }
-        }
+        INPUT_RETRY.getMessage();
+        String retryOrQuit = Console.readLine();
+        Validate.isROrQ(retryOrQuit);
+        return retryOrQuit.toUpperCase();
     }
 }
--- src/test/java/bridge/BridgeTest.java
+++ src/test/java/bridge/BridgeTest.java
@@ -3,8 +3,6 @@ package bridge;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import java.util.List;
-
 import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeTest;
 import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
 import static org.assertj.core.api.Assertions.assertThat;
