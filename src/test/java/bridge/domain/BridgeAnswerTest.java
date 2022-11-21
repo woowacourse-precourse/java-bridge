6759fa3d0549425d226cf8372004cbc297a3422a
hyunseo <goatsfish@gmail.com>
2022-11-21 11:32:37 +0900

    test : 두 다리의 마지막 위치가 같지만 길이가 다를 경우 CONTINUE 반환 성공 테스트

--- src/test/java/bridge/domain/BridgeAnswerTest.java
+++ src/test/java/bridge/domain/BridgeAnswerTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.List;
 import java.util.stream.Stream;
@@ -15,7 +16,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 class BridgeAnswerTest {
 
-    @ParameterizedTest(name = "[{index}] - moveTypes = {0}")
+    @ParameterizedTest(name = "[{index}] moveTypes = {0}")
     @MethodSource("whenCompareBridgeThenSuccessDummy")
     @DisplayName("두 다리를 비교해서 게임 상태 반환 확인을 성공한다.")
     void whenCompareBridgeThenSuccessTest(List<BridgeMoveType> moveTypes) {
@@ -30,6 +31,22 @@ class BridgeAnswerTest {
 
         // then
         assertThat(gameState).isIn(SUCCESS, FAIL, CONTINUE);
+    }
+
+    @ParameterizedTest(name = "[{index}] bridgeSize = {0}")
+    @ValueSource(ints = {3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20})
+    @DisplayName("두 다리의 마지막 위치가 같지만 길이가 다를 경우 CONTINUE 반환을 성공한다.")
+    void whenSameMoveTypeAndDifferentLengthThenSuccessTest(int bridgeSize) {
+        // given
+        BridgeAnswer bridgeAnswer = new BridgeAnswer();
+        bridgeAnswer.setUpAnswerSizeBy(bridgeSize);
+        BridgePlayer bridgePlayer = new BridgePlayer();
+        List<BridgeMoveType> moveTypes = bridgeAnswer.getAnswerMoveHistory().subList(0, bridgeSize - 1);
+        moveTypes.forEach(bridgePlayer::moveTo);
+        //  when
+        BridgeGameState gameState = bridgeAnswer.compareWith(bridgePlayer);
+        // then
+        assertThat(gameState).isIn(CONTINUE);
     }
 
     static Stream<Arguments> whenCompareBridgeThenSuccessDummy() {
