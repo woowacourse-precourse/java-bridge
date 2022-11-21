c94b258f5922e62b81f1c1f492fbe4404b97fbdf
c1ce <chalicenuclear.gmail.com>
2022-11-21 16:33:28 +0900

    feat: 재시작/종료 를 위한 코드개발

--- src/main/java/bridge/BridgeController.java
+++ src/main/java/bridge/BridgeController.java
@@ -4,6 +4,8 @@ import java.util.List;
 
 public class BridgeController {
     private static InputView inputView = new InputView();
+    private static OutputView outputView = new OutputView();
+    private BridgeGame bridgeGame;
     public BridgeGame doBridgeMake(){
 
         int bridgeSize = inputView.readBridgeSize();
@@ -11,18 +13,38 @@ public class BridgeController {
         BridgeNumberGenerator bridgeNumberGenerator = new BridgeRandomNumberGenerator();
         BridgeMaker bridgeMaker = new BridgeMaker(bridgeNumberGenerator);
         List<String> bridgeList = bridgeMaker.makeBridge(bridgeSize);
-        BridgeGame bridgeGame = new BridgeGame(bridgeList);
+        bridgeGame = new BridgeGame(bridgeList);
         return bridgeGame;
     }
 
-    public void doBridgeMove(BridgeGame bridgeGame){
+    public boolean doBridgeMove(BridgeGame bridgeGame){
         String moveWay = inputView.readMoving();
 
         BirdgeMoveType isPossibleMove = bridgeGame.move(moveWay);
         printMove(isPossibleMove);
+
+        if(isPossibleMove.getCode().equals("X")){
+            boolean restart = restartCheck();
+            return restart;
+        }
+        return true;
     }
+
     public void printMove(BirdgeMoveType isPossibleMove){
-        OutputView outputView = new OutputView();
+
+        System.out.println(isPossibleMove);
         outputView.printMap(isPossibleMove);
+    }
+    public boolean restartCheck(){
+        String restartCommand=inputView.readGameCommand();
+        if (restartCommand.equals("R")){
+            outputView = new OutputView();
+            bridgeGame.retry();
+            return true;
+        } else if (restartCommand.equals("Q")) {
+            outputView.printResult();
+            return false;
+        }
+        throw new IllegalArgumentException("[ERROR] R / Q 로 입력해주세요");
     }
 }
--- src/main/java/bridge/InputView.java
+++ src/main/java/bridge/InputView.java
@@ -47,6 +47,11 @@ public class InputView {
      * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
      */
     public String readGameCommand() {
-        return null;
+        System.out.println("게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)");
+        String writeRestart = Console.readLine();
+        if (!writeRestart.equals("R")&&!writeRestart.equals("Q")){
+            throw new IllegalArgumentException("[ERROR] R / Q 로 입력해주세요");
+        }
+        return writeRestart;
     }
 }
