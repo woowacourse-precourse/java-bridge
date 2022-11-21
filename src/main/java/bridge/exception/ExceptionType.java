2700b64a0fc004dc3b1da1523acb0058ed317839
hyun0404woo <hyun0404woo@gmail.com>
2022-11-21 16:26:52 +0900

    refactor: 예외 사항 메시지 수정

--- src/main/java/bridge/exception/ExceptionType.java
+++ src/main/java/bridge/exception/ExceptionType.java
@@ -4,9 +4,9 @@ import static bridge.view.OutputView.ERROR_PREFIX;
 
 public enum ExceptionType {
 
-    BRIDGE_SIZE_OUT_OF_RANGE(ERROR_PREFIX, "3 ~ 20 사이의 자연수를 입력해주세요."),
-    INPUT_WRONG_DIRECTION(ERROR_PREFIX, "잘못된 방향이 입력되었습니다."),
-    INPUT_WRONG_COMMAND(ERROR_PREFIX, "잘못된 명령어가 입력되었습니다."),
+    BRIDGE_SIZE_OUT_OF_RANGE(ERROR_PREFIX, "입력 범위가 잘못되었습니다. (3 ~ 20 사이의 자연수)"),
+    INPUT_WRONG_DIRECTION(ERROR_PREFIX, "잘못된 방향이 입력되었습니다. (U, D)"),
+    INPUT_WRONG_COMMAND(ERROR_PREFIX, "잘못된 명령어가 입력되었습니다. (R, Q)"),
     ;
 
     private final String type;
