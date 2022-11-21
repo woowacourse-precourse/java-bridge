88b3a919e1a969ee21f2cf1acbf65680fef7b3db
zaysverse <zeze@jbnu.ac.kr>
2022-11-22 01:37:16 +0900

    feat(bridge): 게임종료문구를 출력한다

--- src/main/java/bridge/OutputView.java
+++ src/main/java/bridge/OutputView.java
@@ -29,7 +29,12 @@ public class OutputView {
      * <p>
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void printResult() {
+    public void printResult(int total_round, String success) {
+        System.out.println("최종 게임 결과");
+        printMap();
+        System.out.print("게임 성공 여부: " + success);
+        System.out.println("총 시도한 횟수: " + total_round);
+
     }
 
     /**
@@ -47,7 +52,7 @@ public class OutputView {
     /**
      * 다시 시작하기 위해 저장된 게임 진행 상황을 지운다.
      */
-    public void reMap(){
-        map[1]="[";
+    public void reMap() {
+        map[1] = "[";
     }
 }
