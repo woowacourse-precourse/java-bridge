cf0d84a88437b0b86dbbe6e9d94f755ecbd81758
cgk95 <kjkj5868@gmail.com>
2022-11-21 13:46:06 +0900

    feat : 게임의 진행 상황을 시각화 하는 기능 구현

--- src/main/java/bridge/VisualizeController.java
+++ src/main/java/bridge/VisualizeController.java
@@ -1,7 +1,49 @@
 package bridge;
 
 public class VisualizeController {
-    public VisualizeController(){
-        Model.user.peek();
+    public VisualizeController(int idx) { // 지금 몇번째 판떼기에 있는지 받아와서 갱신하는 방식으로 바꾸기
+        StringBuilder upperSubstring = new StringBuilder(Model.upperResult.replaceFirst("]", ""));
+        StringBuilder lowerSubstring = new StringBuilder(Model.lowerResult.replaceFirst("]", ""));
+        String currUser = Model.user.get(idx);
+        String currBridge = Model.bridge.get(idx);
+        if (idx > 0) { // 2칸 이상째 진행이면
+            if (currUser.equals(currBridge)) {
+                if (currUser.equals("U")) {
+                    upperSubstring.append("| O ]");
+                    lowerSubstring.append("|   ]");
+                } else {
+                    upperSubstring.append("|   ]");
+                    lowerSubstring.append("| O ]");
+                }
+            } else {
+                if (currUser.equals("U")) {
+                    upperSubstring.append("| X ]");
+                    lowerSubstring.append("|   ]");
+                } else {
+                    upperSubstring.append("|   ]");
+                    lowerSubstring.append("| X ]");
+                }
+            }
+        } else if (idx == 0) { // 첫 번째 진행이면
+            if (currUser.equals(currBridge)) {
+                if (currUser.equals("U")) {
+                    upperSubstring.append(" O ]");
+                    lowerSubstring.append("   ]");
+                } else {
+                    upperSubstring.append("   ]");
+                    lowerSubstring.append(" O ]");
+                }
+            } else {
+                if (currUser.equals("U")) {
+                    upperSubstring.append(" X ]");
+                    lowerSubstring.append("   ]");
+                } else {
+                    upperSubstring.append("   ]");
+                    lowerSubstring.append(" X ]");
+                }
+            }
+        }
+        Model.upperResult = upperSubstring.toString();
+        Model.lowerResult = lowerSubstring.toString();
     }
 }
