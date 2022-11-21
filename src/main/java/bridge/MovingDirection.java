7cb3e2b7fbc6efe631ec422f6f5c16b64f1798cc
devMuscle <hongsbien@naver.com>
2022-11-22 02:26:28 +0900

    feat: 사용자의 이동방향을 나타내는 Enum

--- src/main/java/bridge/MovingDirection.java
+++ src/main/java/bridge/MovingDirection.java
@@ -1,2 +1,16 @@
-package bridge;public enum MovingDirection {
+package bridge;
+
+public enum MovingDirection {
+    Upper("U"),
+    Lower("D");
+
+    private final String direction;
+
+    private MovingDirection(String direction) {
+        this.direction = direction;
+    }
+
+    public String getDirection() {
+        return direction;
+    }
 }
