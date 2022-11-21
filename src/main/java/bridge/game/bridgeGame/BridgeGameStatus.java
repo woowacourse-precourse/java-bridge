91ff7a8bb68b38b07942d82a09faba5fe3bad333
bellljun3395 <195850@jnu.ac.kr>
2022-11-21 17:20:12 +0900

    style(BridgeGameStatus): 삭제 주석

--- src/main/java/bridge/game/bridgeGame/BridgeGameStatus.java
+++ src/main/java/bridge/game/bridgeGame/BridgeGameStatus.java
@@ -1,30 +1,3 @@
-//package bridge.game.bridgeGame;
-//
-//public class BridgeGameStatus {
-//
-//    private boolean correct;
-//    private boolean fall;
-//
-//    public boolean isCorrect() {
-//        return correct;
-//    }
-//
-//    public boolean isFall() {
-//        return fall;
-//    }
-//
-//
-//    public void correct() {
-//        this.correct = true;
-//    }
-//
-//    public void fall() {
-//        this.fall = true;
-//    }
-//
-//}
-//
-
 package bridge.game.bridgeGame;
 
 public enum BridgeGameStatus {
