0fa5e451556a2f32dc7e45c2942fe7d7f112002e
Invidam <hansupark0208@gmail.com>
2022-11-21 11:25:55 +0900

    refactor(error-message): 에러 메시지에 요구하는 행동 추가

--- src/main/java/bridge/message/ErrorMessage.java
+++ src/main/java/bridge/message/ErrorMessage.java
@@ -2,13 +2,13 @@ package bridge.message;
 
 public enum ErrorMessage {
     ERROR_PREFIX("[ERROR]"),
-    INPUT_SIZE_OUT_OF_RANGE("잘못된 입력입니다. 다리의 길이가 범위에 해당되지 않습니다."),
-    INPUT_SIZE_NOT_INTEGER("잘못된 입력입니다. 다리의 길이 형식인 정수 형식이 아닙니다."),
+    INPUT_SIZE_OUT_OF_RANGE("잘못된 입력입니다. 다리의 길이가 범위에 해당되지 않습니다. 다시 입력해주세요."),
+    INPUT_SIZE_NOT_INTEGER("잘못된 입력입니다. 다리의 길이 형식인 정수 형식이 아닙니다. 다시 입력해주세요."),
     IMMOVABLE_STATE("잘못된 실행입니다. 게임에서 더이상 이동할 수 없습니다."),
-    NEGATIVE_BRIDGE_LENGTH("잘못된 입력입니다. 다리에서의 위치가 양수가 아닙니다."),
-    UNEXPECTED_BRIDGE_VALUE("잘못된 입력입니다. 입력한 값에 해당하는 위치 문자가 존재하지 않습니다."),
-    UNEXPECTED_BRIDGE_CHARACTER("잘못된 입력입니다. 입력한 글자에 해당하는 위치 문자가 존재하지 않습니다."),
-    UNEXPECTED_GAME_COMMAND("잘못된 입력입니다. 입력한 게임 제어 문자가 존재하지 않습니다."),
+    NEGATIVE_BRIDGE_LENGTH("잘못된 입력입니다. 다리에서의 위치가 양수가 아닙니다. 다시 입력해주세요."),
+    UNEXPECTED_BRIDGE_VALUE("잘못된 입력입니다. 입력한 값에 해당하는 위치 문자가 존재하지 않습니다. 다시 입력해주세요."),
+    UNEXPECTED_BRIDGE_CHARACTER("잘못된 입력입니다. 입력한 글자에 해당하는 위치 문자가 존재하지 않습니다. 다시 입력해주세요."),
+    UNEXPECTED_GAME_COMMAND("잘못된 입력입니다. 입력한 게임 제어 문자가 존재하지 않습니다. 다시 입력해주세요."),
     GAME_STOP("잘못된 실행입니다. 게임이 이미 종료되었습니다.");
 
 
