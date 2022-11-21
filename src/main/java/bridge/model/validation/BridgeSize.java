752e310d35e9e7ef9f76a9afa9b3ed668982818d
endlessmomo <soohyuk96@naver.com>
2022-11-22 08:00:22 +0900

    fix(BridgeSize) : 반환값 수정

--- src/main/java/bridge/model/validation/BridgeSize.java
+++ src/main/java/bridge/model/validation/BridgeSize.java
@@ -5,20 +5,20 @@ public class BridgeSize {
     public static final int MIN = 3;
     private final int bridgeSize;
 
-    public BridgeSize(int input){
-        checkRange(input);
-
-        this.bridgeSize = input;
+    public BridgeSize(int bridgeSize){
+        this.bridgeSize = bridgeSize;
     }
 
     public int getBridgeSize(){
         return bridgeSize;
     }
 
-    public void checkRange(int input){
+    public static int checkRange(int input){
         if(input < MIN || input > MAX){
             throw new IllegalArgumentException("");
         }
+
+        return input;
     }
 
 
