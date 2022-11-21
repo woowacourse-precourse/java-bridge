68c4b24b8d065d45fa1afd5dfd55e70330a3bb19
DESKTOP-1ANNTGI\USER <cvc100326@naver.com>
2022-11-21 17:08:53 +0900

    test: 패키지 추가
    - domain 패키지 추가

--- src/test/java/bridge/domain/BridgeGameTest.java
+++ src/test/java/bridge/domain/BridgeGameTest.java
@@ -1,5 +1,6 @@
-package bridge;
+package bridge.domain;
 
+import bridge.domain.BridgeGame;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
--- src/test/java/bridge/domain/DirectionTest.java
+++ src/test/java/bridge/domain/DirectionTest.java
@@ -1,8 +1,9 @@
-package bridge;
+package bridge.domain;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
+import bridge.domain.Direction;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
