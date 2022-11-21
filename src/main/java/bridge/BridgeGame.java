d7501f5c7df0871390b726c47512b6aa52312792
JHyun <wogustkfkd11@gmail.com>
2022-11-21 14:46:53 +0900

    BridgeGame - user's move & Retry or Quit commit

--- src/main/java/bridge/BridgeGame.java
+++ src/main/java/bridge/BridgeGame.java
@@ -10,7 +10,7 @@ public class BridgeGame {
      * <p>
      * 이동을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public String move() {
+    public String move() { // U or D
         InputView input = new InputView();
         System.out.println("이동할 칸을 선택해주세요. (위: U, 아래: D)");
         return input.readMoving();
@@ -22,7 +22,7 @@ public class BridgeGame {
      * <p>
      * 재시작을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public String retry(String command) {
+    public String retry(String command) { // R or Q or ERROR
         if (command.equals("R")) {
             return "R";
         } else if (command.equals("Q")) {
