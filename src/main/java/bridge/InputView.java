32c2c1e805bc7b47510828a6dba480c70c41d341
tjddnr7760 <tjddnr7760@naver.com>
2022-11-21 16:03:57 +0900

    feat : OutputView 기능목록 구현

--- src/main/java/bridge/InputView.java
+++ src/main/java/bridge/InputView.java
@@ -27,7 +27,7 @@ public class InputView {
 
     //사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
     public String readGameCommand() {
-        System.out.println("게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)")
+        System.out.println("게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)");
         String exittype = Console.readLine();
         return exittype;
     }
