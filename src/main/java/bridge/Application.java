77afab3dde700e83a14373cc615c5025e6ad8164
kjw6867 <93850235+kjw6867@users.noreply.github.com>
2022-11-22 02:30:13 +0900

    기능 구현

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -1,8 +1,58 @@
 package bridge;
 
-public class Application {
+import java.util.ArrayList;
+import java.util.List;
 
+public class Application {
+    static BridgeGame bridgeGame;
+    static OutputView outputView;
+    static BridgeMaker bridgeMaker;
+    static InputView inputView;
+
+    public static void define() {
+        bridgeGame = new BridgeGame();
+        outputView = new OutputView(bridgeGame);
+        bridgeMaker = new BridgeMaker(new BridgeRandomNumberGenerator());
+        inputView = new InputView();
+    }
+    public static void doGame() {
+
+    }
+    public static int wrongMove(int size) {
+        String command = inputView.readGameCommand();
+        if(command.equals("R")) {
+            return -1;
+        }
+        return size + 1;
+    }
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        int size;
+        boolean isSuccess = false;
+        String userMove = null;
+        List<String> bridgeInfo = new ArrayList<>();
+
+        define();
+        size = inputView.readBridgeSize();
+        bridgeInfo = bridgeMaker.makeBridge(size);
+
+        //dogame
+        for(int i = 0; i < size; i++){
+
+            userMove = inputView.readMoving();
+            outputView.printMap(bridgeInfo, i, userMove);
+
+            if(bridgeGame.move(bridgeInfo, userMove, i)) {
+                if(i == size-1) {
+                    isSuccess = true;
+                }
+                continue;
+            }
+            if(!bridgeGame.move(bridgeInfo, userMove, i)) {
+                i = wrongMove(size);
+            }
+        }
+        outputView.printResult(isSuccess, bridgeInfo, size-1, userMove, bridgeGame);
     }
+
 }
