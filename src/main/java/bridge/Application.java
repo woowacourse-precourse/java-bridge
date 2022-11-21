e222e304a6d1dd539d5fe718afafeedc85aecccc
limjimin <dkdldsla0000@naver.com>
2022-11-22 02:29:56 +0900

    Feat: 다리 건너기 프로그램 구현

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -1,8 +1,20 @@
 package bridge;
 
+import bridge.controller.GameController;
+import bridge.domain.player.Player;
+import bridge.view.ExceptionView;
+
 public class Application {
 
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        Player player = new Player();
+        BridgeMaker bridgeMaker = new BridgeMaker(new BridgeRandomNumberGenerator());
+        GameController gameController = new GameController(player, bridgeMaker);
+
+        try {
+            gameController.run();
+        } catch (IllegalArgumentException e) {
+            ExceptionView.print(e.getMessage());
+        }
     }
 }
