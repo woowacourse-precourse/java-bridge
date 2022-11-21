3bdcf3d1363aa8ea58402031eb4ca651b8585fc6
wlsh44 <k87913j@gmail.com>
2022-11-22 01:21:27 +0900

    style: 안 쓰는 코드 제거 및 제공된 주석 제거

--- src/main/java/bridge/domain/game/BridgeGame.java
+++ src/main/java/bridge/domain/game/BridgeGame.java
@@ -1,16 +1,12 @@
 package bridge.domain.game;
 
 import bridge.domain.GameStatus;
-import bridge.domain.Move;
 import bridge.domain.bridge.Bridge;
 
 import static bridge.domain.GameStatus.FAILED;
 import static bridge.domain.GameStatus.PLAYING;
 import static bridge.domain.GameStatus.SUCCESS;
 
-/**
- * 다리 건너기 게임을 관리하는 클래스
- */
 public class BridgeGame {
 
     private final Bridge bridge;
@@ -23,11 +19,6 @@ public class BridgeGame {
         this.attempt = new Attempt();
     }
 
-    /**
-     * 사용자가 칸을 이동할 때 사용하는 메서드
-     * <p>
-     * 이동을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
-     */
     public GameStatus move(String move) {
         GameStatus status = moveAndGetStatus(move);
         if (isSuccessToCrossBridge(status)) {
@@ -54,11 +45,6 @@ public class BridgeGame {
         return bridge.getBridgeBlock(position).equals(moving);
     }
 
-    /**
-     * 사용자가 게임을 다시 시도할 때 사용하는 메서드
-     * <p>
-     * 재시작을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
-     */
     public GameStatus retry() {
         attempt.addAttempt();
         gameProgress.clear();
--- src/main/java/bridge/view/InputView.java
+++ src/main/java/bridge/view/InputView.java
@@ -4,18 +4,12 @@ import camp.nextstep.edu.missionutils.Console;
 
 import static bridge.support.ErrorMessage.INVALID_INPUT;
 
-/**
- * 사용자로부터 입력을 받는 역할을 한다.
- */
 public class InputView {
 
     private static final String READ_BRIDGE_SIZE = "다리 길이를 입력해주세요.";
     private static final String READ_MOVING = "이동할 칸을 선택해주세요. (위: U, 아래: D)";
     private static final String READ_GAME_COMMAND = "게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)";
 
-    /**
-     * 다리의 길이를 입력받는다.
-     */
     public int readBridgeSize() {
         System.out.println(READ_BRIDGE_SIZE);
         return toInteger(Console.readLine());
@@ -29,17 +23,11 @@ public class InputView {
         }
     }
 
-    /**
-     * 사용자가 이동할 칸을 입력받는다.
-     */
     public String readMoving() {
         System.out.println(READ_MOVING);
         return Console.readLine();
     }
 
-    /**
-     * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
-     */
     public String readGameCommand() {
         System.out.println(READ_GAME_COMMAND);
         return Console.readLine();
