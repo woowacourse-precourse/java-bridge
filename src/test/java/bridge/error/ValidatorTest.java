eb1b809e38fc54b42e0720ecf49c4816041393ee
seoyoung <sy9723@korea.ac.kr>
2022-11-21 09:43:56 +0900

    test: 예외 처리 테스트 추가 ([다리길이] 입력 값이 숫자가/3이상 20이하가 아닌 경우)

---
+++ src/test/java/bridge/error/ValidatorTest.java
@@ -0,0 +1,27 @@
+package bridge.error;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+class ValidatorTest {
+
+    @DisplayName("다리 길이에 대한 예외 처리")
+    @ValueSource(strings = {"1", "2", "21", "999", "aaa", "bridge!@#"})
+    @ParameterizedTest
+    void validateBridgeSize(String bridgeSize) {
+        assertThatThrownBy(() -> Validator.validateBridgeSize(bridgeSize))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void validateMoving() {
+    }
+
+    @Test
+    void validateGameCommand() {
+    }
+}
