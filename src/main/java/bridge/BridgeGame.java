1c2eb48e037a0716cc682757da7c01e306aa77e9
jwPark6 <67006945+jwPark6@users.noreply.github.com>
2022-11-21 23:43:33 +0900

    feat : 칸 이동 구현 및 해당 기능 분할

--- src/main/java/bridge/BridgeGame.java
+++ src/main/java/bridge/BridgeGame.java
@@ -12,7 +12,7 @@ public class BridgeGame {
         this.secondLine = secondLine;
     }
 
-    public void resetAllLine(){
+    public void resetAllLine() {
         this.firstLine = "";
         this.secondLine = "";
     }
@@ -25,19 +25,38 @@ public class BridgeGame {
         return secondLine;
     }
 
-    public void setLine(String stringOfFirstLine, String stringOfSecondLine){
+    public void setLine(String stringOfFirstLine, String stringOfSecondLine) {
         firstLine += stringOfFirstLine;
         secondLine += stringOfSecondLine;
     }
-
-
 
     /**
      * 사용자가 칸을 이동할 때 사용하는 메서드
      * <p>
      * 이동을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void move() {
+    public boolean move(String elementOfBridge, String input) {
+        String setString = determineSetString(elementOfBridge, input);
+        if (checkAndSetLine(setString, input)) {
+            return true;
+        }
+        return false;
+    }
+
+    public String determineSetString(String elementOfBridge, String input) {
+        if (elementOfBridge.equals(input)) {
+            return "O";
+        }
+        return "X";
+    }
+
+    public boolean checkAndSetLine(String setString, String input) {
+        if (input.equals("U")) {
+            setLine("setString", " ");
+            return true;
+        }
+        setLine(" ", "setString");
+        return false;
     }
 
     /**
@@ -48,7 +67,4 @@ public class BridgeGame {
     public void retry() {
     }
 
-    public void saveGame(){
-
-    }
 }
