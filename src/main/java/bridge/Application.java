665dd82e4bb59c52ab284b79d5969649577233b2
jwp345 <jwp135@naver.com>
2022-11-21 16:22:07 +0900

    fix : InputView 객체가 한번만 생성되도록 변경

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -5,8 +5,9 @@ import java.util.List;
 public class Application {
 
     public static void main(String[] args) {
-        int len = new InputView().readSize();
+        InputView inputView = new InputView();
+        int len = inputView.readSize();
         List<String> bridge = new BridgeMaker(new BridgeRandomNumberGenerator()).makeBridge(len);
-        new BridgeGame(new InputView(), new OutputView(), bridge).move(1);
+        new BridgeGame(inputView, new OutputView(), bridge).move(1);
     }
 }
