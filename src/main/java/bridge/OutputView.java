bdb75e74101675e5d90256c888494fea471bed2b
Eeap <kdg97811@naver.com>
2022-11-21 18:51:58 +0900

    feat(OutputView): add func print PresentBridgeState

--- src/main/java/bridge/OutputView.java
+++ src/main/java/bridge/OutputView.java
@@ -1,5 +1,7 @@
 package bridge;
 
+import java.util.List;
+
 /**
  * 사용자에게 게임 진행 상황과 결과를 출력하는 역할을 한다.
  */
@@ -10,9 +12,24 @@ public class OutputView {
      * <p>
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void printMap() {
+    public void printMap(List<List<String>> userMap) {
+        printUpDown(0, userMap);
+        printUpDown(1,userMap);
+        System.out.println();
     }
-
+    public void printUpDown(int upDown, List<List<String>> userMap) {
+        System.out.print("[ ");
+        for (int cnt = 0; cnt < userMap.get(upDown).size(); cnt++) {
+            System.out.print(userMap.get(upDown).get(cnt));
+            printSlash(cnt, userMap.get(upDown).size());
+        }
+        System.out.println(" ]");
+    }
+    public void printSlash(int cnt, int size) {
+        if (cnt != size - 1) {
+            System.out.print(" | ");
+        }
+    }
     /**
      * 게임의 최종 결과를 정해진 형식에 맞춰 출력한다.
      * <p>
