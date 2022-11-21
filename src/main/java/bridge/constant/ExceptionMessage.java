7c1afac3e4d2a2cc387051fa879390e530671c08
leeyongjun <leeyongjun0310@naver.com>
2022-11-21 23:27:20 +0900

    refactor(bridge): 예외 오류 메시지 상수로 변경

--- src/main/java/bridge/constant/ExceptionMessage.java
+++ src/main/java/bridge/constant/ExceptionMessage.java
@@ -1,4 +1,7 @@
 package bridge.constant;
 
 public class ExceptionMessage {
+    public static final String ERROR_BRIDGE_LENGTH = "[ERROR] 다리의 길이 입력 오류";
+    public static final String ERROR_MOVING = "[ERROR] 이동할 칸 입력 오류";
+    public static final String ERROR_GAME_COMMAND = "[ERROR] 게임을 다시 시도할지 입력 오류";
 }
--- src/main/java/bridge/verifier/InputVerifier.java
+++ src/main/java/bridge/verifier/InputVerifier.java
@@ -1,5 +1,7 @@
 package bridge.verifier;
 
+import bridge.constant.ExceptionMessage;
+
 public class InputVerifier {
     private static final String REGEX_BRIDGE_SIZE_RANGE = "^[3-9]{1}$|^1{1}[0-9]{1}$|^2{1}0{1}$";
     private static final String MOVING_UP = "U";
@@ -9,19 +11,19 @@ public class InputVerifier {
 
     public static void BridgeSize(String bridgeSize) {
         if (!bridgeSize.matches(REGEX_BRIDGE_SIZE_RANGE)) {
-            throw new IllegalArgumentException("[ERROR] 다리의 길이 입력 오류");
+            throw new IllegalArgumentException(ExceptionMessage.ERROR_BRIDGE_LENGTH);
         }
     }
 
     public static void moving(String moving) {
         if (!(moving.equals(MOVING_UP) || moving.equals(MOVING_DOWN))) {
-            throw new IllegalArgumentException("[ERROR] 이동할 칸 입력 오류");
+            throw new IllegalArgumentException(ExceptionMessage.ERROR_MOVING);
         }
     }
 
     public static void gameCommand(String gameCommand) {
         if (!(gameCommand.equals(GameCommand_RESTART) || gameCommand.equals(GameCommand_QUIT))) {
-            throw new IllegalArgumentException("[ERROR] 게임을 다시 시도할지 입력 오류");
+            throw new IllegalArgumentException(ExceptionMessage.ERROR_GAME_COMMAND);
         }
     }
 
