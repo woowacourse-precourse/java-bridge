aee92783b83b3819bd90ab50d5bc1cac51357959
nakalic <nakalic28@gmail.com>
2022-11-22 03:46:39 +0900

    test(RetryInputValidatorTest): 재시작 여부 입력값을 검증하는 테스트 코드 추가

---
+++ src/test/java/bridge/validator/RetryInputValidatorTest.java
@@ -0,0 +1,20 @@
+package bridge.validator;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.*;
+
+import bridge.constant.ValidatorMessage;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+class RetryInputValidatorTest {
+    @DisplayName("입력이 Q나 R이 아니라면 예외를 발생시킨다.")
+    @ParameterizedTest
+    @ValueSource(strings = {"r", " ", "ㅁ", "1",})
+    void createNotUorD(String input) {
+        assertThatThrownBy(() -> RetryInputValidator.validateRetryInput(input))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(ValidatorMessage.RETRY_INPUT_IS_R_OR_Q.toString());
+    }
+}
