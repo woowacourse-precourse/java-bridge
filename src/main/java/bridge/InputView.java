950941638a423930f07532ece8acaf6e3ca3ca7e
kim <kajinori@gachon.ac.kr>
2022-11-21 11:10:40 +0900

    feat(InputView): 이동할 칸, 게임 재시도& 종료 선택 입력받는 기능 구현

--- src/main/java/bridge/InputView.java
+++ src/main/java/bridge/InputView.java
@@ -2,31 +2,22 @@ package bridge;
 
 import camp.nextstep.edu.missionutils.Console;
 
-/**
- * 사용자로부터 입력을 받는 역할을 한다.
- */
 public class InputView {
-
-    /**
-     * 다리의 길이를 입력받는다.
-     */
-    public int readBridgeSize() {
+    public String readBridgeSize() {
         System.out.println("다리의 길이를 입력해주세요.");
         String inputBridgeSize = Console.readLine();
-        return Integer.parseInt(inputBridgeSize);
+        return inputBridgeSize;
     }
 
-    /**
-     * 사용자가 이동할 칸을 입력받는다.
-     */
     public String readMoving() {
-        return null;
+        System.out.println("이동할 칸을 선택해주세요. (위: U, 아래: D)");
+        String inputUpDown = Console.readLine();
+        return inputUpDown;
     }
 
-    /**
-     * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
-     */
     public String readGameCommand() {
-        return null;
+        System.out.println("게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)");
+        String inputRQ = Console.readLine();
+        return inputRQ;
     }
 }
