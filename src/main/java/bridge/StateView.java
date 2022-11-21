a692bb4d4216f4d604d32dbab4c82965ec58edc5
ckn1070 <ckn1070@naver.com>
2022-11-21 22:31:53 +0900

    fix(bridge): modify error messages that commands are not uppercase

--- src/main/java/bridge/StateView.java
+++ src/main/java/bridge/StateView.java
@@ -16,9 +16,9 @@ enum StateMessage {
     ERROR("[ERROR]"),
     ERROR_BRIDGE_SIZE(" 다리 길이는 3부터 20 사이의 숫자여야 합니다."),
     ERROR_NOT_MOVING_COMMAND(" 이동할 칸은 위 (U) 혹은 아래 (D)를 선택헤 주세요."),
-    ERROR_LOWER_MOVING_COMMAND(" 이동할 칸은 위 (U) 혹은 아래 (D)를 대문자로 주세요."),
+    ERROR_LOWER_MOVING_COMMAND(" 이동할 칸은 위 (U) 혹은 아래 (D)를 대문자로 입력해 주세요."),
     ERROR_NOT_RETRY_COMMAND(" 재시도 (R) 혹은 종료 (Q)를 선택해 주세요."),
-    ERROR_LOWER_RETRY_COMMAND(" 재시도 (R) 혹은 종료 (Q)를 선택해 주세요.");
+    ERROR_LOWER_RETRY_COMMAND(" 재시도 (R) 혹은 종료 (Q)를 대문자로 입력해 주세요.");
 
     private final String message;
 
