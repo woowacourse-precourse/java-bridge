51f250e2e1ee416fdb55f894d82719a611f93f83
Kijung Park <kjpark4321@naver.com>
2022-11-22 00:11:53 +0900

    feat: OutputView 클래스의 printMap 메서드 구현

--- src/main/java/bridge/OutputView.java
+++ src/main/java/bridge/OutputView.java
@@ -1,5 +1,8 @@
 package bridge;
 
+import java.util.List;
+import java.util.Objects;
+
 /**
  * 사용자에게 게임 진행 상황과 결과를 출력하는 역할을 한다.
  */
@@ -10,7 +13,26 @@ public class OutputView {
      * <p>
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void printMap() {
+    public void printMap(List<String> curMap, List<String> answerMap) {
+        StringBuilder upperMap = new StringBuilder("[ ");
+        StringBuilder lowerMap = new StringBuilder("[ ");
+
+        for (int i = 0; i < curMap.size(); i++) {
+            if (matchInputAndAnswer(curMap.get(i), answerMap.get(i)) == 0) {
+                upperMap.append(" O");
+            } else if (matchInputAndAnswer(curMap.get(i), answerMap.get(i)) == 1) {
+                upperMap.append(" X");
+            } else if (matchInputAndAnswer(curMap.get(i), answerMap.get(i)) == 2) {
+                lowerMap.append(" O");
+            } else {
+                lowerMap.append(" X");
+            }
+        }
+        upperMap.append(" ]");
+        lowerMap.append(" ]");
+
+        System.out.println(upperMap);
+        System.out.println(lowerMap);
     }
 
     /**
@@ -19,5 +41,26 @@ public class OutputView {
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
     public void printResult() {
+    }
+
+    /**
+     * @return 0: U 방향이면서 정답
+     * 1: U 방향이면서 오답
+     * 2: D 방향이면서 정답
+     * 3: D 방향이면서 오답
+     */
+    private int matchInputAndAnswer(String input, String answer) {
+        if (Objects.equals(input, "U")) {
+            if (input.equals(answer)) {
+                return 0;
+            }
+            return 1;
+        }
+        if (Objects.equals(input, "D")) {
+            if (input.equals(answer)) {
+                return 2;
+            }
+            return 3;
+        }
     }
 }
