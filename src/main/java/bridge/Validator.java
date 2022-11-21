ce97d686ce4bd9d6a1b6ddb76dfc528837076350
Jeangyu <gjwlsrb1700@naver.com>
2022-11-21 16:02:07 +0900

    [FIX: bridge] 문자열 비교를 효과적으로 진행

--- src/main/java/bridge/Validator.java
+++ src/main/java/bridge/Validator.java
@@ -8,13 +8,13 @@ public class Validator {
     }
 
     public static void isMoveAlpha(String alpha){
-        if (!(alpha.charAt(0) == 'D' || alpha.charAt(0) == 'U')){
+        if (!(alpha.equals("D") || alpha.equals("U"))){
             throw new IllegalArgumentException("[ERROR] 이동할 칸은 U, D의 입력만 가능합니다.");
         }
     }
 
     public static void isRestart(String alpha){
-        if(!(alpha.charAt(0) == 'R' || alpha.charAt(0) == 'Q')){
+        if(!(alpha.equals("R") || alpha.equals("Q"))){
             throw new IllegalArgumentException("[ERROR] 재시작은 R, Q의 입력만 가능합니다.");
         }
     }
