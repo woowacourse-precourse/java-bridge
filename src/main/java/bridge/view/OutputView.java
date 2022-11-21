f2eaad885c8f2585d55526eb22e94c385ba87937
jingyu9804 <swc05161@gmail.com>
2022-11-21 21:15:24 +0900

    feat(bridge) : OutputView 출력 기능 구현

--- src/main/java/bridge/view/OutputView.java
+++ src/main/java/bridge/view/OutputView.java
@@ -1,5 +1,9 @@
 package bridge.view;
 
+import bridge.domain.MessageStorage;
+
+import java.util.List;
+
 /**
  * 사용자에게 게임 진행 상황과 결과를 출력하는 역할을 한다.
  */
@@ -8,23 +12,109 @@ public class OutputView {
     private static final String FINAL_GAME_RESULT = "최종 게임 결과";
     private static final String GAME_SUCCESS_RESULT = "게임 성공 여부";
     private static final String TOTAL_TRY_TIME = "총 시도한 횟수";
+    private static final String BRIDGE_START = "[ ";
+    private static final String BRIDGE_NONE = "  ";
+    private static final String BRIDGE_MID = " | ";
+    private static final String BRIDGE_END = " ]";
+    private static final String UP = "U";
+    private static final String DOWN = "D";
+    private static final String NONE = " ";
+    private static final String CORRECT = "O";
+    private static final String IN_CORRECT = "X";
     /**
      * 현재까지 이동한 다리의 상태를 정해진 형식에 맞춰 출력한다.
      * <p>
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public static void printMap() {
+    public static void printMap(List<List<String>> bridgeCorrects) {
+        brSize1(bridgeCorrects);
+        brSize2Plus(bridgeCorrects);
     }
 
+    private static void brSize1(List<List<String>> bridgeCorrects){
+        int bridgeSize = bridgeCorrects.size();
+        if(bridgeSize==1){
+            System.out.print(BRIDGE_START);
+            up(bridgeCorrects,0);
+            System.out.println(BRIDGE_END);
+
+            System.out.print(BRIDGE_START);
+            down(bridgeCorrects,0);
+            System.out.println(BRIDGE_END);
+        }
+    }
+
+    private static void brSize2Plus(List<List<String>> bridgeCorrects){
+        int bridgeSize = bridgeCorrects.size();
+        if(bridgeSize>=2){
+            System.out.print(BRIDGE_START);
+            duplicatedWork1(bridgeCorrects,bridgeSize);
+            duplicatedWork2(bridgeCorrects,bridgeSize);
+            System.out.println(BRIDGE_END);
+        }
+    }
+
+    public static void duplicatedWork1(List<List<String>> bridgeCorrects,int bridgeSize){
+        for(int i=0;i<bridgeSize-1;i++){
+            up(bridgeCorrects,i);
+            System.out.print(BRIDGE_MID);
+        }
+        up(bridgeCorrects,bridgeSize-1);
+        System.out.println(BRIDGE_END);
+    }
+
+    public static void duplicatedWork2(List<List<String>> bridgeCorrects,int bridgeSize){
+        System.out.print(BRIDGE_START);
+        for(int i=0;i<bridgeSize-1;i++){
+            down(bridgeCorrects,i);
+            System.out.print(BRIDGE_MID);
+        }
+        down(bridgeCorrects,bridgeSize-1);
+    }
+
+
+    private static void up(List<List<String>> bridgeCorrects,int i){
+        String isUp = bridgeCorrects.get(i).get(0);
+        String isCorrect = bridgeCorrects.get(i).get(1);
+        if(isUp.equals(UP) && isCorrect.equals(CORRECT)){
+            System.out.print(CORRECT);
+            return;
+        }
+        if(isUp.equals(UP) && isCorrect.equals(IN_CORRECT)){
+            System.out.print(IN_CORRECT);
+            return;
+        }
+        System.out.print(NONE);
+    }
+
+    private static void down(List<List<String>> bridgeCorrects,int i){
+        String isDown = bridgeCorrects.get(i).get(0);
+        String isCorrect = bridgeCorrects.get(i).get(1);
+        if(isDown.equals(DOWN) && isCorrect.equals(CORRECT)){
+            System.out.print(CORRECT);
+            return;
+        }
+        if(isDown.equals(DOWN) && isCorrect.equals(IN_CORRECT)){
+            System.out.print(IN_CORRECT);
+            return;
+        }
+        System.out.print(NONE);
+    }
     /**
      * 게임의 최종 결과를 정해진 형식에 맞춰 출력한다.
      * <p>
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public static void printResult() {
+    public static void printResult(List<List<String>> bridgeCorrects,String isSuccess,int cnt) {
+        System.out.println(FINAL_GAME_RESULT);
+        printMap(bridgeCorrects);
+        System.out.println();
+        System.out.println(GAME_SUCCESS_RESULT+": "+isSuccess);
+        System.out.println(TOTAL_TRY_TIME+": "+cnt);
     }
 
     public static void printGameStart(){
         System.out.println(GAME_START_MESSAGE);
     }
+
 }
