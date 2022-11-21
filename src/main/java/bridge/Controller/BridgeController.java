caff240b1b7895b92acf08a7a9719290f0ffd134
EUNSH1 <tkdgus968@naver.com>
2022-11-21 12:29:31 +0900

    test : 사용자가 선택한 다리가 이동할 수 있다면 O, 아니면 X 출력 테스트

--- src/main/java/bridge/Controller/BridgeController.java
+++ src/main/java/bridge/Controller/BridgeController.java
@@ -1,11 +1,17 @@
 package bridge.Controller;
 
+import bridge.BridgeGame;
+import bridge.BridgeMaker;
+import bridge.BridgeRandomNumberGenerator;
 import bridge.InputView;
 import bridge.OutputView;
 
 public class BridgeController {
 
-    private static InputView inputView = new InputView();
+    private static final InputView inputView = new InputView();
+    private static final OutputView outputView = new OutputView();
+
+
     public void start(){
         GameStartMessage();
         GameTest();
@@ -16,7 +22,20 @@ public class BridgeController {
     }
 
     public void GameTest(){
-        inputView.readBridgeSize();
-        inputView.readMoving();
+        BridgeGame bridgeGame = createBridgeGame(inputView);
+        inputMoving(inputView,outputView,bridgeGame);
+    }
+    private static BridgeGame createBridgeGame(InputView inputView) {
+        return new BridgeGame(new BridgeMaker(new BridgeRandomNumberGenerator())
+                .makeBridge(inputView.readBridgeSize()));
+    }
+
+    private static void inputMoving(InputView inputView, OutputView outputView, BridgeGame bridgeGame) {
+        if (bridgeGame.move(inputView.readMoving())) {
+            System.out.println(outputView.printMap(bridgeGame.currentBridge()));
+        }else{
+            System.out.println(outputView.printMap(bridgeGame.currentBridge()));
+        }
+
     }
 }
