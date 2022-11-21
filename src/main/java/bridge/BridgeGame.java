ebfc5b82028281d03caf8dbf487586018b8ad43f
RumbleKAT <reki318@naver.com>
2022-11-21 22:53:20 +0900

    (feat) 5. 다리 게임 객체 구현 1) 사용자가 칸을 이동할때 로직처리 2) 사용자가 게임을 다시 시도시 로직처리

--- src/main/java/bridge/BridgeGame.java
+++ src/main/java/bridge/BridgeGame.java
@@ -1,5 +1,6 @@
 package bridge;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -9,20 +10,37 @@ public class BridgeGame {
 
     private final BridgeNumberGenerator bridgeMaker;
     private List<String> bridge;
+    private List<String> userBridge;
     private int tryCnt;
 
     public BridgeGame(BridgeNumberGenerator bridgeMaker){
         this.bridgeMaker = bridgeMaker;
         tryCnt = 0;
+        userBridge = new ArrayList<>();
+    }
+
+    public void setUserBridge(List<String> bridge){
+        this.userBridge = bridge;
+    }
+
+    public List<String> getUserBridge(){
+        return this.userBridge;
     }
 
     public void setBridge(List<String> bridge){
         this.bridge = bridge;
     }
 
-    private String convertStep(int step){
-        if(step == 1) return "U";
-        return "D";
+    public List<String> getBridge(){
+        return this.bridge;
+    }
+
+    public boolean isFinish(){
+        if(this.getBridge().size() != this.getUserBridge().size()) return false;
+        for(int i =0;i<this.getBridge().size();i++){
+            if(!this.getBridge().get(i).equals(this.getUserBridge().get(i))) return false;
+        }
+        return true;
     }
 
     public int getTryCnt(){
@@ -33,17 +51,27 @@ public class BridgeGame {
         this.tryCnt += cnt;
     }
 
+    public char[][] calcMap(List<String> map, int row, int col){
+        char [][] bridgeMap = new char[2][map.size()];
+        for(int i=0;i<map.size();i++){
+            if(i>0 && map.get(i).equals("X")){
+                bridgeMap[row][col] = 'X';
+                break;
+            }
+            if(map.get(i).equals("D") && row == 0) row++;
+            if(map.get(i).equals("U") && row == 1) row--;
+            bridgeMap[row][col] = 'O';
+            col++;
+        }return bridgeMap;
+    }
+
     /**
      * 사용자가 칸을 이동할 때 사용하는 메서드
      * <p>
      * 이동을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public boolean move(int size,String curStep) {
-        for(int i=0;i<size;i++){
-            System.out.println("이동할 칸을 선택해주세요. (위: U, 아래: D)");
-            if(!curStep.equals(this.bridge.get(i))) return false;
-        }
-        return true;
+    public boolean move(int index, String curStep) {
+        return curStep.equals(this.getBridge().get(index));
     }
 
     /**
