9f4626da37ce867f0f432fa873ee79dcc03fce83
NAM GIBEOM <rlqja0523@naver.com>
2022-11-22 02:14:35 +0900

    feat: 이동할 칸을 입력받는 메서드 구현

--- src/main/java/bridge/view/InputView.java
+++ src/main/java/bridge/view/InputView.java
@@ -1,6 +1,7 @@
 package bridge.view;
 
 import bridge.exception.BridgeSizeException;
+import bridge.exception.MovingException;
 import bridge.exception.UserException;
 import camp.nextstep.edu.missionutils.Console;
 
@@ -24,7 +25,9 @@ public class InputView {
      * 사용자가 이동할 칸을 입력받는다.
      */
     public String readMoving() {
-        return null;
+        System.out.println("이동할 칸을 선택해주세요. (위: U, 아래: D)");
+        UserException movingException = new MovingException();
+        return checkInput(movingException);
     }
 
     /**
