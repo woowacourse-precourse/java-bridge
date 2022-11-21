798e3c30732c1e7d2c4cdb84112fca748d3bc3c9
kdkdhoho <hkim4410@naver.com>
2022-11-21 17:33:50 +0900

    refactor(Message): Error 메시지의 중복 제거를 위한 리팩토링

--- src/main/java/bridge/model/constant/Message.java
+++ src/main/java/bridge/model/constant/Message.java
@@ -5,10 +5,10 @@ public class Message {
     public static final String INPUT_BRIDGE_LENGTH = "다리의 길이를 입력해주세요.";
     public static final String INPUT_MOVING = "\n이동할 칸을 선택해주세요. (위: U, 아래: D)";
     public static final String INPUT_RETRY_COMMAND = "게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)";
-    public static final String ERROR_PREFIX = "[ERROR] ";
-    public static final String ERROR_BRIDGE_SIZE = "잘못 입력했습니다. 3 이상 20 이하의 숫자를 입력해주세요.";
-    public static final String ERROR_MOVING = "잘못 입력했습니다. U 또는 D를 입력해주세요.";
-    public static final String ERROR_RETRY = "잘못 입력했습니다. R 또는 Q를 입력해주세요.";
+    public static final String ERROR_PREFIX = "[ERROR] 잘못 입력했습니다.";
+    public static final String ERROR_BRIDGE_SIZE = "3 이상 20 이하의 숫자를 입력해주세요.";
+    public static final String ERROR_MOVING = "U 또는 D를 입력해주세요.";
+    public static final String ERROR_RETRY = "R 또는 Q를 입력해주세요.";
     public static final String GAME_RESULT_MESSAGE = "\n최종 게임 결과";
 
     public static final String CORRECT_LOG = " O |";
