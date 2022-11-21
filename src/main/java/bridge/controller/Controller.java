9a26d07eb4ddb5d014ff0a882a2f34c65ffa9eae
areyouhun <areyouhun@gmail.com>
2022-11-22 04:15:43 +0900

    refactor(controller): play 메소드 분리

--- src/main/java/bridge/controller/Controller.java
+++ src/main/java/bridge/controller/Controller.java
@@ -13,12 +13,14 @@ import java.util.List;
 
 public class Controller {
     private static final int INITIAL_COUNT = 1;
-    private static final String BLANK_SPACE = " ";
     private static final String SUCCESS = "성공";
     private static final String FAILURE = "실패";
 
     private final List<String> answerBridge;
     private final BridgeGame bridgeGame;
+
+    private final OneSideResults upsideResults;
+    private final OneSideResults downsideResults;
 
     private int totalTrialCount;
     private String finalResult;
@@ -29,6 +31,8 @@ public class Controller {
         bridgeGame = new BridgeGame();
         totalTrialCount = INITIAL_COUNT;
         finalResult = SUCCESS;
+        upsideResults = new UpsideResults();
+        downsideResults = new DownsideResults();
     }
 
     private List<String> createAnswerBridge(int bridgeSize) {
@@ -39,40 +43,41 @@ public class Controller {
     }
 
     public void play() {
-        final OneSideResults upsideResults = new UpsideResults();
-        final OneSideResults downsideResults = new DownsideResults();
-
         for (int index = 0; index < answerBridge.size(); index++) {
             final String playerMove = InputView.readMoving();
-            final String answerMove = answerBridge.get(index);
-            final String matchResult = bridgeGame.move(playerMove, answerMove);
-            upsideResults.update(playerMove, matchResult);
-            downsideResults.update(playerMove, matchResult);
+            final String matchResult = bridgeGame.move(playerMove, answerBridge.get(index));
+            updateResults(playerMove, matchResult);
             OutputView.printMap(upsideResults, downsideResults);
 
             if (matchResult.equals("X")) {
-                final String playerCommand = InputView.readGameCommand();
-                if (CommandKeys.isRetry(playerCommand)) {
-                    index = -1;
-                    resetResults(upsideResults, downsideResults, playerCommand);
-                    totalTrialCount = bridgeGame.retry(totalTrialCount);
-                }
-                if (CommandKeys.isQuit(playerCommand)) {
-                    finalResult = FAILURE;
-                    break;
-                }
+                index = setIndex(index);
             }
         }
         OutputView.printResult(upsideResults, downsideResults, finalResult);
         OutputView.printTotalTrialCount(totalTrialCount);
     }
 
-    private void resetResults(
-            OneSideResults oneSideResults,
-            OneSideResults theOtherSideResults,
-            String playerCommand
-    ) {
-        oneSideResults.reset(playerCommand);
-        theOtherSideResults.reset(playerCommand);
+    private void updateResults(String playerMove, String matchResult) {
+        upsideResults.update(playerMove, matchResult);
+        downsideResults.update(playerMove, matchResult);
+    }
+
+    private int setIndex(int index) {
+        final String playerCommand = InputView.readGameCommand();
+        if (CommandKeys.isRetry(playerCommand)) {
+            index = -1;
+            resetResults(playerCommand);
+            totalTrialCount = bridgeGame.retry(totalTrialCount);
+        }
+        if (CommandKeys.isQuit(playerCommand)) {
+            finalResult = FAILURE;
+            index = answerBridge.size() - 1;
+        }
+        return index;
+    }
+
+    private void resetResults(String playerCommand) {
+        upsideResults.reset(playerCommand);
+        downsideResults.reset(playerCommand);
     }
 }
