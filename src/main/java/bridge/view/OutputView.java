390746250d15ae1cd56540bea43ad35c0e02f19e
Miseong Kim <miseongk@naver.com>
2022-11-22 00:32:53 +0900

    refactor(OutputView): change message variable to static final

--- src/main/java/bridge/view/OutputView.java
+++ src/main/java/bridge/view/OutputView.java
@@ -9,18 +9,18 @@ import java.util.List;
  */
 public class OutputView {
 
-    private final String START_GAME_MESSAGE = "다리 건너기 게임을 시작합니다.";
-    private final String INPUT_BRIDGE_SIZE_MESSAGE = "\n다리의 길이를 입력해주세요.";
-    private final String INPUT_MOVE_TO_MESSAGE = "이동할 칸을 선택해주세요. (위: U, 아래: D)";
-    private final String INPUT_RETRY_MESSAGE = "게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)";
-    private final String FINAL_GAME_RESULT_GUIDE_MESSAGE = "최종 게임 결과";
-    private final String GAME_RESULT_MESSAGE = "게임 성공 여부: ";
-    private final String TOTAL_NUMBER_OF_TRIAL_MESSAGE = "총 시도한 횟수: ";
-    private final String SUCCESS = "성공";
-    private final String FAILED = "실패";
-    private final String START_POINT = "[ ";
-    private final String END_POINT = " ]";
-    private final String DELIMITER = " | ";
+    private static final String START_GAME_MESSAGE = "다리 건너기 게임을 시작합니다.";
+    private static final String INPUT_BRIDGE_SIZE_MESSAGE = "\n다리의 길이를 입력해주세요.";
+    private static final String INPUT_MOVE_TO_MESSAGE = "이동할 칸을 선택해주세요. (위: U, 아래: D)";
+    private static final String INPUT_RETRY_MESSAGE = "게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)";
+    private static final String FINAL_GAME_RESULT_GUIDE_MESSAGE = "최종 게임 결과";
+    private static final String GAME_RESULT_MESSAGE = "게임 성공 여부: ";
+    private static final String TOTAL_NUMBER_OF_TRIAL_MESSAGE = "총 시도한 횟수: ";
+    private static final String SUCCESS = "성공";
+    private static final String FAILED = "실패";
+    private static final String START_POINT = "[ ";
+    private static final String END_POINT = " ]";
+    private static final String DELIMITER = " | ";
 
     /**
      * 현재까지 이동한 다리의 상태를 정해진 형식에 맞춰 출력한다.
