2a9fb05b1cf92fa330fa2c555c26aabf8b6b7bb6
rabbit0216 <rabbit1999k@gmail.com>
2022-11-21 15:07:31 +0900

    refactor: main함수 간소화

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -1,28 +1,37 @@
 package bridge;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-
 import static bridge.Constants.*;
 
 public class Application {
     static OutputView outputView = new OutputView();
     static InputView inputView = new InputView();
+    static BridgeGame bridgeGame;
+    static int bridgeSize;
+
+    static void init(){
+        outputView.printInit();
+        bridgeSize = inputView.readBridgeSize();
+        bridgeGame = new BridgeGame(bridgeSize);
+    }
+
+    static int isRetry(String result, int size){
+        if(result.equals(CANNOT_CROSS)){
+            outputView.printRetry();
+            String command = inputView.readGameCommand();
+            size = bridgeGame.retry(command, bridgeSize);
+        }
+        return size;
+    }
 
     public static void main(String[] args) {
-        outputView.printInit();
-        int bridgeSize = inputView.readBridgeSize();
-        BridgeGame bridgeGame = new BridgeGame(bridgeSize);
+        init();
         for(int size = 0; size < bridgeSize; size++) {
             outputView.printSelect();
             String moving = inputView.readMoving();
             String result = bridgeGame.move(moving, size);
             outputView.printMap(bridgeGame.getMoveResults(), size, bridgeGame.getUserMove());
             if(result.equals(CANNOT_CROSS)){
-                outputView.printRetry();
-                String command = inputView.readGameCommand();
-                size = bridgeGame.retry(command, bridgeSize);
+                size = isRetry(result, size);
             }
         }
         outputView.printResult(bridgeGame.getMoveResults(), bridgeGame.getUserMove(), bridgeGame.getTryCount());
