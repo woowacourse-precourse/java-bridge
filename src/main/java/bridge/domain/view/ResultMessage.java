df3e503012e0b2d78c1486c535902487ca00d111
peterysh <52229539+peterysh@users.noreply.github.com>
2022-11-21 21:59:33 +0900

    feat: add ResultMessage for OutputView I/O

---
+++ src/main/java/bridge/domain/view/ResultMessage.java
@@ -0,0 +1,8 @@
+package bridge.domain.view;
+
+public interface ResultMessage {
+    String SUCCESS_OR_FAIL = "게임 성공 여부: ";
+    String SUCCESS = "성공";
+    String FAIL = "실패";
+    String TOTAL_TRY = "총 시도한 횟수: ";
+}
