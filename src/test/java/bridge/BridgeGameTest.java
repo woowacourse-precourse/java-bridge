0d39722ec4a8b4337885227f20548c33e8b16b7a
nn98 <jkllhgb@gmail.com>
2022-11-21 17:32:09 +0900

    Test: FunctionTest

--- src/test/java/bridge/BridgeGameTest.java
+++ src/test/java/bridge/BridgeGameTest.java
@@ -1,14 +1,25 @@
 package bridge;
 
 import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import camp.nextstep.edu.missionutils.test.NsTest;
+import java.util.List;
 import org.junit.jupiter.api.Test;
 
 public class BridgeGameTest extends NsTest {
 
     private static final String ERROR_MESSAGE = "[ERROR]";
+
+    @Test
+    void 기능_테스트() {
+        BridgeGame bridgeGame = new BridgeGame(5);
+        List<String> bridge = bridgeGame.getBridge();
+        assertThat(bridge).contains("U", "D");
+        assertThat(bridge).hasSize(5);
+    }
+
 
     @Test
     void 예외_테스트_다리길이_음수() {
