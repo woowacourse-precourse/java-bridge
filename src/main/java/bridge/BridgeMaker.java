4716e1b488bb1d5277e422899613191b2f8d1151
jhsseonn <hosu0125@gmail.com>
2022-11-22 02:02:24 +0900

    fix: 입력받은 다리 길이만큼 다리 생성하기 함수 수정

--- src/main/java/bridge/BridgeMaker.java
+++ src/main/java/bridge/BridgeMaker.java
@@ -19,10 +19,16 @@ public class BridgeMaker {
      * @return 입력받은 길이에 해당하는 다리 모양. 위 칸이면 "U", 아래 칸이면 "D"로 표현해야 한다.
      */
     public List<String> makeBridge(int size) {
+        //0과 1을 각각 D, U로 바꿔 리스트에 저장하고 반환해주는 작업
+        List<Integer> bridgeUsable = BridgeUsableList(size);
         List<String> bridge = new ArrayList<>();
-        for (int i=0; i<size; i++) {
-            bridge.add("U");
-            bridge.add("D");
+        for (int usable: bridgeUsable) {
+            if (usable==0) {
+                bridge.add("D");
+            }
+            if (usable==1) {
+                bridge.add("U");
+            }
         }
         return bridge;
     }
