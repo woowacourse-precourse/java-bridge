bd229f2144af2a57b3911721b00a7dbbfe8ec74d
twoosky <lhjae3@naver.com>
2022-11-22 05:11:05 +0900

    refactor(BridgeMoving): 이동할 칸 입력값 검증 로직 분리

--- src/main/java/bridge/domain/BridgeMoving.java
+++ src/main/java/bridge/domain/BridgeMoving.java
@@ -1,7 +1,5 @@
 package bridge.domain;
 
-import static bridge.utils.ErrorMessages.ERROR_INVALID_MOVING;
-
 public class BridgeMoving {
     private final Bridge bridge;
 
@@ -10,7 +8,6 @@ public class BridgeMoving {
     }
 
     public String move(String moving, int movingIndex) {
-        validateMoving(moving);
         if (isMove(moving, movingIndex)) {
             return "O";
         }
@@ -20,11 +17,5 @@ public class BridgeMoving {
     private boolean isMove (String moving, int movingIndex) {
         String shape = bridge.getShape(movingIndex);
         return shape.equals(moving);
-    }
-
-    private void validateMoving(String moving) {
-        if (!(moving.equals("U") || moving.equals("D"))) {
-            throw new IllegalArgumentException(ERROR_INVALID_MOVING);
-        }
     }
 }
