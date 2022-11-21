4d67e9e3004d17ac585ac0ff4ff9cf4291322f17
sooyoungh <sooyoung.h8@gmail.com>
2022-11-21 21:33:29 +0900

    ✨ feat(BridgeGame): 한 칸 이동하는 기능 구현

--- src/main/java/bridge/model/BridgeGame.java
+++ src/main/java/bridge/model/BridgeGame.java
@@ -5,12 +5,17 @@ package bridge.model;
  */
 public class BridgeGame {
 
+    private Bridge bridge;
+    private User user;
+
     /**
      * 사용자가 칸을 이동할 때 사용하는 메서드
      * <p>
      * 이동을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void move() {
+    public void move(String moveDirection) {
+        user = new User(moveDirection);
+        bridge.addCurrentRoundNumber();
     }
 
     /**
