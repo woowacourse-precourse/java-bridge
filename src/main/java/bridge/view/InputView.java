b4b4adb8275f0a2c464f63c02605be2bbaf0db45
Hi-Imjaeyoung <imjy2757@naver.com>
2022-11-21 13:51:26 +0900

    feat(InputView) : 사용자로부터 게임을 다시 할지에 대한 입력을 받는 기능 구현.

--- src/main/java/bridge/view/InputView.java
+++ src/main/java/bridge/view/InputView.java
@@ -2,9 +2,7 @@ package bridge.view;
 
 import camp.nextstep.edu.missionutils.Console;
 import bridge.model.Validation;
-/**
- * 사용자로부터 입력을 받는 역할을 한다.
- */
+
 public class InputView {
 
     public int readBridgeSize() {
@@ -23,10 +21,11 @@ public class InputView {
         return moveDirection;
     }
 
-    /**
-     * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
-     */
     public String readGameCommand() {
-        return null;
+        String retry = Console.readLine();
+        while (!Validation.bridgeSizeValidationTest(retry)){
+            retry = Console.readLine();
+        }
+        return retry;
     }
 }
