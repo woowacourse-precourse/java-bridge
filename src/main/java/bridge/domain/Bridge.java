2398e3488f4cc09405d54fc3e96aba7f09349530
hhiyeon <hhyyeon0214@gmail.com>
2022-11-22 03:26:46 +0900

    feat:Bridge-set moving bridge up and down list

---
+++ src/main/java/bridge/domain/Bridge.java
@@ -0,0 +1,27 @@
+package bridge.domain;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Bridge {
+    private static final String MOVING_GAP = " ";
+    private static final String EMPTY_GAP = "   ";
+    private List<String> upBridge;
+    private List<String> downBridge;
+    private String lastBridge;
+
+    public Bridge() {
+        this.upBridge = new ArrayList<>();
+        this.downBridge = new ArrayList<>();
+    }
+
+    public void setMovingBridge(String movingInput, String movingResult) {
+        if (movingInput.equals(Direction.MOVING_UP.getMovingDirection())) {
+            upBridge.add(MOVING_GAP + movingResult + MOVING_GAP);
+            downBridge.add(EMPTY_GAP);
+            return;
+        }
+        downBridge.add(MOVING_GAP + movingResult + MOVING_GAP);
+        upBridge.add(EMPTY_GAP);
+    }
+}
