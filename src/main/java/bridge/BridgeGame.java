20122767aea5dc1b44041e1c128bb427da82fb5b
Hong Jin-Seop <jinsub1999@khu.ac.kr>
2022-11-21 14:00:12 +0900

    feat: C-2 게임 성공 여부와 총 시도한 횟수를 관리하는 기능 구현

--- src/main/java/bridge/BridgeGame.java
+++ src/main/java/bridge/BridgeGame.java
@@ -8,10 +8,20 @@ import java.util.List;
 public class BridgeGame {
     private final List<String> bridge;
     private int currentBlock;
+    private int retries;
+    private STATE state;
+
+    public enum STATE {
+        FAILED,
+        MOVING,
+        SUCCESS
+    }
 
     public BridgeGame(List<String> bridge) {
         this.bridge = bridge;
         this.currentBlock = 0;
+        this.retries = 0;
+        this.state = STATE.MOVING;
     }
 
     public List<String> getBridge() {
@@ -22,17 +32,28 @@ public class BridgeGame {
         return currentBlock;
     }
 
+    public int getRetries() {
+        return retries;
+    }
+
+    public STATE getState() {
+        return state;
+    }
+
     /**
      * 사용자가 칸을 이동할 때 사용하는 메서드
      * <p>
      * 이동을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public boolean move(String moving) {
-        if (bridge.get(currentBlock).equals(moving)) {
-            currentBlock++;
-            return true;
+    public void move(String moving) {
+        if (!bridge.get(currentBlock).equals(moving)) {
+            state = STATE.FAILED;
+            return;
         }
-        return false;
+        currentBlock++;
+        if (currentBlock == bridge.size()) {
+            state = STATE.SUCCESS;
+        }
     }
 
     /**
@@ -40,7 +61,12 @@ public class BridgeGame {
      * <p>
      * 재시작을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void retry() {
+    public void retry(String command) {
+        if (command.equals("Q")) {
+            return;
+        }
         currentBlock = 0;
+        retries++;
+        state = STATE.MOVING;
     }
 }
