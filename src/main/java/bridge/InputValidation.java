838edd18cc502ea1cf6a39d46a491eca1a445cad
kim <kajinori@gachon.ac.kr>
2022-11-21 14:27:03 +0900

    feat(InputValidation): 예외 처리 기능 구현
    -이동칸 선택시 U나 D가 아닌 경우 & 재시작 선택시 R이나 Q가 아닌 경우

--- src/main/java/bridge/InputValidation.java
+++ src/main/java/bridge/InputValidation.java
@@ -1,5 +1,7 @@
 package bridge;
 
+import bridge.UserInput.BridgeSelection;
+
 public class InputValidation {
     public static void validateBridgeSize(int inputSize) {
         if (inputSize < 3 || inputSize > 20) {
@@ -12,6 +14,18 @@ public class InputValidation {
             return Integer.parseInt(input);
         } catch (NumberFormatException e) {
             throw new IllegalArgumentException("[ERROR] 숫자만 입력할 수 있습니다.");
+        }
+    }
+
+    public static void validateUpDown(String inputUpDown) {
+        if (!inputUpDown.equals(BridgeSelection.Up.get()) && !inputUpDown.equals(BridgeSelection.Down.get())) {
+            throw new IllegalArgumentException("[ERROR] U 또는 D만 입력 가능합니다.(위: U, 아래: D)");
+        }
+    }
+
+    public static void validateCommand(String command) {
+        if (!command.equals("R") && !command.equals("Q")) {
+            throw new IllegalArgumentException("[ERROR] R 또는 Q만 입력 가능합니다.(재시도: R, 종료: Q)");
         }
     }
 }
