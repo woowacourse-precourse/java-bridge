c494243ba47c3e2b54976704f9344591f6ab7400
Park Sungchul <try615@naver.com>
2022-11-21 21:30:34 +0900

    feat(OutputView.java): 출력 처리 수행 추가
    출력 수행하는 println()을 따로 메소드로 추가

--- src/main/java/bridge/OutputView.java
+++ src/main/java/bridge/OutputView.java
@@ -5,6 +5,10 @@ package bridge;
  */
 public class OutputView {
 
+    private void println(String message) {
+        System.out.println(message);
+    }
+
     /**
      * 현재까지 이동한 다리의 상태를 정해진 형식에 맞춰 출력한다.
      * <p>
