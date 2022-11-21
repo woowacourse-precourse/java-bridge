ce01c636f97790806344e8caccdd236b09ac3191
Isu Shin <isu010614@naver.com>
2022-11-21 15:07:35 +0900

    feat: 게임 진행 기능 구현
    - 게임 중단 조건을 확인한다.
    - 건널 수 없는 다리를 선택한 경우 중단한다.
    - 입력된 다리의 길이 만큼 게임을 진행한 경우 중단한다.
    - 매 이동마다 이동 상황 map을 출력한다.

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -1,8 +1,57 @@
 package bridge;
 
+import bridge.domain.*;
+
+import static bridge.view.InputView.readBridgeSize;
+import static bridge.view.InputView.readMoving;
+import static bridge.view.InputView.readGameCommand;
+import static bridge.view.OutputView.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Application {
+    private static final int INIT_RETRY_COUNT = 1;
 
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        printStartGameMessage();
+        List<String> randomBridge = generateRandomBridge(getBridgeLength());
+        MovingStatusSaver statusSaver = new MovingStatusSaver(new ArrayList<>(), randomBridge);
+        BridgeGame bridgeGame = new BridgeGame(statusSaver, INIT_RETRY_COUNT);
+
+        run(bridgeGame);
+        // 결과 출력
+    }
+
+    private static List<String> generateRandomBridge(BridgeLength length) {
+        BridgeMaker bridgeMaker = new BridgeMaker(new BridgeRandomNumberGenerator());
+        return bridgeMaker.makeBridge(length.getLength());
+    }
+
+    private static void run(BridgeGame bridgeGame) {
+        while (!bridgeGame.isStopCondition()) {
+            bridgeGame.move(getMoving().toString());
+            printMap(bridgeGame.getSavedStatus());
+        }
+        // 재시작 여부 확인
+    }
+
+
+    private static BridgeLength getBridgeLength() {
+        try {
+            return BridgeLength.createBridgeLength(readBridgeSize());
+        } catch (IllegalArgumentException exception) {
+            System.out.println(exception.getMessage());
+            return getBridgeLength();
+        }
+    }
+
+    private static Moving getMoving() {
+        try {
+            return Moving.createMoving(readMoving());
+        } catch (IllegalArgumentException exception) {
+            System.out.println(exception.getMessage());
+            return getMoving();
+        }
     }
 }
