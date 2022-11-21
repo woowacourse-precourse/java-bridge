93ef085544b51ce59311109cf7f3e89522000531
jmjmjames <chungjm0711@gmail.com>
2022-11-21 23:30:57 +0900

    feat: 시간 순서상 오름차순의 Round 리스트 반환 메서드 구현

--- src/main/java/bridge/domain/Round.java
+++ src/main/java/bridge/domain/Round.java
@@ -4,6 +4,8 @@ import bridge.common.ErrorMessage;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class Round {
     public static final int LOWER_BOUND = 1;
@@ -32,5 +34,11 @@ public class Round {
         if (number < LOWER_BOUND || number > UPPER_BOUND) {
             throw new IllegalArgumentException(ErrorMessage.isInvalidRoundRange());
         }
+    }
+
+    public static List<Round> getRoundsOrderByOrderAsc() {
+        return IntStream.rangeClosed(LOWER_BOUND, UPPER_BOUND)
+                .mapToObj(CACHE::get)
+                .collect(Collectors.toList());
     }
 }
