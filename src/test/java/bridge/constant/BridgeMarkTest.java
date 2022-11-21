c21c663c8e3161d81fa4b9efc07c1ec91a74b5a1
heesang <dlagmltkd12@naver.com>
2022-11-21 21:48:30 +0900

    test: U, D 이외의 입력 값으로 BridgeMark 생성 시 예외 발생

--- src/test/java/bridge/constant/BridgeMarkTest.java
+++ src/test/java/bridge/constant/BridgeMarkTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class BridgeMarkTest {
 
@@ -14,5 +15,13 @@ class BridgeMarkTest {
         BridgeMark markDown = BridgeMark.of("D");
         assertThat(markUp.isUp()).isTrue();
         assertThat(markDown.isDown()).isTrue();
+    }
+
+    @DisplayName("U, D 이외의 입력 값으로 BridgeMark 생성 시 예외 발생")
+    @Test
+    void createBridgeInvalidInput() {
+        assertThatThrownBy(() -> BridgeMark.of("e"))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("[ERROR] 이동할 칸의 입력의 경우 U와 D 뿐이어야 합니다.");
     }
 }
