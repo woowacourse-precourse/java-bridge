db35a6eadcde4270df493a8bf86c4c5b1661166e
jmjmjames <chungjm0711@gmail.com>
2022-11-22 08:16:48 +0900

    feat: BridgeGameResult 뼈대 추가

---
+++ src/main/java/bridge/domain/BridgeGameResult.java
@@ -0,0 +1,12 @@
+package bridge.domain;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class BridgeGameResult {
+    private final Map<Round, MovingResult> result = new HashMap<>();
+
+    public BridgeGameResult() {
+    }
+}
