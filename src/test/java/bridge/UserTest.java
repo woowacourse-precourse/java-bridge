e3ea81032cd9b3299525fa2037de89254eed395f
이성준 <75744297+castlejun-2@users.noreply.github.com>
2022-11-22 00:40:35 +0900

    test: 게임 성공 여부 함수 성공 테스트 추가

--- src/test/java/bridge/UserTest.java
+++ src/test/java/bridge/UserTest.java
@@ -2,15 +2,22 @@ package bridge;
 
 import org.junit.jupiter.api.Test;
 
+import java.io.*;
 import java.util.List;
 import java.util.Arrays;
+import java.util.Scanner;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class UserTest {
+    @Test
     void userSuccessTest() {
-        User user = new User(List.of("U","D","U"),3);
+        User user = new User(List.of("D","D","D"),3);
         assertThat(user.isSuccess(3)).isEqualTo(false);
+
+        for (int i = 0; i < 3; i++)
+            user.userMove("D");
+        assertThat(user.isSuccess(3)).isEqualTo(true);
     }
 }
