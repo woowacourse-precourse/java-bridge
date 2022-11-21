eda5afcaab2cbba50a0c10edcab667393db2487d
qdtjj4 <76ylo1@gmail.com>
2022-11-21 14:38:54 +0900

    feat: 이동할 칸과 다리를 비교하여 결과를 얻는 기능 구현
    이동 결과는 매 이동마다 MoveResult 클래스에서 출력 형식 만들기 등 관리하기 위하여 클래스를 추가함

--- src/main/java/bridge/BridgeGame.java
+++ src/main/java/bridge/BridgeGame.java
@@ -1,16 +1,41 @@
 package bridge;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * 다리 건너기 게임을 관리하는 클래스
  */
 public class BridgeGame {
+
+    public void run() {
+        move(createBridge());
+    }
+
+    public List<String> createBridge() {
+        int bridgeSize = new BridgeSize().get();
+        BridgeMaker bridgeMaker = new BridgeMaker(new BridgeRandomNumberGenerator());
+        return bridgeMaker.makeBridge(bridgeSize);
+    }
 
     /**
      * 사용자가 칸을 이동할 때 사용하는 메서드
      * <p>
      * 이동을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void move() {
+    public void move(List<String> bridge) {
+        List<String> compareResult = new ArrayList<>();
+        for (int space = 0; space < bridge.size(); space++) {
+            String OX = compare(bridge.get(space));
+            compareResult.add(OX);
+            MoveResult moveResult = new MoveResult(compareResult);
+        }
+    }
+
+    public String compare(String eachBridgeSpace) {
+        if (eachBridgeSpace == new Moving().get())
+            return "O";
+        return "X";
     }
 
     /**
---
+++ src/main/java/bridge/MoveResult.java
@@ -0,0 +1,13 @@
+package bridge;
+
+import java.util.List;
+
+public class MoveResult {
+
+    private final List<String> moveResult;
+
+    public MoveResult(List<String> moveResult) {
+        this.moveResult = moveResult;
+    }
+
+}
