5e94b86482f313d30922e653f0f660d4bbd3e2d1
Minsoo Choi <lucas0209@yonsei.ac.kr>
2022-11-22 01:59:45 +0900

    feat(Application): Initialize new instances and make greetings to notice the start of the game
    Features added:
    - greetings for game start

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -1,8 +1,24 @@
 package bridge;
 
+import java.util.List;
+
 public class Application {
 
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        InputView inputView = new InputView();
+        OutputView outputView = new OutputView();
+        BridgeMaker bm = new BridgeMaker(new BridgeRandomNumberGenerator());
+        BridgeGame bg = new BridgeGame();
+        User user = new User();
+
+        greetings();
     }
+
+    public static void greetings() {
+        System.out.println("다리 건너기 게임을 시작합니다.\n");
+    }
+
+
+
 }
