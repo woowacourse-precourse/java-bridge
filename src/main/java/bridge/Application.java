246139bef1dcf40beed9f0fd624276606b2321ab
yewonbahn <banyaewon98@gmail.com>
2022-11-21 17:51:52 +0900

    메소드 구현

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -5,11 +5,24 @@ import java.util.*;
 public class Application {
 
     public static void main(String[] args) {
-        int bridge_size;
+
+
         //다리 길이 입력 받기
         System.out.println("다리 건너기 게임을 시작합니다.");
-        bridge_size=InputView.readBridgeSize();
-        BridgeGame.move(bridge_size);
+        while (true){
+            int bridge_size;
+            boolean check;
+            String retry;
+            bridge_size=InputView.readBridgeSize();
+            System.out.println(bridge_size);
+            BridgeGame.move(bridge_size);
+            check=BridgeGame.retry();
+            if(check!=true){
+                break;
+            }
+
+
+        }
 
         }
 
--- src/main/java/bridge/BridgeGame.java
+++ src/main/java/bridge/BridgeGame.java
@@ -52,6 +52,16 @@ public class BridgeGame {
      * <p>
      * 재시작을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void retry() {
+    public static boolean retry() {
+        String retry;
+        retry=InputView.readGameCommand();
+        if(!retry.equals("R")){
+            OutputView.printResult();
+            return false;}
+        if(retry.equals("R")){
+            return true;
+        }
+        return true;
+
     }
 }
