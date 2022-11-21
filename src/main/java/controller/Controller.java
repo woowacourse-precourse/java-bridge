2765b7b14d0fda3546b469ebb5cfd5a34009142f
Kim-Jong-Gyu <whdrb2643@gmail.com>
2022-11-21 18:32:58 +0900

    feat(bridge): 다리 길이 입력 받는 기능 추가
    다리 길이 입력 받는 기능 추가:
    - Controller 클래스 내 inputsize 함수를 통해 inputsize 값을 받는다.
    - Util 클래스 inputvalue라는 함수를 통해 입력값 받는 함수를 static으로 생성함으로써 여러 클래스에서 접근 가능하도록 만듬

---
+++ src/main/java/controller/Controller.java
@@ -0,0 +1,11 @@
+package controller;
+
+import static controller.Util.inputValue;
+
+public class Controller {
+
+    public int inputSize(){
+        int size = Integer.parseInt(inputValue());
+        return size;
+    }
+}
--- src/main/java/controller/Util.java
+++ src/main/java/controller/Util.java
@@ -1,4 +1,11 @@
 package controller;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Util {
+
+    public static String inputValue(){
+        String value = Console.readLine();
+        return value;
+    }
 }
