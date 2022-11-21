0fb9657baf8a6798e28eb87d995b96f38e9b0440
eunseon <agseon9@gmail.com>
2022-11-21 17:32:02 +0900

    feat : main 메소드 작성

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -1,8 +1,19 @@
 package bridge;
 
+import controller.BridgeController;
+
 public class Application {
 
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        BridgeController bridgeController = new BridgeController();
+
+        try{
+            bridgeController.makeBridge();
+            bridgeController.startGame();
+        }
+        catch (IllegalArgumentException e){
+            System.out.println(e.getMessage());
+        }
+
     }
 }
