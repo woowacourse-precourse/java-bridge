d939d3b05ede5af567d4f949ce98df688dbe9c8c
phobyjun <phobyjun@gmail.com>
2022-11-22 04:09:32 +0900

    fix(OutputView): printResultMessage 출력 메세지 수정

--- src/main/java/bridge/OutputView.java
+++ src/main/java/bridge/OutputView.java
@@ -31,7 +31,7 @@ public class OutputView {
     }
 
     public void printResultMessage(boolean gameWinStatus, int totalTryCount) {
-        System.out.println("최종 게임 결과: " + getGameWinStatusMessage(gameWinStatus));
+        System.out.println("게임 성공 여부: " + getGameWinStatusMessage(gameWinStatus));
         System.out.println("총 시도한 횟수: " + totalTryCount);
     }
 
