3c1398b745770791559e8b116105f42754bf53ab
BaeDongHyeon <sasd730@gmail.com>
2022-11-22 00:57:32 +0900

    feat: enum 문자열 추가

--- src/main/java/bridge/ResultSymbol.java
+++ src/main/java/bridge/ResultSymbol.java
@@ -1,16 +1,22 @@
 package bridge;
 
 public enum ResultSymbol {
-    SUCCESS("O"),
-    FAIL("X"),
-    NOTHING(" ");
+    SUCCESS("O", "성공"),
+    FAIL("X", "실패"),
+    NOTHING(" ", "");
 
     private final String symbol;
-    ResultSymbol(String symbol) {
+    private final String status;
+    ResultSymbol(String symbol, String status) {
         this.symbol = symbol;
+        this.status = status;
     }
 
     public String getSymbol() {
         return symbol;
+    }
+
+    public String getStatus() {
+        return status;
     }
 }
