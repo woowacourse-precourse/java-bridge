3e3e7e7074d7313ce0e9b77797271878fe99e879
sohyundoh <lyny123@naver.com>
2022-11-21 11:12:14 +0900

    feat($input): Problem 1 . bridgesize 입력

--- src/main/java/bridge/InputView.java
+++ src/main/java/bridge/InputView.java
@@ -1,5 +1,8 @@
 package bridge;
 
+import static bridge.Exception.checkBridgeSize;
+import static camp.nextstep.edu.missionutils.Console.readLine;
+
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
  */
@@ -8,8 +11,10 @@ public class InputView {
     /**
      * 다리의 길이를 입력받는다.
      */
-    public int readBridgeSize() {
-        return 0;
+    public static int readBridgeSize() {
+        String bridge = readLine();
+        int bridgeSize = checkBridgeSize(bridge);
+        return bridgeSize;
     }
 
     /**
