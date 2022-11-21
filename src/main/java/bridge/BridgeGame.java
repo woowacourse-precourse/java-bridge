9da499d895c88a5854ba3cd3dca4a255ea37ffce
DongwookKim0823 <dwkim0823@naver.com>
2022-11-22 01:21:13 +0900

    refactor(bridge): move 메서드 구조 변경

--- src/main/java/bridge/BridgeGame.java
+++ src/main/java/bridge/BridgeGame.java
@@ -1,7 +1,5 @@
 package bridge;
 
-import camp.nextstep.edu.missionutils.Console;
-
 import java.util.ArrayList;
 import java.util.List;
 
@@ -14,79 +12,83 @@ public class BridgeGame {
     private final InputView inputView = new InputView();
     private final OutputView outputView = new OutputView();
 
-    private List<List<String>> side = new ArrayList<>();
-
     public BridgeGame(BridgeMaker bridgeMaker, int size) {
         this.bridge = bridgeMaker.makeBridge(size);
+    }
+
+    public void playGame() {
+        List<List<String>> side = new ArrayList<>();
+        while (true) {
+            initBridgeStatus(side);
+            List<List<String>> finalState = move(side);
+            clearMap(side);
+        }
     }
 
     /**
      * 사용자가 칸을 이동할 때 사용하는 메서드
      * <p>
      * 이동을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
+     * @param side
+     * @return
      */
-    public void move() {
-        while (true) {
-            initBridgeStatus();
-            isValidDirection();
-            clearMap(side);
-        }
-    }
-
-    private void isValidDirection() {
+    public List<List<String>> move(List<List<String>> side) {
         for (int location = 0; location < bridge.size(); location++) {
             String inputDirection = inputView.readMoving();
-            if (correctDirection(location, inputDirection)) continue;
-            if (notCorrectDirection(location, inputDirection)) break;
+            if (correctDirection(location, inputDirection, side)) continue;
+            if (notCorrectDirection(location, inputDirection, side)) break;
         }
+        return side;
     }
 
-    private void initBridgeStatus() {
+    public List<List<String>> initBridgeStatus(List<List<String>> side) {
         side.add(new ArrayList<>());
         side.add(new ArrayList<>());
+        return side;
     }
 
-    private void clearMap(List<List<String>> side) {
+    public List<List<String>> clearMap(List<List<String>> side) {
         side.clear();
+        return side;
     }
 
-    private boolean notCorrectDirection(int location, String direction) {
+    private boolean notCorrectDirection(int location, String direction, List<List<String>> side) {
         if (!bridge.get(location).equals(direction)) {
-            stepNotCorrectDirection(direction);
+            stepNotCorrectDirection(direction, side);
             outputView.printMap(side);
             return true;
         }
         return false;
     }
 
-    private void stepNotCorrectDirection(String direction) {
+    private void stepNotCorrectDirection(String direction, List<List<String>> side) {
         if (direction.equals("U")) {
-            step("X", " ");
+            step("X", " ", side);
         }
         if (direction.equals("D")) {
-            step(" ", "X");
+            step(" ", "X", side);
         }
     }
 
-    private boolean correctDirection(int location, String direction) {
+    private boolean correctDirection(int location, String direction, List<List<String>> side) {
         if (bridge.get(location).equals(direction)) {
-            stepCorrectDirection(direction);
+            stepCorrectDirection(direction, side);
             outputView.printMap(side);
             return true;
         }
         return false;
     }
 
-    private void stepCorrectDirection(String direction) {
+    private void stepCorrectDirection(String direction, List<List<String>> side) {
         if (direction.equals("U")) {
-            step("O", " ");
+            step("O", " ", side);
         }
         if (direction.equals("D")) {
-            step(" ", "O");
+            step(" ", "O", side);
         }
     }
 
-    private void step(String topSideStatus, String bottomSideStatus) {
+    private void step(String topSideStatus, String bottomSideStatus, List<List<String>> side) {
         side.get(0).add(topSideStatus);
         side.get(1).add(bottomSideStatus);
     }
