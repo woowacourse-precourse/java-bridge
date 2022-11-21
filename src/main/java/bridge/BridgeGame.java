f10e2bc9380b80982707bc203a1cbb2ef38f31e0
JJ503 <63184334+JJ503@users.noreply.github.com>
2022-11-22 04:09:12 +0900

    feat(BridgeGame): 이동과 관련된 객체 초기화 기능 추가
    docs: 이동과 관련된 객체 초기화 완료 체크

--- src/main/java/bridge/BridgeGame.java
+++ src/main/java/bridge/BridgeGame.java
@@ -4,6 +4,17 @@ package bridge;
  * 다리 건너기 게임을 관리하는 클래스
  */
 public class BridgeGame {
+    private int tryCount;
+    private final Bridge bridge;
+    private CurrentBridgeMap currentBridgeMap;
+    private PlayerMovement playerMovement;
+
+    public BridgeGame(Bridge bridge) {
+        tryCount = 1;
+        this.bridge = bridge;
+        currentBridgeMap = new CurrentBridgeMap();
+        playerMovement = new PlayerMovement();
+    }
 
     /**
      * 사용자가 칸을 이동할 때 사용하는 메서드
