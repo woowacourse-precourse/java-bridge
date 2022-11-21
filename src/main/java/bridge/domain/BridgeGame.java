4ff9a3ff27c057af4229c7be22df0dd91bb053c1
SeokHwan An <asok369258@naver.com>
2022-11-21 20:22:53 +0900

    feat : 게임 성공여부를 나타내는 기능 추가

--- src/main/java/bridge/domain/BridgeGame.java
+++ src/main/java/bridge/domain/BridgeGame.java
@@ -1,5 +1,9 @@
 package bridge.domain;
 
+import static bridge.util.Constant.*;
+
+import java.util.List;
+
 /**
  * 다리 건너기 게임을 관리하는 클래스
  */
@@ -19,5 +23,13 @@ public class BridgeGame {
      * 재시작을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
     public void retry() {
+    }
+
+    // 게임 성공여부 나타내는 기능
+    public String checkGameSuccess(Round round, String userInput, List<String> bridge) {
+        if(round.getRound() == bridge.size() && userInput.equals(bridge.get(round.getRound()-1))) {
+            return SUCCESS;
+        }
+        return FAIL;
     }
 }
