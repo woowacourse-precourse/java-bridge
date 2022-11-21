23faec77f0a45bca6d99016f5dc54c409072417f
starwook <star.wookk@gmail.com>
2022-11-22 00:59:39 +0900

    feat(GameResult.java): Add GameResult.java
    게임 결과 정보가 담겨있는 이넘 추가

---
+++ src/main/java/bridge/GameResult.java
@@ -0,0 +1,14 @@
+package bridge;
+
+public enum GameResult {
+    PASSED("성공",true),
+    FAILED("실패",false);
+
+
+    private String isPass;
+    private boolean trueOrFalse;
+    private GameResult(String isPass, boolean trueOrFalse){
+        this.isPass = isPass;
+        this.trueOrFalse = trueOrFalse;
+    }
+}
