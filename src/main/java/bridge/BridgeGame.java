3b4eb6a71f4caa7a8356021e49036cc604cd3761
KJE <12181501@inha.edu>
2022-11-21 20:17:32 +0900

    feat: BridgeGame- 생성자 안에 넣은 기능을 메서드로 분리(다리 사이즈 예외처리, 다리 생성)

--- src/main/java/bridge/BridgeGame.java
+++ src/main/java/bridge/BridgeGame.java
@@ -5,12 +5,22 @@ import java.util.List;
 
 public class BridgeGame {
     private static List<String> bridge;
-    BridgeGame(int bridgeSize){
-        if(bridgeSize<3||bridgeSize>20)
+    private int nowIdx;
+    BridgeGame(){
+        this.nowIdx = -1;
+    }
+
+    public void setSize(int size){
+        if(size<3||size>20)
             OutputView.errorHandling("3~20 사이의 숫자를 입력해주세요.");
+        this.makeBridge(size);
+    }
+
+    public void makeBridge(int bridgeSize){
         BridgeNumberGenerator bridgeNumberGenerator = null;
         BridgeMaker bridgeMaker = new BridgeMaker (bridgeNumberGenerator);
         this.bridge = bridgeMaker.makeBridge(bridgeSize);
+        this.nowIdx = -1;
     }
 
 
@@ -19,7 +29,7 @@ public class BridgeGame {
      * <p>
      * 이동을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void move() {
+    public void move(String userInput) {
     }
 
     /**
