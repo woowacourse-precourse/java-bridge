8d3e4b5905ed67c1e0cfff09d08211c6306fde48
limzzum <chfhddl1234@naver.com>
2022-11-22 00:33:44 +0900

    refactor : 출력 줄바꿈 추가

--- src/main/java/bridge/OutputView.java
+++ src/main/java/bridge/OutputView.java
@@ -30,11 +30,12 @@ public class OutputView {
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
     public void printResult(String result, int tryNum) {
-        System.out.println("게임 성공 여부: " + result);
+        System.out.println("\n게임 성공 여부: " + result);
         System.out.println("총 시도한 횟수: " + tryNum);
     }
 
     public void printEnding() {
         System.out.println("최종 게임 결과");
     }
+
 }
