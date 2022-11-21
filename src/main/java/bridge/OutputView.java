216c9e76f7056f29abe93e3673f7b58c7417e8d0
rabbit0216 <rabbit1999k@gmail.com>
2022-11-21 11:59:49 +0900

    feat: 이동 결과 출력 함수 구현

--- src/main/java/bridge/OutputView.java
+++ src/main/java/bridge/OutputView.java
@@ -1,5 +1,9 @@
 package bridge;
 
+import java.util.List;
+
+import static bridge.Constants.*;
+
 /**
  * 사용자에게 게임 진행 상황과 결과를 출력하는 역할을 한다.
  */
@@ -10,7 +14,54 @@ public class OutputView {
      * <p>
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void printMap() {
+    public void printMap(List<String> bridgeAnswer, int bridgeSize, List<String> userCommand) {
+        String upSentence = "[";
+        String downSentence = "[";
+        for(int size = 0; size < bridgeSize; size++){
+            String command = userCommand.get(size);
+            String answer = bridgeAnswer.get(size);
+            boolean up = isUp(command);
+            boolean down = isDown(command);
+            upSentence += printAnswer(answer, bridgeSize, up);
+            downSentence += printAnswer(answer, bridgeSize, down);
+        }
+        upSentence = removeDelimiter(upSentence) + "]";
+        downSentence = removeDelimiter(downSentence) + "]";
+        System.out.println(upSentence + '\n' + downSentence);
+    }
+
+    String removeDelimiter(String sentence){
+        if(sentence.charAt(sentence.length() - 1) == '|'){
+            sentence = sentence.substring(0, sentence.length() - 1);
+        }
+        return sentence;
+    }
+
+    String printAnswer(String answer, int bridgeSize, boolean isUpOrDown){
+        if(!isUpOrDown){
+            answer = "";
+        }
+        String combinedAnswer = " " + answer + " ";
+        if(bridgeSize > 1){
+            combinedAnswer += "|";
+        }
+        return combinedAnswer;
+    }
+
+    boolean isUp(String command){
+        boolean up = false;
+        if(command.equals("U")){
+            up = true;
+        }
+        return up;
+    }
+
+    boolean isDown(String command){
+        boolean down = false;
+        if(command.equals("D")){
+            down = true;
+        }
+        return down;
     }
 
     /**
