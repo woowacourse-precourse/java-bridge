cb1429786b291fd3dd8053dffce906a831950bb0
shpark30 <seohyeonpark30.gmail.com>
2022-11-21 21:56:04 +0900

    test(BridgeMaker): 정확한 다리 생성 테스트

--- src/test/java/bridge/BridgeMakerTest.java
+++ src/test/java/bridge/BridgeMakerTest.java
@@ -1,18 +1,22 @@
 package bridge;
 
+import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeTest;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
+import camp.nextstep.edu.missionutils.test.NsTest;
 import java.util.List;
 import org.junit.jupiter.api.Test;
 
-class BridgeMakerTest {
+class BridgeMakerTest extends NsTest {
     @Test
     void makeTest() {
-        BridgeNumberGenerator bridgeNumberGenerator = new BridgeRandomNumberGenerator();
-        BridgeMaker bridgeMaker = new BridgeMaker(bridgeNumberGenerator);
-        List<String> bridge =bridgeMaker.makeBridge(5);
-        assertThat(bridge).containsOnly("U","D");
+        assertRandomNumberInRangeTest(() -> run(),
+                1, 0, 1,0);
+//        BridgeNumberGenerator bridgeNumberGenerator = new BridgeRandomNumberGenerator();
+//        BridgeMaker bridgeMaker = new BridgeMaker(bridgeNumberGenerator);
+//        List<String> bridge =bridgeMaker.makeBridge(5);
+        assertThat(output()).isEqualTo("[U, D, U, D]");
     }
 
     @Test
@@ -29,5 +33,14 @@ class BridgeMakerTest {
         BridgeMaker bridgeMaker = new BridgeMaker(bridgeNumberGenerator);
         assertThatThrownBy(() -> bridgeMaker.makeBridge(21))
                 .isInstanceOf(IllegalArgumentException.class);
+
+    }
+
+    @Override
+    protected void runMain() {
+        BridgeNumberGenerator bridgeNumberGenerator = new BridgeRandomNumberGenerator();
+        BridgeMaker bridgeMaker = new BridgeMaker(bridgeNumberGenerator);
+        List<String> bridge =bridgeMaker.makeBridge(4);
+        System.out.print(bridge);
     }
 }
