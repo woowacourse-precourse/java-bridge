f9d5a225fb43cbf5b6cbdb4a8e84ad4cd56b4e01
LJW25 <dbjwl@g.skku.edu>
2022-11-21 18:16:40 +0900

    fix: 에러 메세지 수정

--- src/main/java/bridge/Message.java
+++ src/main/java/bridge/Message.java
@@ -7,10 +7,10 @@ public enum Message {
     MSG_GET_MOVE("이동할 칸을 선택해주세요. (위: U, 아래: D)"),
     MSG_GET_RETRY("게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)"),
 
-    ERROR_INT("다리 길이는 숫자여야 합니다."),
-    ERROR_LENGTH("다리 길이는 3부터 20 사이의 숫자여야 합니다."),
-    ERROR_MOVE("입력은 U 혹은 D여야 합니다. 다시 입력해 주세요. (위: U, 아래: D)"),
-    ERROR_RETRY("입력은 R 혹은 Q여야 합니다. 다시 입력해 주세요. (재시도: R, 종료: Q)");
+    ERROR_INT("[ERROR] 다리 길이는 숫자여야 합니다. 다시 입력해 주세요."),
+    ERROR_LENGTH("[ERROR] 다리 길이는 3부터 20 사이의 숫자여야 합니다. 다시 입력해 주세요."),
+    ERROR_MOVE("[ERROR] 입력은 U 혹은 D여야 합니다. 다시 입력해 주세요. (위: U, 아래: D)"),
+    ERROR_RETRY("[ERROR] 입력은 R 혹은 Q여야 합니다. 다시 입력해 주세요. (재시도: R, 종료: Q)");
 
     private String message;
 
