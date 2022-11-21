fc83974ad2dbcc9f243de03e87f53194ae083175
yewonbahn <banyaewon98@gmail.com>
2022-11-21 16:30:40 +0900

    위아래 두칸으로 이루어진 N길이의 다리를 생성

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -1,12 +1,16 @@
 package bridge;
 
+import java.util.List;
+
 public class Application {
 
     public static void main(String[] args) {
         int bridge_size;
         //다리 길이 입력 받기
+        System.out.println("다리 건너기 게임을 시작합니다.");
         bridge_size=InputView.readBridgeSize();
-
+        List<String> bridge=BridgeMaker.makeBridge(bridge_size);
+        System.out.println(bridge);
 
     }
 }
--- src/main/java/bridge/BridgeMaker.java
+++ src/main/java/bridge/BridgeMaker.java
@@ -1,5 +1,6 @@
 package bridge;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -17,7 +18,22 @@ public class BridgeMaker {
      * @param size 다리의 길이
      * @return 입력받은 길이에 해당하는 다리 모양. 위 칸이면 "U", 아래 칸이면 "D"로 표현해야 한다.
      */
-    public List<String> makeBridge(int size) {
+    public static List<String> makeBridge(int size) {
+        List<String> bridge =  new ArrayList();
+        for(int i=0;i<size;i++) {
+            BridgeRandomNumberGenerator bridgerng=new BridgeRandomNumberGenerator();
+            int num=bridgerng.generate();
+            if(num==1){
+                bridge.add("U");
+            }
+            else{
+                bridge.add("D");
+            }
+
+
+        }
+        System.out.println(bridge);
+
         return null;
     }
 }
