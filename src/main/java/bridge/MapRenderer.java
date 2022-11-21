8c0a792df36661ba27e47fec34402dd6dc36ca53
Devtist <yt930310@snu.ac.kr>
2022-11-21 17:22:52 +0900

    refactor: 매직 넘버 상수로 선언

--- src/main/java/bridge/MapRenderer.java
+++ src/main/java/bridge/MapRenderer.java
@@ -13,6 +13,8 @@ public class MapRenderer {
 	private static final String CROSS = " X ";
 	private static final String SPACE = "   ";
 	private static final String LINE_BREAK_CHARACTER = "\n";
+	private static final int ONE = 1;
+	private static final int FIRST_INDEX = 0;
 
 	StringBuilder upperRow;
 	StringBuilder lowerRow;
@@ -22,10 +24,10 @@ public class MapRenderer {
 	public MapRenderer(List<String> partOfBridge, boolean isCorrectMoving) {
 		init();
 		addEntrance();
-		if (partOfBridge.size() == 1) {
+		if (isSizeOne(partOfBridge)) {
 			map = renderWhenSizeIsOne(partOfBridge, isCorrectMoving);
 		}
-		if (partOfBridge.size() != 1) {
+		if (!isSizeOne(partOfBridge)) {
 			map = renderWhenSizeBiggerThanOne(partOfBridge, isCorrectMoving);
 		}
 	}
@@ -35,17 +37,18 @@ public class MapRenderer {
 	}
 
 	private String renderWhenSizeIsOne(List<String> partOfBridge, boolean isCorrectMoving) {
-		addLastCell(partOfBridge.get(0), isCorrectMoving);
+		addLastCell(partOfBridge.get(FIRST_INDEX), isCorrectMoving);
 		addExit();
 		return upperRow.toString() + LINE_BREAK_CHARACTER + lowerRow.toString();
 	}
 
 	private String renderWhenSizeBiggerThanOne(List<String> partOfBridge, boolean isCorrectMoving) {
-		partOfBridge.subList(0, partOfBridge.size() - 1).forEach(cell -> {
+		int lastIndex = partOfBridge.size() - ONE;
+		partOfBridge.subList(FIRST_INDEX, lastIndex).forEach(cell -> {
 			addCircle(cell);
 			addDivisionLine();
 		});
-		addLastCell(partOfBridge.get(partOfBridge.size() - 1), isCorrectMoving);
+		addLastCell(partOfBridge.get(lastIndex), isCorrectMoving);
 		addExit();
 		return upperRow.toString() + LINE_BREAK_CHARACTER + lowerRow.toString();
 	}
@@ -77,6 +80,10 @@ public class MapRenderer {
 		if (!isCorrectMoving) {
 			addCross(cell);
 		}
+	}
+
+	private boolean isSizeOne(List<String> partOfBridge) {
+		return partOfBridge.size() == 1;
 	}
 
 	private void addCircle(String cell) {
