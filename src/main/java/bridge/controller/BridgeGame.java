727e25f663a85e83d819de376b21dcba09fa6ff1
jyj1143 <jyj1143@gmail.com>
2022-11-21 20:07:42 +0900

    feat(BrigeGame): 플레이어 이동 기능 추가

--- src/main/java/bridge/controller/BridgeGame.java
+++ src/main/java/bridge/controller/BridgeGame.java
@@ -1,16 +1,40 @@
 package bridge.controller;
 
+import bridge.BridgeMaker;
+import bridge.BridgeNumberGenerator;
+import bridge.domain.BridgeResult;
+
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * 다리 건너기 게임을 관리하는 클래스
  */
 public class BridgeGame {
+    private final List<String> answerBridge;
+    private int count;
+    private List<String> playerBridge;
+    private BridgeResult bridgeResult;
+
+    public BridgeGame(List<String> answerBridge, int count) {
+        this.answerBridge = answerBridge;
+        this.playerBridge = new ArrayList<>();
+        this.bridgeResult = new BridgeResult(answerBridge);
+        this.count = count;
+    }
+
+    public int getCount() {
+        return count;
+    }
 
     /**
      * 사용자가 칸을 이동할 때 사용하는 메서드
      * <p>
      * 이동을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void move() {
+    public String move(String input) {
+        playerBridge.add(input);
+        return bridgeResult.matchBridge(playerBridge);
     }
 
     /**
