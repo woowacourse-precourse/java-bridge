68667163aa0a061a11abc0014c130e84850b957e
wugawuga <dnr3655@naver.com>
2022-11-22 00:46:06 +0900

    refactor: 입력값 검증 이전으로 테스트 케이스 추가

--- src/test/java/bridge/service/BridgeGameTest.java
+++ src/test/java/bridge/service/BridgeGameTest.java
@@ -2,8 +2,12 @@ package bridge.service;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
+import bridge.domain.Bridge;
+import java.util.List;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 class BridgeGameTest {
 
@@ -27,13 +31,39 @@ class BridgeGameTest {
                 .isInstanceOf(IllegalStateException.class);
         assertThatThrownBy(game::checkCross)
                 .isInstanceOf(IllegalStateException.class);
-        assertThatThrownBy(() -> game.move("test", 0))
-                .isInstanceOf(IllegalStateException.class);
-        assertThatThrownBy(() -> game.retry("R"))
-                .isInstanceOf(IllegalStateException.class);
         assertThatThrownBy(game::printPlayCount)
                 .isInstanceOf(IllegalStateException.class);
         assertThatThrownBy(game::state)
                 .isInstanceOf(IllegalStateException.class);
+    }
+
+    @ParameterizedTest
+    @DisplayName("게임 커맨드 U, D 아닐 시에 예외")
+    @CsvSource(value = {
+            "A,0",
+            "B,1",
+            "C,2"
+    })
+    void checkUpDown(String input, int defaultCount) {
+        BridgeGame game = new BridgeGame(new Bridge(List.of("U", "D", "D")));
+
+        assertThatThrownBy(() -> game.move(input, defaultCount))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("[ERROR] U 또는 D를 입력해주세요");
+    }
+
+    @ParameterizedTest
+    @DisplayName("게임 종료 커맨드 R, Q 아닐 시에 예외")
+    @CsvSource(value = {
+            "A",
+            "B",
+            "C"
+    })
+    void checkUpDown(String input) {
+        BridgeGame game = new BridgeGame(new Bridge(List.of("U", "D", "D")));
+
+        assertThatThrownBy(() -> game.retry(input))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("[ERROR] Q 또는 R을 입력해주세요");
     }
 }
