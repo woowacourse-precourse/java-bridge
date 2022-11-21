08bccc6593b0cb20af7d1975bedefb781f1c5855
Young37 <younghk37@gmail.com>
2022-11-21 14:09:56 +0900

    feat(bridge): add the feature to make a bridge

--- src/main/java/bridge/BridgeMaker.java
+++ src/main/java/bridge/BridgeMaker.java
@@ -1,6 +1,9 @@
 package bridge;
 
+import java.util.ArrayList;
 import java.util.List;
+
+import bridge.constant.Direction;
 
 /**
  * 다리의 길이를 입력 받아서 다리를 생성해주는 역할을 한다.
@@ -18,6 +21,14 @@ public class BridgeMaker {
      * @return 입력받은 길이에 해당하는 다리 모양. 위 칸이면 "U", 아래 칸이면 "D"로 표현해야 한다.
      */
     public List<String> makeBridge(int size) {
-        return null;
+        List<String> bridge = new ArrayList<String>();
+        
+        for (int index = 0; index < size; index++) {
+            int bridgeNumber = bridgeNumberGenerator.generate();
+            String firstLetter = Direction.getFirstLetter(bridgeNumber);
+            bridge.add(firstLetter);
+        }
+        
+        return bridge;
     }
 }
