07b9f454c71f53bf9fdb10cf0427b0a0f122bc31
shpark30 <seohyeonpark30.gmail.com>
2022-11-22 00:12:12 +0900

    feat(BridgeGame): 이동/재시도 등 게임 관리 기능

--- src/main/java/bridge/BridgeGame.java
+++ src/main/java/bridge/BridgeGame.java
@@ -1,16 +1,46 @@
 package bridge;
 
+import java.util.List;
+
 /**
  * 다리 건너기 게임을 관리하는 클래스
  */
 public class BridgeGame {
+    private List<String> bridge;
+    private boolean succeeds;
+    private int trial;
+
+    public BridgeGame(List<String> bridge) {
+        System.out.println(bridge);
+        this.bridge = bridge;
+        this.succeeds = false;
+        this.trial = 1;
+    }
 
     /**
      * 사용자가 칸을 이동할 때 사용하는 메서드
      * <p>
      * 이동을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void move() {
+    public boolean move(Player player, String direction) {
+        player.move(direction, bridge.get(player.getPathSize()));
+        return player.compareWith(bridge);
+    }
+
+    public int getTotalRounds() {
+        return bridge.size();
+    }
+
+    public int getTrial() {
+        return trial;
+    }
+
+    public void makeSuccess() {
+        this.succeeds = true;
+    }
+
+    public boolean isSucceeds() {
+        return succeeds;
     }
 
     /**
@@ -18,6 +48,18 @@ public class BridgeGame {
      * <p>
      * 재시작을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void retry() {
+    public boolean retry(String command) {
+        validateCommand(command);
+        if (command.equals("R")) {
+            trial += 1;
+            return true;
+        }
+        return false;
+    }
+
+    private void validateCommand(String command) {
+        if (!List.of("R", "Q").contains(command)) {
+            throw new IllegalArgumentException("[ERROR] R(재시작) 또는 Q(종료)만 입력할 수 있습니다.");
+        }
     }
 }
--- src/test/java/bridge/BridgeGameTest.java
+++ src/test/java/bridge/BridgeGameTest.java
@@ -1,4 +1,48 @@
-import static org.junit.jupiter.api.Assertions.*;
+package bridge;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import java.util.List;
+import org.junit.jupiter.api.Test;
+
 class BridgeGameTest {
-  
+    @Test
+    void testRetry() {
+        BridgeGame bridgeGame = new BridgeGame(List.of("U","D","U"));
+        assertThat(bridgeGame.getTrial()).isEqualTo(1);
+        boolean isRetry = bridgeGame.retry("R");
+        assertThat(isRetry).isEqualTo(true);
+        assertThat(bridgeGame.getTrial()).isEqualTo(2);
+    }
+
+    @Test
+    void testQuit() {
+        BridgeGame bridgeGame = new BridgeGame(List.of("U","D","U"));
+        assertThat(bridgeGame.getTrial()).isEqualTo(1);
+        boolean isRetry = bridgeGame.retry("Q");
+        assertThat(isRetry).isEqualTo(false);
+        assertThat(bridgeGame.getTrial()).isEqualTo(1);
+    }
+
+    @Test
+    void testValidate() {
+        BridgeGame bridgeGame = new BridgeGame(List.of("U","D","U"));
+        assertThat(bridgeGame.getTrial()).isEqualTo(1);
+        assertThatThrownBy(() -> bridgeGame.retry("q"))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void testErrorMessage() {
+        BridgeGame bridgeGame = new BridgeGame(List.of("U","D","U"));
+        assertThat(bridgeGame.getTrial()).isEqualTo(1);
+        try {
+            bridgeGame.retry("q");
+            throw new RuntimeException();
+        } catch (IllegalArgumentException e) {
+            assertThat(e.getMessage()).isEqualTo("[ERROR] R(재시작) 또는 Q(종료)만 입력할 수 있습니다.");
+        }
+
+    }
 }
