1973bc5d4471b7e6d0cf0e22f7d68d9df1352948
Hong Jin-Seop <jinsub1999@khu.ac.kr>
2022-11-21 14:21:46 +0900

    feat: A-1 현재까지 건넌 다리를 모두 출력하는 기능 구현

--- src/main/java/bridge/OutputView.java
+++ src/main/java/bridge/OutputView.java
@@ -1,16 +1,40 @@
 package bridge;
 
+import java.util.List;
+
 /**
  * 사용자에게 게임 진행 상황과 결과를 출력하는 역할을 한다.
  */
 public class OutputView {
+    public String getBlockResult(String bridgeBlock, String movesBlock, String direction) {
+        if (!direction.equals(movesBlock)) {
+            return " ";
+        }
+        if (bridgeBlock.equals(movesBlock)) {
+            return "O";
+        }
+        return "X";
+    }
+
+    public void printOneLine(List<String> bridge, List<String> moves, String direction) {
+        System.out.print("[ ");
+        System.out.print(getBlockResult(bridge.get(0), moves.get(0), direction));
+
+        for (int i = 1; i < moves.size(); i++) {
+            System.out.print(" | ");
+            System.out.print(getBlockResult(bridge.get(i), moves.get(i), direction));
+        }
+        System.out.print(" ]\n");
+    }
 
     /**
      * 현재까지 이동한 다리의 상태를 정해진 형식에 맞춰 출력한다.
      * <p>
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void printMap() {
+    public void printMap(List<String> bridge, List<String> moves) {
+        printOneLine(bridge, moves, "U");
+        printOneLine(bridge, moves, "D");
     }
 
     /**
