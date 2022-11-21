aa0ce9c6ebc93103ed4680bbcbd0a4fe8372a8d9
smc2315 <choeyongtae085@gmail.com>
2022-11-22 02:03:50 +0900

    feat: makeMap

--- src/main/java/bridge/view/OutputView.java
+++ src/main/java/bridge/view/OutputView.java
@@ -15,17 +15,27 @@ public class OutputView {
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
     public static void printMap(int index, List<String> bridge) {
-        System.out.print("[");
-        for(String block : bridge){
-            System.out.print();
-        }
+        System.out.println(makeMap(index,bridge));
     }
 
     public static void printMapFail(int index, List<String> bridge) {
-        System.out.print("[");
-        for(String block : bridge){
-            System.out.print();
+
+    }
+
+    private static String makeMap(int index, List<String> bridge) {
+        StringBuilder Up = new StringBuilder("[");
+        StringBuilder Down = new StringBuilder("[");
+        for (String block : bridge) {
+            if (block.equals("U")) {
+                Up.append(" O ");Down.append("   ");
+            }
+            if (block.equals("D")) {
+                Up.append("   ");Down.append(" O ");
+            }
+            Up.append("|");Down.append("|");
         }
+        Up.replace(Up.length()-1,Up.length(),"]");Down.replace(Down.length()-1,Down.length(),"]");
+        return Up.append("\n").append(Down).toString();
     }
 
     /**
