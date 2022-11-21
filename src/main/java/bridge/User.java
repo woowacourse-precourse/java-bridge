ad02aea1a0caa37682365a3912df127b9cbacc69
oreo <ohsimon77@naver.com>
2022-11-21 22:44:40 +0900

    Feat : User 메소드 BridgeSize, userBridge 추가

--- src/main/java/bridge/User.java
+++ src/main/java/bridge/User.java
@@ -1,5 +1,22 @@
 package bridge;
 
-public class User {
+import java.util.ArrayList;
+import java.util.List;
 
+public class User {
+    public static int BridgeSize(){
+        InputView inputView = new InputView();
+        String BridgeSize = inputView.readBridgeSize();
+        return Integer.parseInt(BridgeSize);
+    }
+
+    public static List<String> userBridge(List<String> bridge){
+        InputView inputView = new InputView();
+        List<String> userBridge = new ArrayList<>();
+        for(int i=0;i<bridge.size();i++){
+            String MovingSquare = inputView.readMoving();
+            userBridge.add(MovingSquare);
+        }
+        return userBridge;
+    }
 }
