0105bf2ab14dc9b8fc552bd97d76afdd0c1c8cf7
LeeHaiLim <hailim1997@gmail.com>
2022-11-22 05:02:41 +0900

    test(domain): implement test class input validator test
    Added test methods:
    - input bridge size not number
    - input bridge size not in range
    - input bridge size appropriate

---
+++ src/test/java/bridge/domain/InputValidatorTest.java
@@ -0,0 +1,34 @@
+package bridge.domain;
+
+import static org.assertj.core.api.Assertions.assertThatNoException;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class InputValidatorTest {
+    InputValidator inputValidator = new InputValidator();
+
+    @DisplayName("입력값이 숫자가 아닌 경우 예외가 발생한다.")
+    @Test
+    void inputBridgeSizeNotNumber() {
+        assertThatThrownBy(() -> inputValidator.validateBridgeSize("a"))
+                .isInstanceOf(IllegalArgumentException.class).hasMessage("[ERROR] 숫자만 입력해야 합니다.");
+    }
+
+    @DisplayName("입력값이 3 에서 20 사이가 아닌 경우 예외가 발생한다.")
+    @Test
+    void inputBridgeSizeNotInRange() {
+        assertThatThrownBy(() -> inputValidator.validateBridgeSize("21"))
+                .isInstanceOf(IllegalArgumentException.class).hasMessage("[ERROR] 다리 길이는 3 에서 20 사이의 숫자여야 합니다.");
+        assertThatThrownBy(() -> inputValidator.validateBridgeSize("2"))
+                .isInstanceOf(IllegalArgumentException.class).hasMessage("[ERROR] 다리 길이는 3 에서 20 사이의 숫자여야 합니다.");
+
+    }
+
+    @DisplayName("입력값이 3-20 사이의 숫자인 경우 예외가 발생하지 않는다")
+    @Test
+    void inputBridgeSizeAppropriate() {
+        assertThatNoException().isThrownBy(() -> inputValidator.validateBridgeSize("10"));
+    }
+}
