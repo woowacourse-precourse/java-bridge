b43dfbf9e2a7584d2c719077742a483243df394c
jsc1014 <wotlr5209@gmail.com>
2022-11-21 18:14:23 +0900

    test: 사용자의 입력과 다리를 비교했을때 결과 테스트

--- src/test/java/bridge/BridgeGameTest.java
+++ src/test/java/bridge/BridgeGameTest.java
@@ -1,4 +1,20 @@
+package bridge;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class BridgeGameTest {
-  
+
+    @DisplayName("사용자 입력에 다리가 있으면 O, 없으면 X")
+    @Test
+    void CheckUserMovementEqualBridge(){
+        BridgeGame bridgeGame = new BridgeGame();
+
+        String wrong = bridgeGame.moveEqualBridge("U","D");
+        String right = bridgeGame.moveEqualBridge("U","U");
+        assertEquals("O",right);
+        assertEquals("X",wrong);
+    }
 }
