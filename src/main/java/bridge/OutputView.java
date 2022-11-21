44fefc5a2f2a382db245a3979d38b1d382b0c407
rabbit0216 <rabbit1999k@gmail.com>
2022-11-21 12:13:53 +0900

    feat: 게임 결과 출력 함수 구현

--- src/main/java/bridge/OutputView.java
+++ src/main/java/bridge/OutputView.java
@@ -64,11 +64,37 @@ public class OutputView {
         return down;
     }
 
+    public void printInit(){
+        System.out.println("다리 건너기 게임을 시작합니다.\n");
+        System.out.println("다리의 길이를 입력해주세요.");
+    }
+
+    public void printSelect(){
+        System.out.println("이동할 칸을 선택해주세요. (위: U, 아래: D)");
+    }
+
     /**
      * 게임의 최종 결과를 정해진 형식에 맞춰 출력한다.
      * <p>
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void printResult() {
+    public void printResult(List<String> bridgeAnswer, int bridgeSize, List<String> userCommand,
+                            int tryCount) {
+        System.out.println("최종 게임 결과");
+        printMap(bridgeAnswer, bridgeSize, userCommand);
+
+        String result = SUCCESS;
+        System.out.println("게임 성공 여부: ");
+        if(bridgeAnswer.contains("X")){
+            result = FAIL;
+        }
+        System.out.println(result);
+        System.out.println("총 시도한 횟수: " + tryCount);
+    }
+
+    public void printRetry(String result){
+        if(result.equals("X")){
+            System.out.println("게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)");
+        }
     }
 }
