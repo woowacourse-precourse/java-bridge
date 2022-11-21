845d396115c0bf0b90761906ef4cf452e999cdfe
jmjmjames <chungjm0711@gmail.com>
2022-11-21 23:03:17 +0900

    test: [예외] 올바르지 않은 범위일시 예외 발생 테스트

---
+++ src/test/java/bridge/domain/RoundTest.java
@@ -0,0 +1,22 @@
+package bridge.domain;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import bridge.common.ErrorMessage;
+import java.util.Collection;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+class RoundTest {
+
+    @ParameterizedTest
+    @ValueSource(ints = {-1234, -1, Round.LOWER_BOUND - 1, Round.UPPER_BOUND + 1, 1234})
+    @DisplayName("[예외] 올바르지 않은 범위일시 예외 발생한다.")
+    void isInvalidRoundRange(int number) {
+        assertThatThrownBy(() -> Round.valueOf(number))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(ErrorMessage.isInvalidRoundRange());
+    }
+}
