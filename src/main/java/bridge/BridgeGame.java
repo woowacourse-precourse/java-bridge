6d771c7e1ed68a2938d16721dc65e5a83a3f9500
gnoesnooj <306yyy@naver.com>
2022-11-21 22:15:13 +0800

    feat(BridgeGame) : 게임 진행 클래스 구현
    - 다리 이동
    - 게임 재시도
    - 게임 종료

--- src/main/java/bridge/BridgeGame.java
+++ src/main/java/bridge/BridgeGame.java
@@ -1,23 +1,70 @@
 package bridge;
 
+import bridge.domain.Player;
+import java.util.List;
+
 /**
  * 다리 건너기 게임을 관리하는 클래스
  */
 public class BridgeGame {
 
-    /**
-     * 사용자가 칸을 이동할 때 사용하는 메서드
-     * <p>
-     * 이동을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
-     */
-    public void move() {
+    private final Player player;
+
+    private List<String> bridge;
+
+    private int round = 1;
+
+    private int location = 0 ;
+
+    public BridgeGame(Player player){
+        this.player = player;
     }
 
-    /**
-     * 사용자가 게임을 다시 시도할 때 사용하는 메서드
-     * <p>
-     * 재시작을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
-     */
-    public void retry() {
+    public void generate(List<String> bridge){
+        this.bridge = bridge;
     }
+
+    public void move(String moving) {
+        if(moving.equals("U")){
+            if(bridge.get(location).equals(moving)){
+                player.correctUpsideBridge(location);
+            }
+            if(!(bridge.get(location).equals(moving))){
+                player.wrongUpsideBridge(location);
+            }
+        }
+
+        if(moving.equals("D")){
+            if(bridge.get(location).equals(moving)){
+                player.correctDownsideBridge(location);
+            }
+            if(!(bridge.get(location).equals(moving))){
+                player.wrongDownsideBridge(location);
+            }
+        }
+        increaseLocation();
+    }
+
+    public void retry() {
+        player.retry();
+        increaseRetry();
+        location = 0;
+    }
+
+    public int quit(){
+        return round;
+    }
+
+    public void increaseRetry(){ // 재시도 횟수를 카운트 +1 해준다
+        round++;
+    }
+
+    public void increaseLocation(){ // 다리를 건널때마다 현재 위치에 대한 값을 올려준다.
+        location++;
+    }
+
+    public boolean isAllAnswer(int size){
+        return player.isAllAnswer(size);
+    }
+
 }
