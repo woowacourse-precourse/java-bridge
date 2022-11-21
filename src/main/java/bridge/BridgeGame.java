eea9a2e8fc533c1e0caebb5354d81835cab05dd9
skydreamer21 <skydreamer210@gmail.com>
2022-11-21 11:30:22 +0900

    feat(BridgeGame): move 메서드 구현

--- src/main/java/bridge/BridgeGame.java
+++ src/main/java/bridge/BridgeGame.java
@@ -21,7 +21,11 @@ public class BridgeGame {
      * <p>
      * 이동을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void move() {
+    public boolean move(String userChoice) {
+        increaseAttemptCount();
+        boolean isUserChoiceCorrect = bridge.isCorrectPanel(order, userChoice);
+        increaseOrder();
+        return isUserChoiceCorrect;
     }
 
     /**
@@ -30,6 +34,14 @@ public class BridgeGame {
      * 재시작을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
     public void retry() {
+    private void increaseAttemptCount() {
+        attemptCount++;
+    }
+
+    private void increaseOrder() {
+        order++;
+    }
+
     public int getAttemptCount() {
         return attemptCount;
     }
