a09ce88cc39449c63cccbb86c4d766a617752945
NAM GIBEOM <rlqja0523@naver.com>
2022-11-21 21:39:42 +0900

    feat: R 또는 Q인지 확인하는 예외 메서드 추가

--- src/main/java/bridge/exception/GameCommandException.java
+++ src/main/java/bridge/exception/GameCommandException.java
@@ -1,10 +1,19 @@
 package bridge.exception;
 
+import bridge.service.UserInput;
+
 public class GameCommandException extends UserException {
     @Override
     public void checkException(String userInput) {
         isEmpty(userInput);
         isBlank(userInput);
         hasBlank(userInput);
+        isNotGameCommand(userInput);
+    }
+
+    private void isNotGameCommand(String gameCommand) {
+        if (!UserInput.GAME_COMMAND.getLetters().contains(gameCommand)) {
+            throw new IllegalArgumentException(ExceptionCode.IS_NOT_GAME_COMMAND.getMessage());
+        }
     }
 }
