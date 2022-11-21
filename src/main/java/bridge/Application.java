f1c5b16b7667586312217d0af4a35bf0c9520ab8
baekeunsun <eunsun5460@gmail.com>
2022-11-21 18:17:52 +0900

    feat(bridge/Game, Application): 플레이어 이동, 게임 시작 완료
    - (Applicaiton/main): 게임을 생성
    - (Game/playGame): 플레이어가 이동 및 재시작, 종료를 하게 함

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -1,53 +1,11 @@
 package bridge;
 
-import bridge.view.InputView;
-import bridge.view.OutputView;
-
-import java.util.List;
-
 public class Application {
 
     public static void main(String[] args) {
         // TODO: 프로그램 구현
-        BridgeNumberGenerator bridgeNumberGenerator = new BridgeRandomNumberGenerator();
-
-        BridgeMaker user = new BridgeMaker(bridgeNumberGenerator);
-        int size = InputView.readBridgeSize();
-        List<String> userBridge = user.makeBridge(size);
-        BridgeGame Game = new BridgeGame(userBridge);
-        System.out.println(userBridge);
-        playGame(size, Game);
+        new Game();
     }
 
-    public static void playGame(int size, BridgeGame Game) {
-        int index = 0;
-        String nowResult = "";
-        int matchNum = 1;
-        while (index != size) {
-            String userDirection = InputView.readMoving();
-            nowResult = String.valueOf(Game.move(userDirection,index));
-            OutputView.printMap(nowResult);
-            index += 1;
-            if (nowResult.contains("X")) {
-                index = 0;
-                String userRetry = InputView.readGameCommand();
-                if (userRetry.equals("Q")) {
-                    break;
-                }
-                matchNum += 1;
-                Game.retry();
-            }
-        }
-        if (index == size) {
-            endGame(nowResult,true,matchNum);
-        }
-        if (index != size) {
-            endGame(nowResult,false,matchNum);
-        }
-    }
 
-    public static void endGame(String finalResult, boolean matchResult, int matchNum) {
-        OutputView.printResult(finalResult, matchResult,matchNum);
-
-    }
 }
--- src/main/java/bridge/Game.java
+++ src/main/java/bridge/Game.java
@@ -12,6 +12,7 @@ public class Game {
 
     public Game() {
         makeBridge();
+        playGame();
     }
 
     private void makeBridge() {
@@ -21,5 +22,37 @@ public class Game {
         List<String> userBridge = user.makeBridge(size);
         this.bridgeGame = new BridgeGame(userBridge);
         System.out.println(userBridge);
+    }
+
+    public static void playGame() {
+        int index = 0;
+        String nowResult = "";
+        int matchNum = 1;
+        while (index != size) {
+            String userDirection = InputView.readMoving();
+            nowResult = String.valueOf(bridgeGame.move(userDirection,index));
+            OutputView.printMap(nowResult);
+            index += 1;
+            if (nowResult.contains("X")) {
+                index = 0;
+                String userRetry = InputView.readGameCommand();
+                if (userRetry.equals("Q")) {
+                    break;
+                }
+                matchNum += 1;
+                bridgeGame.retry();
+            }
+        }
+        if (index == size) {
+            endGame(nowResult,true,matchNum);
+        }
+        if (index != size) {
+            endGame(nowResult,false,matchNum);
+        }
+    }
+
+    public static void endGame(String finalResult, boolean matchResult, int matchNum) {
+        OutputView.printResult(finalResult, matchResult,matchNum);
+
     }
 }
