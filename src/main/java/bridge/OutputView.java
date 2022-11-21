0a7762ed15b410f51c42ca803de4a2ebda437e0c
yoon-yoo-tak <dbsdbxkrzz@naver.com>
2022-11-22 02:15:48 +0900

    feat: 다리 출력 및 게임 결과 출력 구현
    게임 상황에 따른 다리 출력과 최종 게임 결과를 출력하는 메서드를 구현합니다

--- src/main/java/bridge/OutputView.java
+++ src/main/java/bridge/OutputView.java
@@ -1,19 +1,43 @@
 package bridge;
 
+import bridge.constant.BridgeConstant;
+import bridge.constant.GameCommand;
 import bridge.constant.Message;
+import java.util.List;
 
 /**
  * 사용자에게 게임 진행 상황과 결과를 출력하는 역할을 한다.
  */
 public class OutputView {
 
+    private static String upperSide;
+    private static String lowerSide;
+
     /**
      * 현재까지 이동한 다리의 상태를 정해진 형식에 맞춰 출력한다.
      * <p>
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void printMap() {
-        
+    public static void printMap(List<String> bridge, List<String> playerInput) {
+        upperSide = "";
+        lowerSide = "";
+        connectBridge(BridgeConstant.LEFT_WRAPPER.getBridgeConstant());
+        for (int i = 0; i < playerInput.size(); i++) {
+            if (0 < i && i < bridge.size()) {
+                connectBridge(BridgeConstant.JOINT.getBridgeConstant());
+            }
+            String answer = bridge.get(i);
+            String currentPlayerInput = playerInput.get(i);
+            if (currentPlayerInput.equals(answer)) {
+                recordResult(currentPlayerInput, BridgeConstant.CAN_GO.getBridgeConstant());
+            }
+            if (!currentPlayerInput.equals(answer)) {
+                recordResult(currentPlayerInput, BridgeConstant.CAN_NOT_GO.getBridgeConstant());
+            }
+        }
+        connectBridge(BridgeConstant.RIGHT_WRAPPER.getBridgeConstant());
+        System.out.println(upperSide);
+        System.out.println(lowerSide);
     }
 
     /**
@@ -21,7 +45,18 @@ public class OutputView {
      * <p>
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void printResult() {
+    public static void printResult(int gamePlayCount, boolean isClear) {
+        System.out.println(Message.FINAL_RESULT.getMessage());
+        System.out.println(upperSide);
+        System.out.println(lowerSide);
+        System.out.print(Message.GAME_RESULT.getMessage());
+        if (isClear) {
+            System.out.println(Message.SUCCESS.getMessage());
+        }
+        if (!isClear) {
+            System.out.println(Message.FAIL.getMessage());
+        }
+        System.out.printf(Message.TOTAL_TRY.getMessage(), gamePlayCount);
     }
 
     public static void startGame() {
@@ -38,5 +73,21 @@ public class OutputView {
 
     public static void printRestartMessage() {
         System.out.println(Message.RESTART.getMessage());
+    }
+
+    public static void connectBridge(String bridgeComponent) {
+        upperSide += bridgeComponent;
+        lowerSide += bridgeComponent;
+    }
+
+    public static void recordResult(String currInput, String selectResult) {
+        if (currInput.equals(GameCommand.GO_UP.getGameCommand())) {
+            upperSide += selectResult;
+            lowerSide += " ";
+        }
+        if (currInput.equals(GameCommand.GO_DOWN.getGameCommand())) {
+            upperSide += " ";
+            lowerSide += selectResult;
+        }
     }
 }
