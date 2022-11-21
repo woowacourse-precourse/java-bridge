f041a5eae78a2a0dd8d4393691d4e9345140bc13
hyun0404woo <hyun0404woo@gmail.com>
2022-11-21 16:31:09 +0900

    feat: 게임 진행 상황 출력 메서드
    - 게임의 bridge와 비교해서 유저의 이동 상태를 출력한다.

--- src/main/java/bridge/view/OutputView.java
+++ src/main/java/bridge/view/OutputView.java
@@ -17,10 +17,10 @@ public class OutputView {
     
     /**
      * 현재까지 이동한 다리의 상태를 정해진 형식에 맞춰 출력한다.
-     * <p>
-     * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
+     * @param bridgeGameStatus
      */
-    public void printMap() {
+    public void printMap(BridgeGameStatus bridgeGameStatus) {
+        System.out.println(bridgeGameStatus);
     }
 
     /**
