d388c3fbfa8d8eee29b8d133696aaa5adbb57474
plusjob70 <plusjob70@gmail.com>
2022-11-21 17:31:29 +0900

    refactor(InputValidator): 하드코딩 -> 상수화

--- src/main/java/bridge/util/validator/InputValidator.java
+++ src/main/java/bridge/util/validator/InputValidator.java
@@ -1,27 +1,35 @@
 package bridge.util.validator;
 
+import static bridge.util.Constants.*;
 import static bridge.util.Error.*;
 
 public class InputValidator {
-    public static void bridgeSizeValidate(String input) {
-        if (!input.matches("[1-2]?\\d")) {
-            throw new IllegalArgumentException(OUT_OF_RANGE.toString());
-        }
+    public static final int BRIDGE_SIZE_LOWER_INCLUSIVE = 3;
+    public static final int BRIDGE_SIZE_UPPER_INCLUSIVE = 20;
+    public static final String VALID_NUMERIC_FORMAT = "[1-2]?\\d";
 
-        int number = Integer.parseInt(input);
-        if (number < 3 || number > 20) {
+    public static void bridgeSizeValidate(String input) {
+        int number = parseBridgeSize(input);
+        if (number < BRIDGE_SIZE_LOWER_INCLUSIVE || number > BRIDGE_SIZE_UPPER_INCLUSIVE) {
             throw new IllegalArgumentException(OUT_OF_RANGE.toString());
         }
     }
 
+    private static int parseBridgeSize(String input) {
+        if (!input.matches(VALID_NUMERIC_FORMAT)) {
+            throw new IllegalArgumentException(OUT_OF_RANGE.toString());
+        }
+        return Integer.parseInt(input);
+    }
+
     public static void moveDirectionValidate(String input) {
-        if (!(input.equals("U") || input.equals("D"))) {
+        if (!(input.equals(UP) || input.equals(DOWN))) {
             throw new IllegalArgumentException(NOT_U_NOR_D.toString());
         }
     }
 
     public static void retryValidate(String input) {
-        if (!(input.equals("R") || input.equals("Q"))) {
+        if (!(input.equals(RETRY) || input.equals(QUIT))) {
             throw new IllegalArgumentException(NOT_R_NOR_Q.toString());
         }
     }
