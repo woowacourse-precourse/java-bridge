cc4b74df62c5b974799ad00b97144904d45085b2
sanghyuk2 <givemesomefoodplz@gmail.com>
2022-11-22 01:40:33 +0900

    feat: 게임 시작 문구 출력 기능 구현

--- src/main/java/bridge/domain/bridgegame/BridgeGameController.java
+++ src/main/java/bridge/domain/bridgegame/BridgeGameController.java
@@ -1,10 +1,17 @@
 package bridge.domain.bridgegame;
 
 import bridge.domain.BridgeGameStarter;
+import bridge.io.output.OutputView;
 
 public class BridgeGameController implements BridgeGameStarter {
+    private final OutputView outputView;
+
+    public BridgeGameController() {
+        this.outputView = new OutputView();
+    }
+
     @Override
     public void start() {
-
+        outputView.printStartMessage();
     }
 }
