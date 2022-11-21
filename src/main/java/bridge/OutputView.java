dd20069a88e47ade7367e4df0975ee2c796d1f10
comet7001 <comet7001@gmail.com>
2022-11-22 04:55:50 +0900

    refactor(OutputView): printMap 메서드 분할 리팩토링

--- src/main/java/bridge/OutputView.java
+++ src/main/java/bridge/OutputView.java
@@ -6,37 +6,56 @@ import Statements.NormalStatements;
 public class OutputView {
 
     public void printMap(int movingTrials, int endOfBridges, String upOrDown) {
-        if(upOrDown=="U"&& movingTrials==1) {
-            BridgeShapes.FIRST_O_BRIDGE.build();
-            BridgeShapes.FIRST_EMPTY_BRIDGE.build();
-            return;
-        }
-        if(upOrDown=="D"&& movingTrials==1){
-            BridgeShapes.FIRST_EMPTY_BRIDGE.build();
-            BridgeShapes.FIRST_O_BRIDGE.build();
-            return;
-        }
-        if(upOrDown=="U"&& movingTrials!=endOfBridges){
-            BridgeShapes.FIRST_O_BRIDGE_OPENED.build();
-            BridgeShapes.FIRST_EMPTY_BRIDGE_OPENED.build();
-            return;
-        }
-        if(upOrDown=="D"&& movingTrials!=endOfBridges){
-            BridgeShapes.FIRST_EMPTY_BRIDGE_OPENED.build();
-            BridgeShapes.FIRST_O_BRIDGE_OPENED.build();
-            return;
-        }
-        if(upOrDown=="U"&&movingTrials==endOfBridges){
-            BridgeShapes.LAST_O_BRIDGE.build();
-            BridgeShapes.LAST_EMPTY_BRIDGE.build();
-            return;
-        }
-        if(upOrDown=="D"&&movingTrials==endOfBridges){
-            BridgeShapes.LAST_EMPTY_BRIDGE.build();
-            BridgeShapes.LAST_O_BRIDGE.build();
-        }
-
+        printFirstUpOBridge(movingTrials, endOfBridges, upOrDown);
+        printFirstDownOBridge(movingTrials, endOfBridges, upOrDown);
+        printOngoingUpOBridge(movingTrials, endOfBridges, upOrDown);
+        printOngoingDownOBridge(movingTrials, endOfBridges, upOrDown);
+        printLastUpOBridge(movingTrials, endOfBridges, upOrDown);
+        printLastDownOBridge(movingTrials, endOfBridges, upOrDown);
     }
+
+    private void printFirstUpOBridge(int movingTrials, int endOfBridges, String upOrDown){
+        if(upOrDown=="U"&& movingTrials==1) {
+            System.out.println(BridgeShapes.FIRST_O_BRIDGE.build());
+            System.out.println(BridgeShapes.FIRST_EMPTY_BRIDGE.build());
+        }
+    }
+
+    private void printFirstDownOBridge(int movingTrials, int endOfBridges, String upOrDown){
+        if(upOrDown=="D"&& movingTrials==1) {
+            System.out.println(BridgeShapes.FIRST_EMPTY_BRIDGE.build());
+            System.out.println(BridgeShapes.FIRST_O_BRIDGE.build());
+        }
+    }
+
+    private void printOngoingUpOBridge(int movingTrials, int endOfBridges, String upOrDown){
+        if(upOrDown=="U"&& movingTrials!=1&& movingTrials!=endOfBridges) {
+            System.out.println(BridgeShapes.FIRST_O_BRIDGE_OPENED.build());
+            System.out.println(BridgeShapes.FIRST_EMPTY_BRIDGE_OPENED.build());
+        }
+    }
+
+    private void printOngoingDownOBridge(int movingTrials, int endOfBridges, String upOrDown){
+        if(upOrDown=="D"&& movingTrials!=1 && movingTrials !=endOfBridges) {
+            System.out.println(BridgeShapes.FIRST_EMPTY_BRIDGE_OPENED.build());
+            System.out.println(BridgeShapes.FIRST_O_BRIDGE_OPENED.build());
+        }
+    }
+
+    private void printLastUpOBridge(int movingTrials, int endOfBridges, String upOrDown){
+        if(upOrDown=="U"&& movingTrials==endOfBridges) {
+            System.out.println(BridgeShapes.LAST_O_BRIDGE.build());
+            System.out.println(BridgeShapes.LAST_EMPTY_BRIDGE.build());
+        }
+    }
+
+    private void printLastDownOBridge(int movingTrials, int endOfBridges, String upOrDown){
+        if(upOrDown=="D"&& movingTrials==endOfBridges) {
+            System.out.println(BridgeShapes.LAST_EMPTY_BRIDGE.build());
+            System.out.println(BridgeShapes.LAST_O_BRIDGE.build());
+        }
+    }
+
 
     public void printFailedMap(int movingTrials, int endOfBridges, String upOrDown){
         if(upOrDown=="U"&& movingTrials==1) {
