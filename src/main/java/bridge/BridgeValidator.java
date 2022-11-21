4e612f76684bb77e1d7fc157888ef1ed28f5d1d8
aksmf1442 <aksmf1442@naver.com>
2022-11-21 15:55:51 +0900

    test(다리 건너기): 다리 길이가 3~20 사이의 숫자가 아닌 경우 예외를 발생시키는 기능 테스트 구현

---
+++ src/main/java/bridge/BridgeValidator.java
@@ -0,0 +1,8 @@
+package bridge;
+
+public class BridgeValidator {
+
+    public void validateBridgeSize(String size) {
+
+    }
+}
---
+++ src/test/java/bridge/BridgeValidatorTest.java
@@ -0,0 +1,30 @@
+package bridge;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class BridgeValidatorTest {
+
+    @DisplayName("[예외 발생] 다리 길이가 3~20 사이의 숫자가 아닌 문자열일 경우 IllegalArgumentException 발생")
+    @Test
+    void validateBridgeSizeByString() {
+        String size = "asdf";
+        BridgeValidator validator = new BridgeValidator();
+        assertThatThrownBy(() -> validator.validateBridgeSize(size)).isInstanceOf(
+            IllegalArgumentException.class);
+    }
+
+    @DisplayName("[예외 발생] 다리 길이가 3~20의 숫자 범위를 벗어나는 경우 IllegalArumentException 발생")
+    @Test
+    void validateBridgeSizeByNumberRange() {
+        String size = "21";
+        BridgeValidator validator = new BridgeValidator();
+        assertThatThrownBy(() -> validator.validateBridgeSize(size)).isInstanceOf(
+            IllegalArgumentException.class);
+    }
+}
