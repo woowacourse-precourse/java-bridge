762aee846593a49e02f805f670668bbb13d18e18
devMuscle <hongsbien@naver.com>
2022-11-22 00:20:40 +0900

    feat: 다리 길이 입력받고 검증하기

--- src/main/java/bridge/InputView.java
+++ src/main/java/bridge/InputView.java
@@ -1,5 +1,7 @@
 package bridge;
 
+import camp.nextstep.edu.missionutils.Console;
+
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
  */
@@ -9,7 +11,27 @@ public class InputView {
      * 다리의 길이를 입력받는다.
      */
     public int readBridgeSize() {
-        return 0;
+        int bridgeSize;
+
+        try {
+            bridgeSize = Integer.parseInt(Console.readLine());
+            validateBridgeSize(bridgeSize);
+        }catch(IllegalArgumentException e) {
+            System.out.println("[ERROR] 다리 길이는 3부터 20 사이의 숫자여야 합니다.");
+            bridgeSize = Integer.parseInt(Console.readLine());
+            validateBridgeSize(bridgeSize);
+        }
+
+        return bridgeSize;
+    }
+
+    /**
+     * 입력받은 다리의 길이를 검증한다.
+     */
+    public void validateBridgeSize(int bridgeSize) {
+        if(bridgeSize<3 || bridgeSize>20) {
+            throw new IllegalArgumentException();
+        }
     }
 
     /**
