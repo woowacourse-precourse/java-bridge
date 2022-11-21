4ef13e08d466f72e297fc58e8c8f4f3178cd2411
bjk1649 <qhwns1649@naver.com>
2022-11-22 01:36:59 +0900

    feat: 사용자의 입력에 따라 칸을 이동하는 기능 구현

--- src/main/java/bridge/service/BridgeGame.java
+++ src/main/java/bridge/service/BridgeGame.java
@@ -42,10 +42,16 @@ public class BridgeGame {
 
     /**
      * 사용자가 칸을 이동할 때 사용하는 메서드
-     * <p>
-     * 이동을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void move() {
+    public boolean move(String move, String answerMove) {
+        boolean result = isCorrectMove(move, answerMove);
+        userMoves.add(move);
+        results.add(result);
+        return result;
+    }
+
+    private boolean isCorrectMove(String userMove, String answerMove) {
+        return userMove.equals(answerMove);
     }
 
     /**
