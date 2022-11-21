ccfeca62eef25c94e9fbb96880b2943e2652a7fd
dhkim <akdlzmf1123@gmail.com>
2022-11-21 15:47:49 +0900

    feat(BridgeGameController, Application): 컨트롤러와 프로그램 진입점 구현

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -1,8 +1,20 @@
 package bridge;
 
+import bridge.controller.BridgeGameController;
+
 public class Application {
 
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        BridgeGameController bridgeGameController = new BridgeGameController(bridgeMaker());
+        bridgeGameController.run();
+    }
+
+    private static BridgeMaker bridgeMaker() {
+        return new BridgeMaker(bridgeNumberGenerator());
+    }
+
+    private static BridgeNumberGenerator bridgeNumberGenerator() {
+        return new BridgeRandomNumberGenerator();
     }
 }
--- src/main/java/bridge/controller/BridgeGameController.java
+++ src/main/java/bridge/controller/BridgeGameController.java
@@ -1,4 +1,65 @@
 package bridge.controller;
 
+import bridge.BridgeMaker;
+import bridge.model.Bridge;
+import bridge.model.BridgeGame;
+import bridge.model.GameCommand;
+import bridge.model.Player;
+import bridge.view.InputView;
+import bridge.view.OutputView;
+
 public class BridgeGameController {
+    private final InputView inputView;
+    private final OutputView outputView;
+    private final BridgeMaker bridgeMaker;
+
+    public BridgeGameController(BridgeMaker bridgeMaker) {
+        inputView = InputView.getInstance();
+        outputView = OutputView.getInstance();
+        this.bridgeMaker = bridgeMaker;
+    }
+
+    public void run() {
+        outputView.printInit();
+        Bridge bridge = makeBridge();
+        BridgeGame bridgeGame = new BridgeGame(bridge);
+        bridgeGame.initialize();
+        Player player = new Player();
+        while (bridgeGame.isContinuing()) {
+            player = new Player();
+            play(player, bridgeGame);
+            if (bridgeGame.isPending()) {
+                String gameCommand = inputView.readGameCommand();
+                bridgeGame.retry(GameCommand.toGameCommand(gameCommand));
+            }
+        }
+        outputView.printResult(player, bridgeGame);
+    }
+
+    private Bridge makeBridge() {
+        try {
+            int bridgeSize = inputView.readBridgeSize();
+            return bridgeMaker.constructBridge(bridgeSize);
+        } catch (IllegalArgumentException e) {
+            outputView.printErrorMessage(e.getMessage());
+            return makeBridge();
+        }
+    }
+
+    private void play(Player player, BridgeGame bridgeGame) {
+        while (bridgeGame.isContinuing()) {
+            movePlayer(player, bridgeGame);
+            outputView.printMap(player);
+        }
+    }
+
+    private void movePlayer(Player player, BridgeGame bridgeGame) {
+        try {
+            String moving = inputView.readMoving();
+            bridgeGame.move(player, moving);
+        } catch (IllegalArgumentException e) {
+            outputView.printErrorMessage(e.getMessage());
+            movePlayer(player, bridgeGame);
+        }
+    }
 }
