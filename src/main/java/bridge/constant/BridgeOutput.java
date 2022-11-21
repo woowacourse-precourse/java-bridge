01d6fc29a4d6f6aa1ba12394dd0b879d5ef8f235
CMHyuk <alsgur9710@naver.com>
2022-11-21 22:33:04 +0900

    feat(BridgeOutput): 다리 관련 상수

---
+++ src/main/java/bridge/constant/BridgeOutput.java
@@ -0,0 +1,9 @@
+package bridge.constant;
+
+public class BridgeOutput {
+    public static final String INITIAL_SETTING = "[]";
+    public static final String FIRST_POINT = "[";
+    public static final String END_POINT = "]";
+    public static final String CHANGE_POINT = "[|";
+    public static final String EMPTY = "|   ]";
+}
