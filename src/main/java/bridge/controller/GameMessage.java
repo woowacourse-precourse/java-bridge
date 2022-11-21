4872267cce0791256b02da7353045fec6040f68f
yoonjuho <dbswn990@naver.com>
2022-11-22 05:25:46 +0900

    refactor(GameMessage): 고정 문자열 이름 변경

--- src/main/java/bridge/controller/GameMessage.java
+++ src/main/java/bridge/controller/GameMessage.java
@@ -3,7 +3,7 @@ package bridge.controller;
 public class GameMessage {
     public static final String RETRY = "R";
     public static final String CLOSE = "Q";
-    public static final String WIN = "성공";
-    public static final String LOSE = "실패";
+    public static final String SUCCESS = "성공";
+    public static final String FAILURE = "실패";
 
 }
