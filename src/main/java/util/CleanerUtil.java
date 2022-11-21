b2075d6f414627af69ce63644d01902cc7e745c5
nylonmusk <106235834+nylonmusk@users.noreply.github.com>
2022-11-21 20:11:51 +0900

    refactor(CleanerUtil): 가독성 향상을 위해 Constant를 클래스변수로 선언

--- src/main/java/util/CleanerUtil.java
+++ src/main/java/util/CleanerUtil.java
@@ -1,17 +1,14 @@
 package util;
 
 import constant.BothSideConstant;
-import constant.NumberConstant;
 import java.util.List;
-import model.BridgeGame;
 
 public class CleanerUtil {
-    public static void clearBothSide(List<List<String>> bothSide) {
-        bothSide.get(BothSideConstant.UP_SIDE.getConstant()).clear();
-        bothSide.get(BothSideConstant.DOWN_SIDE.getConstant()).clear();
-    }
+    private static final int UP_SIDE_INDEX = BothSideConstant.UP_SIDE_INDEX.getConstant();
+    private static final int DOWN_SIDE_INDEX = BothSideConstant.DOWN_SIDE_INDEX.getConstant();
 
-    public static void clearTryNumber() {
-        BridgeGame.tryNumber = NumberConstant.FIRST_TRY.getConstant();
+    public static void clearBothSide(List<List<String>> bothSide) {
+        bothSide.get(UP_SIDE_INDEX).clear();
+        bothSide.get(DOWN_SIDE_INDEX).clear();
     }
 }
