8af7b0832121f2606a5b4ba6e89fb13e16016598
leorb159 <ldgsocial159@naver.com>
2022-11-22 03:30:43 +0900

    feat(InputView): readMoving 구현

--- src/main/java/bridge/InputView.java
+++ src/main/java/bridge/InputView.java
@@ -33,9 +33,12 @@ public class InputView {
      * 사용자가 이동할 칸을 입력받는다.
      */
     public String readMoving() {
-        return null;
-    }
+        System.out.println("이동할 칸을 선택해주세요. (위: U, 아래: D)");
+        String input = Console.readLine();
 
+        return input;
+    }
+    
     /**
      * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
      */
