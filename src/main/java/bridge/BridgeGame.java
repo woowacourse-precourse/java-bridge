52527b241479deb3fe9eedcdc00c6f467841dc20
Choi Seongwon <vlflalektzja@naver.com>
2022-11-21 23:25:10 +0900

    feat, chore: 게임 재시도 구현 및 변수명 변경

--- src/main/java/bridge/BridgeGame.java
+++ src/main/java/bridge/BridgeGame.java
@@ -7,10 +7,11 @@ import java.util.List;
  * 다리 건너기 게임을 관리하는 클래스
  */
 public class BridgeGame {
-    public static final int UP_WARD = 0;
-    public static final int DOWN_WARD = 1;
+    public static final int UPWARD = 0;
+    public static final int DOWNWARD = 1;
     private List<String> bridge;
     private List<List<String>> userInputBridge;
+    private int tryCount = 0;
     int index;
 
     public BridgeGame(int bridgeSize) {
@@ -42,23 +43,23 @@ public class BridgeGame {
 
     private void moveWrong(String moving) {
         if (moving.equals("U")) {
-            userInputBridge.get(UP_WARD).add("X");
-            userInputBridge.get(DOWN_WARD).add(" ");
+            userInputBridge.get(UPWARD).add("X");
+            userInputBridge.get(DOWNWARD).add(" ");
         }
         if (moving.equals("D")) {
-            userInputBridge.get(UP_WARD).add(" ");
-            userInputBridge.get(DOWN_WARD).add("X");
+            userInputBridge.get(UPWARD).add(" ");
+            userInputBridge.get(DOWNWARD).add("X");
         }
     }
 
     private void moveCorrect(String moving) {
         if (moving.equals("U")) {
-            userInputBridge.get(UP_WARD).add("O");
-            userInputBridge.get(DOWN_WARD).add(" ");
+            userInputBridge.get(UPWARD).add("O");
+            userInputBridge.get(DOWNWARD).add(" ");
         }
         if (moving.equals("D")) {
-            userInputBridge.get(UP_WARD).add(" ");
-            userInputBridge.get(DOWN_WARD).add("O");
+            userInputBridge.get(UPWARD).add(" ");
+            userInputBridge.get(DOWNWARD).add("O");
         }
     }
 
@@ -69,5 +70,13 @@ public class BridgeGame {
      * 재시작을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
     public void retry() {
+        for (int i = 0; i < 2; i++) {
+            userInputBridge.get(i).clear();
+        }
+        tryCount++;
+    }
+
+    public int getTryCount() {
+        return tryCount;
     }
 }
