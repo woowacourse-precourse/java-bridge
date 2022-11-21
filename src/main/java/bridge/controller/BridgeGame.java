86ce10d44d75b56c87387c92aaf764d9da15fc40
hyun0404woo <hyun0404woo@gmail.com>
2022-11-21 15:58:13 +0900

    feat:  Bridge 생성 메서드
    - bridgeSize를 입력받아서 랜덤한 다리를 생성한다.

--- src/main/java/bridge/controller/BridgeGame.java
+++ src/main/java/bridge/controller/BridgeGame.java
@@ -22,6 +22,14 @@ public class BridgeGame {
     }
 
     /**
+     * 게임 시작 직후 다리를 생성할 때 사용하는 메서드
+     * @param bridgeSize
+     */
+    public void initialize(int bridgeSize) {
+        gameBridge = bridgeMaker.makeBridge(bridgeSize);
+    }
+
+    /**
      * 사용자가 칸을 이동할 때 사용하는 메서드
      * <p>
      * 이동을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
