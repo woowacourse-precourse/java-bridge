12200f01419b8c0270d015c3a8ad6a1884bc112a
NAM GIBEOM <rlqja0523@naver.com>
2022-11-21 21:30:47 +0900

    feat: 비어있는 값 예외 추가

--- src/main/java/bridge/exception/UserException.java
+++ src/main/java/bridge/exception/UserException.java
@@ -3,4 +3,10 @@ package bridge.exception;
 public abstract class UserException {
 
     public abstract void checkException(String userInput);
+
+    void isEmpty(String input) {
+        if (input.isEmpty()) {
+            throw new IllegalArgumentException(ExceptionCode.IS_EMPTY.getMessage());
+        }
+    }
 }
