eb7b4397e274a0528bdc7e26591de4bfbc7cf2d5
qpdh <74190329+qpdh@users.noreply.github.com>
2022-11-22 02:25:28 +0900

    Feat($InputView) readBridgeSize 메소드 구현

--- src/main/java/bridge/InputView.java
+++ src/main/java/bridge/InputView.java
@@ -1,5 +1,7 @@
 package bridge;
 
+import static camp.nextstep.edu.missionutils.Console.readLine;
+
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
  */
@@ -9,7 +11,31 @@ public class InputView {
      * 다리의 길이를 입력받는다.
      */
     public int readBridgeSize() {
-        return 0;
+        String userInput = readLine();
+        try {
+            return validateBridgeSize(userInput);
+        } catch (IllegalArgumentException e) {
+            return readBridgeSize();
+        }
+    }
+
+    /**
+     * 다리의 길이가 적절한 값인지 판단한다.
+     * 예외 발생시 IllegalArgumentException을 발생시킨다.
+     *
+     * @param userInput 사용자가 입력한 문자열
+     * @return <p>[3,20] - 올바른 값
+     * <p>-1 - 올바르지 않은 값
+     */
+    public int validateBridgeSize(String userInput) {
+        try {
+            int number = Integer.parseInt(userInput);
+            if (3 <= number && number <= 20) return number;
+            System.out.println("[ERROR] 다리 크기는 3이상 20이하의 자연수여야 합니다.");
+        } catch (NumberFormatException e) {
+            System.out.println("[ERROR] 다리 크기는 정수여야 합니다.");
+        }
+        throw new IllegalArgumentException();
     }
 
     /**
