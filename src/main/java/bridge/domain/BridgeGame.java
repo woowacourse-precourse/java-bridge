3a4f6af0757a642c6a6637735b5c5ae2678866e5
WonjunS <seowj0710@gmail.com>
2022-11-21 15:20:26 +1100

    update: removed unnecessary codes

--- src/main/java/bridge/domain/BridgeGame.java
+++ src/main/java/bridge/domain/BridgeGame.java
@@ -1,7 +1,5 @@
 package bridge.domain;
 
-import bridge.BridgeRandomNumberGenerator;
-
 import java.util.ArrayList;
 import java.util.List;
 
@@ -21,11 +19,6 @@ public class BridgeGame {
         return downBridge;
     }
 
-    /**
-     * 사용자가 칸을 이동할 때 사용하는 메서드
-     * <p>
-     * 이동을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
-     */
     public static void move(List<String> bridge, int idx, String input) {
         up(bridge, idx, input);
         down(bridge, idx, input);
@@ -67,11 +60,6 @@ public class BridgeGame {
         downBridge.clear();
     }
 
-    /**
-     * 사용자가 게임을 다시 시도할 때 사용하는 메서드
-     * <p>
-     * 재시작을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
-     */
     public static boolean retry(String input) {
         if(input.equals("Q")) {
             return false;
