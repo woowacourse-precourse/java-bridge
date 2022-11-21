3b26f04d1cb220899d0a117518ba1131054a4e65
gnoesnooj <306yyy@naver.com>
2022-11-21 22:17:39 +0800

    feat(main) : 어플리케이션 실행 위한 코드 작성

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -1,8 +1,14 @@
 package bridge;
 
+import bridge.controller.BridgeController;
+import bridge.service.BridgeService;
+
 public class Application {
 
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        BridgeService bridgeService = new BridgeService();
+        BridgeController bridgeController = new BridgeController(bridgeService);
+        bridgeController.run();
     }
 }
