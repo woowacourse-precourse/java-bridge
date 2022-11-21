31cdc3225266eea8da485a50a975d3b4af9b12da
gwj0421 <gwj0421@gmail.com>
2022-11-21 18:30:05 +0900

    feat: 사용자가 입력한 값("U"혹은 "D")과 컴퓨터가 랜덤으로 선택한 값("U"혹은 "D")와 비교하여 같으면 True를 틀리면 False를 반환하는 함수 추가

--- src/main/java/bridge/BridgeGame.java
+++ src/main/java/bridge/BridgeGame.java
@@ -1,9 +1,18 @@
 package bridge;
 
+import java.util.List;
+
 /**
  * 다리 건너기 게임을 관리하는 클래스
  */
 public class BridgeGame {
+
+    public boolean check_same(List<String> RandomBridgeMap, int idx, String playrerStep) {
+        if (!RandomBridgeMap.get(idx).equals(playrerStep)) {
+            return true;
+        }
+        return false;
+    }
 
     /**
      * 사용자가 칸을 이동할 때 사용하는 메서드
