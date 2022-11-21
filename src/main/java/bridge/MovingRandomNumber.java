ebe83345068a2bf8aa732ed886be43ed7d9c0afa
Amyhds <dasomhan524@gmail.com>
2022-11-22 01:03:11 +0900

    refactor(movingrandomnumber): 클래스 및 메서드 이름 변경

--- src/main/java/bridge/MovingRandomNumber.java
+++ src/main/java/bridge/MovingRandomNumber.java
@@ -1,6 +1,6 @@
 package bridge;
 
-public enum MovingNumber {
+public enum MovingRandomNumber {
     UP("U", 1),
     DOWN("D", 0);
 
@@ -8,7 +8,7 @@ public enum MovingNumber {
     private int randomNumber;
     private String choice;
 
-    MovingNumber(String moving, int randomNumber) {
+    MovingRandomNumber(String moving, int randomNumber) {
         this.moving = moving;
         this.randomNumber = randomNumber;
     }
