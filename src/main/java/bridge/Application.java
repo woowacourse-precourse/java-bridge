b1eeb1e93f7438ca66fe5695064d2b0365681e14
zeunala <zeunala@naver.com>
2022-11-22 01:19:07 +0900

    feat: 재시작시 처음에 만든 다리로 재시도 기능 추가

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -10,10 +10,20 @@ public class Application {
         OutputView outputView = new OutputView();
         BridgeMaker bridgeMaker = new BridgeMaker(new BridgeRandomNumberGenerator());
         BridgeGame bridgeGame = new BridgeGame(bridgeMaker.makeBridge(inputView.readBridgeSize()));
-        bridgeGame.move(inputView.readMoving());
-        outputView.printMap(bridgeGame);
-        if (!bridgeGame.isCorrectChoice()) {
-            inputView.readGameCommand();
+
+        while (!bridgeGame.isSuccess()) {
+            bridgeGame.move(inputView.readMoving());
+            outputView.printMap(bridgeGame);
+            if (bridgeGame.isCorrectChoice()) {
+                continue;
+            }
+            String gameCommand = inputView.readGameCommand();
+            if (gameCommand.equals("R")) {
+                bridgeGame.retry();
+            }
+            if (gameCommand.equals("Q")) {
+                break;
+            }
         }
     }
 }
