17634842d4abe63bf08015bd0935324138055660
tseop <77163842+tseop@users.noreply.github.com>
2022-11-21 23:32:36 +0900

    feat: 게임 진행에 필요한 상수 클래스 생성

---
+++ src/main/java/bridge/resource/GameConstant.java
@@ -0,0 +1,16 @@
+package bridge.resource;
+
+public final class GameConstant {
+    public static final String TOP = "U";
+    public static final String BOTTOM = "D";
+    public static final String GOOD = "O";
+    public static final String BAD = "X";
+    public static final String RE = "R";
+    public static final String EXIT = "Q";
+    public static final String SPACE = " ";
+    public static final String MID_SYMBOL = " | ";
+    public static final String SUCCESS = "성공";
+    public static final String FAILURE = "실패";
+
+
+}
