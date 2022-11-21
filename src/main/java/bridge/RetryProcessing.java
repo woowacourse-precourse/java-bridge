296e95c70223ac248700e2f20eab23ab7f7fe51f
kissshot1104 <kissshot.heartunderblade1104@gmail.com>
2022-11-21 19:09:19 +0900

    Feat(printResult) : 정해진 출력 형식으로 가공 기능
    public void printResult(List<String> upSide, List<String> downSide, int count)
    메소드를 추가했습니다.
    이 메소드는 최종적을 입력된 다리의 정보를 정해진 출력 형식을 가공한다.

--- src/main/java/bridge/RetryProcessing.java
+++ src/main/java/bridge/RetryProcessing.java
@@ -13,6 +13,13 @@ public class RetryProcessing {
         outputView = new OutputView();
     }
 
+    //최종적으로 입력된 다리의 정보를 정해진 출력 형식으로 가공한다.
+    public void printResult(List<String> upSide, List<String> downSide, int count){
+        String upSideBridge = upSide.stream().collect(Collectors.joining(" | ", "[ ", " ]"));
+        String downSideBridge = downSide.stream().collect(Collectors.joining(" | ", "[ ", " ]"));
+        outputView.printResult(upSideBridge, downSideBridge, count);
+    }
+
     //리트라이를 할 때 R을 눌렀는지 아닌지 확인
     private boolean inputRetryCommand(String inputCommand) {
         boolean doRetry = inputCommand.equals("R");
