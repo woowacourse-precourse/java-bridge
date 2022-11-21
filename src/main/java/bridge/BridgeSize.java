c17228748eb0e5c5b537595a8c901aea11c4a402
Amyhds <dasomhan524@gmail.com>
2022-11-21 17:36:58 +0900

    feat(bridgesize): 생성자 구현 및 다리 길이 반환 메서드 구현

--- src/main/java/bridge/BridgeSize.java
+++ src/main/java/bridge/BridgeSize.java
@@ -1,4 +1,16 @@
 package bridge;
 
 public class BridgeSize {
+    private int bridgeSize;
+
+    public BridgeSize(String userBridgeSize) {
+        Validator.validateNumber(userBridgeSize);
+        int tempBridgeSize = Converter.convertToNumber(userBridgeSize);
+        Validator.validateRange(tempBridgeSize);
+        bridgeSize = tempBridgeSize;
+    }
+
+    public int getBridgeSize() {
+        return bridgeSize;
+    }
 }
