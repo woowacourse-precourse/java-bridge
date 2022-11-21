69ad452d78bfca36ff01d9f447738f3df935466b
sen <seeunchoi99@gmail.com>
2022-11-22 01:00:37 +0900

    ✨ InputView.readGameCommand 구현

--- src/main/java/bridge/view/InputView.java
+++ src/main/java/bridge/view/InputView.java
@@ -9,6 +9,7 @@ import java.util.NoSuchElementException;
 public class InputView {
     private static final String INPUT_LENGTH_OF_BRIDGE_MSG = "다리의 길이를 입력해주세요.";
     private static final String INPUT_SELECT_TO_GO_MSG = "이동할 칸을 선택해주세요. (위: U, 아래: D)";
+    private static final String INPUT_SELECT_TO_RETRY_MSG = "게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)";
 
     private String getInput() {
         try {
@@ -58,6 +59,14 @@ public class InputView {
      * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
      */
     public String readGameCommand() {
-        return null;
+        System.out.println(INPUT_SELECT_TO_RETRY_MSG);
+        return validateGameCommand(getInput());
+    }
+
+    private String validateGameCommand(String input) {
+        if (!input.matches("^[RQ]$")) {
+            throw new IllegalArgumentException();
+        }
+        return input;
     }
 }
