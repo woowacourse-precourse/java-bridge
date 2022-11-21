258fba83b0de329eb84206c695887a89480dc50a
viyott-lover <jackk0616@gmail.com>
2022-11-21 21:31:45 +0900

    feat(BridgeGame): 사용자가 게임을 다시 시도할 떄 사용하는 retry() 메소드 작성

--- src/main/java/bridge/model/BridgeGame.java
+++ src/main/java/bridge/model/BridgeGame.java
@@ -7,6 +7,8 @@ import java.util.List;
  * 다리 건너기 게임을 관리하는 클래스
  */
 public class BridgeGame {
+    private static final String retry = "R";
+    private static final String quit = "Q";
     private List<String> marks; // "O",
     private int gameCount;
     List<String> bridge;
@@ -51,7 +53,14 @@ public class BridgeGame {
      * <p>
      * 재시작을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void retry() {
+    public boolean retry(String cmd) {
+        if(cmd.equals(quit)) {
+            return false;
+        }
+        position = 0;
+        marks.clear();
+        gameCount++;
+        return true;
     }
 
     public void makeBridge(int size) {
