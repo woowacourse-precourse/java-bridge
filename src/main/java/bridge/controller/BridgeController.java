77abc5a6937abdf0741a7a1229fd9f5309b9f236
yoonjuho <dbswn990@naver.com>
2022-11-21 18:29:21 +0900

    refactor(BridgeController): 다리 이동 객체 호출 메서드 분리

--- src/main/java/bridge/controller/BridgeController.java
+++ src/main/java/bridge/controller/BridgeController.java
@@ -1,7 +1,7 @@
 package bridge.controller;
 
 import bridge.model.BridgeGame;
-import bridge.model.Row;
+import bridge.model.Column;
 import bridge.view.InputView;
 import bridge.view.OutputView;
 
@@ -12,39 +12,49 @@ public class BridgeController {
     private final InputView inputView;
     private final OutputView outputView;
     private final BridgeGame bridgeGame;
-    private final ValidateInput validate;
     private int totalCount;
     BridgeController(){
         inputView = new InputView();
         outputView = new OutputView();
         bridgeGame = new BridgeGame();
-        validate = new ValidateInput();
         totalCount = 1;
     }
     public void startGame(){
-        int size = validate.numeric(inputView.readBridgeSize());
+        int size = ValidateInput.checkNumeric(inputView.readBridgeSize());
         bridgeGame.start(size);
-        moveGame(size);
+        playing(size);
     }
 
-    private int moveGame(int size){
+    private int playing(int size){
         for(int step = 0; step < size; step++){
-            if(bridgeGame.move(step,validate.letter(inputView.readMoving()))){
+            if(checkAnswer(step)){
+                addToBuffer(step);
                 continue;
             }
-            return judgeGameEnd(step,size);
+            return judgeGameOver(step,size);
         }
         return 0;
     }
-    private int judgeGameEnd(int step, int size){
+
+    private boolean checkAnswer(int step){
+        String letter = inputView.readMoving();
+        ValidateInput.checkLetter(letter);
+        if(bridgeGame.move(step,letter)!= Column.NONE){
+            return true;
+        }
+        return false;
+    }
+    private int judgeGameOver(int step, int size){
         if(step == size-1){
             //게임 끝
         }
         // 게임 다시 시도
         // 출력
-        return moveGame(size);
+       // return moveGame(size);
     }
+    private void addToBuffer(String msg){
 
+    }
 
 
 }
