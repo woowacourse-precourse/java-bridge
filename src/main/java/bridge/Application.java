9e44687f7a5cd7cc6392834dbea29d0e7dcc3692
yewonbahn <banyaewon98@gmail.com>
2022-11-21 16:46:58 +0900

    기능 정리 추가 및 수정

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -1,19 +1,31 @@
 package bridge;
 
 import java.util.List;
+import java.util.Objects;
 
 public class Application {
 
     public static void main(String[] args) {
         int bridge_size;
         String blank;
+        int bridge_index=0;
+        boolean check = true;
         //다리 길이 입력 받기
         System.out.println("다리 건너기 게임을 시작합니다.");
         bridge_size=InputView.readBridgeSize();
         List<String> bridge=BridgeMaker.makeBridge(bridge_size);
         System.out.println(bridge);
-        blank=InputView.readMoving();
+        while (check==true){
+            blank=InputView.readMoving();
+            if(Objects.equals(bridge.get(bridge_index), blank)){
+                System.out.println("answer");
+            }
+            else{
+                check=false;
+            }
+            bridge_index+=1;
+        }
+        }
 
 
-    }
 }
