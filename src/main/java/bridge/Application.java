e497f201c23b5889535cb8db4a92cf41f1142244
RumbleKAT <reki318@naver.com>
2022-11-21 22:54:55 +0900

    (feat) 5. 다리게임 컨트롤러 구현

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -8,10 +8,64 @@ public class Application {
     private static BridgeGame bridgeGame;
     private static BridgeMaker bridgeMaker;
     private static OutputView outputView;
+    private static boolean flag;
 
     public static void main(String[] args) {
         init();
+        generateBridge();
+        playGame(true);
+    }
+    private static void playStage(){
+        outputView.printCase("play");
+        flag = calcBridge(inputView.readMoving());
+        outputView.printMap(bridgeGame.calcMap(bridgeGame.getUserBridge(),0,0), ' ', new StringBuilder());
+    }
 
+    private static void playGame(boolean isPlaying){
+        while(isPlaying){
+            playStage();
+            if(flag){
+                outputView.printCase("regame");
+                isPlaying = bridgeGame.retry(inputView.readGameCommand());
+            }
+            if(bridgeGame.isFinish()){
+                gameFinish();
+                break;
+            }
+        }
+    }
+
+    private static void gameFinish(){
+        if(flag) outputView.printResult("실패",bridgeGame.calcMap(bridgeGame.getUserBridge(),0,0), bridgeGame.getTryCnt());
+        else if(bridgeGame.isFinish()){
+            outputView.printResult("성공",bridgeGame.calcMap(bridgeGame.getUserBridge(),0,0), bridgeGame.getTryCnt());
+        }
+    }
+
+    private static boolean calcBridge(String command){
+        boolean flag = false;
+        List<String> curUserBridge = bridgeGame.getUserBridge();
+        curUserBridge.add(command);
+        if(bridgeGame.getUserBridge().size() >= 2 && !bridgeGame.move(curUserBridge.size()-1,command)){
+            curUserBridge.remove(curUserBridge.size()-1);
+            curUserBridge.add("X");
+            flag = true;
+        }
+        bridgeGame.setUserBridge(curUserBridge);
+        return flag;
+    }
+
+    private static boolean generateBridge(){
+        outputView.printCase("init");
+        int size = 0;
+        try{
+            size = inputView.readBridgeSize();
+            bridgeGame.setBridge(bridgeMaker.makeBridge(size));
+        }catch (IllegalArgumentException e){
+            System.out.println(e.getMessage());
+            return true;
+        }
+        return false;
     }
 
     private static void init(){
@@ -19,12 +73,6 @@ public class Application {
         outputView = new OutputView();
         bridgeMaker = new BridgeMaker(new BridgeRandomNumberGenerator());
         bridgeGame = new BridgeGame(new BridgeRandomNumberGenerator());
-
         bridgeGame.setTryCnt(1);
-        outputView.printInit();
-        int size = inputView.readBridgeSize();
-        bridgeGame.setBridge(bridgeMaker.makeBridge(size));
-
-        String command = inputView.readGameCommand();
     }
 }
