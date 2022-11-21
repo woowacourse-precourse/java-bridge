6e2805c258a861dbdbba0884176e33a222f4b759
syr9707 <yrshin9771@gmail.com>
2022-11-21 19:12:49 +0900

    feat(inputView): 유저의 이동할 칸 입력

--- src/main/java/bridge/InputView.java
+++ src/main/java/bridge/InputView.java
@@ -41,7 +41,14 @@ public class InputView {
      * 사용자가 이동할 칸을 입력받는다.
      */
     public String readMoving() {
-        return null;
+
+        System.out.println("이동할 칸을 선택해주세요. (위: U, 아래: D)");
+        MOVING = Console.readLine();
+        if(!(MOVING.equals("U") || MOVING.equals("D"))) {
+            throw new IllegalArgumentException(ERROR_MOVING);
+        }
+
+        return MOVING;
     }
 
     /**
