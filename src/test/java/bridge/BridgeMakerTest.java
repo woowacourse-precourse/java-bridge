2fd7088276b610eabdbbe861200e72eebcc31295
Jaeyeop-Jung <jjy5349@naver.com>
2022-11-21 19:58:00 +0900

    test: 테스트 코드에서 given에 주어지는 중복되는 코드 Before를 통해 중복 제거

--- src/test/java/bridge/BridgeMakerTest.java
+++ src/test/java/bridge/BridgeMakerTest.java
@@ -6,6 +6,7 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import bridge.constant.ErrorMessage;
 import java.util.List;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.params.ParameterizedTest;
@@ -13,12 +14,18 @@ import org.junit.jupiter.params.provider.ValueSource;
 
 class BridgeMakerTest {
 
+    static BridgeMaker bridgeMaker;
+
+    @BeforeAll
+    static void init() {
+        bridgeMaker = new BridgeMaker(new BridgeRandomNumberGenerator());
+    }
+
     @ParameterizedTest
     @ValueSource(ints = {3, 20})
     @DisplayName("원하는 크기로 다리가 제대로 만들어졌는지 테스트")
     void bridge_size_is_desired_size(Integer desiredSize) {
         // given
-        BridgeMaker bridgeMaker = new BridgeMaker(new BridgeRandomNumberGenerator());
 
         // when
         List<String> bridge = bridgeMaker.makeBridge(desiredSize);
@@ -36,7 +43,6 @@ class BridgeMakerTest {
         @DisplayName("게임 규칙 범위 밖이면 예외를 던진다")
         void exception_is_thrown_if_outside_range_set_by_rule(Integer desiredSize) {
             // given
-            BridgeMaker bridgeMaker = new BridgeMaker(new BridgeRandomNumberGenerator());
 
             // when, then
             assertThatThrownBy(() -> bridgeMaker.makeBridge(desiredSize))
@@ -49,7 +55,6 @@ class BridgeMakerTest {
         @DisplayName("게임 규칙 범위 안이면 예외를 던진다")
         void exception_is_not_thrown_inside_range_set_by_rule(Integer desiredSize) {
             // given
-            BridgeMaker bridgeMaker = new BridgeMaker(new BridgeRandomNumberGenerator());
 
             // when, then
             assertThatCode(() -> bridgeMaker.makeBridge(desiredSize))
--- src/test/java/bridge/domain/BridgeTest.java
+++ src/test/java/bridge/domain/BridgeTest.java
@@ -3,11 +3,19 @@ package bridge.domain;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import java.util.List;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 class BridgeTest {
+
+    static Bridge bridge;
+
+    @BeforeAll
+    static void init() {
+        bridge = new Bridge(List.of("U", "U", "U"));
+    }
 
     @Nested
     @DisplayName("다리의 해당 위치가")
@@ -17,7 +25,6 @@ class BridgeTest {
         @DisplayName("건널 수 없다면 false 반환을 테스트")
         void can_not_cross_then_return_false() {
             // given
-            Bridge bridge = new Bridge(List.of("U", "U", "U"));
 
             // when
             boolean canCross = bridge.canCross(0, "D");
@@ -30,7 +37,6 @@ class BridgeTest {
         @DisplayName("건널 수 있다면 true 반환을 테스트")
         void can_cross_then_return_true() {
             // given
-            Bridge bridge = new Bridge(List.of("U", "U", "U"));
 
             // when
             boolean canCross = bridge.canCross(0, "U");
