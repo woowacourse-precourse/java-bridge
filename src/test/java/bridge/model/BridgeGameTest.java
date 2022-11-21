5b81f813a6934a90eaaf81aff40e3b992eed4577
김현재 <41482946+galug@users.noreply.github.com>
2022-11-21 22:09:45 +0900

    test(bridgegametest): 결과를 반환하는 테스트 추가

--- src/test/java/bridge/model/BridgeGameTest.java
+++ src/test/java/bridge/model/BridgeGameTest.java
@@ -16,39 +16,41 @@ class BridgeGameTest {
     public void beforeEach() {
         Bridge bridge = new Bridge(List.of("U", "D", "U"));
         PassingSpace passingSpace = new PassingSpace();
-        bridgeGame = new BridgeGame(bridge, passingSpace);
+        Round round = new Round(bridge);
+        bridgeGame = new BridgeGame(round, passingSpace);
     }
 
     @DisplayName("사용자가 움직인 공간을 그려준다.")
     @Test
     void drawPassingSpaceAfterOneMoving() {
-        String result = "[ O | X ]\n" +
-                "[   |   ]\n";
         bridgeGame.move("U");
         bridgeGame.move("U");
         String passingSpace = bridgeGame.drawPassingSpace();
 
+        String result = "[ O | X ]\n" + "[   |   ]\n";
         Assertions.assertThat(passingSpace).isEqualTo(result);
     }
 
     @DisplayName("사용자가 움직인 공간을 그려준다.")
     @Test
     void drawPassingSpaceAfterTwoMoving() {
-        String result = "[ O ]\n[   ]\n";
         bridgeGame.move("U");
         bridgeGame.move("D");
         String passingSpace = bridgeGame.drawPassingSpace();
 
+        String result = "[ O |   ]\n" + "[   | O ]\n";
         Assertions.assertThat(passingSpace).isEqualTo(result);
     }
 
-    @DisplayName("사용자가 재시작을 선택한다.")
+    @DisplayName("사용자가 올바른 공간을 선택했는지 판단한다.")
     @ParameterizedTest
-    @CsvSource({"R, true", "Q, false"})
-    void selectRetrying(String selectRetrying, boolean result) {
-        boolean isRetrying = bridgeGame.retry(selectRetrying);
+    @CsvSource({"U, true", "D, false"})
+    void isRightSpace(String selectMoving, boolean isRight) {
+        bridgeGame.move(selectMoving);
 
-        Assertions.assertThat(isRetrying).isEqualTo(result);
+        boolean isRightSpace = bridgeGame.selectRightSpace();
+
+        Assertions.assertThat(isRightSpace).isEqualTo(isRight);
     }
 
     @DisplayName("다리를 건너는데 실패했다..")
@@ -75,13 +77,37 @@ class BridgeGameTest {
         Assertions.assertThat(isSuccessCrossing).isTrue();
     }
 
+    @DisplayName("사용자가 재시작을 선택한다.")
     @ParameterizedTest
-    @CsvSource({"U, true", "D, false"})
-    void isRightSpace(String selectMoving, boolean isRight) {
-        bridgeGame.move(selectMoving);
+    @CsvSource({"R, true", "Q, false"})
+    void selectRetrying(String selectRetrying, boolean result) {
+        boolean isRetrying = bridgeGame.retry(selectRetrying);
 
-        boolean isRightSpace = bridgeGame.isRightSpace();
+        Assertions.assertThat(isRetrying).isEqualTo(result);
+    }
 
-        Assertions.assertThat(isRightSpace).isEqualTo(isRight);
+    @DisplayName("사용자가 재시작 하면 기존 값들이 reset 된다.")
+    @Test
+    void resetByRetrying() {
+        bridgeGame.move("U");
+        bridgeGame.move("U");
+        bridgeGame.retry("R");
+        bridgeGame.move("D");
+
+        String result = "[   ]\n" + "[ X ]\n";
+        Assertions.assertThat(bridgeGame.drawPassingSpace()).isEqualTo(result);
+    }
+
+    @DisplayName("게임 결과를 Dto 로 반환한다.")
+    @Test
+    void getGameResult() {
+        bridgeGame.move("U");
+        bridgeGame.move("U");
+        bridgeGame.retry("Q");
+        GameResultDto gameResult = bridgeGame.getGameResult();
+        String result = "[ O | X ]\n" + "[   |   ]\n";
+        Assertions.assertThat(gameResult.getMap()).isEqualTo(result);
+        Assertions.assertThat(gameResult.isSuccess()).isFalse();
+        Assertions.assertThat(gameResult.getNumberOfTry()).isEqualTo(1);
     }
 }
