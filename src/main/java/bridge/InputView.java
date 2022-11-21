89c6198f35c2d91aa59c335f395b719bc9796f6d
ingpyo <juninpyo72@gmail.com>
2022-11-21 21:10:36 +0900

    feat:이동 명령어 입력 기능

--- src/main/java/bridge/InputView.java
+++ src/main/java/bridge/InputView.java
@@ -13,7 +13,8 @@ public class InputView {
 
 	/**    * 사용자가 이동할 칸을 입력받는다.    */
 	public String readMoving() {
-		return null;
+		String userInputMove = Console.readLine();
+		return userInputMove;
 	}
 
 	/**    * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.    */
