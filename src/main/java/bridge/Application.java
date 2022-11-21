4f64fe10d416714d2b96188cbfeaaeb7dee27a7d
Nugur1i <duckhee092@naver.com>
2022-11-21 23:56:50 +0900

    refactor(BridgeGame): change method parameter

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -1,6 +1,5 @@
 package bridge;
 
-import bridge.BridgeGame.CorrectWrong;
 import java.util.List;
 
 public class Application {
@@ -9,40 +8,47 @@ public class Application {
     static BridgeGame bridgeGame = new BridgeGame();
     static OutputView outputView = new OutputView();
     private static int tryNumber = 0;
-    private static boolean retryOrNot = true;
+    private static boolean retryJudge;
+
+    private static List<String> mapAndOX;
 
 
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
         int inputNumber = inputView.readBridgeSize();
         List<String> bridgeList = new BridgeMaker(bridgeRandomNumberGenerator).makeBridge(inputNumber);
-        //최초 1회 구현
-
-        while(retryOrNot){
-            bridgeGame.resetMap();
-            retryOrNot = false;
-            for (int order = 0; order < bridgeList.size(); order++){
-                String upDownUserInput = inputView.readMoving();
-                String map = bridgeGame.move(bridgeList, upDownUserInput, order);
-                outputView.printMap(map);
-
-                if (map.contains("X")){
-                    if ( inputView.readGameCommand().equals("R")){
-                        retryOrNot = true;
-                        break;
-                    }
-                    break;
-                }
-            }
-
+        do{
+            retryJudge = keepGoingJudge(bridgeList);
             tryNumber++;
+        }while(retryJudge)
+
+
+        outputView.printResult(tryNumber, mapAndJudge.get(1));
+    }
+
+    private static boolean keepGoingJudge(List<String> bridgeList){
+        for (int order = 0; order < bridgeList.size(); order++){
+            mainGame(bridgeList.get(order), order);
+
+            if (mapAndOX.get(1) == "X"){
+                inputView.readGameCommand();
+
+            }
         }
+    }
+private static void retryOrNotInput(String gameCommand){
 
+        if(gameCommand == "R"){
+            return
+        }
+}
 
-
-        outputView.printResult(tryNumber, );
-
-
-
+    private static void mainGame(String bridgeAnswer, int order){
+        String upDownUserInput = inputView.readMoving();
+        mapAndOX = bridgeGame.move(bridgeAnswer, upDownUserInput, order);
+        String map = mapAndOX.get(0);
+        outputView.printMap(map);
+        mapAndOX.get(1);
     }
 }
+
+
--- src/main/java/bridge/BridgeGame.java
+++ src/main/java/bridge/BridgeGame.java
@@ -36,81 +36,76 @@ public class BridgeGame {
         String successFailEnum;
 
 
-
         CorrectWrong(boolean trueFalse, String ox, String successFailEnum) {
             this.trueFalse = trueFalse;
             this.ox = ox;
             this.successFailEnum = successFailEnum;
         }
 
-        public boolean getTrueFalse(){
+        public boolean getTrueFalse() {
             return trueFalse;
         }
 
-        public String getOx(){
+        public String getOx() {
             return ox;
         }
 
-        public String getsuccessFailEnum(){
+        public String getsuccessFailEnum() {
             return successFailEnum;
         }
     }
+
     /**
      * 사용자가 칸을 이동할 때 사용하는 메서드
      * <p>
      * 이동을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public String move(List<String> bridgeList, String upDownUserInput, int order) {
-        boolean ys = bridgeList.get(order).equals(upDownUserInput)
+    public List<String> move(String bridgeAnswer, String upDownUserInput, int order) {
+        boolean oxResult = compare(bridgeAnswer, upDownUserInput);//get 메소드?
 
-        CorrectWrong origin = selectFromOrigin(ys);
+        CorrectWrong origin = selectFromOrigin(oxResult);
 
+        makeAddWord(origin.getOx(), order);
 
+        makeMap(upDownUserInput);
 
-            makeAddWord(origin.getOx(), order);
-
-            makeMap(upDownUserInput);
-
-
-        //그러면 안맞은 것을 출력한다.
-        return str;
-
-
+        return new ArrayList<>(List.of(str, origin.getOx(), origin.getsuccessFailEnum()));
     }
 
-    private CorrectWrong selectFromOrigin(boolean ys) {
-        if (ys = true){
+    private CorrectWrong selectFromOrigin(boolean oxResult) {
+        if (oxResult = true) {
             return CorrectWrong.CORRECT;
         }
         return CorrectWrong.WRONG;
     }
 
-    public String getSuccessFail(){
-        return this.successFail;
+    private boolean compare(String bridgeAnswer, String upDownUserInput ){
+        return bridgeAnswer.equals(upDownUserInput);
     }
 
-    private void makeAddWord(String ox, int order){
+
+    private void makeAddWord(String ox, int order) {
         this.addOxContour = CONTOUR + ox;
         this.emptyContour = CONTOUR + " ";
 
-        if(order == 0){
+        if (order == 0) {
             this.addOxContour = ox;
             this.emptyContour = " ";
         }
     }
 
-    private void makeMap(String upDownUserInput){
-        if(upDownUserInput.equals("U")){
-            this.mapUp.add(mapUp.size()-1,addOxContour);
-            this.mapDown.add(mapDown.size()-1,emptyContour);
+    private void makeMap(String upDownUserInput) {
+        if (upDownUserInput.equals("U")) {
+            this.mapUp.add(mapUp.size() - 1, addOxContour);
+            this.mapDown.add(mapDown.size() - 1, emptyContour);
         } else if (upDownUserInput.equals("D")) {
-            this.mapUp.add(mapUp.size()-1,emptyContour);
-            this.mapDown.add(mapDown.size()-1,addOxContour);
+            this.mapUp.add(mapUp.size() - 1, emptyContour);
+            this.mapDown.add(mapDown.size() - 1, addOxContour);
         }
         this.str = String.join("", mapUp) + "\n" + String.join("", mapDown);
     }
 
-    public void resetMap(){
+    public void resetMap() {
         this.mapUp = new ArrayList<>(Arrays.asList(new String[]{"[ ", " ]"}));
         this.mapDown = new ArrayList<>(Arrays.asList(new String[]{"[ ", " ]"}));
     }
@@ -123,7 +118,7 @@ public class BridgeGame {
      */
     public boolean retry() {
         String retryUserInput = inputView.readGameCommand();
-        if (retryUserInput.equals("R")){
+        if (retryUserInput.equals("R")) {
             return true;
         }
         return false;
