22d7dbc5a420942790d92415169e5bd8fc91d981
zaysverse <zeze@jbnu.ac.kr>
2022-11-21 21:14:48 +0900

    feat(bridge): 게임시작 문구 출력

--- src/main/java/bridge/BridgeGame.java
+++ src/main/java/bridge/BridgeGame.java
@@ -4,6 +4,9 @@ package bridge;
  * 다리 건너기 게임을 관리하는 클래스
  */
 public class BridgeGame {
+    public BridgeGame() {
+        System.out.println("다리 건너기 게임을 시작합니다.");
+    }
 
     /**
      * 사용자가 칸을 이동할 때 사용하는 메서드
