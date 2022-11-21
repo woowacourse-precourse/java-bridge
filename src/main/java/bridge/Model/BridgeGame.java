2e939bd522cd944ac1ffb9fef8bfacbc7d7faf1a
cgk95 <kjkj5868@gmail.com>
2022-11-22 08:40:41 +0900

    refactor(Model) : View에 대한 의존성을 삭제하고 해당 기능들의 책임과 구현을 GameController로 이동

--- src/main/java/bridge/Model/BridgeGame.java
+++ src/main/java/bridge/Model/BridgeGame.java
@@ -1,68 +1,40 @@
 package bridge.Model;
 
-import bridge.View.InputView;
-import bridge.View.OutputView;
-import bridge.GameController;
-import bridge.VisualizeController;
+import bridge.View.UiMessage;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Stack;
 
 public class BridgeGame {
+
+    public List<String> bridge;
+    public Stack<String> user;
+    public String upperResult = UiMessage.EMPTY_GRAPH.getValue();
+    public String lowerResult = UiMessage.EMPTY_GRAPH.getValue();
+
     public int idx;
+
+    public int size;
     public int runCount = 1;
-    public static List<String> bridge;
-    public static Stack<String> user = new Stack<>();
-    public static String upperResult = "[]";
-    public static String lowerResult = "[]";
 
-    public static int size;
-
-    public BridgeGame() {
-        System.out.println("다리 건너기 게임을 시작합니다.\n");
-        GameController.callReadBridgeSize();
-        run(runCount);
-        lowerResult = "[]";
-        upperResult = "[]";
+    private BridgeGame() {
+        this.user = new Stack<>();
+        this.bridge = new ArrayList<>();
     }
 
-    public void run(int runCount) {
-        user = new Stack<>();
-        this.idx = 0;
-        do {
-            move();
-            new VisualizeController(idx);
-            new OutputView().printMap();
-        } while (runCondition(user, bridge));
-        if (idx == size) {
-            new OutputView().printResult(true, runCount);
-            return;
-        }
-        retry();
-    }
-
-    public void move() {
-        user.add(new InputView().readMoving());
-    }
-
-    public boolean runCondition(Stack<String> user, List<String> bridge) {
-        if (user.peek().equals(bridge.get(idx))) {
-            this.idx++;
-            return idx < size;
-        }
-        return false;
+    public void move(String input) {
+        user.add(input);
     }
 
     public void retry() {
-        if (new InputView().readGameCommand().equals("Q")) {
-            new OutputView().printResult(false, runCount);
-            lowerResult = "[]";
-            upperResult = "[]";
-            return;
-        }
-        lowerResult = "[]";
-        upperResult = "[]";
+        user = new Stack<>();
+        upperResult = UiMessage.EMPTY_GRAPH.getValue();
+        lowerResult = UiMessage.EMPTY_GRAPH.getValue();
         runCount++;
-        run(runCount);
+    }
+
+    public static BridgeGame getBridgeGame() {
+        return new BridgeGame();
     }
 }
