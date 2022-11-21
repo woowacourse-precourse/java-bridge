23aaa5a19596225fb8d04bd310f50a8a237e12a3
koo9b9h <90121024+koo9b9h@users.noreply.github.com>
2022-11-21 14:12:40 +0900

    chore: BridgeGame 클래스 패키지 변경

--- src/main/java/bridge/service/BridgeGame.java
+++ src/main/java/bridge/service/BridgeGame.java
@@ -1,27 +1,18 @@
-package bridge.controller;
+package bridge.service;
 
 import bridge.BridgeRandomNumberGenerator;
-import bridge.model.BridgeComparator;
-import bridge.model.BridgeMaker;
-import bridge.model.UserBridgeCorrector;
+import bridge.model.*;
 import bridge.presentation.InputView;
 import bridge.presentation.OutputView;
 
 import java.util.List;
 
-/**
- * 다리 건너기 게임을 관리하는 클래스
- */
+
 public class BridgeGame {
     OutputView outputView = new OutputView();
     InputView inputView = new InputView();
     BridgeComparator compareBridge = new BridgeComparator();
 
-    /**
-     * 사용자가 칸을 이동할 때 사용하는 메서드
-     * <p>
-     * 이동을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
-     */
     public List<String> move(List<String> userSelectResult) {
         UserBridgeCorrector userBridgeCorrector = new UserBridgeCorrector();
         outputView.moveGuide();
@@ -31,25 +22,27 @@ public class BridgeGame {
         return userSelectResult;
     }//move
 
-    /**
-     * 사용자가 게임을 다시 시도할 때 사용하는 메서드
-     * <p>
-     * 재시작을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
-     */
-    public void retry() {
-        inputView.readGameCommand();
-        // 입력에 대한 예외 검사 추가
+    public boolean retry(boolean UnitSuccess, boolean gameSuccess) {
+        boolean restart = false;
+        if (!(gameSuccess) && !(UnitSuccess)) {
+            outputView.restartGuide();
+            String command = inputView.readGameCommand();
+            // 입력에 대한 예외 검사 추가
+            GameRestarter gameRestarter = new GameRestarter();
+            restart = gameRestarter.isRestart(command);
+        }//if
+        return restart;
     }// retry
 
     public List<String> make() {
         BridgeMaker bridgeMaker = new BridgeMaker(new BridgeRandomNumberGenerator());
         int size = inputView.readBridgeSize();
+        // 길이에 대한 에외 검사 추가
         return bridgeMaker.makeBridge(size);
     }//make
 
     public void startSetUp() {
         outputView.startGuide();
-        // 길이에 대한 에외 검사 추가
     }//startSetUp
 
     public boolean isSuccess(List<String> userSelectResult, List<String> computerBridge) {
@@ -59,10 +52,8 @@ public class BridgeGame {
     }//isSuccess
 
     public boolean isGameSuccess(List<String> userSelectResult, List<String> computerBridge, boolean UnitSuccess) {
-        boolean gameSuccess = false;
-        if (userSelectResult.size() == computerBridge.size() && UnitSuccess) {
-            gameSuccess = true;
-        }// if
-        return gameSuccess;
+        GameJudge gameJudge = new GameJudge();
+        return gameJudge.isOver(userSelectResult, computerBridge, UnitSuccess);
     }//isGameSuccess
+
 } // end class
