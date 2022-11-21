9688be537fe3887f9a6d5fe046ab3a91e0455c1a
ddingmin <ddingminyt@gmail.com>
2022-11-22 00:25:05 +0900

    refactor(java-bridge): main 코드 정리

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -1,18 +1,11 @@
 package bridge;
 
 import bridge.domain.Referee;
-import bridge.ui.InputView;
-import bridge.ui.OutputView;
 
 public class Application {
-    private static final InputView inputView = new InputView();
-    private static final OutputView outputView = new OutputView();
-
     public static void main(String[] args) {
         // TODO: 프로그램 구현
         Referee referee = new Referee();
         referee.start();
-
-
     }
 }
