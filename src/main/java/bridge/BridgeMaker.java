b77be07c7f00cb0766cbae6eaf2ebb5104b5b745
dydckd <toofriend09@naver.com>
2022-11-21 13:23:58 +0900

    feat 다리의 길이를 입력받아 다리를 만드는 메소드
    다리의 길이를 입력받아 다리를 만드는 메소드
    0,1로 된 RandomBridge를 0->D로 1->U로 변환한 udBridge가 반환되도록함

--- src/main/java/bridge/BridgeMaker.java
+++ src/main/java/bridge/BridgeMaker.java
@@ -1,6 +1,9 @@
 package bridge;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * 다리의 길이를 입력 받아서 다리를 생성해주는 역할을 한다.
@@ -18,6 +21,14 @@ public class BridgeMaker {
      * @return 입력받은 길이에 해당하는 다리 모양. 위 칸이면 "U", 아래 칸이면 "D"로 표현해야 한다.
      */
     public List<String> makeBridge(int size) {
-        return null;
+        List<Integer> RandomBridge = new ArrayList<>(size);
+        for(int i =0 ; i<size ; i++) {
+            RandomBridge.add(bridgeNumberGenerator.generate());
+        }
+        List<String> udBridge = RandomBridge.stream().map(String::valueOf).collect(Collectors.toList());
+        Collections.replaceAll(udBridge,"0","D");
+        Collections.replaceAll(udBridge,"1","U");
+
+        return udBridge;
     }
 }
