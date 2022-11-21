a7e5ad0353f97173fb97f8f172af12ec612ef48e
qkrdmswl <eunji303@naver.com>
2022-11-21 17:07:01 +0900

    feat: print game message and map

--- src/main/java/bridge/view/OutputView.java
+++ src/main/java/bridge/view/OutputView.java
@@ -1,18 +1,32 @@
 package bridge.view;
 
+import bridge.type.ResultType;
+import utils.BridgePrintTool;
 import utils.IOMessage;
+
+import java.util.List;
 
 /**
  * 사용자에게 게임 진행 상황과 결과를 출력하는 역할을 한다.
  */
 public class OutputView {
-
     /**
      * 현재까지 이동한 다리의 상태를 정해진 형식에 맞춰 출력한다.
      * <p>
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void printMap() {
+    public static void printMap(List<List<String>> directionTypeLists) {
+        printEachMap(directionTypeLists.get(0));
+        printEachMap(directionTypeLists.get(1));
+    }
+
+
+    public static void printEachMap(List<String> directionTypeList) {
+        printListStart();
+        for (String shape : directionTypeList) {
+            System.out.print(shape);
+        }
+        printListEnd();
     }
 
     /**
@@ -20,19 +34,26 @@ public class OutputView {
      * <p>
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void printResult() {
-        System.out.println(IOMessage.GAME_RESULT);
-        System.out.println(IOMessage.TRY_CNT);
+    public static void printResult(ResultType resultType, int tryCnt) {
+        System.out.println(IOMessage.GAME_RESULT +  resultType.getResultType());
+        System.out.println(IOMessage.TRY_CNT + tryCnt);
     }
 
 
-    public void printGameStartMsg() {
+    public static final void printGameStartMsg() {
         System.out.println(IOMessage.GAME_START_MSG);
     }
 
-    public void printGameEndMsg() {
+    public static final void printGameEndMsg() {
         System.out.println(IOMessage.GAME_EXIT_MSG);
     }
 
+    public static final void printListStart() {
+        System.out.print(BridgePrintTool.START);
+    }
 
+    public static final void printListEnd() {
+        System.out.print(BridgePrintTool.END);
+        System.out.println();
+    }
 }
