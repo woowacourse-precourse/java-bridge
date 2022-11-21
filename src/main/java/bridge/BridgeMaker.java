b40c16875181b4dc6110d4bfe42676d1795ee85d
kim0914 <kosa0914@naver.com>
2022-11-21 17:24:42 +0900

    refactor(BridgeMaker): number -> moving 타입 변환 메서드 Convert 객체로 분리

--- src/main/java/bridge/BridgeMaker.java
+++ src/main/java/bridge/BridgeMaker.java
@@ -1,6 +1,6 @@
 package bridge;
 
-import bridge.util.Constant;
+import bridge.util.Converter;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -21,25 +21,11 @@ public class BridgeMaker {
      * @return 입력받은 길이에 해당하는 다리 모양. 위 칸이면 "U", 아래 칸이면 "D"로 표현해야 한다.
      */
     public List<String> makeBridge(int size) {
-        validate(size);
         List<String> bridge = new ArrayList<>();
 
         while (bridge.size() != size) {
-            bridge.add(convertNumberToMoving(bridgeNumberGenerator.generate()));
+            bridge.add(Converter.convertNumberToMoving(bridgeNumberGenerator.generate()));
         }
         return bridge;
-    }
-
-    private void validate(int size) {
-        if (size < Constant.BRIDGE_MIN_SIZE || size > Constant.BRIDGE_MAX_SIZE) {
-            throw new IllegalArgumentException("다리 길이는 3이상, 20이하의 크기만 가능합니다.");
-        }
-    }
-
-    private String convertNumberToMoving(int number) {
-        if (number == Constant.UPPER_MOVING_NUMBER) {
-            return Constant.UPPER_MOVING;
-        }
-        return Constant.LOWER_MOVING;
     }
 }
