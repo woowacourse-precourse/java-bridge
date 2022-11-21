177b7afaa239060c7e6cd0bd848145da4136ecc9
swonny <wonny921@gmail.com>
2022-11-21 16:24:02 +0900

    refactor(Player): 현재 위치 저장 상태값 삭제
    사용자가 이동한 위치를 담는 footprints의 size()로 구할 수 있는 상태값이므로 삭제

--- src/main/java/bridge/Player.java
+++ src/main/java/bridge/Player.java
@@ -1,34 +1,29 @@
 package bridge;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Player {
-    private final int START_POSITION = -1; // 다리는 0번 인덱스부터 시작하므로 사용자의 초기화 위치는 -1이다.
-    private int currentPosition;
     private List<String> footprints;
 
     public Player() {
-        this.currentPosition = START_POSITION;
+        this.footprints = new ArrayList<>();
     }
 
     public void move(String movedSide) {
-        this.currentPosition++;
         footprints.add(movedSide);
     }
 
-    public int getNextPosition() {
-        return this.currentPosition + 1;
-    }
-
     public int getCurrentPosition() {
-        return this.currentPosition;
+        return this.footprints.size() - 1;
     }
 
     public void initializePosition() {
-        currentPosition = START_POSITION;
+        this.footprints = new ArrayList<>();
     }
 
     public String getLastMoving() {
-        return footprints.get(footprints.size() - 1);
+        return footprints.get(getCurrentPosition());
+
     }
 }
