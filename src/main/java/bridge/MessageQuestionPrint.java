b8b2ca79664c8f6ebac3510b378e451ec1554321
ingpyo <juninpyo72@gmail.com>
2022-11-21 20:47:13 +0900

    feat:다리 크기 입력문구 출력 기능

--- src/main/java/bridge/MessageQuestionPrint.java
+++ src/main/java/bridge/MessageQuestionPrint.java
@@ -4,4 +4,8 @@ public class MessageQuestionPrint {
 	public void startPrint() {
 		System.out.println(Constant.START_MESSAGE);
 	}
+
+	public void inputBridgeSizeQuestion() {
+		System.out.println(Constant.NUMBER_INPUT_MESSAGE_QUESTION);
+	}
 }
