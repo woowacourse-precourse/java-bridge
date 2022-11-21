5930d5a7491f2cd94fdda313fc4572dbc6ea6af9
Song kyeong seok <skj0922@kookmin.ac.kr>
2022-11-21 19:39:49 +0900

    feat(GameData): 게임 데이터를 저장하는 클래스 작성

---
+++ src/main/java/bridge/domain/GameData.java
@@ -0,0 +1,13 @@
+package bridge.domain;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class GameData {
+
+    private List<MoveType> moveTypes = new ArrayList<>();
+    private int tryCount = 0;
+
+    public GameData() {
+    }
+}
