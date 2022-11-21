9bc00e00e2cbaca46e74aec424cd7afa28c9da90
aossuper7 <aossuper7@naver.com>
2022-11-21 21:16:24 +0900

    feat: 숫자 확인 정규식 정의

---
+++ src/main/java/bridge/Constant/Regex.java
@@ -0,0 +1,15 @@
+package bridge.Constant;
+
+public enum Regex {
+    NUMBER("\\d+");
+
+    private final String regex;
+
+    Regex(String regex) {
+        this.regex = regex;
+    }
+
+    public String verify() {
+        return regex;
+    }
+}
