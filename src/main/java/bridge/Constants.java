a2003e9d0a8c311cae498239a63beb88984b5a8b
LeeHaiLim <hailim1997@gmail.com>
2022-11-22 03:23:58 +0900

    feat(bridge): add constants
    Added int constants
    - bridge size minimum
    - bridge size maximun

--- src/main/java/bridge/Constants.java
+++ src/main/java/bridge/Constants.java
@@ -1,6 +1,8 @@
 package bridge;
 
 public class Constants {
+    public static final int BRIDGE_SIZE_MIN = 3;
+    public static final int BRIDGE_SIZE_MAX = 20;
     public static final String CODE_UP = "U";
     public static final String CODE_DOWN = "D";
     public static final String CODE_RETRY = "R";
