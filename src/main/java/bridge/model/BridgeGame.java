37f7f18eb85a61d94bc12fa53486098fce2dd520
Hi-Imjaeyoung <imjy2757@naver.com>
2022-11-21 18:22:58 +0900

    style(BridgeGame) : 주석삭제, 코드 정렬.

--- src/main/java/bridge/model/BridgeGame.java
+++ src/main/java/bridge/model/BridgeGame.java
@@ -1,13 +1,13 @@
 package bridge.model;
 
 import java.util.List;
-/**
- * 다리 건너기 게임을 관리하는 클래스
- */
+
 public class BridgeGame {
+
     private final List<String> bridge;
     private int tryNumber;
     private int nowLocation;
+
     public BridgeGame(List<String> bridge){
         this.bridge = bridge;
         this.tryNumber = 1;
@@ -21,18 +21,15 @@ public class BridgeGame {
         }
         return false;
     }
+
     public boolean checkSuccess(){
         return nowLocation == bridge.size();
     }
+
     public int getTryNumber(){
         return tryNumber;
     }
 
-    /**
-     * 사용자가 게임을 다시 시도할 때 사용하는 메서드
-     * <p>
-     * 재시작을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
-     */
     public void retry() {
         tryNumber++;
         nowLocation =0 ;
