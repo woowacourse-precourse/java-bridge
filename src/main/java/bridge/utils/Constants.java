ddc690a7370c19071c851cd0613319c69d5bcad9
yujaemin <woawoa0513@gmail.com>
2022-11-21 14:03:48 +0900

    feat: 플레이어 움직임(위, 아래) 상수 선언

--- src/main/java/bridge/utils/Constants.java
+++ src/main/java/bridge/utils/Constants.java
@@ -3,4 +3,7 @@ package bridge.utils;
 public class Constants {
     public static final int BRIDGE_MIN_SIZE = 3;
     public static final int BRIDGE_MAX_SIZE = 20;
+
+    public static final String UP = "U";
+    public static final String DOWN = "D";
 }
