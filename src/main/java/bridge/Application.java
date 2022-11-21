3e32ee09482358f147f702ba904ccb2cfdf29f65
JunDonghyuk <ehdgur4814@naver.com>
2022-11-21 09:01:00 +0900

    fix(Application): 주석 제거

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -5,7 +5,7 @@ import camp.nextstep.edu.missionutils.Console;
 public class Application {
 
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+
         BridgeGameController bridgeGameController = new BridgeGameController();
         bridgeGameController.startBridgeGame();
     }
