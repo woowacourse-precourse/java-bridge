a50a2dc6f1410c3271cb713d151887ebe8035bf1
ghltjd369 <ghltjd369@gmail.com>
2022-11-21 19:25:55 +0900

    refactor : 사용하지 않는 인스턴스 변수 제거

--- src/main/java/bridge/BridgeGame.java
+++ src/main/java/bridge/BridgeGame.java
@@ -7,8 +7,6 @@ import java.util.List;
  * 다리 건너기 게임을 관리하는 클래스
  */
 public class BridgeGame {
-    private int tryNumber;
-    private boolean successOrFail;
 
     /**
      * 사용자가 칸을 이동할 때 사용하는 메서드
