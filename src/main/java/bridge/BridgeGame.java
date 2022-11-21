0ba0ecc23bc2abdc20310077b5d304d3cdc2f647
juy4556 <kws05050@naver.com>
2022-11-21 12:25:49 +0900

    feat: 이동한 칸과 다리 리스트를 비교한다.

--- src/main/java/bridge/BridgeGame.java
+++ src/main/java/bridge/BridgeGame.java
@@ -1,15 +1,35 @@
 package bridge;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 /**
  * 다리 건너기 게임을 관리하는 클래스
  */
 public class BridgeGame {
     private final List<String> bridge;
+    private List<String> upBridge;
+    private List<String> downBridge;
 
     public BridgeGame(List<String> bridge) {
         this.bridge = bridge;
+        this.upBridge = new ArrayList<>();
+        upBridge.add("[");
+        this.downBridge = new ArrayList<>();
+        downBridge.add("[");
+    }
+
+    public List<String> getBridge() {
+        return bridge;
+    }
+
+    public List<String> getUpBridge() {
+        return upBridge;
+    }
+
+    public List<String> getDownBridge() {
+        return downBridge;
     }
 
     /**
@@ -17,7 +37,34 @@ public class BridgeGame {
      * <p>
      * 이동을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void move() {
+    public void move(String direction, int index) {
+        String validBridge = bridge.get(index);
+        DirectionType directionType = DirectionType.valueOf(direction);
+        String[] moveResult = initializeMovingResult(direction, validBridge, directionType);
+        addBridge(moveResult, index);
+    }
+
+    private void addBridge(String[] moveResult, int index) {
+        downBridge.add(moveResult[0]);
+        upBridge.add(moveResult[1]);
+        downBridge.add("|");
+        upBridge.add("|");
+        if (index == bridge.size()) {
+            downBridge.add("]");
+            upBridge.add("]");
+        }
+    }
+
+    private String[] initializeMovingResult(String direction, String validBridge, DirectionType directionType) {
+        final String X = " X ";
+        final String O = " O ";
+        final String SPACE = "   ";
+        String[] moveResult = {SPACE, SPACE};
+        moveResult[directionType.getNumber()] = X;
+        if (Objects.equals(direction, validBridge)) {
+            moveResult[directionType.getNumber()] = O;
+        }
+        return moveResult;
     }
 
     /**
