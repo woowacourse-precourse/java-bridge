87a4d642ea30b5328f8d7c444909a3336e122c7d
JJ <jeongjae1996@naver.com>
2022-11-22 00:04:41 +0900

    feat: Application에서 게임이 시작되도록 구현

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -4,5 +4,12 @@ public class Application {
 
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        try {
+            Play play = new Play();
+            play.play();
+        }
+        catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+        }
     }
 }
