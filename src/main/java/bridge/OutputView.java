d05d1050b8e94c8e335211529df8032d709e00de
BaeDongHyeon <sasd730@gmail.com>
2022-11-21 21:17:25 +0900

    style: 메소드 명 수정

--- src/main/java/bridge/OutputView.java
+++ src/main/java/bridge/OutputView.java
@@ -22,7 +22,7 @@ public class OutputView {
         System.out.println("이동할 칸을 선택해주세요. (위: U, 아래: D)");
     }
 
-    public void inputRetryMessage() {
+    public void inputRetryCommandMessage() {
         System.out.println("게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)");
     }
 
