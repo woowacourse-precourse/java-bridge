0df3e34554481a0082b99169e9e4f458f89d13ef
Minsoo Choi <lucas0209@yonsei.ac.kr>
2022-11-22 01:54:04 +0900

    feat(BridgeGame): Returns the result containing markers(O, X) and user direction choices(U, D)
    Features added:
    - Compare btw user input and answer to cross the bridge
    - Returns the list of items containing [markers(O or X), user direction(U,D)]

--- src/main/java/bridge/BridgeGame.java
+++ src/main/java/bridge/BridgeGame.java
@@ -1,5 +1,8 @@
 package bridge;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * 다리 건너기 게임을 관리하는 클래스
  */
@@ -10,7 +13,14 @@ public class BridgeGame {
      * <p>
      * 이동을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void move() {
+    // 사용자 이동 경로와 정답 경로를 비교 후 기록
+    public List<List<String>> move(List<String> answer, User user) {
+        List<String> choices = user.getUserMoveChoice();
+        List<List<String>> result = new ArrayList<>();
+        for (int i = 0; i < choices.size(); i++) {
+            result.add(markResult(choices, i, choices.get(i).equals(answer.get(i))));
+        }
+        return result;
     }
 
     /**
