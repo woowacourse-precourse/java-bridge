1a74af25cbed9a179a3a96876c7ccdce2f865c65
hhiyeon <hhyyeon0214@gmail.com>
2022-11-22 03:21:55 +0900

    feat:BridgeGame-move bridge to moving input

--- src/main/java/bridge/domain/BridgeGame.java
+++ src/main/java/bridge/domain/BridgeGame.java
@@ -1,38 +1,45 @@
 package bridge.domain;
 
-/**
- * 다리 건너기 게임을 관리하는 클래스
- */
+import java.util.List;
+
 public class BridgeGame {
     private static final String CAN_MOVING = "O";
     private static final String CAN_NOT_MOVING = "X";
-    /**
-     * 사용자가 칸을 이동할 때 사용하는 메서드
-     * <p>
-     * 이동을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
-     */
 
+    private final Bridge bridge;
+    private List<String> movements;
+    private int movingCount;
+    private int totalGame;
+    private String lastMoving;
 
-    public void move(String crossDirection, String movingInput) {
+    public BridgeGame(Bridge bridge) {
+        this.totalGame = 1;
+        this.bridge = bridge;
+    }
 
+    public BridgeGame(List<String> movements, int totalGame, Bridge bridge) {
+        this.movements = movements;
+        this.movingCount = 0;
+        this.totalGame = totalGame;
+        this.bridge = bridge;
+    }
+
+    public boolean move(String crossDirection, String movingInput) {
+        movements.add(movingInput); // 리스트에 이동 입력 정보 넣기
+        addMovingCount();
+
+        if (checkCanMoving(crossDirection, movingInput)) {
+            setLastMoving(CAN_MOVING);
+            return true;
+        }
+        setLastMoving(CAN_NOT_MOVING);
+        return false;
     }
 
     private boolean checkCanMoving(String crossDirection, String movingInput) {
         return crossDirection.equals(movingInput);
     }
 
-    private String getMovingResult(String crossDirection, String movingInput) {
-        if (checkCanMoving(crossDirection, movingInput)) {
-            return CAN_MOVING; // 이동 가능
-        }
-        return CAN_NOT_MOVING; // 이동 불가능
-    }
-
-    /**
-     * 사용자가 게임을 다시 시도할 때 사용하는 메서드
-     * <p>
-     * 재시작을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
-     */
     public void retry() {
     }
 }
