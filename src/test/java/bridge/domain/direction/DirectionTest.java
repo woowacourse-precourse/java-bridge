dd7e8774a5785717c38f1aff169feb5cca0f2b9a
chanminmaxscaler <jcm04@naver.com>
2022-11-21 11:20:32 +0900

    test: Direction 클래스의 equals 메소드를 테스트한다
    equals 메소드에 Direction 객체가 입력되었을 때 두 객체의 멤버변수로 비교한 결과를 반환하는지 확인한다.

--- src/test/java/bridge/domain/direction/DirectionTest.java
+++ src/test/java/bridge/domain/direction/DirectionTest.java
@@ -2,6 +2,7 @@ package bridge.domain.direction;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -10,7 +11,7 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 class DirectionTest {
 
     @DisplayName("생성자 메소드에 U와 D를 제외한 문자가 입력되었을 때 오류를 발생시키는지 확인")
-    @ParameterizedTest()
+    @ParameterizedTest
     @ValueSource(strings = {"u", "25", "", " ", " U", "D ", "UU", "DU"})
     void constructor_error_test(String input) {
         assertThatThrownBy(() -> new Direction(input)).isInstanceOf(IllegalArgumentException.class)
@@ -18,7 +19,7 @@ class DirectionTest {
     }
 
     @DisplayName("생성자 메소드에 U와 D를 입력하였을 때 Direction 객체가 생성되는지 확인")
-    @ParameterizedTest()
+    @ParameterizedTest
     @ValueSource(strings = {"U", "D"})
     void constructor_test(String input) {
         Direction direction = new Direction(input);
@@ -26,4 +27,14 @@ class DirectionTest {
         assertThat(direction).isInstanceOf(Direction.class);
     }
 
+    @DisplayName("equals 메소드에 Direction 객체가 입력되었을 때 두 객체의 멤버변수로 비교한 결과를 반환하는지 확인")
+    @ParameterizedTest
+    @CsvSource({"U, U, true", "D, D, true", "U, D, false", "D, U, false"})
+    void equals_test(String userInput, String another, boolean expected) {
+        Direction direction = new Direction(userInput);
+
+        boolean actual = direction.equals(new Direction(another));
+
+        assertThat(actual).isEqualTo(expected);
+    }
 }
