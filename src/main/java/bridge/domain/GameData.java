f90a9991988dc7b0387e5c1b57fb3684d6ddce1a
Song kyeong seok <skj0922@kookmin.ac.kr>
2022-11-21 20:43:35 +0900

    feat(GameData): 유저가 입력한 이동할 칸을 저장하는 기능 구현

--- src/main/java/bridge/domain/GameData.java
+++ src/main/java/bridge/domain/GameData.java
@@ -10,4 +10,14 @@ public class GameData {
 
     public GameData() {
     }
+
+    public void saveMove(String moveType) {
+        if (MoveType.UP_TYPE.compareTo(moveType)) {
+            moveTypes.add(MoveType.UP_TYPE);
+        }
+
+        if (MoveType.DOWN_TYPE.compareTo(moveType)) {
+            moveTypes.add(MoveType.DOWN_TYPE);
+        }
+    }
 }
