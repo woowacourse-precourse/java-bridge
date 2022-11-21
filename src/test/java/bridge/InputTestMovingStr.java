326be565d1e808bf6d47a88d58de3e3eff0e0a60
yumble <hnjn1101@gmail.com>
2022-11-21 12:27:30 +0900

    test(input test movingstr) : 테스트 코드 수정

--- src/test/java/bridge/InputTestMovingStr.java
+++ src/test/java/bridge/InputTestMovingStr.java
@@ -1,33 +1,43 @@
 package bridge;
 
+import camp.nextstep.edu.missionutils.Console;
 import camp.nextstep.edu.missionutils.test.NsTest;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+
 import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 
 class InputTestMovingStr extends NsTest {
 
     private static final String ERROR_MESSAGE = "[ERROR]";
-
+    private ValidationCheck validationCheck;
+    @BeforeEach
+    void beforeTestSetting(){
+        validationCheck = new ValidationCheck();
+    }
 
 
     @DisplayName("이동할 칸 입력 테스트 : 다리길이 입력 후 U or D 가 아닌 다른 영어 입력")
     @Test
     void testMovingByOtherEnglish() {
-        assertSimpleTest(() -> {
-            runException("5", "a");
-            assertThat(output()).contains(ERROR_MESSAGE);
-        });
+        InputStream in = new ByteArrayInputStream("a".getBytes());
+        System.setIn(in);
+        assertThatThrownBy(() -> validationCheck.checkMovingEnum(Console.readLine()))
+                .isInstanceOf(IllegalArgumentException.class);
     }
     @DisplayName("이동할 칸 입력 테스트 : 다리길이 입력 후 공백 입력")
     @Test
     void testMovingBySpaceBar() {
-        assertSimpleTest(() -> {
-            runException("5", " ");
-            assertThat(output()).contains(ERROR_MESSAGE);
-        });
+        InputStream in = new ByteArrayInputStream(" ".getBytes());
+        System.setIn(in);
+        assertThatThrownBy(() -> validationCheck.isBlank(Console.readLine()))
+                .isInstanceOf(IllegalArgumentException.class);
     }
     //Todo 다리길이에 영향을 받지 않는 방향으로는 못 짜는 것인가.
 
