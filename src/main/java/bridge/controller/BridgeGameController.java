90752549b3fa95e814db49ec1ffe19a145ca7751
moonchaewon <codnjs8989@gmail.com>
2022-11-21 23:25:31 +0900

    feat(bridgegame): implement function to control game

--- src/main/java/bridge/controller/BridgeGameController.java
+++ src/main/java/bridge/controller/BridgeGameController.java
@@ -1,7 +1,6 @@
 package bridge.controller;
 
-import bridge.domain.BridgeMaker;
-import bridge.domain.BridgeRandomNumberGenerator;
+import bridge.domain.BridgeGame;
 import bridge.view.InputView;
 
 public class BridgeGameController {
@@ -14,67 +13,100 @@ public class BridgeGameController {
     private static final String QUIT_GAME_COMMAND = "Q";
 
     InputView inputView = new InputView();
+    BridgeGame bridgeGame = new BridgeGame();
+    private int bridgeSize;
+    private int userPosition = 0;
+    private int userTryCount = 1;
 
     public void run() {
-        makeBridgeBySize();
-        controlMove();
-        controlRetry();
+        try {
+            this.bridgeSize = setBridgeSize();
+            bridgeGame.setGame(bridgeSize);
+            controlGame();
+            System.out.println("trycount:" + userTryCount);
+        } catch (Exception e) {
+            System.err.println(e.getMessage());
+        }
     }
 
-    public void makeBridgeBySize() {
+    private int setBridgeSize() {
         while (true) {
             try {
                 int bridgeSize = inputView.readBridgeSize();
-                if (!isValidateBridgeSize(bridgeSize)) {
-                    throw new IllegalArgumentException("[ERROR] 다리 길이는 3부터 20 사이의 숫자여야 합니다.");
-                }
-                BridgeMaker bridgeMaker = new BridgeMaker(new BridgeRandomNumberGenerator());
-                System.out.println("bridge: " + bridgeMaker.makeBridge(bridgeSize));
-                return;
+                validateBridgeSize(bridgeSize);
+                return bridgeSize;
             } catch (IllegalArgumentException e) {
                 System.err.println(e.getMessage());
             }
         }
     }
 
-    public void controlMove() {
+    private void controlGame() {
+        while (true) {
+            if (!startRound()) {
+                return;
+            }
+            if (userPosition == bridgeSize) {
+                return;
+            }
+        }
+    }
+
+    private boolean startRound() {
+        if (!setMoving()) {
+            if (!setRetry()) {
+                return false;
+            }
+            userTryCount ++;
+            userPosition = 0;
+            return true;
+        }
+        userPosition++;
+        return true;
+    }
+
+    private boolean setMoving() {
         while (true) {
             try {
                 String moveCommand = inputView.readMoving();
-                if (!isValidateMovingCommand(moveCommand)) {
-                    throw new IllegalArgumentException("[ERROR] 건널 칸은 U 또는 D 문자여야 합니다.");
-                }
-                return;
+                validateMovingCommand(moveCommand);
+                return bridgeGame.move(moveCommand);
             } catch (IllegalArgumentException e) {
                 System.err.println(e.getMessage());
             }
         }
     }
 
-    public void controlRetry() {
+    private boolean setRetry() {
         while (true) {
             try {
                 String retryCommand = inputView.readGameCommand();
-                if (!isValidateRetryCommand(retryCommand)) {
-                    throw new IllegalArgumentException("[ERROR] 게임 재시도 여부는 R 또는 Q 문자여야 합니다.");
-                }
-                return;
+                validateRetryCommand(retryCommand);
+                return bridgeGame.retry(retryCommand);
             } catch (IllegalArgumentException e) {
                 System.err.println(e.getMessage());
             }
         }
     }
 
-    private boolean isValidateBridgeSize(int size) {
-        return size >= MIN_BRIDGE_SIZE && size <= MAX_BRIDGE_SIZE;
+    private void validateBridgeSize(int size) {
+        if (size < MIN_BRIDGE_SIZE || size > MAX_BRIDGE_SIZE) {
+            throw new IllegalArgumentException("[ERROR] 다리 길이는 3부터 20 사이의 숫자여야 합니다.");
+        }
     }
 
-    private boolean isValidateMovingCommand(String command) {
-        return command.equals(UP_MOVE_COMMAND) || command.equals(DOWN_MOVE_COMMAND);
+    private void validateMovingCommand(String command) {
+        if (!command.equals(UP_MOVE_COMMAND) && !command.equals(DOWN_MOVE_COMMAND)) {
+            throw new IllegalArgumentException("[ERROR] 건널 칸은 U 또는 D 문자여야 합니다.");
+        }
     }
 
-    private boolean isValidateRetryCommand(String command) {
-        return command.equals(RETRY_GAME_COMMAND) || command.equals(QUIT_GAME_COMMAND);
+    private void validateRetryCommand(String command) {
+        if (!command.equals(RETRY_GAME_COMMAND) && !command.equals(QUIT_GAME_COMMAND)) {
+            throw new IllegalArgumentException("[ERROR] 게임 재시도 여부는 R 또는 Q 문자여야 합니다.");
+        }
     }
+
+
 }
 
--- src/main/java/bridge/domain/BridgeGame.java
+++ src/main/java/bridge/domain/BridgeGame.java
@@ -1,16 +1,32 @@
 package bridge.domain;
 
+import java.util.List;
+
 /**
  * 다리 건너기 게임을 관리하는 클래스
  */
 public class BridgeGame {
 
+    private int currentPosition = 0;
+    private List<String> bridge;
+
+    public void setGame(int bridgeSize) {
+        BridgeMaker bridgeMaker = new BridgeMaker(new BridgeRandomNumberGenerator());
+        this.bridge = bridgeMaker.makeBridge(bridgeSize);
+        System.out.println("BridgeGame.setGame(): " + this.bridge);
+    }
     /**
      * 사용자가 칸을 이동할 때 사용하는 메서드
      * <p>
      * 이동을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void move() {
+    public boolean move(String command) {
+        if (compare(command)) {
+            System.out.println("BridgeGame.move()");
+            currentPosition ++;
+            return true;
+        }
+        return false;
     }
 
     /**
@@ -18,6 +34,15 @@ public class BridgeGame {
      * <p>
      * 재시작을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void retry() {
+    public boolean retry(String command) {
+        if (command.equals("Q")) {
+            return false;
+        }
+        currentPosition = 0;
+        return true;
+    }
+
+    private boolean compare(String command) {
+        return bridge.get(currentPosition).equals(command);
     }
 }
