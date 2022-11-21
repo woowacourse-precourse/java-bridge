837b7d697a4e7d3e391375f8a58e4e49a1cd874c
bearjun05 <bearjun05@naver.com>
2022-11-21 19:46:53 +0900

    feat : 다리 만들고 만든 다리를 Bridge 클래스에서 생성자로 넘겨주는 기능 구현

--- src/main/java/bridge/GameController.java
+++ src/main/java/bridge/GameController.java
@@ -8,11 +8,14 @@ public class GameController {
 
     private final InputView inputView = new InputView();
     private final OutputView outputView = new OutputView();
+    private final BridgeGame bridgeGame;
     private final BridgeNumberGenerator bridgeNumberGenerator = new BridgeRandomNumberGenerator();
     private final BridgeMaker bridgeMaker = new BridgeMaker(bridgeNumberGenerator);
 
     GameController() {
         int bridgeSize = start();
+        List<String> bridge = bridgeMaker.makeBridge(bridgeSize);
+        bridgeGame = new BridgeGame(bridge);
     }
 
     private int start() {
