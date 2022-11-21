0ebfe474a6593b0e5063edf60cf18d2d27ab84e3
JJ <jeongjae1996@naver.com>
2022-11-21 12:39:12 +0900

    feat: 게임 결과 출력하는 기능 구현

--- src/main/java/bridge/OutputView.java
+++ src/main/java/bridge/OutputView.java
@@ -7,6 +7,12 @@ import java.util.List;
  * OutputView의 메서드의 이름은 변경할 수 없고, 인자와 반환 타입은 필요에 따라 추가하거나 변경할 수 있다.
  */
 public class OutputView {
+    private static final String SUCCESS = "성공";
+    private static final String FAIL = "실패";
+    private static final String TITLE = "최종 게임 결과";
+    private static final String EVENT_MESSAGE = "게임 성공 여부: $gameResult\n총 시도한 횟수: $tryCount\n";
+    private static final String REPLACE_GAME_RESULT_FROM = "$gameResult";
+    private static final String REPLACE_TRY_COUNT_FROM = "$tryCount";
 
     /**
      * 현재까지 이동한 다리의 상태를 정해진 형식에 맞춰 출력한다.
@@ -23,6 +29,19 @@ public class OutputView {
      * <p>
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void printResult() {
+    public void printResult(
+            List<String> map,
+            int tryCount,
+            boolean isCorrectPath) {
+        String gameResult = SUCCESS;
+        if (!isCorrectPath) {
+            gameResult = FAIL;
+        }
+
+        System.out.println(TITLE);
+        printMap(map);
+        System.out.println(EVENT_MESSAGE
+                .replace(REPLACE_GAME_RESULT_FROM, gameResult)
+                .replace(REPLACE_TRY_COUNT_FROM, Integer.toString(tryCount)));
     }
 }
