fdfbcc2765f26ccec72040451b70530314762e58
Hayoon <hy97@sju.ac.kr>
2022-11-21 20:08:51 +0900

    refactor(Application): 생성자 파라미터 갯수 리팩토링

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -9,11 +9,9 @@ public class Application {
 
     public static void main(String[] args) {
         // TODO: 프로그램 구현
-        GameStatistics gameStatistics = new GameStatistics();
-        Bridge bridge = new Bridge(gameStatistics);
-        BridgeGame bridgeGame = new BridgeGame(new Player(), gameStatistics, bridge);
-        OutputView outputView = new OutputView(gameStatistics, bridge);
-        InputView inputView = new InputView(bridgeGame, new BridgeException(), outputView);
-        new GameMachine(inputView, outputView);
+        Bridge bridge = new Bridge(new GameStatistics());
+        BridgeGame bridgeGame = new BridgeGame(new Player(), bridge);
+        InputView inputView = new InputView(bridgeGame, new BridgeException(), new OutputView(bridge));
+        new GameMachine(inputView);
     }
 }
