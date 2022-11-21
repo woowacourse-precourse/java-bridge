3d5ceec6646e521571518104ed19da85ee90664e
seoyoung <sy9723@korea.ac.kr>
2022-11-21 12:12:14 +0900

    test: 기능 테스트 추가 (입력된 값에 따라 재시작할 지, 종료할 지를 결정한다)

--- src/test/java/bridge/domain/JudgementTest.java
+++ src/test/java/bridge/domain/JudgementTest.java
@@ -1,5 +1,7 @@
 package bridge.domain;
 
+import static bridge.domain.Judgement.checkRestart;
+import static bridge.domain.Judgement.checkResult;
 import static org.junit.jupiter.api.Assertions.*;
 
 import java.util.List;
@@ -11,33 +13,39 @@ class JudgementTest {
     /** success: 0 -> down 성공, 1 -> down 실패, 2 -> up 성공, 3 -> up 실패 **/
     @DisplayName("이동한 칸을 건널 수 있는지 없는지 확인한다.(down 성공)")
     @Test
-    void checkResult_downSuccess() {
+    void checkResultTest_downSuccess() {
         List<Integer> movingSuccess = List.of(0, 0, 0);
-        assertEquals(true, Judgement.checkResult(movingSuccess));
+        assertTrue(checkResult(movingSuccess));
     }
 
     @DisplayName("이동한 칸을 건널 수 있는지 없는지 확인한다.(up 성공)")
     @Test
-    void checkResult_upSuccess() {
+    void checkResultTest_upSuccess() {
         List<Integer> movingSuccess = List.of(0, 2, 2);
-        assertEquals(true, Judgement.checkResult(movingSuccess));
+        assertTrue(checkResult(movingSuccess));
     }
 
     @DisplayName("이동한 칸을 건널 수 있는지 없는지 확인한다.(down 실패)")
     @Test
-    void checkResult_downFailure() {
+    void checkResultTest_downFailure() {
         List<Integer> movingSuccess = List.of(0, 1);
-        assertEquals(false, Judgement.checkResult(movingSuccess));
+        assertFalse(checkResult(movingSuccess));
     }
 
     @DisplayName("이동한 칸을 건널 수 있는지 없는지 확인한다.(up 실패)")
     @Test
-    void checkResult_upFailure() {
+    void checkResultTest_upFailure() {
         List<Integer> movingSuccess = List.of(3);
-        assertEquals(false, Judgement.checkResult(movingSuccess));
+        assertFalse(checkResult(movingSuccess));
     }
 
+    @DisplayName("입력된 값에 따라 재시작할 지, 종료할 지를 결정한다.")
     @Test
-    void checkRestart() {
+    void checkRestartTest() {
+        List<String> gameCommands = List.of("R", "Q");
+        List<Boolean> results = List.of(true, false);
+        for (int i = 0; i < gameCommands.size(); i++) {
+            assertEquals(results.get(i), checkRestart(gameCommands.get(i)));
+        }
     }
 }
