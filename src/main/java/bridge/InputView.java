77bc5d330d8f53787318171911c4ae334b73c819
yoon-yoo-tak <dbsdbxkrzz@naver.com>
2022-11-21 19:51:17 +0900

    feat: InputView 메서드 static으로 변경

--- src/main/java/bridge/InputView.java
+++ src/main/java/bridge/InputView.java
@@ -10,7 +10,7 @@ public class InputView {
     /**
      * 다리의 길이를 입력받는다.
      */
-    public int readBridgeSize() {
+    public static int readBridgeSize() {
         OutputView.printInputBridgeLength();
         return Integer.parseInt(Console.readLine());
     }
@@ -18,7 +18,7 @@ public class InputView {
     /**
      * 사용자가 이동할 칸을 입력받는다.
      */
-    public String readMoving() {
+    public static String readMoving() {
         OutputView.printSelectDirection();
         return Console.readLine();
     }
@@ -26,7 +26,7 @@ public class InputView {
     /**
      * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
      */
-    public String readGameCommand() {
+    public static String readGameCommand() {
         OutputView.printRestartMessage();
         return Console.readLine();
     }
