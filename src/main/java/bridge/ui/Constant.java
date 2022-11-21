85237c9246a550778936c636f3b055ea1d29ad15
ddingmin <ddingminyt@gmail.com>
2022-11-22 00:25:52 +0900

    feat(java-bridge): Constant 사용 상수 추가

--- src/main/java/bridge/ui/Constant.java
+++ src/main/java/bridge/ui/Constant.java
@@ -1,7 +1,11 @@
 package bridge.ui;
 
 public class Constant {
+    // TODO: enum 으로 구현하기
     public static final int GAME_CLEAR = 1;
     public static final int GAME_RESTART = -1;
     public static final int GAME_FAIL = 0;
+    public static final String TOKEN_FAIL = "X";
+    public static final String TOKEN_CLEAR = "O";
+    public static final String TOKEN_NULL = " ";
 }
