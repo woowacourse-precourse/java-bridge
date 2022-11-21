3d4e6599178dd6f4e8c8dfbc55bcaed5ada5df9b
DongwookKim0823 <dwkim0823@naver.com>
2022-11-21 23:23:32 +0900

    feat(bridge): 현재까지 이동한 다리의 상태를 출력하는 기능

--- src/main/java/bridge/OutputView.java
+++ src/main/java/bridge/OutputView.java
@@ -1,5 +1,7 @@
 package bridge;
 
+import java.util.List;
+
 /**
  * 사용자에게 게임 진행 상황과 결과를 출력하는 역할을 한다.
  */
@@ -9,8 +11,12 @@ public class OutputView {
      * 현재까지 이동한 다리의 상태를 정해진 형식에 맞춰 출력한다.
      * <p>
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
+     * @param side
      */
-    public void printMap() {
+    public void printMap(List<List<String>> side) {
+        System.out.println("[ " + String.join(" | ", side.get(0)) + " ]");
+        System.out.println("[ " + String.join(" | ", side.get(1)) + " ]");
+        System.out.println();
     }
 
     /**
