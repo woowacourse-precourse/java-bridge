0074cf68c933194dc25559f4e1747f4e4b7a5b10
eojinsarang joo <wndjwlstkfkd@naver.com>
2022-11-21 15:43:26 +0900

    feat: 게임 시작 출력

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -3,6 +3,9 @@ package bridge;
 public class Application {
 
     public static void main(String[] args) {
+        
+        BridgeGame bridgeGame = new BridgeGame();
+        bridgeGame.start();
 
     }
 }
