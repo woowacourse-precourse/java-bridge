d4410fdfa5ffdf4faa703ca1f3b30e0d5348d52d
Namyunsuk <yunsuk0328@naver.com>
2022-11-21 22:43:37 +0900

    feat(Constant): 상수들을 열거한 형태로 관리하는 기능 구현

--- src/main/java/bridge/utils/Constant.java
+++ src/main/java/bridge/utils/Constant.java
@@ -1,4 +1,24 @@
 package bridge.utils;
 
+import java.text.DecimalFormat;
+
 public enum Constant {
+
+    UPPER("U"),
+    DOWN("D"),
+    RIGHT("O"),
+    WRONG("X"),
+    NOT_MATCH(" "),
+    RETRY("R"),
+    QUIT("Q");
+
+    public Object value;
+
+    Constant(Object value) {
+        this.value = value;
+    }
+
+    public Object getvalue() {
+        return value;
+    }
 }
