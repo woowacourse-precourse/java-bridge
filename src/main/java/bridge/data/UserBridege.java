52046e2aee26bae7c3b10e56c8e5e94485b5dade
sora5421 <sojeoung536985@gmail.com>
2022-11-22 03:32:10 +0900

    feat (compareInput) : make method that return string compareresult

--- src/main/java/bridge/data/UserBridege.java
+++ src/main/java/bridge/data/UserBridege.java
@@ -1,4 +1,23 @@
 package bridge.data;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class UserBridege {
+    private List<String> upBridge = new ArrayList<>();
+    private List<String> downBridge = new ArrayList<>();
+
+    public void makeUpBridge(String userInput, boolean correct) {
+       
+    }
+
+    private String compareInput(String upserInput, String standard, boolean correct) {
+        if (standard.equals(upserInput) && correct == true) {
+            return "O";
+        }
+        if (standard.equals(upserInput) && correct == false) {
+            return "X";
+        }
+        return " ";
+    }
 }
