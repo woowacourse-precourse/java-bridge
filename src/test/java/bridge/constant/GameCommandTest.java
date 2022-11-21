284360f36210ebea1e7c42e40d199d29266d981b
heesang <dlagmltkd12@naver.com>
2022-11-22 06:03:08 +0900

    test: GameCommandTest 테스트 이름 정리

--- src/test/java/bridge/constant/GameCommandTest.java
+++ src/test/java/bridge/constant/GameCommandTest.java
@@ -1,5 +1,6 @@
 package bridge.constant;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -7,6 +8,29 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class GameCommandTest {
+
+    private GameCommand restart;
+    private GameCommand quit;
+
+    @BeforeEach
+    void setUp() {
+        restart = GameCommand.RESTART;
+        quit = GameCommand.QUIT;
+    }
+
+    @DisplayName("재시작인지 종료인지 묻는 메소드 검증")
+    @Test
+    void isRestartAndQuit() {
+        assertThat(restart.isRestart()).isTrue();
+        assertThat(quit.isQuit()).isTrue();
+    }
+
+    @DisplayName("가지고 있는 필드 값 검증")
+    @Test
+    void getCommand() {
+        assertThat(restart.getCommand()).isEqualTo("R");
+        assertThat(quit.getCommand()).isEqualTo("Q");
+    }
 
     @DisplayName("R, Q가 주어질 때 GameCommand 생성")
     @Test
@@ -25,29 +49,11 @@ class GameCommandTest {
                 .hasMessage("[ERROR] 게임 재시작/종료 여부 입력의 경우 R과 Q 뿐이어야 합니다.");
     }
 
-    @DisplayName("GameCommand R, Q 이외의 입력 값 검증 시 예외 발생")
+    @DisplayName("R, Q 이외의 입력 값 검증 시 예외 발생")
     @Test
     void validateInput() {
         assertThatThrownBy(() -> GameCommand.validateInput("d"))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("[ERROR] 게임 재시작/종료 여부 입력의 경우 R과 Q 뿐이어야 합니다.");
-    }
-
-    @DisplayName("GameCommand가 재시작인지 종료인지 묻는 메소드 검증")
-    @Test
-    void isRestartAndQuit() {
-        GameCommand restart = GameCommand.RESTART;
-        GameCommand quit = GameCommand.QUIT;
-        assertThat(restart.isRestart()).isTrue();
-        assertThat(quit.isQuit()).isTrue();
-    }
-
-    @DisplayName("GameCommand가 가지고 있는 필드 값 검증")
-    @Test
-    void getCommand() {
-        GameCommand restart = GameCommand.RESTART;
-        GameCommand quit = GameCommand.QUIT;
-        assertThat(restart.getCommand()).isEqualTo("R");
-        assertThat(quit.getCommand()).isEqualTo("Q");
     }
 }
