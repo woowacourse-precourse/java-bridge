25d6be3802eb4b2ebffa4f302b1e4372459b7adf
koo9b9h <90121024+koo9b9h@users.noreply.github.com>
2022-11-21 22:58:37 +0900

    feat: 다리 현황 출력할 데이터 가공 기능 추가
    returnContentsMap() : 출력에 대한 정보 종합 가공
    lineChoice() : 어떤 라인에다가 표시할지 정하는 기능
    addLastMap() : 새로 선택한 칸이 정답인지 오답인지 표시하는 기능

---
+++ src/main/java/bridge/presentation/ReturnPrintMap.java
@@ -0,0 +1,49 @@
+package bridge.presentation;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class ReturnPrintMap {
+    public Map<String, String> returnContentsMap(List<String> userSelectResult, boolean success) {
+        Map<String, String> contentsMap = new HashMap<>();
+        contentsMap.put("upContents", "");
+        contentsMap.put("downContents", "");
+        for (int index = 0; (index + 1) < userSelectResult.size(); index++) {
+            contentsMap = lineChoice(userSelectResult, contentsMap, index);
+        }//for
+        contentsMap = addLastMap(userSelectResult, contentsMap, success);
+        return contentsMap;
+    }//returnContentsMap
+
+    private Map<String, String> lineChoice(List<String> userSelectResult, Map<String, String> contentsMap, int index) {
+        if (userSelectResult.get(index).equals("U")) {
+            contentsMap.put("upContents", contentsMap.get("upContents") + "O | ");
+            contentsMap.put("downContents", contentsMap.get("downContents") + "  | ");
+            return contentsMap;
+        }//if
+        contentsMap.put("upContents", contentsMap.get("upContents") + "  | ");
+        contentsMap.put("downContents", contentsMap.get("downContents") + "O | ");
+        return contentsMap;
+    }// lineChoice
+
+    private Map<String, String> addLastMap(List<String> userSelectResult, Map<String, String> contentsMap, boolean success) {
+        if (userSelectResult.get(userSelectResult.size() - 1).equals("U") && success) {
+            contentsMap.put("upContents", contentsMap.get("upContents") + "O");
+            contentsMap.put("downContents", contentsMap.get("downContents") + " ");
+        }//if
+        if (userSelectResult.get(userSelectResult.size() - 1).equals("D") && success) {
+            contentsMap.put("upContents", contentsMap.get("upContents") + " ");
+            contentsMap.put("downContents", contentsMap.get("downContents") + "O");
+        }//if
+        if (userSelectResult.get(userSelectResult.size() - 1).equals("U") && !success) {
+            contentsMap.put("upContents", contentsMap.get("upContents") + "X");
+            contentsMap.put("downContents", contentsMap.get("downContents") + " ");
+        }//if
+        if (userSelectResult.get(userSelectResult.size() - 1).equals("D") && !success) {
+            contentsMap.put("upContents", contentsMap.get("upContents") + " ");
+            contentsMap.put("downContents", contentsMap.get("downContents") + "X");
+        }//if
+        return contentsMap;
+    }//addLastMap
+}// end class
