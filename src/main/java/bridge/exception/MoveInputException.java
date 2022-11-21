40be3ba5be521df2a59c845df1003ccfc201b422
pasly0920 <inwoo920621@gmail.com>
2022-11-22 01:22:44 +0900

    feat: 사용자 이동 입력 예외 처리

---
+++ src/main/java/bridge/exception/MoveInputException.java
@@ -0,0 +1,25 @@
+package bridge.exception;
+
+public class MoveInputException {
+
+    private static final String USER_UP = "U";
+    private static final String USER_DOWN = "D";
+
+    public boolean isInputValid(String input) {
+        boolean isValid = true;
+        try {
+            inputOnlyConsistOfUD(input);
+        } catch (IllegalArgumentException exception) {
+            System.out.println(exception.getMessage());
+            isValid = false;
+        } finally {
+            return isValid;
+        }
+    }
+
+    private void inputOnlyConsistOfUD(String input) {
+        if (!input.equals(USER_UP) && !input.equals(USER_DOWN)) {
+            throw new IllegalArgumentException("[ERROR] 이동 칸 입력은 U, D중 하나여야 합니다.");
+        }
+    }
+}
