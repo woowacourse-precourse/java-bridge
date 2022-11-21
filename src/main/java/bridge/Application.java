eb9c24e4ad12ee8a3758523a8e97ee295eb556d7
jaewoo <wjdwodn0722@naver.com>
2022-11-21 13:03:44 +0900

    refact : main 함수를 부분별로 나눠서 수정

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -2,8 +2,7 @@ package bridge;
 
 public class Application {
 
-    public static void main(String[] args) {
-        // TODO: 프로그램 구현
+    private static BridgeGame initiate(){
         BridgeNumberGenerator generator = new BridgeRandomNumberGenerator();
         BridgeMaker maker = new BridgeMaker(generator);
 
@@ -13,29 +12,56 @@ public class Application {
         OutputView.printInputBridgeSize();
         BridgeGame game = new BridgeGame(maker.makeBridge(InputView.readBridgeSize()));
         System.out.println();
+        return game;
+    }
 
-        String last, result;
+    private static String startLoop(BridgeGame game){
+        String last ="";
         while(true){
-            OutputView.printInputForward();
-            last = InputView.readMoving();
-            result = game.judge(last);
-            if(result.equals("O")){
-                OutputView.printMap(game, last);
-                if(game.isGameWin(last))
-                    break;
-                continue;
+            if(actLoopContent(game, last)){
+                break;
             }
-            if(result.equals("X")) {
-                OutputView.printMap(game, last);
-                OutputView.printIsGameRetry();
-                if (InputView.readGameCommand().equals("R")) {
-                    System.out.println();
-                    game.retry();
-                    continue;
-                }
-            }
-            break;
         }
+        return last;
+    }
+
+    private static boolean actLoopContent(BridgeGame game, String last){
+        OutputView.printInputForward();
+        last = InputView.readMoving();
+        String result = game.judge(last);
+        if(actIfRight(game,last,result)){   //true 게임 완료 종료
+            return true;
+        }
+        if(actIfWrong(game,last,result)){   //true 게임 재시도 x 종료
+            return true;
+        }
+        return false;
+    }
+
+    private static boolean actIfRight(BridgeGame game, String last, String result){
+        if(result.equals("O")){
+            OutputView.printMap(game, last);
+            return game.isGameWin(last);
+        }
+        return false;
+    }
+
+    private static boolean actIfWrong(BridgeGame game, String last, String result){
+        if(result.equals("X")) {
+            OutputView.printMap(game, last);
+            OutputView.printIsGameRetry();
+            if (InputView.readGameCommand().equals("Q")) {
+                return true;
+            }
+            System.out.println();
+            game.retry();
+        }
+        return false;
+    }
+    public static void main(String[] args) {
+        // TODO: 프로그램 구현
+        BridgeGame game = initiate();
+        String last = startLoop(game);
         OutputView.printResult(game,last);
     }
 }
