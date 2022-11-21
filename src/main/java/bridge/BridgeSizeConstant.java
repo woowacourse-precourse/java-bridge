7c0b17fc60cad8954dc29ac923bb401c4b15cc53
kimJaeHyeon <kmr2644@gmail.com>
2022-11-21 20:06:02 +0900

    feat: private 생성자 추가

--- src/main/java/bridge/BridgeSizeConstant.java
+++ src/main/java/bridge/BridgeSizeConstant.java
@@ -4,5 +4,9 @@ public class BridgeSizeConstant {
     public static final int MAX_BRIDGE_SIZE = 20;
     public static final int MIN_BRIDGE_SIZE = 3;
 
+    private BridgeSizeConstant() {
+
+    }
+
 
 }
