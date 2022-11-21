31c00880b4084f570d01cf3f3784195973566f5f
rjsah09 <83952174+rjsah09@users.noreply.github.com>
2022-11-21 21:44:25 +0900

    test: InputView 클래스 테스트 추가

--- src/test/java/bridge/InputViewTest.java
+++ src/test/java/bridge/InputViewTest.java
@@ -1,4 +1,32 @@
 package bridge;
 
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
 public class InputViewTest {
+
+    InputView inputView = new InputView();
+
+    @Test
+    void 다리길이_예외검출() {
+        List<String> inputs = Arrays.asList("1", "2", "21", "22", "2a", "bbb", "woowacourse");
+
+        for (String input: inputs) {
+            Assertions.assertThatThrownBy(() -> inputView.validateBridgeSize(input))
+                    .isInstanceOf(IllegalArgumentException.class);
+        }
+    }
+
+    @Test
+    void 다리길이_정상값() {
+        List<String> inputs = Arrays.asList("3", "4", "19", "20");
+
+        for (String input: inputs) {
+            Assertions.assertThatNoException()
+                    .isThrownBy(() -> inputView.validateBridgeSize(input));
+        }
+    }
 }
