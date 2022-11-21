6dac37556891d43e95f46c5e0b3cf91e18edc848
dong young <dong5854@gmail.com>
2022-11-22 08:24:09 +0900

    test: 재시도 예외 발생 시 재입력 테스트

--- src/test/java/bridge/ApplicationTest.java
+++ src/test/java/bridge/ApplicationTest.java
@@ -48,7 +48,7 @@ class ApplicationTest extends NsTest {
     }
 
     @Test
-    void 숫자_범위_트스트() {
+    void 숫자_범위_테스트() {
         assertSimpleTest(() -> {
             runException(Integer.toString(InputView.MAX_BRIDGE + 1));
             assertThat(output()).contains(ERROR_MESSAGE);
@@ -70,6 +70,24 @@ class ApplicationTest extends NsTest {
                     "[   | O |   ]",
                     "게임 성공 여부: 성공",
                     "총 시도한 횟수: 1"
+            );
+
+            int upSideIndex = output().indexOf("[ O |   | O ]");
+            int downSideIndex = output().indexOf("[   | O |   ]");
+            assertThat(upSideIndex).isLessThan(downSideIndex);
+        }, 1, 0, 1);
+    }
+
+    @Test
+    void 재시도_재입력_테스트() {
+        assertRandomNumberInRangeTest(() -> {
+            run("3", "U", "D", "D", "P", "R", "U", "D", "U");
+            assertThat(output()).contains(
+                    "최종 게임 결과",
+                    "[ O |   | O ]",
+                    "[   | O |   ]",
+                    "게임 성공 여부: 성공",
+                    "총 시도한 횟수: 2"
             );
 
             int upSideIndex = output().indexOf("[ O |   | O ]");
