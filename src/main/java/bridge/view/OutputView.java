11bdebcdd4b93c58ea1e0835e3f9b81be7e0ebdd
sooyoungh <sooyoung.h8@gmail.com>
2022-11-21 11:30:10 +0900

    ✨ feat(OutputView): 게임 시작 메시지 출력하는 기능 구현

--- src/main/java/bridge/view/OutputView.java
+++ src/main/java/bridge/view/OutputView.java
@@ -15,6 +15,10 @@ public class OutputView {
     private final static String SPACE = " ";
     private final static String BAR = "|";
 
+    public static void printStart() {
+        out.println("다리 건너기 게임을 시작합니다.\n");
+    }
+
     /**
      * 현재까지 이동한 다리의 상태를 정해진 형식에 맞춰 출력한다.
      * <p>
