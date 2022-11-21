cb1c11b385db3c5eee99c58679cd7af02abb25a2
Jaehong Lee <woohong24@naver.com>
2022-11-21 15:57:04 +0900

    feat: 다리 생성 기능 추가
    class BridgeMaker 구현
    class InputView 의 메소드 readBridgeSize() 구현

--- src/main/java/bridge/BridgeMaker.java
+++ src/main/java/bridge/BridgeMaker.java
@@ -1,5 +1,6 @@
 package bridge;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -13,11 +14,53 @@ public class BridgeMaker {
         this.bridgeNumberGenerator = bridgeNumberGenerator;
     }
 
+    public int readBridgeSize() {
+        InputView inputView = new InputView();
+        String bridgeSize = inputView.readBridgeSize();
+        return validateBridgeSize(bridgeSize);
+    }
+
+    private int validateBridgeSize(String bridgeSize) {
+        int integerBridgeSize = 0;
+        integerBridgeSize = validateBridgeSizeInteger(bridgeSize);
+        validateBridgeSizeRange(integerBridgeSize);
+        return integerBridgeSize;
+    }
+
+    private void validateBridgeSizeRange(int integerBridgeSize) {
+        if (integerBridgeSize < 3 || integerBridgeSize > 20) {
+            throw new IllegalArgumentException("[ERROR] 다리 길이는 3부터 20 사이의 숫자여야 합니다.");
+        }
+    }
+
+    private int validateBridgeSizeInteger(String bridgeSize) {
+        int integerBridgeSize;
+        try {
+            integerBridgeSize = Integer.parseInt(bridgeSize);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("[ERROR] 다리 길이는 3부터 20 사이의 숫자여야 합니다.");
+        }
+        return integerBridgeSize;
+    }
+
     /**
      * @param size 다리의 길이
      * @return 입력받은 길이에 해당하는 다리 모양. 위 칸이면 "U", 아래 칸이면 "D"로 표현해야 한다.
      */
     public List<String> makeBridge(int size) {
-        return null;
+        List<String> bridge = new ArrayList<>();
+        for (int i = 0; i < size; i++) {
+            buildBridge(bridge);
+        }
+        return bridge;
+    }
+
+    private void buildBridge(List<String> bridge) {
+        int randomNumber = bridgeNumberGenerator.generate();
+        if (randomNumber == 0) {
+            bridge.add("D");
+        } else if (randomNumber == 1) {
+            bridge.add("U");
+        }
     }
 }
