123f0315cf81cf4aa344ff4cbfe965d5a36cdada
seoyoung <sy9723@korea.ac.kr>
2022-11-21 11:44:38 +0900

    refactor: 수정된 기능 목록에 맞게 테스트 리팩토링

--- src/test/java/bridge/domain/BridgeGameTest.java
+++ src/test/java/bridge/domain/BridgeGameTest.java
@@ -1,7 +1,6 @@
 package bridge.domain;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.util.List;
 import org.junit.jupiter.api.DisplayName;
@@ -12,33 +11,36 @@ class BridgeGameTest {
     private final List<String> bridge = List.of("D", "U", "U");
 
     /** 0 -> down 성공, 1 -> down 실패, 2 -> up 성공, 3 -> up 실패 **/
-    @DisplayName("이동한 칸을 건널 수 있는지 없는지 확인한다: 1. down 성공")
+    @DisplayName("한 칸을 이동시킨다.(down 성공)")
     @Test
     void moveSuccess_down() {
         assertEquals(0, BridgeGame.move(bridge, 0, "D"));
     }
 
-    @DisplayName("이동한 칸을 건널 수 있는지 없는지 확인한다: 2. up 성공")
+    @DisplayName("한 칸을 이동시킨다.(up 성공)")
     @Test
     void moveSuccess_up() {
         assertEquals(2, BridgeGame.move(bridge, 2, "U"));
     }
 
-    @DisplayName("이동한 칸을 건널 수 있는지 없는지 확인한다: 3. down 실패")
+    @DisplayName("한 칸을 이동시킨다.(down 실패)")
     @Test
     void moveFailure_down() {
         assertEquals(1, BridgeGame.move(bridge, 2, "D"));
     }
 
-    @DisplayName("이동한 칸을 건널 수 있는지 없는지 확인한다: 4. up 실패")
+    @DisplayName("한 칸을 이동시킨다.(up 실패)")
     @Test
     void moveFailure_up() {
         assertEquals(3, BridgeGame.move(bridge, 0, "U"));
     }
 
-    @DisplayName("이동한 칸을 건널 수 없는 경우 재시작하도록 한다")
+    @DisplayName("확인한 결과에 따라 재시작 여부를 결정한다.")
     @Test
-    void retry() {
-        assertTrue(BridgeGame.retry(false));
+     void retry() {
+        List<Boolean> presentResults = List.of(true, false);
+        for (Boolean presentResult : presentResults) {
+            assertEquals(!presentResult, BridgeGame.retry(presentResult));
+        }
     }
 }
