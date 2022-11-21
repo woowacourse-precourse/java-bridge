b4fd770175eae85e6c5de1e90d447b0ac05cf374
limzzum <chfhddl1234@naver.com>
2022-11-22 00:40:20 +0900

    refactor : 필드 이름 수정

--- src/main/java/bridge/Validate.java
+++ src/main/java/bridge/Validate.java
@@ -1,21 +1,21 @@
 package bridge;
 
 public class Validate {
-    private final String ERROR= "[ERROR]";
+    private final String ERROR_MESSAGE = "[ERROR]";
 
     public void validateNumberRange(String input , int min, int max){
         if(!input.matches("[0-9]+")){
-            throw new IllegalArgumentException(ERROR+" 숫자만 입력 가능합니다");
+            throw new IllegalArgumentException(ERROR_MESSAGE +" 숫자만 입력 가능합니다");
         }
         int num = Integer.parseInt(input);
         if(num<min || num>max){
-            throw new IllegalArgumentException(ERROR+" "+min+"이상 "+max+"이하의 숫자만 가능합니다");
+            throw new IllegalArgumentException(ERROR_MESSAGE +" "+min+"이상 "+max+"이하의 숫자만 가능합니다");
         }
     }
 
     public void validateContainWord(String input,String word1, String word2){
         if(!input.equals(word1) && !input.equals(word2)){
-            throw new IllegalArgumentException(ERROR+" "+word1+" 혹은 "+word2+"만 입력 가능합니다");
+            throw new IllegalArgumentException(ERROR_MESSAGE +" "+word1+" 혹은 "+word2+"만 입력 가능합니다");
         }
     }
 }
