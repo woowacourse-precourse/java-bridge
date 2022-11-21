da467d1d8e3472681a4a80a2cd00650d98de940e
Yujin830 <rladbwls30@naver.com>
2022-11-21 23:43:53 +0900

    Feat: 우선 입력한 다리길이 만큼 U,D 입력받기

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -6,12 +6,18 @@ public class Application {
 
     public static void main(String[] args) {
         int size;
+        String move;
         Announcement.start();
         size = InputView.readBridgeSize();
 
         BridgeRandomNumberGenerator bridgeRandomNumberGenerator = new BridgeRandomNumberGenerator();
         BridgeMaker bridgeMaker = new BridgeMaker(bridgeRandomNumberGenerator);
         List<String> bridge = bridgeMaker.makeBridge(size);
+
+        for(int i=0;i<size;i++){
+            Announcement.move();
+            move = InputView.readMoving();
+        }
         // TODO: 프로그램 구현
     }
 }
