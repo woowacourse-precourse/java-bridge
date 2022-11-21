5b7a3aeb7d1dd9d0a0c8ee03f86af1104b9f305f
MJoon-Jung <gjgjajaj31@gmail.com>
2022-11-21 16:30:56 +0900

    test<Application>: 재시도 R입력할 경우 플레이어 처음부터 시작 및 지도가 초기화되는지 확인 테스트 추가

--- src/test/java/bridge/ApplicationTest.java
+++ src/test/java/bridge/ApplicationTest.java
@@ -26,11 +26,11 @@ class ApplicationTest extends NsTest {
         assertRandomNumberInRangeTest(() -> {
             run("3", "U", "D", "U");
             assertThat(output()).contains(
-                "최종 게임 결과",
-                "[ O |   | O ]",
-                "[   | O |   ]",
-                "게임 성공 여부: 성공",
-                "총 시도한 횟수: 1"
+                    "최종 게임 결과",
+                    "[ O |   | O ]",
+                    "[   | O |   ]",
+                    "게임 성공 여부: 성공",
+                    "총 시도한 횟수: 1"
             );
 
             int upSideIndex = output().indexOf("[ O |   | O ]");
@@ -45,6 +45,34 @@ class ApplicationTest extends NsTest {
             runException("a");
             assertThat(output()).contains(ERROR_MESSAGE);
         });
+    }
+
+    @Test
+    void 재시도_R입력시_처음부터_시작_및_지도_초기화되어야한다() {
+        assertRandomNumberInRangeTest(() -> {
+            run("3", "U", "U", "R", "U", "D", "U");
+            assertThat(output()).contains(
+                    "다리 건너기 게임을 시작합니다.",
+                    "다리의 길이를 입력해주세요.",
+                    "이동할 칸을 선택해주세요. (위: U, 아래: D)",
+                    "[ O ]",
+                    "[   ]",
+                    "[ O | X ]",
+                    "[   |   ]",
+                    "게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)",
+                    "[ O |   ]",
+                    "[   | O ]",
+                    "최종 게임 결과",
+                    "[ O |   | O ]",
+                    "[   | O |   ]",
+                    "게임 성공 여부: 성공",
+                    "총 시도한 횟수: 2"
+            );
+
+            int upSideIndex = output().indexOf("[ O |   | O ]");
+            int downSideIndex = output().indexOf("[   | O |   ]");
+            assertThat(upSideIndex).isLessThan(downSideIndex);
+        }, 1, 0, 1);
     }
 
     @Override
