6a7cc1fb5e67e6017f05f694593ecc5588199551
NAM GIBEOM <rlqja0523@naver.com>
2022-11-21 21:50:21 +0900

    test: 공백을 입력하면 예외를 던지는지 테스트

--- src/test/java/bridge/exception/UserExceptionTest.java
+++ src/test/java/bridge/exception/UserExceptionTest.java
@@ -4,19 +4,26 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class UserExceptionTest {
+    CustomUserException userException = new CustomUserException();
 
     @Test
     @DisplayName("빈 값을 입력하면 에러 메시지는 ExceptionCode의 에러 메시지다.")
     void isEmptyTest() {
-        CustomUserException userException = new CustomUserException();
         try {
             userException.isEmpty("");
         } catch (IllegalArgumentException e) {
             String errorMessage = e.getMessage();
             assertThat(ExceptionCode.IS_EMPTY.getMessage()).isEqualTo(errorMessage);
         }
+    }
+
+    @Test
+    @DisplayName("공백을 입력하면 예외를 반환한다.")
+    void isBlankTest() {
+        assertThrows(IllegalArgumentException.class, () -> userException.isBlank(" "));
     }
 
     private static class CustomUserException extends UserException {
