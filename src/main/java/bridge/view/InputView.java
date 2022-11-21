081043308fb848607cdd30bd6224d6e44cbc52a0
ryanha <ryanha@handong.ac.kr>
2022-11-21 22:18:39 +0900

    refactor(view): InputView, enums를 활용한 하드코딩 요소 제거

--- src/main/java/bridge/view/InputView.java
+++ src/main/java/bridge/view/InputView.java
@@ -6,6 +6,61 @@ import camp.nextstep.edu.missionutils.Console;
  * 사용자로부터 입력을 받는 역할을 한다.
  */
 public class InputView {
+
+    private static enum BridgeSizeRange {
+        RANGE_START(3),
+        RANGE_END(20);
+
+        private final int rangeValue;
+
+        BridgeSizeRange(int rangeValue) {
+            this.rangeValue = rangeValue;
+        }
+
+        public int getRangeValue() {
+            return this.rangeValue;
+        }
+    }
+
+    private static enum Message {
+        LENGTH_VALIDATION_FAILED("입력값의 길이가 유효하지 않습니다."),
+        DIGIT_VALIDATION_FAILED("입력된 값이 숫자가 아닙니다."),
+        RANGE_VALIDATION_FAILED("입력값은 "
+                + BridgeSizeRange.RANGE_START.getRangeValue()
+                + " ~ "
+                + BridgeSizeRange.RANGE_END.getRangeValue()
+                + "사이의 정수이어야 합니다."),
+        SINGLE_CHARACTER_VALIDATION_FAILED("입력한 명령의 형식이 올바르지 않습니다."),
+        COMMAND_VALIDATION_FAILED("사용할 수 있는 명령이 아닙니다.");
+        
+        private final String message;
+
+        Message(String message) {
+            this.message = message;
+        }
+
+        public String getMessage() {
+            return this.message;
+        }
+    }
+
+    public static enum Command {
+        MOVE_UP("U"),
+        MOVE_DOWM("D"),
+        QUIT("Q"),
+        RETRY("R");
+
+        private final String command;
+
+        Command(String command) {
+            this.command = command;
+        }
+
+        public String getCommand() {
+            return this.command;
+        }
+    }
+
     /**
      * 다리의 길이를 입력받는다.
      */
@@ -23,7 +78,7 @@ public class InputView {
         OutputView outputView = new OutputView();
         outputView.printMessage(message, end); // 입력받기 전 사용자 유도를 위한 안내 메시지를 출력함
         String userInput = Console.readLine();
-        return validateCommand(userInput, "U", "D");
+        return validateCommand(userInput, Command.MOVE_UP.getCommand(), Command.MOVE_DOWM.getCommand());
     }
 
     /**
@@ -33,7 +88,7 @@ public class InputView {
         OutputView outputView = new OutputView();
         outputView.printMessage(message, end); // 입력받기 전 사용자 유도를 위한 안내 메시지를 출력함
         String userInput = Console.readLine();
-        return validateCommand(userInput, "R", "Q");
+        return validateCommand(userInput, Command.RETRY.getCommand(), Command.QUIT.getCommand());
     }
 
     /*
@@ -59,7 +114,7 @@ public class InputView {
     private void lengthIsValid(String userInput) throws IllegalArgumentException {
         if (userInput.length() == 0 || userInput.length() > 2) {
             OutputView outputView = new OutputView();
-            outputView.printErrorMessage("입력값의 길이가 유효하지 않습니다.");
+            outputView.printErrorMessage(Message.LENGTH_VALIDATION_FAILED.getMessage());
             throw new IllegalArgumentException();
         }
     }
@@ -69,18 +124,17 @@ public class InputView {
         for (char digit : eachDigits) {
             if (!Character.isDigit(digit)) {
                 OutputView outputView = new OutputView();
-                outputView.printErrorMessage("입력된 값이 숫자가 아닙니다.");
+                outputView.printErrorMessage(Message.DIGIT_VALIDATION_FAILED.getMessage());
                 throw new IllegalArgumentException();
             }
         }
     }
 
     private void rangeIsValid(int length) throws IllegalArgumentException {
-        if (length < 3 || length > 20) {
+        if (length < BridgeSizeRange.RANGE_START.getRangeValue()
+                || length > BridgeSizeRange.RANGE_END.getRangeValue()) {
             OutputView outputView = new OutputView();
-            outputView.printErrorMessage("입력값은 "
-                    + 3 + " ~ "
-                    + 20 + "사이의 정수이어야 합니다.");
+            outputView.printErrorMessage(Message.RANGE_VALIDATION_FAILED.getMessage());
             throw new IllegalArgumentException();
         }
     }
@@ -88,7 +142,7 @@ public class InputView {
     private void isSingleCharacter(String userInput) throws IllegalArgumentException {
         if (userInput.length() != 1) {
             OutputView outputView = new OutputView();
-            outputView.printErrorMessage("입력한 명령의 형식이 올바르지 않습니다.");
+            outputView.printErrorMessage(Message.SINGLE_CHARACTER_VALIDATION_FAILED.getMessage());
             throw new IllegalArgumentException();
         }
     }
@@ -96,7 +150,7 @@ public class InputView {
     private void isValidCommand(String userInput, String command1, String command2) throws IllegalArgumentException {
         if (!userInput.equals(command1) && !userInput.equals(command2)) {
             OutputView outputView = new OutputView();
-            outputView.printErrorMessage("사용할 수 있는 명령이 아닙니다.");
+            outputView.printErrorMessage(Message.COMMAND_VALIDATION_FAILED.getMessage());
             throw new IllegalArgumentException();
         }
     }
