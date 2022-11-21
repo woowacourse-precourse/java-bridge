5a853871ce2e8d2774eda3871d1706bd96bac0f0
Hi-Imjaeyoung <imjy2757@naver.com>
2022-11-21 18:13:57 +0900

    test(BridgeGameTest) : 게임실행에 관한 테스트코드 작성.

--- src/test/java/bridge/BridgeGameTest.java
+++ src/test/java/bridge/BridgeGameTest.java
@@ -1,14 +1,17 @@
 package bridge;
 
 import bridge.model.BridgeGame;
+import bridge.model.BridgeMaker;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeTest;
+import static org.assertj.core.util.Lists.newArrayList;
 import java.util.List;
-
+import camp.nextstep.edu.missionutils.test.NsTest;
 import static org.assertj.core.api.Assertions.assertThat;
 
-class BridgeGameTest {
+class BridgeGameTest extends NsTest{
     @DisplayName("입력된 이동값과 다리정보가 같다면 true를 리턴")
     @Test
     void 입력된_이동값과_다리정보의_값이_같음(){
@@ -32,5 +35,56 @@ class BridgeGameTest {
         game.move("U");
         assertThat(true).isEqualTo(game.checkSuccess());
         assertThat(1).isEqualTo(game.getTryNumber());
+    }
+    @DisplayName("다리건너기 게임 2번 시도만에 성공")
+    @Test
+    void 두번_시도만에_성공(){
+        assertRandomNumberInRangeTest(() -> {
+            run("3", "U", "D", "D", "R", "U", "D", "U");
+            assertThat(output()).contains(
+                    "최종 게임 결과",
+                    "[ O |   | O ]",
+                    "[   | O |   ]",
+                    "게임 성공 여부: 성공",
+                    "총 시도한 횟수: 2"
+            );
+            int upSideIndex = output().indexOf("[ O |   | O ]");
+            int downSideIndex = output().indexOf("[   | O |   ]");
+            assertThat(upSideIndex).isLessThan(downSideIndex);
+        }, 1, 0, 1);
+    }
+    @DisplayName("다리건너기게임 중도 포기")
+    @Test
+    void 중도포기(){
+        assertRandomNumberInRangeTest(() -> {
+            run("3", "U", "D", "D", "Q");
+            assertThat(output()).contains(
+                    "최종 게임 결과",
+                    "[ O |   |   ]",
+                    "[   | O | X ]",
+                    "게임 성공 여부: 실패",
+                    "총 시도한 횟수: 1"
+            );
+            int upSideIndex = output().indexOf("[ O |   |   ]");
+            int downSideIndex = output().indexOf("[   | O | X ]");
+            assertThat(upSideIndex).isLessThan(downSideIndex);
+        }, 1, 0, 1);
+    }
+    @Override
+    protected void runMain() {
+        Application.main(new String[]{});
+    }
+    static class TestNumberGenerator implements BridgeNumberGenerator {
+
+        private final List<Integer> numbers;
+
+        TestNumberGenerator(List<Integer> numbers) {
+            this.numbers = numbers;
+        }
+
+        @Override
+        public int generate() {
+            return numbers.remove(0);
+        }
     }
 }
