4afd08e42d7647ff72d850c1d36998a31bd8aba9
jinju9553 <69393506+jinju9553@users.noreply.github.com>
2022-11-21 23:59:27 +0900

    Style(view) : 출력 형식에 개행 추가

--- src/main/java/bridge/View/InputView.java
+++ src/main/java/bridge/View/InputView.java
@@ -3,19 +3,13 @@ package bridge.View;
 import bridge.Service.InputValidator;
 import camp.nextstep.edu.missionutils.Console;
 
-/**
- * 사용자로부터 입력을 받는 역할을 한다.
- */
 public class InputView {
 
     private static final String READ_SIZE_MESSAGE = "다리의 길이를 입력해주세요.";
-    private static final String READ_MOVING_MESSAGE = "이동할 칸을 선택해주세요. (위: U, 아래: D)";
-    private static final String READ_COMMAND_MESSAGE = "게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)";
+    private static final String READ_MOVING_MESSAGE = "\n이동할 칸을 선택해주세요. (위: U, 아래: D)";
+    private static final String READ_COMMAND_MESSAGE = "\n게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)";
     private InputValidator inputValidator = new InputValidator();
 
-    /**
-     * 다리의 길이를 입력받는다.
-     */
     public int readBridgeSize() {
         System.out.println(READ_SIZE_MESSAGE);
 
@@ -29,9 +23,6 @@ public class InputView {
         return size;
     }
 
-    /**
-     * 사용자가 이동할 칸을 입력받는다.
-     */
     public String readMoving() {
         System.out.println(READ_MOVING_MESSAGE);
 
@@ -42,9 +33,6 @@ public class InputView {
         return input;
     }
 
-    /**
-     * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
-     */
     public String readGameCommand() {
         System.out.println(READ_COMMAND_MESSAGE);
 
