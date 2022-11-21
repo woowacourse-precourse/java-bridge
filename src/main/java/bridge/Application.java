2da94ef66f4ce1792ea66cacf19992604dee38ca
cgk95 <kjkj5868@gmail.com>
2022-11-22 08:30:49 +0900

    refactor(domain) : Model의 생성자를 호출하는 대신 Controller의 getter 메서드를 호출함으로써 MVC 패턴을 지키고 객체를 함부로 여러 번 생성하는 것을 방지하도록 구현.

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -1,11 +1,9 @@
 package bridge;
 
-import bridge.Model.BridgeGame;
-
 public class Application {
     public static void main(String[] args) {
         try {
-            new BridgeGame();
+            GameController.getGameController();
         } catch (IllegalArgumentException e) {
             System.out.println(e);
         }
