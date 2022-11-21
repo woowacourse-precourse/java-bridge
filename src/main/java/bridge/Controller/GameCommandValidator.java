c25fdc89a9b951ccc5bd09ce415bee50d53d0da9
sypark <soun997@gmail.com>
2022-11-21 23:13:01 +0900

    feat(GameCommandValidator): 해당 클래스 추가 및 validate() 메소드 구현(올바르지 않은 재시작 혹은 종료 명령 입력 시 예외 처리)

---
+++ src/main/java/bridge/Controller/GameCommandValidator.java
@@ -0,0 +1,12 @@
+package bridge.Controller;
+
+public class GameCommandValidator implements Validator{
+
+    private static final String ERROR_NON_EXISTENT_COMMAND = "[ERROR] 올바르지 않은 명령입니다.";
+
+    public void validate(String input){
+        if (!input.equals("R") && !input.equals("Q")){
+            throw new IllegalArgumentException(ERROR_NON_EXISTENT_COMMAND);
+        }
+    }
+}
