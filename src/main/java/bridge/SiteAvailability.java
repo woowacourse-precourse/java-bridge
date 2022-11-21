4162d8ba4ccea9d8926430f2945327a29f5eb5c0
오승윤 <>
2022-11-21 11:55:00 +0900

    feat (SiteAvailability) : 위 아래 칸 접근 여부 Enum

---
+++ src/main/java/bridge/SiteAvailability.java
@@ -0,0 +1,16 @@
+package bridge;
+
+public enum SiteAvailability {
+    ACCESS_UP_SITE(1),
+    ACCESS_DOWN_SITE(0);
+
+    int availability;
+
+    SiteAvailability(int availability) {
+        this.availability = availability;
+    }
+
+    public boolean isSameValue(int value) {
+        return this.availability == value;
+    }
+}
