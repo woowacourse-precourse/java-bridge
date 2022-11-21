ee4b84922dca75a5c9285fb0fe95cf95df4fc59a
wonju-dev <wonjulee.dev@gmail.com>
2022-11-22 03:47:26 +0900

    feat(BridgeGameExecutor): 새로운 라운드 시작 기능 구현
    라운드는 'bride 추측 시도 1회'를 의미합니다. startNewRound는
    추측 시도 동안 발생하는 사용자 입력, 출력, 로직 수행을 담당합니다.

--- src/main/java/bridge/BridgeGameExecutor.java
+++ src/main/java/bridge/BridgeGameExecutor.java
@@ -1,13 +1,30 @@
 package bridge;
 
 
+import bridge.enums.RoundResult;
+import bridge.enums.MoveResult;
+
 public class BridgeGameExecutor {
     private static final BridgeGameExecutor instance = new BridgeGameExecutor();
+    private static final InputView inputView = InputView.getInstance();
+    private static final OutputView outputView = OutputView.getInstance();
 
     private BridgeGameExecutor() {
     }
 
     public static BridgeGameExecutor getInstance() {
         return instance;
+    }
+
+    private RoundResult startNewRound(BridgeGame bridgeGame) {
+        while (!bridgeGame.reachOppositeSide()) {
+            MoveResult moveResult = bridgeGame.move(inputView.readMoving());
+            outputView.printMap(bridgeGame);
+            if (moveResult == MoveResult.NOT_ANSWER) {
+                return RoundResult.FAIL;
+            }
+        }
+
+        return RoundResult.SUCCESS;
     }
 }
