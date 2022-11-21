750bd662eba04733e1eb9f0f8cbabf53ccd074c7
JunHyeon <dlwnsgus07@naver.com>
2022-11-21 20:31:50 +0900

    fix: 출력 내용에 공백이 있는 버그 수정

--- src/main/java/view/OutputView.java
+++ src/main/java/view/OutputView.java
@@ -10,7 +10,8 @@ import java.util.Objects;
  * 사용자에게 게임 진행 상황과 결과를 출력하는 역할을 한다.
  */
 public class OutputView {
-    public static final String IS_CLEAR = "게임 성공 여부 : ";
+    public static final String FINAL_RESULT = "최종 게임 결과";
+    public static final String IS_CLEAR = "게임 성공 여부: ";
     public static final String TOTAL_NUMBER_ATTEMPTS  = "총 시도한 횟수: ";
 
     /**
@@ -52,6 +53,8 @@ public class OutputView {
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
     public static void printResult(BridgeGame bridgeGame) {
+        System.out.println(FINAL_RESULT);
+        printMap(bridgeGame);
         System.out.print(IS_CLEAR);
         if(bridgeGame.isFailure()){
             System.out.println("실패");
