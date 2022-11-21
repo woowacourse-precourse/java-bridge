a52e49475d4ab078c3999ddaeb935e8037b0801e
josolha <josolha@nate.com>
2022-11-22 03:36:58 +0900

    refactor(OutputView) : 메인 어플리케이션 try catch 예외 삭제

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -5,12 +5,9 @@ import bridge.gameController.GameController;
 public class Application {
 
     public static void main(String[] args) {
-        try {
-            GameController gameController = new GameController();
-            gameController.run();
 
-        }catch (Exception e){
+        GameController gameController = new GameController();
+        gameController.run();
 
-        }
     }
 }
