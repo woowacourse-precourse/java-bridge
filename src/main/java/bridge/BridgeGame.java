7406b0046fc5d1e67b09ede72932518145ff8c30
c4fiber <qudcjf153@gmail.com>
2022-11-22 01:29:24 +0900

    feat(BridgeGame): 게임을 몇번 시도했는지 값을 반환하는 기능 추가

--- src/main/java/bridge/BridgeGame.java
+++ src/main/java/bridge/BridgeGame.java
@@ -60,6 +60,10 @@ public class BridgeGame {
         return bridge.equals(movement);
     }
 
+    public int getTried() {
+        return tried;
+    }
+
     /**
      * 사용자가 게임을 다시 시도할 때 사용하는 메서드
      * <p>
