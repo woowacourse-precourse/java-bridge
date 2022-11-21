9431ccccabcf731639272b2da59dceaca95f94b5
jinyeong-afk <xoeh7002@naver.com>
2022-11-21 19:56:11 +0900

    test(BridgeMaker): 현재 다리 상황 저장 및 불러오기 테스트 추가 set을 통해 저장 후 get을 통해 불러와 비교하는 단위 테스트

--- src/test/java/bridge/BridgeMakerTest.java
+++ src/test/java/bridge/BridgeMakerTest.java
@@ -6,7 +6,9 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import bridge.ApplicationTest.TestNumberGenerator;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -19,5 +21,20 @@ class BridgeMakerTest {
         BridgeMaker bridgeMaker = new BridgeMaker(numberGenerator);
         bridgeMaker.makeBridge(4);
         assertThat(bridgeMaker.getBridge()).containsExactly("D", "D", "U", "U");
+    }
+
+    @DisplayName("현재 다리 상황 생성 후 저장 및 불러오기")
+    @Test
+    void 현재_다리_상황_저장_불러오기() {
+        HashMap<String, List<String>> presentBridge = new HashMap<>();
+        presentBridge.put("U", List.of(" ", " ", "O", "O"));
+        presentBridge.put("D", List.of("O", "O", " ", " "));
+        BridgeNumberGenerator bridgeNumberGenerator = new TestNumberGenerator(newArrayList(0, 0, 1, 1));
+        BridgeMaker bridgeMaker = new BridgeMaker(bridgeNumberGenerator);
+
+        bridgeMaker.initPresentBridge();
+        bridgeMaker.setPresentBridge(presentBridge);
+
+        assertThat(presentBridge).isEqualTo(bridgeMaker.getPresentBridge());
     }
 }
