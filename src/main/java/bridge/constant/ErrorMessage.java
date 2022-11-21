c895a55b32c7cc30abddb731d588819bd0c743d0
Woojin Lee <dhkep03@gmail.com>
2022-11-21 12:32:03 +0900

    refactor(ErrorMessage): [ERROR] 문구 누락 수정

--- src/main/java/bridge/constant/ErrorMessage.java
+++ src/main/java/bridge/constant/ErrorMessage.java
@@ -1,7 +1,7 @@
 package bridge.constant;
 
 public enum ErrorMessage {
-    INPUT_ONLY_NUMBER("숫자만 입력해주세요."),
+    INPUT_ONLY_NUMBER("[ERROR] 숫자만 입력해주세요."),
     WRONG_BRIDGE_SIZE("[ERROR] 다리 길이는 %d 이상 %d 이하여야 합니다."),
     WRONG_MOVING("[ERROR] 위로 이동하려면 \"%s\", 아래로 이동하려면 \"%s\"를 입력하세요."),
     WRONG_GAME_COMMAND("[ERROR] 게임을 재시작하려면 \"%s\", 종료하려면 \"%s\"를 입력하세요.");
