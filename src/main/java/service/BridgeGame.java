dfb6b6c15c85d9c499c6254d8f91755035ea7e1f
Devtist <yt930310@snu.ac.kr>
2022-11-22 00:03:43 +0900

    refactor: 서비스 레이어 리팩터링

--- src/main/java/service/BridgeGame.java
+++ src/main/java/service/BridgeGame.java
@@ -1,45 +1,69 @@
 package service;
 
+import java.util.List;
+
 import bridge.Bridge;
 import bridge.BridgeMaker;
 import bridge.BridgeRandomNumberGenerator;
 import bridge.BridgeSize;
 import bridge.GameCommand;
 import bridge.MapRenderer;
-import dto.BridgeDto;
-import dto.BridgeSizeDto;
-import dto.GameCommandDto;
-import dto.IndexDto;
-import dto.MapDto;
-import dto.MovingDto;
+import bridge.Moving;
+import bridge.MovingStack;
+import dto.BridgeResponseDto;
+import dto.BridgeSizeRequestDto;
+import dto.GameCommandRequestDto;
+import dto.MapResponseDto;
+import dto.MovingRequestDto;
 import repository.PlayCount;
 
 /**
  * 다리 건너기 게임을 관리하는 클래스
  */
 public class BridgeGame {
+	private MovingStack movingStack;
+
+	public void initMovingStack() {
+		movingStack = new MovingStack();
+	}
+
 	/**
 	 * 입력 받은 다리 길이로 다리를 생성하는 메서드
 	 */
-	public BridgeDto initBridge(BridgeSizeDto bridgeSizeDto) {
+	public BridgeResponseDto initBridge(BridgeSizeRequestDto bridgeSizeRequestDto) {
 		BridgeRandomNumberGenerator numberGenerator = new BridgeRandomNumberGenerator();
 		BridgeMaker bridgeMaker = new BridgeMaker(numberGenerator);
-		BridgeSize bridgeSize = new BridgeSize(bridgeSizeDto.getBridgeSize());
-		return new Bridge(bridgeMaker.makeBridge(bridgeSize.getBridgeSize())).toBridgeDto();
+		BridgeSize bridgeSize = new BridgeSize(bridgeSizeRequestDto.getBridgeSize());
+		List<String> bridge = bridgeMaker.makeBridge(bridgeSize.getBridgeSize());
+		return new BridgeResponseDto(new Bridge(bridge));
 	}
 
 	/**
 	 * 사용자가 칸을 이동할 때 사용하는 메서드
 	 */
-	public MapDto move(BridgeDto bridgeDto, IndexDto indexDto, MovingDto movingDto) {
-		return new MapRenderer(bridgeDto, indexDto, movingDto).toMapDto();
+	public void move(MovingRequestDto movingRequestDto) {
+		Moving moving = new Moving(movingRequestDto.getMoving());
+		movingStack.addMoving(moving.isUpPosition());
+	}
+
+	private int calculateNumberOfCorrectMoving(MapResponseDto mapResponseDto) {
+		return (int)mapResponseDto.getMap().chars().filter(cell -> cell == 'O').count();
+	}
+	public boolean isEnd(BridgeResponseDto bridgeResponseDto, MapResponseDto mapResponseDto) {
+		List<String> bridge = bridgeResponseDto.getBridge();
+		return calculateNumberOfCorrectMoving(mapResponseDto) != movingStack.getMovingCount() || bridge.equals(movingStack.getMovingStack());
+	}
+
+	public MapResponseDto renderMap(BridgeResponseDto bridgeResponseDto) {
+		List<String> bridge = bridgeResponseDto.getBridge();
+		return new MapResponseDto(new MapRenderer(bridge, movingStack.getMovingStack()));
 	}
 
 	/**
 	 * 사용자가 게임을 다시 시도할 때 사용하는 메서드
 	 */
-	public boolean retry(GameCommandDto gameCommandDto) {
-		GameCommand gameCommand = new GameCommand(gameCommandDto.getGameCommand());
+	public boolean retry(GameCommandRequestDto gameCommandRequestDto) {
+		GameCommand gameCommand = new GameCommand(gameCommandRequestDto.getGameCommand());
 		if (gameCommand.isRetry()) {
 			PlayCount.getInstance().addCount();
 			return true;
