7cf6b41d809ab3c5ed06706f35bedf5958488feb
mckim123 <minchan06@gmail.com>
2022-11-22 04:08:44 +0900

    fix: remove invalid field RANDOM_UPPER_INCLUSIVE from BridgeMaker
    Remove field RANDOM_UPPER_INCLUSIVE from BridgeMaker to satisfy requirement

--- src/main/java/bridge/BridgeMaker.java
+++ src/main/java/bridge/BridgeMaker.java
@@ -5,7 +5,6 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class BridgeMaker {
-    private static final int RANDOM_UPPER_INCLUSIVE = 1;
     private final BridgeNumberGenerator bridgeNumberGenerator;
 
     public BridgeMaker(BridgeNumberGenerator bridgeNumberGenerator) {
@@ -13,6 +12,7 @@ public class BridgeMaker {
     }
 
     public List<String> makeBridge(int size) {
+        final int RANDOM_UPPER_INCLUSIVE = 1;
         List<String> bridge = new ArrayList<>();
         for (int index = 0; index < size; index++) {
             String nextAnswer = Moving.D.toString();
