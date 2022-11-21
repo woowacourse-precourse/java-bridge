e2453b26712cc96f576fef1ad26406e53cfe0f22
eyeofsol <kingjihong@naver.com>
2022-11-22 01:59:59 +0900

    refactor(BridgeStatus): 입력 커맨드 상수화
    입력 커맨드를 상수로 분리했습니다. 하드코딩을 방지하고 이름을 의미있게 작성해
    가독성을 높일 수 있었습니다.

--- src/main/java/bridge/Controller.java
+++ src/main/java/bridge/Controller.java
@@ -5,13 +5,16 @@ import bridge.domain.BridgeGame;
 import java.util.List;
 
 public class Controller {
+    private static final String QUIT_COMMAND = "Q";
+    private static final String RETRY_COMMAND = "R";
+
     private final InputView inputView = new InputView();
     private final OutputView outputView = new OutputView();
 
     public void play() {
         BridgeGame bridgeGame = new BridgeGame(make());
         String command = "";
-        while (!bridgeGame.isEnd() && !command.equals("Q")) {
+        while (!bridgeGame.isEnd() && !command.equals(QUIT_COMMAND)) {
             move(bridgeGame);
             if (!bridgeGame.isMovableArea()) {
                 command = checkReset(bridgeGame);
@@ -24,11 +27,12 @@ public class Controller {
         String command;
 
         command = inputView.readGameCommand();
-        if (command.equals("R")) {
+        if (command.equals(RETRY_COMMAND)) {
             bridgeGame.retry();
         }
         return command;
     }
+
     public List<String> make() {
         while (true) {
             try {
