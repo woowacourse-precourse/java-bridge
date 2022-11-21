170b327766ce1307439d4129d977979c36e3a3e1
Dennis-K1 <dennis.k1.dev@gmail.com>
2022-11-21 19:58:24 +0900

    feat: Update input methods for InputView Class

--- src/main/java/bridge/InputView.java
+++ src/main/java/bridge/InputView.java
@@ -1,5 +1,7 @@
 package bridge;
 
+import static camp.nextstep.edu.missionutils.Console.readLine;
+
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
  */
@@ -9,20 +11,36 @@ public class InputView {
      * 다리의 길이를 입력받는다.
      */
     public int readBridgeSize() {
-        return 0;
+        String input  = readLine();
+        try{
+            Integer.parseInt(input);
+        } catch (IllegalArgumentException e){
+            e.printStackTrace();
+        }
+        int size = Integer.parseInt(input);
+        if (size < 3 || size > 20){
+            throw new IllegalArgumentException();
+        }
+        return size;
     }
 
     /**
      * 사용자가 이동할 칸을 입력받는다.
      */
     public String readMoving() {
-        return null;
+        String input = readLine();
+        if (!input.equals("U") || !input.equals("D")){
+            throw new IllegalArgumentException();}
+        return input;
     }
 
     /**
      * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
      */
     public String readGameCommand() {
-        return null;
+        String input = readLine();
+        if (!input.equals("R") || !input.equals("Q")){
+            throw new IllegalArgumentException();}
+        return input;
     }
 }
