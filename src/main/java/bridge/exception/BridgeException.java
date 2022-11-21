de81cd01c16331d5b69561fba715bf411054ecbe
Kim Seung Beom <88074556+daily1313@users.noreply.github.com>
2022-11-21 14:18:27 +0900

    feat : exception에 해당하는 상수 추가

---
+++ src/main/java/bridge/exception/BridgeException.java
@@ -0,0 +1,15 @@
+package bridge.exception;
+
+public class BridgeException {
+
+    private static final String NUMBER_REGEX = "/^[0-9]*$/";
+
+    private static final String ENGLISH_REGEX = "/^[A-Z]*$/";
+
+
+
+
+
+
+
+}
