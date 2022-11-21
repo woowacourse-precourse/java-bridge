829cf515cc1e768fca8f16ff0227f478a5ef8ceb
Choi Seongwon <vlflalektzja@naver.com>
2022-11-21 15:49:55 +0900

    feat: 출력용 다리 인스턴스 생성

--- src/main/java/bridge/OutputView.java
+++ src/main/java/bridge/OutputView.java
@@ -1,16 +1,24 @@
 package bridge;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * 사용자에게 게임 진행 상황과 결과를 출력하는 역할을 한다.
  */
 public class OutputView {
+    private List<String> viewBridge;
+
+    public OutputView() {
+        this.viewBridge = new ArrayList<>();
+    }
 
     /**
      * 현재까지 이동한 다리의 상태를 정해진 형식에 맞춰 출력한다.
      * <p>
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void printMap() {
+    public void printMap(List<String> tempBridge) {
     }
 
     /**
