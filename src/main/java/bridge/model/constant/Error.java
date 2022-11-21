a2dd9f7a165289e99d937bccfe4cd5d64380c1b0
junpakPark <112045553+junpakPark@users.noreply.github.com>
2022-11-21 14:01:22 +0900

    feat(Error): EXISTENCE 및 ABSENCE enum 추가

--- src/main/java/bridge/model/constant/Error.java
+++ src/main/java/bridge/model/constant/Error.java
@@ -10,6 +10,8 @@ public enum Error {
 	STAIRS_COMMAND(String.format("%s 혹은 %s만 입력해주세요.", Stairs.UP.getCommand(), Stairs.DOWN.getCommand())),
 	STAIRS_NUMBER(String.format("%d 혹은 %d만 입력해주세요.", Stairs.UP.getNumber(), Stairs.DOWN.getNumber())),
 	EXIT_OPTION(String.format("%s 혹은 %s만 입력해주세요.", ExitOption.REPLAY.getCommand(), ExitOption.QUIT.getCommand())),
+	EXISTENCE("이미 다리가 존재하는 경우, 다시 만들 수 없습니다."),
+	ABSENCE("만들어진 다리가 없습니다. makeBridge 메서드를 사용해주세요."),
 	;
 
 	private static final String ERROR = "[ERROR] ";
