1049b13123af8ca7b8e9dc28e583da576aadc42c
Dennis-K1 <dennis.k1.dev@gmail.com>
2022-11-22 00:23:15 +0900

    feat: Update MVP prototype

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -1,8 +1,82 @@
 package bridge;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Application {
 
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        InputView inputView = new InputView();
+        BridgeNumberGenerator bridgeNumberGenerator = new BridgeRandomNumberGenerator();
+        BridgeMaker bridgeMaker = new BridgeMaker(bridgeNumberGenerator);
+        List<String> bridge = bridgeMaker.makeBridge(inputView.readBridgeSize());
+        System.out.println(bridge);
+        BridgeGame bridgeGame = new BridgeGame();
+
+
+        OutputView outputView = new OutputView();
+        // 0성공, 1실패
+        // move의 리턴이 0이라면 게임 move 계속 진행
+        int status = 0;
+        int stage = 1;
+        int trial = 1;
+        int success = 0;
+        while (status == 0){
+            if (stage == bridge.size()+1){
+                success = 1;
+                break;
+            }
+            status = bridgeGame.move(bridge, stage);
+            outputView.printMap(bridge, stage, status);
+            stage++;
+            if (status == 1){
+                System.out.println();
+                outputView.printGameOver();
+                if (inputView.readGameCommand().equals("R")){
+                    stage = 1;
+                    status = 0;
+                    trial++;
+                }
+            }
+        }
+        System.out.println();
+        outputView.printResult();
+        outputView.printMap(bridge, bridge.size(), 0);
+
+        System.out.println();
+        outputView.printSuccess(success);
+        outputView.printTrialAmount(trial);
+
+//        testPrintMap();
+    }
+    public static void testPrintMap(){
+        List<String> bridge =  new ArrayList<>(Arrays.asList("U","D","D"));
+        List<String> upper = new ArrayList<>(Arrays.asList("["));
+        for (String direction: bridge){
+            if (direction.equals("U")){
+                upper.add(" O ");
+                upper.add("|");
+            } else {
+                upper.add("   ");
+                upper.add("|");
+            }
+        }
+        upper.set(upper.size()-1,"]");
+        System.out.println(upper);
+        for (String i : upper){
+            System.out.print(i);
+        }
+        System.out.println();
+
+
+
+        int stage = 2;
+        int stageIndex = stage * 2;
+        upper.set(stageIndex, "]");
+        for (int i=0; i<=stageIndex;i++){
+            System.out.print(upper.get(i));
+        }
     }
 }
--- src/main/java/bridge/BridgeGame.java
+++ src/main/java/bridge/BridgeGame.java
@@ -1,9 +1,19 @@
 package bridge;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 /**
  * 다리 건너기 게임을 관리하는 클래스
  */
 public class BridgeGame {
+
+
+    private  InputView inputView = new InputView();
+    private  OutputView outputView = new OutputView();
+
+    private int trial = 1;
 
 
     /**
@@ -11,7 +21,13 @@ public class BridgeGame {
      * <p>
      * 이동을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void move() {
+    public int move(List<String> bridge, int stage) {
+        //0이면 성공 1이면 실패
+        String direction = inputView.readMoving();
+        if (!bridge.get(stage-1).equals(direction)){
+            return 1;
+        }
+        return 0;
     }
 
     /**
@@ -22,6 +38,36 @@ public class BridgeGame {
     public void retry() {
     }
 
+
+    public List<String> createUpperBridge(List<String> bridge){
+        List<String> upperBridge = new ArrayList<>(Arrays.asList("["));
+        for (String direction: bridge){
+            if (direction.equals("U")){
+                upperBridge.add(" O ");
+                upperBridge.add("|");
+            } else {
+                upperBridge.add("   ");
+                upperBridge.add("|");
+            }
+        }
+        upperBridge.set(upperBridge.size()-1,"]");
+        return upperBridge;
+    }
+
+    public List<String> createBottomBridge(List<String> bridge){
+        List<String> bottomBridge = new ArrayList<>(Arrays.asList("["));
+        for (String direction: bridge){
+            if (direction.equals("D")){
+                bottomBridge.add(" O ");
+                bottomBridge.add("|");
+            } else {
+                bottomBridge.add("   ");
+                bottomBridge.add("|");
+            }
+        }
+        bottomBridge.set(bottomBridge.size()-1,"]");
+        return bottomBridge;
+    }
     public void gameOver(){
 
     }
