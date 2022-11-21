977d835268ab61ba01d68a8c7b1489cba2ec69fb
wath1457 <wath1457@gmail.com>
2022-11-22 03:31:04 +0900

    test(OutputViewTest): 맵 출력 테스트 추가

--- src/test/java/bridge/OutputViewTest.java
+++ src/test/java/bridge/OutputViewTest.java
@@ -2,14 +2,21 @@ package bridge;
 
 import org.junit.jupiter.api.Test;
 
+import java.io.ByteArrayOutputStream;
+import java.io.OutputStream;
+import java.io.PrintStream;
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.*;
 
 class OutputViewTest {
     @Test
     void printMapTest() {
+        OutputStream out = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(out));
+
         OutputView outputView = new OutputView();
-        outputView.printMap(List.of("U","D","D"), List.of("D", "D","D"));
+        outputView.printMap(List.of("U", "D", "D"), List.of("U", "U", "D"));
+        assertThat(out.toString()).contains("[ O | X |   ]\n[   |   | O ]");
     }
 }
