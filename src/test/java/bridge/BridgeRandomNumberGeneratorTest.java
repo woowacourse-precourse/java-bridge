8e8d6624a0f9faec8ed98bc4227fe418c2533ca1
Woojin Lee <dhkep03@gmail.com>
2022-11-21 23:04:27 +0900

    test(BridgeRandomNumberGererator): 사용하지 않는 import 삭제 & 메서드명 변경

--- src/test/java/bridge/BridgeRandomNumberGeneratorTest.java
+++ src/test/java/bridge/BridgeRandomNumberGeneratorTest.java
@@ -1,12 +1,7 @@
 package bridge;
 
-import static bridge.constant.GameStatus.END;
-import static bridge.constant.Moving.LOWER_SIDE;
-import static bridge.constant.Moving.UPPER_SIDE;
 import static org.assertj.core.api.Assertions.assertThat;
 
-import bridge.constant.GameStatus;
-import bridge.domain.BridgeMover;
 import java.util.List;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -15,7 +10,7 @@ class BridgeRandomNumberGeneratorTest {
 
     @DisplayName("0과 1 중 랜덤한 숫자를 생성한다.")
     @Test
-    void should_ReturnEnd_When_ArriveAtDestination() {
+    void should_ReturnZeroOrOne_When_Generate() {
         // given
         BridgeRandomNumberGenerator bridgeRandomNumberGenerator = new BridgeRandomNumberGenerator();
         // when
