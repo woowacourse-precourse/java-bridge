fa348307709a95b80aee5bc85afcd48e38dfa7d7
Yohan <sprae114@naver.com>
2022-11-22 06:54:59 +0900

    feat : 출력 구현

---
+++ src/main/java/bridge/view/OutputView.java
@@ -0,0 +1,26 @@
+package bridge.view;
+
+import bridge.domain.Bridge;
+
+import static bridge.GameController.GAME_COUNT;
+
+
+public class OutputView {
+    private static final String FINAL_GAME_RESULT = "최종 게임 결과";
+    private static final String GAME_SUCCESS = "게임 성공 여부: %s\n";
+    private static final String GAME_TOTAL_COUNT = "총 시도한 횟수: %d";
+
+    public void printMap(Bridge bridge) {
+        System.out.println("[ " + String.join(" | ", bridge.resultUpBridge) + " ]");
+        System.out.println("[ " + String.join(" | ", bridge.resultDownBridge) + " ]");
+    }
+
+
+    public void printResult(String successFail, Bridge bridge) {
+        System.out.println(FINAL_GAME_RESULT);
+        System.out.println("[ " + String.join(" | ", bridge.resultUpBridge) + " ]");
+        System.out.println("[ " + String.join(" | ", bridge.resultDownBridge) + " ]");
+        System.out.printf(GAME_SUCCESS, successFail);
+        System.out.printf(GAME_TOTAL_COUNT, GAME_COUNT);
+    }
+}
