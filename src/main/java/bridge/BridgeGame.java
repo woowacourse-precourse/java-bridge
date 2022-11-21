f7afa40a6bb14682b0ef039134cfe07e4406a52b
Donguk <95donguk.dev@gmail.com>
2022-11-22 01:48:50 +0900

    feat: 게임 재시도를 명령하면 진행 사항 초기화 기능 구현

--- src/main/java/bridge/BridgeGame.java
+++ src/main/java/bridge/BridgeGame.java
@@ -11,6 +11,8 @@ public class BridgeGame {
     private final List<String> movingData = new ArrayList<>();
     private final List<Boolean> movingResults = new ArrayList<>();
 
+    private int totalAttempts = 1;
+
     /**
      * 사용자가 칸을 이동할 때 사용하는 메서드
      * <p>
@@ -34,6 +36,9 @@ public class BridgeGame {
      * 재시작을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
     public void retry() {
+        movingData.clear();
+        movingResults.clear();
+        totalAttempts++;
     }
 
     public String findMovingByIndex(int index) {
