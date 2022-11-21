e84b42c4de80783f2c342f078bfb22f6c2f3ef77
Gyu-Bin-Yoon <gyubinyoon99@gmail.com>
2022-11-22 00:34:18 +0900

    feat(OutputView) : add functions that print messages

--- src/main/java/bridge/OutputView.java
+++ src/main/java/bridge/OutputView.java
@@ -5,6 +5,17 @@ package bridge;
  */
 public class OutputView {
 
+    public void printStartMessage(){
+        System.out.println("다리 건너기 게임을 시작합니다.");
+    }
+
+    public void printBridgeSizeMessage(){
+        System.out.println("다리의 길이를 입력해주세요.");
+    }
+
+    public void printMovingMessage(){
+        System.out.println("이동할 칸을 선택해주세요. (위: U, 아래: D)");
+    }
     /**
      * 현재까지 이동한 다리의 상태를 정해진 형식에 맞춰 출력한다.
      * <p>
