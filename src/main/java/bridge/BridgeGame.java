368ba75890177495cfb44838ee29b0d85552555c
WonSubSub <boxer1995@naver.com>
2022-11-21 21:11:37 +0900

    feat(main): 플레이어의 이동과 관련된 기능 추가

--- src/main/java/bridge/BridgeGame.java
+++ src/main/java/bridge/BridgeGame.java
@@ -1,16 +1,53 @@
 package bridge;
 
+import java.util.List;
+
 /**
  * 다리 건너기 게임을 관리하는 클래스
  */
 public class BridgeGame {
+
+    BridgeRandomNumberGenerator bridgeRandomNumberGenerator = new BridgeRandomNumberGenerator();
+
+    InputView inputView = new InputView();
+    OutputView outputView = new OutputView();
+    BridgeMaker bridgeMaker = new BridgeMaker(bridgeRandomNumberGenerator);
+    private final int size;
+    private final List<String> bridge;
+    boolean wrongMovement = true;
+
+    public BridgeGame() {
+        this.size = getSize();
+        this.bridge = getBridge();
+    }
+
+    public int getSize() {
+        return inputView.readBridgeSize();
+    }
+
+    public List<String> getBridge() {
+        return bridgeMaker.makeBridge(size);
+    }
 
     /**
      * 사용자가 칸을 이동할 때 사용하는 메서드
      * <p>
      * 이동을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void move() {
+    public boolean move() {
+        int count = 0;
+        while (bridge.get(count).equals(inputView.readMoving())) {
+            count += 1;
+            checkCount(count);
+            if (count == size) {break;}
+        }
+        return wrongMovement;
+    }
+
+    public void checkCount(int count) {
+        if (count == size) {
+            wrongMovement = false;
+        }
     }
 
     /**
