cb57bfadf2d8ee2fd0f2ecda0461972ae07920f4
heesang <dlagmltkd12@naver.com>
2022-11-22 06:29:26 +0900

    test: BridgeMakerTest 중복 테스트 제거

--- src/test/java/bridge/BridgeMakerTest.java
+++ src/test/java/bridge/BridgeMakerTest.java
@@ -11,27 +11,12 @@ import static org.assertj.core.util.Lists.newArrayList;
 
 class BridgeMakerTest {
 
-    @DisplayName("다리의 크기가 주어지고, 1만 주어질 때 U로만 이루어진 다리를 반환")
-    @Test
-    void makeBridgeGiveOne() {
-        BridgeMaker bridgeMaker = new BridgeMaker(() -> 1);
-        List<String> bridge = bridgeMaker.makeBridge(6);
-        assertThat(bridge).containsExactly("U", "U", "U", "U", "U", "U");
-    }
-
-    @DisplayName("다리의 크기가 주어지고, 0만 주어질 때 D로만 이루어진 다리를 반환")
-    @Test
-    void makeBridgeGiveZero() {
-        BridgeMaker bridgeMaker = new BridgeMaker(() -> 0);
-        List<String> bridge = bridgeMaker.makeBridge(6);
-        assertThat(bridge).containsExactly("D", "D", "D", "D", "D", "D");
-    }
-
-    @DisplayName("1, 0이 주어질 때 D, U로 이루어진 다리를 반환")
+    @DisplayName("1, 0 값들이 주어질 때 D, U로 이루어진 다리를 반환")
     @Test
     void makeBridge() {
         BridgeNumberGenerator generator = new TestBridgeNumberGenerator(newArrayList(1, 0, 0, 1, 0, 1));
         BridgeMaker bridgeMaker = new BridgeMaker(generator);
+
         List<String> bridge = bridgeMaker.makeBridge(6);
         assertThat(bridge).containsExactly("U", "D", "D", "U", "D", "U");
     }
