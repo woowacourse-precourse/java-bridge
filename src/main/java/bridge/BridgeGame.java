a00e93b420253a3cfcbb211916ec3cea1656534d
고동우 <shp05179@gmail.com>
2022-11-21 19:46:25 +0900

    feat: BridgeGame 기능 추가
    init : 인풋으로 다리의 정보를 받아 저장합니다.
    move : 현위치에서 한 칸 앞으로 가고, 그 값이 사용자가 움직인 값이랑 같으면 true 다르면 false를 리턴합니다.
    retry : 다리정보를 제외한 필드를 초기화합니다.
    getCrossedBridge : 현재까지 건넌 다리 정보를 리턴합니다.
    getTries : 현재까지 시도한 횟수를 리턴합니다.

--- src/main/java/bridge/BridgeGame.java
+++ src/main/java/bridge/BridgeGame.java
@@ -1,16 +1,42 @@
 package bridge;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * 다리 건너기 게임을 관리하는 클래스
  */
 public class BridgeGame {
+    private List<String> bridge;
+    private static List<String> crossedBridge;
+    private static int tries;
+
+    public BridgeGame(List<String> bridge) {
+        this.bridge = bridge;
+        this.crossedBridge = new ArrayList<>();
+        this.tries = 0;
+    }
 
     /**
      * 사용자가 칸을 이동할 때 사용하는 메서드
      * <p>
      * 이동을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void move() {
+    public boolean move(String direction) {
+        crossedBridge.add(direction);
+        if (bridge.get(crossedBridge.size()).equals(direction)) {
+            return true;
+        }
+        tries += 1;
+        return false;
+    }
+
+    public static List<String> getCrossedBridge() {
+        return crossedBridge;
+    }
+
+    public static int getTries() {
+        return tries;
     }
 
     /**
@@ -19,5 +45,7 @@ public class BridgeGame {
      * 재시작을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
     public void retry() {
+        this.crossedBridge = new ArrayList<>();
+        this.tries = 0;
     }
 }
