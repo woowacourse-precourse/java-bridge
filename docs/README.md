6749a62d7c7dae7b9c93e446780877bae12d0581
osm <osm0822@naver.com>
2022-11-22 02:26:47 +0900

    feat(InputView) : 다리 길이 입력, 검증 구현

--- docs/README.md
+++ docs/README.md
@@ -25,7 +25,7 @@
 - [ ] InputView : 사용자로부터 입력을 받는 클래스
     - [x] 생성자 추가
       - InputValidator 설정
-    - [ ] 다리 길이 입력, 검증
+    - [x] 다리 길이 입력, 검증
       - 검증 실패 시 IllegalArgumentException
     - [ ] 이동할 칸 입력, 검증
       - 검증 실패 시 IllegalArgumentException
--- src/main/java/bridge/InputView.java
+++ src/main/java/bridge/InputView.java
@@ -1,5 +1,8 @@
 package bridge;
 
+import bridge.constant.InputValidationError;
+import camp.nextstep.edu.missionutils.Console;
+
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
  */
@@ -14,8 +17,16 @@ public class InputView {
     /**
      * 다리의 길이를 입력받는다.
      */
-    public int readBridgeSize() {
-        return 0;
+    public int readBridgeSize() throws IllegalArgumentException {
+        String cmd = Console.readLine();
+        int size;
+        try {
+            size = Integer.parseInt(cmd);
+        } catch (NumberFormatException e) {
+            throw InputValidationError.ERROR_BRIDGE_SIZE.exception;
+        }
+        validator.validateBridgeSize(size);
+        return size;
     }
 
     /**
