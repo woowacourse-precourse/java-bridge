a0da06579b104a53d6f26b4baa32f45de337be34
comet7001 <comet7001@gmail.com>
2022-11-22 01:43:12 +0900

    chore(NormalStatements): 일관성을 위해 출력 구문 변경

--- src/main/java/Statements/NormalStatements.java
+++ src/main/java/Statements/NormalStatements.java
@@ -3,7 +3,7 @@ package Statements;
 public enum NormalStatements {
     GAME_START ("다리 건너기 게임을 시작합니다."),
     ASK_BRIDGE_LENGTH("다리의 길이를 입력해주세요."),
-    ASK_TILE_TO_MOVE_ON("이동할 칸을 선택해주세요. (위: U, 아래: D)"),
+    ASK_BRIDGE_TO_MOVE_ON("이동할 칸을 선택해주세요. (위: U, 아래: D)"),
     ASK_RETRY("게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)"),
     DECLARE_RESULT("최종 게임 결과"),
     RESULT_SUCCESS("게임 성공 여부: 성공"),
