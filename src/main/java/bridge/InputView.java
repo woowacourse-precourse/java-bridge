d1dbf955db55acf3e3310d56e9cc62be3d81899d
jagajang <kosb7544@gmail.com>
2022-11-21 14:20:58 +0900

    refactor: seperate scan-parse and validate

--- src/main/java/bridge/InputView.java
+++ src/main/java/bridge/InputView.java
@@ -11,66 +11,54 @@ public class InputView {
      * 다리의 길이를 입력받는다.
      */
     public int readBridgeSize() {
-        String line;
-        int size;
+        int size = readInteger();
+        if(size >= 3 && size <= 20)
+            return size;
 
-        try {
-            line = Console.readLine();
-            size = Integer.parseInt(line);
-        } catch(Exception e) {
-            throw new IllegalArgumentException("정수를 입력해야 합니다.");
-        }
-
-        if(size < 3 || size > 20) {
-            throw new IllegalArgumentException("다리 길이는 3부터 20 사이의 숫자여야 합니다.");
-        }
-
-        return size;
+        throw new IllegalArgumentException("다리 길이는 3부터 20 사이의 숫자여야 합니다.")
     }
 
     /**
      * 사용자가 이동할 칸을 입력받는다.
      */
     public String readMoving() {
-        String line = "";
+        String moving = readOneCharacter();
+        if(moving.equals("U") || moving.equals("D"))
+            return moving;
 
-        try {
-            line = Console.readLine();
-        } catch (Exception e) {
-            throw new IllegalArgumentException("입력 과정에서 오류가 발생했습니다.");
-        }
-
-        if(line.length() != 1) {
-            throw new IllegalArgumentException("한 글자만 입력해야 합니다.");
-        }
-
-        if(line.equals("U") || line.equals("D")) {
-            return line;
-        }
-
-        throw new IllegalArgumentException("U나 D를 입력해야 합니다.");
+        throw new IllegalArgumentException("U 또는 D를 입력해야 합니다.");
     }
 
     /**
      * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
      */
     public String readGameCommand() {
-        String line;
+        String moving = readOneCharacter();
+        if(moving.equals("R") || moving.equals("Q"))
+            return moving;
 
+        throw new IllegalArgumentException("R 또는 Q를 입력해야 합니다.");
+    }
+
+    private int readInteger() {
+        try {
+            int number = Integer.parseInt(Console.readLine());
+            return number;
+        } catch(Exception e) {
+            throw new IllegalArgumentException("정수를 입력해야 합니다.");
+        }
+    }
+
+    private String readOneCharacter() {
+        String line;
         try {
             line = Console.readLine();
         } catch (Exception e) {
             throw new IllegalArgumentException("입력 과정에서 오류가 발생했습니다.");
         }
-
-        if(line.length() != 1) {
-            throw new IllegalArgumentException("한 글자만 입력해야 합니다.");
+        if(line.length() != 0) {
+            throw new IllegalArgumentException("알파벳 하나를 입력해야 합니다.");
         }
-
-        if(line.equals("Q") || line.equals("R")) {
-            return line;
-        }
-
-        throw new IllegalArgumentException("Q나 R을 입력해야 합니다.");
+        return line;
     }
 }
