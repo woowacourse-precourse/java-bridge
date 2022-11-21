92471ac378da41e570f10e769592e31c5c0b9f25
Kim Seung Beom <88074556+daily1313@users.noreply.github.com>
2022-11-21 14:23:19 +0900

    refactor : 자동 정렬 후 재커밋

--- src/main/java/bridge/exception/BridgeException.java
+++ src/main/java/bridge/exception/BridgeException.java
@@ -22,37 +22,34 @@ public class BridgeException {
     }
 
     public void checkBridgeLength(int number) {
-        if(number < 3 || number > 20) {
+        if (number < 3 || number > 20) {
             throw new IllegalArgumentException("3~20 사이의 숫자를 입력해주세요. ");
         }
     }
 
     public void checkBridgeLengthAboutOnlyNumber(String number) {
-        if(!number.matches(NUMBER_REGEX)) {
+        if (!number.matches(NUMBER_REGEX)) {
             throw new IllegalArgumentException("다리 길이는 숫자로만 입력해주세요. ");
         }
     }
 
     public void checkBridgeMoveInputAlphabet(String movement) {
-        if(!movement.equals("U") && !movement.equals("D")) {
+        if (!movement.equals("U") && !movement.equals("D")) {
             throw new IllegalArgumentException("U, D 중에 하나를 입력해주세요. ");
         }
     }
 
     public void checkBridgeGameOptionInputAlphabet(String gameOption) {
-        if(!gameOption.equals("R") && !gameOption.equals("Q")) {
+        if (!gameOption.equals("R") && !gameOption.equals("Q")) {
             throw new IllegalArgumentException("R, Q 중에 하나를 입력해주세요. ");
         }
     }
 
     public void checkBridgeInputAlphabetOnlyEnglish(String input) {
-        if(!input.matches(ENGLISH_REGEX)) {
+        if (!input.matches(ENGLISH_REGEX)) {
             throw new IllegalArgumentException("알파벳으로만 입력해주세요. ");
         }
     }
-
-
-
 
 
 }
