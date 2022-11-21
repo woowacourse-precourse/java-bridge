2136ee080e5781c0c644957e4957ff8abd67385b
limjae <limjae96@gmail.com>
2022-11-21 21:58:35 +0900

    style: import 정리

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -2,9 +2,6 @@ package bridge;
 
 import bridge.scenario.BasicBridgeScenario;
 import bridge.scenario.BridgeScenario;
-import bridge.view.input.InputView;
-import bridge.view.output.OutputView;
-import java.util.List;
 
 public class Application {
 
