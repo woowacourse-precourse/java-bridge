5429376a8f9a66aedd3fbd22671442797d87735e
qkdk <86948395+qkdk@users.noreply.github.com>
2022-11-22 02:04:05 +0900

    feat: 입력기능 구현

--- src/main/java/bridge/InputView.java
+++ src/main/java/bridge/InputView.java
@@ -1,28 +1,33 @@
 package bridge;
 
+import camp.nextstep.edu.missionutils.Console;
+
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
+ * 시그니쳐와 반환타입 변경가능
+ * 매서드 추가 가능
+ * 패키지 변경 가능
  */
 public class InputView {
 
     /**
      * 다리의 길이를 입력받는다.
      */
-    public int readBridgeSize() {
-        return 0;
+    public static int readBridgeSize() {
+        return Integer.parseInt(Console.readLine());
     }
 
     /**
      * 사용자가 이동할 칸을 입력받는다.
      */
-    public String readMoving() {
-        return null;
+    public static String readMoving() {
+        return Console.readLine();
     }
 
     /**
      * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
      */
-    public String readGameCommand() {
-        return null;
+    public static String readGameCommand() {
+        return Console.readLine();
     }
 }
