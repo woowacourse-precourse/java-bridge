b307550eee785cb58ae32fa9ed19ee006b34a019
DESKTOP-1ANNTGI\USER <cvc100326@naver.com>
2022-11-21 23:32:59 +0900

    refactor: GameController 리펙터링
    - 필요없는 인스턴스 변수 삭제
    - import문 순서 변경
    - 접근제어자 변경
    - 메서드 이름 변경

--- src/main/java/bridge/controller/GameController.java
+++ src/main/java/bridge/controller/GameController.java
@@ -1,50 +1,45 @@
 package bridge.controller;
 
-import bridge.domain.Bridge;
-import bridge.domain.BridgeGame;
+import java.util.List;
+
 import bridge.BridgeMaker;
 import bridge.BridgeRandomNumberGenerator;
+import bridge.domain.Bridge;
+import bridge.domain.BridgeGame;
 import bridge.domain.MapMaker;
 import bridge.view.InputView;
 import bridge.view.OutputView;
 
-
-import java.util.List;
-
 public class GameController {
     InputView inputView = new InputView();
     OutputView outputView = new OutputView();
-    BridgeRandomNumberGenerator bridgeRandomNumberGenerator = new BridgeRandomNumberGenerator();
-    BridgeMaker bridgeMaker = new BridgeMaker(bridgeRandomNumberGenerator);
     MapMaker mapMaker = new MapMaker();
     BridgeGame bridgeGame;
-
     boolean hasMoved;
     boolean hasRetried;
-    boolean hasWin;
 
-
-    private void makeBridgeByInput() {
+    private void setUpGame() {
+        BridgeMaker bridgeMaker = new BridgeMaker(new BridgeRandomNumberGenerator());
         try {
-            bridgeGame = new BridgeGame(new Bridge(bridgeMaker.makeBridge(inputView.readBridgeSize())));
+            Bridge bridge = new Bridge(bridgeMaker.makeBridge(inputView.readBridgeSize()));
+            bridgeGame = new BridgeGame(bridge);
         } catch (IllegalArgumentException e) {
             outputView.printErrorNotice(e);
-            makeBridgeByInput();
+            setUpGame();
         }
     }
 
-
-    public void start() {
+    private void start() {
         outputView.printStartNotice();
         outputView.printBridgeSizeInputNotice();
-        makeBridgeByInput();
+        setUpGame();
     }
 
-    public void play() {
+    private void play() {
         do {
             outputView.printMovingDirectionInputNotice();
             hasMoved = moveByInput();
-            outputView.printMap(mapMaker.makeMap(bridgeGame.getPlayerData().getMovementHistory(), hasMoved));
+            outputView.printMap(mapMaker.makeMap(bridgeGame.getPlayerData().getMovementRecord(), hasMoved));
             if (bridgeGame.checkWin()) {
                 break;
             }
@@ -62,7 +57,7 @@ public class GameController {
     }
 
 
-    public void reTry() {
+    private void reTry() {
         outputView.printRestartOrQuitInputNotice();
         hasRetried = retryByInput();
     }
@@ -77,8 +72,8 @@ public class GameController {
         }
     }
 
-    public void end() {
-        List<String> map = mapMaker.makeMap(bridgeGame.getPlayerData().getMovementHistory(), hasMoved);
+    private void end() {
+        List<String> map = mapMaker.makeMap(bridgeGame.getPlayerData().getMovementRecord(), hasMoved);
         outputView.printResult(map, bridgeGame.checkWin(), bridgeGame.getPlayerData().getCountOfTry());
     }
 
