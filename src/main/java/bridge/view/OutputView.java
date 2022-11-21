711aa523e1d5c7b03f6fac15df23962f8173374a
Sangin <ksiisk99@gmail.com>
2022-11-21 18:29:12 +0900

    refactor: 출력 문구 상수로 분리

--- src/main/java/bridge/view/OutputView.java
+++ src/main/java/bridge/view/OutputView.java
@@ -1,16 +1,21 @@
 package bridge.view;
 
+import bridge.dto.PathTravel;
+
+import java.util.List;
+
 /**
  * 사용자에게 게임 진행 상황과 결과를 출력하는 역할을 한다.
  */
 public class OutputView {
+    private final String GAME_START = "다리 건너기 게임을 시작합니다.";
 
     /**
      * 현재까지 이동한 다리의 상태를 정해진 형식에 맞춰 출력한다.
      * <p>
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void printMap() {
+    public void printMap(PathTravel pathTravel) {
     }
 
     /**
@@ -22,7 +27,7 @@ public class OutputView {
     }
 
     public void printGameStart() {
-        System.out.println("다리 건너기 게임을 시작합니다.");
+        System.out.println(GAME_START);
     }
 
     public void printError(String message) {
