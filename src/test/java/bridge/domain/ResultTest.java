3059bfcbb15ce13ab5496c24263413fde2bc9338
Miseong Kim <miseongk@naver.com>
2022-11-22 01:10:54 +0900

    test(Result): isSuccess is true after update isSuccess

--- src/test/java/bridge/domain/ResultTest.java
+++ src/test/java/bridge/domain/ResultTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class ResultTest {
 
@@ -22,14 +23,23 @@ class ResultTest {
         result.update("X", "U");
         assertThat(result.getUp().get(0)).isEqualTo("O");
         assertThat(result.getUp().get(1)).isEqualTo("X");
-        assertThat(result.getDown().size()).isEqualTo(0);
+        assertThat(result.getDown().get(0)).isEqualTo(" ");
+        assertThat(result.getDown().get(1)).isEqualTo(" ");
+        assertThat(result.getUp().size()).isEqualTo(2);
     }
 
     @DisplayName("시도 횟수 증가 확인")
     @Test
     void updateNumberOfTrial() {
         result.updateNumberOfTrial();
-        assertThat(result.getNumberOfTrial()).isEqualTo(1);
+        assertThat(result.getNumberOfTrial()).isEqualTo(2);
+    }
+
+    @DisplayName("게임 결과 업데이트 확인")
+    @Test
+    void updateIsSuccess() {
+        result.updateIsSuccess();
+        assertTrue(result.getIsSuccess());
     }
 
     @DisplayName("up과 down 리스트 초기화")
@@ -37,8 +47,8 @@ class ResultTest {
     void reInitialList() {
         result.update("O", "U");
         result.update("O", "D");
-        assertThat(result.getUp().size()).isEqualTo(1);
-        assertThat(result.getDown().size()).isEqualTo(1);
+        assertThat(result.getUp().size()).isEqualTo(2);
+        assertThat(result.getDown().size()).isEqualTo(2);
         result.reInit();
         assertThat(result.getUp().size()).isEqualTo(0);
         assertThat(result.getDown().size()).isEqualTo(0);
