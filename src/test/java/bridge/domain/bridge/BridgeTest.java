f76d4c2a37e611f01a70765694622d10b2dced4e
5uhwann <5uhwann27@gmail.com>
2022-11-22 00:09:13 +0900

    refactor(검증): Bridge, USER_LOCATION 전역상수화

--- src/test/java/bridge/domain/bridge/BridgeTest.java
+++ src/test/java/bridge/domain/bridge/BridgeTest.java
@@ -8,16 +8,17 @@ import org.junit.jupiter.api.Test;
 
 class BridgeTest {
 
+    private static final Bridge bridge = new Bridge(List.of("D", "U", "U"));
+    private static final int USER_LOCATION = 2;
+
     @Test
     @DisplayName("플레이어가 선택한 이동칸이 다리모양과 일치하면 건널 수 있다.")
     void isPassableTest1() {
         //given
-        Bridge bridge = new Bridge(List.of("D", "U", "U"));
-        int uerLocation = 2;
         String userSelection = "U";
 
         //when
-        boolean passableResult = bridge.isPassable(uerLocation, userSelection);
+        boolean passableResult = bridge.isPassable(USER_LOCATION, userSelection);
 
         //then
         assertThat(passableResult).isEqualTo(true);
@@ -28,12 +29,10 @@ class BridgeTest {
     @DisplayName("플레이어가 선택한 이동칸이 다리모양과 일치하지 않으면 건널 수 없다.")
     void isPassableTest2() {
         //given
-        Bridge bridge = new Bridge(List.of("D", "U", "U"));
-        int uerLocation = 2;
         String userSelection = "D";
 
         //when
-        boolean passableResult = bridge.isPassable(uerLocation, userSelection);
+        boolean passableResult = bridge.isPassable(USER_LOCATION, userSelection);
 
         //then
         assertThat(passableResult).isEqualTo(false);
