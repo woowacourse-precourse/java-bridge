868e800e49c204e61c1cbe6e5633ee964c6afa91
Danden1 <goh1211@ajou.ac.kr>
2022-11-21 23:07:20 +0900

    feat : main 함수 작성

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -3,6 +3,11 @@ package bridge;
 public class Application {
 
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        try {
+            GameController gameController = new GameController();
+            gameController.startGame();
+        }catch (CloneNotSupportedException e){
+            System.out.println("clone error");
+        }
     }
 }
