1553f3756e2f5d6a2b1590c460e29b652f4d2c53
hojunking96 <junsong96@naver.com>
2022-11-22 00:42:34 +0900

    refactor(BridgeMakerTest.java) : 코드 정리

--- src/test/java/bridge/BridgeMakerTest.java
+++ src/test/java/bridge/BridgeMakerTest.java
@@ -13,10 +13,23 @@ class BridgeMakerTest {
     @DisplayName("다리를 생성하는 지 확인한다.")
     @Test
     void makeBridge_테스트() {
-        BridgeNumberGenerator numberGenerator
-                = new ApplicationTest.TestNumberGenerator(newArrayList(0, 1, 1, 0, 1));
+        BridgeNumberGenerator numberGenerator = new TestNumberGenerator(newArrayList(0, 1, 1, 0, 1));
         BridgeMaker bridgeMaker = new BridgeMaker(numberGenerator);
         List<String> bridge = bridgeMaker.makeBridge(5);
         assertThat(bridge).containsExactly("D", "U", "U", "D", "U");
+    }
+
+    static class TestNumberGenerator implements BridgeNumberGenerator {
+
+        private final List<Integer> numbers;
+
+        TestNumberGenerator(List<Integer> numbers) {
+            this.numbers = numbers;
+        }
+
+        @Override
+        public int generate() {
+            return numbers.remove(0);
+        }
     }
 }
