9b3ea2607b7eb47260597b5ea7fb6d31d77e856d
KTH-96 <taehyun960@gmail.com>
2022-11-21 23:17:54 +0900

    fix(MoveCommand): validCommand 조건을 or 에서 and로 변경

--- src/main/java/bridge/domain/MoveCommand.java
+++ src/main/java/bridge/domain/MoveCommand.java
@@ -6,13 +6,14 @@ import static bridge.utils.Constant.WRONG_INSERT_POSITION_MESSAGE;
 public class MoveCommand {
 
     private final String command;
+
     public MoveCommand(String command) {
         validCommand(command);
         this.command = command;
     }
 
     private void validCommand(String command) {
-        if (!command.equals(BRIDGE_TOP.getHardBridgePosition()) ||
+        if (!command.equals(BRIDGE_TOP.getHardBridgePosition()) &&
                 !command.equals(BRIDGE_BOTTOM.getHardBridgePosition())) {
             throw new IllegalArgumentException(WRONG_INSERT_POSITION_MESSAGE);
         }
