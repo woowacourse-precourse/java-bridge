76994d507bdb7a06795cd0393e410301e1be75f0
starwook <star.wookk@gmail.com>
2022-11-21 20:28:24 +0900

    refactor(Bridge.java): Add validate method
    브리지의 다리길이를 검증하는 메소드 추가

--- src/main/java/bridge/Bridge.java
+++ src/main/java/bridge/Bridge.java
@@ -7,15 +7,26 @@ public class Bridge {
     private int bridgeSize;
 
     public void bridge(String bridgeSize){
-
+        validate(bridgeSize);
     }
     private void validate(String bridgeSize){
-
+        int bridgeSizeNumber = stringToNumber(bridgeSize);
+        validateBridgeSize(bridgeSizeNumber);
+        this.bridgeSize = bridgeSizeNumber;
     }
     private void validateBridgeSize(int bridgeSize){
         if(bridgeSize>MAX_BRIDGE_SIZE||bridgeSize<MIN_BRIDGE_SIZE){
             throw new IllegalArgumentException(ErrorResource.ERROR_START+ ErrorResource.BRIDGE_SIZE_FRONT+
                     MIN_BRIDGE_SIZE+ErrorResource.TO+MAX_BRIDGE_SIZE+ErrorResource.INPUT_SHOULD_BACK);
+        }
+    }
+    private int stringToNumber(String bridgeSize){
+        try{
+            return Integer.parseInt(bridgeSize);
+        }
+        catch(Exception e){
+            throw new IllegalArgumentException(ErrorResource.ERROR_START+ErrorResource.BRIDGE_SIZE_FRONT+
+                    ErrorResource.NOT_NUMBER);
         }
     }
 }
