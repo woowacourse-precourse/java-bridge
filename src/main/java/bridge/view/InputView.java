013ed3c3a6b0c7cc9b6279718264b66fc78a6a57
yujaemin <woawoa0513@gmail.com>
2022-11-21 14:05:27 +0900

    feat: 이동할 칸 입력 함수 구현
    사용자에게서 이동할 칸을 입력받아 유효성 검사 후 반환한다.

--- src/main/java/bridge/view/InputView.java
+++ src/main/java/bridge/view/InputView.java
@@ -2,6 +2,7 @@ package bridge.view;
 
 import static camp.nextstep.edu.missionutils.Console.readLine;
 import static bridge.utils.Validation.validateBridgeSize;
+import static bridge.utils.Validation.validateMoving;
 
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
@@ -21,7 +22,9 @@ public class InputView {
      * 사용자가 이동할 칸을 입력받는다.
      */
     public String readMoving() {
-        return null;
+        String input = readLine();
+        validateMoving(input);
+        return input;
     }
 
     /**
