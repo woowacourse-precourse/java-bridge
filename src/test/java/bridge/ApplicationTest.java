82453beff2e334fb3041d27756919157a56f5db4
Sehbeom <j3beom@gmail.com>
2022-11-22 03:18:25 +0900

    refactor(test): ApplicationTest 수정
    각 테스트 메소드 실행 전 static 필드 초기화 과정 추가

--- src/test/java/bridge/ApplicationTest.java
+++ src/test/java/bridge/ApplicationTest.java
@@ -5,8 +5,13 @@ import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.util.Lists.newArrayList;
 
+import bridge.domain.model.Bridge;
+import bridge.domain.model.GameResultInformation;
 import camp.nextstep.edu.missionutils.test.NsTest;
+import java.lang.reflect.Field;
+import java.util.ArrayList;
 import java.util.List;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class ApplicationTest extends NsTest {
@@ -26,11 +31,11 @@ class ApplicationTest extends NsTest {
         assertRandomNumberInRangeTest(() -> {
             run("3", "U", "D", "U");
             assertThat(output()).contains(
-                "최종 게임 결과",
-                "[ O |   | O ]",
-                "[   | O |   ]",
-                "게임 성공 여부: 성공",
-                "총 시도한 횟수: 1"
+                    "최종 게임 결과",
+                    "[ O |   | O ]",
+                    "[   | O |   ]",
+                    "게임 성공 여부: 성공",
+                    "총 시도한 횟수: 1"
             );
 
             int upSideIndex = output().indexOf("[ O |   | O ]");
@@ -64,5 +69,39 @@ class ApplicationTest extends NsTest {
         public int generate() {
             return numbers.remove(0);
         }
+    }
+
+    @BeforeEach
+    void resetAll() throws NoSuchFieldException, IllegalAccessException {
+        resetBridge();
+        resetBridgeCreated();
+        resetCountOfRetry();
+    }
+
+    void resetBridge() throws NoSuchFieldException, IllegalAccessException {
+        Bridge bridgeInstance = new Bridge();
+        Field bridge = Bridge.class.getDeclaredField("bridge");
+
+        bridge.setAccessible(true);
+        bridge.set(bridgeInstance, new ArrayList<>());
+        bridge.setAccessible(false);
+    }
+
+    void resetBridgeCreated() throws NoSuchFieldException, IllegalAccessException {
+        Bridge bridgeInstance = new Bridge();
+        Field bridgeCreated = Bridge.class.getDeclaredField("bridgeCreated");
+
+        bridgeCreated.setAccessible(true);
+        bridgeCreated.set(bridgeInstance, false);
+        bridgeCreated.setAccessible(false);
+    }
+
+    void resetCountOfRetry() throws NoSuchFieldException, IllegalAccessException {
+        GameResultInformation gameResultInformation = new GameResultInformation();
+        Field countOfRetry = GameResultInformation.class.getDeclaredField("countOfTry");
+
+        countOfRetry.setAccessible(true);
+        countOfRetry.set(gameResultInformation, 1);
+        countOfRetry.setAccessible(false);
     }
 }
