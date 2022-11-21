15f27953e07fa1e65b05c036f53781b8e7a74d28
Kim-Jong-Gyu <whdrb2643@gmail.com>
2022-11-21 22:41:03 +0900

    feat(bridge): 입력값 이동 저장 기능 추가
    입력값 이동 저장 기능 추가:
    - BridgeGame 클래스 안 move 함수에 User의 movingrout를 getter와 setter를 이용해 구현

--- src/main/java/controller/BridgeGame.java
+++ src/main/java/controller/BridgeGame.java
@@ -1,16 +1,24 @@
 package controller;
 
+import model.User;
+
 /**
  * 다리 건너기 게임을 관리하는 클래스
  */
 public class BridgeGame {
 
+    private Util util;
+    private User user;
     /**
      * 사용자가 칸을 이동할 때 사용하는 메서드
      * <p>
      * 이동을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
     public void move() {
+        util = new Util();
+        user = new User();
+        String moving = util.inputMoving();
+        user.setMovingRoute(user.getMovingRoute(), moving);
     }
 
     /**
--- src/main/java/model/User.java
+++ src/main/java/model/User.java
@@ -7,13 +7,14 @@ import static controller.Util.validateMoving;
 public class User {
 
     private List<String> movingRoute;
+
+    public List<String> getMovingRoute() {
+        return movingRoute;
+    }
+
     public void setMovingRoute(List<String> movingRoute,String moving) {
         validateMoving(moving);
         movingRoute.add(moving);
         this.movingRoute = movingRoute;
     }
-    public List<String> getMovingRoute() {
-        return movingRoute;
-    }
-
 }
