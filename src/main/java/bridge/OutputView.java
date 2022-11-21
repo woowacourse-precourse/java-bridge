7b03700054521551d12d4ef7290c7306167f8464
DaeyeonKim97 <kdysy1130@khu.ac.kr>
2022-11-21 12:45:32 +0900

    feat(output-view): printMap method
    additional implements
    - printLine

--- src/main/java/bridge/OutputView.java
+++ src/main/java/bridge/OutputView.java
@@ -1,5 +1,9 @@
 package bridge;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
 /**
  * 사용자에게 게임 진행 상황과 결과를 출력하는 역할을 한다.
  */
@@ -10,7 +14,19 @@ public class OutputView {
      * <p>
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void printMap() {
+    public void printMap(Map<String,List<String>> resultMap) {
+        List<String> upperLine = resultMap.get("upperLine");
+        List<String> bottomLine = resultMap.get("bottomLine");
+        printLine(upperLine);
+        printLine(bottomLine);
+        System.out.println();
+    }
+
+    private void printLine(List<String> line){
+        String result = "[ ";
+        result += String.join(" | ",line);
+        result += " ]";
+        System.out.println(result);
     }
 
     /**
