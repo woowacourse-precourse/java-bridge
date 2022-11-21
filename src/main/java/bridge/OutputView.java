777a18ef7f6fb25378886daf52ee1591f8b4eaf6
jamangstangs <jamangsong@gmail.com>
2022-11-21 15:25:29 +0900

    feat(OutputView):  printChoosingMovingDirection() -> 이동할 칸을 선택하라는 문구를 출력한다.

--- src/main/java/bridge/OutputView.java
+++ src/main/java/bridge/OutputView.java
@@ -9,6 +9,10 @@ public class OutputView {
         System.out.println("다리 건너기 게임을 시작합니다.\n" + "\n" + "다리의 길이를 입력해주세요.");
     }
 
+    public void printChoosingMovingDirection() {
+        System.out.println("이동할 칸을 선택해주세요. (위: U, 아래: D)");
+    }
+
     /**
      * 현재까지 이동한 다리의 상태를 정해진 형식에 맞춰 출력한다.
      *<p>
