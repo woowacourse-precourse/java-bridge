f2264039cfd2d4778b9fdc3865102077fee8f4b6
leorb159 <ldgsocial159@naver.com>
2022-11-22 03:32:21 +0900

    feat(InputView): readGameCommand 구현

--- src/main/java/bridge/InputView.java
+++ src/main/java/bridge/InputView.java
@@ -35,14 +35,15 @@ public class InputView {
     public String readMoving() {
         System.out.println("이동할 칸을 선택해주세요. (위: U, 아래: D)");
         String input = Console.readLine();
-
         return input;
     }
-    
+
     /**
      * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
      */
     public String readGameCommand() {
-        return null;
+        System.out.println("게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)");
+        String input = Console.readLine();
+        return input;
     }
 }
