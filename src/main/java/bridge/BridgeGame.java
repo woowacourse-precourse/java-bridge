4824e97468d9326aa031e5283e0602d25bfe2254
jihun-24k <gi020477@gmail.com>
2022-11-21 22:21:53 +0900

    Feat(BridgeGame): 생성자로 InputView, OutputView 객체 생성

--- src/main/java/bridge/BridgeGame.java
+++ src/main/java/bridge/BridgeGame.java
@@ -5,6 +5,11 @@ public class BridgeGame {
     private final InputView inputView;
     private final OutputView outputView;
 
+    public BridgeGame() {
+        this.inputView = new InputView();
+        this.outputView = new OutputView();
+    }
+
     public void play() {
         System.out.println("게임 시작");
     }
