1f1df0f7721be2dff988517bcce388682c2e2047
Nitroblack1 <109022435+Nitroblack1@users.noreply.github.com>
2022-11-22 00:19:42 +0900

    refactor(StandardTools) : delete unused constants

--- src/main/java/bridge/Constants/StandardTools.java
+++ src/main/java/bridge/Constants/StandardTools.java
@@ -8,8 +8,6 @@ public class StandardTools {
     public static final String BRIDGE_HEAD = "[";
     public static final boolean FAILED = false;
     public static final boolean SUCCEED = true;
-    public static final boolean POSSIBLE_ZONE = true;
-    public static final boolean IMPOSSIBLE_ZONE = false;
 
     public enum retry {
         RETRY, QUIT
