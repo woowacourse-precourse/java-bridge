f3e8ae3fe0aad358218cf43bf985ee201c694736
ONE <sodlapdlf13@gmail.com>
2022-11-21 20:25:00 +0900

    feat(BridgeGame): 재시도 입력 문구 출력 기능 추가

--- src/main/java/bridge/view/OutputView.java
+++ src/main/java/bridge/view/OutputView.java
@@ -33,6 +33,13 @@ public class OutputView {
     }
 
     /**
+     * 재시작 여부 입력 문구를 출력하는 메소드
+     */
+    public static void printRestartOptionInput() {
+        System.out.println("게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)");
+    }
+
+    /**
      * 현재까지 이동한 다리의 상태를 정해진 형식에 맞춰 출력한다.
      * <p>
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
