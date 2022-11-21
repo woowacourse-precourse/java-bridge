b43d7dbe03abafbd0175ac4d0ab214379f598ebb
gwj0421 <gwj0421@gmail.com>
2022-11-21 18:33:02 +0900

    feat: 사용자가 입력한 값("U"혹은 "D")과 컴퓨터가 랜덤으로 선택한 값("U"혹은 "D")와 비교하여 같으면 문자 그대로("U"혹은 "D") 입력되고 같지 않으면 앞에 fail이 붙은 채로("fail U"혹은 "fail D")로 입력되는 함수 추가
    상황1 - 랜덤한 다리값과 사용자가 입력한 값이 같을 경우
    ==> 리스트에 "U" 혹은 "D"로 입력됨
    상황2 -  랜덤한 다리값과 사용자가 입력한 값이 다를 경우
    ==> 리스트에 "fail U"혹은 "fail D"로 입력됨

--- src/main/java/bridge/BridgeGame.java
+++ src/main/java/bridge/BridgeGame.java
@@ -19,7 +19,30 @@ public class BridgeGame {
      * <p>
      * 이동을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void move() {
+
+    public boolean checkMove(String OpenWay, String nextStep) {
+        if (OpenWay.equals(nextStep)) {
+            return true;
+        }
+        return false;
+    }
+
+    public void moveWrongWay(List<String> playerBridgeMap, String OpenWay) {
+        if (OpenWay.equals("U")) {
+            playerBridgeMap.add("fail D");
+        }
+        if (OpenWay.equals("D")) {
+            playerBridgeMap.add("fail U");
+        }
+    }
+
+    public void move(List<String> playerBridgeMap, List<String> RandomBridgeMap, int idx, String playerStep) {
+        String OpenWay = RandomBridgeMap.get(idx);
+        if (checkMove(OpenWay, playerStep)) {
+            playerBridgeMap.add(OpenWay);
+            return;
+        }
+        moveWrongWay(playerBridgeMap, OpenWay);
     }
 
     /**
