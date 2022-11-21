b6d007e084e9ff387553691ac2a67319b3e22513
zaysverse <zeze@jbnu.ac.kr>
2022-11-21 21:58:52 +0900

    feat(bridge): 플레이어가 이동할 칸을 선택한다

--- src/main/java/bridge/BridgeGame.java
+++ src/main/java/bridge/BridgeGame.java
@@ -7,13 +7,14 @@ import java.util.List;
  * 다리 건너기 게임을 관리하는 클래스
  */
 public class BridgeGame {
-    private List<String> bridge = new ArrayList<>();
+    private List<String> computer, user= new ArrayList<>();
     InputView inputView = new InputView();
+    OutputView outputView = new OutputView();
 
     public BridgeGame() {
         System.out.println("다리 건너기 게임을 시작합니다.");
         BridgeMaker bridgeMaker = new BridgeMaker(new BridgeRandomNumberGenerator());
-        bridge= bridgeMaker.makeBridge(inputView.readBridgeSize());
+        computer= bridgeMaker.makeBridge(inputView.readBridgeSize());
     }
 
     /**
@@ -22,6 +23,7 @@ public class BridgeGame {
      * 이동을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
     public void move() {
+        user.add(inputView.readMoving());
     }
 
     /**
--- src/main/java/bridge/BridgeMaker.java
+++ src/main/java/bridge/BridgeMaker.java
@@ -19,14 +19,14 @@ public class BridgeMaker {
      * @return 입력받은 길이에 해당하는 다리 모양. 위 칸이면 "U", 아래 칸이면 "D"로 표현해야 한다.
      */
     public List<String> makeBridge(int size) {
-        List<String> list = new ArrayList<>();
+        List<String> bridge = new ArrayList<>();
         for (int i = 0; i < size; i++) {
             if (bridgeNumberGenerator.generate() == 1) {
-                list.add("U");
+                bridge.add("U");
                 continue;
             }
-            list.add("D");
+            bridge.add("D");
         }
-        return list;
+        return bridge;
     }
 }
