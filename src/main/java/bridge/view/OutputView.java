8ff633547beadd606fdf732ef1ca05b00b720549
desfox <desfox99@naver.com>
2022-11-21 22:35:28 +0900

    refactor(OutputView): 맵 만드는 로직 테스트하기 위해 getter 구현

--- src/main/java/bridge/view/OutputView.java
+++ src/main/java/bridge/view/OutputView.java
@@ -6,8 +6,16 @@ import java.util.List;
  * 사용자에게 게임 진행 상황과 결과를 출력하는 역할을 한다.
  */
 public class OutputView {
-    private String lowerMap;
     private String upperMap;
+    private String lowerMap;
+
+    public String getUpperMap() {
+        return upperMap;
+    }
+
+    public String getLowerMap() {
+        return lowerMap;
+    }
 
     /**
      * 현재까지 이동한 다리의 상태를 정해진 형식에 맞춰 출력한다.
