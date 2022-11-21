0c51e40cac86c2f0292404a0a50ccdd7a3daf905
JONGMIN SHIN <jongmin373@gmail.com>
2022-11-21 17:01:27 +0900

    Feat<GameStarter> : add Constructor

--- src/main/java/bridge/GameStarter.java
+++ src/main/java/bridge/GameStarter.java
@@ -4,6 +4,15 @@ import java.util.List;
 
 public class GameStarter {
     public static InputView inputView = new InputView();
+    public static OutputView outputView = new OutputView();
+
+    public GameStarter() {
+        List<String> bridge = makeBridge();
+        int totalCount = 1;
+        while (startGame(bridge, totalCount)) {
+            totalCount++;
+        }
+    }
 
     // 다리 생성 메서드
     public static List<String> makeBridge() {
