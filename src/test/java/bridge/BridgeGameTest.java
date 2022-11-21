4f99dcc434c48dc4ab5b83a2325081ff33d564b2
Seiyon Park <seiyonpdev@gmail.com>
2022-11-21 10:02:31 +0900

    test(BridgeGameTest): 테스트 BridgeGameTest 항목 추가
    메소드 checkAnswer, updateMap에 대한 테스트를 작성했다

--- src/test/java/bridge/BridgeGameTest.java
+++ src/test/java/bridge/BridgeGameTest.java
@@ -10,13 +10,35 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 class BridgeGameTest {
     BridgeGame bridgeGame = new BridgeGame();
+    List<String> bridge = Arrays.asList("U","U","D");
 
     @DisplayName("메소드 move 테스트")
     @Test
     void move() {
-        List<String> bridge = Arrays.asList("U","D","U");
         bridgeGame.move(bridge,"U");
         assertThat(bridgeGame.getPlayerLocationIndex()).isEqualTo(0);
+    }
+
+    @DisplayName("메소드 checkAnswer해서 맞았을 경우 테스트")
+    @Test
+    void checkAnswer_right() {
+        bridgeGame.move(bridge,"U");
+        assertThat(bridgeGame.getUpMap().get(bridgeGame.getPlayerLocationIndex())).isEqualTo("O");
+    }
+
+    @DisplayName("메소드 checkAnswer해서 틀렸을 경우 테스트")
+    @Test
+    void checkAnswer_wrong() {
+        bridgeGame.move(bridge,"D");
+        assertThat(bridgeGame.getDownMap().get(bridgeGame.getPlayerLocationIndex())).isEqualTo("X");
+    }
+
+    @DisplayName("메소드 updateMap 테스트")
+    @Test
+    void updateMap() {
+        bridgeGame.move(bridge,"U");
+        bridgeGame.move(bridge,"U");
+        assertThat(bridgeGame.getUpMap().size()).isEqualTo(2);
     }
 
 
