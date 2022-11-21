9a2047d98742631c3a0bae534ca0b408521bcad3
KSB-skyBlueLover <gogotkdqja@yonsei.ac.kr>
2022-11-21 23:07:20 +0900

    feat: implement the function that reads a bridge size

---
+++ src/main/java/bridge/ERROR.java
@@ -0,0 +1,5 @@
+package bridge;
+
+public class ERROR {
+    static final String BRIDGE_SIZE = "[ERROR] 다리 길이는 3부터 20 사이의 숫자여야 합니다.";
+}
--- src/main/java/bridge/InputView.java
+++ src/main/java/bridge/InputView.java
@@ -1,15 +1,26 @@
 package bridge;
 
+import camp.nextstep.edu.missionutils.Console;
+
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
  */
 public class InputView {
+    private static final String readBridgeSizeMsg = "다리의 길이를 입력해주세요.";
+    private static final String readMovingMsg = "이동할 칸을 선택해주세요. (위: U, 아래: D)";
+    private static final String readGameCommandMsg = "게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)";
 
     /**
      * 다리의 길이를 입력받는다.
      */
     public int readBridgeSize() {
-        return 0;
+        System.out.println(readBridgeSizeMsg);
+        try {
+            return Integer.parseInt(Console.readLine());
+        } catch (NumberFormatException e) {
+            System.out.println(ERROR.BRIDGE_SIZE);
+            return readBridgeSize();
+        }
     }
 
     /**
