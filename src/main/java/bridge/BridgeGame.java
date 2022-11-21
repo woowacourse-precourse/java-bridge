35c0e42fd5f8a23f9ac9b794ae0835d8319885c0
skydreamer21 <skydreamer210@gmail.com>
2022-11-21 11:29:42 +0900

    feat(BridgeGame): 필드, 생성자, getter 구현

--- src/main/java/bridge/BridgeGame.java
+++ src/main/java/bridge/BridgeGame.java
@@ -1,9 +1,20 @@
 package bridge;
 
+import bridge.domain.Bridge;
+
 /**
  * 다리 건너기 게임을 관리하는 클래스
  */
 public class BridgeGame {
+    private int attemptCount;
+    private int order;
+    private Bridge bridge;
+
+    public BridgeGame (Bridge bridge) {
+        attemptCount = 0;
+        order = 1;
+        this.bridge = bridge;
+    }
 
     /**
      * 사용자가 칸을 이동할 때 사용하는 메서드
@@ -19,5 +30,11 @@ public class BridgeGame {
      * 재시작을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
     public void retry() {
+    public int getAttemptCount() {
+        return attemptCount;
+    }
+
+    public int getOrder() {
+        return order;
     }
 }
