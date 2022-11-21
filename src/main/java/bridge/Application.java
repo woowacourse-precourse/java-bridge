fb9309917471bde37752ee6d29e1d2393a23e0b0
noh eul <papooa5140@gmail.com>
2022-11-21 20:16:38 +0900

    feat: Application Class 기능 구현

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -4,5 +4,11 @@ public class Application {
 
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        BridgeGame game = new BridgeGame();
+        try {
+            game.moveBridgeGame();
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+        }
     }
 }
