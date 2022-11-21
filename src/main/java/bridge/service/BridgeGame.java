05f6dfd054217e3a25e66eabbd1bd17efe2548cc
qkrdmswl <eunji303@naver.com>
2022-11-22 03:35:20 +0900

    refactor: operate bridge game

--- src/main/java/bridge/service/BridgeGame.java
+++ src/main/java/bridge/service/BridgeGame.java
@@ -1,27 +1,57 @@
-package bridge;
+package bridge.service;
 
-import bridge.view.InputView;
+import bridge.service.impl.CheckService;
+import bridge.service.impl.ResultService;
+import bridge.type.ResultType;
+import bridge.view.OutputView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * 다리 건너기 게임을 관리하는 클래스
  */
 public class BridgeGame {
-    InputView inputView;
 
-    /**
-     * 사용자가 칸을 이동할 때 사용하는 메서드
-     * <p>
-     * 이동을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
-     */
-    public void move() {
-        inputView.readMoving();
+    static final BridgeDrawer bridgeDrawer = new BridgeDrawer();
+    static final CheckService checkService = new CheckServiceImpl();
+    public static final List<String> userDirection = new ArrayList<>();
+
+
+    public boolean move(List<String> bridgeInfo, String direction) {
+        saveUserDirection(direction);
+        bridgeDrawer.makePrintList(direction, bridgeInfo);
+        drawMap();
+        return checkService.checkCrossable(bridgeInfo, direction);
     }
 
-    /**
-     * 사용자가 게임을 다시 시도할 때 사용하는 메서드
-     * <p>
-     * 재시작을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
-     */
-    public void retry() {
+
+    public boolean retry() {
+        userDirection.clear();
+        bridgeDrawer.initializeLists();
+        return true;
     }
+
+    public static void drawMap() {
+        OutputView.printMap(bridgeDrawer.getDirectionTypeLists());
+    }
+
+    public static void drawFinalResult(ResultType resultType, int tryCnt) {
+        OutputView.printMap(bridgeDrawer.getDirectionTypeLists());
+        OutputView.printResult(resultType, tryCnt);
+    }
+
+
+    public void saveUserDirection(String direction) {
+        userDirection.add(direction);
+    }
+
+    public static List<String> getUserDirection() {
+        return userDirection;
+    }
+
+    public static int getUserDirectionSize() {
+        return userDirection.size();
+    }
+
 }
