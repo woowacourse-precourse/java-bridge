8e0d8aefbdadbdebd9f992f419ad60c80e012e4c
Seokhyun Lee <lseokhyun99@gmail.com>
2022-11-21 14:01:21 +0900

    feat(InputTry.java): 이동할 칸 입력 시도 메소드 구현
    InputTry 클래스에
    이동할 칸 입력 시도 메소드
    readMovingTry() 를 구현하였다.
    
    try-catch 구문을 이용하여
    입력된 값이 형식에 맞지 않아 오류 발생 시
    올바른 값을 입력할 때 까지 다시 입력 받는다.

--- src/main/java/bridge/InputTry.java
+++ src/main/java/bridge/InputTry.java
@@ -15,4 +15,16 @@ public class InputTry {
         }
         return bridgeSize;
     }
+    public static String readMovingTry(){
+        String moving = null;
+        while ( moving == null ) {
+            try {
+                moving = input.readMoving();
+            } catch (Exception e) {
+                System.out.println(e.getMessage());
+                moving = null;
+            }
+        }
+        return moving;
+    }
 }
