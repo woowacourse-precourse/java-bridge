bbf7e527ce3dccda07fd4eedb36f2f99781c94b4
swonny <wonny921@gmail.com>
2022-11-21 23:56:52 +0900

    refactor(Map): 중복되는 메소드 제거

--- src/main/java/bridge/Map.java
+++ src/main/java/bridge/Map.java
@@ -15,11 +15,6 @@ public class Map {
         bridgeUserInterface.add(new ArrayList<>());
     }
 
-    private void add(Side side, String isSucceed) {
-        addSymbolToMap(side, isSucceed);
-        addSymbolToMap(Side.getOppositePosition(side), UserInterfaceSymbol.getBlank());
-    }
-
     private void addSymbolToMap(Side side, String symbol) {
         this.bridgeUserInterface.get(Side.getIndex(side))
                 .add(symbol);
@@ -29,13 +24,9 @@ public class Map {
         return Collections.unmodifiableList(bridgeUserInterface);
     }
 
-    public void add(Side side, boolean moved) {
-        String isSucceed = UserInterfaceSymbol.get(UserInterfaceSymbol.AVAILABLE_TO_MOVE);
-        if (!moved) {
-            isSucceed = UserInterfaceSymbol.get(UserInterfaceSymbol.UNAVAILABLE_TO_MOVE);
-            side = Side.getOppositePosition(side);
-        }
-        add(side, isSucceed);
+    public void add(Side playerMovedSide, boolean movable) {
+        addSymbolToMap(playerMovedSide, UserInterfaceSymbol.getMovableSymbol(movable));
+        addSymbolToMap(Side.getOppositePosition(playerMovedSide), UserInterfaceSymbol.getBlank());
     }
 
     public void initialize() {
