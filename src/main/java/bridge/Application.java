2a52121d9af4da88f89a1b9ce51320f5c4bba1e9
Geun Woo(Kay) Park <64457922+parklaus1078@users.noreply.github.com>
2022-11-21 14:55:16 +0900

    [ADD] 다리 게임 - 다리 게임을 진행하는 기능을 추가

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -10,6 +10,7 @@ public class Application {
     public static void main(String[] args) {
         System.out.printf(Constants.Messages.START.getMessage());
         BridgeGame bridgeGame = createBridge();
+        OutputView printer = bridgeGameUI(bridgeGame);
 
     }
 
@@ -32,5 +33,40 @@ public class Application {
                 System.out.println(e);
             }
         }
+    }
+
+    // 이동 선택 UI : 부적절한 문자가 입력될 시, 다시 입력을 받을 준비
+    private static String moveChoiceUI() {
+        System.out.printf(Constants.Messages.MOVE.getMessage());
+        while (true) {
+            try {
+                return receiver.readMoving();
+            } catch (IllegalArgumentException e) {
+                System.out.println(e);
+            }
+        }
+    }
+
+    // 다리 게임 한판
+    private static OutputView bridgeGameUI(BridgeGame bridgeGame) {
+        OutputView printer = new OutputView();
+        for (int i = 0; i < bridgeGame.getBridge().size(); i++) {
+            if (!bridgeGame.move(moveChoiceUI(), i)) {
+                printBadMove(bridgeGame.getBridge(), i, printer);
+                break;
+            }
+            printGoodMove(bridgeGame.getBridge(), i, printer);
+        }
+        return printer;
+    }
+
+    private static void printBadMove(List<String> bridge, int index, OutputView printer) {
+        printer.mapBuilder(false, index, bridge);
+        printer.printMap();
+    }
+
+    private static void printGoodMove(List<String> bridge, int index, OutputView printer) {
+        printer.mapBuilder(true, index, bridge);
+        printer.printMap();
     }
 }
