e4041a35ee8bd97f8c6839687be049d711776bb8
dooboocookie <bkh3030@gmail.com>
2022-11-21 18:19:20 +0900

    test: 시도횟수 올리는 테스트

--- src/test/java/bridge/model/TryCountTest.java
+++ src/test/java/bridge/model/TryCountTest.java
@@ -1,4 +1,23 @@
 package bridge.model;
 
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 public class TryCountTest {
+
+    @DisplayName("매번 시도를 할 때 마다 시도 횟수가 올라가는 테스트")
+    @Test
+    void retryCountTest() {
+        TryCount tryCount = new TryCount();
+
+        assertThat(tryCount.getTryCount()).isEqualTo(1);
+
+        for (int count = 2; count <= 10; count++) {
+            tryCount.retry();
+            assertThat(tryCount.getTryCount()).isEqualTo(count);
+        }
+    }
 }
