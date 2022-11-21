a358b47b64fb1cf830c2b08ed4429a0facaa616e
leorb159 <ldgsocial159@naver.com>
2022-11-22 05:20:47 +0900

    feat(BridgeGame): retry 구현

--- src/main/java/bridge/BridgeGame.java
+++ src/main/java/bridge/BridgeGame.java
@@ -23,7 +23,7 @@ public class BridgeGame {
         this.dashBoard.add(bridgeDown); //0
         this.dashBoard.add(bridgeUp); //1
         this.count =0;
-        this.numberOfTimes =0;
+        this.numberOfTimes =1;
     }
     /**
      * 사용자가 칸을 이동할 때 사용하는 메서드
@@ -62,7 +62,15 @@ public class BridgeGame {
      * 재시작을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
     public void retry() {
-
-
+        numberOfTimes +=1;
+        //초기화
+        int size = this.bridgeUp.size();
+        for(int i=0; i<size; i++){
+            this.bridgeUp.add(" ");
+            this.bridgeDown.add(" ");
+        }
+        this.dashBoard.add(bridgeDown); //0
+        this.dashBoard.add(bridgeUp); //1
+        this.count =0;
     }
 }
