1a9fb60413f8fd97924b2a8d13ef10290dac273d
dydtmd6801 <dydtmd6801@naver.com>
2022-11-21 16:19:11 +0900

    feat(PlayGame): 지금까지 만든 기능을 이용하여 컨트롤러 기능
    PlayGame: 게임 플레이 단계
    1. turn을 0, 마지막 turn을 다리의 크기로 세팅
    2. 플레이어가 이동할 칸을 입력받음
    3. 플레이어가 이동할 칸과 건널 수 있는 칸을 비교
    4. 비교후 결과값을 저장
    5. turn을 하나 증가
    6. 2-5번까지를 마지막 turn까지 반복
    7. 게임이 종료되면 결과를 출력

--- src/main/java/controller/PlayGame.java
+++ src/main/java/controller/PlayGame.java
@@ -1,4 +1,30 @@
 package controller;
 
+import model.BridgeGame;
+import view.InputView;
+import view.OutputView;
+
+import java.util.List;
+
 public class PlayGame {
+    private List<String> crossAble;
+    private final InputView inputView = new InputView();
+    private final BridgeGame bridgeGame = new BridgeGame();
+    private final OutputView outputView = new OutputView();
+
+    public PlayGame(List<String> crossAble) {
+        this.crossAble = crossAble;
+    }
+
+    public void playGame() {
+        int turn = 0;
+        int endTurn = inputView.setBridgeSize();
+        while(turn < endTurn) {
+            String move = inputView.setMoving();
+            Boolean matchResult = crossAble.get(turn).equals(move);
+            outputView.store(matchResult, move, turn);
+            turn++;
+        }
+        outputView.printMap(endTurn);
+    }
 }
