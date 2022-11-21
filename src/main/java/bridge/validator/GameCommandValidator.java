e01be59b5a7043c311d3d9b5dc743919e436692e
wltks98 <wltks98@hanmail.net>
2022-11-21 09:14:01 +0900

    style : 인수명 board -> command 변경

--- src/main/java/bridge/validator/GameCommandValidator.java
+++ src/main/java/bridge/validator/GameCommandValidator.java
@@ -5,10 +5,10 @@ import bridge.exception.ErrorMessage;
 
 public class GameCommandValidator {
 
-	public static String validate(String board) {
-		if (!board.equals(GameRule.RETRY_COMMAND) && !board.equals(GameRule.QUIT_COMMAND)) {
+	public static String validate(String command) {
+		if (!command.equals(GameRule.RETRY_COMMAND) && !command.equals(GameRule.QUIT_COMMAND)) {
 			throw new IllegalArgumentException(ErrorMessage.GAME_COMMAND_ERROR.getMessage());
 		}
-		return board;
+		return command;
 	}
 }
