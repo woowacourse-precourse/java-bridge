63fc788a6f1346110957b01a27d6f81bc3bc7e2f
Choi Seongwon <vlflalektzja@naver.com>
2022-11-21 15:53:11 +0900

    feat: 이동할 칸 입력 안내문구 추가

--- src/main/java/bridge/InputView.java
+++ src/main/java/bridge/InputView.java
@@ -32,6 +32,7 @@ public class InputView {
      * 사용자가 이동할 칸을 입력받는다.
      */
     public String readMoving() throws IllegalArgumentException{
+        System.out.println("이동할 칸을 선택해주세요. (위: U, 아래: D)");
         String moving = Console.readLine();
         validateMoving(moving);
         return moving;
