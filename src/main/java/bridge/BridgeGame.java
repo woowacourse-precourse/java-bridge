0ef7ff10a40b55736eea26ecd934ff827f1d4fda
JJ <jeongjae1996@naver.com>
2022-11-21 12:24:10 +0900

    feat: 게임 재시도 시, 도전 횟수를 세고 map을 초기화하도록 기능 추가

--- src/main/java/bridge/BridgeGame.java
+++ src/main/java/bridge/BridgeGame.java
@@ -11,16 +11,19 @@ import java.util.List;
  */
 public class BridgeGame {
     private static final int START_POINT = 0;
+    private static final int TRY_COUNT = 1;
 
     private final List<String> bridge;
     private String userPath;
     private int userPosition;
+    private int tryCount;
 
     public BridgeGame(int bridgeSize) {
         BridgeMaker bridgeMaker = new BridgeMaker(new BridgeRandomNumberGenerator());
         this.bridge = bridgeMaker.makeBridge(bridgeSize);
         this.userPath = null;
         this.userPosition = START_POINT;
+        this.tryCount = TRY_COUNT;
     }
     /**
      * 사용자가 칸을 이동할 때 사용하는 메서드
@@ -37,8 +40,10 @@ public class BridgeGame {
      * <p>
      * 재시작을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void retry() {
+    public void retry(Map map) {
         this.userPosition = START_POINT;
+        this.tryCount += TRY_COUNT;
+        map.initializeMap();
     }
 
     public boolean isCorrectPath() {
