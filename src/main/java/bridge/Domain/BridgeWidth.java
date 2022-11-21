d61ba589e44e203e5b4687ea95d588411462fcfc
ljh <dusk5588@naver.com>
2022-11-21 23:17:58 +0900

    feat(BridgeWidth) : 다리의 폭 번호와 문자를 enum으로 연결하여 관리하는 로직 구현

--- src/main/java/bridge/Domain/BridgeWidth.java
+++ src/main/java/bridge/Domain/BridgeWidth.java
@@ -1,14 +1,33 @@
 package bridge.Domain;
 
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 public enum BridgeWidth {
     WIDTH1(1, "U"),
     WIDTH2(0, "D");
 
     private int number;
-    private String widthPosition;
+    private String rowPosition;
 
-    private BridgeWidth(int number, String widthPosition){
+    private BridgeWidth(int number, String rowPosition){
         this.number = number;
-        this.widthPosition = widthPosition;
+        this.rowPosition = rowPosition;
+    }
+
+    public int getNumber(){
+        return this.number;
+    }
+
+    public String getRowPosition(){
+        return this.rowPosition;
+    }
+
+    private static final Map<Integer, String> map =
+            Stream.of(values()).collect(Collectors.toMap(BridgeWidth::getNumber, BridgeWidth::getRowPosition));
+
+    private static final String getRowUsingNumber(int number){
+        return map.get(number);
     }
 }
