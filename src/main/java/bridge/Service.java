29ae4026f708f6364b68bfc30544dc556cc391bc
ingpyo <juninpyo72@gmail.com>
2022-11-21 21:42:31 +0900

    feat:입력된 값이 예외처리가되면 에러 메세지 출력 후 다시 입력받는 기능

---
+++ src/main/java/bridge/Service.java
@@ -0,0 +1,39 @@
+package bridge;
+
+public class Service {
+	private InputView inputView = new InputView();
+	private OutputView outputView = new OutputView();
+	private BridgeGame bridgeGame = new BridgeGame();
+	private MessageQuestionPrint messageQuestionPrint = new MessageQuestionPrint();
+	private BridgeSize bridgeSize;
+
+	private String checkUserInputMove() {
+		try {
+			messageQuestionPrint.userMoveQuestion();
+			return inputView.readMoving();
+		} catch (IllegalArgumentException e) {
+			System.out.println(e.getMessage());
+			return checkUserInputMove();
+		}
+	}
+
+	private String checkUserInputCommend() {
+		try {
+			messageQuestionPrint.userCommandQuestion();
+			return inputView.readGameCommand();
+		} catch (IllegalArgumentException e) {
+			System.out.println(e.getMessage());
+			return checkUserInputCommend();
+		}
+	}
+
+	private int checkUserInputBridgeSize() {
+		try {
+			messageQuestionPrint.inputBridgeSizeQuestion();
+			return inputView.readBridgeSize();
+		} catch (IllegalArgumentException e) {
+			System.out.println(e.getMessage());
+			return checkUserInputBridgeSize();
+		}
+	}
+}
