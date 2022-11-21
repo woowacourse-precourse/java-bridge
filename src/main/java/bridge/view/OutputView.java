bbb837298d66aeebfe226c458fbf29914ba8f2d3
hhiyeon <hhyyeon0214@gmail.com>
2022-11-22 00:47:54 +0900

    feat:OutputView-print result for final gameresult

--- src/main/java/bridge/view/OutputView.java
+++ src/main/java/bridge/view/OutputView.java
@@ -1,16 +1,17 @@
 package bridge.view;
 
+import bridge.domain.Bridge;
+
 /**
  * 사용자에게 게임 진행 상황과 결과를 출력하는 역할을 한다.
  */
 public class OutputView {
-
     /**
      * 현재까지 이동한 다리의 상태를 정해진 형식에 맞춰 출력한다.
      * <p>
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void printMap() {
+    public void printMap(Bridge bridge) {
     }
 
     /**
@@ -18,7 +19,23 @@ public class OutputView {
      * <p>
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void printResult() {
+    public void printResult(String gameResult, int gameTryCount, String lastMapResult) {
+        printGameFinalBridgeResult(); // 최종 게임 결과
+        System.out.println(lastMapResult); // 게임 정보 출력
+        printGameSuccessOrFailure(gameResult); // 게임 성공 여부
+        printGameTotalTryCount(gameTryCount); // 총 시도한 횟수
+    }
+
+    public void printGameFinalBridgeResult() {
+        System.out.println("최종 게임 결과");
+    }
+
+    public void printGameSuccessOrFailure(String gameResult) {
+        System.out.println("게임 성공 여부: " + gameResult);
+    }
+
+    public void printGameTotalTryCount(int gameTryCount) {
+        System.out.println("총 시도한 횟수: " + gameTryCount);
     }
 
     public void printStartBridgeGame() {
@@ -36,17 +53,5 @@ public class OutputView {
 
     public void printEnterGameCommand() {
         System.out.println("게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)");
-    }
-
-    public void printGameFinalBridgeResult() {
-        System.out.println("최종 게임 결과");
-    }
-
-    public void printGameSuccessOrFailure(String gameResult){
-        System.out.println("게임 성공 여부: "+gameResult);
-    }
-
-    public void printGameTotalTryCount(int gameTryCount){
-        System.out.println("총 시도한 횟수: "+gameTryCount);
     }
 }
