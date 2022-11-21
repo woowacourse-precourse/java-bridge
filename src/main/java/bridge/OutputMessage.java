3381a92a022bf2ed5754b8112e08c18957cfacb5
dvlpr0 <rladjswkd@naver.com>
2022-11-21 22:04:56 +0900

    fix: Message 클래스를 OutputMessage 클래스로 변경

--- src/main/java/bridge/OutputMessage.java
+++ src/main/java/bridge/OutputMessage.java
@@ -1,11 +1,11 @@
 package bridge;
 
-public enum Message {
+public enum OutputMessage {
     START_MESSAGE("다리 건너기 게임을 시작합니다.\n"),
     BRIDGE_LENGTH_MESSAGE("다리의 길이를 입력해주세요.");
 
     private final String message;
-    Message(String message) {
+    OutputMessage(String message) {
         this.message = message;
     }
     @Override
--- src/main/java/bridge/OutputView.java
+++ src/main/java/bridge/OutputView.java
@@ -1,7 +1,7 @@
 package bridge;
 
-import static bridge.Message.START_MESSAGE;
-import static bridge.Message.BRIDGE_LENGTH_MESSAGE;
+import static bridge.OutputMessage.START_MESSAGE;
+import static bridge.OutputMessage.BRIDGE_LENGTH_MESSAGE;
 /**
  * 사용자에게 게임 진행 상황과 결과를 출력하는 역할을 한다.
  */
