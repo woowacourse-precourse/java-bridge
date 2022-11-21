bf90107167a0c68120d730223754c5a76632f38f
gudals113 <gudals113@gmail.com>
2022-11-21 18:34:22 +0900

    feat: 다리 생성
    feat: 다리 생성
    
    BridgeNumberGenerator 클래스 이용해서 다리 생성하기

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -1,11 +1,17 @@
 package bridge;
 
+import java.util.List;
+
 public class Application {
 
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
         InputView inputView = new InputView();
-        inputView.readBridgeSize();
+        BridgeNumberGenerator bridgeNumberGenerator = new BridgeRandomNumberGenerator();
+        BridgeMaker bridgeMaker = new BridgeMaker(bridgeNumberGenerator);
+
+        int bridgeSize = inputView.readBridgeSize();
+        List<String> bridge = bridgeMaker.makeBridge(bridgeSize);
+
     }
 
 }
