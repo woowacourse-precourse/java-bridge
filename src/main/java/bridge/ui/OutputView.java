7a19f759ab4b30fb57766f00267faa44a25a951b
Jinseo Song <82093280+rxb8k@users.noreply.github.com>
2022-11-22 01:27:01 +0900

    feat: 전체 이동 경로 출력 기능 추가

--- src/main/java/bridge/ui/OutputView.java
+++ src/main/java/bridge/ui/OutputView.java
@@ -19,7 +19,11 @@ public class OutputView {
      * <p>
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void printMap() {
+    public void printMap(Bridge bridge) {
+        String UpperBridgeMapInfo = printEachBridgeMap(bridge, "U");
+        String BelowBridgeMapInfo = printEachBridgeMap(bridge, "D");
+        System.out.println(UpperBridgeMapInfo);
+        System.out.println(BelowBridgeMapInfo);
     }
 
     public String printEachBridgeMap(Bridge bridge, String direction) {
