dcb8465c3b03768b1c7d2662dcedaf1b5594dc4b
drunkenhw <drunkenhw@gmail.com>
2022-11-21 20:04:45 +0900

    refactor(OutputView): Game 결과를 클래스로 분리

---
+++ src/main/java/bridge/domain/BridgeResult.java
@@ -0,0 +1,33 @@
+package bridge.domain;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class BridgeResult {
+    private final List<String> upBridge = new ArrayList<>();
+    private final List<String> downBridge = new ArrayList<>();
+
+    public void putResult(BridgeDirection direction, Answer answer) {
+        if (direction == BridgeDirection.UP) {
+            upBridge.add(answer.getMessage());
+            downBridge.add(" ");
+        }
+        if (direction == BridgeDirection.DOWN) {
+            upBridge.add(" ");
+            downBridge.add(answer.getMessage());
+        }
+    }
+    public void reset() {
+        upBridge.clear();
+        downBridge.clear();
+    }
+
+    public List<String> getUpBridge() {
+        return Collections.unmodifiableList(upBridge);
+    }
+
+    public List<String> getDownBridge() {
+        return Collections.unmodifiableList(downBridge);
+    }
+}
--- src/main/java/bridge/view/OutputView.java
+++ src/main/java/bridge/view/OutputView.java
@@ -1,9 +1,7 @@
 package bridge.view;
 
-import bridge.domain.Answer;
-import bridge.domain.BridgeDirection;
+import bridge.domain.BridgeResult;
 import bridge.domain.GameStatus;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -11,8 +9,7 @@ import java.util.stream.Collectors;
  * 사용자에게 게임 진행 상황과 결과를 출력하는 역할을 한다.
  */
 public class OutputView {
-    List<String> upBridge = new ArrayList<>();
-    List<String> downBridge = new ArrayList<>();
+
 
     public void printStart() {
         System.out.println("다리 건너기 게임을 시작합니다.");
@@ -23,24 +20,14 @@ public class OutputView {
      * <p>
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void printMap(BridgeDirection direction, Answer answer) {
-        if (direction == BridgeDirection.UP) {
-            upBridge.add(answer.getMessage());
-            downBridge.add(" ");
-        }
-        if (direction == BridgeDirection.DOWN) {
-            upBridge.add(" ");
-            downBridge.add(answer.getMessage());
-        }
-        System.out.println(upBridge.stream()
-                .collect(Collectors.joining(" | ", "[ ", " ]")));
-        System.out.println(downBridge.stream()
-                .collect(Collectors.joining(" | ", "[ ", " ]")));
+    public void printMap(BridgeResult bridgeResult) {
+        printBridge(bridgeResult.getUpBridge());
+        printBridge(bridgeResult.getDownBridge());
     }
 
-    public void reset() {
-        upBridge.clear();
-        downBridge.clear();
+    private void printBridge(List<String> bridge) {
+        System.out.println(bridge.stream()
+                .collect(Collectors.joining(" | ", "[ ", " ]")));
     }
 
     /**
@@ -48,12 +35,9 @@ public class OutputView {
      * <p>
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void printResult(GameStatus gameStatus, int tryCount) {
+    public void printResult(BridgeResult bridgeResult, GameStatus gameStatus, int tryCount) {
         System.out.println("최종 게임 결과");
-        System.out.println(upBridge.stream()
-                .collect(Collectors.joining(" | ", "[ ", " ]")));
-        System.out.println(downBridge.stream()
-                .collect(Collectors.joining(" | ", "[ ", " ]")));
+        printMap(bridgeResult);
         System.out.println("게임 성공 여부: " + gameStatus.getMessage());
         System.out.println("총 시도한 횟수: " + tryCount);
     }
