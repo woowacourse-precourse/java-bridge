9ed4478a1ad9977ad45ec8b3681363ca9d02bd80
junyong <jymoon1121@gmail.com>
2022-11-21 15:43:25 +0900

    feat(Application): 다리 건너기 게임 프로그램 구현(main)

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -5,5 +5,19 @@ public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
         System.out.println("다리 건너기 게임을 시작합니다.");
+        BridgeGame bridgeGame = new BridgeGame();
+        bridgeGame.init();
+        gameStart(bridgeGame);
+    }
+    private static void gameStart(BridgeGame bridgeGame) {
+        while(true) {
+            boolean move = bridgeGame.move();
+            if(bridgeGame.isGameCompleted()) {
+                break;
+            }
+            if(!move && !bridgeGame.retry()) {
+                break;
+            }
+        }
     }
 }
