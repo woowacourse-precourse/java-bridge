0957c64bc2fb2079a91cae8bb024c52b7c2978de
koo9b9h <90121024+koo9b9h@users.noreply.github.com>
2022-11-21 23:08:34 +0900

    feat: 유저 다리 선택 현황을 보여주는 기능, 최종결과 보여주는 기능 추가
    printMap(): 다리 현황 보여주는 기능
    printResult() : 최종 결과를 보여주는 기능

--- src/main/java/bridge/presentation/OutputView.java
+++ src/main/java/bridge/presentation/OutputView.java
@@ -1,29 +1,34 @@
 package bridge.presentation;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
-/**
- * 사용자에게 게임 진행 상황과 결과를 출력하는 역할을 한다.
- */
 public class OutputView {
     private final String STARTING_SENTENCE = "다리 건너기 게임을 시작합니다." + System.lineSeparator() + "다리의 길이를 입력해주세요.";
     private final String MOVING_SENTENCE = "이동할 칸을 선택해주세요. (위: U, 아래: D)";
     private final String RESTART_SENTENCE = "게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)";
-    /**
-     * 현재까지 이동한 다리의 상태를 정해진 형식에 맞춰 출력한다.
-     * <p>
-     * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
-     */
-    public void printMap(List<String> userSelectResult , boolean success) {
-    }
+    private final String END_SENTENCE = "최종 게임 결과";
+    private final String SUCCESS_SENTENCE = "게임 성공 여부: ";
+    private final String CHALLENGE_SENTENCE = "총 시도한 횟수: ";
 
-    /**
-     * 게임의 최종 결과를 정해진 형식에 맞춰 출력한다.
-     * <p>
-     * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
-     */
-    public void printResult() {
-    }
+    public void printMap(List<String> userSelectResult, boolean success) {
+        ReturnPrintMap returnPrintMap = new ReturnPrintMap();
+        Map<String, String> contentsMap = returnPrintMap.returnContentsMap(userSelectResult,success);
+        System.out.println("[ " + contentsMap.get("upContents") + " ]");
+        System.out.println("[ " + contentsMap.get("downContents") + " ]");
+    }//printMap
+
+    public void printResult(List<String> userSelectResult, boolean success, boolean gameSuccess, int challengeCount) {
+        String successResult = "실패";
+        if (gameSuccess) {
+            successResult = "성공";
+        }//if
+        System.out.println(END_SENTENCE);
+        printMap(userSelectResult, success);
+        System.out.println(SUCCESS_SENTENCE + successResult);
+        System.out.println(CHALLENGE_SENTENCE + challengeCount);
+    }//printResult
 
     public void startGuide() {
         System.out.println(STARTING_SENTENCE);
@@ -33,7 +38,8 @@ public class OutputView {
         System.out.println(MOVING_SENTENCE);
     }//moveGuide
 
-    public void restartGuide(){
+    public void restartGuide() {
         System.out.println(RESTART_SENTENCE);
     }//restartGuide
+
 }// end class
