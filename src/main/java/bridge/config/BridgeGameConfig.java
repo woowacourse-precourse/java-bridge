fd8848d708a185cf2fb066cd4d7d7d47197f7801
phdljr <phdljr@naver.com>
2022-11-22 01:25:15 +0900

    feat: Config 파일 추가

---
+++ src/main/java/bridge/config/BridgeGameConfig.java
@@ -0,0 +1,12 @@
+package bridge.config;
+
+public interface BridgeGameConfig {
+    int BRIDGE_LENGTH_MIN = 3;
+    int BRIDGE_LENGTH_MAX = 20;
+    String BRIDGE_UP = "U";
+    String BRIDGE_DOWN = "D";
+    int BRIDGE_UP_NUMBER = 1;
+    int BRIDGE_DOWN_NUMBER = 0;
+    String GAME_RETRY = "R";
+    String GAME_EXIT = "E";
+}
