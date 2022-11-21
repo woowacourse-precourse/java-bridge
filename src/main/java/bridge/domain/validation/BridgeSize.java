368376b08bbb30c1435183626acec2505a31634c
endlessmomo <soohyuk96@naver.com>
2022-11-21 12:58:10 +0900

    feat(BridgeSize) : BridgeSize 객체 생성

---
+++ src/main/java/bridge/domain/validation/BridgeSize.java
@@ -0,0 +1,26 @@
+package bridge.domain.validation;
+
+public class BridgeSize {
+    public static final int MAX = 20;
+    public static final int MIN = 3;
+    private final int bridgeSize;
+
+    public BridgeSize(int input){
+        checkRange(input);
+
+        this.bridgeSize = input;
+    }
+
+    public int getBridgeSize(){
+        return bridgeSize;
+    }
+
+    public void checkRange(int input){
+        if(input < MIN || input > MAX){
+            throw new IllegalArgumentException("");
+        }
+    }
+
+
+}
+
