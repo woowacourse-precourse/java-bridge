5c54daa4e86a0cf2b6a7ca4111f41937846f67e1
Woojin Lee <dhkep03@gmail.com>
2022-11-22 05:42:20 +0900

    refactor: 줄 띄움 상수 추가

--- src/main/java/bridge/ui/InputView.java
+++ src/main/java/bridge/ui/InputView.java
@@ -10,11 +10,12 @@ public class InputView {
     private static final String INPUT_BRIDGE_SIZE = "다리의 길이를 입력해주세요.";
     private static final String INPUT_MOVING = "이동할 칸을 선택해주세요. (위: U, 아래: D)";
     private static final String INPUT_GAME_COMMAND = "게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)";
+    private static final String NO_LETTER = "";
 
     public int readBridgeSize() {
         System.out.println(INPUT_BRIDGE_SIZE);
         int bridgeSize = getBridgeSizeInputAfterParse();
-        System.out.println();
+        System.out.println(NO_LETTER);
         return bridgeSize;
     }
 
