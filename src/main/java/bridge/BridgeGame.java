dad148ea0ea8479a64564e5b74db94219c6ae1c5
skydreamer21 <skydreamer210@gmail.com>
2022-11-21 11:31:00 +0900

    feat(BridgeGame): retry 메서드 구현

--- src/main/java/bridge/BridgeGame.java
+++ src/main/java/bridge/BridgeGame.java
@@ -34,12 +34,19 @@ public class BridgeGame {
      * 재시작을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
     public void retry() {
+        decreaseOrder();
+    }
+
     private void increaseAttemptCount() {
         attemptCount++;
     }
 
     private void increaseOrder() {
         order++;
+    }
+
+    private void decreaseOrder() {
+        order--;
     }
 
     public int getAttemptCount() {
