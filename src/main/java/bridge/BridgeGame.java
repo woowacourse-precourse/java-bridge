5bc7a1bc362cd688f599df6da941a31c422861f7
oreo <ohsimon77@naver.com>
2022-11-21 22:17:09 +0900

    Feat : 1차로 기능 구현은 완료, 리팩토링 시작 초기 단계

--- src/main/java/bridge/BridgeGame.java
+++ src/main/java/bridge/BridgeGame.java
@@ -4,57 +4,63 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 
-import static bridge.BridgeMaker.bridge;
+//import static bridge.BridgeMaker.bridge;
 
 /**
  * 다리 건너기 게임을 관리하는 클래스
  */
 public class BridgeGame {
-    public static List<String> userBridge = new ArrayList<>();
-    public static boolean end = false;
+    public static boolean end = true;
+    public static List<String> Result = new ArrayList<>();
+    public static int count = 0;
 
     public void gameStart() {
         System.out.println("다리 건너기 게임을 시작합니다.\n");
 
         InputView inputView = new InputView();
         int bridgeLength = inputView.readBridgeSize();
-        BridgeMaker.makeBridge(bridgeLength);
-        System.out.println(bridge); //sout
 
-        BridgeRandomNumberGenerator bridgeRandomNumberGenerator = new BridgeRandomNumberGenerator();
-        BridgeMaker bridgeMaker = new BridgeMaker(bridgeRandomNumberGenerator);
-        System.out.println(bridgeMaker.bridge);
-        System.out.println(bridgeRandomNumberGenerator);
-
-        OutputView outputView = new OutputView();
-        outputView.printMap(bridge);
-
-        boolean end = false;
-        boolean success = move(bridge);
-        retry();
-        System.out.println(userBridge);
-
-        if (success){
-            System.out.println("게임 성공 여부 : 성공");
+        while(end){
+            gamePlay(bridgeLength);
         }
-        else{
-            System.out.println("게임 성공 여부 : 실패");
-        }
-        int count = 0;
-        System.out.println("총 시도한 횟수 : " + count);
     }
     /**
      * 사용자가 칸을 이동할 때 사용하는 메서드
      * <p>
      * 이동을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
+    public static void gamePlay(int bridgeLength){
+        BridgeRandomNumberGenerator bridgeRandomNumberGenerator = new BridgeRandomNumberGenerator();
+        BridgeMaker bridgeMaker = new BridgeMaker(bridgeRandomNumberGenerator);
+        List<String> bridge = bridgeMaker.makeBridge(bridgeLength);
+        //System.out.println(bridge);
 
-    public boolean move(List<String> bridge) {
+        OutputView outputView = new OutputView();
+        outputView.printMap(bridge);
+
+        boolean success = move(bridge);
+        count++;
+        if (success){
+            System.out.println("\n최종 게임 결과");
+            OutputView.printResult(Result);
+            System.out.println("게임 성공 여부 : 성공");
+            System.out.println("총 시도한 횟수 : " + count);
+            end = false;
+        }
+        else{
+            retry();
+        }
+    }
+
+    public static boolean move(List<String> bridge) {
+        List<String> userBridge = new ArrayList<>();
         for(int i=0;i<bridge.size();i++){
             InputView inputView = new InputView();
             String MovingSquare = inputView.readMoving();
             userBridge.add(MovingSquare);
-            OutputView.print(userBridge);
+
+            Result = OutputView.print(bridge, userBridge);
+            OutputView.printResult(Result);
             if (!Objects.equals(MovingSquare, bridge.get(i))) {
                 return false;
             }
@@ -68,11 +74,15 @@ public class BridgeGame {
      * <p>
      * 재시작을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void retry() {
+    public static void retry() {
         InputView inputView = new InputView();
         String retryCommand = inputView.readGameCommand();
         if (Objects.equals(retryCommand, "Q")){
-            end = true;
+            System.out.println("\n최종 게임 결과");
+            OutputView.printResult(Result);
+            System.out.println("게임 성공 여부 : 실패");
+            System.out.println("총 시도한 횟수 : " + count);
+            end = false;
         }
     }
 }
--- src/main/java/bridge/BridgeMaker.java
+++ src/main/java/bridge/BridgeMaker.java
@@ -3,6 +3,7 @@ package bridge;
 import java.security.spec.RSAOtherPrimeInfo;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 /**
  * 다리의 길이를 입력 받아서 다리를 생성해주는 역할을 한다.
@@ -10,7 +11,7 @@ import java.util.List;
 public class BridgeMaker {
 
     private final BridgeNumberGenerator bridgeNumberGenerator;
-    public static List<String> bridge = new ArrayList<>();
+    //public static List<String> bridge = new ArrayList<>();
 
     public BridgeMaker(BridgeNumberGenerator bridgeNumberGenerator) {
         this.bridgeNumberGenerator = bridgeNumberGenerator;
@@ -20,8 +21,8 @@ public class BridgeMaker {
      * @param size 다리의 길이
      * @return 입력받은 길이에 해당하는 다리 모양. 위 칸이면 "U", 아래 칸이면 "D"로 표현해야 한다.
      */
-    public static List<String> makeBridge(int size) {
-        //List<String> bridge = new ArrayList<>();
+    public List<String> makeBridge(int size) {
+        List<String> bridge = new ArrayList<>();
         BridgeRandomNumberGenerator bridgeRandomNumberGenerator = new BridgeRandomNumberGenerator();
         for(int i=0;i<size;i++){
             int number = bridgeRandomNumberGenerator.generate();
