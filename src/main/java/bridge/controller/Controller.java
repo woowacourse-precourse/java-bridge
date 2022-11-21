cd1cdba62e588b4b6d841d64d2beace6337c48d5
0chil <0@chll.it>
2022-11-21 15:34:48 +0900

    feat(Controller): 다리 길이를 입력받아 다리를 만든다

---
+++ src/main/java/bridge/controller/Controller.java
@@ -0,0 +1,28 @@
+package bridge.controller;
+
+import java.util.List;
+
+import bridge.domain.BridgeGame;
+import bridge.domain.maker.BridgeMaker;
+import bridge.domain.maker.BridgeRandomNumberGenerator;
+import bridge.dto.TrialResult;
+import bridge.view.InputView;
+import bridge.view.OutputView;
+
+public class Controller {
+
+    private final InputView inputView;
+    private final OutputView outputView;
+    private final BridgeMaker bridgeMaker;
+
+    public Controller(InputView inputView, OutputView outputView) {
+        this.inputView = inputView;
+        this.outputView = outputView;
+        this.bridgeMaker = new BridgeMaker(new BridgeRandomNumberGenerator());
+    }
+
+    public void play() {
+        int bridgeSize = inputView.readBridgeSize();
+        List<String> bridge = bridgeMaker.makeBridge(bridgeSize);
+    }
+}
