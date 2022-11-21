d80885806667f1f6f88ae4decbd3e24089285778
MinHyeok Moon <odo27@MinHyeoks-MacBook-Air.local>
2022-11-21 16:04:32 +0900

    refactor(Application): 기능 분할 및 메소드명 변경

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -19,55 +19,66 @@ public class Application {
         bridgeMaker = new BridgeMaker(bridgeRandomNumberGenerator);
     }
 
-    private void start() {
-        OutputView.printStartMessage();
-        int bridgeSize = InputView.readBridgeSize();
-        List<String> bridge = bridgeMaker.makeBridge(bridgeSize);
-        bridgeGame = new BridgeGame(bridge);
-        process();
+    public static void main(String[] args) {
+        Application application = new Application();
+        application.start();
     }
 
-    private void process() {
+    private void start() {
+        OutputView.printStartMessage();
+
+        int bridgeSize = InputView.readBridgeSize();
+        List<String> bridge = bridgeMaker.makeBridge(bridgeSize);
+
+        bridgeGame = new BridgeGame(bridge);
+        startBridgeGame();
+    }
+
+    private void startBridgeGame() {
         boolean quit = false;
+
         while (!quit) {
             int gameStatus = guess();
-            if (gameStatus == Status.FAIL.getNumber()) {
-                quit = fail();
-                continue;
-            }
-            quit = true;
-            finish(true);
+            quit = isQuit(gameStatus);
         }
     }
 
     private int guess() {
         int gameStatus = Status.CONTINUE.getNumber();
+
         while (gameStatus == Status.CONTINUE.getNumber()) {
             String moving = InputView.readMoving();
             gameStatus = bridgeGame.move(moving);
             OutputView.printMap(bridgeGame.getMap());
         }
+
         return gameStatus;
+    }
+
+    private boolean isQuit(int gameStatus) {
+        if (gameStatus == Status.FAIL.getNumber()) {
+            String gameCommand = InputView.readGameCommand();
+            return !isRetry(gameCommand);
+        }
+
+        finish(true);
+        return true;
+    }
+
+    private boolean isRetry(String gameCommand) {
+        if (gameCommand.equals(GameCommand.RETRY.getLetter())) {
+            bridgeGame.retry();
+            return true;
+        }
+
+        finish(false);
+        return false;
     }
 
     private void finish(boolean success) {
         Map lastMap = bridgeGame.getMap();
         int totalTry = bridgeGame.getTotalTry();
+
         OutputView.printResult(lastMap, success, totalTry);
-    }
-
-    private boolean fail() {
-        String gameCommand = InputView.readGameCommand();
-        if (gameCommand.equals(GameCommand.RETRY.getLetter())) {
-            bridgeGame.retry();
-            return false;
-        }
-        finish(false);
-        return true;
-    }
-
-    public static void main(String[] args) {
-        Application app = new Application();
-        app.start();
     }
 }
