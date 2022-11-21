bdfee5749f9632f3cbf9a66a17aba1606d3aa45d
Longstick0 <789456jang@naver.com>
2022-11-22 03:13:58 +0900

    chore: 불필요한 주석 삭제

--- src/main/java/bridge/service/BridgeGame.java
+++ src/main/java/bridge/service/BridgeGame.java
@@ -8,9 +8,6 @@ import bridge.domain.result.BridgeResult;
 import bridge.domain.result.GameState;
 import bridge.domain.result.MovingResult;
 
-/**
- * 다리 건너기 게임을 관리하는 클래스
- */
 public class BridgeGame {
 
     public static final String QUIT_COMMAND = "Q";
@@ -25,11 +22,6 @@ public class BridgeGame {
         return new MovingResult(bridgeBlock, "X");
     }
 
-    /**
-     * 사용자가 게임을 다시 시도할 때 사용하는 메서드
-     * <p>
-     * 재시작을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
-     */
     public void retry(BridgeResult bridgeResult, GameState gameState, RetryCommand retryCommand) {
         if (retryCommand.name().equals(QUIT_COMMAND)) {
             gameState.gameOver();
--- src/main/java/bridge/view/OutputView.java
+++ src/main/java/bridge/view/OutputView.java
@@ -27,13 +27,6 @@ public class OutputView {
         System.out.println("[ "+ bridgeResult.lowerMapToString() + " ]");
     }
 
-    /**
-     * 게임의 최종 결과를 정해진 형식에 맞춰 출력한다.
-     * <p>
-     * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
-     * @param bridgeResult
-     * @param gameState
-     */
     public void printResult(BridgeResult bridgeResult, GameState gameState) {
         System.out.println(FINAL_RESULT_MESSAGE);
         printMapOfCase(bridgeResult);
