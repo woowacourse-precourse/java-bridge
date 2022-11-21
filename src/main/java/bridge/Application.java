03f4dd7045b781e6e50b1e53d1915e90950bbc49
baekeunsun <eunsun5460@gmail.com>
2022-11-21 13:10:19 +0900

    feat: 다리생성

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -1,8 +1,16 @@
 package bridge;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
+import static bridge.view.InputView.readBridgeSize;
+
 public class Application {
 
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        BridgeNumberGenerator bridgeNumberGenerator = new BridgeRandomNumberGenerator();
+
+        BridgeMaker user = new BridgeMaker(bridgeNumberGenerator);
+        System.out.println(user.makeBridge(readBridgeSize()));
     }
 }
