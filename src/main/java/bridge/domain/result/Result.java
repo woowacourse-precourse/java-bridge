ac161a061ea2d1b4e46be0145e486cc062b4d48f
kimJaeHyeon <kmr2644@gmail.com>
2022-11-21 20:02:56 +0900

    refactor: 시도횟수 필드 추가, 이동결과를 담는 Map 에 resultType 을 추가하는 메시지 구현 및 초기화 메서드, 종료확인 메서드 구현

---
+++ src/main/java/bridge/domain/result/Result.java
@@ -0,0 +1,44 @@
+package bridge.domain.result;
+
+public class Result {
+    private final Map map;
+    private ResultType lastResultType;
+    private int tryCount = 1;
+
+    public Result(Map map) {
+        this.map = map;
+        this.lastResultType = ResultType.BLANK;
+    }
+
+    private void updateResult(ResultType resultType) {
+        lastResultType = resultType;
+    }
+
+    public boolean isSucceeded() {
+        return lastResultType.isSucceeded();
+    }
+
+    public void addUpBlock(ResultType resultType) {
+        map.addUpBlock(resultType);
+        updateResult(resultType);
+
+    }
+    public void addDownBlock(ResultType resultType) {
+        map.addDownBlock(resultType);
+        updateResult(resultType);
+    }
+
+    //수정
+    public Map toMap() {
+        return map;
+    }
+
+    public void retry() {
+        map.retry();
+        tryCount++;
+    }
+
+    public boolean isFinished() {
+        return lastResultType.isFinished();
+    }
+}
