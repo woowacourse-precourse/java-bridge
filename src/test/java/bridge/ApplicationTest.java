9d59b119932b99ec60f608d7d20e04e6a0ee61f7
Lee-Jiseung <ljss98@naver.com>
2022-11-21 18:01:07 +0900

    test: 전체 기능 테스트 추가

--- src/test/java/bridge/ApplicationTest.java
+++ src/test/java/bridge/ApplicationTest.java
@@ -41,6 +41,38 @@ class ApplicationTest extends NsTest {
     }
 
     @Test
+    void 기능_테스트_2() {
+        assertRandomNumberInRangeTest(() -> {
+            run("3", "D", "U", "D");
+            assertThat(output()).contains(
+                    "최종 게임 결과",
+                    "[   | O |   ]",
+                    "[ O |   | O ]",
+                    "게임 성공 여부: 성공",
+                    "총 시도한 횟수: 1"
+            );
+
+            int upSideIndex = output().indexOf("[   | O |   ]");
+            int downSideIndex = output().indexOf("[ O |   | O ]");
+            assertThat(upSideIndex).isLessThan(downSideIndex);
+        }, 0, 1, 0);
+    }
+
+    @Test
+    void 기능_테스트_3() {
+        assertRandomNumberInRangeTest(() -> {
+            run("3", "U", "R", "D", "D", "R", "D", "U", "U", "R", "D", "U", "D");
+            assertThat(output()).contains(
+                    "최종 게임 결과",
+                    "[   | O |   ]",
+                    "[ O |   | O ]",
+                    "게임 성공 여부: 성공",
+                    "총 시도한 횟수: 4"
+            );
+        }, 0, 1, 0);
+    }
+
+    @Test
     void 예외_테스트() {
         assertSimpleTest(() -> {
             runException("a");
