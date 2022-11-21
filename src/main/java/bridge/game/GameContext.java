6f0963351c8f169524ac0a03239d92f0aea16c31
dldbdud314 <dldbdud314@naver.com>
2022-11-21 14:35:06 +0900

    style(GameContext): 주석 추가

--- src/main/java/bridge/game/GameContext.java
+++ src/main/java/bridge/game/GameContext.java
@@ -2,6 +2,13 @@ package bridge.game;
 
 import static bridge.game.State.PLAYING;
 
+/**
+ * 전체 게임 현황 정보가 있는 클래스
+ * <p>
+ * - retryCnt : 총 시도한 횟수 (1이 default)
+ * <p>
+ * - state : PLAYING, QUIT_PLAYING (PLAYING이 default)
+ */
 public class GameContext {
     private int retryCnt;
     private State state;
