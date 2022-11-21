8b3feace197dc5a026d4cbf325ccde2c04b949c4
Amyhds <dasomhan524@gmail.com>
2022-11-21 13:59:50 +0900

    feat(map): 다리 건너기 결과 생성 메서드, 추가 메서드, 반환 메서드 구현

--- src/main/java/bridge/Map.java
+++ src/main/java/bridge/Map.java
@@ -1,4 +1,37 @@
 package bridge;
 
 public class Map {
+
+    private String upMap = "";
+    private String downMap = "";
+
+    public void makeMap(String moving, String check) {
+        if (moving.equals("U")) {
+            upMap += "[ " + check;
+            downMap += "[ " + " ";
+        }
+        if (moving.equals("D")) {
+            upMap += "[ " + " ";
+            downMap += "[ " + check;
+        }
+    }
+
+    public void addMap(String moving, String check) {
+        if (moving.equals("U")) {
+            upMap += " | " + check;
+            downMap += " | " + " ";
+        }
+        if (moving.equals("D")) {
+            upMap += " | " + " ";
+            downMap += " | " + check;
+        }
+    }
+
+    public String getUpMap() {
+        return upMap + " ]";
+    }
+
+    public String getDownMap() {
+        return downMap + " ]";
+    }
 }
