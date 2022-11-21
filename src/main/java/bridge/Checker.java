cfb43cc498ee27a29af53e019cce4ba01e701fff
nn98 <jkllhgb@gmail.com>
2022-11-21 13:05:34 +0900

    Feat: 게임 성공 체크

--- src/main/java/bridge/Checker.java
+++ src/main/java/bridge/Checker.java
@@ -69,4 +69,12 @@ public class Checker {
         return gameOption.equals("R");
     }
 
+    public boolean checkGameSuccess(BridgeGame bridgeGame) {
+        boolean success=false;
+        success=bridgeGame.getIndex()==bridgeGame.getBridge().size();
+        for(MoveCase result:bridgeGame.getResults()){
+            success=success&&result.isResult();
+        }
+        return success;
+    }
 }
