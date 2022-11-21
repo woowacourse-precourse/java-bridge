af5e33e4f1691e62adc913f0b5f540b9cd3c5b5e
Your Name <you@example.com>
2022-11-21 14:15:34 +0900

    fix: 시작 메세지와 입력값 받는 메세지 분리

--- src/main/java/bridge/OutputView.java
+++ src/main/java/bridge/OutputView.java
@@ -36,7 +36,7 @@ public class OutputView {
     }
 
     public static void printStart() {
-        System.out.println("다리 건너기 게임을 시작합니다.\n다리의 길이를 입력해주세요.");
+        System.out.println("다리 건너기 게임을 시작합니다.");
     }
 
     public static void printSelectMove() {
@@ -45,5 +45,9 @@ public class OutputView {
 
     public static void printGameOver() {
         System.out.println("게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)");
+    }
+
+    public static void printAskBridgeSize() {
+        System.out.println("다리의 길이를 입력해주세요.");
     }
 }
