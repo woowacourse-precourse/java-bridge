ba27c4d58783897896ab08c2610447f3566cdd72
hoin <apicad0307@gmail.com>
2022-11-21 14:00:13 +0900

    feat(input/InputView.java): 다리의 길이 입력받는 함수 구현 (+범위 벗어날 때 예외처리)

--- src/main/java/bridge/input/InputView.java
+++ src/main/java/bridge/input/InputView.java
@@ -4,49 +4,22 @@ import camp.nextstep.edu.missionutils.Console;
 
 import static bridge.input.InputString.*;
 
-/**
- * 사용자로부터 입력을 받는 역할을 한다.
- */
 public class InputView {
-
-    /**
-     * 다리의 길이를 입력받는다.
-     */
-    public int readBridgeSize() {
+    public static int readBridgeSize() {
         System.out.println(BRIDGE_LENGTH_INPUT);
-        int inputLength;
-        try{
-            inputLength = Integer.parseInt(Console.readLine());
-        }catch (Exception e){
+        String input = Console.readLine();
+        try {
+            int inputLength = Integer.parseInt(input);
+            if (inputLength < 3 || inputLength > 20) {
+                throw new IllegalArgumentException(ERROR_NOT_IN_RANGE);
+            }
+            return inputLength;
+        } catch (NumberFormatException e) {
             throw new IllegalArgumentException(ERROR_NO_NUMBER_INPUT);
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
         }
-        if(inputLength < 3 || inputLength > 20 ) {
-            throw new IllegalArgumentException(ERROR_NOT_IN_RANGE);
-        }
-        return inputLength;
+        return readBridgeSize();
     }
 
-    /**
-     * 사용자가 이동할 칸을 입력받는다.
-     */
-    public String readMoving() {
-        System.out.println(MOVE_INPUT);
-        String input = Console.readLine();
-        if(input != "U" || input != "D"){
-            throw new IllegalArgumentException(ERROR_MOVE_INPUT);
-        }
-        return input;
-    }
-
-    /**
-     * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
-     */
-    public String readGameCommand() {
-        System.out.println(RETRY_INPUT);
-        String input = Console.readLine();
-        if(input != "R" || input != "Q"){
-            throw new IllegalArgumentException(ERROR_RESTART_INPUT);
-        }
-        return input;
-    }
 }
