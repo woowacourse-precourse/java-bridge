596f71dafd777c172f1304a27c168c55a1367982
DaeyeonKim97 <kdysy1130@khu.ac.kr>
2022-11-21 12:49:06 +0900

    feat(application): declare fields
    field lists
    - inputView
    - OutputView
    - BridgeGame

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -1,6 +1,9 @@
 package bridge;
 
 public class Application {
+    private static InputView inputView = new InputView();
+    private static OutputView outputView = new OutputView();
+    private static BridgeGame bridgeGame = new BridgeGame();
 
     public static void main(String[] args) {
         // TODO: 프로그램 구현
