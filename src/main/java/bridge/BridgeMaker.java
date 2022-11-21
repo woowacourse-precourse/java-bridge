f7b14180f06efc7c8429990978c1346f9bdb21c0
E1psycongr00 <39326175+E1psycongr00@users.noreply.github.com>
2022-11-21 17:14:53 +0900

    refactor(brideMaker): 다리 길이 유효성 제거 및 분리

--- src/main/java/bridge/BridgeMaker.java
+++ src/main/java/bridge/BridgeMaker.java
@@ -5,11 +5,6 @@ import java.util.List;
 
 public class BridgeMaker {
 
-    private static final int MIN_BRIDGE_SIZE = 3;
-    private static final int MAX_BRIDGE_SIZE = 20;
-    private static final String ERROR_BRIDGE_SIZE = String.format("다리 길이는 %d부터 %d 사이의 숫자여야 합니다.", MIN_BRIDGE_SIZE,
-            MAX_BRIDGE_SIZE);
-
     private final BridgeNumberGenerator bridgeNumberGenerator;
 
     public BridgeMaker(BridgeNumberGenerator bridgeNumberGenerator) {
@@ -17,7 +12,6 @@ public class BridgeMaker {
     }
 
     public List<String> makeBridge(int size) {
-        validateBridgeSize(size);
         List<String> bridge = new ArrayList<>();
         for (int i = 0 ; i < size; ++i) {
             int randomNumber = bridgeNumberGenerator.generate();
@@ -37,9 +31,4 @@ public class BridgeMaker {
         throw new IllegalArgumentException("0과 1 이외에는 mapping 할 수 없습니다");
     }
 
-    private void validateBridgeSize(int size) {
-        if (size < MIN_BRIDGE_SIZE || size > MAX_BRIDGE_SIZE) {
-            throw new IllegalArgumentException(ERROR_BRIDGE_SIZE);
-        }
-    }
 }
--- src/test/java/bridge/BridgeMakerTest.java
+++ src/test/java/bridge/BridgeMakerTest.java
@@ -5,23 +5,12 @@ import static org.mockito.Mockito.when;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.CsvSource;
 import org.mockito.Mockito;
 
 class BridgeMakerTest {
 
     private final BridgeNumberGenerator bridgeNumberGenerator = Mockito.mock(BridgeRandomNumberGenerator.class);
     private final BridgeMaker bridgeMaker = new BridgeMaker(bridgeNumberGenerator);
-
-    @DisplayName("3에서 20사이에 다리 길이 이외의 입력을 받은 경우 예외")
-    @ParameterizedTest
-    @CsvSource({
-            "2", "1", "0", "21", "22", "100", "-1"
-    })
-    void throwExceptionWhenInputNotIncludeFromThreeToTwenty(int input) {
-        assertThatThrownBy(() -> bridgeMaker.makeBridge(input)).isInstanceOf(IllegalArgumentException.class);
-    }
 
     @DisplayName("랜덤 생성기가 1, 0 이외에 잘못된 값을 리턴한 경우")
     @Test
