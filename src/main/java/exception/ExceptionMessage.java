36729307d763c28866964edabf9c0c643f9e3012
“rotomoo” <quddlf410@naver.com>
2022-11-21 21:35:37 +0900

    feat(ExceptionMessage) : 각 예외 메시지 Enum 구현

--- src/main/java/exception/ExceptionMessage.java
+++ src/main/java/exception/ExceptionMessage.java
@@ -1,7 +1,9 @@
 package exception;
 
 public enum ExceptionMessage {
-    ;
+    INVALID_BRIDGE_SIZE("잘못된 입력 입니다. 다리 길이는 3부터 20 사이의 숫자여야 합니다."),
+    INVALID_MOVE("잘못된 입력 입니다. U(위), D(아래)를 입력해 주세요."),
+    INVALID_INPUT("잘못된 입력 입니다. 알맞게 입력해 주세요.");
 
     private String message;
 
@@ -10,6 +12,6 @@ public enum ExceptionMessage {
     }
 
     public String getMessage() {
-        return message;
+        return "[ERROR] " + message;
     }
 }
