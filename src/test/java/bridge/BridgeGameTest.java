02bb9d1d66b2342201ce002bb4ad5b8d7b854573
ji-mango <susie000301@naver.com>
2022-11-21 23:54:53 +0900

    refactor: BridgeGameTest 반복코드를 ParameterizedTest로 리팩토링

--- src/test/java/bridge/BridgeGameTest.java
+++ src/test/java/bridge/BridgeGameTest.java
@@ -1,28 +1,30 @@
 package bridge;
 
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
-
-import java.util.List;
-
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
-class BridgeGameTest {
-    @DisplayName("다리가 있는 곳으로 이동한 결과가 제대로 나타나는지 테스트")
-    @Test
-    void right_move_test() {
-        BridgeGame bridgeGame = new BridgeGame();
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
-        assertThat(bridgeGame.move(List.of("D", "U", "D", "D"), "D", 0))
-                .isEqualTo(" O ");
+import java.util.Arrays;
+import java.util.List;
+
+class BridgeGameTest {
+    private static final List<String> BRIDGE = Arrays.asList("D", "U", "D", "D");
+
+    private BridgeGame bridgeGame;
+
+    @BeforeEach
+    void setUp() {
+        bridgeGame = new BridgeGame();
     }
 
-    @DisplayName("다리가 없는 곳으로 이동한 결과가 제대로 나타나는지 테스트")
-    @Test
-    void wrong_move_test() {
-        BridgeGame bridgeGame = new BridgeGame();
-
-        assertThat(bridgeGame.move(List.of("D", "U", "D", "D"), "U", 3))
-                .isEqualTo(" X ");
+    @ParameterizedTest
+    @CsvSource({"D,0,' O '", "U,3,' X '"})
+    void move_test(String moving, int bridgeIndex, String resultSymbol) {
+        assertThat(bridgeGame.move(BRIDGE, moving, bridgeIndex))
+                .isEqualTo(resultSymbol);
     }
 }
