b7f2507cf1da9cc22a482aec84b99d3fee041c55
DevJay1024 <devjay1024@gmail.com>
2022-11-21 22:50:38 +0900

    feat: 사용자의 현재 이동 경로 출력 기능 구현

--- src/main/java/bridge/views/OutputView.java
+++ src/main/java/bridge/views/OutputView.java
@@ -1,14 +1,24 @@
 package bridge.views;
 
+import bridge.identifiers.Direction;
+
+import java.text.MessageFormat;
+import java.util.List;
+import java.util.stream.Collectors;
+
 /**
  * 사용자에게 게임 진행 상황과 결과를 출력하는 역할을 한다.
  */
 public class OutputView {
 
     private static String MSG_WELCOME = "다리 건너기 게임을 시작합니다.";
+    private static String FORMAT_MAP = "[ {0} ]";
+    private static String FORMAT_MAP_DELIMITER = " | ";
+    private static String MSG_CORRECT_DIRECTION = "O";
+    private static String MSG_WRONG_DIRECTION = "X";
     private static String MSG_SEPARATOR = "";
 
-    public void printWelcome(){
+    public void printWelcome() {
         System.out.println(MSG_WELCOME);
         printSeparator();
     }
@@ -18,7 +28,16 @@ public class OutputView {
      * <p>
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void printMap() {
+    public void printMap(List<Direction> playerPath, boolean isGameOver) {
+        List<String> directions = playerPath.stream()
+                .map((element) -> MSG_CORRECT_DIRECTION)
+                .collect(Collectors.toList());
+        if (isGameOver) {
+            directions.set(directions.size() - 1, MSG_WRONG_DIRECTION);
+        }
+        String message = directions.stream()
+                .collect(Collectors.joining(FORMAT_MAP_DELIMITER));
+        System.out.println(MessageFormat.format(FORMAT_MAP, message));
     }
 
     /**
@@ -29,7 +48,7 @@ public class OutputView {
     public void printResult() {
     }
 
-    private void printSeparator(){
+    private void printSeparator() {
         System.out.println(MSG_SEPARATOR);
     }
 }
