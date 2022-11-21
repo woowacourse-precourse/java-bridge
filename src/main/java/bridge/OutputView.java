d5582cadda753505d3d09bd360c7ac8615d2f6ab
kissshot1104 <kissshot.heartunderblade1104@gmail.com>
2022-11-21 17:52:34 +0900

    Feat(OutputView) : 현재 다리 상황 출력 기능 추가
    public void printMap(List<String> upSide, List<String> downSide)
    메소드를 추가했습니다.
    이 메소드는 upSide, downSide에 들어있는 현재 다리 상황을 출력해줍니다.

--- src/main/java/bridge/OutputView.java
+++ src/main/java/bridge/OutputView.java
@@ -1,5 +1,8 @@
 package bridge;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 /**
  * 사용자에게 게임 진행 상황과 결과를 출력하는 역할을 한다.
  */
@@ -10,14 +13,12 @@ public class OutputView {
      * <p>
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void printMap() {
+    public void printMap(List<String> upSide, List<String> downSide) {
+        String upSideBridge = upSide.stream().collect(Collectors.joining(" | ", "[ ", " ]"));
+        String downBridge = downSide.stream().collect(Collectors.joining(" | ", "[ ", " ]"));
+        System.out.println(upSideBridge);
+        System.out.println(downBridge);
     }
 
-    /**
-     * 게임의 최종 결과를 정해진 형식에 맞춰 출력한다.
-     * <p>
-     * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
-     */
-    public void printResult() {
-    }
+
 }
