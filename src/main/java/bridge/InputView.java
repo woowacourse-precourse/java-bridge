c55c922a877dff65ae6f22400003ce3306ebe221
noparamin <noparamin@naver.com>
2022-11-21 20:56:31 +0900

    feat(InputView): 게임 재시도, 종료 여부 입력 기능 구현

--- src/main/java/bridge/InputView.java
+++ src/main/java/bridge/InputView.java
@@ -32,6 +32,9 @@ public class InputView {
      * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
      */
     public String readGameCommand() {
-        return null;
+        String inputCommand = Console.readLine();
+        inputException.validateReadGameCommand(inputCommand);
+
+        return inputCommand;
     }
 }
