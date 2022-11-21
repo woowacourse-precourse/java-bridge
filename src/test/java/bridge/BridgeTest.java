c8d6f65dcb9c2b042207070ed112ce5303d93324
heesang <dlagmltkd12@naver.com>
2022-11-22 06:37:01 +0900

    test: BridgeTest 테스트 중복 로컬 변수를 필드로 리팩토링

--- src/test/java/bridge/BridgeTest.java
+++ src/test/java/bridge/BridgeTest.java
@@ -2,6 +2,7 @@ package bridge;
 
 import bridge.constant.BridgeMark;
 import bridge.constant.GameStatus;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -12,10 +13,16 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class BridgeTest {
 
+    private Bridge bridge;
+
+    @BeforeEach
+    void setUp() {
+        bridge = new Bridge(List.of("U", "D", "U", "D"));
+    }
+
     @DisplayName("다리를 한 라운드 건널 때 게임 계속 가능 결과 반환")
     @Test
     void crossContinueResult() {
-        Bridge bridge = new Bridge(List.of("U", "D", "U", "D"));
         GameStatus gameStatus = bridge.cross(1, BridgeMark.UP);
         assertThat(gameStatus.isContinue()).isTrue();
     }
@@ -23,7 +30,6 @@ public class BridgeTest {
     @DisplayName("다리를 한 라운드 건널 때 게임 실패 결과 반환")
     @Test
     void crossFailResult() {
-        Bridge bridge = new Bridge(List.of("U", "D", "U", "D"));
         GameStatus gameStatus = bridge.cross(3, BridgeMark.DOWN);
         assertThat(gameStatus.isFail()).isTrue();
     }
@@ -31,7 +37,6 @@ public class BridgeTest {
     @DisplayName("다리를 한 라운드이자 마지막 라운드를 건널 때 게임 성공 결과 반환")
     @Test
     void crossSuccessResultInLast() {
-        Bridge bridge = new Bridge(List.of("U", "D", "U", "D"));
         GameStatus gameStatus = bridge.cross(4, BridgeMark.DOWN);
         assertThat(gameStatus.isSuccess()).isTrue();
     }
@@ -39,7 +44,6 @@ public class BridgeTest {
     @DisplayName("다리를 한 라운드이자 마지막 라운드를 건널 때 게임 실패 결과 반환")
     @Test
     void crossFailResultInLast() {
-        Bridge bridge = new Bridge(List.of("U", "D", "U", "D"));
         GameStatus gameStatus = bridge.cross(4, BridgeMark.UP);
         assertThat(gameStatus.isFail()).isTrue();
     }
