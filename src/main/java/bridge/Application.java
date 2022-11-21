aec2e1a5979ac817e530cdf07ea78b8414c2e070
Basak <chgy2131@naver.com>
2022-11-22 01:01:40 +0900

    feat(Application): 예외 발생시 관련 문구를 출력하는 기능 추가

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -2,32 +2,38 @@ package bridge;
 
 import bridge.domain.GameProgress;
 import bridge.domain.GameResult;
+import bridge.view.OutputView;
 
 import java.util.regex.Pattern;
 
 public class Application {
     public static void main(String[] args) {
-        // 객체 생성 및 다리 생성
-        BridgeGame game = new BridgeGame();
+        try {
+            // 객체 생성 및 다리 생성
+            BridgeGame game = new BridgeGame();
 
-        // 게임 진행
-        while (true) {
-            // 길 입력받기
-            game.move();
-            // 결과확인
-            GameResult gameResult = game.getResult();
-            BridgeGame.printGameResult(gameResult);
+            // 게임 진행
+            while (true) {
+                // 길 입력받기
+                game.move();
+                // 결과확인
+                GameResult gameResult = game.getResult();
+                BridgeGame.printGameResult(gameResult);
 
-            // 실패시 재시도할지 확인
-            if (gameResult.getProgress() == GameProgress.FAILURE) {
-                if (!game.retry()) {
+                // 실패시 재시도할지 확인
+                if (gameResult.getProgress() == GameProgress.FAILURE) {
+                    if (!game.retry()) {
+                        break;
+                    }
+                }
+                // 성공시 게임 종료
+                if (gameResult.getProgress() == GameProgress.SUCCESS) {
                     break;
                 }
             }
-            // 성공시 게임 종료
-            if (gameResult.getProgress() == GameProgress.SUCCESS) {
-                break;
-            }
+        }
+        catch (IllegalArgumentException e) {
+            OutputView.printErrorMessage(e.toString());
         }
     }
 }
