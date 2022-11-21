c1bfdffb25860f37efa936af99b64aa3be6609a4
josolha <josolha@nate.com>
2022-11-22 00:04:46 +0900

    feat(InputView) : 시작 재시작 입력 받는 기능 생성

--- src/main/java/bridge/view/InputView.java
+++ src/main/java/bridge/view/InputView.java
@@ -31,6 +31,8 @@ public class InputView {
      * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
      */
     public String readGameCommand() {
-
+        String command;
+        command = Console.readLine().toUpperCase();
+        return command;
     }
 }
