f354599463d9e193db7c3b0d658f54d37338cb3d
eyeofsol <kingjihong@naver.com>
2022-11-21 11:37:21 +0900

    style(BridgeMaker): 띄어쓰기 수정
    throws 부분에 띄어쓰기를 추가해 가독성을 높였습니다.
    
    Before:
    
    private void validate(int number) throws IllegalArgumentException{
    
    After:
    
    private void validate(int number) throws IllegalArgumentException {

--- src/main/java/bridge/BridgeMaker.java
+++ src/main/java/bridge/BridgeMaker.java
@@ -19,7 +19,7 @@ public class BridgeMaker {
      * @return 입력받은 길이에 해당하는 다리 모양. 위 칸이면 "U", 아래 칸이면 "D"로 표현해야 한다.
      * 즉, 랜덤 인풋에서 1이면 U, 0이면 D로 표현한다.
      */
-    public List<String> makeBridge(int size) throws IllegalArgumentException{
+    public List<String> makeBridge(int size) throws IllegalArgumentException {
         List<String> bridge = new ArrayList<>();
         int number;
 
@@ -32,7 +32,7 @@ public class BridgeMaker {
         return bridge;
     }
 
-    private void validate(int number) throws IllegalArgumentException{
+    private void validate(int number) throws IllegalArgumentException {
         if (number < 3 || 20 < number) {
             throw new IllegalArgumentException("[ERROR] 다리길이는 3이상 20이하 입니다.");
         }
