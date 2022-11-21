7acd23ee821294e5445a08c6c0a51dfa3f62f4d4
Young37 <younghk37@gmail.com>
2022-11-21 12:13:05 +0900

    test(util): check if invalid value returns false

---
+++ src/test/java/bridge/util/BridgeGameStandardTest.java
@@ -0,0 +1,24 @@
+package bridge.util;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+class BridgeGameStandardTest {
+    private BridgeGameStandard bridgeGameStandard;
+
+    @BeforeEach
+    void setUp() {
+        bridgeGameStandard = new BridgeGameStandard();
+    }
+
+    @DisplayName("범위를 벗어난 다리 길이에 대해 false를 반환한다.")
+    @ValueSource(ints = { 2, 21, 0 })
+    @ParameterizedTest
+    void underLottoPrice(Integer input) {
+        assertThat(bridgeGameStandard.isValidBridgeSize(input)).isEqualTo(false);
+    }
+}
