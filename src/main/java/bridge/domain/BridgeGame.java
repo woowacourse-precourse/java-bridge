45c8529d56e5951aaca3457c94b7f4da1ea96d2f
박시균\psk98 <psg980331@naver.com>
2022-11-21 14:14:52 +0900

    Feat: 게임이 성공으로 끝났는지 또는 사용자의 움직임을 반환해주는 기능

--- src/main/java/bridge/domain/BridgeGame.java
+++ src/main/java/bridge/domain/BridgeGame.java
@@ -36,6 +36,8 @@ public class BridgeGame {
         isPlayerMoveSuccess = false;
     }
 
+
+
     public void makePrintResultIfUp(int bridgeIdx){
         if(bridgeIdx == 0) {
             if(playerMoveWhetherAnswer.get(bridgeIdx).equals(MoveAnswer.UP_TRUE)) {
@@ -108,6 +110,14 @@ public class BridgeGame {
      * 재시작을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
     public void retry() {
+    }
+
+    public void gameSuccess(){
+        this.isSuccessGame = true;
+    }
+
+    public boolean isMoveSuccess(){
+        return isPlayerMoveSuccess;
     }
 
     public void setBridgeLength(int bridgeLength) {
