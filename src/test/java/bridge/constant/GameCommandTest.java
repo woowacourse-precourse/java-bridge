a0ffa72abb7110b4de023860618bc367d300fd81
heesang <dlagmltkd12@naver.com>
2022-11-21 22:41:10 +0900

    test: R, Q 이외의 입력 값으로 GameCommand 생성 시 예외 발생

--- src/test/java/bridge/constant/GameCommandTest.java
+++ src/test/java/bridge/constant/GameCommandTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class GameCommandTest {
 
@@ -14,5 +15,13 @@ class GameCommandTest {
         GameCommand quit = GameCommand.of("Q");
         assertThat(restart.isRestart()).isTrue();
         assertThat(quit.isQuit()).isTrue();
+    }
+
+    @DisplayName("R, Q 이외의 입력 값으로 GameCommand 생성 시 예외 발생")
+    @Test
+    void createCommandInvalidInput() {
+        assertThatThrownBy(() -> GameCommand.of("d"))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("[ERROR] 게임 재시작/종료 여부 입력의 경우 R과 Q 뿐이어야 합니다.");
     }
 }
