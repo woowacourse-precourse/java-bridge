731d860c93c97b5dd8c82575ed7c15da701cf5d6
yangjiyoung <0112jyoung@naver.com>
2022-11-22 01:17:33 +0900

    feat: 예외 사항 메세지 추가

--- src/main/java/bridge/Validation.java
+++ src/main/java/bridge/Validation.java
@@ -3,14 +3,14 @@ package bridge;
 public class Validation {
     public static void isNotRetryOrQuit(String s){
         if(!s.equals(Constant.RETRY) && !s.equals(Constant.QUIT))
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException(Constant.ERROR+Constant.ERROR_MESSAGE_RETRY_QUIT);
     }
     public static void isNotUpOrDown(String s){
         if(!s.equals(Constant.UP) && !s.equals(Constant.DOWN))
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException(Constant.ERROR+Constant.ERROR_MESSAGE_UP_DOWN);
     }
     public static void isBiggerMove(int userMove, int bridgeSize){
         if(userMove>=bridgeSize)
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException(Constant.ERROR+Constant.ERROR_MESSAGE_LENGTH);
     }
 }
