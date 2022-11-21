0d2f6de8e2be94296f14b253496e15473bc905dd
mckim123 <minchan06@gmail.com>
2022-11-22 03:53:06 +0900

    refactor: split printMap method in OutputView
    Split printMap method of OutputView
    - split features: one generates half maps and another print half maps

--- src/main/java/bridge/view/OutputView.java
+++ src/main/java/bridge/view/OutputView.java
@@ -6,12 +6,18 @@ import java.util.List;
 public class OutputView {
     public void printMap(List<String> map) {
         int mid = map.size() / 2;
-        System.out.println(String.format(PrintMessage.MAP_FORMAT.getString(),
-                String.join(PrintMessage.MAP_SEPARATOR.getString(), map.subList(0, mid))));
-        System.out.println(String.format(PrintMessage.MAP_FORMAT.getString(),
-                String.join(PrintMessage.MAP_SEPARATOR.getString(), map.subList(mid, map.size()))));
+        List<String> upperMap = map.subList(0, mid);
+        List<String> lowerMap = map.subList(mid, map.size());
+        printHalfMap(upperMap);
+        printHalfMap(lowerMap);
         System.out.println();
     }
+
+    private void printHalfMap(List<String> halfMap) {
+        System.out.println(String.format(PrintMessage.MAP_FORMAT.getString(),
+                String.join(PrintMessage.MAP_SEPARATOR.getString(), halfMap)));
+    }
+
     public void printResult(boolean isSuccess, int trials, List<String> map) {
         String resultInKor = PrintMessage.FAIL_IN_KOR.getString();
         if (isSuccess) {
