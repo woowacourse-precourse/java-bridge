0032b8f48761d339e9c0c2a076f8b1ab18570c9f
Amyhds <dasomhan524@gmail.com>
2022-11-21 15:23:21 +0900

    style(errormessage): 상수 이름 변경

--- src/main/java/bridge/ErrorMessage.java
+++ src/main/java/bridge/ErrorMessage.java
@@ -5,7 +5,7 @@ public enum ErrorMessage {
     RANGE_ERROR("[ERROR] 다리 길이는 3부터 20 사이의 숫자여야 합니다."),
     LENGTH_ERROR("[ERROR] 한 개의 문자를 입력해야 합니다."),
     MOVING_ERROR("[ERROR] U(위 칸) 또는 D(아래 칸)를 입력해야 합니다."),
-    GAMECOMMAND_ERROR("[ERROR] R(재시작) 또는 Q(종료)를 입력해야 합니다.");
+    GAME_COMMAND_ERROR("[ERROR] R(재시작) 또는 Q(종료)를 입력해야 합니다.");
 
     private String errorMessage;
 
