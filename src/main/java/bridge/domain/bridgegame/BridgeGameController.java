af23d2ec1929b7f8a89e651de819811b58796061
sanghyuk2 <givemesomefoodplz@gmail.com>
2022-11-22 01:48:19 +0900

    feat: 다리 길이 입력 기능 구현

--- src/main/java/bridge/domain/bridgegame/BridgeGameController.java
+++ src/main/java/bridge/domain/bridgegame/BridgeGameController.java
@@ -1,17 +1,21 @@
 package bridge.domain.bridgegame;
 
 import bridge.domain.BridgeGameStarter;
+import bridge.io.input.InputView;
 import bridge.io.output.OutputView;
 
 public class BridgeGameController implements BridgeGameStarter {
     private final OutputView outputView;
+    private final InputView inputView;
 
     public BridgeGameController() {
+        this.inputView = new InputView();
         this.outputView = new OutputView();
     }
 
     @Override
     public void start() {
         outputView.printStartMessage();
+        inputView.readBridgeSize();
     }
 }
