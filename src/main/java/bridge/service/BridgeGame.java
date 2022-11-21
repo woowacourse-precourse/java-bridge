c676542a2628fff0b05c2ec061aa0d24f7370f2e
baksohyeon <bori12370@gmail.com>
2022-11-22 00:08:43 +0900

    feat: Bridge game 실행 관련 비즈니스 로직 구현체 완성

--- src/main/java/bridge/service/BridgeGame.java
+++ src/main/java/bridge/service/BridgeGame.java
@@ -1,40 +1,108 @@
 package bridge.service;
 
 import bridge.domain.BridgeGameService;
+import bridge.domain.CommandConstant;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 /**
  * 다리 건너기 게임을 관리하는 클래스
  */
 public class BridgeGame implements BridgeGameService {
 
+    public static final String SYMBOL_OF_SUCCESS = "O";
+    public static final String SYMBOL_OF_FAILURE = "X";
+    private final List<String> bridge;
+    private List<String> upStep;
+    private List<String> downStep;
+    private int attemptCount = 0;
 
-
-    /**
-     * 사용자가 칸을 이동할 때 사용하는 메서드
-     * <p>
-     * 이동을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
-     * 필드(인스턴스 변수) 추가 가능
-     * 메서드 이름 변경 불가
-     * 인자, 반환 타입 추가, 변경 가능
-     * 메서드 추가/ 변경 가능
-     * 해당 클래스 내에서 View 사용 불가능
-     */
-
+    public BridgeGame(List<String> bridge) {
+        this.bridge = bridge;
+    }
 
     @Override
     public void move(String step) {
+        this.attemptCount = getAttemptCount() + 1;
+        updateMaps(step);
     }
 
-    /**
-     * 사용자가 게임을 다시 시도할 때 사용하는 메서드
-     * <p>
-     * 재시작을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
-     */
+
     @Override
     public void retry(String command) {
+        this.upStep.clear();
+        this.downStep.clear();
     }
 
+    @Override
+    public List<List<String>> getMaps() {
+        List<List<String>> map = new ArrayList<>();
+        map.add(getUpStep());
+        map.add(getDownStep());
+        return map;
+    }
+
+    // break 조건 1
+    @Override
+    public Boolean isGameSuccess(String step) {
+        boolean checkSize = getUpStep().size() == bridge.size();
+        boolean checkLastStep = isValidLastStep(step);
+        return checkSize && checkLastStep;
+    }
+
+    // break 조건 2
+    @Override
+    public Boolean isValidLastStep(String step) {
+        return Objects.equals(step, bridge.get(bridge.size() - 1));
+    }
+
+
+    public int getAttemptCount() {
+        return attemptCount;
+    }
+
+    private void updateMaps(String step) {
+        String symbol = getSymbolOfTrack(step);
+        if (Objects.equals(step, CommandConstant.UP)) {
+            goToUpStep(symbol);
+        }
+        if (Objects.equals(step, CommandConstant.DOWN)) {
+            goToDownStep(symbol);
+        }
+    }
+
+
+    private boolean isMovable(String step) {
+        int index = getUpStep().size();
+        return Objects.equals(step, bridge.get(index));
+    }
+
+
+    private String getSymbolOfTrack(String currentStep) {
+        if (isMovable(currentStep)) {
+            return SYMBOL_OF_SUCCESS;
+        }
+        return SYMBOL_OF_FAILURE;
+    }
+
+
+    private void goToUpStep(String symbol) {
+        upStep.add(symbol);
+        downStep.add(" ");
+    }
+
+    private void goToDownStep(String symbol) {
+        downStep.add(symbol);
+        upStep.add(" ");
+    }
+
+    private List<String> getUpStep() {
+        return upStep;
+    }
+
+    private List<String> getDownStep() {
+        return downStep;
+    }
 }
