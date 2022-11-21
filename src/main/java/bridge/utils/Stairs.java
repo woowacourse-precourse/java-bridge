5510e93186ca373c767975317ef7902cae650354
bearjun05 <bearjun05@naver.com>
2022-11-21 17:03:12 +0900

    feat : 위, 아래 층 enum 구현 및 랜덤 값을 문자 값으로 변환하는 기능 추가

---
+++ src/main/java/bridge/utils/Stairs.java
@@ -0,0 +1,24 @@
+package bridge.utils;
+
+public enum Stairs {
+    UP_STAIRS(1, "U"),
+    DOWN_STAIRS(0, "D");
+
+    private int randomValue;
+    private String abbreviation;
+
+    Stairs(int randomValue, String abbreviation) {
+        this.randomValue = randomValue;
+        this.abbreviation = abbreviation;
+    }
+
+    public String convertIntToStr(int randomValue) {
+        if(randomValue == Stairs.UP_STAIRS.randomValue){
+            return UP_STAIRS.abbreviation;
+        }
+        if(randomValue == Stairs.DOWN_STAIRS.randomValue) {
+            return DOWN_STAIRS.abbreviation;
+        }
+        throw new IllegalStateException("올바른 랜덤 값이 아닙니다.");
+    }
+}
