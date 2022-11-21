4710b1b6fb7c279e502f35a28d73b244e81fbce6
kissshot1104 <kissshot.heartunderblade1104@gmail.com>
2022-11-21 19:15:56 +0900

    Feat(RetryProcessing) : 게임 다시 시작 기능 추가
    public boolean readRetry()
    메소드를 추가했습니다.
    이 메소드는 게임을 다시 시작하려는 여부를 입력하기 위해 사용됩니다.

--- src/main/java/bridge/RetryProcessing.java
+++ src/main/java/bridge/RetryProcessing.java
@@ -13,8 +13,21 @@ public class RetryProcessing {
         outputView = new OutputView();
     }
 
+    //게임을 다시 시도하기 위해 입력을 받는 메소드다
+    public boolean readRetry() {
+        while (true) {
+            System.out.println("게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)");
+            String input = inputView.readGameCommand();
+            if (input.length() == 0) {
+                continue;
+            }
+            boolean doRetry = inputRetryCommand(input);
+            return doRetry;
+        }
+    }
+
     //최종적으로 입력된 다리의 정보를 정해진 출력 형식으로 가공한다.
-    public void printResult(List<String> upSide, List<String> downSide, int count){
+    public void printResult(List<String> upSide, List<String> downSide, int count) {
         String upSideBridge = upSide.stream().collect(Collectors.joining(" | ", "[ ", " ]"));
         String downSideBridge = downSide.stream().collect(Collectors.joining(" | ", "[ ", " ]"));
         outputView.printResult(upSideBridge, downSideBridge, count);
