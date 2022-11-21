3e1df47052d70f7ae5777630e83cbad4812761ff
eunseon <agseon9@gmail.com>
2022-11-22 06:21:32 +0900

    refactor : 에러 메시지 상수화 변경

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -10,6 +10,11 @@ public class InputView {
 
     private static final int MAX_BRIDGE_SIZE = 20;
     private static final int MIN_BRIDGE_SIZE = 3;
+    private static final String OVER_SIZE_ERROR_MESSAGE = "[ERROR] 다리 길이는 3부터 20 사이의 숫자여야 합니다.";
+    private static final String IS_DIGIT_ERROR_MESSAGE = "[ERROR] 다리 길이는 숫자만 입력 가능합니다.";
+    private static final String EMPTY_ERROR_MESSAGE = "[ERROR] 공백은 입력할 수 없습니다.";
+    private static final String UP_AND_DOWN_ERROR_MESSAGE = "[ERROR] 이동하는 칸은 U 혹은 D 으로만 선택 가능합니다.";
+    private static final String RESTART_OR_QUIT_ERROR_MESSAGE = "[ERROR] 게임 종료 후에는 R 또는 Q만 입력 가능합니다.";
 
     /**
      * 다리의 길이를 입력받는다.
@@ -78,33 +83,35 @@ public class InputView {
 
     public void validateOverSize(int bridgeSize) {
         if (bridgeSize < MIN_BRIDGE_SIZE || bridgeSize > MAX_BRIDGE_SIZE) {
-            throw new IllegalArgumentException("[ERROR] 다리 길이는 3부터 20 사이의 숫자여야 합니다.");
+            throw new IllegalArgumentException(OVER_SIZE_ERROR_MESSAGE);
         }
     }
 
     public void validateisDigit(String bridgeSize) {
         for (int i = 0; i < bridgeSize.length(); i++) {
             if (!Character.isDigit(bridgeSize.charAt(i))) {
-                throw new IllegalArgumentException("[ERROR] 다리 길이는 숫자만 입력 가능합니다.");
+                throw new IllegalArgumentException(IS_DIGIT_ERROR_MESSAGE);
             }
         }
     }
 
     public void validateisEmpty(String input) {
         if (input.isBlank()) {
-            throw new IllegalArgumentException("[ERROR] 공백은 입력할 수 없습니다.");
+            throw new IllegalArgumentException(EMPTY_ERROR_MESSAGE);
         }
     }
 
     public void validateUpOrDown(String moving) {
-        if (!moving.equals(BridgeGameMark.UP_MARK.getMark()) && !moving.equals(BridgeGameMark.DOWN_MARK.getMark())) {
-            throw new IllegalArgumentException("[ERROR] 이동하는 칸은 U 혹은 D 으로만 선택 가능합니다.");
+        if (!moving.equals(BridgeGameMark.UP_MARK.getMark())
+                && !moving.equals(BridgeGameMark.DOWN_MARK.getMark())) {
+            throw new IllegalArgumentException(UP_AND_DOWN_ERROR_MESSAGE);
         }
     }
 
     public void validateRestartOrQuit(String doRestart) {
-        if (!doRestart.equals(BridgeGameMark.RETRY_MARK.getMark()) && !doRestart.equals(BridgeGameMark.QUIT_MARK.getMark())) {
-            throw new IllegalArgumentException("[ERROR] 게임 종료 후에는 R 또는 Q만 입력 가능합니다.");
+        if (!doRestart.equals(BridgeGameMark.RETRY_MARK.getMark())
+                && !doRestart.equals(BridgeGameMark.QUIT_MARK.getMark())) {
+            throw new IllegalArgumentException(RESTART_OR_QUIT_ERROR_MESSAGE);
         }
     }
 }
