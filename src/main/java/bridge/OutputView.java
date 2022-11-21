cdc3dac287e42577b83c24089dc1273f0c6556a1
poter0501 <poter0501@gmail.com>
2022-11-22 00:26:36 +0900

    feat(OutputView): 현재까지 이동한 다리의 상태를 출력하는 기능 구현

--- src/main/java/bridge/OutputView.java
+++ src/main/java/bridge/OutputView.java
@@ -1,5 +1,11 @@
 package bridge;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Stream;
+
 /**
  * 사용자에게 게임 진행 상황과 결과를 출력하는 역할을 한다.
  */
@@ -10,9 +16,12 @@ public class OutputView {
      * <p>
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void printMap() {
+    public void printMap(HashMap<String, List<String>> path) {
+        String upPath = String.join(" | ", path.get("U")).replaceAll("N"," ");
+        String downPath = String.join(" | ", path.get("D")).replaceAll("N"," ");
+        System.out.println("[ "+ upPath+" ]");
+        System.out.println("[ "+ downPath+" ]");
     }
-
     /**
      * 게임의 최종 결과를 정해진 형식에 맞춰 출력한다.
      * <p>
