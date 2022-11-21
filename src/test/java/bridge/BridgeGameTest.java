d0e441d7c163d966448a9ce7592f3f323a0ef1f1
hyungkya <hyungkya@uci.edu>
2022-11-21 16:29:29 +0900

    test: 이동 진행 상태 테스트 케이스 추가

--- src/test/java/bridge/BridgeGameTest.java
+++ src/test/java/bridge/BridgeGameTest.java
@@ -30,4 +30,52 @@ public class BridgeGameTest {
         assertThatThrownBy(() -> bridgeGame.move("u"))
                 .isInstanceOf(IllegalArgumentException.class);
     }
+
+    @DisplayName("정상적인 경우 이동 진행 상태가 추가가 되어야 한다. - 1")
+    @Test
+    void validMoveCase1() {
+        BridgeGame bridgeGame = new BridgeGame(List.of("U", "D", "D"));
+        assertThat(bridgeGame.move("U")).isEqualTo(true);
+    }
+
+    @DisplayName("정상적인 경우 이동 진행 상태가 추가가 되어야 한다. - 2")
+    @Test
+    void validMoveCase2() {
+        BridgeGame bridgeGame = new BridgeGame(List.of("U", "D", "D"));
+        bridgeGame.move("U");
+        bridgeGame.move("D");
+        assertThat(bridgeGame.getUserPath()).isEqualTo(List.of("U", "D"));
+    }
+
+    @DisplayName("게임 종료 조건 (게임 성공) 시 이동이 되어선 안된다.")
+    @Test
+    void invalidMoveAfterGameOverWin() {
+        BridgeGame bridgeGame = new BridgeGame(List.of("U", "D", "D"));
+        assertThat(bridgeGame.move("U")).isEqualTo(true);
+        assertThat(bridgeGame.move("D")).isEqualTo(true);
+        assertThat(bridgeGame.move("D")).isEqualTo(true);
+        assertThat(bridgeGame.move("U")).isEqualTo(false);
+    }
+
+    @DisplayName("게임 종료 조건 (게임 실패) 시 이동이 되어선 안된다.")
+    @Test
+    void invalidMoveAfterGameOverLose() {
+        BridgeGame bridgeGame = new BridgeGame(List.of("U", "D", "D"));
+        assertThat(bridgeGame.move("U")).isEqualTo(true);
+        assertThat(bridgeGame.move("U")).isEqualTo(true);
+        assertThat(bridgeGame.move("U")).isEqualTo(false);
+    }
+
+    @DisplayName("잘못된 이동 시도들이 추가가 되어선 안된다.")
+    @Test
+    void userPathAfterInvalidMoveAttempts() {
+        BridgeGame bridgeGame = new BridgeGame(List.of("U", "D", "D"));
+        bridgeGame.move("U");
+        bridgeGame.move("D");
+        bridgeGame.move("U"); // 패배, 마지막 이동이 되어야 함.
+        bridgeGame.move("U");
+        bridgeGame.move("U");
+        bridgeGame.move("U");
+        assertThat(bridgeGame.getUserPath()).isEqualTo(List.of("U", "D", "U"));
+    }
 }
