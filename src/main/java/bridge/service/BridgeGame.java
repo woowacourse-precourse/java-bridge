7e9cb4aeb2942914bb15c41967276a5f3cb6df0d
jw0293 <k38836919@gmail.com>
2022-11-21 13:31:03 +0900

    feat : Basic Game Management [기본적인 게임 관리 기능]

--- src/main/java/bridge/service/BridgeGame.java
+++ src/main/java/bridge/service/BridgeGame.java
@@ -1,4 +1,6 @@
-package bridge;
+package bridge.service;
+
+import bridge.view.BridgePainting;
 
 import java.util.List;
 
@@ -7,14 +9,39 @@ import java.util.List;
  */
 public class BridgeGame {
 
+    private int attemp = 1;
+    private final BridgeInit bridgeInit;
+    private final BridgePainting bridgePainting;
+    private List<String> bridge;
+
+    public BridgeGame(BridgeInit bridgeInit, BridgePainting bridgePainting){
+        this.bridgeInit = bridgeInit;
+        this.bridgePainting = bridgePainting;
+    }
+
+    public void gameInit(){
+        bridge = bridgeInit.makeGame();
+    }
+
+    public List<String> getBridge(){
+        return bridge;
+    }
+
+    public BridgeInit getBridgeInit(){
+        return bridgeInit;
+    }
+
+    public BridgePainting getBridgePainting(){
+        return bridgePainting;
+    }
     /**
      * 사용자가 칸을 이동할 때 사용하는 메서드
      * <p>
      * 이동을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public boolean move(List<String> bridge, int nextIndex, String nextStep) {
-        if(bridge.get(nextIndex).equals(nextStep)) return false;
-        return true;
+    public boolean move(int nextIndex, String nextStep) {
+        if(bridge.get(nextIndex).equals(nextStep)) return true;
+        return false;
     }
 
     /**
@@ -23,5 +50,6 @@ public class BridgeGame {
      * 재시작을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
     public void retry() {
+        bridgePainting.init();
     }
 }
