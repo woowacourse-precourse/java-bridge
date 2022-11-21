504b9e05a0ee9a9252509e84af28d19160e3451c
song-wooseok <sws981202@gmail.com>
2022-11-21 12:59:42 +0900

    feat: 주어진 랜덤 숫자에 맞게 다리 정답 반환 기능

--- src/main/java/bridge/utils/Constants.java
+++ src/main/java/bridge/utils/Constants.java
@@ -1,5 +1,8 @@
 package bridge.utils;
 
+import java.util.Arrays;
+import java.util.Optional;
+
 public class Constants {
 
     public static class BridgeSize{
@@ -8,5 +11,32 @@ public class Constants {
         public static final String ERROR_BRIDGE_SIZE_MESSAGE = "[ERROR] 옳바른 다리의 길이를 입력해주세요.";
 
         public static final String BRIDGE_SIZE_REGEX = "^[3-9]{1}$|^1[0-9]{1}$|^20$";
+    }
+
+    public enum BridgeRoute{
+        UP(1, "U"),
+        DOWN(0, "D");
+
+        private final int routeCode;
+        private final String routh;
+
+        BridgeRoute(int routeCode, String routh){
+            this.routeCode = routeCode;
+            this.routh = routh;
+        }
+
+        public static Optional<BridgeRoute> getMatchRoute(int routeCode){
+            return Arrays.stream(BridgeRoute.values())
+                    .filter(route -> route.getRouteCode() == routeCode)
+                    .findAny();
+        }
+
+        private int getRouteCode(){
+            return this.routeCode;
+        }
+
+        public String getRouth(){
+            return this.routh;
+        }
     }
 }
