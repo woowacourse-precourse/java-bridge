521ca51db8a230264fddcdbc7307dd26fd60e7c1
wnsgh12s <junho1273@gmail.com>
2022-11-21 16:46:05 +0900

    Feat: 이동할 칸의 입력 값 검증 기능 추가
    입력값이 U나 D가 아니면 에러를 발생시키는 기능을 추가했다.

--- src/main/java/bridge/domain/ValidationUtil.java
+++ src/main/java/bridge/domain/ValidationUtil.java
@@ -1,11 +1,21 @@
 package bridge.domain;
 
+import java.util.Objects;
+
 public class ValidationUtil {
     private static final String INVAILED_SIZE = "[ERROR] 다리 길이는 3부터 20 사이의 숫자여야 합니다.";
+    private static final String INVAILED_MOVE = "[ERROR] U 또는 D로 입력해야 합니다.";
 
-    public static void bridgeSizeValidation(int bridgeSize){
-        if(bridgeSize > 20 || bridgeSize < 3){
+    public static void bridgeSizeValidation(int bridgeSize) {
+        if (bridgeSize > 20 || bridgeSize < 3) {
             throw new IllegalArgumentException(INVAILED_SIZE);
         }
     }
+
+    public static void movedValidation(String move) {
+        if(!Objects.equals(move, Constant.MOVED_POSITION_UP) && !Objects.equals(move, Constant.MOVED_POSITION_DOWN)){
+            throw new IllegalArgumentException(INVAILED_MOVE);
+        }
+    }
+
 }
