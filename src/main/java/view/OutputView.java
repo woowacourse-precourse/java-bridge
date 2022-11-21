3e6114e726217b8f7b0ce469c9a55a89730e6117
nylonmusk <106235834+nylonmusk@users.noreply.github.com>
2022-11-21 20:12:34 +0900

    refactor(OutputView): 가독성 향상을 위해 Constant를 클래스변수로 선언

--- src/main/java/view/OutputView.java
+++ src/main/java/view/OutputView.java
@@ -1,10 +1,30 @@
 package view;
 
+import constant.BothSideConstant;
+import constant.NumberConstant;
 import constant.PrintConstant;
 import java.util.List;
+import util.validator.BridgeGameValidator;
 
 public class OutputView {
     private static final OutputView outputView = new OutputView();
+
+    private static final int FIRST_INDEX = NumberConstant.FIRST_INDEX.getConstant();
+    private static final int UP_SIDE_INDEX = BothSideConstant.UP_SIDE_INDEX.getConstant();
+    private static final int DOWN_SIDE_INDEX = BothSideConstant.DOWN_SIDE_INDEX.getConstant();
+    private static final String START_GAME = View.START_GAME.message();
+    private static final String REQUEST_BRIDGE_SIZE = View.REQUEST_BRIDGE_SIZE.message();
+    private static final String REQUEST_MOVE = View.REQUEST_MOVE.message();
+    private static final String REQUEST_RETRY_OR_QUIT = View.REQUEST_RETRY_OR_QUIT.message();
+    private static final String FINAL_RESULT = View.FINAL_RESULT.message();
+    private static final String WIN_FAIL_STATUS = View.WIN_FAIL_STATUS.message();
+    private static final String TOTAL_TRY_NUMBER = View.TOTAL_TRY_NUMBER.message();
+    private static final String BLANK = View.BLANK.message();
+    private static final String WIN = PrintConstant.WIN.getConstant();
+    private static final String FAIL = PrintConstant.FAIL.getConstant();
+    private static final String START_SQUARE_BRACKET = PrintConstant.START_SQUARE_BRACKET.getConstant();
+    private static final String END_SQUARE_BRACKET = PrintConstant.END_SQUARE_BRACKET.getConstant();
+    private static final String DELIMETER = PrintConstant.DELIMETER.getConstant();
 
     private OutputView() {
     }
@@ -14,12 +34,12 @@ public class OutputView {
     }
 
     public static void printStart() {
-        System.out.println(View.START_GAME.message());
+        System.out.println(START_GAME);
         insertLineBreak();
     }
 
     public void printRequestBridgeSize() {
-        System.out.println(View.REQUEST_BRIDGE_SIZE.message());
+        System.out.println(REQUEST_BRIDGE_SIZE);
     }
 
     public void printBridgeSize(int size) {
@@ -28,7 +48,7 @@ public class OutputView {
     }
 
     public void printRequestMove() {
-        System.out.println(View.REQUEST_MOVE.message());
+        System.out.println(REQUEST_MOVE);
     }
 
     public void printUserPath(String path) {
@@ -36,7 +56,7 @@ public class OutputView {
     }
 
     public void printRequestRetryOrQuit() {
-        System.out.println(View.REQUEST_RETRY_OR_QUIT.message());
+        System.out.println(REQUEST_RETRY_OR_QUIT);
     }
 
     public void printUserRetryOrQuit(String retryOrQuit) {
@@ -44,23 +64,23 @@ public class OutputView {
     }
 
     private void printFinalResult() {
-        System.out.println(View.FINAL_RESULT.message());
+        System.out.println(FINAL_RESULT);
     }
 
     public void printMap(List<List<String>> bothSide) {
         printStartSquareBracket();
-        printSide(bothSide.get(1));
+        printSide(bothSide.get(UP_SIDE_INDEX));
         printEndSquareBracket();
 
         printStartSquareBracket();
-        printSide(bothSide.get(0));
+        printSide(bothSide.get(DOWN_SIDE_INDEX));
         printEndSquareBracket();
 
         insertLineBreak();
     }
 
     private void printSide(List<String> side) {
-        for (int index = 0; index < side.size(); index++) {
+        for (int index = FIRST_INDEX; index < side.size(); index++) {
             System.out.print(side.get(index));
             if (index < lastIndex(side)) {
                 printDelimeter();
@@ -85,42 +105,40 @@ public class OutputView {
     }
 
     private void printStartSquareBracket() {
-        System.out.print(PrintConstant.START_SQUARE_BRACKET.getConstant());
+        System.out.print(START_SQUARE_BRACKET);
     }
 
     private void printDelimeter() {
-        System.out.print(PrintConstant.DELIMETER.getConstant());
+        System.out.print(DELIMETER);
     }
 
     private void printEndSquareBracket() {
-        System.out.println(PrintConstant.END_SQUARE_BRACKET.getConstant());
+        System.out.println(END_SQUARE_BRACKET);
     }
 
     private boolean isWin(List<List<String>> bothSide) {
-        return !bothSide.get(0).contains(PrintConstant.WRONG_PATH.getConstant())
-                && !bothSide.get(1).contains(PrintConstant.WRONG_PATH.getConstant());
+        return !BridgeGameValidator.isContainsWrongPath(bothSide);
     }
 
     private boolean isFail(List<List<String>> bothSide) {
-        return bothSide.get(0).contains(PrintConstant.WRONG_PATH.getConstant())
-                || bothSide.get(1).contains(PrintConstant.WRONG_PATH.getConstant());
+        return BridgeGameValidator.isContainsWrongPath(bothSide);
     }
 
     private void printWin() {
-        System.out.print(View.WIN_FAIL_STATUS.message());
-        System.out.print(View.BLANK.message());
-        System.out.println(PrintConstant.WIN.getConstant());
+        System.out.print(WIN_FAIL_STATUS);
+        System.out.print(BLANK);
+        System.out.println(WIN);
     }
 
     private void printFail() {
-        System.out.print(View.WIN_FAIL_STATUS.message());
-        System.out.print(View.BLANK.message());
-        System.out.println(PrintConstant.FAIL.getConstant());
+        System.out.print(WIN_FAIL_STATUS);
+        System.out.print(BLANK);
+        System.out.println(FAIL);
     }
 
     public void printTryNumber(int tryNumber) {
-        System.out.print(View.TOTAL_TRY_NUMBER.message());
-        System.out.print(View.BLANK.message());
+        System.out.print(TOTAL_TRY_NUMBER);
+        System.out.print(BLANK);
         System.out.println(tryNumber);
     }
 
