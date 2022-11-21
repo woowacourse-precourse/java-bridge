2bd617ffcd93d5c5aee52f43f8ae03db8da87ae3
fnzl54 <rnjscksdud1@gmail.com>
2022-11-21 19:54:28 +0900

    feat: cretae printResult method

--- src/main/java/bridge/OutputView.java
+++ src/main/java/bridge/OutputView.java
@@ -28,6 +28,14 @@ public class OutputView {
      * <p>
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void printResult() {
+    public void printResult(boolean nowState, int gameCount) {
+        if (nowState) {
+            System.out.println("게임 성공 여부: 성공");
+            System.out.println("총 시도한 횟수: " + gameCount);
+        }
+        if (!nowState) {
+            System.out.println("게임 성공 여부: 성공");
+            System.out.println("총 시도한 횟수: " + gameCount);
+        }
     }
 }
