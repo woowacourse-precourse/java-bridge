8c646a64904640723c95de4dae04ba45d82f0adf
ONE <sodlapdlf13@gmail.com>
2022-11-21 15:28:34 +0900

    feat(OutputView): 다리 길이 입력 기능 추가

--- src/main/java/bridge/view/InputView.java
+++ src/main/java/bridge/view/InputView.java
@@ -1,5 +1,8 @@
 package bridge.view;
 
+import static bridge.view.OutputView.*;
+import static camp.nextstep.edu.missionutils.Console.*;
+
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
  */
@@ -9,7 +12,13 @@ public class InputView {
      * 다리의 길이를 입력받는다.
      */
     public int readBridgeSize() {
-        return 0;
+        try {
+            printBridgeSizeInput();
+            String size = readLine();
+            return Integer.parseInt(size);
+        } catch (IllegalArgumentException e) {
+            throw new IllegalArgumentException("[ERROR] 다리의 길이에 대한 입력이 잘못되었습니다.");
+        }
     }
 
     /**
