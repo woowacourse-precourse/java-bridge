e7485944082aaf0777e781206a9c603c13b4b787
gyuturn <rbtjrrbals@gmail.com>
2022-11-21 21:35:20 +0900

    feat($bridge): main함수 구현

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -1,8 +1,11 @@
 package bridge;
 
+import controller.MainController;
+
 public class Application {
 
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        MainController mainController = new MainController();
+        mainController.run();
     }
 }
