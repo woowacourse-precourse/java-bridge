761bcbad9c62d31dc8c25d09f990f57a4630a37e
leesangjin <tkdwls0628@gmail.com>
2022-11-21 20:07:54 +0900

    refactor: OutputView에서 모든 출력문을 담당하도록 변경

--- src/main/java/bridge/view/InputView.java
+++ src/main/java/bridge/view/InputView.java
@@ -7,26 +7,22 @@ import camp.nextstep.edu.missionutils.Console;
 
 public class InputView {
 
-    private static final String REQUEST_BRIDGE_SIZE = "다리의 길이를 입력해주세요.";
-    private static final String REQUEST_MOVING = "이동할 칸을 선택해주세요. (위: U, 아래: D)";
-    private static final String REQUEST_GAME_COMMAND = "게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)";
-
     public static int readBridgeSize() {
-        System.out.println(REQUEST_BRIDGE_SIZE);
+        OutputView.requestBridgeSize();
         String input = Console.readLine();
         BridgeSizeValidator.validateBridgeSize(input);
         return Integer.parseInt(input);
     }
 
     public static String readMoving() {
-        System.out.println(REQUEST_MOVING);
+        OutputView.requestMoving();
         String input = Console.readLine();
         MovingValidator.validateMoving(input);
         return input;
     }
 
     public static String readGameCommand() {
-        System.out.println(REQUEST_GAME_COMMAND);
+        OutputView.requestGameCommand();
         String input = Console.readLine();
         GameCommandValidator.validateGameCommand(input);
         return input;
--- src/main/java/bridge/view/OutputView.java
+++ src/main/java/bridge/view/OutputView.java
@@ -1,10 +1,13 @@
 package bridge.view;
 
 import bridge.constant.Constant;
-
 import java.util.List;
 
 public class OutputView {
+
+    private static final String REQUEST_BRIDGE_SIZE = "다리의 길이를 입력해주세요.";
+    private static final String REQUEST_MOVING = "이동할 칸을 선택해주세요. (위: U, 아래: D)";
+    private static final String REQUEST_GAME_COMMAND = "게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)";
 
     private static final String START_BRIDGE_GAME = "다리 건너기 게임을 시작합니다.";
     private static final String RESULT_OF_GAME = "최종 게임 결과";
@@ -13,6 +16,18 @@ public class OutputView {
 
     public static void printStartGame() {
         System.out.println(START_BRIDGE_GAME);
+    }
+
+    public static void requestBridgeSize() {
+        System.out.println(REQUEST_BRIDGE_SIZE);
+    }
+
+    public static void requestMoving() {
+        System.out.println(REQUEST_MOVING);
+    }
+
+    public static void requestGameCommand() {
+        System.out.println(REQUEST_GAME_COMMAND);
     }
 
     public static void printMap(List<List<String>> map) {
