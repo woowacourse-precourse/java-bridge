d8a8f17ad2361f42c7bf6d894abea46ff04a0c07
wonwoo6y <wonwoo6y@gmail.com>
2022-11-21 20:53:07 +0900

    feat(main): 바뀐 플로우차트를 기준으로 재설계

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -1,25 +1,31 @@
 package bridge;
 
 
+import java.util.List;
+
 public class Application {
 
     public static void main(String[] args) {
-        BridgeController bridgeController = new BridgeController(new InputView(), new OutputView());
-        bridgeController.start();
-        crossBridge(bridgeController);
+        BridgeGame bridgeGame = new BridgeGame(new InputView(), new OutputView());
+        runBridgeGame(bridgeGame);
     }
 
-    private static void crossBridge(BridgeController controller) {
-        BridgeBluePrint bluePrint = controller.makeBridgeBluePrint();
-        while (controller.isReady()) {
-            move(controller);
+    private static void runBridgeGame(BridgeGame bridgeGame) {
+        BridgeResult result = bridgeGame.makeBridgeResult();
+        List<String> bridge = bridgeGame.makeBridge();
+        boolean ready = true;
+        while (ready) {
+            ready = crossBridge(bridgeGame, bridge, result);
         }
+        bridgeGame.exit();
     }
 
-     private static void move(BridgeController controller) {
-        boolean done = false;
-        while (!done) {
-
+    private static boolean crossBridge(BridgeGame bridgeGame, List<String> bridge, BridgeResult result) {
+        bridgeGame.retry(result);
+        boolean block = false;
+        while (!block) {
+            block = bridgeGame.move(bridge, result);
         }
+        bridgeGame.endCycle(result);
     }
 }
