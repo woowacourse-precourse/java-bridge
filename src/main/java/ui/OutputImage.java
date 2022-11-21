dda7e61d64d60a3a02ef837fd6a02cea8f5841f8
thousandcows <milcamboo@gmail.com>
2022-11-21 20:40:05 +0900

    feat: create enum class for printMap method

---
+++ src/main/java/ui/OutputImage.java
@@ -0,0 +1,15 @@
+package ui;
+
+public enum OutputImage {
+
+    START("[ "),
+    END(" ]"),
+    SEPARATE(" | "),
+    CORRECT("O"),
+    WRONG("X"),
+    NOT_SELECTED(" ");
+
+    OutputImage(String s) {
+    }
+
+}
