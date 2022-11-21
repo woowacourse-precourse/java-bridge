d5904a66d9c054365e2479adcb09560546bf261b
EUNSH1 <tkdgus968@naver.com>
2022-11-21 12:17:39 +0900

    feat : 사용자가 이동한 칸을 확인하여 리스트에 저장

--- src/main/java/bridge/BridgeGame.java
+++ src/main/java/bridge/BridgeGame.java
@@ -1,16 +1,41 @@
 package bridge;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.Collectors;
+
 /**
  * 다리 건너기 게임을 관리하는 클래스
  */
 public class BridgeGame {
+
+    private static final String RETRY_COMMAND = "R";
+
+    private final List<String> bridge;
+    private final List<String> userAnswer = new ArrayList<>();
+    private int numberOfTry = 1;
+
+    public BridgeGame(List<String> bridge) {
+        this.bridge = bridge;
+    }
 
     /**
      * 사용자가 칸을 이동할 때 사용하는 메서드
      * <p>
      * 이동을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void move() {
+    public boolean move(String location) {
+        int compareLocation = userAnswer.size();
+        userAnswer.add(location);
+        return bridge.get(compareLocation).equals(location);
+    }
+
+    public List<BridgeEnum> currentBridge() {
+        AtomicInteger startBridge = new AtomicInteger();
+        return userAnswer.stream()
+                .map(location -> BridgeEnum.createBridgeStringType(location, bridge.get(startBridge.getAndIncrement()).equals(location)))
+                .collect(Collectors.toList());
     }
 
     /**
