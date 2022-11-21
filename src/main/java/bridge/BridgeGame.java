02f21ddbbf7e16040f7296b7bde9d6ca10de6b85
osm <osm0822@naver.com>
2022-11-21 22:46:04 +0900

    feat(BridgeGame) : 이동 구현

--- src/main/java/bridge/BridgeGame.java
+++ src/main/java/bridge/BridgeGame.java
@@ -22,9 +22,25 @@ public class BridgeGame {
     /**
      * 사용자가 칸을 이동할 때 사용하는 메서드
      * <p>
-     * 이동을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
+     * 이동할 방향을 문자열로 받아 이동
      */
-    public void move() {
+    public void move(String dir) {
+        BridgeGameStstus nextStatus = calcNextStatus(dir);
+        if(nextStatus == BridgeGameStstus.PROCEEDING){
+            step++;
+            return;
+        }
+        status = nextStatus;
+    }
+
+    private BridgeGameStstus calcNextStatus(String dir){
+        if(bridge.get(step).equals(dir)) {
+            if(step + 1 >= bridge.size()){
+                return BridgeGameStstus.SUCCESS;
+            }
+            return BridgeGameStstus.PROCEEDING;
+        }
+        return BridgeGameStstus.FAIL;
     }
 
     /**
