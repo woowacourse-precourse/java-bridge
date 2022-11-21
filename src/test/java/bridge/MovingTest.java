0a9e2b766fbc49114de1697197400129b42a796b
은지 <midzy0228@gmail.com>
2022-11-21 20:58:23 +0900

    test(Moving): 움직이는 방향이 U또는 D가 아닐시 예외테스트 추가

---
+++ src/test/java/bridge/MovingTest.java
@@ -0,0 +1,17 @@
+package bridge;
+
+import bridge.model.Moving;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+
+class MovingTest {
+    @DisplayName("움직이는 값이 U 또는 D가 아닐시 예외가 발생한다.")
+    @Test
+    void createGivenNotUOrD() {
+        assertThatThrownBy(() -> new Moving("NOT"))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+}
