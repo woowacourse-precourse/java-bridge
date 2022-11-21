64ef7f24657e57b7d311f346c9c0f12b5d25b11c
yj2980 <gum0945@naver.com>
2022-11-21 22:18:18 +0900

    feat : 결과값을 저장하는 함수 추가

--- src/main/java/bridge/BridgeResult.java
+++ src/main/java/bridge/BridgeResult.java
@@ -1,2 +1,26 @@
-package bridge;public class BridgeResult {
+package bridge;
+
+public class BridgeResult {
+	private String result;
+	private int count;
+	private boolean isClear;
+
+
+	public BridgeResult(String result, int count, boolean isClear) {
+		this.result = result;
+		this.count = count;
+		this.isClear = isClear;
+	}
+
+	public String getResult() {
+		return result;
+	}
+
+	public int getCount() {
+		return count;
+	}
+
+	public boolean isClear() {
+		return isClear;
+	}
 }
