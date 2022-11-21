cd197265db14e395880fcd30d2098b55f367186d
jipyo0106 <68218081+jipyo0106@users.noreply.github.com>
2022-11-21 17:37:58 +0900

    refactor: 예외 메세지 분리

--- src/main/java/bridge/controller/InputException.java
+++ src/main/java/bridge/controller/InputException.java
@@ -1,23 +1,22 @@
 package bridge.controller;
 
+import bridge.BridgeException;
 import bridge.domain.GameKeySet;
 
 public class InputException {
-    private static final String ERROR_TOKEN = "[ERROR]";
-
     void raiseBridgeSizeValidityException() throws IllegalArgumentException {
-        throw new IllegalArgumentException(ERROR_TOKEN + " 다리 길이는 3이상 20이하의 값만 입력 가능합니다.");
+        throw new IllegalArgumentException(BridgeException.TOKEN.getMessage() + " 다리 길이는 3이상 20이하의 값만 입력 가능합니다.");
     }
 
     void raiseUserMoveValidityException() throws IllegalArgumentException {
-        throw new IllegalArgumentException(ERROR_TOKEN + " " + GameKeySet.UP.getKeySet() + "  혹은 " + GameKeySet.DOWN.getKeySet() + " 문자만 입력 가능합니다.");
+        throw new IllegalArgumentException(BridgeException.TOKEN.getMessage() + " " + GameKeySet.UP.getKeySet() + "  혹은 " + GameKeySet.DOWN.getKeySet() + " 문자만 입력 가능합니다.");
     }
 
     void raiseRetryOrQuitValidityException() throws IllegalArgumentException {
-        throw new IllegalArgumentException(ERROR_TOKEN + " " + GameKeySet.RESTART.getKeySet() + "혹은 " + GameKeySet.QUIT.getKeySet() + " 문자만 입력 가능합니다.");
+        throw new IllegalArgumentException(BridgeException.TOKEN.getMessage() + " " + GameKeySet.RESTART.getKeySet() + "혹은 " + GameKeySet.QUIT.getKeySet() + " 문자만 입력 가능합니다.");
     }
 
     void raiseNumberFormatValidityException() throws IllegalArgumentException {
-        throw new IllegalArgumentException(ERROR_TOKEN + " 다리 길이는 숫자만 입력 가능합니다.");
+        throw new IllegalArgumentException(BridgeException.TOKEN.getMessage() + " 다리 길이는 숫자만 입력 가능합니다.");
     }
 }
