488f3c5245890d54a34b3b7bdca15ef050d7bca5
jagajang <kosb7544@gmail.com>
2022-11-21 15:33:03 +0900

    refactor: move result message to enum class

--- src/main/java/bridge/OutputView.java
+++ src/main/java/bridge/OutputView.java
@@ -49,8 +49,13 @@ public class OutputView {
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
     public void printResult(boolean success, int trial) {
-        System.out.printf("게임 성공 여부: \n");
-        System.out.printf("총 시도한 횟수: %d\n", trial);
+        System.out.printf(OutputMessage.GAME_RESULT.value, successToString(success), trial);
+    }
+
+    private String successToString(boolean success) {
+        if(success)
+            return "성공";
+        return "실패";
     }
 
     /** 추가 출력 메세지
