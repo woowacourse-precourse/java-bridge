d9d20c467c9618d9fafe1adb857d35c0808e1a89
skydreamer21 <skydreamer210@gmail.com>
2022-11-22 01:18:50 +0900

    test(OutputViewTest): 다리건너기 결과 출력 기능 수정에 따른 테스트 수정

--- src/test/java/bridge/view/OutputViewTest.java
+++ src/test/java/bridge/view/OutputViewTest.java
@@ -2,9 +2,11 @@ package bridge.view;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import bridge.service.constant.ChoiceResult;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
 import java.io.ByteArrayOutputStream;
 import java.io.OutputStream;
 import java.io.PrintStream;
@@ -13,38 +15,26 @@ import java.util.List;
 public class OutputViewTest {
     private static OutputView outputView;
     private static OutputStream out;
-    private static List<String> bridge;
-    private static int order;
 
     @BeforeEach
     void beforeEach() {
         outputView = new OutputView();
         out = new ByteArrayOutputStream();
         System.setOut(new PrintStream(out));
-        bridge = List.of("U", "D", "U", "D");
-        order = 3;
     }
 
-    @DisplayName("유저가 특정 칸에 대한 다리 건너기를 성공 했을 때 결과를 출력한다.")
+    @DisplayName("gameLog를 받았을 때 스타일을 입혀 주어진 형식에 맞게 출력한다.")
     @Test
     void printMapWhenUserSucceed() {
-        outputView.printMap(bridge, order, false);
-        assertThat(out.toString())
-                .contains(
-                        "[ O |   | O ]",
-                        "[   | O |   ]"
-                );
-    }
-
-    @DisplayName("유저가 특정 칸에 대한 다리 건너기를 실패 했을 때 결과를 출력한다.")
-    @Test
-    void printMapWhenUserFail() {
-        outputView.printMap(bridge, order, true);
+        List<List<ChoiceResult>> gameLog = List.of(
+                List.of(ChoiceResult.CORRECT, ChoiceResult.NOT_CHOSEN, ChoiceResult.NOT_CHOSEN),
+                List.of(ChoiceResult.NOT_CHOSEN, ChoiceResult.CORRECT, ChoiceResult.WRONG)
+        );
+        outputView.printMap(gameLog);
         assertThat(out.toString())
                 .contains(
                         "[ O |   |   ]",
                         "[   | O | X ]"
                 );
     }
-
 }
