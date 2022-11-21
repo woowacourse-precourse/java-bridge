4f721e7a4ae5a2ccefadc5eff9761f68796bd0eb
Dabeen Jeong <hello70825@gmail.com>
2022-11-22 00:38:27 +0900

    feat: 다리 방향에 맞는 상수를 얻는 기능을 추가

--- src/main/java/bridge/domain/Move.java
+++ src/main/java/bridge/domain/Move.java
@@ -2,24 +2,39 @@ package bridge.domain;
 
 import java.util.Arrays;
 import java.util.function.BiPredicate;
+import java.util.stream.Stream;
 
 public enum Move {
 
-    CORRECT("O", (string, direction) -> string.equals(direction.getDirectionInitial())),
-    WRONG("X", (string, direction) -> string.equals(direction.getDirectionInitial())),
-    EMPTY(" ", (string, direction) -> !string.equals(direction.getDirectionInitial()));
+    CORRECT("O"
+            , (string, bridgeDirection) -> string.equals(bridgeDirection.getDirectionInitial())
+            , (string, direction) -> string.equals(direction.getDirectionInitial())),
+    WRONG("X"
+            , (string, bridgeDirection) -> true
+            , (string, direction) -> !string.equals(direction.getDirectionInitial())),
+    EMPTY(" "
+            , (string, bridgeDirection) -> !string.equals(bridgeDirection.getDirectionInitial())
+            , (string, direction) -> string.equals(direction.getDirectionInitial()));
 
     private final String status;
-    private final BiPredicate<String, Direction> isMatch;
+    private final BiPredicate<String, Direction> isMatchBridge;
+    private final BiPredicate<String, Direction> isMatchDirection;
 
-    Move(String status, BiPredicate<String, Direction> isMatch) {
+    Move(String status, BiPredicate<String, Direction> isMatchBridge, BiPredicate<String, Direction> isMatchDirection) {
         this.status = status;
-        this.isMatch = isMatch;
+        this.isMatchBridge = isMatchBridge;
+        this.isMatchDirection = isMatchDirection;
     }
 
-    public static String getFilterMove(String string, Direction filterDirection) {
-        return Arrays.stream(Move.values())
-                .filter(move -> move.isMatch.test(string, filterDirection))
+    public static String getFilterBridgeMove(String string, Direction bridgeDirection, Direction canDirection) {
+        Stream<Move> moveStream = Arrays.stream(Move.values())
+                .filter(move -> move.isMatchBridge.test(string, bridgeDirection));
+
+        return getFilterDirectionMove(moveStream, string, canDirection);
+    }
+
+    private static String getFilterDirectionMove(Stream<Move> moves, String string, Direction canDirection) {
+        return moves.filter(move -> move.isMatchDirection.test(string, canDirection))
                 .findAny()
                 .orElse(EMPTY)
                 .getStatus();
