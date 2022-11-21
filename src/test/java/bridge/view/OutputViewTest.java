8af7c59ba55f292035351018f6770933995b0ee0
ostar11 <cjw5877@gmail.com>
2022-11-21 14:12:48 +0900

    feat(OutputView): printMap 구현

---
+++ src/test/java/bridge/view/OutputViewTest.java
@@ -0,0 +1,30 @@
+package bridge.view;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+import bridge.BridgeGameResult;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class OutputViewTest {
+    private OutputView outputView;
+    private BridgeGameResult bridgeGameResult;
+
+    @BeforeEach
+    void setOutputView() {
+        outputView = new OutputView();
+        bridgeGameResult = new BridgeGameResult();
+    }
+
+    @Test
+    void printMap() {
+        bridgeGameResult.addResult("U", "O");
+        bridgeGameResult.addResult("D", "O");
+        bridgeGameResult.addResult("U", "O");
+        String input = outputView.printMap(bridgeGameResult);
+
+//        assertThat(input).isEqualTo("[ O |   | O ]");
+//        assertThat(input).isEqualTo("[   | O |   ]");
+    }
+}
