4b695b91b74806660beda9738ae9e34efba07a97
sanghyuk2 <givemesomefoodplz@gmail.com>
2022-11-22 01:58:36 +0900

    feat: 다리 길이 입력 기능 구현
    입력값으로 생성된 다리 반환

--- src/main/java/bridge/domain/bridgegame/BridgeGameController.java
+++ src/main/java/bridge/domain/bridgegame/BridgeGameController.java
@@ -1,21 +1,29 @@
 package bridge.domain.bridgegame;
 
+import bridge.BridgeRandomNumberGenerator;
 import bridge.domain.BridgeGameStarter;
 import bridge.io.input.InputView;
 import bridge.io.output.OutputView;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class BridgeGameController implements BridgeGameStarter {
     private final OutputView outputView;
     private final InputView inputView;
+    private final BridgeMaker bridgeMaker;
+    private List<String> bridge;
 
     public BridgeGameController() {
         this.inputView = new InputView();
         this.outputView = new OutputView();
+        this.bridgeMaker = new BridgeMaker(new BridgeRandomNumberGenerator());
+        this.bridge = new ArrayList<>();
     }
 
     @Override
     public void start() {
         outputView.printStartMessage();
-        inputView.readBridgeSize();
+        bridge = bridgeMaker.makeBridge(inputView.readBridgeSize());
     }
 }
