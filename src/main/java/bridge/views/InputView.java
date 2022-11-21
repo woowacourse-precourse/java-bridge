b3d4f470d91cbac8bc6864dae08084583b347021
DevJay1024 <devjay1024@gmail.com>
2022-11-22 07:48:42 +0900

    feat: 유효한 다리 길이를 입력받는 기능 구현

--- src/main/java/bridge/views/InputView.java
+++ src/main/java/bridge/views/InputView.java
@@ -1,15 +1,34 @@
 package bridge.views;
 
+import bridge.exceptions.CustomIllegalStateException;
+import camp.nextstep.edu.missionutils.Console;
+
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
  */
 public class InputView extends DefaultView {
 
+    private static final String MSG_REQUEST_BRIDGE_SIZE = "다리의 길이를 입력해주세요.";
+
+
     /**
      * 다리의 길이를 입력받는다.
      */
     public int readBridgeSize() {
-        return 0;
+        System.out.println(MSG_REQUEST_BRIDGE_SIZE);
+        String input = Console.readLine();
+        validateBridgeSize(input);
+        return Integer.parseInt(input);
+    }
+
+    private void validateBridgeSize(String input) {
+        try {
+            Integer.parseInt(input);
+        } catch (IllegalArgumentException exception) {
+            throw new CustomIllegalStateException(
+                    "자바 정수 범위 이내의 숫자를 입력해야 합니다."
+            );
+        }
     }
 
     /**
