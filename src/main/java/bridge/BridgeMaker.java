6dabff9920369978c7d8c0f1739eb407030f6c66
josolha <josolha@nate.com>
2022-11-21 23:53:37 +0900

    refactor(BridgeMaker) : 인스턴스 생성 옮기기, 숫자 변경

--- src/main/java/bridge/BridgeMaker.java
+++ src/main/java/bridge/BridgeMaker.java
@@ -8,8 +8,6 @@ import java.util.List;
  */
 public class BridgeMaker {
 
-    private final List<String> bridge = new ArrayList<>();
-
     private final BridgeNumberGenerator bridgeNumberGenerator;
 
     public BridgeMaker(BridgeNumberGenerator bridgeNumberGenerator) {
@@ -21,7 +19,7 @@ public class BridgeMaker {
      * @return 입력받은 길이에 해당하는 다리 모양. 위 칸이면 "U", 아래 칸이면 "D"로 표현해야 한다.
      */
     public List<String> makeBridge(int size) {
-
+        List<String> bridge = new ArrayList<>();
         for (int i = 0; i < size; i++) {
 
             int generate = bridgeNumberGenerator.generate();
