0861060bbd2e92e52f578996aaf03387bb4d54b2
Hoo93 <117077999+Hoo93@users.noreply.github.com>
2022-11-21 14:39:56 +0900

    FEAT[InputView]: 공백문자/ 소문자 입력 예외처리
    공백문자/ 소문자를 입력해도 진행되게 만듦

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -9,16 +9,21 @@ public class InputView {
 
     public int readBridgeSize() {
         System.out.println("다리의 길이를 입력해주세요.");
-        String userInput = Console.readLine();
+        String userInput = Console.readLine().trim();
         if (userInput.matches(".*[a-zA-Z]+.*")) {
-            throw new IllegalArgumentException("[ERROR]");
+            throw new IllegalArgumentException("[ERROR] : 숫자만 입력해 주세요.");
         }
         return Integer.parseInt(userInput);
     }
 
     public String readMoving() {
         System.out.println("이동할 칸을 선택해주세요. (위: U, 아래: D)");
-        return Console.readLine();
+        String userInput = Console.readLine();
+        userInput = userInput.toUpperCase().trim();
+        if (!userInput.equals("U") && !userInput.equals("D")){
+            throw new IllegalArgumentException("[ERROR] : U 또는 D 만 입력해 주세요. ");
+        }
+        return userInput;
     }
 
     /**
@@ -26,6 +31,12 @@ public class InputView {
      */
     public String readGameCommand() {
         System.out.println("게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)");
-        return Console.readLine();
+        String userInput = Console.readLine();
+        userInput = userInput.toUpperCase().trim();
+        System.out.println(userInput);
+        if (!userInput.equals("R") && !userInput.equals("Q")){
+            throw new IllegalArgumentException("[ERROR] : R 또는 Q 만 입력해 주세요. ");
+        }
+        return userInput;
     }
 }
