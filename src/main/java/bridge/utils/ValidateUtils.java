b3d2ec2e888a589ae7d7462b6dd489c7fe5f3e2e
song-wooseok <sws981202@gmail.com>
2022-11-21 14:59:44 +0900

    feat: 사용자가 이동할 칸 입력에 대한 검증하는 기능

--- src/main/java/bridge/utils/ValidateUtils.java
+++ src/main/java/bridge/utils/ValidateUtils.java
@@ -1,5 +1,6 @@
 package bridge.utils;
 
+import bridge.utils.constants.BridgeMove;
 import bridge.utils.constants.BridgeSize;
 
 import java.util.regex.Pattern;
@@ -9,6 +10,12 @@ public class ValidateUtils {
     public static void isCorrectRangeNumbers(String input){
         if(!Pattern.matches(BridgeSize.BRIDGE_SIZE_REGEX, input)){
             throwIllegalArgumentException(BridgeSize.ERROR_BRIDGE_SIZE_MESSAGE);
+        }
+    }
+
+    public static void isCorrectMoveValue(String input){
+        if(!Pattern.matches(BridgeMove.BRIDGE_MOVE_REGEX, input)){
+            throwIllegalArgumentException(BridgeMove.ERROR_BRIDGE_MOVE_MESSAGE);
         }
     }
 
--- src/main/java/bridge/utils/constants/BridgeMove.java
+++ src/main/java/bridge/utils/constants/BridgeMove.java
@@ -4,4 +4,7 @@ public class BridgeMove {
     public static final int START_INDEX = -1;
     public static final int GO_STEP = 1;
     public static final int BACK_STEP = -1;
+    public static final String READ_BRIDGE_MOVE_MESSAGE = "이동할 칸을 선택해주세요. (위: U, 아래: D)";
+    public static final String ERROR_BRIDGE_MOVE_MESSAGE = "[ERROR] 이동할 수 있는 칸은 U 또는 D의 문자입니다.";
+    public static final String BRIDGE_MOVE_REGEX = "^U{1}$|^D{1}$";
 }
