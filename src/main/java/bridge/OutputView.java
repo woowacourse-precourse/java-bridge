7b53467b0fb3cef0a85fb386188ce8f1cdba9e2d
Sunghyun-0 <zkdldk95@naver.com>
2022-11-21 23:05:13 +0900

    게임 결과 출력 기능 추가

--- src/main/java/bridge/OutputView.java
+++ src/main/java/bridge/OutputView.java
@@ -65,6 +65,13 @@ public class OutputView {
      * <p>
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void printResult() {
+    public void printResult(List<String> userMove,List<String> bridge,int count) {
+        System.out.println("최종 게임 결과");
+        String printedMessage = printMap(userMove,bridge);
+        if(printedMessage.contains("X")){
+            System.out.println("게임 성공 여부: 실패"+"\n총 시도한 횟수: "+count);
+            return;
+        }
+        System.out.println("게임 성공 여부: 성공"+"\n총 시도한 횟수: "+count);
     }
 }
