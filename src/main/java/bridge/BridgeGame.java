3036191dc7c93f64eed567d0906914a38f3070ea
Seiyon Park <seiyonpdev@gmail.com>
2022-11-21 10:19:43 +0900

    feat(BridgeGame): 메소드 retry 구현
    재시작을 하기 위해 플레이어 위치와 지도를 초기화하는 메소드를 구현했다

--- src/main/java/bridge/BridgeGame.java
+++ src/main/java/bridge/BridgeGame.java
@@ -51,6 +51,9 @@ public class BridgeGame {
      * 재시작을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
     public void retry() {
+        playerLocationIndex = -1;
+        upMap.clear();
+        downMap.clear();
     }
 
     public int getPlayerLocationIndex() {
