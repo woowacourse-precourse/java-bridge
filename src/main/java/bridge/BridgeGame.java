a84de27e91917c09f24bb009c5f838ed77853862
cgk95 <kjkj5868@gmail.com>
2022-11-21 13:41:15 +0900

    feat(BridgeGame) : 게임의 진행 결과를 출력하는 기능 추가, 게임의 진행 횟수를 저장하는 기능 수정, 주석 제거

--- src/main/java/bridge/BridgeGame.java
+++ src/main/java/bridge/BridgeGame.java
@@ -2,35 +2,32 @@ package bridge;
 
 import java.util.List;
 import java.util.Stack;
-
 /**
- * 다리 건너기 게임을 관리하는 클래스
- * Controller라고 할 수 있다.
- */
+ * View 코드를 사용하지 말라는 것으로 보아 Model 이다.
+ * 따라서 리팩토링이 필요하다.
+ * 그렇다면 게임 진행에 필요한 데이터를 저장하고 수정하는 공간으로 만들어야 한다.
+ * **/
 public class BridgeGame {
     public int idx;
-    public int runCount=0;
-
+    public int runCount=1;
     public BridgeGame() {
         System.out.println("다리 건너기 게임을 시작합니다.\n");
         Model.size = new InputView().readBridgeSize();
         new BridgeMaker(new BridgeRandomNumberGenerator()).makeBridge(Model.size); // 다리 생성
-        run();
+        run(runCount);
     }
 
-    public void run() {
+    public void run(int runCount) {
         Model.user = new Stack<>();
         this.idx = 0;
         do {
             move();
-            // 이곳에 중간 상태 메시지 작성
-            // 모델의 정보를 먼저 수정해준 뒤 뷰를 호출하여 출력하여야 한다.
-            new VisualizeController();
+            new VisualizeController(idx);
             new OutputView().printMap();
         } while (runCondition(Model.user, Model.bridge));
-        runCount++;
-        if (idx == Model.size) { // 다리를 다 올바르게 지나간 상태라면
-            new OutputView().printResult(); // 결과 출력
+
+        if (idx == Model.size) {
+            new OutputView().printResult(true,runCount);
             return;
         }
         retry();
@@ -48,11 +45,14 @@ public class BridgeGame {
         return false;
     }
 
-    public void retry() { // 인스턴스 변수들을 다시 초기화 시켜주어야 한다.
+    public void retry() {
         if (new InputView().readGameCommand().equals("Q")) {
-            new OutputView().printResult();
+            new OutputView().printResult(false,runCount);
             return;
         }
-        run();
+        Model.lowerResult="[]";
+        Model.upperResult="[]";
+        runCount++;
+        run(runCount);
     }
 }
