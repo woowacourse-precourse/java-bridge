a87ad874a6e5cadec46b5192d4b7d4d29f315bf3
Jaehong Lee <woohong24@naver.com>
2022-11-21 17:05:01 +0900

    feat: BridgeMoveController 추가

---
+++ src/main/java/bridge/controller/BridgeMoveController.java
@@ -0,0 +1,20 @@
+package bridge.controller;
+
+import bridge.BridgeMove;
+
+public class BridgeMoveController {
+
+    private final BridgeMove bridgeMove;
+
+    public BridgeMoveController(BridgeMove bridgeMove) {
+        this.bridgeMove = bridgeMove;
+    }
+
+    public void run() {
+
+
+    }
+
+
+
+}
