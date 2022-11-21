ceb4253d22dbd65d57ea850241aabdfb4f0b22bb
Eun-chan Cho <eunchan1001@gmail.com>
2022-11-21 19:55:07 +0900

    feat(BridgeGame): implement move and retry methods in game

--- src/main/java/bridge/BridgeGame.java
+++ src/main/java/bridge/BridgeGame.java
@@ -1,16 +1,34 @@
 package bridge;
 
+import java.util.List;
+
 /**
  * 다리 건너기 게임을 관리하는 클래스
  */
 public class BridgeGame {
+    private List<String> randomBridge;
+    private int size;
+    private Result result;
+
+    public BridgeGame(int size) {
+        this.size = size;
+        BridgeMaker bridgeMaker = new BridgeMaker(new BridgeRandomNumberGenerator());
+        randomBridge = bridgeMaker.makeBridge(size);
+        result = new Result();
+    }
 
     /**
      * 사용자가 칸을 이동할 때 사용하는 메서드
      * <p>
      * 이동을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void move() {
+    public boolean move(int pos, String direction) {
+        if (randomBridge.get(pos).equals(direction)) {
+            result.appendResult(ResultType.SUCCESS, Direction.getDirection(direction));
+            return true;
+        }
+        result.appendResult(ResultType.FAIL, Direction.getDirection(direction));
+        return false;
     }
 
     /**
@@ -19,5 +37,6 @@ public class BridgeGame {
      * 재시작을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
     public void retry() {
+        result = new Result();
     }
 }
