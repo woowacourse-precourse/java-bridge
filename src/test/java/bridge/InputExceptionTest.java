7630e6ef241d1d19f93d0a14f4563dabb0f41c12
noparamin <noparamin@naver.com>
2022-11-21 16:49:56 +0900

    test(InputExceptionTest): 이동할 칸 입력 유효성 검사 테스트

--- src/test/java/bridge/InputExceptionTest.java
+++ src/test/java/bridge/InputExceptionTest.java
@@ -69,4 +69,54 @@ class InputExceptionTest {
 
         assertThat(checkNumber).isFalse();
     }
+
+    @DisplayName("이동할 칸 입력값 길이가 1이 아닌 경우 예외 처리 테스트")
+    @Test
+    void validateReadMovingLength() {
+        String inputPosition = "UU";
+        assertThatIllegalArgumentException().isThrownBy(() -> inputException.validateReadMoving(inputPosition));
+    }
+
+    @DisplayName("이동할 칸 입력값 길이가 U 또는 D가 아닌 경우 예외 처리 테스트")
+    @Test
+    void validateReadMovingRight() {
+        String inputPosition = "A";
+        assertThatIllegalArgumentException().isThrownBy(() -> inputException.validateReadMoving(inputPosition));
+    }
+
+    @DisplayName("이동할 칸 입력값의 길이가 1인 경우 테스트")
+    @Test
+    void isLengthOne() {
+        String inputPosition = "U";
+        boolean checkInputPosition = inputException.isLengthOne(inputPosition);
+        assertThat(checkInputPosition).isTrue();
+    }
+
+    @DisplayName("이동할 칸 입력값의 길이가 1이 아닌 경우 테스트")
+    @Test
+    void isNotLengthOne() {
+        String inputPosition = "UD";
+        boolean checkInputPosition = inputException.isLengthOne(inputPosition);
+        assertThat(checkInputPosition).isFalse();
+    }
+
+    @DisplayName("이동할 칸 입력값이 U 또는 D인 경우 테스트")
+    @Test
+    void isInputPositionRight() {
+        String inputPosition = "D";
+        boolean checkInputPosition = inputException.isInputPositionRight(inputPosition);
+        assertThat(checkInputPosition).isTrue();
+    }
+
+    @DisplayName("이동할 칸 입력값이 U 또는 D가 아닌 경우 테스트")
+    @Test
+    void isNotInputPositionRight() {
+        String inputPosition = "A";
+        boolean checkInputPosition = inputException.isInputPositionRight(inputPosition);
+        assertThat(checkInputPosition).isFalse();
+    }
+
+
+
+
 }
