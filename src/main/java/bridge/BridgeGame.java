8aa62734ed6017b31a31656b8bf63f6d0f1611bf
jagajang <kosb7544@gmail.com>
2022-11-21 14:56:25 +0900

    refactor: move return command out of loop

--- src/main/java/bridge/BridgeGame.java
+++ src/main/java/bridge/BridgeGame.java
@@ -38,15 +38,16 @@ public class BridgeGame {
      * 이동을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
     public boolean move(List<String> bridge) {
+        boolean correct = true;
         for(int pos = 0; pos < size; pos++) {
             outputView.printMoveMessage();
-            boolean correct = inputView.readMoving().equals(bridge.get(pos));
-
+            correct = inputView.readMoving().equals(bridge.get(pos));
             outputView.printMap(bridge, pos, correct);
-            if(!correct)
-                return false;
+            if(!correct) {
+                break;
+            }
         }
-        return true;
+        return correct;
     }
 
     /**
