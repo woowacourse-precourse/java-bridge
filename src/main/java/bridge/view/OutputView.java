267417461f147b9fb5dec812d5c3195200327646
euna <christinejung10@daum.net>
2022-11-22 03:04:10 +0900

    refactor(OutputView): change method names, add comments

--- src/main/java/bridge/view/OutputView.java
+++ src/main/java/bridge/view/OutputView.java
@@ -34,68 +34,61 @@ public class OutputView {
     private static final String BRIDGE_DISABLE_TO_MOVE = "X";
     private static final String BRIDGE_NOT_CHOSEN_TO_MOVE = " ";
 
-    // 게임 시작 문구 출력
+
     public static void printGameStartMessage() {
         System.out.println(GAME_START_MESSAGE);
     }
 
-    // 다리 길이 입력 문구 출력
     public static void askBridgeSize() {
         System.out.println(ASK_BRIDGE_SIZE_MESSAGE);
     }
 
-    // 이동할 칸 입력 문구 출력
     public static void askUserMoveDirection() {
         System.out.println(ASK_USER_MOVE_DIRECTION_MESSAGE);
     }
 
-    // 재시작 / 종료 여부 입력 문구 출력
     public static void askGameCommand() {
         System.out.println(ASK_GAME_COMMAND_MESSAGE);
     }
 
-    // 다리 길이 에러 문구 출력
     public static void printErrorMessage_bridgeSize() {
         System.out.println(ERROR_TITLE_MESSAGE + ERROR_BRIDGE_SIZE_MESSAGE);
     }
 
-    // 이동할 칸 에러 문구 출력
     public static void printErrorMessage_userMoveDirection() {
         System.out.println(ERROR_TITLE_MESSAGE + ERROR_USER_MOVE_DIRECTION_MESSAGE);
     }
 
-    // 재시작 / 종료 여부 에러 문구 출력
     public static void printErrorMessage_gameCommand() {
         System.out.println(ERROR_TITLE_MESSAGE + ERROR_GAME_COMMAND_MESSAGE);
     }
 
+
     /**
      * TODO: 현재까지 이동한 다리의 상태를 정해진 형식에 맞춰 출력한다.
-     * <p>
-     * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      *
-     * @param bridge_answer   정답 다리
-     * @param bridge_userMove 사용자가 현재까지 다리 상에서 이동한 위치
+     * @param bridge_answer   정답 다리 List
+     * @param bridge_userMove 사용자가 현재까지 다리 상에서 이동한 위치 List
      */
     public static void printMap(List<String> bridge_answer, List<String> bridge_userMove) {
-        String[] map = new String[]{BRIDGE_START, BRIDGE_START}; // [0]: map_down, [1]: map_up
+        String[] map = new String[]{BRIDGE_START, BRIDGE_START}; // [0]: 아래 칸, [1]: 위 칸
         for (int i = 0; i < bridge_userMove.size(); i++) {
-            String[] userFootprint = getUserFootprint(bridge_answer.get(i), bridge_userMove.get(i));
+            String[] userFootprint = setUserFootprint(bridge_answer.get(i), bridge_userMove.get(i));
             map[BridgeGame.BridgeShape.DOWN.getIntegerValue()] += userFootprint[BridgeGame.BridgeShape.DOWN.getIntegerValue()];
             map[BridgeGame.BridgeShape.UP.getIntegerValue()] += userFootprint[BridgeGame.BridgeShape.UP.getIntegerValue()];
-            checkIsLastBridge(map, (i == bridge_userMove.size() - 1));
+            setBridgeBoundary(map, (i == bridge_userMove.size() - 1));
         }
         System.out.println(map[BridgeGame.BridgeShape.UP.getIntegerValue()] + "\n" + map[BridgeGame.BridgeShape.DOWN.getIntegerValue()] + "\n");
     }
 
     /**
-     * @param answerDirection   이동할 수 있는 칸의 위치
+     * @param answerDirection   정답 칸의 위치
      * @param userMoveDirection 사용자가 이동한 칸의 위치
      *                          1. 사용자가 이동할 수 있는 칸을 선택했다면 footprint_userMovedToAble 호출
      *                          2. 사용자가 이동할 수 없는 칸을 선택했다면 footprint_userMovedToDisable 호출
      * @return 사용자의 다리 상에서의 발자취 - String[]{아래 칸, 위 칸}, 칸에 들어가는 값은 O 또는 X 또는 공백
      */
-    private static String[] getUserFootprint(String answerDirection, String userMoveDirection) {
+    private static String[] setUserFootprint(String answerDirection, String userMoveDirection) {
         if (answerDirection.compareTo(userMoveDirection) == 0) {
             return footprint_userMovedToAble(userMoveDirection);
         }
@@ -132,11 +125,11 @@ public class OutputView {
     /**
      * 마지막 칸일 경우 다리의 끝을 명시 / 마지막 칸이 아닐 경우 다리의 구분선을 명시
      *
-     * @param map          출력할 다리
-     * @param isLastBridge 마지막 칸이라면 true / 마지막 칸이 아니라면 false
+     * @param map            출력할 다리
+     * @param isLastPosition 현재가 마지막 칸이라면 true / 마지막 칸이 아니라면 false
      */
-    private static void checkIsLastBridge(String[] map, boolean isLastBridge) {
-        if (isLastBridge) {
+    private static void setBridgeBoundary(String[] map, boolean isLastPosition) {
+        if (isLastPosition) {
             map[BridgeGame.BridgeShape.DOWN.getIntegerValue()] += BRIDGE_END;
             map[BridgeGame.BridgeShape.UP.getIntegerValue()] += BRIDGE_END;
             return;
@@ -145,30 +138,29 @@ public class OutputView {
         map[BridgeGame.BridgeShape.UP.getIntegerValue()] += BRIDGE_DIVISION;
     }
 
+
     /**
      * TODO: 게임의 최종 결과를 정해진 형식에 맞춰 출력한다.
-     * <p>
-     * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      *
-     * @param player          사용자 상태
-     * @param bridge_answer   정답 다리
-     * @param bridge_userMove 사용자가 현재까지 다리 상에서 이동한 위치
+     * @param player          사용자 상태값
+     * @param bridge_answer   정답 다리 List
+     * @param bridge_userMove 사용자가 현재까지 다리 상에서 이동한 위치 List
      */
     public static void printResult(User player, List<String> bridge_answer, List<String> bridge_userMove) {
         System.out.println(GAME_RESULT_TITLE_MESSAGE);
         printMap(bridge_answer, bridge_userMove);
 
         String gameResult = GAME_RESULT_IS_GAME_SUCCEED_MESSAGE;
-        gameResult += getGameStatusMessage(player.getUserGameStatus());
+        gameResult += setGameStatusMessage(player.getUserGameStatus());
         gameResult += GAME_RESULT_NUMBER_OF_GAME_TRIALS_MESSAGE + player.getNumberOfGameTrials();
         System.out.println(gameResult);
     }
 
     /**
-     * @param userGameStatus 사용자의 현재 게임 상태 (1인 경우 succeed / 2인 경우 failed)
-     * @return 게임 성공 여부에 따른 String 반환
+     * @param userGameStatus 사용자의 현재 게임 상태 (1인 경우 성공 / 2인 경우 실패)
+     * @return 게임 성공 또는 실패 여부에 따른 String 반환
      */
-    private static String getGameStatusMessage(int userGameStatus) {
+    private static String setGameStatusMessage(int userGameStatus) {
         if (userGameStatus == User.GameStatus.SUCCEED.getStatusNumber()) {
             return GAME_RESULT_STATUS_SUCCEED_MESSAGE;
         }
