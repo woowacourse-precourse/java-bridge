cf1aab8588bef7f1602fa048fd5274676fb46ab0
kimsinhyun <ksh415622@naver.com>
2022-11-21 22:40:23 +0900

    fix: 게임 종료시 출력
    printResult
       - 게임 종료 메시지 출력
       - 결과 확인용 체크 결과 출력
       - 재시도 횟수 출력

--- src/main/java/bridge/OutputView.java
+++ src/main/java/bridge/OutputView.java
@@ -65,6 +65,10 @@ public class OutputView {
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
     public static void printResult(List<String> bridgePattern, List<String>currentInputHistory, String successOrNot, int retryCount) {
+        OutputMessage.GAME_RESULT_MESSAGE.println();
+        printMap(bridgePattern, currentInputHistory);
+        OutputMessage.SUCCESS_FAIL_MESSAGE.printSuccessOrNot(successOrNot);
+        OutputMessage.RETRY_COUNT_MESSAGE.printRetryCount(retryCount);
 
     }
 }
