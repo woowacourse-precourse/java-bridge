f65212720caa6092302a20a1fe8311fbd242720d
YooByeongIk <quddlr96@naver.com>
2022-11-21 19:56:52 +0900

    feat(Application): implementation main method for running an application

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -4,5 +4,14 @@ public class Application {
 
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        try {
+            System.out.println("다리 건너기 게임을 시작합니다.");
+            BridgeMaker bridgeMaker = new BridgeMaker(new BridgeRandomNumberGenerator());
+            BridgeGame bridgeGame = new BridgeGame(bridgeMaker);
+            bridgeGame.play();
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            throw e;
+        }
     }
 }
