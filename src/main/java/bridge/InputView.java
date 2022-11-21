4933eb093d80c83aa0b5a85339c21633f9f0435e
kiarakim <kiarakim413@gmail.com>
2022-11-21 23:39:03 +0900

    feat(bridge) : add exception to input command

--- src/main/java/bridge/InputView.java
+++ src/main/java/bridge/InputView.java
@@ -1,6 +1,7 @@
 package bridge;
 
 import bridge.exception.BridgeSizeException;
+import bridge.exception.CommandException;
 import camp.nextstep.edu.missionutils.Console;
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
@@ -8,6 +9,7 @@ import camp.nextstep.edu.missionutils.Console;
 public class InputView {
 
     BridgeSizeException bridgeSizeException = new BridgeSizeException();
+    CommandException commandException = new CommandException();
 
     public int readBridgeSize() {
 
@@ -18,17 +20,15 @@ public class InputView {
 
 
     public String readMoving() {
-        //사용자가 이동할 칸을 입력받는다.
         String moving = Console.readLine();
-        //예외처리. [ERROR] 이동할 칸(U, D)중 하나의 문자를 입력해주세요.
+        commandException.isValidMove(moving);
         return moving;
     }
 
 
-    public String readGameCommand() {
-        //사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
-        String gameCommand = Console.readLine();
-        //예외처리. [ERROR] 재시작/종료 여부(R, Q)중 하나의 문자를 입력해주세요.
-        return gameCommand;
+    public String readDecision() {
+        String decision = Console.readLine();
+        commandException.isValidDecision(decision);
+        return decision;
     }
 }
