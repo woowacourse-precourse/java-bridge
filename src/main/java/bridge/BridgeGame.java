cb07a1cc0a5c6d7ed334e7a44b4e5fe6db977bce
lullulu__ <75472116+earthyoung@users.noreply.github.com>
2022-11-22 01:46:33 +0900

    feat: BridgeGame - 사용자 입력과 정답 비교, 재시작 여부 판단 로직

--- src/main/java/bridge/BridgeGame.java
+++ src/main/java/bridge/BridgeGame.java
@@ -3,26 +3,44 @@ package bridge;
 import java.util.ArrayList;
 import java.util.List;
 
-/**
- * 다리 건너기 게임을 관리하는 클래스
- */
+
 public class BridgeGame {
 
-    /**
-     * 사용자가 칸을 이동할 때 사용하는 메서드
-     * <p>
-     * 이동을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
-     */
-    public String move(String input, String answer) {
-        if(input.equals(answer)) return "O";
+    private int count = 0;
+    private List<String> answerList;
+    public List<String> inputList;
+
+    public void increaseCount(){
+        this.count++;
+    }
+
+    public int getCount(){
+        return this.count;
+    }
+
+    public BridgeGame(List<String> answerList){
+        this.answerList = answerList;
+        this.inputList = new ArrayList<>();
+    }
+
+    public List<String> getAnswerList(){
+        return this.answerList;
+    }
+
+    public List<String> getInputList(){
+        return this.inputList;
+    }
+
+    public String move(String input) {
+        inputList.add(input);
+        String answer = answerList.get(inputList.size()-1);
+        if(answer.equals(input)) {
+            increaseCount();
+            return "O";
+        }
         return "X";
     }
 
-    /**
-     * 사용자가 게임을 다시 시도할 때 사용하는 메서드
-     * <p>
-     * 재시작을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
-     */
     public boolean retry(String input) {
         if(input.equals("R")) return true;
         return false;
