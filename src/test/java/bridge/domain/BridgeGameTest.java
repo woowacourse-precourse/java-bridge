72d5d5d2d706bf487cc8fa7407c7798250591cce
hoin <apicad0307@gmail.com>
2022-11-21 15:27:11 +0900

    test(domain/BridgeGameTest.java): 입력과 정답에 따른 다리상태 검증 테스트 추가

--- src/test/java/bridge/domain/BridgeGameTest.java
+++ src/test/java/bridge/domain/BridgeGameTest.java
@@ -9,10 +9,26 @@ class BridgeGameTest {
     BridgeGame bridgeGame = new BridgeGame();
 
     @Test
-    void 다리_상태_테스트() {
+    void 다리_상태_테스트1() {
         bridgeGame.move("U", true);
         Assertions.assertThat(bridgeGame.getFirstRoad().toString()).isEqualTo("O");
         Assertions.assertThat(bridgeGame.getSecondRoad().toString()).isEqualTo(" ");
+    }
+
+    @Test
+    void 다리_상태_테스트2() {
+        bridgeGame.move("D", false);
+        Assertions.assertThat(bridgeGame.getFirstRoad().toString()).isEqualTo(" ");
+        Assertions.assertThat(bridgeGame.getSecondRoad().toString()).isEqualTo("X");
+    }
+
+    @Test
+    void 다리_상태_테스트3() {
+        bridgeGame.move("U", true);
+        bridgeGame.move("D", true);
+        bridgeGame.move("U", false);
+        Assertions.assertThat(bridgeGame.getFirstRoad().toString()).isEqualTo("O X");
+        Assertions.assertThat(bridgeGame.getSecondRoad().toString()).isEqualTo(" O ");
     }
 
 }
