a74a981255b759b8921ff96475991522431915f5
jaehee329 <jaehee329@yonsei.ac.kr>
2022-11-21 16:09:41 +0900

    feat(OutputView): 게임 최종 결과 출력 기능 구현

--- src/main/java/bridge/view/OutputView.java
+++ src/main/java/bridge/view/OutputView.java
@@ -22,5 +22,13 @@ public class OutputView {
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
     public void printResult(Bridge bridge) {
+        System.out.println("최종 게임 결과");
+        System.out.println(bridge);
+        String gameWon = "실패";
+        if (bridge.gameWon()) {
+            gameWon = "성공";
+        }
+        System.out.println("게임 성공 여부: " + gameWon);
+        System.out.println("총 시도한 횟수: " + bridge.getTrials());
     }
 }
