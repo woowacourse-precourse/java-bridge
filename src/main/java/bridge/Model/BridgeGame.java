8b6bd8dbe004eb731dd71ba8b14c007e977fd6ab
farmJun <junpreme1205@gmail.com>
2022-11-21 20:20:51 +0900

    feat(BridgeGame): 다리 길이를 관리하는 기능 구현

---
+++ src/main/java/bridge/Model/BridgeGame.java
@@ -0,0 +1,21 @@
+package bridge.Model;
+
+public class BridgeGame {
+
+
+    private static int bridgeSize;
+
+    public static void setBridgeSize(int bridgeSize) {
+        BridgeGame.bridgeSize = bridgeSize;
+    }
+
+    public static int getBridgeSize() {
+        return bridgeSize;
+    }
+
+    public void move() {
+    }
+
+    public void retry() {
+    }
+}
