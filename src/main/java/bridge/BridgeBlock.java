1171f86199fe5f3fdf2be984f078b172c90f8162
phobyjun <phobyjun@gmail.com>
2022-11-22 03:59:33 +0900

    feat(BridgeBlock): 클래스 메서드 추가 및 구현
    문자열을 입력으로 받아 해당하는 enum 인스턴스를 반환하는 static 메서드 구현

--- src/main/java/bridge/BridgeBlock.java
+++ src/main/java/bridge/BridgeBlock.java
@@ -10,6 +10,13 @@ public enum BridgeBlock {
         this.bridgeBlock = bridgeBlock;
     }
 
+    public static BridgeBlock getBridgeBlockType(String bridgeBlock) {
+        if (bridgeBlock.equals("U")) {
+            return BridgeBlock.UPPER_BLOCK;
+        }
+        return BridgeBlock.LOWER_BLOCK;
+    }
+
     public String getBridgeBlock() {
         return bridgeBlock;
     }
