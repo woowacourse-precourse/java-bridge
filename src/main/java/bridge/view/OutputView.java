b0323e5a19bf771ffa009f5df27b49267d6f8ff1
Lee-SungMin <saevers26@gmail.com>
2022-11-22 03:15:51 +0900

    feat: 칸 이동 결과 출력 구현

--- src/main/java/bridge/view/OutputView.java
+++ src/main/java/bridge/view/OutputView.java
@@ -1,5 +1,7 @@
 package bridge.view;
 
+import java.util.List;
+
 /**
  * 사용자에게 게임 진행 상황과 결과를 출력하는 역할을 한다.
  */
@@ -10,7 +12,17 @@ public class OutputView {
      * <p>
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void printMap() {
+    public static void printMap(List<String>[] move) {
+        for (int i = 0; i < move.length; i++) {
+            System.out.print("[ ");
+            for (int j = 0; j < move[i].size(); j++) {
+                if (j != 0) {
+                    System.out.print(" | ");
+                }
+                System.out.print(move[i].get(j));
+            }
+            System.out.println(" ]");
+        }
     }
 
     /**
