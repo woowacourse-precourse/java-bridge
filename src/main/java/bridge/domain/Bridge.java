da9f083de29d1bef1cc5dcd6e6157d306bd50d81
MoonJeWoong <munjin0201@naver.com>
2022-11-21 21:23:40 +0900

    feat(domain): Bridge관련 Enum클래스 생성

---
+++ src/main/java/bridge/domain/Bridge.java
@@ -0,0 +1,18 @@
+package bridge.domain;
+
+public enum Bridge {
+    MIN_LENGTH(3),
+    MAX_LENGTH(20),
+    START_INDEX(0);
+
+
+    final private int value;
+
+    Bridge(int value){
+        this.value = value;
+    }
+
+    public int getValue(){
+        return this.value;
+    }
+}
