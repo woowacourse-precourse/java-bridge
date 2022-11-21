f334c3f3c0ba902c40dab03bbecdb55935db310f
김형기 <khk211113@naver.com>
2022-11-21 21:05:00 +0900

    feat(BridgeGame): 사용자의 이동과 게임 상태를 관리하는 기능

---
+++ src/main/java/bridge/model/BridgeGame.java
@@ -0,0 +1,72 @@
+package bridge.model;
+
+import bridge.config.Direction;
+import bridge.config.GameStatus;
+import bridge.config.InvalidMoveException;
+
+import java.util.Stack;
+
+/**
+ * 다리 건너기 게임을 관리하는 클래스
+ */
+public class BridgeGame {
+
+    private final Bridge bridge;
+    private final Stack<PieceMove> moveTrace = new Stack<>();
+    private GameStatus status = GameStatus.RUNNING;
+
+    public BridgeGame(final Bridge bridge) {
+        this.bridge = bridge;
+    }
+
+    /**
+     * 사용자가 칸을 이동할 때 사용하는 메서드
+     * <p>
+     * 이동을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
+     */
+    public void move(Direction direction) throws InvalidMoveException {
+        checkMoveValidation();
+        boolean allowed = bridge.isAllow(moveTrace.size(), direction);
+        moveTrace.add(new PieceMove(direction, allowed));
+        updateStatus();
+    }
+
+
+    public GameStatus getStatus() {
+        return this.status;
+    }
+
+    private void checkMoveValidation() {
+        if (this.status != GameStatus.RUNNING) {
+            throw new InvalidMoveException();
+        }
+    }
+
+    private void updateStatus() {
+        if (moveTrace.isEmpty()) {
+            return;
+        }
+        checkWinOrLose();
+    }
+
+    private void checkWinOrLose() {
+        if (!moveTrace.peek().getSuccess()) {
+            this.status = GameStatus.LOSE;
+            return;
+        }
+        if (moveTrace.size() == bridge.getNumOfCells()) {
+            this.status = GameStatus.WIN;
+        }
+    }
+
+
+    /**
+     * 사용자가 게임을 다시 시도할 때 사용하는 메서드
+     * <p>
+     * 재시작을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
+     */
+    public void retry() {
+        status = GameStatus.RUNNING;
+        moveTrace.clear();
+    }
+}
