525edf7a642a36916c51a9d538358b021f3c1800
yun <yshyeon102@gmail.com>
2022-11-22 02:59:01 +0900

    fix(Result): failIndex필드 및 활용 생성자 제거

--- src/main/java/bridge/domain/Result.java
+++ src/main/java/bridge/domain/Result.java
@@ -3,13 +3,6 @@ package bridge.domain;
 public class Result {
     private final boolean success;
     private final Bridge selectedBridge;
-    private int failIndex;
-
-    private Result(boolean success, Bridge selectedBridge, int failIndex) {
-        this.success = success;
-        this.selectedBridge = selectedBridge;
-        this.failIndex = failIndex;
-    }
 
     private Result(boolean success, Bridge selectedBridge) {
         this.success = success;
@@ -24,12 +17,9 @@ public class Result {
         return selectedBridge;
     }
 
-    public int getFailIndex() {
-        return failIndex;
-    }
 
-    public static Result fail(Bridge selectedBridge, int failIndex) {
-        return new Result(false, selectedBridge, failIndex);
+    public static Result fail(Bridge selectedBridge) {
+        return new Result(false, selectedBridge);
     }
 
     public static Result success(Bridge selectedBridge) {
