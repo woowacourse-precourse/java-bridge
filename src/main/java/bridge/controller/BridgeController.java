e0d140dec258b090c3904262068543f2a6439b76
Hongsugyeong <olivia258@naver.com>
2022-11-21 16:07:04 +0900

    (feat) : Controller 작성

--- src/main/java/bridge/controller/BridgeController.java
+++ src/main/java/bridge/controller/BridgeController.java
@@ -14,43 +14,121 @@ import java.util.List;
 public class BridgeController {
 
     InputView inputView = new InputView();
-    BridgeSize bridge = new BridgeSize();
+    BridgeSize bridgeSize = new BridgeSize();
     BridgeMaker bridgeMaker = new BridgeMaker(new BridgeRandomNumberGenerator());
     Move mm = new Move();
     BridgeGame bridgeGame = new BridgeGame();
 
     public void start() {
+        String reply;
         OutputView.printGameStart();
+        do {
+                //play();
+            int size = inputSize();
+            List<String> bridge = bridgeMaker.makeBridge(size);
+
+            reply = play(bridge);
+//                if (result.contains("X")) {
+//                    //new BridgeGame();
+//                    //bridges = new ArrayList<>();
+//                    reset();
+//                    //System.out.println();
+//                    break;
+//                }
+//            }
+//            reply = InputView.readGameCommand();
+        }while(!reply.equals("Q"));
+       // }
+    }
+
+    public int inputSize(){
         String size = inputView.readBridgeSize();
-        bridge.isNumber(size);
-        bridge.isRangeNumber(size);
-        int s = Integer.parseInt(size);
-        List<String> bridge = bridgeMaker.makeBridge(s);
-        System.out.println(bridge);
+        bridgeSize.checkNumber(size);
+        return Integer.parseInt(size);
+    }
+
+//    public List<String> makeBrige(int size){
+//        List<String> bridge = bridgeMaker.makeBridge(size);
+//        return bridge;
+//    }
+
+    public void printResult(String fail, int attempt) {
+        OutputView.printResult(bridgeGame.toString());
+        OutputView.printSuccessOrFail(fail);
+        OutputView.printAttemptCount(attempt);
+
+    }
+
+    public String crossBridge(List<String> bridge) {
         for(int i=0;i<bridge.size();i++) {
             String move = inputView.readMoving();
             mm.isProper(move);
-
-            bridgeGame.move(bridge.get(i), move);
-            String result = bridgeGame.toString();
-            OutputView.printMap(result);
-
-
-            if(result.contains("X")) {
-                //new BridgeGame();
-                //bridges = new ArrayList<>();
-                reset();
-                //System.out.println();
-                break;
+            List<List<String>> bridges = bridgeGame.move(bridge.get(i),move);
+            OutputView.printMap(bridgeGame.toString());
+            //reply(bridgeGame.toString());
+            if(endGame(bridges).equals("실패")) {
+                return "실패";
             }
         }
-        InputView.readGameCommand();
+        return "성공";
+    }
+
+    public String crossingBridge(List<String> bridge){
+        reset();
+        return crossBridge(bridge);
+    }
+
+    public String play(List<String> bridge){
+        int attempt = 0;
+        String fail;
+        String reply = "R";
+        do{
+            fail = crossingBridge(bridge);
+            attempt = bridgeGame.retry(reply, attempt);
+            reply = replyQuit(fail);
+
+        }while (!reply.equals("Q"));
+        printResult(fail, attempt);
+        return reply;
+    }
+
+    public String endGame(List<List<String>> bridges){
+        int last = bridges.get(0).size() -1;
+        List<String> bridgeUp = bridges.get(0);
+        List<String> bridgeDown = bridges.get(1);
+
+        if(bridgeUp.get(last).contains("X") || bridgeDown.get(last).contains("X")){
+            return "실패";
+        }
+        return "성공";
 
 
     }
 
+
+//    public void reply(String result) {
+//        if(result.contains("X")) {
+//            reset();
+//            sf = "실패";
+//        }
+//    }
+
+    public String replyQuit(String fail){
+        String reply;
+        if(fail.equals("실패")) {
+            reply = InputView.readGameCommand();
+            return reply;
+        }
+        return "Q";
+    }
+
+
+
+
+
     public void reset() {
         bridgeGame.reset();
+
     }
 
 
--- src/main/java/bridge/domain/BridgeSize.java
+++ src/main/java/bridge/domain/BridgeSize.java
@@ -8,19 +8,24 @@ public class BridgeSize {
 
     private static final Pattern pattern = Pattern.compile("^[0-9]*$");
 
-    public boolean isNumber(String str){
+    private boolean isNumber(String str){
         if(!(pattern.matcher(str).matches())) {
             throw new IllegalArgumentException(ExceptionMessage.isNumberMsg());
         }
         return true;
     }
 
-    public boolean isRangeNumber(String str) {
+    private boolean isRangeNumber(String str) {
         int size = Integer.parseInt(str);
         if(!(size >=3 && size <=20)) {
             throw new IllegalArgumentException(ExceptionMessage.isRangeNumberMsg());
         }
         return true;
+    }
+
+    public void checkNumber(String str){
+        isNumber(str);
+        isRangeNumber(str);
     }
 
 
