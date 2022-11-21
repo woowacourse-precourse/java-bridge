926982ebc02c5b0d36619d110975b0efb757a27b
ingpyo <juninpyo72@gmail.com>
2022-11-21 20:53:37 +0900

    feat:0,1을 D,U로 바꿔주는 기능

--- src/main/java/bridge/BridgeMaker.java
+++ src/main/java/bridge/BridgeMaker.java
@@ -7,17 +7,24 @@ import java.util.List;
  */
 public class BridgeMaker {
 
-    private final BridgeNumberGenerator bridgeNumberGenerator;
+	private final BridgeNumberGenerator bridgeNumberGenerator;
 
-    public BridgeMaker(BridgeNumberGenerator bridgeNumberGenerator) {
-        this.bridgeNumberGenerator = bridgeNumberGenerator;
-    }
+	public BridgeMaker(BridgeNumberGenerator bridgeNumberGenerator) {
+		this.bridgeNumberGenerator = bridgeNumberGenerator;
+	}
 
-    /**
-     * @param size 다리의 길이
-     * @return 입력받은 길이에 해당하는 다리 모양. 위 칸이면 "U", 아래 칸이면 "D"로 표현해야 한다.
-     */
-    public List<String> makeBridge(int size) {
-        return null;
-    }
+	/**
+	 * @param size 다리의 길이
+	 * @return 입력받은 길이에 해당하는 다리 모양. 위 칸이면 "U", 아래 칸이면 "D"로 표현해야 한다.
+	 */
+	public List<String> makeBridge(int size) {
+		return null;
+	}
+
+	public String changeNumber(int number) {
+		if (number == 0)
+			return Constant.SPELLING_D;
+		return Constant.SPELLING_U;
+	}
+
 }
