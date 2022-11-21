252e2427601d3c2d9abbba28513593ef50d0cd28
Sungjaeyeong <wodud2587@gmail.com>
2022-11-21 15:41:09 +0900

    test(bridge): printResult 메서드 테스트

--- src/test/java/bridge/OutputViewTest.java
+++ src/test/java/bridge/OutputViewTest.java
@@ -1,6 +1,5 @@
 package bridge;
 
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -9,6 +8,8 @@ import java.io.ByteArrayOutputStream;
 import java.io.OutputStream;
 import java.io.PrintStream;
 import java.util.Arrays;
+
+import static org.assertj.core.api.Assertions.*;
 
 class OutputViewTest {
 
@@ -26,21 +27,37 @@ class OutputViewTest {
     @DisplayName("printNowMap 성공 테스트")
     void printMap1() {
         outputView.printMap(1, true, "U");
-        Assertions.assertThat(output.toString()).isEqualTo("[ O ]\n[   ]\n");
+        assertThat(output.toString()).isEqualTo("[ O ]\n[   ]\n");
     }
 
     @Test
     @DisplayName("printNowMap 실패 테스트")
     void printMap2() {
         outputView.printMap(1, false, "U");
-        Assertions.assertThat(output.toString()).isEqualTo("[ X ]\n[   ]\n");
+        assertThat(output.toString()).isEqualTo("[ X ]\n[   ]\n");
     }
 
     @Test
     @DisplayName("printPreMap 테스트")
     void printMap3() {
         outputView.printMap(2, true, "U");
-        Assertions.assertThat(output.toString()).isEqualTo("[ O | O ]\n[   |   ]\n");
+        assertThat(output.toString()).isEqualTo("[ O | O ]\n[   |   ]\n");
+    }
+
+    @Test
+    @DisplayName("printResult 성공 테스트")
+    void printResult1() {
+        outputView.printResult(true, 0);
+        assertThat(output.toString()).contains("성공",
+                "1");
+    }
+
+    @Test
+    @DisplayName("printResult 실패 테스트")
+    void printResult2() {
+        outputView.printResult(false, 0);
+        assertThat(output.toString()).contains("실패",
+                "0");
     }
 
 }
