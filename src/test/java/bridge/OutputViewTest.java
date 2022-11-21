ca7bd9ba6c2150f87bc5f3d3430c4f2b599810dc
kissshot1104 <kissshot.heartunderblade1104@gmail.com>
2022-11-21 18:11:28 +0900

    Test(OutputViewTest) : 게임 최종 결과 출력 기능 테스트 코드 추가
    OutputView 클래스의
    public void printResult(String upSideBridge, String downSideBridge, int count)
    메소드의 테스트 코드를 추가했습니다.

--- src/test/java/bridge/OutputViewTest.java
+++ src/test/java/bridge/OutputViewTest.java
@@ -1,12 +1,9 @@
 package bridge;
 
-import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeTest;
-import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.util.Lists.newArrayList;
 
-import camp.nextstep.edu.missionutils.test.NsTest;
 import java.util.List;
+
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.DisplayName;
 
@@ -16,7 +13,7 @@ class OutputViewTest extends CustomNsTest {
 
     @Test
     @DisplayName("현재 다리 상황 출력 테스트")
-    void printMapTest(){
+    void printMapTest() {
 
         //위 1, 3 아래 2 맞췄을 때
         {
@@ -67,6 +64,73 @@ class OutputViewTest extends CustomNsTest {
         }
     }
 
+    @Test
+    @DisplayName("최종 결과 출력 테스트")
+    void printResult() {
 
+        //번갈아 가며 맞았을 경우
+        {
+            String upSideBridge = "[ O |   | O ]";
+            String downSideBridge = "[   | O |   ]";
+            int count = 2;
 
+            outputView.printResult(upSideBridge, downSideBridge, count);
+            assertThat(output()).contains(
+                    "최종 게임 결과",
+                    "[ O |   | O ]",
+                    "[   | O |   ]",
+                    "게임 성공 여부: 성공",
+                    "총 시도한 횟수: 2"
+            );
+        }
+
+        //윗줄 다 맞았을 경우
+        {
+            String upSideBridge = "[ O | O | O ]";
+            String downSideBridge = "[   |   |   ]";
+            int count = 3;
+
+            outputView.printResult(upSideBridge, downSideBridge, count);
+            assertThat(output()).contains(
+                    "최종 게임 결과",
+                    "[ O | O | O ]",
+                    "[   |   |   ]",
+                    "게임 성공 여부: 성공",
+                    "총 시도한 횟수: 3"
+            );
+        }
+
+        //아랫줄 다 맞았을 경우
+        {
+            String upSideBridge = "[   |   |   ]";
+            String downSideBridge = "[ O | O | O ]";
+            int count = 4;
+
+            outputView.printResult(upSideBridge, downSideBridge, count);
+            assertThat(output()).contains(
+                    "최종 게임 결과",
+                    "[   |   |   ]",
+                    "[ O | O | O ]",
+                    "게임 성공 여부: 성공",
+                    "총 시도한 횟수: 4"
+            );
+        }
+
+        //중간에 틀렸을 경우
+        {
+            String upSideBridge = "[   | X ]";
+            String downSideBridge = "[ O |   ]";
+            int count = 5;
+
+            outputView.printResult(upSideBridge, downSideBridge, count);
+            assertThat(output()).contains(
+                    "최종 게임 결과",
+                    "[   | X ]",
+                    "[ O |   ]",
+                    "게임 성공 여부: 실패",
+                    "총 시도한 횟수: 5"
+            );
+        }
+
+    }
 }
