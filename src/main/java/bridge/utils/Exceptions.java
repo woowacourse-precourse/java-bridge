f354903b6693f7d49788eecb3c50d5a17d1fc7a2
smc2315 <choeyongtae085@gmail.com>
2022-11-21 18:49:37 +0900

    feat[InputView]: 사용자 입력

---
+++ src/main/java/bridge/utils/Exceptions.java
@@ -0,0 +1,8 @@
+package bridge.utils;
+
+public class Exceptions {
+    public static final String USER_INPUT_NULL_EXCEPTION = "[ERROR] 값을 입력하여 주세요.";
+    public static final String USER_INPUT_NOT_INTEGER_EXCEPTION = "[ERROR] 숫자를 입력해 주세요.";
+    public static final String USER_INPUT_NOT_U_OR_D = "[ERROR] U 혹은 D를 입력해주세요.";
+    public static final String USER_INPUT_NOT_R_OR_Q = "[ERROR] R 또는 Q를 입력해주세요.";
+}
