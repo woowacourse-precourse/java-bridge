427f5db829bf3da581f152432fe7ac475cd4bf2f
jamangstangs <jamangsong@gmail.com>
2022-11-22 00:48:50 +0900

    feat(OutputView): printMap() -> 이동한 다리의 상태를 형식에 맞춰 출력한다.

--- src/main/java/bridge/OutputView.java
+++ src/main/java/bridge/OutputView.java
@@ -25,14 +25,32 @@ public class OutputView {
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
     public void printMap(List<String> bridge, List<String> route) {
-
+        System.out.printf("[%s]\n", decidePartialMap(bridge, route, "U"));
+        System.out.printf("[%s]\n\n", decidePartialMap(bridge, route, "D"));
     }
 
-    public void printEnding(List<String> bridge, List<String> route, Integer gameRestartCount) {
+    private String decidePartialMap(List<String> bridge, List<String> route, String location) {
+        // 양쪽 공백 + 처음이면 | 안더한다.
+        String partialMap = " " + rightOrWrongStep(bridge.get(0), route.get(0), location) + " ";
+        for (int step = 1; step < route.size(); step++) {
+            partialMap += "| " + rightOrWrongStep(bridge.get(step), route.get(step), location) + " ";
+        }
+        return partialMap;
+    }
+
+    private String rightOrWrongStep(String bridgeScaffolding, String step, String location) {
+        if (bridgeScaffolding.equals(step) && bridgeScaffolding.equals(location)) return "O";
+        if (!bridgeScaffolding.equals(step) && !bridgeScaffolding.equals(location)) return "X";
+        return " ";
+    }
+
+
+    public void printResult(List<String> bridge, List<String> route, List<Integer> gameRestartCount) {
         System.out.println("최종 게임 결과");
         printMap(bridge, route);
-        System.out.println("게임 성공 여부: " + bridge.equals(route));
-        System.out.println("총 시도한 횟수: " + gameRestartCount);
+        if (bridge.equals(route)) System.out.println("게임 성공 여부: 성공");
+        if (!bridge.equals(route)) System.out.println("게임 성공 여부: 실패");
+        System.out.println("총 시도한 횟수: " + gameRestartCount.get(0));
 
     }
 
