a395eb1b6b2bbb4e203176e125359fb67c196205
YuHayeon <youha0930@gmail.com>
2022-11-21 19:21:19 +0900

    feat(BridgeResultTest): Bridge의 결과로 올바른 성공여부를 판단하는지 테스트

--- src/test/java/bridge/model/BridgeResultTest.java
+++ src/test/java/bridge/model/BridgeResultTest.java
@@ -14,4 +14,13 @@ class BridgeResultTest {
         assertThat(new BridgeResult(List.of(true, true, false)).getLast()).isEqualTo(false);
         assertThat(new BridgeResult(List.of(true, true, true)).getLast()).isEqualTo(true);
     }
+
+    @Test
+    void checkSuccess() {
+        Bridge bridge = new Bridge(List.of("U", "D", "u"));
+        assertThat(new BridgeResult(List.of(true, true, true)).checkSuccess(bridge)).isEqualTo(true);
+        assertThat(new BridgeResult(List.of(true, true, false)).checkSuccess(bridge)).isEqualTo(false);
+        assertThat(new BridgeResult(List.of(true, true)).checkSuccess(bridge)).isEqualTo(false);
+        assertThat(new BridgeResult(List.of(true, false)).checkSuccess(bridge)).isEqualTo(false);
+    }
 }
