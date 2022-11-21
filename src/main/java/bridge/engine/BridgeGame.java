eb1f6333ca3eaf2dd37a84428c5d4512581467d2
UJ15 <dbwogml15@naver.com>
2022-11-21 15:56:01 +0900

    feat : implement move method in BridgeGame

--- src/main/java/bridge/engine/BridgeGame.java
+++ src/main/java/bridge/engine/BridgeGame.java
@@ -1,6 +1,10 @@
 package bridge.engine;
 
+import bridge.error.Error;
+import bridge.model.Crossing;
+import bridge.model.PassOrFail;
 import bridge.model.TryCount;
+import java.util.Objects;
 
 /**
  * 다리 건너기 게임을 관리하는 클래스
@@ -18,7 +22,12 @@ public class BridgeGame {
      * <p>
      * 이동을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void move() {
+    public void move(Crossing crossing, PassOrFail passOrFail) {
+        if (Objects.isNull(crossing) || Objects.isNull(passOrFail)) {
+            throw new IllegalArgumentException(Error.SYSTEM_ERROR.getMessage());
+        }
+
+        crossing.savePassOrFail(passOrFail);
     }
 
     /**
