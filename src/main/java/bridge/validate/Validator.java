541abbee217b2ae87d55a666d7c629c0ddebcf9b
splitube <voicesplit@daum.net>
2022-11-21 13:52:54 +0900

    refactor(Validator): 상수명 변경

--- src/main/java/bridge/validate/Validator.java
+++ src/main/java/bridge/validate/Validator.java
@@ -5,10 +5,10 @@ import java.util.List;
 public class Validator {
     private static final int MIN_BRIDGE_LENGTH = 3;
     private static final int MAX_BRIDGE_LENGTH = 20;
-    private static final String MOVE_UPPER_BRIDGE = "U";
-    private static final String MOVE_LOWER_BRIDGE = "D";
-    private static final String RETRY = "R";
-    private static final String QUIT = "Q";
+    private static final String MOVE_UP = "U";
+    private static final String MOVE_LOW = "D";
+    private static final String RETRY_COMMAND = "R";
+    private static final String QUIT_COMMAND = "Q";
 
     public static void isNum(String input) {
         input.chars().forEach(c -> {
@@ -22,7 +22,7 @@ public class Validator {
     public static void isProperBridgeSize(String bridgeSizeInput) {
         int bridgeSize = Integer.parseInt(bridgeSizeInput);
         if (!isInRange(bridgeSize, MIN_BRIDGE_LENGTH, MAX_BRIDGE_LENGTH)) {
-            String errorMessage = "" + ErrorMessage.ERROR + ErrorMessage.NUMBER_OUT_OF_RANGE
+            String errorMessage = ErrorMessage.ERROR.toString() + ErrorMessage.NUMBER_OUT_OF_RANGE
                     + ErrorMessage.PROPER_BRIDGE_LENGTH;
             throw new IllegalArgumentException(errorMessage);
         }
@@ -33,17 +33,17 @@ public class Validator {
     }
 
     public static void isMove(String move) {
-        final List<String> properMoves = List.of(MOVE_UPPER_BRIDGE, MOVE_LOWER_BRIDGE);
+        final List<String> properMoves = List.of(MOVE_UP, MOVE_LOW);
         if (!properMoves.contains(move)) {
-            String errorMessage = "" + ErrorMessage.ERROR + ErrorMessage.NOT_PROPER_MOVE;
+            String errorMessage = ErrorMessage.ERROR.toString() + ErrorMessage.NOT_PROPER_MOVE;
             throw new IllegalArgumentException(errorMessage);
         }
     }
 
     public static void isProperGameCommand(String gameCommand) {
-        final List<String> properMoves = List.of(RETRY, QUIT);
+        final List<String> properMoves = List.of(RETRY_COMMAND, QUIT_COMMAND);
         if (!properMoves.contains(gameCommand)) {
-            String errorMessage = "" + ErrorMessage.ERROR + ErrorMessage.NOT_PROPER_GAME_COMMAND;
+            String errorMessage = ErrorMessage.ERROR.toString() + ErrorMessage.NOT_PROPER_GAME_COMMAND;
             throw new IllegalArgumentException(errorMessage);
         }
     }
