fde9376c5c35780bfba96ff1085e47b435fe76e2
Minsoo Choi <lucas0209@yonsei.ac.kr>
2022-11-22 02:02:07 +0900

    feat(Application): init methods to make answer to cross the bridge
    Features added:
    - Ask user for bridge size and generate random directions for the answer
    - Thrown IllegalArgumentException is handled and bridge size is asked again

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -13,12 +13,24 @@ public class Application {
         User user = new User();
 
         greetings();
+        List<String> answer = init(inputView, bm);
     }
 
     public static void greetings() {
         System.out.println("다리 건너기 게임을 시작합니다.\n");
     }
 
+    // 다리 생성, 길이 초기 입력
+    private static List<String> init(InputView inputView, BridgeMaker bm) {
+        try {
+            int sz = inputView.readBridgeSize();
+            List<String> moveAnswer = bm.makeBridge(sz);
+            return moveAnswer;
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+            return init(inputView, bm);
+        }
+    }
 
 
 }
