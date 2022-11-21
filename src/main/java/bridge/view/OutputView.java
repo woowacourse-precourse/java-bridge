daefc9b4df0abfc890d0e05d596c5b99e9786af4
Cyma-s <ssol0319@naver.com>
2022-11-22 00:54:18 +0900

    feat(OutputView) : add a function that prints starting sentence

--- src/main/java/bridge/view/OutputView.java
+++ src/main/java/bridge/view/OutputView.java
@@ -13,7 +13,11 @@ public class OutputView {
     private static final String BRIDGE_END = " ]";
     private static final String BRIDGE_PARTITION = " | ";
     private static final String BRIDGE_BLANK = " ";
+    private static final String START_STRING = "다리 건너기 게임을 시작합니다.";
 
+    public void printStart() {
+        System.out.println(START_STRING);
+    }
 
     /**
      * 현재까지 이동한 다리의 상태를 정해진 형식에 맞춰 출력한다.
