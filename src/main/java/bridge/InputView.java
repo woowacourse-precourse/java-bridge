f1f22a23203ae9226a0ec32af94259d9224c2cc2
noparamin <noparamin@naver.com>
2022-11-21 21:45:59 +0900

    refactor(InputView): 입력 전 메시지 추가

--- src/main/java/bridge/InputView.java
+++ src/main/java/bridge/InputView.java
@@ -12,6 +12,7 @@ public class InputView {
      * 다리의 길이를 입력받는다.
      */
     public int readBridgeSize() {
+        System.out.println("다리의 길이를 입력해주세요.");
         String inputBridgeLength = Console.readLine();
         inputException.validateReadBridgeSize(inputBridgeLength);
 
@@ -22,6 +23,7 @@ public class InputView {
      * 사용자가 이동할 칸을 입력받는다.
      */
     public String readMoving() {
+        System.out.println("이동할 칸을 선택해주세요. (위: U, 아래: D)");
         String inputMovingPosition = Console.readLine();
         inputException.validateReadMoving(inputMovingPosition);
 
@@ -32,6 +34,7 @@ public class InputView {
      * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
      */
     public String readGameCommand() {
+        System.out.println("게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)");
         String inputCommand = Console.readLine();
         inputException.validateReadGameCommand(inputCommand);
 
