aaf45a204d57841633583059df0c7b9b71a4beaf
ji-mango <susie000301@naver.com>
2022-11-21 15:05:25 +0900

    refactor: print하는 부분 모두 InputView 클래스로 옮김

--- src/main/java/bridge/InputView.java
+++ src/main/java/bridge/InputView.java
@@ -11,6 +11,7 @@ public class InputView {
      * 다리의 길이를 입력받는다.
      */
     public int readBridgeSize() {
+        System.out.println("\n다리의 길이를 입력해주세요.");
         String input = Console.readLine();
         checkIsDigit(input);
 
@@ -24,6 +25,7 @@ public class InputView {
      * 사용자가 이동할 칸을 입력받는다.
      */
     public String readMoving() {
+        System.out.println("\n이동할 칸을 선택해주세요. (위: U, 아래: D)");
         String moving = Console.readLine();
         checkMoving(moving);
 
@@ -34,6 +36,7 @@ public class InputView {
      * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
      */
     public String readGameCommand() {
+        System.out.println("\n게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)");
         String command = Console.readLine();
         checkCommand(command);
 
