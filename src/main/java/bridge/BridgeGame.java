79bd9b397aebeeae33797de0864a445a02cdfe64
poter0501 <poter0501@gmail.com>
2022-11-21 23:16:02 +0900

    feat(BridgeGame): 생성자 및 필드 수정
    경로를 저장하는 path의 타입과  생성자 수정

--- src/main/java/bridge/BridgeGame.java
+++ src/main/java/bridge/BridgeGame.java
@@ -1,18 +1,22 @@
 package bridge;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * 다리 건너기 게임을 관리하는 클래스
  */
 public class BridgeGame {
-    private List<String> path;
+    HashMap<String, List<String>> path;
     private List<String> bridge;
-    private int tryCount=0;
+    private int tryCount=1;
 
     public BridgeGame(List<String> bridge){
-        this.path=new ArrayList<>();
+        List<String> temp = new ArrayList<>(List.of("["));
+        List<String> temp2 = new ArrayList<>(List.of("["));
+        this.path=new HashMap<>(Map.of("U", temp, "D",temp2));
         this.bridge=bridge;
     }
 
@@ -21,7 +25,8 @@ public class BridgeGame {
      * <p>
      * 이동을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void move() {
+    public List<String> move(String movingCommand) {
+        return null;
     }
 
     /**
