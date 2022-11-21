1ece1c19c1a08a6d8501201653e4dcd952e1b182
tjddnr7760 <tjddnr7760@naver.com>
2022-11-22 02:03:02 +0900

    refactor : 출력함수 기능 세분화

--- src/main/java/bridge/OutputView.java
+++ src/main/java/bridge/OutputView.java
@@ -6,6 +6,7 @@ import java.util.List;
 public class OutputView {
     public void printStart() {
         System.out.println("다리 건너기 게임을 시작합니다.");
+        System.out.println();
     }
 
     //현재까지 이동한 다리의 상태를 정해진 형식에 맞춰 출력한다.
@@ -14,17 +15,25 @@ public class OutputView {
         for(int index = 0; index < result.size(); index++) {
             System.out.println(result.get(index));
         }
+        System.out.println();
     }
 
     //게임의 최종 결과를 정해진 형식에 맞춰 출력한다.
     //출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
-    public void printResult(List<String> result, String gamestatus, int trynum) {
+    public void printGameResult(List<String> result) {
         System.out.println("최종 게임 결과");
 
         for(int index = 0; index < result.size(); index++) {
             System.out.println(result.get(index));
         }
+        System.out.println();
+    }
+
+    public void printStatusResult(String gamestatus) {
         System.out.println("게임 성공 여부: " + gamestatus);
+    }
+
+    public void printTimesResult(int trynum) {
         System.out.println("총 시도한 횟수: " + trynum);
     }
 }
