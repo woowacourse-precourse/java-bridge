459d58dfe4e2dd3a020f8505b1dd3bc0d2b93253
60jong <rudwhd515@gmail.com>
2022-11-22 03:26:21 +0900

    feat(OutputView) : '라운드마다 사용자 입력에 따른 다리 결과를 출력할 수 있다.' 기능 구현

--- src/main/java/bridge/view/OutputView.java
+++ src/main/java/bridge/view/OutputView.java
@@ -29,7 +29,9 @@ public class OutputView {
      * <p>
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void printMap() {
+    public void printMap(Player player) {
+        System.out.println(String.format("[ %s ]", String.join(" | ", player.getUpperFootPrint())));
+        System.out.println(String.format("[ %s ]", String.join(" | ", player.getLowerFootPrint())));
     }
 
     /**
