7d94218acdeb9bcb18dc5ecac312c516670a5c89
JunginPark4364 <wjddlsee@gmail.com>
2022-11-22 04:14:16 +0900

    test: Direction 단위 테스트 추가

---
+++ src/test/java/bridge/DirectionTest.java
@@ -0,0 +1,17 @@
+package bridge;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import static org.assertj.core.api.Assertions.*;
+
+class DirectionTest {
+    @DisplayName("UP, DOWN에 해당하는 숫자를 문자열로 변경한다.")
+    @ParameterizedTest
+    @CsvSource({"1,U", "0,D"})
+    public void changeDirectionNumberToDirection(int directionNumber, String direction) {
+        String result = Direction.findByDirectionNumber(directionNumber);
+        assertThat(result).isEqualTo(direction);
+    }
+}
