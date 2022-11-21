a8b0bad63dcbf7c28414f40f48dfb3a3c1de1628
yxnxx <jh4rgh0@gmail.com>
2022-11-21 21:32:11 +0900

    feat(GamePlayer): add playing game

--- src/main/java/bridge/controller/GamePlayer.java
+++ src/main/java/bridge/controller/GamePlayer.java
@@ -1,7 +1,18 @@
 package bridge.controller;
 
-public class GamePlayer {
-    public void play() {
+import bridge.BridgeRandomNumberGenerator;
+import bridge.model.BridgeMaker;
+import bridge.view.InputView;
 
+import java.util.ArrayList;
+import java.util.List;
+
+public class GamePlayer {
+    private static final InputView inputView = new InputView();
+    private final BridgeMaker bridgeMaker = new BridgeMaker(new BridgeRandomNumberGenerator());
+    private List<String> bridge = new ArrayList<>();
+    public void play() {
+        bridge = bridgeMaker.makeBridge(inputView.readBridgeSize());
+        BridgeGame bridgeGame = new BridgeGame();
     }
 }
