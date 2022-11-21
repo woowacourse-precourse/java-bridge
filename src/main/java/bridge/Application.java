268eb938b0a553194aa0895d19a7c3a0847c8ace
aksmf1442 <aksmf1442@naver.com>
2022-11-21 15:45:31 +0900

    feat(다리 건너기): 다리 길이를 입력받는 기능 구현

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -3,6 +3,9 @@ package bridge;
 public class Application {
 
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        InputView inputView = new InputView();
+
+        BridgeMaker bridgeMaker = new BridgeMaker(new BridgeRandomNumberGenerator());
+        bridgeMaker.makeBridge(inputView.readBridgeSize());
     }
 }
