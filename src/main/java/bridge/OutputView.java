76e9200056771f29ab6992276cf1a699cb10e38f
이현진 <ehj1107@naver.com>
2022-11-22 04:35:44 +0900

    refactor: 최종 게임 결과 출력

--- src/main/java/bridge/OutputView.java
+++ src/main/java/bridge/OutputView.java
@@ -25,13 +25,16 @@ public class OutputView {
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
     public void printResult() {
-        System.out.println(Message.GAME_FINAL_RESULT);
-        List<String> answer = getAnswer(Application.bridge, BridgeGame.playerStep);
-        printBridge(answer, BridgeGame.playerStep);
-
+        printFinalGame();
         System.out.println();
         printGameSuccessOrFail();
         printGameTryCount();
+    }
+
+    private void printFinalGame(){
+        System.out.println(Message.GAME_FINAL_RESULT);
+        List<String> answer = getAnswer(Application.bridge, BridgeGame.playerStep);
+        printBridge(answer, BridgeGame.playerStep);
     }
 
     private void printGameTryCount() {
