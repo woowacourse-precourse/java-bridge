6a01d278b36456a1560f221d6e05333729b974da
khyojun <38220795+khyojun@users.noreply.github.com>
2022-11-21 12:19:17 +0900

    refactor(bridge): BridgeGame afterMove과정 객체 분리

---
+++ src/main/java/bridge/domain/BridgeDestination.java
@@ -0,0 +1,57 @@
+package bridge.domain;
+
+import bridge.constant.GameState;
+import bridge.view.InputView;
+import bridge.view.OutputView;
+import java.util.ArrayList;
+import java.util.List;
+
+public class BridgeDestination {
+
+
+    private List<Boolean> upState = new ArrayList<>();
+    private List<Boolean> downState = new ArrayList<>();
+
+    private BridgeProcess bridgeProcess = new BridgeProcess();
+    private OutputView outputView = new OutputView();
+
+    private InputView inputView = new InputView();
+    private final Boolean GAME_OVER = false;
+    private final Boolean RESTART = true;
+
+    public boolean isReachFinal(int bridgeSize,int idx, int gameCount) {
+        if (!BridgePrinting.isMoveStop()) {
+            setQuit(bridgeSize, idx,gameCount);
+            return true;
+        }
+        return false;
+    }
+
+    public void setQuit(int bridgeSize, int idx, int gameCount) {
+        final int final_idx=idx-1;
+        int nowIndex = convertNowIndex(bridgeProcess.getMoving().get(final_idx));
+        BridgePrinting resultBridgePrinting = new BridgePrinting(upState, downState, nowIndex);
+        if (bridgeSize == idx && !BridgePrinting.isMoveStop()) {
+            outputView.printResult(gameCount, resultBridgePrinting, true);
+            return;
+        }
+        outputView.printResult(gameCount, resultBridgePrinting, false);
+    }
+
+    private int convertNowIndex(String nowIndex) {
+        if (nowIndex.equals(GameState.UP)) {
+            return GameState.UP_STATEMENT;
+        }
+        return GameState.DOWN_STATEMENT;
+    }
+
+
+    public boolean judgeRestartOrOver() {
+        String destination = inputView.readGameCommand();
+        if (destination.equals(GameState.QUIT)) {
+            return GAME_OVER;
+        }
+        return RESTART;
+    }
+
+}
