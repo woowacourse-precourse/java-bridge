6f50e12c843d6126d5ac0be3452581cef613873e
jiwon <ljwon77@daum.net>
2022-11-22 01:04:27 +0900

    chore: modify constant output message
    결과 메시지에 개행 문자를 추가하였습니다.

--- src/main/java/bridge/constant/message/OutputMessage.java
+++ src/main/java/bridge/constant/message/OutputMessage.java
@@ -3,7 +3,7 @@ package bridge.constant.message;
 public enum OutputMessage {
 
     RESULT("최종 게임 결과")
-    , SUCCESS_OR_FAILURE("게임 성공 여부: %s")
+    , SUCCESS_OR_FAILURE("게임 성공 여부: %s\n")
     , TOTAL_ATTEMPTS("총 시도한 횟수: %d")
     ;
 
