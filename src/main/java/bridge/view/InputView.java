50a337cab27519657df55735c26af94e15ea30ae
hhiyeon <hhyyeon0214@gmail.com>
2022-11-21 14:40:50 +0900

    feat:inputview-read bridge size number

--- src/main/java/bridge/view/InputView.java
+++ src/main/java/bridge/view/InputView.java
@@ -1,5 +1,8 @@
 package bridge.view;
 
+import bridge.exception.BridgeSize;
+import camp.nextstep.edu.missionutils.Console;
+
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
  */
@@ -9,7 +12,10 @@ public class InputView {
      * 다리의 길이를 입력받는다.
      */
     public int readBridgeSize() {
-        return 0;
+        String bridgeSizeInput = Console.readLine();
+        BridgeSize bridgeSize = new BridgeSize(bridgeSizeInput);
+        
+        return Integer.parseInt(bridgeSizeInput);
     }
 
     /**
