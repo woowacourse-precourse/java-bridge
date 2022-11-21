9fe11e70a07c5c3da48e25633bde22cdd2de65b2
jagajang <kosb7544@gmail.com>
2022-11-21 13:08:02 +0900

    refactor: seperate declaration and definition

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -4,11 +4,16 @@ public class Application {
 
     public static void main(String[] args) {
         // TODO: 프로그램 구현
-        InputView inputView = new InputView();
-        OutputView outputView = new OutputView();
+        InputView inputView;
+        OutputView outputView;
+        BridgeMaker bridgeMaker;
+        BridgeGame bridgeGame;
 
-        BridgeMaker bridgeMaker = new BridgeMaker(new BridgeRandomNumberGenerator());
-        BridgeGame bridgeGame = new BridgeGame(bridgeMaker, inputView, outputView);
+        inputView = new InputView();
+        outputView = new OutputView();
+        bridgeMaker = new BridgeMaker(new BridgeRandomNumberGenerator());
+        bridgeGame = new BridgeGame(bridgeMaker, inputView, outputView);
+
         bridgeGame.startGame();
     }
 }
