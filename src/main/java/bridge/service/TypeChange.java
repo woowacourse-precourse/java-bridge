1ca20a73e4238a6bae74da4905d3207c7a82ee1a
jaehyeon <jhjh0022@naver.com>
2022-11-22 02:08:55 +0900

    feat(TypeChange): 숫자인지 검증하는 기능 구현

--- src/main/java/bridge/service/TypeChange.java
+++ src/main/java/bridge/service/TypeChange.java
@@ -2,7 +2,13 @@ package bridge.service;
 
 public class TypeChange {
 
-    public static int ChangeStringToInteger(String size) {
+    private final String Number_REGEX = "^\\d+$";
+
+    public int ChangeStringToInteger(String size) {
+
+        if(!(size.matches(Number_REGEX))){
+            throw new IllegalArgumentException("숫자를 입력해주세요");
+        }
         return Integer.parseInt(size);
     }
 }
