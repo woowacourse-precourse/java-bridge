6982fe2ad5accc9a669c4424644335aff5f74f23
bjk1649 <qhwns1649@naver.com>
2022-11-22 02:23:54 +0900

    test: BridgeGame의 각 메소드에 대한 단위 테스트 코드 추가

---
+++ src/test/java/bridge/service/BridgeGameTest.java
@@ -0,0 +1,45 @@
+package bridge.service;
+
+import bridge.bo.Bridge;
+import bridge.util.BridgeMaker;
+import bridge.util.BridgeRandomNumberGenerator;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.assertThat;
+
+class BridgeGameTest {
+    BridgeGame bridgeGame;
+
+    @BeforeEach
+    void setUp() {
+        Bridge bridge = new Bridge(new BridgeMaker(new BridgeRandomNumberGenerator()).makeBridge(3));
+        this.bridgeGame = new BridgeGame(bridge);
+        bridgeGame.move("U","U");
+    }
+
+    @Test
+    void move() {
+        boolean realResult = bridgeGame.move("U","D");
+        assertThat(realResult).isFalse().as("MoveFailed");
+        assertThat(bridgeGame.getUserMoves().get(1)).isEqualTo("U").as("moveSaved");
+        assertThat(bridgeGame.getResults().get(1)).isFalse().as("resultSaved");
+    }
+
+    @Test
+    void retry() {
+        int expectedSize = 0;
+        bridgeGame.retry();
+        int realUserMovesSize = bridgeGame.getUserMoves().size();
+        int realResultsSize = bridgeGame.getResults().size();
+        int realTries = bridgeGame.getTries();
+        assertThat(realUserMovesSize).as("userMovesClear").isEqualTo(expectedSize);
+        assertThat(realResultsSize).as("resultsClear").isEqualTo(expectedSize);
+        assertThat(realTries).as("triesNumberPlus").isEqualTo(2);
+    }
+
+    @Test
+    void finish() {
+        bridgeGame.finish();
+        assertThat(bridgeGame.getBridgeIterator().hasNext()).isFalse();
+    }
+}
