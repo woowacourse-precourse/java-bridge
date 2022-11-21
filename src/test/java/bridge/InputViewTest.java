c2b1860d2cf984ea6e033f78d71840f44cb1b59f
kwang <shinkwang@yonsei.ac.kr>
2022-11-22 01:29:13 +0900

    test: Make test for readBridgeSize method
    다리 길이를 입력받는 메소드의 테스트를 만든다. 입력 예외 테스트와 성공 테스트를 만들어 두 테스트 모두 성공하는지 확인한다.

--- src/test/java/bridge/InputViewTest.java
+++ src/test/java/bridge/InputViewTest.java
@@ -11,6 +11,31 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class InputViewTest {
 
+    @ValueSource(strings = {"A", "가", "0", "-50", "2", "21", "100"})
+    @ParameterizedTest
+    void 다리길이_입력_예외테스트(String readSize) {
+        //given
+        InputView inputView = new InputView();
+
+        //when, then
+        assertThatThrownBy(() -> inputView.readBridgeSize(readSize))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @ValueSource(strings = {"3", "10", "20"})
+    @ParameterizedTest
+    void 다리길이_입력_테스트(String readSize) {
+        //given
+        InputView inputView = new InputView();
+
+        //when
+        int size = inputView.readBridgeSize(readSize);
+
+        //then
+        assertThat(size).isEqualTo(Integer.parseInt(readSize));
+    }
+
+
     @ValueSource(strings = {"12", "A", "u", "d", "가"})
     @ParameterizedTest
     void 이동명령_예외테스트(String move) {
