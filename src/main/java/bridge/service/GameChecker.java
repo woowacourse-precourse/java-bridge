858b137dd9e68d118f755336c245f3e45532f5db
오승윤 <>
2022-11-21 12:38:38 +0900

    feat (service/GameChecker) : GameOverChecker, GameWinChecker를 관리하는 기능
    컨트롤러 의존관계 경량화를 위한 Service 레이어

---
+++ src/main/java/bridge/service/GameChecker.java
@@ -0,0 +1,25 @@
+package bridge.service;
+
+import bridge.GameOverChecker;
+import bridge.GameWinChecker;
+import bridge.MovementStatusForm;
+
+import java.util.Deque;
+
+public class GameChecker {
+    private GameOverChecker gameOverChecker;
+    private GameWinChecker gameWinChecker;
+
+    public GameChecker(GameOverChecker gameOverChecker, GameWinChecker gameWinChecker) {
+        this.gameOverChecker = gameOverChecker;
+        this.gameWinChecker = gameWinChecker;
+    }
+
+    public boolean isGameOver(Deque<MovementStatusForm> movementStatus, Integer bridgeSize) {
+        return gameOverChecker.isOver(movementStatus, bridgeSize);
+    }
+
+    public boolean isWin(Deque<MovementStatusForm> movementStatus, Integer bridgeSize) {
+        return gameWinChecker.isWin(movementStatus, bridgeSize);
+    }
+}
