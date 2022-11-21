a810a0f119da880ebdf041d16198b5456af0b459
Nitroblack1 <109022435+Nitroblack1@users.noreply.github.com>
2022-11-22 03:49:41 +0900

    feat(Application) : implement application class to activate bridge game

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -1,14 +1,19 @@
 package bridge;
 
+import static bridge.Constants.StandardTools.SUCCEED;
+
+import bridge.Constants.StandardTools.retry;
 import bridge.Domain.BridgeGame;
 
 public class Application {
 
     public static void main(String[] args) {
-        try {
-            BridgeGame game = new BridgeGame();
-        } catch (IllegalArgumentException e) {
-            System.out.println(e.getMessage());
+        BridgeGame game = new BridgeGame();
+        game.gameStart();
+        while (game.getRetryOrQuit() == retry.RETRY || (game.getIsGameSucceed() != SUCCEED
+                && !game.getIsGameFinished())) {
+            game.move();
         }
     }
+
 }
