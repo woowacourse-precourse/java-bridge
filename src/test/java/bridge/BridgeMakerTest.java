b600fca5d4fe87f54287f5ab4d1e7e2d1021b918
JJ503 <63184334+JJ503@users.noreply.github.com>
2022-11-21 20:08:21 +0900

    test(BridgeMaker): 다리 생성 길이 입력 검증 및 생성, 예외 발생 테스트
    1. 정상 데이터를 입력한 경우
    2. 예외 데이터를 입력한 경우

---
+++ src/test/java/bridge/BridgeMakerTest.java
@@ -0,0 +1,32 @@
+package bridge;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+class BridgeMakerTest {
+    BridgeMaker bridgeMaker = new BridgeMaker(new BridgeRandomNumberGenerator());
+
+    @DisplayName("길이에 따른 다리 생성 - 정상 데이터를 입력한 경우")
+    @ValueSource(ints = {3, 5, 10, 15, 20})
+    @ParameterizedTest
+    void checkNormalBridgeLength(int bridgeLength) {
+        System.out.println(bridgeLength);
+        List<String> bridge = bridgeMaker.makeBridge(bridgeLength);
+
+        assertThat(bridge).size().isEqualTo(bridge.size());
+    }
+
+    @DisplayName("길이에 따른 다리 생성 - 예외 데이터를 입력한 경우")
+    @ValueSource(ints = {1, 2, 21, 22, 100})
+    @ParameterizedTest
+    void checkErrorBridgeLength(int bridgeLength) {
+        assertThatThrownBy(() -> bridgeMaker.makeBridge(bridgeLength))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+}
