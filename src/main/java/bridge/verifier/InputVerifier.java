9271f1c510dda4cb7ae491adb11adc0c547a9c92
leeyongjun <leeyongjun0310@naver.com>
2022-11-21 23:39:32 +0900

    refactor(bridge): gameCommand 상태값 상수로 변경

--- src/main/java/bridge/verifier/InputVerifier.java
+++ src/main/java/bridge/verifier/InputVerifier.java
@@ -1,12 +1,11 @@
 package bridge.verifier;
 
 import bridge.constant.ExceptionMessage;
+import bridge.constant.GameCommand;
 import bridge.constant.Moving;
 
 public class InputVerifier {
     private static final String REGEX_BRIDGE_SIZE_RANGE = "^[3-9]{1}$|^1{1}[0-9]{1}$|^2{1}0{1}$";
-    private static final String GameCommand_RESTART = "R";
-    private static final String GameCommand_QUIT = "Q";
 
     public static void BridgeSize(String bridgeSize) {
         if (!bridgeSize.matches(REGEX_BRIDGE_SIZE_RANGE)) {
@@ -21,7 +20,7 @@ public class InputVerifier {
     }
 
     public static void gameCommand(String gameCommand) {
-        if (!(gameCommand.equals(GameCommand_RESTART) || gameCommand.equals(GameCommand_QUIT))) {
+        if (!(gameCommand.equals(GameCommand.RESTART) || gameCommand.equals(GameCommand.QUIT))) {
             throw new IllegalArgumentException(ExceptionMessage.ERROR_GAME_COMMAND);
         }
     }
