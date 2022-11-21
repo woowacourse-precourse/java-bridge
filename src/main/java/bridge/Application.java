c22f78d914ddc5634bd6d7d3ae927f4cf2700bbe
JohnPrk <uo3641493@gmail.com>
2022-11-21 21:54:29 +0900

    feat(Application, InputView) : 다리 길이 입력 기능 추가

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -2,7 +2,12 @@ package bridge;
 
 public class Application {
 
+    private static final String BRIDGE_GAME_START_MESSAGE = "다리 건너기 게임을 시작합니다.";
+
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        System.out.println(BRIDGE_GAME_START_MESSAGE);
+        InputView inputView = new InputView();
+        int bridgeSize = inputView.readBridgeSize();
     }
 }
