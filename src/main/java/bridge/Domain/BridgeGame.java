04ae6018d7b484b0dbbd2f4dfcb22cf4cda55c01
Sieun Park <psieun0606@gmail.com>
2022-11-22 04:53:36 +0900

    feat: 재시도할 경우 시도 횟수를 증가시키는 기능 추가

--- src/main/java/bridge/Domain/BridgeGame.java
+++ src/main/java/bridge/Domain/BridgeGame.java
@@ -6,6 +6,7 @@ import java.util.List;
  */
 public class BridgeGame {
     private int index;
+    private int attemp = 1;
 
     /**
      * 사용자가 칸을 이동할 때 사용하는 메서드
@@ -27,5 +28,6 @@ public class BridgeGame {
      * 재시작을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
     public void retry() {
+        attemp++;
     }
 }
