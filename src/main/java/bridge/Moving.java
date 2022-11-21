4f9f526f4bfaca94da236d4be873a5d54219569b
Amyhds <dasomhan524@gmail.com>
2022-11-21 18:02:26 +0900

    feat(moving): 검증 메서드와 반환 메서드 구현

--- src/main/java/bridge/Moving.java
+++ src/main/java/bridge/Moving.java
@@ -1,4 +1,16 @@
 package bridge;
 
 public class Moving {
+    private static String moving;
+
+    public static void validateInput(String userMoving) {
+        Validator.validateLength(userMoving);
+        Validator.validateMoving(userMoving);
+        moving = userMoving;
+    }
+
+    public static String getMoving() {
+        return moving;
+    }
+
 }
