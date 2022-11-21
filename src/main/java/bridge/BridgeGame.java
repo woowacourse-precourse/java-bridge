dc4a770b24c916fec8d0e2dfcbbaada343c17c93
kissshot1104 <kissshot.heartunderblade1104@gmail.com>
2022-11-21 18:26:00 +0900

    Feat(BridgeGame) : 게임 종료 기능 추가
    public void endGame()
    메소드를 추가했습니다.
    이 메소드는 게임이 끝 났을 때 결과를 출력하는등
    마무리 작업을 하는 메소드입니다.

--- src/main/java/bridge/BridgeGame.java
+++ src/main/java/bridge/BridgeGame.java
@@ -1,38 +1,25 @@
 package bridge;
 
+import java.util.List;
+
 /**
  * 다리 건너기 게임을 관리하는 클래스
  */
 public class BridgeGame {
+    private MovingProcessing movingProcessing;
+    private final RetryProcessing retryProcessing;
+    private final int count;
 
-    InputView inputView = new InputView();
-    /**
-     * 사용자가 칸을 이동할 때 사용하는 메서드
-     * <p>
-     * 이동을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
-     */
-    public String move() {
-        while (true){
-            System.out.println("이동할 칸을 선택해주세요. (위: U, 아래: D)");
-            String inputKey = inputView.readMoving();
-            if (inputKey.length() != 0) {
-                return inputKey;
-            }
-        }
+    public BridgeGame() {
+        retryProcessing = new RetryProcessing();
+        count = 1;
     }
 
-    /**
-     * 사용자가 게임을 다시 시도할 때 사용하는 메서드
-     * <p>
-     * 재시작을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
-     */
-    public String retry() {
-        while (true){
-            System.out.println("게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)");
-            String inputKey = inputView.readMoving();
-            if (inputKey.length() != 0) {
-                return inputKey;
-            }
-        }
+
+    //여기서 upSide와 downSide를 더해서 출력 하게끔 하자
+    public void endGame() {
+        List<String> upSide = movingProcessing.getUpSide();
+        List<String> downSide = movingProcessing.getDownSide();
+        retryProcessing.printResult(upSide, downSide, count);
     }
 }
