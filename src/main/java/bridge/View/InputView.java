f29eccc3b6573497a2db4f8217147b58997b1765
aossuper7 <aossuper7@naver.com>
2022-11-21 21:24:27 +0900

    feat: 다리 생성 값 검증 후 제대로 된 값이 들어올때까지 입력 받는 기능

--- src/main/java/bridge/View/InputView.java
+++ src/main/java/bridge/View/InputView.java
@@ -1,28 +1,16 @@
 package bridge.View;
 
-/**
- * 사용자로부터 입력을 받는 역할을 한다.
- */
+import bridge.Utils.Verification;
+import camp.nextstep.edu.missionutils.Console;
+
 public class InputView {
 
-    /**
-     * 다리의 길이를 입력받는다.
-     */
-    public int readBridgeSize() {
-        return 0;
-    }
-
-    /**
-     * 사용자가 이동할 칸을 입력받는다.
-     */
-    public String readMoving() {
-        return null;
-    }
-
-    /**
-     * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
-     */
-    public String readGameCommand() {
-        return null;
+    public static int readBridgeSize() {
+        while(true) {
+            String size = Console.readLine();
+            if(Verification.isBridgeNum(size)) {
+                return Integer.parseInt(size);
+            }
+        }
     }
 }
