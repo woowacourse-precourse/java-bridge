6e84f11cec2b935260554a01c799d07283bf5de5
Frog-Slayer <Frog-Slayer@github.com>
2022-11-21 15:07:14 +0900

    feat(InputView): 추가 다리의 길이를 입력 받고 유효성을 체크

--- src/main/java/bridge/view/InputView.java
+++ src/main/java/bridge/view/InputView.java
@@ -1,5 +1,7 @@
 package bridge.view;
 
+import camp.nextstep.edu.missionutils.Console;
+
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
  */
@@ -9,7 +11,18 @@ public class InputView {
      * 다리의 길이를 입력받는다.
      */
     public int readBridgeSize() {
-        return 0;
+        String userInput = Console.readLine();
+        validateInteger(userInput);
+        return Integer.parseInt(userInput);
+    }
+
+    private void validateInteger(String userInput){
+        try {
+            int toInteger = Integer.parseInt(userInput);
+        }
+        catch (NumberFormatException exception){
+            throw new IllegalArgumentException();
+        }
     }
 
     /**
