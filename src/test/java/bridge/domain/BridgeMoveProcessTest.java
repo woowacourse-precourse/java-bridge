22fc223f806fab23e10bede4cf3a9b262882e8fb
khyojun <38220795+khyojun@users.noreply.github.com>
2022-11-21 18:46:31 +0900

    test(bridge): MoveProcess에 대한 테스트 추가

---
+++ src/test/java/bridge/domain/BridgeMoveProcessTest.java
@@ -0,0 +1,56 @@
+package bridge.domain;
+
+import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeTest;
+import static org.assertj.core.api.Assertions.assertThat;
+
+import bridge.Application;
+import bridge.constant.GameState;
+import camp.nextstep.edu.missionutils.test.NsTest;
+import java.util.ArrayList;
+import java.util.List;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+
+public class BridgeMoveProcessTest{
+
+    @Test
+    @DisplayName("사용자의 다리를 그리는 과정에서 잘 그렸는지 확인")
+    void makeUserBridge(){
+        List<Boolean> upState = List.of(true, true, false);
+        List<Boolean> downState = List.of(false, false, true);
+        BridgePrinting bridgePrinting = new BridgePrinting(upState, downState);
+        bridgePrinting.addTraceLocation(GameState.UP_STATEMENT);
+        bridgePrinting.makeBridge();
+        Assertions.assertThat(bridgePrinting.getSize()).isEqualTo(3);
+    }
+
+
+    @Test
+    @DisplayName("위, 아래 다리의 상태를 받아오고 변환하는 과정 - 위 칸을 지나갈 수 있는 경우")
+    void setPrintState_State_UP(){
+        int getState = GameState.UP_STATEMENT;
+        List<Boolean> upState = new ArrayList<>();
+        List<Boolean> downState = new ArrayList<>();
+        if (getState == GameState.UP_STATEMENT) {
+            upState.add(true);
+            downState.add(false);
+        }
+        Assertions.assertThat(upState.get(0)).isEqualTo(true);
+    }
+
+    @Test
+    @DisplayName("위, 아래 다리의 상태를 받아오고 변환하는 과정 - 아래 칸을 지나갈 수 있는 경우")
+    void setPrintState_State_DOWN(){
+        int getState = GameState.DOWN_STATEMENT;
+        List<Boolean> upState = new ArrayList<>();
+        List<Boolean> downState = new ArrayList<>();
+        if (getState == GameState.UP_STATEMENT) {
+            upState.add(false);
+            downState.add(true);
+        }
+        Assertions.assertThat(downState.get(0)).isEqualTo(true);
+    }
+
+}
