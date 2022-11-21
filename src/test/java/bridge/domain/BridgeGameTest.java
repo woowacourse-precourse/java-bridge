51fe68cce3a366e0bc48e3ef9c362edf04e0a316
HwangSeokJin <ghkd3703@gmail.com>
2022-11-21 19:41:28 +0900

    test(BridgeGame): 게임이 아직 시작되지 않았을 때 게임 진행에 관련 된 메소드를 실행하면 예외 처리 테스트 작성

--- src/test/java/bridge/domain/BridgeGameTest.java
+++ src/test/java/bridge/domain/BridgeGameTest.java
@@ -1,25 +1,31 @@
 package bridge.domain;
 
-import bridge.BridgeMaker;
-import bridge.BridgeRandomNumberGenerator;
-
 import java.util.List;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.DisplayName;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class BridgeGameTest {
     @DisplayName("현재까지 건넌 다리를 정확하게 리턴하는지 확인한다.")
     @Test
-    void TestGetCurrentMap() {
+    void testGetCurrentMap() {
         BridgeGame bridgeGame = new BridgeGame();
         bridgeGame.setBridge(List.of("U", "U", "D"));
         bridgeGame.move("U");
         bridgeGame.move("D");
         String[][] result = {{"O", " "}, {" ", "X"}};
         assertThat(isEqual(result, bridgeGame.getCurrentMap())).isEqualTo(true);
+    }
+
+    @DisplayName("게임이 아직 시작되지 않았을 때 게임 진행에 관련 된 메소드를 실행하면 예외 처리 한다.")
+    @Test
+    void playGameWithoutBridge() {
+         BridgeGame bridgeGame = new BridgeGame();
+         assertThatThrownBy(() -> bridgeGame.move("U"))
+                 .isInstanceOf(IllegalArgumentException.class);
     }
 
     private boolean isEqual(String[][] map1, String[][] map2) {
