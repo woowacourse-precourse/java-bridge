1b97f58843e0cd8cd5bcd8e4fac866041f9423fb
qkdk <86948395+qkdk@users.noreply.github.com>
2022-11-22 00:30:57 +0900

    feat: 사용자 이동 기능 구현

--- src/main/java/bridge/BridgeGame.java
+++ src/main/java/bridge/BridgeGame.java
@@ -1,7 +1,13 @@
 package bridge;
 
+import java.util.List;
+
 /**
  * 다리 건너기 게임을 관리하는 클래스
+ * 필드 추가 가능
+ * 패키지 변경 가능
+ * 매서드 이름 변경 불가능, 인자와 반환타입은 추가 변경 가능
+ * 매서드 추가 변경 가능
  */
 public class BridgeGame {
 
@@ -10,7 +16,8 @@ public class BridgeGame {
      * <p>
      * 이동을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void move() {
+    public boolean move(String direction, int targetBlock, List<String> bridge) {
+        return direction.equals(bridge.get(targetBlock));
     }
 
     /**
