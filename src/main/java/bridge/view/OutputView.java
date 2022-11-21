4601957a86a1e772a31240e214f9bc06dbc48de1
nakalic <nakalic28@gmail.com>
2022-11-21 18:15:45 +0900

    feat(OutputView): 게임 진행에 필요한 문구를 출력하는 기능 구현

--- src/main/java/bridge/view/OutputView.java
+++ src/main/java/bridge/view/OutputView.java
@@ -6,6 +6,11 @@ import java.util.List;
 
 public class OutputView {
 
+    private static final String BRIDGE_SIZE = "다리의 길이를 입력해주세요.";
+    private static final String START = "다리 건너기 게임을 시작합니다.";
+    private static final String SPACE_TO_MOVE = "이동할 칸을 선택해주세요. (위: U, 아래: D)";
+    private static final String WHETHER_TO_RETRY = "게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)";
+
     public void printMap(User user) {
         System.out.printf("[ %s ]\n", bridgeToMap(user.topBridge));
         System.out.printf("[ %s ]\n", bridgeToMap(user.bottomBridge));
@@ -28,5 +33,21 @@ public class OutputView {
 
         str += bridge.get(bridge.size() - 1);
         return str;
+    }
+
+    public void askBridgeSize() {
+        System.out.println(BRIDGE_SIZE);
+    }
+
+    public void printStart() {
+        System.out.println(START);
+    }
+
+    public void askSpaceToMove() {
+        System.out.println(SPACE_TO_MOVE);
+    }
+
+    public void askWhetherToRetry() {
+        System.out.println(WHETHER_TO_RETRY);
     }
 }
