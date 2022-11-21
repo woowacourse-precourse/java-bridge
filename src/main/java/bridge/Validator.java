8fe16548d2660ee9a88919c1fd58dc31a9724867
bumwoo <gld55602@gmail.com>
2022-11-21 21:09:36 +0900

    refactor(Validator): 공백 제거 기능 수정

--- src/main/java/bridge/Validator.java
+++ src/main/java/bridge/Validator.java
@@ -9,21 +9,16 @@ public class Validator {
 
     //TODO : 예외 처리 분리
     public void validateBridgeSize(String bridgeSize) {
-        if (!pattern.matcher(bridgeSize.trim()).find()) {
+        if (!pattern.matcher(bridgeSize).find()) {
             throw new IllegalArgumentException();
         }
     }
 
     public void validateMoving(String moving) {
-        moving = moving.trim();
         Direction.from(moving);
     }
 
     public void validateGameCommand(String gameCommand) {
-        gameCommand = gameCommand.trim();
         GameCommand.from(gameCommand);
     }
-
-
-
 }
