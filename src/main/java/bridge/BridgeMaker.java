a4fc77e98973d5168994d3d6c200869f4e23f48f
mckim123 <minchan06@gmail.com>
2022-11-22 01:06:58 +0900

    refactor: wrap some constants which were generated by hard coding
    Wrap some constant and strings which were made by hard coding
    - RANDOM_UPPER_INCLUSIVE : 1
    - "D" to Moving.D.toString()
    - "U" to Moving.U.toString()

--- src/main/java/bridge/BridgeMaker.java
+++ src/main/java/bridge/BridgeMaker.java
@@ -1,10 +1,11 @@
 package bridge;
 
+import bridge.constant.Moving;
 import java.util.ArrayList;
 import java.util.List;
 
 public class BridgeMaker {
-
+    private static final int RANDOM_UPPER_INCLUSIVE = 1;
     private final BridgeNumberGenerator bridgeNumberGenerator;
 
     public BridgeMaker(BridgeNumberGenerator bridgeNumberGenerator) {
@@ -14,9 +15,9 @@ public class BridgeMaker {
     public List<String> makeBridge(int size) {
         List<String> bridge = new ArrayList<>();
         for (int index = 0; index < size; index++) {
-            String nextAnswer = "D";
-            if (bridgeNumberGenerator.generate() == 1) {
-                nextAnswer = "U";
+            String nextAnswer = Moving.D.toString();
+            if (bridgeNumberGenerator.generate() == RANDOM_UPPER_INCLUSIVE) {
+                nextAnswer = Moving.U.toString();
             }
             bridge.add(nextAnswer);
         }
