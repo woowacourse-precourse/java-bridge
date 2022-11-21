f944ac3073b9cc0dc9ae227155abaef82b9489bb
eastsage <dpwk8@naver.com>
2022-11-22 02:21:48 +0900

    style: 주석 제거

--- src/main/java/bridge/view/InputView.java
+++ src/main/java/bridge/view/InputView.java
@@ -3,14 +3,8 @@ package bridge.view;
 import bridge.model.Direction;
 import camp.nextstep.edu.missionutils.Console;
 
-/**
- * 사용자로부터 입력을 받는 역할을 한다.
- */
 public class InputView {
 
-    /**
-     * 다리의 길이를 입력받는다.
-     */
     public int readBridgeSize() {
         String input = Console.readLine();
         try {
@@ -21,9 +15,6 @@ public class InputView {
         return Integer.parseInt(input);
     }
 
-    /**
-     * 사용자가 이동할 칸을 입력받는다.
-     */
     public Direction readMoving() {
         String input = Console.readLine();
         try {
@@ -34,9 +25,6 @@ public class InputView {
         return Direction.valueOf(input);
     }
 
-    /**
-     * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
-     */
     public String readGameCommand() {
         String input = Console.readLine();
         try {
