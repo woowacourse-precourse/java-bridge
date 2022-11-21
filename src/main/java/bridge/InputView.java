c23a35c0ca20e65e88f37cf7fe0a447a55f0cf4b
CHAYEONIL <jeay123@naver.com>
2022-11-21 23:36:55 +0900

    feature - InputView 구성

--- src/main/java/bridge/InputView.java
+++ src/main/java/bridge/InputView.java
@@ -1,28 +1,39 @@
 package bridge;
 
+import camp.nextstep.edu.missionutils.Console;
+
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
  */
 public class InputView {
-
+    public static void GameStart(){
+        System.out.println("다리 건너기 게임을 시작합니다.");
+        System.out.println("");
+    }
     /**
      * 다리의 길이를 입력받는다.
      */
     public int readBridgeSize() {
-        return 0;
+        System.out.println("다리 길이를 입력해주세요");
+        Integer bridgeSize = Integer.valueOf(Console.readLine());
+        return bridgeSize;
     }
 
     /**
      * 사용자가 이동할 칸을 입력받는다.
      */
     public String readMoving() {
-        return null;
+        System.out.println("이동할 칸을 선택해주세요. (위: U, 아래: D)");
+        String Moving = Console.readLine();
+        return Moving;
     }
 
     /**
      * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
      */
     public String readGameCommand() {
-        return null;
+        System.out.println("게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)");
+        String gameCommand = Console.readLine();
+        return gameCommand;
     }
 }
