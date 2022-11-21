5184751c025cbb154e3ccc141bd53f455197396e
yunjeong <uu0078@naver.com>
2022-11-21 23:45:58 +0900

    feat: 다리 길이 입력 요청 및 입력받기 (3 ~ 20 사이 숫자)

---
+++ src/main/java/bridge/view/InputView.java
@@ -0,0 +1,41 @@
+package bridge.view;
+
+import camp.nextstep.edu.missionutils.Console;
+
+/**
+ * 사용자로부터 입력을 받는 역할을 한다.
+ */
+public class InputView {
+
+    public static final String BRIDGE_SIZE_ERROR = "[ERROR] 다리 길이는 3부터 20 사이의 숫자여야 합니다.";
+
+    /**
+     * 다리의 길이를 입력받는다.
+     */
+    public int readBridgeSize() {
+        System.out.println("다리의 길이를 입력해주세요.");
+        try {
+            int bridgeSize = Integer.parseInt(Console.readLine());
+            if (bridgeSize < 3 || 20 < bridgeSize) {
+                throw new IllegalArgumentException(BRIDGE_SIZE_ERROR);
+            }
+            return bridgeSize;
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException(BRIDGE_SIZE_ERROR);
+        }
+    }
+
+    /**
+     * 사용자가 이동할 칸을 입력받는다.
+     */
+    public String readMoving() {
+        return null;
+    }
+
+    /**
+     * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
+     */
+    public String readGameCommand() {
+        return null;
+    }
+}
