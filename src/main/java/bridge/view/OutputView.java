c0a106ff4ba1b789b89bb48971c87c0d12888322
이정우 <acg6138@naver.com>
2022-11-22 02:31:05 +0900

    refactor(OutputView): 변경 전 GameResult에서 담당하던 로직을 OutputView에서 처리하도록 수정

--- src/main/java/bridge/view/OutputView.java
+++ src/main/java/bridge/view/OutputView.java
@@ -1,6 +1,8 @@
 package bridge.view;
 
+import bridge.domain.BridgeGame;
 import bridge.domain.GameResult;
+import bridge.domain.PassResult;
 
 public class OutputView {
     private static final String FINAL_RESULT_MESSAGE = "최종 게임 결과";
@@ -8,17 +10,44 @@ public class OutputView {
     private static final String TOTAL_PLAY_COUNT = "총 시도한 횟수: ";
     private static final String START_SUFFIX = "[ ";
     private static final String END_SUFFIX = " ]";
+    private static final String LINE = " | ";
 
-    public void printMap(GameResult gameResult) {
-        System.out.println(START_SUFFIX + gameResult.getTopResult() + END_SUFFIX);
-        System.out.println(START_SUFFIX + gameResult.getBottomResult() + END_SUFFIX + System.lineSeparator());
+    public void printMap(BridgeGame bridgeGame) {
+        System.out.println(START_SUFFIX + getTopResult(bridgeGame) + END_SUFFIX);
+        System.out.println(START_SUFFIX + getBottomResult(bridgeGame) + END_SUFFIX + System.lineSeparator());
     }
 
-    public void printResult(GameResult gameResult) {
+    public void printResult(BridgeGame bridgeGame) {
         System.out.println(FINAL_RESULT_MESSAGE);
-        System.out.println(START_SUFFIX + gameResult.getTopResult() + END_SUFFIX);
-        System.out.println(START_SUFFIX + gameResult.getBottomResult() + END_SUFFIX);
-        System.out.println(SUCCESS_OR_FAILURE + gameResult.successOrFailure());
-        System.out.println(TOTAL_PLAY_COUNT + gameResult.totalPlayCount());
+        System.out.println(START_SUFFIX + getTopResult(bridgeGame) + END_SUFFIX);
+        System.out.println(START_SUFFIX + getBottomResult(bridgeGame) + END_SUFFIX);
+        System.out.println(SUCCESS_OR_FAILURE + successOrFailure(bridgeGame));
+        System.out.println(TOTAL_PLAY_COUNT + bridgeGame.getPlayCount());
+    }
+
+    public String getTopResult(BridgeGame bridgeGame) {
+        String result = "";
+        for (PassResult top : bridgeGame.getTop()) {
+            result += top.getValue() + LINE;
+        }
+        result = removeLastLine(result);
+        return result;
+    }
+
+    public String getBottomResult(BridgeGame bridgeGame) {
+        String result = "";
+        for (PassResult bottom : bridgeGame.getBottom()) {
+            result += bottom.getValue() + LINE;
+        }
+        result = removeLastLine(result);
+        return result;
+    }
+
+    public String removeLastLine(String result) {
+        return result.substring(0, result.length() - 3);
+    }
+    public String successOrFailure(BridgeGame bridgeGame) {
+        GameResult result = bridgeGame.isClear();
+        return result.getValue();
     }
 }
