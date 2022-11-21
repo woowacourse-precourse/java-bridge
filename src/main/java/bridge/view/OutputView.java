4f832639202e7b085bb84fe123ee2b9327099be1
Hi-Imjaeyoung <imjy2757@naver.com>
2022-11-21 12:48:49 +0900

    feat(OutputView) : 현재 다리건너기 진행 정도를 출력하는 기능 구현.

--- src/main/java/bridge/view/OutputView.java
+++ src/main/java/bridge/view/OutputView.java
@@ -1,16 +1,50 @@
 package bridge.view;
 
+import bridge.constant.BridgeConstant;
+
 /**
  * 사용자에게 게임 진행 상황과 결과를 출력하는 역할을 한다.
  */
 public class OutputView {
-
-    /**
-     * 현재까지 이동한 다리의 상태를 정해진 형식에 맞춰 출력한다.
-     * <p>
-     * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
-     */
-    public void printMap() {
+    private StringBuilder firstLine,secondLine;
+    public OutputView(){
+        firstLine = new StringBuilder();
+        secondLine = new StringBuilder();
+    }
+    private void addSignal(String Direction, String signal){
+        if(Direction.equals(BridgeConstant.CAN_GO_UP.getString())){
+            firstLine.append("| "+signal+" ");
+            secondLine.append("|   ");
+            return;
+        }
+        firstLine.append("|   ");
+        secondLine.append("| "+signal+" ");
+    }
+    private StringBuilder packBrace(StringBuilder lineInformation){
+        StringBuilder packBraceLine = new StringBuilder(lineInformation);
+        packBraceLine.deleteCharAt(0);
+        packBraceLine.insert(0,"[");
+        packBraceLine.append("]");
+        return packBraceLine;
+    }
+    private void reset(){
+        firstLine = new StringBuilder();
+        secondLine = new StringBuilder();
+    }
+    private void checkDirection(boolean pass, String Direction){
+        if(pass){
+            addSignal(Direction,"O");
+            return;
+        }
+        addSignal(Direction,"X");
+    }
+    public void printMap(boolean pass, String Direction) {
+        checkDirection(pass,Direction);
+        System.out.println(packBrace(firstLine));
+        System.out.println(packBrace(secondLine));
+        if(!pass){
+            reset();
+        }
     }
 
     /**
