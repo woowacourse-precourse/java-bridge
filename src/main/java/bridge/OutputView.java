2e4863ab3253bed2b7f87ad32fcec0803cfd5673
Geun Woo(Kay) Park <64457922+parklaus1078@users.noreply.github.com>
2022-11-21 14:51:38 +0900

    [ADD] 다리 게임 - 만든 맵을 출력하는 메소드 추가

--- src/main/java/bridge/OutputView.java
+++ src/main/java/bridge/OutputView.java
@@ -39,6 +39,13 @@ public class OutputView {
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
     public void printMap() {
+        this.result = Map.BOARD_ONE.getPiece() +
+                mapTop +
+                Map.BOARD_TWO.getPiece() +
+                mapBelow +
+                Map.BOARD_THREE.getPiece();
+
+        System.out.printf(Map.BOARD.getPiece(), mapTop, mapBelow);
     }
 
     /**
