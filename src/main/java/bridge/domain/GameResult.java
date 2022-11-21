b588bc1f765ae5cd0929be18712296aa4f5c0058
Basak <chgy2131@naver.com>
2022-11-21 23:32:21 +0900

    feat(GameResult): Move를 인자로 Bridge를 추가하는 기능 추가

--- src/main/java/bridge/domain/GameResult.java
+++ src/main/java/bridge/domain/GameResult.java
@@ -11,6 +11,9 @@ public class GameResult {
     public void addBlock(String upDown) {
         bridge.addBlock(upDown);
     }
+    public void addBlock(Move move) {
+        addBlock(move.getDirection());
+    }
 
     public int getNumberOfAttempts() {
         return numberOfAttempts;
@@ -20,5 +23,8 @@ public class GameResult {
     }
     public GameProgress getProgress() {
         return progress;
+    }
+    public void setProgress(GameProgress progress) {
+        this.progress = progress;
     }
 }
