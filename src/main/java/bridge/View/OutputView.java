3115b819886ee7a9699a8e2a467576bc84c40c66
simba <shim9597@gmail.com>
2022-11-21 16:42:05 +0900

    feat: 최종 결과 출력하는 기능 구현

--- src/main/java/bridge/View/OutputView.java
+++ src/main/java/bridge/View/OutputView.java
@@ -1,18 +1,21 @@
-package bridge;
+package bridge.View;
 
+
+import bridge.PassFail;
+import bridge.UpDown;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 
-import static bridge.TemplateLetters.*;
+import static bridge.BridgeGame.down;
+import static bridge.BridgeGame.up;
+import static bridge.View.TemplateLetters.*;
 
 /**
  * 사용자에게 게임 진행 상황과 결과를 출력하는 역할을 한다.
  */
 public class OutputView {
-    public static List<String> up = new ArrayList<>();
-    public static List<String> down = new ArrayList<>();
 
     /**
      * 현재까지 이동한 다리의 상태를 정해진 형식에 맞춰 출력한다.
@@ -51,6 +54,8 @@ public class OutputView {
         }
         String upJoin = String.join("", up);
         String downJoin = String.join("", down);
+
+        //프린트 하는 기능은 이거 두개고, 위에는 리스트 형식에 맞게 만드는 기능이네.
         System.out.println(upJoin + END.getValue());
         System.out.println(downJoin + END.getValue());
     }
@@ -60,6 +65,11 @@ public class OutputView {
      * <p>
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void printResult() {
+    public void printResult(List<String> up, List<String> down) {
+        String upJoin = String.join("", up);
+        String downJoin = String.join("", down);
+
+        System.out.println(upJoin + END.getValue());
+        System.out.println(downJoin + END.getValue());
     }
 }
