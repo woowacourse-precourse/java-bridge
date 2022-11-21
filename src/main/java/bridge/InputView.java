4493cbccb33af57ccca0a136f8816462947547fe
gwj0421 <gwj0421@gmail.com>
2022-11-21 18:21:41 +0900

    feat: 다리의 길이 입력 기능 및 예외 처리 기능 추가

--- src/main/java/bridge/InputView.java
+++ src/main/java/bridge/InputView.java
@@ -1,15 +1,29 @@
 package bridge;
 
+import camp.nextstep.edu.missionutils.Console;
+
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
  */
 public class InputView {
+    CheckNum checkNum;
+
+    public InputView(CheckNum checkNum) {
+        this.checkNum = new CheckNum();
+    }
 
     /**
      * 다리의 길이를 입력받는다.
      */
     public int readBridgeSize() {
-        return 0;
+        System.out.println("다리의 길이를 입력해주세요.");
+        String _input = Console.readLine();
+        checkNum.checkVoidOrSpace(_input);
+        checkNum.checkString(_input);
+        int bridgeSize = Integer.parseInt(_input);
+        checkNum.checkNegative(bridgeSize);
+        checkNum.checkRange(bridgeSize);
+        return bridgeSize;
     }
 
     /**
