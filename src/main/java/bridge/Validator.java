8e6e16e9f681517b63eacc1e3e359da8a651cb2e
rjsah09 <83952174+rjsah09@users.noreply.github.com>
2022-11-21 21:35:47 +0900

    refactor: Validator.validatorNumericSize() 범위 수정

--- src/main/java/bridge/Validator.java
+++ src/main/java/bridge/Validator.java
@@ -6,15 +6,15 @@ public class Validator {
         try {
             int parsed = Integer.parseInt(input);
         } catch (NumberFormatException e) {
-            throw new IllegalStateException("[ERROR] 숫자 형태의 입력이 아닙니다.");
+            throw new IllegalArgumentException("[ERROR] 숫자 형태의 입력이 아닙니다.");
         }
     }
 
     public static void validateNumericSize(int minLength, int maxLength, String input) {
         int parsed = Integer.parseInt(input);
 
-        if (parsed < maxLength || parsed > maxLength) {
-            throw new IllegalStateException("[ERROR] 숫자의 범위는 " + minLength + " ~ " + maxLength + "입니다.");
+        if (parsed < minLength || parsed > maxLength) {
+            throw new IllegalArgumentException("[ERROR] 숫자의 범위는 " + minLength + " ~ " + maxLength + "입니다.");
         }
     }
 
