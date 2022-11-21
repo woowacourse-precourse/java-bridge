c6daf53cc4aacef068e81f6e71a4f2033a2cfd9e
GwansuShin <happysun7080@naver.com>
2022-11-21 17:36:50 +0900

    feat(OutputMessage): 결과 메시지 추가
    최종 게임 결과, 게임 성공 여부, 시도 횟수 메시지 추가
    게임 성공 여부, 시도 횟수 포맷팅 기능 추가

--- src/main/java/bridge/view/output/OutputMessage.java
+++ src/main/java/bridge/view/output/OutputMessage.java
@@ -2,8 +2,13 @@ package bridge.view.output;
 
 import bridge.view.Message;
 
+import java.util.List;
+
 public enum OutputMessage implements Message {
     START("다리 건너기 게임을 시작합니다."),
+    RESULT("최종 게임 결과"),
+    SUCCESS_OR_FAILURE("게임 성공 여부: "),
+    TRIAL("총 시도한 횟수: "),
     ;
 
     private final String message;
@@ -15,5 +20,9 @@ public enum OutputMessage implements Message {
     @Override
     public String text() {
         return message;
+    }
+
+    public String format(String result) {
+        return String.format("%s%s", message, result);
     }
 }
