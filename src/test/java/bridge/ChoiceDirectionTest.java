ac15854a244ecaba2e62f707ee54c67f93d3988d
TaeMin <xoals3094@naver.com>
2022-11-21 09:01:51 +0900

    test(ChoiceDirectionTest) : compare 메소드 테스트 추가

--- src/test/java/bridge/ChoiceDirectionTest.java
+++ src/test/java/bridge/ChoiceDirectionTest.java
@@ -1,10 +1,17 @@
 package bridge;
 
+import bridge.Enum.Direction;
 import bridge.Model.WrappingType.ChoiceDirection;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.junit.jupiter.params.provider.ValueSource;
+
+import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.*;
 
@@ -29,4 +36,32 @@ public class ChoiceDirectionTest {
             }).isInstanceOf(IllegalArgumentException.class);
         }
     }
+
+    @DisplayName("compare 메소드 테스트(Input : String)")
+    @ParameterizedTest
+    @CsvSource(value = { "U:U:true", "U:D:false", "D:D:true", "D:U:false" }, delimiter = ':')
+    void compareTest(String choice, String directionString, boolean result){
+        ChoiceDirection choiceDirection = new ChoiceDirection(choice);
+        boolean actual = choiceDirection.compare(directionString);
+        assertThat(actual).isEqualTo(result);
+    }
+
+    @DisplayName("compare 메소드 테스트(Input : Direction)")
+    @ParameterizedTest
+    @MethodSource("generateData")
+    void compareTest(String choice, Direction direction, boolean result){
+        ChoiceDirection choiceDirection = new ChoiceDirection(choice);
+        boolean actual = choiceDirection.compare(direction);
+        assertThat(actual).isEqualTo(result);
+    }
+
+    static Stream<Arguments> generateData(){
+        return Stream.of(
+                Arguments.of("U", Direction.UP, true),
+                Arguments.of("U", Direction.DOWN, false),
+                Arguments.of("D", Direction.UP, false),
+                Arguments.of("D", Direction.DOWN, true)
+        );
+    }
+
 }
