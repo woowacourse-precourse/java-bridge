c3a4564e0d100a1c4f56919a310c73dfdb82dd04
desfox <desfox99@naver.com>
2022-11-21 23:34:57 +0900

    refactor(ReadGameCommandValidation): 매직리터럴 리팩토링

--- src/main/java/bridge/validation/ReadGameCommandValidation.java
+++ src/main/java/bridge/validation/ReadGameCommandValidation.java
@@ -1,8 +1,11 @@
 package bridge.validation;
 
 public class ReadGameCommandValidation {
+    private static final String RESTART_COMMAND = "R";
+    private static final String QUIT_COMMAND = "Q";
+
     public void validateInputFormat(String gameCommand) {
-        if ((! gameCommand.equals("R")) && (! gameCommand.equals("Q"))) {
+        if ((! gameCommand.equals(RESTART_COMMAND)) && (! gameCommand.equals(QUIT_COMMAND))) {
             throw new IllegalArgumentException("[ERROR] 재시작, 종료 여부를 R 또는 Q로 입력해주세요.");
         }
     }
