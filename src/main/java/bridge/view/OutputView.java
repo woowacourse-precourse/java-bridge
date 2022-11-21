c24a8f06a9996c7687f15d6b9567776e9032d90e
Beginner <dkstmdwn17@naver.com>
2022-11-21 14:40:35 +0900

    refactor : 최종 게임 결과를 출력하는 부분에서 총 시도한 횟수 출력을 분리
    최종 게임 결과를 출력하는
    로직에서 총 시도한 횟수까지
    출력해주는 것 때문에
    변수를 하나 추가하는 부분이 있다.
    그래서 이를 분리하여
    총 시도한 횟수를 개별로
    출력해주는 방향으로 리팩토링
    하였다.

--- src/main/java/bridge/view/OutputView.java
+++ src/main/java/bridge/view/OutputView.java
@@ -27,10 +27,15 @@ public class OutputView {
     private void printTop(List<String> bridges, int index, boolean isSuccess) {
         for(int i = 0; i <= index; i++){
             if(i!=0) sb.append("|");
+            // 1. 건널 수 있는 다리인 위쪽을 선택하여 건넜을 경우 O
             if(index == i && isSuccess && bridges.get(i).equals("U")) sb.append(" O ");
+            // 2. 건널 수 있는 다리인 아래쪽을 선택하여 건넜을 경우 빈칸
             if(index == i && isSuccess && bridges.get(i).equals("D")) sb.append("   ");
+            // 3. 잘못된 다리인 아래쪽을 선택하여 건넜을 경우 빈칸
             if(index == i && !isSuccess && bridges.get(i).equals("U")) sb.append("   ");
+            // 4. 잘못된 다리인 위쪽을 선택하여 건넜을 경우 X
             if(index == i && !isSuccess && !bridges.get(i).equals("U")) sb.append(" X ");
+            // 이전 다리들 출력하는 것
             if(index != i && bridges.get(i).equals("U"))sb.append(" O ");
             if(index != i && !bridges.get(i).equals("U")) sb.append("   ");
         }
@@ -39,10 +44,15 @@ public class OutputView {
     private void printDown(List<String> bridges, int index, boolean isSuccess) {
         for(int i = 0; i <= index; i++){
             if(i!=0) sb.append("|");
+            // 1. 건널 수 있는 다리인 아래쪽을 선택하여 건넜을 경우 O
             if(index == i && isSuccess && bridges.get(i).equals("D")) sb.append(" O ");
+            // 2. 건널 수 있는 다리인 위쪽을 선택하여 건넜을 경우 빈칸
             if(index == i && isSuccess && bridges.get(i).equals("U")) sb.append("   ");
+            // 3. 잘못된 다리인 위쪽을 선택하여 건넜을 경우 빈칸
             if(index == i && !isSuccess && bridges.get(i).equals("D")) sb.append("   ");
+            // 4. 잘못된 다리인 아래쪽을 선택하여 건넜을 경우 X
             if(index == i && !isSuccess && !bridges.get(i).equals("D")) sb.append(" X ");
+            // 이전 다리들 출력하는 것
             if(index != i && bridges.get(i).equals("D"))sb.append(" O ");
             if(index != i && !bridges.get(i).equals("D")) sb.append("   ");
         }
@@ -55,7 +65,7 @@ public class OutputView {
      * <p>
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void printResult(List<String> bridges, int index, boolean isRunning, int count) {
+    public void printResult(List<String> bridges, int index, boolean isRunning) {
         System.out.println("최종 게임 결과");
         System.out.println("");
         if(isRunning) {
@@ -66,6 +76,9 @@ public class OutputView {
             printMap(bridges, index-1 , false);
             System.out.println("게임 성공 여부: 실패");
         }
-        System.out.printf("총 시도한 횟수: %d", count);
+    }
+
+    public void printTotalAttemptsMade(int numberOfAttempts){
+        System.out.printf("총 시도한 횟수: %d", numberOfAttempts);
     }
 }
