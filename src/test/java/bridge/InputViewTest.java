039ca9a3fd3f5bba06c37058bdc78e17feb8102a
kissshot1104 <kissshot.heartunderblade1104@gmail.com>
2022-11-21 22:48:19 +0900

    Refactor(InputViewTtest) : 주석 추가
    테스트 별 설명을 넣었습니다.

--- src/test/java/bridge/InputViewTest.java
+++ src/test/java/bridge/InputViewTest.java
@@ -5,7 +5,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-class InputViewTest extends CustomNsTest{
+class InputViewTest extends CustomNsTest {
 
     public static final InputView inputView = new InputView();
     public static final String ERROR_MESSAGE = "[ERROR]";
@@ -14,42 +14,42 @@ class InputViewTest extends CustomNsTest{
     @DisplayName("다리의 사이즈를 입력받는지 테스트")
     void readBridegeSizeTest() {
 
-        //잘못된 동작
+        //1 ~ 20범위를 넘었을 경우(에러)
         {
             command("21");
             inputView.readBridgeSize();
             assertThat(output()).contains(ERROR_MESSAGE);
         }
 
-        //잘못된 동작
+        //1 ~ 20범위가 넘어갔을 경우(에러)
         {
             command("0");
             inputView.readBridgeSize();
             assertThat(output()).contains(ERROR_MESSAGE);
         }
 
-        //잘못된 동작
+        //앞에 기호가 들어갔을 경우(에러)
         {
             command("+1");
             inputView.readBridgeSize();
             assertThat(output()).contains(ERROR_MESSAGE);
         }
 
-        //잘못된 동작
+        //앞에 기호가 들어갔고 음수일 경우(에러)
         {
             command("-1");
             inputView.readBridgeSize();
             assertThat(output()).contains(ERROR_MESSAGE);
         }
 
-        //잘못된 동작
+        //맨 앞에 0이 있을 경우(에러)
         {
             command("05");
             inputView.readBridgeSize();
             assertThat(output()).contains(ERROR_MESSAGE);
         }
 
-        //정상 동작
+        //정상된 값을 넣을 경우
         {
             command("5");
             assertThat(inputView.readBridgeSize())
@@ -59,36 +59,36 @@ class InputViewTest extends CustomNsTest{
 
     @Test
     @DisplayName("사용자가 이동할 칸을 입력받는지 테스트")
-    void readMovingTest(){
-        //비정상 동작
+    void readMovingTest() {
+        //잘못된 값을 넣을 경우(에러)
         {
             command("A");
             inputView.readMoving();
             assertThat(output()).contains(ERROR_MESSAGE);
         }
 
-        //비정상 동작
+        //U를 두번 넣을 경우(에러)
         {
             command("UU");
             inputView.readMoving();
             assertThat(output()).contains(ERROR_MESSAGE);
         }
 
-        //비정상 동작
+        //D를 두번 넣을 경우(에러)
         {
             command("DD");
             inputView.readMoving();
             assertThat(output()).contains(ERROR_MESSAGE);
         }
 
-        //정상 동작
+        //정상 값을 넣을 경우
         {
             command("U");
             assertThat(inputView.readMoving())
                     .isEqualTo("U");
         }
 
-        //정상 동작
+        //정상 값을 넣을 경우
         {
             command("D");
             assertThat(inputView.readMoving())
@@ -98,36 +98,36 @@ class InputViewTest extends CustomNsTest{
 
     @Test
     @DisplayName("사용자가 리트라이 여부를 입력받는지 테스트")
-    void readGameCommand(){
-        //비정상 동작
+    void readGameCommand() {
+        //잘못된 값을 넣을 경우(에러)
         {
             command("A");
             inputView.readGameCommand();
             assertThat(output()).contains(ERROR_MESSAGE);
         }
 
-        //비정상 동작
+        //R을 두번 넣을 경우(에러)
         {
             command("RR");
             inputView.readGameCommand();
             assertThat(output()).contains(ERROR_MESSAGE);
         }
 
-        //비정상 동작
+        //Q를 두번 넣을 경우(에러)
         {
             command("QQ");
             inputView.readGameCommand();
             assertThat(output()).contains(ERROR_MESSAGE);
         }
 
-        //정상 동작
+        //정상 값을 넣을 경우
         {
             command("R");
             assertThat(inputView.readGameCommand())
                     .isEqualTo("R");
         }
 
-        //정상 동작
+        //정상 값을 넣을 경우
         {
             command("Q");
             assertThat(inputView.readGameCommand())
