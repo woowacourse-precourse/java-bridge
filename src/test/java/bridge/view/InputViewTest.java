19ce708bee8eeac482b713d2f7720b2faaaee0d5
Lee-Jiseung <ljss98@naver.com>
2022-11-21 15:56:22 +0900

    test: 다리 길이 입력 받기

---
+++ src/test/java/bridge/view/InputViewTest.java
@@ -0,0 +1,24 @@
+package bridge.view;
+
+import bridge.Application;
+import camp.nextstep.edu.missionutils.test.NsTest;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+public class InputViewTest extends NsTest {
+    private InputView inputView = new InputView();
+
+    @DisplayName("다리 길이 입력 받기 테스트")
+    @ParameterizedTest
+    @CsvSource({"3,true", "20,true", "2,false", "21,false", "3a,false", "four,false"})
+    void validateBridgeSizeTest(String input, boolean result) {
+        Assertions.assertThat(inputView.validateBridgeSize(input)).isEqualTo(result);
+    }
+
+    @Override
+    protected void runMain() {
+        Application.main(new String[]{});
+    }
+}
