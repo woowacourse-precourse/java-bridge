e548bd8756db899c28307c9d09c20570ffd9aa37
cgk95 <kjkj5868@gmail.com>
2022-11-21 13:38:45 +0900

    refactor(Model) : 다리의 상태를 저장하는 문자열의 수정

--- src/main/java/bridge/Model.java
+++ src/main/java/bridge/Model.java
@@ -4,12 +4,10 @@ import java.util.List;
 import java.util.Stack;
 
 public class Model {
-    public static List<String> bridge; // BridgeMaker에서 조작
-    public static Stack<String> user = new Stack<>(); // BridgeGame에서 조작
-
-    public static String upperResult="[";
-
-    public static String lowerResult="[";
+    public static List<String> bridge;
+    public static Stack<String> user = new Stack<>();
+    public static String upperResult = "[]";
+    public static String lowerResult = "[]"; // ] 를 찾아서 제거하고 공백을 포함한 문자열을 추가, 마지막엔 다시 ]를 추가
 
     public static int size;
 }
