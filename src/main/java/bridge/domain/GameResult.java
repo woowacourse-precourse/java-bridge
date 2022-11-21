4cc39d3afd779d2a627e293b11d6215d4297bcca
Cyma-s <ssol0319@naver.com>
2022-11-21 23:55:12 +0900

    feat(GameResult) : add a class to store one stage result

---
+++ src/main/java/bridge/domain/GameResult.java
@@ -0,0 +1,13 @@
+package bridge.domain;
+
+import java.util.List;
+
+public class GameResult {
+    private final MoveResult moveResult;
+    private final List<BridgeState> bridgeStates;
+
+    public GameResult(MoveResult moveResult, List<BridgeState> bridgeStates) {
+        this.moveResult = moveResult;
+        this.bridgeStates = bridgeStates;
+    }
+}
