129502bd75e84e3e0e04dbb0f82a4e087f3157d1
Eeap <kdg97811@naver.com>
2022-11-21 18:40:05 +0900

    feat(InputView): add func inputMoving

--- src/main/java/bridge/InputView.java
+++ src/main/java/bridge/InputView.java
@@ -27,9 +27,16 @@ public class InputView {
      * 사용자가 이동할 칸을 입력받는다.
      */
     public String readMoving() {
-        return null;
+        System.out.println("이동할 칸을 선택해주세요. (위: U, 아래: D)");
+        String bridgeUD = readLine();
+        try {
+            isValidateUD(bridgeUD);
+        } catch (IllegalArgumentException e) {
+            System.out.println(e);
+            readMoving();
+        }
+        return bridgeUD;
     }
-
     /**
      * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
      */
