230e0e8b2df1ce5a2bc64fe5281e9af6849e4203
Namyunsuk <yunsuk0328@naver.com>
2022-11-21 13:40:02 +0900

    feat(CalculationResult): 생성한 다리와 사용자의 입력 값이 같은지 확인하는 기능 구현

--- src/main/java/bridge/domain/CalculationResult.java
+++ src/main/java/bridge/domain/CalculationResult.java
@@ -1,5 +1,36 @@
 package bridge.domain;
 
+import java.util.List;
+
 public class CalculationResult {
 
+    private static String RIGHT = "O";
+    private static String WRONG = "X";
+    private static String NOT_MATCH = " ";
+
+    public static String calculateUpper(String bridgeKeyword, String userKeyword) {
+        if (bridgeKeyword == "U" && bridgeKeyword == userKeyword) {
+            return RIGHT;
+        }
+        if (bridgeKeyword == "U" && bridgeKeyword != userKeyword) {
+            return WRONG;
+        }
+        return NOT_MATCH;
+    }
+
+    public static String calculateLower(String bridgeKeyword, String userKeyword) {
+        if (bridgeKeyword == "D" && bridgeKeyword == userKeyword) {
+            return RIGHT;
+        }
+        if (bridgeKeyword == "D" && bridgeKeyword != userKeyword) {
+            return WRONG;
+        }
+        return NOT_MATCH;
+    }
+
+    public static boolean calculateResult() {
+        List<String> bridge = Bridge.getBridge();
+        List<String> userInputBridge = User.getUserMovingRecord();
+        return bridge.equals(userInputBridge);
+    }
 }
