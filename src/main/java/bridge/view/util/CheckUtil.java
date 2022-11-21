2e2831e0f1259a72c3fd4f89fd5f9778ad8172cc
jingyu9804 <swc05161@gmail.com>
2022-11-21 21:14:10 +0900

    feat(bridge) : 예외 처리 기능 구현

--- src/main/java/bridge/view/util/CheckUtil.java
+++ src/main/java/bridge/view/util/CheckUtil.java
@@ -3,17 +3,35 @@ package bridge.view.util;
 import bridge.domain.MessageStorage;
 
 public class CheckUtil {
-    public static int checkInputBridgeSize(int bridgeSize,String message) {
+    private static final String UP = "U";
+    private static final String DOWN = "D";
+    private static final String RESTART = "R";
+    private static final String QUIT = "Q";
+    public static void checkInputBridgeSize(int bridgeSize,String message) {
         if (bridgeSize < 3 || bridgeSize > 20) {
             throw new IllegalArgumentException(message);
         }
-        return bridgeSize;
+    }
+
+    public static int checkIsString(String bridgeSize,String message) throws IllegalArgumentException{
+        try{
+            return Integer.valueOf(bridgeSize);
+        }catch (NumberFormatException e){
+            throw new IllegalArgumentException(message);
+        }
     }
 
     public static String checkInputReadMoving(String readMove,String message){
-        if(!readMove.equals(MessageStorage.UP.getMessage()) || !readMove.equals(MessageStorage.DOWN.getMessage())){
+        if (!(readMove.equals(UP) || readMove.equals(DOWN))){
             throw new IllegalArgumentException(message);
         }
         return readMove;
+    }
+
+    public static String checkInputReadGameCommand(String gameCommand,String message){
+        if(!(gameCommand.equals(RESTART) || gameCommand.equals(QUIT))){
+            throw new IllegalArgumentException(message);
+        }
+        return gameCommand;
     }
 }
