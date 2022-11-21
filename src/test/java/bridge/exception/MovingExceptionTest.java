d48005ad404ecf4b2d37dd0f6f6925290fbb4434
NAM GIBEOM <rlqja0523@naver.com>
2022-11-21 22:35:57 +0900

    test: U 또는 D를 입력하면 정상적으로 통과되는지 테스트

--- src/test/java/bridge/exception/MovingExceptionTest.java
+++ src/test/java/bridge/exception/MovingExceptionTest.java
@@ -3,10 +3,12 @@ package bridge.exception;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.ArrayList;
 import java.util.List;
 
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class MovingExceptionTest {
@@ -19,6 +21,14 @@ public class MovingExceptionTest {
     void isNotMovingTest(String errorMoving) {
         assertThrows(IllegalArgumentException.class, () -> movingException.checkException(errorMoving));
     }
+
+    @ParameterizedTest
+    @DisplayName("U, D를 입력하면 통과한다.")
+    @ValueSource(strings = {"U", "D"})
+    void isMovingTest(String moving) {
+        assertDoesNotThrow(() -> movingException.checkException(moving));
+    }
+
 
     static List<String> generateData() {
         List<String> data = new ArrayList<>();
