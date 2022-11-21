7c35c60fbc68bb371e33167482c9f9a57c80b4bb
jw0293 <k38836919@gmail.com>
2022-11-21 13:36:58 +0900

    feat : Init Game Start Application [애플리케이션 게임 초기화]

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -1,8 +1,25 @@
 package bridge;
 
+import bridge.exception.ValidException;
+import bridge.service.BridgeGame;
+import bridge.service.BridgeInit;
+import bridge.service.BridgeMaker;
+import bridge.service.BridgeService;
+import bridge.view.BridgePainting;
+import bridge.view.InputView;
+import bridge.view.OutputView;
+
 public class Application {
 
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        BridgeInit bridgeInit = new BridgeInit(
+                new InputView(new ValidException()),
+                new BridgeMaker(new BridgeRandomNumberGenerator()));
+        BridgeGame bridgeGame = new BridgeGame(bridgeInit, new BridgePainting());
+        BridgeService bridgeService = new BridgeService(new OutputView(), bridgeGame);
+
+        bridgeGame.gameInit();
+        bridgeService.start();
     }
 }
