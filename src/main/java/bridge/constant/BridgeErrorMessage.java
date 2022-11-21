b172212d1c510e3068e79c68fd8c354f8464c999
Qulip <ilkwon.ryu@gmail.com>
2022-11-22 01:03:18 +0900

    feat: 이동 방향 입력 받기 기능 추가

--- src/main/java/bridge/constant/BridgeErrorMessage.java
+++ src/main/java/bridge/constant/BridgeErrorMessage.java
@@ -3,6 +3,7 @@ package bridge.constant;
 public class BridgeErrorMessage {
     public static final String BRIDGE_LENGTH_IS_NOT_DIGIT_ERROR_MESSAGE = "[ERROR] - 다리의 길이가 숫자가 아닙니다.";
     public static final String BRIDGE_LENGTH_IS_NOT_IN_RANGE_ERROR_MESSAGE = "[ERROR] - 다리의 길이가 3~20 범위 밖의 숫자 입니다.";
+    public static final String DIRECTION_IS_NOT_VALID_ERROR_MESSAGE = "[ERROR] - U/D의 갑승로만 애지사 ㄱ력버ㅏ.";
 
     private BridgeErrorMessage(){}
 }
