b8008952a4e4fdf05567bf804c5ae9f0f088bbd4
yoonjuho <dbswn990@naver.com>
2022-11-22 00:40:05 +0900

    refactor(ValidateInput): 예외처리 클래스 접근제한자 변경

--- src/main/java/bridge/controller/ValidateInput.java
+++ src/main/java/bridge/controller/ValidateInput.java
@@ -3,19 +3,18 @@ package bridge.controller;
 import bridge.model.Column;
 
 public class ValidateInput {
-    static int checkNumeric(String input){
+    protected int checkNumeric(String input){
         if(!input.matches("^[0-9]")){
             throw new IllegalArgumentException();
         }
         return Integer.parseInt(input);
     }
-    static String checkLetter(String input){
+    protected void checkLetter(String input){
         if(!Column.isValidateLetter(input)){
             throw new IllegalArgumentException();
         }
-        return input;
     }
-    static void checkEndLetter(String input){
+    protected void checkEndLetter(String input){
         if(!input.equals(GameMessage.RETRY) || !input.equals(GameMessage.CLOSE)){
             throw new IllegalArgumentException();
         }
--- src/test/java/bridge/controller/BridgeControllerTest.java
+++ src/test/java/bridge/controller/BridgeControllerTest.java
@@ -1,13 +1,10 @@
 package bridge.controller;
 
-import bridge.view.InputView;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
-import static org.junit.jupiter.api.Assertions.*;
 
 class BridgeControllerTest {
     ValidateInput validate;
@@ -19,20 +16,20 @@ class BridgeControllerTest {
     @DisplayName("다리의 길이를 숫자로 입력 받지 않는 경우 예외처리.")
     @Test
     void inputBridgeLengthByNotNumeric() {
-        assertThatThrownBy(() -> validate.numeric("h"))
+        assertThatThrownBy(() -> validate.checkNumeric("h"))
                 .isInstanceOf(IllegalArgumentException.class);
     }
-    @DisplayName("이동할 때 다른 문자가 입력 되는 경우 예외처리.")
+    @DisplayName("이동할 때 다른 문자가 입력 되는 경우 이중 예외처리.")
     @Test
     void inputMoveLetterByAnotherLetter() {
-        assertThatThrownBy(() -> validate.letter("a"))
+        assertThatThrownBy(() -> validate.checkLetter("a"))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
     @DisplayName("게임이 실패 하고 다른 문자가 입력 되는 경우 예외처리.")
     @Test
     void inputEndLetterByAnotherLetter() {
-        assertThatThrownBy(() -> validate.endLetter("b"))
+        assertThatThrownBy(() -> validate.checkEndLetter("b"))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 }
