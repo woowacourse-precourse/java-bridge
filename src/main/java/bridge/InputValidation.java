83a6af54643ad3299bcdd9773480de7c3cc18f08
ensk26 <tjensk26@gmail.com>
2022-11-22 03:42:15 +0900

    refactor(InputValidation): 다리 길이를 숫자로 변환해주는 메서드 이동
    새로운 메서드 추가 to InputValidation
    - 숫자를 변환해주는 과정이 있지만 검증 과정이 있어, 검증하는 클래스로 이동
    - BridgeGameRun에서는 테스트 하기 힘들어서 이동함
    - getNumberToBridgeSize 이동으로 readBridgeSizeValidation 메서드 반환 값이나 일부 구성 변경

--- src/main/java/bridge/InputValidation.java
+++ src/main/java/bridge/InputValidation.java
@@ -7,9 +7,20 @@ public class InputValidation {
     static final int bridgeSizeStart = 3;
     static final int bridgeSizeEnd = 20;
 
-    public void readBridgeSizeValidation(int size) {
+    public int readBridgeSizeValidation(String bridgeSize) {
+        int size = getNumberToBridgeSize(bridgeSize);
+
         if (size < bridgeSizeStart || size > bridgeSizeEnd) {
-            throw new IllegalArgumentException(NOT_BRIDGE_SIZE.getMessage());
+            throw new IllegalArgumentException(String.format(NOT_BRIDGE_SIZE.getMessage(), bridgeSizeStart, bridgeSizeEnd));
+        }
+        return size;
+    }
+
+    private int getNumberToBridgeSize(String bridgeSize) {
+        try {
+            return Integer.parseInt(bridgeSize);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException(NOT_VALUE.getMessage());
         }
     }
 
