0a12929bdd76c7b5c38a869be5d1f4514a1c3204
whdcks420 <whdcks420@gmail.com>
2022-11-21 20:28:26 +0900

    test: 브릿지 U, D 컨버트 test

---
+++ src/test/java/bridge/utils/ConvertTest.java
@@ -0,0 +1,21 @@
+package bridge.utils;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ConvertTest {
+
+    @DisplayName("0,1로 이루어진 U,D로 바꿔주는 테스트")
+    @Test
+    void convertToUpAndDownTest() {
+        List<Integer> bridge = List.of(0, 1, 1, 0);
+        List<String> list = Convert.toUpAndDownBridge(bridge);
+
+        assertEquals(List.of("D", "U", "U", "D"), list);
+        assertNotEquals(List.of("D", "D", "D", "D"), list);
+    }
+}
