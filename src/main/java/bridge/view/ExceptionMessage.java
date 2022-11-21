50fd94ad25d3d5a38be927cc388978f97b285892
Hongsugyeong <olivia258@naver.com>
2022-11-21 16:07:31 +0900

    (style) : [ERROR] 문구 추가

--- src/main/java/bridge/view/ExceptionMessage.java
+++ src/main/java/bridge/view/ExceptionMessage.java
@@ -3,18 +3,18 @@ package bridge.view;
 public class ExceptionMessage {
 
     public static String isNumberMsg() {
-        return "입력하신 값이 숫자가 아닙니다.";
+        return "[ERROR] 입력하신 값이 숫자가 아닙니다.";
     }
 
     public static String isRangeNumberMsg() {
-        return "3이상 20이하의 숫자를 입력해야합니다.";
+        return "[ERROR] 3이상 20이하의 숫자를 입력해야합니다.";
     }
 
     public static String isProperMsg() {
-        return "U 또는 D를 입력해주세요";
+        return "[ERROR] U 또는 D를 입력해주세요";
     }
 
     public static String isReplyMsg() {
-        return "R 또는 Q를 입력해주세요";
+        return "[ERROR] R 또는 Q를 입력해주세요";
     }
 }
