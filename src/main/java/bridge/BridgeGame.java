0d01f0fd6f7b17ba70975ea0a0fce10ffedbd1b2
김산 <kimsan@gimsan-ui-MacBookAir.local>
2022-11-22 03:00:56 +0900

    feat (BridgeGame) : 사용자의 move에 따라 bridge 상태 업데이트하는 함수 추가

--- src/main/java/bridge/BridgeGame.java
+++ src/main/java/bridge/BridgeGame.java
@@ -1,16 +1,43 @@
 package bridge;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * 다리 건너기 게임을 관리하는 클래스
  */
 public class BridgeGame {
+
+    private final List<String> bridge;
+    private final List<String> nowBridge;
+
+    public BridgeGame(List<String> bridge) {
+        this.bridge = bridge;
+        this.nowBridge = new ArrayList<>();
+    }
+
+    public static class MoveResult {
+        public final List<String> nowBridge;
+        public final int flag;
+
+        public MoveResult(int flag, List<String> nowBridge) {
+            this.flag = flag;
+            this.nowBridge = nowBridge;
+        }
+    }
 
     /**
      * 사용자가 칸을 이동할 때 사용하는 메서드
      * <p>
      * 이동을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void move() {
+    public MoveResult move(String userInput) {
+        if (userInput.equals(bridge.get(nowBridge.size()))) {
+            nowBridge.add(userInput);
+            MoveResult moveResult = new MoveResult(1, nowBridge);
+            return moveResult;
+        }
+        return new MoveResult(0, nowBridge);
     }
 
     /**
