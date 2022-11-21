9f79ba4beb8105bd821b6bfc27610c353aa46711
zhu <parkjw573@naver.com>
2022-11-21 15:38:30 +0900

    일단 테스트코드 통과하게 구현

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -1,8 +1,25 @@
 package bridge;
 
-public class Application {
+import java.util.List;
 
+public class Application {
+    public static OutputView outputView = new OutputView();
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        System.out.println("다리 건너기 게임을 시작합니다.\n");
+        BridgeGame bridgeGame = new BridgeGame(new InputView(), new BridgeMaker(new BridgeRandomNumberGenerator()));
+        List<String> bridge = bridgeGame.bridgeMaker.makeBridge(bridgeGame.getBridgeSize());
+        int cnt = 0, trial = 1;
+        while (true) {
+            boolean correct = bridgeGame.move().equals(bridge.get(cnt++));
+            outputView.printMap(bridge, cnt, correct);
+            if (cnt == bridge.size()) break;
+            if(correct) continue;
+            if (!bridgeGame.retry()) break;
+            trial++; cnt = 0;
+        }
+        showResult(bridge, cnt, trial);
+    }
+    public static void showResult(List<String> bridge, int cnt, int trial) {
+        outputView.printResult(bridge, cnt, trial);
     }
 }
--- src/main/java/bridge/OutputView.java
+++ src/main/java/bridge/OutputView.java
@@ -1,5 +1,8 @@
 package bridge;
 
+import java.sql.SQLOutput;
+import java.util.List;
+
 /**
  * 사용자에게 게임 진행 상황과 결과를 출력하는 역할을 한다.
  */
@@ -10,7 +13,24 @@ public class OutputView {
      * <p>
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void printMap() {
+    public void printMap(List<String> bridge, int cnt, boolean correct) {
+        System.out.print("[");
+        for(int i = 0 ; i < cnt ; i++) {
+            if(i != cnt - 1 && bridge.get(i).equals("U")) System.out.print(" O |");
+            if(i != cnt - 1 && bridge.get(i).equals("D")) System.out.print("   |");
+            if(i == cnt - 1 && bridge.get(i).equals("U") && correct) System.out.println(" O ]");
+            if(i == cnt - 1 && bridge.get(i).equals("U") && !correct) System.out.println(" X ]");
+            if(i == cnt - 1 && bridge.get(i).equals("D")) System.out.println("   ]");
+        }
+        System.out.print("[");
+        for(int i = 0 ; i < cnt ; i++) {
+            if(i != cnt - 1 && bridge.get(i).equals("D")) System.out.print(" O |");
+            if(i != cnt - 1 && bridge.get(i).equals("U")) System.out.print("   |");
+            if(i == cnt - 1 && bridge.get(i).equals("D") && correct) System.out.println(" O ]");
+            if(i == cnt - 1 && bridge.get(i).equals("D") && !correct) System.out.println(" X ]");
+            if(i == cnt - 1 && bridge.get(i).equals("U")) System.out.println("   ]");
+        }
+        System.out.println();
     }
 
     /**
@@ -18,6 +38,11 @@ public class OutputView {
      * <p>
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void printResult() {
+    public void printResult(List<String> bridge, int cnt,int trial) {
+        System.out.println("최종 게임 결과");
+        printMap(bridge, cnt, cnt == bridge.size());
+        if(cnt == bridge.size()) System.out.println("게임 성공 여부: 성공");
+        if(cnt != bridge.size()) System.out.println("게임 성공 여부: 실패");
+        System.out.println("총 시도한 횟수: " + trial);
     }
 }
