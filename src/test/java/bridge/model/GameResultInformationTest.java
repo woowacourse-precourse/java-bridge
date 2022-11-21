35615c5c65f778f7a5c42c107c043e3b9a209eef
Sehbeom <j3beom@gmail.com>
2022-11-22 03:03:49 +0900

    refactor(test): 게임 결과 모델 테스트 리팩토링
    static 필드 활용으로 인해 Field 객체를 활용한
    static 필드값 변경 과정 추가

--- src/test/java/bridge/model/GameResultInformationTest.java
+++ src/test/java/bridge/model/GameResultInformationTest.java
@@ -4,6 +4,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import bridge.constants.GameResult;
 import bridge.domain.model.GameResultInformation;
+import java.lang.reflect.Field;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -19,12 +20,22 @@ public class GameResultInformationTest {
 
     @DisplayName("시도한 횟수를 증가시키고, 반환받을 수 있다.")
     @Test
-    void increaseAndGetCountOfTry() {
+    void increaseAndGetCountOfTry() throws NoSuchFieldException, IllegalAccessException {
+        resetCountOfTry();
         int expected = 3;
 
         GameResultInformation.increaseCountOfTry();
         GameResultInformation.increaseCountOfTry();
 
         assertThat(GameResultInformation.getCountOfTry()).isEqualTo(expected);
+    }
+
+    void resetCountOfTry() throws NoSuchFieldException, IllegalAccessException {
+        GameResultInformation gameResultInformation = new GameResultInformation();
+        Field countOfTry = GameResultInformation.class.getDeclaredField("countOfTry");
+
+        countOfTry.setAccessible(true);
+        countOfTry.set(gameResultInformation, 1);
+        countOfTry.setAccessible(false);
     }
 }
