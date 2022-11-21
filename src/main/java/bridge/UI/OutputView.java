081b3445adaccb0f12b6f0e2d800093c6127042d
Nitroblack1 <109022435+Nitroblack1@users.noreply.github.com>
2022-11-21 22:48:23 +0900

    feat(OutputView) : implement printResult method

--- src/main/java/bridge/UI/OutputView.java
+++ src/main/java/bridge/UI/OutputView.java
@@ -78,11 +78,15 @@ public class OutputView {
         }
     }
 
-    /**
-     * 게임의 최종 결과를 정해진 형식에 맞춰 출력한다.
-     * <p>
-     * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
-     */
-    public void printResult() {
+    public void printResult(BridgeData bridgeData, boolean isSucceedOrFail) {
+        System.out.println(FrontMan.FINAL_GAME_RESULT);
+        printMap(bridgeData);
+        if (isSucceedOrFail == SUCCEED) {
+            System.out.println("\n" + FrontMan.SUCCEED_OR_FAIL + "성공");
+        }
+        if (isSucceedOrFail == FAILED) {
+            System.out.println("\n" + FrontMan.SUCCEED_OR_FAIL + "실패");
+        }
+        System.out.println(FrontMan.TOTAL_NUMBER_OF_ATTEMPT + bridgeData.getTotalAttempt());
     }
 }
