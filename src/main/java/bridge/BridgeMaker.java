3fc34cb7670a106032b9cfa058e607231805b066
khj <bbb6254177@gmail.com>
2022-11-21 18:09:53 +0900

    feat(bridge): add function to make bridge

--- src/main/java/bridge/BridgeMaker.java
+++ src/main/java/bridge/BridgeMaker.java
@@ -1,5 +1,6 @@
 package bridge;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -13,11 +14,32 @@ public class BridgeMaker {
         this.bridgeNumberGenerator = bridgeNumberGenerator;
     }
 
+    private void validate(int size) {
+        if (size < 3 || size > 20) {
+            throw new IllegalArgumentException("[ERROR] 다리의 길이가 3에서 20사이의 수가 아닙니다.");
+        }
+    }
+
+    private String convert(int random) {
+        if (random == 0) {
+            return "D";
+        } else {
+            return "U";
+        }
+    }
+
     /**
      * @param size 다리의 길이
      * @return 입력받은 길이에 해당하는 다리 모양. 위 칸이면 "U", 아래 칸이면 "D"로 표현해야 한다.
      */
     public List<String> makeBridge(int size) {
-        return null;
+        validate(size);
+
+        List<String> bridge = new ArrayList<>();
+        for (int i = 0; i < size; i++){
+            bridge.add(convert(bridgeNumberGenerator.generate()));
+        }
+
+        return bridge;
     }
 }
