588fa5cacc09344178e1d21bdd49e4e4d84d8ad7
Kid-Chang <pch3310@gmail.com>
2022-11-21 23:55:34 +0900

    feat(BridgeMaker): 다리 리스트 리턴하는 함수 구현
    이 리스트는 일차원 리스트이며,
    건널 수 있는 칸이 윗칸이라면 0, 아랫칸이라면 1을 담고있다.

--- src/main/java/bridge/model/BridgeMaker.java
+++ src/main/java/bridge/model/BridgeMaker.java
@@ -1,8 +1,10 @@
 package bridge.model;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import bridge.utils.BridgeNumberGenerator;
+import camp.nextstep.edu.missionutils.Randoms;
 
 /**
  * 다리의 길이를 입력 받아서 다리를 생성해주는 역할을 한다.
@@ -20,6 +22,11 @@ public class BridgeMaker {
      * @return 입력받은 길이에 해당하는 다리 모양. 위 칸이면 "U", 아래 칸이면 "D"로 표현해야 한다.
      */
     public List<String> makeBridge(int size) {
-        return null;
+        List<String> bridge = new ArrayList<>();
+        for(int i = 0 ; i<size;i++){
+            int number = bridgeNumberGenerator.generate();
+            bridge.add(Integer.toString(number));
+        }
+        return bridge;
     }
 }
