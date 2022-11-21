0b21b89cf2752400683929f66e4ead7a982eae51
Voyager003 <85725033+Voyager003@users.noreply.github.com>
2022-11-21 22:28:14 +0900

    Feat: 추가 - getGamestatus, getAttempt getGamestatus: 게임의 상태 출력을 불러오기 위한 메서드 getAttempt: 게임 시도횟수를 불러오기 위한 메서드

--- src/main/java/bridge/domain/BridgeGame.java
+++ src/main/java/bridge/domain/BridgeGame.java
@@ -3,7 +3,6 @@ package bridge.domain;
 import bridge.constant.Constant;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 
 /**
@@ -64,12 +63,20 @@ public class BridgeGame {
      * 재시작을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
     public void retry(String gameCommand) {
-        if(gameCommand.equals(Constant.RESTART.getConstant())){
+        if (gameCommand.equals(Constant.RESTART.getConstant())) {
             player.clearMoving();
             bridgeStatus.clear();
             gameStatus = false;
             attempt++;
         }
+    }
+
+    public boolean getGamestatus() {
+        return gameStatus;
+    }
+
+    public int getAttempt() {
+        return attempt;
     }
 }
 
