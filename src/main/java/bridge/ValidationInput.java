4a270ee0d425b4c6f274c0f152ac6af090dd6f94
MuHyeonSon <smuhyeon84@gmail.com>
2022-11-22 00:39:51 +0900

    feat(BridgeGame): 이동할 칸 입력 예외처리

--- src/main/java/bridge/ValidationInput.java
+++ src/main/java/bridge/ValidationInput.java
@@ -1,5 +1,7 @@
 package bridge;
 
+import java.lang.String;
+
 public class ValidationInput {
     public static void checkDigit(String number){
         try{
@@ -11,10 +13,44 @@ public class ValidationInput {
 
     public static void validSizeOfBridge(String number){
         int size = Integer.parseInt(number);
-        if(!(size >= 3 && number <= 20)){
-            throw new IllegalArgumentException("[ERROR] 다리 길이는 3부터 20사이의 숫자여야 합니다.")
+        if(!(size >= 3 && size <= 20)){
+            throw new IllegalArgumentException("[ERROR] 다리 길이는 3부터 20사이의 숫자여야 합니다.");
         }
     }
-    
 
+    public static String validateInputBridge(String size){
+        checkDigit(size);
+        validSizeOfBridge(size);
+        return size;
+    }
+
+    public static void validateUpperAlphaBet(String string){
+        try{
+            if (!(string.matches("^[A-Z]*$"))){ //Alphabet이 아니라면
+                throw new IllegalArgumentException(); // 강제 에러 발생시키기
+            }
+        }catch(Exception e){
+            System.out.println("[ERROR] 이동할 칸은 U(위 칸)와 D(아래 칸) 중에 하나를 입력해주세요.");
+        }
+    }
+
+    public static void validateInputSizeIsOne(String string){
+        try{
+            if (1 != string.length()){ // 입력문자열의 개수가 1이 아니라면
+                throw new IllegalArgumentException(); // 강제 에러 발생시키기
+            }
+        }catch(Exception e){
+            System.out.println("[ERROR] 이동할 칸은 U(위 칸)와 D(아래 칸) 중에 하나를 입력해주세요.");
+        }
+    }
+
+    public static void validateReadMoving(String string) {
+        try {
+            if (!(string == "U" || string == "D")) { // 입력문자열의 개수가 1이 아니라면
+                throw new IllegalArgumentException(); // 강제 에러 발생시키기
+            }
+        } catch (Exception e) {
+            System.out.println("[ERROR] 이동할 칸은 U(위 칸)와 D(아래 칸) 중에 하나를 입력해주세요.");
+        }
+    }
 }
