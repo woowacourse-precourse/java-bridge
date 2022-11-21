8c2473bb4cbb654f56c45a76ecd99364673d31af
Hayoon <hy97@sju.ac.kr>
2022-11-21 19:37:08 +0900

    style(MessageView): 메세지 내용 수정

--- src/main/java/bridge/view/MessageView.java
+++ src/main/java/bridge/view/MessageView.java
@@ -2,7 +2,7 @@ package bridge.view;
 
 public enum MessageView {
 
-    PLAY_BRIDGE_GAME("다리 건너기 게임을 시작합니다." + "\n"),
+    PLAY_BRIDGE_GAME("다리 건너기 게임을 시작합니다."),
     INPUT_BRIDGE_LENGTH("다리의 길이를 입력해주세요."),
     SELECT_TO_MOVE("이동할 칸을 선택해주세요. (위: U, 아래: D)"),
     INPUT_RETRY_OR_NOT_GAME("게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)"),
