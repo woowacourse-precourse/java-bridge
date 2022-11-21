ed6d65d15cef556f0445ebf4b714f1db844cac26
ashlovesliitea <skyrider1811@gmail.com>
2022-11-22 00:21:27 +0900

    feature : Application - 게임 구현

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -1,8 +1,64 @@
 package bridge;
 
+import bridge.model.Bridge;
+import bridge.model.BridgeRandomNumberGenerator;
+import bridge.model.Player;
+import bridge.view.InputView;
+import bridge.view.OutputView;
+
 public class Application {
+    private static InputView inputView = new InputView();
+    private static OutputView outputView = new OutputView();
+    private static BridgeRandomNumberGenerator bridgeRandomNumberGenerator = new BridgeRandomNumberGenerator();
+
+    private static Bridge bridge;
+    private static Player player;
+    private static BridgeGame bridgeGame;
 
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        setGame();
+        while (true) {
+            String inputStep = move();
+            if (bridgeGame.isSuccess(inputStep)) {
+                if (bridgeGame.isFinish()) {
+                    win(inputStep);
+                    break;
+                }
+                continue;
+            }
+
+            String retry = loseAndRetryCheck();
+            if (retry.equals("Q")) {
+                outputView.printResult(bridge, player, inputStep);
+                break;
+            }
+            bridgeGame.retry();
+        }
+    }
+
+    private static void win(String lastStep) {
+        bridgeGame.win();
+        outputView.printResult(bridge, player, lastStep);
+    }
+
+    private static String loseAndRetryCheck() {
+        bridgeGame.lose();
+        String retry = inputView.readGameCommand();
+        return retry;
+    }
+
+    private static String move() {
+        String inputStep = inputView.readMoving();
+        bridgeGame.move();
+        outputView.printMap(bridge, player, inputStep);
+        return inputStep;
+    }
+
+    private static void setGame() {
+        System.out.println("다리 건너기 게임을 시작합니다.\n");
+        int bridgeSize = inputView.readBridgeSize();
+        bridge = new Bridge(bridgeSize, bridgeRandomNumberGenerator);
+        player = new Player();
+        bridgeGame = new BridgeGame(player, bridge);
     }
 }
