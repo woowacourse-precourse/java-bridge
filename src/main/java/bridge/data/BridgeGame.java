4215a1893741d66ba5eb9d0b10a67663044d6146
sora5421 <sojeoung536985@gmail.com>
2022-11-22 05:01:38 +0900

    feat ; make move method

--- src/main/java/bridge/data/BridgeGame.java
+++ src/main/java/bridge/data/BridgeGame.java
@@ -1,16 +1,34 @@
 package bridge.data;
 
+import java.util.List;
+
 /**
  * 다리 건너기 게임을 관리하는 클래스
  */
 public class BridgeGame {
+    private Bridge bridge;
+    private UserBridege userBridege = new UserBridege();
+
+    private boolean correct;
+
+    public void saveBridge(List<String> bridgeResult) {
+        bridge = new Bridge(bridgeResult);
+    }
 
     /**
      * 사용자가 칸을 이동할 때 사용하는 메서드
      * <p>
      * 이동을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void move() {
+    public UserBridege move(int count, String userMove) {
+        boolean correct = getCorrect(count, userMove);
+        userBridege.makeUpBridge(userMove, correct);
+        userBridege.makeDownBridge(userMove, correct);
+        return userBridege;
+    }
+
+    public boolean getCorrect(int count, String userMove) {
+        return bridge.state(count, userMove);
     }
 
     /**
