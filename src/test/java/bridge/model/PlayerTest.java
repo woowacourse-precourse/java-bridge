440706410d82d09b1247dcf7f073f5ab94d168ca
MJoon-Jung <gjgjajaj31@gmail.com>
2022-11-21 13:42:41 +0900

    test<Player>: 존재하지 않는 다리로 이동하면 지도에 X를 그리는지 확인 테스트 추가

---
+++ src/test/java/bridge/model/PlayerTest.java
@@ -0,0 +1,24 @@
+package bridge.model;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.List;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class PlayerTest {
+    Bridge bridge;
+    Player player;
+
+    @BeforeEach
+    void beforeEach() {
+        bridge = new Bridge(List.of("U", "D", "U", "D", "U"));
+        player = new Player();
+    }
+
+    @Test
+    void 존재하지_않는_다리로_이동하면_지도에_X를_그려야한다() {
+        MoveResult move = player.move(bridge, Direction.D);
+        assertThat(move.getDrawType().getDrawCharacter()).isEqualTo("X");
+    }
+}
