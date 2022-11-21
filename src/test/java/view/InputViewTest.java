8d4391188d8a29e03c92281b7910035e6b8125b2
saehim <saeilee.vxv@gmail.com>
2022-11-22 01:06:40 +0900

    test (game) : 게임 생성 테스트
    - 게임 다리 생성
    - 입력범위 넘었을 때 예외처리

--- src/test/java/view/InputViewTest.java
+++ src/test/java/view/InputViewTest.java
@@ -1,4 +1,50 @@
+package view;
+
+import bridge.BridgeMaker;
+import bridge.BridgeRandomNumberGenerator;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.*;
+
 class InputViewTest {
-  
+
+    public static InputStream generateUserInput(String input) {
+        return new ByteArrayInputStream(input.getBytes());
+    }
+
+    @Test
+    void readBridgeSize_기능테스트() {
+        InputStream in = generateUserInput("3");
+        System.setIn(in);
+        int size = InputView.readBridgeSize();
+        BridgeMaker bm = new BridgeMaker(new BridgeRandomNumberGenerator());
+        List<String> bridge = bm.makeBridge(size);
+        assertThat(bridge.size()).isEqualTo(3);
+        System.out.println(bridge.toString());
+    }
+
+    @DisplayName("입력 범위를 넘었을 때 에러 확인")
+    @Test
+    void readBridgeSize_범위넘은_입력() {
+        InputStream in = generateUserInput("31");
+        System.setIn(in);
+        BridgeMaker bm = new BridgeMaker(new BridgeRandomNumberGenerator());
+            assertThatThrownBy(
+                    () -> BridgeMaker.makeBridge())
+                    .isInstanceOf(IllegalArgumentException.class)
+                    .hasMessage("[ERROR] 최소 3이상의 숫자를 적어주세요.");
+    }
+
+    @Test
+    void readBridgeSize() {
+    }
 }
