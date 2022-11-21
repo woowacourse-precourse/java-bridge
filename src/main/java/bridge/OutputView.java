e90b6ea904a8779cfb1773e23d36f482f1a10e27
Sunghyun-0 <zkdldk95@naver.com>
2022-11-21 23:03:23 +0900

    게임 메세지 출력 기능 추가

--- src/main/java/bridge/OutputView.java
+++ src/main/java/bridge/OutputView.java
@@ -11,11 +11,9 @@ public class OutputView {
     public void printGameStartMessage(){
         System.out.println("다리 건너기 게임을 시작합니다.\n\n다리의 길이를 입력해주세요.");
     }
-
     public void printSelectMessage() {
         System.out.println("이동할 칸을 선택해주세요. (위: U, 아래: D)");
     }
-
     public void printRestartMessage() {
         System.out.println("게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)");
     }
