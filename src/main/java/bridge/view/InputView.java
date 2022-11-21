35b4a25c81cccfa5fdedff89c25d8c50bf7d0061
김태용 <amico741@naver.com>
2022-11-22 01:10:26 +0900

    chore: 주석 제거

--- src/main/java/bridge/view/InputView.java
+++ src/main/java/bridge/view/InputView.java
@@ -6,9 +6,6 @@ import camp.nextstep.edu.missionutils.Console;
 
 import java.util.Arrays;
 
-/**
- * 사용자로부터 입력을 받는 역할을 한다.
- */
 public class InputView {
     private static final int MIN_LENGTH = 3;
     private static final int MAX_LENGTH = 20;
@@ -19,27 +16,21 @@ public class InputView {
         return Console.readLine();
     }
 
-    /**
-     * 다리의 길이를 입력받는다.
-     */
+
     public int readBridgeSize(String size) throws IllegalArgumentException {
         validateIsNotEmpty(size);
         validateIsNumber(size);
         return validateIsLengthRange(size);
     }
 
-    /**
-     * 사용자가 이동할 칸을 입력받는다.
-     */
+
     public String readMoving(String direction) throws IllegalArgumentException {
         validateIsNotEmpty(direction);
         validateDirection(direction);
         return direction;
     }
 
-    /**
-     * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
-     */
+
     public String readGameCommand(String decision) throws IllegalArgumentException {
         validateDecision(decision);
         return decision;
--- src/main/java/bridge/view/OutputView.java
+++ src/main/java/bridge/view/OutputView.java
@@ -4,9 +4,6 @@ import bridge.domain.Player;
 import bridge.domain.Result;
 import bridge.util.MessageConstant;
 
-/**
- * 사용자에게 게임 진행 상황과 결과를 출력하는 역할을 한다.
- */
 public class OutputView {
 
     public void printMap(Result result) {
