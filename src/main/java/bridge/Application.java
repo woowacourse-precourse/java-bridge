bf9cab031ca5f21d4bb058385917805a23510e5a
DESKTOP-A922TNL\LeeJinho <hosboy93@naver.com>
2022-11-21 16:23:17 +0900

    feat($Application): 무작위 값을 이용해 다리 생성
    - 다리 생성 메소드 makeBridge 생성
    - bridgeRandomNumberGenerator.generate(); 를 이용해 다리 생성

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -1,10 +1,29 @@
 package bridge;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Application {
 
     public static void main(String[] args) {
         // TODO: 프로그램 구현
         Message.start();
-        InputView.readBridgeSize();
+        int bridgeLength = InputView.readBridgeSize();
+        List<Integer> bridge = makeBridge(bridgeLength);
+        System.out.print("현재 다리 : ");
+        for(int i=0; i<bridge.size(); i++){
+            System.out.print(bridge.get(i) + " ");
+        }
+    }
+
+    public static List<Integer> makeBridge(int bridgeLength) {
+        BridgeRandomNumberGenerator bridgeRandomNumberGenerator = new BridgeRandomNumberGenerator();
+        List<Integer> bridge = new ArrayList<>();
+
+        for(int i=0; i<bridgeLength; i++) {
+            bridge.add(bridgeRandomNumberGenerator.generate());
+        }
+
+        return bridge;
     }
 }
