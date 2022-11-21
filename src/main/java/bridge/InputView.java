662f5169cb22642d870c21a78496e55aa59cd245
es <mascoeskim@gmail.com>
2022-11-21 10:12:52 +0900

    feat(InputView): 사용자에게 다리의 길이를 입력 받는 readBridgeSize 매서드 구현

--- src/main/java/bridge/InputView.java
+++ src/main/java/bridge/InputView.java
@@ -1,5 +1,7 @@
 package bridge;
 
+import camp.nextstep.edu.missionutils.Console;
+
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
  */
@@ -8,10 +10,11 @@ public class InputView {
     /**
      * 다리의 길이를 입력받는다.
      */
-    public int readBridgeSize() {
-        return 0;
+    public static int readBridgeSize() {
+        System.out.println("다리 건너기 게임을 시작합니다.");
+        System.out.println("다리의 길이를 입력해주세요.");
+        return Integer.parseInt(Console.readLine());
     }
-
     /**
      * 사용자가 이동할 칸을 입력받는다.
      */
