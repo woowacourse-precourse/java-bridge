2ab16b12324237062e6a67fca2d32df7edb4df7d
김형기 <khk211113@naver.com>
2022-11-21 15:57:09 +0900

    test: 방향 정보 얻기 테스트 추가

--- src/test/java/bridge/DirectionTest.java
+++ src/test/java/bridge/DirectionTest.java
@@ -3,6 +3,7 @@ package bridge;
 import bridge.model.Direction;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
@@ -12,14 +13,15 @@ public class DirectionTest {
     @DisplayName("올바른 코드를 입력하여 방향에 대해 정보를 얻어올 때 정상 반환")
     @Test
     public void directionGetTest() {
-        assertThat(Direction.of(0)).isEqualTo(Direction.DOWN);
-        assertThat(Direction.of(1)).isEqualTo(Direction.UP);
+        assertThat(Direction.of("D")).isEqualTo(Direction.DOWN);
+        assertThat(Direction.of("U")).isEqualTo(Direction.UP);
     }
 
     @DisplayName("잘못된 코드를 입력하여 방향에 대한 정보를 얻어올 때 예외 처리")
+    @ValueSource(strings = {"Q", "ab", "1", "d"})
     @Test
-    public void wrongDirectionGetTest() {
-        assertThatThrownBy(() -> Direction.of(3))
+    public void wrongDirectionGetTest(String input) {
+        assertThatThrownBy(() -> Direction.of(input))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 }
