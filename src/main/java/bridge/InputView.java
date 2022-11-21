38d883c56b510ce9fe417eba904d9b95a34f10eb
Yujin830 <rladbwls30@naver.com>
2022-11-22 00:10:10 +0900

    Feat: 다리 길이에 숫자 외에 다른 입력이 있을 경우 예외처리

--- src/main/java/bridge/InputView.java
+++ src/main/java/bridge/InputView.java
@@ -10,6 +10,10 @@ public class InputView {
      */
     public static int readBridgeSize() {
         String input = Console.readLine();
+        for(int i=0;i<input.length();i++){
+            if(!(input.charAt(i)>='0' && input.charAt(i)<='9'))
+                throw new IllegalArgumentException(ErrorMessage.BRIDGE_LENGTH_INPUT_ERROR.getErrormessage());
+        }
         int size = Integer.parseInt(input);
         if(size<3||size>20)
             throw new IllegalArgumentException(ErrorMessage.BRIDGE_LENGTH_ERROR.getErrormessage());
