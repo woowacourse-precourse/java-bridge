ee878c83199c07ac3c7915e0c5fd684aeeafcdb2
0112leesy <0112leesy@naver.com>
2022-11-22 13:54:16 +0900

    feat(BridgeGame) : 게임의 최종 결과를 형식에 맞게 반환하는 기능 구현
    재시작 또는 다리를 모두 건넜는지 확인하는 함수를 통해 종료 여부 판단

--- src/main/java/bridge/BridgeGame.java
+++ src/main/java/bridge/BridgeGame.java
@@ -12,7 +12,7 @@ public class BridgeGame {
     private static List<String> bridge;
     private static List<Integer> record;
     private static int trial;
-    private static boolean success;
+    private static String success;
 
     /**
      * 사용자가 칸을 이동할 때 사용하는 메서드
@@ -83,21 +83,42 @@ public class BridgeGame {
         return 'X';
     }
 
+    public boolean checkComplete() {
+        if (record.size() == bridge.size() && !hasFail()) {
+            success = "성공";
+            return true;
+        }
+        return false;
+    }
+
+    public String getResult() {
+        StringBuilder stringBuilder = new StringBuilder("최종 게임 결과\n");
+        stringBuilder.append(getMapToString() + "\n");
+        stringBuilder.append("게임 성공 여부: ");
+        stringBuilder.append(success + "\n");
+        stringBuilder.append("총 시도한 횟수: " + trial);
+        return stringBuilder.toString();
+    }
+
     /**
      * 사용자가 게임을 다시 시도할 때 사용하는 메서드
      * <p>
      * 재시작을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void retry() {
-        this.record = new ArrayList<>();
-        this.trial += 1;
+    public boolean retry(String gameCommand) {
+        if (gameCommand.equals("R")) {
+            this.record = new ArrayList<>();
+            this.trial += 1;
+            return true;
+        }
+        return false;
     }
 
     public void initialize(List<String> bridge) {
         this.bridge = bridge;
         this.record = new ArrayList<>();
         this.trial = 1;
-        this.success = false;
+        this.success = "실패";
     }
 
     public boolean hasFail() {
