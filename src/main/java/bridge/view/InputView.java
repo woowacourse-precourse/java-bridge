08fa40c63b6e16c1de36b13ff8e738e8c1bc976f
username459712 <94822739+username459712@users.noreply.github.com>
2022-11-22 21:26:23 +0900

    feat: static value

---
+++ src/main/java/bridge/view/InputView.java
@@ -0,0 +1,10 @@
+package bridge.view;
+
+import static bridge.constant.Constant.ERROR_MESSAGE;
+
+import bridge.exception.Exception;
+import camp.nextstep.edu.missionutils.Console;
+
+public class InputView {
+    Exception bridgeException = new Exception();
+}
