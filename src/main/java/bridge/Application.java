4f08b9aa8469090b2d4bfa0487ba4c9923998872
Ran <lan4250@naver.com>
2022-11-22 21:58:13 +0900

    REFACTOR/ Application 메서드 10줄 이내로 리팩토링

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -1,72 +1,78 @@
 package bridge;
 
 
+import java.util.ArrayList;
 import java.util.List;
 
 
 public class Application {
 
-    static boolean retryYn = true;  // 종료를 선택하면 false로 바꿔준다.
+    static boolean retryYn = true;
     static int totalCnt = 0;
-
+    static InputView inputView = new InputView();
+    static BridgeRandomNumberGenerator bridgeRandomNumberGenerator = new BridgeRandomNumberGenerator();
+    static BridgeMaker bridgeMaker = new BridgeMaker(bridgeRandomNumberGenerator);
+    static String generatedBridgeStr = "";
+    static int bridgeSize = 0;
+    static List<String> resultBridge = new ArrayList<>();
+    static String inputBridgeStr = "";
+    static boolean successYn;
+    static List<String> generatedBridge = new ArrayList<>();
 
     public static void startBridgeGame() {
         System.out.println("다리 건너기 게임을 시작합니다.");
     }
 
-
-    public static void main(String[] args) {
-        // TODO: 프로그램 구현
-
+    public static void initialSetting() {
         startBridgeGame();
-        InputView inputView = new InputView();
-        BridgeRandomNumberGenerator bridgeRandomNumberGenerator = new BridgeRandomNumberGenerator();
-        BridgeMaker bridgeMaker = new BridgeMaker(bridgeRandomNumberGenerator);
+        inputView = new InputView();
+        // 랜덤으로 다리를 생성한다.
+        bridgeSize = inputView.readBridgeSize();        // 다리 길이를 입력 받아서 저장한다.
+        generatedBridge = bridgeMaker.customMakeBridge(bridgeSize);
+        generatedBridgeStr = bridgeMaker.bridgeStr();        // 1011010111
+        resultBridge = new ArrayList<>();
+        inputBridgeStr = "";
+        successYn = true;
+    }
 
-        // 1) 랜덤으로 다리를 생성한다.
-        int bridgeSize = inputView.readBridgeSize();        // 다리 길이를 입력 받아서 저장한다.
-        List<String> generatedBridge = bridgeMaker.customMakeBridge(bridgeSize);
-        String generatedBridgeStr = bridgeMaker.bridgeStr();        // 1011010111
-
-        /*
+    public static void printRandomBridge() {
         System.out.println("generatedBridgeStr " + generatedBridgeStr);
         System.out.println("랜덤으로 생성한 브릿지");
         for (String b : generatedBridge) {
             System.out.println(b);
         }
-        */
+    }
 
-        List<String> resultBridge;
-
-        String inputBridgeStr = "";
-        boolean successYn = true;
-
-        while (retryYn) {
-
-            successYn = true;
-            ++totalCnt;
-            // 2) 다리 게임
-            inputBridgeStr = inputView.readMoving(generatedBridgeStr,
-                bridgeSize);   // 생성한 다리 정보를 넣어준다
-//            System.out.println("==============================  사용자 입력 inputBridgeStr " + inputBridgeStr);
-
-            if (inputBridgeStr.contains("x") || inputBridgeStr.contains("y")) {
-                successYn = false;
-            }
-
-            if (successYn) {
-                break;
-            }
-            // 3) 실패했으면 게임을 재시작할지 종료할 지 정한다.
-            retryYn = inputView.readGameCommand();
-
-        }
-
-        // 맨 마지막에만 최종 결과 출력
+    public static void finalSetting() {
+        // 최종 결과 출력
         OutputView outputView = new OutputView();
         resultBridge = bridgeMaker.changeStrToBridge(inputBridgeStr);
         outputView.printResult(resultBridge, successYn, totalCnt);
+    }
 
+    public static void main(String[] args) {
+        initialSetting();
+//        printRandomBridge();
+        while (retryYn) {
+            boolean successYn = recursiveBridgeGame();
+            if (successYn) {
+                break;
+            }
+        }
+        finalSetting();
+    }
 
+    public static boolean recursiveBridgeGame() {
+        successYn = true;
+        ++totalCnt;
+        inputBridgeStr = inputView.readMoving(generatedBridgeStr, bridgeSize);
+        if (inputBridgeStr.contains("x") || inputBridgeStr.contains("y")) {
+            successYn = false;
+        }
+        if (successYn) {
+            return true;
+        }
+        retryYn = inputView.readGameCommand();
+        return false;
     }
 }
