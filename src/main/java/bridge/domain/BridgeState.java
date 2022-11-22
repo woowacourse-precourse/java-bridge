1e87a70721556f41598818c68300505048faf571
wnsgml7267 <dlwns7267@naver.com>
2022-11-22 22:29:09 +0900

    feat(view/BridgeState): 칸 이동가능 여부 판단 기능 구현

--- src/main/java/bridge/domain/BridgeState.java
+++ src/main/java/bridge/domain/BridgeState.java
@@ -1,6 +1,30 @@
 package bridge.domain;
 
+import static bridge.command.LocationCommand.*;
+import static bridge.util.Constant.*;
+
+import bridge.command.LocationCommand;
+import java.util.ArrayList;
+import java.util.List;
 
 public class BridgeState {
+    private List<String> map;
+    private final Round round;
+
+    public BridgeState(Round round) {
+        this.map = new ArrayList<>(List.of(INIT_MAP, INIT_MAP));
+        this.round = round;
+    }
+
+    // 이동한 칸이 건널 수 있으면 O 아니면 X를 나타내는 기능
+    public List<String> convertResult(String userInput, List<String> bridge) {
+        if (round.checkRoundFirst()) {
+            convertResultFirstRound(userInput, bridge);
+        } else if (!round.checkRoundFirst()) {
+            convertResultAfterFirstRound(userInput, bridge);
+        }
+        return map;
+    }
+
 
 }
