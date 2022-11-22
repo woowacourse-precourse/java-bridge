53d19e8f9f252118975653faeaf77682248d7f35
juy4556 <kws05050@naver.com>
2022-11-22 16:29:02 +0900

    feat: 다리 길이 입력 시 숫자가 3 이상 20 이하의 숫자가 아닐 때 validation 처리

--- src/main/java/bridge/Exception.java
+++ src/main/java/bridge/Exception.java
@@ -2,11 +2,20 @@ package bridge;
 
 public class Exception {
     private static final String regExp = "^[0-9]+$";
+    private static final int MIN = 3;
+    private static final int MAX = 20;
 
 
     public static void validateIsNumeric(String input) {
         ErrorMessage errorMessage = ErrorMessage.NOT_NUMERIC_VALUE;
         if (!input.matches(regExp)) {
+            throw new IllegalArgumentException(errorMessage.getMessage());
+        }
+    }
+
+    public static void validateInterval(String input) {
+        ErrorMessage errorMessage = ErrorMessage.NOT_VALID_INTERVAL_VALUE;
+        if (Integer.parseInt(input) < MIN || Integer.parseInt(input) > MAX) {
             throw new IllegalArgumentException(errorMessage.getMessage());
         }
     }
