16cc857b751c377f54cd1e51de5ab732f6f826e0
starwook <star.wookk@gmail.com>
2022-11-22 17:39:11 +0900

    test(OutPutViewTest.java): refactor printMap method
    의도와 알맞게 출력되었나 확인

--- src/test/java/bridge/OutputViewTest.java
+++ src/test/java/bridge/OutputViewTest.java
@@ -6,6 +6,9 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.Arrays;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
 
 import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.*;
@@ -15,6 +18,11 @@ class OutputViewTest {
     @DisplayName("문자열 게임에 맞는 출력으로 변환")
     @Test
     void printMap() {
-        OutputView.printMap(Arrays.asList("O O"," X "),2);
+        Map<String, List<String>> bridgeMap = new LinkedHashMap<>();
+        List<String> bridgeUnit = Arrays.asList("O","O"," ");
+        bridgeMap.put("U",bridgeUnit);
+        List<String> bridgeUnit2 = Arrays.asList(" "," ","X");
+        bridgeMap.put("D",bridgeUnit2);
+        OutputView.printMap(bridgeMap,2);
     }
 }
