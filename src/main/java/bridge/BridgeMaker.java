202712218d632140b68b79ce9ab5cdcb9c3b654e
suheonjoo <suheonj95@naver.com>
2022-11-22 20:18:35 +0900

    refactor(BridgeMaker): makeBridge 메서드 indent 1로 메서드 추출

--- src/main/java/bridge/BridgeMaker.java
+++ src/main/java/bridge/BridgeMaker.java
@@ -5,9 +5,6 @@ import java.util.List;
 
 import bridge.util.GameConst;
 
-/**
- * 다리의 길이를 입력 받아서 다리를 생성해주는 역할을 한다.
- */
 public class BridgeMaker {
 
 	private final BridgeNumberGenerator bridgeNumberGenerator;
@@ -16,19 +13,19 @@ public class BridgeMaker {
 		this.bridgeNumberGenerator = bridgeNumberGenerator;
 	}
 
-	/**
-	 * @param size 다리의 길이
-	 * @return 입력받은 길이에 해당하는 다리 모양. 위 칸이면 "U", 아래 칸이면 "D"로 표현해야 한다.
-	 */
 	public List<String> makeBridge(int size) {
 		List<String> bridge = new ArrayList<>();
 		for (int i = 0; i < size; i++) {
-			if (bridgeNumberGenerator.generate() == 1) {
-				bridge.add(GameConst.MOVING_UP);
-				continue;
-			}
-			bridge.add(GameConst.MOVING_DOWN);
+			addBridgeAnswer(bridge);
 		}
 		return bridge;
+	}
+
+	private void addBridgeAnswer(List<String> bridge) {
+		if (bridgeNumberGenerator.generate() == 1) {
+			bridge.add(GameConst.MOVING_UP);
+			return;
+		}
+		bridge.add(GameConst.MOVING_DOWN);
 	}
 }
