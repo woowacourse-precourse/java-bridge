f81974ef724541d718c43490196e96ce3ba49475
2do1 <dowon3036@naver.com>
2022-11-22 20:47:00 +0900

    feat: 게임 시작 게임 재시작 게임 종료 기능 구현

---
+++ src/main/java/bridge/BridgeGameController.java
@@ -0,0 +1,48 @@
+package bridge;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class BridgeGameController {
+
+    static final String GAME_START_MESSAGE = "다리 건너기 게임을 시작합니다.";
+    InputView inputView = new InputView();
+    OutputView outputView = new OutputView();
+    BridgeGame bridgeGame = new BridgeGame();
+    BridgeMaker bridgeMaker = new BridgeMaker(new BridgeRandomNumberGenerator());
+
+    public void startGame() {
+        System.out.println(GAME_START_MESSAGE);
+        int bridgeSize = inputView.readBridgeSize();
+
+        List<String> bridge = bridgeMaker.makeBridge(bridgeSize);
+        do {
+            playGame(bridge);
+        } while (!bridgeGame.isClear() && !bridgeGame.isQuitGame()) ;
+        outputView.printResult(bridgeGame.getUpBridge(), bridgeGame.getDownBridge(), bridgeGame);
+    }
+
+    public void playGame(List<String> bridge) {
+        for (int index = 0; index < bridge.size(); index++) {
+            String moving = inputView.readMoving();
+            boolean canMove = bridgeGame.move(bridge, index, moving);
+            outputView.printMap(bridgeGame.getUpBridge(), bridgeGame.getDownBridge());
+            if(restartOrPlayGame(canMove)) {
+                break;
+            }
+        }
+    }
+
+    public boolean restartOrPlayGame(boolean canMove) {
+        if (!canMove) {
+            String gameCommand = inputView.readGameCommand();
+            if (gameCommand.equals("Q")) {
+                bridgeGame.quit();
+                return true;
+            }
+            bridgeGame.retry();
+            return true;
+        }
+        return false;
+    }
+}
