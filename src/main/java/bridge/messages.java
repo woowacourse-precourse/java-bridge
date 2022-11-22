eaf3b20f12e53b633b4087a3fa4b2269cd9773dd
soyunim <togksk123@naver.com>
2022-11-22 16:48:01 +0900

    refactor: messages.java 수정

--- src/main/java/bridge/messages.java
+++ src/main/java/bridge/messages.java
@@ -2,15 +2,17 @@ package bridge;
 
 public class messages {
     public interface words{
+        int max = 20;
+        int min = 3;
         String right = "O";
         String wrong = "X";
-        String partition = "|";
+        String partition = "| ";
         String success = "성공";
         String losing = "실패";
     }
 
     public interface game{
-        String startGame = "다리 건너기 게임을 시작합니다.";
+        String startGame = "다리 건너기 게임을 시작합니다.\n";
         String bridgeSize = "다리의 길이를 입력해주세요.";
         String movingBridge = "이동할 칸을 선택해주세요. (위: U, 아래: D)";
         String restartGame = "게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)";
