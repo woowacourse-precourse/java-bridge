53bfac079710efbec7bf08c9a82e20a8cc2be2b4
bando99 <bando991225@gmail.com>
2022-11-22 23:26:11 +0900

    REFACT : indent 조정 및 메서드 길이수정, 메서드 기능이 이름에서 나오도록 수정

--- src/main/java/bridge/BridgeGame.java
+++ src/main/java/bridge/BridgeGame.java
@@ -22,16 +22,6 @@ public class BridgeGame {
         location = START_LOCATION;
         this.userDirections = new ArrayList<>();
     }
-    /**
-     * 사용자가 칸을 이동할 때 사용하는 메서드
-     * <p>
-     * 이동을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
-     */
-    public List<String> getUserDirections(){
-        return this.userDirections;
-    }
-
-
 
     public GameState play(String direction){
         move(direction);
@@ -70,20 +60,7 @@ public class BridgeGame {
         }
         return false;
     }
-    public boolean isStop(GameState state){
-        if(state == GameState.FALL){
-            return false;
-        }
-        if (state == GameState.WIN){
-            return false;
-        }
-        return true;
-    }
-    /**
-     * 사용자가 게임을 다시 시도할 때 사용하는 메서드
-     * <p>
-     * 재시작을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
-     */
+
 
     public boolean isRetry(String answer){
         if(answer.equals("Q")){
