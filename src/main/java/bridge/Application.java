ece3e3c2c046108335259e79469bd6b331b4d79c
changeme4585 <benscience@naver.com>
2022-11-22 14:49:17 +0900

    add: 다리 사이즈 입력

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -4,5 +4,8 @@ public class Application {
 
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        System.out.println("다리 건너기 게임을 시작합니다.\n");
+        InputView input = new InputView();
+        int bSize = input.readBridgeSize();
     }
 }
