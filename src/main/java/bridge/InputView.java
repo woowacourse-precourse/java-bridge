c53ed13b04a62ae01fb474285f48ffb773cc9cd7
dhyyp1001 <dhyyp1001@gmail.com>
2022-11-22 18:27:42 +0900

    feat: InputView.java / 다리의 길이를 입력 받는 readBridgeSize() 추가

--- src/main/java/bridge/InputView.java
+++ src/main/java/bridge/InputView.java
@@ -1,9 +1,21 @@
 package bridge;
 
+import camp.nextstep.edu.missionutils.Console;
+
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
  */
 public class InputView {
+    int bridgeSize_in = 0;
+    String upAndDown_in = "";
+    String retryAndQuit_in = "";
+
+    public void readBridgeSize() {
+        System.out.println("다리의 길이를 입력해주세요.");
+        String bridgeSizeLetter_in = Console.readLine();
+        bridgeSizeNotLetterValidation(bridgeSizeLetter_in);
+        bridgeSizeRangeValidation(bridgeSize_in);
+    }
 
     /**
      * 다리의 길이를 입력받는다.
