be0243a889a1b4a2c73cf1a3b8163a68090b09e4
GraphTree <graphnodesed@gmail.com>
2022-11-22 14:44:38 +0900

    feat : run program

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -1,8 +1,14 @@
 package bridge;
 
+import camp.nextstep.edu.missionutils.Console;
+
+import java.util.List;
+
 public class Application {
 
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        GameController gameController = new GameController();
+        gameController.gameStart();
+
     }
 }
