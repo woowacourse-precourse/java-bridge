42ac18edc5ca3714c5dd66ea4abbaf9a2f4d517b
beombu <sunil1369@bible.ac.kr>
2022-11-22 12:51:06 +0900

    Feat(BridgeGame) : 칸 이동시 수행 기능 추가

--- src/main/java/bridge/BridgeGame.java
+++ src/main/java/bridge/BridgeGame.java
@@ -1,16 +1,60 @@
 package bridge;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * 다리 건너기 게임을 관리하는 클래스
  */
 public class BridgeGame {
+    private final List<String> bridge;
+    private List<BridgeResult> topResult;
+    private List<BridgeResult> bottomResult;
+
+    public BridgeGame(List<String> bridge) {
+        this.bridge = bridge;
+        this.topResult = new ArrayList<>();
+        this.bottomResult = new ArrayList<>();
+    }
 
     /**
      * 사용자가 칸을 이동할 때 사용하는 메서드
      * <p>
      * 이동을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void move() {
+    public boolean move(String selectCell, int numberOfHits) {
+        if(checkCell(selectCell, numberOfHits)){
+            success(selectCell);
+            return true;
+        }
+        fail(selectCell);
+        return false;
+    }
+
+    private boolean checkCell(String selectCell, int numberOfHits) {
+        return bridge.get(numberOfHits).equals(selectCell);
+    }
+
+    private void fail(String selectCell) {
+        if(selectCell.equals("U")){
+            topResult.add(BridgeResult.FAIL);
+            bottomResult.add(BridgeResult.NONE);
+        }
+        if(selectCell.equals("D")){
+            topResult.add(BridgeResult.NONE);
+            bottomResult.add(BridgeResult.FAIL);
+        }
+    }
+
+    private void success(String selectCell) {
+        if(selectCell.equals("U")){
+            topResult.add(BridgeResult.SUCCESS);
+            bottomResult.add(BridgeResult.NONE);
+        }
+        if(selectCell.equals("D")){
+            topResult.add(BridgeResult.NONE);
+            bottomResult.add(BridgeResult.SUCCESS);
+        }
     }
 
     /**
