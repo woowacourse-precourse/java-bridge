1aa416eba17a96f1754802a57e4e268b8a26a9eb
limzzum <chfhddl1234@naver.com>
2022-11-22 22:46:06 +0900

    feat : 줄바꿈 메서드 추가

--- src/main/java/bridge/OutputView.java
+++ src/main/java/bridge/OutputView.java
@@ -28,4 +28,7 @@ public class OutputView {
     public void printGameStart(){
         System.out.println("다리 건너기 게임을 시작합니다.");
     }
+    public void enter(){
+        System.out.println();
+    }
 }
