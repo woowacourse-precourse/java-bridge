982b6ca6a2214dc558a5535ab709d1c288b5fb2c
eunji53 <duqtj00@naver.com>
2022-11-22 11:43:06 +0900

    feat : 결과 출력 함수

--- src/main/java/bridge/OutputView.java
+++ src/main/java/bridge/OutputView.java
@@ -23,6 +23,11 @@ public class OutputView {
      * <p>
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void printResult() {
+    public void printResult(List<String> bridgeState,int count) {
+        System.out.println("최종 게임 결과");
+        System.out.println(bridgeState.get(0)+"]");
+        System.out.println(bridgeState.get(1)+"]");
+        System.out.printf("게임 성공 여부: %s\n");
+        System.out.printf("총 시도한 횟수: %d\n",count);
     }
 }
