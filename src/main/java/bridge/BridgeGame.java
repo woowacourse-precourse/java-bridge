61eb7570b5813cc274fc83effe689793f42f297a
JangHyojoon <fdasfaj9@gmail.com>
2022-11-22 20:12:07 +0900

    다리 건너기 - 다리 칸 이동 기능

--- src/main/java/bridge/BridgeGame.java
+++ src/main/java/bridge/BridgeGame.java
@@ -1,16 +1,41 @@
 package bridge;
 
+import java.util.LinkedList;
+import java.util.List;
+
 /**
  * 다리 건너기 게임을 관리하는 클래스
  */
 public class BridgeGame {
+    private final List<String> upMapper;
+    private final List<String> downMapper;
 
+    public BridgeGame() {
+        upMapper = new LinkedList<>();
+        downMapper = new LinkedList<>();
+    }
     /**
      * 사용자가 칸을 이동할 때 사용하는 메서드
      * <p>
      * 이동을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void move() {
+    public List<List<String>> move(Mark mark) {
+        if (mark.getDirection().equals(BridgeDirection.UP.getDirection())) {
+            return addUpMapper(mark);
+        }
+        return addDownMapper(mark);
+    }
+
+    private List<List<String>> addUpMapper(Mark mark) {
+        upMapper.add(mark.getMark());
+        downMapper.add(" ");
+        return List.of(upMapper, downMapper);
+    }
+
+    private List<List<String>> addDownMapper(Mark mark) {
+        downMapper.add(mark.toString());
+        upMapper.add(" ");
+        return List.of(upMapper, downMapper);
     }
 
     /**
--- src/main/java/bridge/BridgeProgram.java
+++ src/main/java/bridge/BridgeProgram.java
@@ -8,21 +8,28 @@ public class BridgeProgram {
   private final InputView input;
   private final OutputView output;
   private final BridgeMaker bridgeMaker;
+  private final BridgeGame bridgeGame;
 
-  public BridgeProgram(InputView input, OutputView output, BridgeMaker bridgeMaker) {
+
+  public BridgeProgram(InputView input, OutputView output, BridgeMaker bridgeMaker,BridgeGame bridgeGame) {
     this.route = new LinkedList<>();
     this.input = input;
     this.output = output;
     this.bridgeMaker = bridgeMaker;
+    this.bridgeGame = bridgeGame;
   }
 
   public void run() {
-    List<String> bridge = bridgeMaker.makeBridge(getBridgeSize());
+    List<String> bridgeRoute = bridgeMaker.makeBridge(getBridgeSize());
+    Bridge bridge = new Bridge(bridgeRoute);
+    int movingCount = 0;
 
     boolean isRunning = true;
     while (isRunning) {
       String inputDirection = getInputDirection();
 
+      Mark mark = bridge.matchRoute(inputDirection, movingCount++);
+      List<List<String>> route =  bridgeGame.move(mark);
     }
   }
   private int getBridgeSize() {
