857ade89a1fa60e9b995fa75815c714f75324c99
c4fiber <qudcjf153@gmail.com>
2022-11-22 18:32:25 +0900

    fix(OutputView): 최종 게임 결과를 출력하지 않는 오류 수정

--- src/main/java/bridge/OutputView.java
+++ src/main/java/bridge/OutputView.java
@@ -30,10 +30,12 @@ public class OutputView {
      * <p>
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void printResult(boolean didWin, int tried) {
+    public void printResult(String[] mapResult, boolean didWin, int tried) {
         String gameResult = "실패";
         if (didWin) gameResult = "성공";
 
+        System.out.println("최종 게임 결과");
+        printMap(mapResult);
         System.out.println("게임 성공 여부: " + gameResult);
         System.out.println("총 시도한 횟수: " + tried);
     }
