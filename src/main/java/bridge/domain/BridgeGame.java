96a67a80b6f870ea1cdcfa4dc7bc7df632e19c07
jmjmjames <chungjm0711@gmail.com>
2022-11-22 12:01:51 +0900

    feat: 다리 건너기 게임 move 메서드 구현

--- src/main/java/bridge/domain/BridgeGame.java
+++ src/main/java/bridge/domain/BridgeGame.java
@@ -4,13 +4,30 @@ package bridge.domain;
  * 다리 건너기 게임을 관리하는 클래스
  */
 public class BridgeGame {
+    private final BridgeGameAnswer gameAnswer;
+    private BridgeGameStatus gameStatus;
+
+
+    public BridgeGame(BridgeGameAnswer gameAnswer) {
+        this.gameAnswer = gameAnswer;
+        gameStatus = BridgeGameStatus.gameStart();
+    }
 
     /**
      * 사용자가 칸을 이동할 때 사용하는 메서드
      * <p>
      * 이동을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void move() {
+    public MovingResult move(Round round, Direction direction) {
+        MovingResult result = gameAnswer.check(round, direction);
+        checkGameQuit(round, result);
+        return result;
+    }
+
+    private void checkGameQuit(Round round, MovingResult result) {
+        if (gameAnswer.isLastRound(round) || result.isFail()) {
+            gameStatus = BridgeGameStatus.QUIT;
+        }
     }
 
     /**
