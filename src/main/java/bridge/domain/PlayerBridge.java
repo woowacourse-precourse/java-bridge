8314ab24e40eaa21a2e05a25c2a227a91a00b7e9
임원정 <oddish0513@gmail.com>
2022-11-22 18:54:09 +0900

    feat(bridge): 예외처리 추가

--- src/main/java/bridge/domain/PlayerBridge.java
+++ src/main/java/bridge/domain/PlayerBridge.java
@@ -3,27 +3,37 @@ package bridge.domain;
 import java.util.ArrayList;
 import java.util.List;
 
+import static bridge.Constants.DOWN;
+import static bridge.Constants.UP;
+
+
 public class PlayerBridge {
 	private final List<String> playerBridge;
 
 	public PlayerBridge() {
-		// todo 예외처리
 		this.playerBridge = new ArrayList<>();
 	}
 
-	void init(){
+	void init() {
 		playerBridge.clear();
 	}
 
-	public void add(String move){
+	public void add(String move) {
+		validate(move);
 		playerBridge.add(move);
 	}
 
-	int getLastIndex(){
+	private void validate(String move) {
+		if (!move.equals(UP) && !move.equals(DOWN)) {
+			throw new IllegalArgumentException();
+		}
+	}
+
+	int getLastIndex() {
 		return playerBridge.size() - 1;
 	}
 
-	String getLastValue(){
+	String getLastValue() {
 		return playerBridge.get(getLastIndex());
 	}
 }
