99d1011dc66414eec0ee3cfbea36609c2c550b85
username459712 <94822739+username459712@users.noreply.github.com>
2022-11-22 21:31:53 +0900

    feat: printBridgeLengthQuestion()-> 길이 입력 문구 출력

--- src/main/java/bridge/view/OutputView.java
+++ src/main/java/bridge/view/OutputView.java
@@ -4,4 +4,8 @@ public class OutputView {
     public void printStartBridgeGame() {
         System.out.println("다리 건너기 게임을 시작합니다.");
     }
+
+    public void printBridgeLengthQuestion() {
+        System.out.println("\n다리의 길이를 입력해주세요");
+    }
 }
