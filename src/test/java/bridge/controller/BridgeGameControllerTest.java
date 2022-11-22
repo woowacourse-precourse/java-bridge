25e96a07cd524c09f32e152dd1b780c779ca15b4
Min-ji99 <61926751+Min-ji99@users.noreply.github.com>
2022-11-22 15:52:05 +0900

    Test : BridgeGameController 입력 오류 시 다시 입력 받는지 확인하는 테스트 코드 작성

---
+++ src/test/java/bridge/controller/BridgeGameControllerTest.java
@@ -0,0 +1,47 @@
+package bridge.controller;
+
+import bridge.util.ErrorMessage;
+import camp.nextstep.edu.missionutils.test.NsTest;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeTest;
+import static org.assertj.core.api.Assertions.assertThat;
+
+class BridgeGameControllerTest extends NsTest{
+    private static final int BRIDGE_SIZE=5;
+    BridgeGameController bridgeGameController;
+
+    @BeforeEach
+    void setup(){
+        bridgeGameController=new BridgeGameController();
+    }
+    @Test
+    @DisplayName("다리의 길이가 잘못된 입력이면 계속 입력받는지 확인")
+    void inputBridgeSize(){
+        assertRandomNumberInRangeTest(() -> {
+            run("K", "3", "U", "U", "Q");
+            assertThat(output()).contains(ErrorMessage.ERROR_IS_NOT_DIGIT.print());
+        }, 1, 0, 1);
+    }
+    @Test
+    @DisplayName("이동할 칸이 잘못된 입력이면 계속 입력받는지 확인")
+    void inputMoveOne(){
+        assertRandomNumberInRangeTest(() -> {
+            run("3", "K", "U", "U", "Q");
+            assertThat(output()).contains(ErrorMessage.ERROR_MOVING.print());
+        }, 1, 0, 1);
+    }
+    @Test
+    void inputRetryOrQuit(){
+        assertRandomNumberInRangeTest(() -> {
+            run("3", "U", "U", "J", "Q");
+            assertThat(output()).contains(ErrorMessage.ERROR_RETRY_OR_QUIT.print());
+        }, 1, 0, 1);
+    }
+    @Override
+    protected void runMain() {
+        bridgeGameController.gameStart();
+    }
+}
