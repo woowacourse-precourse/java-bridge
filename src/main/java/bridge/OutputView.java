9af7bd283f7fcc13a6381512bcdd86f817feece7
p <gustjr2332@naver.com>
2022-11-22 20:01:07 +0900

    feat(OutView) : 최종결과 출력 기능 구현

--- src/main/java/bridge/OutputView.java
+++ src/main/java/bridge/OutputView.java
@@ -10,9 +10,11 @@ public class OutputView {
     private static final String READ_BRIDGESIZE = "다리의 길이를 입력해주세요.";
     private static final String READ_MOVING = "이동할 칸을 선택해주세요. (위: U, 아래: D)";
     private static final String READ_GAMECOMMAND = "게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)";
-
-    private static int ABOVEOFBRIDGE = BridgeDirection.UP.getBridgeRandomNumber();
-    private static int BELOWOFBRIDGE = BridgeDirection.DOWN.getBridgeRandomNumber();
+    private static final String GAME_OVER = "최종 게임 결과";
+    private static final String GAME_SUCCESSORFAIURE = "게임 성공 여부: %s\n";
+    private static final String TOTAL_NUMBEROFATTEMPT = "총 시도한 횟수: %d\n";
+    private static final int ABOVEOFBRIDGE = BridgeDirection.UP.getBridgeRandomNumber();
+    private static final int BELOWOFBRIDGE = BridgeDirection.DOWN.getBridgeRandomNumber();
 
     public void printStartGame() {
         System.out.println(STARTGAME);
@@ -52,7 +54,10 @@ public class OutputView {
      * <p>
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void printResult() {
-
+    public void printResult(List<List<String>> totalBridgeResult, int numberOfAttempt, String successOrFailure) {
+        System.out.println(GAME_OVER);
+        printMap(totalBridgeResult.get(ABOVEOFBRIDGE), totalBridgeResult.get(BELOWOFBRIDGE));
+        System.out.printf(GAME_SUCCESSORFAIURE, successOrFailure);
+        System.out.printf(TOTAL_NUMBEROFATTEMPT, numberOfAttempt);
     }
 }
