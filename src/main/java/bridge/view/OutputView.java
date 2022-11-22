75bf8a1f44651cf26242ce52c5cd63585a0d41c8
kimtaesoo99 <107785279+kimtaesoo99@users.noreply.github.com>
2022-11-22 19:24:54 +0900

    refactor: (OutputView) 온전하게 출력만 하도록 리펙토링

--- src/main/java/bridge/view/OutputView.java
+++ src/main/java/bridge/view/OutputView.java
@@ -1,17 +1,12 @@
 package bridge.view;
 
-
-import static bridge.enums.BridgeType.*;
-import static bridge.enums.GameMapStatus.*;
-
 import bridge.domain.User;
 
 /**
  * 사용자에게 게임 진행 상황과 결과를 출력하는 역할을 한다.
  */
 public class OutputView {
-    private StringBuilder upState = new StringBuilder();
-    private StringBuilder downState = new StringBuilder();
+
 
     public void printStartGame() {
         System.out.println("다리 건너기 게임을 시작합니다.\n");
@@ -29,61 +24,13 @@ public class OutputView {
         System.out.println("게임을 다시 시도할지 여부를 입력해주세여.(재시도: R, 종료: Q)");
     }
 
-    public void printProgressMap() {
-        System.out.println(START_MAP.getSymbol() + upState + END_MAP.getSymbol());
-        System.out.println(START_MAP.getSymbol() + downState + END_MAP.getSymbol());
-    }
-
     /**
      * 현재까지 이동한 다리의 상태를 정해진 형식에 맞춰 출력한다.
      * <p>
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void printMap(int location, boolean pass, String moveUpOrDown) {
-        setBoundaryLine(location);
-        checkUpOrDown(moveUpOrDown, pass);
-        printProgressMap();
-    }
-
-    private void setBoundaryLine(int location) {
-        if (location >= 1) {
-            upState.append(BOUNDARY_LINE.getSymbol());
-            downState.append(BOUNDARY_LINE.getSymbol());
-        }
-    }
-
-    private void checkUpOrDown(String UpAndDown, boolean pass) {
-        if (UpAndDown.equals(UP.getCommand())) {
-            setUpState(pass);
-        }
-        if (UpAndDown.equals(DOWN.getCommand())) {
-            setDownState(pass);
-        }
-    }
-
-    private void setUpState(boolean pass) {
-        if (pass) {
-            upState.append(PASS.getSymbol());
-        }
-        if (!pass) {
-            upState.append(FAIL.getSymbol());
-        }
-        downState.append(SPACE.getSymbol());
-    }
-
-    private void setDownState(boolean pass) {
-        if (pass) {
-            downState.append(PASS.getSymbol());
-        }
-        if (!pass) {
-            downState.append(FAIL.getSymbol());
-        }
-        upState.append(SPACE.getSymbol());
-    }
-
-    public void clearMap() {
-        upState.setLength(0);
-        downState.setLength(0);
+    public void printMap(String map) {
+        System.out.println(map);
     }
 
     /**
@@ -91,9 +38,9 @@ public class OutputView {
      * <p>
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void printResult(User user) {
+    public void printResult(User user, String map) {
         System.out.println("\n최종 게임 결과");
-        printProgressMap();
+        System.out.println(map);
         System.out.println("\n게임 성공 여부: " + isPass(user.isSuccessComplete()));
         System.out.println("총 시도한 횟수: " + user.getTryCount());
     }
