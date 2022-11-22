47208016943f58a8270ca212062b6570888b64c1
KTH-96 <taehyun960@gmail.com>
2022-11-22 13:55:00 +0900

    test(BridgeTest): 특정 인덱스의 위치와 다리의 크기를 구하는 테스트 작성

--- src/test/java/bridge/domain/BridgeTest.java
+++ src/test/java/bridge/domain/BridgeTest.java
@@ -1,10 +1,9 @@
 package bridge.domain;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
-import static org.junit.jupiter.params.provider.Arguments.*;
+import static org.junit.jupiter.params.provider.Arguments.of;
 
-import bridge.utils.Constant;
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Stream;
@@ -16,14 +15,40 @@ import org.junit.jupiter.params.provider.MethodSource;
 class BridgeTest {
 
     @ParameterizedTest
-    @MethodSource("generateBridgeData")
+    @MethodSource("generateWrongBridgeData")
     @DisplayName("양쪽 다리를 지나갈 수 없다면 예외가 발생한다.")
-    void bridgeTest(List<String> bridge) {
-        assertThatThrownBy(() -> new Bridge(bridge))
+    void bridgeMakeTest(List<String> input) {
+        assertThatThrownBy(() -> new Bridge(input))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
+    @ParameterizedTest
+    @MethodSource("generateBridgeData")
+    @DisplayName("특정 인덱스의 다리위치가 맞는지 체크하는 기능")
+    void bridgeCorrectPositionTest(List<String> input) {
+        Bridge bridge = new Bridge(input);
+        for (int i = 0; i < input.size(); i++) {
+            assertThat(bridge.isCorrectLastPosition(i, input.get(i))).isTrue();
+        }
+    }
+
+    @ParameterizedTest
+    @MethodSource("generateBridgeData")
+    @DisplayName("다리 사이즈를 반환하는지 체크하는 테스트")
+    void bridgeSizeTest(List<String> input) {
+        Bridge bridge = new Bridge(input);
+        assertThat(bridge.size()).isEqualTo(input.size());
+    }
+
     public static Stream<Arguments> generateBridgeData() {
+        return Stream.of(
+                of(Arrays.asList("U", "U", "D")),
+                of(Arrays.asList("U", "D", "U")),
+                of(Arrays.asList("D", "U", "D"))
+        );
+    }
+
+    public static Stream<Arguments> generateWrongBridgeData() {
         return Stream.of(
                 of(Arrays.asList("U", "U", "Broken Bridge")),
                 of(Arrays.asList("U", "Broken Bridge", "D")),
