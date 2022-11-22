6d670f0d1758b801fbbb7f860d01cfad9a3683ee
anneu <b.imnuj@gmail.com>
2022-11-22 20:43:11 +0900

    refactor: O, X 출력을 위한 로직을 ui에서 처리하도록 하도록 변경

--- src/main/java/bridge/domain/BridgeMoveState.java
+++ src/main/java/bridge/domain/BridgeMoveState.java
@@ -6,14 +6,6 @@ import java.util.Objects;
 
 public class BridgeMoveState {
 
-    private static final String UP = "U";
-    private static final String DOWN = "D";
-    private static final String BLANK = " ";
-    private static final String MOVE = "O";
-    private static final String STOP = "X";
-
-    private final List<String> upState = new ArrayList<>();
-    private final List<String> downState = new ArrayList<>();
     private final List<Boolean> state = new ArrayList<>();
 
     public void update(boolean canMove) {
@@ -28,33 +20,6 @@ public class BridgeMoveState {
         return state.get(getMoveCount() - 1);
     }
 
-    public void update(String moving, boolean canMove) {
-        String state = makeState(canMove);
-        if (moving.equals(UP)) {
-            upState.add(state);
-            downState.add(BLANK);
-        }
-        if (moving.equals(DOWN)) {
-            downState.add(state);
-            upState.add(BLANK);
-        }
-    }
-
-    private String makeState(boolean canMove) {
-        if (canMove) {
-            return MOVE;
-        }
-        return STOP;
-    }
-
-    public List<String> getUpState() {
-        return upState;
-    }
-
-    public List<String> getDownState() {
-        return downState;
-    }
-
     @Override
     public boolean equals(Object o) {
         if (this == o) {
@@ -63,12 +28,16 @@ public class BridgeMoveState {
         if (o == null || getClass() != o.getClass()) {
             return false;
         }
-        BridgeMoveState that = (BridgeMoveState) o;
-        return Objects.equals(upState, that.upState) && Objects.equals(downState, that.downState);
+        BridgeMoveState moveState = (BridgeMoveState) o;
+        return Objects.equals(state, moveState.state);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(upState, downState);
+        return Objects.hash(state);
+    }
+
+    public List<Boolean> getState() {
+        return state;
     }
 }
