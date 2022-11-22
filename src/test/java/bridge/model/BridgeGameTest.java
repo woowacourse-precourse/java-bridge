df25a658adde43ba500f9b6d129b4975a937abbd
win9 <krrr8@naver.com>
2022-11-22 18:23:36 +0900

    Test(test): 테스트수행전, 후 세팅 구축
    static 변수로 인해 원복되지 않는 버그로 인해
    1로 강제로 변환해주는 메소드를 각 테스트 전, 후로 실행하도록 한다.

--- src/test/java/bridge/model/BridgeGameTest.java
+++ src/test/java/bridge/model/BridgeGameTest.java
@@ -1,7 +1,6 @@
 package bridge.model;
 
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.*;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
@@ -11,6 +10,11 @@ class BridgeGameTest {
     private Player player = new Player();
     private Referee referee = new Referee();
 
+    @BeforeEach
+    @AfterEach
+    void setHoleGameCount(){
+        BridgeGame.holeGameCount = 1;
+    }
     @DisplayName("R을 입력하면 게임이 재시작 되어 전체 게임횟수가 증가한다.")
     @ParameterizedTest
     @ValueSource(strings = {"R"})
