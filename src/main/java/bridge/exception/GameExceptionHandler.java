a616d9c8e3670482b7912fdaec444a1f1757e36a
yongseungLeeDec <yonson.dev@gmail.com>
2022-11-22 15:06:12 +0900

    feat: 연속된 입력 오류 예외 발생시 프로그램 강제 종료 기능 구현

--- src/main/java/bridge/exception/GameExceptionHandler.java
+++ src/main/java/bridge/exception/GameExceptionHandler.java
@@ -2,11 +2,15 @@ package bridge.exception;
 
 public class GameExceptionHandler {
     private static final String ERROR_PREFIX = "[ERROR] ";
+    private static final int SERIAL_EXCEPTION_THRESHOLD = 5;
+    private static final String THRESHOLD_EXCEEDED = "연속된 예외 횟수가 허용치를 초과하여 프로그램이 종료되었습니다.";
 
     private static GameExceptionHandler instance;
 
-    private GameExceptionHandler() {
+    private int numSerialExceptions;
 
+    private GameExceptionHandler() {
+        this.numSerialExceptions = 0;
     }
 
     public static GameExceptionHandler getGameExceptionHandler() {
@@ -18,5 +22,13 @@ public class GameExceptionHandler {
 
     public void handleIllegalArgumentException(IllegalArgumentException exception) {
         System.out.println(ERROR_PREFIX + exception.getMessage());
+        this.numSerialExceptions++;
+        throwExceptionToMainIfExceedsThreshold();
+    }
+
+    private void throwExceptionToMainIfExceedsThreshold() throws IllegalArgumentException {
+        if (SERIAL_EXCEPTION_THRESHOLD < this.numSerialExceptions) {
+            throw new IllegalArgumentException(ERROR_PREFIX + THRESHOLD_EXCEEDED);
+        }
     }
 }
