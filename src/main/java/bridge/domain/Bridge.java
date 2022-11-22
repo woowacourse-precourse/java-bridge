b26909c28edb720eca9d9dddd0d4c7c6baf1c010
footdev <ekclstkfka5@gmail.com>
2022-11-22 18:14:34 +0900

    feat: 다리를 건널 수 있는지 확인하는 기능 추가
    현재 위치의 다리를 건널 수 있는지 확인하는 기능을 추가함.

--- src/main/java/bridge/domain/Bridge.java
+++ src/main/java/bridge/domain/Bridge.java
@@ -1,4 +1,20 @@
 package bridge.domain;
 
+import java.util.List;
+
 public class Bridge {
+
+    public static final int MIN_BOUND = 3;
+    public static final int MAX_BOUND = 20;
+
+    private final List<String> bridge;
+
+    public Bridge(List<String> bridge) {
+        this.bridge = bridge;
+    }
+
+    public boolean canCrossBridge(int phase, String command) {
+        return bridge.get(phase)
+                .equals(command);
+    }
 }
