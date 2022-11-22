1c2d20f728fc458c0ab3295c606be4509c765328
hhiyeon <hhyyeon0214@gmail.com>
2022-11-22 15:21:28 +0900

    test:MovingTest-check moving input value are not U and D

--- src/test/java/bridge/domain/MovingTest.java
+++ src/test/java/bridge/domain/MovingTest.java
@@ -8,8 +8,15 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 public class MovingTest {
     @DisplayName("이동할 칸 입력 값이 길이가 1이 아니면 예외가 발생한다.")
     @Test
-    void checkInputByOverOneSize() {
+    void checkMovingInputByOverSize() {
         assertThatThrownBy(() -> new Moving("1234"))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("이동할 칸 입력 값이 U와 D가 아니면 예외가 발생한다.")
+    @Test
+    void checkMovingInputForm() {
+        assertThatThrownBy(() -> new Moving("u"))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 }
