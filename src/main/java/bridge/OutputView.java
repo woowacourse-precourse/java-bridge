166c140c404375c8777f37a1df914b8963e0b66a
jasonoh22 <jasonoh22@naver.com>
2022-11-22 23:12:02 +0900

    FEAT[java-bridge]: 최종 게임 출력 및 게임과정 출력 수정

--- src/main/java/bridge/OutputView.java
+++ src/main/java/bridge/OutputView.java
@@ -12,54 +12,25 @@ public class OutputView {
      * <p>
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void printMap(String location,  List<String>upBridge ,List<String> downBridge) {
-        if(location.charAt(0) == 'U' && location.charAt(1) == 'O'){
-            BridgeAdd(upBridge, 1);BridgeAddElse(downBridge);
-        }
-        else if(location.charAt(0) == 'U' && location.charAt(1) != 'O'){
-            BridgeAdd(upBridge, 0);BridgeAddElse(downBridge);
-        }
-        else if(location.charAt(0) == 'D' && location.charAt(1) != 'O'){
-            BridgeAdd(downBridge, 1);BridgeAddElse(upBridge);
-        }
-        else{
-            BridgeAdd(downBridge, 0);BridgeAddElse(upBridge);
-        }
-        printResult(upBridge);
-        printResult(downBridge);
+    public void printMap(String move,  List<String>upBridge ,List<String> downBridge) {
+        /*BridgeNumberGenerator BridgeNumberGenerator = null;
+        BridgeMaker BridgeMaker = new BridgeMaker(BridgeNumberGenerator);
+        if(move.charAt(0) == 'U'){
+            BridgeMaker.bridgeAddMove(move , upBridge);
+            BridgeMaker.bridgeAddElseMove(downBridge);
+        }else {
+            BridgeMaker.bridgeAddMove(move , downBridge);
+            BridgeMaker.bridgeAddElseMove(upBridge);}*/
+        printCourse(upBridge);
+        printCourse(downBridge);
     }
-
-    public List<String> BridgeAdd(List<String> Bridge, int chk) {
-        if(chk == 1){
-            Bridge.add(" ");
-            Bridge.add("O");
-            Bridge.add(" ");
-            Bridge.add("|");
-        }
-        else{
-            Bridge.add(" ");
-            Bridge.add("X");
-            Bridge.add(" ");
-            Bridge.add("|");
-        }
-        return Bridge;
-    }
-
-    public List<String> BridgeAddElse(List<String> Bridge) {
-        Bridge.add(" ");
-        Bridge.add(" ");
-        Bridge.add(" ");
-        Bridge.add("|");
-        return Bridge;
-    }
-
     /**
      * 게임의 최종 결과를 정해진 형식에 맞춰 출력한다.
      * <p>
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
 
-    public void printResult(List<String> Bridge) {
+    public void printCourse(List<String> Bridge) {
         System.out.print("[");
         for (int i = 0; i <Bridge.size() ; i++) {
             if(i == Bridge.size() -1){
@@ -69,5 +40,17 @@ public class OutputView {
         }
         System.out.print("]");
         System.out.println("");
+    }
+
+    public void printResult(List<String> upBridge,List<String> downBridge, int size , int retryNum) {
+        System.out.println("최종 게임 결과");
+        printCourse(upBridge);
+        printCourse(downBridge);
+        var msg ="";
+        if(size == 0)msg="성공";
+        else msg="실패";
+        System.out.println("최종 게임 결과" + msg);
+        System.out.println("총 시도한 횟수:"+ retryNum);
+
     }
 }
