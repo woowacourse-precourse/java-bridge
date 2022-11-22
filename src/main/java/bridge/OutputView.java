1b1c14316769299a9799776847602fb10c312312
tunkcalb <wjdgusdndi@gmail.com>
2022-11-22 18:13:01 +0900

    output view

--- src/main/java/bridge/OutputView.java
+++ src/main/java/bridge/OutputView.java
@@ -1,16 +1,37 @@
 package bridge;
 
+import java.util.ArrayList;
+import java.util.List;
 /**
  * 사용자에게 게임 진행 상황과 결과를 출력하는 역할을 한다.
  */
 public class OutputView {
 
+    private static final String Up = "U";
+    private static final String Down = "D";
+    private static String resultMap;
+
+    public void startBridgeGame(){
+        System.out.println("다리 건너기 게임을 시작합니다.");
+    }
     /**
      * 현재까지 이동한 다리의 상태를 정해진 형식에 맞춰 출력한다.
      * <p>
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void printMap() {
+    public void printMap(ArrayList<String> moveResult, List<String> bridge) {
+        ArrayList<String> upMap = new ArrayList<>();
+        ArrayList<String> downMap = new ArrayList<>();
+
+        for (int i = 0; i < moveResult.size(); i++){
+            String move = moveResult.get(i);
+            String updown = bridge.get(i);
+            upMap.add(checkUpMap(move, updown));
+            downMap.add(checkDownMap(move, updown));
+        }
+        resultMap = upMap.toString().replaceAll(", ","|") + "\n"
+                + downMap.toString().replaceAll(", ", "|");
+        System.out.println(resultMap);
     }
 
     /**
@@ -18,6 +39,30 @@ public class OutputView {
      * <p>
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void printResult() {
+    public void printResult(String result, int attempts) {
+        System.out.println("최종 게임 결과");
+        System.out.println(resultMap);
+        System.out.println("게임 성공 여부: " + result);
+        System.out.println("총 시도한 횟수: " + attempts);
+    }
+
+    private static String checkUpMap(String move, String updown){
+        if (move.equals("success") && updown.equals(Up)){
+            return " O ";
+        }
+        if (move.equals("fail") && updown.equals(Down)){
+            return " X ";
+        }
+        return "   ";
+    }
+
+    private static String checkDownMap(String move, String updown){
+        if (move.equals("success") && updown.equals(Down)){
+            return " O ";
+        }
+        if (move.equals("fail") && updown.equals(Up)){
+            return " X ";
+        }
+        return "   ";
     }
 }
