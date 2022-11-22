41a14993c79f033e943f19179212aa1841288f72
wonseokjee <wsji9404@gmail.com>
2022-11-22 17:30:34 +0900

    Test(InputViewTest): 추가 InputViewTest
    스켈레톤 코드 생성

---
+++ src/test/java/bridge/InputViewTest.java
@@ -0,0 +1,24 @@
+package bridge;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class InputViewTest {
+
+    @Test
+    void userInput() {
+    }
+
+    @Test
+    void readBridgeSize() {
+    }
+
+    @Test
+    void readMoving() {
+    }
+
+    @Test
+    void readGameCommand() {
+    }
+}
