0dcbe2b6a67b4eb70549643da23b49368dc8a71b
osm <osm0822@naver.com>
2022-11-22 13:42:29 +0900

    feat(BridgeGame) : 재시작을 위한 초기화 기능 구현

--- src/main/java/bridge/BridgeGame.java
+++ src/main/java/bridge/BridgeGame.java
@@ -46,9 +46,11 @@ public class BridgeGame {
     /**
      * 사용자가 게임을 다시 시도할 때 사용하는 메서드
      * <p>
-     * 재시작을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
+     * 게임의 step정보, status정보를 초기화
      */
     public void retry() {
+        step = 0;
+        status = BridgeGameStstus.PROCEEDING;
     }
 
     public List<String> getBridge() {
