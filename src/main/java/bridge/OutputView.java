378af9f391246fee1add161d133b49a2b2cd3ba0
jsk5341 <102288039+jsk5341@users.noreply.github.com>
2022-11-22 14:21:55 +0900

    feat: implement function that print final result

--- src/main/java/bridge/OutputView.java
+++ src/main/java/bridge/OutputView.java
@@ -56,6 +56,15 @@ public class OutputView {
      */
     public void printResult(List<String> bridgeAnswer, List<String> currentResult, boolean userChoice, int stage,int count) {
 
+        System.out.println("최종 게임 결과");
+        printMap(currentResult);
+        if(bridgeAnswer.size() == stage-1){
+            System.out.println("게임 성공 여부: 성공");
+        }
+        if(bridgeAnswer.size() > stage-1){
+            System.out.println("게임 성공 여부: 실패");
+        }
+        System.out.println("총 시도한 횟수: "+count);
 
     }
 }
