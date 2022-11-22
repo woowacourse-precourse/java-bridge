eab2b060d74c9ebfc7c6f5e037c9a8b0bfc129c6
JunginPark4364 <wjddlsee@gmail.com>
2022-11-22 17:34:49 +0900

    feat: 결과, 라운드마다 상태 출력

--- src/main/java/bridge/view/OutputView.java
+++ src/main/java/bridge/view/OutputView.java
@@ -1,5 +1,9 @@
 package bridge.view;
 
+import bridge.Result;
+
+import java.util.List;
+
 /**
  * 사용자에게 게임 진행 상황과 결과를 출력하는 역할을 한다.
  *
@@ -10,13 +14,32 @@ package bridge.view;
 public class OutputView {
 
     private static final String START_MESSAGE = "다리 건너기 게임을 시작합니다.";
+    private static final String FINAL_RESULT_MESSAGE = "최종 게임 결과";
+    private static final String CHECK_SUCCESS_MESSAGE = "게임 성공 여부: %s";
+    private static final String TRY_COUNT = "총 시도한 횟수: %s";
+    private static final String BEGIN = "[";
+    private static final String END = "]";
+    private static final String DELIMITER = "|";
+    private static final String SUCCESS = "성공";
+    private static final String FAIL = "실패";
+    private static final String BLANK_LINE = "\n";
+    private static final String EXCEPTION = "[ERROR] ";
 
     /**
      * 현재까지 이동한 다리의 상태를 정해진 형식에 맞춰 출력한다.
      * <p>
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void printMap() {
+    public void printMap(Result result) {
+        StringBuilder context = new StringBuilder();
+        for (List<String> bridge : result.getResults()) {
+            context.append(BEGIN);
+            appendResult(context, bridge);
+            context.replace(context.length() - 1, context.length(), END);
+            context.append(BLANK_LINE);
+        }
+
+        System.out.println(context);
     }
 
     /**
@@ -24,10 +47,52 @@ public class OutputView {
      * <p>
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void printResult() {
+    public void printResult(Result result, int count) {
+        System.out.println(FINAL_RESULT_MESSAGE);
+        printMap(result);
+
+        printSuccessOrFail(result);
+
+        printTryCount(count);
+    }
+
+    private void appendResult(StringBuilder sb, List<String> r) {
+        for (String s : r) {
+            sb.append(" ");
+            sb.append(s);
+            sb.append(" ");
+            sb.append(DELIMITER);
+        }
+    }
+
+    private void printSuccessOrFail(Result result) {
+        System.out.printf(CHECK_SUCCESS_MESSAGE, getSuccessOrFail(result));
+        printBlankLine();
+    }
+
+    private void printTryCount(int count) {
+        System.out.printf(TRY_COUNT, count);
+        printBlankLine();
+    }
+
+    private String getSuccessOrFail(Result result) {
+        if (result.hasFail()) {
+            return FAIL;
+        }
+
+        return SUCCESS;
+    }
+
+    public void printBlankLine() {
+        System.out.println();
     }
 
     public void printStart() {
         System.out.println(START_MESSAGE);
+        printBlankLine();
+    }
+
+    public void printException(Exception exception) {
+        System.out.println(EXCEPTION + exception.getMessage());
     }
 }
