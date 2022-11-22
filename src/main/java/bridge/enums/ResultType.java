90f5f860600be2318319f2802b1f0e449f661a5e
eun-byeol <62530884+eun-byeol@users.noreply.github.com>
2022-11-22 17:48:01 +0900

    refactor(ResultType): code 필드 삭제
    ResultType의 code 필드 삭제함.

--- src/main/java/bridge/enums/ResultType.java
+++ src/main/java/bridge/enums/ResultType.java
@@ -1,30 +1,17 @@
 package bridge.enums;
 
-import java.util.Arrays;
-
-import static bridge.utils.ErrorMessage.*;
-
 public enum ResultType {
-    NOT_SELECT(" ", 0),
-    WRONG("X", 1),
-    CORRECT("O", 2);
+    NOT_SELECT(" "),
+    WRONG("X"),
+    CORRECT("O");
 
     private String representation;
-    private final int code;
 
-    ResultType(String representation, int code) {
+    ResultType(String representation) {
         this.representation = representation;
-        this.code = code;
     }
 
-    public static ResultType selectResultTypeByCode(int code) {
-        return Arrays.stream(ResultType.values())
-                .filter(result -> result.code == code)
-                .findAny()
-                .orElseThrow(() -> new IllegalArgumentException(NUMBER_NOT_OPTIONS));
-    }
-
-    public int getCode() {
-        return code;
+    public String getRepresentation() {
+        return representation;
     }
 }
