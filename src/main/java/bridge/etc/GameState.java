4116c51fe03c7063245a45169848d2b92f97bd5c
Insuk Lee <66675919+insukL@users.noreply.github.com>
2022-11-22 21:38:17 +0900

    feat: add bridge string return method
    - 다리 객체 상태 반환 메소드 추가
    - distance 사용 수정에 따른 get 메소드 수정
    
    (cherry picked from commit a1e330b654b3424352a8a8d2e823eb5c8742c073)

--- src/main/java/bridge/etc/GameState.java
+++ src/main/java/bridge/etc/GameState.java
@@ -1,5 +1,39 @@
 package bridge.etc;
 
 public enum GameState {
-    CONTINUE, CLEAR, FAIL, EXIT
+    CONTINUE {
+        @Override
+        public String getLastBlockState(Boolean isRightDirection) {
+            if (isRightDirection) {
+                return String.format(" %s ", PassState.RIGHT);
+            }
+            return String.format(" %s ", PassState.NO_CHOICE);
+        }
+    },
+    CLEAR {
+        @Override
+        public String getLastBlockState(Boolean isRightDirection) {
+            if (isRightDirection) {
+                return String.format(" %s ", PassState.RIGHT);
+            }
+            return String.format(" %s ", PassState.NO_CHOICE);
+        }
+    },
+    FAIL {
+        @Override
+        public String getLastBlockState(Boolean isRightDirection) {
+            if (isRightDirection) {
+                return String.format(" %s ", PassState.NO_CHOICE);
+            }
+            return String.format(" %s ", PassState.FAIL);
+        }
+    },
+    EXIT {
+        @Override
+        public String getLastBlockState(Boolean isRightDirection) {
+            return "";
+        }
+    };
+
+    public abstract String getLastBlockState(Boolean isRightDirection);
 }
