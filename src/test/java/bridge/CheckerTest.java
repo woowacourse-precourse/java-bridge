cc45c252849804d91ba11c2d7f296397a64b4e3f
nn98 <jkllhgb@gmail.com>
2022-11-22 15:44:48 +0900

    Test: FunctionTest

--- src/test/java/bridge/CheckerTest.java
+++ src/test/java/bridge/CheckerTest.java
@@ -5,6 +5,8 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import camp.nextstep.edu.missionutils.test.NsTest;
+import constant.Values.BridgeCase;
+import constant.Values.MoveCase;
 import org.junit.jupiter.api.Test;
 
 public class CheckerTest extends NsTest {
@@ -13,16 +15,42 @@ public class CheckerTest extends NsTest {
 
 
     @Test
-    void 기능_테스트_재시도() {
+    void 기능_테스트_다리길이_입력() {
         assertSimpleTest(() -> {
-            assertThat(new Checker().checkRetry("R")).isEqualTo(true);
+            assertThat(new Checker().checkValidate("15")).isEqualTo(15);
         });
     }
 
     @Test
-    void 기능_테스트_다리길이() {
+    void 기능_테스트_이동_입력() {
         assertSimpleTest(() -> {
-            assertThat(new Checker().checkValidate("15")).isEqualTo(new Integer(15));
+            assertThat(new Checker().checkMoveValidate("U")).isEqualTo("U");
+            assertThat(new Checker().checkMoveValidate("D")).isEqualTo("D");
+        });
+    }
+
+    @Test
+    void 기능_테스트_이동_결과() {
+        assertSimpleTest(() -> {
+            assertThat(new Checker().checkMoveSuccess("U","U")).isEqualTo(MoveCase.UP_UP);
+            assertThat(new Checker().checkMoveSuccess("U","D")).isEqualTo(MoveCase.UP_DOWN);
+            assertThat(new Checker().checkMoveSuccess("D","D")).isEqualTo(MoveCase.DOWN_DOWN);
+            assertThat(new Checker().checkMoveSuccess("D","U")).isEqualTo(MoveCase.DOWN_UP);
+        });
+    }
+
+    @Test
+    void 기능_테스트_재시도_입력() {
+        assertSimpleTest(() -> {
+            assertThat(new Checker().checkGameOption("R")).isEqualTo("R");
+            assertThat(new Checker().checkGameOption("Q")).isEqualTo("Q");
+        });
+    }
+
+    @Test
+    void 기능_테스트_재시도() {
+        assertSimpleTest(() -> {
+            assertThat(new Checker().checkRetry("R")).isEqualTo(true);
         });
     }
 
