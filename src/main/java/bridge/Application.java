55969c45a11fde8b7b7a276eca1173d98b7f83a0
lkdcode <dlrlejr1@naver.com>
2022-11-22 10:41:54 +0900

    ✨Feat : 다리 건너기 게임

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -1,8 +1,14 @@
 package bridge;
 
+import bridge.Controller.SystemController;
+
 public class Application {
 
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+
+        SystemController user = new SystemController();
+        user.SystemController();
+
     }
 }
