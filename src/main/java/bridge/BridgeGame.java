4346e280d034b8be388e50cdbbdf60adc145c0e0
Ryokuman <ryokuman@naver.com>
2022-11-22 22:11:26 +0900

    feature ( BridgeGame ) : add constructor

--- src/main/java/bridge/BridgeGame.java
+++ src/main/java/bridge/BridgeGame.java
@@ -1,8 +1,23 @@
 package bridge;
 
+import java.util.List;
+
 public class BridgeGame {
-    public void move() {
+    private final List<String> bridge;
+    private final int size;
+    private int stage;
+
+    public BridgeGame(int size) {
+        BridgeMaker bridgeMaker = new BridgeMaker(new BridgeRandomNumberGenerator());
+        this.size = size;
+        this.bridge = bridgeMaker.makeBridge(this.size);
+        this.stage = 0;
     }
-    public void retry() {
+
+    public void move(String input) {
+    }
+
+    public void retry(String input) {
+
     }
 }
