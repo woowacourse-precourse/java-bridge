a46d19ba09c6ca3d28af7eb9556595cb9e764a4b
cookienc <kim76744@naver.com>
2022-11-22 15:43:03 +0900

    chore(BridgePositionTest): BridgePositionTest 패키지 변경

--- src/test/java/bridge/model/BridgePositionTest.java
+++ src/test/java/bridge/model/BridgePositionTest.java
@@ -1,6 +1,5 @@
-package bridge;
+package bridge.model;
 
-import bridge.model.BridgePosition;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.params.ParameterizedTest;
