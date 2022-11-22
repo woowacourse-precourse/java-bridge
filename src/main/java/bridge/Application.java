7ec7595fc7f5307e479df9c93f1188cf03a41561
Juuuic <sn990908a@gmail.com>
2022-11-22 17:25:59 +0900

    feat:사용자가 갈 방향입력, 예외 판별 기능 구현

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -1,17 +1,29 @@
 package bridge;
 
+import java.util.List;
+
 public class Application {
 
     public static void main(String[] args) {
+        InputView inputView = new InputView();
+        BridgeException bridgeException = new BridgeException();
+        BridgeRandomNumberGenerator bridgeRandomNumberGenerator = new BridgeRandomNumberGenerator();
+        BridgeMaker bridgeMaker = new BridgeMaker(bridgeRandomNumberGenerator);
+
 
         System.out.print("다리 건너기 게임을 시작합니다.");
 
         // 다리 길이 입력 -> inputView 클래스의 readBridgeSize()
-        InputView inputView = new InputView();
-        int bridgeLength = inputView.readBridgeSize();
+        int inputLength = inputView.readBridgeSize();
+        int bridgeLength = bridgeException.bridgeInput(inputLength);
 
         // 다리 길이만큼 다리 생성 -> BridgeMaker 클래스의 makeBridge(int size)
+        List<String> bridge = bridgeMaker.makeBridge(bridgeLength);
+
         // 이동할 칸 입력받기 -> InputView 클래스의 readMoving()
+        String direction = inputView.readMoving();
+        bridgeException.UserInput(direction);
+
         // 입력받은 값에 따라 이동 -> BridgeGame 클래스의 move()
         // 이동한 후 다리 출력 -> OutputView 클래스의 printMap()
         // 성공 시 -> OutputView 클래스의 printResult() -> 종료
--- src/main/java/bridge/BridgeException.java
+++ src/main/java/bridge/BridgeException.java
@@ -2,6 +2,7 @@ package bridge;
 
 public class BridgeException {
 
+    //다리 길이 입력 예외 판별
     public int bridgeInput(int input) {
         int n = 0;
         try{
@@ -14,5 +15,12 @@ public class BridgeException {
             throw new IllegalArgumentException("[ERROR]");
         }
         return n;
+    }
+
+    //사용자가 갈 방향 선택 예외 판별
+    public void UserInput(String input){
+        if(!(input.equals("U") || input.equals("D"))){
+            throw new IllegalArgumentException("[ERROR]");
+        }
     }
 }
