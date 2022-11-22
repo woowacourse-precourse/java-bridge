6506a0b9b2e38cb7bf2f959916529935de1664f7
hong <hong@hongui-MacBookAir.local>
2022-11-22 20:03:58 +0900

    feat: 예외처리 추가

--- src/main/java/bridge/InputView.java
+++ src/main/java/bridge/InputView.java
@@ -1,5 +1,7 @@
 package bridge;
 
+import camp.nextstep.edu.missionutils.Console;
+
 import static camp.nextstep.edu.missionutils.Console.readLine;
 
 /**
@@ -10,24 +12,53 @@ public class InputView {
     /**
      * 다리의 길이를 입력받는다.
      */
-    public int readBridgeSize() {
-        System.out.println("다리의 길이를 입력해주세요.");
-        return Integer.parseInt(readLine());
+    public int readBridgeSize(){
+        String pattern = "^[3-9]$|^1\\d$|^20$";
+        String message = "다리의 길이를 입력해주세요.";
+        String errorMessage = "[ERROR] 3 이상 20 이하의 숫자를 입력해주세요.";
+        String input = readLine(message, pattern, errorMessage);
+        return Integer.parseInt(input);
     }
 
     /**
      * 사용자가 이동할 칸을 입력받는다.
      */
     public String readMoving() {
-        System.out.println("이동할 칸을 선택해주세요. (위: U, 아래: D)");
-        return readLine();
+        String pattern = "^U$|^D$";
+        String message = "이동할 칸을 선택해주세요. (위: U, 아래: D)";
+        String errorMessage = "[ERROR] U 혹은 D 를 입력해주세요.";
+        return readLine(message, pattern, errorMessage);
     }
 
     /**
      * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
      */
     public String readGameCommand() {
-        System.out.println("게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)");
-        return readLine();
+        String pattern = "^R$|^Q$";
+        String message = "게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)";
+        String errorMessage = "[ERROR] R 혹은 Q 를 입력해주세요.";
+        return readLine(message, pattern, errorMessage);
+    }
+
+    public String readLine(String message, String pattern, String errorMessage){
+        String input;
+        while(true){
+            try{
+                System.out.println(message);
+                input = Console.readLine();
+                ExceptionHandling(input, pattern, errorMessage);
+                break;
+            }
+            catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            }
+        }
+        return input;
+    }
+
+    private void ExceptionHandling(String input, String pattern, String errorMessage) throws IllegalArgumentException{
+        if(!input.matches(pattern)){
+            throw new IllegalArgumentException(errorMessage);
+        }
     }
 }
