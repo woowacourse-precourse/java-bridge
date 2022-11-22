7b83b016d6ed0671fd4505775ac2014ab3d9feb7
koo9b9h <90121024+koo9b9h@users.noreply.github.com>
2022-11-22 17:35:43 +0900

    refactor: BridgeGameController의  excute 실행 메소드 기능 분할
    중점: 들여쓰기 최소화 메소드 10줄 이하로 만들기
    
    excute(): 게임 실행 기능
    
    loopGameUnit(): 반복되는 기능의 부분을 분할
    
    isUnitFail(): 한 칸 단위로 실패 했을 경우 다시 시작 할지 묻는 기능 분할
    
    restart(): 다시 시작하는 경우 유저 다리 초기화 및 도전횟수 1회 추가 기능 분할
    
    isLoopFinish(): 게임이 성공하거나 포기하여 최종적으로 결과를 보여주는 기능 분할

--- src/main/java/bridge/controller/BridgeGameController.java
+++ src/main/java/bridge/controller/BridgeGameController.java
@@ -7,31 +7,51 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class BridgeGameController {
+    BridgeGame bridgeGame = new BridgeGame();
+    int challengeCount = 1;
+
     public void excute() {
-        int challengeCount = 1;
-        OutputView outputView = new OutputView();
         List<String> userSelectResult = new ArrayList<>();
-        BridgeGame bridgeGame = new BridgeGame();
         bridgeGame.startSetUp();
         List<String> computerResult = bridgeGame.make();
-        while (true) {
+        loopGameUnit(userSelectResult, computerResult);
+    }// excute
+
+    private void loopGameUnit(List<String> userSelectResult, List<String> computerResult) {
+        boolean loopController = true;
+        while (loopController) {
             userSelectResult = bridgeGame.move(userSelectResult);
             boolean unitSuccess = bridgeGame.isUnitSuccess(userSelectResult, computerResult);
             boolean gameSuccess = bridgeGame.isGameSuccess(userSelectResult, computerResult, unitSuccess);
-            if (!unitSuccess) {
-                boolean restart = bridgeGame.retry(unitSuccess, gameSuccess);
-                if (restart) {
-                    challengeCount++;
-                    userSelectResult.clear();
-                    continue;
-                }//if
-                outputView.printResult(userSelectResult, unitSuccess, gameSuccess, challengeCount);
-                break;
-            }//if
-            if (gameSuccess) {
-                outputView.printResult(userSelectResult, unitSuccess, gameSuccess, challengeCount);
-                break;
-            }//if
+            boolean willRestart = isUnitFail(unitSuccess, gameSuccess);
+            restart(willRestart, userSelectResult);
+            loopController = isLoopFinish(userSelectResult, unitSuccess, gameSuccess, challengeCount, willRestart);
         }//while
-    }// excute
+    }//loopGameUnit
+
+    private boolean isUnitFail(boolean unitSuccess, boolean gameSuccess) {
+        if (!unitSuccess) {
+            return bridgeGame.retry(unitSuccess, gameSuccess);
+        }//if
+        return false;
+    }//isUnitFail
+
+    private List<String> restart(boolean willRestart, List<String> userSelectResult) {
+        if (willRestart == true) {
+            challengeCount++;
+            userSelectResult.clear();
+        }//if
+        return userSelectResult;
+    }//restart
+
+    private boolean isLoopFinish(
+            List<String> userSelectResult, boolean unitSuccess, boolean gameSuccess, int challengeCount, boolean willRestart) {
+        OutputView outputView = new OutputView();
+        boolean loopController = true;
+        if ((willRestart == false && !unitSuccess) || gameSuccess) {
+            outputView.printResult(userSelectResult, unitSuccess, gameSuccess, challengeCount);
+            loopController = false;
+        }//if
+        return loopController;
+    }// isLoopFinish
 }// end class
