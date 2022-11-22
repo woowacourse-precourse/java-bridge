81e29d492873a2ae7c534fe6db05e6991583aa0d
kissshot1104 <kissshot.heartunderblade1104@gmail.com>
2022-11-22 19:09:43 +0900

    Refactor(OutputViewTest) : 테스트 코드 간소화
    OutputViewTest 클래스의
    너무 긴 메소드의 길이를 줄이기 위해 코드를 간소화 했습니다.

--- src/test/java/bridge/OutputViewTest.java
+++ src/test/java/bridge/OutputViewTest.java
@@ -3,134 +3,87 @@ package bridge;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
 
 class OutputViewTest extends CustomNsTest {
 
     private final OutputView outputView = new OutputView();
 
-    @Test
+    /////////////////////////////////////////////////////////////////////////////////////////////
+    static List<List<List<String>>> printMapTestParameter() {
+        return List.of(
+                List.of(
+                        List.of("O", " ", "O"),//"[ O |   | O ]"
+                        List.of(" ", "O", " ") //"[   | O |   ]"
+                ),
+                List.of(
+                        List.of("O", "O", "O"),//"[ O | O | O ]"
+                        List.of(" ", " ", " ") //"[   |   |   ]"
+                ),
+                List.of(
+                        List.of(" ", " ", " "),//"[   |   |   ]"
+                        List.of("O", "O", "O") //"[ O | O | O ]"
+                ),
+                List.of(
+                        List.of("O", " "), //"[ O |   ]"
+                        List.of(" ", "X")  //"[   | X ]"
+                )
+        );
+    }
+
+    //클래스 하나 만들어 놓자
     @DisplayName("현재 다리 상황 출력 테스트")
-    void printMapTest() {
+    @MethodSource("printMapTestParameter")
+    @ParameterizedTest
+    void printMapTest(final List<List<String>> bridge) {
+        final List<String> upSide = bridge.get(0);
+        final List<String> downSide = bridge.get(1);
+        final String upSideBridge = upSide.stream().collect(Collectors.joining(" | ", "[ ", " ]"));
+        final String downSideBridge = upSide.stream().collect(Collectors.joining(" | ", "[ ", " ]"));
 
-        //위 1, 3 아래 2 맞췄을 때
-        {
-            List<String> upSide = List.of("O", " ", "O");
-            List<String> downSide = List.of(" ", "O", " ");
+        outputView.printMap(upSide, downSide);
+        assertThat(output()).contains(
+                upSideBridge,
+                downSideBridge
+        );
+    }
+/////////////////////////////////////////////////////////////////////////////////////////////////
 
-            outputView.printMap(upSide, downSide);
-            assertThat(output()).contains(
-                    "[ O |   | O ]",
-                    "[   | O |   ]"
-            );
-        }
-
-        //위 1, 2, 3 맞췄을 때
-        {
-            List<String> upSide = List.of("O", "O", "O");
-            List<String> downSide = List.of(" ", " ", " ");
-
-            outputView.printMap(upSide, downSide);
-            assertThat(output()).contains(
-                    "[ O | O | O ]",
-                    "[   |   |   ]"
-            );
-        }
-
-        //아래 1, 2, 3 맞췄을 때
-        {
-            List<String> upSide = List.of(" ", " ", " ");
-            List<String> downSide = List.of("O", "O", "O");
-
-            outputView.printMap(upSide, downSide);
-            assertThat(output()).contains(
-                    "[   |   |   ]",
-                    "[ O | O | O ]"
-            );
-        }
-
-        //중간에 하나 틀렸을 때
-        {
-            List<String> upSide = List.of("O", " ");
-            List<String> downSide = List.of(" ", "X");
-
-            outputView.printMap(upSide, downSide);
-            assertThat(output()).contains(
-                    "[ O |   ]",
-                    "[   | X ]"
-            );
-        }
+    static List<List<String>> printResultTestParameter() {
+        return List.of(
+                List.of(
+                        "[ O |   | O ]",
+                        "[   | O |   ]"
+                ),
+                List.of(
+                        "[ O | O | O ]",
+                        "[   |   |   ]"
+                ),
+                List.of(
+                        "[   |   |   ]",
+                        "[ O | O | O ]"
+                )
+        );
     }
 
-    @Test
     @DisplayName("최종 결과 출력 테스트")
-    void printResult() {
-
-        //번갈아 가며 맞았을 경우
-        {
-            String upSideBridge = "[ O |   | O ]";
-            String downSideBridge = "[   | O |   ]";
-            int count = 2;
-
-            outputView.printResult(upSideBridge, downSideBridge, count);
-            assertThat(output()).contains(
-                    "최종 게임 결과",
-                    "[ O |   | O ]",
-                    "[   | O |   ]",
-                    "게임 성공 여부: 성공",
-                    "총 시도한 횟수: 2"
-            );
-        }
-
-        //윗줄 다 맞았을 경우
-        {
-            String upSideBridge = "[ O | O | O ]";
-            String downSideBridge = "[   |   |   ]";
-            int count = 3;
-
-            outputView.printResult(upSideBridge, downSideBridge, count);
-            assertThat(output()).contains(
-                    "최종 게임 결과",
-                    "[ O | O | O ]",
-                    "[   |   |   ]",
-                    "게임 성공 여부: 성공",
-                    "총 시도한 횟수: 3"
-            );
-        }
-
-        //아랫줄 다 맞았을 경우
-        {
-            String upSideBridge = "[   |   |   ]";
-            String downSideBridge = "[ O | O | O ]";
-            int count = 4;
-
-            outputView.printResult(upSideBridge, downSideBridge, count);
-            assertThat(output()).contains(
-                    "최종 게임 결과",
-                    "[   |   |   ]",
-                    "[ O | O | O ]",
-                    "게임 성공 여부: 성공",
-                    "총 시도한 횟수: 4"
-            );
-        }
-
-        //중간에 틀렸을 경우
-        {
-            String upSideBridge = "[   | X ]";
-            String downSideBridge = "[ O |   ]";
-            int count = 5;
-
-            outputView.printResult(upSideBridge, downSideBridge, count);
-            assertThat(output()).contains(
-                    "최종 게임 결과",
-                    "[   | X ]",
-                    "[ O |   ]",
-                    "게임 성공 여부: 실패",
-                    "총 시도한 횟수: 5"
-            );
-        }
-
+    @MethodSource("printResultTestParameter")
+    @ParameterizedTest
+    void printResultTest(List<String> bridge) {
+        int count = 2;
+        outputView.printResult(bridge.get(0), bridge.get(1), count);
+        assertThat(output()).contains(
+                "최종 게임 결과",
+                bridge.get(0),
+                bridge.get(1),
+                "게임 성공 여부: 성공",
+                "총 시도한 횟수: 2"
+        );
     }
+    ////////////////////////////////////////////////////////////////////////////////////////
 }
