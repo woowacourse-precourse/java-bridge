339f04bdf3fe9847a14a10bc3a5fe535e0239818
hoonn9 <hun9935@gmail.com>
2022-11-22 15:25:07 +0900

    refactor(InputView): input 메서드들의 접근 제한자를 private로 수정

--- src/main/java/bridge/view/InputView.java
+++ src/main/java/bridge/view/InputView.java
@@ -46,7 +46,7 @@ public class InputView {
         return retryInput(this::inputMoving);
     }
     
-    public BridgeMove inputMoving() {
+    private BridgeMove inputMoving() {
         System.out.println("이동할 칸을 선택해주세요. (위: U, 아래: D)");
         String input = Console.readLine();
         validateReadMovingInput(input);
@@ -67,7 +67,7 @@ public class InputView {
         return retryInput(this::inputGameCommand);
     }
     
-    public BridgeGameCommand inputGameCommand() {
+    private BridgeGameCommand inputGameCommand() {
         System.out.println("게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)");
         String input = Console.readLine();
         validateReadGameCommandInput(input);
@@ -80,7 +80,7 @@ public class InputView {
         }
     }
     
-    public int getNumber() throws IllegalArgumentException {
+    private int getNumber() throws IllegalArgumentException {
         String line = Console.readLine();
         validateNumberString(line);
         return Integer.parseInt(line);
