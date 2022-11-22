cf2280d08f41bfb2c04c04909a90d93a195b6fba
dvlpr0 <rladjswkd@naver.com>
2022-11-22 17:18:36 +0900

    feat: 최종 게임 결과를 출력하는 기능 구현

--- src/main/java/bridge/OutputMessage.java
+++ src/main/java/bridge/OutputMessage.java
@@ -4,7 +4,9 @@ public enum OutputMessage {
     START_MESSAGE("다리 건너기 게임을 시작합니다.\n"),
     BRIDGE_LENGTH_MESSAGE("다리의 길이를 입력해주세요."),
     CHOICE_REQUEST_MESSAGE("이동할 칸을 선택해주세요. (위: U, 아래: D)"),
-    RESTART_OR_QUIT_MESSAGE("게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)");
+    RESTART_OR_QUIT_MESSAGE("게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)"),
+    RESULT_MESSAGE("최종 게임 결과"),
+    SUMMARY_MESSAGE("게임 성공 여부: %s\n총 시도한 횟수: %d");
     private final String message;
     OutputMessage(String message) {
         this.message = message;
--- src/main/java/bridge/OutputView.java
+++ src/main/java/bridge/OutputView.java
@@ -4,6 +4,8 @@ import static bridge.OutputMessage.START_MESSAGE;
 import static bridge.OutputMessage.BRIDGE_LENGTH_MESSAGE;
 import static bridge.OutputMessage.CHOICE_REQUEST_MESSAGE;
 import static bridge.OutputMessage.RESTART_OR_QUIT_MESSAGE;
+import static bridge.OutputMessage.RESULT_MESSAGE;
+import static bridge.OutputMessage.SUMMARY_MESSAGE;
 
 /**
  * 사용자에게 게임 진행 상황과 결과를 출력하는 역할을 한다.
@@ -24,7 +26,11 @@ public class OutputView {
      * <p>
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void printResult() {
+    public void printResult(String map, String passOrFail, int tryCount) {
+        System.out.println(RESULT_MESSAGE);
+        printMap(map);
+        System.out.println();
+        System.out.printf(SUMMARY_MESSAGE.toString(), passOrFail, tryCount);
     }
 
     public void printStart() {
