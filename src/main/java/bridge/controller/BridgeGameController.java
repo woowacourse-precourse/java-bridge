028a73d272bcb69c0f5d01d517a7ba0588435498
saehim <saeilee.vxv@gmail.com>
2022-11-22 20:20:04 +0900

    Feat (controller) : 게임 컨트롤러 생성

--- src/main/java/bridge/controller/BridgeGameController.java
+++ src/main/java/bridge/controller/BridgeGameController.java
@@ -1,2 +1,25 @@
-package bridge.controller;public class BridgeGameController {
+package bridge.controller;
+
+import bridge.BridgeGame;
+import bridge.BridgeMaker;
+import bridge.BridgeRandomNumberGenerator;
+import bridge.view.InputView;
+import bridge.view.OutputView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static bridge.view.OutputView.printMap;
+
+public class BridgeGameController {
+
+    private static boolean failFlag;
+    private final InputView inputView;
+    private final OutputView outputView;
+
+    public BridgeGameController() {
+        this.inputView = new InputView();
+        this.outputView = new OutputView();
+        this.failFlag = true;
+    }
 }
