953197606148b3ede5befb9d6cab593472a51a46
gynhyunkim <gyeonghyun.kimm@gmail.com>
2022-11-22 14:06:45 +0900

    fix: 다리 길이 범위를 하드코딩하지 않도록 수정

--- src/main/java/bridge/Validation.java
+++ src/main/java/bridge/Validation.java
@@ -2,8 +2,11 @@ package bridge;
 
 public class Validation {
 
+    private static final int BRIDGE_MIN_SIZE = 3;
+    private static final int BRIDGE_MAX_SIZE = 20;
+
     public static void validateBridgeSize(int size) {
-        if (size < 3 || size > 20) {
+        if (size < BRIDGE_MIN_SIZE || size > BRIDGE_MAX_SIZE) {
             throw new IllegalArgumentException(Error.BRIDGE_OUT_OF_RANGE.getMessage());
         }
     }
