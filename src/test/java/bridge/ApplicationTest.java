1efef2e09f912af914c0f06c109ec5dc96d219f7
Dabeen Jeong <hello70825@gmail.com>
2022-11-22 22:42:22 +0900

    test: 시도횟수가 여러 번 존재하는 테스트 추가

--- src/test/java/bridge/ApplicationTest.java
+++ src/test/java/bridge/ApplicationTest.java
@@ -40,6 +40,35 @@ class ApplicationTest extends NsTest {
     }
 
     @Test
+    void 기능_테스트_성공_시도횟수_세번() {
+        assertRandomNumberInRangeTest(() -> {
+            run("3", "U", "D", "D", "R", "U", "U", "R", "U", "D", "U");
+            assertThat(output()).contains(
+                    "[ O |   |   ]",
+                    "[   | O | X ]",
+                    "[ O | X ]",
+                    "[   |   ]",
+                    "최종 게임 결과",
+                    "[ O |   | O ]",
+                    "[   | O |   ]",
+                    "게임 성공 여부: 성공",
+                    "총 시도한 횟수: 3"
+            );
+
+            int upSideIndexFirst = output().indexOf("[ O |   |   ]");
+            int downSideIndexFirst = output().indexOf("[   | O | X ]");
+            int upSideIndexSecond = output().indexOf("[ O | X ]");
+            int downSideIndexSecond = output().indexOf("[   |   ]");
+            int upSideIndexThird = output().indexOf("[ O |   | O ]");
+            int downSideIndexThird = output().indexOf("[   | O |   ]");
+
+            assertThat(upSideIndexFirst).isLessThan(downSideIndexFirst);
+            assertThat(upSideIndexSecond).isLessThan(downSideIndexSecond);
+            assertThat(upSideIndexThird).isLessThan(downSideIndexThird);
+        }, 1, 0, 1);
+    }
+
+    @Test
     void 예외_테스트() {
         assertSimpleTest(() -> {
             runException("a");
