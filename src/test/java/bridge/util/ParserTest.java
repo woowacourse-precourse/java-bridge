7fdc0940b0ebecb667bf294c61bd94824be54f6d
jin8073 <jin8073@gmail.com>
2022-11-22 20:58:50 +0900

    test(ParserTest): Parser 클래스 예외 테스트 수정 및 추가

--- src/test/java/bridge/util/ParserTest.java
+++ src/test/java/bridge/util/ParserTest.java
@@ -4,34 +4,47 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 public class ParserTest {
 
-    @Test
-    void 다리의_길이_올바른_입력값_처리() {
-        assertThat(new Parser().parseBridgeSize("3")).isEqualTo(3);
+    @ValueSource(strings = {"3", "4", "5", "10", "20"})
+    @ParameterizedTest
+    void 다리의_길이_올바른_입력값_처리(String input) {
+        assertThat(new Parser().parseBridgeSize(input)).isInstanceOf(Integer.class);
     }
 
-    @Test
-    void 다리의_길이_숫자가_아닌_입력값_예외_처리() {
+    @ValueSource(strings = {"java", "2", "0", "30", "21"})
+    @ParameterizedTest
+    void 다리의_입력값_예외_처리(String input) {
         assertThatThrownBy(() ->
-                new Parser().parseBridgeSize("java")).isInstanceOf(IllegalArgumentException.class);
+                new Parser().parseBridgeSize(input)).isInstanceOf(IllegalArgumentException.class);
     }
 
-    @Test
-    void 다리의_길이_범위_밖_숫자_입력값_예외_처리() {
+    @ValueSource(strings = {"", "3", "d", "u"})
+    @ParameterizedTest
+    void 칸_이동_예외_처리(String input) {
         assertThatThrownBy(() ->
-                new Parser().parseBridgeSize("21")).isInstanceOf(IllegalArgumentException.class);
+                new Parser().parseMoving(input)).isInstanceOf(IllegalArgumentException.class);
     }
 
-    @Test
-    void 칸_이동_예외_처리() {
+    @ValueSource(strings = {"U", "D"})
+    @ParameterizedTest
+    void 칸_이동_올바른_입력(String input) {
+        assertThat(new Parser().parseMoving(input)).matches("[U|D]");
+    }
+
+    @ValueSource(strings = {"", "3", "r", "q"})
+    @ParameterizedTest
+    void 재시작_또는_종료_예외_처리(String input) {
         assertThatThrownBy(() ->
-                new Parser().parseMoving("d")).isInstanceOf(IllegalArgumentException.class);
+                new Parser().parseGameCommand(input)).isInstanceOf(IllegalArgumentException.class);
     }
 
-    @Test
-    void 칸_이동_올바른_입력() {
-        assertThat(new Parser().parseMoving("U")).isEqualTo("U");
+    @ValueSource(strings = {"R", "Q"})
+    @ParameterizedTest
+    void 재시작_또는_종료_올바른_입력(String input) {
+        assertThat(new Parser().parseGameCommand(input)).matches("[R|Q]");
     }
 }
