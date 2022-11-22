c801e7d543443e357415d422abd8bd865f8f6d31
thousandcows <milcamboo@gmail.com>
2022-11-22 10:20:57 +0900

    feat: create enum class for error messages

--- src/main/java/validation/ErrorMessage.java
+++ src/main/java/validation/ErrorMessage.java
@@ -1,4 +1,12 @@
 package validation;
 
 public enum ErrorMessage {
+
+    INVALID_BRIDGE_SIZE("[ERROR] 유효한 길이를 입력해주세요"),
+    INVALID_MOVING_ORDER("[ERROR] U(위 칸) 또는 D(아래 칸)을 입력해주세요"),
+    INVALID_GAME_COMMAND("[ERROR] R(재시작) 또는 Q(종료)을 입력해주세요");
+
+    ErrorMessage(String s) {
+
+    }
 }
