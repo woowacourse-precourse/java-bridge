974006ab315cb2444f00cbe77836083ed8bf512b
KJE <12181501@inha.edu>
2022-11-22 18:25:39 +0900

    test: 기능테스트2 수정, 기능테스트3(실패 후 재시작) 추가

--- src/test/java/bridge/ApplicationTest.java
+++ src/test/java/bridge/ApplicationTest.java
@@ -41,7 +41,7 @@ class ApplicationTest extends NsTest {
     @Test
     void 기능_테스트2() {
         assertRandomNumberInRangeTest(() -> {
-            run("3", "U", "U", "R", "U", "D", "D");
+            run("3", "U", "U", "R", "U", "D", "D", "Q");
             assertThat(output()).contains(
                     "최종 게임 결과",
                     "[ O |   |   ]",
@@ -50,8 +50,26 @@ class ApplicationTest extends NsTest {
                     "총 시도한 횟수: 2"
             );
 
-            int upSideIndex = output().indexOf("[ O |   | O ]");
-            int downSideIndex = output().indexOf("[   | O |   ]");
+            int upSideIndex = output().indexOf("[ O |   |   ]");
+            int downSideIndex = output().indexOf("[   | O | X ]");
+            assertThat(upSideIndex).isLessThan(downSideIndex);
+        }, 1, 0, 1);
+    }
+
+    @Test
+    void 기능_테스트3() {
+        assertRandomNumberInRangeTest(() -> {
+            run("3", "U", "U", "Q");
+            assertThat(output()).contains(
+                    "최종 게임 결과",
+                    "[ O | X ]",
+                    "[   |   ]",
+                    "게임 성공 여부: 실패",
+                    "총 시도한 횟수: 1"
+            );
+
+            int upSideIndex = output().indexOf("[ O | X ]");
+            int downSideIndex = output().indexOf("[   |   ]");
             assertThat(upSideIndex).isLessThan(downSideIndex);
         }, 1, 0, 1);
     }
