57a22c378a270bf93e6adb4415edd0fad741b26a
JuSeong1130 <jsmoon1130@naver.com>
2022-11-22 11:29:52 +0900

    test(BridgeGame): 게임 이동 기능 테스트

--- src/test/java/bridge/domain/game/BridgeGameTest.java
+++ src/test/java/bridge/domain/game/BridgeGameTest.java
@@ -4,11 +4,16 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import bridge.domain.maker.BridgeMaker;
 import bridge.domain.maker.BridgeRandomNumberGenerator;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Stream;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.MethodSource;
 
 class BridgeGameTest {
 
@@ -18,5 +23,21 @@ class BridgeGameTest {
 	void 입력값size에맞는_다리길이가_나와야합니다(int size,int expected) {
 		BridgeGame bridgeGame = new BridgeGame();
 		Assertions.assertThat(bridgeGame.makeBridge(size).size()).isEqualTo(expected);
+	}
+
+	@DisplayName("이동 테스트")
+	@ParameterizedTest
+	@MethodSource("generateData")
+	void 다리이동_테스트(List<String> bridges,List<String> move,int index,boolean expected){
+		BridgeGame bridgeGame = new BridgeGame();
+		Assertions.assertThat(bridgeGame.move(bridges,move,index)).isEqualTo(expected);
+	}
+	static Stream<Arguments> generateData() {
+		return Stream.of(
+				Arguments.of(Arrays.asList("U", "D", "U"), Arrays.asList("U"), 0,true),
+				Arguments.of(Arrays.asList("U", "D", "U"), Arrays.asList("U", "D"), 1,true),
+				Arguments.of(Arrays.asList("U", "D", "U"), Arrays.asList("D"), 0,false),
+				Arguments.of(Arrays.asList("U", "D", "U"), Arrays.asList("U", "U"), 1,false)
+		);
 	}
 }
