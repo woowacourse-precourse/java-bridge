0c50b1584c6aa9bf9ff38a466c76bcdb28b8a836
Seung-Been Kim <mainlib990@gmail.com mainlib990s-MacBookPro.local>
2022-11-22 18:33:54 +0900

    test(path): add the test case

---
+++ src/test/java/bridge/path/PathTest.java
@@ -0,0 +1,54 @@
+package bridge.path;
+
+import bridge.command.Movement;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import java.util.Arrays;
+import java.util.EnumMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class PathTest {
+
+    private static final EnumMap<Result, String> format = new EnumMap<>(
+            Map.ofEntries(
+                    Map.entry(Result.PREFIX, "[ "),
+                    Map.entry(Result.SUFFIX, " ]"),
+                    Map.entry(Result.DELIMITER, " | "),
+                    Map.entry(Result.O, "O"),
+                    Map.entry(Result.X, "X"),
+                    Map.entry(Result.UNSELECTED, " ")
+            )
+    );
+
+    @DisplayName("format")
+    @ParameterizedTest(name = "[{index}] {arguments}")
+    @CsvSource({
+            "'UP, DOWN, DOWN, DOWN', 'UP, DOWN, DOWN, DOWN', '[ O |   |   |   ]\n[   | O | O | O ]'",
+            "'UP, DOWN, DOWN, UP', 'UP, DOWN, DOWN, UP', '[ O |   |   | O ]\n[   | O | O |   ]'",
+            "'UP, DOWN, DOWN, UP', 'UP, DOWN, DOWN, DOWN', '[ O |   |   |   ]\n[   | O | O | X ]'"
+    })
+    void format(
+            final String rawlyActualMovements,
+            final String rawlyExpectedMovements,
+            final String expectedFormat
+    ) {
+        final List<Movement> actualMovements = parseMovements(rawlyActualMovements);
+        final List<Movement> expectedMovements = parseMovements(rawlyExpectedMovements);
+        final Path path = Path.of(actualMovements, expectedMovements);
+
+        final String actualFormat = path.format(format);
+        assertThat(actualFormat).isEqualTo(expectedFormat);
+    }
+
+    private List<Movement> parseMovements(String rawMovements) {
+        return Arrays.stream(rawMovements.split(", "))
+                .map(Movement::valueOf)
+                .collect(Collectors.toList());
+    }
+}
