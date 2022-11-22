b7912d88c503d1fd5ca1849701fbaa226ed3fc48
taejeong <sontaewjd@nate.com>
2022-11-22 15:34:17 +0900

    feat: 사용자 입력 받기

--- src/main/java/bridge/InputView.java
+++ src/main/java/bridge/InputView.java
@@ -1,5 +1,7 @@
 package bridge;
 
+import camp.nextstep.edu.missionutils.Console;
+
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
  */
@@ -8,21 +10,36 @@ public class InputView {
     /**
      * 다리의 길이를 입력받는다.
      */
-    public int readBridgeSize() {
-        return 0;
+    public int readBridgeSize() throws NumberFormatException{
+        System.out.println(ConstString.INPUT_SIZE);
+        String input = Console.readLine();
+        int ret;
+        try{
+            ret = Integer.parseInt(input);
+        }
+        catch (NumberFormatException e) {
+            throw new NumberFormatException(ConstString.INPUT_SIZE_ERROR);
+        }
+        return ret;
     }
 
     /**
      * 사용자가 이동할 칸을 입력받는다.
      */
-    public String readMoving() {
-        return null;
+    public String readMoving(){
+        System.out.println(ConstString.CHOOSE_STEP);
+        String input = Console.readLine();
+
+        return input;
     }
 
     /**
      * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
      */
     public String readGameCommand() {
-        return null;
+        System.out.println(ConstString.RETRY_OR_QUIT);
+        String input = Console.readLine();
+
+        return input;
     }
 }
