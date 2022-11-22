5374118adf765fb7f3b197bc1d43ae6d1d0b78af
JunDonghyuk <ehdgur4814@naver.com>
2022-11-22 13:56:19 +0900

    refactor(BridgeGame): 게임 다리 final 추가

--- src/main/java/bridge/BridgeGame.java
+++ src/main/java/bridge/BridgeGame.java
@@ -6,7 +6,7 @@ import java.util.List;
  * 다리 건너기 게임을 관리하는 클래스
  */
 public class BridgeGame {
-    private List<String> bridge;
+    private final List<String> bridge;
     private int location;
     private boolean moveSuccess;
 
