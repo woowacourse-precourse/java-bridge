7537b950b33b79140d87fa1584293cdbd1f19ec2
gmu1026 <gmu1026@gmail.com>
2022-11-22 18:05:08 +0900

    fix: 게임 시작 기능 수정

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -3,9 +3,10 @@ package bridge;
 public class Application {
 
     public static void main(String[] args) {
-        BridgeGame bridgeGame = new BridgeGame(new InputView(), new OutputView());
+
         try {
-            bridgeGame.play();
+            BridgeGameController bridgeGameController = new BridgeGameController();
+            bridgeGameController.play();
         } catch (IllegalArgumentException exception) {
             System.out.println(exception.getMessage());
         }
