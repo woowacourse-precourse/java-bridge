186fdca57c112147b99e01039472e328bf4153c6
UJ15 <dbwogml15@naver.com>
2022-11-22 16:27:50 +0900

    refactor : add constructor validate

--- src/main/java/bridge/model/PassOrFail.java
+++ src/main/java/bridge/model/PassOrFail.java
@@ -1,11 +1,18 @@
 package bridge.model;
 
+import bridge.error.Error;
+import java.util.Objects;
+
 public class PassOrFail {
 
     private final boolean value;
     private final UpOrDown upOrDown;
 
     public PassOrFail(boolean value, UpOrDown upOrDown) {
+        if (Objects.isNull(upOrDown)) {
+            throw new IllegalArgumentException(Error.SYSTEM_ERROR.getMessage());
+        }
+
         this.value = value;
         this.upOrDown = upOrDown;
     }
