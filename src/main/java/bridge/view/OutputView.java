52c16bfe0cfc25c0eac641fb12dad84fe1abb03c
yujeongkwon <yoojeongkwon@naver.com>
2022-11-22 21:24:18 +0900

    feat(OutputView) : 결과 출력

--- src/main/java/bridge/view/OutputView.java
+++ src/main/java/bridge/view/OutputView.java
@@ -26,7 +26,18 @@ public class OutputView {
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
     public void printResult(boolean isSuccess, User user) {
+        System.out.println("최종 게임 결과");
+        System.out.println(printBridge(user.getMapUpperBridge()));
+        System.out.println(printBridge(user.getMapLowerBridge()));
+        System.out.println();
 
+        System.out.print("게임 성공 여부: ");
+        if(isSuccess)
+            System.out.println("성공");
+        if(!isSuccess)
+            System.out.println("실패");
+
+        System.out.println("총 시도한 횟수: " +user.getTryCount());
     }
 
     private String printBridge(List<String> bridge){
