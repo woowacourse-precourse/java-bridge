5fb8e9b9987f27e91d5c8a6f19eb24de1e0bd7f5
kiarakim <kiarakim413@gmail.com>
2022-11-22 17:37:44 +0900

    test(UserTest) : add Constructor & Getter test

--- src/test/java/bridge/domain/UserTest.java
+++ src/test/java/bridge/domain/UserTest.java
@@ -1,5 +1,21 @@
 package bridge.domain;
 
-class UserTest {
+import static org.assertj.core.api.Assertions.assertThat;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class UserTest {
+    @Test
+    @DisplayName("생성자 테스트")
+    public void userConstructorAndGetterTest() {
+        // given
+
+        // when
+        User user = new User();
+
+        // then
+        assertThat(user.isGameDone()).isEqualTo(false);
+        assertThat(user.getTrial()).isEqualTo(1);
+    }
 }
