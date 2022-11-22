69226c2c9161fa9a72f399caa35f3a6adc373a9d
nakalic <nakalic28@gmail.com>
2022-11-22 21:30:29 +0900

    test(SpaceToMoveValidatorTest): 올바른 값이라면 예외를 발생시키지 않는지 검증하는 코드 추가

--- src/test/java/bridge/validation/SpaceToMoveValidatorTest.java
+++ src/test/java/bridge/validation/SpaceToMoveValidatorTest.java
@@ -1,11 +1,10 @@
-package bridge.validator;
+package bridge.validation;
 
 import static org.assertj.core.api.Assertions.assertThatCode;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import bridge.constants.ValidatorMessage;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
@@ -14,16 +13,17 @@ class SpaceToMoveValidatorTest {
     @ParameterizedTest
     @ValueSource(strings = {"A", " ", "u"})
     void createNotUorD(String input) {
-        assertThatThrownBy(() ->SpaceToMoveValidator.validateSpaceToMove(input))
+        assertThatThrownBy(() -> SpaceToMoveValidator.validateSpaceToMove(input))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage(ValidatorMessage.NEXT_SPACE_IS_U_OR_D.toString());
 
     }
 
-    @DisplayName("2,100,000,000번 이하의 시도라면 예외가 발생하지 않는다.")
-    @Test
-    void asd() {
-        assertThatCode(() -> TryCountValidator.validateTryCount(2000))
+    @DisplayName("입력이 U나 D라면 예외를 발생시키지 않는다.")
+    @ParameterizedTest()
+    @ValueSource(strings = {"U", "D"})
+    void createNormalInput(String input) {
+        assertThatCode(() -> SpaceToMoveValidator.validateSpaceToMove(input))
                 .doesNotThrowAnyException();
     }
 }
