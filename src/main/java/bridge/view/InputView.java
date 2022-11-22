fc77bb82f81dcb582c4196224cd1dc678f254267
juno-junho <ssmm0205@naver.com>
2022-11-22 19:18:33 +0900

    feat: create method readGameCommand
    게임 다시 시도할지 종료할지 여부 입력받는 readGameCommand 메서드 생성

--- src/main/java/bridge/view/InputView.java
+++ src/main/java/bridge/view/InputView.java
@@ -25,6 +25,7 @@ public class InputView {
      * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
      */
     public String readGameCommand() {
-        return null;
+        System.out.println(InputMessage.INPUT_RETRY_OR_QUIT_MESSAGE.getMessage());
+        return Console.readLine();
     }
 }
