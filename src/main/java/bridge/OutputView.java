3d3177327b6d884a31bb273fc1d3123cd7dce47a
JooDukHan <bfinecpa1@naver.com>
2022-11-22 19:08:30 +0900

    refactor: OutputView.printResult 파라미터 변경
    OutputView.printResult 파라미터 Player 하나로 변경

--- src/main/java/bridge/OutputView.java
+++ src/main/java/bridge/OutputView.java
@@ -21,15 +21,15 @@ public class OutputView {
      * <p>
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void printResult(PlayerBridge playerBridge, int useCoin) {
+    public void printResult(Player player) {
         System.out.println("최종 게임 결과");
-        printMap(playerBridge);
-        if (playerBridge.successRoot()){
+        printMap(player.getPlayerBridge());
+        if (player.success()){
             System.out.println("게임 성공 여부: 성공");
-        }if (!playerBridge.successRoot()){
+        }if (player.fail()){
             System.out.println("게임 성공 여부: 실패");
         }
-        System.out.printf("총 시도한 횟수: %d", useCoin);
+        System.out.printf("총 시도한 횟수: %d", player.getCoinUsed());
     }
 
     public void printGameStart(){
