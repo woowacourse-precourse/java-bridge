393a0e12c3c1537e8d74bf27820bdb78b8e309bd
yumble <hnjn1101@gmail.com>
2022-11-22 16:54:03 +0900

    refactor : create moveresult enum class
    기존에 BridgeGame 에서 사용자 입력이 무엇인지,
    사용자 입력과 다리가 일치하여 이동할 수 있는지에 따라
    네가지의 케이스로 나누어 upBridge, downBridge 에 넣어주는 값이 달랐다.
    그러나 함수의 내용 자체는 비슷하여 refactor 진행하였다.
    
    -> 밑으로 갔을때 성공, 실패, 위로 갔을때 성공 실패 이렇게
    네가지의 케이스를 나누어 enum을 설정하고 그에따른 함수 실행도
    enum class에서 진행하는 것으로 바꿀 예정이다.

---
+++ src/main/java/bridge/model/MoveResult.java
@@ -0,0 +1,57 @@
+package bridge.model;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.function.BiConsumer;
+
+public enum MoveResult {
+
+    LOWER_DIRECTION_SUCCESS("D", true,(upList,downList)-> makeSuccessBridge(downList,upList) ),
+    LOWER_DIRECTION_FAIL("D", false, (upList,downList)-> makeFailBridge(downList,upList)),
+    UPPER_DIRECTION_SUCCESS("U",true, MoveResult::makeSuccessBridge),
+    UPPER_DIRECTION_FAIL("U", false, MoveResult::makeFailBridge),
+    EMPTY(null, false, null);
+
+
+    private final String moving;
+    private final boolean success;
+    private final BiConsumer<List<String>,List<String>> expression;
+    private static List<String> upBridge = new ArrayList<>();
+    private static List<String> downBridge = new ArrayList<>();
+
+    MoveResult(String moving, boolean success, BiConsumer<List<String>,List<String>> expression) {
+        this.moving = moving;
+        this.success = success;
+        this.expression = expression;
+    }
+    public static void apply(String moving, boolean success) {
+        Arrays.stream(MoveResult.values())
+                .filter(directionTest -> directionTest.moving.equals(moving))
+                .filter(directionTest -> directionTest.success == success)
+                .findAny().get()
+                .expression.accept(upBridge,downBridge);
+    }
+    public static void makeSuccessBridge(List<String> successBridge, List<String> spaceBridge){
+        successBridge.add("O");
+        spaceBridge.add(" ");
+    }
+    public static void makeFailBridge(List<String> failBridge, List<String> spaceBridge){
+        failBridge.add("X");
+        spaceBridge.add(" ");
+
+    }
+
+    public static List<String> getUpBridge() {
+        return Collections.unmodifiableList(upBridge);
+    }
+
+    public static List<String> getDownBridge() {
+        return Collections.unmodifiableList(downBridge);
+    }
+    public static void retrySetting(){
+        upBridge.clear();
+        downBridge.clear();
+    }
+}
