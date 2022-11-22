f07fcec4992e362fc2f43fff119dba93cf9fe8fd
Donguk <95donguk.dev@gmail.com>
2022-11-22 16:10:35 +0900

    refactor(BridgeGameTest): 테스트 클래스 merge 및 코드 리팩토링

--- src/test/java/bridge/domain/BridgeGameTest.java
+++ src/test/java/bridge/domain/BridgeGameTest.java
@@ -14,20 +14,22 @@ class BridgeGameTest {
     @BeforeEach
     void setUp() {
         bridgeGame = new BridgeGame();
+        bridgeGame.move("U", "U");
+        bridgeGame.move("F", "F");
+        bridgeGame.move("U", "U");
     }
 
     @DisplayName("이동 성공 테스트")
-    @ParameterizedTest
-    @CsvSource({"U, U", "F, F"})
-    void moveSuccess(String moving, String block) {
-        assertThat(bridgeGame.move(moving, block)).isTrue();
+    @Test
+    void moveSuccess() {
+        assertThat(bridgeGame.isMovingFail()).isFalse();
     }
 
     @DisplayName("이동 실패 테스트")
-    @ParameterizedTest
-    @CsvSource({"U, F", "F, U"})
-    void moveFail(String moving, String block) {
-        assertThat(bridgeGame.move(moving, block)).isFalse();
+    @Test
+    void moveFail() {
+        bridgeGame.move("F", "U");
+        assertThat(bridgeGame.isMovingFail()).isTrue();
     }
 
     @DisplayName("재시도 시 총 시도한 횟수가 증가하는 지 확인 테스트")
@@ -36,5 +38,26 @@ class BridgeGameTest {
         assertThat(bridgeGame.getTotalAttempts()).isEqualTo(1);
         bridgeGame.retry();
         assertThat(bridgeGame.getTotalAttempts()).isEqualTo(2);
+    }
+
+    @DisplayName("이동 기록 찾기 테스트")
+    @ParameterizedTest
+    @CsvSource({"0, U", "1, F", "2, U"})
+    void findMovingByIndexTest(int index, String moving) {
+        assertThat(bridgeGame.findMovingByIndex(index)).isEqualTo(moving);
+    }
+
+    @DisplayName("이동 결과 찾기 테스트")
+    @ParameterizedTest
+    @CsvSource({"0, true", "1, true", "2, true", "3, false"})
+    void findMovingResultByIndexTest(int index, boolean result) {
+        bridgeGame.move("F", "U");
+        assertThat(bridgeGame.findMovingResultByIndex(index)).isEqualTo(result);
+    }
+
+    @DisplayName("이동 횟수 가져오기 테스트")
+    @Test
+    void getMovingCountTest() {
+        assertThat(bridgeGame.getMovingCount()).isEqualTo(3);
     }
 }
