15a4dd76410e9cb458d6a4d9152467f1a8d993ed
nakalic <nakalic28@gmail.com>
2022-11-22 20:20:57 +0900

    test(BridgeGameTest): 기능 리팩토링에 따른 테스트 코드 변경

--- src/test/java/bridge/service/BridgeGameTest.java
+++ src/test/java/bridge/service/BridgeGameTest.java
@@ -27,16 +27,16 @@ class BridgeGameTest {
     @Test
     public void failToCrossTheBridge() {
         assertThat(bridgeGame.move(gameStatus, bridge, "U")).isTrue();
-        assertThat(gameStatus.topBridge.get(0)).isEqualTo("O");
-        assertThat(gameStatus.bottomBridge.get(0)).isEqualTo(" ");
+        assertThat(gameStatus.topBridge.getDataForOutput(0)).isEqualTo("O");
+        assertThat(gameStatus.bottomBridge.getDataForOutput(0)).isEqualTo(" ");
     }
 
     @DisplayName("다리 건너기에 실패했다면 게임 상태에 X와 공백 한 칸을 추가하고 false를 반환한다.")
     @Test
     public void SucceedToCrossTheBridge() {
         assertThat(bridgeGame.move(gameStatus, bridge, "D")).isFalse();
-        assertThat(gameStatus.topBridge.get(0)).isEqualTo(" ");
-        assertThat(gameStatus.bottomBridge.get(0)).isEqualTo("X");
+        assertThat(gameStatus.topBridge.getDataForOutput(0)).isEqualTo(" ");
+        assertThat(gameStatus.bottomBridge.getDataForOutput(0)).isEqualTo("X");
     }
 
     @DisplayName("재시도 하면 게임 상태는 초기화 되고, tryCount는 1 증가한다.")
@@ -45,6 +45,6 @@ class BridgeGameTest {
         assertThat(gameStatus.getTryCount()).isEqualTo(1);
         gameStatus.resetGameStatus();
         assertThat(gameStatus.getTryCount()).isEqualTo(2);
-        assertThat(gameStatus.topBridge.size()).isEqualTo(0);
+        assertThat(gameStatus.getTopBridge().getSize()).isEqualTo(0);
     }
 }
