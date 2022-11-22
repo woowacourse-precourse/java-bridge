21fcea6311e6449a8e89870521c624407fca4b13
Eun-chan Cho <eunchan1001@gmail.com>
2022-11-22 16:23:38 +0900

    feat(OutputView): implement printResult method

--- src/main/java/bridge/OutputView.java
+++ src/main/java/bridge/OutputView.java
@@ -6,7 +6,9 @@ import java.util.List;
  * 사용자에게 게임 진행 상황과 결과를 출력하는 역할을 한다.
  */
 public class OutputView {
-
+    private final String lastGameResultMessage = "최종 게임 결과";
+    private final String resultTypeMessage = "게임 성공 여부: ";
+    private final String totalNumberOfTryMessage = "총 시도한 횟수: ";
 
     /**
      * 현재까지 이동한 다리의 상태를 정해진 형식에 맞춰 출력한다.
@@ -26,5 +28,10 @@ public class OutputView {
      * <p>
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void printResult() {}
+    public void printResult(Result result) {
+        System.out.println(lastGameResultMessage);
+        printMap(result);
+        System.out.println(resultTypeMessage + result.getLastResultType());
+        System.out.println(totalNumberOfTryMessage + result.getCount());
+    }
 }
