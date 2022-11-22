64481a4f63999944564400d0ca8eedd1e5bd6f76
TeddyYeoni <po99027@naver.com>
2022-11-22 20:19:24 +0900

    feat: 각 메서드에서 값을 입력받는 기능 구현

--- src/main/java/bridge/view/InputView.java
+++ src/main/java/bridge/view/InputView.java
@@ -1,5 +1,7 @@
 package bridge.view;
 
+import camp.nextstep.edu.missionutils.Console;
+
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
  */
@@ -14,21 +16,28 @@ public class InputView {
 	 * 다리의 길이를 입력받는다.
 	 */
 	public int readBridgeSize() {
-		return 0;
+		String readBridgeSize = Console.readLine();
+		validateBridgeSize(readBridgeSize);
+		int bridgeSize = Integer.parseInt(readBridgeSize);
+		return bridgeSize;
 	}
 
 	/**
 	 * 사용자가 이동할 칸을 입력받는다.
 	 */
 	public String readMoving() {
-		return null;
+		String moving = Console.readLine();
+		validateMoving(moving);
+		return moving;
 	}
 
 	/**
 	 * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
 	 */
 	public String readGameCommand() {
-		return null;
+		String gameCommand = Console.readLine();
+		validateGameCommand(gameCommand);
+		return gameCommand;
 	}
 
 	private void validateBridgeSize(String input) {
