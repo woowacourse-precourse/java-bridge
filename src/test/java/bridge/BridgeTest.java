e2ae230a32585ab26882570fb5a92e558843d988
youswim <youswim96@gmail.com>
2022-11-22 16:06:08 +0900

    refactor(BridgeTest.java): 테스트 코드 리팩터링
    ParameterizedTest 적용해서 중복 코드 제거

--- src/test/java/bridge/BridgeTest.java
+++ src/test/java/bridge/BridgeTest.java
@@ -4,6 +4,8 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import java.util.List;
 
@@ -21,47 +23,32 @@ class BridgeTest {
     @DisplayName("compare한 후에 원소가 추가된 results 배열이 기댓값과 같은지 확인")
     @Test
     void compareWithBridgeTest() {
-        bridge.compareWithBridge("U");
-        bridge.compareWithBridge("U");
-        bridge.compareWithBridge("D");
-        bridge.compareWithBridge("U");
+        setResults();
         bridge.compareWithBridge("U");
         Assertions.assertThat(bridge.getResults()).containsExactly(true, true, true, true, false);
     }
 
     @DisplayName("results배열에 false가 있는지를 반환하는 메서드 테스트")
-    @Test
-    void isFalseInResultTest1() {
-        setResults1();
-        Assertions.assertThat(bridge.isFalseInResults()).isEqualTo(true);
+    @ParameterizedTest
+    @CsvSource(value = {"U:true", "D:false"}, delimiter = ':')
+    void isFalseInResultTest(String input, boolean expected) {
+        setResults();
+        bridge.compareWithBridge(input);
+        Assertions.assertThat(bridge.isFalseInResults()).isEqualTo(expected);
     }
 
-    @DisplayName("results배열에 false가 있는지를 반환하는 메서드 테스트")
-    @Test
-    void isFalseInResultTest2() {
-        setResults2();
-        Assertions.assertThat(bridge.isFalseInResults()).isEqualTo(false);
-    }
+    private void setResults() {
+        bridge.compareWithBridge("U");
+        bridge.compareWithBridge("U");
+        bridge.compareWithBridge("D");
+        bridge.compareWithBridge("U");
 
-    private void setResults1() {
-        bridge.compareWithBridge("U");
-        bridge.compareWithBridge("U");
-        bridge.compareWithBridge("D");
-        bridge.compareWithBridge("U");
-        bridge.compareWithBridge("U");
-    }
-    private void setResults2() {
-        bridge.compareWithBridge("U");
-        bridge.compareWithBridge("U");
-        bridge.compareWithBridge("D");
-        bridge.compareWithBridge("U");
-        bridge.compareWithBridge("D");
     }
 
     @DisplayName("restart시, results배열이 잘 비워지는지 확인")
     @Test
     void restartBridge() {
-        setResults1();
+//        setResults1();
         Assertions.assertThat(bridge.getResults().size()).isEqualTo(5);
         bridge.restartBridge();
         Assertions.assertThat(bridge.getResults().size()).isEqualTo(0);
