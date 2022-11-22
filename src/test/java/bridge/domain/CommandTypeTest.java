f6671f17dab4110bdca6d400e47083be6d86f275
kimJaeHyeon <kmr2644@gmail.com>
2022-11-22 18:09:29 +0900

    test: 생성 테스트 작성

--- src/test/java/bridge/domain/CommandTypeTest.java
+++ src/test/java/bridge/domain/CommandTypeTest.java
@@ -1,5 +1,6 @@
 package bridge.domain;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import org.junit.jupiter.api.DisplayName;
@@ -15,4 +16,18 @@ class CommandTypeTest {
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("[ERROR] Q 또는 R 를 입력해야 합니다.");
     }
+    @Test
+    @DisplayName("Q,R 을 인자로받으면 해당하는 CommandType 이 생성 되어야 한다.")
+    public void 생성테스트() {
+        //given
+        String quitInput = "Q";
+        String restartInput = "R";
+        //when
+        CommandType quit = CommandType.create(quitInput);
+        CommandType restart = CommandType.create(restartInput);
+        //then
+        assertThat(quit).isEqualTo(CommandType.QUIT);
+        assertThat(restart).isEqualTo(CommandType.RESTART);
+    }
+
 }
