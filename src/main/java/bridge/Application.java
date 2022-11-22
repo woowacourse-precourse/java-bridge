c616ef2eee39fa8bdef3c53e53b59744b24c3253
zeunala <zeunala@naver.com>
2022-11-22 13:25:47 +0900

    refactor: BridgeController에 역할 위임 및 함수 분리
    게임 전체 실행 로직을 BridgeController 클래스를 통해서 진행,
    함수가 길어지지 않도록 기능별로 분리

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -1,28 +1,12 @@
 package bridge;
 
-import bridge.view.InputView;
-import bridge.view.OutputView;
+import bridge.controller.BridgeController;
 
 public class Application {
 
     public static void main(String[] args) {
-        BridgeMaker bridgeMaker = new BridgeMaker(new BridgeRandomNumberGenerator());
-        BridgeGame bridgeGame = new BridgeGame(bridgeMaker.makeBridge(InputView.readBridgeSize()));
+        BridgeController bridgeController = new BridgeController();
 
-        while (!bridgeGame.isSuccess()) {
-            bridgeGame.move(InputView.readMoving());
-            OutputView.printMap(bridgeGame);
-            if (bridgeGame.isCorrectChoice()) {
-                continue;
-            }
-            String gameCommand = InputView.readGameCommand();
-            if (gameCommand.equals("R")) {
-                bridgeGame.retry();
-            }
-            if (gameCommand.equals("Q")) {
-                break;
-            }
-        }
-        OutputView.printResult(bridgeGame);
+        bridgeController.playNewBridgeGame();
     }
 }
