183657c5bdaf0174e90660e335a7b8e1e4e66df6
Giho <poqw741@gmail.com>
2022-11-22 16:34:00 +0900

    feat(bridgeController) : 다리 게임 생성

--- src/main/java/bridge/controller/BridgeController.java
+++ src/main/java/bridge/controller/BridgeController.java
@@ -1,4 +1,32 @@
 package bridge.controller;
 
+import bridge.BridgeRandomNumberGenerator;
+import bridge.domain.BridgeGame;
+import bridge.domain.BridgeMaker;
+import bridge.ui.InputView;
+import bridge.ui.OutputView;
+
+import java.util.List;
+
 public class BridgeController {
+    private InputView inputView;
+    private OutputView outputView;
+    private BridgeGame bridgeGame;
+
+    public BridgeController(){
+        this.inputView = new InputView();
+        this.outputView = new OutputView();
+    }
+
+    public void start(){
+        createBridgeGame();
+    }
+
+    private void createBridgeGame(){
+        int inputSize = this.inputView.readBridgeSize();
+        BridgeMaker bridgeMaker = new BridgeMaker(new BridgeRandomNumberGenerator());
+        List<String> bridge = bridgeMaker.makeBridge(inputSize);
+        this.bridgeGame = new BridgeGame(bridge);
+    }
+
 }
