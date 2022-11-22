419cf3bed63d4d31fb2b26d9884836dc94209f39
ycopp9 <aciees95@gmail.com>
2022-11-22 18:33:54 +0900

    feat(OutputView): 게임의 기본메세지와 에러메세지 출력 기능 추가

--- src/main/java/bridge/OutputView.java
+++ src/main/java/bridge/OutputView.java
@@ -1,5 +1,7 @@
 package bridge;
 
+import bridge.constant.Message;
+
 /**
  * 사용자에게 게임 진행 상황과 결과를 출력하는 역할을 한다.
  */
@@ -19,5 +21,14 @@ public class OutputView {
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
     public void printResult() {
+    }
+
+    public void printMessage(Message message) {
+        System.out.println(message.getMessage());
+    }
+
+    public void printErrorMessage(String message) {
+        System.out.print("[ERROR] ");
+        System.out.println(message);
     }
 }
