50532e917894a888ee9b67bb71e0175d5d756be7
moonstal1506 <moonstal1506@gmail.com>
2022-11-22 19:05:07 +0900

    feat: 게임 재시작할 때 시도횟수 + 1, 게임 결과 초기화

--- src/test/java/bridge/model/BridgeGameTest.java
+++ src/test/java/bridge/model/BridgeGameTest.java
@@ -1,23 +1,36 @@
 package bridge.model;
 
-import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
 import java.util.List;
 
-import static org.assertj.core.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
 
 class BridgeGameTest {
+
+    BridgeGame game;
+
+    @BeforeEach
+    void before() {
+        game = new BridgeGame(List.of("U"));
+    }
 
     @DisplayName("사용자의 입력값이 정답일 경우 움직일 수 있다.")
     @ParameterizedTest
     @CsvSource(value = {"U,true", "D,false"})
     void move(String input, boolean expected) {
-        BridgeGame game = new BridgeGame(List.of("U"));
         GameResult gameResult = game.move(0, input);
-
         assertThat(gameResult.getGameResult()).containsExactly(expected);
+    }
+
+    @DisplayName("게임을 재시작할 때 시도 횟수가 증가한다.")
+    @Test
+    void retry() {
+        game.retry();
+        assertThat(game.getTryNumber()).isEqualTo(2);
     }
 }
