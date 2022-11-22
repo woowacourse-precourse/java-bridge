6a1b50624cde3c86dc3e47b27a8a117c0e379ab0
hyeonseog-won <smreo75@gmail.com>
2022-11-22 12:18:16 +0900

    feat: 클래스 추가

---
+++ src/main/java/bridge/Bridge.java
@@ -0,0 +1,11 @@
+package bridge;
+
+import java.util.List;
+
+public class Bridge {
+    private final List<String> bridge;
+
+    public Bridge(List<String> bridge) {
+        this.bridge = bridge;
+    }
+}
