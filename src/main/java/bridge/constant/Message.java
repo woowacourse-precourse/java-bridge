737a6d0b87dcfea00bec1d850678e8549bed316f
xxeol2 <jhj01177@naver.com>
2022-11-22 16:44:26 +0900

    feat: 게임 시작 문구를 출력한다

--- src/main/java/bridge/constant/Message.java
+++ src/main/java/bridge/constant/Message.java
@@ -7,7 +7,7 @@ import static bridge.constant.GameCommand.RETRY;
 
 public class Message {
 
-    public static final String START_GAME = "다리 건너기 게임을 시작합니다.\n";
+    public static final String GAME_START = "다리 건너기 게임을 시작합니다.\n";
     public static final String INPUT_BRIDGE_SIZE = "다리의 길이를 입력해주세요.";
     public static final String INPUT_MOVING =
             "이동할 칸을 선택해주세요. (위: " + UP.getCode() + ", 아래: " + DOWN.getCode() + ")";
--- src/main/java/bridge/view/OutputView.java
+++ src/main/java/bridge/view/OutputView.java
@@ -8,6 +8,7 @@ import static bridge.constant.BridgeSymbol.BRIDGE_START;
 import static bridge.constant.BridgeSymbol.MOVING_EMPTY;
 import static bridge.constant.BridgeSymbol.MOVING_O;
 import static bridge.constant.BridgeSymbol.MOVING_X;
+import static bridge.constant.Message.GAME_START;
 
 import bridge.Moving;
 import java.util.ArrayList;
@@ -63,5 +64,9 @@ public class OutputView {
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
     public void printResult() {
+    }
+
+    public void printGameStartMessage() {
+        System.out.println(GAME_START);
     }
 }
