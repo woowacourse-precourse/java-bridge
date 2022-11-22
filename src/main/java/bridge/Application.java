9bead0236dad17aa40c6a904bea03b10eb03d183
jeongyeonKimm <wjddus9132@gmail.com>
2022-11-22 23:37:47 +0900

    feat: main메소드 구현

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -3,5 +3,7 @@ package bridge;
 public class Application {
 
     public static void main(String[] args) {
+        BridgeController bridgeController = new BridgeController();
+        bridgeController.run();
     }
 }
