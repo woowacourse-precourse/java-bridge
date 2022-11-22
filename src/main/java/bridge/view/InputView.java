9d8e3cd48c6f8cf079e2491a8d3e1ea90d7839f4
DavinAn <blueapple1030@gmail.com>
2022-11-22 21:52:21 +0900

    feat(view) : add readBridgeSize

--- src/main/java/bridge/view/InputView.java
+++ src/main/java/bridge/view/InputView.java
@@ -1,5 +1,7 @@
 package bridge.view;
 
+import camp.nextstep.edu.missionutils.Console;
+
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
  */
@@ -13,7 +15,10 @@ public class InputView {
      * 다리의 길이를 입력받는다.
      */
     public int readBridgeSize() {
-        return 0;
+        System.out.println(START_MESSAGE);
+        System.out.println(BRIDGE_SIZE_MESSAGE);
+        String input = Console.readLine().trim();
+        return Integer.parseInt(input);
     }
 
     /**
