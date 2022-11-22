cc468a9b4003765e3c61a3bb74e21a4c287305fa
rjsah09 <83952174+rjsah09@users.noreply.github.com>
2022-11-22 14:59:57 +0900

    test: BridgeGame 클래스 테스트 추가

--- src/test/java/bridge/BridgeGameTest.java
+++ src/test/java/bridge/BridgeGameTest.java
@@ -1,4 +1,35 @@
 package bridge;
 
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
 public class BridgeGameTest {
+
+    private final BridgeGame bridgeGame = new BridgeGame();
+
+    @Test
+    void 다리건너기_정답_테스트() {
+        List<String> footholds = Arrays.asList("U", "U", "D", "D");
+        List<String> crossed = Arrays.asList("U", "U", "D", "D");
+        Bridge bridge = new Bridge(footholds, crossed);
+
+        Boolean correct = bridgeGame.isCorrect(bridge);
+
+        Assertions.assertThat(correct).isEqualTo(true);
+    }
+
+    @Test
+    void 다리건너기_오답_테스트() {
+        List<String> footholds = Arrays.asList("U", "U", "D", "D");
+        List<String> crossed = Arrays.asList("U", "U", "D", "U");
+        Bridge bridge = new Bridge(footholds, crossed);
+
+        Boolean correct = bridgeGame.isCorrect(bridge);
+
+        Assertions.assertThat(correct).isEqualTo(false);
+    }
+
 }
