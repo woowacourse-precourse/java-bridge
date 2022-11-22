7b73d7e10322f088cc58e634575c56e876bfc7c2
Yujin830 <rladbwls30@naver.com>
2022-11-22 12:59:06 +0900

    Test: Input예외 테스트

--- src/test/java/bridge/ApplicationTest.java
+++ src/test/java/bridge/ApplicationTest.java
@@ -11,8 +11,6 @@ import org.junit.jupiter.api.Test;
 
 class ApplicationTest extends NsTest {
 
-    private static final String ERROR_MESSAGE = "[ERROR]";
-
     @Test
     void 다리_생성_테스트() {
         BridgeNumberGenerator numberGenerator = new TestNumberGenerator(newArrayList(1, 0, 0));
@@ -27,24 +25,16 @@ class ApplicationTest extends NsTest {
             run("3", "U", "D", "U");
             assertThat(output()).contains(
                 "최종 게임 결과",
-                "[ O |   | O ]",
-                "[   | O |   ]",
+                "[O |   | O]",
+                "[  | O |  ]",
                 "게임 성공 여부: 성공",
                 "총 시도한 횟수: 1"
             );
 
-            int upSideIndex = output().indexOf("[ O |   | O ]");
-            int downSideIndex = output().indexOf("[   | O |   ]");
+            int upSideIndex = output().indexOf("[O |   | O]");
+            int downSideIndex = output().indexOf("[  | O |  ]");
             assertThat(upSideIndex).isLessThan(downSideIndex);
         }, 1, 0, 1);
-    }
-
-    @Test
-    void 예외_테스트() {
-        assertSimpleTest(() -> {
-            runException("a");
-            assertThat(output()).contains(ERROR_MESSAGE);
-        });
     }
 
     @Override
