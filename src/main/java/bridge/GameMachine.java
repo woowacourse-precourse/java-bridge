90a1e48c57c100ce62976078b2c7369ee907f05e
ulsandonghun <donghoon141@naver.com>
2022-11-22 15:48:25 +0900

    feat : move() 기능 구현

--- src/main/java/bridge/GameMachine.java
+++ src/main/java/bridge/GameMachine.java
@@ -1,13 +1,30 @@
 package bridge;
 
+import java.util.List;
+
 public class GameMachine {
     private InputView ui = new InputView();
     private OutputView view = new OutputView();
     private BridgeMaker bridgeMaker = new BridgeMaker(new BridgeRandomNumberGenerator());
 
-    void run() {
-        int bridgeSize = ui.readBridgeSize();
-        bridgeMaker.makeBridge(bridgeSize);
+    public void run() {
+        List<String> bridge = bridgeMaker.makeBridge(
+                ui.readBridgeSize()
+        );
+
+        play(bridge);
     }
 
+    private void play(List<String> bridge) {
+        BridgeGame bridgeGame = new BridgeGame(bridge);
+        for (int location = 0; location <= bridge.size(); location++) {
+            String userInput = ui.readMoving();
+            if (MoveResult.FAIL == bridgeGame.move(userInput)) {
+                System.out.println("FAIL");
+                return;
+            }
+            System.out.println("pass");
+        }
+
+    }
 }
