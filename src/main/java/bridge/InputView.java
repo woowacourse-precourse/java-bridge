6d7b9c03e9c126f4a222d4d9487499fcf1fc1f1f
BHC-Chicken <tij05078@gmail.com>
2022-11-22 10:09:47 +0900

    feat : 다리 길이 입력 값 검증
    입력 받은  값이 정수 인지 검증하고 예외를 발생시킨다.

--- src/main/java/bridge/InputView.java
+++ src/main/java/bridge/InputView.java
@@ -1,15 +1,29 @@
 package bridge;
 
+import camp.nextstep.edu.missionutils.Console;
+
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
  */
 public class InputView {
+    Validation validation = new Validation();
 
     /**
      * 다리의 길이를 입력받는다.
      */
     public int readBridgeSize() {
-        return 0;
+        System.out.println("다리의 길이를 입력해주세요.");
+
+        return validation.validationNumber(readInteger());
+    }
+
+
+    public int readInteger() {
+        try {
+            return Integer.parseInt(Console.readLine());
+        } catch (IllegalArgumentException e) {
+            throw new IllegalArgumentException("올바른 숫자를 정수로 입력해주세요.");
+        }
     }
 
     /**
