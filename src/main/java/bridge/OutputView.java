05b8e0c5f61fb42a8740f82edb8509dcf8539687
KSB-skyBlueLover <gogotkdqja@yonsei.ac.kr>
2022-11-22 13:14:10 +0900

    feat: implement the function that prints map

--- src/main/java/bridge/OutputView.java
+++ src/main/java/bridge/OutputView.java
@@ -1,9 +1,14 @@
 package bridge;
 
+import java.util.List;
+
 /**
  * 사용자에게 게임 진행 상황과 결과를 출력하는 역할을 한다.
  */
 public class OutputView {
+
+    private List<String> ups;
+    private List<String> downs;
 
     /**
      * 현재까지 이동한 다리의 상태를 정해진 형식에 맞춰 출력한다.
@@ -11,6 +16,16 @@ public class OutputView {
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
     public void printMap() {
+        printOneMap(ups);
+        printOneMap(downs);
+    }
+
+    public static void printOneMap(List<String> upDown) {
+        System.out.print("[ ");
+        for (int i = 0; i < upDown.size() - 1; i++) {
+            System.out.print(upDown.get(i) + " | ");
+        }
+        System.out.println(upDown.get(upDown.size()-1) + " ]");
     }
 
     /**
