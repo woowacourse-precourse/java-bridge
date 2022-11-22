c9126844a105f1a2c96577d2efb4c8e2d104e58c
thuthi <whquddn55@gmail.com>
2022-11-22 19:23:55 +0900

    refactor(bridge): BridgePosition의 fromString 메서드 리팩토링 Stream으로 복잡하게 동작하던 fromString 메서드를 Map을 통해 간단하게 구할 수 있도록 리팩토링함.

--- src/main/java/bridge/bridge/BridgePosition.java
+++ src/main/java/bridge/bridge/BridgePosition.java
@@ -3,10 +3,18 @@ package bridge.bridge;
 import static bridge.exception.IllegalArgumentExceptionMessage.ILLEGAL_BRIDGE_POSITION;
 
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 public enum BridgePosition {
-    BRIDGE_UP("U"),
-    BRIDGE_DOWN("D");
+    BRIDGE_UP("U"), BRIDGE_DOWN("D");
+
+    private static final Map<String, BridgePosition> mapper = new HashMap<>();
+
+    static {
+        Arrays.stream(BridgePosition.values())
+                .forEach(bridgePosition -> mapper.put(bridgePosition.getPosition(), bridgePosition));
+    }
 
     private final String position;
 
@@ -14,14 +22,14 @@ public enum BridgePosition {
         this.position = position;
     }
 
-    public String getPosition() {
-        return position;
+    public static BridgePosition fromString(String position) throws IllegalArgumentException {
+        if (!mapper.containsKey(position)) {
+            throw new IllegalArgumentException(ILLEGAL_BRIDGE_POSITION.getMessage());
+        }
+        return mapper.get(position);
     }
 
-    public static BridgePosition fromString(String moving) {
-        return Arrays.stream(BridgePosition.values())
-                .filter(bridgePosition -> bridgePosition.getPosition().equals(moving))
-                .findAny()
-                .orElseThrow(() -> new IllegalArgumentException(ILLEGAL_BRIDGE_POSITION.getMessage()));
+    public String getPosition() {
+        return position;
     }
 }
