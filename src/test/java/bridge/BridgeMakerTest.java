79942bfec4297d1e62272a41182354a5b5b3a211
Sangwoo Shin <sangwoonoel@naver.com>
2022-11-22 23:05:24 +0900

    refactor(BridgeMaker): 테스트 코드 설명 추가

--- src/test/java/bridge/BridgeMakerTest.java
+++ src/test/java/bridge/BridgeMakerTest.java
@@ -2,6 +2,7 @@ package bridge;
 
 import bridge.domain.BridgeMaker;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
@@ -16,6 +17,7 @@ public class BridgeMakerTest {
     private BridgeNumberGenerator numberGenerator;
     private BridgeMaker bridgeMaker;
 
+    @DisplayName("올바른 길이의 다리를 생성하는지 테스트")
     @ParameterizedTest
     @CsvSource("3, 3, 10, 10, 20, 20")
     void 다리_길이_테스트(int size, int expected) {
@@ -26,6 +28,7 @@ public class BridgeMakerTest {
         assertThat(actual).isEqualTo(expected);
     }
 
+    @DisplayName("1은 U, 0은 D로 다리를 올바르게 생성하는지 테스트")
     @ParameterizedTest
     @CsvSource("1, 0, 0, 3, U, D, D, 1, 1, 1, 3, U, U, U" )
     void 다리_생성_테스트(int element1, int element2, int element3, int size, String moving1, String moving2, String moving3) {
