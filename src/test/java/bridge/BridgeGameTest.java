cd5a88bbabc02a1753f78f64cede4df0e68bfce2
GraphTree <graphnodesed@gmail.com>
2022-11-22 14:46:02 +0900

    test : test BridgeGame

---
+++ src/test/java/bridge/BridgeGameTest.java
@@ -0,0 +1,20 @@
+package bridge;
+
+import camp.nextstep.edu.missionutils.Console;
+import org.junit.jupiter.api.Test;
+
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BridgeGameTest {
+    public BridgeGame bridgeGame = new BridgeGame();
+    public InputView inputView = new InputView();
+
+    @Test
+    public void moveTest() {
+        Console.readLine();
+
+    }
+}
