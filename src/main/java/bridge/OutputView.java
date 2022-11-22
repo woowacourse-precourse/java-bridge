89e11dc887ac2aaee2ff6188da56b9b005d54abe
No BongJun <sewd1224@gmail.com>
2022-11-22 15:11:35 +0900

    [Refactor] <OutputView> Print Present_map

--- src/main/java/bridge/OutputView.java
+++ src/main/java/bridge/OutputView.java
@@ -11,14 +11,18 @@ public class OutputView {
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
     public void printMap() {
-        String move_print = BridgeGame.map_move();
-
-        System.out.println("[ " + move_print + " ]");
-        System.out.println("[ " + move_print + " ]");
+        String move_print = BridgeGame.map_move(); int count = BridgeGame.endgame_2();
+        String count_print = " | " + move_print;
+        for(int i = 0; i <= count; i++){
+            if(count == 1){
+                System.out.println("[ " + move_print + " ]");                System.out.println("[ " + move_print + " ]");
+            }
+            else if(count >= 1){
+                System.out.println("[ " + move_print + count_print + " ]");
+                System.out.println("[ " + move_print + count_print + " | " + move_print + " ]");
+            }
+        }
     }
-
-
-
 
     /**
      * 게임의 최종 결과를 정해진 형식에 맞춰 출력한다.
