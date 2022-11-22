3820d6b73776cef5137a2b3fecc5a3bdd80fb639
dvlpr0 <rladjswkd@naver.com>
2022-11-22 20:33:10 +0900

    refactor: 출력 양식 조정

--- src/main/java/bridge/OutputView.java
+++ src/main/java/bridge/OutputView.java
@@ -7,6 +7,8 @@ import static bridge.OutputMessage.RESTART_OR_QUIT_MESSAGE;
 import static bridge.OutputMessage.RESULT_MESSAGE;
 import static bridge.OutputMessage.SUMMARY_MESSAGE;
 
+import static bridge.BridgeGameConstants.NEW_LINE;
+
 /**
  * 사용자에게 게임 진행 상황과 결과를 출력하는 역할을 한다.
  */
@@ -18,7 +20,7 @@ public class OutputView {
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
     public void printMap(String map) {
-        System.out.println(map);
+        System.out.println(map + NEW_LINE);
     }
 
     /**
@@ -29,7 +31,6 @@ public class OutputView {
     public void printResult(String map, String passOrFail, int tryCount) {
         System.out.println(RESULT_MESSAGE);
         printMap(map);
-        System.out.println();
         System.out.printf(SUMMARY_MESSAGE.toString(), passOrFail, tryCount);
     }
 
