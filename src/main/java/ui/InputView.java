dd22a32242e6127f15a4071bd0b9c40236d53b4c
thousandcows <milcamboo@gmail.com>
2022-11-22 10:12:59 +0900

    refactor: remove duplicated ui logic

--- src/main/java/ui/InputView.java
+++ src/main/java/ui/InputView.java
@@ -47,7 +47,6 @@ public class InputView {
     public String readGameCommand() {
         String gameCommand = "";
         try {
-            System.out.println("게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)");
             gameCommand = Console.readLine();
             validator.validateGameCommand(gameCommand);
         } catch (IllegalArgumentException exception) {
