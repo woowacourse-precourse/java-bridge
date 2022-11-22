5f746df9236dbbf812b753cfdf50f91a6132ac81
akskflwn <akskflwn@gmail.com>
2022-11-22 20:43:18 +0900

    refactor : BridgeMaker, Player 클래스 변수 상수화

--- src/main/java/bridge/domain/Player.java
+++ src/main/java/bridge/domain/Player.java
@@ -2,14 +2,18 @@ package bridge.domain;
 
 public class Player {
 
+    private static final int START_POSITION_VALUE = 0;
+    private static final String START_LAST_SELECTION_VALUE = "";
+    private static final boolean START_LIFE_VALUE = true;
+
     private int position;
     private String lastSelection;
     private boolean isAlive;
 
     public Player() {
-        this.position = 0;
-        this.lastSelection = "";
-        this.isAlive = true;
+        this.position = START_POSITION_VALUE;
+        this.lastSelection = START_LAST_SELECTION_VALUE;
+        this.isAlive = START_LIFE_VALUE;
     }
 
     public int getPosition() {
