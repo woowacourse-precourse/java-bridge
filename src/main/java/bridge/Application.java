a901cd85df8eb7ba5da7b96e3872d2fc2541ba84
seung-hun2 <jungsh2328@naver.com>
2022-11-22 22:14:38 +0900

    - Application 클래스 메인 메서드 구현

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -6,6 +6,42 @@ import java.util.List;
 public class Application {
 
     public static void main(String[] args) {
+        InputView inputView = new InputView();
+        int n = inputView.readBridgeSize();
+        BridgeRandomNumberGenerator bridgeRandomNumberGenerator = new BridgeRandomNumberGenerator();
+        BridgeMaker bm = new BridgeMaker(bridgeRandomNumberGenerator);
+        List<String> bridge = bm.makeBridge(n);
+        OutputView output = new OutputView();
+        List<String> nextStep = new ArrayList<>();
+        BridgeGame game = new BridgeGame();
 
+        int turn=0;
+        int tryCount=1;
+        int count=0;
+        int cnt=0;
+
+        while(turn<bridge.size()){
+            nextStep.add(inputView.readMoving());
+            System.out.println(nextStep);
+            turn = game.move(turn);
+            String ret = output.printMap(nextStep,bridge,turn);
+
+            if(ret.equals(" X ")){
+                String re = inputView.readGameCommand();
+                cnt = game.retry(re);
+                tryCount++;
+                count=turn;
+                turn=0;
+            }
+            if(ret.equals(" X ") && cnt==0) {
+                tryCount--;
+                break;
+            }
+            if (ret.equals(" X ") && nextStep.size() > 0)
+                nextStep.subList(0, nextStep.size()).clear();
+            count=turn;
+        }
+        if(count>0)
+            output.printResult(bridge, nextStep, tryCount, count);
     }
 }
