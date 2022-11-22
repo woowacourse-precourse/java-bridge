ed29ab0e875625b683b0d4d0a8eaf4b62de02da7
dener8 <8dener@gmail.com>
2022-11-22 19:38:26 +0900

    feat: 한 칸씩 움직이며 다리의 상태를 변화시키는 move() 구현
    위로 갈 때는 goUp()
    아래로 갈 때는 goDown()
    이 모두 move()에 종속되어 있음

--- src/main/java/bridge/BridgeGame.java
+++ src/main/java/bridge/BridgeGame.java
@@ -1,9 +1,10 @@
 package bridge;
 
-import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
-import static view.GeneralMessage.*;
+import static message.GeneralMessage.*;
 
 /**
  * 다리 건너기 게임을 관리하는 클래스
@@ -13,14 +14,17 @@ public class BridgeGame {
     BridgeNumberGenerator bridgeNumberGenerator = new BridgeRandomNumberGenerator();
     BridgeMaker bridgeMaker = new BridgeMaker(bridgeNumberGenerator);
 
+    int flag = 1;
 
-    public void start() { // start()에 모든 과정들을 다 담아야하는 것으로 의도한 것 같다.
+
+    public void start() { // start()는 내가 만든 메서드
         int bridgeSize = inputView.readBridgeSize();
 
-        System.out.println(GAME_START + INPUT_BRIDGE_LENGTH);
+        System.out.println(GAME_START.getMessage() + INPUT_BRIDGE_LENGTH.getMessage());
         List<String> answerBridge = bridgeMaker.makeBridge(bridgeSize); // 여기까진 한 번만 실행
-
-
+        // 여기부터 쭉 짜보자
+        List<List<String>> currentBridge = new ArrayList<>(List.of(List.of(), List.of()));
+        move(bridgeSize, answerBridge, currentBridge);
     }
 
     /**
@@ -28,15 +32,46 @@ public class BridgeGame {
      * <p>
      * 이동을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void move(int bridgeSize) {
-        System.out.println(SELECT_UP_OR_DOWN);
-        String upDown = inputView.readMoving(); // 경계선
-
-
+    public List<List<String>> move(int index, List<String> answerBridge, List<List<String>> currentBridge) {
+        String step = selectStep();
+        if (step.equals("U")) {
+            return goUp(index, answerBridge, currentBridge);
+        }
+        return goDown(index, answerBridge, currentBridge);
     }
 
-    public String goOrStop(String upDown, List<String> answerBridge, int index) {
-        if (answerBridge.get(index).equals(upDown)) {
+    public List<List<String>> goUp(int index, List<String> answerBridge, List<List<String>> currentBridge) {
+        currentBridge.get(1).add(" ");
+        if (answerBridge.get(index).equals("U")) {
+            currentBridge.get(0).add("O");
+        }
+        if (answerBridge.get(index).equals("D")) {
+            currentBridge.get(0).add("X");
+        }
+        return currentBridge;
+    }
+
+    public List<List<String>> goDown(int index, List<String> answerBridge, List<List<String>> currentBridge) {
+        currentBridge.get(0).add(" ");
+        if (answerBridge.get(index).equals("D")) {
+            currentBridge.get(1).add("O");
+        }
+        if (answerBridge.get(index).equals("U")) {
+            currentBridge.get(1).add("X");
+        }
+        return currentBridge;
+    }
+
+
+
+    public String selectStep() {
+        System.out.println(SELECT_UP_OR_DOWN);
+        String gamerStep = inputView.readMoving();
+        return gamerStep;
+    }
+
+    public String goOrStop(String gamerStep, List<String> answerBridge, int index) {
+        if (answerBridge.get(index).equals(gamerStep)) {
             return "O";
         }
         return "X";
@@ -48,12 +83,8 @@ public class BridgeGame {
      * 재시작을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
     public void retry() {
-        System.out.println(RETRY_OR_QUIT);
+        System.out.println(RETRY_OR_QUIT.getMessage());
         String retryOrQuit = inputView.readGameCommand();
-        if (retryOrQuit.equals("R")) {
-            // 처음으로 다시 돌아가기
-        } else if (retryOrQuit.equals("Q")) {
-            // 끝내고 결과 보기
-        }
+
     }
 }
