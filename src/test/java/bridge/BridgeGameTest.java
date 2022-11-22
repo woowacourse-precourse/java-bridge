38dbebb07ed4555cb116ddc64b74785ddb3ec764
dvlpr0 <rladjswkd@naver.com>
2022-11-22 16:06:07 +0900

    test: 다리 한 칸을 성공적으로 건넜는지 확인하는 테스트 구현

---
+++ src/test/java/bridge/BridgeGameTest.java
@@ -0,0 +1,21 @@
+package bridge;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class BridgeGameTest {
+    @DisplayName("다리 한 칸을 성공적으로 건넜는지 확인하는 기능에 대한 테스트")
+    @Test
+    public void checkCurrentStatusTest() {
+        BridgeGame game = new BridgeGame(List.of("U", "U", "U"));
+        assertThat(game.checkCurrentStatus()).isTrue();
+        game.move("U");
+        assertThat(game.checkCurrentStatus()).isTrue();
+        game.move("D");
+        assertThat(game.checkCurrentStatus()).isFalse();
+    }
+}
