713c8bf5c04ef3f9164e88d657df94b2f5ea2333
Song kyeong seok <skj0922@kookmin.ac.kr>
2022-11-22 18:04:41 +0900

    test(UserTest): 재시작 시 UserPath 초기화 테스트 구현

--- src/test/java/bridge/domain/UserTest.java
+++ src/test/java/bridge/domain/UserTest.java
@@ -14,7 +14,6 @@ public class UserTest {
 
     @Test
     @DisplayName("유저의 입력에 따라 다리 건너기 단위 테스트 (모두 건너기 성공)")
-    @ParameterizedTest()
     public void crossBridgeTest() {
         Bridge bridge = Bridge.initBridge(List.of("U", "D", "D"));
         List<MoveType> moveTypes = List.of(MoveType.UP_TYPE, MoveType.DOWN_TYPE, MoveType.DOWN_TYPE);
@@ -25,6 +24,25 @@ public class UserTest {
 
         String result = user.getUserPathLog();
         Assertions.assertThat(result).isEqualTo("[ O |   |   ]\n[   | O | O ]\n");
+    }
+
+    @Test
+    @DisplayName("다리 건너기에 실패 했을 때 경로 초기화 확인")
+    public void crossBridgeFailTest() {
+        Bridge bridge = Bridge.initBridge(List.of("U", "D", "D", "U"));
+        List<MoveType> moveTypes = List.of(MoveType.UP_TYPE, MoveType.DOWN_TYPE, MoveType.DOWN_TYPE, MoveType.DOWN_TYPE);
+
+        for (MoveType moveType : moveTypes) {
+            user.cross(bridge, moveType);
+        }
+
+        String beforeResult = user.getUserPathLog();
+        Assertions.assertThat(beforeResult).isEqualTo("[ O |   |   |   ]\n[   | O | O | X ]\n");
+
+        user.resetUserData();
+
+        String afterResult = user.getUserPathLog();
+        Assertions.assertThat(afterResult).isEqualTo("[  ]\n[  ]\n");
     }
 
 }
