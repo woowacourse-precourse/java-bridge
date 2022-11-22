fd48daeb25899e016f84bfaf92086804d1abf95e
sangil <gsl0505@naver.com>
2022-11-22 15:07:13 +0900

    refactor(OutputView): 요구사항에 맞게 안내문 출력 반복 제거

--- src/main/java/bridge/view/InputView.java
+++ src/main/java/bridge/view/InputView.java
@@ -13,8 +13,8 @@ public class InputView {
 	 * 다리의 길이를 입력받는다.
 	 */
 	public static int readBridgeSize() {
+		OutputView.printBridgeLength();
 		while (true) {
-			OutputView.printBridgeLength();
 			try {
 				return InputExceptionHandler.checkBridgeSizeForm(Console.readLine());
 			} catch (IllegalArgumentException errorMessage) {
@@ -27,8 +27,8 @@ public class InputView {
 	 * 사용자가 이동할 칸을 입력받는다.
 	 */
 	public static Move readMoving() {
+		OutputView.printMoving();
 		while (true) {
-			OutputView.printMoving();
 			try {
 				return InputExceptionHandler.checkMovingCommand(Console.readLine());
 			} catch (IllegalArgumentException errorMessage) {
@@ -41,8 +41,8 @@ public class InputView {
 	 * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
 	 */
 	public static String readGameCommand() {
+		OutputView.printRetryMessage();
 		while (true) {
-			OutputView.printRetryMessage();
 			try {
 				return InputExceptionHandler.checkRetryCommand(Console.readLine());
 			} catch (IllegalArgumentException errorMessage) {
