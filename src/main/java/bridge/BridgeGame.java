368b0e1c72ef3489329d8a8a2bf85f598d091668
phj1120 <hyeonj1998@naver.com>
2022-11-22 17:04:45 +0900

    style(bridge): 메서드 설명 추가

--- src/main/java/bridge/BridgeGame.java
+++ src/main/java/bridge/BridgeGame.java
@@ -7,11 +7,18 @@ import java.util.List;
  * 다리 건너기 게임을 관리하는 클래스
  */
 public class BridgeGame {
-
     private List<String> bridge = new ArrayList<>();
     private List<String> userRoute = new ArrayList<>();
-
     private int gameCount;
+
+    /**
+     * 게임 초기화
+     */
+    public void init(List<String> bridge) {
+        this.bridge = bridge;
+        this.userRoute = new ArrayList<>();
+        this.gameCount = 1;
+    }
 
     /**
      * 사용자가 칸을 이동할 때 사용하는 메서드
@@ -25,14 +32,7 @@ public class BridgeGame {
         return false;
     }
 
-    /**
-     * 게임 초기화
-     */
-    public void init(List<String> bridge) {
-        this.bridge = bridge;
-        this.userRoute = new ArrayList<>();
-        this.gameCount = 1;
-    }
+
 
     /**
      * 사용자가 게임을 다시 시도할 때 사용하는 메서드
@@ -49,7 +49,6 @@ public class BridgeGame {
     public List<String> getUserRoute() {
         return userRoute;
     }
-
 
     public int getGameCount() {
         return gameCount;
--- src/main/java/bridge/ui/controller/BridgeGameController.java
+++ src/main/java/bridge/ui/controller/BridgeGameController.java
@@ -9,14 +9,18 @@ import bridge.vo.GameCommand;
 
 import java.util.List;
 
-
+/**
+ * console 으로 요청 받아 게임을 진행하는 클래스
+ */
 public class BridgeGameController {
     private final BridgeMaker bridgeMaker = new BridgeMaker(new BridgeRandomNumberGenerator());
     private final InputView inputView = new InputView();
     private final OutputView outputView = new OutputView();
+    private final BridgeGame bridgeGame = new BridgeGame();
 
-    BridgeGame bridgeGame = new BridgeGame();
-
+    /**
+     * 게임 시작시 게임을 초기화 하는 메서드
+     */
     public BridgeGame initGame() {
         int bridgeSize = inputView.readBridgeSize();
         List<String> bridge = bridgeMaker.makeBridge(bridgeSize);
@@ -25,6 +29,9 @@ public class BridgeGameController {
         return bridgeGame;
     }
 
+    /**
+     * 사용자의 입력을 받아 이동 가능한지 여부를 판단하는 메서드
+     */
     public boolean move() {
         String moving = inputView.readMoving();
         boolean isMovable = bridgeGame.move(moving);
@@ -37,7 +44,6 @@ public class BridgeGameController {
     public void endGame(int count) {
         List<String> bridge = bridgeGame.getBridge();
         List<String> userRoute = bridgeGame.getUserRoute();
-
         outputView.printResult(bridge, userRoute, count);
     }
 
