754c67de921d858b67beb12f9026c9a22f8a75c1
dong yeol <ehfql6363@naver.com>
2022-11-22 15:11:35 +0900

    FEAT(ErrorMessage.java) : Create error message class
    Signed-off-by: dong yeol <ehfql6363@naver.com>

---
+++ src/main/java/bridge/validation/ErrorMessage.java
@@ -0,0 +1,7 @@
+package bridge.validation;
+
+public class ErrorMessage {
+    public static String ERROR_INVALID_BRIDGE_SIZE = "[ERROR] 다리 길이는 3부터 20 사이의 숫자여야 합니다.";
+    public static String ERROR_INVALID_GAME_COMMAND = "[ERROR] 게임 종료 여부에 대한 잘못된 입력값입니다. [R(재시작) 또는 Q(종료)]";
+    public static String ERROR_INVALID_MOVE_COMMAND = "[ERROR] 이동 방향에 대한 잘못된 입력값입니다. [U(위) 또는 D(아래)]";
+}
