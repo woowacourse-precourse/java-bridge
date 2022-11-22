16501044a40d932ea0e82fb071eadaf15a881726
hwiyeon0831 <aab555586@gmail.com>
2022-11-22 19:50:57 +0900

    feat(BridgeMaker): 다리 길이를 입력 받아 랜덤 값을 활용하여 다리를 생성하는 기능 구현

--- src/main/java/bridge/BridgeMaker.java
+++ src/main/java/bridge/BridgeMaker.java
@@ -1,5 +1,6 @@
 package bridge;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -7,17 +8,24 @@ import java.util.List;
  */
 public class BridgeMaker {
 
-    private final BridgeNumberGenerator bridgeNumberGenerator;
+	private final BridgeNumberGenerator bridgeNumberGenerator;
 
-    public BridgeMaker(BridgeNumberGenerator bridgeNumberGenerator) {
-        this.bridgeNumberGenerator = bridgeNumberGenerator;
-    }
+	public BridgeMaker(BridgeNumberGenerator bridgeNumberGenerator) {
+		this.bridgeNumberGenerator = bridgeNumberGenerator;
+	}
 
-    /**
-     * @param size 다리의 길이
-     * @return 입력받은 길이에 해당하는 다리 모양. 위 칸이면 "U", 아래 칸이면 "D"로 표현해야 한다.
-     */
-    public List<String> makeBridge(int size) {
-        return null;
-    }
+	/**
+	 * @param size 다리의 길이
+	 * @return 입력받은 길이에 해당하는 다리 모양. 위 칸이면 "U", 아래 칸이면 "D"로 표현해야 한다.
+	 */
+	public List<String> makeBridge(int size) {
+		List<String> bridge = new ArrayList<>();
+
+		while(size > 0) {
+			bridge.add(MoveCode.getInputCodeByNumberCode(bridgeNumberGenerator.generate()));
+			size--;
+		}
+
+		return bridge;
+	}
 }
