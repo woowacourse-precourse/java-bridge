32d90622b8dcdc3cd63d288377f564502f2dfb81
Jenn Kim <grdd0419@gmail.com>
2022-11-22 17:19:41 +0900

    test: 다리 길이를 입력받는 기능 관련 테스트 코드 추가

---
+++ src/test/java/bridge/CustomTest.java
@@ -0,0 +1,56 @@
+package bridge;
+
+import camp.nextstep.edu.missionutils.test.NsTest;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.util.List;
+
+import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class CustomTest extends NsTest {
+
+    private static final String ERROR_MESSAGE = "[ERROR]";
+
+    @DisplayName("다리 길이로 숫자 이외의 값이 들어오면 예외처리")
+    @ValueSource(strings = {"H", "i", "!"})
+    @ParameterizedTest
+    void bridgeSize_numeric_test(String strings) {
+        assertSimpleTest(() -> {
+            runException(strings);
+            assertThat(output()).contains(ERROR_MESSAGE);
+        });
+    }
+
+    @DisplayName("다리 길이가 3 이상 20 이하에 속하지 않는 경우 예외처리")
+    @ValueSource(strings = {"-1", "0", "2", "21", "100"})
+    @ParameterizedTest
+    void bridgeSize_range_test(String strings) {
+        assertSimpleTest(() -> {
+            runException(strings);
+            assertThat(output()).contains(ERROR_MESSAGE);
+        });
+    }
+
+
+    @Override
+    protected void runMain() {
+        Application.main(new String[]{});
+    }
+
+    static class TestNumberGenerator implements BridgeNumberGenerator {
+
+        private final List<Integer> numbers;
+
+        TestNumberGenerator(List<Integer> numbers) {
+            this.numbers = numbers;
+        }
+
+        @Override
+        public int generate() {
+            return numbers.remove(0);
+        }
+    }
+}
