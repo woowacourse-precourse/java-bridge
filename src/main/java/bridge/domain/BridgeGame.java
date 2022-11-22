a6de9d87324d4bf329a5a3293dfa4759230d8d38
Lee-SungMin <saevers26@gmail.com>
2022-11-22 18:25:03 +0900

    refactor: 게임 성공시 맞춘 개수 초기화

--- src/main/java/bridge/domain/BridgeGame.java
+++ src/main/java/bridge/domain/BridgeGame.java
@@ -13,6 +13,10 @@ public class BridgeGame {
     private static List<String> answerBridge = BridgeService.getAnswerBridge();
     private static int answerCount = 0;
 
+    public static int getAnswerCount(){
+        return answerCount;
+    }
+
     /**
      * 사용자가 칸을 이동할 때 사용하는 메서드
      * <p>
@@ -55,6 +59,7 @@ public class BridgeGame {
             BridgeService.getInitRetryQuit();
         }
         if(check == 2){
+            answerCount = 0;
             BridgeService.viewFinalResult(true);
         }
     }
