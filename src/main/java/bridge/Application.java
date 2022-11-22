aed63cf4825148046d467a0b17cf547e98234b2e
Dasol Jung <greteljds0251@gmail.com>
2022-11-22 15:20:04 +0900

    feat: 게임 시작 메서드

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -1,8 +1,22 @@
 package bridge;
 
+import java.util.List;
+
 public class Application {
 
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+    }
+}
+
+class Bridge {
+    private GameUI gameUI;
+    private InputView inputView;
+    private BridgeMaker bridgeMaker;
+    public List<String> gameStart() {
+        gameUI.gameStart();
+        gameUI.getBridgeSize();
+        int bridgeSize = inputView.readBridgeSize();
+        return bridgeMaker.makeBridge(bridgeSize);
     }
 }
