b2fbf739c86ff3f47efcf78c1ab5273a3b88684f
chaos0103 <lyt1228@naver.com>
2022-11-22 13:13:54 +0900

    refactor(Bridge): 메서드 추출, 코드 수정, 상수로 변경

--- src/main/java/bridge/Constants.java
+++ src/main/java/bridge/Constants.java
@@ -4,9 +4,11 @@ public final class Constants {
 
     public static final String UP = "U";
     public static final String DOWN = "D";
-    public static final String QUIT = "D";
-    public static final String RETRY = "D";
+    public static final String QUIT = "Q";
+    public static final String RETRY = "R";
     public static final String SUCCESS = "성공";
     public static final String FAIL = "실패";
 
+    public static final int MIN_RANGE = 3;
+    public static final int MAX_RANGE = 20;
 }
--- src/main/java/bridge/ui/Validate.java
+++ src/main/java/bridge/ui/Validate.java
@@ -1,36 +1,43 @@
 package bridge.ui;
 
+import java.util.List;
+
+import static bridge.Constants.*;
 import static bridge.message.ExceptionMessage.*;
-import static java.lang.Character.isDigit;
 
 public class Validate {
 
+    private static final List<String> DIRECTIONS = List.of(UP, DOWN);
+    private static final List<String> COMMANDS = List.of(RETRY, QUIT);
+
     //숫자 이외의 문자가 입력되면 예외가 발생한다.
     public static void digitTypeValidate(String input) {
-        for (char ch : input.toCharArray()) {
-            if (!isDigit(ch)) {
-                throw new IllegalArgumentException(TYPE_EXCEPTION);
-            }
+        if (!isDigit(input)) {
+            throw new IllegalArgumentException(TYPE_EXCEPTION);
         }
+    }
+
+    private static boolean isDigit(String input) {
+        return input.chars().allMatch(Character::isDigit);
     }
 
     //입력받은 숫자가 3 이상 20 이하의 숫자가 아니라면 예외가 발생한다.
     public static void numberRangeValidate(int number) {
-        if (!(3 <= number && number <= 20)) {
+        if (!(MIN_RANGE <= number && number <= MAX_RANGE)) {
             throw new IllegalArgumentException(OUT_OF_RANGE_EXCEPTION);
         }
     }
 
     //U(위 칸), D(아래 칸) 이외의 문자가 입력되면 예외가 발생한다.
     public static void directionValidate(String direction) {
-        if (!(direction.equals("U") || direction.equals("D"))) {
+        if (!DIRECTIONS.contains(direction)) {
             throw new IllegalArgumentException(DIRECTION_EXCEPTION);
         }
     }
 
     //R(재시작), Q(종료) 이외의 문자가 입력되면 예외가 발생한다.
     public static void commandValidate(String command) {
-        if (!(command.equals("R") || command.equals("Q"))) {
+        if (!COMMANDS.contains(command)) {
             throw new IllegalArgumentException(COMMAND_EXCEPTION);
         }
     }
