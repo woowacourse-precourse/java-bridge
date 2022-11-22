783497bd0c6296d5426db6c72a65b54c75d6b9fd
p <gustjr2332@naver.com>
2022-11-22 20:08:23 +0900

    feat(ResultRepository) : 현재까지 누적된 결과를 최종결과에 저장 후 최종출력 호출 기능 구현

--- src/main/java/bridge/ResultRepository.java
+++ src/main/java/bridge/ResultRepository.java
@@ -9,6 +9,7 @@ public class ResultRepository {
     private OutputView outView = new OutputView();
     private List<String> aboveOfBridgeResult;
     private List<String> belowOfBridgeResult;
+    private List<List<String>> totalBridgeResult = new ArrayList<>();
 
     private final int FRISTMOVING = 1;
     private final String BEGINNINGOFBRIDGE = "[ ";
@@ -16,11 +17,12 @@ public class ResultRepository {
     private final String DIVISIONOFBRIDGE = " | ";
 
     public ResultRepository() {
+
         this.aboveOfBridgeResult = new ArrayList<>(List.of(BEGINNINGOFBRIDGE, ENDOFBRIDGE));
         this.belowOfBridgeResult = new ArrayList<>(List.of(BEGINNINGOFBRIDGE, ENDOFBRIDGE));
     }
 
-    public void saveResult(Map<String, String> movingResult, int numberOfMoving) {
+    public void saveCumulativeResult(Map<String, String> movingResult, int numberOfMoving) {
         if (numberOfMoving > FRISTMOVING) {
             aboveOfBridgeResult.add(aboveOfBridgeResult.size() - 1, DIVISIONOFBRIDGE);
             belowOfBridgeResult.add(belowOfBridgeResult.size() - 1, DIVISIONOFBRIDGE);
@@ -29,5 +31,12 @@ public class ResultRepository {
         belowOfBridgeResult.add(belowOfBridgeResult.size() - 1, movingResult.get(BridgeDirection.DOWN.getDirection()));
 
         outView.printMap(aboveOfBridgeResult, belowOfBridgeResult);
+    }
+
+    public void saveFinalResult(int numberOfAttempt, String successOrFailure) {
+        totalBridgeResult.add(BridgeDirection.DOWN.getBridgeRandomNumber(), belowOfBridgeResult);
+        totalBridgeResult.add(BridgeDirection.UP.getBridgeRandomNumber(), aboveOfBridgeResult);
+
+        outView.printResult(totalBridgeResult, numberOfAttempt, successOrFailure);
     }
 }
