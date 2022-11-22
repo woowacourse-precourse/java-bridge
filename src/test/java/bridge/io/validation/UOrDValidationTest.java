522134ca63d54e9edbedd535d1289efe292b65a1
yubin <dbqls2821@gmail.com>
2022-11-22 18:18:07 +0900

    test(RangeValidationTest): 입력값이 U 혹은 D가 아닐때 예외를 던지는지 테스트

---
+++ src/test/java/bridge/io/validation/UOrDValidationTest.java
@@ -0,0 +1,22 @@
+package bridge.io.validation;
+
+import bridge.enums.ExceptionMessage;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.assertj.core.api.Assertions.assertThatCode;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.*;
+
+class UOrDValidationTest {
+
+  @ParameterizedTest
+  @ValueSource(strings = {"u", "d", "A", "U ", " D", "UU", "DD", "2424", "0", "-2497", "wooteco"})
+  void 입력값이_U또는_D가_아니라면_예외를_던지는가(String input) {
+    UOrDValidation uOrDValidation = new UOrDValidation(new NullValidation());
+    assertThatThrownBy(() -> uOrDValidation.validate(input))
+      .isInstanceOf(IllegalArgumentException.class)
+      .hasMessageContaining(ExceptionMessage.ERROR.getMessage() + ExceptionMessage.NOT_U_OR_D.getMessage());
+  }
+
+}
