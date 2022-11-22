3d420334d40c44d86ec92369fd0db4706afde454
eom-tae-in <101633465+eom-tae-in@users.noreply.github.com>
2022-11-22 23:33:11 +0900

    feat: bridgeGame 생성

--- src/main/java/bridge/BridgeGame.java
+++ src/main/java/bridge/BridgeGame.java
@@ -1,23 +1,26 @@
-package bridge;
+package bridge.domain;
+import java.util.List;
 
-/**
- * 다리 건너기 게임을 관리하는 클래스
- */
 public class BridgeGame {
-
-    /**
-     * 사용자가 칸을 이동할 때 사용하는 메서드
-     * <p>
-     * 이동을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
-     */
-    public void move() {
+    private int position;
+    private List<String> bridge;
+    public BridgeGame(List<String> bridge) {
+        this.position = 0;
+        this.bridge = bridge;
     }
-
-    /**
-     * 사용자가 게임을 다시 시도할 때 사용하는 메서드
-     * <p>
-     * 재시작을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
-     */
+    public List<String> getBridge() {
+        return this.bridge;
+    }
+    public boolean comparePositionLength() {
+        return this.bridge.size() == this.position;
+    }
+    public boolean correct(String userInput) {
+        return userInput.equals(bridge.get(position));
+    }
+    public void move() {
+        position++;
+    }
     public void retry() {
+        this.position = 0;
     }
 }
