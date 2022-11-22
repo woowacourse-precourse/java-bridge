ef6db9024d41531976cc8403028509dadddd2d1c
ddukddaki <rlfrkdms1@naver.com>
2022-11-22 15:23:20 +0900

    feat(BridgeMaker): 다리를 생성하는 기능

--- src/main/java/bridge/BridgeMaker.java
+++ src/main/java/bridge/BridgeMaker.java
@@ -1,5 +1,6 @@
 package bridge;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -8,6 +9,7 @@ import java.util.List;
 public class BridgeMaker {
 
     private final BridgeNumberGenerator bridgeNumberGenerator;
+    private final List<String> bridge = new ArrayList<>();
 
     public BridgeMaker(BridgeNumberGenerator bridgeNumberGenerator) {
         this.bridgeNumberGenerator = bridgeNumberGenerator;
@@ -18,6 +20,9 @@ public class BridgeMaker {
      * @return 입력받은 길이에 해당하는 다리 모양. 위 칸이면 "U", 아래 칸이면 "D"로 표현해야 한다.
      */
     public List<String> makeBridge(int size) {
-        return null;
+        for (int i = 0; i < size; i++) {
+            bridge.add(UpAndDown.convertRandomValueToCommand(bridgeNumberGenerator.generate()));
+        }
+        return bridge;
     }
 }
