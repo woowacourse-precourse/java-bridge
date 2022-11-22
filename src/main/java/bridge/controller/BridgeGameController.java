48ce9e4becd2dac60da9b45f457eb8d0a22d53bb
Cyma-s <ssol0319@naver.com>
2022-11-22 09:59:54 +0900

    feat(BridgeGameController) : add restart, game complete functions

--- src/main/java/bridge/controller/BridgeGameController.java
+++ src/main/java/bridge/controller/BridgeGameController.java
@@ -1,6 +1,8 @@
 package bridge.controller;
 
 import bridge.domain.BridgeGame;
+import bridge.domain.FinalGameResult;
+import bridge.domain.GameCommand;
 import bridge.domain.GameResult;
 import bridge.view.InputView;
 import bridge.view.OutputView;
@@ -9,6 +11,8 @@ public class BridgeGameController {
     private final InputView inputView;
     private final OutputView outputView;
     private BridgeGame bridgeGame;
+    private boolean isFinished = false;
+    private GameResult gameResult;
 
     public BridgeGameController() {
         this.inputView = new InputView();
@@ -19,10 +23,16 @@ public class BridgeGameController {
         makeBridgeGame();
         while (true) {
             progressGameStage();
+            checksGameEnd();
+            if(isFinished) {
+                completeGame();
+                break;
+            }
         }
     }
 
     public void makeBridgeGame() {
+        outputView.printStart();
         int bridgeSize = inputView.readBridgeSize();
         this.bridgeGame = new BridgeGame(bridgeSize);
     }
@@ -30,6 +40,31 @@ public class BridgeGameController {
     public void progressGameStage() {
         String userMove = inputView.readMoving();
         GameResult gameResult = bridgeGame.move(userMove);
+        this.gameResult = gameResult;
         outputView.printMap(gameResult);
+    }
+
+    public void checksGameEnd() {
+        if(bridgeGame.isSuccess()) {
+            isFinished = true;
+        }
+        if(bridgeGame.isEnd()) {
+            String gameCommand = inputView.readGameCommand();
+            retryOrEnd(gameCommand);
+        }
+    }
+
+    public void retryOrEnd(String gameCommand) {
+        GameCommand userCommand = GameCommand.makeGameCommand(gameCommand);
+        if(userCommand.equals(GameCommand.RESTART)) {
+            bridgeGame.retry();
+            return;
+        }
+        isFinished = true;
+    }
+
+    public void completeGame() {
+        FinalGameResult finalGameResult = FinalGameResult.makeFinalGameResult(bridgeGame.isSuccess());
+        outputView.printResult(finalGameResult, bridgeGame.getTotalTryCount(), gameResult);
     }
 }
