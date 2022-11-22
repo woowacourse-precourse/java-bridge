44d51da1799f6c248493f62a333545c7beb9d7b2
jijongkwon <help3451@naver.com>
2022-11-22 14:52:52 +0900

    fix(OutPutView) : 타입 불일치 수정

--- src/main/java/bridge/view/OutputView.java
+++ src/main/java/bridge/view/OutputView.java
@@ -27,7 +27,7 @@ public class OutputView {
      * <p>
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void printResult(String successOrFail, int tryNumber) {
+    public void printResult(String successOrFail, String tryNumber) {
         System.out.println(viewMessage.RESULT_GAME_SUCCESS_STATUS_MESSAGE.getMessage() + successOrFail);
         System.out.println(viewMessage.RESULT_GAME_TRY_NUMBER_MESSAGE.getMessage() + tryNumber);
     }
