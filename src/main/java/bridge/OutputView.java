d2f94f1397b2437586fa94a9df46cf5deb7428f0
SeongchanCho <wordnumperiod@gmail.com>
2022-11-22 20:16:58 +0900

    feat(OutputView.java): 콘솔을 통해 사용자에게 게임 정보를 알려주는 기능 구현 완료
    게임 정보
    - 게임 결과
    - 게임 진행 현황 (브릿지 이동)

--- src/main/java/bridge/OutputView.java
+++ src/main/java/bridge/OutputView.java
@@ -1,9 +1,20 @@
 package bridge;
 
+import bridge.BridgeGame.Direction;
+import bridge.BridgeGame.Result;
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * 사용자에게 게임 진행 상황과 결과를 출력하는 역할을 한다.
  */
 public class OutputView {
+
+    private BridgeGame game;
+
+    public OutputView(BridgeGame game) {
+        this.game = game;
+    }
 
     /**
      * 현재까지 이동한 다리의 상태를 정해진 형식에 맞춰 출력한다.
@@ -11,6 +22,39 @@ public class OutputView {
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
     public void printMap() {
+        int columnIndex = game.getColumnIndex();
+        int rowIndex = game.getRowIndex();
+        List<String> mapToPrint = new ArrayList<>();
+        List<Direction> map = game.getMap();
+        List<Direction> track = game.getTrack();
+        mapToPrint.add("");
+        mapToPrint.add("");
+        mapToPrint.set(0, "[ ");
+        mapToPrint.set(1, "[ ");
+        for (int i = 0; i < track.size(); i++) {
+            if (track.get(i) == Direction.UP && map.get(i) == Direction.UP) {
+                mapToPrint.set(0, mapToPrint.get(0) + "0");
+                mapToPrint.set(1, mapToPrint.get(1) + " ");
+            } else if (track.get(i) == Direction.UP && map.get(i) == Direction.DOWN) {
+                mapToPrint.set(0, mapToPrint.get(0) + "X");
+                mapToPrint.set(1, mapToPrint.get(1) + "O");
+            } else if (track.get(i) == Direction.DOWN && map.get(i) == Direction.DOWN) {
+                mapToPrint.set(0, mapToPrint.get(0) + " ");
+                mapToPrint.set(1, mapToPrint.get(1) + "O");
+            } else if (track.get(i) == Direction.DOWN && map.get(i) == Direction.UP) {
+                mapToPrint.set(0, mapToPrint.get(0) + "O");
+                mapToPrint.set(1, mapToPrint.get(1) + "X");
+            }
+            if (i < track.size() - 1) {
+                mapToPrint.set(0, " | ");
+                mapToPrint.set(1, " | ");
+            }
+        }
+        mapToPrint.set(0, " ]");
+        mapToPrint.set(1, " ]");
+        for (String row : mapToPrint) {
+            System.out.println(row);
+        }
     }
 
     /**
@@ -19,5 +63,7 @@ public class OutputView {
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
     public void printResult() {
+        System.out.println("게임 성공 여부: 성공");
+        System.out.printf("총 시도한 횟수: %d\n", game.getTryCount());
     }
 }
