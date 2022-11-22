75eee3d3227c1d9260347d80c64870ab9afa0e50
Dasol Jung <greteljds0251@gmail.com>
2022-11-22 23:29:45 +0900

    feat: 다리 생성하기

--- src/main/java/bridge/OutputView.java
+++ src/main/java/bridge/OutputView.java
@@ -7,6 +7,7 @@ import java.util.List;
  * 사용자에게 게임 진행 상황과 결과를 출력하는 역할을 한다.
  */
 public class OutputView {
+    BridgeUI bridgeUI = new BridgeUI();
 
     /**
      * 현재까지 이동한 다리의 상태를 정해진 형식에 맞춰 출력한다.
@@ -18,7 +19,8 @@ public class OutputView {
         List<Integer> bridgeDown = new ArrayList<>();
         bridgeUpper = setBridgeUpper(playerMoving, moveOutcome);
         bridgeDown = setBridgeDown(playerMoving, moveOutcome);
-
+        makeBridge(bridgeUpper);
+        makeBridge(bridgeDown);
     }
 
     public List<Integer> setBridgeUpper(List<String> playerMoving, List<Integer> moveOutcome) {
@@ -53,6 +55,29 @@ public class OutputView {
             return 0;
         }
         return 2;
+    }
+
+    public void makeBridge(List<Integer> bridge) {
+        bridgeUI.start();
+        for (int i = 0; i < bridge.size(); i++) {
+            printOutcome(bridge.get(i));
+            bridgeUI.divideLine();
+        }
+        bridgeUI.end();
+    }
+
+    public void printOutcome(int bridge) {
+        if (bridge == 1) {
+            bridgeUI.success();
+            return;
+        }
+        if (bridge == 0) {
+            bridgeUI.fail();
+            return;
+        }
+        if (bridge == 2) {
+            bridgeUI.empty();
+        }
     }
 
     /**
