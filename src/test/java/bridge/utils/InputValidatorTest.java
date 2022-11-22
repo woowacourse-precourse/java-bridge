166d93ee2a4a2c08b06c8985f9710422e0caff1f
yxnxx <jh4rgh0@gmail.com>
2022-11-22 21:16:25 +0900

    chore(InputValidatorTest): move movement Exception Test

--- src/test/java/bridge/utils/InputValidatorTest.java
+++ src/test/java/bridge/utils/InputValidatorTest.java
@@ -14,6 +14,13 @@ class InputValidatorTest {
             inputValidator.isBridgeSizeNumeric(size);
         });
     }
+    @ParameterizedTest(name="사용자의 이동으로 U와 D 이외의 것이 입력되었을 때 예외 처리")
+    @ValueSource(strings={"40", "ㅁ", "A"})
+    void movementExceptionTest(String movement) {
+        assertThrows(IllegalArgumentException.class, () -> {
+            inputValidator.isMovementValid(movement);
+        });
+    }
     @ParameterizedTest(name="재시작 여부 입력 시 R이나 Q가 아니면 예외 처리")
     @ValueSource(strings={"ABC", "136", "ㅇㅇ"})
     void retryExceptionTest(String retry) {
