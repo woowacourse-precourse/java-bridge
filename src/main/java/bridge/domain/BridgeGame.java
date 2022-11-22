a5eb4e9b260999a7d68f37d115e81584a1a8f37b
star-sil <kimse9811@naver.com>
2022-11-22 22:15:05 +0900

    feat(BridgeGame): update feature to move bridge
    사용자가 입력한 칸으로 이동하는 기능 로직 구현
    이동 결과(이동 가능 여부)를 반환

--- src/main/java/bridge/domain/BridgeGame.java
+++ src/main/java/bridge/domain/BridgeGame.java
@@ -1,16 +1,33 @@
 package bridge.domain;
 
+import bridge.type.Bridge;
+import bridge.type.TextType;
+import java.util.List;
+
 /**
  * 다리 건너기 게임을 관리하는 클래스
  */
 public class BridgeGame {
+    private List<String> bridges;
+    private List<Bridge> results;
+    private int tryCount;
+    private int pos;
 
     /**
      * 사용자가 칸을 이동할 때 사용하는 메서드
      * <p>
      * 이동을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void move() {
+    public boolean move(String move) {
+        if (bridges.get(pos).equals(move)) {
+            makeResult(true,bridges.get(pos));
+            pos++;
+            return true;
+        }
+
+        makeResult(false,bridges.get(pos));
+        tryCount++;
+        return false;
     }
 
     /**
@@ -19,5 +36,17 @@ public class BridgeGame {
      * 재시작을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
     public void retry() {
+    }
+
+    public BridgeGame(List<String> bridges) {
+        this.bridges = bridges;
+    }
+
+    private void makeResult(boolean correctMove, String bridge) {
+
+        if (correctMove && bridge.equals(TextType.UP)) results.add(Bridge.UP_CORRECT);
+        else if (!correctMove && bridge.equals(TextType.UP)) results.add(Bridge.UP_WRONG);
+        else if (correctMove && bridge.equals(TextType.DOWN)) results.add(Bridge.DOWN_CORRECT);
+        else if (!correctMove && bridge.equals(TextType.DOWN)) results.add(Bridge.DOWN_WRONG);
     }
 }
