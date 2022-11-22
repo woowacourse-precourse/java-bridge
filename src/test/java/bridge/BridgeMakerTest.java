5483363052e67fa1809374a7cb814466024e637a
jiisuniui <dudndi9752@gmail.com>
2022-11-22 23:03:18 +0900

    test(BridgeMakerTest): BridgeMaker 테스트 구현

--- src/test/java/bridge/BridgeMakerTest.java
+++ src/test/java/bridge/BridgeMakerTest.java
@@ -3,6 +3,7 @@ package bridge;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import java.util.List;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class BridgeMakerTest {
@@ -10,6 +11,7 @@ public class BridgeMakerTest {
     BridgeMaker bridgeMaker = new BridgeMaker(new BridgeRandomNumberGenerator());
 
     @Test
+    @DisplayName("다리 만들기 테스트")
     void test_makeBridge() {
         int size = 5;
         List<String> bridge = bridgeMaker.makeBridge(size);
