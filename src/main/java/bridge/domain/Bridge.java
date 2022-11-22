8768e71dcef06089368d195072cac68ac748ff10
ycopp9 <aciees95@gmail.com>
2022-11-22 16:50:10 +0900

    feat(Bridge): 다리 출력을 위한 그 전까지의 정보를 반환하는 메소드 구현
    getBridgeInfo 메소드를 통해 다리 출력을 위한 그 전까지의 정보를 반환함

--- src/main/java/bridge/domain/Bridge.java
+++ src/main/java/bridge/domain/Bridge.java
@@ -1,5 +1,8 @@
 package bridge.domain;
 
+import bridge.BridgeInfo;
+import bridge.constant.Token;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -15,7 +18,34 @@ public class Bridge {
     public boolean update(String userInput) {
         int index = correct.size();
         correct.add(answer.get(index).equals(userInput));
-        return correct.get(correct.size()-1);
+        return correct.get(correct.size() - 1);
+    }
+
+    public List<BridgeInfo> getBridgeInfo(String mark) {
+        List<BridgeInfo> infos = new ArrayList<>();
+        for (int index = 0; index < correct.size() - 1; index++) {
+            infos.add(getCorrectInfo(index, mark));
+        }
+        infos.add(getLastInfo(mark));
+        return infos;
+    }
+
+    private BridgeInfo getCorrectInfo(int index, String mark) {
+        if (answer.get(index).equals(mark)) {
+            return BridgeInfo.CORRECT;
+        }
+        return BridgeInfo.EMPTY;
+    }
+
+    private BridgeInfo getLastInfo(String mark) {
+        int index = correct.size() - 1;
+        if (correct.get(index)) {
+            return getCorrectInfo(index, mark);
+        }
+        if (answer.get(index).equals(mark)) {
+            return BridgeInfo.EMPTY;
+        }
+        return BridgeInfo.WRONG;
     }
 
     public void reset() {
