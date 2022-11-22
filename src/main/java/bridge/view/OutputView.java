905021d4a08694153fb2e8e6cf37bb24552f0a3a
Seung-Been Kim <mainlib990@gmail.com mainlib990s-MacBookPro.local>
2022-11-22 17:15:46 +0900

    feat(view): implement the ui that print size of bridge

--- src/main/java/bridge/view/OutputView.java
+++ src/main/java/bridge/view/OutputView.java
@@ -3,10 +3,15 @@ package bridge.view;
 public class OutputView {
 
     private static final String MESSAGE_TITLE = "다리 건너기 게임을 시작합니다.";
+    private static final String MESSAGE_BRIDGE_SIZE = "다리의 길이를 입력해주세요.";
 
     public void printTitle() {
         System.out.println(MESSAGE_TITLE);
         System.out.println();
+    }
+
+    public void printBridgeSize() {
+        System.out.println(MESSAGE_BRIDGE_SIZE);
     }
 
     public void printMap() {
