1b2d171b8b49d92f3b34997a4e20e6e3017c1868
thuthi <whquddn55@gmail.com>
2022-11-22 20:19:54 +0900

    feat(view): 사용자로부터 생성할 다리 길이를 입력 받는 메서드 추가 InputView에 readBridgeSize 구현 및 exception handling 추가.

---
+++ src/main/java/bridge/view/InputView.java
@@ -0,0 +1,40 @@
+package bridge.view;
+
+import static bridge.exception.IllegalArgumentExceptionMessage.ILLEGAL_BRIDGE_LENGTH;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class InputView {
+    public static final int BRIDGE_LOWER_INCLUSIVE = 3;
+    public static final int BRIDGE_UPPER_INCLUSIVE = 20;
+
+    public int readBridgeSize() throws IllegalArgumentException {
+        try {
+            int bridgeSize = Integer.parseInt(Console.readLine());
+            validateBridgeSize(bridgeSize);
+            return bridgeSize;
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException(ILLEGAL_BRIDGE_LENGTH.getMessage());
+        }
+    }
+
+    private void validateBridgeSize(int bridgeSize) throws IllegalArgumentException {
+        if (BRIDGE_LOWER_INCLUSIVE > bridgeSize || bridgeSize > BRIDGE_UPPER_INCLUSIVE) {
+            throw new IllegalArgumentException(ILLEGAL_BRIDGE_LENGTH.getMessage());
+        }
+    }
+
+    /**
+     * 사용자가 이동할 칸을 입력받는다.
+     */
+    public String readMoving() {
+        return null;
+    }
+
+    /**
+     * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
+     */
+    public String readGameCommand() {
+        return null;
+    }
+}
