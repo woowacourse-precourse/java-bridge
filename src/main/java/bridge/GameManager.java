2216ddcbf9659e799f14bd3d3863aa7fbbabc727
kimilchul <huj0159@gmail.com>
2022-11-22 10:57:02 +0900

    feat: play a single game

--- src/main/java/bridge/GameManager.java
+++ src/main/java/bridge/GameManager.java
@@ -10,69 +10,71 @@ public class GameManager {
     BridgeMaker bridgeMaker = new BridgeMaker(new BridgeRandomNumberGenerator());
     BridgeGame bridgeGame;
 
-    public void initGame(){
+    public void initGame() {
         outputView.printGameStart();
 
-        bridgeGame = new BridgeGame(BridgeMap.generateMap(),createBridge());
+        bridgeGame = new BridgeGame(BridgeMap.generateMap(), createBridge());
 
         int tryCount = 1;
+        boolean gameClear = true;
 
-
-        while (!bridgeGame.isEnd()){
-            if(!askMove()){
-                if(askRetry()){
-                   bridgeGame.retry();
-                }else{
+        while (!bridgeGame.isEnd()) {
+            if (!askMove()) {
+                if (askRetry()) {
+                    tryCount++;
+                    bridgeGame.retry();
+                } else {
+                    gameClear=false;
                     break;
                 }
             }
         }
 
-        outputView.printResult(bridgeGame.getBridgeMap());
+        outputView.printResult(bridgeGame.getBridgeMap(),tryCount,gameClear);
 
     }
 
-    private List<String> createBridge(){
+    private List<String> createBridge() {
         while (true) {
             outputView.askBridgeSize();
             try {
                 return bridgeMaker.makeBridge(inputView.readBridgeSize());
-            }catch (IllegalArgumentException e){
+            } catch (IllegalArgumentException e) {
                 System.out.println(e.getMessage());
                 continue;
             }
         }
     }
 
-    private boolean askMove(){
+    private boolean askMove() {
         while (true) {
             outputView.askMove();
             try {
                 boolean result = bridgeGame.move(inputView.readMoving());
                 outputView.printMap(bridgeGame.getBridgeMap());
                 return result;
-            }catch (IllegalArgumentException e){
+            } catch (IllegalArgumentException e) {
                 System.out.println(e.getMessage());
                 continue;
             }
         }
     }
 
-    private boolean askRetry(){
+    private boolean askRetry() {
         while (true) {
             outputView.askRestart();
             try {
                 return isRestart();
-            }catch (IllegalArgumentException e){
+            } catch (IllegalArgumentException e) {
                 System.out.println(e.getMessage());
                 continue;
             }
         }
     }
 
-    private boolean isRestart(){
+    private boolean isRestart() {
         String result = inputView.readGameCommand();
-        if(result.equals("R")){
+        if (result.equals("R")) {
             return true;
         }
         return false;
