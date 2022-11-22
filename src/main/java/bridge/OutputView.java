4f2cfb2679901d08f5589f4a26800939be21d81c
hyeongu <wancern@naver.com>
2022-11-22 23:41:59 +0900

    refactor(OutputView): 필드 추가

--- src/main/java/bridge/OutputView.java
+++ src/main/java/bridge/OutputView.java
@@ -13,7 +13,7 @@ public class OutputView {
      * <p>
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public List<String> printMap(List<String> userMoving, boolean iscorrect) {
+    public void printMap(List<String> userMoving, boolean iscorrect) {
         List<String> printMap = new ArrayList<>();
         printMap.add(getMap(userMoving, iscorrect, "U"));
         printMap.add(getMap(userMoving, iscorrect, "D"));
@@ -21,7 +21,6 @@ public class OutputView {
         for(String map : printMap){
             System.out.println(map);
         }
-        return printMap;
     }
 
     /**
@@ -31,8 +30,7 @@ public class OutputView {
      */
     public void printResult(List<String> result, boolean flag, int count) {
         System.out.println("최종 게임 결과");
-        System.out.println(result.get(0));
-        System.out.println(result.get(1));
+        printMap(result, flag);
 
         System.out.println("게임 성공 여부: " + isSuccess(flag));
         System.out.println("총 시도한 횟수: " + count);
