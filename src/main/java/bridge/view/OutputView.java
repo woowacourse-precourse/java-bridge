cfd2dd4216d1ba7e99adc7ba6c1e3df8dd8caecd
sora5421 <sojeoung536985@gmail.com>
2022-11-22 18:17:19 +0900

    feat : add printTryLengthInput method

--- src/main/java/bridge/view/OutputView.java
+++ src/main/java/bridge/view/OutputView.java
@@ -10,6 +10,9 @@ import java.util.List;
 public class OutputView {
     public void printWelcome() {
         System.out.println("다리 건너기 게임을 시작합니다.\n");
+    }
+
+    public void printTryLengthInput() {
         System.out.println("다리의 길이를 입력해주세요.");
     }
 
