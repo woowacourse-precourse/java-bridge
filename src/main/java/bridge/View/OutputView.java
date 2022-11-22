e01ab32ed2c8bab9809722e72e473396f47bd784
nahyeon <whskgus12@nate.com>
2022-11-22 17:15:30 +0900

    refactor(View): 다리 건너기 게임 메세지 출력하기
    다리 건너기 게임에 필요한 문구들을 Enum 클래스에서 가져오도록 변경하였습니다.

--- src/main/java/bridge/View/OutputView.java
+++ src/main/java/bridge/View/OutputView.java
@@ -8,11 +8,11 @@ import java.util.List;
 public class OutputView {
 
     public void printStartMessage() {
-        System.out.println("다리 건너기 게임을 시작합니다.");
+        System.out.println(GameMessage.GAME_START_MESSAGE.getGameMessage());
     }
 
     public void printMapMessage() {
-        System.out.println("\n최종 게임 결과");
+        System.out.println(GameMessage.GAME_MAP_MESSAGE.getGameMessage());
     }
 
     /**
@@ -39,11 +39,13 @@ public class OutputView {
      */
     public void printResult(boolean isSuccess, int reGameCount) {
         if (isSuccess) {
-            System.out.println("게임 성공 여부: 성공");
+            System.out.println(GameMessage.GAME_SUCCESS_STATUS.getGameMessage()
+                    + GameMessage.GAME_SUCCESS.getGameMessage());
         }
         if (!isSuccess) {
-            System.out.println("게임 성공 여부: 실패");
+            System.out.println(GameMessage.GAME_SUCCESS_STATUS.getGameMessage()
+                    + GameMessage.GAME_FAILURE.getGameMessage());
         }
-        System.out.println("총 시도한 횟수: " + reGameCount);
+        System.out.println(GameMessage.GAME_TRY_COUNT.getGameMessage() + reGameCount);
     }
 }
