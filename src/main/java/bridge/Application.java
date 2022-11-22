96ceea7af1dcfc2d9a8feb1ab4efdbf076a20675
kimdeokyoung <dudejrss@naver.com>
2022-11-22 19:46:56 +0900

    feat (Application) : 사용자 입력에 따른 게임 상태전이 기능

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -6,40 +6,125 @@ import java.util.Map;
 
 public class Application {
 
-    final private Map<List<Integer>, State> rules = setRules();
-    private State currentState;
+    final private static Map<List<Integer>, State> rules = setRules();
+    private static State currentState = State.INITIAL_STATE;
+    private static BridgeGame bg;
+    final private static InputView in = new InputView();
+    final private static OutputView out = new OutputView();
+
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        while (!currentState.end) {
+            try {
+                System.out.println(currentState.name());
+                currentState.routine();
+            } catch (Exception e) {
+                dispatch(TRIGGER.INPUT_INVALID);
+                System.out.println(e.getMessage());
+            }
+        }
     }
+
+    private static void dispatch(TRIGGER trigger) {
+        currentState = rules.get(List.of(currentState.ordinal(), trigger.ordinal()));
+    }
+
+    private static void dispatch2(TRIGGER trigger, boolean result) {
+        currentState = rules.get(List.of(currentState.ordinal(), trigger.ordinal()));
+        currentState.result = result;
+    }
+
+
     private enum State {
-        INITIAL_STATE, SELECTION_STATE, SUCCESS_TEST_STATE, FINISH_TEST_STATE, END
+        INITIAL_STATE {
+            public void routine() {
+                int bridgeSize = in.readBridgeSize();
+                bg = new BridgeGame(out, bridgeSize);
+                currentState = State.SELECTION_STATE;
+                currentState.end = false;
+            }
+        },
+        SELECTION_STATE {
+            public void routine(){
+                String direction = in.readMoving();
+                boolean result = bg.move(direction);
+                dispatch2(TRIGGER.INPUT_VALID, result);
+            }
+        },
+        SUCCESS_TEST_STATE{
+            public void routine(){
+                System.out.println(currentState.result);
+                if (!currentState.result) {
+                    dispatch(TRIGGER.SELECTION_FAILURE);
+                    return;
+                }
+                if(bg.isFinished()){
+                    dispatch(TRIGGER.FINAL_SUCCESS);
+                    return;
+                }
+                dispatch(TRIGGER.SELECTION_SUCCESS);
+            }
+
+        }, FINISH_TEST_STATE{
+            public void routine(){
+
+                if ( !bg.isFinished() && in.readGameCommand()) {
+                    bg.retry();
+                    dispatch(TRIGGER.CONTINUE);
+                    return;
+                }
+                dispatch(TRIGGER.FINISH);
+            }
+        }, END{
+            public void routine(){
+                bg.finish();
+                currentState.end = true;
+            }
+        };
+
+        boolean result;
+        boolean end;
+        public abstract void routine();
     }
 
     private enum TRIGGER {
-        INPUT_VALID, INPUT_INVALID, SELECTION_FAILURE, SELECTION_SUCCESS, CONTINUE, FINISH
+        INPUT_VALID, INPUT_INVALID, SELECTION_FAILURE, SELECTION_SUCCESS, FINAL_SUCCESS, CONTINUE, FINISH
     }
 
-    private Map<List<Integer>, State> setRules() {
+    private static Map<List<Integer>, State> setRules() {
         Map<List<Integer>, State> result = new HashMap<>();
+        setInitialStateRules(result);
         setSelectionStateRules(result);
         setSuccessTestStateRules(result);
         setFinishStateRules(result);
         return result;
     }
 
-    private void setSelectionStateRules(Map<List<Integer>, State> rules){
+    private static void setInitialStateRules(Map<List<Integer>, State> rules) {
+        rules.put(List.of(State.INITIAL_STATE.ordinal(), TRIGGER.INPUT_INVALID.ordinal()),
+                State.INITIAL_STATE);
+    }
+
+    private static void setSelectionStateRules(Map<List<Integer>, State> rules) {
         rules.put(List.of(State.SELECTION_STATE.ordinal(), TRIGGER.INPUT_INVALID.ordinal()),
                 State.SELECTION_STATE);
         rules.put(List.of(State.SELECTION_STATE.ordinal(), TRIGGER.INPUT_VALID.ordinal()),
                 State.SUCCESS_TEST_STATE);
     }
-    private void setSuccessTestStateRules(Map<List<Integer>, State> rules){
+
+    private static void setSuccessTestStateRules(Map<List<Integer>, State> rules) {
+        rules.put(List.of(State.SUCCESS_TEST_STATE.ordinal(), TRIGGER.INPUT_INVALID.ordinal()),
+                State.SUCCESS_TEST_STATE);
         rules.put(List.of(State.SUCCESS_TEST_STATE.ordinal(), TRIGGER.SELECTION_FAILURE.ordinal()),
-                State.SELECTION_STATE);
+                State.FINISH_TEST_STATE);
         rules.put(List.of(State.SUCCESS_TEST_STATE.ordinal(), TRIGGER.SELECTION_SUCCESS.ordinal()),
+                State.SELECTION_STATE);
+        rules.put(List.of(State.SUCCESS_TEST_STATE.ordinal(), TRIGGER.FINAL_SUCCESS.ordinal()),
                 State.FINISH_TEST_STATE);
     }
-    private void setFinishStateRules(Map<List<Integer>, State> rules){
+
+    private static void setFinishStateRules(Map<List<Integer>, State> rules) {
+        rules.put(List.of(State.FINISH_TEST_STATE.ordinal(), TRIGGER.INPUT_INVALID.ordinal()),
+                State.FINISH_TEST_STATE);
         rules.put(List.of(State.FINISH_TEST_STATE.ordinal(), TRIGGER.CONTINUE.ordinal()),
                 State.SELECTION_STATE);
         rules.put(List.of(State.FINISH_TEST_STATE.ordinal(), TRIGGER.FINISH.ordinal()),
