a708ce572228b5ae0d686fd23b4ce7f689fbcf1e
jihwan <sgh002400@naver.com>
2022-11-22 13:16:04 +0900

    feat(BridgeController): 게임 시작 메시지 출력 기능 구현

---
+++ src/main/java/bridge/controller/BridgeController.java
@@ -0,0 +1,14 @@
+package bridge.controller;
+
+import bridge.view.OutputView;
+
+public class BridgeController {
+	public void run() {
+		printBridgeGameStart();
+
+	}
+
+	private void printBridgeGameStart() {
+		OutputView.printBridgeGameStart();
+	}
+}
