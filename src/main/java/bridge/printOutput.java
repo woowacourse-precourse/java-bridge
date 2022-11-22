90f86fc1908226f3b797ee98b251df52d8ef1e3a
wonseokjee <wsji9404@gmail.com>
2022-11-22 17:32:59 +0900

    Refactor(printOutput): 수정 enum 클래스
    문자열에 넣는 값은 고유클래스가 되면 문자취급하기 힘드므로 삭제하고 출력문만 관리

--- src/main/java/bridge/printOutput.java
+++ src/main/java/bridge/printOutput.java
@@ -1,14 +1,16 @@
 package bridge;
 
 public enum printOutput {
-    MOVE_POSSIBLE("O"),
-    MOVIE_IMPOSSIBLE("X"),
-    NOT_SELECTED(" "),
-    BRIDGE_START("["),
-    BRIDGE_END("]"),
-    BRIDGE_MIDDLE("|"),
-    IS_SUCCESS("게임 성공 여부: "),
-    ALL_TRY("총 시도한 횟수: ");
+
+    START_GAME("다리 건너기 게임을 시작합니다."),
+    INPUT_BRIDGE_SIZE("다리의 길이를 입력해주세요."),
+    SELECT_BRIDGE_UP_DOWN("이동할 칸을 선택해주세요. (위: U, 아래: D)"),
+    RESTART_OR_QUIT("게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)"),
+    GAME_RESULT("최종 게임 결과"),
+    IS_SUCCESS_SUCCESS("게임 성공 여부: 성공"),
+    IS_SUCCESS_FAIL("게임 성공 여부: 실패"),
+    ALL_TRY("총 시도한 횟수: "),
+    ;
     private final String gameOutput;
 
     public String getGameOutput() {
