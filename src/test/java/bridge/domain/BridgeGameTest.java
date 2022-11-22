0b1fdf9f311a473526556e7cea478ba962763850
leeyongjun <leeyongjun0310@naver.com>
2022-11-22 19:38:34 +0900

    test(bridge): 올바른 방향 이동하면 'NEXT'가 리턴되는지 확인하는 테스트 코드 작성

--- src/test/java/bridge/domain/BridgeGameTest.java
+++ src/test/java/bridge/domain/BridgeGameTest.java
@@ -1,14 +1,29 @@
 package bridge.domain;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
+import java.util.List;
+
 import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
 
 class BridgeGameTest {
+    private final BridgeGame bridgeGame = new BridgeGame();
 
+    @BeforeEach
+    void beforeEach() {
+        Player player = new Player();
+        List<String> bridge = Arrays.asList("U", "D", "D");
+        bridgeGame.initBridgeGame(player, bridge);
+    }
+
+    @DisplayName("올바른 방향으로 가면 'NEXT'가 리턴되는지 확인")
     @Test
-    void move() {
-
+    void move_올바른_방향_이동() {
+        assertThat(bridgeGame.move("U")).isEqualTo("NEXT");
     }
 
     @Test
