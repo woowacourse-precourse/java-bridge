093abb7d1c394f94f07f2fdee0dfd6a32eebe30e
JuSeong1130 <jsmoon1130@naver.com>
2022-11-22 17:12:50 +0900

    refactor(BridgeResult): 구조 개선 및 Map출력을 위한 메서드 개선

--- src/main/java/bridge/domain/result/BridgeResult.java
+++ src/main/java/bridge/domain/result/BridgeResult.java
@@ -1,14 +1,36 @@
 package bridge.domain.result;
 
+import bridge.domain.command.BridgeMoveCommand;
+import java.util.ArrayList;
+import java.util.List;
+
 public class BridgeResult {
+
+	private static final String SUCCESS = " O ";
+	private static final String FAIL = " X ";
+	private static final String BLANK = "   ";
 	private int countOfAttemps; //시도한 횟수
 	private boolean isSuccess;
-	private String map;
 
-	public BridgeResult() {
+	private List<String> bridges;
+	private List<List<String>> lists;
+
+
+	public BridgeResult(List<String> bridges) {
+		this.bridges = bridges;
 		this.countOfAttemps = 1;
 		this.isSuccess = false;
-		this.map = "";
+		init();
+	}
+	public void init() {
+		int size = BridgeMoveCommand.getSize();
+		lists = new ArrayList<>();
+		for (int i = 0; i < size; i++) {
+			lists.add(new ArrayList<>());
+		}
+	}
+	public List<List<String>> getLists() {
+		return lists;
 	}
 
 	public void addCountOfAttemps(int moveIndex) {
@@ -21,20 +43,31 @@ public class BridgeResult {
 		isSuccess = true;
 	}
 
-	public void updateMap(String map) {
-		this.map = map;
-	}
-
-
 	public int getCountOfAttemps() {
 		return countOfAttemps;
 	}
 
-	public boolean isSuccess() {
-		return isSuccess;
+	public String isSuccess() {
+		if(isSuccess){
+			return "성공";
+		}
+		return "실패";
 	}
 
-	public String getMap() {
-		return map;
+	public void addMaps(String command, int mapIndex) {
+		int index = BridgeMoveCommand.getNumber(command);
+		List<String> strings = lists.get(index);
+		strings.add(isEquals(mapIndex, command));
+		List<String> strings1 = lists.get(Math.abs(index - 1));
+		strings1.add(BLANK);
 	}
+
+	public String isEquals(int index, String command) {
+		String bridge = bridges.get(index);
+		if (bridge.equals(command)) {
+			return SUCCESS;
+		}
+		return FAIL;
+	}
+
 }
