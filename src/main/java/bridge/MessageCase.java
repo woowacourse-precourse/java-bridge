56409a7233fd15cc1b0fee956ec3642ad0a462f7
popomarin28@gmail.com <popomarin28@gmail.com>
2022-11-22 11:58:46 +0900

    feat(MessageCase) : enum으로 메세지Case 집합 구현

--- src/main/java/bridge/MessageCase.java
+++ src/main/java/bridge/MessageCase.java
@@ -1,10 +1,24 @@
-//package bridge;
-//
-//public enum MessageCase {
-//    START("다리 건너기 게임을 시작합니다.\n"),
-//    BRIDESIZE("\"다리의 길이를 입력해주세요.\""),
-//    MOVE("이동할 칸을 선택해주세요. (위: U, 아래: D)"),
-//    RESTART("게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)"),
-//    RESULT("최종 게임 결과"),
-//
-//}
+package bridge;
+
+public enum MessageCase {
+    START("다리 건너기 게임을 시작합니다.\n"),
+    BRIDESIZE("\"다리의 길이를 입력해주세요.\""),
+    MOVE("이동할 칸을 선택해주세요. (위: U, 아래: D)"),
+    RESTART("게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)"),
+    RESULT("최종 게임 결과"),
+    GAMESUCCESS("게임 성공 여부: 성공"),
+    GAMEFAILED("게임 성공 여부: 실패"),
+    GAMETRY("총 시도한 횟수: "),
+    LEFTBRACKET("["),
+    RIGHTBRACKET("]"),
+    DIVISION("|");
+
+    MessageCase(String message){
+        this.message = message;
+    }
+    private final String message;
+
+    public String getMessage() {
+        return message;
+    }
+}
