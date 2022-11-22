8024b8d2064b3dbee7c41810b3dec640e5061bd8
wjh2012 <wjh2012@naver.com>
2022-11-22 13:59:36 +0900

    refactor(validation): 다리 사이즈 검증기능 이동 before - controller: BridgeController after - domain: BridgeMaker

--- src/main/java/bridge/model/BridgeMaker.java
+++ src/main/java/bridge/model/BridgeMaker.java
@@ -6,33 +6,29 @@ import bridge.config.BridgeComponent;
 import java.util.ArrayList;
 import java.util.List;
 
-/**
- * 다리의 길이를 입력 받아서 다리를 생성해주는 역할을 한다.
- */
-
-/**
- * 제공된 BridgeMaker 클래스를 활용해 구현해야 한다.
- * BridgeMaker의 필드(인스턴스 변수)를 변경할 수 없다.
- * BridgeMaker의 메서드의 시그니처(인자, 이름)와 반환 타입은 변경할 수 없다.
- */
 public class BridgeMaker {
+
+    private static final int BRIDGE_MINIMUM = 3;
+    private static final int BRIDGE_MAXIMUM = 20;
 
     private final BridgeNumberGenerator bridgeNumberGenerator;
 
     public BridgeMaker(BridgeNumberGenerator bridgeNumberGenerator) {
         this.bridgeNumberGenerator = bridgeNumberGenerator;
     }
-
-    /**
-     * @param size 다리의 길이
-     * @return 입력받은 길이에 해당하는 다리 모양. 위 칸이면 "U", 아래 칸이면 "D"로 표현해야 한다.
-     */
     public List<String> makeBridge(int size) {
+        validateSize(size);
         List<String> bridge = new ArrayList<>();
         for (int i = 0; i < size; i++) {
             bridge.add(makeOneSquare());
         }
         return bridge;
+    }
+
+    private void validateSize(int size) {
+        if (size < BRIDGE_MINIMUM || size > BRIDGE_MAXIMUM) {
+            throw new IllegalArgumentException("다리 길이는 3부터 20 사이의 숫자여야 합니다.");
+        }
     }
 
     private String makeOneSquare() {
