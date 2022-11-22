b3723102b2e90a86043e6caf0e7597056fba6505
baksohyeon <bori12370@gmail.com>
2022-11-22 14:44:53 +0900

    feat: 오류 발생 시 다시 입력받도록 로직 추가

--- src/main/java/bridge/view/InputView.java
+++ src/main/java/bridge/view/InputView.java
@@ -7,28 +7,50 @@ import camp.nextstep.edu.missionutils.Console;
 public class InputView {
 
     InputHandlerImpl inputHandler;
+    int size;
+    String movement;
+    String gameCommand;
     public InputView() {
         this.inputHandler = new InputHandlerImpl();
     }
 
     public int readBridgeSize() {
-        System.out.println("다리 건너기 게임을 시작합니다.\n\n");
-        System.out.println("다리의 길이를 입력해주세요.");
-        String rawInput = Console.readLine();
-        return inputHandler.getBridgeSize(rawInput);
+            System.out.println("다리 건너기 게임을 시작합니다.\n");
+            System.out.println("다리의 길이를 입력해주세요.");
+        try {
+            String rawInput = Console.readLine();
+            this.size = inputHandler.getBridgeSize(rawInput);
+
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+            readBridgeSize();
+        }
+        return size;
     }
 
 
     public String readMoving() {
-        System.out.println("이동할 칸을 선택해주세요. (위: U, 아래: D)");
-        String rawInput = Console.readLine();
-        return inputHandler.getMovingCommand(rawInput);
+        try {
+            System.out.println("이동할 칸을 선택해주세요. (위: U, 아래: D)");
+            String rawInput = Console.readLine();
+            this.movement = inputHandler.getMovingCommand(rawInput);
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+            readMoving();
+        }
+        return movement;
     }
 
 
     public String readGameCommand() {
-        System.out.println("게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)");
-        String rawInput = Console.readLine();
-        return inputHandler.getGameCommand(rawInput);
+        try {
+            System.out.println("게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)");
+            String rawInput = Console.readLine();
+            this.gameCommand = inputHandler.getGameCommand(rawInput);
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+            readGameCommand();
+        }
+        return gameCommand;
     }
 }
