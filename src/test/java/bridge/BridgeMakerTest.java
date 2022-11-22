b185c0c2e8e0ece757c11f4ebd715b7ce92e02c1
This2sho <zx00018@naver.com>
2022-11-22 16:53:59 +0900

    style(BridgeMakerTest): 테스트명 변경

--- src/test/java/bridge/BridgeMakerTest.java
+++ src/test/java/bridge/BridgeMakerTest.java
@@ -9,7 +9,7 @@ import org.junit.jupiter.api.Test;
 class BridgeMakerTest {
 	@Test
 	@DisplayName("입력된 길이로 다리가 만들어지는지 테스트")
-	void 다리_입력_길트_만들기_테스트() {
+	void 다리_입력_길이_유효성_테스트() {
 		//given
 		BridgeMaker bridgeMaker = new BridgeMaker(new BridgeRandomNumberGenerator());
 		//when
