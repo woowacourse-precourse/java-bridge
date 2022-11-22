1b7aac1c0c79c9c9b525ff5510eeeeb5624c20ee
dener8 <8dener@gmail.com>
2022-11-22 12:28:28 +0900

    refactor: 게임 안내 메시지(오류 메시지 제외)를 담는 클래스를 enum으로 변경

--- src/main/java/bridge/OutputView.java
+++ src/main/java/bridge/OutputView.java
@@ -1,16 +1,32 @@
 package bridge;
 
+import java.util.List;
+import java.util.StringJoiner;
+import java.util.stream.Collectors;
+
+import static message.GeneralMessage.*;
+
 /**
  * 사용자에게 게임 진행 상황과 결과를 출력하는 역할을 한다.
  */
 public class OutputView {
+    StringJoiner stringJoiner = new StringJoiner(" | ", "[ ", " ]" )
 
     /**
      * 현재까지 이동한 다리의 상태를 정해진 형식에 맞춰 출력한다.
      * <p>
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void printMap() {
+    public void printMap(List<List<String>> bridges) {
+        // getter가 여기서 필요한 것 같다..
+        for (List<String> bridge : bridges) {
+            System.out.println(convertListToStringBridge(bridge));
+        }
+    }
+
+    public String convertListToStringBridge(List<String> bridge) {
+        String stringBridge = bridge.stream().collect(Collectors.joining(" | ", "[ ", " ]"));
+        return stringBridge;
     }
 
     /**
@@ -19,5 +35,9 @@ public class OutputView {
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
     public void printResult() {
+        System.out.println(GAME_RESULT);
+//        printMap(); // 최종 다리 모양 출력
+        System.out.println(SUCCESS_OR_NOT); // + 실패 or 성공
+        System.out.println(NUMBER_OF_TRY); // + 횟수(int)
     }
 }
--- src/main/java/message/GeneralMessage.java
+++ src/main/java/message/GeneralMessage.java
@@ -1,11 +1,21 @@
-package view;
+package message;
 
-public class GeneralMessage {
-    public static final String GAME_START =  "다리 건너기 게임을 시작합니다.\n";
-    public static final String INPUT_BRIDGE_LENGTH = "다리의 길이를 입력해주세요.";
-    public static final String SELECT_UP_OR_DOWN = "이동할 칸을 선택해주세요. (위: U, 아래: D)";
-    public static final String RETRY_OR_QUIT = "게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)";
-    public static final String GAME_RESULT = "최종 게임 결과";
-    public static final String SUCCESS_OR_NOT = "게임 성공 여부:";
-    public static final String NUMBER_OF_TRY = "총 시도한 횟수:";
+public enum GeneralMessage {
+    GAME_START("다리 건너기 게임을 시작합니다.\n"),
+    INPUT_BRIDGE_LENGTH("다리의 길이를 입력해주세요."),
+    SELECT_UP_OR_DOWN("이동할 칸을 선택해주세요. (위: U, 아래: D)"),
+    RETRY_OR_QUIT("게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)"),
+    GAME_RESULT("최종 게임 결과"),
+    SUCCESS_OR_NOT("게임 성공 여부:"),
+    NUMBER_OF_TRY("총 시도한 횟수:");
+
+    private String message;
+
+    GeneralMessage(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
 }
