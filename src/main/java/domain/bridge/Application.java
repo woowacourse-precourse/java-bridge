5003da7e0c46cfdf4fd6575f82d80864a09e5e9d
anso33 <hyeon131812@gmail.com>
2022-11-22 19:00:15 +0900

    [FEAT] application

--- src/main/java/domain/bridge/Application.java
+++ src/main/java/domain/bridge/Application.java
@@ -1,8 +1,12 @@
-package bridge;
+package domain.bridge;
+
+import domain.Game;
 
 public class Application {
 
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        Game game = new Game();
+        game.play();
+        game.result();
     }
 }
