43517105c18b63b146ad4ff19107e6c889e812c8
gmu1026 <gmu1026@gmail.com>
2022-11-22 20:28:09 +0900

    feat: 게임 진행 상황을 저장 위한 컬렉션 작성

--- src/main/java/bridge/GameMap.java
+++ src/main/java/bridge/GameMap.java
@@ -4,18 +4,21 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class GameMap {
+    private static final String UP_COMMAND = "U";
+    private static final String DOWN_COMMAND = "D";
+    private static final String BLANK_MARK = " ";
     private final List<String> upstairBridge = new ArrayList<>();
     private final List<String> downstairBridge = new ArrayList<>();
 
     public void add(String moveCommand, String moveResult) {
-        if (moveCommand.equals("U")) {
+        if (moveCommand.equals(UP_COMMAND)) {
             upstairBridge.add(moveResult);
-            downstairBridge.add(" ");
+            downstairBridge.add(BLANK_MARK);
         }
 
-        if (moveCommand.equals("D")) {
+        if (moveCommand.equals(DOWN_COMMAND)) {
             downstairBridge.add(moveResult);
-            upstairBridge.add(" ");
+            upstairBridge.add(BLANK_MARK);
         }
     }
 
@@ -25,5 +28,10 @@ public class GameMap {
 
     public List<String> getDownstairBridge() {
         return downstairBridge;
+    }
+
+    public void clear() {
+        upstairBridge.clear();
+        downstairBridge.clear();
     }
 }
