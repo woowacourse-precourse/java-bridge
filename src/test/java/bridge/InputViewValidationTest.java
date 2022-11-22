bb46f1a10d62d1ad66f1be1a14b3af81601bd3cb
wonseok <tkddls23@konkuk.ac.kr>
2022-11-22 13:25:20 +0900

    test: 입력값이 숫자인지 검증한다.

---
+++ src/test/java/bridge/InputViewValidationTest.java
@@ -0,0 +1,19 @@
+package bridge;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
+
+public class InputViewValidationTest {
+
+    @DisplayName("예외 처리 : 입력값이 숫자가 아닐때 에러 발생 검증")
+    @ParameterizedTest
+    @ValueSource(strings = {"10h","hello"," "})
+    void checkNotValidDigitInputValidation(String input) {
+        assertThatIllegalArgumentException()
+                .isThrownBy(() -> InputViewValidation.isDigitInput(input));
+    }
+}
