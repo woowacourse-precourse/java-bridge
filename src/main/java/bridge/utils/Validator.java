f2b5dcafb06965eb0608f5ea181e63f63e467b13
jinny-codes <77732679+jinny-codes@users.noreply.github.com>
2022-11-22 14:29:08 +0900

    fix(Validator): error message enum 클래스에 따른 변경사항 반영

--- src/main/java/bridge/utils/Validator.java
+++ src/main/java/bridge/utils/Validator.java
@@ -1,39 +1,36 @@
-package bridge;
+package bridge.utils;
+
+import bridge.constants.ErrorMessage;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 public class Validator {
-    private final String ERROR_MESSAGE = "[ERROR] ";
-    private final String ERROR_NOT_INT = ERROR_MESSAGE + "다리 길이는 숫자여야 합니다.";
-    private final String ERROR_NOT_VALID_LENGTH = ERROR_MESSAGE + "다리 길이는 3부터 20 사이의 숫자여야 합니다.";
-    private final String ERROR_NOT_VALID_MOVE = ERROR_MESSAGE + "이동할 칸은 U 또는 D여야 합니다.";
-    private final String ERROR_NOT_VALID_COMMAND = ERROR_MESSAGE + "게임 재시도 여부는 R 또는 Q여야 합니다.";
     public int isInt(String input) {
         try {
             Integer.parseInt(input);
         } catch (NumberFormatException e){
-            throw new IllegalArgumentException(ERROR_NOT_INT);
+            throw new IllegalArgumentException(ErrorMessage.ERROR_NOT_INT.getDescription());
         }
         return Integer.parseInt(input);
     }
     public int isValidLength(int length) {
         if (length < 3 || length > 20) {
-            throw new IllegalArgumentException(ERROR_NOT_VALID_LENGTH);
+            throw new IllegalArgumentException(ErrorMessage.ERROR_NOT_VALID_LENGTH.getDescription());
         }
         return length;
     }
     public void isValidMove(String move) {
         List<String> validMoves = new ArrayList<>(Arrays.asList("U", "D"));
         if (!(validMoves.contains(move.toUpperCase()))) {
-            throw new IllegalArgumentException(ERROR_NOT_VALID_MOVE);
+            throw new IllegalArgumentException(ErrorMessage.ERROR_NOT_VALID_MOVE.getDescription());
         }
     }
     public void isValidCommand(String command) {
         List<String> validCommands = new ArrayList<>(Arrays.asList("R", "Q"));
         if (!(validCommands.contains(command.toUpperCase()))) {
-            throw new IllegalArgumentException(ERROR_NOT_VALID_COMMAND);
+            throw new IllegalArgumentException(ErrorMessage.ERROR_NOT_VALID_COMMAND.getDescription());
         }
     }
 }
