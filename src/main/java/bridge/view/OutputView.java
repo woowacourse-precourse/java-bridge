cf4eba0cd8f58d8a4be116ad7d92c2540de54ccd
SeongbinPark <binni0515@gmail.com>
2022-11-22 18:46:54 +0900

    feat(OutputView): add printMap method

--- src/main/java/bridge/view/OutputView.java
+++ src/main/java/bridge/view/OutputView.java
@@ -1,23 +1,13 @@
 package bridge.view;
 
-/**
- * 사용자에게 게임 진행 상황과 결과를 출력하는 역할을 한다.
- */
+import bridge.Map;
+import java.util.List;
+
 public class OutputView {
 
-    /**
-     * 현재까지 이동한 다리의 상태를 정해진 형식에 맞춰 출력한다.
-     * <p>
-     * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
-     */
-    public void printMap() {
-    }
-
-    /**
-     * 게임의 최종 결과를 정해진 형식에 맞춰 출력한다.
-     * <p>
-     * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
-     */
-    public void printResult() {
+    public void printMap(List<String> upSide, List<String> downSide, Map map) {
+        System.out.println(map.format(upSide));
+        System.out.println(map.format(downSide));
+        System.out.println();
     }
 }
