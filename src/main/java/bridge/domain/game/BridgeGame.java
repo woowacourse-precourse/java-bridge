f4f42e82f021dc300e4d085f03ab1abac731f4f1
semi-cloud <sksmsrkdtpal@naver.com>
2022-11-22 11:28:45 +0900

    style(Bridge): 불필요한 주석들 제거

--- src/main/java/bridge/domain/game/BridgeGame.java
+++ src/main/java/bridge/domain/game/BridgeGame.java
@@ -4,9 +4,7 @@ import bridge.domain.bridge.Bridge;
 import bridge.domain.bridge.Square;
 import bridge.domain.move.MoveResult;
 
-/**
- * 다리 건너기 게임을 관리하는 클래스
- */
+
 public class BridgeGame {
 
     private GameStatus gameStatus = GameStatus.PROGRESS;
@@ -22,11 +20,6 @@ public class BridgeGame {
         retryCount = 1;
     }
 
-    /**
-     * 사용자가 칸을 이동할 때 사용하는 메서드
-     * <p>
-     * 이동을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
-     */
     public SquareResult move(Square userMove) {
         boolean movable = bridge.canMoveForward(userMove, position);
         if (movable) {
@@ -35,11 +28,6 @@ public class BridgeGame {
         return new SquareResult(userMove, MoveResult.of(movable));
     }
 
-    /**
-     * 사용자가 게임을 다시 시도할 때 사용하는 메서드
-     * <p>
-     * 재시작을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
-     */
     public void retry() {
         position = 0;
         retryCount++;
--- src/main/java/bridge/view/InputView.java
+++ src/main/java/bridge/view/InputView.java
@@ -3,9 +3,7 @@ package bridge.view;
 import bridge.exception.BridgeError;
 import camp.nextstep.edu.missionutils.Console;
 
-/**
- * 사용자로부터 입력을 받는 역할을 한다.
- */
+
 public class InputView {
 
     private static final String INPUT_BRIDGE_LENGTH = "다리의 길이를 입력해주세요.";
@@ -13,9 +11,6 @@ public class InputView {
     private static final String INPUT_RETRY = "게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)";
     private static final String REG_XP_DIGIT = "[0-9]+";
 
-    /**
-     * 다리의 길이를 입력받는다.
-     */
     public int readBridgeSize() {
         System.out.println(INPUT_BRIDGE_LENGTH);
         String size = Console.readLine();
@@ -29,17 +24,11 @@ public class InputView {
         }
     }
 
-    /**
-     * 사용자가 이동할 칸을 입력받는다.
-     */
     public String readMoving() {
         System.out.println(INPUT_MOVE_TYPE);
         return Console.readLine();
     }
 
-    /**
-     * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
-     */
     public String readGameCommand() {
         System.out.println(INPUT_RETRY);
         return Console.readLine();
