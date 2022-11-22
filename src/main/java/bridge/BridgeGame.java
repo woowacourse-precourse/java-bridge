5122830cf20f7b07e06f0fc982638e0766162bac
JooDukHan <bfinecpa1@naver.com>
2022-11-22 19:03:31 +0900

    refactor: Player클래스 생성 및 리팩토링
    BridgeGame에서 구현했던 함수를 Player 클래스의 내부함수로 구현, 실패했는지를 알기 위해서 BridgeGame.retry 조건절 추가

--- src/main/java/bridge/BridgeGame.java
+++ src/main/java/bridge/BridgeGame.java
@@ -16,61 +16,23 @@ public class BridgeGame {
      * <p>
      * 이동을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void move(List<String> rightBridge, PlayerBridge playerBridge) {
-        for (String rightRoot : rightBridge) {
-            String moving = inputView.readMoving();
-            compareRightRoot(playerBridge, rightRoot, moving);
-            compareWrongRoot(playerBridge, rightRoot, moving);
-        }
-    }
+    private void move(Player player) {
+        player.moving(inputView, outputView);
 
-    private static void compareWrongRoot(PlayerBridge playerBridge, String rightRoot, String moving) {
-        if(isWrongAndUpRoot(rightRoot, moving)){
-            playerBridge.addUpRoot("X");
-            playerBridge.addDownRoot(" ");
-        }
-        if (isWrongAndDownRoot(rightRoot, moving)){
-            playerBridge.addDownRoot("X");
-            playerBridge.addUpRoot(" ");
-        }
     }
-    private static void compareRightRoot(PlayerBridge playerBridge, String rightRoot, String moving) {
-        if(isRightAndUpRoot(rightRoot, moving)) {
-            playerBridge.addUpRoot(moving);
-            playerBridge.addDownRoot(" ");
-        }
-        if (isRightAndDownRoot(rightRoot, moving)){
-            playerBridge.addDownRoot("O");
-            playerBridge.addUpRoot(" ");
-        }
-    }
-    private static boolean isWrongAndDownRoot(String rightRoot, String moving) {
-        return !moving.equals(rightRoot) && moving.equals("D");
-    }
-
-    private static boolean isWrongAndUpRoot(String rightRoot, String moving) {
-        return !moving.equals(rightRoot) && moving.equals("U");
-    }
-
-    private static boolean isRightAndDownRoot(String rightRoot, String moving) {
-        return moving.equals(rightRoot) && moving.equals("D");
-    }
-
-    private static boolean isRightAndUpRoot(String rightRoot, String moving) {
-        return moving.equals(rightRoot) && moving.equals("U");
-    }
-
 
     /**
      * 사용자가 게임을 다시 시도할 때 사용하는 메서드
      * <p>
      * 재시작을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public boolean retry() {
-        String gameCommand = inputView.readGameCommand();
-        if (gameCommand.equals("R")) return true;
-        if (gameCommand.equals("Q")) return false;
-        return true;
+    public boolean retry(Player player) {
+        if(player.fail()){
+            String gameCommand = inputView.readGameCommand();
+            if (gameCommand.equals("R")) return true;
+            if (gameCommand.equals("Q")) return false;
+        }
+        return false;
     }
 
 }
---
+++ src/main/java/bridge/Player.java
@@ -0,0 +1,82 @@
+package bridge;
+
+import java.util.List;
+
+public class Player {
+
+    private int coinUsed = 0;
+    private PlayerBridge playerBridge;
+    private List<String> rightBridge;
+
+    public void makeNewPlayerBridge(){
+        this.playerBridge = new PlayerBridge();
+    }
+
+    public void makeRightBridge(int bridgeSize) {
+        this.rightBridge = new BridgeMaker(new BridgeRandomNumberGenerator()).makeBridge(bridgeSize);
+    }
+
+    public void useCoin(){
+        coinUsed+=1;
+    }
+
+    public void moving(InputView inputView, OutputView outputView) {
+        for (String rightRoot : rightBridge) {
+            String moving = inputView.readMoving();
+            if(compareRightRoot(rightRoot, moving)){
+                outputView.printMap(playerBridge);
+                continue;}
+            if(compareWrongRoot(rightRoot, moving)){
+                outputView.printMap(playerBridge);
+                break;}}
+    }
+
+
+    private boolean compareRightRoot(String rightRoot, String moving) {
+        if(isRightAndUpRoot(rightRoot, moving)) {
+            playerBridge.addUpRootRight();
+            return true;}
+        if (isRightAndDownRoot(rightRoot, moving)){
+            playerBridge.addDownRootRight();
+            return true;}
+        return false;
+    }
+
+    private boolean compareWrongRoot(String rightRoot, String moving) {
+        if(isWrongAndUpRoot(rightRoot, moving)){
+            playerBridge.addUpRootWrong();
+            return true;}
+        if (isWrongAndDownRoot(rightRoot, moving)){
+            playerBridge.addDownRootWrong();
+            return true;}
+        return false;}
+
+    private boolean isWrongAndDownRoot(String rightRoot, String moving) {
+        return !moving.equals(rightRoot) && moving.equals("D");
+    }
+
+    private boolean isWrongAndUpRoot(String rightRoot, String moving) {
+        return !moving.equals(rightRoot) && moving.equals("U");
+    }
+
+    private boolean isRightAndDownRoot(String rightRoot, String moving) {
+        return moving.equals(rightRoot) && moving.equals("D");
+    }
+
+    private boolean isRightAndUpRoot(String rightRoot, String moving) {
+        return moving.equals(rightRoot) && moving.equals("U");
+    }
+
+
+    public boolean fail() {
+        return !playerBridge.successRoot();
+    }
+
+    public boolean success(){
+        return playerBridge.successRoot();
+    }
+
+    public PlayerBridge getPlayerBridge(){return playerBridge;}
+
+    public int getCoinUsed(){return coinUsed;}
+}
