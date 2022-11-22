c3dd719eabcb4a0f47217b9bc521ffae95b0242b
ckyeon <qsky8520@gmail.com>
2022-11-22 21:06:24 +0900

    feat(Player): add Player class

---
+++ src/main/java/bridge/domain/Player.java
@@ -0,0 +1,14 @@
+package bridge.domain;
+
+import java.util.List;
+
+public class Player {
+
+    private final List<String> map;
+    private int gameAttempts;
+
+    public Player(List<String> map, int gameAttempts) {
+        this.map = map;
+        this.gameAttempts = gameAttempts;
+    }
+}
