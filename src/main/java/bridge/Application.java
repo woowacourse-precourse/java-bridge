c8337339ed20583507807fa2e656ca50533777fd
jay <jaewhan232@naver.com>
2022-11-22 22:48:58 +0900

    Implement : BridgeGame 실행코드 추가

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -4,5 +4,8 @@ public class Application {
 
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+
+        BridgeGameController controller = new BridgeGameController();
+        controller.runGame();
     }
 }
