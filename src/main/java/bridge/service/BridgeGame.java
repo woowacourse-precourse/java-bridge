e218a8933c5ae0ac8c37cb54fb580dba0d453b7b
im-shung <seoyoung1113@gmail.com>
2022-11-22 22:09:43 +0900

    refactor: :recycle: 현재 다리의 건널 수 있는 칸을 알 수 있는 기능 리팩토링

--- src/main/java/bridge/service/BridgeGame.java
+++ src/main/java/bridge/service/BridgeGame.java
@@ -34,11 +34,7 @@ public class BridgeGame {
      * 이동을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
     public void move(String playerMoving) {
-        rounds.add(new Round(playerMoving, bridge.get(getCurrentNumberOfRounds())));
-    }
-
-    public int getCurrentNumberOfRounds() {
-        return rounds.size();
+        rounds.add(new Round(playerMoving, getCurrentBridgeMoving()));
     }
 
     public boolean isCurrentRoundResultFailure() {
@@ -54,11 +50,12 @@ public class BridgeGame {
         rounds.clear();
     }
 
+    public String getCurrentBridgeMoving() {
+        return bridge.get(rounds.size());
+    }
+
     public List<Round> getRounds() {
         return rounds;
     }
 
-    public int getBridgeSize() {
-        return bridge.size();
-    }
 }
