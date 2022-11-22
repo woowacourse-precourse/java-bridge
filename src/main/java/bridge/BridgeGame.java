111ed079e1384380e2edb5e3dac73d10e06266a2
changeme4585 <benscience@naver.com>
2022-11-22 16:13:25 +0900

    add: 재시작 여부 확인 기능구현

--- src/main/java/bridge/BridgeGame.java
+++ src/main/java/bridge/BridgeGame.java
@@ -22,6 +22,25 @@ public class BridgeGame {
      * <p>
      * 재시작을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void retry() {
+    public String[] retry(String flag,int cnt) {
+        String [] stopFlag = new String[2];
+        if (flag.equals("Q")) {
+            qCheck(stopFlag,cnt);
+        }
+        if (flag.equals("R")){
+            rCheck(stopFlag);
+        }
+        return stopFlag;
+    }
+    public String[] qCheck (String[] stopFlag,int cnt) {
+        System.out.println("Q");
+        stopFlag[0]="종료";
+        stopFlag[1]=String.valueOf(cnt);
+        return stopFlag;
+    }
+    public String[] rCheck (String[] stopFlag) {
+        System.out.println("R");
+        stopFlag[0]="다시";
+        return stopFlag;
     }
 }
