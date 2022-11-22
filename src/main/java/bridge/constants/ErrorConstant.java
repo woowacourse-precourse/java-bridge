c56154aa784299669f500c45256138043cf7e997
josolha <josolha@nate.com>
2022-11-22 17:05:12 +0900

    style(ErrorConstant) : 우테코 코드 스타일 적용

--- src/main/java/bridge/constants/ErrorConstant.java
+++ src/main/java/bridge/constants/ErrorConstant.java
@@ -2,10 +2,10 @@ package bridge.constants;
 
 public enum ErrorConstant {
     SIZE_EXCEPTION("[ERROR] 다리 길이는 3부터 20 사이의 숫자여야 합니다."),
-    NUMBER_ERROR ("[ERROR] 숫자를 입력해주세요."),
-    RANGE_ERROR ("[ERROR] 다리 길이는 3~20 사이의 숫자를 입력해주세요."),
-    UPDOWN_ERROR ("[ERROR] U와 D 중 입력해주세요."),
-    RETRY_ERROR ("[ERROR] R과 Q 중 입력해주세요.");
+    NUMBER_ERROR("[ERROR] 숫자를 입력해주세요."),
+    RANGE_ERROR("[ERROR] 다리 길이는 3~20 사이의 숫자를 입력해주세요."),
+    UPDOWN_ERROR("[ERROR] U와 D 중 입력해주세요."),
+    RETRY_ERROR("[ERROR] R과 Q 중 입력해주세요.");
     private String exception;
 
     ErrorConstant(String exception) {
@@ -15,7 +15,8 @@ public enum ErrorConstant {
     public String getException() {
         return exception;
     }
-    public void printException(){
+
+    public void printException() {
         System.out.println(this.exception);
     }
 }
