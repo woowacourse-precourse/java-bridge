35a126559bd3a35a8dee7887334125d2a34e2ea1
juno-junho <ssmm0205@naver.com>
2022-11-22 22:10:48 +0900

    refactor: refactor class InputView
    inputValueConverter 생성

--- src/main/java/bridge/view/InputView.java
+++ src/main/java/bridge/view/InputView.java
@@ -1,31 +1,59 @@
 package bridge.view;
 
-import bridge.constant.InputMessage;
+import bridge.constant.GameCommand;
+import bridge.constant.MoveCommand;
+import bridge.domain.BridgeSizeValidator;
 import camp.nextstep.edu.missionutils.Console;
+
+import java.util.function.Supplier;
 
 public class InputView {
 
-    /**
-     * 다리의 길이를 입력받는다.
-     */
-    public String readBridgeSize() {
-        System.out.println(InputMessage.INPUT_BRIDGE_LENGTH_MESSAGE.getMessage());
+    private static final String ERROR_PREFIX = "[ERROR] ";
+
+    private final InputViewConverter inputValueConverter;
+
+    public InputView(InputViewConverter inputValueConverter) {
+        this.inputValueConverter = inputValueConverter;
+    }
+
+    public BridgeSizeValidator bridgeSize() {
+        return repeatInputUntilSuccess(() ->
+                inputValueConverter.bridgeSize(valueForBridgeSize()));
+    }
+
+    private String valueForBridgeSize() {
+        System.out.println("다리의 길이를 입력해주세요.");
         return Console.readLine();
     }
 
-    /**
-     * 사용자가 이동할 칸을 입력받는다.
-     */
-    public String readMoving() {
-        System.out.println(InputMessage.INPUT_CHOOSE_LOCATION_MESSAGE.getMessage());
+    public MoveCommand moveCommand() {
+        return repeatInputUntilSuccess(() ->
+                inputValueConverter.moveCommands(valueForMoveCommand()));
+    }
+
+    private String valueForMoveCommand() {
+        System.out.println("이동할 칸을 선택해주세요. (위: U, 아래: D)");
         return Console.readLine();
     }
 
-    /**
-     * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
-     */
-    public String readGameCommand() {
-        System.out.println(InputMessage.INPUT_RETRY_OR_QUIT_MESSAGE.getMessage());
+    public GameCommand gameCommand() {
+        return repeatInputUntilSuccess(() ->
+                inputValueConverter.gameCommands(valueForGameCommand()));
+    }
+
+    private String valueForGameCommand() {
+        System.out.println("게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)");
         return Console.readLine();
+    }
+
+    private <T> T repeatInputUntilSuccess(Supplier<T> supplier) {
+        while (true) {
+            try {
+                return supplier.get();
+            } catch (IllegalArgumentException e) {
+                System.out.println(ERROR_PREFIX + e.getMessage());
+            }
+        }
     }
 }
