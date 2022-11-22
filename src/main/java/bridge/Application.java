95772787cdaa81f66563abceac19d9fb48c0ca57
mrbonk97 <mrbonk97@gmail.com>
2022-11-22 14:59:14 +0900

    feat: 에러 처리
    인풋 UI 에러처리

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -8,7 +8,13 @@ public class Application {
     public static void main(String[] args) {
         System.out.println("다리 건너기 게임을 시작합니다.\n");
 
-        int size = inputView.readBridgeSize();
+        int size;
+
+        try{
+            size = inputView.readBridgeSize();
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
 
         // TODO: 프로그램 구현
     }
