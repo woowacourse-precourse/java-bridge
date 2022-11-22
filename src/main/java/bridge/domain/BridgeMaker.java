98b380684445b9fb9c5ebb08ef0fe30b15b3d1f9
SEONGJOON KIM <jobcho6320@naver.com>
2022-11-22 17:42:00 +0900

    test: 다리 생성 테스트 케이스 추가

--- src/main/java/bridge/domain/BridgeMaker.java
+++ src/main/java/bridge/domain/BridgeMaker.java
@@ -1,6 +1,7 @@
 package bridge.domain;
 
 import bridge.BridgeNumberGenerator;
+import bridge.Validator;
 
 import java.util.ArrayList;
 import java.util.List;
---
+++ src/test/java/bridge/domain/BridgeMakerTest.java
@@ -0,0 +1,48 @@
+package bridge.domain;
+
+import bridge.BridgeRandomNumberGenerator;
+import bridge.Validator;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+
+class BridgeMakerTest {
+
+    @DisplayName("다리 생성 시 U, D 값만 포함되어 있는지")
+    @Test
+    void createRandomBridge() {
+        BridgeRandomNumberGenerator bridgeRandomNumberGenerator = new BridgeRandomNumberGenerator();
+        BridgeMaker bridgeMaker = new BridgeMaker(bridgeRandomNumberGenerator);
+        List<String> bridge = bridgeMaker.makeBridge(3);
+
+        Assertions.assertThat(bridge).contains("U", "D");
+    }
+
+    @DisplayName("다리 생성 시 U, D 이외의 값이 포함되어 있으면 예외처리를 하는지")
+    @Test
+    void checkRandomBridge() {
+        Assertions.assertThatThrownBy(()-> Validator.validateMovingWord("H"))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("숫자 생성기에서 0과 1의 숫자만 생성되는지")
+    @Test
+    void checkNumberGenerateIncludeZeroAndOne() {
+        BridgeRandomNumberGenerator bridgeRandomNumberGenerator = new BridgeRandomNumberGenerator();
+        HashSet<Integer> randomNumber = new HashSet<>();
+        while (true) {
+            if (randomNumber.size() == 2) {
+                break;
+            }
+            randomNumber.add(bridgeRandomNumberGenerator.generate());
+        }
+        Assertions.assertThat(randomNumber).contains(0,1);
+    }
+
+}
