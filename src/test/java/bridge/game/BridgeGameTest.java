b4fe056f15eab91f5d52680682755d6bafb66361
jaehong <println@kakao.com>
2022-11-22 10:21:10 +0900

    test (BridgeGameTest) : 중복된 테스트코드 제거

--- src/test/java/bridge/game/BridgeGameTest.java
+++ src/test/java/bridge/game/BridgeGameTest.java
@@ -8,46 +8,50 @@ import bridge.domain.bridge.maker.BridgeMaker;
 import bridge.domain.bridge.maker.BridgeMakerFake;
 import bridge.domain.code.BridgePosition;
 import bridge.utils.TestUtils;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayNameGeneration;
 import org.junit.jupiter.api.DisplayNameGenerator;
 import org.junit.jupiter.api.Test;
 
 @DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
 class BridgeGameTest {
+    private BridgeGame bridgeGame;
 
+    @BeforeEach
+    void setUp() {
+        var fakeGenerate = new BridgeMakerFake(TestUtils.convertStringToListNumber("1,0,1,0"));
+        var makeBridge = new BridgeMaker(fakeGenerate);
+        var bridgeSize = new BridgeSize("4");
+        var bridge = new Bridge(makeBridge, bridgeSize);
+       
+        this.bridgeGame = new BridgeGame(bridge);
+    }
+
+    @AfterEach
+    void tearDown() {
+        this.bridgeGame = null;
+    }
 
     @Test
     void 게임이_모두_완료가_되면_클리어_상태를_출력합니다() {
-        var makeBridge = new BridgeMaker(
-                new BridgeMakerFake(TestUtils.convertStringToListNumber("1,0,1,0"))
-        );
-        var bridgeSize = new BridgeSize("4");
 
-        var bridge = new Bridge(makeBridge, bridgeSize);
-        var game = new BridgeGame(bridge);
+        bridgeGame.move(BridgePosition.UP);
+        bridgeGame.move(BridgePosition.DOWN);
+        bridgeGame.move(BridgePosition.UP);
+        bridgeGame.move(BridgePosition.DOWN);
 
-        game.move(BridgePosition.UP);
-        game.move(BridgePosition.DOWN);
-        game.move(BridgePosition.UP);
-        game.move(BridgePosition.DOWN);
+        var gameStatus = bridgeGame.getGameStatus();
 
-        var gameStatus = game.getGameStatus();
         assertThat(gameStatus.getCode()).isEqualTo("성공");
     }
 
     @Test
     void 게임을_실패하면_실패상태가_됩니다() {
-        var makeBridge = new BridgeMaker(
-                new BridgeMakerFake(TestUtils.convertStringToListNumber("1,0,1,0"))
-        );
-        var bridgeSize = new BridgeSize("4");
+        bridgeGame.move(BridgePosition.DOWN);
 
-        var bridge = new Bridge(makeBridge, bridgeSize);
-        var game = new BridgeGame(bridge);
+        var gameStatus = bridgeGame.getGameStatus();
 
-        game.move(BridgePosition.DOWN);
-
-        var gameStatus = game.getGameStatus();
         assertThat(gameStatus.getCode()).isEqualTo("실패");
     }
 
