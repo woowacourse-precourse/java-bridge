f6397f5fbb15e1768d7fc3c1e0cd267299badf3a
JJONSOO <pjs970616@naver.com>
2022-11-22 21:53:37 +0900

    feat : 칸 이동 메서드

--- src/main/java/bridge/BridgeGame.java
+++ src/main/java/bridge/BridgeGame.java
@@ -19,5 +19,13 @@ public class BridgeGame {
     public void setBridge(){bridge = bridgeMaker.makeBridge(bridgeSize);}
     public static int getUserInputSize(){return userInput.size();}
     public static int getBridgeSize(){return bridgeSize;}
-
+    /**
+     * 사용자가 칸을 이동할 때 사용하는 메서드
+     * <p>
+     * 이동을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
+     */
+    public boolean move(String moving) {
+        userInput.add(moving);
+        return Objects.equals(bridge.get(userInput.size() - 1), moving);
+    }
 }
