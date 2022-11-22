0c8682f42606e1389f66ec9c621168bc4f6f2396
김형기 <khk211113@naver.com>
2022-11-22 10:37:58 +0900

    feat(Application): 매인 스레드 실행 추가

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -1,8 +1,26 @@
 package bridge;
 
+import bridge.controller.BridgeGameController;
+import bridge.view.InputView;
+import bridge.view.OutputView;
+
 public class Application {
 
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        BridgeMaker bridgeMaker = new BridgeMaker(new BridgeRandomNumberGenerator());
+        InputView inputView = new InputView();
+        OutputView outputView = new OutputView();
+        BridgeGameController bridgeGameController = new BridgeGameController(bridgeMaker, inputView, outputView);
+
+        runControllerWithPrintError(bridgeGameController, outputView);
+    }
+
+    private static void runControllerWithPrintError(BridgeGameController bridgeGameController, OutputView outputView) {
+        try {
+            bridgeGameController.run();
+        } catch (IllegalArgumentException e) {
+            outputView.printErrorMessage(e);
+            throw e;
+        }
     }
 }
