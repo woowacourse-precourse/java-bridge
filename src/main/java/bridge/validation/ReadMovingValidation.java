05b0a33d3af2e77a988559217280e3d523794093
desfox <desfox99@naver.com>
2022-11-22 18:44:23 +0900

    refactor(ReadMovingValidation): 에러 문구 ErrorMessage Enum으로 대체

--- src/main/java/bridge/validation/ReadMovingValidation.java
+++ src/main/java/bridge/validation/ReadMovingValidation.java
@@ -1,12 +1,14 @@
 package bridge.validation;
 
+import static bridge.validation.ErrorMessage.*;
+
 public class ReadMovingValidation {
     private static final String DOWN_MOVING_COMMAND = "D";
     private static final String UP_MOVING_COMMAND = "U";
 
     public void validateInputFormat(String movingCommand) {
         if ((! movingCommand.equals(DOWN_MOVING_COMMAND)) && (! movingCommand.equals(UP_MOVING_COMMAND))) {
-            throw new IllegalArgumentException("[ERROR] 이동할 칸을 D 또는 U로 입력해주세요.");
+            throw new IllegalArgumentException(INPUT_COMMAND_IN_D_OR_U.getMessage());
         }
     }
 }
