d800e03bbc572a28dec2711fa9b50976b3c13e8e
jihyun <jihyun@jihyunui-MacBookAir.local>
2022-11-22 18:57:19 +0900

    feat(bridge): Controller 이동한 다리의 참거짓 여부 생성 기능 구현

--- src/main/java/bridge/controller/BridgeController.java
+++ src/main/java/bridge/controller/BridgeController.java
@@ -2,14 +2,23 @@ package bridge.controller;
 
 import bridge.BridgeMaker;
 import bridge.BridgeRandomNumberGenerator;
+import bridge.domain.BridgeMatcher;
 import bridge.view.InputView;
 import bridge.view.OutputView;
+import java.util.ArrayList;
 import java.util.List;
 
 public class BridgeController {
 
     public void startGame() {
         List<String> bridge = generateBridge();
+        BridgeMatcher bridgeMatcher = makeBridgeMatcher();
+    }
+
+    private BridgeMatcher makeBridgeMatcher() {
+        List<Boolean> matchedBridge = new ArrayList<>();
+        int firstTry = 1;
+        return new BridgeMatcher(matchedBridge, firstTry);
     }
 
     private List<String> generateBridge() {
