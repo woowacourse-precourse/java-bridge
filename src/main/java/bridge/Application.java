ea042f268438824541ee4c0f7c65872a569bcd3a
JunginPark4364 <wjddlsee@gmail.com>
2022-11-22 19:30:55 +0900

    feat: 초기 객체 설정

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -1,16 +1,12 @@
 package bridge;
 
+import bridge.config.Config;
 import bridge.controller.GameController;
-import bridge.view.InputView;
-import bridge.view.OutputView;
 
 public class Application {
 
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
-        InputView inputView = new InputView();
-        OutputView outputView = new OutputView();
-        GameController gameController = new GameController(inputView, outputView);
+        GameController gameController = Config.gameController();
         gameController.start();
     }
 }
