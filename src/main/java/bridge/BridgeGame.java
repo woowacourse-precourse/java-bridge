52658964f87bb2816807ab96847d47982b732257
DaeyeonKim97 <kdysy1130@khu.ac.kr>
2022-11-22 09:56:47 +0900

    refactor(bridge-game) : add comments
    field list
    - bridge
    - answerPath
    - attempts
    
    method list
    - move
    - generateResultMap
    - checkFail
    - checkSuccess
    - retry
    - getAttempts

--- src/main/java/bridge/BridgeGame.java
+++ src/main/java/bridge/BridgeGame.java
@@ -9,8 +9,19 @@ import java.util.Map;
  * 다리 건너기 게임을 관리하는 클래스
  */
 public class BridgeGame {
+    /**
+     * 다리의 모양.
+     * (U, D)로 이루어진 문자 배열.
+     */
     private List<String> bridge;
+    /**
+     * 사용자가 입력한 답을 저장하는 배열.
+     * (U, D)로 이루어진 문자 배열.
+     */
     private List<String> answerPath;
+    /**
+     * 게임 시도 횟수
+     */
     private int attempts;
 
     public BridgeGame() {
@@ -28,14 +39,16 @@ public class BridgeGame {
 
     /**
      * 사용자가 칸을 이동할 때 사용하는 메서드
-     * <p>
-     * 이동을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
+     * @param moving 움직일 방향 (U 또는 D)
      */
     public Map<String,List<String>> move(String moving) {
         this.answerPath.add(moving);
         return generateResultMap();
     }
 
+    /**
+     * 현재까지 이동 경로에 대한 정보를 가진 맵을 생성하는 메서드
+     */
     public Map<String, List<String>> generateResultMap() {
         Map<String, List<String>> resultMap = new HashMap<>();
         List<String> upperLine = new ArrayList<>();
@@ -70,6 +83,9 @@ public class BridgeGame {
         return isCorrect;
     }
 
+    /**
+     * 게임이 실패했는지 체크하는 메소드
+     */
     public boolean checkFail() {
         int index = answerPath.size() - 1;
         if (!bridge.get(index).equals(answerPath.get(index))) {
@@ -78,6 +94,9 @@ public class BridgeGame {
         return false;
     }
 
+    /**
+     * 게임이 성공했는지 체크하는 메소드
+     */
     public boolean checkSuccess(){
         if (bridge.size() == answerPath.size()) {
             return true;
@@ -88,8 +107,7 @@ public class BridgeGame {
 
     /**
      * 사용자가 게임을 다시 시도할 때 사용하는 메서드
-     * <p>
-     * 재시작을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
+     * @param command 게임을 재시작 할지 여부  (R 또는 Q)
      */
     public boolean retry(String command) {
         if ("Q".equals(command)) {
@@ -100,6 +118,9 @@ public class BridgeGame {
         return true;
     }
 
+    /**
+     * 사용자의 시도 횟수를 받아오는 메서드
+     */
     public int getAttempts() {
         return attempts;
     }
