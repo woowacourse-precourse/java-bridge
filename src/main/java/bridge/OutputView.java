54ff86b0de2ce0fa4906c18b46d0db8beb985af3
vcho1958 <gusckd57@gmail.com>
2022-11-22 18:04:28 +0900

    chore(UI): 게임시작 메시지 후 줄을 바꾼다.

--- src/main/java/bridge/OutputView.java
+++ src/main/java/bridge/OutputView.java
@@ -4,7 +4,7 @@ package bridge;
  * 사용자에게 게임 진행 상황과 결과를 출력하는 역할을 한다.
  */
 public class OutputView {
-    private static final String GAME_START_MESSAGE = "다리 건너기 게임을 시작합니다.";
+    private static final String GAME_START_MESSAGE = "다리 건너기 게임을 시작합니다.\n";
     private static final String LENGTH_INPUT_REQUEST_MESSAGE = "다리의 길이를 입력해주세요.";
     private static final String SELECT_PANEL_REQUEST_MESSAGE = "이동할 칸을 선택해주세요. (위: U, 아래: D)";
     private static final String RESTART_QUESTION_MESSAGE = "게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)";
