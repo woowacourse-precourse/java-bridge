0231a2bfd9094160e7f0d74bcb7e2d351d5706cd
210_reverof <sheltonwon@gmail.com>
2022-11-22 14:04:26 +0900

    refactor: add enum related with errors

--- src/main/java/bridge/InputView.java
+++ src/main/java/bridge/InputView.java
@@ -14,12 +14,12 @@ public class InputView {
     public int readBridgeSize() {
         String input = readLine();
         if (!input.matches("[0-9]+")) {
-            throw new IllegalArgumentException("[ERROR]");
+            throw new IllegalArgumentException(Error.NOT_NUMBER.label());
         }
 
         int size = Integer.parseInt(input);
         if (size < 3 || size > 20) {
-            throw new IllegalArgumentException("[ERROR]");
+            throw new IllegalArgumentException(Error.NOT_IN_RANGE.label());
         }
         return size;
     }
@@ -30,7 +30,7 @@ public class InputView {
     public String readMoving() {
         String input = readLine();
         if ( !(input.equals("U") || input.equals("D")) ) {
-            throw new IllegalArgumentException("[ERROR]");
+            throw new IllegalArgumentException(Error.NOT_MOVE_INPUT.label());
         }
         return input;
     }
@@ -41,7 +41,7 @@ public class InputView {
     public String readGameCommand() {
         String input = readLine();
         if ( !(input.equals("R") || input.equals("Q")) ) {
-            throw new IllegalArgumentException("[ERROR]");
+            throw new IllegalArgumentException(Error.NOT_RETRY_INPUT.label());
         }
         return input;
     }
