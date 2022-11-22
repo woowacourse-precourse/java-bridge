f4b4c4ac5f17cdf04a67f5e0d34b0ca3ca3eb1da
seung-hun2 <jungsh2328@naver.com>
2022-11-22 22:11:31 +0900

    - InputView 클래스 readBridgeSize() 메서드 구현

--- src/main/java/bridge/InputView.java
+++ src/main/java/bridge/InputView.java
@@ -9,5 +9,17 @@ import java.util.List;
  * 사용자로부터 입력을 받는 역할을 한다.
  */
 public class InputView {
-
+    public int readBridgeSize() {
+        System.out.println("다리 건너기 게임을 시작합니다.");
+        System.out.println();
+        System.out.println("다리의 길이를 입력해주세요.");
+        int size=0;
+        try {
+            size = Integer.parseInt(Console.readLine());
+        }catch (NumberFormatException e){
+            System.out.println("[ERROR] 다리 길이는 3부터 20 사이의 숫자여야 합니다.");
+//            throw new NumberFormatException();
+        }
+        return size;
+    }
 }
