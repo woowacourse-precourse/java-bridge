3556272d8f99f10dfb84fca14a6c351f7223e439
isieun0908 <supiya0908@naver.com>
2022-11-22 22:32:50 +0900

    Feat: 다리 길이 입력받기

--- src/main/java/bridge/InputView.java
+++ src/main/java/bridge/InputView.java
@@ -9,7 +9,24 @@ public class InputView {
      * 다리의 길이를 입력받는다.
      */
     public int readBridgeSize() {
-        return 0;
+        System.out.println("다리의 길이를 입력해주세요.");
+        String inputString = camp.nextstep.edu.missionutils.Console.readLine();
+
+        int bridgeSize = checkBridgeSize(inputString);
+
+        return bridgeSize;
+    }
+
+    public int checkBridgeSize(String inputString) {
+        try {
+            int inputInteger = Integer.parseInt(inputString);
+            if (inputInteger < 3 || inputInteger > 20) {
+                throw new IllegalArgumentException("[ERROR] 다리 길이는 3부터 20 사이의 숫자여야 합니다.");
+            }
+            return inputInteger;
+        } catch (Exception e) {
+            throw new IllegalArgumentException("[ERROR] 다리 길이는 3부터 20 사이의 숫자여야 합니다.");
+        }
     }
 
     /**
