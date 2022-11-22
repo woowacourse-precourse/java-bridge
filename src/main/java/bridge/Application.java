aecc66ccc949770185461ebd26f0dac317f835ae
LeeJejune <leejejune0625@gmail.com>
2022-11-22 17:40:26 +0900

    feat: 메인 프로그램 구현

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -1,8 +1,20 @@
 package bridge;
 
+import bridge.controller.BridgeGameController;
+import bridge.view.InputView;
+import bridge.view.OutputView;
+
 public class Application {
 
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        InputView inputView = new InputView();
+        OutputView outputView = new OutputView();
+        BridgeMaker bridgeMaker = new BridgeMaker(new BridgeRandomNumberGenerator());
+
+        try {
+            new BridgeGameController(inputView, outputView, bridgeMaker).gamePlay();
+        } catch (IllegalArgumentException e) {
+            System.out.println("[ERROR] " + e.getMessage());
+        }
     }
 }
