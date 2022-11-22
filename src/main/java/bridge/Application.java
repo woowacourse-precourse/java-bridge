a4fce459cd60b38faa4a218196976c743cac920f
3juhwan <13selfesteem91@naver.com>
2022-11-22 22:51:45 +0900

    fix(bridge): 어플리케이션 로직 초기화 부분 수정

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -11,14 +11,15 @@ import static bridge.controller.GameStart.gameStart;
  * 다리 건너기 게임 어플리케이션
  */
 public class Application {
-    private static BridgeMaker bridgeMaker = new BridgeMaker(new BridgeRandomNumberGenerator());
+    private static BridgeMaker bridgeMaker;
     private static Bridge bridge;
-    private static UserMovement userMovement = new UserMovement();
-    private static int tryCnt;
+    private static UserMovement userMovement;
 
     public static void main(String[] args) {
+        bridgeMaker = new BridgeMaker(new BridgeRandomNumberGenerator());
+        userMovement = new UserMovement();
         bridge = gameStart(bridgeMaker);
-        tryCnt = gameRun(userMovement, bridge);
+        int tryCnt = gameRun(userMovement, bridge, 0);
         gameResult(userMovement, bridge, tryCnt);
     }
 
