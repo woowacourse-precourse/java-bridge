f527d92e727598284b113425ad40716e56d78176
desfox <desfox99@naver.com>
2022-11-22 19:58:03 +0900

    refactor(BridgeGame): 매직리터럴 리팩토링

--- src/main/java/bridge/domain/BridgeGame.java
+++ src/main/java/bridge/domain/BridgeGame.java
@@ -9,6 +9,9 @@ import java.util.List;
  * 다리 건너기 게임을 관리하는 클래스
  */
 public class BridgeGame {
+    private static final String MOVE_SUCCESS = "O";
+    private static final String MOVE_FAILURE = "X";
+
     private int currPosition = 0;
     private List<String> bridgeCurrStatus = new ArrayList<>();
 
@@ -41,7 +44,7 @@ public class BridgeGame {
     public void addOWhenEqual(String movingCommand, String currBox) {
         if (movingCommand.equals(currBox)) {
             currPosition++;
-            bridgeCurrStatus.add("O");
+            bridgeCurrStatus.add(MOVE_SUCCESS);
         }
 
         BridgeGameValidation bridgeGameValidation = new BridgeGameValidation();
@@ -50,7 +53,7 @@ public class BridgeGame {
 
     public void addXWhenUnequal(String movingCommand, String currBox) {
         if (! movingCommand.equals(currBox)) {
-            bridgeCurrStatus.add("X");
+            bridgeCurrStatus.add(MOVE_FAILURE);
         }
 
         BridgeGameValidation bridgeGameValidation = new BridgeGameValidation();
@@ -71,7 +74,7 @@ public class BridgeGame {
         int moveCnt = bridgeCurrStatus.size();
         int lastIdx = moveCnt - 1;
 
-        if (moveCnt == bridge.size() && bridgeCurrStatus.get(lastIdx) == "O") {
+        if (moveCnt == bridge.size() && bridgeCurrStatus.get(lastIdx) == MOVE_SUCCESS) {
             return true;
         }
 
@@ -81,7 +84,7 @@ public class BridgeGame {
     public boolean isFailure() {
         int lastIdx = bridgeCurrStatus.size() - 1;
 
-        if (bridgeCurrStatus.get(lastIdx) == "X") {
+        if (bridgeCurrStatus.get(lastIdx) == MOVE_FAILURE) {
             return true;
         }
 
