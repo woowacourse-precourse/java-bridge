c2fbf20b270e1e3f3160639f4f744cd3d02a95e3
Cho Changhun <cvcvcx@naver.com>
2022-11-22 13:10:31 +0900

    [feat] : 다리 길이 입력받고 검증

--- src/main/java/bridge/InputView.java
+++ src/main/java/bridge/InputView.java
@@ -1,5 +1,7 @@
 package bridge;
 
+import camp.nextstep.edu.missionutils.Console;
+
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
  */
@@ -9,13 +11,30 @@ public class InputView {
      * 다리의 길이를 입력받는다.
      */
     public int readBridgeSize() {
-        return 0;
+        String bridgeSize = Console.readLine();
+        validateBridgeSize(bridgeSize);
+        int result = Integer.parseInt(bridgeSize);
+        return result;
+    }
+
+    private static void validateBridgeSize(String  bridgeSize) {
+        try {
+            Integer.parseInt(bridgeSize);
+        }catch (Exception e){
+            throw new IllegalArgumentException("[ERROR] 다리의 길이는 정수로 입력해 주십시오");
+        }
+
+        if(bridgeSize.length() <3|| bridgeSize.length() >20){
+            throw new IllegalArgumentException("[ERROR] 다리의 길이는 3부터 20까지입니다.");
+        }
     }
 
     /**
      * 사용자가 이동할 칸을 입력받는다.
      */
     public String readMoving() {
+        String playerMove = Console.readLine();
+
         return null;
     }
 
