e063d6c5ef883a8ae99b398f8c6a930a780dbe75
0112leesy <0112leesy@naver.com>
2022-11-22 14:36:25 +0900

    feat(Application) : 게임 실행 기능 구현
    게임 실행 중 예외 발생 시 에러메시지와 함께 종료되도록 함

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -1,8 +1,37 @@
 package bridge;
 
+import java.util.List;
+
 public class Application {
+
+    static InputView inputView = new InputView();
+    static OutputView outputView = new OutputView();
+    static BridgeMaker bridgeMaker = new BridgeMaker(new BridgeRandomNumberGenerator());
+    static BridgeGame bridgeGame = new BridgeGame();
 
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        try {
+            runBridgeGame();
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    static void runBridgeGame() {
+        List<String> bridge = bridgeMaker.makeBridge(inputView.readBridgeSize());
+        bridgeGame.initialize(bridge);
+        crossBridge();
+        outputView.printResult(bridgeGame.getRecord(), bridgeGame.getSuccess(),
+            bridgeGame.getTrial());
+    }
+
+    static void crossBridge() {
+        while (!bridgeGame.checkComplete()) {
+            outputView.printMap(bridgeGame.move(inputView.readMoving()));
+            if (bridgeGame.hasFail() && !bridgeGame.retry(inputView.readGameCommand())) {
+                break;
+            }
+        }
     }
 }
