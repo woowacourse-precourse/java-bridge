6d7f83c64c980ccf6fa2cf8a2c99c32bc7a46f94
wkdghdwns199 <35947667+wkdghdwns199@users.noreply.github.com>
2022-11-22 14:53:15 +0900

    Feat : 게임에서 한 칸 움직이는 기능 추가

--- src/main/java/bridge/BridgeGame.java
+++ src/main/java/bridge/BridgeGame.java
@@ -1,16 +1,33 @@
 package bridge;
 
+import java.util.List;
+
 /**
  * 다리 건너기 게임을 관리하는 클래스
  */
 public class BridgeGame {
+    private final List<String> bridge;
+    InputView inputView = new InputView();
+    OutputView outputView = new OutputView();
+    CheckMove checkMove;
+    MoveStatus moveStatus = new MoveStatus();
+    public List<List<String>> statusMap = null;
 
+    public BridgeGame (List<String> bridge){
+        this.bridge = bridge;
+    }
     /**
      * 사용자가 칸을 이동할 때 사용하는 메서드
      * <p>
      * 이동을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void move() {
+    public boolean move(int moveValue) {
+        checkMove = new CheckMove(bridge);
+        String moves = inputView.readMoving();
+        boolean availableToMove = checkMove.checkIfAvailableToMove(moveValue,moves);
+        statusMap = moveStatus.printMoves(moves,availableToMove);
+        outputView.printMap(statusMap);
+        return availableToMove;
     }
 
     /**
@@ -18,6 +35,5 @@ public class BridgeGame {
      * <p>
      * 재시작을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void retry() {
-    }
+
 }
