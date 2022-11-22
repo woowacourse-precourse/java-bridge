9b6c1f0afc48368e901e69f0ea1e5f7b70ee0159
ho2n <jungho2n@gmail.com>
2022-11-22 23:13:14 +0900

    refactor(ValidationTest): 재시작/종료 여부를 입력 받을 때 검증을 정상적으로 하는지 테스트하는 코드 작성하고 가독성이 좋도록 메서드명 수정

--- src/test/java/utils/ValidationTest.java
+++ src/test/java/utils/ValidationTest.java
@@ -6,6 +6,7 @@ import static utils.Validation.ERROR_HEAD_MESSAGE;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
@@ -40,9 +41,9 @@ class ValidationTest {
     }
 
     @ParameterizedTest
-    @CsvSource(value = {"NULL", "''", "u", "d", "1", "UU", "DD"}
+    @CsvSource(value = {"NULL", "''", "u", "d", "1"}
                     , nullValues = "NULL")
-    void 입력값이_U_or_D가아닐때_예외를_검증하는지_테스트(String input) {
+    void 입력값이_U도_D도_아닐때_예외를_검증하는지_테스트(String input) {
         assertThatThrownBy(() -> {
             validation.validMovingNext(input);
         }).isInstanceOf(IllegalArgumentException.class)
@@ -51,9 +52,27 @@ class ValidationTest {
 
     @ParameterizedTest
     @ValueSource(strings = {"U", "D"})
-    void 입력값이_U_or_D일때_정상_작동하는지_테스트(String input) {
+    void 입력값이_U_또는_D일때_정상_작동하는지_테스트(String input) {
         assertThatNoException().isThrownBy(() -> {
             validation.validMovingNext(input);
+        });
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"NULL", "''", "r", "q", "1"}
+            , nullValues = "NULL")
+    void 입력값이_R도_Q도_아닐때_예외를_검증하는지_테스트(String input) {
+        assertThatThrownBy(() -> {
+            validation.validGameCommand(input);
+        }).isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(ERROR_HEAD_MESSAGE);
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"R", "Q"})
+    void 입력값이_R_또는_Q일때_정상_작동하는지_테스트(String input) {
+        assertThatNoException().isThrownBy(() -> {
+            validation.validGameCommand(input);
         });
     }
 }
