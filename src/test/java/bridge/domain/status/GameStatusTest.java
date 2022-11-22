8377fc407ad08349581a3e3f838c757c51cdcf01
Song kyeong seok <skj0922@kookmin.ac.kr>
2022-11-22 21:35:38 +0900

    test(GameStatusTest): 게임 상태 테스트 메서드 구현

--- src/test/java/bridge/domain/status/GameStatusTest.java
+++ src/test/java/bridge/domain/status/GameStatusTest.java
@@ -7,11 +7,28 @@ import org.junit.jupiter.api.Test;
 public class GameStatusTest {
 
     @Test
-    @DisplayName("게임 상태를 확인하는 테스트")
-    public void isGameStatusTest() {
+    @DisplayName("게임 재시작 상태를 확인하는 테스트")
+    public void isGameStatusRetryTest() {
         GameStatus gameStatus = GameStatus.RETRY;
 
         Assertions.assertThat(gameStatus.checkIsRetryStatus(GameStatus.RETRY)).isTrue();
+    }
+
+    @Test
+    @DisplayName("게임 진행 상태를 확인하는 테스트")
+    public void isGameStatusProceedTest() {
+        GameStatus gameStatus = GameStatus.PROCEED;
+
+        Assertions.assertThat(gameStatus.checkIsProceedStatus(GameStatus.PROCEED)).isTrue();
+    }
+
+    @Test
+    @DisplayName("게임 상태를 변환 해주는 테스트")
+    public void convertGameStatusTest() {
+        GameStatus expectedStatus = GameStatus.QUIT;
+        String input = "Q";
+
+        Assertions.assertThat(GameStatus.convertGameStatus(input)).isEqualTo(expectedStatus);
     }
 
 }
