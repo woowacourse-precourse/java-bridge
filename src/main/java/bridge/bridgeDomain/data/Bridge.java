00a7377805cdf559fed6121f11d2360ae2eef444
masiljangajji <hjk172262@gmail.com>
2022-11-22 23:17:52 +0900

    style: 줄 바꿈 수정

--- src/main/java/bridge/bridgeDomain/data/Bridge.java
+++ src/main/java/bridge/bridgeDomain/data/Bridge.java
@@ -1,14 +1,13 @@
 package bridge.bridgeDomain.data;
 
-
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
 public class Bridge {
-
     private List<String> bridge = new ArrayList<>();
     private int tryNumber = 1;
+
     public void setBridge(List<String> bridge) {
         this.bridge = bridge;
     }
@@ -24,8 +23,4 @@ public class Bridge {
     public int getTryNumber() {
         return tryNumber;
     }
-
-
-
-
 }
