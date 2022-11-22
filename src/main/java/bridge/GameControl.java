2dc7d20b69de0fa9658a0de285b3033378c9464c
DESKTOP-AUSH0GT\Power <sgh0718@naver.com>
2022-11-22 18:50:32 +0900

    feat : 게임 진행 클래스에서 게임의 시작과 끝을 정리하는 기능
    게임시작 후, 박복문 메서드에서 나와 게임 종료까지 구현한 기능

--- src/main/java/bridge/GameControl.java
+++ src/main/java/bridge/GameControl.java
@@ -9,6 +9,15 @@ public class GameControl {
     CentralGame bring = new CentralGame();
     String retry = "R";
 
+    public GameControl() {
+        int blankNumber = 0;
+        output.ShowMessage(output.start);
+        int BridgeLength = input.readBridgeSize();
+        List<String> answer = bring.correctAnswer(BridgeLength);
+        List<Integer> Counts = oneGame(answer, blankNumber);
+        output.printResult(answer, Counts, BridgeLength);
+    }
+
     public List<Integer> oneGame(List<String> answer, int blankNumber) {
         int count = 1;
         while (true) {
