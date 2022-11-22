e8f66b6b3d4a0dea4b7330c7ee296f53132fb44f
hsj4436 <hsj4436@naver.com>
2022-11-22 17:02:49 +0900

    feat: 현재까지 이동 경로 출력

--- src/main/java/bridge/view/OutputView.java
+++ src/main/java/bridge/view/OutputView.java
@@ -1,5 +1,9 @@
 package bridge.view;
 
+import bridge.domain.Bridge;
+import bridge.domain.Record;
+import bridge.domain.UsersRoute;
+
 /**
  * 사용자에게 게임 진행 상황과 결과를 출력하는 역할을 한다.
  */
@@ -30,7 +34,9 @@ public class OutputView {
      * <p>
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void printMap() {
+    public void printMap(UsersRoute usersRoute, Bridge bridge) {
+        System.out.println(Record.getTopLane(usersRoute, bridge));
+        System.out.println(Record.getBottomLane(usersRoute, bridge));
     }
 
     /**
