6eb1ea9eb57e7cdde4dfe4ab8d73cbbfa3b18735
jmjmjames <chungjm0711@gmail.com>
2022-11-22 09:22:26 +0900

    feat: BridgeGameController 및 Service 골격 추가

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -1,8 +1,16 @@
 package bridge;
 
+import bridge.controller.BridgeGameController;
+import bridge.service.BridgeGameService;
+
 public class Application {
 
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        BridgeGameController bridgeGameController = new BridgeGameController(bridgeGameService());
+        bridgeGameController.run();
+    }
+
+    public static BridgeGameService bridgeGameService() {
+        return new BridgeGameService();
     }
 }
---
+++ src/main/java/bridge/controller/BridgeGameController.java
@@ -0,0 +1,20 @@
+package bridge.controller;
+
+import bridge.service.BridgeGameService;
+import bridge.view.InputView;
+import bridge.view.OutputView;
+
+public class BridgeGameController {
+    private final InputView inputView = new InputView();
+    private final OutputView outputView = new OutputView();
+
+    private final BridgeGameService bridgeGameService;
+
+    public BridgeGameController(BridgeGameService bridgeGameService) {
+        this.bridgeGameService = bridgeGameService;
+    }
+
+    public void run() {
+
+    }
+}
