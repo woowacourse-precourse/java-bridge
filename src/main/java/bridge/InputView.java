b803747c8233233ed5bdb2a985d393dacf3a51f2
Mr.Kim <86274253+Joajy@users.noreply.github.com>
2022-11-22 11:49:13 +0900

    style: InputView
    delete constant variable and use just Constant class more easily

--- src/main/java/bridge/InputView.java
+++ src/main/java/bridge/InputView.java
@@ -1,29 +1,31 @@
 package bridge;
 
-import camp.nextstep.edu.missionutils.Console;
-
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
  */
+import camp.nextstep.edu.missionutils.Console;
+
 public class InputView {
     private static final String REGEX = "^[0-9]*$";
-    private static Constant constant = new Constant();
     /**
      * 다리의 길이를 입력받는다.
      */
     public int readBridgeSize() {
-        try {
-            System.out.println(constant.Select_Size);
-            return Integer.parseInt(Console.readLine());
-        } catch (NumberFormatException e) {
-            throw new IllegalArgumentException(constant.Length_Restrict());
+        String bridgeSize = Console.readLine();
+        int Size;
+        try{
+            Size = Integer.parseInt(bridgeSize);
+        } catch(NumberFormatException e){
+            throw new IllegalArgumentException(Constant.Length_Restrict());
         }
+        System.out.println(Constant.Game_Start());
+        return Size;
     }
-    
+
     public void checkLengthValidate(String input) {
         int length = Integer.parseInt(input);
         if(length < 3 || length > 20 || !input.matches(REGEX))
-            throw new IllegalArgumentException(constant.Length_Restrict());
+            throw new IllegalArgumentException(Constant.Length_Restrict());
     }
 
     /**
@@ -32,29 +34,29 @@ public class InputView {
     public String readMoving() {
         String move = Console.readLine();
         checkMoveValidate(move);        
-        System.out.println(constant.Select_Move());
+        System.out.println(Constant.Select_Move());
         System.out.println(move);
         return move;
     }
 
     public void checkMoveValidate(String input) {
-        if(!(input.equals(constant.Up) || input.equals(constant.Down)))
-            throw new IllegalArgumentException(constant.Move_Restrict());
+        if(!(input.equals(Constant.Up) || input.equals(Constant.Down)))
+            throw new IllegalArgumentException(Constant.Move_Restrict());
     }
 
     /**
      * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
      */
     public String readGameCommand() {
-        System.out.println(constant.Select_Retry);
-        String cmd = Console.readLine();
-        if (!(cmd.equals(constant.Restart) || cmd.equals(constant.Quit)))
-            throw new IllegalArgumentException(constant.Replay_Restrict());
-        return cmd;
+        String Replay = Console.readLine();
+        checkRetryValidate(Replay);
+        System.out.println(Constant.Select_Replay());
+        System.out.println(Replay);
+        return Replay;
     }
-    
+
     public void checkRetryValidate(String input) {
-        if(!(input.equals(constant.Restart) || input.equals(constant.Quit)))
-            throw new IllegalArgumentException(constant.Replay_Restrict());
+        if(!(input.equals(Constant.Restart) || input.equals(Constant.Quit)))
+            throw new IllegalArgumentException(Constant.Replay_Restrict());
     }
 }
