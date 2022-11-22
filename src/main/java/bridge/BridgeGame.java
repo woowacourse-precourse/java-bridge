6c96ba20cbddfe383ed95ac67759fc0b2901bb72
SeongchanCho <wordnumperiod@gmail.com>
2022-11-22 18:57:24 +0900

    feat(BridgeGame.java): 게임 플레이 기능 구현 완료
    - 위아래 두 칸으로 이루어진 다리를 건너는 기능 (조건 체크 기능 또한 구현 완료)
    - 유저 승리 여부를 체크하는 기능 구현 완료
    - 유저 패배 여부를 체크하는 기능 구현 완료
    - 재시작하는 기능 구현 완료

--- src/main/java/bridge/BridgeGame.java
+++ src/main/java/bridge/BridgeGame.java
@@ -1,17 +1,73 @@
 package bridge;
 
+import java.util.List;
+
 /**
  * 다리 건너기 게임을 관리하는 클래스
  */
 public class BridgeGame {
+
+    enum Direction {
+        UP, DOWN
+    }
+
+    List<String> map;
+    int rowIndex, columnIndex;
 
     /**
      * 사용자가 칸을 이동할 때 사용하는 메서드
      * <p>
      * 이동을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void move() {
+
+    public void play(Direction direction) {
+        while (checkIfUserWin()) {
+            move(direction);
+            if (checkIfGameIsOver()) {
+                retry();
+            }
+        }
     }
+
+    public void move(Direction direction) {
+        if (direction == Direction.UP) {
+            moveUp();
+        } else if (direction == Direction.DOWN) {
+            moveDown();
+        }
+    }
+
+    public void moveUp() {
+        if (rowIndex == 1) {
+            rowIndex = 0;
+        }
+        columnIndex += 1;
+    }
+
+    public void moveDown() {
+        if (rowIndex == 0) {
+            rowIndex = 1;
+        }
+        columnIndex += 1;
+    }
+
+    public boolean checkIfGameIsOver() {
+        String whichDirectionIsSafe = map.get(columnIndex);
+        if (whichDirectionIsSafe.equals("U") && rowIndex == 1) {
+            return true;
+        } else if (whichDirectionIsSafe.equals("D") && rowIndex == 0) {
+            return true;
+        }
+        return false;
+    }
+
+    public boolean checkIfUserWin() {
+        if (columnIndex >= map.size()) {
+            return true;
+        }
+        return false;
+    }
+
 
     /**
      * 사용자가 게임을 다시 시도할 때 사용하는 메서드
@@ -19,5 +75,7 @@ public class BridgeGame {
      * 재시작을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
     public void retry() {
+        rowIndex=0;
+        columnIndex=0;
     }
 }
