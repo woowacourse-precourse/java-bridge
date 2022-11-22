4543c355fe22d23753585de4dd3a37da1097ebb1
kissshot1104 <kissshot.heartunderblade1104@gmail.com>
2022-11-22 21:05:17 +0900

    Refactor(InputViewTest) : 성공 케이스 실패 케이스 분리
    성공 케이스와 실패 케이스를 나눴습니다.

--- src/test/java/bridge/InputViewTest.java
+++ src/test/java/bridge/InputViewTest.java
@@ -3,7 +3,6 @@ package bridge;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
@@ -12,33 +11,70 @@ class InputViewTest extends CustomNsTest {
     public static final InputView inputView = new InputView();
     public static final String ERROR_MESSAGE = "[ERROR]";
 
-    @DisplayName("다리의 사이즈를 입력받는지 테스트")
+    @DisplayName("다리의 사이즈를 입력됐는지 테스트(실패 케이스)")
     @ValueSource(strings = {"21", "0", "+1", "-1", "05"})
     @ParameterizedTest
-    void readBridegeSizeTest(String input) {
+    void readBridegeSizeFailTest(String input) {
 
-            command("21");
-            inputView.readBridgeSize();
-            assertThat(output()).contains(ERROR_MESSAGE);
+        command(input);
+        inputView.readBridgeSize();
+        assertThat(output()).contains(ERROR_MESSAGE);
     }
 
-    @DisplayName("사용자가 이동할 칸을 입력받는지 테스트")
+    @DisplayName("다리의 사이즈를 입력됐는지 테스트(성공 케이스)")
+    @ValueSource(strings = {"1", "2", "3", "4", "18", "19", "20"})
+    @ParameterizedTest
+    void readBridegeSizeSuccessTest(String input) {
+
+        command(input);
+        int number = inputView.readBridgeSize();
+        boolean isTrue = (number <= 1 && number >= 20);
+        assertThat(isTrue);
+    }
+
+    ///////////////////////////////////////////////
+
+    @DisplayName("사용자가 이동할 칸을 입력받는지 테스트(실패 케이스)")
     @ValueSource(strings = {"A", "UU", "DD"})
     @ParameterizedTest
-    void readMovingTest(String input) {
+    void readMovingFailTest(String input) {
 
         command(input);
         inputView.readMoving();
         assertThat(output()).contains(ERROR_MESSAGE);
     }
 
-    @DisplayName("사용자가 리트라이 여부를 입력받는지 테스트")
+    @DisplayName("사용자가 이동할 칸을 입력받는지 테스트(성공 케이스)")
+    @ValueSource(strings = {"U", "D"})
+    @ParameterizedTest
+    void readMovingSuccessTest(String input) {
+
+        command(input);
+        String move = inputView.readMoving();
+        boolean isTrue = move.equals("U") || move.equals("D");
+        assertThat(isTrue);
+    }
+
+    ////////////////////////////////////////////////////////////////
+
+    @DisplayName("사용자가 리트라이 여부를 입력받는지 테스트(실패 케이스)")
     @ValueSource(strings = {"A", "RR", "QQ"})
     @ParameterizedTest
-    void readGameCommand(String input) {
+    void readGameCommandFailTest(String input) {
 
         command(input);
         inputView.readGameCommand();
         assertThat(output()).contains(ERROR_MESSAGE);
+    }
+
+    @DisplayName("사용자가 리트라이 여부를 입력받는지 테스트(성공 케이스)")
+    @ValueSource(strings = {"R", "Q"})
+    @ParameterizedTest
+    void readGameCommandSuccessTest(String input) {
+
+        command(input);
+        String command = inputView.readGameCommand();
+        boolean isTrue = command.equals("U") || command.equals("D");
+        assertThat(isTrue);
     }
 }
