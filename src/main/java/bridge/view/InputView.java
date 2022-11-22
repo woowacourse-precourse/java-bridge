694e5ebb2516bfe52b64df0ca0d05ae7fbeabebd
suheonjoo <suheonj95@naver.com>
2022-11-22 18:18:03 +0900

    chore: 프로젝트 초기에 주어진 주석 삭제

--- src/main/java/bridge/view/InputView.java
+++ src/main/java/bridge/view/InputView.java
@@ -2,34 +2,22 @@ package bridge.view;
 
 import camp.nextstep.edu.missionutils.Console;
 
-/**
- * 사용자로부터 입력을 받는 역할을 한다.
- */
 public class InputView {
 
 	public static final String BRIDGE_SIZE_PHRASE = "다리의 길이를 입력해주세요.";
 	public static final String CHOOSE_MOVE_SPACE_PHRASE = "이동할 칸을 선택해주세요. (위: U, 아래: D)";
 	public static final String RESTART_OR_QUIT_PHRASE = "게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)";
 
-	/**
-	 * 다리의 길이를 입력받는다.
-	 */
 	public String readBridgeSize() {
 		System.out.println(BRIDGE_SIZE_PHRASE);
 		return Console.readLine();
 	}
 
-	/**
-	 * 사용자가 이동할 칸을 입력받는다.
-	 */
 	public String readMoving() {
 		System.out.println(CHOOSE_MOVE_SPACE_PHRASE);
 		return Console.readLine();
 	}
 
-	/**
-	 * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
-	 */
 	public String readGameCommand() {
 		System.out.println(RESTART_OR_QUIT_PHRASE);
 		return Console.readLine();
