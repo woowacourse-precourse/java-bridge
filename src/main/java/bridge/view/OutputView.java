e6559f4c0b47027aa2306d81a31e61bbd69ee1ae
jihyun <jihyun@jihyunui-MacBookAir.local>
2022-11-22 18:45:49 +0900

    feat(bridge): 이동중인 다리 출력, 최종 결과 출력 기능 구현

--- src/main/java/bridge/view/OutputView.java
+++ src/main/java/bridge/view/OutputView.java
@@ -1,11 +1,25 @@
 package bridge.view;
 
+import bridge.domain.BridgeMatcher;
+import bridge.domain.GameResult;
+import java.util.List;
+
 /**
  * 사용자에게 게임 진행 상황과 결과를 출력하는 역할을 한다.
  */
 public class OutputView {
 
     private static final String OUTPUT_GAME_START = "다리 건너기 게임을 시작합니다.\n";
+    private static final String OUTPUT_GAME_RESULT_HEADER = "최종 게임 결과\n";
+    private static final String SUCCESS = "성공";
+    private static final String FAIL = "실패";
+    private static final String OUTPUT_FINAL_GAME_RESULT = "게임 성공 여부: %s\n총 시도한 횟수: %d";
+
+    private GameResult gameResult;
+
+    public OutputView(GameResult gameResult) {
+        this.gameResult = gameResult;
+    }
 
     public void printGameStart() {
         System.out.println(OUTPUT_GAME_START);
@@ -16,7 +30,9 @@ public class OutputView {
      * <p>
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void printMap() {
+    public void printMap(String input, boolean isMatch, BridgeMatcher bridgeMatcher) {
+        String map = gameResult.makeBridgeResult(input, isMatch, bridgeMatcher);
+        System.out.println(map);
     }
 
     /**
@@ -24,6 +40,15 @@ public class OutputView {
      * <p>
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void printResult() {
+    public void printResult(List<String> bridge, BridgeMatcher bridgeMatcher) {
+        int count = bridgeMatcher.getTryCount();
+        boolean isSuccess = bridgeMatcher.isGameSuccess(bridge);
+        String finalResult = gameResult.getFinalResult();
+        String result = SUCCESS;
+        if (!isSuccess) {
+            result = FAIL;
+        }
+        System.out.println(OUTPUT_GAME_RESULT_HEADER + finalResult);
+        System.out.printf(OUTPUT_FINAL_GAME_RESULT, result, count);
     }
 }
