43d16bb9d0b93ab7c4e32fdab42fd8c216c45f96
gmu1026 <gmu1026@gmail.com>
2022-11-22 20:26:21 +0900

    feat: 게임 재시작 여부 입력 기능 구현

--- src/main/java/bridge/InputView.java
+++ src/main/java/bridge/InputView.java
@@ -1,7 +1,6 @@
 package bridge;
 
 import camp.nextstep.edu.missionutils.Console;
-import java.util.Arrays;
 
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
@@ -9,12 +8,14 @@ import java.util.Arrays;
 public class InputView {
     private static final String INPUT_BRIDGE_SIZE_MESSAGE = "다리의 길이를 입력해주세요.";
     private static final String INPUT_MOVE_MESSAGE = "이동한 칸을 선택해주세요. (위: U, 아래: D)";
+    private static final String INPUT_RETRY_MESSAGE = "게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)";
 
     private static final int BRIDGE_MIN_RANGE = 3;
     private static final int BRIDGE_MAX_RANGE = 20;
-
     private static final String UP_COMMAND = "U";
     private static final String DOWN_COMMAND = "D";
+    private static final String RETRY_COMMAND = "R";
+    private static final String QUIT_COMMAND = "Q";
 
     /**
      * 다리의 길이를 입력받는다.
@@ -52,17 +53,13 @@ public class InputView {
     public String readMoving() {
         System.out.println(INPUT_MOVE_MESSAGE);
         String inputMoveDirection = Console.readLine();
-        validateMovingDirection(inputMoveDirection);
-
-        return inputMoveDirection.toUpperCase();
-    }
-
-    private void validateMovingDirection(String inputMoveDirection) {
         validateMoveCommand(inputMoveDirection.trim());
+
+        return inputMoveDirection;
     }
 
     private void validateMoveCommand(String inputMoveDirection) {
-        if (!inputMoveDirection.equalsIgnoreCase(UP_COMMAND) && !inputMoveDirection.equalsIgnoreCase(DOWN_COMMAND)) {
+        if (!inputMoveDirection.equals(UP_COMMAND) && !inputMoveDirection.equals(DOWN_COMMAND)) {
             throw new IllegalArgumentException("[ERROR] U 또는 D 값만 입력해야 합니다.");
         }
     }
@@ -71,6 +68,16 @@ public class InputView {
      * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
      */
     public String readGameCommand() {
-        return null;
+        System.out.println(INPUT_RETRY_MESSAGE);
+        String inputGameCommand = Console.readLine();
+        validateGameCommand(inputGameCommand.trim());
+
+        return inputGameCommand;
+    }
+
+    private void validateGameCommand(String inputGameCommand) {
+        if (!inputGameCommand.equals(RETRY_COMMAND) && !inputGameCommand.equals(QUIT_COMMAND)) {
+            throw new IllegalArgumentException("[ERROR] R 또는 Q 값만 입력해야 합니다.");
+        }
     }
 }
