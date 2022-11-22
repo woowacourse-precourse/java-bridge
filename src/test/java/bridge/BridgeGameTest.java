8af29ab3f684d5cf908f87ef3a4638a8280bda8e
fnzl54 <rnjscksdud1@gmail.com>
2022-11-22 11:38:33 +0900

    style: change method name

--- src/test/java/bridge/BridgeGameTest.java
+++ src/test/java/bridge/BridgeGameTest.java
@@ -1,25 +1,21 @@
 
 package bridge;
 
-import bridge.BridgeGame;
-import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class BridgeGameTest {
-    @DisplayName("이동할 수 있을 때 true를 반환한다.")
     @Test
-    void createMovable() {
+    void 움직임이_가능한_경우() {
         BridgeGame bridgeGame = new BridgeGame();
         boolean move = bridgeGame.move("U", "U");
         assertTrue(move);
     }
 
-    @DisplayName("이동할 수 없을 때 false를 반환한다.")
     @Test
-    void createUnMovable() {
+    void 움직임이_불가능한_경우() {
         BridgeGame bridgeGame = new BridgeGame();
         boolean move = bridgeGame.move("D", "U");
         assertFalse(move);
