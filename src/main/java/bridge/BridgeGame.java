c3dbae050ab0a756a2afa7ac3acb951376bae07d
gmu1026 <gmu1026@gmail.com>
2022-11-22 18:14:45 +0900

    feat: 게임 제어 부분 분리

--- src/main/java/bridge/BridgeGame.java
+++ src/main/java/bridge/BridgeGame.java
@@ -7,29 +7,15 @@ import java.util.List;
  * 다리 건너기 게임을 관리하는 클래스
  */
 public class BridgeGame {
-    private static final String START_MESSAGE = "다리 건너기 게임을 시작합니다.";
-    private InputView inputView;
-    private BridgeMaker bridgeMaker = new BridgeMaker(new BridgeRandomNumberGenerator());
-    private OutputView outputView;
-
     private List<String> playerCommands;
-    private int plyerMoveCount = 0;
-
+    private int playerMoveCount = 0;
     private GameMap gameMap = new GameMap();
+    private boolean isOver = false;
 
     private List<String> bridge; // 직접 접근을 막고 bridge to map 으로 반환하여 출력하게끔
-    public BridgeGame(InputView inputView, OutputView outputView) {
-        this.inputView = inputView;
-        this.outputView = outputView;
-    }
-    public void play() {
-        System.out.println(START_MESSAGE);
-        int bridgeSize = inputView.readBridgeSize();
-        bridge = bridgeMaker.makeBridge(bridgeSize);
-        playerCommands = new ArrayList<>();
-        String playerCommand = inputView.readMoving();
-        move(playerCommand);
-        outputView.printMap(gameMap);
+    public BridgeGame(List<String> bridge) {
+        this.bridge = bridge;
+        this.playerCommands = new ArrayList<>();
     }
 
     /**
@@ -39,17 +25,23 @@ public class BridgeGame {
      */
     public void move(String moveCommand) {
         playerCommands.add(moveCommand);
-        plyerMoveCount++;
+        playerMoveCount++;
         gameMap.add(moveCommand, checkMoveResult(moveCommand));
+    }
+
+    private void checkMovable() {
+        if (playerMoveCount == bridge.size()) {
+            isOver = true;
+        }
     }
 
     private String checkMoveResult(String moveCommand) {
         String result = "";
-        if (moveCommand.equals(bridge.get(plyerMoveCount))) {
+        if (moveCommand.equals(bridge.get(playerMoveCount))) {
             result = "O";
         }
 
-        if (!moveCommand.equals(bridge.get(plyerMoveCount))) {
+        if (!moveCommand.equals(bridge.get(playerMoveCount))) {
             result = "X";
         }
         return result;
@@ -61,5 +53,12 @@ public class BridgeGame {
      * 재시작을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
     public void retry() {
+    }
+
+    public GameMap getGameMap() {
+        return gameMap;
+    }
+    public boolean isOver() {
+        return isOver;
     }
 }
