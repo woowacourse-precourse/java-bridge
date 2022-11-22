be2bbc13f207cd631c609e709e3aa81e4b69cda0
dingding-21 <phj8221@naver.com>
2022-11-22 14:11:33 +0900

    feat: 다리 길이 입력

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -4,8 +4,11 @@ public class Application {
 
     public static void main(String[] args) {
         OutputView outputView = new OutputView();
+        InputView inputView = new InputView();
 
         outputView.printGameStart();
         outputView.printInputBridgeSize();
+
+        int bridgeSize = inputView.readBridgeSize();
     }
 }
