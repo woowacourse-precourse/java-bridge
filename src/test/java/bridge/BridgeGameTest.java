71aa7960d8b9f107bf641ca28523fa03ecebf2cb
josolha <josolha@nate.com>
2022-11-22 18:51:37 +0900

    test(BridgeGameTest) : makeBridge 메소드 생성된 다리 U,D 이외값 테스트

--- src/test/java/bridge/BridgeGameTest.java
+++ src/test/java/bridge/BridgeGameTest.java
@@ -1,5 +1,7 @@
 package bridge;
 
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
 import bridge.service.BridgeGame;
 import java.util.List;
 import org.assertj.core.api.Assertions;
@@ -7,6 +9,8 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 class BridgeGameTest {
 
@@ -27,6 +31,14 @@ class BridgeGameTest {
         void bridgeSizeTest() {
             int checkSize = 3;
             Assertions.assertThat(bridge.size()).isEqualTo(checkSize);
+        }
+
+        @DisplayName("생성된 다리 U,D 이외의 값 확인")
+        @ParameterizedTest
+        @CsvSource(value = {"1 : false", "u : false", "d : false", "아 : false", "-1 : false",
+                "10 : false"}, delimiter = ':')
+        void bridgeContainsTest(String element, boolean expected) {
+            assertThat(bridge.contains(element)).isEqualTo(expected);
         }
     }
 }
