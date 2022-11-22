ac78eea01408cdd38a545c1bab9a944c80c6fc5d
inhyeok <ss112d86@gmail.com>
2022-11-22 19:46:23 +0900

    feat: BridgeGame 생성 메소드 구현

--- src/main/java/bridge/domain/BridgeGameService.java
+++ src/main/java/bridge/domain/BridgeGameService.java
@@ -1,54 +1,12 @@
 package bridge.domain;
 
-import bridge.BridgeMaker;
-import bridge.domain.command.BridgeMoveCommand;
-import bridge.domain.command.ReTryCommand;
-import bridge.domain.command.SetBridgeSizeCommand;
-import java.util.List;
+import bridge.domain.command.CreateBridgeGameCommand;
 
 public class BridgeGameService {
 
-  private final Input input;
-  private final BridgeMaker bridgeMaker;
-  private final Output output;
-
-  public BridgeGameService(Input input, BridgeMaker bridgeMaker, Output output) {
-    this.input = input;
-    this.bridgeMaker = bridgeMaker;
-    this.output = output;
-  }
-
-  public void start() {
-    BridgeGame game = init();
-    boolean result = progress(game);
-    output.printResult(game.getGameState(), result);
-  }
-
-  private BridgeGame init() {
-    SetBridgeSizeCommand command = input.readBridgeSize();
-    List<String> bridgeAreas = bridgeMaker.makeBridge(command.getSize());
-    return BridgeGame.createGame(bridgeAreas);
-  }
-
-  private boolean progress(BridgeGame game) {
-    if (game.isClear()) {
-      return true;
-    }
-    if (progressMove(game)) {
-      return progress(game);
-    }
-    ReTryCommand tryCommand = input.readGameCommand();
-    if (tryCommand.isReTry()) {
-      game.retry();
-      return progress(game);
-    }
-    return false;
-  }
-
-  private boolean progressMove(BridgeGame game) {
-    BridgeMoveCommand bridgeMoveCommand = input.readMoving();
-    boolean move = game.move(bridgeMoveCommand.getArea());
-    output.printMap(game.getGameState());
-    return move;
+  public BridgeGame createGame(CreateBridgeGameCommand command) {
+    Bridge bridge = new Bridge(command.getBridges());
+    GameState gameState = new GameState();
+    return new BridgeGame(bridge, gameState);
   }
 }
