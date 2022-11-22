fad2ea617f4323521a088b351e2133e43a9fee18
fnzl54 <rnjscksdud1@gmail.com>
2022-11-22 18:37:14 +0900

    style: rename test methods

--- src/test/java/bridge/BridgeGameTest.java
+++ src/test/java/bridge/BridgeGameTest.java
@@ -8,14 +8,14 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class BridgeGameTest {
     @Test
-    void 움직임이_가능한_경우() {
+    void move_True_SameBridgeStateUserInput() {
         BridgeGame bridgeGame = new BridgeGame();
         boolean move = bridgeGame.move("U", "U");
         assertTrue(move);
     }
 
     @Test
-    void 움직임이_불가능한_경우() {
+    void move_False_DifferentBridgeStateUserInput() {
         BridgeGame bridgeGame = new BridgeGame();
         boolean move = bridgeGame.move("D", "U");
         assertFalse(move);
