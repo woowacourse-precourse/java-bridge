08f81bef9160f8ac8c1067d8e253210fecdbade9
DevJay1024 <devjay1024@gmail.com>
2022-11-22 15:34:42 +0900

    test: BridgeGame 기능 검증용 테스트 코드 리팩토링 (동일한 명령들을 일원화)

--- src/test/java/bridge/BridgeGameTest.java
+++ src/test/java/bridge/BridgeGameTest.java
@@ -11,32 +11,30 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.util.Lists.newArrayList;
 
 class BridgeGameTest {
+    BridgeGame game;
 
-    @Test
-    void 사용자_경로_이동은_다리의_안전경로와_같을때_적절히_실행된다(){
-        BridgeGame game = new BridgeGame();
+    @BeforeEach
+    void init(){
+        game = new BridgeGame();
         game.initComponents(
                 new TestNumberGenerator(newArrayList(1, 0, 1)),
                 3);
-        game.move(Direction.UP);
+    }
 
+    @Test
+    void 사용자_경로_이동은_다리의_안전경로와_같을때_적절히_실행된다(){
+        game.move(Direction.UP);
         assertThat(game.getPlayerPath().getIsAlive()).isTrue();
     }
 
     @Test
     void 사용자_경로_이동은_다리의_안전경로와_다를때_적절히_실행된다(){
-        BridgeGame game = new BridgeGame();
-        game.initComponents(
-                new TestNumberGenerator(newArrayList(1, 0, 1)),
-                3);
         game.move(Direction.DOWN);
-
         assertThat(game.getPlayerPath().getIsAlive()).isFalse();
     }
 
     @Test
     void 게임_재시도_데이처_처리는_정상동작한다(){
-        BridgeGame game = new BridgeGame();
         PlayerPath path1 = game.getPlayerPath();
         game.retry();
         PlayerPath path2 = game.getPlayerPath();
@@ -46,14 +44,6 @@ class BridgeGameTest {
 
     @Nested
     class CheckCompletelyMatchedTest {
-        BridgeGame game;
-        @BeforeEach
-        void init(){
-            game = new BridgeGame();
-            game.initComponents(
-                    new TestNumberGenerator(newArrayList(1, 0, 1)),
-                    3);
-        }
 
         @Test
         void 사용자경로길이가_다리_안전경로보다_모든부분이_일치해야_완전일치다(){
