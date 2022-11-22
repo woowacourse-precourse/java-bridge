e21ee94c452cd91e4cc36f9e5c7d1447f521fab0
Park Sungchul <try615@naver.com>
2022-11-22 14:30:35 +0900

    chore(BridgeGame.java): 루프 구조 정립
    게임이 동작하며 재시작되거나 종료되기까지 루프 과정을 정립

--- src/main/java/bridge/BridgeGame.java
+++ src/main/java/bridge/BridgeGame.java
@@ -5,29 +5,58 @@ package bridge;
  */
 public class BridgeGame {
 
-    private Bridge bridge;
+	private Bridge bridge;
 
-    public BridgeGame() {
-        bridge = new Bridge();
-    }
+	public BridgeGame() {
+		bridge = new Bridge();
+	}
 
-    public void run() {
+	public void run() {
+		init();
+		while (loop()) {
 
-    }
+		}
+	}
 
-    /**
-     * 사용자가 칸을 이동할 때 사용하는 메서드
-     * <p>
-     * 이동을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
-     */
-    public void move() {
-    }
+	/**
+	 * 가 사용자가 칸을 이동할 때 사용하는 메서드
+	 * <p>
+	 * 이동을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
+	 */
+	public void move() {
 
-    /**
-     * 사용자가 게임을 다시 시도할 때 사용하는 메서드
-     * <p>
-     * 재시작을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
-     */
-    public void retry() {
-    }
+	}
+
+	/**
+	 * 사용자가 게임을 다시 시도할 때 사용하는 메서드
+	 * <p>
+	 * 재시작을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
+	 */
+	public boolean retry() {
+		return true;
+	}
+
+	public void init() {
+
+	}
+
+	public boolean loop() {
+		while (action()) {
+
+		}
+		if (retry()) {
+			return true;
+		}
+		return false;
+	}
+
+	public boolean action() {
+		move();
+		return process();
+	}
+
+	public boolean process() {
+
+		return false;
+	}
 }
