686336cf041e1128952e66ae6c60389eac366ada
gitFILO <x1xgudwls@naver.com>
2022-11-22 19:50:47 +0900

    [구현 기능 목록 1번 추가]

--- src/main/java/bridge/BridgeGame.java
+++ src/main/java/bridge/BridgeGame.java
@@ -1,23 +1,37 @@
 package bridge;
 
+import bridge.*;
+import org.mockito.stubbing.Answer;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static bridge.Player.bridges;
+
 /**
  * 다리 건너기 게임을 관리하는 클래스
  */
 public class BridgeGame {
+    GameSystem gameSystem;
+    Player player;
+    int trialNumber=1;
+    private final AnswerBridge answerBridge;
 
-    /**
-     * 사용자가 칸을 이동할 때 사용하는 메서드
-     * <p>
-     * 이동을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
-     */
-    public void move() {
+    public BridgeGame(){
+        BridgeRandomNumberGenerator bridgeRandomNumberGenerator = new BridgeRandomNumberGenerator();
+        BridgeMaker bridgeMaker = new BridgeMaker(bridgeRandomNumberGenerator);
+        answerBridge = new AnswerBridge(bridgeMaker.getBridgeSize());
+        player = new Player(new Bridges(answerBridge.size));
+        gameSystem = new GameSystem(player,answerBridge);
     }
 
-    /**
-     * 사용자가 게임을 다시 시도할 때 사용하는 메서드
-     * <p>
-     * 재시작을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
-     */
-    public void retry() {
+    public void start(){
+        Boolean moveSuccess;
+        do{
+            moveSuccess = move();
+            System.out.println(trialNumber);
+        } while((moveSuccess == false) && retry(gameSystem.retryOrNot()));
     }
+
+   
 }
