f6e352f5a76f8e214d06287ef558b86b7441bade
naaaaag <115922748+naaaaag@users.noreply.github.com>
2022-11-22 15:05:37 +0900

    <feat>Application : 메인 메서드 작성
     '플레이어의 위치가 끝까지 도달하거나 재시도 커맨드가 "Q"라면 끝내기 기능'이 포함되어 있습니다.

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -1,8 +1,19 @@
 package bridge;
 
+import java.util.List;
+
 public class Application {
 
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        BridgeOfSizeMaker bridgeOfSizeMaker = new BridgeOfSizeMaker();
+        List<String> bridge = bridgeOfSizeMaker.makeBridgeOfSize();
+        BridgeGame bridgeGame = new BridgeGame(bridge);
+        GameController gameController = new GameController(bridgeGame);
+        OutputView outputView = new OutputView();
+
+        do {
+            gameController.executeMoving();
+        } while (gameController.doesPlayerWantRetrial && bridgeGame.getPlayerLocation() < bridgeGame.getBridge().size()-1);
+        outputView.printResult(bridgeGame.getTrial(), bridgeGame.getPlayerLocation(), bridgeGame.getBridge());
     }
 }
