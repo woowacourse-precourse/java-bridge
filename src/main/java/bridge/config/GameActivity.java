17246abe2ecbe327f3079642ab6b71c5248a5005
김형기 <khk211113@naver.com>
2022-11-22 16:38:10 +0900

    feat(GameActivity): 예외 발생 시 반복을 지원하는 메소드 추가

---
+++ src/main/java/bridge/config/GameActivity.java
@@ -0,0 +1,48 @@
+package bridge.config;
+
+import java.util.concurrent.Callable;
+
+public abstract class GameActivity {
+
+    private boolean stopped = false;
+
+    public final void run() {
+        onStart();
+        while (!stopped) {
+            onLoop();
+        }
+        onStop();
+    }
+
+    public final <T> T executeUntilNoException(Callable callable) {
+        while (true) {
+            try {
+                return (T) callable.call();
+            } catch (Exception e) {
+                onError(e);
+            }
+        }
+    }
+
+    public final void executeUntilNoException(Runnable runnable) {
+        while (true) {
+            try {
+                runnable.run();
+                return;
+            } catch (Exception e) {
+                onError(e);
+            }
+        }
+    }
+
+
+    protected abstract void onStart();
+    protected abstract void onLoop();
+    protected abstract void onStop();
+    protected abstract void onError(Exception e);
+
+    protected void stop() {
+        this.stopped = true;
+    }
+
+}
