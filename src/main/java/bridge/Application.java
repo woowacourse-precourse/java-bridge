6cce799396a7fc58dd4dfe69ae402fc6b65ab095
JaeHwanCho <makefuture@kakao.com>
2022-11-22 16:05:34 +0900

    feat(function): make bridge
    Add new function:
    - Add makeBridge

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -1,8 +1,20 @@
 package bridge;
 
+import java.util.List;
+
 public class Application {
 
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        InputView inputView = new InputView();
+        BridgeNumberGenerator bridgeNumberGenerator = new BridgeRandomNumberGenerator();
+        BridgeMaker bridgeMaker = new BridgeMaker(bridgeNumberGenerator);
+
+        System.out.println("다리 건너기 게임을 시작합니다.");
+        System.out.println("다리의 길이를 입력해주세요.");
+        int bridgeSize = inputView.readBridgeSize();
+
+        List<String> strings = bridgeMaker.makeBridge(bridgeSize);
+        System.out.println("strings = " + strings);
     }
 }
