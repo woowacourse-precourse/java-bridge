b264a1dedbc0712d19f3b434a11b875fbc31e396
jsc1014 <wotlr5209@gmail.com>
2022-11-22 17:37:10 +0900

    feat: main문 구현

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -6,7 +6,7 @@ import java.util.List;
 public class Application {
 
     public static void main(String[] args) {
-
+        start();
     }
 
     private static List<String> makeBridge(int bridgeSize){
