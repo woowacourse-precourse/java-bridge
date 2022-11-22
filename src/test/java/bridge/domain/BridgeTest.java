caf7fcf6d3d8fcb43925a0e5e5991dab2f841337
ycopp9 <aciees95@gmail.com>
2022-11-22 17:01:40 +0900

    test(Bridge): getBridgeInfo 메소드 test 함수 구현
    위쪽 아래쪽 다리에 맞게 출력을 위한 이전까지 정보를 반환하는 메소드의
    테스트 함수 구현

--- src/test/java/bridge/domain/BridgeTest.java
+++ src/test/java/bridge/domain/BridgeTest.java
@@ -1,5 +1,7 @@
 package bridge.domain;
 
+import bridge.BridgeInfo;
+import bridge.constant.Token;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -14,5 +16,31 @@ public class BridgeTest {
         Bridge bridge = new Bridge(List.of("U", "D"));
         assertThat(bridge.update("U")).isEqualTo(true);
         assertThat(bridge.update("U")).isEqualTo(false);
+    }
+
+    @DisplayName("위쪽 브릿지에 대한 결과 확인")
+    @Test
+    void upperBridgeCheck() {
+        Bridge bridge = new Bridge(List.of("U", "D", "D"));
+        bridge.update("U");
+        bridge.update("D");
+        bridge.update("U");
+        List<BridgeInfo> upperBridge = bridge.getBridgeInfo(Token.UP.getMark());
+        assertThat(upperBridge.get(0)).isEqualTo(BridgeInfo.CORRECT);
+        assertThat(upperBridge.get(1)).isEqualTo(BridgeInfo.EMPTY);
+        assertThat(upperBridge.get(2)).isEqualTo(BridgeInfo.WRONG);
+    }
+
+    @DisplayName("아래쪽 브릿지에 대한 결과 확인")
+    @Test
+    void lowerBridgeCheck() {
+        Bridge bridge = new Bridge(List.of("U", "D", "D"));
+        bridge.update("U");
+        bridge.update("D");
+        bridge.update("U");
+        List<BridgeInfo> upperBridge = bridge.getBridgeInfo(Token.DOWN.getMark());
+        assertThat(upperBridge.get(0)).isEqualTo(BridgeInfo.EMPTY);
+        assertThat(upperBridge.get(1)).isEqualTo(BridgeInfo.CORRECT);
+        assertThat(upperBridge.get(2)).isEqualTo(BridgeInfo.EMPTY);
     }
 }
