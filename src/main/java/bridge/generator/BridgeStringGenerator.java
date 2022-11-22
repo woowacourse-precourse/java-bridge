74136f20ff42f0c90b2d0afeff47cf32b8e08c3c
hunko <rhwlgns4386@naver.com>
2022-11-22 21:42:40 +0900

    fix : edit the part beyond 10 lines

--- src/main/java/bridge/generator/BridgeStringGenerator.java
+++ src/main/java/bridge/generator/BridgeStringGenerator.java
@@ -23,14 +23,23 @@ public abstract class BridgeStringGenerator {
 
         sb.append("[ ");
 
+        generateMiddleString(footprints, direction, sb);
+
+        sb.append(getLastString(footprints, direction));
+        sb.append(" ]\n");
+        return sb.toString();
+    }
+
+    private String getLastString(List<String> footprints, String direction) {
+        if(footprints.size()!=0)return makeLastFigure(footprints.get(footprints.size() - 1), direction);
+        return " ";
+    }
+
+    private void generateMiddleString(List<String> footprints, String direction, StringBuilder sb) {
         for (int j = 0; j < footprints.size() - 1; j++) {
             sb.append(makeCircleString(footprints.get(j), direction));
             sb.append(" | ");
         }
-
-        sb.append(makeLastFigure(footprints.get(footprints.size() - 1), direction));
-        sb.append(" ]\n");
-        return sb.toString();
     }
 
     protected String makeCircleString(String footprintDirection, String checkDirection) {
