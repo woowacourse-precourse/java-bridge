6ff30674b07d9e3a7b3c2462f8a4e8d262c426f0
Dabeen Jeong <hello70825@gmail.com>
2022-11-22 17:23:27 +0900

    feat: 초기값 설정

--- src/main/java/bridge/BridgeGame.java
+++ src/main/java/bridge/BridgeGame.java
@@ -1,9 +1,23 @@
 package bridge;
 
+import bridge.domain.*;
+
 /**
  * 다리 건너기 게임을 관리하는 클래스
  */
 public class BridgeGame {
+    Bridge answerBridge;
+    BridgePrintMaker bridgePrintMaker;
+    Integer position;
+    Integer tryCount;
+    Result result;
+
+    public BridgeGame() {
+        this.bridgePrintMaker = new BridgePrintMaker();
+        this.position = 0;
+        this.tryCount = 1;
+        this.result = Result.CONTINUE;
+    }
 
     /**
      * 사용자가 칸을 이동할 때 사용하는 메서드
