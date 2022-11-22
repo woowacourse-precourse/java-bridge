6f64c1e2299238e6431ff7d2914e29720c888702
mng990 <ryoo0504@pusan.ac.kr>
2022-11-22 09:41:23 +0900

    feat(main/java/bridge/OutputView.java): OutputView.java 작성
    + getDirection()
    + getState()
    + updateMap()
    + drawMap()
    + drawResult()

--- src/main/java/bridge/OutputView.java
+++ src/main/java/bridge/OutputView.java
@@ -3,14 +3,22 @@ package bridge;
 /**
  * 사용자에게 게임 진행 상황과 결과를 출력하는 역할을 한다.
  */
+
+import java.util.ArrayList;
+import java.util.List;
 public class OutputView {
 
+    private static final String EMPTY = " ";
+    private static final List<String> RESULT_MESSAGE =
+            new ArrayList<String>(List.of("최종 게임 결과",  "게임 성공 여부: ","총 시도한 횟수: "));
+    private static final List<String> BRIDGE_MESSAGE = new ArrayList<String>(List.of("[ "," | ", " ]"));
     /**
      * 현재까지 이동한 다리의 상태를 정해진 형식에 맞춰 출력한다.
      * <p>
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void printMap() {
+    public void printMap(List<Glass> history, State lastState) {
+        System.out.println(drawMap(history, lastState));
     }
 
     /**
@@ -18,6 +26,50 @@ public class OutputView {
      * <p>
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void printResult() {
+    public void printResult(List<Glass> history, State lastState, int play) {
+        System.out.println(drawResult(history, lastState, play));
+    }
+
+    public String drawMap(List<Glass> history, State lastState){
+        List<String> map= new ArrayList<String>(List.of(BRIDGE_MESSAGE.get(0),BRIDGE_MESSAGE.get(0)));
+
+        for(int hIdx = 0; hIdx < history.size(); hIdx++){
+            int dir = getDirection(history, hIdx, lastState);
+            State state = getState(hIdx, history.size(), lastState);
+
+            updateMap(map, dir, state);
+        }
+        return map.get(0) + System.lineSeparator() + map.get(1) + System.lineSeparator() ;
+    }
+
+    public State getState(int historyIdx, int historySize, State lastState){
+        if(historyIdx == historySize -1){
+            return lastState;
+        }
+        return State.SAFE;
+    }
+
+    public Integer getDirection(List<Glass> history, int historyIdx, State lastState){
+        int direction = history.get(historyIdx).DIRECTION();
+        if(historyIdx == history.size() - 1 && !lastState.ALIVE()){
+            return 1 - direction;
+        }
+        return direction;
+    }
+
+    public void updateMap(List<String> map, int dir, State state){
+        map.set(dir, map.get(dir) + state.OX() + BRIDGE_MESSAGE.get(1));
+        map.set(1-dir, map.get(1-dir) + EMPTY + BRIDGE_MESSAGE.get(1));
+    }
+
+
+    public String drawResult(List<Glass> history, State lastState, int play){
+        String message = "";
+        message += RESULT_MESSAGE.get(0) + System.lineSeparator();
+        message += drawMap(history, lastState) + System.lineSeparator();
+        message += RESULT_MESSAGE.get(1) + lastState.RESULT() + System.lineSeparator();
+        message += RESULT_MESSAGE.get(2) + play;
+
+        return message;
     }
 }
