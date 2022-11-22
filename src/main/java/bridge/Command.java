5aa9a0d99dcf1b86c4cdf0897f3e24bf176346df
jwj <real980312@gmail.com>
2022-11-22 20:27:04 +0900

    <feat> : 메세지들 command로 리팩토링

---
+++ src/main/java/bridge/Command.java
@@ -0,0 +1,20 @@
+package bridge;
+
+public class Command {
+    public static final String START_MESSAGE = "다리 건너기 게임을 시작합니다.";
+    public static final String LENGTH_MESSAGE = "다리의 길이를 입력해주세요.";
+    public static final String UPDOWN_MESSAGE = "이동할 칸을 선택해주세요. (위: U, 아래: D)";
+    public static final String REGAME_MESSAGE = "게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)";
+    public static final String RESULT_MESSAGE = "최종 게임 결과";
+    public static final String GAME_RESULT = "게임 성공 여부: 성공/실패";
+    public static final String TRY_MESSAGE = "총 시도한 횟수: ";
+
+    public static final String ALPHABET_R = "R";
+    public static final String ALPHABET_Q = "Q";
+    public static final String ALPHABET_U = "U";
+    public static final String ALPHABET_D = "D";
+    public static final String ALPHABET_X = " X ";
+    public static final String BLANK = "   ";
+    public static final String ALPHABET_O = " O ";
+
+}
--- src/main/java/bridge/GameController.java
+++ src/main/java/bridge/GameController.java
@@ -1,9 +1,7 @@
 package bridge;
 
 public class GameController {
-    private static final String START_MESSAGE = "다리 건너기 게임을 시작합니다.";
-    private static final String LENGTH_MESSAGE = "다리의 길이를 입력해주세요.";
-    private static final String UPDOWN_MESSAGE = "이동할 칸을 선택해주세요. (위: U, 아래: D)";
+
 
     private BridgeGame bridgeGame = new BridgeGame();
     private InputView inputView = new InputView();
@@ -13,7 +11,7 @@ public class GameController {
     }
 
     public void GameStart() {
-        System.out.println(START_MESSAGE + "\n" +LENGTH_MESSAGE+ "\n");
+        System.out.println(Command.START_MESSAGE + "\n" +Command.LENGTH_MESSAGE+ "\n");
         inputView.readBridgeSize();
     }
 }
