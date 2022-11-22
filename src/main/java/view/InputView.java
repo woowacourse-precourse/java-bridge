2f129979cedefd42e9630571de1ee27a316730b8
Hoo93 <117077999+Hoo93@users.noreply.github.com>
2022-11-22 18:52:35 +0900

    REFACTOR[InputView]: 불필요한 주석 제거 / 가독성을 위한 변수 추가

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -2,10 +2,13 @@ package view;
 
 import camp.nextstep.edu.missionutils.Console;
 
-/**
- * 사용자로부터 입력을 받는 역할을 한다.
- */
 public class InputView {
+
+    private static final String RESTART = "R";
+    private static final String QUIT = "Q";
+    private static final String UP = "U";
+    private static final String DOWN = "D";
+
 
     public int readBridgeSize() {
         System.out.println("다리의 길이를 입력해주세요.");
@@ -20,21 +23,17 @@ public class InputView {
         System.out.println("이동할 칸을 선택해주세요. (위: U, 아래: D)");
         String userInput = Console.readLine();
         userInput = userInput.toUpperCase().trim();
-        if (!userInput.equals("U") && !userInput.equals("D")){
+        if (!userInput.equals(UP) && !userInput.equals(DOWN)){
             throw new IllegalArgumentException("[ERROR] : U 또는 D 만 입력해 주세요. ");
         }
         return userInput;
     }
-
-    /**
-     * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
-     */
     public String readGameCommand() {
         System.out.println("게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)");
         String userInput = Console.readLine();
         userInput = userInput.toUpperCase().trim();
         System.out.println(userInput);
-        if (!userInput.equals("R") && !userInput.equals("Q")){
+        if (!userInput.equals(RESTART) && !userInput.equals(QUIT)){
             throw new IllegalArgumentException("[ERROR] : R 또는 Q 만 입력해 주세요. ");
         }
         return userInput;
