0a1beec7e5dd3a7be5a3791e935fd0b99ebfce01
shpark30 <seohyeonpark30.gmail.com>
2022-11-22 10:30:22 +0900

    refactor(Player): 관리 책임 변경
    기존 : 진행 경과 관리
    변경 : 생사 여부(게임 실패 여부) 관리

--- src/main/java/bridge/domain/Player.java
+++ src/main/java/bridge/domain/Player.java
@@ -2,32 +2,50 @@ package bridge.domain;
 
 import static org.assertj.core.util.Lists.newArrayList;
 
-import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * 플레이어 상태 관리
+ */
 public class Player {
     private final List<String> DIRECTIONS = newArrayList(
             Direction.DOWN.toString(), Direction.UP.toString());
-    private List<String> path;
     private Map map;
+    private boolean isAlive;
 
-    public Player() {
-        this.path = new ArrayList<>();
-        this.map = new Map();
+    public Player(Map map) {
+        this.map = map;
+        this.isAlive = true;
     }
 
-    public void move(String direction, String answer) {
+    /**
+     * 플레이어 이동에 따라 생사여부(게임 실패 여부)와 지도 업데이트
+     *
+     * @param direction: 이동 방향(U, D)
+     * @param answer: 정답 방향(U, D)
+     */
+    public boolean move(String direction, String answer) {
         validateDirection(direction);
-        map.update(direction, answer);
-        path.add(direction);
+        isAlive = direction.equals(answer);
+        map.update(direction, isAlive);
+        return isAlive;
     }
 
-    public List<String> generateMap() {
+    /**
+     * 메소드 호출 시점까지의 진행 경과 반환
+     *
+     * @return 메소드 호출 시점까지의 진행 경과
+     */
+    public List<String> checkMap() {
         return map.getCurrentMap();
     }
 
+    public boolean isAlive() {
+        return isAlive;
+    }
+
     public boolean hasSucceeded(List<String> bridge) {
-        return path.equals(bridge);
+        return map.getPassedPath().equals(bridge);
     }
 
     private void validateDirection(String direction) {
