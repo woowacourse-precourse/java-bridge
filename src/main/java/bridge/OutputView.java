8ecca4758eb7ee3fe416530dcbe9bf6805c674b8
beombu <sunil1369@bible.ac.kr>
2022-11-22 12:09:02 +0900

    Feat(Output) : 현재 다리의 이동 상태 출력기능 추가

--- src/main/java/bridge/OutputView.java
+++ src/main/java/bridge/OutputView.java
@@ -1,10 +1,15 @@
 package bridge;
 
+import java.util.List;
+
 /**
  * 사용자에게 게임 진행 상황과 결과를 출력하는 역할을 한다.
  */
 public class OutputView {
     public static final String START_MESSAGE = "다리 건너기 게임을 시작합니다.";
+    public static final String START_SQUARE_BRACKET = "[ ";
+    public static final String END_SQUARE_BRACKET = " ]";
+    public static final String POLICE_LINE = " | ";
 
 
     public void startGameMessage(){
@@ -16,7 +21,22 @@ public class OutputView {
      * <p>
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void printMap() {
+    public void printMap(List<BridgeResult> resultTopBridge, List<BridgeResult> resultBottomBridge) {
+        System.out.println(START_SQUARE_BRACKET + getBridgeResult(resultTopBridge) +  END_SQUARE_BRACKET);
+        System.out.println(START_SQUARE_BRACKET + getBridgeResult(resultBottomBridge) +  END_SQUARE_BRACKET);
+    }
+
+    public String getBridgeResult(List<BridgeResult> resultBridge){
+        String result = "";
+        for(BridgeResult cell : resultBridge){
+            result += cell.getValue() + POLICE_LINE;
+        }
+        result = removeLastLine(result);
+        return result;
+    }
+
+    private String removeLastLine(String result) {
+        return result.substring(0, result.length() - 3);
     }
 
     /**
