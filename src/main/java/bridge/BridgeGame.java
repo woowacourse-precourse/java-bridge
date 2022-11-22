44d9fa1ad0cc2bd4881db8775d15cc05200485a2
tlsqj <rnjstlsqja@inha.edu>
2022-11-22 20:01:11 +0900

    feat(BridgeGame.java): BridgeGame.java 구현
    - move()
        - 칸을 이동 시킴
        - 이동 시킨 결과값을 return
    
    - retry()
        - 게임을 다시 시도할 때 사용

--- src/main/java/bridge/BridgeGame.java
+++ src/main/java/bridge/BridgeGame.java
@@ -3,14 +3,28 @@ package bridge;
 /**
  * 다리 건너기 게임을 관리하는 클래스
  */
+
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Objects;
+
 public class BridgeGame {
 
+    BridgeCheck bridgeCheck = new BridgeCheck();
     /**
      * 사용자가 칸을 이동할 때 사용하는 메서드
      * <p>
      * 이동을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void move() {
+    public static String move(List<String> bridgeInfo, int bridgeLength) {
+        String checkResult;
+        for (int i = 0; i < bridgeLength; i++){
+            checkResult = BridgeCheck.isRightBridge(bridgeInfo, i);
+            if (checkResult.contains("Q")) return checkResult;
+            if (checkResult == "R") i--;
+        }
+
+        return "C";
     }
 
     /**
@@ -18,6 +32,12 @@ public class BridgeGame {
      * <p>
      * 재시작을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void retry() {
+    public static String retry(String reGameCommendResult, int i) {
+        if (reGameCommendResult.equals("Q")){
+            String tmp  = "Q" + Integer.toString(i);
+            return tmp;
+        }
+
+        return "R";
     }
 }
