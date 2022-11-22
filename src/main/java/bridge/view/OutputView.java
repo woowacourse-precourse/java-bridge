b50be60b3bace8d49ebaa7ae85755c9e73915481
gomljo <gomljo0307@naver.com>
2022-11-22 09:10:24 +0900

    feat(OutputView): 시작 메시지 출력 기능 구현

--- src/main/java/bridge/view/OutputView.java
+++ src/main/java/bridge/view/OutputView.java
@@ -6,6 +6,12 @@ import java.io.FilterOutputStream;
  * 사용자에게 게임 진행 상황과 결과를 출력하는 역할을 한다.
  */
 public class OutputView {
-    public void printErrorMessage(String errorMessage){System.out.println(errorMessage);}
-    
+    public void printErrorMessage(String errorMessage) {
+        System.out.println(errorMessage);
+    }
+
+    public void printStartMessage() {
+        System.out.println("다리 건너기 게임을 시작합니다.");
+    }
+
 }
