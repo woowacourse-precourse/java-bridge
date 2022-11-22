b47189722d023cb0a613b5561e785e0461f8e0e5
jsk5341 <102288039+jsk5341@users.noreply.github.com>
2022-11-22 20:57:54 +0900

    remove: erase output for debug

--- src/main/java/bridge/BridgeMaker.java
+++ src/main/java/bridge/BridgeMaker.java
@@ -24,7 +24,6 @@ public class BridgeMaker {
             int randomNumber = bridgeNumberGenerator.generate();
             newBridge.add(bridgeIndex,convertNumbersToDorU(randomNumber, bridgeIndex));
         }
-        System.out.println(newBridge);
         return newBridge;
     }
 
