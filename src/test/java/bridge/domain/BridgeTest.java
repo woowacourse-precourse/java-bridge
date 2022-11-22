c576d72c90b0a2ef665927863bbee04dc462059e
cocomozila <joseyun94@naver.com>
2022-11-22 12:30:27 +0900

    feat(BridgeTest): `정답과 비교하여 boolean값 반환 테스트1` 추가

--- src/test/java/bridge/domain/BridgeTest.java
+++ src/test/java/bridge/domain/BridgeTest.java
@@ -23,6 +23,17 @@ class BridgeTest extends NsTest {
         assertThat(map).containsExactly("O", "X");
     }
 
+    @Test
+    @DisplayName("정답과 비교하여 boolean값 반환 테스트1")
+    void 정답과_비교하여_boolean값_반환_테스트() {
+        Bridge bridge = new Bridge(List.of("U", "D", "U"));
+        bridge.initUserKeyList();
+        bridge.move("U");
+        bridge.move("U");
+        boolean answer = bridge.isAnswer();
+        assertThat(answer).isEqualTo(false);
+    }
+
     @Override
     protected void runMain() {
         Application.main(new String[]{});
