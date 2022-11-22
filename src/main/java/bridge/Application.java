7a5efc371aeafc1e7c8cf6b0dd19a1bf8d46db74
sschan99 <seungchan1219@gmail.com>
2022-11-22 13:40:29 +0900

    feat(BridgeGame): cross a bridge according to the player's input

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -1,8 +1,21 @@
 package bridge;
 
 public class Application {
+    static final InputView inputView = new InputView();
+    static final OutputView outputView = new OutputView();
+    static final BridgeMaker bridgeMaker = new BridgeMaker(new BridgeRandomNumberGenerator());
 
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        System.out.println("다리 건너기 게임을 시작합니다.\n");
+        BridgeGame bridgeGame = new BridgeGame(bridgeMaker.makeBridge(inputView.readBridgeSize()));
+
+        while (bridgeGame.move(inputView.readMoving())) {
+            outputView.printMap(bridgeGame.getBridge(), bridgeGame.getUserMove());
+            if (bridgeGame.isClear()) {
+                break;
+            }
+        }
+        System.out.println("최종 게임 결과");
+        outputView.printMap(bridgeGame.getBridge(), bridgeGame.getUserMove());
     }
 }
