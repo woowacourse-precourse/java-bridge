a6aec62356f6322b166a2810344a19fe9ff85466
jmjmjames <chungjm0711@gmail.com>
2022-11-22 19:23:02 +0900

    feat: BridgeGameService$play 메서드 추가

--- src/main/java/bridge/service/BridgeGameService.java
+++ src/main/java/bridge/service/BridgeGameService.java
@@ -1,8 +1,12 @@
 package bridge.service;
 
+import bridge.common.ErrorMessage;
 import bridge.domain.BridgeGame;
 import bridge.domain.BridgeGameAnswer;
 import bridge.domain.BridgeMaker;
+import bridge.domain.Move;
+import bridge.domain.Player;
+import bridge.dto.MoveResultDto;
 import java.util.List;
 
 public class BridgeGameService {
@@ -13,17 +17,33 @@ public class BridgeGameService {
         this.bridgeMaker = bridgeMaker;
     }
 
+    /**
+     * size 를 통해 인스턴스 변수 BridgeGame 초기화
+     *
+     * @param size
+     */
     public void initBridgeGame(int size) {
         List<String> bridge = bridgeMaker.makeBridge(size);
         BridgeGameAnswer gameAnswer = new BridgeGameAnswer(bridge);
         this.bridgeGame = new BridgeGame(gameAnswer);
     }
 
-    public boolean isRunning() {
-        return bridgeGame.isRunning();
+    /**
+     * 이동할 때 사용하는 메서드
+     *
+     * @param player
+     * @param command
+     * @return MoveResultDto
+     */
+    public MoveResultDto play(Player player, String command) {
+        if (!isPlaying()) {
+            throw new IllegalArgumentException(ErrorMessage.isInvalidGameStatus());
+        }
+        player.move(bridgeGame, Move.of(command));
+        return MoveResultDto.from(player.getBridgeGameResult());
     }
 
-    public boolean isGameOver() {
-        return bridgeGame.isGameOver();
+    public boolean isPlaying() {
+        return bridgeGame.isPlayStatus();
     }
 }
