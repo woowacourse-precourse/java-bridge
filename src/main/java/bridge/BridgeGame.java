3b41b0c74006ad0c6a9d7ff28c341ce64a121a8d
Bue-von-hon <dkssudvn2@gmail.com>
2022-11-22 18:22:10 +0900

    refactor : 불필요한 코드 제거

--- src/main/java/bridge/BridgeGame.java
+++ src/main/java/bridge/BridgeGame.java
@@ -25,17 +25,16 @@ public class BridgeGame {
      * <p>
      * 이동을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public boolean move(final String direction) {
+    public void move(final String direction) {
         require(isNotUpOrDown(direction), Error.MOVE);
         require(isNotProgress(), Error.STATE);
         input.add(direction);
         if (isSame(answer.get(position), direction)) {
             state = State.Loss;
-            return false;
+            return;
         }
         position++;
         if (isEnd(answer)) state = State.Win;
-        return true;
     }
 
     private boolean isNotProgress() {
