e9add6c3dade58a1dde4203f1d13e947417fc5ab
kimdeokyoung <dudejrss@naver.com>
2022-11-22 14:16:59 +0900

    refact : State, Trigger 기능 이동 (BridgeGame -> Application)

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -1,8 +1,50 @@
 package bridge;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class Application {
 
+    final private Map<List<Integer>, State> rules = setRules();
+    private State currentState;
     public static void main(String[] args) {
         // TODO: 프로그램 구현
     }
+    private enum State {
+        INITIAL_STATE, SELECTION_STATE, SUCCESS_TEST_STATE, FINISH_TEST_STATE, END
+    }
+
+    private enum TRIGGER {
+        INPUT_VALID, INPUT_INVALID, SELECTION_FAILURE, SELECTION_SUCCESS, CONTINUE, FINISH
+    }
+
+    private Map<List<Integer>, State> setRules() {
+        Map<List<Integer>, State> result = new HashMap<>();
+        setSelectionStateRules(result);
+        setSuccessTestStateRules(result);
+        setFinishStateRules(result);
+        return result;
+    }
+
+    private void setSelectionStateRules(Map<List<Integer>, State> rules){
+        rules.put(List.of(State.SELECTION_STATE.ordinal(), TRIGGER.INPUT_INVALID.ordinal()),
+                State.SELECTION_STATE);
+        rules.put(List.of(State.SELECTION_STATE.ordinal(), TRIGGER.INPUT_VALID.ordinal()),
+                State.SUCCESS_TEST_STATE);
+    }
+    private void setSuccessTestStateRules(Map<List<Integer>, State> rules){
+        rules.put(List.of(State.SUCCESS_TEST_STATE.ordinal(), TRIGGER.SELECTION_FAILURE.ordinal()),
+                State.SELECTION_STATE);
+        rules.put(List.of(State.SUCCESS_TEST_STATE.ordinal(), TRIGGER.SELECTION_SUCCESS.ordinal()),
+                State.FINISH_TEST_STATE);
+    }
+    private void setFinishStateRules(Map<List<Integer>, State> rules){
+        rules.put(List.of(State.FINISH_TEST_STATE.ordinal(), TRIGGER.CONTINUE.ordinal()),
+                State.SELECTION_STATE);
+        rules.put(List.of(State.FINISH_TEST_STATE.ordinal(), TRIGGER.FINISH.ordinal()),
+                State.END);
+    }
+
+
 }
--- src/main/java/bridge/BridgeGame.java
+++ src/main/java/bridge/BridgeGame.java
@@ -9,10 +9,6 @@ import java.util.Map;
  */
 public class BridgeGame {
 
-    final private Map<List<Integer>, State> rules = setRules();
-    private State currentState;
-
-
     /**
      * 사용자가 칸을 이동할 때 사용하는 메서드
      * <p>
@@ -29,40 +25,5 @@ public class BridgeGame {
     public void retry() {
     }
 
-
-    private enum State {
-        INITIAL_STATE, SELECTION_STATE, SUCCESS_TEST_STATE, FINISH_TEST_STATE, END
-    }
-
-    private enum TRIGGER {
-        INPUT_VALID, INPUT_INVALID, SELECTION_FAILURE, SELECTION_SUCCESS, CONTINUE, FINISH
-    }
-
-    private Map<List<Integer>, State> setRules() {
-        Map<List<Integer>, State> result = new HashMap<>();
-        setSelectionStateRules(result);
-        setSuccessTestStateRules(result);
-        setFinishStateRules(result);
-        return result;
-    }
-
-    private void setSelectionStateRules(Map<List<Integer>, State> rules){
-        rules.put(List.of(State.SELECTION_STATE.ordinal(), TRIGGER.INPUT_INVALID.ordinal()),
-                State.SELECTION_STATE);
-        rules.put(List.of(State.SELECTION_STATE.ordinal(), TRIGGER.INPUT_VALID.ordinal()),
-                State.SUCCESS_TEST_STATE);
-    }
-    private void setSuccessTestStateRules(Map<List<Integer>, State> rules){
-        rules.put(List.of(State.SUCCESS_TEST_STATE.ordinal(), TRIGGER.SELECTION_FAILURE.ordinal()),
-                State.SELECTION_STATE);
-        rules.put(List.of(State.SUCCESS_TEST_STATE.ordinal(), TRIGGER.SELECTION_SUCCESS.ordinal()),
-                State.FINISH_TEST_STATE);
-    }
-    private void setFinishStateRules(Map<List<Integer>, State> rules){
-        rules.put(List.of(State.FINISH_TEST_STATE.ordinal(), TRIGGER.CONTINUE.ordinal()),
-                State.SELECTION_STATE);
-        rules.put(List.of(State.FINISH_TEST_STATE.ordinal(), TRIGGER.FINISH.ordinal()),
-                State.END);
-    }
 
 }
