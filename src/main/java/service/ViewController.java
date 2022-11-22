75ed300dbc09c9e6cabe1a366c593f94ac7fad59
UngHeung <t01047377477@gmail.com>
2022-11-22 14:14:58 +0900

    feat : (ViewController) 게임 시작, 다리 크기 관련 입출력 기능 추가

--- src/main/java/service/ViewController.java
+++ src/main/java/service/ViewController.java
@@ -3,4 +3,11 @@ package service;
 public class ViewController {
 	private InputView in = new InputView();
 	private OutputView out = new OutputView();
+
+	public int startView() {
+		out.printGameMessage("START");
+		out.printGameMessage("BRIDGE_SIZE");
+		int size = in.readBridgeSize();
+		return size;
+	}
 }
