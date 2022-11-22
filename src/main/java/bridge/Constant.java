7a11acb37956bf2fb22ead48fb97e31edb5398d0
oreo <ohsimon77@naver.com>
2022-11-22 20:08:44 +0900

    Feat : BridgeResult enum 클래스 생성 (다리 구조 생성)

--- src/main/java/bridge/Constant.java
+++ src/main/java/bridge/Constant.java
@@ -3,8 +3,8 @@ package bridge;
 public class Constant {
     public static final String START_BRIDGE_GAME = "다리 건너기 게임을 시작합니다.\n";
     public static final String TOTAL_GAME_RESULT = "\n최종 게임 결과";
-    public static final String SUCCESS_FAIL = "게임 성공 여부 : ";
-    public static final String TOTAL_COUNT = "총 시도한 횟수 : ";
+    public static final String SUCCESS_FAIL = "게임 성공 여부: ";
+    public static final String TOTAL_COUNT = "총 시도한 횟수: ";
     public static final String INPUT_BRIDGE_LENGTH = "다리의 길이를 입력해주세요.";
     public static final String RETRY_OR_QUIT = "\n게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)";
     public static final String CHOOSE_MOVE = "\n이동할 칸을 선택해주세요. (위: U, 아래: D)";
