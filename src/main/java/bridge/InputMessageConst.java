013c465f368ce7627f0459564a28233fa074b53c
namtndus <rnrmf0721@gmail.com>
2022-11-22 14:14:14 +0900

    refactor(InputMessageConst): InputView의 질문을 저장한 상수 클래스

---
+++ src/main/java/bridge/InputMessageConst.java
@@ -0,0 +1,9 @@
+package bridge;
+
+public class InputMessageConst {
+
+    public static final String READ_BRIDGE_SIZE_MESSAGE = "다리의 길이를 입력해주세요.";
+    public static final String READ_MOVING_MESSAGE = "이동할 칸을 선택해주세요. (위: U, 아래: D)";
+    public static final String READ_GAME_COMMAND_MESSAGE = "게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)";
+
+}
--- src/main/java/bridge/InputView.java
+++ src/main/java/bridge/InputView.java
@@ -1,7 +1,6 @@
 package bridge;
 
-import java.util.PrimitiveIterator;
-
+import static bridge.InputMessageConst.*;
 import static camp.nextstep.edu.missionutils.Console.readLine;
 
 /**
@@ -9,9 +8,6 @@ import static camp.nextstep.edu.missionutils.Console.readLine;
  */
 public class InputView {
 
-    private final String READ_BRIDGE_SIZE_MESSAGE = "다리의 길이를 입력해주세요.";
-    private final String READ_MOVING_MESSAGE = "이동할 칸을 선택해주세요. (위: U, 아래: D)";
-    private final String READ_GAME_COMMAND_MESSAGE = "게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)";
     private final String ERROR_NOT_NUMBER_MESSAGE = "[ERROR] 숫자만 입력을 받습니다." ;
     private final String ERROR_SCOPE_NUMBER_MESSAGE = "[ERROR] 다리 길이는 3부터 20 사이의 숫자여야 합니다.";
     private final String ERROR_MOVING_COMMAND_MESSAGE = "[ERROR] U와 D 중에 하나를 입력하세요.";
