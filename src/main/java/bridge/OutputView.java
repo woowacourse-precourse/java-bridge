ac1b0c35e125cf80653bf5bd31c8e0b6f76c9f8d
No BongJun <sewd1224@gmail.com>
2022-11-22 15:02:35 +0900

    [Add] <OutputView> Print present_map, Print final_result

--- src/main/java/bridge/OutputView.java
+++ src/main/java/bridge/OutputView.java
@@ -11,7 +11,14 @@ public class OutputView {
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
     public void printMap() {
+        String move_print = BridgeGame.map_move();
+
+        System.out.println("[ " + move_print + " ]");
+        System.out.println("[ " + move_print + " ]");
     }
+
+
+
 
     /**
      * 게임의 최종 결과를 정해진 형식에 맞춰 출력한다.
@@ -19,5 +26,10 @@ public class OutputView {
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
     public void printResult() {
+
+        String final_result = BridgeGame.endgame_1();
+        int final_count = BridgeGame.endgame_2();
+        System.out.println("\n게임 성공 여부: " + final_result);
+        System.out.println("총 시도한 횟수: " + final_count);
     }
 }
