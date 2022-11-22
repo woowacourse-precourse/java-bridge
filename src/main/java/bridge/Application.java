2789ede17cd06acf93fde80c4a432dca2a77276a
Dennis-K1 <dennis.k1.dev@gmail.com>
2022-11-22 15:30:08 +0900

    refactor: Shorten method length

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -1,57 +1,28 @@
 package bridge;
 
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 public class Application {
 
     public static void main(String[] args) {
         // TODO: 프로그램 구현
-        InputView inputView = new InputView();
-        BridgeNumberGenerator bridgeNumberGenerator = new BridgeRandomNumberGenerator();
-        BridgeMaker bridgeMaker = new BridgeMaker(bridgeNumberGenerator);
-
         try {
-            List<String> bridge = bridgeMaker.makeBridge(inputView.readBridgeSize());
-            System.out.println(bridge);
-            BridgeGame bridgeGame = new BridgeGame();
-
-
-            OutputView outputView = new OutputView();
-            // 0성공, 1실패
-            // move의 리턴이 0이라면 게임 move 계속 진행
-            int status = 0;
-            int stage = 1;
-            int trial = 1;
-            int success = 0;
-            while (status == 0) {
-                if (stage == bridge.size() + 1) {
-                    success = 1;
-                    break;
-                }
-                status = bridgeGame.move(bridge, stage);
-                outputView.printMap(bridge, stage, status);
-                stage++;
-                if (status == 1) {
-                    System.out.println();
-                    outputView.printGameOver();
-                    if (inputView.readGameCommand().equals("R")) {
-                        stage = 1;
-                        status = 0;
-                        trial++;
-                    }
-                }
-            }
-            System.out.println();
-            outputView.printResult();
-            outputView.printMap(bridge, bridge.size(), 0);
-
-            System.out.println();
-            outputView.printSuccess(success);
-            outputView.printTrialAmount(trial);
+            run();
         } catch (IllegalArgumentException e){
             System.out.println(e.getMessage());
         }
+    }
+
+
+    public static void run(){
+        InputView inputView = new InputView();
+        BridgeNumberGenerator bridgeNumberGenerator = new BridgeRandomNumberGenerator();
+        BridgeMaker bridgeMaker = new BridgeMaker(bridgeNumberGenerator);
+        BridgeGame bridgeGame = new BridgeGame();
+        OutputView outputView = new OutputView();
+        outputView.printGameStart();
+        List<String> bridge = bridgeMaker.makeBridge(inputView.readBridgeSize());
+        bridgeGame.game(bridge);
+        bridgeGame.finalResult(bridge);
     }
 }
