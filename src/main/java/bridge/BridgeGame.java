749a541dc54aa9afb5a7621a406b570d7efdd1ee
JunTaeINC <juntea3523@naver.com>
2022-11-22 20:33:38 +0900

    style : BridgeGame 하드코딩 제거 / 상수화

--- src/main/java/bridge/BridgeGame.java
+++ src/main/java/bridge/BridgeGame.java
@@ -5,13 +5,14 @@ import bridge.model.Moving;
 import bridge.model.ReEnter;
 
 public class BridgeGame {
+    private final static String RE_START = "R";
 
     public static boolean move(String computer, String userInput) {
         return computer.equals(userInput);
     }
 
     public static boolean retry() {
-        if (ReEnter.reGetExitOrRestart().equals("R")) {
+        if (ReEnter.reGetExitOrRestart().equals(RE_START)) {
             Moving.clearMoving();
             return true;
         }
