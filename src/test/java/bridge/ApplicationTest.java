fafa41bb38ff905ac0ca044fd54b5a547b710709
Min-ji99 <61926751+Min-ji99@users.noreply.github.com>
2022-11-22 15:53:00 +0900

    Test : ApplicationTest 기능 테스트 추가

--- src/test/java/bridge/ApplicationTest.java
+++ src/test/java/bridge/ApplicationTest.java
@@ -24,7 +24,7 @@ class ApplicationTest extends NsTest {
     }
 
     @Test
-    void 기능_테스트() {
+    void 기능_테스트1() {
         assertRandomNumberInRangeTest(() -> {
             run("3", "U", "D", "U");
             assertThat(output()).contains(
@@ -40,7 +40,40 @@ class ApplicationTest extends NsTest {
             assertThat(upSideIndex).isLessThan(downSideIndex);
         }, 1, 0, 1);
     }
+    @Test
+    void 기능_테스트2() {
+        assertRandomNumberInRangeTest(() -> {
+            run("5", "U", "D", "U", "U", "R", "U", "D", "U", "D", "D");
+            assertThat(output()).contains(
+                    "최종 게임 결과",
+                    "[ O |   | O |   |   ]",
+                    "[   | O |   | O | O ]",
+                    "게임 성공 여부: 성공",
+                    "총 시도한 횟수: 2"
+            );
 
+            int upSideIndex = output().indexOf("[ O |   | O |   |   ]");
+            int downSideIndex = output().indexOf("[   | O |   | O | O ]");
+            assertThat(upSideIndex).isLessThan(downSideIndex);
+        }, 1, 0, 1, 0, 0);
+    }
+    @Test
+    void 기능_테스트3() {
+        assertRandomNumberInRangeTest(() -> {
+            run("5", "U", "D", "U", "U", "R", "U", "D", "U", "D", "U", "Q");
+            assertThat(output()).contains(
+                    "최종 게임 결과",
+                    "[ O |   | O |   | X ]",
+                    "[   | O |   | O |   ]",
+                    "게임 성공 여부: 실패",
+                    "총 시도한 횟수: 2"
+            );
+
+            int upSideIndex = output().indexOf("[ O |   | O |   | X ]");
+            int downSideIndex = output().indexOf("[   | O |   | O |   ]");
+            assertThat(upSideIndex).isLessThan(downSideIndex);
+        }, 1, 0, 1, 0, 0);
+    }
     @Test
     void 예외_테스트() {
         assertSimpleTest(() -> {
