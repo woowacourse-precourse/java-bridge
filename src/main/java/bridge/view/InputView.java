2d47febe2732c18c0d08a97fd56f197865702116
i960107 <i960107@naver.com>
2022-11-22 16:18:12 +0900

    feat(InputView): 입력 기능 추가

--- src/main/java/bridge/view/InputView.java
+++ src/main/java/bridge/view/InputView.java
@@ -1,28 +1,54 @@
 package bridge.view;
 
-/**
- * 사용자로부터 입력을 받는 역할을 한다.
- */
+import camp.nextstep.edu.missionutils.Console;
+
 public class InputView {
-
-    /**
-     * 다리의 길이를 입력받는다.
-     */
-    public static int readBridgeSize() {
-        return 0;
+    public static Integer readBridgeSize() {
+        Integer bridgeSize = null;
+        System.out.println("다리의 길이를 입력해주세요.");
+        try {
+            bridgeSize = Integer.parseInt(Console.readLine());
+        } catch (NumberFormatException e) {
+            handleInputException(e);
+        }
+        return bridgeSize;
     }
 
-    /**
-     * 사용자가 이동할 칸을 입력받는다.
-     */
+    public static void handleInputException(Exception e) {
+        if (e.getClass().equals(NumberFormatException.class)) {
+            throw new IllegalArgumentException("숫자만 입력해주세요.");
+        }
+    }
+
+
     public static String readMoving() {
-        return null;
+        String moving = null;
+        try {
+            System.out.println("이동할 칸을 선택해주세요. (위: U, 아래: D)");
+            moving = Console.readLine();
+        } catch (Exception e) {
+            handleInputException(e);
+        }
+        checkInputNotNullAndNotEmpty(moving);
+        return moving;
     }
 
-    /**
-     * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
-     */
+
     public static String readGameCommand() {
-        return null;
+        System.out.println("게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)");
+        String command = null;
+        try {
+            command = Console.readLine();
+        } catch (Exception e) {
+            handleInputException(e);
+        }
+        checkInputNotNullAndNotEmpty(command);
+        return command;
+    }
+
+    public static void checkInputNotNullAndNotEmpty(String input) {
+        if (input == null || input.isEmpty()) {
+            throw new IllegalArgumentException("공백이 아닌 문자를 입력해주세요.");
+        }
     }
 }
