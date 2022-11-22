e64e59c70aa3b654a293bbccd37da751eadf8e50
DESKTOP-AUSH0GT\Power <sgh0718@naver.com>
2022-11-22 18:10:37 +0900

    feat : 출력 기능 클래스의 출력에 필요한 문자열 관리 기능
    출력에 필요한 문자열을 모아두고 메세지 출력 메서드 구현

--- src/main/java/bridge/OutputView.java
+++ src/main/java/bridge/OutputView.java
@@ -1,9 +1,22 @@
 package bridge;
 
+import java.util.*;
+
 /**
  * 사용자에게 게임 진행 상황과 결과를 출력하는 역할을 한다.
  */
 public class OutputView {
+
+    String start = "다리 건너기 게임을 시작합니다.";
+    String finish = "최종 게임 결과";
+    String isClear = "게임 성공 여부: ";
+    String attemptNumber = "총 시도한 횟수: ";
+    String success = "성공";
+    String fail = "실패";
+    
+    public void ShowMessage(String message) {
+        System.out.println(message);
+    }
 
     /**
      * 현재까지 이동한 다리의 상태를 정해진 형식에 맞춰 출력한다.
@@ -13,6 +26,7 @@ public class OutputView {
     public void printMap() {
     }
 
+
     /**
      * 게임의 최종 결과를 정해진 형식에 맞춰 출력한다.
      * <p>
@@ -20,4 +34,5 @@ public class OutputView {
      */
     public void printResult() {
     }
+
 }
