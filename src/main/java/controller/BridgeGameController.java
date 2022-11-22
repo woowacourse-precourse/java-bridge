c07377ab6d6aa3e661b819cf529836eace553f4b
jeongwle <dldl4568@naver.com>
2022-11-22 14:15:39 +0900

    chore: 쓰지 않는 import문 삭제

--- src/main/java/controller/BridgeGameController.java
+++ src/main/java/controller/BridgeGameController.java
@@ -3,7 +3,6 @@ package controller;
 import bridge.BridgeMaker;
 import bridge.BridgeNumberGenerator;
 import bridge.BridgeRandomNumberGenerator;
-import java.util.List;
 import model.Bridge;
 import model.BridgeGame;
 import view.InputView;
