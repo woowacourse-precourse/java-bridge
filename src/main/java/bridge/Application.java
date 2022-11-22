1aabc7159e59127b253e449022cb35fb89b2f7b8
hyejuc <chjjudy@naver.com>
2022-11-22 22:21:20 +0900

    feat: 다리 건너기 게임 실행 main 기능 추가

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -1,8 +1,38 @@
 package bridge;
 
+import bridge.view.InputView;
+import bridge.view.OutputView;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Application {
+    private static final InputView inputView = new InputView();
+    private static final OutputView outputView = new OutputView();
+    private static final BridgeGame bridgeGame = new BridgeGame();
+
+    private static List<String> bridge;
+    private static int bridgeSize;
+    private static List<String> upBridgeResult;
+    private static List<String> downBridgeResult;
+    private static String gameStatus;
+    private static int location;
+    private static int cnt;
 
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        outputView.printGameStart();
+        //다리 생성 시작
+        cnt = 0;
+        location = 0;
+        do{
+            cnt ++;
+            upBridgeResult = new ArrayList<>();
+            downBridgeResult = new ArrayList<>();
+            gameStatus = "성공";
+            //playGame();
+        } while(//retry);
+        outputView.printResult(upBridgeResult, downBridgeResult, gameStatus, cnt);
     }
+
 }
