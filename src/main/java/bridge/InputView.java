11c39fc383eaedebae71a8654f0efe6f783411a0
HyowonSin <tlsgydnjs@naver.com>
2022-11-22 22:03:45 +0900

    fix($InputView): fix static of method
    fix static:
    - readMoving()
    - readGameCommand()

--- src/main/java/bridge/InputView.java
+++ src/main/java/bridge/InputView.java
@@ -22,7 +22,7 @@ public class InputView {
     /**
      * 사용자가 이동할 칸을 입력받는다.
      */
-    public String readMoving() {
+    public static String readMoving() {
         System.out.println("이동할 칸을 선택해주세요. (위: U, 아래: D)");
         String moving = camp.nextstep.edu.missionutils.Console.readLine();
 
@@ -36,7 +36,7 @@ public class InputView {
     /**
      * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
      */
-    public String readGameCommand() {
+    public static String readGameCommand() {
         System.out.println("게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)");
         String gameCommand = camp.nextstep.edu.missionutils.Console.readLine();
 
