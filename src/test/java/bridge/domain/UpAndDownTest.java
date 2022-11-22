faa1acb22c94fa6ea118b5d6246e8a7f40e1418a
ddukddaki <rlfrkdms1@naver.com>
2022-11-22 22:11:06 +0900

    test(UpAndDownTest): 입력값이 주어지면 해당 입력 값이 UpAndDown내에 있는지 확인할 수 있다

--- src/test/java/bridge/domain/UpAndDownTest.java
+++ src/test/java/bridge/domain/UpAndDownTest.java
@@ -4,6 +4,8 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 class UpAndDownTest {
 
@@ -25,5 +27,14 @@ class UpAndDownTest {
 
         assertThat(upCommand).isEqualTo("U");
         assertThat(downCommand).isEqualTo("D");
+    }
+
+    @ParameterizedTest
+    @CsvSource({"U,true","D,true","u,false","d,false","I,false"})
+    @DisplayName("입력값이 주어지면 해당 입력 값이 UpAndDown내에 있는지 확인할 수 있다.")
+    void tryIsUpOrDown(String moving, boolean result) {
+        boolean isUpOrDown = UpAndDown.isUpOrDown(moving);
+
+        assertThat(isUpOrDown).isEqualTo(result);
     }
 }
