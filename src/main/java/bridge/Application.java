2c891c59986d07c9cf67ccec8e45ddaaa343175d
JJ503 <63184334+JJ503@users.noreply.github.com>
2022-11-22 14:48:26 +0900

    fix(Application): try-catch 문 적용

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -5,7 +5,11 @@ import bridge.controller.BridgeGameController;
 public class Application {
 
     public static void main(String[] args) {
-        BridgeGameController bridgeGameController = new BridgeGameController();
-        bridgeGameController.gameStart();
+        try {
+            BridgeGameController bridgeGameController = new BridgeGameController();
+            bridgeGameController.gameStart();
+        } catch (IllegalArgumentException exception) {
+            System.out.println(exception.getMessage());
+        }
     }
 }
