e0466eadb1ce2acc8bd498287fe39d7c1e0e8524
ljh <dusk5588@naver.com>
2022-11-22 17:28:30 +0900

    fix(OutputView) : 오타 수정

--- src/main/java/bridge/View/OutputView.java
+++ src/main/java/bridge/View/OutputView.java
@@ -16,7 +16,7 @@ public class OutputView {
     private static final String FAILURE_MESSAGE = "실패";
     private static final String PRINT_SUCCESS_OR_FAILURE_MESSAGE = "게임 성공 여부: ";
     private static final String PRINT_GAME_TRY_COUNT = "총 시도한 횟수: ";
-    private ㄱ가static final String GAME_END_MESSAGE = "최종 게임 결과";
+    private static final String GAME_END_MESSAGE = "최종 게임 결과";
 
     public void printGameEndMessage(){
         System.out.println(GAME_END_MESSAGE);
