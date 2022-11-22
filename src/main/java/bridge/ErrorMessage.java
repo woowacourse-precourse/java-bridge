330c235cfe4d6dbc4887b4835f2781f6b171870f
juy4556 <kws05050@naver.com>
2022-11-22 16:52:23 +0900

    feat: 타당하지 않은 명령 입력 시 출력되는 에러 메세지 추가

--- src/main/java/bridge/ErrorMessage.java
+++ src/main/java/bridge/ErrorMessage.java
@@ -3,7 +3,8 @@ package bridge;
 public enum ErrorMessage {
     NOT_NUMERIC_VALUE("[ERROR] 숫자만 입력해야 합니다."),
     NOT_VALID_INTERVAL_VALUE("[ERROR] 다리 길이는 3부터 20 사이의 숫자여야 합니다."),
-    NOT_VALID_MOVING("[ERROR] 이동은 U(UP) 또는 D(DOWN) 중 하나만 선택하여 입력해야 합니다.")
+    NOT_VALID_MOVING("[ERROR] 이동은 U(UP) 또는 D(DOWN) 중 하나만 선택하여 입력해야 합니다."),
+    NOT_VALID_COMMAND("[ERROR] 커맨드는 R(RETRY) 또는 Q(QUIT) 중 하나만 선택하여 입력해야 합니다.")
     ;
 
     private final String message;
