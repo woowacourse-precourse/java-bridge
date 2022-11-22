eb2985629a08e97bcbeaa8bd8aa1a0e37b5f9d3c
DaWoon Lee <qkgk61@gmail.com>
2022-11-22 20:19:19 +0900

    Feat : 게임을 수행하는 데 필요한 객체 생성 구현

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -1,8 +1,33 @@
 package bridge;
 
+import bridge.view.InputView;
+import bridge.view.OutputView;
+import bridge.view.View;
+import java.util.List;
+
 public class Application {
 
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        View view = createView();
+        List<String> bridge = createBridge(view);
+        BridgeGame bridgeGame = new BridgeGame();
+
+        GameController gameController = new GameController(view, bridge, bridgeGame);
+        gameController.play(1, 0);
     }
+
+    private static View createView() {
+        InputView inputView = new InputView();
+        OutputView outputView = new OutputView();
+        return new View(inputView, outputView);
+    }
+
+    private static List<String> createBridge(View view) {
+        BridgeRandomNumberGenerator bridgeRandomNumberGenerator = new BridgeRandomNumberGenerator();
+        BridgeMaker bridgeMaker = new BridgeMaker(bridgeRandomNumberGenerator);
+
+        int size = view.readBridgeSize();
+        return bridgeMaker.makeBridge(size);
+    }
+
 }
