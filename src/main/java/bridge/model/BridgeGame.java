bb0de19325b20aa89776aa60195d33c08fbbb266
HangSub999 <gkdtjq999@gmail.com>
2022-11-22 21:53:29 +0900

    style: 줄 바꿈

--- src/main/java/bridge/model/BridgeGame.java
+++ src/main/java/bridge/model/BridgeGame.java
@@ -3,9 +3,6 @@ package bridge.model;
 import java.util.ArrayList;
 import java.util.List;
 
-/**
- * 다리 건너기 게임을 관리하는 클래스
- */
 public class BridgeGame {
 
     private final List<Bridge> result = new ArrayList<>();
