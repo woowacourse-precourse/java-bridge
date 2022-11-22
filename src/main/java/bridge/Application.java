6226daf0cfa5cb38800136619b0481b62db16d85
wjh2012 <wjh2012@naver.com>
2022-11-22 18:46:21 +0900

    style(All): 코드 정리

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -6,6 +6,7 @@ public class Application {
 
     public static void main(String[] args) {
         BridgeController bridgeController = new BridgeController();
+
         bridgeController.startGame();
         bridgeController.makeBridge();
         bridgeController.run();
