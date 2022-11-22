ab366690d6e3bbab6353e6f682b526bf54c57b9c
jsk5341 <102288039+jsk5341@users.noreply.github.com>
2022-11-22 14:33:46 +0900

    fix: error on print result

--- src/main/java/bridge/OutputView.java
+++ src/main/java/bridge/OutputView.java
@@ -54,14 +54,14 @@ public class OutputView {
      * <p>
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void printResult(List<String> bridgeAnswer, List<String> currentResult, boolean userChoice, int stage,int count) {
+    public void printResult(List<String> bridgeAnswer, List<String> currentResult, int stage,int count) {
 
         System.out.println("최종 게임 결과");
         printMap(currentResult);
-        if(bridgeAnswer.size() == stage-1){
+        if(bridgeAnswer.size() == stage+1){
             System.out.println("게임 성공 여부: 성공");
         }
-        if(bridgeAnswer.size() > stage-1){
+        if(stage+1<bridgeAnswer.size()){
             System.out.println("게임 성공 여부: 실패");
         }
         System.out.println("총 시도한 횟수: "+count);
