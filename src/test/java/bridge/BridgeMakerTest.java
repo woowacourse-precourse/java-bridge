8840b4274a994f073bea1a25e1664fed0d006518
moonstal1506 <moonstal1506@gmail.com>
2022-11-22 22:55:44 +0900

    refactor: 다리 생성 테스트 설명 자세히 작성

--- src/test/java/bridge/BridgeMakerTest.java
+++ src/test/java/bridge/BridgeMakerTest.java
@@ -3,25 +3,29 @@ package bridge;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 class BridgeMakerTest {
 
-    @DisplayName("위에 다리가 생성된다.")
+    @DisplayName("방향에 대한 숫자가 1일 경우 위쪽 방향에 다리가 생성된다.")
     @Test
     void makeUpBridge() {
         BridgeMaker bridgeMaker = new BridgeMaker(() -> 1);
+        List<String> bridge = bridgeMaker.makeBridge(1);
 
-        assertThat(bridgeMaker.makeBridge(1)).hasSize(1);
-        assertThat(bridgeMaker.makeBridge(1)).containsExactly("U");
+        assertThat(bridge).hasSize(1);
+        assertThat(bridge).containsExactly("U");
     }
 
-    @DisplayName("아래에 다리가 생성된다.")
+    @DisplayName("방향에 대한 숫자가 0일 경우 아래쪽 방향에 다리가 생성된다.")
     @Test
     void makeDownBridge() {
         BridgeMaker bridgeMaker = new BridgeMaker(() -> 0);
+        List<String> bridge = bridgeMaker.makeBridge(1);
 
-        assertThat(bridgeMaker.makeBridge(1)).hasSize(1);
-        assertThat(bridgeMaker.makeBridge(1)).containsExactly("D");
+        assertThat(bridge).hasSize(1);
+        assertThat(bridge).containsExactly("D");
     }
 }
