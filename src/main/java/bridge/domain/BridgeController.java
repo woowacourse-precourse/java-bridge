4b3c2e36027059ac53ce4f2bf26758b515b33b02
DDing77 <devseo2021@gmail.com>
2022-11-22 20:42:48 +0900

    feat(bridge) : 게임 종료 처리 진행 메소드

--- src/main/java/bridge/domain/BridgeController.java
+++ src/main/java/bridge/domain/BridgeController.java
@@ -2,7 +2,7 @@ package bridge.domain;
 
 import bridge.BridgeRandomNumberGenerator;
 import bridge.constant.command.Command;
-import bridge.exception.ExceptionInput;
+import bridge.exception.InputException;
 import bridge.service.BridgeGameService;
 import bridge.view.InputView;
 import bridge.view.OutputView;
@@ -15,11 +15,12 @@ public class BridgeController {
         OutputView.printStart();
         initial();
         progress();
+        quit();
     }
 
     private void initial() {
         try {
-            int size = ExceptionInput.validateInputLength(InputView.readBridgeSize());
+            int size = InputException.validateInputLength(InputView.readBridgeSize());
             BridgeMaker bridgeMaker = new BridgeMaker(new BridgeRandomNumberGenerator());
             bridgeGameService = new BridgeGameService(new BridgeGame(bridgeMaker.makeBridge(size)));
         } catch (IllegalArgumentException e) {
@@ -37,16 +38,17 @@ public class BridgeController {
             moving();
         }
     }
+
     private String getMoveCommand() {
-        return ExceptionInput.validateInputMoveCommand(InputView.readMoving());
+        return InputException.validateInputMoveCommand(InputView.readMoving());
     }
 
     private void progress() {
-        while(bridgeGameService.success() && !bridgeGameService.isComplete()) {
+        while (bridgeGameService.success() && !bridgeGameService.isComplete()) {
             moving();
             OutputView.printMap(bridgeGameService.getBridge(), bridgeGameService.getSelections());
         }
-        if(!bridgeGameService.isComplete()) {
+        if (!bridgeGameService.isComplete()) {
             retry();
         }
     }
@@ -54,7 +56,7 @@ public class BridgeController {
     private void retry() {
         try {
             String command = getRetryCommand();
-            if( command.equals(Command.RETRY.getCommand())) {
+            if (command.equals(Command.RETRY.getCommand())) {
                 bridgeGameService.restart();
                 progress();
             }
@@ -65,6 +67,10 @@ public class BridgeController {
     }
 
     private String getRetryCommand() {
-        return ExceptionInput.validateInputRetryCommand(InputView.readGameCommand());
+        return InputException.validateInputRetryCommand(InputView.readGameCommand());
+    }
+
+    private void quit() {
+        OutputView.printResult(bridgeGameService);
     }
 }
