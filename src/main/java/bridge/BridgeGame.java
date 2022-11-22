1c0daa4a877aab2beda701ba9facef757d8c3383
gynhyunkim <gyeonghyun.kimm@gmail.com>
2022-11-22 15:52:13 +0900

    feat: 사용자가 이동할 칸이 이동할 수 있는지 검사

--- src/main/java/bridge/BridgeGame.java
+++ src/main/java/bridge/BridgeGame.java
@@ -1,16 +1,35 @@
 package bridge;
 
+import java.util.List;
+
 /**
  * 다리 건너기 게임을 관리하는 클래스
  */
 public class BridgeGame {
+    private List<String> bridge;
+    private int current;
+
+    public BridgeGame(List<String> bridge) {
+        this.bridge = bridge;
+        current = 0;
+    }
+
+    private boolean isAvailable(String moving) {
+        return bridge.get(current + 1).equals(moving);
+    }
 
     /**
      * 사용자가 칸을 이동할 때 사용하는 메서드
      * <p>
      * 이동을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void move() {
+    public boolean move(String moving) {
+        Validation.validateMoving(moving);
+        if (!isAvailable(moving)) {
+            return false;
+        }
+        current++;
+        return true;
     }
 
     /**
@@ -19,5 +38,6 @@ public class BridgeGame {
      * 재시작을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
     public void retry() {
+
     }
 }
