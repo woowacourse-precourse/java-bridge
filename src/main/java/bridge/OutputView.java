22d768c2fe688db41816def9718ee5ee5409518b
ChaCha3088 <cha3088@gmail.com>
2022-11-22 13:52:25 +0900

    refactor(/src/main/java/bridge/OutputView.java): refactor 메소드 10줄 제한

--- src/main/java/bridge/OutputView.java
+++ src/main/java/bridge/OutputView.java
@@ -14,38 +14,38 @@ public class OutputView {
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
     public static void printMap(List<String> playerList, List<String> BRIDGEANSWER) {
-        List<String> lowerList = new ArrayList<>();
-        List<String> upperList = new ArrayList<>();
-
+        List<String> lowerList = new ArrayList<>(), upperList = new ArrayList<>();
         for (int i = 0; i < playerList.size(); i++) {
-            if (playerList.get(i).equals(BRIDGEANSWER.get(i))) {
-                if (playerList.get(i).equals("U")) {
-                    upperList.add(" O ");
-                    lowerList.add("   ");
-                }
-                if (playerList.get(i).equals("D")) {
-                    upperList.add("   ");
-                    lowerList.add(" O ");
-                }
-            }
-            if (!playerList.get(i).equals(BRIDGEANSWER.get(i))) {
-                if (playerList.get(i).equals("U")) {
-                    upperList.add(" X ");
-                    lowerList.add("   ");
-                }
-                if (playerList.get(i).equals("D")) {
-                    upperList.add("   ");
-                    lowerList.add(" X ");
-                }
-            }
+            if (playerList.get(i).equals(BRIDGEANSWER.get(i))) {AddEqualCase(playerList.get(i), upperList, lowerList);}
+            if (!playerList.get(i).equals(BRIDGEANSWER.get(i)))
+            { AddNotEqualCase(playerList.get(i), upperList, lowerList); }
         }
+        ConvertCommaToBar(upperList, lowerList);
+    }
 
+    private static void ConvertCommaToBar(List<String> upperList, List<String> lowerList) {
         StringBuffer upperString = new StringBuffer(upperList.toString().replace(", ", "|"));
-
         StringBuffer lowerString = new StringBuffer(lowerList.toString().replace(", ", "|"));
-
         String(upperString.toString());
         String(lowerString.toString());
+    }
+
+    private static void AddEqualCase(String letter, List<String> upperList, List<String> lowerList) {
+        if (letter.equals("U"))
+        { upperList.add(" O ");
+            lowerList.add("   "); }
+        if (letter.equals("D"))
+        { upperList.add("   ");
+            lowerList.add(" O "); }
+    }
+
+    private static void AddNotEqualCase(String letter, List<String> upperList, List<String> lowerList) {
+        if (letter.equals("U"))
+        { upperList.add(" X ");
+            lowerList.add("   "); }
+        if (letter.equals("D"))
+        { upperList.add("   ");
+            lowerList.add(" X "); }
     }
 
     /**
@@ -54,7 +54,7 @@ public class OutputView {
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
     public static void printResult(Boolean success, Integer tryCount) {
-        String result = Success(success);
+        String result = SuccessConvert(success);
         String("최종 게임 결과");
         String("게임 성공 여부: "+result);
         String("총 시도한 횟수: "+tryCount);
@@ -64,14 +64,10 @@ public class OutputView {
         System.out.println(message);
     }
 
-    public static String Success(Boolean boo) {
+    public static String SuccessConvert(Boolean boo) {
         String result = "";
-        if (boo.equals(true)) {
-            result = "성공";
-        }
-        if (boo.equals(false)) {
-            result = "실패";
-        }
+        if (boo.equals(true)) { result = "성공"; }
+        if (boo.equals(false)) { result = "실패"; }
         return result;
     }
 }
