f1eb148a098e498023d23d693d07dc02b82a2901
fnzl54 <rnjscksdud1@gmail.com>
2022-11-22 10:34:51 +0900

    feat: create gameSetting method

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -1,8 +1,24 @@
 package bridge;
 
+import java.util.ArrayList;
+import java.util.List;
 public class Application {
 
+    private static List<String> bridgeNumberList = new ArrayList<>();
+    static InputView userInput = new InputView();
+    static OutputView display = new OutputView();
+    static BridgeGame Game = new BridgeGame();
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+    }
+    public static void gameSetting() {
+        System.out.println("다리 건너기 게임을 시작합니다.");
+        System.out.println("다리 길이를 입력해주세요.");
+        BridgeRandomNumberGenerator gameRandomNumber = new BridgeRandomNumberGenerator();
+        BridgeMaker makeBridge = new BridgeMaker(gameRandomNumber);
+
+        int bridgeSize = userInput.readBridgeSize();
+
+        bridgeNumberList = makeBridge.makeBridge(bridgeSize);
     }
 }
