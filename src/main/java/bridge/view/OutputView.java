30e241be399ab4f147a241396c0ea99a0413bd51
LeeJejune <leejejune0625@gmail.com>
2022-11-22 17:38:04 +0900

    feat: output출력을 위한 클래스 구현

---
+++ src/main/java/bridge/view/OutputView.java
@@ -0,0 +1,10 @@
+package bridge.view;
+
+import bridge.dto.BridgeGameResultDto;
+import bridge.dto.BridgeGameLocationDto;
+
+/**
+ * 사용자에게 게임 진행 상황과 결과를 출력하는 역할을 한다.
+ */
+public class OutputView {
+}
