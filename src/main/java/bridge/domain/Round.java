04b41089707d94a10ac4a540a7f2a389389322e1
im-shung <seoyoung1113@gmail.com>
2022-11-22 13:02:14 +0900

    feat: :sparkles: 사용자의 이동할 칸과 다리의 건널 수 있는 칸을 비교하는 기능 추가

--- src/main/java/bridge/domain/Round.java
+++ src/main/java/bridge/domain/Round.java
@@ -1,14 +1,31 @@
 package bridge.domain;
 
 import bridge.exception.Validator;
+import java.util.Objects;
 
 public class Round {
 
-    private final String movingExpression;
-    private RoundResult roundResult;
+    private final String playerMoving;
+    private RoundResult result;
 
-    public Round(String movingExpression) {
-        Validator.validMoving(movingExpression);
-        this.movingExpression = movingExpression;
+    public Round(String playerMoving, String bridgeMoving) {
+        Validator.validMoving(playerMoving);
+        this.playerMoving = playerMoving;
+        this.result = compare(bridgeMoving);
+    }
+
+    public String getPlayerMoving() {
+        return playerMoving;
+    }
+
+    public RoundResult getResult() {
+        return result;
+    }
+
+    public RoundResult compare(String bridgeMoving) {
+        if (Objects.equals(bridgeMoving, playerMoving)) {
+            return RoundResult.SUCCESS;
+        }
+        return RoundResult.FAILURE;
     }
 }
