abf75a11b0b9c5f9513012d5f6660dac8837e720
jingyu9804 <swc05161@gmail.com>
2022-11-22 16:37:56 +0900

    style(bridge) : 코드 정리

--- src/main/java/bridge/domain/BridgeHelp.java
+++ src/main/java/bridge/domain/BridgeHelp.java
@@ -3,26 +3,26 @@ package bridge.domain;
 import java.util.List;
 
 public enum BridgeHelp {
-    UP(1,"U"),
+    UP(1, "U"),
     DOWN(0, "D");
 
     private final int number;
     private final String value;
 
-    BridgeHelp(int number,String value){
-        this.number=number;
-        this.value=value;
+    BridgeHelp(int number, String value) {
+        this.number = number;
+        this.value = value;
     }
 
-    public String getValue(){
+    public String getValue() {
         return value;
     }
 
     public static BridgeHelp generateBridge(int number) {
-        if(number == UP.number){
+        if (number == UP.number) {
             return UP;
         }
-        if(number== DOWN.number){
+        if (number == DOWN.number) {
             return DOWN;
         }
         throw new IllegalArgumentException();
