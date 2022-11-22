eabbe984e06a56cb01653e51defa869e4528cf65
jungdoyoon <jungdo8016@gmail.com>
2022-11-22 21:23:34 +0900

    feat(Input): 입력 기능

--- src/main/java/bridge/InputView.java
+++ src/main/java/bridge/InputView.java
@@ -1,28 +1,86 @@
 package bridge;
 
+import static camp.nextstep.edu.missionutils.Console.readLine;
+
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
  */
 public class InputView {
 
+    OutputView outView = new OutputView();
+    private int sizeValidate(){
+
+        int size;
+
+        try {
+            size = Integer.parseInt(readLine());
+            checkSize(size);
+
+        }
+        catch (NumberFormatException e){
+            throw new IllegalArgumentException("[ERROR] 숫자로 입력해주세요.");
+        }
+        return size;
+    }
+
+    private void checkSize(int size) {
+        if (size < 3 || size > 20)
+            throw new IllegalArgumentException("[ERROR] 3이상 20이하의 수만 가능합니다.");
+    }
+
+
     /**
      * 다리의 길이를 입력받는다.
      */
     public int readBridgeSize() {
-        return 0;
+        int size;
+        System.out.println("다리의 길이를 입력해 주세요.");
+        size = sizeValidate();
+
+        return size;
     }
 
-    /**
-     * 사용자가 이동할 칸을 입력받는다.
-     */
+    private void MoveCheck(String word){
+        if (word.charAt(0) != 'U' && word.charAt(0) != 'D') {
+            throw new IllegalArgumentException("[ERROR] 올바르게 입력해 주세요.");
+        }
+    }
     public String readMoving() {
-        return null;
+        System.out.println("이동할 칸을 선택해주세요. (위: U, 아래: D)");
+        String word = readLine();
+        System.out.println(word);
+        try {
+            MoveCheck(word);
+        }
+        catch (IllegalArgumentException e){
+            outView.printMessage(e.getMessage());
+            return readMoving();
+        }
+        return word;
     }
-
+    public void checkRegameException(String regame){
+        if ((!regame.equals("R")) && (!regame.equals("Q"))){
+            throw new IllegalArgumentException("[ERROR] 다시 입력해 주세요.");
+        }
+    }
     /**
      * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
      */
     public String readGameCommand() {
-        return null;
+        String regame;
+
+        regame = readLine();
+        try {
+            checkRegameException(regame);
+        }
+        catch (IllegalArgumentException e){
+            outView.printMessage(e.getMessage());
+            return readGameCommand();
+        }
+        return regame;
+    }
+
+    public void printReGameMessage(){
+        System.out.println("게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)");
     }
 }
