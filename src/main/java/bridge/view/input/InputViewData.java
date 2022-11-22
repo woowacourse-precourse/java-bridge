321df8f22adc2227a5ca21b2a59e87dfa0c82d78
DESKTOP-E5VRH4Q\cnddk <cnddkscndgus@naver.com>
2022-11-22 20:06:00 +0900

    style(InputViewData): 한 줄의 길이기 120자에 가까워 enter를 추가함
    - Woowa Cource Java Style Guide 에서 열 제한을 120자로 두고 있어 들여쓰기를 함

--- src/main/java/bridge/view/input/InputViewData.java
+++ src/main/java/bridge/view/input/InputViewData.java
@@ -6,8 +6,10 @@ import java.util.function.Predicate;
 public enum InputViewData {
 
     BRIDGE_LENGTH("\n다리의 길이를 입력해주세요.",
-            input -> Constants.MIN_LENGTH <= Integer.parseInt(input) && Integer.parseInt(input) <= Constants.MAX_LENGTH,
-            String.format("[ERROR] %s 이상 %s 이하의 숫자만 입력할 수 있습니다.", Constants.MIN_LENGTH, Constants.MAX_LENGTH)),
+            input -> Constants.MIN_LENGTH <= Integer.parseInt(input)
+                    && Integer.parseInt(input) <= Constants.MAX_LENGTH,
+            String.format("[ERROR] %s 이상 %s 이하의 숫자만 입력할 수 있습니다.",
+                    Constants.MIN_LENGTH, Constants.MAX_LENGTH)),
     MOVING_ROUTE(String.format("\n이동할 칸을 선택해주세요. (위: %s, 아래: %s)", Constants.UP, Constants.DOWN),
             input -> input.equals(Constants.UP) || input.equals(Constants.DOWN),
             "[ERROR] 해당하는 문자만 입력할 수 있습니다."),
