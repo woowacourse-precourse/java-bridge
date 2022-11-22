a6b14d15f19e0661119bc3391e63082b8820865d
DaWoon Lee <qkgk61@gmail.com>
2022-11-22 22:08:05 +0900

    Refactor : 게임을 수행하는 책임을 GameController에게 위임

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -1,37 +1,24 @@
 package bridge;
 
 import bridge.logic.BridgeGame;
-import bridge.util.BridgeMaker;
-import bridge.util.BridgeRandomNumberGenerator;
 import bridge.view.InputView;
 import bridge.view.OutputView;
 import bridge.view.View;
-import java.util.List;
 
 public class Application {
 
     public static void main(String[] args) {
         View view = createView();
-        List<String> bridge = createBridge(view);
         BridgeGame bridgeGame = new BridgeGame();
 
-        GameController gameController = new GameController(view, bridge, bridgeGame);
-        gameController.play(1, 0);
+        GameController gameController = new GameController(view, bridgeGame);
+        gameController.play(1);
     }
 
     private static View createView() {
         InputView inputView = new InputView();
         OutputView outputView = new OutputView();
         return new View(inputView, outputView);
-    }
-
-    private static List<String> createBridge(View view) {
-        BridgeRandomNumberGenerator bridgeRandomNumberGenerator = new BridgeRandomNumberGenerator();
-        BridgeMaker bridgeMaker = new BridgeMaker(bridgeRandomNumberGenerator);
-
-        view.printStartGame();
-        int size = view.readBridgeSize();
-        return bridgeMaker.makeBridge(size);
     }
 
 }
