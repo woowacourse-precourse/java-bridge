002b5ea13c4036184b106d4a0aa0b3e76071cbde
Kwak-JunYoung <klop9090@g.skku.edu>
2022-11-22 10:55:06 +0900

    feat(Application/main) Overall game process but the result-printing part
    Implemented the overall gaming process except that of the result-printing part

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -1,10 +1,24 @@
 package bridge;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Application {
 
     public static void main(String[] args) {
         // TODO: 프로그램 구현
         System.out.println("다리 건너기 게임을 시작합니다.");
-
+        InputView inputView = new InputView();
+        OutputView outputView = new OutputView();
+        int bridgeSize = inputView.readBridgeSize();
+        BridgeNumberGenerator bridgeNumberGenerator = new BridgeRandomNumberGenerator();
+        BridgeMaker bridgeMaker = new BridgeMaker(bridgeNumberGenerator);
+        List<String> userMoveHistory = new ArrayList<String>();
+        List<String> bridge = bridgeMaker.makeBridge(bridgeSize);
+        for (int i = 0; i < bridgeSize; i++) {
+            String userMove = inputView.readMoving();
+            userMoveHistory.add(userMove);
+            outputView.printMap(userMoveHistory, bridge, bridgeSize);
+        }
     }
 }
