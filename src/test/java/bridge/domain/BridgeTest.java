2f9d88df18e2d3d65b563132eaf066a3a4e2543f
jindaram-stu <jinsb1999@naver.com>
2022-11-22 23:36:44 +0900

    test(BridgeTest): 다리 생성 예외 및 결과 테스트
    다리 생성 시 예외상황 및 결과 반환 시 테스트 케이스 추가

--- src/test/java/bridge/domain/BridgeTest.java
+++ src/test/java/bridge/domain/BridgeTest.java
@@ -21,4 +21,36 @@ class BridgeTest {
         assertThat(bridge.isSame(testBridge)).isEqualTo(false);
     }
 
+    @DisplayName("문자열 배열을 기반으로 다리 객체를 생성합니다.")
+    @Test
+    void 다리_생성_예외() {
+        List<String> originalBridge = Arrays.asList("U","D","A");
+
+        assertThatCode(() -> new Bridge(originalBridge))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(ExceptionMessage.DOES_NOT_EXIST_UP_DOWN);
+    }
+
+    @DisplayName("주어진 인덱스와 움직임이 같을 경우 성공 결과를 반환합니다.")
+    @Test
+    void 결과_반환_테스트_성공() {
+        List<String> originalBridge = Arrays.asList("U","D","D");
+        Bridge bridge = new Bridge(originalBridge);
+
+        Result result1 = bridge.distinguish(0, Move.UP);
+
+        assertThat(result1).isEqualTo(Result.SUCCESS);
+    }
+
+    @DisplayName("주어진 인덱스와 움직임이 다를 경우 실패 결과를 반환합니다.")
+    @Test
+    void 결과_반환_테스트_실패() {
+        List<String> originalBridge = Arrays.asList("U","D","D");
+        Bridge bridge = new Bridge(originalBridge);
+
+        Result result1 = bridge.distinguish(0,Move.DOWN);
+
+        assertThat(result1).isEqualTo(Result.FAIL);
+    }
+
 }
