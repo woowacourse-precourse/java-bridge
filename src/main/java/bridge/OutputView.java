0416cc78257bba3f8484daa6f858cac59ee2060a
kim <kajinori@gachon.ac.kr>
2022-11-22 19:40:43 +0900

    feat(Constant): 게임 재시작 입력값, 다리 O,X 출력값 Enum 생성

--- src/main/java/bridge/OutputView.java
+++ src/main/java/bridge/OutputView.java
@@ -1,6 +1,7 @@
 package bridge;
 
-import bridge.UserInput.BridgeSelection;
+import bridge.Constant.BridgeSelection;
+import bridge.Constant.OutputValue;
 
 import java.util.List;
 
@@ -12,7 +13,6 @@ public class OutputView {
     public static void printMap(List<String> bridge, List<String> result) {
         System.out.println(upBridge(bridge, result));
         System.out.println(downBridge(bridge, result));
-        System.out.println();
     }
 
     private static String upBridge(List<String> bridge, List<String> result) {
@@ -33,12 +33,25 @@ public class OutputView {
     }
 
     private static String getOX(String bridgeMark, String resultMark, String floor) {
-        if (bridgeMark.equals(floor) && resultMark.equals("O")) {
-            return "O";
+        if (bridgeMark.equals(floor) && resultMark.equals(OutputValue.rightBlock.get())) {
+            return OutputValue.rightBlock.get();
         }
-        if (!bridgeMark.equals(floor) && resultMark.equals("X")) {
-            return "X";
+        if (!bridgeMark.equals(floor) && resultMark.equals(OutputValue.wrongBlock.get())) {
+            return OutputValue.wrongBlock.get();
         }
         return " ";
+    }
+
+    public static void printResult(GameHelper gameHelper, int trialCount) {
+        System.out.println("최종 게임 결과");
+        printMap(gameHelper.getBridge(), gameHelper.getResultBridge());
+        System.out.println();
+        System.out.println("게임 성공 여부: " + isSuccess(gameHelper));
+        System.out.println("총 시도한 횟수: " + trialCount);
+    }
+
+    public static String isSuccess(GameHelper gameHelper) {
+        if (!gameHelper.reachEnd()) return "실패";
+        return "성공";
     }
 }
