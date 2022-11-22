42d9a7c58b561d0b3e353ab9977bbc03365ca419
popomarin28@gmail.com <popomarin28@gmail.com>
2022-11-22 15:02:16 +0900

    test(InputView) : 유효한 다리 사이즈가 들어때 까지 쭉 입력 받는 test

---
+++ src/test/java/bridge/InputViewTest.java
@@ -0,0 +1,43 @@
+package bridge;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import java.util.List;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+class InputViewTest {
+
+    @DisplayName("올바른 사이즈가 입력 될 때 까지 입력값 요구")
+    @Test
+    void getBrideSizeTest() {
+        List<String> inputs = List.of("0", "aa", "100", "28", "pp", "K", "3");
+        String inputWithDivision = String.join("\n", inputs);
+        System.setIn(new ByteArrayInputStream(inputWithDivision.getBytes()));
+
+        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(byteArrayOutputStream));
+
+        InputView inputView = new InputView();
+        inputView.getBrideSize();
+
+        String [] result = byteArrayOutputStream.toString().split("\n");
+        for (String resultAll : result) {
+            assertThat(resultAll).contains("ERROR");
+        }
+    }
+
+    @
+    @Test
+    void getReadMoving() {
+    }
+
+    @Test
+    void getReadCommand() {
+    }
+}
