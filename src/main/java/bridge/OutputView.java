7ba480c3d37b8fb54c1ffbb1f32ea8788a74e344
KJE <12181501@inha.edu>
2022-11-22 15:25:59 +0900

    feat: OutputView- bridge 출력 메서드 작성

--- src/main/java/bridge/OutputView.java
+++ src/main/java/bridge/OutputView.java
@@ -8,16 +8,39 @@ import java.util.List;
  */
 public class OutputView {
 
-    /**
-     * 현재까지 이동한 다리의 상태를 정해진 형식에 맞춰 출력한다.
-     * <p>
-     * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
-     */
-    //public void printMap(List<Integer> bridge) {
-    //    List<Integer> li = new ArrayList<Integer>();
-    //    return li;
-    //}
+    public static void printMap(List<String> bridge, int size, String userInput) {
+        System.out.print("[");
+        for(int i = 0; i < size-1;i++){
+            printMapChar(bridge.get(i), "U");
+        }
+        printLastChar(bridge.get(size), "U", userInput);
+        for(int i = 0; i < size-1;i++){
+            printMapChar(bridge.get(i), "D");
+        }
+        printLastChar(bridge.get(size), "D", userInput);
+    }
 
+    private static int printMapChar(String bridgeChar, String upDown){
+        if(bridgeChar.equals(upDown)){
+            System.out.print(" O |");
+            return 0;
+        }
+        System.out.print("   |");
+        return 0;
+    }
+
+    private static int printLastChar(String bridgeChar, String upDown, String userInput){
+        if(!bridgeChar.equals(upDown) && userInput.equals(upDown)){
+            System.out.println(" X ]");
+            return 0;
+        }
+        if(bridgeChar.equals(userInput) && bridgeChar.equals(upDown)){
+            System.out.println(" O ]");
+            return 0;
+        }
+        System.out.println(" X ]");
+        return 0;
+    }
     /**
      * 게임의 최종 결과를 정해진 형식에 맞춰 출력한다.
      * <p>
