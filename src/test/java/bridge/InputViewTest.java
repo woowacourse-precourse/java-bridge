4a8a716ddef064c438a4d4b1477b11b9d3a4a8ca
DavinAn <blueapple1030@gmail.com>
2022-11-22 22:39:09 +0900

    test : add skeleton code to InputViewTest

--- src/test/java/bridge/InputViewTest.java
+++ src/test/java/bridge/InputViewTest.java
@@ -1,4 +1,19 @@
 package bridge;
 
+import bridge.controller.InputViewException;
+
 public class InputViewTest {
+    private final InputViewException inputViewException = new InputViewException();
+
+    void readBridgeSize(String bridgeSize, String exception) {
+
+    }
+
+    void readMoving(String playerMove, String exception) {
+
+    }
+
+    void readGameCommand(String playerCommand, String exception) {
+
+    }
 }
