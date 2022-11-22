b369d7e0afea8b09943962160089311683ee1ba8
sudo-dustle <asdf748596123@gmail.com>
2022-11-22 23:39:50 +0900

    fix: 테스트 코드 수정

--- src/test/java/bridge/BridgeMakerTest.java
+++ src/test/java/bridge/BridgeMakerTest.java
@@ -3,37 +3,33 @@ package bridge;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.ValueSource;
+import org.junit.jupiter.params.provider.MethodSource;
 
 import java.util.List;
 import java.util.Set;
+import java.util.stream.IntStream;
 
 import static org.assertj.core.api.Assertions.*;
 
 public class BridgeMakerTest {
 
-    @DisplayName("0/1로 들어온 다리를 U/D로 변환하여 저장 성공")
-    @Test
-    void testMakeBridge() {
-        BridgeRandomNumberGenerator bridgeRandomNumberGenerator = new BridgeRandomNumberGenerator();
-        BridgeMaker bridgeMaker = new BridgeMaker(bridgeRandomNumberGenerator);
-
-        List<String> bridgeList = bridgeMaker.makeBridge(6);
-
-        assertThat(bridgeList).allMatch(bridge -> Set.of("U", "D")
-                .contains(bridge));
-    }
-
-    @DisplayName("다리의 길이가 3 이상 20 이하가 아니면 실패")
+    @DisplayName("다리의 길이를 입력 받아 U, D 만 포함한 다리를 생성한다")
     @ParameterizedTest
-    @ValueSource(ints = {2, 21})
-    void testBridgeRange(int size) {
+    @MethodSource("bridgeRange")
+    void testMakeBridge(int size) {
         // given
         BridgeRandomNumberGenerator generator = new BridgeRandomNumberGenerator();
-        BridgeMaker bridgeMaker = new BridgeMaker(generator);
+        BridgeMaker bridgeMaker = new BridgeMaker(generator);;
 
         // when
-        assertThatExceptionOfType(IllegalArgumentException.class)
-                .isThrownBy(() -> bridgeMaker.makeBridge(size));
+        List<String> bridges = bridgeMaker.makeBridge(size);
+
+        // then
+        assertThat(bridges.size()).isEqualTo(size);
+        assertThat(bridges).containsAnyOf("U", "D");
+    }
+
+    static IntStream bridgeRange() {
+        return IntStream.range(3, 20);
     }
 }
