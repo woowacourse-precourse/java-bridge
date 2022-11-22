49e0dea583b46dab791e2e21d9a8743d10f81fb4
Dabeen Jeong <hello70825@gmail.com>
2022-11-22 17:17:01 +0900

    feat: 게임 시작 출력 기능 추가

--- src/main/java/bridge/view/OutputView.java
+++ src/main/java/bridge/view/OutputView.java
@@ -9,6 +9,10 @@ public class OutputView {
     private static final String RESULT_STATUS_MESSAGE = "게임 성공 여부: %s";
     private static final String TRY_MESSAGE = "총 시도한 횟수: %d";
 
+    public void printStart() {
+        System.out.println(START_GAME_MESSAGE);
+    }
+
     /**
      * 현재까지 이동한 다리의 상태를 정해진 형식에 맞춰 출력한다.
      * <p>
