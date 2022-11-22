6ec42028b3da5e339aef1ece6e6a66501a0c41b9
lkdcode <dlrlejr1@naver.com>
2022-11-22 10:42:52 +0900

    ✨Feat : 다리의 길이를 입력 받아서 다리 생성

--- src/main/java/bridge/Model/BridgeMaker.java
+++ src/main/java/bridge/Model/BridgeMaker.java
@@ -1,5 +1,7 @@
-package bridge;
+package bridge.Model;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 /**
@@ -18,6 +20,13 @@ public class BridgeMaker {
      * @return 입력받은 길이에 해당하는 다리 모양. 위 칸이면 "U", 아래 칸이면 "D"로 표현해야 한다.
      */
     public List<String> makeBridge(int size) {
-        return null;
+        List<String> answers = new ArrayList<>();
+        for (int i = 0; i < size; i++) {
+            int answerNumber = bridgeNumberGenerator.generate();
+            answers.add(String.valueOf(answerNumber));
+        }
+        Collections.replaceAll(answers, "1", "U");
+        Collections.replaceAll(answers, "0", "D");
+        return answers;
     }
 }
