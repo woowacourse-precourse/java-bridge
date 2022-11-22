a752512e87364eb73d6897058439b0a538f579b8
Damgom <wisewish1@gmail.com>
2022-11-22 14:09:35 +0900

    REFACTOR: test

--- src/test/java/bridge/domain/BridgeMakerTest.java
+++ src/test/java/bridge/domain/BridgeMakerTest.java
@@ -1,5 +1,6 @@
 package bridge.domain;
 
+import bridge.BridgeMaker;
 import bridge.util.BridgeNumberGenerator;
 import bridge.util.BridgeRandomNumberGenerator;
 import org.junit.jupiter.api.DisplayName;
