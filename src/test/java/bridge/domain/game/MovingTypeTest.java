3e1c94e17e67a6421968c66b6d01ff3944038abe
alswn <alswn9938@gmail.com>
2022-11-22 15:33:35 +0900

    test(MovingTypeTest): modify ParameterizedTest로 변경

--- src/test/java/bridge/domain/game/MovingTypeTest.java
+++ src/test/java/bridge/domain/game/MovingTypeTest.java
@@ -2,43 +2,36 @@ package bridge.domain.game;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
-import static org.assertj.core.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class MovingTypeTest {
 
     @Nested
     @DisplayName("숫자로 BridgeType 가져오기 로직 검증")
-    class getBridgeTypeByNumNotation{
+    class getBridgeTypeByNumNotation {
 
-        @Test
-        @DisplayName("입력이 1이면 가져온 BridgeType의 engNotation은 U")
-        void 입력_1(){
+        @ParameterizedTest(name = "입력이 {0}면 가져온 BridgeType의 engNotation은 {1}")
+        @CsvSource({"1,U", "0, D"})
+        void 가져오기(String input, String output) {
             // given
-            int numNotation = 1;
+            int numNotation = Integer.parseInt(input);
             // when
             String resultEngNotation = getResultEngNotation(numNotation);
             // then
-            assertThat(resultEngNotation).isEqualTo("U");
+            assertThat(resultEngNotation).isEqualTo(output);
         }
 
-        @Test
-        @DisplayName("입력이 0이면 가져온 BridgeType의 engNotation은 D")
-        void 입력_2(){
-            // given
-            int numNotation = 0;
-            // when
-            String resultEngNotation = getResultEngNotation(numNotation);
-            // then
-            assertThat(resultEngNotation).isEqualTo("D");
-        }
-
-        @Test
+        @ParameterizedTest(name = "{0}은 유효한 옵션이 아니므로 예외 발생")
+        @ValueSource(ints = {3, 4, 10, -1})
         @DisplayName("1이나 2가 아닌 입력이 들어오면 예외")
-        void 예외(){
+        void 예외(int input) {
             // given
-            int numNotation = 2;
+            int numNotation = input;
             // when
             // then
             assertThatThrownBy(() -> getResultEngNotation(numNotation))
@@ -53,35 +46,24 @@ class MovingTypeTest {
 
     @Nested
     @DisplayName("BridgeType에 포함된 옵션인지 확인하는 로직 검증")
-    class ContainOption{
+    class ContainOption {
 
-        @Test
-        @DisplayName("선택지에 없는 옵션은 false값 리턴")
-        void 선택지에_없는_옵션(){
+        @ParameterizedTest(name = "선택지에 없는 {0}옵션은 false값 리턴")
+        @ValueSource(strings = {"X", "R", "Q", "Y"})
+        void 선택지에_없는_옵션(String input) {
             // given
-            String bridgeType = "X";
+            String bridgeType = input;
             // when
             boolean result = MovingType.isContains(bridgeType);
             // then
             assertThat(result).isFalse();
         }
 
-        @Test
-        @DisplayName("U 옵션은 true 값 리턴")
-        void 선택지에_있는_U_옵션(){
+        @ParameterizedTest(name = "선택지에 있는 {0}옵션은 true 값 리턴")
+        @ValueSource(strings = {"U", "D"})
+        void 선택지에_있는_옵션(String input) {
             // given
-            String bridgeType = "U";
-            // when
-            boolean result = MovingType.isContains(bridgeType);
-            // then
-            assertThat(result).isTrue();
-        }
-
-        @Test
-        @DisplayName("D 옵션은 true 값 리턴")
-        void 선택지에_있는_D_옵션(){
-            // given
-            String bridgeType = "D";
+            String bridgeType = input;
             // when
             boolean result = MovingType.isContains(bridgeType);
             // then
