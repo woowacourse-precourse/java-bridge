e83dd17234daf3bac4e459aa9cb837275022a37d
은지 <midzy0228@gmail.com>
2022-11-22 16:36:46 +0900

    test(BridgeGame): 이동시 올바르게 브릿지 그리는지 확인 테스트 케이스 추가

--- src/test/java/bridge/BridgeGameTest.java
+++ src/test/java/bridge/BridgeGameTest.java
@@ -1,11 +1,14 @@
 package bridge;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.assertj.core.api.Assertions.assertThat;
+
 import bridge.model.BridgeGame;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import java.util.List;
 
 public class BridgeGameTest {
     @DisplayName("이동할 수 있을 때 true를 반환한다.")
@@ -22,5 +25,27 @@ public class BridgeGameTest {
         BridgeGame bridgeGame = new BridgeGame();
         boolean move = bridgeGame.move("D", "U");
         assertFalse(move);
+    }
+
+    @DisplayName("이동 성공했을떄 올바른 브릿지 상태를 그린다.")
+    @Test
+    void createMoveSuccessBridge() {
+        BridgeGame bridgeGame = new BridgeGame();
+        boolean moveOne = bridgeGame.move("U", "U");
+        boolean moveTwo = bridgeGame.move("D", "D");
+        boolean moveThree = bridgeGame.move("D", "D");
+        assertThat(bridgeGame.getHighBridge()).isEqualTo(List.of("O", " ", " "));
+        assertThat(bridgeGame.getLowBridge()).isEqualTo(List.of(" ", "O", "O"));
+    }
+
+    @DisplayName("이동 실패했을떄 올바른 브릿지 상태를 그린다.")
+    @Test
+    void createMoveFailureBridge() {
+        BridgeGame bridgeGame = new BridgeGame();
+        boolean moveOne = bridgeGame.move("D", "D");
+        boolean moveTwo = bridgeGame.move("D", "D");
+        boolean moveThree = bridgeGame.move("U", "D");
+        assertThat(bridgeGame.getHighBridge()).isEqualTo(List.of(" ", " ", "X"));
+        assertThat(bridgeGame.getLowBridge()).isEqualTo(List.of("O", "O", " "));
     }
 }
