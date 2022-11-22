35ae6260756a5be5f677f05ff2ae377cc0c176ab
xxeol2 <jhj01177@naver.com>
2022-11-22 16:34:18 +0900

    refactor: 게임 코맨드 상수 GameCommand 클래스로 분리

---
+++ src/main/java/bridge/constant/GameCommand.java
@@ -0,0 +1,8 @@
+package bridge.constant;
+
+public class GameCommand {
+    
+    public static final String RETRY = "R";
+    public static final String QUIT = "Q";
+
+}
--- src/main/java/bridge/validator/GameCommandValidator.java
+++ src/main/java/bridge/validator/GameCommandValidator.java
@@ -1,12 +1,12 @@
 package bridge.validator;
 
+import static bridge.constant.GameCommand.QUIT;
+import static bridge.constant.GameCommand.RETRY;
+
 import bridge.exception.WrongGameCommandException;
 import java.util.List;
 
 public class GameCommandValidator {
-
-    private static final String RETRY = "R";
-    private static final String QUIT = "Q";
 
     public static void validateGameCommand(String gameCommand) {
         if (!List.of(RETRY, QUIT).contains(gameCommand)) {
