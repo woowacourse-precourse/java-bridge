149f7ee100cfc2080c63e86e077e51bbebdf46a6
MIN-04 <kimji_0424@naver.com>
2022-11-22 17:28:16 +0900

    refactor(Application.java): refactor retry
    Refactored retry:
    - main 함수 코드 10줄 맞춤
    - add method retry

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -6,28 +6,20 @@ import java.util.List;
 public class Application {
     private static final InputView inputView = new InputView();
     private static final OutputView outputView = new OutputView();
+    private static final BridgeGame bridgeGame = new BridgeGame();
+    private static int cnt;
+
     public static void main(String[] args) {
         outputView.printStart();
-        int cnt = 0;
 
         int bridgeSize = inputView.readBridgeSize();
-
         List<String> bridge = makeBridge(bridgeSize);
 
-        BridgeGame bridgeGame = new BridgeGame();
         List<String> result = new ArrayList<>();
-        String gameStatus = tryGame(bridgeGame, bridge, result);
-        cnt += 1;
+        String gameStatus = tryGame(bridge, result);
+        cnt = 1;
 
-        String regameStatus = "";
-        while (gameStatus.equals("실패") && !regameStatus.equals("Q")) {
-            regameStatus = inputView.readGameCommand();
-            if (regameStatus.equals("R")) {
-                result = new ArrayList<>();
-                gameStatus = tryGame(bridgeGame, bridge, result);
-                cnt += 1;
-            }
-        }
+        gameStatus = retryGame(gameStatus, bridge, result);
 
         outputView.printResult(gameStatus, cnt, result, bridge);
     }
@@ -38,7 +30,7 @@ public class Application {
         return bridgeMaker.makeBridge(bridgeSize);
     }
 
-    private static String tryGame(BridgeGame bridgeGame, List<String> bridge, List<String> result) {
+    private static String tryGame(List<String> bridge, List<String> result) {
         for (int i = 0; i < bridge.size(); i++) {
             String movingValue = inputView.readMoving();
             String movingResult = bridgeGame.move(movingValue, bridge.get(i));
@@ -49,5 +41,16 @@ public class Application {
             }
         }
         return "성공";
+    }
+
+    private static String retryGame(String gameStatus, List<String> bridge, List<String> result) {
+        while (gameStatus.equals("실패")) {
+            String regameInput = inputView.readGameCommand();
+            if (regameInput.equals("Q")) break;
+            result = bridgeGame.retry(result);
+            gameStatus = tryGame(bridge, result);
+            cnt += 1;
+        }
+        return gameStatus;
     }
 }
