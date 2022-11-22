3650324170acd5f663c5d21ff0847954217b3873
alpacaHyun <flaehdan@gmail.com>
2022-11-22 11:44:52 +0900

    type : refactor 기존 코드 삭제 후 추가
    body :

---
+++ src/test/java/bridge/BridgeTest.java
@@ -0,0 +1,45 @@
+package bridge;
+
+import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeTest;
+import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatCode;
+import static org.assertj.core.util.Lists.newArrayList;
+
+import bridge.validator.Validator;
+import camp.nextstep.edu.missionutils.test.NsTest;
+import java.util.List;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class BridgeTest extends NsTest {
+
+    private static final String ERROR_MESSAGE = "[ERROR]";
+
+    @Test
+    @DisplayName("다리길이 올바른 입력")
+    void correctBridgeSizeInput() {
+        assertThatCode(() -> Validator.checkBridgeSize(10))
+                .doesNotThrowAnyException();
+    }
+
+
+    @Override
+    protected void runMain() {
+        Application.main(new String[]{});
+    }
+
+    static class TestNumberGenerator implements BridgeNumberGenerator {
+
+        private final List<Integer> numbers;
+
+        TestNumberGenerator(List<Integer> numbers) {
+            this.numbers = numbers;
+        }
+
+        @Override
+        public int generate() {
+            return numbers.remove(0);
+        }
+    }
+}
