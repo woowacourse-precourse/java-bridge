ecd5509a8b387e5d1d27afda3ad74897ff925cc1
a1dotls <meeae9408@gmail.com>
2022-11-22 11:41:44 +0900

    refactor: 메인메서드 리팩토링

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -3,18 +3,21 @@ package bridge;
 import java.util.List;
 
 public class Application {
+    private static final InputView inputView = new InputView();
+    private static final BridgeRandomNumberGenerator bridgeRandomNumberGenerator = new BridgeRandomNumberGenerator();
+    private static final BridgeMaker bridgeMaker = new BridgeMaker(bridgeRandomNumberGenerator);
 
     public static void main(String[] args) {
-        InputView inputView = new InputView();
+        start();
+    }
+
+    private static void start() {
         inputView.viewGameStartMessage();
         inputView.checkBridgeValidate();
-        BridgeRandomNumberGenerator bridgeRandomNumberGenerator = new BridgeRandomNumberGenerator();
-        BridgeMaker bridgeMaker = new BridgeMaker(bridgeRandomNumberGenerator);
         List<String> bridge = bridgeMaker.makeBridge(inputView.getSize());
         System.out.println(bridge);
         String input = inputView.readMoving();
         BridgeGame bridgeGame = new BridgeGame();
-        bridgeGame.move(input, bridge);
         OutputView outputView = new OutputView();
         outputView.printMap(input, bridge);
     }
