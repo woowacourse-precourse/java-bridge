e42671eef7c433b42b727a2018077f10611db378
footdev <ekclstkfka5@gmail.com>
2022-11-22 15:17:07 +0900

    feat: 이동 시 입력값 유효성 검증 기능 추가
    이동 시 입력값의 유효성을 검증하는 기능을 추가함.

--- src/main/java/bridge/util/MovingValidator.java
+++ src/main/java/bridge/util/MovingValidator.java
@@ -1,4 +1,21 @@
 package bridge.util;
 
+import bridge.view.OutputView;
+
 public class MovingValidator {
+
+    private static final int LENGTH = 1;
+    private static final char UP = 'U';
+    private static final char DOWN = 'U';
+
+    public static void validateMoving(String input) {
+        if(input.charAt(0) != LENGTH) {
+            throw new IllegalArgumentException(OutputView.MOVING_ERROR_MESSAGE);
+        }
+
+        char command = input.charAt(0);
+        if(command != UP && command != DOWN) {
+            throw new IllegalArgumentException(OutputView.MOVING_ERROR_MESSGAE);
+        }
+    }
 }
