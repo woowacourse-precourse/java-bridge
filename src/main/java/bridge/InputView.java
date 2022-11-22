1590d902a583f74a16e851dc2f61d3fbb998a63d
gitFILO <x1xgudwls@naver.com>
2022-11-22 20:27:55 +0900

    [구현 기능 목록 15번 추가]

--- src/main/java/bridge/InputView.java
+++ src/main/java/bridge/InputView.java
@@ -1,28 +1,41 @@
 package bridge;
 
+import bridge.ExceptionHandler;
+import bridge.OutputView;
+
+import static camp.nextstep.edu.missionutils.Console.readLine;
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
  */
 public class InputView {
-
     /**
      * 다리의 길이를 입력받는다.
      */
-    public int readBridgeSize() {
-        return 0;
-    }
+    public static int readBridgeSize()
+    {
+        OutputView.printStart();
+        String bridgeSize="";
 
+        return  ExceptionHandler.sizeValidTest(bridgeSize);
+    }
     /**
      * 사용자가 이동할 칸을 입력받는다.
      */
-    public String readMoving() {
-        return null;
+    public static String readMoving() {
+        OutputView.printMoving();
+        String moving="";
+
+        return ExceptionHandler.movingValidTest(moving);
     }
 
     /**
      * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
      */
-    public String readGameCommand() {
-        return null;
+    public static String readGameCommand() {
+        OutputView.printRetry();
+        String gameCommand = "";
+        return ExceptionHandler.gameCommandValidTest(gameCommand);
     }
+
 }
+
