57ef77f73e13636cb4df18930464d30f328dd529
JeonginKim <47661695+mywnajsldkf@users.noreply.github.com>
2022-11-22 18:44:10 +0900

    refactor : retry 메서드 수정

--- src/main/java/bridge/game/BridgeGame.java
+++ src/main/java/bridge/game/BridgeGame.java
@@ -60,17 +60,14 @@ public class BridgeGame {
         return bridge.drawFailUpperBridge(bridges);
     }
 
-    // TODO: retry command 검증
     /**
      * 사용자가 게임을 다시 시도할 때 사용하는 메서드
      * <p>
      * 재시작을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
     public boolean retry(String command) {
-        if (command.equals(RETRY_GAME)) {
-            return true;
-        }
-        return false;
+        validateCommand(command);
+        return command.equals(RETRY_GAME);
     }
 
     private void validateCommand(String command) {
