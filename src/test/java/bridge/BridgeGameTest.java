8f9a0744dbb45a61421e2df8b331bb45d4c9bc99
mardi2020 <mardi@khu.ac.kr>
2022-11-22 12:42:38 +0900

    test: move() 테스트 추가

---
+++ src/test/java/bridge/BridgeGameTest.java
@@ -0,0 +1,43 @@
+package bridge;
+
+import java.util.Arrays;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class BridgeGameTest {
+
+    private BridgeGame bridgeGame;
+
+    @BeforeEach
+    public void setUp() {
+        bridgeGame = new BridgeGame(Arrays.asList("U", "U", "D"), 3);
+    }
+
+    @AfterEach
+    public void after() {
+        bridgeGame.retry();
+    }
+
+    @DisplayName("사용자 칸과 정답이 맞을 경우 이동이 가능하다.")
+    @Test
+    public void moveTest() {
+        Assertions.assertEquals(bridgeGame.move("U"), 0);
+    }
+
+    @DisplayName("사용자 칸과 정답이 다를 경우 이동할 수 없다.")
+    @Test
+    public void notMoveTest() {
+        Assertions.assertEquals(bridgeGame.move("D"), -1);
+    }
+
+    @DisplayName("다리 끝에 도착하게 되면 1을 리턴한다.")
+    @Test
+    public void moveEndBridgeTest() {
+        bridgeGame.move("U");
+        bridgeGame.move("U");
+        Assertions.assertEquals(bridgeGame.move("D"), 1);
+    }
+}
