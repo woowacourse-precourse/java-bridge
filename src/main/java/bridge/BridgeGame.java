59a5c3e43eb19dadaeeadb042aaa93a3350aad9e
jiwon <ljwon77@daum.net>
2022-11-22 12:00:52 +0900

    feat: add game move and retry function
    게임의 이동과 재시도하는 로직 및 게임 종료 여부, 재시도 여부를 체크하는 기능을 추가하였습니다.

--- src/main/java/bridge/BridgeGame.java
+++ src/main/java/bridge/BridgeGame.java
@@ -1,9 +1,22 @@
 package bridge;
 
+import java.util.List;
+
 /**
  * 다리 건너기 게임을 관리하는 클래스
  */
 public class BridgeGame {
+
+    private Boolean isRetryGame = false;
+
+    private final List<String> bridge;
+
+    private final BridgePlayer bridgePlayer;
+
+    public BridgeGame(List<String> bridge, BridgePlayer bridgePlayer) {
+        this.bridge = bridge;
+        this.bridgePlayer = bridgePlayer;
+    }
 
     /**
      * 사용자가 칸을 이동할 때 사용하는 메서드
@@ -11,6 +24,7 @@ public class BridgeGame {
      * 이동을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
     public void move() {
+        bridgePlayer.checkMove(bridge);
     }
 
     /**
@@ -18,6 +32,15 @@ public class BridgeGame {
      * <p>
      * 재시작을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void retry() {
+    public void retry(boolean isRetry) {
+        this.isRetryGame = isRetry;
+    }
+
+    public boolean getRetryStatus() {
+        return isRetryGame;
+    }
+
+    public boolean isNotGameEnd() {
+        return bridgePlayer.getCurrentBridgeSpace() < bridge.size();
     }
 }
