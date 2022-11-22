f1246a7125a876924ce0b5c994e249fc08a9682e
SuhHyeonjun <dmsgktn0208@gmail.com>
2022-11-22 19:12:50 +0900

    fix : 삭제한 테스트 코드 다시 추가

---
+++ src/test/java/bridge/BridgeGameTest.java
@@ -0,0 +1,32 @@
+package bridge;
+
+import static org.assertj.core.util.Lists.newArrayList;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+
+import bridge.ApplicationTest.TestNumberGenerator;
+import bridge.domain.BridgeGame;
+
+import java.util.List;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class BridgeGameTest {
+    private final BridgeGame bridgeGame = new BridgeGame();
+    private final BridgeNumberGenerator bridgeNumberGenerator = new TestNumberGenerator(newArrayList(1, 0, 0));
+    private final BridgeMaker bridgeMaker = new BridgeMaker(bridgeNumberGenerator);
+    private final List<String> bridge = bridgeMaker.makeBridge(3);
+
+    @DisplayName("이동 결과 O, X 테스트")
+    @Test
+        // bridge 현재 값은 [U, D, D]
+    void moveTest() {
+        String firstMove = bridgeGame.move(bridge.get(0), "U");
+        String secondMove = bridgeGame.move(bridge.get(1), "D");
+        String thirdMove = bridgeGame.move(bridge.get(2), "U");
+        assertEquals("O", firstMove);
+        assertEquals("O", secondMove);
+        assertNotEquals("O", thirdMove);
+    }
+}
---
+++ src/test/java/bridge/BridgeTest.java
@@ -0,0 +1,45 @@
+package bridge;
+
+import static bridge.domain.UpDownBridge.getDownBridge;
+import static bridge.domain.UpDownBridge.getUpBridge;
+import static bridge.domain.UpDownBridge.initBridge;
+import static bridge.domain.UpDownBridge.makeUpDownBridge;
+
+import static org.assertj.core.util.Lists.newArrayList;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import bridge.ApplicationTest.TestNumberGenerator;
+import bridge.domain.BridgeGame;
+
+import java.util.List;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class BridgeTest {
+    BridgeGame bridgeGame = new BridgeGame();
+    BridgeNumberGenerator bridgeNumberGenerator = new TestNumberGenerator(newArrayList(1, 0, 0));
+    BridgeMaker bridgeMaker = new BridgeMaker(bridgeNumberGenerator);
+    List<String> bridges = bridgeMaker.makeBridge(3);
+    String moveResult = bridgeGame.move(bridges.get(0), "U");
+
+    @DisplayName("위,아래 다리 값 테스트")
+    @Test
+    void makeUpDownBridgeTest() {
+        makeUpDownBridge("U", moveResult);
+        String upBridge = getUpBridge().get(0);
+        String downBridge = getDownBridge().get(0);
+        assertEquals(upBridge, "O");
+        assertEquals(downBridge, " ");
+    }
+
+    @DisplayName("위,아래 다리 초기화 테스트")
+    @AfterEach
+    @Test
+    void UpDownBridgeInitTest() {
+        initBridge();
+        assertEquals(0, getUpBridge().size());
+        assertEquals(0, getDownBridge().size());
+    }
+}
