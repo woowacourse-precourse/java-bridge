0c01c202dc8bc7a328b71d7c43b583369158f390
DaWoon Lee <qkgk61@gmail.com>
2022-11-22 22:07:37 +0900

    Feat : 게임을 수행하는 클래스 GameController 구현

--- src/main/java/bridge/GameController.java
+++ src/main/java/bridge/GameController.java
@@ -3,42 +3,70 @@ package bridge;
 import bridge.domain.GameResult;
 import bridge.domain.MovingResult;
 import bridge.logic.BridgeGame;
+import bridge.util.BridgeMaker;
+import bridge.util.BridgeRandomNumberGenerator;
 import bridge.view.View;
 import java.util.List;
 
 public class GameController {
 
     private final View view;
-    private final List<String> bridge;
     private final BridgeGame bridgeGame;
 
-    public GameController(View view, List<String> bridge, BridgeGame bridgeGame) {
+    public GameController(View view, BridgeGame bridgeGame) {
         this.view = view;
-        this.bridge = bridge;
         this.bridgeGame = bridgeGame;
     }
 
-    public void play(int countOfTry, int round) {
-        while (round != bridge.size()) {
-            MovingResult movingResult = bridgeGame.move(view, bridge, round++);
-            view.printMap(movingResult);
+    public void play(Integer countOfTry) {
+        List<String> bridge = createBridge();
 
-            if (movingResult.isSuccess()) {
-                continue;
-            }
+        if (movePlayer(bridge, countOfTry)) {
+            end("성공", countOfTry);
+        }
 
-            if (!bridgeGame.retry(view)) {
-                GameResult gameResult = new GameResult("실패", countOfTry);
-                view.printResult(gameResult);
-                continue;
+        end("실패", countOfTry);
+    }
+
+    private List<String> createBridge() {
+        BridgeRandomNumberGenerator bridgeRandomNumberGenerator = new BridgeRandomNumberGenerator();
+        BridgeMaker bridgeMaker = new BridgeMaker(bridgeRandomNumberGenerator);
+
+        view.printStartGame();
+        int size = view.readBridgeSize();
+        return bridgeMaker.makeBridge(size);
+    }
+
+    private boolean movePlayer(List<String> bridge, Integer countOfTry) {
+        while (!canCrossBridge(bridge)) {
+            String gameCommand = view.readGameCommand();
+            if (!bridgeGame.retry(gameCommand)) {
+                return false;
             }
 
             countOfTry++;
-            round = 0;
             view.initMap();
         }
 
-        GameResult gameResult = new GameResult("성공", countOfTry);
+        return true;
+    }
+
+    private boolean canCrossBridge(List<String> bridge) {
+        for (int length = bridge.size(), round = 0; round < length; round++) {
+            String moving = view.readMoving();
+            MovingResult movingResult = bridgeGame.move(moving, bridge, round);
+            view.printMap(movingResult);
+
+            if (!movingResult.isSuccess()) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    private void end(String result, int countOfTry) {
+        GameResult gameResult = new GameResult(result, countOfTry);
         view.printResult(gameResult);
     }
 
