d6bc2138c188620eee5450c59fc4e05503d70148
desfox <desfox99@naver.com>
2022-11-22 18:38:26 +0900

    refactor(BridgeGameValidation): 에러 문구 ErrorMessage Enum으로 대체

--- src/main/java/bridge/validation/BridgeGameValidation.java
+++ src/main/java/bridge/validation/BridgeGameValidation.java
@@ -2,6 +2,8 @@ package bridge.validation;
 
 import java.util.List;
 
+import static bridge.validation.ErrorMessage.*;
+
 public class BridgeGameValidation {
     private static final String UNEXPECTED_WHEN_EQUAL = "X";
     private static final String UNEXPECTED_WHEN_UNEQUAL = "O";
@@ -11,7 +13,7 @@ public class BridgeGameValidation {
             int lastIdx = bridgeCurrStatus.size() - 1;
 
             if (bridgeCurrStatus.get(lastIdx).equals(UNEXPECTED_WHEN_EQUAL)) {
-                throw new IllegalArgumentException("[ERROR] 이동 가능한 칸이지만 이동하지 않았습니다.");
+                throw new IllegalArgumentException(MOVE_AVAILABLE_BUT_NOT_MOVED.getMessage());
             }
         }
     }
@@ -21,7 +23,7 @@ public class BridgeGameValidation {
             int lastIdx = bridgeCurrStatus.size() - 1;
 
             if (bridgeCurrStatus.get(lastIdx).equals(UNEXPECTED_WHEN_UNEQUAL)) {
-                throw new IllegalArgumentException("[ERROR] 이동할 수 없는 칸으로 이동했습니다.");
+                throw new IllegalArgumentException(MOVED_WHEN_UNAVAILABLE.getMessage());
             }
         }
     }
