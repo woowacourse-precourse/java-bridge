9f0a743674d1ba501cd3d755b1cd8d780b6d07be
sora5421 <sojeoung536985@gmail.com>
2022-11-22 19:06:55 +0900

    feat : add that check the user input is number

--- src/main/java/bridge/valid/InputValid.java
+++ src/main/java/bridge/valid/InputValid.java
@@ -2,12 +2,18 @@ package bridge.valid;
 
 public class InputValid {
     public boolean checkLengthValid(String test) {
-
+        isNumber(test);
         return true;
     }
 
     public boolean checkChoice(String test) {
 
         return true;
+    }
+
+    private void isNumber(String test) {
+        if (test.matches("[+-]?\\d*(\\.\\d+)?") == false) {
+            throw new IllegalArgumentException("[ERROR] 다리 길이는 하나의 숫자여야 합니다.");
+        }
     }
 }
