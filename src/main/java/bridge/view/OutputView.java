0e13ecb1c537746a0a8512aa80f331fad32b6104
Dabeen Jeong <hello70825@gmail.com>
2022-11-22 17:18:55 +0900

    feat: 게임 결과를 출력하는 기능 추가

--- src/main/java/bridge/view/OutputView.java
+++ src/main/java/bridge/view/OutputView.java
@@ -1,5 +1,7 @@
 package bridge.view;
 
+import bridge.BridgeGame;
+
 /**
  * 사용자에게 게임 진행 상황과 결과를 출력하는 역할을 한다.
  */
@@ -27,6 +29,10 @@ public class OutputView {
      * <p>
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void printResult() {
+    public void printResult(BridgeGame bridgeGame) {
+        System.out.println(END_GAME_MESSAGE);
+        System.out.println(bridgeGame.getBridgePrintMaker());
+        System.out.printf((RESULT_STATUS_MESSAGE) + "%n", bridgeGame.getResultMessage());
+        System.out.printf((TRY_MESSAGE) + "%n", bridgeGame.getTryCount());
     }
 }
