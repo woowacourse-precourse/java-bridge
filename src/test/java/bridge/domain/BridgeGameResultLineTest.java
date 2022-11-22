903778308687f99faade7717914902b17d34e74a
juno-junho <ssmm0205@naver.com>
2022-11-22 23:34:57 +0900

    test(domain): test refactoring

--- src/test/java/bridge/domain/BridgeGameResultLineTest.java
+++ src/test/java/bridge/domain/BridgeGameResultLineTest.java
@@ -1,33 +1,30 @@
 package bridge.domain;
 
-import bridge.constant.MoveCommand;
-import bridge.constant.MoveSign;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static bridge.constant.MoveCommand.*;
-import static bridge.constant.MoveSign.MOVE_SUCCESS;
+import static bridge.constant.MoveSign.*;
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
 
 class BridgeGameResultLineTest {
 
-    @DisplayName("받아온 MoveCommands가 자신이 가진 값과 같으면 moveResults를 리스트에 저장한다.")
-    @Test
-    void addMoveResults() {
-        BridgeGameResultLine upLineResults = new BridgeGameResultLine(MOVE_UP_VALUE);
-        upLineResults.addMoveResults(MOVE_UP_VALUE, MOVE_SUCCESS.getMessage());
-
-        assertThat(upLineResults.results()).containsOnly("O");
-    }
-
-    @DisplayName("받아온 MoveCommands가 자신이 가진 값과 다르면 공백을 리스트에 저장한다.")
+    @DisplayName("자신이 가진 값과 받아온 MoveCommand가 다르면 공백을 리스트에 저장")
     @Test
     void addBlank() {
         BridgeGameResultLine upLineResults = new BridgeGameResultLine(MOVE_UP_VALUE);
         upLineResults.addMoveResults(MOVE_DOWN_VALUE, MOVE_SUCCESS.getMessage());
 
         assertThat(upLineResults.results()).containsOnly(BridgeGameResultLine.BLANK);
+    }
+
+    @DisplayName("자신이 가진 값과 받아온 MoveCommand가 같으면 moveResults를 리스트에 저장")
+    @Test
+    void addMoveResults() {
+        BridgeGameResultLine upLineResults = new BridgeGameResultLine(MOVE_UP_VALUE);
+        upLineResults.addMoveResults(MOVE_UP_VALUE, MOVE_SUCCESS.getMessage());
+
+        assertThat(upLineResults.results()).containsOnly("O");
     }
 
 }
