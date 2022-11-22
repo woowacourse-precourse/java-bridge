5d8355011e874f9860b8c72a875c34c49f6d9be6
CYC-ChoiYoungChang <choiyc1446@gmail.com>
2022-11-22 15:04:27 +0900

    test: Judge 테스트 추가

--- src/test/java/bridge/BridgeTest.java
+++ src/test/java/bridge/BridgeTest.java
@@ -2,6 +2,7 @@ package bridge;
 
 import bridge.util.BridgeSizeValidator;
 import bridge.util.CommandValidator;
+import bridge.util.Judge;
 import camp.nextstep.edu.missionutils.Console;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -9,13 +10,14 @@ import org.junit.jupiter.api.Test;
 import java.io.ByteArrayInputStream;
 import java.io.InputStream;
 
-import static org.assertj.core.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class BridgeTest {
 
     @DisplayName("범위 벗어나는 숫자 입력시 예외 발생")
     @Test
-    void createBridgeByOverRange(){
+    void createBridgeByOverRange() {
         InputStream in = new ByteArrayInputStream("50".getBytes());
         System.setIn(in);
         assertThatThrownBy(() -> new BridgeSizeValidator().run(Console.readLine()))
@@ -24,7 +26,7 @@ public class BridgeTest {
 
     @DisplayName("잘못된 값 입력시 예외 발생")
     @Test
-    void createBridgeByWrongInput(){
+    void createBridgeByWrongInput() {
         InputStream in = new ByteArrayInputStream("10J".getBytes());
         System.setIn(in);
         assertThatThrownBy(() -> new BridgeSizeValidator().run(Console.readLine()))
@@ -33,7 +35,7 @@ public class BridgeTest {
 
     @DisplayName("길이 만큼 다리 생성")
     @Test
-    void createBridge(){
+    void createBridge() {
         BridgeMaker bridgeMaker = new BridgeMaker(new BridgeRandomNumberGenerator());
         assertThat(bridgeMaker.makeBridge(3).size())
                 .isEqualTo(3);
@@ -41,12 +43,21 @@ public class BridgeTest {
 
     @DisplayName("사용자 명령 잘못된 값일 경우 예외 발생")
     @Test
-    void moveCommandByWrongInput(){
+    void moveCommandByWrongInput() {
         CommandValidator commandValidator = new CommandValidator();
         assertThatThrownBy(() -> commandValidator.validate("W"))
                 .isInstanceOf(IllegalArgumentException.class);
 
         assertThatThrownBy(() -> commandValidator.retryValidate("W"))
                 .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("정답이면 O, 오답이면 X 반환")
+    @Test
+    void moveCommandCorrectTest() {
+        Judge judge = new Judge();
+
+        assertThat(judge.isCorrect("U", "U")).contains("O");
+        assertThat(judge.isCorrect("U", "D")).contains("X");
     }
 }
