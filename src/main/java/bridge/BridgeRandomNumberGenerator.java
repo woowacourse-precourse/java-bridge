9770f427e1b26e7a68a28403b31338d53fef7b97
wnsgml7267 <dlwns7267@naver.com>
2022-11-22 22:44:10 +0900

    refactor(bridge/BridgeRandomNumberGenerator): 오타 수정

--- src/main/java/bridge/BridgeRandomNumberGenerator.java
+++ src/main/java/bridge/BridgeRandomNumberGenerator.java
@@ -1,9 +1,8 @@
 package bridge;
 
-import bridge.domain.BridgeState;
 import camp.nextstep.edu.missionutils.Randoms;
 
-public class BridgeRandomNumberGenerator implements BridgeState {
+public class BridgeRandomNumberGenerator implements BridgeNumberGenerator {
 
     private static final int RANDOM_LOWER_INCLUSIVE = 0;
     private static final int RANDOM_UPPER_INCLUSIVE = 1;
