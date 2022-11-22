65e06bb7730ddc3baeac1cceb101fc536cf25d4a
권태현 <kygkth2011@gmail.com>
2022-11-22 23:13:43 +0900

    feat(bridge): Application 에서 try-catch 문 제거

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -6,15 +6,10 @@ import bridge.view.OutputView;
 
 public class Application {
     public static void main(String[] args) {
-        try {
             GameController gameController = new GameController();
             gameController.StartGame();
             while (gameController.playEachGame()) {
                 ;
             }
-        } catch (Exception e) {
-            System.out.println("ERROR : " + e.getMessage());
-        }
-
     }
 }
