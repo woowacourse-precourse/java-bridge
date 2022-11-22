ef0bdd18b83c0f6d002e6b9fa9ba34fbdabd1974
code-tree <dltjdqls1200@naver.com>
2022-11-22 11:51:41 +0900

    refactor: BridgeMaker 대신 일급 컬렉션인 Bridge에서 검증하도록 변경

--- src/main/java/bridge/BridgeMaker.java
+++ src/main/java/bridge/BridgeMaker.java
@@ -1,6 +1,5 @@
 package bridge;
 
-import bridge.domain.GameException;
 import bridge.domain.Move;
 
 import java.util.ArrayList;
@@ -10,10 +9,6 @@ import java.util.List;
  * 다리의 길이를 입력 받아서 다리를 생성해주는 역할을 한다.
  */
 public class BridgeMaker {
-
-    private static final int MINIMUM_SIZE = 3;
-    private static final int MAXIMUM_SIZE = 20;
-
     private final BridgeNumberGenerator bridgeNumberGenerator;
 
     public BridgeMaker(BridgeNumberGenerator bridgeNumberGenerator) {
@@ -25,19 +20,12 @@ public class BridgeMaker {
      * @return 입력받은 길이에 해당하는 다리 모양. 위 칸이면 "U", 아래 칸이면 "D"로 표현해야 한다.
      */
     public List<String> makeBridge(int size) {
-        validateSize(size);
-        final List<String> bridge = new ArrayList<>(size);
+        final List<String> bridge = new ArrayList<>();
 
         for (int i = 0; i < size; i++) {
             bridge.add(Move.convertDirection(bridgeNumberGenerator.generate()));
         }
 
         return bridge;
-    }
-
-    private void validateSize(int size) {
-        if (size < MINIMUM_SIZE || size > MAXIMUM_SIZE) {
-            throw new IllegalArgumentException(GameException.INVALID_BRIDGE_SIZE.name());
-        }
     }
 }
--- src/main/java/bridge/domain/Bridge.java
+++ src/main/java/bridge/domain/Bridge.java
@@ -3,9 +3,13 @@ package bridge.domain;
 import java.util.List;
 
 public class Bridge {
+    private static final int MINIMUM_SIZE = 3;
+    private static final int MAXIMUM_SIZE = 20;
+
     private final List<String> bridges;
 
     public Bridge(List<String> bridges) {
+        validateSize(bridges);
         this.bridges = bridges;
     }
 
@@ -17,6 +21,12 @@ public class Bridge {
         Move destination = Move.from(bridges.get(distance));
 
         return MoveResult.decide(destination, moveTo);
+    }
+
+    private void validateSize(List<String> bridges) {
+        if (bridges.size() < MINIMUM_SIZE || bridges.size() > MAXIMUM_SIZE) {
+            throw new IllegalArgumentException(GameException.INVALID_BRIDGE_SIZE.name());
+        }
     }
 
     @Override
