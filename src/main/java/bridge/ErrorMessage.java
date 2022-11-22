d85c23977c3c309e52435a333cc4bdbccdcbe386
juy4556 <kws05050@naver.com>
2022-11-22 16:47:09 +0900

    refactor: 에러메세지 변경

--- src/main/java/bridge/ErrorMessage.java
+++ src/main/java/bridge/ErrorMessage.java
@@ -1,9 +1,9 @@
 package bridge;
 
 public enum ErrorMessage {
-    NOT_NUMERIC_VALUE("[Error] 숫자만 입력해야 합니다."),
-    NOT_VALID_INTERVAL_VALUE("[Error] 다리 길이는 3부터 20 사이의 숫자여야 합니다."),
-    NOT_VALID_MOVING("[Error] 이동은 U(UP) 또는 D(DOWN) 중 하나만 선택하여 입력해야 합니다.")
+    NOT_NUMERIC_VALUE("[ERROR] 숫자만 입력해야 합니다."),
+    NOT_VALID_INTERVAL_VALUE("[ERROR] 다리 길이는 3부터 20 사이의 숫자여야 합니다."),
+    NOT_VALID_MOVING("[ERROR] 이동은 U(UP) 또는 D(DOWN) 중 하나만 선택하여 입력해야 합니다.")
     ;
 
     private final String message;
