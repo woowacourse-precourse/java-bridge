85caf08ff4514c13521b2518982c1cee5475eeb6
kiarakim <kiarakim413@gmail.com>
2022-11-22 16:29:29 +0900

    feat(bridge) : change bridgeController to bridgeGame

--- src/main/java/bridge/BridgeGame.java
+++ src/main/java/bridge/BridgeGame.java
@@ -1,23 +1,62 @@
 package bridge;
 
-/**
- * 다리 건너기 게임을 관리하는 클래스
- */
-public class BridgeGame {
+import bridge.domain.Bridge;
+import bridge.domain.User;
+import bridge.service.BridgeService;
+import bridge.service.ViewService;
 
-    /**
-     * 사용자가 칸을 이동할 때 사용하는 메서드
-     * <p>
-     * 이동을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
-     */
-    public void move() {
+public class BridgeGame {
+    private static final String RETRY = "R";
+    private static final String QUIT = "Q";
+    private final BridgeService bridgeService = new BridgeService();
+    private final ViewService viewService = new ViewService();
+
+    public void start() {
+        showStartMessage();
+        Bridge bridge = bridgeService.bridgeMaker();
+        User user = new User();
+        play(user, bridge);
     }
 
-    /**
-     * 사용자가 게임을 다시 시도할 때 사용하는 메서드
-     * <p>
-     * 재시작을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
-     */
-    public void retry() {
+    public void showStartMessage() {
+        viewService.printGameStart();
+    }
+
+    public void play(User user, Bridge bridge) {
+        while (!user.isGameDone()) {
+            String movement = viewService.requestMovement();
+            boolean isCorrect = bridge.isCorrectMovement(movement);
+            viewService.printMap(bridge.getCurrentBlock(), movement, isCorrect);
+            process(bridge, user, isCorrect);
+        }
+        viewService.printResult(user);
+    }
+
+    public void process(Bridge bridge, User user, boolean isCorrect) {
+        if (!isCorrect) {
+            String status = viewService.requestDecision();
+            processFailure(bridge, user, status);
+            return;
+        }
+        processSuccess(bridge, user);
+    }
+
+    public void processFailure(Bridge bridge, User user, String status) {
+        if (status.equals(RETRY)) {
+            viewService.initMap();
+            user.retry();
+            bridge.retry();
+        }
+        if (status.equals(QUIT)) {
+            user.loseGame();
+        }
+    }
+
+    public void processSuccess(Bridge bridge, User user) {
+        if (bridge.isLastBlock()) {
+            user.winGame();
+            return;
+        }
+        bridge.move();
     }
 }
