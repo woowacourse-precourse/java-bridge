60ae04d725ec700a0fb75f7fcc013be4a3264c8a
seheonCho <whtpgjs94@gmail.com>
2022-11-22 17:30:33 +0900

    refactor(InputMessage): 클래스 시그니처 변경
    final 을 추가하여, 상속을 막음

--- src/main/java/bridge/message/InputMessage.java
+++ src/main/java/bridge/message/InputMessage.java
@@ -1,6 +1,6 @@
 package bridge.message;
 
-public class InputMessage {
+public final class InputMessage {
 
     public static final String PLEASE_ENTER_BRIDGE_SIZE = "다리의 길이를 입력해주세요.";
     public static final String PLEASE_ENTER_MOVE_COMMAND = "이동할 칸을 선택해주세요. (위: U, 아래: D)";
