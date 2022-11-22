f3250149f9279a43bc9865fc284fe99daa6c0295
mgOh <issac_oh@naver.com>
2022-11-22 13:35:59 +0900

    feat: 사용자가 이동할 때마다 다리 건너기 결과를 출력하는 기능

--- src/main/java/bridge/Gaming.java
+++ src/main/java/bridge/Gaming.java
@@ -20,9 +20,33 @@ public class Gaming {
         return 0;
     }
     public boolean gameLoop(int count){
-        while (true){
+        /** 건널 수 있는 칸으로 이동했을 시, 다리를 끝까지 건넜는지 확인하는 기능 */
+        while (count < this.bridge.size()){
+            new OutputView().printGetMoving();
+            boolean moveResult = new BridgeGame().move(bridge, count, new InputView().readMoving()); // 잘 움직였는지 결과 가져오고
+            fillBridgeMap(count, moveResult); // 지도저장
             return false;
         }
+        return true;
+    }
+    /** 매 턴마다 결과에 따른 다리 형태를 두 문자열로 저장하는 기능 */
+    private boolean fillBridgeMap(int count, boolean moveResult){
+        if (count != 0) {this.upBridgeMap += "|"; this.DownBridgeMap += "|";}
+        if (moveResult) {fillUpBridgeMapCorrect(count); return true;}
+        fillUpBridgeMapIncorrect(count); return true;
+    }
+    /** 이동할 수 있는 칸을 선택한 경우 O, 이동할 수 없는 칸을 선택한 경우 X 표시하는 기능 */
+    private boolean fillUpBridgeMapCorrect(int count){
+        if (this.bridge.get(count).equals("U")){
+            this.upBridgeMap += " O "; this.DownBridgeMap += "   "; return true;
+        }
+        this.upBridgeMap += "   "; this.DownBridgeMap += " O "; return true;
+    }
+    private boolean fillUpBridgeMapIncorrect(int count){
+        if (this.bridge.get(count).equals("U")){
+            this.upBridgeMap += "   "; this.DownBridgeMap += " X "; return true;
+        }
+        this.upBridgeMap += " X "; this.DownBridgeMap += "   "; return true;
     }
 
 
--- src/main/java/bridge/OutputView.java
+++ src/main/java/bridge/OutputView.java
@@ -8,8 +8,18 @@ public class OutputView {
     public void printGameStart(){
         System.out.println("다리 건너기 게임을 시작합니다.\n");
     }
-
-    public void printMap() {
+    /** 길이의 다리를 입력하라는 안내 문구를 출력하는 기능 */
+    public void printGetBridgeSize(){
+        System.out.println("다리의 길이를 입력해주세요.");
+    }
+    /** 이동할 칸을 선택하라는 안내 문구를 출력하는 기능 */
+    public void printGetMoving(){
+        System.out.println("\n이동할 칸을 선택해주세요. (위: U, 아래: D)");
+    }
+    /** 사용자가 이동할 때마다 다리 건너기 결과를 출력하는 기능 */
+    public void printMap(String upBridgeMap, String DownBridgeMap) {
+        System.out.println("[" + upBridgeMap + "]");
+        System.out.println("[" + DownBridgeMap + "]");
     }
 
     public void printResult() {
