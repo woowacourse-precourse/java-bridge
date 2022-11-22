d5932f13136ef4840b203e67bef2365e5eea4f7d
gomljo <gomljo0307@naver.com>
2022-11-22 17:11:49 +0900

    refactor(GameRecordMaker): 기능 이전
    현재의 게임 기록을 만드는 기능 GameRecordGenerator로 이전

--- src/main/java/bridge/domain/model/GameRecordMaker.java
+++ src/main/java/bridge/domain/model/GameRecordMaker.java
@@ -1,68 +1,43 @@
 package bridge.domain.model;
 
-import bridge.domain.constant.GameRecordFormat;
-import bridge.domain.constant.GameRecordSymbol;
+import bridge.domain.constant.*;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.ListIterator;
 
 public class GameRecordMaker {
-    private final List<List<String>> TOTAL_RESULT;
-    private final int ZERO=0;
+    private List<List<String>> TOTAL_RESULT;
 
-    private List<String> currentGameRecord;
-    private int recordLength;
-
-    public GameRecordMaker(){
-        recordLength=ZERO;
-        TOTAL_RESULT=List.of(new ArrayList<>(), new ArrayList<>());
-    }
-    private boolean isRecordLengthOverOne(){
-        return recordLength==ZERO;
+    public GameRecordMaker() {
+        TOTAL_RESULT = List.of(new ArrayList<>(), new ArrayList<>());
     }
 
-    private String makeFormat(boolean isFirst, String record){
-        GameRecordFormat gameRecordFormat = GameRecordFormat.findByTrial(isFirst);
-        return gameRecordFormat.generate(record);
-    }
+    public void updateResult(List<String> currentGameRecord) {
+        ListIterator<List<String>> totalResult = TOTAL_RESULT.listIterator();
 
-    private List<String> transformCurrentRecord(boolean isFirst, List<String> gameRecord){
-        List<String> transformedRecord = new ArrayList<>();
-        for(String record: gameRecord){
-            String formattedRecord = makeFormat(isFirst, record);
-            transformedRecord.add(formattedRecord);
-        }
-        return transformedRecord;
-    }
-
-    public void updateCurrentRecord(List<String> gameRecord){
-        boolean isFirst = isRecordLengthOverOne();
-        currentGameRecord=transformCurrentRecord(isFirst, gameRecord);
-    }
-
-    public void updateResult(){
-        ListIterator<List<String>> totalResult =TOTAL_RESULT.listIterator();
-        while(totalResult.hasNext()){
+        while (totalResult.hasNext()) {
             int currentIndex = totalResult.nextIndex();
             List<String> result = totalResult.next();
             result.add(currentGameRecord.get(currentIndex));
         }
-        ++recordLength;
     }
 
-    public String makeMapFormat(List<String> result){
-        StringBuilder formattedResult = new StringBuilder();
-        formattedResult.append(GameRecordSymbol.START_BRACKET.getSymbol());
-        formattedResult.append(String.join(GameRecordSymbol.BLANK.getSymbol(), result));
-        formattedResult.append(GameRecordSymbol.END_BRACKET.getSymbol());
-        formattedResult.append(GameRecordSymbol.NEW_LINE.getSymbol());
-        return formattedResult.toString();
+    public String makeMapFormat(List<String> result) {
+        String formattedResult = GameRecordSymbol.START_BRACKET.getSymbol() +
+                String.join(GameRecordSymbol.BLANK.getSymbol(), result) +
+                GameRecordSymbol.END_BRACKET.getSymbol() +
+                GameRecordSymbol.NEW_LINE.getSymbol();
+        return formattedResult;
+    }
+
+    public void reset(){
+        TOTAL_RESULT = List.of(new ArrayList<>(), new ArrayList<>());
     }
 
     public String getRecord() {
         StringBuilder record = new StringBuilder();
-        for(List<String> result: TOTAL_RESULT){
+        for (List<String> result : TOTAL_RESULT) {
             record.append(makeMapFormat(result));
         }
         return record.toString();
