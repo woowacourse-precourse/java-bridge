02f4d01e6b99e2da1257d915ce766dc7a8aba950
JJONSOO <pjs970616@naver.com>
2022-11-22 21:56:57 +0900

    feat : 게임 재시도 플로우

--- src/main/java/bridge/Controller.java
+++ src/main/java/bridge/Controller.java
@@ -17,4 +17,30 @@ public class Controller {
         run();
         checkRetry();
     }
+    private void run(){
+        while (bridgeGame.getUserInputSize()< bridgeGame.getBridgeSize()){
+            OutputView.printMove();
+            boolean moveCheck = bridgeGame.move(InputView.readMoving());
+            OutputView.printUpDownBridge(bridgeGame.bridge,bridgeGame.userInput);
+            if(!moveCheck){break;}
+        }
+    }
+    private void checkRetry(){
+        if(bridgeGame.gameOver()){
+            OutputView.printResult(bridgeGame.bridge, bridgeGame.userInput, gameCount);
+            return;
+        }
+        askRetry();
+    }
+    private void askRetry(){
+        OutputView.printRetry();
+        String command = InputView.readGameCommand();
+        if(bridgeGame.retry(command)){
+            gameCount+=1;
+            gameStart();
+        }
+        if(!bridgeGame.retry(command)){
+            OutputView.printResult(bridgeGame.bridge,bridgeGame.userInput,gameCount);
+        }
+    }
 }
