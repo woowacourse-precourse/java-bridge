5dc0bd541ba450d1ddcb5a078738ad97e7dde3d4
HAERYN <syt06119@naver.com>
2022-11-22 19:49:12 +0900

    feat: OutputView 기능 추가

--- src/main/java/bridge/BridgeMaker.java
+++ src/main/java/bridge/BridgeMaker.java
@@ -19,19 +19,7 @@ public class BridgeMaker {
      * @return 입력받은 길이에 해당하는 다리 모양. 위 칸이면 "U", 아래 칸이면 "D"로 표현해야 한다.
      */
     public List<String> makeBridge(int size) {
-        int index = 17;
-        int turn = 17;
-        int life = 16;
-        boolean[][] arr_bool = new boolean[18][2];
-        String[][] arr_glass = new String[18][2];
-        for (int i = 0; i < arr_bool.length; i++) {
-            arr_bool[i][0] = new Random().nextBoolean();
-            arr_bool[i][1] = !arr_bool[i][0];
-            for (int j = 0; j < arr_bool[i].length; j++) {
-                arr_glass[i][j] = "□";
-            };
-        }
+
         return null;
     }
-
 }
