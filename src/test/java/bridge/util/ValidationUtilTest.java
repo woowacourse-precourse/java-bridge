641953c0a7f86133d95471978bbe0741fe686a76
jiwon <ljwon77@daum.net>
2022-11-22 15:58:13 +0900

    teat: validate bridge length about wrong input range
    잘못된 범위의 다리 길이가 들어왔을 때 예외가 발생하는지 테스트를 진행하였습니다.

--- src/test/java/bridge/util/ValidationUtilTest.java
+++ src/test/java/bridge/util/ValidationUtilTest.java
@@ -6,6 +6,9 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
+import static bridge.constant.BridgeRange.MAX_LENGTH;
+import static bridge.constant.BridgeRange.MIN_LENGTH;
+import static bridge.constant.message.ExceptionMessage.BRIDGE_LENGTH_RANGE;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class ValidationUtilTest {
@@ -21,6 +24,20 @@ class ValidationUtilTest {
                 validationUtil.validateBridgeLength(input))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining(ExceptionMessage.BRIDGE_LENGTH_TYPE.getMessage());
+    }
+
+    @DisplayName("잘못된 범위의 값이 들어왔을 때 예외를 터트려야 한다.")
+    @ParameterizedTest
+    @ValueSource(strings = {"21", "2", "100"})
+    void 다리_길이_범위_검증_테스트(String input) throws Exception {
+        //given
+        ValidationUtil validationUtil = new ValidationUtil();
+        //when, then
+        assertThatThrownBy(() ->
+                validationUtil.validateBridgeLength(input))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(String.format(BRIDGE_LENGTH_RANGE.getMessage(),
+                        MIN_LENGTH.getNumber(), MAX_LENGTH.getNumber()));
     }
 }
 
