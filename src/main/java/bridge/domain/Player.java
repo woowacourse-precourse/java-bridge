2f5c6048b071db6c4a506369ebfef7f98de72e8e
juno-junho <ssmm0205@naver.com>
2022-11-22 23:22:31 +0900

    feat(domain): create class Player

---
+++ src/main/java/bridge/domain/Player.java
@@ -0,0 +1,35 @@
+package bridge.domain;
+
+public class Player {
+    private Position position;
+    private ChallengeNumbers challengeNumbers;
+
+    public Player(Position position, ChallengeNumbers challengeNumbers) {
+        this.position = position;
+        this.challengeNumbers = challengeNumbers;
+    }
+
+    public boolean isOnTheBridge(final Bridge bridge) {
+        return bridge.isLongerThan(position);
+    }
+
+    public void move() {
+        position = position.increase();
+    }
+
+    public void resetPosition() {
+        position = position.reset();
+    }
+
+    public void increaseChallenges() {
+        challengeNumbers = challengeNumbers.increase();
+    }
+
+    public int position() {
+        return position.value();
+    }
+
+    public int numberOfChallenges() {
+        return challengeNumbers.value();
+    }
+}
