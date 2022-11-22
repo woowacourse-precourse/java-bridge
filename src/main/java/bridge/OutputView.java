7d1149dffa16fc4445439ea6cf972a8d5306cc4e
Yeongseon Park <yseon406@gmail.com>
2022-11-22 15:04:17 +0900

    feat: 최종 게임 결과 출력 추가

--- src/main/java/bridge/OutputView.java
+++ src/main/java/bridge/OutputView.java
@@ -1,13 +1,9 @@
 package bridge;
 
-import java.util.List;
-
 /**
  * 사용자에게 게임 진행 상황과 결과를 출력하는 역할을 한다.
  */
 public class OutputView {
-    private String upResult = "";
-    private String downResult = "";
 
     public void printStartMessage() {
         System.out.println("다리 건너기 게임을 시작합니다.");
@@ -18,27 +14,7 @@ public class OutputView {
      * <p>
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void printMap(List<String> player, String result) {
-        for (String move : player) {
-            if (move.equals("U")) {
-                upResult += result;
-                downResult += " ";
-            }
-            if (move.equals("D")) {
-                upResult += " ";
-                downResult += result;
-            }
-        }
-        printResult();
-    }
-
-    /**
-     * 게임의 최종 결과를 정해진 형식에 맞춰 출력한다.
-     * <p>
-     * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
-     */
-    public void printResult() {
-        System.out.println("최종 게임 결과");
+    public void printMap(String upResult, String downResult) {
         System.out.printf("[ %c", upResult.charAt(0));
         for (int index = 1; index < upResult.length(); index++) {
             System.out.printf(" | %c", upResult.charAt(index));
@@ -49,5 +25,16 @@ public class OutputView {
             System.out.printf(" | %c", downResult.charAt(index));
         }
         System.out.println(" ]");
+    }
+
+
+    /**
+     * 게임의 최종 결과를 정해진 형식에 맞춰 출력한다.
+     * <p>
+     * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
+     */
+    public void printResult(String upResult, String downResult) {
+        System.out.println("최종 게임 결과");
+        printMap(upResult, downResult);
     }
 }
