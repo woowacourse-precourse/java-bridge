b6be98e77dd4d80cd03cc6ad5ba3d60266f2e820
jeongjihun <wlgns2234@naver.com>
2022-11-22 18:13:32 +0900

    refactor: 원시문자들을 멤버변수로 분리

--- src/main/java/bridge/validation/Exception.java
+++ src/main/java/bridge/validation/Exception.java
@@ -3,10 +3,18 @@ package bridge.validation;
 import bridge.constant.ErrorMessage;
 
 public class Exception {
+    private static final int MIN_BRIDGE_SIZE = 3;
+    private static final int MAX_BRIDGE_SIZE = 20;
+    private static final int ASCII_MIN_NUMBER = 48;
+    private static final int ASCII_MAX_NUMBER = 57;
+    private static final String INPUT_UP_TO_JUDGE= "U";
+    private static final String INPUT_DOWN_TO_JUDGE = "D";
+    private static final String INPUT_QUIT_TO_JUDGE = "Q";
+    private static final String INPUT_RESTART_TO_JUDGE = "R";
 
     public static void checkSizeException(String input) {
         for (int i = 0; i < input.length(); i++) {
-            if ((int) input.charAt(i) < 48 || (int) input.charAt(i) > 57) {
+            if ((int) input.charAt(i) < ASCII_MIN_NUMBER || (int) input.charAt(i) > ASCII_MAX_NUMBER) {
                 throw new IllegalArgumentException(ErrorMessage.IS_NUMBER.message);
             }
         }
@@ -14,19 +22,19 @@ public class Exception {
 
     public static void checkRangeException(String input) {
         int size = Integer.parseInt(input);
-        if (size < 3 || 19 < size) {
+        if (size < MIN_BRIDGE_SIZE || MAX_BRIDGE_SIZE < size) {
             throw new IllegalArgumentException();
         }
     }
 
     public static void checkCorrectInputUpDown(String input) {
-        if (!(input.equals("U") || input.equals("D"))) {
+        if (!(input.equals(INPUT_UP_TO_JUDGE) || input.equals(INPUT_DOWN_TO_JUDGE))) {
             throw new IllegalArgumentException(ErrorMessage.INPUT_UP_DOWN.message);
         }
     }
 
     public static void checkCorrectInputQuitRestart(String input) {
-        if (!(input.equals("Q") || input.equals("R"))) {
+        if (!(input.equals(INPUT_QUIT_TO_JUDGE) || input.equals(INPUT_RESTART_TO_JUDGE))) {
             throw new IllegalArgumentException(ErrorMessage.INPUT_QUIT_RESTART.message);
         }
     }
