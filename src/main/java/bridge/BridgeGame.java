838a00775f810af3f2dbc24708105b6a2b4672f4
yeop0740 <ssucgoo@gmail.com>
2022-11-22 22:32:23 +0900

    feat(Application): 다리 건너기 게임을 순서에 맞게 진행

--- src/main/java/bridge/BridgeGame.java
+++ src/main/java/bridge/BridgeGame.java
@@ -7,6 +7,16 @@ import java.util.List;
  */
 public class BridgeGame {
 
+    BridgeMaker bridgeMaker;
+    InputView inputView;
+    OutputView outputView;
+
+    public BridgeGame() {
+        this.bridgeMaker = new BridgeMaker(new BridgeRandomNumberGenerator());
+        this.inputView = new InputView();
+        this.outputView = new OutputView();
+    }
+
     /**
      * 사용자가 칸을 이동할 때 사용하는 메서드
      * <p>
