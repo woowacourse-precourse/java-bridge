97776f39101e91693dea3fa185e7af160c2509bc
이건회 <rg970604@naver.com>
2022-11-22 19:47:34 +0900

    style(SafeSpot.java) : on demand static 선언

--- src/main/java/bridge/domain/SafeSpot.java
+++ src/main/java/bridge/domain/SafeSpot.java
@@ -1,12 +1,12 @@
 package bridge.domain;
 
 
-import bridge.util.Constant;
-import bridge.util.Validator;
+import static bridge.util.Constant.*;
+import static bridge.util.Validator.*;
 
 public enum SafeSpot {
-    DOWN(0, Constant.DOWN),
-    UP(1, Constant.UP);
+    DOWN(0, DOWNLOCATION),
+    UP(1, UPLOCATION);
 
     private final int locationNum;
     private final String location;
@@ -17,12 +17,12 @@ public enum SafeSpot {
     }
 
     public static String locationOf(int locationNum) {
-        Validator.validateLocationNum(locationNum);
+        validateLocationNum(locationNum);
         for (SafeSpot spot : SafeSpot.values()) {
             if (locationNum == spot.locationNum) {
                 return spot.location;
             }
         }
-        throw new IllegalArgumentException(Constant.SPOTNAME_NOTING);
+        throw new IllegalArgumentException(SPOTNAME_NOTING);
     }
 }
