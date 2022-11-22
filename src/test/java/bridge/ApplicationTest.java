44bb3cba25324dfb576ce9559544b4dc7c9f8140
KSH <tkdgns543@gmail.com>
2022-11-22 14:24:22 +0900

    test: 게임 3번째 시도에 성공 테스트

--- src/test/java/bridge/ApplicationTest.java
+++ src/test/java/bridge/ApplicationTest.java
@@ -194,5 +194,25 @@ class ApplicationTest extends NsTest {
                 assertThat(upSideIndex).isLessThan(downSideIndex);
             }, 1, 0, 0);
         }
+
+        @Test
+        void gameCountTest() {
+            assertRandomNumberInRangeTest(() -> {
+                run("3", "U", "D", "U", "R", "U", "U", "R", "U", "D", "D");
+                assertThat(output()).contains(
+                        "[ O |   | X ]",
+                        "[   | O |   ]",
+                        "[ O | X ]",
+                        "[   |   ]",
+                        "[ O |   |   ]",
+                        "[   | O | O ]",
+                        "최종 게임 결과",
+                        "[ O |   |   ]",
+                        "[   | O | O ]",
+                        "게임 성공 여부: 성공",
+                        "총 시도한 횟수: 3"
+                );
+            }, 1, 0, 0);
+        }
     }
 }
