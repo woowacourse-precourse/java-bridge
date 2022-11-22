5ada05b3a90c45a8637f813c9bf3535d5029ea2e
heejeong <hjx66@naver.com>
2022-11-22 22:12:58 +0900

    fix: main문에서 예외 처리 추가

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -5,6 +5,11 @@ import bridge.controller.BridgeSystem;
 public class Application {
     public static void main(String[] args) {
         BridgeSystem bridgeSystem = new BridgeSystem();
-        bridgeSystem.run();
+
+        try {
+            bridgeSystem.run();
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+        }
     }
 }
