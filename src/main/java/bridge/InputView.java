7c469d78d1a38d66a9ce7522584310afcafff413
jay <jaewhan232@naver.com>
2022-11-22 18:51:48 +0900

    Refactor : BridgeGameView 레이어 삭제 및 InputView로 통합

--- src/main/java/bridge/InputView.java
+++ src/main/java/bridge/InputView.java
@@ -60,4 +60,37 @@ public class InputView {
             throw new IllegalArgumentException("재시도 여부는 R 또는 Q로 입력해주세요.");
     }
 
+    public int getBridgeSize() {
+        System.out.println("다리의 길이를 입력해주세요.");
+
+        try {
+            return readBridgeSize();
+        } catch (IllegalArgumentException e) {
+            System.out.println("[ERROR] " + e.getMessage());
+            return getBridgeSize();
+        }
+    }
+
+    public String getMoving() {
+        System.out.println("이동할 칸을 선택해주세요. (위: U, 아래: D)");
+
+        try {
+            return readMoving();
+        } catch (IllegalArgumentException e) {
+            System.out.println("[ERROR] " + e.getMessage());
+            return getMoving();
+        }
+    }
+
+    public String getGameCommand() {
+        System.out.println("게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)");
+
+        try {
+            return readGameCommand();
+        } catch (IllegalArgumentException e) {
+            System.out.println("[ERROR] " + e.getMessage());
+            return getGameCommand();
+        }
+    }
+
 }
