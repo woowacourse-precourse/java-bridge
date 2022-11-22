670cb305a2f42b6a3de29f8353da28a84bcfe225
EUNSH1 <tkdgus968@naver.com>
2022-11-22 16:44:36 +0900

    refactor : 사용하지 않는 임포트문 제거

--- src/test/java/bridge/InputViewTest.java
+++ src/test/java/bridge/InputViewTest.java
@@ -1,19 +1,11 @@
 package bridge;
 
-import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
-import static org.assertj.core.util.Lists.newArrayList;
-
-import bridge.exception.BridgeGameException;
 import bridge.view.InputView;
 import java.io.ByteArrayInputStream;
 import java.io.InputStream;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.ValueSource;
 
 
 class InputViewTest {
