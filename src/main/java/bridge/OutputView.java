abe88c349e6143f0dd53edc6a7dabff954850669
hong <hong@hongui-MacBookAir.local>
2022-11-22 16:20:08 +0900

    feat: 출력 UI 추가

--- src/main/java/bridge/OutputView.java
+++ src/main/java/bridge/OutputView.java
@@ -1,5 +1,7 @@
 package bridge;
 
+import java.util.List;
+
 /**
  * 사용자에게 게임 진행 상황과 결과를 출력하는 역할을 한다.
  */
@@ -10,7 +12,38 @@ public class OutputView {
      * <p>
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void printMap() {
+    public void printMap(BridgeGame bridgeGame) {
+        StringBuilder upUI;
+        StringBuilder downUI;
+
+        upUI = progressUI(bridgeGame.getMoveProgress(), true);
+        downUI = progressUI(bridgeGame.getMoveProgress(), false);
+
+        System.out.println(upUI);
+        System.out.println(downUI);
+    }
+
+    private StringBuilder progressUI(List<String> progress, boolean isUp){
+        StringBuilder sb = new StringBuilder();
+        sb.append("[ ");
+        for(int i=0; i<progress.size(); i++){
+            sb.append(progressUIMiddle(progress.get(i), isUp));
+            if(progress.size()-1 != i){
+                sb.append(" | ");
+            }
+        }
+        sb.append(" ]");
+        return sb;
+    }
+
+    private String progressUIMiddle(String direction, boolean isUp){
+        if(isUp && direction.equals("U")){
+            return "O";
+        }
+        if(!isUp && direction.equals("D")){
+            return "O";
+        }
+        return " ";
     }
 
     /**
@@ -18,6 +51,16 @@ public class OutputView {
      * <p>
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void printResult() {
+    public void printResult(BridgeGame bridgeGame) {
+        System.out.println("최종 게임 결과");
+        printMap(bridgeGame);
+
+        String successText = "실패";
+        if(bridgeGame.isMoveOk()){
+            successText = "성공";
+        }
+
+        System.out.println("게임 성공 여부: " + successText);
+        System.out.println("총 시도한 횟수: " + bridgeGame.getTryCnt());
     }
 }
