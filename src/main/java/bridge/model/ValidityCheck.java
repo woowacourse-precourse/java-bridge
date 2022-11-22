e2048673407f62f97a15a586b92f4d0086cca46d
JunTaeINC <juntea3523@naver.com>
2022-11-22 21:52:30 +0900

    feat : (ValidityCheck) numberRangeChecker 기능 구현

--- src/main/java/bridge/model/ValidityCheck.java
+++ src/main/java/bridge/model/ValidityCheck.java
@@ -4,9 +4,20 @@ import java.util.regex.Pattern;
 
 public class ValidityCheck {
     private static final String ZERO_TO_NINE = "^[0-9]*$";
+    private static final int RANGE_MIN = 3;
+    private static final int RANGE_MAX = 20;
+
 
     public boolean stringChecker(String testObject) {
         Pattern zeroToNine = Pattern.compile(ZERO_TO_NINE);
         return zeroToNine.matcher(testObject).matches();
+    }
+
+    public boolean numberRangeChecker(String testObject) {
+        if (stringChecker(testObject)) {
+            return RANGE_MIN <= Integer.parseInt(testObject) &&
+                    Integer.parseInt(testObject) <= RANGE_MAX;
+        }
+        return true;
     }
 }
