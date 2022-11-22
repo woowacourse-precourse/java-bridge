4cb14ab5f376065ea00f0a19d44d6146d77b51d0
qkdk <86948395+qkdk@users.noreply.github.com>
2022-11-22 20:39:25 +0900

    test: GameRecord Test코드 작성

---
+++ src/test/java/bridge/GameRecordTest.java
@@ -0,0 +1,56 @@
+package bridge;
+
+import static org.assertj.core.api.Assertions.*;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class GameRecordTest {
+
+    private final GameRecord gameRecord = new GameRecord();
+
+    @Test
+    @DisplayName("update할때마다 playTime은 1씩 증가")
+    void updatePlayTimeAndGetPlayTime() {
+        gameRecord.updatePlayTime();
+        gameRecord.updatePlayTime();
+
+        assertThat(gameRecord.getPlayTime()).isEqualTo(2);
+    }
+
+    @Test
+    @DisplayName("정상적으로 다리기록이 만들어 졌을때")
+    void makeBridgeRecordByRightInput() {
+        gameRecord.addRecord("U", true);
+        gameRecord.addRecord("D", true);
+        gameRecord.addRecord("D", false);
+
+        assertThat(gameRecord.makeBridgeRecord()).isEqualTo("[ O |   |   ]\n[   | O | X ]\n");
+    }
+
+    @Test
+    @DisplayName("레코드가 없다면 결과에는 O나 X가 들어있지 않아야 한다.")
+    void recordClear() {
+        gameRecord.addRecord("U", true);
+
+        gameRecord.clear();
+
+        assertThat(gameRecord.makeBridgeRecord().toCharArray()).doesNotContain('O', 'X');
+    }
+
+    @Test
+    @DisplayName("게임이 성공했다면 true를 반환한다.")
+    void whenGameSuccess() {
+        gameRecord.addRecord("D", true);
+
+        assertThat(gameRecord.isGameSuccess()).isTrue();
+    }
+
+    @Test
+    @DisplayName("게임이 실패했다면 false를 반환한다.")
+    void whenGameFailure() {
+        gameRecord.addRecord("D", false);
+
+        assertThat(gameRecord.isGameSuccess()).isFalse();
+    }
+}
