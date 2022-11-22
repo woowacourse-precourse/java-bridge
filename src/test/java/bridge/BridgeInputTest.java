6f7c75fb6423b7d9d48be465d12df4ca169c839d
Jongha Park <jongha2788@u.sogang.ac.kr>
2022-11-22 19:07:03 +0900

    fix : 테스트 추가

---
+++ src/test/java/bridge/BridgeInputTest.java
@@ -0,0 +1,33 @@
+package bridge;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
+
+import bridge.Validator.Validator;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class BridgeInputTest {
+    @DisplayName("다리 길이는 3이상 20이하여야 한다.")
+    @Test
+    void 다리_길이_LOWER_테스트() {
+        String bridgeLength = "2";
+        assertThatThrownBy(() -> Validator.validateBridgeLength(bridgeLength)).isInstanceOf(
+            IllegalArgumentException.class);
+    }
+
+    @DisplayName("다리 길이는 3이상 20이하여야 한다.")
+    @Test
+    void 다리_길이_UPPER_테스트() {
+        String bridgeLength = "21";
+        assertThatThrownBy(() -> Validator.validateBridgeLength(bridgeLength)).isInstanceOf(
+            IllegalArgumentException.class);
+    }
+
+    @DisplayName("다리 길이는 정수여야 한다.")
+    @Test
+    void 다리_길이_정수_테스트() {
+        String bridgeLength = "SS";
+        assertThatThrownBy(() -> Validator.validateNumber(bridgeLength)).isInstanceOf(
+            IllegalArgumentException.class);
+    }
+}
---
+++ src/test/java/bridge/DirectionTypeTest.java
@@ -0,0 +1,25 @@
+package bridge;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
+
+import bridge.Validator.Validator;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class DirectionTypeTest {
+    @DisplayName("Direction는  U 또는 D 이므로 길이가 1이어야 한다.")
+    @Test
+    void Direction_D_R_len() {
+        String bridgeMoving = "UU";
+        assertThatThrownBy(() -> Validator.validateMovingLen(bridgeMoving)).isInstanceOf(
+            IllegalArgumentException.class);
+    }
+
+    @DisplayName("Direction 은 U 또는 D 여야 한다.")
+    @Test
+    void Direction_D_R() {
+        String bridgeMoving = "A";
+        assertThatThrownBy(() -> Validator.validateMoving(bridgeMoving)).isInstanceOf(
+            IllegalArgumentException.class);
+    }
+}
