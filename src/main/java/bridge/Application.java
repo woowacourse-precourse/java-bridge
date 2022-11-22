ee51767e0d93f764713495137a84faa91f52ffea
KJE <12181501@inha.edu>
2022-11-22 17:05:26 +0900

    feat: Application-startGame 메서드 작성

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -3,8 +3,44 @@ package bridge;
 public class Application {
 
     public static void main(String[] args) {
-
+        System.out.println("다리 건너기 게임을 시작합니다.\n");
+        BridgeGame bridgeGame = new BridgeGame();
+        startGame(bridgeGame);
+        while(moveGame(bridgeGame)){
+            if(!retryGame(bridgeGame))
+                break;
+        }
+        endGame(bridgeGame);
     }
 
+    public static void startGame(BridgeGame bridgeGame){
+        boolean flag ;
+        do {
+            try {
+                flag = false;
+                int bridgeSize = InputView.readBridgeSize();
+                bridgeGame.setSize(bridgeSize);
+            } catch (IllegalArgumentException e) {
+                flag = true;
+            }
+        } while (flag);
+    }
+
+    public static boolean moveGame(BridgeGame bridgeGame){
+        boolean continueGame = true;
+        boolean bridgeStatus = true;
+        while (continueGame) {
+            while (bridgeStatus) {
+                String userMove = InputView.readMoving();
+                bridgeStatus = bridgeGame.move(userMove);
+                OutputView.printMap(bridgeGame.getBridge(), bridgeGame.getNowIdx(), userMove);
+            }
+        }
+    }
+
+    public static boolean retryGame(BridgeGame bridgeGame){}
+    public static void endGame(BridgeGame bridgeGame){
+
+    }
 
 }
