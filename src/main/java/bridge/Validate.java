3414ff086ecc100726aa63e19932b4ae59fd9064
Jaewoong Kim <kimje1109@gmail.com>
2022-11-22 20:10:03 +0900

    refactor: 예외처리 에러 메세지 상수 분리

--- src/main/java/bridge/Validate.java
+++ src/main/java/bridge/Validate.java
@@ -1,52 +1,45 @@
 package bridge;
 
 import java.util.regex.Pattern;
+import static bridge.ValidateDetail.*;
 
 public class Validate {
-    private final static String NUMERIC_PATTERN = "^[0-9]*$";
-    private final static String UP_OR_DOWN_PATTERN = "^[UD]*$";
-    private final static String RESTART_OR_QUIT_PATTERN = "^[RQ]*$";
-    private final static int MAX_BRIDGE_SIZE = 20;
-    private final static int MIN_BRIDGE_SIZE = 3;
-    private final static String RANGE_ERROR = "[ERROR] 다리 길이는 3부터 20 사이의 숫자여야 합니다.";
-    private final static String MOVING_ERROR = "[ERROR] 이동할 칸은 U또는 D 중 하나의 문자여야 합니다.";
-    private final static String COMMAND_ERROR = "[ERROR] 게임 재시작/종료 여부는 R또는 Q 중 하나의 문자여야합니다.";
 
     private boolean isNumeric(String input) {
-        return Pattern.matches(NUMERIC_PATTERN, input);
+        return Pattern.matches(NUMERIC_PATTERN.getPattern(), input);
     }
 
     private boolean isBridgeSizeRange(String input) {
         if (isNumeric(input)){
-            return (MIN_BRIDGE_SIZE <= Integer.parseInt(input)
-                    && Integer.parseInt(input) <= MAX_BRIDGE_SIZE);
+            return (MIN_BRIDGE_SIZE.getSize() <= Integer.parseInt(input)
+                    && Integer.parseInt(input) <= MAX_BRIDGE_SIZE.getSize());
         }
         return false;
     }
 
     public void isBridgeSize(String input){
         if (!isBridgeSizeRange(input)) {
-            throw new IllegalArgumentException(RANGE_ERROR);
+            throw new IllegalArgumentException(RANGE_ERROR.getErrorMassage());
         }
     }
 
     private boolean isUpOrDown(String input) {
-        return Pattern.matches(UP_OR_DOWN_PATTERN, input);
+        return Pattern.matches(UP_OR_DOWN_PATTERN.getPattern(), input);
     }
 
     public void isMoving(String input) {
         if (!isUpOrDown(input)){
-            throw new IllegalArgumentException(MOVING_ERROR);
+            throw new IllegalArgumentException(MOVING_ERROR.getErrorMassage());
         }
     }
 
     private boolean isRestartOrQuit(String input) {
-        return Pattern.matches(RESTART_OR_QUIT_PATTERN, input);
+        return Pattern.matches(RESTART_OR_QUIT_PATTERN.getPattern(), input);
     }
 
     public void isGameCommand(String input) {
         if (!isRestartOrQuit(input)){
-            throw new IllegalArgumentException(COMMAND_ERROR);
+            throw new IllegalArgumentException(COMMAND_ERROR.getErrorMassage());
         }
     }
 }
---
+++ src/main/java/bridge/ValidateDetail.java
@@ -0,0 +1,27 @@
+package bridge;
+
+public enum ValidateDetail {
+    NUMERIC_PATTERN("^[0-9]*$"),
+    UP_OR_DOWN_PATTERN("^[UD]*$"),
+    RESTART_OR_QUIT_PATTERN("^[RQ]*$"),
+    MAX_BRIDGE_SIZE("20"),
+    MIN_BRIDGE_SIZE("3"),
+    ERROR("[ERROR] "),
+    RANGE_ERROR("다리 길이는 3부터 20 사이의 숫자여야 합니다."),
+    MOVING_ERROR("이동할 칸은 U또는 D 중 하나의 문자여야 합니다."),
+    COMMAND_ERROR("게임 재시작/종료 여부는 R또는 Q 중 하나의 문자여야합니다.");
+
+    private final String message;
+
+    ValidateDetail(String message) {
+        this.message = message;
+    }
+
+    public String getPattern() { return message; }
+
+    public String getErrorMassage() {
+        return ERROR.message + message;
+    }
+
+    public Integer getSize() { return Integer.parseInt(message); }
+}
