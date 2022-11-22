4da9c931123f5e377539f27f40683a21b70023b7
JangHyojoon <fdasfaj9@gmail.com>
2022-11-22 20:23:52 +0900

    다리 건너기 입력 유효성 검사

--- src/main/java/bridge/InputView.java
+++ src/main/java/bridge/InputView.java
@@ -22,7 +22,9 @@ public class InputView {
      * 사용자가 이동할 칸을 입력받는다.
      */
     public String readMoving() {
-        return Console.readLine();
+        String direction = Console.readLine();
+        Validator.checkMovingDirection(direction);
+        return direction;
     }
 
     /**
--- src/main/java/bridge/Validator.java
+++ src/main/java/bridge/Validator.java
@@ -6,7 +6,7 @@ public class Validator {
   private static final String RANGE_ERROR = "[ERROR] 다리 길이는 3부터 20 사이의 숫자여야 합니다.";
   private static final String NUMBER_ERROR = "[ERROR] 숫자만 입력해주세요.";
   private static final Pattern NUMBER = Pattern.compile("^([0-9])+");
-
+  private static final String DIRECTION_ERROR = "[ERROR] 방향은 U와 D만 입력해주세요.";
   public static void checkSizeRange(int size) {
     if (isUnderMin(size) || isOverMax(size)) {
       throw new IllegalArgumentException(RANGE_ERROR);
@@ -24,6 +24,18 @@ public class Validator {
   public static void isNumber(String inputString) {
     if (!NUMBER.matcher(inputString).matches()) {
       throw new IllegalArgumentException(NUMBER_ERROR);
+    }
+  }
+  private static boolean isUp(String direction) {
+    return direction.equals(BridgeDirection.UP.getDirection());
+  }
+
+  private static boolean isDown(String direction) {
+    return direction.equals(BridgeDirection.DOWN.getDirection());
+  }
+  public static void checkMovingDirection(String direction) {
+    if (!isUp(direction) && !isDown(direction)) {
+      throw new IllegalArgumentException(DIRECTION_ERROR);
     }
   }
 }
