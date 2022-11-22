2680883a1ed5fa5099f3509561b4731fa4434ed4
안광민 <creativeduck0518@gmail.com>
2022-11-22 09:48:24 +0900

    test(BridgeMaker): Mock 클래스 내부 클래스에서 외부 클래스로 변경

--- src/test/java/bridge/BridgeMakerTest.java
+++ src/test/java/bridge/BridgeMakerTest.java
@@ -1,5 +1,6 @@
 package bridge;
 
+import bridge.mock.MockBridgeNumberGenerator;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
@@ -27,18 +28,5 @@ public class BridgeMakerTest {
         }
 
         assertTrue(bridge.containsAll(expected));
-    }
-
-    class MockBridgeNumberGenerator implements BridgeNumberGenerator {
-        private final int mark;
-
-        public MockBridgeNumberGenerator(int mark) {
-            this.mark = mark;
-        }
-
-        @Override
-        public int generate() {
-            return mark;
-        }
     }
 }
