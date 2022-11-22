468e6454d393e39f6bb353b8601e84861f81315b
masiljangajji <hjk172262@gmail.com>
2022-11-22 16:28:10 +0900

    test: 올바른 다리 길이 입력을 테스트한다

--- src/test/java/userdomaintest/BridgeSizeTest.java
+++ src/test/java/userdomaintest/BridgeSizeTest.java
@@ -26,6 +26,12 @@ public class BridgeSizeTest {
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
-
+    @DisplayName("다리 길이 입력 기능을 테스트한다")
+    @ValueSource(strings = {"3", "5","6","10","16","20"})
+    @ParameterizedTest
+    void 다리_길이_입력_테스트(String bridgeSize){
+        assertThat(new UserException().checkBridgeSize(bridgeSize))
+                .isEqualTo(false);
+    }
 
 }
