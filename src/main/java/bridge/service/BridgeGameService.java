ab59fe16edd0088146c3995fefba1676fdc89adb
jhsseonn <hosu0125@gmail.com>
2022-11-22 22:42:33 +0900

    refactor: try catch문 적용 및 isNumericInput 인자 타입 변경

--- src/main/java/bridge/service/BridgeGameService.java
+++ src/main/java/bridge/service/BridgeGameService.java
@@ -2,22 +2,41 @@ package bridge.service;
 
 public class BridgeGameService {
 
+    public void isNumericInput(String input) {
+        try {
+            if (!input.matches("-?\\d+")) {
+                throw new IllegalArgumentException();
+            }
+        } catch(IllegalArgumentException e) {
+            System.out.println("[ERROR] 입력한 길이 값이 정수가 아닙니다.");
+        }
+    }
     public void isValidBridgeRange(int bridgeSize) {
-        if (bridgeSize <= 3 || bridgeSize >= 20) {
+        try {
+            if (bridgeSize <= 3 || bridgeSize >= 20) {
+                throw new IllegalArgumentException();
+            }
+        } catch(IllegalArgumentException e) {
             System.out.println("[ERROR] 다리 크기가 3~20이 아닙니다.");
-            throw new IllegalArgumentException();
         }
     }
 
     public void isValidMovingInput(String move) {
-        if (move.equals("U") || move.equals(("D"))) {
+        try {
+            if (move.equals("U") || move.equals(("D"))) {
+                throw new IllegalArgumentException();
+            }
+        } catch(IllegalArgumentException e) {
             System.out.println("[ERROR] 다리 이동엔 U 또는 D만을 입력해야합니다.");
-            throw new IllegalArgumentException();
         }
     }
 
     public void isValidCommand(String command) {
-        if (command.equals("R") || command.equals("Q")) {
+        try{
+            if (command.equals("R") || command.equals("Q")) {
+                throw new IllegalArgumentException();
+            }
+        } catch(IllegalArgumentException e) {
             System.out.println("[ERROR] 게임 재시작 여부엔 R 또는 Q만을 입력해야합니다.");
         }
     }
