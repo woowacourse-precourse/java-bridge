a247b7f72256ceb8629c6e327bfb58590de3e5e1
MinjeongKong <kong1301@ajou.ac.kr>
2022-11-22 13:52:45 +0900

    refactor: 에러처리 후 다시 입력

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -4,10 +4,6 @@ public class Application {
 
     public static void main(String[] args) {
         Controller controller = new Controller();
-        try {
-            controller.run();
-        } catch (Exception e) {
-            System.out.println("[ERROR] " + e.getMessage());
-        }
+        controller.run();
     }
 }
--- src/main/java/bridge/view/InputView.java
+++ src/main/java/bridge/view/InputView.java
@@ -13,10 +13,15 @@ public class InputView {
      * 다리의 길이를 입력받는다.
      */
     public int readBridgeSize() {
-        System.out.println("다리의 길이를 입력해주세요.");
-        String input = Console.readLine();
-        validateBridgeSize(input);
-        return Integer.parseInt(input);
+        try {
+            System.out.println("다리의 길이를 입력해주세요.");
+            String input = Console.readLine();
+            validateBridgeSize(input);
+            return Integer.parseInt(input);
+        } catch (NumberFormatException e) {
+            System.out.println("[ERROR] " + e.getMessage());
+            return readBridgeSize();
+        }
     }
 
     private void validateBridgeSize(String input) {
@@ -28,10 +33,15 @@ public class InputView {
      * 사용자가 이동할 칸을 입력받는다.
      */
     public String readMoving() {
-        System.out.println("이동할 칸을 선택해주세요. (위: U, 아래: D)");
-        String input = Console.readLine();
-        validateMoving(input);
-        return input;
+        try {
+            System.out.println("이동할 칸을 선택해주세요. (위: U, 아래: D)");
+            String input = Console.readLine();
+            validateMoving(input);
+            return input;
+        } catch (IllegalArgumentException e) {
+            System.out.println("[ERROR] " + e.getMessage());
+            return readMoving();
+        }
     }
 
     private void validateMoving(String input) {
@@ -42,10 +52,15 @@ public class InputView {
      * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
      */
     public String readGameCommand() {
-        System.out.println("게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)");
-        String input = Console.readLine();
-        validateGameCommand(input);
-        return input;
+        try {
+            System.out.println("게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)");
+            String input = Console.readLine();
+            validateGameCommand(input);
+            return input;
+        } catch (IllegalArgumentException e) {
+            System.out.println("[ERROR] " + e.getMessage());
+            return readGameCommand();
+        }
     }
 
     private void validateGameCommand(String input) {
