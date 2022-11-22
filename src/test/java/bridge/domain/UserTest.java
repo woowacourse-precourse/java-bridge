59128dd63b0e18beaca6f267ed13fc170dba34d8
JeongHunHui <deca3248@gmail.com>
2022-11-22 19:33:29 +0900

    test: User 클래스 관련 메소드 테스트 코드 작성 완료

---
+++ src/test/java/bridge/domain/UserTest.java
@@ -0,0 +1,58 @@
+package bridge.domain;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class UserTest {
+  private User user;
+
+  @BeforeEach
+  void init() {
+    user = new User();
+  }
+
+  @Test
+  @DisplayName("사용자가 이동했을때 이동횟수와 현재 index가 증가한다.")
+  void userMoveCountTest() {
+    // given
+    user.move(Direction.UP.getDirection());
+    user.move(Direction.DOWN.getDirection());
+
+    // then
+    assertEquals(2, user.getMoveCount());
+    assertEquals(1, user.getCurrentIndex());
+  }
+
+  @Test
+  @DisplayName("재시작을 하면 시도횟수가 증가하고, 이동상태가 초기화 된다.")
+  void userRetryTest() {
+    // given
+    user.move(Direction.UP.getDirection());
+
+    // when
+    user.retryGame();
+
+    // then
+    assertEquals(2, user.getTryCount());
+    assertEquals(0, user.getMoveCount());
+  }
+
+  @Test
+  @DisplayName("재시작을 하면 시도횟수가 증가하고, 이동상태가 초기화 된다.")
+  void isUserCorrectlyMoveTest() {
+    // given
+    List<String> bridgeShape = List.of("U", "D", "U");
+
+    // then
+    assertTrue(user.isCorrectlyMove(bridgeShape));
+    user.move(Direction.UP.getDirection());
+    assertTrue(user.isCorrectlyMove(bridgeShape));
+    user.move(Direction.UP.getDirection());
+    assertFalse(user.isCorrectlyMove(bridgeShape));
+  }
+}
