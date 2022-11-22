ae367a4aa2cc7c918bf2c241a4846ad4ccbe2716
recicoco068 <gkapf838@gmail.com>
2022-11-22 17:23:39 +0900

    fix(InputView.java) : 에러 발생시 재입력 받도록 수정

--- src/main/java/bridge/view/InputView.java
+++ src/main/java/bridge/view/InputView.java
@@ -15,58 +15,71 @@ public class InputView {
 
     public int readBridgeSize() {
         try {
-            System.out.println("다리의 길이를 입력해주세요.");
-            String input = Console.readLine();
+            String input = getInput("다리의 길이를 입력해주세요.");
             validateAllNumber(input);
             validateNumberRange(input);
             return Integer.parseInt(input);
-        } catch (NoSuchElementException | NumberFormatException e) {
-            throw new IllegalArgumentException(Error.NULL_INPUT.getMessage());
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+            return readBridgeSize();
         }
     }
 
     public String readMoving() {
         try {
-            System.out.println("이동할 칸을 선택해주세요. (위: U, 아래: D)");
-            String input = Console.readLine();
+            String input = getInput("이동할 칸을 선택해주세요. (위: U, 아래: D)");
             validateMoveCommand(input);
             return input;
-        } catch (NoSuchElementException e) {
-            throw new IllegalArgumentException(Error.NULL_INPUT.getMessage());
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+            return readMoving();
         }
     }
 
     public String readGameCommand() {
         try {
-            System.out.println("게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)");
-            String input = Console.readLine();
+            String input = getInput("게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)");
             validateGameCommand(input);
             return input;
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+            return readGameCommand();
+        }
+    }
+
+    private String getInput(String message) {
+        try {
+            System.out.println(message);
+            return Console.readLine();
         } catch (NoSuchElementException e) {
             throw new IllegalArgumentException(Error.NULL_INPUT.getMessage());
         }
     }
 
-    public void validateAllNumber(String input) {
+    private void validateAllNumber(String input) {
         if (!input.matches("[0-9]*")) {
             throw new IllegalArgumentException(Error.ONLY_NUMBER.getMessage());
         }
     }
 
-    public void validateNumberRange(String input) {
-        int number = Integer.parseInt(input);
-        if (number < SIZE_MIN || number > SIZE_MAX) {
-            throw new IllegalArgumentException(Error.NUMBER_RANGE_OVER.getMessage());
+    private void validateNumberRange(String input) {
+        try {
+            int number = Integer.parseInt(input);
+            if (number < SIZE_MIN || number > SIZE_MAX) {
+                throw new IllegalArgumentException(Error.NUMBER_RANGE_OVER.getMessage());
+            }
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException(Error.NULL_INPUT.getMessage());
         }
     }
 
-    public void validateMoveCommand(String input) {
+    private void validateMoveCommand(String input) {
         if (!input.equals(Command.UP.getCommand()) && !input.equals(Command.DOWN.getCommand())) {
             throw new IllegalArgumentException(Error.INVALID_COMMAND.getMessage());
         }
     }
 
-    public void validateGameCommand(String input) {
+    private void validateGameCommand(String input) {
         if (!input.equals(Command.RETRY.getCommand()) && !input.equals(Command.QUIT.getCommand())) {
             throw new IllegalArgumentException(Error.INVALID_COMMAND.getMessage());
         }
