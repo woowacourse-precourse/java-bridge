64a3007ad0cd6d68a2ff5feb32de09dcc414885b
syahn <496300@naver.com>
2022-11-22 16:39:49 +0900

    style: 중괄호 삭제

--- src/main/java/bridge/View/OutputView.java
+++ src/main/java/bridge/View/OutputView.java
@@ -19,12 +19,11 @@ public class OutputView {
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
     public void printMap(BridgeMap bridgeMap) {
-
         for (int i = 1; i >= 0; i--) {
             List<String> block = new ArrayList<>();
-            for (List<String> partition : bridgeMap.getMap()) {
+            for (List<String> partition : bridgeMap.getMap())
                 block.add(partition.get(i));
-            }
+
             String map = String.join(" | ", block);
             System.out.println(OPEN_BRACKET.getMessage() + map + CLOSED_BRACKET.getMessage());
         }
