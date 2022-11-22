300b64f45fc485e83ee94464d416bab28ca11833
wkdghdwns199 <35947667+wkdghdwns199@users.noreply.github.com>
2022-11-22 15:44:46 +0900

    Feat : 움직임의 성패를 확인하는 기능 추가

--- src/main/java/bridge/MoveStatus.java
+++ src/main/java/bridge/MoveStatus.java
@@ -37,5 +37,10 @@ public class MoveStatus {
         moveUpper.add(successResult(checkIfAvailableToMove));
         moveDowner.add("   ");
     }
-
+    private String successResult(boolean checkIfAvailableToMove) {
+        if (checkIfAvailableToMove){
+            return " O ";
+        }
+        return " X ";
+    }
 }
