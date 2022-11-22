1f8c21f52eaa8d1bc437e6c738054a31bbd68147
gmu1026 <gmu1026@gmail.com>
2022-11-22 21:45:39 +0900

    refactor: 사용하지 않는 import문 삭제

--- src/test/java/bridge/InputViewTest.java
+++ src/test/java/bridge/InputViewTest.java
@@ -3,7 +3,6 @@ package bridge;
 
 import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.in;
 
 import camp.nextstep.edu.missionutils.test.NsTest;
 import org.junit.jupiter.api.Test;
