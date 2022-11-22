256d935025f12e5f65b2b94b88316d7db66a5465
Sangwoo Shin <sangwoonoel@naver.com>
2022-11-22 15:15:52 +0900

    fix(BridgeGame): move함수 내의 주소 비교를 값 비교로 수정

--- src/main/java/bridge/BridgeGame.java
+++ src/main/java/bridge/BridgeGame.java
@@ -27,15 +27,15 @@ public class BridgeGame {
      * 이동을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
     public boolean move(int index, String moving) {
-        if (bridge.get(index) == moving) {
-            if (bridge.get(index) == UP.getMoving()) {
+        if (bridge.get(index).equals(moving)) {
+            if (bridge.get(index).equals(UP.getMoving())) {
                 results.add(UP_SUCCESS);
                 return true;
             }
             results.add(DOWN_SUCCESS);
             return true;
         }
-        if (bridge.get(index) == UP.getMoving()) {
+        if (bridge.get(index).equals(UP.getMoving())) {
             results.add(UP_FAIL);
             return false;
         }
