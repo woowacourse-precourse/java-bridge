e957a890dbf8dcc6121049730dede47b9c177853
JunTaeINC <juntea3523@naver.com>
2022-11-22 17:02:44 +0900

    fix : 기존 클래스 원상 복구

--- src/main/java/bridge/BridgeMaker.java
+++ src/main/java/bridge/BridgeMaker.java
@@ -5,6 +5,9 @@ import java.util.List;
 
 import static bridge.enums.DorU.numberConvertName;
 
+/**
+ * 다리의 길이를 입력 받아서 다리를 생성해주는 역할을 한다.
+ */
 public class BridgeMaker {
 
     private final BridgeNumberGenerator bridgeNumberGenerator;
@@ -13,6 +16,10 @@ public class BridgeMaker {
         this.bridgeNumberGenerator = bridgeNumberGenerator;
     }
 
+    /**
+     * @param size 다리의 길이
+     * @return 입력받은 길이에 해당하는 다리 모양. 위 칸이면 "U", 아래 칸이면 "D"로 표현해야 한다.
+     */
     public List<String> makeBridge(int size) {
         List<String> bridgeWay = new ArrayList<>();
         for (int stage = 0; stage < size; stage++) {
