fbce5c02172cab64651e236f8bb83476216c8317
geujeog <her4386@gmail.com>
2022-11-22 14:22:57 +0900

    Feat / 게임 실행 기능

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -1,8 +1,22 @@
 package bridge;
 
+import bridge.domain.BridgeGame;
+import bridge.view.InputView;
+import bridge.view.OutputView;
+
 public class Application {
 
+    private static InputView inputView = new InputView();
+    private static OutputView outputView = new OutputView();
+
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        outputView.printGameStart();
+
+        BridgeGame bridgeGame = new BridgeGame(inputView, outputView);
+        do {
+            bridgeGame.playGame();
+        } while (bridgeGame.retry());
+
+        bridgeGame.end();
     }
 }
--- src/main/java/bridge/domain/BridgeGame.java
+++ src/main/java/bridge/domain/BridgeGame.java
@@ -1,23 +1,76 @@
-package bridge;
+package bridge.domain;
 
-/**
- * 다리 건너기 게임을 관리하는 클래스
- */
+import bridge.service.BridgeRandomNumberGenerator;
+import bridge.service.validator.BridgeGameEnd;
+import bridge.service.validator.BridgeLength;
+import bridge.service.validator.BridgeMaker;
+import bridge.view.InputView;
+import bridge.view.OutputView;
+
 public class BridgeGame {
 
-    /**
-     * 사용자가 칸을 이동할 때 사용하는 메서드
-     * <p>
-     * 이동을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
-     */
-    public void move() {
+    private final InputView inputView;
+    private final OutputView outputView;
+
+    private Bridge bridge;
+    private boolean isEndGame = false;
+
+    private GameRound gameRound;
+    private int tryCount = 0;
+
+    public BridgeGame(InputView inputView, OutputView outputView) {
+        this.inputView = inputView;
+        this.outputView = outputView;
+
+        setBridge(setBridgeLength());
     }
 
-    /**
-     * 사용자가 게임을 다시 시도할 때 사용하는 메서드
-     * <p>
-     * 재시작을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
-     */
-    public void retry() {
+    private int setBridgeLength() {
+        BridgeLength bridgeLength = new BridgeLength(inputView);
+        return bridgeLength.inputBridgeSize();
+    }
+
+    private void setBridge(int bridgeLength) {
+        BridgeRandomNumberGenerator bridgeRandomNumberGenerator = new BridgeRandomNumberGenerator();
+        BridgeMaker bridgeMaker = new BridgeMaker(bridgeRandomNumberGenerator);
+
+        this.bridge = new Bridge(bridgeMaker.makeBridge(bridgeLength));
+    }
+
+    public void playGame() {
+        this.gameRound = new GameRound(this.inputView, this.outputView, this.bridge);
+        this.tryCount++;
+
+        while (true) {
+            if (!this.gameRound.isContinueRound()) {
+                this.isEndGame = true;
+                break;
+            }
+
+            if (!move(this.gameRound.winRound()))
+                break;
+        }
+    }
+
+    private boolean move(boolean isWin) {
+        if (!isWin)
+            return false;
+
+        return true;
+    }
+
+    public boolean retry() {
+        if (this.isEndGame)
+            return false;
+
+        BridgeGameEnd bridgeGameEnd = new BridgeGameEnd(inputView);
+        if (bridgeGameEnd.isEnd())
+            return false;
+
+        return true;
+    }
+
+    public void end() {
+        gameRound.end(this.isEndGame, this.tryCount);
     }
 }
