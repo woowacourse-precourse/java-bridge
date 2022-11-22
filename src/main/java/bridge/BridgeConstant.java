231ca57f00e050a9369c41df049685f942758052
jeongyeonKimm <wjddus9132@gmail.com>
2022-11-22 23:28:01 +0900

    feat: 재시작 여부 에러메시지 추가

--- src/main/java/bridge/BridgeConstant.java
+++ src/main/java/bridge/BridgeConstant.java
@@ -5,7 +5,8 @@ public class BridgeConstant {
     public static final int MAX_LENGTH = 20;
     public static final String DOWN = "D";
     public static final String UP = "U";
-
+    public static final String RESTART = "R";
+    public static final String QUIT = "Q";
     public static final String SUCCESS = "O";
     public static final String FAIL = "X";
     public static final String NOTHING = " ";
--- src/main/java/bridge/ExceptionConstant.java
+++ src/main/java/bridge/ExceptionConstant.java
@@ -5,7 +5,9 @@ public enum ExceptionConstant {
     INCORRECT_LENGTH(String.format("다리 길이는 %d부터 %d 사이의 숫자여야 합니다.",
                           BridgeConstant.MIN_LENGTH, BridgeConstant.MAX_LENGTH)),
     INCORRECT_MOVING_INPUT(String.format("이동할 칸은 %s 또는 %s로 입력해야 합니다.",
-            BridgeConstant.DOWN, BridgeConstant.UP));;
+            BridgeConstant.DOWN, BridgeConstant.UP)),
+    INCORRECT_GAME_COMMAND(String.format("재시작 여부 입력은 %s 또는 %s이어야 합니다.",
+            BridgeConstant.RESTART, BridgeConstant.QUIT));
 
     private final String message;
 
