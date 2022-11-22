b2c0c3bf27d923084a84bb56f412027c4d8ff5f0
Giho <poqw741@gmail.com>
2022-11-22 16:54:46 +0900

    feat(bridgeMonitor) : 위와 아래 다리 최초 괄호열기 생성자 구현

--- src/main/java/bridge/domain/BridgeMonitor.java
+++ src/main/java/bridge/domain/BridgeMonitor.java
@@ -1,4 +1,20 @@
 package bridge.domain;
 
 public class BridgeMonitor {
+    private StringBuffer upperLine;
+    private StringBuffer lowerLine;
+    private static final String START_BRACKET = "[";
+    private static final String END_BRACKET = "]";
+    private static final String CORRECT_MOVING = " O ";
+    private static final String WRONG_MOVING = " X ";
+    private static final String SPACE = "   ";
+    private static final String MID_LINE = "|";
+
+    public BridgeMonitor(){
+        this.upperLine = new StringBuffer(START_BRACKET);
+        this.lowerLine = new StringBuffer(START_BRACKET);
+    }
+
+
+
 }
