92c557306efdc360987409a2a298424726f1184d
nahyeon <whskgus12@nate.com>
2022-11-22 16:50:01 +0900

    Test(Exception): 에러 메세지 일치 여부 파악하기
    Exception 클래스에서 private로 선언된 예외처리 함수에 접근하기 위해 public 함수를 사용합니다.
    해당 함수들은 2개의 예외 처리 함수가 엮여있기 때문에 예외가 각각의 함수에서 잘 처리되는지 확인하기 위해 에러 메세지를 통해 확인하도록 변경합니다.

--- src/test/java/bridge/Exception/BridgeInputExceptionTest.java
+++ src/test/java/bridge/Exception/BridgeInputExceptionTest.java
@@ -12,51 +12,51 @@ class BridgeInputExceptionTest {
     BridgeInputException bridgeInputException = new BridgeInputException();
 
     @ParameterizedTest
-    @ValueSource(strings = {"A", "1", "끝", "", " "})
+    @ValueSource(strings = {"A", "1", "끝", " "})
     @DisplayName("재시작/종료 여부가 R 또는 Q가 아닌 다른 문자일 때 예외 처리")
     void validInputFormRetry(String retry) {
-        assertThatThrownBy(() -> bridgeInputException.validBridgeRetry(retry))
-                .isInstanceOf(IllegalArgumentException.class);
+        IllegalArgumentException e = assertThrows(IllegalArgumentException.class, () -> bridgeInputException.validBridgeRetry(retry));
+        assertEquals("[ERROR] 재시작/종료 여부는 R 또는 Q만 입력하세요", e.getMessage());
     }
 
     @ParameterizedTest
-    @ValueSource(strings = {"RQ", "RR", "R ", "Q "})
+    @ValueSource(strings = {"RQ", "RR", "RRR", "QRQ"})
     @DisplayName("재시작/종료 여부가 두글자 이상의 문자일 때 예외 처리")
     void validInputSizeRetry(String retry) {
-        assertThatThrownBy(() -> bridgeInputException.validBridgeRetry(retry))
-                .isInstanceOf(IllegalArgumentException.class);
+        IllegalArgumentException e = assertThrows(IllegalArgumentException.class, () -> bridgeInputException.validBridgeRetry(retry));
+        assertEquals("[ERROR] 재시작/종료 여부는 한 문자로 입력하세요.", e.getMessage());
     }
 
     @ParameterizedTest
     @ValueSource(strings = {"k", "five", " ", "삼"})
     @DisplayName("다리 길이가 숫자가 아닐 때 예외 처리")
     void validInputFormLength(String length) {
-        assertThatThrownBy(() -> bridgeInputException.validBridgeLength(length))
-                .isInstanceOf(IllegalArgumentException.class);
+        IllegalArgumentException e = assertThrows(IllegalArgumentException.class, () -> bridgeInputException.validBridgeLength(length));
+        assertEquals("[ERROR] 다리의 길이는 숫자만 입력하세요.", e.getMessage());
     }
 
     @ParameterizedTest
     @ValueSource(strings = {"2999", "21", "2"})
     @DisplayName("다리 길이가 3 이상 20 이하가 아니면 예외 처리")
     void validInputRangeLength(String length) {
-        assertThatThrownBy(() -> bridgeInputException.validBridgeLength(length))
-                .isInstanceOf(IllegalArgumentException.class);
+        IllegalArgumentException e = assertThrows(IllegalArgumentException.class, () -> bridgeInputException.validBridgeLength(length));
+        assertEquals("[ERROR] 다리의 길이는 3 이상 20 이하의 숫자입니다.", e.getMessage());
     }
 
     @ParameterizedTest
     @ValueSource(strings = {"앞", "u", "d", "1", " "})
     @DisplayName("사용자의 이동 방향이 U 또는 D가 아닌 문자일 때 예외 처리")
     void validInputFormMove(String move) {
-        assertThatThrownBy(() -> bridgeInputException.validMoveDirection(move))
-                .isInstanceOf(IllegalArgumentException.class);
+        IllegalArgumentException e = assertThrows(IllegalArgumentException.class, () -> bridgeInputException.validMoveDirection(move));
+        assertEquals("[ERROR] 이동할 칸은 U 또는 D만 입력하세요.", e.getMessage());
     }
 
     @ParameterizedTest
     @ValueSource(strings = {"UU", "DD", "UD", "UDU"})
     @DisplayName("사용자의 이동 방향이 두글자 이상의 문자일 때 예외처리")
     void validInputSizeMove(String move) {
-        assertThatThrownBy(() -> bridgeInputException.validMoveDirection(move))
-                .isInstanceOf(IllegalArgumentException.class);
+        IllegalArgumentException e = assertThrows(IllegalArgumentException.class, () -> bridgeInputException.validMoveDirection(move));
+        assertEquals("[ERROR] 이동할 칸은 한 문자만 입력하세요.", e.getMessage());
     }
 }
 
