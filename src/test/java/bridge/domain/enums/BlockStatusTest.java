9cddfd63158af130cb6f01cb4879aa22847949e7
HiiWee <wpdlzhf159@gmail.com>
2022-11-22 15:09:22 +0900

    test: 숫자 형식의 다리 블록 구성을 알맞은 문자열 리스트로 변경 테스트 추가

--- src/test/java/bridge/domain/enums/BlockStatusTest.java
+++ src/test/java/bridge/domain/enums/BlockStatusTest.java
@@ -11,13 +11,27 @@ import org.junit.jupiter.api.Test;
 class BlockStatusTest {
     @DisplayName("문자열 형식의 다리 블록 구성을 알맞은 enum 타입 리스트로 변경하는 테스트")
     @Test
-    void convertToBlockStatues() {
+    void convertToBlockStatues_success() {
         // given
         List<String> inputBlock = List.of("U", "U", "D", "D");
         List<BlockStatus> expected = List.of(UP, UP, DOWN, DOWN);
 
         // when
         List<BlockStatus> actual = BlockStatus.convertToBlockStatues(inputBlock);
+
+        // then
+        assertThat(actual).containsSequence(expected);
+    }
+
+    @DisplayName("숫자 형식의 다리 블록 구성을 알맞은 문자열 타입 리스트로 변경하는 테스트")
+    @Test
+    void createSymbolBlocks_success() {
+        // given
+        List<Integer> inputBlock = List.of(1, 0, 1, 0);
+        List<String> expected = List.of("U", "D", "U", "D");
+
+        // when
+        List<String> actual = BlockStatus.createSymbolBlocks(inputBlock);
 
         // then
         assertThat(actual).containsSequence(expected);
