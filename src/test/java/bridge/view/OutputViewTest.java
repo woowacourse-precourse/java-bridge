3f6f7c74c5641e1bec4a34fe908253bff5e8c9a8
ostar11 <cjw5877@gmail.com>
2022-11-22 17:36:59 +0900

    test(OutputViewTest): 성공여부테스트, 이동결과만들기테스트

--- src/test/java/bridge/view/OutputViewTest.java
+++ src/test/java/bridge/view/OutputViewTest.java
@@ -4,33 +4,34 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 import bridge.BridgeGameResult;
+import java.util.Arrays;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 class OutputViewTest {
     private OutputView outputView;
-    private BridgeGameResult bridgeGameResult;
 
     @BeforeEach
     void setOutputView() {
         outputView = new OutputView();
-        bridgeGameResult = new BridgeGameResult();
-        bridgeGameResult.addResult("U", "O");
-        bridgeGameResult.addResult("D", "O");
-        bridgeGameResult.addResult("U", "O");
+    }
+
+    @ParameterizedTest
+    @CsvSource({"true,성공", "false,실패"})
+    void 성공_여부_테스트(boolean isSuccess, String SuccessPhrase) {
+        String input = outputView.getSuccessPhrase(isSuccess);
+        assertThat(input).isEqualTo(SuccessPhrase);
     }
 
     @Test
-    void 이동_결과_출력_테스트() {
-//        String input = outputView.printMap(bridgeGameResult);
-
-//        assertThat(input).isEqualTo("[ O |   | X ]");
-//        assertThat(input).isEqualTo("[   | O ]");
-    }
-
-    @Test
-    void 최종_결과_출력_테스트() {
-
-
+    void 이동_결과_만들기_테스트() {
+        String input = outputView.makeIndex(Arrays.asList("O", " ", "O"));
+        String expected = "[ O |   | O ]";
+        String input2 = outputView.makeIndex(Arrays.asList("O", "O", " "));
+        String expected2 = "[ O | O |   ]";
+        assertThat(input).isEqualTo(expected);
+        assertThat(input2).isEqualTo(expected2);
     }
 }
