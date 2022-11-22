2919602964fad92cef49f4f269a1900b56547b4a
SEONGJOON KIM <jobcho6320@naver.com>
2022-11-22 20:45:03 +0900

    fix: 다리 길이 입력 값 에러 발생시 재시도 가능하도록 수정

--- src/main/java/bridge/view/InputView.java
+++ src/main/java/bridge/view/InputView.java
@@ -3,13 +3,29 @@ package bridge.view;
 import bridge.Validator;
 import camp.nextstep.edu.missionutils.Console;
 
+import static bridge.util.Constant.*;
+
 public class InputView {
 
     public static int readBridgeSize() {
-        String inputSize = Console.readLine();
-        Validator.validateInputBridgeSizeType(inputSize);
-        Validator.validateInputBridgeSizeRange(inputSize);
-        return Integer.parseInt(inputSize);
+        OutputView.printInputBridgeSizeMessage();
+        int result = BRIDGE_SIZE_INPUT_RETRY_POINT;
+        while (result == BRIDGE_SIZE_INPUT_RETRY_POINT) {
+            result = retryBridgeSizeInput();
+        }
+        return result;
+    }
+
+    private static int retryBridgeSizeInput() {
+        try {
+            String inputSize = Console.readLine();
+            Validator.validateInputBridgeSizeType(inputSize);
+            Validator.validateInputBridgeSizeRange(inputSize);
+            return Integer.parseInt(inputSize);
+        } catch (IllegalArgumentException error) {
+            System.out.println(error.getMessage());
+        }
+        return BRIDGE_SIZE_INPUT_RETRY_POINT;
     }
 
     public static String inputMoving() {
