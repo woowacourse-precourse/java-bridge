895127352fadbc2897ad13b48b6d29ce14fb9c4c
Devtist <yt930310@snu.ac.kr>
2022-11-22 16:00:16 +0900

    test: 주석 추가

--- src/test/java/controller/GameControllerTest.java
+++ src/test/java/controller/GameControllerTest.java
@@ -10,35 +10,33 @@ import camp.nextstep.edu.missionutils.test.NsTest;
 
 class GameControllerTest extends NsTest {
 
-	@DisplayName("정상적으로 실행되는지 검증한다.")
-	@Test
-	void controlNormal() {
-		assertRandomNumberInRangeTest(() -> {
-			run("4", "U", "D", "U", "D");
-			assertThat(output()).contains(
-				"최종 게임 결과",
-				"[ O |   | O |   ]",
-				"[   | O |   | O ]",
-				"게임 성공 여부: 성공",
-				"총 시도한 횟수: 1"
-			);
-
-			int upSideIndex = output().indexOf("[ O |   | O |   ]");
-			int downSideIndex = output().indexOf("[   | O |   | O ]");
-			assertThat(upSideIndex).isLessThan(downSideIndex);
-		}, 1, 0, 1, 0);
-	}
-
 	@DisplayName("예외를 처리할 수 있는지 검증한다.")
 	@Test
 	void controlAbnormal() {
 		assertRandomNumberInRangeTest(() -> {
+			//given, when
 			run("3", "R", "U", "D", "U");
+			//then
 			assertThat(output()).contains(
 				"[ERROR]",
 				"이동할 칸을 선택해주세요."
 			);
 		}, 1, 0, 1);
+	}
+
+	@DisplayName("정상적으로 실행되는지 검증한다.")
+	@Test
+	void controlNormal() {
+		assertRandomNumberInRangeTest(() -> {
+			//given, when
+			run("4", "U", "D", "U", "D");
+			//then
+			assertThat(output()).contains(
+				"최종 게임 결과",
+				"게임 성공 여부: 성공",
+				"총 시도한 횟수: "
+			);
+		}, 1, 0, 1, 0);
 	}
 
 	@Override
--- src/test/java/service/BridgeGameTest.java
+++ src/test/java/service/BridgeGameTest.java
@@ -22,12 +22,14 @@ class BridgeGameTest {
 	@DisplayName("사용자의 입력 모음이 초기화 되는지 확인한다.")
 	@Test
 	void initMovingStack() {
+		//given
 		BridgeGame bridgeGame = new BridgeGame();
 		bridgeGame.initMovingStack();
 		bridgeGame.move(new MovingRequestDto("U"));
 		bridgeGame.move(new MovingRequestDto("D"));
 		bridgeGame.initMovingStack();
 		bridgeGame.move(new MovingRequestDto("U"));
+		//when, then
 		assertThat(
 			bridgeGame.produceResult(new BridgeResponseDto(new Bridge(List.of("U", "D", "U")))).getMap()).isEqualTo(
 			new MoveResultResponseDto(new MoveResult(List.of("U", "D", "D"), List.of("U"))).getMap());
@@ -36,17 +38,21 @@ class BridgeGameTest {
 	@DisplayName("다리가 정상적으로 생성되는지 확인한다.")
 	@Test
 	void initBridge() {
+		//given
 		BridgeGame bridgeGame = new BridgeGame();
+		//when, then
 		assertEquals(3, bridgeGame.initBridge(new BridgeSizeRequestDto(3)).getBridge().size());
 	}
 
 	@DisplayName("사용자의 입력이 정상적으로 추가되는지 확인한다.")
 	@Test
 	void move() {
+		//given
 		BridgeGame bridgeGame = new BridgeGame();
 		bridgeGame.initMovingStack();
 		bridgeGame.move(new MovingRequestDto("U"));
 		bridgeGame.move(new MovingRequestDto("D"));
+		//when, then
 		assertThat(
 			bridgeGame.produceResult(new BridgeResponseDto(new Bridge(List.of("U", "D", "U")))).getMap()).isEqualTo(
 			new MoveResultResponseDto(new MoveResult(List.of("U", "D", "D"), List.of("U", "D"))).getMap());
@@ -55,9 +61,12 @@ class BridgeGameTest {
 	@DisplayName("사용자의 입력에 대해 제대로 결과를 출력하는지 확인한다.")
 	@Test
 	void produceResult() {
+		//given
 		BridgeGame bridgeGame = new BridgeGame();
 		bridgeGame.initMovingStack();
+		//when
 		bridgeGame.move(new MovingRequestDto("U"));
+		//then
 		assertEquals(1,
 			bridgeGame.produceResult(new BridgeResponseDto(new Bridge(List.of("U", "D", "U")))).getNumberOfCorrect());
 	}
@@ -65,8 +74,10 @@ class BridgeGameTest {
 	@DisplayName("게임이 끝났을 때 결과를 제대로 반환하는지 확인한다.")
 	@Test
 	void checkGameIsEnd() {
+		//given
 		BridgeGame bridgeGame = new BridgeGame();
 		bridgeGame.initMovingStack();
+		//when, then
 		assertThat(bridgeGame.checkGameIsEnd(
 				new MoveResultResponseDto(new MoveResult(List.of("U", "D", "U"), List.of("U", "D", "U"))))
 			.getMap()).isEqualTo("[ O |   | O ]\n[   | O |   ]");
@@ -75,14 +86,18 @@ class BridgeGameTest {
 	@DisplayName("총 게임 횟수를 정상적으로 반환하는지 확인한다.")
 	@Test
 	void makePlayCountDto() {
+		//given
 		BridgeGame bridgeGame = new BridgeGame();
+		//when, then
 		assertThat(bridgeGame.makePlayCountDto().getPlayCount()).isEqualTo(PlayCount.getInstance().getCount());
 	}
 
 	@DisplayName("재시작 여부를 정상적으로 반환하는지 확인한다.")
 	@Test
 	void retry() {
+		//given
 		BridgeGame bridgeGame = new BridgeGame();
+		//when, then
 		assertTrue(bridgeGame.retry(new GameCommandRequestDto("R")));
 	}
 }
