38f856991f3639b51be60eba60ebb2b1ed1c9a3a
kim <kajinori@gachon.ac.kr>
2022-11-22 13:04:04 +0900

    refactor(InputView): 메서드 static 변경

--- src/main/java/bridge/InputView.java
+++ src/main/java/bridge/InputView.java
@@ -3,21 +3,21 @@ package bridge;
 import camp.nextstep.edu.missionutils.Console;
 
 public class InputView {
-    public String readBridgeSize() {
+    public static String readBridgeSize() {
         System.out.println("다리의 길이를 입력해주세요.");
-        String inputBridgeSize = Console.readLine();
-        return inputBridgeSize;
+        Console.readLine();
+        return Console.readLine();
     }
 
-    public String readMoving() {
+    public static String readMoving() {
         System.out.println("이동할 칸을 선택해주세요. (위: U, 아래: D)");
-        String inputUpDown = Console.readLine();
-        return inputUpDown;
+        Console.readLine();
+        return Console.readLine();
     }
 
-    public String readGameCommand() {
+    public static String readGameCommand() {
         System.out.println("게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)");
-        String inputRQ = Console.readLine();
-        return inputRQ;
+        Console.readLine();
+        return Console.readLine();
     }
 }
