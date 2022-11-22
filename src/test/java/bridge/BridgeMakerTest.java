cd9b72e664bc602e27395f862aef69b02b17aa97
swonny <wonny921@gmail.com>
2022-11-22 20:09:41 +0900

    test(BridgeMakerTest): 다리 생성 테스트코드 추가

--- src/test/java/bridge/BridgeMakerTest.java
+++ src/test/java/bridge/BridgeMakerTest.java
@@ -1,40 +1,28 @@
 package bridge;
 
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 public class BridgeMakerTest {
+    @ParameterizedTest
+    @ValueSource(ints = {3, 10, 20})
     @DisplayName("다리 생성 테스트")
-    @Test
-    void 생성한_다리_길이_테스트() {
+    void 생성한_다리_길이_테스트(int bridgeSize) {
         BridgeMaker bridgeMaker = new BridgeMaker(new BridgeRandomNumberGenerator());
-        assertThat(bridgeMaker.makeBridge(3).size())
-                .isEqualTo(3);
+        assertThat(bridgeMaker.makeBridge(bridgeSize).size())
+                .isEqualTo(bridgeSize);
     }
 
-    @DisplayName("다리 이용 가능한 칸 포함 테스트")
-    @Test
-    void 다리_위치_포함_테스트() {
+    @ParameterizedTest
+    @ValueSource(ints = {3, 10, 20})
+    @DisplayName("U 또는 D 포함 테스트")
+    void 다리_저장_포맷_테스트(int bridgeSize) {
         BridgeMaker bridgeMaker = new BridgeMaker(new BridgeRandomNumberGenerator());
-        assertThat(bridgeMaker.makeBridge(3).toString())
-                .contains("D", "U");
+        assertThat(bridgeMaker.makeBridge(bridgeSize))
+                .toString()
+                .matches("UD");
     }
-
-//    @DisplayName("0과 1에 U또는 D가 매핑되는 것 확인")
-//    @Test
-//    void DOWN_매핑_테스트() {
-//        BridgeMaker bridgeMaker = new BridgeMaker(new BridgeRandomNumberGenerator());
-//        assertThat(bridgeMaker.getMovableSide(0))
-//                .isEqualTo("D");
-//    }
-
-//    @DisplayName("0과 1에 U또는 D가 매핑되는 것 확인")
-//    @Test
-//    void UP_매핑_테스트() {
-//        BridgeMaker bridgeMaker = new BridgeMaker(new BridgeRandomNumberGenerator());
-//        assertThat(bridgeMaker.getMovableSide(1))
-//                .isEqualTo("U");
-//    }
 }
