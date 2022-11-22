5c2d0384f796a97df519b13dfa8b756b52a6c9dd
eom-tae-in <eti0728@daum.net>
2022-11-22 21:15:50 +0900

    feat(BridgeGame): 유저가 다리 끝까지 도착했는지 확인하는 메서드 구현

--- src/main/java/bridge/domain/BridgeGame.java
+++ src/main/java/bridge/domain/BridgeGame.java
@@ -3,17 +3,23 @@ package bridge.domain;
 import java.util.List;
 
 public class BridgeGame {
-    private int position;
+    private final int position;
     private final List<String> bridge;
 
     public BridgeGame(List<String> bridge) {
         this.position = 0;
         this.bridge = bridge;
     }
+
     public List<String> getBridge() {
         return this.bridge;
     }
+
     public int getPosition() {
         return this.position;
+    }
+
+    public boolean comparePositionLength() {
+        return this.bridge.size() == this.position;
     }
 }
