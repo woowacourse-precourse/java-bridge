0a95a1f6b1c3f8ac581849be2f9b8d77ea3e4ddd
jaehyeon <jhjh0022@naver.com>
2022-11-22 20:19:01 +0900

    rename: 클래스 이름 맞춤법 수정

--- src/main/java/bridge/view/Valification.java
+++ src/main/java/bridge/view/Valification.java
@@ -7,14 +7,14 @@ import java.util.Objects;
 
 public class Valification {
 
-    private final int MIN_NUMBER = 3;
-    private final int MAX_NUMBER = 20;
-
     private final String ERROR_MESSAGE_SIZE = "다리 길이는 3부터 20 사이의 숫자여야 합니다.";
     private final String ERROR_MOVE_UD = "U, D 두개 중에 하나 선택 해야 합니다.";
     private final String ERROR_RETRY_RQ = "R, Q 두개 중에 하나 선택 해야 합니다.";
 
     public void verifyBridgeSize(int size) {
+        final int MIN_NUMBER = 3;
+        final int MAX_NUMBER = 20;
+
         if (!(MIN_NUMBER <= size && size <= MAX_NUMBER)) {
             throw new IllegalArgumentException(ERROR_MESSAGE_SIZE);
         }
