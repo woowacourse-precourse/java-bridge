ebe80c25624aa146e6f27076f0416239dadbb920
yujeongkwon <yoojeongkwon@naver.com>
2022-11-22 23:16:54 +0900

    Implement(Application): 게임 구현

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -1,8 +1,56 @@
 package bridge;
 
+import bridge.controller.BridgeGame;
+import bridge.view.InputView;
+import bridge.view.OutputView;
+
 public class Application {
+    private static final OutputView outputView = new OutputView();
+    private static final InputView inputView = new InputView();
+    private static final BridgeGame bridgeGame = new BridgeGame();
 
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        startGame();
+        boolean moveSuccess;
+        do {
+            moveSuccess = isSuccessMoving();
+            outputView.printMap(bridgeGame.getUser());
+        } while (retryStatus(moveSuccess));
+
+        outputView.printResult(moveSuccess,bridgeGame.getUser());
+    }
+
+    private static void startGame(){
+        while(true){
+            try {
+                bridgeGame.initGame(inputView.readBridgeSize());
+                return;
+            }catch (IllegalArgumentException i){
+                System.out.println(i.getMessage());
+            }
+        }
+    }
+
+    private static boolean isSuccessMoving(){
+        while(true){
+            try {
+                return bridgeGame.move(inputView.readMoving());
+            }catch (IllegalArgumentException i){
+                System.out.println(i.getMessage());
+            }
+        }
+    }
+
+    private static boolean retryStatus(boolean moveSuccess){
+        while(true){
+            try {
+                if(moveSuccess)
+                    return bridgeGame.gameRetryStatus();
+                if(!moveSuccess)
+                    return bridgeGame.retry(inputView.readGameCommand());
+            }catch (IllegalArgumentException i){
+                System.out.println(i.getMessage());
+            }
+        }
     }
 }
