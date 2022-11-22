5290ebac46155095b6171557b5f2c012819afd50
kite707 <kite707707@gmail.com>
2022-11-22 16:17:02 +0900

    fix(Validator): 비교 연산자를 equals로 수정

--- src/main/java/bridge/Validator.java
+++ src/main/java/bridge/Validator.java
@@ -22,6 +22,6 @@ public class Validator {
     }
 
     public boolean readMovingValidator(String input) {
-        return input == "U" || input == "D";
+        return input.equals("U") || input.equals("D");
     }
 }
