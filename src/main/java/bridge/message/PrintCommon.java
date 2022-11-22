84da6d8313b1635bda0d4ca2220a4c45712d8e57
HyeonjungYun <guswnd9971@gmail.com>
2022-11-22 20:53:56 +0900

    "refactor(PrintCommon): enum클래스에 문자열 상수 하나 추가"

--- src/main/java/bridge/message/PrintCommon.java
+++ src/main/java/bridge/message/PrintCommon.java
@@ -6,7 +6,8 @@ public enum PrintCommon {
     INPUT_SIZE("\n다리의 길이를 입력해주세요."),
     INPUT_MOVING("\n이동할 칸을 선택해주세요. (위: U, 아래: D)"),
     INPUT_RETRY("\n게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)"),
-    RESULT("\n게임 성공 여부: "),
+    RESULT("\n최종 게임 결과"),
+    RESULT_CLEAR("\n게임 성공 여부: "),
     RESULT_NUMBER("총 시도한 횟수: ");
 
     private final String message;
