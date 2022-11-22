1c1f8806813d58523d5c068b2f5a0e156aec3971
jmjmjames <chungjm0711@gmail.com>
2022-11-22 12:01:00 +0900

    feat: BridgeGameStatus 게임 진행 여부 클래스 구현

---
+++ src/main/java/bridge/domain/BridgeGameStatus.java
@@ -0,0 +1,16 @@
+package bridge.domain;
+
+public enum BridgeGameStatus {
+    PLAY("R"),
+    QUIT("Q");
+
+    private final String command;
+
+    BridgeGameStatus(String command) {
+        this.command = command;
+    }
+
+    public static BridgeGameStatus gameStart() {
+        return PLAY;
+    }
+}
