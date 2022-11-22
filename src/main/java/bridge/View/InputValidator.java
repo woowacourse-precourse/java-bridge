13691f8ec6eb93bea4fa0027be1456239f7363d8
skck0226 <skck0226@ajou.ac.kr>
2022-11-22 14:03:48 +0900

    [refactor] 상수 final 변수 or enum으로 변경

--- src/main/java/bridge/View/InputValidator.java
+++ src/main/java/bridge/View/InputValidator.java
@@ -1,6 +1,7 @@
 package bridge.View;
 
 import static bridge.Enum.Error.*;
+import static bridge.Enum.Regex.*;
 
 public class InputValidator {
     public static void validateBridgeSize(String bridgeSizeInput) {
@@ -9,26 +10,30 @@ public class InputValidator {
     }
 
     private static void validateBridgeSizeRange(String bridgeSizeInput) {
+        final int MINIMAL_BRIDGE_SIZE = 3;
+        final int MAXIMAL_BRIDGE_SIZE = 3;
+
         int bridgeSize = Integer.parseInt(bridgeSizeInput);
-        if (bridgeSize < 3 || bridgeSize > 20) {
+
+        if (bridgeSize < MINIMAL_BRIDGE_SIZE || bridgeSize > MAXIMAL_BRIDGE_SIZE) {
             throw new IllegalArgumentException(BRIDGE_SIZE_RANGE_ERROR.getMessage());
         }
     }
 
     private static void validateBridgeSizeFormat(String bridgeSizeInput) {
-        if(!bridgeSizeInput.matches("\\d+")){
+        if(!bridgeSizeInput.matches(BRIDGE_SIZE_ONLY_NUMBER.getRegex())){
             throw new IllegalArgumentException(BRIDGE_SIZE_FORMAT_ERROR.getMessage());
         }
     }
 
     public static void validateMovingDirection(String movingDirection) {
-        if(!movingDirection.matches("[UD]")){
+        if(!movingDirection.matches(MOVING_DIRECTION_ONLY_UD.getRegex())){
             throw new IllegalArgumentException(MOVING_DIRECTION_ERROR.getMessage());
         }
     }
 
     public static void validateGameCommand(String gameCommand) {
-        if (!gameCommand.matches("[QR]")) {
+        if (!gameCommand.matches(GAME_COMMAND_ONLY_RQ.getRegex())) {
             throw new IllegalArgumentException(GAME_COMMAND_ERROR.getMessage());
         }
     }
