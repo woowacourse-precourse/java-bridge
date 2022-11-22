6ef8fa25827c2d3c3f30dd2febfee9c8208e8238
BHC-Chicken <tij05078@gmail.com>
2022-11-22 10:07:02 +0900

    feat : 다리 길이 입력
    유저로 부터 다리 길이를 입력받고 길이만큼 0과 1로 초기화 시킨다.

---
+++ src/main/java/bridge/Progress.java
@@ -0,0 +1,23 @@
+package bridge;
+
+public class Progress {
+
+    private static final InputView inputView = new InputView();
+    private static final OutputView outputView = new OutputView();
+    private static BridgeGame bridgeGame;
+    private static boolean loop = true;
+    
+    public void makeBridge() {
+        while (true) {
+            try {
+                bridgeGame = new BridgeGame(inputView.readBridgeSize());
+                System.out.println(" ");
+                break;
+            } catch (IllegalArgumentException e) {
+                System.out.println(outputView.errorMessage(e.getMessage()));
+            }
+        }
+    }
+
+
+}
