6cd9ebe218c50d05423853762cc4f545e694d716
rjsah09 <83952174+rjsah09@users.noreply.github.com>
2022-11-22 22:06:05 +0900

    test: Output 클래스 테스트 수정

--- src/test/java/bridge/OutputViewTest.java
+++ src/test/java/bridge/OutputViewTest.java
@@ -17,9 +17,9 @@ public class OutputViewTest {
     @Test
     void 다리_라인_출력_정상_테스트() {
         List<String> bridge = Arrays.asList("U", "U", "D", "U", "D", "U", "U");
-        List<String> crossed = Arrays.asList("U", "U", "D", "U", "D", "D");
+        List<String> crossed = Arrays.asList("U", "U", "U", "U", "D", "D");
         String lineName = "U";
-        String correctResult = "[ o | o |   | o |   | x ]";
+        String correctResult = "[ O | O | X | O |   |   ]";
 
         String result = outputView.getCrossedLine(lineName, bridge, crossed);
 
@@ -30,7 +30,7 @@ public class OutputViewTest {
     void 다리_출력_정상_테스트() {
         List<String> bridge = Arrays.asList("U", "U", "D", "U", "D", "U", "U");
         List<String> crossed = Arrays.asList("U", "U", "D", "U", "D", "D");
-        String correctResult = "[ o | o |   | o |   | x ]\n[   |   | o |   | o |   ]";
+        String correctResult = "[ O | O |   | O |   |   ]\n[   |   | O |   | O | X ]";
 
         String result = outputView.getMap(bridge, crossed);
 
