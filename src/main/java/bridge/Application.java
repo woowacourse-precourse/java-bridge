637adb1ca3b5dde5992d36ebe7f3728193446cb5
brotherstone97 <brotherstone97@gmail.com>
2022-11-22 19:09:10 +0900

    feat(InputValidator): 예외별 에러 메시지

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -5,7 +5,12 @@ import bridge.controller.BridgeGameController;
 public class Application {
 
     public static void main(String[] args) {
-        BridgeGameController bridgeGameController = new BridgeGameController();
-        bridgeGameController.run();
+        try {
+            BridgeGameController bridgeGameController = new BridgeGameController();
+            bridgeGameController.run();
+        }catch(IllegalArgumentException e){
+            System.out.println(e.getMessage());
+        }
+
     }
 }
--- src/main/java/bridge/controller/InputValidator.java
+++ src/main/java/bridge/controller/InputValidator.java
@@ -1,13 +1,17 @@
 package bridge.controller;
 
 public class InputValidator {
-    public int validatePureNumber(String input) throws NumberFormatException {
-        return Integer.parseInt(input);
+    public int validatePureNumber(String input){
+        try {
+            return Integer.parseInt(input);
+        }catch (NumberFormatException e) {
+            throw new IllegalArgumentException("[ERROR] 다리 길이는 숫자만 입력 가능합니다.");
+        }
     }
 
     public void validateBridgeSizeRange(int size) {
         if (size < 3 || size > 20) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException("[ERROR] 다리 길이는 3부터 20 사이의 숫자여야 합니다.");
         }
     }
 
@@ -15,13 +19,13 @@ public class InputValidator {
         if (input.equals("U") || input.equals("D")) {
             return input;
         }
-        throw new IllegalArgumentException();
+        throw new IllegalArgumentException("[ERROR] 이동할 칸 입력은 대문자 U또는 대문자 D만 가능합니다.");
     }
 
     public String validateRestart(String input) {
         if (input.equals("R") || input.equals("Q")) {
             return input;
         }
-        throw new IllegalArgumentException();
+        throw new IllegalArgumentException("[ERROR] 재시작 여부 입력은 대문자 R또는 대문자 Q만 가능합니다.");
     }
 }
