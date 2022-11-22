4a7426421964224717ca0c465a7f26e05610f80f
nakalic <nakalic28@gmail.com>
2022-11-22 16:37:55 +0900

    test(BridgeGameTest): 다리 건너기, 게임 재시도를 검증하는 테스트 코드 추가

---
+++ src/test/java/bridge/service/BridgeGameTest.java
@@ -0,0 +1,50 @@
+package bridge.service;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.util.Lists.newArrayList;
+import static org.junit.jupiter.api.Assertions.*;
+
+import bridge.domain.Bridge;
+import bridge.domain.GameStatus;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class BridgeGameTest {
+
+    Bridge bridge;
+    BridgeGame bridgeGame;
+    GameStatus gameStatus;
+
+    @BeforeEach
+    public void setting() {
+        bridge = new Bridge(newArrayList("U", "D", "D"));
+        bridgeGame = new BridgeGame();
+        gameStatus = new GameStatus();
+    }
+
+    @DisplayName("다리 건너기에 성공했다면 게임 상태에 O와 공백 한 칸을 추가하고 false를 반환한다.")
+    @Test
+    public void failToCrossTheBridge() {
+        assertThat(bridgeGame.move(gameStatus, bridge, "U")).isTrue();
+        assertThat(gameStatus.topBridge.get(0)).isEqualTo("O");
+        assertThat(gameStatus.bottomBridge.get(0)).isEqualTo(" ");
+    }
+
+    @DisplayName("다리 건너기에 실패했다면 게임 상태에 X와 공백 한 칸을 추가하고 false를 반환한다.")
+    @Test
+    public void SucceedToCrossTheBridge() {
+        assertThat(bridgeGame.move(gameStatus, bridge, "D")).isFalse();
+        assertThat(gameStatus.topBridge.get(0)).isEqualTo(" ");
+        assertThat(gameStatus.bottomBridge.get(0)).isEqualTo("X");
+    }
+
+    @DisplayName("재시도 하면 게임 상태는 초기화 되고, tryCount는 1 증가한다.")
+    @Test
+    public void retryToCrossTheBride() {
+        assertThat(gameStatus.tryCount).isEqualTo(1);
+        gameStatus.resetGameStatus();
+        assertThat(gameStatus.tryCount).isEqualTo(2);
+        assertThat(gameStatus.topBridge.size()).isEqualTo(0);
+    }
+}
