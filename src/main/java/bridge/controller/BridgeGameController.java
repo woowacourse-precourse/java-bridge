a2873b9230822b6768cb7340ac15f89782f9ea49
core <jinjs6@gmail.com>
2022-11-22 20:44:23 +0900

    refactor(bridge): 기능 요구 사항(메서드 10라인)에 맞추기 위해서 BridgeGameController 코드 리펙토링

--- src/main/java/bridge/controller/BridgeGameController.java
+++ src/main/java/bridge/controller/BridgeGameController.java
@@ -19,20 +19,23 @@ public class BridgeGameController {
     private static int numberOfGamePlay = 1;
 
     public static void execute() {
+        beforeGameStart();
+        BridgeStatusSaver bridgeStatusSaver = new BridgeStatusSaver(new ArrayList<>(), bridgeUpDown);
+        BridgeGame bridgeGame = new BridgeGame(bridgeStatusSaver, numberOfGamePlay);
+        executeBridgeGame(bridgeGame);
+    }
+
+    public static void beforeGameStart() {
         OutputView.printGameStartMessage();
         bridgeLength = InputView.readBridgeSize();
         InputView.validateBridgeLength(bridgeLength);
         bridgeLengthInt = Integer.parseInt(bridgeLength);
         InputView.validateBridgeLengthRange(bridgeLengthInt);
-        executeBridgeGame();
+        bridgeUpDown = BridgeUpDownGenerator.generateUpDownInformation(bridgeLengthInt);
     }
 
-    public static void executeBridgeGame() {
-        //정보를 가진 다리를 생성하기
-        bridgeUpDown = BridgeUpDownGenerator.generateUpDownInformation(bridgeLengthInt);
-        //다리 정보 저장할 클래스 생성
-        BridgeStatusSaver bridgeStatusSaver = new BridgeStatusSaver(new ArrayList<>(), bridgeUpDown);
-        BridgeGame bridgeGame = new BridgeGame(bridgeStatusSaver, numberOfGamePlay);
+    public static void executeBridgeGame(BridgeGame bridgeGame) {
+
     }
 }
 
