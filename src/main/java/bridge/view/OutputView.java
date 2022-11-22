db637899d7d3c8520b787c719f6a8f1c296710fe
DDing77 <devseo2021@gmail.com>
2022-11-22 20:41:07 +0900

    feat(view) : 최종 결과 출력 메소드

--- src/main/java/bridge/view/OutputView.java
+++ src/main/java/bridge/view/OutputView.java
@@ -2,6 +2,7 @@ package bridge.view;
 
 import bridge.constant.command.Command;
 import bridge.constant.message.GameMessage;
+import bridge.service.BridgeGameService;
 
 import java.util.List;
 
@@ -9,6 +10,10 @@ import java.util.List;
  * 사용자에게 게임 진행 상황과 결과를 출력하는 역할을 한다.
  */
 public class OutputView {
+
+    public static void printStart() {
+        System.out.println(GameMessage.GAME_START.getMessage());
+    }
 
     /**
      * 현재까지 이동한 다리의 상태를 정해진 형식에 맞춰 출력한다.
@@ -54,10 +59,12 @@ public class OutputView {
      * <p>
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void printResult() {
+    public static void printResult(BridgeGameService bridgeGameService) {
+        System.out.println(GameMessage.GAME_RESULT_FIRST.getMessage());
+        printMap(bridgeGameService.getBridge(), bridgeGameService.getSelections());
+        System.out.println();
+        System.out.println(GameMessage.GAME_RESULT_SECOND.getMessage() + bridgeGameService.gameStatus());
+        System.out.println(GameMessage.GAME_RESULT_THIRD.getMessage() + bridgeGameService.getTryCnt());
     }
 
-    public static void printStart() {
-        System.out.println(GameMessage.GAME_START.getMessage());
-    }
 }
