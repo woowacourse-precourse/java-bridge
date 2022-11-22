03ff91c9013d07b849af1cf218ac68e601c28fe7
saehim <saeilee.vxv@gmail.com>
2022-11-22 20:20:50 +0900

    feat (controller) : 게임, 라운드 모듈화
    - 게임 시작 init
    - 라운드 모듈
    - 게임 종료 모듈

--- src/main/java/bridge/controller/BridgeGameController.java
+++ src/main/java/bridge/controller/BridgeGameController.java
@@ -22,4 +22,65 @@ public class BridgeGameController {
         this.outputView = new OutputView();
         this.failFlag = true;
     }
+
+    public static void setFailFlag(boolean failFlag) {
+        BridgeGameController.failFlag = failFlag;
+    }
+
+    /**
+     * start game
+     * */
+    public void startGame() {
+        outputView.printStart();
+        BridgeGame bg = newGame();
+        playRound(bg);
+        outputView.printResult(failFlag, bg.getTryCount());
+    }
+
+    private BridgeGame newGame() {
+        BridgeMaker bm = new BridgeMaker(new BridgeRandomNumberGenerator());
+        List<String> bridge = bm.makeBridge();
+        return new BridgeGame(bridge);
+    }
+
+    public static void playRound(BridgeGame bg) {
+        setFailFlag(true);
+        List<String> moves = new ArrayList<>();
+        List<String> bridge = bg.getBridge();
+
+        do {
+            moves = bg.move(InputView.readMoving());
+            printMap(moves, bridge);
+            isFail();
+        } while (failFlag && isBridgeEnd(moves, bridge));
+
+        askRetry(bg);
+    }
+
+    private static boolean isBridgeEnd(List<String> moves, List<String> bridge) {
+        return moves.size() != bridge.size();
+    }
+
+    private static void askRetry(BridgeGame bg) {
+        if (!failFlag) {
+            tryAgain(bg);
+        }
+    }
+
+    private static void tryAgain(BridgeGame bg) {
+        String command = InputView.readGameCommand();
+        if (command.equals("R")) {
+            bg.retry();
+            playRound(bg);
+        }
+    }
+
+    private static boolean isFail() {
+        String bridge = OutputView.getUpDown();
+        if (bridge.contains("X")) {
+            BridgeGameController.failFlag = false;
+        }
+        return failFlag;
+    }
+
 }
