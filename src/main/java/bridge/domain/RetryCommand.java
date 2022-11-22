ca660a039e15be173169fbcc830593f0b791fb61
yuntaeLee <leeyt1201@gmail.com>
2022-11-22 13:42:01 +0900

    feat: 재시도 커멘드 기능 구현

--- src/main/java/bridge/domain/RetryCommand.java
+++ src/main/java/bridge/domain/RetryCommand.java
@@ -2,18 +2,18 @@ package bridge.domain;
 
 import java.util.Arrays;
 
-public enum BridgeGameRetryCommand {
+public enum RetryCommand {
     RETRY("R"),
     QUIT("Q");
 
     private final String command;
 
-    BridgeGameRetryCommand(String command) {
+    RetryCommand(String command) {
         this.command = command;
     }
 
-    public static BridgeGameRetryCommand getRetryCommandWithCommand(String command) {
-        return Arrays.stream(BridgeGameRetryCommand.values())
+    public static RetryCommand getRetryCommandWithCommand(String command) {
+        return Arrays.stream(RetryCommand.values())
                 .filter(retryCommand -> retryCommand.command.equals(command))
                 .findAny()
                 .orElseThrow(() -> new IllegalArgumentException(""));
