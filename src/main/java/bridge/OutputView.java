e5f7ea7238165f935898df29ec5a07ad347e8477
fnzl54 <rnjscksdud1@gmail.com>
2022-11-22 21:12:34 +0900

    style: change line

--- src/main/java/bridge/OutputView.java
+++ src/main/java/bridge/OutputView.java
@@ -13,7 +13,6 @@ public class OutputView {
     private static final String BRIDGE_DIVISION = " | ";
     private static final String ERROR_MESSAGE = "[ERROR]";
     private static final String COUNT_MESSAGE = "총 시도한 횟수: ";
-
     private static final String SUCCESS_FAIL_MESSAGE = "게임 성공 여부: ";
     private static final String SUCCESS_MESSAGE = "성공";
     private static final String FAIL_MESSAGE = "실패";
@@ -25,6 +24,7 @@ public class OutputView {
     private static final String RETRY_MESSAGE = "게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)";
     private static final String MOVE_MESSAGE = "이동할 칸을 선택해주세요. (위: U, 아래: D)";
     private static final String END_MESSAGE = "최종 게임 결과";
+
     /**
      * 현재까지 이동한 다리의 상태를 정해진 형식에 맞춰 출력한다.
      * <p>
@@ -79,6 +79,7 @@ public class OutputView {
     public static void getMoveMessage() {
         System.out.println(MOVE_MESSAGE);
     }
+
     public static void getEndMessage() {
         System.out.println(END_MESSAGE);
     }
