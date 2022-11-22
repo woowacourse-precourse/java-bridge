47777b16bf509a8950a379fd628c0e6a07e65cde
DESKTOP-1ANNTGI\USER <cvc100326@naver.com>
2022-11-22 23:17:56 +0900

    test: 초기 준비단계 필드로 변경

--- src/test/java/bridge/domain/BridgeGameTest.java
+++ src/test/java/bridge/domain/BridgeGameTest.java
@@ -1,20 +1,21 @@
 package bridge.domain;
 
-import bridge.domain.BridgeGame;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
-
-import java.util.List;
-
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
+import java.util.List;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
 public class BridgeGameTest {
+
+    Bridge bridge = new Bridge(List.of("U", "D", "D"));
+    BridgeGame bridgeGame = new BridgeGame(bridge);
+
     @DisplayName("올바른 칸 이동")
     @Test
     void move() {
-        Bridge bridge = new Bridge(List.of("U", "D", "D"));
-        BridgeGame bridgeGame = new BridgeGame(bridge);
         assertThat(bridgeGame.move("U")).isEqualTo(true);
         assertThat(bridgeGame.move("D")).isEqualTo(true);
         assertThat(bridgeGame.move("U")).isEqualTo(false);
@@ -23,8 +24,6 @@ public class BridgeGameTest {
     @DisplayName("이동시 잘못된 입력값인 경우")
     @Test
     void moveByInvaildInput() {
-        Bridge bridge = new Bridge(List.of("U", "D", "D"));
-        BridgeGame bridgeGame = new BridgeGame(bridge);
         assertThatThrownBy(
                 () -> bridgeGame.move("u")).isInstanceOf(IllegalArgumentException.class);
     }
@@ -32,24 +31,18 @@ public class BridgeGameTest {
     @DisplayName("올바른 재시작 명령어")
     @Test
     void retryByCommand() {
-        Bridge bridge = new Bridge(List.of("U", "D", "D"));
-        BridgeGame bridgeGame = new BridgeGame(bridge);
         assertThat(bridgeGame.retry("R")).isEqualTo(true);
     }
 
     @DisplayName("올바른 종료 명령어")
     @Test
     void quitByCommand() {
-        Bridge bridge = new Bridge(List.of("U", "D", "D"));
-        BridgeGame bridgeGame = new BridgeGame(bridge);
         assertThat(bridgeGame.retry("Q")).isEqualTo(false);
     }
 
     @DisplayName("재시작,종료시 잘못된 명령어 입력")
     @Test
     void commandByInvaildInput() {
-        Bridge bridge = new Bridge(List.of("U", "D", "D"));
-        BridgeGame bridgeGame = new BridgeGame(bridge);
         assertThatThrownBy(
                 () -> bridgeGame.retry("q")).isInstanceOf(IllegalArgumentException.class);
     }
@@ -57,8 +50,6 @@ public class BridgeGameTest {
     @DisplayName("승리 확인")
     @Test
     void checkwin() {
-        Bridge bridge = new Bridge(List.of("U", "D", "D"));
-        BridgeGame bridgeGame = new BridgeGame(bridge);
         bridgeGame.move("U");
         bridgeGame.move("D");
         bridgeGame.move("D");
