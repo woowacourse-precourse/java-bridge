8a40cccefd2b3d34dd237a33e4dd97e81e7b049d
park <pjh971212@gmail.com>
2022-11-22 15:10:22 +0900

    feat : 다리의 길이 입력 및 생성

--- src/main/java/bridge/controller/BridgeController.java
+++ src/main/java/bridge/controller/BridgeController.java
@@ -1,14 +1,24 @@
 package bridge.controller;
 
+import bridge.BridgeMaker;
+import bridge.BridgeRandomNumberGenerator;
 import bridge.view.InputView;
 import bridge.view.OutputView;
+import java.util.List;
 
 public class BridgeController {
-    private static InputView inputView = new InputView();
-    private static OutputView outputView = new OutputView();
+    private static final InputView inputView = new InputView();
+    private static final OutputView outputView = new OutputView();
 
     public static void run() {
         outputView.printGameStart();
         int bridgeSize = inputView.readBridgeSize();
+        BridgeMaker bridgeMaker = new BridgeMaker(new BridgeRandomNumberGenerator());
+        List<String> bridge = bridgeMaker.makeBridge(bridgeSize);
+
+        for (String s : bridge) {
+            System.out.print(s);
+        }
+        System.out.println();
     }
 }
