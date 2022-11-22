d2a27df14a90f277521277a8893cb85b1bd57c44
yongseungLeeDec <yonson.dev@gmail.com>
2022-11-22 12:37:48 +0900

    fix(modify): MoveGetter 클래스 싱글턴 패턴으로 변경

--- src/main/java/bridge/input/getter/MoveGetter.java
+++ src/main/java/bridge/input/getter/MoveGetter.java
@@ -7,12 +7,21 @@ public class MoveGetter {
     private static final String TEXT_PROMPT_FOR_MOVE = "이동할 칸을 선택해주세요. (위: U, 아래: D)";
     private static final String ERROR_INVALID_MOVE = "이동은 위칸(\"U\") 또는 아래칸(\"U\")으로만 가능합니다.";
 
+    private static MoveGetter instance;
+
     private final InputView inputView;
     private final MoveValidator validator;
 
-    public MoveGetter() {
+    private MoveGetter() {
         this.inputView = new InputView();
         this.validator = new MoveValidator();
+    }
+
+    public static MoveGetter getMoveGetter() {
+        if (instance == null) {
+            instance = new MoveGetter();
+        }
+        return instance;
     }
 
     public String getMoveFromConsole() throws IllegalArgumentException {
