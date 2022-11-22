906b79769cf92e882824935917888540140611bf
JunginPark4364 <wjddlsee@gmail.com>
2022-11-22 16:19:56 +0900

    feat: 사용자가 선택한 칸 이동

--- src/main/java/bridge/BridgeGame.java
+++ src/main/java/bridge/BridgeGame.java
@@ -1,16 +1,38 @@
 package bridge;
 
+import java.util.List;
+
 /**
  * 다리 건너기 게임을 관리하는 클래스
+ *
+ * 참고
+ *
+ * InputView, OutputView 를 사용하지 않는다.
+ * 인스턴스 변수 추가 가능
+ * 메서드 이름 변경X
+ * 인자, 반환 타입 변경 가능
+ * 게임 진행을 위해 필요한 메서드를 추가 하거나 변경가능
  */
 public class BridgeGame {
+    private Result result;
+    private int count;
+
+    public BridgeGame(List<String> answer) {
+        this(new Result(answer));
+    }
+
+    public BridgeGame(Result result) {
+        this.result = result;
+        count = 1;
+    }
 
     /**
      * 사용자가 칸을 이동할 때 사용하는 메서드
      * <p>
      * 이동을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void move() {
+    public void move(String place) {
+        result.add(place);
     }
 
     /**
@@ -19,5 +41,9 @@ public class BridgeGame {
      * 재시작을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
     public void retry() {
+    }
+
+    public int getCount() {
+        return count;
     }
 }
--- src/main/java/bridge/Result.java
+++ src/main/java/bridge/Result.java
@@ -5,6 +5,9 @@ import java.util.List;
 
 public class Result {
     private static final int PATH_COUNT = 2;
+    private static final String CORRECT_PATH = "O";
+    private static final String WRONG_PATH = "X";
+    private static final String OTHER_PATH = " ";
 
     private List<String> answer;
     private List<String>[] results;
@@ -16,5 +19,45 @@ public class Result {
         for (int index = 0; index < PATH_COUNT; index++) {
             results[index] = new ArrayList<>();
         }
+    }
+
+    public void add(String place) {
+        if (isCorrect(place)) {
+            addCorrect(place);
+            return;
+        }
+
+        addIncorrect(place);
+    }
+
+    private boolean isCorrect(String place) {
+        int current = getCurrent();
+        return answer.get(current).equals(place);
+    }
+
+    private void addIncorrect(String place) {
+        if (Direction.isUp(place)) {
+            results[0].add(WRONG_PATH);
+            results[1].add(OTHER_PATH);
+            return;
+        }
+
+        results[0].add(OTHER_PATH);
+        results[1].add(WRONG_PATH);
+    }
+
+    private void addCorrect(String place) {
+        if (Direction.isUp(place)) {
+            results[0].add(CORRECT_PATH);
+            results[1].add(OTHER_PATH);
+            return;
+        }
+
+        results[0].add(OTHER_PATH);
+        results[1].add(CORRECT_PATH);
+    }
+
+    public int getCurrent() {
+        return results[0].size();
     }
 }
