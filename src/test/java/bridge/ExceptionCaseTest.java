17ea119df4a3bef5a73fabee546fc98a0f4e2c1b
fnzl54 <rnjscksdud1@gmail.com>
2022-11-22 17:22:49 +0900

    feat: create isInputAllNumber test

--- src/test/java/bridge/ExceptionCaseTest.java
+++ src/test/java/bridge/ExceptionCaseTest.java
@@ -1,2 +1,15 @@
-package bridge;public class ExceptionCaseTest {
+package bridge;
+
+import org.junit.jupiter.api.Test;
+
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+public class ExceptionCaseTest {
+
+    @Test
+    void bridge_size가_3보다_작은_경우() {
+        assertThatThrownBy(() -> ExceptionCase.isInputAllNumber("2"))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
 }
