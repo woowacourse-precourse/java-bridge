db8b1e32b9d3fbd4a8a213a0e5fb172986653ab2
jaehyeon <jhjh0022@naver.com>
2022-11-22 19:42:16 +0900

    refactor(OutputView): 출력 문구 상수화로 인한 수정

--- src/main/java/bridge/view/OutputView.java
+++ src/main/java/bridge/view/OutputView.java
@@ -1,79 +1,38 @@
 package bridge.view;
 
-import static bridge.constants.Error.ERROR_MESSAGE;
+import bridge.constants.Message;
 
-/**
- * 사용자에게 게임 진행 상황과 결과를 출력하는 역할을 한다.
- */
 public class OutputView {
 
-    private final String START_GAME ="다리 건너기 게임을 시작합니다.";
-    private final String REQUEST_SIZE = "다리의 길이를 입력해주세요.";
-    private final String REQUEST_BRIDGE = "이동할 칸을 선택해주세요. (위: U, 아래: D)";
-    private final String REQUEST_RETRY = "게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)";
-    private final String RESULT = "최종 게임 결과";
-    private final String SUCESS_WHETHER = "게임 성공 여부: ";
-    private final String TRY_COUNT = "총 시도한 횟수: ";
-    private final String SUCESS = "성공";
-    private final String FAIL = "실패";
-    private final String EMPTY = "";
-
-    public void printStart(){
-        System.out.println(START_GAME);
+    public void printStart() {
+        System.out.println(Message.START_GAME.getValue());
         emptyLine();
     }
 
-    public void printRequestBridgeSize(){
-        System.out.println(REQUEST_SIZE);
-        emptyLine();
-    }
-
-    public void printRequestMove(){
-        System.out.println(REQUEST_BRIDGE);
-    }
-
-    /**
-     * 현재까지 이동한 다리의 상태를 정해진 형식에 맞춰 출력한다.
-     * <p>
-     * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
-     */
     public void printBridge(StringBuilder upBridge, StringBuilder downBridge) {
         System.out.println(upBridge);
         System.out.println(downBridge);
         emptyLine();
     }
 
-    public void printRequestRetry(){
-        System.out.println(REQUEST_RETRY);
+    public void printFinalGame() {
+        System.out.println(Message.RESULT_GAME.getValue());
     }
 
-    public void printFinalGame(){
-        System.out.println(RESULT);
+    public void printResult(boolean success, int cnt) {
+        System.out.println(Message.SUCCESS_WHETHER.getValue() + printSuccessWhether(success));
+        System.out.println(Message.TRY_COUNT.getValue() + cnt);
     }
 
-    /**
-     * 게임의 최종 결과를 정해진 형식에 맞춰 출력한다.
-     * <p>
-     * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
-     */
-    public void printResult(boolean sucess, int cnt) {
-        System.out.println(SUCESS_WHETHER + printSucessWheter(sucess));
-        System.out.println(TRY_COUNT + cnt);
-    }
-
-    private String printSucessWheter(boolean sucessGame){
-        if(sucessGame){
-            return SUCESS;
+    private String printSuccessWhether(boolean successGame) {
+        if (successGame) {
+            return Message.SUCCESS.getValue();
         }
-        return FAIL;
+        return Message.FAIL.getValue();
     }
 
-    public void printErrorMessage(String message){
-        System.out.println(ERROR_MESSAGE+ message);
-    }
-
-    public void emptyLine(){
-        System.out.println(EMPTY);
+    public void emptyLine() {
+        System.out.println(Message.EMPTY.getValue());
 
     }
 
