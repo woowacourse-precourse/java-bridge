3725b255134840571c9046ac06c90a3823d52c88
Sangwoo Shin <sangwoonoel@naver.com>
2022-11-22 16:32:46 +0900

    refactor(OutputMessage): 시도한 횟수에 대한  출력 메세지에 띄어쓰기 추가

--- src/main/java/bridge/OutputMessage.java
+++ src/main/java/bridge/OutputMessage.java
@@ -6,7 +6,7 @@ public enum OutputMessage {
     FINAL_GAME_RESULT_MESSAGE("최종 게임 결과"),
     GAME_SUCCESS_MESSAGE("게임 성공 여부: 성공"),
     GAME_FAIL_MESSAGE("게임 성공 여부: 실패"),
-    TOTAL_TRY_COUNT_MESSAGE("총 시도한 횟수:");
+    TOTAL_TRY_COUNT_MESSAGE("총 시도한 횟수: ");
 
     private final String message;
 
