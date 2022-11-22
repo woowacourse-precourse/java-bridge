5a89b1cad8c4e7ea1973a5ea3dbe9bcef95c8e60
ezzanzzan <cjsdmswjd821@naver.com>
2022-11-22 17:46:45 +0900

    feat: add function of compare to bridge with user

--- src/main/java/bridge/BridgeGame.java
+++ src/main/java/bridge/BridgeGame.java
@@ -1,5 +1,7 @@
 package bridge;
 
+import constants.GameCommand;
+import constants.Symbol;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -7,7 +9,6 @@ import java.util.List;
  * 다리 건너기 게임을 관리하는 클래스
  */
 public class BridgeGame {
-
     private List<String> upBridge = new ArrayList<>();
     private List<String> downBridge = new ArrayList<>();
 
@@ -25,6 +26,11 @@ public class BridgeGame {
      * 이동을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
     public void move() {
+    public boolean isCross(String bridge, String moving) {
+        if (bridge.equals(moving)) {
+            return true;
+        }
+        return false;
     }
 
     /**
