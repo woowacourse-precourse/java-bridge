e2545c8d7e1d82ced61e265ea86723c778986c6e
hyun0404woo <hyun0404woo@gmail.com>
2022-11-22 15:08:13 +0900

    test: TestNumberGenerator public class로 수정
    - BridgeGameTest를 위해서 public 객체로 따로 추출

--- src/test/java/bridge/service/BridgeGameTest.java
+++ src/test/java/bridge/service/BridgeGameTest.java
@@ -1,24 +1,25 @@
 package bridge.service;
 
 import bridge.BridgeNumberGenerator;
-import bridge.BridgeRandomNumberGenerator;
+import bridge.TestNumberGenerator;
 import bridge.domain.Moving;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.stream.IntStream;
 
+import static org.assertj.core.util.Lists.newArrayList;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 
 public class BridgeGameTest {
-
-    private final static BridgeNumberGenerator bridgeNumberGenerator = new BridgeRandomNumberGenerator();
 
     @DisplayName("다리의 길이만큼 이동했기 때문에 더 이상 이동할 수 없음")
     @Test
     void canNotMoveContinueBecauseMoveEnd() {
         // given
-        BridgeGame bridgeGame = new BridgeGame(bridgeNumberGenerator);
+        BridgeNumberGenerator numberGenerator = new TestNumberGenerator(newArrayList(1, 1, 1));
+
+        BridgeGame bridgeGame = new BridgeGame(numberGenerator);
         int bridgeSize = 3;
         bridgeGame.initialize(bridgeSize);
         
