fc475629aa13388916cff749954f26830fd6d3ab
Dabeen Jeong <hello70825@gmail.com>
2022-11-22 15:41:54 +0900

    feat: 사용자가 선택한 방향에 따라 다리를 보여주는 기능 추가

---
+++ src/main/java/bridge/domain/BridgePrintMaker.java
@@ -0,0 +1,23 @@
+package bridge.domain;
+
+import java.util.List;
+import java.util.StringJoiner;
+
+public class BridgePrintMaker {
+    StringJoiner upStringJoiner;
+    StringJoiner downStringJoiner;
+
+    public BridgePrintMaker() {
+        this.upStringJoiner = new StringJoiner(" | ", "[ ", " ]");
+        this.downStringJoiner = new StringJoiner(" | ", "[ ", " ]");
+    }
+
+    public void addStepBridge(String userSelectMove, Direction stepDirection) {
+        upStringJoiner.add(Move.getFilterBridgeMove(userSelectMove, Direction.UP, stepDirection));
+        downStringJoiner.add(Move.getFilterBridgeMove(userSelectMove, Direction.DOWN, stepDirection));
+    }
+
+    public List<String> getBridgePrint() {
+        return List.of(upStringJoiner.toString(), downStringJoiner.toString());
+    }
+}
