cb16d20ccba6947af4272a4a6a4638bbe94b767a
JaeHwanCho <makefuture@kakao.com>
2022-11-22 17:43:47 +0900

    feat(function): add game logic
    Add new function:
    - add game logic

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -7,8 +7,10 @@ public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
         InputView inputView = new InputView();
+        OutputView outputView = new OutputView();
         BridgeNumberGenerator bridgeNumberGenerator = new BridgeRandomNumberGenerator();
         BridgeMaker bridgeMaker = new BridgeMaker(bridgeNumberGenerator);
+        int count = 0;
 
         System.out.println("다리 건너기 게임을 시작합니다.");
         System.out.println("다리의 길이를 입력해주세요.");
@@ -16,5 +18,34 @@ public class Application {
 
         List<String> strings = bridgeMaker.makeBridge(bridgeSize);
         System.out.println("strings = " + strings);
+
+        while (true) {
+            count++;
+            try {
+                for (int i = 0; i < bridgeSize; i++) {
+                    System.out.println("이동할 칸을 선택해주세요.");
+                    String movingPos = inputView.readMoving(); // s : U or D // 1,3,5 위치
+                    boolean equals = movingPos.charAt(0) == (strings.get(0).charAt(i * 2));
+                    if (!(movingPos.charAt(0) == (strings.get(0).charAt(i * 2)))) {
+                        throw new IllegalArgumentException();
+                    }
+                    outputView.printMap();
+                }
+                System.out.println("게임 성공 여부: 성공");
+                System.out.println("총 시도한 횟수: " + count);
+                return;
+            } catch (IllegalArgumentException e) {
+                System.out.println("틀렸습니다.");
+                System.out.println("게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)");
+                String s = inputView.readGameCommand();
+                if (s.equals("Q")) {
+                    outputView.printMap();
+                    System.out.println("게임 성공 여부: 실패");
+                    System.out.println("총 시도한 횟수: " + count);
+                    return;
+                };
+            }
+        }
+
     }
 }
