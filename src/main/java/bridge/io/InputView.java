930d159b0d6e01d49c19d8823cfa83980da780a0
yubin <dbqls2821@gmail.com>
2022-11-22 10:38:31 +0900

    chore(InputView): 잘못된 값이 들어온경우 인풋을 다시 받도록 수정

--- src/main/java/bridge/io/InputView.java
+++ src/main/java/bridge/io/InputView.java
@@ -1,7 +1,6 @@
 package bridge.io;
 
 import bridge.enums.InGameMessage;
-import bridge.io.validation.*;
 import camp.nextstep.edu.missionutils.Console;
 
 /**
@@ -23,15 +22,43 @@ public class InputView {
      * 다리의 길이를 입력받는다.
      */
     public int readBridgeSize() {
+        String bridgeSize = null;
+        while (true) {
+            try {
+                bridgeSize = getValidBridgeSize();
+                break;
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            }
+        }
+        return Integer.parseInt(bridgeSize);
+    }
+
+    private String getValidBridgeSize() {
         System.out.println(InGameMessage.INPUT_BRIDGE_LENGTH.getMessage());
         String input = Console.readLine();
-        return inputValidation.validateAndReturnBridgeSize(input);
+        inputValidation.validateBridgeSize(input);
+        return input;
     }
 
     /**
      * 사용자가 이동할 칸을 입력받는다.
      */
     public String readMoving() {
+        String move = null;
+        while (true) {
+            try {
+                move = getValidMove();
+                break;
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            }
+        }
+        return move;
+    }
+
+    private String getValidMove() {
+        System.out.println();
         System.out.println(InGameMessage.INPUT_DIRECTION.getMessage());
         String input = Console.readLine();
         return inputValidation.validateAndReturnMove(input);
@@ -41,6 +68,19 @@ public class InputView {
      * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
      */
     public String readGameCommand() {
+        String command = null;
+        while (true) {
+            try {
+                command = getValidCommand();
+                break;
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            }
+        }
+        return command;
+    }
+
+    private String getValidCommand() {
         System.out.println(InGameMessage.INPUT_RETRY_OR_QUIT.getMessage());
         String input = Console.readLine();
         return inputValidation.validateAndReturnCommand(input);
