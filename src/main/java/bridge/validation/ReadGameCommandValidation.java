d06cf44731f000141a344ef0d360b3933d2ee61e
desfox <desfox99@naver.com>
2022-11-22 18:43:22 +0900

    refactor(ReadGameCommandValidation): 에러 문구 ErrorMessage Enum으로 대체

--- src/main/java/bridge/validation/ReadGameCommandValidation.java
+++ src/main/java/bridge/validation/ReadGameCommandValidation.java
@@ -1,12 +1,14 @@
 package bridge.validation;
 
+import static bridge.validation.ErrorMessage.*;
+
 public class ReadGameCommandValidation {
     private static final String RESTART_COMMAND = "R";
     private static final String QUIT_COMMAND = "Q";
 
     public void validateInputFormat(String gameCommand) {
         if ((! gameCommand.equals(RESTART_COMMAND)) && (! gameCommand.equals(QUIT_COMMAND))) {
-            throw new IllegalArgumentException("[ERROR] 재시작, 종료 여부를 R 또는 Q로 입력해주세요.");
+            throw new IllegalArgumentException(INPUT_COMMAND_IN_R_OR_Q.getMessage());
         }
     }
 }
