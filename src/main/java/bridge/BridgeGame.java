8232f9bf2cdc9f59ecfd61bac9211e010f089e1c
gynhyunkim <gyeonghyun.kimm@gmail.com>
2022-11-22 16:39:31 +0900

    feat: 사용자가 이동할 칸 검증

--- src/main/java/bridge/BridgeGame.java
+++ src/main/java/bridge/BridgeGame.java
@@ -6,16 +6,10 @@ import java.util.List;
  * 다리 건너기 게임을 관리하는 클래스
  */
 public class BridgeGame {
-    private List<String> bridge;
-    private int current;
+    Bridge bridge;
 
-    public BridgeGame(List<String> bridge) {
+    public BridgeGame(Bridge bridge) {
         this.bridge = bridge;
-        current = 0;
-    }
-
-    private boolean isAvailable(String moving) {
-        return bridge.get(current + 1).equals(moving);
     }
 
     /**
@@ -23,13 +17,9 @@ public class BridgeGame {
      * <p>
      * 이동을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public boolean move(String moving) {
-        Validation.validateMoving(moving);
-        if (!isAvailable(moving)) {
-            return false;
-        }
-        current++;
-        return true;
+    public void move(String moving) {
+        bridge.move(moving);
+        System.out.println(bridge);
     }
 
     /**
--- src/main/java/bridge/Error.java
+++ src/main/java/bridge/Error.java
@@ -3,8 +3,9 @@ package bridge;
 public enum Error {
 
     BRIDGE_OUT_OF_RANGE("[ERROR] 다리 길이는 3부터 20 사이의 숫자여야 합니다."),
-    INPUT_IS_NOT_NUM("[ERROR] 입력값이 숫자가 아닙니다.");
-    private String message;
+    INPUT_IS_NOT_NUM("[ERROR] 입력값이 숫자가 아닙니다."),
+    INVALID_MOVING("[ERROR] 이동할 칸은 U 혹은 D 여야 합니다.");
+    private final String message;
 
     Error(String message) {
         this.message = message;
