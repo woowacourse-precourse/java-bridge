03079949f0834b7eacfda2115115fcb2ce09be54
song-wooseok <sws981202@gmail.com>
2022-11-22 21:00:12 +0900

    refactor: 메시지에 커맨드가 포함되어 있을 경우 해당 부분 상수로 리팩터링

--- src/main/java/bridge/utils/constants/ErrorMessage.java
+++ src/main/java/bridge/utils/constants/ErrorMessage.java
@@ -1,8 +1,8 @@
 package bridge.utils.constants;
 
 public enum ErrorMessage {
-    ERROR_GAME_COMMAND("[ERROR] 다시 입력하세요. (재시도: R, 종료: Q)"),
-    ERROR_BRIDGE_MOVE_MESSAGE("[ERROR] 이동할 수 있는 칸은 U 또는 D의 문자입니다."),
+    ERROR_GAME_COMMAND("[ERROR] 다시 입력하세요. (재시도: "+GameCommand.RETRY.getCommand()+", 종료: "+GameCommand.QUIT.getCommand()+")"),
+    ERROR_BRIDGE_MOVE_MESSAGE("[ERROR] 이동할 수 있는 칸은 "+GameCommand.GO_UP.getCommand()+" 또는 "+GameCommand.GO_DOWN.getCommand()+"의 문자입니다."),
     ERROR_BRIDGE_SIZE_MESSAGE("[ERROR] 옳바른 다리의 길이를 입력해주세요.");
 
     private final String message;
--- src/main/java/bridge/utils/constants/InputMessage.java
+++ src/main/java/bridge/utils/constants/InputMessage.java
@@ -1,8 +1,8 @@
 package bridge.utils.constants;
 
 public enum InputMessage {
-    READ_GAME_RETRY_COMMAND("게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)"),
-    READ_BRIDGE_MOVE_MESSAGE("이동할 칸을 선택해주세요. (위: U, 아래: D)"),
+    READ_GAME_RETRY_COMMAND("게임을 다시 시도할지 여부를 입력해주세요. (재시도: "+GameCommand.RETRY.getCommand()+", 종료: "+GameCommand.QUIT.getCommand()+")"),
+    READ_BRIDGE_MOVE_MESSAGE("이동할 칸을 선택해주세요. (위: "+GameCommand.GO_UP.getCommand()+", 아래: "+GameCommand.GO_DOWN.getCommand()+")"),
     READ_BRIDGE_SIZE_MESSAGE("다리의 길이를 입력해주세요.");
     private final String message;
 
