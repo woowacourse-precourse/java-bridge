dab477ca59cd46cee4bf364becf9308bfb2a0fc5
gnoesnooj <306yyy@naver.com>
2022-11-22 15:03:10 +0800

    feat(MovingOption) : 윗다리인지 아랫다리인지에 대한 판별 메소드 추가

--- src/main/java/bridge/rule/MovingOption.java
+++ src/main/java/bridge/rule/MovingOption.java
@@ -14,4 +14,12 @@ public enum MovingOption {
     public static boolean isCorrectMoving(String moving) {
         return UP.moving.equals(moving) || DOWN.moving.equals(moving);
     }
+
+    public static boolean isUpside(String moving){
+        return moving.equals(UP.moving);
+    }
+
+    public static boolean isDownside(String moving){
+        return moving.equals(DOWN.moving);
+    }
 }
