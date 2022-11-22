a5d54955ba5d86185df3a8a2b1965b8b7d2280e6
DevJay1024 <devjay1024@gmail.com>
2022-11-22 17:56:19 +0900

    feat: 난이도 설정 이벤트 관리 기능 구현

--- src/main/java/bridge/controller/BridgeGameController.java
+++ src/main/java/bridge/controller/BridgeGameController.java
@@ -1,8 +1,15 @@
 package bridge.controller;
 
 import bridge.BridgeGame;
+import bridge.BridgeRandomNumberGenerator;
 import bridge.views.InputView;
 import bridge.views.OutputView;
+
+import java.lang.reflect.Executable;
+import java.util.Observable;
+import java.util.function.Consumer;
+import java.util.function.Function;
+import java.util.function.Supplier;
 
 public class BridgeGameController {
 
@@ -16,9 +23,47 @@ public class BridgeGameController {
         outputView.printWelcome();
     }
 
-    private void initComponents(){
+    private void initComponents() {
         inputView = new InputView();
         outputView = new OutputView();
         bridgeGame = new BridgeGame();
     }
+
+    public void setDifficulty() {
+        retryWhenExceptionOrTryOnce(outputView, () -> {
+            int bridgeSize = inputView.readBridgeSize();
+            bridgeGame.initComponents(new BridgeRandomNumberGenerator(), bridgeSize);
+        });
+    }
+
+    public boolean isGameDone() {
+        return false;
+    }
+
+    public void playGame() {
+
+    }
+
+    public boolean askRetry() {
+        return false;
+    }
+
+    public void end() {
+
+    }
+
+    private static void retryWhenExceptionOrTryOnce(OutputView outputView, Task task) {
+        while (true) {
+            try {
+                task.run();
+                break;
+            } catch (IllegalArgumentException exception) {
+                outputView.printErrorMessage(exception.getMessage());
+            }
+        }
+    }
+}
+
+interface Task {
+    void run();
 }
