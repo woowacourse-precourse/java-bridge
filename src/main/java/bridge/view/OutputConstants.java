0a563e9c823ae760ceba777eabc6a3ebb7cfe66d
akskflwn <akskflwn@gmail.com>
2022-11-22 09:55:16 +0900

    refactor(OutputView): 출력 로직 수정 - View는 view 역활만 할 수 있게 리펙토링

--- src/main/java/bridge/view/OutputConstants.java
+++ src/main/java/bridge/view/OutputConstants.java
@@ -2,16 +2,11 @@ package bridge.view;
 
 public class OutputConstants {
 
-    public static final String UP_STAIR = "U";
-    public static final String DOWN_STAIR = "D";
-    public static final String START_PREFIX = "[";
-    public static final String END_REGEX = "]";
-    public static final String WRONG = " X ";
-    public static final String CORRECT = " O ";
-    public static final String BLANK = "   ";
-    public static final String DELIMITER = "|";
+    public static final String DELIMITER = "\\|";
     public static final String SUCCESS = "성공";
     public static final String FAILURE = "실패";
+    public static final String LINE_SEPARATOR ="\n";
+    public static final String BLANK = "";
 
     public static final String GAME_START_MESSAGE = "다리 건너기 게임을 시작합니다.";
     public static final String GAME_RESULT_MESSAGE = "최종 게임 결과";
--- src/main/java/bridge/view/OutputView.java
+++ src/main/java/bridge/view/OutputView.java
@@ -1,7 +1,7 @@
 package bridge.view;
 
-import bridge.domain.BridgeGame;
-import bridge.domain.Player;
+import bridge.domain.map.BridgeMap;
+import bridge.domain.map.BridgeMark;
 import java.util.List;
 
 import static bridge.view.OutputConstants.*;
@@ -13,62 +13,38 @@ public class OutputView {
         System.out.println();
     }
 
-    public static void printMap(BridgeGame bridgeGame) {
-        printDependOn(UP_STAIR, bridgeGame);
-        printDependOn(DOWN_STAIR, bridgeGame);
+    public static void printMap(BridgeMap map) {
+        System.out.println(convertToString(map));
         System.out.println();
     }
 
-    private static void printDependOn(String stair, BridgeGame bridgeGame) {
-        StringBuilder sb = new StringBuilder(START_PREFIX);
-        List<String> bridge = bridgeGame.getBridge();
-        int maxStage = bridgeGame.getStageNumber();
-
-        sb.append(getPrevious(bridge, maxStage, stair));
-        sb.append(getNext(bridgeGame, stair));
-        sb.append(END_REGEX);
-        System.out.println(sb);
+    private static String convertToString(BridgeMap map) {
+        List<BridgeMark> up = map.getUp();
+        List<BridgeMark> down = map.getDown();
+        return lineToString(up) + LINE_SEPARATOR + lineToString(down);
     }
 
-    private static String getNext(BridgeGame bridgeGame, String stage) {
-        Player player = bridgeGame.getPlayer();
-        if (player.isAlive() && stage.equals(player.getLastSelection())) {
-            return CORRECT;
+    private static String lineToString(List<BridgeMark> line) {
+        StringBuilder sb = new StringBuilder();
+        for (BridgeMark mapSymbol : line) {
+            sb.append(mapSymbol.getMark());
         }
-        if (!player.isAlive() && stage.equals(player.getLastSelection())) {
-            return WRONG;
-        }
-        return BLANK;
+        return sb.toString().replaceFirst(DELIMITER, BLANK);
     }
 
-    private static String getPrevious(List<String> bridge, int maxStage, String upOrDown) {
-        StringBuilder previous = new StringBuilder();
-        for (int i = 0; i < maxStage; i++) {
-            String previousStage = bridge.get(i);
-            previous.append(getCorrectOrBlank(upOrDown, previousStage));
-            previous.append(DELIMITER);
-        }
-        return previous.toString();
-    }
-
-    private static String getCorrectOrBlank(String upOrDown, String previousStage) {
-        if (previousStage.equals(upOrDown)) {
-            return CORRECT;
-        }
-        return BLANK;
-    }
-
-    public static void printResult(BridgeGame bridgeGame) {
+    public static void printResult(BridgeMap map,int count,boolean isAlive) {
         System.out.println(GAME_RESULT_MESSAGE);
-        printMap(bridgeGame);
-        System.out.println(String.format(SUCCESS_OR_FAILURE, isSuccess(bridgeGame)));
-        System.out.println(String.format(TRIAL_COUNT_MESSAGE, bridgeGame.getTrialCount()));
+        printMap(map);
+        System.out.println(String.format(SUCCESS_OR_FAILURE, isSuccess(isAlive)));
+        System.out.println(String.format(TRIAL_COUNT_MESSAGE, count));
     }
 
-    private static String isSuccess(BridgeGame bridgeGame) {
-        if (bridgeGame.isPlayerAlive()) {
+    private static String isSuccess(boolean isAlive) {
+        if (isAlive) {
             return SUCCESS;
         }
         return FAILURE;
     }
+
+
 }
