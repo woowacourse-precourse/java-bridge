7f818d8f7b440069721b3c06e40f538c85a99af0
geneaky <kaktus418@gmail.com>
2022-11-22 12:54:31 +0900

    [feat] add constant enum used at whole bridge package

--- src/main/java/bridge/constant/MovingDirection.java
+++ src/main/java/bridge/constant/MovingDirection.java
@@ -1,14 +1,17 @@
 package bridge.constant;
 
 import static bridge.constant.ErrorResponse.INVALID_INPUT_MOVING_DIRECTION;
+import static bridge.constant.ErrorResponse.NOT_FOUND_DIRECTION;
 
 public enum MovingDirection {
-    U("U"), D("D");
+    U("U", "1"), D("D", "0");
 
     private final String direction;
+    private final String directionNumber;
 
-    MovingDirection(String direction) {
+    MovingDirection(String direction, String directionNumber) {
         this.direction = direction;
+        this.directionNumber = directionNumber;
     }
 
     public static MovingDirection from(String inputMovingDirection) {
@@ -20,11 +23,18 @@ public enum MovingDirection {
         throw INVALID_INPUT_MOVING_DIRECTION.getValue();
     }
 
-    private static boolean isSameDirection(String inputMovingDirection, MovingDirection direction) {
-        return direction.name().equals(inputMovingDirection);
+    private static boolean isSameDirection(String inputMovingDirection,
+        MovingDirection movingDirection) {
+        return movingDirection.direction.equals(inputMovingDirection)
+            || movingDirection.directionNumber.equals(inputMovingDirection);
     }
 
-    public String getValue() {
-        return this.direction;
+    public static String getDirection(int directionNumber) {
+        for (MovingDirection movingDirection : MovingDirection.values()) {
+            if (movingDirection.directionNumber.equals(String.valueOf(directionNumber))) {
+                return movingDirection.direction;
+            }
+        }
+        throw NOT_FOUND_DIRECTION.getValue();
     }
 }
