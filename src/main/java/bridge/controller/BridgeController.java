4308bf406ff2d4b1f3782d95c71a66cd158dbd6f
eom-tae-in <eti0728@daum.net>
2022-11-22 20:50:01 +0900

    feat(BridgeController): game() -> 구현한 게임을 작동하기 위한 메서드 구현

--- src/main/java/bridge/controller/BridgeController.java
+++ src/main/java/bridge/controller/BridgeController.java
@@ -3,19 +3,24 @@ package bridge.controller;
 import bridge.BridgeNumberGenerator;
 import bridge.BridgeRandomNumberGenerator;
 import bridge.domain.BridgeGame;
+import bridge.domain.User;
 import bridge.util.BridgeMaker;
 import bridge.util.DataTypeChanger;
 import bridge.view.InputView;
 import bridge.view.OutputView;
 
 public class BridgeController {
-
     OutputView outputView = new OutputView();
     InputView inputView = new InputView();
     DataTypeChanger dataTypeChanger = new DataTypeChanger();
     BridgeNumberGenerator bridgeNumberGenerator = new BridgeRandomNumberGenerator();
     BridgeMaker bridgeMaker = new BridgeMaker(bridgeNumberGenerator);
 
+    public void game() {
+        BridgeGame bridgeGame = create();
+        User user = new User();
+        play(bridgeGame, user);
+    }
 
     private BridgeGame create() {
         outputView.printStartBridgeGame();
@@ -24,6 +29,7 @@ public class BridgeController {
         int size = dataTypeChanger.StringToInteger(bridgeSize);
         return new BridgeGame(bridgeMaker.makeBridge(size));
     }
+
     private void play(BridgeGame bridgeGame, User user) {
         while (!user.getQuit() && !bridgeGame.comparePositionLength()) {
             outputView.printMoveToWhereQuestion();
