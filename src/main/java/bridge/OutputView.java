48a347e0b5d1ffdd5e5fa1ada802fcb6e9b460ef
Yeongseon Park <yseon406@gmail.com>
2022-11-22 14:06:17 +0900

    refactor: 게임 결과 출력 기능 수정

--- src/main/java/bridge/OutputView.java
+++ src/main/java/bridge/OutputView.java
@@ -1,9 +1,13 @@
 package bridge;
 
+import java.util.List;
+
 /**
  * 사용자에게 게임 진행 상황과 결과를 출력하는 역할을 한다.
  */
 public class OutputView {
+    private String upResult = "";
+    private String downResult = "";
 
     public void printStartMessage() {
         System.out.println("다리 건너기 게임을 시작합니다.");
@@ -14,17 +18,18 @@ public class OutputView {
      * <p>
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void printMap(String up, String down) {
-        System.out.printf("[ %c", up.charAt(0));
-        for(int index = 1; index<up.length(); index++){
-            System.out.printf(" | %c", up.charAt(index));
+    public void printMap(List<String> player, String result) {
+        for (String move : player) {
+            if (move.equals("U")) {
+                upResult += result;
+                downResult += " ";
+            }
+            if (move.equals("D")) {
+                upResult += " ";
+                downResult += result;
+            }
         }
-        System.out.println(" ]");
-        System.out.printf("[ %c", down.charAt(0));
-        for(int index = 1; index<down.length(); index++){
-            System.out.printf(" | %c", down.charAt(index));
-        }
-        System.out.println(" ]");
+        printResult();
     }
 
     /**
@@ -33,5 +38,16 @@ public class OutputView {
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
     public void printResult() {
+        System.out.println("최종 게임 결과");
+        System.out.printf("[ %c", upResult.charAt(0));
+        for (int index = 1; index < upResult.length(); index++) {
+            System.out.printf(" | %c", upResult.charAt(index));
+        }
+        System.out.println(" ]");
+        System.out.printf("[ %c", downResult.charAt(0));
+        for (int index = 1; index < downResult.length(); index++) {
+            System.out.printf(" | %c", downResult.charAt(index));
+        }
+        System.out.println(" ]");
     }
 }
