06f7ecd45c5bc0ab31426688c0e5196820ea062d
JangAJang <99702271+JangAJang@users.noreply.github.com>
2022-11-22 19:20:39 +0900

    feat: BridgeGame 플레이어와 Bridge로 만든 결과를 반환하는 기능 구현

--- src/main/java/bridge/BridgeGame.java
+++ src/main/java/bridge/BridgeGame.java
@@ -2,6 +2,9 @@ package bridge;
 
 import bridge.model.Bridge;
 import bridge.model.Player;
+import bridge.util.BridgeViewConstructor;
+
+import java.util.List;
 
 public class BridgeGame{
     private final Bridge bridge;
@@ -12,5 +15,10 @@ public class BridgeGame{
         BridgeMaker bridgeMaker = new BridgeMaker(bridgeRandomNumberGenerator);
         bridge = new Bridge(bridgeMaker.makeBridge(size));
         player = new Player();
+    }
+
+    private String constructBridge(){
+        BridgeViewConstructor bridgeViewConstructor = new BridgeViewConstructor();
+        return bridgeViewConstructor.constructBridge(player.getBridges(), bridge.getBridges());
     }
 }
