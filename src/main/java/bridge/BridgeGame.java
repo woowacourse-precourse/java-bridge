e630af005d95efb96107806acd177e300b4c32b7
xxeol2 <jhj01177@naver.com>
2022-11-22 13:49:58 +0900

    feat: 라운드마다 사용자를 이동시킨다

--- src/main/java/bridge/BridgeGame.java
+++ src/main/java/bridge/BridgeGame.java
@@ -1,16 +1,28 @@
 package bridge;
 
+import java.util.List;
+
 /**
  * 다리 건너기 게임을 관리하는 클래스
  */
 public class BridgeGame {
+
+    private final List<String> bridge;
+    private int round = 0;
+
+    public BridgeGame(List<String> bridge) {
+        this.bridge = bridge;
+    }
 
     /**
      * 사용자가 칸을 이동할 때 사용하는 메서드
      * <p>
      * 이동을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void move() {
+    public boolean move(String moving) {
+        increaseRound();
+        String row = bridge.get(round - 1);
+        return moving.equals(row);
     }
 
     /**
@@ -19,5 +31,9 @@ public class BridgeGame {
      * 재시작을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
     public void retry() {
+    }
+
+    private void increaseRound() {
+        this.round += 1;
     }
 }
