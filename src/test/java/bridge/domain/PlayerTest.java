cabf3022980569fdc808f217275e67b316d4c460
code-tree <dltjdqls1200@naver.com>
2022-11-22 15:59:49 +0900

    test: 게임 재도전시 플레이어 newLife() 테스트 코드 작성
    lifeCount가 1 증가한다.
    이동 결과가 담긴 moveResults가 초기화된다.
    이동 거리가 초기화된다.

--- src/test/java/bridge/domain/PlayerTest.java
+++ src/test/java/bridge/domain/PlayerTest.java
@@ -1,6 +1,7 @@
 package bridge.domain;
 
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
@@ -14,13 +15,14 @@ import static bridge.domain.MoveResult.*;
 import static org.assertj.core.api.Assertions.assertThat;
 
 class PlayerTest {
+    final Player player = new Player();
+
     @DisplayName("다리 이동 입력에 따른 결과 테스트")
     @MethodSource(value = "moveTestData")
     @ParameterizedTest(name = "{index}. 입력: {0}")
     public void playerMoveTest(List<Move> input, List<MoveResult> UpSideResults, List<MoveResult> DownSideResults) {
         //given
         Bridge bridge = new Bridge(List.of("U", "U", "D", "U"));
-        Player player = new Player();
 
         //when
         for (Move move : input) {
@@ -33,7 +35,46 @@ class PlayerTest {
         assertThat(playerMoved.get(DOWN)).isEqualTo(DownSideResults);
     }
 
-    static Stream<Arguments> moveTestData() {
+    @DisplayName("newLife 호출 시 플레이어 lifeCount가 1 증가한다.")
+    @Test
+    public void newLifePlayerLifeCountTest() {
+        Bridge bridge = new Bridge(List.of("U", "D", "U"));
+        int beforeLifeCount = player.getLifeCount();
+        player.move(bridge, UP);
+
+        player.newLife();
+
+        assertThat(player.getLifeCount()).isEqualTo(beforeLifeCount + 1);
+    }
+
+    @DisplayName("newLife 호출 시 플레이어 moveDistance가 0으로 된다.")
+    @Test
+    public void newLifePlayerMoveDistanceInitTest() {
+        Bridge bridge = new Bridge(List.of("U", "D", "U"));
+        player.move(bridge, UP);
+        player.move(bridge, DOWN);
+        int beforeMoveDistance = player.getMoveDistance();
+
+        player.newLife();
+
+        assertThat(beforeMoveDistance).isEqualTo(2);
+        assertThat(player.getMoveDistance()).isEqualTo(0);
+    }
+
+    @DisplayName("newLife 호출 시 플레이어의 moveResults가 초기화 된다.")
+    @Test
+    public void newLifePlayerMoveResultsInitTest() {
+        Bridge bridge = new Bridge(List.of("U", "D", "U"));
+        player.move(bridge, UP);
+        player.move(bridge, DOWN);
+        Map<Move, List<MoveResult>> beforeMoveResults = Map.copyOf(player.getMoveResults());
+
+        player.newLife();
+
+        assertThat(beforeMoveResults).isNotEqualTo(player.getMoveResults());
+    }
+
+        static Stream<Arguments> moveTestData() {
         return Stream.of(
                 Arguments.of(
                         List.of(UP, UP, DOWN, UP),
