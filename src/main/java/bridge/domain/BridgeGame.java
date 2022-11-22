fe02629c0e6f927ec0b63d55b3b14940cc7cf18a
metorg <metrog950@gmail.com>
2022-11-22 21:04:07 +0900

    feat: 비교값을 기준으로 인덱스 이동 메서드 생성

--- src/main/java/bridge/domain/BridgeGame.java
+++ src/main/java/bridge/domain/BridgeGame.java
@@ -8,12 +8,22 @@ import java.util.List;
  */
 public class BridgeGame {
 
-    private int current=0;
+    private BridgeBoard bridgeBoard;
+    private int current;
+
+    public BridgeGame(){
+        this.bridgeBoard = new BridgeBoard();
+        current =0;
+    }
+
 
     public boolean compare(List<String> bridge,String userInput){
         if(bridge.get(current)==covertUserInput(userInput)){
-           move();
+           move(userInput,"O");
+           return true;
         }
+        move(userInput,"X");
+        return false;
     }
 
     private String covertUserInput(String userInput){
@@ -26,9 +36,18 @@ public class BridgeGame {
      * <p>
      * 이동을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void move() {
+    public void move(String input,String result) {
+        if(current>0)bridgeBoard.addWall();
+        bridgeBoard.addMove(input,result);
+        indexMove(result);
+    }
 
-
+    public void indexMove(String result){
+        if(result=="O"){
+            current=current+1;
+            return;
+        }
+            current=0;
     }
 
     /**
