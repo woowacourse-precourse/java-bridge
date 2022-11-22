eea7ceee3f9a65bfebf628490251e8188a345b1e
hoonn9 <hun9935@gmail.com>
2022-11-22 16:38:38 +0900

    test(OutputView): 최종 결과 출력 테스트

--- src/test/java/bridge/view/OutputViewTest.java
+++ src/test/java/bridge/view/OutputViewTest.java
@@ -8,6 +8,7 @@ import bridge.TestNumberGenerator;
 import bridge.domain.bridge.Bridge;
 import bridge.domain.bridge.BridgeMove;
 import bridge.domain.game.BridgeGame;
+import bridge.domain.game.BridgeGameStatus;
 import camp.nextstep.edu.missionutils.test.NsTest;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -15,6 +16,8 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.EnumSource;
 
 public class OutputViewTest {
     
@@ -95,6 +98,105 @@ public class OutputViewTest {
         @Override
         protected void runMain() {
             outputView.printMap(bridgeGame);
+        }
+    }
+    
+    @Nested
+    class PrintResultTest extends NsTest {
+        
+        BridgeGame bridgeGame;
+        
+        @BeforeEach
+        void setUp() {
+            List<BridgeMove> expectMoves = List.of(
+                    BridgeMove.DOWN,
+                    BridgeMove.DOWN,
+                    BridgeMove.DOWN,
+                    BridgeMove.UP
+            );
+            TestNumberGenerator testNumberGenerator = new TestNumberGenerator(
+                    expectMoves.stream().map(BridgeMove::getGenerateNumber).collect(Collectors.toList()));
+            Bridge bridge = new Bridge(new BridgeMaker(testNumberGenerator).makeBridge(expectMoves.size()));
+            bridgeGame = new BridgeGame(bridge);
+        }
+        
+        @DisplayName("시도한 경우 중 가장 멀리간 결과를 최종 결과로 출력한다.")
+        @Test
+        void printByBestRecord() {
+            bridgeGame.move(BridgeMove.DOWN);
+            bridgeGame.move(BridgeMove.DOWN);
+            bridgeGame.fail(BridgeMove.UP);
+            
+            bridgeGame.retry();
+            bridgeGame.move(BridgeMove.DOWN);
+            bridgeGame.move(BridgeMove.DOWN);
+            bridgeGame.move(BridgeMove.DOWN);
+            bridgeGame.move(BridgeMove.DOWN);
+            
+            bridgeGame.retry();
+            bridgeGame.move(BridgeMove.DOWN);
+            bridgeGame.move(BridgeMove.DOWN);
+            bridgeGame.fail(BridgeMove.UP);
+            
+            bridgeGame.setStatus(BridgeGameStatus.FAILURE);
+            
+            assertSimpleTest(() -> {
+                run();
+                assertThat(output()).contains(
+                        "최종 게임 결과",
+                        "[   |   |   |   ]",
+                        "[ O | O | O | O ]",
+                        "게임 성공 여부: 실패"
+                );
+            });
+        }
+        
+        
+        // 방향이 더 추가 되었을 때를 고려함
+        @DisplayName("시도한 경우 중 이동에 성공한 거리가 같은 경우 최근의 결과를 출력한다.")
+        @Test
+        void printByRecentlyRecord() {
+            bridgeGame.move(BridgeMove.DOWN);
+            bridgeGame.move(BridgeMove.DOWN);
+            bridgeGame.fail(BridgeMove.UP);
+            
+            bridgeGame.retry();
+            bridgeGame.move(BridgeMove.DOWN);
+            bridgeGame.move(BridgeMove.DOWN);
+            bridgeGame.fail(BridgeMove.DOWN);
+            
+            bridgeGame.setStatus(BridgeGameStatus.FAILURE);
+            
+            assertSimpleTest(() -> {
+                run();
+                assertThat(output()).contains(
+                        "최종 게임 결과",
+                        "[   |   |   ]",
+                        "[ O | O | X ]",
+                        "게임 성공 여부: 실패"
+                );
+            });
+        }
+        
+        
+        @DisplayName("게임 상태 별로 게임 결과 여부를 출력한다.")
+        @EnumSource(BridgeGameStatus.class)
+        @ParameterizedTest
+        void printByGameStatus(BridgeGameStatus status) {
+            bridgeGame.setStatus(status);
+            assertSimpleTest(() -> {
+                run();
+                assertThat(output()).contains(
+                        "최종 게임 결과",
+                        "게임 성공 여부:",
+                        status.getOutputText()
+                );
+            });
+        }
+        
+        @Override
+        protected void runMain() {
+            outputView.printResult(bridgeGame);
         }
     }
 }
