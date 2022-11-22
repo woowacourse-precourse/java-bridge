b099d5d893ecc01e137c247b4c877dd0c34d0aa8
cgk95 <kjkj5868@gmail.com>
2022-11-22 09:53:42 +0900

    refactor(Controller) : 패키지 구조 변경

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -1,5 +1,7 @@
 package bridge;
 
+import bridge.Controller.GameController;
+
 public class Application {
     public static void main(String[] args) {
         try {
--- src/main/java/bridge/Controller/VisualizeController.java
+++ src/main/java/bridge/Controller/VisualizeController.java
@@ -1,4 +1,4 @@
-package bridge;
+package bridge.Controller;
 
 import bridge.Model.BridgeGame;
 import bridge.View.UiMessage;
