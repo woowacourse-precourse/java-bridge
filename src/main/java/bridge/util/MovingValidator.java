312e9b207a74fed400c6b96899ad19ba496a3c9f
footdev <ekclstkfka5@gmail.com>
2022-11-22 14:43:44 +0900

    feat: Status 클래스 추가
    사다리의 상태 정보를 책임지는 Status 클래스를 추가함.

--- src/main/java/bridge/util/MovingValidator.java
+++ src/main/java/bridge/util/MovingValidator.java
@@ -1,4 +1,19 @@
 package bridge.util;
 
+import bridge.view.OutputView;
+
 public class MovingValidator {
+
+    private static final int LENGTH = 1;
+    private static final char UP = 'U';
+    private static final char DOWN = 'D';
+
+    public static void validateMoving(String input) {
+        if(input.length() != LENGTH) {
+            throw new IllegalArgumentException(OutputView.MOVING_ERROR_MESSAGE);
+        }
+        if(input.charAt(0) != UP && input.charAt(0) != DOWN) {
+            throw new IllegalArgumentException(OutputView.MOVING_ERROR>MESSAGE);
+        }
+    }
 }
