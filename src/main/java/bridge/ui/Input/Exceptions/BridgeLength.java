0b10e7c371516407552bf7a42aaca9017d9f6e3b
KSH <tkdgns543@gmail.com>
2022-11-22 19:52:29 +0900

    refactor(input): 다리 길이 범위 숫자 하드코딩 문제 해결, 상수화

--- src/main/java/bridge/ui/Input/Exceptions/BridgeLength.java
+++ src/main/java/bridge/ui/Input/Exceptions/BridgeLength.java
@@ -3,7 +3,11 @@ package bridge.ui.Input.Exceptions;
 import bridge.ui.Input.Repetitor;
 
 public class BridgeLength extends Repetitor {
-    private static final String ERROR_MESSAGE = "[ERROR] 다리 길이는 3부터 20 사이의 숫자여야 합니다.";
+
+    private static final int MINIMUM_LENGTH = 3;
+    private static final int MAXIMUM_LENGTH = 20;
+    private static final String ERROR_MESSAGE =
+            "[ERROR] 다리 길이는 " + MINIMUM_LENGTH + "부터 " + MAXIMUM_LENGTH + " 사이의 숫자여야 합니다.";
 
     @Override
     protected String checkInputException(String input) throws IllegalArgumentException {
@@ -22,7 +26,7 @@ public class BridgeLength extends Repetitor {
     }
 
     private void checkLengthRange(int input) throws IllegalArgumentException{
-        if (!(input >= 3 && input <= 20)) {
+        if (!(input >= MINIMUM_LENGTH && input <= MAXIMUM_LENGTH)) {
             throw new IllegalArgumentException(ERROR_MESSAGE);
         }
     }
