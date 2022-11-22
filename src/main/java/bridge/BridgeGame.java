604cba2972357f538f49bdee77dbcb5ed57a90bf
yangjiyoung <0112jyoung@naver.com>
2022-11-22 18:10:49 +0900

    style: 추가 주석 제거

--- src/main/java/bridge/BridgeGame.java
+++ src/main/java/bridge/BridgeGame.java
@@ -15,12 +15,6 @@ public class BridgeGame {
         move();
     }
 
-    /**
-     * 사용자가 칸을 이동할 때 사용하는 메서드
-     * <p>
-     * 이동을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
-     */
-
     public void prepare() {
         OutputView.printStartGame();
         OutputView.printRequireSize();
@@ -60,12 +54,7 @@ public class BridgeGame {
             return true;
         return false;
     }
-
-    /**
-     * 사용자가 게임을 다시 시도할 때 사용하는 메서드
-     * <p>
-     * 재시작을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
-     */
+    
     public void retry() {
         user.increaseTryCount();
         isSuccess = true;
