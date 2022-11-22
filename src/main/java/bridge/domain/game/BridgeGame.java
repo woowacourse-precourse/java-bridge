e97487e6ebe9cf8dc3055558f657c34807226e11
JuSeong1130 <jsmoon1130@naver.com>
2022-11-22 18:48:59 +0900

    refactor(BridgeGame): move,retry 등 구조 개선

--- src/main/java/bridge/domain/game/BridgeGame.java
+++ src/main/java/bridge/domain/game/BridgeGame.java
@@ -2,7 +2,9 @@ package bridge.domain.game;
 
 import bridge.BridgeMaker;
 import bridge.BridgeRandomNumberGenerator;
+import bridge.domain.command.BridgeMoveCommand;
 import bridge.domain.command.BridgeRestartCommand;
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -10,12 +12,17 @@ import java.util.List;
  */
 public class BridgeGame {
 
-	private static final int END_GAME = Integer.MAX_VALUE;
-	private static final int RETRY_GAME = 0;
+	private static final String SUCCESS = " O ";
+	private static final String FAIL = " X ";
+	private static final String BLANK = "   ";
+	private int countOfAttemps = 1;
+	private boolean isSuccessStage = true;
+	private int stageLevel = 0;
+	private List<String> bridges;
+	private List<List<String>> resultMap;
 
-	public List<String> makeBridge(int size) {
-		BridgeMaker bridgeMaker = new BridgeMaker(new BridgeRandomNumberGenerator());
-		return bridgeMaker.makeBridge(size);
+	public BridgeGame() {
+		resultMapInit();
 	}
 
 	/**
@@ -23,19 +30,97 @@ public class BridgeGame {
 	 * <p>
 	 * 이동을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
 	 */
-	public boolean move(List<String> bridges, String moveCommand, int index) {
-		String bridgeColumn = bridges.get(index);
-		return bridgeColumn.equals(moveCommand);
+	public void move(String moveCommand) {
+		recordTheMovement(moveCommand);
+		if (isFailStage(moveCommand)) {
+			isSuccessStage = false;
+		}
+		stageLevel++;
 	}
+
+	public boolean isFailStage(String moveCommand) {
+		String bridgeColumn = bridges.get(stageLevel);
+		return !bridgeColumn.equals(moveCommand);
+	}
+
 
 	/**
 	 * 사용자가 게임을 다시 시도할 때 사용하는 메서드
 	 * <p>
 	 * 재시작을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
 	 */
-	public int retry(String command) {
-		return BridgeRestartCommand.getRestartCode(command);
+	public void retry(String command) {
+		if (BridgeRestartCommand.isRetry(command)) {
+			resultMapInit();
+			addCountOfAttemps();
+			isSuccessStage = true;
+			stageLevel = 0;
+		}
+	}
+
+	public void recordTheMovement(String moveCommand) {
+		int storeIndex = BridgeMoveCommand.findStoreIndex(moveCommand);
+		addToTheAppropriateRepository(resultMap.get(storeIndex), getKeyword(moveCommand));
+		addAnotherRepository(resultMap.get(Math.abs(storeIndex - 1)), BLANK);
+	}
+
+	public void addToTheAppropriateRepository(List<String> store, String keyword) {
+		store.add(keyword);
+	}
+
+	public void addAnotherRepository(List<String> store, String keyword) {
+		store.add(keyword);
+	}
+
+	public String getKeyword(String command) {
+		String bridge = bridges.get(stageLevel);
+		if (bridge.equals(command)) {
+			return SUCCESS;
+		}
+		return FAIL;
 	}
 
 
+	public boolean canPlay() {
+		return stageLevel < bridges.size() && isSuccessStage;
+	}
+
+	public boolean isEndGame() {
+		return !isSuccessStage;
+	}
+
+	public String isSuccessGame() {
+		if (isSuccessStage && bridges.size() == stageLevel) {
+			return "성공";
+		}
+		return "실패";
+	}
+
+	public void resultMapInit() {
+		int size = BridgeMoveCommand.getSize();
+		resultMap = new ArrayList<>();
+		for (int i = 0; i < size; i++) {
+			resultMap.add(new ArrayList<>());
+		}
+	}
+
+	private void addCountOfAttemps() {
+		countOfAttemps++;
+	}
+
+	public void setBridges(List<String> bridges) {
+		this.bridges = bridges;
+	}
+
+	public int getCountOfAttemps() {
+		return countOfAttemps;
+	}
+
+	public List<List<String>> getResultMap() {
+		return resultMap;
+	}
+
+	public boolean isSuccessStage() {
+		return isSuccessStage;
+	}
 }
