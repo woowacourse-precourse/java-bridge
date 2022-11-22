f6dfa36db9ee5ba52cbe866f84676f909cd17cc6
i960107 <i960107@naver.com>
2022-11-22 17:44:54 +0900

    test(Application): 통합 테스트 케이스 추가

--- src/test/java/bridge/ApplicationTest.java
+++ src/test/java/bridge/ApplicationTest.java
@@ -5,14 +5,20 @@ import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import bridge.domain.Bridge;
+import bridge.domain.BridgeLength;
 import bridge.domain.BridgeMaker;
 import bridge.domain.BridgeMove;
 import bridge.domain.BridgeNumberGenerator;
-import bridge.domain.BridgeLength;
 import camp.nextstep.edu.missionutils.test.NsTest;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Stream;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 class ApplicationTest extends NsTest {
 
@@ -20,7 +26,7 @@ class ApplicationTest extends NsTest {
 
     @Test
     void 다리_생성_테스트() {
-        List<Integer> moveCodes = List.of(1, 0, 0);
+        List<Integer> moveCodes = new ArrayList<>(Arrays.asList(1, 0, 0));
         BridgeNumberGenerator numberGenerator = new TestNumberGenerator(moveCodes);
         BridgeMaker bridgeMaker = new BridgeMaker(numberGenerator);
         Bridge bridge = bridgeMaker.makeBridge(new BridgeLength(3));
@@ -29,28 +35,75 @@ class ApplicationTest extends NsTest {
         assertThat(actualMoveCodes).containsExactly(BridgeMove.UP, BridgeMove.DOWN, BridgeMove.DOWN);
     }
 
-    @Test
-    void 기능_테스트() {
+    @ParameterizedTest(name = "기능테스트:{0}")
+    @MethodSource
+    void 기능_테스트(String[] args, String[] outputs, String upside, String downSide) {
         assertRandomNumberInRangeTest(() -> {
-            run("3", "U", "D", "U");
-            assertThat(output()).contains(
-                    "최종 게임 결과",
-                    "[ O |   | O ]",
-                    "[   | O |   ]",
-                    "게임 성공 여부: 성공",
-                    "총 시도한 횟수: 1"
-            );
+            run(args);
+            assertThat(output()).contains(outputs);
 
-            int upSideIndex = output().indexOf("[ O |   | O ]");
-            int downSideIndex = output().indexOf("[   | O |   ]");
+            int upSideIndex = output().indexOf(upside);
+            int downSideIndex = output().indexOf(downSide);
             assertThat(upSideIndex).isLessThan(downSideIndex);
-        }, 1, 0, 1);
+        }, 1, 0, 1, 1, 0);
     }
 
-    @Test
-    void 예외_테스트() {
+    private static Stream<Arguments> 기능_테스트() {
+        return Stream.of(
+                Arguments.of(
+                        new String[]{"5", "U", "D", "U", "U", "D"},
+                        new String[]{
+                                "최종 게임 결과",
+                                "[ O |   | O | O |   ]",
+                                "[   | O |   |   | O ]",
+                                "게임 성공 여부: 성공",
+                                "총 시도한 횟수: 1"
+                        },
+                        "[ O |   | O | O |   ]",
+                        "[   | O |   |   | O ]"
+                ),
+                Arguments.of(
+                        new String[]{"5", "D", "Q"},
+                        new String[]{
+                                "최종 게임 결과",
+                                "[   ]",
+                                "[ X ]",
+                                "게임 성공 여부: 실패",
+                                "총 시도한 횟수: 1"},
+                        "[   ]",
+                        "[ X ]"
+                ),
+                Arguments.of(
+                        new String[] {"3", "U", "D", "U"},
+                        new String[] {
+                                "최종 게임 결과",
+                                "[ O |   | O ]",
+                                "[   | O |   ]",
+                                "게임 성공 여부: 성공",
+                                "총 시도한 횟수: 1"},
+                        "[ O |   | O ]",
+                        "[   | O |   ]"
+                ),
+                Arguments.of(
+                        new String[] {"5", "D", "R", "U", "D", "D", "Q"},
+                        new String[] {
+                                "최종 게임 결과",
+                                "[ O |   |   ]",
+                                "[   | O | X ]",
+                                "게임 성공 여부: 실패",
+                                "총 시도한 횟수: 2"},
+                        "[ O |   |   ]",
+                        "[   | O | X ]"
+                        )
+        );
+    }
+
+    @ParameterizedTest(name = "예외 테스트{0}")
+    @ValueSource(strings = {"a", "21,2", "3,X"})
+    void 예외_테스트(String arg) {
+        String[] args = arg.split(",");
         assertSimpleTest(() -> {
-            runException("a");
+            runException(args);
             assertThat(output()).contains(ERROR_MESSAGE);
         });
     }
