700d013912df13d9b03eccfda699bf21cd302277
jiwon <ljwon77@daum.net>
2022-11-22 16:00:05 +0900

    test: validate bridge length about correct input
    올바른 다리 길이의 값이 들어왔을 때 오류가 발생하지 않는지 테스트를 진행하였습니다.
    특히 경계값이 들어왔을 때 예외가 발생하지 않는지 체크하였습니다.

--- src/test/java/bridge/util/ValidationUtilTest.java
+++ src/test/java/bridge/util/ValidationUtilTest.java
@@ -9,6 +9,7 @@ import org.junit.jupiter.params.provider.ValueSource;
 import static bridge.constant.BridgeRange.MAX_LENGTH;
 import static bridge.constant.BridgeRange.MIN_LENGTH;
 import static bridge.constant.message.ExceptionMessage.BRIDGE_LENGTH_RANGE;
+import static org.assertj.core.api.Assertions.*;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class ValidationUtilTest {
@@ -16,9 +17,10 @@ class ValidationUtilTest {
     @DisplayName("다리 길이로 잘못된 타입이 들어왔을 때 예외를 터트려야 한다.")
     @ParameterizedTest
     @ValueSource(strings = {"3a", "a33", "abc", "@#$#("})
-    void 다리_길이_타입_검증_테스트(String input) throws Exception {
+    void 다리_길이_타입_검증_테스트(String input) {
         //given
         ValidationUtil validationUtil = new ValidationUtil();
+
         //when, then
         assertThatThrownBy(() ->
                 validationUtil.validateBridgeLength(input))
@@ -29,15 +31,29 @@ class ValidationUtilTest {
     @DisplayName("잘못된 범위의 값이 들어왔을 때 예외를 터트려야 한다.")
     @ParameterizedTest
     @ValueSource(strings = {"21", "2", "100"})
-    void 다리_길이_범위_검증_테스트(String input) throws Exception {
+    void 다리_길이_범위_검증_테스트(String input) {
         //given
         ValidationUtil validationUtil = new ValidationUtil();
+
         //when, then
         assertThatThrownBy(() ->
                 validationUtil.validateBridgeLength(input))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining(String.format(BRIDGE_LENGTH_RANGE.getMessage(),
                         MIN_LENGTH.getNumber(), MAX_LENGTH.getNumber()));
+    }
+
+    @DisplayName("다리 길이로 3~20 사이의 숫자값이 들어오면 통과해야 한다.")
+    @ParameterizedTest
+    @ValueSource(strings = {"3", "20", "15", "8"})
+    void 다리_길이_올바른_값_검증_테스트(String input) {
+        //given
+        ValidationUtil validationUtil = new ValidationUtil();
+
+        //when, then
+        assertThatCode(() ->
+                        validationUtil.validateBridgeLength(input))
+                .doesNotThrowAnyException();
     }
 }
 
