bb6d1e6bd8da148ba72632816cdb7997b867850b
wonseokjee <wsji9404@gmail.com>
2022-11-22 22:59:23 +0900

    Test(BridgeGameTest): 추가 makeBridgeTest, moveTest

--- src/test/java/bridge/BridgeGameTest.java
+++ src/test/java/bridge/BridgeGameTest.java
@@ -1,28 +1,38 @@
 package bridge;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.*;
+
 
 class BridgeGameTest {
 
     @Test
-    void makeBridge() {
+    public void makeBridgeTest() {
+        //given
+        BridgeGame bridgeGame = new BridgeGame();
+        int bridgeSize = 20;
+
+        //when
+        List<String> bridge = bridgeGame.makeBridge(bridgeSize);
+
+        //then
+        assertThat(bridge.size()).isEqualTo(bridgeSize);
+        assertThat(bridge.contains("A")).isEqualTo(false);
+        assertThat(bridge.contains("U")).isEqualTo(true);
+        assertThat(bridge.contains("D")).isEqualTo(true);
     }
 
     @Test
-    void move() {
+    void moveTest() {
         BridgeGame bridgeGame = new BridgeGame();
         /*given*/
-        String moveTest = "[U]";
+        String moveTest = "[ O ]" +"\n"+"[   ]";
         /*when*/
         String moveActual = bridgeGame.move(List.of("U","U"),0,"U");
         /*then*/
-    }
-
-    @Test
-    void retry() {
+        assertThat(moveActual).isEqualTo(moveTest);
     }
 }
