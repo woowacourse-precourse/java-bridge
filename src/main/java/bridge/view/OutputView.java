8112a22f7228c162460957d500079e7ff7baa134
TeddyYeoni <po99027@naver.com>
2022-11-22 20:22:40 +0900

    feat: outputView 상수 선언 및 메서드 생성

--- src/main/java/bridge/view/OutputView.java
+++ src/main/java/bridge/view/OutputView.java
@@ -4,7 +4,14 @@ package bridge.view;
  * 사용자에게 게임 진행 상황과 결과를 출력하는 역할을 한다.
  */
 public class OutputView {
-
+	private static final String START_MESSAGE = "다리 건너기 게임을 시작합니다.";
+	private static final String BRIDGE_SIZE_MESSAGE = "다리의 길이를 입력해주세요.";
+	private static final String MOVING_MESSAGE = "이동할 칸을 선택해주세요. (위: U, 아래: D)";
+	private static final String GAME_COMMAND_MESSAGE = "게임을 다시 시도할지 여부를 입력해주세요. (재시도:R, 종료: Q)";
+	private static final String FINAL_GAME_RESULT_MESSAGE = "최종 게임 결과";
+	private static final String GAME_SUCCESS_MEESAGE = "게임 성공 여부: 성공";
+	private static final String GAME_FAILURE_MEESAGE = "게임 성공 여부: 실패";
+	private static final String NUMBER_OF_ATTEMPTS_MESSAGE = "총 시도한 횟수: ";
     /**
      * 현재까지 이동한 다리의 상태를 정해진 형식에 맞춰 출력한다.
      * <p>
@@ -19,5 +26,23 @@ public class OutputView {
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
     public void printResult() {
+    }
+    
+    public void printStart() {
+    }
+    
+    public void printBridgeSize() {
+    }
+    
+    public void printMoving() {
+    }
+    
+    public void printGameCommand() {
+    }
+    
+    public void lowerBridge () {
+    }
+    
+    public void upperBridge () {
     }
 }
