8b7177dbf970d76b650e8e8aa4640b8cc66280dc
raycho <raycho521@yonsei.ac.kr>
2022-11-22 20:25:39 +0900

    feat(BridgeGame.java): add play, move method for BridgeGame.java
    2021.11.22 by Ray

--- src/main/java/bridge/BridgeGame.java
+++ src/main/java/bridge/BridgeGame.java
@@ -1,16 +1,45 @@
 package bridge;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * 다리 건너기 게임을 관리하는 클래스
  */
 public class BridgeGame {
+    private final InputView inputView;
+    private final OutputView outputView;
+
+    public BridgeGame(InputView inputView, OutputView outputView) {
+        this.inputView = inputView;
+        this.outputView = outputView;
+    }
+
+    /**
+     * 1회 플레이 시작, 게임 성공여부 반환
+     */
+    public boolean play(List<String> bridge) {
+        //사용자의 이동 기록을 담을 리스트 생성
+        List<String> userPosition = new ArrayList<>();
+
+        //move를 실행한다. true이면 게임을 계속한다.
+        while (move(bridge, userPosition)) {
+            if (userPosition.equals(bridge))
+                return true;
+        }
+        return false;
+    }
 
     /**
      * 사용자가 칸을 이동할 때 사용하는 메서드
      * <p>
      * 이동을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void move() {
+    public boolean move(List<String> bridge, List<String> userPosition) {
+        String move = inputView.readMoving();
+        userPosition.add(move);
+        outputView.printMap(bridge,userPosition);
+        return isCorrectStep(bridge, userPosition);
     }
 
     public boolean isCorrectStep(List<String> bridge, List<String> userPosition) {
@@ -27,6 +56,13 @@ public class BridgeGame {
      * <p>
      * 재시작을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void retry() {
+    public boolean retry() {
+        String command = inputView.readGameCommand();
+        // 비교하여 t/f 반환하기
+        if (command.equals("R"))
+            return true;
+        if (command.equals("Q"))
+            return false;
+        throw new IllegalArgumentException("[ERROR] 재시도 여부는 (재시도: R, 종료: Q)의 값이어야 합니다.");
     }
 }
