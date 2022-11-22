57f62a2deb86aacf051476c890561d419abb300d
Ran <lan4250@naver.com>
2022-11-22 14:48:00 +0900

    FEAT/ 실패하는 경우 출력

--- src/main/java/bridge/BridgeMaker.java
+++ src/main/java/bridge/BridgeMaker.java
@@ -9,6 +9,9 @@ import java.util.List;
  */
 public class BridgeMaker {
 
+
+    private static String bridgeStr = "";
+
     private final BridgeNumberGenerator bridgeNumberGenerator;
 
     public BridgeMaker(BridgeNumberGenerator bridgeNumberGenerator) {
@@ -22,66 +25,83 @@ public class BridgeMaker {
      */
     public List<String> makeBridge(int size) {
 
-        List<String> bridge = new ArrayList<>();
-
         if (size < 3 || size > 20) {
             throw new IllegalArgumentException("[ERROR] 3 이상 20 이하의 숫자만 입력 가능합니다.");
-
         }
 
-        String str = "";
+        bridgeStr = "";     // 초기화
 
         for (int i = 0; i < size; i++) {
-            str += bridgeNumberGenerator.generate();
+            bridgeStr += bridgeNumberGenerator.generate();
         }
-        System.out.println(" 다리 " + str);
+        System.out.println(" 다리 " + bridgeStr);
 
-        int[][] arr = new int[2][str.length()];
-        for (int i = 0; i < str.length(); i++) {
-            if (str.charAt(i) == '0') {
+        int[][] arr = new int[2][bridgeStr.length()];
+        for (int i = 0; i < bridgeStr.length(); i++) {
+            if (bridgeStr.charAt(i) == '0') {
                 arr[1][i] = 1;
-            } else if (str.charAt(i) == '1') {
+            } else if (bridgeStr.charAt(i) == '1') {
                 arr[0][i] = 1;
             }
         }
 
+        System.out.println("BridgeMaker.makeBridge() arr 타입");
         for (int[] a : arr) {
             System.out.println(Arrays.toString(a));
         }
 
+        return change(bridgeStr);
+    }
 
-        return change(str);
+    // 10110 같은 형태를 가지고 사용자가 하나씩 입력하는 값과 비교해준다.
+    public String bridgeStr() {
+
+        return bridgeStr;
     }
 
 
-    public static List<String> change(String bridgeStr) {
+    // String 00111010101  => List<String> 형태로 바꾼다.
+    // 10101x 이면?
+    public  List<String> change(String inputBridgeStr) {
 
         List<String> result = new ArrayList<>();
         String up = "[";
         String down = "[";
 
-        for (int i = 0; i < bridgeStr.length(); i++) {
+        for (int i = 0; i < inputBridgeStr.length(); i++) {
 
-            if (bridgeStr.charAt(i) == '1') {
 
-                if (i == bridgeStr.length() - 1) {
+            if (inputBridgeStr.charAt(i) == '1') {
+
+                if (i == inputBridgeStr.length() - 1) {
                     up += " O ";
                     down += "   ";
                     continue;
                 }
                 up += " O |";
                 down += "   |";
-            } else if (bridgeStr.charAt(i) == '0') {
 
-                if (i == bridgeStr.length() - 1) {
+            } else if (inputBridgeStr.charAt(i) == '0') {
+
+                if (i == inputBridgeStr.length() - 1) {
                     down += " O ";
                     up += "   ";
                     continue;
                 }
                 down += " O |";
                 up += "   |";
+
+            } else if (inputBridgeStr.charAt(i) == 'x') {        // x, y => 매치 되지 않는 상황인 경우
+                down += " X ";
+                up += "   ";
+                break;      //여기서 더이상 다리 만들기를 끝내야한다.
+
+            } else if (inputBridgeStr.charAt(i) == 'y') {       // 밑에가 x인 경우
+                    down += "   ";
+                    up += " X ";
+                    break;      //여기서 더이상 다리 만들기를 끝내야한다.
+                }
             }
-        }
 
         up += "]";
         down += "]";
