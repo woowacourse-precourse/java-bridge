77fd4c0a4920755e6da0ca0efe7ba6348e9af7e6
DavinAn <blueapple1030@gmail.com>
2022-11-22 22:46:01 +0900

    test : add setup and tearDown to BridgeGameTest, handles console input and output

--- src/test/java/bridge/BridgeGameTest.java
+++ src/test/java/bridge/BridgeGameTest.java
@@ -1,4 +1,29 @@
 package bridge;
 
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+
+import java.io.ByteArrayOutputStream;
+import java.io.InputStream;
+import java.io.PrintStream;
+
 public class BridgeGameTest {
+
+    private final InputStream standardIn = System.in;
+    private final PrintStream standardOut = System.out;
+    private final ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+    private final PrintStream printStream = new PrintStream(outputStream);
+
+
+    @BeforeEach
+    void setUp() {
+        System.setOut(printStream);
+
+    }
+
+    @AfterEach
+    void tearDown() {
+        System.setIn(standardIn);
+        System.setOut(standardOut);
+    }
 }
