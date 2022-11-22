9dc256d794c0e8d058f6e25e494ef04215b26b21
조미정 <ing06164@naver.com>
2022-11-22 23:50:32 +0900

    feat: 브릿지 만들기 - makingBridge()

--- src/main/java/bridge/BridgeGame.java
+++ src/main/java/bridge/BridgeGame.java
@@ -1,10 +1,28 @@
 package bridge;
 
+import java.util.List;
+
 /**
  * 다리 건너기 게임을 관리하는 클래스
  */
 public class BridgeGame {
+    private InputView inputView = new InputView();
+    private List<String> bridgeList ;
+    private int count;          // 시도 횟수
 
+    public List<String> makingBridge(){
+        int bridgeSize = inputView.readBridgeSize();
+
+        BridgeNumberGenerator bridgeNumberGenerator = new BridgeRandomNumberGenerator();
+        BridgeMaker bridgeMaker = new BridgeMaker(bridgeNumberGenerator);
+
+        return bridgeMaker.makeBridge(bridgeSize);
+    }
+
+    public void printMove(boolean isPossibleMove){
+        OutputView outputView = new OutputView();
+        outputView.printMap();
+    }
     /**
      * 사용자가 칸을 이동할 때 사용하는 메서드
      * <p>
