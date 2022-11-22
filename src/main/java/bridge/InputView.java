bed8c4c84eab69cb388f68bd5e0ed079376481ad
SeongchanCho <wordnumperiod@gmail.com>
2022-11-22 20:16:05 +0900

    feat(InputView.java): 콘솔을 통해 사용자로부터 값을 입력 받는 기능 구현 완료

--- src/main/java/bridge/InputView.java
+++ src/main/java/bridge/InputView.java
@@ -1,5 +1,7 @@
 package bridge;
 
+import camp.nextstep.edu.missionutils.Console;
+
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
  */
@@ -8,21 +10,40 @@ public class InputView {
     /**
      * 다리의 길이를 입력받는다.
      */
-    public int readBridgeSize() {
-        return 0;
+    public int readBridgeSize() throws IllegalArgumentException {
+        System.out.println("다리의 길이를 입력해주세요.");
+        return getIntegerFromConsole("[ERROR] 다리의 길이를 다시 입력해주세요.");
+    }
+
+    private int getIntegerFromConsole(String errorMessage) {
+        try {
+            return Integer.parseInt(Console.readLine());
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException(errorMessage);
+        }
     }
 
     /**
      * 사용자가 이동할 칸을 입력받는다.
      */
-    public String readMoving() {
-        return null;
+    public String readMoving() throws IllegalArgumentException {
+        System.out.println("이동할 칸을 선택해주세요. (위: U, 아래: D");
+        String direction = Console.readLine();
+        if (direction.equals("D") || direction.equals("U")) {
+            return direction;
+        }
+        throw new IllegalArgumentException("[ERROR] 올바른 이동 칸을 선택해주세요.");
     }
 
     /**
      * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
      */
-    public String readGameCommand() {
-        return null;
+    public String readGameCommand() throws IllegalArgumentException {
+        System.out.println("게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q");
+        String choice = Console.readLine();
+        if (choice.equals("R") || choice.equals("Q")) {
+            return choice;
+        }
+        throw new IllegalArgumentException("[ERROR] 올바른 명령값을 입력해주세요.");
     }
 }
