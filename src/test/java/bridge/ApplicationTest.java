63e376bd580a80cf525b3bb11154e5979c52df4c
Dan-HeeYoung9182 <ldan9182@gmail.com>
2022-11-22 10:20:21 +0900

    성공했을때 최종 게임 결과 메시지 출력 테스트

--- src/test/java/bridge/ApplicationTest.java
+++ src/test/java/bridge/ApplicationTest.java
@@ -61,6 +61,21 @@ class ApplicationTest extends NsTest {
         }, 0, 0, 1, 1, 1);
     }
 
+    @Test
+    void 게임성공_메세지_시도한_횟수_출력_테스트() {
+        assertRandomNumberInRangeTest(() -> {
+            run("3", "D", "D", "D", "R", "U", "U", "D", "R", "D", "D", "R", "D", "U", "D");
+            assertThat(output()).contains(
+                    "최종 게임 결과",
+                    "게임 성공 여부: 성공",
+                    "총 시도한 횟수: 4"
+            );
+        }, 0, 0, 1,
+                1, 1, 1,
+                0, 1, 0,
+                0, 1, 0);
+    }
+
     @Override
     protected void runMain() {
         Application.main(new String[]{});
