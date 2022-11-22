bb4e2e449bad264efb28f864598018c713446156
taekwon-dev <taekwon_dev@naver.com>
2022-11-22 11:49:50 +0900

    refactor(BridgeGame, BridgeMaker, InputView, OutputView): 주석 제거

--- src/main/java/bridge/domain/BridgeGame.java
+++ src/main/java/bridge/domain/BridgeGame.java
@@ -4,9 +4,6 @@ import java.util.List;
 
 import static bridge.domain.Constants.*;
 
-/**
- * 다리 건너기 게임을 관리하는 클래스
- */
 public class BridgeGame {
 
     private List<String> bridge;
@@ -19,11 +16,6 @@ public class BridgeGame {
         this.bridgeGameState = bridgeGameState;
     }
 
-    /**
-     * 사용자가 칸을 이동할 때 사용하는 메서드
-     * <p>
-     * 이동을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
-     */
     public void move(String moving) {
         bridgeState.mark(makeBridgeStatus(moving));
         updateGameResult(moving);
@@ -49,11 +41,6 @@ public class BridgeGame {
         return BridgeStatus.valueOfArrowAndMark(moving, X);
     }
 
-    /**
-     * 사용자가 게임을 다시 시도할 때 사용하는 메서드
-     * <p>
-     * 재시작을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
-     */
     public boolean retry(String command) {
         if (command.equals(RETRY)) {
             bridgeGameState.initGameRound();
--- src/main/java/bridge/view/input/InputView.java
+++ src/main/java/bridge/view/input/InputView.java
@@ -5,14 +5,8 @@ import camp.nextstep.edu.missionutils.Console;
 import static bridge.domain.Constants.*;
 import static bridge.domain.exception.ErrorMessage.*;
 
-/**
- * 사용자로부터 입력을 받는 역할을 한다.
- */
 public class InputView {
 
-    /**
-     * 다리의 길이를 입력받는다.
-     */
     public int readBridgeSize() {
         int bridgeSize = parseBridgeSizeToInt(Console.readLine());
         validateBrideSize(bridgeSize);
@@ -33,9 +27,6 @@ public class InputView {
         }
     }
 
-    /**
-     * 사용자가 이동할 칸을 입력받는다.
-     */
     public String readMoving() {
         String moving = Console.readLine();
         validateBridgeMoving(moving);
@@ -48,9 +39,6 @@ public class InputView {
         }
     }
 
-    /**
-     * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
-     */
     public String readGameCommand() {
         String command = Console.readLine();
         validateGameCommnd(command);
