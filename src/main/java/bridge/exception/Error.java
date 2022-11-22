47ed9164bb12872f4a0a24b4b9123d6eebc05e6c
im-shung <seoyoung1113@gmail.com>
2022-11-22 13:28:09 +0900

    feat: :sparkles: R(재시작)과 Q(종료)가 아닌 값을 입력할 경우의 에러 메시지를 가진 WRONG_GAME_COMMAND 필드 추가

--- src/main/java/bridge/exception/Error.java
+++ src/main/java/bridge/exception/Error.java
@@ -1,12 +1,19 @@
 package bridge.exception;
 
 import bridge.domain.BridgeLength;
+import bridge.domain.GameCommand;
 import bridge.domain.Moving;
 
 public enum Error {
     NOT_INT("숫자를 입력하세요."),
     WRONG_BRIDGE_LENGTH("다리 길이는 " + BridgeLength.MIN + "부터 " + BridgeLength.MAX + " 사이의 숫자여야 합니다."),
-    WRONG_MOVING("이동할 수 있는 칸은 " + Moving.UP.getExpression() + "(" + Moving.UP.getDescription() +")" + "와 " + Moving.DOWN.getExpression() + "(" + Moving.DOWN.getDescription() +")" +"여야 합니다.");
+    WRONG_MOVING("이동할 수 있는 칸은 " + Moving.UP.getExpression() + "(" + Moving.UP.getDescription() + ")"
+                         + "와 " + Moving.DOWN.getExpression() + "(" + Moving.DOWN.getDescription()
+                         + ")" + "여야 합니다."),
+    WRONG_GAME_COMMAND("게임 재시작/종료 여부는 " + GameCommand.RETRY.getExpression() + "("
+                               + GameCommand.RETRY.getDescription() + ")" + "와 "
+                               + GameCommand.QUIT.getExpression() + "("
+                               + GameCommand.QUIT.getDescription() + ")" + "여야 합니다.");
 
     private final String ERROR = "[ERROR] ";
     private final String message;
