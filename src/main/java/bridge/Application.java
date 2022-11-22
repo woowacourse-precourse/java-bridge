b8ba3036a0b03a69dc5810c3d4f55ab2162660db
HyeonjungYun <guswnd9971@gmail.com>
2022-11-22 15:45:19 +0900

    "refactor(BridgeGame): 기존 Application 기능 Connector로 이관"

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -1,54 +1,8 @@
 package bridge;
 
-import bridge.domain.BridgeGame;
-import bridge.domain.BridgeMaker;
-import bridge.domain.BridgeRandomNumberGenerator;
-import bridge.view.InputView;
-import bridge.view.OutputView;
-
-import java.util.List;
-
 public class Application {
-
-    private static boolean gameClear;
-    private static int attempt = 1;
-
-
     public static void main(String[] args) {
-        InputView input = new InputView();
-        OutputView output = new OutputView();
-        BridgeMaker randomBridge = new BridgeMaker(new BridgeRandomNumberGenerator());
-        output.printStart();
-        BridgeGame bridgeGame = new BridgeGame(randomBridge.makeBridge(input.readBridgeSize()));
-
-        playingBridge(input, output, bridgeGame);
-    }
-
-    private static void playingBridge (InputView input, OutputView output, BridgeGame bridgeGame)  {
-
-        while (bridgeGame.move(input.readMoving())) {
-
-            if (!bridgeGame.getPassHistory().contains("UX") && !bridgeGame.getPassHistory().contains("DX")) {
-                output.printMap(bridgeGame.getPassHistory());
-            }
-        }
-        output.printResult(attempt, gameClear, bridgeGame.getPassHistory());
-    }
-
-    public boolean selectRetry (List<String> passHistory) {
-        OutputView output = new OutputView();
-        InputView input = new InputView();
-
-        output.printMap(passHistory);
-
-        return input.readGameCommand().equals("R");
-    }
-
-    public void setGameClear (boolean gameClear) {
-        this.gameClear = gameClear;
-    }
-
-    public void setAttempt (int attempt) {
-        this.attempt += attempt;
+        Connector connector = new Connector();
+        connector.playBridge();
     }
 }
