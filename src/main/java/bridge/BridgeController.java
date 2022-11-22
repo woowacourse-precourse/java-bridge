209a6573ce031caf0690edbcc2e6899ab77a757a
mng990 <ryoo0504@pusan.ac.kr>
2022-11-22 18:37:28 +0900

    feat(main/java/bridge/BridgeController.java): BridgeController.java 작성
    Bridge Game의 도메인 로직
    BridgeView 및 BridgeGame 조작
    
    members:
        - bridgeGame
        - bridgeView
    methods:
        - getSize()
        - getMove()
        - getCommand()
        - quitGame()
        - retryGame()
        - dead()
        # makeBridge()
        + initGame()
        + playGame()
        + BridgeController()

---
+++ src/main/java/bridge/BridgeController.java
@@ -0,0 +1,91 @@
+package bridge;
+
+import static bridge.Command.QUIT;
+
+import java.util.List;
+
+public class BridgeController extends BridgeMaker{
+
+    private static BridgeGame model;
+    private static BridgeView view;
+    private static ExceptionHandler exc;
+
+    public void initGame(){
+        view.printStartMessage();
+        int size = getSize();
+        model = new BridgeGame(makeBridge(size));
+    }
+
+    private int getSize(){
+        String size;
+        try{
+            size = exc.validBridgeSize(view.sizeIO());
+        }
+        catch (IllegalArgumentException e){
+            size = exc.reEnter(view, e);
+        }
+        return Integer.parseInt(size);
+    }
+
+    private String getCommand(){
+        String command;
+        try{
+            command = exc.validGameCommand(view.commandIO());
+        }
+        catch (IllegalArgumentException e){
+            command = exc.reEnter(view, e);
+        }
+        return command;
+    }
+
+    public void playGame(boolean play){
+        if(model.isClear()){ play = quitGame(State.SAFE); }
+
+        if(!play) return;
+
+        State move = getMove();
+        view.printMap(model.history(), move);
+
+        if(!move.ALIVE()){ play = dead(State.FAIL); }
+
+        playGame(play);
+    }
+
+    private State getMove(){
+        String nextMove;
+        try{
+            nextMove = exc.validMoving(view.moveIO());
+        }
+        catch (IllegalArgumentException e){
+            nextMove = exc.reEnter(view, e);
+        }
+        return model.move(Glass.glassOf(nextMove));
+    }
+
+    private boolean quitGame(State state){
+        view.printResult(model.history(), state, model.playTime());
+        return false;
+    }
+
+    private boolean retryGame(){
+        model.retry();
+        return true;
+    }
+
+    private boolean dead(State state){
+        String command = getCommand();
+        if(command.equals(QUIT.KEY())){
+            return quitGame(state);
+        }
+        return retryGame();
+    }
+
+
+    public BridgeController(BridgeNumberGenerator bridgeNumberGenerator) {
+        super(bridgeNumberGenerator);
+
+        model = new BridgeGame(List.of());
+        view = new BridgeView();
+        exc = new ExceptionHandler();
+    }
+}
