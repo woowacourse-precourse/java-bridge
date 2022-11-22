209171e47519de0639db59d5b772069f5f628422
qpdh <74190329+qpdh@users.noreply.github.com>
2022-11-22 15:10:52 +0900

    Refactor($ValidateInputView) InputView에서 입력값 검증 기능 분리

---
+++ src/main/java/bridge/ValidateInputView.java
@@ -0,0 +1,50 @@
+package bridge;
+
+public class ValidateInputView {
+    /**
+     * 다리의 길이가 적절한 값인지 판단한다.
+     * 예외 발생시 IllegalArgumentException을 발생시킨다.
+     *
+     * @param userInput 사용자가 입력한 문자열
+     * @return <p>[3,20] - 올바른 값
+     * <p>-1 - 올바르지 않은 값
+     */
+    public int validateBridgeSize(String userInput) {
+        try {
+            int number = Integer.parseInt(userInput);
+            if (3 <= number && number <= 20) return number;
+            System.out.println("[ERROR] 다리 크기는 3이상 20이하의 자연수여야 합니다.");
+        } catch (NumberFormatException e) {
+            System.out.println("[ERROR] 다리 크기는 정수여야 합니다.");
+        }
+        throw new IllegalArgumentException();
+    }
+
+
+    /**
+     * 이동할 칸의 입력이 적절한지 판단한다.
+     * 예외 발생시 IllegalArgumentException을 발생시킨다.
+     *
+     * @param userInput 사용자가 입력한 문자열
+     * @return <p>[U,D] - 올바른 값
+     * <p>-1 - 올바르지 않은 값
+     */
+    public void validateMove(String userInput) {
+        if (userInput.equals("U") || userInput.equals("D")) return;
+        System.out.println("[ERROR] 입력 값은 \"U\" 또는 \"D\"여야 합니다.");
+        throw new IllegalArgumentException();
+    }
+
+    /**
+     * 다시 시도 입력값이 적절한지 판단한다.
+     * 예외 발생시 IllegalArgumentException을 발생시킨다.
+     *
+     * @param userInput 사용자가 입력한 문자열
+     * @return [R, Q] - 올바른 값
+     */
+    public void validateGameCommand(String userInput) {
+        if (userInput.equals("R") || userInput.equals("Q")) return;
+        System.out.println("[ERROR] 입력 값은 \"R\" 또는 \"Q\"여야 합니다.");
+        throw new IllegalArgumentException();
+    }
+}
