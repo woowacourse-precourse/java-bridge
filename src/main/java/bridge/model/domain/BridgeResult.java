ed641ef9ce7e40a9e9b014f22689a6ce317cf01b
endlessmomo <soohyuk96@naver.com>
2022-11-22 10:13:08 +0900

    refactoring(BridgeResult) : 메소드 수정 - 추가적으로 불필요한 import문 제거 및 자동 줄 완성

--- src/main/java/bridge/model/domain/BridgeResult.java
+++ src/main/java/bridge/model/domain/BridgeResult.java
@@ -1,31 +1,45 @@
 package bridge.model.domain;
 
-import java.util.List;
-
+import static bridge.model.constnce.Text.FAIL;
 import static bridge.model.constnce.Text.SUCCESS;
 
 public class BridgeResult {
     private String result;
     private int attempt;
+    private boolean isClear;
 
-    public BridgeResult(){
-        this.result = "실패";
-        this.attempt = 1;
+    public BridgeResult() {
+        this.result = SUCCESS;
+        this.attempt = 0;
+        this.isClear = true;
+    }
+
+    public void failedRound() {
+        isClear = false;
+    }
+
+    public void init() {
+        isClear = true;
+        result = SUCCESS;
     }
 
     public void addAttempt() {
         attempt += 1;
     }
 
-    public void isAllRoundClear(){
-        result = "성공";
+    public void failedGame() {
+        result = FAIL;
     }
 
-    public String getResult(){
+    public boolean getIsClear() {
+        return isClear;
+    }
+
+    public String getResult() {
         return result;
     }
 
-    public int getAttempt(){
+    public int getAttempt() {
         return attempt;
     }
 }
