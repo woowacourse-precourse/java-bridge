59a520ab1937f661eecb915167f151ad8085ec6a
Hyun-Seo Oh <ohseo8813@gmail.com>
2022-11-22 11:54:45 +0900

    <refactor>(OutputView.java): MapGenerator를 OutputView에 통합

--- src/main/java/bridge/view/OutputView.java
+++ src/main/java/bridge/view/OutputView.java
@@ -1,27 +1,36 @@
 package bridge.view;
 
+import static bridge.domain.vo.MoveCommand.MOVE_TO_LOWER_BLOCK;
+import static bridge.domain.vo.MoveCommand.MOVE_TO_UPPER_BLOCK;
+
 import bridge.dto.GameResult;
 import bridge.dto.MoveResult;
+import java.util.ArrayList;
+import java.util.List;
 
 public class OutputView {
 
     private static final String GAME_SUCCESS = "성공";
     private static final String GAME_FAIL = "실패";
+    private static final String ANSWER_IS_RIGHT = "O";
+    private static final String ANSWER_IS_WRONG = "X";
+    private static final String BLANK = " ";
+    private static final String ROUND_SEPARATE_MARK = " | ";
 
-    private final MapGenerator mapGenerator = new MapGenerator();
+    private final List<String> upperBlocks = new ArrayList<>();
+    private final List<String> lowerBlocks = new ArrayList<>();
+
 
     public void printMap(MoveResult moveResult) {
-        mapGenerator.addCurrentRoundMap(moveResult);
+        String resultMark = getResultMark(moveResult.isSuccess());
+        addCurrentRoundMap(moveResult.getMessage(), resultMark);
         printJoiningMap();
     }
 
-    private void printJoiningMap() {
-        System.out.printf("[ %s ]\n", mapGenerator.getUpperMap());
-        System.out.printf("[ %s ]\n", mapGenerator.getLowerMap());
-    }
 
     public void clearMap() {
-        mapGenerator.clear();
+        upperBlocks.clear();
+        lowerBlocks.clear();
     }
 
     public void printResult(GameResult gameResult) {
@@ -29,6 +38,29 @@ public class OutputView {
         printJoiningMap();
         System.out.printf("게임 성공 여부: %s\n", getGameClearResultMessage(gameResult));
         System.out.printf("총 시도한 횟수: %s\n", gameResult.getTryCount());
+    }
+
+    private void addCurrentRoundMap(String message, String resultMark) {
+        if (message.equals(MOVE_TO_UPPER_BLOCK)) {
+            upperBlocks.add(resultMark);
+            lowerBlocks.add(BLANK);
+        }
+        if (message.equals(MOVE_TO_LOWER_BLOCK)) {
+            upperBlocks.add(BLANK);
+            lowerBlocks.add(resultMark);
+        }
+    }
+
+    private String getResultMark(boolean success) {
+        if (success) {
+            return ANSWER_IS_RIGHT;
+        }
+        return ANSWER_IS_WRONG;
+    }
+
+    private void printJoiningMap() {
+        System.out.printf("[ %s ]\n", String.join(ROUND_SEPARATE_MARK, upperBlocks));
+        System.out.printf("[ %s ]\n", String.join(ROUND_SEPARATE_MARK, lowerBlocks));
     }
 
     private String getGameClearResultMessage(GameResult gameResult) {
