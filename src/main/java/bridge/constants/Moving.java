0ea13b0bfa2ecb9895bf6d211ad71adfa6068df5
aak2075 <35948985+aak2075@users.noreply.github.com>
2022-11-22 16:10:57 +0900

    refactor(bridge) : 하드코딩된 문자열 상수로 변경

--- src/main/java/bridge/constants/Moving.java
+++ src/main/java/bridge/constants/Moving.java
@@ -10,12 +10,14 @@ public enum Moving {
         this.number = number;
     }
 
+    private static final String NUMBER_ERROR = "[ERROR] 숫자 생성은 0과 1만 가능합니다";
     final int number;
+
 
     public static Moving getMovingFromNumber(int number) {
         return Arrays.stream(Moving.values())
                 .filter(move -> move.number == number)
                 .findAny()
-                .orElseThrow(() -> new IllegalArgumentException("[ERROR] 숫자 생성은 0과 1만 가능합니다"));
+                .orElseThrow(() -> new IllegalArgumentException(NUMBER_ERROR));
     }
 }
