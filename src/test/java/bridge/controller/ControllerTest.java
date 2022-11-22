7de17c5b2a5debdf86298f0f67d5622b38ad3e53
dooboocookie <bkh3030@gmail.com>
2022-11-22 14:13:14 +0900

    test: 게임 시작 테스트

--- src/test/java/bridge/controller/ControllerTest.java
+++ src/test/java/bridge/controller/ControllerTest.java
@@ -1,26 +1,49 @@
 package bridge.controller;
 
+import bridge.model.BridgeGame;
+import bridge.view.InputView;
+import bridge.view.OutputView;
 import camp.nextstep.edu.missionutils.Randoms;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.mockito.MockedStatic;
 import org.mockito.Mockito;
 
-public class ControllerTest {
+import java.io.*;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+@DisplayName("컨트롤러 메소드를 실행하는 통합 테스트")
+public class ControllerTest {
+    static BridgeGameController bridgeGameController;
     static MockedStatic<Randoms> randomsMockedStatic;
+    static OutputStream out;
+
 
     @BeforeAll
     static void beforeAll() {
         randomsMockedStatic = Mockito.mockStatic(Randoms.class);
         Mockito.when(Randoms.pickNumberInRange(0,1)).thenReturn(1,0,1);
+
+        InputView inputView = new InputView();
+        OutputView outputView = new OutputView();
+        BridgeGame bridgeGame = new BridgeGame();
+        bridgeGameController = new BridgeGameController(inputView, outputView, bridgeGame);
+        out = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(out));
     }
 
     @DisplayName("게임 시작 시 출력 테스트")
     @Test
     void startNewGameTest() {
+        bridgeGameController.startNewGame();
 
+        String expectedOutput = "다리 건너기 게임을 시작합니다.";
+
+        assertThat(out.toString().trim()).isEqualTo(expectedOutput);
     }
 
     @DisplayName("새로운 다리 만들기 테스트")
