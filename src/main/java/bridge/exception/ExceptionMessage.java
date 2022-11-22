a8fe5b5a5d98df09cb0d4111db46acc2f18648b5
Song kyeong seok <skj0922@kookmin.ac.kr>
2022-11-22 21:59:49 +0900

    chore(ExceptionMessage): 에러 메세지 수정

--- src/main/java/bridge/exception/ExceptionMessage.java
+++ src/main/java/bridge/exception/ExceptionMessage.java
@@ -3,7 +3,7 @@ package bridge.exception;
 public enum ExceptionMessage {
     ERROR_MESSAGE_PREFIX("[ERROR] "),
     BRIDGE_SIZE_IS_NULL_ERROR("다리의 길이를 입력 해주세요."),
-    BRIDGE_SIZE_IS_NOT_NUMBER_ERROR("다리의 길이는 숫자만 입력 가능합니다."),
+    BRIDGE_SIZE_IS_NOT_NUMBER_ERROR("다리의 길이는 정수만 입력 가능합니다."),
     BRIDGE_SIZE_RANGE_ERROR("다리의 길이는 3 이상 20 이하의 정수이어야 합니다."),
     COMMEND_NOT_FOUND_ERROR("입력 가능한 선택지는 'R'과 'Q' 입니다.");
 
