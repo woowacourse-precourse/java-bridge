064b1e380e9cbabdc71afe0250bdad24ebcd9199
fnzl54 <rnjscksdud1@gmail.com>
2022-11-22 18:34:44 +0900

    style: rename test methods

--- src/test/java/bridge/ExceptionCaseTest.java
+++ src/test/java/bridge/ExceptionCaseTest.java
@@ -8,25 +8,25 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 public class ExceptionCaseTest {
 
     @Test
-    void bridge_size가_3보다_작은_경우() {
+    void isInputAllNumber_IllegalArgumentException_bridgeSizeLessThan3() {
         assertThatThrownBy(() -> ExceptionCase.isInputAllNumber("2"))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
     @Test
-    void bridge_size가_20보다_큰_경우() {
+    void isInputAllNumber_IllegalArgumentException_bridgeSizeMoreThan20() {
         assertThatThrownBy(() -> ExceptionCase.isInputAllNumber("50"))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
     @Test
-    void R_Q_이외의_값이_들어오는_경우() {
+    void isInputTry_IllegalArgumentException_InputIsNotQW() {
         assertThatThrownBy(() -> ExceptionCase.isInputTry("W"))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
     @Test
-    void U_D_이외의_값이_들어오는_경우() {
+    void isInputUpOrDown_IllegalArgumentException_InputIsNotUD() {
         assertThatThrownBy(() -> ExceptionCase.isInputUpOrDown("W"))
                 .isInstanceOf(IllegalArgumentException.class);
     }
