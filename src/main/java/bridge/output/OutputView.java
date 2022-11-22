371b7743878bb1f6a601a806b9358e9ff29e6057
hoin <apicad0307@gmail.com>
2022-11-22 13:58:03 +0900

    refactor(output/OutputView.java): 변수 이름 road에서 bridge로 변경

--- src/main/java/bridge/output/OutputView.java
+++ src/main/java/bridge/output/OutputView.java
@@ -8,41 +8,43 @@ public class OutputView {
         System.out.println();
     }
 
-    public static void printMap(StringBuilder upperRoad, StringBuilder lowerRoad) {
-        StringBuilder upperRoadFormat = new StringBuilder();
-        StringBuilder lowerRoadFormat = new StringBuilder();
-        appendSeparator(upperRoad, lowerRoad, upperRoadFormat, lowerRoadFormat);
-        addStartEndSign(upperRoadFormat, lowerRoadFormat);
-        System.out.println(upperRoadFormat);
-        System.out.println(lowerRoadFormat);
+    public static void printMap(StringBuilder upperBridge, StringBuilder lowerBridge) {
+        StringBuilder upperBridgeFormat = new StringBuilder();
+        StringBuilder lowerBridgeFormat = new StringBuilder();
+        appendSeparator(upperBridge, lowerBridge, upperBridgeFormat, lowerBridgeFormat);
+        addStartEndSign(upperBridgeFormat, lowerBridgeFormat);
+        System.out.println(upperBridgeFormat);
+        System.out.println(lowerBridgeFormat);
         System.out.println();
     }
 
-    private static void addStartEndSign(StringBuilder upperRoadFormat, StringBuilder lowerRoadFormat) {
-        upperRoadFormat.insert(0, START_SIGN);
-        lowerRoadFormat.insert(0, START_SIGN);
-        upperRoadFormat.append(END_SIGN);
-        lowerRoadFormat.append(END_SIGN);
+    private static void addStartEndSign(StringBuilder upperBridgeFormat, StringBuilder lowerBridgeFormat) {
+        upperBridgeFormat.insert(0, START_SIGN);
+        lowerBridgeFormat.insert(0, START_SIGN);
+        upperBridgeFormat.append(END_SIGN);
+        lowerBridgeFormat.append(END_SIGN);
     }
 
-    private static void appendSeparator(StringBuilder upperRoad, StringBuilder lowerRoad, StringBuilder upperRoadFormat, StringBuilder lowerRoadFormat) {
-        for (int i = 0; i < upperRoad.length(); i++) {
-            upperRoadFormat.append(upperRoad.charAt(i));
-            if (i != upperRoad.length() - 1) {
-                upperRoadFormat.append(SEPARATOR);
+    private static void appendSeparator(StringBuilder upperBridge, StringBuilder lowerBridge, StringBuilder upperBridgeFormat, StringBuilder lowerBridgeFormat) {
+        for (int i = 0; i < upperBridge.length(); i++) {
+            upperBridgeFormat.append(upperBridge.charAt(i));
+            lowerBridgeFormat.append(lowerBridge.charAt(i));
+            if (i != upperBridge.length() - 1) {
+                upperBridgeFormat.append(SEPARATOR);
+                lowerBridgeFormat.append(SEPARATOR);
             }
         }
-        for (int i = 0; i < lowerRoad.length(); i++) {
-            lowerRoadFormat.append(lowerRoad.charAt(i));
-            if (i != lowerRoad.length() - 1) {
-                lowerRoadFormat.append(SEPARATOR);
-            }
-        }
+//        for (int i = 0; i < lowerBridge.length(); i++) {
+//            lowerBridgeFormat.append(lowerBridge.charAt(i));
+//            if (i != lowerBridge.length() - 1) {
+//                lowerBridgeFormat.append(SEPARATOR);
+//            }
+//        }
     }
 
-    public static void printResult(boolean isSuccess, StringBuilder upperRoad, StringBuilder lowerRoad, int round) {
+    public static void printResult(boolean isSuccess, StringBuilder upperBridge, StringBuilder lowerBridge, int round) {
         System.out.println(FINAL_RESULT);
-        printMap(upperRoad, lowerRoad);
+        printMap(upperBridge, lowerBridge);
         System.out.print(SUCCESS_OR_NOT);
         printSuccessOrNot(isSuccess, round);
     }
