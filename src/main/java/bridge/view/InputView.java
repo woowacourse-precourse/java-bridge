c55494516e9f4ff13d180db095b18d5b2bf740ed
LeeJejune <leejejune0625@gmail.com>
2022-11-22 17:37:16 +0900

    feat: 다리 길이 입력을 위한 메서드 구현

--- src/main/java/bridge/view/InputView.java
+++ src/main/java/bridge/view/InputView.java
@@ -17,4 +17,26 @@ public class InputView {
     private static final String INPUT_IS_NOT_NUMBER_MESSAGE = "입력은 숫자로만 이루어져야 합니다";
     private static final String MIN_SIZE_MESSAGE = "입력은 " + BRIDGE_MIN_LENGTH + "이상이어야 합니다";
     private static final String MAX_SIZE_MESSAGE = "입력은 " + BRIDGE_MAX_LENGTH + "이하여야 합니다";
+
+    /**
+     * 다리의 길이를 입력받는다.
+     */
+    public int readBridgeSize() {
+        String input = Console.readLine();
+        if (!NUMBERS_CORRECT_PATTERN.matcher(input).matches()){
+            throw new IllegalArgumentException(INPUT_IS_NOT_NUMBER_MESSAGE);
+        }
+        int bridgeLength = Integer.parseInt(input);
+        validateBridgeLength(bridgeLength);
+        return bridgeLength;
+    }
+
+    private void validateBridgeLength(int bridgeLength) {
+        if (bridgeLength < BRIDGE_MIN_LENGTH) {
+            throw new IllegalArgumentException(MIN_SIZE_MESSAGE);
+        }
+        if (bridgeLength > BRIDGE_MAX_LENGTH) {
+            throw new IllegalArgumentException(MAX_SIZE_MESSAGE);
+        }
+    }
 }
