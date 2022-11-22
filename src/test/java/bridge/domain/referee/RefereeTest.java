9975ac4ed4c4cbefb42652468abb67d7b79aa9c8
chanminmaxscaler <jcm04@naver.com>
2022-11-22 20:23:47 +0900

    test: Referee 클래스의 isEnd 메소드의 테스트 코드를 작성한다
    isEnd 메소드는 리팩토링 과정에서 새로 만든 메소드로 위치가 입력되면 다리의 끝인지 확인해서 반환한다.
    isEnd 메소드에 위치가 입력되었을 때 다리의 끝이 맞는지 확인한 값을 반환하는지 확인한다.

--- src/test/java/bridge/domain/referee/RefereeTest.java
+++ src/test/java/bridge/domain/referee/RefereeTest.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.MethodSource;
 
 import java.util.List;
@@ -17,14 +18,13 @@ import static org.assertj.core.api.Assertions.assertThat;
 class RefereeTest {
 
     private Referee referee;
-    private Bridge bridge;
 
     @BeforeEach
     void setUp() {
         List<Direction> directions = Stream.of("U", "D", "U", "D", "U")
                 .map(Direction::new)
                 .collect(Collectors.toList());
-        bridge = new Bridge(directions);
+        Bridge bridge = new Bridge(directions);
         referee = Referee.from(bridge);
     }
 
@@ -35,6 +35,15 @@ class RefereeTest {
         Judgement judgement = referee.checkDirection(direction, location);
 
         assertThat(judgement.isTrue()).isEqualTo(expected);
+    }
+
+    @DisplayName("isEnd 메소드에 위치가 입력되었을 때 다리의 끝이 맞는지 확인한 값을 반환하는지 확인")
+    @ParameterizedTest
+    @CsvSource({"1, false", "4, false", "5, true", "6, false"})
+    void isEnd_test(int location, boolean expectedValue) {
+        boolean actualValue = referee.isEnd(location);
+
+        assertThat(actualValue).isEqualTo(expectedValue);
     }
 
     static Stream<Arguments> provideArgumentsForCheckingTest() {
