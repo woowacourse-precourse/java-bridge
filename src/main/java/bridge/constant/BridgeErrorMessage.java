b8dc6465e8ad976c3b617992d76eeab27a0903a2
Il kwon <alexryu1105@naver.com>
2022-11-22 16:02:01 +0900

    fix: 오류 메세지 오탈자 수정

--- src/main/java/bridge/constant/BridgeErrorMessage.java
+++ src/main/java/bridge/constant/BridgeErrorMessage.java
@@ -2,8 +2,9 @@ package bridge.constant;
 
 public class BridgeErrorMessage {
     public static final String BRIDGE_LENGTH_IS_NOT_DIGIT_ERROR_MESSAGE = "[ERROR] - 다리의 길이가 숫자가 아닙니다.";
-    public static final String BRIDGE_LENGTH_IS_NOT_IN_RANGE_ERROR_MESSAGE = "[ERROR] - 다리의 길이가 3~20 범위 밖의 숫자 입니다.";
-    public static final String DIRECTION_IS_NOT_VALID_ERROR_MESSAGE = "[ERROR] - U/D의 갑승로만 애지사 ㄱ력버ㅏ.";
+    public static final String BRIDGE_LENGTH_IS_NOT_IN_RANGE_ERROR_MESSAGE = "[ERROR] - 다리 길이는 3부터 20 사이의 숫자여야 합니다.";
+    public static final String DIRECTION_IS_NOT_VALID_ERROR_MESSAGE = "[ERROR] - U/D의 값으로만 입력해주세요";
 
-    private BridgeErrorMessage(){}
+    private BridgeErrorMessage() {
+    }
 }
