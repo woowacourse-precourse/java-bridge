42c9dbabd972d1425b1042935a0166fd68c40a1b
박원종 <wonjong1030@naver.com>
2022-11-22 11:02:05 +0900

    refactor(GameResult): 하드 코딩 수정

--- src/main/java/bridge/service/GameResult.java
+++ src/main/java/bridge/service/GameResult.java
@@ -6,29 +6,33 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class GameResult {
+    private static final int UP_INDEX = 0;
+    private static final int DOWN_INDEX = 1;
+    private static final int LIST_SIZE = 2;
+
     private List<String>[] result;
 
     public GameResult() {
-        this.result = new ArrayList[2];
-        for (int i = 0; i < 2; i++) {
+        this.result = new ArrayList[LIST_SIZE];
+        for (int i = 0; i < LIST_SIZE; i++) {
             result[i] = new ArrayList<>();
         }
     }
     public void add(int index, String answer) {
         result[index].add(answer);
-        result[(index + 1) % 2].add(BridgeStatus.EMPTY.getValue());
+        result[(index + 1) % LIST_SIZE].add(BridgeStatus.EMPTY.getValue());
     }
 
     public boolean isUnavailable(int lastIndex) {
-        if (BridgeStatus.UNAVAILABLE.equals(result[0].get(lastIndex)) ||
-                BridgeStatus.UNAVAILABLE.equals(result[1].get(lastIndex))) {
+        if (BridgeStatus.UNAVAILABLE.equals(result[UP_INDEX].get(lastIndex)) ||
+                BridgeStatus.UNAVAILABLE.equals(result[DOWN_INDEX].get(lastIndex))) {
             return true;
         }
         return false;
     }
     public void clear() {
-        result[0].clear();
-        result[1].clear();
+        result[UP_INDEX].clear();
+        result[DOWN_INDEX].clear();
     }
     public List<String>[] getResult() {
         return result;
