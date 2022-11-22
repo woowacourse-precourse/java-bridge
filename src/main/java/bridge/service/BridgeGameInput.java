dbbe2555e66ebf705e56fd74f98845aa3a6a03cc
jhsseonn <hosu0125@gmail.com>
2022-11-22 21:46:19 +0900

    refactor: inputView, outputView 사용 대신 BridgeGameInput 클래스 함수 사용

---
+++ src/main/java/bridge/service/BridgeGameInput.java
@@ -0,0 +1,43 @@
+package bridge.service;
+
+import bridge.view.InputView;
+import bridge.view.OutputView;
+
+import java.util.HashMap;
+import java.util.List;
+
+public class BridgeGameInput {
+
+    InputView inputView = new InputView();
+
+    OutputView outputView = new OutputView();
+    BridgeGameService bridgeGameService = new BridgeGameService();
+
+    public int getBridgeSize() {
+        int bridgeSize=inputView.readBridgeSize();
+        bridgeGameService.isValidBridgeRange(bridgeSize);
+        return bridgeSize;
+    }
+
+    public String getMoving() {
+        String inputMoving = inputView.readMoving();
+        bridgeGameService.isValidMovingInput(inputMoving);
+
+        return inputMoving;
+    }
+
+    public String getCommand() {
+        String inputCommand = inputView.readGameCommand();
+        bridgeGameService.isValidCommand(inputCommand);
+
+        return inputCommand;
+    }
+
+    public void getBridgePrint(HashMap<Integer, List<String>> resultMap) {
+        outputView.printMap(resultMap);
+    }
+
+    public void getResultPrint(String result, int retrycount, HashMap<Integer, List<String>> resultMap) {
+        outputView.printResult(result, retrycount, resultMap);
+    }
+}
