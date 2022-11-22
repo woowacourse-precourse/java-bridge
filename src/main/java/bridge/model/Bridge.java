d11ac237e4ff90d8ae51027551266c15d1fceab2
wxxhyeong <wxxhyeong@gmail.com>
2022-11-22 16:24:08 +0900

    fix: bridgeMaker package 이동

--- src/main/java/bridge/model/Bridge.java
+++ src/main/java/bridge/model/Bridge.java
@@ -1,10 +1,5 @@
 package bridge.model;
 
-import bridge.controller.BridgeMaker;
-import bridge.BridgeRandomNumberGenerator;
-import bridge.ValidationException;
-import bridge.view.InputView;
-
 import java.util.List;
 
 public class Bridge {
--- src/test/java/bridge/BridgeMakerTest.java
+++ src/test/java/bridge/BridgeMakerTest.java
@@ -1,6 +1,5 @@
 package bridge;
 
-import bridge.controller.BridgeMaker;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
