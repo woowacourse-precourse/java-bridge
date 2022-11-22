b4a76ddd124e6c7d08f843cadd416b0d213fb9c6
JJ503 <63184334+JJ503@users.noreply.github.com>
2022-11-22 11:06:14 +0900

    style(BridgeGameController): 다리 길이 입력을 통한 다리 생성을 함수로 분리

--- src/main/java/bridge/BridgeGameController.java
+++ src/main/java/bridge/BridgeGameController.java
@@ -6,10 +6,16 @@ public class BridgeGameController {
     OutputView outputView = new OutputView();
     InputView inputView = new InputView();
     BridgeMaker bridgeMaker = new BridgeMaker(new BridgeRandomNumberGenerator());
+    BridgeGame bridgeGame;
 
     public void gameStart() {
         outputView.printStartMessage();
+        initBridge();
+    }
+
+    private void initBridge() {
         Bridge bridge = new Bridge(makeBridge());
+        bridgeGame = new BridgeGame(bridge);
     }
 
     private List<String> makeBridge() {
