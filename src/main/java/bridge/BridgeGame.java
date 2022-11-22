fadbe4c10c8e4f83b02a6d6e6de48b3d91465208
Jae Ung Jung <roosa0210@gmail.com>
2022-11-22 22:00:36 +0900

    [feat] BridgeGame.move 사용자가 칸을 이동할 때 사용하는 메서드 업데이트
    인덱스 1이 있는 위치를 찾는 함수를 indexOf(1)로 변경

--- src/main/java/bridge/BridgeGame.java
+++ src/main/java/bridge/BridgeGame.java
@@ -16,14 +16,14 @@ public class BridgeGame {
      * 이동을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
     public List<Integer> move(List<String> bridge, List<Integer> position_map, String moving) {
-        int position = Arrays.asList(position_map).indexOf(1);  //i 번째에 1의 값이 있으면 i번째 칸에 위치, 0이면 출발안함
+        int position = position_map.indexOf(1);  //i 번째에 1의 값이 있으면 i번째 칸에 위치, 0이면 출발안함
+        System.out.println(position);
         if(moving.equals(bridge.get(position))) {
             position_map.set(position,0);
             position_map.set(position+1,1);
         }
         if(!moving.equals(bridge.get(position))) {
             position_map.set(position,0);
-            position_map.set(0,1);
         }
         return position_map;
     }
@@ -35,11 +35,7 @@ public class BridgeGame {
      */
     public List<Integer> retry(List<Integer> position_map, String input) {
         if(input.equals("R")) {
-            Collections.replaceAll(position_map,1,0);
             position_map.set(0,1);
-        }
-        if(input.equals("Q")) {
-            Collections.replaceAll(position_map,1,0);
         }
         return position_map;
     }
