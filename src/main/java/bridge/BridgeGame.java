313a5235f10772e3f03e1aa3c794446d98046fd4
JunTaeINC <juntea3523@naver.com>
2022-11-22 22:32:52 +0900

    feat : (BridgeGame) retry 기능 구현

--- src/main/java/bridge/BridgeGame.java
+++ src/main/java/bridge/BridgeGame.java
@@ -1,9 +1,15 @@
 package bridge;
 
+
+import bridge.controller.GameController;
+import bridge.model.Moving;
+import bridge.model.ReEnter;
+
 /**
  * 다리 건너기 게임을 관리하는 클래스
  */
 public class BridgeGame {
+    private final static String RE_START = "R";
 
     /**
      * 사용자가 칸을 이동할 때 사용하는 메서드
@@ -19,6 +25,12 @@ public class BridgeGame {
      * <p>
      * 재시작을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void retry() {
+    public static boolean retry() {
+        if (ReEnter.reGetExitOrRestart().equals(RE_START)) {
+            Moving.clearMoving();
+            return true;
+        }
+        GameController.printMovingAndResult();
+        return false;
     }
 }
