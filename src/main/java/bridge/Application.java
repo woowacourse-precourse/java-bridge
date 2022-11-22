faf9fea37b5d20c4f295d8e1716a862d8c570fda
lacram <chemin9898@gmail.com>
2022-11-22 21:16:26 +0900

    refactor: 추가된 생성자를 통해 프로그램 실행시키도록 수정

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -1,15 +1,10 @@
 package bridge;
 
-import static bridge.BridgeProgram.runBridgeProgram;
-
 public class Application {
 
     public static void main(String[] args) {
         // TODO: 프로그램 구현
-        try {
-            runBridgeProgram();
-        } catch (IllegalArgumentException e) {
-            System.out.println(e.getMessage());
-        }
+        BridgeProgram bridgeProgram = new BridgeProgram(new BridgeGame(), new InputView(), new OutputView());
+        bridgeProgram.runBridgeProgram();
     }
 }
