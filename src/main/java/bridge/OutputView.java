c35d968e2a2697af658e158fded724afa7534ec7
Jae Ung Jung <roosa0210@gmail.com>
2022-11-22 22:32:32 +0900

    [feat] OutputView.printMap 현재까지 이동한 다리의 상태를 출력하는 메서드 업데이트
    OuputView.Upbridge 와 OuputView.Upbridge를 이용해서 상태를 출력

--- src/main/java/bridge/OutputView.java
+++ src/main/java/bridge/OutputView.java
@@ -1,17 +1,68 @@
 package bridge;
 
+import java.awt.*;
+import java.util.List;
+
 /**
  * 사용자에게 게임 진행 상황과 결과를 출력하는 역할을 한다.
  */
 public class OutputView {
+
+
+    public void Upbridge(List<String> bridge, List<Integer> position_map, String moving) {
+        for(int i = 0; i < position_map.indexOf(1); i++) {
+            if(bridge.get(i).equals("U")) {
+                System.out.print(" O |");
+            }
+            if(bridge.get(i).equals("D")) {
+                System.out.print("   |");
+            }
+        }
+        if(bridge.get(position_map.indexOf(1)).equals("U") && moving.equals("U")) {
+            System.out.print(" O ");
+        }
+        if(bridge.get(position_map.indexOf(1)).equals("U") && moving.equals("D")) {
+            System.out.print("   ");
+        }
+        if(bridge.get(position_map.indexOf(1)).equals("D") && moving.equals("U")) {
+            System.out.print(" X ");
+        }
+    }
+
+    public void Downbridge(List<String> bridge, List<Integer> position_map, String moving) {
+        for(int i = 0; i <= position_map.indexOf(1); i++) {
+            if(bridge.get(i).equals("D")) {
+                System.out.print(" O |");
+            }
+            if(bridge.get(i).equals("U")) {
+                System.out.print("   |");
+            }
+        }
+        if(bridge.get(position_map.indexOf(1)).equals("D") && moving.equals("D")) {
+            System.out.print(" O ");
+        }
+        if(bridge.get(position_map.indexOf(1)).equals("D") && moving.equals("U")) {
+            System.out.print("   ");
+        }
+        if(bridge.get(position_map.indexOf(1)).equals("U") && moving.equals("D")) {
+            System.out.print(" X ");
+        }
+    }
+
 
     /**
      * 현재까지 이동한 다리의 상태를 정해진 형식에 맞춰 출력한다.
      * <p>
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void printMap() {
+    public void printMap(List<String> bridge, List<Integer> position_map, String moving) {
+        System.out.print("[");
+        Upbridge(bridge, position_map, moving);
+        System.out.print("]\n");
 
+        System.out.print("[");
+        Downbridge(bridge, position_map, moving);
+        System.out.print("]\n");
     }
 
     /**
