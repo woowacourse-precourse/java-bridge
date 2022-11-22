818cde474183610b304d5d620ab53f8df29433ad
kmularise <kmula222@gmail.com>
2022-11-22 18:53:23 +0900

    feat(bridge): 다리 건너기 게임 진행 기능 구현

--- src/main/java/bridge/BridgeGame.java
+++ src/main/java/bridge/BridgeGame.java
@@ -1,9 +1,22 @@
 package bridge;
 
+import java.util.List;
+
 /**
  * 다리 건너기 게임을 관리하는 클래스
  */
 public class BridgeGame {
+    private final BridgeNumberGenerator numberGenerator = new BridgeRandomNumberGenerator();
+    private final BridgeMaker bridgeMaker = new BridgeMaker(numberGenerator);
+    private final List<String> bridge;
+    private final Player player;
+    private int totalTry;
+
+    public BridgeGame(int size) {
+        bridge = bridgeMaker.makeBridge(size);
+        player = new Player(bridge);
+        totalTry = 0;
+    }
 
     /**
      * 사용자가 칸을 이동할 때 사용하는 메서드
@@ -11,6 +24,18 @@ public class BridgeGame {
      * 이동을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
     public void move() {
+        InputController.checkMove(player);
+    }
+    public void play() {
+        do {
+            move();
+        } while (!player.isCompleted() && player.isSuccess());
+        if (!player.isSuccess()) {
+            retry();
+        }
+        if (player.isSuccess()) {
+            complete();
+        }
     }
 
     /**
@@ -19,5 +44,19 @@ public class BridgeGame {
      * 재시작을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
     public void retry() {
+        String condition = InputController.checkGameCommend(player);
+        if (condition.equals("R")) {
+            play();
+        }
+        if (condition.equals("Q")) {
+            complete();
+        }
+    }
+
+    /**
+     * 내가 만든 함수들
+     */
+    public void complete() {
+        player.complete();
     }
 }
