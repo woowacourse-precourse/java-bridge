45cef5c0b56f73f98106a2be6e9379a7d6a91527
eom-tae-in <101633465+eom-tae-in@users.noreply.github.com>
2022-11-22 23:35:43 +0900

    feat: OutputView 생성

--- src/main/java/bridge/OutputView.java
+++ src/main/java/bridge/OutputView.java
@@ -1,23 +1,41 @@
-package bridge;
+package bridge.view;
 
-/**
- * 사용자에게 게임 진행 상황과 결과를 출력하는 역할을 한다.
- */
 public class OutputView {
-
-    /**
-     * 현재까지 이동한 다리의 상태를 정해진 형식에 맞춰 출력한다.
-     * <p>
-     * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
-     */
-    public void printMap() {
+    public void printStartBridgeGame() {
+        System.out.println("다리 건너기 게임을 시작합니다.");
+    }
+    public void printBridgeLengthQuestion() {
+        System.out.println();
+        System.out.println("다리의 길이를 입력해주세요");
+    }
+    public void printMoveToWhereQuestion() {
+        System.out.println();
+        System.out.println("이동할 칸을 선택해주세요. (위: U, 아래: D)");
+    }
+    public void printRestartOrDoneQuestion() {
+        System.out.println();
+        System.out.println("게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)");
+    }
+    public void printMap(StringBuilder upBridge, StringBuilder downBridge) {
+        System.out.println(upBridge);
+        System.out.println(downBridge);
     }
 
-    /**
-     * 게임의 최종 결과를 정해진 형식에 맞춰 출력한다.
-     * <p>
-     * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
-     */
-    public void printResult() {
+    public void printResult(StringBuilder upBridge, StringBuilder downBridge) {
+        System.out.println();
+        System.out.println("최종 게임 결과");
+        printMap(upBridge, downBridge);
+    }
+
+    public void printSuccessOrDefeat(boolean successOrDefeat) {
+        System.out.println();
+        if (successOrDefeat) {
+            System.out.println("게임 성공 여부: " + "성공");
+            return;
+        }
+        System.out.println("게임 성공 여부: " + "실패");
+    }
+    public void printPlayerTryCount(int count) {
+        System.out.println("총 시도한 횟수: " + count);
     }
 }
