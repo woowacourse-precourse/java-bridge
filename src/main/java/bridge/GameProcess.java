475ae5c435428879374723c9d7e3e09b92ebda54
soyunim <togksk123@naver.com>
2022-11-22 16:40:16 +0900

    feat: 이동할 칸 입력

--- src/main/java/bridge/GameProcess.java
+++ src/main/java/bridge/GameProcess.java
@@ -1,5 +1,8 @@
 package bridge;
 
+import static bridge.messages.error.*;
+import static bridge.messages.words.*;
+
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -7,6 +10,7 @@ import java.util.List;
 public class GameProcess {
     private static final InputView inputView = new InputView();
     private static final OutputView outputView = new OutputView();
+    private static final BridgeGame bridgeGame = new BridgeGame();
     private static final BridgeRandomNumberGenerator bridgeRandomNumberGenerator = new BridgeRandomNumberGenerator();
     private static final BridgeMaker bridgeMaker = new BridgeMaker(bridgeRandomNumberGenerator);
 
@@ -14,13 +18,11 @@ public class GameProcess {
     public static String length;
     public static int size;
     public static List<String> bridge;
-    public static List<String> makingBridge = new ArrayList<>(Arrays.asList("[]","[]"));
+    public static List<String> makingBridge = new ArrayList<>(Arrays.asList("[ ]","[ ]"));
 
     public static void gameStart(){
         outputView.printStart();
         size = getSize();
-        bridge = bridgeMaker.makeBridge(size);
-        gameRunning(bridge, size);
     }
 
     public static int getSize() {
@@ -29,17 +31,21 @@ public class GameProcess {
             size = Integer.parseInt(length);
         }
         catch(NumberFormatException e){
-            throw new IllegalArgumentException("[ERROR] 다리 길이 입력값이 잘못됨.");
+            throw new IllegalArgumentException(inputE);
         }
-        if(size>20 || size<3){
-            throw new IllegalArgumentException("[ERROR] 다리 길이 입력범위가 잘못됨.");
+        if(size>max || size<min){
+            throw new IllegalArgumentException(rangeE);
         }
         return size;
     }
 
-    public static void gameRunning(List<String> bridge, int size){
-        makingBridge = new ArrayList<>(Arrays.asList("[]","[]"));
 
+    private static String getStatus(List<String> bridge, int idx) {
+        String moving = inputView.readMoving();
+        String status = BridgeGame.rightOrWrong(bridge,moving,idx);
+        makingBridge = bridgeGame.move(makingBridge,status,moving);
+        outputView.printMap(makingBridge);
+        return status;
     }
 
     public static void gameEnd(){
