ae0129b670a780a2073f5d31906830da005fe17b
nswon <swnam0729@gmail.com>
2022-11-22 20:23:06 +0900

    test: 게임 재시작 여부를 판단하는 로직 테스트 코드 작성

--- src/test/java/bridge/service/BridgeGameTest.java
+++ src/test/java/bridge/service/BridgeGameTest.java
@@ -7,12 +7,15 @@ import bridge.domain.Bridge;
 import bridge.domain.Player;
 import bridge.facade.BridgeGameFacade;
 import bridge.service.dto.request.BridgeSizeRequestDto;
+import bridge.service.dto.request.GameRetryRequestDto;
 import bridge.service.dto.request.SelectBlockRequestDto;
 import bridge.service.dto.response.BridgeResponseDto;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
 
 public class BridgeGameTest {
     private BridgeGame bridgeGame;
@@ -34,11 +37,10 @@ public class BridgeGameTest {
         final String bridgeSize = "3";
         BridgeSizeRequestDto dto = new BridgeSizeRequestDto(bridgeSize);
         bridgeGame.create(dto);
-        Assertions.assertThat(bridge.isDoneCrossingBridge(3)).isTrue();
+        assertThat(bridge.isDoneCrossingBridge(3)).isTrue();
     }
 
-    //실패 TODO : Random 값을 어떻게 알아내야 할지 생각하기 (테스트하기 좋은 코드로 개선)
-    @DisplayName("건널 수 있는 다리들을 매개변수로 받아 다리 생성")
+    @DisplayName("플레이어가 이동하려는 칸을 입력받아서 건널 수 있는지 없는지 표시")
     @Test
     void should_BridgeStatus_When_move() {
         final String bridgeSize = "3";
@@ -47,7 +49,25 @@ public class BridgeGameTest {
         String blockToMove = "U";
         SelectBlockRequestDto requestDto = new SelectBlockRequestDto(blockToMove);
         BridgeResponseDto responseDto = bridgeGame.move(requestDto);
-        Assertions.assertThat(responseDto.getBridgeStatus().getUpBlocks()).contains("O");
-        Assertions.assertThat(responseDto.getBridgeStatus().getDownBlocks()).contains(" ");
+        assertThat(responseDto.getBridgeStatus()).isNotNull();
+        assertThat(responseDto.getBridgeStatus()).isNotNull();
+    }
+
+    @DisplayName("게임 재시작 여부를 입력받아 재시작을 원한다면 true")
+    @Test
+    void should_True_When_retry() {
+        final String INPUT_RETRY = "R";
+        GameRetryRequestDto dto = new GameRetryRequestDto(INPUT_RETRY);
+        boolean canRetry = bridgeGame.retry(dto);
+        assertThat(canRetry).isTrue();
+    }
+
+    @DisplayName("게임 재시작 여부를 입력받아 그대로 게임종료를 원한다면 false")
+    @Test
+    void should_False_When_retry() {
+        final String INPUT_RETRY = "Q";
+        GameRetryRequestDto dto = new GameRetryRequestDto(INPUT_RETRY);
+        boolean canRetry = bridgeGame.retry(dto);
+        assertThat(canRetry).isFalse();
     }
 }
