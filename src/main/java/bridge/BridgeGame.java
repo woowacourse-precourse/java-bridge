a761a7184229861f858a2ba53c5d49bfe90981c5
이지영 <wldudwldhd@naver.com>
2022-11-22 21:21:01 +0900

    feat : 게임 시작, 게임 종료 함수 작성

--- src/main/java/bridge/BridgeGame.java
+++ src/main/java/bridge/BridgeGame.java
@@ -1,9 +1,45 @@
 package bridge;
 
+import java.util.List;
+
 /**
  * 다리 건너기 게임을 관리하는 클래스
  */
 public class BridgeGame {
+    private InputView inputView;
+    private OutputView outputView;
+    private BridgeMaker bridgeMaker;
+
+    private int stepCnt = 0;
+    private int isAnswer = 1;
+    private List<String> bridge;
+
+    public BridgeGame(InputView inputView, BridgeMaker bridgeMaker, OutputView outputView) {
+        this.inputView = inputView;
+        this.outputView = outputView;
+        this.bridgeMaker = bridgeMaker;
+
+    }
+
+    public boolean startGame(){
+        outputView.printStartComment();
+        bridge = setBridgeBySize();
+        progressGame();
+
+        if(isAnswer == 1){
+            endGame();
+            return false;
+        }
+        return retry();
+    }
+    public void endGame() {
+        outputView.printResult(stepCnt, bridge, isAnswer);
+    }
+
+    public int getStepCnt() {
+        return stepCnt;
+    }
+
 
     /**
      * 사용자가 칸을 이동할 때 사용하는 메서드
