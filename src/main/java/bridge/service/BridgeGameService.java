81597a1dc6417982f1c5e914d7cb62c98dbc633e
jmjmjames <chungjm0711@gmail.com>
2022-11-22 13:00:30 +0900

    feat: BridgeGameService$initBridgeGame 추가

--- src/main/java/bridge/service/BridgeGameService.java
+++ src/main/java/bridge/service/BridgeGameService.java
@@ -1,5 +1,21 @@
 package bridge.service;
 
-public class BridgeGameService {
+import bridge.domain.BridgeGame;
+import bridge.domain.BridgeGameAnswer;
+import bridge.domain.BridgeMaker;
+import java.util.List;
 
+public class BridgeGameService {
+    private final BridgeMaker bridgeMaker;
+    private BridgeGame bridgeGame;
+
+    public BridgeGameService(BridgeMaker bridgeMaker) {
+        this.bridgeMaker = bridgeMaker;
+    }
+
+    public void initBridgeGame(int size) {
+        List<String> bridge = bridgeMaker.makeBridge(size);
+        BridgeGameAnswer gameAnswer = new BridgeGameAnswer(bridge);
+        this.bridgeGame = new BridgeGame(gameAnswer);
+    }
 }
