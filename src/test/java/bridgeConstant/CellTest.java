48c7287803bc1931e96ca87d8521413ba65db07c
Devtist <yt930310@snu.ac.kr>
2022-11-22 19:17:13 +0900

    style: 테스트코드에 주석 추가

--- src/test/java/bridgeConstant/CellTest.java
+++ src/test/java/bridgeConstant/CellTest.java
@@ -9,9 +9,11 @@ import org.junit.jupiter.params.provider.ValueSource;
 class CellTest {
 
 	@DisplayName("숫자에 따라 칸의 선택이 올바른지 검증한다.")
+	//given
 	@ValueSource(ints = {1, 0})
 	@ParameterizedTest
 	void convertToPosition(int ints) {
+		//when, then
 		assertTrue(Cell.convertToPosition(ints).equals("U") || Cell.convertToPosition(ints).equals("D"));
 	}
 }
--- src/test/java/util/ValidatorTest.java
+++ src/test/java/util/ValidatorTest.java
@@ -9,46 +9,58 @@ import org.junit.jupiter.params.provider.ValueSource;
 
 class ValidatorTest {
 	@DisplayName("한자리, 두자리 숫자인 다리 길이를 입력받는다.")
+	//given
 	@ValueSource(strings = {"20", "0", "3", "04"})
 	@ParameterizedTest
 	void validateSizeIsNumberNormal(String strings) {
+		//when, then
 		assertDoesNotThrow(() -> Validator.validateSizeIsNaturalNumber(strings));
 	}
 
 	@DisplayName("숫자가 아닌 다리 길이 입력에 대해 예외를 발생한다.")
+	//given
 	@ValueSource(strings = {"25.1", "abcde", "가나다라", "1ㅁ2a3", "", "-1"})
 	@ParameterizedTest
 	void validateSizeIsNumberAbnormal(String strings) {
+		//when, then
 		assertThatThrownBy(() -> Validator.validateSizeIsNaturalNumber(strings)).isInstanceOf(
 			IllegalArgumentException.class);
 	}
 
 	@DisplayName("이동할 칸을 입력받는다.")
+	//given
 	@ValueSource(strings = {"U", "D"})
 	@ParameterizedTest
 	void validateCellPositionNormal(String strings) {
+		//when, then
 		assertDoesNotThrow(() -> Validator.validateMovingIsCellPosition(strings));
 	}
 
 	@DisplayName("U와 D가 아닌 이동할 칸 입력에 대한 예외를 발생한다.")
+	//given
 	@ValueSource(strings = {"25.1", "abcde", "가나다라", "1ㅁ2a3", "u", ""})
 	@ParameterizedTest
 	void validateCellPositionAbnormal(String strings) {
+		//when, then
 		assertThatThrownBy(() -> Validator.validateMovingIsCellPosition(strings)).isInstanceOf(
 			IllegalArgumentException.class);
 	}
 
 	@DisplayName("재시작 여부를 입력 받는다.")
+	//given
 	@ValueSource(strings = {"R", "Q"})
 	@ParameterizedTest
 	void validateGameCommandNormal(String strings) {
+		//when, then
 		assertDoesNotThrow(() -> Validator.validateGameCommand(strings));
 	}
 
 	@DisplayName("R와 Q가 아닌 재시작 여부 입력에 대한 예외를 발생한다.")
+	//given
 	@ValueSource(strings = {"25.1", "abcde", "가나다라", "1ㅁ2a3", "u", ""})
 	@ParameterizedTest
 	void validateGameCommandAbnormal(String strings) {
+		//when, then
 		assertThatThrownBy(() -> Validator.validateGameCommand(strings)).isInstanceOf(IllegalArgumentException.class);
 	}
 }
