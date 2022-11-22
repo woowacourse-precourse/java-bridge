d0ed763a4380621bfb7f65269c4e6f985bf49c6a
SeongbinPark <binni0515@gmail.com>
2022-11-22 18:35:48 +0900

    feat(Moving): add getMarkByMoving

--- src/main/java/bridge/Moving.java
+++ src/main/java/bridge/Moving.java
@@ -1,5 +1,7 @@
 package bridge;
 
+import static bridge.constant.mark.OutputMark.BLANK;
+
 import bridge.validation.exception.InvalidMovingException;
 import java.util.Arrays;
 
@@ -25,5 +27,17 @@ public enum Moving {
 
     public static boolean isSame(String mark, Moving moving) {
         return mark.equals(moving.mark);
+    }
+
+    public static String getMarkByMoving(Moving mapMoving, Moving userMoving, String canOrCanNotMove) {
+        if (canCross(mapMoving, userMoving)) {
+            return canOrCanNotMove;
+        }
+
+        return BLANK.getMark();
+    }
+
+    private static boolean canCross(Moving mapMoving, Moving userMoving) {
+        return mapMoving == userMoving;
     }
 }
