6624c00e71c70298a51852b38bfb4963cb7a4b2c
WhoIsWoony <WhoIsWoony@gmail.com>
2022-11-22 15:34:02 +0900

    chore: add newline

--- src/main/java/bridge/OutputView.java
+++ src/main/java/bridge/OutputView.java
@@ -43,7 +43,7 @@ public class OutputView {
             addFailResult("D", board.get(step), resultDown);
         }
         System.out.println("[ "+String.join(" | ", resultUp)+" ]");
-        System.out.println("[ "+String.join(" | ", resultDown)+" ]");
+        System.out.println("[ "+String.join(" | ", resultDown)+" ]\n");
     }
 
     /**
@@ -52,7 +52,7 @@ public class OutputView {
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
     public void printResult(BridgeGame bridgeGame) {
-        System.out.println("최종 게임 결과\n");
+        System.out.println("최종 게임 결과");
         printMap(bridgeGame.board, bridgeGame.step, bridgeGame.result);
 
         String result = "실패";
