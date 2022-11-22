3bc23c3b84c8b2fdcb216564cec6b4fb8672845a
recicoco068 <gkapf838@gmail.com>
2022-11-22 17:39:57 +0900

    style : 안 쓰는 import 제거

--- src/main/java/bridge/view/InputView.java
+++ src/main/java/bridge/view/InputView.java
@@ -3,7 +3,6 @@ package bridge.view;
 import bridge.domain.enums.Command;
 import bridge.domain.enums.Error;
 import camp.nextstep.edu.missionutils.Console;
-import java.util.NoSuchElementException;
 
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
