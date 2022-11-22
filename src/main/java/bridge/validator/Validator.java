abdc0c088f525dc68ee0ec3a98c155c7255fa683
jihyun <jihyun@jihyunui-MacBookAir.local>
2022-11-22 18:34:56 +0900

    refactor(bridge): Validator static 메서드화

--- src/main/java/bridge/validator/Validator.java
+++ src/main/java/bridge/validator/Validator.java
@@ -12,21 +12,21 @@ public class Validator {
     private static final String MOVING_INPUT_ERROR_MESSAGE = "[ERROR] 이동은 U(위)/D(아래)로만 할 수 있습니다.";
     private static final String GAME_COMMAND_ERROR_MESSAGE = "[ERROR] 명령은 R(재시작)/Q(종료)로만 할 수 있습니다.";
 
-    public boolean isValidBridgeSize(int input) {
+    public static boolean isValidBridgeSize(int input) {
         if (!(input >= MIN_BRIDGE_SIZE && input <= MAX_BRIDGE_SIZE)) {
             throw new IllegalArgumentException(OUT_OF_BOUNDS_SIZE_ERROR_MESSAGE);
         }
         return true;
     }
 
-    public boolean isValidMoving(String input) {
+    public static boolean isValidMoving(String input) {
         if (!(input.equals(MOVE_UP) || input.equals(MOVE_DOWN))) {
             throw new IllegalArgumentException(MOVING_INPUT_ERROR_MESSAGE);
         }
         return true;
     }
 
-    public boolean isValidGameCommand(String input) {
+    public static boolean isValidGameCommand(String input) {
         if (!(input.equals(GAME_RESTART_COMMAND) || input.equals(GAME_QUIT_COMMAND))) {
             throw new IllegalArgumentException(GAME_COMMAND_ERROR_MESSAGE);
         }
