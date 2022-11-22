5f0f4fda91e459d51bffa2567a49f7d80dcbaaf0
simjaeseo <delpho@naver.com>
2022-11-22 23:12:05 +0900

    feat(BridgeMaker): 입력받은 길이의 다리를 생성하는 makeBridge 함수 구현

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -3,11 +3,17 @@ package bridge;
 public class Application {
     static OutputView outputView = new OutputView();
     static InputView inputView = new InputView();
+    static BridgeNumberGenerator bridgeNumberGenerator = new BridgeRandomNumberGenerator();
 
     public static void main(String[] args) {
         // TODO: 프로그램 구현
 
         outputView.printGameStart();
-        inputView.readBridgeSize();
+        int bridgeSize = inputView.readBridgeSize();
+
+        BridgeMaker bridgeMaker = new BridgeMaker(bridgeNumberGenerator);
+
+        bridgeMaker.makeBridge(bridgeSize);
+
     }
 }
