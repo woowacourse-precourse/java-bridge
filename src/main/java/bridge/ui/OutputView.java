1fa813dad8514351a569ff6fa2bed5e2e4fbef02
Limjongho <whdgh9873@icloud.com>
2022-11-22 14:39:46 +0900

    refactor(OutputView): 리펙토링으로 코드 수정

--- src/main/java/bridge/ui/OutputView.java
+++ src/main/java/bridge/ui/OutputView.java
@@ -13,64 +13,39 @@ import static bridge.util.Constant.MOVING_UP;
  */
 public class OutputView {
 
-    /**
-     * 현재까지 이동한 다리의 상태를 정해진 형식에 맞춰 출력한다.
-     * <p>
-     * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
-     */
     public void printMap(BridgeGame bridgeGame, boolean isMovable) {
         int position = bridgeGame.getPosition();
         Bridge bridge = bridgeGame.getBridge();
         List<String> crossedBridge = bridge.getCrossedBridge(position);
 
-        printCrossedUp(crossedBridge, isMovable);
-        printCrossedDown(crossedBridge, isMovable);
+        new StringBuilder();
+        printCrossedUp(crossedBridge, isMovable, new StringBuilder());
+        printCrossedDown(crossedBridge, isMovable, new StringBuilder());
         System.out.println(" ");
     }
 
-    /**
-     * 게임의 최종 결과를 정해진 형식에 맞춰 출력한다.
-     * <p>
-     * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
-     */
     public void printResult(BridgeGame bridgeGame, boolean isSuccess) {
         System.out.println("최종 게임 결과");
         printMap(bridgeGame, isSuccess);
         if (isSuccess) {
-            System.out.println("게임 성공 여부: 성공");
-            System.out.println("총 시도한 횟수: " + bridgeGame.getAttempts());
+            printIsSuccess(true, bridgeGame.getAttempts());
             return;
         }
-        System.out.println("게임 성공 여부: 실패");
-        System.out.println("총 시도한 횟수: " + bridgeGame.getAttempts());
+        printIsSuccess(false, bridgeGame.getAttempts());
     }
 
-    private void printCrossedUp(List<String> crossedUpsideRecords, boolean isMovable) {
-        StringBuilder recordForPrint = new StringBuilder();
+    private void printCrossedUp(List<String> crossedUpRecords, boolean isMovable, StringBuilder recordForPrint) {
         recordForPrint.append("[");
-        for (int i = 0; i < crossedUpsideRecords.size() - 1; i++) {
-            saveCrossedUpsideBridge(crossedUpsideRecords.get(i), recordForPrint);
+        for (int i = 0; i < crossedUpRecords.size() - 1; i++) {
+            saveCrossedUp(crossedUpRecords.get(i), recordForPrint);
             recordForPrint.append("|");
         }
-        saveLastUpsideMove(crossedUpsideRecords, isMovable, recordForPrint);
+        saveLastCrossedUp(crossedUpRecords.get(crossedUpRecords.size() - 1), isMovable, recordForPrint);
         recordForPrint.append("]");
         System.out.println(recordForPrint);
     }
 
-    private void saveLastUpsideMove(List<String> crossedUpsideRecords, boolean isMovable, StringBuilder recordForPrint) {
-        if (isMovable) {
-            saveCrossedUpsideBridge(crossedUpsideRecords.get(crossedUpsideRecords.size() - 1), recordForPrint);
-            return;
-        }
-        if (crossedUpsideRecords.get(crossedUpsideRecords.size() - 1).equals(MOVING_UP)) {
-            recordForPrint.append("   ");
-        }
-        if (!crossedUpsideRecords.get(crossedUpsideRecords.size() - 1).equals(MOVING_UP)) {
-            recordForPrint.append(" X ");
-        }
-    }
-
-    private void saveCrossedUpsideBridge(String block, StringBuilder recordForPrint) {
+    private void saveCrossedUp(String block, StringBuilder recordForPrint) {
         if (block.equals(MOVING_UP)) {
             recordForPrint.append(" O ");
         }
@@ -79,37 +54,67 @@ public class OutputView {
         }
     }
 
-    private void printCrossedDown(List<String> crossedDownsideRecords, boolean isMovable) {
-        StringBuilder recordForPrint = new StringBuilder();
-        recordForPrint.append("[");
-        for (int i = 0; i < crossedDownsideRecords.size() - 1; i++) {
-            saveCrossedDownsideBridge(crossedDownsideRecords.get(i), recordForPrint);
-            recordForPrint.append("|");
-        }
-        saveLastDownsideMove(crossedDownsideRecords, isMovable, recordForPrint);
-        recordForPrint.append("]");
-        System.out.println(recordForPrint);
-    }
-
-    private void saveLastDownsideMove(List<String> crossedDownsideRecords, boolean isMovable, StringBuilder recordForPrint) {
+    private void saveLastCrossedUp(String lastMove, boolean isMovable, StringBuilder recordForPrint) {
         if (isMovable) {
-            saveCrossedDownsideBridge(crossedDownsideRecords.get(crossedDownsideRecords.size() - 1), recordForPrint);
+            saveCrossedUp(lastMove, recordForPrint);
             return;
         }
-        if (crossedDownsideRecords.get(crossedDownsideRecords.size() - 1).equals(MOVING_DOWN)) {
+        saveUnmovableLastUpBlock(lastMove, recordForPrint);
+    }
+
+    private void saveUnmovableLastUpBlock(String lastBlock, StringBuilder recordForPrint) {
+        if (lastBlock.equals(MOVING_UP)) {
             recordForPrint.append("   ");
         }
-        if (!crossedDownsideRecords.get(crossedDownsideRecords.size() - 1).equals(MOVING_DOWN)) {
+        if (!lastBlock.equals(MOVING_UP)) {
             recordForPrint.append(" X ");
         }
     }
 
-    private void saveCrossedDownsideBridge(String block, StringBuilder recordForPrint) {
+    private void printCrossedDown(List<String> crossedDownRecords, boolean isMovable, StringBuilder recordForPrint) {
+        recordForPrint.append("[");
+        for (int i = 0; i < crossedDownRecords.size() - 1; i++) {
+            saveCrossedDown(crossedDownRecords.get(i), recordForPrint);
+            recordForPrint.append("|");
+        }
+        saveLastCrossedDown(crossedDownRecords.get(crossedDownRecords.size() - 1), isMovable, recordForPrint);
+        recordForPrint.append("]");
+        System.out.println(recordForPrint);
+    }
+
+    private void saveCrossedDown(String block, StringBuilder recordForPrint) {
         if (block.equals(MOVING_DOWN)) {
             recordForPrint.append(" O ");
         }
         if (!block.equals(MOVING_DOWN)) {
             recordForPrint.append("   ");
         }
+    }
+
+    private void saveLastCrossedDown(String lastMove, boolean isMovable, StringBuilder recordForPrint) {
+        if (isMovable) {
+            saveCrossedDown(lastMove, recordForPrint);
+            return;
+        }
+        saveUnmovableLastDownBlock(lastMove, recordForPrint);
+    }
+
+    private void saveUnmovableLastDownBlock(String lastBlock, StringBuilder recordForPrint) {
+        if (lastBlock.equals(MOVING_UP)) {
+            recordForPrint.append(" X ");
+        }
+        if (!lastBlock.equals(MOVING_UP)) {
+            recordForPrint.append("   ");
+        }
+    }
+
+    private void printIsSuccess(boolean isSuccess, int attempts) {
+        if (isSuccess) {
+            System.out.println("게임 성공 여부: 성공");
+            System.out.println("총 시도한 횟수: " + attempts);
+            return;
+        }
+        System.out.println("게임 성공 여부: 실패");
+        System.out.println("총 시도한 횟수: " + attempts);
     }
 }
