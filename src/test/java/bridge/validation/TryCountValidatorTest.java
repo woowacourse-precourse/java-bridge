64a414f5c1fed4890258e451634d5b30dd33bb0d
nakalic <nakalic28@gmail.com>
2022-11-22 21:30:55 +0900

    test(TryCountValidator): 올바른 값이라면 예외를 발생시키지 않는지 검증하는 코드 추가

--- src/test/java/bridge/validation/TryCountValidatorTest.java
+++ src/test/java/bridge/validation/TryCountValidatorTest.java
@@ -1,11 +1,10 @@
-package bridge.validator;
+package bridge.validation;
 
 import static org.assertj.core.api.Assertions.assertThatCode;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import bridge.constants.ValidatorMessage;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
@@ -13,18 +12,19 @@ class TryCountValidatorTest {
 
     @DisplayName("2,100,000,000번 이상 게임을 시도하면 예외를 발생한다.")
     @ParameterizedTest()
-    @ValueSource(strings = {"2100000001", "2147483647"})
+    @ValueSource(strings = {"2100000000", "2147483647"})
     void createNumberOutOfRange(int input) {
-        assertThatThrownBy(() ->TryCountValidator.validateTryCount(input))
+        assertThatThrownBy(() -> TryCountValidator.validateTryCount(input))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage(ValidatorMessage.TOO_MUCH_GAME.toString());
 
     }
 
     @DisplayName("2,100,000,000번 이하의 시도라면 예외가 발생하지 않는다.")
-    @Test
-    void createNormalTryCount() {
-        assertThatCode(() -> TryCountValidator.validateTryCount(2000))
+    @ParameterizedTest()
+    @ValueSource(strings = {"0", "2099999999"})
+    void createNormalTryCount(int input) {
+        assertThatCode(() -> TryCountValidator.validateTryCount(input))
                 .doesNotThrowAnyException();
     }
 }
