737fe7b1a1b736d6c0d172dde51d58039405ac18
kissshot1104 <kissshot.heartunderblade1104@gmail.com>
2022-11-22 20:02:15 +0900

    Refactor(UtilTest) : 테스트 코드 간소화
    UtilTest 클래스의 너무 긴 메소드의 길이를 줄이기 위해 코드를 간소화 했습니다.

--- src/test/java/bridge/UtilTest.java
+++ src/test/java/bridge/UtilTest.java
@@ -1,151 +1,92 @@
 package bridge;
 
-
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 class UtilTest {
 
     public static final String ERROR_MESSAGE = "[ERROR]";
 
-    @Test
     @DisplayName("숫자인지 아닌지 검사하는 테스트 코드")
-    void isNumberTest() {
-        //정상적인 값
-        {
-            boolean isTrue = Util.isNumber("1");
-            assertThat(isTrue)
-                    .isTrue();
-        }
+    @ParameterizedTest
+    @CsvSource({
+            "1, true",
+            "a, false"
+    })
+    void isNumberTest(String input, boolean isSuccess) {
+        boolean isTrue = Util.isNumber(input);
 
-        //잘못된 값
-        {
-            boolean isFalse = Util.isNumber("a");
-            assertThat(isFalse)
-                    .isFalse();
-        }
+        assertThat(isTrue)
+                .isEqualTo(isSuccess);
     }
 
-    @Test
     @DisplayName("문자인지 아닌지 검사하는 테스트 코드")
-    void isLetterTest() {
-        //정상적인 값
-        {
-            boolean isTrue = Util.isLetter("a");
-            assertThat(isTrue)
-                    .isTrue();
-        }
+    @ParameterizedTest
+    @CsvSource({
+            "a, true",
+            "1, false"
+    })
+    void isLetterTest(String input, boolean isSuccess) {
+        boolean isTrue = Util.isLetter(input);
 
-        //잘못된 값
-        {
-            boolean isFalse = Util.isLetter("1");
-            assertThat(isFalse)
-                    .isFalse();
-        }
+        assertThat(isTrue)
+                .isEqualTo(isSuccess);
     }
 
-    @Test
     @DisplayName("한 글자인지 아닌지 검사하는 테스트 코드")
-    void isOneLetterTest() {
-        //정상적인 값
-        {
-            boolean isTrue = Util.isOneLetter("a");
-            assertThat(isTrue)
-                    .isTrue();
-        }
+    @ParameterizedTest
+    @CsvSource({
+            "1, true",
+            "aa, false",
+            "'', false"
+    })
+    void isOneLetterTest(String input, boolean isSuccess) {
+        boolean isTrue = Util.isOneLetter(input);
 
-        //잘못된 값
-        {
-            boolean isFalse = Util.isOneLetter("aa");
-            assertThat(isFalse)
-                    .isFalse();
-        }
+        assertThat(isTrue)
+                .isEqualTo(isSuccess);
     }
 
 
-    @Test
     @DisplayName("여러 글자인지 아닌지 검사하는 테스트 코드")
-    void isStringTest() {
-        //정상적인 값
-        {
-            boolean isTrue = Util.isString("aa");
+    @ParameterizedTest
+    @CsvSource({
+            "1, false",
+            "'', false",
+            "aa, true"
+    })
+    void isStringTest(String input, boolean isSuccess) {
+            boolean isTrue = Util.isString(input);
+
             assertThat(isTrue)
-                    .isTrue();
-        }
-
-        //비 정상적인 값
-        {
-            boolean isFalse = Util.isString("a");
-            assertThat(isFalse)
-                    .isFalse();
-        }
+                    .isEqualTo(isSuccess);
     }
 
-    @Test
     @DisplayName("1입력시 U, 0입력시 D 리턴 테스트 코드")
-    void convertBridgeNumberGeneratorTest() {
-        //1은 U
+    @ParameterizedTest
+    @CsvSource({
+            "1, U",
+            "0, D"
+    })
+    void convertBridgeNumberGeneratorTest(int input, String bridgeValue) {
         {
-            assertThat(Util.convertBridgeNumberGenerator(1))
-                    .isEqualTo("U");
-        }
-
-        //0은 D
-        {
-            assertThat(Util.convertBridgeNumberGenerator(0))
-                    .isEqualTo("D");
+            assertThat(Util.convertBridgeNumberGenerator(input))
+                    .isEqualTo(bridgeValue);
         }
     }
 
-
-    @Test
     @DisplayName("숫자열을 정수로 바꿔준다. 범위는 1~20이 최대다")
+    @ParameterizedTest
+    @ValueSource(strings={"0", "-1", "111222333444555666777888999000", "010"})
     void stringToIntegerTest() {
-        //0인 수
-        {
-            assertThatThrownBy(() ->
-                    Util.stringToInteger("0"))
-                    .isInstanceOf(IllegalArgumentException.class)
-                    .hasMessageContaining(ERROR_MESSAGE);
-        }
-
-        // 음수 일 결우
-        {
-            assertThatThrownBy(() ->
-                    Util.stringToInteger("-1"))
-                    .isInstanceOf(IllegalArgumentException.class)
-                    .hasMessageContaining(ERROR_MESSAGE);
-        }
-
-        //너무 큰 수
-        {
-            assertThatThrownBy(() ->
-                    Util.stringToInteger("100000000000"))
-                    .isInstanceOf(IllegalArgumentException.class)
-                    .hasMessageContaining(ERROR_MESSAGE);
-        }
-
-        //앞에 0이 들어갈 경우
-        {
-            assertThatThrownBy(() ->
-                    Util.stringToInteger("010"))
-                    .isInstanceOf(IllegalArgumentException.class)
-                    .hasMessageContaining(ERROR_MESSAGE);
-        }
-
-        //허용된 최소값
-        {
-            assertThat(Util.stringToInteger("1"))
-                    .isEqualTo(1);
-        }
-
-        //허용된 최대값
-        {
-            assertThat(Util.stringToInteger("20"))
-                    .isEqualTo(20);
-        }
+        assertThatThrownBy(() ->
+                Util.stringToInteger("0"))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(ERROR_MESSAGE);
     }
 }
