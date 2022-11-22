26eaf6fa919ba34cd7e4a29ac40828142ae5ac54
jeongseungchan <jschan0911@naver.com>
2022-11-22 13:16:40 +0900

    Feat: 길이 입력 받기 기능 추가

--- src/main/java/bridge/InputView.java
+++ src/main/java/bridge/InputView.java
@@ -1,5 +1,7 @@
 package bridge;
 
+import camp.nextstep.edu.missionutils.Console;
+
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
  */
@@ -9,7 +11,13 @@ public class InputView {
      * 다리의 길이를 입력받는다.
      */
     public int readBridgeSize() {
-        return 0;
+        while(true) {
+            System.out.println("다리의 길이를 입력해주세요.");
+            String inputBridgeLength = Console.readLine();
+            // 만약 올바른 값이라면 그 값 리턴
+            // 올바르지 않은 값이라면 에러 Exception 발생 후 에러 메시지 출력 후 반복
+            return 0;
+        }
     }
 
     /**
