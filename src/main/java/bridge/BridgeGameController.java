cda3227707a8922e79bdb7402362880882a8b03e
kim <kajinori@gachon.ac.kr>
2022-11-22 20:09:14 +0900

    feat: 길이 입력값, 이동값, 게임종료 입력값 받고 저장하는 메서드 구현
    -잘못된 입력시 다시 그 부분 부터 입력 받음

--- src/main/java/bridge/BridgeGameController.java
+++ src/main/java/bridge/BridgeGameController.java
@@ -1,33 +1,82 @@
 package bridge;
 
+import bridge.Constant.GameCommand;
+
 public class BridgeGameController {
-    private BridgeGame bridgeGame;
+    private GameHelper gameHelper;
 
     public void run() {
+        System.out.println("다리 건너기 게임을 시작합니다.");
+        System.out.println();
         setGame();
         processGame();
+        winGame();
     }
 
     private void setGame() {
-        int size = Integer.parseInt(InputView.readBridgeSize());
-        BridgeMaker bridgeMaker = new BridgeMaker(new BridgeRandomNumberGenerator());
-        bridgeGame = new BridgeGame(bridgeMaker.makeBridge(size));
+        try {
+            int size = getBridgeSize();
+            BridgeMaker bridgeMaker = new BridgeMaker(new BridgeRandomNumberGenerator());
+            gameHelper = new GameHelper(new BridgeGame(bridgeMaker.makeBridge(size)));
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+            setGame();
+        }
+    }
+
+    private int getBridgeSize() {
+        int size = InputValidation.convertToInt(InputView.readBridgeSize());
+        InputValidation.validateBridgeSize(size);
+        return size;
     }
 
     public void processGame() {
-        for (int index = 0; index < bridgeGame.getBridgeSize(); index++) {
-            if (!bridgeGame.move(index, InputView.readMoving()))
-                break;
-            OutputView.printMap(bridgeGame.getBridge(), bridgeGame.getResultBridge());
+        while (gameHelper.selectRightBlock() && !gameHelper.reachEnd()) {
+            move();
+            OutputView.printMap(gameHelper.getBridge(), gameHelper.getResultBridge());
         }
-        if (bridgeGame.getBridgeSize() != bridgeGame.getResultBridgeSize())
+        if (!gameHelper.reachEnd())
             retry();
-
     }
-    private void retry() {
-        if(InputView.readGameCommand().equals("R")) {
-            bridgeGame.retry();
-            processGame();
+
+    private void move() {
+        try {
+            String upOrDown = getMoving();
+            gameHelper.initGame(upOrDown);
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+            move();
         }
+    }
+
+    private static String getMoving() {
+        String moving = InputView.readMoving();
+        InputValidation.validateUpDown(moving);
+        return moving;
+    }
+
+    private void winGame() {
+        OutputView.printResult(gameHelper, gameHelper.getTrialCount());
+    }
+
+    private void retry() {
+        try {
+            String command = getGameCommand();
+            if (command.equals(GameCommand.RETRY.get())) retryGame();
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+            retry();
+        }
+    }
+
+    private void retryGame() {
+        gameHelper.restart();
+        processGame();
+    }
+
+    private static String getGameCommand() {
+        String command = InputView.readGameCommand();
+        InputValidation.validateCommand(command);
+        return command;
     }
 }
