68d44af863d23af79cf702def1064461c97fc091
suheonjoo <suheonj95@naver.com>
2022-11-22 14:03:35 +0900

    test(ApplicationTest): ApplicationTest 에 추가한 테스트 제거 => 원래 상태로 복귀

--- src/test/java/bridge/ApplicationTest.java
+++ src/test/java/bridge/ApplicationTest.java
@@ -6,7 +6,6 @@ import static org.assertj.core.util.Lists.*;
 
 import java.util.List;
 
-import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 
 import camp.nextstep.edu.missionutils.test.NsTest;
@@ -14,11 +13,6 @@ import camp.nextstep.edu.missionutils.test.NsTest;
 class ApplicationTest extends NsTest {
 
 	private static final String ERROR_MESSAGE = "[ERROR]";
-
-	@AfterEach
-	void tearDown() {
-
-	}
 
 	@Test
 	void 다리_생성_테스트() {
@@ -38,44 +32,6 @@ class ApplicationTest extends NsTest {
 				"[   | O |   ]",
 				"게임 성공 여부: 성공",
 				"총 시도한 횟수: 1"
-			);
-
-			int upSideIndex = output().indexOf("[ O |   | O ]");
-			int downSideIndex = output().indexOf("[   | O |   ]");
-			assertThat(upSideIndex).isLessThan(downSideIndex);
-		}, 1, 0, 1);
-	}
-
-	@Test
-	void 기능_실패테스트() {
-		assertRandomNumberInRangeTest(() -> {
-			run("3", "U", "U", "Q");
-			assertThat(output()).contains(
-				"게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)",
-				"최종 게임 결과",
-				"[ O | X ]",
-				"[   |   ]",
-				"게임 성공 여부: 실패",
-				"총 시도한 횟수: 1"
-			);
-
-			int upSideIndex = output().indexOf("[ O | X ]");
-			int downSideIndex = output().indexOf("[   |   ]");
-			assertThat(upSideIndex).isLessThan(downSideIndex);
-		}, 1, 0, 1);
-	}
-
-	@Test
-	void 기능_실패_재시작테스트() {
-		assertRandomNumberInRangeTest(() -> {
-			run("3", "U", "U", "R", "U", "D", "U");
-			assertThat(output()).contains(
-				"게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)",
-				"최종 게임 결과",
-				"[ O |   | O ]",
-				"[   | O |   ]",
-				"게임 성공 여부: 성공",
-				"총 시도한 횟수: 2"
 			);
 
 			int upSideIndex = output().indexOf("[ O |   | O ]");
