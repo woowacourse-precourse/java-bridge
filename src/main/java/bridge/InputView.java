bab9ea0d367f649b2781a5ddd808cc9691a17f98
yeop0740 <ssucgoo@gmail.com>
2022-11-22 22:03:19 +0900

    feat(InputView): 입력받은 길이가 범위 내에 존재하는지 확인하는 기능구현(validateBridgeSize)

--- src/main/java/bridge/InputView.java
+++ src/main/java/bridge/InputView.java
@@ -37,4 +37,10 @@ public class InputView {
     public String readGameCommand() {
         return null;
     }
+
+    private void validateBridgeSize(int bridgeSize) throws IllegalArgumentException {
+        if (bridgeSize < 3 || bridgeSize > 20) {
+            throw new IllegalArgumentException("[ERROR] 다리 길이는 3부터 20 사이의 숫자여야 합니다.");
+        }
+    }
 }
