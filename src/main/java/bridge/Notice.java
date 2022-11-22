419da875d98c7b9eb8235e784e84d8ef67ac5b14
고동우 <shp05179@gmail.com>
2022-11-22 17:49:36 +0900

    refactor: 에러메세지 Enum으로 관리
    기존
    ValidCheck에서 상수로 관리
    
    수정후
    Notice Enum에 에러메세지를 추가

--- src/main/java/bridge/Notice.java
+++ src/main/java/bridge/Notice.java
@@ -8,7 +8,10 @@ public enum Notice {
     REQUEST_RETRY_QUIT("게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)"),
     FINAL_RESULT("최종 게임 결과"),
     SUCCESS_OR_NOT("게임 성공 여부: "),
-    TRIES("총 시도한 횟수: ");
+    TRIES("총 시도한 횟수: "),
+    BRIDGE_LENGTH_ERROR("다리 길이는 3부터 20 사이의 숫자여야 합니다."),
+    WRONG_DIRECTION_ERROR("정확히 U나 D로 입력해주세요."),
+    WRONG_QUIT_ERROR("정확히 R이나 U로 입력해주세요.");
 
     private String message;
 
--- src/main/java/bridge/ValidCheck.java
+++ src/main/java/bridge/ValidCheck.java
@@ -1,16 +1,13 @@
 package bridge;
 
 public class ValidCheck {
-    private static final String BRIDGE_LENGTH_ERROR = "다리 길이는 3부터 20 사이의 숫자여야 합니다.";
-    private static final String WRONG_DIRECTION_ERROR = "정확히 U나 D로 입력해주세요.";
-    private static final String WRONG_QUIT_ERROR = "정확히 R이나 U로 입력해주세요.";
 
     private static int numberFormat(String inputVal) {
         int bridgeSize;
         try {
             bridgeSize = Integer.parseInt(inputVal);
         } catch (NumberFormatException e) {
-            throw new IllegalArgumentException(BRIDGE_LENGTH_ERROR);
+            throw new IllegalArgumentException(Notice.BRIDGE_LENGTH_ERROR.getMessage());
         }
         return bridgeSize;
     }
@@ -18,20 +15,20 @@ public class ValidCheck {
     public static int bridgeSize(String inputVal) {
         int bridgeSize = numberFormat(inputVal);
         if (bridgeSize < 3 || bridgeSize > 20) {
-            throw new IllegalArgumentException(BRIDGE_LENGTH_ERROR);
+            throw new IllegalArgumentException(Notice.BRIDGE_LENGTH_ERROR.getMessage());
         }
         return bridgeSize;
     }
 
     public static void upOrDown(String inputVal) {
         if (!inputVal.equals("D") && !inputVal.equals("U")) {
-            throw new IllegalArgumentException(WRONG_DIRECTION_ERROR);
+            throw new IllegalArgumentException(Notice.WRONG_DIRECTION_ERROR.getMessage());
         }
     }
 
     public static void retryOrQuit(String inputVal) {
         if (!inputVal.equals("R") && !inputVal.equals("Q")) {
-            throw new IllegalArgumentException(WRONG_QUIT_ERROR);
+            throw new IllegalArgumentException(Notice.WRONG_QUIT_ERROR.getMessage());
         }
     }
 
