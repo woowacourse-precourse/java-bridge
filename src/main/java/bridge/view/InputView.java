91a302e1a5b7ec41056b275e8ee4b66050ecc506
이정우 <acg6138@naver.com>
2022-11-22 15:58:45 +0900

    refactor(InputView): validate 메서드 타입 변경 void -> boolean, 로직 수정

--- src/main/java/bridge/view/InputView.java
+++ src/main/java/bridge/view/InputView.java
@@ -5,8 +5,8 @@ import camp.nextstep.edu.missionutils.Console;
 public class InputView {
     private static final String GAME_START_MESSAGE = "다리 건너기 게임을 시작합니다.";
     private static final String BRIDGE_SIZE_INPUT_MESSAGE = System.lineSeparator() + "다리의 길이를 입력해주세요.";
-    private static final String MOVE_INPUT_MESSAGE = "이동할 칸을 선택해주세요. (위: U, 아래: D)";
-    private static final String RETRY_INPUT_MESSAGE = "게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)";
+    private static final String MOVE_INPUT_MESSAGE = System.lineSeparator() + "이동할 칸을 선택해주세요. (위: U, 아래: D)";
+    private static final String RETRY_INPUT_MESSAGE = System.lineSeparator() + "게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)";
     private static final String WRONG_BRIDGE_SIZE_MESSAGE = "[ERROR] 다리 길이는 3부터 20사이의 숫자여야 합니다.";
     private static final String WRONG_DIRECTION_MESSAGE = "[ERROR] 이동할 칸은 U 또는 D만 입력할 수 있습니다.";
     private static final String WRONG_COMMAND_MESSAGE = "[ERROR] 게임 재시작 여부는 R 또는 Q만 입력할 수 있습니다.";
@@ -18,11 +18,8 @@ public class InputView {
     public int readBridgeSize() {
         System.out.println(BRIDGE_SIZE_INPUT_MESSAGE);
         try {
-            String input = Console.readLine();
-            validateBridgeSize(input);
-            System.out.println();
-            return Integer.parseInt(input);
-        }catch (IllegalArgumentException e) {
+            return Integer.parseInt(validateBridgeSize(Console.readLine()));
+        } catch (IllegalArgumentException e) {
             System.out.println(e.getMessage());
         }
         return readBridgeSize();
@@ -30,10 +27,8 @@ public class InputView {
 
     public String readMoving() {
         System.out.println(MOVE_INPUT_MESSAGE);
-        try{
-            String input = Console.readLine();
-            validateMoveInput(input);
-            return input;
+        try {
+            return validateMoveInput(Console.readLine());
         } catch (IllegalArgumentException e) {
             System.out.println(e.getMessage());
         }
@@ -43,19 +38,18 @@ public class InputView {
     public String readGameCommand() {
         System.out.println(RETRY_INPUT_MESSAGE);
         try {
-            String input = Console.readLine();
-            validateCommand(input);
-            return input;
-        }catch (IllegalArgumentException e) {
+            return validateCommand(Console.readLine());
+        } catch (IllegalArgumentException e) {
             System.out.println(e.getMessage());
         }
         return readGameCommand();
     }
 
-    public static void validateBridgeSize(String input) {
+    public static String validateBridgeSize(String input) {
         if (!input.matches("^[0-9]+") || !isInRange(Integer.parseInt(input))) {
             throw new IllegalArgumentException(WRONG_BRIDGE_SIZE_MESSAGE);
         }
+        return input;
     }
 
     public static boolean isInRange(int input) {
@@ -65,15 +59,17 @@ public class InputView {
         return false;
     }
 
-    public static void validateMoveInput(String input) {
+    public static String validateMoveInput(String input) {
         if (!input.equals("U") && !input.equals("D")) {
             throw new IllegalArgumentException(WRONG_DIRECTION_MESSAGE);
         }
+        return input;
     }
 
-    public static void validateCommand(String input) {
+    public static String validateCommand(String input) {
         if (!input.equals("Q") && !input.equals("R")) {
             throw new IllegalArgumentException(WRONG_COMMAND_MESSAGE);
         }
+        return input;
     }
 }
