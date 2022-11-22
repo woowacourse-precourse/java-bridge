a486c4574ae52780d561e07f6fa852478abad3bd
CAEL0 <kjongmin26@gmail.com>
2022-11-22 15:11:00 +0900

    test(BridgeGameTest): 게임 재시작 여부가 R이나 Q가 아니면 예외 발생

--- src/test/java/bridge/domain/BridgeGameTest.java
+++ src/test/java/bridge/domain/BridgeGameTest.java
@@ -16,4 +16,13 @@ public class BridgeGameTest {
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("[ERROR] 입력은 U 또는 D이어야 합니다.");
     }
+
+    @DisplayName("게임 재시작 여부가 R이나 Q가 아니면 예외를 발생한다.")
+    @ParameterizedTest
+    @ValueSource(strings = {"a", "r", "q", "RR", "RQ", "QQ", " R", "R ", " R "})
+    void ThrowExceptionIfGameCommandIsIncorrect(String gameCommand) {
+        assertThatThrownBy(() -> new BridgeGame("5").retry(gameCommand))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("[ERROR] 입력은 R 또는 Q이어야 합니다.");
+    }
 }
