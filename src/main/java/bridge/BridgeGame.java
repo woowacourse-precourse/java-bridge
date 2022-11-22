cf6dfabf8920464ccf4bbeaa9c66e4239e3bdf85
JungYoonshin <sju08227@naver.com>
2022-11-22 23:46:33 +0900

    feat: 출력 기능 추가

--- src/main/java/bridge/BridgeGame.java
+++ src/main/java/bridge/BridgeGame.java
@@ -1,12 +1,19 @@
 package bridge;
 
+import java.util.ArrayList;
 import java.util.List;
+import view.OutputView;
 
 /**
  * 다리 건너기 게임을 관리하는 클래스
  */
 public class BridgeGame {
+
     private static int i = 0;
+    private OutputView outputView = new OutputView();
+    private static List<String> moveList = new ArrayList<>();
+
+    private boolean success = false;
 
     /**
      * 사용자가 칸을 이동할 때 사용하는 메서드
@@ -14,9 +21,12 @@ public class BridgeGame {
      * 이동을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
     public void move(String move, List<String> bridge) {
-        if(bridge.get(i).equals(move)){
-
+        if (bridge.get(i).equals(move)) {
+            success = true;
         }
+        moveList.add(move);
+        System.out.println(success);
+        outputView.printMap(moveList, success);
         retry();
         i += 1;
     }
--- src/main/java/view/OutputView.java
+++ src/main/java/view/OutputView.java
@@ -1,6 +1,7 @@
 package view;
 
 import domain.Message;
+import java.util.List;
 
 /**
  * 사용자에게 게임 진행 상황과 결과를 출력하는 역할을 한다.
@@ -16,14 +17,49 @@ public class OutputView {
      * <p>
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void printMap() {
+    public void printMap(List<String> moveList, boolean success) {
+        System.out.println(getRows(moveList, success, "U"));
+        System.out.println(getRows(moveList, success, "D"));
     }
+
+    public String getRows(List<String> moveList, boolean success, String position){
+        StringBuilder result = new StringBuilder();
+        for (int i = 0; i < moveList.size(); i ++) {
+            String temp = "   |";
+            if (moveList.get(i).equals(position)) {
+                temp = appendElement(success, moveList.size() -1 == i);
+            }
+            result.append(temp);
+        }
+        return "[" + result.substring(0, result.length() - 1) + "]";
+    }
+
+    public String appendElement(boolean success, boolean last) {
+        String temp = "";
+        if (!success && last) {
+            temp = " X |";
+        }
+        if (success){
+            temp = " O |";
+        }
+        return temp;
+    }
+
+    public static void printInitialMap(){
+        System.out.printf("[");
+    }
+
+    public static void printLastMap(){
+        System.out.println("]");
+    }
+
 
     /**
      * 게임의 최종 결과를 정해진 형식에 맞춰 출력한다.
      * <p>
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
+
     public void printResult() {
     }
 }
