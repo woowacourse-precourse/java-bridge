b2553e92724a698d1c45e64c395bb11c3622f392
baekeunsun <eunsun5460@gmail.com>
2022-11-22 18:25:46 +0900

    test: 게임 흐름 테스트

--- src/test/java/bridge/GameTest.java
+++ src/test/java/bridge/GameTest.java
@@ -1,9 +1,6 @@
 package bridge;
 
-import bridge.message.ErrorMessage;
-import bridge.message.PrintMessage;
 import camp.nextstep.edu.missionutils.test.NsTest;
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
@@ -14,7 +11,6 @@ import java.io.InputStream;
 import java.lang.reflect.Method;
 
 import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeTest;
-import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.*;
@@ -40,6 +36,52 @@ class GameTest extends NsTest {
         int value = (int)retryMethod.invoke(1);
 
         assertTrue(value == -1);
+    }
+
+    @DisplayName("게임 흐름이 잘 흘러가는지 확인_성공")
+    @Test
+    void testGameSuccess() {
+        assertRandomNumberInRangeTest(() -> {
+            run("5", "U", "U", "D", "U", "D");
+            assertThat(output()).contains(
+                    "최종 게임 결과",
+                    "[ O | O |   | O |   ]",
+                    "[   |   | O |   | O ]",
+                    "게임 성공 여부: 성공",
+                    "총 시도한 횟수: 1"
+            );
+
+            int upSideIndex = output().indexOf("[ O | O |   | O |   ]");
+            int downSideIndex = output().indexOf("[   |   | O |   | O ]");
+            assertThat(upSideIndex).isLessThan(downSideIndex);
+        }, 1, 1, 0, 1, 0);
+    }
+
+    @DisplayName("게임 흐름이 잘 흘러가는지 확인_실패")
+    @Test
+    void testGameFailed() {
+        assertRandomNumberInRangeTest(() -> {
+            run("5", "U", "U", "D", "U", "D","Q");
+            assertThat(output()).contains(
+                    "최종 게임 결과",
+                    "[ O | O |   | O |   ]",
+                    "[   |   | O |   | X ]",
+                    "게임 성공 여부: 실패",
+                    "총 시도한 횟수: 1"
+            );
+
+            int upSideIndex = output().indexOf("[ O | O |   | O |   ]");
+            int downSideIndex = output().indexOf("[   |   | O |   | X ]");
+            assertThat(upSideIndex).isLessThan(downSideIndex);
+        }, 1, 1, 0, 1, 1);
+    }
+
+    @DisplayName("게임 흐름 예외 확인")
+    @Test
+    void testGameException() {
+        assertThatThrownBy(() ->
+                runException("3, K"))
+                .isInstanceOf(IllegalArgumentException.class);
     }
 
     @Override
