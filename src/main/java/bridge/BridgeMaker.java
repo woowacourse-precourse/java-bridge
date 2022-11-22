ec23feb80d4ae12918d75ebfe9870a9796bd2663
taejeong <sontaewjd@nate.com>
2022-11-22 15:52:18 +0900

    feat: 입력된 사이즈가 범위를 벗어나면 예외 처리
    3미만 20초과 시 예외 처리

--- src/main/java/bridge/BridgeMaker.java
+++ src/main/java/bridge/BridgeMaker.java
@@ -18,16 +18,22 @@ public class BridgeMaker {
      * @param size 다리의 길이
      * @return 입력받은 길이에 해당하는 다리 모양. 위 칸이면 "U", 아래 칸이면 "D"로 표현해야 한다.
      */
-    public List<String> makeBridge(int size) {
+    public List<String> makeBridge(int size) throws IllegalArgumentException{
         List<String> ret = new ArrayList<>();
-        
+        if (size < ConstInt.MIN_SIZE || size > ConstInt.MAX_SIZE){
+            throw new IllegalArgumentException(ConstString.INPUT_SIZE_ERROR);
+        }
         for (int i = 0; i < size; i++){
-            if (bridgeNumberGenerator.generate() == ConstInt.UP){
-                ret.add(ConstString.UP);
-                continue;
-            }
-            ret.add(ConstString.DOWN);
+            makeOneBridge(ret);
         }
         return ret;
+    }
+
+    private void makeOneBridge(List<String> list) {
+        if (bridgeNumberGenerator.generate() == ConstInt.UP) {
+            list.add(ConstString.UP);
+            return;
+        }
+        list.add(ConstString.DOWN);
     }
 }
