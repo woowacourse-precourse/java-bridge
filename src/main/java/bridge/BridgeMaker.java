fdc6de3ee1e34b28faa079f49e2a18e44e0a0b8e
Mr.Kim <86274253+Joajy@users.noreply.github.com>
2022-11-22 18:38:26 +0900

    refactor(BridgeMaker): To increase readability

--- src/main/java/bridge/BridgeMaker.java
+++ src/main/java/bridge/BridgeMaker.java
@@ -1,16 +1,15 @@
-package bridge;
+package bridge.Model;
 
 import java.util.ArrayList;
 import java.util.List;
 
+import bridge.Constant.Constant;
+
 public class BridgeMaker {
-
     private final BridgeNumberGenerator bridgeNumberGenerator;
-
     public BridgeMaker(BridgeNumberGenerator bridgeNumberGenerator) {
         this.bridgeNumberGenerator = bridgeNumberGenerator;
     }
-
     /**
      * @param size 다리의 길이
      * @return 입력받은 길이에 해당하는 다리 모양. 위 칸이면 "U", 아래 칸이면 "D"로 표현해야 한다.
