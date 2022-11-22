c23f7626529401a92e20d89913427479ea8c1975
Song kyeong seok <skj0922@kookmin.ac.kr>
2022-11-22 15:38:33 +0900

    test(GameStatusTest): 게임 상태를 확인하는 테스트 메서드 구현

---
+++ src/test/java/bridge/domain/status/GameStatusTest.java
@@ -0,0 +1,17 @@
+package bridge.domain.status;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class GameStatusTest {
+
+    @Test
+    @DisplayName("게임 상태를 확인하는 테스트")
+    public void isGameStatusTest() {
+        GameStatus gameStatus = GameStatus.RETRY;
+
+        Assertions.assertThat(gameStatus.isGameStatus(GameStatus.RETRY)).isTrue();
+    }
+
+}
