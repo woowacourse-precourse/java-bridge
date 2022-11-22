ab709f50cef711c2b2ddce70e8987d1f148b0490
LeeJejune <leejejune0625@gmail.com>
2022-11-22 17:28:10 +0900

    feat: 움직임을 통한 메서드 구현

--- src/main/java/bridge/domain/BridgeVisited.java
+++ src/main/java/bridge/domain/BridgeVisited.java
@@ -6,4 +6,9 @@ import java.util.List;
 public class BridgeVisited {
     private final List<BridgeGamePosition> positions = new ArrayList<>();
     private final BridgeGameMoveInfo moveInfo = new BridgeGameMoveInfo();
+
+    public void move(final BridgeGamePosition move, final Bridge bridge) {
+        moveInfo.moveInfo(move, bridge.isEqual(positions.size(), move));
+        positions.add(move);
+    }
 }
