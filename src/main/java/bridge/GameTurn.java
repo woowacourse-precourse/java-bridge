513655e740e617837b4a91e69d118666ae95430d
Juuuic <sn990908a@gmail.com>
2022-11-22 21:22:32 +0900

    fix: 다리길이 입력 후 예외 처리 기능 수정

--- src/main/java/bridge/GameTurn.java
+++ src/main/java/bridge/GameTurn.java
@@ -2,6 +2,13 @@ package bridge;
 
 public class GameTurn {
 
+    //클래스 변수로 설정
+    private static final InputView inputView = new InputView();
+    private static final OutputView outputView = new OutputView();
+    private static final BridgeGame bridgeGame = new BridgeGame();
+    public static String bridgeInput;
+    public static int bridgeLength = 0;
+
     public static void BridgeGameStart(){
         //게임 시작
     }
@@ -11,8 +18,19 @@ public class GameTurn {
         //한번 생성한 다리로 계속 진행
     }
 
-    public static void getBridgeLength(){
+    public static int getBridgeLength(){
         //다리 길이 입력 받고 예외 처리까지
+        try {
+            bridgeInput = inputView.readBridgeSize();
+            bridgeLength = Integer.parseInt(bridgeInput);
+        }
+        catch (NumberFormatException e){
+            throw new IllegalArgumentException("[ERROR] 다리 길이는 숫자여야 합니다");
+        }
+        if(bridgeLength < 3 || bridgeLength > 20) {
+            throw new IllegalArgumentException("[ERROR] 다리 길이는 3과 20 사이의 숫자여야 합니다");
+        }
+        return bridgeLength;
     }
 
     public static void ExceptionBridgeDirection(){
