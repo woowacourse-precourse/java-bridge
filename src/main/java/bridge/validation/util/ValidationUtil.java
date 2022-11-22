4414e1fcacd913893f6a0cf44a73126f7199ed4a
SeongbinPark <binni0515@gmail.com>
2022-11-22 22:23:33 +0900

    refactor(ValidationUtil): change to constants

--- src/main/java/bridge/validation/util/ValidationUtil.java
+++ src/main/java/bridge/validation/util/ValidationUtil.java
@@ -1,5 +1,13 @@
 package bridge.validation.util;
 
+import static bridge.Bridge.INPUT_SIZE;
+import static bridge.Bridge.MAX_BRIDGE_SIZE;
+import static bridge.Bridge.MIN_BRIDGE_SIZE;
+import static bridge.constant.mark.InputMark.DOWN;
+import static bridge.constant.mark.InputMark.QUIT;
+import static bridge.constant.mark.InputMark.RETRY;
+import static bridge.constant.mark.InputMark.UP;
+
 public class ValidationUtil {
 
     public static boolean isInteger(String bridgeSize) {
@@ -8,24 +16,25 @@ public class ValidationUtil {
         } catch (NumberFormatException e) {
             return false;
         }
+
         return true;
     }
 
     public static boolean isWithinValidRange(String bridgeSize) {
         int input = Integer.parseInt(bridgeSize);
 
-        return 3 <= input && input <= 20;
+        return MIN_BRIDGE_SIZE <= input && input <= MAX_BRIDGE_SIZE;
     }
 
     public static boolean isOneCharacter(String input) {
-        return input.length() == 1;
+        return input.length() == INPUT_SIZE;
     }
 
     public static boolean isValidMoving(String moving) {
-        return moving.equals("U") || moving.equals("D");
+        return moving.equals(UP.getMark()) || moving.equals(DOWN.getMark());
     }
 
     public static boolean isValidGameCommand(String gameCommand) {
-        return gameCommand.equals("R") || gameCommand.equals("Q");
+        return gameCommand.equals(RETRY.getMark()) || gameCommand.equals(QUIT.getMark());
     }
 }
