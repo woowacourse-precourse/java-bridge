f8dc6b2799facead7802511a054bdd44d99c905a
gynhyunkim <gyeonghyun.kimm@gmail.com>
2022-11-22 16:38:19 +0900

    feat: 사용자가 이동할 칸 입력

--- src/main/java/bridge/InputView.java
+++ src/main/java/bridge/InputView.java
@@ -7,22 +7,23 @@ import camp.nextstep.edu.missionutils.Console;
  */
 public class InputView {
 
-    private final String READ_BRIDGE_SIZE = "다리의 길이를 입력해주세요.";
+    private final String REQUEST_BRIDGE_SIZE = "다리의 길이를 입력해주세요.";
+    private final String REQUEST_BRIDGE_MOVING = "이동할 칸을 선택해주세요. (위: U, 아래: D)";
 
     /**
      * 다리의 길이를 입력받는다.
      */
     public int readBridgeSize() {
-        System.out.println(READ_BRIDGE_SIZE);
-        int size = Utils.parseInt(Console.readLine());
-        return size;
+        System.out.println(REQUEST_BRIDGE_SIZE);
+        return Utils.parseInt(Console.readLine());
     }
 
     /**
      * 사용자가 이동할 칸을 입력받는다.
      */
     public String readMoving() {
-        return null;
+        System.out.println(REQUEST_BRIDGE_MOVING);
+        return Console.readLine();
     }
 
     /**
