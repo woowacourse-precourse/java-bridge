0aaed42b639205a4cf18315056af22600c199e80
JunTaeINC <juntea3523@naver.com>
2022-11-22 22:10:23 +0900

    test : (InputView) getMoveString 테스트 기능 구현

--- src/test/java/bridge/InputViewTest.java
+++ src/test/java/bridge/InputViewTest.java
@@ -43,4 +43,33 @@ class InputViewTest {
             assertEquals(ER_BRIDGE_LENGTH_INPUT.getMessage(), illegalArgumentException.getMessage());
         }
     }
+
+    @DisplayName("이동할 값 입력시 U,D가 아닐경우 예외가 발생한다.")
+    @Nested
+    class 다리_이동_입력_예외 {
+        @Test
+        void case_1() {
+            String input = "업";
+            OutputStream out = new ByteArrayOutputStream();
+            System.setOut(new PrintStream(out));
+            InputStream in = new ByteArrayInputStream(input.getBytes());
+            System.setIn(in);
+            IllegalArgumentException illegalArgumentException = assertThrows(IllegalArgumentException.class, () ->
+                    inputView.getMoveString());
+            assertEquals(ER_NOT_UP_OR_DOWN.getMessage(), illegalArgumentException.getMessage());
+        }
+
+        @Test
+        void case_2() {
+            String input = "u";
+            OutputStream out = new ByteArrayOutputStream();
+            System.setOut(new PrintStream(out));
+            InputStream in = new ByteArrayInputStream(input.getBytes());
+            System.setIn(in);
+            IllegalArgumentException illegalArgumentException = assertThrows(IllegalArgumentException.class, () ->
+                    inputView.getMoveString());
+            assertEquals(ER_NOT_UP_OR_DOWN.getMessage(), illegalArgumentException.getMessage());
+        }
+    }
+
 }
