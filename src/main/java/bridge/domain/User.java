d82d43cfa9bdabfb664e23f844e9eac9f84b0ae2
eom-tae-in <eti0728@daum.net>
2022-11-22 21:26:08 +0900

    feat(User): User 필드 및 생성자 구현

---
+++ src/main/java/bridge/domain/User.java
@@ -0,0 +1,11 @@
+package bridge.domain;
+
+public class User {
+    private int playCount;
+    private boolean quit;
+
+    public User() {
+        this.playCount = 1;
+        quit = false;
+    }
+}
