92398260acc781867d7938e27e7919d8dee2e504
sunghyuki <sunghyuk1609@gmail.com>
2022-11-22 10:07:09 +0900

    feat : 다리 길이 입력 기능 추가

--- src/main/java/bridge/InputView.java
+++ src/main/java/bridge/InputView.java
@@ -1,15 +1,27 @@
 package bridge;
 
+import camp.nextstep.edu.missionutils.Console;
+
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
  */
 public class InputView {
+    private static final String BRIDGE_SIZE_OUT_OF_RANGE = "[ERROR] 다리 길이는 3부터 20 사이의 숫자여야 합니다.";
+    private static final String GAME_START = "다리 건너기 게임을 시작합니다.";
+    private static final String INPUT_BRIDGE_SIZE = "다리의 길이를 입력해주세요.";
+    private static final int BRIDGE_SIZE_MIN_VALUE = 3;
+    private static final int BRIDGE_SIZE_MAX_VALUE = 20;
+    private static final String NEW_LINE = "\n";
 
     /**
      * 다리의 길이를 입력받는다.
      */
     public int readBridgeSize() {
-        return 0;
+        System.out.println(GAME_START + NEW_LINE);
+        System.out.println(INPUT_BRIDGE_SIZE);
+        String bridgeSize = Console.readLine();
+        validateBridgeSize(bridgeSize);
+        return Integer.parseInt(bridgeSize);
     }
 
     /**
@@ -24,5 +36,15 @@ public class InputView {
      */
     public String readGameCommand() {
         return null;
+    }
+
+    private void validateBridgeSize(String bridgeSize) {
+        if (isNotInBridgeSizeRange(Integer.parseInt(bridgeSize))) {
+            throw new IllegalArgumentException(BRIDGE_SIZE_OUT_OF_RANGE);
+        }
+    }
+
+    private boolean isNotInBridgeSizeRange(int bridgeSize) {
+        return bridgeSize < BRIDGE_SIZE_MIN_VALUE || bridgeSize > BRIDGE_SIZE_MAX_VALUE;
     }
 }
