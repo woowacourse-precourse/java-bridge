a9a34db30e4e52d7f194263096140ade2a63fef4
Dabeen Jeong <hello70825@gmail.com>
2022-11-22 17:27:47 +0900

    feat: 사용자가 칸을 이동하는 기능 추가

--- src/main/java/bridge/BridgeGame.java
+++ src/main/java/bridge/BridgeGame.java
@@ -48,7 +48,9 @@ public class BridgeGame {
      * <p>
      * 이동을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void move() {
+    public void move(String userMove) {
+        bridgePrintMaker.addStepBridge(userMove, Direction.convertDirection(answerBridge.get(position)));
+        position++;
     }
 
     /**
