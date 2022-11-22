15decf8880436dafd353b76a2be4873c24a1bc28
gitFILO <x1xgudwls@naver.com>
2022-11-22 20:03:31 +0900

    [구현 기능 목록 5번 추가]

--- src/main/java/bridge/GameSystem.java
+++ src/main/java/bridge/GameSystem.java
@@ -14,6 +14,41 @@ public class GameSystem {
         this.bridgeSize = answerBridge.size;
     }
 
+    public Boolean move(int index){
+        String choice = InputView.readMoving();
+        if(Objects.equals(choice, answerBridge.answer.get(index))){
+            System.out.println("SUCCESS");
+            moveSuccess(index,choice);
+            OutputView.printMap(player,index);
+            return true;
+        }
+        System.out.println("FAIL");
+        moveFail(index,choice);
+        OutputView.printMap(player,index);
+        return false;
+    }
+
+    public void moveSuccess(int index, String choice){
+        if(Objects.equals(choice,"U")) {
+            player.bridges.upBridge.add(index,"O");
+            player.bridges.downBridge.add(index," ");
+        }
+        if(Objects.equals(choice, "D")){
+            player.bridges.upBridge.add(index," ");
+            player.bridges.downBridge.add(index,"O");
+        }
+    }
+    public void moveFail(int index,String choice){
+        if(Objects.equals(choice,"U")) {
+            player.bridges.upBridge.add(index,"X");
+            player.bridges.downBridge.add(index," ");
+        }
+        if(Objects.equals(choice,"D")){
+            player.bridges.upBridge.add(index," ");
+            player.bridges.downBridge.add(index,"X");
+        }
+    }
+
     public void printResult(Boolean moveSuccess,int trialNumber){
         OutputView.printFinalResult(player.bridges,moveSuccess,trialNumber);
     }
