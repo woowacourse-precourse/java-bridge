7f1be339c1183db64d4380a2eed2a6696bc66684
고동우 <shp05179@gmail.com>
2022-11-22 17:25:56 +0900

    refactor: 게임 컨트롤러 생성
    기존 Application 에서 하던 initGame과 gameFlow를
    별도의 Controller를 만들어 담당시킴.

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -1,47 +1,14 @@
 package bridge;
 
-import java.util.List;
-
 public class Application {
 
-    private static String QUIT_SIGN = "Q";
-
     public static void main(String[] args) {
-        BridgeGame game = initGame();
+        GameController controller = new DefaultGameController();
+        controller.initGame();
         while (true) {
-            if (!gameFlow(game)) {
+            if (!controller.gameFlow()) {
                 break;
             }
         }
-    }
-
-    private static BridgeGame initGame() {
-        BridgeNumberGenerator randomNumberGenerator = new BridgeRandomNumberGenerator();
-        BridgeMaker bridgeMaker = new BridgeMaker(randomNumberGenerator);
-        OutputView.printStartGame();
-        int size = InputView.readBridgeSizeUntilSuccess();
-        return new BridgeGame(bridgeMaker.makeBridge(size));
-    }
-
-    private static boolean gameFlow(BridgeGame game) {
-        String direction = InputView.readMovingUntilSuccess();
-        boolean isAvailable = game.move(direction);
-        OutputView.printMap(game.getCrossedBridge(), isAvailable);
-        if (isEnd(isAvailable, game.getCrossedBridge(), game)) {
-            OutputView.printResult(game.getCrossedBridge(), isAvailable, game.getTries());
-            return false;
-        }
-        return true;
-    }
-
-    private static boolean isEnd(boolean isAvailable, List<String> history, BridgeGame game) {
-        if (isAvailable && game.mapSize() != history.size()) {
-            return false;
-        }
-        if (isAvailable || InputView.readGameCommandUntilSuccess().equals(QUIT_SIGN)) {
-            return true;
-        }
-        game.retry();
-        return false;
     }
 }
---
+++ src/main/java/bridge/DefaultGameController.java
@@ -0,0 +1,43 @@
+package bridge;
+
+import java.util.List;
+
+public class DefaultGameController implements GameController {
+    BridgeGame game;
+    private static final String QUIT_SIGN = "Q";
+
+    @Override
+    public void initGame() {
+        BridgeNumberGenerator randomNumberGenerator = new BridgeRandomNumberGenerator();
+        BridgeMaker bridgeMaker = new BridgeMaker(randomNumberGenerator);
+        OutputView.printStartGame();
+        int size = InputView.readBridgeSizeUntilSuccess();
+        game = new BridgeGame(bridgeMaker.makeBridge(size));
+    }
+
+    /**
+     * @return 게임 종료조건 충족시 false 리턴하면 된다
+     */
+    @Override
+    public boolean gameFlow() {
+        String direction = InputView.readMovingUntilSuccess();
+        boolean isAvailable = game.move(direction);
+        OutputView.printMap(game.getCrossedBridge(), isAvailable);
+        if (isEnd(isAvailable, game.getCrossedBridge(), game)) {
+            OutputView.printResult(game.getCrossedBridge(), isAvailable, game.getTries());
+            return false;
+        }
+        return true;
+    }
+
+    private boolean isEnd(boolean isAvailable, List<String> history, BridgeGame game) {
+        if (isAvailable && game.mapSize() != history.size()) {
+            return false;
+        }
+        if (isAvailable || InputView.readGameCommandUntilSuccess().equals(QUIT_SIGN)) {
+            return true;
+        }
+        game.retry();
+        return false;
+    }
+}
