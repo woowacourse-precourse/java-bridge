75c04baa2de585fcf1cc6bcc836d6b513d8076bb
qpdh <74190329+qpdh@users.noreply.github.com>
2022-11-22 15:06:52 +0900

    Feat($Application) 생성 객체 변경
    Before
            new BridgeGame().start();
    After
            new BridgeGameManager().startGame();

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -1,9 +1,10 @@
 package bridge;
 
 public class Application {
+// TODO 상수 구현
 
     public static void main(String[] args) {
         // TODO: 프로그램 구현
-        new BridgeGame().start();
+        new BridgeGameManager().startGame();
     }
 }
