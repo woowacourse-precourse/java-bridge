b4f755fe228dcbd63ebe92f1f10677ca9aefe27f
Gibeom Nam <rlqja0523@naver.com>
2022-11-22 18:18:01 +0900

    refactor: 디렉토리 이동

--- src/test/java/bridge/service/BridgeMakerTest.java
+++ src/test/java/bridge/service/BridgeMakerTest.java
@@ -1,5 +1,7 @@
-package bridge;
+package bridge.service;
 
+import bridge.BridgeMaker;
+import bridge.BridgeRandomNumberGenerator;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
