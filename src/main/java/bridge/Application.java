fc343dd86028b8029aaa07b8e49f3d7d9159eaab
c1ce <chalicenuclear.gmail.com>
2022-11-22 20:37:43 +0900

    feat: main Application try/catch 예외 출력문 코드 추가

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -6,13 +6,13 @@ public class Application {
 
     public static void main(String[] args) {
         // TODO: 프로그램 구현
-        BridgeController bridgeController = new BridgeController();
-        BridgeGame bridgeGame = bridgeController.doBridgeMake();
+        try {
+            BridgeController bridgeController = new BridgeController();
+            BridgeGame bridgeGame = bridgeController.doBridgeMake();
+            while (true) if(!bridgeController.doBridgeMove(bridgeGame)) break;
 
-        while (true){
-            if(!bridgeController.doBridgeMove(bridgeGame)){
-
-            }
+        }catch (Exception e){
+            System.out.println(e);
         }
     }
 }
