36aa0250a3582c7992c256adc89edcace1eef34c
hyeongu <wancern@naver.com>
2022-11-22 23:30:47 +0900

    refactor(Application): main 분리

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -4,37 +4,39 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Application {
+    private static BridgeRandomNumberGenerator bridgeRandomNumberGenerator = new BridgeRandomNumberGenerator();
+    private static BridgeMaker bridgeMaker = new BridgeMaker(bridgeRandomNumberGenerator);
+    private static InputView inputView = new InputView();
+    private static OutputView outputView = new OutputView();
+    private static int size = inputView.readBridgeSize();
+    private static List<String> bridge = bridgeMaker.makeBridge(size);
 
     public static void main(String[] args) {
         try{
-            BridgeRandomNumberGenerator bridgeRandomNumberGenerator = new BridgeRandomNumberGenerator();
-            BridgeMaker bridgeMaker = new BridgeMaker(bridgeRandomNumberGenerator);
-            InputView inputView = new InputView();
-            OutputView outputView = new OutputView();
-
-            int size = inputView.readBridgeSize();
-            List<String> bridge = bridgeMaker.makeBridge(size);
-
             BridgeGame game = new BridgeGame(bridge);
-            List<String> lastMap = new ArrayList<>();
-            boolean flag = true;
+            playGame(game);
 
-            for(int i = 0; i < size; i++){
-                String moving = inputView.readMoving();
-                flag = game.move(moving);
-                lastMap = outputView.printMap(game.userMoving, flag);
-
-                if(!flag){
-                    if(!inputView.readGameCommand()){
-                        break;
-                    }
-                    game.retry();
-                    i = -1;
-                }
-            }
-            outputView.printResult(lastMap, flag, game.getCount());
         }catch(IllegalArgumentException e){
             System.out.println(e.getMessage());
         }
+    }
+
+    public static void playGame(BridgeGame game){
+        boolean flag = true;
+
+        for(int i = 0; i < size; i++){
+            String moving = inputView.readMoving();
+            flag = game.move(moving);
+            outputView.printMap(game.userMoving, flag);
+
+            if(!flag){
+                if(!inputView.readGameCommand()){
+                    break;
+                }
+                game.retry();
+                i = -1;
+            }
+        }
+        List<String> lastMap = new ArrayList<>();
     }
 }
