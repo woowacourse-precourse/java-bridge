402cbd84dc12651dd794aad795bd8d14de4bec9f
mgOh <issac_oh@naver.com>
2022-11-22 13:20:15 +0900

    feat: 게임을 시작하는 기능

--- src/main/java/bridge/Gaming.java
+++ src/main/java/bridge/Gaming.java
@@ -1,2 +1,29 @@
-package bridge;public class Gaming {
+package bridge;
+
+import java.util.List;
+
+public class Gaming {
+    private static List<String> bridge;
+    private String upBridgeMap;
+    private String DownBridgeMap;
+    private int gameTryCount;
+
+    Gaming(List<String> bridge, int turn){
+        this.bridge = bridge;
+        this.upBridgeMap = "";
+        this.DownBridgeMap = "";
+        this.gameTryCount = turn;
+    }
+    /** 게임을 시작하는 기능 */
+    public int startGame(){
+        gameLoop(0);
+        return 0;
+    }
+    public boolean gameLoop(int count){
+        while (true){
+            return false;
+        }
+    }
+
+
 }
