7feba49d1183eaa5b2ad1dc0b3365427c136aaf6
Sangin <ksiisk99@gmail.com>
2022-11-22 14:36:58 +0900

    test: 다리 생성을 각각의 테스트 코드에 넣기

--- src/test/java/bridge/BridgeGameTest.java
+++ src/test/java/bridge/BridgeGameTest.java
@@ -2,6 +2,7 @@ package bridge;
 
 import bridge.dto.Bridge;
 import bridge.dto.MoveResult;
+import bridge.dto.PathTravel;
 import bridge.service.BridgeGame;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
@@ -12,13 +13,11 @@ import java.util.List;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class BridgeGameTest {
-    private Bridge bridge;
     private BridgeGame bridgeGame;
     private Long playerId;
 
     @BeforeEach
     void 세팅() {
-        bridge = new Bridge(List.of("U", "D", "U", "D"));
         bridgeGame = new BridgeGame();
         playerId = bridgeGame.generatePlayer();
     }
@@ -26,6 +25,7 @@ public class BridgeGameTest {
     @DisplayName("플레이어 이동 실패")
     @Test
     void 플레이어_이동_실패() {
+        Bridge bridge =  new Bridge(List.of("U","D","U"));
         String position = "D";
 
         MoveResult moveResult = bridgeGame.move(playerId, bridge, position);
@@ -36,6 +36,7 @@ public class BridgeGameTest {
     @DisplayName("플레이어 이동 성공")
     @Test
     void 플레이어_이동_성공() {
+        Bridge bridge =  new Bridge(List.of("U","D","U"));
         String position = "U";
 
         MoveResult moveResult = bridgeGame.move(playerId, bridge, position);
