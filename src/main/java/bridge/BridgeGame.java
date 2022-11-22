d18975b4d0b49864119f38a1e18ee8a7b6bb5f6b
kjg1514 <kjg1514@naver.com>
2022-11-22 14:53:47 +0900

    feat : X가 나오면 종료

--- src/main/java/bridge/BridgeGame.java
+++ src/main/java/bridge/BridgeGame.java
@@ -2,7 +2,7 @@ package bridge;
 
 import java.util.ArrayList;
 import java.util.List;
-
+import java.util.Map;
 
 /**
  * 다리 건너기 게임을 관리하는 클래스
@@ -10,6 +10,7 @@ import java.util.List;
 public class BridgeGame {
     public List<String> bridgeU = new ArrayList<>();
     public List<String> bridgeD = new ArrayList<>();
+
     /**
      * 사용자가 칸을 이동할 때 사용하는 메서드
      * <p>
@@ -52,6 +53,11 @@ public class BridgeGame {
      * <p>
      * 재시작을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void retry() {
+    public boolean retry(Map<Integer, String> map) {
+        if (map.get(0).contains("X") || map.get(1).contains("X")) {
+            return false;
+        }
+        return true;
     }
+
 }
