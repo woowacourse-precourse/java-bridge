1177cfb2362a75df111ce9f59913c2cfbc33f4de
sunghyuki <sunghyuk1609@gmail.com>
2022-11-22 20:15:48 +0900

    feat : 게임 시작 문구 출력 기능 추가

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -9,33 +9,7 @@ public class Application {
         BridgeMaker bridgeMaker = new BridgeMaker(new BridgeRandomNumberGenerator());
         BridgeGameStarter bridgeGameStarter = new BridgeGameStarter(bridgeGame, bridgeMaker);
 
+        ConsoleUtils.printGameStart();
         bridgeGameStarter.start();
-    }
-
-    private static int inputBridgeSize() {
-        try {
-            return ConsoleUtils.inputBridgeSize();
-        } catch (IllegalArgumentException e) {
-            System.out.println(e.getMessage());
-            return inputBridgeSize();
-        }
-    }
-
-    private static String inputmoving() {
-        try {
-            return ConsoleUtils.inputMoving();
-        } catch (IllegalArgumentException e) {
-            System.out.println(e.getMessage());
-            return inputmoving();
-        }
-    }
-
-    private static String inputGameCommand() {
-        try {
-            return ConsoleUtils.inputGameCommand();
-        } catch (IllegalArgumentException e) {
-            System.out.println(e.getMessage());
-            return inputGameCommand();
-        }
     }
 }
