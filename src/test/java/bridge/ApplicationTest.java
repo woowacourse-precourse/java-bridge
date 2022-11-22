71b44b2586b8e28ce68aa469da55086e806bebdc
dsaf2007 <dsaf2007@naver.com>
2022-11-22 21:20:55 +0900

    fix : ApplicationTest에서 도메인 로직 테스트 삭제, BridgeGameTest에서 기존 테스트 삭제

--- src/test/java/bridge/ApplicationTest.java
+++ src/test/java/bridge/ApplicationTest.java
@@ -21,34 +21,7 @@ class ApplicationTest extends NsTest {
         assertThat(bridge).containsExactly("U", "D", "D");
     }
 
-    @Test
-    void 실패_후_종료_테스트() {
-        assertRandomNumberInRangeTest(() -> {
-            run("3", "U", "U", "Q");
-            assertThat(output()).contains(
-                    "최종 게임 결과",
-                    "[ O | X ]",
-                    "[   |   ]",
-                    "게임 성공 여부: 실패",
-                    "총 시도한 횟수: 1");
-        }, 1, 0, 1);
-    }
-
-    @Test
-    void 실패_후_재시도_성공_테스트(){
-        assertRandomNumberInRangeTest(() -> {
-            run("3", "U", "U","R", "U", "D","U");
-            assertThat(output()).contains(
-                    "게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)",
-                    "[ O | X ]",
-                    "[   |   ]",
-                    "최종 게임 결과",
-                    "[ O |   | O ]",
-                    "[   | O |   ]",
-                    "게임 성공 여부: 성공",
-                    "총 시도한 횟수: 2");
-        }, 1, 0, 1);
-    }
+   
 
     @Test
     void 기능_테스트() {
@@ -75,25 +48,7 @@ class ApplicationTest extends NsTest {
         });
     }
 
-    @Test
-    void commad_예외_테스트(){
-        assertRandomNumberInRangeTest(() -> {
-            runException("3", "U", "U", "X");
-            assertThat(output()).contains(
-            "게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)",
-            ERROR_MESSAGE);
-        }, 1, 0, 1);
-    }
-
-    @Test
-    void move_예외_테스트(){
-        assertRandomNumberInRangeTest(() -> {
-            runException("3", "K");
-            assertThat(output()).contains(
-            ERROR_MESSAGE);
-        }, 1, 0, 1);
-    }
-
+  
     @Override
     protected void runMain() {
         Application.main(new String[] {});
