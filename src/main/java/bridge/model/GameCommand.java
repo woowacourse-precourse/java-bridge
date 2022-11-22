93fb3cd67da0bf12f9786b5e75a0a9582c1cc991
MJoon-Jung <gjgjajaj31@gmail.com>
2022-11-22 16:48:13 +0900

    refactor<GameCommand>: 기존의 상수명을 값으로 하고 RETRY, QUIT으로 상수명 변경
    - 더 명확하게 의미를 전달하기 위해 R, Q를 대신하여 RETRY, QUIT으로 이름 변경

--- src/main/java/bridge/model/GameCommand.java
+++ src/main/java/bridge/model/GameCommand.java
@@ -1,15 +1,25 @@
 package bridge.model;
 
 import bridge.util.BridgeGameExceptionMessage;
+import java.util.Arrays;
 
 public enum GameCommand {
-    R, Q;
+    RESTART("R"), QUIT("Q");
+    private final String command;
+
+    GameCommand(String command) {
+        this.command = command;
+    }
 
     public static GameCommand fromInput(String gameCommand) {
-        try {
-            return valueOf(gameCommand);
-        } catch (IllegalArgumentException e) {
-            throw new IllegalArgumentException(BridgeGameExceptionMessage.COMMAND_NOT_MATCH.getMessage());
-        }
+        return Arrays.stream(values())
+                .filter(command -> command.getCommand().equals(gameCommand))
+                .findAny()
+                .orElseThrow(() ->
+                        new IllegalArgumentException(BridgeGameExceptionMessage.COMMAND_NOT_MATCH.getMessage()));
+    }
+
+    public String getCommand() {
+        return command;
     }
 }
