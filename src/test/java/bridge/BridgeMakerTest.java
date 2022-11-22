07f18f834c0cf72dfe689f00c2037b55d6c0818e
sora5421 <sojeoung536985@gmail.com>
2022-11-22 23:27:06 +0900

    feat : make BridgeMaker Test

---
+++ src/test/java/bridge/BridgeMakerTest.java
@@ -0,0 +1,17 @@
+package bridge;
+
+import static org.assertj.core.api.Assertions.*;
+
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class BridgeMakerTest {
+    BridgeMaker make = new BridgeMaker(new BridgeRandomNumberGenerator());
+
+    @DisplayName("랜덤으로 값을 생성하고 주어진 크기만큼 bridge 리스트 값을 생성한다")
+    @Test
+    void createBridgeList() {
+        assertThat(make.makeBridge(5).size()).isEqualTo(5);
+    }
+}
