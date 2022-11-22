8edaa549f168c30edc953a5b93d434b8f85b62f5
seung-hun2 <jungsh2328@naver.com>
2022-11-22 22:12:33 +0900

    - OutputView 클래스 checkBridge() 메서드 구현

--- src/main/java/bridge/OutputView.java
+++ src/main/java/bridge/OutputView.java
@@ -7,5 +7,14 @@ import java.util.List;
  * 사용자에게 게임 진행 상황과 결과를 출력하는 역할을 한다.
  */
 public class OutputView {
-
+    public String checkBridge(List<String> nextStep, List<String> Bridge , int i){
+        String str="";
+        if(nextStep.get(i).equals(Bridge.get(i))){
+            str=" O ";
+        }
+        if(!nextStep.get(i).equals(Bridge.get(i))){
+            str=" X ";
+        }
+        return str;
+    }
 }
