a85d442e2ebc341c8654d53e587c762639d3766d
Sangwoo Shin <sangwoonoel@naver.com>
2022-11-22 23:01:30 +0900

    refactor(BridgeMaker): 테스트 코드 리팩터링

--- src/test/java/bridge/BridgeMakerTest.java
+++ src/test/java/bridge/BridgeMakerTest.java
@@ -1,7 +1,9 @@
 package bridge;
 
 import bridge.domain.BridgeMaker;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import java.util.List;
 
@@ -9,11 +11,20 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class BridgeMakerTest {
 
-    @Test
-    void 다리_길이_테스트() {
-        BridgeNumberGenerator numberGenerator = new BridgeRandomNumberGenerator();
-        BridgeMaker bridgeMaker = new BridgeMaker(numberGenerator);
-        List<String> bridge = bridgeMaker.makeBridge(3);
-        assertThat(bridge.size()).isEqualTo(3);
+    private BridgeNumberGenerator numberGenerator;
+    private BridgeMaker bridgeMaker;
+
+    @BeforeEach
+    void setUp() {
+        numberGenerator = new BridgeRandomNumberGenerator();
+        bridgeMaker = new BridgeMaker(numberGenerator);
+    }
+
+    @ParameterizedTest
+    @CsvSource("3, 3, 10, 10, 20, 20")
+    void 다리_길이_테스트(int size, int expected) {
+        List<String> bridge = bridgeMaker.makeBridge(size);
+        int actual = bridge.size();
+        assertThat(actual).isEqualTo(expected);
     }
 }
