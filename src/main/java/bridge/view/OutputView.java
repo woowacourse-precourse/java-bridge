4f3377127acbc36ce60632820f2e9315245f3daf
DDing77 <devseo2021@gmail.com>
2022-11-22 19:40:16 +0900

    feat(view) : 이동후 진행 상황 출력 메소드

--- src/main/java/bridge/view/OutputView.java
+++ src/main/java/bridge/view/OutputView.java
@@ -1,6 +1,9 @@
 package bridge.view;
 
+import bridge.constant.command.Command;
 import bridge.constant.message.GameMessage;
+
+import java.util.List;
 
 /**
  * 사용자에게 게임 진행 상황과 결과를 출력하는 역할을 한다.
@@ -12,7 +15,38 @@ public class OutputView {
      * <p>
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void printMap() {
+    public static void printMap(List<String> bridge, List<String> selections) {
+        System.out.println(up(bridge, selections, Command.UP.getCommand()));
+        System.out.println(down(bridge, selections, Command.DOWN.getCommand()));
+    }
+
+    private static String up(List<String> bridge, List<String> selections, String position) {
+        return getString(bridge, selections, position);
+    }
+
+    private static String down(List<String> bridge, List<String> selections, String position) {
+        return getString(bridge, selections, position);
+    }
+
+    private static String getString(List<String> bridge, List<String> selections, String position) {
+        StringBuilder sb = new StringBuilder("[ ");
+
+        for (int i = 0; i < selections.size(); i++) {
+            sb.append(getMarkCommand(bridge.get(i), selections.get(i), position));
+            sb.append(" | ");
+        }
+
+        return sb.replace(sb.length() - 2, sb.length() - 1, "]").toString();
+    }
+
+    private static String getMarkCommand(String bridgeMark, String selectionMark, String stair) {
+        if (bridgeMark.equals(stair) && selectionMark.equals(Command.POSSIBLE.getCommand())) {
+            return Command.POSSIBLE.getCommand();
+        }
+        if (!bridgeMark.equals(stair) && selectionMark.equals(Command.IMPOSSIBLE.getCommand())) {
+            return Command.IMPOSSIBLE.getCommand();
+        }
+        return " ";
     }
 
     /**
