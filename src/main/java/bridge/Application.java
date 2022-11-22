20525954854693e669ba7226c132897ab36175c2
xxeol2 <jhj01177@naver.com>
2022-11-22 17:38:22 +0900

    feat: 예외 문구 출력

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -8,6 +8,10 @@ public class Application {
     public static void main(String[] args) {
         BridgeGameController bridgeGameController = new BridgeGameController(new InputView(),
                 new OutputView(), new BridgeMaker(new BridgeRandomNumberGenerator()));
-        bridgeGameController.play();
+        try {
+            bridgeGameController.play();
+        } catch (IllegalArgumentException | IllegalStateException e) {
+            System.out.println(e.getMessage());
+        }
     }
 }
