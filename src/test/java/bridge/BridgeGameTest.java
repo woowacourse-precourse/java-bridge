fecb371ec98e417b6aa89d59c371a4b27843d0d5
thousandcows <milcamboo@gmail.com>
2022-11-22 10:31:59 +0900

    feat: create createBridgeTest

--- src/test/java/bridge/BridgeGameTest.java
+++ src/test/java/bridge/BridgeGameTest.java
@@ -1,13 +1,17 @@
 package bridge;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import ui.InputView;
+import ui.OutputView;
 
 import java.util.List;
 
 import static bridge.ApplicationTest.*;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.util.Lists.newArrayList;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class BridgeGameTest {
 
@@ -18,5 +22,12 @@ public class BridgeGameTest {
         BridgeMaker bridgeMaker = new BridgeMaker(numberGenerator);
         List<String> newBridge = bridgeMaker.makeBridge(3);
         assertThat(newBridge).containsExactly("D", "U", "U");
+    }
+
+    @DisplayName("애플리케이션 실행 시 다리 길이를 입력받아 리턴한다.")
+    @Test
+    void createBridgeTest() {
+        BridgeGame bridgeGame = new BridgeGame(new OutputView(), new InputView());
+        assertEquals(bridgeGame.createBridge(5).size(), 5);
     }
 }
