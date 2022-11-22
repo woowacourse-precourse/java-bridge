1256a78e492762078dc24fe368b4b200d7c9d246
jxixeun <jxixeun@gmail.com>
2022-11-22 17:34:33 +0900

    feat: main 함수 작성

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -1,8 +1,16 @@
 package bridge;
 
+import java.util.List;
+
 public class Application {
 
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        OutputView outputView = new OutputView();
+        InputView inputView = new InputView();
+        BridgeMaker bridgeMaker = new BridgeMaker(new BridgeRandomNumberGenerator());
+
+        BridgeGameController bridgeGameController = new BridgeGameController(inputView, outputView, bridgeMaker);
+        bridgeGameController.play();
     }
 }
