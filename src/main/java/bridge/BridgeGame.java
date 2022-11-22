22ae3a7075e1653b444d6ae6c3749e738d30872e
Yeongseon Park <yseon406@gmail.com>
2022-11-22 15:24:43 +0900

    style: 주석 제거

--- src/main/java/bridge/BridgeGame.java
+++ src/main/java/bridge/BridgeGame.java
@@ -3,9 +3,6 @@ package bridge;
 import java.util.ArrayList;
 import java.util.List;
 
-/**
- * 다리 건너기 게임을 관리하는 클래스
- */
 public class BridgeGame {
 
     private final List<String> bridge;
@@ -24,8 +21,8 @@ public class BridgeGame {
         while (bridge.size() != player.size()) {
             String playerMove = input.readMoving();
             if (!move(playerMove)) {
-                resultToString(playerMove,"X");
-                printer.printMap(upResult,downResult);
+                resultToString(playerMove, "X");
+                printer.printMap(upResult, downResult);
                 player.clear();
                 if (retry(input.readGameCommand())) {
                     upResult = "";
@@ -33,27 +30,22 @@ public class BridgeGame {
                     retryCount++;
                     continue;
                 }
-                printer.printResult(upResult,downResult);
+                printer.printResult(upResult, downResult);
                 return false;
             }
-            resultToString(playerMove,"O");
-            printer.printMap(upResult,downResult);
+            resultToString(playerMove, "O");
+            printer.printMap(upResult, downResult);
         }
-        printer.printResult(upResult,downResult);
+        printer.printResult(upResult, downResult);
         return true;
     }
 
-    /**
-     * 사용자가 칸을 이동할 때 사용하는 메서드
-     * <p>
-     * 이동을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
-     */
     public boolean move(String playerInput) {
         player.add(playerInput);
         return bridge.get(player.size() - 1).equals(playerInput);
     }
 
-    public void resultToString(String playerInput, String result){
+    public void resultToString(String playerInput, String result) {
         if (playerInput.equals("U")) {
             upResult += result;
             downResult += " ";
@@ -64,16 +56,11 @@ public class BridgeGame {
         }
     }
 
-    /**
-     * 사용자가 게임을 다시 시도할 때 사용하는 메서드
-     * <p>
-     * 재시작을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
-     */
     public boolean retry(String userInput) {
         return userInput.equals("R");
     }
 
-    public int getRetryCount(){
+    public int getRetryCount() {
         return retryCount;
     }
 }
