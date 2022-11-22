ef676e69dd0112faac310217c2c0215996553a23
jsk5341 <102288039+jsk5341@users.noreply.github.com>
2022-11-22 14:20:28 +0900

    refactor: reclassify bridge list builder

--- src/main/java/bridge/BridgeGame.java
+++ src/main/java/bridge/BridgeGame.java
@@ -8,6 +8,10 @@ import java.util.List;
  */
 public class BridgeGame {
 
+    String correct = " O ";
+    String notSelected = "   ";
+    String wrong = " X ";
+
     /**
      * 사용자가 칸을 이동할 때 사용하는 메서드
      * <p>
@@ -33,6 +37,56 @@ public class BridgeGame {
         if (retryAnswer.equals("Q")) {
             return false;
         }
-return false;
+        return false;
     }
+
+
+    public List<String> printMapCorrect(List<String> bridgeAnswer, List<String> currentResult, int stage) {
+        if (bridgeAnswer.get(stage).equals("U")) {
+            currentResult = printMapUpsideCorrect(currentResult, stage);
+        }
+        if (bridgeAnswer.get(stage).equals("D")) {
+            currentResult = printMapDownsideCorrect(currentResult, stage);
+        }
+        return currentResult;
+    }
+
+    public List<String> printMapWrong(List<String> bridgeAnswer, List<String> currentResult, int stage) {
+        if (bridgeAnswer.get(stage).equals("U")) {
+            currentResult = printMapUpsideWrong(currentResult, stage);
+        }
+        if (bridgeAnswer.get(stage).equals("D")) {
+            currentResult = printMapDownsideWrong(currentResult, stage);
+        }
+        return currentResult;
+    }
+
+    public List<String> printMapUpsideCorrect(List<String> currentResult, int stage) {
+
+        currentResult.add(notSelected);
+        currentResult.add(stage, correct);
+
+        return currentResult;
+    }
+
+    public List<String> printMapDownsideCorrect(List<String> currentResult, int stage) {
+        currentResult.add(currentResult.size() - stage, notSelected);
+        currentResult.add(correct);
+        return currentResult;
+    }
+
+    public List<String> printMapUpsideWrong(List<String> currentResult, int stage) {
+        currentResult.add(wrong);
+        currentResult.add(stage, notSelected);
+
+        return currentResult;
+    }
+
+    public List<String> printMapDownsideWrong(List<String> currentResult, int stage) {
+        currentResult.add(currentResult.size() - stage, wrong);
+        currentResult.add(notSelected);
+        return currentResult;
+    }
+
+
 }
--- src/main/java/bridge/OutputView.java
+++ src/main/java/bridge/OutputView.java
@@ -8,19 +8,15 @@ import java.util.List;
  */
 public class OutputView {
 
-    String correct = " O ";
-    String notSelected = "   ";
-    String wrong = " X ";
+
 
     /**
      * 현재까지 이동한 다리의 상태를 정해진 형식에 맞춰 출력한다.
      * <p>
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void printMap(List<String> bridgeAnswer, List<String> currentResult, Boolean userChoice, int stage) {
-        List<String> finalResult = designBridgeMap(bridgeAnswer, currentResult, userChoice, stage);
+    public void printMap(List<String> finalResult){
         int size = finalResult.size();
-
         System.out.printf("[");
         for (int index = 0; index < size / 2; index++) {
             System.out.printf(finalResult.get(index));
@@ -41,63 +37,15 @@ public class OutputView {
         System.out.println("]");
     }
 
-
-
-    public List<String> designBridgeMap(List<String> bridgeAnswer, List<String> currentResult, Boolean userChoice, int stage) {
+    public void designBridgeMap(List<String> bridgeAnswer, List<String> currentResult, boolean userChoice, int stage) {
+        BridgeGame bridgeGame = new BridgeGame();
         if (userChoice) {
-            currentResult = printMapCorrect(bridgeAnswer, currentResult, stage);
+            currentResult = bridgeGame.printMapCorrect(bridgeAnswer, currentResult, stage);
         }
         if (!userChoice) {
-            currentResult = printMapWrong(bridgeAnswer, currentResult, stage);
+            currentResult = bridgeGame.printMapWrong(bridgeAnswer, currentResult, stage);
         }
-        return currentResult;
-    }
-
-    public List<String> printMapCorrect(List<String> bridgeAnswer, List<String> currentResult, int stage) {
-        if (bridgeAnswer.get(stage).equals("U")) {
-            currentResult = printMapUpsideCorrect(currentResult, stage);
-        }
-        if (bridgeAnswer.get(stage).equals("D")) {
-            currentResult = printMapDownsideCorrect(currentResult, stage);
-        }
-        return currentResult;
-    }
-
-    public List<String> printMapWrong(List<String> bridgeAnswer, List<String> currentResult, int stage) {
-        if (bridgeAnswer.get(stage).equals("U")) {
-            currentResult = printMapUpsideWrong(currentResult, stage);
-        }
-        if (bridgeAnswer.get(stage).equals("D")) {
-            currentResult = printMapDownsideWrong(currentResult, stage);
-        }
-        return currentResult;
-    }
-
-    public List<String> printMapUpsideCorrect(List<String> currentResult, int stage) {
-
-        currentResult.add(notSelected);
-        currentResult.add(stage, correct);
-
-        return currentResult;
-    }
-
-    public List<String> printMapDownsideCorrect(List<String> currentResult, int stage) {
-        currentResult.add(currentResult.size() - stage, notSelected);
-        currentResult.add(correct);
-        return currentResult;
-    }
-
-    public List<String> printMapUpsideWrong(List<String> currentResult, int stage) {
-        currentResult.add(wrong);
-        currentResult.add(stage, notSelected);
-
-        return currentResult;
-    }
-
-    public List<String> printMapDownsideWrong(List<String> currentResult, int stage) {
-        currentResult.add(currentResult.size() - stage, wrong);
-        currentResult.add(notSelected);
-        return currentResult;
+        printMap(currentResult);
     }
 
 
@@ -106,9 +54,8 @@ public class OutputView {
      * <p>
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void printResult(List<String> bridgeAnswer, List<String> currentResult, Boolean userChoice, int stage) {
+    public void printResult(List<String> bridgeAnswer, List<String> currentResult, boolean userChoice, int stage,int count) {
 
-        System.out.println("최종 게임 결과");
 
     }
 }
