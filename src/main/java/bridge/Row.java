52f7aeb9c7c2f94b638e5aabcef8b1e8d8ec5c29
xxeol2 <jhj01177@naver.com>
2022-11-22 15:43:02 +0900

    feat: 라운드마다 사용자의 이동 결과를 출력한다

--- src/main/java/bridge/Row.java
+++ src/main/java/bridge/Row.java
@@ -1,5 +1,8 @@
 package bridge;
 
+import bridge.exception.WrongMovingException;
+import java.util.Arrays;
+
 public enum Row {
 
     DOWN("D"), UP("U");
@@ -16,5 +19,12 @@ public enum Row {
 
     public static String getRowCodeByIndex(int index) {
         return values()[index].getCode();
+    }
+
+    public static Row of(String code) {
+        return Arrays.stream(values())
+                .filter(v -> code.equals(v.code))
+                .findFirst()
+                .orElseThrow(WrongMovingException::new);
     }
 }
--- src/main/java/bridge/view/OutputView.java
+++ src/main/java/bridge/view/OutputView.java
@@ -1,16 +1,69 @@
 package bridge.view;
 
+import static bridge.Row.DOWN;
+import static bridge.Row.UP;
+
+import bridge.Row;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 /**
  * 사용자에게 게임 진행 상황과 결과를 출력하는 역할을 한다.
  */
 public class OutputView {
+
+    public static final String BRIDGE_START = "[ ";
+    public static final String BRIDGE_END = " ]";
+    public static final String BRIDGE_DELIMITER = " | ";
+    public static final String MOVING_O = "O";
+    public static final String MOVING_X = "X";
+    public static final String MOVING_EMPTY = " ";
 
     /**
      * 현재까지 이동한 다리의 상태를 정해진 형식에 맞춰 출력한다.
      * <p>
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void printMap() {
+    public void printMap(List<String> bridge, int round, boolean result) {
+        Map<Row, List<String>> row = new HashMap<>();
+        row.put(UP, new ArrayList<>());
+        row.put(DOWN, new ArrayList<>());
+        for (int i = 0; i < round - 1; i++) {
+            cross(row, Row.of(bridge.get(i)));
+        }
+        recentCross(row, Row.of(bridge.get(round - 1)), result);
+        printRow(row.get(UP));
+        printRow(row.get(DOWN));
+    }
+
+    private void printRow(List<String> row) {
+        StringBuilder result = new StringBuilder(BRIDGE_START);
+        result.append(String.join(BRIDGE_DELIMITER, row));
+        result.append(BRIDGE_END);
+        System.out.println(result.toString());
+    }
+
+    private void cross(Map<Row, List<String>> row, Row moving) {
+        Row otherSide = getOtherSide(moving);
+        row.get(moving).add(MOVING_O);
+        row.get(otherSide).add(MOVING_EMPTY);
+    }
+
+    private void recentCross(Map<Row, List<String>> row, Row moving, boolean isCorrectMoving) {
+        Row otherSide = getOtherSide(moving);
+        if (isCorrectMoving) {
+            row.get(otherSide).add(MOVING_EMPTY);
+            row.get(moving).add(MOVING_O);
+            return;
+        }
+        row.get(otherSide).add(MOVING_X);
+        row.get(moving).add(MOVING_EMPTY);
+    }
+
+    private Row getOtherSide(Row moving) {
+        return Row.values()[(moving.ordinal() + 1) % 2];
     }
 
     /**
