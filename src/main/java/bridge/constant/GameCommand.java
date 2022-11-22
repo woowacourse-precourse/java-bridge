77f54b4b420b68d85132b8cec261cae4f5b821e8
juno-junho <ssmm0205@naver.com>
2022-11-22 23:21:40 +0900

    feat(constant): create enum GameCommand

---
+++ src/main/java/bridge/constant/GameCommand.java
@@ -0,0 +1,34 @@
+package bridge.constant;
+
+import java.util.Arrays;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+public enum GameCommand {
+    NOTHING(""),
+    RETRY("R"),
+    QUIT("Q");
+
+    private static final Map<String, GameCommand> GAME_COMMANDS = Arrays.stream(values())
+            .filter(gameCommands -> !gameCommands.is(NOTHING))
+            .collect(Collectors.toUnmodifiableMap(
+                    gameCommands -> gameCommands.command, Function.identity()));
+
+    private final String command;
+
+    GameCommand(String command) {
+        this.command = command;
+    }
+
+    public static GameCommand of(final String command) {
+        if (!GAME_COMMANDS.containsKey(command)) {
+            throw new IllegalArgumentException("R 또는 Q만 입력해 주세요. (R:재시작, Q:종료)");
+        }
+        return GAME_COMMANDS.get(command);
+    }
+
+    public boolean is(final GameCommand gameCommand) {
+        return this == gameCommand;
+    }
+}
--- src/main/java/bridge/controller/BridgeGameController.java
+++ src/main/java/bridge/controller/BridgeGameController.java
@@ -1,5 +1,11 @@
 package bridge.controller;
 
+import bridge.constant.GameCommand;
+import bridge.constant.MoveCommand;
+import bridge.constant.MoveSign;
+import bridge.domain.BridgeGame;
+import bridge.domain.BridgeGameResults;
+import bridge.domain.BridgeSizeValidator;
 import bridge.service.BridgeGameService;
 import bridge.view.InputView;
 import bridge.view.OutputView;
@@ -18,6 +24,39 @@ public class BridgeGameController {
 
     public void startBridgeGame() {
         outputView.printStartMessage();
+
+        BridgeSizeValidator bridgeSize = inputView.bridgeSize();
+        BridgeGame bridgeGame = bridgeGameService.bridgeGame(bridgeSize);
+
+        playBridgeGame(bridgeGame);
+        outputView.printResult(bridgeGame);
     }
 
+    private void playBridgeGame(final BridgeGame bridgeGame) {
+        GameCommand gameCommand = GameCommand.NOTHING;
+
+        while (bridgeGame.isNotOver(gameCommand)) {
+            String moveResult = moveResult(bridgeGame);
+
+            if (moveResult.equals(MoveSign.MOVE_FAIL.getMessage())) {
+                gameCommand = inputView.gameCommand();
+                bridgeGame.retryOrQuit(gameCommand);
+            }
+        }
+    }
+
+    private String moveResult(final BridgeGame bridgeGame) {
+        MoveCommand moveCommand = inputView.moveCommand();
+        String moveResult = bridgeGame.bridgeGameResult(moveCommand);
+
+        printCurrentPosition(bridgeGame, moveCommand, moveResult);
+        bridgeGame.move(moveResult);
+
+        return moveResult;
+    }
+
+    private void printCurrentPosition(BridgeGame bridgeGame, MoveCommand moveCommand, String moveResult) {
+        BridgeGameResults moveResults = bridgeGame.bridgeGameResults(moveCommand, moveResult);
+        outputView.printMap(moveResults);
+    }
 }
