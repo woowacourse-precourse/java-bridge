f480e9195d79ffbceb46a4b94af669b28eda550b
Hiluxy <sunset.lhj@gmail.com>
2022-11-22 20:59:05 +0900

    feat: 출력 메소드

--- src/main/java/data/OutputForm.java
+++ src/main/java/data/OutputForm.java
@@ -1,8 +1,13 @@
 package data;
 
 public enum OutputForm {
-    START_MESSAGE("다리 건너기 게임을 시작합니다.");
+    START_MESSAGE("다리 건너기 게임을 시작합니다."),
+    END_MESSAGE("게임 성공 여부: "),
+    CNT_MESSAGE("총 시도한 횟수: "),
+    FINISH_MESSAGE("최종 게임 결과");
+
     private final String message;
+
 
     OutputForm(String message) {
         this.message = message;
@@ -10,5 +15,11 @@ public enum OutputForm {
 
     public void printMessage() {
         System.out.println(message);
+    }
+    public void finishMessage(String success) {
+        System.out.println(message + success);
+    }
+    public void cntMessage(int cnt) {
+        System.out.println(message + cnt);
     }
 }
