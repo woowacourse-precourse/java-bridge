a8ed3807a70e7e55868b1676a31984ce8df1bd7b
SEONGJOON KIM <jobcho6320@naver.com>
2022-11-22 14:34:07 +0900

    refactor: 컨트롤러 게임 로직 세분화
    - run 메소드에 실행되는 로직 분리

--- src/main/java/bridge/controller/BridgeController.java
+++ src/main/java/bridge/controller/BridgeController.java
@@ -3,55 +3,69 @@ package bridge.controller;
 import bridge.BridgeRandomNumberGenerator;
 import bridge.domain.BridgeGame;
 import bridge.domain.BridgeMaker;
-import bridge.domain.BridgeResult;
 import bridge.view.InputView;
 import bridge.view.OutputView;
 
 import java.util.List;
 
 public class BridgeController {
+    private static boolean gameStatus = true;
     public void run() {
-        OutputView.printGameStartMessage();
-        int bridgeSize = InputView.inputBridgeSize();
+        int bridgeSize = InputView.readBridgeSize();
+        BridgeGame bridgeGame = getBridgeGame(bridgeSize);
+        for (int bridgeIndex = 0; bridgeIndex < bridgeSize; bridgeIndex++) {
+            String moveSide = inputMoveSide(bridgeIndex);
+            if (checkMoveSide(bridgeGame, bridgeIndex, moveSide)) continue;
+            if (gameStatus(gameStatus, bridgeGame, bridgeIndex)) break;
+            bridgeIndex = backToPreviousIndex(bridgeIndex);
+        }
+        OutputView.printBridgeResult(gameStatus, bridgeGame.getTryCount());
+    }
 
+    private static int backToPreviousIndex(int bridgeIndex) {
+        bridgeIndex--;
+        return bridgeIndex;
+    }
+
+    private BridgeGame getBridgeGame(int bridgeSize) {
+        int tryCount = 1;
+        List<String> bridgeMakeResult = generateBridge(bridgeSize);
+        return new BridgeGame(bridgeMakeResult, tryCount);
+    }
+    
+    private static boolean checkMoveSide(BridgeGame bridgeGame, int bridgeIndex, String moveSide) {
+        if (isMoveSuccessfulOrNot(bridgeGame, bridgeIndex, moveSide)) return true;
+        gameStatus = bridgeGame.retry();
+        return false;
+    }
+
+    private static String inputMoveSide(int bridgeIndex) {
+        OutputView.replaceCloseBracket(bridgeIndex);
+        return InputView.inputMoving();
+    }
+
+    private static boolean isMoveSuccessfulOrNot(BridgeGame bridgeGame, int bridgeIndex, String moveSide) {
+        if (bridgeGame.move(moveSide, bridgeIndex)) {
+            OutputView.moveSuccess(moveSide, bridgeIndex);
+            return true;
+        }
+        if (!bridgeGame.move(moveSide, bridgeIndex)) {
+            OutputView.moveFail(moveSide, bridgeIndex);
+        }
+        return false;
+    }
+
+    private static boolean gameStatus(boolean gameResult, BridgeGame bridgeGame, int bridgeIndex) {
+        if (gameResult) {
+            bridgeGame.addTryCount();
+            OutputView.goBackBeforeOneStep(bridgeIndex);
+        }
+        return !gameResult;
+    }
+
+    private List<String> generateBridge(int size) {
         BridgeRandomNumberGenerator bridgeRandomNumberGenerator = new BridgeRandomNumberGenerator();
         BridgeMaker bridgeMaker = new BridgeMaker(bridgeRandomNumberGenerator);
-        List<String> bridgeMakeResult = bridgeMaker.makeBridge(bridgeSize);
-        BridgeResult bridgeResult = new BridgeResult(true);
-        boolean gameResult = bridgeResult.getResult();
-        BridgeGame bridgeGame = new BridgeGame(bridgeMakeResult, 1);
-
-        System.out.println("다리 결과" + bridgeMakeResult);
-        for (int bridgeIndex = 0; bridgeIndex < bridgeSize; bridgeIndex++) {
-            OutputView.replaceCloseBracket(bridgeIndex);
-            String moveSide = InputView.inputMoving();
-            if (bridgeGame.move(moveSide, bridgeIndex)) {
-                OutputView.moveSuccess(bridgeGame,moveSide, bridgeIndex);
-                OutputView.printMap();
-                continue;
-            }
-            if (!bridgeGame.move(moveSide, bridgeIndex)) {
-                OutputView.moveFail(bridgeGame,moveSide, bridgeIndex);
-                OutputView.printMap();
-                gameResult = bridgeGame.retry();
-            }
-            if (gameResult) {
-                bridgeGame.retryCount();
-                OutputView.goBackBeforeOneStep(bridgeIndex);
-                bridgeIndex--;
-            }
-            if (!gameResult) {
-                break;
-            }
-        }
-        OutputView.printBridgeResult(gameResult, bridgeGame.getTryCount());
+        return bridgeMaker.makeBridge(size);
     }
-    /*
-    1. 다리 길이 입력
-    2. for
-    3. 이동할 칸 선택
-    실패시 재시도 여부
-    4. 게임 결과 출
-     */
-
 }
