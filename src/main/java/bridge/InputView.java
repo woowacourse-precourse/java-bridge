1e0a1586d385f41375fbbeb27351e70c8a0c419a
eom-tae-in <101633465+eom-tae-in@users.noreply.github.com>
2022-11-22 23:35:11 +0900

    feat: InputView 생성

--- src/main/java/bridge/InputView.java
+++ src/main/java/bridge/InputView.java
@@ -1,28 +1,52 @@
-package bridge;
+package bridge.view;
 
-/**
- * 사용자로부터 입력을 받는 역할을 한다.
- */
+import static bridge.constant.Constant.ERROR_MESSAGE;
+
+import bridge.exception.BridgeException;
+import camp.nextstep.edu.missionutils.Console;
+
 public class InputView {
-
-    /**
-     * 다리의 길이를 입력받는다.
-     */
-    public int readBridgeSize() {
-        return 0;
+    BridgeException bridgeException = new BridgeException();
+    public String readBridgeSize() {
+        try {
+            String bridgeSize = Console.readLine();
+            checkLengthInput(bridgeSize);
+            return bridgeSize;
+        } catch (IllegalArgumentException e) {
+            System.out.println(ERROR_MESSAGE + "다리 길이는 3부터 20 사이의 숫자여야 합니다.");
+            return readBridgeSize();
+        }
     }
-
-    /**
-     * 사용자가 이동할 칸을 입력받는다.
-     */
+    public void checkLengthInput(String bridgeSize) {
+        bridgeException.inputContainsCharException(bridgeSize);
+        bridgeException.checkNumberRangeException(bridgeSize);
+    }
     public String readMoving() {
-        return null;
+        try {
+            String moveInput = Console.readLine();
+            checkMoveInput(moveInput);
+            return moveInput;
+        } catch (IllegalArgumentException e) {
+            System.out.println(ERROR_MESSAGE + "위쪽으로 이동하길 원하신다면 U, 아래쪽으로 이동하길 원하신다면 D를 입력해주세요.");
+            return readMoving();
+        }
     }
-
-    /**
-     * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
-     */
+    public void checkMoveInput(String moveInput) {
+        bridgeException.checkCorrectUpOrDownException(moveInput);
+        bridgeException.checkInputSizeException(moveInput);
+    }
     public String readGameCommand() {
-        return null;
+        try {
+            String retryInput = Console.readLine();
+            checkRetryInput(retryInput);
+            return retryInput;
+        } catch (IllegalArgumentException e) {
+            System.out.println(ERROR_MESSAGE + "게임을 재시작하고 싶으시면 R, 그만하고 싶으시면 Q를 입력해주세요.");
+            return readGameCommand();
+        }
+    }
+    public void checkRetryInput(String retryInput) {
+        bridgeException.checkRestartOrDoneException(retryInput);
+        bridgeException.checkInputSizeException(retryInput);
     }
 }
