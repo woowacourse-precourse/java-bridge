b5aa280d46dc555b6852cd605307a0934f4dad40
swonny <wonny921@gmail.com>
2022-11-22 20:31:53 +0900

    test(BridgeGameTest): BridgeGame 테스트코드 추가

--- src/test/java/bridge/BridgeGameTest.java
+++ src/test/java/bridge/BridgeGameTest.java
@@ -1,73 +1,44 @@
-//package bridge;
-//
-//import controller.Controller;
-//import enumCollections.GameStatus;
-//import org.junit.jupiter.api.DisplayName;
-//import org.junit.jupiter.api.Test;
-//import view.InputView;
-//import view.OutputView;
-//
-//import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
-//
-//public class BridgeGameTest {
-//    @DisplayName("다음 칸이 이동 가능한 칸인지 확인하는 테스트")
-//    @Test
-//    void 아래칸_이동_테스트() {
-//        BridgeGame bridgeGame = new BridgeGame();
-//        assertThat(bridgeGame.tryMoveTo("D"))
-//                .isEqualTo(GameStatus.CONTINUE);
-//    }
-//
-//    @DisplayName("다음 칸이 이동 가능한 칸인지 확인하는 테스트")
-//    @Test
-//    void 위칸_이동_테스트() {
-//        BridgeGame bridgeGame = new BridgeGame();
-//        assertThat(bridgeGame.tryMoveTo("U"))
-//                .isEqualTo(GameStatus.CONTINUE);
-//    }
-//
-//    @DisplayName("다음칸 이동 불가능으로 게임 실패 케이스 테스트")
-//    @Test
-//    void 위칸_이동_실패_테스트() {
-//        BridgeGame bridgeGame = new BridgeGame();
-//        assertThat(bridgeGame.tryMoveTo("U"))
-//                .isEqualTo(GameStatus.FAILURE);
-//    }
-//
-//    @DisplayName("다음칸 이동 불가능으로 게임 실패 케이스 테스트")
-//    @Test
-//    void 아래칸_이동_실패_테스트() {
-//        BridgeGame bridgeGame = new BridgeGame();
-//        assertThat(bridgeGame.tryMoveTo("D"))
-//                .isEqualTo(GameStatus.FAILURE);
-//    }
-//
-//    @DisplayName("다리 길이가 1일 때 게임 성공 테스트")
-//    @Test
-//    void 다리_길이_1_게임_성공_테스트() {
-//        BridgeGame bridgeGame = new BridgeGame();
-//        assertThat(bridgeGame.tryMoveTo("U"))
-//                .isEqualTo(GameStatus.SUCCESS);
-//    }
-//
-//    @DisplayName("다리 길이가 1일 때 게임 성공 테스트")
-//    @Test
-//    void 다리_길이_2_게임_성공_테스트() {
-//        BridgeGame bridgeGame = new BridgeGame();
-//        bridgeGame.tryMoveTo("U");
-//        assertThat(bridgeGame.tryMoveTo("D"))
-//                .isEqualTo(GameStatus.SUCCESS);
-//    }
-//
-//    @DisplayName("삭제")
-//    @Test
-//    void 삭제() {
-//        Controller controller = new Controller(new OutputView(), new InputView());
-//        BridgeGame bridgeGame = new BridgeGame();
-//        bridgeGame.generateBridge(4);
-//        controller.play(bridgeGame, GameStatus.CONTINUE);
-//        controller.play(bridgeGame, GameStatus.CONTINUE);
-//        controller.play(bridgeGame, GameStatus.CONTINUE);
-//        controller.play(bridgeGame, GameStatus.CONTINUE);
-//    }
-//}
+package bridge;
+
+import enumCollections.AvailableInput;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+public class BridgeGameTest {
+    private final int DEFAULT_BRIDGE_SIZE = 10;
+    private final int INITIALIZED_TRIAL = 1;
+    BridgeGame bridgeGame;
+
+    @BeforeEach
+    void setUp() {
+        bridgeGame = new BridgeGame();
+        bridgeGame.generateBridge(DEFAULT_BRIDGE_SIZE);
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"U:D"}, delimiter = ':')
+    @DisplayName("사용자 이동 테스트")
+    void 사용자_이동_테스트(String UP, String DOWN) {
+        bridgeGame.move(UP);
+        bridgeGame.move(DOWN);
+        assertThat(bridgeGame.getMap())
+                .toString()
+                .matches("OX");
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {1, 10, 20})
+    @DisplayName("사용자 재시도 테스트")
+    void 사용자_재시도_테스트(int trials) {
+        for (int trial = 0; trial < trials; trial++) {
+            bridgeGame.retry(AvailableInput.RETRY_COMMAND.getUserInput());
+        }
+        assertThat(bridgeGame.getTrial())
+                .isEqualTo(Integer.toString(trials + INITIALIZED_TRIAL));
+    }
+}
