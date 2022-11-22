6e732c9da4c2a9550498ad6662d7e625090ebb36
Seung-Been Kim <mainlib990@gmail.com mainlib990s-MacBookPro.local>
2022-11-22 17:12:53 +0900

    feat(view): implement the ui that print title

---
+++ src/main/java/bridge/view/OutputView.java
@@ -0,0 +1,17 @@
+package bridge.view;
+
+public class OutputView {
+
+    private static final String MESSAGE_TITLE = "다리 건너기 게임을 시작합니다.";
+
+    public void printTitle() {
+        System.out.println(MESSAGE_TITLE);
+        System.out.println();
+    }
+
+    public void printMap() {
+    }
+
+    public void printResult() {
+    }
+}
