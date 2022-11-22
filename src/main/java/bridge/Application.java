2fd2802fa2d068397ec54f6798412f489ca0a281
이지영 <wldudwldhd@naver.com>
2022-11-22 21:56:09 +0900

    feat : application기능 구현

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -1,8 +1,20 @@
 package bridge;
 
 public class Application {
-
+    static int gameCnt = 1;
+    static BridgeNumberGenerator bridgeNumberGenerator = new BridgeRandomNumberGenerator();
+    static InputView inputView = new InputView();
+    static OutputView outputView = new OutputView();
+    static BridgeMaker bridgeMaker = new BridgeMaker(bridgeNumberGenerator);
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        BridgeGame bridgeGame = new BridgeGame(inputView, bridgeMaker, outputView);
+        while(bridgeGame.startGame()){
+            gameCnt++;
+            bridgeGame = new BridgeGame(inputView, bridgeMaker, outputView);
+        }
+        outputView.printGameCnt(gameCnt);
+
+
     }
 }
