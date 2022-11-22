262791ca6f6ac4f6ef57c9028590599457101784
JoSihun <kepler452k@naver.com>
2022-11-22 20:25:59 +0900

    refactor(BridgeGameController.java): refactoring function run() depart function run() as function run() and initGameRoutine() depart function run() as function run() and mainGameRoutine() depart function run() as function run() and finishGameRoutine()

--- src/main/java/bridge/BridgeGameController.java
+++ src/main/java/bridge/BridgeGameController.java
@@ -11,19 +11,29 @@ public class BridgeGameController {
     private int tryCount = 0;
 
     public void run() {
+        initGameRoutine();
+        do {
+            mainGameRoutine();
+        } while (!bridgeGameService.isSuccess(bridge, map) && bridgeGameService.isRetry(bridgeGameService.askCommand()));
+        finishGameRoutine();
+    }
+
+    private void initGameRoutine() {
+        System.out.println("다리 건너기 게임을 시작합니다.");
         Integer bridgeSize = bridgeGameService.askBridgeSize();
         bridge = bridgeGameService.makeBridge(bridgeSize);
+    }
 
+    private void mainGameRoutine() {
+        tryCount++;
+        map.clear();
         do {
-            tryCount++;
-            map.clear();
-            do {
-                map.add(bridgeGameService.askMoving());
-                bridgeGameService.printMapStatus(bridge, map);
-            } while (bridgeGameService.isMovable(bridge, map));
+            map.add(bridgeGameService.askMoving());
+            bridgeGameService.printMapStatus(bridge, map);
+        } while (bridgeGameService.isMovable(bridge, map));
+    }
 
-        } while (!bridgeGameService.isSuccess(bridge, map) && bridgeGameService.isRetry(bridgeGameService.askCommand()));
-
+    private void finishGameRoutine() {
         System.out.println("\n최종 게임 결과");
         bridgeGameService.printMapStatus(bridge, map);
         bridgeGameService.printResult(bridge, map, tryCount);
