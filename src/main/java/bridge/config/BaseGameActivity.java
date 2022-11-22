804e05b58a270e841dc0a1cd67b7e983b8c7a9ec
김형기 <khk211113@naver.com>
2022-11-22 10:09:42 +0900

    feat(BaseGameActivity): 게임의 라이프사이클 관리하는 부모 클래스 생성

---
+++ src/main/java/bridge/config/BaseGameActivity.java
@@ -0,0 +1,25 @@
+package bridge.config;
+
+public abstract class BaseGameActivity {
+
+    private boolean stopped = false;
+
+    public void run() {
+        onStart();
+
+        while (!stopped) {
+            onLoop();
+        }
+
+        onStop();
+    }
+
+    protected abstract void onStart();
+    protected abstract void onLoop();
+    protected abstract void onStop();
+
+    protected void stop() {
+        this.stopped = true;
+    }
+
+}
