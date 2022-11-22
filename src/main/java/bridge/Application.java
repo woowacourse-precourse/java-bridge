cf5bf385f84a0e829148e8cd435b28ea911ba472
kathyleesh <90851865+kathyleesh@users.noreply.github.com>
2022-11-22 17:37:37 +0900

    refactor(Application) : 기능 함수화

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -16,14 +16,27 @@ public class Application {
         int bridgeSize = InputView.readBridgeSize();
 
         // 랜덤으로 건널 수 있는 다리 생성
-        BridgeNumberGenerator numberGenerator = new BridgeRandomNumberGenerator();
-        BridgeMaker bridgeMaker = new BridgeMaker(numberGenerator);
-        List<String> bridgeShape = bridgeMaker.makeBridge(bridgeSize);
+        List<String> bridgeShape = getRandomBridge(bridgeSize);
         List<String> bridgeCopy = new ArrayList<>(bridgeShape);
 
         // 이동 할 다리를 선택하여 다리 건너기
         new CrossBridge(bridgeSize, bridgeShape);
 
+        // 게임 재시작 or 게임 종료 선택
+        int count = selectRetryOrEnd(bridgeSize, bridgeCopy);
+
+        // 게임 종료
+        String gameState = checkGameState(bridgeShape);
+        OutputView.printResult(gameState, count);
+    }
+
+    private static List<String> getRandomBridge(int bridgeSize){
+        BridgeNumberGenerator numberGenerator = new BridgeRandomNumberGenerator();
+        BridgeMaker bridgeMaker = new BridgeMaker(numberGenerator);
+        return bridgeMaker.makeBridge(bridgeSize);
+    }
+
+    private static int selectRetryOrEnd(int bridgeSize, List<String> bridgeCopy){
         int count = 1;
         Boolean flag = true;
         while (flag) {
@@ -35,12 +48,10 @@ public class Application {
             }
             flag = false;
         }
-
-        String gameState = checkGameState(bridgeShape);
-        OutputView.printResult(gameState, count);
+        return count;
     }
 
-    public static String checkGameState(List<String> bridgeShape){
+    private static String checkGameState(List<String> bridgeShape){
         if (bridgeShape.isEmpty()){
             return "성공";
         }
