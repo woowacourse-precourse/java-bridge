411f302f803d70ea7c8605dcacd05c48417ca42d
Basak <chgy2131@naver.com>
2022-11-22 11:35:59 +0900

    test(ApplicationTest): 메인 로직 테스트 케이스 추가
    - 실패 후 성공하는 테스트 케이스 작성
    - 잘못된 다리 길이가 주어지는 테스트 케이스 작성
    - 잘못된 이동 입력이 주어지는
    테스트 케이스 작성

--- src/test/java/bridge/ApplicationTest.java
+++ src/test/java/bridge/ApplicationTest.java
@@ -6,8 +6,13 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.util.Lists.newArrayList;
 
 import camp.nextstep.edu.missionutils.test.NsTest;
+
 import java.util.List;
+
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 class ApplicationTest extends NsTest {
 
@@ -22,15 +27,23 @@ class ApplicationTest extends NsTest {
     }
 
     @Test
-    void 기능_테스트() {
+    void 다리_생성_테스트2() {
+        BridgeNumberGenerator numberGenerator = new TestNumberGenerator(newArrayList(1, 0, 0, 1, 0, 1));
+        BridgeMaker bridgeMaker = new BridgeMaker(numberGenerator);
+        List<String> bridge = bridgeMaker.makeBridge(6);
+        assertThat(bridge).containsExactly("U", "D", "D", "U", "D", "U");
+    }
+
+    @Test
+    void 기능_테스트_한번에성공() {
         assertRandomNumberInRangeTest(() -> {
             run("3", "U", "D", "U");
             assertThat(output()).contains(
-                "최종 게임 결과",
-                "[ O |   | O ]",
-                "[   | O |   ]",
-                "게임 성공 여부: 성공",
-                "총 시도한 횟수: 1"
+                    "최종 게임 결과",
+                    "[ O |   | O ]",
+                    "[   | O |   ]",
+                    "게임 성공 여부: 성공",
+                    "총 시도한 횟수: 1"
             );
 
             int upSideIndex = output().indexOf("[ O |   | O ]");
@@ -40,9 +53,45 @@ class ApplicationTest extends NsTest {
     }
 
     @Test
-    void 예외_테스트() {
+    void 기능_테스트_실패후성공() {
+        assertRandomNumberInRangeTest(() -> {
+            run("3", "U", "U", "R", "U", "D", "U");
+            assertThat(output()).contains(
+                    "[ O | X ]",
+                    "[   |   ]",
+                    "게임 성공 여부: 실패",
+                    "총 시도한 횟수: 1",
+                    "게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)",
+                    "최종 게임 결과",
+                    "[ O |   | O ]",
+                    "[   | O |   ]",
+                    "게임 성공 여부: 성공",
+                    "총 시도한 횟수: 1"
+            );
+
+            int upSideIndex = output().indexOf("[ O |   | O ]");
+            int downSideIndex = output().indexOf("[   | O |   ]");
+            assertThat(upSideIndex).isLessThan(downSideIndex);
+        }, 1, 0, 1);
+    }
+
+    @DisplayName("예외 테스트 _ 다리길이")
+    @ValueSource(strings = {"A", "B", "C", "d", "E", "F", "Z", "u", "QQ", "RR", "0", "1", "2", "-1", "-2", "100"})
+    @ParameterizedTest
+    void 예외_테스트(String input) {
         assertSimpleTest(() -> {
-            runException("a");
+            runException(input);
+            assertThat(output()).contains(ERROR_MESSAGE);
+        });
+    }
+
+
+    @DisplayName("예외 테스트 _ 이동입력")
+    @ValueSource(strings = {"A", "B", "C", "d", "E", "F", "Z", "u", "QQ", "RR", "2", "-1", "u", "d"})
+    @ParameterizedTest
+    void 예외_테스트_이동입력(String input) {
+        assertSimpleTest(() -> {
+            runException("3", input);
             assertThat(output()).contains(ERROR_MESSAGE);
         });
     }
