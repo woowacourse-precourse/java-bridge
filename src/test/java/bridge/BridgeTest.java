78cace22ce32581acf7152d3e59d1af7bf5078e0
swonny <wonny921@gmail.com>
2022-11-22 18:01:37 +0900

    test(BridgeTest): 테스트 코드 추가

--- src/test/java/bridge/BridgeTest.java
+++ src/test/java/bridge/BridgeTest.java
@@ -1,10 +1,15 @@
 package bridge;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Stream;
@@ -13,50 +18,43 @@ import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 
 public class BridgeTest {
-    final static BridgeRandomNumberGenerator bridgeRandomNumberGenerator = new BridgeRandomNumberGenerator();
-    final static BridgeMaker bridgeMaker = new BridgeMaker(bridgeRandomNumberGenerator);
     final static List<String> defaultBridge = List.of("U", "U", "D", "D");
     Bridge bridge;
 
+    @BeforeEach
+    void setUp() {
+        bridge = new Bridge(defaultBridge);
+    }
+
     @ParameterizedTest
     @MethodSource("generateWrongBridgeValue")
-    void 다리_생성_예외_테스트(List<String> movableSides) {
+    void 다리_생성_잘못된_값_입력_예외_테스트(List<String> movableSides) {
         assertThatThrownBy(() -> new Bridge(movableSides))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
-    @Test
-    void 윗칸_이동_가능_확인_테스트() {
-        bridge = new Bridge(List.of("U"));
-        assertThat(bridge.isMovableSide("U", 0))
-                .isTrue();
+    @ParameterizedTest
+    @MethodSource("generateOutRangedBridge")
+    @DisplayName("사이즈 예외 테스트")
+    void 다리_생성_사이즈_예외_테스트(List<String> movableSides) {
+        assertThatThrownBy(() -> new Bridge(movableSides))
+                .isInstanceOf(IllegalArgumentException.class);
     }
 
-    @Test
-    void 아래칸_이동_가능_확인_테스트() {
-        bridge = new Bridge(List.of("U", "D"));
-        assertThat(bridge.isMovableSide("D", 1))
-                .isTrue();
+    @ParameterizedTest
+    @CsvSource(value = {"U:0:true", "D:2:true", "U:2:false", "U:3:false"}, delimiter = ':')
+    @DisplayName("이동 가능 여부 묻기")
+    void 이동_가능_여부_묻기_테스트(String selectedSides, String position, String isMovableSide) {
+        assertThat(bridge.isMovableSide(selectedSides, Integer.valueOf(position)))
+                .isEqualTo(Boolean.valueOf(isMovableSide));
     }
 
-    @Test
-    void 윗칸_이동_불가능_확인_테스트() {
-        bridge = new Bridge(List.of("D"));
-        assertThat(bridge.isMovableSide("U", 0))
-                .isFalse();
-    }
-
-    @Test
-    void 아래칸_이동_불가능_확인_테스트() {
-        bridge = new Bridge(List.of("U"));
-        assertThat(bridge.isMovableSide("D", 0))
-                .isFalse();
-    }
-
-    @Test
-    void 마지막_위치에_있는지_확인_테스트() {
-        bridge = new Bridge(defaultBridge);
-        assertThat(bridge.isLastPosition(defaultBridge.size() - 1))
+    @ParameterizedTest
+    @MethodSource("generateMovableBridge")
+    @DisplayName("게임에서 이겼는지 확인 (=사용자가 마지막 위치에 있는지 확인)")
+    void 마지막_위치_확인_테스트(List<String> movableSide) {
+        bridge = new Bridge(movableSide);
+        assertThat(bridge.isLastPosition(movableSide.size() - 1))
                 .isTrue();
     }
 
@@ -73,6 +71,30 @@ public class BridgeTest {
                 Arguments.of(Arrays.asList("U", "D ")),
                 Arguments.of(Arrays.asList("U", "D ", "U")),
                 Arguments.of(Arrays.asList("U.", "D ", "U"))
+        );
+    }
+
+    static Stream<Arguments> generateOutRangedBridge() {
+        return Stream.of(
+                Arguments.of(Arrays.asList("U")),
+                Arguments.of(Arrays.asList("U")),
+                Arguments.of(Arrays.asList("U", "U")),
+                Arguments.of(Arrays.asList(
+                        "U", "U", "U", "U", "U", "U", "U", "U", "U", "U", "U", "U",
+                        "U", "U", "U", "U", "U", "U", "U", "U", "U", "U", "U", "U"
+                ))
+        );
+    }
+
+    static Stream<Arguments> generateMovableBridge() {
+        return Stream.of(
+                Arguments.of(Arrays.asList("U", "U", "U")),
+                Arguments.of(Arrays.asList("U", "U", "U", "U")),
+                Arguments.of(Arrays.asList("U", "U", "U", "U", "U")),
+                Arguments.of(Arrays.asList(
+                        "U", "U", "U", "U", "U", "U", "U", "U", "U", "U",
+                        "U", "U", "U", "U", "U", "U", "U", "U", "U", "U"
+                ))
         );
     }
 }
