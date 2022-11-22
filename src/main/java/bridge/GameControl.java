81e004faf4b5e754dc62a23f60764167ff5d02b4
kyunghyun <rudgusee@gmail.com>
2022-11-22 21:59:26 +0900

    다리진행결과 출력

--- src/main/java/bridge/GameControl.java
+++ src/main/java/bridge/GameControl.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class GameControl {
+    
     InputView inputView = new InputView();
     OutputView outputView = new OutputView();
     BridgeMaker bridgeMaker = new BridgeMaker(new BridgeRandomNumberGenerator());
