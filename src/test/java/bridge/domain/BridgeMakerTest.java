fff145e2e448a2c168f815a7a07b19bab1cf7e8f
jeonghoon <shb03323@naver.com>
2022-11-22 11:27:59 +0900

    refactor: 분리했던 TestNumberGenerator를다시 합병
    TestNumberGenerator 클래스를 BridgeMakerTest 클래스의 내부 클래스와 ApplicationTest의 내부에 있도록 변경

--- src/test/java/bridge/domain/BridgeMakerTest.java
+++ src/test/java/bridge/domain/BridgeMakerTest.java
@@ -4,6 +4,8 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.util.Lists.newArrayList;
 
@@ -27,5 +29,19 @@ public class BridgeMakerTest {
     @Test
     void checkBridgeSize() {
         assertThat(bridgeMaker.makeBridge(3).size()).isEqualTo(3);
+    }
+
+    static class TestNumberGenerator implements BridgeNumberGenerator {
+
+        private final List<Integer> numbers;
+
+        TestNumberGenerator(List<Integer> numbers) {
+            this.numbers = numbers;
+        }
+
+        @Override
+        public int generate() {
+            return numbers.remove(0);
+        }
     }
 }
