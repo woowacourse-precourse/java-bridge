681c0164e8123c16558e461cc04a60c7d32de6d3
wjh2012 <wjh2012@naver.com>
2022-11-22 14:10:30 +0900

    test(OutputView): 양식에 맞게 기록 출력

---
+++ src/test/java/bridge/view/OutputViewTest.java
@@ -0,0 +1,36 @@
+package bridge.view;
+
+import bridge.DTO.MoveRecord;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.OutputStream;
+import java.io.PrintStream;
+import java.util.Arrays;
+import java.util.List;
+
+class OutputViewTest {
+
+    @DisplayName("양식에 맞게 기록을 출력한다")
+    @Test
+    void printMap() {
+        String answer = "[ O |   | X ]" + System.lineSeparator() +
+                "[   | O |   ]" + System.lineSeparator();
+        OutputStream out = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(out));
+
+        OutputView outputView = new OutputView();
+        List<MoveRecord> record =
+                Arrays.asList(
+                        MoveRecord.addRecord("U", true)
+                        ,MoveRecord.addRecord("D", true)
+                        ,MoveRecord.addRecord("U", false));
+        outputView.printMap(record);
+
+        Assertions.assertThat(out.toString()).isEqualTo(answer);
+    }
+}
