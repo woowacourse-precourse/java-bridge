6142070fbe06c0506c40bc4ed74f88b29ff7e5e4
hwiyeon0831 <aab555586@gmail.com>
2022-11-22 20:39:17 +0900

    feat(ErrorMessage) : 출력할 에러메시지에 대한 정의 객체 구현

---
+++ src/main/java/bridge/ErrorMessage.java
@@ -0,0 +1,7 @@
+package bridge;
+
+public class ErrorMessage {
+	public static String INVALID_INPUT_ERROR_MESSAGE = "잘못된 입력입니다.";
+	public static String ILLEGAL_STATE_ERROR_MESSAGE = "잘못된 상태 값이 출력됐습니다.";
+	public static String INVALID_BRIDGE_NUMBER_ERROR_MESSAGE = "다리 길이는 3부터 20 사이의 숫자여야 합니다.";
+}
