630cf7ff1739a2f24bd12bb6dee8cc89040391ae
JunHyeon <dlwnsgus07@naver.com>
2022-11-22 14:17:20 +0900

    refactor: 테스트를 명확하게 이해할 수 있도록 함수명 변경

--- src/test/java/bridge/BridgeMakerTest.java
+++ src/test/java/bridge/BridgeMakerTest.java
@@ -9,14 +9,14 @@ class BridgeMakerTest {
     BridgeMaker bridgeMaker = new BridgeMaker(bridgeNumberGenerator);
     @DisplayName("사이즈가 3미만인 크기를 입력하였을 때 예외 발생")
     @Test
-    void makeUnderSizeBridge() {
+    void 다리_생성_크기가_3보다_작을떄_예외발생() {
         Assertions.assertThatThrownBy(()->bridgeMaker.makeBridge(2))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
     @DisplayName("사이즈가 20 초과인 크기를 입력하였을 때 예외 발생")
     @Test
-    void makeOverSizeBridge(){
+    void 다리_생성_크기가_20보다_클떄_예외발생(){
         Assertions.assertThatThrownBy(()->bridgeMaker.makeBridge(21))
                 .isInstanceOf(IllegalArgumentException.class);
     }
