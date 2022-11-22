8d4ca110ff4e29e507560156f2e9dcf4f7396a08
DESKTOP-AUSH0GT\Power <sgh0718@naver.com>
2022-11-22 18:18:19 +0900

    feat : 출력 기능 클래스의 현재까지 이동한 다리 정보 출력 기능
    다리 진행 결과 출력 기능 클래스를 두 객체로 나누어 진행중인 다리를 출력하는 메서드

--- src/main/java/bridge/OutputView.java
+++ src/main/java/bridge/OutputView.java
@@ -13,7 +13,10 @@ public class OutputView {
     String attemptNumber = "총 시도한 횟수: ";
     String success = "성공";
     String fail = "실패";
-    
+
+    BridgeViewMaker upBridge = new BridgeViewMaker("U");
+    BridgeViewMaker downBridge = new BridgeViewMaker("D");
+
     public void ShowMessage(String message) {
         System.out.println(message);
     }
@@ -23,7 +26,10 @@ public class OutputView {
      * <p>
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void printMap() {
+    public void printMap(List<String> answer, String userUpDown, int blankNumber) {
+        upBridge.resultBridge(answer, userUpDown, blankNumber);
+        downBridge.resultBridge(answer, userUpDown, blankNumber);
+
     }
 
 
