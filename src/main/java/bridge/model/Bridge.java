ae5c32835f796739395c7b6654f2dcfb01558231
sooyoungh <sooyoung.h8@gmail.com>
2022-11-22 18:12:49 +0900

    ♻ refactor(Bridge): 멤버필드 currentRoundNumber 삭제
    해당 클래스에 불필요한 멤버필드 삭제

--- src/main/java/bridge/model/Bridge.java
+++ src/main/java/bridge/model/Bridge.java
@@ -6,7 +6,6 @@ import java.util.List;
 public class Bridge {
 
     private List<String> bridge;
-    private int currentRoundNumber = 0;
 
     public Bridge (List<String> bridge) {
         this.bridge = bridge;
@@ -19,17 +18,5 @@ public class Bridge {
     public Boolean isRoundLeft(int currentRoundNumber) {
         int finalRound = bridge.size();
         return currentRoundNumber < finalRound;
-    }
-
-    public void addCurrentRoundNumber() {
-        currentRoundNumber +=1 ;
-    }
-
-    public void resetCurrentRoundNumber() {
-        currentRoundNumber = 0;
-    }
-
-    public int getCurrentRoundNumber() {
-        return currentRoundNumber;
     }
 }
