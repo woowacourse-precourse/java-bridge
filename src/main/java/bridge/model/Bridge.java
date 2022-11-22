04b53efd45ce3c87cd0f73cef218d1f5482ff50f
chj <bluedog129@gmail.com>
2022-11-22 18:33:13 +0900

    feat(Player, Bridge) : 플레이어가 다리를 이동하는 기능 구현

---
+++ src/main/java/bridge/model/Bridge.java
@@ -0,0 +1,11 @@
+package bridge.model;
+
+import java.util.List;
+
+public class Bridge {
+    private final List<String> answers;
+
+    public Bridge(final List<String> answers) {
+        this.answers = answers;
+    }
+}
