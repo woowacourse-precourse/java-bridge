f9860e3c901abe721268a9558731ab2faa985168
nylonmusk <106235834+nylonmusk@users.noreply.github.com>
2022-11-22 10:31:55 +0900

    test(BridgeSizeTest): 불필요한 코드 제거

--- src/test/java/bridge/BridgeSizeTest.java
+++ src/test/java/bridge/BridgeSizeTest.java
@@ -166,18 +166,4 @@ public class BridgeSizeTest extends NsTest {
     protected void runMain() {
         Application.main(new String[]{});
     }
-
-    static class TestNumberGenerator implements BridgeNumberGenerator {
-
-        private final List<Integer> numbers;
-
-        TestNumberGenerator(List<Integer> numbers) {
-            this.numbers = numbers;
-        }
-
-        @Override
-        public int generate() {
-            return numbers.remove(0);
-        }
-    }
 }
