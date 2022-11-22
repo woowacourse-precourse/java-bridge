9749efdb9253c79f02228f29a17bdbd0f7dfa4c3
sonsoowon <yuwoo1018@gmail.com>
2022-11-22 15:34:31 +0900

    test(TotalResultTest): 게임 성공 여부, 총 시도 횟수 반환 테스트 추가

--- src/test/java/bridge/model/TotalResultTest.java
+++ src/test/java/bridge/model/TotalResultTest.java
@@ -8,15 +8,63 @@ import java.util.List;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class TotalResultTest {
-    private static final PlayerMap playerMap = new PlayerMap(
-            List.of("O", " "),
-            List.of(" ", "X")
-    );
-    private static final TotalResult result = new TotalResult(playerMap, 1);
 
-    @DisplayName("다리 건너기를 실패하면 false를 반환한다")
+    private static final BridgeGame bridgeGame = new BridgeGame(List.of("U", "D", "D"), 1);
+    private static final PlayerMap playerMap1 = new PlayerMap(
+            List.of("O", " ", "X"),
+            List.of(" ", "O", " ")
+    );
+
+    private static final PlayerMap playerMap2 = new PlayerMap(
+            List.of("O", " "),
+            List.of(" ", "O")
+    );
+
+    private static final PlayerMap playerMap3 = new PlayerMap(
+            List.of("O", " ", " "),
+            List.of(" ", "O", "O")
+    );
+
+    @DisplayName("이동한 칸을 건널 수 없다면 true를 반환한다")
     @Test
-    void loseTest() {
-        assertThat(result.win()).isFalse();
+    void loseGameTest() {
+        TotalResult result = new TotalResult(bridgeGame, playerMap1);
+        assertThat(result.loseGame()).isTrue();
+    }
+
+    @DisplayName("이동한 칸을 건널 수 있다면 false를 반환한다")
+    @Test
+    void notLoseGameTest() {
+        TotalResult result = new TotalResult(bridgeGame, playerMap2);
+        assertThat(result.loseGame()).isFalse();
+    }
+
+
+    @DisplayName("다리를 끝까지 건너면 true를 반환한다")
+    @Test
+    void winGameTest() {
+        TotalResult result = new TotalResult(bridgeGame, playerMap3);
+        assertThat(result.winGame()).isTrue();
+    }
+
+    @DisplayName("다리 끝까지 이동했지만 건널 수 없다면 false를 반환한다")
+    @Test
+    void notWinGameTest1() {
+        TotalResult result = new TotalResult(bridgeGame, playerMap1);
+        assertThat(result.winGame()).isFalse();
+    }
+
+    @DisplayName("이동한 칸을 건널 수 있지만 다리 끝까지 이동하지 않았다면 false를 반환한다")
+    @Test
+    void notWinGameTest2(){
+        TotalResult result = new TotalResult(bridgeGame, playerMap2);
+        assertThat(result.winGame()).isFalse();
+    }
+
+    @DisplayName("총 시도 횟수를 반환한다")
+    @Test
+    void getTryCntTest() {
+        TotalResult result = new TotalResult(bridgeGame, playerMap1);
+        assertThat(result.getTryCnt()).isEqualTo(1);
     }
 }
