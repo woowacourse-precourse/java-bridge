44d32abd1cc138c9b40cec795d3203066156a916
endlessmomo <soohyuk96@naver.com>
2022-11-22 13:15:05 +0900

    fix(Text) : test 코드와 일치시키기 위해 상수값 변경

--- src/main/java/bridge/model/constnce/Text.java
+++ src/main/java/bridge/model/constnce/Text.java
@@ -6,8 +6,8 @@ public class Text {
     public static final String MOVE_TO_WHERE = "\n이동할 칸을 선택해주세요. (위: U, 아래: D)";
     public static final String RETRY_MESSAGE = "게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)";
     public static final String END_MESSAGE = "\n최종 게임 결과";
-    public static final String RESULT_MESSAGE = "\n게임 성공 여부 : ";
-    public static final String TOTAL_ATTEMPT_MESSAGE = "총 시도한 횟수 : " ;
+    public static final String RESULT_MESSAGE = "\n게임 성공 여부: ";
+    public static final String TOTAL_ATTEMPT_MESSAGE = "총 시도한 횟수: " ;
     public static final String SUCCESS = "성공";
     public static final String FAIL = "실패";
 }
