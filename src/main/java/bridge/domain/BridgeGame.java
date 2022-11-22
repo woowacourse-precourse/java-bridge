5891103a75b0418929832486cd8b9a149c7c4dfb
kki4504 <rhkddlf9876@gmail.com>
2022-11-22 17:07:33 +0900

    feat(moveWay) : 옳은 길인지 틀린 길인지 확인 기능

--- src/main/java/bridge/domain/BridgeGame.java
+++ src/main/java/bridge/domain/BridgeGame.java
@@ -1,7 +1,14 @@
 package bridge.domain;
 
+import bridge.BridgeMaker;
+import bridge.BridgeNumberGenerator;
+import bridge.BridgeRandomNumberGenerator;
 import bridge.view.InputView;
+import bridge.view.OutputView;
 import org.assertj.core.api.ThrowableAssert;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static bridge.messages.ExceptionMessage.ERROR_INVALID_GAME_COMMAND;
 import static bridge.messages.ExceptionMessage.ERROR_INVALID_MOVE_COMMAND;
@@ -22,21 +29,27 @@ public class BridgeGame {
     private final static String MOVE_DOWN_COMMAND = "D";
     private final static String RETRY_GAME_COMMAND = "R";
     private final static String END_GAME_COMMAND = "Q";
+    private final static String RIGHT_WAY = " O ";
+    private final static String WRONG_WAY = " X ";
+    private final static String BLACK = "   ";
+    private List<String> upBridge = new ArrayList<>();
+    private List<String> downBridge = new ArrayList<>();
+    private boolean gameResult = true;
 
     /**
      * 사용자가 칸을 이동할 때 사용하는 메서드
      * <p>
      * 이동을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public String move(String moveCommand) {
-        return moveValidate(moveCommand);
-    }
-
-    private String moveValidate(String moveCommand) {
-        if (moveCommand.equals(MOVE_UP_COMMAND) || moveCommand.equals(MOVE_DOWN_COMMAND)) {
-            return moveCommand;
+    public boolean move(String moveCommand, String bridgeValue) {
+        boolean correctAnswer;
+        if (moveValidate(moveCommand).equals(bridgeValue)) {
+            moveRightWay(moveCommand);
+            return correctAnswer = true;
         }
-        throw new IllegalArgumentException(ERROR_INVALID_MOVE_COMMAND.getMessage());
+        moveWrongWay(moveCommand);
+        gameResult = false;
+        return correctAnswer = false;
     }
 
     /**
@@ -44,8 +57,42 @@ public class BridgeGame {
      * <p>
      * 재시작을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public String retry(String gameCommand) {
-        return retryValidate(gameCommand);
+    public boolean retry(String gameCommand) {
+        if (retryValidate(gameCommand).equals(RETRY_GAME_COMMAND)) {
+            return true;
+        }
+        return false;
+    }
+
+    public List<List<String>> getBridges() {
+        return List.of(upBridge, downBridge);
+    }
+
+    private void moveRightWay(String moveCommand) {
+        if (moveCommand.equals(MOVE_UP_COMMAND)) {
+            upBridge.add(RIGHT_WAY);
+            downBridge.add(BLACK);
+            return;
+        }
+        downBridge.add(RIGHT_WAY);
+        upBridge.add(BLACK);
+    }
+
+    private void moveWrongWay(String moveCommand) {
+        if (moveCommand.equals(MOVE_UP_COMMAND)) {
+            upBridge.add(WRONG_WAY);
+            downBridge.add(BLACK);
+            return;
+        }
+        downBridge.add(WRONG_WAY);
+        upBridge.add(BLACK);
+    }
+
+    private String moveValidate(String moveCommand) {
+        if (moveCommand.equals(MOVE_UP_COMMAND) || moveCommand.equals(MOVE_DOWN_COMMAND)) {
+            return moveCommand;
+        }
+        throw new IllegalArgumentException(ERROR_INVALID_MOVE_COMMAND.getMessage());
     }
 
     private String retryValidate(String gameCommand) {
