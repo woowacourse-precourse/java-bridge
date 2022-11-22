93e74e11f817bc486df46e44e0f94568990b2abd
HyowonSin <tlsgydnjs@naver.com>
2022-11-22 20:21:17 +0900

    feat($InputView): add input method (bridge size, moving, game command)
    add method in $InputView:
    - readBridgeSize()
    - readMoving()
    - readGameCommand()

--- src/main/java/bridge/InputView.java
+++ src/main/java/bridge/InputView.java
@@ -9,20 +9,41 @@ public class InputView {
      * 다리의 길이를 입력받는다.
      */
     public int readBridgeSize() {
-        return 0;
+        System.out.println("다리의 길이를 입력해주세요.");
+        int bridgeSize = Integer.parseInt(camp.nextstep.edu.missionutils.Console.readLine());
+
+        if (!(bridgeSize >= 3 && bridgeSize <= 20)) {
+            throw new IllegalArgumentException("[ERROR] 다리 길이는 3부터 20 사이의 숫자여야 합니다.");
+        }
+
+        return bridgeSize;
     }
 
     /**
      * 사용자가 이동할 칸을 입력받는다.
      */
     public String readMoving() {
-        return null;
+        System.out.println("이동할 칸을 선택해주세요. (위: U, 아래: D)");
+        String moving = camp.nextstep.edu.missionutils.Console.readLine();
+
+        if (!moving.equals("U") && !moving.equals("D")) {
+            throw new IllegalArgumentException("[ERROR] 이동할 칸은 U 또는 D여야 합니다.");
+        }
+
+        return moving;
     }
 
     /**
      * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
      */
     public String readGameCommand() {
-        return null;
+        System.out.println("게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)");
+        String gameCommand = camp.nextstep.edu.missionutils.Console.readLine();
+
+        if (!gameCommand.equals("R") && !gameCommand.equals("Q")) {
+            throw new IllegalArgumentException("[ERROR] 게임을 다시 시도할지 여부는 R 또는 Q여야 합니다.");
+        }
+
+        return gameCommand;
     }
 }
