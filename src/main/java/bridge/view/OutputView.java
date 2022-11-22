61b638f33af824ea5894c61e083260f7c338d348
himjjong <jjijji5728@tukorea.ac.kr>
2022-11-22 15:04:36 +0900

    refactor : 다리 건너기 무한 루프문으로 while에서 do-while 문으로 수정

--- src/main/java/bridge/view/OutputView.java
+++ src/main/java/bridge/view/OutputView.java
@@ -20,10 +20,9 @@ public class OutputView {
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
     public void printMap(List<String> bridgeData) {
-        while(true){
+        do {
             stepBridge(bridgeData);
-            if(!exitLoop()) {break;}
-        }
+        } while (exitLoop());
     }
 
     private boolean exitLoop() {
