d981307a5802996e0eaa84d0cd3af971d2207a84
YongNyeo <won05121@naver.com>
2022-11-22 13:00:49 +0900

    feat: 게임 시작 전 다리 세팅 기능 추가

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -1,11 +1,24 @@
 package bridge;
 
+import java.util.List;
+
 public class Application {
 
     public static void main(String[] args) {
         // TODO: 프로그램 구현
         InputView SizeInputView = new InputView();
         int size = SizeInputView.readBridgeSize();
-
+        ManageGameSettings(size);
     }
+
+    public static void ManageGameSettings(int size) {
+        OutputView outputView = new OutputView();
+        BridgeRandomNumberGenerator bridgeRandomNumberGenerator = new BridgeRandomNumberGenerator();
+        BridgeMaker bridgeMaker = new BridgeMaker(bridgeRandomNumberGenerator);
+        List<String> Bridge = bridgeMaker.makeBridge(size);
+        int tryNum = ManageGame(Bridge,outputView);
+        outputView.printResult(tryNum, output);
+    }
+
 }
+
