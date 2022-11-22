67c6585e6b9c0f93ac9c8dfce0a0eb81f3589991
mng990 <ryoo0504@pusan.ac.kr>
2022-11-22 18:39:02 +0900

    feat(main/java/bridge/BridgeGame.java): isClear() 추가
    클리어 여부를 리턴

--- src/main/java/bridge/BridgeGame.java
+++ src/main/java/bridge/BridgeGame.java
@@ -3,41 +3,30 @@ package bridge;
 import java.util.ArrayList;
 import java.util.List;
 
-/**
- * 다리 건너기 게임을 관리하는 클래스
- */
+
 public class BridgeGame {
 
-    /**
-     * 사용자가 칸을 이동할 때 사용하는 메서드
-     * <p>
-     * 이동을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
-     */
 
     private final List<Glass> BRIDGE;
     private static Integer step;
-    private static Integer play;
+    private static Integer playTime;
 
 
     public List<Glass> history() {
         return BRIDGE.subList(0, step);
     }
 
-    public Integer play() { return play;    }
-    public Integer step() { return step;    }
+    public Integer playTime() { return playTime;    }
 
-    /**
-     * 사용자가 게임을 다시 시도할 때 사용하는 메서드
-     * <p>
-     * 재시작을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
-     */
-
+    public boolean isClear(){
+        return step == BRIDGE.size();
+    }
     public State move(Glass user) {
         return State.stateOf(user == BRIDGE.get(step++));
     }
 
     public void retry() {
-        play++;
+        playTime++;
         step = 0;
     }
 
@@ -52,7 +41,7 @@ public class BridgeGame {
 
     BridgeGame(List<String> bridge){
         step = 0;
-        play = 1;
+        playTime = 1;
         BRIDGE = buildBridge(bridge);
     }
 }
