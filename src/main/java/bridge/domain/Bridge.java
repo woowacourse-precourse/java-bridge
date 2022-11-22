7c6d540332a19813a04e6c45faa650c5b0a52676
임원정 <oddish0513@gmail.com>
2022-11-22 15:55:07 +0900

    feat(bridge): 예외처리 추가

--- src/main/java/bridge/domain/Bridge.java
+++ src/main/java/bridge/domain/Bridge.java
@@ -2,12 +2,20 @@ package bridge.domain;
 
 import java.util.List;
 
+import static bridge.Constants.DOWN;
+import static bridge.Constants.UP;
+
 public class Bridge {
 	private final List<String> bridge;
 
 	public Bridge(List<String> bridge) {
-		// Todo 예외처리
+		validate(bridge);
 		this.bridge = bridge;
+	}
+
+	private void validate(List<String> bridge) {
+		if (!bridge.contains(UP) && !bridge.contains(DOWN))
+			throw new IllegalArgumentException();
 	}
 
 	String getValueOfIndex(int index){
