62a3c6db32b49ecef6dbffe4949b1f6d6b586e6e
K-jun98 <alap_u@naver.com>
2022-11-22 13:56:46 +0900

    refactor - OutputMessage 출력 메시지 분류

--- src/main/java/bridge/view/OutputView.java
+++ src/main/java/bridge/view/OutputView.java
@@ -1,5 +1,7 @@
 package bridge.view;
 
+import static bridge.constant.OutputMessage.*;
+
 import bridge.domain.BridgeGame;
 import bridge.domain.BridgeToken;
 import bridge.domain.Direction;
@@ -11,7 +13,6 @@ import java.util.stream.Collectors;
  */
 public class OutputView {
 
-    private static final String BRIDGE_GAME_START_MESSAGE = "다리 건너기 게임을 시작합니다.\n";
     private static final String ERROR = "[ERROR]";
     private static final String MOVABLE_SIGN = "O";
     private static final String IMMOVABLE_SIGN = "X";
@@ -43,12 +44,12 @@ public class OutputView {
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
     public void printResult(BridgeGame bridgeGame) {
-        System.out.println("최종 게임 결과");
+        System.out.println(GAME_RESULT_MESSAGE);
         printMap(bridgeGame.getBridgeTokens());
 
         System.out.println(printSuccessStatus(bridgeGame.isEnd()));
 
-        System.out.println("총 시도한 횟수: " + bridgeGame.getRetryCount());
+        System.out.println(TRY_COUNT_MESSAGE + bridgeGame.getRetryCount());
     }
 
     public static void printExceptionMessage(String message) {
@@ -61,9 +62,9 @@ public class OutputView {
 
     private String printSuccessStatus(boolean status) {
         if (status) {
-            return "게임 성공 여부: 성공";
+            return GAME_SUCCESS_MESSAGE;
         }
-        return "게임 성공 여부: 실패";
+        return GAME_FAIL_MESSAGE;
     }
 
     private String toSign(Direction direction, BridgeToken bridgeToken) {
