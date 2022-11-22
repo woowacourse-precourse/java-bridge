4821a0da77eda49183a07ba286d851fd607d0bf9
vcho1958 <gusckd57@gmail.com>
2022-11-22 16:58:34 +0900

    chore: 필요 인스턴스들을 초기화한다.

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -1,8 +1,16 @@
 package bridge;
 
 public class Application {
+    private InputView input;
+    private OutputView output;
+    Application(){
+        this.input = new InputView();
+        this.output = new OutputView();
+    }
 
     public static void main(String[] args) {
+        BridgeGame game = new BridgeGame();
+        BridgeMaker maker = new BridgeMaker(new BridgeRandomNumberGenerator());
         // TODO: 프로그램 구현
     }
 }
