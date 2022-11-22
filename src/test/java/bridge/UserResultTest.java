c6eaae71099b34b12251196fb24573130818b004
dlrghks2090 <dlrghks2090@gmail.com>
2022-11-22 19:44:33 +0900

    test: 사용자가 서택한 칸들을 모아놓는 기능에 대한 테스트 케이스 추가

--- src/test/java/bridge/UserResultTest.java
+++ src/test/java/bridge/UserResultTest.java
@@ -3,10 +3,32 @@ package bridge;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
 
 class UserResultTest {
     @Test
-    void name() {
+    void createUpperResultByUserResult() {
+        UserResult userResult = new UserResult();
+        String expect = "[ O |   | O |   ]";
 
+        userResult.plusResult("U", true);
+        userResult.plusResult("D", true);
+        userResult.plusResult("U", true);
+        userResult.plusResult("D", false);
+
+        assertThat(userResult.getFinalUpperResult()).isEqualTo(expect);
+    }
+
+    @Test
+    void createLowerResultByUserResult() {
+        UserResult userResult = new UserResult();
+        String expect = "[   | O |   | X ]";
+
+        userResult.plusResult("U", true);
+        userResult.plusResult("D", true);
+        userResult.plusResult("U", true);
+        userResult.plusResult("D", false);
+
+        assertThat(userResult.getFinalLowerResult()).isEqualTo(expect);
     }
 }
