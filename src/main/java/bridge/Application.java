509896e67d73a94a0cded57079359c8f2391df94
Bue-von-hon <dkssudvn2@gmail.com>
2022-11-22 10:58:28 +0900

    refactor : 안에서 생성하게 수정

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -6,10 +6,7 @@ import bridge.service.BridgeGameService;
 public class Application {
 
     public static void main(String[] args) {
-        BridgeGameService service = new BridgeGameService();
-        InputView inputView = new InputView();
-        OutputView outputView = new OutputView();
-        Controller controller = new Controller(service, inputView, outputView);
+        Controller controller = new Controller(new BridgeGameService(), new InputView(), new OutputView());
         try {
             controller.start();
             controller.move();
