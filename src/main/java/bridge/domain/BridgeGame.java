900f8b2820d7eaf2736defbeb63434ad34aa08f5
wnsgml7267 <dlwns7267@naver.com>
2022-11-22 22:23:53 +0900

    feat(view/BridgeGame): 사용자 칸 이동 기능 구현

--- src/main/java/bridge/domain/BridgeGame.java
+++ src/main/java/bridge/domain/BridgeGame.java
@@ -1,15 +1,30 @@
 package bridge.domain;
 
+import static bridge.util.Constant.*;
+import java.util.List;
+
 /**
  * 다리 건너기 게임을 관리하는 클래스
  */
 public class BridgeGame {
+
+    private final Round round;
+    private final GameAttempt gameAttempt;
+    private final BridgeState bridgeState;
+
+    public BridgeGame(Round round, GameAttempt gameAttempt, BridgeState bridgeState){
+        this.round = round;
+        this.gameAttempt = gameAttempt;
+        this.bridgeState = bridgeState;
+    }
+
     /**
      * 사용자가 칸을 이동할 때 사용하는 메서드
      * <p>
      * 이동을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void move() {
+    public void move(String userInput, List<String> bridge) {
+        bridgeState.convertResult(userInput, bridge);
     }
 
     /**
