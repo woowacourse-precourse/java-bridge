7b5180a9fae46d216bb3455dd418ae9451a326c1
taejeong <sontaewjd@nate.com>
2022-11-22 15:23:45 +0900

    feat: 에러메시지 상수 문자열에 추가

--- src/main/java/bridge/ConstString.java
+++ src/main/java/bridge/ConstString.java
@@ -10,4 +10,9 @@ public class ConstString {
     public static final String TRY_COUNT = "총 시도한 횟수: ";
     public static final String UP = "U";
     public static final String DOWN = "D";
+
+
+    public static final String INPUT_SIZE_ERROR = "[ERROR] 다리 길이는 3부터 20 사이의 숫자여야 합니다.";
+    public static final String INPUT_MOVE_ERROR = "[ERROR] U 또는 D를 입력해야 됩니다.";
+    public static final String INPUT_RETRY_OR_QUIT_ERROR = "[ERROR] R 또는 Q를 입력해야 됩니다.";
 }
