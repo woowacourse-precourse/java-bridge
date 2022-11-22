1639165211bc5f1e7db0093b000d0978a2a0cb2c
ONE <sodlapdlf13@gmail.com>
2022-11-22 21:12:50 +0900

    test(bridge): Bridge class에 대한 테스트 코드 수정

--- src/test/java/bridge/domain/BridgeTest.java
+++ src/test/java/bridge/domain/BridgeTest.java
@@ -1,25 +1,41 @@
-//package bridge.domain;
-//
-//import org.junit.jupiter.api.DisplayName;
-//import org.junit.jupiter.api.Test;
-//
-//import static org.assertj.core.api.Assertions.assertThatThrownBy;
-//
-//class BridgeTest {
-//
-//    @DisplayName("다리의 길이가 20보다 크면 예외가 발생한다.")
-//    @Test
-//    void createBridgeByOverRange() {
-//        assertThatThrownBy(() -> Bridge.of(21))
-//                .isInstanceOf(IllegalArgumentException.class);
-//    }
-//
-//    @DisplayName("다리의 길이가 3보다 작으면 예외가 발생한다.")
-//    @Test
-//    void createBridgeByLowerRange() {
-//        assertThatThrownBy(() -> Bridge.of(2))
-//                .isInstanceOf(IllegalArgumentException.class);
-//    }
-//
-//
-//}
+package bridge.domain;
+
+import bridge.view.InputView;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+class BridgeTest {
+
+    private static InputStream generateUserInput(String input) {
+        return new ByteArrayInputStream(input.getBytes());
+    }
+
+    @DisplayName("다리의 길이가 20보다 크면 예외가 발생한다.")
+    @Test
+    void createBridgeByOverRange() {
+        InputStream in = generateUserInput("21");
+        System.setIn(in);
+
+        assertThatThrownBy(InputView::readBridgeSize)
+                .hasMessageContaining("[ERROR] 다리 길이는 3부터 20 사이의 숫자여야 합니다.")
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("다리의 길이가 3보다 작으면 예외가 발생한다.")
+    @Test
+    void createBridgeByLowerRange() {
+        InputStream in = generateUserInput("1");
+        System.setIn(in);
+
+        assertThatThrownBy(InputView::readBridgeSize)
+                .hasMessageContaining("[ERROR] 다리 길이는 3부터 20 사이의 숫자여야 합니다.")
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+
+}
