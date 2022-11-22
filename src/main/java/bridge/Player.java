1366de7f7cfdbdd6dc5bb5db55bc441722ab0e09
jihun-24k <gi020477@gmail.com>
2022-11-22 13:01:44 +0900

    Feat(Player): 플레이어 클래스 생성자 추가

--- src/main/java/bridge/Player.java
+++ src/main/java/bridge/Player.java
@@ -3,4 +3,9 @@ package bridge;
 public class Player {
     private int gameCount;
     private boolean GameClear;
+
+    public Player() {
+        this.gameCount = 0;
+        this.GameClear = false;
+    }
 }
