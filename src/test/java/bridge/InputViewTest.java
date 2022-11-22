bc724e06818212782e72f0344da4b28c3014d111
JunTaeINC <juntea3523@naver.com>
2022-11-22 22:03:29 +0900

    test : (InputView) getBridgeLength 테스트 기능 구현

---
+++ src/test/java/bridge/InputViewTest.java
@@ -0,0 +1,46 @@
+package bridge;
+
+import org.junit.jupiter.api.*;
+
+import java.io.*;
+
+import static bridge.enums.ErrorMessage.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+class InputViewTest {
+
+    private InputView inputView;
+
+    @BeforeEach
+    public void setUp() {
+        inputView = new InputView();
+    }
+
+    @DisplayName("다리의 길이 입력시 범위(3~20)를 벗어나거나 정수가 아닐시 예외가 발생한다")
+    @Nested
+    class 다리_길이_예외 {
+        @Test
+        void case_문자_입력() {
+            String input = "18k";
+            OutputStream out = new ByteArrayOutputStream();
+            System.setOut(new PrintStream(out));
+            InputStream in = new ByteArrayInputStream(input.getBytes());
+            System.setIn(in);
+            IllegalArgumentException illegalArgumentException = assertThrows(IllegalArgumentException.class, () ->
+                    inputView.getBridgeLength());
+            assertEquals(ER_BRIDGE_LENGTH_INPUT.getMessage(), illegalArgumentException.getMessage());
+        }
+
+        @Test
+        void case_범위_밖_입력() {
+            String input = "21";
+            OutputStream out = new ByteArrayOutputStream();
+            System.setOut(new PrintStream(out));
+            InputStream in = new ByteArrayInputStream(input.getBytes());
+            System.setIn(in);
+            IllegalArgumentException illegalArgumentException = assertThrows(IllegalArgumentException.class, () ->
+                    inputView.getBridgeLength());
+            assertEquals(ER_BRIDGE_LENGTH_INPUT.getMessage(), illegalArgumentException.getMessage());
+        }
+    }
+}
