fcc99f3a9991df043ac0779c6c1297ae6e65a1a1
dclxxi <dclxxi@kakao.com>
2022-11-22 18:56:43 +0900

    test: PositionType의 getOtherType 테스트 추가

--- src/test/java/model/bridge/PositionTypeTest.java
+++ src/test/java/model/bridge/PositionTypeTest.java
@@ -1,9 +1,12 @@
 package model.bridge;
 
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
+
+import java.util.Arrays;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -27,5 +30,19 @@ class PositionTypeTest {
                 PositionType.getStringByNumber(number)
         ).isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("1 또는 0이 아닙니다.");
+    }
+
+    @DisplayName("U 전달 시 D 반환")
+    @Test
+    void getDType() {
+        PositionType otherType = PositionType.getOtherType(PositionType.U);
+        assertEquals(otherType, PositionType.D);
+    }
+
+    @DisplayName("D 전달 시 U 반환")
+    @Test
+    void getUType() {
+        PositionType otherType = PositionType.getOtherType(PositionType.D);
+        assertEquals(otherType, PositionType.U);
     }
 }
