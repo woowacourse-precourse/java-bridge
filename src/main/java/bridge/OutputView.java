8220517edaedc21116fa5777cfe63d0c99485ecd
Mr.Kim <86274253+Joajy@users.noreply.github.com>
2022-11-22 10:45:19 +0900

    feat: add method into OutputView
    make printMap method and add some change of printResult

--- src/main/java/bridge/OutputView.java
+++ src/main/java/bridge/OutputView.java
@@ -1,5 +1,7 @@
 package bridge;
 
+import java.util.*;
+
 /**
  * 사용자에게 게임 진행 상황과 결과를 출력하는 역할을 한다.
  */
@@ -11,8 +13,13 @@ public class OutputView {
      * <p>
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void printMap(Integer length) {
-        
+    public void printMap(List<List<String>> bridge) {
+        for(List<String> row:bridge){
+            System.out.println(constant.Front + 
+                               String.join(constant.Delimiter, row) +
+                               constant.Back);
+        }
+        System.out.println();
     }
 
     /**
@@ -20,10 +27,10 @@ public class OutputView {
      * <p>
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void printResult(String result, Integer length, Integer count) {
+    public void printResult(BridgeGame bridgeGame) {
         System.out.println(constant.Result_of_game);
-        //printmap을 사용하여 작성(parameter length)
-        System.out.println(constant.Result_of_try + result);
-        System.out.println(constant.Total_Try + count);
+        printMap(bridgeGame.createMap());
+        System.out.println(constant.Total_Try + bridgeGame.getCount());
+        System.out.println(constant.Result_of_try + bridgeGame.getResult());
     }
 }
