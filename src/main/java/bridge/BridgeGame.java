43e85bf6e69eda53b5ffa21e4e9a6ac048f50dab
YOON <seoung59@gmail.com>
2022-11-22 23:36:49 +0900

    Feat : 경로출력

--- src/main/java/bridge/BridgeGame.java
+++ src/main/java/bridge/BridgeGame.java
@@ -36,8 +36,8 @@ public class BridgeGame {
 
     private int toMovingNumber(String moving) {
         if (moving.equals("D"))
-            return 0;
-        return 1;
+            return 1;
+        return 0;
     }
 
     /**
@@ -46,5 +46,9 @@ public class BridgeGame {
      * 재시작을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
     public void retry() {
+    }
+
+    public List<List<String>> getBridgePattern() {
+        return bridgePattern;
     }
 }
