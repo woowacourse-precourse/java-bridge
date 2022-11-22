af0e1f76a51148bcf3eda85f9f5e6abd27a2806f
taehyun <didls1643@gmail.com>
2022-11-22 18:14:40 +0900

    test: success method test

--- src/test/java/bridge/BridgeGameTest.java
+++ src/test/java/bridge/BridgeGameTest.java
@@ -33,4 +33,15 @@ class BridgeGameTest {
         assertThat(result).isEqualTo(true);
     }
 
+    @DisplayName("success함수가 다리와 같은 리스트를 넣었을때 true를 반환하는 지 테스트")
+    @Test
+    void success() {
+        final BridgeGame bridgeGame = new BridgeGame(3);
+        List<String> move = bridgeGame.getBridge();
+        List<String> bridge = bridgeGame.getBridge();
+        for(String str : move){
+            bridgeGame.canMove(str);
+        }
+        assertThat(bridgeGame.success()).isEqualTo(true);
+    }
 }
