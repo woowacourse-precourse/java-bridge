22ca17bf5f33875ccd0a701a648d43f73931c742
jinhyeon <vmflwu123@gmail.com>
2022-11-22 17:52:54 +0900

    feat(GameStatus): 재시작 or 종료 입력에 대한 예외 처리 로직 추가

--- src/main/java/bridge/type/GameStatus.java
+++ src/main/java/bridge/type/GameStatus.java
@@ -1,5 +1,8 @@
 package bridge.type;
 
+import bridge.core.exception.Error;
+import bridge.core.exception.InvalidInputException;
+
 public enum GameStatus implements ProcessCondition {
 
     START("S"),
@@ -14,10 +17,19 @@ public enum GameStatus implements ProcessCondition {
     }
 
     public static GameStatus getGameStatusByGameCommand(String statusSymbol) {
+        validate(statusSymbol);
         if (statusSymbol.equals(START.statusSymbol)) return START;
         if (statusSymbol.equals(ONGOING.statusSymbol)) return ONGOING;
         if (statusSymbol.equals(RESTART.statusSymbol)) return RESTART;
         if (statusSymbol.equals(QUIT.statusSymbol)) return QUIT;
         return null;
+    }
+
+    //== validation ==//
+    private static void validate(String statusSymbol) {
+        if (statusSymbol.equals("R") || statusSymbol.equals("Q")) {
+            return;
+        }
+        throw new InvalidInputException(Error.GAME_COMMAND_ERROR);
     }
 }
