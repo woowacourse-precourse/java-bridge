da15765ee9f765b5f97e323970db8fb1fd7178c3
3juhwan <13selfesteem91@naver.com>
2022-11-22 18:54:50 +0900

    test(bridge): 다리와 유저 경로 비교 결과 로직 테스트 구현

---
+++ src/test/java/bridge/domain/UserPathTest.java
@@ -0,0 +1,54 @@
+package bridge.domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Stream;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class UserPathTest {
+
+    @DisplayName("")
+    @Nested
+    static class userPathTest {
+
+        private static Stream<Arguments> generateSuccessData() {
+            return Stream.of(
+                    Arguments.of(
+                            new UserMovement(Arrays.asList("U", "U", "D", "D")),
+                            new Bridge(Arrays.asList("U", "U", "D", "D")),
+                            Arrays.asList(Arrays.asList("O", "O", " ", " "), Arrays.asList(" ", " ", "O", "O"))
+                    ),
+                    Arguments.of(
+                            new UserMovement(Arrays.asList("D", "D", "U")),
+                            new Bridge(Arrays.asList("D", "D", "D", "U", "U")),
+                            Arrays.asList(Arrays.asList(" ", " ", "X"), Arrays.asList("O", "O", " "))
+                    ),
+                    Arguments.of(
+                            new UserMovement(Arrays.asList("U")),
+                            new Bridge(Arrays.asList("D")),
+                            Arrays.asList(Arrays.asList("X"), Arrays.asList(" "))
+                    ),
+                    Arguments.of(
+                            new UserMovement(Arrays.asList()),
+                            new Bridge(Arrays.asList("U", "D", "D")),
+                            Arrays.asList(Arrays.asList(), Arrays.asList())
+                    )
+            );
+        }
+
+        @DisplayName("성공 케이스")
+        @ParameterizedTest
+        @MethodSource("generateSuccessData")
+        void success(UserMovement userMovement, Bridge bridge, List<List<String>> result) {
+            assertThat(new UserPath(userMovement, bridge).getUpperPath()).isEqualTo(result.get(0));
+            assertThat(new UserPath(userMovement, bridge).getLowerPath()).isEqualTo(result.get(1));
+        }
+    }
+}
