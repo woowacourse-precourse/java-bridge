9a4f9a286eff46c15de8e0f81c45867f1ca48d2f
juno-junho <ssmm0205@naver.com>
2022-11-22 23:24:52 +0900

    feat(domain): create class BridgeGame

--- src/main/java/bridge/domain/BridgeGame.java
+++ src/main/java/bridge/domain/BridgeGame.java
@@ -1,23 +1,80 @@
 package bridge.domain;
 
+import bridge.constant.GameCommand;
+import bridge.constant.MoveCommand;
+import bridge.constant.MoveSign;
+
+import static bridge.constant.MoveSign.MOVE_FAIL;
+import static bridge.constant.MoveSign.MOVE_SUCCESS;
+
 /**
  * 다리 건너기 게임을 관리하는 클래스
  */
 public class BridgeGame {
 
-    /**
-     * 사용자가 칸을 이동할 때 사용하는 메서드
-     * <p>
-     * 이동을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
-     */
-    public void move() {
+    private static final String GAME_SUCCESS = "성공";
+    private static final String GAME_FAIL = "실패";
+
+    private final Bridge bridge;
+    private final Player player;
+    private final BridgeGameResults bridgeGameResults = new BridgeGameResults();
+
+    private String gameResult = GAME_SUCCESS;
+
+    public BridgeGame(final Bridge bridge, final Player player) {
+        this.bridge = bridge;
+        this.player = player;
+    }
+    public boolean isNotOver(final GameCommand gameCommand) {
+        return !gameCommand.is(GameCommand.QUIT) && playerIsOnTheBridge();
     }
 
-    /**
-     * 사용자가 게임을 다시 시도할 때 사용하는 메서드
-     * <p>
-     * 재시작을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
-     */
-    public void retry() {
+    private boolean playerIsOnTheBridge() {
+        return player.isOnTheBridge(bridge);
+    }
+
+    public String bridgeGameResult(MoveCommand moveCommand) {
+        if (bridge.isMatched(player, moveCommand)) {
+            return MOVE_SUCCESS.getMessage();
+        }
+        return MOVE_FAIL.getMessage();
+    }
+
+    public BridgeGameResults bridgeGameResults(MoveCommand moveCommand, String gameResult) {
+        bridgeGameResults.addResults(moveCommand, gameResult);
+        return bridgeGameResults;
+    }
+
+    public void move(String moveResult) {
+        if (moveResult.equals(MOVE_SUCCESS.getMessage())) {
+            player.move();
+        }
+    }
+
+    public void retryOrQuit(GameCommand gameCommand) {
+        if (gameCommand.is(GameCommand.RETRY)) {
+            retry();
+            return;
+        }
+
+        this.gameResult = GAME_FAIL;
+    }
+
+    private void retry() {
+        bridgeGameResults.reset();
+        player.resetPosition();
+        player.increaseChallenges();
+    }
+
+    public BridgeGameResults bridgeGameResults() {
+        return bridgeGameResults;
+    }
+
+    public int totalNumberOfChallenges() {
+        return player.numberOfChallenges();
+    }
+
+    public String gameResult() {
+        return gameResult;
     }
 }
