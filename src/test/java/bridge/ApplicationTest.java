4c36b9cb30e42542a9d75baf740a5e3324a7ad47
kkirby54 <sdk926@gmail.com>
2022-11-22 21:14:51 +0900

    refactor: 테스트에서 DisplayName 표시

--- src/test/java/bridge/ApplicationTest.java
+++ src/test/java/bridge/ApplicationTest.java
@@ -5,12 +5,14 @@ import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import camp.nextstep.edu.missionutils.test.NsTest;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class ApplicationTest extends NsTest {
 
     private static final String ERROR_MESSAGE = "[ERROR]";
 
+    @DisplayName("프로그램이 성공적으로 작동한다.")
     @Test
     void 기능_테스트() {
         assertRandomNumberInRangeTest(() -> {
@@ -29,14 +31,15 @@ class ApplicationTest extends NsTest {
         }, 1, 0, 1);
     }
 
+    @DisplayName("다리의 길이가 숫자가 아니면 예외가 발생한다.")
     @Test
-    void 예외_테스트() {
+    void 다리_길이_입력_예외_테스트() {
         assertSimpleTest(() -> {
             runException("a");
             assertThat(output()).contains(ERROR_MESSAGE);
         });
     }
-
+    
     @Override
     protected void runMain() {
         Application.main(new String[]{});
