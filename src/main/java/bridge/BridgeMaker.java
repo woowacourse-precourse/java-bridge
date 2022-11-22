c09a5c9cfc607ae8310f5763587eee94555ec161
Seoyoung <ryupark1006@gmail.com>
2022-11-22 15:40:22 +0900

    feat: 다리 길이에 맞게 랜덤 값 생성 기능 구현

--- src/main/java/bridge/BridgeMaker.java
+++ src/main/java/bridge/BridgeMaker.java
@@ -1,6 +1,9 @@
 package bridge;
 
+import java.util.ArrayList;
 import java.util.List;
+
+import static bridge.Error.BRIDGE_NUMBER_ERROR;
 
 /**
  * 다리의 길이를 입력 받아서 다리를 생성해주는 역할을 한다.
@@ -18,6 +21,12 @@ public class BridgeMaker {
      * @return 입력받은 길이에 해당하는 다리 모양. 위 칸이면 "U", 아래 칸이면 "D"로 표현해야 한다.
      */
     public List<String> makeBridge(int size) {
+        for (int i = 0; i < size; i++) {
+            int number = bridgeNumberGenerator.generate();
+            if (number != 0 && number != 1) {
+                throw new IllegalArgumentException(BRIDGE_NUMBER_ERROR.getMessage());
+            }
+        }
         return null;
     }
 }
--- src/main/java/bridge/Error.java
+++ src/main/java/bridge/Error.java
@@ -2,7 +2,8 @@ package bridge;
 
 public enum Error {
 
-    BRIDGE_SIZE_ERROR("[ERROR] 다리 길이는 3이상 20 이하의 숫자여야 합니다.");
+    BRIDGE_SIZE_ERROR("[ERROR] 다리 길이는 3이상 20 이하의 숫자여야 합니다."),
+    BRIDGE_NUMBER_ERROR("[ERROR] 무작위 값은 0 또는 1이어야 합니다.");
 
     private final String message;
 
