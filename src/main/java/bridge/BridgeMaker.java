f2b992e62d0d3fb225f9dee72a65e409b70ab7a6
BuildTools <unconfigured@null.spigotmc.org>
2022-11-22 20:17:28 +0900

    refactor: 수정 주석제거
    본 커밋은 마지막커밋이 test면 우테코사이트에서 오류가나서 넣었습니다

--- src/main/java/bridge/BridgeMaker.java
+++ src/main/java/bridge/BridgeMaker.java
@@ -15,10 +15,6 @@ public class BridgeMaker {
         this.bridgeNumberGenerator = bridgeNumberGenerator;
     }
 
-    /**
-     * @param size 다리의 길이
-     * @return 입력받은 길이에 해당하는 다리 모양. 위 칸이면 "U", 아래 칸이면 "D"로 표현해야 한다.
-     */
     public List<String> makeBridge(int size) {
         return Stream.generate(bridgeNumberGenerator::generate)
                 .map(this::convertBridgeNumberToBridgeString)
