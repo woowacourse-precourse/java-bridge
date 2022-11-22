fc9c387c002c7d0dd122be38e9bcae9d20eb7d48
mihye126 <mihea12@gmail.com>
2022-11-22 21:05:10 +0900

    [test] ApplicationTest 테스트 케이스 추가- 실패 후 종료

--- src/test/java/bridge/ApplicationTest.java
+++ src/test/java/bridge/ApplicationTest.java
@@ -46,7 +46,7 @@ class ApplicationTest extends NsTest {
         assertRandomNumberInRangeTest(() -> {
             run("3", "U", "D", "U","R","U", "D","D");
             assertThat(output()).contains(
-                " O |   | X ]",
+                "[ O |   | X ]",
                 "[   | O |   ]",
                 "게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)",
 
@@ -59,6 +59,27 @@ class ApplicationTest extends NsTest {
 
             int upSideIndex = output().indexOf("[ O |   |   ]");
             int downSideIndex = output().indexOf("[   | O | O ]");
+            assertThat(upSideIndex).isLessThan(downSideIndex);
+        }, 1, 0, 0);
+    }
+
+    @Test
+    @DisplayName("실패 후 종료")
+    void quit_test(){
+        assertRandomNumberInRangeTest(() -> {
+            run("3", "U", "D", "U","Q");
+            assertThat(output()).contains(
+                "[ O |   | X ]",
+                "[   | O |   ]",
+                "게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)",
+
+                "최종 게임 결과",
+                "게임 성공 여부: 실패",
+                "총 시도한 횟수: 1"
+            );
+
+            int upSideIndex = output().indexOf("[ O |   | X ]");
+            int downSideIndex = output().indexOf("[   | O |   ]");
             assertThat(upSideIndex).isLessThan(downSideIndex);
         }, 1, 0, 0);
     }
