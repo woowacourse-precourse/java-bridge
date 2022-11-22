5027c6cbd674dd06d92b5d41e42fd24695c90824
Go-Jaecheol <gojaech@naver.com>
2022-11-22 19:47:17 +0900

    style: 다리 길이가 3보다 작은 경우 테스트 메서드명 변경

--- src/test/java/bridge/domain/BridgeTest.java
+++ src/test/java/bridge/domain/BridgeTest.java
@@ -14,9 +14,9 @@ import java.util.List;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class BridgeTest {
-    @DisplayName("다리 길이는 3보다 작으면 예외가 발생한다.")
+    @DisplayName("다리 길이가 3보다 작으면 예외가 발생한다.")
     @Test
-    void inputAmountByInvalidUnit() {
+    void inputLengthByUnderRange() {
         assertThatThrownBy(() -> new Bridge(2, new BridgeMaker(new BridgeRandomNumberGenerator())))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining("[ERROR] 다리 길이는 3부터 20 사이의 숫자여야 합니다.");
