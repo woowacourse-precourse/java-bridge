d83a8da6c7c7bd3b343ad593dc39f04619572d54
CupRaccoon <41440969+CupRaccoon@users.noreply.github.com>
2022-11-22 19:38:02 +0900

    Add Output message

--- src/main/java/bridge/InputView.java
+++ src/main/java/bridge/InputView.java
@@ -10,7 +10,7 @@ public class InputView {
     /**
      * 다리의 길이를 입력받는다.
      */
-    public int readBridgeSize() {
+    public int readBridgeSize() throws IllegalArgumentException {
         String input = Console.readLine();
         int bridgeSize = inputToNumber(input);
         if(bridgeSize < 3 || bridgeSize > 20){
@@ -22,7 +22,7 @@ public class InputView {
     /**
      * 사용자가 이동할 칸을 입력받는다.
      */
-    public String readMoving() {
+    public String readMoving() throws IllegalArgumentException {
         String input = Console.readLine();
         if(!input.equals("U") && !input.equals("D")){
             throw new IllegalArgumentException("이동할 칸은 \"U\"또는 \"D\"를 입력해야 합니다.");
@@ -33,7 +33,7 @@ public class InputView {
     /**
      * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
      */
-    public String readGameCommand() {
+    public String readGameCommand() throws  IllegalArgumentException {
         String input = Console.readLine();
         if(!input.equals("R") && !input.equals("Q")){
             throw new IllegalArgumentException("재시작/종료 여부는 \"R\"또는 \"Q\"를 입력해야 합니다.");
--- src/main/java/bridge/OutputView.java
+++ src/main/java/bridge/OutputView.java
@@ -8,21 +8,39 @@ import org.w3c.dom.ls.LSOutput;
 public class OutputView {
 
 
-    public void printMap() {
+    public void printMap(BridgeGame bridgeGame) {
+        System.out.println(bridgeGame.toString());
+
     }
 
-    public void printResult() {
+    public void printResult(boolean isSuccess, BridgeGame bridgeGame) {
+        System.out.println("최종 게임 결과");
+        printMap(bridgeGame);
+        if(isSuccess){
+            System.out.println("게임 성공 여부: " + "성공");
+        }
+        if(!isSuccess){
+            System.out.println("게임 성공 여부: " + "실패");
+        }
+        System.out.println("총 시도한 횟수: " + bridgeGame.getTryNumber());
     }
-    public void printStart(){
+
+    public void printStart() {
         System.out.println("다리 건너기 게임을 시작합니다.");
     }
-    public void printBridgeMake(){
+
+    public void printBridgeMake() {
         System.out.println("다리의 길이를 입력해주세요.");
     }
-    public void printMove(){
+
+    public void printMove() {
         System.out.println("이동할 칸을 선택해주세요. (위: U, 아래: D)");
     }
-    public void printRetry(){
+
+    public void printRetry() {
         System.out.println("게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)");
+    }
+    public void printException(Exception e){
+        System.out.println("[ERROR] "+e.getMessage());
     }
 }
