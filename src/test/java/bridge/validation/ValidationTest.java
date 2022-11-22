6df040499fd4da0cc0dda6180289169a0acba799
SeokHwan An <asok369258@naver.com>
2022-11-22 09:30:21 +0900

    test : 다리의 길이 입력값이 숫자로 구성되었는지 검증하는 기능 테스트 케이스 추가

--- src/test/java/bridge/validation/ValidationTest.java
+++ src/test/java/bridge/validation/ValidationTest.java
@@ -13,7 +13,7 @@ class ValidationTest {
 
     @DisplayName("다리의 길이 입력값이 숫자 이외의 값이 들어오면 에러가 발생한다.")
     @ParameterizedTest
-    @ValueSource(strings = {"1a34, ㄱ1ㄴㄷ, !1aㄴ, .12@3"})
+    @ValueSource(strings = {"1a34, ㄱ1ㄴㄷ, !1aㄴ, .12@3, '', ' '"})
     void createBridgeLengthNumOtherThanNum(String bridgeLength) {
         assertThatThrownBy(() -> Validation.checkBridgeLengthConsistOfNum(bridgeLength))
             .isInstanceOf(IllegalArgumentException.class)
