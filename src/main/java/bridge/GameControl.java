23fb180958f6ffc00f54c77be008ddcb3bbbd134
DESKTOP-AUSH0GT\Power <sgh0718@naver.com>
2022-11-22 18:46:06 +0900

    feat : 게임 진행 클래스 에서 게임을 실패, 성공 경우 진행되는 기능
    반복문을 통해 게임의 재시작 여부와 성공, 실패 경우 까지 구현한 메서드

--- src/main/java/bridge/GameControl.java
+++ src/main/java/bridge/GameControl.java
@@ -7,6 +7,23 @@ public class GameControl {
     InputView input = new InputView();
     OutputView output = new OutputView();
     CentralGame bring = new CentralGame();
+    String retry = "R";
+
+    public List<Integer> oneGame(List<String> answer, int blankNumber) {
+        int count = 1;
+        while (true) {
+            blankNumber = oneStep(answer, blankNumber);
+            if (blankNumber == answer.size()) {
+                break;
+            }
+            if (!bring.Compare(retry, input.readGameCommand())) {
+                break;
+            }
+            count = bring.centralRetry(count);
+            blankNumber = 0;
+        }
+        return bring.result(blankNumber, count);
+    }
 
     public int oneStep(List<String> answer, int blankNumber) {
         while (blankNumber < answer.size()) {
