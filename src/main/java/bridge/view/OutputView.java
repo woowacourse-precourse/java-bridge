47c3cf940a1eb01eecf8e84f6418bd588f431745
username459712 <94822739+username459712@users.noreply.github.com>
2022-11-22 21:33:10 +0900

    feat: printMap()-> upstringBuilder, downStringBuilder 출력

--- src/main/java/bridge/view/OutputView.java
+++ src/main/java/bridge/view/OutputView.java
@@ -16,4 +16,9 @@ public class OutputView {
     public void printRestartOrQuit() {
         System.out.println("\n게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)");
     }
+
+    public void printMap(StringBuilder upBuilder, StringBuilder downBuilder) {
+        System.out.println(upBuilder);
+        System.out.println(downBuilder);
+    }
 }
