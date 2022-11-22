bdf75ef618fe02a6552c11c30feba353ae1295dd
Voyager003 <85725033+Voyager003@users.noreply.github.com>
2022-11-22 16:58:39 +0900

    Style: 변경 ERROR_RANGE 에러 메시지 변경

--- src/main/java/bridge/constant/Error.java
+++ src/main/java/bridge/constant/Error.java
@@ -2,7 +2,7 @@ package bridge.constant;
 
 public enum Error {
     ERROR_EMPTY("[ERROR] 아무것도 입력되지 않았습니다. 메시지 조건에 맞게 입력해주세요."),
-    ERROR_RANGE("[ERROR] 3부터 20 사이의 숫자를 입력해주세요."),
+    ERROR_RANGE("[ERROR] 다리 길이는 3부터 20 사이의 숫자여야 합니다."),
     ERROR_MOVING("[ERROR] 잘못된 입력입니다. U와 D중 하나를 입력해주세요."),
     ERROR_RESTART("[ERROR] 잘못된 입력입니다. R과 Q중 하나를 입력해주세요.");
 
