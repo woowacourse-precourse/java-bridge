3bba42887169ab42eea93bfa402319c5843cb994
JohnPrk <uo3641493@gmail.com>
2022-11-22 14:34:45 +0900

    feat(InputView) : 사용자의 게임 재시작 여부 입력 및 입력 받은 값을 GameCommand 객체로 만들어 유효성 검사 처리 기능 생성, 입력값이 문제가 된다면 에러 메시지를 출력 후 그 부분부터 입력을 다시 받는다.

--- src/main/java/bridge/InputView.java
+++ src/main/java/bridge/InputView.java
@@ -9,6 +9,8 @@ public class InputView {
 
     private static final String INPUT_BRIDGE_SIZE_MESSAGE = "\n다리의 길이를 입력해주세요.";
     private static final String INPUT_BRIDGE_LOCATION_MESSAGE = "\n이동할 칸을 선택해주세요. (위: U, 아래: D)";
+    private static final String INPUT_RESTART_OR_QUIT_MESSAGE = "\n게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)";
+    private static final String RESTART = "R";
 
     /**
      * 다리의 길이를 입력받는다.
@@ -23,7 +25,7 @@ public class InputView {
             System.out.println(illegalArgumentException.getMessage());
             return readBridgeSize();
         }
-     }
+    }
 
     /**
      * 사용자가 이동할 칸을 입력받는다.
@@ -41,7 +43,14 @@ public class InputView {
     /**
      * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
      */
-    public String readGameCommand() {
-        return null;
+    public boolean readGameCommand() {
+        System.out.println(INPUT_RESTART_OR_QUIT_MESSAGE);
+        try {
+            GameCommand gameCommand = GameCommand.getGameCommand(Console.readLine());
+            return gameCommand.getCommand().equals(RESTART);
+        } catch (IllegalArgumentException illegalArgumentException) {
+            System.out.println(illegalArgumentException.getMessage());
+            return readGameCommand();
+        }
     }
 }
