2b77d86db5686525434076c78b5d25803a5c53aa
yoojin Jang <yoojinjangjang@gmail.com>
2022-11-22 15:45:55 +0900

    [style] 공백 수정

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -6,7 +6,7 @@ public class Application {
 
     public static void main(String[] args) {
         GameController gameController = new GameController(new BridgeRandomNumberGenerator());
-        try{
+        try {
             gameController.playGame();
         }catch (IllegalArgumentException e) {
             System.out.println(e.getMessage());
