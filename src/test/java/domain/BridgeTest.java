35d7d8f7c86a10cbafa85344ef058b0499969ec8
CMHyuk <alsgur9710@naver.com>
2022-11-22 11:58:54 +0900

    test(BridgeTest): 다리 성공, 실패 로직 테스트

---
+++ src/test/java/domain/BridgeTest.java
@@ -0,0 +1,55 @@
+package domain;
+
+import bridge.domain.Bridge;
+import org.junit.jupiter.api.*;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class BridgeTest {
+
+    Bridge bridge;
+
+    @BeforeEach
+    @AfterEach
+    void clear() {
+        bridge = new Bridge();
+        bridge.setUpBridge();
+        bridge.setDownBridge();
+    }
+
+    @DisplayName("윗 다리 성공 테스트")
+    @Test
+    void createSuccessUpBridge() {
+        bridge.succeedUpMovement();
+        bridge.replaceBridge();
+        String upBridge = bridge.getUpBridge();
+        assertThat(upBridge).isEqualTo("[ O ]");
+    }
+
+    @DisplayName("밑 다리 성공 테스트")
+    @Test
+    void createSuccessDownBridge() {
+        bridge.succeedDownMovement();
+        bridge.replaceBridge();
+        String downBridge = bridge.getDownBridge();
+        assertThat(downBridge).isEqualTo("[ O ]");
+    }
+
+    @DisplayName("윗 다리 실패 테스트")
+    @Test
+    void createFailUpBridge() {
+        bridge.failUpMovement();
+        bridge.replaceBridge();
+        String upBridge = bridge.getUpBridge();
+        assertThat(upBridge).isEqualTo("[ X ]");
+    }
+
+    @DisplayName("밑 다리 실패 테스트")
+    @Test
+    void createFailDownBridge() {
+        bridge.failDownMovement();
+        bridge.replaceBridge();
+        String downBridge = bridge.getDownBridge();
+        assertThat(downBridge).isEqualTo("[ X ]");
+    }
+}
