b5c03d8c2db1410f9acfb0e93bda32f32807a635
BHC-Chicken <tij05078@gmail.com>
2022-11-22 11:12:00 +0900

    feat : 입력 저장
    최종결과 출력을 위해 유저의 입력을 저장한다.

--- src/main/java/bridge/BridgeGame.java
+++ src/main/java/bridge/BridgeGame.java
@@ -40,6 +40,13 @@ public class BridgeGame {
      * <p>
      * 재시작을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void retry() {
+
+    public List<String> inputHistory() {
+        List<String> moveHistory = new ArrayList<>();
+        for (int i = 0; i < userInput.size(); i++) {
+            moveHistory.add(returnResultValue(i));
+        }
+        return moveHistory;
     }
+        }
 }
