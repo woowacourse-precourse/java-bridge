429d297b039ada3330a80f10b1fc8666646d93ea
Sojeong <k5k5s5s5j5j5@naver.com>
2022-11-22 18:13:45 +0900

    refactor(main/BridgeTest): 코드 정리 및 메소드 분리

--- src/test/java/bridge/BridgeGameTest.java
+++ src/test/java/bridge/BridgeGameTest.java
@@ -15,14 +15,15 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class BridgeGameTest {
 
+    static List<String> bridge = List.of("U", "D", "U");
+
+
     @DisplayName("움직였을 때 position 테스트")
     @Test
     void positionTest() {
         int position = 1;
-        List<String> bridge = List.of("U", "D", "U");
         BridgeGame bridgeGame = new BridgeGame(1, position, bridge, "U");
-        final byte[] buf = String.join("\n", "D").getBytes();
-        System.setIn(new ByteArrayInputStream(buf));
+        input("D");
         bridgeGame.move();
         assertThat(bridgeGame).extracting("position").isEqualTo(position + 1);
     }
@@ -31,13 +32,9 @@ public class BridgeGameTest {
     @DisplayName("재시작 시 다리 구성 동일한지 테스트")
     @Test
     void sameBridgeTest() {
-        List<String> bridge = List.of("U", "D", "U");
         BridgeGame bridgeGame = new BridgeGame(1, 0, bridge, "");
-        final byte[] buf = String.join("\n", "U", "D", "D", "R", "U").getBytes();
-        System.setIn(new ByteArrayInputStream(buf));
-        bridgeGame.moveUntilGameOver();
-        EndType type = bridgeGame.gameEnded();
-        bridgeGame.successEndOrFailEnd(type);
+        input("U", "D", "D", "R", "U");
+        playRoundEndType(bridgeGame);
         bridgeGame.move();
         assertThat(bridgeGame).extracting("bridge").isEqualTo(bridge);
     }
@@ -46,13 +43,9 @@ public class BridgeGameTest {
     @DisplayName("게임 재시작 테스트")
     @Test
     void retryTest() {
-        List<String> bridge = List.of("U", "D", "U");
-        final byte[] buf = String.join("\n", "U", "D", "D", "R").getBytes();
-        System.setIn(new ByteArrayInputStream(buf));
+        input("U", "D", "D", "Q");
         BridgeGame bridgeGame = new BridgeGame(1, 0, bridge, "");
-        bridgeGame.moveUntilGameOver();
-        EndType type = bridgeGame.gameEnded();
-        bridgeGame.successEndOrFailEnd(type);
+        EndType type = playRoundEndType(bridgeGame);
         assertThat(type).isEqualTo(FAIL_RETRY);
         assertThat(bridgeGame).extracting("trial").isEqualTo(2);
         assertThat(bridgeGame).extracting("position").isEqualTo(0);
@@ -60,14 +53,23 @@ public class BridgeGameTest {
 
     @DisplayName("게임 종료 테스트")
     @Test
-    void quitTest(){
-        List<String> bridge = List.of("U", "D", "U");
-        final byte[] buf = String.join("\n", "U", "D", "D", "Q").getBytes();
-        System.setIn(new ByteArrayInputStream(buf));
+    void quitTest() {
+        input("U", "D", "D", "Q");
         BridgeGame bridgeGame = new BridgeGame(1, 0, bridge, "");
+        EndType type = playRoundEndType(bridgeGame);
+        assertThat(type).isEqualTo(FAIL_QUIT);
+    }
+
+    void input(String... command) {
+        final byte[] buf = String.join("\n", command).getBytes();
+        System.setIn(new ByteArrayInputStream(buf));
+    }
+
+    EndType playRoundEndType(BridgeGame bridgeGame) {
         bridgeGame.moveUntilGameOver();
         EndType type = bridgeGame.gameEnded();
         bridgeGame.successEndOrFailEnd(type);
-        assertThat(type).isEqualTo(FAIL_QUIT);
+        return type;
     }
+
 }
