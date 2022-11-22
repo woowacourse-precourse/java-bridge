5e079b50e38db207d5866937f1f296d9da1c4c74
jooys130 <wndustn130@gmail.com>
2022-11-22 11:05:09 +0900

    feat: 재시작 또는 종료를 입력 받는다
    - 게임을 다시 시도할지 여부를 입력하라는 문구를 출력한다
    - R 또는 D가 아니면 예외가 발생한다

--- src/main/java/bridge/ErrorMessage.java
+++ src/main/java/bridge/ErrorMessage.java
@@ -2,7 +2,8 @@ package bridge;
 
 public enum ErrorMessage {
     BRIDGE_SIZE_OUT_OF_RANGE("3 이상 20 이하의 숫자를 입력해주세요"),
-    MOVING_OUT_OF_RANGE("U 또는 D를 입력해주세요");
+    MOVING_OUT_OF_RANGE("U 또는 D를 입력해주세요"),
+    GAME_COMMAND_OUT_OF_RANGE("R 또는 Q를 입력해주세요");
 
     private final String errorMessage;
 
--- src/main/java/bridge/InputView.java
+++ src/main/java/bridge/InputView.java
@@ -44,7 +44,7 @@ public class InputView {
         return moving;
     }
 
-    public static void validateUpOrDown(String moving){
+    public static void validateUpOrDown(String moving) {
         if (!moving.equals("U") && !moving.equals("D")) {
             throw new IllegalArgumentException(MOVING_OUT_OF_RANGE.toString());
         }
@@ -54,6 +54,15 @@ public class InputView {
      * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
      */
     public String readGameCommand() {
-        return null;
+        String gameCommand = printAndRead(
+                "게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)");
+        validateRetryOrQuit(gameCommand);
+        return gameCommand;
+    }
+
+    public void validateRetryOrQuit(String gameCommand) {
+        if (!gameCommand.equals("R") && !gameCommand.equals("Q")) {
+            throw new IllegalArgumentException(GAME_COMMAND_OUT_OF_RANGE.toString());
+        }
     }
 }
