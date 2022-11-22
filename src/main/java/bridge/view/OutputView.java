ed2df853964107498ef4d1916e5ba3d7dd6a5cf2
jiwon <ljwon77@daum.net>
2022-11-22 13:23:26 +0900

    feat: add result constant message
    최종 결과를 출력하는 메시지를 추가하였습니다.

--- src/main/java/bridge/view/OutputView.java
+++ src/main/java/bridge/view/OutputView.java
@@ -1,5 +1,6 @@
 package bridge.view;
 
+import bridge.BridgeGame;
 import bridge.BridgePlayer;
 
 import static bridge.constant.BridgeConst.*;
@@ -8,8 +9,7 @@ import static bridge.constant.BridgeControl.RESTART;
 import static bridge.constant.BridgeMove.DOWN;
 import static bridge.constant.BridgeMove.UP;
 import static bridge.constant.message.InputMessage.*;
-import static bridge.constant.message.OutputMessage.SUCCESS_OR_FAILURE;
-import static bridge.constant.message.OutputMessage.TOTAL_ATTEMPTS;
+import static bridge.constant.message.OutputMessage.*;
 
 /**
  * 사용자에게 게임 진행 상황과 결과를 출력하는 역할을 한다.
@@ -147,6 +147,7 @@ public class OutputView {
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
     public void printResult() {
+        System.out.println(RESULT.getMessage());
         System.out.printf(SUCCESS_OR_FAILURE.getMessage(), getGameResult());
         System.out.printf(TOTAL_ATTEMPTS.getMessage(), bridgePlayer.getTryCount());
     }
