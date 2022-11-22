ac9f244f865b081e00c84057f231629dfa69a4d4
Kim Seung Beom <88074556+daily1313@users.noreply.github.com>
2022-11-22 17:49:15 +0900

    refactor(Bridge) : Bridge 도메인에 필요한 메서드 수정 및 기능 추가

--- src/main/java/bridge/domain/Bridge.java
+++ src/main/java/bridge/domain/Bridge.java
@@ -1,20 +1,41 @@
 package bridge.domain;
 
-import java.util.ArrayList;
 import java.util.List;
 
 public class Bridge {
-    private List<String> Bridge = new ArrayList<>();
+    private List<String> bridge;
+    private int nowIndex;
 
     public Bridge(List<String> bridge) {
-        Bridge = bridge;
+        this.bridge = bridge;
+        this.nowIndex = 0;
     }
 
     public List<String> getBridge() {
-        return Bridge;
+        return bridge;
     }
 
     public void setBridge(List<String> bridge) {
-        Bridge = bridge;
+        this.bridge = bridge;
+    }
+
+    public int getNowIndex() {
+        return this.nowIndex;
+    }
+
+    public boolean isSameValueOfComputerAndUser(String move) {
+        return bridge.get(this.nowIndex).equals(move);
+    }
+
+    public boolean isEndPoint() {
+        return bridge.size() - 1 == nowIndex;
+    }
+
+    public void nextIndex() {
+        this.nowIndex += 1;
+    }
+
+    public void resetIndex() {
+        nowIndex = 0;
     }
 }
