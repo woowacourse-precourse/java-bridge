df7468b23c52f6e3ea957395d4f24ae5f956d6b1
juno-junho <ssmm0205@naver.com>
2022-11-22 23:44:34 +0900

    test(view): test refactoring

--- src/test/java/bridge/view/InputViewConverterTest.java
+++ src/test/java/bridge/view/InputViewConverterTest.java
@@ -4,12 +4,11 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
-import static org.junit.jupiter.api.Assertions.*;
 
 class InputViewConverterTest {
 
-    @ParameterizedTest(name = "숫자가 아닌 문자가 입력되면 예외를 발생시킨다. 입력: {0}")
-    @ValueSource(strings = {"a", "10j", "하잉"})
+    @ParameterizedTest(name = "숫자가 아닌 문자 입력시 예외 발생. 입력: {0}")
+    @ValueSource(strings = {"abc", "가나다"})
     void validateThisIsNumberTest(String inputValue) {
         InputViewConverter inputValueConverter = new InputViewConverter();
 
