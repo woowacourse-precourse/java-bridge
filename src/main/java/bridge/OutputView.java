f33472caf023ca3994200ded326f88351341191e
brotherstone97 <brotherstone97@gmail.com>
2022-11-22 13:19:51 +0900

    feat(OutputView): 게임 시작 문구 및 입력별 안내 메시지 출력 기능

--- src/main/java/bridge/OutputView.java
+++ src/main/java/bridge/OutputView.java
@@ -20,4 +20,14 @@ public class OutputView {
      */
     public void printResult() {
     }
+    public void requestStartMessage(){
+        System.out.println("다리 건너기 게임을 시작합니다.");
+    }
+    public void requestReadMovingMessage(){
+        System.out.println("이동할 칸을 선택해주세요. (위: U, 아래: D)");
+    }
+    public void requestReadGameCommandMessage(){
+        System.out.println("게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)");
+    }
 }
+
