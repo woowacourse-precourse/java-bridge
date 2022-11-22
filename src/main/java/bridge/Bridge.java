48e2122bf676f6736faec87f4326026e56de9780
rjsah09 <83952174+rjsah09@users.noreply.github.com>
2022-11-22 17:20:35 +0900

    refactor: Bridge 클래스 tried 인스턴스 추가, setter 생성

--- src/main/java/bridge/Bridge.java
+++ src/main/java/bridge/Bridge.java
@@ -5,10 +5,12 @@ import java.util.List;
 public class Bridge {
     private List<String> footholds;
     private List<String> crossed;
+    private int tried;
 
     public Bridge(List<String> footholds, List<String> crossed) {
         this.footholds = footholds;
         this.crossed = crossed;
+        tried = 1;
     }
 
     public List<String> getFootholds() {
@@ -17,6 +19,18 @@ public class Bridge {
 
     public List<String> getCrossed() {
         return this.crossed;
+    }
+
+    public int getTried() {
+        return this.tried;
+    }
+
+    public void setCrossed(List<String> crossed) {
+        this.crossed = crossed;
+    }
+
+    public void setTried(int tried) {
+        this.tried = tried;
     }
 
 }
