7a08de31ae60779dc78ee26499bd1e094ff34a27
koo9b9h <90121024+koo9b9h@users.noreply.github.com>
2022-11-22 14:14:35 +0900

    feat: 지정된 유형이 아니면 예외 발생하는 기능 추가
    allNumberCheck() : 문자열이 숫자인지 체크하는 기능
    
    unitNumberCheck(): 자릿수마다 숫자인지 체크하는 기능
    
    UDcheck(): 들어온 문자가 U 또는 D인지 체크하는 기능
    
    RQcheck() : 들어온 문자가 R 또는 Q인지 체크하는 기능

---
+++ src/main/java/bridge/exception/DesignatedTypeException.java
@@ -0,0 +1,32 @@
+package bridge.exception;
+
+public class DesignatedTypeException {
+    private static final String BASIC_ERROR_MESSAGE = "[ERROR]";
+
+    public static void allNumberCheck(String size) {
+        for (char number : size.toCharArray()) {
+            unitNumberCheck(number);
+        }//for
+    }// numberCheck
+
+    private static void unitNumberCheck(char number) {
+        if (57 < number || number < 47) {
+            System.out.println(BASIC_ERROR_MESSAGE + " 다리의 길이를 숫자로 입력해주세요.");
+            throw new IllegalArgumentException(BASIC_ERROR_MESSAGE + " 다리의 길이를 숫자로 입력해주세요.");
+        }//if
+    }//unitNumberCheck
+
+    public static void UDcheck(String movement) {
+        if (!(movement.equals("D") || movement.equals("U"))) {
+            System.out.println(BASIC_ERROR_MESSAGE + " 지정된 문자를 입력해 주세요.");
+            throw new IllegalArgumentException(BASIC_ERROR_MESSAGE + " 지정된 문자를 입력해 주세요.");
+        }//if
+    }// UDcheck
+
+    public static void RQcheck(String movement) {
+        if (!(movement.equals("R") || movement.equals("Q"))) {
+            System.out.println(BASIC_ERROR_MESSAGE + " 지정된 문자를 입력해 주세요.");
+            throw new IllegalArgumentException(BASIC_ERROR_MESSAGE + " 지정된 문자를 입력해 주세요.");
+        }//if
+    }// RQcheck
+}//end class
