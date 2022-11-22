4f75d37b05c5b7da22620349f41ea4222ceaa85a
footdev <ekclstkfka5@gmail.com>
2022-11-22 15:12:30 +0900

    feat: Crossble 인터페이스 추가
    인터페이스를 추가함.

---
+++ src/main/java/bridge/domain/Crossable.java
@@ -0,0 +1,10 @@
+package bridge.domain;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public interface Crossable {
+    public final List<String> bridge = new ArrayList<>();
+
+    int get();
+}
