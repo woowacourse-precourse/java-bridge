b6bd423890cb2fa3aa4be8847cb5130211368554
gomljo <gomljo0307@naver.com>
2022-11-22 17:10:05 +0900

    refactor(GameRecordGenerator): 기능 이전
    현재 게임 기록에 대한 기능만 가지도록 GameRecordMaker에서 가지고 있는 현재 게임 기록 변환 기능을 이전

--- src/main/java/bridge/domain/model/GameRecordGenerator.java
+++ src/main/java/bridge/domain/model/GameRecordGenerator.java
@@ -1,17 +1,43 @@
 package bridge.domain.model;
 
 import bridge.domain.constant.BridgeDirection;
-import bridge.domain.constant.CrossingState;
+import bridge.domain.constant.CrossedState;
 import bridge.domain.constant.GameRecord;
+import bridge.domain.constant.GameRecordFormat;
 
 import java.util.List;
+import java.util.ListIterator;
 
 public class GameRecordGenerator {
-
-    public List<String> generate(boolean crossedBridgeState, BridgeDirection currentDirection) {
-        String result = CrossingState.transform(crossedBridgeState);
+    private final int FIRST_TRIAL = 1;
+    private List<String> currentGameRecord;
+    public void generate(boolean crossedBridgeState, BridgeDirection currentDirection) {
+        String result = CrossedState.transform(crossedBridgeState);
         GameRecord gameRecord = GameRecord.findLocation(currentDirection);
-        return gameRecord.generate(result);
+        currentGameRecord = gameRecord.generate(result);
+    }
+
+    private boolean isFirstTrial(int trial) {
+        return trial == FIRST_TRIAL;
+    }
+
+    private String makeFormat(boolean isFirst, String record) {
+        GameRecordFormat gameRecordFormat = GameRecordFormat.findByTrial(isFirst);
+        return gameRecordFormat.generate(record);
+    }
+
+    public void updateCurrentRecord(int trial) {
+        boolean isFirst = isFirstTrial(trial);
+        ListIterator<String> currentResult = currentGameRecord.listIterator();
+
+        while (currentResult.hasNext()) {
+            int currentIndex = currentResult.nextIndex();
+            currentGameRecord.set(currentIndex, makeFormat(isFirst, currentResult.next()));
+        }
+    }
+
+    public List<String> getCurrentGameRecord(){
+        return currentGameRecord;
     }
 
 }
