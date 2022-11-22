127a47586ec0605b1aa387785bb2fbbddaec6c79
dev_kong <ahdjd5@gmail.com>
2022-11-22 13:13:31 +0900

    test(BridgeGame): add test code to check getPlayResult

--- src/test/java/bridge/BridgeGameTest.java
+++ src/test/java/bridge/BridgeGameTest.java
@@ -7,8 +7,10 @@ import bridge.domain.User;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
+import bridge.utils.enums.GameState;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
@@ -40,5 +42,23 @@ public class BridgeGameTest {
     @CsvSource({"U, true", "D, false"})
     void checkMoveTest(String input, boolean expect) {
         assertThat(game.move(input)).isEqualTo(expect);
+    }
+
+    @DisplayName("playResult playing 또는 loose 테스트")
+    @ParameterizedTest(name = "input = {0}, expect = {1}")
+    @CsvSource({"U, PLAYING", "D, LOOSE"})
+    void checkGetPlayResultIsPlaying(String input, String expect) {
+        game.move(input);
+        assertThat(game.getPlayResult()).isEqualTo(expect);
+    }
+
+    @DisplayName("playResult WIN 테스트")
+    @Test
+    void checkGetPlayResultIsWin() {
+        List<String> moveInputs = List.of("U","D","U");
+        for (String moveInput : moveInputs) {
+            game.move(moveInput);
+        }
+        assertThat(game.getPlayResult()).isEqualTo("WIN");
     }
 }
