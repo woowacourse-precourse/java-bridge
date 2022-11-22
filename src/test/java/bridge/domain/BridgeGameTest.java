8afea11d1d8d663cb4006fb3226f053f7fb794d6
hhiyeon <hhyyeon0214@gmail.com>
2022-11-22 16:20:40 +0900

    test:BridgeGameTest-increase the total game count as the game retry

---
+++ src/test/java/bridge/domain/BridgeGameTest.java
@@ -0,0 +1,19 @@
+package bridge.domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class BridgeGameTest {
+    private final BridgeGame bridgeGame = new BridgeGame(new Bridge());
+
+    @DisplayName("게임을 재시작 하면서 게임 총 시도 횟수 증가 성공")
+    @Test
+    void increaseRetryTotalGame() {
+        int beforeResult = bridgeGame.getTotalGame();
+        bridgeGame.retry();
+        int afterResult = bridgeGame.getTotalGame();
+        assertThat(beforeResult).isNotEqualTo(afterResult);
+    }
+}
