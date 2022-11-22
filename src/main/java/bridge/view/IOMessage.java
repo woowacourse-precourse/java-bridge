9514b0cfaec9bd8e91129bd255803272d6689002
minje <alswp0910@naver.com>
2022-11-22 15:43:47 +0900

    feat: 게임의 최종 결과를 보여주는 기능 추가

--- src/main/java/bridge/view/IOMessage.java
+++ src/main/java/bridge/view/IOMessage.java
@@ -6,7 +6,10 @@ public enum IOMessage {
     INPUT_RESTART_MESSAGE("게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)"),
     START_BRIDGE ("[ "),
     END_BRIDGE (" ]"),
-    DIVIDE_BRIDGE (" | ");
+    DIVIDE_BRIDGE (" | "),
+    FINAL_RESULT_GAME("최종 게임 결과"),
+    GAME_SUCCESS_OR_NOT("게임 성공 여부: "),
+    COUNT_GAME("총 시도한 횟수: ");
     private final String message;
 
     IOMessage(final String message) {
