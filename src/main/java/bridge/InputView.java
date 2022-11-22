12fbee2223c1c3ee012da1cf3d477733ce266d2a
jeongseungchan <jschan0911@naver.com>
2022-11-22 14:39:34 +0900

    Feat: 입력 값을 정수화 하는 기능 추가

--- src/main/java/bridge/InputView.java
+++ src/main/java/bridge/InputView.java
@@ -11,14 +11,15 @@ public class InputView {
      * 다리의 길이를 입력받는다.
      */
     public int readBridgeSize() {
+        System.out.println("다리의 길이를 입력해주세요.");
         while(true) {
-            System.out.println("다리의 길이를 입력해주세요.");
             String inputBridgeLength = Console.readLine();
             // 만약 올바른 값이라면 그 값 리턴
-
+            if(isValidBridgeSize(inputBridgeLength)) {
+                return Integer.parseInt(inputBridgeLength);
+            }
             // 올바르지 않은 값이라면 에러 Exception 발생 후 에러 메시지 출력 후 반복
             throwExceptionIfNotValidBrideSize(inputBridgeLength);
-            return 0;
         }
     }
 
@@ -42,6 +43,18 @@ public class InputView {
             return true;
         }
         return false;
+    }
+
+    /**
+     * 입력 받은 다리 길이가 올바른지 판별한다.
+     */
+    public boolean isValidBridgeSize(String s) {
+        if (!isNumber(s)) return false;
+        if (isNumber(s)) {
+            int num = Integer.parseInt(s);
+            if (!isBetween3To20(num)) return false;
+        }
+        return true;
     }
 
     /**
