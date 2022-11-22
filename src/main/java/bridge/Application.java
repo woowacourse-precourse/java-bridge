b3e9a7aa183051d1736358595fabc734f04a94bd
YSEO <1park4170@gmail.com>
2022-11-22 15:09:06 +0900

    chore<JDK> : JDK버전 변경

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -5,7 +5,6 @@ import bridge.controller.Controller;
 public class Application {
     public static void main(String[] args) {
             Controller controller = new Controller();
-
             controller.game();
     }
 }
