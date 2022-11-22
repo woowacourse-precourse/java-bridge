e2bb00ac36b1fb9d45d125d7c433b26397d671ee
jeongyeonKimm <wjddus9132@gmail.com>
2022-11-22 21:30:51 +0900

    feat: 다리 출력

--- src/main/java/bridge/OutputView.java
+++ src/main/java/bridge/OutputView.java
@@ -1,5 +1,7 @@
 package bridge;
 
+import java.util.List;
+
 /**
  * 사용자에게 게임 진행 상황과 결과를 출력하는 역할을 한다.
  */
@@ -39,7 +41,12 @@ public class OutputView {
      * <p>
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void printMap() {
+    public void printMap(List<List<String>> bridge) {
+        bridge.forEach(this::printBridge);
+    }
+
+    private void printBridge(List<String> row) {
+        System.out.println("[" + String.join("|", row) + "]");
     }
 
     /**
@@ -47,9 +54,9 @@ public class OutputView {
      * <p>
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void printResult(String result, int count) {
+    public void printResult(List<List<String>> bridge, String result, int count) {
         printEndGame();
-        printMap();
+        printMap(bridge);
         printSuccessOrFailure(result);
         printCount(count);
     }
