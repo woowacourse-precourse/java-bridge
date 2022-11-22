0831197df8a44ae477cd768066b502eae66d4a19
eun-byeol <62530884+eun-byeol@users.noreply.github.com>
2022-11-22 15:50:56 +0900

    feat(BridgeGame): 이동할 수 있는 칸인지 검증하고 결과 추가

--- src/main/java/bridge/domain/BridgeGame.java
+++ src/main/java/bridge/domain/BridgeGame.java
@@ -1,16 +1,22 @@
-package bridge;
+package bridge.domain;
 
-/**
- * 다리 건너기 게임을 관리하는 클래스
- */
 public class BridgeGame {
+    private GameResult gameResult;
+    private GameReferee referee;
 
-    /**
-     * 사용자가 칸을 이동할 때 사용하는 메서드
-     * <p>
-     * 이동을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
-     */
-    public void move() {
+    public BridgeGame(Bridge bridge) {
+        gameResult = new GameResult();
+        referee = new GameReferee(bridge);
+    }
+
+    public GameResult getGameResult() {
+        return gameResult;
+    }
+
+    public boolean move(PlayerMoving playerMoving) {
+        boolean isCorrect = referee.isCorrectMoving(playerMoving);
+        gameResult.addGameResult(isCorrect, playerMoving);
+        return isCorrect; //맞추면 true, 틀리면 false
     }
 
     /**
