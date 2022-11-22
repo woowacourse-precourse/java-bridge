88f38ebb4a15b2c91de434717012e6776fbf9dfb
DDing77 <devseo2021@gmail.com>
2022-11-22 20:34:48 +0900

    feat(message) : 게임 진행 결과 메세지 Enum 속성 추가

--- src/main/java/bridge/constant/message/GameMessage.java
+++ src/main/java/bridge/constant/message/GameMessage.java
@@ -8,7 +8,9 @@ public enum GameMessage {
     GAME_INPUT_RESTART("게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)"),
     GAME_RESULT_FIRST("최종 게임 결과"),
     GAME_RESULT_SECOND("게임 성공 여부: "),
-    GAME_RESULT_THIRD("총 시도한 횟수: ");
+    GAME_RESULT_THIRD("총 시도한 횟수: "),
+    GAME_RESULT_SUCCESS("성공"),
+    GAME_RESULT_FAIL("실패");
 
     private final String message;
 
