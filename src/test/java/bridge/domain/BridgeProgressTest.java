44b03c8bae739cf259a7f6e749b21e057082d371
Frog-Slayer <Frog-Slayer@github.com>
2022-11-22 21:28:46 +0900

    test: BridgeProgress test

--- src/test/java/bridge/domain/BridgeProgressTest.java
+++ src/test/java/bridge/domain/BridgeProgressTest.java
@@ -1,28 +1,32 @@
 package bridge.domain;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.Assertions.as;
 import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
 
 class BridgeProgressTest {
-
+    BridgeProgress progress = new BridgeProgress();
     @Test
-    void size() {
+    @BeforeEach
+    public void progressTest(){
+        Integer size = 20;
+        for (int i = 0; i < size; i++) {
+            progress.put("O", " ");
+        }
     }
 
     @Test
-    void put() {
+    public void sizeTest(){
+        assertThat(progress.size()).isEqualTo(20);
     }
 
     @Test
-    void clear() {
+    public void clearTest(){
+        progress.clear();
+        assertThat(progress.size()).isEqualTo(0);
     }
 
-    @Test
-    void getUpperBlocks() {
-    }
-
-    @Test
-    void getLowerBlocks() {
-    }
 }
