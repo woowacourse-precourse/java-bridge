742b5e678a494dc845cb97844a191f7df0be4ea0
SeongbinPark <binni0515@gmail.com>
2022-11-22 20:00:49 +0900

    feat(Validator): change to custom exceptions

--- src/main/java/bridge/validation/Validator.java
+++ src/main/java/bridge/validation/Validator.java
@@ -6,35 +6,41 @@ import static bridge.validation.util.ValidationUtil.isValidGameCommand;
 import static bridge.validation.util.ValidationUtil.isValidMoving;
 import static bridge.validation.util.ValidationUtil.isWithinValidRange;
 
+import bridge.validation.exception.InvalidGameCommandException;
+import bridge.validation.exception.InvalidMovingException;
+import bridge.validation.exception.InvalidRangeException;
+import bridge.validation.exception.NotIntegerException;
+import bridge.validation.exception.NotOneCharacterException;
+
 public class Validator {
 
     public static void validateBridgeSize(String bridgeSize) {
         if (!isInteger(bridgeSize)) {
-            throw new IllegalArgumentException();
+            throw new NotIntegerException();
         }
 
         if (!isWithinValidRange(bridgeSize)) {
-            throw new IllegalArgumentException();
+            throw new InvalidRangeException();
         }
     }
 
     public static void validateMoving(String moving) {
         if (!isOneCharacter(moving)) {
-            throw new IllegalArgumentException();
+            throw new NotOneCharacterException();
         }
 
         if (!isValidMoving(moving)) {
-            throw new IllegalArgumentException();
+            throw new InvalidMovingException();
         }
     }
 
     public static void validateGameCommand(String gameCommand) {
         if (!isOneCharacter(gameCommand)) {
-            throw new IllegalArgumentException();
+            throw new NotOneCharacterException();
         }
 
         if (!isValidGameCommand(gameCommand)) {
-            throw new IllegalArgumentException();
+            throw new InvalidGameCommandException();
         }
     }
 }
