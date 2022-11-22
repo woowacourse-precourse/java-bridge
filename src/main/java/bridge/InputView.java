1292c9a1778a64c7fdb9326040a7b05d481164d8
hyeongu <wancern@naver.com>
2022-11-22 22:31:46 +0900

    feat(InputView): sizeValidate 메소드 구현

--- src/main/java/bridge/InputView.java
+++ src/main/java/bridge/InputView.java
@@ -2,19 +2,23 @@ package bridge;
 
 import camp.nextstep.edu.missionutils.Console;
 
+import java.util.regex.Pattern;
+
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
  */
 public class InputView {
-
+    private final String numberPattern = "^[0-9]+$";
     /**
      * 다리의 길이를 입력받는다.
      */
     public int readBridgeSize() {
         System.out.println("다리의 길이를 입력해주세요.");
-        String input = Console.readLine();
 
-        return Integer.parseInt(input);
+        String input = Console.readLine();
+        int bridgeSize = sizeValidate(input);
+
+        return bridgeSize;
     }
 
     /**
@@ -29,5 +33,16 @@ public class InputView {
      */
     public String readGameCommand() {
         return null;
+    }
+
+    public int sizeValidate(String number){
+        if(!(Pattern.matches(numberPattern, number))){
+            throw new IllegalArgumentException("[ERROR] 다리의 길이가 숫자 형식이 아닙니다.");
+        }
+        int result = Integer.parseInt(number);
+        if(result < 3 || result > 20){
+            throw new IllegalArgumentException("[ERROR] 다리의 길이가 범위를 벗어납니다. (3 ~ 20)");
+        }
+        return result;
     }
 }
