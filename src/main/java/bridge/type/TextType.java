f44ce53c3db42bcb4a2e20f2de3d58700d779514
star-sil <kimse9811@naver.com>
2022-11-22 20:43:06 +0900

    feat(OutPutView): update feature to print bridge map
    다리의 상태를 일정한 형식에 맞춰 출력하는 기능 로직 구현

--- src/main/java/bridge/type/TextType.java
+++ src/main/java/bridge/type/TextType.java
@@ -6,7 +6,12 @@ public enum TextType {
     DOWN("D"),
     RESTART("R"),
     EXIT("Q"),
-    START_MESSAGE("다리 건너기 게임을 시작합니다.");
+    START_MESSAGE("다리 건너기 게임을 시작합니다."),
+    BRIDGE_START("[ "),
+    BRIDGE_END(" ]"),
+    BRIDGE_MID(" | "),
+    POSSIBLE_SPACE("O"),
+    IMPOSSIBLE_SPACE("X");
 
     private final String text;
 
--- src/main/java/bridge/view/OutputView.java
+++ src/main/java/bridge/view/OutputView.java
@@ -1,6 +1,9 @@
 package bridge.view;
 
+import bridge.type.Bridge;
 import bridge.type.TextType;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * 사용자에게 게임 진행 상황과 결과를 출력하는 역할을 한다.
@@ -12,7 +15,16 @@ public class OutputView {
      * <p>
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void printMap() {
+    public void printMap(List<Bridge> bridges) {
+        List<String> upBridgeMap = new ArrayList<>();
+        List<String> downBridgeMap = new ArrayList<>();
+
+        initMap(bridges.get(0), upBridgeMap, downBridgeMap);
+
+        makeBridgeToMap(bridges, upBridgeMap, downBridgeMap);
+
+        System.out.println(String.join("", upBridgeMap));
+        System.out.println(String.join("", downBridgeMap));
     }
 
     /**
@@ -25,5 +37,37 @@ public class OutputView {
 
     public void printStart() {
         System.out.println(TextType.START_MESSAGE.getText());
+    }
+
+    public void initMap(Bridge firstBridge, List<String> upBridgeMap, List<String> downBridgeMap) {
+        upBridgeMap.add(TextType.BRIDGE_START.getText());
+        downBridgeMap.add(TextType.BRIDGE_START.getText());
+
+        makeMap(firstBridge, upBridgeMap, downBridgeMap);
+    }
+
+    public void makeBridgeToMap(List<Bridge> bridges, List<String> upBridgeMap,
+            List<String> downBridgeMap) {
+
+        for (int i = 1; i < bridges.size(); i++) {
+            upBridgeMap.add(TextType.BRIDGE_MID.getText());
+            downBridgeMap.add(TextType.BRIDGE_MID.getText());
+
+            makeMap(bridges.get(i), upBridgeMap, downBridgeMap);
+        }
+
+        upBridgeMap.add(TextType.BRIDGE_END.getText());
+        downBridgeMap.add(TextType.BRIDGE_END.getText());
+    }
+
+    public void makeMap(Bridge bridge, List<String> upBridgeMap, List<String> downBridgeMap) {
+        if (bridge.equals(Bridge.UP_CORRECT) || bridge.equals(Bridge.DOWN_WRONG)) {
+            upBridgeMap.add(TextType.POSSIBLE_SPACE.getText());
+            downBridgeMap.add(TextType.IMPOSSIBLE_SPACE.getText());
+
+        } else if (bridge.equals(Bridge.UP_WRONG) || bridge.equals(Bridge.DOWN_CORRECT)) {
+            upBridgeMap.add(TextType.IMPOSSIBLE_SPACE.getText());
+            downBridgeMap.add(TextType.POSSIBLE_SPACE.getText());
+        }
     }
 }
