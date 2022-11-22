971bb3885f482c97b0d5abccebb12810f37b3050
JJONSOO <pjs970616@naver.com>
2022-11-22 21:48:19 +0900

    feat : 다리 크기 입력

--- src/main/java/bridge/InputView.java
+++ src/main/java/bridge/InputView.java
@@ -28,37 +28,7 @@ public class InputView  {
         }
         return bridgeSizeInt;
     }
-    /**
-     * 사용자가 이동할 칸을 입력받는다.
-     */
-    public static String readMoving() {
-        String moving ="";
-        while(true){
-            try{
-                moving =validate.validateMoving(input());
-                break;
-            }catch (IllegalArgumentException e){
-                ErrorMessage.INVALID_MOVING.getDesc();
-            }
-        }
-        return moving;
-    }
-    /**
-     * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
-     */
-    public static String readGameCommand() {
-        String gameCommand = "";
-        while (true) {
-            try {
-                gameCommand = validate.validateGameCommand(input());
-                System.out.println(gameCommand);
-                break;
-            } catch (IllegalArgumentException e) {
-                ErrorMessage.INVALID_RETRY.getDesc();
-            }
-        }
-        return gameCommand;
-    }
+
     public InputView(){
         this.validate = new Validate();
     }
