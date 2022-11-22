2a589efba3397f5a9464aa88fd214cb89a9070ba
thousandcows <milcamboo@gmail.com>
2022-11-22 10:20:33 +0900

    feat: create main logic

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -1,8 +1,14 @@
 package bridge;
 
+import ui.InputView;
+import ui.OutputView;
+
 public class Application {
 
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        OutputView outputView = new OutputView();
+        InputView inputView = new InputView();
+        BridgeGame bridgeGame = new BridgeGame(outputView, inputView);
+        bridgeGame.run();
     }
 }
