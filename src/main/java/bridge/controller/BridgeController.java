0e5dd4f6b0c3c422e13b0382640e6ef86ac2d0fa
EUNSH1 <tkdgus968@naver.com>
2022-11-22 15:15:55 +0900

    refactor : 사용자 게임 성공, 재시도 메서드 리팩토링

--- src/main/java/bridge/controller/BridgeController.java
+++ src/main/java/bridge/controller/BridgeController.java
@@ -1,8 +1,7 @@
 package bridge.controller;
 
-import bridge.service.BridgeGame;
-import bridge.domain.BridgeMaker;
-import bridge.BridgeRandomNumberGenerator;
+import bridge.domain.BridgeGame;
+import bridge.service.BridgeService;
 import bridge.view.InputView;
 import bridge.view.OutputView;
 
@@ -10,6 +9,10 @@ public class BridgeController {
 
     private static final InputView inputView = new InputView();
     private static final OutputView outputView = new OutputView();
+    private static final BridgeService bridgeService = new BridgeService();
+    private boolean gameRunningCheck = true;
+    private boolean gameEndCheck = false;
+    private BridgeGame bridgeGame;
 
 
     public void start() {
@@ -17,28 +20,20 @@ public class BridgeController {
         GameTest();
     }
 
-    public void GameStartMessage() {
+    public void start() {
         OutputView.printGameStart();
+        bridgeGame = bridgeService.bridgeMaker(inputView);
+        playBridgeGame();
+        outputView.printResult(bridgeGame.currentBridge(), gameEndCheck, bridgeGame.getNumberOfTry());
+
     }
 
-    public void GameTest() {
-        boolean gameRunningCheck = true;
-        boolean gameEndCheck = false;
-        BridgeGame bridgeGame = createBridgeGame(inputView);
+    public void playBridgeGame() {
         while (gameRunningCheck) {
             gameRunningCheck = inputMoving(inputView, outputView, bridgeGame);
-            if (gameRunningCheck == false) {
-                gameRunningCheck = bridgeGame.retry(inputView.readGameCommand());
-            }
-
-            if (bridgeGame.endGame()) {
-                gameRunningCheck = false;
-                gameEndCheck = true;
-            }
+            isGameRunningCheck();
+            isGameEndCheck();
         }
-        outputView.printResult(bridgeGame.currentBridge(),gameEndCheck,bridgeGame.getNumberOfTry());
-    }
-
     }
 
     private static boolean inputMoving(InputView inputView, OutputView outputView, BridgeGame bridgeGame) {
@@ -49,4 +44,22 @@ public class BridgeController {
         outputView.printMap(bridgeGame.currentBridge());
         return false;
     }
+
+
+    public boolean isGameRunningCheck() {
+        if (!gameRunningCheck) {
+            gameRunningCheck = bridgeGame.retry(inputView.readGameCommand());
+        }
+        return gameRunningCheck;
+    }
+
+    public boolean isGameEndCheck() {
+        if (bridgeGame.endGame()) {
+            gameRunningCheck = false;
+            gameEndCheck = true;
+        }
+        return gameRunningCheck;
+    }
+
+
 }
