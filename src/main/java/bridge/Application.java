9e286d210556539c0707bdd26a1dc697584dff30
baksohyeon <bori12370@gmail.com>
2022-11-22 21:36:09 +0900

    feat: 번호 랜덤 생성 에러 핸들링 코드 추가

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -6,7 +6,11 @@ public class Application {
 
     public static void main(String[] args){
         // TODO: 프로그램 구현
+        try {
         BridgeGameController bridgeGameController = new BridgeGameController();
         bridgeGameController.run();
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+        }
     }
 }
