48ce18a75c7df0fe38ad3c562a36a3305e1fef02
junbeom-Son <89973303+junbeom-Son@users.noreply.github.com>
2022-11-22 20:11:34 +0900

    refactor : roll back accessor of BridgeGame#play() from public to private, because play method is not called directly out of classes but in start() after bridgeNumberGenerator is injected

--- src/main/java/bridge/BridgeGame.java
+++ src/main/java/bridge/BridgeGame.java
@@ -43,7 +43,7 @@ public class BridgeGame {
     /**
      * 실제 게임이 진행되는 메서드
      */
-    public boolean play(int bridgeSize, List<String> bridge, List<String> moves) {
+    private boolean play(int bridgeSize, List<String> bridge, List<String> moves) {
         tryCount = tryCount + counting;
         moves.clear();
         for (int i = 0; i < bridgeSize; ++i) {
