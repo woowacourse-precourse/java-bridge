2f694ebe8e57a03368cf0932433074042e95c50f
thousandcows <milcamboo@gmail.com>
2022-11-22 11:21:46 +0900

    feat: create retry method

--- src/main/java/bridge/BridgeGame.java
+++ src/main/java/bridge/BridgeGame.java
@@ -78,7 +78,20 @@ public class BridgeGame {
         return playerInput;
     }
 
+    /**
+     * 사용자가 게임을 다시 시도할 때 사용하는 메서드
+     * <p>
+     * 재시작을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
+     */
     public void retry() {
+        outputView.printRestart();
+        String newCommand = inputView.readGameCommand();
+        if (Objects.equals(newCommand, "Q")) {
+            outputView.printResult("실패", gameTrialCount);
+            return;
+        }
+        gameTrialCount += 1;
+        trial(new ArrayList<>());
     }
 
 
