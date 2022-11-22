16aa4c4db832962fcf5c3dce7f527e4301432aee
kingseungwooya <ksw_0505@nate.com>
2022-11-22 20:08:52 +0900

    feat(view): add final result output

--- src/main/java/bridge/view/OutputView.java
+++ src/main/java/bridge/view/OutputView.java
@@ -8,7 +8,10 @@ import bridge.view.utils.OutputUtils;
  * 사용자에게 게임 진행 상황과 결과를 출력하는 역할을 한다.
  */
 public class OutputView {
-
+    
+    private static final String GAME_RESULT_MESSAGE = "게임 성공 여부: ";
+    private static final String GAME_TRY_MESSAGE = "총 시도한 횟수: ";
+    private static final String FINAL_RESULT_MESSAGE = "최종 게임 결과";
 
     private OutputView() {
     }
@@ -22,6 +25,20 @@ public class OutputView {
      */
     public static void printMap(ScoreMap scoreMap) {
         OutputUtils.outputLine(scoreMap.toString());
+    }
+
+    /**
+     * 게임의 최종 결과를 정해진 형식에 맞춰 출력한다.
+     * <p>
+     * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
+     *
+     * @param gameResult
+     */
+    public static void printResult(GameResult gameResult) {
+        OutputUtils.outputLine(FINAL_RESULT_MESSAGE);
+        OutputUtils.outputLine(gameResult.getScoreMap());
+        OutputUtils.outputLine(GAME_RESULT_MESSAGE + gameResult.getResult());
+        OutputUtils.outputLine(GAME_TRY_MESSAGE + gameResult.getTryCount());
     }
 
 }
