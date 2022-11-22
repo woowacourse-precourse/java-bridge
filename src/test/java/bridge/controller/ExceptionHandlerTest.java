cec582352f0b6a54ad7cc31104473b3e393411b9
mng990 <ryoo0504@pusan.ac.kr>
2022-11-22 23:28:51 +0900

    refactor(/test/java/bridge/util): 유용한 메서드를 라이브러리로 이동

--- src/test/java/bridge/controller/ExceptionHandlerTest.java
+++ src/test/java/bridge/controller/ExceptionHandlerTest.java
@@ -4,36 +4,25 @@ import static bridge.data.Message.ERROR_COMMAND;
 import static bridge.data.Message.ERROR_MOVE;
 import static bridge.data.Message.ERROR_SIZE;
 
+import static bridge.util.lib.setInput;
+import static bridge.util.lib.thrownBy;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 
-
+import bridge.data.Command;
+import bridge.data.Glass;
+import bridge.util.StringFunction;
 import bridge.view.BridgeView;
-import java.io.ByteArrayInputStream;
-import java.io.InputStream;
 
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 class ExceptionHandlerTest{
     ExceptionHandler exc = new ExceptionHandler();
     BridgeView view = new BridgeView();
+
     StringFunction sf;
 
-    interface StringFunction{
-        String valid(String param);
-    }
-
-    public void setInput(String input){
-        InputStream in = new ByteArrayInputStream(input.getBytes());
-        System.setIn(in);
-    } // Scanner에 input을 미리 입력해둔다
-
-    public void thrownBy(String input, StringFunction sf){
-        Assertions.assertThatThrownBy(() -> sf.valid(input))
-                .isInstanceOf(IllegalArgumentException.class);
-    }
 
     public void testReEnter(IllegalArgumentException error, String correctInput){
         setInput(correctInput); // size로 적합한 값을 입력
@@ -42,15 +31,15 @@ class ExceptionHandlerTest{
     }
 
     @Test
-    void 에러를_받으면_에러를_호출한_인자에_적합한_값을_얻기_위해_해당_입력을_다시_받는지() {
+    void 에러를_호출한_인자에_적합한_값을_얻기_위해_해당_입력을_다시_받는지() {
 
         IllegalArgumentException sizeError = new IllegalArgumentException(ERROR_SIZE.OF());
         IllegalArgumentException moveError = new IllegalArgumentException(ERROR_MOVE.OF());
         IllegalArgumentException commandError = new IllegalArgumentException(ERROR_COMMAND.OF());
 
         testReEnter(sizeError, "3");
-        testReEnter(moveError, "U");
-        testReEnter(commandError, "Q");
+        testReEnter(moveError, Glass.UP.MOVE());
+        testReEnter(commandError, Command.QUIT.KEY());
     }
 
     @Test
