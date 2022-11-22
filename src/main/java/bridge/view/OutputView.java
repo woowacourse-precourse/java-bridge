d94bad26de5ad037e456fa89b88b89d298468408
username459712 <94822739+username459712@users.noreply.github.com>
2022-11-22 21:32:41 +0900

    feat: printRestartOrQuit()-> 다시 시도할지 여부 문구 출력

--- src/main/java/bridge/view/OutputView.java
+++ src/main/java/bridge/view/OutputView.java
@@ -12,4 +12,8 @@ public class OutputView {
     public void printMoveUpOrDown() {
         System.out.println("\n이동할 칸을 선택해주세요. (위: U, 아래: D)");
     }
+
+    public void printRestartOrQuit() {
+        System.out.println("\n게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)");
+    }
 }
