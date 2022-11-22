a6d565c9e7bc91b2dc362c972e2a4f0ff3ac709e
gynhyunkim <gyeonghyun.kimm@gmail.com>
2022-11-22 19:18:42 +0900

    feat: 게임 실패 시 재시도 여부 입력

--- src/main/java/bridge/InputView.java
+++ src/main/java/bridge/InputView.java
@@ -9,7 +9,7 @@ public class InputView {
 
     private final String REQUEST_BRIDGE_SIZE = "다리의 길이를 입력해주세요.";
     private final String REQUEST_BRIDGE_MOVING = "이동할 칸을 선택해주세요. (위: U, 아래: D)";
-
+    private final String REQUEST_RETRY = "게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)";
     /**
      * 다리의 길이를 입력받는다.
      */
@@ -30,6 +30,7 @@ public class InputView {
      * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
      */
     public String readGameCommand() {
-        return null;
+        System.out.println(REQUEST_RETRY);
+        return Console.readLine();
     }
 }
