403d8ee691f3c4ba40f26e4cd7b497328943d84a
JJONSOO <pjs970616@naver.com>
2022-11-22 21:46:04 +0900

    feat : 게임 진행 시 필요한 문구 출력

--- src/main/java/bridge/OutputView.java
+++ src/main/java/bridge/OutputView.java
@@ -57,5 +57,16 @@ public class OutputView{
         System.out.println("총 시도한 횟수: "+gameCount);
     }
 
-
+    public static void printGameStart(){
+        System.out.println("다리 건너기 게임을 시작합니다.");
+    }
+    public static void printBridgeSize(){
+        System.out.println("다리의 길이를 입력해주세요.");
+    }
+    public static void printMove(){
+        System.out.println("이동할 칸을 선택해주세요. (위: U, 아래: D)");
+    }
+    public static void printRetry(){
+        System.out.println("게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)");
+    }
 }
