d49db4b8bd7e2c757fe8fdaa212abad6d52ce056
dlgosla <dlgoslaek1@gmail.com>
2022-11-22 13:18:56 +0900

    feat: int로 변경하는 메소드 추가

--- src/main/java/bridge/Util.java
+++ src/main/java/bridge/Util.java
@@ -1,2 +1,13 @@
-package bridge;public class Util {
+package bridge;
+
+public class Util {
+
+    public static int toInt(String number) {
+        try {
+            return Integer.parseInt(number);
+
+        } catch (Exception e) {
+            throw new IllegalArgumentError("숫자를 입력해주세요.");
+        }
+    }
 }
