3471aa89d3b735149e861f731b4c0199be3fb8a4
kissshot1104 <kissshot.heartunderblade1104@gmail.com>
2022-11-22 13:06:36 +0900

    Refactor(OutputGameMessage) : 함수명 대문자로 수정
    함수명을 전부 대문자로 바꾸었습니다.

--- src/main/java/bridge/OutputGameMessage.java
+++ src/main/java/bridge/OutputGameMessage.java
@@ -1,10 +1,10 @@
 package bridge;
 
 enum OutputGameMessage {
-    gameStartMessage("다리 건너기 게임을 시작합니다."),
-    rangeInputMessage("다리의 길이를 입력해주세요."),
-    moveInputMessage("이동할 칸을 선택해주세요. (위: U, 아래: D)"),
-    retryInputMessage("게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)");
+    GAME_START_MESSAGE("다리 건너기 게임을 시작합니다."),
+    RANGE_INPUT_MESSAGE("다리의 길이를 입력해주세요."),
+    MOVE_INPUT_MESSAGE("이동할 칸을 선택해주세요. (위: U, 아래: D)"),
+    RETRY_INPUT_MESSAGE("게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)");
 
     private final String outputMessage;
 
