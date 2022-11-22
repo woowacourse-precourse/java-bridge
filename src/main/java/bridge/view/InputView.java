827ffe6d748e65572cc91590bc2e7b6ac8846418
Ridealist <cjsn92@gmail.com>
2022-11-22 21:04:00 +0900

    style: 한줄 띄기 추가

--- src/main/java/bridge/view/InputView.java
+++ src/main/java/bridge/view/InputView.java
@@ -7,6 +7,7 @@ public class InputView {
     private static final String REQUEST_BRIDGE_SIZE = "다리의 길이를 입력해주세요.";
     private static final String REQUEST_MOVING_DIRECTION = "이동할 칸을 선택해주세요. (위: U, 아래: D)";
     private static final String REQUEST_RESTART_OR_QUIT = "게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)";
+
     /**
      * 다리의 길이를 입력받는다.
      */
