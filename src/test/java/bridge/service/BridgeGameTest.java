46a8cdd85857846af021c2ca818e2b5e2d891a42
Gibeom Nam <rlqja0523@naver.com>
2022-11-22 20:16:27 +0900

    test: R이면 true, Q면 false를 반환하는지 테스트

--- src/test/java/bridge/service/BridgeGameTest.java
+++ src/test/java/bridge/service/BridgeGameTest.java
@@ -2,9 +2,12 @@ package bridge.service;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import java.util.List;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class BridgeGameTest {
@@ -18,5 +21,12 @@ public class BridgeGameTest {
         for (String moving : movings) {
             assertTrue(bridgeGame.move(moving));
         }
+    }
+
+    @ParameterizedTest
+    @DisplayName("R을 입력하면 true, Q를 입력하면 false를 반환한다.")
+    @CsvSource(value = {"R:true", "Q:false"}, delimiter = ':' )
+    void retryTest(String gamCommand, boolean expect) {
+        assertThat(bridgeGame.retry(gamCommand)).isEqualTo(expect);
     }
 }
