d9b8776bf5bf7133dfe564b364f40664a485e0b5
NotableDeveloper <castlereal@naver.com>
2022-11-22 02:47:46 -0800

    <test> : OutputView 테스트 클래스 수정 OutputView 클래스 리팩토링에 따라, 테스트가 통과하도록 수정함.

--- src/test/java/bridge/OutputViewTest.java
+++ src/test/java/bridge/OutputViewTest.java
@@ -7,6 +7,7 @@ import org.junit.jupiter.api.Test;
 import java.io.ByteArrayOutputStream;
 import java.io.OutputStream;
 import java.io.PrintStream;
+import java.util.ArrayList;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -23,45 +24,45 @@ public class OutputViewTest {
         System.setOut(new PrintStream(captor));
     }
 
+    private List<StringBuilder> initializeBridge(){
+        List<StringBuilder> bridgePair = new ArrayList();
+        StringBuilder top = new StringBuilder().append('O').append('O').append(' ');
+        StringBuilder bottom = new StringBuilder().append(' ').append(' ').append('O');
+        bridgePair.add(top);
+        bridgePair.add(bottom);
+
+        return bridgePair;
+    }
     protected final String output() {
         return captor.toString().trim();
     }
-    @DisplayName("크기가 3인 다리를 위, 위, 아래 순서로 이동하는 경우를 출력한다.")
+    @DisplayName("크기가 3인 다리를 위, 위, 아래 순서로 이동한 경우를 출력한다.")
     @Test
     void printBridgeSize3ToSuccessful() {
-        BridgeNumberGenerator generator = new TestNumberGenerator(newArrayList(0, 0, 1));
-        BridgeMaker bridgeMaker = new BridgeMaker(generator);
-        List<String> bridge = bridgeMaker.makeBridge(3);
-        OutputView outputView = new OutputView(bridge);
-        String[] move = {"U", "U", "D"};
-
-        for(int i = 0; i < bridge.size(); i++)
-            outputView.printMap(i, move[i]);
+        OutputView outputView = new OutputView();
+        List<StringBuilder> bridgePair = initializeBridge();
+        outputView.printMap(bridgePair);
 
         assertThat(output()).contains(
-                "[ O ]\n[   ]",
-                "[ O | O ]\n[   |   ]",
-                "[ O | O |   ]\n[   |   | O ]");
+                "[ O | O |   ]",
+                "[   |   | O ]"
+        );
     }
 
-    @DisplayName("크기가 3인 다리를 아래, 아래, 위 순서로 성공적으로 이동한 경우의 게임 결과를 출력한다.")
+    @DisplayName("크기가 3인 다리를 위, 위, 아래 순서로 성공적으로 이동한 경우의 게임 결과를 출력한다.")
     @Test
     void printResultByBridgeSize3() {
-        BridgeNumberGenerator generator = new TestNumberGenerator(newArrayList(1, 1, 0));
-        BridgeMaker bridgeMaker = new BridgeMaker(generator);
-        List<String> bridge = bridgeMaker.makeBridge(3);
-        OutputView outputView = new OutputView(bridge);
-        String[] move = {"D", "D", "U"};
+        OutputView outputView = new OutputView();
+        List<StringBuilder> bridgePair = initializeBridge();
 
-        for(int i = 0; i < bridge.size(); i++)
-            outputView.printMap(i, move[i]);
 
-        outputView.printResult(true, 1);
+        outputView.printMap(bridgePair);
+        outputView.printResult(true, 1, bridgePair);
 
         assertThat(output()).contains(
                 "최종 게임 결과",
-                "[   |   | O ]",
                 "[ O | O |   ]",
+                "[   |   | O ]",
                 "게임 성공 여부: 성공",
                 "총 시도한 횟수: 1"
         );
