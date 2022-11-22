82c2a913ea32ec01a5b01670c2166544b5e43c36
antaeho <an981022@naver.com>
2022-11-22 15:38:40 +0900

    Feat: 예외처리 추가

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -5,8 +5,13 @@ import bridge.controller.GameController;
 public class Application {
 
     public static void main(String[] args) {
-        GameController gameController = new GameController();
-        gameController.gameStart();
+        try {
+            GameController gameController = new GameController();
+            gameController.gameStart();
+        }catch (IllegalArgumentException exception) {
+            System.out.println(exception.getMessage());
+        }
+
     }
 }
 
