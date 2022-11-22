4d6c73ce271342419886bd2f42d6df94912aca98
eom-tae-in <eti0728@daum.net>
2022-11-22 21:35:18 +0900

    feat(BridgeGameException): 입력 값의 범위가 3 ~ 20 사이인지 확인하는 예외처리 메서드 구현

--- src/main/java/bridge/exception/BridgeGameException.java
+++ src/main/java/bridge/exception/BridgeGameException.java
@@ -8,4 +8,11 @@ public class BridgeGameException {
             }
         }
     }
+
+    public void checkNumberRangeException(String number) {
+        String REGEX = "^[3-9]{1}$|^1{1}[0-9]{1}$|^2{1}0{1}$";
+        if (!number.matches(REGEX)) {
+            throw new IllegalArgumentException();
+        }
+    }
 }
