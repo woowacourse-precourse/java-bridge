ad180fa01217cf82998c67714cb8d9819128ca47
yuntaeLee <leeyt1201@gmail.com>
2022-11-22 18:17:43 +0900

    feat: main() 메서드 작성

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -1,8 +1,18 @@
 package bridge;
 
+import bridge.controller.BridgeGameController;
+import bridge.domain.BridgeMaker;
+import bridge.domain.BridgeRandomNumberGenerator;
+import bridge.view.InputView;
+import bridge.view.OutputView;
+
 public class Application {
 
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        InputView inputView = new InputView();
+        OutputView outputView = new OutputView();
+        BridgeMaker bridgeMaker = new BridgeMaker(new BridgeRandomNumberGenerator());
+        BridgeGameController bridgeGameController = new BridgeGameController(inputView, outputView, bridgeMaker);
+        bridgeGameController.play();
     }
 }
