9c467989d40776d23f4595cb3ee7402e49c4e2e1
Dasol Jung <greteljds0251@gmail.com>
2022-11-22 21:45:46 +0900

    feat: 게임 재시작/종료 입력이 올바르지 않은 경우

--- src/main/java/bridge/Error.java
+++ src/main/java/bridge/Error.java
@@ -3,13 +3,21 @@ package bridge;
 public class Error {
     public void errorBridgeSize(int bridgeSize) {
         if (bridgeSize > 20 || bridgeSize < 3) {
-            throw new IllegalAccessError("[ERROR] 다리의 길이가 3~20 사이에 포함되지 않습니다.");
+            throw new IllegalArgumentException("[ERROR] 다리의 길이가 3~20 사이에 포함되지 않습니다.");
         }
     }
 
     public void errorMoving(String moving) {
         if (moving.equals("D") || moving.equals("U")) {
-            throw new IllegalAccessError("[ERROR] 이동할 칸이 올바르지 않습니다.");
+            return;
         }
+        throw new IllegalArgumentException("[ERROR] 이동할 칸이 올바르지 않습니다.");
+    }
+
+    public void errorGameCommand(String gameCommand) {
+        if (gameCommand.equals("R") || gameCommand.equals("Q")) {
+            return;
+        }
+        throw new IllegalArgumentException("[ERROR] 재시작/종료 여부가 올바르지 않습니다.");
     }
 }
