2d52dec993facbb50cf77afed29449101f867772
Myungyulee <102199101+Myungyulee@users.noreply.github.com>
2022-11-22 20:58:40 +0900

    fix: 입력 검증 변경 및 에러메시지 변수설정

--- src/main/java/bridge/InputView.java
+++ src/main/java/bridge/InputView.java
@@ -6,6 +6,16 @@ import camp.nextstep.edu.missionutils.Console;
  * 사용자로부터 입력을 받는 역할을 한다.
  */
 public class InputView {
+    public final String NUMBER_TYPE_ERROR = "숫자를 입력해주세요.";
+    public final String BRIDGE_SIZE_RANGE_ERROR = "다리 길이는 3부터 20 사이의 숫자여야 합니다.";
+    public final String MOVE_INPUT_ERROR = "U 또는 D를 입력해주세요.";
+    public final String RETRY_INPUT_ERROR = "R 또는 Q를 입력해주세요.";
+
+
+    private final static String UP = "U";
+    private final static String DOWN = "D";
+    private final static String RETRY = "R";
+    private final static String EXIT = "Q";
 
     /**
      * 다리의 길이를 입력받는다.
@@ -13,9 +23,16 @@ public class InputView {
     public int readBridgeSize() {
         try {
             int bridgeSize = Integer.parseInt(Console.readLine());
+            validateBridgeSize(bridgeSize);
             return bridgeSize;
         } catch (NumberFormatException numberFormatException) {
-            throw new IllegalArgumentException("숫자를 입력해주세요.");
+            throw new IllegalArgumentException(NUMBER_TYPE_ERROR);
+        }
+    }
+
+    public void validateBridgeSize(int bridgeSize) {
+        if (bridgeSize < 3 || bridgeSize > 20) {
+            throw new IllegalArgumentException(BRIDGE_SIZE_RANGE_ERROR);
         }
     }
 
@@ -23,13 +40,29 @@ public class InputView {
      * 사용자가 이동할 칸을 입력받는다.
      */
     public String readMoving() {
-        return Console.readLine();
+        String selectedSpace = Console.readLine();
+        validateMovingInput(selectedSpace);
+        return selectedSpace;
+    }
+
+    public void validateMovingInput(String selectedSpace) {
+        if (!selectedSpace.equals(UP) && !selectedSpace.equals(DOWN)) {
+            throw new IllegalArgumentException(MOVE_INPUT_ERROR);
+        }
     }
 
     /**
      * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
      */
     public String readGameCommand() {
-        return null;
+        String selectedRetry = Console.readLine();
+        validateGameCommand(selectedRetry);
+        return selectedRetry;
+    }
+
+    public void validateGameCommand(String selectedRetry) {
+        if (!selectedRetry.equals(RETRY) && !selectedRetry.equals(EXIT)) {
+            throw new IllegalArgumentException(RETRY_INPUT_ERROR);
+        }
     }
 }
