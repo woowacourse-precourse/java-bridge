32613874460638108fa5e26258a1ddb2e10a8513
JuSeong1130 <jsmoon1130@naver.com>
2022-11-22 18:49:14 +0900

    test(BridgeGame): move,retry 등 구조 개선에 따른 테스트 구조개선

--- src/test/java/bridge/domain/game/BridgeGameTest.java
+++ src/test/java/bridge/domain/game/BridgeGameTest.java
@@ -1,5 +1,6 @@
 package bridge.domain.game;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Stream;
@@ -12,36 +13,30 @@ import org.junit.jupiter.params.provider.MethodSource;
 
 class BridgeGameTest {
 
-	@DisplayName("다리만들기 테스트")
-	@ParameterizedTest
-	@CsvSource(value = {"3:3","6:6"},delimiterString = ":")
-	void 입력값size에맞는_다리길이가_나와야합니다(int size,int expected) {
-		BridgeGame bridgeGame = new BridgeGame();
-		Assertions.assertThat(bridgeGame.makeBridge(size).size()).isEqualTo(expected);
-	}
-
 	@DisplayName("이동 테스트")
 	@ParameterizedTest
 	@MethodSource("generateData")
-	void 다리이동_테스트(List<String> bridges,String moveCommand,int index,boolean expected){
+	void 다리이동_테스트(List<String> mockBridges, String moveCommand, boolean expected) {
 		BridgeGame bridgeGame = new BridgeGame();
-		Assertions.assertThat(bridgeGame.move(bridges,moveCommand,index)).isEqualTo(expected);
+		bridgeGame.setBridges(mockBridges);
+		bridgeGame.move(moveCommand);
+		Assertions.assertThat(bridgeGame.isSuccessStage()).isEqualTo(expected);
 	}
+
 	static Stream<Arguments> generateData() {
 		return Stream.of(
-				Arguments.of(Arrays.asList("U", "D", "U"), "U", 0,true),
-				Arguments.of(Arrays.asList("U", "D", "U"), "D", 1,true),
-				Arguments.of(Arrays.asList("U", "D", "U"), "D", 0,false),
-				Arguments.of(Arrays.asList("U", "D", "U"), "U", 1,false)
+				Arguments.of(Arrays.asList("U", "D", "U"), "U", true),
+				Arguments.of(Arrays.asList("U", "D", "U"), "D", false)
 		);
 	}
 
 	@DisplayName("재시작 기능 테스트")
 	@ParameterizedTest
-	@CsvSource(value = {"R:0","Q:2147483647"},delimiterString = ":")
-	void 재시작_기능_테스트(String command,int expected){
+	@CsvSource(value = {"R:2:true"}, delimiterString = ":")
+	void 재시작_기능_테스트(String command, int countExcpected, boolean isSuccessStageExpected) {
 		BridgeGame bridgeGame = new BridgeGame();
-		Assertions.assertThat(bridgeGame.retry(command)).isEqualTo(expected);
+		bridgeGame.retry(command);
+		Assertions.assertThat(bridgeGame.getCountOfAttemps()).isEqualTo(countExcpected);
+		Assertions.assertThat(bridgeGame.isSuccessStage()).isEqualTo(isSuccessStageExpected);
 	}
-
 }
