b599b2460bc4e7a7e489c7896c36a190613a20c5
yujaemin <woawoa0513@gmail.com>
2022-11-22 13:14:52 +0900

    feat: 이동 결과 O/X로 변환 함수 구현
    이동한 칸이 이동 이동 가능한 칸일 때 O, 불가능한 칸일 떄 X를 반환한다.

---
+++ src/main/java/bridge/utils/Converter.java
@@ -0,0 +1,11 @@
+package bridge.utils;
+
+public class Converter {
+
+    public static String convertToOX(boolean answer) {
+        if (answer) {
+            return "O";
+        }
+        return "X";
+    }
+}
