c1fdc6bfe487b07dc773b5f3561505c9c76a2e11
jay <jaewhan232@naver.com>
2022-11-22 22:26:37 +0900

    Refactor : 사용하지 않는 에러메세지 삭제

--- src/main/java/bridge/GameCommand.java
+++ src/main/java/bridge/GameCommand.java
@@ -5,8 +5,6 @@ public enum GameCommand {
     RETRY("R"),
     QUIT("Q");
 
-    private static final String errorMessage = "재시도 여부는 R 또는 Q로 입력해주세요.";
-
     private String command;
 
     GameCommand(String command) {
