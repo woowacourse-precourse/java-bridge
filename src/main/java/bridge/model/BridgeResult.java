dba3098c1c98ea8188ab94b0b8766afad6d7e3c8
hyeonseog-won <smreo75@gmail.com>
2022-11-22 20:37:07 +0900

    feat: 사용자가 다리를 건널 수 있는지 여부를 체크하는 기능 추가

--- src/main/java/bridge/model/BridgeResult.java
+++ src/main/java/bridge/model/BridgeResult.java
@@ -7,6 +7,7 @@ public class BridgeResult {
     private final Bridge bridge;
     private final List<ResultFlag> upperLine = new ArrayList<>();
     private final List<ResultFlag> lowerLine = new ArrayList<>();
+    private int crossingCount = 0;
 
     public BridgeResult(Bridge bridge) {
         this.bridge = bridge;
@@ -14,10 +15,20 @@ public class BridgeResult {
 
     private void saveResult(String moveCommand) {
         if (moveCommand.equals("U")) {
-
+            upperLine.add(isCrossing(moveCommand));
+            lowerLine.add(ResultFlag.NOTHING);
         }
         if (moveCommand.equals("D")) {
-
+            upperLine.add(ResultFlag.NOTHING);
+            lowerLine.add(isCrossing(moveCommand));
         }
+    }
+
+    private ResultFlag isCrossing(String moveCommand) {
+        List<String> bridges = bridge.getBridge();
+        if (bridges.get(crossingCount).equals(moveCommand)) {
+            return ResultFlag.SUCCESS;
+        }
+        return ResultFlag.FAIL;
     }
 }
