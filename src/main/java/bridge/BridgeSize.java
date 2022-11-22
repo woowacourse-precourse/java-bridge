079f61ef3e41af78f9ae2a85c1c9405360ab8601
doxxx <doxxx93@gmail.com>
2022-11-22 17:47:40 +0900

    refactor(BridgeSize): divide validate()

--- src/main/java/bridge/BridgeSize.java
+++ src/main/java/bridge/BridgeSize.java
@@ -3,25 +3,29 @@ package bridge;
 import java.util.Objects;
 
 public class BridgeSize {
+    public static final int MIN_LENGTH = 3;
+    public static final int MAX_LENGTH = 20;
     private final int size;
 
     public BridgeSize(String size) {
-        validate(size);
-        this.size = Integer.parseInt(size);
+        this.size = validate(size);
+        validate(this.size);
     }
 
-    private void validate(final String size) {
+    private void validate(final int size) {
+        if (size < MIN_LENGTH || size > MAX_LENGTH) {
+            throw new IllegalArgumentException("[ERROR] 다리의 길이는 3 이상 20 이하만 입력 가능합니다.");
+        }
+    }
+
+    private int validate(final String size) {
         if (size == null || size.isEmpty()) {
             throw new IllegalArgumentException("[ERROR] 다리의 길이는 숫자만 입력 가능합니다.");
         }
-        int brideSize;
         try {
-            brideSize = Integer.parseInt(size);
+            return Integer.parseInt(size);
         } catch (NumberFormatException e) {
             throw new IllegalArgumentException("[ERROR] 다리의 길이는 숫자만 입력 가능합니다.");
-        }
-        if (brideSize < 3 || brideSize > 20) {
-            throw new IllegalArgumentException("[ERROR] 다리의 길이는 3 이상 20 이하만 입력 가능합니다.");
         }
     }
 
