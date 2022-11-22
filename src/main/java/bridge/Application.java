560142b6aedc25002d4694b16a70d3d85b04ee3a
renechoi90@gmail.com <renechoi90@gmail.com>
2022-11-22 12:27:58 +0900

    chore : 제출 오류(예기치 못한 오류 실패) 확인을 위한 수정

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -8,13 +8,13 @@ public class Application {
 	private static final String EXIT_MESSAGE = "어플리케이션을 종료합니다.";
 
 	public static void main(String[] args) {
-		try {
+//		try {
 			Controller controller = new Controller();
 			controller.run();
-		} catch (IllegalArgumentException ignored) {
-		} finally {
-			OutputView.withContentOf(EXIT_MESSAGE, true, false).ConsoleMessage();
-		}
+//		} catch (IllegalArgumentException ignored) {
+//		} finally {
+//			OutputView.withContentOf(EXIT_MESSAGE, true, false).ConsoleMessage();
+//		}
 	}
 }
 
