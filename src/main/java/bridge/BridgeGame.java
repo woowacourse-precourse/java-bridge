d624bcd5d16917807b62293d71861178b14793d6
tjwjdgus12 <01048733438a@gmail.com>
2022-11-22 18:31:30 +0900

    feat: 브릿지 게임 로직 구현

--- src/main/java/bridge/BridgeGame.java
+++ src/main/java/bridge/BridgeGame.java
@@ -1,16 +1,29 @@
 package bridge;
 
+import java.util.List;
+
 /**
  * 다리 건너기 게임을 관리하는 클래스
  */
 public class BridgeGame {
 
+    private final List<String> bridge;
+    private int turn = 0;
+    private boolean dead = false;
+
+    public BridgeGame (int bridgeSize) {
+        BridgeMaker bridgeMaker = new BridgeMaker(new BridgeRandomNumberGenerator());
+        bridge = bridgeMaker.makeBridge(bridgeSize);
+    }
+
     /**
      * 사용자가 칸을 이동할 때 사용하는 메서드
-     * <p>
-     * 이동을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void move() {
+    public void move(String direction) {
+        turn++;
+        if (!direction.equals(bridge.get(turn))) {
+            dead = true;
+        }
     }
 
     /**
@@ -19,5 +32,19 @@ public class BridgeGame {
      * 재시작을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
     public void retry() {
+        turn = 0;
+        dead = false;
+    }
+
+    public List<String> getBridge() {
+        return bridge;
+    }
+
+    public int getTurn() {
+        return turn;
+    }
+
+    public boolean isDead() {
+        return dead;
     }
 }
