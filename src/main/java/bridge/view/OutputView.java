39e2ef5f54d4f45ed5f101d7e1d40b02fd0586ca
0chil <0@chll.it>
2022-11-22 10:06:15 +0900

    refactor(OutputView): 열 단위로 출력, 메서드 분리

--- src/main/java/bridge/view/OutputView.java
+++ src/main/java/bridge/view/OutputView.java
@@ -1,5 +1,8 @@
 package bridge.view;
 
+import static bridge.constant.Direction.LOWER;
+import static bridge.constant.Direction.UPPER;
+
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -7,23 +10,20 @@ import bridge.constant.Direction;
 import bridge.dto.TrialResult;
 
 public class OutputView {
-
     public void printMap(List<TrialResult> trialResults) {
-        printAsDecks(Direction.UPPER, trialResults);
-        printAsDecks(Direction.LOWER, trialResults);
+        printRow(getRow(UPPER, trialResults));
+        printRow(getRow(LOWER, trialResults));
+        System.out.println();
     }
 
-    private void printAsDecks(Direction deckDirection, List<TrialResult> trialResults) {
-        String deck = trialResults.stream()
-                .map(trialResult -> getDeck(deckDirection, trialResult))
-                .collect(Collectors.joining(" | "));
-        System.out.print("[ ");
-        System.out.print(deck);
-        System.out.println(" ]");
+    private List<String> getRow(Direction row, List<TrialResult> trialResults) {
+        return trialResults.stream()
+                .map(trialResult -> getDeck(row, trialResult))
+                .collect(Collectors.toList());
     }
 
-    private String getDeck(Direction deckDirection, TrialResult trialResult) {
-        if (deckDirection != trialResult.getDirection()) {
+    private String getDeck(Direction row, TrialResult trialResult) {
+        if (row != trialResult.getDirection()) {
             return " ";
         }
         if (trialResult.wasSuccessful()) {
@@ -32,17 +32,31 @@ public class OutputView {
         return "X";
     }
 
+    private void printRow(List<String> row) {
+        System.out.print("[ ");
+        System.out.print(String.join(" | ", row));
+        System.out.println(" ]");
+    }
+
     public void printResult(List<TrialResult> trialResults, int trialCount, boolean finished) {
         System.out.println("최종 게임 결과");
         printMap(trialResults);
+        printFinished(finished);
+        printTrialCount(trialCount);
         System.out.println();
+    }
+
+    private void printFinished(boolean finished) {
         if (finished) {
             System.out.println("게임 성공 여부: 성공");
         }
         if (!finished) {
             System.out.println("게임 성공 여부: 실패");
         }
-        System.out.printf("총 시도한 횟수: %d" + System.lineSeparator(), trialCount);
+    }
+
+    private void printTrialCount(int trialCount) {
+        System.out.printf("총 시도한 횟수: %d", trialCount);
     }
 
     public void printException(String message) {
