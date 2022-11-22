98d4e9ad6f6863129a1628c5ae8d864cd7125a06
sangeun-b <baek0007@algonquinlive.com>
2022-11-22 10:39:43 +0900

    refactor: 불필요한 상수 삭제

--- src/main/java/bridge/exception/BridgeException.java
+++ src/main/java/bridge/exception/BridgeException.java
@@ -8,7 +8,6 @@ public class BridgeException {
     public static final String SIZE_LANGE_ERROR = "[ERROR] 다리 길이는 3부터 20 사이의 숫자여야 합니다.";
     public static final String MOVING_ERROR = "[ERROR] 이동할 칸은 대문자 U와 대문자 D만 입력 가능합니다.";
     public static final String RESTART_ERROR = "[ERROR] R(재시작)과 Q(종료) 중 하나의 문자만 입력가능 합니다.";
-    public static final String BLANK_ERROR = "[ERROR] 값을 입력해야합니다.";
 
     public void validateBridgeSizeFormat(String size) {
         if(!size.matches(REGEX_BRIDGE)) {
