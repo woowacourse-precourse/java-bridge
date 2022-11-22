e06684b1f2bf09a9bc63cbcf1563a7d73d508dbd
2do1 <dowon3036@naver.com>
2022-11-22 20:48:12 +0900

    feat: 게임 실행 로직 구현

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -1,8 +1,15 @@
 package bridge;
 
-public class Application {
+import java.util.List;
 
+public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        try {
+            BridgeGameController bridgeGameController = new BridgeGameController();
+            bridgeGameController.startGame();
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
     }
 }
