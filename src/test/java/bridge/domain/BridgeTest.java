d3c6b9e9f1c251882463cddfb10e85a5db1b81be
nuyh <hjaehyun25@gmail.com>
2022-11-22 14:02:47 +0900

    test: 정답 판단하기 및 다리 생성하기 테스트 경계값 추가

--- src/test/java/bridge/domain/BridgeTest.java
+++ src/test/java/bridge/domain/BridgeTest.java
@@ -2,6 +2,7 @@ package bridge.domain;
 
 import bridge.BridgeMaker;
 import bridge.BridgeRandomNumberGenerator;
+import bridge.service.BridgeGame;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
@@ -28,19 +29,53 @@ class BridgeTest {
     }
 
     @ParameterizedTest
-    @ValueSource(ints = {0, 1, 2, 3, 4, 5, 10})
-    void 정답_판단하기(int stage) throws Exception{
+    @ValueSource(ints = {3, 10, 20})
+    void 정답과_유저의_마지막_답이_다를_때(int size) throws Exception{
         //given
-        List<String> bridge = bridgeMaker.makeBridge(stage+1);
+        BridgeGame bridgeGame = new BridgeGame(new BridgeRandomNumberGenerator());
+        bridgeGame.initBridge(size);
+        bridgeGame.move("Incorrect Answer");
+
+        //when
+        GameStatus currentStatus = bridgeGame.getCurrentStatus();
+
+        //then
+        assertThat(currentStatus).isEqualTo(GameStatus.OVER);
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {0, 1, 2, 3, 4, 5, 10})
+    void 정답_판단하기(int size) throws Exception{
+        //given
+        List<String> bridge = bridgeMaker.makeBridge(size);
         Bridge answer = new Bridge(bridge);
         Bridge user = new Bridge(new ArrayList<>());
 
         //when
-        for(int i=0; i<=stage; i++)
+        for(int i=0; i<size; i++)
             user.moveNext(answer.getBridge().get(i));
 
         //then
         assertThat(user.matchAnswer(answer)).allMatch(o -> o.equals("O"));
-        assertThat(user.matchAnswer(answer).size()).isEqualTo(stage+1);
+        assertThat(user.matchAnswer(answer).size()).isEqualTo(size);
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {3, 4, 5, 10, 20})
+    void 정답_판단하기_마지막_하나만_틀렸을_때(int size) throws Exception{
+        //given
+        List<String> bridge = bridgeMaker.makeBridge(size);
+        Bridge answer = new Bridge(bridge);
+        Bridge user = new Bridge(new ArrayList<>());
+
+        //when
+        for(int i=0; i<size-1; i++)
+            user.moveNext(answer.getBridge().get(i));
+        user.moveNext("Incorrect Answer");
+
+        //then
+        assertThat(user.matchAnswer(answer).subList(0, size-1)).allMatch(o->o.equals("O"));
+        assertThat(user.matchAnswer(answer).get(size-1)).isEqualTo("X");
+        assertThat(user.matchAnswer(answer).size()).isEqualTo(size);
     }
 }
