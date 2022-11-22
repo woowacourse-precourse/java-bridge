bbcc3667e788e687cbe00eaf0e1089b7476f17b3
Beginner <dkstmdwn17@naver.com>
2022-11-22 15:03:13 +0900

    refactor : 출력하는 부분을 static final으로 리팩토링
    출력하는 부분을 static final로
    리팩토링 하였다.

--- src/main/java/bridge/view/InputView.java
+++ src/main/java/bridge/view/InputView.java
@@ -6,24 +6,29 @@ import camp.nextstep.edu.missionutils.Console;
 
 public class InputView {
 
+    private static final String START_MESSAGE = "다리 건너기 게임을 시작합니다.";
+    private static final String ENTER_THE_BRIDGE_SIZE_MESSAGE = "다리의 길이를 입력해주세요.";
+    private static final String MOVE_MESSAGE = "이동할 칸을 선택해주세요. (위: U, 아래: D)";
+    private static final String RETRY_OR_QUIT_MESSAGE = "게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)";
+
     public int readBridgeSize() {
-        System.out.println("다리 건너기 게임을 시작합니다.");
+        System.out.println(START_MESSAGE);
         System.out.println();
-        System.out.println("다리의 길이를 입력해주세요.");
+        System.out.println(ENTER_THE_BRIDGE_SIZE_MESSAGE);
         String userInput = Console.readLine().trim();
         Validation.verifyUserInputBridgeLength(userInput);
         return Integer.parseInt(userInput);
     }
 
     public String readMoving() {
-        System.out.println("이동할 칸을 선택해주세요. (위: U, 아래: D)");
+        System.out.println(MOVE_MESSAGE);
         String move = Console.readLine().trim();
         Validation.verifyUserInputIsCharacter(move, Mode.UpOrDown);
         return move;
     }
 
     public String readGameCommand() {
-        System.out.println("게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)");
+        System.out.println(RETRY_OR_QUIT_MESSAGE);
         String retryOrQuit = Console.readLine().trim();
         Validation.verifyUserInputIsCharacter(retryOrQuit, Mode.RetryOrQuit);
         return retryOrQuit;
