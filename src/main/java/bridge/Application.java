80b280bbd85fb5865a04c15f7cd8050434d3b20d
yejin <kwonyj1022@naver.com>
2022-11-22 16:33:26 +0900

    fix: 게임 시작 출력문 추가

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -1,8 +1,11 @@
 package bridge;
 
+import static bridge.constant.PrintMessage.START_GAME;
+
 public class Application {
 
     public static void main(String[] args) {
+        System.out.println(START_GAME);
         new Run();
     }
 }
