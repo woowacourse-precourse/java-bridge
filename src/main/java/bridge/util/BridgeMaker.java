2305ffcc14ebc7aa184dc0a73bfd9a2774b03268
username459712 <94822739+username459712@users.noreply.github.com>
2022-11-22 21:24:10 +0900

    feat: static value and constructor

---
+++ src/main/java/bridge/util/BridgeMaker.java
@@ -0,0 +1,13 @@
+package bridge.util;
+
+import bridge.BridgeNumberGenerator;
+import java.util.ArrayList;
+import java.util.List;
+
+public class BridgeMaker {
+    private final BridgeNumberGenerator bridgeNumberGenerator;
+
+    public BridgeMaker(BridgeNumberGenerator bridgeNumberGenerator) {
+        this.bridgeNumberGenerator = bridgeNumberGenerator;
+    }
+}
