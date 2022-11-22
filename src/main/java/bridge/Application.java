0a1a5dae3bd7012411b73441220af3b49afd53fc
junyong <jymoon1121@gmail.com>
2022-11-22 20:54:42 +0900

    fix(Application): main 메서드에 try/catch문 추가

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -11,9 +11,14 @@ public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
         System.out.println("다리 건너기 게임을 시작합니다.");
-        init();
-        gameStart();
-        printGameResult();
+        try {
+            init();
+            gameStart();
+            printGameResult();
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+        }
+
     }
     private static void init() {
         String input = inputView.readBridgeSize();
