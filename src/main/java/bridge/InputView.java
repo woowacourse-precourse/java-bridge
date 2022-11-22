ccbb8a1b5f6175542b0f3116e1e6cfdf7eca36e1
hong <hong@hongui-MacBookAir.local>
2022-11-22 15:34:08 +0900

    feat(add): 입력시 실명 텍스트 추가

--- src/main/java/bridge/InputView.java
+++ src/main/java/bridge/InputView.java
@@ -11,6 +11,7 @@ public class InputView {
      * 다리의 길이를 입력받는다.
      */
     public int readBridgeSize() {
+        System.out.println("다리의 길이를 입력해주세요.");
         return Integer.parseInt(readLine());
     }
 
@@ -18,6 +19,7 @@ public class InputView {
      * 사용자가 이동할 칸을 입력받는다.
      */
     public String readMoving() {
+        System.out.println("이동할 칸을 선택해주세요. (위: U, 아래: D)");
         return readLine();
     }
 
@@ -25,6 +27,7 @@ public class InputView {
      * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
      */
     public String readGameCommand() {
+        System.out.println("게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)");
         return readLine();
     }
 }
