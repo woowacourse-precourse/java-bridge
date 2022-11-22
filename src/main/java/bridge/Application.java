ec8252b533f0928f94b9828de075a33526119e8e
jaehyeon <jhjh0022@naver.com>
2022-11-22 20:02:59 +0900

    fix(Application): GameController 이름 수정으로 인한 수정

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -1,6 +1,6 @@
 package bridge;
 
-import bridge.controller.GameControll;
+import bridge.controller.GameController;
 import bridge.view.TypeChange;
 import bridge.view.Valification;
 import bridge.view.InputView;
@@ -9,7 +9,7 @@ import bridge.view.OutputView;
 public class Application {
 
     public static void main(String[] args) {
-        GameControll gameControll = new GameControll(new InputView(new Valification(), new TypeChange()), new OutputView());
-        gameControll.play();
+        GameController gameController = new GameController(new InputView(new Valification(), new TypeChange()), new OutputView());
+        gameController.play();
     }
 }
