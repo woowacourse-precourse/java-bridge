8d7f3be09dad8c400f9e569b857c35a2af037d01
jeongyeonKimm <wjddus9132@gmail.com>
2022-11-22 12:20:49 +0900

    feat: 게임 종료 메시지 출력

--- src/main/java/bridge/OutputView.java
+++ src/main/java/bridge/OutputView.java
@@ -7,6 +7,9 @@ public class OutputView {
 
     private static final String START_MESSAGE = "다리 건너기 게임을 시작합니다.";
     private static final String RESTART_MESSAGE = "게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)";
+    private static final String END_MESSAGE = "최종 게임 결과";
+    private static final String SUCCESS_OR_FAILURE = "게임 성공 여부: %s";
+    private static final String COUNT_MESSAGE = "총 시도한 횟수: %d";
 
     public void printStart() {
         System.out.println(START_MESSAGE);
@@ -15,6 +18,21 @@ public class OutputView {
     public void printRestart() {
         System.out.println(RESTART_MESSAGE);
     }
+
+    public void printEndGame() {
+        System.out.println(END_MESSAGE);
+    }
+
+    public void printSuccessOrFailure(String result) {
+        System.out.printf(SUCCESS_OR_FAILURE, result);
+        System.out.println();
+    }
+
+    public void printCount(int count) {
+        System.out.printf(COUNT_MESSAGE, count);
+        System.out.println();
+    }
+
 
     /**
      * 현재까지 이동한 다리의 상태를 정해진 형식에 맞춰 출력한다.
@@ -29,6 +47,10 @@ public class OutputView {
      * <p>
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void printResult() {
+    public void printResult(String result, int count) {
+        printEndGame();
+        printMap();
+        printSuccessOrFailure(result);
+        printCount(count);
     }
 }
