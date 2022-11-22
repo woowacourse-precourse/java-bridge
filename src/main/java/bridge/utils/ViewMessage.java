b1b61ffa69f726ce76db3071c107bf2ca1c374d9
eun-byeol <62530884+eun-byeol@users.noreply.github.com>
2022-11-22 22:19:25 +0900

    fix(ViewMessage): 게임 시작 메시지 개행 문자 수정

--- src/main/java/bridge/utils/ViewMessage.java
+++ src/main/java/bridge/utils/ViewMessage.java
@@ -1,7 +1,7 @@
 package bridge.utils;
 
 public class ViewMessage {
-    public static final String GAME_START = "다리 건너기 게임을 시작합니다.%n";
+    public static final String GAME_START = "다리 건너기 게임을 시작합니다.\n";
 
     public static final String INPUT_BRIDGE_SIZE = "다리의 길이를 입력해주세요.";
     public static final String INPUT_MOVING = "\n이동할 칸을 선택해주세요. (위: U, 아래: D)";
