defb426e26381ba21c4db3fe343ba6e53fedd44c
TaeHwan <kimta2hwan@gmail.com>
2022-11-22 18:16:25 +0900

    feat: 라운드의 다리 건너기 결과를 출력하는 기능 구현

--- src/main/java/bridge/view/OutputView.java
+++ src/main/java/bridge/view/OutputView.java
@@ -1,5 +1,9 @@
 package bridge.view;
 
+import bridge.domain.RoundResult;
+import bridge.service.BridgeMapMaker;
+import bridge.domain.Constants;
+
 /**
  * 사용자에게 게임 진행 상황과 결과를 출력하는 역할을 한다.
  */
@@ -10,7 +14,32 @@ public class OutputView {
      * <p>
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void printMap() {
+    public static void printMap(BridgeMapMaker bridgeMapMaker, RoundResult roundResult) {
+        if (roundResult.equals(RoundResult.SELECTED_UPPER_CASE_IF_CORRECT)) {
+            printPartOfTheMap(bridgeMapMaker.getUpperBridgeMap(), Constants.SUCCESSFUL_CASE_MAP);
+            printPartOfTheMap(bridgeMapMaker.getLowerBridgeMap(), Constants.NON_ANSWER_CASE_MAP);
+        }
+
+        if (roundResult.equals(RoundResult.SELECTED_UPPER_CASE_IF_WRONG)) {
+            printPartOfTheMap(bridgeMapMaker.getUpperBridgeMap(), Constants.FAILED_CASE_MAP);
+            printPartOfTheMap(bridgeMapMaker.getLowerBridgeMap(), Constants.NON_ANSWER_CASE_MAP);
+        }
+
+        if (roundResult.equals(RoundResult.SELECTED_LOWER_CASE_IF_CORRECT)) {
+            printPartOfTheMap(bridgeMapMaker.getUpperBridgeMap(), Constants.NON_ANSWER_CASE_MAP);
+            printPartOfTheMap(bridgeMapMaker.getLowerBridgeMap(), Constants.SUCCESSFUL_CASE_MAP);
+        }
+
+        if (roundResult.equals(RoundResult.SELECTED_LOWER_CASE_IF_WRONG)) {
+            printPartOfTheMap(bridgeMapMaker.getUpperBridgeMap(), Constants.NON_ANSWER_CASE_MAP);
+            printPartOfTheMap(bridgeMapMaker.getLowerBridgeMap(), Constants.FAILED_CASE_MAP);
+        }
+    }
+
+    private static void printPartOfTheMap(StringBuilder priorMap, String roundResult) {
+        System.out.print(priorMap);
+        System.out.print(roundResult);
+        System.out.println(Constants.END_MAP);
     }
 
     /**
@@ -18,6 +47,6 @@ public class OutputView {
      * <p>
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void printResult() {
+    public static void printResult() {
     }
 }
