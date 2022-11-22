5520a8322bf0874f1557841abcd1b7b521d2702a
skydreamer21 <skydreamer210@gmail.com>
2022-11-22 19:03:54 +0900

    feat(User): User 클래스 생성

---
+++ src/main/java/bridge/domain/User.java
@@ -0,0 +1,45 @@
+package bridge.domain;
+
+import bridge.service.constant.GameStatus;
+
+public class User {
+    private int attemptCount;
+    private int round;
+    private GameStatus status;
+
+    public User() {
+        attemptCount = 1;
+        round = 1;
+        status = GameStatus.PLAYING;
+    }
+
+    public void playNextRound() {
+        round++;
+    }
+
+    public void playSameRoundAgain() {
+        round--;
+        attemptCount++;
+        status = GameStatus.PLAYING;
+    }
+
+    public void failRound() {
+        status = GameStatus.FAIL;
+    }
+
+    public void clearGame() {
+        status = GameStatus.SUCCESS;
+    }
+
+    public int getAttemptCount() {
+        return attemptCount;
+    }
+
+    public int getRound() {
+        return round;
+    }
+
+    public GameStatus getStatus() {
+        return status;
+    }
+}
