616121641a462e7dbc211298b94007b29d43a1c1
daeun <gain3436@gmail.com>
2022-11-22 15:52:07 +0900

    test : BridgeShapeTest 작성

--- src/test/java/bridge/domain/BridgeShapeTest.java
+++ src/test/java/bridge/domain/BridgeShapeTest.java
@@ -1,4 +1,25 @@
 package bridge.domain;
 
+import bridge.view.OutputView;
+import org.junit.jupiter.api.*;
+import org.mockito.Mock;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
+
+
 public class BridgeShapeTest {
+
+    BridgeShape bridgeShape;
+    MyBridge myBridge;
+    @DisplayName("input list에 따라 다리 모양을 생성한다.")
+    @Test
+    void makeBridgeShape() throws Exception{
+        myBridge = new MyBridge(List.of("U", "U", "D", "D"), List.of("U"));
+        bridgeShape.makeLines(myBridge);
+    }
 }
