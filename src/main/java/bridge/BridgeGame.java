e91585df0a2d0706774af4acb654837842482993
thousandcows <milcamboo@gmail.com>
2022-11-22 10:27:00 +0900

    feat: create class field and constructor of BridgeGame class

--- src/main/java/bridge/BridgeGame.java
+++ src/main/java/bridge/BridgeGame.java
@@ -1,28 +1,30 @@
 package bridge;
 
+import ui.InputView;
+import ui.OutputView;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
 /**
  * 다리 건너기 게임을 관리하는 클래스
  */
 public class BridgeGame {
-    /**
-     * 다리 건너기 게임을 실행하는 메서드
-     */
-    public void run() {
 
+    private final OutputView outputView;
+    private final InputView inputView;
+    private List<String> newBridge;
+    private int gameTrialCount = 1;
+
+    public BridgeGame(OutputView outputView, InputView inputView) {
+        this.outputView = outputView;
+        this.inputView = inputView;
     }
-    /**
-     * 사용자가 칸을 이동할 때 사용하는 메서드
-     * <p>
-     * 이동을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
-     */
+
     public void move() {
     }
 
-    /**
-     * 사용자가 게임을 다시 시도할 때 사용하는 메서드
-     * <p>
-     * 재시작을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
-     */
     public void retry() {
     }
 }
