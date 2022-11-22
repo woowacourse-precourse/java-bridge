0f89b16fb5c0cea31bf818016b2bea14f2100023
Seoyoung <ryupark1006@gmail.com>
2022-11-22 20:21:26 +0900

    feat: 게임 실행 로직 구현

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -3,6 +3,11 @@ package bridge;
 public class Application {
 
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        try {
+            BridgeGameController bridgeGameController = new BridgeGameController();
+            bridgeGameController.startBridgeGame();
+        } catch (Exception e) {
+            System.out.println(e);
+        }
     }
 }
