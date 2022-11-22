aa6f78b77c81945848e92c31135dedb57699ab8f
nahyeon <whskgus12@nate.com>
2022-11-22 14:24:19 +0900

    test(bridgeGame): 이동방향이 D일때, 이동 가능하면 O 불가능하면 X를 입력
    이동방향이 D일때, 이동이 가능하면 O가 입력되고 불가능하면 X가 입력되는지 테스트합니다.
    테스트 대상은 BridgeGameResult의 addResultDown입니다.

--- src/test/java/bridge/bridgeGame/BridgeGameResultTest.java
+++ src/test/java/bridge/bridgeGame/BridgeGameResultTest.java
@@ -11,7 +11,7 @@ import static org.junit.jupiter.api.Assertions.*;
 class BridgeGameResultTest {
 
     BridgeGameResult bridgeGameResult = new BridgeGameResult();
-    
+
     @Test
     @DisplayName("사용자가 이동하려는 방향이 U일때, 이동이 가능하면 O를 불가능하면 X를 입력한다.")
     void addResultUp() {
@@ -27,7 +27,17 @@ class BridgeGameResultTest {
     }
 
     @Test
+    @DisplayName("사용자가 이동하려는 방향이 D일때, 이동이 가능하면 O를 불가능하면 X를 입력한다.")
     void addResultDown() {
+        //given
+        List<Boolean> enableMove = Arrays.asList(false, true);
+        List<String> expectResult = Arrays.asList("X", "O"); //false - X , true - O
+
+        //when
+        enableMove.forEach(enable -> bridgeGameResult.addResultDown(enable));
+
+        //then
+        assertEquals(expectResult, bridgeGameResult.getResultDown());
     }
 
     @Test
