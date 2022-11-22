23129bb1c21424ce2215da218e1d445e83b2ae51
yongseungLeeDec <yonson.dev@gmail.com>
2022-11-22 15:06:27 +0900

    feat: 어플리케이션 구동 코드 추가

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -3,6 +3,10 @@ package bridge;
 public class Application {
 
     public static void main(String[] args) {
-        BridgeGame.getBridgeGame().runGame();
+        try {
+            BridgeGame.getBridgeGame().runGame();
+        } catch (IllegalArgumentException exception) {
+            System.out.println(exception.getMessage());
+        }
     }
 }
