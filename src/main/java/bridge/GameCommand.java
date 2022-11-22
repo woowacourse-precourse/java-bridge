e6a8ac7c8db5112f2234467f9497bb5acdf2b5c9
jay <jaewhan232@naver.com>
2022-11-22 14:55:09 +0900

    Implement : Command Enum에 해당하는 명령이 존재하는지 확인하는 hasCommandOf 매소드 구현

--- src/main/java/bridge/GameCommand.java
+++ src/main/java/bridge/GameCommand.java
@@ -12,4 +12,11 @@ public enum GameCommand {
     GameCommand(String command) {
         this.command = command;
     }
+
+    public static boolean hasCommandOf(String inputCommand) {
+        for (GameCommand command : GameCommand.values()) {
+            if (command.equals(inputCommand)) return true;
+        }
+        return false;
+    }
 }
--- src/main/java/bridge/MovingCommand.java
+++ src/main/java/bridge/MovingCommand.java
@@ -5,7 +5,6 @@ public enum MovingCommand {
     UP("U", 1),
     DOWN("D", 0);
 
-    private static final String INVALID_USER_INPUT = "이동할 칸은 U 또는 D로 이동할 수 있습니다.";
     private static final String INVALID_NUMBER = "해당하는 명령을 찾을 수 없습니다.";
 
     private String command;
@@ -21,5 +20,12 @@ public enum MovingCommand {
             if (cmd.number == number) return cmd.command;
         }
         throw new IllegalArgumentException(INVALID_NUMBER);
+    }
+
+    public static boolean hasCommandOf(String inputCommand) {
+        for (MovingCommand cmd : MovingCommand.values()) {
+            if (cmd.command.equals(inputCommand)) return true;
+        }
+        return false;
     }
 }
