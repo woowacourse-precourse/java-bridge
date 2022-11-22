35ab7c57fd4d0f35829820fd481765c294e2697b
김선동 <mok03127@gmail.com>
2022-11-22 17:51:47 +0900

    test: 다리 건너기 결과 확인 테스트 추가

--- src/test/java/bridge/BridgeGameTest.java
+++ src/test/java/bridge/BridgeGameTest.java
@@ -38,4 +38,34 @@ public class BridgeGameTest {
         assertThat(Arrays.asList(firstStep, secondStep, thirdStep))
                 .isEqualTo(Arrays.asList(false, false, false));
     }
+
+    @Test
+    @DisplayName("다리를 처음부터 끝까지 정확히 건넌 경우 결과 성공 검증")
+    void isSuccessIfCrossedCorrectlyFromBeginningToEnd() {
+        // Given
+        BridgeGame bridgeGame = new BridgeGame(3, Arrays.asList("U", "D", "U"));
+
+        // When
+        boolean firstStep = bridgeGame.move("U", 0);
+        boolean secondStep = bridgeGame.move("D", 1);
+        boolean thirdStep = bridgeGame.move("U", 2);
+
+        // Then
+        assertThat(bridgeGame.isSuccess()).isEqualTo(true);
+    }
+
+    @Test
+    @DisplayName("다리를 일부만 건넌 경우 결과 실패 검증")
+    void isSuccess_NotEqualsStepCount_ReturnFalse() {
+        // Given
+        BridgeGame bridgeGame = new BridgeGame(3, Arrays.asList("U", "D", "U"));
+
+        // When
+        boolean firstStep = bridgeGame.move("U", 0);
+        boolean secondStep = bridgeGame.move("D", 1);
+        boolean thirdStep = bridgeGame.move("D", 2);
+
+        // Then
+        assertThat(bridgeGame.isSuccess()).isEqualTo(false);
+    }
 }
