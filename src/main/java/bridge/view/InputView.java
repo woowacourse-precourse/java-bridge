a151b61bd19556f58fde9a2e9c6eeca14d481b2f
footdev <ekclstkfka5@gmail.com>
2022-11-22 15:54:55 +0900

    feat: 다리 길이 입력 받는 기능 추가
    다리 길이를 입력받는 기능을 추가함.

--- src/main/java/bridge/view/InputView.java
+++ src/main/java/bridge/view/InputView.java
@@ -1,5 +1,10 @@
 package bridge.view;
 
+
+import bridge.util.BridgeLengthValidator;
+
+import static camp.nextstep.edu.missionutils.Console.readLine;
+
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
  */
@@ -9,7 +14,9 @@ public class InputView {
      * 다리의 길이를 입력받는다.
      */
     public int readBridgeSize() {
-        return 0;
+        String input = readLine();
+        BridgeLengthValidator.validateLength(input);
+        return Integer.parseInt(input);
     }
 
     /**
