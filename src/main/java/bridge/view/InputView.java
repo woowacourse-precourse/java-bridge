86fc800256ae55f983080c66e7c3d477c7cd6a76
Song kyeong seok <skj0922@kookmin.ac.kr>
2022-11-22 16:30:12 +0900

    feat(InputView): 게임 재시작 여부를 물어보는 메서드 구현

--- src/main/java/bridge/view/InputView.java
+++ src/main/java/bridge/view/InputView.java
@@ -9,6 +9,7 @@ public class InputView {
 
     private static final String INPUT_BRIDGE_LENGTH = "다리의 길이를 입력해주세요";
     private static final String INPUT_BRIDGE_TYPE = "이동할 칸을 선택해주세요. (위: U, 아래: D)";
+    private static final String INPUT_RETRY_MESSAGE = "게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)";
 
     /**
      * 다리의 길이를 입력받는다.
@@ -30,6 +31,7 @@ public class InputView {
      * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
      */
     public String readGameCommand() {
-        return null;
+        System.out.println(INPUT_RETRY_MESSAGE);
+        return Console.readLine();
     }
 }
