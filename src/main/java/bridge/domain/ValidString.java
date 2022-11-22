5ee95f5861892f6cdf4315fa4f8f6061c9cf4139
hoin <apicad0307@gmail.com>
2022-11-22 15:21:49 +0900

    refactor(domain/ValidString.java): 에러메시지를 관리하는 역할의 클래스 생성

---
+++ src/main/java/bridge/domain/ValidString.java
@@ -0,0 +1,9 @@
+package bridge.domain;
+
+public class ValidString {
+    public static final String ERROR_NO_NUMBER_INPUT = "[ERROR] 숫자를 입력해주세요.";
+    public static final String ERROR_NOT_IN_RANGE = "[ERROR] 3~20 사이의 수를 입력해주세요";
+    public static final String ERROR_MOVE_INPUT = "[ERROR] U 또는 D를 입력해주세요.";
+    public static final String ERROR_RESTART_INPUT = "[ERROR] R 또는 Q를 입력해주세요";
+
+}
