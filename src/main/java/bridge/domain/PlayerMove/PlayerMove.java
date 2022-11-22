8d6b30696a49d975395fedb3090ed4108a98b430
hyee0715 <hyee0715@naver.com>
2022-11-22 17:28:39 +0900

    feat: 플레이어가 이동한 다리의 상태 출력하는 기능 구현

--- src/main/java/bridge/domain/PlayerMove/PlayerMove.java
+++ src/main/java/bridge/domain/PlayerMove/PlayerMove.java
@@ -2,6 +2,7 @@ package bridge.domain.PlayerMove;
 
 import bridge.domain.gameManagement.BridgeState;
 import bridge.view.InputView;
+import bridge.view.OutputView;
 
 import java.util.Arrays;
 import java.util.List;
@@ -24,32 +25,34 @@ public class PlayerMove {
         String movingPosition = takeMovingPosition();
 
         if (!movingPosition.equals(bridge.get(currentLocation))) {
-            currentLocation = moveFail(currentBridge, bridge, currentLocation);
+            return moveFail(currentBridge, bridge, currentLocation);
         }
 
-        if (movingPosition.equals(bridge.get(currentLocation))) {
-            currentLocation = moveSuccess(currentBridge, movingPosition, currentLocation);
-        }
-
-        return currentLocation;
+        return moveSuccess(currentBridge, movingPosition, currentLocation);
     }
 
     public static int moveFail(List<String> currentBridge, List<String> bridge, int currentLocation) {
+        OutputView outputView = new OutputView();
+
         String stateOfBridgeLine = bridge.get(currentLocation);
         Arrays.stream(BridgeState.values())
                 .filter(bridgeState -> bridgeState.getMovablePosition().equals(stateOfBridgeLine))
                 .forEach(bridgeState -> currentBridge.add(bridgeState.getFailedBridgeState()));
 
         currentLocation++;
+        outputView.printMap(currentBridge);
         return currentLocation;
     }
 
     public static int moveSuccess(List<String> currentBridge, String movingPosition, int currentLocation) {
+        OutputView outputView = new OutputView();
+
         Arrays.stream(BridgeState.values())
                 .filter(bridgeState -> bridgeState.getMovablePosition().equals(movingPosition))
                 .forEach(bridgeState -> currentBridge.add(bridgeState.getSuccessfulBridgeState()));
 
         currentLocation++;
+        outputView.printMap(currentBridge);
         return currentLocation;
     }
 }
--- src/main/java/bridge/view/OutputView.java
+++ src/main/java/bridge/view/OutputView.java
@@ -1,5 +1,7 @@
 package bridge.view;
 
+import java.util.List;
+
 /**
  * 사용자에게 게임 진행 상황과 결과를 출력하는 역할을 한다.
  */
@@ -7,13 +9,30 @@ public class OutputView {
     private final String GAME_START_MESSAGE = "다리 건너기 게임을 시작합니다.";
 
     private final String LINE_BREAK = "\n";
+    private final String PREFIX_SQUARE_BRACKET = "[";
+    private final String SUFFIX_SQUARE_BRACKET = "]";
+    private final String DIVIDING_VERTICAL_LINE = "|";
+    private final String BLANK = " ";
+
+    private final int START_INDEX = 0;
+    private final int ONE_INDEX = 1;
+    private final int START_LINE = 0;
 
     /**
      * 현재까지 이동한 다리의 상태를 정해진 형식에 맞춰 출력한다.
      * <p>
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void printMap() {
+    public void printMap(List<String> currentBridge) {
+        int bridgeWidth = currentBridge.get(START_INDEX).length();
+
+        for (int bridgeWidthCount = bridgeWidth - ONE_INDEX ; bridgeWidthCount >= START_INDEX; bridgeWidthCount--) {
+            System.out.print(PREFIX_SQUARE_BRACKET + BLANK);
+            printCurrentBridgeLine(currentBridge, bridgeWidthCount);
+            System.out.println(BLANK + SUFFIX_SQUARE_BRACKET);
+        }
+
+        System.out.print(LINE_BREAK);
     }
 
     /**
@@ -26,5 +45,15 @@ public class OutputView {
 
     public void printGameStartMessage() {
         System.out.println(GAME_START_MESSAGE + LINE_BREAK);
+    }
+
+    public void printCurrentBridgeLine(List<String> currentBridge, int currentLine) {
+        for (int lineCount = START_LINE; lineCount < currentBridge.size(); lineCount++) {
+            if (lineCount != START_LINE) {
+                System.out.print(BLANK + DIVIDING_VERTICAL_LINE + BLANK);
+            }
+
+            System.out.print(currentBridge.get(lineCount).charAt(currentLine));
+        }
     }
 }
