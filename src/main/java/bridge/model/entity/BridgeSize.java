07304e732422faeca093b768dc853634119ae99d
leewoojin <zmfpdl64@naver.com>
2022-11-22 13:05:17 +0900

    Feat(BridgeSze): 다리 사이즈 입력 예외 처리 추가 #35
    예외 조건:
    1. 공백이 아닌가?  And 숫자인가?
    2. 범위가 3 ~ 20 인가?
    
    BridgeRange:
    매직 넘버로 최소값 최대값을 작성했다.

--- src/main/java/bridge/model/entity/BridgeSize.java
+++ src/main/java/bridge/model/entity/BridgeSize.java
@@ -1,7 +1,11 @@
 package bridge.model.entity;
 
+import bridge.model.value.BridgeRange;
+
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+
+import static bridge.model.value.BridgeRange.*;
 
 /*
 * 숫자여야 함
@@ -19,13 +23,22 @@ public class BridgeSize {
     }
 
     private void validate(String bridgeSize) {
-        String pattern = "^[1-9]{1}[0-9]{0,3}";
-
+        String pattern = "^[\\d]{1,2}";
         Pattern compile = Pattern.compile(pattern);
-        if(compile.matcher(bridgeSize).find()){
-            return;
+        isDigit(compile, bridgeSize);
+        outRange(bridgeSize);
+    }
+
+    private void isDigit(Pattern compile, String bridgeSize) {
+        if(!compile.matcher(bridgeSize).find()){
+            throw new IllegalArgumentException("[ERROR]입력값이 정수가 아닙니다. 다시 입력해주세요 :" + bridgeSize);
         }
-        throw new IllegalArgumentException("[ERROR]입력 조건에 맞지 않습니다: 1~1000 :" + bridgeSize);
+    }
+    private void outRange(String bridgeSize){
+        int size = Integer.parseInt(bridgeSize);
+        if(size > MAX || size < MIN){ // TODO: 사이즈 MagicNum
+            throw new IllegalArgumentException("[ERROR]입력값이 범위에서 벗어났습니다 3 ~ 20 다시 입력해주세요 :" + bridgeSize);
+        }
     }
 
     public static BridgeSize of(String bridgeSize){
---
+++ src/main/java/bridge/model/value/BridgeRange.java
@@ -0,0 +1,6 @@
+package bridge.model.value;
+
+public class BridgeRange {
+    public static final int MAX = 20;
+    public static final int MIN = 3;
+}
