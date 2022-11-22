12e35ae4ee06770365430d22cf52e24f30bbf3af
skydreamer21 <skydreamer210@gmail.com>
2022-11-22 16:00:37 +0900

    feat(GameController): 게임 생성 기능 구현

--- src/main/java/bridge/controller/GameController.java
+++ src/main/java/bridge/controller/GameController.java
@@ -1,6 +1,13 @@
 package bridge.controller;
 
+import bridge.service.BridgeGame;
+import bridge.view.InputView;
+import bridge.view.OutputView;
+
 public class GameController {
+    private static final InputView inputView = InputView.getInstance();
+    private static final OutputView outputView = OutputView.getInstance();
+
     public static GameController instance = new GameController();
 
     public static GameController getInstance() {
@@ -8,5 +15,14 @@ public class GameController {
     }
 
     private GameController() {}
+
+    public BridgeGame create() {
+        try {
+            return new BridgeGame(inputView.readBridgeSize());
+        } catch (IllegalArgumentException e) {
+            outputView.printErrorMessage(e.getMessage());
+            return create();
+        }
+    }
 
 }
