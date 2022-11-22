b28ce6174ad6b079bb4b3c2b039bd39ba59c4323
kokomong2 <ko9603@naver.com>
2022-11-22 22:01:08 +0900

    fix(bridge) : 나의 테스트코드 import 수정(과제 제출 오류 원인 찾기 위해)

--- src/test/java/bridge/domain/PlayerTest.java
+++ src/test/java/bridge/domain/PlayerTest.java
@@ -1,7 +1,5 @@
 package bridge.domain;
 
-import bridge.constant.ErrorCode;
-import camp.nextstep.edu.missionutils.test.NsTest;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
@@ -13,36 +11,36 @@ import java.util.List;
 import static org.assertj.core.api.Assertions.*;
 
 class PlayerTest {
-//    private final List<BridgeShapeMatcher> bridgeShapeMatchers = new ArrayList<>();
-//
-//    @BeforeEach
-//    void player_생성(){
-//        bridgeShapeMatchers.add(BridgeShapeMatcher.DOWN_TRUE);
-//        bridgeShapeMatchers.add(BridgeShapeMatcher.UP_TRUE);
-//        bridgeShapeMatchers.add(BridgeShapeMatcher.DOWN_FALSE);
-//        bridgeShapeMatchers.add(BridgeShapeMatcher.DOWN_TRUE);
-//    }
-//
-//    @Test
-//    void 플레이어_입력_예외테스트1(){
-//        bridgeShapeMatchers.add(BridgeShapeMatcher.NONE);
-//        assertThatThrownBy(()-> new Player(bridgeShapeMatchers)).isInstanceOf(IllegalArgumentException.class);
-//    }
-//
-//    @ParameterizedTest
-//    @CsvSource({"X,true", "u,true","UU,false"})
-//    void 플레이어_입력_예외테스트2(String input, boolean isTrue){
-//        BridgeShapeMatcher bridgeShapeMatcher = BridgeShapeMatcher.findBridgeShape(input,isTrue);
-//        bridgeShapeMatchers.add(bridgeShapeMatcher);
-//        assertThatThrownBy(()-> new Player(bridgeShapeMatchers)).isInstanceOf(IllegalArgumentException.class);
-//    }
-//
-//    @Test
-//    void 플레이어_이동거리_예외테스트(){
-//        for (int i = 0; i < 20; i++) {
-//            bridgeShapeMatchers.add(BridgeShapeMatcher.UP_TRUE);
-//        }
-//        assertThatThrownBy(()-> new Player(bridgeShapeMatchers)).isInstanceOf(IllegalArgumentException.class);
-//    }
+    private final List<BridgeShapeMatcher> bridgeShapeMatchers = new ArrayList<>();
+
+    @BeforeEach
+    void player_생성(){
+        bridgeShapeMatchers.add(BridgeShapeMatcher.DOWN_TRUE);
+        bridgeShapeMatchers.add(BridgeShapeMatcher.UP_TRUE);
+        bridgeShapeMatchers.add(BridgeShapeMatcher.DOWN_FALSE);
+        bridgeShapeMatchers.add(BridgeShapeMatcher.DOWN_TRUE);
+    }
+
+    @Test
+    void 플레이어_입력_예외테스트1(){
+        bridgeShapeMatchers.add(BridgeShapeMatcher.NONE);
+        assertThatThrownBy(()-> new Player(bridgeShapeMatchers)).isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @ParameterizedTest
+    @CsvSource({"X,true", "u,true","UU,false"})
+    void 플레이어_입력_예외테스트2(String input, boolean isTrue){
+        BridgeShapeMatcher bridgeShapeMatcher = BridgeShapeMatcher.findBridgeShape(input,isTrue);
+        bridgeShapeMatchers.add(bridgeShapeMatcher);
+        assertThatThrownBy(()-> new Player(bridgeShapeMatchers)).isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void 플레이어_이동거리_예외테스트(){
+        for (int i = 0; i < 20; i++) {
+            bridgeShapeMatchers.add(BridgeShapeMatcher.UP_TRUE);
+        }
+        assertThatThrownBy(()-> new Player(bridgeShapeMatchers)).isInstanceOf(IllegalArgumentException.class);
+    }
 
 }
