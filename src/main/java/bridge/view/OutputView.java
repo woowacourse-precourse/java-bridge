ae7c9b306a7f5e79375ebffea68adf3433f84001
thuthi <whquddn55@gmail.com>
2022-11-22 22:01:13 +0900

    refactor(view): OutputView에 사용되던 상수들을 모두 enum으로 변경 OutputMessage enum을 통해 관리하도록 변경

--- src/main/java/bridge/view/OutputView.java
+++ src/main/java/bridge/view/OutputView.java
@@ -1,13 +1,27 @@
 package bridge.view;
 
+import static bridge.view.OutputMessage.BRIDGE_EMPTY;
+import static bridge.view.OutputMessage.BRIDGE_END;
+import static bridge.view.OutputMessage.BRIDGE_MATCHED;
+import static bridge.view.OutputMessage.BRIDGE_SEPARATOR;
+import static bridge.view.OutputMessage.BRIDGE_START;
+import static bridge.view.OutputMessage.BRIDGE_UNMATCHED;
+import static bridge.view.OutputMessage.GAME_END;
+import static bridge.view.OutputMessage.GAME_FAIL;
+import static bridge.view.OutputMessage.GAME_REQUEST_BRIDGE_LENGTH;
+import static bridge.view.OutputMessage.GAME_REQUEST_POSITION;
+import static bridge.view.OutputMessage.GAME_REQUEST_RETRY;
+import static bridge.view.OutputMessage.GAME_START;
+import static bridge.view.OutputMessage.GAME_SUCCESS;
+import static bridge.view.OutputMessage.GAME_TOTAL_TRY_COUNT;
+import static bridge.view.OutputMessage.GAME_WHETHER_SUCCESS;
+
 import bridge.bridge.BridgePosition;
 import bridge.bridgeGame.BridgeGame;
 import bridge.bridgeGame.BridgeGameState;
+import bridge.bridgeGame.GameCommand;
 
 public class OutputView {
-    public static final String BRIDGE_MATCHED = "O";
-    public static final String BRIDGE_UNMATCHED = "X";
-    public static final String BRIDGE_EMPTY = " ";
     public void printMap(BridgeGameState bridgeGameState) {
         printLine(bridgeGameState, BridgePosition.BRIDGE_UP);
         printLine(bridgeGameState, BridgePosition.BRIDGE_DOWN);
@@ -15,53 +29,53 @@ public class OutputView {
     }
 
     private void printLine(BridgeGameState bridgeGameState, BridgePosition position) {
-        System.out.print("[ ");
+        System.out.print(BRIDGE_START.getMessage());
         for (int i = 0; i < bridgeGameState.currentSize(); ++i) {
             if (i != 0) {
-                System.out.print(" | ");
+                System.out.print(BRIDGE_SEPARATOR.getMessage());
             }
             System.out.print(getMatchString(bridgeGameState.getCurrentAt(i), bridgeGameState.getMatched(i), position));
         }
-        System.out.println(" ]");
+        System.out.println(BRIDGE_END.getMessage());
     }
 
     private String getMatchString(String currentAt, boolean matched, BridgePosition position) {
         if (!currentAt.equals(position.getPosition())) {
-            return BRIDGE_EMPTY;
+            return BRIDGE_EMPTY.getMessage();
         }
         if (matched) {
-            return BRIDGE_MATCHED;
+            return BRIDGE_MATCHED.getMessage();
         }
-        return BRIDGE_UNMATCHED;
+        return BRIDGE_UNMATCHED.getMessage();
     }
 
     public void printResult(BridgeGame bridgeGame) {
-        System.out.println("최종 게임 결과");
+        System.out.println(GAME_END.getMessage());
         printMap(bridgeGame.getBridgeGameState());
-        System.out.println("게임 성공 여부: " + getSuccessString(bridgeGame.isWon()));
-        System.out.println("총 시도한 횟수: " + bridgeGame.getTryCount());
+        System.out.println(GAME_WHETHER_SUCCESS.getMessage() + getSuccessString(bridgeGame.isWon()));
+        System.out.println(GAME_TOTAL_TRY_COUNT.getMessage() + bridgeGame.getTryCount());
     }
 
     private String getSuccessString(boolean isSuccess) {
         if (isSuccess) {
-            return "성공";
+            return GAME_SUCCESS.getMessage();
         }
-        return "실패";
+        return GAME_FAIL.getMessage();
     }
 
     public void printInitGameMessage() {
-        System.out.println("다리 건너기 게임을 시작합니다.");
+        System.out.println(GAME_START.getMessage());
         System.out.println();
-        System.out.println("다리의 길이를 입력해주세요.");
+        System.out.println(GAME_REQUEST_BRIDGE_LENGTH.getMessage());
     }
 
     public void printRequestBridgeMoving() {
-        System.out.printf("이동할 칸을 선택해주세요. (위: %s, 아래: %s)\n",
-                BridgePosition.BRIDGE_UP.getPosition(),
+        System.out.printf(GAME_REQUEST_POSITION.getMessage(), BridgePosition.BRIDGE_UP.getPosition(),
                 BridgePosition.BRIDGE_DOWN.getPosition());
     }
 
     public void printRequestGameRetry() {
-        System.out.println("게임을 다시 시도할지 여부를 입력해주세요. (재시작: R, 종료: Q)");
+        System.out.printf(GAME_REQUEST_RETRY.getMessage(), GameCommand.COMMAND_RETRY.getCommandString(),
+                GameCommand.COMMAND_QUIT.getCommandString());
     }
 }
