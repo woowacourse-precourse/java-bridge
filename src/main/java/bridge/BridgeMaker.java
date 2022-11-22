f5e1a8fb1456e7e437505cac927d4dda1d51a633
MAYY27 <117031840+MAYY27@users.noreply.github.com>
2022-11-22 17:19:33 +0900

    <feat> BridgeNumber

--- src/main/java/bridge/BridgeMaker.java
+++ src/main/java/bridge/BridgeMaker.java
@@ -1,23 +1,32 @@
 package bridge;
 
+import java.util.ArrayList;
 import java.util.List;
 
-/**
- * 다리의 길이를 입력 받아서 다리를 생성해주는 역할을 한다.
- */
+
 public class BridgeMaker {
 
     private final BridgeNumberGenerator bridgeNumberGenerator;
+    private final List<String> bridge = new ArrayList<>();
+
 
     public BridgeMaker(BridgeNumberGenerator bridgeNumberGenerator) {
         this.bridgeNumberGenerator = bridgeNumberGenerator;
     }
 
-    /**
-     * @param size 다리의 길이
-     * @return 입력받은 길이에 해당하는 다리 모양. 위 칸이면 "U", 아래 칸이면 "D"로 표현해야 한다.
-     */
+   
     public List<String> makeBridge(int size) {
-        return null;
+        for (int i = 0; i < size; i++) {
+            int bridgeNumber = bridgeNumberGenerator.generate();
+            bridge.add(checkUpOrDown(bridgeNumber));
+        }
+        return bridge;
+    }
+
+    private String checkUpOrDown(int number){
+        if(number==0){
+            return "D";
+        }
+        return "U";
     }
 }
