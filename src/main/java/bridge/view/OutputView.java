273e36b2d4dbfb25e764bb2d749149f7165c7bde
mihye126 <mihea12@gmail.com>
2022-11-22 19:33:50 +0900

    [refactor] OutputView에서 원시값 대신 GameResult 출력으로 수정

--- src/main/java/bridge/view/OutputView.java
+++ src/main/java/bridge/view/OutputView.java
@@ -1,5 +1,6 @@
 package bridge.view;
 
+import bridge.domain.GameResult;
 import java.util.List;
 
 
@@ -13,9 +14,9 @@ public class OutputView {
      * <p>
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void printMap(List<String> up,List<String> down) {
-        System.out.println("[ " + String.join(" | ", up) + " ]");
-        System.out.println("[ " + String.join(" | ", down) + " ]");
+    public void printMap(GameResult result) {
+        System.out.println(result.upBridgeToString());
+        System.out.println(result.downBridgeToString());
         System.out.println();
     }
 
@@ -25,11 +26,11 @@ public class OutputView {
      * <p>
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void printResult(List<String> up,List<String> down, String gameTotalResult, int attempts) {
+    public void printResult(GameResult result, String gameTotalResult) {
         System.out.println("최종 게임 결과");
-        printMap( up,down);
+        printMap(result);
         System.out.println("게임 성공 여부: " + gameTotalResult);
-        System.out.println("총 시도한 횟수: " + attempts);
+        System.out.println("총 시도한 횟수: " + result.getAttempts());
     }
 
 }
