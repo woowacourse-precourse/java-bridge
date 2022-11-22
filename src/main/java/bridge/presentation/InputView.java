a371aee6feb06652dc63704976a1223b01128522
koo9b9h <90121024+koo9b9h@users.noreply.github.com>
2022-11-22 14:17:27 +0900

    feat : 입력부분에서 예외처리 기능 추가
    try부분에서 check 메소드로 조건에 맞지않으면 예외 발생 후 이를 재귀함수로 처리

--- src/main/java/bridge/presentation/InputView.java
+++ src/main/java/bridge/presentation/InputView.java
@@ -1,21 +1,42 @@
 package bridge.presentation;
 
+import bridge.exception.SizeOverException;
+import bridge.exception.DesignatedTypeException;
 import camp.nextstep.edu.missionutils.Console;
 
 public class InputView {
 
     public int readBridgeSize() {
-        int bridgeSize = Integer.parseInt(Console.readLine());
-        return bridgeSize;
+        try {
+            String size = Console.readLine();
+            DesignatedTypeException.allNumberCheck(size);
+            int bridgeSize = Integer.parseInt(size);
+            SizeOverException.bridgeSizeCheck(bridgeSize);
+            return bridgeSize;
+        } catch (IllegalArgumentException e) {
+            return readBridgeSize();
+        }//try-catch
     }//readBridgeSize
 
     public String readMoving() {
-        String movement = Console.readLine();
-        return movement;
+        try {
+            String movement = Console.readLine();
+            SizeOverException.oneWordCheck(movement);
+            DesignatedTypeException.UDcheck(movement);
+            return movement;
+        } catch (IllegalArgumentException e) {
+            return readMoving();
+        }//try-catch
     }//readMoving
 
     public String readGameCommand() {
-        String command = Console.readLine();
-        return command;
+        try {
+            String command = Console.readLine();
+            SizeOverException.oneWordCheck(command);
+            DesignatedTypeException.RQcheck(command);
+            return command;
+        } catch (IllegalArgumentException e) {
+            return readGameCommand();
+        }//try-catch
     }//readGameCommand
 }// end class
