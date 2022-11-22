ff6d83329229abe0a444ed49db7c23b301f1c1ef
안광민 <creativeduck0518@gmail.com>
2022-11-22 10:56:46 +0900

    refactor(PlayerMap): 사용자의 보여지는 맵을 관장하므로, presentation 패키지로 이동

--- src/main/java/bridge/presentation/PlayerMap.java
+++ src/main/java/bridge/presentation/PlayerMap.java
@@ -1,4 +1,4 @@
-package bridge;
+package bridge.presentation;
 
 import bridge.data.Direction;
 
--- src/test/java/bridge/PlayerMapTest.java
+++ src/test/java/bridge/PlayerMapTest.java
@@ -1,6 +1,7 @@
 package bridge;
 
 import bridge.data.Direction;
+import bridge.presentation.PlayerMap;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
