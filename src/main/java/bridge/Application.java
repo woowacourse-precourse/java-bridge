2bab59db0d538dcd65d5c3351ab3bf34aac7b0d0
glass <glass7312@gmail.com>
2022-11-22 18:29:55 +0900

    feat:

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -1,8 +1,36 @@
 package bridge;
 
+import java.util.List;
+
+import static bridge.BridgeGame.game_status;
+import static bridge.GameController.count;
+//import static bridge.GameController.round_count;
+
 public class Application {
 
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        GameController gameController = new GameController();
+        int input = gameController.inputView().readBridgeSize();
+        if (input == 1) {
+            System.out.println("[ERROR]");
+            return;
+        }
+        // System.out.print(bridge.toString());
+        gameController.set_bridge(input);
+        while (BridgeGame.game_status.equals("playing") && gameController.Bridge().size() != count) {
+            String position = gameController.inputView().readMoving();
+            if (gameController.BridgeGame().move(position, gameController.Bridge(), count).equals("call")) {
+                String get = gameController.BridgeGame().retry(gameController.inputView().readGameCommand());
+                if (get.equals("playing")) {
+                    count = 0;
+                    gameController.round_count++;
+                    continue;
+                }
+            }
+            gameController.outputView().printMap();
+            count++;
+            gameController.total_print();
+        }
     }
 }
+
