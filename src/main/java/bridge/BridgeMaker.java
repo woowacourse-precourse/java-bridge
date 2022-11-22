65427e5f54b64fc9bd3cbe3bf61e50e97a432e91
eyeofsol <kingjihong@naver.com>
2022-11-22 21:16:22 +0900

    refactor(BridgeSetting): 상수를 모아놓은 클래스 구현
    여기저기 사용되는 상수들을 한데 모아 클래스로 구현했습니다.
    따로 enum으로 분리하지 않은 이유는 자주 사용되지 않고 연관성이 각각
    깊지 않다고 판단하여 작성하게 됐습니다.

--- src/main/java/bridge/BridgeMaker.java
+++ src/main/java/bridge/BridgeMaker.java
@@ -1,5 +1,7 @@
 package bridge;
 
+import bridge.controller.BridgeStatus;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -8,8 +10,6 @@ import java.util.List;
  */
 public class BridgeMaker {
 
-    private static final int MIN_RANGE = 3;
-    private static final int MAX_RANGE = 20;
     private final BridgeNumberGenerator bridgeNumberGenerator;
 
     public BridgeMaker(BridgeNumberGenerator bridgeNumberGenerator) {
@@ -21,23 +21,16 @@ public class BridgeMaker {
      * @return 입력받은 길이에 해당하는 다리 모양. 위 칸이면 "U", 아래 칸이면 "D"로 표현해야 한다.
      * 즉, 랜덤 인풋에서 1이면 U, 0이면 D로 표현한다.
      */
-    public List<String> makeBridge(int size) throws IllegalArgumentException {
+    public List<String> makeBridge(int size) {
         List<String> bridge = new ArrayList<>();
         int number;
 
-        validate(size);
         for (int i = 0; i < size; i++) {
             number = generateZeroOrOne();
             bridge.add(parseZeroOrOneToUorD(number));
         }
 
         return bridge;
-    }
-
-    private void validate(int number) throws IllegalArgumentException {
-        if (number < MIN_RANGE || MAX_RANGE < number) {
-            throw new IllegalArgumentException("[ERROR] 다리길이는 3이상 20이하 입니다.");
-        }
     }
 
     private int generateZeroOrOne() {
@@ -48,7 +41,7 @@ public class BridgeMaker {
         String movement = "";
         for (BridgeStatus bridgeStatus : BridgeStatus.values()) {
             if (number == bridgeStatus.getNumber()) {
-                movement = bridgeStatus.getMovement();
+                movement = bridgeStatus.getMoving();
             }
         }
         return movement;
---
+++ src/main/java/bridge/controller/BridgeSetting.java
@@ -0,0 +1,11 @@
+package bridge.controller;
+
+public class BridgeSetting {
+    public static final int MIN_RANGE = 3;
+    public static final int MAX_RANGE = 20;
+    public static final String QUIT_COMMAND = "Q";
+    public static final String RETRY_COMMAND = "R";
+    public static final String CORRECT = "O";
+    public static final String FALSE = "X";
+    public static final String EMPTY = " ";
+}
