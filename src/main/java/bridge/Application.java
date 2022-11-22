01b47c539bddfbc12078da8ce9b0e3b48c3c65d7
JUHA <khabh@naver.com>
2022-11-22 16:06:03 +0900

    refactor(BridgeGame): 컨트롤러에서 BridgeMapService 사용하도록 수정

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -1,19 +1,12 @@
 package bridge;
 
-import bridge.controller.BridgeController;
 import bridge.controller.BridgeGameController;
 
 public class Application {
     private static final BridgeGameController bridgeGameController = new BridgeGameController();
-    private static final BridgeController bridgeController = new BridgeController();
 
     public static void main(String[] args) {
-        init();
-        bridgeGameController.run();
-    }
-
-    private static void init() {
         bridgeGameController.generateBridgeGame();
-        bridgeController.generateBridge();
+        bridgeGameController.run();
     }
 }
--- src/main/java/bridge/controller/BridgeGameController.java
+++ src/main/java/bridge/controller/BridgeGameController.java
@@ -1,45 +1,66 @@
 package bridge.controller;
 
+import bridge.BridgeMaker;
+import bridge.BridgeRandomNumberGenerator;
+import bridge.domain.bridgeGame.gameStatus.gameStatics.GameResult;
+import bridge.exception.NotNumericException;
 import bridge.service.BridgeGameService;
-import bridge.service.BridgeService;
+import bridge.service.BridgeMapService;
+import bridge.utils.StringUtils;
+import bridge.validator.BridgeSizeValidator;
 import bridge.validator.GameCommandValidator;
 import bridge.validator.MovingDirectionValidator;
 import bridge.view.InputView;
 import bridge.view.OutputView;
 
+import java.util.List;
 import java.util.Queue;
 
 public class BridgeGameController {
     private final static String RESTART_GAME = "R";
 
-    private final BridgeGameService bridgeGameService = new BridgeGameService();
-    private final BridgeService bridgeService = new BridgeService();
+    private final BridgeGameService bridgeGameService = BridgeGameService.getInstance();
+    private final BridgeMapService bridgeMapService = BridgeMapService.getInstance();
+
+    public List<String> generateBridge() {
+        String input = InputView.readBridgeSize();
+
+        try {
+            return initBridge(input);
+        } catch (IllegalArgumentException exception) {
+            System.out.println(exception.getMessage());
+            return generateBridge();
+        }
+    }
+
+    private List<String> initBridge(String input) {
+        if (!StringUtils.isNumeric(input)) {
+            throw new NotNumericException();
+        }
+        int bridgeSize = Integer.parseInt(input);
+        BridgeSizeValidator.validateBridgeSize(bridgeSize);
+
+        BridgeMaker bridgeMaker = new BridgeMaker(new BridgeRandomNumberGenerator());
+        return bridgeMaker.makeBridge(bridgeSize);
+    }
 
     public void generateBridgeGame() {
         OutputView.startGame();
-        bridgeGameService.initBridgeGame();
+
+        List<String> bridge = generateBridge();
+        bridgeGameService.initBridgeGame(bridge);
+        bridgeMapService.initMap();
     }
 
     private String getCrossingDirection() {
         try {
             String crossingDirection = InputView.readMoving();
             MovingDirectionValidator.validateDirection(crossingDirection);
+
             return crossingDirection;
         } catch (IllegalArgumentException exception) {
             System.out.println(exception.getMessage());
             return getCrossingDirection();
-        }
-    }
-
-    private void play() {
-        for (int distance = 0; distance + 1 <= bridgeService.getBridgeSize(); distance++) {
-            String crossingDirection = getCrossingDirection();
-            boolean crossSuccess = bridgeService.isPassable(distance, crossingDirection);
-            bridgeGameService.crossOneBlock(crossingDirection, crossSuccess);
-            OutputView.printMap(bridgeGameService.getBridgeMap());
-            if (!crossSuccess) {
-                break;
-            }
         }
     }
 
@@ -57,21 +78,26 @@ public class BridgeGameController {
     private boolean retryGame() {
         if (getGameCommand().equals(RESTART_GAME)) {
             bridgeGameService.retryGame();
+            bridgeMapService.resetBridgeMap();
             return true;
         }
         return false;
     }
 
     private void printGameResult() {
-        String finalMap = bridgeGameService.getBridgeMap();
+        String finalMap = bridgeMapService.getBridgeMap();
         Queue<String> gameStatics = bridgeGameService.getGameStatics();
         OutputView.printResult(finalMap, gameStatics);
     }
 
     public void run() {
-        do {
-            play();
-        } while (!bridgeGameService.getCrosseSuccess() && retryGame());
+        GameResult gameResult = GameResult.NOT_EXIST;
+        while (gameResult.equals(GameResult.NOT_EXIST) || gameResult.equals(GameResult.FAIL) && retryGame()) {
+            String direction = getCrossingDirection();
+            gameResult = bridgeGameService.move(direction);
+            bridgeMapService.generateMap(direction, gameResult.crossSuccess());
+            OutputView.printMap(bridgeMapService.getBridgeMap());
+        }
         printGameResult();
     }
 }
