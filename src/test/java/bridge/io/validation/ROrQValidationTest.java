837594f6041f0d46a22c86a6e5c4466e78e126db
yubin <dbqls2821@gmail.com>
2022-11-22 18:15:57 +0900

    test(RangeValidationTest): 입력값이 R 혹은 Q가 아닐때 예외를 던지는지 테스트

---
+++ src/test/java/bridge/io/validation/ROrQValidationTest.java
@@ -0,0 +1,22 @@
+package bridge.io.validation;
+
+import bridge.enums.ExceptionMessage;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.assertj.core.api.Assertions.assertThatCode;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.*;
+
+class ROrQValidationTest {
+
+  @ParameterizedTest
+  @ValueSource(strings = {"r", "q", "A", "R ", " Q", "RR", "QQ", "2424", "0", "-2497", "wooteco"})
+  void 입력값이_R또는_Q가_아니라면_예외를_던지는가(String input) {
+    ROrQValidation rOrQValidation = new ROrQValidation(new NullValidation());
+    assertThatThrownBy(() -> rOrQValidation.validate(input))
+      .isInstanceOf(IllegalArgumentException.class)
+      .hasMessageContaining(ExceptionMessage.ERROR.getMessage() + ExceptionMessage.NOT_R_OR_Q.getMessage());
+  }
+
+}
