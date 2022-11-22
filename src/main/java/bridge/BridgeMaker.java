706ff7c45c39a444817cdcb8230cc5445103e0aa
juno-junho <ssmm0205@naver.com>
2022-11-22 19:48:18 +0900

    feat: create method makeBridge
    makeRandomNums 메서드를 통해 랜덤값을 생성하고, convertToMatchingValue 통해 1, 0을 문자로 변환해 다리를 만드는 메서드 생성

--- src/main/java/bridge/BridgeMaker.java
+++ src/main/java/bridge/BridgeMaker.java
@@ -1,12 +1,16 @@
 package bridge;
 
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 /**
  * 다리의 길이를 입력 받아서 다리를 생성해주는 역할을 한다.
  */
 public class BridgeMaker {
 
+    private static final String MOVE_UP_VALUE = "U";
+    private static final String MOVE_DOWN_VALUE = "D";
     private final BridgeNumberGenerator bridgeNumberGenerator;
 
     public BridgeMaker(BridgeNumberGenerator bridgeNumberGenerator) {
@@ -18,6 +22,24 @@ public class BridgeMaker {
      * @return 입력받은 길이에 해당하는 다리 모양. 위 칸이면 "U", 아래 칸이면 "D"로 표현해야 한다.
      */
     public List<String> makeBridge(int size) {
-        return null;
+        List<Integer> randomNums = makeRandomNums(size);
+        return randomNums
+                .stream()
+                .map(this::convertToMatchingValue)
+                .collect(Collectors.toList());
+    }
+
+    private List<Integer> makeRandomNums(int size) {
+        return IntStream.rangeClosed(1,size)
+                .mapToObj(i -> bridgeNumberGenerator.generate())
+                .collect(Collectors.toList());
+    }
+
+    private String convertToMatchingValue(int randomNum) {
+        final int MOVE_UP_NUMBER = 1;
+        if (randomNum == MOVE_UP_NUMBER) {
+            return MOVE_UP_VALUE;
+        }
+        return MOVE_DOWN_VALUE;
     }
 }
