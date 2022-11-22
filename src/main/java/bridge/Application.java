046755406db44b84fd746fb1a2a9ea22bd862adf
김선동 <mok03127@gmail.com>
2022-11-22 19:37:00 +0900

    rename: 자바 컨벤션에 맞게 수정

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -1,6 +1,6 @@
 package bridge;
 
-import Controller.BridgeGameController;
+import controller.BridgeGameController;
 
 public class Application {
     public static void main(String[] args) {
--- src/main/java/controller/BridgeGameController.java
+++ src/main/java/controller/BridgeGameController.java
@@ -1,4 +1,4 @@
-package Controller;
+package controller;
 
 import bridge.BridgeGame;
 import bridge.BridgeMaker;
