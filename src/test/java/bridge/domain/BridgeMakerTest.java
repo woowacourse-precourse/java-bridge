e742c68a124ba5aa7e472992adcc1896701bdaed
Giho <poqw741@gmail.com>
2022-11-22 21:18:36 +0900

    test(bridgeMaker) : 다리 사이즈 및 요소값 테스트

--- src/test/java/bridge/domain/BridgeMakerTest.java
+++ src/test/java/bridge/domain/BridgeMakerTest.java
@@ -1,4 +1,34 @@
-import static org.junit.jupiter.api.Assertions.*;
+package bridge.domain;
+
+import bridge.BridgeRandomNumberGenerator;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+
 class BridgeMakerTest {
-  
+
+    @Test
+    void 사이즈_확인_테스트() {
+        BridgeMaker bridgeMaker = new BridgeMaker(new BridgeRandomNumberGenerator());
+        int size = 20;
+
+        List<String> test = bridgeMaker.makeBridge(size);
+
+        assertThat(test.size()).isEqualTo(20);
+
+    }
+
+    void 요소값_테스트() {
+        BridgeMaker bridgeMaker = new BridgeMaker(new BridgeRandomNumberGenerator());
+        int size = 20;
+        List<String> compareValue = List.of("U","D");
+
+        List<String> test = bridgeMaker.makeBridge(size);
+
+        assertThat(test).isIn(compareValue);
+
+    }
 }
