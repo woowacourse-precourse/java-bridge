94f1e8c4436eef99674303b696afd07c54cf4895
YUSEONJUN <seonjun0906@gmail.com>
2022-11-22 19:41:45 +0900

    test : 이동 D 기능 테스트

--- src/test/java/bridge/ApplicationTest.java
+++ src/test/java/bridge/ApplicationTest.java
@@ -126,6 +126,23 @@ class ApplicationTest extends NsTest {
 		}, 1, 1, 1);
 	}
 
+	@Test
+	void move_D_기능_테스트() {
+		assertRandomNumberInRangeTest(() -> {
+			run("3", "D", "D", "D");
+			assertThat(output()).contains(
+				"최종 게임 결과",
+				"[   |   |   ]",
+				"[ O | O | O ]",
+				"게임 성공 여부: 성공",
+				"총 시도한 횟수: 1"
+			);
+			int upSideIndex = output().indexOf("[   ]");
+			int downSideIndex = output().indexOf("[ O ]");
+			assertThat(upSideIndex).isLessThan(downSideIndex);
+		}, 0, 0, 0);
+	}
+
 	@Override
 	protected void runMain() {
 		Application.main(new String[] {});
