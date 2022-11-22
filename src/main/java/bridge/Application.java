ba8db3578c727fca389d4ed52952305701dc1035
tlsqj <rnjstlsqja@inha.edu>
2022-11-22 22:19:20 +0900

    feat(Application.java): Application.java 구현
    - BridgeGameStarter 을 실행하도록 구현

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -1,8 +1,10 @@
 package bridge;
 
 public class Application {
-
+    static BridgeGameStarter bridgeGameStarter = new BridgeGameStarter();
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+
+        bridgeGameStarter.BridgeGamePlay();
     }
 }
