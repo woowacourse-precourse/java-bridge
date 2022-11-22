c29dbd61b8f52aa181f22568baf3af3cfbfbfcff
SeokHwan An <asok369258@naver.com>
2022-11-22 19:55:30 +0900

    refactor : Validation 내 메소드 접근제어자 변경

--- src/main/java/bridge/validation/Validation.java
+++ src/main/java/bridge/validation/Validation.java
@@ -13,7 +13,7 @@ public class Validation {
     }
 
     // 다리의 길이 입력값이 숫자인지 검증하는 기능
-    public static void checkBridgeLengthConsistOfNum(String bridgeLength) {
+    private static void checkBridgeLengthConsistOfNum(String bridgeLength) {
         if (bridgeLength.isEmpty() || bridgeLength.isBlank()) {
             throw new IllegalArgumentException(ERROR_MESSAGE_BRIDGE_LENGTH_BLANK_OR_EMPTY);
         }
@@ -23,14 +23,14 @@ public class Validation {
     }
 
     // 다리의 길이 입력값이 0으로 시작하는지 검증하느 기능
-    public static void checkBridgeLengthStartZero(String bridgeLength) {
+    private static void checkBridgeLengthStartZero(String bridgeLength) {
         if (bridgeLength.charAt(0) == ZERO) {
             throw new IllegalArgumentException(ERROR_MESSAGE_BRIDGE_LENGTH_START_ZERO);
         }
     }
 
     // 다리의 길이 입력값이 3이상 20이하인지 검증하는 기능
-    public static void checkBridgeLengthRange3To20(String bridgeLengthInput) {
+    private static void checkBridgeLengthRange3To20(String bridgeLengthInput) {
         int bridgeLength = Integer.parseInt(bridgeLengthInput);
         if (!(BRIDGE_LENGTH_MIN <= bridgeLength && bridgeLength <= BRIDGE_LENGTH_MAX)) {
             throw new IllegalArgumentException(ERROR_MESSAGE_BRIDGE_LENGTH_RANGE_3_TO_20);
