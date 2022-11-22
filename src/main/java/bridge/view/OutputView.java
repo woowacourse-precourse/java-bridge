cb1245b3496dac371913eb7bf1e168aca6d01dd1
Kim Seung Beom <88074556+daily1313@users.noreply.github.com>
2022-11-22 19:31:17 +0900

    feat(OutputView) : printNowMap() 메서드 구현

--- src/main/java/bridge/view/OutputView.java
+++ src/main/java/bridge/view/OutputView.java
@@ -35,5 +35,23 @@ public class OutputView {
         System.out.println("\n이동할 칸을 선택해주세요. (위: U, 아래: D)");
     }
 
+    public void printInputGameRestartOptionMessage() {
+        System.out.println("\n게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)");
+    }
+
+    /**
+     * 현재까지 이동한 다리의 상태를 정해진 형식에 맞춰 출력한다.
+     * <p>
+     * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
+     */
+    public void printNowMap(int nowIndex, String moveDirection, boolean isSameValueComputerAndUser) {
+        if (nowIndex >= 1) {
+            upView.append(DIVIDING_LINE);
+            downView.append(DIVIDING_LINE);
+        }
+        appendDirection(moveDirection, isSameValueComputerAndUser);
+        printResultMap();
+    }
+
 
 }
