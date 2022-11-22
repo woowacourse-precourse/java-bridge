8746309d5bdc88fddd6fc7a28bd72bcb6f3846e8
Insuk Lee <66675919+insukL@users.noreply.github.com>
2022-11-22 22:57:55 +0900

    feat: add game end method
    - 게임 마무리 로직 추가

--- src/main/java/bridge/game/GameManager.java
+++ src/main/java/bridge/game/GameManager.java
@@ -7,16 +7,20 @@ import bridge.view.OutputView;
 
 public class GameManager {
 
-    private BridgeMaker bridgeMaker = new BridgeMaker(new BridgeRandomNumberGenerator());
-    private InputView inputView = new InputView();
-    private OutputView outputView = new OutputView();
+    private final BridgeMaker bridgeMaker = new BridgeMaker(new BridgeRandomNumberGenerator());
+    private final InputView inputView = new InputView();
+    private final OutputView outputView = new OutputView();
     private BridgeGame bridgeGame;
     private GameState gameState = GameState.CONTINUE;
 
     public void game() {
         outputView.printStartMessage();
         initBridge();
-        moveBridge();
+
+        while (!gameState.equals(GameState.EXIT)) {
+            moveBridge();
+            endGame();
+        }
     }
 
     private void initBridge() {
@@ -35,6 +39,31 @@ public class GameManager {
             String command = inputView.readMoving();
             gameState = bridgeGame.move(command);
             outputView.printMap(bridgeGame.getBridge(), gameState);
+        }
+    }
+
+    private void endGame() {
+        if (gameState.equals(GameState.FAIL)) {
+            retryBridge();
+        }
+
+        if (gameState.equals(GameState.CLEAR)) {
+            clearGame();
+        }
+    }
+
+    private void clearGame() {
+        outputView.printResult(bridgeGame.getBridge(), bridgeGame.getTryCount(), GameState.CLEAR);
+        this.gameState = GameState.EXIT;
+    }
+
+    private void retryBridge() {
+        outputView.printInputRetryMessage();
+        String command = inputView.readGameCommand();
+
+        gameState = bridgeGame.retry(command);
+        if (gameState.equals(GameState.EXIT)) {
+            outputView.printResult(bridgeGame.getBridge(), bridgeGame.getTryCount(), GameState.FAIL);
         }
     }
 }
