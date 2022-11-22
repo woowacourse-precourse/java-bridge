8d6c0a0652e2d19c012fce21b61e8ce51d7a13cf
Giho <poqw741@gmail.com>
2022-11-22 17:45:14 +0900

    feat(outputView) : 게임 시작 안내 문구 추가

--- src/main/java/bridge/ui/OutputView.java
+++ src/main/java/bridge/ui/OutputView.java
@@ -6,17 +6,18 @@ import java.util.List;
  * 사용자에게 게임 진행 상황과 결과를 출력하는 역할을 한다.
  */
 public class OutputView {
+    private static final String START_GAME_ANNOUNCEMENT = "다리 건너기 게임을 시작합니다.";
 
+    public void announceStartGame() {
+        System.out.println(START_GAME_ANNOUNCEMENT);
+    }
     /**
      * 현재까지 이동한 다리의 상태를 정해진 형식에 맞춰 출력한다.
      * <p>
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
     public void printMap(List<String> upperBridgeStatus, List<String> downBridgeStatus) {
-        if(input==0){
-            upperBridgeStatus.add("   ");
-            downBridgeStatus.add(" 0 ");
-        }
+
     }
 
     /**
