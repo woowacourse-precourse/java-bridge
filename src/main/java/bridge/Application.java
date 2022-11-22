775dfab4ed8f2aa0f0a23b4c6b5ca50c76c0003a
devMuscle <hongsbien@naver.com>
2022-11-22 17:09:24 +0900

    feat: 다리 건너기 미션 구현

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -1,8 +1,22 @@
 package bridge;
 
+import bridge.io.InputView;
+import bridge.io.OutputView;
+import bridge.validation.InputValidator;
+
 public class Application {
 
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        BridgeNumberGenerator bridgeNumberGenerator = new BridgeRandomNumberGenerator();
+        BridgeMaker bridgeMaker = new BridgeMaker(bridgeNumberGenerator);
+        BridgeChecker bridgeChecker = new BridgeChecker();
+        InputValidator inputValidator = new InputValidator();
+        InputView inputView = new InputView(inputValidator);
+        OutputView outputView = new OutputView(bridgeChecker);
+        BridgeGame bridgeGame = new BridgeGame(bridgeChecker ,bridgeMaker);
+
+        BridgeGameHost bridgeGameHost = new BridgeGameHost(inputView, outputView, bridgeGame);
+        bridgeGameHost.runGame();
     }
 }
---
+++ src/main/java/bridge/mapper/GameCommandMapper.java
@@ -0,0 +1,2 @@
+package bridge.mapper;public class GameCommandMapper {
+}
