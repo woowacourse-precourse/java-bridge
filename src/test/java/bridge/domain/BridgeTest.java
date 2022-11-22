45abcf96f285b8faa6187be469e886d99f378ef0
KTH-96 <taehyun960@gmail.com>
2022-11-22 14:39:32 +0900

    test(BridgeTest): 다리 생성구조를 변경해 테스트 전체 수정

--- src/test/java/bridge/domain/BridgeTest.java
+++ src/test/java/bridge/domain/BridgeTest.java
@@ -7,6 +7,7 @@ import static org.junit.jupiter.params.provider.Arguments.of;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Stream;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
@@ -14,11 +15,18 @@ import org.junit.jupiter.params.provider.MethodSource;
 
 class BridgeTest {
 
+    private Bridge bridge;
+
+    @BeforeEach
+    void setUp() {
+        bridge = new Bridge();
+    }
+
     @ParameterizedTest
     @MethodSource("generateWrongBridgeData")
     @DisplayName("양쪽 다리를 지나갈 수 없다면 예외가 발생한다.")
     void bridgeMakeTest(List<String> input) {
-        assertThatThrownBy(() -> new Bridge(input))
+        assertThatThrownBy(() -> bridge.createBridge(input))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
@@ -26,7 +34,7 @@ class BridgeTest {
     @MethodSource("generateBridgeData")
     @DisplayName("특정 인덱스의 다리위치가 맞는지 체크하는 기능")
     void bridgeCorrectPositionTest(List<String> input) {
-        Bridge bridge = new Bridge(input);
+        bridge.createBridge(input);
         for (int i = 0; i < input.size(); i++) {
             assertThat(bridge.isCorrectLastPosition(i, input.get(i))).isTrue();
         }
@@ -36,7 +44,7 @@ class BridgeTest {
     @MethodSource("generateBridgeData")
     @DisplayName("다리 사이즈를 반환하는지 체크하는 테스트")
     void bridgeSizeTest(List<String> input) {
-        Bridge bridge = new Bridge(input);
+        bridge.createBridge(input);
         assertThat(bridge.size()).isEqualTo(input.size());
     }
 
