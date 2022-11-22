d1f07234875353abee4cdd3207769e3a6d769808
KJE <12181501@inha.edu>
2022-11-22 18:43:25 +0900

    refactor: Application에서 출력이 필요한 부분 모두 OutputView로 이동

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -2,7 +2,7 @@ package bridge;
 
 public class Application {
     public static void main(String[] args) {
-        System.out.println("다리 건너기 게임을 시작합니다.\n");
+        OutputView.startGame();
         BridgeGame bridgeGame = new BridgeGame();
         BridgeGameController.startGame(bridgeGame);
         boolean flag = true;
--- src/main/java/bridge/OutputView.java
+++ src/main/java/bridge/OutputView.java
@@ -6,6 +6,9 @@ import java.util.List;
  * 사용자에게 게임 진행 상황과 결과를 출력하는 역할을 한다.
  */
 public class OutputView {
+    public static void startGame(){
+        System.out.println("다리 건너기 게임을 시작합니다.\n");
+    }
 
     public static void printMap(List<String> bridge, int size, String userInput) {
         System.out.print("[");
