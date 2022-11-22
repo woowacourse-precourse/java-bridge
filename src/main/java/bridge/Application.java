04995cecc027bac24acded097510175954e8ec31
TeddyYeoni <po99027@naver.com>
2022-11-22 20:43:44 +0900

    feat: 메인 메서드 객체 추가 생성

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -1,8 +1,15 @@
 package bridge;
 
+import bridge.cotroller.BridgeGame;
+import bridge.cotroller.BridgeMaker;
+import bridge.view.InputView;
+import bridge.view.OutputView;
+
 public class Application {
 
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+       InputView inputView = new InputView();
+       OutputView outputView = new OutputView();
+       BridgeGame bridgeGame = new BridgeGame();
     }
 }
