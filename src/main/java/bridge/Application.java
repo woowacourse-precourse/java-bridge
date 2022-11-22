991336d124ff345d573276a411103e91a4651ee9
DongwookKim0823 <dwkim0823@naver.com>
2022-11-22 20:26:05 +0900

    feat(bridge): 메인 함수에서 게임 시작 기능

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -1,8 +1,15 @@
 package bridge;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Application {
 
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        InputView inputView = new InputView();
+        BridgeRandomNumberGenerator bridgeRandomNumberGenerator = new BridgeRandomNumberGenerator();
+        BridgeMaker bridgeMaker = new BridgeMaker(bridgeRandomNumberGenerator);
+        BridgeGame bridgeGame = new BridgeGame(bridgeMaker, inputView.readBridgeSize());
+        bridgeGame.playGame();
     }
 }
