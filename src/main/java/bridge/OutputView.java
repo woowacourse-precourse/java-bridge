eaa0a4608d54c313f846428bdfb5b7085c01002b
BHC-Chicken <tij05078@gmail.com>
2022-11-22 11:45:17 +0900

    style : 출력 변경
    예외 발생 시 메시지에 [ERROR] 추가
    게임 시작 메시지 추가

--- src/main/java/bridge/OutputView.java
+++ src/main/java/bridge/OutputView.java
@@ -12,7 +12,14 @@ public class OutputView {
      * <p>
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void printMap() {
+    public void startGameMessage() {
+        System.out.println("다리 건너기 게임을 시작합니다.\n");
+    }
+
+    public String errorMessage(String message) {
+        return "[ERROR] " + message;
+    }
+
     public void printMap(List<String> userInputResult) {
         upDown(userInputResult, true);
         upDown(userInputResult, false);
