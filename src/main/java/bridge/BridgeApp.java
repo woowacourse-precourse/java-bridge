a593a7cfc0c55528c5bc5cab12718db2f8ff6880
mardi2020 <mardi@khu.ac.kr>
2022-11-22 12:58:04 +0900

    feat(BridgeApp.java): 게임 결과 확인 메서드 추가

--- src/main/java/bridge/BridgeApp.java
+++ src/main/java/bridge/BridgeApp.java
@@ -33,4 +33,38 @@ public class BridgeApp {
             move = inputView.readMoving();
         } while (move.equals("[ERROR]"));
     }
+
+    public int checkGame(BridgeGame bridgeGame, int arrive) {
+        int result = -1;
+        if (arrive == 1) {
+            result = winGame(bridgeGame);
+        } else if (arrive == -1) {
+            result = retryGame(bridgeGame);
+        }
+        return result;
+    }
+
+    private int winGame(BridgeGame bridgeGame) {
+        outputView.printResult(bridgeGame, count);
+        return 0;
+    }
+
+    private int retryGame(BridgeGame bridgeGame) {
+        endOrRetryGame();
+        if (command.equals("Q")) {
+            outputView.printResult(bridgeGame, count);
+            return 0;
+        }
+        if (command.equals("R")) {
+            bridgeGame.retry();
+            count++;
+        }
+        return -1;
+    }
+
+    private void endOrRetryGame() {
+        do {
+            command = inputView.readGameCommand();
+        } while (command.equals("[ERROR]"));
+    }
 }
