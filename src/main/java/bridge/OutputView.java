7145df17127d52693d23b7f6b3194d6b455888f1
wkdghdwns199 <35947667+wkdghdwns199@users.noreply.github.com>
2022-11-22 15:58:26 +0900

    Feat : 게임 결과 출력 기능 추가

--- src/main/java/bridge/OutputView.java
+++ src/main/java/bridge/OutputView.java
@@ -24,5 +24,12 @@ public class OutputView {
      * <p>
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
+    public void printResult(BridgeGameController bridgeGameController, BridgeGame bridgeGame, boolean successStatus) {
+        printResultLine();
+        this.printMap(bridgeGame.statusMap);
+        System.out.println("게임 성공 여부: " + checkIfSucceed(successStatus));
+        System.out.println("총 시도한 횟수: " + bridgeGameController.getTryCount());
+    }
+
 
 }
