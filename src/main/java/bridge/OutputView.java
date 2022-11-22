bd335a5ffc16063fc9c5119444d3a5f55f9ed418
DongwookKim0823 <dwkim0823@naver.com>
2022-11-22 15:07:51 +0900

    feat(bridge): 최종 게임 결과를 보여주는 기능

--- src/main/java/bridge/OutputView.java
+++ src/main/java/bridge/OutputView.java
@@ -23,7 +23,13 @@ public class OutputView {
      * 게임의 최종 결과를 정해진 형식에 맞춰 출력한다.
      * <p>
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
+     * @param finalState
      */
-    public void printResult() {
+    public void printResult(List<List<String>> finalState, int gameResult, int gameCount) {
+        System.out.println("최종 게임 결과");
+        System.out.println("[ " + String.join(" | ", finalState.get(0)) + " ]");
+        System.out.println("[ " + String.join(" | ", finalState.get(1)) + " ]");
+        System.out.println();
+        System.out.println();
     }
 }
