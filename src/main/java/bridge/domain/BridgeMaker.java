4d1f6053fbf79b8f5275357a95e663e7879451b3
footdev <ekclstkfka5@gmail.com>
2022-11-22 15:51:52 +0900

    feat: 다리를 생성하는 기능 추가
    입력받은 만큼 다리를 생성하는 기능 추가함.

--- src/main/java/bridge/domain/BridgeMaker.java
+++ src/main/java/bridge/domain/BridgeMaker.java
@@ -1,10 +1,8 @@
 package bridge.domain;
 
+import java.util.ArrayList;
 import java.util.List;
 
-/**
- * 다리의 길이를 입력 받아서 다리를 생성해주는 역할을 한다.
- */
 public class BridgeMaker {
 
     private final BridgeNumberGenerator bridgeNumberGenerator;
@@ -18,6 +16,17 @@ public class BridgeMaker {
      * @return 입력받은 길이에 해당하는 다리 모양. 위 칸이면 "U", 아래 칸이면 "D"로 표현해야 한다.
      */
     public List<String> makeBridge(int size) {
-        return null;
+        List<String> bridge = new ArrayList<>();
+        for (int phase = 0; phase < size; phase++) {
+            bridge.add(convert(bridgeNumberGenerator.generate()));
+        }
+        return bridge;
+    }
+
+    private String convert(int number) {
+        if(number == Command.UP.getStatusInteger()) {
+            return Command.UP.getStatusString();
+        }
+        return Command.DOWN.getStatusString();
     }
 }
