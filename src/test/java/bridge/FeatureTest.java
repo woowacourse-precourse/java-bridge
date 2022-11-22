1e9bc93375944169cb03ad451f7dd5ebc37657a3
Minsoo Choi <lucas0209@yonsei.ac.kr>
2022-11-22 18:03:10 +0900

    test(FeatureTest): test each main logic features
    Feature tested:
    - Given bridge size, check if it generates the random directions
    - Check if the saved user input contains exactly the same directions
    - Compare user inputs and answer and check if the output consists of [marker(O,X), direction(U,D)] format
    - Test retry method given user input

---
+++ src/test/java/bridge/FeatureTest.java
@@ -0,0 +1,69 @@
+package bridge;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.*;
+
+public class FeatureTest {
+
+    @Test
+    void 다리_만들기_테스트() {
+        BridgeMaker bm = new BridgeMaker(new BridgeRandomNumberGenerator());
+        int bridgeSize = 3;
+        List<String> directions = bm.makeBridge(bridgeSize);
+
+        assertThat(directions).contains("U");
+        assertThat(directions).contains("D");
+        assertThat(directions).hasSize(bridgeSize);
+    }
+
+    @Test
+    void 사용자_객체_테스트() {
+        User user = new User();
+        user.saveChoice("U");
+        user.saveChoice("D");
+        user.saveChoice("D");
+
+        assertThat(user.getUserMoveChoice()).hasSize(3);
+        assertThat(user.getUserMoveChoice()).isEqualTo(Arrays.asList("U", "D", "D"));
+    }
+
+    @Test
+    void 사용자_입력과_정답_비교_일치_테스트() {
+        BridgeGame bg = new BridgeGame();
+        List<String> answer = new ArrayList<>(Arrays.asList("U", "D", "D"));
+        User user = new User();
+        user.saveChoice("U");
+        user.saveChoice("D");
+        user.saveChoice("D");
+
+        assertThat(bg.move(answer, user)).hasSize(3);
+        assertThat(bg.move(answer, user)).isEqualTo(Arrays.asList(Arrays.asList("O", "U"), Arrays.asList("O", "D"), Arrays.asList("O", "D")));
+    }
+
+    @Test
+    void 사용자_입력과_정답_비교_불일치_테스트() {
+        BridgeGame bg = new BridgeGame();
+        List<String> answer = new ArrayList<>(Arrays.asList("U", "U", "U"));
+        User user = new User();
+        user.saveChoice("U");
+        user.saveChoice("D");
+        user.saveChoice("D");
+
+        assertThat(bg.move(answer, user)).hasSize(3);
+        assertThat(bg.move(answer, user)).isEqualTo(Arrays.asList(Arrays.asList("O", "U"), Arrays.asList("X", "D"), Arrays.asList("X", "D")));
+    }
+
+    @Test
+    void 재시도_여부_테스트() {
+        BridgeGame bg = new BridgeGame();
+        assertThat(bg.retry("R")).isEqualTo(true);
+        assertThat(bg.retry("Q")).isEqualTo(false);
+    }
+
+}
