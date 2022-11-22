715727f9ddabda5d87cd5e1f746db4d832f204ed
gomljo <gomljo0307@naver.com>
2022-11-22 13:33:46 +0900

    refactor(BridgeGameController): 담당 역할 변경
    기존의 BridgeGameService에서 담당하던 역할을 컨트롤러로 이동
    캡슐화를 위해 객체에게 요청하는 방식으로 리팩터링 진행

---
+++ src/main/java/bridge/controller/BridgeGameController.java
@@ -0,0 +1,74 @@
+package bridge.controller;
+
+import bridge.controller.cosntant.GameCommand;
+import bridge.domain.model.BridgeGame;
+import bridge.domain.model.Player;
+import bridge.view.OutputView;
+
+public class BridgeGameController {
+    InputController inputController = new InputController();
+    OutputView outputView = new OutputView();
+    BridgeGame bridgeGame = new BridgeGame();
+    Player player = new Player();
+
+    public void startBridgeMakingProcess() {
+        int bridgeSize = inputController.checkBridgeSize();
+        bridgeGame.prepare();
+        bridgeGame.constructBridge(bridgeSize);
+    }
+
+    public void bridgeCrossProcess() {
+        String moving = inputController.checkMoving();
+        player.saveDirection(moving);
+        bridgeGame.move(player.notifyDirection());
+        String report = bridgeGame.makeReport(player.getCurrentDirection());
+        outputView.printMap(report);
+    }
+
+    public boolean isSingleGameEnd() {
+        return (bridgeGame.isGameEnd() && bridgeGame.isCrossSuccess());
+    }
+
+    public void playSingleGame() {
+        bridgeGame.prepare();
+        while (isSingleGameEnd()) {
+            bridgeCrossProcess();
+        }
+        bridgeGame.updateTotalCount();
+    }
+
+    public void retryProcess() {
+        if (!bridgeGame.isCrossSuccess()) {
+            bridgeGame.Restart(askRetry());
+        }
+    }
+
+    public boolean askRetry() {
+        String gameCommand = inputController.checkGameCommand();
+        return GameCommand.transformToAction(gameCommand);
+    }
+
+    public boolean isGameContinue() {
+        if (bridgeGame.getRestartState()) {
+            return true;
+        }
+        return bridgeGame.isCrossSuccess() && bridgeGame.getRestartState();
+    }
+
+    public void endProcess() {
+        String report = bridgeGame.getFinalResult();
+        outputView.printMapResult(report);
+        outputView.printGameResult(bridgeGame.getBridgeGameState());
+        outputView.printTotalTrial(bridgeGame.getTotalTrial());
+    }
+
+    public void start() {
+        outputView.printStartMessage();
+        startBridgeMakingProcess();
+        do {
+            playSingleGame();
+            retryProcess();
+        } while (isGameContinue());
+        endProcess();
+    }
+}
