112459c17e131f3e7e599c4129f81291c8370738
김형기 <khk211113@naver.com>
2022-11-22 19:47:22 +0900

    docs(CommandType): 명령어 주석 추가

--- src/main/java/bridge/view/CommandType.java
+++ src/main/java/bridge/view/CommandType.java
@@ -28,6 +28,12 @@ public enum CommandType {
         return command;
     }
 
+    /**
+     * 입력된 문자열을 이용해 해당하는 명령어 정보 찾기
+     * @param command 입력된 문자열
+     * @return 명령어
+     * @throws NoSuchElementException 해당하는 명령어가 없을 경우 예외 발생
+     */
     public static CommandType of(final String command) throws NoSuchElementException {
         String name = COMMAND_MAP.get(command);
         if (name == null) {
