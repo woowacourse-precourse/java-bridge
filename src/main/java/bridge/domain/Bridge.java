639d12f8f2b323135be893f5af1aa2359401e43c
ckyeon <qsky8520@gmail.com>
2022-11-22 20:55:35 +0900

    feat(Bridge): add Bridge class

---
+++ src/main/java/bridge/domain/Bridge.java
@@ -0,0 +1,12 @@
+package bridge.domain;
+
+import java.util.List;
+
+public class Bridge {
+
+    private final List<String> bridge;
+
+    public Bridge(List<String> bridge) {
+        this.bridge = bridge;
+    }
+}
