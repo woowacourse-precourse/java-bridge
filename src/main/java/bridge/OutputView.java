7921975a5281a7164178f0e7129c1ca828e0c48b
CupRaccoon <41440969+CupRaccoon@users.noreply.github.com>
2022-11-22 18:01:26 +0900

    feat: Add Ouput message of game flow

--- src/main/java/bridge/OutputView.java
+++ src/main/java/bridge/OutputView.java
@@ -1,23 +1,28 @@
 package bridge;
 
+import org.w3c.dom.ls.LSOutput;
+
 /**
  * 사용자에게 게임 진행 상황과 결과를 출력하는 역할을 한다.
  */
 public class OutputView {
 
-    /**
-     * 현재까지 이동한 다리의 상태를 정해진 형식에 맞춰 출력한다.
-     * <p>
-     * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
-     */
+
     public void printMap() {
     }
 
-    /**
-     * 게임의 최종 결과를 정해진 형식에 맞춰 출력한다.
-     * <p>
-     * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
-     */
     public void printResult() {
+    }
+    public void printStart(){
+        System.out.println("다리 건너기 게임을 시작합니다.");
+    }
+    public void printBridgeMake(){
+        System.out.println("다리의 길이를 입력해주세요.");
+    }
+    public void printMove(){
+        System.out.println("이동할 칸을 선택해주세요. (위: U, 아래: D)");
+    }
+    public void printRetry(){
+        System.out.println("게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)");
     }
 }
