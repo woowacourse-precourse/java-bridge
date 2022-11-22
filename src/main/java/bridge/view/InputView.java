ad4c6cd08be5dac5794b55e0eb8102eb3fdb8574
Be-student <presentsong@naver.com>
2022-11-22 18:45:41 +0900

    chore: 안 쓰는 코드 제거

--- src/main/java/bridge/view/InputView.java
+++ src/main/java/bridge/view/InputView.java
@@ -5,14 +5,10 @@ import bridge.command.ReadMovingCommand;
 import bridge.command.RetryCommand;
 import camp.nextstep.edu.missionutils.Console;
 
-import java.util.regex.Pattern;
-
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
  */
 public final class InputView {
-    private static final Pattern NUMBERS = Pattern.compile("^\\d+$");
-    private static final String NOT_NUMBER_MESSAGE = "입력은 숫자로만 이루어져야 합니다";
     private static final String OVER_RANGE_MESSAGE = "입력의 범위를 초과했습니다";
 
     /**
