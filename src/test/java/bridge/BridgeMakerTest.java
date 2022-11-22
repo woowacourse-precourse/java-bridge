97c10898533bc78ceac4dc07fead31c924fb4441
ostar11 <cjw5877@gmail.com>
2022-11-22 18:05:38 +0900

    test(BridgeMakerTest): 테스트 추가 구현
    랜덤숫자생성테스트, 다리위치획득테스트

--- src/test/java/bridge/BridgeMakerTest.java
+++ src/test/java/bridge/BridgeMakerTest.java
@@ -3,14 +3,39 @@ package bridge;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import bridge.domain.BridgeMaker;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 class BridgeMakerTest {
+    private final int MINIMUM_NUMBER = 1;
+    private final int MAXIMUM_NUMBER = 45;
+    private BridgeMaker bridgeMaker;
+    private BridgeRandomNumberGenerator bridgeRandomNumberGenerator;
+
+    @BeforeEach
+    void setUp() {
+        bridgeRandomNumberGenerator = new BridgeRandomNumberGenerator();
+        bridgeMaker = new BridgeMaker(bridgeRandomNumberGenerator);
+    }
 
     @Test
     void 다리_만들기_테스트() {
-        BridgeRandomNumberGenerator bridgeRandomNumberGenerator = new BridgeRandomNumberGenerator();
-        BridgeMaker bridgeMaker = new BridgeMaker(bridgeRandomNumberGenerator);
         assertThat(bridgeMaker.makeBridge(3)).containsAnyOf("U", "D");
+    }
+
+    @Test
+    void 랜덤_숫자_생성_테스트() {
+        int input = bridgeMaker.createRandomNumber();
+        assertThat(input).isGreaterThan(MINIMUM_NUMBER - 1)
+                .isLessThan(MAXIMUM_NUMBER + 1);
+    }
+
+    @ParameterizedTest
+    @CsvSource({"0,D", "1,U"})
+    void 다리_위치_획득_테스트(int randomNumber, String sign) {
+        String input = bridgeMaker.getBridgeSide(randomNumber);
+        assertThat(input).isEqualTo(sign);
     }
 }
