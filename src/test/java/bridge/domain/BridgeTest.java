04ca3d9ae3dfed89fed6388ef159201e754bc443
hhiyeon <hhyyeon0214@gmail.com>
2022-11-22 15:46:02 +0900

    test:BridgeTest-initialization moving result bridge list

--- src/test/java/bridge/domain/BridgeTest.java
+++ src/test/java/bridge/domain/BridgeTest.java
@@ -21,4 +21,16 @@ public class BridgeTest {
 
         assertThat(result).isEqualTo(expect);
     }
+
+    @DisplayName("이전 게임의 다리 정보 리스트를 초기화 하는 기능 성공")
+    @Test
+    void initMovingResultBridgeList() {
+        Bridge bridge = new Bridge();
+        bridge.setMovingBridge("U", "O");
+        bridge.initBridge();
+        List<String> result = bridge.getUpBridge();
+        List<String> expect = new ArrayList<>();
+
+        assertThat(result).isEqualTo(expect);
+    }
 }
