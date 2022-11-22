59ad74134349fd74ae0e2caea878ecba01cef4e7
Sanmgmin <sangmins930@gmail.com>
2022-11-22 22:08:49 +0900

    test: Add several test case

--- src/test/java/bridge/InputValidatorTest.java
+++ src/test/java/bridge/InputValidatorTest.java
@@ -2,23 +2,25 @@ package bridge;
 
 import org.junit.jupiter.api.Test;
 
-import static bridge.View.Validator.InputValidator.validateInteger;
-import static bridge.View.Validator.InputValidator.validateRange;
+import static bridge.View.Validator.InputValidator.*;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 
 public class InputValidatorTest {
     @Test
-    void validateNumeric_NotNumeric_IllegalArgumentException() {
-        // 영어 대문자
+    void validateInteger_NotInteger_IllegalArgumentException() {
+        // 영어 문자
         assertThatThrownBy(() -> validateInteger("Z"))
                 .isInstanceOf(IllegalArgumentException.class);
-        //영어 소문자
+        //영어 문장
         assertThatThrownBy(() -> validateInteger("class"))
                 .isInstanceOf(IllegalArgumentException.class);
-        //특수문자
+        //특수 문자
         assertThatThrownBy(() -> validateInteger(";"))
                 .isInstanceOf(IllegalArgumentException.class);
         assertThatThrownBy(() -> validateInteger("*"))
+                .isInstanceOf(IllegalArgumentException.class);
+        // 엄청 큰 수
+        assertThatThrownBy(() -> validateInteger("222665441154654564651231325464651231515615656156"))
                 .isInstanceOf(IllegalArgumentException.class);
         // 영어 숫자 혼합
         assertThatThrownBy(() -> validateInteger("F112"))
@@ -27,16 +29,41 @@ public class InputValidatorTest {
 
     @Test
     void validateRange_OutOfRange_IllegalArgumentException() {
-        // 엄청 큰 수
-        assertThatThrownBy(() -> validateInteger("222665441154654564651231325464651231515615656156"))
-                .isInstanceOf(IllegalArgumentException.class);
         // 20보다 큰 수
         assertThatThrownBy(() -> validateRange(100))
                 .isInstanceOf(IllegalArgumentException.class);
         // 3보다 작은 수
         assertThatThrownBy(() -> validateRange(-12))
                 .isInstanceOf(IllegalArgumentException.class);
+    }
 
+    @Test
+    void validateUpOrDown_NotUOrD_IllegalArgumentException() {
+        // 다른 문자
+        assertThatThrownBy(() -> validateUpOrDown("Z"))
+                .isInstanceOf(IllegalArgumentException.class);
+        // 숫자
+        assertThatThrownBy(() -> validateUpOrDown("1111111"))
+                .isInstanceOf(IllegalArgumentException.class);
+        // 소문자
+        assertThatThrownBy(() -> validateUpOrDown("u"))
+                .isInstanceOf(IllegalArgumentException.class);
+        assertThatThrownBy(() -> validateUpOrDown("d"))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
 
+    @Test
+    void validateRetryOrQuit_NotROrQ_IllegalArgumentException() {
+        // 다른 문자
+        assertThatThrownBy(() -> validateUpOrDown("Z"))
+                .isInstanceOf(IllegalArgumentException.class);
+        // 숫자
+        assertThatThrownBy(() -> validateUpOrDown("1111111"))
+                .isInstanceOf(IllegalArgumentException.class);
+        // 소문자
+        assertThatThrownBy(() -> validateUpOrDown("q"))
+                .isInstanceOf(IllegalArgumentException.class);
+        assertThatThrownBy(() -> validateUpOrDown("r"))
+                .isInstanceOf(IllegalArgumentException.class);
     }
 }
