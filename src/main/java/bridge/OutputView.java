c29cf611345ff84a834e4656612dbda4317beabb
Kwak-JunYoung <klop9090@g.skku.edu>
2022-11-22 10:53:56 +0900

    fix(OutputView.java) Modification of paramters of the methods
    Modified paramters of the methods in the class from String to List<String>

--- src/main/java/bridge/OutputView.java
+++ src/main/java/bridge/OutputView.java
@@ -1,27 +1,29 @@
 package bridge;
 
+import java.util.List;
+
 /**
  * 사용자에게 게임 진행 상황과 결과를 출력하는 역할을 한다.
  */
 public class OutputView {
 
-    public void printMapLine(String userMovement, String bridge, int bridgeSize, char target) {
+    public void printMapLine(List<String> userMovement, List<String> bridge, int bridgeSize, String target) {
         System.out.print("[ ");
-        printOX(userMovement.charAt(0), bridge.charAt(0), target);
-        for (int i = 1; i < bridgeSize; i++) {
+        printOX(userMovement.get(0), bridge.get(0), target);
+        for (int i = 1; i < userMovement.size(); i++) {
             System.out.print(" | ");
-            printOX(userMovement.charAt(i), bridge.charAt(i), target);
+            printOX(userMovement.get(i), bridge.get(i), target);
         }
         System.out.println(" ]");
     }
-    public void printOX(char userChoice, char bridgeStatus, char target) {
-        if(userChoice == target && bridgeStatus == target) {
+    public void printOX(String userChoice, String bridgeStatus, String target) {
+        if(userChoice.equals(target) && bridgeStatus.equals(target)) {
             System.out.print("O");
         }
-        if(userChoice == target && bridgeStatus != target) {
+        if(userChoice.equals(target) && !bridgeStatus.equals(target)) {
             System.out.print("X");
         }
-        if(userChoice != target) {
+        if(!userChoice.equals(target)) {
             System.out.print(" ");
         }
     }
@@ -31,9 +33,9 @@ public class OutputView {
      * <p>
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void printMap(String userMovement, String bridge, int bridgeSize) {
-        printMapLine(userMovement, bridge, bridgeSize, 'U');
-        printMapLine(userMovement, bridge, bridgeSize, 'D');
+    public void printMap(List<String> userMovement, List<String> bridge, int bridgeSize) {
+        printMapLine(userMovement, bridge, bridgeSize, "U");
+        printMapLine(userMovement, bridge, bridgeSize, "D");
     }
 
     /**
