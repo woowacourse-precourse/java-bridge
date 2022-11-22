6d26217236f5cbb2627f4148aa7c4bbc16397984
delaylee <delaylee219@gmail.com>
2022-11-22 19:23:52 +0900

    refactor: 테스트 코드 파라미터 테스트 ParameterizedTest 사용하도록 변경

--- src/test/java/bridge/InputViewReadBridgeSizeTest.java
+++ src/test/java/bridge/InputViewReadBridgeSizeTest.java
@@ -3,6 +3,8 @@ package bridge;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.Assertions.assertThatNoException;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
@@ -17,40 +19,36 @@ class InputViewReadBridgeSizeTest {
     }
 
     @DisplayName("입력 받은 다리 길이가 숫자가 아닌 경우 에러 발생")
-    @Test
-    void readBridgeSizeValidErrorTest() {
-        String input = "abc";
-
+    @ValueSource(strings = {"abc", "test", "한글"})
+    @ParameterizedTest
+    void readBridgeSizeValidErrorTest(String input) {
         assertThatThrownBy(() -> inputView.sizeValidation(input))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining("[ERROR] 다리 길이는 3부터 20 사이의 숫자여야 합니다");
     }
 
     @DisplayName("입력 받은 다리 길이가 숫자가 3보다 작은 경우")
-    @Test
-    void readBridgeSizeValidErrorTest2() {
-        String input = "-2";
-
+    @ValueSource(strings = {"-2", "0", "2.0"})
+    @ParameterizedTest
+    void readBridgeSizeValidErrorTest2(String input) {
         assertThatThrownBy(() -> inputView.sizeValidation(input))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining("[ERROR] 다리 길이는 3부터 20 사이의 숫자여야 합니다");
     }
 
     @DisplayName("입력 받은 다리 길이가 숫자가 20보다 큰 경우")
-    @Test
-    void readBridgeSizeValidErrorTest3() {
-        String input = "20.5";
-
+    @ValueSource(strings = {"20.5", "1000", "20.1"})
+    @ParameterizedTest
+    void readBridgeSizeValidErrorTest3(String input) {
         assertThatThrownBy(() -> inputView.sizeValidation(input))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining("[ERROR] 다리 길이는 3부터 20 사이의 숫자여야 합니다");
     }
 
-    @DisplayName("입력 받은 다리 길이가 5인 경우")
-    @Test
-    void movingValidErrorTestU() {
-        String input = "5";
-
+    @DisplayName("입력 받은 다리 길이가 3~20 사이인 경우")
+    @ValueSource(strings = {"3", "10", "20"})
+    @ParameterizedTest
+    void movingValidErrorTestU(String input) {
         inputView.sizeValidation(input);
 
         assertThatNoException();
--- src/test/java/bridge/InputViewReadGameCommandTest.java
+++ src/test/java/bridge/InputViewReadGameCommandTest.java
@@ -3,6 +3,8 @@ package bridge;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.Assertions.assertThatNoException;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
@@ -17,50 +19,27 @@ class InputViewReadGameCommandTest {
     }
 
     @DisplayName("입력 받은 값이 R 또는 Q가 아닌 경우")
-    @Test
-    void movingValidErrorTest() {
-        String input = "abc";
-
+    @ValueSource(strings = {"abc", "test", "한글"})
+    @ParameterizedTest
+    void movingValidErrorTest(String input) {
         assertThatThrownBy(() -> inputView.gameCommandValidation(input))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining("[ERROR] R(재시도) 또는 Q(종료) 중 하나의 문자여야 합니다");
     }
 
-    @DisplayName("입력 받은 이동 칸이 소문자 r인 경우")
-    @Test
-    void movingValidErrorTestLowerR() {
-        String input = "r";
-
+    @DisplayName("입력 받은 이동 칸이 소문자인 경우")
+    @ValueSource(strings = {"r", "q"})
+    @ParameterizedTest
+    void movingValidErrorTestLower(String input) {
         assertThatThrownBy(() -> inputView.gameCommandValidation(input))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining("[ERROR] R(재시도) 또는 Q(종료) 중 하나의 문자여야 합니다");
     }
 
-    @DisplayName("입력 받은 이동 칸이 소문자 q인 경우")
-    @Test
-    void movingValidErrorTestLowerQ() {
-        String input = "q";
-
-        assertThatThrownBy(() -> inputView.gameCommandValidation(input))
-                .isInstanceOf(IllegalArgumentException.class)
-                .hasMessageContaining("[ERROR] R(재시도) 또는 Q(종료) 중 하나의 문자여야 합니다");
-    }
-
-    @DisplayName("입력 받은 이동 칸이 R인 경우")
-    @Test
-    void movingValidErrorTestR() {
-        String input = "R";
-
-        inputView.gameCommandValidation(input);
-
-        assertThatNoException();
-    }
-
-    @DisplayName("입력 받은 이동 칸이 Q인 경우")
-    @Test
-    void movingValidErrorTestQ() {
-        String input = "Q";
-
+    @DisplayName("입력 받은 이동 칸이 R 또는 Q인 경우")
+    @ValueSource(strings = {"R", "Q"})
+    @ParameterizedTest
+    void movingValidErrorCorrectTest(String input) {
         inputView.gameCommandValidation(input);
 
         assertThatNoException();
