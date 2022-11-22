88917ae0e396a4c768d2595fac720b1d0009430d
saehim <saeilee.vxv@gmail.com>
2022-11-22 20:24:13 +0900

    test (view) : 테스트 메서드 수정

--- src/test/java/bridge/view/InputViewTest.java
+++ src/test/java/bridge/view/InputViewTest.java
@@ -1,8 +1,7 @@
-package view;
+package bridge.view;
 
 import bridge.BridgeMaker;
 import bridge.BridgeRandomNumberGenerator;
-import bridge.view.InputView;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
--- src/test/java/bridge/view/OutputViewTest.java
+++ src/test/java/bridge/view/OutputViewTest.java
@@ -1,4 +1,21 @@
-import static org.junit.jupiter.api.Assertions.*;
+package bridge.view;
+
+import bridge.view.OutputView;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+
 class OutputViewTest {
-  
+
+    @Test
+    void printMap_기능구현() {
+        OutputView outputView = new OutputView(Arrays.asList("U", "D", "D"));
+        outputView.printMap(Arrays.asList("U","D","D"), Arrays.asList("U","D","D"));
+    }
+
+    @Test
+    void printMap_틀렸을때() {
+        OutputView outputView = new OutputView(Arrays.asList("U", "D", "D"));
+        outputView.printMap(Arrays.asList("U", "D", "D"), Arrays.asList("U","D","U"));
+    }
 }
