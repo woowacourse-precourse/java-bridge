be535297f046c7dd203999a7105c6011398a2446
heejeong <hjx66@naver.com>
2022-11-22 20:00:14 +0900

    refactor: 하드코딩 값을 상수로 변경 & 명령어를 모아둔 Command 클래스 생성

--- src/main/java/bridge/view/InputView.java
+++ src/main/java/bridge/view/InputView.java
@@ -1,18 +1,30 @@
 package bridge.view;
 
+import bridge.domain.Command;
 import camp.nextstep.edu.missionutils.Console;
 
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
  */
 public class InputView {
+    private static final String ERROR_MESSAGE_BRIDGE_SIZE = "[ERROR]  다리 길이는 3부터 20 사이의 숫자여야 합니다.";
+    private static final String ERROR_MESSAGE_NUMBER_FORMAT = "[ERROR] 숫자 외 문자가 입력되었습니다.";
+    private static final String ERROR_MESSAGE_INPUT_MOVING = "[ERROR] U 또는 D만 입력할 수 있습니다.";
+    private static final String ERROR_MESSAGE_GAME_COMMAND = "[ERROR] R 또는 Q만 입력할 수 있습니다.";
+
+    private static final String INPUT_MESSAGE_BRIDGE_SIZE = "다리의 길이를 입력해주세요.";
+    private static final String INPUT_MESSAGE_MOVING = "이동할 칸을 선택해주세요. (위: U, 아래: D)";
+    private static final String INPUT_MESSAGE_GAME_COMMAND = "게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)";
+
+    private static final int MIN_BRIDGE_SIZE = 3;
+    private static final int MAX_BRIDGE_SIZE = 21;
 
     /**
      * 다리의 길이를 입력받는다.
      */
     public int readBridgeSize() {
         int bridgeSize;
-        System.out.println("다리의 길이를 입력해주세요.");
+        System.out.println(INPUT_MESSAGE_BRIDGE_SIZE);
         String input = Console.readLine();
         try {
             validateNumberFormat(input);
@@ -26,8 +38,8 @@ public class InputView {
     }
 
     private void validateBridgeSizeRange(int bridgeSize) {
-        if (bridgeSize < 3 || bridgeSize > 21) {
-            throw new IllegalArgumentException("[ERROR]  다리 길이는 3부터 20 사이의 숫자여야 합니다.");
+        if (bridgeSize < MIN_BRIDGE_SIZE || bridgeSize > MAX_BRIDGE_SIZE) {
+            throw new IllegalArgumentException(ERROR_MESSAGE_BRIDGE_SIZE);
         }
     }
 
@@ -35,7 +47,7 @@ public class InputView {
         try {
             Integer.parseInt(s);
         } catch (NumberFormatException e) {
-            throw new IllegalArgumentException("[ERROR] 숫자 외 문자가 입력되었습니다.");
+            throw new IllegalArgumentException(ERROR_MESSAGE_NUMBER_FORMAT);
         }
     }
 
@@ -43,7 +55,7 @@ public class InputView {
      * 사용자가 이동할 칸을 입력받는다.
      */
     public String readMoving() {
-        System.out.println("이동할 칸을 선택해주세요. (위: U, 아래: D)");
+        System.out.println(INPUT_MESSAGE_MOVING);
         String inputMoving = Console.readLine();
         try {
             validateInputMoving(inputMoving);
@@ -55,8 +67,8 @@ public class InputView {
     }
 
     public void validateInputMoving(String inputMoving) {
-        if (!inputMoving.equals("U") && !inputMoving.equals("D")) {
-            throw new IllegalArgumentException("[ERROR] U 또는 D만 입력할 수 있습니다.");
+        if (!inputMoving.equals(Command.UP) && !inputMoving.equals(Command.DOWN)) {
+            throw new IllegalArgumentException(ERROR_MESSAGE_INPUT_MOVING);
         }
     }
 
@@ -64,7 +76,7 @@ public class InputView {
      * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
      */
     public String readGameCommand() {
-        System.out.println("게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)");
+        System.out.println(INPUT_MESSAGE_GAME_COMMAND);
         String inputCommand = Console.readLine();
         try {
             validateGameCommand(inputCommand);
@@ -75,9 +87,9 @@ public class InputView {
         return inputCommand;
     }
 
-    private void validateGameCommand(String inputCommand){
-        if (!inputCommand.equals("R") && !inputCommand.equals("Q")) {
-            throw new IllegalArgumentException("[ERROR] R 또는 Q만 입력할 수 있습니다.");
+    private void validateGameCommand(String inputCommand) {
+        if (!inputCommand.equals(Command.RETRY) && !inputCommand.equals(Command.QUIT)) {
+            throw new IllegalArgumentException(ERROR_MESSAGE_GAME_COMMAND);
         }
     }
 }
