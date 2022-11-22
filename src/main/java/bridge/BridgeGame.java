60d7af9cf515f656f974b1c3b0b3c87201e47357
davinc71998 <davinc71998@gmail.com>
2022-11-22 21:01:40 +0900

    Refactor: 변경 BridgeGame 띄어쓰기 magicNumber 리팩토링

--- src/main/java/bridge/BridgeGame.java
+++ src/main/java/bridge/BridgeGame.java
@@ -13,14 +13,18 @@ public class BridgeGame {
     static final int WRONG_MATCH_DOWN = 2;
     static final int WRONG_MATCH_UP = 3;
 
+    static final int GAME_TRY_FAIL = 1;
+    static final int GAME_TRY_EXIT = 2;
+
+
     /**
      * 사용자가 칸을 이동할 때 사용하는 메서드
      * <p>
      * 이동을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
     public int move(List<String> bridge, int bridgeIndex, String input) {
-        if(bridge.get(bridgeIndex).equals(input)) {
-            if(input.equals("U")) {
+        if (bridge.get(bridgeIndex).equals(input)) {
+            if (input.equals("U")) {
                 return RIGHT_MATCH_UP;
             }
             return RIGHT_MATCH_DOWN;
@@ -38,9 +42,9 @@ public class BridgeGame {
      * 재시작을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
     public int retry(String input) {
-        if(input.equals("R")) {
-            return 1;
+        if (input.equals("R")) {
+            return GAME_TRY_FAIL;
         }
-        return 0;
+        return GAME_TRY_EXIT;
     }
 }
