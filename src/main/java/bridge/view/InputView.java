94fba0dbad8b18d117f68db18cf5064391932fdf
jiwon1217 <koagjw@gmail.com>
2022-11-22 15:00:05 +0900

    style: 주석 제거

--- src/main/java/bridge/view/InputView.java
+++ src/main/java/bridge/view/InputView.java
@@ -6,14 +6,7 @@ import static bridge.exception.InputException.isValidBridgeSize;
 import static bridge.exception.InputException.isValidMovementDirection;
 import static bridge.exception.InputException.isValidRestartOrQuitCommand;
 
-/**
- * 사용자로부터 입력을 받는 역할을 한다.
- */
 public class InputView {
-
-    /**
-     * 다리의 길이를 입력받는다.
-     */
     public static int readBridgeSize() {
         try {
             OutputView.printInputBridgeSize();
@@ -26,9 +19,6 @@ public class InputView {
         }
     }
 
-    /**
-     * 사용자가 이동할 칸을 입력받는다.
-     */
     public static String readMoving() {
         try {
             OutputView.printSelectMovementDirection();
@@ -41,9 +31,6 @@ public class InputView {
         }
     }
 
-    /**
-     * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
-     */
     public static String readGameCommand() {
         try {
             OutputView.printInputRestartOrQuitCommand();
--- src/main/java/bridge/view/OutputView.java
+++ src/main/java/bridge/view/OutputView.java
@@ -3,9 +3,6 @@ package bridge.view;
 import bridge.domain.BridgeGame;
 import bridge.domain.BridgeGameResult;
 
-/**
- * 사용자에게 게임 진행 상황과 결과를 출력하는 역할을 한다.
- */
 public class OutputView {
     private static final String NEW_LINE = System.lineSeparator();
     private static final String GAME_START = "다리 건너기 게임을 시작합니다.";
@@ -33,21 +30,11 @@ public class OutputView {
         System.out.println(INPUT_RESTART_OR_QUIT_COMMAND);
     }
 
-    /**
-     * 현재까지 이동한 다리의 상태를 정해진 형식에 맞춰 출력한다.
-     * <p>
-     * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
-     */
     public static void printMap(BridgeGameResult moveResult) {
         System.out.println(moveResult.getUpperBridge());
         System.out.println(moveResult.getLowerBridge());
     }
 
-    /**
-     * 게임의 최종 결과를 정해진 형식에 맞춰 출력한다.
-     * <p>
-     * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
-     */
     public static void printResult(BridgeGame bridgeGame) {
         System.out.printf(NEW_LINE);
         System.out.println(GAME_RESULT);
