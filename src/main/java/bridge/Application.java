e270217074360e87a5563d6466bc0459b2dc8f78
5nam <ohnam90@gmail.com>
2022-11-22 18:37:31 +0900

    feat: GameController 객체 생성

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -1,47 +1,14 @@
 package bridge;
 
-import bridge.Model.Bridge;
-import bridge.Model.BridgeGame;
-import bridge.Model.BridgeMaker;
-import bridge.UI.InputView;
-import bridge.View.OutputView;
-
-import java.util.ArrayList;
-import java.util.List;
+import bridge.Controller.GameController;
 
 public class Application {
 
     public static void main(String[] args) {
-        BridgeGame bridgeGame = new BridgeGame();
-        BridgeNumberGenerator bridgeNumberGenerator = new BridgeRandomNumberGenerator();
-        BridgeMaker bridgeMaker = new BridgeMaker(bridgeNumberGenerator);
-        InputView inputView = new InputView();
-        OutputView outputView = new OutputView();
+        GameController gameController = new GameController();
 
-        // TODO: 프로그램 구현
-        System.out.println("다리 건너기 게임을 시작합니다.");
-        // 다리 길이 입력
-        int size = inputView.readBridgeSize();
-        List<String> madeBridge = bridgeMaker.makeBridge(size);
-        List<List<String>> userBridge = new ArrayList<>();
-
-        // 방향 입력
-        while (bridgeGame.getCount() <= size) {
-            String userDirection = inputView.readMoving();
-            userBridge = bridgeGame.move(madeBridge, userDirection);
-            // 다리 그리기
-            outputView.printMap(userBridge);
-
-            // 틀렸는지 체크
-            if (!Bridge.getInstance.isRightAnswer()) {
-                String cont = inputView.readGameCommand();
-                if (!bridgeGame.retry(cont)) {
-                    break;
-                }
-            }
-        }
-
-        // 최종 결과
-        outputView.printResult(userBridge, bridgeGame.getGameResult(), bridgeGame.getTried());
+        gameController.start();
+        gameController.progress();
+        gameController.end();
     }
 }
---
+++ src/main/java/bridge/Controller/GameController.java
@@ -0,0 +1,63 @@
+package bridge.Controller;
+
+import bridge.BridgeNumberGenerator;
+import bridge.BridgeRandomNumberGenerator;
+import bridge.Model.Bridge;
+import bridge.Model.BridgeGame;
+import bridge.Model.BridgeMaker;
+import bridge.UI.InputView;
+import bridge.View.OutputView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class GameController {
+    BridgeGame bridgeGame = new BridgeGame();
+    BridgeNumberGenerator bridgeNumberGenerator = new BridgeRandomNumberGenerator();
+    BridgeMaker bridgeMaker = new BridgeMaker(bridgeNumberGenerator);
+    InputView inputView = new InputView();
+    OutputView outputView = new OutputView();
+    int size;
+    List<String> madeBridge;
+    List<List<String>> userBridge = new ArrayList<>();
+
+
+    public void start() {
+        // TODO: 프로그램 구현
+        System.out.println("다리 건너기 게임을 시작합니다.");
+        // 다리 길이 입력
+        size = inputView.readBridgeSize();
+        madeBridge = bridgeMaker.makeBridge(size);
+    }
+
+    public void progress() {
+        // 방향 입력
+        while (bridgeGame.getCount() <= size) {
+            String userDirection = inputView.readMoving();
+            userBridge = bridgeGame.move(madeBridge, userDirection);
+            // 다리 그리기
+            outputView.printMap(userBridge);
+
+            if(!wrongCheck()) {
+                return;
+            }
+        }
+    }
+
+    private boolean wrongCheck() {
+        // 틀렸는지 체크
+        if (!Bridge.getInstance.isRightAnswer()) {
+            String cont = inputView.readGameCommand();
+            if (!bridgeGame.retry(cont)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public void end() {
+        // 최종 결과
+        outputView.printResult(userBridge, bridgeGame.getGameResult(), bridgeGame.getTried());
+
+    }
+}
