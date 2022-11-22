3685c12d5d56b8817ae9be2cb409e1b6271ecc46
gnoesnooj <306yyy@naver.com>
2022-11-22 09:16:08 +0800

    feat(CommandOption) : enum 활용한 command 조건 구현

---
+++ src/main/java/bridge/rule/CommandOption.java
@@ -0,0 +1,17 @@
+package bridge.rule;
+
+public enum CommandOption {
+
+    RESTART("R"),
+    QUIT("Q");
+
+    private final String command;
+
+    CommandOption(String command) {
+        this.command = command;
+    }
+
+    public static boolean isCorrectCommand(String command) {
+        return QUIT.command.equals(command) || RESTART.command.equals(command);
+    }
+}
