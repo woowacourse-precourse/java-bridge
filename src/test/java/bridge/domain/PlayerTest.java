2d9fd89bfaa9b051fc881721c6fff44ae4e0e4d2
shpark30 <seohyeonpark30.gmail.com>
2022-11-22 16:18:40 +0900

    refactor: 최종 게임 승리 여부 검사 코드 중복 제거

--- src/test/java/bridge/domain/PlayerTest.java
+++ src/test/java/bridge/domain/PlayerTest.java
@@ -2,14 +2,11 @@ package bridge.domain;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.util.Lists.newArrayList;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.util.List;
 import java.util.stream.Stream;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Nested;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
@@ -27,7 +24,7 @@ class PlayerTest {
     }
 
     @DisplayName("정답이 U일 때 정답/오답을 입력한 경우 각각 게임 성공 여부 true/false")
-    @MethodSource("ProvideAnswer")
+    @MethodSource("provideAnswer")
     @ParameterizedTest
     void testAliveWhenTakeRightOrWrongAnswers(String command, boolean expected) {
         Player player = new Player(new Map());
@@ -35,45 +32,29 @@ class PlayerTest {
         assertEquals(expected, player.isAlive());
     }
 
-    private static Stream<Arguments> ProvideAnswer() {
+    private static Stream<Arguments> provideAnswer() {
         return Stream.of(
                 Arguments.of("U", true),
                 Arguments.of("D", false)
         );
     }
 
-    @Nested
-    class SucceedTest {
-        private List<String> bridge = newArrayList("U", "U", "D");
-
-        @DisplayName("최종 정답")
-        @Test
-        void right() {
-            Player player = new Player(new Map());
-            player.move("U", "U");
-            player.move("U", "U");
-            player.move("D", "D");
-
-            assertThat(player.hasSucceeded(bridge)).isEqualTo(true);
+    @DisplayName("최종 게임 승리 여부 검사")
+    @MethodSource("provideSucceedArguments")
+    @ParameterizedTest(name = "{index}: {3}")
+    void testSucceed(List<String> bridge, List<String> inputs, boolean expected, String message) {
+        Player player = new Player(new Map());
+        for (int i = 0; i < bridge.size(); i++) {
+            player.move(inputs.get(i), bridge.get(i));
         }
+        assertThat(player.hasSucceeded(bridge)).isEqualTo(expected);
+    }
 
-        @DisplayName("최종 오답: 끝까지 가지 전에 탈락")
-        @Test
-        void wrong1() {
-            Player player = new Player(new Map());
-            player.move("U", "U");
-            player.move("U", "D");
-            assertThat(player.hasSucceeded(bridge)).isEqualTo(false);
-        }
-
-        @DisplayName("최종 오답: 끝까지 가서 탈락")
-        @Test
-        void wrong2() {
-            Player player = new Player(new Map());
-            player.move("U", "U");
-            player.move("U", "U");
-            player.move("U", "D");
-            assertThat(player.hasSucceeded(bridge)).isEqualTo(false);
-        }
+    private static Stream<Arguments> provideSucceedArguments() {
+        return Stream.of(
+                Arguments.of(List.of("U", "U", "D"), List.of("U", "U", "D"), true, "최종 정답"),
+                Arguments.of(List.of("U","D"), List.of("U","U"), false, "다리 끝에 도달 전에 오답"),
+                Arguments.of(List.of("U", "U", "D"), List.of("U", "U", "U"), false, "다리 끝에서 오답")
+        );
     }
 }
