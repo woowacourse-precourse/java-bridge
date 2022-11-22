2f68703ec10ff04822b4df9fad93e374867cf51b
CMHyuk <alsgur9710@naver.com>
2022-11-22 11:56:55 +0900

    feat(ClassConfig): Bridge 객체 반환

--- src/main/java/bridge/config/ClassConfig.java
+++ src/main/java/bridge/config/ClassConfig.java
@@ -1,20 +1,25 @@
 package bridge.config;
 
-import bridge.BridgeGame;
+import bridge.domain.BridgeGame;
 import bridge.BridgeMaker;
 import bridge.BridgeRandomNumberGenerator;
 import bridge.controller.BridgeController;
+import bridge.domain.Bridge;
 import bridge.view.InputView;
 import bridge.view.OutputView;
 import validation.Validator;
 
 public class ClassConfig {
+    public Bridge bridge() {
+        return new Bridge();
+    }
+
     public InputView inputView() {
         return new InputView(new Validator());
     }
 
     public OutputView outputView() {
-        return new OutputView();
+        return new OutputView(bridge());
     }
 
     public BridgeMaker bridgeMaker() {
