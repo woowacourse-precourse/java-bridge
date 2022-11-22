d880d6a827416cdb0be3a4b664bb766e6095c5a3
yangjiyoung <0112jyoung@naver.com>
2022-11-22 12:59:19 +0900

    style: 주석 제거

--- src/main/java/bridge/InputView.java
+++ src/main/java/bridge/InputView.java
@@ -7,9 +7,6 @@ import camp.nextstep.edu.missionutils.Console;
  */
 public class InputView {
 
-    /**
-     * 다리의 길이를 입력받는다.
-     */
     public static String readBridgeSize() {
         String inputSize = Console.readLine();
         try{
@@ -22,9 +19,6 @@ public class InputView {
         return inputSize;
     }
 
-    /**
-     * 사용자가 이동할 칸을 입력받는다.
-     */
     public static String readMoving() {
         System.out.println(Constant.SELECT_MOVE_MESSAGE);
         String moveWord = Console.readLine();
@@ -37,9 +31,6 @@ public class InputView {
         return moveWord;
     }
 
-    /**
-     * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
-     */
     public static String readGameCommand() {
         System.out.println(Constant.SELECT_RETRY_MESSAGE);
         String retryWord = Console.readLine();
