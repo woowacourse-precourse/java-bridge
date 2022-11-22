4e24db727a0eabb98b2040bda5953f7e54e6e31d
HamSeungChan <head0618@naver.com>
2022-11-22 16:07:03 +0900

    refactor : 이동 결과 출력 기능 수정

--- src/main/java/bridge/view/OutputView.java
+++ src/main/java/bridge/view/OutputView.java
@@ -1,16 +1,23 @@
 package bridge.view;
 
 import java.util.List;
+import java.util.stream.Collectors;
+
 
 public class OutputView {
 
     private static final String PRINT_GAME_START_MESSAGE = "다리 건너기 게임을 시작합니다.\n";
     private static final String INPUT_BRIDGE_SIZE_MESSAGE = "다리의 길이를 입력해주세요.";
     private static final String INPUT_MOVE_DIRECTION_MESSAGE = "\n이동할 칸을 선택해주세요. (위: U, 아래: D)";
-    private static final String BridgeStart = "[ ";
-    private static final String BridgeEnd = "]\n";
+    private static final String INPUT_GAME_RESTART_MESSAGE = "게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)";
+    private static final String BRIDGE_PREFIX = "[ ";
+    private static final String BRIDGE_DELIMITER = " | ";
+    private static final String BRIDGE_SUFFIX = " ]";
 
-    public void printMap() {
+    public static void printMap(List<List<String>> map) {
+        for (List<String> mapLayer : map)
+            System.out.println(mapLayer.stream().
+                    collect(Collectors.joining(BRIDGE_DELIMITER, BRIDGE_PREFIX, BRIDGE_SUFFIX)));
     }
 
     public void printResult() {
@@ -28,13 +35,8 @@ public class OutputView {
         System.out.println(INPUT_MOVE_DIRECTION_MESSAGE);
     }
 
-    public static void printMoveResult(List<List<String>> map) {
-        for(List<String> layer : map){
-            System.out.print(BridgeStart);
-            for(String result : layer){
-                System.out.print(result+" ");
-            }
-            System.out.print(BridgeEnd);
-        }
+    public static void inputGameRestart() {
+        System.out.println(INPUT_GAME_RESTART_MESSAGE);
     }
+
 }
