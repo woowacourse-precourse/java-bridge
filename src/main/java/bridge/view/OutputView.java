fc1b7f57b9c6b8d04ded7f52c05e0aaed0589b2e
hyejuc <chjjudy@naver.com>
2022-11-22 23:17:31 +0900

    feat: 게임 종료 문구 기능 분리

--- src/main/java/bridge/view/OutputView.java
+++ src/main/java/bridge/view/OutputView.java
@@ -55,10 +55,12 @@ public class OutputView {
      * <p>
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void printResult(List<String> upBridgeResult, List<String> downBridgeResult, String gameStatus, int cnt) {
+    public void printResult(List<String> upBridgeResult, List<String> downBridgeResult) {
         System.out.println("최종 게임 결과");
         printCrossResult(upBridgeResult, downBridgeResult);
         System.out.println();
+    }
+    public void printGameResult(String gameStatus, int cnt){
         System.out.println("게임 성공 여부: " + gameStatus);
         System.out.println("총 시도한 횟수: " + cnt);
     }
