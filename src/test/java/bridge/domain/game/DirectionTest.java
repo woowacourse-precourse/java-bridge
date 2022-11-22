724e78b44ecfac331aa11396df3d20c99fedcd65
minju412 <ln8847@ajou.ac.kr>
2022-11-22 22:39:02 +0900

    test(DirectionTest): change test
    - 중복을 제거하기 위해 변경

--- src/test/java/bridge/domain/game/DirectionTest.java
+++ src/test/java/bridge/domain/game/DirectionTest.java
@@ -3,7 +3,6 @@ package bridge.domain.game;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
@@ -11,16 +10,9 @@ class DirectionTest {
 
     @DisplayName("방향이 U 또는 D 가 아니라면 예외가 발생한다.")
     @ParameterizedTest
-    @CsvSource({"'r'", "'4'", "'!'", "'u'"})
+    @CsvSource({"'r'", "'4'", "'!'", "'u'", "''", "' '"})
     void createDirection_with_invalid_format(String direction) {
         assertThatThrownBy(() -> new Direction(direction))
-                .isInstanceOf(IllegalArgumentException.class);
-    }
-
-    @DisplayName("방향이 공백이라면 예외가 발생한다.")
-    @Test
-    void createDirection_with_blank() {
-        assertThatThrownBy(() -> new Direction(""))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 }
