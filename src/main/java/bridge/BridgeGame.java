40f49f07f558019f140dfabd513ca28aa00e79f9
xxeol2 <jhj01177@naver.com>
2022-11-22 17:23:10 +0900

    refactor: map 계산 로직 BridgeGame으로 이동

--- src/main/java/bridge/BridgeGame.java
+++ src/main/java/bridge/BridgeGame.java
@@ -1,6 +1,15 @@
 package bridge;
 
+import static bridge.Moving.DOWN;
+import static bridge.Moving.UP;
+import static bridge.constant.BridgeSymbol.MOVING_EMPTY;
+import static bridge.constant.BridgeSymbol.MOVING_O;
+import static bridge.constant.BridgeSymbol.MOVING_X;
+
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * 다리 건너기 게임을 관리하는 클래스
@@ -10,6 +19,7 @@ public class BridgeGame {
     private final List<String> bridge;
     private int round = 0;
     private int trialCount = 1;
+    private boolean success = false;
 
     public BridgeGame(List<String> bridge) {
         this.bridge = bridge;
@@ -23,7 +33,9 @@ public class BridgeGame {
     public boolean move(String moving) {
         increaseRound();
         String row = bridge.get(round - 1);
-        return moving.equals(row);
+        boolean result = moving.equals(row);
+        this.success = result;
+        return result;
     }
 
     /**
@@ -46,5 +58,44 @@ public class BridgeGame {
 
     private void resetGame() {
         this.round = 0;
+    }
+
+    public int getBridgeSize() {
+        return bridge.size();
+    }
+
+    public int getTrialCount() {
+        return trialCount;
+    }
+
+    public Map<Moving, List<String>> getMap() {
+        Map<Moving, List<String>> map = new HashMap<>();
+        map.put(UP, new ArrayList<>());
+        map.put(DOWN, new ArrayList<>());
+        for (int i = 0; i < round - 1; i++) {
+            cross(map, Moving.of(bridge.get(i)), true);
+        }
+        cross(map, Moving.of(bridge.get(round - 1)), success);
+        return map;
+    }
+
+
+    private void cross(Map<Moving, List<String>> row, Moving moving, boolean isCorrectMoving) {
+        Moving otherSide = getOtherSide(moving);
+        if (isCorrectMoving) {
+            row.get(otherSide).add(MOVING_EMPTY);
+            row.get(moving).add(MOVING_O);
+            return;
+        }
+        row.get(otherSide).add(MOVING_X);
+        row.get(moving).add(MOVING_EMPTY);
+    }
+
+    private Moving getOtherSide(Moving moving) {
+        return Moving.values()[(moving.ordinal() + 1) % 2];
+    }
+
+    public boolean isSuccess() {
+        return success;
     }
 }
--- src/main/java/bridge/view/OutputView.java
+++ src/main/java/bridge/view/OutputView.java
@@ -5,15 +5,12 @@ import static bridge.Moving.UP;
 import static bridge.constant.BridgeSymbol.BRIDGE_DELIMITER;
 import static bridge.constant.BridgeSymbol.BRIDGE_END;
 import static bridge.constant.BridgeSymbol.BRIDGE_START;
-import static bridge.constant.BridgeSymbol.MOVING_EMPTY;
-import static bridge.constant.BridgeSymbol.MOVING_O;
-import static bridge.constant.BridgeSymbol.MOVING_X;
 import static bridge.constant.Message.GAME_START;
 import static bridge.constant.Message.INPUT_BRIDGE_SIZE;
+import static bridge.constant.Message.INPUT_GAME_COMMAND;
+import static bridge.constant.Message.INPUT_MOVING;
 
 import bridge.Moving;
-import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -28,35 +25,13 @@ public class OutputView {
      * <p>
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void printMap(List<String> bridge, int round, boolean result) {
-        Map<Moving, List<String>> row = new HashMap<>();
-        row.put(UP, new ArrayList<>());
-        row.put(DOWN, new ArrayList<>());
-        for (int i = 0; i < round - 1; i++) {
-            cross(row, Moving.of(bridge.get(i)), true);
-        }
-        cross(row, Moving.of(bridge.get(round - 1)), result);
-        printRow(row.get(UP));
-        printRow(row.get(DOWN));
+    public void printMap(Map<Moving, List<String>> map) {
+        printRow(map.get(UP));
+        printRow(map.get(DOWN));
     }
 
     private void printRow(List<String> row) {
         System.out.println(BRIDGE_START + String.join(BRIDGE_DELIMITER, row) + BRIDGE_END);
-    }
-
-    private void cross(Map<Moving, List<String>> row, Moving moving, boolean isCorrectMoving) {
-        Moving otherSide = getOtherSide(moving);
-        if (isCorrectMoving) {
-            row.get(otherSide).add(MOVING_EMPTY);
-            row.get(moving).add(MOVING_O);
-            return;
-        }
-        row.get(otherSide).add(MOVING_X);
-        row.get(moving).add(MOVING_EMPTY);
-    }
-
-    private Moving getOtherSide(Moving moving) {
-        return Moving.values()[(moving.ordinal() + 1) % 2];
     }
 
     /**
@@ -73,5 +48,13 @@ public class OutputView {
 
     public void printBridgeSizeInputMessage() {
         System.out.println(INPUT_BRIDGE_SIZE);
+    }
+
+    public void printMovingInputMessage() {
+        System.out.println(INPUT_MOVING);
+    }
+
+    public void printGameCommandInputMessage() {
+        System.out.println(INPUT_GAME_COMMAND);
     }
 }
