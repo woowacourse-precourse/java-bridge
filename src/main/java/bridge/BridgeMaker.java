6b7813423c299bd0e964287ac2774a825adc7cfa
dntmdals <a01030521713@gmail.com>
2022-11-22 21:13:44 +0900

    new
    git commit -m new

--- src/main/java/bridge/BridgeMaker.java
+++ src/main/java/bridge/BridgeMaker.java
@@ -1,23 +1,121 @@
 package bridge;
 
-import java.util.List;
+import java.util.*;
 
 /**
  * 다리의 길이를 입력 받아서 다리를 생성해주는 역할을 한다.
  */
 public class BridgeMaker {
 
-    private final BridgeNumberGenerator bridgeNumberGenerator;
 
-    public BridgeMaker(BridgeNumberGenerator bridgeNumberGenerator) {
-        this.bridgeNumberGenerator = bridgeNumberGenerator;
+
+
+
+    
+    
+    public static int CreateN()
+    {   
+        Random rd = new Random();
+        int n = rd.nextInt(1); 
+        return n;
+    }
+    
+    public static boolean[][] makeBridge(int size) {
+        boolean array[][];
+        array= new boolean[2][size];
+        int n;
+        boolean m;
+        for(int i=0;i<size;i++){
+            n=CreateN();
+            m=reverse(n);
+
+            array[0][i]=m;
+            array[1][i]=!m;
+        }
+        
+        return array; 
     }
 
-    /**
-     * @param size 다리의 길이
-     * @return 입력받은 길이에 해당하는 다리 모양. 위 칸이면 "U", 아래 칸이면 "D"로 표현해야 한다.
-     */
-    public List<String> makeBridge(int size) {
-        return null;
+
+    public static boolean reverse(int n){
+        //n=CreateN();
+        boolean m;
+        
+        if (n==1){
+            m=true;
+        }
+        else{
+            m=false;
+        }
+
+        return m;
     }
+
+    public static int startGame(String move,boolean [][] array,int j)
+    {
+        int i;
+        
+        if (move=="U"){
+
+            i=upMove(array,j);
+        }
+        
+        else if(move=="D"){
+            i=downMove(array,j);
+        }
+        else{
+
+            System.out.println("[ERROR] 입력값이 틀렸습니다.");
+            return 0;
+        }
+
+        return i;
+    }
+
+    public static int upMove(boolean [][] array,int i){
+        //boolean [][] array=makeBridge(int size)
+        
+        if(array[0][i]==false){
+            i=0;
+        }
+
+        else{
+
+            i++;
+
+        }
+        return i;
+            
+        
+    }
+
+    public static int downMove(boolean [][] array,int i){
+        
+        
+        if(array[1][i]==true){
+            
+            i++;
+
+        }
+
+        else{
+            i=0;
+        }
+
+        return i;
+            
+        
+    }
+
+
+
+
+
+
+
+
+
+        
 }
+
+
--- src/main/java/bridge/InputView.java
+++ src/main/java/bridge/InputView.java
@@ -1,28 +1,44 @@
 package bridge;
-
+import java.util.*;
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
  */
 public class InputView {
 
-    /**
-     * 다리의 길이를 입력받는다.
-     */
-    public int readBridgeSize() {
-        return 0;
+    
+
+
+
+
+
+    public static int readBridgeSize() //다리 길이 입력받기
+    {   
+        Scanner sc = new Scanner(System.in);
+        System.out.println("다리길이를 입력하시오.");
+        int bridgeLength=sc.nextInt();
+       
+        return bridgeLength;
+    }
+    
+    
+    public static String readMoving(){ //이동할 칸 선택하기
+        Scanner sc = new Scanner(System.in);
+        System.out.println("이동할 칸을 선택 하시오  (위: U,아래: D)");
+        String moveBridge=sc.nextLine();
+        return moveBridge;
     }
 
-    /**
-     * 사용자가 이동할 칸을 입력받는다.
-     */
-    public String readMoving() {
-        return null;
+    
+    public static String readGameCommand  (){ //게임 재시작 여부결정
+        Scanner sc = new Scanner(System.in);
+        System.out.println("게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)");
+        String regame=sc.nextLine();
+        return regame;
     }
 
-    /**
-     * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
-     */
-    public String readGameCommand() {
-        return null;
-    }
+
+
+    
 }
+
+
