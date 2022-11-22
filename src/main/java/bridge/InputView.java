51c087836735583f87ff0b914d7cefea8000311b
0112leesy <0112leesy@naver.com>
2022-11-22 15:54:34 +0900

    refactor(InputView) : InputValidation 클래스 활용
    기존 검증 코드를 InputValidation 클래스 내 함수들로 변경

--- src/main/java/bridge/InputView.java
+++ src/main/java/bridge/InputView.java
@@ -7,16 +7,17 @@ import static camp.nextstep.edu.missionutils.Console.readLine;
  */
 public class InputView {
 
+    static InputValidation inputValidation = new InputValidation();
     /**
      * 다리의 길이를 입력받는다.
      */
     public int readBridgeSize() throws IllegalArgumentException {
         System.out.println("다리의 길이를 입력해주세요.");
         String input = readLine();
-        if (!input.matches("^[0-9]*$")) {
-            throw new IllegalArgumentException("[ERROR] <임시 에러 메시지>");
-        }
+        inputValidation.checkBlank(input);
+        inputValidation.checkNumeric(input);
         int bridgeSize = Integer.parseInt(input);
+        inputValidation.checkBridgeSize(bridgeSize);
         return bridgeSize;
     }
 
@@ -26,9 +27,7 @@ public class InputView {
     public String readMoving() throws IllegalArgumentException {
         System.out.println("이동할 칸을 선택해주세요. (위: U, 아래: D)");
         String input = readLine();
-        if (!input.equals("U") && !input.equals("D")) {
-            throw new IllegalArgumentException("[ERROR] <임시 에러 메시지>");
-        }
+        inputValidation.checkMoving(input);
         return input;
     }
 
@@ -38,9 +37,7 @@ public class InputView {
     public String readGameCommand() throws IllegalArgumentException {
         System.out.println("게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)");
         String input = readLine();
-        if (!input.equals("R") && !input.equals("Q")) {
-            throw new IllegalArgumentException("[ERROR] <임시 에러 메시지>");
-        }
+        inputValidation.checkGameCommand(input);
         return input;
     }
 }
