2c2ba75b3bed43d4545064f5f8be88d8551034d2
yeop0740 <ssucgoo@gmail.com>
2022-11-22 22:17:03 +0900

    feat(BridgeGame): 입력받은 사용자의 움직임을 생성된 다리와 비교하여 기록하는 기능 구현(move)

--- src/main/java/bridge/BridgeGame.java
+++ src/main/java/bridge/BridgeGame.java
@@ -1,5 +1,7 @@
 package bridge;
 
+import java.util.List;
+
 /**
  * 다리 건너기 게임을 관리하는 클래스
  */
@@ -10,7 +12,15 @@ public class BridgeGame {
      * <p>
      * 이동을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void move() {
+    public boolean move(String moving, List<String> bridge, List<String> states) {
+        if (moving.equals(bridge.get(states.size()))) {
+            states.add("O");
+            if (states.size() == bridge.size())
+                return false;
+            return true;
+        }
+        states.add("X");
+        throw new IllegalStateException();
     }
 
     /**
