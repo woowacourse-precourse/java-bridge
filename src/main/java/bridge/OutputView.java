5df17411a1efba83e19dcdd05f6bed70c8361b83
c1ce <chalicenuclear.gmail.com>
2022-11-22 20:42:40 +0900

    feat: 게임 종료이후 성공 결과 출력을 위한 OutputView 생성

--- src/main/java/bridge/OutputView.java
+++ src/main/java/bridge/OutputView.java
@@ -13,26 +13,30 @@ public class OutputView {
      * <p>
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    private List<List<String>> mapList;
+    private List<ArrayList<String>> mapList;
 
     public OutputView() {
         mapList = new ArrayList<>();
-        mapList.add(List.of());
-        mapList.add(List.of());
-        System.out.println("CHECK"+mapList);
+        mapList.add(new ArrayList<>());
+        mapList.add(new ArrayList<>());
     }
 
-    public void printMap(BirdgeMoveType isPossibleMove) {
+    public boolean printMap(BirdgeMoveType isPossibleMove,InputView inputView,BridgeGame bridgeGame) {
 
-        List<List<String>> move=checkIndex(isPossibleMove);
-        List<String> resultList = printMapBorder(move);
+        checkIndex(isPossibleMove);
+
+        printMapDetail(mapList);
+        return checkPrintResult(inputView,bridgeGame,isPossibleMove);
+    }
+    public void printMapDetail(List<ArrayList<String>> map){
+        List<String> resultList = printMapBorder(map);
         for (String result:
                 resultList) {
-            System.out.print(result);
+            System.out.println(result);
         }
         System.out.println("\n");
     }
-    public List<List<String>> checkIndex(BirdgeMoveType whereMove){
+    public List<ArrayList<String>> checkIndex(BirdgeMoveType whereMove){
 
         if(whereMove.getIndex()==0){
             mapList.get(whereMove.getIndex()).add("| "+whereMove.getCode()+" ");
@@ -43,12 +47,10 @@ public class OutputView {
         }
         return mapList;
     }
-    public List<String> printMapBorder(List<List<String>> mapView){
+    public List<String> printMapBorder(List<ArrayList<String>> mapView){
         List<String> result=new ArrayList<>();
-        System.out.println("C  "+mapView);
         for (List<String> map: mapView) {
             String resultText="";
-            System.out.println(map);
             for (String mapText: map) { resultText+=mapText; }
             String formatString="["+resultText.substring(1)+"]";
             result.add(formatString);
@@ -61,6 +63,21 @@ public class OutputView {
      * <p>
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void printResult() {
+    public void printResult(BridgeGame bridgeGame) {
+        System.out.println(String.format("총 시도한 횟수: %d",bridgeGame.getTryCount()));
+    }
+    public boolean checkPrintResult(InputView inputView,BridgeGame bridgeGame,BirdgeMoveType isPossibleMove){
+        printLastResult();
+        if(inputView.checkBridgeSize(mapList.get(0).size())&&isPossibleMove.getCode().equals("O")){
+            System.out.println("게임 성공 여부: 성공");
+            printResult(bridgeGame);
+            return true;
+        }
+        return false;
+    }
+
+    public void printLastResult(){
+        System.out.println("최종 게임 결과");
+        printMapDetail(mapList);
     }
 }
