7211cfa0c07fc2ef18d15608e77f8e3e0445a8aa
Amenable-C <duswo0624@naver.com>
2022-11-22 22:24:58 +0900

    refactor : import 정리

--- src/test/java/bridge/ExceptionTest.java
+++ src/test/java/bridge/ExceptionTest.java
@@ -1,14 +1,10 @@
 package bridge;
 
 import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeTest;
-import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
-import static org.assertj.core.util.Lists.newArrayList;
 
 import camp.nextstep.edu.missionutils.test.NsTest;
 import java.util.List;
-import java.util.Scanner;
 import org.junit.jupiter.api.Test;
 
 public class ExceptionTest extends NsTest {
--- src/test/java/bridge/NormalWorkingTest.java
+++ src/test/java/bridge/NormalWorkingTest.java
@@ -1,11 +1,9 @@
 package bridge;
 
 import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeTest;
-import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.util.Lists.newArrayList;
 
-import bridge.ApplicationTest.TestNumberGenerator;
 import camp.nextstep.edu.missionutils.test.NsTest;
 import java.util.List;
 import org.junit.jupiter.api.Test;
