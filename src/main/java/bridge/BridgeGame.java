9feeefaae9a01755490d987cf72ecad6acabad53
kyunghyun <rudgusee@gmail.com>
2022-11-22 21:52:34 +0900

    게임의 메인 움직임을 관리

--- src/main/java/bridge/BridgeGame.java
+++ src/main/java/bridge/BridgeGame.java
@@ -33,6 +33,7 @@ public class BridgeGame {
         return false;
     }
     public boolean downBridgeMove(){
+        
         if (downBridgeMoveCheck()) {
             GameControl.resultBridge.add("O_down");
             position++;
