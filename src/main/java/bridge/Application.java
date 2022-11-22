69c476f5222c8dd81b5c5a02ac94d861fad5e390
YEJIN <yejin9858@sookmyung.ac.kr>
2022-11-22 23:09:05 +0900

    [FEAT] 다리 길이 입력 예외처리

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -4,11 +4,23 @@ import java.util.List;
 
 public class Application {
 
+    private static int inputBridgeSize(InputView inputView) {
+        int bridgeSize = 0;
+        while (true) {
+            try {
+                bridgeSize = inputView.readBridgeSize();
+                break;
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            }
+        }
+        return bridgeSize;
+    }
+
     public static void main(String[] args) {
         InputView inputView = new InputView();
         BridgeMaker bridgeMaker = new BridgeMaker(new BridgeRandomNumberGenerator());
-        List<String> bridge = bridgeMaker.makeBridge(inputView.readBridgeSize());
-
+        List<String> bridge = bridgeMaker.makeBridge(inputBridgeSize(inputView));
 
 
 
--- src/main/java/bridge/InputView.java
+++ src/main/java/bridge/InputView.java
@@ -10,15 +10,17 @@ public class InputView {
     /**
      * 다리의 길이를 입력받는다.
      */
-    public int readBridgeSize() {
+    public int readBridgeSize() throws IllegalArgumentException{
         int bridgeSize = 0;
+        String errorMsg = "[ERROR] 잘못된 다리 길이 입력, 다시 입력하세요";
         try {
             bridgeSize = Integer.parseInt(Console.readLine());
         }catch (Exception e){
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException(errorMsg);
         }
-        if(bridgeSize < 0)
-            throw new IllegalArgumentException();
+        if (bridgeSize < 3 || bridgeSize > 20) {
+            throw new IllegalArgumentException(errorMsg);
+        }
         return bridgeSize;
     }
 
@@ -26,7 +28,10 @@ public class InputView {
      * 사용자가 이동할 칸을 입력받는다.
      */
     public String readMoving() {
-        return null;
+        String answer = Console.readLine();
+        if(answer.equals("U") || answer.equals("D"))
+            return answer;
+        throw new IllegalArgumentException("[ERROR] 잘못된 이동할 칸 입력");
     }
 
     /**
