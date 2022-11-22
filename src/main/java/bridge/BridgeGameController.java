9b799829eb4f457e10024a78173fdb0af9886f79
KSB-skyBlueLover <gogotkdqja@yonsei.ac.kr>
2022-11-22 16:18:32 +0900

    feat: add a bridge game controller and implemented main

---
+++ src/main/java/bridge/BridgeGameController.java
@@ -0,0 +1,60 @@
+package bridge;
+
+import java.util.List;
+
+public class BridgeGameController {
+    private BridgeGame bridgeGame;
+    private BridgeMaker bridgeMaker;
+    private BridgeNumberGenerator bridgeNumberGenerator;
+    private InputView inputView;
+    private OutputView outputView;
+    private STATUS status;
+    private int totalTries;
+
+    public BridgeGameController() {
+    }
+
+    public void init() {
+        this.status = STATUS.RUNNING;
+        this.bridgeNumberGenerator = new BridgeRandomNumberGenerator();
+        this.bridgeMaker = new BridgeMaker(this.bridgeNumberGenerator);
+        this.inputView = new InputView();
+        this.outputView = new OutputView();
+        run();
+        outputView.printResult(bridgeGame.getUps(), bridgeGame.getDowns(), status.getSuccess(), totalTries);
+    }
+
+    public void run() {
+        this.totalTries = 0;
+        setBridgeGame();
+        while(status == STATUS.RUNNING) {
+            totalTries++;
+            while (status == STATUS.RUNNING)
+                moves();
+            checkContinue();
+        }
+    }
+
+    public void setBridgeGame() {
+        outputView.printGreeting();
+        int size = inputView.readBridgeSize();
+        System.out.println();
+        this.bridgeGame = new BridgeGame(this.bridgeMaker, size);
+    }
+
+    public void moves() {
+        bridgeGame.move(inputView.readMoving());
+        outputView.printMap(bridgeGame.getUps(), bridgeGame.getDowns());
+        status = bridgeGame.getStatus();
+    }
+    public void checkContinue() {
+        if (status == STATUS.END_WITH_SUCCESS) {
+            return;
+        }
+        boolean keepRunning = inputView.readGameCommand();
+        if (keepRunning) {
+            status = STATUS.RUNNING;
+            bridgeGame.retry();
+        }
+    }
+}
