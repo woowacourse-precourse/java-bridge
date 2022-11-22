9950d6891a38419c40f76fae0d0e94928b9b92af
김형기 <khk211113@naver.com>
2022-11-22 18:58:36 +0900

    docs: 열거형 주석 추가

--- src/main/java/bridge/domain/model/GameStatus.java
+++ src/main/java/bridge/domain/model/GameStatus.java
@@ -1,5 +1,8 @@
 package bridge.domain.model;
 
+/**
+ * 게임의 상태를 나타내는 열거형
+ */
 public enum GameStatus {
     WIN, LOSE, RUNNING
 }
--- src/main/java/bridge/domain/model/MoveResult.java
+++ src/main/java/bridge/domain/model/MoveResult.java
@@ -1,5 +1,8 @@
 package bridge.domain.model;
 
+/**
+ * 다리 이동 결과를 나타내는 열거형
+ */
 public enum MoveResult {
     SUCCESS, FAIL
 }
