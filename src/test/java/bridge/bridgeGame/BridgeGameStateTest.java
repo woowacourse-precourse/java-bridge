ce29c885ecc1daa3cf47a118ef82dd04804d8e04
thuthi <whquddn55@gmail.com>
2022-11-22 20:01:31 +0900

    test(bridgeGame): BridgeGameState의 currentSize 메서드 단위 테스트 케이스 추가

--- src/test/java/bridge/bridgeGame/BridgeGameStateTest.java
+++ src/test/java/bridge/bridgeGame/BridgeGameStateTest.java
@@ -2,6 +2,7 @@ package bridge.bridgeGame;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import bridge.bridge.BridgePosition;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -19,5 +20,21 @@ class BridgeGameStateTest {
 
         // then
         assertThat(bridgeSize).isEqualTo(5);
+    }
+
+    @Test
+    public void currentSizeTest() throws Exception {
+        // given
+        List<String> finalState = new ArrayList<>(Arrays.asList("U", "D", "U", "D", "D"));
+        BridgeGameState bridgeGameState = new BridgeGameState(finalState);
+        bridgeGameState.move(BridgePosition.BRIDGE_UP);
+        bridgeGameState.move(BridgePosition.BRIDGE_DOWN);
+        bridgeGameState.move(BridgePosition.BRIDGE_DOWN);
+
+        // when
+        Integer currentSize = bridgeGameState.currentSize();
+
+        // then
+        assertThat(currentSize).isEqualTo(3);
     }
 }
