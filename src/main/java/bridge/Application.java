8e2a7d6830d27d5630c1e84405f7b35160043954
Minsoo Choi <lucas0209@yonsei.ac.kr>
2022-11-22 18:27:02 +0900

    fix(Application): Fix access modifier to private
    Feature fixed:
    - No reason to maintain the method public.

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -89,7 +89,7 @@ public class Application {
         outputView = new OutputView();
     }
 
-    public static void greetings() {
+    private static void greetings() {
         System.out.println("다리 건너기 게임을 시작합니다.\n");
     }
 
