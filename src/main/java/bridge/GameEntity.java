031a2ccc41442ed0951e69ada6d802455170eb83
joohyeongKim <wnguddl96@knou.ac.kr>
2022-11-22 11:04:06 +0900

    feat(GameEntity) : 사용자 칸 이동 기능을 영속성 계층에서 관리

--- src/main/java/bridge/GameEntity.java
+++ src/main/java/bridge/GameEntity.java
@@ -1,34 +1,52 @@
-package bridge.persistence;
+package bridge;
 
-import bridge.BridgeGame;
-import bridge.GameRepository;
-import bridge.ui.UserInterface;
+import static bridge.BridgeStructure.FALSE;
+import static bridge.BridgeStructure.TRUE;
+import static bridge.Expression.DOWN;
+import static bridge.Expression.UP;
+
+import java.util.List;
 
 public class GameEntity extends BridgeGame implements GameRepository {
     private int tryCount;
     private UserInterface userInterface;
+    private StringBuilder upRow;
+    private StringBuilder downRow;
 
     public GameEntity() {
-
-
+        this.upRow = new StringBuilder("[");
+        this.downRow = new StringBuilder("[");
 
     }
 
     @Override
-    public int manageRetryStatus(String gameCommand) {
-        tryCount +=  new BridgeGame().retry(gameCommand);
+    public int manageGameStatus(String gameCommand) {
+        gameCommand = new InputView().readGameCommand();
+        tryCount +=  retry(gameCommand);
 
         return tryCount;
     }
 
+    // 이동 기능
     @Override
-    public int move() {
-        return super.move();
+    public String move() {
+        String readMoving = new InputView().readMoving();
+        List<String> bridgeEntity = new BridgeEntity().manageBridgeStatus();
+        if ( readMoving.equals(UP.expressThat())) {
+            upRow.append(TRUE.buildStructure());
+        }
+
+        if ( readMoving.equals(DOWN.expressThat())) {
+            downRow.append(FALSE.buildStructure());
+        }
+
+        String resultTable = upRow + "\n" + downRow;
+
+        return resultTable;
     }
 
-    public UserInterface getUserInterface() {
-        return userInterface;
-    }
+
+
 
 
 
