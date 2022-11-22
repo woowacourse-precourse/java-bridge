d2c74144b792cbe5869a907cee53f4f51a77319b
leesuengyeol <48343629+syhojeo@users.noreply.github.com>
2022-11-22 15:40:42 +0900

    refactor: MVC 패턴으로 인한 main문 코드 리펙토링

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -4,30 +4,11 @@ import java.util.List;
 
 public class Application {
 
-    private final static InputView inputView = new InputView();
-    private final static BridgeMaker bridgeMaker = new BridgeMaker(new BridgeRandomNumberGenerator());
-    private final static BridgeGame bridgeGame = new BridgeGame();
-
     public static void main(String[] args) {
         // TODO: 프로그램 구현
-        CurrentBridgeState currentBridgeState = new CurrentBridgeState();
-        int tryNumber = 1;
-        boolean isPossibleMove = true;
-        boolean isRetry = true;
-        int bridgeSize = inputView.readBridgeSize();
-        List<String> bridge = bridgeMaker.makeBridge(bridgeSize);
 
-        while (isRetry) {
-            for (int turn = 0; turn < bridgeSize; turn++) {
-                isPossibleMove = bridgeGame.move(bridge, currentBridgeState, bridgeSize);
-                if (isPossibleMove == false) {
-                    isRetry = bridgeGame.retry(currentBridgeState, isPossibleMove, tryNumber);
-                }else if (isPossibleMove == true) {
-                    outputView.printResult(currentBridgeState, isPossibleMove, tryNumber);
-                    return ;
-                }
-            }
-            tryNumber++;
-        }
+        BridgeGame bridgeGame = new BridgeGame();
+
+        bridgeGame.run();
     }
 }
