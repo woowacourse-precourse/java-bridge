d4920df7eda42e48f349c8848d57ac9e11165260
yongseungLeeDec <yonson.dev@gmail.com>
2022-11-22 12:22:17 +0900

    fix(modify): GameExceptionHandler를 싱글턴 패턴으로 변경

--- src/main/java/bridge/exception/GameExceptionHandler.java
+++ src/main/java/bridge/exception/GameExceptionHandler.java
@@ -3,6 +3,19 @@ package bridge.exception;
 public class GameExceptionHandler {
     private static final String ERROR_PREFIX = "[ERROR] ";
 
+    private static GameExceptionHandler instance;
+
+    private GameExceptionHandler() {
+
+    }
+
+    public static GameExceptionHandler getGameExceptionHandler() {
+        if (instance == null) {
+            instance = new GameExceptionHandler();
+        }
+        return instance;
+    }
+
     public void handleIllegalArgumentException(IllegalArgumentException exception) {
         System.out.println(ERROR_PREFIX + exception.getMessage());
     }
