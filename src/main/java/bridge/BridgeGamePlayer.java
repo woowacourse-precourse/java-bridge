1523df9b9b19ea8bae814b104898749faa9e72d5
metorg <metrog950@gmail.com>
2022-11-22 17:58:44 +0900

    feat: 게임 진행 클래스 생성 브릿지 생성 메서드 생성 브릿지 크기 가져오는 메서드 생성

--- src/main/java/bridge/BridgeGamePlayer.java
+++ src/main/java/bridge/BridgeGamePlayer.java
@@ -1,2 +1,39 @@
-package bridge;public class BridgeGamePlayer {
+package bridge;
+
+import static bridge.util.message.GameMessage.*;
+
+import bridge.domain.BridgeMaker;
+import bridge.view.InputView;
+import bridge.view.OutputView;
+
+
+import javax.naming.ldap.StartTlsRequest;
+import java.util.ArrayList;
+import java.util.List;
+
+public class BridgeGamePlayer {
+
+    private static final InputView inputView = new InputView();
+    private static final OutputView outputView = new OutputView();
+
+    public boolean startGame(){
+
+        List<String> bridge = new ArrayList<>();
+        outputView.showMessage(START_MESSAGE.getMessage());
+        bridge = getBridge();
+
+        return true;
+    }
+
+    private static List<String> getBridge(){
+
+        BridgeMaker bridgeMaker = new BridgeMaker(new BridgeRandomNumberGenerator());
+        return bridgeMaker.makeBridge(getBridgeSize());
+    }
+
+    private static int getBridgeSize(){
+        return inputView.readBridgeSize();
+    }
+
+
 }
