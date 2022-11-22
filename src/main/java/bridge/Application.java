c836f0e835745c942ebeacbdaa85b0b4d9a19215
syr9707 <yrshin9771@gmail.com>
2022-11-22 15:12:10 +0900

    feat(bridge main): 메인 함수 구현

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -2,7 +2,20 @@ package bridge;
 
 public class Application {
 
+    public static int BRIDGE_SIZE;
+    public static String MOVING;
+    public static String GAME_COMMAND;
+    private static final String ERROR_MESSAGE = "[ERROR]";
+
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+
+        InputView inputView = new InputView();
+        try {
+            BRIDGE_SIZE = inputView.readBridgeSize();
+        } catch (IllegalArgumentException e) {
+            System.err.println(ERROR_MESSAGE);
+        }
+
     }
 }
