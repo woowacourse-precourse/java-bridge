4c62195e8a5c9149ced1d9a66bdd88c755e31321
hyeonseog-won <smreo75@gmail.com>
2022-11-22 21:30:04 +0900

    feat: 구현한 기능 Controller 적용

--- src/main/java/bridge/controller/BridgeController.java
+++ src/main/java/bridge/controller/BridgeController.java
@@ -1,4 +1,36 @@
 package bridge.controller;
 
+import bridge.model.BridgeGame;
+import bridge.model.BridgeMaker;
+import bridge.model.GameCommandFlag;
+import bridge.model.ResultFlag;
+import bridge.view.ConsolidatedView;
+
+import java.util.List;
+
 public class BridgeController {
+    private final ConsolidatedView consolidatedView;
+    private final BridgeMaker bridgeMaker;
+    private BridgeGame bridgeGame;
+
+    public BridgeController(ConsolidatedView consolidatedView, BridgeMaker bridgeMaker) {
+        this.consolidatedView = consolidatedView;
+        this.bridgeMaker = bridgeMaker;
+    }
+
+    public void startGame() {
+        consolidatedView.printStartMessage();
+        List<String> bridge = bridgeMaker.makeBridge(consolidatedView.inputBridgeSize());
+
+    }
+
+    private void playGame() {
+
+    }
+
+    private GameCommandFlag moveBridge() {
+        bridgeGame.move(consolidatedView.inputMoving());
+        consolidatedView.printMoveMap(bridgeGame.getMap());
+
+    }
 }
