c28a4fbe145f69f5a940cf440c5c8dd9f87bd5ca
Joonsoo Kim <kjsoo1901@gmail.com>
2022-11-22 13:51:48 +0900

    fix: BridgeGame 요구 사항 준수
    BridgeGame에서 InputView 사용 안하도록 변경

--- src/main/java/bridge/domain/BridgeGame.java
+++ src/main/java/bridge/domain/BridgeGame.java
@@ -13,28 +13,28 @@ public class BridgeGame {
     Bridge bridge;
     int bridgeSize;
     int blockNumber;
-    InputView inputView;
 
     public BridgeGame(int bridgeSize){
         player = new Player();
         bridge = new Bridge(bridgeSize);
         this.bridgeSize = bridgeSize;
         blockNumber = 0;
-        inputView = new InputView();
     }
     /**
      * 사용자가 칸을 이동할 때 사용하는 메서드
      * <p>
      * 이동을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void move() {
-        player.MoveTo(inputView.readMoving());
+    public boolean move(String playerMove) {
+        player.MoveTo(playerMove);
         if(!player.isSuccess(bridge, blockNumber))
-            retry();
+            return false;
 
         blockNumber++;
-        if(bridgeSize == blockNumber)
+        if(bridgeSize == blockNumber) {
             success();
+        }
+        return true;
     }
 
     /**
@@ -42,8 +42,7 @@ public class BridgeGame {
      * <p>
      * 재시작을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void retry() {
-        String playerRetry = inputView.readGameCommand();
+    public void retry(String playerRetry) {
         if(playerRetry == PLAYER_RETRY){
             player = new Player();
             blockNumber = 0;
