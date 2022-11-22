b9d19c883b41ed0d9f43fc70d29e3cdb57a6134d
Beginner <dkstmdwn17@naver.com>
2022-11-22 14:49:44 +0900

    refactor : 주석제거 + 변수 명 변경
    StringBuilder의 변수
    sb -> gameProgress 로 변경하였다.
    
    주석을 제거하였다.
    
    메서드 안의 문자들을
    static final로 설정하였다.

--- src/main/java/bridge/view/OutputView.java
+++ src/main/java/bridge/view/OutputView.java
@@ -7,78 +7,76 @@ import java.util.List;
  */
 public class OutputView {
 
-    /**
-     * 현재까지 이동한 다리의 상태를 정해진 형식에 맞춰 출력한다.
-     * <p>
-     * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
-     */
-    private StringBuilder sb;
+    private static final String UP = "U";
+    private static final String DOWN = "D";
+    private static final String LINE = "|";
+    private static final String EMPTY = "   ";
+    private static final String CIRCLE = " O ";
+    private static final String X_MARK = " X ";
+    private static final String FINAL_GAME_RESULTS_MESSAGE = "최종 게임 결과\n";
+    private static final String GAME_SUCCESS = "게임 성공 여부: 성공";
+    private static final String GAME_FAIL = "게임 성공 여부: 실패";
+    private static final String TOTAL_ATTEMPTS_MADE_MESSAGE = "총 시도한 횟수: %d";
+
+    private StringBuilder gameProgress;
     public void printMap(List<String> bridges, int index, boolean isSuccess) {
-        sb = new StringBuilder();
-        sb.append("[");
+        gameProgress = new StringBuilder();
+        gameProgress.append("[");
         printTop(bridges,index,isSuccess);
-        sb.append("]\n");
-        sb.append("[");
+        gameProgress.append("]\n");
+        gameProgress.append("[");
         printDown(bridges,index,isSuccess);
-        sb.append("]\n");
-        System.out.println(sb);
+        gameProgress.append("]\n");
+        System.out.println(gameProgress);
     }
 
     private void printTop(List<String> bridges, int index, boolean isSuccess) {
         for(int i = 0; i <= index; i++){
-            if(i!=0) sb.append("|");
+            if(i!=0) gameProgress.append(LINE);
             // 1. 건널 수 있는 다리인 위쪽을 선택하여 건넜을 경우 O
-            if(index == i && isSuccess && bridges.get(i).equals("U")) sb.append(" O ");
+            if(index == i && isSuccess && bridges.get(i).equals(UP)) gameProgress.append(CIRCLE);
             // 2. 건널 수 있는 다리인 아래쪽을 선택하여 건넜을 경우 빈칸
-            if(index == i && isSuccess && bridges.get(i).equals("D")) sb.append("   ");
+            if(index == i && isSuccess && bridges.get(i).equals(DOWN)) gameProgress.append(EMPTY);
             // 3. 잘못된 다리인 아래쪽을 선택하여 건넜을 경우 빈칸
-            if(index == i && !isSuccess && bridges.get(i).equals("U")) sb.append("   ");
+            if(index == i && !isSuccess && bridges.get(i).equals(UP)) gameProgress.append(EMPTY);
             // 4. 잘못된 다리인 위쪽을 선택하여 건넜을 경우 X
-            if(index == i && !isSuccess && !bridges.get(i).equals("U")) sb.append(" X ");
+            if(index == i && !isSuccess && !bridges.get(i).equals(UP)) gameProgress.append(X_MARK);
             // 이전 다리들 출력하는 것
-            if(index != i && bridges.get(i).equals("U"))sb.append(" O ");
-            if(index != i && !bridges.get(i).equals("U")) sb.append("   ");
+            if(index != i && bridges.get(i).equals(UP)) gameProgress.append(CIRCLE);
+            if(index != i && !bridges.get(i).equals(UP)) gameProgress.append(EMPTY);
         }
     }
 
     private void printDown(List<String> bridges, int index, boolean isSuccess) {
         for(int i = 0; i <= index; i++){
-            if(i!=0) sb.append("|");
+            if(i!=0) gameProgress.append(LINE);
             // 1. 건널 수 있는 다리인 아래쪽을 선택하여 건넜을 경우 O
-            if(index == i && isSuccess && bridges.get(i).equals("D")) sb.append(" O ");
+            if(index == i && isSuccess && bridges.get(i).equals(DOWN)) gameProgress.append(CIRCLE);
             // 2. 건널 수 있는 다리인 위쪽을 선택하여 건넜을 경우 빈칸
-            if(index == i && isSuccess && bridges.get(i).equals("U")) sb.append("   ");
+            if(index == i && isSuccess && bridges.get(i).equals(UP)) gameProgress.append(EMPTY);
             // 3. 잘못된 다리인 위쪽을 선택하여 건넜을 경우 빈칸
-            if(index == i && !isSuccess && bridges.get(i).equals("D")) sb.append("   ");
+            if(index == i && !isSuccess && bridges.get(i).equals(DOWN)) gameProgress.append(EMPTY);
             // 4. 잘못된 다리인 아래쪽을 선택하여 건넜을 경우 X
-            if(index == i && !isSuccess && !bridges.get(i).equals("D")) sb.append(" X ");
+            if(index == i && !isSuccess && !bridges.get(i).equals(DOWN)) gameProgress.append(X_MARK);
             // 이전 다리들 출력하는 것
-            if(index != i && bridges.get(i).equals("D"))sb.append(" O ");
-            if(index != i && !bridges.get(i).equals("D")) sb.append("   ");
+            if(index != i && bridges.get(i).equals(DOWN)) gameProgress.append(CIRCLE);
+            if(index != i && !bridges.get(i).equals(DOWN)) gameProgress.append(EMPTY);
         }
     }
 
-
-
-    /**
-     * 게임의 최종 결과를 정해진 형식에 맞춰 출력한다.
-     * <p>
-     * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
-     */
     public void printResult(List<String> bridges, int index, boolean isRunning) {
-        System.out.println("최종 게임 결과");
-        System.out.println("");
+        System.out.println(FINAL_GAME_RESULTS_MESSAGE);
         if(isRunning) {
             printMap(bridges, index-1 , true);
-            System.out.println("게임 성공 여부: 성공");
+            System.out.println(GAME_SUCCESS);
         }
         if(!isRunning) {
             printMap(bridges, index-1 , false);
-            System.out.println("게임 성공 여부: 실패");
+            System.out.println(GAME_FAIL);
         }
     }
 
     public void printTotalAttemptsMade(int numberOfAttempts){
-        System.out.printf("총 시도한 횟수: %d", numberOfAttempts);
+        System.out.printf(TOTAL_ATTEMPTS_MADE_MESSAGE, numberOfAttempts);
     }
 }
