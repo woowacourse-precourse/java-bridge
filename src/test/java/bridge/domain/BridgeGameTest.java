a32fbb6a4b350468166620c8fe61bccf00ba6a02
ddukddaki <rlfrkdms1@naver.com>
2022-11-22 21:19:39 +0900

    test(BridgeGameTest): move를 했을 때 결과가 O,X,공백등으로 알맞는 값이 담긴다.

---
+++ src/test/java/bridge/domain/BridgeGameTest.java
@@ -0,0 +1,50 @@
+package bridge.domain;
+
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import java.util.List;
+import java.util.Map;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class BridgeGameTest {
+
+    private MovingMap movingMap = new MovingMap();
+    BridgeGame bridgeGame = new BridgeGame();
+    List<String> bridge = List.of("U", "U", "D");
+
+    @BeforeEach
+    void setUp() {
+        movingMap.init();
+    }
+
+    @Test
+    @DisplayName("move를 했을 때 result에 값이 제대로 담길 수 있다. ")
+    void tryMoveSuccess() {
+        String moving = "U";
+
+        bridgeGame.move(bridge, moving, movingMap);
+        Map<String, List<String>> result = movingMap.getMovingMap();
+        String uStatus = result.get("U").get(0);
+        String dStatus = result.get("D").get(0);
+
+        assertThat(uStatus).isEqualTo(" O ");
+        assertThat(dStatus).isEqualTo("   ");
+    }
+
+    @Test
+    @DisplayName("move를 했을 때 result에 값이 제대로 담길 수 있다. ")
+    void tryMoveFail() {
+        String moving = "D";
+        bridgeGame.move(bridge, moving, movingMap);
+        Map<String, List<String>> result = movingMap.getMovingMap();
+        String uStatus = result.get("U").get(0);
+        String dStatus = result.get("D").get(0);
+
+        assertThat(uStatus).isEqualTo("   ");
+        assertThat(dStatus).isEqualTo(" X ");
+    }
+    
+}
