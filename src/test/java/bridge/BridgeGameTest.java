0ff8c3788b33191bab46ce2c2cbadf1a815f38d1
SuhHyeonjun <dmsgktn0208@gmail.com>
2022-11-22 11:49:36 +0900

    test : 빌드오류로 재시작 테스트 삭제

--- src/test/java/bridge/BridgeGameTest.java
+++ src/test/java/bridge/BridgeGameTest.java
@@ -1,10 +1,5 @@
 package bridge;
 
-import static bridge.controller.BridgeGameController.getCount;
-
-import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeTest;
-
-import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.util.Lists.newArrayList;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
@@ -36,28 +31,6 @@ public class BridgeGameTest extends NsTest {
         assertEquals("O", firstMove);
         assertEquals("O", secondMove);
         assertNotEquals("O", thirdMove);
-    }
-
-    @DisplayName("재시작 테스트")
-    @Test
-    void retryTest() {
-        assertRandomNumberInRangeTest(() -> {
-            run("3", "U", "D", "U", "R", "U", "D", "D");
-            assertThat(output()).contains(
-                    "[ O |   | X ]",
-                    "[   | O |   ]",
-                    "게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)",
-                    "이동할 칸을 선택해주세요. (위: U, 아래: D)",
-                    "최종 게임 결과",
-                    "[ O |   |   ]",
-                    "[   | O | O ]",
-                    "게임 성공 여부: 성공",
-                    "총 시도한 횟수: 2"
-            );
-
-            int count = getCount();
-            assertEquals(2, count);
-        }, 1, 0, 0);
     }
 
     @Override
