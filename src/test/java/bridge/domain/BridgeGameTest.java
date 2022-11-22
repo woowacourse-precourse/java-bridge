d4babd22a6d1a0de686127a147145575e0d068f5
leeyongjun <leeyongjun0310@naver.com>
2022-11-22 19:49:55 +0900

    test(bridge): 이동할때 마다 사용자의 bridgeMap이 반영되는지 확인하는 테스트 코드 작성

--- src/test/java/bridge/domain/BridgeGameTest.java
+++ src/test/java/bridge/domain/BridgeGameTest.java
@@ -12,10 +12,11 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class BridgeGameTest {
     private final BridgeGame bridgeGame = new BridgeGame();
+    Player player;
 
     @BeforeEach
     void beforeEach() {
-        Player player = new Player();
+        player = new Player();
         List<String> bridge = Arrays.asList("U", "D", "D");
         bridgeGame.initBridgeGame(player, bridge);
     }
@@ -30,6 +31,7 @@ class BridgeGameTest {
     @Test
     void move_잘못된_방향_이동() {
         assertThat(bridgeGame.move("D")).isEqualTo("FAIL");
+        assertThat(bridgeGame.move("D")).isEqualTo("FAIL");
     }
 
     @DisplayName("다리 끝까지 올바른 방향으로 가면 'SUCCESS'가 리턴되는지 확인")
@@ -40,6 +42,15 @@ class BridgeGameTest {
                 () -> assertEquals(bridgeGame.move("D"), "NEXT"),
                 () -> assertEquals(bridgeGame.move("D"), "SUCCESS")
         );
+    }
+
+    @DisplayName("사용자의 BridgeMap이 정상적으로 반영되는지 확인")
+    @Test
+    void move_사용자의_BridgeMap_반영되는지_확인() {
+        List<String> answer = Arrays.asList("O", "X");
+        bridgeGame.move("U");
+        bridgeGame.move("U");
+        assertThat(player.getBridgeMap()).isEqualTo(answer);
     }
 
     @Test
