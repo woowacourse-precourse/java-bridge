30d9f98dcd88e7b94eb39598d14c0f1db903f5ed
Seokhyun Lee <lseokhyun99@gmail.com>
2022-11-22 14:35:32 +0900

    test(InputErrorCheckTest.java): 다리 사이즈 입력 예외 처리 테스트 코드 추가
    다리 크기 입력 시 발생할 수 있는 예외 상황을
    테스트 하는 코드를 작성하였다.
    
    입력값이 숫자 형식이 아닌 경우
    입력값이 공백인 경우
    입력값이 3보다 작은 경우
    입력값이 20보다 큰 경우
    입력값이 음수인 경우 등에 대해 테스트 한다.

--- src/test/java/bridge/InputErrorCheckTest.java
+++ src/test/java/bridge/InputErrorCheckTest.java
@@ -1,4 +1,23 @@
 package bridge;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static bridge.InputErrorCheck.readBridgeSizeErrorCheck;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
 public class InputErrorCheckTest {
+
+    private static final String ERROR_MESSAGE = "[ERROR]";
+
+    @DisplayName("다리 사이즈 입력 시 형식에 맞지 않는 값 예외 처리 테스트")
+    @ParameterizedTest
+    @ValueSource(strings = {"asd","one","i"," ","\n","-1","1","22"})
+    void BridgeSizeIsOutofFormat(String input){
+        assertThatThrownBy(()->
+                readBridgeSizeErrorCheck(input))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(ERROR_MESSAGE);
+    }
 }
