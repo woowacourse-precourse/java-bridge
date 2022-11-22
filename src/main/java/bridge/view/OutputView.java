8ab564b43f85f15bc9c4258c5de9a0a7b13baed5
shpark30 <seohyeonpark30.gmail.com>
2022-11-22 13:45:53 +0900

    refactor: 개행 문자 1개 제거

--- src/main/java/bridge/view/OutputView.java
+++ src/main/java/bridge/view/OutputView.java
@@ -31,7 +31,7 @@ public class OutputView {
             result = "실패";
         }
 
-        System.out.println("\n게임 성공 여부: " + result);
+        System.out.println("게임 성공 여부: " + result);
         System.out.println("총 시도한 횟수: " + bridgeGame.getTrial());
     }
 }
