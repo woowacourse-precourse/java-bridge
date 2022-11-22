cf2266fa64e22e0d78cab4f586b5736b6b70a8de
JJ503 <63184334+JJ503@users.noreply.github.com>
2022-11-22 11:46:34 +0900

    feat(InputView): 게임 재시도 및 종료 입력 입력 기능 추가
    docs: 게임 재시도 및 종료 입력 완료 체크

--- src/main/java/bridge/InputView.java
+++ src/main/java/bridge/InputView.java
@@ -10,6 +10,7 @@ import java.io.IOException;
 public class InputView {
     private final String INPUT_BRIDGE_LENGTH_MESSAGE = "다리의 길이를 입력해주세요.";
     private final String INPUT_BRIDGE_MOVING_MESSAGE = "\n이동할 칸을 선택해주세요. (위: U, 아래: D)";
+    private final String INPUT_GAME_RETRY_MESSAGE = "\n게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)";
 
     /**
      * 다리의 길이를 입력받는다.
@@ -38,6 +39,7 @@ public class InputView {
      * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
      */
     public String readGameCommand() {
-        return null;
+        System.out.println(INPUT_GAME_RETRY_MESSAGE);
+        return Console.readLine();
     }
 }
