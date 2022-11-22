a19cdce9fbdf69a644e721eac17c34affb9f3144
parkjiyun <pjyun05200@gmail.com>
2022-11-22 19:47:54 +0900

    move()와 retry() 메서드 추가

--- src/main/java/bridge/BridgeGame.java
+++ src/main/java/bridge/BridgeGame.java
@@ -20,20 +20,16 @@ public class BridgeGame {
         this.size = size;
     }
 
-    /**
-     * 사용자가 칸을 이동할 때 사용하는 메서드
-     * <p>
-     * 이동을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
-     */
     public void move(String gameCommand) {
+        if (!(gameCommand.equals("U") || gameCommand.equals("D"))) {
+            throw new IllegalArgumentException();
+        }
     }
 
-    /**
-     * 사용자가 게임을 다시 시도할 때 사용하는 메서드
-     * <p>
-     * 재시작을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
-     */
     public void retry(String retryCommand) {
+        if (!(retryCommand.equals("Q") || retryCommand.equals("R"))) {
+            throw new IllegalArgumentException();
+        }
     }
 
     public int getSize() {
