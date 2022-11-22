6ba83f8823adf9deb008591e89d9f4e970f36bb3
jihwan <sgh002400@naver.com>
2022-11-22 20:01:48 +0900

    fix(Application): Exception 잡는 로직 추가

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -6,6 +6,10 @@ public class Application {
 
 	public static void main(String[] args) {
 		BridgeController bridgeController = new BridgeController();
-		bridgeController.run();
+		try {
+			bridgeController.run();
+		} catch (IllegalArgumentException e) {
+			System.out.println(e.getMessage());
+		}
 	}
 }
