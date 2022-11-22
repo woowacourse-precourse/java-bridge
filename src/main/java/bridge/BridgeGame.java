a65fd9482d3abc152a8deebf3b84631c977c3751
MIN-04 <kimji_0424@naver.com>
2022-11-22 16:21:35 +0900

    feat(BridgeGame.java): add retry
    Added method retry:
    - 재시도시 결과값을 초기화한다.

--- src/main/java/bridge/BridgeGame.java
+++ src/main/java/bridge/BridgeGame.java
@@ -1,5 +1,8 @@
 package bridge;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * 다리 건너기 게임을 관리하는 클래스
  */
@@ -22,6 +25,7 @@ public class BridgeGame {
      * <p>
      * 재시작을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void retry() {
+    public List<String> retry() {
+        return new ArrayList<>();
     }
 }
