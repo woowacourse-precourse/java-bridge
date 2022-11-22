7d4db9b2aa4cacba3d10796c3ec7a25746ac41c2
SeokyungKim <kskyung0624@gmail.com>
2022-11-22 22:19:17 +0900

    fix(Application) : 확인되지 않은 에러에 대한 처리를 추가한다

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -8,6 +8,9 @@ public class Application {
             new GameMachine().run();
         } catch (IllegalArgumentException e) {
             System.out.println(e.getMessage());
+        } catch (Exception e) {
+            System.out.println("[ERROR] 확인되지 못한 에러가 발생하였습니다.");
+            System.out.println(e.getMessage());
         }
     }
 }
