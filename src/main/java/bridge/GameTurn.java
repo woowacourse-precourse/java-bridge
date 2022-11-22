5fec3a8e9bd166f70fb70be51fda1d3165e310e6
Juuuic <sn990908a@gmail.com>
2022-11-22 22:52:32 +0900

    feat: 게임 종료 기능 구현

--- src/main/java/bridge/GameTurn.java
+++ src/main/java/bridge/GameTurn.java
@@ -1,21 +1,41 @@
 package bridge;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class GameTurn {
 
     //클래스 변수로 설정
     private static final InputView inputView = new InputView();
     private static final OutputView outputView = new OutputView();
     private static final BridgeGame bridgeGame = new BridgeGame();
+    private static final BridgeRandomNumberGenerator bridgeRandomNumberGenerator = new BridgeRandomNumberGenerator();
+    private static final BridgeMaker bridgeMaker = new BridgeMaker(bridgeRandomNumberGenerator);
     public static String bridgeInput;
-    public static int bridgeLength = 0;
+    public static int bridgeLength;
+    public static List<String> bridge;
+    public static List<String> bridgeFrame = new ArrayList<>(Arrays.asList("[ ]","[ ]"));
+    public static int Cnt = 0;
 
     public static void BridgeGameStart(){
         //게임 시작
+        outputView.printStart();
+        try {
+
+        }
     }
 
     public static void BridgeGameTurn(){
         //한 게임 안에서 반복되는 게임 순서들
         //한번 생성한 다리로 계속 진행
+        bridgeLength = getBridgeLength();
+        bridge = bridgeMaker.makeBridge(bridgeLength);
+        Cnt = 0;
+        while(true){
+            Cnt++;
+            if(!)
+        }
     }
 
     public static int getBridgeLength(){
@@ -35,5 +55,9 @@ public class GameTurn {
 
     public static void ExceptionBridgeDirection(){
         //이동할 방향 예외 처리
+    }
+
+    public static void BridgeGameEnd(List<String> bridge, int cnt){
+        outputView.printResult(bridge, cnt);
     }
 }
