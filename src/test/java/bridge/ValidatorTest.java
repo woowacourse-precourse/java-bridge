5206dc55351ca5bc63dbe98b31d59f2b04ca06fb
Jeangyu <gjwlsrb1700@naver.com>
2022-11-22 16:42:00 +0900

    [FEAT: test] 숫자인지 확인하는 메서드 테스트

--- src/test/java/bridge/ValidatorTest.java
+++ src/test/java/bridge/ValidatorTest.java
@@ -1,2 +1,17 @@
-package bridge;public class ValidatorTest {
+package bridge;
+
+import bridge.utils.Validator;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+public class ValidatorTest {
+    @ParameterizedTest
+    @ValueSource(strings = {"1234j", "123.", "1e1", "jeangyu"})
+    public void 숫자가_맞는지_확인(String number){
+        assertThatThrownBy(()->{
+            Validator.isNumber(number);
+        }).isInstanceOf(IllegalArgumentException.class);
+    }
 }
