bca795977d8e893a206cb44be4f74f7a72489f91
박원종 <wonjong1030@naver.com>
2022-11-22 17:25:19 +0900

    refactor(BridgeGameTest): 테스트 코드 중복 리팩토링

--- src/test/java/bridge/domain/BridgeGameTest.java
+++ src/test/java/bridge/domain/BridgeGameTest.java
@@ -1,5 +1,7 @@
 package bridge.domain;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 import java.util.Arrays;
@@ -8,46 +10,68 @@ import java.util.List;
 import static org.assertj.core.api.Assertions.assertThat;
 
 class BridgeGameTest {
-    @Test
-    void moveTest_count3() {
-        BridgeGame bridgeGame = new BridgeGame(Arrays.asList("U","D","D"));
-        bridgeGame.move("U");
-        bridgeGame.move("D");
-        bridgeGame.move("D");
-        List<String>[] list = bridgeGame.getResult();
-        assertThat(list[0]).containsExactly("O", " ", " ");
-        assertThat(list[1]).containsExactly(" ", "O" ,"O");
+    private BridgeGame bridgeGame;
+    private static final List<String> list_count3 = Arrays.asList("U","D","D");
+    private static final List<String> list_count1 = Arrays.asList("U");
+
+    @Nested
+    class MoveTest {
+        @BeforeEach
+        void setUp() {
+            bridgeGame = new BridgeGame(list_count3);
+        }
+        @Test
+        void moveTest_count3() {
+            bridgeGame.move("U");
+            bridgeGame.move("D");
+            bridgeGame.move("D");
+            List<String>[] list = bridgeGame.getResult();
+            assertThat(list[0]).containsExactly("O", " ", " ");
+            assertThat(list[1]).containsExactly(" ", "O" ,"O");
+        }
+        @Test
+        void moveTest_count2() {
+            bridgeGame.move("U");
+            bridgeGame.move("D");
+            List<String>[] list = bridgeGame.getResult();
+            assertThat(list[0]).containsExactly("O", " ");
+            assertThat(list[1]).containsExactly(" ", "O");
+        }
     }
-    @Test
-    void moveTest_count2() {
-        BridgeGame bridgeGame = new BridgeGame(Arrays.asList("U","D","D"));
-        bridgeGame.move("U");
-        bridgeGame.move("D");
-        List<String>[] list = bridgeGame.getResult();
-        assertThat(list[0]).containsExactly("O", " ");
-        assertThat(list[1]).containsExactly(" ", "O");
+
+    @Nested
+    class EndTest {
+        @BeforeEach
+        void setUp() {
+            bridgeGame = new BridgeGame(list_count1);
+        }
+        @Test
+        void end_true_test() {
+            bridgeGame.move("U");
+            assertThat(bridgeGame.isEnd()).isEqualTo(true);
+        }
+        @Test
+        void end_false_test() {
+            assertThat(bridgeGame.isEnd()).isEqualTo(false);
+        }
     }
-    @Test
-    void end_true_test() {
-        BridgeGame bridgeGame = new BridgeGame(Arrays.asList("U"));
-        bridgeGame.move("U");
-        assertThat(bridgeGame.isEnd()).isEqualTo(true);
-    }
-    @Test
-    void end_false_test() {
-        BridgeGame bridgeGame = new BridgeGame(Arrays.asList("U"));
-        assertThat(bridgeGame.isEnd()).isEqualTo(false);
-    }
-    @Test
-    void is_fail_test_true() {
-        BridgeGame bridgeGame = new BridgeGame(Arrays.asList("U"));
-        bridgeGame.move("D");
-        assertThat(bridgeGame.isGameFail()).isEqualTo(true);
-    }
-    @Test
-    void is_fail_test_false() {
-        BridgeGame bridgeGame = new BridgeGame(Arrays.asList("U"));
-        bridgeGame.move("U");
-        assertThat(bridgeGame.isGameFail()).isEqualTo(false);
+    @Nested
+    class FailTest {
+        @BeforeEach
+        void setUp() {
+            bridgeGame = new BridgeGame(list_count1);
+        }
+        @Test
+        void is_fail_test_true() {
+            BridgeGame bridgeGame = new BridgeGame(Arrays.asList("U"));
+            bridgeGame.move("D");
+            assertThat(bridgeGame.isGameFail()).isEqualTo(true);
+        }
+        @Test
+        void is_fail_test_false() {
+            BridgeGame bridgeGame = new BridgeGame(Arrays.asList("U"));
+            bridgeGame.move("U");
+            assertThat(bridgeGame.isGameFail()).isEqualTo(false);
+        }
     }
 }
