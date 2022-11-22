b8547c775d05b6438b341f955bd9c2af30d85481
Sanmgmin <sangmins930@gmail.com>
2022-11-22 14:29:06 +0900

    refactor: seperate String of Error constants from InputValidator

--- src/main/java/bridge/Validator/ErrorConstants.java
+++ src/main/java/bridge/Validator/ErrorConstants.java
@@ -1,2 +1,8 @@
-package bridge.Validator;public class ErrorConstants {
+package bridge.Validator;
+
+public class ErrorConstants {
+    public static final String ERROR_MESSAGE = "[ERROR]";
+    public static final String ERROR_FOR_LENGTH_OF_BRIDGE = ERROR_MESSAGE + " " + "다리의 길이는 3이상 20이하의 숫자만 가능합니다.";
+    public static final String ERROR_FOR_UP_OR_DOWN = ERROR_MESSAGE + " " + "위: \"U\", 아래: \"D\"로만 가능합니다.";
+    public static final String ERROR_FOR_RETRY_OR_QUIT = ERROR_MESSAGE + " " + "재시도: \"R\", 종료: \"Q\"로만 가능합니다";
 }
--- src/main/java/bridge/Validator/InputValidator.java
+++ src/main/java/bridge/Validator/InputValidator.java
@@ -1,10 +1,11 @@
 package bridge.Validator;
 
+import static bridge.Validator.ErrorConstants.*;
+
 public class InputValidator {
     public static void validateNumeric(String input) {
         boolean isNumeric = isNumeric(input);
         if (!isNumeric) {
-            String ERROR_FOR_LENGTH_OF_BRIDGE = "[ERROR] 다리의 길이는 3이상 20이하의 숫자만 가능합니다.";
             throw new IllegalArgumentException(ERROR_FOR_LENGTH_OF_BRIDGE);
         }
     }
@@ -15,7 +16,6 @@ public class InputValidator {
 
     public static void validateRange(int input) {
         if (input < 3 || input > 20) {
-            String ERROR_FOR_LENGTH_OF_BRIDGE = "[ERROR] 다리의 길이는 3이상 20이하의 숫자만 가능합니다.";
             throw new IllegalArgumentException(ERROR_FOR_LENGTH_OF_BRIDGE);
         }
     }
@@ -24,7 +24,7 @@ public class InputValidator {
         String UP = "U";
         String DOWN = "D";
         if (!(input.equals(UP) || input.equals(DOWN))) {
-            String ERROR_FOR_UP_OR_DOWN = "[ERROR] 위: \"U\", 아래: \"D\"로만 가능합니다.";
+
             throw new IllegalArgumentException(ERROR_FOR_UP_OR_DOWN);
         }
     }
@@ -33,7 +33,6 @@ public class InputValidator {
         String RETRY = "R";
         String QUIT = "Q";
         if (!(input.equals(RETRY) || input.equals(QUIT))) {
-            String ERROR_FOR_RETRY_OR_QUIT = "[ERROR] 재시도: \"R\", 종료: \"Q\"로만 가능합니다";
             throw new IllegalArgumentException(ERROR_FOR_RETRY_OR_QUIT);
         }
     }
