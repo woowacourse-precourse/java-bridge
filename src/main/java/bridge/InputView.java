441b6cdee489f8f4ed6b47edcd15b19b78469078
jsc1014 <wotlr5209@gmail.com>
2022-11-22 14:48:24 +0900

    feat: 재시작 커맨드 입력

--- src/main/java/bridge/InputView.java
+++ src/main/java/bridge/InputView.java
@@ -43,7 +43,7 @@ public class InputView {
      * 사용자가 이동할 칸을 입력받는다.
      */
     public static String readMoving() {
-        System.out.println("이동할 칸을 선택해주세요. (위 : U, 아래 : D)");
+        System.out.println("\n이동할 칸을 선택해주세요. (위 : U, 아래 : D)");
         String inputUpDown = Console.readLine();
         return inputUpDown;
     }
@@ -69,7 +69,27 @@ public class InputView {
     /**
      * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
      */
-    public String readGameCommand() {
-        return null;
+    public static String readGameCommand() {
+        System.out.println("\n게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)");
+        String inputCommand = Console.readLine();
+        return inputCommand;
+    }
+
+    public static String repeatInputRetry(){
+        while(true){
+            try{
+                String input = readGameCommand();
+                errorInputNotROrQ(input);
+                return input;
+            }catch (IllegalArgumentException e) {
+                System.out.println("[ERROR] R 또는 Q를 입력해야 합니다.");
+            }
+        }
+    }
+
+    public static void errorInputNotROrQ(String input){
+        if(!input.equals("R") && !input.equals("Q")){
+            throw new IllegalArgumentException();
+        }
     }
 }
