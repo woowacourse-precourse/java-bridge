4df1985d7326855a6349cf43c1a09143f196a254
im-shung <seoyoung1113@gmail.com>
2022-11-22 22:43:51 +0900

    test: :white_check_mark: "U"와 "D"로 Moving 객체 찾는 테스트

--- src/test/java/bridge/domain/MovingTest.java
+++ src/test/java/bridge/domain/MovingTest.java
@@ -2,6 +2,7 @@ package bridge.domain;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import java.util.Optional;
 import java.util.stream.Stream;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Named;
@@ -24,6 +25,23 @@ class MovingTest {
                 Arguments.of(Named.of("1", 1),
                         Named.of(Moving.UP.name(), Moving.UP)),
                 Arguments.of(Named.of("0", 0),
+                        Named.of(Moving.DOWN.name(), Moving.DOWN))
+        );
+    }
+
+    @DisplayName("\"U\"와 \"D\"로 Moving 객체 찾기")
+    @ParameterizedTest
+    @MethodSource("provideExpressions")
+    void findByExpression(String expression, Moving expected) {
+        Optional<Moving> actual = Moving.findByExpression(expression);
+        actual.ifPresent(gameCommand -> assertThat(gameCommand).isEqualTo(expected));
+    }
+
+    private static Stream<Arguments> provideExpressions() {
+        return Stream.of(
+                Arguments.of(Named.of("\"U\"", "R"),
+                        Named.of(Moving.UP.name(), Moving.UP)),
+                Arguments.of(Named.of("\"D\"", "Q"),
                         Named.of(Moving.DOWN.name(), Moving.DOWN))
         );
     }
