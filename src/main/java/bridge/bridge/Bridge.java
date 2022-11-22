8c821127b275a997464b9f0864f3286663ab873f
thuthi <whquddn55@gmail.com>
2022-11-22 21:38:26 +0900

    refactor(bridgeGame): 다리의 길이 validation 작업을 Bridge 클래스로 이동 InputView에서 작업하던 validation작업을 Bridge 클래스로 이동함.

--- src/main/java/bridge/bridge/Bridge.java
+++ src/main/java/bridge/bridge/Bridge.java
@@ -1,17 +1,28 @@
 package bridge.bridge;
 
+import static bridge.exception.IllegalArgumentExceptionMessage.ILLEGAL_BRIDGE_LENGTH;
+
 import java.util.ArrayList;
 import java.util.List;
 
 public class Bridge {
+    public static final int BRIDGE_LOWER_INCLUSIVE = 3;
+    public static final int BRIDGE_UPPER_INCLUSIVE = 20;
     private final List<String> state;
 
     public Bridge() {
         this.state = new ArrayList<>();
     }
 
-    public Bridge(List<String> state) {
+    public Bridge(List<String> state) throws IllegalArgumentException {
+        validateBridgeSize(state.size());
         this.state = state;
+    }
+
+    private void validateBridgeSize(int bridgeSize) throws IllegalArgumentException {
+        if (BRIDGE_LOWER_INCLUSIVE > bridgeSize || bridgeSize > BRIDGE_UPPER_INCLUSIVE) {
+            throw new IllegalArgumentException(ILLEGAL_BRIDGE_LENGTH.getMessage());
+        }
     }
 
     public int size() {
--- src/main/java/bridge/view/InputView.java
+++ src/main/java/bridge/view/InputView.java
@@ -1,30 +1,17 @@
 package bridge.view;
 
-import static bridge.bridgeGame.GameCommand.COMMAND_QUIT;
-import static bridge.bridgeGame.GameCommand.COMMAND_RETRY;
 import static bridge.exception.IllegalArgumentExceptionMessage.ILLEGAL_BRIDGE_LENGTH;
-import static bridge.exception.IllegalArgumentExceptionMessage.ILLEGAL_GAME_COMMAND;
 
 import bridge.bridge.BridgePosition;
 import bridge.bridgeGame.GameCommand;
 import camp.nextstep.edu.missionutils.Console;
 
 public class InputView {
-    public static final int BRIDGE_LOWER_INCLUSIVE = 3;
-    public static final int BRIDGE_UPPER_INCLUSIVE = 20;
 
     public int readBridgeSize() throws IllegalArgumentException {
         try {
-            int bridgeSize = Integer.parseInt(Console.readLine());
-            validateBridgeSize(bridgeSize);
-            return bridgeSize;
+            return Integer.parseInt(Console.readLine());
         } catch (NumberFormatException e) {
-            throw new IllegalArgumentException(ILLEGAL_BRIDGE_LENGTH.getMessage());
-        }
-    }
-
-    private void validateBridgeSize(int bridgeSize) throws IllegalArgumentException {
-        if (BRIDGE_LOWER_INCLUSIVE > bridgeSize || bridgeSize > BRIDGE_UPPER_INCLUSIVE) {
             throw new IllegalArgumentException(ILLEGAL_BRIDGE_LENGTH.getMessage());
         }
     }
