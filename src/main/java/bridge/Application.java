71ef250e22afe03e0c1264ac104ded37e422ecd9
ddukddaki <rlfrkdms1@naver.com>
2022-11-22 17:52:40 +0900

    feat(Application): 프로그램을 실행하는 기능

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -4,5 +4,7 @@ public class Application {
 
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        BridgeController bridgeController = new BridgeController(new BridgeMaker(new BridgeRandomNumberGenerator()), new InputView(), new BridgeGame(), new OutputView(), new MovingMap());
+        bridgeController.run();
     }
 }
