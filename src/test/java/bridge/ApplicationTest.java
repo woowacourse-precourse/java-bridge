b87a98d8aebab1ff2d71c6d26851ed02461e6ead
이현진 <ehj1107@naver.com>
2022-11-22 13:48:44 +0900

    test: 정상적인 기능 동작 테스트

--- src/test/java/bridge/ApplicationTest.java
+++ src/test/java/bridge/ApplicationTest.java
@@ -46,6 +46,93 @@ class ApplicationTest extends NsTest {
         });
     }
 
+    @Test
+    void 다리길이_최대값_테스트() {
+        assertRandomNumberInRangeTest(() -> {
+            run("20", "U", "U", "D", "D", "U", "D", "U", "D", "U", "U",
+                    "U", "U", "D", "U", "D", "D", "U", "D", "U", "U");
+            assertThat(output()).contains(
+                    "[ O | O |   |   | O |   | O |   | O | O | O | O |   | O |   |   | O |   | O | O ]",
+                    "[   |   | O | O |   | O |   | O |   |   |   |   | O |   | O | O |   | O |   |   ]",
+                    "게임 성공 여부: 성공",
+                    "총 시도한 횟수: 1"
+            );
+        }, 1, 1, 0, 0, 1, 0, 1, 0, 1, 1, 1, 1, 0, 1, 0, 0, 1, 0, 1, 1);
+    }
+
+    @Test
+    void 게임_종료_테스트() {
+        assertRandomNumberInRangeTest(() -> {
+            run("10", "U", "U", "D", "D", "U", "D", "D", "Q");
+            assertThat(output()).contains(
+                    "[ O | O |   |   | O |   |   ]",
+                    "[   |   | O | O |   | O | X ]",
+                    "게임 성공 여부: 실패",
+                    "총 시도한 횟수: 1"
+            );
+        }, 1, 1, 0, 0, 1, 0, 1, 0, 1, 1);
+    }
+
+    @Test
+    void 게임_재시도_테스트() {
+        assertRandomNumberInRangeTest(() -> {
+            run("10", "U", "U", "D", "D", "U", "D", "D", "R",
+                    "U", "U", "D", "D", "U", "D", "U","D","U","U" );
+            assertThat(output()).contains(
+                    "[ O | O |   |   | O |   |   ]",
+                    "[   |   | O | O |   | O | X ]",
+                    "[ O | O |   |   | O |   | O |   | O | O ]",
+                    "[   |   | O | O |   | O |   | O |   |   ]",
+                    "게임 성공 여부: 성공",
+                    "총 시도한 횟수: 2"
+            );
+        }, 1, 1, 0, 0, 1, 0, 1, 0, 1, 1);
+    }
+
+    @Test
+    void 사용자가_다리길이를_잘못_입력_후_정상작동_테스트(){
+        assertRandomNumberInRangeTest(() -> {
+            run("a", "3", "U", "D", "D");
+            assertThat(output()).contains(
+                    ERROR_MESSAGE,
+                    "[ O |   |   ]",
+                    "[   | O | O ]",
+                    "게임 성공 여부: 성공",
+                    "총 시도한 횟수: 1"
+            );
+        }, 1, 0, 0);
+    }
+
+    @Test
+    void 사용자가_이동선택을_잘못_입력_후_정상작동_테스트(){
+        assertRandomNumberInRangeTest(() -> {
+            run("3", "U", "Q", "D", "D");
+            assertThat(output()).contains(
+                    ERROR_MESSAGE,
+                    "[ O |   |   ]",
+                    "[   | O | O ]",
+                    "게임 성공 여부: 성공",
+                    "총 시도한 횟수: 1"
+            );
+        }, 1, 0, 0);
+    }
+
+    @Test
+    void 사용자가_재시도를_잘못_입력_후_정상작동_테스트(){
+        assertRandomNumberInRangeTest(() -> {
+            run("3", "U", "U", "U", "R", "U", "D", "D");
+            assertThat(output()).contains(
+                    "[ O | X ]",
+                    "[   |   ]",
+                    ERROR_MESSAGE,
+                    "[ O |   |   ]",
+                    "[   | O | O ]",
+                    "게임 성공 여부: 성공",
+                    "총 시도한 횟수: 2"
+            );
+        }, 1, 0, 0);
+    }
+
     @Override
     protected void runMain() {
         Application.main(new String[]{});
