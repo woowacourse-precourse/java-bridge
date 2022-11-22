532829a08b946efe9bc78525c670108d409174d2
sora5421 <sojeoung536985@gmail.com>
2022-11-22 18:49:16 +0900

    feat : add welcome method

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -6,6 +6,7 @@ public class Application {
 
     public static void main(String[] args) {
         GameSystem system = new GameSystem();
+        system.welcome();
         system.inputBridgeLength();
         system.calculate();
         system.result();
