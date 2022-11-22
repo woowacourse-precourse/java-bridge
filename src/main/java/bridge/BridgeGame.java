4bc13cc6d75a04740d7cca418f06c13dc6715808
Wooseobee <dntjq0807@naver.com>
2022-11-22 19:57:49 +0900

    comment: 메서드별 주석 추가

--- src/main/java/bridge/BridgeGame.java
+++ src/main/java/bridge/BridgeGame.java
@@ -26,6 +26,14 @@ public class BridgeGame {
         return command.equals("R");
     }
 
+
+    /**
+     * 사용자가 게임을 시도한 횟수를 세는 메서드
+     *
+     * @param command : 사용자가 재시도 하면
+     * @param cnt     : cnt + 1
+     * @return
+     */
     public int countGameNum(String command, int cnt) {
         if (command.equals("R")) {
             return cnt + 1;
@@ -33,6 +41,12 @@ public class BridgeGame {
         return cnt;
     }
 
+    /**
+     * 사용자가 이동한 칸이 이동 가능한지 확인하는 메서드
+     *
+     * @param equals : 사용자가 선택한 칸이 이동 가능한지
+     * @return 이동가능하면 O, 이동 불가능하면 X
+     */
     public String canGo(boolean equals) {
         if (equals) {
             return "O";
@@ -40,13 +54,26 @@ public class BridgeGame {
         return "X";
     }
 
+    /**
+     * 사용자가 선택한 칸이 위칸인지 아래칸인지 확인하는 메서드
+     *
+     * @param userChoice : 유저가 입력한 Command(U or D)
+     * @return 위칸 다리라면 true, 아래칸 다리라면 false
+     */
     public boolean upperOrLower(String userChoice) {
         return userChoice.equals("U");
     }
 
-    public String isBlank(boolean isUpper, String s) {
+    /**
+     * 선택한 다리의 반대 다리에는 공백을 넣어주는 메서드
+     *
+     * @param isUpper : 선택한 다리가 위칸 다리인지, 아래 카 다리인지
+     * @param OorX    : 선택한 다리가 건널 수 있으면 O, 건널 수 없으면 X
+     * @return 선택한 다리라면 O or X, 선택한 다리가 아니라면 공백
+     */
+    public String isBlank(boolean isUpper, String OorX) {
         if (isUpper) {
-            return s;
+            return OorX;
         }
         return " ";
     }
