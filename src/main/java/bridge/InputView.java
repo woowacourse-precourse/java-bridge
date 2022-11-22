a777b1792e5628cf2cfabe934d6416c55eec650d
HGB <hsw8147@gmail.com>
2022-11-22 22:32:12 +0900

    refactor: Inputview class
    조건에 맞게 에러를 출력하여 다시 입력받도록 수정

--- src/main/java/bridge/InputView.java
+++ src/main/java/bridge/InputView.java
@@ -33,16 +33,34 @@ public class InputView {
     /**
      * 다리의 길이를 입력받는다.
      */
-    public String readBridgeSize() {
+    public int readBridgeSize() {
+        System.out.println("다리의 길이를 입력해주세요.");
         String input = Console.readLine();
-        return input;
+        if(!readBridgeSizeValidation(input)){
+            try {
+                throw new IllegalArgumentException();
+            } catch (IllegalArgumentException e){
+                System.out.println("[ERROR] 다리 길이는 3부터 20 사이의 숫자여야 합니다.");
+                readBridgeSize();
+            }
+        }
+        return Integer.parseInt(input);
     }
 
     /**
      * 사용자가 이동할 칸을 입력받는다.
      */
     public String readMoving() {
+        System.out.println("이동할 칸을 선택해주세요. (위: U, 아래: D)");
         String input = Console.readLine();
+        if(!readMovingValidation(input)){
+            try {
+                throw new IllegalArgumentException();
+            } catch (IllegalArgumentException e){
+                System.out.println("[ERROR] U 또는 D를 입력해 주세요.");
+                readMoving();
+            }
+        }
         return input;
     }
 
@@ -50,7 +68,16 @@ public class InputView {
      * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
      */
     public String readGameCommand() {
+        System.out.println("게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)");
         String input = Console.readLine();
+        if(!readGameCommandValidation(input)){
+            try {
+                throw new IllegalArgumentException();
+            } catch (IllegalArgumentException e){
+                System.out.println("[ERROR] R 또는 Q를 입력해 주세요.");
+                readGameCommand();
+            }
+        }
         return input;
     }
 }
