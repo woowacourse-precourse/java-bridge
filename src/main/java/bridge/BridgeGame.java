a355f4ad4ffc9f84442e8b31c5351d95b25dc033
bnbnac <bnbnac@naver.com>
2022-11-22 09:55:49 +0900

    feat: 현재 지점까지의 다리 모양을 반환한다.

--- src/main/java/bridge/BridgeGame.java
+++ src/main/java/bridge/BridgeGame.java
@@ -1,34 +1,41 @@
 package bridge;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class BridgeGame {
-
     private final List<String> bridge;
-    public Integer step;
+    private Integer step;
 
     public BridgeGame(List<String> bridge) {
         this.bridge = bridge;
         this.step = 0;
     }
 
-    public void move(String moving) {
-        if (step > bridge.size()) {
-//            succeed
-        }
+    public Boolean move(String moving) {
         if (match(moving)) {
             this.step += 1;
-            System.out.println("match~");
+            return true;
         }
-        System.out.println("die~");
-        this.step = 0;
-//        ask retry
+        return false;
     }
 
     public Boolean match(String moving) {
         return bridge.get(step).equals(moving);
     }
 
+    public Boolean isEnd() {
+        return step > bridge.size();
+    }
+
+    public List<String> getCurrentBridge() {
+        if (step > 0) {
+            return bridge.subList(0, step - 1);
+        }
+        return null;
+    }
+
     public void retry() {
+        this.step = 0;
     }
 }
