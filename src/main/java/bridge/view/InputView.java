6769a8f1e3a6d481b6fe1f867c16b7481adcce93
SeongbinPark <binni0515@gmail.com>
2022-11-22 17:14:12 +0900

    style(InputView): remove javadoc

--- src/main/java/bridge/view/InputView.java
+++ src/main/java/bridge/view/InputView.java
@@ -3,9 +3,6 @@ package bridge.view;
 import bridge.validation.Validator;
 import camp.nextstep.edu.missionutils.Console;
 
-/**
- * 사용자로부터 입력을 받는 역할을 한다.
- */
 public class InputView {
 
     public int readBridgeSize() {
