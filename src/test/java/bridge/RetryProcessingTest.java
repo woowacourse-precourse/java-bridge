c1d3e0f229be4d771a369e8161b5d7b7f184e748
kissshot1104 <kissshot.heartunderblade1104@gmail.com>
2022-11-22 21:30:18 +0900

    Refactor(RetryProcessingTest) : 성공 케이스 추가
    성공 케이스를 추가했습니다.

--- src/test/java/bridge/RetryProcessingTest.java
+++ src/test/java/bridge/RetryProcessingTest.java
@@ -12,20 +12,34 @@ class RetryProcessingTest extends CustomNsTest {
             new RetryProcessing();
 
     @ParameterizedTest
-    @DisplayName("게임 다시 시작 여부 묻기")
+    @DisplayName("게임 다시 시작 여부 묻기(실패 케이스)")
     @CsvSource({
             "QQ, Q",
             "RR, R",
             "D, R"
     })
-    void readRetryTest(String firstCommand, String secondCommand) {
+    void readRetryFailTest(String firstCommand, String secondCommand) {
 
-        //처음 잘못된 문자로 인해 오류가 날 경우
         command(firstCommand, secondCommand);
         retryProcessing.readRetry();
         assertThat(output()).contains(
                 "게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)",
                 "[ERROR]"
+        );
+    }
+
+    @ParameterizedTest
+    @DisplayName("게임 다시 시작 여부 묻기(성공 케이스)")
+    @CsvSource({
+            "Q",
+            "R",
+    })
+    void readRetrySuccessTest(String firstCommand) {
+
+        command(firstCommand);
+        retryProcessing.readRetry();
+        assertThat(output()).contains(
+                "게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)"
         );
     }
 }
