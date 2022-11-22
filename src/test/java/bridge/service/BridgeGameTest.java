dad09733c03b2cb9d100463e02b0ad4fc007132b
seheonCho <whtpgjs94@gmail.com>
2022-11-22 16:24:29 +0900

    test(Player): 테스트코드 작성
    커밋에 포함된 테스트 작성된 메서드 목록
    1. BridgeGame(생성자)
    2. initBridge()
    3. initPlayer()
    4. move()
    5. retry()
    6. getBridgeMap()
    7. getTryCount()
    8. moveAgain()

---
+++ src/test/java/bridge/service/BridgeGameTest.java
@@ -0,0 +1,121 @@
+package bridge.service;
+
+import bridge.util.BridgeUtil;
+import bridge.util.View;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class BridgeGameTest {
+
+    /**
+     * 생성자를 포함한 아래의 메서드를 테스트 합니다.
+     * 1. BridgeGame()
+     * 2. initBridge(int bridgeSize)
+     * 3. initPlayer()
+     * 4. move(String moveCommand)
+     * 5. getBridgeMap()
+     */
+
+    @DisplayName("(생성자 테스트) bridgeMaker")
+    @Test
+    void 생성자와_다리생성_테스트() {
+        BridgeGame bridgeGame = new BridgeGame();
+        bridgeGame.initBridge(3);
+        bridgeGame.initPlayer();
+        bridgeGame.move(BridgeUtil.UP);
+
+        assertThat(bridgeGame.getBridgeMap().get(0).size()).isEqualTo(1);
+    }
+
+    @DisplayName("재시도 횟수는 기존 1과 반복횟수의 합이므로 2번 반복 (retry()) 시 3이 반환 되어야한다.")
+    @Test
+    void 재시도_횟수_테스트() {
+        BridgeGame bridgeGame = new BridgeGame();
+        bridgeGame.initBridge(3);
+        bridgeGame.initPlayer();
+
+        int expectTryCount = 2;
+        for (int tryTimes = 0; tryTimes < expectTryCount; tryTimes++) {
+            bridgeGame.retry();
+        }
+
+        assertThat(bridgeGame.getTryCount()).isEqualTo(expectTryCount + 1);
+    }
+
+    @DisplayName("마지막에 다다르면 false 반환 이전 조건에 만족하지 않고 최근의 choice와 bridge 의 동일 인덱스 값이 같으면 true 반환" +
+            "둘다 포함되지 않으면 false 반환")
+    @Test
+    void 계속_이동이_가능한지_확인() {
+        BridgeGame bridgeGame = new BridgeGame();
+        bridgeGame.initBridge(3);
+        bridgeGame.initPlayer();
+
+        int expectTryCount = 3;
+        for (int move = 0; move < expectTryCount; move++) {
+            bridgeGame.move(BridgeUtil.UP);
+        }
+
+        assertThat(bridgeGame.moveAgain()).isFalse();
+        assertThat(bridgeGame.getBridgeMap().get(0).get(2)).isIn(View.ANSWER_RESULT, View.WRONG_ANSWER_RESULT);
+    }
+
+    @DisplayName("계속 이동이 불가능하면 false 반환")
+    @Test
+    void 계속_이동이_불가능한것을_확인() {
+        BridgeGame bridgeGame = new BridgeGame();
+        bridgeGame.initBridge(3);
+        bridgeGame.initPlayer();
+
+        int expectTryCount = 3;
+        for (int move = 0; move < expectTryCount; move++) {
+            bridgeGame.move(BridgeUtil.UP);
+        }
+
+        assertThat(bridgeGame.moveAgain()).isFalse();
+    }
+
+    @DisplayName("가장 마지막이 O면 게임의 클리어 확인")
+    @Test
+    void 가장마지막_결과가_O_이고_그위치를알면_클리어() {
+        BridgeGame bridgeGame = new BridgeGame();
+        bridgeGame.initBridge(3);
+        bridgeGame.initPlayer();
+
+        int expectTryCount = 3;
+        for (int move = 0; move < expectTryCount; move++) {
+            bridgeGame.move(BridgeUtil.UP);
+        }
+
+        assertThat(bridgeGame.getBridgeMap()
+                             .get(0)
+                             .get(expectTryCount - 1)
+                              .equals(BridgeUtil.UP))
+                .isEqualTo(bridgeGame.isClearGame());
+    }
+
+    @Test
+    void validateBridgeSize() {
+    }
+
+    @Test
+    void validateInputMoveCommand() {
+    }
+
+    @Test
+    void validateInputGameCommand() {
+    }
+
+    @Test
+    void isRestartGame() {
+    }
+
+    @Test
+    void getTryCount() {
+    }
+
+    @Test
+    void isCrossAllBridge() {
+    }
+}
