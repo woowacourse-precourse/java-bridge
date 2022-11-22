b6c8d3a400831db279c0bcbcf86bbe52de87c941
juno-junho <ssmm0205@naver.com>
2022-11-22 23:23:42 +0900

    feat(domain): create class BridgeGameResultLine

---
+++ src/main/java/bridge/domain/BridgeGameResultLine.java
@@ -0,0 +1,35 @@
+package bridge.domain;
+
+import bridge.constant.MoveCommand;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class BridgeGameResultLine {
+    public static final String BLANK = " ";
+
+    private final MoveCommand moveCommands;
+    private final List<String> results = new ArrayList<>();
+
+    public BridgeGameResultLine(MoveCommand moveCommands) {
+        this.moveCommands = moveCommands;
+    }
+
+    void addMoveResults(MoveCommand moveCommands, String moveResult) {
+        if (this.moveCommands == moveCommands) {
+            results.add(moveResult);
+            return;
+        }
+        results.add(BLANK);
+    }
+
+    List<String> results() {
+        return Collections.unmodifiableList(results);
+    }
+
+    void clear() {
+        results.clear();
+    }
+
+}
