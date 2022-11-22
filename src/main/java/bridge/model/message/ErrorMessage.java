a4e1a047c34cb190bfcc5efcac74015b182471dc
chj <bluedog129@gmail.com>
2022-11-22 20:41:40 +0900

    feat(Message) : 에러메시지 이넘 클래스 생성

--- src/main/java/bridge/model/message/ErrorMessage.java
+++ src/main/java/bridge/model/message/ErrorMessage.java
@@ -1,9 +1,10 @@
-package bridge.message;
+package bridge.model.message;
 
 public enum ErrorMessage {
-    WARNING_BRIDGE_SIZE_RANGE("[ERROR] 다리길이는 3이상 20이하의 숫자를 입력해주세요"),
-    WARNING_BRIDGE_INPUT_TYPE("[ERROR] 다리길이는 3이상 20이하의 숫자를 입력해주세요");
-
+    ERROR_PREFIX("[ERROR] 잘못 입력했습니다."),
+    ERROR_MOVING("[ERROR]U 또는 D를 입력해주세요."),
+    ERROR_BRIDGE_SIZE("[ERROR] 다리길이는 3이상 20이하의 숫자를 입력해주세요"),
+    ERROR_RETRY("[ERROR] 또는 Q를 입력해주세요.");
     private final String errorMessage;
 
     ErrorMessage(String errorMessage) {
