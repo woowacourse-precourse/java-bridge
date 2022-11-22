86bbbabc2a383a3845b97efea6b95faa57188c30
HangSub999 <gkdtjq999@gmail.com>
2022-11-22 21:26:27 +0900

    fix: 다리길이 3이상 20이하 예외처리

--- src/main/java/bridge/model/LengthBridge.java
+++ src/main/java/bridge/model/LengthBridge.java
@@ -9,8 +9,20 @@ public class LengthBridge {
     }
 
     public void validate(String s) {
+        checkNumbers(s);
+        checkrange(s);
+    }
+
+    private void checkNumbers(String s) {
         if (!s.matches("-?\\d+")) {
             throw new IllegalArgumentException("[ERROR] 숫자가 아닌 입력 값입니다.");
+        }
+    }
+
+    private void checkrange(String s) {
+        int userinput = Integer.parseInt(s);
+        if (userinput < 3 || userinput > 20) {
+            throw new IllegalArgumentException("[ERROR] 3 이상 20 이하의 숫자가 아닙니다.");
         }
     }
 
--- src/main/java/bridge/view/OutputView.java
+++ src/main/java/bridge/view/OutputView.java
@@ -11,23 +11,13 @@ import java.util.List;
 public class OutputView {
 
     public static void printgameStart() {
-        System.out.println("다리 건너기 게임을 시작합니다\n");
+        System.out.println("다리 건너기 게임을 시작합니다.\n");
     }
 
-    /**
-     * 현재까지 이동한 다리의 상태를 정해진 형식에 맞춰 출력한다.
-     * <p>
-     * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
-     */
     public static void printMap(StringBuilder upline, StringBuilder downline) {
         System.out.println(upline + "\n" + downline);
     }
 
-    /**
-     * 게임의 최종 결과를 정해진 형식에 맞춰 출력한다.
-     * <p>
-     * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
-     */
     public static void printResult(BridgeGame bridgeGame, String success, int count) {
         System.out.println("\n최종 게임 결과");
         printMap(bridgeGame.upline(), bridgeGame.downline());
