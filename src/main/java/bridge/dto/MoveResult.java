05747b584329a2ec510425a846d4c21c6be4df38
Hyun-Seo Oh <ohseo8813@gmail.com>
2022-11-22 16:16:48 +0900

    <style>(All): 쓸모없는 공백 및 이젠 사용하지 않는 import 제거

--- src/main/java/bridge/dto/MoveResult.java
+++ src/main/java/bridge/dto/MoveResult.java
@@ -5,16 +5,16 @@ public class MoveResult {
     private final String message;
     private final boolean success;
 
+    public MoveResult(String message, boolean success) {
+        this.message = message;
+        this.success = success;
+    }
+
     public String getMessage() {
         return message;
     }
 
     public boolean isSuccess() {
         return success;
-    }
-
-    public MoveResult(String message, boolean success) {
-        this.message = message;
-        this.success = success;
     }
 }
