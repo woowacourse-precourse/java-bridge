78c2144437405b798ed3fc2858937273b80d0885
kiarakim <kiarakim413@gmail.com>
2022-11-22 17:41:33 +0900

    test(UserTest) : add count trial test, success and failure test

--- src/test/java/bridge/domain/UserTest.java
+++ src/test/java/bridge/domain/UserTest.java
@@ -31,4 +31,44 @@ class UserTest {
         // then
         assertThat(user.isGameDone()).isEqualTo(true);
     }
+
+    @Test
+    @DisplayName("게임 재시작 후 trial 카운팅 테스트")
+    public void countTrialTest() {
+        // given
+        User user = new User();
+
+        // when
+        user.retry();
+
+        // then
+        assertThat(user.getTrial()).isEqualTo(2);
+    }
+
+    @Test
+    @DisplayName("게임 성공 테스트")
+    public void isSuccessTest() {
+        // given
+        User user = new User();
+
+        // when
+        user.winGame();
+
+        // then
+        assertThat(user.isGameDone()).isEqualTo(true);
+    }
+
+    @Test
+    @DisplayName("게임 실패 테스트")
+    public void isFailureTest() {
+        // given
+        User user = new User();
+
+        // when
+        user.loseGame();
+
+        // then
+        assertThat(user.isSuccessful()).isEqualTo(false);
+        assertThat(user.isGameDone()).isEqualTo(true);
+    }
 }
