7b8e31d74c16e444953e2dfc3e7f8526f8747642
DESKTOP-1ANNTGI\USER <cvc100326@naver.com>
2022-11-22 23:06:39 +0900

    test: 리펙터링 했던 클래스 테스트코드 추가 및 삭제

--- src/test/java/bridge/domain/BridgeTest.java
+++ src/test/java/bridge/domain/BridgeTest.java
@@ -3,13 +3,13 @@ package bridge.domain;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
+import java.util.List;
+
 import bridge.BridgeMaker;
 import bridge.BridgeRandomNumberGenerator;
-import bridge.domain.Direction;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import java.util.List;
 
 public class BridgeTest {
     @DisplayName("특정위치에서 다리가 같으면 true")
@@ -28,23 +28,13 @@ public class BridgeTest {
         assertThat(result).isEqualTo(false);
     }
 
-
-    @DisplayName("같은 다리이면 true")
+    @DisplayName("다리 길이 확인하기")
     @Test
-    void checkSameBridge1() {
+    void checkLength() {
         Bridge bridge = new Bridge(List.of("U", "D", "D"));
-        boolean result = bridge.isSame(List.of("U", "D", "D"));
-        assertThat(result).isEqualTo(true);
+        int result = bridge.getLength();
+        assertThat(result).isEqualTo(3);
     }
-
-    @DisplayName("다른 다리이면 false")
-    @Test
-    void checkSameBridge2() {
-        Bridge bridge = new Bridge(List.of("U", "D", "D"));
-        boolean result = bridge.isSame(List.of("U", "D"));
-        assertThat(result).isEqualTo(false);
-    }
-
 
     @DisplayName("다리의 길이가 20보다 큰 경우")
     @Test
@@ -65,4 +55,5 @@ public class BridgeTest {
         assertThatThrownBy(
                 () -> new Bridge(bridge)).isInstanceOf(IllegalArgumentException.class);
     }
+
 }
