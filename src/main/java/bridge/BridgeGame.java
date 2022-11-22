58e6ea282e09bb368af350f39bae0a1ce312e1b2
glass <glass7312@gmail.com>
2022-11-22 14:20:34 +0900

    refactor:set_positions 기능 수정 up_positions,down_positions 각각 set하지 않고 한 메서드에서 처리.

--- src/main/java/bridge/BridgeGame.java
+++ src/main/java/bridge/BridgeGame.java
@@ -7,9 +7,8 @@ import java.util.List;
  * 다리 건너기 게임을 관리하는 클래스
  */
 public class BridgeGame {
-
-   // private Positions positions = new Positions();
     public static String game_status = "playing";
+    private boolean status;
 
     /**
      * 사용자가 칸을 이동할 때 사용하는 메서드
@@ -17,28 +16,25 @@ public class BridgeGame {
      * 이동을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
     public String move(String inputPosition, List<String> bridge, int count) {
+        status = inputPosition.equals(bridge.get(count));
         if (inputPosition.equals("U")) {
-            if (inputPosition.equals(bridge.get(count))) {
-                Positions.set_UpPositions("O");
-                Positions.set_downPositions(" ");
+            if(inputPosition.equals(bridge.get(count))) {
+                Positions.set_positions("O", " ");
                 return "pass";
             }
             if (!inputPosition.equals(bridge.get(count))) {
-                Positions.set_UpPositions("X");
-                Positions.set_downPositions(" ");
+                Positions.set_positions("X", " ");
                 game_status = "END";
                 return "call";
             }
         }
         if (inputPosition.equals("D")) {
             if (inputPosition.equals(bridge.get(count))) {
-                Positions.set_downPositions("O");
-                Positions.set_UpPositions(" ");
+                Positions.set_positions(" ", "O");
                 return "pass";
             }
             if (!inputPosition.equals(bridge.get(count))) {
-                Positions.set_downPositions("X");
-                Positions.set_UpPositions(" ");
+                Positions.set_positions(" ", "X");
                 game_status = "END";
                 return "call";
             }
@@ -54,10 +50,9 @@ public class BridgeGame {
     public String retry(String command) {
         if (command.equals("R")) {
             Positions.reset();
-            game_status="playing";
+            game_status = "playing";
             return "playing";
         }
-
         if (command.equals("Q")) {
             game_status = "End";
             return "End";
@@ -65,11 +60,23 @@ public class BridgeGame {
         return "playing";
     }
 
-  //  public Positions getPositions() {
-    //    return positions;
-    //}
-
-    public Positions get_instance() {
-        return new Positions();
+    private void setPositions(String position, boolean status) {
+        if (status && position.equals("U")) {
+            Positions.set_positions("O", " ");
+        }
+        if (!status && position.equals("U")) {
+            Positions.set_positions("X", " ");
+        }
+        if (status && position.equals("D")) {
+            Positions.set_positions(" ", "O");
+        }
+        if (!status && position.equals("D")) {
+            Positions.set_positions(" ", "X");
+        }
     }
 }
+
+
+
+
+
--- src/main/java/bridge/Positions.java
+++ src/main/java/bridge/Positions.java
@@ -13,16 +13,14 @@ public class Positions {
     public static List<String> getDown_positions(){
         return down_positions;
     }
-    public static void set_UpPositions(String position){
-        up_positions.add(position);
+    public static void set_positions(String up_position,String down_position){
+        up_positions.add(up_position);
+        down_positions.add(down_position);
     }
 
     public static void reset() {
         up_positions.clear();
         down_positions.clear();
-    }
-    public static void set_downPositions(String position) {
-        down_positions.add(position);
     }
 
 }
