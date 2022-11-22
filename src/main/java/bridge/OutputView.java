c1a91d3ca784a98cf375f8afe93b3534339980ec
imsehwan <dla612@naver.com>
2022-11-22 13:58:08 +0900

    feat : 성공 여부 출력 기능 및 총 시도 횟수 출력 기능 추가
    OutputView 클래스 안에 printResult 메소드를 통해 출력 기능 추가

--- src/main/java/bridge/OutputView.java
+++ src/main/java/bridge/OutputView.java
@@ -18,6 +18,13 @@ public class OutputView {
      * <p>
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void printResult() {
+    public void printResult(boolean isEnd, int gameTryCount) {
+        if (isEnd) {
+            System.out.println("게임 성공 여부: 성공");
+            System.out.println("총 시도한 횟수: " + gameTryCount);
+            return;
+        }
+        System.out.println("게임 성공 여부: 실패");
+        System.out.println("총 시도한 횟수: " + gameTryCount);
     }
 }
