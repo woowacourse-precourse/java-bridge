a92f535facfd57c44f7c396ef788f5ed5d0ee44d
username459712 <94822739+username459712@users.noreply.github.com>
2022-11-22 21:02:21 +0900

    feat: constant(자주 쓰는 final value)

---
+++ src/main/java/bridge/constant/Constant.java
@@ -0,0 +1,9 @@
+package bridge.constant;
+
+public class Constant {
+    public static final String ERROR_MESSAGE = "[ERROR] ";
+    public static final String MOVE_UP = "U";
+    public static final String correctBridge = " O ]";
+    public static final String wrongBridge = " X ]";
+    public static final String emptyBridge = "   ]";
+}
