20796660938d06ecfd96dd3ce9d771b33d52b447
seo <fungod19@gmail.com>
2022-11-22 21:25:15 +0900

    feat:사용자 리스트 만들기

--- src/main/java/bridge/view/InputView.java
+++ src/main/java/bridge/view/InputView.java
@@ -13,15 +13,21 @@ public class InputView {
     public int readBridgeSize() {
             System.out.println("다리의 길이를 입력해주세요.");
             String input = Console.readLine();
-            int size = Integer.parseInt(input);
-            validateNumber(size);
-            return size;
+            validateNumber(input);
+            return Integer.parseInt(input);
     }
 
-    private void validateNumber(int size) {
-        if(size < 3 || size > 20){
+    private void validateNumber(String input) {
+        try{
+            int size = Integer.parseInt(input);
+            if(size < 3 || size > 20){
+                throw new IllegalArgumentException("[ERROR] 다리 길이는 3부터 20 사이의 숫자여야 합니다.");
+            }
+        }catch (Exception e){
             throw new IllegalArgumentException("[ERROR] 다리 길이는 3부터 20 사이의 숫자여야 합니다.");
         }
+
+
     }
 
     /**
@@ -30,12 +36,15 @@ public class InputView {
     public String readMoving() {
         System.out.println("\n이동할 칸을 선택해주세요. (위: U, 아래: D)");
         String input = Console.readLine();
-        validateMethod(input);
+        //validateMethod(input);
         return input;
     }
 
     private void validateMethod(String input) {
-        if(!input.equals( "U" )|| !input.equals( "D" )){
+        if(input.equals("U")){
+            throw new IllegalArgumentException("[ERROR] U 나 D 를 입력해주세요.");
+        }
+        if(!input.equals("D")){
             throw new IllegalArgumentException("[ERROR] U 나 D 를 입력해주세요.");
         }
     }
@@ -47,19 +56,11 @@ public class InputView {
         System.out.println("\n게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)");
         String input = Console.readLine();
         validateEnd(input);
-        deciseEnd(input);
         return input;
 
     }
 
-    private void deciseEnd(String input) {
-        if(input.equals("R")){
-            readMoving();
-        }
-        if(input.equals("Q")){
-            //최종게임결과
-        }
-    }
+
 
     private void validateEnd(String input) {
         if(!input.equals( "R" )|| !input.equals( "Q" )){
