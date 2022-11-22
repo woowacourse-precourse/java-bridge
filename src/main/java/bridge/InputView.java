bcfd89a7275251ba66ddb75a84e60a572b53ae53
조미정 <ing06164@naver.com>
2022-11-22 22:59:42 +0900

    feat: 사용자가 이동할 칸 입력받기 - readMoving()

--- src/main/java/bridge/InputView.java
+++ src/main/java/bridge/InputView.java
@@ -9,9 +9,12 @@ public class InputView {
     private int bridgeSize;
     //다리의 길이 입력
     public int readBridgeSize() {
+        System.out.println("* 다리 건너기 게임을 시작합니다. *");
+        System.out.println("다리의 길이를 입력해주세요.");
         bridgeSize = Integer.parseInt(Console.readLine());
 
         validateBridge(bridgeSize);
+
         return bridgeSize;
     }
 
@@ -27,16 +30,29 @@ public class InputView {
      * 사용자가 이동할 칸을 입력받는다.
      */
     public String readMoving() {
-        return null;
+        System.out.println("이동할 칸을 선택해주세요. (위: U, 아래: D)");
+        String upOrDown = Console.readLine();
+
+        validateMoving(upOrDown);
+        return upOrDown;
     }
 
+    public void validateMoving(String upOrDown){
+        if (!upOrDown.equals("U")&&!upOrDown.equals("D")){
+            throw new IllegalArgumentException("[ERROR] U / D 로 입력해주세요");
+        }
+    }
     /**
      * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
      */
     public boolean readGameCommand() {
         String RestartOrQuit = Console.readLine();
-        if(RestartOrQuit.equals("Q"))
+        if(RestartOrQuit.equals("Q")){
             return true;
-        return false;
+        }
+        else if(RestartOrQuit.equals("R")){
+            return false;
+        }
+        throw new IllegalArgumentException("[ERROR] R이나 Q를 입력해 주세요");
     }
 }
