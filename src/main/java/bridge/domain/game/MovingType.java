668aa3b87cd08fc971553c75148926febfc43f16
alswn <alswn9938@gmail.com>
2022-11-22 11:48:42 +0900

    feat(MovingType): add MovingType 도메인 클래스

---
+++ src/main/java/bridge/domain/game/MovingType.java
@@ -0,0 +1,46 @@
+package bridge.domain.game;
+
+import java.util.*;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public enum MovingType {
+    MOVING_UP(1, "U"),
+    MOVING_DOWN(0, "D");
+
+    private final int numNotation;
+    private final String engNotation;
+
+    MovingType(int numNotation, String engNotation){
+        this.numNotation = numNotation;
+        this.engNotation = engNotation;
+    }
+
+    private static final Map<Integer, MovingType> movingTypeByNumNotation =
+            Collections.unmodifiableMap(Stream.of(values())
+                    .collect(Collectors.toMap(MovingType::getNumNotation, Function.identity())));
+
+    private static final List<String> movingTypeEngNotations =
+            Collections.unmodifiableList(Stream.of(MovingType.values())
+                    .map(MovingType::getEngNotation)
+                    .collect(Collectors.toList()));
+
+    public int getNumNotation(){
+        return this.numNotation;
+    }
+
+    public String getEngNotation(){
+        return engNotation;
+    }
+
+    public static MovingType getMovingTypeByNumNotation(int numNotation){
+        return Optional
+                .ofNullable(movingTypeByNumNotation.get(numNotation))
+                .orElseThrow(() -> new IllegalArgumentException("[ERROR] 찾을 수 없음"));
+    }
+
+    public static boolean isContains(String engNotation){
+        return movingTypeEngNotations.contains(engNotation);
+    }
+}
