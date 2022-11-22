f15344fbb3195ff4625992b7d69f06c5ca40691f
eun-byeol <62530884+eun-byeol@users.noreply.github.com>
2022-11-22 18:03:58 +0900

    feat(OutputView): 현재까지 건넌 다리를 모두 출력

--- src/main/java/bridge/view/OutputView.java
+++ src/main/java/bridge/view/OutputView.java
@@ -1,16 +1,23 @@
-package bridge;
+package bridge.view;
 
-/**
- * 사용자에게 게임 진행 상황과 결과를 출력하는 역할을 한다.
- */
+import bridge.domain.GameResult;
+
+import java.util.List;
+
+import static bridge.utils.ViewMessage.*;
+
 public class OutputView {
 
-    /**
-     * 현재까지 이동한 다리의 상태를 정해진 형식에 맞춰 출력한다.
-     * <p>
-     * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
-     */
-    public void printMap() {
+    public void printMap(GameResult gameResult) {
+        System.out.println(OUTPUT_FINAL_RESULT);
+        printCurrentBridgeMap(gameResult.getUpSideResult());
+        printCurrentBridgeMap(gameResult.getDownSideResult());
+        System.out.println();
+    }
+
+    public void printCurrentBridgeMap(List<String> bridgeResult) {
+        String result = String.join(SEPARATOR_OF_BRIDGE, bridgeResult);
+        System.out.println(START_OF_BRIDGE + result + END_OF_BRIDGE);
     }
 
     /**
