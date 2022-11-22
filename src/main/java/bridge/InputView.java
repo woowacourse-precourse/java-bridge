b2bea7698964f0f5ad4ca9ff715121e6475c1721
tjwjdgus12 <01048733438a@gmail.com>
2022-11-22 17:07:51 +0900

    refactor: input read 부분을 재귀적으로 구현

--- src/main/java/bridge/InputView.java
+++ src/main/java/bridge/InputView.java
@@ -11,18 +11,15 @@ public class InputView {
      * 다리의 길이를 입력받는다.
      */
     public int readBridgeSize() {
-        String input;
-        while (true) {
-            try {
-                System.out.println("다리의 길이를 입력해주세요.");
-                input = Console.readLine();
-                validateBridgeSizeInput(input);
-                break;
-            } catch (IllegalArgumentException e) {
-                System.out.println(e.getMessage());
-            }
+        try {
+            System.out.println("다리의 길이를 입력해주세요.");
+            String input = Console.readLine();
+            validateBridgeSizeInput(input);
+            return Integer.parseInt(input);
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+            return readBridgeSize();
         }
-        return Integer.parseInt(input);
     }
 
     private void validateBridgeSizeInput(String bridgeSizeInput) throws IllegalArgumentException {
@@ -41,18 +38,15 @@ public class InputView {
      * 사용자가 이동할 칸을 입력받는다.
      */
     public String readMoving() {
-        String input;
-        while (true) {
-            try {
-                System.out.println("이동할 칸을 선택해주세요. (위: U, 아래: D)");
-                input = Console.readLine();
-                validateMovingInput(input);
-                break;
-            } catch (IllegalArgumentException e) {
-                System.out.println(e.getMessage());
-            }
+        try {
+            System.out.println("이동할 칸을 선택해주세요. (위: U, 아래: D)");
+            String input = Console.readLine();
+            validateMovingInput(input);
+            return input;
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+            return readMoving();
         }
-        return input;
     }
 
     private void validateMovingInput(String movingInput) throws IllegalArgumentException {
@@ -65,18 +59,15 @@ public class InputView {
      * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
      */
     public String readGameCommand() {
-        String input;
-        while (true) {
-            try {
-                System.out.println("게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)");
-                input = Console.readLine();
-                validateGameCommandInput(input);
-                break;
-            } catch (IllegalArgumentException e) {
-                System.out.println(e.getMessage());
-            }
+        try {
+            System.out.println("게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)");
+            String input = Console.readLine();
+            validateGameCommandInput(input);
+            return input;
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+            return readGameCommand();
         }
-        return input;
     }
 
     private void validateGameCommandInput(String gameCommandInput) throws IllegalArgumentException {
