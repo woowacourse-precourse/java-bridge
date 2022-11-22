b97056183ef151cdf6ede5c4b6ef16035c51ee79
nswon <swnam0729@gmail.com>
2022-11-22 11:22:53 +0900

    refactor: 하드코딩된 에러메세지를 상수로 변경

--- src/main/java/bridge/exception/InputException.java
+++ src/main/java/bridge/exception/InputException.java
@@ -5,6 +5,7 @@ public class InputException {
     private static final String NOT_MIN_SIZE = "[ERROR] 다리의 길이는 최소 3이상이어야 합니다.";
     private static final String NOT_MAX_SIZE = "[ERROR] 다리의 길이는 최대 20미만이어야 합니다.";
     private static final String NOT_ONLY_ONE_SIZE = "[ERROR] 크기는 1이어야 합니다.";
+    private static final String NOT_INPUT_FORMAT = "[ERROR] 입력 형식이 맞지 않습니다.";
 
     public static void printNotOnlyNumberException() {
         throw new IllegalArgumentException(NOT_ONLY_NUMBER);
@@ -23,6 +24,6 @@ public class InputException {
     }
 
     public static void printNotFormatException() {
-        throw new NotInputFormatException();
+        throw new NotInputFormatException(NOT_INPUT_FORMAT);
     }
 }
