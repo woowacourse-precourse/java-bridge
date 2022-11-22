ea2b038f66c12d5a16cda3f0f2829777b4bdae1a
LJW25 <dbjwl@g.skku.edu>
2022-11-22 16:20:01 +0900

    fix: curBridge type 수정
    - 기존의 1차원 list에서 2차원 list로 수정함: 위, 아래 줄의 정보를 포함하기 위해

--- src/main/java/bridge/BridgeGame.java
+++ src/main/java/bridge/BridgeGame.java
@@ -9,17 +9,23 @@ import static bridge.GameStatus.*;
  * 다리 건너기 게임을 관리하는 클래스
  */
 public class BridgeGame {
+    private final String RIGHT = "O";
+    private final String WRONG = "X";
+    private final String NOTHING = " ";
 
     private final List<String> answerBridge;
-    private List<String> curBridge;
+    //private List<String> curBridge;;
+    private List<List<String>> curBridge;
     private int stageNum;
     private GameStatus status;
 
     BridgeGame(int size){
         BridgeMaker bridgeMaker = new BridgeMaker(new BridgeRandomNumberGenerator());
+        List<String> upper = new ArrayList<>();
+        List<String> lower = new ArrayList<>();
 
         this.answerBridge = bridgeMaker.makeBridge(size);
-        this.curBridge = new ArrayList<>();
+        this.curBridge = List.of(upper, lower);
         this.stageNum = 1;
         this.status = STATUS_PLAY;
     }
@@ -28,24 +34,44 @@ public class BridgeGame {
         return this.status;
     }
 
-    public void move(String moving) {
-        curBridge.add(moving);
-        compareMoving();
+    public List<List<String>> move(String moving) {
+        String answer = answerBridge.get(curBridge.get(0).size());
+        addCurBridge(answer, moving);
 
-    }
-    private void compareMoving(){
-        int idx = curBridge.size();
-        if(!answerBridge.get(idx-1).equals(curBridge.get(idx-1))){
-            this.status = STATUS_FAIL;
+        if(this.status != STATUS_FAIL){
+            checkSuccess();
         }
-        if(answerBridge.size() == idx){
+        return this.curBridge;
+    }
+
+    private void addCurBridge(String answer, String moving){
+        String isRight = compareCurMoving(answer, moving);
+        if(answer.equals("U")){
+            curBridge.get(0).add(isRight);
+            curBridge.get(1).add(NOTHING);
+        }
+        if(answer.equals("D")){
+            curBridge.get(0).add(NOTHING);
+            curBridge.get(1).add(isRight);
+        }
+    }
+    private String compareCurMoving(String answer, String moving){
+        if(answer.equals(moving)){
+            return RIGHT;
+        }
+        this.status = STATUS_FAIL;
+        return WRONG;
+    }
+
+    private void checkSuccess(){
+        if(this.answerBridge.size() == this.curBridge.get(0).size()) {
             this.status = STATUS_SUCCESS;
         }
     }
 
-
     public void retry() {
         this.stageNum += 1;
-        this.curBridge.clear();
+        this.curBridge.get(0).clear();
+        this.curBridge.get(1).clear();
     }
 }
