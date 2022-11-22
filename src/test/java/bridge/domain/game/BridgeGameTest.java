2aa5d828c352b5d2cab52068262a76f27f347fb3
chanminmaxscaler <jcm04@naver.com>
2022-11-22 20:42:38 +0900

    refactor: BridgeGameTest 클래스를 리팩토링한다
    BridgeGame 클래스를 리팩토링함에 따라 해당 클래스를 테스트하는 클래스인 BridgeGameTest 역시 리팩토링한다.
    BridgeGame 클래스에 맞게 메소드명을 수정한다.
    하드코딩을 없애고 상수를 사용한다.

--- src/test/java/bridge/domain/game/BridgeGameTest.java
+++ src/test/java/bridge/domain/game/BridgeGameTest.java
@@ -1,10 +1,8 @@
 package bridge.domain.game;
 
-import bridge.domain.bridge.Bridge;
 import bridge.domain.direction.Direction;
-import bridge.domain.factory.BridgeFactory;
+import bridge.domain.factory.BridgeGameFactory;
 import bridge.domain.pedestrian.Pedestrian;
-import bridge.domain.referee.Referee;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
@@ -13,7 +11,6 @@ import org.junit.jupiter.params.provider.MethodSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.List;
-import java.util.Map;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
@@ -21,43 +18,41 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 class BridgeGameTest {
 
+    private static final int FIRST_ROUND = 1;
+    private static final int STARTING_POINT = 0;
+    private static final int EXAMPLE_SIZE = 5;
     private BridgeGame game;
     private Pedestrian pedestrian;
 
     @BeforeEach
     void setUp() {
-        int size = 5;
-        BridgeFactory bridgeFactory = new BridgeFactory();
-        Bridge bridge = bridgeFactory.createBridge(size);
+        BridgeGameFactory bridgeGameFactory = new BridgeGameFactory();
         pedestrian = new Pedestrian();
-        game = BridgeGame.from(Referee.from(bridge));
+        game = bridgeGameFactory.createBridgeGame(EXAMPLE_SIZE);
     }
 
-    @DisplayName("move 메소드에 방향을 입력하였을 때 방향이 Pedestrian 객체의 필드에 기록되는지 확인")
+    @DisplayName("move 메소드에 방향을 입력하였을 때 Pedestrian의 위치가 변하는지 확인")
     @ParameterizedTest
     @MethodSource("provideArgumentsForMoveTest")
     void move_test(List<String> inputDirections) {
         List<Direction> directions = createDirections(inputDirections);
+        int expectedLocation = directions.size();
 
         moveInManyDirections(game, directions);
 
-        assertThat(pedestrian.readRecord()
-                .map(Map.Entry::getValue)
-                .collect(Collectors.toList())).isEqualTo(directions);
+        assertThat(pedestrian.findLocation()).isEqualTo(expectedLocation);
     }
 
-    @DisplayName("retry 메소드에 Pedestrian 객체를 입력하였을 때 Pedestrian 객체의 기록을 지우는지 확인")
+    @DisplayName("retry 메소드에 Pedestrian 객체를 입력하였을 때 Pedestrian이 시작점의 위치를 갖는지 확인")
     @ParameterizedTest
     @MethodSource("provideArgumentsForMoveTest")
     void retry_test(List<String> inputDirections) {
         List<Direction> directions = createDirections(inputDirections);
         moveInManyDirections(game, directions);
-        int expectedCount = 0;
 
         game.retry(pedestrian);
 
-        assertThat(pedestrian.readRecord()
-                .count()).isEqualTo(expectedCount);
+        assertThat(pedestrian.findLocation()).isEqualTo(STARTING_POINT);
     }
 
 
@@ -73,31 +68,33 @@ class BridgeGameTest {
                 .collect(Collectors.toList());
     }
 
-    @DisplayName("countRound 메소드를 호출하였을 때 게임 시도 횟수를 반환하는지 확인")
+    @DisplayName("createRound 메소드가 게임 라운드를 담은 Round 객체를 반환하는지 확인")
     @ParameterizedTest(name = "{index} : 시도횟수 {0}")
     @ValueSource(ints = {1, 2, 3, 4, 5, 6})
-    void countRound_test(int times) {
-        int count = playGameMultiple(times);
+    void createRound_test(int times) {
+        retryGameMultiple(times);
+        Round round = game.createRound();
+        int expectedRound = times + FIRST_ROUND;
 
-        assertThat(count).isEqualTo(times);
+        int actualRound = round.getGameRound();
+
+        assertThat(actualRound).isEqualTo(expectedRound);
     }
 
-    private int playGameMultiple(int times) {
-        int count = 0;
+    private void retryGameMultiple(int times) {
         for (int i = 0; i < times; i++) {
-            count = game.countRound();
+            game.retry(pedestrian);
         }
-        return count;
     }
 
-    @DisplayName("isSuccess 메소드에 Pedestrian 객체를 입력하였을 때 Pedestrian가 다리 끝에 위치했는지 여부를 반환하는지 확인")
+    @DisplayName("isEndLocation 메소드에 Pedestrian 객체를 입력하였을 때 Pedestrian가 다리 끝에 위치했는지 여부를 반환하는지 확인")
     @ParameterizedTest
     @MethodSource("provideArgumentsForIsSuccessTest")
-    void isSuccess_test(List<String> inputDirections, boolean expected) {
+    void isEndLocation_test(List<String> inputDirections, boolean expected) {
         List<Direction> directions = createDirections(inputDirections);
         moveInManyDirections(game, directions);
 
-        boolean actual = game.isSuccess(pedestrian);
+        boolean actual = game.isEndLocation(pedestrian);
 
         assertThat(actual).isEqualTo(expected);
     }
