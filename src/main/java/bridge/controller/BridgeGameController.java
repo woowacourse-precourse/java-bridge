a0c68b609b11876d22441d7260b46a3b4345f4a7
kimtaesoo99 <107785279+kimtaesoo99@users.noreply.github.com>
2022-11-22 15:04:14 +0900

    refactor: (BridgeGameController) 서비스를 삭제하여 도메인 값을 바로 호출하도록 리펙토링

--- src/main/java/bridge/controller/BridgeGameController.java
+++ src/main/java/bridge/controller/BridgeGameController.java
@@ -7,9 +7,8 @@ import static bridge.enums.GameCommand.RETRY;
 import bridge.BridgeMaker;
 import bridge.BridgeNumberGenerator;
 import bridge.BridgeRandomNumberGenerator;
-import bridge.domain.Bridge;
 import bridge.domain.User;
-import bridge.service.BridgeGame;
+import bridge.domain.BridgeGame;
 import bridge.service.ViewService;
 import java.util.List;
 
@@ -17,13 +16,12 @@ public class BridgeGameController {
     private final BridgeNumberGenerator bridgeNumberGenerator = new BridgeRandomNumberGenerator();
     private final BridgeMaker bridgeMaker = new BridgeMaker(bridgeNumberGenerator);
     private final ViewService viewService = new ViewService();
-    private final BridgeGame bridgeGame = new BridgeGame();
 
     public void start() {
         printStartGame();
-        Bridge bridge = new Bridge(createBridge());
+        BridgeGame bridgeGame = new BridgeGame(createBridge());
         User user = new User();
-        moveBridge(bridge, user);
+        moveBridge(bridgeGame, user);
     }
 
     public void printStartGame() {
@@ -34,33 +32,41 @@ public class BridgeGameController {
         return bridgeMaker.makeBridge(viewService.inputBridgeSize());
     }
 
-    public void moveBridge(Bridge bridge, User user) {
+    public void moveBridge(BridgeGame bridgeGame, User user) {
         while (!user.isGameOver()) {
             String moveUpOrDown = viewService.inputMoving();
-            boolean pass = bridge.isPass(moveUpOrDown);
-            viewService.printMap(bridge.getLocation(), pass, moveUpOrDown);
-            runPassOrFailCase(pass, bridge, user);
+            boolean pass = bridgeGame.isPass(moveUpOrDown);
+            viewService.printMap(bridgeGame.getLocation(), pass, moveUpOrDown);
+            runPassOrFailCase(pass, bridgeGame, user);
+            completeBridge(bridgeGame, user);
         }
         printResult(user);
     }
 
-    public void runPassOrFailCase(boolean pass, Bridge bridge, User user) {
-        if (pass) {
-            bridgeGame.move(bridge, user);
-        }
-        if (!pass) {
-            String gameCommand = viewService.inputGameCommand();
-            runFailCase(bridge, user, gameCommand);
+    public void completeBridge(BridgeGame bridgeGame, User user) {
+        if (bridgeGame.isEndOfLocation()) {
+            user.finishWithWin();
         }
     }
 
-    public void runFailCase(Bridge bridge, User user, String gameCommand) {
+    public void runPassOrFailCase(boolean pass, BridgeGame bridgeGame, User user) {
+        if (pass) {
+            bridgeGame.move();
+        }
+        if (!pass) {
+            String gameCommand = viewService.inputGameCommand();
+            runFailCase(bridgeGame, user, gameCommand);
+        }
+    }
+
+    public void runFailCase(BridgeGame bridgeGame, User user, String gameCommand) {
         if (gameCommand.equals(RETRY.getCommand())) {
             viewService.clearMap();
-            bridgeGame.retry(bridge, user);
+            user.increaseTryCount();
+            bridgeGame.retry();
         }
         if (gameCommand.equals(QUIT.getCommand())) {
-            bridgeGame.giveUp(user);
+            user.finishWithFail();
         }
     }
 
