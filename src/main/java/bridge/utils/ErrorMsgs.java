8acbc7d79a686c46beae840b21d537eb796da5e5
Kid-Chang <pch3310@gmail.com>
2022-11-22 19:10:13 +0900

    feat(ErrorMsgs): 에러케이스 문장 추가

--- src/main/java/bridge/utils/ErrorMsgs.java
+++ src/main/java/bridge/utils/ErrorMsgs.java
@@ -4,4 +4,5 @@ public class ErrorMsgs {
     public static String ERROR_BRIDGE_SIZE_NOT_INT = "[ERROR] 받은 다리 길이가 숫자가 아닙니다.";
     public static String ERROR_BRIDGE_SIZE_LOW = "[ERROR] 입력하는 다리 길이가 3 이상 20 이하의 숫자이어야 합니다.";
     public static String ERROR_BRIDGE_MOVE_TYPE = "[ERROR] 입력하는 이동 명령어가 U 또는 D 이어야 합니다.";
+    public static String ERROR_COMMAND_TYPE = "[ERROR] 입력하는 게임 명령어가 R 또는 Q 이어야 합니다.";
 }
