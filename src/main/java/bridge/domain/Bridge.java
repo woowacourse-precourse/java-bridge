6a76a6667de1e36014b2501b5dfc552158091499
jeomxon <jeomxon@gmail.com>
2022-11-22 11:38:04 +0900

    chore: Bridge필드 변수명 변경

--- src/main/java/bridge/domain/Bridge.java
+++ src/main/java/bridge/domain/Bridge.java
@@ -1,9 +1,7 @@
 package bridge.domain;
 
-import static java.lang.Integer.*;
 import static java.lang.Integer.parseInt;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import bridge.BridgeMaker;
@@ -13,24 +11,24 @@ public class Bridge {
     private static final String INPUT_NOT_INTEGER_ERROR = "[ERROR] 생성할 다리의 길이는 정수만 입력이 가능합니다.";
     private static final String INPUT_VALID_RANGE_OF_NUMBER_ERROR = "[ERROR] 다리 길이는 3부터 20 사이의 숫자여야 합니다.";
 
-    private final List<String> bridge;
+    private final List<String> blocks;
 
     public Bridge(String input) {
         validateBridgeSize(input);
         BridgeMaker bridgeMaker = new BridgeMaker(new BridgeRandomNumberGenerator());
-        this.bridge = bridgeMaker.makeBridge(parseInt(input));
+        this.blocks = bridgeMaker.makeBridge(parseInt(input));
     }
 
-    public List<String> getBridge() {
-        return bridge;
+    public List<String> getBlocks() {
+        return blocks;
     }
 
     public int getSize() {
-        return bridge.size();
+        return blocks.size();
     }
 
     public String getBlock(int index) {
-        return bridge.get(index);
+        return blocks.get(index);
     }
 
     private void validateBridgeSize(String input) {
@@ -44,7 +42,6 @@ public class Bridge {
         if (!size.matches(regex)) {
             throw new IllegalArgumentException(INPUT_NOT_INTEGER_ERROR);
         }
-
     }
 
     private void validateRange(int size) {
