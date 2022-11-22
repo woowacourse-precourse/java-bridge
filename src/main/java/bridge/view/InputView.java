c2ba3b5af158ec038383e53c5061cdf7b454a059
hhiyeon <hhyyeon0214@gmail.com>
2022-11-22 15:00:49 +0900

    feat:InputView-repeat try catch statement  for valid input

--- src/main/java/bridge/view/InputView.java
+++ src/main/java/bridge/view/InputView.java
@@ -1,5 +1,7 @@
 package bridge.view;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class InputView {
     private static final int BRIDGE_SIZE = 1;
     private static final int MOVING = 2;
@@ -8,16 +10,27 @@ public class InputView {
 
     public String readBridgeSize() {
         while (true) {
-            input = tryCatch(BRIDGE_SIZE);
+            input = repeatForValid(BRIDGE_SIZE);
             break;
         }
         return input;
     }
 
+    private String repeatForValid(int methodType) {
+        try {
+            input = Console.readLine();
+            checkObject(methodType, input);
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+            System.out.println("다시 입력해주세요");
+            input = checkMethod(methodType);
+        }
+        return input;
+    }
 
     public String readMoving() {
         while (true) {
-            input = tryCatch(MOVING);
+            input = repeatForValid(MOVING);
             break;
         }
         return input;
@@ -25,7 +38,7 @@ public class InputView {
 
     public String readGameCommand() {
         while (true) {
-            input = tryCatch(GAME_COMMAND);
+            input = repeatForValid(GAME_COMMAND);
             break;
         }
         return input;
