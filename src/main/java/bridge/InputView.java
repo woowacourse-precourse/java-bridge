8740a60cccc2d1fe916e3b628b5d12fb03a43ac1
YongNyeo <won05121@naver.com>
2022-11-22 14:12:41 +0900

    feat: 이동할 칸 입력 받기 기능 추가

--- src/main/java/bridge/InputView.java
+++ src/main/java/bridge/InputView.java
@@ -15,6 +15,7 @@ public class InputView {
         System.out.println("다리 건너기 게임을 시작합니다.\n");
         return validBridgeSizeNumber();
     }
+
     private int validBridgeSizeNumber() {
         while (true) {
             System.out.println("\n" + "다리의 길이를 입력해주세요.");
@@ -28,6 +29,7 @@ public class InputView {
             }
         }
     }
+
     private void validBridgeSize(int size) {
         if (!(size >= 3 && size <= 20)) {
             throw new IllegalArgumentException("[ERROR] 다리 길이는 3부터 20 사이의 숫자여야 합니다.");
@@ -38,8 +40,18 @@ public class InputView {
      * 사용자가 이동할 칸을 입력받는다.
      */
     public String readMoving() {
-        return null;
+        while (true) {
+            System.out.println("이동할 칸을 선택해주세요. (위: U, 아래: D)");
+            String moving = readLine();
+            try {
+                validMoving(moving);
+                return moving;
+            } catch (IllegalArgumentException e) {
+                System.out.println("[ERROR] 이동할 칸 입력은 U 또는 D 로만 가능합니다.");
+            }
+        }
     }
+
 
     /**
      * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
