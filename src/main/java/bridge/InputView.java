eb308ff01f475568c45e89a5736ba5e18cb4662e
wonseokjee <wsji9404@gmail.com>
2022-11-22 17:29:45 +0900

    Refactor(InputView): 수정 readBridgeSize, readMoving, readGameCommand
    print문의 적절한 위치 찾는 중

--- src/main/java/bridge/InputView.java
+++ src/main/java/bridge/InputView.java
@@ -6,7 +6,6 @@ import static camp.nextstep.edu.missionutils.Console.readLine;
  * 사용자로부터 입력을 받는 역할을 한다.
  */
 public class InputView {
-    BridgeGame bridgeGame = new BridgeGame();
     ValidateError validateError = new ValidateError();
 
     /**
@@ -18,7 +17,6 @@ public class InputView {
     }
 
     public int readBridgeSize(String userInput) {
-        bridgeGame.printInputBridgeSize();
         validateError.validIsNumber(userInput);
         validateError.validIsInRange(userInput);
         return Integer.parseInt(userInput);
@@ -29,16 +27,17 @@ public class InputView {
      * 사용자가 이동할 칸을 입력받는다.
      */
     public String readMoving(String userInput) {
-        bridgeGame.printSelectBridgeUpDown();
+
         validateError.validIsNotSuitableMovingCommand(userInput);
         return userInput;
+
     }
 
     /**
      * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
      */
     public String readGameCommand(String userInput) {
-        bridgeGame.printRestartOrQuit();
+        System.out.println(printOutput.RESTART_OR_QUIT);
         validateError.validIsNotSuitableEndCommand(userInput);
         return userInput;
     }
