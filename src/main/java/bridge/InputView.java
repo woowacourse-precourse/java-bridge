682766d7b9613f6ec92753c948f080e3f611c523
southoftheriver <anwjsrlrhwkd@naver.com>
2022-11-22 18:54:44 +0900

    feat: implement view to get input

--- src/main/java/bridge/InputView.java
+++ src/main/java/bridge/InputView.java
@@ -1,5 +1,10 @@
 package bridge;
 
+import static bridge.GameMsg.GET_LENGTH_MSG;
+import static bridge.GameMsg.GET_WAY_MSG;
+import static bridge.GameMsg.RETRY_MSG;
+import static camp.nextstep.edu.missionutils.Console.readLine;
+
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
  */
@@ -9,20 +14,29 @@ public class InputView {
      * 다리의 길이를 입력받는다.
      */
     public int readBridgeSize() {
-        return 0;
+        System.out.println(GET_LENGTH_MSG.getMsg());
+        String size = input();
+        Validator.validateNumeric(size);
+        return Integer.parseInt(size);
     }
 
     /**
      * 사용자가 이동할 칸을 입력받는다.
      */
     public String readMoving() {
-        return null;
+        System.out.println(GET_WAY_MSG.getMsg());
+        return input();
     }
 
     /**
      * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
      */
     public String readGameCommand() {
-        return null;
+        System.out.println(RETRY_MSG.getMsg());
+        return input();
+    }
+
+    private String input() {
+        return readLine().strip();
     }
 }
