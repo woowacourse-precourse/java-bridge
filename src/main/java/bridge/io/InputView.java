e801381b2eb8d0405f8278de7ac95141588bdc4e
hongjk7777 <51441261+hongjk7777@users.noreply.github.com>
2022-11-22 13:37:29 +0900

    refactor: 필요없는 주석 삭제

--- src/main/java/bridge/io/InputView.java
+++ src/main/java/bridge/io/InputView.java
@@ -3,14 +3,10 @@ package bridge.io;
 import bridge.validator.Validator;
 import camp.nextstep.edu.missionutils.Console;
 
-/**
- * 사용자로부터 입력을 받는 역할을 한다.
- */
+
 public class InputView {
     Validator validator = new Validator();
-    /**
-     * 다리의 길이를 입력받는다.
-     */
+
     public int readBridgeSize() {
         System.out.println();
         System.out.println(InputText.BRIDGE_SIZE.getInputText());
@@ -21,9 +17,6 @@ public class InputView {
         return Integer.parseInt(input);
     }
 
-    /**
-     * 사용자가 이동할 칸을 입력받는다.
-     */
     public String readMoving() {
         System.out.println();
         System.out.println(InputText.MOVING.getInputText());
@@ -34,9 +27,6 @@ public class InputView {
         return input;
     }
 
-    /**
-     * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
-     */
     public String readGameCommand() {
         System.out.println();
         System.out.println(InputText.GAME_COMMAND.getInputText());
