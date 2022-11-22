c4db02b36ac7f2c4245a29e50c4ca43bbed9038b
ckyeon <qsky8520@gmail.com>
2022-11-22 20:50:22 +0900

    feat(InputView): implement 플레이어가 이동할 칸을 입력받는 기능

--- src/main/java/bridge/view/InputView.java
+++ src/main/java/bridge/view/InputView.java
@@ -34,7 +34,8 @@ public class InputView {
      * 사용자가 이동할 칸을 입력받는다.
      */
     public String readMoving() {
-        return null;
+        String moving = Console.readLine();
+        return moving;
     }
 
     /**
