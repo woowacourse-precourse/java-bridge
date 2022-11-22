4fd56e453b965d93dc4dd61e7f85060dfae74ea0
bearjun05 <bearjun05@naver.com>
2022-11-22 18:52:20 +0900

    test : 다리 길이 경계값 통과 테스트 추가

--- src/test/java/bridge/ValidatorTest.java
+++ src/test/java/bridge/ValidatorTest.java
@@ -1,23 +1,29 @@
 package bridge;
 
+import static bridge.Validator.*;
+import static org.assertj.core.api.Assertions.assertThatCode;
+import static org.assertj.core.api.Assertions.assertThatNoException;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
-import static org.junit.jupiter.api.Assertions.*;
 
-import java.util.List;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.CsvFileSource;
-import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 class ValidatorTest {
 
     @DisplayName("다리 길이 경계값 예외 테스트 ")
     @ParameterizedTest
-    @ValueSource(ints = {21, 2})
+    @ValueSource(ints = {2, 21})
     void validateSizeRangeExceptionTest(int size) {
-        assertThatThrownBy(() -> Validator.validateSizeRange(size))
+        assertThatThrownBy(() -> validateSizeRange(size))
                 .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("다리 길이 경계값 통과 테스트 ")
+    @ParameterizedTest
+    @ValueSource(ints = {3, 20})
+    void validateSizeRangeTest(int size) {
+        assertThatCode(() -> validateSizeRange(size)).doesNotThrowAnyException();
     }
 }
