da12f193b5439be420d1a61496cf8eaf88cb2347
hyunseo <goatsfish@gmail.com>
2022-11-22 13:40:57 +0900

    feat : 다리 게임 매니저가 플레이어 다리 이동 요청 기능 추가

--- src/main/java/bridge/BridgeGameManager.java
+++ src/main/java/bridge/BridgeGameManager.java
@@ -4,6 +4,9 @@ import bridge.domain.*;
 import bridge.view.InputView;
 import bridge.view.OutputView;
 
+import static bridge.domain.BridgeGameState.*;
+import static bridge.domain.BridgeGameState.START;
+
 public class BridgeGameManager {
 
     private static final InputView inputView = new InputView();
@@ -18,6 +21,26 @@ public class BridgeGameManager {
         while (true) {
             try {
                 return inputView.readBridgeSize();
+            } catch (IllegalArgumentException e) {
+                outputView.printException(e.getMessage());
+            }
+        }
+    }
+
+    private BridgeGameState handlePlayerMove() {
+        while (true) {
+            try {
+                return bridgeGame.move(getMoveType());
+            } catch (IllegalArgumentException e) {
+                outputView.printException(e.getMessage());
+            }
+        }
+    }
+
+    private BridgeMoveType getMoveType() {
+        while (true) {
+            try {
+                return inputView.readMoving();
             } catch (IllegalArgumentException e) {
                 outputView.printException(e.getMessage());
             }
