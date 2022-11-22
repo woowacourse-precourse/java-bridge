dedf52185d4b850e795529b22fd99869579ec27b
southoftheriver <anwjsrlrhwkd@naver.com>
2022-11-22 18:55:35 +0900

    feat: implement view to show user domain object

--- src/main/java/bridge/OutputView.java
+++ src/main/java/bridge/OutputView.java
@@ -1,5 +1,11 @@
 package bridge;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 /**
  * 사용자에게 게임 진행 상황과 결과를 출력하는 역할을 한다.
  */
@@ -10,7 +16,54 @@ public class OutputView {
      * <p>
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void printMap() {
+    public void printMap(List<String> dir, boolean isFail) {
+        final List<String> key = List.of("U", "D");
+        Map<String, List<String>> initialMap = initMap(dir.size());
+        Map<String, List<String>> map = setWay(initialMap, dir, isFail);
+
+        final String prefix = "[ ";
+        final String suffix = " ]";
+        for (String s : key) {
+            System.out.println(prefix + String.join(" | ", map.get(s)) + suffix);
+        }
+    }
+
+    private Map<String, List<String>> setWay(Map<String, List<String>> map,
+        List<String> dir, boolean isFail) {
+
+        for (int i = 0; i < dir.size(); i++) {
+            String success = getSuccessOrFail(i, dir, isFail);
+            List<String> way = map.get(dir.get(i));
+            way.set(i, success);
+            map.put(dir.get(i), way);
+        }
+        return map;
+    }
+
+    private String getSuccessOrFail(int i, List<String> dir, boolean isFail) {
+        final String success = "O";
+        final String fail = "X";
+        if (isFail && i + 1 == dir.size()) {
+            return fail;
+        }
+        return success;
+    }
+
+    private Map<String, List<String>> initMap(int size) {
+        Map<String, List<String>> dirMap = new HashMap<>();
+        for (Direction dir : Direction.values()) {
+            List<String> initialMap = generateMap(size);
+            dirMap.put(dir.toString(), initialMap);
+        }
+
+        return dirMap;
+    }
+
+    private List<String> generateMap(int size) {
+        String initialValue = " ";
+        String[] map = new String[size];
+        Arrays.fill(map, initialValue);
+        return new ArrayList<>(Arrays.asList(map));
     }
 
     /**
@@ -18,6 +71,19 @@ public class OutputView {
      * <p>
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void printResult() {
+    public void printResult(boolean isFail, int times) {
+        System.out.println(GameMsg.RESULT_MSG.getMsg() + "\n");
+        System.out.println(GameMsg.SUCCESS_MSG.getMsg() + getGameResult(isFail));
+        System.out.println(GameMsg.TRY_COUNT_MSG.getMsg() + times);
+    }
+
+    private String getGameResult(boolean isFail) {
+        String success = "성공";
+        String fail = "실패";
+
+        if (isFail) {
+            return fail;
+        }
+        return success;
     }
 }
