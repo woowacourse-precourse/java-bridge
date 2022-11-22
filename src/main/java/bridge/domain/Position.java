6b4353ce0d37258a5e003917c7c8824047297bc0
yuntaeLee <leeyt1201@gmail.com>
2022-11-22 13:41:31 +0900

    style: rename class

--- src/main/java/bridge/domain/Position.java
+++ src/main/java/bridge/domain/Position.java
@@ -2,30 +2,30 @@ package bridge.domain;
 
 import java.util.Arrays;
 
-public enum BridgePosition {
+public enum Position {
     UP("U", 1),
     DOWN("D", 0);
 
     private final String command;
     private final int value;
 
-    BridgePosition(String command, int value) {
+    Position(String command, int value) {
         this.command = command;
         this.value = value;
     }
 
     public static String getCommandByValue(int value) {
-        return Arrays.stream(BridgePosition.values())
+        return Arrays.stream(Position.values())
                 .filter(position -> position.value == value)
-                .findFirst()
+                .findAny()
                 .orElseThrow(() -> new IllegalArgumentException(""))
                 .command;
     }
 
-    public static BridgePosition getPositionByCommand(String command) {
-        return Arrays.stream(BridgePosition.values())
+    public static Position getPositionByCommand(String command) {
+        return Arrays.stream(Position.values())
                 .filter(position -> position.command.equals(command))
-                .findFirst()
+                .findAny()
                 .orElseThrow(() -> new IllegalArgumentException(""));
     }
 }
