20383428de9cb443dbeca7e85ff2e40d4752eeff
jinhyeon <vmflwu123@gmail.com>
2022-11-22 23:32:17 +0900

    revert(BridgeGame): move 메서드 복구

--- src/main/java/bridge/core/BridgeGame.java
+++ src/main/java/bridge/core/BridgeGame.java
@@ -39,12 +39,15 @@ public class BridgeGame {
 
     public ProcessCondition move(String selectBlock) {
         try {
-            if (bridge.checkPassableBlock(gameStatusOperator.changePosition(), selectBlock)) return PassCondition.PASS;
+            int nextPosition = gameStatusOperator.changePosition();
+            if (bridge.checkPassableBlock(nextPosition, selectBlock)) return PassCondition.PASS;
             return PassCondition.FAIL;
         } catch (InvalidInputException e) {
             gameStatusOperator.turnBackPosition();
             ExceptionHandler.handle(e);
             return null;
+        } catch (Exception e) {
+            throw new CommonException(Error.FAIL, "플레이어 이동");
         }
     }
 
