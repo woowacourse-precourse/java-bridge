77307f272e885852651acd13084e5905e474a0a1
jsk5341 <102288039+jsk5341@users.noreply.github.com>
2022-11-22 16:10:22 +0900

    feat: implement function to handle exception
    exception when user puts wrong next step

--- src/main/java/bridge/InputView.java
+++ src/main/java/bridge/InputView.java
@@ -4,6 +4,7 @@ import camp.nextstep.edu.missionutils.Console;
 
 
 // TODO 입력 예외 처리를 여기서 할까 밖에서 할까?
+
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
  */
@@ -16,31 +17,31 @@ public class InputView {
 
         System.out.println("다리의 길이를 입력해주세요.");
 
-        String bridgeLengthInput= Console.readLine();
-        if(!bridgeLengthInput.matches("[0-9]+")){
+        String bridgeLengthInput = Console.readLine();
+        if (!bridgeLengthInput.matches("[0-9]+")) {
             throw new IllegalArgumentException("[ERROR] 숫자만 입력 가능합니다.");
         }
         int bridgeLength = Integer.parseInt(bridgeLengthInput);
 
-        if(bridgeLength<3 && bridgeLength>20){
+        if (bridgeLength < 3 && bridgeLength > 20) {
             throw new IllegalArgumentException("[ERROR] 다리의 길이는 3이상 20이하로만 설정 가능합니다.");
         }
 
         return bridgeLength;
     }
-    public int readBridgeSizeWithValidityCheck(){
+
+    public int readBridgeSizeWithValidityCheck() {
         int bridgeLength;
-        while(true){
-            try{
-                bridgeLength=readBridgeSize();
+        while (true) {
+            try {
+                bridgeLength = readBridgeSize();
                 break;
-            }catch(IllegalArgumentException e){
+            } catch (IllegalArgumentException e) {
                 System.out.println(e.getMessage());
             }
         }
         return bridgeLength;
     }
-
 
 
     /**
@@ -49,9 +50,27 @@ public class InputView {
     public String readMoving() {
         String nextStepInput;
         nextStepInput = Console.readLine();
+        if (!nextStepInput.equals("U") || !nextStepInput.equals("D")) {
+            throw new IllegalArgumentException("[ERROR] U 혹은 D만 입력 가능합니다.");
+        }
+        if (nextStepInput.length() > 1) {
+            throw new IllegalArgumentException("[ERROR] 한자리만 입력 가능합니다.");
+        }
         return nextStepInput;
     }
 
+    public String readMovingWithValidityCheck() {
+        String nextStepInput;
+        while (true) {
+            try {
+                nextStepInput=readMoving();
+                break;
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            }
+        }
+        return nextStepInput;
+    }
 
     /**
      * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
@@ -63,7 +82,6 @@ public class InputView {
 
         return askRetry;
     }
-
 
 
 }
