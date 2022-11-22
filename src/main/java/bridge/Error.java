c6a2b8bde3a187134c9fc2721618c1f9de090ea6
Dasol Jung <greteljds0251@gmail.com>
2022-11-22 21:40:39 +0900

    feat: 이동할 칸으로 입력받은 값이 U, D가 아닌 경우

--- src/main/java/bridge/Error.java
+++ src/main/java/bridge/Error.java
@@ -6,4 +6,10 @@ public class Error {
             throw new IllegalAccessError("[ERROR] 다리의 길이가 3~20 사이에 포함되지 않습니다.");
         }
     }
+
+    public void errorMoving(String moving) {
+        if (moving.equals("D") || moving.equals("U")) {
+            throw new IllegalAccessError("[ERROR] 이동할 칸이 올바르지 않습니다.");
+        }
+    }
 }
