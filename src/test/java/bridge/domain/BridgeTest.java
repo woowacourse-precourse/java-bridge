293d87d8c85ea0f52f83c62479d577f05af5ace5
Somi Kim <rlathal009@gmail.com>
2022-11-22 20:13:45 +0900

    test : 정답 여부 판단 테스트 코드 추가

--- src/test/java/bridge/domain/BridgeTest.java
+++ src/test/java/bridge/domain/BridgeTest.java
@@ -3,7 +3,10 @@ package bridge.domain;
 import bridge.BridgeMaker;
 import bridge.BridgeRandomNumberGenerator;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -23,5 +26,22 @@ class BridgeTest {
         //then
         assertThat(newBridge.getSize()).isEqualTo(10);
         assertThat(newBridge.getBridge()).allMatch(o -> o.equals("U") || o.equals("D"));
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {0, 1, 2, 3, 4, 5, 10})
+    public void 정답_판단하기(int size) throws Exception {
+        //given
+        List<String> bridge = bridgeMaker.makeBridge(size);
+        Bridge answer = new Bridge(bridge);
+        Bridge user = new Bridge(new ArrayList<>());
+
+        //when
+        for(int i=0; i<size; i++)
+            user.moveNext(answer.getBridge().get(i));
+
+        //then
+        assertThat(user.matchAnswer(answer)).allMatch(o -> o.equals("O"));
+        assertThat(user.matchAnswer(answer).size()).isEqualTo(size);
     }
 }
