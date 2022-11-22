e7f601fcc1306eb7e26dd926d898f3ec26c17d1c
yu-heejin <jkgt0521@naver.com>
2022-11-23 00:01:36 +0900

    fix: 예외처리 수정 아깝다...

--- src/main/java/bridge/view/InputView.java
+++ src/main/java/bridge/view/InputView.java
@@ -16,9 +16,13 @@ public class InputView {
         System.out.println("다리의 길이를 입력해주세요.");
         String bridgeSize = Console.readLine();
 
-        CHECK_EXCEPTION.isNumber(bridgeSize);
-        return Integer.parseInt(bridgeSize);
-
+        try {
+            CHECK_EXCEPTION.isNumber(bridgeSize);
+            return Integer.parseInt(bridgeSize);
+        } catch (IllegalArgumentException ex) {
+            System.out.println("[ERROR] 올바른 길이 값이 아닙니다.");
+        }
+        return -1;
     }
 
     /**
@@ -27,9 +31,14 @@ public class InputView {
     public String readMoving() {
         System.out.println("이동할 칸을 선택해주세요. (위: U, 아래: D)");
         String moveSpaceAnswer = Console.readLine();
+        try {
+            CHECK_EXCEPTION.isInputUorD(moveSpaceAnswer);
+            return moveSpaceAnswer;
+        } catch (IllegalArgumentException ex) {
+            System.out.println("[ERROR] 올바른 입력 값이 아닙니다.");
+        }
 
-        CHECK_EXCEPTION.isInputUorD(moveSpaceAnswer);
-        return moveSpaceAnswer;
+        return null;
     }
 
     /**
@@ -39,7 +48,13 @@ public class InputView {
         System.out.println("게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)");
         String retryAnswer = Console.readLine();
 
-        CHECK_EXCEPTION.isInputRorQ(retryAnswer);
-        return retryAnswer;
+        try {
+            CHECK_EXCEPTION.isInputRorQ(retryAnswer);
+        } catch (IllegalArgumentException ex) {
+            System.out.println("[ERROR] 올바른 입력 값이 아닙니다.");
+        }
+
+
+        return null;
     }
 }
