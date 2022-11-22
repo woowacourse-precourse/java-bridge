c8d9775c18bab7fb9b0e188c1f58268a82d599e4
JeongHunHui <deca3248@gmail.com>
2022-11-22 22:55:48 +0900

    test: 게임 명령어 검증로직 테스트코드 작성 완료

---
+++ src/test/java/bridge/validator/domain/GameCommandValidatorTest.java
@@ -0,0 +1,25 @@
+package bridge.validator.domain;
+
+import bridge.domain.GameCommand;
+import bridge.exception.InvalidGameCommandException;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class GameCommandValidatorTest {
+  @Test
+  @DisplayName("잘못된 명령어를 입력하면 예외가 반환된다.")
+  void invalidGameCommandInputTest() {
+    assertThrows(InvalidGameCommandException.class, () -> GameCommandValidator.validate("D"));
+    assertThrows(InvalidGameCommandException.class, () -> GameCommandValidator.validate("QQ"));
+    assertThrows(InvalidGameCommandException.class, () -> GameCommandValidator.validate(" "));
+  }
+
+  @Test
+  @DisplayName("옳바른 명령어를 입력한다.")
+  void validGameCommandInputTest() {
+    assertDoesNotThrow(() -> GameCommandValidator.validate(GameCommand.QUIT.getCommand()));
+    assertDoesNotThrow(() -> GameCommandValidator.validate(GameCommand.RETRY.getCommand()));
+  }
+}
