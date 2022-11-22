b071cb5f0bbbce62d1a80fea657342391c88cb4d
xxeol2 <jhj01177@naver.com>
2022-11-22 17:42:00 +0900

    comment: 주석 제거

--- src/main/java/bridge/BridgeGame.java
+++ src/main/java/bridge/BridgeGame.java
@@ -11,9 +11,6 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-/**
- * 다리 건너기 게임을 관리하는 클래스
- */
 public class BridgeGame {
 
     private final List<String> bridge;
@@ -25,11 +22,6 @@ public class BridgeGame {
         this.bridge = bridge;
     }
 
-    /**
-     * 사용자가 칸을 이동할 때 사용하는 메서드
-     * <p>
-     * 이동을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
-     */
     public boolean move(String moving) {
         increaseRound();
         String row = bridge.get(round - 1);
@@ -38,11 +30,6 @@ public class BridgeGame {
         return result;
     }
 
-    /**
-     * 사용자가 게임을 다시 시도할 때 사용하는 메서드
-     * <p>
-     * 재시작을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
-     */
     public void retry() {
         increaseTrialCount();
         resetGame();
--- src/main/java/bridge/view/InputView.java
+++ src/main/java/bridge/view/InputView.java
@@ -5,32 +5,22 @@ import static bridge.validator.GameCommandValidator.validateGameCommand;
 import static bridge.validator.MovingValidator.validateMoving;
 import static camp.nextstep.edu.missionutils.Console.readLine;
 
-/**
- * 사용자로부터 입력을 받는 역할을 한다.
- */
+
 public class InputView {
 
-    /**
-     * 다리의 길이를 입력받는다.
-     */
+
     public int readBridgeSize() {
         String bridgeSize = readLine().trim();
         validateBridgeSize(bridgeSize);
         return Integer.parseInt(bridgeSize);
     }
 
-    /**
-     * 사용자가 이동할 칸을 입력받는다.
-     */
     public String readMoving() {
         String moving = readLine().trim();
         validateMoving(moving);
         return moving;
     }
-
-    /**
-     * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
-     */
+    
     public String readGameCommand() {
         String gameCommand = readLine().trim();
         validateGameCommand(gameCommand);
