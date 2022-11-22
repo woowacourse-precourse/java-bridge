ca2497c6aca9ea12729d2f879fec633238c76b12
SEUNGHEE96 <dnffktmdgml@gmail.com>
2022-11-22 23:50:44 +0900

    feat: 다리 통과 실패 테스트 케이스 추가

--- src/test/java/model/BridgeGameTest.java
+++ src/test/java/model/BridgeGameTest.java
@@ -5,14 +5,23 @@ import static org.assertj.core.api.Assertions.assertThat;
 import java.util.Arrays;
 
 import bridge.model.BridgeGame;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class BridgeGameTest {
-
+    private BridgeGame bridgeGame;
+    @BeforeEach
+    void setUp() {
+        bridgeGame = new BridgeGame();
+    }
     @Test
-    public void 다리통과() {
-        BridgeGame bridgeGame = new BridgeGame();
+    public void 다리통과성공() {
         String result = bridgeGame.resultOfPhrase(Arrays.asList("U", "U", "U"), Arrays.asList("U", "U", "U"));
         assertThat(result).isEqualTo("[ O | O | O ]" + System.lineSeparator() + "[   |   |   ]");
+    }
+    @Test
+    public void 다리통과실패() {
+        String result = bridgeGame.resultOfPhrase(Arrays.asList("U", "D"), Arrays.asList("U", "U", "U"));
+        assertThat(result).isEqualTo("[ O |   ]" + System.lineSeparator() + "[   | X ]");
     }
 }
