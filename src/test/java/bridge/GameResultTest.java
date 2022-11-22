092aa49770484ec93bb2e51686109e72ea1667d6
eun-byeol <62530884+eun-byeol@users.noreply.github.com>
2022-11-22 17:55:25 +0900

    test(GameResultTest): 검증 기준을 문자로 수정
    결과 리스트 검증시 contains(1)에서 contains(X) 방식으로 수정함.

--- src/test/java/bridge/GameResultTest.java
+++ src/test/java/bridge/GameResultTest.java
@@ -9,19 +9,19 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class GameResultTest {
     GameResult gameResult = new GameResult();
 
-    @DisplayName("고른 칸 결과 리스트에 정답(2)을 추가하고 고르지 않은 칸 리스트에 공백(0)을 추가한다.")
+    @DisplayName("고른 칸 결과 리스트에 정답을 추가하고 고르지 않은 칸 리스트에 공백을 추가한다.")
     @Test
     void addUpSideGameResult() {
         gameResult.addGameResult(true, new PlayerMoving("U"));
-        assertThat(gameResult.getUpSideResult()).contains(2);
-        assertThat(gameResult.getDownSideResult()).contains(0);
+        assertThat(gameResult.getUpSideResult()).contains("O");
+        assertThat(gameResult.getDownSideResult()).contains(" ");
     }
 
-    @DisplayName("고른 칸 결과 리스트에 오답(1)을 추가하고 고르지 않은 칸 리스트에 공백(0)을 추가한다.")
+    @DisplayName("고른 칸 결과 리스트에 오답을 추가하고 고르지 않은 칸 리스트에 공백을 추가한다.")
     @Test
     void addDownSideGameResult() {
         gameResult.addGameResult(false, new PlayerMoving("D"));
-        assertThat(gameResult.getDownSideResult()).contains(1);
-        assertThat(gameResult.getUpSideResult()).contains(0);
+        assertThat(gameResult.getDownSideResult()).contains("X");
+        assertThat(gameResult.getUpSideResult()).contains(" ");
     }
 }
