170a0e598599d224e0ca6a9279a72a9a25ed12b7
gyumin159 <gyumin159@naver.com>
2022-11-22 17:19:45 +0900

    style : 메소드 설명 정리

--- src/main/java/bridge/OutputView.java
+++ src/main/java/bridge/OutputView.java
@@ -7,8 +7,7 @@ public class OutputView {
 
     /**
      * 현재까지 이동한 다리의 상태를 정해진 형식에 맞춰 출력한다.
-     * <p>
-     * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
+     *
      */
     public void printMap(Map map) {
         for (String printableMap : map.getPrintableMap()){
@@ -19,8 +18,7 @@ public class OutputView {
 
     /**
      * 게임의 최종 결과를 정해진 형식에 맞춰 출력한다.
-     * <p>
-     * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
+     *
      */
     public void printResult(Map map, boolean isGameCleared, int totalAttempts) {
         String winOrLose = translateWinOrLose(isGameCleared);
@@ -55,5 +53,4 @@ public class OutputView {
         }
         System.out.println(comment.line + additionalInputs);
     }
-
 }
