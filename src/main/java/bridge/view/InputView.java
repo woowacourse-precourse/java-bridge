c5901801ef2ccb5bea7e9a4b36e7a1048872f6b0
chj <bluedog129@gmail.com>
2022-11-22 20:43:59 +0900

    feat(InputView) : 플레이어의 입력에 대한 뷰클래스 구현

--- src/main/java/bridge/view/InputView.java
+++ src/main/java/bridge/view/InputView.java
@@ -1,31 +1,57 @@
-package bridge;
+package bridge.view;
 
-import bridge.message.Message;
-import camp.nextstep.edu.missionutils.Console;
+import bridge.controller.ExceptionController;
+
+import static camp.nextstep.edu.missionutils.Console.readLine;
 
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
  */
 public class InputView {
+    private final OutputView outputView = new OutputView();
+    private final ExceptionController exception = new ExceptionController();
     /**
      * 다리의 길이를 입력받는다.
      */
-    public String readBridgeSize() {
-        System.out.println(Message.INPUT_SIZE.getMessage());
-        return Console.readLine();
+    public int readBridgeSize() {
+        while (true) {
+            String input = readLine();
+            try {
+                exception.checkBridgeSize(input);
+                return Integer.parseInt(input);
+            } catch (IllegalArgumentException illegalArgumentException) {
+                outputView.printError(illegalArgumentException);
+            }
+        }
     }
 
     /**
      * 사용자가 이동할 칸을 입력받는다.
      */
-    public String readMoving() {
-        return null;
+    public String readChoice() {
+        while (true) {
+            String input = readLine();
+            try {
+                exception.checkMoving(input);
+                return input;
+            } catch (IllegalArgumentException illegalArgumentException) {
+                outputView.printError(illegalArgumentException);
+            }
+        }
     }
 
     /**
      * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
      */
-    public String readGameCommand() {
-        return null;
+    public String readRetryCommand() {
+        while (true) {
+            try {
+                String input = readLine();
+                exception.checkRetryCommand(input);
+                return input;
+            } catch (IllegalArgumentException illegalArgumentException) {
+                outputView.printError(illegalArgumentException);
+            }
+        }
     }
 }
