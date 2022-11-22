7c77947700aa2f6591d42ddf017b965d7eb08cf7
jhsseonn <hosu0125@gmail.com>
2022-11-22 14:40:41 +0900

    feat: 사용자가 이동할 칸 입력받기

--- src/main/java/bridge/InputView.java
+++ src/main/java/bridge/InputView.java
@@ -22,7 +22,9 @@ public class InputView extends InputConfig {
      * 사용자가 이동할 칸을 입력받는다.
      */
     public String readMoving() {
-        return null;
+        System.out.println(SELECT_BRIDGE);
+
+        return Console.readLine();
     }
 
     /**
