657ef36ad585484188af32b1c2270057df3670a1
isieun0908 <supiya0908@naver.com>
2022-11-22 22:41:15 +0900

    Feat: 다리 생성

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -1,12 +1,18 @@
 package bridge;
 
+import java.util.List;
+
 public class Application {
 
     public static void main(String[] args) {
         // TODO: 프로그램 구현
         System.out.println("다리 건너기 게임을 시작합니다./n");
         InputView inputView = new InputView();
-        inputView.readBridgeSize();
+        int size = inputView.readBridgeSize();
 
+        BridgeRandomNumberGenerator bridgeRandomNumberGenerator = new BridgeRandomNumberGenerator();
+        BridgeMaker bridgeMaker = new BridgeMaker(bridgeRandomNumberGenerator);
+        List<String> bridge = bridgeMaker.makeBridge(size);
+        System.out.println(bridge);
     }
 }
