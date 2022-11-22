98366a62ab4452eb8224214c3cefe8a748b30136
aak2075 <35948985+aak2075@users.noreply.github.com>
2022-11-22 18:12:04 +0900

    style(bridge) : 불필요한 코멘트 제거

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -5,7 +5,6 @@ import bridge.controller.BridgeController;
 public class Application {
 
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
         BridgeController bridgeController = new BridgeController();
         bridgeController.run();
     }
