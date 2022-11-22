2dd53412b6f8469bd9b5f8c5dc037b1eb36a6b2c
kathyleesh <90851865+kathyleesh@users.noreply.github.com>
2022-11-22 16:09:45 +0900

    style(BridgeGame) : 리스트 명명법 오류로 수정

--- src/main/java/bridge/BridgeGame.java
+++ src/main/java/bridge/BridgeGame.java
@@ -13,9 +13,9 @@ public class BridgeGame {
      * <p>
      * 이동을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public static String move(String selectMove, List<String> bridgeshape) {
-        if (Objects.equals(selectMove, bridgeshape.get(0))){
-            bridgeshape.remove(0);
+    public static String move(String selectMove, List<String> bridgeShape) {
+        if (Objects.equals(selectMove, bridgeShape.get(0))){
+            bridgeShape.remove(0);
             return selectMove;
         }
         return "END";
