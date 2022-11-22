8d99c42823e27c41afe65fdc2ca9cd4d1bea732d
saehim <saeilee.vxv@gmail.com>
2022-11-22 20:23:43 +0900

    refactor (view) : 에러 발생시 반복 삽입 기능 구현방식 변경

--- src/main/java/bridge/view/InputView.java
+++ src/main/java/bridge/view/InputView.java
@@ -1,6 +1,4 @@
-package view;
-
-import camp.nextstep.edu.missionutils.Console;
+package bridge.view;
 
 import static camp.nextstep.edu.missionutils.Console.readLine;
 
@@ -9,58 +7,67 @@ import static camp.nextstep.edu.missionutils.Console.readLine;
  */
 public class InputView {
     StringBuilder sb;
+    static final String ASK_BRIDGE_LONG = "다리의 길이를 입력해주세요.";
+    static final String ASK_MOVE_BRIDGE = "\n이동할 칸을 선택해주세요. (위: U, 아래: D)";
+    static final String ASK_RETRY = "\n게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)";
+    static final String MOVE_COMMAND = "DU";
+    static final String RETRY_COMMAND = "RQ";
 
     /**
      * 다리의 길이를 입력받는다.
      */
-    static public int readBridgeSize() {
-        System.out.println(ViewMessage.ASK_BRIDGE_LONG);
-        int size = readInt();
-        return size;
+    public static int readBridgeSize() {
+        System.out.println(ASK_BRIDGE_LONG);
+        return readInt(readLine());
     }
 
     /**
      * 사용자가 이동할 칸을 입력받는다.
      */
     public static String readMoving() {
-        while (true) {
-            try {
-                System.out.println(ViewMessage.ASK_MOVE_BRIDGE);
-                String s = vaildUD();
-                return  s;
-            } catch (IllegalArgumentException e) {
-                e.printStackTrace();
-            }
-        }
+        System.out.println(ASK_MOVE_BRIDGE);
+        return vaildMoveCommand(readLine());
     }
 
     /**
      * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
      */
-    public String readGameCommand() {
-        System.out.println(ViewMessage.ASK_RETRY);
-        String s = readLine();
-        // 예외처리 : 지정된 문자열이 아닐 때
-        return s;
+    public static String readGameCommand() {
+        System.out.println(ASK_RETRY);
+        return vaildRetryCommand(readLine());
     }
 
-    static private int readInt() {
-        while (true) {
-            try {
-                return Integer.parseInt(readLine());
-            } catch (NumberFormatException e) {
-                e.getStackTrace();
-                System.out.println("숫자가 아닙니다. 다시 입력하세요.");
+    static private int readInt(String number) {
+        try {
+            return Integer.parseInt(number);
+        } catch (NumberFormatException e) {
+            System.out.println("[ERROR] 숫자가 아닙니다. 다시 입력하세요.");
+            return readBridgeSize();
+        }
+    }
+
+
+    private static String vaildMoveCommand(String move) {
+        try {
+            if (!MOVE_COMMAND.contains(move)) {
+                throw new IllegalArgumentException("[ERROR] U 또는 D를 입력해 주세요.");
             }
+            return move;
+        } catch (IllegalArgumentException e) {
+            e.printStackTrace();
+            return readMoving();
         }
     }
 
-    static private String vaildUD() {
-        String move = readLine();
-        if (!move.equals("U") && !move.equals("D")) {
-            throw new IllegalArgumentException("[ERROR]");
+    private static String vaildRetryCommand(String command) {
+        try {
+            if (!RETRY_COMMAND.contains(command)) {
+                throw new IllegalArgumentException("[ERROR] R 또는 Q를 입력해 주세요.");
+            }
+            return command;
+        } catch (IllegalArgumentException e) {
+            e.printStackTrace();
+            return readGameCommand();
         }
-        return move;
     }
-
 }
