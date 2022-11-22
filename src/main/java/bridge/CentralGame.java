0592e39081b4990d33afd7c098de66e0a4232a3c
DESKTOP-AUSH0GT\Power <sgh0718@naver.com>
2022-11-22 18:30:20 +0900

    feat : 게임 중앙 처리 기능 클래스의 칸이 넘어가거나 재시작 처리 기능
    게임 진행에 필요한 메서드를 모아서 관리하는 기능

--- src/main/java/bridge/CentralGame.java
+++ src/main/java/bridge/CentralGame.java
@@ -7,6 +7,7 @@ public class CentralGame {
 
     BridgeRandomNumberGenerator random = new BridgeRandomNumberGenerator();
     BridgeMaker answer = new BridgeMaker(random);
+    BridgeGame core = new BridgeGame();
 
     public String answerStep(List<String> answerList, int blankNumber) {
         return answerList.get(blankNumber);
@@ -18,6 +19,21 @@ public class CentralGame {
 
     public boolean Compare(String answerBlank, String userUpDown) {
         return answerBlank.equals(userUpDown);
+    }
+
+    public int centralMove(int blankNumber) {
+        return blankNumber + 1;
+    }
+
+    public int centralRetry(int count) {
+        return core.retry(count);
+    }
+
+    public List<Integer> result(int blankNumber, int count) {
+        List<Integer> resultList = new ArrayList<>();
+        resultList.add(blankNumber);
+        resultList.add(count);
+        return resultList;
     }
 
 }
