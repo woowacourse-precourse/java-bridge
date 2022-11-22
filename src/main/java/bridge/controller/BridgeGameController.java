ce875287fcee5769a38b98c8155b3f9967217216
juno-junho <ssmm0205@naver.com>
2022-11-22 19:35:25 +0900

    feat: create method startBridgeGame
    controller에서 service와 domain을 받아 게임을 시작하는 메서드 생성

--- src/main/java/bridge/controller/BridgeGameController.java
+++ src/main/java/bridge/controller/BridgeGameController.java
@@ -1,4 +1,23 @@
 package bridge.controller;
 
+import bridge.service.BridgeGameService;
+import bridge.view.InputView;
+import bridge.view.OutputView;
+
 public class BridgeGameController {
+
+    private final InputView inputView;
+    private final OutputView outputView;
+    private final BridgeGameService bridgeGameService;
+
+    public BridgeGameController(InputView inputView, OutputView outputView, BridgeGameService bridgeGameService) {
+        this.inputView = inputView;
+        this.outputView = outputView;
+        this.bridgeGameService = bridgeGameService;
+    }
+
+    public void startBridgeGame() {
+        outputView.printStartMessage();
+    }
+
 }
