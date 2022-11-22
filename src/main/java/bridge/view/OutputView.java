0d3ecc2f1404bece3b1c8d898fe869d614939368
ddubidubap <park62000@gmail.com>
2022-11-22 13:17:20 +0900

    refactor: 게임 진행을 위한 문구 Constant 클래스로 이동

--- src/main/java/bridge/view/OutputView.java
+++ src/main/java/bridge/view/OutputView.java
@@ -2,17 +2,9 @@ package bridge.view;
 
 import java.util.List;
 
-public class OutputView {
-    static final String START_GAME = "다리 건너기 게임을 시작합니다.";
-    static final String INPUT_BRIDGE_SIZE = "다리의 길이를 입력해주세요.";
-    static final String INPUT_MOVING = "이동할 칸을 선택해주세요. (위: U, 아래: D)";
-    static final String INPUT_COMMAND = "게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)";
-    static final String RESULT = "최종 게임 결과";
-    static final String IS_SUCCESSFUL = "게임 성공 여부: ";
-    static final String SUCCESS = "성공";
-    static final String FAIL = "실패";
-    static final String TOTAL_TRIES = "총 시도한 횟수: ";
+import static bridge.Constants.*;
 
+public class OutputView {
     public void printStart() {
         System.out.println(START_GAME);
         System.out.println();
