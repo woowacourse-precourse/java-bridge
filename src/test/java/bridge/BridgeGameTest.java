5a62ec7a6a7e3a921b90f76402ac07f34e5220ca
Bumsoo <rhqjatn2398@naver.com>
2022-11-22 17:17:05 +0900

    test(BridgeGameTest): 재시작 기능의 테스트 코드 추가

--- src/test/java/bridge/BridgeGameTest.java
+++ src/test/java/bridge/BridgeGameTest.java
@@ -7,6 +7,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.stream.Stream;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.CsvSource;
@@ -87,4 +88,25 @@ class BridgeGameTest {
         );
     }
 
+    @ParameterizedTest
+    @MethodSource("재시작_입력_생성")
+    void retry(List<String> moveCommands) {
+        for (var moveCommand : moveCommands) {
+            bridgeGame.move(moveCommand);
+        }
+
+        bridgeGame.retry();
+
+        assertThat(bridgeGame.isCompleted()).isEqualTo(false);
+    }
+
+    private static Stream<List<String>> 재시작_입력_생성() {
+        return Stream.of(
+                List.of("U", "U", "U", "D"),
+                List.of("U", "U", "D"),
+                List.of("U", "D"),
+                List.of("D"),
+                List.of("U", "U", "U")
+        );
+    }
 }
