19832c38f4a08e6be25242b7580c72217367f8e1
Yurim Park <63230463+lina1919@users.noreply.github.com>
2022-11-22 20:59:15 +0900

    feat : add bridge print constant

--- src/main/java/bridge/Enum/GameOutput.java
+++ src/main/java/bridge/Enum/GameOutput.java
@@ -1,2 +1,22 @@
-package bridge.Enum;public enum GameOutput {
+package bridge.Enum;
+
+public enum GameOutput {
+    COMMA(", "),
+    BAR("|"),
+    PASS_POSSIBLE(" O "),
+    PASS_IMPOSSIBLE(" X "),
+    EMPTY("   "),
+    BRIDGE_START("[ "),
+    BRIDGE_END(" ]"),
+    ;
+
+    private final String message;
+
+    GameOutput(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
 }
