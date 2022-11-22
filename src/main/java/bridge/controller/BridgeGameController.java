b0f6bf139c87a145a7861453960385285938fd97
ckyeon <qsky8520@gmail.com>
2022-11-22 21:24:05 +0900

    feat(BridgeGameController): add BridgeGameController class

---
+++ src/main/java/bridge/controller/BridgeGameController.java
@@ -0,0 +1,18 @@
+package bridge.controller;
+
+import bridge.domain.BridgeGame;
+import bridge.view.InputView;
+import bridge.view.OutputView;
+
+public class BridgeGameController {
+
+    private final InputView inputView;
+    private final OutputView outputView;
+    private final BridgeGame bridgeGame;
+
+    public BridgeGameController(InputView inputView, OutputView outputView, BridgeGame bridgeGame) {
+        this.inputView = inputView;
+        this.outputView = outputView;
+        this.bridgeGame = bridgeGame;
+    }
+}
