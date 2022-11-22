6e77e9eb2b557d2b9e8ba549ed0b5f157d924a37
sunwng <sunwgna@gmail.com>
2022-11-22 12:48:27 +0900

    refactor: compress duplicated codes

--- src/main/java/bridge/InputView.java
+++ src/main/java/bridge/InputView.java
@@ -6,8 +6,7 @@ public class InputView {
     public static int readBridgeSize() {
         try {
             System.out.println("다리의 길이를 입력해주세요.");
-            String userInputSize= UserInputException.numberCheck(Console.readLine());
-            return Integer.parseInt(userInputSize);
+            return Integer.parseInt(UserInputException.numberCheck(Console.readLine()));
         } catch (IllegalArgumentException e) {
             System.out.println("[ERROR] 다리 길이는 3부터 20 사이의 숫자여야 합니다.");
             return readBridgeSize();
@@ -17,8 +16,7 @@ public class InputView {
     public static String readMoving() {
         try {
             System.out.println("이동할 칸을 선택해주세요. (위: U, 아래: D)");
-            String userInputCommand = UserInputException.commandCheck(Console.readLine(), "read");
-            return userInputCommand;
+            return UserInputException.commandCheck(Console.readLine(), "read");
         } catch (IllegalArgumentException e) {
             System.out.println("[ERROR] 이동할 칸은 U 또는 D 의 알파벳 대문자여야 합니다.");
             return readMoving();
@@ -27,8 +25,7 @@ public class InputView {
     public static String readGameCommand() {
         try {
             System.out.println("게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)");
-            String userInputCommand = UserInputException.commandCheck(Console.readLine(), "process");
-            return userInputCommand;
+            return UserInputException.commandCheck(Console.readLine(), "process");
         } catch (IllegalArgumentException e) {
             System.out.println("[ERROR] 시도 여부는 R 또는 Q 의 알파벳 대문자여야 합니다.");
             return readGameCommand();
