e0b18faafc2870486a050002d195e9fcfe98854c
kjg1514 <kjg1514@naver.com>
2022-11-22 18:29:42 +0900

    fix : static으로 변경

--- src/main/java/bridge/BridgeGame.java
+++ src/main/java/bridge/BridgeGame.java
@@ -8,8 +8,8 @@ import java.util.Map;
  * 다리 건너기 게임을 관리하는 클래스
  */
 public class BridgeGame {
-    public List<String> bridgeU = new ArrayList<>();
-    public List<String> bridgeD = new ArrayList<>();
+    public static List<String> bridgeU = new ArrayList<>();
+    public static List<String> bridgeD = new ArrayList<>();
 
     /**
      * 사용자가 칸을 이동할 때 사용하는 메서드
@@ -75,5 +75,6 @@ public class BridgeGame {
         }
         return true;
     }
+
 
 }
