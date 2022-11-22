edaab241e02807f77c730042d8f9927677aa3f59
sora5421 <sojeoung536985@gmail.com>
2022-11-22 20:24:03 +0900

    feat : add method that chekc user game try command is valid

--- src/main/java/bridge/valid/InputValid.java
+++ src/main/java/bridge/valid/InputValid.java
@@ -22,6 +22,16 @@ public class InputValid {
         return true;
     }
 
+    public boolean checkCommand(String test) {
+        try {
+            isContinue(test);
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+            return false;
+        }
+        return true;
+    }
+
     private void isNumber(String test) {
         if (test.matches("[+-]?\\d*(\\.\\d+)?") == false) {
             throw new IllegalArgumentException("[ERROR] 다리 길이는 하나의 숫자여야 합니다.\n");
@@ -38,6 +48,12 @@ public class InputValid {
     private void isUpDown(String test) {
         if (!test.equals("U") && !test.equals("D")) {
             throw new IllegalArgumentException("[ERROR] 이동할 칸은 (위: U, 아래: D) 여야 합니다.\n");
+        }
+    }
+
+    private void isContinue(String test) {
+        if (!test.equals("R") && !test.equals("Q")) {
+            throw new IllegalArgumentException("[ERROR] 게임 시도 여부는 (재시도: R, 종료: Q)로 입력해야 합니다.\n");
         }
     }
 }
