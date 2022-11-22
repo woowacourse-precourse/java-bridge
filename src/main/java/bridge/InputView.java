7c3d1e5202f79098b163b9f8eaea4f1202b85ecc
jhsseonn <hosu0125@gmail.com>
2022-11-22 18:21:06 +0900

    feat: 게임 재시도 출력 여부 입력받기

--- src/main/java/bridge/InputView.java
+++ src/main/java/bridge/InputView.java
@@ -31,6 +31,8 @@ public class InputView extends InputConfig {
      * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
      */
     public String readGameCommand() {
-        return null;
+        System.out.println(GAME_RETRY);
+
+        return Console.readLine();
     }
 }
