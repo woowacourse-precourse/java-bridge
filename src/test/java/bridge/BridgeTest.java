7961f7e2f79d88633884496860fea6b9d58fadf9
syahn <496300@naver.com>
2022-11-22 14:40:27 +0900

    refactor: BridgeMakerTest로 다리 길이 입력 검증 테스트 옮김

--- src/test/java/bridge/BridgeTest.java
+++ src/test/java/bridge/BridgeTest.java
@@ -1,7 +1,5 @@
 package bridge;
 
-import bridge.Model.Bridge;
-import bridge.Model.BridgeGame;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -12,16 +10,9 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class BridgeTest {
     private final BridgeRandomNumberGenerator bridgeRandomNumberGenerator = new BridgeRandomNumberGenerator();
 
-    @DisplayName("다리 길이 입력으로 3이상 20이하의 숫자를 입력한다.")
-    @Test
-    void 다리_길이_정상_입력() {
-        BridgeMaker bridgeMaker = new BridgeMaker(bridgeRandomNumberGenerator);
-        assertThat(bridgeMaker.validateBridgeSize("15"));
-    }
-
     @DisplayName("다리 길이 입력과 동일한 크기의 다리가 만들어졌는지 확인하고, 모두 U와 D로 이루어져있는지 확인한다.")
     @Test
-    void 다리_정상인지_확인() {
+    void 다리_정상적으로_만들어졌는지_확인() {
         int size = 3;
         BridgeMaker bridgeMaker = new BridgeMaker(bridgeRandomNumberGenerator);
         List<String> bridgeAnswer = bridgeMaker.makeBridge(size);
@@ -35,10 +26,9 @@ public class BridgeTest {
 
     @DisplayName("다리 길이 입력에 대해 답과 비교하여 성공 여부를 올바르게 체크하는지 확인한다.")
     @Test
-    void 다리_정답과_비교() {
+    void 다리_정답과_비교해_실패여부_확인() {
         List<String> bridgeAnswer = List.of("U", "D", "D");
         Bridge bridge = new Bridge(bridgeAnswer);
-        BridgeGame bridgeGame = new BridgeGame(bridge);
         assertThat(bridge.checkAnswer("U", 1)).isEqualTo(false);
     }
 }
