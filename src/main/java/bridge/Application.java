c1e51db85fcb353f1c8e67247249839648d216f6
davinc71998 <davinc71998@gmail.com>
2022-11-22 20:30:25 +0900

    Feat: 추가 main 함수 실행 예외 처리
    예외 처리 출력을 위한 try catch 처리

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -105,5 +105,11 @@ public class Application {
 
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        try {
+            bridgeGame();
+        } catch (Exception e){
+            System.out.println(e.getMessage());
+        }
     }
+
 }
