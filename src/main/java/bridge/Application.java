dbce6eccb9d18716003459e090521dc06f756dab
jw0293 <k38836919@gmail.com>
2022-11-22 22:20:27 +0900

    test : build

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -13,8 +13,7 @@ public class Application {
 
     public static void main(String[] args) {
         // TODO: 프로그램 구현
-        BridgeInit bridgeInit = new BridgeInit(
-                new InputView(new ValidException()),
+        BridgeInit bridgeInit = new BridgeInit(new InputView(new ValidException()),
                 new BridgeMaker(new BridgeRandomNumberGenerator()));
         BridgeGame bridgeGame = new BridgeGame(bridgeInit, new BridgePainting());
         BridgeService bridgeService = new BridgeService(new OutputView(), bridgeGame);
