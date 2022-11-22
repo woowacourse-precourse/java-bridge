eadb783c7fd25460409e7a5dd8963a274c719f0b
SeongbinPark <binni0515@gmail.com>
2022-11-22 22:37:06 +0900

    test(GameTest): 실패 후 종료 테스트

---
+++ src/test/java/bridge/GameTest.java
@@ -0,0 +1,39 @@
+package bridge;
+
+import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeTest;
+import static org.assertj.core.api.Assertions.assertThat;
+
+import camp.nextstep.edu.missionutils.test.NsTest;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class GameTest extends NsTest {
+
+    private static final String ERROR_MESSAGE = "[ERROR]";
+
+    @DisplayName("실패 후 종료 테스트")
+    @Test
+    void failureAndQuit() {
+        assertRandomNumberInRangeTest(() -> {
+            run("3", "U", "D", "D", "Q");
+            assertThat(output()).contains(
+                    "이동할 칸을 선택해주세요. (위: U, 아래: D)",
+                    "게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)",
+                    "최종 게임 결과",
+                    "[ O |   |   ]",
+                    "[   | O | X ]",
+                    "게임 성공 여부: 실패",
+                    "총 시도한 횟수: 1"
+            );
+
+            int upSideIndex = output().indexOf("[ O |   |  ]");
+            int downSideIndex = output().indexOf("[   | O | X ]");
+            assertThat(upSideIndex).isLessThan(downSideIndex);
+        }, 1, 0, 1);
+    }
+
+    @Override
+    protected void runMain() {
+        Application.main(new String[]{});
+    }
+}
