33544884dd0b660fc7ab11c1fa2551c5098abaf9
JunTaeINC <juntea3523@naver.com>
2022-11-22 22:20:42 +0900

    feat : (OutputView) printMap 기능 구현

--- src/main/java/bridge/OutputView.java
+++ src/main/java/bridge/OutputView.java
@@ -1,12 +1,18 @@
 package bridge;
 
 
+import java.util.List;
+
 import static bridge.enums.Sentence.*;
 
 /**
  * 사용자에게 게임 진행 상황과 결과를 출력하는 역할을 한다.
  */
 public class OutputView {
+
+    private final static String COMMA = ", ";
+    private final static String V_BAR = "|";
+
 
     public static void printStartGame() {
         System.out.println(GAME_START.getValue());
@@ -18,7 +24,9 @@ public class OutputView {
      * <p>
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void printMap() {
+    public static void printMap(List<String> upLine, List<String> downLine) {
+        System.out.println(upLine.toString().replace(COMMA, V_BAR));
+        System.out.println(downLine.toString().replace(COMMA, V_BAR));
     }
 
     /**
