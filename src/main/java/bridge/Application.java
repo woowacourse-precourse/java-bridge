01250fcde2eb3e2d3983f791dec4966193bfbb1c
kmularise <kmula222@gmail.com>
2022-11-22 18:18:30 +0900

    test(bridge): 예기지 못한 오류 잡아내기

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -3,6 +3,18 @@ package bridge;
 public class Application {
 
     public static void main(String[] args) {
+        play();
         // TODO: 프로그램 구현
+    }
+
+    private static void play() {
+        try {
+            new InputView().readBridgeSize();
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+        }
+//        int size = InputController.checkSize(new InputView());
+//        BridgeGame game = new BridgeGame(size);
+//        game.play();
     }
 }
