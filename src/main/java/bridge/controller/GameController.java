a5296fb8f7d14d61b90b4ca97b15c320d813d396
skydreamer21 <skydreamer210@gmail.com>
2022-11-22 16:02:12 +0900

    feat(GameController): 게임 진행 기능 구현

--- src/main/java/bridge/controller/GameController.java
+++ src/main/java/bridge/controller/GameController.java
@@ -1,8 +1,11 @@
 package bridge.controller;
 
 import bridge.service.BridgeGame;
+import bridge.service.constant.GameStatus;
 import bridge.view.InputView;
 import bridge.view.OutputView;
+
+import java.util.function.Supplier;
 
 public class GameController {
     private static final InputView inputView = InputView.getInstance();
@@ -22,6 +25,23 @@ public class GameController {
         } catch (IllegalArgumentException e) {
             outputView.printErrorMessage(e.getMessage());
             return create();
+        }
+    }
+
+    public void operate (BridgeGame game) {
+        while (game.getStatus() == GameStatus.PLAYING) {
+            game.move(requestStringInput(inputView::readMoving));
+            outputView.printMap(game.obtainGameLog());
+            outputView.insertLineBreak();
+        }
+    }
+
+    private String requestStringInput(Supplier<String> readStringInput) {
+        try {
+            return readStringInput.get();
+        } catch (IllegalArgumentException e) {
+            outputView.printErrorMessage(e.getMessage());
+            return requestStringInput(readStringInput);
         }
     }
 
