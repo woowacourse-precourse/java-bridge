57974c36e9904fb1d0dab9ac91346d4dcd4c5519
jingyu9804 <swc05161@gmail.com>
2022-11-22 16:19:25 +0900

    refactor(bridge) : 예제 테스트 실패에 따른 리팩토링

--- src/main/java/bridge/domain/BridgeMaker.java
+++ src/main/java/bridge/domain/BridgeMaker.java
@@ -21,7 +21,7 @@ public class BridgeMaker {
      * @return 입력받은 길이에 해당하는 다리 모양. 위 칸이면 "U", 아래 칸이면 "D"로 표현해야 한다.
      */
     public List<String> makeBridge(int size) {
-        List<String> bridge = new ArrayList<>(size);
+        List<String> bridge = new ArrayList<>();
         for (int i = 0; i < size; i++) {
             int generatedNum = bridgeNumberGenerator.generate();
             bridge.add(BridgeHelp.generateBridge(generatedNum).getValue());
