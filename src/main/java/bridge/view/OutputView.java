193572233e016749d98658bf819d68d2e81e1159
hsj4436 <hsj4436@naver.com>
2022-11-22 18:53:00 +0900

    feat: 게임 결과 출력

--- src/main/java/bridge/view/OutputView.java
+++ src/main/java/bridge/view/OutputView.java
@@ -12,6 +12,11 @@ public class OutputView {
     private static final String GET_BRIDGE_LENGTH = "다리의 길이를 입력해주세요.";
     private static final String GET_MOVE_DIRECTION = "이동할 칸을 선택해주세요. (위: U, 아래: D)";
     private static final String GONNA_RETRY = "게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)";
+    private static final String FINAL_RESULT = "최종 게임 결과";
+    private static final String IS_SUCCESSED = "게임 성공 여부: ";
+    private static final String SUCCESS = "성공";
+    private static final String FAIL = "실패";
+    private static final String TRY_COUNT = "총 시도한 횟수: ";
 
     public void printGameStart() {
         System.out.println(GAME_START);
@@ -44,7 +49,20 @@ public class OutputView {
      * <p>
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void printResult() {
+    public void printResult(int tryCount, boolean isSuccess) {
+        if (isSuccess) {
+            System.out.println(IS_SUCCESSED + SUCCESS);
+            System.out.println(TRY_COUNT + tryCount);
+            return;
+        }
+        System.out.println(IS_SUCCESSED + FAIL);
+        System.out.println(TRY_COUNT + tryCount);
+    }
+
+    public void printFinalResult(UsersRoute usersRoute, Bridge bridge) {
+        System.out.println(FINAL_RESULT);
+        System.out.println(Record.getTopLane(usersRoute, bridge));
+        System.out.println(Record.getBottomLane(usersRoute, bridge));
     }
 
     public void printErrorMessage(String message) {
