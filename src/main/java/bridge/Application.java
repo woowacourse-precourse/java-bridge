21a4e21f8d12e7f4b3d78a5c71eb556dddf23116
KimRiun <kimriun27@gmail.com>
2022-11-22 18:05:26 +0900

    feat: 다리 길이 입력받기

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -5,5 +5,8 @@ public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
         System.out.println("다리 건너기 게임을 시작합니다.");
+        System.out.println("다리 길이를 입력해주세요.");
+        InputView inputView = new InputView();
+        int size = inputView.readBridgeSize();
     }
 }
