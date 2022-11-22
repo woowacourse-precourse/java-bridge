2a46028e39ba9b0f23ed1e419cdf4b9bd2db6705
hunko <rhwlgns4386@naver.com>
2022-11-22 21:30:14 +0900

    chore : chage error message

--- src/main/java/bridge/BridgeMaker.java
+++ src/main/java/bridge/BridgeMaker.java
@@ -42,6 +42,6 @@ public class BridgeMaker {
     }
 
     private void validate(int size) {
-        if (size < 3 || 20 < size) throw new IllegalArgumentException("숫자 범위를 잘못 입력하셨습니다.");
+        if (size < 3 || 20 < size) throw new IllegalArgumentException("다리 길이는 3부터 20 사이의 숫자여야 합니다.");
     }
 }
--- src/main/java/bridge/mediator/ViewMediator.java
+++ src/main/java/bridge/mediator/ViewMediator.java
@@ -5,8 +5,6 @@ import java.util.HashMap;
 public interface ViewMediator {
     void start();
     void generateBridge(int i);
-
     Runnable moveBridge(String s, HashMap<String, String> map);
-
     void replay(String readGameCommand) ;
 }
