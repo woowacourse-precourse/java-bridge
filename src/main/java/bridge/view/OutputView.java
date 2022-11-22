d9f52d2f7ecc82bf330ff8d357c5fc6beee9035c
SeongbinPark <binni0515@gmail.com>
2022-11-22 18:59:22 +0900

    feat(OutputView): add printStartOfGame method

--- src/main/java/bridge/view/OutputView.java
+++ src/main/java/bridge/view/OutputView.java
@@ -1,5 +1,7 @@
 package bridge.view;
 
+import static bridge.constant.message.OutputMessage.START_OF_GAME;
+
 import bridge.Map;
 import java.util.List;
 
@@ -9,5 +11,9 @@ public class OutputView {
         System.out.println(map.format(upSide));
         System.out.println(map.format(downSide));
         System.out.println();
+    }
+
+    public void printStartOfGame() {
+        System.out.println(START_OF_GAME.getMessage());
     }
 }
