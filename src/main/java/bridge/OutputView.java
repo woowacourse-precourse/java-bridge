dc71b69de4744571ed076581a8865d91e518fb20
khj <bbb6254177@gmail.com>
2022-11-22 15:29:39 +0900

    feat(bridge): add function to print map

--- src/main/java/bridge/OutputView.java
+++ src/main/java/bridge/OutputView.java
@@ -1,16 +1,45 @@
 package bridge;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
 /**
  * 사용자에게 게임 진행 상황과 결과를 출력하는 역할을 한다.
  */
 public class OutputView {
+    private String convertToMapComp(String command, String bridgeElem, boolean isEqual) {
+        if ((!isEqual && bridgeElem.equals(command)) || (isEqual && !bridgeElem.equals(command))) {
+            return " ";
+        } else if (isEqual) {
+            return "O";
+        } else {
+            return "X";
+        }
+    }
+
+    private List<String> convertToMapCompList(String command, List<String> subBridge, boolean isEqual) {
+        List<String> mapCompList = new ArrayList<>();
+
+        int index = 0;
+        while (index < subBridge.size() - 1){
+            mapCompList.add(convertToMapComp(command, subBridge.get(index), true));
+            index++;
+        }
+        mapCompList.add(convertToMapComp(command, subBridge.get(index), isEqual));
+
+        return mapCompList;
+    }
 
     /**
      * 현재까지 이동한 다리의 상태를 정해진 형식에 맞춰 출력한다.
      * <p>
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void printMap() {
+    public void printMap(List<String> bridge, int position, boolean isEqual) {
+        List<String> subBridge = bridge.subList(0, position);
+        System.out.println("[ " + String.join(" | ",convertToMapCompList("U", subBridge, isEqual)) + " ]");
+        System.out.println("[ " + String.join(" | ",convertToMapCompList("D", subBridge, isEqual)) + " ]");
     }
 
     /**
