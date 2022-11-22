e255e8e77d1a028d6f7a1df6f9facb6eae35afe8
HCH <clgusdl2@naver.com>
2022-11-22 20:28:19 +0900

    [FEAT] : 입력 사이즈에 의한 다리생성 및 에러체크

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -1,8 +1,31 @@
 package bridge;
 
+import java.util.List;
+
 public class Application {
+     InputView input = new InputView();
+    static OutputView outputView = new OutputView();
+     inputWrongError errorCheck = new inputWrongError();
 
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        BridgeGame bridge = new BridgeGame();
+        outputView.printStartGame();
+        bridge.startGame();
     }
+
+    public  int makeBridgeSize(){
+        int size;
+        outputView.printLengthBridge();
+        try{
+            size = input.readBridgeSize();
+            errorCheck.bridgeSizeError(size);
+        }catch(IllegalArgumentException ex){
+            System.out.println(ex.getMessage());
+            return makeBridgeSize();
+        }
+        return size;
+    }
+
+
 }
