a739eb202f669425beb99570f759eef839d90ef0
jin8073 <jin8073@gmail.com>
2022-11-22 19:54:03 +0900

    test(IllegalArgumentTest): 예외 처리 확인 테스트 추가

--- src/test/java/bridge/exception/IllegalArgumentTest.java
+++ src/test/java/bridge/exception/IllegalArgumentTest.java
@@ -2,37 +2,56 @@ package bridge.exception;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 public class IllegalArgumentTest {
 
-    @Test
-    void 숫자가_아닌_입력값() {
-        assertThat(IllegalArgument.isNotNumber("숫자아님")).isTrue();
+    @ValueSource(strings = {"숫자아님", "", " ", "\n", "jkdf", "3d"})
+    @ParameterizedTest
+    void 숫자가_아닌_다리의_길이_입력값(String input) {
+        assertThat(IllegalArgument.isNotNumber(input)).isTrue();
     }
 
-    @Test
-    void 숫자인_입력값() {
-        assertThat(IllegalArgument.isNotNumber("3")).isFalse();
+    @ValueSource(strings = {"0", "1", "3", "20", "21"})
+    @ParameterizedTest
+    void 숫자인_다리의_길이_입력값(String input) {
+        assertThat(IllegalArgument.isNotNumber(input)).isFalse();
     }
 
-    @Test
-    void 다리의_길이_범위_밖_입력값() {
-        assertThat(IllegalArgument.isNotInBridgeSizeRange("24")).isTrue();
+    @ValueSource(strings = {"0", "1", "2", "21"})
+    @ParameterizedTest
+    void 다리의_길이_범위_밖_입력값(String input) {
+        assertThat(IllegalArgument.isNotInBridgeSizeRange(input)).isTrue();
     }
 
-    @Test
-    void 다리의_길이_범위_안_입력값() {
-        assertThat(IllegalArgument.isNotInBridgeSizeRange("20")).isFalse();
+    @ValueSource(strings = {"3", "10", "20"})
+    @ParameterizedTest
+    void 다리의_길이_범위_안_입력값(String input) {
+        assertThat(IllegalArgument.isNotInBridgeSizeRange(input)).isFalse();
     }
 
-    @Test
-    void 이동할_칸_올바르지_않은_입력() {
-        assertThat(IllegalArgument.isIncorrectMoving("0")).isTrue();
+    @ValueSource(strings = {"0", "", "d", "30", "E"})
+    @ParameterizedTest
+    void 이동할_칸_올바르지_않은_입력(String input) {
+        assertThat(IllegalArgument.isIncorrectMoving(input)).isTrue();
     }
 
-    @Test
-    void 이동할_칸_올바른_입력() {
-        assertThat(IllegalArgument.isIncorrectMoving("U")).isFalse();
+    @ValueSource(strings = {"U", "D"})
+    @ParameterizedTest
+    void 이동할_칸_올바른_입력(String input) {
+        assertThat(IllegalArgument.isIncorrectMoving(input)).isFalse();
+    }
+
+    @ValueSource(strings = {"D", "", "3"})
+    @ParameterizedTest
+    void 재시작_또는_종료_올바르지_않은_입력(String input) {
+        assertThat(IllegalArgument.isIncorrectGameCommand(input)).isTrue();
+    }
+
+    @ValueSource(strings = {"R", "Q"})
+    @ParameterizedTest
+    void 재시작_또는_종료_올바른_입력(String input) {
+        assertThat(IllegalArgument.isIncorrectGameCommand(input)).isFalse();
     }
 }
