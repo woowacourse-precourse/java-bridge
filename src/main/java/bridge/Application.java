65fce611a97b3f58e3732eab30efd97e7a62c8f3
dohyun <ldh19738@gmail.com>
2022-11-22 19:35:32 +0900

    feat(Application): 게임 실행

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -1,8 +1,17 @@
 package bridge;
 
+import bridge.controller.GameController;
+import bridge.view.OutputView;
+
 public class Application {
 
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        BridgeGame bridgeGame = new BridgeGame();
+        Result result = new Result();
+        GameController gameController = new GameController(bridgeGame, result);
+
+        OutputView.printStart();
+        gameController.playGame();
     }
 }
