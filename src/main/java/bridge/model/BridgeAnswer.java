3c0828fae56355753f57eabb7a426a578642a857
miffyKing <whqudghk28@gmail.com>
2022-11-22 15:52:29 +0900

    refactor: Change to constants and remove printing result method from Answer class.

--- src/main/java/bridge/model/BridgeAnswer.java
+++ src/main/java/bridge/model/BridgeAnswer.java
@@ -1,50 +1,41 @@
 package bridge.model;
 
 import bridge.BridgeMaker;
-import bridge.BridgeRandomNumberGenerator;
+import bridge.model.ScreenGenerator.ScreenGenerator;
 import java.util.List;
 
 public class BridgeAnswer {
-    public List<String> answer;
+
+    private final List<String> answer;
     private static Integer currentSpot = 0;
+    private static final int REACHED_END = 1;
+    private static final int COMPARE_SAME = 2;
+    private static final int COMPARE_DIFFERENT = 3;
 
     private BridgeAnswer(final List<String> answerBridge) {
         this.answer = answerBridge;
     }
 
-
-    public static BridgeAnswer using(final BridgeRandomNumberGenerator bridgeGenerator, BridgeLength bridgeLength) {
-        BridgeMaker bridgeMaker = new BridgeMaker(bridgeGenerator);
+    public static BridgeAnswer using(final BridgeMaker bridgeMaker, BridgeLength bridgeLength) {
         return new BridgeAnswer(bridgeMaker.makeBridge(bridgeLength.getLength()));
     }
-    public String printAnswer(int printCase) {
-        BridgeScreenGenerator bridgeScreenGenerator = new BridgeScreenGenerator(answer, currentSpot, printCase);
+
+    public ScreenGenerator printCurrentBridgeStatus(int printCase) {
+        BridgeScreenModeSelector bridgeScreenGenerator = new BridgeScreenModeSelector(answer, currentSpot, printCase);
         return bridgeScreenGenerator.generateOutputScreen();
     }
 
     public int compareWithUserMove(int place, String userInput) {
         currentSpot = place;
-        if (place == answer.size() -1 && answer.get(place).equals(userInput) == true) {
-            return 1;
+        if (place == answer.size() - 1 && answer.get(place).equals(userInput)) {
+            return REACHED_END;
+        } else if (answer.get(place).equals(userInput)) {
+            return COMPARE_SAME;
         }
-        if (answer.get(place).equals(userInput) == true) {
-            return 2;
-        }
-        return 3;
-    }
-
-    public String printFinalResult(String latestOutput, int tryCount, String successOrFail) {
-        StringBuilder sb = new StringBuilder();
-        sb.append("최종 게임 결과\n" + latestOutput+ "\n");
-        sb.append(("게임 성공 여부: " + successOrFail));
-        sb.append("총 시도한 횟수:" + tryCount);
-        return sb.toString();
+        return COMPARE_DIFFERENT;
     }
 
     public void resetCurrentSpot() {
         currentSpot = 0;
-    }
-    public void printAnswerInBeginForTest() {
-        System.out.println(answer);
     }
 }
