d10866ba321ec286369b01da3be45daaaf916c1e
Go-Jaecheol <gojaech@naver.com>
2022-11-22 20:14:19 +0900

    style: messagePositionInput에서 messageMovingInput으로 메서드명 변경

--- src/main/java/bridge/view/OutputView.java
+++ src/main/java/bridge/view/OutputView.java
@@ -12,7 +12,7 @@ public class OutputView {
         System.out.println("\n다리의 길이를 입력해주세요.");
     }
 
-    public static void messagePositionInput() {
+    public static void messageMovingInput() {
         System.out.println("\n이동할 칸을 선택해주세요. (위: U, 아래: D)");
     }
 
