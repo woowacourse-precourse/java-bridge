18f4867cb5ba7a9ee228bd7d1cd3da105fa349b6
hhiyeon <hhyyeon0214@gmail.com>
2022-11-22 15:11:14 +0900

    refactor:BridgeError-remove unnecessary error message

--- src/main/java/bridge/exception/BridgeError.java
+++ src/main/java/bridge/exception/BridgeError.java
@@ -6,8 +6,7 @@ public enum BridgeError {
     INVALID_RANDOM_NUMBER("[ERROR] 무작위 값이 0또는 1이 아닙니다."),
     IS_NOT_ONE_CHARACTER("[ERROR] 이동할 칸 입력 값의 길이가 1이 아닙니다."),
     INVALID_DIRECTION("[ERROR] 이동할 칸 입력 값이 올바르지 않습니다."),
-    INVALID_GAME_STATUS("[ERROR] 게임 재시도 입력 값이 올바르지 않습니다."),
-    UNINITIALIZED_BRIDGE("[ERROR] 이전 게임의 다리 결과가 초기화 되지 않았습니다.");
+    INVALID_GAME_STATUS("[ERROR] 게임 재시도 입력 값이 올바르지 않습니다.");
 
     private final String errorMessage;
 
