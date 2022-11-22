fa8a3ab830de257f3255917e88a1970c2cb9861e
endlessmomo <soohyuk96@naver.com>
2022-11-22 10:03:15 +0900

    refactor(Map) : 불필요한 imp1ort문 제거 및 자동 줄 완성

--- src/main/java/bridge/model/domain/Map.java
+++ src/main/java/bridge/model/domain/Map.java
@@ -2,29 +2,32 @@ package bridge.model.domain;
 
 import bridge.model.validation.Move;
 
-import java.util.ArrayList;
 import java.util.List;
 
 public class Map {
-    private final List <List <String>> Map = new ArrayList <>();
+    private final StringBuilder map1;
+    private final StringBuilder map2;
 
     public Map() {
-        Map.add(new ArrayList <>());
-        Map.add(new ArrayList <>());
+        map1 = new StringBuilder();
+        map2 = new StringBuilder();
     }
 
     public void draw(List <String> roundTrace) {
-        Map.get(0).add(roundTrace.get(Move.MOVE_DOWN.getMoveValue()));
-        Map.get(1).add(roundTrace.get(Move.MOVE_UP.getMoveValue()));
+        map1.append(roundTrace.get(Move.MOVE_DOWN.getMoveValue()));
+        map2.append(roundTrace.get(Move.MOVE_UP.getMoveValue()));
+    }
+
+    public String getMap1() {
+        return map1.substring(0, map1.length() - 1);
+    }
+
+    public String getMap2() {
+        return map2.substring(0, map2.length() - 1);
     }
 
     public void reset() {
-        Map.get(0).clear();
-        Map.get(1).clear();
+        map1.setLength(0);
+        map2.setLength(0);
     }
-
-    public List <List <String>> getMap() {
-        return Map;
-    }
-
 }
