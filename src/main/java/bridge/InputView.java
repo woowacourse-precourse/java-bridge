4c9dbbd1801dc38a2578bc97bd5755e1ff54e5b3
JaeHwanCho <makefuture@kakao.com>
2022-11-22 14:58:44 +0900

    feat(function): receive input number
    Add new function:
    - Add ReadbridgeSize
    - Add numeric exception handling between 3 and 20 to "ReadbridgeSize"
    - Add ReadMoving
    - Add exception handling except U and D to "ReadMoving"
    - Add ReadGameCommand
    - Add exception handling except R and Q to "ReadGameCommand"

--- src/main/java/bridge/InputView.java
+++ src/main/java/bridge/InputView.java
@@ -1,5 +1,7 @@
 package bridge;
 
+import camp.nextstep.edu.missionutils.Console;
+
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
  */
@@ -9,20 +11,53 @@ public class InputView {
      * 다리의 길이를 입력받는다.
      */
     public int readBridgeSize() {
-        return 0;
+        while (true) {
+            Integer bridgeSize = Integer.valueOf(Console.readLine());
+            System.out.println("입력받은 값 : " + bridgeSize);
+            try {
+                if (bridgeSize >= 3 && bridgeSize <= 20) {
+                    return bridgeSize;
+                }
+                throw new IllegalArgumentException();
+            } catch (IllegalArgumentException e) {
+                System.out.println("[ERROR] 올바른 다리 숫자가 아닙니다.");
+            }
+        }
     }
 
     /**
      * 사용자가 이동할 칸을 입력받는다.
      */
     public String readMoving() {
-        return null;
+        while (true) {
+            String input = Console.readLine();
+            System.out.println("입력받은 값 : " + input);
+            try {
+                if (input.equals("U") || input.equals("D")) {
+                    return input;
+                }
+                throw new IllegalArgumentException();
+            } catch (IllegalArgumentException e) {
+                System.out.println("[ERROR] 올바른 이동 값이 아닙니다.");
+            }
+        }
     }
 
     /**
      * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
      */
     public String readGameCommand() {
-        return null;
+        while (true) {
+            String input = Console.readLine();
+            System.out.println("입력받은 값 : " + input);
+            try {
+                if (input.equals("R") || input.equals("Q")) {
+                    return input;
+                }
+                throw new IllegalArgumentException();
+            } catch (IllegalArgumentException e) {
+                System.out.println("[ERROR] 올바른 게임 종료 값이 아닙니다.");
+            }
+        }
     }
 }
