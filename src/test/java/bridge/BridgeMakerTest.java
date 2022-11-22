0143bcf671ce7c9cc199d6cbc206f595e9845c1c
Jaehong Lee <woohong24@naver.com>
2022-11-22 20:32:06 +0900

    test: 다리 만들기 테스트 기능 추가

--- src/test/java/bridge/BridgeMakerTest.java
+++ src/test/java/bridge/BridgeMakerTest.java
@@ -1,2 +1,26 @@
-package bridge;public class BridgeMakerTest {
+package bridge;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class BridgeMakerTest {
+
+    @DisplayName("다리의 길이가 정수가 아니라면 예외가 발생한다.")
+    @Test
+    void makeBridgeByNotInteger() {
+        Assertions.assertThatThrownBy(() -> new BridgeMakerValidate("a")).isInstanceOf(IllegalArgumentException.class);
+        Assertions.assertThatThrownBy(() -> new BridgeMakerValidate(" ")).isInstanceOf(IllegalArgumentException.class);
+        Assertions.assertThatThrownBy(() -> new BridgeMakerValidate("hello world1")).isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("다리의 길이가 3이상 20이하의 숫자가 아니라면 예외가 발생한다.")
+    @Test
+    void makeBridgeByExtraRange() {
+        Assertions.assertThatThrownBy(() -> new BridgeMakerValidate("1")).isInstanceOf(IllegalArgumentException.class);
+        Assertions.assertThatThrownBy(() -> new BridgeMakerValidate("21")).isInstanceOf(IllegalArgumentException.class);
+        Assertions.assertThatThrownBy(() -> new BridgeMakerValidate("0")).isInstanceOf(IllegalArgumentException.class);
+    }
+
+
 }
