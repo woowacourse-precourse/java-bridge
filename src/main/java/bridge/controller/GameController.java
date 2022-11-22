7d2f6d4d218f827941c57856b93f62cd90f8698c
mihye126 <mihea12@gmail.com>
2022-11-22 20:25:49 +0900

    [feat] GameController 생성자 구현

--- src/main/java/bridge/controller/GameController.java
+++ src/main/java/bridge/controller/GameController.java
@@ -1,5 +1,18 @@
 package bridge.controller;
 
+import bridge.domain.BridgeGame;
+import bridge.view.InputView;
+import bridge.view.OutputView;
+
 public class GameController {
+    private final BridgeGame game;
+    private final InputView inputView;
+    private final OutputView outputView;
+
+    private GameController() {
+        this.inputView = new InputView();
+        this.outputView = new OutputView();
+        this.game = new BridgeGame(this.inputView.readBridgeSize());
+    }
 
 }
