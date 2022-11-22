ba51d3dab20f8891fa0a1d904978e68f3ee2c69b
YunByungil <dbsquddlfz@naver.com>
2022-11-22 21:27:03 +0900

    test: bridge game 성공 실패 테스트 작성

--- src/test/java/bridge/BridgeGameTest.java
+++ src/test/java/bridge/BridgeGameTest.java
@@ -26,6 +26,67 @@ public class BridgeGameTest extends NsTest {
                 }, 1, 1, 1, 0, 0, 0, 0, 0, 1, 1);
     }
 
+    @DisplayName("다리 건너기 성공 확인 테스트")
+    @Test
+    void 다리_건너기_성공_테스트() {
+        assertRandomNumberInRangeTest(() -> {
+            run("3", "D", "U", "D");
+            assertThat(output()).contains(
+                    "최종 게임 결과",
+                    "[   | O |   ]",
+                    "[ O |   | O ]",
+                    "게임 성공 여부: 성공",
+                    "총 시도한 횟수: 1"
+            );
+
+            int upSideIndex = output().indexOf("[   | O |   ]");
+            int downSideIndex = output().indexOf("[ O |   | O ]");
+            assertThat(upSideIndex).isLessThan(downSideIndex);
+        }, 0, 1, 0);
+    }
+
+    @DisplayName("다리 건너기 실패 확인 테스트")
+    @Test
+    void 다리_건너기_실패_테스트() {
+        assertRandomNumberInRangeTest(() -> {
+            run("3", "D", "D", "Q");
+            assertThat(output()).contains(
+                    "최종 게임 결과",
+                    "[   |   ]",
+                    "[ O | X ]",
+                    "게임 성공 여부: 실패",
+                    "총 시도한 횟수: 1"
+            );
+
+            int upSideIndex = output().indexOf("[   |   ]");
+            int downSideIndex = output().indexOf("[ O | X ]");
+            assertThat(upSideIndex).isLessThan(downSideIndex);
+        }, 0, 1);
+    }
+
+    @DisplayName("다리 건너기 실패 - 재시작 후, 성공 테스트")
+    @Test
+    void 다리_건너기_실패_후_재시작_테스트() {
+        assertRandomNumberInRangeTest(() -> {
+            run("3", "D", "D", "R", "D", "U", "U");
+            assertThat(output()).contains(
+                    "최종 게임 결과",
+                    "[   |   ]",
+                    "[ O | X ]",
+                    "게임 성공 여부: 성공",
+                    "총 시도한 횟수: 2"
+            );
+
+            int upSideIndex = output().indexOf("[   |   ]");
+            int downSideIndex = output().indexOf("[ O | X ]");
+            assertThat(upSideIndex).isLessThan(downSideIndex);
+
+        }, 0, 1, 1);
+    }
+
+
+
+
     @Override
     protected void runMain() {
         Application.main(new String[]{});
