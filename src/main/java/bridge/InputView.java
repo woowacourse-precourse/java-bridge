8c45652989592f4e5c1da597d4d43d8578c0e6b9
kim <kajinori@gachon.ac.kr>
2022-11-22 19:51:35 +0900

    refactor(InputView): readLine 임포트로 코드 한줄 감소

--- src/main/java/bridge/InputView.java
+++ src/main/java/bridge/InputView.java
@@ -1,23 +1,20 @@
 package bridge;
 
-import camp.nextstep.edu.missionutils.Console;
+import static camp.nextstep.edu.missionutils.Console.readLine;
 
 public class InputView {
     public static String readBridgeSize() {
         System.out.println("다리의 길이를 입력해주세요.");
-        Console.readLine();
-        return Console.readLine();
+        return readLine();
     }
 
     public static String readMoving() {
         System.out.println("이동할 칸을 선택해주세요. (위: U, 아래: D)");
-        Console.readLine();
-        return Console.readLine();
+        return readLine();
     }
 
     public static String readGameCommand() {
         System.out.println("게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)");
-        Console.readLine();
-        return Console.readLine();
+        return readLine();
     }
 }
