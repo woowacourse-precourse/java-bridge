33200cb61e872e43676c288927a2dbd37a8a5409
jxixeun <jxixeun@gmail.com>
2022-11-22 22:06:25 +0900

    refactor: 주석 삭제, 출력 메세지 상수로 치환

--- src/main/java/bridge/InputValidator.java
+++ src/main/java/bridge/InputValidator.java
@@ -5,9 +5,10 @@ package bridge;
  */
 public class InputValidator {
 
-    /**
-     * input String값이 숫자면 ture를, 아니면 false를 반환한다.
-     */
+    public static final String RETRY_COMMAND_ERROR = "[ERROR] 입력은 R과 Q 중 하나의 문자여야 합니다.";
+    public static final String MOVING_COMMAND_ERROR = "[ERROR] 이동할 칸은 U와 D 중 하나의 문자여야 합니다.";
+    public static final String BRIDGE_SIZE_INPUT_ERROR = "[ERROR] 다리 길이는 3부터 20 사이의 숫자여야 합니다.";
+
     public boolean isNumeric(String input) {
         try {
             Integer.parseInt(input);
@@ -17,37 +18,31 @@ public class InputValidator {
         }
     }
 
-    /**
-     * 입력 받은 다리의 길이가 올바른 값인지 검사한다.
-     */
     public void checkBridgeSize(String input) {
         if (!isNumeric(input)){
-            throw new IllegalArgumentException("[ERROR] 다리 길이는 3부터 20 사이의 숫자여야 합니다.");
+            throw new IllegalArgumentException(BRIDGE_SIZE_INPUT_ERROR);
         }
         int bridgeSizeInput = Integer.parseInt(input);
         if (bridgeSizeInput < 3 || bridgeSizeInput > 20) {
-            throw new IllegalArgumentException("[ERROR] 다리 길이는 3부터 20 사이의 숫자여야 합니다.");
+            throw new IllegalArgumentException(BRIDGE_SIZE_INPUT_ERROR);
         }
     }
 
-    /**
-     * 입력받은 이동할 칸이 올바른 값인지 검사한다.
-     */
     public void checkMoving(String input) {
         if (input.length() != 1) {
-            throw new IllegalArgumentException("[ERROR] 이동할 칸은 U와 D 중 하나의 문자여야 합니다.");
+            throw new IllegalArgumentException(MOVING_COMMAND_ERROR);
         }
         if (!input.equals("U") && !input.equals("D")){
-            throw new IllegalArgumentException("[ERROR] 이동할 칸은 U와 D 중 하나의 문자여야 합니다.");
+            throw new IllegalArgumentException(MOVING_COMMAND_ERROR);
         }
     }
 
     public void checkRetryCommand(String input) {
         if (input.length() != 1) {
-            throw new IllegalArgumentException("[ERROR] 입력은 R과 Q 중 하나의 문자여야 합니다.");
+            throw new IllegalArgumentException(RETRY_COMMAND_ERROR);
         }
         if (!input.equals("R") && !input.equals("Q")){
-            throw new IllegalArgumentException("[ERROR] 입력은 R과 Q 중 하나의 문자여야 합니다.");
+            throw new IllegalArgumentException(RETRY_COMMAND_ERROR);
         }
     }
 
