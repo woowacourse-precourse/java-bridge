f2bff25c803011c99ff166cbeb88642eb883cb60
josolha <josolha@nate.com>
2022-11-22 20:49:24 +0900

    chore(InputView) : 주석 삭제

--- src/main/java/bridge/view/InputView.java
+++ src/main/java/bridge/view/InputView.java
@@ -10,14 +10,9 @@ import static bridge.validator.Validate.validateCheckRetry;
 
 import camp.nextstep.edu.missionutils.Console;
 
-/**
- * 사용자로부터 입력을 받는 역할을 한다.
- */
 public class InputView {
 
-    /**
-     * 다리의 길이를 입력받는다.
-     */
+
     public int readBridgeSize() {
         String bridgeSizeInput = Console.readLine();
         try {
@@ -30,9 +25,7 @@ public class InputView {
         return Integer.parseInt(bridgeSizeInput);
     }
 
-    /**
-     * 사용자가 이동할 칸을 입력받는다.
-     */
+
     public String readMoving() {
         String userMove = Console.readLine();
         try {
@@ -44,9 +37,6 @@ public class InputView {
         return userMove;
     }
 
-    /**
-     * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
-     */
     public String readGameCommand() {
         String command = Console.readLine();
         try {
