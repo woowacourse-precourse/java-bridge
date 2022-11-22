6e482231b33d7af52c8aaa77f1a1db21de435d3f
DavinAn <blueapple1030@gmail.com>
2022-11-22 22:26:51 +0900

    feat (controller) : add isInteger, helper method to catch NumberFormatException

--- src/main/java/bridge/controller/InputViewException.java
+++ src/main/java/bridge/controller/InputViewException.java
@@ -6,4 +6,22 @@ public class InputViewException {
     public static final String error03 = "[ERROR] 03 숫자의 범위가 올바르지 않습니다. (3 이상, 20 이하)";
     public static final String error04 = "[ERROR] 04 잘못된 칸을 입력하였습니다. (위: U, 아래: D)";
     public static final String error05 = "[ERROR] 05 잘못된 문자를 입력하였습니다. (재시도: R, 종료:  Q)";
+
+
+    /**
+     * Helper method to catch a NumberFormatException and change it into a IllegalArgumentException
+     *
+     * @param str String to confirm if integer or not
+     * @return false if String is an integer, true otherwise
+     */
+    public static boolean isInteger(String str) {
+        try {
+            Integer.parseInt(str);
+            return false;
+        } catch (NumberFormatException e) {
+            return true;
+        }
+    }
+
+
 }
