4194bb1b2d1dd2dbb5e81e32fedc9ceb00f7cf5b
desfox <desfox99@naver.com>
2022-11-22 18:40:11 +0900

    refactor(BridgeMakerValidation): 에러 문구 ErrorMessage Enum으로 대체

--- src/main/java/bridge/validation/BridgeMakerValidation.java
+++ src/main/java/bridge/validation/BridgeMakerValidation.java
@@ -2,6 +2,8 @@ package bridge.validation;
 
 import java.util.List;
 
+import static bridge.validation.ErrorMessage.*;
+
 public class BridgeMakerValidation {
     private static final int RANDOM_NUM_LOWER = 0;
     private static final int RANDOM_NUM_UPPER = 1;
@@ -25,7 +27,7 @@ public class BridgeMakerValidation {
 
         if (generatedNum == RANDOM_NUM_LOWER) {
             if (! bridge.get(lastIdx).equals(LOWER_BOX)) {
-                throw new IllegalArgumentException("[ERROR] 랜덤값이 0일 경우, 다리에 아래 칸이 추가되어야 합니다.");
+                throw new IllegalArgumentException(LOWER_BRIDGE_IF_RANDOM_VALUE_ZERO.getMessage());
             }
         }
     }
@@ -35,7 +37,7 @@ public class BridgeMakerValidation {
 
         if (generatedNum == RANDOM_NUM_UPPER) {
             if (! bridge.get(lastIdx).equals(UPPER_BOX)) {
-                throw new IllegalArgumentException("[ERROR] 랜덤값이 1일 경우, 다리에 위 칸이 추가되어야 합니다.");
+                throw new IllegalArgumentException(UPPER_BRIDGE_IF_RANDOM_VALUE_ONE.getMessage());
             }
         }
     }
