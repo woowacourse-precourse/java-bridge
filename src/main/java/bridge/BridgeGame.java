b45225d6416c12b9b66cce140d0104f08bb0c857
dlgosla <dlgoslaek1@gmail.com>
2022-11-22 13:25:22 +0900

    feat: 재시도 시 횟수 업데이트 및 포인터 초기화

--- src/main/java/bridge/BridgeGame.java
+++ src/main/java/bridge/BridgeGame.java
@@ -1,5 +1,7 @@
 package bridge;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 /**
@@ -8,7 +10,9 @@ import java.util.List;
 public class BridgeGame {
 
     private final Bridge bridge;
+
     private int pointer = 0;
+    private int tryCount = 1;
 
     BridgeGame(List<String> bridge) {
         this.bridge = new Bridge(bridge);
@@ -36,5 +40,27 @@ public class BridgeGame {
         }
     }
 
+    /**
+     * 사용자가 게임을 다시 시도할 때 사용하는 메서드
+     * <p>
+     * 재시작을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
+     */
+    public boolean retry(Command command) {
+        if (command.getType().equals(Command.Retry.getType())) {
+            initializePointer();
+            tryCount++;
+            return true;
+        }
+        return false;
+    }
+    
 
+    public void initializePointer() {
+        pointer = 0;
+    }
+
+
+    public int getTryCount() {
+        return tryCount;
+    }
 }
