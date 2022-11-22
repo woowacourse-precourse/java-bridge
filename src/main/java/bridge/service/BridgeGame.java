5bd3cfe2bc23f1a4600a1e5373036d18c87feaea
nakalic <nakalic28@gmail.com>
2022-11-22 11:16:43 +0900

    refactor(BridgeGame): 입력값에 따라 다르게 동작하는 코드 메소드로 추출

--- src/main/java/bridge/service/BridgeGame.java
+++ src/main/java/bridge/service/BridgeGame.java
@@ -12,36 +12,65 @@ public class BridgeGame {
     private static final String WHITE_SPACE = " ";
 
     public boolean move(GameStatus gameStatus, Bridge bridge, String s) {
-        if (s.equals(UP)) {
-            if (bridge.getBridge().get(0).equals(s)) {
-                gameStatus.topBridge.add(CORRECT);
-                gameStatus.bottomBridge.add(WHITE_SPACE);
-                bridge.getBridge().remove(0);
-                return true;
-            }
-            if (!bridge.getBridge().get(0).equals(s)) {
-                gameStatus.topBridge.add(WRONG);
-                gameStatus.bottomBridge.add(WHITE_SPACE);
-            }
-        }
-
-        if (s.equals(DOWN)) {
-            if (bridge.getBridge().get(0).equals(s)) {
-                gameStatus.bottomBridge.add(CORRECT);
-                gameStatus.topBridge.add(WHITE_SPACE);
-                bridge.getBridge().remove(0);
-                return true;
-            }
-            if (!bridge.getBridge().get(0).equals(s)) {
-                gameStatus.bottomBridge.add(WRONG);
-                gameStatus.topBridge.add(WHITE_SPACE);
-            }
-        }
-        return false;
+        return inputIsUp(gameStatus, bridge, s) || inputIsDown(gameStatus, bridge, s);
     }
 
     public void retry(GameStatus gameStatus) {
         gameStatus.tryCount += 1;
         gameStatus.resetGameStatus();
+    }
+
+    private boolean inputIsUp(GameStatus gameStatus, Bridge bridge, String s) {
+        if (s.equals(UP)) {
+            if (upAndCorrect(gameStatus, bridge, s)) {
+                return true;
+            }
+            upAndWrong(gameStatus, bridge, s);
+        }
+        return false;
+    }
+
+    private boolean upAndCorrect(GameStatus gameStatus, Bridge bridge, String s) {
+        if (bridge.getBridge().get(0).equals(s)) {
+            gameStatus.topBridge.add(CORRECT);
+            gameStatus.bottomBridge.add(WHITE_SPACE);
+            bridge.getBridge().remove(0);
+            return true;
+        }
+        return false;
+    }
+
+    private void upAndWrong(GameStatus gameStatus, Bridge bridge, String s) {
+        if (!bridge.getBridge().get(0).equals(s)) {
+            gameStatus.topBridge.add(WRONG);
+            gameStatus.bottomBridge.add(WHITE_SPACE);
+        }
+    }
+
+    private boolean inputIsDown(GameStatus gameStatus, Bridge bridge, String s) {
+        if (s.equals(DOWN)) {
+            if (downAndCorrect(gameStatus, bridge, s)) {
+                return true;
+            }
+            downAndWrong(gameStatus, bridge, s);
+        }
+        return false;
+    }
+
+    private boolean downAndCorrect(GameStatus gameStatus, Bridge bridge, String s) {
+        if (bridge.getBridge().get(0).equals(s)) {
+            gameStatus.bottomBridge.add(CORRECT);
+            gameStatus.topBridge.add(WHITE_SPACE);
+            bridge.getBridge().remove(0);
+            return true;
+        }
+        return false;
+    }
+
+    private void downAndWrong(GameStatus gameStatus, Bridge bridge, String s) {
+        if (!bridge.getBridge().get(0).equals(s)) {
+            gameStatus.bottomBridge.add(WRONG);
+            gameStatus.topBridge.add(WHITE_SPACE);
+        }
     }
 }
