cfaf600a5eeb49befbb636c664d3a63938e52466
jiwon <ljwon77@daum.net>
2022-11-22 18:24:09 +0900

    test: validate user move input value
    사용자가 입력한 이동할 칸에 대한 값의 검증 테스트를 추가하였습니다.

--- src/test/java/bridge/util/ValidationUtilTest.java
+++ src/test/java/bridge/util/ValidationUtilTest.java
@@ -1,15 +1,17 @@
 package bridge.util;
 
 import bridge.constant.message.ExceptionMessage;
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
+import static bridge.constant.BridgeMove.DOWN;
+import static bridge.constant.BridgeMove.UP;
 import static bridge.constant.BridgeRange.MAX_LENGTH;
 import static bridge.constant.BridgeRange.MIN_LENGTH;
 import static bridge.constant.message.ExceptionMessage.BRIDGE_LENGTH_RANGE;
-import static org.assertj.core.api.Assertions.*;
+import static bridge.constant.message.ExceptionMessage.PLAYER_MOVE;
+import static org.assertj.core.api.Assertions.assertThatCode;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class ValidationUtilTest {
@@ -55,5 +57,34 @@ class ValidationUtilTest {
                         validationUtil.validateBridgeLength(input))
                 .doesNotThrowAnyException();
     }
+
+    @DisplayName("이동할 칸으로 U 혹은 D가 들어오면 통과해야 한다.")
+    @ParameterizedTest
+    @ValueSource(strings = {"U", "D"})
+    void 이동할_칸_올바른_값_검증_테스트(String input) {
+        // given
+        ValidationUtil validationUtil = new ValidationUtil();
+
+        // when, then
+        assertThatCode(() ->
+                validationUtil.validatePlayerMoveInput(input))
+                .doesNotThrowAnyException();
+    }
+
+    @DisplayName("이동할 칸으로 U, D가 아닌 다른 값이 들어오면 오류가 발생해야 한다.")
+    @ParameterizedTest
+    @ValueSource(strings = {"29384", "A", "UU", "UD", "(@#$"})
+    void 이동할_칸_잘못된_값_검증_테스트(String input) {
+        // given
+        ValidationUtil validationUtil = new ValidationUtil();
+
+        // when, then
+        assertThatThrownBy(() ->
+                validationUtil.validatePlayerMoveInput(input))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(String.format(PLAYER_MOVE.getMessage(),
+                        UP.getIdentifier(), DOWN.getIdentifier()));
+    }
+
 }
 
