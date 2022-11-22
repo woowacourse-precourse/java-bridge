3ead02d6b18dca3ea440426ff8af6807dae28be7
sanghyuk2 <givemesomefoodplz@gmail.com>
2022-11-22 12:58:56 +0900

    feat: Retry value 탐색 기능 구현

--- src/main/java/bridge/domain/bridgegame/Retry.java
+++ src/main/java/bridge/domain/bridgegame/Retry.java
@@ -1,5 +1,7 @@
 package bridge.domain.bridgegame;
 
+import java.util.Arrays;
+
 public enum Retry {
     RESTART("R"),
     QUIT("Q");
@@ -8,6 +10,17 @@ public enum Retry {
 
     Retry(final String retryValue) {
         this.retryValue = retryValue;
+    }
+
+    public static Retry findRetry(String input) {
+        return Arrays.stream(Retry.values())
+                .filter(retry -> retry.isMatch(input))
+                .findFirst()
+                .orElseThrow(() -> new IllegalArgumentException("[ERROR] Retry enum 에 문제가 발생했습니다."));
+    }
+
+    private boolean isMatch(String input) {
+        return this.retryValue.equals(input);
     }
 
     public String getRetryValue() {
