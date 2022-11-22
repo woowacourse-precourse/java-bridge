2287cc594f915c130d410dcc4807b37c10f2a03b
SeHyeok <imasp@naver.com>
2022-11-22 13:58:46 +0900

    app

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -1,8 +1,47 @@
 package bridge;
 
 public class Application {
+    private final InputView input = new InputView();
+    private final OutputView output = new OutputView();
+    private final BridgeMaker newBridge = new BridgeMaker(new BridgeRandomNumberGenerator());
 
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        Application app = new Application();
+        BridgeGame newGame = app.settingGame();
+        app.runningGame(newGame);
+        app.closingGame(newGame);
+    }
+
+    public BridgeGame settingGame() {
+        output.printGameStart();
+        output.printAskBridgeSize();
+        return new BridgeGame(newBridge.makeBridge(input.readBridgeSize()));
+    }
+
+    public void runningGame(BridgeGame game) {
+        boolean playing = true;
+        while (playing && game.moveResult.size() < game.bridge.size()) {
+            playing = moving(game);
+            output.printMap(game.bridge, game.moveResult);
+            if (!playing) {
+                playing = playAgain(game);
+            }
+        }
+    }
+
+    public void closingGame(BridgeGame game) {
+        output.printResult(game);
+    }
+
+
+    private boolean moving(BridgeGame game) {
+        output.printAskMovement();
+        return game.move(input.readMoving());
+    }
+
+    private boolean playAgain(BridgeGame game) {
+        output.printAskRetry();
+        return game.retry(input.readGameCommand());
     }
 }
