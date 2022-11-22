48cdd07947694dcf1c11e0f8913603bcca3044ea
thuthi <whquddn55@gmail.com>
2022-11-22 19:53:00 +0900

    feat(bridgeGame): BridgeGame의 move 메서드 구현 다리 게임을 진행하여 입력으로 들어오는 BridgePosition만큼 현재 상태를 이동하는 move 메서드를 구현함.

--- src/main/java/bridge/bridgeGame/BridgeGame.java
+++ src/main/java/bridge/bridgeGame/BridgeGame.java
@@ -2,11 +2,12 @@ package bridge.bridgeGame;
 
 import bridge.bridge.BridgeMaker;
 import bridge.bridge.BridgeNumberGenerator;
+import bridge.bridge.BridgePosition;
 
 public class BridgeGame {
     private final BridgeMaker bridgeMaker;
     private BridgeGameState bridgeGameState;
-    private Integer tryCount = 1;
+    private final Integer tryCount = 1;
 
     public BridgeGame(BridgeNumberGenerator bridgeNumberGenerator) {
         this.bridgeMaker = new BridgeMaker(bridgeNumberGenerator);
@@ -18,5 +19,9 @@ public class BridgeGame {
 
     public Integer getTryCount() {
         return tryCount;
+    }
+
+    public void move(BridgePosition bridgePosition) {
+        bridgeGameState.move(bridgePosition);
     }
 }
--- src/main/java/bridge/bridgeGame/BridgeGameState.java
+++ src/main/java/bridge/bridgeGame/BridgeGameState.java
@@ -1,6 +1,7 @@
 package bridge.bridgeGame;
 
 import bridge.bridge.Bridge;
+import bridge.bridge.BridgePosition;
 import java.util.List;
 
 public class BridgeGameState {
@@ -11,5 +12,9 @@ public class BridgeGameState {
 
     public BridgeGameState(List<String> finalState) {
         this.finalState = new Bridge(finalState);
+    }
+
+    public void move(BridgePosition moving) {
+        currentState.add(moving.getPosition());
     }
 }
