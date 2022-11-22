100c716afb70c8f5ad1c257ce7d88e3f70e85e18
desfox <desfox99@naver.com>
2022-11-22 18:42:28 +0900

    refactor(ReadBridgeSizeValidation): 에러 문구 ErrorMessage Enum으로 대체

--- src/main/java/bridge/validation/ReadBridgeSizeValidation.java
+++ src/main/java/bridge/validation/ReadBridgeSizeValidation.java
@@ -1,5 +1,7 @@
 package bridge.validation;
 
+import static bridge.validation.ErrorMessage.*;
+
 public class ReadBridgeSizeValidation {
     private static final int MIN_BRIDGE_SIZE = 3;
     private static final int MAX_BRIDGE_SIZE = 20;
@@ -13,7 +15,7 @@ public class ReadBridgeSizeValidation {
         try {
             Integer.parseInt(inputBridgeSize);
         } catch (NumberFormatException e) {
-            throw new IllegalArgumentException("[ERROR] 다리의 길이를 숫자로 입력해주세요.");
+            throw new IllegalArgumentException(INPUT_BRIDGE_SIZE_IN_INTEGER.getMessage());
         }
     }
 
@@ -21,7 +23,7 @@ public class ReadBridgeSizeValidation {
         int bridgeSize = Integer.parseInt(inputBridgeSize);
 
         if (bridgeSize < MIN_BRIDGE_SIZE || bridgeSize > MAX_BRIDGE_SIZE) {
-            throw new IllegalArgumentException("[ERROR] 다리의 길이는 3이상, 20이하여야 합니다.");
+            throw new IllegalArgumentException(BRIDGE_SIZE_BETWEEN_THREE_AND_TWENTY.getMessage());
         }
     }
 
