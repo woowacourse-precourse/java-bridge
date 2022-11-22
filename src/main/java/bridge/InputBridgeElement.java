18b3a2d2b26c3b57789876dffc8c325339a87ceb
namtndus <rnrmf0721@gmail.com>
2022-11-22 18:14:46 +0900

    feat(InputBridgeElement): 인스턴트 변수 추가 및 변수를 활용한 함수inputResultChecked 구현

--- src/main/java/bridge/InputBridgeElement.java
+++ src/main/java/bridge/InputBridgeElement.java
@@ -3,14 +3,16 @@ package bridge;
 import java.util.Arrays;
 
 public enum InputBridgeElement {
-    O(true,"O"),X(false,"X");
+    O(true,"O","성공"),X(false,"X","실패");
 
     final boolean isChecked;
     final String inputLetter;
+    final String inputResult;
 
-    InputBridgeElement(boolean isChecked, String inputLetter) {
+    InputBridgeElement(boolean isChecked, String inputLetter, String inputResult) {
         this.inputLetter = inputLetter;
         this.isChecked = isChecked;
+        this.inputResult = inputResult;
     }
 
     public static String inputLetterChecked (boolean isChecked){
@@ -20,5 +22,11 @@ public enum InputBridgeElement {
                 .get().inputLetter;
     }
 
+    public static String inputResultChecked(boolean isChecked){
+        return Arrays.stream(InputBridgeElement.values())
+                .filter(x -> x.isChecked == isChecked)
+                .findAny()
+                .get().inputResult;
+    }
 
 }
