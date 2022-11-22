b04efd74d427e42b5598cdc31fb45f36c5f613b8
GilHun <suleemon1@naver.com>
2022-11-22 21:52:18 +0900

    feat(Bridge): 플레이어와 다리객체 비교 기능 추가

--- src/main/java/bridge/model/Bridge.java
+++ src/main/java/bridge/model/Bridge.java
@@ -6,15 +6,19 @@ import java.util.List;
 public class Bridge {
     private List<String> bridge;
 
-    public Bridge(int bridgeSize, BridgeMaker bridgeMaker){
-        this.bridge = bridgeMaker.makeBridge(bridgeSize);
-    }
-
-    public boolean checkPosition(int position, String moveSquare){
-        return bridge.get(position).equals(moveSquare);
+    public Bridge(List<String> bridge){
+        this.bridge = bridge;
     }
 
     public boolean checkEqual(List<String> playerBridge){
-        return Arrays.equals(bridge.toArray(),playerBridge.toArray());
+        return Arrays.equals(bridge.subList(0, playerBridge.size()).toArray(),playerBridge.toArray());
+    }
+
+    public boolean isSame(List<String> playerBridge){
+        return Arrays.equals(bridge.toArray(), playerBridge.toArray());
+    }
+
+    public String toString(){
+        return bridge.toString();
     }
 }
