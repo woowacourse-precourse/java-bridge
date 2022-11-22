39c429a96026e766a62b5e37c2c2b378768afa78
dooboocookie <bkh3030@gmail.com>
2022-11-22 11:34:09 +0900

    feat: InputView에서 소문자 입력 시 모두 대문자로 변환하여 읽을 수 있도록 추가

--- src/main/java/bridge/view/InputView.java
+++ src/main/java/bridge/view/InputView.java
@@ -13,6 +13,20 @@ public class InputView {
     private final String ASK_NEXT_STEP = "이동할 칸을 선택해주세요. (위: U, 아래: D)";
     private final String ASK_RETRY_GAME = "게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)";
 
+    private String readString() {
+        String input = Console.readLine().trim();
+        return inputToUpperCase(input);
+    }
+
+    private String inputToUpperCase(String input) {
+        StringBuilder stringBuilder = new StringBuilder();
+        for (int inputIndex = 0; inputIndex < input.length(); inputIndex++) {
+            char upperCase = Character.toUpperCase(input.charAt(inputIndex));
+            stringBuilder.append(upperCase);
+        }
+        return stringBuilder.toString();
+    }
+
     public int readBridgeSize() {
         System.out.println(ASK_BRIDGE_SIZE);
         String input = readString();
@@ -62,9 +76,5 @@ public class InputView {
         return !input.matches(
                 Rules.GAME_RETRY
                         + "|" + Rules.GAME_QUIT);
-    }
-
-    private String readString() {
-        return Console.readLine().trim();
     }
 }
