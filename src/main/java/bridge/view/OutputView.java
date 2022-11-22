a74b123666658d3fbe73c45578f9aaf4ac3133a0
thuthi <whquddn55@gmail.com>
2022-11-22 20:24:36 +0900

    feat(view): 게임 시작 문구를 출력하는 메서드 추가 OutputView에 printInitGameMessage 메서드 구현 추가.

--- src/main/java/bridge/view/OutputView.java
+++ src/main/java/bridge/view/OutputView.java
@@ -1,4 +1,7 @@
-package bridge;
+package bridge.view;
+
+import bridge.bridgeGame.BridgeGame;
+import bridge.bridgeGame.BridgeGameState;
 
 /**
  * 사용자에게 게임 진행 상황과 결과를 출력하는 역할을 한다.
@@ -10,7 +13,7 @@ public class OutputView {
      * <p>
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void printMap() {
+    public void printMap(BridgeGameState bridgeGameState) {
     }
 
     /**
@@ -18,6 +21,12 @@ public class OutputView {
      * <p>
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void printResult() {
+    public void printResult(BridgeGame bridgeGame) {
+    }
+
+    public void printInitGameMessage() {
+        System.out.println("다리 건너기 게임을 시작합니다.");
+        System.out.println();
+        System.out.println("다리의 길이를 입력해주세요.");
     }
 }
