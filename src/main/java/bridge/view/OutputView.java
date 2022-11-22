9abc822466cc69a9f1b89cc0b6924f06db0cec8b
This2sho <zx00018@naver.com>
2022-11-22 12:07:58 +0900

    feat(OutputView): 플레이어의 이동에 대한 건너기 결과 출력 기능 구현

--- src/main/java/bridge/view/OutputView.java
+++ src/main/java/bridge/view/OutputView.java
@@ -1,6 +1,8 @@
 package bridge.view;
 
 import bridge.Constants;
+import bridge.domain.GameRecord;
+import java.util.List;
 
 /**
  * 사용자에게 게임 진행 상황과 결과를 출력하는 역할을 한다.
@@ -16,9 +18,16 @@ public class OutputView {
      * <p>
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void printMap() {
+    public void printMap(GameRecord gameRecord) {
+        System.out.println(logToString(gameRecord.getUpLog()) + "\n" + logToString(gameRecord.getDownLog()));
     }
 
+    private String logToString(List<String> log) {
+        String PREFIX = "[ ";
+        String DELIMITER = " | ";
+        String SUFFIX = " ]";
+        return PREFIX + String.join(DELIMITER, log) + SUFFIX;
+    }
     /**
      * 게임의 최종 결과를 정해진 형식에 맞춰 출력한다.
      * <p>
