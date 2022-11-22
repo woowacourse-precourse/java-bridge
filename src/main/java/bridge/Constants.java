d618c2cf7b3b6d3f418bfadef190cf449eafd691
seonpilKim <ksp970306@gmail.com>
2022-11-22 23:38:21 +0900

    Feat: 상수 추가

---
+++ src/main/java/bridge/Constants.java
@@ -0,0 +1,14 @@
+package bridge;
+
+public final class Constants {
+
+	public static final String ERROR_PREFIX = "[ERROR]";
+	public static final String BLANK = " ";
+	public static final String OPEN_SQUARE_BRACKET = "[";
+	public static final String CLOSE_SQUARE_BRACKET = "]";
+	public static final String VERTICAL_LINE = "|";
+
+	private Constants() {
+	}
+
+}
