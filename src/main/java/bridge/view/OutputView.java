b7129af05d10bd1759094a8433c446556c09924e
yujeongkwon <yoojeongkwon@naver.com>
2022-11-22 21:24:03 +0900

    feat(OutputView) : 다리 그리기

---
+++ src/main/java/bridge/view/OutputView.java
@@ -0,0 +1,42 @@
+package bridge.view;
+
+import bridge.controller.BridgeGame;
+import bridge.domain.User;
+
+import java.util.List;
+
+/**
+ * 사용자에게 게임 진행 상황과 결과를 출력하는 역할을 한다.
+ */
+public class OutputView {
+    /**
+     * 현재까지 이동한 다리의 상태를 정해진 형식에 맞춰 출력한다.
+     * <p>
+     * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
+     */
+    public void printMap(User user) {
+        System.out.println(printBridge(user.getMapUpperBridge()));
+        System.out.println(printBridge(user.getMapLowerBridge()));
+        System.out.println();
+    }
+
+    /**
+     * 게임의 최종 결과를 정해진 형식에 맞춰 출력한다.
+     * <p>
+     * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
+     */
+    public void printResult(boolean isSuccess, User user) {
+
+    }
+
+    private String printBridge(List<String> bridge){
+        String printWord = "[ ";
+        for(int i=0; i<bridge.size(); i++) {
+            printWord += bridge.get(i);
+            if (i != bridge.size() - 1)
+                System.out.print(" | ");
+        }
+
+        return printWord + " ]";
+    }
+}
