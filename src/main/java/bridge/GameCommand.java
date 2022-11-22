a52a89db319c98cff1c4d492409d81ef85970588
SeongbinPark <binni0515@gmail.com>
2022-11-22 18:24:24 +0900

    feat(GameCommand): add static factory method

--- src/main/java/bridge/GameCommand.java
+++ src/main/java/bridge/GameCommand.java
@@ -1,5 +1,8 @@
 package bridge;
 
+import bridge.validation.exception.InvalidGameCommandException;
+import java.util.Arrays;
+
 public enum GameCommand {
 
     RETRY("R"),
@@ -9,6 +12,13 @@ public enum GameCommand {
 
     GameCommand(String mark) {
         this.mark = mark;
+    }
+
+    public static GameCommand from(String mark) {
+        return Arrays.stream(values())
+                .filter(g -> g.mark.equals(mark))
+                .findFirst()
+                .orElseThrow(InvalidGameCommandException::new);
     }
 
     public static boolean isRetry(GameCommand userCommand) {
