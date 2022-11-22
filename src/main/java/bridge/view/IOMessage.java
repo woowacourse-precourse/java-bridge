f0711127d8bf8a1553af275b228d52a101dab59f
minje <alswp0910@naver.com>
2022-11-22 15:38:30 +0900

    feat: 다리를 보여주는 기능 추가

--- src/main/java/bridge/view/IOMessage.java
+++ src/main/java/bridge/view/IOMessage.java
@@ -3,7 +3,10 @@ package bridge.view;
 public enum IOMessage {
     INPUT_SIZE_MESSAGE("다리의 길이를 입력해주세요."),
     INPUT_MOVE_MESSAGE("이동할 칸을 선택해주세요. (위: U, 아래: D)"),
-    INPUT_RESTART_MESSAGE("게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)");
+    INPUT_RESTART_MESSAGE("게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)"),
+    START_BRIDGE ("[ "),
+    END_BRIDGE (" ]"),
+    DIVIDE_BRIDGE (" | ");
     private final String message;
 
     IOMessage(final String message) {
