9a3436c183da708a699bb5b1794dd79fcba18b46
raycho <raycho521@yonsei.ac.kr>
2022-11-22 20:26:12 +0900

    feat(bridge): add main method
    2021.11.22 by Ray

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -1,8 +1,36 @@
 package bridge;
 
+import java.util.List;
+
 public class Application {
+    private static final InputView inputView = new InputView();
+    private static final OutputView outputView = new OutputView();
+    private static final BridgeMaker bridgeMaker = new BridgeMaker(new BridgeRandomNumberGenerator());
+    private static final BridgeGame bridgeGame = new BridgeGame(inputView, outputView);
 
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        List<String> bridge = start();
+        boolean playgame = true, isSuccess = false;
+        int count = 0;
+        while (playgame) {
+            count++;
+            isSuccess = bridgeGame.play(bridge);
+            if (isSuccess)
+                break;
+            else {
+                playgame = bridgeGame.retry();
+            }
+        }
+        quit(isSuccess,count);
+    }
+
+    public static List<String> start() {
+        System.out.println("다리 건너기 게임을 시작합니다.\n");
+        int size = inputView.readBridgeSize();
+        return bridgeMaker.makeBridge(size);
+    }
+
+    public static void quit(boolean isSuccess,int count) {
+        outputView.printResult(isSuccess, count);
     }
 }
