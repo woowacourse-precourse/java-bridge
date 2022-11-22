d6a9ce316ffe2ddde7489c2ca8de4583763279df
LeeJejune <leejejune0625@gmail.com>
2022-11-22 17:30:26 +0900

    feat: 포지션에 따른 다리 추가를 위한 메서드 구현

--- src/main/java/bridge/domain/BridgeGameMoveInfo.java
+++ src/main/java/bridge/domain/BridgeGameMoveInfo.java
@@ -10,4 +10,29 @@ public class BridgeGameMoveInfo {
     private static final String WRONG_SIGN = " X |";
     private final StringBuilder up = new StringBuilder("[");
     private final StringBuilder down = new StringBuilder("[");
+
+    public void moveInfo(BridgeGamePosition move, boolean correct) {
+        validateMove(move);
+
+        if (move == UP) {
+            addBridgeMove(up, down, correct);
+            return;
+        }
+        addBridgeMove(down, up, correct);
+    }
+
+    private void validateMove(BridgeGamePosition move) {
+        if (move == null) {
+            throw new IllegalArgumentException(MOVE_COMMAND_IS_NOT_NULL);
+        }
+    }
+
+    private void addBridgeMove(StringBuilder signBuilder, StringBuilder spaceBuilder, boolean correct) {
+        spaceBuilder.append(BLANK_SIGN);
+        if (correct) {
+            signBuilder.append(ANSWER_SIGN);
+            return;
+        }
+        signBuilder.append(WRONG_SIGN);
+    }
 }
