7906b8b23a50ce51776f2a94c08b20abbb493f02
hyeonseog-won <smreo75@gmail.com>
2022-11-22 22:47:50 +0900

    feat: 최종 게임 결과 출력 기능 컨트롤러, 메인에 적용

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -1,24 +1,19 @@
 package bridge;
 
+import bridge.controller.BridgeController;
+import bridge.model.BridgeGame;
+import bridge.model.BridgeMaker;
+import bridge.view.ConsolidatedView;
 import bridge.view.InputView;
 import bridge.view.OutputView;
 
 public class Application {
 
     public static void main(String[] args) {
-        try {
-            InputView inputView = new InputView();
-            OutputView outputView = new OutputView();
-
-            outputView.printStartMessage();
-            outputView.printMessageForBridgeSize();
-            inputView.readBridgeSize();
-            outputView.printMessageForMoving();
-            inputView.readMoving();
-            outputView.printMessageForGameCommand();
-            inputView.readGameCommand();
-        } catch (IllegalArgumentException error) {
-            System.out.println(error.getMessage());
-        }
+        ConsolidatedView consolidatedView = new ConsolidatedView(new InputView(), new OutputView());
+        BridgeMaker bridgeMaker = new BridgeMaker(new BridgeRandomNumberGenerator());
+        BridgeGame bridgeGame = new BridgeGame(bridgeMaker);
+        BridgeController bridgeController = new BridgeController(consolidatedView, bridgeGame);
+        bridgeController.startGame();
     }
 }
--- src/main/java/bridge/controller/BridgeController.java
+++ src/main/java/bridge/controller/BridgeController.java
@@ -1,36 +1,57 @@
 package bridge.controller;
 
 import bridge.model.BridgeGame;
-import bridge.model.BridgeMaker;
 import bridge.model.GameCommandFlag;
 import bridge.model.ResultFlag;
 import bridge.view.ConsolidatedView;
 
-import java.util.List;
-
 public class BridgeController {
     private final ConsolidatedView consolidatedView;
-    private final BridgeMaker bridgeMaker;
-    private BridgeGame bridgeGame;
+    private final BridgeGame bridgeGame;
 
-    public BridgeController(ConsolidatedView consolidatedView, BridgeMaker bridgeMaker) {
+    public BridgeController(ConsolidatedView consolidatedView, BridgeGame bridgeGame) {
         this.consolidatedView = consolidatedView;
-        this.bridgeMaker = bridgeMaker;
+        this.bridgeGame = bridgeGame;
     }
 
     public void startGame() {
         consolidatedView.printStartMessage();
-        List<String> bridge = bridgeMaker.makeBridge(consolidatedView.inputBridgeSize());
-
+        bridgeGame.create(consolidatedView.inputBridgeSize());
+        playGame();
+        endGame();
     }
 
     private void playGame() {
-
+        boolean isContinue = true;
+        while (isContinue) {
+            GameCommandFlag gameCommandFlag = moveBridge();
+            if (gameCommandFlag == GameCommandFlag.RETRY) {
+                bridgeGame.retry();
+            }
+            isContinue = isGameContinue(gameCommandFlag);
+        }
     }
 
     private GameCommandFlag moveBridge() {
         bridgeGame.move(consolidatedView.inputMoving());
         consolidatedView.printMoveMap(bridgeGame.getMap());
+        if (bridgeGame.getResultFlag() == ResultFlag.FAIL) {
+            return consolidatedView.inputGameCommand();
+        }
+        return GameCommandFlag.NOTHING;
+    }
 
+    private boolean isGameContinue(GameCommandFlag gameCommandFlag) {
+        if (gameCommandFlag == GameCommandFlag.QUIT) {
+            return false;
+        }
+        if (bridgeGame.completion()) {
+            return false;
+        }
+        return true;
+    }
+
+    private void endGame() {
+        consolidatedView.printGameResult(bridgeGame.getMap(), bridgeGame.getResultFlag(), bridgeGame.getTryCount());
     }
 }
