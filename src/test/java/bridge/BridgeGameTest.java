9a939db7962dd6f31a756175c568a024294e1cdf
neppiness <0414kjh@naver.com>
2022-11-22 15:00:21 +0900

    refactor: BridgeGameTest 클래스 수정 (src/test/java/bridge)
    - NsTest의 command 메소드를 활용해 콘솔 입력을 전달
    - move, retry, printMap, isEnd, isMoveWrong의 정상 출력 값을 확인

--- src/test/java/bridge/BridgeGameTest.java
+++ src/test/java/bridge/BridgeGameTest.java
@@ -4,6 +4,7 @@ import camp.nextstep.edu.missionutils.test.NsTest;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.io.ByteArrayInputStream;
 import java.util.List;
 
 import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
@@ -14,71 +15,125 @@ class BridgeGameTest extends NsTest {
     private static final String ERROR_MESSAGE = "[ERROR]";
     static BridgeGame bridgeGame;
 
-    @DisplayName("BridgeGame 클래스 종합 테스트")
+    private void command(final String... args) {
+        final byte[] buf = String.join("\n", args).getBytes();
+        System.setIn(new ByteArrayInputStream(buf));
+    }
+
+    @DisplayName("BridgeGame 클래스 종합 테스트1")
     @Test
-    void bridgeGameTest() {
+    void bridgeGameTest1() {
+        List<String> bridge = List.of("U", "D", "U");
+        bridgeGame = new BridgeGame(bridge);
+
         assertSimpleTest(() -> {
-            run("U", "D", "D", "U");
+            command("D");
+            bridgeGame.move();
+            bridgeGame.printMap();
+
+            System.out.printf("isEnd 반환값: %b\n", bridgeGame.isEnd());
+            System.out.printf("isMoveWrong 반환값: %b\n", bridgeGame.isMoveWrong());
+
             assertThat(output()).doesNotContain(ERROR_MESSAGE).contains(
-                    "[ O ]",
                     "[   ]",
-                    "#1 isEnd 반환값: false",
-                    "#1 isMoveWrong 반환값: false",
+                    "[ X ]",
+                    "isEnd 반환값: false",
+                    "isMoveWrong 반환값: true"
+            );
+
+            int upSideIndex = output().indexOf("[   ]");
+            int downSideIndex = output().indexOf("[ X ]");
+            assertThat(upSideIndex).isLessThan(downSideIndex);
+        });
+    }
+
+    @DisplayName("BridgeGame 클래스 종합 테스트2")
+    @Test
+    void bridgeGameTest2() {
+        List<String> bridge = List.of("U", "D", "U");
+        bridgeGame = new BridgeGame(bridge);
+
+        assertSimpleTest(() -> {
+            command("U", "D");
+            bridgeGame.move();
+            bridgeGame.move();
+            bridgeGame.printMap();
+
+            System.out.printf("isEnd 반환값: %b\n", bridgeGame.isEnd());
+            System.out.printf("isMoveWrong 반환값: %b\n", bridgeGame.isMoveWrong());
+
+            assertThat(output()).doesNotContain(ERROR_MESSAGE).contains(
                     "[ O |   ]",
                     "[   | O ]",
-                    "#2 isEnd 반환값: false",
-                    "#2 isMoveWrong 반환값: false",
+                    "isEnd 반환값: false",
+                    "isMoveWrong 반환값: false"
+            );
+
+            int upSideIndex = output().indexOf("[ O |   ]");
+            int downSideIndex = output().indexOf("[   | O ]");
+            assertThat(upSideIndex).isLessThan(downSideIndex);
+        });
+    }
+
+    @DisplayName("BridgeGame 클래스 종합 테스트3")
+    @Test
+    void bridgeGameTest3() {
+        List<String> bridge = List.of("U", "D", "U");
+        bridgeGame = new BridgeGame(bridge);
+
+        assertSimpleTest(() -> {
+            command("U", "D", "D");
+            bridgeGame.move();
+            bridgeGame.move();
+            bridgeGame.move();
+            bridgeGame.printMap();
+
+            System.out.printf("isEnd 반환값: %b\n", bridgeGame.isEnd());
+            System.out.printf("isMoveWrong 반환값: %b\n", bridgeGame.isMoveWrong());
+
+            assertThat(output()).doesNotContain(ERROR_MESSAGE).contains(
                     "[ O |   |   ]",
                     "[   | O | X ]",
-                    "#3 isEnd 반환값: false",
-                    "#3 isMoveWrong 반환값: true",
-                    "[ O |   | O ]",
-                    "[   | O |   ]",
-                    "#4 isEnd 반환값: true",
-                    "#4 isMoveWrong 반환값: false",
-                    "최종 게임 결과",
-                    "[ O |   | O ]",
-                    "[   | O |   ]",
-                    "게임 성공 여부: 성공",
-                    "총 시도한 횟수: 1000000"
+                    "isEnd 반환값: false",
+                    "isMoveWrong 반환값: true"
             );
+
+            int upSideIndex = output().indexOf("[ O |   |   ]");
+            int downSideIndex = output().indexOf("[   | O | X ]");
+            assertThat(upSideIndex).isLessThan(downSideIndex);
+        });
+    }
+
+    @DisplayName("BridgeGame 클래스 종합 테스트4")
+    @Test
+    void bridgeGameTest4() {
+        List<String> bridge = List.of("U", "D", "U");
+        bridgeGame = new BridgeGame(bridge);
+
+        assertSimpleTest(() -> {
+            command("U", "D", "D", "U");
+            bridgeGame.move();
+            bridgeGame.move();
+            bridgeGame.move();
+            bridgeGame.retry();
+            bridgeGame.printMap();
+
+            System.out.printf("isEnd 반환값: %b\n", bridgeGame.isEnd());
+            System.out.printf("isMoveWrong 반환값: %b\n", bridgeGame.isMoveWrong());
+
+            assertThat(output()).doesNotContain(ERROR_MESSAGE).contains(
+                    "[ O |   | O ]",
+                    "[   | O |   ]",
+                    "isEnd 반환값: true",
+                    "isMoveWrong 반환값: false"
+            );
+
+            int upSideIndex = output().indexOf("[ O |   | O ]");
+            int downSideIndex = output().indexOf("[   | O |   ]");
+            assertThat(upSideIndex).isLessThan(downSideIndex);
         });
     }
 
     @Override
-    protected void runMain() {
-        List<String> bridge = List.of("U", "D", "U");
-        bridgeGame = new BridgeGame(bridge);
-
-        int numberOfMove = 1;
-        int numberOfTry = 1_000_000;
-
-        bridgeGame.move();
-        bridgeGame.printMap();
-        System.out.printf("#%d isEnd 반환값: %b\n", numberOfMove, bridgeGame.isEnd());
-        System.out.printf("#%d isMoveWrong 반환값: %b\n", numberOfMove, bridgeGame.isMoveWrong());
-        System.out.println();
-
-        numberOfMove++;
-        bridgeGame.move();
-        bridgeGame.printMap();
-        System.out.printf("#%d isEnd 반환값: %b\n", numberOfMove, bridgeGame.isEnd());
-        System.out.printf("#%d isMoveWrong 반환값: %b\n", numberOfMove, bridgeGame.isMoveWrong());
-        System.out.println();
-
-        numberOfMove++;
-        bridgeGame.move();
-        bridgeGame.printMap();
-        System.out.printf("#%d isEnd 반환값: %b\n", numberOfMove, bridgeGame.isEnd());
-        System.out.printf("#%d isMoveWrong 반환값: %b\n", numberOfMove, bridgeGame.isMoveWrong());
-        System.out.println();
-
-        numberOfMove++;
-        bridgeGame.retry();
-        bridgeGame.printMap();
-        System.out.printf("#%d isEnd 반환값: %b\n", numberOfMove, bridgeGame.isEnd());
-        System.out.printf("#%d isMoveWrong 반환값: %b\n", numberOfMove, bridgeGame.isMoveWrong());
-
-        bridgeGame.printResult(numberOfTry);
-    }
+    protected void runMain() {}
 }
