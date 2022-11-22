277221a7e59ab6581bc07d8cf1101c23bf7f13e1
ycopp9 <aciees95@gmail.com>
2022-11-22 17:37:36 +0900

    feat(BridgeGame): 사용자 입력에 따른 게임 재시작 기능 구현
    사용자 입력에 따라 재시작하는 경우 다리 입력 정보를 초기화하거나 그대로
    게임을 끝내는 메소드 구현

--- src/main/java/bridge/BridgeGame.java
+++ src/main/java/bridge/BridgeGame.java
@@ -1,5 +1,6 @@
 package bridge;
 
+import bridge.constant.Token;
 import bridge.domain.Bridge;
 
 /**
@@ -27,7 +28,12 @@ public class BridgeGame {
      * <p>
      * 재시작을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void retry() {
+    public boolean retry(String userInput) {
+        if (userInput.equals(Token.RESTART.getMark())) {
+            bridge.reset();
+            return true;
+        }
+        return false;
     }
 
     public void start(int size) {
