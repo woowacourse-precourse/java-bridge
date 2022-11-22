492b94f6e300c85636f7bf7bdbe95f8439f537eb
jagajang <kosb7544@gmail.com>
2022-11-22 11:38:28 +0900

    test: create new class for custom input setting

---
+++ src/main/java/bridge/view/TestInputView.java
@@ -0,0 +1,30 @@
+package bridge.view;
+
+import java.util.List;
+
+public class TestInputView extends InputView {
+    private int bridgeSize;
+    private List<String> userInput;
+    private int inputIdx;
+
+    TestInputView(int bridgeSize, List<String> userInput) {
+        this.bridgeSize = bridgeSize;
+        this.userInput = userInput;
+        this.inputIdx = 0;
+    }
+
+    @Override
+    public int readBridgeSize() {
+        return bridgeSize;
+    }
+
+    @Override
+    public String readMoving() {
+        return userInput.get(inputIdx++);
+    }
+
+    @Override
+    public String readGameCommand() {
+        return userInput.get(inputIdx++);
+    }
+}
