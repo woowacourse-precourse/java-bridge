014deef4a42014c33f65b33d26dd73043c0f748b
yeop0740 <ssucgoo@gmail.com>
2022-11-22 22:30:34 +0900

    feat(OutputView): 게임 종료시 결과를 나타내는 기능 구현(printResult)

--- src/main/java/bridge/OutputView.java
+++ src/main/java/bridge/OutputView.java
@@ -56,6 +56,14 @@ public class OutputView {
      * <p>
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void printResult() {
+    public void printResult(List<String> bridge, List<String> lastStates, String gameCommand, int count) {
+        String result = "성공";
+        System.out.println("최종 게임 결과");
+        printMap(bridge, lastStates);
+        if (gameCommand.equals("Q")) {
+            result = "실패";
+        }
+        System.out.println("게임 성공 여부: " + result);
+        System.out.println("총 시도한 횟수: " + count);
     }
 }
