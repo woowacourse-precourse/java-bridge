3c3dfed3f28ecaed7d9e54c294f3b1c275028c1a
YEJIN <yejin9858@sookmyung.ac.kr>
2022-11-22 22:35:21 +0900

    [FEAT] 다리 생성하기

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -1,10 +1,17 @@
 package bridge;
 
+import java.util.List;
+
 public class Application {
 
     public static void main(String[] args) {
         InputView inputView = new InputView();
-        inputView.readBridgeSize();
+        BridgeMaker bridgeMaker = new BridgeMaker(new BridgeRandomNumberGenerator());
+        List<String> bridge = bridgeMaker.makeBridge(inputView.readBridgeSize());
+
+
+
+
 
 
     }
