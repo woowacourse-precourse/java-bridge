a0a699ddc3069dcb718ad305d65472c5e36bdd9b
박정현 <ros28081@naver.com>
2022-11-22 14:38:08 +0900

    [FEAT] OutputView - 게임 시작, 다리 길이 입력, 이동할 칸 선택 메시지 출력

--- src/main/java/bridge/OutputView.java
+++ src/main/java/bridge/OutputView.java
@@ -4,6 +4,18 @@ package bridge;
  * 사용자에게 게임 진행 상황과 결과를 출력하는 역할을 한다.
  */
 public class OutputView {
+    public void printStartingGameMessage() {
+        System.out.println("다리 건너기 게임을 시작합니다.");
+        System.out.println();
+    }
+
+    public void printAskingBridgeLengthMessage() {
+        System.out.println("다리의 길이를 입력해주세요.");
+    }
+
+    public void printMovingUpDownMessage() {
+        System.out.println("이동할 칸을 선택해주세요. (위: U, 아래: D)");
+    }
 
     /**
      * 현재까지 이동한 다리의 상태를 정해진 형식에 맞춰 출력한다.
@@ -11,6 +23,7 @@ public class OutputView {
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
     public void printMap() {
+
     }
 
     /**
@@ -19,5 +32,11 @@ public class OutputView {
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
     public void printResult() {
+        /*
+            1. 최종 게임 결과 불러오기
+            2. 게임 성공/실패 여부 불러오기
+            3. 총 시도한 횟수 불러오기
+         */
+        System.out.println("최종 게임 결과");
     }
 }
