02951460431644277afb021d81f49b924180b65c
josolha <josolha@nate.com>
2022-11-22 15:33:09 +0900

    fix(Validte) : 숫자 3~20 사이 이외값 예외처리

--- src/main/java/bridge/validator/Validate.java
+++ src/main/java/bridge/validator/Validate.java
@@ -2,7 +2,9 @@ package bridge.validator;
 
 public class Validate {
 
-    public static void validateBridgeSize(String bridgeSizeInput) {
+    public static void validateBridgeSizeNumber(String bridgeSizeInput) {
+        int number = Integer.parseInt(bridgeSizeInput);
+        validateBetweenNumber(number);
         for (int i = 0; i < bridgeSizeInput.length(); i++) {
             if (!Character.isDigit(bridgeSizeInput.charAt(i))) {
                 throw new IllegalArgumentException();
@@ -10,14 +12,19 @@ public class Validate {
         }
 
     }
-    public static void validateCheckRetry(String command){
-        if(!command.equals("Q") && !command.equals("R")){
+    public static void validateCheckRetry(String command) {
+        if (!command.equals("Q") && !command.equals("R")) {
             throw new IllegalArgumentException();
         }
-
     }
-    public static void validateCheckMove(String userMove){
-        if(!userMove.equals("U") && !userMove.equals("D")){
+
+    public static void validateCheckMove(String userMove) {
+        if (!userMove.equals("U") && !userMove.equals("D")) {
+            throw new IllegalArgumentException();
+        }
+    }
+    public static void validateBetweenNumber(int number) {
+        if (!(number <= 20 && number >= 3)) {
             throw new IllegalArgumentException();
         }
     }
