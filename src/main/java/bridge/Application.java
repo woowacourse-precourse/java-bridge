0ff16c70614b07a995c8b8d9810d9399f142dbeb
Kwak-JunYoung <klop9090@g.skku.edu>
2022-11-22 12:03:00 +0900

    feat(Application/main) Applied try-catch to catch errors such as IllegalArgumentException or IllegalStateException

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -8,17 +8,11 @@ public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
         System.out.println("다리 건너기 게임을 시작합니다.");
-        InputView inputView = new InputView();
-        OutputView outputView = new OutputView();
-        int bridgeSize = inputView.readBridgeSize();
-        BridgeNumberGenerator bridgeNumberGenerator = new BridgeRandomNumberGenerator();
-        BridgeMaker bridgeMaker = new BridgeMaker(bridgeNumberGenerator);
-        List<String> userMoveHistory = new ArrayList<String>();
-        List<String> bridge = bridgeMaker.makeBridge(bridgeSize);
-        for (int i = 0; i < bridgeSize; i++) {
-            String userMove = inputView.readMoving();
-            userMoveHistory.add(userMove);
-            outputView.printMap(userMoveHistory, bridge, bridgeSize);
+        try {
+            BridgeGame bridgeGame = new BridgeGame();
+            bridgeGame.move(1);
+        } catch (Exception e) {
+            System.out.println(e);
         }
     }
 }
