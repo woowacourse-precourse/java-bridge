8d88ef4a42c34cbef32e59a71a6b5c27a77ab04a
mng990 <ryoo0504@pusan.ac.kr>
2022-11-22 18:45:43 +0900

    feat(main/java/bridge/Application.java): Application.java 작성
    프로그램을 실행하는 main 클래스

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -2,7 +2,19 @@ package bridge;
 
 public class Application {
 
+    static BridgeController bridgeController =
+            new BridgeController(new BridgeRandomNumberGenerator());
+
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+
+        init();
+        play(true);
+    }
+    public static void init(){
+        bridgeController.initGame();
+    }
+    public static void play(boolean play){
+        bridgeController.playGame(play);
     }
 }
