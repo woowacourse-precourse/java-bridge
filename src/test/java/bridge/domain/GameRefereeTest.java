ba46212bd165440e4a230939440608024cfd0194
eun-byeol <62530884+eun-byeol@users.noreply.github.com>
2022-11-22 22:04:06 +0900

    Style(GameRefereeTest): 사용하지 않는 import문 삭제

--- src/test/java/bridge/domain/GameRefereeTest.java
+++ src/test/java/bridge/domain/GameRefereeTest.java
@@ -1,20 +1,14 @@
 package bridge.domain;
 
-import bridge.domain.Bridge;
-import bridge.domain.GameReferee;
-import bridge.domain.PlayerMoving;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
-import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.List;
 import java.util.stream.Stream;
 
-import static bridge.utils.ErrorMessage.*;
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class GameRefereeTest {
 
