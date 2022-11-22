d6a6b55e0b54bad6518ec6d66afc0c7b340d923f
LeeJejune <leejejune0625@gmail.com>
2022-11-22 17:37:45 +0900

    feat: 다리의 칸 및 재시작 여부를 위한 입력 메서드 구현

--- src/main/java/bridge/view/InputView.java
+++ src/main/java/bridge/view/InputView.java
@@ -39,4 +39,18 @@ public class InputView {
             throw new IllegalArgumentException(MAX_SIZE_MESSAGE);
         }
     }
+
+    /**
+     * 사용자가 이동할 칸을 입력받는다.
+     */
+    public String readMoving() {
+        return Console.readLine();
+    }
+
+    /**
+     * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
+     */
+    public String readGameCommand() {
+        return Console.readLine();
+    }
 }
