1e88ab79b4301093217971bc7fb5f30d1c8777f0
jaehyeon <jhjh0022@naver.com>
2022-11-22 19:40:23 +0900

    refactor: 출력 문구 상수화

--- src/main/java/bridge/constants/Message.java
+++ src/main/java/bridge/constants/Message.java
@@ -1,16 +1,27 @@
 package bridge.constants;
 
-public enum Error {
+public enum Message {
 
-    ERROR_PREFIX("[ERROR] ");
+    ERROR_PREFIX("[ERROR] "),
+    START_GAME("다리 건너기 게임을 시작합니다."),
+    REQUEST_SIZE("다리의 길이를 입력해주세요."),
+    REQUEST_BRIDGE("이동할 칸을 선택해주세요. (위: U, 아래: D)"),
+    REQUEST_RETRY("게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)"),
+    RESULT_GAME("최종 게임 결과"),
+    SUCCESS_WHETHER("게임 성공 여부: "),
+    TRY_COUNT("총 시도한 횟수: "),
+    SUCCESS("성공"),
+    FAIL("실패"),
+    EMPTY("");
+
 
     private final String value;
 
-    Error(String value){
+    Message(String value) {
         this.value = value;
     }
 
-    public String getValue(){
+    public String getValue() {
         return value;
     }
 
