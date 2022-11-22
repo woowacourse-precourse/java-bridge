25f546870cf77906694526cb9c7446c40e1babed
DDing77 <devseo2021@gmail.com>
2022-11-22 19:04:33 +0900

    refactor(exception) : 에러 문자열 생성 로직 변경

--- src/main/java/bridge/exception/ExceptionInput.java
+++ src/main/java/bridge/exception/ExceptionInput.java
@@ -4,9 +4,10 @@ import bridge.constant.message.ErrorMessage;
 
 public class ExceptionInput {
 
-    private static StringBuilder sb = new StringBuilder(ErrorMessage.NOTICE.getMessage());
+    private static StringBuilder sb;
 
     public static int validateInputLength(int size) {
+        sb = new StringBuilder(ErrorMessage.NOTICE.getMessage());
         if( !(3 <= size && size <= 20)) {
             sb.append(ErrorMessage.ERROR_INPUT_LENGTH_RANGE.getMessage());
             throw new IllegalArgumentException(sb.toString());
