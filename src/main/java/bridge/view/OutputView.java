2217d37e663874cad5367f670cea1fbd9c0f447f
jihwan <sgh002400@naver.com>
2022-11-22 16:32:27 +0900

    chore(OutputView): 이동할 칸 선택 메시지 수정

--- src/main/java/bridge/view/OutputView.java
+++ src/main/java/bridge/view/OutputView.java
@@ -8,7 +8,7 @@ import bridge.domain.BridgeGameResult;
 public class OutputView {
 	private static final String GAME_START_MESSAGE = "다리 건너기 게임을 시작합니다.";
 	private static final String INPUT_BRIDGE_SIZE_MESSAGE = "다리의 길이를 입력하세요.";
-	private static final String INPUT_MOVING_DIRECTION_MESSAGE = "이동할 칸을 선택해주세요." + " 위: U, 아래: D";
+	private static final String INPUT_MOVING_DIRECTION_MESSAGE = "이동할 칸을 선택해주세요." + " (위: U, 아래: D)";
 	private static final String WHETHER_RESTART_OR_NOT_MESSAGE = "게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)";
 	private static final String FINAL_GAME_RESULT = "최종 게임 결과";
 	private static final String GAME_SUCCESS_OR_FAIL_MESSAGE = "게임 성공 여부: ";
