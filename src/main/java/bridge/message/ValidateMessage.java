38d126cb9639300d69e21b9f1a016122dde41734
seheonCho <whtpgjs94@gmail.com>
2022-11-22 17:30:04 +0900

    refactor(ValidateMessage): 클래스 시그니처 변경
    final 을 추가하여, 상속을 막음

--- src/main/java/bridge/message/ValidateMessage.java
+++ src/main/java/bridge/message/ValidateMessage.java
@@ -1,6 +1,6 @@
 package bridge.message;
 
-public class ValidateMessage {
+public final class ValidateMessage {
 
     private static final String MESSAGE_SUFFIX = "[ERROR] ";
     public static final String INVALID_INPUT = MESSAGE_SUFFIX + "올바르지 않은 입력입니다.";
