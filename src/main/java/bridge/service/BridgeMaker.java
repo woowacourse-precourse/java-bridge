434601866fcbba3a8b0a5ed097c19d103e8a2f30
baksohyeon <bori12370@gmail.com>
2022-11-22 14:47:45 +0900

    refactor: 하드코딩된 변수 값 수정
    comment: 불필요한 주석 삭제

--- src/main/java/bridge/service/BridgeMaker.java
+++ src/main/java/bridge/service/BridgeMaker.java
@@ -1,15 +1,13 @@
 package bridge.service;
 
+import bridge.domain.CommandConstant;
 import bridge.domain.ErrorMessageConstant;
 import bridge.util.BridgeNumberGenerator;
 
 import java.util.ArrayList;
 import java.util.List;
 
-/**
- * 다리의 길이를 입력 받아서 다리를 생성해주는 역할을 한다.
- * 클래스 제한 사항: 인스턴스 변수(필드) 변경 불가, 인자, 이름, 반환타입 변경 불가
- */
+
 public class BridgeMaker {
 
     private final BridgeNumberGenerator bridgeNumberGenerator;
@@ -18,10 +16,7 @@ public class BridgeMaker {
         this.bridgeNumberGenerator = bridgeNumberGenerator;
     }
 
-    /**
-     * @param size 다리의 길이
-     * @return 입력받은 길이에 해당하는 다리 모양. 위 칸이면 "U", 아래 칸이면 "D"로 표현해야 한다.
-     */
+
     public List<String> makeBridge(int size) {
         List<String> bridge = new ArrayList<>();
         for (int i = 0; i < size; i++) {
@@ -34,10 +29,10 @@ public class BridgeMaker {
 
     private String designatePath(int randomNumber) {
         if (randomNumber == 0) {
-            return "D";
+            return CommandConstant.DOWN;
         }
         if(randomNumber == 1) {
-            return "U";
+            return CommandConstant.UP;
         }
         throw new IllegalArgumentException(ErrorMessageConstant.RANDOM_NUMBER_GENERATOR_OPERATION_FAILED);
     }
