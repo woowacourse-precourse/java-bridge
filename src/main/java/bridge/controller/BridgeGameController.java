fdc4e4252cc60a44a510f3d89586c56b03cbfee0
jmjmjames <chungjm0711@gmail.com>
2022-11-22 13:57:04 +0900

    feat: OutputView$printMap 구현

--- src/main/java/bridge/controller/BridgeGameController.java
+++ src/main/java/bridge/controller/BridgeGameController.java
@@ -1,9 +1,11 @@
 package bridge.controller;
 
+import bridge.domain.MoveResult;
 import bridge.domain.Player;
 import bridge.service.BridgeGameService;
 import bridge.view.InputView;
 import bridge.view.OutputView;
+import java.util.List;
 import java.util.function.Supplier;
 
 public class BridgeGameController {
@@ -19,6 +21,8 @@ public class BridgeGameController {
     public void run() {
         initGame();
         Player player = new Player();
+        List<List<MoveResult>> result = List.of(List.of(MoveResult.SUCCESS, MoveResult.SUCCESS, MoveResult.FAIL));
+        outputView.printMap(result);
     }
 
     private void initGame() {
--- src/main/java/bridge/view/OutputView.java
+++ src/main/java/bridge/view/OutputView.java
@@ -1,10 +1,19 @@
 package bridge.view;
 
+import static java.text.MessageFormat.format;
+import static java.util.stream.Collectors.*;
+
+import bridge.domain.MoveResult;
+import java.util.List;
+import java.util.stream.Collectors;
+
 /**
  * 사용자에게 게임 진행 상황과 결과를 출력하는 역할을 한다.
  */
 public class OutputView {
     private static final String GAME_START_MESSAGE = "다리 건너기 게임을 시작합니다.";
+    private static final String MAP_MESSAGE_FORMAT = "[ {0} ]";
+    private static final String MAP_MESSAGE_DELIMITER = " | ";
 
     public void printGameStartMessage() {
         System.out.println(GAME_START_MESSAGE);
@@ -15,7 +24,16 @@ public class OutputView {
      * <p>
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void printMap() {
+    public void printMap(List<List<MoveResult>> results) {
+        for (List<MoveResult> result : results) {
+            System.out.println(getMapMessage(result));
+        }
+    }
+
+    private String getMapMessage(List<MoveResult> result) {
+        return format(MAP_MESSAGE_FORMAT, result.stream()
+                .map((MoveResult::toString))
+                .collect(joining(MAP_MESSAGE_DELIMITER)));
     }
 
     /**
@@ -26,11 +44,6 @@ public class OutputView {
     public void printResult() {
     }
 
-    /**
-     * 발생한 에러에 대해 맞춰 출력한다.
-     * <p>
-     *
-     */
     public void printError(String message) {
         System.out.println(message);
     }
