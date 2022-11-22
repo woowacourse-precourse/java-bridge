07e897344ba42b9ff82ce5092d3a4681368f0a14
SeongbinPark <binni0515@gmail.com>
2022-11-22 19:04:08 +0900

    feat(OutputView): add printFinalMessage method

--- src/main/java/bridge/view/OutputView.java
+++ src/main/java/bridge/view/OutputView.java
@@ -5,6 +5,8 @@ import static bridge.constant.message.InputMessage.RETRY_OR_QUIT;
 import static bridge.constant.message.InputMessage.SELECT_WHERE_TO_MOVE;
 import static bridge.constant.message.OutputMessage.RESULT_OF_GAME;
 import static bridge.constant.message.OutputMessage.START_OF_GAME;
+import static bridge.constant.message.OutputMessage.SUCCESS_OR_FAILURE;
+import static bridge.constant.message.OutputMessage.TOTAL_ATTEMPTS;
 
 import bridge.Map;
 import java.util.List;
@@ -37,5 +39,10 @@ public class OutputView {
     public void printResult(Map map) {
         System.out.println(RESULT_OF_GAME.getMessage());
         map.print();
+    }
+
+    public void printFinalMessage(String success, int attempts) {
+        System.out.println(SUCCESS_OR_FAILURE.getMessage() + success);
+        System.out.println(TOTAL_ATTEMPTS.getMessage() + attempts);
     }
 }
