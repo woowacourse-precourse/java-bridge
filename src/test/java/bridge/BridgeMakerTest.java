7af3b7a42f32e9c306c86dfc65e6ad2c57f99641
juno-junho <ssmm0205@naver.com>
2022-11-22 23:44:57 +0900

    test: test refactoring

--- src/test/java/bridge/BridgeMakerTest.java
+++ src/test/java/bridge/BridgeMakerTest.java
@@ -10,7 +10,7 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class BridgeMakerTest {
 
-    @ParameterizedTest(name = "{0}을 입력시 길이가 {0}인 List<String>을 반환한다.")
+    @ParameterizedTest(name = "{0}을 입력시 길이가 {0}인 List 반환")
     @ValueSource(ints = {1, 5, 15})
     void createBridgeSuccess(int size) {
         BridgeMaker bridgeMaker = new BridgeMaker(new BridgeRandomNumberGenerator());
