ef9933d64d607e5b48bea313419adb7ab472902e
SeongbinPark <binni0515@gmail.com>
2022-11-22 22:43:35 +0900

    test(ValidationUtilTest): change InputTest to ValidationUtilTest

--- src/test/java/bridge/ValidationUtilTest.java
+++ src/test/java/bridge/ValidationUtilTest.java
@@ -7,7 +7,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
-public class InputTest {
+public class ValidationUtilTest {
 
     @DisplayName("다리 크기 범위 밖의 입력에 대한 예외 처리")
     @ValueSource(strings = {"99999999999", "-1", "0", "-1000000000000000000"})
