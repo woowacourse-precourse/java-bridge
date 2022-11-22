7a5a32cd9da8db51d4b9d00baccdd2ca19870cf4
mardi2020 <mardi@khu.ac.kr>
2022-11-22 12:56:59 +0900

    feat(BridgeApp.java): 다리 크기 받는 메서드 추가

---
+++ src/main/java/bridge/BridgeApp.java
@@ -0,0 +1,30 @@
+package bridge;
+
+public class BridgeApp {
+
+    private static int count = 1;
+
+    private final BridgeMaker bridgeMaker;
+    private final OutputView outputView;
+    private final InputView inputView;
+    private BridgeGame bridgeGame;
+
+    private String move;
+    private String command;
+
+    public BridgeApp() {
+        this.inputView = new InputView();
+        this.outputView = new OutputView();
+        this.bridgeMaker = new BridgeMaker(new BridgeRandomNumberGenerator());
+        this.move = "";
+        this.command = "";
+    }
+
+    public void startGame() {
+        int bridgeSize = -1;
+        while (bridgeSize < 3 || bridgeSize > 20) {
+            bridgeSize = inputView.readBridgeSize();
+        }
+        bridgeGame = new BridgeGame(bridgeMaker.makeBridge(bridgeSize), bridgeSize);
+    }
+}
