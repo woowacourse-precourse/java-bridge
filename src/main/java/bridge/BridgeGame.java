b3f73f6931160252dd105556a88cc7029dbbf0ce
소대훈 <theogns0681@naver.com>
2022-11-22 09:26:01 +0900

    [refactor] enum 추가

--- src/main/java/bridge/BridgeGame.java
+++ src/main/java/bridge/BridgeGame.java
@@ -22,18 +22,21 @@ public class BridgeGame {
      * 재시작을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
     public boolean retry(String input) {
-        return Game.checkRetry(input);
+        return Game.valueOf(input).check();
     }
 }
+
 enum Game {
     R(true),
     Q(false);
 
     private boolean check;
+
     Game(boolean b) {
         check = b;
     }
-    static boolean checkRetry(String input){
-        return Game.valueOf(input).check;
+
+    boolean check() {
+        return check;
     }
 }
