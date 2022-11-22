21a324ad9534af47e0eaa789e54f8b1b6ab2f6d7
es <mascoeskim@gmail.com>
2022-11-22 17:59:58 +0900

    test(OutputViewTest): OutputViewTest 구현

---
+++ src/test/java/bridge/OutputViewTest.java
@@ -0,0 +1,26 @@
+package bridge;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.assertj.core.util.Lists.*;
+
+import java.util.ArrayList;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class OutputViewTest {
+	OutputView test = new OutputView();
+
+	@DisplayName("입력한 현재좌표 ArrayList 에 대해 단계적으로 bridgeShape 모양이 출력되는지")
+	@Test
+	void verifyBridgeShape() {
+		ArrayList<String> currentCoordinate = newArrayList("U","D","U");
+		test.printMap(0,currentCoordinate);
+		assertThat(test.mapU).isEqualTo("[ O ");
+		test.printMap(1,currentCoordinate);
+		assertThat(test.mapU).isEqualTo("[ O |   ");
+		test.printMap(2,currentCoordinate);
+		assertThat(test.mapU).isEqualTo("[ O |   | O ");
+
+	}
+}
