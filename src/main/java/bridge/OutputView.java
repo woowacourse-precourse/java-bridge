fb81edb722a56180e0e02cf25464a505596e0ea8
gynhyunkim <gyeonghyun.kimm@gmail.com>
2022-11-22 14:03:20 +0900

    feat: 게임시작 메세지 출력

--- src/main/java/bridge/OutputView.java
+++ src/main/java/bridge/OutputView.java
@@ -5,6 +5,11 @@ package bridge;
  */
 public class OutputView {
 
+    private final String START_MESSAGE = "다리 건너기 게임을 시작합니다.";
+
+    public void printStartMsg() {
+        System.out.println(START_MESSAGE);
+    }
     /**
      * 현재까지 이동한 다리의 상태를 정해진 형식에 맞춰 출력한다.
      * <p>
