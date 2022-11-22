ae65166172d90439e0a7084362d241938826938b
Dabeen Jeong <hello70825@gmail.com>
2022-11-22 16:28:59 +0900

    feat: 게임을 계속 진행할 수 있는지 확인하는 기능 추가

--- src/main/java/bridge/domain/BridgePrintMaker.java
+++ src/main/java/bridge/domain/BridgePrintMaker.java
@@ -6,18 +6,34 @@ import java.util.StringJoiner;
 public class BridgePrintMaker {
     StringJoiner upStringJoiner;
     StringJoiner downStringJoiner;
+    boolean continueGame;
 
     public BridgePrintMaker() {
         this.upStringJoiner = new StringJoiner(" | ", "[ ", " ]");
         this.downStringJoiner = new StringJoiner(" | ", "[ ", " ]");
+        this.continueGame = true;
+    }
+
+    public boolean isContinueGame() {
+        return continueGame;
     }
 
     public void addStepBridge(String userSelectMove, Direction stepDirection) {
-        upStringJoiner.add(Move.getFilterBridgeMove(userSelectMove, Direction.UP, stepDirection));
-        downStringJoiner.add(Move.getFilterBridgeMove(userSelectMove, Direction.DOWN, stepDirection));
+        String upMoveResult = Move.getFilterBridgeMove(userSelectMove, Direction.UP, stepDirection);
+        String downMoveResult = Move.getFilterBridgeMove(userSelectMove, Direction.DOWN, stepDirection);
+
+        upStringJoiner.add(upMoveResult);
+        downStringJoiner.add(downMoveResult);
+        updateContinueGame(upMoveResult, downMoveResult);
     }
 
     public String getBridgePrint() {
         return upStringJoiner.toString() + "\n" + downStringJoiner.toString();
+    }
+
+    private void updateContinueGame(String upMoveResult, String downMoveResult) {
+        if (upMoveResult.equals("X") || downMoveResult.equals("X")) {
+            continueGame = false;
+        }
     }
 }
