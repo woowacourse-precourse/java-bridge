805ea152b8b5464834db349ac5497d403255fa05
devMuscle <hongsbien@naver.com>
2022-11-22 17:12:44 +0900

    feat: 게임 진행상황을 관리하는 Class

--- src/main/java/bridge/GameProgress.java
+++ src/main/java/bridge/GameProgress.java
@@ -1,2 +1,28 @@
-package bridge;public class GameProgress {
+package bridge;
+
+import bridge.enums.GameCommand;
+import bridge.enums.GameOver;
+import bridge.enums.GameResult;
+
+public class GameProgress {
+    GameOver gameOver = GameOver.YET;
+    GameResult gameResult = GameResult.UNDETERMINED;
+    GameCommand gameCommand;
+    int trial = 1;
+
+    public GameOver getGameOver() {
+        return gameOver;
+    }
+
+    public GameCommand getGameCommand() {
+        return gameCommand;
+    }
+
+    public GameResult getGameResult() {
+        return gameResult;
+    }
+
+    public int getTrial() {
+        return trial;
+    }
 }
