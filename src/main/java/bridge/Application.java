a583166a98f50d80db6de640c4c577bba9d132d2
sschan99 <seungchan1219@gmail.com>
2022-11-22 16:24:14 +0900

    feat(BridgeGame): methodize the game's try and retry according to the command

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -8,14 +8,28 @@ public class Application {
     public static void main(String[] args) {
         System.out.println("다리 건너기 게임을 시작합니다.\n");
         BridgeGame bridgeGame = new BridgeGame(bridgeMaker.makeBridge(inputView.readBridgeSize()));
-
-        while (bridgeGame.move(inputView.readMoving())) {
-            outputView.printMap(bridgeGame.getBridge(), bridgeGame.getUserMove());
-            if (bridgeGame.isClear()) {
-                break;
-            }
+        while (tryOnce(bridgeGame)) {
+            bridgeGame.retry();
         }
         System.out.println("최종 게임 결과");
         outputView.printMap(bridgeGame.getBridge(), bridgeGame.getUserMove());
+        outputView.printResult(bridgeGame.isClear(), bridgeGame.getTryNumber());
+    }
+
+    /**
+     * 게임을 1회 시도하는 메서드
+     *
+     * @return 게임을 재시작 해야하는지 여부. 건너다 실패하고 R을 입력한 경우 true를 반환한다.
+     * 게임을 클리어하거나, 건너다 실패하고 Q를 입력한 경우 false를 반환한다.
+     */
+    public static boolean tryOnce(BridgeGame bridgeGame) {
+        while (!bridgeGame.isClear()) {
+            boolean lastMoveSuccess = bridgeGame.move(inputView.readMoving());
+            outputView.printMap(bridgeGame.getBridge(), bridgeGame.getUserMove());
+            if (!lastMoveSuccess) {
+                return inputView.readGameCommand().equals("R");
+            }
+        }
+        return false;
     }
 }
