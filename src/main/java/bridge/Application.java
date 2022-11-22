d17b2e6c8e62a2eb6dfb6ba99e4a4781c3f1dbb9
alswn <alswn9938@gmail.com>
2022-11-22 11:56:21 +0900

    style(BridgeGameController): modify 생성자로 BridgeNumberGenerator 주입받도록 변경

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -1,10 +1,13 @@
 package bridge;
 
 import bridge.controller.BridgeGameController;
+import bridge.domain.bridge.BridgeRandomNumberGenerator;
 
 public class Application {
     public static void main(String[] args) {
-        BridgeGameController bridgeGameController = new BridgeGameController();
+        BridgeGameController bridgeGameController
+                = new BridgeGameController(new BridgeRandomNumberGenerator());
+
         try {
             bridgeGameController.start();
         } catch (IllegalArgumentException e) {
