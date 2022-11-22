b920a8b141833519a31b29351b27b98a5ccac28b
이건회 <rg970604@naver.com>
2022-11-22 19:43:16 +0900

    refactor(Application.java) : 프로그램 실행 클래스 변경

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -1,11 +1,11 @@
 package bridge;
 
-import bridge.play.ProgramStarter;
+import bridge.service.GameSequence;
 
 public class Application {
 
     public static void main(String[] args) {
-        ProgramStarter.startProgram();
+        GameSequence.play();
     }
 
 }
