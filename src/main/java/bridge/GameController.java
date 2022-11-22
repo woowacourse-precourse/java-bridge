c9fcbbb0d42e5829c90302cba107e33f34705103
naaaaag <115922748+naaaaag@users.noreply.github.com>
2022-11-22 14:57:57 +0900

    <feat>GameController : 메서드 추가 & 삭제
    makeBridgeOfSize 메서드가 GameController 클래스와 관련이 없다고 생각하여  새로운 클래스로 분리했습니다. 또한 이동과 재시도 관련 기능을 구현했습니다.

--- src/main/java/bridge/GameController.java
+++ src/main/java/bridge/GameController.java
@@ -3,16 +3,30 @@ package bridge;
 import java.util.List;
 
 public class GameController {
-    public List<String> makeBridgeOfSize() {
-        InputView inputView = new InputView();
-        int size = inputView.readBridgeSize();
-        BridgeRandomNumberGenerator bridgeRanNumberGenerator = new BridgeRandomNumberGenerator();
-        BridgeMaker bridgeMaker = new BridgeMaker(bridgeRanNumberGenerator);
-
-        return bridgeMaker.makeBridge(size);
+    BridgeGame bridgeGame;
+    OutputView outputView = new OutputView();
+    boolean doesPlayerWantRetrial = true;
+    public GameController(BridgeGame bridgeGame) {
+        this.bridgeGame = bridgeGame;
     }
 
-    public
+    public void executeMoving() {
+        InputView inputView = new InputView();
+        String playerMoving = inputView.readMoving();
+        boolean doesPlayerSucceedMoving = bridgeGame.checkSuccessOfMoving(playerMoving);
 
+        outputView.printMap(bridgeGame.getBridge(), bridgeGame.getPlayerLocation(), doesPlayerSucceedMoving);
+        bridgeGame.move(playerMoving);
+        executeRetrial(doesPlayerSucceedMoving);
+    }
 
+    public void executeRetrial(boolean doesPlayerSucceedMoving) {
+        if (!doesPlayerSucceedMoving) {
+            InputView inputView = new InputView();
+            String gameCommand = inputView.readGameCommand();
+
+            bridgeGame.retry(gameCommand);
+            this.doesPlayerWantRetrial = bridgeGame.checkPlayerWantsRetrial(gameCommand);
+        }
+    }
 }
