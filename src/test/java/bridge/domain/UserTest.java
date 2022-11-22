62f14a14e9a2683e6ab6d139b8069fddf7848cf1
HamSeungChan <head0618@naver.com>
2022-11-22 22:48:53 +0900

    Test : Map 클래스 테스트 구현

--- src/test/java/bridge/domain/UserTest.java
+++ src/test/java/bridge/domain/UserTest.java
@@ -8,8 +8,6 @@ import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
-import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
-
 public class UserTest {
 
     private User user;
