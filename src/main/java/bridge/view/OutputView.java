188dfcca1765f4808457ed196391986b5f88716f
hyejuc <chjjudy@naver.com>
2022-11-22 18:46:01 +0900

    feat: 게임 종료 문구 출력 기능 추가

--- src/main/java/bridge/view/OutputView.java
+++ src/main/java/bridge/view/OutputView.java
@@ -51,5 +51,12 @@ public class OutputView {
      * <p>
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void printResult() {}
+    public void printResult(List<String> upBridgeResult, List<String> downBridgeResult, String gameStatus, int cnt) {
+        System.out.println("최종 게임 결과");
+        printMap(upBridgeResult);
+        printMap(downBridgeResult);
+        System.out.println();
+        System.out.println("게임 성공 여부: " + gameStatus);
+        System.out.println("총 시도한 횟수: " + cnt);
+    }
 }
