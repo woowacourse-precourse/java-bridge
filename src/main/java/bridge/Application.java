526f9c51dd9507ed2d719bfb2e728e6c73fd3d3b
SuhHyeonjun <dmsgktn0208@gmail.com>
2022-11-22 13:26:59 +0900

    refactor : 들여쓰기 수정, 인코딩 오류 수정

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -5,6 +5,6 @@ import bridge.controller.BridgeGameController;
 public class Application {
     public static void main(String[] args) {
         BridgeGameController bridgeGameController = new BridgeGameController();
-            bridgeGameController.start();
+        bridgeGameController.start();
     }
 }
