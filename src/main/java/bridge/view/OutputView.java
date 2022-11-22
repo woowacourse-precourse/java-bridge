3792670b24d74e9a5ed35f838dd17e0fbe5e26c5
fakerdeft <whakswp123@naver.com>
2022-11-22 23:42:24 +0900

    feat: 게임 종료 메세지 추가

--- src/main/java/bridge/view/OutputView.java
+++ src/main/java/bridge/view/OutputView.java
@@ -76,9 +76,17 @@ public class OutputView {
      * <p>
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void printResult(List<String> result) {
+    public void printClearResult(List<String> result) {
         System.out.println(ConstantMessage.GAME_RESULT_PHRASE);
         System.out.println(result.get(0));
         System.out.println(result.get(1));
+        System.out.println("게임 성공 여부: 성공");
+    }
+
+    public void printFailResult(List<String> result) {
+        System.out.println(ConstantMessage.GAME_RESULT_PHRASE);
+        System.out.println(result.get(0));
+        System.out.println(result.get(1));
+        System.out.println("게임 성공 여부: 실패");
     }
 }
