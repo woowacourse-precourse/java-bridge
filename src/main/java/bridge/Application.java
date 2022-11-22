bd7664da2f974edff9ea3bc2d9bdd22f3d4d3ad6
kim0914 <kosa0914@naver.com>
2022-11-22 15:57:33 +0900

    refactor(Application): main 함수 try - catch 구조로 변경

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -1,12 +1,17 @@
 package bridge;
 
 import bridge.controller.BridgeGameController;
+import bridge.util.Log;
 
 public class Application {
 
     public static void main(String[] args) {
         // TODO: 프로그램 구현
-        BridgeGameController gameController = new BridgeGameController();
-        gameController.run();
+        try{
+            BridgeGameController gameController = new BridgeGameController();
+            gameController.run();
+        } catch (IllegalArgumentException e) {
+            Log.error(e.getMessage());
+        }
     }
 }
