36ffe22eabad8e84284caac59e071b55fd6d73b6
devMuscle <hongsbien@naver.com>
2022-11-22 17:16:15 +0900

    refactor: 생성자 접근제한자 제거, UNDETERMINED 추가

--- src/main/java/bridge/enums/GameResult.java
+++ src/main/java/bridge/enums/GameResult.java
@@ -2,11 +2,12 @@ package bridge.enums;
 
 public enum GameResult {
     SUCCESS("성공"),
-    FAILED("실패");
+    FAILED("실패"),
+    UNDETERMINED("아직 결정되지 않음");
 
     private final String comment;
 
-    private GameResult(String comment) {
+    GameResult(String comment) {
         this.comment = comment;
     }
 
