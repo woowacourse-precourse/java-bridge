088ebb163078698916218df6b1c31ecf8d55a6bc
Song kyeong seok <skj0922@kookmin.ac.kr>
2022-11-22 19:56:17 +0900

    chore(InputView): 개행 문자 추가

--- src/main/java/bridge/view/InputView.java
+++ src/main/java/bridge/view/InputView.java
@@ -9,7 +9,7 @@ public class InputView {
 
     private static final String INPUT_BRIDGE_LENGTH = "다리의 길이를 입력해주세요";
     private static final String INPUT_BRIDGE_TYPE = "이동할 칸을 선택해주세요. (위: U, 아래: D)";
-    private static final String INPUT_RETRY_MESSAGE = "게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)";
+    private static final String INPUT_RETRY_MESSAGE = "\n게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)";
 
     /**
      * 다리의 길이를 입력받는다.
