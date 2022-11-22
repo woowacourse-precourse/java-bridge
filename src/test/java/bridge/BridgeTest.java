556c348ca1a56e13f9d2fc396b622edac6505d6d
이지영 <wldudwldhd@naver.com>
2022-11-22 21:13:06 +0900

    test: 다리생성-숫자범위 확인테스트 추가

--- src/test/java/bridge/BridgeTest.java
+++ src/test/java/bridge/BridgeTest.java
@@ -1,5 +1,6 @@
 package bridge;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -9,13 +10,28 @@ import java.util.List;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class BridgeTest {
+    InputView inputView = new InputView();
+    OutputView outputView = new OutputView();
+    BridgeMaker bridgeMaker = new BridgeMaker();
+    BridgeNumberGenerator bridgeNumberGenerator = new BridgeNumberGenerator();
 
-    @DisplayName("입력받은 다리 길이만큼 다리 생성")
+
+    @DisplayName("입력받은 다리 길이만큼 다리 생성했는지 확인 ")
     @Test
     public void makeBridgeTest() {
         BridgeRandomNumberGenerator bridgeRandomNumberGenerator = new BridgeRandomNumberGenerator();
         BridgeMaker bridgeMaker = new BridgeMaker(bridgeRandomNumberGenerator);
        assertEquals(bridgeMaker.makeBridge(3).size(), 3);
     }
+
+    @DisplayName("허용된 범위 안에서 입력했는지 확인")
+    @Test
+    public void IsInRange(){
+        int overNum = 30;
+        Assertions.assertThrows(IllegalArgumentException.class, ()->{
+            inputView.checkRange(overNum);
+        });
+    }
+
 
 }
