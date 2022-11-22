d84d7fd8e0197a9ad97723f8ff12bd184501c34f
SeokHwan An <asok369258@naver.com>
2022-11-22 19:48:06 +0900

    test : BridgeState 내 메소드 접근제어자 변경시 테스트 코드 수정

--- src/test/java/bridge/domain/BridgeStateTest.java
+++ src/test/java/bridge/domain/BridgeStateTest.java
@@ -2,6 +2,9 @@ package bridge.domain;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
@@ -14,34 +17,60 @@ class BridgeStateTest {
 
     @DisplayName("첫 라운드에 맞춘 경우에는 첫번째 칸이 O가 된다.")
     @ParameterizedTest
-    @CsvSource(value = {"U:[[   ], [ O ]]", "D:[[ O ], [   ]]"}, delimiter = ':')
-    void correctFirstRound(String userInput, String output) {
-        result.convertCorrectResultFirstRound(userInput);
-        assertThat(result.getMap().toString()).isEqualTo(output);
+    @CsvSource(value = {"U:U D U:[[   ], [ O ]]", "D:D U D:[[ O ], [   ]]"}, delimiter = ':')
+    void correctFirstRound(String userInput, String map, String output) {
+        // given
+        List<String> bridge = Arrays.asList(map.split(" "));
+
+        // when
+        List<String> answer = result.convertResult(userInput, bridge);
+
+        // then
+        assertThat(answer.toString()).isEqualTo(output);
     }
 
     @DisplayName("첫 라운드에 틀린 경우에는 첫번째 칸이 X가 된다.")
     @ParameterizedTest
-    @CsvSource(value = {"U:[[   ], [ X ]]", "D:[[ X ], [   ]]"}, delimiter = ':')
-    void wrongFirstRound(String userInput, String output) {
-        result.convertWrongResultFirstRound(userInput);
-        assertThat(result.getMap().toString()).isEqualTo(output);
+    @CsvSource(value = {"U:D U D:[[   ], [ X ]]", "D:U D U:[[ X ], [   ]]"}, delimiter = ':')
+    void wrongFirstRound(String userInput, String map, String output) {
+        // given
+        List<String> bridge = Arrays.asList(map.split(" "));
+
+        // when
+        List<String> answer = result.convertResult(userInput, bridge);
+
+        // then
+        assertThat(answer.toString()).isEqualTo(output);
     }
 
     @DisplayName("첫 라운드 이후에 맞춘 경우에는 다음칸이 O가 된다.")
     @ParameterizedTest
-    @CsvSource(value = {"U:[[ |   ], [ | O ]]", "D:[[ | O ], [ |   ]]"}, delimiter = ':')
-    void correctOtherRound(String userInput, String output) {
-        result.convertCorrectResultAfterFirstRound(userInput);
-        assertThat(result.getMap().toString()).isEqualTo(output);
+    @CsvSource(value = {"U:D U D:[[ |   ], [ | O ]]", "D:U D U:[[ | O ], [ |   ]]"}, delimiter = ':')
+    void correctOtherRound(String userInput, String map, String output) {
+        // given
+        List<String> bridge = Arrays.asList(map.split(" "));
+        round.plusRound();
+
+        // when
+        List<String> answer = result.convertResult(userInput, bridge);
+
+        // then
+        assertThat(answer.toString()).isEqualTo(output);
     }
 
     @DisplayName("첫 라운드 이후에 틀린 경우에는 다음칸이 X가 된다.")
     @ParameterizedTest
-    @CsvSource(value = {"U:[[ |   ], [ | X ]]", "D:[[ | X ], [ |   ]]"}, delimiter = ':')
-    void wrongOtherRound(String userInput, String output) {
-        result.convertWrongResultAfterFirstRound(userInput);
-        assertThat(result.getMap().toString()).isEqualTo(output);
+    @CsvSource(value = {"U:U D U:[[ |   ], [ | X ]]", "D:D U D:[[ | X ], [ |   ]]"}, delimiter = ':')
+    void wrongOtherRound(String userInput, String map, String output) {
+        // given
+        List<String> bridge = Arrays.asList(map.split(" "));
+        round.plusRound();
+
+        // when
+        List<String> answer = result.convertResult(userInput, bridge);
+
+        // then
+        assertThat(answer.toString()).isEqualTo(output);
     }
 
     @DisplayName("맵을 초기화 하면 맵은 [[ ], [ ]]이 된다,")
@@ -49,7 +78,8 @@ class BridgeStateTest {
     void initMap() {
         // given
         String userInput = "U";
-        result.convertCorrectResultAfterFirstRound(userInput);
+        List<String> bridge = new ArrayList<>(List.of("U", "D", "U"));
+        result.convertResult(userInput, bridge);
 
         // when
         result.init();
