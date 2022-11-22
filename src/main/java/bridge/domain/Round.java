ba7fbc43f5184599bbbf3006f9e29de1c8061eca
wnsgml7267 <dlwns7267@naver.com>
2022-11-22 22:37:57 +0900

    feat(domain/Round):  첫 라운드인지 판별하는 기능 구현

--- src/main/java/bridge/domain/Round.java
+++ src/main/java/bridge/domain/Round.java
@@ -1,4 +1,17 @@
 package bridge.domain;
 
+import static bridge.util.Constant.*;
+
 public class Round {
+
+    private int round;
+
+    public Round() {
+        this.round = 0;
+    }
+
+    // 첫 라운드인지 판별하는 기능
+    public boolean checkRoundFirst() {
+        return round == FIRST_ROUND;
+    }
 }
