c4ff5610d5fcd8c4292b167a557f59f958c32ca2
SeongbinPark <binni0515@gmail.com>
2022-11-22 19:03:07 +0900

    feat(OutputView): add printResult method

--- src/main/java/bridge/view/OutputView.java
+++ src/main/java/bridge/view/OutputView.java
@@ -3,6 +3,7 @@ package bridge.view;
 import static bridge.constant.message.InputMessage.ENTER_LENGTH_OF_BRIDGE;
 import static bridge.constant.message.InputMessage.RETRY_OR_QUIT;
 import static bridge.constant.message.InputMessage.SELECT_WHERE_TO_MOVE;
+import static bridge.constant.message.OutputMessage.RESULT_OF_GAME;
 import static bridge.constant.message.OutputMessage.START_OF_GAME;
 
 import bridge.Map;
@@ -31,5 +32,10 @@ public class OutputView {
 
     public void printGameCommandInput() {
         System.out.println(RETRY_OR_QUIT.getMessage());
+    }
+
+    public void printResult(Map map) {
+        System.out.println(RESULT_OF_GAME.getMessage());
+        map.print();
     }
 }
