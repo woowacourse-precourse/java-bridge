c034b53a7e06125e86aa8c1a8f25f92de95b5fb4
chaeyeong222 <iky1505@naver.com>
2022-11-22 18:54:33 +0900

    test : inputView 입력값 테스트

---
+++ src/test/java/bridge/view/InputViewTest.java
@@ -0,0 +1,32 @@
+package bridge.view;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.*;
+
+class InputViewTest {
+    private static InputView inputView;
+
+    @BeforeAll
+    static void beforeAll() {
+        inputView = new InputView();
+    }
+
+    @DisplayName("다리길이 사용자 입력값 예외 테스트")
+    @ParameterizedTest
+    @ValueSource(strings = {"2", "-1", "21", "hihi", "안녕"})
+    void readBridgeSizeExceptionTest(String input) {
+        InputStream inputStream = new ByteArrayInputStream(input.getBytes());
+        System.setIn(inputStream);
+        assertThatThrownBy(() -> inputView.readBridgeSize())
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+}
