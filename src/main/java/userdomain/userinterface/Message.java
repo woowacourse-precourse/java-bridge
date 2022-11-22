ae850de207287d52133dc5797b5e9294ac0847a1
masiljangajji <hjk172262@gmail.com>
2022-11-22 22:33:23 +0900

    refactor: 빠진 괄호 추가

--- src/main/java/userdomain/userinterface/Message.java
+++ src/main/java/userdomain/userinterface/Message.java
@@ -6,7 +6,7 @@ public enum Message {
     BRIDGE_SIZE("다리의 길이를 입력해주세요."),
     BRIDGE_MOVING("다리의 길이를 입력해주세요."),
     SELECT_MOVING("이동할 칸을 선택해주세요. (위: U, 아래: D)"),
-    RE_START("게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q"),
+    RE_START("게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)"),
     GAME_RESULT("최종 게임 결과"),
     GAME_SUCCESS("게임 성공 여부:"),
     TOTAL_TRY("총 시도한 횟수: "),
