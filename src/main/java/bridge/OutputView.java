f3f4ed550ddebacdd19f4b3ee9281eeebe86c08e
junyong <jymoon1121@gmail.com>
2022-11-22 21:09:58 +0900

    refactor: 사용하지 않는 인스턴스 변수 제거

--- src/main/java/bridge/OutputView.java
+++ src/main/java/bridge/OutputView.java
@@ -6,10 +6,7 @@ import java.util.List;
  * 사용자에게 게임 진행 상황과 결과를 출력하는 역할을 한다.
  */
 public class OutputView {
-
-    private final String SUCCESS = "O";
-    private final String UP = "U";
-    private final String DOWN = "D";
+    
 
     /**
      * 현재까지 이동한 다리의 상태를 정해진 형식에 맞춰 출력한다.
