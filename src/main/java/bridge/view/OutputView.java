93378dee9373e6043affc7dbe32aa4ee2f380e22
eun-byeol <62530884+eun-byeol@users.noreply.github.com>
2022-11-22 19:21:13 +0900

    feat(OutputView): 최종 결과 출력 시 다리 결과 출력 추가

--- src/main/java/bridge/view/OutputView.java
+++ src/main/java/bridge/view/OutputView.java
@@ -9,19 +9,27 @@ import static bridge.utils.ViewMessage.*;
 public class OutputView {
 
     public void printMap(GameResult gameResult) {
-        System.out.println(OUTPUT_FINAL_RESULT);
         printCurrentBridgeMap(gameResult.getUpSideResult());
         printCurrentBridgeMap(gameResult.getDownSideResult());
-        System.out.println();
     }
 
-    public void printCurrentBridgeMap(List<String> bridgeResult) {
+    private void printCurrentBridgeMap(List<String> bridgeResult) {
         String result = String.join(SEPARATOR_OF_BRIDGE, bridgeResult);
         System.out.println(START_OF_BRIDGE + result + END_OF_BRIDGE);
     }
 
-    public void printResult(boolean isSuccess, int numberOfAttempts) {
-        System.out.printf(OUTPUT_GAME_SUCCESS_OR_NOT, isSuccess);
+    public void printResult(GameResult gameResult, boolean isSuccess, int numberOfAttempts) {
+        System.out.println(OUTPUT_FINAL_RESULT);
+        printMap(gameResult);
+        printGameSuccessOrNot(isSuccess);
         System.out.printf(OUTPUT_NUMBER_OF_ATTEMPTS, numberOfAttempts);
+    }
+
+    private void printGameSuccessOrNot(boolean isSuccess) {
+        if (isSuccess) {
+            System.out.println(OUTPUT_GAME_SUCCESS);
+            return;
+        }
+        System.out.println(OUTPUT_GAME_FAIL);
     }
 }
