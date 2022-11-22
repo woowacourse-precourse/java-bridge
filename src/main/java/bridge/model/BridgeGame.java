685f0f6ec7bca45ffcd9778835b1daabd809ec81
yoonjuho <dbswn990@naver.com>
2022-11-22 20:13:46 +0900

    refactor(BridgeGame): 변수 명 변경

--- src/main/java/bridge/model/BridgeGame.java
+++ src/main/java/bridge/model/BridgeGame.java
@@ -32,8 +32,8 @@ public class BridgeGame {
         return buffer;
     }
 
-    public Column move(int step, String input) {
-        Column inputColumn = Column.valueOfCapitalLetter(input);
+    public Column move(int step, String position) {
+        Column inputColumn = Column.valueOfCapitalLetter(position);
         if (bridge.get(step).equals(inputColumn)) {
             return inputColumn;
         }
@@ -46,8 +46,8 @@ public class BridgeGame {
      * 재시작을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
     public void retry() {
-        buffer.clear();
         totalCount++;
+        buffer.clear();
         buffer.initialize();
     }
 
