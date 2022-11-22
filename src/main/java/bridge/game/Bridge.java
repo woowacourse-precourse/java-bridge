d9bca439087f70e5d16616b5db210fe56f2ea6f5
Insuk Lee <66675919+insukL@users.noreply.github.com>
2022-11-22 19:05:58 +0900

    feat: add game move
    - 다리 게임 이동 메소드 추가
    - 이동 관련 입력 Validation 추가
    - 다리 객체 메소드 추가

--- src/main/java/bridge/game/Bridge.java
+++ src/main/java/bridge/game/Bridge.java
@@ -5,15 +5,32 @@ import java.util.List;
 public class Bridge {
 
     private final List<String> bridge;
-    private final Integer distance;
+    private Integer distance;
 
     public Bridge(List<String> bridge) {
         this.bridge = bridge;
         this.distance = 0;
     }
 
+    public List<String> getBridge() {
+        return bridge;
+    }
+
     public Integer getDistance() {
         return distance;
+    }
+
+    public String getBlock(Integer index) {
+        return bridge.get(index);
+    }
+
+    public Boolean pass(String choice) {
+        if (getBlock(distance).equals(choice)) {
+            distance += 1;
+            return true;
+        }
+
+        return false;
     }
 
 }
--- src/main/java/bridge/game/BridgeGame.java
+++ src/main/java/bridge/game/BridgeGame.java
@@ -10,15 +10,19 @@ public class BridgeGame {
 
     public BridgeGame(Bridge bridge) {
         this.bridge = bridge;
-        this.count = 0;
+        this.count = 1;
     }
 
     /**
      * 사용자가 칸을 이동할 때 사용하는 메서드
-     * <p>
-     * 이동을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
+     *
+     * @param input 사용자가 입력한 입력값
      */
     public void move(String input) {
+        checkInputLength(input);
+        checkMoveInput(input);
+
+        bridge.pass(input);
     }
 
     /**
@@ -27,5 +31,17 @@ public class BridgeGame {
      * 재시작을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
     public void retry(String input) {
+    }
+
+    private void checkInputLength(String input) {
+        if (input.length() != 1) {
+            throw new IllegalArgumentException("입력은 길이 1개까지 가능합니다.");
+        }
+    }
+
+    private void checkMoveInput(String input) {
+        if (!(input.equals("U") || input.equals("D"))) {
+            throw new IllegalArgumentException("이동은 U와 D만 입력할 수 있습니다.");
+        }
     }
 }
