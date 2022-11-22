de6f1e78ff7404aafbe5e29cc4d724ac5ca89b60
bando99 <bando991225@gmail.com>
2022-11-22 23:29:27 +0900

    REFACT : 기능 구분, 메서드 길이 수정, 예외처리기능 추가

--- src/main/java/bridge/InputView.java
+++ src/main/java/bridge/InputView.java
@@ -2,45 +2,49 @@ package bridge;
 
 import camp.nextstep.edu.missionutils.Console;
 
+import static bridge.Exceptions.inputBridgeSizeException;
+
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
  */
 public class InputView {
-
-    /**
-     * 다리의 길이를 입력받는다.
-     */
-    public int readBridgeSize() {
+    private String inputBridgeSize;
+    private static int bridgesize;
+    private InputStringShape inputStringShape;
+    public InputView(){
+        inputStringShape = new StringToInt();
+    }
+    public void readBridgeSize() {
         System.out.println("다리의 길이를 입력해주세요.");
-        String inputsize = Console.readLine();
-        //예외처리(inputsize); //숫자 이외의 입력이 들어왔을 때.
-        int size = Integer.parseInt(inputsize);
-        return size;
+        inputBridgeSize = Console.readLine();
+        while(inputBridgeSizeException(inputBridgeSize)){
+            readBridgeSize();
+        }
+        bridgesize = inputStringShape.toInt(inputBridgeSize);
     }
 
-    /**
-     * 사용자가 이동할 칸을 입력받는다.
-     */
+
+    public int getSize(){
+        return bridgesize;
+    }
+
     public String readMoving() {
         System.out.println("이동할 칸을 선택해 주세요. (위: U, 아래: D)");
         String moving = Console.readLine();
-        //예외처리(moving); //U,D 이외의 입력이 들어왔을 때.
+        if(Exceptions.inputDirectionException(moving)){
+            return readMoving();
+        }
         System.out.println(moving);
         return moving;
     }
-    public String readRetry(){
-        System.out.println("게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)");
-        String retry = Console.readLine();
-        System.out.println(retry);
-        return retry;
-    }
-    /**
-     * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
-     */
+
     public String readGameCommand() {
         System.out.println("게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)");
         String answer = Console.readLine();
-
+        if(Exceptions.inputReplayException(answer)){
+            return readGameCommand();
+        }
+        System.out.println(answer);
         return answer;
     }
 }
