6c6476a5a3ee78d83b00e0e61bfc91691053a025
hwiyeon0831 <aab555586@gmail.com>
2022-11-22 20:47:24 +0900

    feat(GameResultCode) : 게임 결과에 대한 코드 값 정의

---
+++ src/main/java/bridge/game/GameResultCode.java
@@ -0,0 +1,16 @@
+package bridge.game;
+
+public enum GameResultCode {
+	SUCCESS("성공"),
+	FAIL("실패");
+
+	private final String resultMessage;
+
+	GameResultCode(String resultMessage) {
+		this.resultMessage = resultMessage;
+	}
+
+	public String getResultMessage() {
+		return resultMessage;
+	}
+}
