d572841f2ef6903a19384bbae1b73f44b716f5a5
YongNyeo <won05121@naver.com>
2022-11-22 14:20:04 +0900

    feat: 현재 입력받는 다리를 맵으로 보여주는 기능 추가

--- src/main/java/bridge/OutputView.java
+++ src/main/java/bridge/OutputView.java
@@ -1,5 +1,7 @@
 package bridge;
 
+import java.util.List;
+
 /**
  * 사용자에게 게임 진행 상황과 결과를 출력하는 역할을 한다.
  */
@@ -10,7 +12,17 @@ public class OutputView {
      * <p>
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void printMap() {
+    public void printMap(List<String> Prog1, List<String> Prog2) {
+        System.out.print("["+Prog1.get(0));
+        for (int i = 1; i < Prog1.size(); i++) {
+            System.out.print("|"+Prog1.get(i));
+        }
+        System.out.println("]");
+        System.out.print("["+Prog2.get(0));
+        for (int i = 1; i < Prog2.size(); i++) {
+            System.out.print("|"+Prog2.get(i));
+        }
+        System.out.println("]");
     }
 
     /**
