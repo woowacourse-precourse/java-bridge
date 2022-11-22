cda477fa79053ad0399cc84ce25c142da5f3c55f
gmu1026 <gmu1026@gmail.com>
2022-11-22 21:43:00 +0900

    typo: 게임 완료 메시지 오타 수정

--- src/main/java/bridge/view/OutputView.java
+++ src/main/java/bridge/view/OutputView.java
@@ -11,7 +11,7 @@ public class OutputView {
     private static final String BRIDGE_START = "[ ";
     private static final String BRIDGE_END = " ]";
     private static final String BRIDGE_SEPARATOR = " | ";
-    private static final String GAME_STATUS_MESSAGE = "게임 생성 여부: %s";
+    private static final String GAME_STATUS_MESSAGE = "게임 성공 여부: %s";
     private static final String TOTAL_TRY_COUNT_MESSAGE = "총 시도한 횟수: %s";
 
     /**
