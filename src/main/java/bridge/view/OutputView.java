6529430e0fb86a124b7318ebbcb72eb8f576590c
wonju-dev <wonjulee.dev@gmail.com>
2022-11-22 16:34:37 +0900

    refactor(OutputView): MessageGenerator적용

--- src/main/java/bridge/view/OutputView.java
+++ src/main/java/bridge/view/OutputView.java
@@ -1,17 +1,18 @@
 package bridge.view;
 
 import bridge.BridgeGame;
-import bridge.Guess;
-import bridge.enums.Direction;
+import bridge.MessageGenerator;
 
-import java.util.ArrayList;
 import java.util.List;
+
+import static bridge.MessageGenerator.*;
 
 /**
  * 사용자에게 게임 진행 상황과 결과를 출력하는 역할을 한다.
  */
 public class OutputView {
     private static final OutputView instance = new OutputView();
+    private static final MessageGenerator messageGenerator = MessageGenerator.getInstance();
 
     private OutputView() {
     }
@@ -29,44 +30,17 @@ public class OutputView {
      * <p>
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    // 변수명 변경 필요
     public void printMap(BridgeGame bridgeGame) {
-        List<Guess> guesses = bridgeGame.getGuess();
-        // "[ O |   | O ]",
-        // "[   | O |   ]",
-        List<List<String>> parsedResult = getParsedResult(guesses);
+        List<String> mapImage = messageGenerator.getMapImage(bridgeGame);
 
-        for (int i = parsedResult.size() - 1; i > -1; i--) {
-            System.out.println("[" + String.join("|", parsedResult.get(i)) + "]");
+        printBackward(mapImage);
+    }
+
+    private void printBackward(List<String> mapImage) {
+        for (int i = mapImage.size() - 1; i > -1; i--) {
+            System.out.println(mapImage.get(i));
         }
         System.out.println();
-    }
-
-    private List<List<String>> getParsedResult(List<Guess> guesses) {
-        List<List<String>> layers = init();
-
-        for (Guess guess : guesses) {
-            Integer code = Direction.getCodeByAbbr(guess.getGuess());
-            for (int i = 0; i < Direction.values().length; i++) {
-                if (i == code) {
-                    layers.get(i).add(" " + guess.getResult() + " ");
-                } else {
-                    layers.get(i).add("   ");
-                }
-            }
-        }
-
-        return layers;
-    }
-
-    private List<List<String>> init() {
-        List<List<String>> layers = new ArrayList<>();
-
-        for (int i = 0; i < Direction.values().length; i++) {
-            layers.add(new ArrayList<>());
-        }
-
-        return layers;
     }
 
     /**
@@ -75,14 +49,13 @@ public class OutputView {
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
     public void printResult(BridgeGame bridgeGame) {
-        System.out.println("최종 게임 결과");
+        System.out.println(GAME_FINISH_HEADER_MSG);
         printMap(bridgeGame);
-        System.out.println();
         if (bridgeGame.reachOppositeSide()) {
-            System.out.println("게임 성공 여부: 성공");
+            System.out.println(GAME_FINISH_RESULT_SUCCESS_MSG);
         } else {
-            System.out.println("게임 성공 여부: 실패");
+            System.out.println(GAME_FINISH_RESULT_FAIL_MSG);
         }
-        System.out.println("총 시도한 횟수: " + bridgeGame.getTryCount());
+        System.out.println(GAME_FINISH_TOTAL_TRY_MSG + bridgeGame.getTryCount());
     }
 }
