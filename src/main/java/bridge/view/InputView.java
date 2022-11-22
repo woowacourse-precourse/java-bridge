359b5c2774489f8f2912e19900d86bca20b3de0c
username459712 <94822739+username459712@users.noreply.github.com>
2022-11-22 21:27:03 +0900

    feat: readBridgeSize()->다리의 길이를 입력받는 기능, 3부터 20까지 들어오는지 유효성 검사, 검사가 실패날 경우, ERROR_MESSAGE를 띄우고 이 메소드를 재귀

--- src/main/java/bridge/view/InputView.java
+++ src/main/java/bridge/view/InputView.java
@@ -7,4 +7,15 @@ import camp.nextstep.edu.missionutils.Console;
 
 public class InputView {
     Exception bridgeException = new Exception();
+
+    public String readBridgeSize() {
+        try {
+            String bridgeSize = Console.readLine();
+            bridgeException.checkNumberRangeException(bridgeSize);
+            return bridgeSize;
+        } catch (IllegalArgumentException e) {
+            System.out.println(ERROR_MESSAGE + "다리 길이는 3부터 20 사이의 숫자여야 합니다.");
+            return readBridgeSize();
+        }
+    }
 }
