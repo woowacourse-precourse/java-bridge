2dca675904d2d3a7d34ad2b19e435fd16b7753d0
Koonayoung518 <koony000415@gmail.com>
2022-11-22 16:48:22 +0900

    refactor(bridge): test 실행 문제점 수정

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -8,34 +8,34 @@ import view.OutputView;
 public class Application {
     private static OutputView outputView = new OutputView();
     private static InputView inputView = new InputView();
-    private static BridgeGame bridgeGame = new BridgeGame();
 
     public static void main(String[] args) {
         outputView.printStart();
+        BridgeGame bridgeGame = new BridgeGame();
         Bridge bridge = bridgeGame.setBridge(inputView.readBridgeSize());
-        play(bridge);
+        play(bridge, bridgeGame);
         outputView.printResult(bridgeGame, bridge);
     }
 
-    public static void play(Bridge bridge) {
+    public static void play(Bridge bridge, BridgeGame bridgeGame) {
         while (!bridgeGame.isFinish(bridge)) {
-            if (bridgeGame.isMoving(bridge, move(bridge))) {
+            if (bridgeGame.isMoving(bridge, move(bridge, bridgeGame))) {
                 continue;
             }
-            if (!isRestart()) {
+            if (!isRestart(bridgeGame)) {
                 break;
             }
         }
     }
 
-    private static Moving move(Bridge bridge) {
+    private static Moving move(Bridge bridge, BridgeGame bridgeGame) {
         Moving moving =  inputView.readMoving();
         bridgeGame.move(bridge, moving);
         outputView.printMap(bridgeGame.getMovedBridge());
         return moving;
     }
 
-    private static boolean isRestart() {
+    private static boolean isRestart(BridgeGame bridgeGame) {
         if (!inputView.readGameCommand().isRestart())
             return false;
         bridgeGame.retry();
--- src/test/java/bridge/ApplicationTest.java
+++ src/test/java/bridge/ApplicationTest.java
@@ -45,39 +45,38 @@ class ApplicationTest extends NsTest {
     @Test
     void 재시도_테스트() {
         assertRandomNumberInRangeTest(() -> {
-            run("3", "U", "U", "R", "U", "D", "D");
+            run("3", "U", "U", "R","U","D","U");
             assertThat(output()).contains(
-                    "게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)",
                     "최종 게임 결과",
-                    "[ O |   |   ]",
-                    "[   | O | O ]",
+                    "[ O |   | O ]",
+                    "[   | O |   ]",
                     "게임 성공 여부: 성공",
                     "총 시도한 횟수: 2"
             );
 
-            int upSideIndex = output().indexOf("[ O |   |   ]");
-            int downSideIndex = output().indexOf("[   | O | O ]");
+            int upSideIndex = output().indexOf("[ O |   | O ]");
+            int downSideIndex = output().indexOf("[   | O |   ]");
             assertThat(upSideIndex).isLessThan(downSideIndex);
-        }, 1, 0, 0);
+        }, 1, 0, 1);
     }
 
 
     @Test
     void 종료_테스트() {
         assertRandomNumberInRangeTest(() -> {
-            run("3", "U", "U", "Q");
+            run("3", "U", "D", "Q");
             assertThat(output()).contains(
                     "최종 게임 결과",
-                    "[ O | X ]",
-                    "[   |   ]",
+                    "[ O |   ]",
+                    "[   | X ]",
                     "게임 성공 여부: 실패",
                     "총 시도한 횟수: 1"
             );
 
-            int upSideIndex = output().indexOf("[ O | X ]");
-            int downSideIndex = output().indexOf("[   |   ]");
+            int upSideIndex = output().indexOf("[ O |   ]");
+            int downSideIndex = output().indexOf("[   | X ]");
             assertThat(upSideIndex).isLessThan(downSideIndex);
-        }, 1, 0, 1);
+        }, 1, 1);
     }
 
     @Test
@@ -96,7 +95,7 @@ class ApplicationTest extends NsTest {
             int upSideIndex = output().indexOf("[ O | X ]");
             int downSideIndex = output().indexOf("[   |   ]");
             assertThat(upSideIndex).isLessThan(downSideIndex);
-        }, 1, 0, 1);
+        }, 1, 0);
     }
 
     @Test
