a05ff4895799b325015542fae149204d9770bd3a
minsu <bill7666@naver.com>
2022-11-22 19:52:06 +0900

    완료

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -6,44 +6,59 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+import static org.assertj.core.util.Lists.newArrayList;
+
 public class Application {
 
     public static void main(String[] args) {
         // TODO: 프로그램 구현
-       String Top = "[]";
-       String Bottom = "[]";
-       String Correct = " O ";
-       String False = " X ";
-       String Space = "   ";
-       StringBuffer T = new StringBuffer();
-       StringBuffer B = new StringBuffer();
-       T.append(Top);
-       B.append(Bottom);
-       int answer = 1;
-       int slice = 4;
-       System.out.println("몇번하실껀가요?");
-       int count = Integer.parseInt(Console.readLine());
-       for(int i = 0;i<count;i++) {
-          System.out.println("이동하실칸 선택해주세요.(위: U, 아래: D)");
-          String choice = Console.readLine();
-          if(choice.equals("U")) {
-             T.insert(answer, Correct);
-             B.insert(answer, Space);
-          } else if (choice.equals("D")) {
-             T.insert(answer, False);
-             B.insert(answer, Space);
-          }
-          System.out.println(T);
-          System.out.println(B);
-          answer = answer +4;
-          if(i!=count-1){
-            T.insert(answer - 1, "|");
-            B.insert(answer - 1, "|");
-         }
-      }
+        System.out.println("다리 건너기 게임을 시작합니다.");
+        System.out.println("횟수입력");
+        InputView Input  =new InputView();
+        int size = Input.readBridgeSize();
+        BridgeRandomNumberGenerator random = new BridgeRandomNumberGenerator();
+        BridgeMaker maker = new BridgeMaker(random);
+        List<String> auto =  maker.makeBridge(size);
+        BridgeGame game = new BridgeGame();
+        int count = 1;
+        InputView input = new InputView();
+        BridgeGame gaming = new BridgeGame();
+        String passOrFail = null;
+        int AllCount = 1;
+        for(int i = 0;i<size;i++){
+            String choice = input.readMoving();
+            OutputView view = new OutputView();
+            List<StringBuffer> Answer =  game.move(auto.get(i),i,choice);
+            int result = view.printMap(Answer,i);
+            if(size -1 != i) {
+                count = count + 4;
+                gaming.slice(i, count, Answer.get(0), Answer.get(1));
+            }
+           if(result  == 1){
+               String Retry = game.retry();
+               if(Retry.equals("R")){
+                   game = new BridgeGame();
+                   i=-1;
+                   count = 1;
+                   AllCount = AllCount+1;
+               } else if (Retry.equals("Q")) {
+                   System.out.println("최종 게임 결과");
+                   view.printMap(Answer,i);
+                   passOrFail = "실패";
+                   view.printResult(passOrFail,AllCount);
+                   break;
 
+               }
+           }
+            if(i==size-1){
+                System.out.println("최종 게임 결과");
+                view.printMap(Answer,i);
+                passOrFail = "성공";
+                view.printResult(passOrFail,AllCount);
+                gaming.slice(i,count,Answer.get(0),Answer.get(1));
 
-
+            }
+        }
 
     }
 }
--- src/main/java/bridge/InputView.java
+++ src/main/java/bridge/InputView.java
@@ -1,5 +1,9 @@
 package bridge;
 
+import camp.nextstep.edu.missionutils.Console;
+
+import javax.sound.midi.SysexMessage;
+
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
  */
@@ -9,20 +13,46 @@ public class InputView {
      * 다리의 길이를 입력받는다.
      */
     public int readBridgeSize() {
-        return 0;
+        int size = 0;
+        try {
+            size = Integer.parseInt(Console.readLine());
+
+
+        }catch (NumberFormatException e){
+            System.out.println("[ERROR] 숫자만 입력가능 합니다.");
+        }
+        return size;
     }
 
     /**
      * 사용자가 이동할 칸을 입력받는다.
      */
     public String readMoving() {
-        return null;
+        System.out.println("이동할 칸을 선택해주세요. (위: U, 아래: D)");
+        String choice = Console.readLine();
+        ChoiceCheck(choice);
+        return choice;
     }
 
     /**
      * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
      */
     public String readGameCommand() {
-        return null;
+        System.out.println("게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)");
+        String Retry = Console.readLine();
+        RetryCheck(Retry);
+        return Retry;
+    }
+
+    private void ChoiceCheck (String choice) {
+        if (!choice.equals("U") && !choice.equals("D")) {
+            throw new IllegalArgumentException("[ERROR] U 또는 D를 입력해야 합니다.");
+        }
+    }
+
+    private void RetryCheck (String Retry) {
+        if (!Retry.equals("R") && !Retry.equals("Q")) {
+            throw new IllegalArgumentException("[ERROR] R 또는 Q를 입력해야 합니다.");
+        }
     }
 }
