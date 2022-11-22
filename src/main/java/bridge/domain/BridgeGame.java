28b8d969323eac134526fa7a16fa87d26ade179e
eom-tae-in <eti0728@daum.net>
2022-11-22 21:13:48 +0900

    feat(BridgeGame): BridgeGame 도메인 생성자 및 필드 구현

---
+++ src/main/java/bridge/domain/BridgeGame.java
@@ -0,0 +1,13 @@
+package bridge.domain;
+
+import java.util.List;
+
+public class BridgeGame {
+    private int position;
+    private final List<String> bridge;
+
+    public BridgeGame(List<String> bridge) {
+        this.position = 0;
+        this.bridge = bridge;
+    }
+}
