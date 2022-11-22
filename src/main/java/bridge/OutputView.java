f8c0af2f59cff3a778ca11e3c2e503b562b781d8
kimilchul <huj0159@gmail.com>
2022-11-22 10:56:32 +0900

    feat: add result print

--- src/main/java/bridge/OutputView.java
+++ src/main/java/bridge/OutputView.java
@@ -29,7 +29,7 @@ public class OutputView {
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
     public void printMap(BridgeMap bridgeMap) {
-        System.out.println(bridgeMap.toString());
+        System.out.println(bridgeMap.toString()+"\n");
     }
 
     /**
@@ -37,8 +37,18 @@ public class OutputView {
      * <p>
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void printResult(BridgeMap bridgeMap) {
+    public void printResult(BridgeMap bridgeMap,int tryCount,boolean gameClear) {
+
+        String result = "";
         System.out.println("최종 게임 결과");
-        System.out.println(bridgeMap.toString());
+        printMap(bridgeMap);
+        if(gameClear==true){
+            result = "성공";
+        }
+        if (gameClear==false){
+            result = "실패";
+        }
+        System.out.println("게임 성공 여부: "+result+"\n" +
+                "총 시도한 횟수: "+tryCount);
     }
 }
