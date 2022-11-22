be3a52fdddac372fd0d55cf0d67e563cccee3eb3
kathyleesh <90851865+kathyleesh@users.noreply.github.com>
2022-11-22 19:14:12 +0900

    feat(InputView) : U와 D가 아니면 예외처리 후 재시작, readGameCommand 예외 수정

--- src/main/java/bridge/InputView.java
+++ src/main/java/bridge/InputView.java
@@ -1,7 +1,9 @@
 package bridge;
+
 import camp.nextstep.edu.missionutils.Console;
 
 import java.util.NoSuchElementException;
+import java.util.Objects;
 
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
@@ -16,15 +18,15 @@ public class InputView {
      * 다리의 길이를 입력받는다.
      */
     public static int readBridgeSize() {
-        try{
+        try {
             System.out.println(ENTER_BRIDGE_SIZE);
             int bridgeSize = Integer.parseInt(Console.readLine());
-            if (bridgeSize < 3 | bridgeSize > 20){
+            if (bridgeSize < 3 | bridgeSize > 20) {
                 System.out.println("[ERROR] 다리 길이는 3이상 20이사의 숫자를 입력할 수 있습니다.");
                 return readBridgeSize();
             }
             return bridgeSize;
-        }catch (NumberFormatException e){
+        } catch (NumberFormatException e) {
             System.out.println("[ERROR] 숫자만 입력가능합니다.");
             return readBridgeSize();
         }
@@ -34,9 +36,17 @@ public class InputView {
      * 사용자가 이동할 칸을 입력받는다.
      */
     public static String readMoving() {
+
         System.out.println(ENTER_UP_OR_DOWN);
         String selectMove = Console.readLine();
+        System.out.println(selectMove);
+        if (!Objects.equals(selectMove, "U") && !Objects.equals(selectMove, "D")) {
+            System.out.println("[ERROR] U와 D만 입력 가능 합니다.");
+            return readMoving();
+        }
         return selectMove;
+
+
     }
 
     /**
@@ -44,12 +54,17 @@ public class InputView {
      */
     public static String readGameCommand() {
         System.out.println(RESTART_GAME);
-        try{
-            return Console.readLine();
-        } catch (NoSuchElementException e){
+        try {
+            String selectRestart =  Console.readLine();
+            if (!Objects.equals(selectRestart, "R") && !Objects.equals(selectRestart, "Q")){
+                System.out.println("[ERROR] R(재시작)과 Q(종료) 중 하나의 문자를 입력할 수 있습니다.");
+                return readGameCommand();
+            }
+            return selectRestart;
+        } catch (NoSuchElementException e) {
             System.out.println("[ERROR] 입력 값이 없어 종료합니다.");
             return "Q";
-        }catch (IllegalArgumentException e){
+        } catch (IllegalArgumentException e) {
             System.out.println("[ERROR] R(재시작)과 q(종료) 중 하나의 문자를 입력할 수 있습니다.");
             return readGameCommand();
         }
