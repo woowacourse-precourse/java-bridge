02916270b3ac3ae0531b13ae8a16bcfd3162f0bc
metorg <metrog950@gmail.com>
2022-11-22 15:04:43 +0900

    feat: 다리길이 예외처리 메서드 생성

--- src/main/java/bridge/view/InputView.java
+++ src/main/java/bridge/view/InputView.java
@@ -1,9 +1,11 @@
-package bridge;
+package bridge.view;
 
 import camp.nextstep.edu.missionutils.Console;
 
 import static bridge.util.message.GameMessage.*;
-import static bridge.OutputView.*;
+import static bridge.util.message.ErrorMessage.*;
+import static bridge.view.OutputView.*;
+
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
  */
@@ -14,9 +16,26 @@ public class InputView {
      */
     public int readBridgeSize() {
         showMessage(START_MESSAGE.getMessage());
-        input();
+        String bridgeSize = input();
+        exceptionBridgeSize(bridgeSize);
 
         return 0;
+    }
+
+    private void exceptionBridgeSize(String size){
+
+        //예외 범위 입력시
+        if(size.length()<3||size.length()>20){
+            throw new IllegalArgumentException(OUT_OF_RANGE.getMessage());
+        }
+
+        //숫자 이외에 문자 입력시
+        for(int i=0;i<size.length();i++){
+            if('1'>size.charAt(i)||size.charAt(i)>'9'){
+                throw new IllegalArgumentException(INPUT_NUMERIC.getMessage());
+            }
+        }
+
     }
     /**
      * 사용자가 이동할 칸을 입력받는다.
