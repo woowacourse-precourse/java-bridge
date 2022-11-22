a3879bf1121b647f317ff9e8bef6e9ef4f77a233
osm <osm0822@naver.com>
2022-11-22 13:14:27 +0900

    feat(InputView) : 입력 멘트 추가

--- src/main/java/bridge/InputView.java
+++ src/main/java/bridge/InputView.java
@@ -1,5 +1,7 @@
 package bridge;
 
+import bridge.constant.BridgeMoveCommand;
+import bridge.constant.BridgeRetryCommand;
 import bridge.constant.InputValidationError;
 import camp.nextstep.edu.missionutils.Console;
 
@@ -8,6 +10,17 @@ import camp.nextstep.edu.missionutils.Console;
  */
 public class InputView {
 
+    private static final String MENT_READ_BRIDGE_SIZE = "다리의 길이를 입력해 주세요.";
+    private static final String MENT_READ_MOVE =
+            "이동할 칸을 선택해 주세요. (위: " +
+                    BridgeMoveCommand.MOVE_UP.command +
+                    ", 아래: " +
+                    BridgeMoveCommand.MOVE_DOWN.command;
+    private static final String MENT_READ_COMMAND =
+            "게임을 다시 시도할지 여부를 입력해주세요. (재시도: " +
+                    BridgeRetryCommand.RETRY.command +
+                    ", 종료: " +
+                    BridgeRetryCommand.QUIT.command;
     private InputValidator validator;
 
     public InputView() {
@@ -18,6 +31,7 @@ public class InputView {
      * 다리의 길이를 입력받는다.
      */
     public int readBridgeSize() throws IllegalArgumentException {
+        System.out.println(MENT_READ_BRIDGE_SIZE);
         String cmd = Console.readLine();
         int parsedCmd = validator.validateBridgeSizeParsable(cmd);
         validator.validateBridgeSize(parsedCmd);
@@ -28,6 +42,7 @@ public class InputView {
      * 사용자가 이동할 칸을 입력받는다.
      */
     public String readMoving() throws IllegalArgumentException {
+        System.out.println(MENT_READ_MOVE);
         String cmd = Console.readLine();
         validator.validateMove(cmd);
         return cmd;
@@ -37,6 +52,7 @@ public class InputView {
      * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
      */
     public String readGameCommand() throws IllegalArgumentException {
+        System.out.printf(MENT_READ_COMMAND);
         String cmd = Console.readLine();
         validator.validateGameCommand(cmd);
         return cmd;
