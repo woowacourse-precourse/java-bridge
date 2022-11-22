87515006dabd297bf19ad0a62a0961261ee44f2a
DevJay1024 <devjay1024@gmail.com>
2022-11-22 11:29:24 +0900

    test: Bridge 리팩토링에 따른 테스트 코드 리팩토링

--- src/test/java/bridge/paths/BridgeTest.java
+++ src/test/java/bridge/paths/BridgeTest.java
@@ -1,7 +1,7 @@
 package bridge.paths;
 
 import bridge.identifiers.Direction;
-import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
@@ -13,50 +13,37 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 class BridgeTest {
 
     @Test
-    void 다리길이가_안전경로_길이와_다르면_예외처리한다(){
-        assertThatThrownBy(()->{
-            List<String> securePath = List.of("U", "D", "D");
-            Bridge bridge = new Bridge(securePath);
-        }).isInstanceOf(InternalError.class);
-    }
-
-    @Test
     void 안전경로가_주어지지_않으면_예외처리한다(){
         assertThatThrownBy(()->{
             Bridge bridge = new Bridge(null);
         }).isInstanceOf(InternalError.class);
     }
 
-    @Test
-    void 안전경로가_사용자_경로와_같을_때_확인(){
-        Bridge bridge = new Bridge(List.of("U", "D", "D"));
-        PlayerPath playerPath = new PlayerPath();
-        playerPath.saveDirection(Direction.UP);
-        playerPath.saveDirection(Direction.DOWN);
-        playerPath.saveDirection(Direction.DOWN);
+    @Nested
+    class CanBeSteppedByTest {
 
-        assertThat(bridge.canBeSteppedBy(playerPath)).isTrue();
+        @Test
+        void 안전경로가_사용자_경로와_같을_때_확인() {
+            Bridge bridge = new Bridge(List.of("U", "D", "D"));
+            PlayerPath playerPath = new PlayerPath();
+            playerPath.saveDirection(Direction.UP);
+            playerPath.saveDirection(Direction.DOWN);
+            playerPath.saveDirection(Direction.DOWN);
+
+            assertThat(bridge.canBeSteppedBy(playerPath)).isTrue();
+        }
+
+        @Test
+        void 안전경로가_사용자_경로와_다를_때_확인() {
+            Bridge bridge = new Bridge(List.of("U", "D", "D"));
+            PlayerPath playerPath = new PlayerPath();
+            playerPath.saveDirection(Direction.UP);
+            playerPath.saveDirection(Direction.DOWN);
+            playerPath.saveDirection(Direction.UP);
+
+            assertThat(bridge.canBeSteppedBy(playerPath)).isFalse();
+        }
     }
 
-    @Test
-    void 안전경로가_사용자_경로와_다를_때_확인(){
-        Bridge bridge = new Bridge(List.of("U", "D", "D"));
-        PlayerPath playerPath = new PlayerPath();
-        playerPath.saveDirection(Direction.UP);
-        playerPath.saveDirection(Direction.DOWN);
-        playerPath.saveDirection(Direction.UP);
 
-        assertThat(bridge.canBeSteppedBy(playerPath)).isFalse();
-    }
-
-    @Test
-    void 안전경로도_사용자_경로의_마지막_인덱스로_확인하는지_검사(){
-        Bridge bridge = new Bridge(List.of("U", "D", "D"));
-        PlayerPath playerPath = new PlayerPath();
-        playerPath.saveDirection(Direction.UP);
-        playerPath.saveDirection(Direction.UP);
-        playerPath.saveDirection(Direction.DOWN);
-
-        assertThat(bridge.canBeSteppedBy(playerPath)).isTrue();
-    }
 }
