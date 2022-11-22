6d740d829c13dbf94a018dfb23a33243759d0829
KSH <tkdgns543@gmail.com>
2022-11-22 17:28:31 +0900

    refactor(input): 재시도 확인에 대한 예외 확인 메서드를 구현한 Repetitor 자손 클래스

---
+++ src/main/java/bridge/ui/Input/GameCommand.java
@@ -0,0 +1,21 @@
+package bridge.ui.Input;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class GameCommand extends Repetitor{
+
+    private static final List<String> CORRECT_GAME_COMMAND = new ArrayList<>(Arrays.asList("R", "Q"));
+    private static final String ERROR_MESSAGE = "[ERROR] 대문자 'R' 혹은 'Q' 만 입력 가능합니다";
+
+    @Override
+    protected String checkInputException(String input) throws IllegalArgumentException {
+        boolean correctCommand = CORRECT_GAME_COMMAND.contains(input);
+        if (!correctCommand) {
+            throw new IllegalArgumentException(ERROR_MESSAGE);
+        }
+
+        return input;
+    }
+}
