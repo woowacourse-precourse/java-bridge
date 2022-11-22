1649f8bffe606d088e890a7b8b4f028290528e2e
이규승 <rpg8988@naver.com>
2022-11-22 22:48:48 +0900

    feat: 마무리 포인트 및 루프 추가

--- src/main/java/bridge/BridgeGame.java
+++ src/main/java/bridge/BridgeGame.java
@@ -1,23 +1,38 @@
 package bridge;
 
-/**
- * 다리 건너기 게임을 관리하는 클래스
- */
+import java.util.List;
+import java.util.Objects;
+
+import bridge.EnumClass.EnumStates;
+
 public class BridgeGame {
 
-    /**
-     * 사용자가 칸을 이동할 때 사용하는 메서드
-     * <p>
-     * 이동을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
-     */
-    public void move() {
+    private int index = 0;
+
+    public EnumStates move(String command, List<String> bridge) {
+        if (isFinished(command, bridge)) {
+            return EnumStates.Success;
+        }
+        return isNotFinished(command, bridge);
     }
 
-    /**
-     * 사용자가 게임을 다시 시도할 때 사용하는 메서드
-     * <p>
-     * 재시작을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
-     */
+    private boolean isFinished(String command, List<String> bridge) {
+        EnumStates.Index.setWorth(index + 1);
+        return (index == bridge.size() - 1) && (Objects.equals(bridge.get(index), command));
+    }
+
+    private EnumStates isNotFinished(String command, List<String> bridge) {
+        if (Objects.equals(bridge.get(index), command)) {
+            index++;
+            EnumStates.Index.setWorth(index);
+            return EnumStates.In_game;
+        }
+        index++;
+        return EnumStates.Retry;
+    }
+
     public void retry() {
+        index = 0;
+        EnumStates.Index.setWorth(index);
     }
 }
