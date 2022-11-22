2c0bafde4a0fef472ad4b74a836e549a63054fb9
osy9757 <osy9757@gmail.com>
2022-11-22 20:20:34 +0900

    feat : 정답 brdige 생성

--- src/main/java/bridge/BridgeMaker.java
+++ src/main/java/bridge/BridgeMaker.java
@@ -1,5 +1,6 @@
 package bridge;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -9,6 +10,10 @@ import java.util.List;
  * BridgeMaker의 메서드의 시그니처(인자, 이름)와 반환 타입은 변경할 수 없다.
  */
 public class BridgeMaker {
+
+    private final int DOWN = 0;
+    private final int Up = 1;
+
 
     private final BridgeNumberGenerator bridgeNumberGenerator;
 
@@ -21,6 +26,18 @@ public class BridgeMaker {
      * @return 입력받은 길이에 해당하는 다리 모양. 위 칸이면 "U", 아래 칸이면 "D"로 표현해야 한다.
      */
     public List<String> makeBridge(int size) {
-        return null;
+        List<String> correctBridge = new ArrayList<>();
+        for (int i = 0; i < size; i++) {
+            int randomNum = bridgeNumberGenerator.generate();
+            correctBridge.add(convertBridgeNum(randomNum));
+        }
+        return correctBridge;
+    }
+
+    public String convertBridgeNum(int randomNUm) {
+        if(randomNUm == Up) {
+            return GameConst.CHOOSE_UP.getMessage();
+        }
+        return GameConst.CHOOSE_DOWN.getMessage();
     }
 }
