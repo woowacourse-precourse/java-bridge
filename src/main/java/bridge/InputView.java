0bf750272586763adda07348a1f5e1ca61be6989
jamangstangs <jamangsong@gmail.com>
2022-11-22 15:27:52 +0900

    feat(InputView): 예외처리 로직 적용

--- src/main/java/bridge/InputView.java
+++ src/main/java/bridge/InputView.java
@@ -8,42 +8,62 @@ import java.util.regex.Pattern;
 public class InputView {
 
     public int readBridgeSize() {
-        String bridgeSize = camp.nextstep.edu.missionutils.Console.readLine();
-        validateBridgeSize(bridgeSize);
+        String bridgeSize = "";
+        try {
+            bridgeSize = camp.nextstep.edu.missionutils.Console.readLine();
+            validateBridgeSize(bridgeSize);
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+            bridgeSize = camp.nextstep.edu.missionutils.Console.readLine();
+            validateBridgeSize(bridgeSize);
+        }
         return Integer.parseInt(bridgeSize);
     }
 
     public String readMoving() {
-        String movingStep = camp.nextstep.edu.missionutils.Console.readLine();
-        validateMovingStep(movingStep);
+        String movingStep = "";
+        try {
+            movingStep = camp.nextstep.edu.missionutils.Console.readLine();
+            validateMovingStep(movingStep);
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+            movingStep = camp.nextstep.edu.missionutils.Console.readLine();
+            validateMovingStep(movingStep);
+        }
         return movingStep;
-
     }
 
     public String readGameCommand() {
-        String gameCommand = camp.nextstep.edu.missionutils.Console.readLine();
-        validateGameCommand(gameCommand);
+        String gameCommand = "";
+        try {
+            gameCommand = camp.nextstep.edu.missionutils.Console.readLine();
+            validateGameCommand(gameCommand);
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+            gameCommand = camp.nextstep.edu.missionutils.Console.readLine();
+            validateGameCommand(gameCommand);
+        }
         return gameCommand;
     }
 
     private void validateBridgeSize(String bridgeSize) {
         if (!Pattern.matches("[0-9]+", bridgeSize)) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException("[ERROR] 다리의 길이가 숫자가 아닙니다.");
         }
         if (Integer.parseInt(bridgeSize) < 3 || Integer.parseInt(bridgeSize) > 20) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException("[ERROR] 다리의 길이가 3에서 20이 아닙니다.");
         }
     }
 
     private void validateMovingStep(String movingStep) throws IllegalArgumentException {
         if (!movingStep.equals('U') ^ !movingStep.equals('D')) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException("[ERROR] 입력은 U 혹은 D만 가능합니다. ");
         }
     }
 
     private void validateGameCommand(String gameCommand) throws IllegalArgumentException{
         if (!gameCommand.equals('R') ^ !gameCommand.equals('Q')) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException("[ERROR] 입력은 R 혹은 Q만 가능합니다. ");
         }
     }
 
