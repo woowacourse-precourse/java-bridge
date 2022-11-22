87bcf03c25e65c7a3085c035aba2db05fae16ba8
jojaeng2 <ds4ouj@naver.com>
2022-11-22 18:36:19 +0900

    feat : 게임에 사용될 명령어를 관리하는 기능구현

---
+++ src/main/java/bridge/enums/GameCommand.java
@@ -0,0 +1,16 @@
+package bridge.enums;
+
+public enum GameCommand {
+    RETRY("R"),
+    QUIT("Q");
+
+    private final String command;
+
+    GameCommand(String command) {
+        this.command = command;
+    }
+
+    public String getCommand() {
+        return command;
+    }
+}
