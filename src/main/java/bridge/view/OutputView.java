fcda429eedffde62ce68e63c14627ef0cdbe61be
wnsgml7267 <dlwns7267@naver.com>
2022-11-22 22:10:10 +0900

    feat(view/OutputView): 게임 결과 출력 기능 구현

--- src/main/java/bridge/view/OutputView.java
+++ src/main/java/bridge/view/OutputView.java
@@ -30,7 +30,10 @@ public class OutputView {
      * <p>
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void printResult() {
+    public void printResult(BridgeGame bridgeGame, String userInput, List<String> bridge) {
+        System.out.println(GAME_RESULT_MSG);
+        printMap(bridgeGame);
+        System.out.println(GAME_SUCCESS_OR_FAIL_MSG + bridgeGame.checkGameSuccess(userInput, bridge));
+        System.out.println(TOTAL_PLAY_COUNT_MSG + bridgeGame.getGameAttempt().getAttempt());
     }
-
 }
