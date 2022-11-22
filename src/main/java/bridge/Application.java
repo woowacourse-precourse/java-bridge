091c1f800d12bfd5083bc6773fd762da58d35ac2
dldmsdn99 <dldmsdn99@naver.com>
2022-11-22 15:53:41 +0900

    main문 구현

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -1,8 +1,18 @@
 package bridge;
+import java.util.InputMismatchException;
+
 
 public class Application {
-
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        try {
+            Game_Control gamecontrol = new Game_Control();
+            gamecontrol.start();
+        } catch (IllegalArgumentException e) {
+            System.out.println(Message.INPUT_NUM_ERROR);
+        } catch (IllegalStateException e) {
+            System.out.println(Message.INPUT_U_D_ERROR);
+        }catch(InputMismatchException e){
+            System.out.println(Message.INPUT_CORRECT_COMMAND);
+        }
     }
 }
