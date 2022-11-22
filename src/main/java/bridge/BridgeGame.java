f62465c6c2410e24625737124d6957191747e9d6
jhsseonn <hosu0125@gmail.com>
2022-11-22 18:02:39 +0900

    fix: 사용자에게 입력받아 이동하는 메서드 수정

--- src/main/java/bridge/BridgeGame.java
+++ src/main/java/bridge/BridgeGame.java
@@ -1,6 +1,7 @@
 package bridge;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 /**
@@ -9,13 +10,24 @@ import java.util.List;
 public class BridgeGame {
 
     InputView inputView;
+    OutputView outputView;
     BridgeMaker bridgeMaker;
     public List<String> bridgeUsable = new ArrayList<>();
-    public int currentIndex=0;
-    public List<String> result = new ArrayList<>();
 
-    public void startGame() {
+    public HashMap<Integer, List<String>> result = new HashMap<Integer, List<String>>();
 
+    public int currentIndex = 0;
+
+    public void startGame(int currentIndex) {
+        int bridgeSize = inputView.readBridgeSize();
+        bridgeUsable = bridgeMaker.makeBridge(bridgeSize);
+        String resultUp="";
+        String resultDown="";
+        for (String move: bridgeUsable) {
+            String inputMove = inputView.readMoving();
+            move(move, inputMove, currentIndex);
+            outputView.printMap(result);
+        }
     }
 
     /**
@@ -23,17 +35,43 @@ public class BridgeGame {
      * <p>
      * 이동을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public String move(String move, List<String> bridgeUsable, int currentIndex) {
-        String result="";
-        if (move.equals(bridgeUsable.get(currentIndex))) {
-            result=("O");
+    public void move(String move, String inputMove, int currentIndex) {
+        String resultUp="";
+        String resultDown="";
+        List<String> results = new ArrayList<>();
+        if (move.equals("U")){
+            if (move.equals(inputMove)){
+                resultUp="O";
+                resultDown="E";
+                results.add(resultUp);
+                results.add(resultDown);
+                result.put(currentIndex, results);
+                currentIndex += 1;
+            }
+            if (!move.equals(inputMove)){
+                resultUp="X";
+                resultDown="E";
+                retry();
+            }
         }
-        if (!move.equals(bridgeUsable.get(currentIndex))) {
-            result=("X");
+        if (move.equals("D")){
+            if (move.equals(inputMove)){
+                resultUp="E";
+                resultDown="O";
+                results.add(resultUp);
+                results.add(resultDown);
+                result.put(currentIndex, results);
+                currentIndex += 1;
+            }
+            if (!move.equals(inputMove)){
+                resultUp="E";
+                resultDown="X";
+                retry();
+            }
         }
-
-        return result;
     }
+
+
 
     /**
      * 사용자가 게임을 다시 시도할 때 사용하는 메서드
@@ -41,6 +79,13 @@ public class BridgeGame {
      * 재시작을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
     public void retry() {
+        String gameCommand = inputView.readGameCommand();
+        if (gameCommand.equals("R")) {
+            startGame(currentIndex);
+        }
+        if (gameCommand.equals("Q")) {
 
+            return;
+        }
     }
 }
