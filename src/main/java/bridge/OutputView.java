daebfb7e2e320b7e0512ff80d3bee7edaa92592f
WonSubSub <boxer1995@naver.com>
2022-11-22 13:51:54 +0900

    refactor(main): 플레이어가 이동값 입력시 처리 기능 오류 수정

--- src/main/java/bridge/OutputView.java
+++ src/main/java/bridge/OutputView.java
@@ -7,12 +7,77 @@ import java.util.List;
  */
 public class OutputView {
 
+    InputView inputView = new InputView();
+    String upResult = "";
+    String downResult = "";
+
     /**
      * 현재까지 이동한 다리의 상태를 정해진 형식에 맞춰 출력한다.
      * <p>
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void printMap() {
+    public void printMap(String alphabet, List<String> bridge, int bridgeCount) {
+        if (bridgeCount == 0) {
+            firstResult(alphabet);
+        }
+        if (bridgeCount > 0) {
+            otherResult(bridge, bridgeCount);
+        }
+        printResult();
+    }
+
+    public void firstResult(String alphabet){
+        if (alphabet.equals("U")) {
+            upResult += "[ O ]";
+            downResult += "[   ]";}
+        if (alphabet.equals("D")) {
+            upResult += "[   ]";
+            downResult += "[ O ]";}
+    }
+
+    public void otherResult(List<String> bridge, int bridgeCount) {
+        insertLeftBracket();
+        for (int i = 0; i < bridgeCount + 1; i++) {
+            insertAlphabet(bridge.get(i));
+            if (i != bridgeCount) {
+                insertLineShape();
+            }
+        }
+        insertRightBracket();
+    }
+
+    public void insertLeftBracket(){
+        upResult = "[";
+        downResult = "[";
+    }
+
+    public void insertRightBracket(){
+        upResult += "]";
+        downResult += "]";
+    }
+
+    public void insertAlphabet(String alphabet) {
+        if (alphabet.equals("U")) {
+            upResult += " O ";
+            downResult += "   ";}
+        if (alphabet.equals("D")) {
+            upResult += "   ";
+            downResult += " O ";}
+    }
+
+    public void insertLineShape() {
+        upResult += "|";
+        downResult += "|";
+    }
+
+    public void printResult() {
+        System.out.println(upResult);
+        System.out.println(downResult);
+    }
+
+    public void clearResult() {
+        upResult = "";
+        downResult = "";
     }
 
     /**
