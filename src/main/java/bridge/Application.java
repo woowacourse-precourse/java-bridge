462ae5411fa19b414e3de9dcfd0a3b4e69aaee0f
KJE <12181501@inha.edu>
2022-11-22 18:14:44 +0900

    feat: Application- main, 게임 시작, move, retry 메서드 작성

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -6,11 +6,15 @@ public class Application {
         System.out.println("다리 건너기 게임을 시작합니다.\n");
         BridgeGame bridgeGame = new BridgeGame();
         startGame(bridgeGame);
+        boolean flag = true;
+        while (flag) {
         while(moveGame(bridgeGame)){
-            if(!retryGame(bridgeGame))
-                break;
         }
-        endGame(bridgeGame);
+        //게임 리트라이/끝
+            //false: 끝
+            //true; rery--===
+            flag = endOrRetry(bridgeGame);
+        }
     }
 
     public static void startGame(BridgeGame bridgeGame){
@@ -27,20 +31,61 @@ public class Application {
     }
 
     public static boolean moveGame(BridgeGame bridgeGame){
-        boolean continueGame = true;
-        boolean bridgeStatus = true;
-        while (continueGame) {
-            while (bridgeStatus) {
-                String userMove = InputView.readMoving();
-                bridgeStatus = bridgeGame.move(userMove);
-                OutputView.printMap(bridgeGame.getBridge(), bridgeGame.getNowIdx(), userMove);
+        boolean flag;
+        do {
+            try {
+                flag = false;
+                return moveGame1(bridgeGame);
+            } catch (IllegalArgumentException e) {
+                flag = true;
             }
-        }
+        } while (flag);
+        return false;
     }
 
-    public static boolean retryGame(BridgeGame bridgeGame){}
-    public static void endGame(BridgeGame bridgeGame){
+    public static boolean moveGame1(BridgeGame bridgeGame){
+        String userMove = InputView.readMoving();
+        OutputView.printMap(bridgeGame.getBridge(), bridgeGame.getNowIdx(), userMove);
+        return bridgeGame.move(userMove);
+    }
 
+    public static boolean retryGame(BridgeGame bridgeGame){
+        boolean flag;
+        do {
+            try {
+                flag = false;
+                return bridgeGame.retry(InputView.readGameCommand());
+            } catch (IllegalArgumentException e) {
+                flag = true;
+            }
+        } while (flag);
+        return false;
+    }
+
+    public static boolean endGame(BridgeGame bridgeGame){
+        OutputView.printResult(bridgeGame);
+        return false;
+    }
+
+    public static boolean endOrRetry(BridgeGame bridgeGame){
+        boolean flag;
+        do {
+            try {
+                flag = false;
+                endOrRetry1(bridgeGame);
+            } catch (IllegalArgumentException e) {flag = true;}
+        } while (flag);
+        return false;
+    }
+
+    public static boolean endOrRetry1(BridgeGame bridgeGame){
+        if(bridgeGame.getWin())
+            return endGame(bridgeGame);
+        if(retryGame(bridgeGame) == false)
+            return endGame(bridgeGame);
+        return true;
     }
 
 }
+
+
