26029214dc1e208f1054da0937175353ed35fa4f
juhee park <jouhi3322@gmail.com>
2022-11-22 16:12:08 +0900

    test : 통합 테스트 추가

--- src/test/java/bridge/ApplicationTest.java
+++ src/test/java/bridge/ApplicationTest.java
@@ -1,6 +1,7 @@
 package bridge;
 
 import camp.nextstep.edu.missionutils.test.NsTest;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
@@ -37,6 +38,50 @@ class ApplicationTest extends NsTest {
             int upSideIndex = output().indexOf("[ O |   | O ]");
             int downSideIndex = output().indexOf("[   | O |   ]");
             assertThat(upSideIndex).isLessThan(downSideIndex);
+        }, 1, 0, 1);
+    }
+
+    @DisplayName("출력 문자열 확인")
+    @Test
+    void 기능_테스트1() {
+        assertRandomNumberInRangeTest(() -> {
+            run("3", "U", "U", "R", "U", "D", "U");
+            assertThat(output()).contains(
+                    "다리 건너기 게임을 시작합니다.",
+                    "다리의 길이를 입력해주세요.",
+                    "이동할 칸을 선택해주세요. (위: U, 아래: D)",
+                    "게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)"
+            );
+        }, 1, 0, 1);
+    }
+
+    @DisplayName("출력 화면 확인1_성공 경우")
+    @Test
+    void 기능_테스트2() {
+        assertRandomNumberInRangeTest(() -> {
+            run("3", "U", "U", "R", "U", "D", "U");
+            assertThat(output()).contains(
+                    "[ O | X ]",
+                    "[   |   ]",
+                    "최종 게임 결과",
+                    "게임 성공 여부: 성공",
+                    "총 시도한 횟수: 2"
+            );
+        }, 1, 0, 1);
+    }
+
+    @DisplayName("출력 화면 확인2_ 실패경우")
+    @Test
+    void 기능_테스트3() {
+        assertRandomNumberInRangeTest(() -> {
+            run("3", "U", "U", "Q");
+            assertThat(output()).contains(
+                    "최종 게임 결과",
+                    "[ O | X ]",
+                    "[   |   ]",
+                    "게임 성공 여부: 실패",
+                    "총 시도한 횟수: 1"
+            );
         }, 1, 0, 1);
     }
 
