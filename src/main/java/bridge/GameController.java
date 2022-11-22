71d62d4b9e6d593921ec93f7a086260342f970d8
Mr.Kim <86274253+Joajy@users.noreply.github.com>
2022-11-22 21:14:36 +0900

    refactor(GameController): implement code convention

--- src/main/java/bridge/GameController.java
+++ src/main/java/bridge/GameController.java
@@ -2,27 +2,27 @@ package bridge;
 
 public class GameController {
     public static void setGame() {
-        final OutputView Output = new OutputView(); 
-        Output.ResetAll();   
+        final OutputView output = new OutputView(); 
+        output.resetAll();   
         BridgeGame.makeGame(InputView.readBridgeSize());
         while (BridgeGame.play()) {
-            printMove(Output);
+            printMove(output);
             if (BridgeGame.getstop()) 
-                resetGame(Output);
+                resetGame(output);
         }
-        Output.printResult(BridgeGame.getstop(), BridgeGame.getGameCount());
+        output.printResult(BridgeGame.getstop(), BridgeGame.getGameCount());
     }
 
-    public static void printMove(OutputView Output) {
+    public static void printMove(OutputView output) {
         BridgeGame.move(InputView.readMoving());
-        Output.printMap(BridgeGame.getLocation(), BridgeGame.getstop());
+        output.printMap(BridgeGame.getLocation(), BridgeGame.getstop());
     }
 
-    public static void resetGame(OutputView Output) {
+    public static void resetGame(OutputView output) {
         String cmd = InputView.readGameCommand();
         if(cmd.equals(Constant.RESTART)){
             BridgeGame.replay();
-            Output.ResetAll();
+            output.resetAll();
         }
     }
 }
