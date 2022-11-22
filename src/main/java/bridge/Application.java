78866a110ef47871408b3222be677e10e95f6052
Seung-Been Kim <mainlib990@gmail.com mainlib990s-MacBookPro.local>
2022-11-22 17:25:27 +0900

    feat(application): connect the function that read size of bridge

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -1,18 +1,22 @@
 package bridge;
 
+import bridge.command.BridgeSize;
+import bridge.view.InputView;
 import bridge.view.OutputView;
 
 public class Application {
 
     private static final OutputView outputView = new OutputView();
+    private static final InputView inputView = new InputView();
 
     public static void main(String[] args) {
         printTitle();
-        askBridgeSize();
+        final BridgeSize bridgeSize = askBridgeSize();
     }
 
-    private static void askBridgeSize() {
+    private static BridgeSize askBridgeSize() {
         outputView.printBridgeSize();
+        return inputView.readBridgeSize();
     }
 
     private static void printTitle() {
