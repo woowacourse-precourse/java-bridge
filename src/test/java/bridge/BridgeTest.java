90989c4ae15ca48b012d119f20f8c804a6230e02
JUHA <khabh@naver.com>
2022-11-22 15:49:48 +0900

    test(Bridge): 다리 기능 테스트

--- src/test/java/bridge/BridgeTest.java
+++ src/test/java/bridge/BridgeTest.java
@@ -1,50 +1,26 @@
 package bridge;
 
-import bridge.controller.BridgeController;
-import bridge.dto.BridgeDto;
-import bridge.service.BridgeService;
+import bridge.domain.bridgeGame.Bridge;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.ValueSource;
 
-import java.io.ByteArrayInputStream;
-import java.io.ByteArrayOutputStream;
-import java.io.OutputStream;
-import java.io.PrintStream;
 import java.util.List;
-import java.util.NoSuchElementException;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
-class BridgeTest {
-    private final BridgeController bridgeController = new BridgeController();
-    private final BridgeService bridgeService = new BridgeService();
+public class BridgeTest {
+    Bridge bridge;
 
-    @DisplayName("다리 사이즈가 올바른 형식이 아닌 경우")
-    @ValueSource(strings = {"a", "가", "1", "-1", "21"})
-    @ParameterizedTest
-    void notNumericBridgeSize(String input) {
-        final String ERROR_MESSAGE = "[ERROR]";
-        OutputStream out = new ByteArrayOutputStream();
-        System.setOut(new PrintStream(out));
-
-        byte[] bridgeSize = String.join("\n", input).getBytes();
-        System.setIn(new ByteArrayInputStream(bridgeSize));
-
-        try {
-            bridgeController.generateBridge();
-        } catch (NoSuchElementException ignore) {
-        }
-
-        assertThat(out.toString()).contains(ERROR_MESSAGE);
+    @BeforeEach
+    void setUp() {
+        bridge = Bridge.from(List.of("U", "U", "D"));
     }
 
-    @DisplayName("플레이어가 이동할 칸이 건널 수 있는 칸인지 확인")
+    @DisplayName("건널 수 있는 방향 조회")
     @Test
-    void isBridgeBlockPassable() {
-        bridgeService.initBridge(BridgeDto.from(List.of("U", "D")));
-        assertThat(bridgeService.isPassable(0, "U")).isTrue();
-        assertThat(bridgeService.isPassable(1, "U")).isFalse();
+    void getPassableDirection() {
+        String direction = bridge.passableDirectionAt(0);
+        assertThat(direction).isEqualTo("U");
     }
 }
