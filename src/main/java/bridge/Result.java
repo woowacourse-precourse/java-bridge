72de4fb95d33efba80f2b71995779d3e03fdd9b4
noparamin <noparamin@naver.com>
2022-11-22 17:16:23 +0900

    feat(Result): 다리 건너기 결과 추가 기능 구현

---
+++ src/main/java/bridge/Result.java
@@ -0,0 +1,28 @@
+package bridge;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Result {
+    List<List<String>> results;
+    int tryCount;
+    String isSuccess;
+
+    Result() {
+        tryCount = 1;
+        isSuccess = "성공";
+        results = new ArrayList<>();
+    }
+
+    public void addResult(String movingPosition, boolean isCorrect) {
+        if (isCorrect) {
+            results.add(List.of(movingPosition, "O"));
+        }
+
+        if (!isCorrect) {
+            results.add(List.of(movingPosition, "X"));
+        }
+    }
+
+
+}
