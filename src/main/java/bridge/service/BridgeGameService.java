5d895476e1ce025c9ae25d4030b39c2703fc02b8
juno-junho <ssmm0205@naver.com>
2022-11-22 23:20:54 +0900

    feat(service): create method in BridgeGameService
     BridgeGameService에 메서드 추가

--- src/main/java/bridge/service/BridgeGameService.java
+++ src/main/java/bridge/service/BridgeGameService.java
@@ -1,4 +1,26 @@
 package bridge.service;
 
+import bridge.BridgeMaker;
+import bridge.domain.*;
+
 public class BridgeGameService {
+    private static final int INIT_VALUE_OF_POSITION = 0;
+    private static final int INIT_VALUE_OF_CHALLENGES = 1;
+
+    private final BridgeMaker bridgeMaker;
+
+    public BridgeGameService(BridgeMaker bridgeMaker) {
+        this.bridgeMaker = bridgeMaker;
+    }
+
+    public BridgeGame bridgeGame(BridgeSizeValidator bridgeSize) {
+        Bridge bridge = new Bridge(
+                bridgeMaker.makeBridge(bridgeSize.bridgeSize()));
+
+        Player player = new Player(
+                Position.of(INIT_VALUE_OF_POSITION),
+                new ChallengeNumbers(INIT_VALUE_OF_CHALLENGES));
+
+        return new BridgeGame(bridge, player);
+    }
 }
