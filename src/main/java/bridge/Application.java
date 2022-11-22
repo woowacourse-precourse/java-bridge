af2b6369800de33003ee05c65bd6a44ffbcb6195
Sanmgmin <sangmins930@gmail.com>
2022-11-22 23:49:20 +0900

    feat: excute Application

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -1,8 +1,11 @@
 package bridge;
 
+import bridge.Controller.Controller;
+
 public class Application {
 
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        Controller.runGame();
     }
 }
