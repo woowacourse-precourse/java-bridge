ba9417d1e3c5fdcee06e75b0d15e91086094bf32
dvlpr0 <rladjswkd@naver.com>
2022-11-22 20:28:27 +0900

    fix: GameStatus에 Invalid 상태 추가

--- src/main/java/bridge/GameStatus.java
+++ src/main/java/bridge/GameStatus.java
@@ -3,7 +3,8 @@ package bridge;
 public enum GameStatus {
     GAME_FAIL(0),
     GAME_SUCCESS(1),
-    GAME_CONTINUE(2);
+    GAME_CONTINUE(2),
+    GAME_INVALID(3);
 
     private final int status;
     GameStatus(int status) {
@@ -15,11 +16,17 @@ public enum GameStatus {
     public boolean isFail() {
         return (status == 0);
     }
+    public boolean isInvalid() {
+        return (status == 3);
+    }
     @Override
     public String toString() {
         if (status == 1) {
             return ("성공");
         }
-        return ("실패");
+        if (status == 0) {
+            return ("실패");
+        }
+        return ("");
     }
 }
