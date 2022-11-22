067f27f3dacc426daa2d8b2f9bd4cccf85c7ecaa
opuopuo <pureuml.dev@gmail.com>
2022-11-22 14:54:02 +0900

    test(InputManagementTest): 재시작/종료 예외 처리 테스트 코드 추가

--- src/test/java/bridge/InputManagementTest.java
+++ src/test/java/bridge/InputManagementTest.java
@@ -2,6 +2,7 @@ package bridge;
 
 import static bridge.constant.ErrorMessage.INPUT_BRIDGE_SIZE_ERROR;
 import static bridge.constant.ErrorMessage.INPUT_DIRECTION_ERROR;
+import static bridge.constant.ErrorMessage.INPUT_RETRY_OR_QUIT_ERROR;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import org.junit.jupiter.api.DisplayName;
@@ -9,7 +10,7 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.EmptySource;
 import org.junit.jupiter.params.provider.ValueSource;
 
-public class InputValidatorTest {
+public class InputManagementTest {
 //    @ParameterizedTest
 //    @MethodSource("generateNumber")
 //    void shouldGetDataBit(int first, String second, List<String> third) {
@@ -25,25 +26,36 @@ public class InputValidatorTest {
 //        );
 //    }
 
-    @DisplayName("3 ~ 20 범위 숫자가 아닌 다리 길이 입력 예외 처리")
-    @ValueSource(strings = {"0", "2", "21", "4.1", "-14"})
+    @DisplayName("양의 정수가 아닌 다리 길이 예외 처리")
+    @ValueSource(strings = {".0", "test", "1 3", "4.1", "-14", " 5 1"})
     @ParameterizedTest
     @EmptySource
     void notValidBridgeSize(String input) {
-        InputValidator inputValidator = new InputValidator(input);
-        assertThatThrownBy(() -> inputValidator.isInRangeSize())
+        InputManagement inputManagement = new InputManagement(input);
+        assertThatThrownBy(() -> inputManagement.isNumber())
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining(String.valueOf(INPUT_BRIDGE_SIZE_ERROR));
     }
 
-    @DisplayName("U, D 아닌 이동 방향 입력 예외 처리")
+    @DisplayName("U, D 아닌 이동 방향 예외 처리")
     @ValueSource(strings = {"A", "U1", "Ua", ".D"})
     @ParameterizedTest
     @EmptySource
     void notValidDirection(String input) {
-        InputValidator inputValidator = new InputValidator(input);
-        assertThatThrownBy(() -> inputValidator.isUpOrDown())
+        InputManagement inputManagement = new InputManagement(input);
+        assertThatThrownBy(() -> inputManagement.isUpOrDown())
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining(String.valueOf(INPUT_DIRECTION_ERROR));
+    }
+
+    @DisplayName("R, Q 아닌 재시작/종료 예외 처리")
+    @ValueSource(strings = {"A", "r", "q", "R."})
+    @ParameterizedTest
+    @EmptySource
+    void notRetryOrQuit(String input) {
+        InputManagement inputManagement = new InputManagement(input);
+        assertThatThrownBy(() -> inputManagement.isRetryOrQuit())
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(String.valueOf(INPUT_RETRY_OR_QUIT_ERROR));
     }
 }
