57406d5c20632b0e381ce6924596735fabe376e8
juhee park <jouhi3322@gmail.com>
2022-11-22 16:10:55 +0900

    refactor : 클래스 분리

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -1,55 +1,14 @@
 package bridge;
 
-import java.util.List;
-
-import static constants.Constants.*;
-
 public class Application {
-    private static final InputView inputView = new InputView();
-    private static final OutputView outputView = new OutputView();
 
     public static void main(String[] args) {
         try {
-            System.out.println(GAME_START_MESSAGE);
-            BridgeGame bridgeGame = new BridgeGame(makeBridge());
-            startGame(bridgeGame);
+            BridgeGameRunner bridgeGameRunner = new BridgeGameRunner();
+            bridgeGameRunner.run();
         } catch (IllegalArgumentException e) {
             System.out.println(e.getMessage());
         }
-    }
-
-    private static void startGame(BridgeGame bridgeGame) {
-        while (true) {
-            play(bridgeGame);
-            if (userWantedQuit(bridgeGame.getStatus())) {
-                break;
-            }
-            bridgeGame.retry();
-        }
-        outputView.printResult(bridgeGame);
-    }
-
-    private static void play(BridgeGame bridgeGame) {
-        while (bridgeGame.getStatus() == Status.PLAYING) {
-            String userWantedDirection = inputView.readMoving();
-            bridgeGame.move(userWantedDirection);
-            outputView.printMap(bridgeGame);
-        }
-    }
-
-    private static boolean userWantedQuit(Status status) {
-        //다리 끝까지 와서 게임이 끝난경우(유저의 입력을 받지 않고 종료 시킨다)
-        if (status == Status.ENDING) {
-            return true;
-        }
-
-        String wantedDirection = inputView.readGameCommand();
-        return wantedDirection.equals(GAME_COMMAND_QUIT);
-    }
-
-    private static List<String> makeBridge() {
-        int bridgeSize = inputView.readBridgeSize();
-        return new BridgeMaker(new BridgeRandomNumberGenerator()).makeBridge(bridgeSize);
     }
 
 }
---
+++ src/main/java/bridge/BridgeGameRunner.java
@@ -0,0 +1,52 @@
+package bridge;
+
+import java.util.List;
+
+import static constants.Constants.GAME_COMMAND_QUIT;
+import static constants.Constants.GAME_START_MESSAGE;
+
+public class BridgeGameRunner {
+    private static final InputView inputView = new InputView();
+    private static final OutputView outputView = new OutputView();
+
+    public void run() {
+        System.out.println(GAME_START_MESSAGE);
+        BridgeGame bridgeGame = new BridgeGame(makeBridge());
+        startGame(bridgeGame);
+    }
+
+    private void startGame(BridgeGame bridgeGame) {
+        while (true) {
+            play(bridgeGame);
+            if (userWantedQuit(bridgeGame.getStatus())) {
+                break;
+            }
+            bridgeGame.retry();
+        }
+        outputView.printResult(bridgeGame);
+    }
+
+    private void play(BridgeGame bridgeGame) {
+        while (bridgeGame.getStatus() == Status.PLAYING) {
+            String userWantedDirection = inputView.readMoving();
+            bridgeGame.move(userWantedDirection);
+            outputView.printMap(bridgeGame);
+        }
+    }
+
+    private boolean userWantedQuit(Status status) {
+        //다리 끝까지 와서 게임이 끝난경우(유저의 입력을 받지 않고 종료 시킨다)
+        if (status == Status.ENDING) {
+            return true;
+        }
+
+        String wantedDirection = inputView.readGameCommand();
+        return wantedDirection.equals(GAME_COMMAND_QUIT);
+    }
+
+    private List<String> makeBridge() {
+        int bridgeSize = inputView.readBridgeSize();
+        return new BridgeMaker(new BridgeRandomNumberGenerator()).makeBridge(bridgeSize);
+    }
+
+}
