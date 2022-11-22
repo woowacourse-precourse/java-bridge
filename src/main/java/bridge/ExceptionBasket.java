8fe2ebcf502070d8b6f90793ebe46c05f4310f4a
kissshot1104 <kissshot.heartunderblade1104@gmail.com>
2022-11-22 10:32:07 +0900

    Refactor(ExceptionBasket) : 예외 메시지 반환 기능 옮김
    예외 메시지를 OutputExceptionMessage라는 enum 클래스로 옮겼습니다.

--- src/main/java/bridge/ExceptionBasket.java
+++ src/main/java/bridge/ExceptionBasket.java
@@ -6,47 +6,40 @@ public class ExceptionBasket {
 
     public static final int MAX = 20;
     public static final int MIN = 1;
-    public static final String ERROR_MESSAGE = "[ERROR]";
 
-    //1 ~ 20이외의 정수나 맨 앞에 0과 +가 나온다면 예외 처리
+    /**1 ~ 20이외의 정수나 맨 앞에 0과 +가 나온다면 예외 처리 */
     public static void inputIsNotRangeException(String input) throws IllegalArgumentException {
         BigInteger tmpInput;
         try {
             tmpInput = new BigInteger(input);
-        }catch (IllegalArgumentException iae) {
-            throw new IllegalArgumentException(ERROR_MESSAGE + " 1~20사이의 정수가 아닙니다.");
+        } catch (IllegalArgumentException iae) {
+            throw OutputExceptionMessage.NOT_RANGE_EXCEPTION.getException();
         }
         if (tmpInput.compareTo(BigInteger.valueOf(MIN)) == -1 || tmpInput.compareTo(BigInteger.valueOf(MAX)) == 1
-                || input.indexOf('0') == 0 || input.indexOf('+') == 0){
-            throw new IllegalArgumentException(ERROR_MESSAGE + " 1~20사이의 정수가 아닙니다.");
+                || input.indexOf('0') == 0 || input.indexOf('+') == 0) {
+            throw OutputExceptionMessage.NOT_RANGE_EXCEPTION.getException();
         }
     }
 
-    //숫자가 아닐 경우 예외 처리
+    /**숫자가 아닐 경우 예외 처리 */
     public static void inputIsNotNumberException(String input) throws IllegalArgumentException {
-        if (!Util.isNumber(input)){
-            throw new IllegalArgumentException(ERROR_MESSAGE + " 숫자가 아닙니다!");
+        if (!Util.isNumber(input)) {
+            throw OutputExceptionMessage.NOT_NUMBER_EXCEPTION.getException();
         }
     }
 
+    /** 다리를 이동 중 잘못된 키를 입력시 예외 발생 */
     public static void invalidGameKey(String input) throws IllegalArgumentException {
-        if (!input.equals("U") && !input.equals("D")){
-            throw new IllegalArgumentException(ERROR_MESSAGE + " U나 D를 누르세요!");
+        if (!input.equals("U") && !input.equals("D")) {
+            throw OutputExceptionMessage.INVALID_GAME_KEY_EXCEPTION.getException();
         }
     }
 
-    public static void invalidRetryRequestInputKey(String input) throws IllegalArgumentException{
-        if (!input.equals("R") && !input.equals("Q")){
-            throw new IllegalArgumentException(ERROR_MESSAGE + " R이나 Q를 누르세요!");
+    /** 게임을 다시 시작할 때 잘못된 키를 입력 시 예외 발생 */
+    public static void invalidRetryRequestInputKey(String input) throws IllegalArgumentException {
+        if (!input.equals("R") && !input.equals("Q")) {
+            throw OutputExceptionMessage.INVALID_RETRY_REQUEST_INPUT_KEY_EXCEPTION.getException();
         }
     }
-
-    /*public static void invalidNumberic(String input) throws IllegalArgumentException {
-        if (input.indexOf('+') == 0
-                || input.indexOf('0') == 0
-                || input.indexOf('-') == 0){
-            throw new IllegalArgumentException(ERROR_MESSAGE + " 잘못된 숫자열입니다.");
-        }
-    }*/
 
 }
