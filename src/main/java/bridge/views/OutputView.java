591b25f38bd8a74f3e7994a29ed5913f4841d305
jonghun <jonghuncu@gmail.com>
2022-11-22 23:05:13 +0900

    feat(bridge) : 결과 출력 기능

--- src/main/java/bridge/views/OutputView.java
+++ src/main/java/bridge/views/OutputView.java
@@ -1,17 +1,45 @@
-package bridge;
+package bridge.views;
+
+import bridge.controller.BridgeController;
+import bridge.domain.BridgeJudgmentCase;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * 사용자에게 게임 진행 상황과 결과를 출력하는 역할을 한다.
  */
 public class OutputView {
 
+
+    public static List<String> Lst1 = new ArrayList<>();
+    public static List<String> Lst2 = new ArrayList<>();
+
+
+    public OutputView() {
+    }
+
     /**
      * 현재까지 이동한 다리의 상태를 정해진 형식에 맞춰 출력한다.
      * <p>
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void printMap() {
+    public static void printMap() {
+        if(BridgeController.playerStatus()) {
+            System.out.println("O");
+            return;
+        }
+        if(!BridgeController.playerStatus()) {
+            System.out.println("X");
+        }
+//        BridgeJudgmentCase.BridgeJudgmentCaseReturn();
+
     }
+
+
+
+
+
 
     /**
      * 게임의 최종 결과를 정해진 형식에 맞춰 출력한다.
