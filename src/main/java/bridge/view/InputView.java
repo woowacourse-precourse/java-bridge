3210cc0f75b0db5ac72b95353f90d9e06a0929de
nahyeon <whskgus12@nate.com>
2022-11-22 14:49:03 +0900

    feat(view): 다리의 길이 입력받기
    사용자에게 생성할 다리의 길이를 입력받습니다. 입력은 요구사항에 주어진대로 readLine 메서드를 사용했습니다.
    다리의 길이 입력 후 예외처리를 실시해, 메서드 호출 시 정상적인 값의 길이만 전달되도록 합니다.

--- src/main/java/bridge/view/InputView.java
+++ src/main/java/bridge/view/InputView.java
@@ -1,15 +1,21 @@
 package bridge.view;
 
+import bridge.Exception.BridgeInputException;
+import camp.nextstep.edu.missionutils.Console;
+
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
  */
 public class InputView {
-
+    BridgeInputException bridgeInputException = new BridgeInputException();
     /**
      * 다리의 길이를 입력받는다.
      */
     public int readBridgeSize() {
-        return 0;
+        System.out.println("다리의 길이를 입력해주세요.");
+        String lenBridge = Console.readLine();
+        bridgeInputException.validBridgeLength(lenBridge);
+        return Integer.parseInt(lenBridge);
     }
 
     /**
