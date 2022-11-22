cfc9354a35345c2f7e4306ad2609b497d6a2c94d
sen <seeunchoi99@gmail.com>
2022-11-22 21:51:57 +0900

    🔥 BridgeRecord를 non-static으로 변경

--- src/main/java/bridge/domain/BridgeRecord.java
+++ src/main/java/bridge/domain/BridgeRecord.java
@@ -5,13 +5,13 @@ import java.util.Collections;
 import java.util.List;
 
 public class BridgeRecord {
-    private static final List<Command> bridgeRecord = new ArrayList<>();
+    private final List<Command> bridgeRecord = new ArrayList<>();
 
-    public static List<Command> getBridgeRecord() {
+    public List<Command> getBridgeRecord() {
         return Collections.unmodifiableList(bridgeRecord);
     }
 
-    public static void update(Command command) {
+    public void update(Command command) {
         bridgeRecord.add(command);
     }
 }
