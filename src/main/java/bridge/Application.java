37d7ce625050098590e2a59b64d841b7b4cb0cb4
SuhHyeonjun <dmsgktn0208@gmail.com>
2022-11-22 17:03:49 +0900

    fix : 예제 테스트 오류 찾기

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -4,7 +4,11 @@ import bridge.controller.BridgeGameController;
 
 public class Application {
     public static void main(String[] args) {
-        BridgeGameController bridgeGameController = new BridgeGameController();
-        bridgeGameController.start();
+        try {
+            BridgeGameController bridgeGameController = new BridgeGameController();
+            bridgeGameController.start();
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+        }
     }
 }
