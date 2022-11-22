65634673546524fc508221d59fd10d8b091076c0
mng990 <ryoo0504@pusan.ac.kr>
2022-11-22 12:18:48 +0900

    style(main/java/bridge/BridgeGame.java): method 순서 변경

--- src/main/java/bridge/BridgeGame.java
+++ src/main/java/bridge/BridgeGame.java
@@ -19,27 +19,29 @@ public class BridgeGame {
     private static Integer play;
 
 
-    public State move(Glass user) {
-        return State.stateOf(user == BRIDGE.get(step++));
+    public List<Glass> history() {
+        return BRIDGE.subList(0, step);
     }
 
     public Integer play() { return play;    }
     public Integer step() { return step;    }
-    public List<Glass> history() {
-        return BRIDGE.subList(0, step);
-    }
 
     /**
      * 사용자가 게임을 다시 시도할 때 사용하는 메서드
      * <p>
      * 재시작을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
+
+    public State move(Glass user) {
+        return State.stateOf(user == BRIDGE.get(step++));
+    }
+
     public void retry() {
         play++;
         step = 0;
     }
 
-    public List<Glass> initBridge(List<String> bridge){
+    public List<Glass> buildBridge(List<String> bridge){
         List<Glass> newBridge= new ArrayList<Glass>();
 
         for(String move: bridge){
@@ -51,6 +53,6 @@ public class BridgeGame {
     BridgeGame(List<String> bridge){
         step = 0;
         play = 1;
-        BRIDGE = initBridge(bridge);
+        BRIDGE = buildBridge(bridge);
     }
 }
