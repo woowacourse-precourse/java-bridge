ec49f97ac4454147b7ca7b5584b501acb986f1f6
dhyyp1001 <dhyyp1001@gmail.com>
2022-11-22 18:18:06 +0900

    feat: BridgeGame.java / 유저의 이동위치 맞게 출력을 위한 move()의 세부 메소드 추가

--- src/main/java/bridge/BridgeGame.java
+++ src/main/java/bridge/BridgeGame.java
@@ -1,8 +1,8 @@
 package bridge;
 
-/**
- * 다리 건너기 게임을 관리하는 클래스
- */
+import java.util.ArrayList;
+import java.util.List;
+
 public class BridgeGame {
 
     /**
@@ -13,11 +13,41 @@ public class BridgeGame {
     public void move() {
     }
 
-    /**
-     * 사용자가 게임을 다시 시도할 때 사용하는 메서드
-     * <p>
-     * 재시작을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
-     */
-    public void retry() {
+    public void moveUpRoadEqualCount(String userDirection, List<String> bridge, int successCount) {
+        if (userDirection.equals(bridge.get(successCount))) {
+            if (userDirection.equals("U"))
+                trialMap_up.add(" O ");
+            if (userDirection.equals("D"))
+                trialMap_up.add("   ");
+        }
     }
+
+    public void moveUpRoadUnequalCount(String userDirection, List<String> bridge, int successCount) {
+        if (!userDirection.equals(bridge.get(successCount))) {
+            if (userDirection.equals("U"))
+                trialMap_up.add(" X ");
+            if (userDirection.equals("D"))
+                trialMap_up.add("   ");
+        }
+    }
+
+    public void moveDownRoadEqualCount(String userDirection, List<String> bridge, int successCount) {
+        if (userDirection.equals(bridge.get(successCount))) {
+            if (userDirection.equals("U"))
+                trialMap_down.add("   ");
+            if (userDirection.equals("D"))
+                trialMap_down.add(" O ");
+        }
+    }
+
+    public void moveDownRoadUnequalCount(String userDirection, List<String> bridge, int successCount) {
+        if (!userDirection.equals(bridge.get(successCount))) {
+            if (userDirection.equals("U"))
+                trialMap_down.add("   ");
+            if (userDirection.equals("D"))
+                trialMap_down.add(" X ");
+        }
+    }
+
+
 }
