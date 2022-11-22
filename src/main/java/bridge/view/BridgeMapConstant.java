0df1578cc0016326d9919036adfffd5dc4b78e95
nomushroomplz <aaron97@knou.ac.kr>
2022-11-22 19:02:28 +0900

    feat: BridgeMapConstant 출력 상수 선언

---
+++ src/main/java/bridge/view/BridgeMapConstant.java
@@ -0,0 +1,15 @@
+package bridge.view;
+
+public class BridgeMapConstant {
+    public static final String START_HEAD = "[";
+    public static final String ROUND_CENTER = "|";
+    public static final String END_FAIL = "]";
+
+    public static final String SUCCESS_SIGN = "O";
+    public static final String BLANK_SIGN = "  ";
+    public static final String FAIL_SIGN = "X";
+
+    private BridgeMapConstant() {
+
+    }
+}
