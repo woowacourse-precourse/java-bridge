49e485c5497210747272bf5780ef5724cc7b9530
thuthi <whquddn55@gmail.com>
2022-11-22 22:26:05 +0900

    test(application): 통합 테스트 케이스 추가 다리 생성 길이 예외테스트, 다리 위치 입력 예외 테스트, 게임 재시작 입력 예외 테스트 케이스 추가. 게임 실패 종료 테스트, 게임 실패 재시잭 테스트 케이스 추가.

--- src/test/java/bridge/ApplicationTest.java
+++ src/test/java/bridge/ApplicationTest.java
@@ -7,6 +7,7 @@ import static org.assertj.core.util.Lists.newArrayList;
 
 import bridge.bridge.BridgeMaker;
 import bridge.bridge.BridgeNumberGenerator;
+import bridge.exception.IllegalArgumentExceptionMessage;
 import camp.nextstep.edu.missionutils.test.NsTest;
 import java.util.List;
 import org.junit.jupiter.api.Test;
@@ -47,6 +48,70 @@ class ApplicationTest extends NsTest {
             runException("a");
             assertThat(output()).contains(ERROR_MESSAGE);
         });
+    }
+
+    @Test
+    void 다리_생성_짧은_길이_예외_테스트() {
+        assertSimpleTest(() -> {
+            runException("2");
+            assertThat(output()).contains(IllegalArgumentExceptionMessage.ILLEGAL_BRIDGE_LENGTH.getMessage());
+        });
+    }
+
+    @Test
+    void 다리_생성_긴_길이_예외_테스트() {
+        assertSimpleTest(() -> {
+            runException("21");
+            assertThat(output()).contains(IllegalArgumentExceptionMessage.ILLEGAL_BRIDGE_LENGTH.getMessage());
+        });
+    }
+
+    @Test
+    void 다리_위치_입력_예외_테스트() {
+        assertRandomNumberInRangeTest(() -> {
+            runException("4", "D", "U", "U", "A");
+            assertThat(output()).contains(IllegalArgumentExceptionMessage.ILLEGAL_BRIDGE_POSITION.getMessage());
+        }, 0, 1, 1, 1);
+    }
+
+    @Test
+    void 게임_재시작_입력_예외_테스트() {
+        assertRandomNumberInRangeTest(() -> {
+            runException("4", "D", "U", "U", "D", "T");
+            assertThat(output()).contains(IllegalArgumentExceptionMessage.ILLEGAL_GAME_COMMAND.getMessage());
+        }, 0, 1, 1, 1);
+    }
+
+    @Test
+    void 게임_실패_종료_테스트() {
+        assertRandomNumberInRangeTest(() -> {
+            run("4", "D", "U", "U", "D", "Q");
+            assertThat(output()).contains(
+                    "최종 게임 결과",
+                    "[   | O | O |   ]",
+                    "[ O |   |   | X ]",
+                    "게임 성공 여부: 실패",
+                    "총 시도한 횟수: 1"
+            );
+        }, 0, 1, 1, 1);
+    }
+    @Test
+    void 게임_실패_재시작_테스트() {
+        assertRandomNumberInRangeTest(() -> {
+            run("4", "D", "U", "U", "D", "R", "D", "U", "U", "U");
+            assertThat(output()).contains(
+                    "[   | O | O |   ]",
+                    "[ O |   |   | X ]"
+            );
+
+            assertThat(output()).contains(
+                    "최종 게임 결과",
+                    "[   | O | O | O ]",
+                    "[ O |   |   |   ]",
+                    "게임 성공 여부: 성공",
+                    "총 시도한 횟수: 2"
+            );
+        }, 0, 1, 1, 1);
     }
 
     @Override
