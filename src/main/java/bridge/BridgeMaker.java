fd3f43ebd84590c9d4c7fc4e0ff2630c93d62dee
YOON <seoung59@gmail.com>
2022-11-22 23:07:18 +0900

    Feat: 자동으로 생성할 다리 길이를 입력 받는다

--- src/main/java/bridge/BridgeMaker.java
+++ src/main/java/bridge/BridgeMaker.java
@@ -1,5 +1,6 @@
 package bridge;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -18,6 +19,20 @@ public class BridgeMaker {
      * @return 입력받은 길이에 해당하는 다리 모양. 위 칸이면 "U", 아래 칸이면 "D"로 표현해야 한다.
      */
     public List<String> makeBridge(int size) {
+        List<String> bridge = new ArrayList<>();
+
+        for(int i=0; i<size; i++){
+            int num = bridgeNumberGenerator.generate();
+            String pattern = numToPatter(num);
+            bridge.add(pattern);
+        }
+
+        return bridge;
+    }
+
+    private String numToPatter(int num) {
+        if(num == 0) return "D";
+        if(num == 1) return "U";
         return null;
     }
 }
