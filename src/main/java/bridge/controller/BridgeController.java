9282c7349c9b1d9b63990575921e84fbc68640b5
dlrghks2090 <dlrghks2090@gmail.com>
2022-11-22 20:21:19 +0900

    feat: 게임 실행 기능

--- src/main/java/bridge/controller/BridgeController.java
+++ src/main/java/bridge/controller/BridgeController.java
@@ -1,17 +1,49 @@
 package bridge.controller;
 
-import bridge.Bridge;
+import bridge.*;
 import bridge.view.InputView;
 import bridge.view.OutputView;
 
+import java.util.List;
+
 public class BridgeController {
     private int bridgeLength;
+    private int playCount = 0;
+    private List<String> makedBridge;
+
     InputView inputView = new InputView();
     OutputView outputView = new OutputView();
+    BridgeGame bridgeGame = new BridgeGame();
+    UserResult userResult;
+    BridgeNumberGenerator bridgeNumberGenerator = new BridgeRandomNumberGenerator();
+    BridgeMaker bridgeMaker = new BridgeMaker(bridgeNumberGenerator);
 
     public void play() {
-        outputView.printStart();
-        Bridge bridge = new Bridge(inputView.readBridgeSize());
+        try {
+            outputView.printStart();
+            Bridge bridge = new Bridge(inputView.readBridgeSize());
+            makedBridge = bridgeMaker.makeBridge(bridgeLength);
+            do {
+                userResult = new UserResult();
+                if (moveBridge(bridge.getBridgeLength())) {
+                    break;
+                }
+            }while(bridgeGame.retry(inputView.readGameRetryCommand()));
+            outputView.printResult();
+        }catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+        }
+    }
 
+    private boolean moveBridge(int bridgeLength) {
+        boolean success = true;
+        for (int i=0; i<bridgeLength; i++) {
+            success = bridgeGame.move(userResult, inputView.readMoving(), makedBridge.get(i));
+            outputView.printMap();
+            if (!success) {
+                return success;
+            }
+        }
+        return success;
     }
 }
