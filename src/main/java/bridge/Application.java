80f5b64de582b7afcf8b47848f9eb92bfa457eec
Basak <chgy2131@naver.com>
2022-11-22 13:13:54 +0900

    feat(BridgeGameController, Application): 다리게임을 총괄 운영하는 컨트롤러 객체 추가
    - 예외 처리 책임은 Apllication 측으로 넘긴다.
    - 기존 Application
    로직을
    BridgeGameController를 사용하여 대체함

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -1,5 +1,6 @@
 package bridge;
 
+import bridge.controller.BridgeGameController;
 import bridge.domain.GameProgress;
 import bridge.domain.GameResult;
 import bridge.view.OutputView;
@@ -8,29 +9,10 @@ import java.util.regex.Pattern;
 
 public class Application {
     public static void main(String[] args) {
+        BridgeGameController controller = new BridgeGameController();
+
         try {
-            // 객체 생성 및 다리 생성
-            BridgeGame game = new BridgeGame();
-
-            // 게임 진행
-            while (true) {
-                // 길 입력받기
-                game.move();
-                // 결과확인
-                GameResult gameResult = game.getResult();
-                BridgeGame.printGameResult(gameResult);
-
-                // 실패시 재시도할지 확인
-                if (gameResult.getProgress() == GameProgress.FAILURE) {
-                    if (!game.retry()) {
-                        break;
-                    }
-                }
-                // 성공시 게임 종료
-                if (gameResult.getProgress() == GameProgress.SUCCESS) {
-                    break;
-                }
-            }
+            controller.playGame();
         }
         catch (IllegalArgumentException e) {
             OutputView.printErrorMessage(e.toString());
---
+++ src/main/java/bridge/controller/BridgeGameController.java
@@ -0,0 +1,42 @@
+package bridge.controller;
+
+import bridge.BridgeGame;
+import bridge.domain.GameProgress;
+import bridge.domain.GameResult;
+
+public class BridgeGameController {
+    private BridgeGame game;
+
+    public BridgeGameController() {
+        // 객체 생성 및 다리 생성
+        game = new BridgeGame();
+    }
+
+    public void playGame() {
+        while (true) {
+            GameResult gameResult = chooseWay();  // 길 선택
+            if(!checkProgressAndContinue(gameResult.getProgress())) {  // 실패시 재시도할지 확인, 성공시 게임 종료
+                break;
+            }
+        }
+    }
+
+    private GameResult chooseWay() {
+        // 길 입력받기
+        game.move();
+        // 결과확인
+        GameResult gameResult = game.getResult();
+        BridgeGame.printGameResult(gameResult);
+
+        return gameResult;
+    }
+
+    private boolean checkProgressAndContinue(GameProgress progress) {
+        // 실패시 재시도할지 확인
+        if (progress == GameProgress.FAILURE && !game.retry()) {
+                return false;
+        }
+        // 성공시 게임 종료
+        return progress != GameProgress.SUCCESS;
+    }
+}
