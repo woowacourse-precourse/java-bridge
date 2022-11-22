5d8679d1c903f354b9810b0157c97dfdaa67ce33
Daeeun Ko <7273322@naver.com>
2022-11-22 18:36:58 +0900

    refactor: change ResultGenerator enum to class

--- src/main/java/bridge/domain/ResultGenerator.java
+++ src/main/java/bridge/domain/ResultGenerator.java
@@ -1,30 +1,23 @@
 package bridge.domain;
 
-public enum ResultGenerator {
+public class ResultGenerator {
 
-    IS_SUCCESS("게임 성공 여부: "),
-    SUCCESS("성공"),
-    FAIL("실패"),
-    TRIAL("총 시도한 횟수: "),
-    NEWLINE("\n"),
-    ;
+    private ResultGenerator(){}
 
-    private final String message;
-
-    ResultGenerator(String message) {
-        this.message = message;
-    }
+    public static final String IS_SUCCESS = "게임 성공 여부: ";
+    public static final String SUCCESS = "성공";
+    public static final String FAIL = "실패";
+    public static final String TRIAL = "총 시도한 횟수: ";
+    public static final String NEWLINE = "\n";
 
     private static String isSuccess(boolean isEnd) {
         if (isEnd) {
-            return SUCCESS.message;
+            return SUCCESS;
         }
-
-        return FAIL.message;
+        return FAIL;
     }
 
     public static String generateResult(boolean isEnd, int trial) {
-        return IS_SUCCESS.message + isSuccess(isEnd) + NEWLINE.message
-                + TRIAL.message + trial;
+        return IS_SUCCESS + isSuccess(isEnd) + NEWLINE + TRIAL + trial;
     }
 }
