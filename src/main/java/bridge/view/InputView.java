0c3a82d7ea5417aef9464cb082b7a3b4b1bec1be
DESKTOP-1ANNTGI\USER <cvc100326@naver.com>
2022-11-22 18:07:58 +0900

    refactor: 주석 삭제 및 변수 이름 변경

--- src/main/java/bridge/view/InputView.java
+++ src/main/java/bridge/view/InputView.java
@@ -2,16 +2,9 @@ package bridge.view;
 
 import camp.nextstep.edu.missionutils.Console;
 
-
-/**
- * 사용자로부터 입력을 받는 역할을 한다.
- */
 public class InputView {
     private static final String INVAILD_INPUT_ERROR = "[ERROR] 잘못된 입력값 입니다.";
 
-    /**
-     * 다리의 길이를 입력받는다.
-     */
     public int readBridgeSize() {
         try {
             int size = Integer.parseInt(Console.readLine());
@@ -21,16 +14,10 @@ public class InputView {
         }
     }
 
-    /**
-     * 사용자가 이동할 칸을 입력받는다.
-     */
     public String readMoving() {
         return Console.readLine();
     }
 
-    /**
-     * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
-     */
     public String readGameCommand() {
         return Console.readLine();
     }
--- src/main/java/bridge/view/OutputView.java
+++ src/main/java/bridge/view/OutputView.java
@@ -1,11 +1,7 @@
 package bridge.view;
 
 import java.util.List;
-import java.util.StringJoiner;
 
-/**
- * 사용자에게 게임 진행 상황과 결과를 출력하는 역할을 한다.
- */
 public class OutputView {
     private static final String START_NOTICE = "다리 건너기 게임을 시작합니다.";
     private static final String BRIDGE_SIZE_INPUT_NOTICE = "다리의 길이를 입력해주세요.";
@@ -25,12 +21,7 @@ public class OutputView {
         }
     }
 
-    /**
-     * 게임의 최종 결과를 정해진 형식에 맞춰 출력한다.
-     * <p>
-     * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
-     */
-    public void printResult(List<String> map, boolean hasWin, int tryCount) {
+    public void printResult(List<String> map, boolean hasWin, int countOfTry) {
         printResultNotice();
         printMap(map);
         String winOrNot = FAIL;
@@ -38,7 +29,7 @@ public class OutputView {
             winOrNot = SUCCESS;
         }
         System.out.printf(WIN_OR_NOT_NOTICE + NEW_LINE, winOrNot);
-        System.out.printf(TOTAL_TRY_COUNT_NOTICE + NEW_LINE, tryCount);
+        System.out.printf(TOTAL_TRY_COUNT_NOTICE + NEW_LINE, countOfTry);
     }
 
     public void printStartNotice() {
