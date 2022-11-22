e1197c411d3172ddceb3cc04cbd8590110edf5aa
jxixeun <jxixeun@gmail.com>
2022-11-22 17:33:23 +0900

    fix: 출력 파라미터 수정

--- src/main/java/bridge/OutputView.java
+++ src/main/java/bridge/OutputView.java
@@ -20,8 +20,8 @@ public class OutputView {
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
     public void printMap(BridgeGame bridgeGame) {
-        printCells(bridgeGame.getUpCells());
-        printCells(bridgeGame.getDownCells());
+        printCells(bridgeGame.getMovingHistory().getUpCells());
+        printCells(bridgeGame.getMovingHistory().getDownCells());
         System.out.println();
     }
 
