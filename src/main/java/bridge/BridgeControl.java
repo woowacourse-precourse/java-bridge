5f30e6a1c400fe5986d7a8662879e6ed15c0981a
leorb159 <ldgsocial159@naver.com>
2022-11-22 15:05:05 +0900

    feat(BridgeControl): BridgeControl 클래스 생성

---
+++ src/main/java/bridge/BridgeControl.java
@@ -0,0 +1,17 @@
+package bridge;
+
+public class BridgeControl {
+
+    public void start() {
+        //게임 시작 메시지 출력
+
+        //다리 길이 입력받기
+
+        //다리 생성
+
+        //게임 시작
+        
+        //결과 출력
+    }
+
+}
