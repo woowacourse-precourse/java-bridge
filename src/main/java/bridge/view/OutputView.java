99b22516eea452b08980953fd60a4d48f46c6a5d
60jong <rudwhd515@gmail.com>
2022-11-22 18:33:20 +0900

    refactor : 콘솔 출력 메시지 enum으로 추출

--- src/main/java/bridge/view/OutputView.java
+++ src/main/java/bridge/view/OutputView.java
@@ -2,26 +2,28 @@ package bridge.view;
 
 import bridge.domain.Player;
 
+import static bridge.constant.ConsoleMessage.*;
+
 /**
  * 사용자에게 게임 진행 상황과 결과를 출력하는 역할을 한다.
  */
 public class OutputView {
 
     public void printGameStart() {
-        System.out.println("다리 건너기 게임을 시작합니다.");
+        System.out.println(START_GAME.getMessage());
         printBlank();
     }
 
     public void printAskForBridgeSize() {
-        System.out.println("다리 길이를 입력해주세요.");
+        System.out.println(ASK_FOR_BRIDGE_SIZE.getMessage());
     }
 
     public void printAskForStep() {
-        System.out.println("이동할 칸을 선택해주세요. (위: U, 아래: D)");
+        System.out.println(ASK_FOR_USER_STEP.getMessage());
     }
 
     public void printAskForRetry() {
-        System.out.println("게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)");
+        System.out.println(ASK_FOR_RETRY.getMessage());
     }
     /**
      * 현재까지 이동한 다리의 상태를 정해진 형식에 맞춰 출력한다.
@@ -41,7 +43,7 @@ public class OutputView {
      */
 
     public void printResult(Player player) {
-        System.out.println("최종 게임 결과");
+        System.out.println(FINAL_RESULT.getMessage());
         printMap(player);
         printGameSuccess(player);
         printTrials(player);
@@ -52,17 +54,18 @@ public class OutputView {
     }
 
     private void printGameSuccess(Player player) {
-        System.out.printf("게임 성공 여부: %s\n",isGameSuccess(player));
+        if (player.isSuccess()) {
+            System.out.println(GAME_SUCCESS.getMessage());
+        }
+
+        if (!player.isSuccess()) {
+            System.out.println(GAME_FAIL.getMessage());
+        }
     }
 
     private void printTrials(Player player) {
-        System.out.printf("총 시도한 횟수: %d\n", player.showTrials());
-    }
-
-    private String isGameSuccess(Player player) {
-        if (player.isSuccess()) {
-            return "성공";
-        }
-        return "실패";
+        System.out.println(
+                String.format(TOTAL_TRIALS.getMessage(), player.showTrials())
+        );
     }
 }
