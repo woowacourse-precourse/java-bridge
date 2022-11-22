33434882cac7575e94eae9775ca2cc10f34d8b33
Ran <lan4250@naver.com>
2022-11-22 15:42:55 +0900

    FEAT/ 최종 결과 출력 메서드 내용 추가

--- src/main/java/bridge/OutputView.java
+++ src/main/java/bridge/OutputView.java
@@ -42,12 +42,24 @@ public class OutputView {
      * <p>
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void printResult(List<String> bridge, boolean successYn) {
+    public void printResult(List<String> bridge, boolean successYn, int gameCnt) {       // 게임 시도 횟수 추가
         System.out.println("최종 게임 결과");
 
         for (String b : bridge) {
             System.out.println(b);
 
         }
+
+        System.out.print("게임 성공 여부: ");
+
+        if(successYn){
+            System.out.println("성공");
+            System.out.println("총 시도한 횟수: " + gameCnt );
+            return;
+        }
+        System.out.println("실패");
+        System.out.println("총 시도한 횟수: "  + gameCnt );
+
+
     }
 }
