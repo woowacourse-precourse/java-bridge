90956076a62489578909d91faf5ab68ed5d35fee
shpark30 <seohyeonpark30.gmail.com>
2022-11-22 10:21:38 +0900

    refactor(Map): 진행 경과 관리 책임 부여
    Player가 게임을 진행하면서 지도에 체크한다는 도메인 시나리오에 따라
    Map 객체가 진행 경과를 관리하도록 책임 부여
    
    기존 책임 : Player
    변경 책임 : Map

--- src/main/java/bridge/domain/Map.java
+++ src/main/java/bridge/domain/Map.java
@@ -1,5 +1,7 @@
 package bridge.domain;
 
+import static org.assertj.core.util.Lists.newArrayList;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -7,43 +9,64 @@ import java.util.List;
  * 현재 진행 상황을 출력하는 로직
  */
 public class Map {
-    List<String> upperPath;
-    List<String> lowerPath;
+    private List<String> passedPath;
+    private List<String> arePaths;
 
     public Map() {
-        upperPath = new ArrayList<>();
-        lowerPath = new ArrayList<>();
+        passedPath = new ArrayList<>();
+        arePaths = new ArrayList<>();
     }
 
     /**
-     * 현재 진행 상황을 업데이트하는 메소드
-     * @param direction: 사용자가 입력 방향(U, D)
-     * @param answer: 정답 방향(U, D)
+     * 게임 진행에 따라 지도(게임 경과)를 업데이트
+     *
+     * @param direction: 이동 방향 (U, D)
+     * @param isPath:    정답 경로 여부
      */
-    public void update(String direction, String answer) {
-        String mark;
-        if (direction.equals(answer)) {
-            mark = Mark.PATH.toString();
-        } else {
-            mark = Mark.BLOCK.toString();
+    public void update(String direction, boolean isPath) {
+        passedPath.add(direction);
+        if (isPath) {
+            arePaths.add(Mark.PATH.toString());
+            return;
         }
-
-        if (direction.equals(Direction.UP.toString())) {
-            upperPath.add(mark);
-            lowerPath.add(Mark.UNKNOWN.toString());
-        }
-        if (direction.equals(Direction.DOWN.toString())) {
-            upperPath.add(Mark.UNKNOWN.toString());
-            lowerPath.add(mark);
+        if (!isPath) {
+            arePaths.add(Mark.BLOCK.toString());
         }
     }
 
     /**
      * 현재 진행상황을 정해진 형식에 맞추어 반환
+     *
      * @return: upperPath와 lowerPath 순으로 담긴 List
      */
     public List<String> getCurrentMap() {
-        return List.of(makePathStringFormat(upperPath), makePathStringFormat(lowerPath));
+        validateSameArrayLength();
+        List<String> upperPath = new ArrayList<>();
+        List<String> lowerPath = new ArrayList<>();
+        for (int i = 0; i < passedPath.size(); i++) {
+            String direction = passedPath.get(i);
+            String isAnswer = arePaths.get(i);
+            if (direction.equals(Direction.UP.toString())) {
+                upperPath.add(isAnswer);
+                lowerPath.add(Mark.UNKNOWN.toString());
+                continue;
+            }
+            if (direction.equals(Direction.DOWN.toString())) {
+                upperPath.add(Mark.UNKNOWN.toString());
+                lowerPath.add(isAnswer);
+            }
+        }
+        return newArrayList(makePathStringFormat(upperPath), makePathStringFormat(lowerPath));
+    }
+
+    public List<String> getPassedPath() {
+        return passedPath;
+    }
+
+    private void validateSameArrayLength() {
+        if (passedPath.size() != arePaths.size()) {
+            throw new IllegalStateException("[ERROR] 진행 경과를 기록한 리스트와 정답을 기록한 리스트의 길이가 다릅니다.");
+        }
     }
 
     private String makePathStringFormat(List<String> path) {
