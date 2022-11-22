1f59031ab45b25cbdfa25081ce15ccca3479513d
JohnPrk <uo3641493@gmail.com>
2022-11-22 14:19:49 +0900

    feat(BridgeGame) : move 메서드 생성(입력한 값과 결과값을 비교한 값을 BridgeResults의 report 메서드 호출하고 비교한 값이 맞다면 인덱스를 하나 올려준다.

--- src/main/java/bridge/BridgeGame.java
+++ src/main/java/bridge/BridgeGame.java
@@ -1,16 +1,34 @@
 package bridge;
 
+import java.util.List;
+
 /**
  * 다리 건너기 게임을 관리하는 클래스
  */
 public class BridgeGame {
+    private static final String RESTART = "R";
+
+    private final List<String> randomBridge;
+    private final BridgeResults bridgeResults;
+
+    public BridgeGame(List<String> randomBridge, BridgeResults bridgeResults) {
+        this.randomBridge = randomBridge;
+        this.bridgeResults = bridgeResults;
+    }
 
     /**
      * 사용자가 칸을 이동할 때 사용하는 메서드
      * <p>
      * 이동을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void move() {
+    public boolean move(BridgeLocationStatus bridgeLocationStatus, BridgeResults bridgeResults, BridgeIndex index) {
+        boolean compareBridgeResult = bridgeLocationStatus.compareBridge(randomBridge.get(index.getBridgeIndex()));
+        bridgeResults.report(bridgeLocationStatus, compareBridgeResult);
+        if(compareBridgeResult) {
+            index.moveForward();
+            return true;
+        }
+        return false;
     }
 
     /**
@@ -18,6 +36,7 @@ public class BridgeGame {
      * <p>
      * 재시작을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void retry() {
+    public boolean retry(boolean command) {
+        return true;
     }
 }
