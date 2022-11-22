f64cc25c0b5e53576915604248fc506783a17eba
gomljo <gomljo0307@naver.com>
2022-11-22 09:29:32 +0900

    feat(GameCommand): 게임 커맨드를 이용하여 재시작 여부를 반환하는 기능 구현

--- src/main/java/bridge/service/constant/GameCommand.java
+++ src/main/java/bridge/service/constant/GameCommand.java
@@ -7,11 +7,30 @@ import java.util.Objects;
 public enum GameCommand {
     Retry("R", true),
     Quit("Q", false);
-    private String gameCommand;
-    private boolean doRetry;
-    GameCommand(String gameCommand, boolean doRetry){
-        this.gameCommand=gameCommand;
-        this.doRetry=doRetry;
+    private final String gameCommand;
+    private final boolean doRetry;
+
+    GameCommand(String gameCommand, boolean doRetry) {
+        this.gameCommand = gameCommand;
+        this.doRetry = doRetry;
     }
 
+    private static boolean isSameCommand(GameCommand gameCommand, String userGameCommand) {
+        return gameCommand.gameCommand.equals(userGameCommand);
+    }
+
+    private static GameCommand findByGameCommand(String userGameCommand) {
+        return Arrays.stream(GameCommand.values())
+                .filter(gameCommand -> GameCommand.isSameCommand(gameCommand, userGameCommand))
+                .findAny()
+                .orElseThrow(NoSuchElementException::new);
+    }
+
+    public static boolean transformToAction(String userGameCommand) {
+        GameCommand gameCommand = GameCommand.findByGameCommand(userGameCommand);
+        if (Objects.isNull(gameCommand)) {
+            throw new NullPointerException();
+        }
+        return gameCommand.doRetry;
+    }
 }
