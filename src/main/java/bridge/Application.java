4e40cd7fb0bc7ed0be7d35e7af1fd76c34a74184
Dabeen Jeong <hello70825@gmail.com>
2022-11-22 22:00:07 +0900

    refactor: try-catch 삭제

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -5,11 +5,7 @@ import bridge.controller.BridgeGameController;
 public class Application {
 
     public static void main(String[] args) {
-        try {
-            BridgeGameController bridgeGameController = new BridgeGameController();
-            bridgeGameController.start();
-        } catch (Exception e) {
-            System.out.println(e.getMessage());
-        }
+        BridgeGameController bridgeGameController = new BridgeGameController();
+        bridgeGameController.start();
     }
 }
