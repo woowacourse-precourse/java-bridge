9a64ad57ebe55e8f44b1282e13621558f6396522
im-shung <seoyoung1113@gmail.com>
2022-11-22 13:39:24 +0900

    feat: :sparkles: 현재 라운드 결과 값이 실패인지 알 수 있는 기능 추가

--- src/main/java/bridge/service/BridgeGame.java
+++ src/main/java/bridge/service/BridgeGame.java
@@ -41,6 +41,10 @@ public class BridgeGame {
         return rounds.size();
     }
 
+    public boolean isCurrentRoundResultFailure() {
+        return rounds.get(rounds.size() - 1).getResult() == RoundResult.FAILURE;
+    }
+
     /**
      * 사용자가 게임을 다시 시도할 때 사용하는 메서드
      * <p>
