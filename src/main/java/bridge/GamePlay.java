3eca87e4bc5ad4e6c983a992f717385f6fb07277
안광민 <creativeduck0518@gmail.com>
2022-11-22 10:47:20 +0900

    feat(GamePlay): 게임 및 변수 초기화 기능 구현

--- src/main/java/bridge/GamePlay.java
+++ src/main/java/bridge/GamePlay.java
@@ -10,4 +10,11 @@ public class GamePlay {
     private int tryCount;
     private final boolean WON = true;
     private final boolean FAILURE = false;
+
+    public void initGame() {
+        inputView = new InputView();
+        outputView = new OutputView();
+        bridgeGame = new BridgeGame();
+        tryCount = 0;
+    }
 }
