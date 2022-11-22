f33ab043853d7b77a0434b0c229ae2320b3d655d
yubin <dbqls2821@gmail.com>
2022-11-22 17:21:56 +0900

    refactor(OutputView): 결과 출력 메소드 수정

--- src/main/java/bridge/io/OutputView.java
+++ src/main/java/bridge/io/OutputView.java
@@ -37,8 +37,7 @@ public class OutputView {
      * <p>
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void printResult(boolean success, int gameCount) {
-        System.out.println(InGameMessage.SUCCESS_OR_FAILURE.getMessage()+ InGameMessage.findMessageBySuccessOrNot(success));
-        System.out.println(InGameMessage.TRY_COUNT.getMessage() + Integer.toString(gameCount));
+    public void printResult(String result) {
+        System.out.println(result);
     }
 }
