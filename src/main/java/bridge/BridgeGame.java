f10f689734da06d1115314377e5ab826dfad23d3
ijung-won <dlwnddnjs96@gmail.com>
2022-11-22 15:51:48 +0900

    docs(README.md): 진행과정 작성 수정

--- src/main/java/bridge/BridgeGame.java
+++ src/main/java/bridge/BridgeGame.java
@@ -1,6 +1,5 @@
 package bridge;
 
-import camp.nextstep.edu.missionutils.Console;
 
 import java.util.ArrayList;
 import java.util.List;
---
+++ src/test/java/bridge/BridgeGameTest.java
@@ -0,0 +1,13 @@
+package bridge;
+
+import org.junit.jupiter.api.Test;
+
+
+class BridgeGameTest {
+    private final BridgeGame bridgeGame = new BridgeGame();
+
+    @Test
+    public void 맞춘_경우(){
+
+    }
+}
