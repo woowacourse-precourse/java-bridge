2300d3b22ffe1cdfa018f1bafb6f9b84b689896d
DevJay1024 <devjay1024@gmail.com>
2022-11-22 11:33:53 +0900

    test: 다리의 안전경로와 사용자의 이동경로의 완전 일치 여부 확인 기능 검증을 위한 테스트 케이스 작성

--- src/test/java/bridge/paths/BridgeTest.java
+++ src/test/java/bridge/paths/BridgeTest.java
@@ -45,5 +45,51 @@ class BridgeTest {
         }
     }
 
+    @Nested
+    class IsCompletedWithTest {
+        @Test
+        void 안전경로가_사용자_경로와_완전히_같을_때_확인() {
+            Bridge bridge = new Bridge(List.of("U", "D", "D"));
+            PlayerPath playerPath = new PlayerPath();
+            playerPath.saveDirection(Direction.UP);
+            playerPath.saveDirection(Direction.DOWN);
+            playerPath.saveDirection(Direction.DOWN);
+
+            assertThat(bridge.canBeSteppedBy(playerPath)).isTrue();
+        }
+
+        @Test
+        void 안전경로가_사용자_경로와_끝부분이_다를_때_확인() {
+            Bridge bridge = new Bridge(List.of("D", "D", "U"));
+            PlayerPath playerPath = new PlayerPath();
+            playerPath.saveDirection(Direction.UP);
+            playerPath.saveDirection(Direction.DOWN);
+            playerPath.saveDirection(Direction.UP);
+
+            assertThat(bridge.canBeSteppedBy(playerPath)).isFalse();
+        }
+
+        @Test
+        void 안전경로가_사용자_경로와_중간부분이_다를_때_확인() {
+            Bridge bridge = new Bridge(List.of("U", "U", "U"));
+            PlayerPath playerPath = new PlayerPath();
+            playerPath.saveDirection(Direction.UP);
+            playerPath.saveDirection(Direction.DOWN);
+            playerPath.saveDirection(Direction.UP);
+
+            assertThat(bridge.canBeSteppedBy(playerPath)).isFalse();
+        }
+
+        @Test
+        void 안전경로가_사용자_경로와_첫부분이_다를_때_확인() {
+            Bridge bridge = new Bridge(List.of("D", "D", "D"));
+            PlayerPath playerPath = new PlayerPath();
+            playerPath.saveDirection(Direction.UP);
+            playerPath.saveDirection(Direction.DOWN);
+            playerPath.saveDirection(Direction.DOWN);
+
+            assertThat(bridge.canBeSteppedBy(playerPath)).isFalse();
+        }
+    }
 
 }
