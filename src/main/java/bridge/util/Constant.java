96c3df5a38ae0c5c8aa14cfd64b6552f5e31eb5c
SEONGJOON KIM <jobcho6320@naver.com>
2022-11-22 23:02:36 +0900

    refactor: 요구 사항 반영

--- src/main/java/bridge/util/Constant.java
+++ src/main/java/bridge/util/Constant.java
@@ -19,7 +19,7 @@ public class Constant {
     public static final StringBuilder upsideBridge = new StringBuilder("[]");
     public static final StringBuilder downsideBridge = new StringBuilder("[]");
 
-    public static final String GAME_START_MESSAGE = "다리 건너기 게임을 시작합니다";
+    public static final String GAME_START_MESSAGE = "다리 건너기 게임을 시작합니다.";
     public static final String INPUT_BRIDGE_SIZE_MESSAGE = "다리의 길이를 입력해주세요.";
     public static final String MOVING_SELECT_MESSAGE = "이동할 칸을 선택해주세요. (위: U, 아래: D)";
     public static final String GAME_RETRY_MESSAGE = "게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)";
