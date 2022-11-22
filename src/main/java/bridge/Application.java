11271750162df5fde5cd89e212319120fab0f80a
mgOh <issac_oh@naver.com>
2022-11-22 13:23:55 +0900

    feat: 입력받은 길이의 다리를 생성하는 기능

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -1,8 +1,16 @@
 package bridge;
 
-public class Application {
+import java.util.List;
 
+public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        new OutputView().printGameStart();
+        List<String> bridge = printAndReadBridgeSize();
+    }
+    /** 입력받은 길이의 다리를 생성하는 기능 */
+    static List<String> printAndReadBridgeSize(){
+        new OutputView().printGetBridgeSize();
+        return new BridgeMaker(new BridgeRandomNumberGenerator()).makeBridge(new InputView().readBridgeSize());
     }
 }
