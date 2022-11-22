369eaee5e2ef2d21a0c204f7abd476c261bc1031
jeongseungchan <jschan0911@naver.com>
2022-11-22 21:21:59 +0900

    printResult로 출력할 경우 양 끝을 대괄호로 묶는 기능 추가

--- src/main/java/bridge/OutputView.java
+++ src/main/java/bridge/OutputView.java
@@ -37,6 +37,10 @@ public class OutputView {
      * <p>
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void printResult() {
+    public void printResult(String inputMoving) {
+        List<StringBuilder> result = bridgeGame.getResult(inputMoving);
+        for (int i = 0; i < 2; i++) {
+            System.out.println("[ " + result.get(i) + " ]");
+        }
     }
 }
