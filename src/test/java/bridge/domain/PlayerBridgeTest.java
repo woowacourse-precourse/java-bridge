61f4eeb701ee1991b5a45c7f38c8e6008f1038f8
임원정 <oddish0513@gmail.com>
2022-11-22 19:12:42 +0900

    refactor(bridge): 테스트 추가

--- src/test/java/bridge/domain/PlayerBridgeTest.java
+++ src/test/java/bridge/domain/PlayerBridgeTest.java
@@ -1,7 +1,32 @@
 package bridge.domain;
 
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.*;
 
 class PlayerBridgeTest {
 
+
+	@ValueSource(strings = {" U", "d", "3", " "})
+	@ParameterizedTest
+	void 예외처리(String input) {
+		PlayerBridge playerBridge = new PlayerBridge();
+
+		assertThatThrownBy(() -> playerBridge.add(input))
+				.isInstanceOf(IllegalArgumentException.class);
+	}
+
+	@Test
+	void getLastValue_테스트() {
+		String answer = "U";
+		PlayerBridge playerBridge = new PlayerBridge();
+		playerBridge.add("D");
+		playerBridge.add("D");
+		playerBridge.add(answer);
+
+		assertEquals(answer, playerBridge.getLastValue());
+	}
 }
