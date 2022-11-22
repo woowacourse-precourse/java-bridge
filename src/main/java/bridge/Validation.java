b738042b3a5c753d67d84b477253885010f6df5a
BHC-Chicken <tij05078@gmail.com>
2022-11-22 13:24:15 +0900

    feat : 종료 여부 입력 값 검증
    R또는 Q값 이외의 값 입력 받을 시 예외처리한다.

--- src/main/java/bridge/Validation.java
+++ src/main/java/bridge/Validation.java
@@ -9,11 +9,19 @@ public class Validation {
         }
         throw new IllegalArgumentException("다리의 길이는 3이상 20이하 입니다.");
     }
-    
+
     public boolean validationMoving(String exit) {
         if (!(exit.equals("U") || exit.equals("D"))) {
             throw new IllegalArgumentException("U또는 D만 입력해주세요.");
         }
+        return true;
+    }
+
+    public boolean validationIsExitCommand(String exit) {
+        if (!(exit.equals("R") || exit.equals("Q"))) {
+            throw new IllegalArgumentException("R또는 Q만 입력해주세요.");
+        }
+
         return true;
     }
 }
