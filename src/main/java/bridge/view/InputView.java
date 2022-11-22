643d3df728114cd6a46319a0855464f72ff3df17
aak2075 <35948985+aak2075@users.noreply.github.com>
2022-11-22 18:13:00 +0900

    refactor(bridge) : 상수명 변경
    상수명을 조금 더 명확하게 표현

--- src/main/java/bridge/view/InputView.java
+++ src/main/java/bridge/view/InputView.java
@@ -8,12 +8,12 @@ import camp.nextstep.edu.missionutils.Console;
  * 사용자로부터 입력을 받는 역할을 한다.
  */
 public class InputView {
-    private static final String READ_SIZE = "다리의 길이를 입력해주세요.";
-    private static final String SIZE_ERROR = "[ERROR] 다리 길이는 3부터 20 사이의 숫자여야 합니다.";
-    private static final String READ_MOVING = "이동할 칸을 선택해주세요. (위: U, 아래: D)";
-    private static final String MOVING_ERROR = "[ERROR] 이동은 U와 D로만 가능합니다.";
-    private static final String READ_COMMAND = "게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)";
-    private static final String GAME_COMMAND_ERROR = "[ERROR] 게임 재시작과 종료는 R과 Q로만 가능합니다";
+    private static final String READ_SIZE_MESSAGE = "다리의 길이를 입력해주세요.";
+    private static final String SIZE_ERROR_MESSAGE = "[ERROR] 다리 길이는 3부터 20 사이의 숫자여야 합니다.";
+    private static final String READ_MOVING_MESSAGE = "이동할 칸을 선택해주세요. (위: U, 아래: D)";
+    private static final String MOVING_ERROR_MESSAGE = "[ERROR] 이동은 U와 D로만 가능합니다.";
+    private static final String READ_COMMAND_MESSAGE = "게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)";
+    private static final String GAME_COMMAND_ERROR_MESSAGE = "[ERROR] 게임 재시작과 종료는 R과 Q로만 가능합니다";
     private static final int MIN_SIZE = 3;
     private static final int MAX_SIZE = 20;
 
@@ -21,7 +21,7 @@ public class InputView {
      * 다리의 길이를 입력받는다.
      */
     public int readBridgeSize() {
-        System.out.println(READ_SIZE);
+        System.out.println(READ_SIZE_MESSAGE);
         try {
             int size = stringToInteger(Console.readLine());
             validateSize(size);
@@ -36,13 +36,13 @@ public class InputView {
         try {
             return Integer.parseInt(input);
         } catch (NumberFormatException e) {
-            throw new IllegalArgumentException(SIZE_ERROR);
+            throw new IllegalArgumentException(SIZE_ERROR_MESSAGE);
         }
     }
 
     private void validateSize(int size) {
         if (size < MIN_SIZE || size > MAX_SIZE) {
-            throw new IllegalArgumentException(SIZE_ERROR);
+            throw new IllegalArgumentException(SIZE_ERROR_MESSAGE);
         }
     }
 
@@ -50,11 +50,11 @@ public class InputView {
      * 사용자가 이동할 칸을 입력받는다.
      */
     public Moving readMoving() {
-        System.out.println(READ_MOVING);
+        System.out.println(READ_MOVING_MESSAGE);
         try {
             return Moving.valueOf(Console.readLine());
         } catch (IllegalArgumentException e) {
-            System.out.println(MOVING_ERROR);
+            System.out.println(MOVING_ERROR_MESSAGE);
             return readMoving();
         }
     }
@@ -63,11 +63,11 @@ public class InputView {
      * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
      */
     public GameCommand readGameCommand() {
-        System.out.println(READ_COMMAND);
+        System.out.println(READ_COMMAND_MESSAGE);
         try {
             return GameCommand.valueOf(Console.readLine());
         } catch (IllegalArgumentException e) {
-            System.out.println(GAME_COMMAND_ERROR);
+            System.out.println(GAME_COMMAND_ERROR_MESSAGE);
             return readGameCommand();
         }
     }
