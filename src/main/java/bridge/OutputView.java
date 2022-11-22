2a6135eb5f4ec4670701448f98d83b4f174585aa
liarreez <liarreez@gmail.com>
2022-11-22 20:07:09 +0900

    refactor(OutputView) : 불필요한 주석 코드 삭제

--- src/main/java/bridge/OutputView.java
+++ src/main/java/bridge/OutputView.java
@@ -69,59 +69,11 @@ public class OutputView {
         return "게임 성공 여부: 성공";
     }
 
-//    public static void printUpperBridge(List<String> bridge, List<String> userMoving) {
-//        System.out.print("[");
-//        for (int turn = 0 ; turn < userMoving.size(); turn++) {
-//            printUpperMoveResult(bridge, userMoving, turn);
-//            printSeparateOrEndBridge(userMoving, turn);
-//        }
-//    }
-//
-//    public static void printLowerBridge(List<String> bridge, List<String> userMoving){
-//        System.out.print("[");
-//        for (int turn = 0 ; turn < userMoving.size(); turn++) {
-//            printLowerMoveResult(bridge, userMoving, turn);
-//            printSeparateOrEndBridge(userMoving, turn);
-//        }
-//    }
-//
-//    public static void printUpperMoveResult(List<String> bridge, List<String> userMoving, int turn) {
-//        if (userMoving.get(turn).equals("U")) {
-//            if(userMoving.get(turn).equals(bridge.get(turn))){
-//                System.out.print(" O");
-//            }
-//            BridgeGame.failMove(bridge, userMoving, turn);
-//        }
-//        if (userMoving.get(turn).equals("D")) {
-//            System.out.print(" ");
-//        }
-//    }
-//    public static void printLowerMoveResult(List<String> bridge, List<String> userMoving, int turn) {
-//        if (userMoving.get(turn).equals("D")) {
-//            if(userMoving.get(turn).equals(bridge.get(turn))){
-//                System.out.print(" O");
-//            }
-//            BridgeGame.failMove(bridge, userMoving, turn);
-//        }
-//        if (userMoving.get(turn).equals("U")) {
-//            System.out.print(" ");
-//        }
-//    }
-//    public static void printSeparateOrEndBridge(List<String> userMoving, int turn){
-//        if (turn == userMoving.size()-1) {
-//            System.out.println(" ]");
-//        }
-//        if (turn != userMoving.size()-1) {
-//            System.out.print(" |");
-//        }
-//    }
-
     /**
      * 게임의 최종 결과를 정해진 형식에 맞춰 출력한다.
      * <p>
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-
     public static void printResult(List<String> bridge, List<String> userMoving, int failCheck, int playCount) {
         System.out.println("최종 게임 결과");
         System.out.println(upperBridge(bridge,userMoving));
