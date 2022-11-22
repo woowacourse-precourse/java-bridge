d6e9d29581fd23acd12bbcdf16996e1e3e70abe4
Hyeonjun Park <phjppo0918@gmail.com>
2022-11-22 13:03:56 +0900

    test(BridgeMapTest) : 위 아래로 번갈아가며 3번 씩 이동 성공시

--- src/test/java/bridge/BridgeMapTest.java
+++ src/test/java/bridge/BridgeMapTest.java
@@ -11,8 +11,27 @@ class BridgeMapTest {
     void initAndToString() throws Exception {
         //given
         BridgeMap bridgeMap = new BridgeMap();
-        String expect = "[  ]\n[  ]\n";
+        String expect = "[  ]\n" +
+                        "[  ]\n";
         //when
+        String result = bridgeMap.toString();
+        //then
+        assertThat(result).isEqualTo(expect);
+    }
+    @Test
+    @DisplayName("위 아래로 번갈아가며 3번 씩 이동 성공시")
+    void upDownRepeat3times() throws Exception {
+        //given
+        BridgeMap bridgeMap = new BridgeMap();
+        String expect = "[ O |   | O |   | O |   ]\n" +
+                        "[   | O |   | O |   | O ]\n";
+        //when
+        bridgeMap.moveSuccess(Command.UP);
+        bridgeMap.moveSuccess(Command.DOWN);
+        bridgeMap.moveSuccess(Command.UP);
+        bridgeMap.moveSuccess(Command.DOWN);
+        bridgeMap.moveSuccess(Command.UP);
+        bridgeMap.moveSuccess(Command.DOWN);
         String result = bridgeMap.toString();
         //then
         assertThat(result).isEqualTo(expect);
