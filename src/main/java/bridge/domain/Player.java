74dd8d8377c403ebb4846f1b54538ed0639e91bb
code-tree <dltjdqls1200@naver.com>
2022-11-22 16:02:31 +0900

    style: 플레이어의 moveResults final로 선언

--- src/main/java/bridge/domain/Player.java
+++ src/main/java/bridge/domain/Player.java
@@ -6,7 +6,7 @@ import java.util.List;
 import java.util.Map;
 
 public class Player {
-    private Map<Move, List<MoveResult>> moveResults = new EnumMap<>(Move.class);
+    private final Map<Move, List<MoveResult>> moveResults = new EnumMap<>(Move.class);
     private int moveDistance;
     private int lifeCount;
 
