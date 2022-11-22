b54990673dd17c19bfc1c60f493b92dc0cc15858
dsaf2007 <dsaf2007@naver.com>
2022-11-22 20:58:41 +0900

    feat : 게임 실패시 종료와 실패 이후 재시도 하여 성공하는 경우에 대한 테스트 추가 게임 실패 테스트를 통해 이동 명령 수행과, 수행 후 결과에 대한 테스트 동시에 진행 게임 실패 테스트와 실패 후 재시도 성공을 수행 하며 R Q command 입력에 대한 테스트

--- src/test/java/bridge/ApplicationTest.java
+++ src/test/java/bridge/ApplicationTest.java
@@ -22,16 +22,44 @@ class ApplicationTest extends NsTest {
     }
 
     @Test
+    void 실패_후_종료_테스트() {
+        assertRandomNumberInRangeTest(() -> {
+            run("3", "U", "U", "Q");
+            assertThat(output()).contains(
+                    "최종 게임 결과",
+                    "[ O | X ]",
+                    "[   |   ]",
+                    "게임 성공 여부: 실패",
+                    "총 시도한 횟수: 1");
+        }, 1, 0, 1);
+    }
+
+    @Test
+    void 실패_후_재시도_성공_테스트(){
+        assertRandomNumberInRangeTest(() -> {
+            run("3", "U", "U","R", "U", "D","U");
+            assertThat(output()).contains(
+                    "게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)",
+                    "[ O | X ]",
+                    "[   |   ]",
+                    "최종 게임 결과",
+                    "[ O |   | O ]",
+                    "[   | O |   ]",
+                    "게임 성공 여부: 성공",
+                    "총 시도한 횟수: 2");
+        }, 1, 0, 1);
+    }
+
+    @Test
     void 기능_테스트() {
         assertRandomNumberInRangeTest(() -> {
             run("3", "U", "D", "U");
             assertThat(output()).contains(
-                "최종 게임 결과",
-                "[ O |   | O ]",
-                "[   | O |   ]",
-                "게임 성공 여부: 성공",
-                "총 시도한 횟수: 1"
-            );
+                    "최종 게임 결과",
+                    "[ O |   | O ]",
+                    "[   | O |   ]",
+                    "게임 성공 여부: 성공",
+                    "총 시도한 횟수: 1");
 
             int upSideIndex = output().indexOf("[ O |   | O ]");
             int downSideIndex = output().indexOf("[   | O |   ]");
@@ -49,7 +77,7 @@ class ApplicationTest extends NsTest {
 
     @Override
     protected void runMain() {
-        Application.main(new String[]{});
+        Application.main(new String[] {});
     }
 
     static class TestNumberGenerator implements BridgeNumberGenerator {
