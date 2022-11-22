bf35acecad78be1f595b203ee9df8de6048fe1ea
jeongyeonKimm <wjddus9132@gmail.com>
2022-11-22 23:16:12 +0900

    feat: 이동할 칸 물어보는 메시지 출력

--- src/main/java/bridge/OutputView.java
+++ src/main/java/bridge/OutputView.java
@@ -9,18 +9,23 @@ public class OutputView {
 
     private static final String START_MESSAGE = "다리 건너기 게임을 시작합니다.";
     private static final String INPUT_BRIDGE_SIZE = "다리의 길이를 입력해주세요.";
+    private static final String INPUT_MOVING_DIRECTION = "이동할 칸을 선택해주세요. (위: U, 아래: D)";
     private static final String RESTART_MESSAGE = "게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)";
     private static final String END_MESSAGE = "최종 게임 결과";
     private static final String SUCCESS_OR_FAILURE = "게임 성공 여부: %s";
     private static final String COUNT_MESSAGE = "총 시도한 횟수: %d";
-    private static final String EMPTY_LINE = "";
 
     public void printStart() {
         System.out.println(START_MESSAGE);
+        System.out.println();
     }
 
     public void printAskLength() {
         System.out.println(INPUT_BRIDGE_SIZE);
+    }
+
+    public void printAskDirection() {
+        System.out.println(INPUT_MOVING_DIRECTION);
     }
 
     public void printRestart() {
@@ -40,7 +45,6 @@ public class OutputView {
         System.out.printf(COUNT_MESSAGE, count);
         System.out.println();
     }
-
 
     /**
      * 현재까지 이동한 다리의 상태를 정해진 형식에 맞춰 출력한다.
