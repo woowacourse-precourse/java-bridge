b79fdecb8bf2939c63ef385dac8a2b6d7512e940
aossuper7 <aossuper7@naver.com>
2022-11-22 12:05:29 +0900

    refactor: 0 - 아래칸, 1 - 위 칸으로 수정

--- src/main/java/bridge/Model/BridgeMap.java
+++ src/main/java/bridge/Model/BridgeMap.java
@@ -6,18 +6,18 @@ import java.util.List;
 
 public class BridgeMap {
 
-    private static final int UP = 0;
-    private static final int DOWN = 1;
+    private static final int UP = 1;
+    private static final int DOWN = 0;
     private final List<Bridge> bridge = new ArrayList<>();
 
     public BridgeMap() {
-        bridge.add(new Bridge()); // 윗 다리
         bridge.add(new Bridge()); // 아래 다리
+        bridge.add(new Bridge()); // 윗 다리
     }
 
     private void saveState(final String up, final String down) {
-        bridge.get(UP).saveCrossedBridge(up);
-        bridge.get(DOWN).saveCrossedBridge(down);
+        bridge.get(DOWN).saveCrossedBridge(up);
+        bridge.get(UP).saveCrossedBridge(down);
     }
 
     public boolean isCrossUp(final Bridge rightBridge, final int position) {
