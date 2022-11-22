b386301af3de48c64c349ba3996f5963cd160bda
Joonsoo Kim <kjsoo1901@gmail.com>
2022-11-22 13:55:25 +0900

    refactor: InputView 메시지 상수화

--- src/main/java/bridge/view/InputView.java
+++ src/main/java/bridge/view/InputView.java
@@ -1,6 +1,9 @@
 package bridge.view;
 
 import camp.nextstep.edu.missionutils.Console;
+
+import static bridge.utils.Constants.*;
+
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
  */
@@ -10,6 +13,7 @@ public class InputView {
      * 다리의 길이를 입력받는다.
      */
     public int readBridgeSize() {
+        System.out.println(MESSAGE_INPUT_BRIDGE_SIZE);
         return Integer.valueOf(Console.readLine());
     }
 
@@ -17,6 +21,7 @@ public class InputView {
      * 사용자가 이동할 칸을 입력받는다.
      */
     public String readMoving() {
+        System.out.println(MESSAGE_INPUT_PLAYER_MOVE);
         return Console.readLine();
     }
 
@@ -24,6 +29,7 @@ public class InputView {
      * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
      */
     public String readGameCommand() {
+        System.out.println(MESSAGE_INPUT_PLAYER_RETRY);
         return Console.readLine();
     }
 }
