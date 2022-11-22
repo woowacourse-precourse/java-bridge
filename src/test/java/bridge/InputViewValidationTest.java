7fc31a42b2a6d72962f99ba3c0e112f05fbdd583
HGB <hsw8147@gmail.com>
2022-11-22 22:33:15 +0900

    test: Refactor InputviewTest
    InputView 내용이 바뀜에 따라 테스트 수정

--- src/test/java/bridge/InputViewValidationTest.java
+++ src/test/java/bridge/InputViewValidationTest.java
@@ -5,7 +5,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-public class InputViewTest {
+public class InputViewValidationTest {
     @DisplayName("유효성 검사 테스트")
     @Test
     public void validationTest(){
