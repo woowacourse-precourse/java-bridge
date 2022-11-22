02655c829ab2b8d7dec9362a3195e89c3b6377d7
beombu <sunil1369@bible.ac.kr>
2022-11-22 15:20:46 +0900

    Feat(Controller) : 게임 컨트롤러를 통해 view와 model 연결

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -3,6 +3,11 @@ package bridge;
 public class Application {
 
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        try {
+            BridgeGameController bridgeGameController = new BridgeGameController();
+            bridgeGameController.play();
+        }catch (IllegalArgumentException e){
+            System.out.println(e.getMessage());
+        }
     }
 }
---
+++ src/main/java/bridge/BridgeGameController.java
@@ -0,0 +1,45 @@
+package bridge;
+
+import java.util.List;
+
+public class BridgeGameController {
+    private static final InputView inputView = new InputView();
+    private static final OutputView outputView = new OutputView();
+
+    public BridgeGameController() {
+    }
+
+    public void play(){
+        outputView.startGameMessage();
+        int size = inputView.readBridgeSize();
+        BridgeMaker bridgeMaker = new BridgeMaker(new BridgeRandomNumberGenerator());
+        List<String> bridge = bridgeMaker.makeBridge(size);
+        BridgeGame bridgeGame = new BridgeGame(bridge);
+
+        moveBridge(bridgeGame);
+        restart(bridgeGame);
+
+        outputView.printResult(bridgeGame.getTopResult(), bridgeGame.getBottomResult(), bridgeGame.successOrFailure(), bridgeGame.getTotalPlayCnt());
+    }
+
+    public void moveBridge(BridgeGame bridgeGame){
+        for(int i = 0; i < bridgeGame.getBridge().size(); i++){
+            boolean success = bridgeGame.move(inputView.readMoving(), i);
+            outputView.printMap(bridgeGame.getTopResult(), bridgeGame.getBottomResult());
+            if(!success){//종료
+                return;
+            }
+        }
+    }
+
+    public void restart(BridgeGame bridgeGame){
+        if(bridgeGame.successOrFailure() == GameResult.SUCCESS){
+            return;
+        }
+        String restartCommand = inputView.readGameCommand();
+        if(restartCommand.equals("R")){
+            bridgeGame.retry();
+            moveBridge(bridgeGame);
+        }
+    }
+}
