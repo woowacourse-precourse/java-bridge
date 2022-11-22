aa89d83653fe8bbb126ff46cd753fed6a9136fc9
lacram <chemin9898@gmail.com>
2022-11-22 21:15:36 +0900

    refactor: 잘못된 값을 입력할 경우 에러를 발생시키고 그 부분부터 입력을 다시 받도록 수정

--- src/main/java/bridge/BridgeGame.java
+++ src/main/java/bridge/BridgeGame.java
@@ -2,8 +2,6 @@ package bridge;
 
 import java.util.List;
 
-import static bridge.BridgeProgram.runBridgeGame;
-
 /**
  * 다리 건너기 게임을 관리하는 클래스
  */
@@ -14,7 +12,7 @@ public class BridgeGame {
      * <p>
      * 이동을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public static void move(List<String> moved, String moving) {
+    public void move(List<String> moved, String moving) {
         moved.add(moving);
     }
 
@@ -23,24 +21,24 @@ public class BridgeGame {
      * <p>
      * 재시작을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public static void retry(List<String> bridge, int tryCount) {
-        runBridgeGame(bridge, tryCount+1);
+    public void retry(List<String> moved) {
+        moved.clear();
     }
 
     // 다리를 전부 건넜는지 확인하는 기능
-    public static boolean isArrivedDestination(List<String> bridge, List<String> moved) {
+    public boolean isSucceeded(List<String> bridge, List<String> moved) {
         int idx = moved.size() - 1;
         return (bridge.size() == moved.size()) && (moved.get(idx).equals(bridge.get(idx)));
     }
 
     // 다리를 건너다 실패했는지 확인하는 기능
-    public static boolean isFailed(List<String> bridge, List<String> moved) {
+    public boolean isFailed(List<String> bridge, List<String> moved) {
         int idx = moved.size() - 1;
         return !moved.get(idx).equals(bridge.get(idx));
     }
 
     // 게임이 끝났는지 확인하는 기능
-    public static boolean isFinished(List<String> bridge, List<String> moved) {
-        return isArrivedDestination(bridge, moved) || isFailed(bridge, moved);
+    public boolean isFinished(List<String> bridge, List<String> moved) {
+        return isSucceeded(bridge, moved) || isFailed(bridge, moved);
     }
 }
--- src/main/java/bridge/BridgeProgram.java
+++ src/main/java/bridge/BridgeProgram.java
@@ -3,44 +3,83 @@ package bridge;
 import java.util.ArrayList;
 import java.util.List;
 
-import static bridge.BridgeGame.*;
-import static bridge.InputView.*;
-import static bridge.OutputView.printMap;
-import static bridge.OutputView.printResult;
-
 public class BridgeProgram {
 
-    // 전체 프로그램을 가동하는 기능
-    public static void runBridgeProgram() {
-        System.out.println("다리 건너기 게임을 시작합니다.");
-        int bridgeSize = readBridgeSize();
-        BridgeMaker bridgeMaker = new BridgeMaker(new BridgeRandomNumberGenerator());
-        List<String> bridge = bridgeMaker.makeBridge(bridgeSize);
+    private final BridgeGame bridgeGame;
+    private final InputView inputView;
+    private final OutputView outputView;
 
-        runBridgeGame(bridge,1);
+    public BridgeProgram(BridgeGame bridgeGame, InputView inputView, OutputView outputView) {
+        this.bridgeGame = bridgeGame;
+        this.inputView = inputView;
+        this.outputView = outputView;
     }
-    
-    public static void runBridgeGame(List<String> bridge, int tryCount) {
+
+    // 전체 프로그램을 가동하는 기능
+    public void runBridgeProgram() {
+        System.out.println("다리 건너기 게임을 시작합니다.\n");
+        BridgeMaker bridgeMaker = new BridgeMaker(new BridgeRandomNumberGenerator());
+
+        runBridgeGame(bridgeMaker.makeBridge(getBridgeSize()));
+    }
+
+    public void runBridgeGame(List<String> bridge) {
+        int tryCount = 0;
         List<String> moved = new ArrayList<>();
         do {
-            String moving = readMoving();
-            move(moved, moving);
-            printMap(bridge, moved);
-        } while (!isFinished(bridge, moved));
-
-        if (!canRestartWhenFailed(bridge, moved, tryCount)) {
-            printResult(bridge, moved, isArrivedDestination(bridge, moved) ,tryCount);
-        }
+            String moving = getMoving();
+            bridgeGame.move(moved, moving);
+            outputView.printMap(bridge, moved);
+            tryCount = getTryCount(bridge, tryCount, moved);
+        } while (!bridgeGame.isFinished(bridge, moved) || wantRetry(bridge, moved));
+        outputView.printResult(bridge, moved, tryCount);
     }
 
-    public static boolean canRestartWhenFailed(List<String> bridge, List<String> moved, int tryCount) {
-        if (isFailed(bridge, moved)) {
-            String gameCommand = readGameCommand();
+    private int getTryCount(List<String> bridge, int tryCount, List<String> moved) {
+        if (bridgeGame.isFinished(bridge, moved)) {
+            tryCount++;
+        }
+        return tryCount;
+    }
+
+    private boolean wantRetry(List<String> bridge, List<String> moved) {
+        if (bridgeGame.isFailed(bridge, moved)) {
+            String gameCommand = getGameCommand();
             if (gameCommand.equals("R")) {
-                retry(bridge, tryCount);
+                bridgeGame.retry(moved); // moved를 초기화, tryCount++;
                 return true;
             }
         }
         return false;
+    }
+
+    private int getBridgeSize() {
+        while (true) {
+            try {
+                return inputView.readBridgeSize();
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            }
+        }
+    }
+
+    private String getMoving() {
+        while (true) {
+            try {
+                return inputView.readMoving();
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            }
+        }
+    }
+
+    private String getGameCommand() {
+        while (true) {
+            try {
+                return inputView.readGameCommand();
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            }
+        }
     }
 }
