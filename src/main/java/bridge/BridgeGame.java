bdeb284e5c96070a13efbb099c2de3b00c4ae8de
sunghyuki <sunghyuk1609@gmail.com>
2022-11-22 20:14:20 +0900

    feat : 게임 시작 기능 추가

--- src/main/java/bridge/BridgeGame.java
+++ src/main/java/bridge/BridgeGame.java
@@ -1,16 +1,46 @@
 package bridge;
 
+import bridge.view.InputView;
+
+import static bridge.view.InputView.D_INPUT_CLIPPED_WORD;
+import static bridge.view.InputView.U_INPUT_CLIPPED_WORD;
+
 /**
  * 다리 건너기 게임을 관리하는 클래스
  */
 public class BridgeGame {
-
     /**
      * 사용자가 칸을 이동할 때 사용하는 메서드
      * <p>
      * 이동을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void move() {
+    public void move(Bridge bridge, PlayerStatus playerStatus) {
+        String direction = playerStatus.getDirection();
+        boolean isMatch = playerStatus.isMatch();
+
+        if (direction.equals(U_INPUT_CLIPPED_WORD)) {
+            moveUpper(bridge, isMatch);
+            return;
+        }
+        if (direction.equals(D_INPUT_CLIPPED_WORD)) {
+            moveLower(bridge, isMatch);
+        }
+    }
+
+    private void moveUpper(Bridge bridge, boolean isMatch) {
+        if (isMatch) {
+            bridge.moveUpperBridge();
+            return;
+        }
+        bridge.cannotMoveUpperBridge();
+    }
+
+    private void moveLower(Bridge bridge, boolean isMatch) {
+        if (isMatch) {
+            bridge.moveLowerBridge();
+            return;
+        }
+        bridge.cannotMoveLowerBridge();
     }
 
     /**
@@ -18,6 +48,7 @@ public class BridgeGame {
      * <p>
      * 재시작을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void retry() {
+    public boolean retry(final String command) {
+        return command.equals(InputView.Q_INPUT_CLIPPED_WORD);
     }
 }
--- src/main/java/bridge/BridgeGameStarter.java
+++ src/main/java/bridge/BridgeGameStarter.java
@@ -1,5 +1,10 @@
 package bridge;
 
+import bridge.view.utils.ConsoleUtils;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class BridgeGameStarter {
     private BridgeGame bridgeGame;
     private BridgeMaker bridgeMaker;
@@ -10,6 +15,94 @@ public class BridgeGameStarter {
     }
 
     public void start() {
+        List<String> bridge = bridgeMaker.makeBridge(inputBridgeSize());
+        int tryCount = 0;
+        while (true) {
+            tryCount++;
+            Bridge userBridge = new Bridge(new ArrayList<>(), new ArrayList<>());
+            PlayerStatus playerStatus = moveBridge(bridge, userBridge);
+            if(isFinishCondition(tryCount, userBridge, playerStatus)) {
+                return;
+            }
+        }
+    }
 
+    public PlayerStatus moveBridge(List<String> bridge, Bridge userBridge) {
+        PlayerStatus playerStatus = new PlayerStatus();
+
+        for (String currentDirection : bridge) {
+            playerStatus = move(userBridge, playerStatus, currentDirection);
+            if (!playerStatus.isMatch()) {
+                break;
+            }
+        }
+        return playerStatus;
+    }
+
+    private PlayerStatus move(Bridge userBridge, PlayerStatus playerStatus, String currentDirection) {
+        playerStatus = isMatchWithBridge(currentDirection);
+        bridgeGame.move(userBridge, playerStatus);
+        ConsoleUtils.printBridge(userBridge);
+
+        return playerStatus;
+    }
+
+    private PlayerStatus isMatchWithBridge(String currentDirection) {
+        String nextDirection = inputmoving();
+
+        if (currentDirection.equals(nextDirection)) {
+            return new PlayerStatus(nextDirection, true);
+        }
+
+        return new PlayerStatus(nextDirection, false);
+    }
+
+    public boolean isFinishCondition(int tryCount, Bridge userBridge, PlayerStatus playerStatus) {
+        return finishWithWin(tryCount, userBridge, playerStatus) || finishWithLose(tryCount, userBridge, playerStatus);
+    }
+
+    private boolean finishWithWin(int tryCount, Bridge userBridge, PlayerStatus playerStatus) {
+        if (playerStatus.isMatch()) {
+            ConsoleUtils.printGameResult(userBridge, tryCount, playerStatus);
+            return true;
+        }
+        return false;
+    }
+
+    private boolean finishWithLose(int tryCount, Bridge userBridge, PlayerStatus playerStatus) {
+        String gameCommand = inputGameCommand();
+
+        if (bridgeGame.retry(gameCommand)) {
+            ConsoleUtils.printGameResult(userBridge, tryCount, playerStatus);
+            return true;
+        }
+        return false;
+    }
+
+    private static int inputBridgeSize() {
+        try {
+            return ConsoleUtils.inputBridgeSize();
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+            return inputBridgeSize();
+        }
+    }
+
+    private static String inputmoving() {
+        try {
+            return ConsoleUtils.inputMoving();
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+            return inputmoving();
+        }
+    }
+
+    private static String inputGameCommand() {
+        try {
+            return ConsoleUtils.inputGameCommand();
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+            return inputGameCommand();
+        }
     }
 }
