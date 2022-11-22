ae17a278de0fbf185636911c3a9cd3af0a8909e1
WhalesBob <ndw8200@naver.com>
2022-11-22 13:12:29 +0900

    test(BridgeCrossingStatus) : make test data about clearAll feature

--- src/test/java/bridge/businesslogic/bridgestatus/BridgeCrossingStatusTest.java
+++ src/test/java/bridge/businesslogic/bridgestatus/BridgeCrossingStatusTest.java
@@ -56,6 +56,32 @@ class BridgeCrossingStatusTest {
             assertThat(upLine.toString()).isEqualTo("O |  ");
             assertThat(downLine.toString()).isEqualTo("  | O");
         }
-
     }
+
+    @Nested
+    class ClearAllTest{
+
+        @DisplayName("데이터가 없을 때는, clearAll 해도 그냥 데이터가 없는 상태이다.")
+        @Test
+        void clearAll_case1(){
+            StringBuilder upLine = new StringBuilder();
+            StringBuilder downLine = new StringBuilder();
+            bridgeCrossing = new BridgeCrossingStatus(upLine,downLine);
+            bridgeCrossing.clearAll();
+            assertThat(upLine.toString()).isEqualTo("");
+            assertThat(downLine.toString()).isEqualTo("");
+        }
+
+        @DisplayName("데이터가 있을 때, clearAll 하면 두 라인의 데이터 모두가 지워진다.")
+        @Test
+        void clearAll_case2(){
+            StringBuilder upLine = new StringBuilder("O |   | O");
+            StringBuilder downLine = new StringBuilder("  | O |  ");
+            bridgeCrossing = new BridgeCrossingStatus(upLine,downLine);
+            bridgeCrossing.clearAll();
+            assertThat(upLine.toString()).isEqualTo("");
+            assertThat(downLine.toString()).isEqualTo("");
+        }
+    }
+
 }
