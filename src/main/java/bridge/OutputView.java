cef2bfdb1d9f464b1909e3f0fbcc372a3683ad90
parkjiyun <pjyun05200@gmail.com>
2022-11-22 20:16:01 +0900

    현재까지의 다리 상태를 출력하는 기능 추가

--- src/main/java/bridge/OutputView.java
+++ src/main/java/bridge/OutputView.java
@@ -1,16 +1,25 @@
 package bridge;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * 사용자에게 게임 진행 상황과 결과를 출력하는 역할을 한다.
  */
 public class OutputView {
 
-    /**
-     * 현재까지 이동한 다리의 상태를 정해진 형식에 맞춰 출력한다.
-     * <p>
-     * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
-     */
+    private final List<String> upStr = new ArrayList<>();
+    private final List<String> downStr = new ArrayList<>();
+
     public void printMap() {
+        String upperBridge = "[" + " ";
+        String bottomBridge = "[" + " ";
+
+        upperBridge = String.join(" | ", upStr);
+        bottomBridge = String.join(" | ", downStr);
+
+        System.out.println(upperBridge + " " + "]");
+        System.out.println(bottomBridge + " " + "]");
     }
 
     /**
@@ -22,6 +31,7 @@ public class OutputView {
     }
 
     public void clearMap() {
-
+        upStr.clear();
+        downStr.clear();
     }
 }
