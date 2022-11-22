21e6adee0fa2821c164f96f6a07a6d4c57e6ca95
DavinAn <blueapple1030@gmail.com>
2022-11-22 22:41:32 +0900

    test : update readBridgeSize to check for exception message

--- src/test/java/bridge/InputViewTest.java
+++ src/test/java/bridge/InputViewTest.java
@@ -1,13 +1,34 @@
 package bridge;
 
 import bridge.controller.InputViewException;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
-public class InputViewTest {
+import java.util.stream.Stream;
+
+import static bridge.controller.InputViewException.*;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+class InputViewTest {
     private final InputViewException inputViewException = new InputViewException();
 
+
+    @DisplayName("다리 길이 입력에 대한 예외 테스트")
+    @ParameterizedTest
+    @MethodSource("readBridgeSizeParam")
     void readBridgeSize(String bridgeSize, String exception) {
+        assertThatThrownBy(() -> inputViewException.readBridgeSizeException(bridgeSize))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(exception);
+    }
+
+    static Stream<Arguments> readBridgeSizeParam() {
 
     }
+
+
 
     void readMoving(String playerMove, String exception) {
 
