a24219cd7e4fa462fd815e35880c87491772f92c
wonseok <tkddls23@konkuk.ac.kr>
2022-11-22 13:38:01 +0900

    test: 값을 입력했는지에 대해 검증한다.

--- src/test/java/bridge/InputViewValidationTest.java
+++ src/test/java/bridge/InputViewValidationTest.java
@@ -1,9 +1,12 @@
 package bridge;
 
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
+import static bridge.constant.ErrorConstant.ERROR_BLANK_INPUT;
+import static bridge.constant.ErrorConstant.ERROR_NON_INTEGER;
 import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
 
 public class InputViewValidationTest {
@@ -13,6 +16,15 @@ public class InputViewValidationTest {
     @ValueSource(strings = {"10h","hello"," "})
     void checkNotValidDigitInputValidation(String input) {
         assertThatIllegalArgumentException()
-                .isThrownBy(() -> InputViewValidation.isValidInput(input));
+                .isThrownBy(() -> InputViewValidation.isValidInput(input))
+                .withMessageStartingWith(ERROR_NON_INTEGER);
+    }
+
+    @DisplayName("예외 처리 : 값을 입력하지 않았을때 에러 발생 검증")
+    @Test
+    void checkBlankInputValidation() {
+        assertThatIllegalArgumentException()
+                .isThrownBy(() -> InputViewValidation.isValidInput(""))
+                .withMessageStartingWith(ERROR_BLANK_INPUT);
     }
 }
