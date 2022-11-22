e8b733c685c13feee47eced09e2e4ff99b245923
desfox <desfox99@naver.com>
2022-11-22 19:48:58 +0900

    refactor(InputView): 출력할 문구 ConsoleMessage Enum으로 대체

--- src/main/java/bridge/view/InputView.java
+++ src/main/java/bridge/view/InputView.java
@@ -5,6 +5,8 @@ import bridge.validation.ReadGameCommandValidation;
 import bridge.validation.ReadMovingValidation;
 import camp.nextstep.edu.missionutils.Console;
 
+import static bridge.view.ConsoleMessage.*;
+
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
  */
@@ -14,7 +16,7 @@ public class InputView {
      * 다리의 길이를 입력받는다.
      */
     public int readBridgeSize() {
-        System.out.println("다리의 길이를 입력해주세요.");
+        System.out.println(INPUT_BRIDGE_SIZE.getMessage());
         String inputBridgeSize = Console.readLine();
         System.out.println();
 
@@ -27,7 +29,7 @@ public class InputView {
      * 사용자가 이동할 칸을 입력받는다.
      */
     public String readMoving() {
-        System.out.println("이동할 칸을 입력해주세요. (위: U, 아래: D)");
+        System.out.println(INPUT_MOVING.getMessage());
         String movingCommand = Console.readLine();
         System.out.println();
 
@@ -40,7 +42,7 @@ public class InputView {
      * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
      */
     public String readGameCommand() {
-        System.out.println("게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)");
+        System.out.println(INPUT_GAME_COMMAND.getMessage());
         String gameCommand = Console.readLine();
 
         ReadGameCommandValidation readGameCommandValidation = new ReadGameCommandValidation();
