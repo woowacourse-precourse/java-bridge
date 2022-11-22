c4aeb2c999247c9c3302b0a97af2e461654dbd4e
WhoIsWoony <WhoIsWoony@gmail.com>
2022-11-22 16:02:40 +0900

    chore: change input using api

--- src/main/java/bridge/InputView.java
+++ src/main/java/bridge/InputView.java
@@ -1,21 +1,18 @@
 package bridge;
 
-import java.util.InputMismatchException;
-import java.util.Scanner;
+import static camp.nextstep.edu.missionutils.Console.readLine;
 
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
  */
 public class InputView {
-    Scanner sc = new Scanner(System.in);
-
     /**
      * 다리의 길이를 입력받는다.
      */
     public int readBridgeSize() {
         System.out.println("다리의 길이를 입력해주세요.");
         try{
-            String size = sc.next();
+            String size = readLine();
             return Integer.parseInt(size);
         }catch (NumberFormatException e){
             throw new IllegalArgumentException();
@@ -27,7 +24,7 @@ public class InputView {
      */
     public String readMoving() {
         System.out.println("이동할 칸을 선택해주세요. (위: U, 아래: D)");
-        return sc.next();
+        return readLine();
     }
 
     /**
@@ -35,6 +32,6 @@ public class InputView {
      */
     public String readGameCommand() {
         System.out.println("게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)");
-        return sc.next();
+        return readLine();
     }
 }
