56d72ba9ce6eb32eceefe74abd9e646d10ed077f
HamSeungChan <head0618@naver.com>
2022-11-22 16:09:19 +0900

    feat : 이동 결과와 출력을 관리하는 service 기능 구현

---
+++ src/main/java/bridge/service/GameService.java
@@ -0,0 +1,43 @@
+package bridge.service;
+
+import bridge.domain.User;
+import bridge.domain.Map;
+
+import java.util.List;
+
+public class GameService {
+
+    private final List<String> bridge;
+    private final User user;
+    private final Map map;
+
+    public GameService(List<String> bridge, User user, Map map) {
+        this.user = user;
+        this.bridge = bridge;
+        this.map = map;
+    }
+
+    public void move(String moveDirection) {
+        user.inputMoveDirection(moveDirection);
+    }
+
+    private boolean canMove() {
+        int matchIndex = user.getCountInputMoveDirection() - 1;
+        if (bridge.get(matchIndex).equals(user.getMoveDirection())) {
+            return true;
+        }
+        return false;
+    }
+
+    public List<List<String>> drawMap() {
+        map.drawMap(user.getMoveDirectionNumber(), canMove());
+        return map.getMap();
+    }
+
+    public boolean CanKeepGoingGame() {
+        if (user.getCountInputMoveDirection() == bridge.size() || !canMove()) {
+            return false;
+        }
+        return true;
+    }
+}
