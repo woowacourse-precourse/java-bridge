25758fd5b74f7b12a2d63fd606a5cfb15e303547
thousandcows <milcamboo@gmail.com>
2022-11-22 14:21:37 +0900

    feat: add field and constructor to return error message

--- src/main/java/validation/ErrorMessage.java
+++ src/main/java/validation/ErrorMessage.java
@@ -6,7 +6,13 @@ public enum ErrorMessage {
     INVALID_MOVING_ORDER("[ERROR] U(위 칸) 또는 D(아래 칸)을 입력해주세요"),
     INVALID_GAME_COMMAND("[ERROR] R(재시작) 또는 Q(종료)을 입력해주세요");
 
-    ErrorMessage(String s) {
+    private final String message;
 
+    ErrorMessage(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
     }
 }
