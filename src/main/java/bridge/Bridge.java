faac642fc67b59dcc3dd790e7e84a1d6b6906fd4
rjsah09 <83952174+rjsah09@users.noreply.github.com>
2022-11-22 14:45:29 +0900

    refactor: Bridge.bridge -> Bridge.footholds 명칭 변경

--- src/main/java/bridge/Bridge.java
+++ src/main/java/bridge/Bridge.java
@@ -3,20 +3,20 @@ package bridge;
 import java.util.List;
 
 public class Bridge {
-    private List<String> bridge;
+    private List<String> footholds;
     private List<String> crossed;
 
-    public Bridge(List<String> bridge, List<String> crossed) {
-        this.bridge = bridge;
+    public Bridge(List<String> footholds, List<String> crossed) {
+        this.footholds = footholds;
         this.crossed = crossed;
     }
 
-    public List<String> getBridge() {
-        return this.bridge;
+    public List<String> getFootholds() {
+        return this.footholds;
     }
 
     public List<String> getCrossed() {
         return this.crossed;
     }
-    
+
 }
