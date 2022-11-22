58cad9d2ace9388390977e210cf7ae7d0d313d77
jay <jaewhan232@naver.com>
2022-11-22 14:13:57 +0900

    Implement : 입력받은 크기의 다리를 생성하는 makeBridge() 메소드 구현 MovingCommand - commandOf(int number) - 해당하는 숫자의 다음 이동 칸을 반환하는 메소드

--- src/main/java/bridge/BridgeMaker.java
+++ src/main/java/bridge/BridgeMaker.java
@@ -1,5 +1,6 @@
 package bridge;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -21,6 +22,16 @@ public class BridgeMaker {
      * @return 입력받은 길이에 해당하는 다리 모양. 위 칸이면 "U", 아래 칸이면 "D"로 표현해야 한다.
      */
     public List<String> makeBridge(int size) {
-        return null;
+        List<String> bridge = new ArrayList<>();
+
+        for (int i = 0; i < size; i++) {
+            bridge.add(getCommandMatchWith(
+                    bridgeNumberGenerator.generate()));
+        }
+        return bridge;
+    }
+
+    public String getCommandMatchWith(int number) {
+        return MovingCommand.commandOf(number);
     }
 }
--- src/main/java/bridge/MovingCommand.java
+++ src/main/java/bridge/MovingCommand.java
@@ -2,14 +2,24 @@ package bridge;
 
 public enum MovingCommand {
 
-    UP("U"),
-    DOWN("D");
+    UP("U", 1),
+    DOWN("D", 0);
 
-    private static final String errorMessage = "이동할 칸은 U 또는 D로 이동할 수 있습니다.";
+    private static final String INVALID_USER_INPUT = "이동할 칸은 U 또는 D로 이동할 수 있습니다.";
+    private static final String INVALID_NUMBER = "해당하는 명령을 찾을 수 없습니다.";
 
     private String command;
+    private int number;
 
-    MovingCommand(String command) {
+    MovingCommand(String command, int number) {
         this.command = command;
+        this.number = number;
+    }
+
+    public static String commandOf(int number) {
+        for (MovingCommand cmd : MovingCommand.values()) {
+            if (cmd.number == number) return cmd.command;
+        }
+        throw new IllegalArgumentException(INVALID_NUMBER);
     }
 }
