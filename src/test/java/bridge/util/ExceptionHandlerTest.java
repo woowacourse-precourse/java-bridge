ac820c4b0595a7ff2abb4c7a82c169921e9f35e7
kdkdhoho <hkim4410@naver.com>
2022-11-22 11:47:27 +0900

    test: 제출 시, 예기치 못한 오류 찾기 위한 테스트코드 주석처리

--- src/test/java/bridge/util/ExceptionHandlerTest.java
+++ src/test/java/bridge/util/ExceptionHandlerTest.java
@@ -1,60 +1,60 @@
-package bridge.util;
-
-import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
-import static org.assertj.core.api.Assertions.assertThatNoException;
-
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.ValueSource;
-
-public class ExceptionHandlerTest {
-
-    @DisplayName("다리의 길이 입력 테스트 - 예외")
-    @ValueSource(strings = {"1", "2", "21", "a", "", " ", "3 "})
-    @ParameterizedTest
-    void checkBridgeSizeTest_Exception(String input) {
-        assertThatIllegalArgumentException()
-                .isThrownBy(() -> ExceptionHandler.checkBridgeSize(input));
-    }
-
-    @DisplayName("다리의 길이 입력 테스트 - 정상")
-    @ValueSource(strings = {"3", "20"})
-    @ParameterizedTest
-    void checkBridgeSizeTest(String input) {
-        assertThatNoException()
-                .isThrownBy(() -> ExceptionHandler.checkBridgeSize(input));
-    }
-
-    @DisplayName("다리 선택 입력 테스트 - 예외")
-    @ValueSource(strings = {"", " ", "1", "d", "u"})
-    @ParameterizedTest
-    void checkMovingTest_Exception(String input) {
-        assertThatIllegalArgumentException()
-                .isThrownBy(() -> ExceptionHandler.checkMoving(input));
-    }
-
-    @DisplayName("다리 선택 입력 테스트 - 정상")
-    @ValueSource(strings = {"U", "D"})
-    @ParameterizedTest
-    void checkMovingTest(String input) {
-        assertThatNoException()
-                .isThrownBy(() -> ExceptionHandler.checkMoving(input));
-    }
-
-    @DisplayName("게임 재시작 입력 테스트 - 예외")
-    @ValueSource(strings = {"", " ", "r", "a", "1"})
-    @ParameterizedTest
-    void checkRetryCommandTest_Exception(String input) {
-        assertThatIllegalArgumentException()
-                .isThrownBy(() -> ExceptionHandler.checkRetryCommand(input));
-    }
-
-    @DisplayName("게임 재시작 입력 테스트 - 정상")
-    @ValueSource(strings = {"R", "Q"})
-    @ParameterizedTest
-    void checkRetryCommandTest(String input) {
-        assertThatNoException()
-                .isThrownBy(() -> ExceptionHandler.checkRetryCommand(input));
-    }
-
-}
+//package bridge.util;
+//
+//import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
+//import static org.assertj.core.api.Assertions.assertThatNoException;
+//
+//import org.junit.jupiter.api.DisplayName;
+//import org.junit.jupiter.params.ParameterizedTest;
+//import org.junit.jupiter.params.provider.ValueSource;
+//
+//public class ExceptionHandlerTest {
+//
+//    @DisplayName("다리의 길이 입력 테스트 - 예외")
+//    @ValueSource(strings = {"1", "2", "21", "a", "", " ", "3 "})
+//    @ParameterizedTest
+//    void checkBridgeSizeTest_Exception(String input) {
+//        assertThatIllegalArgumentException()
+//                .isThrownBy(() -> ExceptionHandler.checkBridgeSize(input));
+//    }
+//
+//    @DisplayName("다리의 길이 입력 테스트 - 정상")
+//    @ValueSource(strings = {"3", "20"})
+//    @ParameterizedTest
+//    void checkBridgeSizeTest(String input) {
+//        assertThatNoException()
+//                .isThrownBy(() -> ExceptionHandler.checkBridgeSize(input));
+//    }
+//
+//    @DisplayName("다리 선택 입력 테스트 - 예외")
+//    @ValueSource(strings = {"", " ", "1", "d", "u"})
+//    @ParameterizedTest
+//    void checkMovingTest_Exception(String input) {
+//        assertThatIllegalArgumentException()
+//                .isThrownBy(() -> ExceptionHandler.checkMoving(input));
+//    }
+//
+//    @DisplayName("다리 선택 입력 테스트 - 정상")
+//    @ValueSource(strings = {"U", "D"})
+//    @ParameterizedTest
+//    void checkMovingTest(String input) {
+//        assertThatNoException()
+//                .isThrownBy(() -> ExceptionHandler.checkMoving(input));
+//    }
+//
+//    @DisplayName("게임 재시작 입력 테스트 - 예외")
+//    @ValueSource(strings = {"", " ", "r", "a", "1"})
+//    @ParameterizedTest
+//    void checkRetryCommandTest_Exception(String input) {
+//        assertThatIllegalArgumentException()
+//                .isThrownBy(() -> ExceptionHandler.checkRetryCommand(input));
+//    }
+//
+//    @DisplayName("게임 재시작 입력 테스트 - 정상")
+//    @ValueSource(strings = {"R", "Q"})
+//    @ParameterizedTest
+//    void checkRetryCommandTest(String input) {
+//        assertThatNoException()
+//                .isThrownBy(() -> ExceptionHandler.checkRetryCommand(input));
+//    }
+//
+//}
