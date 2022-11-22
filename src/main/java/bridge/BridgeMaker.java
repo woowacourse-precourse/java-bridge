b6c75206a2db7021394b002650a378b90618945b
Yongwoo Choi <cyw7515@naver.com>
2022-11-22 15:09:15 +0900

    fix (BridgeMaker): BridgeMaker 패키지 변경해 발생한 오류 수정 (요구 사항에서 달리 명시하지 않는 한 파일, 패키지 이름을 수정하거나 이동하지 않는다.)

--- src/main/java/bridge/BridgeMaker.java
+++ src/main/java/bridge/BridgeMaker.java
@@ -1,6 +1,5 @@
-package bridge.domain;
+package bridge;
 
-import bridge.BridgeNumberGenerator;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -9,7 +8,6 @@ import java.util.List;
  */
 public class BridgeMaker {
 
-    private static final List<String> board = List.of("D", "U");
     private final BridgeNumberGenerator bridgeNumberGenerator;
 
     public BridgeMaker(BridgeNumberGenerator bridgeNumberGenerator) {
@@ -21,6 +19,7 @@ public class BridgeMaker {
      * @return 입력받은 길이에 해당하는 다리 모양. 위 칸이면 "U", 아래 칸이면 "D"로 표현해야 한다.
      */
     public List<String> makeBridge(int size) {
+        List<String> board = List.of("D", "U");
         List<String> bridge = new ArrayList<>();
 
         for (int i = 0; i < size; i++) {
