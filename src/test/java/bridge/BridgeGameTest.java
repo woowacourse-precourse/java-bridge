b3ff5cb9305a0d407633255f35cc43cb3fa104da
leewoojin <zmfpdl64@naver.com>
2022-11-22 19:18:20 +0900

    Refactory: 얘기치 못한 오류 해결 중 #55

--- src/test/java/bridge/BridgeGameTest.java
+++ src/test/java/bridge/BridgeGameTest.java
@@ -1,7 +1,8 @@
-package bridge.service;
+package bridge;
 
 import bridge.model.entity.BridgeMap;
 import bridge.model.entity.Bridge;
+import bridge.service.BridgeGame;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
