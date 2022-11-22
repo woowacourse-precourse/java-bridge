5016b2b68235828caf8cb57fb06569a7a99d1b11
tjrqhd121@naver.com <tjrqhd121@naver.com>
2022-11-22 16:11:42 +0900

    feat(BridgeGame): 기능 구현
    void move(int count, String command): bridge 에 결과 데이터를 저장한다
    boolean retry(String command): 재시작(R) 시 결과 리스트 초기화, 종료(Q) 시 결과반환

--- src/main/java/bridge/BridgeGame.java
+++ src/main/java/bridge/BridgeGame.java
@@ -1,16 +1,25 @@
 package bridge;
 
+import domain.Bridge;
+
 /**
  * 다리 건너기 게임을 관리하는 클래스
  */
 public class BridgeGame {
+    public static final String QUIT = "Q";
+    private final Bridge bridge;
+    private int gameCount = 1;
+    public BridgeGame(Bridge bridge) {
+        this.bridge = bridge;
+    }
 
     /**
      * 사용자가 칸을 이동할 때 사용하는 메서드
      * <p>
      * 이동을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void move() {
+    public void move(int count, String command) {
+        bridge.commandResult(count, command);
     }
 
     /**
@@ -18,6 +27,19 @@ public class BridgeGame {
      * <p>
      * 재시작을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void retry() {
+    public boolean retry(String command) {
+        if(command.equals(QUIT)){
+            return false;
+        }
+        bridge.answerReset();
+        return true;
+    }
+
+    public void plusGameCount() {
+        gameCount++;
+    }
+
+    public int getGameCount() {
+        return gameCount;
     }
 }
