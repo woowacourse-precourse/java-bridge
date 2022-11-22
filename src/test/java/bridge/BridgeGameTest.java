58f90f3713474b7323ef67b6352a78afc00d72db
YooSuYeon <usuyn000@gmail.com>
2022-11-22 14:25:00 +0900

    test: 이동 성공 여부 확인 테스트

--- src/test/java/bridge/BridgeGameTest.java
+++ src/test/java/bridge/BridgeGameTest.java
@@ -2,10 +2,15 @@ package bridge;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 import repository.BridgeRepository;
 
+import java.util.List;
+import java.util.stream.IntStream;
+
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class BridgeGameTest {
@@ -20,7 +25,7 @@ public class BridgeGameTest {
 
     @DisplayName("잘못된 다리 길이 입력에 대한 예외 처리")
     @ParameterizedTest
-    @ValueSource(strings = {"0", "2", "21", "15.", "2.5", " ", "십오", "four"})
+    @ValueSource(strings = {"0", "2", "21", "15.", "2.5", " ", "십오", "four", ""})
     void 다리_길이_입력_예외_발생(String bridgeSize) {
         assertThatThrownBy(() -> bridgeGame.generateBridge(bridgeSize))
                 .isInstanceOf(IllegalArgumentException.class);
@@ -28,9 +33,34 @@ public class BridgeGameTest {
 
     @DisplayName("잘못된 이동할 칸 입력에 대한 예외 처리")
     @ParameterizedTest
-    @ValueSource(strings = {" ", "UU", "위", "up", "u", "d"})
+    @ValueSource(strings = {" ", "UU", "위", "up", "u", "d", ""})
     void 이동칸_입력_예외_발생(String direction) {
         assertThatThrownBy(() -> bridgeGame.move(direction))
                 .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("이동 성공 확인")
+    @Test
+    void 이동_성공() {
+        List<String> bridge = List.of("U", "D", "D", "U", "D");
+        List<String> progress = List.of("U", "D", "D", "U", "D");
+        bridgeRepository.saveBridge(bridge);
+
+        IntStream.range(0, progress.size())
+                .forEach(i -> {
+                    bridgeRepository.saveProgress(progress.get(i));
+                    assertThat(bridgeGame.canMove()).isEqualTo(true);
+                });
+    }
+
+    @DisplayName("이동 실패 확인")
+    @Test
+    void 이동_실패() {
+        List<String> bridge = List.of("U", "D", "D", "U", "D");
+
+        bridgeRepository.saveBridge(bridge);
+        bridgeRepository.saveProgress("D");
+
+        assertThat(bridgeGame.canMove()).isEqualTo(false);
     }
 }
