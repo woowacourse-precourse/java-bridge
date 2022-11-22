7a9e5430f814fc34a934963cf139f9a607dc4fa1
Jeeseob <jeeseob5761@gmail.com>
2022-11-22 15:24:09 +0900

    refactor: inputViewTest 불필요한 package import문 제거

--- src/test/java/bridge/view/InputViewTest.java
+++ src/test/java/bridge/view/InputViewTest.java
@@ -1,20 +1,9 @@
 package bridge.view;
 
-import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeTest;
-import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
-import static org.assertj.core.util.Lists.newArrayList;
-
-import bridge.util.IllegalArgumentExceptionMessage;
-import camp.nextstep.edu.missionutils.test.NsTest;
 
 import java.io.*;
-import java.lang.reflect.InvocationTargetException;
-import java.lang.reflect.Method;
-import java.util.List;
 
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 /**
