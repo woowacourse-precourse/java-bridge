bb32cacbaa9a16e0c8b2d5bb0ba985859c9b1bb7
yongseungLeeDec <yonson.dev@gmail.com>
2022-11-22 12:06:08 +0900

    feat(ongoing): 다리 이동 결과 보여주기 기능 구현
    OutputView 클래스의 printMap 메서드를 구현함.
    - 해당 메서드는 호출자로부터 현재까지의 다리 칸별 이동 결과 목록을 인자로 받음.
    - 의존성 MapDrawer 멤버 객체의 drawMap을 호출하여 moveResults에 대응하는 다리 그림을 그린 뒤, 콘솔에 이를 출력함.

--- src/main/java/bridge/console/OutputView.java
+++ src/main/java/bridge/console/OutputView.java
@@ -1,6 +1,23 @@
 package bridge.console;
 
+import bridge.console.utility.MapDrawer;
+import bridge.data.MoveResult;
+
+import java.util.List;
+
 public class OutputView {
 
-}
+    private final MapDrawer mapDrawer;
 
+    public OutputView() {
+        this.mapDrawer = new MapDrawer();
+    }
+
+    public void printMap(List<MoveResult> moveResults) {
+        StringBuilder upperRow = new StringBuilder();
+        StringBuilder lowerRow = new StringBuilder();
+        this.mapDrawer.drawMap(moveResults, upperRow, lowerRow);
+
+        System.out.println(upperRow + System.lineSeparator() + lowerRow + System.lineSeparator());
+    }
+}
