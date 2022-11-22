9ffa179408060e8d1d9217bb53462f58b46b27be
Be-student <presentsong@naver.com>
2022-11-22 18:47:02 +0900

    chore: command 객체 바깥 trycatch 제거
    전에는 command 객체 바깥에서 예외를 잡았기에 필요했습니다
    후에는 command 객체 안쪽에서 예외를 처리하기에, 필요하지 않습니다

--- src/main/java/bridge/view/InputView.java
+++ src/main/java/bridge/view/InputView.java
@@ -9,18 +9,13 @@ import camp.nextstep.edu.missionutils.Console;
  * 사용자로부터 입력을 받는 역할을 한다.
  */
 public final class InputView {
-    private static final String OVER_RANGE_MESSAGE = "입력의 범위를 초과했습니다";
 
     /**
      * 다리의 길이를 입력받는다.
      */
     public BridgeSizeCommand readBridgeSize() {
         final String input = Console.readLine();
-        try {
-            return new BridgeSizeCommand(input);
-        } catch (final NumberFormatException e) {
-            throw new IllegalArgumentException(OVER_RANGE_MESSAGE);
-        }
+        return new BridgeSizeCommand(input);
     }
 
 
