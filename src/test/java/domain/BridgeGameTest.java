722d3a2b4896a1692581734ba97624533442f259
DDing77 <devseo2021@gmail.com>
2022-11-22 21:37:32 +0900

    test(bridge) : 완전한 종료 성공 테스트

--- src/test/java/domain/BridgeGameTest.java
+++ src/test/java/domain/BridgeGameTest.java
@@ -4,6 +4,7 @@ import bridge.BridgeRandomNumberGenerator;
 import bridge.domain.BridgeGame;
 import bridge.domain.BridgeMaker;
 import bridge.service.BridgeGameService;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
@@ -18,7 +19,14 @@ public class BridgeGameTest {
 
     private static BridgeMaker bridgeMaker;
     private static BridgeGame bridgeGame;
+    private static BridgeGameService bridgeGameService;
 
+    @BeforeEach
+    void beforeEach() {
+        bridgeMaker = new BridgeMaker(new BridgeRandomNumberGenerator());
+        bridgeGame = new BridgeGame(bridgeMaker.makeBridge(20));
+        bridgeGameService = new BridgeGameService(bridgeGame);
+    }
 
     @DisplayName("다리 생성 성공 테스트")
     @ValueSource(ints = {5, 15, 20})
@@ -34,13 +42,23 @@ public class BridgeGameTest {
     @ValueSource(ints = {5, 15, 20})
     @ParameterizedTest()
     void 다리_생성시_UD_테스트(int size) {
-        bridgeMaker = new BridgeMaker(new BridgeRandomNumberGenerator());
         bridgeGame = new BridgeGame(bridgeMaker.makeBridge(size));
         BridgeGameService bridgeGameService = new BridgeGameService(bridgeGame);
-
         List<String> bridge = bridgeGameService.getBridge();
-        List<String> filtered = bridge.stream().filter( s -> s.equals("U") || s.equals("D"))
-                        .collect(Collectors.toList());
+        List<String> filtered = bridge.stream().filter(s -> s.equals("U") || s.equals("D"))
+                .collect(Collectors.toList());
         assertThat(bridge).isEqualTo(filtered);
     }
+
+    @DisplayName("다리 끝까지 성공 후 도달했다면 종료 성공")
+    void 다리끝_도달후_종료성공_테스트() {
+        List<String> bridge = bridgeGameService.getBridge();
+
+        for (String command : bridge) {
+            bridgeGameService.moveNext(command);
+        }
+
+        assertThat(bridgeGameService.isComplete());
+    }
+
 }
