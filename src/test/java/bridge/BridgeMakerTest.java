52cddc4c62e1ceb88438e00201c85f5a273ddc57
Be-student <presentsong@naver.com>
2022-11-22 20:06:04 +0900

    test: bridgeMaker 생성 범위 검증 추가

--- src/test/java/bridge/BridgeMakerTest.java
+++ src/test/java/bridge/BridgeMakerTest.java
@@ -5,10 +5,14 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.DisplayNameGeneration;
 import org.junit.jupiter.api.DisplayNameGenerator;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 
 @DisplayName("BridgeMaker 클래스")
 @DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
@@ -33,5 +37,19 @@ class BridgeMakerTest {
         final List<String> result = new BridgeMaker(generator).makeBridge(10);
         assertThat(result).isEqualTo(List.of("U", "D", "U", "D", "D", "U", "U", "D", "D", "U"))
                 .hasSize(10);
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {2, 21})
+    void makeBridge_메서드는_주어진_숫자의_범위가_벗어나면_예외(final int input) {
+        assertThatIllegalArgumentException().isThrownBy(
+                () -> new BridgeMaker(new BridgeRandomNumberGenerator()).makeBridge(input));
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {3, 20})
+    void makeBridge_메서드는_주어진_숫자의_범위가_올바르면_정상_작동(final int input) {
+        assertDoesNotThrow(
+                () -> new BridgeMaker(new BridgeRandomNumberGenerator()).makeBridge(input));
     }
 }
