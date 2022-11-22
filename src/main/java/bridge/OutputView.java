d0d4c2e6bff9cd406deca8e575993dd2a73881d2
inwoo <korean5078@gmail.com>
2022-11-22 11:31:21 +0900

    feat(OutputView) : Map 출력하는 함수 작성

--- src/main/java/bridge/OutputView.java
+++ src/main/java/bridge/OutputView.java
@@ -1,5 +1,7 @@
 package bridge;
 
+import java.util.List;
+
 /**
  * 사용자에게 게임 진행 상황과 결과를 출력하는 역할을 한다.
  */
@@ -10,7 +12,20 @@ public class OutputView {
      * <p>
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void printMap() {
+    public void printMap(List<String> result1, List<String> result2, int index) {
+        printOneMap(result1, index);
+        printOneMap(result2, index);
+    }
+
+    public void printOneMap(List<String> result, int index) {
+        System.out.print("[");
+        for (int i = 0; i < index; i++) {
+            System.out.print(" " + result.get(i));
+            if (i < index - 1) {
+                System.out.print(" |");
+            }
+        }
+        System.out.println(" ]");
     }
 
     /**
