3683a0db381b619bce92f1b5727d0fd30fd5cded
tjrqhd121@naver.com <tjrqhd121@naver.com>
2022-11-22 15:04:52 +0900

    style(Message): 메세지 오타 수정

--- src/main/java/domain/ErrorMessage.java
+++ src/main/java/domain/ErrorMessage.java
@@ -1,7 +1,7 @@
 package domain;
 
 public enum ErrorMessage {
-    NOT_MATCHING_MOVING_COMMAND("[ERROR] 커맨드에 맞는 값(E, U)을 입력하여 주십시오."),
+    NOT_MATCHING_MOVING_COMMAND("[ERROR] 커맨드에 맞는 값(U, D)을 입력하여 주십시오."),
     NOT_MATCHING_GAME_COMMAND("[ERROR] 커맨드에 맞는 값(Q, R)을 입력하여 주십시오."),
     NOT_NUMBER("[ERROR] 숫자를 입력하여 주십시오."),
     NOT_BRIDGE_SIZE("[ERROR] 다리 길이는 3 이상 20 이하만 가능합니다.");
--- src/main/java/domain/InputMessage.java
+++ src/main/java/domain/InputMessage.java
@@ -4,7 +4,7 @@ public enum InputMessage {
     START_MESSAGE("다리 건너기 게임을 시작합니다."),
     INPUT_BRIDGE_MESSAGE("다리의 길이를 입력해주세요."),
     INPUT_MOVING_MESSAGE("이동할 칸을 선택해주세요. (위: U, 아래: D)"),
-    INPUT_GAME_MESSAGE("게임을 다시 시돟라지 여부를 입력해주세요. (재시도: R, 종료: Q)");
+    INPUT_GAME_MESSAGE("게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)");
 
     private final String message;
 
