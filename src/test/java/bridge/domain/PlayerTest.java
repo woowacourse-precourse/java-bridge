36918702e4d5fca32eb4becc0ace0c131028aa11
shpark30 <seohyeonpark30.gmail.com>
2022-11-22 11:01:14 +0900

    test(Player): 최종 게임 성공 여부 테스트

--- src/test/java/bridge/domain/PlayerTest.java
+++ src/test/java/bridge/domain/PlayerTest.java
@@ -2,7 +2,9 @@ package bridge.domain;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.util.Lists.newArrayList;
 
+import java.util.List;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
@@ -45,6 +47,41 @@ class PlayerTest {
             Player player = new Player(new Map());
             player.move("U", "D");
             assertThat(player.isAlive()).isEqualTo(false);
+        }
+    }
+
+    @Nested
+    class SucceedTest {
+        private List<String> bridge = newArrayList("U", "U", "D");
+
+        @DisplayName("최종 정답")
+        @Test
+        void right() {
+            Player player = new Player(new Map());
+            player.move("U", "U");
+            player.move("U", "U");
+            player.move("D", "D");
+
+            assertThat(player.hasSucceeded(bridge)).isEqualTo(true);
+        }
+
+        @DisplayName("최종 오답: 끝까지 가지 전에 탈락")
+        @Test
+        void wrong1() {
+            Player player = new Player(new Map());
+            player.move("U", "U");
+            player.move("U", "D");
+            assertThat(player.hasSucceeded(bridge)).isEqualTo(false);
+        }
+
+        @DisplayName("최종 오답: 끝까지 가서 탈락")
+        @Test
+        void wrong2() {
+            Player player = new Player(new Map());
+            player.move("U", "U");
+            player.move("U", "U");
+            player.move("U", "D");
+            assertThat(player.hasSucceeded(bridge)).isEqualTo(false);
         }
     }
 
