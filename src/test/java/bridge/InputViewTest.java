433ae770e8065a63a65b41876d98e52ea18a3931
YooraKim <62877255+YooraKim@users.noreply.github.com>
2022-11-22 23:31:46 +0900

    test: InputViewTest 안쓰는 패키지 삭제

--- src/test/java/bridge/InputViewTest.java
+++ src/test/java/bridge/InputViewTest.java
@@ -4,7 +4,6 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
-import static bridge.InputView.*;
 
 public class InputViewTest {
 
