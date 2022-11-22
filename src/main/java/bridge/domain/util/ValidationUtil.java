e86f5f1d3fad6f5f76ce6bd91e91292884801fe8
wnsgh12s <junho1273@gmail.com>
2022-11-22 12:15:31 +0900

    Refactor: 의미 있는 상수 변수에 담아 쓰기

--- src/main/java/bridge/domain/util/ValidationUtil.java
+++ src/main/java/bridge/domain/util/ValidationUtil.java
@@ -6,9 +6,11 @@ public class ValidationUtil {
     private static final String INVAILED_SIZE = "[ERROR] 다리 길이는 3부터 20 사이의 숫자여야 합니다.";
     private static final String INVAILED_MOVE = "[ERROR] U 또는 D로 입력해야 합니다.";
     private static final String INVAILED_GAME_COMMAND = "[ERROR] R 또는 Q를 입력하셔야 합니다.";
+    private static final int MIN_SIZE = 3;
+    private static final int MAX_SIZE = 20;
 
     public static void bridgeSizeValidation(int bridgeSize) {
-        if (bridgeSize > 20 || bridgeSize < 3) {
+        if (bridgeSize > MAX_SIZE || bridgeSize < MIN_SIZE) {
             throw new IllegalArgumentException(INVAILED_SIZE);
         }
     }
