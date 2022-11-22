8636f075a71db1bbb7ae7147c8a5c74bb7167666
Beginner <dkstmdwn17@naver.com>
2022-11-22 13:30:38 +0900

    feature : 입력받을 때 공백을 제거 + 주석 제거
    입력받을 때 공백을 제거해서
    받도록 처리해주었고
    
    주석들도 제거해주었다.

--- src/main/java/bridge/view/InputView.java
+++ src/main/java/bridge/view/InputView.java
@@ -4,39 +4,27 @@ import bridge.validation.Validation;
 import bridge.validation.enumeration.Mode;
 import camp.nextstep.edu.missionutils.Console;
 
-/**
- * 사용자로부터 입력을 받는 역할을 한다.
- */
 public class InputView {
 
-    /**
-     * 다리의 길이를 입력받는다.
-     */
     public int readBridgeSize() {
         System.out.println("다리 건너기 게임을 시작합니다.");
         System.out.println();
         System.out.println("다리의 길이를 입력해주세요.");
-        String userInput = Console.readLine();
+        String userInput = Console.readLine().trim();
         Validation.verifyUserInputBridgeLength(userInput);
         return Integer.parseInt(userInput);
     }
 
-    /**
-     * 사용자가 이동할 칸을 입력받는다.
-     */
     public String readMoving() {
         System.out.println("이동할 칸을 선택해주세요. (위: U, 아래: D)");
-        String move = Console.readLine();
+        String move = Console.readLine().trim();
         Validation.verifyUserInputIsCharacter(move, Mode.UpOrDown);
         return move;
     }
 
-    /**
-     * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
-     */
     public String readGameCommand() {
         System.out.println("게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)");
-        String retryOrQuit = Console.readLine();
+        String retryOrQuit = Console.readLine().trim();
         Validation.verifyUserInputIsCharacter(retryOrQuit, Mode.RetryOrQuit);
         return retryOrQuit;
     }
