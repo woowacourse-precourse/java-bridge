697172a1add30506ce93a544a65c10ebc441b72f
alivejuicy <kkddhh1998@gmail.com>
2022-11-22 10:40:07 +0900

    Feat : 테스트 추가

--- src/main/java/bridge/view/InputView.java
+++ src/main/java/bridge/view/InputView.java
@@ -1,5 +1,11 @@
 package bridge.view;
 
+import bridge.domain.provider.BridgeGame;
+import bridge.domain.provider.BridgeMaker;
+
+import camp.nextstep.edu.missionutils.Console;
+
+
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
  */
@@ -9,7 +15,9 @@ public class InputView {
      * 다리의 길이를 입력받는다.
      */
     public int readBridgeSize() {
-        return 0;
+        System.out.println("다리의 길이를 입력해주세요.");
+        String input = Console.readLine();
+        return Integer.parseInt(input);
     }
 
     /**
