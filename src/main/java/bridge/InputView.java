f39fdeb265b58f73cd87ce424f3106370ccc80c3
davinc71998 <davinc71998@gmail.com>
2022-11-22 21:01:22 +0900

    Refactor: 변경 InputView 띄어쓰기 magicNumber 리팩토링

--- src/main/java/bridge/InputView.java
+++ src/main/java/bridge/InputView.java
@@ -10,6 +10,13 @@ public class InputView {
     /**
      * 다리의 길이를 입력받는다.
      */
+    static final int BRIDGE_MIN_LENGTH = 3;
+    static final int BRIDGE_MAX_LENGTH = 20;
+
+    static final int INPUT_POSITION = 0;
+    static final int LENGTH_NULL = 1;
+
+
     public int readBridgeSize() throws IllegalArgumentException {
         System.out.println("다리의 길이를 입력해주세요.");
         String userInputSize = readLine();
@@ -20,7 +27,7 @@ public class InputView {
             throw new IllegalArgumentException("[ERROR] 정수가 아닌 값 입력");
         }
         System.out.println("inputsize = " + inputsize);
-        if(inputsize < 3 || inputsize > 20) {
+        if (inputsize < BRIDGE_MIN_LENGTH || inputsize > BRIDGE_MAX_LENGTH) {
             throw new IllegalArgumentException("[ERROR] 숫자 범위가 잘못되었습니다.");
         }
         return inputsize;
@@ -31,12 +38,12 @@ public class InputView {
     public String readMoving() throws IllegalArgumentException{
         System.out.println("이동할 칸을 선택해주세요. (위: U, 아래: D)");
         String userInputMove = readLine();
-        if(userInputMove.length() != 1) {
+        if (userInputMove.length() != LENGTH_NULL) {
             throw new IllegalArgumentException("[ERROR] U 나 D를 입력해주세요.");
         }
 
-        char inputMove = userInputMove.charAt(0);
-        if(inputMove != 'U' && inputMove != 'D') {
+        char inputMove = userInputMove.charAt(INPUT_POSITION);
+        if (inputMove != 'U' && inputMove != 'D') {
             throw new IllegalArgumentException("[ERROR] U 나 D를 입력해주세요.");
         }
         System.out.println(inputMove);
@@ -49,12 +56,12 @@ public class InputView {
     public String readGameCommand() throws IllegalArgumentException{
         System.out.println("게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)");
         String userRestart = readLine();
-        if(userRestart.length() != 1) {
+        if (userRestart.length() != LENGTH_NULL) {
             throw new IllegalArgumentException("[ERROR] R 나 Q를 입력해주세요.");
         }
 
-        char inputRestart = userRestart.charAt(0);
-        if(inputRestart != 'R' && inputRestart != 'Q') {
+        char inputRestart = userRestart.charAt(INPUT_POSITION);
+        if (inputRestart != 'R' && inputRestart != 'Q') {
             throw new IllegalArgumentException("[ERROR] R 나 Q를 입력해주세요.");
         }
         System.out.println(inputRestart);
