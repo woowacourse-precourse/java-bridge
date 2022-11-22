8c0d190880152a38a42758a79076f59186a10b52
baksohyeon <bori12370@gmail.com>
2022-11-22 22:48:47 +0900

    feat: 유틸 메서드 에러핸들링 로직 추가
    우테코 제출 시 오류가 발생하여 수정하였던 파일들 다시 복구함

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -5,7 +5,11 @@ import bridge.controller.BridgeGameController;
 public class Application {
 
     public static void main(String[] args){
+        try{
         BridgeGameController bridgeGameController = new BridgeGameController();
         bridgeGameController.run();
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+        }
     }
 }
