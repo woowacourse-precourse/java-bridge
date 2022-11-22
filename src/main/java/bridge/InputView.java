f2108c7f869f9f47768497ac735f588786607b87
kimsinhyun <ksh415622@naver.com>
2022-11-22 20:01:18 +0900

    feat: Bridge와 User에 관련된 모든 input을 관리하는 클래쓰 1. readBridgeSize:    - Bridge size를 입력의 유효성을 체크 및 return 2. readMoving:    - User가 어느 방향으로 갈지의 입력의 유효성 체크 및 return 3. readGameCommand    - User가 게임을 종료 혹은 다시할지의 입력의 유효성 체크 및 return

--- src/main/java/bridge/InputView.java
+++ src/main/java/bridge/InputView.java
@@ -1,5 +1,10 @@
 package bridge;
 
+import camp.nextstep.edu.missionutils.Console;
+
+import bridge.Bridge.ValidBridgeInput;
+import bridge.User.ValidUserInput;
+
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
  */
@@ -8,21 +13,34 @@ public class InputView {
     /**
      * 다리의 길이를 입력받는다.
      */
-    public int readBridgeSize() {
-        return 0;
+    public static int readBridgeSize() {
+        String bridgeSizeInput = Console.readLine();
+
+        ValidBridgeInput.checkBridgeSizeNumeric(bridgeSizeInput);
+        ValidBridgeInput.checkBridgeSizeValid(bridgeSizeInput);
+
+        return Integer.parseInt(bridgeSizeInput);
     }
 
     /**
      * 사용자가 이동할 칸을 입력받는다.
      */
-    public String readMoving() {
-        return null;
+    public static String readMoving() {
+        String userMovingInput = Console.readLine();
+
+        ValidUserInput.checkInputUpAndDown(userMovingInput);
+
+        return userMovingInput;
     }
 
     /**
      * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
      */
-    public String readGameCommand() {
-        return null;
+    public static String readGameCommand() {
+        String userCommandInput = Console.readLine();
+
+        ValidUserInput.commandIsRorQ(userCommandInput);
+
+        return userCommandInput;
     }
 }
