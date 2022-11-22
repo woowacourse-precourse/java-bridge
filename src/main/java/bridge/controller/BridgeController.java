ab7e0ac5ccf4bce72c599ab8ef2d3bb4fda90692
moonstal1506 <moonstal1506@gmail.com>
2022-11-22 20:08:36 +0900

    feat: 게임 실행

--- src/main/java/bridge/controller/BridgeController.java
+++ src/main/java/bridge/controller/BridgeController.java
@@ -4,6 +4,8 @@ package bridge.controller;
 import bridge.BridgeMaker;
 import bridge.BridgeNumberGenerator;
 import bridge.BridgeRandomNumberGenerator;
+import bridge.model.BridgeGame;
+import bridge.model.GameResult;
 import bridge.view.InputView;
 import bridge.view.OutputView;
 
@@ -18,10 +20,46 @@ public class BridgeController {
     public void run() {
         outputView.printStartMessage();
         List<String> bridge = generateBridge();
+        BridgeGame bridgeGame = new BridgeGame(bridge);
+
+        boolean gameEnd = false;
+        GameResult result = new GameResult();
+
+        while (!gameEnd) {
+            result = playGame(bridge, bridgeGame, result);
+            gameEnd = isGameEnd(result);
+            resetGame(bridgeGame, gameEnd);
+        }
+
+        outputView.printResult(result, bridge, bridgeGame.getTryNumber());
     }
 
     private List<String> generateBridge() {
         BridgeMaker bridgeMaker = new BridgeMaker(numberGenerator);
         return bridgeMaker.makeBridge(inputView.readBridgeSize());
+    }
+
+    private GameResult playGame(List<String> bridge, BridgeGame bridgeGame, GameResult result) {
+        for (int round = 0; round < bridge.size(); round++) {
+            result = bridgeGame.move(round, inputView.readMoving());
+            outputView.printMap(bridge, result.getGameResult());
+            if (result.isGameOver()) {
+                break;
+            }
+        }
+        return result;
+    }
+
+    private boolean isGameEnd(GameResult result) {
+        if (result.isGameOver()) {
+            return inputView.readGameCommand().equals("Q");
+        }
+        return true;
+    }
+
+    private void resetGame(BridgeGame bridgeGame, boolean gameEnd) {
+        if (!gameEnd) {
+            bridgeGame.retry();
+        }
     }
 }
