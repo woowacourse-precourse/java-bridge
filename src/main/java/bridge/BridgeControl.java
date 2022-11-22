ec349163b7bac59e1ea193c075515ff81e061636
leorb159 <ldgsocial159@naver.com>
2022-11-22 15:15:17 +0900

    feat(BridgeControl): BridgeControl 필드 및생성자 구현

--- src/main/java/bridge/BridgeControl.java
+++ src/main/java/bridge/BridgeControl.java
@@ -1,6 +1,20 @@
 package bridge;
 
+import  bridge.BridgeGame;
+import  bridge.InputView;
+import  bridge.OutputView;
 public class BridgeControl {
+
+    private InputView inputView;
+    private OutputView outputView;
+    private BridgeGame bridgeGame;
+
+
+    public BridgeControl() {
+        inputView = new InputView();
+        outputView = new OutputView();
+        //BridgeGame = new BridgeGame();
+    }
 
     public void start() {
         //게임 시작 메시지 출력
