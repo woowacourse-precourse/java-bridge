0c546241b2a0f30bcbdbf8a8e5a10ba25fbd3e82
jmjmjames <chungjm0711@gmail.com>
2022-11-22 14:23:42 +0900

    feat: MoveResultDto UI 콘솔화면에 Message 전달

---
+++ src/main/java/bridge/dto/MoveResultDto.java
@@ -0,0 +1,17 @@
+package bridge.dto;
+
+import bridge.domain.BridgeGameResult;
+import bridge.domain.MoveResult;
+import java.util.List;
+
+public class MoveResultDto {
+    private final List<List<MoveResult>> moveResult;
+
+    private MoveResultDto(BridgeGameResult bridgeGameResult) {
+        this.moveResult = bridgeGameResult.getResult();
+    }
+
+    public static MoveResultDto from(BridgeGameResult bridgeGameResult) {
+        return new MoveResultDto(bridgeGameResult);
+    }
+}
