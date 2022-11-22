90f9d11f12fc2bacf7a7fe82e7c8f5a41f3281aa
hwiyeon0831 <aab555586@gmail.com>
2022-11-22 20:48:33 +0900

    feat(GameResult) : 최종 게임 결과에 대한 객체 구현

---
+++ src/main/java/bridge/game/GameResult.java
@@ -0,0 +1,23 @@
+package bridge.game;
+
+public class GameResult {
+	private final GameResultCode gameResultCode;
+	private final int retryNumber;
+
+	public GameResult(GameResultCode gameResultCode, int retryNumber) {
+		this.gameResultCode = gameResultCode;
+		this.retryNumber = retryNumber;
+	}
+
+	public boolean isLosing() {
+		return GameResultCode.FAIL == gameResultCode;
+	}
+
+	public GameResultCode getGameResultCode() {
+		return gameResultCode;
+	}
+
+	public int getRetryNumber() {
+		return retryNumber;
+	}
+}
