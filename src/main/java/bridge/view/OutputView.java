429e644db224c683910dbb4ccf26a4599264a912
wjh2012 <wjh2012@naver.com>
2022-11-22 18:56:55 +0900

    feat(View): OutputView 출력문 상수화

--- src/main/java/bridge/view/OutputView.java
+++ src/main/java/bridge/view/OutputView.java
@@ -7,45 +7,62 @@ import java.util.stream.Collectors;
 
 public class OutputView {
 
+    private static final String BRIDGE_FORMAT_PREFIX = "[ ";
+    private static final String BRIDGE_FORMAT_POSTFIX = " ]";
+    private static final String BRIDGE_FORMAT_SEPARATOR = " | ";
+
+    private static final String BRIDGE_RESULT_SUCCESS = "O";
+    private static final String BRIDGE_RESULT_FAIL = "X";
+    private static final String BRIDGE_RESULT_NONE = " ";
+
+    private static final String BRIDGE_CROSS_SUCCESS = "성공";
+    private static final String BRIDGE_CROSS_FAIL = "실패";
+
+    private static final String BRIDGE_UPPER = "U";
+    private static final String BRIDGE_LOWER = "D";
+
+    private static final String ERROR_FORMAT_PREFIX = "[ERROR] ";
+
+
     public void printGameStart() {
         System.out.println("다리 건너기 게임을 시작합니다.");
     }
 
     public void printError(String msg) {
-        System.out.println("[ERROR] " + msg);
+        System.out.println(ERROR_FORMAT_PREFIX + msg);
     }
 
     public void printMap(List<MoveRecord> records) {
-        System.out.println(printFormattedLine(records, "U"));
-        System.out.println(printFormattedLine(records, "D"));
+        System.out.println(printFormattedLine(records, BRIDGE_UPPER));
+        System.out.println(printFormattedLine(records, BRIDGE_LOWER));
     }
 
     private String printFormattedLine(List<MoveRecord> records, String direction) {
         String result = "";
-        result += "[ ";
+        result += BRIDGE_FORMAT_PREFIX;
         result += printRecord(records, direction);
-        result += " ]";
+        result += BRIDGE_FORMAT_POSTFIX;
         return result;
     }
 
     private String printRecord(List<MoveRecord> records, String direction) {
         return records.stream()
                 .map(record -> printStep(record, direction))
-                .collect(Collectors.joining(" | "));
+                .collect(Collectors.joining(BRIDGE_FORMAT_SEPARATOR));
     }
 
     private String printStep(MoveRecord record, String direction) {
         if (record.getStep().equals(direction)) {
             return printOX(record);
         }
-        return " ";
+        return BRIDGE_RESULT_NONE;
     }
 
     private String printOX(MoveRecord record) {
         if (record.isSuccess()) {
-            return "O";
+            return BRIDGE_RESULT_SUCCESS;
         }
-        return "X";
+        return BRIDGE_RESULT_FAIL;
     }
 
     public void printFinalResultTitle() {
@@ -59,9 +76,9 @@ public class OutputView {
 
     private String winOrLose(boolean success) {
         if (success) {
-            return "성공";
+            return BRIDGE_CROSS_SUCCESS;
         }
-        return "실패";
+        return BRIDGE_CROSS_FAIL;
     }
 
     public void printTrial(int n) {
