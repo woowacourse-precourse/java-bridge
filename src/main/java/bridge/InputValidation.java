930c0b2b799b56d82d96a84181d6a06ac7c1b844
kim <kajinori@gachon.ac.kr>
2022-11-22 19:53:01 +0900

    refactor: validation Enum 적용 및 if문 수정

--- src/main/java/bridge/InputValidation.java
+++ src/main/java/bridge/InputValidation.java
@@ -1,10 +1,11 @@
 package bridge;
 
-import bridge.UserInput.BridgeSelection;
+import bridge.Constant.BridgeSelection;
+import bridge.Constant.GameCommand;
 
 public class InputValidation {
     public static void validateBridgeSize(int inputSize) {
-        if (inputSize < 3 || inputSize > 20) {
+        if (!(inputSize >= 3 && inputSize <= 20)) {
             throw new IllegalArgumentException("[ERROR] 다리 길이는 3부터 20 사이의 숫자여야 합니다.");
         }
     }
@@ -18,13 +19,13 @@ public class InputValidation {
     }
 
     public static void validateUpDown(String inputUpDown) {
-        if (!inputUpDown.equals(BridgeSelection.Up.get()) && !inputUpDown.equals(BridgeSelection.Down.get())) {
+        if (!(inputUpDown.equals(BridgeSelection.Up.get()) || inputUpDown.equals(BridgeSelection.Down.get()))) {
             throw new IllegalArgumentException("[ERROR] U 또는 D만 입력 가능합니다.(위: U, 아래: D)");
         }
     }
 
     public static void validateCommand(String command) {
-        if (!command.equals("R") && !command.equals("Q")) {
+        if (!(command.equals(GameCommand.RETRY.get()) || command.equals(GameCommand.QUIT.get()))) {
             throw new IllegalArgumentException("[ERROR] R 또는 Q만 입력 가능합니다.(재시도: R, 종료: Q)");
         }
     }
