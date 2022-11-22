ec3e762f9a7c855979b2587fbaf2c7b7aca9a0fd
brotherstone97 <brotherstone97@gmail.com>
2022-11-22 13:21:32 +0900

    feat(OutputView): 게임 종료 문구 출력 기능

--- src/main/java/bridge/OutputView.java
+++ src/main/java/bridge/OutputView.java
@@ -18,7 +18,17 @@ public class OutputView {
      * <p>
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void printResult() {
+    public void printResult(boolean isSuccess, int attempts) {
+        String suffix = decideSuccessOrFailure(isSuccess);
+        System.out.println("게임 성공 여부: " + suffix);
+        System.out.println("총 시도한 횟수: " + attempts);
+    }
+
+    private String decideSuccessOrFailure(boolean isSuccess) {
+        if (isSuccess) {
+            return "성공";
+        }
+        return "실패";
     }
     public void requestStartMessage(){
         System.out.println("다리 건너기 게임을 시작합니다.");
