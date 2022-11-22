f068a5414636d1f8f6799e7820004bba488bfac7
inwoo <korean5078@gmail.com>
2022-11-22 23:58:53 +0900

    perf(BridgeController) : 최종 게임결과 실패시 버그 수정

--- src/main/java/bridge/BridgeController.java
+++ src/main/java/bridge/BridgeController.java
@@ -7,10 +7,10 @@ public class BridgeController {
     private BridgeGame bridgeGame;
     private InputView inputView = new InputView();
     private OutputView outputView = new OutputView();
-    private List<String> bridge;
     private int bridgeSize;
     private boolean success = true;
     private boolean retryOption;
+    private int index = 0;
 
     public void run() {
         outputView.printGameStart();
@@ -20,21 +20,30 @@ public class BridgeController {
     public void gameStart() {
         bridgeSize = inputView.readBridgeSize();
         bridgeGame = new BridgeGame(bridgeSize);
-        for (int i = 0; i < bridgeSize; i++) {
-            String direction = inputView.readMoving();
-            retryOption = bridgeGame.move(direction);
-            outputView.printMap(bridgeGame);
-            retryOrQuit(i);
-            if (!success) break;
+        for ( index = 0; index < bridgeSize; index++) {
+            MoveAndPrint();
+            boolean x = retryOrQuit();
+            if (!x) break;
         }
         outputView.printResult(bridgeGame, success);
     }
 
-    public void retryOrQuit(int index) {
+    public boolean retryOrQuit() {
         if (!retryOption) {
             success = inputView.readGameCommand();
-            index = -1;
+            if (!success) return false;
+            indexReset();
             bridgeGame.retry();
         }
+        return true;
+    }
+
+    public void indexReset() {
+         this.index = -1;
+    }
+    public void MoveAndPrint() {
+        String direction = inputView.readMoving();
+        retryOption = bridgeGame.move(direction);
+        outputView.printMap(bridgeGame);
     }
 }
