c4eadb9e9e4e657d9e2c8530a51b4565d117c222
mgOh <issac_oh@naver.com>
2022-11-22 13:51:02 +0900

    fix: 이동 실패 시, 게임을 다시 시도할지 여부를 입력받는 기능

--- src/main/java/bridge/InputView.java
+++ src/main/java/bridge/InputView.java
@@ -33,6 +33,10 @@ public class InputView {
      * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
      */
     public String readGameCommand() {
-        return null;
+        String input = Console.readLine();
+        if (!new UserInputException().checkInputException(3, input)){
+            throw new IllegalArgumentException("[ERROR]");
+        }
+        return input;
     }
 }
