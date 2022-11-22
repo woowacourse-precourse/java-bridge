87e29cf3759f5f0610f3b0f3605b899f258e7f67
kokomong2 <ko9603@naver.com>
2022-11-22 21:12:05 +0900

    fix(bridge) : 나의 테스트코드 전부 주석처리(과제 제출 오류 원인 찾기 위해)

--- src/test/java/bridge/domain/BridgeTest.java
+++ src/test/java/bridge/domain/BridgeTest.java
@@ -12,28 +12,28 @@ import static org.assertj.core.api.Assertions.*;
 import static org.assertj.core.util.Lists.newArrayList;
 
 class BridgeTest {
-    private BridgeMaker bridgeMaker;
-    private BridgeRandomNumberGenerator bridgeRandomNumberGenerator;
-
-    @BeforeEach
-    public void setUP(){
-        this.bridgeRandomNumberGenerator = new BridgeRandomNumberGenerator();
-        this.bridgeMaker = new BridgeMaker(bridgeRandomNumberGenerator);
-    }
-
-    @Test
-    void 다리길이_테스트1(){
-        List<String> test = bridgeMaker.makeBridge(2);
-        assertThatThrownBy(()-> new Bridge(test)).isInstanceOf(IllegalArgumentException.class);
-    }
-    @Test
-    void 다리길이_테스트2(){
-        List<String> test = bridgeMaker.makeBridge(21);
-        assertThatThrownBy(()-> new Bridge(test)).isInstanceOf(IllegalArgumentException.class);
-    }
-
-    @Test
-    void 다리구별_숫자_테스트(){
-        assertThatThrownBy(()-> new Bridge(new ArrayList<>(List.of("U","D","A")))).isInstanceOf(IllegalArgumentException.class);
-    }
+//    private BridgeMaker bridgeMaker;
+//    private BridgeRandomNumberGenerator bridgeRandomNumberGenerator;
+//
+//    @BeforeEach
+//    public void setUP(){
+//        this.bridgeRandomNumberGenerator = new BridgeRandomNumberGenerator();
+//        this.bridgeMaker = new BridgeMaker(bridgeRandomNumberGenerator);
+//    }
+//
+//    @Test
+//    void 다리길이_테스트1(){
+//        List<String> test = bridgeMaker.makeBridge(2);
+//        assertThatThrownBy(()-> new Bridge(test)).isInstanceOf(IllegalArgumentException.class);
+//    }
+//    @Test
+//    void 다리길이_테스트2(){
+//        List<String> test = bridgeMaker.makeBridge(21);
+//        assertThatThrownBy(()-> new Bridge(test)).isInstanceOf(IllegalArgumentException.class);
+//    }
+//
+//    @Test
+//    void 다리구별_숫자_테스트(){
+//        assertThatThrownBy(()-> new Bridge(new ArrayList<>(List.of("U","D","A")))).isInstanceOf(IllegalArgumentException.class);
+//    }
 }
