ecbf709fd991237771ba68cd13c65a6273e1b883
desfox <desfox99@naver.com>
2022-11-22 19:51:43 +0900

    refactor(OutputView): 출력할 문구 ConsoleMessage Enum으로 대체

--- src/main/java/bridge/view/OutputView.java
+++ src/main/java/bridge/view/OutputView.java
@@ -2,6 +2,8 @@ package bridge.view;
 
 import java.util.List;
 
+import static bridge.view.ConsoleMessage.*;
+
 /**
  * 사용자에게 게임 진행 상황과 결과를 출력하는 역할을 한다.
  */
@@ -31,21 +33,21 @@ public class OutputView {
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
     public void printResult() {
-        System.out.println("최종 게임 결과");
+        System.out.println(GAME_RESULT.getMessage());
         System.out.println(mapGenerator.getUpperMap());
         System.out.println(mapGenerator.getLowerMap());
         System.out.println();
     }
 
     public void printSuccess() {
-        System.out.println("게임 성공 여부: 성공");
+        System.out.println(GAME_SUCCESS.getMessage());
     }
 
     public void printFailure() {
-        System.out.println("게임 성공 여부: 실패");
+        System.out.println(GAME_FAILURE.getMessage());
     }
 
     public void printTrialCnt(int trialCnt) {
-        System.out.println("총 시도한 횟수: " + trialCnt);
+        System.out.println(NUM_OF_TRIAL.getMessage() + trialCnt);
     }
 }
