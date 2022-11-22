bfd7223d818a806babbcfb257c52159ba089ddf2
JunTaeINC <juntea3523@naver.com>
2022-11-22 22:00:51 +0900

    feat : (OutputView) printStartGame 기능 구현

--- src/main/java/bridge/OutputView.java
+++ src/main/java/bridge/OutputView.java
@@ -1,9 +1,17 @@
 package bridge;
 
+
+import static bridge.enums.Sentence.*;
+
 /**
  * 사용자에게 게임 진행 상황과 결과를 출력하는 역할을 한다.
  */
 public class OutputView {
+
+    public static void printStartGame() {
+        System.out.println(GAME_START.getValue());
+        System.out.println();
+    }
 
     /**
      * 현재까지 이동한 다리의 상태를 정해진 형식에 맞춰 출력한다.
