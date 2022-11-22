0e7929ff3ee31587118aacdeb58070683e62c80c
juno-junho <ssmm0205@naver.com>
2022-11-22 22:09:22 +0900

    feat: create main method

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -1,8 +1,24 @@
 package bridge;
 
+import bridge.controller.BridgeGameController;
+import bridge.service.BridgeGameService;
+import bridge.view.InputView;
+import bridge.view.InputViewConverter;
+import bridge.view.OutputView;
+import bridge.view.OutputViewConverter;
+
 public class Application {
 
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        InputView inputView = new InputView(new InputViewConverter());
+        OutputView outputView = new OutputView(new OutputViewConverter());
+
+        BridgeMaker bridgeMaker = new BridgeMaker(new BridgeRandomNumberGenerator());
+        BridgeGameService bridgeGameService = new BridgeGameService(bridgeMaker);
+
+        BridgeGameController bridgeGameController = new BridgeGameController(inputView, outputView,
+                bridgeGameService);
+
+        bridgeGameController.startBridgeGame();
     }
 }
