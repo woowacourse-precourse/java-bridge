0d5170b759fc6cf43475ed66967df27d602ea3c9
wonju-dev <wonjulee.dev@gmail.com>
2022-11-22 16:25:06 +0900

    feat(MessageGenerator): 메시지 생성 기능 구현
    convertToMapImage는 파라미터로 전달받은 이차원 List를 순회하면서,
    각 List를 join한 결과 List를 반환합니다.

--- src/main/java/bridge/MessageGenerator.java
+++ src/main/java/bridge/MessageGenerator.java
@@ -1,9 +1,15 @@
 package bridge;
 
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class MessageGenerator {
 
     private static final MessageGenerator instance = new MessageGenerator();
+    private static final String START_SIGN = "[";
+    private static final String END_SIGN = "]";
+    private static final String DELIMITER = "|";
+
     public static final String GAME_FINISH_HEADER_MSG = "최종 게임 결과";
     public static final String GAME_FINISH_RESULT_SUCCESS_MSG = "게임 성공 여부: 성공";
     public static final String GAME_FINISH_RESULT_FAIL_MSG = "게임 성공 여부: 실패";
@@ -14,5 +20,13 @@ public class MessageGenerator {
 
     public static MessageGenerator getInstance() {
         return instance;
+    }
+
+    private List<String> convertToMapImage(List<List<String>> formattedMap) {
+        return formattedMap.stream()
+                .map((List<String> layer) ->
+                        START_SIGN + String.join(DELIMITER, layer) + END_SIGN
+                )
+                .collect(Collectors.toList());
     }
 }
