ef73aa4d0a90ecba232ec147f0eca62adfb61c5c
HaYoung Jung <82437499+summerlight636@users.noreply.github.com>
2022-11-22 15:18:46 +0900

    refactor: startGame 메서드 리팩토링
    startGame 메소드의 반복되는 부분을 분리

--- src/main/java/bridge/GameController.java
+++ src/main/java/bridge/GameController.java
@@ -13,20 +13,22 @@ public class GameController {
         try {
             outputView.printStartMessage();
             BridgeGame bridgeGame = new BridgeGame(inputView.readBridgeSize());
-            while(bridgeGame.keepGoing()) {
-                bridgeGame.move(inputView.readMoving());
-                outputView.printMap(bridgeGame);
-                bridgeGame.setStatus(bridgeGame.calculateStatus());
-                if(bridgeGame.getStatus().equals("실패")||bridgeGame.getStatus().equals("성공")){
-                    bridgeGame.setNumberOfTry(bridgeGame.getNumberOfTry()+1);
-                    if(bridgeGame.getStatus().equals("실패")){
-                        bridgeGame.retry(inputView.readGameCommand());
-                    }
-                }
-            }
+            loop(bridgeGame);
             outputView.printResult(bridgeGame);
         } catch (IllegalArgumentException e) {
             outputView.printErrorMessage(e.getMessage());
+        }
+    }
+
+    private void loop(BridgeGame bridgeGame) {
+        while (bridgeGame.keepGoing()) {
+            bridgeGame.move(inputView.readMoving());
+            outputView.printMap(bridgeGame);
+            bridgeGame.setStatus(bridgeGame.calculateStatus());
+            if (bridgeGame.getStatus().equals("실패") || bridgeGame.getStatus().equals("성공")) {
+                bridgeGame.setNumberOfTry(bridgeGame.getNumberOfTry() + 1);
+                if (bridgeGame.getStatus().equals("실패")) bridgeGame.retry(inputView.readGameCommand());
+            }
         }
     }
 }
