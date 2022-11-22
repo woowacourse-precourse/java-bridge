3684f217f97b1b3700d3d9d32be5f64f41704c7f
dlgosla <dlgoslaek1@gmail.com>
2022-11-22 13:40:17 +0900

    feat: 게임 시작

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -4,5 +4,13 @@ public class Application {
 
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        try {
+            System.out.println("다리 건너기 게임을 시작합니다.");
+            new Controller().run();
+        } catch (IllegalArgumentError ignored) {
+
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
     }
 }
