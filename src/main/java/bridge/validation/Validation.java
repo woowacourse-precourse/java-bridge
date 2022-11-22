d95e19805e299fae47ff8dab7359a0ecd3672e98
dong yeol <ehfql6363@naver.com>
2022-11-22 15:12:29 +0900

    FEAT(Validation.java) : Create validation class
    Signed-off-by: dong yeol <ehfql6363@naver.com>

--- src/main/java/bridge/validation/Validation.java
+++ src/main/java/bridge/validation/Validation.java
@@ -1,45 +1,47 @@
-package bridge;
+package bridge.validation;
 
-import java.util.Objects;
+import static bridge.validation.ErrorMessage.ERROR_INVALID_BRIDGE_SIZE;
+import static bridge.validation.ErrorMessage.ERROR_INVALID_GAME_COMMAND;
+import static bridge.validation.ErrorMessage.ERROR_INVALID_MOVE_COMMAND;
+
+import bridge.command.GameCommand;
+import bridge.command.MoveCommand;
 
 public class Validation {
-    public static int validateInputBridgeSize(String bridgeSize) {
-        int size = validateParesInt(bridgeSize);
-        validateRange(size);
-        return size;
-    }
-
-    public static int validateParesInt(String stringNumber) {
-        try {
-            return Integer.parseInt(stringNumber);
-        } catch (NumberFormatException e) {
-            throw new IllegalArgumentException("다리의 길이를 숫자로 입력해주세요.");
+    public static void validateGameCommand(String userInput) {
+        if (!GameCommand.exist(userInput)) {
+            throw new IllegalArgumentException(ERROR_INVALID_GAME_COMMAND);
         }
     }
 
-    public static void validateRange(int bridgeSize) {
-        if (bridgeSize > 21 || bridgeSize < 3) {
-            throw new IllegalArgumentException("다리의 길이는 3부터 20사이의 숫자입니다.");
+    public static void validateMoveCommand(String userInput) {
+        if (!MoveCommand.exist(userInput)) {
+            throw new IllegalArgumentException(ERROR_INVALID_MOVE_COMMAND);
         }
     }
 
-    public static void validateMoving(String move) {
-        if (!Objects.equals(move, "U") && !Objects.equals(move, "D")) {
-            throw new IllegalArgumentException("다리를 건너기 위해 이동할 수 있는 칸은 U(위) / D(아래) 입니다.");
-        }
+    public static void validateBridgeSize(String userInput) {
+        isEmpty(userInput);
+        isNumeric(userInput);
+        isBridgeSizeInRange(userInput);
+    }
 
-        if (move.length() != 1) {
-            throw new IllegalArgumentException("다리를 건너기 위해 위 / 아래 중 한 곳만 선택할 수 있습니다.");
+    private static void isEmpty(String userInput) {
+        if (userInput.isEmpty()) {
+            throw new IllegalArgumentException(ERROR_INVALID_BRIDGE_SIZE);
         }
     }
 
-    public static void validateReGame(String message) {
-        if (!Objects.equals(message, "R") && !Objects.equals(message, "Q")) {
-            throw new IllegalArgumentException("게임을 재시작하려면 R / 종료하려면 Q를 입력하세요.");
+    private static void isNumeric(String userInput) {
+        if (!userInput.matches("[0-9]+")) {
+            throw new IllegalArgumentException(ERROR_INVALID_BRIDGE_SIZE);
         }
+    }
 
-        if (message.length() != 1) {
-            throw new IllegalArgumentException("게임 재시작 / 종료에 대한 입력값을 하나만 입력해주세요.");
+    private static void isBridgeSizeInRange(String userInput) {
+        int bridgeSize = Integer.parseInt(userInput);
+        if (bridgeSize < 3 || bridgeSize > 20) {
+            throw new IllegalArgumentException(ERROR_INVALID_BRIDGE_SIZE);
         }
     }
 }
