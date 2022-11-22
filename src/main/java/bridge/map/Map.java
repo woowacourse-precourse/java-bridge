f4e49a30642939dddfc53d9d893b3c2274df81e6
jihun-24k <gi020477@gmail.com>
2022-11-22 13:41:02 +0900

    Feat(Map): 맵 출력을 위한 Enum 생성

---
+++ src/main/java/bridge/map/Map.java
@@ -0,0 +1,19 @@
+package bridge.map;
+
+public enum Map {
+    BRIDGE_START("[ "),
+    BRIDGE_END(" ]"),
+    BLOCK_SECTION(" | "),
+    BLOCK_CAN_MOVE("O"),
+    BLOCK_CANT_MOVE("X");
+
+    private String mark;
+
+    private Map(String mark) {
+        this.mark = mark;
+    }
+
+    public String getMark() {
+        return mark;
+    }
+}
