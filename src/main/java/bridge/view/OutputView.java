b362e8f381e627b521d76dbdb080827d62df1577
sujin0607 <goweg0607@gmail.com>
2022-11-22 23:32:42 +0900

    feat: map을 출력하는 메서드 추가

--- src/main/java/bridge/view/OutputView.java
+++ src/main/java/bridge/view/OutputView.java
@@ -9,8 +9,12 @@ import java.util.List;
 public class OutputView {
     private DrawMap drawMap = new DrawMap();
 
-    public void setUpDrawMap() {
+    public void clearDrawMap() {
         this.drawMap = new DrawMap();
+    }
+
+    public void setUpDrawMap(String user, String upDown){
+        drawMap.map(user, upDown);
     }
 
     /**
@@ -18,23 +22,22 @@ public class OutputView {
      * <p>
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void printMap(String user, String upDown) {
-        drawMap.map(user, upDown);
+    public void printMap() {
         List<String> topList = drawMap.getTopList();
         List<String> bottomList = drawMap.getBottomList();
-        System.out.printf("[");
-        for(int i=0; i < topList.size(); i++){
-            System.out.printf(" %s |",topList.get(i));
-        }
-        System.out.printf("]");
-        System.out.println("");
-        System.out.printf("[");
-        for(int i=0; i < bottomList.size(); i++){
-            System.out.printf(" %s |",bottomList.get(i));
-        }
-        System.out.printf("]");
-        System.out.println("");
+        printList(topList);
+        printList(bottomList);
+    }
 
+    public void printList(List<String> list){
+        System.out.printf("[ ");
+        for (int i = 0; i < list.size(); i++) {
+            System.out.printf("%s", list.get(i));
+            if (i != list.size()-1) {
+                System.out.printf(" | ");
+            }
+        }
+        System.out.printf(" ]\n");
     }
 
     /**
@@ -44,15 +47,25 @@ public class OutputView {
      */
     public void printResult(int gameCount, boolean gameStatus) {
         System.out.println("최종 게임 결과");
+        printMap();
         System.out.println("게임 성공 여부: " + printSucess(gameStatus));
         System.out.println("총 시도한 횟수: " + gameCount);
     }
 
+    /**
+     * 게임 상태를 출력한다
+     * @param gameStatus
+     * @return 성공, 실패
+     */
     public String printSucess(boolean gameStatus) {
         if (!gameStatus) {
             return "성공";
         }
         return "실패";
+    }
+
+    public String printStart(){
+        return "다리 건너기 게임을 시작합니다.";
     }
 
 }
