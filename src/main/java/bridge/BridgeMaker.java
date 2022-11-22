50271b058ff5af0448825642084eedbe13e4542b
이규승 <rpg8988@naver.com>
2022-11-22 22:49:38 +0900

    feat: 게임 프로젝트 설정

--- src/main/java/bridge/BridgeMaker.java
+++ src/main/java/bridge/BridgeMaker.java
@@ -1,23 +1,33 @@
 package bridge;
 
+import bridge.EnumClass.EnumCommand;
+
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
-/**
- * 다리의 길이를 입력 받아서 다리를 생성해주는 역할을 한다.
- */
 public class BridgeMaker {
-
     private final BridgeNumberGenerator bridgeNumberGenerator;
 
     public BridgeMaker(BridgeNumberGenerator bridgeNumberGenerator) {
         this.bridgeNumberGenerator = bridgeNumberGenerator;
     }
 
-    /**
-     * @param size 다리의 길이
-     * @return 입력받은 길이에 해당하는 다리 모양. 위 칸이면 "U", 아래 칸이면 "D"로 표현해야 한다.
-     */
     public List<String> makeBridge(int size) {
+        List<Integer> result = new ArrayList<>();
+        for (int i = 0; i < size; i++) {
+            result.add(bridgeNumberGenerator.generate());
+        }
+        return result.stream().map(this::mapToCommand).collect(Collectors.toList());
+    }
+
+    private String mapToCommand(int command){
+        if(command == EnumCommand.Up.getWorth()){
+            return EnumCommand.Up.getCommand();
+        }
+        if(command == EnumCommand.Down.getWorth()){
+            return EnumCommand.Down.getCommand();
+        }
         return null;
     }
 }
