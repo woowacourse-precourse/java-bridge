313352e5c26725f2905129a56cc18a6a20fd2cc2
nahyeon <whskgus12@nate.com>
2022-11-22 13:14:55 +0900

    feat(Exception): 예외 처리 함수 생성
    private으로 선언한 예외처리 함수에 접근할 수 있도록 재게임 여부, 길이 , 방향 3가지 카테고리를 나눠 public 함수를 생성합니다.

--- src/main/java/bridge/Exception/BridgeInputException.java
+++ src/main/java/bridge/Exception/BridgeInputException.java
@@ -4,6 +4,21 @@ import java.util.regex.Pattern;
 
 public class BridgeInputException {
 
+    public void validBridgeRetry(String retry) {
+        validInputFormRetry(retry);
+        validInputSizeRetry(retry);
+    }
+
+    public void validBridgeLength(String size) {
+        validInputFormLength(size);
+        validInputRangeLength(size);
+    }
+
+    public void validMoveDirection(String direction) {
+        validInputFormMove(direction);
+        validInputSizeMove(direction);
+    }
+
     private void validInputFormRetry(String retry) {
         String regexInput = "^[QR]*$";
         if (!Pattern.matches(regexInput, retry)) {
@@ -38,7 +53,7 @@ public class BridgeInputException {
             throw new IllegalArgumentException("[ERROR] 이동할 칸은 U 또는 D만 입력하세요.");
         }
     }
-    
+
     private void validInputSizeMove(String direction) {
         if (direction.length() != 1) {
             throw new IllegalArgumentException("[ERROR] 이동할 칸은 한 문자만 입력하세요.");
