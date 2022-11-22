150a09ae49410cba0a9b8e6a44c0d9ff9319543e
eun-byeol <62530884+eun-byeol@users.noreply.github.com>
2022-11-22 19:18:40 +0900

    feat(BridgeGame): bridgeIndex 인스턴스 변수 추가
    move 메서드에서 isCorrectMoving 호출 후 결과가 false일 경우 bridgeIndex를 0으로 초기화

--- src/main/java/bridge/domain/BridgeGame.java
+++ src/main/java/bridge/domain/BridgeGame.java
@@ -5,6 +5,7 @@ import bridge.enums.CommandType;
 public class BridgeGame {
     private GameResult gameResult;
     private GameReferee referee;
+    private int bridgeIndex = 0;
     private int numberOfAttempts = 1;
 
     public BridgeGame(Bridge bridge) {
@@ -21,14 +22,18 @@ public class BridgeGame {
     }
 
     public boolean move(PlayerMoving playerMoving) {
-        boolean isCorrect = referee.isCorrectMoving(playerMoving);
+        boolean isCorrect = referee.isCorrectMoving(playerMoving, bridgeIndex++);
         gameResult.addGameResult(isCorrect, playerMoving);
+        if (!isCorrect) {
+            bridgeIndex = 0;
+        }
         return isCorrect; //맞추면 true, 틀리면 false
     }
 
     public boolean retry(PlayerCommand playerCommand) {
-        if (playerCommand.equals(CommandType.valueOf("RETRY"))){
+        if (playerCommand.getCommand().equals(CommandType.valueOf("RETRY").getInitial())){
             numberOfAttempts++;
+            gameResult = new GameResult();
             return true;
         }
         return false;
