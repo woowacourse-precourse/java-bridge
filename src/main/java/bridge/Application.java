0584a370ed57abc8a43f757054b801575e3b5600
chanminmaxscaler <jcm04@naver.com>
2022-11-22 22:41:57 +0900

    feat: Application 클래스의 main 메소드를 구현한다
    Application 클래스는 main 메소드에서 BridgeGameController 객체를 생성해서 runGameMain 메소드를 호출하여 게임을 실행한다.

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -1,11 +1,13 @@
 package bridge;
 
 import bridge.controller.BridgeGameController;
+import bridge.view.InputView;
+import bridge.view.OutputView;
 
 public class Application {
 
     public static void main(String[] args) {
-        BridgeGameController gameController = new BridgeGameController();
+        BridgeGameController gameController = new BridgeGameController(new InputView(), new OutputView());
         gameController.runGameMain();
     }
 }
