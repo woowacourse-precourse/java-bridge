0929f0246e94a3a3ba6562b2d3e5ca7fa61cd699
gmu1026 <gmu1026@gmail.com>
2022-11-22 20:30:04 +0900

    feat: 게임 상태 확인 및 재시작 기능 구현

--- src/main/java/bridge/BridgeGame.java
+++ src/main/java/bridge/BridgeGame.java
@@ -7,15 +7,17 @@ import java.util.List;
  * 다리 건너기 게임을 관리하는 클래스
  */
 public class BridgeGame {
-    private List<String> playerCommands;
+    private List<String> playerCommands = new ArrayList<>();
     private int playerMoveCount = 0;
     private GameMap gameMap = new GameMap();
-    private boolean isOver = false;
-
+    private GameStatus gameStatus;
+    private int playCount;
     private List<String> bridge; // 직접 접근을 막고 bridge to map 으로 반환하여 출력하게끔
+
     public BridgeGame(List<String> bridge) {
         this.bridge = bridge;
-        this.playerCommands = new ArrayList<>();
+        playCount = 1;
+        gameStatus = GameStatus.PLAYING;
     }
 
     /**
@@ -26,25 +28,34 @@ public class BridgeGame {
     public void move(String moveCommand) {
         playerCommands.add(moveCommand);
         playerMoveCount++;
-        gameMap.add(moveCommand, checkMoveResult(moveCommand));
-    }
-
-    private void checkMovable() {
-        if (playerMoveCount == bridge.size()) {
-            isOver = true;
-        }
+        String moveResult = checkMoveResult(moveCommand);
+        checkFailed(moveResult);
+        gameMap.add(moveCommand, moveResult);
+        checkFinished();
     }
 
     private String checkMoveResult(String moveCommand) {
         String result = "";
-        if (moveCommand.equals(bridge.get(playerMoveCount))) {
+        if (moveCommand.equals(bridge.get(playerMoveCount - 1))) {
             result = "O";
         }
 
-        if (!moveCommand.equals(bridge.get(playerMoveCount))) {
+        if (!moveCommand.equals(bridge.get(playerMoveCount - 1))) {
             result = "X";
         }
         return result;
+    }
+
+    private void checkFinished() {
+        if (gameStatus != GameStatus.GAME_OVER && playerMoveCount == bridge.size()) {
+            gameStatus = GameStatus.GAME_CLEAR;
+        }
+    }
+
+    private void checkFailed(String moveResult) {
+        if (moveResult.equals("X")) {
+            gameStatus = GameStatus.GAME_OVER;
+        }
     }
 
     /**
@@ -53,12 +64,34 @@ public class BridgeGame {
      * 재시작을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
     public void retry() {
+        playCount++;
+        playerCommands.clear();
+        playerMoveCount = 0;
+        gameMap.clear();
+        gameStatus = GameStatus.PLAYING;
     }
 
     public GameMap getGameMap() {
         return gameMap;
     }
+
+    public boolean isPlaying() {
+        return gameStatus.equals(GameStatus.PLAYING);
+    }
+
     public boolean isOver() {
-        return isOver;
+        return gameStatus.equals(GameStatus.GAME_OVER);
+    }
+
+    public boolean isClear() {
+        return gameStatus.equals(GameStatus.GAME_CLEAR);
+    }
+
+    public int getPlayCount() {
+        return playCount;
+    }
+
+    public GameStatus getGameStatus() {
+        return gameStatus;
     }
 }
