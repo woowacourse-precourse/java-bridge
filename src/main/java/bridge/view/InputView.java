5d16ace7b1af1f30f1d01ae7e0a36ffaa964eb79
aak2075 <35948985+aak2075@users.noreply.github.com>
2022-11-22 12:52:24 +0900

    feat(bridge) : 잘못된 값 입력시 에러 발생후 재입력 받는 기능 추가

--- src/main/java/bridge/view/InputView.java
+++ src/main/java/bridge/view/InputView.java
@@ -11,14 +11,22 @@ public class InputView {
 
     private static final String SIZE_RANGE_ERROR = "[ERROR] 다리의 길이는 3 이상 20 이하 입니다.";
     private static final String SIZE_NUMBER_ERROR = "[ERROR] 다리의 길이는 숫자만 입력 가능합니다.";
+    private static final String MOVING_ERROR = "[ERROR] 이동은 U와 D로만 가능합니다.";
+    private static final String GAME_COMMAND_ERROR = "[ERROR] 게임 재시작과 종료는 R과 Q로만 가능합니다";
     private static final int MIN_SIZE = 3;
     private static final int MAX_SIZE = 20;
     /**
      * 다리의 길이를 입력받는다.
      */
     public int readBridgeSize() {
-        int size = stringToInteger(Console.readLine());
-        validateSize(size);
+        int size;
+        try {
+            size = stringToInteger(Console.readLine());
+            validateSize(size);
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+            return readBridgeSize();
+        }
         return size;
     }
 
@@ -40,13 +48,23 @@ public class InputView {
      * 사용자가 이동할 칸을 입력받는다.
      */
     public Moving readMoving() {
-        return Moving.valueOf(Console.readLine());
+        try {
+            return Moving.valueOf(Console.readLine());
+        } catch (IllegalArgumentException e) {
+            System.out.println(MOVING_ERROR);
+            return readMoving();
+        }
     }
 
     /**
      * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
      */
     public GameCommand readGameCommand() {
-        return GameCommand.valueOf(Console.readLine());
+        try {
+            return GameCommand.valueOf(Console.readLine());
+        } catch (IllegalArgumentException e) {
+            System.out.println(GAME_COMMAND_ERROR);
+            return readGameCommand();
+        }
     }
 }
