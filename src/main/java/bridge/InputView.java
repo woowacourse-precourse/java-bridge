e5dd99c43d04778167904e9b448b890ef10a312c
Polite-Jehu <werber112@kyonggi.ac.kr>
2022-11-22 09:47:43 +0900

    refactor(bridge): remove duplicate code
    readGameCommand, readMoving, readBridgeSize has same code
    then combine validInput method

--- src/main/java/bridge/InputView.java
+++ src/main/java/bridge/InputView.java
@@ -2,8 +2,6 @@ package bridge;
 
 import camp.nextstep.edu.missionutils.Console;
 
-import java.util.List;
-
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
  */
@@ -14,13 +12,7 @@ public class InputView {
      */
     public int readBridgeSize() {
         String input = Console.readLine();
-        String checkMessage = this.checkMessage(input,ErrorMessage.BRIDGELENGTHERROREXCEPTION);
-        while(!checkMessage.equals("PASS"))
-        {
-            System.out.println(checkMessage);
-            input = Console.readLine();
-            checkMessage = this.checkMessage(input,ErrorMessage.BRIDGELENGTHERROREXCEPTION);
-        }
+        input = validInput(input,ErrorMessage.BRIDGELENGTHERROREXCEPTION);
         return Integer.parseInt(input);
     }
 
@@ -29,13 +21,7 @@ public class InputView {
      */
     public String readMoving() {
         String input = Console.readLine();
-        String checkMessage = this.checkMessage(input,ErrorMessage.WRONGMOVEINPUTEXCEPTION);
-        while(!checkMessage.equals("PASS"))
-        {
-            System.out.println(checkMessage);
-            input = Console.readLine();
-            checkMessage = this.checkMessage(input,ErrorMessage.WRONGMOVEINPUTEXCEPTION);
-        }
+        input = validInput(input,ErrorMessage.WRONGMOVEINPUTEXCEPTION);
         return input;
     }
 
@@ -43,51 +29,52 @@ public class InputView {
      * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
      */
     public String readGameCommand() {
-        String input =Console.readLine();
-        String checkMessage = this.checkMessage(input,ErrorMessage.WRONGGAMEENDINPUTEXCEPTION);
-        while(!checkMessage.equals("PASS"))
-        {
+        String input = Console.readLine();
+        input = validInput(input,ErrorMessage.WRONGGAMEENDINPUTEXCEPTION);
+        return input;
+    }
+
+    public String validInput(String input,ErrorMessage errorMessage){
+        String checkMessage = this.checkMessage(input, errorMessage);
+        while (!checkMessage.equals(ErrorMessage.PASS.getErrorMessage())) {
             System.out.println(checkMessage);
             input = Console.readLine();
-            checkMessage = this.checkMessage(input,ErrorMessage.WRONGGAMEENDINPUTEXCEPTION);
+            checkMessage = this.checkMessage(input, errorMessage);
         }
         return input;
     }
 
-    public String checkMessage(String value, ErrorMessage errorValue){
+    public String checkMessage(String value, ErrorMessage errorValue) {
         StringBuilder stringBuilder = new StringBuilder("[ERROR] ");
-        if(errorValue.equals(ErrorMessage.BRIDGELENGTHERROREXCEPTION))
+        if (errorValue.equals(ErrorMessage.BRIDGELENGTHERROREXCEPTION))
             errorValue = bridgeLengthChecker(value);
-        if(errorValue.equals(ErrorMessage.WRONGGAMEENDINPUTEXCEPTION))
+        if (errorValue.equals(ErrorMessage.WRONGGAMEENDINPUTEXCEPTION))
             errorValue = endGameChecker(value);
-        if(errorValue.equals(ErrorMessage.WRONGMOVEINPUTEXCEPTION))
+        if (errorValue.equals(ErrorMessage.WRONGMOVEINPUTEXCEPTION))
             errorValue = moveInputChecker(value);
-        if(!errorValue.equals(ErrorMessage.PASS))
+        if (!errorValue.equals(ErrorMessage.PASS))
             return stringBuilder.append(errorValue.getErrorMessage()).toString();
         return ErrorMessage.PASS.toString();
     }
 
     private ErrorMessage moveInputChecker(String value) {
-        if(value.equals(BridgeJoyStick.U.toString())||value.equals(BridgeJoyStick.D.toString()))
+        if (value.equals(BridgeJoyStick.U.toString()) || value.equals(BridgeJoyStick.D.toString()))
             return ErrorMessage.PASS;
         return ErrorMessage.WRONGMOVEINPUTEXCEPTION;
     }
 
     private ErrorMessage endGameChecker(String value) {
-        if(value.equals("R")||value.equals("Q"))
+        if (value.equals(BridgeJoyStick.R.toString()) || value.equals(BridgeJoyStick.Q.toString()))
             return ErrorMessage.PASS;
         return ErrorMessage.WRONGGAMEENDINPUTEXCEPTION;
     }
 
-    public ErrorMessage bridgeLengthChecker(String value)
-    {
-        try{
+    public ErrorMessage bridgeLengthChecker(String value) {
+        try {
             int length = Integer.parseInt(value);
-            if(length<3||length>20)
+            if (length < 3 || length > 20)
                 throw new NumberFormatException();
-        }
-        catch(NumberFormatException e)
-        {
+        } catch (NumberFormatException e) {
             return ErrorMessage.BRIDGELENGTHERROREXCEPTION;
         }
         return ErrorMessage.PASS;
