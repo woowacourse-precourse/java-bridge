9f2ded9fbbe1b97d30ad0a8f7729cf2219c2b47d
이지영 <wldudwldhd@naver.com>
2022-11-22 22:04:39 +0900

    fix:결과 출력 리스트 오류 수정

--- src/main/java/bridge/OutputView.java
+++ src/main/java/bridge/OutputView.java
@@ -7,21 +7,102 @@ import java.util.List;
  * 사용자에게 게임 진행 상황과 결과를 출력하는 역할을 한다.
  */
 public class OutputView {
-
     /**
      * 현재까지 이동한 다리의 상태를 정해진 형식에 맞춰 출력한다.
      * <p>
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
     public void printMap(int num, List<String> bridge, int isAnswer) {
-        if(num == 1){
+        if(num == 1) {
             printOneValue(bridge, isAnswer);
             return;
         }
-        printManyValue(num, bridge, isAnswer);
+        printManyValueList(num, bridge, isAnswer);
         return;
-
     }
+
+    /**
+     * 게임의 최종 결과를 정해진 형식에 맞춰 출력한다.
+     * <p>
+     * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
+     */
+    public void printResult(int num, List<String> bridge, int isAnswer) {
+        System.out.println("최종 게임 결과");
+        printMap(num, bridge, isAnswer);
+        System.out.println("");
+        if(isAnswer == 0 ) System.out.println("게임 성공 여부: 실패");
+        if(isAnswer == 1 ) System.out.println("게임 성공 여부: 성공");
+    }
+
+    public void printManyValueList(int num, List<String> bridge, int isAnswer) {
+        if(isAnswer == 0) {
+            printIsFailedValueList(bridge, num);
+            return;
+        }
+        if(isAnswer == 1) {
+            printIsSuccessValueList(bridge, num);
+            return;
+        }
+    }
+
+    public void printIsSuccessValueList(List<String> bridge, int num) {
+        List<String> upString = new ArrayList<>();
+        List<String> downString = new ArrayList<>();
+
+        for(int i = 0; i < num; i++) {
+            if(i > 0) printLine(upString, downString);
+            checkValue(bridge.get(i).charAt(0), upString, downString);
+        }
+
+        System.out.println("[ " + String.join(",",upString).replace(",", "") + "]");
+        System.out.println("[ " + String.join(",",downString).replace(",","")+ "]");
+    }
+
+    public void printIsFailedValueList(List<String> bridge, int num) {
+        List<String> upString = new ArrayList<>();
+        List<String> downString = new ArrayList<>();
+
+
+        for(int i = 0; i < num-1; i++) {
+            if(i > 0) printLine(upString, downString);
+            checkValue(bridge.get(i).charAt(0), upString, downString);
+        }
+        printLine(upString, downString);
+        checkFailedValue(bridge.get(num-1).charAt(0), upString, downString);
+
+
+        System.out.println("[ " + String.join(",",upString).replace(",", "") + "]");
+        System.out.println("[ " + String.join(",",downString).replace(",","")+ "]");
+    }
+
+    public void printOneValue(List<String> bridge, int isAnswer) {
+        List<String> upString = new ArrayList<>();
+        List<String> downString = new ArrayList<>();
+
+        if(isAnswer == 1) checkValue(bridge.get(0).charAt(0), upString, downString);
+        if(isAnswer == 0) checkFailedValue(bridge.get(0).charAt(0), upString, downString);
+        System.out.println("[ " + String.join(",",upString).replace(",", "") + "]");
+        System.out.println("[ " + String.join(",",downString).replace(",","")+ "]");
+    }
+
+    public void checkValue(char c, List<String> upSting, List<String> downString) {
+        if(c == 'U') {
+            printUp(upSting, downString);
+        }
+        if(c == 'D') {
+            printDown(upSting, downString);
+        }
+    }
+
+    public void checkFailedValue(char c, List<String> upString, List<String> downString) {
+        if(c == 'U') {
+            printDownFailed(upString, downString);
+        }
+        if(c == 'D') {
+            printUpFailed(upString, downString);
+        }
+    }
+
     public void printLine(List<String> upString, List<String> downString) {
         upString.add("| ");
         downString.add("| ");
@@ -46,99 +127,9 @@ public class OutputView {
         upString.add("  ");
         downString.add("X ");
     }
-    /**
-     * 게임의 최종 결과를 정해진 형식에 맞춰 출력한다.
-     * <p>
-     * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
-     */
-    public void printResult(int num, List<String> bridge, int isAnswer) {
-        System.out.println("최종 게임 결과");
-        printMap(num, bridge, isAnswer);
-        System.out.println("");
-        if(isAnswer == 0){
-            System.out.println("게임 성공 여부 : 실패");
-        }
-        if(isAnswer == 1){
-            System.out.println("게임 성공 여부 : 성공");
-        }
-
-    }
-    public void checkValue(char c, List<String> upSting, List<String> downString) {
-        if(c == 'U') {
-            printUp(upSting, downString);
-        }
-        if(c == 'D') {
-            printDown(upSting, downString);
-        }
-    }
-
-    public void checkFailedValue(char c, List<String> upString, List<String> downString) {
-        if(c == 'U') {
-            printDownFailed(upString, downString);
-        }
-        if(c == 'D') {
-            printUpFailed(upString, downString);
-        }
-    }
-    public void printOneValue(List<String> bridge, int isAnswer){
-        List<String> upString = new ArrayList<>();
-        List<String> downString = new ArrayList<>();
-
-        if(isAnswer == 1){
-            checkValue(bridge.get(0).charAt(0), upString, downString);
-        }
-        if(isAnswer == 0){
-            checkFailedValue(bridge.get(0).charAt(0), upString, downString);
-        }
-        System.out.println("[ " + String.join(",",upString).replace(",", "") + "]");
-        System.out.println("[ " + String.join(",",downString).replace(",","")+ "]");
-    }
-    public void printManyValue(int num, List<String> bridge, int isAnswer) {
-        if(isAnswer == 0) {
-            printIsFailedValue(bridge, num);
-            return;
-        }
-        if(isAnswer == 1) {
-            printIsSuccessValue(bridge, num);
-            return;
-        }
-    }
-
-    public void printIsSuccessValue(List<String> bridge, int num) {
-        List<String> upString = new ArrayList<>();
-        List<String> downString = new ArrayList<>();
-
-        for(int i = 0; i < num; i++) {
-            if(i > 0) printLine(upString, downString);
-            checkValue(bridge.get(i).charAt(0), upString, downString);
-        }
-
-        System.out.println("[ " + String.join(",",upString).replace(",", "") + "]");
-        System.out.println("[ " + String.join(",",downString).replace(",","")+ "]");
-    }
-
-    public void printIsFailedValue(List<String> bridge, int num) {
-        List<String> upString = new ArrayList<>();
-        List<String> downString = new ArrayList<>();
-
-
-        for(int i = 0; i < num-1; i++) {
-            if(i > 0) printLine(upString, downString);
-            checkValue(bridge.get(i).charAt(0), upString, downString);
-        }
-        printLine(upString, downString);
-        checkFailedValue(bridge.get(num-1).charAt(0), upString, downString);
-
-
-        System.out.println("[ " + String.join(",",upString).replace(",", "") + "]");
-        System.out.println("[ " + String.join(",",downString).replace(",","")+ "]");
-    }
 
     public void printGameCnt(int gameCnt) {
         System.out.println("총 시도한 횟수: "+gameCnt);
-    }
-    public void printMoveQuestion() {
-        System.out.println("이동할 칸을 선택해주세요. (위: U, 아래: D)");
     }
 
     public void printStartComment() {
@@ -146,9 +137,12 @@ public class OutputView {
         System.out.println("");
         System.out.println("다리의 길이를 입력해주세요.");
     }
-    public void printRestartQuestion(){
-        System.out.println("게임을 다시 시도하지 여부를 입력해주세요. (재시도: R, 종료: Q");
+
+    public void printMoveQuestion() {
+        System.out.println("이동할 칸을 선택해주세요. (위: U, 아래: D)");
     }
 
-
+    public void printRestartQuestion() {
+        System.out.println("게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)");
+    }
 }
