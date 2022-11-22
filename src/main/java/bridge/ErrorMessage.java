6b0fd974e7f4e508dfed5308ddba4f06aef77168
whrod <whrodtube@gmail.com>
2022-11-22 15:10:57 +0900

    Feat : ErrorMessage Enum Create

---
+++ src/main/java/bridge/ErrorMessage.java
@@ -0,0 +1,4 @@
+package bridge;
+
+public enum ErrorMessage {
+}
--- src/main/java/bridge/InputValidation.java
+++ src/main/java/bridge/InputValidation.java
@@ -1,11 +1,28 @@
-//package bridge;
-//
-//public class InputValidation {
-//
-//    public void SizeValidation() {
-//        InputView inputView = new InputView();
-//        int size = inputView.readBridgeSize();
-//
-//    }
-//
-//}
+package bridge;
+
+public class InputValidation {
+
+    public static void validateBridgeSize (String bridgeSize){
+        try{
+            int parseIntBridgeSize = Integer.parseInt(bridgeSize);
+            if(parseIntBridgeSize < 3 || parseIntBridgeSize >20){
+                throw new IllegalArgumentException(ErrorMessage.errorMessage.ONLY_INPUT_BETWEEN_3_20.getValue());
+            }
+        }catch (NumberFormatException e){
+            throw new IllegalArgumentException(ErrorMessage.errorMessage.ONLY_INPUT_INTEGER.getValue());
+        };
+
+    }
+    public static void validateMoving (String moving){
+        if(!Objects.equals(moving, "U") && !Objects.equals(moving, "D")){
+            throw new IllegalArgumentException(ErrorMessage.errorMessage.ONLY_INPUT_U_AND_D.getValue());
+        }
+    }
+
+    public static void validateIsRestart (String isRestart){
+        if(!Objects.equals(isRestart, "R") && !Objects.equals(isRestart, "Q")){
+            throw new IllegalArgumentException((ErrorMessage.errorMessage.ONLY_INPUT_R_AND_Q.getValue()));
+        }
+    }
+}
+}
