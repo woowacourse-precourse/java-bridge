196ad46da641624967fb52dbd7c509b2a63bfddd
changeme4585 <benscience@naver.com>
2022-11-22 16:23:45 +0900

    add: 최종결과 출력 기능구현

--- src/main/java/bridge/OutputView.java
+++ src/main/java/bridge/OutputView.java
@@ -69,11 +69,20 @@ public class OutputView {
         System.out.println(up);
         System.out.println(down+"\n");
     }
+    public  String  getCheckFlag() {
+        return checkFlag;
+    }
     /**
      * 게임의 최종 결과를 정해진 형식에 맞춰 출력한다.
      * <p>
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void printResult() {
+    public void printResult(String flag,String R) {
+        System.out.println("최종 게임 결과");
+        System.out.println(up);
+        System.out.println(down);
+        System.out.println("");
+        System.out.println("게임 성공 여부: "+flag);//성공 or 실패 변수
+        System.out.println("총 시도한 횟수: "+R);
     }
 }
