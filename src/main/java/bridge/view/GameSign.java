2c9f32ad5794befbde2a4adf961a95451f2cc3bf
jijongkwon <help3451@naver.com>
2022-11-22 17:19:15 +0900

    feat(GameSign) : 게임 기호 메시지 구현

---
+++ src/main/java/bridge/view/GameSign.java
@@ -0,0 +1,18 @@
+package bridge.view;
+
+public enum GameSign {
+    UP_MOVING("U"),
+    DOWN_MOVING("D"),
+    MOVING_SUCCESS(" O "),
+    MOVING_FAIL(" X "),
+    MOVING_NOT("   "),
+    RETRY("R");
+    private final String gameSign;
+    GameSign(String message){
+        this.gameSign = message;
+    }
+
+    public String getMessage() {
+        return gameSign;
+    }
+}
