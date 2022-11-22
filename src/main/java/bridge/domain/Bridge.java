f39613ad66a7c09897547a0f97626402fe1417e9
ycopp9 <aciees95@gmail.com>
2022-11-22 15:03:49 +0900

    feat(Bridge): 스켈레톤 작성 및 초기화를 위한 reset 메소드 작성
    Bridge 클래스에 필요한 멤버 변수 및 재게임을 위한 유저가 입력한 정보
    초기화 기능 추가

--- src/main/java/bridge/domain/Bridge.java
+++ src/main/java/bridge/domain/Bridge.java
@@ -1,2 +1,18 @@
-package bridge.domain;public class Bridge {
+package bridge.domain;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Bridge {
+    private List<String> answer;
+    private List<Boolean> correct;
+
+    Bridge(List<String> answer) {
+        this.answer = answer;
+        correct = new ArrayList<>();
+    }
+
+    public void reset() {
+        correct.clear();
+    }
 }
