194ab21dafeb7605cbb448c0cfda64cca6354155
kite707 <kite707707@gmail.com>
2022-11-22 12:08:20 +0900

    feat(outputView): 다리 출력 완료

---
+++ src/main/java/bridge/GameController.java
@@ -0,0 +1,35 @@
+package bridge;
+
+import java.util.List;
+
+public class GameController {
+    private final InputView inputView = new InputView();
+    private final BridgeRandomNumberGenerator bridgeRandomNumberGenerator = new BridgeRandomNumberGenerator();
+    private final BridgeMaker bridgeMaker = new BridgeMaker(bridgeRandomNumberGenerator);
+    private List<String> bridge;
+    private final BridgeGame bridgeGame = new BridgeGame();
+
+    public void getSizeAndMakeBridge() {
+        int bridgeSize = 0;
+        try {
+            bridgeSize = inputView.readBridgeSize();
+        } catch (Error e) {
+            bridgeSize = inputView.readBridgeSize();
+        }
+        this.bridge = bridgeMaker.makeBridge(bridgeSize);
+    }
+
+    public boolean getMovingResult(int inputIdx) {
+        String inputMoving = "";
+        try {
+            inputMoving = inputView.readMoving();
+        } catch (Error e) {
+            inputMoving = inputView.readMoving();
+        }
+        return bridgeGame.move(bridge, inputMoving, inputIdx);
+    }
+
+    public List<String> getbridge() {
+        return bridge;
+    }
+}
--- src/main/java/bridge/OutputView.java
+++ src/main/java/bridge/OutputView.java
@@ -1,5 +1,7 @@
 package bridge;
 
+import java.util.List;
+
 /**
  * 사용자에게 게임 진행 상황과 결과를 출력하는 역할을 한다.
  */
@@ -10,7 +12,47 @@ public class OutputView {
      * <p>
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void printMap() {
+    public void printUpLineMap(List<String> userInput,List<Boolean> result) {
+//        [ O |   |   ]
+//        [   | O | O ]
+
+//        [ O ]
+//        [   ]
+
+//        [ O | X ]
+//        [   |   ]
+        System.out.print("[");
+        for(int i=0;i<userInput.size()-1;i++){
+            printByLine(userInput.get(i),result.get(i),"U");
+            System.out.print(" |");
+        }
+        printByLine(userInput.get(userInput.size()-1),result.get(userInput.size()-1),"U");
+        System.out.println(" ]");
+
+
+    }
+    public void printDownLineMap(List<String> userInput,List<Boolean> result) {
+        System.out.print("[");
+        for(int i=0;i<userInput.size()-1;i++){
+            printByLine(userInput.get(i),result.get(i),"D");
+            System.out.print(" |");
+        }
+        printByLine(userInput.get(userInput.size()-1),result.get(userInput.size()-1),"D");
+        System.out.print(" ]");
+
+
+    }
+
+    public void printByLine(String userInput,boolean result, String lineCode){
+        if(userInput==lineCode&&result){
+            System.out.print(" O");
+        }
+        if(userInput==lineCode&&!result){
+            System.out.print(" X");
+        }
+        if(userInput!=lineCode){
+            System.out.print("  ");
+        }
     }
 
     /**
