0422170ceaf3b521c4ac23759ad2d191149ba449
Hiluxy <sunset.lhj@gmail.com>
2022-11-22 20:58:19 +0900

    refactor: static 변경

--- src/main/java/bridge/BridgeGame.java
+++ src/main/java/bridge/BridgeGame.java
@@ -9,8 +9,8 @@ import java.util.Map;
  */
 public class BridgeGame {
 
-    public List<String> bridgeU = new ArrayList<>();
-    public List<String> bridgeD = new ArrayList<>();
+    public static List<String> bridgeU = new ArrayList<>();
+    public static List<String> bridgeD = new ArrayList<>();
 
     /**
      * 사용자가 칸을 이동할 때 사용하는 메서드
