6da7efc103639b1ab52cc24fbcb7e2aea386e86a
kathyleesh <90851865+kathyleesh@users.noreply.github.com>
2022-11-22 16:11:44 +0900

    feat(InputView) : 게임 재시작 여부를 묻고 그에 따른 결과로 도출

--- src/main/java/bridge/InputView.java
+++ src/main/java/bridge/InputView.java
@@ -8,6 +8,7 @@ public class InputView {
 
     static final String ENTER_BRIDGE_SIZE = "다리의 길이를 입력해주세요.";
     static final String ENTER_UP_OR_DOWN = "이동할 칸을 선택해주세요. (위: U, 아래: D)";
+    static final String RESTART_GAME = "게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)";
 
     /**
      * 다리의 길이를 입력받는다.
@@ -30,7 +31,8 @@ public class InputView {
     /**
      * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
      */
-    public String readGameCommand() {
-        return null;
+    public static String readGameCommand() {
+        System.out.println(RESTART_GAME);
+        return Console.readLine();
     }
 }
