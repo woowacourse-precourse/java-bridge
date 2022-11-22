aa03dbe59a83033fb4ee3818e0e94cc85dd97414
SEUNGHEE96 <dnffktmdgml@gmail.com>
2022-11-22 23:06:52 +0900

    test: 테스트 케이스 추가: 포

--- src/test/java/bridge/ApplicationTest.java
+++ src/test/java/bridge/ApplicationTest.java
@@ -39,7 +39,7 @@ class ApplicationTest extends NsTest {
         }, 1, 0, 1);
     }
     @Test
-    void 다리길이_20개() {
+    void 다리길이_20개_테스트() {
         assertRandomNumberInRangeTest(() -> {
             run("20",
                     "U", "D", "D",
@@ -63,6 +63,33 @@ class ApplicationTest extends NsTest {
                     "[ O |   | O |   |   |   | O | O | O |   | O | O |   | O |   |   | O | O |   |   ]");
             int downSideIndex = output().indexOf(
                     "[   | O |   | O | O | O |   |   |   | O |   |   | O |   | O | O |   |   | O | O ]");
+            assertThat(upSideIndex).isLessThan(downSideIndex);
+        }, 1, 0, 1, 0, 0, 0, 1, 1, 1, 0, 1, 1, 0, 1, 0, 0, 1, 1, 0, 0);
+    }
+    @Test
+    void 포기_테스트() {
+        assertRandomNumberInRangeTest(() -> {
+            run("20",
+                    "U", "D", "D",
+                    "R",
+                    "U", "D", "U", "D", "D", "D", "U", "D",
+                    "R",
+                    "U", "D", "U", "D", "D", "D", "U", "U", "U", "D", "U", "U", "U",
+                    "R",
+                    "U", "D", "U", "D", "D", "D", "U", "U", "U", "D", "U", "U", "D", "U", "D", "D", "U", "U", "U",
+                    "Q");
+            assertThat(output()).contains(
+                    "최종 게임 결과",
+                    "[ O |   | O |   |   |   | O | O | O |   | O | O |   | O |   |   | O | O | X ]",
+                    "[   | O |   | O | O | O |   |   |   | O |   |   | O |   | O | O |   |   |   ]",
+                    "게임 성공 여부: 실패",
+                    "총 시도한 횟수: 4"
+            );
+
+            int upSideIndex = output().indexOf(
+                    "[ O |   | O |   |   |   | O | O | O |   | O | O |   | O |   |   | O | O | X ]");
+            int downSideIndex = output().indexOf(
+                    "[   | O |   | O | O | O |   |   |   | O |   |   | O |   | O | O |   |   |   ]");
             assertThat(upSideIndex).isLessThan(downSideIndex);
         }, 1, 0, 1, 0, 0, 0, 1, 1, 1, 0, 1, 1, 0, 1, 0, 0, 1, 1, 0, 0);
     }
