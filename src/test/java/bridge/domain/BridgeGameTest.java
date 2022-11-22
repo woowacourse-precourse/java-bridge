09661771e46fa6da202182b7f1f77cf03bd3b70c
iamjooon2 <iamjooon2@gmail.com>
2022-11-22 17:35:36 +0900

    test: 다리를 선택했을 떄의 성공/실패를 확인

--- src/test/java/bridge/domain/BridgeGameTest.java
+++ src/test/java/bridge/domain/BridgeGameTest.java
@@ -1,12 +1,16 @@
 package bridge.domain;
 
+import bridge.type.ResultType;
 import java.util.List;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
 
 class BridgeGameTest {
 
+    @InjectMocks
+    private BridgeGame target;
     private int bridgeSize = 5;
     List<String> sampleBridge = List.of("U", "D", "U", "D", "U");
     BridgeGame bridgeGame = new BridgeGame(sampleBridge, new MoveLog(sampleBridge.size()));
@@ -16,5 +20,18 @@ class BridgeGameTest {
     void checkBridgeSize() {
         Assertions.assertThat(bridgeGame.getBridgeSize()).isEqualTo(bridgeSize);
     }
+
+    @DisplayName("오답인 다리를 선택했을 때 실패가 나오는지 확인")
+    @Test
+    void checkResultByWrongMove() {
+        Assertions.assertThat(bridgeGame.move("D")).isEqualTo(ResultType.FAIL);
+    }
+
+    @DisplayName("정답인 다리를 선택했을 때 성공이 나왔는지 확인")
+    @Test
+    void checkResultByCorrectMove() {
+        Assertions.assertThat(bridgeGame.move("U")).isEqualTo(ResultType.SUCCESS);
+    }
+
 
 }
