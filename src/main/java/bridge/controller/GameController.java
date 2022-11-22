3cb9b20de096c3ee4fdd3c6d2963c3c3d3463f56
dohyun <ldh19738@gmail.com>
2022-11-22 19:30:43 +0900

    feat(GameController): 고정된 값(다리, 다리길이) 저장하는 기능 구현

---
+++ src/main/java/bridge/controller/GameController.java
@@ -0,0 +1,39 @@
+package bridge.controller;
+
+import bridge.BridgeGame;
+import bridge.Result;
+import bridge.view.InputView;
+import bridge.view.OutputView;
+
+import java.util.List;
+
+public class GameController {
+    private static final String SUCCESS = "성공";
+    private static final String FAIL = "실패";
+    private final BridgeGame bridgeGame;
+    private final Result result;
+    private List<String> bridge;
+    private int countTry;
+    private int bridgeSize;
+    private boolean retry;
+
+    public GameController(BridgeGame bridgeGame, Result result) {
+        this.countTry = 0;
+        this.bridgeGame = bridgeGame;
+        this.result = result;
+    }
+
+    private void setBridge(BridgeController bridgeController) {
+        this.bridge = bridgeController.bridge();
+    }
+
+    private void setBridgeSize(BridgeController bridgeController) {
+        this.bridgeSize = bridgeController.getBridgeSize();
+    }
+
+    private void setEternalThing() {
+        BridgeController bridgeController = new BridgeController();
+        setBridge(bridgeController);
+        setBridgeSize(bridgeController);
+    }
+}
