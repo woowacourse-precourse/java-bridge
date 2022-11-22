d79d6f06544b00184ecd08fbed0937b244fc11a9
Young37 <younghk37@gmail.com>
2022-11-22 21:00:04 +0900

    test(domain): check not arriving and valid or not

--- src/test/java/bridge/domain/PlayerChoiceTest.java
+++ src/test/java/bridge/domain/PlayerChoiceTest.java
@@ -7,6 +7,9 @@ import java.util.List;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.EnumSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import bridge.constant.Direction;
 
@@ -24,5 +27,25 @@ class PlayerChoiceTest {
         this.playerChoice.add(Direction.UP);
         this.playerChoice.add(Direction.DOWN);
         assertThat(playerChoice.toStringWith(List.of("U","D"), Direction.UP)).isEqualTo("[ O |   ]");
+    }
+    
+    @DisplayName("도착하지 않은 상태 테스트")
+    @Test
+    void isArrived_도착하지_않은_상태_테스트() {
+        this.playerChoice.add(Direction.UP);
+        this.playerChoice.add(Direction.UP);
+        assertThat(playerChoice.isArrived(List.of("U", "D"))).isEqualTo(false);
+        assertThat(playerChoice.isArrived(List.of("U", "U", "U"))).isEqualTo(false);
+    }
+    
+    @DisplayName("isValid_테스트")
+    @ValueSource(strings = { "U", "D" })
+    @ParameterizedTest
+    void isValid_테스트(String input) {
+        this.playerChoice.add(Direction.UP);
+        this.playerChoice.add(Direction.UP);
+        assertThat(playerChoice.isValid(List.of("U", "U"))).isEqualTo(true);
+        assertThat(playerChoice.isValid(List.of("U", "U", input))).isEqualTo(true);
+        assertThat(playerChoice.isValid(List.of("D", "D"))).isEqualTo(false);
     }
 }
