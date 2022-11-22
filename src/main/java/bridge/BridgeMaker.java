89a2e75b5fecb987ac472a3c6dd3d4cbb070d4dd
Yurim Park <63230463+lina1919@users.noreply.github.com>
2022-11-22 20:58:19 +0900

    feat : make bridge by random number generation

--- src/main/java/bridge/BridgeMaker.java
+++ src/main/java/bridge/BridgeMaker.java
@@ -1,10 +1,10 @@
 package bridge;
 
+import bridge.Enum.PlayerInput;
+
+import java.util.ArrayList;
 import java.util.List;
 
-/**
- * 다리의 길이를 입력 받아서 다리를 생성해주는 역할을 한다.
- */
 public class BridgeMaker {
 
     private final BridgeNumberGenerator bridgeNumberGenerator;
@@ -13,11 +13,17 @@ public class BridgeMaker {
         this.bridgeNumberGenerator = bridgeNumberGenerator;
     }
 
-    /**
-     * @param size 다리의 길이
-     * @return 입력받은 길이에 해당하는 다리 모양. 위 칸이면 "U", 아래 칸이면 "D"로 표현해야 한다.
-     */
     public List<String> makeBridge(int size) {
-        return null;
+        List<String> bridge = new ArrayList<>();
+        for(int i = 0; i < size; i++) {
+            String upDown = convertToEnum(bridgeNumberGenerator.generate());
+            bridge.add(upDown);
+        }
+        return bridge;
+    }
+
+    private String convertToEnum(int num){
+        if(num == 0) return String.valueOf(PlayerInput.DOWN);
+        return String.valueOf(PlayerInput.UP);
     }
 }
---
+++ src/main/java/bridge/Enum/GameOutput.java
@@ -0,0 +1,2 @@
+package bridge.Enum;public enum GameOutput {
+}
