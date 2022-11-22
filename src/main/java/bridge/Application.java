594156fac08baaf2db8d6a8a3693d85557b1e1ac
Mr.Kim <86274253+Joajy@users.noreply.github.com>
2022-11-22 21:11:55 +0900

    refactor(Application): implement code convention

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -2,7 +2,7 @@ package bridge;
 
 public class Application {
     public static void main(String[] args) {
-        System.out.println(Constant.Start_Game);
+        System.out.println(Constant.START_GAME);
         try {
             GameController.setGame();
         } catch (Exception e) {
