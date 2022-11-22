90dd4b5a23aed14515cd770238fa5b227e879627
nahyeon <whskgus12@nate.com>
2022-11-22 15:05:02 +0900

    feat(view): 사용자가 시도한 횟수와 성공 여부 출력
    사용자가 게임을 재시도한 횟수와 성공 여부를 출력합니다.

--- src/main/java/bridge/view/OutputView.java
+++ src/main/java/bridge/view/OutputView.java
@@ -37,6 +37,13 @@ public class OutputView {
      * <p>
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void printResult() {
+    public void printResult(boolean isSuccess, int reGameCount) {
+        if (isSuccess) {
+            System.out.println("게임 성공 여부: 성공");
+        }
+        if (!isSuccess) {
+            System.out.println("게임 성공 여부: 실패");
+        }
+        System.out.println("총 시도한 횟수: " + reGameCount);
     }
 }
