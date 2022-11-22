0187db6021fb67975f5a5c581211ceac45e6e954
wonseokjee <wsji9404@gmail.com>
2022-11-22 17:47:49 +0900

    Feat(BridgeGame): 추가 isStart, isUpperCollectOrWrong, isLowerCollectOrWrong, isEnd, isSuccess

--- src/main/java/bridge/BridgeGame.java
+++ src/main/java/bridge/BridgeGame.java
@@ -1,5 +1,7 @@
 package bridge;
 
+import java.util.List;
+
 /**
  * 다리 건너기 게임을 관리하는 클래스
  */
@@ -31,8 +33,68 @@ public class BridgeGame {
      * <p>
      * 이동을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void move() {
+    public String move(List<String> makeBridge, int acrossBridge, String moving) {
+        isStart(acrossBridge);
+        isUpperCollectOrWrong(makeBridge, acrossBridge, moving);
+        isLowerCollectOrWrong(makeBridge, acrossBridge, moving);
+        isEnd(makeBridge, acrossBridge);
+
+        String upperResult = upperBridge.substring(0, upperBridge.length() - 1) + BRIDGE_END;
+        String lowerResult = lowerBridge.substring(0, lowerBridge.length() - 1) + BRIDGE_END;
+        return upperResult + System.lineSeparator() + lowerResult;
     }
+
+    public void isStart(int acrossBridge) {
+        if (acrossBridge == 0) {
+            upperBridge.append(BRIDGE_START);
+            lowerBridge.append(BRIDGE_START);
+        }
+    }
+
+    public void isUpperCollectOrWrong(List<String> makeBridge, int acrossBridge, String moving) {
+        if (moving.equals("U")) {
+            if (makeBridge.get(acrossBridge).equals(moving)) {
+                upperBridge.append(MOVE_POSSIBLE);
+            }
+            if (!(makeBridge.get(acrossBridge).equals(moving))) {
+                upperBridge.append(MOVE_IMPOSSIBLE);
+            }
+            lowerBridge.append(NOT_SELECTED);
+        }
+    }
+
+    public void isLowerCollectOrWrong(List<String> makeBridge, int acrossBridge, String moving) {
+        if (moving.equals("D")) {
+            if (makeBridge.get(acrossBridge).equals(moving)) {
+                lowerBridge.append(MOVE_POSSIBLE);
+            }
+            if (!(makeBridge.get(acrossBridge).equals(moving))) {
+                lowerBridge.append(MOVE_IMPOSSIBLE);
+            }
+            upperBridge.append(NOT_SELECTED);
+        }
+    }
+
+    public void isEnd(List<String> makeBridge, int acrossBridge) {
+        if (makeBridge.size() - 1 == acrossBridge) {
+            upperBridge.append(BRIDGE_END);
+            lowerBridge.append(BRIDGE_END);
+        }
+        if (makeBridge.size() - 1 != acrossBridge) {
+            upperBridge.append(BRIDGE_MIDDLE);
+            lowerBridge.append(BRIDGE_MIDDLE);
+        }
+    }
+
+    public void isSuccess(List<String> bridge, int acrossBridge) {
+        if (acrossBridge == bridge.size()) {
+            System.out.println(printOutput.IS_SUCCESS_SUCCESS.getGameOutput());
+        }
+        if (acrossBridge != bridge.size()) {
+            System.out.println(printOutput.IS_SUCCESS_FAIL.getGameOutput());
+        }
+    }
+
 
     /**
      * 사용자가 게임을 다시 시도할 때 사용하는 메서드
