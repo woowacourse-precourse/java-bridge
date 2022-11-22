7bbeb517d6a1f90633182ed3a791b276803b8e0a
sosow0212 <sosow0212@naver.com>
2022-11-22 22:10:42 +0900

    refactor : (BridgeController) 컨트롤러에서 BridgeService의 기능을 BridgeMaker에서 대체하게 리팩토링 진행

--- src/main/java/bridge/controller/BridgeController.java
+++ src/main/java/bridge/controller/BridgeController.java
@@ -1,21 +1,29 @@
 package bridge.controller;
 
+import bridge.BridgeMaker;
+import bridge.BridgeRandomNumberGenerator;
 import bridge.domain.User;
 import bridge.domain.BridgeGame;
-import bridge.service.BridgeService;
 import bridge.service.ViewService;
+import java.util.List;
 
 public class BridgeController {
     private static final String RESTART_GAME = "R";
     private static final String QUIT_GAME = "Q";
-    private final BridgeService bridgeService = new BridgeService();
+    private final BridgeMaker bridgeMaker = new BridgeMaker(new BridgeRandomNumberGenerator());
     private final ViewService viewService = new ViewService();
 
     public void start() {
         gameStartMessage();
-        BridgeGame bridgeGame = bridgeService.bridgeGameMaker();
+        BridgeGame bridgeGame = new BridgeGame(bridgeMaker());
         User user = new User();
         playGame(user, bridgeGame);
+    }
+
+    public List<String> bridgeMaker() {
+        int bridgeSize = viewService.requestBridgeSize();
+        List<String> bridgeGame = bridgeMaker.makeBridge(bridgeSize);
+        return bridgeGame;
     }
 
     public void gameStartMessage() {
