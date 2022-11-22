1ac421711d22ff5ffde89f06ea0a71e207ddf8c2
Dennis-K1 <dennis.k1.dev@gmail.com>
2022-11-22 15:26:58 +0900

    refactor: Shorten method length

--- src/main/java/bridge/OutputView.java
+++ src/main/java/bridge/OutputView.java
@@ -1,6 +1,5 @@
 package bridge;
 
-import java.util.Arrays;
 import java.util.List;
 
 /**
@@ -14,46 +13,26 @@ public class OutputView {
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
     public void printMap(List<String> bridge, int stage, int status) {
-        // stage길이 만큼 bridge 출력 2개의 배열 생성
         BridgeGame bridgeGame = new BridgeGame();
-
-        List<String> upperBridge = bridgeGame.createUpperBridge(bridge);
-        List<String> bottomBridge = bridgeGame.createBottomBridge(bridge);
         int stageIndex = stage * 2;
-        upperBridge.set(stageIndex, "]");
-        bottomBridge.set(stageIndex, "]");
-
+        List<List<String>> bothBridges = bridgeGame.createBridges(bridge, stageIndex);
         if (status == 1) {
-            if (upperBridge.get(stageIndex - 1).equals(" O ")) {
-                upperBridge.set((stageIndex-1), "   ");
-                bottomBridge.set((stageIndex-1), " X ");
-            }
-            if (bottomBridge.get(stageIndex - 1).equals(" O ")) {
-                bottomBridge.set((stageIndex-1), "   ");
-                upperBridge.set((stageIndex-1), " X ");
-            }
-            for (int i = 0; i <= stageIndex; i++) {
-                System.out.print(upperBridge.get(i));
-            }
-            System.out.println();
-            for (int i = 0; i <= stageIndex; i++) {
-                System.out.print(bottomBridge.get(i));
-            }
+            bridgeGame.moveSuccess(bothBridges, stageIndex);
+            printBridge(bothBridges, stageIndex);
         }
-
-
         if (status == 0) {
-            for (int i = 0; i <= stageIndex; i++) {
-                System.out.print(upperBridge.get(i));
-            }
-            System.out.println();
-            for (int i = 0; i <= stageIndex; i++) {
-                System.out.print(bottomBridge.get(i));
-            }
+            printBridge(bothBridges, stageIndex);
         }
+    }
 
-
-
+    public void printBridge(List<List<String>> bothBridges, int stageIndex) {
+        for (int i = 0; i <= stageIndex; i++) {
+            System.out.print(bothBridges.get(0).get(i));
+        }
+        System.out.println();
+        for (int i = 0; i <= stageIndex; i++) {
+            System.out.print(bothBridges.get(1).get(i));
+        }
     }
 
     /**
@@ -62,6 +41,8 @@ public class OutputView {
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
     public void printResult() {
+        System.out.println();
+        System.out.println();
         System.out.println("최종 게임 결과");
     }
 
@@ -71,32 +52,31 @@ public class OutputView {
     }
 
     public void printBridgeSizeInput() {
+        System.out.println();
         System.out.println("다리의 길이를 입력해주세요.");
     }
     public void printMovingInput() {
+        System.out.println();
         System.out.println("이동할 칸을 선택해주세요. (위: U, 아래: D)");
     }
     public void printGameOver() {
+        System.out.println();
         System.out.println("게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)");
     }
     public void printSuccess(int success) {
-        String result = "";
         if (success == 1){
+            System.out.printf("%n%n");
             System.out.println("게임 성공 여부: 성공");
-        } else {
+        }
+    }
+    public void printFailure(int success) {
+        if (success == 0){
+            System.out.printf("%n%n");
             System.out.println("게임 성공 여부: 실패");
         }
     }
     public void printTrialAmount(int trial) {
         System.out.println("총 시도한 횟수: " + trial);
-    }
-
-    public void printNotMovable() {
-
-    }
-
-    public void printMovable (){
-
     }
 
 
