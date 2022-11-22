9296bebfaae90b7f394f7d2a66bc02a7cd6ce96a
bando99 <bando991225@gmail.com>
2022-11-22 23:31:13 +0900

    FEAT : 주석 삭제

--- src/main/java/bridge/BridgeRandomNumberGenerator.java
+++ src/main/java/bridge/BridgeRandomNumberGenerator.java
@@ -8,8 +8,6 @@ public class BridgeRandomNumberGenerator implements BridgeNumberGenerator {
     private static final int RANDOM_UPPER_INCLUSIVE = 1;
 
 
-    // 0과 1 중 랜덤으로 하나를 출력하는 것
-    // 정답 다리를 정하는 거 같음.
     @Override
     public int generate() {
         return Randoms.pickNumberInRange(RANDOM_LOWER_INCLUSIVE, RANDOM_UPPER_INCLUSIVE);
