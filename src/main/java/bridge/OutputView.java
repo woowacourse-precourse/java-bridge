cfbb2f87ce311b7319ce2c808df7cec5f7cfd8e5
jay <jaewhan232@naver.com>
2022-11-22 22:44:30 +0900

    Implement : 전달받은 메세지를 출력하는 printMessage() 메소드, 게임 시작 메시지 출력하는 printStartMessage() 메소드 구현

--- src/main/java/bridge/OutputView.java
+++ src/main/java/bridge/OutputView.java
@@ -19,6 +19,14 @@ public class OutputView {
         return outputView;
     }
 
+    public void printMessage(String message) {
+        System.out.println(message);
+    }
+
+    public void printStartMessage() {
+        printMessage(startMessage);
+    }
+
     /**
      * 현재까지 이동한 다리의 상태를 정해진 형식에 맞춰 출력한다.
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
