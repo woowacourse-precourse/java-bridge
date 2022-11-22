99ae4c19f7ee65713c772fb4edfb4bfc0629a8be
wjh2012 <wjh2012@naver.com>
2022-11-22 18:00:00 +0900

    refactor(Bridge): 다리의 끝점을 확인하는 기능

--- src/main/java/bridge/model/Bridge.java
+++ src/main/java/bridge/model/Bridge.java
@@ -3,14 +3,18 @@ package bridge.model;
 import java.util.List;
 
 public class Bridge {
+
     private final List<String> bridge;
 
     public Bridge(List<String> bridge) {
         this.bridge = bridge;
     }
 
-    public boolean checkStep( int index, String step) {
-        return bridge.get(index)
-                .equals(step);
+    public boolean checkStep(int index, String step) {
+        return bridge.get(index).equals(step);
+    }
+
+    public boolean isEnd(int index) {
+        return bridge.size() == index + 1;
     }
 }
