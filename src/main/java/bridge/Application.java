4d1268909acfea5e87c9a3a3798f81fde4cf77d3
Sanha Ko <55884834+headF1rst@users.noreply.github.com>
2022-11-22 22:35:28 +0900

    feat: 프로그램 시작 로직 추가

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -3,6 +3,11 @@ package bridge;
 public class Application {
 
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        try {
+            BridgeGame bridgeGame = new BridgeGame();
+            bridgeGame.run();
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+        }
     }
 }
