0703da35ecf3292407360e1b46e03228773311da
kite707 <kite707707@gmail.com>
2022-11-22 16:19:58 +0900

    feat(GameController): 잘못된 값 입력 시 오류메시지 출력 및 재입력

--- src/main/java/bridge/GameController.java
+++ src/main/java/bridge/GameController.java
@@ -11,34 +11,56 @@ public class GameController {
     private final OutputView outputView = new OutputView();
 
     public void getSizeAndMakeBridge() {
-        System.out.println("다리 건너기 게임을 시작합니다.");
+        System.out.println("다리 건너기 게임을 시작합니다.\n");
         int bridgeSize = 0;
-        try {
-            bridgeSize = inputView.readBridgeSize();
-        } catch (Error e) {
-            bridgeSize = inputView.readBridgeSize();
+        while (true) {
+            try {
+                bridgeSize = inputView.readBridgeSize();
+                break;
+            } catch (Exception e) {
+                System.out.println(e.getMessage());
+            }
         }
         this.bridge = bridgeMaker.makeBridge(bridgeSize);
     }
 
+
     public boolean getMovingResult(int inputIdx) {
         String inputMoving = "";
-        try {
-            inputMoving = inputView.readMoving();
-        } catch (Error e) {
-            inputMoving = inputView.readMoving();
+        while (true) {
+            try {
+                inputMoving = inputView.readMoving();
+                break;
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            }
         }
         return bridgeGame.move(bridge, inputMoving, inputIdx);
     }
 
-    public void printLongMap(List<String> userInput, List<Boolean> result) {
-        outputView.printLongMap(userInput, result);
+    public void printLongMap() {
+        System.out.println(bridgeGame.userInputList());
+        System.out.println(bridgeGame.getResult());
+        outputView.printLongMap(bridgeGame.userInputList(), bridgeGame.getResult());
     }
 
-    public void printShortMap(List<String> userInput, List<Boolean> result){
-        outputView.printShortMap(userInput,result);
+    public void printShortMap() {
+        outputView.printShortMap(bridgeGame.userInputList(), bridgeGame.getResult());
     }
+
+    public void printMap() {
+        if (bridgeGame.getUserInputListLength() == 1) {
+            printShortMap();
+            return;
+        }
+        printLongMap();
+    }
+
     public List<String> getbridge() {
         return bridge;
+    }
+
+    public int getbridgeLength() {
+        return bridge.size();
     }
 }
