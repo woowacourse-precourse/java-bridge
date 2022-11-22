636ee67c23487c58a1d286bf3d456ff0c805c8ff
jhsseonn <hosu0125@gmail.com>
2022-11-22 21:47:23 +0900

    style: 오타 수정

--- src/main/java/bridge/view/OutputView.java
+++ src/main/java/bridge/view/OutputView.java
@@ -15,7 +15,7 @@ public class OutputView extends OutputConfig {
      * <p>
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void printMap(HashMap<Integer, List<String>> r현esult) {
+    public void printMap(HashMap<Integer, List<String>> result) {
         System.out.print("[ ");
         printUpper(result);
         System.out.println(" ]");
