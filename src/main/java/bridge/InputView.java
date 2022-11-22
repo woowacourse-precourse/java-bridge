a933c917354c9cbab6976e18fbefb4cf91c3187f
naaaaag <115922748+naaaaag@users.noreply.github.com>
2022-11-22 19:51:31 +0900

    <feat>InputView : 새로운 예외를 처리

--- src/main/java/bridge/InputView.java
+++ src/main/java/bridge/InputView.java
@@ -12,16 +12,18 @@ public class InputView {
     private static String readBridgeSizeMessage = "다리의 길이를 입력해주세요.";
     private static String readMovingMessage = "이동할 칸을 선택해주세요. (위: U, 아래: D)";
     private static String readGameCommandMessage = "게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)";
-    private static String bridgeSizeErrorMessage = "[ERROR] 다리 길이는 3부터 20사이의 숫자여야 합니다.";
+    private static String bridgeSizeNonIntegerErrorMessage = "[ERROR] 문자를 입력하셨습니다.";
+    private static String bridgeSizeWithinRangeErrorMessage = "[ERROR] 다리 길이는 3부터 20사이의 숫자여야 합니다.";
     private static String movingErrorMessage = "[ERROR] 이동 할 칸은 U 혹은 D여야 합니다.";
     private static String gameCommandErrorMessage = "[ERROR] 재시작/종료 여부는 R 혹은 Q여야 합니다.";
 
     public Integer readBridgeSize() {
         try {
             System.out.println(readBridgeSizeMessage);
-            int size = Integer.parseInt(readLine());
-            validateBridgeSize(size);
-            return size;
+            String size = readLine();
+            validateBridgeSizeNonInteger(size);
+            validateBridgeSizeWithinRange(size);
+            return Integer.parseInt(size);
         } catch (ArithmeticException arithmeticException) {
             readBridgeSize();
         }
@@ -52,10 +54,17 @@ public class InputView {
         return null;
     }
 
-    private void validateBridgeSize(int size) {
-        if (size < minimumSize || size > maximumSize) {
-            System.out.println(bridgeSizeErrorMessage);
-            throw new ArithmeticException(bridgeSizeErrorMessage);
+    private void validateBridgeSizeNonInteger(String size) {
+        if (!size.chars().allMatch((Character::isDigit))) {
+            System.out.println(bridgeSizeNonIntegerErrorMessage);
+            throw new ArithmeticException(bridgeSizeNonIntegerErrorMessage);
+        }
+    }
+
+    private void validateBridgeSizeWithinRange(String size) {
+        if (Integer.parseInt(size) < minimumSize || Integer.parseInt(size) > maximumSize) {
+            System.out.println(bridgeSizeWithinRangeErrorMessage);
+            throw new ArithmeticException(bridgeSizeWithinRangeErrorMessage);
         }
     }
 
