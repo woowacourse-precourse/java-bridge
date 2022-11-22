19db582abb17874dfe63445ee3df8f6e03b06dbd
cgk95 <kjkj5868@gmail.com>
2022-11-22 22:57:38 +0900

    refactor(Test) : 컨벤션에 따라 코드 수정

--- src/test/java/bridge/Model/BridgeGameTest.java
+++ src/test/java/bridge/Model/BridgeGameTest.java
@@ -5,7 +5,6 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
-
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -18,6 +17,7 @@ class BridgeGameTest {
     void init() {
         bridgeGame = BridgeGame.getBridgeGame();
     }
+
     @DisplayName("move메소드를 통해서 user의 정보가 잘 변화하는지 확인")
     @ParameterizedTest
     @CsvSource(value = {"U"})
@@ -26,5 +26,4 @@ class BridgeGameTest {
         assertEquals(bridgeGame.user, List.of("U"));
 
     }
-
 }
