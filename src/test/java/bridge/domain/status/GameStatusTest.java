fd6c3137a228fdd06bac87f3fa93f82c749e3191
Song kyeong seok <skj0922@kookmin.ac.kr>
2022-11-22 21:49:36 +0900

    test(GameStatusTest): 게임 상태를 변환 해주는 테스트 예외 발생 메서드 구현

--- src/test/java/bridge/domain/status/GameStatusTest.java
+++ src/test/java/bridge/domain/status/GameStatusTest.java
@@ -1,5 +1,6 @@
 package bridge.domain.status;
 
+import bridge.exception.ExceptionMessage;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -29,6 +30,16 @@ public class GameStatusTest {
         String input = "Q";
 
         Assertions.assertThat(GameStatus.convertGameStatus(input)).isEqualTo(expectedStatus);
+    }
+
+    @Test
+    @DisplayName("게임 상태를 변환 해주는 테스트 (예외 발생)")
+    public void convertGameStatusExceptionTest() {
+        String input = "A";
+
+        Assertions.assertThatThrownBy(() -> GameStatus.convertGameStatus(input))
+            .isInstanceOf(IllegalArgumentException.class)
+            .hasMessageContaining(String.valueOf(ExceptionMessage.COMMEND_NOT_FOUND_ERROR));
     }
 
 }
