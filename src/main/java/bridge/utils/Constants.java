dca93abff48355111bf39227c008f19c872cc315
Joonsoo Kim <kjsoo1901@gmail.com>
2022-11-22 13:38:42 +0900

    feat: OutputView 구현

--- src/main/java/bridge/utils/Constants.java
+++ src/main/java/bridge/utils/Constants.java
@@ -10,6 +10,17 @@ public class Constants {
     public static final int MIN_BRIDGE_SIZE = 3;
     public static final int MAX_BRIDGE_SIZE = 20;
 
+    public static final boolean IS_UP_BLOCK = true;
+    public static final boolean IS_DOWN_BLOCK = true;
+    public static final boolean IS_FAILURE = true;
+    public static final boolean IS_NOT_FAILURE = false;
+    public static final String BLOCK_SUCCESS = " O ";
+    public static final String BLOCK_FAILURE = " O ";
+    public static final String BLOCK_BLANK = " O ";
+    public static final String BLOCK_SPLIT = "|";
+    public static final String LINE_START = "[";
+    public static final String LINE_END = "]";
+
     public static final String ERROR_BRIDGE_SIZE_NOT_INTEGER = "[ERROR] 다리 길이는 숫자여야 합니다";
     public static final String ERROR_BRIDGE_SIZE_NOT_IN_RANGE = "[ERROR] 다리 길이는 3부터 20 사이의 숫자여야 합니다.";
     public static final String ERROR_BRIDGE_SIZE_IS_ZERO = "[ERROR] 다리 길이는 0이 아닌 숫자여야 합니다.";
--- src/main/java/bridge/view/OutputView.java
+++ src/main/java/bridge/view/OutputView.java
@@ -1,5 +1,9 @@
 package bridge.view;
 
+import bridge.domain.Bridge;
+
+import static bridge.utils.Constants.*;
+
 /**
  * 사용자에게 게임 진행 상황과 결과를 출력하는 역할을 한다.
  */
@@ -10,14 +14,60 @@ public class OutputView {
      * <p>
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void printMap() {
+    public void printMap(Bridge bridge, int blockNumber) {
+        printLine(bridge, blockNumber, IS_UP_BLOCK);
+        printLine(bridge, blockNumber, IS_DOWN_BLOCK);
     }
 
-    /**
-     * 게임의 최종 결과를 정해진 형식에 맞춰 출력한다.
-     * <p>
-     * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
-     */
-    public void printResult() {
+    private void printLine(Bridge bridge, int blockNumber, boolean isUpBlock){
+        System.out.print(LINE_START);
+        for(int i = 0; i < blockNumber - 1; i++){
+            printBlock(bridge, i, isUpBlock);
+            System.out.print(BLOCK_SPLIT);
+        }
+        printBlock(bridge, blockNumber - 1, isUpBlock);
+        System.out.println(LINE_END);
+    }
+
+    private void printBlock(Bridge bridge, int blockNumber, boolean isUpBlock){
+        if(isUpBlock && bridge.isUpBlock(blockNumber) == true){
+            System.out.print(BLOCK_SUCCESS);
+            return;
+        }
+        if(!isUpBlock && bridge.isUpBlock(blockNumber) == false){
+            System.out.print(BLOCK_SUCCESS);
+            return;
+        }
+        System.out.print(BLOCK_BLANK);
+    }
+
+    private void printFailureLine(Bridge bridge, int blockNumber, boolean isUpBlock, boolean isFailure){
+        System.out.print(LINE_START);
+        for(int i = 0; i < blockNumber - 1; i++){
+            printBlock(bridge, i, isUpBlock);
+            System.out.print(BLOCK_SPLIT);
+        }
+        if(isFailure){
+            System.out.print(BLOCK_SUCCESS);
+            System.out.println(LINE_END);
+        }
+        if(!isFailure){
+            System.out.print(BLOCK_BLANK);
+            System.out.println(LINE_END);
+        }
+    }
+
+    public void printResult(Bridge bridge, int blockNumber, boolean isSuccess, boolean isUpBlock) {
+        if(isSuccess){
+            printMap(bridge, blockNumber);
+        }
+        if(!isSuccess && isUpBlock){
+            printFailureLine(bridge, blockNumber, IS_UP_BLOCK, IS_FAILURE);
+            printFailureLine(bridge, blockNumber, IS_DOWN_BLOCK, IS_NOT_FAILURE);
+        }
+        if(!isSuccess && !isUpBlock){
+            printFailureLine(bridge, blockNumber, IS_UP_BLOCK, IS_NOT_FAILURE);
+            printFailureLine(bridge, blockNumber, IS_DOWN_BLOCK, IS_FAILURE);
+        }
     }
 }
