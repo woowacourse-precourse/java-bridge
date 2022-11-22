e7f80ba488cea531c9919fad1522e0c31110a775
mgOh <issac_oh@naver.com>
2022-11-22 13:52:54 +0900

    feat: 게임 최종 결과를 출력하는 기능

--- src/main/java/bridge/OutputView.java
+++ src/main/java/bridge/OutputView.java
@@ -16,12 +16,26 @@ public class OutputView {
     public void printGetMoving(){
         System.out.println("\n이동할 칸을 선택해주세요. (위: U, 아래: D)");
     }
+    /** 이동할 칸을 선택하라는 안내 문구를 출력하는 기능 */
+    public void printGetGameCommand(){
+        System.out.println("\n게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)");
+    }
     /** 사용자가 이동할 때마다 다리 건너기 결과를 출력하는 기능 */
     public void printMap(String upBridgeMap, String DownBridgeMap) {
         System.out.println("[" + upBridgeMap + "]");
         System.out.println("[" + DownBridgeMap + "]");
     }
-
-    public void printResult() {
+    /** 최종 게임 결과를 출력하는 기능 */
+    public void printResult(boolean result, String upBridgeMap, String DownBridgeMap, int gameTryCount) {
+        System.out.println("최종 게임 결과");
+        System.out.println("[" + upBridgeMap + "]");
+        System.out.println("[" + DownBridgeMap + "]\n");
+        printSuccessOrFailure(result);
+        System.out.println("총 시도한 횟수: " + gameTryCount);
+    }
+    /** 사용자의 게임 성공 or 실패 여부에 따라 다른 메시지를 출력하는 기능 */
+    private boolean printSuccessOrFailure(boolean result){
+        if (result){System.out.println("게임 성공 여부: 성공");return true;}
+        System.out.println("게임 성공 여부: 실패");return true;
     }
 }
