db214baa5ac09428973cc14019233f84c70ed79c
im-shung <seoyoung1113@gmail.com>
2022-11-22 13:05:45 +0900

    feat: :sparkles: 게임의 최종 결과를 정해진 형식에 맞춰 출력하는 기능 추가

--- src/main/java/bridge/view/OutputView.java
+++ src/main/java/bridge/view/OutputView.java
@@ -1,5 +1,10 @@
 package bridge.view;
 
+import bridge.domain.Moving;
+import bridge.domain.Round;
+import java.util.List;
+import java.util.Objects;
+
 /**
  * 사용자에게 게임 진행 상황과 결과를 출력하는 역할을 한다.
  */
@@ -46,6 +51,11 @@ public class OutputView {
      * <p>
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void printResult() {
+    public void printResult(List<Round> rounds, int totalNumberOfAttempts) {
+        System.out.println("최종 게임 결과");
+        printMap(rounds);
+        System.out.println("게임 성공 여부: " + rounds.get(rounds.size() - 1).getResult().getDescription());
+        System.out.println("총 시도한 횟수: " + totalNumberOfAttempts);
+
     }
 }
