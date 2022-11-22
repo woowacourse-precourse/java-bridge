70901673abccf19ef759cbc29ce2d3d6af494a42
Kim woo jong <skekdnwhd@gmail.com>
2022-11-22 22:51:56 +0900

    Test : 테스트 케이스 삭제

--- src/test/java/bridge/ApplicationTest.java
+++ src/test/java/bridge/ApplicationTest.java
@@ -48,26 +48,6 @@ class ApplicationTest extends NsTest {
         });
     }
 
-    @Test
-    void 기능_실패_테스트() {
-        assertRandomNumberInRangeTest(() -> {
-            run("3", "D", "R", "U", "D", "U");
-            assertThat(output()).contains(
-                    "[   ]",
-                    "[ X ]",
-                    "최종 게임 결과",
-                    "[ O |   | O ]",
-                    "[   | O |   ]",
-                    "게임 성공 여부: 성공",
-                    "총 시도한 횟수: 2"
-            );
-
-            int upSideIndex = output().indexOf("[ O |   | O ]");
-            int downSideIndex = output().indexOf("[   | O |   ]");
-            assertThat(upSideIndex).isLessThan(downSideIndex);
-        }, 1, 0, 1);
-    }
-
     @Override
     protected void runMain() {
         Application.main(new String[]{});
