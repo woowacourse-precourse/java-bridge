6c7e897ec993619c486318209891f645fde36e06
minaver <psmjoshua@gmail.com>
2022-11-22 14:57:18 +0900

    refactor: validation 메서드 Validation 클래스로 이동

--- src/main/java/bridge/Validation.java
+++ src/main/java/bridge/Validation.java
@@ -1,5 +1,9 @@
 package bridge;
 
+import bridge.constant.Constant;
+
+import java.util.Arrays;
+import java.util.List;
 import java.util.Objects;
 
 public class Validation {
@@ -7,6 +11,49 @@ public class Validation {
         if (Objects.isNull(object)) {
             System.out.println("[ERROR] Null 객체는 유효하지 않습니다.");
             throw new NullPointerException();
+        }
+    }
+
+    /**
+     * @param input 사용자에게 입력받는 다리의 길이
+     */
+    public static void validateSizeType(String input) {
+        List<String> inputSplit = Arrays.asList(input.split(""));
+        boolean typeValid = inputSplit.stream().anyMatch(i -> Character.isDigit(i.charAt(0)));
+        if (typeValid == false) {
+            System.out.println("[ERROR] 입력한 size의 타입이 유효하지 않습니다.");
+            throw new IllegalArgumentException();
+        }
+    }
+
+    /**
+     * @param input 사용자에게 입력받는 다리의 길이
+     */
+    public static void validateSizeRange(String input) {
+        int inputNumber = Integer.parseInt(input);
+        if (inputNumber < Constant.minBridgeLength || inputNumber > Constant.maxBridgeLength) {
+            System.out.println("[ERROR] 입력한 size의 범위가 유효하지 않습니다.");
+            throw new IllegalArgumentException();
+        }
+    }
+
+    /**
+     * @param input 사용자에게 입력받는 이동할 칸
+     */
+    public static void validateMoveValid(String input) {
+        if (!(input.equals("U") || input.equals("D"))) {
+            System.out.println("[ERROR] 입력한 칸이 유효하지 않습니다.");
+            throw new IllegalArgumentException();
+        }
+    }
+
+    /**
+     * @param input 사용자에게 입력받는 재시도 여부
+     */
+    public static void validateRetryValid(String input) {
+        if (!(input.equals("R") || input.equals("Q"))) {
+            System.out.println("[ERROR] 유효하지 않은 명령어 입니다.");
+            throw new IllegalArgumentException();
         }
     }
 }
--- src/main/java/bridge/view/InputView.java
+++ src/main/java/bridge/view/InputView.java
@@ -1,11 +1,7 @@
 package bridge.view;
 
-import bridge.constant.Constant;
+import bridge.Validation;
 import camp.nextstep.edu.missionutils.Console;
-
-import java.lang.reflect.Array;
-import java.util.Arrays;
-import java.util.List;
 
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
@@ -18,8 +14,8 @@ public class InputView {
     public int readBridgeSize() throws IllegalArgumentException{
         System.out.println("다리의 길이를 입력해주세요.");
         String input = Console.readLine();
-        validateSizeType(input);
-        validateSizeRange(input);
+        Validation.validateSizeType(input);
+        Validation.validateSizeRange(input);
         return Integer.parseInt(input);
     }
 
@@ -29,7 +25,7 @@ public class InputView {
     public String readMoving() {
         System.out.println("이동할 칸을 선택해주세요. (위: U, 아래: D)");
         String input = Console.readLine();
-        validateMoveValid(input);
+        Validation.validateMoveValid(input);
         return input;
     }
 
@@ -39,51 +35,8 @@ public class InputView {
     public String readGameCommand() {
         System.out.println("게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)");
         String input = Console.readLine();
-        validateRetryValid(input);
+        Validation.validateRetryValid(input);
         return input;
-    }
-
-    /**
-     * @param input 사용자에게 입력받는 다리의 길이
-     */
-    private void validateSizeType(String input) {
-        List<String> inputSplit = Arrays.asList(input.split(""));
-        boolean typeValid = inputSplit.stream().anyMatch(i -> Character.isDigit(i.charAt(0)));
-        if (typeValid == false) {
-            System.out.println("[ERROR] 입력한 size의 타입이 유효하지 않습니다.");
-            throw new IllegalArgumentException();
-        }
-    }
-
-    /**
-     * @param input 사용자에게 입력받는 다리의 길이
-     */
-    private void validateSizeRange(String input) {
-        int inputNumber = Integer.parseInt(input);
-        if (inputNumber < Constant.minBridgeLength || inputNumber > Constant.maxBridgeLength) {
-            System.out.println("[ERROR] 입력한 size의 범위가 유효하지 않습니다.");
-            throw new IllegalArgumentException();
-        }
-    }
-
-    /**
-     * @param input 사용자에게 입력받는 이동할 칸
-     */
-    private void validateMoveValid(String input) {
-        if (!(input.equals("U") || input.equals("D"))) {
-            System.out.println("[ERROR] 입력한 칸이 유효하지 않습니다.");
-            throw new IllegalArgumentException();
-        }
-    }
-
-    /**
-     * @param input 사용자에게 입력받는 재시도 여부
-     */
-    private void validateRetryValid(String input) {
-        if (!(input.equals("R") || input.equals("Q"))) {
-            System.out.println("[ERROR] 유효하지 않은 명령어 입니다.");
-            throw new IllegalArgumentException();
-        }
     }
 
 }
