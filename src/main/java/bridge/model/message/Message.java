20eb87a0901e069525c1b072b02715bf98cf63cc
chj <bluedog129@gmail.com>
2022-11-22 23:00:39 +0900

    feat(Message) : 메시지 이넘 추가

--- src/main/java/bridge/model/message/Message.java
+++ src/main/java/bridge/model/message/Message.java
@@ -8,7 +8,18 @@ public enum Message {
     INPUT_MOVING("\n이동할 칸을 선택해주세요. (위: U, 아래: D)"),
     INPUT_RETRY_COMMAND("게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)"),
     RE_START("R"),
-    QUIT("Q");
+    QUIT("Q"),
+    GAME_RESULT_MESSAGE("\n최종 게임 결과"),
+    SUCCESS_OR_FAIL("\n게임 성공 여부: "),
+    TOTAL_TRY_COUNT_IS("총 시도한 횟수: "),
+    START_MAP("["),
+    NEXT_LINE("\n"),
+    SUCCESS("성공"),
+    FAIL("실패"),
+    CORRECT_LOG(" O |"),
+    FAIL_LOG(" X |"),
+    EMPTY_LOG("   |"),
+    END_MAP("]");
 
 
 
