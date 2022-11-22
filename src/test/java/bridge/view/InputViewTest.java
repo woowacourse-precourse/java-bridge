18a3ad48c83047c573b0bcd40a189b9397a17ae6
ddukddaki <rlfrkdms1@naver.com>
2022-11-22 22:50:37 +0900

    test(InputViewTest): 사용자가 이동할 칸을 입력할 때 D,U외의 입력을 할 경우 예외문구를 출력하고 다시 입력을 받을 수 있다

--- src/test/java/bridge/view/InputViewTest.java
+++ src/test/java/bridge/view/InputViewTest.java
@@ -46,6 +46,24 @@ class InputViewTest extends NsTest {
         }, 1, 0, 1);
     }
 
+    @Test
+    @DisplayName("사용자가 이동할 칸을 입력할 때 D,U외의 입력을 할 경우 예외문구를 출력하고 다시 입력을 받을 수 있다.")
+    void 사용자가_이동할_칸을_잘못_입력한_테스트() {
+        assertRandomNumberInRangeTest(() -> {
+            run("3", "U", "R", "D", "U");
+            assertThat(output()).contains(
+                    ERROR_MESSAGE,
+                    "최종 게임 결과",
+                    "[ O |   | O ]",
+                    "[   | O |   ]",
+                    "게임 성공 여부: 성공",
+                    "총 시도한 횟수: 1"
+            );
+
+        }, 1, 0, 1);
+    }
+
+
 
 
 
