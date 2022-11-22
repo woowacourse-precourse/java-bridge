611a1d88f5165b4414896a4088901aaab7135c28
splitube <voicesplit@daum.net>
2022-11-22 14:41:16 +0900

    test(InputView): 단위 테스트 추가

--- src/test/java/bridge/view/InputViewTest.java
+++ src/test/java/bridge/view/InputViewTest.java
@@ -3,11 +3,16 @@ package bridge.view;
 import static org.assertj.core.api.Assertions.*;
 
 import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import java.util.List;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
 class InputViewTest {
+    private final InputView inputView = new InputView();
     private final String ERROR = "[ERROR]";
 
     @DisplayName("[예외 테스트] 다리길이 입력값이 숫자가 아님")
@@ -15,7 +20,6 @@ class InputViewTest {
     @ParameterizedTest
     void bridgeSizeNotANum(String input) {
         //given
-        InputView inputView = new InputView();
         System.setIn(new ByteArrayInputStream(input.getBytes()));
         //when then
         assertThatThrownBy(() -> inputView.readBridgeSize())
@@ -28,7 +32,6 @@ class InputViewTest {
     @ParameterizedTest
     void bridgeSizeOutOfRange(String input) {
         //given
-        InputView inputView = new InputView();
         System.setIn(new ByteArrayInputStream(input.getBytes()));
         //when then
         assertThatThrownBy(() -> inputView.readBridgeSize())
@@ -41,7 +44,6 @@ class InputViewTest {
     @ParameterizedTest
     void readBridgeSize(String input) {
         //given
-        InputView inputView = new InputView();
         System.setIn(new ByteArrayInputStream(input.getBytes()));
         //when
         int bridgeSize = inputView.readBridgeSize();
@@ -54,7 +56,6 @@ class InputViewTest {
     @ParameterizedTest
     void movingInputNotProper(String input) {
         //given
-        InputView inputView = new InputView();
         System.setIn(new ByteArrayInputStream(input.getBytes()));
         //when then
         assertThatThrownBy(() -> inputView.readMoving())
@@ -67,7 +68,6 @@ class InputViewTest {
     @ParameterizedTest
     void readMoving(String input) {
         //given
-        InputView inputView = new InputView();
         System.setIn(new ByteArrayInputStream(input.getBytes()));
         //when
         String moving = inputView.readMoving();
@@ -80,7 +80,6 @@ class InputViewTest {
     @ParameterizedTest
     void gameCommandInputNotProper(String input) {
         //given
-        InputView inputView = new InputView();
         System.setIn(new ByteArrayInputStream(input.getBytes()));
         //when then
         assertThatThrownBy(() -> inputView.readGameCommand())
@@ -93,11 +92,58 @@ class InputViewTest {
     @ParameterizedTest
     void readGameCommand(String input) {
         //given
-        InputView inputView = new InputView();
         System.setIn(new ByteArrayInputStream(input.getBytes()));
         //when
         String gameCommand = inputView.readGameCommand();
         //then
         assertThat(gameCommand).isEqualTo(input);
+    }
+
+    @DisplayName("제대로 된 다리 사이즈 입력받을 떄까지 요구")
+    @Test
+    void reReadBridgeSizeWhenError() {
+        //given
+        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(byteArrayOutputStream));
+        List<String> inputs = List.of("0", "a", "1", "abd", "2", "21", "24", "20");
+        String input = String.join("\n", inputs);
+        System.setIn(new ByteArrayInputStream(input.getBytes()));
+        //when
+        inputView.reReadBridgeSizeWhenError();
+        //then
+        String[] resultLines = byteArrayOutputStream.toString().split("\n");
+        assertThat(resultLines).allSatisfy(result -> assertThat(result).contains(ERROR));
+    }
+
+    @DisplayName("제대로 된 이동을 입력받을 떄까지 요구")
+    @Test
+    void reReadMovingWhenError() {
+        //given
+        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(byteArrayOutputStream));
+        List<String> inputs = List.of("a", "1", "4", "UP", "DOWN", "U");
+        String input = String.join("\n", inputs);
+        System.setIn(new ByteArrayInputStream(input.getBytes()));
+        //when
+        inputView.reReadMovingWhenError();
+        //then
+        String[] resultLines = byteArrayOutputStream.toString().split("\n");
+        assertThat(resultLines).allSatisfy(result -> assertThat(result).contains(ERROR));
+    }
+
+    @DisplayName("제대로 된 재도전 여부를 입력받을 떄까지 요구")
+    @Test
+    void reReadGameCommandWhenError() {
+        //given
+        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(byteArrayOutputStream));
+        List<String> inputs = List.of("1", "a", "r", "q", "RETRY", "R");
+        String input = String.join("\n", inputs);
+        System.setIn(new ByteArrayInputStream(input.getBytes()));
+        //when
+        inputView.reReadGameCommandWhenError();
+        //then
+        String[] resultLines = byteArrayOutputStream.toString().split("\n");
+        assertThat(resultLines).allSatisfy(result -> assertThat(result).contains(ERROR));
     }
 }
