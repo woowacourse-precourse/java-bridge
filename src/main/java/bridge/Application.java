7172695b4c013e893403902cb815f49c30dd3894
dingding-21 <phj8221@naver.com>
2022-11-22 22:15:00 +0900

    feat: 재시도 여부에 따른 결과

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -2,39 +2,31 @@ package bridge;
 
 import java.util.List;
 
-import static bridge.PrintMessage.FAIL;
+import static bridge.InputView.readBridgeSize;
+import static bridge.OutputView.printGameStartMessage;
+import static bridge.OutputView.printInputBridgeSizeMessage;
 
 public class Application {
 
     public static void main(String[] args) {
-        OutputView outputView = new OutputView();
-        InputView inputView = new InputView();
 
-        outputView.printGameStartMessage();
-        outputView.printInputBridgeSizeMessage();
+        printGameStartMessage();
+        printInputBridgeSizeMessage();
 
         try {
-            int bridgeSize = inputView.readBridgeSize();
+            int bridgeSize = readBridgeSize();
+            int count = 1;
 
             List<String> randomBridge = new BridgeMaker(new BridgeRandomNumberGenerator()).makeBridge(bridgeSize);
             System.out.println(randomBridge);
 
             BridgeGame bridgeGame = new BridgeGame();
-            for (int i = 0; i < bridgeSize; i++) {
-                outputView.printChooseSpaceToMoveMessage();
-                String space = inputView.readMoving();
-
-                List<String> bridges = bridgeGame.move(space, i, randomBridge.get(i));
-                outputView.printMap(bridges);
-
-                if (bridges.get(2).equals(FAIL)) {
-                    break;
+            if (!bridgeGame.initGame(bridgeSize, bridgeGame, randomBridge)) {
+                while (!bridgeGame.retry(bridgeSize, randomBridge)) {
+                    count += 1;
                 }
             }
-
-            outputView.printChooseRetryGameMessage();
-            inputView.readGameCommand();
-
+            System.out.println(count);
         } catch (IllegalArgumentException exception) {
             System.out.println(exception.getMessage());
         }
--- src/main/java/bridge/BridgeGame.java
+++ src/main/java/bridge/BridgeGame.java
@@ -4,14 +4,17 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+import static bridge.InputView.readGameCommand;
+import static bridge.InputView.readMoving;
+import static bridge.OutputView.*;
 import static bridge.PrintMessage.*;
 
 /**
  * 다리 건너기 게임을 관리하는 클래스
  */
 public class BridgeGame {
-    private static String upBridge = "";
-    private static String downBridge = "";
+    private String upBridge = "";
+    private String downBridge = "";
 
     /**
      * 사용자가 칸을 이동할 때 사용하는 메서드
@@ -97,6 +100,27 @@ public class BridgeGame {
      * <p>
      * 재시작을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void retry() {
+    public Boolean retry(int bridgeSize, List<String> randomBridge) {
+        printChooseRetryGameMessage();
+        String retryGameInput = readGameCommand();
+        if (retryGameInput.equals(QUIT)) return true;
+
+        BridgeGame bridgeGame = new BridgeGame();
+        return initGame(bridgeSize, bridgeGame, randomBridge);
+    }
+
+    public Boolean initGame(int bridgeSize, BridgeGame bridgeGame, List<String> randomBridge) {
+        for (int i = 0; i < bridgeSize; i++) {
+            printChooseSpaceToMoveMessage();
+            String space = readMoving();
+
+            List<String> bridges = bridgeGame.move(space, i, randomBridge.get(i));
+            printMap(bridges);
+
+            if (bridges.get(2).equals(FAIL)) {
+                return false;
+            }
+        }
+        return true;
     }
 }
