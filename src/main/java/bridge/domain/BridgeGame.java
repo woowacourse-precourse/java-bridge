1500c583e55fc81d323110b0dbadda5e9734f5ce
jaehyeon <jhjh0022@naver.com>
2022-11-22 20:05:40 +0900

    style: 주석 제거

--- src/main/java/bridge/domain/BridgeGame.java
+++ src/main/java/bridge/domain/BridgeGame.java
@@ -6,20 +6,11 @@ import bridge.constants.Move;
 import java.util.List;
 import java.util.Objects;
 
-/**
- * 다리 건너기 게임을 관리하는 클래스
- */
 public class BridgeGame {
 
     private boolean canMove;
     private boolean doRetry;
 
-
-    /**
-     * 사용자가 칸을 이동할 때 사용하는 메서드
-     * <p>
-     * 이동을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
-     */
     public void move(String move, int idx, List<String> bridge) {
 
         canMove = checkOneStep(move, bridge, idx);
@@ -36,12 +27,6 @@ public class BridgeGame {
         return Objects.equals(bridge.get(idx), Move.UP_BRIDGE.getValue());
     }
 
-
-    /**
-     * 사용자가 게임을 다시 시도할 때 사용하는 메서드
-     * <p>
-     * 재시작을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
-     */
     public void retry(String retryOrQuit) {
         doRetry = Objects.equals(retryOrQuit, Command.RETRY.getCommandGame());
     }
--- src/main/java/bridge/domain/BridgeMaker.java
+++ src/main/java/bridge/domain/BridgeMaker.java
@@ -5,9 +5,6 @@ import bridge.constants.Move;
 import java.util.ArrayList;
 import java.util.List;
 
-/**
- * 다리의 길이를 입력 받아서 다리를 생성해주는 역할을 한다.
- */
 public class BridgeMaker {
 
     private final BridgeNumberGenerator bridgeNumberGenerator;
