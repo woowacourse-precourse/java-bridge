c76e692331a7db612c87ae19fe1f6b7bc6fb032f
Devtist <yt930310@snu.ac.kr>
2022-11-22 17:42:54 +0900

    refactor: TODO 제거

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -4,8 +4,6 @@ import controller.GameController;
 
 public class Application {
     public static void main(String[] args) {
-        // TODO: 리드미 확인할 것
-        // TODO: 요구사항 제발 제대로 찬찬히 읽어보기
         GameController gameController = new GameController();
         gameController.control();
     }
