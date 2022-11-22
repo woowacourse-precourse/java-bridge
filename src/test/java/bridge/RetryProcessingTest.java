e8446cd878a4f9f2f62a252dc91ff9ec52dc3577
kissshot1104 <kissshot.heartunderblade1104@gmail.com>
2022-11-22 19:15:05 +0900

    Refactor(RetryProcessingTest) : 테스트 코드 간소화
    RetryProcessingTest 클래스의
    너무 긴 메소드의 길이를 줄이기 위해 코드를 간소화 했습니다.

--- src/test/java/bridge/RetryProcessingTest.java
+++ src/test/java/bridge/RetryProcessingTest.java
@@ -3,63 +3,29 @@ package bridge;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 class RetryProcessingTest extends CustomNsTest {
 
     private final RetryProcessing retryProcessing =
             new RetryProcessing();
 
-    @Test
+    @ParameterizedTest
     @DisplayName("게임 다시 시작 여부 묻기")
-    void readRetryTest() {
-
-        //정상 종료할 경우
-        {
-            command("Q");
-            retryProcessing.readRetry();
-            assertThat(output()).contains(
-                    "게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)"
-            );
-        }
-
-        //리트라이 할 경우
-        {
-            command("R");
-            retryProcessing.readRetry();
-            assertThat(output()).contains(
-                    "게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)"
-            );
-        }
+    @CsvSource({
+            "QQ, Q",
+            "RR, R",
+            "D, R"
+    })
+    void readRetryTest(String firstCommand, String secondCommand) {
 
         //처음 잘못된 문자로 인해 오류가 날 경우
-        {
-            command("QQ", "Q");
-            retryProcessing.readRetry();
-            assertThat(output()).contains(
-                    "게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)",
-                    "[ERROR]"
-            );
-        }
-
-        //처음 잘못된 문자로 인해 오류가 날 경우
-        {
-            command("RR", "R");
-            retryProcessing.readRetry();
-            assertThat(output()).contains(
-                    "게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)",
-                    "[ERROR]"
-            );
-        }
-
-        //처음 잘못된 문자로 인해 오류가 날 경우
-        {
-            command("D", "R");
-            retryProcessing.readRetry();
-            assertThat(output()).contains(
-                    "게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)",
-                    "[ERROR]"
-            );
-        }
+        command(firstCommand, secondCommand);
+        retryProcessing.readRetry();
+        assertThat(output()).contains(
+                "게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)",
+                "[ERROR]"
+        );
     }
 }
