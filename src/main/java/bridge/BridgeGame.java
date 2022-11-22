e9a5f6f6a53fc9464c7fe2800d1afce672b6c9c4
WonSubSub <boxer1995@naver.com>
2022-11-22 15:31:30 +0900

    refactor(main): 요구사항에 따른 기능 세분화

--- src/main/java/bridge/BridgeGame.java
+++ src/main/java/bridge/BridgeGame.java
@@ -16,6 +16,8 @@ public class BridgeGame {
     private final List<String> bridge;
     int bridgeCount = 0;
 
+    int tryCount = 0;
+
     String alphabet;
     boolean wrongMovement = true;
     boolean playMore = true;
@@ -33,29 +35,30 @@ public class BridgeGame {
         return bridgeMaker.makeBridge(size);
     }
 
+    public String getAlphabet() {return inputView.readMoving();}
+
     /**
      * 사용자가 칸을 이동할 때 사용하는 메서드
      * <p>
      * 이동을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void move(int bridgeCount) {
+    public void move() {
         System.out.println(bridge);
         bridgeCount = -1;
         do {
-            alphabet = inputView.readMoving();
+            alphabet = getAlphabet();
             bridgeCount += 1;
             outputView.printMap(alphabet, bridge, bridgeCount);
             checkCount(bridgeCount);
         } while (bridge.get(bridgeCount).equals(alphabet) && bridgeCount != size -1);
     }
 
-    public boolean getMove(int bridgeCount) {
-        move(bridgeCount);
+    public boolean getMove() {
+        move();
         return wrongMovement;
     }
 
     public void checkCount(int bridgeCount) {
-        System.out.println(bridgeCount);
         if (bridgeCount == size - 1 && bridge.get(bridgeCount).equals(alphabet)) {
             wrongMovement = false;
         }
@@ -73,14 +76,19 @@ public class BridgeGame {
         return playMore;
     }
 
-    public void resultPrint() {
-        int tryCount = 0;
+    public void playOneGame() {
+        tryCount = 0;
         while (playMore && wrongMovement) {
             outputView.clearResult();
-            wrongMovement = getMove(bridgeCount);
+            wrongMovement = getMove();
             if (wrongMovement) {playMore = retry();}
             tryCount += 1;
         }
+    }
+    public void resultPrint() {
+        playOneGame();
+        outputView.printFinal();
+        outputView.printResult();
         outputView.printResult(wrongMovement, tryCount);
     }
 
