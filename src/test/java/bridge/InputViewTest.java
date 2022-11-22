b4d100fc7200304f40787908c96f20214d6fee79
gmu1026 <gmu1026@gmail.com>
2022-11-22 21:55:58 +0900

    test: 입력 값 검증 테스트 구현

--- src/test/java/bridge/InputViewTest.java
+++ src/test/java/bridge/InputViewTest.java
@@ -4,7 +4,9 @@ package bridge;
 import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
 import static org.assertj.core.api.Assertions.assertThat;
 
+import bridge.domain.BridgeGame;
 import camp.nextstep.edu.missionutils.test.NsTest;
+import java.util.List;
 import org.junit.jupiter.api.Test;
 
 public class InputViewTest extends NsTest {
@@ -12,9 +14,8 @@ public class InputViewTest extends NsTest {
 
     @Test
     void 다리_길이_숫자_아닌_값_입력_테스트() {
-        String inputBridgeSize = "a";
         assertSimpleTest(() -> {
-            runException(inputBridgeSize);
+            runException("a");
             assertThat(output()).contains(ERROR_MESSAGE);
         });
 
@@ -22,9 +23,25 @@ public class InputViewTest extends NsTest {
 
     @Test
     void 다리_길이_범위_벗어난_값_입력_테스트() {
-        String inputBridgeSize = "1";
         assertSimpleTest(() -> {
-            runException(inputBridgeSize);
+            runException("1");
+            assertThat(output()).contains(ERROR_MESSAGE);
+        });
+    }
+
+    @Test
+    void 올바르지_않은_다리_이동_값_입력_테스트() {
+        assertSimpleTest(() -> {
+            runException("3", "A");
+            assertThat(output()).contains(ERROR_MESSAGE);
+        });
+    }
+
+    @Test
+    void 올바르지_않은_게임_명령어_입력_테스트() {
+        BridgeGame bridgeGame = new BridgeGame(List.of("U", "D", "D"));
+        assertSimpleTest(() -> {
+            runException("3", "U", "D", "U", "A");
             assertThat(output()).contains(ERROR_MESSAGE);
         });
     }
