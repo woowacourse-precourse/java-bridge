4add5bd5f9b3738896ba827c69954df69822b2cc
wnsgml7267 <dlwns7267@naver.com>
2022-11-22 22:40:41 +0900

    feat(validation/Validation): 다리의 길이 입력값이 숫자인지 검증하는 기능 추가

--- src/main/java/bridge/validation/Validation.java
+++ src/main/java/bridge/validation/Validation.java
@@ -1,4 +1,25 @@
 package bridge.validation;
 
+import static bridge.command.GameCommand.*;
+import static bridge.command.LocationCommand.*;
+import static bridge.util.Constant.*;
+
 public class Validation {
+
+    public static void checkBridgeSizeInput(String bridgeLength) {
+        checkBridgeLengthConsistOfNum(bridgeLength);
+        checkBridgeLengthStartZero(bridgeLength);
+        checkBridgeLengthRange3To20(bridgeLength);
+    }
+
+    // 다리의 길이 입력값이 숫자인지 검증하는 기능
+    private static void checkBridgeLengthConsistOfNum(String bridgeLength) {
+        if (bridgeLength.isEmpty() || bridgeLength.isBlank()) {
+            throw new IllegalArgumentException(ERROR_MESSAGE_BRIDGE_LENGTH_BLANK_OR_EMPTY);
+        }
+        if (!bridgeLength.matches(BRIDGE_LENGTH_REGEX)) {
+            throw new IllegalArgumentException(ERROR_MESSAGE_BRIDGE_LENGTH_CONSIST_OF_NUM);
+        }
+    }
+
 }
