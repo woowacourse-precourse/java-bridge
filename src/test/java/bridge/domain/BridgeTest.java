e89105857ba6b4358e85c579a8a5528520806821
SangWoo Yun <64531982+Awarduuu@users.noreply.github.com>
2022-11-22 16:44:07 +0900

    test(BridgeTest): Bridge 내장 함수 테스트

--- src/test/java/bridge/domain/BridgeTest.java
+++ src/test/java/bridge/domain/BridgeTest.java
@@ -1,14 +1,33 @@
 package bridge.domain;
 
+import static bridge.utils.Constant.DOWN;
+import static bridge.utils.Constant.UP;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
+import java.util.ArrayList;
+import java.util.List;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class BridgeTest {
+    Bridge bridge = new Bridge("3");
 
+    @DisplayName("현재 스텝의 성공 여부를 저장")
     @Test
-    void name() {
+    void checkNowGoodMove() {
+        List<BridgeResult> bridgeResults = new ArrayList<>();
+        bridgeResults.add(new BridgeResult("up",true));
+        boolean result = bridge.nowGoodMove(bridgeResults);
+
+        assertThat(result).isEqualTo(true);
     }
+
+    @DisplayName("스텝이 bridge 사이즈보다 작은지 여부 저장")
+    @Test
+    void checkNotOverStep() {
+        boolean result = bridge.notOverStep();
+        assertThat(result).isEqualTo(true);
+    }
+
 }
