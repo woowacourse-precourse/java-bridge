1da0e2225d350291d693bb4bb37870aae9646ed8
KIMSEI1124 <workju1124@gmail.com>
2022-11-22 16:23:49 +0900

    refactor: BridgeLayer static import 제거

--- src/main/java/bridge/BridgeMaker.java
+++ src/main/java/bridge/BridgeMaker.java
@@ -1,11 +1,10 @@
 package bridge;
 
+import bridge.service.BridgeLayer;
+
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
-
-import static bridge.service.BridgeLayer.LOWER_LAYER;
-import static bridge.service.BridgeLayer.UPPER_LAYER;
 
 /**
  * 다리의 길이를 입력 받아서 다리를 생성해주는 역할을 한다.
@@ -26,10 +25,10 @@ public class BridgeMaker {
         List<String> bridge = new ArrayList<>(Collections.emptyList());
         for (int i = 0; i < size; i++) {
             int generator = bridgeNumberGenerator.generate();
-            if (generator == LOWER_LAYER.getLayer()) {
+            if (generator == BridgeLayer.LOWER_LAYER.getLayer()) {
                 bridge.add("D");
             }
-            if (generator == UPPER_LAYER.getLayer()) {
+            if (generator == BridgeLayer.UPPER_LAYER.getLayer()) {
                 bridge.add("U");
             }
         }
