24879f1e7cd7d78a3d969a7df81482075aa13aec
DevJay1024 <devjay1024@gmail.com>
2022-11-22 15:30:48 +0900

    test: BridgeGame의 성공여부 반환 기능(사용자경로와 다리-안전경로의 완전일치 확인 기능) 검증을 위한 테스트케이스 작성

--- src/test/java/bridge/BridgeGameTest.java
+++ src/test/java/bridge/BridgeGameTest.java
@@ -3,6 +3,8 @@ package bridge;
 import bridge.ApplicationTest.TestNumberGenerator;
 import bridge.identifiers.Direction;
 import bridge.paths.PlayerPath;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -40,5 +42,55 @@ class BridgeGameTest {
         PlayerPath path2 = game.getPlayerPath();
 
         assertThat(path2).isNotEqualTo(path1);
+    }
+
+    @Nested
+    class CheckCompletelyMatchedTest {
+        BridgeGame game;
+        @BeforeEach
+        void init(){
+            game = new BridgeGame();
+            game.initComponents(
+                    new TestNumberGenerator(newArrayList(1, 0, 1)),
+                    3);
+        }
+
+        @Test
+        void 사용자경로길이가_다리_안전경로보다_모든부분이_일치해야_완전일치다(){
+            game.move(Direction.UP);
+            game.move(Direction.DOWN);
+            game.move(Direction.UP);
+            assertThat(game.getIsSuccess()).isTrue();
+        }
+        @Test
+        void 사용자경로길이가_다리_안전경로보다_짧으면_완전일치가_아니다(){
+            game.move(Direction.UP);
+            game.move(Direction.DOWN);
+            assertThat(game.getIsSuccess()).isFalse();
+        }
+
+        @Test
+        void 사용자경로길이가_다리_안전경로와_첫부분이_다르면_완전일치가_아니다(){
+            game.move(Direction.DOWN);
+            game.move(Direction.DOWN);
+            game.move(Direction.UP);
+            assertThat(game.getIsSuccess()).isFalse();
+        }
+
+        @Test
+        void 사용자경로길이가_다리_안전경로와_중간부분이_다르면_완전일치가_아니다(){
+            game.move(Direction.UP);
+            game.move(Direction.UP);
+            game.move(Direction.UP);
+            assertThat(game.getIsSuccess()).isFalse();
+        }
+
+        @Test
+        void 사용자경로길이가_다리_안전경로와_끝부분이_다르면_완전일치가_아니다(){
+            game.move(Direction.UP);
+            game.move(Direction.DOWN);
+            game.move(Direction.DOWN);
+            assertThat(game.getIsSuccess()).isFalse();
+        }
     }
 }
