941c5b0bcb2bd6fea7a2374c9e19162350385eb9
endlessmomo <soohyuk96@naver.com>
2022-11-22 09:59:31 +0900

    refactor(BridgeGame) : 불필요한 import 삭제 및 자동 줄

--- src/main/java/bridge/model/Service/BridgeGame.java
+++ src/main/java/bridge/model/Service/BridgeGame.java
@@ -13,7 +13,6 @@ import java.util.Collections;
 import java.util.List;
 
 import static bridge.model.constnce.Text.FAIL;
-import static bridge.model.constnce.Text.SUCCESS;
 
 /**
  * 다리 건너기 게임을 관리하는 클래스
@@ -60,7 +59,7 @@ public class BridgeGame {
         return bridgeResult.getResult().equals(FAIL);
     }
 
-    public void failedGame(){
+    public void failedGame() {
         bridgeResult.failedGame();
     }
 
