150102316d4f690a03312c5dc657144c2f9a914d
Yerim lee <103623292+lyrWinterCat@users.noreply.github.com>
2022-11-22 13:11:09 +0900

    Revert "feat (BridgeGameController) : IllegalStateException 발생 시 게임 종료 기능 추가"
    This reverts commit 74cf8d39ce23d25d73c22e41f7b09ec3075be6b0.

--- src/main/java/bridge/controller/BridgeGameController.java
+++ src/main/java/bridge/controller/BridgeGameController.java
@@ -1,19 +1,14 @@
 package bridge.controller;
 
 import bridge.service.BridgeService;
-import bridge.vo.ErrorMessage;
 
 public class BridgeGameController {
     BridgeService bridgeService = new BridgeService();
 
     public void run() {
-        try {
-            bridgeService.startBridgeGame();
-            bridgeService.moveBridge();
-            System.out.println();
-            bridgeService.endBridgeGame();
-        } catch (IllegalStateException e) {
-            System.out.println(ErrorMessage.STATE_EXCEPTION.getErrorMessage());
-        }
+        bridgeService.startBridgeGame();
+        bridgeService.moveBridge();
+        System.out.println();
+        bridgeService.endBridgeGame();
     }
 }
