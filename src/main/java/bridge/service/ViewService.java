bc5d62a6cf3c8431bad62b79be1356c504c2249a
kiarakim <kiarakim413@gmail.com>
2022-11-22 15:27:24 +0900

    feat(bridge) : connect view service with input view and output view

---
+++ src/main/java/bridge/service/ViewService.java
@@ -0,0 +1,41 @@
+package bridge.service;
+
+import bridge.domain.User;
+import bridge.view.InputView;
+import bridge.view.OutputView;
+
+public class ViewService {
+    private final OutputView outputView = new OutputView();
+    private final InputView inputView = new InputView();
+
+    public int requestBridgeSize() {
+        outputView.askBridgeSize();
+        return inputView.readBridgeSize();
+    }
+
+    public String requestMovement() {
+        outputView.askMovement();
+        return inputView.readMoving();
+    }
+
+    public String requestDecision() {
+        outputView.askDecision();
+        return inputView.readDecision();
+    }
+
+    public void printMap(int blockIndex, String movement, boolean isCorrect) {
+        outputView.printMap(blockIndex, movement, isCorrect);
+    }
+
+    public void printResult(User user){
+        outputView.printResult(user);
+    }
+
+    public void printGameStart(){
+        outputView.startBridgeGame();
+    }
+
+    public void initMap(){
+        outputView.initMap();
+    }
+}
