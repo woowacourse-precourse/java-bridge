4b5acc4abca40f8e14d52bf890ffa0cb5c8bff47
juy4556 <kws05050@naver.com>
2022-11-22 16:30:34 +0900

    feat: 이동할 칸 입력 시 올바른 입력(U 또는 D)인지 validation 처리

--- src/main/java/bridge/Exception.java
+++ src/main/java/bridge/Exception.java
@@ -4,6 +4,8 @@ public class Exception {
     private static final String regExp = "^[0-9]+$";
     private static final int MIN = 3;
     private static final int MAX = 20;
+    private static final String UP = "U";
+    private static final String DOWN = "D";
 
 
     public static void validateIsNumeric(String input) {
@@ -18,5 +20,13 @@ public class Exception {
         if (Integer.parseInt(input) < MIN || Integer.parseInt(input) > MAX) {
             throw new IllegalArgumentException(errorMessage.getMessage());
         }
+    }
+
+    public static void validateIsValidMoving(String input) {
+        ErrorMessage errorMessage = ErrorMessage.NOT_VALID_MOVING;
+        if (input.equals(UP) || input.equals(DOWN)){
+            return;
+        }
+        throw new IllegalArgumentException(errorMessage.getMessage());
     }
 }
