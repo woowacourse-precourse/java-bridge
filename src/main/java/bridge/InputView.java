07fee9acd46dd8a2ceb1055319a1988ac9b6b551
Mr.Kim <86274253+Joajy@users.noreply.github.com>
2022-11-22 20:00:11 +0900

    feat: InputView class

--- src/main/java/bridge/InputView.java
+++ src/main/java/bridge/InputView.java
@@ -1,28 +1,55 @@
 package bridge;
+import camp.nextstep.edu.missionutils.Console;
 
-/**
- * 사용자로부터 입력을 받는 역할을 한다.
- */
 public class InputView {
+    private static final String REGEX = "^[0-9]*$";
 
-    /**
-     * 다리의 길이를 입력받는다.
-     */
-    public int readBridgeSize() {
-        return 0;
+    public static int readBridgeSize() {
+        String bridgeSize = Console.readLine();
+        int Size = 0;
+        boolean Try = true;
+        System.out.println(Constant.Input_Length);
+        while(Try){
+            try{
+                Size = Integer.parseInt(bridgeSize);
+                Try = false;
+            } catch(NumberFormatException e){
+                throw new IllegalArgumentException(Constant.Length_Restrict());
+            }
+        }
+        System.out.println(Size);
+        return Size;
     }
 
-    /**
-     * 사용자가 이동할 칸을 입력받는다.
-     */
-    public String readMoving() {
-        return null;
+    public static void checkLengthValidate(String input) {
+        int length = Integer.parseInt(input);
+        if(length < 3 || length > 20 || !input.matches(REGEX))
+            throw new IllegalArgumentException(Constant.Length_Restrict());
     }
 
-    /**
-     * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
-     */
-    public String readGameCommand() {
-        return null;
+    public static String readMoving() {
+        String move = Console.readLine();
+        checkMoveValidate(move);        
+        System.out.println(Constant.Select_Move());
+        System.out.println(move);
+        return move;
+    }
+
+    public static void checkMoveValidate(String input) {
+        if(!(input.equals(Constant.Up) || input.equals(Constant.Down)))
+            throw new IllegalArgumentException(Constant.Move_Restrict());
+    }
+
+    public static String readGameCommand() {
+        String Replay = Console.readLine();
+        checkRetryValidate(Replay);
+        System.out.println(Constant.Select_Replay());
+        System.out.printf(Replay);
+        return Replay;
+    }
+
+    public static void checkRetryValidate(String input) {
+        if(!(input.equals(Constant.Restart) || input.equals(Constant.Quit)))
+            throw new IllegalArgumentException(Constant.Replay_Restrict());
     }
 }
