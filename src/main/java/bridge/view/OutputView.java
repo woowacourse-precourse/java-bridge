679bc47b97ce9b5b1760d03e38f1d318480f90eb
username459712 <94822739+username459712@users.noreply.github.com>
2022-11-22 21:32:19 +0900

    feat: printMoveUpOrDown()-> U로 갈지 D로 갈지 문구 출력

--- src/main/java/bridge/view/OutputView.java
+++ src/main/java/bridge/view/OutputView.java
@@ -8,4 +8,8 @@ public class OutputView {
     public void printBridgeLengthQuestion() {
         System.out.println("\n다리의 길이를 입력해주세요");
     }
+
+    public void printMoveUpOrDown() {
+        System.out.println("\n이동할 칸을 선택해주세요. (위: U, 아래: D)");
+    }
 }
