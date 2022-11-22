1eea3884fc2d9365b4d1190c684fb2c1119630a5
im-shung <seoyoung1113@gmail.com>
2022-11-22 17:34:28 +0900

    feat: :sparkles: main 함수에서 게임 실행 함수 호출

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -1,8 +1,15 @@
 package bridge;
 
+import bridge.service.BridgeGameService;
+
 public class Application {
 
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        BridgeGameService bridgeGameService = new BridgeGameService();
+        try {
+            bridgeGameService.execute();
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
     }
 }
