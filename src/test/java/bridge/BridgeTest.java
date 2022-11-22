2919484235d0a762c29792fe7f733e47c5cdb30e
watterricecake <fruturum@nate.com>
2022-11-22 21:06:23 +0900

    refactor: 불필요한코드 제거

--- src/test/java/bridge/BridgeTest.java
+++ src/test/java/bridge/BridgeTest.java
@@ -1,11 +1,8 @@
 package bridge;
 
 import bridge.model.Bridge;
-import bridge.model.User;
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.assertj.core.util.Lists.newArrayList;
 
