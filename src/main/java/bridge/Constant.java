26f22224dab8bb1423b1db8889caec3fa475b26d
Mr.Kim <86274253+Joajy@users.noreply.github.com>
2022-11-22 12:27:43 +0900

    style: Constant's Code Formatting

--- src/main/java/bridge/Constant.java
+++ src/main/java/bridge/Constant.java
@@ -11,12 +11,12 @@ public class Constant {
     public static String Err_Retry = "게임 종료 및 재시작 관련 입력은 R와 Q만 사용 가능합니다.";
     
     public static String Result_of_game = "최종 게임 결과";
-    public static String Total_Try = "총 시도한 횟수: %d";
-    public static String Result_of_try = "게임 성공 여부: %s";
+    public static String Total_Try = "총 시도한 횟수: ";
+    public static String Result_of_try = "게임 성공 여부: ";
     public static String Num_Regex = "[1-9][0-9]*";
     public static String Front = "[ ";
-    public static String Back = " ]";
-    public static String Delimiter = " | ";
+    public static String Back = "]";
+    public static String Delimiter = "| ";
 
     public static String Win = "성공";
     public static String Lose = "실패";
@@ -28,13 +28,8 @@ public class Constant {
     public static String Quit = "Q";
 
     public static String Success = "O";
-    public static String Failure = "X";
+    public static String Fail = "X";
     public static String Space = " ";
-    
-
-    public static String Game_Start(){
-        return Start_Game;
-    }
 
     public static String Select_Move(){
         return Select_Location;
