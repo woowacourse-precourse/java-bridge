be99ee9323941d511250b147f9162c4bc4a4a23f
YuHayeon <youha0930@gmail.com>
2022-11-22 23:50:20 +0900

    test(Retry): 재시도 입력받고 재시도 여부 확인 테스트

--- src/test/java/bridge/model/RetryTest.java
+++ src/test/java/bridge/model/RetryTest.java
@@ -1,6 +1,9 @@
 package bridge.model;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -18,5 +21,21 @@ class RetryTest {
     void countTry() {
         retry.countTry();
         assertThat(new Retry(2)).isEqualTo(retry);
+    }
+
+    @Test
+    void checkRetryReturnTrue() {
+        assertTrue(retry.checkRetry("R"));
+    }
+
+    @Test
+    void checkRetryReturnFalse() {
+        assertFalse(retry.checkRetry("Q"));
+    }
+
+    @Test
+    void checkRetryException() {
+        assertThatThrownBy(() -> retry.checkRetry("U"))
+                .isInstanceOf(IllegalArgumentException.class);
     }
 }
