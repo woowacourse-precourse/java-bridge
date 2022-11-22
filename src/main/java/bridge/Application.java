8babee66549989ae7028c80ae36e264030a5f1c9
SangMinLeeAI <sangmin.lee.ai@gmail.com>
2022-11-22 21:37:18 +0900

    Feat: Exception 발생시 오류 메세지 출력후 애플리케이션 종료
    Validator에 의해 일어난 Exception에 대응하여 오류 메세지를 출력하고 프로그램을 종료한다.
    
    Signed-off-by: SangMinLeeAI <sangmin.lee.ai@gmail.com>

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -6,7 +6,11 @@ public class Application {
 
     public static void main(String[] args) {
         // TODO: 프로그램 구현
-        BridgeController bridgeController = new BridgeController();
-        bridgeController.process();
+        try {
+            BridgeController bridgeController = new BridgeController();
+            bridgeController.process();
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+        }
     }
 }
