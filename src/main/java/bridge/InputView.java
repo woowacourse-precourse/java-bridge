e75f646f25cd69398c703bf8f9dc545a515c07a5
kyunghyun <rudgusee@gmail.com>
2022-11-22 22:00:06 +0900

    입력

--- src/main/java/bridge/InputView.java
+++ src/main/java/bridge/InputView.java
@@ -50,6 +50,7 @@ public class InputView {
      * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
      */
     public String readGameCommand() {
+        
         while(true) {
             System.out.println("게임을 다시 시도 할지 여부를 입력해주세요. (재시도 : R, 종료 : Q) ");
             String answerRetry = scanner.next();
