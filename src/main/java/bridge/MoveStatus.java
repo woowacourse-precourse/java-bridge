e01681287ab0419fd98846fe99ffeb12fd07a957
wkdghdwns199 <35947667+wkdghdwns199@users.noreply.github.com>
2022-11-22 15:37:34 +0900

    Feat : 움직임 현황 맵 생성 기능 추가

--- src/main/java/bridge/MoveStatus.java
+++ src/main/java/bridge/MoveStatus.java
@@ -17,5 +17,16 @@ public class MoveStatus {
         return makeStatusMap(moveUpper, moveDowner);
     }
 
+    private List<List<String>> makeStatusMap(List<String> moveUpper, List<String> moveDowner) {
+        if(moveMap.isEmpty()){
+            moveMap.add(moveUpper);
+            moveMap.add(moveDowner);
+            return moveMap;
+        }
+        moveMap.set(0,moveUpper);
+        moveMap.set(1,moveDowner);
+        return moveMap;
+    }
+
 
 }
