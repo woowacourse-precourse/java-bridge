c6e6a30a3917ed33eedfb3c6503686c5a332dc66
footdev <ekclstkfka5@gmail.com>
2022-11-22 18:25:22 +0900

    feat: Status getter 생성
    Status의 getter를 생성함.

--- src/main/java/bridge/domain/Status.java
+++ src/main/java/bridge/domain/Status.java
@@ -1,4 +1,24 @@
 package bridge.domain;
 
+import java.util.List;
+
 public class Status {
+
+    private List<String> crossedBridge;
+    private int totalTryNumber;
+    private boolean isSuccess;
+
+    public Status(List<String> crossedBridge, int totalTryNumber, boolean isSuccess) {
+        this.crossedBridge = crossedBridge;
+        this.totalTryNumber = totalTryNumber;
+        this.isSuccess = isSuccess;
+    }
+
+    public int getTotalTryNumber() {
+        return totalTryNumber;
+    }
+
+    public boolean isSuccess() {
+        return isSuccess;
+    }
 }
