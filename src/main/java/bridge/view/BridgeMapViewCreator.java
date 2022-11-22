93804a7337fd7d2ebd0e9199bd1002a0a138c2d7
nomushroomplz <aaron97@knou.ac.kr>
2022-11-22 19:52:27 +0900

    feat: BridgeValidator 유효한 다리 길이값 판별 기능 구현

--- src/main/java/bridge/view/BridgeMapViewCreator.java
+++ src/main/java/bridge/view/BridgeMapViewCreator.java
@@ -21,6 +21,4 @@ public class BridgeMapViewCreator {
 
     private BridgeMapViewCreator() {
     }
-
-    private
 }
---
+++ src/main/java/bridge/view/BridgeValidator.java
@@ -0,0 +1,21 @@
+package bridge.view;
+
+import bridge.exception.BridgeGameError;
+
+public class BridgeValidator {
+    private static final int MIN_LENGTH = 3;
+    private static final int MAX_LENGTH = 20;
+
+    private BridgeValidator() {
+    }
+
+    public static void validateSize(int size) {
+        if (isOutRange(size)) {
+            throw new IllegalArgumentException(BridgeGameError.BRIDGE_SIZE_NOT_INCLUDE_RANGE.getErrorMessage());
+        }
+    }
+
+    private static boolean isOutRange(int size) {
+        return (size < MIN_LENGTH || size > MAX_LENGTH);
+    }
+}
