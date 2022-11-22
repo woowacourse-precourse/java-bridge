d3f00feb6e7b1ee3d41d38ec3b45c2f826f75ff6
BHC-Chicken <tij05078@gmail.com>
2022-11-22 14:13:26 +0900

    feat : 게임 시작
    게임 시작 메시지 출력 후 다리 크기를 입렫 받아 다리를 만든다.
    재시작 메시지가 false가 될때 까지 반복, 재시작 할때마다 카운트를 1씩 증가 시킨다. 게임 종료 후 최종 결과를 출력하고 마무리 한다.

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -1,8 +1,18 @@
 package bridge;
 
 public class Application {
+    private static final OutputView outputView = new OutputView();
+    private static final Progress progress = new Progress();
 
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        int count = 1;
+
+        outputView.startGameMessage();
+        progress.makeBridge();
+        while (progress.game()) {
+            count++;
+        }
+        progress.printResult(count);
     }
+
 }
