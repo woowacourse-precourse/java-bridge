40ad5f42f62504abf8355313da2b1104b341c2dc
hyeongu <wancern@naver.com>
2022-11-22 23:11:06 +0900

    feat(OuterView): printResult 메소드 구현

--- src/main/java/bridge/OutputView.java
+++ src/main/java/bridge/OutputView.java
@@ -29,7 +29,18 @@ public class OutputView {
      * <p>
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void printResult() {
+    public void printResult(List<String> result, boolean flag, int count) {
+        System.out.println("최종 게임 결과");
+        System.out.println(result.get(0));
+        System.out.println(result.get(1));
+
+        if(flag){
+            System.out.println("게임 성공 여부: 성공");
+            System.out.println("총 시도한 횟수: " + count);
+            return;
+        }
+        System.out.println("게임 성공 여부: 실패");
+        System.out.println("총 시도한 횟수: " + count);
     }
 
     public String getMap(List<String> userMoving, boolean iscorrect, String command){
