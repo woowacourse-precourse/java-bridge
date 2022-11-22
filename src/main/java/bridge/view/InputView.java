3996cea0b911893b4d42177df2821f07b86b54b5
Young37 <younghk37@gmail.com>
2022-11-22 17:24:14 +0900

    refactor(view): modify return type and add missing message

--- src/main/java/bridge/view/InputView.java
+++ src/main/java/bridge/view/InputView.java
@@ -1,5 +1,6 @@
 package bridge.view;
 
+import bridge.constant.Direction;
 import bridge.util.BridgeGameStandard;
 
 import camp.nextstep.edu.missionutils.Console;
@@ -8,14 +9,23 @@ import camp.nextstep.edu.missionutils.Console;
  * 사용자로부터 입력을 받는 역할을 한다.
  */
 public class InputView {
-    private static final String BRIDGE_SIZE_ERROR_MESSAGE = "[ERROR] 다리 길이는 3부터 20 사이의 숫자여야 합니다.";
-    private static final String INVALID_DIRECTION_ERROR_MESSAGE = "[ERROR] 이동할 칸을 올바르게 선택해주세요.(위: U, 아래: D)";
+    private static final String BRIDGE_SIZE_INPUT_MESSAGE = "다리의 길이를 입력해주세요.";
+    private static final String DIRECTION_INPUT_MESSAGE = "이동할 칸을 선택해주세요. (위: U, 아래: D)";
+    private static final String GAME_COMMAND_INPUT_MESSAGE = "게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)";
+    
+    private static final String BRIDGE_SIZE_ERROR_MESSAGE = 
+            "[ERROR] 다리 길이는 3부터 20 사이의 숫자여야 합니다.";
+    private static final String INVALID_DIRECTION_ERROR_MESSAGE =
+            "[ERROR] 이동할 칸을 올바르게 선택해주세요.(위: U, 아래: D)";
+    private static final String INVALID_GAME_COMMAND_ERROR_MESSAGE =
+            "[ERROR] 게임을 다시 시도할지 여부를 올바르게 선택해주세요.(위: U, 아래: D)";
 
     /**
      * 다리의 길이를 입력받는다.
      */
     public int readBridgeSize() {
         try {
+            System.out.println(BRIDGE_SIZE_INPUT_MESSAGE);
             String input = Console.readLine();
             validateInteger(input);
             int convertedInput = Integer.parseInt(input);
@@ -51,11 +61,12 @@ public class InputView {
     /**
      * 사용자가 이동할 칸을 입력받는다.
      */
-    public String readMoving() {
+    public Direction readMoving() {
         try {
+            System.out.println(DIRECTION_INPUT_MESSAGE);
             String directcion = Console.readLine();
             validateDirectcion(directcion);
-            return directcion;
+            return Direction.getDirection(directcion);
         } catch (IllegalArgumentException exception) {
             throw new IllegalArgumentException(INVALID_DIRECTION_ERROR_MESSAGE);
         }
