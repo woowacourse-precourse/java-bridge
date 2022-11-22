54444990b8e3181a7892ac043e1b2fe4f1c66909
YUSEONJUN <seonjun0906@gmail.com>
2022-11-22 19:19:10 +0900

    feat : 다리 생성 함수

---
+++ src/main/java/bridge/BridgeProgram.java
@@ -0,0 +1,23 @@
+package bridge;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class BridgeProgram {
+	private final InputView inputView = new InputView();
+	private final BridgeGame bridgeGame = new BridgeGame();
+	private final BridgeRandomNumberGenerator bridgeRandomNumberGenerator = new BridgeRandomNumberGenerator();
+	private final BridgeMaker bridgeMaker = new BridgeMaker(bridgeRandomNumberGenerator);
+	private final Bridge bridge = new Bridge();
+	private final OutputView outputView = new OutputView();
+
+	private final List<String> upBridge = new ArrayList<>();
+	private final List<String> downBridge = new ArrayList<>();
+
+	private int count = 1;
+	private int stack = 0;
+
+	public void useBridge() {
+		bridge.matchBridge(bridgeMaker.makeBridge(checkBridgeSizeInputOrMove()));
+	}
+}
