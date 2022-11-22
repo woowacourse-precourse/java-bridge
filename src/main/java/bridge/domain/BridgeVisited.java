e26ac8373f4d267d8b901dea957b17ee1c303937
LeeJejune <leejejune0625@gmail.com>
2022-11-22 17:27:30 +0900

    feat: 다리의 방문 여부를 정하는 클래스 구현

---
+++ src/main/java/bridge/domain/BridgeVisited.java
@@ -0,0 +1,9 @@
+package bridge.domain;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class BridgeVisited {
+    private final List<BridgeGamePosition> positions = new ArrayList<>();
+    private final BridgeGameMoveInfo moveInfo = new BridgeGameMoveInfo();
+}
