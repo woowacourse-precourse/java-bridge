9712801edc9404c97aeb9b946152882dc8e4e968
Hiluxy <sunset.lhj@gmail.com>
2022-11-22 20:51:35 +0900

    feat: 문자열 변환 후 출력

--- src/main/java/bridge/OutputView.java
+++ src/main/java/bridge/OutputView.java
@@ -6,21 +6,31 @@ import java.util.List;
  * 사용자에게 게임 진행 상황과 결과를 출력하는 역할을 한다.
  */
 public class OutputView {
-    BridgeGame bridgeGame = new BridgeGame();
-    private List<String> bridgeU = bridgeGame.bridgeU;
-    private List<String> bridgeD = bridgeGame.bridgeD;
+
     /**
      * 현재까지 이동한 다리의 상태를 정해진 형식에 맞춰 출력한다.
      * <p>
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void printMap() {
-
+    public void printMap(List<String> bridgeU, List<String> bridgeD) {
+        String str_BridgeU = str_BridgeUD(bridgeU);
+        String str_BridgeD = str_BridgeUD(bridgeD);
+        String re_BridgeU = bridgeReplace(str_BridgeU);
+        String re_BridgeD = bridgeReplace(str_BridgeD);
+        System.out.println(re_BridgeU);
+        System.out.println(re_BridgeD);
     }
 
-    private String str_BridgeUD(List<String> bridge_U_Or_D) {
+    public String str_BridgeUD(List<String> bridge_U_Or_D) {
         String bridgeOX = bridge_U_Or_D.toString();
         return bridgeOX;
+    }
+
+    public String bridgeReplace(String str_Bridge) {
+        String bridgeRe = str_Bridge.replaceAll(",", "|")
+            .replaceFirst("\\[", "[ ")
+            .replaceAll("  ", "   ");
+        return bridgeRe;
     }
 
     /**
