0eec037f4686c157af568b2802ee970718de6b76
username459712 <94822739+username459712@users.noreply.github.com>
2022-11-22 21:17:51 +0900

    feat: static value and default Constructor

---
+++ src/main/java/bridge/domain/User.java
@@ -0,0 +1,10 @@
+package bridge.domain;
+
+public class User {
+    private int playingCount;
+    private boolean quit;
+    public User() {
+        this.playingCount = 1;
+        quit = false;
+    }
+}
