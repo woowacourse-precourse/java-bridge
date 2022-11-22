6ffdd52926d13c8234775aa6a22acffe2ac5689d
Young37 <younghk37@gmail.com>
2022-11-22 19:27:37 +0900

    feat(application): process invalid bridge and arrival

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -63,6 +63,63 @@ public class Application {
         OutputView outputView = new OutputView();
         outputView.printMap(bridgeGameStatus);
 
+        tryCount = processInvalidBridge(bridgeGame, bridgeGameStatus, tryCount);
+        tryCount = processArrival(bridgeGameStatus, outputView, tryCount);
+        return tryCount;
+    }
+
+    private static int processInvalidBridge(BridgeGame bridgeGame, BridgeGameStatus bridgeGameStatus, int tryCount) {
+        InputView inputView = new InputView();
+
+        if (!BridgeGameStatus.isValid(bridgeGameStatus) && !loopReadGameCommand(inputView)) {
+            return quitGame(bridgeGameStatus, tryCount);
+        }
+
+        if (!BridgeGameStatus.isValid(bridgeGameStatus)) {
+            return retryGame(bridgeGame, tryCount);
+        }
+        return tryCount;
+    }
+
+    private static boolean loopReadGameCommand(InputView inputView) {
+        Boolean isGameContinue = null;
+
+        while (isGameContinue == null) {
+            isGameContinue = ReadGameCommand(inputView);
+        }
+
+        return isGameContinue;
+    }
+
+    private static Boolean ReadGameCommand(InputView inputView) {
+        try {
+            boolean isGameContinue = inputView.readGameCommand();
+            return isGameContinue;
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+            return null;
+        }
+    }
+
+    private static int quitGame(BridgeGameStatus bridgeGameStatus, int tryCount) {
+        OutputView outputView = new OutputView();
+        tryCount++;
+        outputView.printResult(bridgeGameStatus, tryCount);
+        return -1;
+    }
+
+    private static int retryGame(BridgeGame bridgeGame, int tryCount) {
+        bridgeGame.retry();
+        return ++tryCount;
+    }
+
+    private static int processArrival(BridgeGameStatus bridgeGameStatus, OutputView outputView, int tryCount) {
+        if (BridgeGameStatus.isArrived(bridgeGameStatus)) {
+            tryCount++;
+            outputView.printResult(bridgeGameStatus, tryCount);
+            return -1;
+        }
+
         return tryCount;
     }
 }
