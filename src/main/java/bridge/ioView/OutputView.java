4e7875efd8bbbb9a548cffc07e584ef1e013284a
zcae1020 <zcae1020@naver.com>
2022-11-22 18:13:25 +0900

    fix: 게임관련 문구 출력 시, 사용자의 보기 좋은 UI를 위한 줄바꿈 추가

--- src/main/java/bridge/ioView/OutputView.java
+++ src/main/java/bridge/ioView/OutputView.java
@@ -13,7 +13,7 @@ public class OutputView {
     private final BridgeGame bridgeGame = new BridgeGame();
 
     public void printGameStart() {
-        System.out.println("다리 건너기 게임을 시작합니다.");
+        System.out.println("다리 건너기 게임을 시작합니다.\n");
     }
 
     public void printBridgeSizeGuide() {
@@ -21,11 +21,11 @@ public class OutputView {
     }
 
     public void printMovingGuide() {
-        System.out.println("이동할 칸을 선택해주세요. (위: U, 아래: D)");
+        System.out.println("\n이동할 칸을 선택해주세요. (위: U, 아래: D)");
     }
 
     public void printGameCommandGuide() {
-        System.out.println("게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)");
+        System.out.println("\n게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)");
     }
 
     /**
@@ -72,9 +72,9 @@ public class OutputView {
      * @param userRoute
      */
     public void printResult(int trial, List<String> userRoute, List<String> bridge) {
-        System.out.println("최종 게임 결과");
+        System.out.println("\n최종 게임 결과");
         printMap(userRoute, bridge);
-        System.out.println("게임 성공 여부: " +
+        System.out.println("\n게임 성공 여부: " +
                 convertSuccessToString(bridgeGame.checkCrossingBridge(userRoute, bridge)));
         System.out.println("총 시도한 횟수: " + trial);
     }
