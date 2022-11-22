88a65821a78c6e9dd8ed6645f33fa6a788a41c90
SangMinLeeAI <sangmin.lee.ai@gmail.com>
2022-11-22 21:34:31 +0900

    Feat: 게임 실행후 다리를 이동할때 사용자 다리 이동 커맨드 검증
    다리를 이동할때 사용자가 정확한 입력을 했는지 검증한다
    
    Signed-off-by: SangMinLeeAI <sangmin.lee.ai@gmail.com>

---
+++ src/main/java/bridge/validator/BridgeMoveInputValidator.java
@@ -0,0 +1,30 @@
+package bridge.validator;
+
+public class BridgeMoveInputValidator {
+
+    public static final String ERROR_PREFIX = "[ERROR] ";
+
+    public static void validate(String input) throws IllegalArgumentException {
+        sizeCheck(input);
+        typeCheck(input);
+        rangeCheck(input);
+    }
+
+    public static void sizeCheck(String input) {
+        if (input.length() != 1) {
+            throw new IllegalArgumentException(ERROR_PREFIX + "입력값이 1자리가 아닙니다.");
+        }
+    }
+
+    public static void typeCheck(String input) {
+        if (!input.equals("U") && !input.equals("D")) {
+            throw new IllegalArgumentException(ERROR_PREFIX + "입력값이 U 또는 D가 아닙니다.");
+        }
+    }
+
+    public static void rangeCheck(String input) {
+        if (input.length() > 1) {
+            throw new IllegalArgumentException(ERROR_PREFIX + "입력값이 1자리가 아닙니다.");
+        }
+    }
+}
