c5dc8f79df459c890d247dc97796a7fd40a0ab4a
YOON <seoung59@gmail.com>
2022-11-22 23:55:39 +0900

    Refactoring

--- src/main/java/bridge/InputView.java
+++ src/main/java/bridge/InputView.java
@@ -1,66 +1,41 @@
 package bridge;
 
-import camp.nextstep.edu.missionutils.Console;
 
-/**
- * 사용자로부터 입력을 받는 역할을 한다.
- */
+import static camp.nextstep.edu.missionutils.Console.readLine;
+
 public class InputView {
-    static final int MIN_BRIDGE_SIZE = 3;
-    static final int MAX_BRIDGE_SIZE = 20;
+    Validator validator = new Validator();
 
-    /**
-     * 다리의 길이를 입력받는다.
-     */
     public int readBridgeSize() {
-        String bridgeSize = Console.readLine();
-        validInput(bridgeSize);
+        String bridgeSize = readLine();
+        try {
+            validator.validateBridgeSize(bridgeSize);
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+            return readBridgeSize();
+        }
         return Integer.parseInt(bridgeSize);
     }
 
-    /**
-     * 사용자가 이동할 칸을 입력받는다.
-     */
     public String readMoving() {
-        String move = Console.readLine();
-        validMoveInput(move);
-        return move;
+        String moving = readLine();
+        try {
+            validator.validateMoving(moving);
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+            return readMoving();
+        }
+        return moving;
     }
 
-    /**
-     * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
-     */
     public String readGameCommand() {
-        String command = Console.readLine();
-        validGameCommand(command);
+        String command = readLine();
+        try {
+            validator.validateGameCommand(command);
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+            return readGameCommand();
+        }
         return command;
-    }
-    private int strToInt(String input){
-        try{
-            return Integer.parseInt(input);
-        }catch (NumberFormatException e){
-            throw new IllegalArgumentException(Messages.IS_NOT_NUMBER.getMessage());
-        }
-    }
-    private void isBetweenRange(int input) {
-        if (input < MIN_BRIDGE_SIZE || input > MAX_BRIDGE_SIZE) {
-            throw new IllegalArgumentException(Messages.ERROR_INPUT_BRIDGE_MESSAGE.getMessage());
-        }
-    }
-    private void validInput(String input){
-        int size = strToInt(input);
-        isBetweenRange(size);
-    }
-
-    private void validMoveInput(String input){
-        if(!input.equals("D") && !input.equals("U")){
-            throw new IllegalArgumentException(Messages.ERROR_INPUT_MOVE_MESSAGE.getMessage());
-        }
-    }
-
-    private void validGameCommand(String input){
-        if(!input.equals("R") && !input.equals("Q")){
-            throw new IllegalArgumentException(Messages.ERROR_RETRY_MESSAGE.getMessage());
-        }
     }
 }
--- src/main/java/bridge/OutputView.java
+++ src/main/java/bridge/OutputView.java
@@ -2,51 +2,42 @@ package bridge;
 
 import java.util.List;
 
-/**
- * 사용자에게 게임 진행 상황과 결과를 출력하는 역할을 한다.
- */
-public class OutputView {
-    static final String IS_SUCCESSFUL = "게임 성공 여부: ";
-    static final String SUCCESS = "성공";
-    static final String FAIL = "실패";
-    static final String TOTAL_TRIES = "총 시도한 횟수: ";
+import static bridge.Constants.*;
 
-    /**
-     * 현재까지 이동한 다리의 상태를 정해진 형식에 맞춰 출력한다.
-     * <p>
-     * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
-     */
-    public void printMap(List<List<String>> bridgePattern) {
-        for(List<String> row : bridgePattern){
+public class OutputView {
+    public void printStart() {
+        System.out.println(START_GAME);
+        System.out.println();
+    }
+
+    public void printInputBridgeSize() {
+        System.out.println(INPUT_BRIDGE_SIZE);
+    }
+
+    public void printInputMoving() {
+        System.out.println(INPUT_MOVING);
+    }
+
+    public void printInputCommand() {
+        System.out.println(INPUT_COMMAND);
+    }
+
+    public void printMap(List<List<String>> bridgeMap) {
+        for (List<String> row: bridgeMap) {
             System.out.println(rowFormat(row));
         }
     }
 
-    private String rowFormat(List<String> row) {
-        String join = String.join(" | ", row);
-        return "[ " + join + " ]";
+    public void printResult(List<List<String>> bridgeMap, boolean isSuccess, int tries) {
+        System.out.println();
+        System.out.println(RESULT);
+        printMap(bridgeMap);
+
+        System.out.println();
+        printSuccess(isSuccess);
+        printTries(tries);
     }
 
-    /**
-     * 게임의 최종 결과를 정해진 형식에 맞춰 출력한다.
-     * <p>
-     * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
-     */
-    public void printResult() {
-    }
-
-    public void printStart(){
-        System.out.println(Messages.START_MESSAGE);
-    }
-    public void printInputBridgeSize(){
-        System.out.println(Messages.INPUT_BRIDGE_MESSAGE);
-    }
-    public void printInputMove(){
-        System.out.println(Messages.INPUT_MOVE_MESSAGE);
-    }
-    public void printInputCommand(){
-        System.out.println(Messages.RETRY_MESSAGE);
-    }
     private void printSuccess(boolean isSuccess) {
         System.out.print(IS_SUCCESSFUL);
         if (isSuccess) {
@@ -55,8 +46,14 @@ public class OutputView {
         }
         System.out.println(FAIL);
     }
+
     private void printTries(int tries) {
         System.out.print(TOTAL_TRIES);
         System.out.println(tries);
+    }
+
+    private String rowFormat(List<String> row) {
+        String joinedRow = String.join(" | ", row);
+        return "[ " + joinedRow + " ]";
     }
 }
