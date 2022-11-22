8a8d765bb395a136913f08a325e7c6e8c03501e7
wnsgml7267 <dlwns7267@naver.com>
2022-11-22 22:17:15 +0900

    feat(view/GameCommand): 게임 커맨드 기능 구현

--- src/main/java/bridge/command/GameCommand.java
+++ src/main/java/bridge/command/GameCommand.java
@@ -1,4 +1,21 @@
 package bridge.command;
 
-public class GameCommand {
+public enum GameCommand {
+
+    Restart("R"),
+    Quit("Q");
+
+    private String gameCommand;
+
+    GameCommand(String gameCommand) {
+        this.gameCommand = gameCommand;
+    }
+
+    public static boolean isRestart(String userInput) {
+        return Restart.gameCommand.equals(userInput);
+    }
+
+    public String getGameCommand() {
+        return gameCommand;
+    }
 }
