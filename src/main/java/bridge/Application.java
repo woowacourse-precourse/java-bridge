eec8feabb25c9af8ed74ab6ae1d0e9442984ad4e
Minsoo Choi <lucas0209@yonsei.ac.kr>
2022-11-22 17:12:17 +0900

    fix(Application): extract class instances as static fields
    Features fixed:
    - Changing class instances to static fields helps maintain fewer parameters on methods
    - Allows init methods to initialize instances

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -4,18 +4,35 @@ import java.util.List;
 
 public class Application {
 
+    static InputView inputView;
+    static OutputView outputView;
+    static BridgeMaker bm;
+    static BridgeGame bg;
+    static User user;
+    static boolean restart = false;
+
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
-        InputView inputView = new InputView();
-        OutputView outputView = new OutputView();
-        BridgeMaker bm = new BridgeMaker(new BridgeRandomNumberGenerator());
-        BridgeGame bg = new BridgeGame();
-        User user = new User();
+        int trial = 0;
+
+        initView();
+        initBridge();
+        user = new User();
 
         greetings();
-        List<String> answer = init(inputView, bm);
+        List<String> answer = init();
         System.out.println("answer = " + answer.toString());
-        moveUntilDone(user, bg, inputView, outputView, answer);
+        askUntilDone(answer, trial);
+    }
+
+
+    private static void initBridge() {
+        bm = new BridgeMaker(new BridgeRandomNumberGenerator());
+        bg = new BridgeGame();
+    }
+
+    private static void initView() {
+        inputView = new InputView();
+        outputView = new OutputView();
     }
 
     public static void greetings() {
@@ -23,51 +40,24 @@ public class Application {
     }
 
     // 다리 생성, 길이 초기 입력
-    private static List<String> init(InputView inputView, BridgeMaker bm) {
+    private static List<String> init() {
         try {
             int sz = inputView.readBridgeSize();
             List<String> moveAnswer = bm.makeBridge(sz);
             return moveAnswer;
         } catch (IllegalArgumentException e) {
             System.out.println(e.getMessage());
-            return init(inputView, bm);
+            return init();
         }
     }
 
-    // 다리 건너기 성공할 때까지 게임 진행
-    private static void moveUntilDone(User user, BridgeGame bg, InputView inputView, OutputView outputView, List<String> answer) {
-        boolean done = false;
-        boolean succeed = false;
-        int trial = 0;
-        while (true) {
-            if (succeed) {
-                break;
-            }
-            if (done) {
-                trial++;
-                if (bg.retry(inputView)) {
-                    user = new User();
-                    done = false;
-                    continue;
-                }
-                outputView.printResult(bg.move(answer, user), "실패", trial);
-                break; // Termination condition: 종료(Q)
-            }
-            done = userSingleMove(user, bg, inputView, outputView, answer);
-            succeed = checkSucceed(bg.move(answer, user), outputView, answer.size(), trial);
-        }
-    }
-
-    private static boolean userSingleMove(User user, BridgeGame bg, InputView inputView, OutputView outputView, List<String> answer) {
+    private static boolean tryAgain() {
         try {
-            String dir = inputView.readMoving();
-            user.saveChoice(dir);
-            List<List<String>> comparedResult = bg.move(answer, user);
-            outputView.printMap(comparedResult);
-            return checkGameOver(comparedResult);
+            String cmd = inputView.readGameCommand();
+            return bg.retry(cmd);
         } catch (IllegalArgumentException e) {
             System.out.println(e.getMessage());
-            return userSingleMove(user, bg, inputView, outputView, answer);
+            return tryAgain();
         }
     }
 
@@ -83,7 +73,7 @@ public class Application {
     }
 
     // Termination condition: 성공
-    private static boolean checkSucceed(List<List<String>> result, OutputView outputView, int size, int trial) {
+    private static boolean checkSucceed(List<List<String>> result, int size, int trial) {
         if (result.size() == size) {
             if (result.get(size - 1).get(0).equals("O")) {
                 trial++;
