72366d3e073e8407fc6def77067c6630d4e2f4f8
leesuengyeol <48343629+syhojeo@users.noreply.github.com>
2022-11-22 19:54:51 +0900

    refactor: 유효성 검사를 진행하는 Validation 객체 생성 및 사용, error message 상수화

--- src/main/java/bridge/InputView.java
+++ src/main/java/bridge/InputView.java
@@ -7,12 +7,20 @@ import camp.nextstep.edu.missionutils.Console;
  */
 public class InputView {
 
+    private final static Validation validation = new Validation();
+    private final static String startMessage = "다리 건너기 게임을 시작합니다.\n\n다리의 길이를 입력해주세요.";
+    private final static String bridgeSizeErrorMessage = "[ERROR] 3 ~ 20 사이의 값을 입력해야 합니다. 다시 입력해주세요.";
+    private final static String moveCommandErrorMessage = "[ERROR] 입력값은 U, D 만 입력 가능합니다. 다시 입력해주세요.(위: U, 아래: D )";
+    private final static String moveSelectMessage = "이동할 칸을 선택해주세요. (위: U, 아래: D)";
+    private final static String retryMessage = "게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)";
+    private final static String retryErrorMessage = "[ERROR] 입력값은 R, Q 만 입력 가능합니다. 다시 입력해주세요.(재시도: U, 종료: D )";
+
     /**
      * 다리의 길이를 입력받는다.
      */
     public int readBridgeSize() {
 
-        System.out.println("다리 건너기 게임을 시작합니다.\n\n다리의 길이를 입력해주세요.");
+        System.out.println(startMessage);
         boolean reInput = true;
         String bridgeSize = "";
 
@@ -20,58 +28,20 @@ public class InputView {
             bridgeSize = Console.readLine();
             reInput = checkValidationOfBridgeSize(bridgeSize);
         }
-
         return Integer.parseInt(bridgeSize);
     }
 
     public boolean checkValidationOfBridgeSize(String bridgeSize) {
         try {
-            checkNull(bridgeSize);
-            checkLengthOfBridgeSize(bridgeSize);
-            checkInteger(bridgeSize);
-            checkNumberOfBridgeSize(bridgeSize);
+            validation.checkIntegrationBridgeSize(bridgeSize);
         } catch (IllegalArgumentException e) {
-            System.out.println("[ERROR] 3 ~ 20 사이의 값을 입력해야 합니다. 다시 입력해주세요.");
+            System.out.println(bridgeSizeErrorMessage);
             return true;
         } catch (NullPointerException e) {
-            System.out.println("[ERROR] 3 ~ 20 사이의 값을 입력해야 합니다. 다시 입력해주세요.");
+            System.out.println(bridgeSizeErrorMessage);
             return true;
         }
         return false;
-    }
-
-    private void checkNull(String bridgeSize) throws NullPointerException{
-
-        if (bridgeSize == null) {
-            throw new NullPointerException();
-        }
-    }
-
-    private void checkNumberOfBridgeSize(String bridgeSize) throws IllegalArgumentException{
-
-        int translatedBridgeSize = Integer.parseInt(bridgeSize);
-
-        if (translatedBridgeSize < 3 || translatedBridgeSize > 20) {
-            throw new IllegalArgumentException();
-        }
-    }
-
-    private void checkInteger(String bridgeSize) throws IllegalArgumentException{
-
-        for (char argument : bridgeSize.toCharArray()) {
-            if (argument < '0' || argument > '9') {
-                throw new IllegalArgumentException();
-            }
-        }
-    }
-
-    private void checkLengthOfBridgeSize(String bridgeSize) throws IllegalArgumentException{
-
-        int stringLength = bridgeSize.length();
-
-        if (stringLength < 1 || stringLength > 2) {
-            throw new IllegalArgumentException();
-        }
     }
 
     /**
@@ -79,7 +49,7 @@ public class InputView {
      */
     public String readMoving() {
 
-        System.out.println("이동할 칸을 선택해주세요. (위: U, 아래: D)");
+        System.out.println(moveSelectMessage);
         boolean reInput = true;
         String inputMoveCommand = "";
         while (reInput) {
@@ -91,35 +61,17 @@ public class InputView {
     }
 
     public boolean checkValidationOfMoveCommand(String inputMoveCommand) {
+
         try {
-            checkNull(inputMoveCommand);
-            checkLengthOfCommand(inputMoveCommand);
-            checkValueOfMoveCommand(inputMoveCommand);
+            validation.checkIntegrationMoveCommand(inputMoveCommand);
         } catch (IllegalArgumentException e) {
-            System.out.println("[ERROR] 입력값은 U, D 만 입력 가능합니다. 다시 입력해주세요.(위: U, 아래: D )");
+            System.out.println(moveCommandErrorMessage);
             return true;
         } catch (NullPointerException e) {
-            System.out.println("[ERROR] 입력값은 U, D 만 입력 가능합니다. 다시 입력해주세요.(위: U, 아래: D )");
+            System.out.println(moveCommandErrorMessage);
             return true;
         }
         return false;
-    }
-
-    private void checkValueOfMoveCommand(String inputMoveCommand) throws IllegalArgumentException{
-
-        char translatedMoveCommand = inputMoveCommand.charAt(0);
-
-        if (translatedMoveCommand == 'D' || translatedMoveCommand == 'U') {
-            return ;
-        }
-        throw new IllegalArgumentException();
-    }
-
-    private void checkLengthOfCommand(String inputCommand) throws IllegalArgumentException{
-
-        if (inputCommand.length() != 1) {
-            throw new IllegalArgumentException();
-        }
     }
 
     /**
@@ -127,7 +79,7 @@ public class InputView {
      */
     public String readGameCommand() {
 
-        System.out.println("게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)");
+        System.out.println(retryMessage);
         boolean reInput = true;
         String inputGameCommand = "";
         while (reInput) {
@@ -139,25 +91,14 @@ public class InputView {
 
     public boolean checkValidationOfGameCommand(String inputGameCommand) {
         try {
-            checkNull(inputGameCommand);
-            checkLengthOfCommand(inputGameCommand);
-            checkValueOfGameCommand(inputGameCommand);
+            validation.checkIntegrationGameCommand(inputGameCommand);
         } catch (IllegalArgumentException e) {
-            System.out.println("[ERROR] 입력값은 R, Q 만 입력 가능합니다. 다시 입력해주세요.(재시도: U, 종료: D )");
+            System.out.println(retryErrorMessage);
             return true;
         } catch (NullPointerException e) {
-            System.out.println("[ERROR] 입력값은 R, Q 만 입력 가능합니다. 다시 입력해주세요. (재시도: U, 종료: D )");
+            System.out.println(retryErrorMessage);
             return true;
         }
         return false;
-    }
-
-    private void checkValueOfGameCommand(String inputGameCommand) throws IllegalArgumentException{
-        char translatedMoveCommand = inputGameCommand.charAt(0);
-
-        if (translatedMoveCommand == 'R' || translatedMoveCommand == 'Q') {
-            return ;
-        }
-        throw new IllegalArgumentException();
     }
 }
---
+++ src/main/java/bridge/Validation.java
@@ -0,0 +1,86 @@
+package bridge;
+
+public class Validation {
+
+    private void checkNull(String bridgeSize) throws NullPointerException {
+
+        if (bridgeSize == null) {
+            throw new NullPointerException();
+        }
+    }
+
+    private void checkNumberOfBridgeSize(String bridgeSize) throws IllegalArgumentException {
+
+        int translatedBridgeSize = Integer.parseInt(bridgeSize);
+
+        if (translatedBridgeSize < 3 || translatedBridgeSize > 20) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private void checkLengthOfBridgeSize(String bridgeSize) throws IllegalArgumentException {
+
+        int stringLength = bridgeSize.length();
+
+        if (stringLength < 1 || stringLength > 2) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private void checkInteger(String bridgeSize) throws IllegalArgumentException {
+
+        for (char argument : bridgeSize.toCharArray()) {
+            if (argument < '0' || argument > '9') {
+                throw new IllegalArgumentException();
+            }
+        }
+    }
+
+    private void checkValueOfMoveCommand(String inputMoveCommand) throws IllegalArgumentException {
+
+        char translatedMoveCommand = inputMoveCommand.charAt(0);
+
+        if (translatedMoveCommand == 'D' || translatedMoveCommand == 'U') {
+            return;
+        }
+        throw new IllegalArgumentException();
+    }
+
+    private void checkLengthOfCommand(String inputCommand) throws IllegalArgumentException {
+
+        if (inputCommand.length() != 1) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private void checkValueOfGameCommand(String inputGameCommand) throws IllegalArgumentException {
+        char translatedMoveCommand = inputGameCommand.charAt(0);
+
+        if (translatedMoveCommand == 'R' || translatedMoveCommand == 'Q') {
+            return;
+        }
+        throw new IllegalArgumentException();
+    }
+
+    public void checkIntegrationBridgeSize(String bridgeSize) throws IllegalArgumentException, NullPointerException {
+
+        checkNull(bridgeSize);
+        checkLengthOfBridgeSize(bridgeSize);
+        checkInteger(bridgeSize);
+        checkNumberOfBridgeSize(bridgeSize);
+    }
+
+    public void checkIntegrationMoveCommand(String inputMoveCommand) throws IllegalArgumentException, NullPointerException {
+
+        checkNull(inputMoveCommand);
+        checkLengthOfCommand(inputMoveCommand);
+        checkValueOfMoveCommand(inputMoveCommand);
+    }
+
+    public void checkIntegrationGameCommand(String inputGameCommand) throws IllegalArgumentException, NullPointerException {
+
+        checkNull(inputGameCommand);
+        checkLengthOfCommand(inputGameCommand);
+        checkValueOfGameCommand(inputGameCommand);
+    }
+}
