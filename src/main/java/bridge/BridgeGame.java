7a3653c20388f43f16f3adf35b18da2a95987734
davinc71998 <davinc71998@gmail.com>
2022-11-22 15:59:36 +0900

    Feat: 추가 move 함수
    move 함수를 통해 올바르게 다리를 건너는지를 출력해주는 함수 구현

--- src/main/java/bridge/BridgeGame.java
+++ src/main/java/bridge/BridgeGame.java
@@ -1,16 +1,35 @@
 package bridge;
 
+import java.util.List;
+
 /**
  * 다리 건너기 게임을 관리하는 클래스
  */
 public class BridgeGame {
+
+
+    static final int RIGHT_MATCH_DOWN = 0;
+    static final int RIGHT_MATCH_UP = 1;
+    static final int WRONG_MATCH_DOWN = 2;
+    static final int WRONG_MATCH_UP = 3;
 
     /**
      * 사용자가 칸을 이동할 때 사용하는 메서드
      * <p>
      * 이동을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void move() {
+    public int move(List<String> bridge, int bridgeIndex, String input) {
+        if(bridge.get(bridgeIndex).equals(input)) {
+            if(input == "U") {
+                return RIGHT_MATCH_UP;
+            }
+            return RIGHT_MATCH_DOWN;
+        }
+
+        if (input == "U") {
+            return WRONG_MATCH_UP;
+        }
+        return WRONG_MATCH_DOWN;
     }
 
     /**
