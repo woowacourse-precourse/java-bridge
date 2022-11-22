4253fcef6c34da146fce4778cc7c4b04241c6829
DESKTOP-AUSH0GT\Power <sgh0718@naver.com>
2022-11-22 16:56:56 +0900

    feat : 입력 기능 클래스에 다리길이, 칸 이동, 게임 재시작여부 등을 입력하는 기능
    간단한 기능들이기 때문에 한 번에 정리해서 올렸다.

--- src/main/java/bridge/InputView.java
+++ src/main/java/bridge/InputView.java
@@ -12,8 +12,9 @@ public class InputView {
      */
     public int readBridgeSize() {
         System.out.println("다리의 길이를 입력해주세요.");
-        Console.readLine();
-        return 0;
+        String size = Console.readLine();
+        int Size = Integer.parseInt(size);
+        return Size;
     }
 
     /**
@@ -21,8 +22,8 @@ public class InputView {
      */
     public String readMoving() {
         System.out.println("이동할 칸을 선택해주세요. (위: U, 아래: D)");
-        Console.readLine();
-        return null;
+        String userUD = Console.readLine();
+        return userUD;
     }
 
     /**
@@ -30,7 +31,7 @@ public class InputView {
      */
     public String readGameCommand() {
         System.out.println("게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)");
-        Console.readLine();
-        return null;
+        String userRQ = Console.readLine();
+        return userRQ;
     }
 }
