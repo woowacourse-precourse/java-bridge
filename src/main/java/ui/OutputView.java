55b9eb6129f7e4d3dfbba07aa6a0f29bb0d56bed
ParkJoungWoo <jug78@naver.com>
2022-11-22 16:16:22 +0900

    refactor(ui/OutputView.java) : 출력 기능 리팩토링
    출력 기능을 요구사항에 맞게 리팩토링 했습니다.
    
    - 출력 양식 수정

--- src/main/java/ui/OutputView.java
+++ src/main/java/ui/OutputView.java
@@ -1,15 +1,13 @@
 package ui;
 
-import java.util.List;
 import java.util.Map;
 
 /**
  * 사용자에게 게임 진행 상황과 결과를 출력하는 역할을 한다.
  */
 public class OutputView {
-    private final String GAME_START_MESSAGE = "다리 건너기 게임을 시작합니다.";
+    private final String GAME_START_MESSAGE = "다리 건너기 게임을 시작합니다.\n";
     private final String GAME_SUCCESS_RESULT = "게임 성공 여부: ";
-    // 게임 성공, 실패는 ENUM으로 관리하도록 고려할 것.
     private final Map<Boolean, String> GAME_RESULT = Map.of(true, "성공",
             false, "실패");
     private final String GAME_TRY = "총 시도한 횟수: ";
@@ -30,10 +28,12 @@ public class OutputView {
     public void printGameTryResultMessage(int userTry) {
         System.out.println(GAME_TRY + userTry);
     }
-    private void printFinalGameMapMessage(){
+
+    private void printFinalGameMapMessage() {
         System.out.println(FINAL_GAME_MAP);
     }
-    public void printFinalGameMap(String map){
+
+    public void printFinalGameMap(String map) {
         printFinalGameMapMessage();
         printMap(map);
     }
