56f1314540f752bb38be60151257df900fc911b2
JUHA <khabh@naver.com>
2022-11-22 16:07:30 +0900

    test(BridgeGame): 컨트롤러에서 BridgeMapService 사용하도록 수정

--- src/test/java/bridge/BridgeGameTest.java
+++ src/test/java/bridge/BridgeGameTest.java
@@ -1,32 +1,38 @@
 package bridge;
 
 import bridge.domain.bridgeGame.BridgeGame;
+import bridge.domain.bridgeGame.gameStatus.gameStatics.GameResult;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import java.util.Queue;
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 public class BridgeGameTest {
+    private List<String> bridge;
     private BridgeGame bridgeGame;
 
     @BeforeEach
     void setUp() {
-        bridgeGame = BridgeGame.initGame();
+        bridge = new ArrayList<>(List.of("U", "U", "D"));
+        bridgeGame = BridgeGame.initGame(bridge);
     }
 
-    @DisplayName("재시도 시 게임 통계 업데이트")
+    @DisplayName("다리 건너기 성공")
     @Test
-    void updateGameStatics() {
-        bridgeGame.move("U", false);
-        Queue<String> gameStatics = bridgeGame.gameStatics();
-        assertThat(gameStatics.poll()).isEqualTo("실패");
-        bridgeGame.retry();
-        bridgeGame.move("D", true);
-        gameStatics = bridgeGame.gameStatics();
-        assertThat(gameStatics.poll()).isEqualTo("성공");
-        assertThat(gameStatics.poll()).isEqualTo("2");
+    void crossSuccess() {
+        bridge.forEach(bridgeGame::move);
+        String gameResult = bridgeGame.getGameStatics().poll();
+        assertThat(gameResult).isEqualTo("성공");
+    }
+
+    @DisplayName("다리 건너기 실패")
+    @Test
+    void crossFail() {
+        GameResult gameResult = bridgeGame.move("D");
+        assertThat(gameResult.toString()).isEqualTo("실패");
     }
 }
