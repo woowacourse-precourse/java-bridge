2f5d178584b0cf5755b2a4c1a1b6a6aa4102c217
shpark30 <seohyeonpark30.gmail.com>
2022-11-22 16:25:15 +0900

    refactor: 재시작/종료 테스트 코드 통합

--- src/test/java/bridge/domain/BridgeGameTest.java
+++ src/test/java/bridge/domain/BridgeGameTest.java
@@ -4,25 +4,30 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import java.util.List;
+import java.util.stream.Stream;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 class BridgeGameTest {
-    @Test
-    void testRetry() {
+    @DisplayName("오답 입력 시 재시작/종료 커맨드 정상 작동 여부 테스트")
+    @MethodSource("provideRetryTestArguments")
+    @ParameterizedTest(name = "{index}: {3}")
+    void testRetryAndQuit(String command, boolean expectedIsRetry, int expectedTrial, String message) {
         BridgeGame bridgeGame = new BridgeGame(List.of("U","D","U"), new Player(new Map()));
         assertThat(bridgeGame.getTrial()).isEqualTo(1);
-        boolean isRetry = bridgeGame.retry("R");
-        assertThat(isRetry).isEqualTo(true);
-        assertThat(bridgeGame.getTrial()).isEqualTo(2);
+        boolean isRetry = bridgeGame.retry(command);
+        assertThat(isRetry).isEqualTo(expectedIsRetry);
+        assertThat(bridgeGame.getTrial()).isEqualTo(expectedTrial);
     }
 
-    @Test
-    void testQuit() {
-        BridgeGame bridgeGame = new BridgeGame(List.of("U","D","U"), new Player(new Map()));
-        assertThat(bridgeGame.getTrial()).isEqualTo(1);
-        boolean isRetry = bridgeGame.retry("Q");
-        assertThat(isRetry).isEqualTo(false);
-        assertThat(bridgeGame.getTrial()).isEqualTo(1);
+    private static Stream<Arguments> provideRetryTestArguments() {
+        return Stream.of(
+                Arguments.of("R", true, 2, "재시작"),
+                Arguments.of("Q", false, 1, "종료")
+        );
     }
 
     @Test
