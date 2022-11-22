68529dfb09cb179523df4cee798d2be499054f5f
HangSub999 <gkdtjq999@gmail.com>
2022-11-22 19:47:44 +0900

    feat: 다리 건너기 게임을 시작합니다 메세지 출력 기능 구현

---
+++ src/main/java/bridge/BridheGameController.java
@@ -0,0 +1,12 @@
+package bridge;
+
+public class BridheGameController {
+    public BridheGameController() {
+        gameStart();
+    }
+
+    private void gameStart() {
+        OutputView outputView = new OutputView();
+        outputView.gameStart();
+    }
+}
