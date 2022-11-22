51d945674aba494d0d7e7d4a4c93ffeda8bce93b
JHS <wnghdtjr129@naver.com>
2022-11-22 19:25:18 +0900

    refactor: 검증 로직 위해 key 타입 변경

--- src/main/java/bridge/model/MovementResultManager.java
+++ src/main/java/bridge/model/MovementResultManager.java
@@ -1,12 +1,14 @@
 package bridge.model;
 
+import bridge.resources.GameCommand;
+
 import java.util.HashMap;
 import java.util.Map;
 
-import static bridge.resources.GameConfig.*;
+import static bridge.resources.GameCommand.*;
 
 public class MovementResultManager {
-    private final Map<String, MovementResult> movementResult;
+    private final Map<GameCommand, MovementResult> movementResult;
 
     public MovementResultManager() {
         movementResult = new HashMap<>();
@@ -15,7 +17,7 @@ public class MovementResultManager {
     }
 
     public void setResult(String moveSpace, boolean canMove) {
-        movementResult.get(moveSpace).setResults(canMove);
+        movementResult.get(getBridgeCommand(moveSpace)).setResults(canMove);
         movementResult.get(reverse(moveSpace)).addBlank();
     }
 
@@ -30,12 +32,5 @@ public class MovementResultManager {
         return movementResult.get(UPPER) +
                 "\n" +
                 movementResult.get(LOWER);
-    }
-
-    private String reverse(String moveSpace) {
-        if (moveSpace.equals(UPPER)) {
-            return LOWER;
-        }
-        return UPPER;
     }
 }
