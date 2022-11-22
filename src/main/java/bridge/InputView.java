76cf483a03569533d6ef47f39e3c0dd1518478d4
limzzum <chfhddl1234@naver.com>
2022-11-22 15:19:18 +0900

    style : 주석 제거

--- src/main/java/bridge/InputView.java
+++ src/main/java/bridge/InputView.java
@@ -2,32 +2,20 @@ package bridge;
 
 import camp.nextstep.edu.missionutils.Console;
 
-/**
- * 사용자로부터 입력을 받는 역할을 한다.
- */
 public class InputView {
 
-    /**
-     * 다리의 길이를 입력받는다.
-     */
     public String readBridgeSize() {
         System.out.println("\n다리의 길이를 입력해주세요.");
         String bridgeLen = Console.readLine();
         return bridgeLen;
     }
 
-    /**
-     * 사용자가 이동할 칸을 입력받는다.
-     */
     public String readMoving() {
         System.out.println("\n이동할 칸을 선택해주세요. (위: U, 아래: D)");
         String direction = Console.readLine();
         return direction;
     }
-
-    /**
-     * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
-     */
+    
     public String readGameCommand() {
         System.out.println("\n게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)");
         String select = Console.readLine();
