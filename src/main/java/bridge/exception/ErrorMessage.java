1543cda7f5a5fd41fd0d654e8a526b3d7e90a535
wltks98 <wltks98@hanmail.net>
2022-11-22 15:12:23 +0900

    fix : 에러 메세지에 [ERROR]가 누락된 부분 수정

--- src/main/java/bridge/exception/ErrorMessage.java
+++ src/main/java/bridge/exception/ErrorMessage.java
@@ -7,8 +7,8 @@ public enum ErrorMessage {
 	NOT_NUMBER_ERROR("[ERROR] 숫자를 입력 해주세요"),
 	BRIDGE_SIZE_RANGE_ERROR(
 		"[ERROR] " + GameRule.MIN_BRIDGE_LENGTH + "에서 " + GameRule.MAX_BRIDGE_LENGTH + "사이의 숫자를 입력해 주세요"),
-	BRIDGE_BOARD_ERROR(GameRule.UP_BOARD + "와 " + GameRule.DOWN_BOARD + "중에 입력해 주세요"),
-	GAME_COMMAND_ERROR(GameRule.RETRY_COMMAND + "와 " + GameRule.QUIT_COMMAND + "중에 입력해 주세요");
+	BRIDGE_BOARD_ERROR("[ERROR] " + GameRule.UP_BOARD + "와 " + GameRule.DOWN_BOARD + "중에 입력해 주세요"),
+	GAME_COMMAND_ERROR("[ERROR] " + GameRule.RETRY_COMMAND + "와 " + GameRule.QUIT_COMMAND + "중에 입력해 주세요");
 
 	private final String message;
 
