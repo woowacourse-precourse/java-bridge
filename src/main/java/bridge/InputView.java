098484f5258ec7e89d2166488003ae74dc3bfea1
jwj <real980312@gmail.com>
2022-11-22 19:09:14 +0900

    <feat> : 다리길이 입력 메서드 사용자 이동 입력 메서드

--- src/main/java/bridge/InputView.java
+++ src/main/java/bridge/InputView.java
@@ -1,5 +1,8 @@
 package bridge;
 
+import java.util.*;
+import static camp.nextstep.edu.missionutils.Console.readLine;
+
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
  */
@@ -9,14 +12,30 @@ public class InputView {
      * 다리의 길이를 입력받는다.
      */
     public int readBridgeSize() {
-        return 0;
+        String input = readLine();
+        int length_bridge;
+        try {
+            length_bridge = Integer.parseInt(input);
+        }catch (Exception e){
+            throw new IllegalArgumentException("[ERROR] 정수가 아닙니다.");
+        }
+        if (length_bridge<3 &&length_bridge>20){
+            throw new IllegalArgumentException("[ERROR] 3이상 20이하의 숫자를 입력하세요");
+        }
+        return length_bridge;
     }
 
     /**
      * 사용자가 이동할 칸을 입력받는다.
      */
     public String readMoving() {
-        return null;
+        String Moving = readLine();
+        if (Moving != "U" || Moving != "D"){
+            throw new IllegalArgumentException("[ERROR] 대문자 U 또는 D를 입력하세요");
+        }
+
+
+        return Moving;
     }
 
     /**
