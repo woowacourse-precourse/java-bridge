fe268473ddb3c9d126a4b3bdcffd6cc538bad624
Kim Seung Beom <88074556+daily1313@users.noreply.github.com>
2022-11-22 19:30:15 +0900

    feat(OutputView) : printInputBridgeLengthMessage() 메서드 구현

--- src/main/java/bridge/view/OutputView.java
+++ src/main/java/bridge/view/OutputView.java
@@ -27,5 +27,9 @@ public class OutputView {
         System.out.println("다리 건너기 게임을 시작합니다.");
     }
 
+    public void printInputBridgeLengthMessage() {
+        System.out.println("\n다리의 길이를 입력해주세요.");
+    }
+
 
 }
