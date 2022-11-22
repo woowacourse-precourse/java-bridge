438fbf1b8f4a4f36ad23881cf10b6a718c76a83b
fnzl54 <rnjscksdud1@gmail.com>
2022-11-22 10:24:39 +0900

    feat: create isInputBound method

--- src/main/java/bridge/ExceptionCase.java
+++ src/main/java/bridge/ExceptionCase.java
@@ -1,2 +1,20 @@
-package bridge;public class ExceptionCase {
+package bridge;
+
+public class ExceptionCase {
+
+    static OutputView display = new OutputView();
+
+    static public void isInputUpOrDown(String input) {
+        if (!(input.equals("U") || input.equals("D"))) {
+            throw new IllegalArgumentException("[ERROR] 다리는 U와 D로만 움직여야 합니다.");
+        }
+    }
+
+    static public void isInputBound(int input) {
+
+        if (!(input >= 3 && input <= 20)) {
+            throw new IllegalArgumentException("[ERROR] 다리의 길이는 3부터 20사이의 숫자여야 합니다.");
+        }
+    }
+
 }
