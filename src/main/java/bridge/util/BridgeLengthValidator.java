a84e81f6006e3ba5e0540bdf64ddd45be3e444d4
footdev <ekclstkfka5@gmail.com>
2022-11-22 11:04:32 +0900

    refactor: BridgeLengthValidator 코드 수정
    에러 메세지를 enum으로 따로 두지 않고 OutputView 클래스에서 상수를 가져오도록 수정함.
    
    #10 #14

--- src/main/java/bridge/util/BridgeLengthValidator.java
+++ src/main/java/bridge/util/BridgeLengthValidator.java
@@ -1,16 +1,17 @@
 package bridge.util;
 
 import bridge.domain.Bridge;
+import bridge.view.OutputView;
 
 public class BridgeLengthValidator {
 
     public static void validateLength(String input) {
         if (isNumeric(input)) {
-            throw new IllegalArgumentException(Message.LENGTH_ERROR.getMessage());
+            throw new IllegalArgumentException(OutputView.LENGTH_ERROR_MESSAGE);
         }
         int length = Integer.parseInt(input);
         if(length < Bridge.minBound || length > Bridge.maxBound) {
-            throw new IllegalArgumentException(Message.LENGTH_ERROR.getMessage());
+            throw new IllegalArgumentException(OutputView.LENGTH_ERROR_MESSAGE);
         }
     }
 
