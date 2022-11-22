b6e30abfe51fd12b4ded9eb2c7693e815bbe3695
hi-june <wnsgur735@gmail.com>
2022-11-22 18:25:57 +0900

    feat(OutputView): add method that create bridge printing format

--- src/main/java/bridge/View/OutputView.java
+++ src/main/java/bridge/View/OutputView.java
@@ -28,4 +28,34 @@ public class OutputView {
     public static void printGameStartMessage() {
         System.out.println(GAME_START_MESSAGE);
     }
+
+    /**
+     * 현재까지 이동한 다리의 상태를 정해진 형식에 맞춰 출력한다.
+     *
+     * @param bridgeFormat 다리의 현재 상태를 갖고 있는 형식
+     */
+    public static void printMap(List<List<String>> bridgeFormat) {
+        String upSideIndex = getUpOrDownSideIndex(bridgeFormat, 0);
+        String downSideIndex = getUpOrDownSideIndex(bridgeFormat, 1);
+        System.out.println(upSideIndex);
+        System.out.println(downSideIndex);
+    }
+
+    /**
+     * 다리 출력 형식에 필요한 sideIndex를 만들고 반환하는 메소드
+     *
+     * @param bridgeFormat 다리의 현재 상태를 갖고 있는 형식
+     * @param index up 일 경우 0이, down일 경우 1이 입력됨
+     * @return sideIndex(String)
+     */
+    private static String getUpOrDownSideIndex(List<List<String>> bridgeFormat, int index) {
+        StringBuilder sideIndex = new StringBuilder();
+        sideIndex.append(START_BRIDGE_POINT);
+        for (List<String> tmp : bridgeFormat) {
+            sideIndex.append(tmp.get(index) + MIDDLE_BRIDGE_POINT);
+        }
+        sideIndex.delete(sideIndex.length()-3, sideIndex.length());
+        sideIndex.append(CLOSE_BRIDGE_POINT);
+        return sideIndex.toString();
+    }
 }
