4d4de70841d54e99732d7871ba524a028418d9ac
ttaehee <ttaehee1105@gmail.com>
2022-11-22 16:09:27 +0900

    refactor : retry 메서드, 기능별로 추가 분리

--- src/main/java/bridge/domain/BridgeGame.java
+++ src/main/java/bridge/domain/BridgeGame.java
@@ -59,9 +59,21 @@ public class BridgeGame {
      * 재시작을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
     public void retry() {
+        resetRoute();
+        resetMovingCount();
+        countGame();
+    }
+
+    private void resetRoute() {
         upMapper.clear();
         downMapper.clear();
+    }
+
+    private void resetMovingCount() {
         movingCount = 0;
+    }
+
+    private void countGame() {
         gameCount++;
     }
 
