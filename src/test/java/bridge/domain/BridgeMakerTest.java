88d7ac4c81add8449c83aab441385b01da982c1c
shpark30 <seohyeonpark30.gmail.com>
2022-11-22 16:54:06 +0900

    refactor: NsTest 상속 삭제 및 다리 길이 범위 테스트 코드 통합

--- src/test/java/bridge/domain/BridgeMakerTest.java
+++ src/test/java/bridge/domain/BridgeMakerTest.java
@@ -1,43 +1,45 @@
 package bridge.domain;
 
-import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeTest;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.assertj.core.util.Lists.newArrayList;
 
-import camp.nextstep.edu.missionutils.test.NsTest;
 import java.util.List;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
-class BridgeMakerTest extends NsTest {
+class BridgeMakerTest {
     @Test
     void makeTest() {
-        assertRandomNumberInRangeTest(() -> run(),
-                1, 0, 1, 0);
-        assertThat(output()).isEqualTo("[U, D, U, D]");
-    }
-
-    @Test
-    void makeUnder3() {
-        BridgeNumberGenerator bridgeNumberGenerator = new BridgeRandomNumberGenerator();
-        BridgeMaker bridgeMaker = new BridgeMaker(bridgeNumberGenerator);
-        assertThatThrownBy(() -> bridgeMaker.makeBridge(2))
-                .isInstanceOf(IllegalArgumentException.class);
-    }
-
-    @Test
-    void makeOver20() {
-        BridgeNumberGenerator bridgeNumberGenerator = new BridgeRandomNumberGenerator();
-        BridgeMaker bridgeMaker = new BridgeMaker(bridgeNumberGenerator);
-        assertThatThrownBy(() -> bridgeMaker.makeBridge(21))
-                .isInstanceOf(IllegalArgumentException.class);
-
-    }
-
-    @Override
-    protected void runMain() {
-        BridgeNumberGenerator bridgeNumberGenerator = new BridgeRandomNumberGenerator();
-        BridgeMaker bridgeMaker = new BridgeMaker(bridgeNumberGenerator);
+        BridgeNumberGenerator numberGenerator = new BridgeMakerTest.TestNumberGenerator(newArrayList(1, 0, 1, 0));
+        BridgeMaker bridgeMaker = new BridgeMaker(numberGenerator);
         List<String> bridge = bridgeMaker.makeBridge(4);
-        System.out.print(bridge);
+        assertThat(bridge).isEqualTo(List.of("U", "D", "U", "D"));
+    }
+
+    @DisplayName("다리 길이가 3 미만, 20 초과 경우에 대한 예외 처리")
+    @ValueSource(ints = {-100,-1, 0, 1, 2, 21, 30, 100, 1000})
+    @ParameterizedTest
+    void outOfRangeBridgeLength(Integer input) {
+        BridgeNumberGenerator bridgeNumberGenerator = new BridgeRandomNumberGenerator();
+        BridgeMaker bridgeMaker = new BridgeMaker(bridgeNumberGenerator);
+        assertThatThrownBy(() -> bridgeMaker.makeBridge(input))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    static class TestNumberGenerator implements BridgeNumberGenerator {
+
+        private final List<Integer> numbers;
+
+        TestNumberGenerator(List<Integer> numbers) {
+            this.numbers = numbers;
+        }
+
+        @Override
+        public int generate() {
+            return numbers.remove(0);
+        }
     }
 }
