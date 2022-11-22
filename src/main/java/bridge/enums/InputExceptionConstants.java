242895b1265b20d2a9305f555bcb052a863e65ff
qkdk <86948395+qkdk@users.noreply.github.com>
2022-11-22 17:16:51 +0900

    feat: Input 오류 처리 기능 구현

--- src/main/java/bridge/enums/InputExceptionConstants.java
+++ src/main/java/bridge/enums/InputExceptionConstants.java
@@ -1,7 +1,7 @@
 package bridge.enums;
 
 public enum InputExceptionConstants {
-    ERROR("[ERROR]"),
+    ERROR("[ERROR] "),
     NONE_NUMERIC("입력이 숫자가 아닙니다."),
     BRIDGE_RANGE("다리 길이는 3부터 20 사이의 숫자여야 합니다."),
     MOVE_COMMAND("잘못된 이동 명령입니다."),
--- src/main/java/bridge/view/InputView.java
+++ src/main/java/bridge/view/InputView.java
@@ -1,5 +1,6 @@
 package bridge.view;
 
+import bridge.Validator;
 import camp.nextstep.edu.missionutils.Console;
 
 /**
@@ -14,20 +15,32 @@ public class InputView {
      * 다리의 길이를 입력받는다.
      */
     public int readBridgeSize() {
-        return Integer.parseInt(Console.readLine());
+        String input = Console.readLine();
+        Validator.isNumeric(input);
+
+        int bridgeSize = Integer.parseInt(input);
+        Validator.isInRange(bridgeSize);
+
+        return bridgeSize;
     }
 
     /**
      * 사용자가 이동할 칸을 입력받는다.
      */
     public String readMoving() {
-        return Console.readLine();
+        String input = Console.readLine();
+        Validator.isMoveCommand(input);
+
+        return input;
     }
 
     /**
      * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
      */
     public String readGameCommand() {
-        return Console.readLine();
+        String input = Console.readLine();
+        Validator.isGameCommand(input);
+
+        return input;
     }
 }
