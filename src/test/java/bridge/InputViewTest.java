6847c5680b9dfe6930d6b22c112332010d66ba01
elizabethgim <yujingim@kookmin.ac.kr>
2022-11-22 23:58:18 +0900

    refactor: @ParameterizedTest를 사용하여 Input 테스트

--- src/test/java/bridge/InputViewTest.java
+++ src/test/java/bridge/InputViewTest.java
@@ -3,6 +3,8 @@ package bridge;
 import camp.nextstep.edu.missionutils.test.NsTest;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import java.io.ByteArrayInputStream;
 import java.io.InputStream;
@@ -15,19 +17,14 @@ public class InputViewTest extends NsTest {
     static void initialize(){
         input = new InputView();
     }
-    @Test
-    void testInput(){
+    @ValueSource(strings = {"U", "D"})
+    @ParameterizedTest
+    void testInput(String example){
         InputStream stdin = System.in;
 
-        String example = "U";
         InputStream stream = new ByteArrayInputStream(example.getBytes(StandardCharsets.UTF_8));
         System.setIn(stream);
-        assertThat(input.readMoving()).isEqualTo("U");
-
-        example = "D";
-        stream = new ByteArrayInputStream(example.getBytes(StandardCharsets.UTF_8));
-        System.setIn(stream);
-        assertThat(input.readMoving()).isEqualTo("D");
+        assertThat(input.readMoving()).isEqualTo(example);
 
         System.setIn(stdin);
     }
