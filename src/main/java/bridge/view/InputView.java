f4d899e68524eab905698c2ae5a82598aa49a3ff
김형기 <khk211113@naver.com>
2022-11-22 19:54:09 +0900

    docs(InputView): 주석 추가

--- src/main/java/bridge/view/InputView.java
+++ src/main/java/bridge/view/InputView.java
@@ -11,7 +11,9 @@ import java.util.NoSuchElementException;
 public class InputView {
 
     /**
-     * 다리의 길이를 입력받는다.
+     * 사용자로부터 다리의 길이를 입력 받는다.
+     * @return 다리의 길이
+     * @throws IllegalArgumentException 숫자 형태의 입력이 아닐 경우 예외 발생
      */
     public int readBridgeSize() throws IllegalArgumentException {
         try {
@@ -22,7 +24,9 @@ public class InputView {
     }
 
     /**
-     * 사용자가 이동할 칸을 입력받는다.
+     * 사용자가 이동할 방향을 입력받는다.
+     * @return 방향 명령어
+     * @throws IllegalArgumentException 명령어를 찾을 수 없거나, 현재 사용할 수 있는 명령어가 아닌 경우 예외 발생
      */
     public CommandType readMoving() throws IllegalArgumentException {
         String input = Console.readLine();
@@ -31,12 +35,21 @@ public class InputView {
 
     /**
      * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
+     * @return 게임 제어 명령어
+     * @throws IllegalArgumentException 명령어를 찾을 수 없거나, 현재 사용할 수 있는 명령어가 아닌 경우 예외 발생
      */
     public CommandType readGameCommand() throws IllegalArgumentException {
         String input = Console.readLine();
         return validatedCommandByGroup(input, CommandGroup.GAME_CONTROL);
     }
 
+    /**
+     * 현재 사용 가능한 명령어인지 검증한다.
+     * @param input 사용자 입력
+     * @param commandGroup 허용하는 명령어 그룹
+     * @return 명령어
+     * @throws IllegalArgumentException 명령어를 찾을 수 없거나, 현재 사용할 수 있는 명령어가 아닌 경우 예외 발생
+     */
     private static CommandType validatedCommandByGroup(String input, CommandGroup commandGroup) throws IllegalArgumentException {
         try {
             CommandType commandType = CommandType.of(input);
