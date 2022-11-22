b582b88f746ef563c1686b82eee3801bbfea8f5d
footdev <ekclstkfka5@gmail.com>
2022-11-22 10:59:16 +0900

    feat: 다리 길이 유효성 검증 기능 추가
    입력으로 들어온 input의 길이가 요구사항에 맞는지 유효성을 검증하는 기능을 추가함.

--- src/main/java/bridge/util/BridgeLengthValidator.java
+++ src/main/java/bridge/util/BridgeLengthValidator.java
@@ -1,4 +1,21 @@
 package bridge.util;
 
+import bridge.domain.Bridge;
+
 public class BridgeLengthValidator {
+
+    public static void validateLength(String input) {
+        if (isNumeric(input)) {
+            throw new IllegalArgumentException(Message.LENGTH_ERROR.getMessage());
+        }
+        int length = Integer.parseInt(input);
+        if(length < Bridge.minBound || length > Bridge.maxBound) {
+            throw new IllegalArgumentException(Message.LENGTH_ERROR.getMessage());
+        }
+    }
+
+    private static boolean isNumeric(String input) {
+        return input.chars()
+                .allMatch(Character::isDigit);
+    }
 }
