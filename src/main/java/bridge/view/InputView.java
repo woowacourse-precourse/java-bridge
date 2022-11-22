a377e8aabbda744da539f8c3930edf5aca078648
yularete <keinikeiwo@gmail.com>
2022-11-22 15:14:36 +0900

    feat: 다리 길이 입력 받기

--- src/main/java/bridge/view/InputView.java
+++ src/main/java/bridge/view/InputView.java
@@ -1,5 +1,10 @@
 package bridge.view;
 
+import bridge.validator.BridgeMakerValidator;
+import camp.nextstep.edu.missionutils.Console;
+
+import static camp.nextstep.edu.missionutils.Console.readLine;
+
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
  */
@@ -9,7 +14,15 @@ public class InputView {
      * 다리의 길이를 입력받는다.
      */
     public int readBridgeSize() {
-        return 0;
+        final BridgeMakerValidator bridgeMakerValidator = new BridgeMakerValidator();
+
+        try {
+            String inputNumber = readLine();
+            bridgeMakerValidator.validateNumber(inputNumber);
+            return Integer.parseInt(inputNumber);
+        } catch (IllegalArgumentException exception) {
+            return readBridgeSize();
+        }
     }
 
     /**
