e7041e42dab0b23c84509a8a4649736fafdab3d3
Kijung Park <kjpark4321@naver.com>
2022-11-22 22:05:06 +0900

    refactor: BridgeMaker 리팩토링

--- src/main/java/bridge/BridgeMaker.java
+++ src/main/java/bridge/BridgeMaker.java
@@ -19,7 +19,6 @@ public class BridgeMaker {
      * @return 입력받은 길이에 해당하는 다리 모양. 위 칸이면 "U", 아래 칸이면 "D"로 표현해야 한다.
      */
     public List<String> makeBridge(int size) {
-
         List<String> generatedBridge = new ArrayList<>();
 
         for (int i = 0; i < size; i++) {
