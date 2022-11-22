02bee7fafc65c88e66ae6b3da5373666a006a21b
tlsqj <rnjstlsqja@inha.edu>
2022-11-22 19:57:05 +0900

    feat(InputView.java): InputView.java 구현
    - readBridgeSize()
        - 다리 길이 입력 받기
    
    - readMoving()
        - 이동할 칸 입력 받기
    
    - readGameCommand()
        - 사용자가 게임을 다시 시도할지 종료할지 여부 입력받기

--- src/main/java/bridge/InputView.java
+++ src/main/java/bridge/InputView.java
@@ -1,28 +1,48 @@
 package bridge;
 
+import camp.nextstep.edu.missionutils.Console;
+
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
  */
 public class InputView {
 
+    OutputView outputView = new OutputView();
     /**
      * 다리의 길이를 입력받는다.
      */
-    public int readBridgeSize() {
-        return 0;
+    public static int readBridgeSize() {
+        int userInputBridgeLength = 0;
+        while(userInputBridgeLength > 25 || userInputBridgeLength < 3) {
+            OutputView.whatSizeBridge();
+            userInputBridgeLength = Integer.parseInt(Console.readLine());
+            if (userInputBridgeLength > 25 || userInputBridgeLength < 3) {
+                OutputView.printError(1);
+            }
+        }
+        return userInputBridgeLength;
     }
 
     /**
      * 사용자가 이동할 칸을 입력받는다.
      */
-    public String readMoving() {
-        return null;
+    public static String readMoving() {
+        String userInput = "";
+        while(!userInput.equals("U") && !userInput.equals("D")){
+            OutputView.printInputWhereToGo();
+            userInput = Console.readLine();
+            if (!userInput.equals("U") && !userInput.equals("D")){
+                OutputView.printError(2);
+            }
+        }
+        return userInput;
     }
 
     /**
      * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
      */
-    public String readGameCommand() {
-        return null;
+    public static String readGameCommand(int i) {
+        OutputView.printInputReGameOrNot();
+        return BridgeGame.retry(Console.readLine(), i);
     }
 }
