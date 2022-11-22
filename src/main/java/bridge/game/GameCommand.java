cd3d46472a047367930b8e46a45785753e6ccc2d
hwiyeon0831 <aab555586@gmail.com>
2022-11-22 20:49:08 +0900

    feat(GameCommand) : 게임 조작 관련 상수 값 정의

---
+++ src/main/java/bridge/game/GameCommand.java
@@ -0,0 +1,23 @@
+package bridge.game;
+
+import bridge.io.ErrorMessage;
+
+import java.util.Arrays;
+
+public enum GameCommand {
+	RETRY("R"),
+	QUIT("Q");
+
+	private final String gameCommandCode;
+
+	GameCommand(String gameCommandCode) {
+		this.gameCommandCode = gameCommandCode;
+	}
+
+	public static GameCommand getGameCommandByGameCommandCode(String gameCommandCode) {
+		return Arrays.stream(values())
+				.filter(gameCommand -> gameCommandCode.equals(gameCommand.gameCommandCode))
+				.findAny()
+				.orElseThrow(() -> new IllegalArgumentException(ErrorMessage.INVALID_INPUT_ERROR_MESSAGE));
+	}
+}
