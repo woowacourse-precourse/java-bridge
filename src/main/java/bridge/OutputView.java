268084b84a11eea8f9f120495b477ff53dd9e5c4
dhyyp1001 <dhyyp1001@gmail.com>
2022-11-22 18:39:43 +0900

    feat: OutputView.java / 시작 메세지를 출력하는 startMessage() 추가

--- src/main/java/bridge/OutputView.java
+++ src/main/java/bridge/OutputView.java
@@ -1,15 +1,19 @@
 package bridge;
 
-/**
- * 사용자에게 게임 진행 상황과 결과를 출력하는 역할을 한다.
- */
+import java.util.ArrayList;
+import java.util.List;
+
 public class OutputView {
 
-    /**
-     * 현재까지 이동한 다리의 상태를 정해진 형식에 맞춰 출력한다.
-     * <p>
-     * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
-     */
+    List<String> trialMap_down = BridgeGame.trialMap_down;
+    List<String> trialMap_up = BridgeGame.trialMap_up;
+    List<String> recordUpTable = new ArrayList<>();
+    List<String> recordDownTable = new ArrayList<>();
+
+    public void startMessage() {
+        System.out.println("다리 건너기 게임을 시작합니다.");
+    }
+
     public void printMap() {
     }
 
