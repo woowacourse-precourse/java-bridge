30f6972953e30b8821fe67421754407a54d50110
K-IMjihun <62210749+K-IMjihun@users.noreply.github.com>
2022-11-22 15:38:53 +0900

    Application // BridgeGame의 input, output 구문 제거

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -1,16 +1,62 @@
 package bridge;
 
+import java.util.List;
+
 public class Application {
     static InputView inputView = new InputView();
+    static OutputView outputView = new OutputView();
     static BridgeGame bridgeGame = new BridgeGame();
+    static BridgeRandomNumberGenerator bridgeRandomNumberGenerator = new BridgeRandomNumberGenerator();
+    static BridgeMaker bridgeMaker = new BridgeMaker(bridgeRandomNumberGenerator);
+    static List<String> randomBridges;
+    static String falseOrTure = "성공";
+    static int count = 1;
 
     public static void main(String[] args) {
         try {
             inputView.readBridgeSize();
-            bridgeGame.move();
+            bridgeGameStart();
         }
         catch(IllegalArgumentException e) {
             System.out.println(e.getMessage());
         }
+    }
+    public static void bridgeGameStart(){
+        randomBridges = bridgeMaker.makeBridge(inputView.bridgeLength);   //랜덤다리 생성
+        for (String randomBridge : randomBridges) {
+            movingCheck(randomBridge, inputView.readMoving());            //값 비교
+            if(falseOrTure.equals("실패")){
+                break;
+            }
+        }
+        successOrFalseCheck(falseOrTure);    //성공 시 결과 출력, 실패 시 retry 구문 실행
+    }
+    public static void movingCheck(String randomBridge, String moving) {
+        if(bridgeGame.move(randomBridge, moving) == true) {
+            outputView.printMap(moving);         //값이 같을 시 출력값에 O 삽입
+        }
+        if (bridgeGame.move(randomBridge, moving) == false) {
+            outputView.falsePrintMap(moving);    //값이 다를 시 출력값에 X 삽입
+            falseOrTure = "실패";
+        }
+    }
+    public static void successOrFalseCheck(String falseOrTure){
+        if(falseOrTure.equals("성공")){   //성공 시 값 출력
+            outputView.printResult(falseOrTure, count);
+        }
+        if(falseOrTure.equals("실패")){   //실패 시 retry 구문 실행
+            String retry = inputView.readGameCommand();
+            bridgeGame.retry(retry);
+        }
+    }
+    public static void exit(){
+        outputView.printResult(falseOrTure, count);
+    }
+    public static void  clearAndRetry(){
+        outputView.init();
+        randomBridges.clear();
+        falseOrTure = "성공";
+        count++;
+        bridgeGameStart();
     }
 }
