e9b9d08d2124fe07950cb307c56075bad1eb3ba6
Hyun-Seo Oh <ohseo8813@gmail.com>
2022-11-22 16:34:28 +0900

    <refactor>(BridgeGameController.java): 로직 application이 아닌 BridgeGameController에서 실행

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -1,56 +1,22 @@
 package bridge;
 
-import bridge.domain.BridgeGame;
-import bridge.domain.BridgeGameRepository;
-import bridge.domain.vo.BridgeSize;
-import bridge.dto.GameResult;
-import bridge.dto.MoveResult;
+import bridge.controller.BridgeGameController;
 import bridge.view.InputView;
 import bridge.view.InputViewProxy;
 import bridge.view.OutputView;
 
-//TODO 객체 관계 그래프 작성
 public class Application {
 
-    private static final InputView inputView = new InputViewProxy(new InputView());
-    private static final OutputView outputView = new OutputView();
-
     public static void main(String[] args) {
-        inputView.printStartMessage();
-        BridgeGame bridgeGame = createBridgeGame(inputView.readBridgeSize());
-        GameResult gameResult = playBridgeGame(bridgeGame);
-        outputView.printResult(gameResult);
+        BridgeGameController bridgeGameController = new BridgeGameController(initInputView(), initOutputView());
+        bridgeGameController.run();
     }
 
-    private static BridgeGame createBridgeGame(BridgeSize bridgeSize) {
-        BridgeMaker bridgeMaker = new BridgeMaker(new BridgeRandomNumberGenerator());
-        BridgeGameRepository bridgeGameRepository = new BridgeGameRepository();
-        return BridgeGame.of(bridgeGameRepository, bridgeMaker, bridgeSize);
+    private static InputView initInputView() {
+        return new InputViewProxy(new InputView());
     }
 
-    private static GameResult playBridgeGame(BridgeGame bridgeGame) {
-        MoveResult moveResult = moveToSelectedBlock(bridgeGame);
-        if (!moveResult.isSuccess()) {
-            return selectWhetherToRetry(bridgeGame);
-        }
-
-        if (!bridgeGame.isGameClear()) {
-            return playBridgeGame(bridgeGame);
-        }
-        return bridgeGame.closeGame();
-    }
-
-    private static MoveResult moveToSelectedBlock(BridgeGame bridgeGame) {
-        MoveResult moveResult = bridgeGame.move(inputView.readMoving());
-        outputView.printMap(moveResult);
-        return moveResult;
-    }
-
-    private static GameResult selectWhetherToRetry(BridgeGame bridgeGame) {
-        if (bridgeGame.retry(inputView.readGameCommand())) {
-            outputView.clearMap();
-            return playBridgeGame(bridgeGame);
-        }
-        return bridgeGame.closeGame();
+    private static OutputView initOutputView() {
+        return new OutputView();
     }
 }
