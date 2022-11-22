c9149b86eb2fb59ea88adc2fdcba72abbef84ff5
Min-ji99 <61926751+Min-ji99@users.noreply.github.com>
2022-11-22 15:17:24 +0900

    Refactor : ResultTest 리펙토링
    하드코딩한 값들 기존에 선언해둔 상수 사용하도록 수정

--- src/test/java/bridge/domain/ResultTest.java
+++ src/test/java/bridge/domain/ResultTest.java
@@ -21,14 +21,14 @@ class ResultTest {
     @DisplayName("moving이 U이고 결과가 X인 경우 저장된 값 확인")
     void addResult1(){
         result.addResult(Constants.MOVE_UP, Constants.WRONG);
-        assertThat(result.getPlayResult().get(0)).isEqualTo(List.of("X"));
-        assertThat(result.getPlayResult().get(1)).isEqualTo(List.of(" "));
+        assertThat(result.getPlayResult().get(0)).isEqualTo(List.of(Constants.WRONG));
+        assertThat(result.getPlayResult().get(1)).isEqualTo(List.of(Constants.NOTHING));
     }
     @Test
     @DisplayName("moving이 D이고 결과가 O인 경우 저장된 값 확인")
     void addResult2(){
         result.addResult(Constants.MOVE_DOWN, Constants.CORRECT);
-        assertThat(result.getPlayResult().get(0)).isEqualTo(List.of(" "));
-        assertThat(result.getPlayResult().get(1)).isEqualTo(List.of("O"));
+        assertThat(result.getPlayResult().get(0)).isEqualTo(List.of(Constants.NOTHING));
+        assertThat(result.getPlayResult().get(1)).isEqualTo(List.of(Constants.CORRECT));
     }
 }
