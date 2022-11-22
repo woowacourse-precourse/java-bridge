37cab52965862d6d74a7e34f50241de40037fa45
wnsgml7267 <dlwns7267@naver.com>
2022-11-22 22:20:43 +0900

    feat(view/BridgeGameController): 최종 게임 진행 기능 구현

--- src/main/java/bridge/controller/BridgeGameController.java
+++ src/main/java/bridge/controller/BridgeGameController.java
@@ -1,4 +1,37 @@
 package bridge.controller;
 
+import static bridge.command.GameCommand.*;
+import static bridge.util.Constant.*;
+
+import bridge.BridgeMaker;
+import bridge.domain.BridgeGame;
+import bridge.view.InputView;
+import bridge.view.OutputView;
+import bridge.view.View;
+import java.util.List;
+
 public class BridgeGameController {
+
+    private final BridgeMaker bridgeMaker;
+    private final InputView inputView;
+    private final OutputView outputView;
+    private final BridgeGame bridgeGame;
+    private String userInput;
+
+    public BridgeGameController(View view, BridgeGame bridgeGame, BridgeMaker bridgeMaker) {
+        this.bridgeMaker = bridgeMaker;
+        this.inputView = view.getInputView();
+        this.outputView = view.getOutputView();
+        this.bridgeGame = bridgeGame;
+    }
+
+    // 게임을 최종적으로 진행하는 기능
+    public void run() {
+        outputView.printStart();
+        int bridgeSize = inputView.readBridgeSize();
+        List<String> bridge = bridgeMaker.makeBridge(bridgeSize);
+        playGame(bridge);
+    }
+
+
 }
