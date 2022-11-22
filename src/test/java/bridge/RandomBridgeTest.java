117b9054cb5970de544e32a0b1657513775ee8ec
kissshot1104 <kissshot.heartunderblade1104@gmail.com>
2022-11-22 19:10:03 +0900

    Refactor(RandomBridgeTest) : 테스트 코드 간소화
    RandomBridgeTest 클래스의
    너무 긴 메소드의 길이를 줄이기 위해 코드를 간소화 했습니다.

--- src/test/java/bridge/RandomBridgeTest.java
+++ src/test/java/bridge/RandomBridgeTest.java
@@ -2,46 +2,31 @@ package bridge;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import java.util.List;
 
 class RandomBridgeTest extends CustomNsTest {
     public final RandomBridge randomBridge = new RandomBridge();
 
-    @Test
+    @ParameterizedTest
     @DisplayName("다리 크기를 제대로 입력 받고 할당하는지 테스트")
-    void initBridgeTest() {
-        {
-            command("20");
-            randomBridge.initBridge();
-            List<String> bridge = randomBridge.getBridge();
-            assertThat(bridge.size()).isEqualTo(20);
-            assertThat(output())
-                    .contains("다리의 길이를 입력해주세요.");
-        }
+    @CsvSource({
+            "-1, 1, 1",
+            "21, 10, 10",
+            "111222333444555666777888999000, 20, 20"
+    })
+    void initBridgeTest(String firstCommand, String secondCommand, int bridgeSize) {
 
-        {
-            command("-1", "5");
-            randomBridge.initBridge();
-            List<String> bridge = randomBridge.getBridge();
-            assertThat(bridge.size()).isEqualTo(5);
-            assertThat(output()).contains(
-                    "[ERROR]",
-                    "다리의 길이를 입력해주세요."
-            );
-        }
-
-        {
-            command("21", "5");
-            randomBridge.initBridge();
-            List<String> bridge = randomBridge.getBridge();
-            assertThat(bridge.size()).isEqualTo(5);
-            assertThat(output()).contains(
-                    "[ERROR]",
-                    "다리의 길이를 입력해주세요."
-            );
-        }
+        command(firstCommand, secondCommand);
+        randomBridge.initBridge();
+        List<String> bridge = randomBridge.getBridge();
+        assertThat(bridge.size()).isEqualTo(bridgeSize);
+        assertThat(output()).contains(
+                "[ERROR]",
+                "다리의 길이를 입력해주세요."
+        );
     }
 }
