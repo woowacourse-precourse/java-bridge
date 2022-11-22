b9039d8fe0632fea49ef36cb5c21abf077b27e26
footdev <ekclstkfka5@gmail.com>
2022-11-22 21:11:55 +0900

    feat: CurrentBridge 추가
    현재 사다리의 상태를 저장하는 CurrentBridge 클래스를 추가함.

---
+++ src/main/java/bridge/domain/CurrentBridge.java
@@ -0,0 +1,20 @@
+package bridge.domain;
+
+import java.util.List;
+
+public class CurrentBridge {
+
+    private final List<String> currentCommand;
+
+    public CurrentBridge(List<String> currentCommand) {
+        this.currentCommand = currentCommand;
+    }
+
+    public String getCurrentCommand(int phase) {
+        return currentCommand.get(phase);
+    }
+
+    public void setCurrentCommand(String command) {
+        currentCommand.add(command);
+    }
+}
