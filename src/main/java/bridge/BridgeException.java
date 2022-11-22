63d424d826cfcf264aabaadbb4c5a7c61ed9acff
tenta <eunoia3802@gmail.com>
2022-11-22 17:08:05 +0900

    feat: 다리 길이 3~20  외에 숫자 입력 받을 시 예외처리 기능 구현

--- src/main/java/bridge/BridgeException.java
+++ src/main/java/bridge/BridgeException.java
@@ -8,11 +8,30 @@ public class BridgeException {
     public int bridgeSizeInputException() {
         try {
             int size = new InputView().readBridgeSize();
+            bridgeLengthException(size);
             return size;
         } catch (IllegalArgumentException e) {
             System.out.println(BRIDGE_SIZE_INPUT_ERROR);
             new BridgeController().makeBridgeController();
         }
         throw new IllegalArgumentException();
+    }
+
+    public void bridgeLengthException(int size) {
+        try {
+            if (!isBridgeSize3To20(size)) {
+                throw new IllegalArgumentException();
+            }
+        } catch (IllegalArgumentException e) {
+            System.out.println(BRIDGE_SIZE_INPUT_ERROR);
+            new BridgeController().makeBridgeController();
+        }
+    }
+
+    public boolean isBridgeSize3To20(int size) {
+        if (size >= 3 && size <= 20) {
+            return true;
+        }
+        return false;
     }
 }
