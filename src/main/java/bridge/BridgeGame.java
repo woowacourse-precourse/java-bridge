be3befb53f460f8adb68f0270d24196e5ed8ef79
c1ce <chalicenuclear.gmail.com>
2022-11-22 20:39:48 +0900

    feat: 마지막 출력결과 중 횟수를 위한 tryCount변수추가

--- src/main/java/bridge/BridgeGame.java
+++ src/main/java/bridge/BridgeGame.java
@@ -9,9 +9,11 @@ import java.util.List;
 public class BridgeGame {
     private List<String> bridgeList ;
     private int count;
+    private int tryCount;
     public BridgeGame(List<String> bridgeList){
         this.bridgeList = bridgeList;
         count = 0;
+        tryCount=1;
     }
 
     /**
@@ -46,6 +48,11 @@ public class BridgeGame {
      * 재시작을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
     public void retry() {
+        tryCount+=1;
         count=0;
+    }
+
+    public int getTryCount(){
+        return tryCount;
     }
 }
