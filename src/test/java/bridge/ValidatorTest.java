f87329028533d382eb9d9b0fe5c3c4304e0f1ede
bearjun05 <bearjun05@naver.com>
2022-11-22 19:06:54 +0900

    test : 다리 길이 입력 값이 숫자 일 때 통과테스트

--- src/test/java/bridge/ValidatorTest.java
+++ src/test/java/bridge/ValidatorTest.java
@@ -1,29 +1,43 @@
 package bridge;
 
-import static bridge.Validator.*;
+import static bridge.Validator.validateIsDigit;
+import static bridge.Validator.validateSizeRange;
 import static org.assertj.core.api.Assertions.assertThatCode;
-import static org.assertj.core.api.Assertions.assertThatNoException;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
 class ValidatorTest {
 
-    @DisplayName("다리 길이 경계값 예외 테스트 ")
+    @DisplayName("다리 길이 사이즈가 범위를 벗어 날 때 경계값 예외테스트 ")
     @ParameterizedTest
-    @ValueSource(ints = {2, 21})
+    @ValueSource(ints = {2, 21, 0, -10})
     void validateSizeRangeExceptionTest(int size) {
         assertThatThrownBy(() -> validateSizeRange(size))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
-    @DisplayName("다리 길이 경계값 통과 테스트 ")
+    @DisplayName("다리 길이 사이즈가 범위 내 일 때 경계값 통과테스트 ")
     @ParameterizedTest
     @ValueSource(ints = {3, 20})
-    void validateSizeRangeTest(int size) {
+    void validateSizeRangePassTest(int size) {
         assertThatCode(() -> validateSizeRange(size)).doesNotThrowAnyException();
+    }
+
+    @DisplayName("다리 길이 입력 값이 숫자가 아닐 때 예외테스트")
+    @ParameterizedTest
+    @ValueSource(strings = {"삼3", "2two", "열개"})
+    void validateIsDigitExceptionTest(String length) {
+        assertThatThrownBy(() -> validateIsDigit(length))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("다리 길이 입력 값이 숫자 일 때 통과테스트")
+    @ParameterizedTest
+    @ValueSource(strings = {"3", "100", "0"})
+    void validateIsDigitPassTest(String length) {
+        assertThatCode(() -> validateIsDigit(length)).doesNotThrowAnyException();
     }
 }
