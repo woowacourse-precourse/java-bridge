117eaf60b3c70485e800ddad7a4451648da4f205
Torres-09 <dla612@naver.com>
2022-11-22 22:47:32 +0900

    feat : 플레이어가 이동하는 입력 값이 올바른 값이 아닌 경우 예외처리 기능 추가

--- src/main/java/bridge/InputView.java
+++ src/main/java/bridge/InputView.java
@@ -10,6 +10,8 @@ public class InputView {
     private final int minBridgeLength = 3;
     private final int maxBridgeLength = 20;
     private final String readBridgeSizeComment = "다리의 길이를 입력해주세요.";
+    private final String moveErrorMessage = "[ERROR] U(위 칸)와 D(아래 칸) 중 하나의 문자를 입력할 수 있습니다.";
+    private final String bridgeSizeErrorMessage = "[ERROR] 다리 길이는 3부터 20 사이의 숫자여야 합니다.";
 
 
     /**
@@ -33,7 +35,7 @@ public class InputView {
             if (bridgeLength >= 3 && bridgeLength <= 20) {
                 return true;
             }
-            throw new IllegalArgumentException("[ERROR] 다리 길이는 3부터 20 사이의 숫자여야 합니다.");
+            throw new IllegalArgumentException(bridgeSizeErrorMessage);
         } catch (IllegalArgumentException e) {
             return errorCase(e);
         }
@@ -41,27 +43,39 @@ public class InputView {
 
     private boolean isInteger(String bridgeLengthInput) {
         try {
-            if (!bridgeLengthInput.chars().allMatch(Character::isDigit)) {
-                throw new IllegalArgumentException("[ERROR] 다리 길이는 3부터 20 사이의 숫자여야 합니다.");
+            if (bridgeLengthInput.chars().allMatch(Character::isDigit)) {
+                return true;
             }
-            return true;
+            throw new IllegalArgumentException(bridgeSizeErrorMessage);
         } catch (IllegalArgumentException e) {
             return errorCase(e);
         }
     }
 
-    private boolean errorCase(IllegalArgumentException e) {
-        System.out.println(e.getMessage());
-        return false;
-    }
+
 
     /**
      * 사용자가 이동할 칸을 입력받는다.
      */
     public String readMoving() {
-        System.out.println("이동할 칸을 선택해주세요. (위: U, 아래: D)");
-        String movingInput = Console.readLine();
-        return movingInput;
+        String moveInput;
+        do {
+            System.out.println("이동할 칸을 선택해주세요. (위: U, 아래: D)");
+            moveInput = Console.readLine();
+
+        } while (!isCorrectMove(moveInput));
+        return moveInput;
+    }
+
+    private boolean isCorrectMove(String movingInput) {
+        try {
+            if (movingInput.equals("U") || movingInput.equals("D")) {
+                return true;
+            }
+            throw new IllegalArgumentException(moveErrorMessage);
+        } catch (IllegalArgumentException e) {
+            return errorCase(e);
+        }
     }
 
     /**
@@ -71,5 +85,10 @@ public class InputView {
         System.out.println("게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)");
         String gameCommandInput = Console.readLine();
         return gameCommandInput;
+    }
+
+    private boolean errorCase(IllegalArgumentException e) {
+        System.out.println(e.getMessage());
+        return false;
     }
 }
