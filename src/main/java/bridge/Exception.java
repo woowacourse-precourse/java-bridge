83324e8af16c475745be500dc1f407bc26af97b7
Voyager003 <85725033+Voyager003@users.noreply.github.com>
2022-11-22 15:00:51 +0900

    Feat: validateInputEmpty  아무런 입력을 받지 않는다면 예외 발생-> 모든 입력메서드에 적용

--- src/main/java/bridge/Exception.java
+++ src/main/java/bridge/Exception.java
@@ -4,6 +4,9 @@ package bridge;
 public class Exception {
 
     public static int validateBridgeSize(String input) {
+        if (validateInputEmpty(input)) {
+            throw new IllegalArgumentException("[ERROR] 아무것도 입력되지 않았습니다. 메시지에 맞도록 입력해주세요.");
+        }
         if (validateInputtype(input) || validateInputrange(input)) {
             throw new IllegalArgumentException("[ERROR] 다리 길이는 3부터 20 사이의 숫자여야 합니다.");
         }
@@ -27,16 +30,42 @@ public class Exception {
 
 
     public static String validateMoving(String input) {
-        if (input.matches("[^UD]")) {
-            throw new IllegalArgumentException("[ERROR] 잘못된 입력입니다. U와 D중 하나를 입력해주세요.");
+        if (validateInputEmpty(input)) {
+            throw new IllegalArgumentException("[ERROR] 아무것도 입력되지 않았습니다. 메시지 조건에 맞게 입력해주세요.");
         }
+        if (validateMovinginput(input))
+            throw new IllegalArgumentException("[ERROR] 잘못된 입력입니다. U와 D중 하나를 입력해주세요.");
         return input;
     }
 
+    private static boolean validateMovinginput(String input) {
+        if (input.matches("[^UD]")) {
+            return true;
+        }
+        return false;
+    }
+
     public static String validateGameCommand(String restartInput) {
-        if (restartInput.matches("[^RQ]")) {
+        if (validateInputEmpty(restartInput)) {
+            throw new IllegalArgumentException("[ERROR] 아무것도 입력되지 않았습니다. 메시지 조건에 맞게 입력해주세요.");
+        }
+        if ( validateGameCommandinput(restartInput)){
             throw new IllegalArgumentException("[ERROR] 잘못된 입력입니다. R과 Q중 하나를 입력해주세요.");
         }
         return restartInput;
+    }
+
+    private static boolean validateGameCommandinput(String restartInput) {
+        if (restartInput.matches("[^RQ]")) {
+            return true;
+        }
+        return false;
+    }
+
+    private static boolean validateInputEmpty(String input) {
+        if (input.equals("")) {
+            return true;
+        }
+        return false;
     }
 }
