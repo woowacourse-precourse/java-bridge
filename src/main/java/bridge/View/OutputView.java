f624edd02b8294c1adfc6358717951797dfb73d5
syahn <496300@naver.com>
2022-11-22 09:59:51 +0900

    refactor: Message enum을 사용하여 map과 결과 출력하도록 변경
    - enum을 사용한 메시지 출력
    - up을 좀 더 성질을 잘 표현하는 block이라는 이름으로 변경

--- src/main/java/bridge/View/OutputView.java
+++ src/main/java/bridge/View/OutputView.java
@@ -6,10 +6,13 @@ import bridge.Model.BridgeMap;
 import java.util.ArrayList;
 import java.util.List;
 
+import static bridge.View.Message.*;
+
 /**
  * 사용자에게 게임 진행 상황과 결과를 출력하는 역할을 한다.
  */
 public class OutputView {
+
     /**
      * 현재까지 이동한 다리의 상태를 정해진 형식에 맞춰 출력한다.
      * <p>
@@ -18,14 +21,12 @@ public class OutputView {
     public void printMap(BridgeMap bridgeMap) {
 
         for (int i = 1; i >= 0; i--) {
-            List<String> up = new ArrayList<>();
+            List<String> block = new ArrayList<>();
             for (List<String> partition : bridgeMap.getMap()) {
-                up.add(partition.get(i));
+                block.add(partition.get(i));
             }
-            String map = String.join(" | ", up);
-            System.out.print("[ ");
-            System.out.print(map);
-            System.out.println(" ]");
+            String map = String.join(" | ", block);
+            System.out.println(OPEN_BRACKET.getMessage() + map + CLOSED_BRACKET.getMessage());
         }
     }
 
@@ -35,18 +36,18 @@ public class OutputView {
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
     public void printResult(BridgeGame bridgeGame) {
-        System.out.println("최종 게임 결과");
+        System.out.println(RESULT_MESSAGE.getMessage());
         printMap(bridgeGame.getBridgeMap());
-        System.out.print("게임 성공 여부: ");
+        System.out.print(SUCCESS_OR_FAILURE_MESSAGE.getMessage());
         String result = checkResult(bridgeGame);
         System.out.println(result);
-        System.out.print("총 시도한 횟수: ");
+        System.out.print(TOTAL_ROUND_MESSAGE.getMessage());
         System.out.println(bridgeGame.getRound());
     }
 
     String checkResult(BridgeGame bridgeGame) {
         if (bridgeGame.checkSuccess())
-            return "성공";
-        return "실패";
+            return SUCCESS_MESSAGE.getMessage();
+        return FAILURE_MESSAGE.getMessage();
     }
 }
