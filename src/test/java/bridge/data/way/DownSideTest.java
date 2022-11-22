df4b0c59db3ef6fb8e65db9c66c2245ea9e00bb4
hojunking96 <junsong96@naver.com>
2022-11-22 11:05:56 +0900

    refactor(DownSideTest.java) : 오타 수정

--- src/test/java/bridge/data/way/DownSideTest.java
+++ src/test/java/bridge/data/way/DownSideTest.java
@@ -11,7 +11,7 @@ class DownSideTest {
 
     @DisplayName("다리 아랫부분을 제대로 생성하는 지 확인한다.")
     @Test
-    void UpSide_생성_테스트() {
+    void makeDownSide_테스트() {
         DownSide downSide = new DownSide();
         downSide.downSideArr = Arrays.asList("O", " ", "O");
         downSide.makeDownSide();
