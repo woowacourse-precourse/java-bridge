3be1d6c1fd124212079304f37bf2730832576d65
bearjun05 <bearjun05@naver.com>
2022-11-22 19:49:59 +0900

    refactor : 하드코딩 되어 있던 출력문 상수 처리

--- src/main/java/bridge/view/OutputView.java
+++ src/main/java/bridge/view/OutputView.java
@@ -3,16 +3,18 @@ package bridge.view;
 import java.util.List;
 import java.util.Stack;
 
-/**
- * 사용자에게 게임 진행 상황과 결과를 출력하는 역할을 한다.
- */
 public class OutputView {
+    public static final String FINAL_GAME_RESULT = "\n최종 게임 결과";
+    public static final String GAME_SUCCESS_STATUS = "\n게임 성공 여부: ";
+    public static final String TOTAL_ATTEMPTS_NUM = "총 시도한 횟수: ";
+    public static final String SUCCESS = "성공";
+    public static final String FAIL = "실패";
+    public static final String START_GAME = "다리 건너기 게임을 시작합니다.";
+    public static final String ENTER_LENGTH = "\n다리의 길이를 입력해주세요.";
+    public static final String SELECT_MOVING_SPACE = "\n이동할 칸을 선택해주세요. (위: U, 아래: D)";
+    public static final String RETRY_STATUS = "\n게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)";
 
-    /**
-     * 현재까지 이동한 다리의 상태를 정해진 형식에 맞춰 출력한다.
-     * <p>
-     * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
-     */
+
     public void printMap(List<Stack> stairs) {
         for (Stack stair : stairs) {
             for (Object o : stair) {
@@ -22,40 +24,35 @@ public class OutputView {
         }
     }
 
-    /**
-     * 게임의 최종 결과를 정해진 형식에 맞춰 출력한다.
-     * <p>
-     * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
-     */
     public void printResult(List<Stack> stairs, int runCnt, boolean isSuccess) {
-        System.out.println("\n최종 게임 결과");
+        System.out.println(FINAL_GAME_RESULT);
         printMap(stairs);
         String success = convertBooleanToStr(isSuccess);
-        System.out.println("\n게임 성공 여부: " + success);
-        System.out.println("총 시도한 횟수: " + runCnt);
+        System.out.println(GAME_SUCCESS_STATUS + success);
+        System.out.println(TOTAL_ATTEMPTS_NUM + runCnt);
     }
 
     private String convertBooleanToStr(boolean isSuccess) {
         if (isSuccess) {
-            return "성공";
+            return SUCCESS;
         }
         if (!isSuccess) {
-            return "실패";
+            return FAIL;
         }
         throw new IllegalArgumentException("잘못된 입력 값입니다.");
     }
 
     public void printStart() {
-        System.out.println("다리 건너기 게임을 시작합니다.\n");
-        System.out.println("다리의 길이를 입력해주세요.");
+        System.out.println(START_GAME);
+        System.out.println(ENTER_LENGTH);
     }
 
     public void printPlay() {
-        System.out.println("이동할 칸을 선택해주세요. (위: U, 아래: D)");
+        System.out.println(SELECT_MOVING_SPACE);
     }
 
     public void printRetry() {
-        System.out.println("게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)");
+        System.out.println(RETRY_STATUS);
     }
 
     public static void printExceptionMessage(String message) {
