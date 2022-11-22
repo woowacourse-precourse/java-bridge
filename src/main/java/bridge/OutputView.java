2b08a4524389e0cf73e199d3bfe73c7ca73777f7
jay <jaewhan232@naver.com>
2022-11-22 22:47:03 +0900

    Implement  : 게임 최종 결과 메세지 출력 메소드 printResult() 구현

--- src/main/java/bridge/OutputView.java
+++ src/main/java/bridge/OutputView.java
@@ -41,10 +41,13 @@ public class OutputView {
      * 게임의 최종 결과를 정해진 형식에 맞춰 출력한다.
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void printResult() {
+    public void printResult(BridgeGame bridgeGame) {
+        printMessage(gameResultMessage);
+        for (MovingCommand command: MovingCommand.values()){
+            printMessage(bridgeGame.getResultStringOf(command.getCommand()));
+        }
+        printMessage(String.format(whetherSuccessMessage, bridgeGame.getGameResult()));
+        printMessage(String.format(numberOfTrialMessage, bridgeGame.getNumberOfTrials()));
     }
 
-    public void pringMessage(String message) {
-        System.out.println(message);
-    }
 }
