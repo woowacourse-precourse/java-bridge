9f3577d09636ae994771904f8cbf80d36d99d9d0
SeongbinPark <binni0515@gmail.com>
2022-11-22 19:31:59 +0900

    feat(GameController): add constructor

--- src/main/java/bridge/controller/GameController.java
+++ src/main/java/bridge/controller/GameController.java
@@ -1,7 +1,24 @@
 package bridge.controller;
 
+import static bridge.constant.message.OutputMessage.FAILURE;
+
+import bridge.GameCommand;
+import bridge.Map;
+import bridge.view.OutputView;
+
 public class GameController {
 
+    private final OutputView outputView;
+    private static int attempts = 1;
+    private String success;
+    private static GameCommand gameCommand = GameCommand.RETRY;
+    private Map map;
+
+    public GameController() {
+        outputView = new OutputView();
+        success = FAILURE.getMessage();
+    }
+    
     public void start() {
 
     }
