2a66f097a47d9e939daf2973ba6ba1c791a7b83b
Kim Seung Beom <88074556+daily1313@users.noreply.github.com>
2022-11-22 17:58:53 +0900

    feat(Game) : Game 클래스에 필요한 상수 목록 및 생성자 추가

---
+++ src/main/java/bridge/domain/Game.java
@@ -0,0 +1,17 @@
+package bridge.domain;
+
+public class Game {
+    private boolean isGameDone;
+    private boolean isWinGame;
+
+    private int gameCount;
+
+    public Game() {
+        this.isGameDone = false;
+        this.isWinGame = false;
+        this.gameCount = 1;
+    }
+
+
+
+}
