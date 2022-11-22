90dd30e78d8f9bc61e9fc811d7779bc80af0370c
jay <jaewhan232@naver.com>
2022-11-22 22:47:43 +0900

    Implement : 다리 건너기를 한차례 시도하는 runPhase() 메소드 구현

--- src/main/java/bridge/BridgeGameController.java
+++ src/main/java/bridge/BridgeGameController.java
@@ -12,5 +12,18 @@ public class BridgeGameController {
         this.bridgeMaker = new BridgeMaker(new BridgeRandomNumberGenerator());
     }
 
+    private void runPhase(BridgeGame bridgeGame) {
+        while (!bridgeGame.isCompleted()) {
+            bridgeGame.move(inputView.getMoving());
+            outputView.printMap(bridgeGame);
+            if (bridgeGame.isCorrect()) continue;
+            if (inputView.getGameCommand().equals(GameCommand.RETRY.getCommand())) {
+                bridgeGame.retry();
+                continue;
+            }
+            bridgeGame.saveCurrentPlay();
+            break;
+        }
+    }
 
 }
