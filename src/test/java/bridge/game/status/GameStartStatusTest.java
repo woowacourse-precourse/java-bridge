9e745772505ce341dee193c18b7c890810a5b9ab
jaehong <println@kakao.com>
2022-11-22 10:25:30 +0900

    test (GameStartStatusTest) : TestUtils를 활용하여 중복된 코드 제거

--- src/test/java/bridge/game/status/GameStartStatusTest.java
+++ src/test/java/bridge/game/status/GameStartStatusTest.java
@@ -1,9 +1,9 @@
 package bridge.game.status;
 
-import bridge.game.context.BridgeGameContextImpl;
-import bridge.view.InputView;
-import bridge.view.OutputView;
-import org.assertj.core.api.Assertions;
+import static org.assertj.core.api.Assertions.assertThat;
+
+import bridge.game.context.FakeContext;
+import bridge.utils.TestUtils;
 import org.junit.jupiter.api.DisplayNameGeneration;
 import org.junit.jupiter.api.DisplayNameGenerator;
 import org.junit.jupiter.api.Test;
@@ -11,12 +11,11 @@ import org.junit.jupiter.api.Test;
 @DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
 class GameStartStatusTest {
 
-
     @Test
     void 게임시작상태_다음은_게임_다리_생성_상태입니다() {
-        var actual = new GameStartStatus().next(new BridgeGameContextImpl(), new InputView(), new OutputView());
-        Assertions.assertThat(actual)
-                .isInstanceOf(GenerateBridgeStatus.class);
+        var actual = TestUtils.statusNext(new GameStartStatus(), new FakeContext());
+
+        assertThat(actual).isInstanceOf(GenerateBridgeStatus.class);
     }
 
 }
