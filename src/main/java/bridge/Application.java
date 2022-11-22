189c7271f44e042f79433b33df0f479400210263
elizabethgim <yujingim@kookmin.ac.kr>
2022-11-22 15:43:16 +0900

    add: 게임 시작 문구, 다리 길이 입력 문구 추가

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -29,6 +29,9 @@ public class Application {
     }
     public static void main(String[] args) {
         try{
+            System.out.println("다리 건너기 게임을 시작합니다.");
+
+            System.out.println("다리 길이를 입력해주세요.");
             int size;
             size = input.readBridgeSize();
             List<String> bridge = bridgeMaker.makeBridge(size);
