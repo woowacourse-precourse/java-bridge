75ecb5996199a1712c056988f017a317e72fa1b5
song-wooseok <sws981202@gmail.com>
2022-11-22 15:02:25 +0900

    feat: 게임 진행 결과(다리 지도)를 출력하는 기능

--- src/main/java/bridge/views/OutputView.java
+++ src/main/java/bridge/views/OutputView.java
@@ -1,23 +1,28 @@
 package bridge.views;
 
-/**
- * 사용자에게 게임 진행 상황과 결과를 출력하는 역할을 한다.
- */
+import bridge.utils.constants.BridgeRoute;
+import bridge.utils.constants.BridgeShape;
+
+import java.util.HashMap;
+import java.util.List;
+
 public class OutputView {
 
-    /**
-     * 현재까지 이동한 다리의 상태를 정해진 형식에 맞춰 출력한다.
-     * <p>
-     * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
-     */
-    public void printMap() {
+    public void printMap(HashMap<String, List<String>> mapInfo) {
+        StringBuilder map = new StringBuilder();
+        bridgeLineToString(map, mapInfo.get(BridgeRoute.UP.getRoute()));
+        bridgeLineToString(map, mapInfo.get(BridgeRoute.DOWN.getRoute()));
+        System.out.println(map);
     }
 
-    /**
-     * 게임의 최종 결과를 정해진 형식에 맞춰 출력한다.
-     * <p>
-     * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
-     */
-    public void printResult() {
+    private void bridgeLineToString(StringBuilder map, List<String> bridgeLine){
+        map.append(BridgeShape.START_BRIDGE);
+        map.append(String.join(BridgeShape.BOUNDARY_BRIDGE, bridgeLine));
+        map.append(BridgeShape.END_BRIDGE+"\n");
     }
+
+    public void printResult() {
+
+    }
+
 }
