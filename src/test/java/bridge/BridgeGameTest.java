8b1c2b35ec65f9840499196b5cfe38c0ebc0ca85
rt3310 <btac3310@gmail.com>
2022-11-22 13:05:42 +0900

    test: 클래스 분리에 따른 테스트 코드 수정

--- src/test/java/bridge/BridgeGameTest.java
+++ src/test/java/bridge/BridgeGameTest.java
@@ -2,6 +2,8 @@ package bridge;
 
 import bridge.domain.Bridge;
 import bridge.domain.BridgeGame;
+import bridge.domain.User;
+import bridge.util.BridgeBlock;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import java.util.List;
@@ -12,8 +14,8 @@ public class BridgeGameTest {
     @DisplayName("이동에 성공했으면 true를 반환한다")
     @Test
     void moveSuccess() {
-        Bridge bridge = new Bridge(List.of("D", "U", "D"));
-        BridgeGame bridgeGame = new BridgeGame(bridge);
+        User user = new User(new Bridge(List.of("D", "U", "D")));
+        BridgeGame bridgeGame = new BridgeGame(user);
 
         boolean move = bridgeGame.move("D");
 
@@ -23,46 +25,68 @@ public class BridgeGameTest {
     @DisplayName("이동에 실패했으면 false를 반환한다")
     @Test
     void moveFail() {
-        Bridge bridge = new Bridge(List.of("D", "U", "D"));
-        BridgeGame bridgeGame = new BridgeGame(bridge);
+        User user = new User(new Bridge(List.of("D", "U", "D")));
+        BridgeGame bridgeGame = new BridgeGame(user);
 
         boolean move = bridgeGame.move("U");
 
         assertThat(move).isEqualTo(false);
     }
 
+    @DisplayName("재시작을 선택하면 시도 횟수가 증가한다")
+    @Test
+    void getTrialCountByRetry() {
+        User user = new User(new Bridge(List.of("D", "U", "D")));
+        BridgeGame bridgeGame = new BridgeGame(user);
+
+        bridgeGame.retry("R");
+
+        assertThat(bridgeGame.getTrialCount()).isEqualTo(2);
+    }
+
+    @DisplayName("재시작을 선택하지 않으면 시도 횟수가 증가하지 않는다")
+    @Test
+    void getTrialCountByQuit() {
+        User user = new User(new Bridge(List.of("D", "U", "D")));
+        BridgeGame bridgeGame = new BridgeGame(user);
+
+        bridgeGame.retry("Q");
+
+        assertThat(bridgeGame.getTrialCount()).isEqualTo(1);
+    }
+
     @DisplayName("위쪽 다리의 결과를 가져온다")
     @Test
     void getMovementsInUpBridge() {
-        Bridge bridge = new Bridge(List.of("D", "U", "D"));
-        BridgeGame bridgeGame = new BridgeGame(bridge);
+        User user = new User(new Bridge(List.of("D", "U", "D")));
+        BridgeGame bridgeGame = new BridgeGame(user);
 
         bridgeGame.move("U");
         bridgeGame.move("U");
 
-        assertThat(bridgeGame.getUpBridgeResult()).isEqualTo(List.of(" X ", " O "));
+        assertThat(bridgeGame.getUpBridgeMoveResult()).isEqualTo(List.of(BridgeBlock.FAIL_MOVED.getBlock(), BridgeBlock.SUCCESS_MOVED.getBlock()));
     }
 
     @DisplayName("아래쪽 다리의 결과를 가져온다")
     @Test
     void getMovementsInDownBridge() {
-        Bridge bridge = new Bridge(List.of("D", "U", "D"));
-        BridgeGame bridgeGame = new BridgeGame(bridge);
+        User user = new User(new Bridge(List.of("D", "U", "D")));
+        BridgeGame bridgeGame = new BridgeGame(user);
 
         bridgeGame.move("D");
         bridgeGame.move("D");
 
-        assertThat(bridgeGame.getDownBridgeResult()).isEqualTo(List.of(" O ", " X "));
+        assertThat(bridgeGame.getDownBridgeResult()).isEqualTo(List.of(BridgeBlock.SUCCESS_MOVED.getBlock(), BridgeBlock.FAIL_MOVED.getBlock()));
     }
 
     @DisplayName("마지막 순서면 true를 반환한다")
     @Test
     void isFinished() {
-        Bridge bridge = new Bridge(List.of("D", "U", "D"));
-        BridgeGame bridgeGame = new BridgeGame(bridge);
+        User user = new User(new Bridge(List.of("D", "U", "D")));
+        BridgeGame bridgeGame = new BridgeGame(user);
 
         bridgeGame.move("D");
-        bridgeGame.move("D");
+        bridgeGame.move("U");
         bridgeGame.move("D");
 
         assertThat(bridgeGame.isFinished()).isEqualTo(true);
@@ -71,11 +95,11 @@ public class BridgeGameTest {
     @DisplayName("마지막 순서가 아니면 false를 반환한다")
     @Test
     void isNotFinished() {
-        Bridge bridge = new Bridge(List.of("D", "U", "D"));
-        BridgeGame bridgeGame = new BridgeGame(bridge);
+        User user = new User(new Bridge(List.of("D", "U", "D")));
+        BridgeGame bridgeGame = new BridgeGame(user);
 
         bridgeGame.move("D");
-        bridgeGame.move("D");
+        bridgeGame.move("U");
 
         assertThat(bridgeGame.isFinished()).isEqualTo(false);
     }
