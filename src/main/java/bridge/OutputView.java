6fb1dbc15e8dce7c2d59fb45c5de44357ed27b86
sschan99 <seungchan1219@gmail.com>
2022-11-22 12:37:16 +0900

    feat(OutputView): print game clear or not and total number of try

--- src/main/java/bridge/OutputView.java
+++ src/main/java/bridge/OutputView.java
@@ -38,7 +38,6 @@ public class OutputView {
      * @param bridge 다리에서 건널 수 있는 칸
      * @param userMove 사용자가 이동한 칸
      * @param oneSide 출력하려는 쪽. 위 쪽이면 "U", 아래 쪽이면 "D"
-     * <p>
      */
     private void printOneResult(String bridge, String userMove, String oneSide) {
         if (!userMove.equals(oneSide)) {
@@ -54,9 +53,16 @@ public class OutputView {
 
     /**
      * 게임의 최종 결과를 정해진 형식에 맞춰 출력한다.
-     * <p>
-     * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
+     * @param gameClear 게임의 성공 여부
+     * @param tryNumber 게임을 시도한 횟수
      */
-    public void printResult() {
+    public void printResult(boolean gameClear, int tryNumber) {
+        if (gameClear) {
+            System.out.println("게임 성공 여부: 성공");
+            System.out.println("총 시도한 횟수: " + tryNumber);
+            return;
+        }
+        System.out.println("게임 성공 여부: 실패");
+        System.out.println("총 시도한 횟수: " + tryNumber);
     }
 }
