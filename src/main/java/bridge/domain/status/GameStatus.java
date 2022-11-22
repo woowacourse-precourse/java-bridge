073f43cdf75cb893580fed2211308fce18b82577
Song kyeong seok <skj0922@kookmin.ac.kr>
2022-11-22 16:53:32 +0900

    feat(GameStatus): 게임 상태를 유저 인풋으로 부터 객체로 바꿔 주는 메서드 구현

--- src/main/java/bridge/domain/status/GameStatus.java
+++ src/main/java/bridge/domain/status/GameStatus.java
@@ -5,13 +5,25 @@ public enum GameStatus {
     QUIT("Q"),
     PROCEED("P");
 
-    private String gameStatus;
+    private String gameStatusMessage;
 
-    GameStatus(String gameStatus) {
-        this.gameStatus = gameStatus;
+    GameStatus(String gameStatusMessage) {
+        this.gameStatusMessage = gameStatusMessage;
     }
 
     public boolean checkGameStatus(GameStatus gameStatus) {
-        return this.gameStatus.equals(gameStatus.gameStatus);
+        return this.gameStatusMessage.equals(gameStatus.gameStatusMessage);
     }
+
+    public static GameStatus convertGameStatus(String inputStatus) {
+        return java.util.Arrays.stream(GameStatus.values())
+            .filter(gameStatus -> gameStatus.compareTo(inputStatus))
+            .findFirst()
+            .orElseThrow(() -> new IllegalArgumentException());
+    }
+
+    public boolean compareTo(String inputStatus) {
+        return inputStatus.equals(this.gameStatusMessage);
+    }
+
 }
