998ee914fc1c8e137e4928dee27745877d961c45
shinjaewon99 <tlswodnjs99@naver.com>
2022-11-22 18:33:53 +0900

    feat(BridgeMaker) : 다리 생성 클래스

--- src/main/java/bridge/BridgeMaker.java
+++ src/main/java/bridge/BridgeMaker.java
@@ -2,6 +2,7 @@ package bridge;
 
 import java.util.List;
 
+
 /**
  * 다리의 길이를 입력 받아서 다리를 생성해주는 역할을 한다.
  */
@@ -9,8 +10,12 @@ public class BridgeMaker {
 
     private final BridgeNumberGenerator bridgeNumberGenerator;
 
+
+    InputView inputView = new InputView();
+
     public BridgeMaker(BridgeNumberGenerator bridgeNumberGenerator) {
         this.bridgeNumberGenerator = bridgeNumberGenerator;
+
     }
 
     /**
@@ -18,6 +23,40 @@ public class BridgeMaker {
      * @return 입력받은 길이에 해당하는 다리 모양. 위 칸이면 "U", 아래 칸이면 "D"로 표현해야 한다.
      */
     public List<String> makeBridge(int size) {
-        return null;
+
+        List<Integer> bridgeSize = inputView.readBridgeSize();
+
+        String[] Column = new String[size];
+        for (int i = 0; i < Column.length; i++) {
+            if (bridgeSize.get(i) == 1) {
+                Column[i] = " O";
+            } else if (bridgeSize.get(i) != 1) {
+                Column[i] = " X";
+            }
+        }
+        String ColumnString = String.join(" | ", Column);
+
+
+        return List.of(ColumnString);
     }
+
+    public List<String> downMakeBridge(int size) {
+
+        List<Integer> downbridgeSize = inputView.readBridgeSize();
+        String[] downColumn = new String[size];
+        for (int i = 0; i < downColumn.length; i++) {
+            if (downbridgeSize.get(i) == 0) {
+                downColumn[i] = " O";
+            } else if (downbridgeSize.get(i) != 0) {
+                downColumn[i] = " X";
+            }
+        }
+        String downColumnString = String.join(" | ", downColumn);
+
+        return List.of(downColumnString);
+    }
+
+
+
+
 }
