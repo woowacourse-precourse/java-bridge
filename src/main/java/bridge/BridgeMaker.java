335ece317472290072928a30d6177661d5e0611a
Kim Seung Beom <88074556+daily1313@users.noreply.github.com>
2022-11-22 19:15:43 +0900

    feat(BridgeGameController) : gameStart() 메서드 구현

--- src/main/java/bridge/BridgeMaker.java
+++ src/main/java/bridge/BridgeMaker.java
@@ -1,5 +1,6 @@
 package bridge;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -7,17 +8,28 @@ import java.util.List;
  */
 public class BridgeMaker {
 
+    private static final String UP = "U";
+    private static final String DOWN = "D";
+
+    private static final int DOWN_NUMBER = 0;
     private final BridgeNumberGenerator bridgeNumberGenerator;
+
 
     public BridgeMaker(BridgeNumberGenerator bridgeNumberGenerator) {
         this.bridgeNumberGenerator = bridgeNumberGenerator;
     }
 
-    /**
-     * @param size 다리의 길이
-     * @return 입력받은 길이에 해당하는 다리 모양. 위 칸이면 "U", 아래 칸이면 "D"로 표현해야 한다.
-     */
     public List<String> makeBridge(int size) {
-        return null;
+        List<String> bridge = new ArrayList<>();
+        for (int i = 0; i < size; i++) {
+            int number = bridgeNumberGenerator.generate();
+            if (number == DOWN_NUMBER) {
+                bridge.add(DOWN);
+                continue;
+            }
+            bridge.add(UP);
+        }
+        return bridge;
     }
+
 }
