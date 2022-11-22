80e9634c035c0437e0a65a2d3847050d3499342d
juno-junho <ssmm0205@naver.com>
2022-11-22 23:30:35 +0900

    test(domain): create class BridgeGameTest

---
+++ src/test/java/bridge/domain/BridgeGameTest.java
@@ -0,0 +1,119 @@
+package bridge.domain;
+
+import bridge.constant.GameCommand;
+import bridge.constant.MoveCommand;
+import bridge.constant.MoveSign;
+
+import static bridge.constant.GameCommand.QUIT;
+import static bridge.constant.GameCommand.RETRY;
+import static bridge.constant.MoveCommand.MOVE_DOWN_VALUE;
+import static bridge.constant.MoveSign.MOVE_FAIL;
+import static org.assertj.core.api.Assertions.assertThat;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import java.util.List;
+
+import static bridge.constant.MoveCommand.MOVE_UP_VALUE;
+import static bridge.constant.MoveSign.MOVE_SUCCESS;
+
+class BridgeGameTest {
+    private static final int INIT_VALUE_OF_POSITION = 0;
+    private static final int INIT_VALUE_OF_CHALLENGES = 1;
+
+    private Player player;
+    private BridgeGame bridgeGame;
+    private BridgeGameResults bridgeGameResults;
+
+    @BeforeEach
+    void setUp() {
+        Bridge bridge = new Bridge(List.of("U", "D", "U"));
+
+        player = new Player(
+                Position.of(INIT_VALUE_OF_POSITION),
+                new ChallengeNumbers(INIT_VALUE_OF_CHALLENGES));
+
+        bridgeGame = new BridgeGame(bridge, player);
+        bridgeGameResults = bridgeGame.bridgeGameResults(MOVE_UP_VALUE, MOVE_SUCCESS.getMessage());
+    }
+
+    @DisplayName("Bridge와 같은 값을 입력 받으면 O를 반환한다. (Bridge의 값: U)")
+    @Test
+    void moveResultSuccess() {
+        String moveResult = bridgeGame.bridgeGameResult(MOVE_UP_VALUE);
+        assertThat(moveResult).isEqualTo(MOVE_SUCCESS.getMessage());
+    }
+
+    @DisplayName("Bridge와 다른 값을 입력 받으면 X를 반환한다. (Bridge의 값: U)")
+    @Test
+    void moveResultFail() {
+        String moveResult = bridgeGame.bridgeGameResult(MOVE_DOWN_VALUE);
+        assertThat(moveResult).isEqualTo(MOVE_FAIL.getMessage());
+    }
+
+    @DisplayName("Bridge와 같은 값을 입력받으면 Player의 position이 1 증가한다. (Bridge의 값: U)")
+    @Test
+    void moveSuccessIncreasePosition() {
+        bridgeGame.move(MOVE_SUCCESS.getMessage());
+        int position = player.position();
+
+        assertThat(position).isEqualTo(1);
+    }
+
+    @DisplayName("Bridge와 다른 값을 입력받으면 Player의 position에 변화가 없다. (Bridge의 값: U)")
+    @Test
+    void moveFailNothingHappenedInPosition() {
+        bridgeGame.move(MOVE_FAIL.getMessage());
+        int position = player.position();
+
+        assertThat(position).isEqualTo(0);
+    }
+
+    @ParameterizedTest(name = "{0}회 retry하면 Player의 총 시도 횟수가 {1}회가 된다.")
+    @CsvSource({"1,2", "5,6", "10,11"})
+    void increaseNumberOfChallenges(int tryNum, int result) {
+        for (int i = 0; i < tryNum; i++) {
+            bridgeGame.retryOrQuit(RETRY);
+        }
+        assertThat(bridgeGame.totalNumberOfChallenges()).isEqualTo(result);
+    }
+
+    @DisplayName("GameCommands.RETRY가 입력되면 초기화가 된다.")
+    @Test
+    void retryGameInit() {
+        player.move();
+        bridgeGame.retryOrQuit(RETRY);
+
+        assertThat(player.position()).isEqualTo(INIT_VALUE_OF_POSITION);
+        assertThat(bridgeGameResults.upLineResults()).hasSize(0);
+        assertThat(bridgeGameResults.downLineResults()).hasSize(0);
+    }
+
+    @DisplayName("GameCommands.QUIT이 입력되면 초기화가 되지 않는다.")
+    @Test
+    void doNotInit() {
+        player.move();
+        bridgeGame.retryOrQuit(QUIT);
+
+        assertThat(player.position()).isEqualTo(1);
+        assertThat(bridgeGameResults.upLineResults()).hasSize(1);
+        assertThat(bridgeGameResults.downLineResults()).hasSize(1);
+    }
+
+    @DisplayName("RETRY가 입력되면 gameResult가 변경되지 않는다.")
+    @Test
+    void gameResultNoChange() {
+        bridgeGame.retryOrQuit(RETRY);
+        assertThat(bridgeGame).extracting("gameResult").isEqualTo("성공");
+    }
+
+    @DisplayName("QUIT이 입력되면 gameResult가 '실패'로 변경된다.")
+    @Test
+    void gameResultToFail() {
+        bridgeGame.retryOrQuit(QUIT);
+        assertThat(bridgeGame).extracting("gameResult").isEqualTo("실패");
+    }
+}
