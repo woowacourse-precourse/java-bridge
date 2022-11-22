817b924994d21c8b88e586e37bc976d376091939
정석준 <tjrwns1010@naver.com>
2022-11-22 09:01:56 +0900

    feat: Exception 발생 시 에러 메시지 추가

--- src/main/java/bridge/InputView.java
+++ src/main/java/bridge/InputView.java
@@ -10,15 +10,15 @@ public class InputView {
     /**
      * 다리의 길이를 입력받는다.
      */
+    // TODO: 검증로직 따로 클래스로 분리하기
     public int readBridgeSize() throws IllegalArgumentException {
         String userInput = Console.readLine();
         if (userInput == null || !userInput.matches("^[0-9]{1,2}$")) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException("[ERROR] 다리 길이는 3부터 20 사이의 숫자여야 합니다.");
         }
-
         int convertedInput = Integer.parseInt(userInput);
         if (convertedInput < 3 || convertedInput > 20) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException("[ERROR] 다리 길이는 3부터 20 사이의 숫자여야 합니다.");
         }
         return convertedInput;
     }
@@ -27,9 +27,10 @@ public class InputView {
      * 사용자가 이동할 칸을 입력받는다.
      */
     public String readMoving() throws IllegalArgumentException {
+        String ERROR_MSG = "[ERROR] 이동할 칸은 U나 D로 입력해야 합니다.";
         String userInput = Console.readLine();
         if (userInput == null || !userInput.matches("^[U,D]$")) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException(ERROR_MSG);
         }
         return userInput;
     }
@@ -38,9 +39,10 @@ public class InputView {
      * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
      */
     public String readGameCommand() {
+        String ERROR_MSG = "[ERROR] 다시 시작할 지 여부는 R이나 Q로 입력해야 합니다.";
         String userInput = Console.readLine();
         if (userInput == null | !userInput.matches("^[R,Q]$")) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException(ERROR_MSG);
         }
         return userInput;
     }
