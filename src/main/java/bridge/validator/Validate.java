781f7ce59e33893b07a5380e4d883b6e36cc7eff
josolha <josolha@nate.com>
2022-11-22 17:34:13 +0900

    fix(Validate) : 예외처리 메소드 숫자 체크, 3~20 사이값 체크 메소드로 나눔

--- src/main/java/bridge/validator/Validate.java
+++ src/main/java/bridge/validator/Validate.java
@@ -2,16 +2,13 @@ package bridge.validator;
 
 public class Validate {
 
-    public static void validateBridgeSizeNumber(String bridgeSizeInput) {
+    public static void validateBridgeCheck(String bridgeSizeInput) {
         int number = Integer.parseInt(bridgeSizeInput);
-        validateBetweenNumber(number);
-        for (int i = 0; i < bridgeSizeInput.length(); i++) {
-            if (!Character.isDigit(bridgeSizeInput.charAt(i))) {
-                throw new IllegalArgumentException();
-            }
+        if (!(number <= 20 && number >= 3)) {
+            throw new IllegalArgumentException();
         }
-
     }
+
     public static void validateCheckRetry(String command) {
         if (!command.equals("Q") && !command.equals("R")) {
             throw new IllegalArgumentException();
@@ -23,9 +20,12 @@ public class Validate {
             throw new IllegalArgumentException();
         }
     }
-    public static void validateBetweenNumber(int number) {
-        if (!(number <= 20 && number >= 3)) {
-            throw new IllegalArgumentException();
+
+    public static void validateBridgeNumber(String bridgeSizeInput) {
+        for (int i = 0; i < bridgeSizeInput.length(); i++) {
+            if (!Character.isDigit(bridgeSizeInput.charAt(i))) {
+                throw new IllegalArgumentException();
+            }
         }
     }
 }
