754cf2589fcec1c27175a58af2b907f091862ad3
rjsah09 <83952174+rjsah09@users.noreply.github.com>
2022-11-22 21:04:13 +0900

    feat: Application 클래스 구현 (게임 동작)

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -1,8 +1,48 @@
 package bridge;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Application {
 
+    private static final BridgeGame bridgeGame = new BridgeGame();
+    private static final BridgeMaker bridgeMaker = new BridgeMaker(new BridgeRandomNumberGenerator());
+    private static final InputView inputView = new InputView();
+    private static final OutputView outputView = new OutputView();
+
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        Bridge bridge = setBridge();
+        System.out.println(bridge.getFootholds().toString());
+        crossUntilEnd(bridge);
+        outputView.printResult(bridge, bridgeGame.isClear(bridge));
+    }
+
+    private static Bridge setBridge() {
+        System.out.println("다리 건너기 게임을 시작합니다.");
+        int bridgeSize = inputView.readBridgeSizeUntilConstant();
+        Bridge bridge = new Bridge(bridgeMaker.makeBridge(bridgeSize), new ArrayList<>());
+        return bridge;
+    }
+
+    private static void cross(Bridge bridge) {
+        String input = inputView.readMovingUntilConstant();
+        bridgeGame.move(bridge, input);
+        outputView.printMap(bridge);
+        if (!bridgeGame.isCorrect(bridge)) {
+            String gameCommand = inputView.readGameCommandUntilConstant();
+            if (gameCommand.equals("R")) {
+                bridgeGame.retry(bridge);
+            }
+        }
+    }
+
+
+    private static void crossUntilEnd(Bridge bridge) {
+        while (!bridgeGame.isAllCrossed(bridge)) {
+            cross(bridge);
+            if (!bridgeGame.isCorrect(bridge)) {
+                break;
+            }
+        }
     }
 }
