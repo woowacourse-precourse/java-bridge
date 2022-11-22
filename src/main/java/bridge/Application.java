fa8d211c0fafa394afed153177081b96f024abb5
seyeon ji <joeun9705@gmail.com>
2022-11-22 16:43:05 +0900

    fix: add features of Controller methods

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -1,18 +1,8 @@
 package bridge;
 
 public class Application {
-    InputView inputView = new InputView();
-
     public static void main(String[] args) {
-        InputView inputView = new InputView();
-        inputView.readBridgeSize();
-        int bridgeSize = inputView.getBridgeSize();
-        inputView.readMoving();
-        String move = inputView.getReadMoving();
-        inputView.readGameCommand();
-        String retry = inputView.getReadGameCommand();
-
-
-
+        Controller controller = new Controller();
+        controller.startGame();
     }
 }
