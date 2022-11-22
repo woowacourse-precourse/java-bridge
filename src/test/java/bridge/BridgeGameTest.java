d9c93798d24fdfb1a57869b77daf27ecab1f2315
jeongyeonKimm <wjddus9132@gmail.com>
2022-11-22 23:53:09 +0900

    test: 재시작 여부 테스트

--- src/test/java/bridge/BridgeGameTest.java
+++ src/test/java/bridge/BridgeGameTest.java
@@ -15,4 +15,13 @@ public class BridgeGameTest {
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("[ERROR] 이동할 칸은 D 또는 U로 입력해야 합니다.");
     }
+
+    @DisplayName("게임 재시작 여부가 R이나 Q가 아니면 예외를 발생한다.")
+    @ParameterizedTest
+    @ValueSource(strings = {"a", "r", "q", "RR", "RQ", "QQ", " R", "R ", " R "})
+    void ThrowExceptionIfGameCommandIsIncorrect(String gameCommand) {
+        assertThatThrownBy(() -> new BridgeGame(5).retry(gameCommand))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("[ERROR] 재시작 여부 입력은 R 또는 Q이어야 합니다.");
+    }
 }
