ee7e0798d250ff5d91739194ef1a9b6ef3bfda50
jagajang <kosb7544@gmail.com>
2022-11-22 11:51:54 +0900

    refactor: remove unused library and move streamCaptor to Test class variable

--- src/test/java/bridge/UnitTest.java
+++ src/test/java/bridge/UnitTest.java
@@ -9,11 +9,24 @@ import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
 import java.util.List;
 
-import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeTest;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.util.Lists.newArrayList;
 
 public class UnitTest {
+    private final ByteArrayOutputStream outputStreamCaptor = new ByteArrayOutputStream();
+    private final PrintStream standardOut = System.out;
+    private final OutputView outputView = new OutputView();
+
+    @BeforeEach
+    void setUp() {
+        System.setOut(new PrintStream(outputStreamCaptor));
+    }
+
+    @AfterEach
+    void tearDown() {
+        System.setOut(standardOut);
+    }
+
     static class TestNumberGenerator implements BridgeNumberGenerator {
 
         private final List<Integer> numbers;
@@ -71,20 +84,6 @@ public class UnitTest {
     @Nested
     @DisplayName("다리 출력 테스트")
     class PrintMapTest{
-        private final ByteArrayOutputStream outputStreamCaptor = new ByteArrayOutputStream();
-        private final PrintStream standardOut = System.out;
-        private final OutputView outputView = new OutputView();
-
-        @BeforeEach
-        void setUp() {
-            System.setOut(new PrintStream(outputStreamCaptor));
-        }
-
-        @AfterEach
-        void tearDown() {
-            System.setOut(standardOut);
-        }
-
         @Test
         @DisplayName("다리 중간 출력")
         void printBridgeInMiddle() {
@@ -125,20 +124,6 @@ public class UnitTest {
     @Nested
     @DisplayName("이동 테스트")
     class MoveTest {
-        private final ByteArrayOutputStream outputStreamCaptor = new ByteArrayOutputStream();
-        private final PrintStream standardOut = System.out;
-        private final OutputView outputView = new OutputView();
-
-        @BeforeEach
-        void setUp() {
-            System.setOut(new PrintStream(outputStreamCaptor));
-        }
-
-        @AfterEach
-        void tearDown() {
-            System.setOut(standardOut);
-        }
-
         @Test
         @DisplayName("이동 실패")
         void moveFail() {
@@ -159,17 +144,6 @@ public class UnitTest {
         @Test
         @DisplayName("이동 성공")
         void moveSuccess() {
-            assertRandomNumberInRangeTest(() -> {
-                run("5", "D", "D", "D", "D", "D");
-                assertThat(output()).contains(
-                        "최종 게임 결과",
-                        "[   |   |   |   |   ]",
-                        "[ O | O | O | O | O ]",
-                        "게임 성공 여부: 성공",
-                        "총 시도한 횟수: 1"
-                );
-
-            }, 0, 0, 0, 0, 0);
         }
     }
 }
