975cb640ff3d7dfaaddd1af9e6eee0fc1d914a39
Yujin830 <rladbwls30@naver.com>
2022-11-22 11:14:37 +0900

    Fix: main에서 코드 분리

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -1,5 +1,6 @@
 package bridge;
 
+import controller.GameController;
 import view.AnnouncementView;
 import view.InputView;
 import view.OutputView;
@@ -13,7 +14,7 @@ public class Application {
         try{
             AnnouncementView.start();
             int size = InputView.readBridgeSize();
-            start_game(make_bridge(size), size);
+            GameController.start_game(make_bridge(size), size);
         } catch (IllegalStateException e){
             System.out.println(e.getMessage());
         }
@@ -23,54 +24,5 @@ public class Application {
         BridgeMaker bridgeMaker = new BridgeMaker(bridgeRandomNumberGenerator);
         List<String> bridge = bridgeMaker.makeBridge(size);
         return bridge;
-    }
-    public static void start_game(List<String> bridge, int size){
-        String re="R";
-        int try_count = 1;
-        boolean success = true;
-
-        while(re.contains("R")) {
-            success = try_cross(bridge, size);
-            if(success_info(size, success)) break;
-            re = ask_retry();
-            try_count = retry_cross(re, try_count);
-        }
-
-        print_result(success, try_count);
-    }
-    public static boolean success_info(int size, boolean success){
-        if(BridgeGame.getUp_bridge().size()==size && success == true)
-            return true;
-        return false;
-    }
-    public static boolean try_cross(List<String> bridge, int size){
-        boolean success = true;
-        String move;
-
-        for (int i = 0; i < size; i++) {
-            AnnouncementView.move();
-            move = InputView.readMoving();
-            success = BridgeGame.move(bridge, move, i);
-            OutputView.printMap(BridgeGame.getUp_bridge(), BridgeGame.getDown_bridge());
-            if (success == false)
-                break;
-        }
-        return success;
-    }
-    public static String ask_retry(){
-        AnnouncementView.restart();
-        return InputView.readGameCommand();
-    }
-    public static int retry_cross(String re, int try_count){
-        if(re.contains("R")) {
-            try_count += 1;
-            BridgeGame.retry();
-        }
-        return try_count;
-    }
-    public static void print_result(boolean success, int try_count){
-        AnnouncementView.bridge_result();
-        OutputView.printMap(BridgeGame.getUp_bridge(), BridgeGame.getDown_bridge());
-        OutputView.printResult(success, try_count);
     }
 }
