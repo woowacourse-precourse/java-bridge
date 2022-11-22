a31cf21e1306ea953dee2e446352aae13f216e8a
김형기 <khk211113@naver.com>
2022-11-22 21:38:21 +0900

    docs(OutputView): 출력 주석 추가

--- src/main/java/bridge/view/OutputView.java
+++ src/main/java/bridge/view/OutputView.java
@@ -13,8 +13,8 @@ public class OutputView {
 
     /**
      * 현재까지 이동한 다리의 상태를 정해진 형식에 맞춰 출력한다.
-     * <p>
-     * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
+     * @param bridgeGame 출력할 상태가 있는 다리 게임
+     * @param bridgeTranslator 출력 방법에 대한 구현체
      */
     public void printMap(BridgeGame bridgeGame, BridgeTranslator bridgeTranslator) {
         System.out.println(bridgeGame.getPrintable(bridgeTranslator));
@@ -22,8 +22,8 @@ public class OutputView {
 
     /**
      * 게임의 최종 결과를 정해진 형식에 맞춰 출력한다.
-     * <p>
-     * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
+     * @param bridgeGame 출력할 상태가 있는 다리 게임
+     * @param bridgeTranslator 출력 방법에 대한 구현체
      */
     public void printResult(BridgeGame bridgeGame, BridgeTranslator bridgeTranslator) {
         System.out.println(PrintMessageConstant.GAME_RESULT_TITLE);
@@ -32,30 +32,57 @@ public class OutputView {
         System.out.println(String.format(PrintMessageConstant.GAME_RESULT_TRIAL_COUNT_FORMAT, bridgeGame.getTrialCount()));
     }
 
+    /**
+     * 게임 시작을 알리는 문구를 출력한다.
+     */
     public void printGameStart() {
         System.out.println(PrintMessageConstant.START_GAME);
     }
 
+    /**
+     * 다리의 길이를 입력하라고 알리는 문구를 출력한다.
+     */
     public void printEnterBridgeLength() {
         System.out.println(PrintMessageConstant.PLEASE_ENTER_BRIDGE_LENGTH);
     }
 
+    /**
+     * 이동할 방향을 입력하라고 알리는 문구를 출력한다.
+     */
     public void printEnterMoveDirection() {
         System.out.println(PrintMessageConstant.PLEASE_ENTER_MOVE_DIRECTION);
     }
 
+    /**
+     * 재시작 여부를 입력하라고 알리는 문구를 출력한다.
+     */
     public void printEnterGameRetry() {
         System.out.println(PrintMessageConstant.PLEASE_ENTER_GAME_RETRY);
     }
 
+    /**
+     * 예측된 예외를 사용자에게 출력한다.
+     * @param e 사용자의 잘못된 입력으로 발생한 예측된 예외
+     */
     public void printExpectedErrorMessage(IllegalArgumentException e) {
         System.out.println(String.format(PrintMessageConstant.ERROR_EXPECTED_FORMAT, e.getMessage()));
     }
 
+    /**
+     * 예측치 못한 예외를 사용자에게 출력한다.
+     * @param e 예측되지 못한 예외
+     */
     public void printUnexpectedErrorMessage(Exception e) {
         System.out.println(String.format(PrintMessageConstant.ERROR_UNEXPECTED_FORMAT, e.getClass(), e.getCause(), e.getMessage()));
     }
 
+    /**
+     * 게임의 성공 또는 실패 여부를 형식에 맞게 출력한다.
+     * @param gameStatus 게임 상태
+     * @return 성공 / 실패 문자열
+     * @throws IllegalArgumentException 게임이 RUNNING 상태일 때 호출 시 예외 발생
+     * @see GameStatus
+     */
     private static String getSuccessOrNot(GameStatus gameStatus) throws IllegalArgumentException {
         if (gameStatus == GameStatus.WIN) {
             return PrintMessageConstant.GAME_RESULT_SUCCESS;
