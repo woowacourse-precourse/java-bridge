af9a8ab33aac98b95bbd41419f0be49bf2422232
hyeonseog-won <smreo75@gmail.com>
2022-11-22 21:29:31 +0900

    feat: Map을 game에서 불러올 수 있게 기능 추가

--- src/main/java/bridge/model/BridgeGame.java
+++ src/main/java/bridge/model/BridgeGame.java
@@ -6,18 +6,22 @@ import java.util.List;
  * 다리 건너기 게임을 관리하는 클래스
  */
 public class BridgeGame {
+    private final BridgeMaker bridgeMaker;
+    private Bridge bridge;
+    private BridgeResult bridgeResult;
     private int tryCount = 1;
+
+    public BridgeGame(BridgeMaker bridgeMaker) {
+        this.bridgeMaker = bridgeMaker;
+    }
 
     /**
      * 사용자가 칸을 이동할 때 사용하는 메서드
      * <p>
      * 이동을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public boolean move(List<String> bridge, String userMovingInput, int index) {
-        if (bridge.get(index).equals(userMovingInput)){
-            return true;
-        }
-        return false;
+    public void move(String userMovingCommand) {
+        ResultFlag move = bridgeResult.move(userMovingCommand);
     }
 
     /**
@@ -31,5 +35,9 @@ public class BridgeGame {
 
     public int getTryCount() {
         return tryCount;
+    }
+
+    public BridgeResult getMap() {
+        return bridgeResult;
     }
 }
