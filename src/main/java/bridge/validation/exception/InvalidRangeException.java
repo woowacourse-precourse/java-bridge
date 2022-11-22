9659ef43e75c862cebf7e395318f9423e172d784
SeongbinPark <binni0515@gmail.com>
2022-11-22 20:11:47 +0900

    refactor(InvalidRangeException): change to constants

--- src/main/java/bridge/validation/exception/InvalidRangeException.java
+++ src/main/java/bridge/validation/exception/InvalidRangeException.java
@@ -1,14 +1,16 @@
 package bridge.validation.exception;
 
+import static bridge.Bridge.MAX_BRIDGE_SIZE;
+import static bridge.Bridge.MIN_BRIDGE_SIZE;
 import static bridge.validation.Message.ERROR_MESSAGE;
 import static bridge.validation.Message.OR_LESS;
 import static bridge.validation.Message.OR_OVER;
 
 public class InvalidRangeException extends IllegalArgumentException {
     public InvalidRangeException() {
-        super(ERROR_MESSAGE.getMessage() + 3
+        super(ERROR_MESSAGE.getMessage() + MIN_BRIDGE_SIZE
                 + OR_OVER.getMessage()
-                + 20
+                + MAX_BRIDGE_SIZE
                 + OR_LESS.getMessage());
     }
 }
