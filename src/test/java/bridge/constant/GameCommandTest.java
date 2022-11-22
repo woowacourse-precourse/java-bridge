96e59e93829623bdcb14e6aedf5ed945dd90ba3e
0chil <0@chll.it>
2022-11-22 22:21:19 +0900

    test(GameCommand): 잘못된 명령 문자열은 예외를 던진다

--- src/test/java/bridge/constant/GameCommandTest.java
+++ src/test/java/bridge/constant/GameCommandTest.java
@@ -1,7 +1,9 @@
 package bridge.constant;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
 
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
@@ -12,4 +14,12 @@ class GameCommandTest {
     void 문자열로_명령을_찾을_수_있다(String character, GameCommand expected) {
         assertThat(GameCommand.from(character)).isEqualTo(expected);
     }
+
+    @Test
+    void 잘못된_명령_문자열은_예외를_던진다() {
+        String anomalyCommand = "XX";
+
+        assertThatIllegalArgumentException().isThrownBy(() -> GameCommand.from(anomalyCommand));
+    }
+
 }
