7435a636bd226ef2089171d37436c22de60f8249
juno-junho <ssmm0205@naver.com>
2022-11-22 23:31:32 +0900

    test: create class BridgeMakerTest

---
+++ src/test/java/bridge/BridgeMakerTest.java
@@ -0,0 +1,22 @@
+package bridge;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+class BridgeMakerTest {
+
+    @ParameterizedTest(name = "{0}을 입력시 길이가 {0}인 List<String>을 반환한다.")
+    @ValueSource(ints = {1, 5, 15})
+    void createBridgeSuccess(int size) {
+        BridgeMaker bridgeMaker = new BridgeMaker(new BridgeRandomNumberGenerator());
+        List<String> stringList = bridgeMaker.makeBridge(size);
+
+        assertThat(stringList).hasSize(size);
+    }
+
+}
