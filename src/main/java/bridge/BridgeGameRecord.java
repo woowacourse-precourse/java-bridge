0d53b1c393e0ad67692c7fe95f0f0c49ed6d9628
jay <jaewhan232@naver.com>
2022-11-22 11:11:16 +0900

    Implement : 경기 기록에 해당하는 BridgeGameRecord 클래스 선언 및 getRecord() 메소드 구현

---
+++ src/main/java/bridge/BridgeGameRecord.java
@@ -0,0 +1,13 @@
+package bridge;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class BridgeGameRecord {
+
+    private List<String> record = new ArrayList<>();
+
+    public List<String> getRecord() {
+        return record;
+    }
+}
