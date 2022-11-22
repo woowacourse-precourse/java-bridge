6630e2958f185068d94d9db257f6b24ce8f16b2b
Jae Ung Jung <roosa0210@gmail.com>
2022-11-22 21:03:07 +0900

    [feat] BridgeGame.move 사용자가 칸을 이동할 때 사용하는 메서드 업데이트
    사용자가 현재 위치한 값을 나타내는 position_map을 입력한 moving값과 bridge 상태에 따라 이동하여 반환

--- src/main/java/bridge/BridgeGame.java
+++ src/main/java/bridge/BridgeGame.java
@@ -1,5 +1,9 @@
 package bridge;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 /**
  * 다리 건너기 게임을 관리하는 클래스
  */
@@ -10,7 +14,17 @@ public class BridgeGame {
      * <p>
      * 이동을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void move() {
+    public List<Integer> move(List<String> bridge, List<Integer> position_map, String moving) {
+        int position = Arrays.asList(position_map).indexOf(1);  //i 번째에 1의 값이 있으면 i번째 칸에 위치, 0이면 출발안함
+        if(moving.equals(bridge.get(position))) {
+            position_map.set(position,0);
+            position_map.set(position+1,1);
+        }
+        if(!moving.equals(bridge.get(position))) {
+            position_map.set(position,0);
+            position_map.set(0,1);
+        }
+        return position_map;
     }
 
     /**
