3d5094b36016dc6960f0944ab49c9bf4189b16e2
Dasol Jung <greteljds0251@gmail.com>
2022-11-22 23:43:26 +0900

    feat: 메인 메서드

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -7,6 +7,9 @@ public class Application {
 
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        Bridge bridge = new Bridge();
+        List<String> bridgeSize = bridge.gameStart();
+        bridge.gamePlay(bridgeSize);
     }
 }
 
