507b0b15ede7741b69e25e80cf574faa35c2c621
Jeeseob <jeeseob5761@gmail.com>
2022-11-22 16:05:50 +0900

    feat: 게임 진행 상황을 출력하기 위한 함수 추가

--- src/main/java/bridge/service/MovedBridgeService.java
+++ src/main/java/bridge/service/MovedBridgeService.java
@@ -47,4 +47,45 @@ public class MovedBridgeService {
         MovedBridge.getInstance().reset();
         gameStatusService.find().addAttemptCount();
     }
+
+    // TODO: 10줄 이내, depth 3이내로 수정 필요
+    public String printMap() {
+        List<String> bridgeAnswer = bridgeService.find().getBridge();
+        List<String> moveBridge = this.find().getBridge();
+
+        StringBuilder upBridge = new StringBuilder(START);
+        StringBuilder downBridge = new StringBuilder(START);
+
+        for (int i = 0; i < moveBridge.size() - 1; i++) {
+            if (moveBridge.get(i).equals(bridgeAnswer.get(i))) {
+                if (moveBridge.get(i).equals(Command.MOVE_UPPER_LETTER.getCommand())) {
+                    upBridge.append(CORRECT + DIVISION);
+                    downBridge.append(NONE + DIVISION);
+                } else if (moveBridge.get(i).equals(Command.MOVE_LOWER_LETTER.getCommand())) {
+                    upBridge.append(NONE + DIVISION);
+                    downBridge.append(CORRECT + DIVISION);
+                }
+            }
+        }
+
+        int lastIndex = moveBridge.size() - 1;
+        if (moveBridge.get(lastIndex).equals(bridgeAnswer.get(lastIndex))) {
+            if (moveBridge.get(lastIndex).equals(Command.MOVE_UPPER_LETTER.getCommand())) {
+                upBridge.append(CORRECT + END);
+                downBridge.append(NONE + END);
+            } else if (moveBridge.get(lastIndex).equals(Command.MOVE_LOWER_LETTER.getCommand())) {
+                upBridge.append(NONE + END);
+                downBridge.append(CORRECT + END);
+            }
+        } else {
+            if (moveBridge.get(lastIndex).equals(Command.MOVE_UPPER_LETTER.getCommand())) {
+                upBridge.append(WRONG + END);
+                downBridge.append(NONE + END);
+            } else if (moveBridge.get(lastIndex).equals(Command.MOVE_LOWER_LETTER.getCommand())) {
+                upBridge.append(NONE + END);
+                downBridge.append(WRONG + END);
+            }
+        }
+        return upBridge.toString() + NEW_LINE + downBridge.toString() + NEW_LINE;
+    }
 }
