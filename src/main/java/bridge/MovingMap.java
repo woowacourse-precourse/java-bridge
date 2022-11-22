1b8af726c52f11a11b8016ba16846eeaa7cdb7e0
ddukddaki <rlfrkdms1@naver.com>
2022-11-22 15:43:06 +0900

    feat(MovingMap): 성공이라면 O, 실패라면 X, 선택하지 않은 다리라면 공백을 반환하는 기능

--- src/main/java/bridge/MovingMap.java
+++ src/main/java/bridge/MovingMap.java
@@ -7,11 +7,25 @@ import java.util.Map;
 
 public class MovingMap {
 
+    private static final String CORRECT = " O ";
+    private static final String FAIL = " X ";
+    private static final String NOT_SELECTED = "   ";
+
     private Map<String, List<String>> movingMap;
 
     public void init() {
         movingMap = new LinkedHashMap<>();
         movingMap.put(UpAndDown.UP.getCommand(), new ArrayList<>());
         movingMap.put(UpAndDown.DOWN.getCommand(), new ArrayList<>());
+    }
+
+    public String judgeStatus(String targetUpAndDown, String moving, boolean moveStatus) {
+        if (moveStatus && targetUpAndDown.equals(moving)) { //
+            return CORRECT;
+        }
+        if (!moveStatus && targetUpAndDown.equals(moving)) {
+            return FAIL;
+        }
+        return NOT_SELECTED;
     }
 }
