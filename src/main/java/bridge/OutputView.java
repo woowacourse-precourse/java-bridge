7cb5c3423d1de785b32c0c74d0a73f33188871bc
lacram <chemin9898@gmail.com>
2022-11-22 21:06:21 +0900

    refactor: 불필요하게 public으로 선언되어 있는 함수 private하게 변경

--- src/main/java/bridge/OutputView.java
+++ src/main/java/bridge/OutputView.java
@@ -12,13 +12,13 @@ public class OutputView {
      * <p>
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public static void printMap(List<String> bridge, List<String> moved) {
+    public void printMap(List<String> bridge, List<String> moved) {
         printLine(bridge, moved, "U");
         printLine(bridge, moved, "D");
         System.out.println();
     }
 
-    private static void printLine(List<String> bridge, List<String> moved, String updown) {
+    private void printLine(List<String> bridge, List<String> moved, String updown) {
         StringBuilder stringBuilder = new StringBuilder();
         stringBuilder.append("[");
         for (int i = 0; i < moved.size(); i++) {
@@ -31,7 +31,7 @@ public class OutputView {
         System.out.println(stringBuilder);
     }
 
-    public static StringBuilder getMovingResult(String bridge, String moving, String updown) {
+    private StringBuilder getMovingResult(String bridge, String moving, String updown) {
         StringBuilder stringBuilder = new StringBuilder();
         if (moving.equals(updown)) {
             stringBuilder.append(" ");
@@ -42,7 +42,7 @@ public class OutputView {
         return new StringBuilder("   ");
     }
 
-    public static String isSame(String bridge, String moving) {
+    private String isSame(String bridge, String moving) {
         if (bridge.equals(moving)) {
             return "O";
         }
@@ -54,14 +54,15 @@ public class OutputView {
      * <p>
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public static void printResult(List<String> bridge, List<String> moved, boolean isSuccess, int tryCount) {
+    public void printResult(List<String> bridge, List<String> moved, int tryCount) {
+        BridgeGame bridgeGame = new BridgeGame();
         System.out.println("최종 게임 결과");
         printMap(bridge, moved);
-        printIsSuccess(isSuccess);
+        printIsSuccess(bridgeGame.isSucceeded(bridge, moved));
         printTryCount(tryCount);
     }
 
-    public static void printIsSuccess(boolean isSuccess) {
+    private void printIsSuccess(boolean isSuccess) {
         if (isSuccess) {
             System.out.println("게임 성공 여부: 성공");
             return;
@@ -69,7 +70,7 @@ public class OutputView {
         System.out.println("게임 성공 여부: 실패");
     }
 
-    public static void printTryCount(int tryCount) {
+    private void printTryCount(int tryCount) {
         System.out.println("총 시도한 횟수: " + tryCount);
     }
 }
