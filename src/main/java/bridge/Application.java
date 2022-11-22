3125a1b3507c5936a02b5394acb59bab7549d12f
kiarakim <kiarakim413@gmail.com>
2022-11-22 16:30:06 +0900

    feat(bridge) : make new object to start game

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -3,7 +3,7 @@ package bridge;
 public class Application {
 
     public static void main(String[] args) {
-        BridgeController bridgeController = new BridgeController();
-        bridgeController.start();
+        BridgeGame game = new BridgeGame();
+        game.start();
     }
 }
