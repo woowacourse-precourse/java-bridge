13ad82b94f472a1b74e10f3097657fcf49253ddf
juhee park <jouhi3322@gmail.com>
2022-11-22 16:56:28 +0900

    refactor : 함수 분리

--- src/main/java/bridge/domain/BridgeGame.java
+++ src/main/java/bridge/domain/BridgeGame.java
@@ -13,27 +13,15 @@ public class BridgeGame {
 
     public BridgeGame(List<String> bridge) {
         this.bridge = bridge;
-        challengesCnt = 0;
-        retry();
+        challengesCnt = 1;
+        initialization();
     }
 
-    /**
-     * 사용자가 칸을 이동할 때 사용하는 메서드
-     * <p>
-     * 이동을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
-     */
     public void move(String userWantedDirection) {
         if (checkingMoving(userWantedDirection)) {
             checkingEnding();
         }
         nowLocation++;
-    }
-
-    //방금 이동한 칸이 마지막 칸인경우 게임을 종료한다.
-    private void checkingEnding() {
-        if (nowLocation == bridge.size() - 1) {
-            status = Status.ENDING;
-        }
     }
 
     //다음칸으로 이동이 가능한지 확인한다.(이동이 불가능 하다면 게임상황을 FAIL 로 변경한다)
@@ -45,13 +33,19 @@ public class BridgeGame {
         return false;
     }
 
-    /**
-     * 사용자가 게임을 다시 시도할 때 사용하는 메서드
-     * <p>
-     * 재시작을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
-     */
+    //방금 이동한 칸이 마지막 칸인경우 게임의 상태를 종료로 변경한다.
+    private void checkingEnding() {
+        if (nowLocation == bridge.size() - 1) {
+            status = Status.ENDING;
+        }
+    }
+
     public void retry() {
         challengesCnt++;
+        initialization();
+    }
+
+    private void initialization() {
         nowLocation = 0;
         status = Status.PLAYING;
     }
