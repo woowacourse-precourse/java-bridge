d222acf522fd3e49da50c972389c5903290d7ada
이규승 <rpg8988@naver.com>
2022-11-22 22:50:11 +0900

    feat: 사용자로부터 입력받는 부분

--- src/main/java/bridge/InputView.java
+++ src/main/java/bridge/InputView.java
@@ -1,28 +1,20 @@
 package bridge;
 
-/**
- * 사용자로부터 입력을 받는 역할을 한다.
- */
+import bridge.EnumClass.EnumConstant;
+import bridge.EnumClass.Validator;
+import camp.nextstep.edu.missionutils.Console;
+
 public class InputView {
 
-    /**
-     * 다리의 길이를 입력받는다.
-     */
-    public int readBridgeSize() {
-        return 0;
+    public static int readBridgeSize() {
+        return Validator.validateBridgeInput(Console.readLine());
     }
 
-    /**
-     * 사용자가 이동할 칸을 입력받는다.
-     */
-    public String readMoving() {
-        return null;
+    public static String readMoving() {
+        return Validator.validateCommand(Console.readLine(), EnumConstant.State_Move);
     }
 
-    /**
-     * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
-     */
-    public String readGameCommand() {
-        return null;
+    public static String readGameCommand() {
+        return Validator.validateCommand(Console.readLine(), EnumConstant.State_Retry);
     }
 }
