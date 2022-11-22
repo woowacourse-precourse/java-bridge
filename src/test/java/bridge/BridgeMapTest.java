d4a05c3852355423bdf6356db02f0d034d696116
Hyeonjun Park <phjppo0918@gmail.com>
2022-11-22 13:11:59 +0900

    test(BridgeMapTest) : 이동하다가 실패시 표기되는 지도 비교

--- src/test/java/bridge/BridgeMapTest.java
+++ src/test/java/bridge/BridgeMapTest.java
@@ -36,4 +36,40 @@ class BridgeMapTest {
         //then
         assertThat(result).isEqualTo(expect);
     }
+
+    @Test
+    @DisplayName("위 아래로 번갈아가며 2번 씩 이동 후 마지막 위로 이동 실패시")
+    void upDownRepeat2timeAndFailMoveUp() throws Exception {
+        //given
+        BridgeMap bridgeMap = new BridgeMap();
+        String expect = "[ O |   | O |   | X ]\n" +
+                        "[   | O |   | O |   ]\n";
+        //when
+        bridgeMap.moveSuccess(Command.UP);
+        bridgeMap.moveSuccess(Command.DOWN);
+        bridgeMap.moveSuccess(Command.UP);
+        bridgeMap.moveSuccess(Command.DOWN);
+        bridgeMap.moveFail(Command.UP);
+        String result = bridgeMap.toString();
+        //then
+        assertThat(result).isEqualTo(expect);
+    }
+
+    @Test
+    @DisplayName("위 아래로 번갈아가며 2번 씩 이동 후 마지막 아래로 이동 실패시")
+    void upDownRepeat2timeAndFailMoveDown() throws Exception {
+        //given
+        BridgeMap bridgeMap = new BridgeMap();
+        String expect = "[ O |   | O |   |   ]\n" +
+                        "[   | O |   | O | X ]\n";
+        //when
+        bridgeMap.moveSuccess(Command.UP);
+        bridgeMap.moveSuccess(Command.DOWN);
+        bridgeMap.moveSuccess(Command.UP);
+        bridgeMap.moveSuccess(Command.DOWN);
+        bridgeMap.moveFail(Command.DOWN);
+        String result = bridgeMap.toString();
+        //then
+        assertThat(result).isEqualTo(expect);
+    }
 }
