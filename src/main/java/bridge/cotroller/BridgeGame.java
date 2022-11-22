069816c4360624848bfbd9de391637edf3bd57fe
TeddyYeoni <po99027@naver.com>
2022-11-22 20:52:20 +0900

    feat: 게임 진행 메서드 추가

--- src/main/java/bridge/cotroller/BridgeGame.java
+++ src/main/java/bridge/cotroller/BridgeGame.java
@@ -4,7 +4,11 @@ package bridge.cotroller;
  * 다리 건너기 게임을 관리하는 클래스
  */
 public class BridgeGame {
-
+	
+	public void gameStart() {
+		
+	}
+	
     /**
      * 사용자가 칸을 이동할 때 사용하는 메서드
      * <p>
@@ -19,5 +23,8 @@ public class BridgeGame {
      * 재시작을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
     public void retry() {
+    }
+    
+    public void gameExit() {
     }
 }
