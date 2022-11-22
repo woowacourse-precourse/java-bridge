f6adaed23aa5519d904c621a4ab2b884eb5280ab
Hongsugyeong <olivia258@naver.com>
2022-11-22 19:21:50 +0900

    (Test) : BridgeGame 단위테스트 수행

---
+++ src/test/java/bridge/BrideGameTest.java
@@ -0,0 +1,47 @@
+package bridge;
+
+import bridge.domain.BridgeGame;
+import org.junit.jupiter.api.Test;
+
+import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class BrideGameTest {
+
+    @Test
+    void 이동_성공_테스트() {
+        BridgeGame bridgeGame = new BridgeGame();
+        assertSimpleTest(() -> {
+            String square = "U";
+            String choice = "U";
+            bridgeGame.move(square,choice);
+            //System.out.println(bridgeGame.toString());
+            assertThat(bridgeGame.toString()).contains(
+                    "[ O ]",
+                    "[   ]"
+            );
+
+            int upSideIndex = bridgeGame.toString().indexOf("[ O ]");
+            int downSideIndex = bridgeGame.toString().indexOf("[   ]");
+            assertThat(upSideIndex).isLessThan(downSideIndex);
+        });
+    }
+
+    @Test
+    void 재시도_횟수_증가_테스트() {
+        BridgeGame bridgeGame = new BridgeGame();
+        String reply ="R";
+        int attempt = 1;
+        attempt = bridgeGame.retry(reply,attempt);
+        assertThat(attempt).isEqualTo(2);
+    }
+
+    @Test
+    void 종료시_시도횟수_테스트(){
+        BridgeGame bridgeGame = new BridgeGame();
+        String reply ="Q";
+        int attempt = 1;
+        attempt = bridgeGame.retry(reply,attempt);
+        assertThat(attempt).isEqualTo(1);
+    }
+}
