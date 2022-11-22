84ee691765c9cff89a265d4e8a2f57ad2ac472c5
This2sho <zx00018@naver.com>
2022-11-22 17:12:50 +0900

    refactor(BridgeMaker): 다리 만드는 기능 코드 간결화

--- src/main/java/bridge/BridgeMaker.java
+++ src/main/java/bridge/BridgeMaker.java
@@ -2,34 +2,29 @@ package bridge;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 /**
  * 다리의 길이를 입력 받아서 다리를 생성해주는 역할을 한다.
  */
 public class BridgeMaker {
 
-    private final BridgeNumberGenerator bridgeNumberGenerator;
+	private final BridgeNumberGenerator bridgeNumberGenerator;
 
-    public BridgeMaker(BridgeNumberGenerator bridgeNumberGenerator) {
-        this.bridgeNumberGenerator = bridgeNumberGenerator;
-    }
+	public BridgeMaker(BridgeNumberGenerator bridgeNumberGenerator) {
+		this.bridgeNumberGenerator = bridgeNumberGenerator;
+	}
 
-    /**
-     * @param size 다리의 길이
-     * @return 입력받은 길이에 해당하는 다리 모양. 위 칸이면 "U", 아래 칸이면 "D"로 표현해야 한다.
-     */
-    public List<String> makeBridge(int size) {
-        List<String> result = new ArrayList<>();
-        for (int i = 0; i < size; i++) {
-            result.add(getUpOrDown());
-        }
-        return result;
-    }
-
-    private String getUpOrDown() {
-        if (bridgeNumberGenerator.generate() == 0) {
-            return Constants.DOWN;
-        }
-        return Constants.UP;
-    }
+	/**
+	 * @param size 다리의 길이
+	 * @return 입력받은 길이에 해당하는 다리 모양. 위 칸이면 "U", 아래 칸이면 "D"로 표현해야 한다.
+	 */
+	public List<String> makeBridge(int size) {
+		List<String> result = new ArrayList<>();
+		Map<Integer, String> mapToMoving = Map.of(0, Constants.DOWN, 1, Constants.UP);
+		for (int i = 0; i < size; i++) {
+			result.add(mapToMoving.get(bridgeNumberGenerator.generate()));
+		}
+		return result;
+	}
 }
