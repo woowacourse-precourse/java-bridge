47a52af0cceff229cbbc11757abe23f35e0c930c
sschan99 <seungchan1219@gmail.com>
2022-11-22 10:33:16 +0900

    feat(InputView): read the size of bridge

--- src/main/java/bridge/InputView.java
+++ src/main/java/bridge/InputView.java
@@ -1,5 +1,9 @@
 package bridge;
 
+import camp.nextstep.edu.missionutils.Console;
+
+import java.util.regex.Pattern;
+
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
  */
@@ -9,7 +13,9 @@ public class InputView {
      * 다리의 길이를 입력받는다.
      */
     public int readBridgeSize() {
-        return 0;
+        String bridgeSize = Console.readLine();
+        validateBridgeSize(bridgeSize);
+        return Integer.parseInt(bridgeSize);
     }
 
     /**
@@ -24,5 +30,18 @@ public class InputView {
      */
     public String readGameCommand() {
         return null;
+    }
+
+    /**
+     * 다리의 길이가 올바르게 입력되었는지 검사한다.
+     */
+    private void validateBridgeSize(String bridgeSize) {
+        if (Pattern.matches("^[0-9]+$", bridgeSize)) {
+            throw new IllegalArgumentException("[ERROR] 숫자만 입력하십시오.");
+        }
+        int number = Integer.parseInt(bridgeSize);
+        if (number < 3 || number > 20) {
+            throw new IllegalArgumentException("[ERROR] 3 이상 20 이하의 값을 입력하십시오.");
+        }
     }
 }
