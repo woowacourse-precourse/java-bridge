6aaad29c356fe8867a762bf4a185de9071a2f1e3
KimSunU <kgw6147@naver.com>
2022-11-22 23:11:12 +0900

    [feat] Input을 위한 구현

--- src/main/java/bridge/InputView.java
+++ src/main/java/bridge/InputView.java
@@ -11,6 +11,11 @@ public class InputView {
 
     private static final int MIN_BRIDGE_SIZE = 3;
     private static final int MAX_BRIDGE_SIZE = 20;
+    private static final int MOVE_STRING_LENGTH = 1;
+    private static final String MOVE_BRIDGE_UP = "U";
+    private static final String MOVE_BRIDGE_DOWN = "D";
+    private static final String GAME_QUIT = "Q";
+    private static final String GAME_RESTART = "R";
     private static final String ERROR_BRIDGE_SIZE = "다리의 길이는 3-20 사이 입니다.";
     private static final String ERROR_STRING_LENGTH = "칸의 입력은 한글자만 가능합니다.";
     private static final String ERROR_MOVE_RANGE = "칸의 입력은 U,D 둘 중 하나만 가능합니다.";
@@ -18,10 +23,12 @@ public class InputView {
     private static final String INPUT_BRIDGE_COMMENT = "다리의 길이를 입력해주세요.";
     private static final String INPUT_MOVE_COMMENT = "이동할 칸을 선택해주세요. (위: U, 아래: D)";
     private static final String INPUT_RESTART_COMMENT = "게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)";
+
+
     /**
      * 다리의 길이를 입력받는다.
      */
-    public int readBridgeSize() {
+    public static int readBridgeSize() {
         System.out.println(INPUT_BRIDGE_COMMENT);
         String inputBridge = Console.readLine();
 
@@ -32,7 +39,7 @@ public class InputView {
     /**
      * 사용자가 이동할 칸을 입력받는다.
      */
-    public String readMoving() {
+    public static String readMoving() {
         System.out.println(INPUT_MOVE_COMMENT);
         String inputMove = Console.readLine();
 
@@ -44,36 +51,36 @@ public class InputView {
     /**
      * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
      */
-    public String readGameCommand() {
+    public static String readGameCommand() {
         System.out.println(INPUT_RESTART_COMMENT);
         String inputRestart = Console.readLine();
 
         validateLength(inputRestart);
         validateRestart(inputRestart);
-        return null;
+        return inputRestart;
     }
 
-    private void validateBridgeSize(int inputBridge){
+    private static void validateBridgeSize(int inputBridge){
         if(inputBridge<MIN_BRIDGE_SIZE ||
                 inputBridge >MAX_BRIDGE_SIZE){
             throw new IllegalArgumentException(ERROR_BRIDGE_SIZE);
         }
     }
 
-    private void validateMove(String inputMove){
-        if(inputMove != "U" && inputMove !="D"){
+    private static void validateMove(String inputMove){
+        if(inputMove != MOVE_BRIDGE_UP && inputMove != MOVE_BRIDGE_DOWN){
             throw new IllegalArgumentException(ERROR_MOVE_RANGE);
         }
     }
 
-    private void validateRestart(String inputRestart){
-        if(inputRestart != "R" && inputRestart !="Q"){
+    private static void validateRestart(String inputRestart){
+        if(inputRestart != GAME_RESTART && inputRestart != GAME_QUIT){
             throw new IllegalArgumentException(ERROR_RESTART_RANGE);
         }
     }
 
-    private void validateLength(String inputString){
-        if(inputString.length() != 1){
+    private static void validateLength(String inputString){
+        if(inputString.length() != MOVE_STRING_LENGTH){
             throw new IllegalArgumentException(ERROR_STRING_LENGTH);
         }
     }
