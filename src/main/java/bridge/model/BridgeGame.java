abde3a3c4b9ad62a1428760e13824c4766f2b89a
hyeonseog-won <smreo75@gmail.com>
2022-11-22 18:44:34 +0900

    refactor: 패키지 이동

--- src/main/java/bridge/model/BridgeGame.java
+++ src/main/java/bridge/model/BridgeGame.java
@@ -1,4 +1,6 @@
-package bridge;
+package bridge.model;
+
+import java.util.List;
 
 /**
  * 다리 건너기 게임을 관리하는 클래스
@@ -11,7 +13,11 @@ public class BridgeGame {
      * <p>
      * 이동을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void move() {
+    public boolean move(List<String> bridge, String userMovingInput, int index) {
+        if (bridge.get(index).equals(userMovingInput)){
+            return true;
+        }
+        return false;
     }
 
     /**
@@ -19,7 +25,7 @@ public class BridgeGame {
      * <p>
      * 재시작을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void retry() {
+    public void retry(String gameCommand) {
         tryCount++;
     }
 
