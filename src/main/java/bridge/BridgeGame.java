5b55f1d789d1cffd775f8d91a2b7449cfc7f3e29
p <gustjr2332@naver.com>
2022-11-22 19:48:51 +0900

    feat(BridgeGame) : 사용자가 칸을 이동할 때 결과를 확인하고 저장소에 넘겨주는 기능 구현

--- src/main/java/bridge/BridgeGame.java
+++ src/main/java/bridge/BridgeGame.java
@@ -1,12 +1,14 @@
 package bridge;
 
 import java.util.List;
+import java.util.Map;
 
 /**
  * 다리 건너기 게임을 관리하는 클래스
  */
 public class BridgeGame {
 
+    private JudgeMovingResult judgeMovingResult = new JudgeMovingResult();
     private ResultRepository resultRepositiory;
     private List<String> bridge;
     private int numberOfAttempt;
@@ -22,7 +24,13 @@ public class BridgeGame {
      * <p>
      * 이동을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void move() {
+    public String move(int numberOfMoving, String moving) {
+        String movingResult = judgeMovingResult.isMovingPossible(moving, bridge.get(numberOfMoving-1));
+
+        Map<String, String> movingRecord = judgeMovingResult.recordMoving(moving, movingResult);
+        resultRepositiory.saveCumulativeResult(movingRecord, numberOfMoving);
+
+        return movingResult;
     }
 
     /**
