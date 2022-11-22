9c2e41cfcc568902553495d2e76a5391258a31a3
lee <whddlsk123@naver.com>
2022-11-22 17:31:54 +0900

    feat(Application) : 주석제거

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -5,7 +5,6 @@ import bridge.controller.BridgeController;
 
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
         BridgeConfig bridgeConfig = new BridgeConfig();
 
         BridgeController bridgeController = bridgeConfig.bridgeController();
