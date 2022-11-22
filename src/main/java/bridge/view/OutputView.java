4e9800d1fbec83b4402e6882da66588e9cd6511a
Cyma-s <ssol0319@naver.com>
2022-11-22 09:56:47 +0900

    feat(OutputView) : add printResult logic to output the final game result

--- src/main/java/bridge/view/OutputView.java
+++ src/main/java/bridge/view/OutputView.java
@@ -1,6 +1,7 @@
 package bridge.view;
 
 import bridge.domain.BridgeState;
+import bridge.domain.FinalGameResult;
 import bridge.domain.GameResult;
 import bridge.domain.MoveResult;
 import java.util.List;
@@ -14,6 +15,9 @@ public class OutputView {
     private static final String BRIDGE_PARTITION = " | ";
     private static final String BRIDGE_BLANK = " ";
     private static final String START_STRING = "다리 건너기 게임을 시작합니다.";
+    private static final String LAST_GAME_RESULT_STRING = "최종 게임 결과";
+    private static final String WHETHER_SUCCESS_OR_NOT_STRING = "게임 성공 여부: ";
+    private static final String TOTAL_TRY_COUNT_STRING = "총 시도한 횟수: ";
 
     public void printStart() {
         System.out.println(START_STRING);
@@ -44,7 +48,7 @@ public class OutputView {
             System.out.printf(MoveResult.SAME.getResult() + BRIDGE_PARTITION);
             return;
         }
-        System.out.printf(BRIDGE_BLANK+BRIDGE_PARTITION);
+        System.out.printf(BRIDGE_BLANK + BRIDGE_PARTITION);
     }
 
     public void printCurrentTile(MoveResult moveResult, BridgeState userState, BridgeState bridgeState) {
@@ -61,6 +65,10 @@ public class OutputView {
      * <p>
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void printResult(boolean isSuccess, int totalTryCount) {
+    public void printResult(FinalGameResult finalGameResult, int totalTryCount, GameResult gameResult) {
+        System.out.println(LAST_GAME_RESULT_STRING);
+        printMap(gameResult);
+        System.out.println(WHETHER_SUCCESS_OR_NOT_STRING + finalGameResult.getFinalResult());
+        System.out.println(TOTAL_TRY_COUNT_STRING + totalTryCount);
     }
 }
