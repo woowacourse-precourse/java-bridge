ce598fb73e48b5ecf2819a00c8e6e3ac554367da
win9 <krrr8@naver.com>
2022-11-22 18:28:46 +0900

    Test(test): 테스트 실행 순서 제어
    테스트가 순서대로 진행되지 않아서 입력부가 제대로
    입력되지 않아서 생기는 오류를 테스트들의 순서를 정해서
    실행하도록 하여 오류를 수정한다.

--- src/test/java/bridge/controller/GameControllerRestartTest.java
+++ src/test/java/bridge/controller/GameControllerRestartTest.java
@@ -2,19 +2,24 @@ package bridge.controller;
 
 import bridge.Application;
 import camp.nextstep.edu.missionutils.test.NsTest;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
+
+import java.io.Console;
 
 import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeTest;
 import static org.assertj.core.api.Assertions.assertThat;
 
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 public class GameControllerRestartTest extends NsTest {
 
     private GameController gameController = new GameController();
 
+    @Order(1)
     @DisplayName("중간에 이동입력시 잘못된 값을 입력해도 결과가 제대로 출력된다.")
     @Test
     void 이동_예외_입력_테스트() {
+        System.out.println("GameControllerRestartTest.이동_예외_입력_테스트");
         assertRandomNumberInRangeTest(() -> {
             run("4", "U", "U", "X", "D", "D");
             assertThat(output()).contains(
@@ -24,12 +29,18 @@ public class GameControllerRestartTest extends NsTest {
                     "게임 성공 여부: 성공",
                     "총 시도한 횟수: 1"
             );
+
+            int upSideIndex = output().indexOf("[ O | O |   |   ]");
+            int downSideIndex = output().indexOf("[   |   | O | O ]");
+            assertThat(upSideIndex).isLessThan(downSideIndex);
         }, 1, 1, 0, 0);
     }
 
+    @Order(2)
     @DisplayName("게임을 실패후 그만두면 게임 성공여부에 실패가 표시된다.")
     @Test
     void 게임_실패_입력_테스트() {
+        System.out.println("GameControllerRestartTest.게임_실패_입력_테스트");
         assertRandomNumberInRangeTest(() -> {
             run("4", "U", "D", "Q");
             assertThat(output()).contains(
@@ -39,12 +50,17 @@ public class GameControllerRestartTest extends NsTest {
                     "게임 성공 여부: 실패",
                     "총 시도한 횟수: 1"
             );
+            int upSideIndex = output().indexOf("[ O |   ]");
+            int downSideIndex = output().indexOf("[   | X ]");
+            assertThat(upSideIndex).isLessThan(downSideIndex);
         }, 1, 1, 0, 0);
     }
 
+    @Order(3)
     @DisplayName("게임을 실패후 재시작시 성공여부에 실패가 표시된다.")
     @Test
     void 게임_재시작_입력_테스트() {
+        System.out.println("GameControllerRestartTest.게임_재시작_입력_테스트");
         assertRandomNumberInRangeTest(() -> {
             run("4", "U", "D", "R", "U", "U", "D", "D");
             assertThat(output()).contains(
