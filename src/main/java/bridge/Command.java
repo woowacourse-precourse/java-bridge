b723c602b3e5bda212fdd0391bb90e1f8019066c
dlgosla <dlgoslaek1@gmail.com>
2022-11-22 13:13:41 +0900

    feat: 명령어를 저장할 enum구현

---
+++ src/main/java/bridge/Command.java
@@ -0,0 +1,25 @@
+package bridge;
+
+public enum Command {
+    Retry("R"),
+    Quit("Q");
+
+    private final String type;
+
+    Command(String type) {
+        this.type = type;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public static Command toCommand(String command) {
+        for (Command type : Command.values()) {
+            if (type.getType().equals(command)) {
+                return type;
+            }
+        }
+        throw new IllegalArgumentError("올바르지 않은 명령어입니다.");
+    }
+}
