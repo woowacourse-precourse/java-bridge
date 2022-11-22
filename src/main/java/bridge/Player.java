7b2a1c57023fb1e885617f65361a1bb1e035aedd
chaos0103 <lyt1228@naver.com>
2022-11-22 12:29:41 +0900

    refactor(Bridge): 메서드 추출, 코드 수정

--- src/main/java/bridge/Player.java
+++ src/main/java/bridge/Player.java
@@ -2,21 +2,22 @@ package bridge;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.IntStream;
 
 import static bridge.Result.*;
 
 public class Player {
-    private final List<String> bridge;
+    private final List<String> bridges;
     private final List<String> directions = new ArrayList<>();
     private int playCount;
 
     public Player(List<String> bridge) {
-        this.bridge = bridge;
+        this.bridges = bridge;
         this.playCount = 1;
     }
 
-    public List<String> getBridge() {
-        return bridge;
+    public List<String> getBridges() {
+        return bridges;
     }
 
     public List<String> getDirections() {
@@ -27,13 +28,12 @@ public class Player {
         return playCount;
     }
 
-    //== 비즈니스 로직 ==//
     public void addDirection(String direction) {
         directions.add(direction);
     }
 
     public boolean isGameEnd() {
-        return directions.size() == bridge.size();
+        return directions.size() == bridges.size();
     }
 
     public void resetDirections() {
@@ -43,24 +43,51 @@ public class Player {
 
     public boolean isMovePossible() {
         int index = directions.size() - 1;
-        return directions.get(index).equals(bridge.get(index));
+        return directions.get(index).equals(bridges.get(index));
     }
 
     public List<Result> createMapInformation(String target) {
         List<Result> results = new ArrayList<>();
-        for (int i = 0; i < directions.size(); i++) {
-            String direction = directions.get(i);
-            if (direction.equals(target) && direction.equals(bridge.get(i))) {
-                results.add(POSSIBLE);
-            } else if (direction.equals(target) && !direction.equals(bridge.get(i))) {
-                results.add(IMPOSSIBLE);
-            } else {
-                results.add(BLANK);
-            }
-            if (i < directions.size() - 1) {
-                results.add(DIV);
-            }
-        }
+        IntStream.range(0, directions.size())
+                .forEach(index -> addResults(target, results, index));
         return results;
+    }
+
+    private void addResults(String target, List<Result> results, int index) {
+        String direction = directions.get(index);
+        String bridge = bridges.get(index);
+
+        Result result = getResult(target, direction, bridge);
+        results.add(result);
+
+        addDiv(results, index);
+    }
+
+    private Result getResult(String target, String direction, String bridge) {
+        if (isMovingPossible(target, direction, bridge)) {
+            return POSSIBLE;
+        }
+        if (isMovingImpossible(target, direction, bridge)) {
+            return IMPOSSIBLE;
+        }
+        return BLANK;
+    }
+
+    private void addDiv(List<Result> results, int index) {
+        if (!isLastIndex(index)) {
+            results.add(DIV);
+        }
+    }
+
+    private boolean isLastIndex(int index) {
+        return index == directions.size() - 1;
+    }
+
+    private boolean isMovingPossible(String target, String direction, String bridge) {
+        return direction.equals(target) && direction.equals(bridge);
+    }
+
+    private boolean isMovingImpossible(String target, String direction, String bridge) {
+        return direction.equals(target) && !direction.equals(bridge);
     }
 }
