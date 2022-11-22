69bddc34933c66a0c655ab1b2925ce83c75cbec8
ddukddaki <rlfrkdms1@naver.com>
2022-11-22 21:22:38 +0900

    refactor(BridgeGameTest): import문 위치 수정

--- src/test/java/bridge/domain/BridgeGameTest.java
+++ src/test/java/bridge/domain/BridgeGameTest.java
@@ -1,13 +1,12 @@
 package bridge.domain;
 
+import static org.assertj.core.api.Assertions.assertThat;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import java.util.List;
 import java.util.Map;
-
-import static org.assertj.core.api.Assertions.assertThat;
 
 class BridgeGameTest {
 
