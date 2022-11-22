7805a1084eaafb02844aadf6b8ee912f898b07e4
rjsah09 <83952174+rjsah09@users.noreply.github.com>
2022-11-22 18:10:29 +0900

    test: BridgeGame 클래스 추가 테스트

--- src/test/java/bridge/BridgeGameTest.java
+++ src/test/java/bridge/BridgeGameTest.java
@@ -1,5 +1,6 @@
 package bridge;
 
+import org.assertj.core.api.Assert;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
@@ -69,6 +70,39 @@ public class BridgeGameTest {
         bridgeGame.increaseTried(bridge);
 
         Assertions.assertThat(bridge.getTried()).isEqualTo(2);
+    }
+
+    @Test
+    void 게임_클리어_실패_미도착_테스트() {
+        List<String> footholds = Arrays.asList("U", "D", "U", "D", "U");
+        List<String> crossed = Arrays.asList("U", "D", "U", "D");
+        Bridge bridge = new Bridge(footholds, crossed);
+
+        boolean clear = bridgeGame.isClear(bridge);
+
+        Assertions.assertThat(clear).isEqualTo(false);
+    }
+
+    @Test
+    void 게임_클리어_실패_오답_테스트() {
+        List<String> footholds = Arrays.asList("U", "D", "U", "D", "U");
+        List<String> crossed = Arrays.asList("U", "D", "U", "D", "D");
+        Bridge bridge = new Bridge(footholds, crossed);
+
+        boolean clear = bridgeGame.isClear(bridge);
+
+        Assertions.assertThat(clear).isEqualTo(false);
+    }
+
+    @Test
+    void 게임_클리어_성공_테스트() {
+        List<String> footholds = Arrays.asList("U", "D", "U", "D", "U");
+        List<String> crossed = Arrays.asList("U", "D", "U", "D", "U");
+        Bridge bridge = new Bridge(footholds, crossed);
+
+        boolean clear = bridgeGame.isClear(bridge);
+
+        Assertions.assertThat(clear).isEqualTo(true);
     }
 
 }
