a77234e9115717f15142380e1e0da0b796f63943
jsk5341 <102288039+jsk5341@users.noreply.github.com>
2022-11-22 22:44:03 +0900

    refactor: split function to move and print result

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -1,13 +1,11 @@
 package bridge;
 
-
 import java.util.ArrayList;
 import java.util.List;
 
 public class Application {
 
     static InputView inputs = new InputView();
-
     static OutputView outputs = new OutputView();
     static BridgeGame bridgeGame = new BridgeGame();
 
@@ -30,39 +28,43 @@ public class Application {
     public static void main(String[] args) {
         List<String> currentResult = new ArrayList<>();
         List<String> newBridge = buildBridge();
-        boolean stageResult;
         boolean retryPlay = true;
         int triedCounter = 0;
-        int round;
-
         while (retryPlay) {
-            retryPlay = false;
             triedCounter++;
-            for (round = 0; round < newBridge.size(); round++) {
-                String userAnswer = inputs.readMovingWithValidityCheck();
-                stageResult = bridgeGame.move(userAnswer, newBridge, round);
-                outputs.designBridgeMap(newBridge, currentResult, stageResult, round);
-                outputs.printMap(currentResult);
-                if (!stageResult) {
-                    retryPlay = whenItsWrong(currentResult);
-                    break;
-                }//if
-            }
-            outputs.printResult(newBridge, currentResult, round, triedCounter);
-        }//while
+            retryPlay = gamePlay(newBridge,currentResult,triedCounter);
+        }
     }
 
+    static boolean gamePlay(List<String> newBridge, List<String> currentResult, int triedCounter) {
+        int round;
+        boolean retryPlay=false;
+        for (round = 0; round < newBridge.size(); round++) {
+            boolean stageResult = gamePlayMoving(newBridge, currentResult,round);
+            if (!stageResult) {
+                retryPlay = whenItsWrong(currentResult);
+                break;
+            }
+        }
+        printResultOrNot(newBridge, currentResult, round, triedCounter, retryPlay);
+        return retryPlay;
+    }
 
+    static boolean gamePlayMoving(List<String>newBridge,List<String> currentResult, int round){
+        String userAnswer = inputs.readMovingWithValidityCheck();
+        boolean stageResult = bridgeGame.move(userAnswer, newBridge, round);
+        outputs.designBridgeMap(newBridge, currentResult, stageResult, round);
+
+        return stageResult;
+    }
+    static void printResultOrNot(List<String> newBridge, List<String> currentResult, int round, int triedCounter, boolean retryPlay) {
+        if (!retryPlay) {
+            outputs.printResult(newBridge, currentResult, round, triedCounter);
+        }
+    }
     static boolean whenItsWrong(List<String> currentResult) {
         String retryAnswer = inputs.readGameCommandWithValidityCheck();
         boolean retry = bridgeGame.retry(retryAnswer, currentResult);
         return retry;
     }
-
-    static int resetStatus(List<String> currentResult, int triedCounter) { //진행상황 초기화 및 시도 카운터 증가
-        currentResult.clear();
-        triedCounter = playCounter(triedCounter);
-        return triedCounter;
-    }
-
 }
