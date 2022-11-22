38f63cf50b5489790ec5146a45b83b3dd8225bb8
Seoyoung <ryupark1006@gmail.com>
2022-11-22 15:41:45 +0900

    feat: 랜덤 값 U, D로 변환 기능 구현

--- src/main/java/bridge/BridgeMaker.java
+++ src/main/java/bridge/BridgeMaker.java
@@ -21,12 +21,21 @@ public class BridgeMaker {
      * @return 입력받은 길이에 해당하는 다리 모양. 위 칸이면 "U", 아래 칸이면 "D"로 표현해야 한다.
      */
     public List<String> makeBridge(int size) {
+        List<String> bridge = new ArrayList<>();
         for (int i = 0; i < size; i++) {
             int number = bridgeNumberGenerator.generate();
             if (number != 0 && number != 1) {
                 throw new IllegalArgumentException(BRIDGE_NUMBER_ERROR.getMessage());
             }
+            bridge.add(numberToString(number));
         }
-        return null;
+        return bridge;
+    }
+
+    private String numberToString(int number) {
+        if (number == 0) {
+            return "D";
+        }
+        return "U";
     }
 }
