d3737e0e20927048cbdaf3395e355da4c831e894
60jong <rudwhd515@gmail.com>
2022-11-22 18:44:12 +0900

    refactor(BridgeConstant) : 필드 value를 final 변수로 수정

--- src/main/java/bridge/constant/BridgeConstant.java
+++ src/main/java/bridge/constant/BridgeConstant.java
@@ -4,7 +4,7 @@ public enum BridgeConstant {
     BRIDGE_SIZE_START_INCLUSIVE(3),
     BRIDGE_SIZE_END_INCLUSIVE(20);
 
-    private int value;
+    private final int value;
 
     BridgeConstant(int value) {
         this.value = value;
