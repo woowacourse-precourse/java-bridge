a8a45e0590c51455e5a0a67073c001730dfd35c0
xxeol2 <jhj01177@naver.com>
2022-11-22 16:36:54 +0900

    feat: 출력 메세지 상수 정의

---
+++ src/main/java/bridge/constant/Message.java
@@ -0,0 +1,21 @@
+package bridge.constant;
+
+import static bridge.Moving.DOWN;
+import static bridge.Moving.UP;
+import static bridge.constant.GameCommand.QUIT;
+import static bridge.constant.GameCommand.RETRY;
+
+public class Message {
+
+    public static final String START_GAME = "다리 건너기 게임을 시작합니다.\n";
+    public static final String INPUT_BRIDGE_SIZE = "다리의 길이를 입력해주세요.";
+    public static final String INPUT_MOVING =
+            "이동할 칸을 선택해주세요. (위: " + UP.getCode() + ", 아래: " + DOWN.getCode() + ")";
+    public static final String INPUT_GAME_COMMAND =
+            "게임을 다시 시도할지 여부를 입력해주세요. (재시도: " + RETRY + ", 종료: " + QUIT + ")";
+    public static final String GAME_RESULT = "최종 게임 결과";
+    public static final String GAME_SUCCESS = "게임 성공 여부: ";
+    public static final String SUCCESS = "성공";
+    public static final String FAIL = "실패";
+    public static final String TOTAL_TRIAL_COUNT = "총 시도한 횟수: ";
+}
