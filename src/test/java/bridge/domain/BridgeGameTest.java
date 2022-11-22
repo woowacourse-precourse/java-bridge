2855472fd80f2f480c3ec9eca75084368d5e369c
LeeHaiLim <hailim1997@gmail.com>
2022-11-22 18:04:29 +0900

    test(bridge): change variable name

--- src/test/java/bridge/domain/BridgeGameTest.java
+++ src/test/java/bridge/domain/BridgeGameTest.java
@@ -7,28 +7,28 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class BridgeGameTest {
-    private final BridgeGame bridgeGame = new BridgeGame(new Bridge(List.of("U","D","D")));
+    private final BridgeGame testBridgeGame = new BridgeGame(new Bridge(List.of("U","D","D")));
 
     @DisplayName("다리가 이어진 곳으로 건너면 CROSSING 상태를 반환한다.")
     @Test
     void moveCrossingTest() {
-        GameState gameState = bridgeGame.move("U");
+        GameState gameState = testBridgeGame.move("U");
         assertThat(gameState.isCrossing()).isTrue();
     }
 
     @DisplayName("다리가 이어진 곳으로 모두 건너면 CROSSED 상태를 반환한다.")
     @Test
     void moveCrossedTest() {
-        bridgeGame.move("U");
-        bridgeGame.move("D");
-        GameState gameState = bridgeGame.move("D");
+        testBridgeGame.move("U");
+        testBridgeGame.move("D");
+        GameState gameState = testBridgeGame.move("D");
         assertThat(gameState.isCrossed()).isTrue();
     }
 
     @DisplayName("다리가 이어지지 않은 곳으로 건너면 FALL 상태를 반환한다.")
     @Test
     void moveFallTest() {
-        GameState gameState = bridgeGame.move("D");
+        GameState gameState = testBridgeGame.move("D");
         assertThat(gameState.isFall()).isTrue();
     }
 }
--- src/test/java/bridge/domain/GameStateTest.java
+++ src/test/java/bridge/domain/GameStateTest.java
@@ -9,27 +9,27 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
 public class GameStateTest {
-    private static final GameState GAME_STATE = new GameState();
-    private static final List<String> MOVES = GAME_STATE.getMoves();
+    private static final GameState TEST_GAME_STATE = new GameState();
+    private static final List<String> TEST_MOVES = TEST_GAME_STATE.getMoves();
 
     @DisplayName("상태가 업데이트 되는 지 테스트한다.")
     @ValueSource(strings = {"U,1", "D,2", "D,3", "U,4"})
     @ParameterizedTest
     void updateStateTest(String input) {
         String[] inputs = input.split(",");
-        GAME_STATE.updateState(inputs[0], BridgeState.CROSSING);
-        System.out.println(MOVES.size());
-        assertThat(MOVES.get(MOVES.size() - 1)).isEqualTo(inputs[0]);
-        assertThat(GAME_STATE.nextLocation()).isEqualTo(Integer.parseInt(inputs[1]));
+        TEST_GAME_STATE.updateState(inputs[0], BridgeState.CROSSING);
+        System.out.println(TEST_MOVES.size());
+        assertThat(TEST_MOVES.get(TEST_MOVES.size() - 1)).isEqualTo(inputs[0]);
+        assertThat(TEST_GAME_STATE.nextLocation()).isEqualTo(Integer.parseInt(inputs[1]));
     }
 
     @DisplayName("초기화되는 지 테스트한다.")
     @Test
     void initTest() {
-        int attemptBefore = GAME_STATE.getAttempt();
-        GAME_STATE.init();
-        assertThat(MOVES.isEmpty()).isTrue();
-        assertThat(GAME_STATE.getAttempt()).isEqualTo(attemptBefore + 1);
+        int attemptBefore = TEST_GAME_STATE.getAttempt();
+        TEST_GAME_STATE.init();
+        assertThat(TEST_MOVES.isEmpty()).isTrue();
+        assertThat(TEST_GAME_STATE.getAttempt()).isEqualTo(attemptBefore + 1);
     }
 
 }
