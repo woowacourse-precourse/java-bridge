da463e07c923b6c327623a5b28798c2481692879
eom-tae-in <eti0728@daum.net>
2022-11-22 21:27:02 +0900

    feat(User): Getter, Setter 생성

--- src/main/java/bridge/domain/User.java
+++ src/main/java/bridge/domain/User.java
@@ -1,11 +1,23 @@
 package bridge.domain;
 
 public class User {
-    private int playCount;
+    private final int playCount;
     private boolean quit;
 
     public User() {
         this.playCount = 1;
         quit = false;
+    }
+
+    public int getPlayCount() {
+        return this.playCount;
+    }
+
+    public boolean getQuit() {
+        return this.quit;
+    }
+
+    public void setQuit() {
+        this.quit = true;
     }
 }
