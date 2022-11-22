a10a984c0edf91ce5e81970d0e0262739d38ecb3
Somi Kim <rlathal009@gmail.com>
2022-11-22 19:38:47 +0900

    feat : 다리 생성하기 기능 추가

--- src/main/java/bridge/service/BridgeGame.java
+++ src/main/java/bridge/service/BridgeGame.java
@@ -1,9 +1,30 @@
 package bridge.service;
 
+import bridge.BridgeMaker;
+import bridge.BridgeNumberGenerator;
+import bridge.domain.Bridge;
+
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * 다리 건너기 게임을 관리하는 클래스
  */
 public class BridgeGame {
+    private final BridgeMaker bridgeMaker;
+    private Bridge bridge;
+    private Bridge user;
+
+    public BridgeGame(BridgeNumberGenerator bridgeNumberGenerator) {
+        this.bridgeMaker = new BridgeMaker(bridgeNumberGenerator);
+    }
+
+    public void initBridge(int size) {
+        List<String> bridge = bridgeMaker.makeBridge(size);
+
+        this.bridge = new Bridge(bridge);
+        this.user = new Bridge(new ArrayList<>());
+    }
 
     /**
      * 사용자가 칸을 이동할 때 사용하는 메서드
