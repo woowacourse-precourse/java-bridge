c6b740d931b919c14d8ef14a2c5a5aae14473a19
wonju-dev <wonjulee.dev@gmail.com>
2022-11-22 18:36:15 +0900

    feat(MessageGenerator): 시스템 메시지 구성

--- src/main/java/bridge/domain/MessageGenerator.java
+++ src/main/java/bridge/domain/MessageGenerator.java
@@ -20,6 +20,11 @@ public class MessageGenerator {
     public static final String GAME_FINISH_RESULT_FAIL_MSG = "게임 성공 여부: 실패";
     public static final String GAME_FINISH_TOTAL_TRY_MSG = "총 시도한 횟수: ";
 
+    public static final String GAME_START_HEADER_MSG = "다리 건너기 게임을 시작합니다.";
+    public static final String GAME_ASK_BRIDGE_SIZE_MSG = "다리의 길이를 입력해주세요.";
+    public static final String GAME_ASK_ACTION_MSG = "이동할 칸을 선택해주세요. (위: U, 아래: D)";
+    public static final String GAME_ASK_RETRY_MSG = "게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)";
+
     private MessageGenerator() {
     }
 
