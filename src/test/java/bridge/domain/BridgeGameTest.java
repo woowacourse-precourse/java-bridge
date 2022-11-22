d99a72054b9e3fa01c284434ca89960fa3602450
ryanha <ryanha@handong.ac.kr>
2022-11-22 20:21:32 +0900

    refactor(domainTest): 코드 중복요소, 불필요한 코드 삭제

--- src/test/java/bridge/domain/BridgeGameTest.java
+++ src/test/java/bridge/domain/BridgeGameTest.java
@@ -6,15 +6,17 @@ import org.junit.jupiter.api.*;
 import java.util.ArrayList;
 import java.util.List;
 
-@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 public class BridgeGameTest {
     private static BridgeGame gameManager;
 
+    @BeforeEach
+    void create_bridge_game_instance() {
+        gameManager = new BridgeGame();
+    }
+
     @DisplayName("현재의 이동명령이 이동가능한 경우 True를 반환하는지 테스트")
     @Test
-    @Order(1)
     void isMovalble_test() {
-        gameManager = new BridgeGame();
         List<String> bridgeStatus = new ArrayList<>(List.of("U", "D", "D", "U", "U"));
         gameManager.setBridgeStatus(bridgeStatus);
 
@@ -24,9 +26,7 @@ public class BridgeGameTest {
 
     @DisplayName("현재의 이동명령이 불가한 경우 False를 반환하는 지 테스트")
     @Test
-    @Order(2)
     void isNotMovable_test() {
-        gameManager = new BridgeGame();
         List<String> bridgeStatus = new ArrayList<>(List.of("U", "D", "D", "U", "U"));
         gameManager.setBridgeStatus(bridgeStatus);
 
@@ -36,9 +36,7 @@ public class BridgeGameTest {
 
     @DisplayName("게임을 다시 시도하는 기능에 대한 테스트")
     @Test
-    @Order(3)
     void retry_test() {
-        gameManager = new BridgeGame();
         int beforeCount = gameManager.getAttemptsCount();
         gameManager.retry();
 
@@ -50,9 +48,7 @@ public class BridgeGameTest {
 
     @DisplayName("게임을 성공한 경우 제대로 판정하는 지 테스트")
     @Test
-    @Order(4)
     void game_success_judgement_test() {
-        gameManager = new BridgeGame();
         List<String> bridgeStatus = new ArrayList<>(List.of("U", "D", "D", "U", "U"));
         gameManager.setBridgeStatus(bridgeStatus);
         List<String> commands = new ArrayList<>(List.of("U", "D", "D", "U", "U"));
@@ -60,15 +56,12 @@ public class BridgeGameTest {
             gameManager.move(command);
         }
 
-        assertThat(gameManager.isGameSuccess())
-                .isTrue();
+        assertThat(gameManager.isGameSuccess()).isTrue();
     }
 
     @DisplayName("게임을 실패한 경우 제대로 판정하는 지 테스트")
     @Test
-    @Order(5)
     void game_failed_judgement_test() {
-        gameManager = new BridgeGame();
         List<String> bridgeStatus = new ArrayList<>(List.of("U", "D", "D", "U", "U"));
         gameManager.setBridgeStatus(bridgeStatus);
         List<String> commands = new ArrayList<>(List.of("U", "D", "U", "U", "U"));
@@ -76,7 +69,6 @@ public class BridgeGameTest {
             gameManager.move(command);
         }
 
-        assertThat(gameManager.isGameSuccess())
-                .isFalse();
+        assertThat(gameManager.isGameSuccess()).isFalse();
     }
 }
