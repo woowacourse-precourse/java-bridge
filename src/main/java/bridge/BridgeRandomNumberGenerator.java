1415b4f02b2965725e67a58c665825be68d734d5
sunghyuki <sunghyuk1609@gmail.com>
2022-11-22 10:29:10 +0900

    feat : 다리 생성 기능 추가

--- src/main/java/bridge/BridgeRandomNumberGenerator.java
+++ src/main/java/bridge/BridgeRandomNumberGenerator.java
@@ -4,8 +4,8 @@ import camp.nextstep.edu.missionutils.Randoms;
 
 public class BridgeRandomNumberGenerator implements BridgeNumberGenerator {
 
-    private static final int RANDOM_LOWER_INCLUSIVE = 0;
-    private static final int RANDOM_UPPER_INCLUSIVE = 1;
+    public static final int RANDOM_LOWER_INCLUSIVE = 0;
+    public static final int RANDOM_UPPER_INCLUSIVE = 1;
 
     @Override
     public int generate() {
