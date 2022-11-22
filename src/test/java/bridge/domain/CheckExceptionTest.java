e36b6293817cc67d3ad08dadb5f7607915594595
jijongkwon <help3451@naver.com>
2022-11-22 16:02:42 +0900

    test : 다리길이의 입렵값에 대한 예외처리 테스트 구현

---
+++ src/test/java/bridge/domain/CheckExceptionTest.java
@@ -0,0 +1,30 @@
+package bridge.domain;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+class CheckExceptionTest {
+    private final ByteArrayOutputStream outputStreamCaptor = new ByteArrayOutputStream();
+    CheckException checkException = new CheckException();
+    private static final String ERROR_MESSAGE = "[ERROR]";
+
+    @BeforeEach
+    public void setUp() {
+        System.setOut(new PrintStream(outputStreamCaptor));
+    }
+
+    @Test
+    void 다리길이_예외처리() {
+        assertSimpleTest(() -> {
+            checkException.checkBridgeSize(1);
+            assertThat(outputStreamCaptor.toString()).contains(ERROR_MESSAGE);
+        });
+    }
+}
