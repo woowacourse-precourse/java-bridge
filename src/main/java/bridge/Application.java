7d2d700d26d72f4107f322a9f63ff459fa889a52
ouohoon <begreat@kakao.com>
2022-11-22 17:29:10 +0900

    feat(Application): 게임 반복 실행 로직 구현

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -1,8 +1,42 @@
 package bridge;
 
-public class Application {
+import java.util.List;
 
+public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        BridgeMaker bridgeMaker =
+                new BridgeMaker(new BridgeRandomNumberGenerator());
+        InputView inputView = new InputView();
+        OutputView outputView = new OutputView();
+        Player player = new Player();
+        outputView.printStart();
+        outputView.printBridgeSizeInput();
+        List<String> bridge = bridgeMaker.makeBridge(inputView.readBridgeSize());
+        BridgeGame bridgeGame = new BridgeGame(bridge.size());
+        boolean isRun = true;
+        while (isRun) {
+            outputView.printMoveInput();
+            String moving = inputView.readMoving();
+            Boolean moveResult = bridgeGame.move(bridge, moving);
+            outputView.printMap(bridgeGame.getPlayerPos(), bridge, moveResult);
+            if (!moveResult) {
+                outputView.printCommandInput();
+                String command = inputView.readGameCommand();
+                if ("R".equals(command)) {
+                    bridgeGame.retry();
+                }
+                if ("Q".equals(command)) {
+                    isRun = false;
+                }
+            }
+            if (bridgeGame.isClear()) {
+                isRun = false;
+            }
+        }
+        outputView.printResult(bridgeGame.getPlayerPos(), bridge, bridgeGame.isClear(), bridgeGame.getTotalTry());
+    }
+
+    private static void init() {
+
     }
 }
