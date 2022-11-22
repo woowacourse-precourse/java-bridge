36421f5083536210cbf5000705d235fa01fae15e
Koonayoung518 <koony000415@gmail.com>
2022-11-22 19:38:42 +0900

    feat(bridge): bridgeController 추가

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -1,39 +1,9 @@
 package bridge;
 
 public class Application {
-    private static OutputView outputView = new OutputView();
-    private static InputView inputView = new InputView();
 
     public static void main(String[] args) {
-        outputView.printStart();
-        BridgeGame bridgeGame = new BridgeGame();
-        Bridge bridge = bridgeGame.setBridge(inputView.readBridgeSize());
-        play(bridge, bridgeGame);
-        outputView.printResult(bridgeGame, bridge);
+        BridgeController.game();
     }
 
-    public static void play(Bridge bridge, BridgeGame bridgeGame) {
-        while (!bridgeGame.isFinish(bridge)) {
-            if (bridgeGame.isMoving(bridge, move(bridge, bridgeGame))) {
-                continue;
-            }
-            if (!isRestart(bridgeGame)) {
-                break;
-            }
-        }
-    }
-
-    private static Moving move(Bridge bridge, BridgeGame bridgeGame) {
-        Moving moving =  inputView.readMoving();
-        bridgeGame.move(bridge, moving);
-        outputView.printMap(bridgeGame.getMovedBridge());
-        return moving;
-    }
-
-    private static boolean isRestart(BridgeGame bridgeGame) {
-        if (!inputView.readGameCommand().isRestart())
-            return false;
-        bridgeGame.retry();
-        return true;
-    }
 }
---
+++ src/main/java/bridge/BridgeController.java
@@ -0,0 +1,41 @@
+package bridge;
+
+public class BridgeController {
+
+    private static OutputView outputView = new OutputView();
+    private static InputView inputView = new InputView();
+
+    public static void game() {
+        outputView.printStart();
+        BridgeGame bridgeGame = new BridgeGame();
+        Bridge bridge = bridgeGame.setBridge(inputView.readBridgeSize());
+        play(bridge, bridgeGame);
+        outputView.printResult(bridgeGame, bridge);
+
+    }
+
+    public static void play(Bridge bridge, BridgeGame bridgeGame) {
+        while (!bridgeGame.isFinish(bridge)) {
+            if (bridgeGame.isMoving(bridge, move(bridge, bridgeGame))) {
+                continue;
+            }
+            if (!isRestart(bridgeGame)) {
+                break;
+            }
+        }
+    }
+
+    private static Moving move(Bridge bridge, BridgeGame bridgeGame) {
+        Moving moving = inputView.readMoving();
+        bridgeGame.move(bridge, moving);
+        outputView.printMap(bridgeGame.getMovedBridge());
+        return moving;
+    }
+
+    private static boolean isRestart(BridgeGame bridgeGame) {
+        if (!inputView.readGameCommand().isRestart())
+            return false;
+        bridgeGame.retry();
+        return true;
+    }
+}
