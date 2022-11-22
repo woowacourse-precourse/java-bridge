d4cf515d65238b527b4e21b29174149152f51801
minju412 <ln8847@ajou.ac.kr>
2022-11-22 22:41:28 +0900

    test(LengthTest): change test
    - 중복을 제거하기 위해 변경

--- src/test/java/bridge/domain/game/LengthTest.java
+++ src/test/java/bridge/domain/game/LengthTest.java
@@ -3,32 +3,16 @@ package bridge.domain.game;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
 class LengthTest {
 
-    @DisplayName("다리 길이가 3 이상 20 이하가 아니라면 예외가 발생한다.")
+    @DisplayName("다리 길이가 3 이상 20 이하 숫자가 아니라면 예외가 발생한다.")
     @ParameterizedTest
-    @CsvSource({"'0'", "-1", "'21'"})
+    @CsvSource({"'0'", "-1", "'21'", "'r'", "'4$'", "''", "' '"})
     void createLength_with_invalid_size(String size) {
         assertThatThrownBy(() -> new Length(size))
-                .isInstanceOf(IllegalArgumentException.class);
-    }
-
-    @DisplayName("다리 길이에 문자가 포함되어 있으면 예외가 발생한다.")
-    @ParameterizedTest
-    @CsvSource({"'r'", "'4$'"})
-    void createLength_with_invalid_format(String size) {
-        assertThatThrownBy(() -> new Length(size))
-                .isInstanceOf(IllegalArgumentException.class);
-    }
-
-    @DisplayName("다리 길이가 공백이면 예외가 발생한다.")
-    @Test
-    void createLength_with_blank() {
-        assertThatThrownBy(() -> new Length(""))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 }
