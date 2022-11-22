2fb9af3d32b04ceea5056b112ebd9b1c00ece5d0
baexxbin <soo3886@gmail.com>
2022-11-22 23:21:07 +0900

    test(UserPlayerTest): UserPlayer의 함수 테스트 작성

--- src/test/java/bridge/vo/UserPlayerTest.java
+++ src/test/java/bridge/vo/UserPlayerTest.java
@@ -1,32 +1,38 @@
 package bridge.vo;
 
+import static org.assertj.core.api.Assertions.*;
+
+import java.util.List;
+
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class UserPlayerTest {
-	@DisplayName("사용자 입력 테스트")
-	@Test
-	void inputMovingDirection() {
-		//	given
-		// UserPlayer userPlayer = UserPlayer.getInstance();
-		// List<String> inputValue = List.of("D,D,U,D,U");
-
-		//	when & then
-		/*assertThat(userPlayer.setMovingDirection("F")).isEqualTo(false);
-		assertThat(userPlayer.setMovingDirection("D")).isEqualTo(true);*/
-
-	}
-
 	@DisplayName("건널 수 있는 값인지 판단")
 	@Test
-	void isCrossableStep() {
+	void isCrossStep() {
+		// given
+		UserPlayer userPlayer = new UserPlayer();
+		userPlayer.setMovingDirection("D");
+		userPlayer.setMovingDirection("U");
+		userPlayer.setMovingDirection("D");
+		//	when
+		Bridge bridge = new Bridge(List.of("U", "D", "U"));
+		// then
+		assertThat(userPlayer.isCrossStep(bridge, 0)).isFalse();
+	}
+
+	@DisplayName("게임 다시 시도시 총 도전횟수 업데이트 확인 테스트")
+	@Test
+	void initUserPlayInformation() {
 		// //given
-		// UserPlayer userPlayer = UserPlayer.getInstance();
-		//
+		UserPlayer userPlayer = new UserPlayer();
+		userPlayer.setMovingDirection("D");
+
 		// //when
-		// userPlayer.setMovingDirection("U");
-		//
+		userPlayer.initUserPlayInformation();
+
 		// //then
-		// System.out.println("Record = " + userPlayer.showMoveRecord());
+		assertThat(userPlayer.getTotalAttempt()).isEqualTo(2);
 	}
 }
