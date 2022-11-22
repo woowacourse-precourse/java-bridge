d4828baf1340776a0afb67a1fcb83c3c1687fba5
jaehong <println@kakao.com>
2022-11-22 09:52:49 +0900

    test (BridgeTest) : 중복된 다리 생성 제거

--- src/test/java/bridge/domain/bridge/BridgeTest.java
+++ src/test/java/bridge/domain/bridge/BridgeTest.java
@@ -6,9 +6,9 @@ import bridge.domain.bridge.maker.BridgeMaker;
 import bridge.domain.bridge.maker.BridgeMakerFake;
 import bridge.domain.code.BridgePosition;
 import bridge.domain.code.SurviveStatus;
-import java.util.Arrays;
-import java.util.List;
-import java.util.stream.Collectors;
+import bridge.utils.TestUtils;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayNameGeneration;
 import org.junit.jupiter.api.DisplayNameGenerator;
 import org.junit.jupiter.api.Test;
@@ -16,15 +16,23 @@ import org.junit.jupiter.api.Test;
 @DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
 class BridgeTest {
 
+    private Bridge bridge;
+
+    @BeforeEach
+    void setUp() {
+        var fakeGenerator = new BridgeMakerFake(TestUtils.convertStringToListNumber("1,0,1,0"));
+        var makeBridge = new BridgeMaker(fakeGenerator);
+        var bridgeSize = new BridgeSize("4");
+        this.bridge = new Bridge(makeBridge, bridgeSize);
+    }
+
+    @AfterEach
+    void tearDown() {
+        this.bridge = null;
+    }
+
     @Test
     void 한칸을_건널때_생존상태를_반환합니다() {
-        var makeBridge = new BridgeMaker(
-                new BridgeMakerFake(stringToNumberList("1,0,1,0"))
-        );
-        var bridgeSize = new BridgeSize("4");
-
-        var bridge = new Bridge(makeBridge, bridgeSize);
-
         assertThat(bridge.next(BridgePosition.UP)).isEqualTo(SurviveStatus.SURVIVE);
         assertThat(bridge.next(BridgePosition.DOWN)).isEqualTo(SurviveStatus.SURVIVE);
         assertThat(bridge.next(BridgePosition.UP)).isEqualTo(SurviveStatus.SURVIVE);
@@ -33,22 +41,11 @@ class BridgeTest {
 
     @Test
     void 한칸을_건널때_생존실패상태를_반환합니다() {
-        var makeBridge = new BridgeMaker(
-                new BridgeMakerFake(stringToNumberList("1,0,1,0"))
-        );
-        var bridgeSize = new BridgeSize("4");
-
-        var bridge = new Bridge(makeBridge, bridgeSize);
-
         assertThat(bridge.next(BridgePosition.DOWN)).isEqualTo(SurviveStatus.DIE);
         assertThat(bridge.next(BridgePosition.UP)).isEqualTo(SurviveStatus.DIE);
         assertThat(bridge.next(BridgePosition.DOWN)).isEqualTo(SurviveStatus.DIE);
         assertThat(bridge.next(BridgePosition.UP)).isEqualTo(SurviveStatus.DIE);
     }
 
-    private List<Integer> stringToNumberList(final String input) {
-        return Arrays.stream(input.split(","))
-                .map(Integer::parseInt)
-                .collect(Collectors.toList());
-    }
+
 }
