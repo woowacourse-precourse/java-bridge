2564da6a1a994a5fdec33819f9c06a23cd1f24dd
realslimtaek <89964264+realslimtaek@users.noreply.github.com>
2022-11-22 13:54:57 +0900

    feat : 예외사항 추가 (다리 길이, 입력 가능 문자)

--- src/main/java/bridge/InputView.java
+++ src/main/java/bridge/InputView.java
@@ -17,8 +17,8 @@ public class InputView {
     public void readBridgeSize() {
         System.out.println("다리의 길이를 입력해주세요.");
         try {
-//            System.out.println("asd" + write_bridge_size());
             size = write_bridge_size();
+            checkSizeValidation(size);
         } catch (IllegalArgumentException e) {
             System.out.println(e.getMessage());
             readBridgeSize();
@@ -29,37 +29,57 @@ public class InputView {
         int answer;
         try {
             answer = Integer.parseInt(Console.readLine());
-//            System.out.println(answer);
         } catch (NumberFormatException e) {
             throw new IllegalArgumentException("[ERROR] 잘못된 입력을 하였습니다.");
         }
         return answer;
     }
 
-    public void readMoving() {
+    public static void checkSizeValidation(int size){
+        if(size < 3 || size > 20){
+            throw new IllegalArgumentException("[ERROR] 다리의 크기는 3 이상 20 이하이여야 합니다.");
+        }
+    }
+
+    public static void readMoving() {
         System.out.println("이동할 칸을 선택해주세요. (위: U, 아래: D)");
         try {
             move.add(checkMoving());
         } catch (IllegalArgumentException e) {
-            System.err.println(e.getMessage());
+            System.out.println(e.getMessage());
             readMoving();
         }
     }
 
-    public String checkMoving() {
+    public static String checkMoving() {
         String answer = "";
         try {
             answer = Console.readLine();
-//            checkValidateMove(answer);
+            movingValidation(answer);
         } catch (IllegalArgumentException e) {
             throw new IllegalArgumentException("[ERROR] 잘못된 입력을 하였습니다.");
         }
         return answer;
     }
 
+    public static void movingValidation(String answer){
+        if(!Objects.equals(answer, "U") && !Objects.equals(answer, "D")){
+            throw new IllegalArgumentException("[ERROR] U와 D로만 입력해주세요.");
+        }
+
+    }
+
     public static String readGameCommand() {
         System.out.println("게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)");
-        String a = Console.readLine();
+        String a = "";
+        try {
+            a = Console.readLine();
+            checkGameCommandValidation(a);
+            checkGameCommand(a);
+        } catch (IllegalArgumentException e){
+            System.out.println(e.getMessage());
+            readGameCommand();
+        }
         return a;
     }
 
@@ -71,27 +91,37 @@ public class InputView {
         return move;
     }
 
-    public void checkGameCommand(String a){
+    public static void checkGameCommand(String a){
         if(Objects.equals(a, "R")){
-            ++BridgeGame.play_count;
-            OutputView.now_size = 0;
-            move.clear();
-            Application.playBridgeGame();
+            restartGame();
         }
         if(Objects.equals(a,"Q")){
-            System.out.println("최종 게임 결과");
-            outputView.printResult();
-            System.out.println("\n 게임 성공 여부 : 실패");
-            System.out.println("총 시도한 횟수 : " + BridgeGame.play_count);
+            endGame();
         }
+    }
+
+    public static void restartGame(){
+        ++BridgeGame.play_count;
+        OutputView.now_size = 0;
+        move.clear();
+        BridgeGame.playBridgeGame();
+    }
+
+    public static void endGame(){
+        System.out.println("최종 게임 결과");
+        outputView.printResult();
+        System.out.println("\n게임 성공 여부 : 실패" +"\n" +
+                "총 시도한 횟수 : " + BridgeGame.play_count);
+    }
+
+    public static void checkGameCommandValidation(String a){
+        if(!Objects.equals(a, "Q") && !Objects.equals(a, "R")){
+            throw new IllegalArgumentException("[ERROR] Q 와 R로만 입력해주세요.");
+        }
+
     }
     public void clearMove(){
         move.clear();
     }
 
-//    public void checkValidateMove(String a){
-//        if(!Objects.equals(a, "D") || !Objects.equals(a, "U")){
-//            throw new IllegalArgumentException("[ERROR] 잘못된 입력을 하였습니다.");
-//        }
-//    }
 }
