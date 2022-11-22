38ff4625fdcd31038734d0f30e0d72ad2c04f0f8
fnzl54 <rnjscksdud1@gmail.com>
2022-11-22 13:17:14 +0900

    style: separate print method

--- src/main/java/bridge/ExceptionCase.java
+++ src/main/java/bridge/ExceptionCase.java
@@ -6,28 +6,32 @@ public class ExceptionCase {
 
     static public void isInputUpOrDown(String input) {
         if (!(input.equals("U") || input.equals("D"))) {
-            throw new IllegalArgumentException("[ERROR] 다리는 U와 D로만 움직여야 합니다.");
+            throw new IllegalArgumentException(OutputView.getErrorMessageUpOrDown());
         }
     }
 
     static public void isInputBound(int input) {
 
         if (!(input >= 3 && input <= 20)) {
-            throw new IllegalArgumentException("[ERROR] 다리의 길이는 3부터 20사이의 숫자여야 합니다.");
+            throw new IllegalArgumentException("다리의 길이는 3부터 20사이의 숫자여야 합니다.");
         }
     }
 
     static public void isInputTry(String input) {
         if (!(input.equals("R") || input.equals("Q"))) {
-            throw new IllegalArgumentException("[ERROR] 재시작은 R과 Q로만 입력 가능합니다..");
+            throw new IllegalArgumentException(OutputView.getErrorMessageInputTry());
         }
     }
 
     static public int isInputAllNumber(String input) {
         try {
+            int inputNumber = Integer.parseInt(input);
+            if (!(inputNumber >= 3 && inputNumber <= 20)) {
+                throw new IllegalArgumentException();
+            }
             return Integer.parseInt(input);
-        } catch (NumberFormatException e) {
-            throw new IllegalArgumentException("[ERROR] 자연수를 입력해주세요.");
+        } catch (IllegalArgumentException e) {
+            throw new IllegalArgumentException(OutputView.getErrorMessageAllNumber());
         }
     }
 }
