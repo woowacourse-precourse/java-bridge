811c70d6f5c7179f024957d6d886b9cbcc7a4665
Dasol Jung <greteljds0251@gmail.com>
2022-11-22 21:24:56 +0900

    feat: 사용자가 이동할 칸을 반복해서 입력

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -1,5 +1,6 @@
 package bridge;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Application {
@@ -18,5 +19,19 @@ class Bridge {
         gameUI.getBridgeSize();
         int bridgeSize = inputView.readBridgeSize();
         return bridgeMaker.makeBridge(bridgeSize);
+    }
+
+    public void gamePlay(List<String> bridgeShape) {
+        List<Integer> playerMove = new ArrayList<>();
+        for (int i = 0; i < bridgeShape.size(); i++) {
+            movingPlayer(bridgeShape.get(i), playerMove);
+        }
+    }
+
+    public void movingPlayer(String bridgeShape, List<Integer> playerMove) {
+        BridgeGame bridgeGame = new BridgeGame();
+        gameUI.moving();
+        String moving = inputView.readMoving();
+        playerMove.add(bridgeGame.move(moving, bridgeShape));
     }
 }
