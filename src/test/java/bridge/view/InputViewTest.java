c4afbef1769e477f1ad51d1869deb6f2a40ff500
ddukddaki <rlfrkdms1@naver.com>
2022-11-22 22:49:41 +0900

    test(InputViewTest): 다리길이를 입력 받을 때 범위를 넘어섰다면 예외 문구를 출력하고 다시 입력 받을 수 있다

--- src/test/java/bridge/view/InputViewTest.java
+++ src/test/java/bridge/view/InputViewTest.java
@@ -29,7 +29,25 @@ class InputViewTest extends NsTest {
         }, 1, 0, 1);
     }
 
-   
+    @Test
+    @DisplayName("다리길이를 입력 받을 때 범위를 넘어섰다면 예외 문구를 출력하고 다시 입력 받을 수 있다.")
+    void 다리길이를_입력_받을때_범위를_넘은_테스트() {
+        assertRandomNumberInRangeTest(() -> {
+            run("30","3", "U", "D", "U");
+            assertThat(output()).contains(
+                    ERROR_MESSAGE,
+                    "최종 게임 결과",
+                    "[ O |   | O ]",
+                    "[   | O |   ]",
+                    "게임 성공 여부: 성공",
+                    "총 시도한 횟수: 1"
+            );
+
+        }, 1, 0, 1);
+    }
+
+
+
 
     @Override
     protected void runMain() {
