4ca5a5228b9019a5281428bfa5f4199cc60f8f9e
GilHun <suleemon1@naver.com>
2022-11-22 22:44:19 +0900

    feat(Application): Bridge Application 기능 구현

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -1,8 +1,16 @@
 package bridge;
 
+import bridge.view.DefaultView;
+
+import javax.swing.text.View;
+
 public class Application {
 
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        ApplicationContext context = ApplicationContext.getContext();
+        DefaultView view = context.getView();
+
+        view.render();
     }
 }
