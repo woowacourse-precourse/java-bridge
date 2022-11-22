d69eba9907cab233161f40ddc1a0d94cb41d06df
ddukddaki <rlfrkdms1@naver.com>
2022-11-22 17:22:17 +0900

    feat(OutputView): 게임의 성공 여부를 판단해 출력 문구를 반환하는 기능

--- src/main/java/bridge/OutputView.java
+++ src/main/java/bridge/OutputView.java
@@ -12,6 +12,9 @@ public class OutputView {
 
     private static final String OUTPUT_GAME_START = "다리 건너기 게임을 시작합니다.";
 
+    private static final String OUTPUT_FAIL = "실패";
+    private static final String OUTPUT_SUCCESS = "성공";
+    
     public void printStartGame() {
         System.out.println(OUTPUT_GAME_START);
     }
@@ -37,5 +40,13 @@ public class OutputView {
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
     public void printResult() {
+
+    }
+
+    private String isSuccess(boolean isGameOver) {
+        if (isGameOver) {
+            return OUTPUT_SUCCESS;
+        }
+        return OUTPUT_FAIL;
     }
 }
