66a48cd9fd5814108a427cc194157dbf4cc4b7fb
bsh1603 <bsh1603@github.com>
2022-11-22 12:26:46 +0900

    Test: 우테코지원사이트

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -9,10 +9,8 @@ public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
         BridgeController bridgeController = new BridgeController();
-
         bridgeController.startGame();
         List<String> bridge = bridgeController.bridgeMake();
-
         bridgeController.playBridgeGame(bridge);
     }
 }
