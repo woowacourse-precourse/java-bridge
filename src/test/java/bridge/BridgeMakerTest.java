486afc4f750a552c2ee30275644d57d1952048e4
JunHyeon <dlwnsgus07@naver.com>
2022-11-22 20:12:13 +0900

    test: 적절한 다리가 생성되는지 테스트

--- src/test/java/bridge/BridgeMakerTest.java
+++ src/test/java/bridge/BridgeMakerTest.java
@@ -3,6 +3,11 @@ package bridge;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import testBridgeGenerator.TestBridgeGenerator;
+
+import java.util.List;
+
+import static bridge.ApplicationTest.*;
 
 class BridgeMakerTest {
     BridgeNumberGenerator bridgeNumberGenerator = new BridgeRandomNumberGenerator();
@@ -19,5 +24,12 @@ class BridgeMakerTest {
     void 다리_생성_크기가_20보다_클떄_예외발생(){
         Assertions.assertThatThrownBy(()->bridgeMaker.makeBridge(21))
                 .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void 알맞게_다리가_생성되는지_확인(){
+        BridgeMaker bridgeMakerTest = new BridgeMaker(new TestBridgeGenerator(List.of(1,0,1)));
+        List<String> bridge = bridgeMakerTest.makeBridge(3);
+        Assertions.assertThat(bridge).isEqualTo(List.of("U","D","U"));
     }
 }
