87b2e742988278cf10b9f890e67de2f460a5e592
gnoesnooj <306yyy@naver.com>
2022-11-22 15:14:21 +0800

    test(BridgeMaker) : BridgeMaker 테스트코드 작성

---
+++ src/test/java/bridge/BridgeMakerTest.java
@@ -0,0 +1,19 @@
+package bridge;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import bridge.domain.BridgeMaker;
+import java.util.List;
+import org.junit.jupiter.api.Test;
+
+class BridgeMakerTest {
+
+    private final BridgeNumberGenerator bridgeNumberGenerator = new BridgeRandomNumberGenerator();
+    private final BridgeMaker bridgeMaker = new BridgeMaker(bridgeNumberGenerator);
+
+    @Test
+    void makeBridge() {
+        List<String> bridge = bridgeMaker.makeBridge(5);
+        assertThat(bridge.size()).isEqualTo(5);
+    }
+}
