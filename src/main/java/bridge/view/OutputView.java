a5d5c46e131ef3cbb2c4d47171e5bd452cba5d49
gomljo <gomljo0307@naver.com>
2022-11-22 09:09:20 +0900

    feat(OutputView): 에러 메시지 출력 기능 구현

---
+++ src/main/java/bridge/view/OutputView.java
@@ -0,0 +1,11 @@
+package bridge.view;
+
+import java.io.FilterOutputStream;
+
+/**
+ * 사용자에게 게임 진행 상황과 결과를 출력하는 역할을 한다.
+ */
+public class OutputView {
+    public void printErrorMessage(String errorMessage){System.out.println(errorMessage);}
+    
+}
