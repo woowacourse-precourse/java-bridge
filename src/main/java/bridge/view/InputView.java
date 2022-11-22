f2b553a5ec246c3686cd2686e95905e7a75e8028
yujaemin <woawoa0513@gmail.com>
2022-11-22 16:28:01 +0900

    style: 주석 제거

--- src/main/java/bridge/view/InputView.java
+++ src/main/java/bridge/view/InputView.java
@@ -5,14 +5,8 @@ import static bridge.utils.Validation.validateGameCommand;
 import static bridge.utils.Validation.validateBridgeSize;
 import static bridge.utils.Validation.validateMoving;
 
-/**
- * 사용자로부터 입력을 받는 역할을 한다.
- */
 public class InputView {
 
-    /**
-     * 다리의 길이를 입력받는다.
-     */
     public int readBridgeSize() {
         String input = readLine();
         validateBridgeSize(input);
@@ -20,18 +14,12 @@ public class InputView {
         return Integer.parseInt(input);
     }
 
-    /**
-     * 사용자가 이동할 칸을 입력받는다.
-     */
     public String readMoving() {
         String input = readLine();
         validateMoving(input);
         return input;
     }
 
-    /**
-     * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
-     */
     public String readGameCommand() {
         String input = readLine();
         validateGameCommand(input);
--- src/main/java/bridge/view/OutputView.java
+++ src/main/java/bridge/view/OutputView.java
@@ -4,9 +4,6 @@ import bridge.domain.GameCommand;
 import bridge.domain.Moving;
 import bridge.utils.Converter;
 
-/**
- * 사용자에게 게임 진행 상황과 결과를 출력하는 역할을 한다.
- */
 public class OutputView {
     private static final String PREFIX = "[ ";
     private static final String INFIX = "| ";
