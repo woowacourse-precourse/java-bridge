8ac9fdca1d0ca3d90a0fbfd507b565af73d2d02d
SEONGJOON KIM <jobcho6320@naver.com>
2022-11-22 15:33:16 +0900

    refactor: 게임 실행 메소드 로직 분리
    - 사이즈 입력 하고 난 후 playGame 메소드로 분리 실행되도록 수정
    - run 메소드 명 runGame으로 변경

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -6,7 +6,7 @@ public class Application {
     public static void main(String[] args) {
         try {
             BridgeController bridgeController = new BridgeController();
-            bridgeController.run();
+            bridgeController.runGame();
         } catch (Exception error) {
             System.out.println(error.getMessage());
         }
--- src/main/java/bridge/controller/BridgeController.java
+++ src/main/java/bridge/controller/BridgeController.java
@@ -9,39 +9,56 @@ import bridge.view.OutputView;
 import java.util.List;
 
 public class BridgeController {
+    private static final int BRIDGE_FIRST_INDEX = 0;
+    private static final int DEFAULT_TRY_COUNT = 1;
     private static boolean gameStatus = true;
-    public void run() {
+
+    public void runGame() {
+        OutputView.printGameStartMessage();
+        OutputView.printInputBridgeSizeMessage();
         int bridgeSize = InputView.readBridgeSize();
-        BridgeGame bridgeGame = getBridgeGame(bridgeSize);
-        for (int bridgeIndex = 0; bridgeIndex < bridgeSize; bridgeIndex++) {
+        BridgeGame bridgeGame = createBridgeGame(bridgeSize);
+        playGame(bridgeSize, bridgeGame);
+        OutputView.printBridgeResult(gameStatus, bridgeGame.getTryCount());
+    }
+
+    private static void playGame(int bridgeSize, BridgeGame bridgeGame) {
+        for (int bridgeIndex = BRIDGE_FIRST_INDEX; bridgeIndex < bridgeSize; bridgeIndex++) {
             String moveSide = inputMoveSide(bridgeIndex);
             if (checkMoveSide(bridgeGame, bridgeIndex, moveSide)) continue;
             if (gameStatus(gameStatus, bridgeGame, bridgeIndex)) break;
             bridgeIndex = backToPreviousIndex(bridgeIndex);
         }
-        OutputView.printBridgeResult(gameStatus, bridgeGame.getTryCount());
     }
 
-    private static int backToPreviousIndex(int bridgeIndex) {
-        bridgeIndex--;
-        return bridgeIndex;
-    }
-
-    private BridgeGame getBridgeGame(int bridgeSize) {
-        int tryCount = 1;
+    private BridgeGame createBridgeGame(int bridgeSize) {
         List<String> bridgeMakeResult = generateBridge(bridgeSize);
-        return new BridgeGame(bridgeMakeResult, tryCount);
+        return new BridgeGame(bridgeMakeResult, DEFAULT_TRY_COUNT);
     }
-    
-    private static boolean checkMoveSide(BridgeGame bridgeGame, int bridgeIndex, String moveSide) {
-        if (isMoveSuccessfulOrNot(bridgeGame, bridgeIndex, moveSide)) return true;
-        gameStatus = bridgeGame.retry();
-        return false;
+
+    private List<String> generateBridge(int size) {
+        BridgeRandomNumberGenerator bridgeRandomNumberGenerator = new BridgeRandomNumberGenerator();
+        BridgeMaker bridgeMaker = new BridgeMaker(bridgeRandomNumberGenerator);
+        return bridgeMaker.makeBridge(size);
     }
 
     private static String inputMoveSide(int bridgeIndex) {
         OutputView.replaceCloseBracket(bridgeIndex);
         return InputView.inputMoving();
+    }
+
+    private static boolean gameStatus(boolean gameResult, BridgeGame bridgeGame, int bridgeIndex) {
+        if (gameResult) {
+            bridgeGame.addTryCount();
+            OutputView.goBackBeforeOneStep(bridgeIndex);
+        }
+        return !gameResult;
+    }
+
+    private static boolean checkMoveSide(BridgeGame bridgeGame, int bridgeIndex, String moveSide) {
+        if (isMoveSuccessfulOrNot(bridgeGame, bridgeIndex, moveSide)) return true;
+        gameStatus = bridgeGame.retry();
+        return false;
     }
 
     private static boolean isMoveSuccessfulOrNot(BridgeGame bridgeGame, int bridgeIndex, String moveSide) {
@@ -55,17 +72,8 @@ public class BridgeController {
         return false;
     }
 
-    private static boolean gameStatus(boolean gameResult, BridgeGame bridgeGame, int bridgeIndex) {
-        if (gameResult) {
-            bridgeGame.addTryCount();
-            OutputView.goBackBeforeOneStep(bridgeIndex);
-        }
-        return !gameResult;
-    }
-
-    private List<String> generateBridge(int size) {
-        BridgeRandomNumberGenerator bridgeRandomNumberGenerator = new BridgeRandomNumberGenerator();
-        BridgeMaker bridgeMaker = new BridgeMaker(bridgeRandomNumberGenerator);
-        return bridgeMaker.makeBridge(size);
+    private static int backToPreviousIndex(int bridgeIndex) {
+        bridgeIndex--;
+        return bridgeIndex;
     }
 }
