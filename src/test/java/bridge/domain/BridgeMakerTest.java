2fba19a85f6259ee5ed9e8f2000e4939c2b78176
ddukddaki <rlfrkdms1@naver.com>
2022-11-22 21:18:10 +0900

    test(BridgeMakerTest): size가 주어지면 size길이의 다리를 만들 수 있다

---
+++ src/test/java/bridge/domain/BridgeMakerTest.java
@@ -0,0 +1,22 @@
+package bridge.domain;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+import java.util.List;
+
+class BridgeMakerTest {
+
+    BridgeMaker bridgeMaker = new BridgeMaker(new BridgeRandomNumberGenerator());
+
+    @ParameterizedTest
+    @ValueSource(ints = {3, 5, 6, 7, 10, 13, 18})
+    @DisplayName("size가 주어지면 size 길이의 다리가 만들어진다. ")
+    void createBridge(int size) {
+        List<String> bridge = bridgeMaker.makeBridge(size);
+
+        assertThat(bridge.size()).isEqualTo(size);
+    }
+}
