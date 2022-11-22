16c39c917548fa7f493f2ac646fdd1e5672f11bc
yback1223 <yoyob1223@gmail.com>
2022-11-22 15:23:38 +0900

    refactor: Delete unnecessary part of message
    Closes #47

--- src/main/java/bridge/view/ExceptionMessage.java
+++ src/main/java/bridge/view/ExceptionMessage.java
@@ -1,9 +1,9 @@
 package bridge.view;
 
 public enum ExceptionMessage {
-    WRONG_SIZE_INPUT("[ERROR] 3 ~ 20 사이의 정수를 입력하세요 - FirstInstanceBuilder"),
-    WRONG_UD_INPUT("[ERROR] U나 D를 입력해야 합니다. - UDRQInstanceBuilder"),
-    WRONG_RQ_INPUT("[ERROR] R이나 Q를 입력해야 합니다. - UDRQInstanceBuilder");
+    WRONG_SIZE_INPUT("[ERROR] 3 ~ 20 사이의 정수를 입력하세요."),
+    WRONG_UD_INPUT("[ERROR] U나 D를 입력해야 합니다."),
+    WRONG_RQ_INPUT("[ERROR] R이나 Q를 입력해야 합니다.");
 
     final String message;
 
