d0f855ace66abfe7771c536de767ab5a03e807bc
HyeonjungYun <guswnd9971@gmail.com>
2022-11-22 18:05:57 +0900

    "style(PrintCommon): 출력 메시지 수정"

--- src/main/java/bridge/message/PrintCommon.java
+++ src/main/java/bridge/message/PrintCommon.java
@@ -2,11 +2,11 @@ package bridge.message;
 
 public enum PrintCommon {
 
-    START("다리 건너기 게임을 시작합니다.\n"),
-    INPUT_SIZE("다리의 길이를 입력해주세요."),
+    START("다리 건너기 게임을 시작합니다."),
+    INPUT_SIZE("\n다리의 길이를 입력해주세요."),
     INPUT_MOVING("\n이동할 칸을 선택해주세요. (위: U, 아래: D)"),
-    INPUT_RETRY("게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)"),
-    RESULT("게임 성공 여부: "),
+    INPUT_RETRY("\n게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)"),
+    RESULT("\n게임 성공 여부: "),
     RESULT_NUMBER("총 시도한 횟수: ");
 
     private final String message;
