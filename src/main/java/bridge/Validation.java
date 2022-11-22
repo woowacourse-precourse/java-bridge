885348f637f5a4a520d1c385bd55f225f531202a
gynhyunkim <gyeonghyun.kimm@gmail.com>
2022-11-22 19:17:30 +0900

    feat: 실패 후 사용자가 입력한 커맨드 검증

--- src/main/java/bridge/Validation.java
+++ src/main/java/bridge/Validation.java
@@ -7,8 +7,9 @@ public class Validation {
 
     private static final int BRIDGE_MIN_SIZE = 3;
     private static final int BRIDGE_MAX_SIZE = 20;
-    private static final int MOVING_SIZE = 1;
+    private static final int INPUT_SIZE = 1;
     private static final List<String> MOVING_VALUES = Arrays.asList("U", "D");
+    private static final List<String> COMMAND_VALUES = Arrays.asList("R", "Q");
 
     public static void validateBridgeSize(int size) {
         if (size < BRIDGE_MIN_SIZE || size > BRIDGE_MAX_SIZE) {
@@ -16,9 +17,22 @@ public class Validation {
         }
     }
 
+    public static boolean isValidInput(List<String> values, String input) {
+        int index = values.indexOf(input);
+        if (input.length() > INPUT_SIZE || index == -1) {
+            return false;
+        }
+        return true;
+    }
+
+    public static void validateCommand(String command) {
+        if (!isValidInput(COMMAND_VALUES, command)) {
+            throw new IllegalArgumentException(Error.INVALID_COMMAND.getMessage());
+        }
+    }
+
     public static void validateMoving(String moving) {
-        int isContains = MOVING_VALUES.indexOf(moving);
-        if (moving.length() > MOVING_SIZE || isContains == -1) {
+        if (!isValidInput(MOVING_VALUES, moving)) {
             throw new IllegalArgumentException(Error.INVALID_MOVING.getMessage());
         }
     }
