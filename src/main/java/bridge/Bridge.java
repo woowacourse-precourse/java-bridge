20fbb89e6bfa6427c8010cc8dbd40a6808171e04
youswim <youswim96@gmail.com>
2022-11-22 14:41:58 +0900

    feat(Bridge.java): 기능 3개, 인스턴스 변수 1개 추가
    results배열에 false가 있는지 확인하는 기능,
    restart를 위해 results배열 초기화 하는 기능,
    해당 Bridge에 몇번 시도했는지를 기록하는 tryCount필드 추가,
    tryCount에 1을 더하는 메서드 추가.

--- src/main/java/bridge/Bridge.java
+++ src/main/java/bridge/Bridge.java
@@ -8,6 +8,8 @@ public class Bridge {
     private final List<String> bridge;
     private final List<Boolean> results = new ArrayList<>();
 
+    private int tryCount = 1;
+
     public Bridge(List<String> bridge) {
         this.bridge = bridge;
     }
@@ -17,6 +19,21 @@ public class Bridge {
         results.add(result);
     }
 
+    public boolean isFalseInResults() {
+        for (Boolean result : results) {
+            if(!result)
+                return true;
+        }
+        return false;
+    }
+
+    public void restartBridge() {
+        results.clear();
+    }
+
+    public void plusTryCount() {
+        tryCount++;
+    }
 
     public List<String> getBridge() {
         return bridge;
@@ -24,5 +41,9 @@ public class Bridge {
 
     public List<Boolean> getResults() {
         return results;
+    }
+
+    public int getTryCount() {
+        return tryCount;
     }
 }
