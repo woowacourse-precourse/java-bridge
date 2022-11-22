fb81d91edd7410d6c90a8b4968faeeb6e5a6df1f
NotableDeveloper <castlereal@naver.com>
2022-11-21 22:22:07 -0800

    <test> : OutputView의 게임 결과 출력 관련 테스트 코드 구현 OutputView 클래스의 게임 결과 출력과 관련하여 테스트 코드를 구현함. (게임 결과를 성공적으로 출력하는 지를 검사)

--- src/test/java/bridge/OutputViewTest.java
+++ src/test/java/bridge/OutputViewTest.java
@@ -45,4 +45,27 @@ public class OutputViewTest {
                 "[ O | O ]\n[   |   ]",
                 "[ O | O |   ]\n[   |   | O ]");
     }
+
+    @DisplayName("크기가 3인 다리를 아래, 아래, 위 순서로 성공적으로 이동한 경우의 게임 결과를 출력한다.")
+    @Test
+    void printResultByBridgeSize3() {
+        BridgeNumberGenerator generator = new TestNumberGenerator(newArrayList(1, 1, 0));
+        BridgeMaker bridgeMaker = new BridgeMaker(generator);
+        List<String> bridge = bridgeMaker.makeBridge(3);
+        OutputView outputView = new OutputView(bridge);
+        String[] move = {"D", "D", "U"};
+
+        for(int i = 0; i < bridge.size(); i++)
+            outputView.printMap(i, move[i]);
+
+        outputView.printResult(true, 1);
+
+        assertThat(output()).contains(
+                "최종 게임 결과",
+                "[   |   | O ]",
+                "[ O | O |   ]",
+                "게임 성공 여부: 성공",
+                "총 시도한 횟수: 1"
+        );
+    }
 }
