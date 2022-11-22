27ab86443da59dc78974018e49e2fa12952367b8
Sanmgmin <sangmins930@gmail.com>
2022-11-22 16:08:45 +0900

    style: change ENTER_MOVE and ENTER_RETRY_OR_NOT

--- src/main/java/bridge/View/Constants/OutputConstants.java
+++ src/main/java/bridge/View/Constants/OutputConstants.java
@@ -1,10 +1,12 @@
 package bridge.View.Constants;
 
+import static bridge.View.Constants.InputConstants.*;
+
 public class OutputConstants {
     public static final String GAME_START = "다리 건너기 게임을 시작합니다.";
     public static final String ENTER_LENGTH_OF_BRIDGE = "다리의 길이를 입력해주세요.";
-    public static final String ENTER_MOVE = "이동할 칸을 선택해주세요. (위: U, 아래: D)";
-    public static final String ENTER_RETRY_OR_NOT = "게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)";
+    public static final String ENTER_MOVE = "이동할 칸을 선택해주세요. (위: " + UP + ", 아래: " + DOWN + ")";
+    public static final String ENTER_RETRY_OR_NOT = "게임을 다시 시도할지 여부를 입력해주세요. (재시도: " + RETRY + ", 종료: " + QUIT + ")";
     public static final String SUCCESS_OR_NOT = "게임 성공 여부: ";
     public static final String SUCCESS = "성공";
     public static final String FAIL = "실패";
