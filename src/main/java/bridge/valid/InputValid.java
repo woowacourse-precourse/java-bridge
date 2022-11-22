c1043afc26e4ba5b57de51b87844ceac9d1e0c64
sora5421 <sojeoung536985@gmail.com>
2022-11-22 19:13:30 +0900

    feat : add method that check the user input is in range

--- src/main/java/bridge/valid/InputValid.java
+++ src/main/java/bridge/valid/InputValid.java
@@ -2,7 +2,13 @@ package bridge.valid;
 
 public class InputValid {
     public boolean checkLengthValid(String test) {
-        isNumber(test);
+        try {
+            isNumber(test);
+            isRange(test);
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+            return false;
+        }
         return true;
     }
 
@@ -13,7 +19,14 @@ public class InputValid {
 
     private void isNumber(String test) {
         if (test.matches("[+-]?\\d*(\\.\\d+)?") == false) {
-            throw new IllegalArgumentException("[ERROR] 다리 길이는 하나의 숫자여야 합니다.");
+            throw new IllegalArgumentException("[ERROR] 다리 길이는 하나의 숫자여야 합니다.\n");
+        }
+    }
+
+    private void isRange(String test) {
+        int check = Integer.parseInt(test);
+        if (check < 3 || check > 20) {
+            throw new IllegalArgumentException("[ERROR] 다리 길이는 3부터 20 사이의 숫자여야 합니다.\n");
         }
     }
 }
