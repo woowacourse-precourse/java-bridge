083daf3d38ce421421adbeb036206279e6ed5b23
sypark <soun997@gmail.com>
2022-11-22 17:07:42 +0900

    feat(OutputView): printResult() 메소드 구현(게임 최종 결과를 정해진 형식에 맞춰 출력)

--- src/main/java/bridge/View/OutputView.java
+++ src/main/java/bridge/View/OutputView.java
@@ -7,6 +7,12 @@ import bridge.Model.BridgeResult;
  */
 public class OutputView {
 
+    private static final String MESSAGE_PRINT_RESULT =
+            "최종 게임 결과\n" +
+                    "%s\n\n" +
+                    "게임 성공 여부: %s" +
+                    "총 시도한 횟수: %d";
+
     /**
      * 현재까지 이동한 다리의 상태를 정해진 형식에 맞춰 출력한다.
      * @param bridgeResult 현재까지 이동한 다리의 상태
@@ -18,9 +24,15 @@ public class OutputView {
 
     /**
      * 게임의 최종 결과를 정해진 형식에 맞춰 출력한다.
-     * <p>
+     * @param bridgeResult 현재까지 이동한 다리의 상태
+     * @param result 성공 / 실패 여부
+     * @param tries 시도 횟수
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void printResult() {
+    public void printResult(BridgeResult bridgeResult, String result, int tries) {
+        System.out.println("최종 게임 결과");
+        System.out.println(bridgeResult);
+        System.out.println("게임 성공 여부: " + result);
+        System.out.println("총 시도한 횟수: " + tries);
     }
 }
