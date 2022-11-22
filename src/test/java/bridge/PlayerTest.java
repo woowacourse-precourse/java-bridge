5279a340a8d88bfde81ef78f39a4edcb6ea3dd7f
김태용 <amico741@naver.com>
2022-11-22 22:00:36 +0900

    test: Player의 게임 시도 횟수 테스트 추가

--- src/test/java/bridge/PlayerTest.java
+++ src/test/java/bridge/PlayerTest.java
@@ -2,6 +2,12 @@ package bridge;
 
 import bridge.domain.Player;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.stream.IntStream;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class PlayerTest {
     Player player;
@@ -11,4 +17,12 @@ public class PlayerTest {
         player = new Player();
     }
 
+    @Test
+    @DisplayName("플레이어가 게임을 시도할때마다 시도 횟수를 저장한다.")
+    void 플레이어가_게임을시도하면_횟수를_저장한다() {
+        IntStream.range(0, 10).forEach((idx) -> {
+            player.tryGame();
+            assertThat(player.toString()).isEqualTo(String.valueOf(idx + 1));
+        });
+    }
 }
