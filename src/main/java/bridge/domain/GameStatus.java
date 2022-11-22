fe6ea55975d2e4aa9f4272a24d4ad3dc04a11e9b
yuntaeLee <leeyt1201@gmail.com>
2022-11-22 13:42:53 +0900

    feat: 게임 상태 저장 및 출력 기능 구현

---
+++ src/main/java/bridge/domain/GameStatus.java
@@ -0,0 +1,18 @@
+package bridge.domain;
+
+public enum GameStatus {
+    RESULT_PREFIX("게임 성공 여부: "),
+    SUCCESS("성공"),
+    FAIL("실패"),
+    RUNNING("진행 중");
+
+    private final String status;
+
+    GameStatus(String status) {
+        this.status = status;
+    }
+
+    public String getResult() {
+        return RESULT_PREFIX + status;
+    }
+}
