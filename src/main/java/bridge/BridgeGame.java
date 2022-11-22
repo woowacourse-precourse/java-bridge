0ca25acc72433ef310d2c35545ade1a544f22e74
JohnPrk <uo3641493@gmail.com>
2022-11-22 14:44:09 +0900

    feat(BridgeGame) : 사용자의 게임 재시작 여부를 기준으로 게임을 재시작하게 된다면 bridgeResults 객체에 이전에 했던 결과 되돌리기 요청

--- src/main/java/bridge/BridgeGame.java
+++ src/main/java/bridge/BridgeGame.java
@@ -6,7 +6,6 @@ import java.util.List;
  * 다리 건너기 게임을 관리하는 클래스
  */
 public class BridgeGame {
-    private static final String RESTART = "R";
 
     private final List<String> randomBridge;
     private final BridgeResults bridgeResults;
@@ -37,6 +36,10 @@ public class BridgeGame {
      * 재시작을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
     public boolean retry(boolean command) {
+        if (command) {
+            bridgeResults.revert();
+            return false;
+        }
         return true;
     }
 }
