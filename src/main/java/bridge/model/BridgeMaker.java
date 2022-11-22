4efb3831fb2b93829c83f44c4854cdfd58a5a0b8
SangMinLeeAI <sangmin.lee.ai@gmail.com>
2022-11-22 20:54:00 +0900

    Feat: 다리 만드는 기능
    사용자에게 입력값을 받은 후 이를 이용해여 다리를 생성
    
    Signed-off-by: SangMinLeeAI <sangmin.lee.ai@gmail.com>

--- src/main/java/bridge/model/BridgeMaker.java
+++ src/main/java/bridge/model/BridgeMaker.java
@@ -1,6 +1,8 @@
-package bridge;
+package bridge.model;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * 다리의 길이를 입력 받아서 다리를 생성해주는 역할을 한다.
@@ -18,6 +20,23 @@ public class BridgeMaker {
      * @return 입력받은 길이에 해당하는 다리 모양. 위 칸이면 "U", 아래 칸이면 "D"로 표현해야 한다.
      */
     public List<String> makeBridge(int size) {
+        List<Integer> bridge = new ArrayList<>();
+        for (int i = 0; i < size; i++) {
+            bridge.add(bridgeNumberGenerator.generate());
+        }
+        return bridge.stream()
+                .map(this::convertToBridge)
+                .collect(Collectors.toList());
+
+    }
+
+    private String convertToBridge(Integer integer) {
+        if (integer == 0) {
+            return "U";
+        }
+        if (integer == 1) {
+            return "D";
+        }
         return null;
     }
 }
