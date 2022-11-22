064198b304572fc8e90998eedc6e135ffc646895
starwook <star.wookk@gmail.com>
2022-11-22 21:16:30 +0900

    feat(BridgeMachine): Add validateBridge method
    bridge에 관한 입출력,예외검사까지 하여 bridge를 생성하는 메소드

--- src/main/java/bridge/BridgeMachine.java
+++ src/main/java/bridge/BridgeMachine.java
@@ -5,82 +5,27 @@ public class BridgeMachine {
     private int index = 0;
     private boolean success = false;
 
-    public void initiateBridgeMachine() {
-        BridgeGame bridgeGame = new BridgeGame();
-        OutputView.gameStart();
+
+    private Bridge validateBridge() {
         Bridge bridge;
-        while(true){
-            try{
-                OutputView.inputBridgeSize();
-                String bridgeSize = InputView.readBridgeSize();
-                bridge = new Bridge(bridgeSize);
-                break;
-            }
-            catch(IllegalArgumentException e){
-                OutputView.printError(e.getMessage());
-            }
-
-        }
-        while (true) {
-            String userMoveCommand;
-            while(true){
-                try{
-                    OutputView.inputUserMoveCommand();
-                    userMoveCommand = InputView.readMoving();
-                    break;
-                }
-                catch(IllegalArgumentException e){
-                    OutputView.printError(e.getMessage());
-                }
-
-            }
-            if (!bridgeGame.move(userMoveCommand, bridge, index)) {
-                bridge.changeBridgeMap(index, userMoveCommand, "X");
-                OutputView.printMap(bridge.getBridgeMap(), index);
-                String replay;
-                while(true){
-                    try{
-                        OutputView.inputUserReplayCommand();
-                        replay = InputView.readGameCommand();
-                        break;
-                    }
-                    catch(IllegalArgumentException e){
-                        OutputView.printError(e.getMessage());
-                    }
-                }
-                if (replay.equals(UserCommand.REPLAY.getCommand())) {
-                    bridgeGame.retry(bridge);
-                    totalTry++;
-                    index = 0;
-                } else {
-                    break;
-                }
-            } else {
-                bridge.changeBridgeMap(index, userMoveCommand, "O");
-                OutputView.printMap(bridge.getBridgeMap(), index);
-                index++;
-            }
-            if (index == bridge.getBridgeSize()) {
-                success = true;
-                break;
-            }
-        }
-        if (success) {
-            index--;
-        }
-        printAfterGame(bridge);
+        OutputView.inputBridgeSize();
+        String bridgeSize = InputView.readBridgeSize();
+        bridge = new Bridge(bridgeSize);
+        return bridge;
     }
+
     private void printAfterGame(Bridge bridge) {
         OutputView.printLastBridgeResult();
         OutputView.printMap(bridge.getBridgeMap(), index);
         printGameResult();
         OutputView.printTotalTry(totalTry);
     }
+
     private void printGameResult() {
         if (success == GameResult.FAILED.getTrueOrFalse()) {
             OutputView.printResult(GameResult.FAILED);
         }
-        if(success == GameResult.PASSED.getTrueOrFalse()) {
+        if (success == GameResult.PASSED.getTrueOrFalse()) {
             OutputView.printResult(GameResult.PASSED);
         }
     }
