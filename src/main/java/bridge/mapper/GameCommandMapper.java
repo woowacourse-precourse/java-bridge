dbebf30e6953c5ee1e0592704dc7ee20dbdd0ac9
devMuscle <hongsbien@naver.com>
2022-11-22 17:18:40 +0900

    feat: 사용자의 재시도 여부를 GameCommand로 변경

--- src/main/java/bridge/mapper/GameCommandMapper.java
+++ src/main/java/bridge/mapper/GameCommandMapper.java
@@ -1,2 +1,20 @@
-package bridge.mapper;public class GameCommandMapper {
+package bridge.mapper;
+
+import bridge.enums.GameCommand;
+
+public class GameCommandMapper {
+
+    private static GameCommandMapper instance = new GameCommandMapper();
+
+    public static GameCommandMapper getInstance() {
+        return instance;
+    }
+
+    public GameCommand stringToGameCommand(String input) {
+        if(input.equals("R")) {
+            return GameCommand.Retrial;
+        }
+
+        return GameCommand.Quit;
+    }
 }
