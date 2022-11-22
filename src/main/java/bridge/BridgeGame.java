f88d9dc8589b0e812ce85be146a87d1c960ae5ab
MJ Choi <1621139@gmail.com>
2022-11-22 23:19:10 +0900

    feature bridgegame

--- src/main/java/bridge/BridgeGame.java
+++ src/main/java/bridge/BridgeGame.java
@@ -1,23 +1,92 @@
 package bridge;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * 다리 건너기 게임을 관리하는 클래스
  */
 public class BridgeGame {
+    private int retryCount;
+    private List<Boolean> gameState;
+    private Bridge bridge;
+    private User user;
+    private int bridgeSize;
 
-    /**
-     * 사용자가 칸을 이동할 때 사용하는 메서드
-     * <p>
-     * 이동을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
-     */
-    public void move() {
+    private boolean restart;
+
+    public BridgeGame() {
+        retryCount = 1;
+        restart = true;
     }
 
-    /**
-     * 사용자가 게임을 다시 시도할 때 사용하는 메서드
-     * <p>
-     * 재시작을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
-     */
-    public void retry() {
+    private void gameReset() {
+        gameState.clear();
+        user.getUserLocation().clear();
+    }
+
+    public void gameStart() {
+        OutputView.printStartMessage();
+    }
+
+    public void setBridgeSize() {
+        OutputView.printInputBridgeSizeMessage();
+        bridgeSize = InputView.readBridgeSize();
+
+        bridge = new Bridge(bridgeSize);
+        user = new User(bridgeSize);
+
+        gameState = new ArrayList<>(bridgeSize);
+    }
+
+
+    public boolean move(String direction) {
+        user.move(direction);
+        return (bridge.getBrideState().get(user.getUserLocation().size() - 1).equals(direction));
+    }
+
+    public void playRound() {
+        while (restart) {
+            OutputView.printInputMovingMessage();
+            restart = move(InputView.readMoving());
+            OutputView.printMap(user.getUserLocation(), restart);
+            checkAnswer();
+        }
+    }
+
+
+    public boolean compareBridgeLength() {
+        return bridge.getBrideSize() == user.getUserLocation().size();
+    }
+
+    public void checkAnswer() {
+        if (restart && compareBridgeLength()) {
+            EndGame();
+            restart = false;
+            return;
+        }
+        if (!restart) {
+            OutputView.printInputGameCommandMessage();
+            wrongAnswerPlayAgain();
+        }
+    }
+
+    public void wrongAnswerPlayAgain() {
+        String gameCommand = InputView.readGameCommand();
+        if (gameCommand.equals("R")) {
+            retry(bridgeSize);
+            retryCount += 1;
+            restart = true;
+            return;
+        }
+        EndGame();
+    }
+
+    public void EndGame() {
+        OutputView.printResult(user.getUserLocation(), restart, retryCount);
+    }
+
+    public void retry(int bridgeSize){
+        user = new User(bridgeSize);
     }
 }
