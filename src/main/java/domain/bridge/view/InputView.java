013a7e608abfdf511f9adbf5581fa3c08a52b40e
anso33 <hyeon131812@gmail.com>
2022-11-22 19:00:58 +0900

    [FEAT] add error handling that numberformatException

--- src/main/java/domain/bridge/view/InputView.java
+++ src/main/java/domain/bridge/view/InputView.java
@@ -1,28 +1,48 @@
-package view;
+package domain.bridge.view;
+
+import static camp.nextstep.edu.missionutils.Console.readLine;
 
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
  */
 public class InputView {
 
-    /**
-     * 다리의 길이를 입력받는다.
-     */
-    public int readBridgeSize() {
-        return 0;
+  /**
+   * 다리의 길이를 입력받는다.
+   */
+  public int readBridgeSize() {
+    int size;
+    try {
+      size = Integer.parseInt(readLine());
+    } catch (NumberFormatException e) {
+      throw new IllegalArgumentException("[ERROR] 숫자를 입력하세요!");
     }
+    return size;
+  }
 
-    /**
-     * 사용자가 이동할 칸을 입력받는다.
-     */
-    public String readMoving() {
-        return null;
+  /**
+   * 사용자가 이동할 칸을 입력받는다.
+   */
+  public String readMoving() {
+    String move;
+    try {
+      move = readLine();
+    } catch (NumberFormatException e) {
+      throw new IllegalArgumentException("[ERROR] 문자를 입력하세요!");
     }
+    return move;
+  }
 
-    /**
-     * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
-     */
-    public String readGameCommand() {
-        return null;
+  /**
+   * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
+   */
+  public String readGameCommand() {
+    String play;
+    try {
+      play = readLine();
+    } catch (NumberFormatException e) {
+      throw new IllegalArgumentException("[ERROR] 문자를 입력하세요!");
     }
+    return play;
+  }
 }
