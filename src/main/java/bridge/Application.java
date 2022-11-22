1aeadb7805b2a27536d5aae9a1378bdd12092138
고동우 <shp05179@gmail.com>
2022-11-22 11:46:36 +0900

    feat: 구현한 함수들을 토대로 게임을 진행하는 Application 작성
    main:
    게임을 시작하고, 게임flow에따라 게임을 진행함
    
    initGame:
    길이를 입력받아서 해당 길이를 갖는 새로운 BridgeGame을 생성하여 리턴
    
    gameFlow:
    입력받은 방향으로 움직이고, 움직임의 결과에 따라 적절한 기능 수행

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -1,8 +1,37 @@
 package bridge;
 
+import java.util.List;
+
 public class Application {
 
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        BridgeGame game = initGame();
+        boolean isContinue = true;
+        while (isContinue) {
+            isContinue = gameFlow(game);
+        }
+    }
+
+    private static BridgeGame initGame() {
+        BridgeNumberGenerator randomNumberGenerator = new BridgeRandomNumberGenerator();
+        BridgeMaker bridgeMaker = new BridgeMaker(randomNumberGenerator);
+        OutputView.printStartGame();
+        int size = InputView.readBridgeSizeUntilSuccess();
+        return new BridgeGame(bridgeMaker.makeBridge(size));
+    }
+
+    private static boolean gameFlow(BridgeGame game) {
+        String direction = InputView.readMovingUntilSuccess();
+        boolean isAvailable = game.move(direction);
+        List<String> history = game.getCrossedBridge();
+        OutputView.printMap(history, isAvailable);
+        if (!isAvailable || game.mapSize() == history.size()) {
+            if (isAvailable || InputView.readGameCommandUntilSuccess().equals("Q")) {
+                OutputView.printResult(history, isAvailable, game.getTries());
+                return false;
+            }
+            game.retry();
+        }
+        return true;
     }
 }
