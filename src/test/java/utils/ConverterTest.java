b27c093c5a29461abce562659255f8aead871577
Egenieee <hhgg0925@naver.com>
2022-11-22 16:44:58 +0900

    refactor: 상수 값으로 변경

--- src/test/java/utils/ConverterTest.java
+++ src/test/java/utils/ConverterTest.java
@@ -7,11 +7,13 @@ import static org.assertj.core.api.Assertions.*;
 
 public class ConverterTest {
 
+    private static final int RANDOM_LOWER = 0;
+    private static final int RANDOM_UPPER = 1;
+
     @DisplayName("랜덤 생성 숫자 1을 U로 변환한다.")
     @Test
     void convertRandomNumberOntToUpperBridge() {
-        int randomNumber = 1;
-        String bridge = Converter.convertNumberToBridgeUpOrDown(randomNumber);
+        String bridge = Converter.convertNumberToBridgeUpOrDown(RANDOM_UPPER);
 
         assertThat(bridge).isEqualTo(ConstValue.UPPER_BRIDGE);
     }
@@ -19,8 +21,7 @@ public class ConverterTest {
     @DisplayName("랜덤 생성 숫자 0을 D로 변환한다.")
     @Test
     void convertRandomNumberZeroToLowerBridge() {
-        int randomNumber = 0;
-        String bridge = Converter.convertNumberToBridgeUpOrDown(randomNumber);
+        String bridge = Converter.convertNumberToBridgeUpOrDown(RANDOM_LOWER);
 
         assertThat(bridge).isEqualTo(ConstValue.LOWER_BRIDGE);
     }
