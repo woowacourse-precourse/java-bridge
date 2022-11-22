9a6995376de9902dc6602da11440e56faf17f7af
kawkmin <86940335+kawkmin@users.noreply.github.com>
2022-11-22 14:03:35 +0900

    style: OutPutView를 Enum을 통해서 관리하도록 바꿈

--- src/main/java/bridge/view/OutputView.java
+++ src/main/java/bridge/view/OutputView.java
@@ -22,81 +22,77 @@ public class OutputView {
     public static void printMap(BridgeGame bridgeGame) {
         int bridgeSize = bridgeGame.getUpPresentBridge().size();
 
-        System.out.print("[");
+        System.out.print(PRINT_START_BRIDGE.getMessage());
         for (int i = 0; i < bridgeSize - 1; i++) {
-            if (bridgeGame.getUserMoveList().get(i).equals("U")) {
-                System.out.printf(" %s |", bridgeGame.getUpPresentBridge().get(i));
+            if (bridgeGame.getUserMoveList().get(i).equals(PRINT_U.getMessage())) {
+                System.out.printf(PRINT_CONTINUE_HAVE_BRIDGE.getMessage(), bridgeGame.getUpPresentBridge().get(i));
             } else {
-                System.out.print("   |");
+                System.out.print(PRINT_CONTINUE_BRIDGE.getMessage());
             }
         }
-        if (bridgeGame.getUserMoveList().get(bridgeSize - 1).equals("U")) {
-            System.out.printf(" %s ]\n", bridgeGame.getUpPresentBridge().get(bridgeSize - 1));
+        if (bridgeGame.getUserMoveList().get(bridgeSize - 1).equals(PRINT_U.getMessage())) {
+            System.out.printf(PRINT_END_HAVE_BRIDGE.getMessage(), bridgeGame.getUpPresentBridge().get(bridgeSize - 1));
         } else {
-            System.out.printf("   ]\n");
+            System.out.printf(PRINT_END_BRIDGE.getMessage());
         }
 
-        System.out.print("[");
+        System.out.print(PRINT_START_BRIDGE.getMessage());
         for (int i = 0; i < bridgeSize - 1; i++) {
-            if (bridgeGame.getUserMoveList().get(i).equals("D")) {
-                System.out.printf(" %s |", bridgeGame.getDownPresentBridge().get(i));
+            if (bridgeGame.getUserMoveList().get(i).equals(PRINT_D.getMessage())) {
+                System.out.printf(PRINT_CONTINUE_HAVE_BRIDGE.getMessage(), bridgeGame.getDownPresentBridge().get(i));
             } else {
-                System.out.print("   |");
+                System.out.print(PRINT_CONTINUE_BRIDGE.getMessage());
             }
         }
-        if (bridgeGame.getUserMoveList().get(bridgeSize - 1).equals("D")) {
-            System.out.printf(" %s ]\n", bridgeGame.getDownPresentBridge().get(bridgeSize - 1));
+        if (bridgeGame.getUserMoveList().get(bridgeSize - 1).equals(PRINT_D.getMessage())) {
+            System.out.printf(PRINT_END_HAVE_BRIDGE.getMessage(), bridgeGame.getDownPresentBridge().get(bridgeSize - 1));
         } else {
-            System.out.printf("   ]\n");
+            System.out.printf(PRINT_END_BRIDGE.getMessage());
         }
 
-        if (bridgeGame.getUserMoveList().get(bridgeSize - 1).equals("D")) {
-            if (bridgeGame.getDownPresentBridge().get(bridgeSize - 1).equals("X")) {
+        if (bridgeGame.getUserMoveList().get(bridgeSize - 1).equals(PRINT_D.getMessage())) {
+            if (bridgeGame.getDownPresentBridge().get(bridgeSize - 1).equals(PRINT_X.getMessage())) {
                 throw new IllegalArgumentException();
             }
         } else
-            if (bridgeGame.getUpPresentBridge().get(bridgeSize - 1).equals("X")) {
+            if (bridgeGame.getUpPresentBridge().get(bridgeSize - 1).equals(PRINT_X.getMessage())) {
                 throw new IllegalArgumentException();
             }
         }
 
-    /**
-     * 게임의 최종 결과를 정해진 형식에 맞춰 출력한다.
-     * <p>
-     * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
-     */
     public static void printResult(BridgeGame bridgeGame) {
         System.out.println(OUTPUT_GAME_RESULT_MAP_PRINT_MESSAGE.getMessage());
 
         int bridgeSize = bridgeGame.getUpPresentBridge().size();
 
-        System.out.print("[");
+        System.out.print(PRINT_START_BRIDGE.getMessage());
         for (int i = 0; i < bridgeSize - 1; i++) {
-            if (bridgeGame.getUserMoveList().get(i).equals("U")) {
-                System.out.printf(" %s |", bridgeGame.getUpPresentBridge().get(i));
+            if (bridgeGame.getUserMoveList().get(i).equals(PRINT_U.getMessage())) {
+                System.out.printf(PRINT_CONTINUE_HAVE_BRIDGE.getMessage(), bridgeGame.getUpPresentBridge().get(i));
             } else {
-                System.out.print("   |");
+                System.out.print(PRINT_CONTINUE_BRIDGE.getMessage());
             }
         }
-        if (bridgeGame.getUserMoveList().get(bridgeSize - 1).equals("U")) {
-            System.out.printf(" %s ]\n", bridgeGame.getUpPresentBridge().get(bridgeSize - 1));
+        if (bridgeGame.getUserMoveList().get(bridgeSize - 1).equals(PRINT_U.getMessage())) {
+            System.out.printf(PRINT_END_HAVE_BRIDGE.getMessage(), bridgeGame.getUpPresentBridge().get(bridgeSize - 1));
         } else {
-            System.out.printf("   ]\n");
+            System.out.printf(PRINT_END_BRIDGE.getMessage());
         }
 
-        System.out.print("[");
+        System.out.print(PRINT_START_BRIDGE.getMessage());
         for (int i = 0; i < bridgeSize - 1; i++) {
-            if (bridgeGame.getUserMoveList().get(i).equals("D")) {
-                System.out.printf(" %s |", bridgeGame.getDownPresentBridge().get(i));
+            if (bridgeGame.getUserMoveList().get(i).equals(PRINT_D.getMessage())) {
+                System.out.printf(PRINT_CONTINUE_HAVE_BRIDGE.getMessage(), bridgeGame.getDownPresentBridge().get(i));
             } else {
-                System.out.print("   |");
+                System.out.print(PRINT_CONTINUE_BRIDGE.getMessage());
             }
         }
-        if (bridgeGame.getUserMoveList().get(bridgeSize - 1).equals("D")) {
-            System.out.printf(" %s ]\n", bridgeGame.getDownPresentBridge().get(bridgeSize - 1));
+        if (bridgeGame.getUserMoveList().get(bridgeSize - 1).equals(PRINT_D.getMessage())) {
+            System.out.printf(PRINT_END_HAVE_BRIDGE.getMessage(), bridgeGame.getDownPresentBridge().get(bridgeSize - 1));
         } else {
-            System.out.printf("   ]\n");
+            System.out.printf(PRINT_END_BRIDGE.getMessage());
         }
+
         System.out.printf(OUTPUT_GAME_RESULT_MESSAGE.getMessage(),bridgeGame.getGameSuccess());
         System.out.printf(OUTPUT_TOTAL_GAME_COUNT_MESSAGE.getMessage(),bridgeGame.getGameCount());
     }
--- src/main/java/bridge/view/ViewEnum.java
+++ src/main/java/bridge/view/ViewEnum.java
@@ -5,10 +5,20 @@ public enum ViewEnum {
     INPUT_USER_MOVE_BRIDGE_MESSAGE("이동할 칸을 입력해주세요. (위: U, 아래: D)"),
     INPUT_WANT_RE_GAME_MESSAGE("게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)"),
 
+    PRINT_START_BRIDGE("["),
+    PRINT_CONTINUE_HAVE_BRIDGE(" %s |"),
+    PRINT_CONTINUE_BRIDGE("   |"),
+    PRINT_END_HAVE_BRIDGE(" %s ]\n"),
+    PRINT_END_BRIDGE("   ]\n"),
+    PRINT_D("D"),
+    PRINT_U("U"),
+    PRINT_X("U"),
+
     OUTPUT_GAME_START_MESSAGE("다리 건너기 게임을 시작합니다"),
     OUTPUT_GAME_RESULT_MAP_PRINT_MESSAGE("최종 게임 결과"),
     OUTPUT_GAME_RESULT_MESSAGE("게임 성공 여부: %s\n"),
     OUTPUT_TOTAL_GAME_COUNT_MESSAGE("총 시도한 횟수: %d");
+
 
     private String message;
 
