e40fd96c5e2fcc9ded23eb99335a91327fb59732
CAEL0 <kjongmin26@gmail.com>
2022-11-22 14:14:31 +0900

    refactor(OutputView): 인자명 bridge -> map으로 변경

--- src/main/java/bridge/view/OutputView.java
+++ src/main/java/bridge/view/OutputView.java
@@ -44,8 +44,8 @@ public class OutputView {
      * <p>
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void printMap(List<List<String>> bridge) {
-        bridge.forEach(this::printRow);
+    public void printMap(List<List<String>> map) {
+        map.forEach(this::printRow);
         System.out.println();
     }
 
