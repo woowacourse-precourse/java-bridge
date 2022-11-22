d11663a2ed3980aaf904735d60c4f5bfc01011bc
Mr.Kim <86274253+Joajy@users.noreply.github.com>
2022-11-22 21:12:59 +0900

    refactor(Constant): implement code convention

--- src/main/java/bridge/Constant.java
+++ src/main/java/bridge/Constant.java
@@ -1,56 +1,54 @@
 package bridge;
 
 public class Constant {
-    public static final String Start_Game = "다리 건너기 게임을 시작합니다.";
-    public static final String Select_Location = "\n이동할 칸을 선택해주세요. (위: U, 아래: D)";
-    public static final String Select_Retry = "\n게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)";
-    public static final String Input_Length = "\n다리의 길이를 입력해주세요.";
+    public static final String START_GAME = "다리 건너기 게임을 시작합니다.";
+    public static final String SELECT_LOCATION = "\n이동할 칸을 선택해주세요. (위: U, 아래: D)";
+    public static final String SELECT_RETRY = "\n게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)";
+    public static final String INPUT_LENGTH = "\n다리의 길이를 입력해주세요.";
 
-    public static final String Error_Message = "[ERROR] ";
-    public static final String Err_Bridge_Length = "다리 길이는 3부터 20 사이의 숫자여야 합니다.";
-    public static final String Err_Move = "이동 관련 입력은 U와 D만 사용 가능합니다.";
-    public static final String Err_Retry = "게임 종료 및 재시작 관련 입력은 R와 Q만 사용 가능합니다.";
+    public static final String ERROR_MESSAGE = "[ERROR] ";
+    public static final String ERROR_BRIDGE_LENGTH = "다리 길이는 3부터 20 사이의 숫자여야 합니다.";
+    public static final String ERROR_MOVE = "이동 관련 입력은 U와 D만 사용 가능합니다.";
+    public static final String ERROR_RETRY = "게임 종료 및 재시작 관련 입력은 R와 Q만 사용 가능합니다.";
     
-    public static final String Result_of_game = "\n최종 게임 결과";
-    public static final String Total_Try = "총 시도한 횟수: ";
-    public static final String Result_of_try = "\n게임 성공 여부: ";
-    public static final String Num_Regex = "[1-9][0-9]*";
-    public static final String Front = "[ ";
-    public static final String Back = "]";
-    public static final String Delimiter = "| ";
+    public static final String RESULT_OF_GAME = "\n최종 게임 결과";
+    public static final String TOTAL_TRY = "총 시도한 횟수: ";
+    public static final String RESULT_OF_TRY = "\n게임 성공 여부: ";
+    public static final String NUM_REGEX = "[1-9][0-9]*";
+    public static final String FRONT = "[ ";
+    public static final String BACK = "]";
+    public static final String DELIMITER = "| ";
 
-    public static final String Win = "성공";
-    public static final String Lose = "실패";
+    public static final String WIN = "성공";
+    public static final String LOSE = "실패";
 
-    public static final String Down = "D";
-    public static final String Up = "U";
+    public static final String DOWN = "D";
+    public static final String UP = "U";
 
-    public static final String Restart = "R";
-    public static final String Quit = "Q";
+    public static final String RESTART = "R";
+    public static final String QUIT = "Q";
 
-    public static final String Success = "O";
-    public static final String Fail = "X";
-    public static final String Space = " ";
+    public static final String SUCCESS = "O";
+    public static final String FAIL = "X";
+    public static final String SPACE = " ";
 
     public static String Select_Move(){
-        return Select_Location;
+        return SELECT_LOCATION;
     }
 
     public static String Select_Replay(){
-        return Select_Retry;
+        return SELECT_RETRY;
     }
 
     public static String Length_Restrict(){
-        return Error_Message + Err_Bridge_Length;
+        return ERROR_MESSAGE + ERROR_BRIDGE_LENGTH;
     }
     
     public static String Move_Restrict(){
-        return Error_Message + Err_Move;
-    }
-    
+        return ERROR_MESSAGE + ERROR_MOVE;
+    }    
 
     public static String Replay_Restrict(){
-        return Error_Message + Err_Retry;
+        return ERROR_MESSAGE + ERROR_RETRY;
     }
-
 }
