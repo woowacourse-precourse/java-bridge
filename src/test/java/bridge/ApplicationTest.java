4595c1c6fbf20a81219959f98e0886d6b5764d96
twknds <twknds@naver.com>
2022-11-22 16:32:16 +0900

    test($ApplicationTest): 전체 기능 동작 테스트 추가
    R 재작동 테스트, Q 종료 테스트, 20개 테스트 추가

--- src/test/java/bridge/ApplicationTest.java
+++ src/test/java/bridge/ApplicationTest.java
@@ -39,7 +39,57 @@ class ApplicationTest extends NsTest {
             assertThat(upSideIndex).isLessThan(downSideIndex);
         }, 1, 0, 1);
     }
+    @Test
+    void 기능_테스트2() {
+        assertRandomNumberInRangeTest(() -> {
+            run("3", "U", "D", "D","R","U","D","U");
+            assertThat(output()).contains(
+                    "최종 게임 결과",
+                    "[ O |   | O ]",
+                    "[   | O |   ]",
+                    "게임 성공 여부: 성공",
+                    "총 시도한 횟수: 2"
+            );
 
+            int upSideIndex = output().indexOf("[ O |   | O ]");
+            int downSideIndex = output().indexOf("[   | O |   ]");
+            assertThat(upSideIndex).isLessThan(downSideIndex);
+        }, 1, 0, 1);
+    }
+    @Test
+    void 기능_테스트3() {
+        assertRandomNumberInRangeTest(() -> {
+            run("20", "U", "D", "U","D","U", "D", "U","D","U", "D", "U","D","U", "D", "U","D","U", "D", "U","D");
+            assertThat(output()).contains(
+                    "최종 게임 결과",
+                    "[ O |   | O |   | O |   | O |   | O |   | O |   | O |   | O |   | O |   | O |   ]",
+                    "[   | O |   | O |   | O |   | O |   | O |   | O |   | O |   | O |   | O |   | O ]",
+                    "게임 성공 여부: 성공",
+                    "총 시도한 횟수: 1"
+            );
+
+            int upSideIndex = output().indexOf("[ O |   | O |   | O |   | O |   | O |   | O |   | O |   | O |   | O |   | O |   ]");
+            int downSideIndex = output().indexOf("[   | O |   | O |   | O |   | O |   | O |   | O |   | O |   | O |   | O |   | O ]");
+            assertThat(upSideIndex).isLessThan(downSideIndex);
+        }, 1, 0, 1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0);
+    }
+    @Test
+    void 기능_테스트4() {
+        assertRandomNumberInRangeTest(() -> {
+            run("3", "U", "D", "D","Q");
+            assertThat(output()).contains(
+                    "최종 게임 결과",
+                    "[ O |   |   ]",
+                    "[   | O | X ]",
+                    "게임 성공 여부: 실패",
+                    "총 시도한 횟수: 1"
+            );
+
+            int upSideIndex = output().indexOf("[ O |   |   ]");
+            int downSideIndex = output().indexOf("[   | O | X ]");
+            assertThat(upSideIndex).isLessThan(downSideIndex);
+        }, 1, 0, 1);
+    }
     @Test
     void 예외_테스트() {
         assertSimpleTest(() -> {
