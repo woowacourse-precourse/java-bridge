e7c8a3567a94b2de2f1b490fb1493d8ad6193534
Juuuic <sn990908a@gmail.com>
2022-11-22 21:08:26 +0900

    feat: GameTurn() 게임 진행 루틴 기능 구현

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -6,33 +6,16 @@ import java.util.List;
 public class Application {
 
     public static void main(String[] args) {
-        InputView inputView = new InputView();
-        BridgeException bridgeException = new BridgeException();
-        BridgeRandomNumberGenerator bridgeRandomNumberGenerator = new BridgeRandomNumberGenerator();
-        BridgeMaker bridgeMaker = new BridgeMaker(bridgeRandomNumberGenerator);
-        BridgeGame bridgeGame = new BridgeGame();
-        OutputView outputView = new OutputView();
-
-
-        System.out.print("다리 건너기 게임을 시작합니다.");
 
         // 다리 길이 입력 -> inputView 클래스의 readBridgeSize()
-        int inputLength = inputView.readBridgeSize();
-        int bridgeLength = bridgeException.bridgeInput(inputLength);
 
         // 다리 길이만큼 다리 생성 -> BridgeMaker 클래스의 makeBridge(int size)
-        List<String> bridge = bridgeMaker.makeBridge(bridgeLength);
 
         // 이동할 칸 입력받기 -> InputView 클래스의 readMoving()
-        String direction = inputView.readMoving();
-        bridgeException.UserInput(direction);
 
         // 입력받은 값에 따라 이동 -> BridgeGame 클래스의 move()
-        List<List<String>> resultBridge = new ArrayList<>();
-        resultBridge = bridgeGame.move(bridge,direction,0);
 
         // 이동한 후 다리 출력 -> OutputView 클래스의 printMap()
-        outputView.printMap(resultBridge);
         // 성공 시 -> OutputView 클래스의 printResult() -> 종료
         // 실패 시 재시작 여부 입력 받음-> InputView 클래스의 readGameCommand()
         // 종료 시 -> OutputView 클래스의 printResult() -> 종료
--- src/main/java/bridge/GameTurn.java
+++ src/main/java/bridge/GameTurn.java
@@ -1,4 +1,21 @@
 package bridge;
 
 public class GameTurn {
+
+    public static void BridgeGameStart(){
+        //게임 시작
+    }
+
+    public static void BridgeGameTurn(){
+        //한 게임 안에서 반복되는 게임 순서들
+        //한번 생성한 다리로 계속 진행
+    }
+
+    public static void getBridgeLength(){
+        //다리 길이 입력 받고 예외 처리까지
+    }
+
+    public static void ExceptionBridgeDirection(){
+        //이동할 방향 예외 처리
+    }
 }
