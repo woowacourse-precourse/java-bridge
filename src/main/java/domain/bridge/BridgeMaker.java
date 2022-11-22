20da40261f2f9e67107a619de07c068f4e07b3d0
anso33 <hyeon131812@gmail.com>
2022-11-22 17:03:54 +0900

    [FEAT] function to make answer bridge

---
+++ src/main/java/domain/bridge/BridgeMaker.java
@@ -0,0 +1,38 @@
+package domain.bridge;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 다리의 길이를 입력 받아서 다리를 생성해주는 역할을 한다.
+ */
+public class BridgeMaker {
+
+  private final BridgeNumberGenerator bridgeNumberGenerator;
+
+  public BridgeMaker(BridgeNumberGenerator bridgeNumberGenerator) {
+    this.bridgeNumberGenerator = bridgeNumberGenerator;
+  }
+
+  /**
+   * @param size 다리의 길이
+   * @return 입력받은 길이에 해당하는 다리 모양. 위 칸이면 "U", 아래 칸이면 "D"로 표현해야 한다.
+   */
+  public List<String> makeBridge(int size) {
+    List<String> bridge = new ArrayList<>();
+    for (int i = 0; i < size; i++) {
+      addBridgeValue(bridge);
+    }
+    return bridge;
+  }
+
+  private void addBridgeValue(List<String> bridge) {
+    int value = bridgeNumberGenerator.generate();
+    if (value == 0) {
+      bridge.add("D");
+    }
+    if (value == 1) {
+      bridge.add("U");
+    }
+  }
+}
