0b4a7560bca6e76892be9e69cc0b461134312858
jay <jaewhan232@naver.com>
2022-11-22 22:07:05 +0900

    Implement : 출력 메세지 포맷 OutputView 클래스의 상수로 변경

--- src/main/java/bridge/OutputView.java
+++ src/main/java/bridge/OutputView.java
@@ -9,6 +9,10 @@ package bridge;
  */
 public class OutputView {
 
+    private static final String startMessage = "다리 건너기 게임을 시작합니다.";
+    private static final String gameResultMessage = "\n최종 게임 결과";
+    private static final String whetherSuccessMessage = "\n게임 성공 여부: %s";
+    private static final String numberOfTrialMessage = "총 시도한 횟수: %d";
     /**
      * 현재까지 이동한 다리의 상태를 정해진 형식에 맞춰 출력한다.
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
