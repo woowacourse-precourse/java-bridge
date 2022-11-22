4d79ecda6debf1485c8ca7a7660e34b5bdb3b70f
Giho <poqw741@gmail.com>
2022-11-22 18:39:12 +0900

    feat(controller) : 컨트롤러 추가

--- src/main/java/bridge/controller/BridgeController.java
+++ src/main/java/bridge/controller/BridgeController.java
@@ -19,7 +19,12 @@ public class BridgeController {
     }
 
     public void start(){
+        this.outputView.announceStartGame();
         createBridgeGame();
+        do{
+            playTurn();
+        }while(!this.bridgeGame.isEnd());
+        announceFinalResult();
     }
 
     private void createBridgeGame(){
@@ -27,6 +32,24 @@ public class BridgeController {
         BridgeMaker bridgeMaker = new BridgeMaker(new BridgeRandomNumberGenerator());
         List<String> bridge = bridgeMaker.makeBridge(inputSize);
         this.bridgeGame = new BridgeGame(bridge);
+    }
+
+    private void announceFinalResult() {
+        boolean isSuccess = this.bridgeGame.isSuccess();
+        this.outputView.announceEndGame();
+        String bridgePicture = this.bridgeGame.getPicture();
+        int tryCount = this.bridgeGame.getTryCount();
+        this.outputView.printResult(isSuccess, tryCount);
+    }
+
+    private void playTurn(){
+        String moving = this.inputView.readMoving();
+        String bridgePicture = this.bridgeGame.move(moving);
+        this.outputView.printMap(bridgePicture);
+        if(this.bridgeGame.isFail()){
+            String gameCommand = this.inputView.readGameCommand();
+            this.bridgeGame.retry(gameCommand);
+        }
     }
 
 }
