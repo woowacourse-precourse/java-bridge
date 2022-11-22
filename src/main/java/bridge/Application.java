a04ae8fbae9e8d39a6e18c7d9c3bac0a6c0115e5
fnzl54 <rnjscksdud1@gmail.com>
2022-11-22 19:35:35 +0900

    refactor: separate execution statement

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -1,58 +1,10 @@
 package bridge;
 
-import java.util.ArrayList;
-import java.util.List;
 public class Application {
-
-    private static List<String> bridgeNumberList = new ArrayList<>();
-    static InputView userInput = new InputView();
-    static OutputView display = new OutputView();
-    static BridgeGame Game = new BridgeGame();
     public static void main(String[] args) {
         // TODO: 프로그램 구현
-        run();
-    }
-
-    private static void run() {
-        gameSetting();
-
-        while (!gameRun()) {
-            OutputView.getRetryMessage();
-            if (userInput.readGameCommand().equals("Q")) {
-                break;
-            }
-            Game.retry();
-        }
-        printTotalResult(Game.getCount());
-    }
-    private static void gameSetting() {
-        OutputView.getStartMessage();
-        OutputView.getInputSizeMessage();
-        BridgeRandomNumberGenerator gameRandomNumber = new BridgeRandomNumberGenerator();
-        BridgeMaker makeBridge = new BridgeMaker(gameRandomNumber);
-
-        int bridgeSize = userInput.readBridgeSize();
-
-        bridgeNumberList = makeBridge.makeBridge(bridgeSize);
-    }
-
-    private static boolean gameRun() {
-        for (int i = 0; i < bridgeNumberList.size(); i++) {
-            OutputView.getMoveMessage();
-            String moveUpOrDown = userInput.readMoving();
-            boolean state = Game.move(bridgeNumberList.get(i), moveUpOrDown);
-            display.printMap(Game.getBridgeLineOne(), Game.getBridgeLineTwo());
-            if (state == false) {
-                return false;
-            }
-        }
-        return true;
-    }
-
-    private static void printTotalResult(int count) {
-        OutputView.getEndMessage();
-        display.printMap(Game.getBridgeLineOne(), Game.getBridgeLineTwo());
-        display.printResult(Game.getNowState(), count);
+        BridgeGame Game = new BridgeGame();
+        Run.run(Game);
     }
 
 }
--- src/main/java/bridge/Run.java
+++ src/main/java/bridge/Run.java
@@ -1,4 +1,52 @@
 package bridge;
-public  class Run {
 
+import java.util.ArrayList;
+import java.util.List;
+
+public  class Run {
+    private static List<String> bridgeNumberList = new ArrayList<>();
+    static InputView userInput = new InputView();
+    static OutputView display = new OutputView();
+
+    public static void run(BridgeGame Game) {
+        gameSetting();
+
+        while (!gameRun(Game)) {
+            OutputView.getRetryMessage();
+            if (userInput.readGameCommand().equals("Q")) {
+                break;
+            }
+            Game.retry();
+        }
+        printTotalResult(Game, Game.getCount());
+    }
+    private static void gameSetting() {
+        OutputView.getStartMessage();
+        OutputView.getInputSizeMessage();
+        BridgeRandomNumberGenerator gameRandomNumber = new BridgeRandomNumberGenerator();
+        BridgeMaker makeBridge = new BridgeMaker(gameRandomNumber);
+
+        int bridgeSize = userInput.readBridgeSize();
+
+        bridgeNumberList = makeBridge.makeBridge(bridgeSize);
+    }
+
+    private static boolean gameRun(BridgeGame Game) {
+        for (int i = 0; i < bridgeNumberList.size(); i++) {
+            OutputView.getMoveMessage();
+            String moveUpOrDown = userInput.readMoving();
+            boolean state = Game.move(bridgeNumberList.get(i), moveUpOrDown);
+            display.printMap(Game.getBridgeLineOne(), Game.getBridgeLineTwo());
+            if (state == false) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private static void printTotalResult(BridgeGame Game, int count) {
+        OutputView.getEndMessage();
+        display.printMap(Game.getBridgeLineOne(), Game.getBridgeLineTwo());
+        display.printResult(Game.getNowState(), count);
+    }
 }
