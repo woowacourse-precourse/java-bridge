f72302aeca2949084aed99352c60e4d3f62376f8
josolha <josolha@nate.com>
2022-11-22 20:47:39 +0900

    chore(BridgeGame) : 주석 삭제

--- src/main/java/bridge/service/BridgeGame.java
+++ src/main/java/bridge/service/BridgeGame.java
@@ -4,25 +4,12 @@ import bridge.BridgeMaker;
 import bridge.BridgeRandomNumberGenerator;
 import java.util.List;
 
-/**
- * 다리 건너기 게임을 관리하는 클래스
- */
 public class BridgeGame {
-
-    private static int location;
-//    private static List<String> builtbridge;
-
-
-    /**
-     * 사용자가 칸을 이동할 때 사용하는 메서드
-     * <p>
-     * 이동을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
-     */
 
     public List<String> makeBridge(int bridgeSize) {
         BridgeMaker bridgeMaker = new BridgeMaker(new BridgeRandomNumberGenerator());
         List<String> builtbridge = bridgeMaker.makeBridge(bridgeSize);
-//        System.out.println(builtbridge);
+        System.out.println(builtbridge);
         return builtbridge;
     }
 
@@ -34,11 +21,6 @@ public class BridgeGame {
         return false;
     }
 
-    /**
-     * 사용자가 게임을 다시 시도할 때 사용하는 메서드
-     * <p>
-     * 재시작을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
-     */
     public boolean retry(String command) {
 
         if (command.equals("Q")) {
