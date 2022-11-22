96ce7654a9e9196be9c4f2e252ae9d2df50b5f3b
안광민 <creativeduck0518@gmail.com>
2022-11-22 10:46:56 +0900

    feat(GamePlay): 전체 게임 플레이 클래스 생성

---
+++ src/main/java/bridge/GamePlay.java
@@ -0,0 +1,13 @@
+package bridge;
+
+import bridge.presentation.InputView;
+import bridge.presentation.OutputView;
+
+public class GamePlay {
+    private InputView inputView;
+    private OutputView outputView;
+    private BridgeGame bridgeGame;
+    private int tryCount;
+    private final boolean WON = true;
+    private final boolean FAILURE = false;
+}
