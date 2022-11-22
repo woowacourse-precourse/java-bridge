e134c065dfa5ad91cb55ec5f591afd9ae9c4ee50
YUSEONJUN <seonjun0906@gmail.com>
2022-11-22 19:32:23 +0900

    feat : 문자열 및 상수화 변수 정의

---
+++ src/main/java/bridge/Constants.java
@@ -0,0 +1,27 @@
+package bridge;
+
+public class Constants {
+	public static final String ERROR = "[ERROR]";
+	public static final String ERROR_NUMBER_PATTERN = ERROR + " 숫자로만 입력되지 않았습니다.";
+	public static final String ERROR_NUMBER_RANGE = ERROR + " 범위 내 숫자가 입력되지 않았습니다.";
+	public static final String ERROR_ALPHABET_INPUT = ERROR + " 알파벳으로 입력되지 않았습니다.";
+	public static final String ERROR_U_D_INPUT = ERROR + " U또는 D만 입력할 수 있습니다.";
+	public static final String ERROR_R_Q_INPUT = ERROR + " R또는 Q만 입력할 수 있습니다.";
+	public static final String GAME_FAIL = "게임 성공 여부: 실패";
+	public static final String TOTAL_COUNT = "총 시도한 횟수: ";
+	public static final String TOTAL_RESULT = "최종 게임 결과";
+	public static final String GAME_SUCCESS = "게임 성공 여부: 성공";
+	public static final String BRIDGE_GAME_START = "다리 건너기 게임을 시작합니다.";
+	public static final String INPUT_BRIDGE_SIZE = "다리의 길이를 입력해주세요.";
+	public static final String CHOOSE_UP_DOWN = "이동할 칸을 선택해주세요. (위: U, 아래: D)";
+	public static final String CHOOSE_R_Q = "게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)";
+
+	public static final String ALPHABET_R = "R";
+	public static final String ALPHABET_Q = "Q";
+	public static final String ALPHABET_U = "U";
+	public static final String ALPHABET_D = "D";
+
+	public static final int NUMBER_THREE = 3;
+	public static final int NUMBER_TWENTY = 20;
+
+}
