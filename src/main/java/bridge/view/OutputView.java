6cc57315b31d88e5974ba6115583db83dcd37393
DavinAn <blueapple1030@gmail.com>
2022-11-22 22:20:03 +0900

    feat (view) : update printResult, prints map, game result, and number of attempts

--- src/main/java/bridge/view/OutputView.java
+++ src/main/java/bridge/view/OutputView.java
@@ -58,16 +58,24 @@ public class OutputView {
 
 
 
-
-
-
     /**
      * 게임의 최종 결과를 정해진 형식에 맞춰 출력한다.
      * <p>
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void printResult() {
+    public void printResult(List<String> bridge, boolean gameResult, int gameCount) {
+        System.out.println("최종 게임 결과");
+        this.printMap(bridge, gameResult);
+        String result = "실패";
+        if (gameResult) {
+            result = "성공";
+        }
+        System.out.println("\n게임 성공 여부: " + result + "\n총 시도한 횟수: " + gameCount);
     }
+
+
+
+
 
     public String toString(List<String> bridge) {
         return "[ " + String.join(" | ", bridge) + " ]";
