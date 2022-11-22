08f4d1d1723fe5293f60cdad415af10f25d09002
chj <bluedog129@gmail.com>
2022-11-22 20:27:14 +0900

    feat(ExceptionController) : 다리 길이에 대한 예외처리 구현

--- src/main/java/bridge/controller/ExceptionController.java
+++ src/main/java/bridge/controller/ExceptionController.java
@@ -4,6 +4,8 @@ import bridge.model.message.Message;
 import bridge.model.message.ErrorMessage;
 
 public class ExceptionController {
+    private static final int BRIDGE_SIZE_MIN = 3;
+    private static final int BRIDGE_SIZE_MAX = 20;
     public ExceptionController() {
     }
 
@@ -20,6 +22,11 @@ public class ExceptionController {
         if (!isValidMoving(input)) {
             String message = ErrorMessage.ERROR_PREFIX.getErrorMessage() + ErrorMessage.ERROR_MOVING.getErrorMessage();
             throw new IllegalArgumentException(message);
+        }
+    }
+    private void isValidRange(int size) {
+        if (!(BRIDGE_SIZE_MIN <= size && size <= BRIDGE_SIZE_MAX)) {
+            throw new IllegalArgumentException();
         }
     }
 
