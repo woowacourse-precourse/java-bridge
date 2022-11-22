9e4d17cd669c9c7f53d2d7e383f0f817cdcb90fd
JuSeong1130 <jsmoon1130@naver.com>
2022-11-22 11:00:51 +0900

    feat(BridgeMoveCommand): 다리 이동 유형 Enum 선언 및 기능 구현
    0,1 입력시 알맞는 Column return 기능 구현

---
+++ src/main/java/bridge/domain/command/BridgeMoveCommand.java
@@ -0,0 +1,28 @@
+package bridge.domain.command;
+
+import java.util.Arrays;
+
+public enum BridgeMoveCommand {
+	UP("U",1),
+	DOWN("D",0);
+	private String column;
+	private int number;
+
+
+	BridgeMoveCommand(String column, int number) {
+		this.column = column;
+		this.number = number;
+	}
+
+	public static String getBridgeColumn(int number) {
+		return Arrays.stream(values())
+				.filter(bridgeColumn -> bridgeColumn.number == number)
+				.findAny()
+				.orElseThrow(IllegalArgumentException::new)
+				.getColumn();
+	}
+
+	public String getColumn() {
+		return column;
+	}
+}
