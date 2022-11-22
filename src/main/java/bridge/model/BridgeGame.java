17e9654c8892a8ac8d6db8e8c91b1f08f780a9a3
lvalentine6 <ysr1227@naver.com>
2022-11-22 18:18:20 +0900

    test : Comparison 테스트 코드 구현

--- src/main/java/bridge/model/BridgeGame.java
+++ src/main/java/bridge/model/BridgeGame.java
@@ -8,7 +8,7 @@ import java.util.List;
  */
 public class BridgeGame {
     public static int moveIndex = 0;
-    static List<String> bridgeResult = GameController.bridgeResult;
+    static List<String> bridgeResult;
 
     /**
      * 사용자가 칸을 이동할 때 사용하는 메서드
@@ -16,6 +16,7 @@ public class BridgeGame {
      * 이동을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
     public static boolean move(String moveInput) {
+        bridgeResult = GameController.bridgeResult;
         return !Comparison.comparisonSpace(moveInput, bridgeResult);
     }
 
--- src/test/java/bridge/model/ComparisonTest.java
+++ src/test/java/bridge/model/ComparisonTest.java
@@ -1,10 +1,18 @@
 package bridge.model;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.ArrayList;
+import java.util.List;
+import org.junit.jupiter.api.Test;
+
 class ComparisonTest {
 
-//    @Test
-//    void comparisonSpace() {
-//        String input = "U";
-//        assertTrue(Comparison.comparisonSpace(input, new ArrayList<>(List.of("U", "D", "D"))));
-//    }
+    @Test
+    void comparisonSpace() {
+        String input = "U";
+        assertTrue(Comparison.comparisonSpace(input, new ArrayList<>(List.of("U", "D", "D"))));
+        System.out.println(BridgeGame.bridgeResult);
+        System.out.println(BridgeGame.moveIndex);
+    }
 }
