b168e41286cef5334bdc60a9bcde9a46eb98a972
ParkJoungWoo <jug78@naver.com>
2022-11-22 15:09:26 +0900

    refactor(ui/OutputView.java) : 출력 기능 리팩토링
     map 구조 변경에 따른 출력 기능 리팩토링
    
    - map의 String화에 따른 출력 방식 변경
    - 최종 맵 출력 방식 수정

--- src/main/java/ui/OutputView.java
+++ src/main/java/ui/OutputView.java
@@ -9,32 +9,14 @@ import java.util.Map;
 public class OutputView {
     private final String GAME_START_MESSAGE = "다리 건너기 게임을 시작합니다.";
     private final String GAME_SUCCESS_RESULT = "게임 성공 여부: ";
-    private final String BLOCK_START = "[";
-    private final String BLOCK_END = "]";
-    private final String BLOCK_SEP = "|";
     // 게임 성공, 실패는 ENUM으로 관리하도록 고려할 것.
     private final Map<Boolean, String> GAME_RESULT = Map.of(true, "성공",
             false, "실패");
     private final String GAME_TRY = "총 시도한 횟수: ";
     private final String FINAL_GAME_MAP = "최종 게임 결과";
 
-    public void printMap(List<List<Integer>> map) {
-        printOneLine(map.get(0));
-        printOneLine(map.get(1));
-        System.out.println();
-    }
-    private void printOneLine(List<Integer> line) {
-        System.out.print(BLOCK_START);
-        for (int i = 0; i < line.size(); i++) {
-            printBlock(line.get(i));
-            if (i != line.size() - 1) {
-                System.out.print(BLOCK_SEP);
-            }
-        }
-        System.out.println(BLOCK_END);
-    }
-    private void printBlock(final int index) {
-        System.out.print(" " + Block.getMessage(index) + " ");
+    public void printMap(String map) {
+        System.out.println(map);
     }
 
     public void printResult(final boolean result) {
@@ -51,7 +33,7 @@ public class OutputView {
     private void printFinalGameMapMessage(){
         System.out.println(FINAL_GAME_MAP);
     }
-    public void printFinalGameMap(List<List<Integer>> map){
+    public void printFinalGameMap(String map){
         printFinalGameMapMessage();
         printMap(map);
     }
