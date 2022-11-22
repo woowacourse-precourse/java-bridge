e0760924fbab836fb679b8e7fcd30df8feddd000
Ran <lan4250@naver.com>
2022-11-22 22:00:05 +0900

    REFACTOR/ BridgeMaker 메서드 10줄 이내로 리팩토링

--- src/main/java/bridge/BridgeMaker.java
+++ src/main/java/bridge/BridgeMaker.java
@@ -1,6 +1,7 @@
 package bridge;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 /**
@@ -8,14 +9,131 @@ import java.util.List;
  */
 public class BridgeMaker {
 
-
+    private static String up = "";
+    private static String down = "";
     private static final String errorMsg = "[ERROR]";
     private static String bridgeStr = "";
-
+    private char tmpChar = ' ';
+    private boolean isFinalIdx = false;
+    private int[][] tmpArr;
+    private static List<String> resultBridge = new ArrayList<>();
     private final BridgeNumberGenerator bridgeNumberGenerator;
+    private  List<String> res = new ArrayList<>();
+
 
     public BridgeMaker(BridgeNumberGenerator bridgeNumberGenerator) {
         this.bridgeNumberGenerator = bridgeNumberGenerator;
+    }
+
+
+    public void initialSetting() {
+        resultBridge = new ArrayList<>();
+        up = "[";
+        down = "[";
+    }
+
+    public void finalSetting() {
+        up += "]";
+        down += "]";
+        resultBridge.add(up);
+        resultBridge.add(down);
+    }
+
+    // String 00111010101  => List<String> 형태로 바꾼다.
+    public List<String> changeStrToBridge(String inputBridgeStr) {
+        initialSetting();
+        recursiveFor(inputBridgeStr, inputBridgeStr.length());
+        finalSetting();
+        return resultBridge;
+    }
+
+    public void recursiveFor(String inputBridgeStr, int length){
+
+        for (int i = 0; i < inputBridgeStr.length(); i++) {
+            tmpChar = inputBridgeStr.charAt(i);
+            isFinalIdx = (i == inputBridgeStr.length() - 1);
+            if (tmpChar == 'x' || tmpChar == 'y') {failureHandler(tmpChar, isFinalIdx);
+                break;
+            }
+            successHandler(tmpChar, isFinalIdx);
+        }
+
+    }
+
+    public void upSuccess(boolean isFinalIdx) {
+
+        if (isFinalIdx) { // 업
+            up += " O ";
+            down += "   ";
+            return;
+        }
+        up += " O |";
+        down += "   |";
+    }
+
+    public void downSuccess(boolean isFinalIdx) {
+        if (isFinalIdx) { // 다운
+            up += "   ";
+            down += " O ";
+            return;
+        }
+        up += "   |";
+        down += " O |";
+    }
+
+    public void upFail(boolean isFinalIdx) {
+        if (isFinalIdx) {
+            up += " X ";
+            down += "   ";
+            return;
+        }
+        up += " X |";
+        down += "   |";
+    }
+
+    public void downFail(boolean isFinalIdx) {
+        if (isFinalIdx) {
+            up += "   ";
+            down += " X ";
+            return;
+        }
+        up += "   |";
+        down += " X |";
+    }
+
+    public void successHandler(char tmpChar, boolean isFinalIdx) {
+        if (tmpChar == '1') {
+            upSuccess(isFinalIdx);
+        } else if (tmpChar == '0') {
+            downSuccess(isFinalIdx);
+        }
+    }
+
+    public void failureHandler(char tmpChar, boolean isFinalIdx) {
+        if (tmpChar == 'x') {
+            upFail(isFinalIdx);
+        } else if (tmpChar == 'y') {
+            downFail(isFinalIdx);
+        }
+    }
+
+    public void makeArr() {
+
+        int[][] tmpArr = new int[2][bridgeStr.length()];
+        for (int i = 0; i < bridgeStr.length(); i++) {
+            if (bridgeStr.charAt(i) == '0') {       //  0인 경우 아래 칸 건널 수 있다.
+                tmpArr[1][i] = 1;
+            } else if (bridgeStr.charAt(i) == '1') {    // 0인 경우 위 칸이 건널 수 있다.
+                tmpArr[0][i] = 1;
+            }
+        }
+    }
+
+    public void printArr() {
+        System.out.println("BridgeMaker.makeBridge() arr 타입");
+        for (int[] a : tmpArr) {
+            System.out.println(Arrays.toString(a));
+        }
     }
 
     /**
@@ -24,33 +142,14 @@ public class BridgeMaker {
      * <p>
      */
     public List<String> customMakeBridge(int size) {
-
         if (size < 3 || size > 20) {
-            System.out.println(errorMsg + " 3 이상 20 이하의 숫자만 입력 가능합니다.");
             throw new IllegalArgumentException(errorMsg + " 3 이상 20 이하의 숫자만 입력 가능합니다.");
         }
-
         bridgeStr = "";     // while문에 들어가기 때문에  초기화 꼭 필요
-
         for (int i = 0; i < size; i++) {
             bridgeStr += bridgeNumberGenerator.generate();
         }
-//        System.out.println(" 다리 " + bridgeStr);
-
-        int[][] arr = new int[2][bridgeStr.length()];
-        for (int i = 0; i < bridgeStr.length(); i++) {
-            if (bridgeStr.charAt(i) == '0') {       //  0인 경우 아래 칸 건널 수 있다.
-                arr[1][i] = 1;
-            } else if (bridgeStr.charAt(i) == '1') {    // 0인 경우 위 칸이 건널 수 있다.
-                arr[0][i] = 1;
-            }
-        }
-
-//        System.out.println("BridgeMaker.makeBridge() arr 타입");
-//        for (int[] a : arr) {
-//            System.out.println(Arrays.toString(a));
-//        }
-
+        makeArr();//      printArr();
         return changeStrToBridge(bridgeStr);
     }
 
@@ -61,85 +160,26 @@ public class BridgeMaker {
     }
 
 
-    // String 00111010101  => List<String> 형태로 바꾼다.
-    // 10101x 이면?
-    public List<String> changeStrToBridge(String inputBridgeStr) {
-
-        List<String> result = new ArrayList<>();
-        String up = "[";
-        String down = "[";
-
-        for (int i = 0; i < inputBridgeStr.length(); i++) {
-
-            if (inputBridgeStr.charAt(i) == '1') {
-
-                if (i == inputBridgeStr.length() - 1) {
-                    up += " O ";
-                    down += "   ";
-                    continue;
-                }
-                up += " O |";
-                down += "   |";
-
-            } else if (inputBridgeStr.charAt(i) == '0') {
-
-                if (i == inputBridgeStr.length() - 1) {
-                    down += " O ";
-                    up += "   ";
-                    continue;
-                }
-                down += " O |";
-                up += "   |";
-
-            } else if (inputBridgeStr.charAt(i) == 'x') {        // x, y => 매치 되지 않는 상황인 경우
-
-                down += "   ";
-                up += " X ";
-                break;      //여기서 더이상 다리 만들기를 끝내야한다.
-
-            } else if (inputBridgeStr.charAt(i) == 'y') {       // 밑에가 x인 경우
-
-                down += " X ";
-                up += "   ";
-                break;      //여기서 더이상 다리 만들기를 끝내야한다.
-            }
-        }
-
-        up += "]";
-        down += "]";
-        result.add(up);
-        result.add(down);
-        return result;
-
-    }
 
     public List<String> makeBridge(int size) {
-        String inputBridgeStr = "";
-
-        if (size < 3 || size > 20) {
-            System.out.println(errorMsg + " 3 이상 20 이하의 숫자만 입력 가능합니다.");
-            throw new IllegalArgumentException(errorMsg + " 3 이상 20 이하의 숫자만 입력 가능합니다.");
-        }
-
-        bridgeStr = "";     // while문에 들어가기 때문에  초기화 꼭 필요
-
-        for (int i = 0; i < size; i++) {
-            inputBridgeStr += bridgeNumberGenerator.generate();
-        }
-
-
-        List<String> res = new ArrayList<>();
-
+        String inputBridgeStr = forRecursive(size);
+        bridgeStr = "";
+        res = new ArrayList<>();
         for (char c : inputBridgeStr.toCharArray()) {
             if (c == '1') {
-                res.add("U");
-                continue;
+                res.add("U"); continue;
             }
             res.add("D");
-        }
-
-        return res;
+        }   return res;
     }
 
+    public String forRecursive(int size) {
+        String res = "";
+
+        for (int i = 0; i < size; ++i) {
+            res += bridgeNumberGenerator.generate();
+        }
+        return res;
+    }
 
 }
