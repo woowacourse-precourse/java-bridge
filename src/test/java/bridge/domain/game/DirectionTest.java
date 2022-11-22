c9be60d41dd13572a850a095353f8791980203b2
minju412 <ln8847@ajou.ac.kr>
2022-11-22 22:22:52 +0900

    test(DirectionTest): add tests

--- src/test/java/bridge/domain/game/DirectionTest.java
+++ src/test/java/bridge/domain/game/DirectionTest.java
@@ -4,20 +4,23 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 class DirectionTest {
+
+    @DisplayName("방향이 U 또는 D 가 아니라면 예외가 발생한다.")
+    @ParameterizedTest
+    @CsvSource({"'r'", "'4'", "'!'", "'u'"})
+    void createLength_with_invalid_size(String direction) {
+        assertThatThrownBy(() -> new Direction(direction))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
 
     @DisplayName("방향이 공백이라면 예외가 발생한다.")
     @Test
     void createDirectionWithBlank() {
         assertThatThrownBy(() -> new Direction(""))
-                .isInstanceOf(IllegalArgumentException.class);
-    }
-
-    @DisplayName("방향이 U 또는 D 가 아니라면 예외가 발생한다.")
-    @Test
-    void createDirectionWithInvalidCharacter() {
-        assertThatThrownBy(() -> new Direction("r"))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 }
