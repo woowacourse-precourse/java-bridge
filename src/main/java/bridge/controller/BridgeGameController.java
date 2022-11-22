b2665a5d1e7bc0270bc2a617e70f10cac35d6abc
Grooomit <tmd5972@gmail.com>
2022-11-22 22:03:28 +0900

    feat: 다리길이 입력 예외 시 메시지 출력 및 입력값 다시 받기

--- src/main/java/bridge/controller/BridgeGameController.java
+++ src/main/java/bridge/controller/BridgeGameController.java
@@ -6,13 +6,18 @@ import bridge.view.View;
 public class BridgeGameController {
     public static void setGame() {
         View.gameStartMessage();
+        int startNumber = 1;
 
-        View.requestBridgeLengthMessage();
-        try{
-            InputView inputView = new InputView();
-            int bridgeSize = inputView.readBridgeSize();
-        }catch (IllegalArgumentException e) {
-            View.exceptionMessage(e);
+        while (startNumber != 0) {
+            View.requestBridgeLengthMessage();
+            try {
+                InputView inputView = new InputView();
+                int bridgeSize = inputView.readBridgeSize();
+                startNumber = 0;
+            } catch (IllegalArgumentException e) {
+                View.exceptionMessage(e);
+                startNumber = 1;
+            }
         }
     }
 }
