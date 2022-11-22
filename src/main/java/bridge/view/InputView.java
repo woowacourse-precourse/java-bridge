f47d496ee15b7b107641b4175ecbb3bd3daa6114
권태현 <kygkth2011@gmail.com>
2022-11-22 23:13:08 +0900

    feat(bridge): 게임 종료 커맨드 잘못 입력시 다시 입력 받음

--- src/main/java/bridge/view/InputView.java
+++ src/main/java/bridge/view/InputView.java
@@ -38,9 +38,14 @@ public class InputView {
      * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
      */
     public String readGameCommand() {
-        System.out.println("게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)");
-        String input = Console.readLine();
-        CommandInput commandInput = new CommandInput(input);
-        return commandInput.getCommandInput();
+        try{
+            System.out.println("게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)");
+            String input = Console.readLine();
+            CommandInput commandInput = new CommandInput(input);
+            return commandInput.getCommandInput();
+        } catch (IllegalArgumentException e){
+            System.out.println(e.getMessage());
+            return readGameCommand();
+        }
     }
 }
