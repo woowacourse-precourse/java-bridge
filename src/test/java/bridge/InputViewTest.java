fbdd33d443dc31265a7e28f490f0c23f73ad1c3b
dvlpr0 <rladjswkd@naver.com>
2022-11-22 15:02:53 +0900

    test: 사용자가 입력한 이동할 칸에 대한 유효성 검사 테스트 및 유효한 다리길이 입력에 대한 테스트 추가

--- src/test/java/bridge/InputViewTest.java
+++ src/test/java/bridge/InputViewTest.java
@@ -1,5 +1,6 @@
 package bridge;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 
 import org.junit.jupiter.api.DisplayName;
@@ -7,14 +8,40 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
 import java.io.ByteArrayInputStream;
+import java.util.List;
 
 public class InputViewTest {
     @DisplayName("사용자가 입력한 다리 길이에 대한 유효성 검사 테스트")
     @ValueSource(strings = {"a", "1", "21"})
     @ParameterizedTest
-    void readBridgeSizeTest(String input) {
+    void readBridgeSizeInvalidTest(String input) {
         System.setIn(new ByteArrayInputStream(input.getBytes()));
         assertThatThrownBy(() -> new InputView().readBridgeSize())
                 .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("사용자가 입력한 다리 길이에 대한 유효성 검사 테스트")
+    @ValueSource(strings = {"3", "15", "20"})
+    @ParameterizedTest
+    void readBridgeSizeValidTest(String input) {
+        System.setIn(new ByteArrayInputStream(input.getBytes()));
+        assertThat(new InputView().readBridgeSize()).isBetween(3, 20);
+    }
+
+    @DisplayName("사용자가 입력한 이동할 칸에 대한 유효성 검사 테스트")
+    @ValueSource(strings = {"u", "d", "0"})
+    @ParameterizedTest
+    void readMovingInvalidTest(String input) {
+        System.setIn(new ByteArrayInputStream(input.getBytes()));
+        assertThatThrownBy(() -> new InputView().readMoving())
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("사용자가 입력한 이동할 칸에 대한 유효성 검사 테스트")
+    @ValueSource(strings = {"U", "D"})
+    @ParameterizedTest
+    void readMovingValidTest(String input) {
+        System.setIn(new ByteArrayInputStream(input.getBytes()));
+        assertThat(List.of("U", "D")).contains(new InputView().readMoving());
     }
 }
