e26a815f7351a357cb7dcb841324e73aa86e7584
jxixeun <jxixeun@gmail.com>
2022-11-22 23:29:29 +0900

    test: 입력 테스트 수정

--- src/test/java/bridge/InputViewTest.java
+++ src/test/java/bridge/InputViewTest.java
@@ -2,6 +2,8 @@ package bridge;
 
 import camp.nextstep.edu.missionutils.test.NsTest;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeTest;
 import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
@@ -14,34 +16,30 @@ class InputViewTest extends NsTest {
     private static final String MOVING_COMMAND_ERROR = "[ERROR] 이동할 칸은 U와 D 중 하나의 문자여야 합니다.";
     private static final String BRIDGE_SIZE_INPUT_ERROR = "[ERROR] 다리 길이는 3부터 20 사이의 숫자여야 합니다.";
 
-    @Test
-    void 다리_길이에_숫자_아닌_입력(){
+
+    @ValueSource(strings = {"999", "0", "-123", "ABCDEFG"})
+    @ParameterizedTest
+    void 다리_길이_입력(String input){
         assertSimpleTest(() -> {
-            runException("abcefs!!fasdj");
+            runException(input);
             assertThat(output()).contains(BRIDGE_SIZE_INPUT_ERROR);
         });
     }
 
-    @Test
-    void 다리_길이에_범위_아닌_숫자_입력(){
+    @ValueSource(strings = {"A", "abcdeafdgsasdf", "-123", " "})
+    @ParameterizedTest
+    void 이동_커맨드가_아닌_입력(String input) {
         assertSimpleTest(() -> {
-            runException("21");
-            assertThat(output()).contains(BRIDGE_SIZE_INPUT_ERROR);
-        });
-    }
-
-    @Test
-    void 이동_커맨드가_아닌_입력() {
-        assertSimpleTest(() -> {
-            runException("3","A");
+            runException("3", input);
             assertThat(output()).contains(MOVING_COMMAND_ERROR);
         });
     }
 
-    @Test
-    void 재시작_커맨드가_아닌_입력(){
+    @ValueSource(strings = {"U", "abcdeafdgsasdf", "-123", " "})
+    @ParameterizedTest
+    void 재시작_커맨드가_아닌_입력(String input){
         assertRandomNumberInRangeTest(() -> {
-            runException("3", "U", "U", "abc");
+            runException("3", "U", "U", input);
             assertThat(output()).contains(RETRY_COMMAND_ERROR);
         }, 1, 0, 1);
     }
