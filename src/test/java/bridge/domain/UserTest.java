1d83a9e4042581db42341ccad1f67064ded6e168
Song kyeong seok <skj0922@kookmin.ac.kr>
2022-11-22 21:21:12 +0900

    test(UserTest): 유저 위치 증가 테스트 구현

--- src/test/java/bridge/domain/UserTest.java
+++ src/test/java/bridge/domain/UserTest.java
@@ -4,13 +4,19 @@ import bridge.domain.user.MoveType;
 import bridge.domain.user.User;
 import java.util.List;
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.ParameterizedTest;
 
 public class UserTest {
 
-    User user = new User();
+    User user;
+
+    @BeforeEach
+    void setUp() {
+        user = new User();
+        Assertions.assertThat(user).isNotNull();
+    }
 
     @Test
     @DisplayName("유저의 입력에 따라 다리 건너기 단위 테스트 (모두 건너기 성공)")
@@ -23,26 +29,40 @@ public class UserTest {
         }
 
         String result = user.getUserPathLog();
-        Assertions.assertThat(result).isEqualTo("[ O |   |   ]\n[   | O | O ]\n");
+        Assertions.assertThat(result).isEqualTo("[ O |   |   ]\n[   | O | O ]\n\n");
     }
 
     @Test
     @DisplayName("다리 건너기에 실패 했을 때 경로 초기화 확인")
     public void crossBridgeFailTest() {
         Bridge bridge = Bridge.initBridge(List.of("U", "D", "D", "U"));
-        List<MoveType> moveTypes = List.of(MoveType.UP_TYPE, MoveType.DOWN_TYPE, MoveType.DOWN_TYPE, MoveType.DOWN_TYPE);
+        List<MoveType> moveTypes = List.of(MoveType.UP_TYPE, MoveType.DOWN_TYPE, MoveType.DOWN_TYPE,
+            MoveType.DOWN_TYPE);
 
         for (MoveType moveType : moveTypes) {
             user.cross(bridge, moveType);
         }
 
         String beforeResult = user.getUserPathLog();
-        Assertions.assertThat(beforeResult).isEqualTo("[ O |   |   |   ]\n[   | O | O | X ]\n");
+        Assertions.assertThat(beforeResult).isEqualTo("[ O |   |   |   ]\n[   | O | O | X ]\n\n");
 
         user.resetUserData();
 
         String afterResult = user.getUserPathLog();
-        Assertions.assertThat(afterResult).isEqualTo("[  ]\n[  ]\n");
+        Assertions.assertThat(afterResult).isEqualTo("[  ]\n[  ]\n\n");
+    }
+
+    @Test
+    @DisplayName("유저 위치 증가 테스트")
+    public void increasePositionTest() {
+        Bridge bridge = Bridge.initBridge(List.of("U", "D", "D", "U"));
+        List<MoveType> moveTypes = List.of(MoveType.UP_TYPE, MoveType.DOWN_TYPE, MoveType.DOWN_TYPE);
+
+        for (MoveType moveType : moveTypes) {
+            user.cross(bridge, moveType);
+        }
+
+        Assertions.assertThat(user).extracting("nextLocation").isEqualTo(3);
     }
 
 }
