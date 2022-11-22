72b92678d37bc3d837ad9c8cb4d54b093004ef4f
anso33 <hyeon131812@gmail.com>
2022-11-22 21:08:25 +0900

    [REFACTOR] feat application constant string

--- src/main/java/exception/InputException.java
+++ src/main/java/exception/InputException.java
@@ -1,5 +1,7 @@
 package exception;
 
+import static constant.ApplicationMessage.*;
+
 public class InputException {
 
   public static boolean isRightBridgeSize(int size) {
@@ -7,17 +9,17 @@ public class InputException {
   }
 
   public static boolean isRightBridgeMove(String move) {
-    if (move.equals("U"))
+    if (move.equals(UP))
       return true;
-    if (move.equals("D"))
+    if (move.equals(DOWN))
       return true;
     return false;
   }
 
   public static boolean isRightBridgePlay(String play) {
-    if (play.equals("Q"))
+    if (play.equals(QUIT))
       return true;
-    if (play.equals("R"))
+    if (play.equals(RETRY))
       return true;
     return false;
   }
