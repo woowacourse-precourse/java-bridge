61bcc099324a87d34e5a7d09405e8340b4a9c722
이지영 <wldudwldhd@naver.com>
2022-11-22 21:47:45 +0900

    feat : 재시작여부 화면 출력 함수 작성

--- src/main/java/bridge/OutputView.java
+++ src/main/java/bridge/OutputView.java
@@ -52,6 +52,15 @@ public class OutputView {
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
     public void printResult(int num, List<String> bridge, int isAnswer) {
+        System.out.println("최종 게임 결과");
+        printMap(num, bridge, isAnswer);
+        System.out.println("");
+        if(isAnswer == 0){
+            System.out.println("게임 성공 여부 : 실패");
+        }
+        if(isAnswer == 1){
+            System.out.println("게임 성공 여부 : 성공");
+        }
 
     }
     public void checkValue(char c, List<String> upSting, List<String> downString) {
@@ -128,14 +137,18 @@ public class OutputView {
     public void printGameCnt(int gameCnt) {
         System.out.println("총 시도한 횟수: "+gameCnt);
     }
+    public void printMoveQuestion() {
+        System.out.println("이동할 칸을 선택해주세요. (위: U, 아래: D)");
+    }
 
     public void printStartComment() {
         System.out.println("다리 건너기 게임을 시작합니다.");
         System.out.println("");
         System.out.println("다리의 길이를 입력해주세요.");
     }
-
-    public void printMoveQuestion() {
-        System.out.println("이동할 칸을 선택해주세요. (위: U, 아래: D)");
+    public void printRestartComment(){
+        System.out.println("게임을 다시 시도하지 여부를 입력해주세요. (재시도: R, 종료: Q");
     }
+
+
 }
