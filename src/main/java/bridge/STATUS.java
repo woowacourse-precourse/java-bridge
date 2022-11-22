279a72c641f7062fe62e7ee8bf51669f4fdb24d7
KSB-skyBlueLover <gogotkdqja@yonsei.ac.kr>
2022-11-22 16:17:29 +0900

    refactor: change the structure of enum STATUS

--- src/main/java/bridge/STATUS.java
+++ src/main/java/bridge/STATUS.java
@@ -1,7 +1,17 @@
 package bridge;
 
 public enum STATUS {
-    RUNNING,
-    END_WITH_SUCCESS,
-    END_WITH_FAILURE
+    RUNNING("실패"),
+    END_WITH_SUCCESS("성공"),
+    END_WITH_FAILURE("실패");
+
+    private final String success;
+
+    STATUS(String success) {
+        this.success = success;
+    }
+
+    public String getSuccess(){
+        return success;
+    }
 }
