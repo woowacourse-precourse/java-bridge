0e9054cceefb64d391742b9f5cd1cecc5de76522
DavinAn <blueapple1030@gmail.com>
2022-11-22 22:48:13 +0900

    test : add test case for when user fails to cross bridge

--- src/test/java/bridge/BridgeGameTest.java
+++ src/test/java/bridge/BridgeGameTest.java
@@ -1,11 +1,23 @@
 package bridge;
 
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import bridge.controller.BridgeController;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
+import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.InputStream;
 import java.io.PrintStream;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Stream;
 
 public class BridgeGameTest {
 
@@ -25,5 +37,23 @@ public class BridgeGameTest {
     void tearDown() {
         System.setIn(standardIn);
         System.setOut(standardOut);
+    }
+
+
+
+    private final BridgeController bridgeController = new BridgeController();
+
+
+    @DisplayName("다리 길이 3. 게임 실패 경우")
+    @ParameterizedTest
+    @MethodSource("bridge_len3_lose_param")
+    void bridge_len3_lose(String input, List<String> expected) {
+        List<String> bridge_len3 = Arrays.asList("D", "D", "U");
+        System.setIn(new ByteArrayInputStream(input.getBytes()));
+        bridgeController.newGame(bridge_len3);
+        assertThat(outputStream.toString().trim()).contains(expected);
+    }
+
+    static Stream<Arguments> bridge_len3_lose_param() {
     }
 }
