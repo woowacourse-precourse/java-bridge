66b1ddd60f3309f367612d3e18806a3fc9734b9f
junyong <jymoon1121@gmail.com>
2022-11-25 23:38:08 +0900

    refactor: 불필요한 부분 삭제

--- src/test/java/bridge/BridgeMakerTest.java
+++ src/test/java/bridge/BridgeMakerTest.java
@@ -1,7 +1,6 @@
 package bridge;
 
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
@@ -19,9 +18,5 @@ class BridgeMakerTest {
     void makeBridge(int size) {
         List<String> bridge = bridgeMaker.makeBridge(size);
         assertEquals(bridge.size(), size);
-    }
-
-    @Test
-    void name() {
     }
 }
--- src/test/java/bridge/BridgeTest.java
+++ src/test/java/bridge/BridgeTest.java
@@ -6,7 +6,6 @@ import org.junit.jupiter.params.provider.CsvSource;
 
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.*;
 
 class BridgeTest {
 
