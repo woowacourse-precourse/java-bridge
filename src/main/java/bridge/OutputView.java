a2b90e1cc919579582b31c6a09ba26b34b896bbb
junyong <jymoon1121@gmail.com>
2022-11-25 23:31:18 +0900

    feat(OutputView) : 줄바꿈 메서드 추가

--- src/main/java/bridge/OutputView.java
+++ src/main/java/bridge/OutputView.java
@@ -41,4 +41,8 @@ public class OutputView {
     public void printGameStart() {
         System.out.println("다리 건너기 게임을 시작합니다.");
     }
+
+    public void enter() {
+        System.out.println();
+    }
 }
