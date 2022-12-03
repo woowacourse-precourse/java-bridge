6b38a9336a3ff4eeca3a340c0655d5fe425ff23a
impati <impati0716@icloud.com>
2022-12-03 17:55:52 +0900

    refactor : FrontController 개념 도입
    Main 기능 호출 담당 -> Main 설정 및 run() 역할 담당
    기능 호출 담당 -> FrontController

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -1,5 +1,6 @@
 package bridge;
 
+import bridge.controller.FrontController;
 import bridge.domain.Bridge;
 import bridge.domain.BridgeConst;
 import bridge.domain.BridgeGame;
@@ -8,43 +9,12 @@ import bridge.view.OutputView;
 import bridge.view.ProxyInputView;
 
 public class Application {
-    private static Input inputView = new ProxyInputView(new InputValidation());
-    private static OutputView outputView = new OutputView();
-
     public static void main(String[] args) {
-        startGame();
-        BridgeGame bridgeGame = new BridgeGame(makeBridge());
-        do {
-            playingGame(bridgeGame);
-        }while (isRetry(bridgeGame));
-        finalResult(bridgeGame);
+        FrontController frontController = configuration();
+        frontController.run();
     }
-    static void finalResult(BridgeGame bridgeGame){
-        outputView.printResult(bridgeGame.returnMap(),bridgeGame.getCount());
+    static FrontController configuration(){
+        return new FrontController(new ProxyInputView(new InputValidation()),new OutputView());
     }
-    static boolean isRetry(BridgeGame bridgeGame){
-        if(bridgeGame.isComplete())return false;
-        String command = inputView.readGameCommand();
-        if(command.equals(BridgeConst.QUIT)) return false;
-        bridgeGame.retry();
-        return true;
-    }
-    static void playingGame(BridgeGame bridgeGame){
-        while(bridgeGame.isContinue()) {
-            String moving = inputView.readMoving();
-            bridgeGame.move(moving);
-            bridgeGame.changeStatus();
-            String map = bridgeGame.returnMap();
-            outputView.printMap(map);
-        }
-    }
-    static void startGame(){
-        System.out.println("다리 건너기 게임을 시작합니다.");
-    }
-    static Bridge makeBridge(){
-        BridgeMaker bridgeMaker = new BridgeMaker(new BridgeRandomNumberGenerator());
-        int size = inputView.readBridgeSize();
-        Bridge bridge = new Bridge(bridgeMaker.makeBridge(size));
-        return bridge;
-    }
+
 }
