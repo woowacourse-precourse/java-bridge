16d1733aeca39f6e5dd4401881cb7d84b9ec2d2f
parkjungyoon <tbr06057@naver.com>
2022-12-08 23:11:25 +0900

    refactor: 하드 코딩된 부분을 enum타입을 활용하도록 변경

--- src/main/java/bridge/validator/CommandValidator.java
+++ src/main/java/bridge/validator/CommandValidator.java
@@ -1,12 +1,14 @@
 package bridge.validator;
 
+import bridge.MoveCommand;
+
 public class CommandValidator {
-    private final static String INVALID_TYPE_ERROR_MESSAGE = "[ERROR] 이동할 칸은 U와 D로만 입력할 수 있습니다.";
+    private final static String INVALID_MOVE_TYPE_ERROR_MESSAGE = "[ERROR] 이동할 칸은 U와 D로만 입력할 수 있습니다.";
     private final static String INVALID_RETRY_TYPE_ERROR_MESSAGE = "[ERROR] 게임 재시작 여부는 R과 Q로만 입력할 수 있습니다.";
 
     public static void validateInvalidType(String input) {
-        if (!input.equals("U") && !input.equals("D")) {
-            throw new IllegalArgumentException(INVALID_TYPE_ERROR_MESSAGE);
+        if (!input.equals(MoveCommand.TOP.getMoveCommand()) && !input.equals(MoveCommand.BOTTOM.getMoveCommand())) {
+            throw new IllegalArgumentException(INVALID_MOVE_TYPE_ERROR_MESSAGE);
         }
     }
 
