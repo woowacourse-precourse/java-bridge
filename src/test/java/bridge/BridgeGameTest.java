0ed38e77ee7a574ec2c36c5929e7d2f2450409f1
parkjungyoon <tbr06057@naver.com>
2022-12-09 00:21:18 +0900

    test: 리팩토링에 따라 테스트코드 변경, 추가로 테스트 코드 작성 필요함

--- src/test/java/bridge/BridgeGameTest.java
+++ src/test/java/bridge/BridgeGameTest.java
@@ -1,5 +1,6 @@
 package bridge;
 
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
@@ -13,45 +14,56 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 
 class BridgeGameTest {
-    @DisplayName("이동이 성공했는지 확인하는 메서드 테스트")
+    static BridgeGame bridgeGame;
+
+    @BeforeAll
+    public static void setup() {
+        bridgeGame = new BridgeGame();
+    }
+    
+    @DisplayName("모든 이동이 성공했는지 확인하는 메서드 테스트")
     @Test
     void move() {
-        BridgeGame bridgeGame = new BridgeGame();
+        String command1 = "U";
+        String command2 = "U";
+        String command3 = "U";
 
-        int nextIndex = 1;
-        String command = "U";
-        List<String> bridge = Arrays.asList("U", "D", "U");
-        assertThat(bridgeGame.move(command, bridge.get(nextIndex))).isEqualTo(false);
+        Bridge bridge = new Bridge(Arrays.asList("U", "D", "U"));
+        assertThat(bridgeGame.move(bridge, command1)).isEqualTo(true);
+        assertThat(bridgeGame.move(bridge, command2)).isEqualTo(false);
+        assertThat(bridgeGame.move(bridge, command3)).isEqualTo(true);
     }
 
-    @DisplayName("움직이는 명령어가 'U', 'D'가 아닌 경우 예외가 발생한다.")
-    @CsvSource({"k", "&&", "f4j"})
-    @ParameterizedTest
-    void incorrectBridgeCommand(String command) {
-        BridgeGame bridgeGame = new BridgeGame();
 
-        int nextIndex = 2;
-        List<String> bridge = Arrays.asList("U", "D", "U");
-        assertThatThrownBy(() -> bridgeGame.move(command, bridge.get(nextIndex)))
-                .isInstanceOf(IllegalArgumentException.class);
-    }
-
-    @DisplayName("재시작 여부 확인하는 메서드 테스트")
-    @Test
-    void retry() {
-        BridgeGame bridgeGame = new BridgeGame();
-
-        String command = "R";
-        assertThat(bridgeGame.retry(command)).isEqualTo(true);
-    }
-
-    @DisplayName("움직이는 명령어가 'U', 'D'가 아닌 경우 예외가 발생한다.")
-    @CsvSource({"k", "&&", "f4j"})
-    @ParameterizedTest
-    void incorrectRetryCommand(String command) {
-        BridgeGame bridgeGame = new BridgeGame();
-
-        assertThatThrownBy(() -> bridgeGame.retry(command))
-                .isInstanceOf(IllegalArgumentException.class);
-    }
+//
+//    @DisplayName("움직이는 명령어가 'U', 'D'가 아닌 경우 예외가 발생한다.")
+//    @CsvSource({"k", "&&", "f4j"})
+//    @ParameterizedTest
+//    void incorrectBridgeCommand(String command) {
+//        BridgeGame bridgeGame = new BridgeGame();
+//
+//        int nextIndex = 2;
+//        List<String> bridge = Arrays.asList("U", "D", "U");
+//        assertThatThrownBy(() -> bridgeGame.move(command, bridge.get(nextIndex)))
+//                .isInstanceOf(IllegalArgumentException.class);
+//    }
+//
+//    @DisplayName("재시작 여부 확인하는 메서드 테스트")
+//    @Test
+//    void retry() {
+//        BridgeGame bridgeGame = new BridgeGame();
+//
+//        String command = "R";
+//        assertThat(bridgeGame.retry(command)).isEqualTo(true);
+//    }
+//
+//    @DisplayName("움직이는 명령어가 'U', 'D'가 아닌 경우 예외가 발생한다.")
+//    @CsvSource({"k", "&&", "f4j"})
+//    @ParameterizedTest
+//    void incorrectRetryCommand(String command) {
+//        BridgeGame bridgeGame = new BridgeGame();
+//
+//        assertThatThrownBy(() -> bridgeGame.retry(command))
+//                .isInstanceOf(IllegalArgumentException.class);
+//    }
 }
--- src/test/java/bridge/view/InputViewTest.java
+++ src/test/java/bridge/view/InputViewTest.java
@@ -12,41 +12,41 @@ import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 class InputViewTest {
     private static final String ERROR_MESSAGE = "[ERROR]";
 
-    @DisplayName("다리 길이가 숫자가 아닌 경우에 대한 예외 처리")
-    @CsvSource({"k","&&","f4j"})
-    @ParameterizedTest
-    void nonNumericBridgeLength(String string) {
-        InputStream in = generateUserInput(string);
-        System.setIn(in);
-
-        assertThatThrownBy(() -> InputView.readBridgeSize())
-                .isInstanceOf(IllegalArgumentException.class)
-                .hasMessageContaining(ERROR_MESSAGE);
-    }
-
-    @DisplayName("다리 길이가 3이상 20이하가 아닌 경우에 대한 예외 처리")
-    @CsvSource({"2","-33","100"})
-    @ParameterizedTest
-    void outOfRangeBridgeLength(String string) {
-        InputStream in = generateUserInput(string);
-        System.setIn(in);
-
-        assertThatThrownBy(() -> InputView.readBridgeSize())
-                .isInstanceOf(IllegalArgumentException.class)
-                .hasMessageContaining(ERROR_MESSAGE);
-    }
-
-    @DisplayName("이동할 칸이 U 또는 D가 아닌 경우에 대한 예외 처리")
-    @CsvSource({"k","...","4","X","ㄸ어모ㅓㅊ"})
-    @ParameterizedTest
-    void invalidType(String string) {
-        InputStream in = generateUserInput(string);
-        System.setIn(in);
-
-        assertThatThrownBy(() -> InputView.readMoving())
-                .isInstanceOf(IllegalArgumentException.class)
-                .hasMessageContaining(ERROR_MESSAGE);
-    }
+//    @DisplayName("다리 길이가 숫자가 아닌 경우에 대한 예외 처리")
+//    @CsvSource({"k","&&","f4j"})
+//    @ParameterizedTest
+//    void nonNumericBridgeLength(String string) {
+//        InputStream in = generateUserInput(string);
+//        System.setIn(in);
+//
+//        assertThatThrownBy(() -> InputView.readBridgeSize())
+//                .isInstanceOf(IllegalArgumentException.class)
+//                .hasMessageContaining(ERROR_MESSAGE);
+//    }
+//
+//    @DisplayName("다리 길이가 3이상 20이하가 아닌 경우에 대한 예외 처리")
+//    @CsvSource({"2","-33","100"})
+//    @ParameterizedTest
+//    void outOfRangeBridgeLength(String string) {
+//        InputStream in = generateUserInput(string);
+//        System.setIn(in);
+//
+//        assertThatThrownBy(() -> InputView.readBridgeSize())
+//                .isInstanceOf(IllegalArgumentException.class)
+//                .hasMessageContaining(ERROR_MESSAGE);
+//    }
+//
+//    @DisplayName("이동할 칸이 U 또는 D가 아닌 경우에 대한 예외 처리")
+//    @CsvSource({"k","...","4","X","ㄸ어모ㅓㅊ"})
+//    @ParameterizedTest
+//    void invalidType(String string) {
+//        InputStream in = generateUserInput(string);
+//        System.setIn(in);
+//
+//        assertThatThrownBy(() -> InputView.readMoving())
+//                .isInstanceOf(IllegalArgumentException.class)
+//                .hasMessageContaining(ERROR_MESSAGE);
+//    }
 
     public static InputStream generateUserInput(String input) {
         return new ByteArrayInputStream(input.getBytes());
