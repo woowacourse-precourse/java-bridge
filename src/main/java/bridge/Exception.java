3e6da56995598132e23fe0db0f92be83522d0229
hingjae <ljh23270@naver.com>
2022-12-09 19:19:13 +0900

    숫자 여부 체크

--- src/main/java/bridge/Exception.java
+++ src/main/java/bridge/Exception.java
@@ -1,2 +1,17 @@
-package bridge;public class Exception {
+package bridge;
+
+public class Exception {
+    public static void isNum(String input) {
+        for (char c : input.toCharArray()) {
+            if (!(Character.isDigit(c))) {
+                throw new IllegalArgumentException("숫자만 입력할 수 있습니다.");
+            }
+        }
+    }
+
+    public static void isUorD(String input) {
+        if(input != "U" && input != "D") {
+            throw new IllegalArgumentException("U 또는 D만 입력할 수 있습니다.");
+        }
+    }
 }
