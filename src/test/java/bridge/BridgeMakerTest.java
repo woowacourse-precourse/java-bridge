681d9a6ea0df91859bd4f458dbb76dbd00cc02f9
hingjae <ljh23270@naver.com>
2022-12-09 19:21:17 +0900

    다리 그리기 테스트

--- src/test/java/bridge/BridgeMakerTest.java
+++ src/test/java/bridge/BridgeMakerTest.java
@@ -1,4 +1,23 @@
+package bridge;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class BridgeMakerTest {
-  
+
+    BridgeMaker bridgeMaker = new BridgeMaker(new BridgeRandomNumberGenerator());
+    @Test
+    void 다리길이만큼_다리생성() {
+//        List bridge = new ArrayList();
+        List bridge = bridgeMaker.makeBridge(4);
+        Assertions.assertThat(bridge.size()).isEqualTo(4);
+        Assertions.assertThat(bridge).isInstanceOf(List.class);
+
+    }
+
 }
