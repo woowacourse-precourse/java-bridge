3a0816b5ed477ef8bf7a26940d224d0c0d15018d
fakerdeft <whakswp123@naver.com>
2022-12-11 21:29:46 +0900

    feat: 상수명 수정

--- src/main/java/bridge/util/Constant.java
+++ src/main/java/bridge/util/Constant.java
@@ -3,15 +3,14 @@ package bridge.util;
 import bridge.BridgeRandomNumberGenerator;
 import bridge.controller.BridgeGame;
 import bridge.model.BridgeMaker;
-import bridge.model.BridgeNumberGenerator;
 import bridge.view.InputView;
 import bridge.view.OutputView;
 
 public class Constant {
-    public static final InputView inputView = new InputView();
-    public static final OutputView outputView = new OutputView();
-    public static final Exceptions exceptions = new Exceptions();
-    public static final BridgeNumberGenerator numberGenerator = new BridgeRandomNumberGenerator();
-    public static final BridgeMaker bridgeMaker = new BridgeMaker(numberGenerator);
-    public static final BridgeGame bridgeGame = new BridgeGame();
+    public static final InputView INPUT_VIEW = new InputView();
+    public static final OutputView OUTPUT_VIEW = new OutputView();
+    public static final Exceptions EXCEPTIONS = new Exceptions();
+    public static final BridgeRandomNumberGenerator NUMBER_GENERATOR = new BridgeRandomNumberGenerator();
+    public static final BridgeMaker BRIDGE_MAKER = new BridgeMaker(NUMBER_GENERATOR);
+    public static final BridgeGame BRIDGE_GAME = new BridgeGame();
 }
